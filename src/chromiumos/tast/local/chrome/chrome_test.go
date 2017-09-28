c9ebbca17dea58c0b87262d65f4f5482420ecf59
Daniel Erat <derat@chromium.org>
2017-09-28 14:20:26 -0700

    tast-tests: Let Chrome choose its own debugging port.
    Make the local/chrome package pass --remote-debugging-port=0
    when restarting Chrome and read the resulting port from
    /home/chronos/DevToolsActivePort. This is apparently what
    Telemetry does now.
    
    BUG=none
    TEST=added a unit test; ui.ChromeSanity also still passes
    
    Change-Id: If7ad2bce48da64c762ba3b90c41bfebcb1009c12

---
+++ src/chromiumos/tast/local/chrome/chrome_test.go
@@ -0,0 +1,51 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package chrome
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestReadDebuggingPort(t *testing.T) {
+	td, err := ioutil.TempDir("", "chrome_test.")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { os.Remove(td) }()
+
+	for _, tc := range []struct {
+		name, data string
+		port       int // expected port or -1 if error is expected
+	}{
+		{"full", "56245\n/devtools/browser/01db187e-2e2a-42c5-833e-bd4dbea9e313", 56245},
+		{"oneline", "123", 123},
+		{"garbage", "foo", -1},
+		{"empty", "", -1},
+	} {
+		p := filepath.Join(td, tc.name)
+		if err = ioutil.WriteFile(p, []byte(tc.data), 0644); err != nil {
+			t.Fatal(err)
+		}
+		port, err := readDebuggingPort(p)
+		if tc.port == -1 {
+			if err == nil {
+				t.Errorf("readDebuggingPort(%q) (data %q) didn't return expected error", tc.name, tc.data)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("readDebuggingPort(%q) (data %q) returned error: %v", tc.name, tc.data, err)
+			} else if port != tc.port {
+				t.Errorf("readDebuggingPort(%q) (data %q) = %d; want %d", tc.name, tc.data, port, tc.port)
+			}
+		}
+	}
+
+	if _, err = readDebuggingPort(filepath.Join(td, "missing")); err == nil {
+		t.Error("readDebuggingPort didn't return expected error for missing file")
+	}
+}
