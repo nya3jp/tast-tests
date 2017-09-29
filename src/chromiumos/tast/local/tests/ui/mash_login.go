25c1714696af6013231c3f637974f75ac66289a4
James Cook <jamescook@chromium.org>
2017-09-29 10:44:27 -0700

    tast-tests: Add MashLogin to test chrome --mash
    * Add support for starting chrome with the --mash flag
    * Add test similar to the desktopui_MashLogin autotest
    * Clean up function comments to satisfy linter
    
    BUG=none
    TEST=tast -verbose run 172.18.165.182 ui.MashLogin
    
    Change-Id: I6ebe3a9297d3c899565567fbcf82e5ccdf35f237

---
+++ src/chromiumos/tast/local/tests/ui/mash_login.go
@@ -0,0 +1,27 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"chromiumos/tast/common/testing"
+	"chromiumos/tast/local/chrome"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: MashLogin,
+		Desc: "Checks that chrome --mash starts",
+		Attr: []string{"bvt", "chrome"},
+	})
+}
+
+// MashLogin is described above.
+func MashLogin(s *testing.State) {
+	cr, err := chrome.New(s.Context(), chrome.MashEnabled())
+	if err != nil {
+		s.Fatal("Chrome probably crashed on startup: ", err)
+	}
+	defer cr.Close(s.Context())
+}
