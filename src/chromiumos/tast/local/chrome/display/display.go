d7953e107cda521b77285906d2b14496f7d9dc9f
Daniel Erat <derat@chromium.org>
2017-11-03 17:48:45 -0700

    tast-tests: Add local/chrome/display.
    Add a new "display" package that can be used by local tests
    to interact with Chrome's chrome.system.display API. The
    package currently wraps only the getInfo and
    setDisplayProperties functions and associated objects.
    
    BUG=none
    TEST=verified via a test that i'm able to get info about the
         internal display and rotate it using
         setDisplayProperties
    
    Change-Id: I1558570bfea4086d9dd3466cf3dc13493d876b01
    Reviewed-on: https://chromium-review.googlesource.com/753853
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

---
+++ src/chromiumos/tast/local/chrome/display/display.go
@@ -0,0 +1,135 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package display wraps the chrome.system.display API.
+//
+// Functions require a chrome.Conn with permission to use the chrome.system.display API.
+// chrome.Chrome.TestAPIConn has such permission and may be passed here.
+package display
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"chromiumos/tast/local/chrome"
+)
+
+// Bounds holds onscreen bounds.
+// See https://developer.chrome.com/apps/system.display#type-Bounds.
+type Bounds struct {
+	Left   int `json:"left"`
+	Top    int `json:"top"`
+	Width  int `json:"width"`
+	Height int `json:"height"`
+}
+
+// Insets holds onscreen insets.
+// See https://developer.chrome.com/apps/system.display#type-Insets.
+type Insets struct {
+	Left   int `json:"left"`
+	Top    int `json:"top"`
+	Right  int `json:"right"`
+	Bottom int `json:"bottom"`
+}
+
+// DisplayMode holds a mode supported by the display.
+// See https://developer.chrome.com/apps/system.display#type-DisplayMode.
+type DisplayMode struct {
+	Width                int     `json:"width"`
+	Height               int     `json:"height"`
+	WidthInNativePixels  int     `json:"widthInNativePixels"`
+	HeightInNativePixels int     `json:"heightInNativePixels"`
+	UIScale              float64 `json:"uiScale"`
+	DeviceScaleFactor    float64 `json:"deviceScaleFactor"`
+	IsNative             bool    `json:"isNative"`
+	IsSelected           bool    `json:"isSelected"`
+}
+
+// Info holds information about a display and is returned by GetInfo.
+// See https://developer.chrome.com/apps/system_display#method-getInfo.
+type Info struct {
+	ID                string         `json:"id"`
+	Name              string         `json:"name"`
+	MirroringSourceID string         `json:"mirroringSourceId"`
+	IsPrimary         bool           `json:"isPrimary"`
+	IsInternal        bool           `json:"isInternal"`
+	IsEnabled         bool           `json:"isEnabled"`
+	IsUnified         bool           `json:"isUnified"`
+	DPIX              float64        `json:"dpiX"`
+	DPIY              float64        `json:"dpiY"`
+	Rotation          int            `json:"rotation"`
+	Bounds            *Bounds        `json:"bounds"`
+	Overscan          *Insets        `json:"overscan"`
+	WorkArea          *Bounds        `json:"workArea"`
+	Modes             []*DisplayMode `json:"modes"`
+	HasTouchSupport   bool           `json:"hasTouchSupport"`
+}
+
+// GetInfo calls chrome.system.display.getInfo to get information about connected displays.
+// See https://developer.chrome.com/apps/system_display#method-getInfo.
+func GetInfo(ctx context.Context, c *chrome.Conn) ([]Info, error) {
+	infos := make([]Info, 0)
+	err := c.EvalPromise(ctx,
+		`new Promise(function(resolve, reject) {
+			chrome.system.display.getInfo(function(info) { resolve(info); });
+		})`, &infos)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get display info: %v", err)
+	}
+	return infos, nil
+}
+
+// GetInternalInfo returns information about the internal display.
+// An error is returned if no internal display is present.
+func GetInternalInfo(ctx context.Context, c *chrome.Conn) (*Info, error) {
+	infos, err := GetInfo(ctx, c)
+	if err != nil {
+		return nil, err
+	}
+	for i := range infos {
+		if infos[i].IsInternal {
+			return &infos[i], nil
+		}
+	}
+	return nil, errors.New("no internal display")
+}
+
+// DisplayProperties holds properties to change and is passed to SetDisplayProperties.
+// nil fields are ignored. See https://developer.chrome.com/apps/system_display#method-setDisplayProperties.
+type DisplayProperties struct {
+	IsUnified         *bool        `json:"isUnified,omitempty"`
+	MirroringSourceID *string      `json:"mirroringSourceId,omitempty"`
+	IsPrimary         *bool        `json:"isPrimary,omitempty"`
+	Overscan          *Insets      `json:"overscan,omitempty"`
+	Rotation          *int         `json:"rotation,omitempty"`
+	BoundsOriginX     *int         `json:"boundsOriginX,omitempty"`
+	BoundsOriginY     *int         `json:"boundsOriginY,omitempty"`
+	DisplayMode       *DisplayMode `json:"displayMode,omitempty"`
+}
+
+// SetDisplayProperties updates the properties for the display specified by id.
+// See https://developer.chrome.com/apps/system_display#method-setDisplayProperties.
+func SetDisplayProperties(ctx context.Context, c *chrome.Conn, id string, dp DisplayProperties) error {
+	b, err := json.Marshal(&dp)
+	if err != nil {
+		return err
+	}
+	expr := fmt.Sprintf(
+		`new Promise(function(resolve, reject) {
+			chrome.system.display.setDisplayProperties(
+				%q, %s, function() {
+					resolve(chrome.runtime.lastError ? chrome.runtime.lastError.message : "");
+				});
+		})`, id, string(b))
+
+	msg := ""
+	if err = c.EvalPromise(ctx, expr, &msg); err != nil {
+		return err
+	} else if msg != "" {
+		return errors.New(msg)
+	}
+	return nil
+}
