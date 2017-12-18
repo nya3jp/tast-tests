9d24e6c6551d80c813d9ca83bd1b436e389f1903
Daniel Erat <derat@chromium.org>
2017-12-18 12:25:43 -0800

    tast-tests: Add "cros" test bundles.
    Add main functions for new "cros" local and remote test
    bundles. These bundles will be executed by the
    local_test_runner and remote_test_runner executables, which
    will live inside of the main tast repository.
    
    The new test locations are a bit cumbersome, so also add
    symlinks from the top of the repository to them.
    
    BUG=chromium:784944
    TEST=used them in conjunction with test runners and updated
         tast comand
    
    Change-Id: Icf9eacf00ee25520d8b56df721434bd2a640d776

---
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -0,0 +1,24 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package main implements the "cros" local test bundle.
+//
+// This executable contains standard Chrome OS tests.
+package main
+
+import (
+	"os"
+
+	"chromiumos/tast/bundle"
+
+	// These packages register their tests via init functions.
+	_ "chromiumos/tast/local/bundles/cros/example"
+	_ "chromiumos/tast/local/bundles/cros/power"
+	_ "chromiumos/tast/local/bundles/cros/security"
+	_ "chromiumos/tast/local/bundles/cros/ui"
+)
+
+func main() {
+	os.Exit(bundle.Local(os.Args[1:]))
+}
---
+++ src/chromiumos/tast/remote/bundles/cros/main.go
@@ -0,0 +1,21 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package main implements the "cros" remote test bundle.
+//
+// This executable contains standard Chrome OS tests.
+package main
+
+import (
+	"os"
+
+	"chromiumos/tast/bundle"
+
+	// These packages register their tests via init functions.
+	_ "chromiumos/tast/remote/bundles/cros/power"
+)
+
+func main() {
+	os.Exit(bundle.Remote(os.Args[1:]))
+}
