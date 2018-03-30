24adfacb4101aa7fcc0bb45242ee0800b4fa2434
Stephen Barber <smbarber@chromium.org>
2018-03-30 15:52:18 -0700

    tast-tests: add vm.StartTerminaVM
    Add a basic test that starts a Termina VM with concierge.
    
    BUG=chromium:725692
    TEST=vm.StartTerminaVM
    
    Change-Id: Ifbdecb60a42efd064ed77db9c38bb3e69a4b1499

--- src/chromiumos/tast/local/bundles/cros/main.go
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -17,6 +17,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/power"
 	_ "chromiumos/tast/local/bundles/cros/security"
 	_ "chromiumos/tast/local/bundles/cros/ui"
+	_ "chromiumos/tast/local/bundles/cros/vm"
 )
 
 func main() {
