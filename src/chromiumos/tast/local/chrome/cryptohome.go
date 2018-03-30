ef7e6f9a9cb7fc535ca2d633eb369ab29b464c1d
Stephen Barber <smbarber@chromium.org>
2018-03-30 15:32:51 -0700

    tast-tests: add CryptohomeHash function
    Add a function for getting the cryptohome hash for a user.
    
    BUG=chromium:725692
    TEST=use with follow-on StartTerminaVM test
    
    Change-Id: Id87aaa39888227fd529d125ec862820d7333d8c0

--- src/chromiumos/tast/local/chrome/cryptohome.go
+++ src/chromiumos/tast/local/chrome/cryptohome.go
@@ -10,10 +10,42 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 
 	"chromiumos/tast/testing"
 )
+
+var cryptohomeRegexp *regexp.Regexp
+
+func init() {
+	cryptohomeRegexp = regexp.MustCompile("^/home/user/([[:xdigit:]]+)$")
+}
+
+// userCryptohomePath gets the path to the logged in user's cryptohome.
+func userCryptohomePath(user string) (string, error) {
+	b, err := exec.Command("cryptohome-path", "user", user).Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to get cryptohome for %s: %v", user, err)
+	}
+
+	p := strings.TrimSpace(string(b))
+	return p, nil
+}
+
+// CryptohomeHash returns the logged-in user's cryptohome hash.
+func CryptohomeHash(c *Chrome) (string, error) {
+	p, err := userCryptohomePath(c.user)
+	if err != nil {
+		return "", err
+	}
+	m := cryptohomeRegexp.FindStringSubmatch(p)
+	if m == nil {
+		return "", fmt.Errorf("failed to read cryptohome hash for %s: %v", c.user, err)
+	}
+
+	return m[1], nil
+}
 
 // clearCryptohome clears user's encrypted home directory.
 func clearCryptohome(ctx context.Context, user string) error {
@@ -27,12 +59,10 @@ func clearCryptohome(ctx context.Context, user string) error {
 
 // waitForCryptohome waits for user's encrypted home directory to be mounted.
 func waitForCryptohome(ctx context.Context, user string) error {
-	b, err := exec.Command("cryptohome-path", "user", user).Output()
+	p, err := userCryptohomePath(user)
 	if err != nil {
-		return fmt.Errorf("failed to get cryptohome for %s: %v", user, err)
+		return err
 	}
-
-	p := strings.TrimSpace(string(b))
 	testing.ContextLog(ctx, "Waiting for cryptohome ", p)
 	err = poll(ctx, func() bool {
 		f, err := os.Open("/etc/mtab")
