516f864a95fcc746a4cfbf2362fdcea5b625b3f2
Stephen Barber <smbarber@chromium.org>
2018-03-30 15:52:18 -0700

    tast-tests: add vm.StartTerminaVM
    Add a basic test that starts a Termina VM with concierge.
    
    BUG=chromium:725692
    TEST=vm.StartTerminaVM
    
    Change-Id: Ifbdecb60a42efd064ed77db9c38bb3e69a4b1499

---
+++ src/chromiumos/tast/local/bundles/cros/vm/start_termina_vm.go
@@ -0,0 +1,38 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package vm
+
+import (
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/vm"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         StartTerminaVM,
+		Desc:         "Checks that a Termina VM starts up with concierge",
+		Attr:         []string{"bvt"},
+		SoftwareDeps: []string{"chrome_login", "virtual_machines"},
+	})
+}
+
+func StartTerminaVM(s *testing.State) {
+	cr, err := chrome.New(s.Context())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(s.Context())
+
+	concierge, err := vm.New(s.Context(), cr.User)
+	if err != nil {
+		s.Fatal("Failed to start concierge: ", err)
+	}
+
+	err = concierge.StartTerminaVM(s.Context())
+	if err != nil {
+		s.Fatal("Failed to start VM: ", err)
+	}
+}
