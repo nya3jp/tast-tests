b98849e79eb8a6ddbaf7c8732b9fbd27d033d3a1
Daniel Erat <derat@chromium.org>
2018-05-10 07:37:45 -0700

    tast-tests: Add example.ReconnectToDUT remote test.
    Add an example remote test that demonstrates managing the
    SSH connection to the DUT. I'm mostly just adding this so I
    have a way to exercise the code for running remote tests
    without running power.Reboot, which takes much longer to
    complete and probably annoys my officemates due to the
    loud beep at the dev mode screen.
    
    BUG=none
    TEST=ran it and saw that it passes
    
    Change-Id: I7814c3719cb9c814378d52e6c6498762f1ad8aa7
    Reviewed-on: https://chromium-review.googlesource.com/1095572
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/remote/bundles/cros/example/reconnect_to_dut.go
@@ -0,0 +1,51 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"chromiumos/tast/dut"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ReconnectToDUT,
+		Desc: "Demonstrates connecting to and disconnecting from DUT",
+	})
+}
+
+func ReconnectToDUT(s *testing.State) {
+	ctx := s.Context()
+	d, ok := dut.FromContext(ctx)
+	if !ok {
+		s.Fatal("Failed to get DUT")
+	}
+	if !d.Connected(ctx) {
+		s.Error("Not initially connected to DUT")
+	}
+
+	s.Log("Disconnecting from DUT")
+	if err := d.Disconnect(ctx); err != nil {
+		s.Error("Failed to disconnect from DUT: ", err)
+	}
+	if d.Connected(ctx) {
+		s.Error("Still connected after disconnecting")
+	}
+
+	s.Log("Connecting to DUT")
+	if err := d.Connect(ctx); err != nil {
+		s.Error("Failed to connect to DUT: ", err)
+	}
+	if !d.Connected(ctx) {
+		s.Error("Not connected after connecting")
+	}
+
+	// Leave the DUT in a disconnected state.
+	// The connection should automatically be reestablished before the next test is run.
+	s.Log("Disconnecting from DUT again")
+	if err := d.Disconnect(ctx); err != nil {
+		s.Error("Failed to disconnect from DUT: ", err)
+	}
+}
