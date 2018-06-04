5018c890a7b9ad2927a4b3e7c7ce836bf61f606e
Daniel Erat <derat@chromium.org>
2018-06-04 16:34:14 -0700

    tast-tests: Make chrome package watch for restarts.
    Introduce an internal browserWatcher type in the chrome
    package that watches for changes to the browser process's
    PID to try to identify Chrome crashes.
    
    BUG=none
    TEST=Chrome is helpfully crashing all the time on
         chromeos1-row1-rack1-host3, so verified that
         ui.ChromeLogin now fails with "Failed to connect to
         Chrome: failed to read Chrome debugging port from
         /home/chronos/DevToolsActivePort: browser process 19003
         went away" when run against it. also updated
         ui.ChromeLogin to call WaitForExpr with "false", sent
         SIGKILL to the browser process, and checked that the
         test fails with "browser process 22948 went away"
         instead of "context deadline exceeded"
    
    Change-Id: Ib3b592b586cf5e6817b475908b3feddec94b5f79
    Reviewed-on: https://chromium-review.googlesource.com/1086351
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>

---
+++ src/chromiumos/tast/local/chrome/watcher.go
@@ -0,0 +1,90 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package chrome
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	checkBrowserInterval = 100 * time.Millisecond // interval to check browser process
+)
+
+// browserWatcher watches the browser process to attempt to identify situations where Chrome is crashing.
+type browserWatcher struct {
+	initialPID int        // first browser PID that was seen; initially -1
+	browserErr error      // error that was detected, if any
+	mutex      sync.Mutex // protects initialPID and browserErr
+	done       chan bool  // used to tell the watcher's goroutine to exit
+}
+
+func newBrowserWatcher() *browserWatcher {
+	return &browserWatcher{initialPID: -1, done: make(chan bool, 1)}
+}
+
+func (bw *browserWatcher) close() {
+	bw.done <- true
+}
+
+// start begins asynchronously watching the browser process.
+func (bw *browserWatcher) start() {
+	go func() {
+		bw.check()
+		for {
+			select {
+			case <-bw.done:
+				break
+			case <-time.After(checkBrowserInterval):
+				if !bw.check() {
+					break
+				}
+			}
+		}
+	}()
+}
+
+// err returns the first error that was observed or nil if no error was observed.
+func (bw *browserWatcher) err() error {
+	bw.mutex.Lock()
+	defer bw.mutex.Unlock()
+	return bw.browserErr
+}
+
+// check is an internal method that checks the browser process, updating initialPID and browserErr as needed.
+// Returns false after an error has been encountered, indicating that no further calls are needed.
+func (bw *browserWatcher) check() bool {
+	pid, err := GetRootPID()
+	if err != nil {
+		pid = -1
+	}
+
+	bw.mutex.Lock()
+	defer bw.mutex.Unlock()
+
+	// TODO(derat): Consider also checking for crash reports or log messages.
+
+	// If Chrome hadn't previously started (and possibly still hasn't started), keep checking.
+	if bw.initialPID == -1 {
+		bw.initialPID = pid
+		return true
+	}
+
+	// If we didn't find the browser process now but we previously saw it, then it probably crashed.
+	if pid == -1 {
+		bw.browserErr = fmt.Errorf("browser process %d went away", bw.initialPID)
+		return false
+	}
+
+	// If the browser's PID changed, then it probably crashed and got restarted between checks.
+	if pid != bw.initialPID {
+		bw.browserErr = fmt.Errorf("browser process %d replaced by %d", bw.initialPID, pid)
+		return false
+	}
+
+	// The original browser process is still running, so keep checking.
+	return true
+}
