1858967563e84635daed4331654557b153fbf595
Daniel Erat <derat@chromium.org>
2018-06-04 15:35:17 -0700

    tast-tests: Add dbusutil.CallProtoMethod.
    Add a CallProtoMethod helper function to the dbusutil
    package that makes it less tedious to pass protobufs to
    D-Bus methods and read them from responses.
    
    Also rework a few error messages in the vm package to
    include more details about what went wrong.
    
    BUG=none
    TEST=successfully ran vm.StartTerminaVM on an eve device
    
    Change-Id: I62ed5ba344322d2d4d45b5feb3aab2dadb6c8f0d

---
+++ src/chromiumos/tast/local/dbusutil/proto.go
@@ -0,0 +1,37 @@
+package dbusutil
+
+import (
+	"fmt"
+
+	"github.com/godbus/dbus"
+	"github.com/golang/protobuf/proto"
+)
+
+// CallProtoMethod marshals in, passes it as a byte array arg to method on obj,
+// and unmarshals a byte array arg from the response to out. method should be prefixed
+// by a D-Bus interface name. Either in or out may be nil.
+func CallProtoMethod(obj dbus.BusObject, method string, in, out proto.Message) error {
+	var args []interface{}
+	if in != nil {
+		marshIn, err := proto.Marshal(in)
+		if err != nil {
+			return fmt.Errorf("failed marshaling %s arg: %v", method, err)
+		}
+		args = append(args, marshIn)
+	}
+
+	call := obj.Call(method, 0, args...)
+	if call.Err != nil {
+		return fmt.Errorf("failed calling %s: %v", method, call.Err)
+	}
+	if out != nil {
+		var marshOut []byte
+		if err := call.Store(&marshOut); err != nil {
+			return fmt.Errorf("failed reading %s response: %v", method, err)
+		}
+		if err := proto.Unmarshal(marshOut, out); err != nil {
+			return fmt.Errorf("failed unmarshaling %s response: %v", method, err)
+		}
+	}
+	return nil
+}
