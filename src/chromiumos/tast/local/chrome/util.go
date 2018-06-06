3c8d71f6e7a3d0264170d15c04c3db10eb0c82ce
Daniel Erat <derat@chromium.org>
2018-06-06 15:18:53 -0700

    tast-tests: Minor improvements to chrome package.
    Introduce a new chrome.Chrome.NewConnForTarget method that
    returns a connection to the first target matched by a
    caller-provided function.
    
    Also update chrome.Conn.EvalPromise to check for an
    exception before trying to unmarshal the resulting value,
    and improve the internal poll() function to return the
    last-seen error instead of the generic "context deadline
    exceeded" error produced by the context.
    
    Finally, make minor improvements and corrections to some
    comments for the Conn type.
    
    BUG=none
    TEST=manually verified that a nicer error message is
         returned when an exception is encountered during
         EvalPromise; also verified that existing UI tests pass
    
    Change-Id: Ibd1c257d3bfaf3d47073e5d898b5feab12ecf699

--- src/chromiumos/tast/local/chrome/util.go
+++ src/chromiumos/tast/local/chrome/util.go
@@ -18,18 +18,26 @@ const (
 	pollInterval = 10 * time.Millisecond
 )
 
-// poll runs f repeatedly until it returns true and then returns nil.
-// If ctx returns an error before then, the error is returned.
-// TODO(derat): Probably ought to give f a way to return an error too.
-func poll(ctx context.Context, f func() bool) error {
+// poll runs f repeatedly until it returns nil and then also returns nil.
+// If ctx returns an error before then, the last error returned by f is returned.
+func poll(ctx context.Context, f func() error) error {
+	var last error
 	for {
-		if f() {
+		if last = f(); last == nil {
 			return nil
 		}
 		if ctx.Err() != nil {
-			return ctx.Err()
+			return last
 		}
 		time.Sleep(pollInterval)
+
+		// f may be using ctx as well, which could lead to it producing a less-useful
+		// error during the next iteration if it also checks if the deadline was reached.
+		// Check if the context expired while we were sleeping and return the last error
+		// that f returned if so.
+		if ctx.Err() != nil {
+			return last
+		}
 	}
 }
 
