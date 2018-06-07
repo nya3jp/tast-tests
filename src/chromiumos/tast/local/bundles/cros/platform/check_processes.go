b0fbd6de903d8f6cd05cecb023d63516236d73ca
Shuhei Takahashi <nya@chromium.org>
2018-06-07 12:45:03 +0900

    tast-tests: Add platform.CheckProcesses.
    This test corresponds to platform_Check*Process tests in autotest.
    
    BUG=None
    TEST=tast -verbose run betty.DUT platform.CheckProcesses
    
    Change-Id: I476976d07d0abc8693b97069e4179639ed1ef3b1

---
+++ src/chromiumos/tast/local/bundles/cros/platform/check_processes.go
@@ -0,0 +1,53 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"chromiumos/tast/testing"
+
+	"github.com/shirou/gopsutil/process"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CheckProcesses,
+		Desc: "Checks that all expected processes are running",
+		Attr: []string{"bvt"},
+	})
+}
+
+var expectedProcessNames = []string{
+	"debugd",
+	"metrics_daemon",
+	"powerd",
+	"tlsdated",
+}
+
+func CheckProcesses(s *testing.State) {
+	procs, err := process.Processes()
+	if err != nil {
+		s.Fatal("Failed to get a list of processes: ", err)
+	}
+
+	names := make(map[string]bool)
+	for _, proc := range procs {
+		if name, err := proc.Name(); err == nil {
+			names[name] = true
+		}
+	}
+
+	var missingNames []string
+	for _, name := range expectedProcessNames {
+		if !names[name] {
+			missingNames = append(missingNames, name)
+		} else {
+			s.Log("Found: ", name)
+		}
+	}
+
+	if len(missingNames) > 0 {
+		s.Fatal("Critical process(es) not found: ", missingNames)
+	}
+}
