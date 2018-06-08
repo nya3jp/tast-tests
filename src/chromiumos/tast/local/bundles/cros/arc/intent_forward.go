55ef7b46953c3cdba72c7143f24843d7420fc601
Shuhei Takahashi <nya@chromium.org>
2018-06-08 14:32:30 +0900

    tast-tests: Add arc.IntentForward test.
    This test corresponds to cheets_AndroidToChromeIntent autotest.
    
    BUG=None
    TEST=tast -verbose run betty-DUT arc.IntentForward
    
    Change-Id: Id79fd73aa0265063eabe1d91d79e3183f29fe39e

---
+++ src/chromiumos/tast/local/bundles/cros/arc/intent_forward.go
@@ -0,0 +1,90 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         IntentForward,
+		Desc:         "Checks Android intents are forwarded to Chrome",
+		Attr:         []string{"bvt"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+	})
+}
+
+func IntentForward(s *testing.State) {
+	const (
+		viewAction          = "android.intent.action.VIEW"
+		viewDownloadsAction = "android.intent.action.VIEW_DOWNLOADS"
+		setWallpaperAction  = "android.intent.action.SET_WALLPAPER"
+
+		filesAppURL        = "chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj/main.html"
+		wallpaperPickerURL = "chrome-extension://obklkkbkpaoaejdabbfldmcfplpdgolj/main.html"
+	)
+
+	cr, err := chrome.New(s.Context(), chrome.ARCEnabled())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(s.Context())
+
+	if err = arc.WaitArcIntentHelper(s.Context()); err != nil {
+		s.Fatal("ArcIntentHelper did not come up: ", err)
+	}
+
+	waitForTab := func(ctx context.Context, cr *chrome.Chrome, url string) error {
+		newCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
+		testing.ContextLogf(ctx, "Waiting for a tab: %s", url)
+		_, err := cr.NewConnForTarget(newCtx, func(t *chrome.Target) bool {
+			return t.URL == url
+		})
+		if err != nil {
+			// HACK: Print existing targets when we could not find the desired one
+			// for debugging purpose.
+			testing.ContextLogf(ctx, "Timeout expired on waiting for a tab. Existing tabs:")
+			cr.NewConnForTarget(ctx, func(t *chrome.Target) bool {
+				testing.ContextLog(ctx, "  ", t.URL)
+				return true
+			})
+		}
+		return err
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "It worked!")
+	}))
+	defer server.Close()
+	localWebURL := server.URL + "/" // Must end with a slash
+
+	arc.SendIntent(viewAction, localWebURL)
+	err = waitForTab(s.Context(), cr, localWebURL)
+	if err != nil {
+		s.Error("Failed to open a web page: ", err)
+	}
+
+	arc.SendIntent(viewDownloadsAction, "")
+	err = waitForTab(s.Context(), cr, filesAppURL)
+	if err != nil {
+		s.Error("Failed to open Downloads: ", err)
+	}
+
+	arc.SendIntent(setWallpaperAction, "")
+	err = waitForTab(s.Context(), cr, wallpaperPickerURL)
+	if err != nil {
+		s.Error("Failed to open wallpaper picker: ", err)
+	}
+}
