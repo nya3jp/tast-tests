c42bfc32ab83aa6c14c3d60e7818fc30461be0f6
Shuhei Takahashi <nya@chromium.org>
2018-06-08 14:32:30 +0900

    tast-tests: Add arc.IntentForward test.
    This test corresponds to cheets_AndroidToChromeIntent autotest.
    
    BUG=None
    TEST=tast -verbose run betty-DUT arc.IntentForward
    
    Change-Id: Id79fd73aa0265063eabe1d91d79e3183f29fe39e

---
+++ src/chromiumos/tast/local/arc/exec.go
@@ -0,0 +1,14 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"os/exec"
+)
+
+// Command runs a command in Android container.
+func Command(name string, arg ...string) *exec.Cmd {
+	return exec.Command("android-sh", append([]string{"-c", "exec \"$@\"", "-", name}, arg...)...)
+}
