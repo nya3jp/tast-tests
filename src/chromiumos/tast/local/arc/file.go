6d30b829399b1fedd8ca6e3784469696f2008005
Shuhei Takahashi <nya@chromium.org>
2018-06-12 16:40:23 +0900

    tast-tests: Add arc.Downloads.
    This is a test ported from cheets_DownloadsFileSystem.
    
    BUG=None
    TEST=tast -verbose run betty-DUT arc.Downloads
    
    Change-Id: Ibfb2b34dd571ffdffbc0535bc44c6f19f1e9fa8a

---
+++ src/chromiumos/tast/local/arc/file.go
@@ -0,0 +1,34 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+// ReadFile reads a file in Android file system.
+func ReadFile(filename string) ([]byte, error) {
+	return Command("cat", filename).Output()
+}
+
+// WriteFile writes to a file in Android file system.
+func WriteFile(filename string, data []byte) error {
+	cmd := Command("sh", "-c", "cat > \"$1\"", "-", filename)
+	w, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	if err = cmd.Start(); err != nil {
+		// Docs guarantee pipes to be closed only when Wait() is called.
+		w.Close()
+		return err
+	}
+
+	_, err = w.Write(data)
+	if cerr := w.Close(); err == nil {
+		err = cerr
+	}
+	if werr := cmd.Wait(); err == nil {
+		err = werr
+	}
+	return err
+}
