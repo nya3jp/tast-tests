93692614e80c148f4e4e91404837657d3ca4eb9d
Shuhei Takahashi <nya@chromium.org>
2018-06-12 16:40:23 +0900

    tast-tests: Add arc.Downloads.
    This is a test ported from cheets_DownloadsFileSystem.
    
    BUG=None
    TEST=tast -verbose run betty-DUT arc.Downloads
    
    Change-Id: Ibfb2b34dd571ffdffbc0535bc44c6f19f1e9fa8a

---
+++ src/chromiumos/tast/local/bundles/cros/arc/downloads.go
@@ -0,0 +1,77 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Downloads,
+		Desc:         "Checks Downloads integration is working",
+		Attr:         []string{"bvt"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+		Data:         []string{"capybara.jpg"},
+	})
+}
+
+func Downloads(s *testing.State) {
+	const (
+		filename    = "capybara.jpg"
+		crosPath    = "/home/chronos/user/Downloads/" + filename
+		androidPath = "/storage/emulated/0/Download/" + filename
+	)
+
+	cr, err := chrome.New(s.Context(), chrome.ARCEnabled())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(s.Context())
+
+	expected, err := ioutil.ReadFile(s.DataPath(filename))
+	if err != nil {
+		s.Fatal("Could not read the test file: ", err)
+	}
+
+	os.Remove(crosPath)
+	if _, err := os.Stat(crosPath); !os.IsNotExist(err) {
+		s.Fatalf("Failed to ensure %s does not exist: %v", crosPath, err)
+	}
+
+	// CrOS -> Android
+	if err = ioutil.WriteFile(crosPath, expected, 0666); err != nil {
+		s.Fatalf("Could not write to %s: %v", crosPath, err)
+	}
+	actual, err := arc.ReadFile(androidPath)
+	if err != nil {
+		s.Error("CrOS -> Android failed: ", err)
+	} else if !bytes.Equal(actual, expected) {
+		s.Error("CrOS -> Android failed: content mismatch")
+	}
+	if err = os.Remove(crosPath); err != nil {
+		s.Fatal("Failed to remove a file: ", err)
+	}
+
+	// Android -> CrOS
+	if err = arc.WriteFile(androidPath, expected); err != nil {
+		s.Fatalf("Could not write to %s: %v", androidPath, err)
+	}
+	actual, err = ioutil.ReadFile(crosPath)
+	if err != nil {
+		s.Error("Android -> CrOS failed: ", err)
+	} else if !bytes.Equal(actual, expected) {
+		s.Error("Android -> CrOS failed: content mismatch")
+	}
+	if err = os.Remove(crosPath); err != nil {
+		s.Fatal("Failed to remove a file: ", err)
+	}
+}
