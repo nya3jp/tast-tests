81d0802dcc3052ccb3d53bf2547ece3ee9bbb140
Shuhei Takahashi <nya@chromium.org>
2018-06-14 18:00:35 +0900

    tast-tests: Use ADB to run commands in Android container.
    Command execution environment of android-sh is not exactly the same
    as the actual Android container, so some commands (e.g. am, pm) may
    fail with android-sh.
    
    This change replaces the implementation of arc.Command() to use
    adb shell instead of android-sh.
    
    BUG=None
    TEST=tast -verbose run caroline-DUT arc.IntentForward
    TEST=tast -verbose run caroline-DUT arc.Downloads
    
    Change-Id: I9dd0e7ae1c28d2622634f35db6058cec264bc5cb
    Reviewed-on: https://chromium-review.googlesource.com/1100723
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- src/chromiumos/tast/local/arc/commands.go
+++ src/chromiumos/tast/local/arc/commands.go
@@ -4,10 +4,33 @@
 
 package arc
 
-import "os/exec"
+import (
+	"os/exec"
+	"strings"
+)
 
-// Command runs a command in Android container with android-sh.
+// Command runs a command in Android container with adb shell.
 func Command(name string, arg ...string) *exec.Cmd {
+	// Unfortunately, adb shell always passes the command line to /bin/sh, so
+	// we need to escape arguments.
+	escaped := make([]string, len(arg)+1)
+	escaped[0] = shellEscape(name)
+	for i, a := range arg {
+		escaped[i+1] = shellEscape(a)
+	}
+	shell := "exec " + strings.Join(escaped, " ")
+	return adbCommand("shell", shell)
+}
+
+func shellEscape(s string) string {
+	return "'" + strings.Replace(s, "'", `'"'"'`, -1) + "'"
+}
+
+// bootstrapCommand runs a command with android-sh.
+// Command execution environment of android-sh is not exactly the same as actual
+// Android container, so this should be used only before ADB connection gets
+// ready.
+func bootstrapCommand(name string, arg ...string) *exec.Cmd {
 	return exec.Command("android-sh", append([]string{"-c", "exec \"$@\"", "-", name}, arg...)...)
 }
 
