f2b5550f565f2a8ec9aee80b9319cf3a65c39619
Shuhei Takahashi <nya@chromium.org>
2018-06-19 18:04:13 +0900

    tast-tests: Add platform.TPMSanity test.
    This is a port of kernel_TPMPing autotest.
    
    The original autotest also contains a check about "gentle shutdown"
    kernel patch, but it will be no longer needed after four years.
    
    BUG=None
    TEST=tast -verbose run caroline-DUT platform.TPMSanity
    CQ-DEPEND=CL:1105128
    CQ-DEPEND=CL:1104085
    
    Change-Id: Iac894718cbac3ed323e0a7fdad63f75daca8ba9c

---
+++ src/chromiumos/tast/local/bundles/cros/platform/tpm.go
@@ -0,0 +1,33 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"strings"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: TPMSanity,
+		Desc: "Checks that TPM is working",
+		Attr: []string{"informational"},
+	})
+}
+
+func TPMSanity(s *testing.State) {
+	cmd := testexec.CommandContext(s.Context(), "tpm_version")
+	out, err := cmd.Output()
+	if err != nil {
+		cmd.DumpLog(s.Context())
+		s.Fatal("Failed to run tpm_version: ", err)
+	}
+
+	if !strings.Contains(string(out), "Version Info") {
+		s.Error("Unexpected tpm_version output:\n", string(out))
+	}
+}
