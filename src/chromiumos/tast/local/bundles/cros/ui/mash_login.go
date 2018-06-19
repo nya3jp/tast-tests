10248a86deeacaf9233b4c1405670eb75d30c0b6
Daniel Erat <derat@chromium.org>
2018-06-19 16:37:32 -0700

    tast-tests: Make ui.MashLogin collect debug files.
    Make the ui.MashLogin test collect /home/chronos/Local State
    and /var/lib/whitelist/policy.* on failure to try to track
    down Chrome crashes in IsGuestSessionAllowed().
    
    BUG=chromium:850139
    TEST=manual: ran it after inverting failure check and
         verified that expected files are in test's output dir
    
    Change-Id: Id8ee2397d8ad0537927ebc68d009cf3a55e7f722
    Reviewed-on: https://chromium-review.googlesource.com/1107204
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/mash_login.go
+++ src/chromiumos/tast/local/bundles/cros/ui/mash_login.go
@@ -5,6 +5,9 @@
 package ui
 
 import (
+	"io"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"chromiumos/tast/local/chrome"
@@ -32,6 +35,31 @@ func init() {
 func MashLogin(s *testing.State) {
 	cr, err := chrome.New(s.Context(), chrome.MashEnabled())
 	if err != nil {
+		saveFile := func(p string) error {
+			sf, err := os.Open(p)
+			if err != nil {
+				return err
+			}
+			defer sf.Close()
+
+			df, err := os.Create(filepath.Join(s.OutDir(), filepath.Base(p)))
+			if err != nil {
+				return err
+			}
+			defer df.Close()
+
+			_, err = io.Copy(df, sf)
+			return err
+		}
+
+		// TODO(crbug.com/850139): Stop collecting these files after fixing IsGuestSessionAllowed segfaults.
+		ps, _ := filepath.Glob("/var/lib/whitelist/policy.*")
+		for _, p := range append(ps, "/home/chronos/Local State") {
+			if err = saveFile(p); err != nil {
+				s.Errorf("Failed to save %s: %v", p, err)
+			}
+		}
+
 		s.Fatal("Chrome login failed: ", err)
 	}
 	defer cr.Close(s.Context())
