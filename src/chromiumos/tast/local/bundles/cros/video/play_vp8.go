469dc15b1ca709142333af174f1f2dd989bd8d1b
Shuhei Takahashi <nya@chromium.org>
2018-06-19 14:35:59 +0900

    tast-tests: Add video.Play*.
    They are ports of video_VideoSanity autotest.
    
    BUG=None
    TEST=tast -verbose run caroline-DUT video.Play{H264,VP8,VP9}
    CQ-DEPEND=CL:1105128
    
    Change-Id: I75c75373713f26deb07c17d4248894455c913c23

---
+++ src/chromiumos/tast/local/bundles/cros/video/play_vp8.go
@@ -0,0 +1,24 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"chromiumos/tast/local/bundles/cros/video/play"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PlayVP8,
+		Desc:         "Checks VP8 video playback is working",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+		Data:         []string{"bear.webm", "video.html"},
+	})
+}
+
+func PlayVP8(s *testing.State) {
+	play.TestPlay(s, "bear.webm")
+}
