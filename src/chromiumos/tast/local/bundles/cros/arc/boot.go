723f5938821af6a224f05d9db107a8ed99909d12
Shuhei Takahashi <nya@chromium.org>
2018-06-19 11:17:36 +0900

    tast-tests: Add arc.Smoke test.
    This is a port of cheets_ContainerSmokeTest autotest. It is similar
    to ui.ARCStartup, but it also checks package manager service is
    running.
    
    BUG=None
    TEST=tast -verbose run caroline-DUT arc.Boot
    
    Change-Id: I6fe08aeeaa50ca0f5d9fe74119b519cb0799cf72

---
+++ src/chromiumos/tast/local/bundles/cros/arc/boot.go
@@ -0,0 +1,54 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"strings"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Boot,
+		Desc:         "Checks that Android boots",
+		Attr:         []string{"bvt"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+	})
+}
+
+func Boot(s *testing.State) {
+	ctx := s.Context()
+
+	cr, err := chrome.New(ctx, chrome.ARCEnabled())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	// Run "pm list packages" and ensure "android" package exists.
+	// This ensures package manager service is running at least.
+	cmd := arc.Command(ctx, "pm", "list", "packages")
+	out, err := cmd.Output()
+	if err != nil {
+		cmd.DumpLog(ctx)
+		s.Fatal("pm list failed: ", err)
+	}
+
+	pkgs := strings.Split(string(out), "\n")
+	found := false
+	for _, p := range pkgs {
+		if p == "package:android" {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		s.Error("android package not found: ", pkgs)
+	}
+}
