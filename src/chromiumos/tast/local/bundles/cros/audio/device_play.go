0edac45cf0d619489bd4d569efb4187500c6c242
Shuhei Takahashi <nya@chromium.org>
2018-06-19 17:36:53 +0900

    tast-tests: Add audio.Device* tests.
    They are ports of sound_infrastructure autotest.
    
    BUG=None
    TEST=tast -verbose run caroline-DUT audio.Device{Play,Record}
    CQ-DEPEND=CL:1111767
    
    Change-Id: Ib738d3784c2e22b4ced3dc9e83147e2762252c4f

---
+++ src/chromiumos/tast/local/bundles/cros/audio/device_play.go
@@ -0,0 +1,24 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package audio
+
+import (
+	"chromiumos/tast/local/bundles/cros/audio/device"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DevicePlay,
+		Desc:         "Checks that sound devices for playing are recognized",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"audio_play"},
+	})
+}
+
+func DevicePlay(s *testing.State) {
+	device.TestDeviceFiles(s, `^pcmC\d+D\d+p$`)
+	device.TestALSACommand(s, "aplay")
+}
