4d939b86bcf80179c5f66693a240a1127e7136c6
Daniel Erat <derat@chromium.org>
2018-06-25 16:53:21 -0700

    tast-tests: Move Chrome debugging to ui.ChromeLogin.
    Move some debug code that captures Local State and policy
    files from ui.MashLogin to ui.ChromeLogin. I added this to
    help investigate IsGuestSessionAllowed segfaults in Chrome,
    but ui.MashLogin is failing frequently due to an unrelated
    issue (https://crbug.com/850168) and it's easier to identify
    these segfaults in ui.ChromeLogin.
    
    BUG=chromium:850139
    TEST=ran tests
    
    Change-Id: Iebb092e46ef84b16c7229eede9a0cc790a9f159e

--- src/chromiumos/tast/local/bundles/cros/ui/mash_login.go
+++ src/chromiumos/tast/local/bundles/cros/ui/mash_login.go
@@ -5,9 +5,6 @@
 package ui
 
 import (
-	"io"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"chromiumos/tast/local/chrome"
@@ -35,33 +32,7 @@ func init() {
 func MashLogin(s *testing.State) {
 	cr, err := chrome.New(s.Context(), chrome.MashEnabled())
 	if err != nil {
-		cerr := err // save to pass to s.Fatal later
-
-		saveFile := func(p string) error {
-			sf, err := os.Open(p)
-			if err != nil {
-				return err
-			}
-			defer sf.Close()
-
-			df, err := os.Create(filepath.Join(s.OutDir(), filepath.Base(p)))
-			if err != nil {
-				return err
-			}
-			defer df.Close()
-
-			_, err = io.Copy(df, sf)
-			return err
-		}
-		// TODO(crbug.com/850139): Stop collecting these files after fixing IsGuestSessionAllowed segfaults.
-		ps, _ := filepath.Glob("/var/lib/whitelist/policy.*")
-		for _, p := range append(ps, "/home/chronos/Local State") {
-			if err = saveFile(p); err != nil {
-				s.Errorf("Failed to save %s: %v", p, err)
-			}
-		}
-
-		s.Fatal("Chrome login failed: ", cerr)
+		s.Fatal("Chrome login failed: ", err)
 	}
 	defer cr.Close(s.Context())
 
