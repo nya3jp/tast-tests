99eef91d509c49a0409a610b5bbae8809d372d14
Andres Calderon Jaramillo <andrescj@chromium.org>
2018-07-18 13:56:17 -0400

    Create the starter code to port graphics_dEQP tests to tast.
    This CL simply provides the skeleton Go file to start porting the
    graphics drawElements Quality Program test suite from autotest to tast.
    
    TEST=passes when running tast -verbose run <ip> graphics.GraphicsDEQP
    BUG=chromium:865107
    
    Change-Id: I450aeb9963a2c4159ee2a4021d7e4897734724d9

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/graphics_deqp.go
@@ -0,0 +1,21 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: GraphicsDEQP,
+		Desc: "Runs the drawElements Quality Program test suite shipped with test images",
+		Attr: []string{"disabled"},
+	})
+}
+
+func GraphicsDEQP(s *testing.State) {
+	// The actual test goes here.
+}
