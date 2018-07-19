b2306c370556998e8c3c0be7845cd6827409d7f0
Andres Calderon Jaramillo <andrescj@chromium.org>
2018-07-19 17:24:38 -0400

    Port the get_gles_version() function into a tast support package.
    This CL creates a support package for tast graphics tests and ports the
    get_gles_version() function (defined in
    autotest/files/client/cros/graphics/graphics_utils.py). Additional
    helper functions had to be ported as well.
    
    TEST=to be added in an additional patchset in this CL
    BUG=chromium:865107
    
    Change-Id: Ie39a1fb45d094bf5282af226d60660068629c2eb

---
+++ src/chromiumos/tast/local/graphics/utils_test.go
@@ -0,0 +1,122 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+// flagsStringToMap converts a space-separated list of flags into a
+// map[string]struct{} where the keys are the flags in the string and the values
+// are zero-byte values.
+func flagsStringToMap(s string) map[string]struct{} {
+	fmap := make(map[string]struct{})
+	for _, flag := range strings.Fields(s) {
+		fmap[flag] = struct{}{}
+	}
+	return fmap
+}
+
+func TestParseUIUseFlags(t *testing.T) {
+	// Create a temporary directory for input configuration files.
+	tmpd, err := ioutil.TempDir("", t.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpd)
+	for _, tc := range []struct {
+		name  string
+		conf  string
+		flags string
+	}{
+		{"EmptyConf", "", ""},
+		{"SingleFlag", "abc", "abc"},
+		{"MultipleFlags", "abc\ndef", "abc def"},
+		{"EmptyLines", "abc\n\n  \ndef\n\n", "abc def"},
+		{"ExtraWhitespace", "abc\r\t\r\n\t  def  \t\t", "abc def"},
+		{"CommentLines", "# c1\nabc\n#c2\ndef\n  # c3", "abc def"},
+		{"OnlyComments", "# c1\n# c2\n# c3", ""},
+		{"OnlyWhitespace", "   \n\t  \r\t\r\n\n\n", ""},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			// Create a temporary file for the input configuration.
+			f := filepath.Join(tmpd, tc.name)
+			err = ioutil.WriteFile(f, []byte(tc.conf), 0600)
+
+			// Run parseUIUseFlags and compare result.
+			expected := flagsStringToMap(tc.flags)
+			actual, err := parseUIUseFlags(f)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if actual != nil {
+				if !reflect.DeepEqual(expected, actual) {
+					t.Errorf("parseUIUseFlags on %q = %v; want %v", tc.conf,
+						actual, expected)
+				}
+			} else {
+				t.Errorf("parseUIUseFlags on %q = nil; want %v", tc.conf,
+					expected)
+			}
+		})
+	}
+}
+
+func TestExtractOpenGLVersion(t *testing.T) {
+	for _, tc := range []struct {
+		name    string
+		wflout  string
+		major   int
+		minor   int
+		wantErr bool
+	}{
+		{"EmptyOutput", "", 0, 0, true},
+		{"TypicalOutput", `
+Waffle platform: null
+Waffle api: gles2
+OpenGL vendor string: Intel Open Source Technology Center
+OpenGL renderer string: Mesa DRI Intel(R) HD Graphics 615 (Kaby Lake GT2)
+OpenGL version string: OpenGL ES 3.2 Mesa 18.1.0-devel (git-131e871385)`, 3, 2, false},
+		{"DuplicateVersionString", `
+Waffle platform: null
+Waffle api: gles2
+OpenGL vendor string: Intel Open Source Technology Center
+OpenGL version string: OpenGL ES 3.3 Mesa 18.1.0-devel (git-131e871385)
+OpenGL renderer string: Mesa DRI Intel(R) HD Graphics 615 (Kaby Lake GT2)
+OpenGL version string: OpenGL ES 3.2 Mesa 18.1.0-devel (git-131e871385)`, 0, 0, true},
+		{"NoVersionString", `
+Waffle platform: null
+Waffle api: gles2
+OpenGL vendor string: Intel Open Source Technology Center`, 0, 0, true},
+		{"BadMajorVersion", "OpenGL version string: OpenGL ES 999999999999999999999.2 Mesa 18.1.0-devel (git-131e871385)", 0, 0, true},
+		{"BadMinorVersion", "OpenGL version string: OpenGL ES 3.999999999999999999999 Mesa 18.1.0-devel (git-131e871385)", 0, 0, true},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			amajor, aminor, aerr := extractOpenGLVersion(context.Background(),
+				strings.TrimLeft(tc.wflout, "\n"))
+
+			// Complain if a) no error was returned, but we expected one, or
+			// b) an error was returned, but we expected none, or c) the
+			// returned major and minor versions are not as expected.
+			if tc.wantErr && aerr == nil {
+				t.Errorf("extractOpenGLVersion(%q) unexpectedly succeeded", tc.wflout)
+				return
+			} else if !tc.wantErr && aerr != nil {
+				t.Errorf("extractOpenGLVersion(%q) unexpectedly failed: %v", tc.wflout, aerr)
+				return
+			}
+
+			if amajor != tc.major || aminor != tc.minor {
+				t.Errorf("extractOpenGLVersion(%q) = [%v, %v]; want [%v, %v]", tc.wflout, amajor, aminor, tc.major, tc.minor)
+			}
+		})
+	}
+}
