bdbef39e55329a74c0ee205c87c4d6718f7dc7d9
Shuhei Takahashi <nya@chromium.org>
2018-08-01 12:54:23 +0900

    perf: Support recording performance metrics.
    BUG=chromium:868593
    TEST=fast_build.sh -T
    TEST=tast run DUT example.Perf
    
    Change-Id: I8a3bb4371a017e8d63e01fcf4ba4adcdfa7b35d3

---
+++ src/chromiumos/tast/local/bundles/cros/example/perf.go
@@ -0,0 +1,47 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"chromiumos/tast/local/faillog"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Perf,
+		Desc: "Demonstrates how to emit perf metrics",
+		Attr: []string{"informational"},
+	})
+}
+
+func Perf(s *testing.State) {
+	defer faillog.SaveIfError(s)
+
+	var (
+		bootTime    = perf.Metric{Name: "example_boot_time", Unit: "ms", Direction: perf.SmallerIsBetter}
+		refreshRate = perf.Metric{Name: "example_refresh_rate", Unit: "fps", Direction: perf.BiggerIsBetter, Multiple: true}
+
+		p = perf.Values{}
+	)
+
+	// Single-valued data series can be recorded with Set.
+	p.Set(bootTime, 2.83)
+
+	// Multi-valued data series can be recorded with Append.
+	p.Append(refreshRate, 60)
+	p.Append(refreshRate, 30, 72, 11.5)
+
+	// Variant can be also specified.
+	for _, name := range []string{"a.h264", "a.vp8"} {
+		cpuUsage := perf.Metric{Name: "example_cpu_usage", Variant: name, Unit: "percent", Direction: perf.SmallerIsBetter}
+		p.Set(cpuUsage, 50.0)
+	}
+
+	if err := p.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
