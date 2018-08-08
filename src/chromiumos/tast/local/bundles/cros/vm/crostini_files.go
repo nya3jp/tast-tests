7e1cd7eb3154f2a776851cbcc89a543479c7cc55
Joel Hockey <joelhockey@chromium.org>
2018-08-07 22:46:03 -0700

    tast-tests: crostini_files verifies sshfs file mount
    Call autotestPrivate.runCrostiniInstaller() to invoke
    the graphical installer to create default crostini
    vm and container and perform sshfs mount.
    
    Verify mount directory exists, and write a file into it.
    
    BUG=chromium:854401
    TEST=vm.CrostiniFiles
    
    Change-Id: I5f5b37ef405d3ef0e9e4bc7d3f05095c1472f159

---
+++ src/chromiumos/tast/local/bundles/cros/vm/crostini_files.go
@@ -0,0 +1,72 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package vm
+
+import (
+	"io/ioutil"
+	"os"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CrostiniFiles,
+		Desc:         "Checks that crostini sshfs mount works",
+		Attr:         []string{"informational"},
+		Timeout:      5 * time.Minute,
+		SoftwareDeps: []string{"chrome_login", "vm_host"},
+	})
+}
+
+func CrostiniFiles(s *testing.State) {
+	ctx := s.Context()
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to log in: ", err)
+	}
+	defer cr.Close(ctx)
+
+	hash, err := cryptohome.UserHash(cr.User())
+	if err != nil {
+		s.Fatal("Failed to get user hash: ", err)
+	}
+	dir := "/media/fuse/crostini_" + hash + "_termina_penguin"
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+	s.Log("Waiting for crostini to install (typically ~ 3 mins) and mount sshfs dir: ", dir)
+	if err = tconn.EvalPromise(ctx,
+		`new Promise((resolve, reject) => {
+		   chrome.autotestPrivate.runCrostiniInstaller(() => {
+		     if (chrome.runtime.lastError == undefined) {
+		       resolve();
+		     } else {
+		       reject(new Error(chrome.runtime.lastError.message));
+		     }
+		   });
+		 })`, nil); err != nil {
+		s.Fatal("Running autotestPrivate.runCrostiniInstaller failed: ", err)
+	}
+
+	if stat, err := os.Stat(dir); err != nil {
+		s.Fatal("Didn't find sshfs mount: ", err)
+	} else if !stat.IsDir() {
+		s.Fatal("Didn't get directory for sshfs mount: ", dir)
+	}
+
+	// Verify mount works for writing a file.
+	err = ioutil.WriteFile(dir+"/hello.txt", []byte("hello"), 0644)
+	if err != nil {
+		s.Fatal("Failed writing file into sshfs mount directory: ", err)
+	}
+
+	// TODO(joehockey): Use terminal app to verify hello.txt.
+}
