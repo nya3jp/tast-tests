819bc87a18eadf46b73876eeb9e3c263f0af116b
Shuhei Takahashi <nya@chromium.org>
2018-08-09 15:58:05 +0900

    arc: Support UI Automator.
    Introduces ui package which allows interacting with Android apps
    by UI Automator API.
    
    Also adds a simple test making use of ui package. It is in
    "arcapp" category to distinguish ARC application tests from
    ARC fundamental tests.
    
    BUG=chromium:873919
    TEST=tast -verbose run DUT arcapp.Sample
    CQ-DEPEND=CL:1175515
    
    Change-Id: I968435ade7602b95226d22036cb5ff86bc2d0e7b

---
+++ src/chromiumos/tast/local/arc/ui/device.go
@@ -0,0 +1,213 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package ui allows interacting with Android apps by Android UI Automator API.
+// We use android-uiautomator-server, a JSON-RPC server running as an Android app,
+// to invoke UI Automator methods remotely.
+package ui
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+const (
+	// StartTimeout is th etimeout of NewDevice.
+	StartTimeout = 30 * time.Second
+
+	host = "100.115.92.2:9008"
+
+	serverPackage  = "com.github.uiautomator.test"
+	serverActivity = "android.support.test.runner.AndroidJUnitRunner"
+)
+
+var apkPaths = []string{
+	"/usr/local/share/android-uiautomator-server/app-uiautomator.apk",
+	"/usr/local/share/android-uiautomator-server/app-uiautomator-test.apk",
+}
+
+// Device provides access to state information about the Android system.
+//
+// Close must be called to clean up resources when a test is over.
+//
+// This object corresponds to UiDevice in UI Automator API:
+// https://developer.android.com/reference/android/support/test/uiautomator/UiDevice
+type Device struct {
+	a     *arc.ARC
+	sp    *testexec.Cmd // Server process
+	debug bool
+}
+
+type jsonRPCRequest struct {
+	Version string        `json:"jsonrpc"`
+	Method  string        `json:"method"`
+	ID      int           `json:"id"`
+	Params  []interface{} `json:"params,omitempty"`
+}
+
+type jsonRPCError struct {
+	Message string `json:"message"`
+}
+
+type jsonRPCResponse struct {
+	Version string          `json:"jsonrpc"`
+	Result  json.RawMessage `json:"result"`
+	Error   *jsonRPCError   `json:"error"`
+}
+
+// NewDevice creates a Device object by starting and connecting to UI Automator server.
+//
+// Close must be called to clean up resources when a test is over.
+func NewDevice(ctx context.Context, a *arc.ARC) (*Device, error) {
+	ictx, cancel := context.WithTimeout(ctx, StartTimeout)
+	defer cancel()
+
+	testing.ContextLog(ctx, "Starting UI Automator server")
+
+	if err := installServer(ictx, a); err != nil {
+		return nil, err
+	}
+
+	sp := a.Command(ctx, "am", "instrument", "-w", serverPackage+"/"+serverActivity)
+	if err := sp.Start(); err != nil {
+		return nil, fmt.Errorf("failed starting UI Automator server: %v", err)
+	}
+
+	s := &Device{a, sp, false}
+
+	if err := s.waitServer(ictx); err != nil {
+		s.Close()
+		return nil, fmt.Errorf("UI Automator server did not bring up: %v", err)
+	}
+
+	return s, nil
+}
+
+// installServer installs UI Automator server to Android system.
+func installServer(ctx context.Context, a *arc.ARC) error {
+	for _, p := range apkPaths {
+		if err := a.Install(ctx, p); err != nil {
+			return fmt.Errorf("failed installing %s: %v", p, err)
+		}
+	}
+	return nil
+}
+
+// waitServer waits for UI Automator server to bring up.
+func (d *Device) waitServer(ctx context.Context) error {
+	for {
+		if ok := func() bool {
+			ctx, cancel := context.WithTimeout(ctx, 200*time.Millisecond)
+			defer cancel()
+			var res string
+			return d.call(ctx, "ping", &res) == nil && res == "pong"
+		}(); ok {
+			break
+		}
+
+		select {
+		case <-time.After(100 * time.Millisecond):
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+	return nil
+}
+
+// EnableDebug enables verbose RPC logging.
+func (d *Device) EnableDebug() {
+	d.debug = true
+}
+
+// Close releases resources associated to d.
+func (d *Device) Close() error {
+	d.sp.Kill()
+	return d.sp.Wait()
+}
+
+// call calls a remote server method by JSON-RPC.
+// method is a method name.
+// out is a variable to store a returned result. If it is nil, results are discarded.
+// params is a list of parameters to the remote method.
+func (d *Device) call(ctx context.Context, method string, out interface{}, params ...interface{}) error {
+	// Prepare the request.
+	req, err := http.NewRequest("POST", "http://"+host+"/jsonrpc/0", nil)
+	if err != nil {
+		return fmt.Errorf("%s: failed initializing request: %v", method, err)
+	}
+	req = req.WithContext(ctx)
+
+	reqData := jsonRPCRequest{
+		Version: "2.0",
+		Method:  method,
+		Params:  params,
+	}
+	reqBody, err := json.Marshal(&reqData)
+	if err != nil {
+		return fmt.Errorf("%s: failed marshaling request: %v", method, err)
+	}
+	req.Body = ioutil.NopCloser(bytes.NewReader(reqBody))
+	req.ContentLength = int64(len(reqBody))
+	req.Header.Add("Content-Type", "application/json")
+
+	if d.debug {
+		testing.ContextLog(ctx, "-> ", string(reqBody))
+	}
+
+	// Send the request.
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("%s: %v", method, err)
+	}
+	defer res.Body.Close()
+
+	// Status should be OK.
+	if res.StatusCode != 200 {
+		return fmt.Errorf("%s: got status %d", method, res.StatusCode)
+	}
+
+	// Read and parse the response.
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("%s: failed reading response: %v", method, err)
+	}
+
+	if d.debug {
+		testing.ContextLog(ctx, "<- ", string(resBody))
+	}
+
+	var resData jsonRPCResponse
+	if err := json.Unmarshal(resBody, &resData); err != nil {
+		return fmt.Errorf("%s: failed unmarshaling response: %v", method, err)
+	}
+
+	// Check an error.
+	if resData.Error != nil {
+		return fmt.Errorf("%s: %s", method, resData.Error.Message)
+	}
+
+	// If the caller does not need results, we can return now.
+	if out == nil {
+		return nil
+	}
+
+	// Parse the result.
+	if len(resData.Result) == 0 {
+		return fmt.Errorf("%s: missing result", method)
+	}
+	if err := json.Unmarshal([]byte(resData.Result), out); err != nil {
+		testing.ContextLogf(ctx, "Failed unmarshaling to %T: %q", out, string(resData.Result))
+		return fmt.Errorf("%s: failed unmarshaling result: %v", method, err)
+	}
+	return nil
+}
---
+++ src/chromiumos/tast/local/bundles/cros/arcapp/sample.go
@@ -0,0 +1,72 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arcapp
+
+import (
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arcapp/apptest"
+	"chromiumos/tast/local/faillog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Sample,
+		Desc:         "Runs a sample app",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+		Data:         []string{"todo-mvp.apk"},
+		Timeout:      3 * time.Minute,
+	})
+}
+
+func Sample(s *testing.State) {
+	const (
+		// This is a sample TODO app available at:
+		// https://github.com/googlesamples/android-architecture/tree/todo-mvp/
+		apkName      = "todo-mvp.apk"
+		packageName  = "com.example.android.architecture.blueprints.todomvp"
+		activityName = "com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity"
+
+		titleID      = "com.example.android.architecture.blueprints.todomvp:id/title"
+		addButtonID  = "com.example.android.architecture.blueprints.todomvp:id/fab_add_task"
+		titleInputID = "com.example.android.architecture.blueprints.todomvp:id/add_task_title"
+		doneButtonID = "com.example.android.architecture.blueprints.todomvp:id/fab_edit_task_done"
+
+		defaultTitle1 = "Build tower in Pisa"
+		defaultTitle2 = "Finish bridge in Tacoma"
+		customTitle   = "Meet the team at Sagrada Familia"
+	)
+
+	defer faillog.SaveIfError(s)
+
+	ctx := s.Context()
+	must := func(err error) {
+		if err != nil {
+			s.Fatal(err)
+		}
+	}
+
+	apptest.Run(s, apkName, packageName, activityName, func(a *arc.ARC, d *ui.Device) {
+		// Wait for the default entries to show up.
+		must(d.Object(ui.ID(titleID), ui.Text(defaultTitle1)).WaitForExists(ctx))
+		must(d.Object(ui.ID(titleID), ui.Text(defaultTitle2)).WaitForExists(ctx))
+
+		// Click the add button.
+		must(d.Object(ui.ID(addButtonID)).Click(ctx))
+
+		// Fill the form and click the done button.
+		input := d.Object(ui.ID(titleInputID))
+		must(input.WaitForExists(ctx))
+		must(input.SetText(ctx, customTitle))
+		must(d.Object(ui.ID(doneButtonID)).Click(ctx))
+
+		// Wait for our new entry to show up.
+		must(d.Object(ui.ID(titleID), ui.Text(customTitle)).WaitForExists(ctx))
+	})
+}
