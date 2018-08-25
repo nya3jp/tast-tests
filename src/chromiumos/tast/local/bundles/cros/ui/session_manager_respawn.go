01f63a0546106c76f866accb535c6e03f7284497
Daniel Erat <derat@chromium.org>
2018-08-25 10:34:51 -0700

    tast-tests: Make ui.SessionManagerRespawn check that we stop.
    Update the ui.SessionManagerRespawn test to additionally
    check that session_manager eventually doesn't get respawned
    anymore if it exits repeatedly. This incorporates the
    behavior from the desktopui_Respawn Autotest test.
    
    BUG=none
    TEST=it passes
    
    Change-Id: I36dd5087366c35312694a6a6d9bff6bffd64e603

--- src/chromiumos/tast/local/bundles/cros/ui/session_manager_respawn.go
+++ src/chromiumos/tast/local/bundles/cros/ui/session_manager_respawn.go
@@ -6,6 +6,8 @@ package ui
 
 import (
 	"fmt"
+	"syscall"
+	"time"
 
 	"chromiumos/tast/local/bundles/cros/ui/respawn"
 	"chromiumos/tast/local/upstart"
@@ -45,5 +47,30 @@ func SessionManagerRespawn(s *testing.State) {
 	if err := upstart.EnsureJobRunning(s.Context(), "ui"); err != nil {
 		s.Fatal("Failed to ensure ui job is running: ", err)
 	}
-	respawn.TestRespawn(s, "session_manager", getPID)
+	pid := respawn.TestRespawn(s, "session_manager", getPID)
+
+	respawnStopped := false
+	const (
+		maxRespawns    = 30 // very high upper bound; see ui-respawn script for actual logic
+		respawnTimeout = 5 * time.Second
+	)
+	s.Log("Repeatedly killing session_manager to check that ui-respawn stops restarting it eventually")
+	for i := 0; i < maxRespawns; i++ {
+		s.Logf("Killing %d and watching for respawn", pid)
+		if err := syscall.Kill(pid, syscall.SIGKILL); err != nil {
+			s.Fatalf("Failed to kill %d: %v", pid, err)
+		}
+		var err error
+		if pid, err = respawn.WaitForProc(s.Context(), getPID, respawnTimeout, pid); err != nil {
+			respawnStopped = true
+			break
+		}
+	}
+	if !respawnStopped {
+		s.Errorf("session_manager was still respawned after being killed %d times", maxRespawns)
+	}
+
+	if err := upstart.EnsureJobRunning(s.Context(), "ui"); err != nil {
+		s.Fatal("Failed to ensure ui job is running: ", err)
+	}
 }
