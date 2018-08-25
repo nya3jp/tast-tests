7114dd4de34cbf4c1002cf2b02e2974d5e3488c3
Daniel Erat <derat@chromium.org>
2018-08-24 18:16:16 -0700

    tast-tests: Add security.MountSymlink.
    Add a new local test that verifies that symlinks can't be
    used as mount targets. This is a port of the
    security_ChromiumOSLSM Autotest-based test.
    
    BUG=chromium:877733
    TEST=passes on caroline
    
    Change-Id: Ica4fbc70bf912b7fc38b5f0148bc64f5d1eee2a9
    Reviewed-on: https://chromium-review.googlesource.com/1188991
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/security/mount_symlink.go
@@ -0,0 +1,62 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: MountSymlink,
+		Desc: "Verifies that the chromiumos LSM prevents paths with symlinks from being mounted",
+	})
+}
+
+func MountSymlink(s *testing.State) {
+	base, err := ioutil.TempDir("/tmp", "mount_symlink_test.")
+	if err != nil {
+		s.Fatal("Failed creating temp dir: ", err)
+	}
+	defer os.RemoveAll(base)
+
+	mount := func(target string) error {
+		s.Log("Mounting", target)
+		cmd := testexec.CommandContext(s.Context(),
+			"mount", "-c", "-n", "-t", "tmpfs", "-o", "nodev,noexec,nosuid", "test", target)
+		err := cmd.Run()
+
+		if err == nil {
+			s.Log("Mount succeeded; unmounting")
+			if err := testexec.CommandContext(s.Context(), "umount", "-n", target).Run(); err != nil {
+				s.Errorf("Unmounting %v failed: %v", target, err)
+			}
+		} else {
+			s.Log("Mount failed: ", err)
+		}
+		return err
+	}
+
+	mntTarget := filepath.Join(base, "mount_point")
+	if err = os.Mkdir(mntTarget, 0700); err != nil {
+		s.Fatal("Failed creating mount target: ", err)
+	}
+	if err = mount(mntTarget); err != nil {
+		s.Errorf("Mounting %v failed: %v", mntTarget, err)
+	}
+
+	symTarget := filepath.Join(base, "symlink")
+	if err = os.Symlink(filepath.Base(mntTarget), symTarget); err != nil {
+		s.Fatal("Failed creating symlink: ", err)
+	}
+	if err = mount(symTarget); err == nil {
+		s.Errorf("Mounting %v unexpectedly succeeded", symTarget)
+	}
+}
