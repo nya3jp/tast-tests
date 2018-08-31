9998c7b7f9e1ed1ea5cfad4b9212cbc013135197
Daniel Erat <derat@chromium.org>
2018-08-31 07:11:21 -0700

    tast-tests: Add platform.Histograms test.
    Add a new chromiumos/tast/local/chrome/metrics package
    containing a GetHistogram function that can be used to get
    histograms from Chrome. It also contains WaitForHistogram
    and WaitForHistogramUpdate helper functions that can be used
    to wait for a histogram to appear or for a histogram to be
    updated.
    
    Also add a new platform.Histograms test that calls
    metrics_client to verify the libmetrics->Chrome path that
    Chrome OS system daemons use to report metrics.
    
    BUG=chromium:878641
    TEST=platform.Histograms and new unit tests pass
    
    Change-Id: I71604c347314bed4730b6bfcdfe9b0ecc24de7db

---
+++ src/chromiumos/tast/local/chrome/metrics/histogram.go
@@ -0,0 +1,177 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package metrics
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// Histogram contains data from a single Chrome histogram.
+type Histogram struct {
+	// Buckets contains ranges of reported values.
+	// The buckets are disjoint and stored in ascending order.
+	Buckets []HistogramBucket `json:"buckets"`
+}
+
+// validate checks h's buckets and returns an error if any have invalid ranges or overlap.
+func (h *Histogram) validate() error {
+	var lastMax int64 = math.MinInt64
+	for _, b := range h.Buckets {
+		if b.Min >= b.Max {
+			return fmt.Errorf("invalid bucket [%d,%d)", b.Min, b.Max)
+		}
+		if b.Min < lastMax {
+			return fmt.Errorf("bucket [%d,%d) overlaps previous bucket", b.Min, b.Max)
+		}
+		lastMax = b.Max
+	}
+	return nil
+}
+
+// TotalCount returns the total number of samples stored across all buckets.
+func (h *Histogram) TotalCount() int64 {
+	var t int64
+	for _, b := range h.Buckets {
+		t += b.Count
+	}
+	return t
+}
+
+// Diff returns a histogram containing the additional samples in h that aren't in old, an older version of the same histogram.
+// Buckets that haven't changed are omitted from the returned histogram.
+// old must be an earlier snapshot -- an error is returned if any counts decreased or if old contains buckets not present in h.
+func (h *Histogram) Diff(old *Histogram) (*Histogram, error) {
+	if len(old.Buckets) > len(h.Buckets) {
+		return nil, fmt.Errorf("old histogram has %d bucket(s), new only has %d", len(old.Buckets), len(h.Buckets))
+	}
+
+	diff := &Histogram{}
+	oi := 0
+	for _, hb := range h.Buckets {
+		// If we've already looked at all of the old buckets, copy the new bucket over.
+		if oi >= len(old.Buckets) {
+			diff.Buckets = append(diff.Buckets, hb)
+			continue
+		}
+
+		ob := old.Buckets[oi]
+
+		switch {
+		case ob.Min < hb.Min:
+			// The old histogram shouldn't contain any buckets that aren't in the new one.
+			return nil, fmt.Errorf("bucket [%d,%d) is present in old histogram but not new one", ob.Min, ob.Max)
+		case ob.Min > hb.Min:
+			// If this bucket isn't present in the old histogram, just copy it over.
+			if ob.Min < hb.Max {
+				return nil, fmt.Errorf("old bucket [%d,%d) overlaps new bucket [%d,%d)", ob.Min, ob.Max, hb.Min, hb.Max)
+			}
+			diff.Buckets = append(diff.Buckets, hb)
+		case ob.Min == hb.Min:
+			// If we're looking at the same bucket in both histograms, save the difference (if any) and move to the next old bucket.
+			if ob.Max != hb.Max {
+				return nil, fmt.Errorf("old bucket [%d,%d) doesn't match new bucket [%d,%d)", ob.Min, ob.Max, hb.Min, hb.Max)
+			}
+			if hb.Count < ob.Count {
+				return nil, fmt.Errorf("old bucket [%d,%d) has count %d, new only has %d", ob.Min, ob.Max, ob.Count, hb.Count)
+			} else if hb.Count > ob.Count {
+				diff.Buckets = append(diff.Buckets, HistogramBucket{hb.Min, hb.Max, hb.Count - ob.Count})
+			}
+			oi++
+		}
+	}
+	return diff, nil
+}
+
+// String contains a human-readable representation of h as "[[0,5):2 [5,10):1 ...]",
+// where each space-separated term is "[<min>,<max>):<count>".
+func (h *Histogram) String() string {
+	var strs []string
+	for _, b := range h.Buckets {
+		strs = append(strs, fmt.Sprintf("[%d,%d):%d", b.Min, b.Max, b.Count))
+	}
+	return "[" + strings.Join(strs, " ") + "]"
+}
+
+// HistogramBucket contains a set of reported samples within a fixed range.
+type HistogramBucket struct {
+	// Min contains the minimum value that can be stored in this bucket.
+	Min int64 `json:"min"`
+	// Max contains the exclusive maximum value for this bucket.
+	Max int64 `json:"max"`
+	// Count contains the number of samples that are stored in this bucket.
+	Count int64 `json:"count"`
+}
+
+// GetHistogram returns the current state of a Chrome histogram (e.g. "Tabs.TabCountActiveWindow").
+// An error is returned if no samples have been reported for the histogram since Chrome was started.
+func GetHistogram(ctx context.Context, cr *chrome.Chrome, name string) (*Histogram, error) {
+	conn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	h := Histogram{}
+	expr := fmt.Sprintf(
+		`new Promise(function(resolve, reject) {
+			chrome.autotestPrivate.getHistogram(%q, function(h) {
+				if (chrome.runtime.lastError === undefined) {
+					resolve(h);
+				} else {
+					reject(chrome.runtime.lastError.message);
+				}
+			});
+		})`, name)
+	if err := conn.EvalPromise(ctx, expr, &h); err != nil {
+		return nil, err
+	}
+	if err = h.validate(); err != nil {
+		return nil, fmt.Errorf("bad histogram %v: %v", h, err)
+	}
+	return &h, nil
+}
+
+// WaitForHistogram is a convenience function that calls GetHistogram until the requested histogram is available,
+// ctx's deadline is reached, or timeout (if positive) has elapsed.
+func WaitForHistogram(ctx context.Context, cr *chrome.Chrome, name string, timeout time.Duration) (*Histogram, error) {
+	var h *Histogram
+	err := testing.Poll(ctx, func(ctx context.Context) error {
+		var err error
+		h, err = GetHistogram(ctx, cr, name)
+		return err
+	}, &testing.PollOptions{Timeout: timeout})
+	return h, err
+}
+
+// WaitForHistogramUpdate is a convenience function that calls GetHistogram until the requested histogram contains
+// at least one sample not present in old, an earlier snapshot of the same histogram.
+// A histogram containing the new samples is returned; see Histogram.Diff for details.
+func WaitForHistogramUpdate(ctx context.Context, cr *chrome.Chrome, name string,
+	old *Histogram, timeout time.Duration) (*Histogram, error) {
+	var h *Histogram
+	err := testing.Poll(ctx, func(ctx context.Context) error {
+		var err error
+		if h, err = GetHistogram(ctx, cr, name); err != nil {
+			return err
+		}
+		if reflect.DeepEqual(h, old) {
+			return errors.New("histogram unchanged")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: timeout})
+
+	if err != nil {
+		return nil, err
+	}
+	return h.Diff(old)
+}
---
+++ src/chromiumos/tast/local/chrome/metrics/histogram_test.go
@@ -0,0 +1,158 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package metrics
+
+import (
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+// makeHist parses a sequence of pipe-separated "<min> <max> <count>" buckets,
+// e.g. "0 5 1 | 5 10 3 | 10 15 2" for [0,5):1 [5,10):3 [10,15):2.
+func makeHist(t *testing.T, s string) *Histogram {
+	parseNum := func(s string) int64 {
+		n, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			t.Fatalf("Failed parsing %q: %v", s, err)
+		}
+		return n
+	}
+
+	h := &Histogram{}
+	for _, b := range strings.Split(s, "|") {
+		nums := strings.Fields(b)
+		if len(nums) == 0 {
+			continue
+		} else if len(nums) != 3 {
+			t.Fatalf("Didn't find \"<min> <max> <samples>\" in %q", b)
+		}
+		h.Buckets = append(h.Buckets, HistogramBucket{parseNum(nums[0]), parseNum(nums[1]), parseNum(nums[2])})
+	}
+	return h
+}
+
+func TestHistogramValidate(t *testing.T) {
+	for _, tc := range []struct {
+		buckets string // passed to makeHist
+		valid   bool
+	}{
+		{"", true},
+		{"0 3 4", true},
+		{"6 8 1", true},
+		{"-1 1 1", true},
+		{"0 3 4 | 3 6 8", true},
+		{"0 1 4 | 1 2 8 | 2 3 5", true},
+		{"2 2 5", false},          // max must be >= min
+		{"3 2 5", false},          // max must be >= min
+		{"5 6 1 | 4 5 2", false},  // buckets must be increasing
+		{"0 5 1 | 4 10 2", false}, // buckets can't overlap
+	} {
+		h := makeHist(t, tc.buckets)
+		err := h.validate()
+		if err != nil && tc.valid {
+			t.Errorf("validate() failed for %v: %v", *h, err)
+		} else if err == nil && !tc.valid {
+			t.Errorf("validate() unexpectedly succeeded for %v", *h)
+		}
+	}
+}
+
+func TestHistogramDiff(t *testing.T) {
+	const expectErr = "err"
+	for _, tc := range []struct {
+		before, after, diff string // passed to makeHist
+	}{
+		{
+			before: "0 5 1 | 5 10 2 | 10 20 2",
+			after:  "0 5 3 | 5 10 6 | 10 20 3",
+			diff:   "0 5 2 | 5 10 4 | 10 20 1",
+		},
+		{
+			before: "0 5 1 | 5 10 2 |        ",
+			after:  "0 5 3 | 5 10 6 | 10 20 3",
+			diff:   "0 5 2 | 5 10 4 | 10 20 3",
+		},
+		{
+			before: "0 5 1 |        | 10 20 2",
+			after:  "0 5 3 | 5 10 6 | 10 20 3",
+			diff:   "0 5 2 | 5 10 6 | 10 20 1",
+		},
+		{
+			before: "      | 5 10 1 | 10 20 2",
+			after:  "0 5 3 | 5 10 6 | 10 20 3",
+			diff:   "0 5 3 | 5 10 5 | 10 20 1",
+		},
+		{
+			before: "      |        |        ",
+			after:  "0 5 3 | 5 10 6 | 10 20 3",
+			diff:   "0 5 3 | 5 10 6 | 10 20 3",
+		},
+		{
+			before: "      | 5 10 2 |        ",
+			after:  "0 5 3 | 5 10 6 | 10 20 3",
+			diff:   "0 5 3 | 5 10 4 | 10 20 3",
+		},
+		{
+			before: "0 5 1 | 5 10 2 | 10 20 2",
+			after:  "0 5 1 | 5 10 2 | 10 20 2",
+			diff:   "", // unchanged (same values)
+		},
+		{
+			before: "",
+			after:  "",
+			diff:   "", // unchanged (both empty)
+		},
+		{
+			before: "0 5 2",
+			after:  "0 5 1",
+			diff:   expectErr, // count decreased
+		},
+		{
+			before: "0 4 1",
+			after:  "0 5 2",
+			diff:   expectErr, // same min but different max
+		},
+		{
+			before: "0 5 1",
+			after:  "1 5 2",
+			diff:   expectErr, // same max but before min is less
+		},
+		{
+			before: "1 5 1",
+			after:  "0 5 2",
+			diff:   expectErr, // same max but before min is greater
+		},
+		{
+			before: "0 5 1 | 5 10 2 | 10 20 3",
+			after:  "0 5 2 |        | 10 20 4",
+			diff:   expectErr, // before has bucket not in after
+		},
+		{
+			before: "1 5 1",
+			after:  "",
+			diff:   expectErr, // after is empty
+		},
+	} {
+		before := makeHist(t, tc.before)
+		after := makeHist(t, tc.after)
+		diff, err := after.Diff(before)
+		if err != nil {
+			if tc.diff != expectErr {
+				t.Errorf("%v.Diff(%v) failed: %v", after, before, err)
+			}
+		} else {
+			if tc.diff == expectErr {
+				t.Errorf("%v.Diff(%v) = %v; want error", after, before, diff)
+			} else {
+				exp := makeHist(t, tc.diff)
+				if !reflect.DeepEqual(*diff, *exp) {
+					t.Errorf("%v.Diff(%v) = %v; want %v", after, before, diff, exp)
+				}
+			}
+		}
+	}
+}
