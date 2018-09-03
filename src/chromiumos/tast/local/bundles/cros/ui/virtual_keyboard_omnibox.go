8268d7abe0409fcbfd20a0edfaec7479367f41a4
Darren Shen <shend@chromium.org>
2018-09-03 14:28:49 +1000

    Add new virtualkeyboard.Omnibox test.
    Add a new test which checks that the virtual keyboard appears when
    the user clicks on the omnibox.
    
    BUG=chromium:879073
    
    TEST=tested with eve device. Confirmed test fails if we don't enable the
         virtual keyboard.
    
    Change-Id: Ib20acad3015b824025843067f866f4bbdf13db23

---
+++ src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_omnibox.go
@@ -0,0 +1,81 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/ui/vkb"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/faillog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VirtualKeyboardOmnibox,
+		Desc:         "Checks that the virtual keyboard appears when clicking on the omnibox",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func VirtualKeyboardOmnibox(s *testing.State) {
+	// Click on the omnibox using the automation API.
+	clickOmnibox := func(ctx context.Context, tconn *chrome.Conn) error {
+		return tconn.EvalPromise(ctx, `
+new Promise((resolve, reject) => {
+	chrome.automation.getDesktop(root => {
+		root.addEventListener('loadComplete', () => {
+			const omnibox = root.find({ attributes: { role: 'textField', inputType: 'url' }});
+			if (omnibox) {
+				omnibox.doDefault();
+				resolve();
+			} else {
+				reject('Could not find the omnibox in accessibility tree');
+			}
+		});
+	});
+})
+`, nil)
+	}
+
+	defer faillog.SaveIfError(s)
+
+	ctx := s.Context()
+
+	cr, err := chrome.New(s.Context(), chrome.ExtraArgs([]string{"--enable-virtual-keyboard"}))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	shown, err := vkb.IsShown(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to check if the virtual keyboard is initially hidden: ", err)
+	}
+	if shown {
+		s.Fatal("Virtual keyboard is shown, but expected it to be hidden")
+	}
+
+	if err := clickOmnibox(ctx, tconn); err != nil {
+		s.Fatal("Failed to click the omnibox: ", err)
+	}
+
+	s.Log("Waiting for the virtual keyboard to show")
+	if err := vkb.WaitUntilShown(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for the virtual keyboard to show: ", err)
+	}
+
+	s.Log("Waiting for the virtual keyboard to render buttons")
+	if err := vkb.WaitUntilButtonsRender(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for the virtual keyboard to render: ", err)
+	}
+}
