d4d4b37fc8f7af88364539b7268e7846ed307a74
Qijiang Fan <fqj@chromium.org>
2018-09-06 17:23:05 +0900

    tast-tests: add test security.SELinux
    Initialize test security.SELinux.
    
    BUG=b:114172200
    TEST=tast run DUT security.SELinux*
    CQ-DEPEND=CL:1212644,CL:1215504,CL:1215505
    
    Change-Id: I85e5b4fd51f7142c38d48dc2ac9f562fd9ff3865

---
+++ src/chromiumos/tast/local/bundles/cros/security/selinux_file_label.go
@@ -0,0 +1,63 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"os"
+	"syscall"
+
+	"chromiumos/tast/local/bundles/cros/security/selinux"
+	"chromiumos/tast/testing"
+)
+
+type checkArgs struct {
+	path      string
+	context   string
+	recursive bool
+	filter    selinux.SELinuxFileLabelCheckFilter
+}
+
+// SELinux file label check args
+var testArgs = []checkArgs{
+	checkArgs{"/sbin/init", "u:object_r:chromeos_init_exec:s0", false, nil},
+	checkArgs{"/run/cras", "u:object_r:cras_socket:s0", true, nil},
+	checkArgs{"/sys/fs/cgroup", "u:object_r:tmpfs:s0", false, nil},
+	checkArgs{"/sys/fs/cgroup", "u:object_r:cgroup:s0", true, selinux.IgnorePath("/sys/fs/cgroup")},
+	checkArgs{"/sys/fs/pstore", "u:object_r:pstorefs:s0", false, nil},
+	checkArgs{"/sys/fs/selinux", "u:object_r:selinuxfs:s0", true, selinux.IgnorePath("/sys/fs/selinux/null")},
+	checkArgs{"/sys/fs/selinux", "u:object_r:null_device:s0", true, selinux.FilterReverse(selinux.IgnorePath("/sys/fs/selinux/null"))},
+	checkArgs{"/run/chrome/wayland-0", "u:object_r:wayland_socket:s0", false, nil},
+	checkArgs{"/sys/kernel/config", "u:object_r:configfs:s0", false, nil},
+	checkArgs{"/sys/kernel/debug", "u:object_r:debugfs:s0", false, nil},
+	checkArgs{"/sys/kernel/debug/tracing", "u:object_r:debugfs_tracing:s0", false, nil},
+	checkArgs{"/sys/kernel/debug/debugfs_tracing_on", "u:object_r:debugfs_tracing:s0", false, selinux.SkipNonExist},
+	checkArgs{"/sys/kernel/debug/trace_marker", "u:object_r:debugfs_trace_marker:s0", false, selinux.SkipNonExist},
+	checkArgs{"/sys/devices/system/cpu", "u:object_r:sysfs_devices_system_cpu:s0", true, func(_ *testing.State, _ string, f os.FileInfo) bool {
+		mode := f.Mode()
+		return mode.IsRegular() && ((mode.Perm() & (syscall.S_IWUSR | syscall.S_IWGRP | syscall.S_IWOTH)) > 0)
+	}},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SELinuxFileLabel,
+		Desc:         "Checks that SELinux file labels are set correctly.",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"selinux"},
+	})
+}
+
+func SELinuxFileLabel(s *testing.State) {
+	for _, testArg := range testArgs {
+		stat, _ := os.Lstat(testArg.path)
+		if testArg.filter == nil || (!testArg.filter(s, testArg.path, stat)) {
+			selinux.AssertSELinuxFileContext(s, testArg.path, testArg.context)
+		}
+		if testArg.recursive {
+			selinux.CheckSELinuxFileContextRecursively(s, testArg.path, testArg.context, testArg.filter)
+		}
+	}
+
+}
