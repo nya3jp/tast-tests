78f6a8bd42c803450e92c685069d7ab4824cb3c9
Keiichi Watanabe <keiichiw@chromium.org>
2018-09-11 17:11:40 +0900

    video: Port WebRTC camera tests from autotest
    Add video.WebRTCCamera and video.WebRTCPeerConnectionWithCamera(H264|VP8).
    These are Tast version of video_WebRtcCamera and
    video_WebRtcPeerConnectionWithCamera.(h264|vp8) in autotest, respectively.
    
    Since main procedures for each test are originally written in
    JavaScript, HTML/JS files added by this commit are almost same with
    ones in autotests repository.
    Each test is executed by calling JS functions from webrtc.go.
    
    If there tests are executed on VM, vivid will be used as a video input
    device.
    
    BUG=chromium:882433, chromium:852302
    TEST=Run each test on kevin by 'tast run'
    TEST=Run each test on amd64-generic VM by 'tast run'
    CQ-DEPEND=CL:1220426, CL:1222293, CL:1223890, CL:1214903
    
    Change-Id: I666c4f960688686a9a91a79d04ee4db49d7b1e6f
    Signed-off-by: Keiichi Watanabe <keiichiw@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/video/webrtc_peer_connection_with_camera_h264.go
@@ -0,0 +1,40 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"chromiumos/tast/local/bundles/cros/video/webrtc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WebRTCPeerConnectionWithCameraH264,
+		Desc:         "Ensures WebRTC call can be established and produces healthy H264 video",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+		Data:         append(webrtc.DataFiles(), "munge_sdp.js", "loopback.html"),
+	})
+}
+
+// WebRTCPeerConnectionWithCameraH264 starts a loopback WebRTC call with two
+// peer connections and ensures it successfully establishes the call (otherwise
+// the test will simply fail). If successful, it looks at the video frames
+// coming out on the receiving side of the call and looks for freezes and black
+// frames.
+//
+// If this test shows black frames and video.WebRTCCamera does not, it could
+// mean H264 video isn't encoded/decoded right on this device but that the
+// camera works. Finally, input and output FPS are logged.
+//
+// This test uses the real webcam unless it is running under QEMU. Under QEMU,
+// it uses "vivid" instead, which is the virtual video test driver and can be
+// used as an external USB camera.
+//
+// TODO(keiichiw): When adding perf metrics, add comments here.
+func WebRTCPeerConnectionWithCameraH264(s *testing.State) {
+	webrtc.RunTest(s, "loopback.html", "testWebRtcLoopbackCall('H264')")
+	// TODO(keiichiw): Add perf metrics.
+}
