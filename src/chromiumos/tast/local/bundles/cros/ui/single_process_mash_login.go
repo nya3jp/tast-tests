0dc107ea59d50412bdb539ada61c4ff8fdbfacf2
James Cook <jamescook@chromium.org>
2018-09-12 16:10:02 -0700

    tast-tests: Add SingleProcessMashLogin test
    This exercises chrome login with --enable-features=SingleProcessMash.
    SingleProcessMash exercises the new mash window service mojo APIs,
    but keeps ash system UI code running in the browser process. It's
    the next milestone for the mustash team.
    
    BUG=chromium:847901
    TEST=tast -verbose run ui.SingleProcessMashLogin on veyron_minnie and also amd64-generic in a VM
    
    Change-Id: I6b4f6485da207aeb7373c1da33ee97e1ca3f979a

---
+++ src/chromiumos/tast/local/bundles/cros/ui/single_process_mash_login.go
@@ -0,0 +1,31 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/faillog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SingleProcessMashLogin,
+		Desc:         "Checks that chrome --enable-features=SingleProcessMash starts",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+// SingleMashLogin checks that chrome --enable-features=SingleProcessMash starts normally.
+func SingleProcessMashLogin(s *testing.State) {
+	defer faillog.SaveIfError(s)
+
+	cr, err := chrome.New(s.Context(), chrome.ExtraArgs([]string{"--enable-features=SingleProcessMash"}))
+	if err != nil {
+		s.Fatal("Chrome login failed: ", err)
+	}
+	cr.Close(s.Context())
+}
