cebd1834b2faacb20e6bf2aea5755c74d6cc1437
Hidehiko Abe <hidehiko@chromium.org>
2018-09-14 04:31:26 +0900

    tast-test: Add security.Sandboxed.
    This CL translates security_SandboxStatus._CheckAdequatelySandboxed
    into tast.
    
    BUG=chromium:883197
    TEST=Ran tast.
    
    Change-Id: Ic63b3e1f488771cae7525f820003651c586b0ab1

---
+++ src/chromiumos/tast/local/bundles/cros/security/sandboxed.go
@@ -0,0 +1,61 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/faillog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Sandboxed,
+		Desc: "Verify sandbox status",
+	})
+}
+
+func Sandboxed(s *testing.State) {
+	const (
+		url      = "chrome://sandbox"
+		text     = "You are adequately sandboxed."
+		waitExpr = "document.getElementsByTagName('p')[0].textContent"
+	)
+	defer faillog.SaveIfError(s)
+
+	ctx := s.Context()
+
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	conn, err := cr.NewConn(ctx, "")
+	if err != nil {
+		s.Fatal("Failed to create a new connection: ", err)
+	}
+	defer conn.Close()
+
+	if err = conn.Navigate(ctx, url); err != nil {
+		s.Fatal("Failed to open a page: ", err)
+	}
+
+	if err = conn.WaitForExpr(ctx, waitExpr, chrome.Timeout(30*time.Second)); err != nil {
+		s.Fatalf("Failed to evaluate in %s \"%s\"", url, waitExpr)
+	}
+
+	c, err := conn.PageContent(ctx)
+	if err != nil {
+		s.Fatal("Failed to obtain the page content.")
+	}
+
+	if !strings.Contains(c, text) {
+		s.Errorf("Could not find \"%s\" \"%s\" in %s", text, c, url)
+	}
+}
