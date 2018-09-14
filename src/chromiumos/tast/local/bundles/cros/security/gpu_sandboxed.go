60143f5f3cbbb5f76bb2f1ef154324bb021fa8e6
Hidehiko Abe <hidehiko@chromium.org>
2018-09-15 00:55:23 +0900

    tast-test: Add security.GpuSandboxed.
    This CL translates security_SandboxStatus._CheckGPUSandboxed
    into tast.
    
    BUG=chromium:883197
    TEST=Ran tast.
    
    Change-Id: Ie253f737780a2260b7a8307e9ccbe6f5acbcf12e

---
+++ src/chromiumos/tast/local/bundles/cros/security/gpu_sandboxed.go
@@ -0,0 +1,62 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/faillog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GpuSandboxed,
+		Desc:         "Verify GPU sandbox status",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func GpuSandboxed(s *testing.State) {
+	const (
+		url      = "chrome://gpu"
+		waitExpr = "browserBridge.isSandboxedForTesting()"
+	)
+	defer faillog.SaveIfError(s)
+
+	ctx := s.Context()
+
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	// "chrome://about" cannot be opened directly, due to cdp
+	// implementation.
+	// https://github.com/mafredri/cdp/blob/master/devtool/devtool.go#L80
+	// This is workaround; create the connection and then naviagate
+	// to the page.
+	conn, err := cr.NewConn(ctx, "")
+	if err != nil {
+		s.Fatal("Failed to create a new connection: ", err)
+	}
+	defer conn.Close()
+
+	if err = conn.Navigate(ctx, url); err != nil {
+		s.Fatalf("Failed to open \"%s\": %v", url, err)
+	}
+
+	{
+		ectx, cancel := context.WithTimeout(ctx, 30*time.Second)
+		defer cancel()
+		if err = conn.WaitForExpr(ectx, waitExpr); err != nil {
+			s.Fatalf("Failed to evaluate in %s \"%s\"", url, waitExpr)
+		}
+	}
+}
