b90afae6401a16e3243466be87731b5bcc90fde4
Daniel Erat <derat@chromium.org>
2018-09-17 15:43:09 -0700

    tast-tests: Make meta tests save stdout and stderr.
    Update tastrun.Run (used by meta.ListTests and
    meta.RunTests) to write stdout.txt and stderr.txt output
    files. This is intending to make debugging test failures
    easier.
    
    BUG=none
    TEST=manual: passed a bogus command-line flag and checked
         that meta.ListTests saves tast's stderr to stderr.txt
    
    Change-Id: I7ef87c85b5874bd2013e40788f70f2790722c6c6
    Reviewed-on: https://chromium-review.googlesource.com/1229393
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/remote/bundles/cros/meta/tastrun/run.go
+++ src/chromiumos/tast/remote/bundles/cros/meta/tastrun/run.go
@@ -7,7 +7,9 @@ package tastrun
 
 import (
 	"errors"
+	"io/ioutil"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"chromiumos/tast/testing"
@@ -17,6 +19,8 @@ import (
 // subcmd contains the subcommand to use, e.g. "list" or "run".
 // flags contains subcommand-specific flags.
 // patterns contains a list of patterns matching tests.
+// stdout is saved to an stdout.txt output file unconditionally.
+// stderr is saved to stderr.txt if the command fails.
 func Run(s *testing.State, subcmd string, flags, patterns []string) (stdout []byte, err error) {
 	meta := s.Meta()
 	if meta == nil {
@@ -28,6 +32,17 @@ func Run(s *testing.State, subcmd string, flags, patterns []string) (stdout []by
 	args = append(args, meta.Target)
 	args = append(args, patterns...)
 	cmd := exec.CommandContext(s.Context(), meta.TastPath, args...)
+
 	s.Log("Running ", strings.Join(cmd.Args, " "))
-	return cmd.Output()
+	out, err := cmd.Output()
+
+	if werr := ioutil.WriteFile(filepath.Join(s.OutDir(), "stdout.txt"), out, 0644); werr != nil {
+		s.Log("Failed to save stdout: ", werr)
+	}
+	if exitErr, ok := err.(*exec.ExitError); ok && exitErr.Stderr != nil {
+		if werr := ioutil.WriteFile(filepath.Join(s.OutDir(), "stderr.txt"), exitErr.Stderr, 0644); werr != nil {
+			s.Log("Failed to save stderr: ", werr)
+		}
+	}
+	return out, err
 }
