ccd4c2c47e5c1b3ab8b595b3ea91fa4b8bd7dd69
Andrew Moylan <amoylan@chromium.org>
2018-09-18 15:33:02 +1000

    tast-tests: Test Chrome->ML Service IPC bootstrap
    This test calls autotestPrivate.bootstrapMachineLearningService
    introduced in crrev.com/c/1229878 to make a simple call from Chrome to
    ML Service, which triggers the Mojo IPC bootstrap between the two
    processes.
    
    TEST=tast -verbose run $IP platform.MlServiceBootstrap
    BUG=chromium:836102
    
    Change-Id: I66e827560e85716ab23cda326de64a06e727a2ce

---
+++ src/chromiumos/tast/local/bundles/cros/platform/ml_service_bootstrap.go
@@ -0,0 +1,63 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MlServiceBootstrap,
+		Desc:         "Checks that Chrome can establish a Mojo connection to ML Service",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func MlServiceBootstrap(s *testing.State) {
+	ctx := s.Context()
+
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	const job = "ml-service"
+
+	s.Log("Stopping ML Service daemon if it is running")
+	if err = upstart.StopJob(ctx, job); err != nil {
+		s.Fatalf("Failed to stop %s: %v", job, err)
+	}
+
+	s.Log("Waiting for Chrome to complete a basic call to ML Service")
+	if err = tconn.EvalPromise(ctx,
+		`new Promise((resolve, reject) => {
+		   chrome.autotestPrivate.bootstrapMachineLearningService(() => {
+		     if (chrome.runtime.lastError === undefined) {
+		       resolve();
+		     } else {
+		       reject(chrome.runtime.lastError.message);
+		     }
+		   });
+		 })`, nil); err != nil {
+		s.Fatal("Running autotestPrivate.bootstrapMachineLearningService failed: ", err)
+	}
+
+	s.Log("Checking ML Service is running")
+	if _, state, _, err := upstart.JobStatus(ctx, job); err != nil {
+		s.Fatalf("Failed to get status of job %s: %v", job, err)
+	} else if state != upstart.RunningState {
+		s.Fatalf("Job %s is %s; want running", job, state)
+	}
+}
