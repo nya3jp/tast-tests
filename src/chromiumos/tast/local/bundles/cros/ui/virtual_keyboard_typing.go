a9ac6f972fbc49a3f032eaedd0d5d0ce4446d0af
Darren Shen <shend@chromium.org>
2018-09-20 17:57:27 +1000

    Add new ui.VirtualkeyboardTyping test.
    Add a new test which checks that tapping keys on the virtual
    keyboard sends those keys to a text field on a web page.
    
    BUG=chromium:879073
    
    TEST=tested with eve device.
    
    Change-Id: I21ef9232dc81f0953913acc3bd352868e4411c19

---
+++ src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_typing.go
@@ -0,0 +1,92 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/local/bundles/cros/ui/vkb"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VirtualKeyboardTyping,
+		Desc:         "Checks that the virtual keyboard can type into a text field",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func VirtualKeyboardTyping(s *testing.State) {
+	ctx := s.Context()
+
+	cr, err := chrome.New(ctx, chrome.ExtraArgs([]string{"--enable-virtual-keyboard"}))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	// Show a page with a text field that autofocuses. Turn off autocorrect as it
+	// can interfere with the test.
+	const html = `<input type="text" id="text" autocorrect="off" autofocus/>`
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		io.WriteString(w, html)
+	}))
+	defer server.Close()
+
+	conn, err := cr.NewConn(ctx, server.URL)
+	if err != nil {
+		s.Fatal("Creating renderer for test page failed: ", err)
+	}
+	defer conn.Close()
+
+	// Wait for the text field to focus.
+	if err := conn.WaitForExpr(ctx,
+		`document.getElementById('text') === document.activeElement`); err != nil {
+		s.Fatal("Failed to wait for text field to focus: ", err)
+	}
+
+	if err := vkb.ShowVirtualKeyboard(ctx, tconn); err != nil {
+		s.Fatal("Failed to show the virtual keyboard: ", err)
+	}
+
+	s.Log("Waiting for the virtual keyboard to show")
+	if err := vkb.WaitUntilShown(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for the virtual keyboard to show: ", err)
+	}
+
+	kconn, err := vkb.UIConn(cr, ctx)
+	if err != nil {
+		s.Fatal("Creating connection to virtual keyboard UI failed: ", err)
+	}
+	defer kconn.Close()
+
+	// Press a sequence of keys.
+	keys := []string{
+		"h", "e", "l", "l", "o", "space", "w", "o",
+		"backspace", "backspace", "t", "a", "s", "t"}
+
+	for _, key := range keys {
+		if err := vkb.TapKey(ctx, kconn, key); err != nil {
+			s.Fatalf("Failed to tap %q: %v", key, err)
+		}
+	}
+
+	s.Log("Waiting for the text field to have the correct contents")
+	if err := conn.WaitForExpr(ctx,
+		`document.getElementById('text').value === 'hello tast'`); err != nil {
+		s.Fatal("Failed to get the contents of the text field: ", err)
+	}
+}
