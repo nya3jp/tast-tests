651ae1a93e3c37f5c5a6a5d237fa86bebf93d3bc
Darren Shen <shend@chromium.org>
2018-09-20 17:57:27 +1000

    Add new ui.VirtualkeyboardTyping test.
    Add a new test which checks that tapping keys on the virtual
    keyboard sends those keys to a text field on a web page.
    
    BUG=chromium:879073
    
    TEST=tested with eve device.
    
    Change-Id: I21ef9232dc81f0953913acc3bd352868e4411c19

--- src/chromiumos/tast/local/bundles/cros/ui/vkb/vkb.go
+++ src/chromiumos/tast/local/bundles/cros/ui/vkb/vkb.go
@@ -7,9 +7,20 @@ package vkb
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"chromiumos/tast/local/chrome"
 )
+
+// ShowVirtualKeyboard forces the virtual keyboard to open.
+func ShowVirtualKeyboard(ctx context.Context, tconn *chrome.Conn) error {
+	return tconn.EvalPromise(ctx, `
+new Promise((resolve, reject) => {
+	chrome.inputMethodPrivate.showInputView(resolve);
+})
+`, nil)
+}
 
 // IsShown checks if the virtual keyboard is currently shown. It checks whether
 // there is a visible DOM element with an accessibility role of "keyboard".
@@ -65,4 +76,31 @@ new Promise((resolve, reject) => {
 	});
 })
 `, nil)
+}
+
+// UIConn returns a connection to the virtual keyboard HTML page,
+// where JavaScript can be executed to simulate interactions with the UI.
+// The connection is lazily created, and this function will block until the
+// extension is loaded or ctx's deadline is reached. The caller should close
+// the returned connection.
+func UIConn(c *chrome.Chrome, ctx context.Context) (*chrome.Conn, error) {
+	extURLPrefix := "chrome-extension://jkghodnilhceideoidjikpgommlajknk/inputview.html"
+	f := func(t *chrome.Target) bool { return strings.HasPrefix(t.URL, extURLPrefix) }
+	return c.NewConnForTarget(ctx, f)
+}
+
+// TapKey simulates a tap event on the middle of the specified key. The key can
+// be any letter of the alphabet, "space" or "backspace".
+func TapKey(ctx context.Context, kconn *chrome.Conn, key string) error {
+	return kconn.Eval(ctx, fmt.Sprintf(`
+	(() => {
+		const key = document.querySelector('[aria-label=%q]');
+		const rect = key.getBoundingClientRect();
+		const e = new Event('pointerdown');
+		e.clientX = rect.x + rect.width / 2;
+		e.clientY = rect.y + rect.height / 2;
+		key.dispatchEvent(e);
+		key.dispatchEvent(new Event('pointerup'));
+	})()
+`, key), nil)
 }
