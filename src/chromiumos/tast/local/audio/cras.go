791677127fb724d3b5c57a48ad52dcdc2d272b02
Hidehiko Abe <hidehiko@chromium.org>
2018-09-21 02:20:40 +0900

    tast: Convert audio_Microphone into tast.
    BUG=chromium:887563
    TEST=Ran locally.
    
    Change-Id: I700b8bb68ab5d1893417d3b5e445ea418a017f66
    Reviewed-on: https://chromium-review.googlesource.com/1237013
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/local/audio/cras.go
@@ -0,0 +1,93 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package audio interacts with audio operation.
+package audio
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/testing"
+)
+
+const (
+	dbusName      = "org.chromium.cras"
+	dbusPath      = "/org/chromium/cras"
+	dbusInterface = "org.chromium.cras.Control"
+)
+
+// Cras is used to interact with the cras process over D-Bus.
+// For detailed spec, please find src/third_party/adhd/cras/README.dbus-api.
+type Cras struct {
+	obj dbus.BusObject
+}
+
+func NewCras(ctx context.Context) (*Cras, error) {
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		return nil, fmt.Errorf("Failed connection to system bus: %v", err)
+	}
+
+	testing.ContextLogf(ctx, "Waiting for %s D-Bus service", dbusName)
+	if err := dbusutil.WaitForService(ctx, conn, dbusName); err != nil {
+		return nil, fmt.Errorf("Failed waiting for Cras service: %v", err)
+	}
+
+	obj := conn.Object(dbusName, dbusPath)
+	return &Cras{obj}, nil
+}
+
+// CrasNode contains the metadata of Node in Cras.
+// Currently fields which are actually needed by tests are defined.
+// Please find src/third_party/adhd/cras/README.dbus-api for the meaning of
+// each fields.
+type CrasNode struct {
+	Active     bool
+	IsInput    bool
+	DeviceName string
+}
+
+// GetNodes calls cras.Control.GetNodes over D-Bus.
+func (c *Cras) GetNodes(ctx context.Context) ([]CrasNode, error) {
+	call := c.call(ctx, "GetNodes")
+	if call.Err != nil {
+		return nil, call.Err
+	}
+
+	// cras.Control.GetNodes D-Bus method's signature is not fixed.
+	// Specifically, the number of output values depends on the actual
+	// number of nodes.
+	// That usage is not common practice, and it is less supported in
+	// godbus. Here, instead, values are manually converted via
+	// dbus.Variant.
+	nodes := make([]CrasNode, len(call.Body))
+	for i, n := range call.Body {
+		mp := n.(map[string]dbus.Variant)
+		if active, ok := mp["Active"]; !ok {
+			return nil, fmt.Errorf("'Active' not found: %v", mp)
+		} else if nodes[i].Active, ok = active.Value().(bool); !ok {
+			return nil, fmt.Errorf("'Active' is not bool: %v", mp)
+		}
+		if isInput, ok := mp["IsInput"]; !ok {
+			return nil, fmt.Errorf("'IsInput' not found: %v", mp)
+		} else if nodes[i].IsInput, ok = isInput.Value().(bool); !ok {
+			return nil, fmt.Errorf("'IsInput' is not bool: %v", mp)
+		}
+		if deviceName, ok := mp["DeviceName"]; !ok {
+			return nil, fmt.Errorf("'DeviceName' not found: %v", mp)
+		} else if nodes[i].DeviceName, ok = deviceName.Value().(string); !ok {
+			return nil, fmt.Errorf("'DeviceName' is not string: %v", mp)
+		}
+	}
+	return nodes, nil
+}
+
+// call is a wrapper around CallWithContext for convenience.
+func (c *Cras) call(ctx context.Context, method string, args ...interface{}) *dbus.Call {
+	return c.obj.CallWithContext(ctx, dbusInterface+"."+method, 0, args...)
+}
