ae97704d0c0e213ce41d29f031a16c95b52925eb
Daniel Erat <derat@chromium.org>
2018-09-19 17:40:03 -0700

    tast-tests: Add EventWriter to input package.
    Add a new EventWriter type that can be used by local tests
    to inject input events. The interface is currently low-level
    (e.g. callers need to send synchronization events), with a
    focus on keyboard devices.
    
    Also add a short example.Input test demonstrating how to use
    the package to inject keyboard events.
    
    BUG=chromium:878734
    TEST=added unit tests; also verified that example.Input
         passes on a DUT with an internal keyboard
    
    Change-Id: Ia3e7dc86f6c5006ae0e2ef6f611cc8eb404bc2f1

---
+++ src/chromiumos/tast/local/bundles/cros/example/input.go
@@ -0,0 +1,95 @@
+package example
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Input,
+		Desc: "Demonstrates injecting keyboard events",
+		// TODO(derat): Remove "disabled" if/when there's a way to depend on an internal keyboard.
+		Attr:         []string{"disabled", "informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func Input(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to log in: ", err)
+	}
+	defer cr.Close(ctx)
+
+	const html = "<!DOCTYPE html><input id='text' type='text' autofocus>"
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		io.WriteString(w, html)
+	}))
+	defer server.Close()
+
+	s.Log("Loading input page")
+	conn, err := cr.NewConn(ctx, server.URL)
+	if err != nil {
+		s.Fatal("Creating renderer failed: ", err)
+	}
+	defer conn.Close()
+
+	s.Log("Waiting for focus")
+	if err := conn.WaitForExpr(ctx, `document.getElementById('text') === document.activeElement`); err != nil {
+		s.Fatal("Failed waiting for focus: ", err)
+	}
+
+	s.Log("Finding and opening keyboard device")
+	ew, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to open keyboard device: ", err)
+	}
+	defer ew.Close()
+
+	// TODO(derat): Replace all of this once the input package exposes friendly
+	// methods for injecting sequences of events.
+	s.Log("Injecting keyboard events")
+	for _, code := range []input.EventCode{input.KEY_H, input.KEY_E, input.KEY_L, input.KEY_L, input.KEY_O} {
+		if err := ew.Event(input.EV_KEY, code, 1); err != nil {
+			s.Fatal("Failed to write key down event for 0x%x: %v", code, err)
+		}
+		if err := ew.Sync(); err != nil {
+			s.Fatal("Failed to write key down sync for 0x%x: %v", code, err)
+		}
+		if err := ew.Event(input.EV_KEY, code, 0); err != nil {
+			s.Fatal("Failed to write key up event for 0x%x: %v", code, err)
+		}
+		if err := ew.Sync(); err != nil {
+			s.Fatal("Failed to write key up sync for 0x%x: %v", code, err)
+		}
+	}
+
+	const (
+		expr     = "document.getElementById('text').value"
+		expected = "hello"
+	)
+	s.Log("Waiting for text")
+	if err := conn.WaitForExpr(ctx, fmt.Sprintf("%s.length === %d", expr, len(expected))); err != nil {
+		s.Fatal("Failed to wait for text: ", err)
+	}
+	var actual string
+	if err := conn.Eval(ctx, expr, &actual); err != nil {
+		s.Fatal("Failed to get text: ", err)
+	}
+	s.Logf("Got text %q", actual)
+
+	// Support Caps Lock and Dvorak.
+	if lower := strings.ToLower(actual); lower != expected && lower != "d.nnr" {
+		s.Errorf("Got text %q; typed %q", actual, expected)
+	}
+}
---
+++ src/chromiumos/tast/local/input/event.go
@@ -0,0 +1,102 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package input supports injecting input events via kernel devices.
+package input
+
+import (
+	"context"
+	"encoding/binary"
+	"io"
+	"os"
+	"syscall"
+	"time"
+	"unsafe"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+//go:generate go run gen/gen_constants.go ../../../../../../../third_party/kernel/v4.14/include/uapi/linux/input-event-codes.h generated_constants.go
+//go:generate go fmt generated_constants.go
+
+// TODO(derat): Define some constants for event values, e.g. 0 for key up, 1 for key down, 2 for key repeat.
+
+// EventWriter supports injecting input events into a device.
+//
+// TODO(derat): Add methods for injecting sequences of keystrokes and for typing accelerators.
+type EventWriter struct {
+	w       io.WriteCloser // device
+	nowFunc func() time.Time
+}
+
+// Keyboard returns an EventWriter to inject events into an arbitrary keyboard device.
+func Keyboard(ctx context.Context) (*EventWriter, error) {
+	infos, err := readDevices(procDevices)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to read %v", procDevices)
+	}
+	for _, info := range infos {
+		if info.isKeyboard() {
+			testing.ContextLogf(ctx, "Opening keyboard device %+v", info)
+			return Device(ctx, info.path)
+		}
+	}
+	return nil, errors.New("didn't find keyboard device")
+}
+
+// Device returns an EventWriter for injecting input events into the input event device at path.
+func Device(ctx context.Context, path string) (*EventWriter, error) {
+	f, err := os.OpenFile(path, os.O_WRONLY, 0600)
+	if err != nil {
+		return nil, err
+	}
+	return &EventWriter{f, time.Now}, nil
+}
+
+// Close closes the device.
+func (ew *EventWriter) Close() error {
+	return ew.w.Close()
+}
+
+// Event injects an event containing the supplied values into the device.
+func (ew *EventWriter) Event(et EventType, ec EventCode, val int32) error {
+	tv := syscall.NsecToTimeval(ew.nowFunc().UnixNano())
+
+	// input_event contains a timeval struct, which uses "long" for its members.
+	// binary.Write wants explicitly-sized data, so we need to pass a different
+	// struct depending on the system's int size.
+	var ev interface{}
+	switch intSize := unsafe.Sizeof(int(1)); intSize {
+	case 4:
+		ev = &event32{int32(tv.Sec), int32(tv.Usec), uint16(et), uint16(ec), val}
+	case 8:
+		ev = &event64{tv, uint16(et), uint16(ec), val}
+	default:
+		return errors.Errorf("unexpected int size of %d byte(s)", intSize)
+	}
+
+	// Little-endian is appropriate regardless of the system's underlying endianness.
+	return binary.Write(ew.w, binary.LittleEndian, ev)
+}
+
+// event32 corresponds to a 32-bit input_event struct.
+type event32 struct {
+	Sec, Usec  int32
+	Type, Code uint16
+	Val        int32
+}
+
+// event64 corresponds to a 64-bit input_event struct.
+type event64 struct {
+	Tv         syscall.Timeval
+	Type, Code uint16
+	Val        int32
+}
+
+// Sync writes a synchronization event delineating a packet of input data occurring at a single point in time.
+// It's shorthand for Event(t, EV_SYN, SYN_REPORT, 0).
+func (ew *EventWriter) Sync() error {
+	return ew.Event(EV_SYN, SYN_REPORT, 0)
+}
