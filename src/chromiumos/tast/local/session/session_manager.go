7abd2d9dc42b17a358c8ef461516553a572ed444
Hidehiko Abe <hidehiko@chromium.org>
2018-09-20 23:08:11 +0900

    Extract SessionManager to package.
    This is preparation to support more SessionManager D-Bus methods
    and listeners.
    
    BUG=None
    TEST=Ran tast with chrome.
    
    Change-Id: I52a61f9b5a7d20e3004404e47b361a19a4da74c8

---
+++ src/chromiumos/tast/local/session/session_manager.go
@@ -0,0 +1,64 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package session interacts with session_manager.
+package session
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/testing"
+)
+
+const (
+	dbusName      = "org.chromium.SessionManager"
+	dbusPath      = "/org/chromium/SessionManager"
+	dbusInterface = "org.chromium.SessionManagerInterface"
+)
+
+// SessionManager is used to interact with the session_manager process over
+// D-Bus.
+// For detailed spec of each D-Bus method, please find
+// src/platform2/login_manager/dbus_bindings/org.chromium.SessionManagerInterface.xml
+type SessionManager struct {
+	obj dbus.BusObject
+}
+
+func NewSessionManager(ctx context.Context) (*SessionManager, error) {
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		return nil, fmt.Errorf("Failed connection to system bus: %v", err)
+	}
+
+	testing.ContextLogf(ctx, "Waiting for %s D-Bus service", dbusName)
+	if err := dbusutil.WaitForService(ctx, conn, dbusName); err != nil {
+		return nil, fmt.Errorf("Failed waiting for SessionManager service: %v", err)
+	}
+
+	obj := conn.Object(dbusName, dbusPath)
+	return &SessionManager{obj}, nil
+}
+
+// EnableChromeTesting calls SessionManager.EnableChromeTesting D-Bus method.
+func (m *SessionManager) EnableChromeTesting(
+	ctx context.Context,
+	forceRelaunch bool,
+	extraArguments []string,
+	extraEnvironmentVariables []string) (filepath string, err error) {
+	c := m.call(ctx, "EnableChromeTesting",
+		forceRelaunch, extraArguments, extraEnvironmentVariables)
+	if err := c.Store(&filepath); err != nil {
+		return "", err
+	}
+	return filepath, nil
+}
+
+// call is thin wrapper of CallWithContext for convenience.
+func (m *SessionManager) call(ctx context.Context, method string, args ...interface{}) *dbus.Call {
+	return m.obj.CallWithContext(ctx, dbusInterface+"."+method, 0, args...)
+}
