03e3fd6640291ad44488cc2b9377b54c1a734cdf
Qijiang Fan <fqj@chromium.org>
2018-09-26 13:33:33 +0900

    tast-tests: move wayland socket check to a separate test.
    wayland socket depends on to be started. Some other tests may kill
    Chrome browser. And adding chrome.New to a test that doesn't depends on
    chrome seems weird. (Currently selinux implies arc, which implies
    chrome, but things may change in the future).
    
    BUG=b:114172200,chromium:884909
    TEST=tast run DUT security.SELinux*
    
    Change-Id: Icaeb804ca7bb638c3d46876709e13493e2f0c094

---
+++ src/chromiumos/tast/local/bundles/cros/security/selinux_file_label_with_chrome.go
@@ -0,0 +1,42 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"chromiumos/tast/local/bundles/cros/security/selinux"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SELinuxFileLabelWithChrome,
+		Desc:         "Checks that SELinux file managed by Chrome are set correctly",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome", "selinux"},
+	})
+}
+
+func SELinuxFileLabelWithChrome(s *testing.State) {
+	ctx := s.Context()
+	cr, err := chrome.New(ctx, chrome.NoLogin())
+	defer cr.Close(ctx)
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	for _, testArg := range []struct {
+		path, context string
+		recursive     bool
+		filter        selinux.FileLabelCheckFilter
+	}{
+		{"/run/chrome/wayland-0", "u:object_r:wayland_socket:s0", false, nil},
+	} {
+		filter := testArg.filter
+		if filter == nil {
+			filter = selinux.CheckAll
+		}
+		selinux.CheckContext(s, testArg.path, testArg.context, testArg.recursive, filter)
+	}
+}
