238290b1a378b75507951703d3b0ca465fe573f6
Hirokazu Honda <hiroh@chromium.org>
2018-10-02 17:41:37 +0900

    video: Add PlaybackPerfH264 to Tast
    video_PlaybackPerfH264 is video_PlaybackPerf.h264 in autotest.
    It measures SW/HW playback performance and reports the performance values on
    dashboard.
    
    BUG=chromium:890733
    TEST=video.PlaybackPerfH264
    
    Change-Id: I331442d72d5bfe0a589d4e61a0f2fe5d4466c651

---
+++ src/chromiumos/tast/local/bundles/cros/video/playback_perf_h264.go
@@ -0,0 +1,28 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/playback"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PlaybackPerfH264,
+		Desc:         "Measure Playback Performance with/without HW accelerationr.",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+		Data:         []string{"traffic-1920x1080-8005020218f6b86bfa978e550d04956e.mp4"},
+	})
+}
+
+// PlaybackPerfH264 plays H264 1080p 30 fps video and measures the peformance values with/without
+// HW decoding acceleration. The values are reported to performance dashboard.
+func PlaybackPerfH264(ctx context.Context, s *testing.State) {
+	playback.RunTest(ctx, s, "traffic-1920x1080-8005020218f6b86bfa978e550d04956e.mp4", "h264_1080p")
+}
