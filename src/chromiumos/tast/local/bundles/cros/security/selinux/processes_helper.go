320b15bbf1c08c6b70c9f441b167a2065a89d9d2
Qijiang Fan <fqj@chromium.org>
2018-10-02 16:58:38 +0900

    tast-tests: add test to test process SELinux context.
    Add initial tests to test SELinux context for processes.
    This initial test only contains very few number of test cases for
    demonstration purpose.
    
    This test should be non-informational to be run in CQ to prevent changes
    breaking domain transitions.
    
    BUG=b:114172200
    TEST=tast run DUT security.SELinux*
    
    Change-Id: Iae5252f889f5023058b85357fe432b36385ab933
    Reviewed-on: https://chromium-review.googlesource.com/1255748
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Qijiang Fan <fqj@google.com>
    Reviewed-by: Dan Erat <derat@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/security/selinux/processes_helper.go
@@ -0,0 +1,81 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package selinux
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/shirou/gopsutil/process"
+)
+
+// Process represents a running process with an SELinux context.
+type Process struct {
+	PID       int
+	Cmdline   string
+	Exe       string
+	Comm      string
+	SEContext string
+}
+
+// GetProcesses returns currently-running processes.
+func GetProcesses() ([]Process, error) {
+	ps, err := process.Processes()
+	if err != nil {
+		return nil, fmt.Errorf("failed to list processes: %v", err)
+	}
+	var processes []Process
+	for _, p := range ps {
+		proc := Process{PID: int(p.Pid)}
+
+		if proc.Exe, err = p.Exe(); err != nil && !os.IsNotExist(err) {
+			// Kernel process may have exe throwing no such file when readlink.
+			// We don't want to skip kernel process.
+			return nil, err
+		}
+
+		// Read /proc/<pid>/{cmdline,comm,attr/current}
+		// Ignore this process if it doesn't exist.
+		if proc.Cmdline, err = p.Cmdline(); err != nil {
+			if !os.IsNotExist(err) {
+				return nil, err
+			}
+			continue
+		}
+
+		comm, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/comm", proc.PID))
+		if err != nil {
+			if !os.IsNotExist(err) {
+				return nil, err
+			}
+			continue
+		}
+		proc.Comm = string(comm)
+
+		secontext, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/attr/current", proc.PID))
+		if err != nil {
+			return nil, err
+		}
+		if len(secontext) == 0 || secontext[len(secontext)-1] != 0 {
+			return nil, fmt.Errorf("invalid secontext %q", secontext)
+		}
+		proc.SEContext = string(secontext[:len(secontext)-1])
+
+		processes = append(processes, proc)
+	}
+	return processes, nil
+}
+
+// FindProcessesByExe returns processes from ps with Exe fields matching exe.
+func FindProcessesByExe(ps []Process, exe string) []Process {
+	var found []Process
+	for _, proc := range ps {
+		if proc.Exe == exe {
+			found = append(found, proc)
+		}
+	}
+	return found
+}
