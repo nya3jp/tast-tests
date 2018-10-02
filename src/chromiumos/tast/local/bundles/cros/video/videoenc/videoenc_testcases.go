a9de677083a36fd1cb83fcf69464ff60bce00d1e
Hirokazu Honda <hiroh@chromium.org>
2018-10-02 17:44:59 +0900

    video: add VideoencAccelH264I420 and VideoencAccelH264NV12 in Tast
    VideoencAccelH264I420/NV12 are video_VideoEncodeAccelerator.h264.bvt in autotest.
    It runs video_encode_accelerator_unittest preinstalled on test image, with
    small raw data, bear_320x192_40frames.yuv and bear_320x192_40frames.nv12.yuv.
    
    BUG=chromium:889496
    TEST=VideoencAccelH264I420 and VideoencAccelH264NV12
    
    Change-Id: If6941ba3b818f9087e765026d8c65f2dab25d456

---
+++ src/chromiumos/tast/local/bundles/cros/video/videoenc/videoenc_testcases.go
@@ -0,0 +1,25 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package videoenc
+
+import "chromiumos/tast/local/bundles/cros/video/lib/videotype"
+
+// BearI420 is the test parameters of video_encode_accelerator_unittest for "bear_320x192_40frames.yuv."
+var BearI420 StreamParam = StreamParam{
+	Name:    "bear_320x192_40frames.yuv",
+	Width:   320,
+	Height:  192,
+	Bitrate: 200000,
+	Format:  videotype.I420,
+}
+
+// BearNV12 is the test parameters of video_encode_accelerator_unittest for "bear_320x192_40frames.nv12.yuv."
+var BearNV12 StreamParam = StreamParam{
+	Name:    "bear_320x192_40frames.nv12.yuv",
+	Width:   320,
+	Height:  192,
+	Bitrate: 200000,
+	Format:  videotype.NV12,
+}
---
+++ src/chromiumos/tast/local/bundles/cros/video/videoenc_accel_h264_i420.go
@@ -0,0 +1,29 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
+	"chromiumos/tast/local/bundles/cros/video/videoenc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VideoencAccelH264I420,
+		Desc:         "Run Chrome video_encode_accelerator_unittest from I420 raw frames to H264 stream",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{caps.HWEncodeH264},
+		Data:         []string{videoenc.BearI420.Name},
+	})
+}
+
+// VideoencAccelH264I420 runs encode_accelerator_unittest to encode H264 encoding with I420 raw data, bear_320x192_40frames.yuv.
+func VideoencAccelH264I420(ctx context.Context, s *testing.State) {
+	videoenc.RunTest(ctx, s, videotype.H264, videoenc.BearI420)
+}
