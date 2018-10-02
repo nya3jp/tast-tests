2632c32af7785dc3e35a7c95bc56b61a89b30c9c
Hirokazu Honda <hiroh@chromium.org>
2018-10-02 17:41:37 +0900

    video: Add PlaybackPerfH264 to Tast
    video_PlaybackPerfH264 is video_PlaybackPerf.h264 in autotest.
    It measures SW/HW playback performance and reports the performance values on
    dashboard.
    This is the first step to migrate video_PlaybackPerf. It measures the number of
    dropped frames and the rate of dropped frames.
    
    BUG=chromium:890733
    TEST=video.PlaybackPerfH264 on veyron_minnie
    
    Change-Id: I331442d72d5bfe0a589d4e61a0f2fe5d4466c651

---
+++ src/chromiumos/tast/local/bundles/cros/video/lib/logging/log_helper.go
@@ -0,0 +1,100 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package logging controls logging in video drivers.
+package logging
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	"chromiumos/tast/errors"
+)
+
+// ChromeVmoduleFlag returns a command line option for Chrome (binary test) to
+// enable logging in media/gpu code.
+func ChromeVmoduleFlag() string {
+	loggingPatterns := []string{
+		"*/media/gpu/*video_decode_accelerator.cc=2",
+		"*/media/gpu/*video_encode_accelerator.cc=2",
+		"*/media/gpu/*jpeg_decode_accelerator.cc=2",
+		"*/media/gpu/*jpeg_encode_accelerator.cc=2",
+		"*/media/gpu/*image_processor.cc=2",
+		"*/media/gpu/*v4l2_device.cc=2",
+	}
+	return "--vmodule=" + strings.Join(loggingPatterns, ",")
+}
+
+// logSpec denotes the information to enable verbose logging in a video driver.
+type logSpec struct {
+	files        []string // files to write a value to to control verbose logging in a video driver.
+	enableValue  []byte   // data written to files to enable verbose logging.
+	disableValue []byte   // data written to files to disable verbose logging.
+	desc         string   // human-readable description of driver.
+}
+
+// getLogSpecs returns a list of logSpec which represents files and values to be written on DUT.
+func getLogSpecs() ([]logSpec, error) {
+	var specs []logSpec
+
+	for _, l := range []struct {
+		glob, enable, disable, desc string
+	}{
+		{"/sys/module/videobuf2_*/parameters/debug", "1", "0", "videobuf2"},
+		{"/sys/module/s5p_mfc/parameters/debug", "1", "0", "s5p_mfc"},
+		{"/sys/module/go2001/parameters/go2001_debug_level", "1", "0", "go2001"},
+
+		// The debug level is a bitfield, with 3 enabling log levels 0 and 1
+		{"/sys/module/rockchip_vpu/parameters/debug", "3", "0", "rk3399"},
+		{"/sys/module/rk3288_vpu/parameters/debug", "3", "0", "rk3288"},
+	} {
+		files, err := filepath.Glob(l.glob)
+		if err != nil {
+			return nil, errors.Wrapf(err, "bad glob %q", l.glob)
+		}
+		if len(files) > 0 {
+			specs = append(specs, logSpec{files, []byte(l.enable), []byte(l.disable), l.desc})
+		}
+	}
+	return specs, nil
+}
+
+// VideoLogger enables/disables verbose logging in a video driver properly during the test.
+type VideoLogger struct {
+	specs []logSpec
+}
+
+// NewVideoLogger enables verbose logging in a video driver and returns a VideoLogger.
+// VideoLogger.Close disables the verbose logging.
+func NewVideoLogger() (*VideoLogger, error) {
+	specs, err := getLogSpecs()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get log specs")
+	}
+	vl := VideoLogger{specs}
+
+	for _, l := range vl.specs {
+		for _, f := range l.files {
+			if err := ioutil.WriteFile(f, l.enableValue, 0644); err != nil {
+				vl.Close()
+				return nil, errors.Wrap(err, "failed to set log level")
+			}
+		}
+	}
+	return &vl, nil
+}
+
+// Close disables verbose logging.
+func (vl *VideoLogger) Close() error {
+	var lastErr error
+	for _, l := range vl.specs {
+		for _, f := range l.files {
+			if err := ioutil.WriteFile(f, l.disableValue, 0644); err != nil {
+				lastErr = errors.Wrap(err, "failed to set log level")
+			}
+		}
+	}
+	return lastErr
+}
---
+++ src/chromiumos/tast/local/bundles/cros/video/playback/playback.go
@@ -0,0 +1,276 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package playback provides common code for video.Playback* tests.
+package playback
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/video/lib/constants"
+	"chromiumos/tast/local/bundles/cros/video/lib/logging"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/testing"
+)
+
+// Identifier for a map of measured values with/without HW Acceleration stored during a test.
+type playbackType int
+
+const (
+	playbackWithHWAccel playbackType = iota
+	playbackWithoutHWAccel
+)
+
+type hwAccelState int
+
+const (
+	hwAccelDisabled hwAccelState = iota
+	hwAccelEnabled
+)
+
+type metricDesc string
+type metricValue float64
+
+const (
+	// Time to sleep while collecting data.
+	measurementDuration = 30 * time.Second
+
+	// Timeout to get idle CPU.
+	waitIdleCPUTimeout = 30 * time.Second
+
+	// The percentage of CPU usage when it is idle.
+	idleCPUUsagePercent = 1.0
+
+	// Description for measured values shown in dashboard.
+	// A video description (e.g. h264_1080p) is appended to them.
+	droppedFrameDesc        metricDesc = "video_dropped_frames_"
+	droppedFramePercentDesc metricDesc = "video_dropped_frames_percent_"
+
+	// Video Element in the page to play a video.
+	videoElement = "document.getElementsByTagName('video')[0]"
+)
+
+type collectedPerfData map[playbackType]map[metricDesc]metricValue
+type metricDef struct {
+	desc metricDesc
+	unit string
+	dir  perf.Direction
+}
+
+// TODO(crbug.com/890733): Add CPU usage.
+// metricDefs is a list of metric measured in this test.
+var metricDefs = []metricDef{
+	{droppedFrameDesc, "frames", perf.SmallerIsBetter},
+	{droppedFramePercentDesc, "percent", perf.SmallerIsBetter},
+}
+
+// RunTest measures dropped frames and dropped frames percent in playing a video with/without HW Acceleration.
+// The measured values are reported to a dashboard. videoDesc is a video description shown on the dashboard.
+func RunTest(ctx context.Context, s *testing.State, videoName, videoDesc string) {
+	vl, err := logging.NewVideoLogger()
+	if err != nil {
+		s.Fatal("Failed to set values for verbose logging")
+	}
+	defer vl.Close()
+
+	// TODO(crbug.com/890733): Check if video is downloaded correctly.
+	// The MD5 value of the video must match MD5 string included in videoName.
+
+	perfData := collectedPerfData{}
+	if err := measure(ctx, s.DataFileSystem(), videoName, perfData); err != nil {
+		s.Fatal("Failed to collect performance values: ", err)
+	}
+	s.Log("Measured metrics (dropped frames and percent): ", perfData)
+
+	// TODO(crbug.com/890733): Measure CPU usage.
+
+	if err := savePerfResults(ctx, perfData, videoDesc, s.OutDir()); err != nil {
+		s.Fatal("Failed to save perf data: ", perfData)
+	}
+}
+
+// measure collects video playback performance playing a video with SW decoder and
+// also with HW decoder if available.
+func measure(ctx context.Context, fileSystem http.FileSystem, videoName string, perfData collectedPerfData) error {
+	// Try Software playback.
+	if err := measureWithConfig(ctx, fileSystem, videoName, perfData, hwAccelDisabled); err != nil {
+		return err
+	}
+
+	// Try with Chrome's default settings. Even in this case, HW Acceleration may not be used, since a device doesn't
+	// have a capability to play the video with HW acceleration.
+	if err := measureWithConfig(ctx, fileSystem, videoName, perfData, hwAccelEnabled); err != nil {
+		return err
+	}
+	return nil
+}
+
+// measureWithConfig plays video one time and measures performance values.
+// The measured values are recorded in perfData.
+func measureWithConfig(ctx context.Context, fileSystem http.FileSystem, videoName string, perfData collectedPerfData, hwState hwAccelState) error {
+	chromeArgs := []string{logging.ChromeVmoduleFlag()}
+	if hwState == hwAccelDisabled {
+		chromeArgs = append(chromeArgs, "--disable-accelerated-video-decode")
+	}
+
+	cr, err := chrome.New(ctx, chrome.ExtraArgs(chromeArgs))
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to Chrome")
+	}
+	defer cr.Close(ctx)
+
+	// TODO(hiroh): Wait until CPU is idle state.
+
+	server := httptest.NewServer(http.FileServer(fileSystem))
+	defer server.Close()
+
+	initHistogram, err := metrics.GetHistogram(ctx, cr, constants.MediaGVDInitStatus)
+	if err != nil {
+		return errors.Wrap(err, "failed to get initial histogram")
+	}
+	testing.ContextLogf(ctx, "Initial %s histogram: %v", constants.MediaGVDInitStatus, initHistogram.Buckets)
+
+	conn, err := cr.NewConn(ctx, server.URL+"/"+videoName)
+	if err != nil {
+		return errors.Wrap(err, "failed to open video page")
+	}
+	defer conn.Close()
+
+	// Play a video repeatedly during measurement.
+	if err := conn.Exec(ctx, videoElement+".loop=true"); err != nil {
+		return errors.Wrap(err, "failed to settle video looping")
+	}
+
+	time.Sleep(measurementDuration)
+	vs, err := getDroppedFrames(ctx, conn)
+	if err != nil {
+		return errors.Wrap(err, "failed to gather performance values")
+	}
+
+	// Stop video.
+	if err := conn.Exec(ctx, videoElement+".pause()"); err != nil {
+		return errors.Wrap(err, "failed to stop video")
+	}
+
+	return recordMetrics(ctx, vs, perfData, cr, initHistogram, hwState)
+}
+
+// recordMetrics records the measured performance values in perfData.
+func recordMetrics(ctx context.Context, vs map[metricDesc]metricValue, perfData collectedPerfData, cr *chrome.Chrome, initHistogram *metrics.Histogram, hwState hwAccelState) error {
+	hwAccelUsed, err := wasHWAccelUsed(ctx, cr, initHistogram)
+	if err != nil {
+		return errors.Wrap(err, "failed to check for hardware acceleration")
+	}
+	if hwAccelUsed && hwState == hwAccelDisabled {
+		return errors.New("hardware acceleration used despite being disabled")
+	}
+	if !hwAccelUsed && hwState == hwAccelEnabled {
+		// Software playback performance is not recorded, unless HW Acceleration is disabled.
+		return nil
+	}
+
+	pType := playbackWithoutHWAccel
+	if hwAccelUsed {
+		pType = playbackWithHWAccel
+	}
+
+	if perfData[pType] == nil {
+		perfData[pType] = map[metricDesc]metricValue{}
+	}
+	for desc, value := range vs {
+		perfData[pType][desc] = value
+	}
+	return nil
+}
+
+// wasHWAccelUsed returns whether a video in cr played with HW acceleration.
+func wasHWAccelUsed(ctx context.Context, cr *chrome.Chrome, initHistogram *metrics.Histogram) (bool, error) {
+	// There are three valid cases.
+	// 1. No histogram is updated. This is the case HW Acceleration is disabled due to Chrome flag, --disable-accelerated-video-decode.
+	// 2. Histogram is updated with 15. This is the case Chrome tries to initailize VDA but it fails because the codec is not supported on DUT.
+	// 3. Histogram is updated with 0. This is the case Chrome sucessfully initializes VDA.
+
+	// err is not nil here if HW Acceleration is disabled and then Chrome doesn't try VDA initialization at all.
+	// For the case 1, we pass a short time context to WaitForHistogramUpdate to avoid the whole test context (ctx) from reaching deadline.
+	histogramDiff, err := metrics.WaitForHistogramUpdate(ctx, cr, constants.MediaGVDInitStatus, initHistogram, 5*time.Second)
+	if err != nil {
+		// This is the first case; no histogram is updated.
+		return false, nil
+	}
+
+	testing.ContextLogf(ctx, "Got update to %s histogram: %v", constants.MediaGVDInitStatus, histogramDiff.Buckets)
+	if len(histogramDiff.Buckets) > 1 {
+		return false, errors.Wrapf(err, "unexpected histogram update: %v", histogramDiff)
+	}
+
+	// If HW acceleration is used, the sole bucket is {0, 1, X}.
+	diff := histogramDiff.Buckets[0]
+	hwAccelUsed := diff.Min == constants.MediaGVDInitSuccess && diff.Max == constants.MediaGVDInitSuccess+1
+	return hwAccelUsed, nil
+}
+
+// savePerfResults saves performance results in outDir.
+func savePerfResults(ctx context.Context, perfData collectedPerfData, videoDesc, outDir string) error {
+	// TODO(hiroh): Remove tastPrefix after removing video_PlaybackPerf in autotest.
+	p := &perf.Values{}
+	const tastPrefix = "tast_"
+	for _, pType := range []playbackType{playbackWithHWAccel, playbackWithoutHWAccel} {
+		keyval, found := perfData[pType]
+		if !found {
+			if pType == playbackWithHWAccel {
+				testing.ContextLog(ctx, "No HW playback performance result")
+				continue
+			} else {
+				// SW playback performance results should be collected in any cases.
+				return errors.Errorf("no SW playback performance result: %v", perfData)
+			}
+		}
+		perfPrefix := tastPrefix
+		if pType == playbackWithHWAccel {
+			perfPrefix += "hw_"
+		} else {
+			perfPrefix += "sw_"
+		}
+		for _, m := range metricDefs {
+			val, found := keyval[m.desc]
+			perfName := perfPrefix + string(m.desc) + videoDesc
+			if !found {
+				return errors.Errorf("no performance result for %s: %v", perfName, perfData)
+			}
+			p.Set(perf.Metric{Name: perfName, Unit: m.unit, Direction: m.dir}, float64(val))
+		}
+	}
+	return p.Save(outDir)
+}
+
+// getDroppedFrames obtains the number of decoded frames and dropped frames by JavaScript,
+// and returns the number of dropped frames and the rate of dropped frames.
+func getDroppedFrames(ctx context.Context, conn *chrome.Conn) (map[metricDesc]metricValue, error) {
+
+	var decodedFrameCount, droppedFrameCount int64
+	if err := conn.Eval(ctx, videoElement+".webkitDecodedFrameCount", &decodedFrameCount); err != nil {
+		return nil, errors.Wrap(err, "failed to get number of decoded frames")
+	}
+	if err := conn.Eval(ctx, videoElement+".webkitDroppedFrameCount", &droppedFrameCount); err != nil {
+		return nil, errors.Wrap(err, "failed to get number of dropped frames")
+	}
+
+	var droppedFramePercent float64
+	if decodedFrameCount != 0 {
+		droppedFramePercent = 100.0 * float64(droppedFrameCount) / float64(decodedFrameCount)
+	} else {
+		testing.ContextLogf(ctx, "No decoded frames; setting dropped percent to 100")
+		droppedFramePercent = 100.0
+	}
+	return map[metricDesc]metricValue{
+		droppedFrameDesc:        metricValue(droppedFrameCount),
+		droppedFramePercentDesc: metricValue(droppedFramePercent),
+	}, nil
+}
