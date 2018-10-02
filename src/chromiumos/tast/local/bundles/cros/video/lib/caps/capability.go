cbdf7604d2a50d7feeb5f50bacc453614abe5e21
Hirokazu Honda <hiroh@chromium.org>
2018-10-02 17:41:37 +0900

    video: Add PlaybackPerfH264 to Tast
    video_PlaybackPerfH264 is video_PlaybackPerf.h264 in autotest.
    It measures SW/HW playback performance and reports the performance values on
    dashboard.
    
    BUG=chromium:890733
    TEST=video.PlaybackPerfH264
    
    Change-Id: I331442d72d5bfe0a589d4e61a0f2fe5d4466c651

---
+++ src/chromiumos/tast/local/bundles/cros/video/lib/caps/capability.go
@@ -0,0 +1,48 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package caps is a package for capabilities used in autotest-capability.
+package caps
+
+// These are constant strings for capabilities in autotest-capability.
+// See the below link for detail.
+// https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/master/chromeos-base/autotest-capability-default/.
+const (
+	capPrefix = "autotest-capability:"
+
+	// Video Decoding
+	HWDecodeH264      = capPrefix + "hw_dec_h264_1080_30"
+	HWDecodeH264_60   = capPrefix + "hw_dec_h264_1080_60"
+	HWDecodeH264_4K   = capPrefix + "hw_dec_h264_2160_30"
+	HWDecodeH264_4K60 = capPrefix + "hw_dec_h264_2160_60"
+
+	HWDecodeVP8      = capPrefix + "hw_dec_vp8_1080_30"
+	HWDecodeVP8_60   = capPrefix + "hw_dec_vp8_1080_60"
+	HWDecodeVP8_4K   = capPrefix + "hw_dec_vp8_2160_30"
+	HWDecodeVP8_4K60 = capPrefix + "hw_dec_vp8_2160_60"
+
+	HWDecodeVP9      = capPrefix + "hw_dec_vp9_1080_30"
+	HWDecodeVP9_60   = capPrefix + "hw_dec_vp9_1080_60"
+	HWDecodeVP9_4K   = capPrefix + "hw_dec_vp9_2160_30"
+	HWDecodeVP9_4K60 = capPrefix + "hw_dec_vp9_2160_60"
+
+	HWDecodeVP9_2      = capPrefix + "hw_dec_vp9-2_1080_30"
+	HWDecodeVP9_2_60   = capPrefix + "hw_dec_vp9-2_1080_60"
+	HWDecodeVP9_2_4K   = capPrefix + "hw_dec_vp9-2_2160_30"
+	HWDecodeVP9_2_4K60 = capPrefix + "hw_dec_vp9-2_2160_60"
+
+	// JPEG Decoding
+	HWDecodeJPEG = capPrefix + "hw_dec_jpeg"
+
+	// Video Encoding
+	HWEncodeH264 = capPrefix + "hw_enc_h264_1080_30"
+	HWEncodeVP8  = capPrefix + "hw_enc_vp8_1080_30"
+	HWEncodeVP9  = capPrefix + "hw_enc_vp9_1080_30"
+
+	// JPEG Encoding
+	HWEncodeJPEG = capPrefix + "hw_enc_jpeg"
+
+	// Camera
+	USBCamera = capPrefix + "usb_camera"
+)
