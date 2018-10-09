8b4e371f460260fda51117ea487e49908b6f773d
Qijiang Fan <fqj@chromium.org>
2018-10-09 18:05:49 +0900

    tast-tests: add more test cases for by-exe process domain check.
    BUG=b:114172200
    TEST=tast run DUT security.SELinux*
    
    Change-Id: I7518e8c73bbeb2a71095bbdf076a72b5e94350bc
    Reviewed-on: https://chromium-review.googlesource.com/1270422
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Qijiang Fan <fqj@google.com>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/security/selinux_process_context.go
+++ src/chromiumos/tast/local/bundles/cros/security/selinux_process_context.go
@@ -38,14 +38,30 @@ func SELinuxProcessContext(ctx context.Context, s *testing.State) {
 		exe searchType = iota
 	)
 	for _, testCase := range []struct {
-		query   string
 		field   searchType
+		query   string
 		context string
 	}{
-		{"/sbin/init", exe, "u:r:cros_init:s0"},
-		{"/sbin/udevd", exe, "u:r:cros_udevd:s0"},
-		{"/usr/bin/anomaly_collector", exe, "u:r:cros_anomaly_collector:s0"},
-		{"/usr/sbin/rsyslogd", exe, "u:r:cros_rsyslogd:s0"},
+		{exe, "/sbin/debugd", "u:r:cros_debugd:s0"},
+		{exe, "/sbin/init", "u:r:cros_init:s0"},
+		{exe, "/sbin/session_manager", "u:r:cros_session_manager:s0"},
+		{exe, "/sbin/udevd", "u:r:cros_udevd:s0"},
+		{exe, "/sbin/upstart-socket-bridge", "u:r:cros_upstart_socket_bridge:s0"},
+		{exe, "/usr/bin/anomaly_collector", "u:r:cros_anomaly_collector:s0"},
+		{exe, "/usr/bin/dbus-daemon", "u:r:cros_dbus_daemon:s0"},
+		{exe, "/usr/bin/memd", "u:r:cros_memd:s0"},
+		{exe, "/usr/bin/metrics_daemon", "u:r:cros_metrics_daemon:s0"},
+		{exe, "/usr/bin/powerd", "u:r:cros_powerd:s0"},
+		{exe, "/usr/sbin/ModemManager", "u:r:cros_modem_manager:s0"},
+		{exe, "/usr/sbin/avahi-daemon", "u:r:cros_avahi_daemon:s0"},
+		{exe, "/usr/sbin/chapsd", "u:r:cros_chapsd:s0"},
+		{exe, "/usr/sbin/conntrackd", "u:r:cros_conntrackd:s0"},
+		{exe, "/usr/sbin/cryptohomed", "u:r:cros_cryptohomed:s0"},
+		{exe, "/usr/sbin/rsyslogd", "u:r:cros_rsyslogd:s0"},
+		{exe, "/usr/sbin/wpa_supplicant", "u:r:wpa_supplicant:s0"},
+		{exe, "/usr/sbin/update_engine", "u:r:cros_update_engine:s0"},
+		{exe, "/usr/bin/tlsdated", "u:r:cros_tlsdated:s0"},
+		{exe, "/usr/bin/cras", "u:r:cros_cras:s0"},
 	} {
 		var p []selinux.Process
 		switch testCase.field {
