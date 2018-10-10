bf7c846b786e82ff96d9d0a25a6613218480c4f7
Keiichi Watanabe <keiichiw@chromium.org>
2018-10-10 20:34:53 +0900

    tast-tests: Check and store perf metrics in video.WebRTC tests
    Add checking for frame statics and make tests fail if there were too many frames during video capturing.
    In addition, video.WebRTC*Perf will upload the statistics as perf metrics.
    
    BUG=chromium:852302, chromium:882433
    TEST=run on amd64-generic VM and kevin
    
    Change-Id: I46051e1c2807ad97b0b90f364cfd0d76cf941033
    Signed-off-by: Keiichi Watanabe <keiichiw@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/webrtc_camera_perf.go
+++ src/chromiumos/tast/local/bundles/cros/video/webrtc_camera_perf.go
@@ -8,6 +8,7 @@ import (
 	"context"
 
 	"chromiumos/tast/local/bundles/cros/video/webrtc"
+	"chromiumos/tast/local/perf"
 	"chromiumos/tast/testing"
 )
 
@@ -22,16 +23,34 @@ func init() {
 }
 
 // WebRTCCameraPerf is the full version of WebRTCCamera.
-// It renders the camera's media stream in VGA and 720p (if supported) for
-// 20 seconds.
+// It renders the camera's media stream in VGA and 720p for 20 seconds.
+// If there is no error while exercising the camera, it uploads statistics of
+// black/frozen frames.
+// This test will fail when an error occurs or too many frames are broken.
 //
 // This test uses the real webcam unless it is running under QEMU. Under QEMU,
 // it uses "vivid" instead, which is the virtual video test driver and can be
 // used as an external USB camera.
-//
-// TODO(keiichiw): When adding perf metrics, add comments here.
 func WebRTCCameraPerf(ctx context.Context, s *testing.State) {
 	// Run tests for 20 seconds per resolution.
-	webrtc.RunTest(ctx, s, "getusermedia.html", "testNextResolution(20)")
-	// TODO(keiichiw): Add perf metrics.
+	results := webrtc.RunWebRTCCamera(ctx, s, 20)
+
+	// Set and upload frame statistics below.
+	p := &perf.Values{}
+
+	for _, result := range results {
+		if len(result.Errors) != 0 {
+			for _, msg := range result.Errors {
+				s.Errorf("Error occured for %dx%d: %s",
+					result.Width, result.Height, msg)
+			}
+			continue
+		}
+
+		result.SetPerf(p)
+	}
+
+	if err := p.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
 }
--- src/chromiumos/tast/local/bundles/cros/video/webrtc_peer_connection_with_camera_h264_perf.go
+++ src/chromiumos/tast/local/bundles/cros/video/webrtc_peer_connection_with_camera_h264_perf.go
@@ -8,6 +8,7 @@ import (
 	"context"
 
 	"chromiumos/tast/local/bundles/cros/video/webrtc"
+	"chromiumos/tast/local/perf"
 	"chromiumos/tast/testing"
 )
 
@@ -21,17 +22,24 @@ func init() {
 	})
 }
 
-// WebRTCPeerConnectionWithCameraH264Perf is a long version of
+// WebRTCPeerConnectionWithCameraH264Perf is the full version of
 // video.WebRTCPeerConnectionWithCameraH264.
 // This test performs a WebRTC loopback call for 20 seconds.
+// If there is no error while exercising the camera, it uploads statistics of
+// black/frozen frames and input/output FPS will be logged.
 //
 // This test uses the real webcam unless it is running under QEMU. Under QEMU,
 // it uses "vivid" instead, which is the virtual video test driver and can be
 // used as an external USB camera.
-//
-// TODO(keiichiw): When adding perf metrics, add comments.
 func WebRTCPeerConnectionWithCameraH264Perf(ctx context.Context, s *testing.State) {
+	const codec = "H264"
 	// Run loopback call for 20 seconds.
-	webrtc.RunTest(ctx, s, "loopback.html", "testWebRtcLoopbackCall('H264', 20)")
-	// TODO(keiichiw): Add perf metrics.
+	result := webrtc.RunWebRTCPeerConnectionWithCamera(ctx, s, codec, 20)
+
+	// Set and upload perf metrics below.
+	p := &perf.Values{}
+	result.SetPerf(p, codec)
+	if err := p.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
 }
