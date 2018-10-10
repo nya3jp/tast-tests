8fc4cde00326069a411f7297da372693013463b7
Keiichi Watanabe <keiichiw@chromium.org>
2018-10-10 20:34:53 +0900

    tast-tests: Check and store perf metrics in video.WebRTC tests
    Add checking for frame statics and make tests fail if there were too many frames during video capturing.
    In addition, video.WebRTC*Perf will upload the statistics as perf metrics.
    
    BUG=chromium:852302, chromium:882433
    TEST=run on amd64-generic VM and kevin
    
    Change-Id: I46051e1c2807ad97b0b90f364cfd0d76cf941033
    Signed-off-by: Keiichi Watanabe <keiichiw@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/webrtc_peer_connection_with_camera_vp8.go
+++ src/chromiumos/tast/local/bundles/cros/video/webrtc_peer_connection_with_camera_vp8.go
@@ -6,7 +6,9 @@ package video
 
 import (
 	"context"
+	"time"
 
+	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
 	"chromiumos/tast/local/bundles/cros/video/webrtc"
 	"chromiumos/tast/testing"
 )
@@ -29,12 +31,12 @@ func init() {
 //
 // If this test shows black frames and video.WebRTCCamera does not, it could
 // mean VP8 video isn't encoded/decoded right on this device but that the
-// camera works. Finally, input and output FPS are logged.
+// camera works.
 //
 // This test uses the real webcam unless it is running under QEMU. Under QEMU,
 // it uses "vivid" instead, which is the virtual video test driver and can be
 // used as an external USB camera.
 func WebRTCPeerConnectionWithCameraVP8(ctx context.Context, s *testing.State) {
 	// Run loopback call for 3 seconds.
-	webrtc.RunTest(ctx, s, "loopback.html", "testWebRtcLoopbackCall('VP8', 3)")
+	webrtc.RunWebRTCPeerConnectionWithCamera(ctx, s, videotype.VP8, 3*time.Second)
 }
