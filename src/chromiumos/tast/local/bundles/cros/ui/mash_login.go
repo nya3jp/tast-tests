a255b84479e4b90beab075bbf7dbf67dccca4615
James Cook <jamescook@chromium.org>
2018-10-15 12:52:09 -0700

    tast-tests: Don't parse command line in ui.MashLogin
    This avoids flaky failures due to command line arguments sometimes
    being truncated by Chrome's implementation of setproctitle().
    
    BUG=chromium:891470
    TEST=Tast ui.MashLogin
    
    Change-Id: I911961ea09a4a8c48db70c1d25d8f6449948f26f

--- src/chromiumos/tast/local/bundles/cros/ui/mash_login.go
+++ src/chromiumos/tast/local/bundles/cros/ui/mash_login.go
@@ -6,20 +6,9 @@ package ui
 
 import (
 	"context"
-	"strings"
-
-	"github.com/shirou/gopsutil/process"
 
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
-)
-
-const (
-	// Switch that identifies the ash mojo service. Keep in sync with chromium
-	// switches::kMashServiceName in src/chrome/common/chrome_switches.cc
-	// TODO(crbug.com/891470): Sometimes the chrome command line has truncated
-	// arguments. Change this to "mash-service-name" when that is fixed.
-	mashServiceName = "mash-s"
 )
 
 func init() {
@@ -39,30 +28,7 @@ func MashLogin(ctx context.Context, s *testing.State) {
 	}
 	defer cr.Close(ctx)
 
-	pids, err := chrome.GetPIDs()
-	if err != nil {
-		s.Fatal("Could not get chrome PIDs: ", err)
-	}
-
-	found := false
-	cmds := make([]string, 0)
-	for _, pid := range pids {
-		// If we see errors, assume the process exited.
-		proc, err := process.NewProcess(int32(pid))
-		if err != nil {
-			continue
-		}
-		cmd, err := proc.Cmdline()
-		if err != nil {
-			continue
-		}
-		if strings.Contains(cmd, mashServiceName) {
-			found = true
-			break
-		}
-		cmds = append(cmds, cmd)
-	}
-	if !found {
-		s.Errorf("No chrome process containing %q among %v", mashServiceName, cmds)
-	}
+	// TODO(jamescook): Check that a mash process is running. The test used to
+	// do this (see git history) but we had to stop due to flake from chrome's
+	// command line sometimes being truncated. https://crbug.com/891470
 }
