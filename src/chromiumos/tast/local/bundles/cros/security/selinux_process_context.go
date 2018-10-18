c86710d05561f566137addf5127c040edad3bdc3
Qijiang Fan <fqj@chromium.org>
2018-10-18 14:26:54 +0900

    tast-tests: allow parameterlized optional process for selinux check.
    BUG=b:114172200
    TEST=tast run DUT security.SELinux*
    
    Change-Id: Ied97a8489d817ae04d1ed63a09365a41f2613757

--- src/chromiumos/tast/local/bundles/cros/security/selinux_process_context.go
+++ src/chromiumos/tast/local/bundles/cros/security/selinux_process_context.go
@@ -40,32 +40,38 @@ func SELinuxProcessContext(ctx context.Context, s *testing.State) {
 		exe     searchType = iota // absolute executable path
 		cmdline                   // partial regular expression matched against command line
 	)
+	const (
+		OPTIONAL int = 0
+		REQUIRED = 1
+		MULTIPLE = 2
+	)
 	for _, testCase := range []struct {
 		field   searchType
 		query   string
 		context string
+		minProcessCount int
 	}{
-		{cmdline, "/usr/bin/periodic_scheduler", "u:r:cros_periodic_scheduler:s0"},
-		{exe, "/sbin/debugd", "u:r:cros_debugd:s0"},
-		{exe, "/sbin/init", "u:r:cros_init:s0"},
-		{exe, "/sbin/session_manager", "u:r:cros_session_manager:s0"},
-		{exe, "/sbin/udevd", "u:r:cros_udevd:s0"},
-		{exe, "/sbin/upstart-socket-bridge", "u:r:cros_upstart_socket_bridge:s0"},
-		{exe, "/usr/bin/anomaly_collector", "u:r:cros_anomaly_collector:s0"},
-		{exe, "/usr/bin/cras", "u:r:cros_cras:s0"},
-		{exe, "/usr/bin/dbus-daemon", "u:r:cros_dbus_daemon:s0"},
-		{exe, "/usr/bin/memd", "u:r:cros_memd:s0"},
-		{exe, "/usr/bin/metrics_daemon", "u:r:cros_metrics_daemon:s0"},
-		{exe, "/usr/bin/powerd", "u:r:cros_powerd:s0"},
-		{exe, "/usr/bin/tlsdated", "u:r:cros_tlsdated:s0"},
-		{exe, "/usr/sbin/ModemManager", "u:r:cros_modem_manager:s0"},
-		{exe, "/usr/sbin/avahi-daemon", "u:r:cros_avahi_daemon:s0"},
-		{exe, "/usr/sbin/chapsd", "u:r:cros_chapsd:s0"},
-		{exe, "/usr/sbin/conntrackd", "u:r:cros_conntrackd:s0"},
-		{exe, "/usr/sbin/cryptohomed", "u:r:cros_cryptohomed:s0"},
-		{exe, "/usr/sbin/rsyslogd", "u:r:cros_rsyslogd:s0"},
-		{exe, "/usr/sbin/update_engine", "u:r:cros_update_engine:s0"},
-		{exe, "/usr/sbin/wpa_supplicant", "u:r:wpa_supplicant:s0"},
+		{cmdline, "/usr/bin/periodic_scheduler", "u:r:cros_periodic_scheduler:s0", MULTIPLE},
+		{exe, "/sbin/debugd", "u:r:cros_debugd:s0", REQUIRED},
+		{exe, "/sbin/init", "u:r:cros_init:s0", REQUIRED},
+		{exe, "/sbin/session_manager", "u:r:cros_session_manager:s0", REQUIRED},
+		{exe, "/sbin/udevd", "u:r:cros_udevd:s0", REQUIRED},
+		{exe, "/sbin/upstart-socket-bridge", "u:r:cros_upstart_socket_bridge:s0", REQUIRED},
+		{exe, "/usr/bin/anomaly_collector", "u:r:cros_anomaly_collector:s0", REQUIRED},
+		{exe, "/usr/bin/cras", "u:r:cros_cras:s0", REQUIRED},
+		{exe, "/usr/bin/dbus-daemon", "u:r:cros_dbus_daemon:s0", REQUIRED},
+		{exe, "/usr/bin/memd", "u:r:cros_memd:s0", OPTIONAL},
+		{exe, "/usr/bin/metrics_daemon", "u:r:cros_metrics_daemon:s0", REQUIRED},
+		{exe, "/usr/bin/powerd", "u:r:cros_powerd:s0", REQUIRED},
+		{exe, "/usr/bin/tlsdated", "u:r:cros_tlsdated:s0", REQUIRED},
+		{exe, "/usr/sbin/ModemManager", "u:r:cros_modem_manager:s0", REQUIRED},
+		{exe, "/usr/sbin/avahi-daemon", "u:r:cros_avahi_daemon:s0", REQUIRED},
+		{exe, "/usr/sbin/chapsd", "u:r:cros_chapsd:s0", REQUIRED},
+		{exe, "/usr/sbin/conntrackd", "u:r:cros_conntrackd:s0", REQUIRED},
+		{exe, "/usr/sbin/cryptohomed", "u:r:cros_cryptohomed:s0", REQUIRED},
+		{exe, "/usr/sbin/rsyslogd", "u:r:cros_rsyslogd:s0", REQUIRED},
+		{exe, "/usr/sbin/update_engine", "u:r:cros_update_engine:s0", REQUIRED},
+		{exe, "/usr/sbin/wpa_supplicant", "u:r:wpa_supplicant:s0", REQUIRED},
 	} {
 		var p []selinux.Process
 		var err error
@@ -82,6 +88,12 @@ func SELinuxProcessContext(ctx context.Context, s *testing.State) {
 			continue
 		}
 		s.Logf("Found %d process(es) for test case %v: %v", len(p), testCase, p)
+		if len(p) < testCase.minProcessCount {
+			s.Errorf(
+				"Found %d processes for testcase %v, require at least %d processes",
+				len(p), testCase, testCase.minProcessCount)
+		}
+		// Also checks the context even number of processes is not enough.
 		assertContext(p, testCase.context)
 	}
 }
