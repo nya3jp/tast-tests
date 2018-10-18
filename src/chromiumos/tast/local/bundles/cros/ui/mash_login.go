a97923dd8617ddeccc462c3d4a77a98a63f7fe03
Daniel Erat <derat@chromium.org>
2018-10-17 21:40:05 -0700

    tast-tests: Make ui.MashLogin important.
    Similar to what was done earlier with
    ui.SingleProcessMashLogin, remove the "informational"
    attribute from the ui.MashLogin test so it will start
    running on the CrOS and Chrome CQs and the Chrome PFQ.
    
    This test has been consistently passing on -release builders
    since it was updated to not check Chrome command lines.
    
    BUG=chromium:847901
    TEST=none
    
    Change-Id: I4240dfb7beeee1ec51652af865b86e19e0f96b43
    Reviewed-on: https://chromium-review.googlesource.com/1287810
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/mash_login.go
+++ src/chromiumos/tast/local/bundles/cros/ui/mash_login.go
@@ -15,7 +15,6 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         MashLogin,
 		Desc:         "Checks that chrome --enable-features=Mash starts",
-		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"chrome_login"},
 	})
 }
