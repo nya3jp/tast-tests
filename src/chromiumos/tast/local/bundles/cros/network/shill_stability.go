e0147234369511c46ec1fb2ffed206ccb46130b0
Brian Norris <briannorris@chromium.org>
2018-10-23 11:36:03 -0700

    tast-tests: Port network_ShillStability
    I shortened the timeout and interval, since a really-respawning shill
    would actually happen more frequently than 10 seconds. And even with a
    10-second respawn, 30 seconds should be enough to catch several
    restarts.
    
    Some of getPID() was adapted from
    src/chromiumos/tast/local/chrome/proc.go.
    
    BUG=chromium:871645
    TEST=`tast -verbose run $HOST network.ShillStability`, while:
     (a) running `restart shill` (FAIL);
     (b) starting up an extra shill process (FAIL); or
     (c) leaving things alone (PASS)
    
    Change-Id: I0100eb3f542af1e235444e09857780851fddc778
    Signed-off-by: Brian Norris <briannorris@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/network/shill_stability.go
@@ -0,0 +1,71 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"github.com/shirou/gopsutil/process"
+
+	"chromiumos/tast/testing"
+)
+
+// GetPID returns PID of shill. Raises an error if we don't find exactly 1
+// shill process.
+func getPID(s *testing.State) int {
+	const (
+		shillExecPath = "/usr/bin/shill"
+	)
+
+	all, err := process.Processes()
+	if err != nil {
+		s.Fatal(err)
+	}
+
+	pids := make([]int, 0)
+	for _, proc := range all {
+		if exe, err := proc.Exe(); err == nil && exe == shillExecPath {
+			pids = append(pids, int(proc.Pid))
+		}
+	}
+	if len(pids) != 1 {
+		s.Fatal("Unexpected number of shill processes: ", pids)
+	}
+	return pids[0]
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ShillStability,
+		Desc: "Checks that shill isn't respawning",
+		Attr: []string{"informational"},
+	})
+}
+
+func ShillStability(ctx context.Context, s *testing.State) {
+	const (
+		stabilityIntervalSeconds = 6
+		stabilitySeconds         = 30
+	)
+
+	pid := getPID(s)
+
+	ctx, cancel := context.WithTimeout(ctx, stabilitySeconds*time.Second)
+	defer cancel()
+
+	for {
+		select {
+		case <-time.After(stabilityIntervalSeconds * time.Second):
+			if latest := getPID(s); latest != pid {
+				s.Fatalf("PID changed: %v -> %v", pid, latest)
+			}
+			s.Log("PID stable at: ", pid)
+		case <-ctx.Done():
+			// We reached the time limit without failures.
+			return
+		}
+	}
+}
