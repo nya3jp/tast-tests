fcaec3927cce282003157bd3a45d6f57c4fe6a3e
Cheng-Yu Lee <cylee@chromium.org>
2018-10-27 20:12:19 +0800

    tast-tests: Add Crostini disk IO performance test.
    BUG=chromium:897240
    TEST=tast --verbose run DUT vm.CrostiniPerf
    
    Change-Id: Ib3b1e47dbb367398874ad8ddd4d5113dd00506a7

---
+++ src/chromiumos/tast/local/bundles/cros/vm/crostini_disk_io_perf.go
@@ -0,0 +1,291 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package vm
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/vm"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	var dataFiles []string
+	for _, job := range fioJobs {
+		dataFiles = append(dataFiles, job.fileName)
+	}
+
+	testing.AddTest(&testing.Test{
+		Func: CrostiniDiskIOPerf,
+		Desc: "Tests Crostini Disk IO Performance",
+		// TODO(cylee): A presubmit check enforce "informational". Confirm if we should remove the checking.
+		Attr:         []string{"informational", "group:crosbolt", "crosbolt_nightly"},
+		Data:         dataFiles,
+		Timeout:      30 * time.Minute,
+		SoftwareDeps: []string{"chrome_login", "vm_host"},
+	})
+}
+
+// Defines whether to report write and/or read bandwidth for each fioJob.
+type reportFlags int
+
+const (
+	reportRead reportFlags = 1 << iota
+	reportWrite
+)
+
+type fioJob struct {
+	fileName, name string
+	reportFlag     reportFlags
+}
+
+var (
+	fioJobs = []fioJob{
+		{"disk_io_perf_fio_seq_write.ini", "seq_write", reportWrite},
+		{"disk_io_perf_fio_seq_read.ini", "seq_read", reportRead},
+		{"disk_io_perf_fio_rand_write.ini", "rand_write", reportWrite},
+		{"disk_io_perf_fio_rand_read.ini", "rand_read", reportRead},
+		{"disk_io_perf_fio_stress_rw.ini", "stress_rw", reportWrite | reportRead},
+	}
+)
+
+// Describes either a container(guest) environment or native(host) environment on which a performance test is executed.
+type runEnv struct {
+	// Returns the file name the disk I/O test is performed on.
+	testDataPath string
+	// Returns the absolute path of the fio jobfile name.
+	jobFilePath func(jobFile string) string
+	// Returns the full fio command to execute.
+	fioCmd func(ctx context.Context, jobFile string, envArgs []string, args []string) *testexec.Cmd
+}
+
+type fioSettings struct {
+	fileSize, blockSize string // fio size description, e.g. "1G" or "4m"
+	loopNum             int
+}
+
+// runFIO runs a fio command and returns the average read/write bandwidth in kB per second.
+// |jobFile| is the .ini file to be used by the fio command. The job file may contain variables which
+// can be substituted by environment variables passed to fio command. For example, if the .ini file has lines like:
+//
+//   [fio_rand_write]
+//   filename=${FILENAME}
+//   size=${FILESIZE}
+//   bs=${BLOCKSIZE}
+//   ...
+//
+// An example fio command could be like
+//     FILESIZE=1G FILENAME=fio_test_data BLOCKSIZE=4m fio fio_seq_write --output-format=json
+// |setting| are fio parameters to be passed via the environment variables.
+func runFIO(ctx context.Context, re runEnv, jobFile string, setting fioSettings) (avgRead, avgWrite float64, err error) {
+	envArgs := []string{
+		"FILENAME=" + re.testDataPath,
+		"FILESIZE=" + setting.fileSize,
+		"BLOCKSIZE=" + setting.blockSize,
+		fmt.Sprintf("LOOPNUM=%v", setting.loopNum),
+	}
+
+	extraArgs := []string{"--output-format=json", "--end_fsync=1"}
+
+	cmd := re.fioCmd(ctx, re.jobFilePath(jobFile), envArgs, extraArgs)
+	out, err := cmd.Output()
+	if err != nil {
+		cmd.DumpLog(ctx)
+		// Only append the first line of the output to the error.
+		if idx := bytes.IndexByte(out, '\n'); idx != -1 {
+			out = out[:idx]
+		}
+		return 0, 0, errors.Wrap(err, "failed to run fio: "+string(out))
+	}
+
+	var result struct {
+		Jobs []struct {
+			Read, Write struct {
+				Bw float64
+			}
+		}
+	}
+	if err := json.Unmarshal(out, &result); err != nil {
+		return 0, 0, errors.Wrap(err, "failed to parse fio result")
+	}
+
+	var totalRead, totalWrite float64
+	for _, job := range result.Jobs {
+		totalRead += job.Read.Bw
+		totalWrite += job.Write.Bw
+	}
+	if numJobs := len(result.Jobs); numJobs > 0 {
+		avgRead = totalRead / float64(numJobs)
+		avgWrite = totalWrite / float64(numJobs)
+	}
+
+	return avgRead, avgWrite, nil
+}
+
+// reportMetric given the metric name |metricName| and perf numbers of the same configuration running in the container as |guestValue|
+// and on the native host machine as |hostValue|, reports three metrics:
+// - guest_|metricName| : The perf value in the container.
+// - host_|metricName| : The perf value on the host machine.
+// - ratio_|metricName| : The ratio of |guestValue| divided by |hostValue|.
+func reportMetric(ctx context.Context, metricName string, guestValue, hostValue float64, perfValues perf.Values) {
+	ratio := guestValue / hostValue
+	testing.ContextLogf(ctx, "Reporting metric %.2f: %.2f %.2f %.2f", metricName, guestValue, hostValue, ratio)
+	perfValues.Append(perf.Metric{
+		Name:      "crosini_disk_io",
+		Variant:   "guest_" + metricName,
+		Unit:      "KB_per_sec",
+		Direction: perf.BiggerIsBetter,
+		Multiple:  true,
+	}, guestValue)
+
+	perfValues.Append(perf.Metric{
+		Name:      "crosini_disk_io",
+		Variant:   "host_" + metricName,
+		Unit:      "KB_per_sec",
+		Direction: perf.BiggerIsBetter,
+		Multiple:  true,
+	}, hostValue)
+
+	perfValues.Append(perf.Metric{
+		Name:      "crosini_disk_io",
+		Variant:   "ratio_" + metricName,
+		Unit:      "percentage",
+		Direction: perf.BiggerIsBetter,
+		Multiple:  true,
+	}, ratio)
+}
+
+func runFIOJob(ctx context.Context, guestEnv, hostEnv runEnv, job fioJob, perfValues perf.Values) {
+	testing.ContextLog(ctx, "Running job ", job.name)
+
+	// Delete the test data file on host.
+	defer os.RemoveAll(hostEnv.testDataPath)
+
+	for _, setting := range []fioSettings{
+		{"300m", "4k", 1},
+		{"500m", "64k", 1},
+		{"500m", "1m", 2},
+		{"500m", "16m", 2},
+	} {
+		baseMetricName := fmt.Sprintf("%v_bs_%v", job.name, setting.blockSize)
+
+		const numTries = 3
+
+		for i := 1; i <= numTries; i++ {
+			testing.ContextLogf(ctx, "Running %v with block size %v in container (%v/%v)", job.name, setting.blockSize, i, numTries)
+			guestRead, guestWrite, err := runFIO(ctx, guestEnv, job.fileName, setting)
+			if err != nil {
+				testing.ContextLogf(ctx, "%v with block size %v failed: %v", job.name, setting.blockSize, err)
+				continue
+			}
+
+			testing.ContextLogf(ctx, "Running %v with bs %v on host (%v/%v)", job.name, setting.blockSize, i, numTries)
+			hostRead, hostWrite, err := runFIO(ctx, hostEnv, job.fileName, setting)
+			if err != nil {
+				testing.ContextLogf(ctx, "%v with block size %v failed: %v", job.name, setting.blockSize, err)
+				continue
+			}
+
+			if job.reportFlag&reportWrite != 0 {
+				reportMetric(ctx, baseMetricName+"_write", guestWrite, hostWrite, perfValues)
+			}
+			if job.reportFlag&reportRead != 0 {
+				reportMetric(ctx, baseMetricName+"_read", guestRead, hostRead, perfValues)
+			}
+		}
+	}
+}
+
+// CrostiniDiskIOPerf runs disk IO performance tests by running the tool "fio".
+func CrostiniDiskIOPerf(ctx context.Context, s *testing.State) {
+	// TODO(cylee): Consolidate container creation logic in a util function since it appears in multiple files.
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	s.Log("Enabling Crostini preference setting")
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+	if err = vm.EnableCrostini(ctx, tconn); err != nil {
+		s.Fatal("Failed to enable Crostini preference setting: ", err)
+	}
+
+	s.Log("Setting up component ", vm.StagingComponent)
+	err = vm.SetUpComponent(ctx, vm.StagingComponent)
+	if err != nil {
+		s.Fatal("Failed to set up component: ", err)
+	}
+
+	s.Log("Creating default container")
+	cont, err := vm.CreateDefaultContainer(ctx, cr.User(), vm.StagingImageServer)
+	if err != nil {
+		s.Fatal("Failed to set up default container: ", err)
+	}
+	defer func() {
+		if err := cont.DumpLog(ctx, s.OutDir()); err != nil {
+			s.Error("Failure dumping container log: ", err)
+		}
+	}()
+
+	testing.ContextLog(ctx, "Installing fio")
+	if err := cont.Command(ctx, "sudo", "apt-get", "-y", "install", "fio").Run(); err != nil {
+		s.Fatal("Failed to install fio", err)
+	}
+
+	const (
+		containerHomeDir = "/home/testuser"
+		testDataFileName = "fio_test_data"
+	)
+
+	testing.ContextLog(ctx, "Copying job files to container")
+	for _, job := range fioJobs {
+		containerJobFilePath := filepath.Join(containerHomeDir, job.fileName)
+		if err := cont.PushFile(ctx, s.DataPath(job.fileName), containerJobFilePath); err != nil {
+			s.Errorf("Failed to push %v to container: %v", job.fileName, err)
+		}
+	}
+
+	guestEnv := runEnv{
+		testDataPath: filepath.Join(containerHomeDir, testDataFileName),
+		jobFilePath: func(jobFile string) string {
+			return filepath.Join(containerHomeDir, jobFile)
+		},
+		fioCmd: func(ctx context.Context, jobFile string, envArgs []string, args []string) *testexec.Cmd {
+			cmdLine := append(envArgs, "fio", jobFile)
+			cmdLine = append(cmdLine, args...)
+			return cont.Command(ctx, cmdLine...)
+		},
+	}
+
+	hostEnv := runEnv{
+		testDataPath: filepath.Join("/mnt/stateful_partition", testDataFileName),
+		jobFilePath:  s.DataPath,
+		fioCmd: func(ctx context.Context, jobFile string, envArgs []string, args []string) *testexec.Cmd {
+			cmd := testexec.CommandContext(ctx, "fio", append([]string{jobFile}, args...)...)
+			cmd.Env = envArgs
+			return cmd
+		},
+	}
+
+	perfValues := perf.Values{}
+	for _, job := range fioJobs {
+		runFIOJob(ctx, guestEnv, hostEnv, job, perfValues)
+	}
+	perfValues.Save(s.OutDir())
+}
