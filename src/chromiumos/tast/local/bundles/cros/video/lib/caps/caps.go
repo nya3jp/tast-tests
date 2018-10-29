d31c7c704716f6695c9e839f746618888c9872b3
Hirokazu Honda <hiroh@chromium.org>
2018-10-29 15:10:09 +0900

    video.Capability: Add video_VideoCapability in Tast
    video.Capability is equivalent to video_VideoCapability in autotest.
    This ensures the capabilities computed by autocaps package are correct.
    
    BUG=chromium:899595
    TEST=video.Capability on kevin
    CQ-DEPEND=CL:1307193, CL:1297560
    
    Change-Id: I2259b810533f86df682234db86517715c9486025

--- src/chromiumos/tast/local/bundles/cros/video/lib/caps/caps.go
+++ src/chromiumos/tast/local/bundles/cros/video/lib/caps/caps.go
@@ -10,40 +10,41 @@ package caps
 // See the below link for detail.
 // https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/master/chromeos-base/autotest-capability-default/.
 const (
-	prefix = "autotest-capability:"
+	// Prefix is the prefix of capability.
+	Prefix = "autotest-capability:"
 
 	// Video Decoding
-	HWDecodeH264      = prefix + "hw_dec_h264_1080_30"
-	HWDecodeH264_60   = prefix + "hw_dec_h264_1080_60"
-	HWDecodeH264_4K   = prefix + "hw_dec_h264_2160_30"
-	HWDecodeH264_4K60 = prefix + "hw_dec_h264_2160_60"
+	HWDecodeH264      = Prefix + "hw_dec_h264_1080_30"
+	HWDecodeH264_60   = Prefix + "hw_dec_h264_1080_60"
+	HWDecodeH264_4K   = Prefix + "hw_dec_h264_2160_30"
+	HWDecodeH264_4K60 = Prefix + "hw_dec_h264_2160_60"
 
-	HWDecodeVP8      = prefix + "hw_dec_vp8_1080_30"
-	HWDecodeVP8_60   = prefix + "hw_dec_vp8_1080_60"
-	HWDecodeVP8_4K   = prefix + "hw_dec_vp8_2160_30"
-	HWDecodeVP8_4K60 = prefix + "hw_dec_vp8_2160_60"
+	HWDecodeVP8      = Prefix + "hw_dec_vp8_1080_30"
+	HWDecodeVP8_60   = Prefix + "hw_dec_vp8_1080_60"
+	HWDecodeVP8_4K   = Prefix + "hw_dec_vp8_2160_30"
+	HWDecodeVP8_4K60 = Prefix + "hw_dec_vp8_2160_60"
 
-	HWDecodeVP9      = prefix + "hw_dec_vp9_1080_30"
-	HWDecodeVP9_60   = prefix + "hw_dec_vp9_1080_60"
-	HWDecodeVP9_4K   = prefix + "hw_dec_vp9_2160_30"
-	HWDecodeVP9_4K60 = prefix + "hw_dec_vp9_2160_60"
+	HWDecodeVP9      = Prefix + "hw_dec_vp9_1080_30"
+	HWDecodeVP9_60   = Prefix + "hw_dec_vp9_1080_60"
+	HWDecodeVP9_4K   = Prefix + "hw_dec_vp9_2160_30"
+	HWDecodeVP9_4K60 = Prefix + "hw_dec_vp9_2160_60"
 
-	HWDecodeVP9_2      = prefix + "hw_dec_vp9-2_1080_30"
-	HWDecodeVP9_2_60   = prefix + "hw_dec_vp9-2_1080_60"
-	HWDecodeVP9_2_4K   = prefix + "hw_dec_vp9-2_2160_30"
-	HWDecodeVP9_2_4K60 = prefix + "hw_dec_vp9-2_2160_60"
+	HWDecodeVP9_2      = Prefix + "hw_dec_vp9-2_1080_30"
+	HWDecodeVP9_2_60   = Prefix + "hw_dec_vp9-2_1080_60"
+	HWDecodeVP9_2_4K   = Prefix + "hw_dec_vp9-2_2160_30"
+	HWDecodeVP9_2_4K60 = Prefix + "hw_dec_vp9-2_2160_60"
 
 	// JPEG Decoding
-	HWDecodeJPEG = prefix + "hw_dec_jpeg"
+	HWDecodeJPEG = Prefix + "hw_dec_jpeg"
 
 	// Video Encoding
-	HWEncodeH264 = prefix + "hw_enc_h264_1080_30"
-	HWEncodeVP8  = prefix + "hw_enc_vp8_1080_30"
-	HWEncodeVP9  = prefix + "hw_enc_vp9_1080_30"
+	HWEncodeH264 = Prefix + "hw_enc_h264_1080_30"
+	HWEncodeVP8  = Prefix + "hw_enc_vp8_1080_30"
+	HWEncodeVP9  = Prefix + "hw_enc_vp9_1080_30"
 
 	// JPEG Encoding
-	HWEncodeJPEG = prefix + "hw_enc_jpeg"
+	HWEncodeJPEG = Prefix + "hw_enc_jpeg"
 
 	// Camera
-	USBCamera = prefix + "usb_camera"
+	USBCamera = Prefix + "usb_camera"
 )
