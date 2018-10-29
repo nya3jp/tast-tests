1aa142c1ddaf5f1889af011efe3b8709df58ac4e
Hirokazu Honda <hiroh@chromium.org>
2018-10-29 15:10:09 +0900

    video.Capability: Add video_VideoCapability in Tast
    video.Capability is equivalent to video_VideoCapability in autotest.
    This ensures the capabilities computed by autocaps package are correct.
    
    BUG=chromium:899595
    TEST=video.Capability on kevin
    CQ-DEPEND=CL:1307193, CL:1297560
    
    Change-Id: I2259b810533f86df682234db86517715c9486025

---
+++ src/chromiumos/tast/local/bundles/cros/video/capability.go
@@ -0,0 +1,104 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"chromiumos/tast/autocaps"
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Capability,
+		Desc: "Compare capabilities computed by autocaps package with ones detected by avtest_label_detect",
+		Attr: []string{"informational"},
+	})
+}
+
+// avtestLabelToCapability is map from label detected by avtest_label_detect to capability computed by autocaps package.
+var avtestLabelToCapability = map[string]string{
+	"hw_video_acc_h264":     caps.HWDecodeH264,
+	"hw_video_acc_vp8":      caps.HWDecodeVP8,
+	"hw_video_acc_vp9":      caps.HWDecodeVP9,
+	"hw_video_acc_vp9_2":    caps.HWDecodeVP9_2,
+	"hw_jpeg_acc_dec":       caps.HWDecodeJPEG,
+	"hw_video_acc_enc_h264": caps.HWEncodeH264,
+	"hw_video_acc_enc_vp8":  caps.HWEncodeVP8,
+	"hw_video_acc_enc_vp9":  caps.HWEncodeVP9,
+	"hw_jpeg_acc_enc":       caps.HWEncodeJPEG,
+	"webcam":                caps.USBCamera,
+}
+
+// Capability compares the results between autocaps package and avtest_label_detect.
+// The test failure is decided as follows, where OK and Fail stands for success and
+// failure, respectively. For the capability marked "disable", we don't check
+// them, because the capability is not disabled in driver level, but disabled in
+// Chrome level by default, which an user can enable it by chrome://flags.
+//  avldetect/autocaps | Yes  | No   | Disable |
+//        detect       | OK   | Fail | OK      |
+//        not detect   | Fail | OK   | OK      |
+func Capability(ctx context.Context, s *testing.State) {
+	// Get capabilities computed by autocaps package.
+	var info autocaps.SysInfo
+	staticCaps, err := autocaps.Read(autocaps.DefaultCapabilityDir, &info)
+	if err != nil {
+		s.Fatal("Failed to read statically-set capabilities: ", err)
+	}
+	testing.ContextLog(ctx, "Statically-set capabilities: ", staticCaps)
+
+	// Get capabilities detected by "avtest_label_detect" command.
+	cmd := testexec.CommandContext(ctx, "avtest_label_detect")
+	avOut, err := cmd.Output()
+	if err != nil {
+		cmd.DumpLog(ctx)
+		s.Fatal("Failed to execute avtest_label_detect: ", err)
+	}
+
+	var detectedLabelRegexp = regexp.MustCompile(`(?m)^Detected label: (.*)$`)
+	detectedCaps := make(map[string]struct{})
+	for _, m := range detectedLabelRegexp.FindAllStringSubmatch(string(avOut), -1) {
+		label := strings.TrimSpace(m[1])
+		if c, found := avtestLabelToCapability[label]; found {
+			detectedCaps[stripPrefix(c)] = struct{}{}
+		}
+	}
+	testing.ContextLog(ctx, "avtest_label_detect result: ", detectedCaps)
+
+	for _, c := range avtestLabelToCapability {
+		c = stripPrefix(c)
+		_, wasDetected := detectedCaps[c]
+		state, ok := staticCaps[c]
+		if !ok {
+			s.Errorf("static capabilities don't include %q", c)
+			continue
+		}
+
+		switch state {
+		case autocaps.Yes:
+			if !wasDetected {
+				s.Errorf("%q statically set but not detected", c)
+			}
+		case autocaps.No:
+			if wasDetected {
+				s.Errorf("%q detected but not statically set", c)
+			}
+		}
+	}
+}
+
+// stripPrefix removes caps.Prefix from the beginning of cap.
+func stripPrefix(cap string) string {
+	if !strings.HasPrefix(cap, caps.Prefix) {
+		panic(fmt.Sprintf("%q doesn't start with %q", cap, caps.Prefix))
+	}
+	return cap[len(caps.Prefix):]
+}
