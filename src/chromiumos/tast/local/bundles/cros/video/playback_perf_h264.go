00d6bb007c8125fa80e8caad247605a70f9384a9
Hirokazu Honda <hiroh@chromium.org>
2018-10-30 14:26:32 +0900

    video.PlaybackPerfH264: Lengthen timeout to 3 minutes
    Tast default timeout is two minutes. In video.PlaybackPerfH264, video playes for
    30 seconds two times. Therefore, 1 minutes are remained for other tasks, e.g.,
    loging chrome and getting histogram values. It turns out from log results that 1
    minutes is not enough for the tasks on low-end devices.
    This change lengthen test timeout to 3 minutes.
    
    BUG=chromium:890733
    TEST=video.PlaybackPerfH264 on daisy
    
    Change-Id: Iabb82c67c47f0ab28373a596a1227511215b39a7

--- src/chromiumos/tast/local/bundles/cros/video/playback_perf_h264.go
+++ src/chromiumos/tast/local/bundles/cros/video/playback_perf_h264.go
@@ -6,6 +6,7 @@ package video
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/local/bundles/cros/video/playback"
 	"chromiumos/tast/testing"
@@ -15,9 +16,11 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         PlaybackPerfH264,
 		Desc:         "Measure video playback performance with/without HW acceleration",
-		Attr:         []string{"informational"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome_login"},
 		Data:         []string{"traffic-1920x1080-8005020218f6b86bfa978e550d04956e.mp4"},
+		// Default timeout (i.e. 2 minutes) is not enough for low-end devices.
+		Timeout: 3 * time.Minute,
 	})
 }
 
