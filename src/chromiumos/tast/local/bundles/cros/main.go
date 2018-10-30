937549069a1731df2b245224ceb1290e1e0f2253
Shuhei Takahashi <nya@chromium.org>
2018-10-30 14:30:02 +0900

    tast-tests: Move waitUntilReady to a shared package.
    This function will be called from other local bundles too.
    
    BUG=chromium:875141
    TEST=tast run DUT example.Pass
    
    Change-Id: I120e8e59bc6c2dfbb4c81f0d539cee7d034170b6

--- src/chromiumos/tast/local/bundles/cros/main.go
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -8,14 +8,10 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"os"
-	"time"
 
 	// Underscore-imported packages register their tests via init functions.
 	"chromiumos/tast/bundle"
-	"chromiumos/tast/errors"
 	_ "chromiumos/tast/local/bundles/cros/arc"
 	_ "chromiumos/tast/local/bundles/cros/arcapp"
 	_ "chromiumos/tast/local/bundles/cros/audio"
@@ -32,68 +28,9 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/ui"
 	_ "chromiumos/tast/local/bundles/cros/video"
 	_ "chromiumos/tast/local/bundles/cros/vm"
-	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/local/ready"
 )
 
 func main() {
-	os.Exit(bundle.Local(os.Stdin, os.Stdout, os.Stderr, waitUntilReady))
-}
-
-// waitUntilReady waits until the system is (marginally) ready for tests to run.
-// Tast can sometimes be run against a freshly-booted VM, and we don't want every test that
-// depends on a critical daemon to need to call upstart.WaitForJobStatus to wait for the
-// corresponding job to be running. See https://crbug.com/897521 for more details.
-func waitUntilReady(ctx context.Context, log func(string)) error {
-	// Periodically log a message to make it clearer what we're doing.
-	// Sending a periodic control message is also needed to let the main tast process
-	// know that the DUT is still responsive.
-	done := make(chan struct{})
-	defer func() { done <- struct{}{} }()
-	go func() {
-		for {
-			select {
-			case <-time.After(30 * time.Second):
-				log("Still waiting for important system services to be running")
-			case <-done:
-				return
-			}
-		}
-	}()
-
-	// If system-services doesn't enter "start/running", everything's probably broken, so give up.
-	const systemServicesJob = "system-services"
-	if err := upstart.WaitForJobStatus(ctx, systemServicesJob, upstart.StartGoal, upstart.RunningState,
-		upstart.TolerateWrongGoal, 2*time.Minute); err != nil {
-		return errors.Wrapf(err, "failed waiting for %v job", systemServicesJob)
-	}
-
-	// Make a best effort for important daemon jobs that start later, but just log errors instead of failing.
-	// We don't want to abort the whole test run if there's a bug in a daemon that prevents it from starting.
-	var daemonJobs = []string{
-		"debugd",         // "start on started ui"
-		"metrics_daemon", // "start on stopped crash-boot-collect"
-		"shill",          // "start on started network-services and started wpasupplicant"
-	}
-	type jobError struct {
-		job string
-		err error
-	}
-	ch := make(chan *jobError)
-	for _, job := range daemonJobs {
-		go func(job string) {
-			if err := upstart.WaitForJobStatus(ctx, job, upstart.StartGoal, upstart.RunningState,
-				upstart.TolerateWrongGoal, time.Minute); err == nil {
-				ch <- nil
-			} else {
-				ch <- &jobError{job, err}
-			}
-		}(job)
-	}
-	for range daemonJobs {
-		if je := <-ch; je != nil {
-			log(fmt.Sprintf("Failed waiting for job %v: %v", je.job, je.err))
-		}
-	}
-
-	return nil
+	os.Exit(bundle.Local(os.Stdin, os.Stdout, os.Stderr, ready.Wait))
 }
