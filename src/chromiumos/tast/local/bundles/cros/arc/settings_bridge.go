aa135816dbe05c2c156f38c7dfac49284f51b345
Sara Kato <sarakato@google.com>
2018-10-30 17:10:51 +0900

    tast-tests: Add arc.SettingsBridge.
    Add a new local test that verifies that accessibility
    settings are passed to Android.
    
    BUG=b:111049216
    TEST=tast run DUT arc.SettingsBridge
    Change-Id: I1ddffb6b8c973dcce6f7d76661da462bd2d56ec3

---
+++ src/chromiumos/tast/local/bundles/cros/arc/settings_bridge.go
@@ -0,0 +1,97 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SettingsBridge,
+		Desc:         "Checks that Chrome settings are persisted in ARC",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func enableAccessibility(ctx context.Context, conn *chrome.Conn) error {
+	err := conn.Exec(ctx, `
+	window.__spoken_feedback_set_complete = false;
+	chrome.accessibilityFeatures.spokenFeedback.set({value: true});
+	chrome.accessibilityFeatures.spokenFeedback.get({}, () => {
+		window.__spoken_feedback_set_complete = true;
+	})`)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func isAccessibilityEnabled(ctx context.Context, a *arc.ARC) (bool, error) {
+	cmd := a.Command(ctx, "settings", "--user", "0", "get", "secure", "accessibility_enabled")
+	res, err := cmd.Output()
+	if err != nil {
+		cmd.DumpLog(ctx)
+		return false, err
+	}
+	if strings.TrimSpace(string(res)) == "1" {
+		return true, nil
+	}
+	return false, nil
+}
+
+func SettingsBridge(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs([]string{"--force-renderer-accessibility"}))
+	if err != nil {
+		s.Log("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	res, err := isAccessibilityEnabled(ctx, a)
+	if err != nil {
+		s.Fatal("Failed to check whether accessibility is enabled in Android: ", err)
+	}
+	if res {
+		s.Fatal("Failed to ensure accessibility is disabled: ", err)
+	}
+
+	if err = enableAccessibility(ctx, tconn); err != nil {
+		s.Fatal("Failed to enable spoken feedback: ", err)
+	}
+
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		res, err := isAccessibilityEnabled(ctx, a)
+		if err != nil {
+			s.Fatal("Failed to check whether accessibility is enabled in Android: ", err)
+		}
+		if res {
+			return nil
+		}
+		return errors.New("accessibility not enabled")
+	}, &testing.PollOptions{Timeout: 30 * time.Second})
+
+	if err != nil {
+		s.Fatal("Failed to ensure accessibility is enabled: ", err)
+	}
+}
