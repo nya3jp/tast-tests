848674a9a82dd2de87e1c68d0fc3d366c73c12df
David Staessens <dstaessens@chromium.org>
2018-11-06 09:21:50 +0900

    tast/local/bundles/cros: Add JPEG HW decode accelerator tests to Tast.
    TEST=DecodeAccelJpeg
    
    BUG=chromium:899604
    
    Change-Id: I37f12c0437818bb5a16e106446af6d854e3e4347

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_jpeg.go
@@ -0,0 +1,38 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DecodeAccelJPEG,
+		Desc:         "Run Chrome jpeg_decode_accelerator_unittest",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{caps.HWDecodeJPEG},
+		Data:         jpegTestFiles,
+	})
+}
+
+// testFileList lists the files required by the JPEG decode accelerator test.
+var jpegTestFiles = []string{
+	"peach_pi-1280x720.jpg",
+	"peach_pi-40x23.jpg",
+	"peach_pi-41x22.jpg",
+	"peach_pi-41x23.jpg",
+	"pixel-1280x720.jpg",
+}
+
+// DecodeAccelJPEG runs a set of HW JPEG decode tests, defined in
+// jpeg_decode_accelerator_unittest.
+func DecodeAccelJPEG(ctx context.Context, s *testing.State) {
+	decode.RunAccelJPEGTest(ctx, s, "DecodeAccelJpeg", jpegTestFiles)
+}
