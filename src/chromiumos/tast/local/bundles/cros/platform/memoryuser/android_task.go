d2b1613f80412c9502228931863d7ea8ca9539c4
Alexis Savery <asavery@chromium.org>
2018-11-05 17:37:42 -0800

    tast-tests: Add test for multifaceted memory usage
    memoryuser contains the basic structure of running ARC
    apps, opening Chrome tabs, and running commands in a VM
    in parallel. Also collects /proc/meminfo, iostat -c,
    /var/log/vmlog/ and /var/log/memd/ information.
    
    The core functionality of running ARC, Chrome, and VMs together and
    collecting logs has been separated out into the memoryuser directory
    to make it easier to replace the specific apps, tabs, and vm commands
    as well as adding additional tests.
    
    heavy_memory_user is a test that opens Chrome tabs and runs dd in a
    vm in parallel to use a large amount of memory.
    
    BUG=chromium:902164
    TEST=Ran the test
    
    Change-Id: Idc76ee6abc3387a00d537442d550049798b9463d

---
+++ src/chromiumos/tast/local/bundles/cros/platform/memoryuser/android_task.go
@@ -0,0 +1,54 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package memoryuser
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/testing"
+)
+
+// AndroidTask implements MemoryTask to run ARC apps.
+type AndroidTask struct {
+	// ApkPath is the path to use to access the apk file
+	ApkPath string
+	// Apk is a filename of an APK file in the data directory.
+	Apk string
+	// Pkg is the package name of the app to launch.
+	Pkg string
+	// ActivityName is the activity class name of the app to launch.
+	ActivityName string
+	// TestFunc is the test body function to run.
+	TestFunc func(a *arc.ARC, d *ui.Device)
+}
+
+// Run installs the app apk and runs the test function defined in the AndroidTask in the existing ARC instance.
+func (at *AndroidTask) Run(ctx context.Context, testEnv *TestEnv) error {
+	testing.ContextLog(ctx, "Starting app")
+	if err := testEnv.ARC.Install(ctx, at.ApkPath); err != nil {
+		return errors.Wrapf(err, "failed installing app %s", at.Apk)
+	}
+
+	if err := testEnv.ARC.Command(ctx, "am", "start", "-W", at.Pkg+"/"+at.ActivityName).Run(); err != nil {
+		return errors.Wrapf(err, "failed starting app %s", at.Apk)
+	}
+
+	at.TestFunc(testEnv.ARC, testEnv.ARCDevice)
+	return nil
+}
+
+// Close stops everything associated with the package defined in the AndroidTask.
+func (at *AndroidTask) Close(ctx context.Context, testEnv *TestEnv) {
+	testEnv.ARC.Command(ctx, "am", "force-stop", at.Pkg).Run()
+}
+
+// String returns a string describing the AndroidTask.
+func (at *AndroidTask) String() string {
+	return fmt.Sprintf("AndroidTask with apk: %s, pkg: %s, activity: %s", at.Apk, at.Pkg, at.ActivityName)
+}
