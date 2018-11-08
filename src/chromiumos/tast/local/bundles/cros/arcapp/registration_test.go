4bef7084039f7c959b562653f05e9572ff2c1e8b
Shuhei Takahashi <nya@chromium.org>
2018-11-08 16:06:40 +0900

    test-tests: Add registration tests to check timeouts and deps.
    BUG=None
    TEST=fast_build.sh -T
    CQ-DEPEND=CL:1325260
    CQ-DEPEND=CL:1325258
    
    Change-Id: Ia47704afcebedc65283cbd3d3c593c38bf700d6e

--- src/chromiumos/tast/local/bundles/cros/arcapp/registration_test.go
+++ src/chromiumos/tast/local/bundles/cros/arcapp/registration_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package arc
+package arcapp
 
 import (
 	gotesting "testing"
@@ -12,6 +12,8 @@ import (
 	"chromiumos/tast/testing"
 )
 
+const pattern = "arcapp.*"
+
 func TestTimeout(t *gotesting.T) {
 	const (
 		chromeBootTime  = 60 * time.Second
@@ -20,17 +22,9 @@ func TestTimeout(t *gotesting.T) {
 		minTimeout = chromeBootTime + arc.BootTimeout + minTestBodyTime
 	)
 
-	tests, err := testing.GlobalRegistry().TestsForPatterns([]string{"arc.*"})
-	if err != nil {
-		t.Fatal("Failed to get ARC tests: ", err)
-	}
-	if len(tests) == 0 {
-		t.Fatal("No tests matched for arc.*")
-	}
+	testing.CheckTimeout(t, pattern, minTimeout)
+}
 
-	for _, tst := range tests {
-		if tst.Timeout < minTimeout {
-			t.Errorf("%s: timeout is too short (%v < %v)", tst.Name, tst.Timeout, minTimeout)
-		}
-	}
+func TestSoftwareDeps(t *gotesting.T) {
+	testing.CheckSoftwareDeps(t, pattern, []string{"chrome_login", "android"})
 }
