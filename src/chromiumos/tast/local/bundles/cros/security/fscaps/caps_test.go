8e3a935935f022dcf15bb44a25e456b55e833ced
Daniel Erat <derat@chromium.org>
2018-11-07 17:52:34 -0800

    tast-tests: Add security.PrivilegedFiles.
    Port the existing security_SuidBinaries Autotest test (which
    also checks setgid bits and file capabilities) to
    security.PrivilegedFiles.
    
    Also add a small fscaps package that makes syscalls to read
    file capabilities. The Autotest instead runs the fscaps
    program for every file, but doing so makes the test
    substantially slower (28s rather than ~500ms), so the added
    complexity to avoid that seems worthwhile.
    
    BUG=chromium:877733
    TEST=ran it on a few DUTs; also added unit tests
    
    Change-Id: Id826c5493795e7a8cecac7309bc8a9d6a6731dee

---
+++ src/chromiumos/tast/local/bundles/cros/security/fscaps/caps_test.go
@@ -0,0 +1,53 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fscaps
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCapsStringAndEmpty(t *testing.T) {
+	for _, tc := range []struct {
+		eff, inh, perm uint64
+		exp            string // caps are sorted by their defined order when printed
+		empty          bool
+	}{
+		{0, 0, 0, "[]", true},
+		{KILL, 0, 0, "[e:kill]", false},
+		{SYS_PTRACE, 0, SETPCAP, "[e:sys_ptrace p:setpcap]", false},
+		{SETPCAP, CHOWN, WAKE_ALARM, "[e:setpcap i:chown p:wake_alarm]", false},
+		{SETPCAP | AUDIT_READ, CHOWN, WAKE_ALARM | SYS_ADMIN | SYS_CHROOT,
+			"[e:setpcap|audit_read i:chown p:sys_chroot|sys_admin|wake_alarm]", false},
+	} {
+		c := Caps{Effective: tc.eff, Inheritable: tc.inh, Permitted: tc.perm}
+		desc := fmt.Sprintf("Caps{0x%x 0x%x 0x%x}", tc.eff, tc.inh, tc.perm)
+		if s := c.String(); s != tc.exp {
+			t.Errorf("%s.String() = %q; want %q", desc, s, tc.exp)
+		}
+		if empty := c.Empty(); empty != tc.empty {
+			t.Errorf("%s.Empty() = %v; want %v", desc, empty, tc.empty)
+		}
+	}
+}
+
+func TestCapsEqual(t *testing.T) {
+	caps := []Caps{
+		Caps{},
+		Caps{Effective: KILL},
+		Caps{Effective: KILL | LEASE},
+		Caps{Effective: KILL, Inheritable: LEASE},
+		Caps{Effective: KILL, Inheritable: LEASE, Permitted: MKNOD},
+	}
+
+	for i := 0; i < len(caps); i++ {
+		for j := 0; j < len(caps); j++ {
+			self := i == j
+			if eq := caps[i].Equal(caps[j]); eq != self {
+				t.Errorf("%s.Equal(%s) = %v; want %v", caps[i], caps[j], eq, self)
+			}
+		}
+	}
+}
