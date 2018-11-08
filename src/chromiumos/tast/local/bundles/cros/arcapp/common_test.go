92f667c63f9e59c0619c16648601a3e725b99868
Shuhei Takahashi <nya@chromium.org>
2018-11-08 15:06:09 +0900

    arc.SettingBridge: Add "android" to SoftwareDeps.
    Obviously ARC is required for the test.
    
    BUG=b:118865111
    TEST=fast_build.sh -T
    TEST=tast run betty-DUT arc.SettingsBridge
    TEST=Try removing a required dep and make sure the unit test fails
    
    Change-Id: I59aca6bbb48721aac50bf8574c620111211afd44

---
+++ src/chromiumos/tast/local/bundles/cros/arcapp/common_test.go
@@ -0,0 +1,57 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arcapp
+
+import (
+	"fmt"
+	gotesting "testing"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/testing"
+)
+
+func getTests() []*testing.Test {
+	const pattern = "arcapp.*"
+	tests, err := testing.GlobalRegistry().TestsForPatterns([]string{pattern})
+	if err != nil {
+		panic(fmt.Sprintf("Failed to get ARC app tests: %v", err))
+	}
+	if len(tests) == 0 {
+		panic(fmt.Sprintf("No tests matched for %s", pattern))
+	}
+	return tests
+}
+
+func TestTimeout(t *gotesting.T) {
+	const (
+		chromeBootTime  = 60 * time.Second
+		minTestBodyTime = 30 * time.Second
+
+		minTimeout = chromeBootTime + arc.BootTimeout + minTestBodyTime
+	)
+
+	for _, tst := range getTests() {
+		if tst.Timeout < minTimeout {
+			t.Errorf("%s: timeout is too short (%v < %v)", tst.Name, tst.Timeout, minTimeout)
+		}
+	}
+}
+
+func TestSoftwareDeps(t *gotesting.T) {
+	requiredDeps := []string{"chrome_login", "android"}
+
+	for _, tst := range getTests() {
+		deps := make(map[string]struct{})
+		for _, d := range tst.SoftwareDeps {
+			deps[d] = struct{}{}
+		}
+		for _, d := range requiredDeps {
+			if _, ok := deps[d]; !ok {
+				t.Errorf("%s: missing software dependency %q", tst.Name, d)
+			}
+		}
+	}
+}
