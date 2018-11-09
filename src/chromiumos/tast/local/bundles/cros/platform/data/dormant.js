e0c9e852a9e32e7a84ef34515a2e95b4d7cd2a33
Luigi Semenzato <semenzato@chromium.org>
2018-11-09 10:02:09 -0800

    tast-tests: add platform/memory_pressure.go
    This is a rewrite of the platform_MemoryPressure autotest.
    There are slight changes in functionality.
    Most importantly, it uses Web Page Replay to serve all requests.
    
    This test reproduces a streamlined user behavior, with the intent
    of filling up RAM quickly in a somewhat realistic way.  It opens
    a bunch of tabs from a list, going over the list multiple times
    as needed.  After opening each tab, it cycles through a small
    number of recently-used tabs.  While visiting each tab, it scrolls
    the tab down and up for a short time.  After the first tab discard
    (or OOM kill) it collects some stats, then it rests for a while
    and collects some more stats.
    
    BUG=chromium:921196
    TEST=ran on x86 and ARM platforms
    
    Change-Id: I6e778b4173ad4b43eeb2abff093cc462482f7c6d

---
+++ src/chromiumos/tast/local/bundles/cros/platform/data/dormant.js
@@ -0,0 +1,36 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Code which evaluates to |true| when the page has quiesced, i.e. has become
+// dormant because no further load events have happened for a while.
+
+// This code is used in WaitForExpr, which wraps it inside !!( ... ).
+// It cannot be a plain block because JS syntax does not allow that.
+
+(function () {
+  const QUIESCENCE_TIMEOUT_MS = 2000;
+
+  if (document.readyState !== 'complete') {
+    return false;
+  }
+
+  const resourceTimings = performance.getEntriesByType('resource');
+  let lastEntry = null;
+  if (resourceTimings.length > 0) {
+    lastEntry = resourceTimings.pop();
+    performance.clearResourceTimings();
+  }
+
+  const loadTime = performance.timing.loadEventEnd -
+        performance.timing.navigationStart;
+  let lastResponseTimeMs = 0;
+
+  if (!lastEntry || lastEntry.responseEnd < loadTime) {
+    lastResponseTimeMs = performance.now() - loadTime;
+  } else {
+    lastResponseTimeMs = performance.now() - lastEntry.responseEnd;
+  }
+
+  return lastResponseTimeMs >= QUIESCENCE_TIMEOUT_MS;
+})()
