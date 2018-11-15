8f01247978dc416138b938e579ff74e2dd0c79dd
Daniel Erat <derat@chromium.org>
2018-11-14 22:08:59 -0800

    tast-tests: Add security.NetworkListeners* local tests.
    Port the security_NetworkListener Autotest test to
    security.NetworkListenersARC and
    security.NetworkListenersNonARC. These tests compare
    processes listening for TCP connections against a baseline
    while logged into Chrome on systems that do and do not
    support ARC.
    
    BUG=chromium:877733
    TEST=ran both tests
    CQ-DEPEND=
    
    Change-Id: I98e01f842c3d9aa335effcd61a0186e6c270e6b2

---
+++ src/chromiumos/tast/local/bundles/cros/security/network_listeners_arc.go
@@ -0,0 +1,46 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"context"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/security/netlisten"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NetworkListenersARC,
+		Desc:         "Checks TCP listeners on ARC systems",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login", "android"},
+		Timeout:      arc.BootTimeout,
+	})
+}
+
+func NetworkListenersARC(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ARCEnabled())
+	if err != nil {
+		s.Fatal("Failed to log in: ", err)
+	}
+	defer cr.Close(ctx)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed waiting for Android to boot: ", err)
+	}
+	defer a.Close()
+
+	netlisten.CheckPorts(ctx, s, map[string]string{
+		cr.DebugAddrPort(): chrome.ExecPath,
+		"127.0.0.1:5037":   "/usr/bin/adb",
+		// sslh is installed on ARC-capable systems to multiplex port 22 traffic between sshd and adb.
+		"*:22":   "/usr/sbin/sslh-fork",
+		"*:2222": "/usr/sbin/sshd",
+	})
+}
