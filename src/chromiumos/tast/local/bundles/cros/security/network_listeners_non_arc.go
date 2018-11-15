2a562575b1860022df08acd5304498f4f1af68e3
Daniel Erat <derat@chromium.org>
2018-11-14 22:08:59 -0800

    tast-tests: Add security.NetworkListeners* local tests.
    Port the security_NetworkListener Autotest test to
    security.NetworkListenersARC and
    security.NetworkListenersNonARC. These tests compare
    processes listening for TCP connections against a baseline
    while logged into Chrome on systems that do and do not
    support ARC.
    
    BUG=chromium:877733
    TEST=ran both tests
    CQ-DEPEND=I5f15b434ff85906e15b4b4f463c98da385778c86
    
    Change-Id: I98e01f842c3d9aa335effcd61a0186e6c270e6b2
    Reviewed-on: https://chromium-review.googlesource.com/1337435
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/security/network_listeners_non_arc.go
@@ -0,0 +1,35 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/security/netlisten"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NetworkListenersNonARC,
+		Desc:         "Checks TCP listeners on non-ARC systems",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login", "no_android"},
+	})
+}
+
+func NetworkListenersNonARC(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to log in: ", err)
+	}
+	defer cr.Close(ctx)
+
+	netlisten.CheckPorts(ctx, s, map[string]string{
+		cr.DebugAddrPort(): chrome.ExecPath,
+		"*:22":             "/usr/sbin/sshd",
+	})
+}
