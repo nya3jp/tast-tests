d7e6bfee1a21e42762994ff299f97d96baee8875
Daniel Erat <derat@chromium.org>
2018-11-15 17:09:55 -0800

    tast-tests: Add security.ModuleLocking local test.
    Port the security_ModuleLocking Autotest test to
    security.ModuleLocking. The old test also unsets
    /proc/sys/kernel/chromiumos/module_locking and checks that
    unsafe loading is then permitted, but I didn't bother
    porting this because I'm worried about leaving DUTs in a bad
    state and because module_locking doesn't even exist on the
    lab DUTs that I've checked.
    
    BUG=chromium:877733
    TEST=ran against several DUTs
    
    Change-Id: Ia6ce6fa5f486589a3a5813c4a8a72e0c24f71642

---
+++ src/chromiumos/tast/local/bundles/cros/security/module_locking.go
@@ -0,0 +1,161 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"compress/gzip"
+	"context"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ModuleLocking,
+		Desc: "Checks that kernel modules can't be loaded from outside the root filesystem",
+		Attr: []string{"informational"},
+	})
+}
+
+func ModuleLocking(ctx context.Context, s *testing.State) {
+	const (
+		sysctl = "/proc/sys/kernel/chromiumos/module_locking"
+		module = "test_module" // installed in test images
+	)
+
+	s.Log("Checking ", sysctl)
+	if b, err := ioutil.ReadFile(sysctl); err != nil && !os.IsNotExist(err) {
+		s.Fatalf("Failed to read %s: %v", sysctl, err)
+	} else if err == nil && string(b) != "1\n" {
+		s.Fatalf("%v contains %q; want 1", sysctl, string(b))
+	}
+
+	cmd := testexec.CommandContext(ctx, "uname", "-r")
+	out, err := cmd.Output()
+	if err != nil {
+		defer cmd.DumpLog(ctx)
+		s.Fatal("Failed to get kernel release: ", err)
+	}
+	moduleDir := filepath.Join("/lib/modules", strings.TrimSpace(string(out)))
+	modulePath := filepath.Join(moduleDir, "kernel/kernel", module+".ko")
+	if _, err := os.Stat(modulePath); err != nil {
+		s.Fatalf("Failed to find %q module: %v", module, err)
+	}
+	s.Log("Using module ", modulePath)
+
+	// Unloads the module if it's currently loaded.
+	unload := func() {
+		if moduleLoaded(s, module) {
+			cmd := testexec.CommandContext(ctx, "rmmod", module)
+			if err := cmd.Run(); err != nil {
+				defer cmd.DumpLog(ctx)
+				s.Fatalf("Failed to run %q: %v", strings.Join(cmd.Args, " "), err)
+			}
+		}
+	}
+
+	// Runs the supplied command. An test error is reported if the result doesn't match wantSuccess.
+	run := func(wantSuccess bool, name string, args ...string) {
+		cmd := testexec.CommandContext(ctx, name, args...)
+		if err := cmd.Run(); err != nil && wantSuccess {
+			s.Errorf("%q failed: %v", strings.Join(cmd.Args, " "), err)
+			cmd.DumpLog(ctx)
+		} else if err == nil && !wantSuccess {
+			s.Errorf("%q unexpectedly succeeded", strings.Join(cmd.Args, " "))
+		}
+	}
+
+	unload()
+
+	s.Log("Attempting to modprobe ", module)
+	run(true, "modprobe", module)
+	unload()
+
+	s.Log("Attempting to insmod ", modulePath)
+	run(true, "insmod", modulePath)
+	unload()
+
+	td, err := ioutil.TempDir("", "security.ModuleLocking.")
+	if err != nil {
+		s.Fatal("Failed to create temp dir: ", err)
+	}
+	defer os.RemoveAll(td)
+
+	tmpPath := filepath.Join(td, module+".ko")
+	copyModule(s, modulePath, tmpPath, false /* gzip */)
+	s.Log("Attempting to insmod ", tmpPath)
+	run(false, "insmod", tmpPath)
+	unload()
+
+	tmpGzPath := filepath.Join(td, module+".ko.gz")
+	copyModule(s, modulePath, tmpGzPath, true /* gzip */)
+	s.Logf("Attempting to insmod %s to trigger old blob-style kernel syscall", tmpGzPath)
+	run(false, "insmod", tmpGzPath)
+	unload()
+
+	// Guard against a regression of http://b/21762937, where a bind unmount would
+	// incorrectly trigger protections against unmounts of pinned filesystems.
+	s.Log("Bind-mounting/unmounting and attempting to modprobe ", module)
+	mountPoint := filepath.Join(td, "mount")
+	if err := os.Mkdir(mountPoint, 0755); err != nil {
+		s.Fatalf("Failed to create %v: %v", mountPoint, err)
+	}
+	run(true, "mount", "-o", "bind", "/", mountPoint)
+	run(true, "umount", mountPoint)
+	run(true, "modprobe", module)
+	unload()
+}
+
+// copyModule copies the kernel module file at srcPath to dstPath.
+// If useGzip is true, the file is gzip-compressed as it is copied.
+// Errors cause fatal test errors to be reported via s.
+func copyModule(s *testing.State, srcPath, dstPath string, useGzip bool) {
+	src, err := os.Open(srcPath)
+	if err != nil {
+		s.Fatalf("Failed to open %v: %v", srcPath, err)
+	}
+	defer src.Close()
+
+	var dst io.WriteCloser
+	if dst, err = os.Create(dstPath); err != nil {
+		s.Fatalf("Failed to create %v: %v", dstPath, err)
+	}
+
+	if useGzip {
+		// Wrap the dest file and check that the gzip.Writer closes successfully instead.
+		defer dst.Close()
+		dst = gzip.NewWriter(dst)
+	}
+
+	if _, err := io.Copy(dst, src); err != nil {
+		dst.Close()
+		s.Fatalf("Failed to copy from %v to %v: %v", srcPath, dstPath, err)
+	}
+	if err := dst.Close(); err != nil {
+		s.Fatalf("Failed to close %v: %v", dstPath, err)
+	}
+}
+
+// moduleLoaded returns true if the named kernel module (e.g. "test_module") is currently loaded.
+// Errors cause fatal test errors to be reported via s.
+func moduleLoaded(s *testing.State, module string) bool {
+	const modulesPath = "/proc/modules"
+	b, err := ioutil.ReadFile(modulesPath)
+	if err != nil {
+		s.Fatalf("Failed to read %s: %v", modulesPath, err)
+	}
+	for _, line := range strings.Split(string(b), "\n") {
+		if parts := strings.Fields(line); len(parts) > 0 && parts[0] == module {
+			return true
+		}
+	}
+	return false
+}
