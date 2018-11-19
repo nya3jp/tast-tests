c849b681750f6a42a436d95560e57d0e05bc0ce5
Hirokazu Honda <hiroh@chromium.org>
2018-11-19 21:23:36 +0900

    video.EncodeAccel*: Generate NV12 raw video data from .webm
    Formerly, NV12 raw video data is put on test image. To save the disk usage in
    test image more, NV12 raw video data are generated from .webm as well as I420.
    Since vpxdec deosn't support NV12, we perform the conversion from I420 to NV12.
    The conversion is done by go code, because using imagemagick for it seems
    complicated, comparing to golang implementation by myself.
    
    BUG=chromium:889496
    TEST=video.EncodeAccel*
    CQ-DEPEND=CL:1341711
    
    Change-Id: Id9bd1af9d4c0201ff2af32d23b7b046d18e1bf09

--- src/chromiumos/tast/local/bundles/cros/video/encode/test_cases.go
+++ src/chromiumos/tast/local/bundles/cros/video/encode/test_cases.go
@@ -6,56 +6,37 @@ package encode
 
 import "chromiumos/tast/local/bundles/cros/video/lib/videotype"
 
-// BearI420 is the test parameters of video_encode_accelerator_unittest for "bear_320x192_40frames.yuv".
-var BearI420 = StreamParams{
-	Name:    "bear_320x192_40frames.yuv",
-	Width:   320,
-	Height:  192,
+// Bear192P is the test parameters of video_encode_accelerator_unittest for "bear_320x192_40frames.yuv".
+var Bear192P = StreamParams{
+	Name:    "bear-320x192.vp9.webm",
+	Size:    videotype.Size{W: 320, H: 192},
 	Bitrate: 200000,
-	Format:  videotype.I420,
 }
 
-// BearNV12 is the test parameters of video_encode_accelerator_unittest for "bear_320x192_40frames.nv12.yuv".
-var BearNV12 = StreamParams{
-	Name:    "bear_320x192_40frames.nv12.yuv",
-	Width:   320,
-	Height:  192,
-	Bitrate: 200000,
-	Format:  videotype.NV12,
-}
-
-// Crowd1080PI420 is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "crowd1920x1080.webm".
-var Crowd1080PI420 = StreamParams{
-	Name:    "crowd-1920x1080.webm",
-	Width:   1920,
-	Height:  1080,
+// Crowd1080P is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "crowd1920x1080.webm".
+var Crowd1080P = StreamParams{
+	Name:    "crowd-1920x1080.vp9.webm",
+	Size:    videotype.Size{W: 1920, H: 1080},
 	Bitrate: 4000000,
-	Format:  videotype.I420,
 }
 
-// Tulip720PI420 is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "tulip2-1280x720.webm".
-var Tulip720PI420 = StreamParams{
-	Name:    "tulip2-1280x720.webm",
-	Width:   1280,
-	Height:  720,
+// Tulip720P is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "tulip2-1280x720.webm".
+var Tulip720P = StreamParams{
+	Name:    "tulip2-1280x720.vp9.webm",
+	Size:    videotype.Size{W: 1280, H: 720},
 	Bitrate: 1200000,
-	Format:  videotype.I420,
 }
 
-// Tulip360PI420 is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "tulip2-640x360.webm".
-var Tulip360PI420 = StreamParams{
-	Name:    "tulip2-640x360.webm",
-	Width:   640,
-	Height:  360,
+// Tulip360P is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "tulip2-640x360.webm".
+var Tulip360P = StreamParams{
+	Name:    "tulip2-640x360.vp9.webm",
+	Size:    videotype.Size{W: 640, H: 360},
 	Bitrate: 500000,
-	Format:  videotype.I420,
 }
 
-// Tulip180PI420 is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "tulip2-320x180.webm".
-var Tulip180PI420 = StreamParams{
-	Name:    "tulip2-320x180.webm",
-	Width:   320,
-	Height:  180,
+// Tulip180P is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "tulip2-320x180.webm".
+var Tulip180P = StreamParams{
+	Name:    "tulip2-320x180.vp9.webm",
+	Size:    videotype.Size{W: 320, H: 180},
 	Bitrate: 100000,
-	Format:  videotype.I420,
 }
