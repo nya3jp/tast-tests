079738462c705980b61cca9d26da230c570dd4f2
Daniel Erat <derat@chromium.org>
2018-11-19 10:49:22 -0800

    tast-tests: Make ui.ChromeCrashNotLoggedIn sleep briefly.
    Make the ui.ChromeCrashNotLoggedIn test sleep 3 seconds
    before killing Chrome as a speculative workaround for Chrome
    hangs that may be caused when it receives SIGSEGV just after
    starting.
    
    BUG=chromium:906690
    TEST=ran the test
    
    Change-Id: I90dab680ecfdcd43b73dca81e74ea38260fffa32
    Reviewed-on: https://chromium-review.googlesource.com/c/1342957
    Tested-by: Dan Erat <derat@chromium.org>
    Trybot-Ready: Dan Erat <derat@chromium.org>
    Reviewed-by: Gregory Meinke <gmeinke@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Dan Erat <derat@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/chrome_crash_not_logged_in.go
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_crash_not_logged_in.go
@@ -6,6 +6,7 @@ package ui
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/local/bundles/cros/ui/chromecrash"
 	"chromiumos/tast/local/chrome"
@@ -26,6 +27,16 @@ func ChromeCrashNotLoggedIn(ctx context.Context, s *testing.State) {
 		s.Fatal("Chrome startup failed: ", err)
 	}
 	defer cr.Close(ctx)
+
+	// Sleep briefly as a speculative workaround for Chrome hangs that are occasionally seen
+	// when this test sends SIGSEGV to Chrome soon after it starts: https://crbug.com/906690
+	const delay = 3 * time.Second
+	s.Logf("Sleeping %v to wait for Chrome to stabilize", delay)
+	select {
+	case <-time.After(delay):
+	case <-ctx.Done():
+		s.Fatal("Timed out while waiting for Chrome startup: ", err)
+	}
 
 	if dumps, err := chromecrash.KillAndGetDumps(ctx); err != nil {
 		s.Fatal("Couldn't kill Chrome or get dumps: ", err)
