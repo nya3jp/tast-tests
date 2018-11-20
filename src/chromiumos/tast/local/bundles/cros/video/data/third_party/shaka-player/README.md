7e26da2a33725642777010e0b5d8444002f2eccf
Keiichi Watanabe <keiichiw@chromium.org>
2018-11-20 23:01:23 +0900

    video: Add PlayDecodeAccelUsedMSE*
    video.PlayDecodeAccelUsedMSE(VP8|VP9|H264) tests are corresponding to
    video_ChromeHWDecodeUsed.(vp8|vp9|h264).mse in autotest.
    These tests check if video can be played properly when Media Source
    Extensions (MSE) is used.
    
    While the autotest version accessed crosvideo.appspot.com, the Tast
    version use Shaka Player to run tests offline.
    Shaka Player plays video streams by using MPEG-DASH.
    
    BUG=chromium:900102
    TEST=tast run kevin video.*MSE*
    CQ-DEPEND=CL:1343919
    
    Change-Id: Iae6b767a41af3f43e8d1f126e6bf18270ef7e031
    Signed-off-by: Keiichi Watanabe <keiichiw@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/video/data/third_party/shaka-player/README.md
@@ -0,0 +1,11 @@
+# Shaka Player
+
+[Shaka Player](https://github.com/google/shaka-player) is an open-source
+JavaScript library for adaptive media.
+
+## Version
+
+2.4.5
+
+The compiled file was downloaded from
+[cdnjs](https://cdnjs.com/libraries/shaka-player).
---
+++ src/chromiumos/tast/local/bundles/cros/video/play_decode_accel_used_mse_vp8.go
@@ -0,0 +1,35 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/bundles/cros/video/play"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PlayDecodeAccelUsedMSEVP8,
+		Desc:         "Verifies that VP8 video decode acceleration works when MSE is used",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{caps.HWDecodeVP8, "chrome_login"},
+		Data: append(
+			play.MSEDataFiles(),
+			"bear-320x240-video-only.vp8.webm",
+			"bear-320x240-audio-only.vorbis.webm",
+			"bear-320x240.vp8.mpd",
+		),
+	})
+}
+
+// PlayDecodeAccelUsedMSEVP8 plays a VP8 video stream by shaka player, which uses
+// Media Source Extensions (MSE).
+// After that, it checks if video decode accelerator was used.
+func PlayDecodeAccelUsedMSEVP8(ctx context.Context, s *testing.State) {
+	play.TestPlay(ctx, s, "bear-320x240.vp8.mpd", play.MSEVideo, play.CheckHistogram)
+}
