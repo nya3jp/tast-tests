9778438253be3056baab5fbf7ed848508f840640
Hirokazu Honda <hiroh@chromium.org>
2018-11-22 20:29:15 +0900

    video.EncodeAccelVP8*: Add VP8 large test cases
    BUG=chromium:889496
    TEST=video.EncodeAccelVP8*
    
    Change-Id: I12b0a185712bb954ef9525ac99feab293ef7476e

---
+++ src/chromiumos/tast/local/bundles/cros/video/encode_accel_vp8_360p_i420.go
@@ -0,0 +1,29 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/encode"
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         EncodeAccelVP8360PI420,
+		Desc:         "Run Chrome video_encode_accelerator_unittest from 360p I420 raw frames to VP8 stream",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{caps.HWEncodeVP8},
+		Data:         []string{encode.Tulip360P.Name},
+	})
+}
+
+// EncodeAccelVP8360PI420 runs video_encode_accelerator_unittest to encode VP8 encoding with 360p I420 raw data compressed in tulip2-640x360.webm.
+func EncodeAccelVP8360PI420(ctx context.Context, s *testing.State) {
+	encode.RunAccelVideoTest(ctx, s, videotype.VP8Prof, encode.Tulip360P, videotype.I420)
+}
