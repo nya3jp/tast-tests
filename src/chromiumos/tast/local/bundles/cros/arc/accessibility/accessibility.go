815827306ed06a73b634dfc929398cce3d11c4c2
Sara Kato <sarakato@google.com>
2018-11-22 11:05:34 +0900

    Basic accessibility test using tast for ARC
    TEST=tast run DUT_IP arcapp.AccessibilityEvent
    BUG=b:118784915
    Change-Id: I2d8b268910f1e3fd597746d3da59a8ef6de477c2

---
+++ src/chromiumos/tast/local/bundles/cros/arc/accessibility/accessibility.go
@@ -0,0 +1,25 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package accessibility provides functions to assist with interacting with accessibility settings
+// in ARC accessibility tests.
+package accessibility
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/local/arc"
+)
+
+// Enabled checks if accessibility is enabled in Android.
+func Enabled(ctx context.Context, a *arc.ARC) (enabled bool, err error) {
+	cmd := a.Command(ctx, "settings", "--user", "0", "get", "secure", "accessibility_enabled")
+	res, err := cmd.Output()
+	if err != nil {
+		cmd.DumpLog(ctx)
+		return false, err
+	}
+	return strings.TrimSpace(string(res)) == "1", nil
+}
