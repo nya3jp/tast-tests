c2c13e353522241b130832a368a06cc955a55299
Xiaochu Liu <xiaochu@chromium.org>
2018-11-29 15:41:16 -0800

    platform: dlcservice tast local test
    This tast local test launches dlcservice, waits for it to stop, calls
    D-Bus method of dlcservice, checks success/failure and waits for it to
    stop.
    
    BUG=chromium:921670
    TEST=tast -verbose run 100.90.28.11 platform.DLCService
    
    Change-Id: I5071a0f705e7f89d10bf7bce2568b446adc3d936

---
+++ src/chromiumos/tast/local/bundles/cros/platform/dlcservice.go
@@ -0,0 +1,59 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DLCService,
+		Desc: "Launches dlcservice to verify dlcservice exits on idle and can accept API calls",
+		Attr: []string{"informational"},
+	})
+}
+
+func DLCService(ctx context.Context, s *testing.State) {
+	const job = "dlcservice"
+
+	// dlcservice is a short-lived process.
+	// Restarts dlcservice and checks if it exits on idle.
+	s.Logf("Restarting %s job", job)
+	if err := upstart.RestartJob(ctx, job); err != nil {
+		s.Fatalf("Failed to start %s: %v", job, err)
+	}
+
+	if err := upstart.WaitForJobStatus(ctx, job, upstart.StopGoal, upstart.WaitingState, upstart.TolerateWrongGoal, time.Minute); err != nil {
+		s.Fatalf("Job %s did not exit on idle: %v", job, err)
+	}
+	s.Logf("Job %s stopped", job)
+
+	// dlcservice is activated on-demand via D-Bus method call.
+	// Calls dlcservice's GetInstalled D-Bus method, checks the return results, and checks if it exits on idle.
+	s.Logf("Asking dlcservice for installed DLC modules")
+	cmd := testexec.CommandContext(ctx, "dlcservice_util", "--list")
+	if out, err := cmd.Output(); err != nil {
+		cmd.DumpLog(ctx)
+		s.Fatal("Failed to get installed DLC modules: ", err)
+	} else {
+		// Logs the installed DLC modules info into a file in output dir.
+		if err := ioutil.WriteFile(filepath.Join(s.OutDir(), "installed_dlc_modules.txt"), out, 0644); err != nil {
+			s.Fatal("Failed to write output: ", err)
+		}
+	}
+
+	if err := upstart.WaitForJobStatus(ctx, job, upstart.StopGoal, upstart.WaitingState, upstart.TolerateWrongGoal, time.Minute); err != nil {
+		s.Fatalf("Job %s did not exit on idle: %v", job, err)
+	}
+	s.Logf("Job %s stopped", job)
+}
