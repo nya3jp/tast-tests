065fcef95d5935bfc9115f4a1aeb8270d5534eec
David Valleau <valleau@chromium.org>
2018-12-04 10:25:13 -0800

    Adding tests which make print requests using the virtual usb printer
    BUG=chromium:885915
    TEST=Tests run successfully on asuka device using tast command
    
    Change-Id: I1d3beb02d35ad77516cbc48ecf2a72dfbd9a5eae

---
+++ src/chromiumos/tast/local/bundles/cros/printer/print_ippusb.go
@@ -0,0 +1,42 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package printer
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/local/bundles/cros/printer/usbprinter"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PrintIPPUSB,
+		Desc:         "Tests ipp-over-usb printing",
+		Contacts:     []string{"valleau@chromium.org"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"cups", "virtual_usb_printer"},
+		Data:         []string{"print_ippusb_to_print.pdf", "print_ippusb_golden.pdf"},
+	})
+}
+
+func PrintIPPUSB(ctx context.Context, s *testing.State) {
+	const (
+		descriptors = "/etc/virtual-usb-printer/ippusb_printer.json"
+		attributes  = "/etc/virtual-usb-printer/ipp_attributes.json"
+	)
+
+	tmpDir, err := ioutil.TempDir("", "tast.printer.PrintIPPUSB.")
+	if err != nil {
+		s.Fatal("Failed to create temporary directory")
+	}
+	defer os.RemoveAll(tmpDir)
+	recordPath := filepath.Join(tmpDir, "record.pdf")
+
+	usbprinter.RunPrintTest(ctx, s, descriptors, attributes, recordPath, "", s.DataPath("print_ippusb_to_print.pdf"), s.DataPath("print_ippusb_golden.pdf"))
+}
