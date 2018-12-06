961109a6c3295be401807ac19aa9cd3ca21c546b
David Staessens <dstaessens@chromium.org>
2018-12-06 11:21:29 +0900

    video.DecodeAccelJPEGPerf: Add functionality to disable CPU frequency scaling.
    This functionality is required by the JPEG HW decode accelerator performance
    test that is currently being migrated to Tast.
    
    Also adds a bit more documentation to the cpu.MeasureUsage function.
    
    TEST=DecodeAccelJPEGPerf on nocturne
    BUG=chromium:912413
    Change-Id: Iac97d92073a6b229fb5cd4224b7df4b8c9c82514

--- src/chromiumos/tast/local/bundles/cros/video/lib/cpu/cpu.go
+++ src/chromiumos/tast/local/bundles/cros/video/lib/cpu/cpu.go
@@ -7,6 +7,8 @@ package cpu
 
 import (
 	"context"
+	"io/ioutil"
+	"path/filepath"
 	"time"
 
 	"github.com/shirou/gopsutil/cpu"
@@ -41,6 +43,8 @@ func WaitForIdle(ctx context.Context, timeout time.Duration, maxUsage float64) e
 // MeasureUsage measures utilization across all CPUs during duration.
 // Returns a percentage in the range [0.0, 100.0].
 func MeasureUsage(ctx context.Context, duration time.Duration) (float64, error) {
+	// Get the total time the CPU spent in different states (read from
+	// /proc/stat on linux machines).
 	statBegin, err := getStat()
 	if err != nil {
 		return 0, err
@@ -52,6 +56,11 @@ func MeasureUsage(ctx context.Context, duration time.Duration) (float64, error) 
 		return 0, ctx.Err()
 	}
 
+	// Get the total time the CPU spent in different states again. By looking at
+	// the difference with the values we got earlier, we can calculate the time
+	// the processor was idle. The gopsutil library also has a function that
+	// does this directly, but unfortunately we can't use it as that function
+	// doesn't abort when the timeout in ctx is exceeded.
 	statEnd, err := getStat()
 	if err != nil {
 		return 0, err
@@ -76,4 +85,57 @@ func getStat() (*cpu.TimesStat, error) {
 		return nil, err
 	}
 	return &times[0], nil
+}
+
+// DisableCPUFrequencyScaling disables frequency scaling. All CPU cores will be
+// set to always run at their maximum frequency. A function is returned so the
+// caller can restore the original CPU frequency scaling configuration.
+// Depending on the platform different mechanisms are present:
+// - Most platforms use the scaling_governor to control CPU frequency scaling.
+// - Some platforms (e.g. Dru) use a different CPU frequency scaling governor.
+// - Some Intel-based platforms (e.g. Eve and Nocturne) ignore the values set
+//   in the scaling_governor, and instead use the intel_pstate application to
+//   control CPU frequency scaling.
+func DisableCPUFrequencyScaling(ctx context.Context) (func() error, error) {
+	origConfig := make(map[string]string)
+	optimizedConfig := make(map[string]string)
+	for glob, value := range map[string]string{
+		"/sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_governor": "performance",
+		"/sys/class/devfreq/devfreq[0-9]*/governor":                  "performance",
+		"/sys/devices/system/cpu/intel_pstate/no_turbo":              "1",
+		"/sys/devices/system/cpu/intel_pstate/min_perf_pct":          "100",
+		"/sys/devices/system/cpu/intel_pstate/max_perf_pct":          "100",
+	} {
+		// Resolve paths containing wildcards.
+		paths, err := filepath.Glob(glob)
+		if err != nil {
+			return nil, err
+		}
+		for _, path := range paths {
+			origValue, err := ioutil.ReadFile(path)
+			if err != nil {
+				return nil, err
+			}
+			origConfig[path] = string(origValue)
+			optimizedConfig[path] = value
+		}
+	}
+
+	undo := func() error { return applyConfig(origConfig) }
+	if err := applyConfig(optimizedConfig); err != nil {
+		undo()
+		return nil, err
+	}
+	return undo, nil
+}
+
+// applyConfig applies the specified frequency scaling configuration. A map of
+// path-value pairs need to be provided.
+func applyConfig(config map[string]string) error {
+	for path, value := range config {
+		if err := ioutil.WriteFile(path, []byte(value), 0644); err != nil {
+			return err
+		}
+	}
+	return nil
 }
