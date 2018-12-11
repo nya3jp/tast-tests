f9b127e67b646dc7ce4d2af289858c8e007d4c7b
Brian Norris <briannorris@chromium.org>
2018-12-10 17:24:49 -0800

    tast-tests: ShillStability: Ignore shill children
    If network.ShillStability runs very soon after shill starts (or
    restarts), shill could still be forking new processes. Those processes
    would briefly (fork/exec) also be considered instances of
    /usr/bin/shill. Filter these false matches by looking for parent PIDs.
    For parents/children/siblings, only bother with the parent. In all other
    cases (too many / too few shill processes), still Fail.
    
    BUG=chromium:904117
    TEST=network.ShillStability
    
    Change-Id: I67339aa2833d081def7c50081bbf46a11f9a5d68

--- src/chromiumos/tast/local/bundles/cros/network/shill_stability.go
+++ src/chromiumos/tast/local/bundles/cros/network/shill_stability.go
@@ -18,7 +18,6 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func: ShillStability,
 		Desc: "Checks that shill isn't respawning",
-		Attr: []string{"informational"},
 	})
 }
 
@@ -28,8 +27,8 @@ func ShillStability(ctx context.Context, s *testing.State) {
 		stabilityDuration = 20 * time.Second
 	)
 
-	// Returns PID of shill. Calls s.Fatal if we don't find exactly 1 shill
-	// process.
+	// Returns PID of main shill process. Calls s.Fatal if we can't find
+	// shill, or there are too many non-parent/child instances.
 	getPID := func() int {
 		const shillExecPath = "/usr/bin/shill"
 
@@ -38,16 +37,41 @@ func ShillStability(ctx context.Context, s *testing.State) {
 			s.Fatal("Failed to get process list: ", err)
 		}
 
-		var pids []int
+		var shillProc *process.Process
 		for _, proc := range all {
-			if exe, err := proc.Exe(); err == nil && exe == shillExecPath {
-				pids = append(pids, int(proc.Pid))
+			exe, err := proc.Exe()
+			if err != nil || exe != shillExecPath {
+				continue
 			}
+			ppid1, err := proc.Ppid()
+			if err != nil {
+				continue
+			}
+			if shillProc == nil {
+				shillProc = proc
+				continue
+			}
+			// We're a child of shill, so skip.
+			if ppid1 == shillProc.Pid {
+				continue
+			}
+			ppid2, err := shillProc.Ppid()
+			// Other process is gone now, or we're the parent.
+			if err != nil || ppid2 == proc.Pid {
+				shillProc = proc
+				continue
+			}
+			// Siblings: we'll find the common parent later.
+			if ppid1 == ppid2 {
+				continue
+			}
+			// Found 2 that weren't parent/child.
+			s.Fatalf("Found 2 shill processes: %v, %v", shillProc.Pid, proc.Pid)
 		}
-		if len(pids) != 1 {
-			s.Fatalf("Found %v shill processes (%v); want 1", len(pids), pids)
+		if shillProc == nil {
+			s.Fatal("Could not find shill")
 		}
-		return pids[0]
+		return int(shillProc.Pid)
 	}
 
 	pid := getPID()
