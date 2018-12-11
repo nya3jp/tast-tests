dee66d06af6ff3db37964834e8c90e0311f7252d
Daniel Erat <derat@chromium.org>
2018-12-10 18:28:34 -0800

    tast-tests: Add security.ExecStack test.
    Port the security_RuntimeExecStack Autotest test to a new
    security.ExecStack Tast test.
    
    BUG=chromium:877733
    TEST=ran security.ExecStack on kevin and caroline
    
    Change-Id: I4b51c50f86fbbd14c89b54c24f294a2b57f3e13b

---
+++ src/chromiumos/tast/local/bundles/cros/security/exec_stack.go
@@ -0,0 +1,79 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+
+	"github.com/shirou/gopsutil/process"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ExecStack,
+		Desc: "Checks that no running processes have executable stacks",
+		Attr: []string{"informational"},
+	})
+}
+
+func ExecStack(ctx context.Context, s *testing.State) {
+	// Strip repeated spaces to make maps more readable.
+	spaceRegexp := regexp.MustCompile("  +")
+
+	checkMaps := func(pid int32, exe string) error {
+		// Ignore errors, which likely indicate that the process went away.
+		b, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/maps", pid))
+		if err != nil {
+			return nil
+		}
+
+		var numMaps, numStacks int
+		for _, line := range strings.Split(string(b), "\n") {
+			line := spaceRegexp.ReplaceAllString(strings.TrimSpace(line), " ")
+			numMaps++
+
+			if !strings.Contains(line, "[stack") {
+				continue
+			}
+			numStacks++
+
+			if parts := strings.Fields(line); len(parts) < 2 {
+				return errors.Errorf("unparsable map line %q", line)
+			} else if perms := parts[1]; len(perms) != 4 { // "rwxp"
+				return errors.Errorf("bad perm field in %q (want e.g. \"rwxp\")", line)
+			} else if !strings.Contains(perms, "w") { // sanity check
+				return errors.Errorf("non-writable stack (%q)", line)
+			} else if strings.Contains(perms, "x") {
+				return errors.Errorf("executable stack (%q)", line)
+			}
+		}
+
+		if numStacks == 0 {
+			return errors.Errorf("no stacks found among %d map(s)", numMaps)
+		}
+		return nil
+	}
+
+	procs, err := process.Processes()
+	if err != nil {
+		s.Fatal("Failed to list processes: ", err)
+	}
+	s.Logf("Checking %v processes", len(procs))
+	for _, proc := range procs {
+		// Read the exe link so we can skip kernel threads.
+		if exe, err := proc.Exe(); err == nil {
+			if err := checkMaps(proc.Pid, exe); err != nil {
+				s.Errorf("Process %v (%v): %v", proc.Pid, exe, err)
+			}
+		}
+	}
+}
