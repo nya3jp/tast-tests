45103d9b2b197e380d926b532b3e30ac80a0a081
Satoshi Niwa <niwa@google.com>
2018-12-13 16:00:06 +0900

    New tast test for checking first_api_level property
    Forgetting to set first_api_level for a new device is really bad
    because it causes us to overlook failures of CTS guarded by
    first_api_level until the last minute. Adding this test so we never
    forget to set this property.
    
    BUG=b:113642032
    TEST=tast run arc.BuildProperty
    
    Change-Id: I3f76348d2a02254f4e5a1f9ca26574643225503f

---
+++ src/chromiumos/tast/local/bundles/cros/arc/build_properties.go
@@ -0,0 +1,85 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         BuildProperties,
+		Desc:         "Checks important properties such as first_api_level",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+		Timeout:      4 * time.Minute,
+	})
+}
+
+const apiLevelN = "25"
+
+// Map of board name -> expected first API level for upreved boards.
+// First API level is expected to be the same as current SDK version if the
+// board name doesn't exist in this map.
+var expectedFirstAPILevelMap = map[string]string{
+	"caroline":         apiLevelN,
+	"caroline-arcnext": apiLevelN,
+	"eve":              apiLevelN,
+	"eve-arcnext":      apiLevelN,
+	"kevin":            apiLevelN,
+	"kevin-arcnext":    apiLevelN,
+	"nautilus":         apiLevelN,
+	"scarlet":          apiLevelN,
+	"scarlet-arcnext":  apiLevelN,
+	"soraka":           apiLevelN,
+}
+
+func BuildProperties(ctx context.Context, s *testing.State) {
+	const (
+		propertyBoard         = "ro.product.board"
+		propertyFirstAPILevel = "ro.product.first_api_level"
+		propertySDKVersion    = "ro.build.version.sdk"
+	)
+
+	cr, err := chrome.New(ctx, chrome.ARCEnabled())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	board := getProperty(ctx, s, a, propertyBoard)
+
+	expectedFirstAPILevel, ok := expectedFirstAPILevelMap[board]
+	if !ok {
+		expectedFirstAPILevel = getProperty(ctx, s, a, propertySDKVersion)
+	}
+
+	firstAPILevel := getProperty(ctx, s, a, propertyFirstAPILevel)
+	if firstAPILevel != expectedFirstAPILevel {
+		s.Fatalf("%v property is %q; want %q", propertyFirstAPILevel,
+			firstAPILevel, expectedFirstAPILevel)
+	}
+}
+
+func getProperty(ctx context.Context, s *testing.State, a *arc.ARC,
+	propertyName string) string {
+	out, err := a.Command(ctx, "getprop", propertyName).Output()
+	if err != nil {
+		s.Fatalf("Failed to get %q: %v", propertyName, err)
+	}
+	return strings.TrimSpace(string(out))
+}
