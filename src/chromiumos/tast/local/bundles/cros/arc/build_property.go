ecbb93b30a0ed6d828c44de1deb122e092ddbe2e
Satoshi Niwa <niwa@google.com>
2018-12-13 16:00:06 +0900

    New tast test for checking first_api_level property
    Forgetting to set first_api_level for a new device is really bad
    because it causes us to overlook failures of CTS guarded by
    first_api_level until the last minute. Adding this test so we never
    forget to set this property.
    
    BUG=b:113642032
    TEST=tast run arc.BuildProperty
    
    Change-Id: I3f76348d2a02254f4e5a1f9ca26574643225503f

---
+++ src/chromiumos/tast/local/bundles/cros/arc/build_property.go
@@ -0,0 +1,127 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         BuildProperty,
+		Desc:         "Checks important properties such as first_api_level",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func BuildProperty(ctx context.Context, s *testing.State) {
+	const (
+		propertyBoard         = "ro.product.board"
+		propertyFirstAPILevel = "ro.product.first_api_level"
+		propertySDKVersion    = "ro.build.version.sdk"
+	)
+
+	cr, err := chrome.New(ctx, chrome.ARCEnabled())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	board := getProperty(ctx, s, a, propertyBoard)
+
+	expectedFirstAPILevel, ok := expectedFirstAPILevelMap[board]
+	if !ok {
+		expectedFirstAPILevel = getProperty(ctx, s, a, propertySDKVersion)
+	}
+
+	firstAPILevel := getProperty(ctx, s, a, propertyFirstAPILevel)
+	if firstAPILevel != expectedFirstAPILevel {
+		s.Fatalf("%v property is %q; want %q", propertyFirstAPILevel,
+			firstAPILevel, expectedFirstAPILevel)
+	}
+}
+
+func getProperty(ctx context.Context, s *testing.State, a *arc.ARC,
+	propertyName string) string {
+	out, err := a.Command(ctx, "getprop", propertyName).Output()
+	if err != nil {
+		s.Fatalf("Failed to get %q: %v", propertyName, err)
+	}
+	return strings.TrimSpace(string(out))
+}
+
+const (
+	apiLevelN = "25"
+	apiLevelP = "28"
+)
+
+// Map of board name -> expected first API level for upreved boards.
+// First API level is expected to be the same as current SDK version if the
+// board name doesn't exist in this map.
+var expectedFirstAPILevelMap = map[string]string{
+	"atlas":    apiLevelP,
+	"grunt":    apiLevelP,
+	"nocturne": apiLevelP,
+	"octopus":  apiLevelP,
+	"rammus":   apiLevelP,
+
+	"asuka":                   apiLevelN,
+	"auron_paine":             apiLevelN,
+	"auron_yuna":              apiLevelN,
+	"banon":                   apiLevelN,
+	"bob":                     apiLevelN,
+	"caroline":                apiLevelN,
+	"caroline-ndktranslation": apiLevelN,
+	"cave":                    apiLevelN,
+	"celes":                   apiLevelN,
+	"chell":                   apiLevelN,
+	"coral":                   apiLevelN,
+	"cyan":                    apiLevelN,
+	"edgar":                   apiLevelN,
+	"elm":                     apiLevelN,
+	"eve":                     apiLevelN,
+	"fizz":                    apiLevelN,
+	"gandof":                  apiLevelN,
+	"hana":                    apiLevelN,
+	"kefka":                   apiLevelN,
+	"kevin":                   apiLevelN,
+	"lars":                    apiLevelN,
+	"lulu":                    apiLevelN,
+	"nami":                    apiLevelN,
+	"nautilus":                apiLevelN,
+	"pyro":                    apiLevelN,
+	"reef":                    apiLevelN,
+	"reks":                    apiLevelN,
+	"relm":                    apiLevelN,
+	"samus":                   apiLevelN,
+	"sand":                    apiLevelN,
+	"scarlet":                 apiLevelN,
+	"sentry":                  apiLevelN,
+	"setzer":                  apiLevelN,
+	"snappy":                  apiLevelN,
+	"soraka":                  apiLevelN,
+	"terra":                   apiLevelN,
+	"ultima":                  apiLevelN,
+	"veyron_fievel":           apiLevelN,
+	"veyron_jerry":            apiLevelN,
+	"veyron_mighty":           apiLevelN,
+	"veyron_minnie":           apiLevelN,
+	"veyron_tiger":            apiLevelN,
+	"wizpig":                  apiLevelN,
+}
