32bb0748e07f099dc09b626d4a48338948537a01
Hirokazu Honda <hiroh@chromium.org>
2018-12-14 15:34:34 +0900

    video: Add video_ChromeRTCDecodeAccelUsed and video_ChromeRTCEncodeAccelUsed
    BUG=chromium:915143
    TEST=tast run video.WebRTCDecodeAccelUsedVP8
    TEST=tast run video.WebRTCEncodeAccelUsedVP8
    
    Change-Id: Icb58aeaa5fcaef627ec7f031b1b6acc9f8bad7b0

---
+++ src/chromiumos/tast/local/bundles/cros/video/webrtc_encode_accel_used_vp8.go
@@ -0,0 +1,28 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/bundles/cros/video/lib/constants"
+	"chromiumos/tast/local/bundles/cros/video/webrtc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WebRTCEncodeAccelUsedVP8,
+		Desc:         "Checks HW encoding used for WebRTC/VP8",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login", caps.HWEncodeVP8},
+		Data:         []string{"crowd720_25frames.y4m", "loopback.html"},
+	})
+}
+
+func WebRTCEncodeAccelUsedVP8(ctx context.Context, s *testing.State) {
+	webrtc.RunWebRTCVideo(ctx, s, "crowd720_25frames.y4m", constants.RTCVEInitStatus, constants.RTCVEInitSuccess)
+}
