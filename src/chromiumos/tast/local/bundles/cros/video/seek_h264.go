f3448a00d83f83712173a69c1b29889301d44d47
Alexandre Courbot <acourbot@chromium.org>
2018-12-14 15:42:25 +0900

    [WIP] tast-tests: video: port video_VideoSeek.* to Tast
    BUG=chromium:911095
    TEST=tast run hana video.Seek*
    
    Change-Id: Idcce8dfc01eb3ffb423b65e9f531e4a668fe5583

---
+++ src/chromiumos/tast/local/bundles/cros/video/seek_h264.go
@@ -0,0 +1,29 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/play"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SeekH264,
+		Desc: "Verifies that H.264 non-resolution changing seek works in Chrome",
+		Attr: []string{"informational"},
+		// "chrome_internal" is needed because H.264 is a proprietary codec.
+		SoftwareDeps: []string{"chrome_login", "chrome_internal"},
+		Data:         []string{"video_seek.mp4", "video.html"},
+	})
+}
+
+// SeekH264 plays a non-resolution changing H264 file with
+// Chrome and checks that it can safely be seeked into.
+func SeekH264(ctx context.Context, s *testing.State) {
+	play.TestSeek(ctx, s, "video_seek.mp4")
+}
---
+++ src/chromiumos/tast/local/bundles/cros/video/seek_vp8.go
@@ -0,0 +1,28 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/play"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SeekVP8,
+		Desc:         "Verifies that vp8 non-resolution changing seek works in Chrome",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+		Data:         []string{"video_seek.webm", "video.html"},
+	})
+}
+
+// SeekVP8 plays a non-resolution changing VP8 file with
+// Chrome and checks that it can safely be seeked into.
+func SeekVP8(ctx context.Context, s *testing.State) {
+	play.TestSeek(ctx, s, "video_seek.webm")
+}
