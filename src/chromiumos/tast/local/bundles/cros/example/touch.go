aa88cd55c68ff26252ab33dc1fada15b9bf47488
Ricardo Quesada <ricardoq@google.com>
2018-12-14 17:28:21 -0800

    tast: add Touch event support to event.
    Add Multi Touch support to event & device.
    Add example that shows how to the use the API.
    Split intput.go in three: raw.go, keyboard.go and touch.go
    Split intput_test.go in two: keyboard_test.go and touch_test.go
    Add touch.go example.
    Add unittest.
    
    This CL adds the needed functionality that will be used to create
    move/resize/drag tests for ARC++.
    
    Limitations/scope: This CL does not try to implement the full
    multi-touch specification. Only the one needed to perform certain
    tests.
    
    BUG=chromium:916174
    TEST=tast run DUT example.Keyboard
    TEST=tast run DUT example.Touch
    TEST=fast_build.sh -T
    TEST=tested on Eve, Nocture, Caroline, Kevin and Scarlet. Works as expected.
    Change-Id: Ic7cca5d085e31f28d8909d9ceebff02bbbf02ff7

---
+++ src/chromiumos/tast/local/bundles/cros/example/touch.go
@@ -0,0 +1,180 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE
+
+package example
+
+import (
+	"context"
+	"math"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Touch,
+		Desc: "Demonstrates injecting touch events",
+		// TODO(derat): Remove "disabled" if/when there's a way to depend on an internal keyboard.
+		Attr:         []string{"disabled", "informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func Touch(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to log in: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to open connection: ", err)
+	}
+	defer tconn.Close()
+
+	info, err := display.GetInternalInfo(ctx, tconn)
+	if err != nil {
+		s.Fatal("No display: ", err)
+	}
+
+	// TODO(ricardoq): This page might change/break in the future. If so, a built-in
+	// HTML page that accepts drawing should be used. Additionally, Kleki seems to ignore
+	// the 2nd & last events when drawing splines. But for the purpose of showing how
+	// to use the API is good enough.
+	conn, err := cr.NewConn(ctx, "http://kleki.com")
+	if err != nil {
+		s.Fatal("Failed to open connection: ", err)
+	}
+	defer conn.Close()
+
+	if err := conn.WaitForExpr(ctx, "document.getElementsByTagName('canvas').length > 0"); err != nil {
+		s.Fatal("Timed out waiting for page load: ", err)
+	}
+
+	s.Log("Finding and opening touchscreen device")
+	// It is possible to send raw events to the Touchscreen type. But it is recommended to
+	// use the Touchscreen.TouchEventWriter struct since it already has functions to manipulate
+	// Touch events.
+	tsw, err := input.Touchscreen(ctx)
+	if err != nil {
+		s.Fatal("Failed to open touchscreen device: ", err)
+	}
+	defer tsw.Close()
+
+	// Touchscreen bounds: The size of the touchscreen might not be the same
+	// as the display size. In fact, might be even up to 4x bigger.
+	touchWidth := tsw.Width()
+	touchHeight := tsw.Height()
+
+	// Display bounds
+	displayWidth := float64(info.Bounds.Width)
+	displayHeight := float64(info.Bounds.Height)
+
+	pixelToTouchFactorX := float64(touchWidth) / displayWidth
+	pixelToTouchFactorY := float64(touchHeight) / displayHeight
+
+	centerX := displayWidth * pixelToTouchFactorX / 2
+	centerY := displayHeight * pixelToTouchFactorY / 2
+
+	stw, err := tsw.NewSingleTouchWriter()
+	if err != nil {
+		s.Fatal("Could not get a new TouchEventWriter: ", err)
+	}
+	defer stw.Close()
+
+	// Draw a dotted line:
+	// SingleTouchEventWriter is being reused for the 15 dots. The event is "ended" after each touch.
+	// "End" is equivalent as lifting the finger from the touchscreen.
+	// Thus generating a "dotted" line, instead of continuos one.
+	for i := 0; i < 15; i++ {
+		// Values must be in "touchscreen coordinates", not pixel coordinates.
+		stw.Move(input.TouchCoord(centerX+float64(i)*50.0), input.TouchCoord(centerY+float64(i)*50.0))
+		stw.End()
+		sleep(ctx, 100*time.Millisecond)
+	}
+
+	// Delay to make the test visually more pleasing.
+	sleep(ctx, 500*time.Millisecond)
+
+	// Draw a circle:
+	// Draws a circle with 120 touch events. The touch event is moved to
+	// 120 different locations generating a continuous circle.
+	stw, err = tsw.NewSingleTouchWriter()
+	if err != nil {
+		s.Fatal("Could not create TouchEventWriter: ", err)
+	}
+	defer stw.Close()
+
+	const radius = 200 // in pixels
+	const segments = 120
+	for i := 0; i < segments; i++ {
+		rads := 2.0*math.Pi*(float64(i)/segments) + math.Pi
+		x := radius * pixelToTouchFactorX * math.Cos(rads)
+		y := radius * pixelToTouchFactorY * math.Sin(rads)
+		stw.Move(input.TouchCoord(centerX+x), input.TouchCoord(centerY+y))
+		if err := sleep(ctx, 15*time.Millisecond); err != nil {
+			s.Fatal("Timeout: ", err)
+		}
+	}
+	// And finally "end" (lift the finger) the line.
+	stw.End()
+
+	// Delay to make the test visually more pleasing.
+	if err := sleep(ctx, 500*time.Millisecond); err != nil {
+		s.Fatal("Timeout: ", err)
+	}
+
+	// Multitouch test: Zoom out + zoom in
+	// Get a multitouch writer for two touches.
+	mtw, err := tsw.NewMultiTouchWriter(2)
+	if err != nil {
+		s.Fatal("Could not get a new TouchEventWriter: ", err)
+	}
+	defer mtw.Close()
+
+	// Get the individual TouchState
+	ts0 := mtw.TouchState(0)
+	ts1 := mtw.TouchState(1)
+
+	// Zoom out
+	for i := 5; i < 100; i++ {
+		deltaX := float64(i) * pixelToTouchFactorX
+		deltaY := float64(i) * pixelToTouchFactorY
+
+		ts0.SetPos(input.TouchCoord(centerX-deltaX), input.TouchCoord(centerY-deltaY))
+		ts1.SetPos(input.TouchCoord(centerX+deltaX), input.TouchCoord(centerY+deltaY))
+		mtw.Send()
+		if err := sleep(ctx, 15*time.Millisecond); err != nil {
+			s.Fatal("Timeout: ", err)
+		}
+	}
+
+	// Zoom in
+	for i := 100; i > 15; i-- {
+		deltaX := float64(i) * pixelToTouchFactorX
+		deltaY := float64(i) * pixelToTouchFactorY
+
+		ts0.SetPos(input.TouchCoord(centerX-deltaX), input.TouchCoord(centerY-deltaY))
+		ts1.SetPos(input.TouchCoord(centerX+deltaX), input.TouchCoord(centerY+deltaY))
+		mtw.Send()
+		if err := sleep(ctx, 15*time.Millisecond); err != nil {
+			s.Fatal("Timeout: ", err)
+		}
+	}
+	mtw.End()
+}
+
+func sleep(ctx context.Context, t time.Duration) error {
+	select {
+	case <-time.After(t):
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+	return nil
+}
---
+++ src/chromiumos/tast/local/input/touch_test.go
@@ -0,0 +1,80 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package input
+
+import (
+	"bytes"
+	"reflect"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func TestEventWriterTouch(t *testing.T) {
+	const (
+		x       = 13
+		y       = 17
+		p       = 62
+		tMajor  = 4
+		tMinor  = 3
+		touchID = 12345
+	)
+
+	b := testBuffer{}
+	now := time.Unix(5, 0)
+	mw := TouchscreenEventWriter{
+		rw:            &RawEventWriter{&b, func() time.Time { return now }},
+		nextTouchID:   touchID,
+		width:         1000,
+		height:        1000,
+		maxTouches:    9,
+		maxTrackingID: 65536,
+		maxPressure:   128,
+	}
+
+	tw, err := mw.NewSingleTouchWriter()
+	if err != nil {
+		t.Fatalf("TouchEvent returned error: %v", err)
+	}
+	defer tw.Close()
+
+	tw.touches[0].absPressure = p
+	tw.touches[0].touchMajor = tMajor
+	tw.touches[0].touchMinor = tMinor
+	tw.touches[0].touchID = touchID
+
+	tw.Move(x, y)
+	tw.End()
+
+	written, err := readAllEvents(bytes.NewReader(b.buf.Bytes()))
+	if err != nil {
+		t.Error("Failed to read events: ", err)
+	}
+
+	tv := syscall.NsecToTimeval(now.UnixNano())
+	syn := eventString(tv, uint16(EV_SYN), uint16(SYN_REPORT), 0)
+	expected := []string{
+		eventString(tv, uint16(EV_ABS), uint16(ABS_MT_SLOT), 0),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_MT_TRACKING_ID), touchID),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_MT_POSITION_X), x),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_MT_POSITION_Y), y),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_MT_PRESSURE), p),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_MT_TOUCH_MAJOR), tMajor),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_MT_TOUCH_MINOR), tMinor),
+		eventString(tv, uint16(EV_KEY), uint16(BTN_TOUCH), 1),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_X), x),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_Y), y),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_PRESSURE), p),
+		syn,
+		eventString(tv, uint16(EV_ABS), uint16(ABS_MT_SLOT), 0),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_MT_TRACKING_ID), -1),
+		eventString(tv, uint16(EV_ABS), uint16(ABS_PRESSURE), 0),
+		eventString(tv, uint16(EV_KEY), uint16(BTN_TOUCH), 0),
+		syn,
+	}
+	if !reflect.DeepEqual(written, expected) {
+		t.Errorf("Wrote %v; want %v", written, expected)
+	}
+}
