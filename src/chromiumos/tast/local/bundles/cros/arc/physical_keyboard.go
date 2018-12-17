a32fde5e233cc157a1c621a3ca965a4843dec7d7
Tetsui Ohkubo <tetsui@chromium.org>
2018-12-17 17:13:24 +0900

    Add arc.PhysicalKeyboard test.
    This is a port of cheets_KeyboardTest autotest. It checks if an Android
    app text field successfully accepts physical keyboard input from Chrome.
    
    BUG=None
    TEST=tast run eve-DUT arc.PhysicalKeyboard
    
    Change-Id: I0efd9ee78095c70c90bd3643e2c0df03fb6454ad

---
+++ src/chromiumos/tast/local/bundles/cros/arc/physical_keyboard.go
@@ -0,0 +1,86 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PhysicalKeyboard,
+		Desc:         "Checks physical keyboard works on Android",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+		Data:         []string{"ArcKeyboardTest.apk"},
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func PhysicalKeyboard(ctx context.Context, s *testing.State) {
+	const (
+		apk = "ArcKeyboardTest.apk"
+		pkg = "org.chromium.arc.testapp.keyboard"
+		cls = "org.chromium.arc.testapp.keyboard.MainActivity"
+
+		fieldID = "org.chromium.arc.testapp.keyboard:id/text"
+
+		keystrokes = "google"
+	)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	d, err := ui.NewDevice(ctx, a)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	defer d.Close()
+
+	s.Log("Starting app")
+
+	if err := a.Install(ctx, s.DataPath(apk)); err != nil {
+		s.Fatal("Failed installing app: ", err)
+	}
+
+	if err := a.Command(ctx, "am", "start", "-W", pkg+"/"+cls).Run(); err != nil {
+		s.Fatal("Failed starting app: ", err)
+	}
+
+	field := d.Object(ui.ID(fieldID))
+	if err := field.WaitForExists(ctx); err != nil {
+		s.Fatal("Failed to find field: ", err)
+	}
+	if err := field.Click(ctx); err != nil {
+		s.Fatal("Failed to click field: ", err)
+	}
+	if err := field.SetText(ctx, ""); err != nil {
+		s.Fatal("Failed to empty field: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+	if err := kb.Type(ctx, keystrokes); err != nil {
+		s.Fatalf("Failed to type %q: %v", keystrokes, err)
+	}
+
+	if actual, err := field.GetText(ctx); err != nil {
+		s.Fatal("Failed to get text: ", err)
+	} else if actual != keystrokes {
+		s.Errorf("Got input %q from field after typing %q", actual, keystrokes)
+	}
+}
