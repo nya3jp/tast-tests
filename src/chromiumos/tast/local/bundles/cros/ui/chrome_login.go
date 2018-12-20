e6fb4909abfa0c652ee83a391d6c3552bb9b0d94
Hidehiko Abe <hidehiko@chromium.org>
2018-12-21 04:55:24 +0900

    tast-tests: Watch "stopped" signal on Chrome logout.
    This merges login_LoginSuccess test into ui.ChromeLogin.
    The missing part in ui.ChromeLogin was observing "stopped",
    so this adds it.
    
    BUG=chromium:899758
    TEST=Ran locally.
    
    Change-Id: I15e770c713c5c0854520df7de9aacf9e291d8c98

--- src/chromiumos/tast/local/bundles/cros/ui/chrome_login.go
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_login.go
@@ -12,6 +12,7 @@ import (
 
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/session"
+	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
 
@@ -29,48 +30,69 @@ func ChromeLogin(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to connect session_manager: ", err)
 	}
-	sw, err := sm.WatchSessionStateChanged(ctx, "started")
+	func() {
+		sw, err := sm.WatchSessionStateChanged(ctx, "started")
+		if err != nil {
+			s.Fatal("Failed to watch for D-Bus signals: ", err)
+		}
+		defer sw.Close(ctx)
+
+		cr, err := chrome.New(ctx)
+		if err != nil {
+			s.Fatal("Chrome login failed: ", err)
+		}
+		defer cr.Close(ctx)
+
+		s.Log("Waiting for SessionStateChanged \"started\" D-Bus signal from session_manager")
+		select {
+		case <-sw.Signals:
+			s.Log("Got SessionStateChanged signal")
+		case <-ctx.Done():
+			s.Fatal("Didn't get SessionStateChanged signal: ", ctx.Err())
+		}
+
+		const expected = "Hooray, it worked!"
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			io.WriteString(w, expected)
+		}))
+		defer server.Close()
+
+		conn, err := cr.NewConn(ctx, server.URL)
+		if err != nil {
+			s.Fatal("Creating renderer failed: ", err)
+		}
+		defer conn.Close()
+
+		if err := conn.WaitForExpr(ctx, "document.readyState === 'complete'"); err != nil {
+			s.Fatal("Waiting load failed: ", err)
+		}
+
+		var actual string
+		if err = conn.Eval(ctx, "document.documentElement.innerText", &actual); err != nil {
+			s.Fatal("Getting page content failed: ", err)
+		}
+		s.Logf("Got content %q", actual)
+		if actual != expected {
+			s.Fatalf("Expected page content %q, got %q", expected, actual)
+		}
+	}()
+
+	sw, err := sm.WatchSessionStateChanged(ctx, "stopped")
 	if err != nil {
 		s.Fatal("Failed to watch for D-Bus signals: ", err)
 	}
 	defer sw.Close(ctx)
 
-	cr, err := chrome.New(ctx)
-	if err != nil {
-		s.Fatal("Chrome login failed: ", err)
+	// Emulate logout.
+	if err = upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Fatal("Chrome logout failed: ", err)
 	}
-	defer cr.Close(ctx)
 
-	s.Log("Waiting for SessionStateChanged \"started\" D-Bus signal from session_manager")
+	s.Log("Waiting for SessionStateChanged \"stopped\" D-Bus signal from session_manager")
 	select {
 	case <-sw.Signals:
 		s.Log("Got SessionStateChanged signal")
 	case <-ctx.Done():
 		s.Fatal("Didn't get SessionStateChanged signal: ", ctx.Err())
-	}
-
-	const expected = "Hooray, it worked!"
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, expected)
-	}))
-	defer server.Close()
-
-	conn, err := cr.NewConn(ctx, server.URL)
-	if err != nil {
-		s.Fatal("Creating renderer failed: ", err)
-	}
-	defer conn.Close()
-
-	if err := conn.WaitForExpr(ctx, "document.readyState === 'complete'"); err != nil {
-		s.Fatal("Waiting load failed: ", err)
-	}
-
-	var actual string
-	if err = conn.Eval(ctx, "document.documentElement.innerText", &actual); err != nil {
-		s.Fatal("Getting page content failed: ", err)
-	}
-	s.Logf("Got content %q", actual)
-	if actual != expected {
-		s.Fatalf("Expected page content %q, got %q", expected, actual)
 	}
 }
