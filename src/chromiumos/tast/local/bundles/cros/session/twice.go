d90b7f93ed6a5eda17b326a8a20cbdacde7b0ed1
Hidehiko Abe <hidehiko@chromium.org>
2018-12-21 03:19:28 +0900

    tast-tests: Introduce session.Twice.
    This is ported from login_SameSessionTwice.
    
    BUG=chromium:899758
    TEST=Ran locally.
    
    Change-Id: I43df0bf285ac9322971ff1d4044c2bca9656fa33

---
+++ src/chromiumos/tast/local/bundles/cros/session/twice.go
@@ -0,0 +1,58 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package session
+
+import (
+	"context"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/local/session"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Twice,
+		Desc:         "Ensures that the session_manager won't start the smae session twice",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func Twice(ctx context.Context, s *testing.State) {
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Fatal("Failed to restart session_manager: ", err)
+	}
+
+	const user = "first_user@nowhere.com"
+
+	// Create clean vault.
+	if err := cryptohome.RemoveVault(ctx, user); err != nil {
+		s.Fatalf("Failed to remove the vault for %s: %v", user, err)
+	}
+	if err := cryptohome.CreateVault(ctx, user, ""); err != nil {
+		s.Fatalf("Failed to create a vault for %s: %v", user, err)
+	}
+	defer cryptohome.RemoveVault(ctx, user)
+
+	// Start the first session.
+	sm, err := session.NewSessionManager(ctx)
+	if err != nil {
+		s.Fatal("Failed to create session_manager binding: ", err)
+	}
+	if err = sm.StartSession(ctx, user, ""); err != nil {
+		s.Fatalf("Failed to start new session for %s: %v", user, err)
+	}
+
+	// Second StartSession() should be failed with SessionExists.
+	if err = sm.StartSession(ctx, user, ""); err == nil {
+		s.Fatalf("Unexpectedly succeeded to start session for %s twice", user)
+	} else if e, ok := err.(dbus.Error); !ok || e.Name != "org.chromium.SessionManagerInterface.SessionExists" {
+		s.Error("Unexpected error: ", err)
+	}
+}
