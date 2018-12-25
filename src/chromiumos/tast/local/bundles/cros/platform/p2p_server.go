61ed52835748f61dd103c9e6520ed2f9a24f6334
Shuhei Takahashi <nya@chromium.org>
2018-12-25 14:29:10 +0900

    platform: Add P2P tests.
    These Tast tests are ported from Autotest:
    
    - platform.P2PClient (p2p_ConsumeFiles):
      Tests that Chromium OS can download files from local network peers
      with p2p-client by running fake P2P servers.
    - platform.P2PServer (p2p_ServeFiles, p2p_ShareFiles):
      Tests that Chromium OS can serve files to local network peers with
      p2p-server by running a fake P2P client.
    
    The most challenging part of these tests is that the P2P protocol
    uses multicasts for DNS-SD, but multicasts do not work properly with
    loopback network interfaces. The original P2P autotests avoided this
    problem by using TUN/TAP network devices and emulating the network
    stack in Python, which is incredible but a lot of work.
    
    New Tast tests use network namespaces and virtual network interfaces
    instead to tackle with the problem. We run avahi-daemon in the
    network namespace, and connect it to the default network namespace
    (where Tast test process run) with a pair of virtual network
    interfaces. This way we can make sure multicast packets go through
    routing instead of loopback. See p2p.go for a diagram illustrating
    the configuration.
    
    BUG=chromium:916903
    TEST=tast run DUT platform.P2P{Client,Server}
    CQ-DEPEND=CL:1390794
    CQ-DEPEND=CL:1390795
    
    Change-Id: I33fc31484805e74755370c6aa9a6863693e19be4

---
+++ src/chromiumos/tast/local/bundles/cros/platform/p2p_server.go
@@ -0,0 +1,189 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/hashicorp/mdns"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/platform/p2p"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: P2PServer,
+		Desc: "Tests that Chromium OS can serve files to local network peers with p2p-server",
+		Attr: []string{"informational"},
+	})
+}
+
+// queryP2PServices queries available P2P services.
+func queryP2PServices(ctx context.Context, timeout time.Duration) ([]*mdns.ServiceEntry, error) {
+	if dl, ok := ctx.Deadline(); ok {
+		ctxTimeout := dl.Sub(time.Now())
+		if ctxTimeout < timeout {
+			timeout = ctxTimeout
+		}
+	}
+
+	ch := make(chan *mdns.ServiceEntry)
+	var err error
+	go func() {
+		defer close(ch)
+		params := &mdns.QueryParam{
+			Timeout: timeout,
+			Domain:  "local",
+			Service: p2p.ServiceType,
+			Entries: ch,
+		}
+		err = mdns.Query(params)
+	}()
+
+	var srvs []*mdns.ServiceEntry
+	for srv := range ch {
+		srvs = append(srvs, srv)
+	}
+	return srvs, err
+}
+
+// waitP2PService waits for a P2P service to be ready. It is an error if there are more than
+// a single P2P service.
+func waitP2PService(ctx context.Context) (*mdns.ServiceEntry, error) {
+	var srvs []*mdns.ServiceEntry
+	for wait := 500 * time.Millisecond; len(srvs) == 0; wait *= 2 {
+		var err error
+		srvs, err = queryP2PServices(ctx, wait)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if len(srvs) > 1 {
+		return nil, errors.New("multiple services found")
+	}
+	return srvs[0], nil
+}
+
+func generateRandomFile(size int) ([]byte, error) {
+	f, err := os.Open("/dev/urandom")
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	out := make([]byte, size)
+	if _, err := io.ReadFull(f, out); err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func P2PServer(fullCtx context.Context, s *testing.State) {
+	// Shorten the timeout to allow some time for cleanup.
+	ctx, cancel := ctxutil.Shorten(fullCtx, 10*time.Second)
+	defer cancel()
+
+	if err := p2p.SetUp(ctx); err != nil {
+		s.Fatal("Failed to set up: ", err)
+	}
+	defer func() {
+		if err := p2p.CleanUp(fullCtx); err != nil {
+			s.Error("Failed to clean up: ", err)
+		}
+	}()
+
+	if err := upstart.EnsureJobRunning(ctx, "p2p"); err != nil {
+		s.Fatal("Failed to start p2p: ", err)
+	}
+
+	// Find the P2P service and check that the announced information is correct.
+	s.Log("Discovering P2P service")
+	srv, err := waitP2PService(ctx)
+	if err != nil {
+		s.Fatal("Failed to find P2P service: ", err)
+	}
+
+	s.Logf("P2P service found at %s:%d; %s", srv.Addr.String(), srv.Port, srv.Info)
+
+	if srv.Addr.String() != p2p.IsolatedNSIP {
+		s.Errorf("Service address is %s; want %s", srv.AddrV4.String(), p2p.IsolatedNSIP)
+	}
+	if srv.Port != p2p.ServicePort {
+		s.Errorf("Service port is %d; want %d", srv.Port, p2p.ServicePort)
+	}
+
+	// Share a file and check that it is advertised.
+	s.Log("Testing a new file is advertised")
+
+	const (
+		testFileBase = "somefile"
+		testFileName = "somefile.p2p"
+		testFileSize = 1234567
+		advertisedID = "id_somefile=1234567"
+	)
+	rand, err := generateRandomFile(testFileSize)
+	if err != nil {
+		s.Fatal("Failed to generate a random file: ", err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(p2p.SharedDir, testFileName), rand, 0666); err != nil {
+		s.Fatalf("Failed to save %s: %v", testFileName, err)
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		var err error
+		srv, err = waitP2PService(ctx)
+		if err != nil {
+			return err
+		}
+		for _, txt := range srv.InfoFields {
+			if txt == advertisedID {
+				return nil
+			}
+		}
+		return errors.Errorf("file not advertised; info=%s", srv.Info)
+	}, nil); err != nil {
+		s.Fatal("Failed to wait for advertisement: ", err)
+	}
+
+	s.Logf("P2P service at %s:%d; %s", srv.Addr.String(), srv.Port, srv.Info)
+
+	// Attempt to download the file, but we can't download it locally (crbug.com/309708).
+	s.Log("Testing that local download is blocked")
+	for _, host := range []string{p2p.DefaultNSIP, "127.0.0.1"} {
+		url := fmt.Sprintf("http://%s:%d/%s", host, srv.Port, testFileBase)
+		// We use curl here instead of net/http to align with the later test.
+		cmd := testexec.CommandContext(ctx, "curl", url)
+		err := cmd.Run()
+		// curl's exit code 7: Failed to connect to host.
+		if st, ok := testexec.GetWaitStatus(err); !ok {
+			s.Errorf("curl %s failed: ", url, err)
+		} else if st.ExitStatus() != 7 {
+			s.Errorf("curl %s exited with status %d; want 7 (failed to connect to host)", url, st.ExitStatus())
+		}
+	}
+
+	// Download succeeds from remote.
+	s.Log("Testing that remote download succeeds")
+	url := fmt.Sprintf("http://%s:%d/%s", p2p.DefaultNSIP, srv.Port, testFileBase)
+	cmd := testexec.CommandContext(ctx, "ip", "netns", "exec", p2p.NSName, "curl", url)
+	if out, err := cmd.Output(); err != nil {
+		cmd.DumpLog(ctx)
+		s.Error("curl failed: ", err)
+	} else if !bytes.Equal(out, rand) {
+		s.Error("Served file is corrupted")
+	}
+}
