a8f02b2a2ff6c9111045402af29cf7f75db45915
Hirokazu Honda <hiroh@chromium.org>
2018-12-25 11:23:21 +0900

    video: Add more tests and videos for tast.video.PlaybackPerf
    This CL adds the following videos for video playback performance test;
    * H264 1080P@30FPS
    * H264 1080P@60FPS
    * H264 2160P@30FPS
    * H264 2160P@60FPS
    * VP8  1080P@30FPS
    * VP8  1080P@60FPS
    * VP8  2160P@30FPS
    * VP8  2160P@60FPS
    * VP9  1080P@30FPS
    * VP9  1080P@60FPS
    * VP9  2160P@30FPS
    * VP9  2160P@60FPS
    
    All of them are 10 seconds video and have sound.
    
    BUG=chromium:890733
    TEST=tast run video.PlaybackPerf*
    Change-Id: I0d2db1050922c6bab569c83a9f2f8afba91aa6fc
    Reviewed-on: https://chromium-review.googlesource.com/1390678
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/video/playback_perf_vp8_2160p_30fps.go
@@ -0,0 +1,31 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/video/playback"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PlaybackPerfVP82160P30FPS,
+		Desc:         "Measures video playback performance with/without HW acceleration for VP8 2160p@30fps video",
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome_login"},
+		Data:         []string{"2160p_30fps_300frames.vp8.webm"},
+		// Default timeout (i.e. 2 minutes) is not enough for low-end devices.
+		Timeout: 3 * time.Minute,
+	})
+}
+
+// PlaybackPerfVP82160P30FPS plays VP8 2160P 30 FPS video and measures the performance values with/without
+// HW decoding acceleration. The values are reported to performance dashboard.
+func PlaybackPerfVP82160P30FPS(ctx context.Context, s *testing.State) {
+	playback.RunTest(ctx, s, "2160p_30fps_300frames.vp8.webm", "vp8_2160p_30fps")
+}
