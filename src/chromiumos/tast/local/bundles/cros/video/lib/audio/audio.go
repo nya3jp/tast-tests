6a169c20485294fdeb32472b9b103d9cbbe0d5c1
Hirokazu Honda <hiroh@chromium.org>
2018-12-25 12:40:49 +0900

    video: Add functions to Mute/Unmute not to make a sound during testing
    Videos in some tests has an audio. It doesn't matter whether DUT sounds or not.
    It is convenient for developers that test automatically makes DUT mute during
    the test.
    Note that it matters that chrome processes for sound, because not only we want
    to measure metrics in chrome standard video playback but also chrome video
    playback behavior is changed by the existence of sound in the video.
    
    BUG=chromium:890733, chromium:911095
    TEST=tast run video.Play*
    
    Change-Id: Ibe4895c1cfc6991beaf5672724bb8c9bf15f194c
    Reviewed-on: https://chromium-review.googlesource.com/1390677
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/video/lib/audio/audio.go
@@ -0,0 +1,32 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package audio mutes/unmutes DUT.
+package audio
+
+import (
+	"context"
+
+	"chromiumos/tast/local/testexec"
+)
+
+// Mute lets DUT be muted. That is, after Mute() is done, DUT doesn't sound when a video plays.
+func Mute(ctx context.Context) error {
+	cmd := testexec.CommandContext(ctx, "cras_test_client", "--mute", "1")
+	if err := cmd.Run(); err != nil {
+		cmd.DumpLog(ctx)
+		return err
+	}
+	return nil
+}
+
+// Unmute lets DUT be unmuted, if it is muted by Mute().
+func Unmute(ctx context.Context) error {
+	cmd := testexec.CommandContext(ctx, "cras_test_client", "--mute", "0")
+	if err := cmd.Run(); err != nil {
+		cmd.DumpLog(ctx)
+		return err
+	}
+	return nil
+}
