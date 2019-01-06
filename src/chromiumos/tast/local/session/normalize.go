60c3e9a555c340e46949fc5000f6c04ea358d268
Daniel Erat <derat@chromium.org>
2019-01-06 00:23:04 +0000

    tast-tests: Make chrome package support GAIA login.
    Add a new GAIALogin option that can be passed to chrome.New
    to indicate that real login should be performed using the
    GAIA backend.
    
    BUG=chromium:903521
    TEST=manually verified using a new test; also added unit
         tests for email-normalization code
    
    Change-Id: Ib51215fbaee1b0c2d868b2b735c06ecc86ef2cbb

---
+++ src/chromiumos/tast/local/session/normalize.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package session
+
+import (
+	"regexp"
+	"strings"
+
+	"chromiumos/tast/errors"
+)
+
+// Matches "user" or "user@domain".
+var emailRegexp = regexp.MustCompile("^([^@]+)(?:@([^@]+))?$")
+
+// NormalizeEmail normalizes the supplied email address as would be done for GAIA login.
+// The address is lowercased, periods in the username are removed, and a gmail.com domain is appended if needed.
+func NormalizeEmail(email string) (string, error) {
+	matches := emailRegexp.FindStringSubmatch(email)
+	if matches == nil {
+		return "", errors.New("not user or user@domain")
+	}
+
+	user := strings.ToLower(matches[1])
+	domain := strings.ToLower(matches[2])
+	if domain == "" || domain == "gmail.com" {
+		user = strings.Replace(user, ".", "", -1)
+		domain = "gmail.com"
+	}
+
+	return user + "@" + domain, nil
+}
