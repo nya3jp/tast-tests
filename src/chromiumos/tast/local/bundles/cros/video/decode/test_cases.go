7b55a025dc22c36c7a671b4a9fe6dab25d994fb3
Dean Liao <deanliao@chromium.org>
2019-01-09 13:10:26 +0800

    video: Migrate video_VDASanity autotest to Tast.
    Add video.VDASanity test, which is the Tast version of
    video_VDASanity, which run Chrome's binary test
    "video_decode_accelerator_unittest" with gtest
    VideoDecodeAcceleratorTest.NoCrash.
    
    BUG=chromium:920060
    TEST=tast run bob video.VDASanity
    CQ-DEPEND=CL:1401907
    
    Change-Id: I8de63ee6a50e8ae86970dbbdd6b14e8a2877f6a7

--- src/chromiumos/tast/local/bundles/cros/video/decode/test_cases.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode/test_cases.go
@@ -49,3 +49,47 @@ var Test25FPSVP92 = TestVideoData{
 	MinFPSNoRender:   150,
 	Profile:          videotype.VP9_2Prof,
 }
+
+// VDASanityBearProfile1VP9 is the test parameters of video_decode_accelerator_unittest for "vda_sanity-bear_profile1.vp9".
+var VDASanityBearProfile1VP9 = TestVideoData{
+	Name:             "vda_sanity-bear_profile1.vp9",
+	Size:             videotype.NewSize(320, 180),
+	NumFrames:        30,
+	NumFragments:     30,
+	MinFPSWithRender: 0,
+	MinFPSNoRender:   0,
+	Profile:          videotype.VP9Prof,
+}
+
+// VDASanityBearProfile2VP9 is the test parameters of video_decode_accelerator_unittest for "vda_sanity-bear_profile2.vp9".
+var VDASanityBearProfile2VP9 = TestVideoData{
+	Name:             "vda_sanity-bear_profile2.vp9",
+	Size:             videotype.NewSize(320, 180),
+	NumFrames:        30,
+	NumFragments:     30,
+	MinFPSWithRender: 0,
+	MinFPSNoRender:   0,
+	Profile:          videotype.VP9Prof,
+}
+
+// VDASanityBearProfile3VP9 is the test parameters of video_decode_accelerator_unittest for "vda_sanity-bear_profile3.vp9".
+var VDASanityBearProfile3VP9 = TestVideoData{
+	Name:             "vda_sanity-bear_profile3.vp9",
+	Size:             videotype.NewSize(320, 180),
+	NumFrames:        30,
+	NumFragments:     30,
+	MinFPSWithRender: 0,
+	MinFPSNoRender:   0,
+	Profile:          videotype.VP9Prof,
+}
+
+// VDASanityVP90217ShowExistingFrame is the test parameters of video_decode_accelerator_unittest for "vda_sanity-vp90_2_17_show_existing_frame.vp9".
+var VDASanityVP90217ShowExistingFrame = TestVideoData{
+	Name:             "vda_sanity-vp90_2_17_show_existing_frame.vp9",
+	Size:             videotype.NewSize(352, 288),
+	NumFrames:        30,
+	NumFragments:     30,
+	MinFPSWithRender: 0,
+	MinFPSNoRender:   0,
+	Profile:          videotype.VP9Prof,
+}
