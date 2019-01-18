68ae011328ca38c68e406172b740e6d1a28cf206
Sara Kato <sarakato@google.com>
2019-01-18 09:36:27 +0900

    Add slider bars tast test
    Use ChromeVox to interact with slider (by incrementing value), and check
    that this is reflected in the value of slider.
    
    TEST=tast run DUT_IP arcapp.AccessibilityEvent
    BUG=b:121359306
    Change-Id: I189ed4fafed57e55118954d065272d7cbf378a61

--- src/chromiumos/tast/local/bundles/cros/arc/accessibility_event.go
+++ src/chromiumos/tast/local/bundles/cros/arc/accessibility_event.go
@@ -81,6 +81,32 @@ func waitForElementChecked(ctx context.Context, chromeVoxConn *chrome.Conn, clas
 	return nil
 }
 
+// waitForValueFocused polls until specified UI element with spacified value (expectedValue) has focus.
+// Returns error after 30 seconds.
+func waitForValueFocused(ctx context.Context, chromeVoxConn *chrome.Conn, className string, expectedValue int) error {
+	script := fmt.Sprintf(
+		`new Promise((resolve, reject) => {
+			chrome.automation.getFocus((node) => {
+				if (node.className === '%s') {
+					resolve(node.valueForRange);
+				}
+			});
+		})`, className)
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		var gotValue int
+		if err := chromeVoxConn.EvalPromise(ctx, script, &gotValue); err != nil {
+			return err
+		}
+		if gotValue != expectedValue {
+			return errors.Errorf("%s is not increment correctly", className)
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+		return errors.Wrap(err, "failed to check if element is incremented")
+	}
+	return nil
+}
+
 // waitForElementFocused polls until specified UI element (focusClassName) has focus.
 // Returns error after 30 seconds.
 func waitForElementFocused(ctx context.Context, chromeVoxConn *chrome.Conn, focusClassName string) error {
@@ -101,6 +127,90 @@ func waitForElementFocused(ctx context.Context, chromeVoxConn *chrome.Conn, focu
 		return nil
 	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
 		return errors.Wrap(err, "failed to get current focus")
+	}
+	return nil
+}
+
+// getSeekBarValue returns the value of the currently focused seekBar.
+func getSeekBarValue(ctx context.Context, chromeVoxConn *chrome.Conn, elementClass string) (int, error) {
+	var currentValue int
+	script := fmt.Sprintf(`
+		new Promise((resolve, reject) => {
+			chrome.automation.getFocus((node) => {
+				if (node.className === '%s') {
+					resolve(node.valueForRange);
+				}
+			});
+		})`, elementClass)
+	if err := chromeVoxConn.EvalPromise(ctx, script, &currentValue); err != nil {
+		return 0, errors.Wrap(err, "could not get value of focused seekbar")
+	}
+	return currentValue, nil
+}
+
+// checkOutputLog gets the current ChromeVox log and checks that it matches with expected log.
+func checkOutputLog(ctx context.Context, chromeVoxConn *chrome.Conn, expectedOutput []string, outputFilePath string) error {
+	var gotOutput string
+	if err := chromeVoxConn.Eval(ctx, "LogStore.instance.getLogsOfType(TextLog.LogType.EVENT).toString()", &gotOutput); err != nil {
+		return errors.Wrap(err, "failed to get event log")
+	}
+
+	// Determine if output matches expected value, and write to file if it does not match.
+	if diff := getEventDiff(strings.Split(gotOutput, ","), expectedOutput); len(diff) != 0 {
+		if err := ioutil.WriteFile(outputFilePath, []byte(strings.Join(diff, "\n")), 0644); err != nil {
+			return errors.Errorf("failed to write to %q: %v", outputFilePath, err)
+		}
+	}
+	return nil
+}
+
+// focusAndIncrementElement uses ChromeVox navigation (using Tab), to navigate to the next
+// UI element (specified by elementClass, and is expected to be a seekBar).
+// ChromeVox will the interact with the seeekBar, by incrementing its value using '='.
+// Returns an error indicating the success of both actions.
+func focusAndIncrementElement(ctx context.Context, chromeVoxConn *chrome.Conn, elementClass string, expectedOutput []string, outputFilePath string, initialValue, expectedValue int) error {
+	ew, err := input.Keyboard(ctx)
+	if err != nil {
+		return errors.Wrap(err, "error with creating EventWriter from keyboard")
+	}
+	defer ew.Close()
+
+	// Ensure that ChromeVox log is cleared before proceeding.
+	if err := chromeVoxConn.EvalPromise(ctx, "LogStore.instance.clearLog()", nil); err != nil {
+		return errors.Wrap(err, "error with clearing ChromeVox Log")
+	}
+
+	// Move focus to the next UI element.
+	if err := ew.Accel(ctx, "Tab"); err != nil {
+		return errors.Wrap(err, "Accel(Tab) returned error")
+	}
+
+	// Wait for element with expected initial value to receive focus.
+	if err := waitForValueFocused(ctx, chromeVoxConn, elementClass, initialValue); err != nil {
+		return errors.Wrap(err, "timed out polling for element")
+	}
+
+	// Check initial value of seekBar.
+	currentValue, err := getSeekBarValue(ctx, chromeVoxConn, elementClass)
+	if err != nil {
+		return errors.Wrap(err, "could not get seekBar value")
+	}
+
+	if currentValue != initialValue {
+		return errors.Errorf("seekBar value was not as expected, got %d want %d", currentValue, initialValue)
+	}
+
+	// Increment value of seekBar.
+	if err := ew.Accel(ctx, "="); err != nil {
+		return errors.Wrap(err, "Accel(=) returned error")
+	}
+
+	// Check that seekbar was incremented correctly.
+	if err := waitForValueFocused(ctx, chromeVoxConn, elementClass, expectedValue); err != nil {
+		return errors.Wrap(err, "timed out polling for element incremented")
+	}
+	if err := checkOutputLog(ctx, chromeVoxConn, expectedOutput, outputFilePath); err != nil {
+		return err
 	}
 	return nil
 }
@@ -146,21 +256,30 @@ func focusAndCheckElement(ctx context.Context, chromeVoxConn *chrome.Conn, eleme
 	}
 
 	// Determine if output matches expected value, and write to file if it does not match.
-	if diff := getEventDiff(strings.Split(gotOutput, ","), expectedOutput); len(diff) != 0 {
-		if err := ioutil.WriteFile(outputFilePath, []byte(strings.Join(diff, "\n")), 0644); err != nil {
-			return errors.Wrapf(err, "failed to write to: %s", outputFilePath)
-		}
+	if err := checkOutputLog(ctx, chromeVoxConn, expectedOutput, outputFilePath); err != nil {
+		return err
 	}
 	return nil
 }
 
 func AccessibilityEvent(ctx context.Context, s *testing.State) {
 	const (
-		apkName                = "accessibility_sample.apk"
-		checkBox               = "android.widget.CheckBox"
-		toggleButton           = "android.widget.ToggleButton"
-		toggleButtonOutputFile = "accessibility_event_diff_toggle_button_output.txt"
-		checkBoxOutputFile     = "accessibility_event_diff_checkbox_output.txt"
+		apkName = "accessibility_sample.apk"
+
+		checkBox     = "android.widget.CheckBox"
+		toggleButton = "android.widget.ToggleButton"
+		seekBar      = "android.widget.SeekBar"
+
+		toggleButtonOutputFile    = "accessibility_event_diff_toggle_button_output.txt"
+		checkBoxOutputFile        = "accessibility_event_diff_checkbox_output.txt"
+		seekBarOutputFile         = "accessibility_event_diff_seekbar_output.txt"
+		seekBarDiscreteOutputFile = "accessibility_event_diff_seekbar_discrete_output.txt"
+
+		seekBarInitialValue  = 25
+		seekBarExpectedValue = 26
+
+		seekBarDiscreteInitialValue  = 3
+		seekBarDiscreteExpectedValue = 4
 	)
 	cr, err := accessibility.NewChrome(ctx)
 	if err != nil {
@@ -230,5 +349,27 @@ func AccessibilityEvent(ctx context.Context, s *testing.State) {
 	// Focus to and check checkBox element.
 	if err := focusAndCheckElement(ctx, chromeVoxConn, checkBox, checkBoxOutput, filepath.Join(s.OutDir(), checkBoxOutputFile)); err != nil {
 		s.Fatal("Failed focusing checkbox: ", err)
+	}
+
+	seekBarOutput := []string{
+		"EventType = focus",
+		"TargetName = seekBar",
+		"RootName = AccessibilitySample",
+		"DocumentURL = undefined",
+	}
+	// Focus to and increment seekBar element.
+	if err := focusAndIncrementElement(ctx, chromeVoxConn, seekBar, seekBarOutput, filepath.Join(s.OutDir(), toggleButtonOutputFile), seekBarInitialValue, seekBarExpectedValue); err != nil {
+		s.Fatal("Failed focusing seekBar: ", err)
+	}
+
+	seekBarDiscreteOutput := []string{
+		"EventType = focus",
+		"TargetName = seekBarDiscrete",
+		"RootName = AccessibilitySample",
+		"DocumentURL = undefined",
+	}
+	// Focus to and increment seekBarDiscrete element.
+	if err := focusAndIncrementElement(ctx, chromeVoxConn, seekBar, seekBarDiscreteOutput, filepath.Join(s.OutDir(), toggleButtonOutputFile), seekBarDiscreteInitialValue, seekBarDiscreteExpectedValue); err != nil {
+		s.Fatal("Failed focusing seekBarDiscrete: ", err)
 	}
 }
