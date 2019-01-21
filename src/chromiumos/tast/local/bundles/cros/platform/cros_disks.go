86b8d2b486accb915b5344118c25f3c1dcf638dc
Hidehiko Abe <hidehiko@chromium.org>
2019-01-21 22:55:04 +0900

    tast-tests: Port platform_CrosDisksDBus to tast.
    BUG=chromium:923875
    TEST=Ran on DUT.
    
    Change-Id: If3452c2c733f4e15b9bfdd454d235a1d54995f0d

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_disks.go
@@ -0,0 +1,25 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/platform/crosdisks"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosDisks,
+		Desc: "Verifies CrosDisks' D-Bus API works",
+		Attr: []string{"informational"},
+	})
+}
+
+func CrosDisks(ctx context.Context, s *testing.State) {
+	// Run series of tests. Please see crosdisks package for details.
+	crosdisks.RunTests(ctx, s)
+}
