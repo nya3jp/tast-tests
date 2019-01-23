8641d598df39f5814692500e278aa53d292d775d
Hidehiko Abe <hidehiko@chromium.org>
2019-01-23 11:15:48 +0900

    tast-tests: port platform_DMVerity(Bit)?Corruption to tast.
    BUG=chromium:924398
    TEST=Ran on DUT.
    
    Change-Id: Ie0e786cd7ff81b7a6a8eb8ed04275f96a17ae51d
    Reviewed-on: https://chromium-review.googlesource.com/1429308
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/platform/dm_verity.go
@@ -0,0 +1,24 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/platform/verity"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DMVerity,
+		Desc: "Verify dm-verity reports IO errors on bad data",
+		Attr: []string{"informational"},
+	})
+}
+
+func DMVerity(ctx context.Context, s *testing.State) {
+	verity.RunTests(ctx, s)
+}
