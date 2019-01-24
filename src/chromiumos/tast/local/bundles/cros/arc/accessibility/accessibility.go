070c1bb4798ca7babd93d9dcb8fa1db3bb474c53
Sara Kato <sarakato@google.com>
2019-01-24 09:48:25 +0900

    Move utility functions to accessibility package
    As more tests are added (for instance accessibility tree test https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1419717),
    having an accessibility package to contain common functions would be useful.
    
    
    TEST=tast run DUT_IP arcapp.AccessibilityEvent
    BUG=b:111049216
    Change-Id: I5c0f01ddd21720b33dc049ebc736a5ab03586ad6

--- src/chromiumos/tast/local/bundles/cros/arc/accessibility/accessibility.go
+++ src/chromiumos/tast/local/bundles/cros/arc/accessibility/accessibility.go
@@ -9,8 +9,25 @@ package accessibility
 import (
 	"context"
 	"strings"
+	"time"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+const (
+	// This is a build of an application containing a single activity and basic UI elements.
+	// The source code is in vendor/google_arc.
+	packageName  = "org.chromium.arc.testapp.accessibility_sample"
+	activityName = "org.chromium.arc.testapp.accessibility_sample.AccessibilityActivity"
+
+	toggleButtonID    = "org.chromium.arc.testapp.accessibility_sample:id/toggleButton"
+	checkBoxID        = "org.chromium.arc.testapp.accessibility_sample:id/checkBox"
+
+	extURL = "chrome-extension://mndnfokpggljbaajbnioimlmbfngpief/cvox2/background/background.html"
 )
 
 // Enabled checks if accessibility is enabled in Android.
@@ -22,4 +39,112 @@ func Enabled(ctx context.Context, a *arc.ARC) (bool, error) {
 		return false, err
 	}
 	return strings.TrimSpace(string(res)) == "1", nil
+}
+
+// ChromeVoxExtConn returns a connection to the ChromeVox extension's background page.
+// If the extension is not ready, the connection will be closed before returning.
+// Otherwise the calling function will close the connection.
+func ChromeVoxExtConn(ctx context.Context, c *chrome.Chrome) (*chrome.Conn, error) {
+	testing.ContextLog(ctx, "Waiting for extension at ", extURL)
+	extConn, err := c.NewConnForTarget(ctx, chrome.MatchTargetURL(extURL))
+	if err != nil {
+		return nil, err
+	}
+
+	// Ensure that we don't attempt to use the extension before its APIs are
+	// available: https://crbug.com/789313.
+	if err := extConn.WaitForExpr(ctx, "ChromeVoxState.instance"); err != nil {
+		extConn.Close()
+		return nil, errors.Wrap(err, "ChromeVox unavailable")
+	}
+
+	testing.ContextLog(ctx, "Extension is ready")
+	return extConn, nil
+}
+
+// EnableSpokenFeedback will enable spoken feedback on Chrome.
+// Also checks that setting is reflected in Android as well.
+func EnableSpokenFeedback(ctx context.Context, cr *chrome.Chrome, a *arc.ARC) error {
+	conn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return errors.Wrap(err, "creating test API connection failed")
+	}
+
+	if err := conn.EvalPromise(ctx, `
+		new Promise((resolve, reject) => {
+			chrome.accessibilityFeatures.spokenFeedback.set({value: true});
+			chrome.accessibilityFeatures.spokenFeedback.get({}, (details) => {
+				if (details.value) {
+					resolve();
+				} else {
+					reject();
+				}
+			});
+		})`, nil); err != nil {
+		return errors.Wrap(err, "failed to enable spoken feedback")
+	}
+
+	// Wait until spoken feedback is enabled.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if res, err := Enabled(ctx, a); err != nil {
+			return errors.Wrap(err, "failed to check whether accessibility is enabled in Android")
+		} else if !res {
+			return errors.New("accessibility not enabled")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+		return errors.Wrap(err, "failed to ensure accessibility is enabled: ")
+	}
+	return nil
+}
+
+// StartChrome starts Chrome calling chrome.New() with accessibility enabled.
+// The calling function will close the connection.
+func NewChrome(ctx context.Context) (*chrome.Chrome, error) {
+	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs([]string{"--force-renderer-accessibility"}))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect to Chrome: ")
+	}
+	return cr, nil
+}
+
+// NewARC starts ARC by calling arc.New().
+// The calling function will close the connection.
+func NewARC(ctx context.Context, outDir string) (*arc.ARC, error) {
+	a, err := arc.New(ctx, outDir)
+	if err != nil {
+		return nil, errors.New("failed to start ARC")
+	}
+	return a, err
+}
+
+// InstallAndStartAccessibilityApp starts the test application, and checks that UI components exist.
+func InstallAndStartAccessibilityApp(ctx context.Context, a *arc.ARC, apkPath string) error {
+	testing.ContextLog(ctx, "Installing app")
+	// Install accessibility_sample.apk
+	if err := a.Install(ctx, apkPath); err != nil {
+		return errors.Wrap(err, "failed installing app: ")
+	}
+
+	testing.ContextLog(ctx, "Starting app")
+	// Run accessibility_sample.apk.
+	if err := a.Command(ctx, "am", "start", "-W", packageName+"/"+activityName).Run(); err != nil {
+		return errors.Wrap(err, "failed starting app")
+	}
+
+	// Setup UI Automator.
+	d, err := ui.NewDevice(ctx, a)
+	if err != nil {
+		return errors.Wrap(err, "failed initializing UI Automator")
+	}
+	defer d.Close()
+
+	// Check UI components exist as expected.
+	if err := d.Object(ui.ID(toggleButtonID)).WaitForExists(ctx); err != nil {
+		return err
+	}
+	if err := d.Object(ui.ID(checkBoxID)).WaitForExists(ctx); err != nil {
+		return err
+	}
+	return nil
 }
