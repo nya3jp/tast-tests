e53744d68316fb061518f08c884e01ab891c052c
Darren Shen <shend@chromium.org>
2019-01-24 17:28:16 +1100

    tast: Add VirtualKeyboardSuggestions test.
    The virtual keyboard gets its word suggestions from a service
    called the decoder. However, this decoder is built elsewhere, so
    we don't really have any tests that make sure the decoder works
    with the UI.
    
    This test ensures that the virtual keyboard does indeed load the
    decoder correctly and that the decoder can provide suggestions to
    the virtual keyboard.
    
    There is a quirk with decoders however. Decoders ignore what the
    user types until it is fully loaded. There is no easy way to wait
    for the decoder to load, so we just have to keep pressing keys
    until the decoder provides suggestions. Until we have a good way
    of waiting for the decoder to load, we'll just do this hack.
    
    BUG=chromium:879073
    TEST=veyron_minnie
    
    Change-Id: I2893e3e108fd76371cf31f7f996c863038a0cecb

---
+++ src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_suggestions.go
@@ -0,0 +1,101 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/ui/vkb"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VirtualKeyboardSuggestions,
+		Desc:         "Checks that the virtual keyboard displays suggestions",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func VirtualKeyboardSuggestions(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ExtraArgs([]string{"--enable-virtual-keyboard"}))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	// Show a page with a text field that autofocuses.
+	const html = `<input type="text" id="text" autofocus/>`
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		io.WriteString(w, html)
+	}))
+	defer server.Close()
+
+	conn, err := cr.NewConn(ctx, server.URL)
+	if err != nil {
+		s.Fatal("Creating renderer for test page failed: ", err)
+	}
+	defer conn.Close()
+
+	// Wait for the text field to focus.
+	if err := conn.WaitForExpr(ctx,
+		`document.getElementById('text') === document.activeElement`); err != nil {
+		s.Fatal("Failed to wait for text field to focus: ", err)
+	}
+
+	if err := vkb.ShowVirtualKeyboard(ctx, tconn); err != nil {
+		s.Fatal("Failed to show the virtual keyboard: ", err)
+	}
+
+	s.Log("Waiting for the virtual keyboard to show")
+	if err := vkb.WaitUntilShown(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for the virtual keyboard to show: ", err)
+	}
+
+	s.Log("Waiting for the virtual keyboard to render buttons")
+	if err := vkb.WaitUntilButtonsRender(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for the virtual keyboard to render: ", err)
+	}
+
+	kconn, err := vkb.UIConn(ctx, cr)
+	if err != nil {
+		s.Fatal("Creating connection to virtual keyboard UI failed: ", err)
+	}
+	defer kconn.Close()
+
+	// The IME decoder, which provides the suggestions for the virtual keyboard,
+	// ignores key presses until it is fully loaded. Thus, this test presses keys
+	// periodically until the decoder is ready and suggestions are shown.
+	s.Log("Waiting for the decoder to provide suggestions")
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		if err := vkb.TapKey(ctx, kconn, "a"); err != nil {
+			return err
+		}
+		suggestions, err := vkb.GetSuggestions(ctx, kconn)
+		if err != nil {
+			return err
+		}
+		if len(suggestions) == 0 {
+			return errors.New("No suggestions found")
+		}
+		return nil
+	}, &testing.PollOptions{})
+
+	if err != nil {
+		s.Fatal("Failed to wait for suggestions to appear: ", err)
+	}
+}
