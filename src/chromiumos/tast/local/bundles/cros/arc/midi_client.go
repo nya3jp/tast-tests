05aa9f7b429110fe6ef6997d263b2ae9769f6ac0
Prashant Malani <pmalani@chromium.org>
2019-01-24 18:22:13 -0800

    tast tests: Add skeleton for MidiClient test
    Add basic skeleton code (based off of ArcKeyboardTest) for a MIDI
    client test. The skeleton just installs and executes the ARC++ apk.
    
    The logic to read the MIDI message sent by the APK will come in the
    next cl.
    
    BUG=b:22860765
    TEST=tast -verbose run -build=true pmalani-kevin arc.MidiClient passes.
    
    Change-Id: Ia29aabed40d903569b58e4e7a58ea153ed5be242

---
+++ src/chromiumos/tast/local/bundles/cros/arc/midi_client.go
@@ -0,0 +1,62 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MidiClient,
+		Desc:         "Checks MIDI Apps can send messages to devices",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+		Data:         []string{"ArcMidiClientTest.apk"},
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func MidiClient(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ARCEnabled())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	d, err := ui.NewDevice(ctx, a)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	defer d.Close()
+
+	const (
+		apk = "ArcMidiClientTest.apk"
+		pkg = "org.chromium.arc.testapp.arcmidiclienttest"
+		cls = "org.chromium.arc.testapp.arcmidiclienttest.MainActivity"
+	)
+
+	s.Log("Installing app")
+	if err := a.Install(ctx, s.DataPath(apk)); err != nil {
+		s.Fatal("Failed installing app: ", err)
+	}
+
+	s.Log("Starting app")
+	if err := a.Command(ctx, "am", "start", "-W", pkg+"/"+cls).Run(); err != nil {
+		s.Fatal("Failed starting app: ", err)
+	}
+}
