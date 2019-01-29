4fc48471def4a71b5444f6d89e8ea6a682b0e519
Allen Webb <allenwebb@google.com>
2019-01-29 09:50:48 -0800

    usbguard: Test upstart scripts for different feature flag states.
    USBGuard is a tool for enforcing USB device authorization policy. On
    Chrome OS is being used for lock-screen hardening. This adds a test that
    checks that the feature flag D-Bus methods, and usbguard related upstart
    scripts work as intended.
    
    CQ-DEPEND=CL:1440125
    BUG=chromium:926222
    TEST=tast run $TESTHOST security.USBGuard
    
    Change-Id: I8db2e37015f166a11d45fd044654d32ede368241

---
+++ src/chromiumos/tast/local/bundles/cros/security/usbguard.go
@@ -0,0 +1,226 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"context"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         USBGuard,
+		Desc:         "Check that USBGuard-related feature flags work as intended",
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"usbguard"},
+	})
+}
+
+func USBGuard(ctx context.Context, s *testing.State) {
+	const (
+		usbguardFeature    = "USBGuard"
+		usbbouncerFeature  = "USBBouncer"
+		usbguardJob        = "usbguard"
+		usbguardWrapperJob = "usbguard-wrapper"
+		usbguardProcess    = "usbguard-daemon"
+		usbguardPolicy     = "/run/usbguard/rules.conf"
+
+		screenLockedEvent   = "screen-locked"
+		screenUnlockedEvent = "screen-unlocked"
+
+		jobTimeout     = 10 * time.Second
+		respawnTimeout = 10 * time.Second
+	)
+
+	isFeatureEnabled := func(feature string) (bool, error) {
+		const (
+			dbusName      = "org.chromium.ChromeFeaturesService"
+			dbusPath      = "/org/chromium/ChromeFeaturesService"
+			dbusInterface = "org.chromium.ChromeFeaturesServiceInterface"
+			dbusMethod    = ".IsFeatureEnabled"
+		)
+		enabled := false
+
+		_, obj, err := dbusutil.Connect(ctx, dbusName, dbus.ObjectPath(dbusPath))
+		if err != nil {
+			return enabled, err
+		}
+
+		if err := obj.CallWithContext(ctx, dbusInterface+dbusMethod, 0, feature).Store(&enabled); err != nil {
+			return enabled, err
+		}
+
+		return enabled, nil
+	}
+
+	setScreenLocked := func(locked bool) error {
+		const (
+			dbusName      = "org.chromium.SessionManager"
+			dbusPath      = "/org/chromium/SessionManager"
+			dbusInterface = "org.chromium.SessionManagerInterface"
+		)
+
+		var dbusMethod string
+		if locked {
+			dbusMethod = ".LockScreen"
+		} else {
+			dbusMethod = ".HandleLockScreenDismissed"
+		}
+
+		_, obj, err := dbusutil.Connect(ctx, dbusName, dbus.ObjectPath(dbusPath))
+		if err != nil {
+			return errors.Wrap(err, "D-Bus connect failed while setting the lock-screen state")
+		}
+
+		if err = obj.CallWithContext(ctx, dbusInterface+dbusMethod, 0).Store(); err != nil {
+			return errors.Wrapf(err, "failed to invoke %q", dbusInterface+dbusMethod)
+		}
+
+		return nil
+	}
+
+	expectUsbguardRunning := func(running, onLockScreen bool) error {
+		goal := upstart.StopGoal
+		state := upstart.WaitingState
+		if running {
+			goal = upstart.StartGoal
+			state = upstart.RunningState
+		}
+		err := upstart.WaitForJobStatus(ctx, usbguardJob, goal, state, upstart.TolerateWrongGoal, jobTimeout)
+		if err != nil {
+			return errors.Wrapf(err, "unexpected job status for %v", usbguardJob)
+		}
+
+		if running {
+			_, err = os.Stat(usbguardPolicy)
+			if err != nil {
+				return errors.Wrapf(err, "failed finding policy %v", usbguardPolicy)
+			}
+		} else if !onLockScreen {
+			err := upstart.WaitForJobStatus(ctx, usbguardWrapperJob, goal, state, upstart.TolerateWrongGoal, jobTimeout)
+			if err != nil {
+				return errors.Wrapf(err, "failed to wait on job %v to stop", usbguardWrapperJob)
+			}
+			if _, err = os.Stat(usbguardPolicy); err == nil {
+				return errors.Errorf("policy %v unexpectedly exists", usbguardPolicy)
+			} else if err != nil && !os.IsNotExist(err) {
+				return errors.Wrapf(err, "failed checking policy %v", usbguardPolicy)
+			}
+		}
+		return nil
+	}
+
+	checkUsbguardRespawn := func() error {
+		err := testing.Poll(ctx, func(ctx context.Context) error {
+			c := testexec.CommandContext(ctx, "killall", usbguardProcess)
+			_, err := c.Output()
+			if err != nil {
+				c.DumpLog(ctx)
+				err = errors.Wrapf(err, "failed to kill %v", usbguardProcess)
+			}
+			return err
+		}, &testing.PollOptions{Timeout: respawnTimeout})
+		if err != nil {
+			return err
+		}
+
+		if err = expectUsbguardRunning(true /*running*/, true /*onLockScreen*/); err != nil {
+			return errors.Wrapf(err, "failed to check that %v job was respawned", usbguardJob)
+		}
+		return nil
+	}
+
+	runTest := func(usbguardEnabled, usbbouncerEnabled bool) {
+		featureValues := map[string]bool{
+			usbguardFeature:   usbguardEnabled,
+			usbbouncerFeature: usbbouncerEnabled,
+		}
+		var enabled, disabled []string
+		for name, val := range featureValues {
+			if val {
+				enabled = append(enabled, name)
+			} else {
+				disabled = append(disabled, name)
+			}
+		}
+
+		var args []string
+		if len(enabled) > 0 {
+			args = append(args, "--enable-features="+strings.Join(enabled, ","))
+		}
+		if len(disabled) > 0 {
+			args = append(args, "--disable-features="+strings.Join(disabled, ","))
+		}
+
+		cr, err := chrome.New(ctx, chrome.ExtraArgs(args))
+		if err != nil {
+			s.Fatal("Failed to start Chrome: ", err)
+		}
+		defer cr.Close(ctx)
+
+		boolToEnabled := func(value bool) string {
+			if value {
+				return "enabled"
+			}
+			return "disabled"
+		}
+
+		for name, val := range featureValues {
+			if en, err := isFeatureEnabled(name); err != nil {
+				s.Error("Failed if the %v feature is enabled: ", err)
+				return
+			} else if en != val {
+				s.Errorf("The %v feature is supposed to be %s but it is %s instead", name,
+					boolToEnabled(val), boolToEnabled(en))
+				return
+			}
+		}
+
+		if err = expectUsbguardRunning(false /*running*/, false /*onLockScreen*/); err != nil {
+			s.Errorf("Unexpected initial job status for %q: %v", usbguardJob, err)
+			return
+		}
+
+		s.Log("Locking the screen")
+		if err = setScreenLocked(true); err != nil {
+			s.Errorf("Failed to lock the screen: ", err)
+			return
+		}
+		if err = expectUsbguardRunning(usbguardEnabled /*running*/, true /*onLockScreen*/); err != nil {
+			s.Error("Failed to check if usbguard is running: ", err)
+		}
+
+		if usbguardEnabled {
+			s.Logf("Killing %q to check for respawn", usbguardProcess)
+			if err = checkUsbguardRespawn(); err != nil {
+				s.Errorf("Failed to check that %v job respawns: %v", usbguardJob, err)
+			}
+		}
+
+		s.Log("Unlocking the screen")
+		if err = setScreenLocked(false); err != nil {
+			s.Errorf("Failed to unlock the screen: ", err)
+			return
+		}
+		if err = expectUsbguardRunning(false /*running*/, false /*onLockScreen*/); err != nil {
+			s.Errorf("Unexpected final job status for %q: %v", usbguardJob, err)
+		}
+	}
+
+	runTest(true /*usbguardEnabled*/, false /*usbbouncerEnabled*/)
+	runTest(false /*usbguardEnabled*/, false /*usbbouncerEnabled*/)
+	// Testing USB Bouncer requires the usb_bouncer ebuild which isn't installed by default yet.
+}
