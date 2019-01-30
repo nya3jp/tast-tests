f6a937a4cf6d953ec78be8d793b5eddb9ae2885b
Pin-chih Lin <johnylin@google.com>
2019-01-30 14:18:43 +0800

    video: Migrate video_VEAPerf autotest to Tast
    video_VEAPerf executes video_encode_accelerator_unittest four times, hence four binArgs
    arguments are provided to runAccelVideoTest(). And runBinaryTestWithCPUMeasurement() is
    implemented for CPU usage benchmark.
    
    The method of getting CPU usage is changed compared to video_VEAPerf autotest, since we still
    have some issues to specify prefix for chronos to invoke the test binary in Tast environment now.
    So in Tast test we changed to use the similar method as DecodeAccelJPEGPerf. We have wrapped
    the common procedure into MeasureUsageOnRunningBinary() in cpu.go.
    
    vidyo1-1280x720.vp9.webm is added, which is more representative to video-conference scenario
    for perf testing.
    
    In bintest.go, os.MkdirAll() should be used to avoid returning error if directory already exists.
    
    BUG=chromium:924837
    TEST=tast run eve video.EncodeAccelPerf*, video.EncodeAccel*, video.DecodeAccelJPEGPerf
    
    Change-Id: I426065925c64b4f1ee89244a67418d0698a8ccac

---
+++ src/chromiumos/tast/local/bundles/cros/video/encode_accel_perf_vp8_720p_i420_vidyo.go
@@ -0,0 +1,34 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/encode"
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         EncodeAccelPerfVP8720PI420Vidyo,
+		Desc:         "Runs Chrome video_encode_accelerator_unittest to measure the performance of VP8 encoding for 720p I420 video vidyo1, which is more representative of a video-conference scenario",
+		Attr:         []string{"informational"},
+		Contacts:     []string{"johnylin@chromium.org", "chromeos-video-eng@google.com"},
+		SoftwareDeps: []string{caps.HWEncodeVP8},
+		Data:         []string{encode.Vidyo720P.Name},
+	})
+}
+
+func EncodeAccelPerfVP8720PI420Vidyo(ctx context.Context, s *testing.State) {
+	encode.RunAccelVideoPerfTest(ctx, s, encode.TestOptions{
+		Profile:     videotype.VP8Prof,
+		Params:      encode.Vidyo720P,
+		PixelFormat: videotype.I420,
+		InputMode:   encode.SharedMemory,
+	})
+}
