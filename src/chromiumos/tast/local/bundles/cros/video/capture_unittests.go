b2c7f97c3b1c54176deccc4d828e9fad3f673108
Keiichi Watanabe <keiichiw@chromium.org>
2019-02-01 15:51:11 +0900

    video.CaptureUnittests: Promote to CQ
    Since video.CaptureUnittests looks stable recently, this promotes
    it to CQ.
    https://cros-health-prod.googleplex.com/test/video.CaptureUnittests
    
    Note:
    * Failures on leon were due to a broken DUT (b/131091450)
    * CaptureUnittests won't run on veyron_fievel after CL:1530173
    
    BUG=chromium:893501
    TEST=fast_build.sh -C
    
    Change-Id: I4cc1696c99a9e119638828a24e65126438675587
    Signed-off-by: Keiichi Watanabe <keiichiw@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/capture_unittests.go
+++ src/chromiumos/tast/local/bundles/cros/video/capture_unittests.go
@@ -25,8 +25,7 @@ func init() {
 		Func:         CaptureUnittests,
 		Desc:         "Runs Chrome capture_unittests to exercise Chrome's video capture stack",
 		Contacts:     []string{"keiichiw@chromium.org", "chromeos-video-eng@google.com"},
-		Attr:         []string{"informational"},
-		SoftwareDeps: []string{caps.BuiltinCamera, "camera_720p"},
+		SoftwareDeps: []string{caps.BuiltinOrVividCamera, "camera_720p", "chrome"},
 		Data:         []string{"bear.mjpeg"},
 	})
 }
