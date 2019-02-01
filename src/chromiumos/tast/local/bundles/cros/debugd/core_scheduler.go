54ede276ae75e9026fa9c33234199d552b07f933
Greg Kerr <kerrnel@chromium.org>
2019-02-01 15:44:52 -0800

    tast: Add tests for debugd SetSchedulerConfiguration.
    This adds a tast test for  debugd's SetSchedulerConfiguration method.
    
    BUG=chromium:927840
    TEST=tast -verbose run  100.115.91.38 debugd.CoreScheduler
    CQ-DEPEND=CL:1449828
    
    Change-Id: Iba387123605ae4d96dd651ea15c8529c394aef20

---
+++ src/chromiumos/tast/local/bundles/cros/debugd/core_scheduler.go
@@ -0,0 +1,109 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package debugd
+
+import (
+	"context"
+	"io/ioutil"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/debugd"
+	"chromiumos/tast/testing"
+)
+
+type scheduler string
+
+const (
+	conservative = "conservative"
+	performance  = "performance"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CoreScheduler,
+		Desc: "Verifies Debugd's SetSchedulerConfiguration D-Bus API works",
+		Attr: []string{"informational"},
+	})
+}
+
+func CoreScheduler(ctx context.Context, s *testing.State) {
+	d, err := debugd.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect debugd D-Bus service: ", err)
+	}
+
+	testScheduler := func(ctx context.Context, s *testing.State, d *debugd.Debugd, sched scheduler, expectOfflineCPUs bool) bool {
+		status, err := d.SetSchedulerConfiguration(ctx, string(sched))
+		if err != nil {
+			s.Error("Failed to run SetSchedulerConfiguration: ", err)
+			return false
+		}
+
+		if !status {
+			s.Error("SetSchedulerConfiguration returned false")
+			return false
+		}
+
+		// Now see if the CPUs are offline.
+		terminator := [1]byte{0xa}
+		offlineDat, err := ioutil.ReadFile("/sys/devices/system/cpu/offline")
+		if err != nil {
+			s.Error("Failed to open offline cpu file: ", err)
+			return false
+		}
+		if expectOfflineCPUs && (len(offlineDat) <= 1 || offlineDat[0] == terminator[0]) {
+			s.Error("No offline CPUs reported: ", string(offlineDat))
+			return false
+		}
+
+		return true
+	}
+
+	// Restore the original setting.
+	defer func(ctx context.Context) {
+		if status, err := d.SetSchedulerConfiguration(ctx, string(performance)); err != nil {
+			s.Error("Failed to restore device state: ", err)
+		} else if !status {
+			s.Error("Failed to restore device state")
+		}
+	}(ctx)
+
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	// Make sure the dbus calls work in various orderings.
+	if !testScheduler(ctx, s, d, performance, false) {
+		return
+	}
+
+	if !testScheduler(ctx, s, d, conservative, true) {
+		return
+	}
+
+	if !testScheduler(ctx, s, d, performance, false) {
+		return
+	}
+
+	if !testScheduler(ctx, s, d, performance, false) {
+		return
+	}
+
+	if !testScheduler(ctx, s, d, conservative, true) {
+		return
+	}
+
+	if !testScheduler(ctx, s, d, conservative, true) {
+		return
+	}
+
+	if !testScheduler(ctx, s, d, performance, false) {
+		return
+	}
+
+	if !testScheduler(ctx, s, d, performance, false) {
+		return
+	}
+}
