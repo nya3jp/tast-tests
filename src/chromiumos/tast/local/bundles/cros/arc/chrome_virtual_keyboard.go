9775ae7ec1eb122c0ab7641e6361db59961f7ec7
Tetsui Ohkubo <tetsui@chromium.org>
2019-02-06 18:03:26 +0900

    arc.ChromeVirtualKeyboard: Avoid pressing space
    When using virtual keyboard, pressing space key triggers autocomplete.
    This might cause uncertain test results in some devices, so it's better
    to avoid pressing space during the test.
    
    BUG=b:122456478
    TEST=tast run eve-DUT arc.ChromeVirtualKeyboard
    
    Change-Id: I8d515d5d5c768a4d34340c3c54cb8ddba6dc2b39

--- src/chromiumos/tast/local/bundles/cros/arc/chrome_virtual_keyboard.go
+++ src/chromiumos/tast/local/bundles/cros/arc/chrome_virtual_keyboard.go
@@ -139,9 +139,10 @@ func ChromeVirtualKeyboard(ctx context.Context, s *testing.State) {
 	}
 	defer kconn.Close()
 
-	// Press a sequence of keys.
+	// Press a sequence of keys. Avoid using Space since it triggers autocomplete, which can
+	// cause flaky failures: http://b/122456478#comment4
 	keys := []string{
-		"h", "e", "l", "l", "o", "space", "w", "o",
+		"h", "e", "l", "l", "o", "w", "o",
 		"backspace", "backspace", "t", "a", "s", "t"}
 
 	for i, key := range keys {
@@ -160,7 +161,7 @@ func ChromeVirtualKeyboard(ctx context.Context, s *testing.State) {
 		}
 	}
 
-	const expected = "hello tast"
+	const expected = "hellotast"
 
 	// In order to use GetText() after timeout, we should have shorter timeout than ctx.
 	if err := d.Object(ui.ID(fieldID), ui.Text(expected)).WaitForExists(ctx, 2*time.Minute); err != nil {
