b407c947f773437d2724e32cff0cb9697a0b19f4
Hidehiko Abe <hidehiko@chromium.org>
2019-02-09 02:26:03 +0900

    tast-tests: Port login_RemoteOwnership.
    BUG=chromium:899758
    TEST=Ran on DUT.
    
    Change-Id: I1968f9028a2849d3bf81c7035a387960c37f28be

---
+++ src/chromiumos/tast/local/bundles/cros/session/remote_ownership.go
@@ -0,0 +1,116 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package session
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/google/go-cmp/cmp"
+
+	"chromiumos/tast/local/bundles/cros/session/ownership"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/local/session"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: RemoteOwnership,
+		Desc: "Verifies that Ownership API can be used to set device policies (as an enterprise might do)",
+		Contacts: []string{
+			"mnissler@chromium.org", // session_manager owner
+			"derat@chromium.org",    // session_manager owner
+			"hidehiko@chromium.org", // Tast port author
+		},
+		Attr: []string{"informational"},
+		Data: []string{"testcert.p12"},
+	})
+}
+
+func RemoteOwnership(ctx context.Context, s *testing.State) {
+	if err := ownership.SetUpDevice(ctx); err != nil {
+		s.Fatal("Failed to reset device ownership: ", err)
+	}
+
+	privKey, err := ownership.ExtractPrivKey(s.DataPath("testcert.p12"))
+	if err != nil {
+		s.Fatal("Failed to parse PKCS #12 file: ", err)
+	}
+
+	sm, err := session.NewSessionManager(ctx)
+	if err != nil {
+		s.Fatal("Failed to create session_manager binding: ", err)
+	}
+
+	// Initial policy set up.
+	settings := ownership.BuildTestSettings("")
+	if err := ownership.StoreSettings(ctx, sm, "", privKey, nil, settings); err != nil {
+		s.Fatal("Failed to store settings: ", err)
+	}
+	if retrieved, err := ownership.RetrieveSettings(ctx, sm); err != nil {
+		s.Fatal("Failed to retrieve settings: ", err)
+	} else if diff := cmp.Diff(settings, retrieved); diff != "" {
+		const diffName = "diff.txt"
+		if err = ioutil.WriteFile(filepath.Join(s.OutDir(), diffName), []byte(diff), 0644); err != nil {
+			s.Error("Failed to write diff: ", err)
+		}
+		s.Fatal("Unexpected settings were retrieved. Diff is found in ", diffName)
+	}
+
+	// Force re-key the device.
+	privKey, err = rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		s.Fatal("Failed to generate RSA key: ", err)
+	}
+	if err := ownership.StoreSettings(ctx, sm, "", privKey, nil, settings); err != nil {
+		s.Fatal("Failed to store rekeyed settings: ", err)
+	}
+	if retrieved, err := ownership.RetrieveSettings(ctx, sm); err != nil {
+		s.Fatal("Failed to retrieve rekeyed settings: ", err)
+	} else if diff := cmp.Diff(settings, retrieved); diff != "" {
+		const diffName = "diff-rekeyed.txt"
+		if err = ioutil.WriteFile(filepath.Join(s.OutDir(), diffName), []byte(diff), 0644); err != nil {
+			s.Error("Failed to write diff: ", err)
+		}
+		s.Fatal("Unexpected rekeyed settings were retrieved. Diff is found in ", diffName)
+	}
+
+	// Rotate key gracefully.
+	const (
+		testUser = "test@foo.com"
+		testPass = "test_password"
+	)
+	// Create clean vault for the test user.
+	if err = cryptohome.RemoveVault(ctx, testUser); err != nil {
+		s.Fatal("Failed to remove vault: ", err)
+	}
+	if err = cryptohome.CreateVault(ctx, testUser, testPass); err != nil {
+		s.Fatal("Failed to create vault: ", err)
+	}
+	newPrivKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		s.Fatalf("Failed to generate RSA key for user %s: %v", testUser, err)
+	}
+	// Start a session for the user, then store the settings.
+	if err = sm.StartSession(ctx, testUser, ""); err != nil {
+		s.Fatal("Failed to start session: ", err)
+	}
+	if err := ownership.StoreSettings(ctx, sm, "", newPrivKey, privKey, settings); err != nil {
+		s.Fatal("Failed to store user settings: ", err)
+	}
+	if retrieved, err := ownership.RetrieveSettings(ctx, sm); err != nil {
+		s.Fatal("Failed to retrieve user settings: ", err)
+	} else if diff := cmp.Diff(settings, retrieved); diff != "" {
+		const diffName = "diff-user.txt"
+		if err = ioutil.WriteFile(filepath.Join(s.OutDir(), diffName), []byte(diff), 0644); err != nil {
+			s.Error("Failed to write diff: ", err)
+		}
+		s.Fatal("Unexpected user settings were retrieved. Diff is found in ", diffName)
+	}
+}
