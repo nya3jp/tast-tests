096e9636362893ac0bd02246e84297768c9b88ee
Anand K Mistry <amistry@chromium.org>
2019-02-14 17:12:04 +1100

    platform.Mtpd: Add a basic sanity test for mtpd.
    This test ensures mtpd is running and responds to DBus calls.
    
    BUG=chromium:929968
    TEST=tast run <nocturne dut> platform.Mtpd
    CQ-DEPEND=CL:1472230
    
    Change-Id: I9b717fd17baf709bd3ef525659f5524b6611dff9

---
+++ src/chromiumos/tast/local/bundles/cros/platform/mtpd.go
@@ -0,0 +1,51 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Mtpd,
+		Desc: "Verifies mtpd is running and responds to D-Bus calls",
+		Contacts: []string{
+			"amistry@chromium.org",
+		},
+		Attr: []string{"informational"},
+	})
+}
+
+func Mtpd(ctx context.Context, s *testing.State) {
+	const (
+		dbusName      = "org.chromium.Mtpd"
+		dbusPath      = "/org/chromium/Mtpd"
+		dbusInterface = "org.chromium.Mtpd"
+
+		job = "mtpd"
+	)
+
+	s.Log("Restarting mtpd service and waiting for D-Bus service")
+	if err := upstart.RestartJob(ctx, job); err != nil {
+		s.Fatal("Failed to restart mtpd: ", err)
+	}
+
+	_, dbusObj, err := dbusutil.Connect(ctx, dbusName, dbusPath)
+	if err != nil {
+		s.Fatalf("Failed to connect to D-Bus service %s: %v", dbusName, err)
+	}
+
+	var result bool
+	if err := dbusObj.CallWithContext(ctx, dbusInterface+".IsAlive", 0).Store(&result); err != nil {
+		s.Error("Failed to call IsAlive D-Bus method: ", err)
+	} else if !result {
+		s.Error("Unexpected false result from IsAlive")
+	}
+}
