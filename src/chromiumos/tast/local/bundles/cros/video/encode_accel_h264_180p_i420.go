24c32d3e55046f5287a674d174f32db33aa8a68f
Pin-chih Lin <johnylin@google.com>
2019-02-15 16:02:25 +0800

    video: allow multiple binary test runs with different testArgs
    This CL split binArgs struct out of TestOptions. TestOptions specifies common info provided by
    test configutation, and binArgs contains arguments and modes which should be determined by test
    categories.
    
    video_VEAPerf executes video_encode_accelerator_unittest multiple times which we will migrate in
    the next CL. To save the repetitive video data de-compression time, this CL modified
    runAccelVideoTest() to be variadic to accept an arbitrary number of binArgs,
    video_encode_accelerator_unittest is run per binArgs.
    
    BUG=chromium:924837
    TEST=tast run video.EncodeAccel* on Eve
    
    Change-Id: If6bbadbe42b7d0e318a7e5cda23b357213e328c9

--- src/chromiumos/tast/local/bundles/cros/video/encode_accel_h264_180p_i420.go
+++ src/chromiumos/tast/local/bundles/cros/video/encode_accel_h264_180p_i420.go
@@ -26,5 +26,5 @@ func init() {
 
 // EncodeAccelH264180PI420 runs video_encode_accelerator_unittest to encode H264 encoding with 180p I420 raw data compressed in tulip2-320x180.webm.
 func EncodeAccelH264180PI420(ctx context.Context, s *testing.State) {
-	encode.RunAccelVideoTest(ctx, s, encode.TestOptions{Profile: videotype.H264Prof, Params: encode.Tulip180P, PixelFormat: videotype.I420})
+	encode.RunAllAccelVideoTests(ctx, s, encode.TestOptions{Profile: videotype.H264Prof, Params: encode.Tulip180P, PixelFormat: videotype.I420, InputMode: encode.SharedMemory})
 }
--- src/chromiumos/tast/local/bundles/cros/video/encode_accel_h264_360p_i420.go
+++ src/chromiumos/tast/local/bundles/cros/video/encode_accel_h264_360p_i420.go
@@ -26,5 +26,5 @@ func init() {
 
 // EncodeAccelH264360PI420 runs video_encode_accelerator_unittest to encode H264 encoding with 360p I420 raw data compressed in tulip2-640x360.webm.
 func EncodeAccelH264360PI420(ctx context.Context, s *testing.State) {
-	encode.RunAccelVideoTest(ctx, s, encode.TestOptions{Profile: videotype.H264Prof, Params: encode.Tulip360P, PixelFormat: videotype.I420})
+	encode.RunAllAccelVideoTests(ctx, s, encode.TestOptions{Profile: videotype.H264Prof, Params: encode.Tulip360P, PixelFormat: videotype.I420, InputMode: encode.SharedMemory})
 }
