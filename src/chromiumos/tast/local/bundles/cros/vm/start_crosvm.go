501fd86e63a2dfef8cec7095dd8833b1839448bc
Shuhei Takahashi <nya@chromium.org>
2019-02-20 11:58:00 +0000

    Revert "vm.StartCrosvm: Promote to CQ."
    This reverts commit 26e12368d59ddd6692a130371b0526b351c20d6a.
    
    Reason for revert: Flakiness was observed in CQ.
    
    Original change's description:
    > vm.StartCrosvm: Promote to CQ.
    > 
    > This test passed last 20 consecutive builds.
    > 
    > BUG=None
    > TEST=tast run nocturne vm.StartCrosvm  # for sanity check
    > 
    > Change-Id: Ibcb19bd8f5fa3188dea96f50e2a9e8aa37a45350
    > Reviewed-on: https://chromium-review.googlesource.com/1457564
    > Commit-Ready: Shuhei Takahashi <nya@chromium.org>
    > Tested-by: Shuhei Takahashi <nya@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    
    BUG=chromium:933716
    
    Change-Id: Ie90294c3011a8767c0b6968b3b79adbf2de88409

--- src/chromiumos/tast/local/bundles/cros/vm/start_crosvm.go
+++ src/chromiumos/tast/local/bundles/cros/vm/start_crosvm.go
@@ -21,6 +21,7 @@ func init() {
 		Func:         StartCrosvm,
 		Desc:         "Checks that crosvm starts and runs commands",
 		Contacts:     []string{"jkardatzke@chromium.org", "cros-containers-dev@google.com"},
+		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"vm_host"},
 	})
 }
