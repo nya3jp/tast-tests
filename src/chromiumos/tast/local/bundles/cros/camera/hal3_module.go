2ce62ea4a0f0db8f95484b3a5e9390a9bff8e7c6
Shik Chen <shik@chromium.org>
2019-02-22 14:16:06 +0800

    tast-tests: Add camera.HAL3* tests.
    These are tests ported from camera_HAL3.* autotests [1].
    
    [1] http://cs/chromeos_public/src/third_party/autotest/files/client/site_tests/camera_HAL3/camera_HAL3.py?rcl=ae09491545ac4508c0fecdc581dfff3b67644e8b
    
    BUG=b:117817080
    CQ-DEPEND=CL:1482415
    TEST=tast run <dut> 'camera.HAL3*'. Note that camera.HAL3Frame and
         camera.HAL3StillCapture are current failing on many boards, just
         like the camera_HAL3 autotests.
    
    Change-Id: Ide5a18217ba54bf74f4949d16f5a69a71e913e8b
    Reviewed-on: https://chromium-review.googlesource.com/1482416
    Commit-Ready: Shik Chen <shik@chromium.org>
    Tested-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/camera/hal3_module.go
@@ -0,0 +1,29 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/camera/hal3"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HAL3Module,
+		Desc:         "Verifies camera module function with HAL3 interface",
+		Contacts:     []string{"shik@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"arc_camera3"},
+	})
+}
+
+func HAL3Module(ctx context.Context, s *testing.State) {
+	hal3.RunTest(ctx, s, hal3.TestConfig{
+		GtestFilter:            "Camera3ModuleFixture.*",
+		RequireRecordingParams: true,
+	})
+}
