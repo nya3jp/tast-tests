1dde76edb4367c5475814b46be64f2e2e5e73ecb
Pin-chih Lin <johnylin@google.com>
2019-02-22 18:39:43 +0800

    video: Unittest for encode/perf.go
    The unittest checks the validity for report* functions, and saves obtained metric values
    to results-chart.json to compare to golden JSON file.
    
    BUG=chromium:924837
    TEST=fast_build.sh -t chromiumos/tast/local/bundles/cros/video/encode
    
    Change-Id: Ie5d0b0001b3dcda38096e2560940b9d7f6a1cd2f
    Reviewed-on: https://chromium-review.googlesource.com/1482417
    Commit-Ready: Pin-chih Lin <johnylin@chromium.org>
    Tested-by: Pin-chih Lin <johnylin@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/video/encode/perf_test.go
@@ -0,0 +1,94 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package encode
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"chromiumos/tast/diff"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/testutil"
+)
+
+func loadJSON(path string) (interface{}, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var v interface{}
+	if err := json.NewDecoder(f).Decode(&v); err != nil {
+		return nil, errors.Wrapf(err, "failed decoding %s", path)
+	}
+	return v, nil
+}
+
+func jsonEquals(path1, path2 string) error {
+	v1, err := loadJSON(path1)
+	if err != nil {
+		return err
+	}
+	v2, err := loadJSON(path2)
+	if err != nil {
+		return err
+	}
+
+	if !reflect.DeepEqual(v1, v2) {
+		return errors.New("JSON files differ")
+	}
+	return nil
+}
+
+func saveAndCompare(t *testing.T, p *perf.Values, goldenPath string) {
+	td := testutil.TempDir(t)
+	defer os.RemoveAll(td)
+
+	if err := p.Save(td); err != nil {
+		t.Fatal("Failed saving JSON: ", err)
+	}
+
+	path := filepath.Join(td, "results-chart.json")
+	if err := jsonEquals(path, goldenPath); err != nil {
+		if data, rerr := ioutil.ReadFile(path); rerr != nil {
+			t.Fatal(rerr)
+		} else if golden, gerr := ioutil.ReadFile(goldenPath); gerr != nil {
+			t.Fatal(gerr)
+		} else if diff, derr := diff.Diff(string(data), string(golden)); derr != nil {
+			t.Fatal(derr)
+		} else {
+			t.Fatalf("%v; output:\n%s", err, diff)
+		}
+	}
+}
+
+func TestReportMetrics(t *testing.T) {
+	const name = "crowd-1920x1080_h264"
+
+	p := perf.NewValues()
+	if err := reportFPS(p, name, "testdata/TestFPS.log"); err != nil {
+		t.Error("Failed at reportFPS(): ", err)
+	}
+
+	if err := reportEncodeLatency(p, name, "testdata/TestLatency.log"); err != nil {
+		t.Error("Failed at reportEncodeLatency(): ", err)
+	}
+
+	if err := reportCPUUsage(p, name, "testdata/TestCPU.log"); err != nil {
+		t.Error("Failed at reportCPUUsage(): ", err)
+	}
+
+	if err := reportFrameStats(p, name, "testdata/TestFrameStats.log"); err != nil {
+		t.Error("Failed at reportFrameStats(): ", err)
+	}
+
+	saveAndCompare(t, p, "testdata/TestResultsChart.json")
+}
