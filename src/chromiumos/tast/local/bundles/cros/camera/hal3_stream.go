75d36156989424829f43ef6cfb84d5029c0897a0
Shik Chen <shik@chromium.org>
2019-02-22 14:16:06 +0800

    tast-tests: Add camera.HAL3* tests.
    These are tests ported from camera_HAL3.* autotests [1].
    
    [1] http://cs/chromeos_public/src/third_party/autotest/files/client/site_tests/camera_HAL3/camera_HAL3.py?rcl=ae09491545ac4508c0fecdc581dfff3b67644e8b
    
    BUG=b:117817080
    CQ-DEPEND=CL:1482415
    TEST=tast run <dut> 'camera.HAL3*'. Note that camera.HAL3Frame and
         camera.HAL3StillCapture are current failing on many boards, just
         like the camera_HAL3 autotests.
    
    Change-Id: Ide5a18217ba54bf74f4949d16f5a69a71e913e8b

---
+++ src/chromiumos/tast/local/bundles/cros/camera/hal3_stream.go
@@ -0,0 +1,26 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/camera/hal3"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HAL3Stream,
+		Desc:         "Verifies camera stream function with HAL3 interface",
+		Contacts:     []string{"shik@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"arc_camera3"},
+	})
+}
+
+func HAL3Stream(ctx context.Context, s *testing.State) {
+	hal3.RunTest(ctx, s, hal3.TestConfig{GtestFilter: "Camera3StreamTest/*"})
+}
