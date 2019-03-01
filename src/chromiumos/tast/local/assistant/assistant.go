b3c35ce5d3a040c6726d57b150f3f5e63f5b60ef
Meilin Wang <meilinw@google.com>
2019-02-28 17:15:48 -0800

    Add assistant startup test in Tast.
    BUG:b:124415316
    TEST: tast -verbose run DUT assistant.Startup
    Change-Id: I4e69278844a171022e2bcc4c8e54963a17a50d28

---
+++ src/chromiumos/tast/local/assistant/assistant.go
@@ -0,0 +1,29 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package assistant supports interaction with Assistant service.
+package assistant
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+)
+
+// EnableAssistant brings up Google Assistant service and returns
+// any errors.
+func EnableAssistant(ctx context.Context, tconn *chrome.Conn) error {
+	return tconn.EvalPromise(ctx,
+		`new Promise((resolve, reject) => {
+		  chrome.autotestPrivate.setAssistantEnabled(true,
+        10 * 1000 /* timeout_ms */,
+        () => {
+		      if (chrome.runtime.lastError === undefined) {
+		        resolve();
+		      } else {
+		        reject(chrome.runtime.lastError.message);
+		      }
+		    });
+		  })`, nil)
+}
