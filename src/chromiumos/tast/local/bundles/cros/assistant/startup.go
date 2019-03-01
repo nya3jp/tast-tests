0e7c456c467a1ec20043939ae0206e8a83ae169e
Meilin Wang <meilinw@google.com>
2019-02-28 17:15:48 -0800

    Add assistant startup test in Tast without gaia login.
    This change added a new test in Tast for Assistant to test service
    startup, which was previously implemented using Autotest.
    
    BUG=b:124415316
    TEST=Test on DUT with command: tast -verbose run DUT assistant.Startup
    Change-Id: I4e69278844a171022e2bcc4c8e54963a17a50d28

---
+++ src/chromiumos/tast/local/bundles/cros/assistant/startup.go
@@ -0,0 +1,40 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package assistant
+
+import (
+	"context"
+
+	"chromiumos/tast/local/assistant"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Startup,
+		Desc:         "Starts Google Assistant service and checks the running status",
+		Contacts:     []string{"meilinw@chromium.org", "xiaohuic@chromium.org"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login"},
+	})
+}
+
+func Startup(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ExtraArgs("--enable-features=ChromeOSAssistant"))
+	if err != nil {
+		s.Fatal("Failed to log in: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	if err := assistant.EnableAssistant(ctx, tconn); err != nil {
+		s.Fatal("Failed to enable Assistant: ", err)
+	}
+}
