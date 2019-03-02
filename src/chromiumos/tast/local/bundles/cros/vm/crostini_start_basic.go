0d926434a9c698e0dc7468e4613f0dac0730cb1e
Stephen Barber <smbarber@chromium.org>
2019-03-01 19:15:02 -0800

    tast-tests: add vm.CrostiniStartBasic test
    Add a basic Crostini test that uses only the live, production VM and container
    images. The goal is to have a rock-solid test that can quickly graduate to
    provide CQ coverage for Crostini, and allow us to make progress on improving
    vm.CrostiniStartEverything.
    
    BUG=chromium:933580
    TEST=vm.CrostiniStartBasic
    
    Change-Id: I9b258dd86c22381099e971d0b443c960aa0dbb64
    Reviewed-on: https://chromium-review.googlesource.com/1497656
    Commit-Ready: Stephen Barber <smbarber@chromium.org>
    Tested-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/vm/crostini_start_basic.go
@@ -0,0 +1,67 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package vm
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/vm"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CrostiniStartBasic,
+		Desc:         "Tests basic Crostini startup only",
+		Contacts:     []string{"smbarber@chromium.org", "cros-containers-dev@google.com"},
+		Attr:         []string{"informational"},
+		Timeout:      5 * time.Minute,
+		SoftwareDeps: []string{"chrome_login", "vm_host"},
+	})
+}
+
+func CrostiniStartBasic(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	s.Log("Enabling Crostini preference setting")
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+	if err = vm.EnableCrostini(ctx, tconn); err != nil {
+		s.Fatal("Failed to enable Crostini preference setting: ", err)
+	}
+
+	s.Log("Setting up component")
+	err = vm.SetUpComponent(ctx, vm.ComponentUpdater)
+	if err != nil {
+		s.Fatal("Failed to set up component: ", err)
+	}
+
+	s.Log("Creating default container")
+	cont, err := vm.CreateDefaultContainer(ctx, s.OutDir(), cr.User(), vm.LiveImageServer)
+	if err != nil {
+		s.Fatal("Failed to set up default container: ", err)
+	}
+	defer vm.StopConcierge(ctx)
+	defer func() {
+		if err := cont.DumpLog(ctx, s.OutDir()); err != nil {
+			s.Error("Failure dumping container log: ", err)
+		}
+	}()
+
+	s.Log("Verifying pwd command works")
+	cmd := cont.Command(ctx, "pwd")
+	if err = cmd.Run(); err != nil {
+		cmd.DumpLog(ctx)
+		s.Fatal("Failed to run pwd: ", err)
+	}
+}
