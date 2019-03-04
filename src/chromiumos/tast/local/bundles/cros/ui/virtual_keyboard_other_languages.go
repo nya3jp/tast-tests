c5d41239f1da306a5920cf95f54c7e4e0886514d
Darren Shen <shend@chromium.org>
2019-03-04 11:22:47 +1100

    tast: Add test for Japanese input on virtual keyboard.
    We would like to have sanity checks for all supported languages on
    Chrome OS. We'll start with Japanese, which has two different input
    methods.
    
    The test just loops through the two input methods and checks whether
    the right suggestions show up when tapping certain keys.
    
    We'll be adding additional languages in the future.
    
    BUG=chromium:879073
    TEST=veyron_minnie
    Change-Id: I4bda68acc559c56fcc99a8c93845e77cc9614670

---
+++ src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_other_languages.go
@@ -0,0 +1,129 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/ui/vkb"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VirtualKeyboardOtherLanguages,
+		Desc:         "Checks that the virtual keyboard works for languages other than US English",
+		Contacts:     []string{"shend@chromium.org", "essential-inputs-team@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func VirtualKeyboardOtherLanguages(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ExtraArgs("--enable-virtual-keyboard"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	// Show a page with a text field that autofocuses. Turn off autocorrect as it
+	// can interfere with the test.
+	const html = `<input type="text" id="target" autocorrect="off" autofocus/>`
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		io.WriteString(w, html)
+	}))
+	defer server.Close()
+
+	conn, err := cr.NewConn(ctx, server.URL)
+	if err != nil {
+		s.Fatal("Creating renderer for test page failed: ", err)
+	}
+	defer conn.Close()
+
+	// Wait for the text field to focus.
+	if err := conn.WaitForExpr(ctx,
+		`document.getElementById('target') === document.activeElement`); err != nil {
+		s.Fatal("Failed to wait for text field to focus: ", err)
+	}
+
+	if err := vkb.ShowVirtualKeyboard(ctx, tconn); err != nil {
+		s.Fatal("Failed to show the virtual keyboard: ", err)
+	}
+
+	s.Log("Waiting for the virtual keyboard to show")
+	if err := vkb.WaitUntilShown(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for the virtual keyboard to show: ", err)
+	}
+
+	s.Log("Waiting for the virtual keyboard to render buttons")
+	if err := vkb.WaitUntilButtonsRender(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for the virtual keyboard to render: ", err)
+	}
+
+	kconn, err := vkb.UIConn(ctx, cr)
+	if err != nil {
+		s.Fatal("Failed to create connection to virtual keyboard UI: ", err)
+	}
+	defer kconn.Close()
+
+	// The input method ID is from:
+	// src/chrome/browser/resources/chromeos/input_method/google_xkb_manifest.json
+	testCases := []struct {
+		InputMethod        string
+		Keys               []string
+		ExpectedSuggestion string
+	}{
+		{"nacl_mozc_jp", []string{"n", "i", "h", "o", "n", "g", "o", "enter"}, "日本語"},
+		{"nacl_mozc_us", []string{"n", "i", "h", "o", "n", "g", "o", "enter"}, "日本語"},
+	}
+
+	const xkbExtensionID = "_comp_ime_jkghodnilhceideoidjikpgommlajknk"
+
+	for _, testCase := range testCases {
+		s.Log("Testing ", testCase.InputMethod, ", expecting: ", testCase.ExpectedSuggestion)
+
+		if err := vkb.SetCurrentInputMethod(ctx, tconn, xkbExtensionID+testCase.InputMethod); err != nil {
+			s.Error("Failed to set input method: ", err)
+			continue
+		}
+
+		if err := vkb.TapKeys(ctx, kconn, testCase.Keys); err != nil {
+			s.Error("Failed to type: ", err)
+			continue
+		}
+
+		s.Log("Waiting for the decoder to provide suggestions")
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			suggestions, err := vkb.GetSuggestions(ctx, kconn)
+			if err != nil {
+				return err
+			}
+			for _, suggestion := range suggestions {
+				if suggestion == testCase.ExpectedSuggestion {
+					return nil
+				}
+			}
+			return errors.Errorf("%q not found in suggestions", testCase.ExpectedSuggestion)
+		}, nil); err != nil {
+			s.Fatal("Failed to wait for suggestions to appear: ", err)
+		}
+
+		// Tap enter to exit composition mode.
+		if err := vkb.TapKey(ctx, kconn, "enter"); err != nil {
+			s.Fatal("Failed to tap enter: ", err)
+		}
+	}
+}
