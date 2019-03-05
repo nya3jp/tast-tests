46c970ca376628228bb5253034a0b82c3179e6a9
Pin-chih Lin <johnylin@google.com>
2019-03-05 14:10:53 +0800

    video: Add 2160p (4k) test cases for EncodeAccel(Perf)
    Add 2160p (4K, UHD) test cases to video encode accelerater test and performance
    test.
    
    BUG=chromium:927277
    TEST=run EncodeAccel(Perf)(H264|VP8)2160PI420 on Caroline.
    
    Change-Id: I4c20eed3cbfd6e332e2e30488236e4ea4c8bd697

--- src/chromiumos/tast/local/bundles/cros/video/encode/test_cases.go
+++ src/chromiumos/tast/local/bundles/cros/video/encode/test_cases.go
@@ -47,3 +47,11 @@ var Tulip180P = StreamParams{
 	Size:    videotype.NewSize(320, 180),
 	Bitrate: 100000,
 }
+
+// Crowd2160P is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "crowd-3840x2160.webm". The H264 level needs to be 51 (AVC Level5.1) for the capability of 2160p@30FPS encoding.
+var Crowd2160P = StreamParams{
+	Name:    "crowd-3840x2160.vp9.webm",
+	Size:    videotype.NewSize(3840, 2160),
+	Bitrate: 20000000,
+	Level:   51,
+}
---
+++ src/chromiumos/tast/local/bundles/cros/video/encode_accel_perf_vp8_2160p_i420.go
@@ -0,0 +1,34 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/encode"
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         EncodeAccelPerfVP82160PI420,
+		Desc:         "Runs Chrome video_encode_accelerator_unittest to measure the performance of VP8 encoding for 2160p I420 video",
+		Attr:         []string{"informational"},
+		Contacts:     []string{"johnylin@chromium.org", "chromeos-video-eng@google.com"},
+		SoftwareDeps: []string{caps.HWEncodeVP8_4K},
+		Data:         []string{encode.Crowd2160P.Name},
+	})
+}
+
+func EncodeAccelPerfVP82160PI420(ctx context.Context, s *testing.State) {
+	encode.RunAccelVideoPerfTest(ctx, s, encode.TestOptions{
+		Profile:     videotype.VP8Prof,
+		Params:      encode.Crowd2160P,
+		PixelFormat: videotype.I420,
+		InputMode:   encode.SharedMemory,
+	})
+}
