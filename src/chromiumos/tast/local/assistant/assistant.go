c983a125f561b8414f49325dd151334f8742b6c2
Meilin Wang <meilinw@google.com>
2019-03-05 12:29:30 -0800

    Add assistant_text_queries test in Tast.
    This change added a time query test ("what time is it in UTC?") for Assistant to test
    whether Assistant can return the correct time response.
    
    BUG=b:124415316
    TEST=Test on DUT with command: tast -verbose run DUT ui.AssistantTextQueries
    
    Change-Id: I97190d9d703745398acd565e6ae232ff61667b94

--- src/chromiumos/tast/local/assistant/assistant.go
+++ src/chromiumos/tast/local/assistant/assistant.go
@@ -7,9 +7,19 @@ package assistant
 
 import (
 	"context"
+	"fmt"
 
 	"chromiumos/tast/local/chrome"
 )
+
+// Response contains a subset of the results returned from the Assistant server when the
+// user emits a query.
+type Response struct {
+	// Fallback contains text messages used as the "fallback" for HTML card rendering.
+	// Generally the fallback text is similar to transcribed TTS, e.g. "It's exactly 6
+	// o'clock." or "Turning bluetooth off.".
+	Fallback string `json:"htmlFallback"`
+}
 
 // Enable brings up Google Assistant service and returns any errors.
 func Enable(ctx context.Context, tconn *chrome.Conn) error {
@@ -25,4 +35,24 @@ func Enable(ctx context.Context, tconn *chrome.Conn) error {
 		        }
 		      });
 		  })`, nil)
+}
+
+// SendTextQuery sends text query to Assistant and gets the query response.
+func SendTextQuery(ctx context.Context, tconn *chrome.Conn, query string) (Response, error) {
+	expr := fmt.Sprintf(
+		`new Promise((resolve, reject) => {
+		  chrome.autotestPrivate.sendAssistantTextQuery(%q,
+		      10 * 1000 /* timeout_ms */,
+		      function(response) {
+		        if (chrome.runtime.lastError === undefined) {
+		          resolve(response);
+		        } else {
+		          reject(chrome.runtime.lastError.message);
+		        }
+		      });
+		  })`, query)
+
+	var re Response
+	err := tconn.EvalPromise(ctx, expr, &re)
+	return re, err
 }
