55b5d94c8cfa176487285ee2e6dffb3785d4a48b
Meilin Wang <meilinw@google.com>
2019-03-05 12:29:30 -0800

    Add assistant_text_queries test in Tast.
    This change added a time query test ("what time is it in UTC?") for Assistant to test
    whether Assistant can return the correct time response.
    
    BUG=b:124415316
    TEST=Test on DUT with command: tast -verbose run DUT ui.AssistantTextQueries
    
    Change-Id: I97190d9d703745398acd565e6ae232ff61667b94
    Reviewed-on: https://chromium-review.googlesource.com/1504099
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/ui/assistant_text_queries.go
@@ -0,0 +1,127 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/assistant"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AssistantTextQueries,
+		Desc:         "Tests Assistant's responses to text queries",
+		Contacts:     []string{"meilinw@chromium.org", "xiaohuic@chromium.org"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login", "chrome_internal"},
+	})
+}
+
+func AssistantTextQueries(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ExtraArgs("--enable-features=ChromeOSAssistant"))
+	if err != nil {
+		s.Fatal("Failed to log in: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	// Starts Assistant service.
+	if err := assistant.Enable(ctx, tconn); err != nil {
+		s.Fatal("Failed to enable Assistant: ", err)
+	}
+
+	// Tests time query and verifies the result.
+	testAssistantTimeQuery(ctx, tconn, s)
+}
+
+// testAssistantTimeQuery verifies the correctness of the Assistant time response by parsing
+// the fallback string to a time.Time object and comparing it with the current UTC time.
+func testAssistantTimeQuery(ctx context.Context, tconn *chrome.Conn, s *testing.State) {
+	response, err := assistant.SendTextQuery(ctx, tconn, "what time is it in UTC?")
+	if err != nil {
+		s.Error("Failed to get Assistant time response: ", err)
+		return
+	}
+
+	if len(response.Fallback) == 0 {
+		s.Error("No htmlFallback field found in the response")
+		return
+	}
+
+	now := time.Now().UTC()
+	assistantTime, err := parseTimeNearNow(response.Fallback, now)
+	if err != nil {
+		s.Error("Failed to parse Assistant time response: ", err)
+		return
+	}
+
+	// Two time results may not exactly match due to the response latency or clock drift, so we
+	// instead check if |assistantTime| falls within an acceptable time range around |now|.
+	tolerance := time.Minute
+	earliest := now.Add(-tolerance)
+	latest := now.Add(tolerance)
+	if assistantTime.Before(earliest) || assistantTime.After(latest) {
+		s.Errorf("Response time %v not within %v of current time %v",
+			assistantTime, tolerance.Round(time.Second), now)
+		return
+	}
+}
+
+// parseTimeNearNow extracts the numeric components in the fallback string to construct a time
+// object based on now time. For this particular query, a typical fallback text will have format:
+// "It is (HH/H):MM AM/PM in the Coordinated Universal Time zone.".
+func parseTimeNearNow(fallback string, now time.Time) (time.Time, error) {
+	re := regexp.MustCompile(`(\d{1,2})(:\d\d)? ([AaPp][Mm])`)
+	matches := re.FindStringSubmatch(fallback)
+	if matches == nil {
+		return time.Time{}, errors.New("fallback doesn't contain any well-formatted time results")
+	}
+
+	// Parses hours and minutes from the time string.
+	hrs, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return time.Time{}, err
+	}
+	var min int
+	if len(matches[2]) != 0 {
+		min, err = strconv.Atoi(matches[2][1:])
+		if err != nil {
+			return time.Time{}, err
+		}
+	}
+
+	// Converts the time response to 24-hour clock to be consistent with the system time.
+	if strings.EqualFold(matches[3], "AM") {
+		if hrs == 12 {
+			hrs -= 12
+		}
+	} else if strings.EqualFold(matches[3], "PM") {
+		if hrs != 12 {
+			hrs += 12
+		}
+	}
+
+	// Because the fallback string only contains HH:MM, we choose the interpretation closest to
+	// the now time.
+	assistantTime := time.Date(now.Year(), now.Month(), now.Day(), hrs, min, 0, 0, now.Location())
+	if diff := assistantTime.Sub(now); diff > 12*time.Hour {
+		assistantTime.AddDate(0, 0, -1)
+	} else if diff < -12*time.Hour {
+		assistantTime.AddDate(0, 0, 1)
+	}
+	return assistantTime, nil
+}
