daa5c207311c5d938d089fdbf808125bf3946a26
Darren Shen <shend@chromium.org>
2019-03-10 22:01:21 +0000

    tast: Enable ui.VirtualKeyboardSuggestions.
    Make failures block the CQ. Test looks stable enough:
    https://stainless.corp.google.com/search?view=list&first_date=2019-03-08&last_date=2019-03-10&branch=master&test=tast.ui.VirtualKeyboardSuggestions&status=FAIL&status=ERROR&exclude_cts=false&exclude_not_run=false&exclude_non_release=false&exclude_au=false&exclude_acts=false&exclude_retried=false&exclude_non_production=false
    
    BUG=chromium:879073
    TEST=veyron_minnie
    CQ-DEPEND=CL:1487558
    
    Change-Id: I10f0a890f05dbb1995673fe13b85d5aa273c36e6
    Reviewed-on: https://chromium-review.googlesource.com/1514132
    Commit-Ready: Darren Shen <shend@chromium.org>
    Tested-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_suggestions.go
+++ src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_suggestions.go
@@ -21,8 +21,8 @@ func init() {
 		Func:         VirtualKeyboardSuggestions,
 		Desc:         "Checks that the virtual keyboard displays suggestions",
 		Contacts:     []string{"essential-inputs-team@google.com"},
-		Attr:         []string{"informational"},
-		SoftwareDeps: []string{"chrome_login"},
+		// "cros_internal" is needed to use the official proprietary virtual keyboard.
+		SoftwareDeps: []string{"chrome_login", "cros_internal"},
 	})
 }
 
