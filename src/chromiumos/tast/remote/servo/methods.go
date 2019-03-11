21d29e32c7817c54e8c04b9eaafd64e7291a2305
Jeff Carpenter <jeffcarp@chromium.org>
2019-03-11 14:40:46 -0700

    Add initial Servo library and example test
    BUG=chromium:867312
    TEST=?
    Change-Id: I8f6f3b6c539ca3c246a5a5d908c12048bf3b4f51

---
+++ src/chromiumos/tast/remote/servo/methods.go
@@ -0,0 +1,26 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package servo is used to communicate with servo devices connected to DUTs.
+// It communicates with servod over XML-RPC.
+// More details on servo: https://www.chromium.org/chromium-os/servo
+package servo
+
+import (
+	"context"
+)
+
+// Echo calls the Servo echo method.
+func (s *Servo) Echo(ctx context.Context, message string) (string, error) {
+	var val string
+	err := s.run(ctx, newCall("echo", message), &val)
+	return val, err
+}
+
+// PowerNormalPress calls the Servo power_normal_press method.
+func (s *Servo) PowerNormalPress(ctx context.Context) (bool, error) {
+	var val bool
+	err := s.run(ctx, newCall("power_normal_press"), &val)
+	return val, err
+}
---
+++ src/chromiumos/tast/remote/servo/servo_test.go
@@ -0,0 +1,39 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package servo
+
+import (
+	"testing"
+)
+
+func TestParseConnSpec(t *testing.T) {
+	for _, tc := range []struct {
+		input        string
+		expectedHost string
+		expectedPort int
+		expectErr    bool
+	}{
+		{"", "", 0, true},
+		{"localhost", "localhost", 9999, false},
+		{"localhost:1234", "localhost", 1234, false},
+		{"rutabaga:localhost:1234", "", 0, true},
+	} {
+		actualHost, actualPort, err := parseConnSpec(tc.input)
+		if err != nil && !tc.expectErr {
+			t.Errorf("input %q gave unexpected error: %q", tc.input, err)
+			return
+		}
+		if err == nil && tc.expectErr {
+			t.Errorf("input %q did not throw expected error: %q", tc.input, err)
+			return
+		}
+		if actualHost != tc.expectedHost {
+			t.Errorf("got %q; want %q", actualHost, tc.expectedHost)
+		}
+		if actualPort != tc.expectedPort {
+			t.Errorf("got %q; want %q", actualPort, tc.expectedPort)
+		}
+	}
+}
