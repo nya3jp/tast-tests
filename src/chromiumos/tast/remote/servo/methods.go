4259a3f197eaf51f3594e31b512b3132a21d2b4b
Jeff Carpenter <jeffcarp@chromium.org>
2019-03-11 14:40:46 -0700

    Add initial Servo library and example test
    BUG=chromium:867312
    TEST=?
    Change-Id: I8f6f3b6c539ca3c246a5a5d908c12048bf3b4f51
    Reviewed-on: https://chromium-review.googlesource.com/1509073
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Jeff Carpenter <jeffcarp@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

---
+++ src/chromiumos/tast/remote/servo/methods.go
@@ -0,0 +1,23 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package servo
+
+import (
+	"context"
+)
+
+// Echo calls the Servo echo method.
+func (s *Servo) Echo(ctx context.Context, message string) (string, error) {
+	var val string
+	err := s.run(ctx, newCall("echo", message), &val)
+	return val, err
+}
+
+// PowerNormalPress calls the Servo power_normal_press method.
+func (s *Servo) PowerNormalPress(ctx context.Context) (bool, error) {
+	var val bool
+	err := s.run(ctx, newCall("power_normal_press"), &val)
+	return val, err
+}
---
+++ src/chromiumos/tast/remote/servo/xmlrpc_test.go
@@ -0,0 +1,96 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package servo
+
+import (
+	"testing"
+)
+
+func TestXMLBooleanToBool(t *testing.T) {
+	for _, tc := range []struct {
+		input     string
+		expected  bool
+		expectErr bool
+	}{
+		{"1", true, false},
+		{"0", false, false},
+		{"rutabaga", false, true},
+	} {
+		actual, err := xmlBooleanToBool(tc.input)
+		if tc.expectErr {
+			if err == nil {
+				t.Errorf("xmlBooleanToBool(%q) unexpectedly succeeded", tc.input)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("xmlBooleanToBool(%q) failed: %v", tc.input, err)
+			}
+			if actual != tc.expected {
+				t.Errorf("xmlBooleanToBool(%q) = %v; want %v", tc.input, actual, tc.expected)
+			}
+		}
+	}
+}
+
+func TestBoolToXMLBoolean(t *testing.T) {
+	for _, tc := range []struct {
+		input    bool
+		expected string
+	}{
+		{true, "1"},
+		{false, "0"},
+	} {
+		actual := boolToXMLBoolean(tc.input)
+		if actual != tc.expected {
+			t.Errorf("boolToXMLBoolean(%v) = %q; want %q", tc.input, actual, tc.expected)
+		}
+	}
+}
+func TestNewValue(t *testing.T) {
+	expectedStr := "rutabaga"
+	v, err := newValue(expectedStr)
+	if err != nil {
+		t.Errorf("newValue(%q) failed: %v", expectedStr, err)
+		return
+	}
+	if v.String != expectedStr {
+		t.Errorf("got %q; want %q", v.String, expectedStr)
+	}
+
+	expectedBool := true
+	expectedBoolStr := "1"
+	v, err = newValue(expectedBool)
+	if err != nil {
+		t.Errorf("input %v gave unexpected error: %v", expectedStr, err)
+		return
+	}
+	if v.Boolean != expectedBoolStr {
+		t.Errorf("got %q; want %q", v.Boolean, expectedBoolStr)
+	}
+
+	expectedUnsupported := 1234
+	v, err = newValue(expectedUnsupported)
+	if err == nil {
+		t.Errorf("input %v did not throw expected error", expectedUnsupported)
+	}
+}
+
+func TestNewParams(t *testing.T) {
+	actual, err := newParams([]interface{}{"rutabaga", true})
+
+	if err != nil {
+		t.Errorf("got unexpected error: %v", err)
+		return
+	}
+	if len(actual) != 2 {
+		t.Errorf("got len %d; want %d", len(actual), 3)
+	}
+	if actual[0].Value.String != "rutabaga" {
+		t.Errorf("for first return value got %q; want %q", actual[0].Value.String, "rutabaga")
+	}
+	if actual[1].Value.Boolean != "1" {
+		t.Errorf("for second return value got %q; want %q", actual[1].Value.Boolean, "1")
+	}
+}
