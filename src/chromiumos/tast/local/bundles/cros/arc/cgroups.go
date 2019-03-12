922fc95b5d84363f00c57802cc0ed996cf35e448
Sonny Rao <sonnyrao@chromium.org>
2019-03-12 16:49:10 -0700

    test-test: add ARC cpu cgroups test
    This test verifies the ARC container CPU cgroup settings are behaving
    as expected.  It tests the value of the cpu.shares parameter set by
    session manager when Chrome throttles and unthrottles the container.
    
    BUG=chromium:931580
    TEST=run arc.Cgroups
    
    Change-Id: I79262e1ea3cc4b4132acacee840b36a6ed65903c

---
+++ src/chromiumos/tast/local/bundles/cros/arc/cgroups.go
@@ -0,0 +1,129 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Cgroups,
+		Desc:         "Checks that foreground/background status of ARC applications reflects properly in cgroup limits",
+		Contacts:     []string{"sonnyrao@chromium.org", "arc-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "android_p", "chrome"},
+		Timeout:      4 * time.Minute,
+	})
+}
+
+// see platform2/login_manager/session_manager_impl.cc for defintion of these constants.
+const (
+	// cpuShares is a file that indicates the relative amount of CPU this cgroup gets when there's contention
+	cpuShares = "/sys/fs/cgroup/cpu/session_manager_containers/cpu.shares"
+	// cpuSharesPreArcBoot is the value for cpu.shares before ARC is done booting
+	cpuSharesArcPreBoot = 32
+	// cpuSharesArcBackground is the value for cpu.shares when ARC has nothing in the foreground
+	cpuSharesArcBackground = 64
+	// cpuSharesArcForegroun is the value of cpu.shares when ARC is in the foreground
+	cpuSharesArcForeground = 1024
+)
+
+// Retrieve the current value for cpu.shares for the container
+func getCPUCgroupShares(ctx context.Context) (int, error) {
+	b, err := ioutil.ReadFile(cpuShares)
+	if err != nil {
+		return 0, err
+	}
+	shares, err := strconv.Atoi(strings.TrimSpace(string(b)))
+	if err != nil {
+		return 0, errors.Wrapf(err, "bad integer: %q", b)
+	}
+	return shares, nil
+}
+
+func Cgroups(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.RestrictARCCPU())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	// Check shares before ARC starts
+	share, err := getCPUCgroupShares(ctx)
+	if err != nil {
+		s.Fatal("Failed to get ARC CPU shares value: ", err)
+	}
+	if share != cpuSharesArcPreBoot {
+		s.Fatal("Unexpected ARC CPU shares value pre-boot:  ", share)
+	}
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	// Check shares after ARC is done booting but nothing is active
+	share, err = getCPUCgroupShares(ctx)
+	if err != nil {
+		s.Fatal("Failed to get ARC CPU shares value: ", err)
+	}
+	if share != cpuSharesArcBackground {
+		s.Fatal("Unexpected ARC CPU shares value for background:  ", share)
+	}
+
+	act, err := arc.NewActivity(a, "com.android.settings", ".Settings")
+	if err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx); err != nil {
+		s.Fatal("Failed start Settings activity: ", err)
+	}
+
+	if err := act.SetWindowState(ctx, arc.WindowStateNormal); err != nil {
+		s.Fatal("Failed to set window state to Normal: ", err)
+	}
+
+	if err := act.WaitForIdle(ctx, 4*time.Second); err != nil {
+		s.Fatal("Failed to wait for idle activity: ", err)
+	}
+
+	// Check shares after ARC window is up and in the foreground
+	share, err = getCPUCgroupShares(ctx)
+	if err != nil {
+		s.Fatal("Failed to get ARC CPU shares value: ", err)
+	}
+	if share != cpuSharesArcForeground {
+		s.Fatal("Unexpected ARC CPU shares value foreground:  ", share)
+	}
+
+	// Close ARC window and ensure we go back to background shares
+	if err := act.SetWindowState(ctx, arc.WindowStateMinimized); err != nil {
+		s.Fatal("Failed to set window state to Minimized: ", err)
+	}
+	if err := act.WaitForIdle(ctx, 4*time.Second); err != nil {
+		s.Fatal("Failed to wait for idle activity: ", err)
+	}
+
+	share, err = getCPUCgroupShares(ctx)
+	if err != nil {
+		s.Fatal("Failed to get ARC CPU shares value: ", err)
+	}
+	if share != cpuSharesArcBackground {
+		s.Fatal("Unexpected ARC CPU shares value after minimize:  ", share)
+	}
+}
