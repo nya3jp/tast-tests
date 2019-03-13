73523128b67c874c0ffcb810d50a5c4e5e3a1526
Daniel Erat <derat@chromium.org>
2019-03-13 07:24:27 -0700

    tast-tests: Add security.PtraceThread test.
    Port the thread-specific portion of the
    security_ptraceRestrictions Autotest test to a new
    security.PtraceThread Tast test.
    
    This thread executes a thread-prctl C executable installed
    by the tast-local-helpers-cros package.
    
    BUG=chromium:911879
    TEST=ran it
    CQ-DEPEND=I5ce55f6515e44304206fbb7907226b56aa01abae
    
    Change-Id: Ic4dc5f282f94c6d28a6ba9c4e8d81893f8a994f5

---
+++ src/chromiumos/tast/local/bundles/cros/security/ptrace_thread.go
@@ -0,0 +1,70 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"context"
+	"strconv"
+	"syscall"
+
+	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PtraceThread,
+		Desc: "Checks that the kernel restricts ptrace between threads",
+		Contacts: []string{
+			"jorgelo@chromium.org", // Security team
+			"derat@chromium.org",   // Tast port author
+			"chromeos-security@google.com",
+		},
+		Attr: []string{"informational"},
+	})
+}
+
+func PtraceThread(ctx context.Context, s *testing.State) {
+	// TODO(derat): Consider moving the base helper path to a shared constant somewhere.
+	const threadPrctlPath = "/usr/local/libexec/tast/helpers/local/cros/security.PtraceThread.thread-prctl"
+
+	// See the thread-prctl executable installed by the security_tests package for details.
+	type behavior int
+	const (
+		tracerForksTracee                   behavior = 0
+		traceeCallsPrctlFromMainProcess              = 1
+		traceeCallsPrctlFromNonLeaderThread          = 2
+	)
+
+	type source int
+	const (
+		ptraceFromNonLeaderThread source = 0
+		ptraceFromMainProcess            = 1
+	)
+
+	runThreadTest := func(desc string, bh behavior, src source) {
+		// Return early if we've already timed out.
+		if ctx.Err() != nil {
+			return
+		}
+
+		s.Log("Testing ", desc)
+		cmd := testexec.CommandContext(ctx, threadPrctlPath, strconv.Itoa(int(bh)), strconv.Itoa(int(src)))
+		cmd.Cred(syscall.Credential{Uid: sysutil.ChronosUID, Gid: sysutil.ChronosGID})
+		if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+			s.Errorf("%v failed: %v", desc, err)
+		}
+	}
+
+	runThreadTest("ptrace child from parent", tracerForksTracee, ptraceFromMainProcess)
+	runThreadTest("ptrace child from thread", tracerForksTracee, ptraceFromNonLeaderThread)
+
+	runThreadTest("PR_SET_PTRACER from main process", traceeCallsPrctlFromMainProcess, ptraceFromMainProcess)
+	runThreadTest("PR_SET_PTRACER from thread", traceeCallsPrctlFromNonLeaderThread, ptraceFromMainProcess)
+
+	runThreadTest("ptrace from main process after PR_SET_PTRACER", traceeCallsPrctlFromMainProcess, ptraceFromNonLeaderThread)
+	runThreadTest("ptrace from thread after PR_SET_PTRACER", traceeCallsPrctlFromNonLeaderThread, ptraceFromNonLeaderThread)
+}
