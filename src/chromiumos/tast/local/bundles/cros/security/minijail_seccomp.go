96087a4bc98bf28053487e86955a6d019ae47cd1
Daniel Erat <derat@chromium.org>
2019-03-14 15:51:09 -0700

    tast-tests: Make security.MinijailSeccomp run on CQ.
    Remove the "informational" attribute from the
    security.MinijailSeccomp test to make it run on the CQ.
    
    BUG=chromium:911879
    TEST=stainless reports no failures on release builders in
         last 14 days
    
    Change-Id: If0edbd1d600d1bdac802f0ad784c4eaab013d38f

--- src/chromiumos/tast/local/bundles/cros/security/minijail_seccomp.go
+++ src/chromiumos/tast/local/bundles/cros/security/minijail_seccomp.go
@@ -21,7 +21,6 @@ func init() {
 			"derat@chromium.org",   // Tast port author
 			"chromeos-security@google.com",
 		},
-		Attr: []string{"informational"},
 		Data: []string{
 			minijailSeccompDefaultPolicy,
 			minijailSeccompReadOnlyPolicy,
