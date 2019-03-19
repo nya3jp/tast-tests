74b6c0044f218cd6a7c78f9b7ee4a2e22aa5d50e
Sheng-Hao Tsao <shenghao@google.com>
2019-03-19 17:33:49 +0800

    Migrate MediaRecorderHWEncodeUsed test to tast
    This CL migrates the video_MediaRecorderHWEncodeUsed autotest to tast.
    
    BUG=chromium:917745
    TEST=Verified on eve that h264 and vp8 pass while vp9 fails.
    
    Change-Id: I53c2f1a8995db37dd3e9207e34f7a9b859ae83ed

---
+++ src/chromiumos/tast/local/bundles/cros/video/media_recorder_encode_accel_used_h264.go
@@ -0,0 +1,28 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/bundles/cros/video/mediarecorder"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MediaRecorderEncodeAccelUsedH264,
+		Desc:         "Checks H.264 video encode acceleration is used in MediaRecorder",
+		Contacts:     []string{"shenghao@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome_login", "chrome_internal", caps.HWEncodeH264},
+		Data:         []string{"loopback_media_recorder.html"},
+	})
+}
+
+func MediaRecorderEncodeAccelUsedH264(ctx context.Context, s *testing.State) {
+	mediarecorder.VerifyEncodeAccelUsed(ctx, s, "h264")
+}
---
+++ src/chromiumos/tast/local/bundles/cros/video/mediarecorder/media_recorder.go
@@ -0,0 +1,79 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package mediarecorder provides common code for video.MediaRecorder tests.
+package mediarecorder
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/video/lib/constants"
+	"chromiumos/tast/local/bundles/cros/video/lib/logging"
+	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/testing"
+)
+
+// VerifyEncodeAccelUsed checks whether HW encode is used for given codec when running
+// MediaRecorder.
+func VerifyEncodeAccelUsed(ctx context.Context, s *testing.State, codec videotype.Codec) {
+	chromeArgs := []string{
+		logging.ChromeVmoduleFlag(),
+		// See https://webrtc.org/testing/
+		// "--use-fake-device-for-media-stream" avoids the need to grant camera/microphone permissions.
+		// "--use-fake-ui-for-media-stream" feeds a test pattern to getUserMedia() instead of live camera input.
+		"--use-fake-device-for-media-stream",
+		"--use-fake-ui-for-media-stream",
+	}
+
+	cr, err := chrome.New(ctx, chrome.ExtraArgs(chromeArgs...))
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	initHistogram, err := metrics.GetHistogram(ctx, cr, constants.MediaRecorderVEAUsed)
+	if err != nil {
+		s.Fatal("Failed to get initial histogram: ", err)
+	}
+
+	conn, err := cr.NewConn(ctx, server.URL+"/loopback_media_recorder.html")
+	if err != nil {
+		s.Fatal("Failed to open recorder page: ", err)
+	}
+	defer conn.Close()
+
+	if err := conn.WaitForExpr(ctx, "pageLoaded"); err != nil {
+		s.Fatal("Timed out waiting for page loading: ", err)
+	}
+
+	startRecordJS := fmt.Sprintf("startRecording('%s')", codec)
+	if err := conn.EvalPromise(ctx, startRecordJS, nil); err != nil {
+		s.Fatal("Failed to evaluate %v: %v", startRecordJS, err)
+	}
+
+	histogramDiff, err := metrics.WaitForHistogramUpdate(ctx, cr, constants.MediaRecorderVEAUsed, initHistogram, 5*time.Second)
+	if err != nil {
+		s.Fatal("Failed to get histogram diff: ", err)
+	}
+
+	if len(histogramDiff.Buckets) > 1 {
+		s.Fatal("Unexpected histogram update: ", histogramDiff)
+	}
+
+	bucket := histogramDiff.Buckets[0]
+	bucketValue := int64(constants.MediaRecorderVEAUsedSuccess)
+	// Expected histogram is [bucketValue, bucketValue+1, 1].
+	if bucket.Min != bucketValue || bucket.Max != bucketValue+1 || bucket.Count != 1 {
+		s.Error("Unexpected histogram update: want [%d, %d, 1], got %v", bucketValue, bucketValue+1, bucket)
+	}
+}
