11d07e976dacf6b96d51f2d9e9b783599eaf9b17
Hidehiko Abe <hidehiko@chromium.org>
2019-03-26 21:54:47 +0900

    tast-tests: port graphics_Drm vk_glow test.
    BUG=chromium:934072
    TEST=Ran on DUT.
    CQ-DEPEND=CL:1539422, CL:1538165
    
    Change-Id: I6e60fea0d4b5c8d6f3e231b774cc9b8dc93e6ac6

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/drm_vk_glow.go
@@ -0,0 +1,36 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/graphics/drm"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DRMVKGlow,
+		Desc: "Verifies DRM vk_glow runs successfully",
+		Contacts: []string{
+			"andrescj@chromium.org",
+			"chromeos-gfx@google.com",
+			"hidehiko@chromium.org", // Tast port.
+		},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"display_backlight", "vulkan"},
+	})
+}
+
+func DRMVKGlow(ctx context.Context, s *testing.State) {
+	if err := drm.SetUp(ctx); err != nil {
+		s.Fatal("Failed to set up the DRM test: ", err)
+	}
+	defer drm.TearDown(ctx)
+
+	drm.RunTest(ctx, s, 20*time.Second, "/usr/local/bin/vk_glow")
+}
