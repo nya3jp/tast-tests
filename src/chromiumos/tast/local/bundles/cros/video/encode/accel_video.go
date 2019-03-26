2376333455a0326cb9a1739f910ef88f1c6795e1
Pin-chih Lin <johnylin@google.com>
2019-03-26 20:55:04 +0800

    tast-tests: improve ARC test result checking
    The true result of ARC tests need to be parsed from xml file generated
    by --gtest_output.
    
    We temporarily use parse functions in tast/local/bundles/cros/camera/hal3,
    and should be changed to use functions from gtest package once they are
    merged (crbug.com/946390).
    
    BUG=b:118848258
    TEST=tast run $(DUT) video.ARCEncodeAccelH264I420
    
    Change-Id: I768929d96f017694bb1e4db59ae1a8563b9e77a6

--- src/chromiumos/tast/local/bundles/cros/video/encode/accel_video.go
+++ src/chromiumos/tast/local/bundles/cros/video/encode/accel_video.go
@@ -18,6 +18,7 @@ import (
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/camera/hal3"
 	"chromiumos/tast/local/bundles/cros/video/lib/cpu"
 	"chromiumos/tast/local/bundles/cros/video/lib/logging"
 	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
@@ -26,6 +27,7 @@ import (
 	"chromiumos/tast/local/perf"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/shutil"
 	"chromiumos/tast/testing"
 )
 
@@ -199,23 +201,55 @@ func runARCVideoTest(ctx context.Context, s *testing.State, opts TestOptions, ba
 
 	// Execute binary in ARC.
 	for _, exec := range execs {
-		s.Logf("Running %v %v", exec, strings.Join(args, " "))
-		cmd := a.Command(ctx, exec, args...)
-		out, err := cmd.Output()
-		if err != nil {
+		if err := runARCBinary(ctx, a, exec, args, s.OutDir()); err != nil {
 			s.Errorf("Failed to run %v: %v", exec, err)
-			cmd.DumpLog(ctx)
-			continue
-		}
-		// Because the return value of the adb command is always 0, we cannot use the value to determine whether the test passes.
-		// Therefore we parse the output result as alternative.
-		if err := ioutil.WriteFile(filepath.Join(s.OutDir(), filepath.Base(exec)+".log"), out, 0644); err != nil {
-			s.Error("Failed to write output to file: ", err)
-		}
-		if strings.Contains(string(out), "FAILED TEST") {
-			s.Errorf("Test failed: %s %s", exec, strings.Join(args, " "))
 		}
 	}
+}
+
+// runARCBinary runs exec once and produces gtest xml output and log files.
+// Always report by --gtest_output because we cannot rely on the return value of the adb command to
+// determine whether the test passes (which is always 0). Parse from gtest output as alternative.
+func runARCBinary(ctx context.Context, a *arc.ARC, exec string, args []string, outDir string) error {
+	xmlPath := filepath.Join(arc.ARCTmpDirPath, filepath.Base(exec)+".xml")
+	execArgs := append(args, "--gtest_output=xml:"+xmlPath)
+
+	out, err := os.Create(filepath.Join(outDir, filepath.Base(exec)+".log"))
+	if err != nil {
+		return errors.Wrap(err, "failed to create output log file")
+	}
+	defer out.Close()
+
+	cmd := a.Command(ctx, exec, execArgs...)
+	cmd.Stdout = out
+
+	testing.ContextLog(ctx, "Running ", shutil.EscapeSlice(cmd.Args))
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrapf(err, "failed to run %v", exec)
+	}
+
+	if err := a.PullFile(ctx, xmlPath, outDir); err != nil {
+		return errors.Wrapf(err, "failed to pull file %v from ARC", xmlPath)
+	}
+	xmlOutPath := filepath.Join(outDir, filepath.Base(xmlPath))
+	xml, err := ioutil.ReadFile(xmlOutPath)
+	if err != nil {
+		return errors.Wrapf(err, "failed to read file %v", xmlOutPath)
+	}
+
+	// extractFailedTests in tast/local/chrome/bintest cannot be used here because gtest has different
+	// versions on Chrome and Chrome OS.
+	// TODO(johnylin): use common gtest parse function instead once the gtest package is merged.
+	//                 crbug.com/946390
+	failures, err := hal3.GetFailedTestNames(strings.NewReader(string(xml)))
+	if err != nil {
+		return errors.Wrapf(err, "failed to get failed tests from %v", xmlOutPath)
+	}
+	if len(failures) > 0 {
+		return errors.Errorf("failed to run %v with %d test failure(s): %v",
+			exec, len(failures), strings.Join(failures, ", "))
+	}
+	return nil
 }
 
 // runBinaryTestWithCPUMeasurement measures CPU usage while running video_encode_accelerator_unittest asynchronously.
