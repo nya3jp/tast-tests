1e81b39f3b73dcf033c62252f378b71bab255e41
Sheng-Hao Tsao <shenghao@google.com>
2019-03-28 18:26:18 +0800

    Add MediaRecorderPerf tast test
    This CL adds MediaRecorderPerf test to tast. It measures frame
    processing time and CPU usage during recording with MediaRecorder APIs.
    
    BUG=b:130848417
    CQ-DEPEND=CL:1575257
    TEST=Run on eve and the numbers look consistent to crosbolt results of
    the existing autotest
    
    Change-Id: I076f114e59a2ea28a4e8826dbc31024e166b60e2

---
+++ src/chromiumos/tast/local/bundles/cros/video/media_recorder_perf_h264.go
@@ -0,0 +1,40 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
+	"chromiumos/tast/local/bundles/cros/video/mediarecorder"
+	"chromiumos/tast/testing"
+)
+
+const (
+	mediaRecorderH264StreamFile = "crowd720_25frames.y4m"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     MediaRecorderPerfH264,
+		Desc:     "Captures performance data about MediaRecorder for SW and HW with H.264",
+		Contacts: []string{"shenghao@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:     []string{"group:crosbolt", "crosbolt_perbuild"},
+		// "chrome_internal" is needed because H.264 is a proprietary codec.
+		SoftwareDeps: []string{"chrome_login", "chrome_internal"},
+		Data:         []string{mediaRecorderH264StreamFile, "loopback_media_recorder.html"},
+	})
+}
+
+// MediaRecorderPerfH264 captures the perf data of MediaRecorder for HW and SW cases with H.264 codec and uploads to server.
+func MediaRecorderPerfH264(ctx context.Context, s *testing.State) {
+	const (
+		fps   = 30
+		codec = videotype.H264
+	)
+	if err := mediarecorder.MeasurePerf(ctx, s.DataFileSystem(), s.OutDir(), codec, s.DataPath(mediaRecorderH264StreamFile), fps); err != nil {
+		s.Error("Failed to measure performance: ", err)
+	}
+}
