cd3cde0a2302fb64d7c350a492344e8efeb2e889
Luigi Semenzato <semenzato@chromium.org>
2019-04-02 14:26:23 -0700

    platform.MemoryPressure: measure LRU tab switches
    This changes the test in multiple ways.
    
    First, the test no longer does preallocation.  The test timeout
    is increased to 2 hours to support the larger devices (16GB RAM).
    
    Second, at the end of phase 1, the test performs one set
    of tab switches to a number of LRU tabs (also called "cold tabs")
    and measures the times for those.
    
    Third, the test tries to be resilient to attempts to switch to
    a discarded tab.  A broken devtools connection means that the
    tab has either crashed or discarded.  After we detect the first
    discard (via the chrome dbus signal) we assume that a broken
    connection is due to the discard.  This can have false negatives
    but they should be relatively rare.  Before the first discard,
    broken connections cause fatal errors.
    
    Fourth, some additional system measurements (direct and computed)
    are added.
    
    BUG=none
    TEST=ran the test
    
    Change-Id: I47b1209099ddea1fdb077727d1a2745d888fbf11

--- src/chromiumos/tast/local/bundles/cros/platform/memory_pressure.go
+++ src/chromiumos/tast/local/bundles/cros/platform/memory_pressure.go
@@ -18,11 +18,9 @@ func init() {
 		Desc:     "Create memory pressure and collect various measurements from Chrome and from the kernel",
 		Contacts: []string{"semenzato@chromium.org", "sonnyrao@chromium.org", "chromeos-memory@google.com"},
 		Attr:     []string{"group:crosbolt", "crosbolt_nightly"},
-		Timeout:  30 * time.Minute,
+		Timeout:  120 * time.Minute,
 		Data: []string{
-			mempressure.CompressibleData,
 			mempressure.DormantCode,
-			mempressure.PreallocatorScript,
 			mempressure.WPRArchiveName,
 		},
 		SoftwareDeps: []string{"chrome_login"},
@@ -33,9 +31,7 @@ func init() {
 func MemoryPressure(ctx context.Context, s *testing.State) {
 	p := &mempressure.RunParameters{
 		DormantCodePath:          s.DataPath(mempressure.DormantCode),
-		PageFilePath:             s.DataPath(mempressure.CompressibleData),
 		PageFileCompressionRatio: 0.40,
-		PreallocatorPath:         s.DataPath(mempressure.PreallocatorScript),
 		WPRArchivePath:           s.DataPath(mempressure.WPRArchiveName),
 	}
 	mempressure.Run(ctx, s, p)
