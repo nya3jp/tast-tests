16516c3f17eeeb0eb3526f1194547e69dd9258ec
Srilakshmi Manjunath <srilakshmi.manjunath@intel.com>
2019-04-03 14:43:41 +0530

    Multi tab video playback from diferrent source
    BUG=none
    TEST=Run the below command for the test execution,the test should open
         three tabs i.e a file from chromiumos-test-assets-public, youtube
         and one from the server and check the video playback for the same .
    
    tast -verbose run -buildtype=local -build=true <DUT IP> video.VideoTabs
    
    Change-Id: If26a05ddde86c207175fa122bed9f30c1d7a4191
    Signed-off-by: Srilakshmi Manjunath <srilakshmi.manjunath@intel.com>

---
+++ src/chromiumos/tast/local/bundles/cros/video/video_tabs.go
@@ -0,0 +1,38 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+        "context"
+        "time"
+        "chromiumos/tast/local/bundles/cros/video/play"
+        "chromiumos/tast/local/chrome"
+        "chromiumos/tast/testing"
+)
+
+func init() {
+
+        testing.AddTest(&testing.Test{
+                Func:         VideoTabs,
+                Desc:         "Checks video playback in three tabs i.e local file-google drive video file, youtube and one from the server",
+                Contacts:     []string{"keiichiw@chromium.org", "chromeos-video-eng@google.com"},
+                Attr:         []string{"informational"},
+                SoftwareDeps: []string{"chrome_login"},
+                Pre:          chrome.LoggedIn(),
+                Data:         []string{"bear-320x240.vp9.webm","bear.mjpeg.external", "video.html","youtube.html"},
+        })
+}
+
+// Checks video playback in three tabs i.e local file-google drive video file, youtube and one from the server
+func VideoTabs(ctx context.Context, s *testing.State) {
+        time.Sleep(5 * time.Second)
+        play.TestPlay(ctx, s, s.PreValue().(*chrome.Chrome),
+                "bear-320x240.vp9.webm", play.NormalVideo,play.NoCheckHistogram)
+	play.TestPlay(ctx, s, s.PreValue().(*chrome.Chrome),"", play.YoutubeVideo,play.NoCheckHistogram)
+        play.TestPlay(ctx, s, s.PreValue().(*chrome.Chrome),
+                "http://commondatastorage.googleapis.com/chromiumos-test-assets-public/traffic/traffic-1920x1080-8005020218f6b86bfa978e550d04956e.mp4", play.NormalVideo,play.NoCheckHistogram)
+        }
+
+
