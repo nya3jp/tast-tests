534ac26b152675698a894b7de615f4aacae6d9a5
Shuhei Takahashi <nya@chromium.org>
2019-04-03 15:02:03 +0900

    video: Promote stable video.EncodeAccelH264* tests.
    These 3 tests are consistently passing at least for 2 weeks.
    
    http://tastboard/test/video.EncodeAccelH264180PI420
    http://tastboard/test/video.EncodeAccelH264360PI420
    http://tastboard/test/video.EncodeAccelH264I420
    
    BUG=None
    TEST=fast_build.sh -T  # to check build
    
    Change-Id: Ia9745747a211a4e07cb98910f185d5d3a4d4eb6f
    Reviewed-on: https://chromium-review.googlesource.com/1549137
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/encode_accel_h264_i420.go
+++ src/chromiumos/tast/local/bundles/cros/video/encode_accel_h264_i420.go
@@ -18,7 +18,6 @@ func init() {
 		Func:         EncodeAccelH264I420,
 		Desc:         "Run Chrome video_encode_accelerator_unittest from I420 raw frames to H264 stream",
 		Contacts:     []string{"hiroh@chromium.org", "chromeos-video-eng@google.com"},
-		Attr:         []string{"informational"},
 		SoftwareDeps: []string{caps.HWEncodeH264},
 		Data:         []string{encode.Bear192P.Name},
 	})
