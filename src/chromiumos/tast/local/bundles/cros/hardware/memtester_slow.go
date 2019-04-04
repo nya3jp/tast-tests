5ccebc2930b3a45611944a9a5904d067858d4328
Daniel Erat <derat@chromium.org>
2019-04-04 13:35:08 -0700

    tast-tests: Add hardware.Memtester{Fast,Slow}.
    Port the hardware_Memtester.quick Autotest test to Tast as a
    new hardware.MemtesterFast local test. It just runs the
    external memtester command to test 10 MiB of memory. Also
    add a slower disabled hardware.MemtesterSlow test that
    matches the standard hardware_Memtester Autotest test.
    
    This change also introduces a new "hardware" test category.
    The existing "platform" category doesn't seem like a great
    fit for these tests, since "platform" primarily tests
    userspace functionality. We may want to consider moving the
    existing platform.TPMResponsive test to the hardware
    category.
    
    BUG=chromium:949744
    TEST=manual: ran both tests against a caroline device
    
    Change-Id: Ib30200b3c34b69f7fe5de2c170e40d7a6f836800
    Reviewed-on: https://chromium-review.googlesource.com/1553723
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/hardware/memtester_slow.go
@@ -0,0 +1,51 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hardware
+
+import (
+	"context"
+	"time"
+
+	"github.com/shirou/gopsutil/mem"
+
+	"chromiumos/tast/local/bundles/cros/hardware/memtester"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:    MemtesterSlow,
+		Desc:    "Runs one iteration of memtester using 95% of free memory to find memory subsystem faults",
+		Timeout: 12 * time.Hour, // 5-10 minutes per GiB of memory is common
+		Contacts: []string{
+			"puthik@chromium.org", // Original Autotest author
+			"derat@chromium.org",  // Tast port author
+			"cros-partner-avl@google.com",
+		},
+		Attr: []string{
+			"disabled", // this test can be very slow
+			"informational",
+		},
+	})
+}
+
+func MemtesterSlow(ctx context.Context, s *testing.State) {
+	vmstat, err := mem.VirtualMemory()
+	if err != nil {
+		s.Fatal("Failed to get memory stats: ", err)
+	}
+	const mb = 1024 * 1024
+	useBytes := int64(float64(vmstat.Free) * 0.95)
+
+	s.Logf("Testing %.1f MiB (system is using %.1f of %.1f MB)",
+		float64(useBytes)/mb, float64(vmstat.Used)/mb, float64(vmstat.Total)/mb)
+	// TODO(derat): Switch this to perform 100 iterations and make it run during hardware
+	// qualification (see the hardware_Memtester.memory_qual Autotest test). That can take
+	// many hours to complete, so we should probably also parse the output from the memtester
+	// process so we can log progress updates.
+	if err := memtester.Run(ctx, useBytes, 1); err != nil {
+		s.Fatal("memtester failed: ", err)
+	}
+}
