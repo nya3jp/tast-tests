02eabd44491fd44f92bd273c4c3cdb0d4b0b83f6
Jasmine Chen <lnishan@google.com>
2019-04-08 17:22:15 +0800

    Add still capture and recording tests for GoogleCameraArc (GCA)
    This CL adds still capture and recording tests for GoogleCameraArc
    (GCA).
    GCAStillCapture: Takes a photo with default resolution.
    GCARecording: Records a 3-second video with default resolution.
    
    BUG=b:117816663
    TEST=tast run ${DUT} 'camera.GCAStillCapture'
    TEST=tast run ${DUT} 'camera.GCARecording'
    
    Change-Id: Icf59983f95715021b180b526ef77c19b14c11745

---
+++ src/chromiumos/tast/local/bundles/cros/camera/gca_recording.go
@@ -0,0 +1,43 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/camera/gca"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GCARecording,
+		Desc:         "Tests video recording with GoogleCameraArc (GCA)",
+		Contacts:     []string{"lnishan@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+		Pre:          arc.Booted(),
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func GCARecording(ctx context.Context, s *testing.State) {
+	gca.RunTest(ctx, s, func(ctx context.Context, s *testing.State, d *ui.Device) {
+		// Switch to video mode.
+		gca.SwitchMode(ctx, s, d, gca.VideoMode)
+
+		// Start recording.
+		gca.ClickShutterButton(ctx, s, d)
+
+		// Record for 3 seconds.
+		testing.Sleep(ctx, 3*time.Second) // NOLINT: We actually need to wait for 3 seconds for recording.
+
+		// Stop recording by clicking on the shutter button again.
+		gca.ClickShutterButtonAndVerifyFile(ctx, s, d, gca.VideoFormat)
+	})
+}
