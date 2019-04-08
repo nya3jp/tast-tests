6ee87deb5526cda31cb6669dc940ad7880a38736
Shuhei Takahashi <nya@chromium.org>
2019-04-08 11:13:33 +0900

    video: Set timeout for video.EncodeAccelH264* tests.
    After several runs, I found those tests take up to 3 minutes
    when they pass.
    
    Raw data:
    
                                 cnt   min   max   avg
    EncodeAccelH264180PI420      298    7s   53s   23s
    EncodeAccelH264360PI420      298   13s   76s   38s
    EncodeAccelH264720PI420      280   40s  170s   97s
    EncodeAccelH2641080PI420     272   43s  165s   92s
    EncodeAccelH2642160PI420     143   51s  121s   84s
    
    BUG=chromium:949008
    TEST=fast_build.sh -T  # to check build
    
    Change-Id: I2d595aa9890952fa4f4c948c71897cb609e7d3a5
    Reviewed-on: https://chromium-review.googlesource.com/1557295
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/encode_accel_h264_720p_i420.go
+++ src/chromiumos/tast/local/bundles/cros/video/encode_accel_h264_720p_i420.go
@@ -22,9 +22,8 @@ func init() {
 		Attr:         []string{"informational"},
 		SoftwareDeps: []string{caps.HWEncodeH264},
 		Data:         []string{encode.Tulip720P.Name},
-		// TODO(crbug.com/948593): This timeout can be longer than it needs to be.
-		// Revisit it soon after measuring actual run time.
-		Timeout: 5 * time.Minute,
+		// This test takes up to 3 minutes when it passes. Allow 4 minutes with 1-minute buffer.
+		Timeout: 4 * time.Minute,
 	})
 }
 
