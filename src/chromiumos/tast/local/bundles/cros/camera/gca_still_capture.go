e7e37219f57415202df64e8484b42b25efcd8842
Jasmine Chen <lnishan@google.com>
2019-04-08 17:22:15 +0800

    Add still capture and recording tests for GoogleCameraArc (GCA)
    This CL adds still capture and recording tests for GoogleCameraArc
    (GCA).
    GCAStillCapture: Takes a photo with default resolution.
    GCARecording: Records a 3-second video with default resolution.
    
    BUG=b:117816663
    TEST=tast run ${DUT} 'camera.GCAStillCapture'
    TEST=tast run ${DUT} 'camera.GCARecording'
    
    Change-Id: Icf59983f95715021b180b526ef77c19b14c11745

---
+++ src/chromiumos/tast/local/bundles/cros/camera/gca_still_capture.go
@@ -0,0 +1,41 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/camera/gca"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GCAStillCapture,
+		Desc:         "Tests still capture with GoogleCameraArc (GCA)",
+		Contacts:     []string{"lnishan@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "chrome_login"},
+		Pre:          arc.Booted(),
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func GCAStillCapture(ctx context.Context, s *testing.State) {
+	must := func(err error) {
+		if err != nil {
+			s.Fatal(err) // NOLINT: arc/ui returns loggable errors
+		}
+	}
+
+	gca.RunTest(ctx, s, func(ctx context.Context, s *testing.State, d *ui.Device) {
+		// GCA always starts in photo mode.
+		// Clicking on the shutter button initiates a still capture.
+		must(gca.ClickShutterButtonAndVerifyFile(ctx, s, d, gca.ImageFormat))
+	})
+}
