72ff3897bdecfcf06ef631ad6f4f7b213f9d80af
Hidehiko Abe <hidehiko@chromium.org>
2019-04-12 00:20:25 +0900

    tast-tests: Port GBM test part3.
    - test_import_vgem
    - test_import_dmabuf
    - test_import_modifier
    - test_gem_map
    - test_dmabuf_map
    - test_gem_map_tiling
    
    BUG=chromium:934072
    TEST=Ran on DUT.
    
    Change-Id: Ia81e9d7f8d9e39fa4a207e30eaef960acc73c504

--- helpers/local/graphics.GBM.gbmtest.cc
+++ helpers/local/graphics.GBM.gbmtest.cc
@@ -3,8 +3,11 @@
 // found in the LICENSE file.
 
 #include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
+#include <linux/dma-buf.h>
 
 #include <base/files/scoped_file.h>
 #include <base/posix/eintr_wrapper.h>
@@ -16,6 +19,8 @@
 #include <xf86drmMode.h>
 
 namespace {
+
+constexpr size_t kPixelSize = sizeof(uint32_t);
 
 namespace internal {
 struct ScopedDrmFDCloseTraits {
@@ -61,6 +66,40 @@ struct GbmBoDeleter {
 };
 }  // namespace internal
 using ScopedGbmBo = std::unique_ptr<gbm_bo, internal::GbmBoDeleter>;
+
+class ScopedGbmBoMap {
+ public:
+  ScopedGbmBoMap() = default;
+  ScopedGbmBoMap(void* map, gbm_bo* bo) : map_(map), bo_(bo) {}
+
+  ~ScopedGbmBoMap() {
+    if (map_)
+      gbm_bo_unmap(bo_, map_);
+  }
+
+ private:
+  void* map_ = nullptr;
+  gbm_bo* bo_ = nullptr;
+};
+
+class ScopedMmapMemory {
+ public:
+  ScopedMmapMemory() = default;
+  ScopedMmapMemory(void* addr, size_t length)
+      : addr_(addr), length_(length) {}
+
+  ~ScopedMmapMemory() {
+    if (addr_ != MAP_FAILED)
+      munmap(addr_, length_);
+  }
+
+  bool is_valid() { return addr_ != MAP_FAILED; }
+  void* get() { return addr_; }
+
+ private:
+  void* addr_ = MAP_FAILED;
+  size_t length_ = 0;
+};
 
 constexpr uint32_t kFormatList[] = {
   GBM_FORMAT_C8,
@@ -198,6 +237,20 @@ std::string UsageToString(uint32_t usage) {
   }
 }
 
+constexpr uint32_t kBufferList[] = {
+  GBM_BO_USE_SCANOUT | GBM_BO_USE_SW_READ_RARELY | GBM_BO_USE_SW_WRITE_RARELY,
+  GBM_BO_USE_RENDERING | GBM_BO_USE_SW_READ_RARELY | GBM_BO_USE_SW_WRITE_RARELY,
+  GBM_BO_USE_SW_READ_RARELY | GBM_BO_USE_SW_WRITE_RARELY,
+  GBM_BO_USE_SW_READ_RARELY | GBM_BO_USE_SW_WRITE_RARELY | GBM_BO_USE_TEXTURING,
+  GBM_BO_USE_SW_READ_RARELY | GBM_BO_USE_SW_WRITE_RARELY | GBM_BO_USE_TEXTURING,
+  GBM_BO_USE_RENDERING | GBM_BO_USE_SW_READ_RARELY |
+      GBM_BO_USE_SW_WRITE_RARELY | GBM_BO_USE_TEXTURING,
+  GBM_BO_USE_RENDERING | GBM_BO_USE_SCANOUT | GBM_BO_USE_SW_READ_RARELY |
+      GBM_BO_USE_SW_WRITE_RARELY,
+  GBM_BO_USE_RENDERING | GBM_BO_USE_SCANOUT | GBM_BO_USE_SW_READ_RARELY |
+      GBM_BO_USE_SW_WRITE_RARELY | GBM_BO_USE_TEXTURING,
+};
+
 void ExpectBo(gbm_bo* bo) {
   ASSERT_TRUE(bo);
   EXPECT_GE(gbm_bo_get_width(bo), 0);
@@ -296,6 +349,35 @@ ScopedDrmFD DrmOpen() {
   }
 
   return {};
+}
+
+base::ScopedFD DrmOpenVgem() {
+  for (int i = 0; i < 16; ++i) {
+    struct stat st;
+    auto sys_card_path =
+        base::StringPrintf("/sys/bus/platform/devices/vgem/drm/card%d", i);
+    if (stat(sys_card_path.c_str(), &st) == 0) {
+      auto dev_card_path = base::StringPrintf("/dev/dri/card%d", i);
+      return base::ScopedFD(HANDLE_EINTR(open(dev_card_path.c_str(), O_RDWR)));
+    }
+  }
+
+  return {};
+}
+
+int CreateVgemBo(int fd, size_t size, uint32_t* handle) {
+  struct drm_mode_create_dumb create = {};
+  create.height = size;
+  create.width = 1;
+  create.bpp = 8;
+
+  int ret = drmIoctl(fd, DRM_IOCTL_MODE_CREATE_DUMB, &create);
+  if (ret)
+    return ret;
+
+  CHECK_GE(create.size, size);
+  *handle = create.handle;
+  return 0;
 }
 
 class GraphicsGbmTest : public testing::Test {
@@ -437,6 +519,312 @@ TEST_F(GraphicsGbmTest, Export) {
 
   base::ScopedFD prime_fd(gbm_bo_get_fd(bo.get()));
   EXPECT_TRUE(prime_fd.is_valid());
+}
+
+// Tests prime import using VGEM sharing buffer.
+TEST_F(GraphicsGbmTest, ImportVgem) {
+  constexpr uint32_t kWidth = 123;
+  constexpr uint32_t kHeight = 456;
+  constexpr uint32_t kBytesPerPixel = 4;
+
+  base::ScopedFD vgem_fd = DrmOpenVgem();
+  ASSERT_TRUE(vgem_fd.is_valid());
+  struct drm_prime_handle prime_handle;
+  ASSERT_EQ(0, CreateVgemBo(vgem_fd.get(), kWidth * kHeight * kBytesPerPixel,
+                            &prime_handle.handle));
+  prime_handle.flags = DRM_CLOEXEC;
+  ASSERT_EQ(0, drmIoctl(vgem_fd.get(), DRM_IOCTL_PRIME_HANDLE_TO_FD,
+                        &prime_handle));
+  base::ScopedFD prime_fd(prime_handle.fd);
+
+  struct gbm_import_fd_data fd_data;
+  fd_data.fd = prime_fd.get();
+  fd_data.width = kWidth;
+  fd_data.height = kHeight;
+  fd_data.stride = kWidth * kBytesPerPixel;
+  fd_data.format = GBM_FORMAT_XRGB8888;
+
+  ScopedGbmBo bo(gbm_bo_import(
+      gbm_.get(), GBM_BO_IMPORT_FD, &fd_data, GBM_BO_USE_RENDERING));
+  EXPECT_BO(bo.get());
+}
+
+// Tests prime import using dma-buf API.
+TEST_F(GraphicsGbmTest, ImportDmabuf) {
+  constexpr uint32_t kWidth = 123;
+  constexpr uint32_t kHeight = 456;
+
+  ScopedGbmBo bo1(gbm_bo_create(
+      gbm_.get(), kWidth, kHeight, GBM_FORMAT_XRGB8888, GBM_BO_USE_RENDERING));
+  ASSERT_BO(bo1.get());
+
+  base::ScopedFD prime_fd(gbm_bo_get_fd(bo1.get()));
+  ASSERT_TRUE(prime_fd.is_valid());
+
+  struct gbm_import_fd_data fd_data;
+  fd_data.fd = prime_fd.get();
+  fd_data.width = kWidth;
+  fd_data.height = kHeight;
+  fd_data.stride = gbm_bo_get_stride(bo1.get());
+  fd_data.format = GBM_FORMAT_XRGB8888;
+
+  bo1.reset();
+
+  ScopedGbmBo bo2(gbm_bo_import(
+      gbm_.get(), GBM_BO_IMPORT_FD, &fd_data, GBM_BO_USE_RENDERING));
+  ASSERT_BO(bo2.get());
+  EXPECT_EQ(kWidth, gbm_bo_get_width(bo2.get()));
+  EXPECT_EQ(kHeight, gbm_bo_get_height(bo2.get()));
+  EXPECT_EQ(fd_data.stride, gbm_bo_get_stride(bo2.get()));
+}
+
+// Tests GBM_BO_IMPORT_FD_MODIFIER entry point.
+TEST_F(GraphicsGbmTest, ImportModifier) {
+  constexpr uint32_t kWidth = 567;
+  constexpr uint32_t kHeight = 891;
+
+  for (const auto format : kFormatList) {
+    if (!gbm_device_is_format_supported(
+            gbm_.get(), format, GBM_BO_USE_RENDERING)) {
+      continue;
+    }
+    SCOPED_TRACE("Format: " + std::to_string(format));
+    ScopedGbmBo bo1(gbm_bo_create(
+        gbm_.get(), kWidth, kHeight, format, GBM_BO_USE_RENDERING));
+    const size_t num_planes = gbm_bo_get_plane_count(bo1.get());
+
+    std::vector<base::ScopedFD> fds;
+    for (size_t p = 0; p < num_planes; ++p)
+      fds.emplace_back(gbm_bo_get_plane_fd(bo1.get(), p));
+
+    struct gbm_import_fd_modifier_data fd_data;
+    fd_data.num_fds = num_planes;
+    for (size_t p = 0; p < num_planes; ++p) {
+      fd_data.fds[p] = fds[p].get();
+      fd_data.strides[p] = gbm_bo_get_stride_for_plane(bo1.get(), p);
+      fd_data.offsets[p] = gbm_bo_get_offset(bo1.get(), p);
+    }
+    fd_data.modifier = gbm_bo_get_modifier(bo1.get());
+    fd_data.width = kWidth;
+    fd_data.height = kHeight;
+    fd_data.format = format;
+
+    bo1.reset();
+
+    ScopedGbmBo bo2(gbm_bo_import(
+        gbm_.get(), GBM_BO_IMPORT_FD_MODIFIER, &fd_data, GBM_BO_USE_RENDERING));
+
+    ASSERT_BO(bo2.get());
+    EXPECT_EQ(kWidth, gbm_bo_get_width(bo2.get()));
+    EXPECT_EQ(kHeight, gbm_bo_get_height(bo2.get()));
+    EXPECT_EQ(fd_data.modifier, gbm_bo_get_modifier(bo2.get()));
+
+    for (size_t p = 0; p < num_planes; ++p) {
+      EXPECT_EQ(fd_data.strides[p], gbm_bo_get_stride_for_plane(bo2.get(), p))
+          << "Unexpected stride at " << p;
+      EXPECT_EQ(fd_data.offsets[p], gbm_bo_get_offset(bo2.get(), p))
+          << "Unexpected offset at " << p;
+    }
+  }
+}
+
+TEST_F(GraphicsGbmTest, GemMap) {
+  constexpr uint32_t kWidth = 666;
+  constexpr uint32_t kHeight = 777;
+
+  ScopedGbmBo bo(gbm_bo_create(
+      gbm_.get(), kWidth, kHeight, GBM_FORMAT_ARGB8888,
+      GBM_BO_USE_SW_READ_RARELY | GBM_BO_USE_SW_WRITE_RARELY));
+  ASSERT_BO(bo.get());
+
+  {
+    void* raw_map_data = nullptr;
+    uint32_t stride = 0;
+    void* addr = gbm_bo_map(bo.get(), 0, 0, kWidth, kHeight,
+                            GBM_BO_TRANSFER_READ_WRITE, &stride, &raw_map_data,
+                            0);
+    ASSERT_NE(MAP_FAILED, addr);
+    ASSERT_TRUE(raw_map_data);
+    ScopedGbmBoMap map_data(raw_map_data, bo.get());
+    EXPECT_GT(stride, 0);
+
+    uint32_t* pixel = static_cast<uint32_t*>(addr);
+    pixel[(kHeight / 2) * (stride / kPixelSize) + kWidth / 2] = 0xABBAABBA;
+  }
+
+  // Remap and verify written previously data.
+  {
+    void* raw_map_data = nullptr;
+    uint32_t stride = 0;
+    void* addr = gbm_bo_map(bo.get(), 0, 0, kWidth, kHeight,
+                            GBM_BO_TRANSFER_READ_WRITE, &stride, &raw_map_data,
+                            0);
+    ASSERT_NE(MAP_FAILED, addr);
+    ASSERT_TRUE(raw_map_data);
+    ScopedGbmBoMap map_data(raw_map_data, bo.get());
+    EXPECT_GT(stride, 0);
+    uint32_t* pixel = static_cast<uint32_t*>(addr);
+    EXPECT_EQ(0xABBAABBA,
+              pixel[(kHeight / 2) * (stride / kPixelSize) + kWidth / 2]);
+  }
+}
+
+TEST_F(GraphicsGbmTest, DmabufMap) {
+  constexpr uint32_t kWidth = 666;
+  constexpr uint32_t kHeight = 777;
+
+  ScopedGbmBo bo(gbm_bo_create(
+      gbm_.get(), kWidth, kHeight, GBM_FORMAT_ARGB8888, GBM_BO_USE_LINEAR));
+  ASSERT_BO(bo.get());
+
+  {
+    base::ScopedFD prime_fd(gbm_bo_get_fd(bo.get()));
+    ASSERT_TRUE(prime_fd.is_valid());
+
+    uint32_t stride = gbm_bo_get_stride(bo.get());
+    ASSERT_GT(stride, 0);
+    uint32_t length = gbm_bo_get_plane_size(bo.get(), 0);
+    ASSERT_GT(length, 0);
+
+    ScopedMmapMemory addr(
+        mmap(nullptr, length, (PROT_READ | PROT_WRITE), MAP_SHARED,
+             prime_fd.get(), 0),
+        length);
+    ASSERT_TRUE(addr.is_valid());
+
+    uint32_t* pixel = static_cast<uint32_t*>(addr.get());
+    size_t stride_pixels = stride / kPixelSize;
+
+    struct dma_buf_sync sync_start = {
+      DMA_BUF_SYNC_START | DMA_BUF_SYNC_WRITE
+    };
+    ASSERT_EQ(
+        0,
+        HANDLE_EINTR(ioctl(prime_fd.get(), DMA_BUF_IOCTL_SYNC, &sync_start)));
+
+    for (uint32_t y = 0; y < kHeight; ++y) {
+      for (uint32_t x = 0; x < kWidth; ++x)
+        pixel[y * stride_pixels + x] = ((y << 16) | x);
+    }
+
+    struct dma_buf_sync sync_end = { DMA_BUF_SYNC_END | DMA_BUF_SYNC_WRITE};
+    ASSERT_EQ(
+        0,
+        HANDLE_EINTR(ioctl(prime_fd.get(), DMA_BUF_IOCTL_SYNC, &sync_end)));
+  }
+
+  {
+    base::ScopedFD prime_fd(gbm_bo_get_fd(bo.get()));
+    ASSERT_TRUE(prime_fd.is_valid());
+
+    uint32_t stride = gbm_bo_get_stride(bo.get());
+    ASSERT_GT(stride, 0);
+    uint32_t length = gbm_bo_get_plane_size(bo.get(), 0);
+    ASSERT_GT(length, 0);
+
+    ScopedMmapMemory addr(
+        mmap(nullptr, length, (PROT_READ | PROT_WRITE), MAP_SHARED,
+             prime_fd.get(), 0),
+        length);
+    ASSERT_TRUE(addr.is_valid());
+
+    uint32_t* pixel = static_cast<uint32_t*>(addr.get());
+    size_t stride_pixels = stride / kPixelSize;
+
+    struct dma_buf_sync sync_start = {
+      DMA_BUF_SYNC_START | DMA_BUF_SYNC_WRITE
+    };
+    ASSERT_EQ(
+        0,
+        HANDLE_EINTR(ioctl(prime_fd.get(), DMA_BUF_IOCTL_SYNC, &sync_start)));
+
+    for (uint32_t y = 0; y < kHeight; ++y) {
+      for (uint32_t x = 0; x < kWidth; ++x) {
+        EXPECT_EQ(((y << 16) | x), pixel[y * stride_pixels + x])
+            << "Pixel mismatch at (" << x << ", " << y << ")";
+      }
+    }
+
+    struct dma_buf_sync sync_end = { DMA_BUF_SYNC_END | DMA_BUF_SYNC_WRITE };
+    ASSERT_EQ(
+        0,
+        HANDLE_EINTR(ioctl(prime_fd.get(), DMA_BUF_IOCTL_SYNC, &sync_end)));
+  }
+
+  void* raw_map_data = nullptr;
+  uint32_t stride = 0;
+  void* addr = gbm_bo_map(bo.get(), 0, 0, kWidth, kHeight,
+                          GBM_BO_TRANSFER_READ_WRITE, &stride, &raw_map_data,
+                          0);
+  ASSERT_NE(MAP_FAILED, addr);
+  ASSERT_TRUE(raw_map_data);
+  ScopedGbmBoMap map_data(raw_map_data, bo.get());
+  EXPECT_GT(stride, 0);
+  uint32_t* pixel = static_cast<uint32_t*>(addr);
+  size_t stride_pixels = stride / kPixelSize;
+
+  for (uint32_t y = 0; y < kHeight; ++y) {
+    for (uint32_t x = 0; x < kWidth; ++x) {
+      EXPECT_EQ(((y << 16) | x), pixel[y * stride_pixels + x])
+          << "Pixel mismatch at (" << x << ", " << y << ")";
+    }
+  }
+}
+
+TEST_F(GraphicsGbmTest, GemMapTiling) {
+  // TODO(crbug.com/752669)
+  if (strcmp(gbm_device_get_backend_name(gbm_.get()), "tegra"))
+    return;
+
+  constexpr uint32_t kWidth = 666;
+  constexpr uint32_t kHeight = 777;
+
+  for (const auto buffer_create_flag : kBufferList) {
+    ScopedGbmBo bo(gbm_bo_create(
+        gbm_.get(), kWidth, kHeight, GBM_FORMAT_ARGB8888, buffer_create_flag));
+    ASSERT_BO(bo.get());
+
+    {
+      void* raw_map_data = nullptr;
+      uint32_t stride = 0;
+      void* addr = gbm_bo_map(bo.get(), 0, 0, kWidth, kHeight,
+                              GBM_BO_TRANSFER_WRITE, &stride, &raw_map_data, 0);
+      ASSERT_NE(MAP_FAILED, addr);
+      ASSERT_TRUE(addr);
+      ASSERT_TRUE(raw_map_data);
+      ScopedGbmBoMap map_data(raw_map_data, bo.get());
+
+      uint32_t* pixel = static_cast<uint32_t*>(addr);
+      const uint32_t stride_pixels = stride / kPixelSize;
+
+      for (uint32_t y = 0; y < kHeight; ++y) {
+        for (uint32_t x = 0; x < kWidth; ++x)
+          pixel[y * stride_pixels + x] = ((y << 16 | x));
+      }
+    }
+
+    // Remap and verify written previously data.
+    {
+      void* raw_map_data = nullptr;
+      uint32_t stride = 0;
+      void* addr = gbm_bo_map(bo.get(), 0, 0, kWidth, kHeight,
+                              GBM_BO_TRANSFER_WRITE, &stride, &raw_map_data, 0);
+      ASSERT_NE(MAP_FAILED, addr);
+      ASSERT_TRUE(addr);
+      ASSERT_TRUE(raw_map_data);
+      ScopedGbmBoMap map_data(raw_map_data, bo.get());
+
+      uint32_t* pixel = static_cast<uint32_t*>(addr);
+      const uint32_t stride_pixels = stride / kPixelSize;
+
+      for (uint32_t y = 0; y < kHeight; ++y) {
+        for (uint32_t x = 0; x < kWidth; ++x) {
+          EXPECT_EQ(((y << 16) | x), pixel[y * stride_pixels + x])
+              << "Pixel mismatch at (" << x << ", " << y << ")";
+        }
+      }
+    }
+  }
 }
 
 }  // namespace
