ccc6686061d9a36c388de6006bb1e93a6b886adc
Wei-Cheng Xiao <garryxiao@chromium.org>
2019-04-15 13:25:22 +0800

    tast-tests: rename the package cryptohome to hwsec
    Per offline discussion, we want to put all HWSec-related tests in a
    single package for now. Therefore, renamed cryptohome to the more
    generic term "hwsec".
    
    BUG=None
    TEST=FEATURES=test emerge-chell tast-local-tests-cros;
         tast run <ip> hwsec.Login;
         tast run <ip> hwsec.LoginGuest
    
    Change-Id: I6a33272436dd50739b4f364c2d1de43d907cf033
    Reviewed-on: https://chromium-review.googlesource.com/1569166
    Commit-Ready: Wei-Cheng Xiao <garryxiao@chromium.org>
    Tested-by: Wei-Cheng Xiao <garryxiao@chromium.org>
    Reviewed-by: Wei-Cheng Xiao <garryxiao@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/doc.go
@@ -0,0 +1,14 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package hwsec contains local Tast tests that exercise TPM-related daemons.
+//
+// See details of each daemon at the following locations:
+//  https://chromium.googlesource.com/chromiumos/platform2/+/master/attestation
+//  https://chromium.googlesource.com/chromiumos/platform2/+/master/chaps
+//  https://chromium.googlesource.com/chromiumos/platform2/+/master/cryptohome
+//  https://chromium.googlesource.com/chromiumos/platform2/+/master/tpm_manager
+//  https://chromium.googlesource.com/chromiumos/platform2/+/master/trunks
+//  https://chromium.googlesource.com/chromiumos/platform2/+/master/u2fd
+package hwsec
