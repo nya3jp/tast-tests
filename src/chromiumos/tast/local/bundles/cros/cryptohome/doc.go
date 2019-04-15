57d56130b05bd9493715e407df0a7e58cb93f728
Wei-Cheng Xiao <garryxiao@chromium.org>
2019-04-15 13:25:22 +0800

    tast-tests: rename the package cryptohome to hwsec
    Per offline discussion, we want to put all HWSec-related tests in a
    single package for now. Therefore, renamed cryptohome to the more
    generic term "hwsec".
    
    BUG=None
    TEST=FEATURES=test emerge-chell tast-local-tests-cros;
         tast run <ip> hwsec.Login;
         tast run <ip> hwsec.LoginGuest
    
    Change-Id: I6a33272436dd50739b4f364c2d1de43d907cf033

--- src/chromiumos/tast/local/bundles/cros/cryptohome/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2018 The Chromium OS Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Package cryptohome contains local Tast tests that exercise the cryptohomed daemon.
-//
-// See https://chromium.googlesource.com/chromiumos/platform2/+/master/cryptohome for more details.
-package cryptohome
