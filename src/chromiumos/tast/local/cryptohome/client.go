d5efec9c5b0cdcf3f519eae2099424e4efa101d9
Maksim Ivanov <emaxx@google.com>
2019-04-17 00:23:17 +0200

    tast-tests: Add challenge-response mount test
    Add tests for cryptohome challenge-response protected mounts
    (a.k.a. smart card login).
    
    The test directly issues commands to the cryptohomed daemon,
    simulating the commands that the Chrome browser would do in the
    real-world smart card login.
    
    BUG=chromium:832226
    TEST=tast run ${IP} hwsec.ChallengeResponseMount
    Cq-Depend: chromium:1571632, chromium:1581687, chromium:1571631, chromium:1571633
    
    Change-Id: Ia3edcd41214aa17ae5521c22fe483e3b9a5b7d72
    Reviewed-on: https://chromium-review.googlesource.com/1570825
    Commit-Ready: Maksim Ivanov <emaxx@chromium.org>
    Tested-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>

---
+++ src/chromiumos/tast/local/cryptohome/client.go
@@ -0,0 +1,78 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"context"
+
+	"github.com/godbus/dbus"
+	"github.com/golang/protobuf/proto"
+
+	cpb "chromiumos/system_api/cryptohome_proto"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+)
+
+const (
+	dbusName      = "org.chromium.Cryptohome"
+	dbusPath      = "/org/chromium/Cryptohome"
+	dbusInterface = "org.chromium.CryptohomeInterface"
+)
+
+// Client is used to interact with the cryptohomed process over D-Bus.
+// For detailed spec of each D-Bus method, please find
+// src/platform2/cryptohome/dbus_bindings/org.chromium.CryptohomeInterface.xml
+type Client struct {
+	conn *dbus.Conn
+	obj  dbus.BusObject
+}
+
+// NewClient connects to cryptohomed via D-Bus and returns a Client object.
+func NewClient(ctx context.Context) (*Client, error) {
+	conn, obj, err := dbusutil.Connect(ctx, dbusName, dbusPath)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{conn, obj}, nil
+}
+
+// Mount calls the MountEx cryptohomed D-Bus method.
+func (c *Client) Mount(
+	ctx context.Context, accountID string, authReq cpb.AuthorizationRequest,
+	mountReq cpb.MountRequest) error {
+	marshAccountID, err := proto.Marshal(
+		&cpb.AccountIdentifier{
+			AccountId: &accountID,
+		})
+	if err != nil {
+		return errors.Wrap(err, "failed marshaling AccountIdentifier")
+	}
+	marshAuthReq, err := proto.Marshal(&authReq)
+	if err != nil {
+		return errors.Wrap(err, "failed marshaling AuthorizationRequest")
+	}
+	marshMountReq, err := proto.Marshal(&mountReq)
+	if err != nil {
+		return errors.Wrap(err, "failed marshaling MountRequest")
+	}
+	call := c.obj.CallWithContext(
+		ctx, "org.chromium.CryptohomeInterface.MountEx", 0, marshAccountID,
+		marshAuthReq, marshMountReq)
+	if call.Err != nil {
+		return errors.Wrap(call.Err, "failed calling cryptohomed MountEx")
+	}
+	var marshMountReply []byte
+	if err := call.Store(&marshMountReply); err != nil {
+		return errors.Wrap(err, "failed reading BaseReply")
+	}
+	var mountReply cpb.BaseReply
+	if err := proto.Unmarshal(marshMountReply, &mountReply); err != nil {
+		return errors.Wrap(err, "failed unmarshaling BaseReply")
+	}
+	if mountReply.Error != nil {
+		return errors.Errorf("MountEx call failed with %s", mountReply.Error)
+	}
+	return nil
+}
