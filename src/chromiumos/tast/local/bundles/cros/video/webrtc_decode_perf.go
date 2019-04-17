e03965d24f218623bc3d40595a74212d30f50ea9
Dean Liao <deanliao@chromium.org>
2019-04-17 14:25:19 +0800

    video: Migrate video_WebRtcPerf autotest to Tast.
    It migrates video_WebRtcPerf autotest to Tast test video.WebRTCDecodePerf.
    The variant video_WebRtcPerf.power will be migrate later on.
    
    Also, it moves some common functions for webrtc_decode_perf.go and
    webrtc_video.go to webrtc.go: chromeArgsWithCameraInput(),
    LoopbackDataFiles() and two constants.
    
    BUG=chromium:918362
    TEST=Pass Tast tests:
    tast run $DUT_IP video.WebRTCDecodePerf \
    video.WebRTCDecodeAccelUsedJPEG \
    video.WebRTCDecodeAccelUsedVP8 \
    WebRTCEncodeAccelUsedVP8
    
    Change-Id: I51f2bc8394ea49463740d90d7cdf939c7cbaf920

---
+++ src/chromiumos/tast/local/bundles/cros/video/webrtc_decode_perf.go
@@ -0,0 +1,42 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"io/ioutil"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/video/webrtc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WebRTCDecodePerf,
+		Desc:         "Measures WebRTC decode performance in terms of CPU usage and decode time with and without hardware acceleration",
+		Contacts:     []string{"deanliao@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome_login"},
+		Data:         append(webrtc.LoopbackDataFiles(), "crowd720_25frames.y4m", webrtc.AddStatsJSFile),
+		// Default timeout (i.e. 2 minutes) is not enough.
+		Timeout: 3 * time.Minute,
+	})
+}
+
+// WebRTCDecodePerf opens a WebRTC loopback page that loops a given capture stream to measure decode time and CPU usage.
+func WebRTCDecodePerf(ctx context.Context, s *testing.State) {
+	addStatsJS, err := ioutil.ReadFile(s.DataPath(webrtc.AddStatsJSFile))
+	if err != nil {
+		s.Fatal("Failed to read JS for gathering decode time: ", err)
+	}
+	webrtc.RunWebRTCDecodePerf(ctx, s, "crowd720_25frames.y4m", webrtc.MeasureConfig{
+		CPUStabilize:      10 * time.Second,
+		CPUMeasure:        30 * time.Second,
+		DecodeTimeTimeout: 30 * time.Second,
+		DecodeTimeSamples: 10,
+		AddStatsJS:        string(addStatsJS),
+	})
+}
