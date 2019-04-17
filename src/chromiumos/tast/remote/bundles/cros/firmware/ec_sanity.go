85cbfa8f89e06e3ed3f03adc4682d98223ffbce1
ningappa tirakannavar <ningappa.tirakannavar@intel.com>
2019-04-17 15:47:00 +0530

    test-tests: Added test to check google EC support
    Test checks the EC support using ectool version command
    
    BUG=chromium:955393
    TEST=Ran on DUT
    
    Change-Id: I6a7daef63b82ebd027cda18b9e9c3a32f5366fdd

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/ec_sanity.go
@@ -0,0 +1,48 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/remote/ec"
+	"chromiumos/tast/remote/servo"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ECSanity,
+		Desc:     "Check Google EC support",
+		Contacts: []string{"ningappa.tirakannavar@intel.com","kasaiah.bogineni@intel.com"},
+		Attr:     []string{"informational"},
+	})
+}
+
+func ECSanity(ctx context.Context, s *testing.State) {
+	d, ok := dut.FromContext(ctx)
+	if !ok {
+		s.Fatal("Failed to get DUT")
+	}
+
+	svo, err := servo.Default(ctx)
+	if err != nil {
+		s.Fatal("Failed to obtain servo: ", err)
+	}
+	chromeEC := ec.New(svo)
+	var regexps = []string{`Chip:\s+([^\r\n]*)\r\n`, `RO:\s+([^\r\n]*)\r\n`, `RW_?[AB]?:\s+([^\r\n]*)\r\n`, `Build:\s+([^\r\n]*)\r\n`}
+	val, err := chromeEC.Output(ctx, "version", regexps)
+	if err != nil {
+		s.Fatal("Failed in version SendEcCommandAndGetOutput: ", err)
+	}
+	s.Logf("EC version is %s", val)
+	if val == "None" {
+		s.Error("EC console not enabled")
+	}
+	if _, err := d.Run(ctx, "ectool version"); err != nil {
+		s.Error("No support for Google EC")
+	}
+}
---
+++ src/chromiumos/tast/remote/ec/ec.go
@@ -0,0 +1,65 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package ec is used to communicate with EC console through servo
+package ec
+
+import (
+	"context"
+	"errors"
+	"strings"
+  
+	"chromiumos/tast/remote/servo"
+)
+
+const (
+	ecUARTCmd = "ec_uart_cmd"
+	ecRegexp  = "ec_uart_regexp"
+)
+
+type EC struct {
+	s *servo.Servo
+}
+
+func New(s *servo.Servo) *EC {
+	return &EC{s}
+}
+
+func (e *EC) uartCmd(ctx context.Context, command string) (bool, error) {
+	if _, err := e.uartRegexp(ctx, []string{}); err != nil {
+		return false, errors.New("Failed to set ec_uart_regexp")
+	}
+	var val bool
+	if err := e.s.Run(ctx, servo.NewCall("set", ecUARTCmd, command), &val); err != nil {
+		return false, err
+	}
+	return val, nil
+}
+
+func (e *EC) uartRegexp(ctx context.Context, patterns []string) (bool, error) {
+	var expr string
+	if len(patterns) == 0 {
+		expr = "None"
+	} else {
+		// Convert Go string array to Python string array literal
+		expr = "['" + strings.Join(patterns, "','") + "']"
+	}
+	var val bool
+	err := e.s.Run(ctx, servo.NewCall("set", ecRegexp, expr), &val)
+	return val, err
+
+}
+
+func (e *EC) Output(ctx context.Context, command string, patterns []string) (string, error) {
+	if _, err := e.uartRegexp(ctx, patterns); err != nil {
+		return "", errors.New("Failed to set ec_uart_regexp")
+	}
+	var val bool
+	e.s.Run(ctx, servo.NewCall("set", ecUARTCmd, command), &val)
+	var ecoutput string
+	if err := e.s.Run(ctx, servo.NewCall("get", ecUARTCmd), &ecoutput); err != nil {
+		return "", err
+	}
+	return ecoutput, nil
+}
