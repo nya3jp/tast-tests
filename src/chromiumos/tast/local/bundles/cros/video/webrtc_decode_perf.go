6de2515317e8e595177625bb00168ff955b61813
Dean Liao <deanliao@chromium.org>
2019-04-17 14:25:19 +0800

    video: Migrate video_WebRtcPerf autotest to Tast.
    It migrates video_WebRtcPerf autotest to Tast test.
    The variant video_WebRtcPerf.power will be migrate later on.
    
    Also, it moves some common functions for webrtc_decode_perf.go and
    webrtc_video.go to webrtc.go: chromeArgsWithCameraInput(),
    LoopbackDataFiles() and two constants.
    
    BUG=chromium:918362
    TEST=Pass Tast tests:
    tast run $DUT_IP video.WebRTCDecodePerf \
    video.WebRTCDecodeAccelUsedJPEG \
    video.WebRTCDecodeAccelUsedVP8 \
    WebRTCEncodeAccelUsedVP8
    
    Change-Id: I51f2bc8394ea49463740d90d7cdf939c7cbaf920

---
+++ src/chromiumos/tast/local/bundles/cros/video/webrtc_decode_perf.go
@@ -0,0 +1,32 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/webrtc"
+	"chromiumos/tast/testing"
+)
+
+// streamFile is a y4m stream to be used as a fake camera stream.
+const streamFile = "crowd720_25frames.y4m"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WebRTCDecodePerf,
+		Desc:         "Measures WebRTC decode performance in terms of CPU usage and decode time with and without hardware acceleration to the performance dashboard",
+		Contacts:     []string{"deanliao@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome_login"},
+		Data:         append(webrtc.LoopbackDataFiles(), streamFile, webrtc.AddStatsJSFile),
+	})
+}
+
+// WebRTCDecodePerf opens a WebRTC loopback page that loops a given capture stream
+// to measure decode time and CPU usage.
+func WebRTCDecodePerf(ctx context.Context, s *testing.State) {
+	webrtc.RunWebRTCDecodePerf(ctx, s, streamFile)
+}
