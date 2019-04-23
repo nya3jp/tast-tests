fd6ccb9329562bdabb486eff030677067284f08c
Hidehiko Abe <hidehiko@chromium.org>
2019-04-24 01:47:55 +0900

    tast-tests: Port cheets_ContainerMount.
    Most poart is ported into arc.ContainerMount.
    assert_all_containers_perform_pivot_root() should be covered by
    security.SandboxedServices.
    assert_no_leftover_mounts() is ported to arc.Shutdown to ensure
    "after logout" situation.
    
    BUG=chromium:950455
    TEST=Ran on DUT.
    Cq-Depend: chromium:1579299
    
    Change-Id: I9b0d6da9363328f9ae00e138378c676af97d9f6d

--- src/chromiumos/tast/local/bundles/cros/arc/shutdown.go
+++ src/chromiumos/tast/local/bundles/cros/arc/shutdown.go
@@ -6,10 +6,12 @@ package arc
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/sysutil"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
@@ -61,5 +63,40 @@ func Shutdown(ctx context.Context, s *testing.State) {
 	newPID, err := arc.InitPID()
 	if err == nil && newPID == oldPID {
 		s.Fatal("ARC was not relaunched. Got PID: ", oldPID)
+	}
+
+	// Make sure that ARC related mount points are released, except
+	// ones for Mini container.
+	ms, err := sysutil.MountInfoForPID(sysutil.SelfPID)
+	if err != nil {
+		s.Fatal("Failed to get mount info: ", err)
+	}
+
+	isARCMount := func(path string) bool {
+		return strings.HasPrefix(path, "/opt/google/containers/android/") ||
+			strings.HasPrefix(path, "/opt/google/containers/arc-") ||
+			strings.HasPrefix(path, "/run/arc/")
+	}
+
+	miniContainerMounts := map[string]int{
+		"/opt/google/containers/android/rootfs/root":                        0,
+		"/opt/google/containers/arc-obb-mounter/mountpoints/container-root": 0,
+		"/opt/google/containers/arc-sdcard/mountpoints/container-root":      0,
+		"/run/arc/adbd":            0,
+		"/run/arc/debugfs/tracing": 0,
+		"/run/arc/media":           0,
+		"/run/arc/obb":             0,
+		"/run/arc/oem":             0,
+		"/run/arc/sdcard":          0,
+		"/run/arc/shared_mounts":   0,
+	}
+
+	for _, m := range ms {
+		if !isARCMount(m.MountPath) {
+			continue
+		}
+		if _, ok := miniContainerMounts[m.MountPath]; !ok {
+			s.Error("Mountpoint leaked after logout: ", m.MountPath)
+		}
 	}
 }
