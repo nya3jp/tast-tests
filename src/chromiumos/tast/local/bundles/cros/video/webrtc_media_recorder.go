22f85a5d371b5f0e3a0e547bf50d9f2b5ec4aab9
Sheng-Hao Tsao <shenghao@google.com>
2019-04-23 18:38:53 +0800

    Add WebRtcMediaRecorder tast test
    This CL adds WebRtcMediaRecorder test to tast. It verifies basic
    MediaRecorder functions such as start, stop, pause and resume.
    
    BUG=chromium:917746
    TEST=Run on eve and verified that test passed
    
    Change-Id: I6f42656bd0c07260bae1df54a5a7f7ab039305d8

---
+++ src/chromiumos/tast/local/bundles/cros/video/webrtc_media_recorder.go
@@ -0,0 +1,80 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     WebRTCMediaRecorder,
+		Desc:     "Checks MediaRecorder on local and remote streams",
+		Contacts: []string{"shenghao@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:     []string{"informational"},
+		// "chrome_internal" is needed because H.264 is a proprietary codec.
+		SoftwareDeps: []string{"chrome_login", "chrome_internal"},
+		Data:         []string{"webrtc_media_recorder.html", "webrtc_media_recorder.js"},
+		Timeout:      3 * time.Minute,
+	})
+}
+
+// WebRTCMediaRecorder checks that MediaRecorder is able to record a local stream or a
+// peer connection remote stream. It also checks the basic Media Recorder
+// functions such as start, stop, pause, resume. The test fails if the media recorder
+// cannot exercise these basic functions.
+func WebRTCMediaRecorder(ctx context.Context, s *testing.State) {
+	chromeArgs := []string{
+		"--use-fake-ui-for-media-stream",     // Avoids the need to grant camera/microphone permissions.
+		"--use-fake-device-for-media-stream", // Feeds fake stream with specified fps to getUserMedia() instead of live camera input.
+	}
+
+	cr, err := chrome.New(ctx, chrome.ExtraArgs(chromeArgs...))
+	if err != nil {
+		s.Error(err, "Failed to connect to Chrome: ")
+	}
+	defer cr.Close(ctx)
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	runTest := func(js string) error {
+		conn, err := cr.NewConn(ctx, server.URL+"/webrtc_media_recorder.html")
+		if err != nil {
+			return errors.Wrap(err, "failed to open recorder page")
+		}
+		defer conn.Close()
+		defer conn.CloseTarget(ctx)
+
+		if err := conn.WaitForExpr(ctx, "document.readyState === 'complete'"); err != nil {
+			return errors.Wrap(err, "timed out waiting for page loading")
+		}
+
+		if err := conn.EvalPromise(ctx, js, nil); err != nil {
+			return errors.Wrap(err, "failed to evaluate test function")
+		}
+		return nil
+	}
+
+	for _, js := range []string{
+		// Test start and stop.
+		"testStartAndRecorderState()",
+		"testStartStopAndRecorderState()",
+		"testStartAndDataAvailable('video/webm; codecs=h264')",
+		"testStartAndDataAvailable('video/webm; codecs=vp9')",
+		"testStartAndDataAvailable('video/webm; codecs=vp8')",
+	} {
+		if err := runTest(js); err != nil {
+			s.Errorf("%v failed: %v", js, err)
+		}
+	}
+}
