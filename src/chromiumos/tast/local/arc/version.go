8e986ac53d1b924f34831acbec1e7d1ed375fce0
Hidehiko Abe <hidehiko@chromium.org>
2019-04-24 01:47:55 +0900

    tast-tests: Port cheets_ContainerMount.
    Most parts are ported into arc.ContainerMount.
    assert_all_containers_perform_pivot_root() should be covered by
    security.SandboxedServices.
    assert_no_leftover_mounts() is ported to arc.Shutdown to ensure
    "after logout" situation.
    
    BUG=chromium:950455
    TEST=Ran on DUT.
    Cq-Depend: chromium:1579299, chrome-internal:1203488
    
    Change-Id: I9b0d6da9363328f9ae00e138378c676af97d9f6d

---
+++ src/chromiumos/tast/local/arc/version.go
@@ -0,0 +1,38 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"strconv"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/lsbrelease"
+)
+
+const (
+	// SDKN is the SDK version of Android N.
+	SDKN = 25
+
+	// SDKP is the SDK version of Android P.
+	SDKP = 28
+)
+
+// SDKVersion returns the ARC's Android SDK version for the current ARC image
+// installed into the DUT.
+func SDKVersion() (int, error) {
+	m, err := lsbrelease.Load()
+	if err != nil {
+		return 0, err
+	}
+	val, ok := m[lsbrelease.ARCSDKVersion]
+	if !ok {
+		return 0, errors.Errorf("failed to find %s in /etc/lsb-release", lsbrelease.ARCSDKVersion)
+	}
+	ret, err := strconv.Atoi(val)
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to parse SDK version %q", val)
+	}
+	return ret, nil
+}
