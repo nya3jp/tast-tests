963e1f11f4db1dd8f3614ed4c4c85c714eee6809
Hidehiko Abe <hidehiko@chromium.org>
2019-04-24 04:04:28 +0900

    test-tests: Port cheets_LoginScreen.
    BUG=chromium:950455
    TEST=Ran on DUT.
    
    Change-Id: Ie3c6aedc556ca0139161a8c72c8e219f317f25b5

---
+++ src/chromiumos/tast/local/bundles/cros/arc/mini_container_state.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/arc/minicontainer"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: MiniContainerState,
+		Desc: "Verifies ARC mini container starts right after Chrome OS shows the login screen",
+		Contacts: []string{
+			"yusukes@chromium.org", // Original author.
+			"arc-eng@google.com",
+			"hidehiko@chromium.org", // Tast port.
+		},
+		Attr: []string{"informational"},
+		// TODO(crbug.com/952125): Consider to relax the SoftwareDeps.
+		SoftwareDeps: []string{"android", "chrome"},
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func MiniContainerState(ctx context.Context, s *testing.State) {
+	minicontainer.RunTest(ctx, s)
+}
