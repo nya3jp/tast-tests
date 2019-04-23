d1386684ea16aa1a9130896fd5aae9a2a220bc76
Bogineni Kasaiah <kasaiah.bogineni@intel.com>
2019-04-23 20:13:18 +0530

    tast-tests: Add test for hot plug usb through servo
    Test will switch the USB device connected to servo USB_KEY port between
    DUT and HOST
    USB device needs to be connected to servo USB_KEY port
    
    BUG=chromium:955457
    TEST=Ran on DUT
    
    Change-Id: Idd3225971c9f3ff630696b5b1edc03f6bfcf0dc8

---
+++ src/chromiumos/tast/remote/bundles/cros/storage/hotplug_usb.go
@@ -0,0 +1,74 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package storage
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/remote/servo"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     HotplugUSB,
+		Desc:     "Hotplug USB device through servo hub and verifies the detection",
+		Contacts: []string{
+			"kasaiah.bogineni@intel.com",
+			"ningappa.tirakannavar@intel.com",
+		},
+	})
+}
+
+// HotplugUSB device through servo hub and verifies the detection
+// Need to connect usb device to Servo USB_KEY port
+func HotplugUSB(ctx context.Context, s *testing.State) {
+	d, ok := dut.FromContext(ctx)
+	if !ok {
+		s.Fatal("Failed to get DUT")
+	}
+	defer d.Close(ctx)
+	svo, err := servo.Default(ctx)
+	if err != nil {
+		s.Fatal("Failed to intialize servo: ", err)
+	}
+	s.Log("Turn off usb key")
+	if err := svo.SwitchUSBKeyPower(ctx, "off"); err != nil {
+		s.Fatalf("Failed to turn off usb key: %v", err)
+	}
+	time.Sleep(2 * time.Second) // Added 2 secs delay between SwitchUSBKeyPower and SwitchUSBKey
+	getUSBDevices := func() ([]string, error) {
+		devices := []string{}
+		output, err := d.Run(ctx, "lsusb")
+		if err != nil {
+			return devices, err
+		}
+		// Sample lsusb output is : Bus 002 Device 003: ID 0bda:8153 Realtek Semiconductor Corp.
+		for _, device := range strings.Split(strings.TrimSpace(string(output)), "\n") {
+			devices = append(devices, strings.Split(device, " ")[5])
+		}
+		return devices, nil
+	}
+	devsBeforePlug, err := getUSBDevices()
+	if err != nil {
+		s.Fatal("Failed to get usb devices", err)
+	}
+	s.Logf("USB devices before plug: %v", devsBeforePlug)
+	s.Log("Hot plug usb device")
+	if err := svo.SwitchUSBKey(ctx, "dut"); err != nil {
+		s.Fatalf("Failed to switch usb key: %v", err)
+	}
+	devsAfterPlug, err := getUSBDevices()
+	if err != nil {
+		s.Fatal("Failed to get usb devices:", err)
+	}
+	s.Logf("USB devices after plug: %v", devsAfterPlug)
+	if len(devsAfterPlug) == len(devsBeforePlug) {
+		s.Fatal("Hot plug unsuccessful, no new usb found")
+	}
+}
--- src/chromiumos/tast/remote/servo/methods.go
+++ src/chromiumos/tast/remote/servo/methods.go
@@ -6,6 +6,8 @@ package servo
 
 import (
 	"context"
+
+	"chromiumos/tast/errors"
 )
 
 // Echo calls the Servo echo method.
@@ -20,4 +22,81 @@ func (s *Servo) PowerNormalPress(ctx context.Context) (bool, error) {
 	var val bool
 	err := s.run(ctx, newCall("power_normal_press"), &val)
 	return val, err
+}
+
+// Set calls Servo get and set functions
+// First will check the default value then proceeds with setting value
+func (s *Servo) Set(ctx context.Context, args ...interface{}) error {
+	var val interface{}
+	// Checking the value is set already
+	value, err := s.Get(ctx, args[0])
+	if err != nil {
+		return errors.Errorf("Failed to get %q value", args[0])
+	}
+	if value == args[1] {
+		return nil
+	}
+
+	// Setting the value if not set
+	if err := s.run(ctx, newCall("set", args...), &val); err != nil {
+		return errors.Errorf("Failed to set %q", args)
+	}
+
+	// Verifying the value is set correctly
+	value, err = s.Get(ctx, args[0])
+	if err != nil {
+		return errors.Errorf("Failed to get %q value", args[0])
+	}
+	if value != args[1].(string) {
+		return errors.Errorf("%q is not set", args)
+	}
+	return nil
+
+}
+
+// SetNoCheck calls Servo set function
+// Method sets the vlaue without any check
+func (s *Servo) SetNoCheck(ctx context.Context, args ...interface{}) error {
+	var val interface{}
+	if err := s.run(ctx, newCall("set", args...), &val); err != nil {
+		return errors.Errorf("Failed to set %q", args)
+	}
+	return nil
+
+}
+
+// Get calls servo get function
+func (s *Servo) Get(ctx context.Context, args ...interface{}) (string, error) {
+	var val string
+	if err := s.run(ctx, newCall("get", args...), &val); err != nil {
+		return val, errors.Errorf("Failed to get %q value", args)
+	}
+	return val, nil
+}
+
+// SwitchUSBKey switches the usb connected servo USB_KEY port between DUT and HOST
+func (s *Servo) SwitchUSBKey(ctx context.Context, usbState string) error {
+	var muxDirection string
+	if usbState == "host" {
+		muxDirection = "servo_sees_usbkey"
+	} else if usbState == "dut" {
+		muxDirection = "dut_sees_usbkey"
+	} else {
+		return errors.Errorf("%q usbstate is not supported", usbState)
+	}
+	if err := s.SetNoCheck(ctx, "image_usbkey_direction", muxDirection); err != nil {
+		return err
+	}
+	return nil
+}
+
+// SwitchUSBKeyPower turns off the servo USB_KEY port
+func (s *Servo) SwitchUSBKeyPower(ctx context.Context, powerState string) error {
+	if powerState != "off" && powerState != "on" {
+		return errors.Errorf("%q powerstate is not supported", powerState)
+	}
+	if err := s.Set(ctx, "image_usbkey_pwr", powerState); err != nil {
+		return err
+	}
+	return nil
 }
