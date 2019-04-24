c1a83ad33a163d545028969dec956ee529dc0dca
Dean Liao <deanliao@chromium.org>
2019-04-24 16:23:36 +0800

    video: Add video.WebRTCDecodePerfCPU test.
    video.WebRTCDecodePerfCPU measures WebRTC performance in terms of CPU usage.
    
    Also, it makes performance measurement logic as a function passed into webRTCPerf()
    as the test flow for measuring WebRTC loopback performance on CPU, power and decode
    time are nearly the same.
    
    This CL also renames
      webRTCPerf ->  webRTCDecodePerf
      RunWebRTCDecodePerf -> RunWebRTCDecodePerfTime
    
    BUG=chromium:918362
    TEST=Pass Tast tests:
    tast run $DUT_IP video.WebRTCDecodePerfCPU video.WebRTCDecodePerfTime
    
    Change-Id: Ia9c079fc35cca8f2dc05ea2f1a0e7eaf58d9799e

---
+++ src/chromiumos/tast/local/bundles/cros/video/webrtc_decode_perf_time.go
@@ -0,0 +1,29 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/webrtc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WebRTCDecodePerfTime,
+		Desc:         "Measures WebRTC decode performance in terms of  decode time with and without hardware acceleration",
+		Contacts:     []string{"deanliao@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome_login"},
+		Data:         append(webrtc.LoopbackDataFiles(), "crowd720_25frames.y4m", webrtc.AddStatsJSFile),
+	})
+}
+
+// WebRTCDecodePerfTime opens a WebRTC loopback page that loops a given capture stream
+// to measure decode time and CPU usage.
+func WebRTCDecodePerfTime(ctx context.Context, s *testing.State) {
+	webrtc.RunWebRTCDecodePerfTime(ctx, s, "crowd720_25frames.y4m")
+}
