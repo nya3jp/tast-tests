f45ef91ac2c1ba49d35755766db343da95900d89
David Staessens <dstaessens@chromium.org>
2019-04-25 12:40:16 +0900

    video.DecodeAccelH264ResolutionSwitch: Add H.264 resolution switching test.
    This cl adds a testcase based on the new video decoder tests, that uses a H.264
    stream that switches resolution multiple times between 1080p and 720p.
    
    TEST=DecodeAccelH264ResolutionSwitch on eve
    
    BUG=chromium:934065
    
    CQ-DEPEND=CL:1583024
    
    Change-Id: I82ee63f934541a463b06d4e7be8ce619cb742973

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_h264_resolution_switch.go
@@ -0,0 +1,30 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DecodeAccelH264ResolutionSwitch,
+		Desc:         "Run Chrome video_decode_accelerator_tests with an H.264 resolution switching video",
+		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{caps.HWDecodeH264},
+		// TODO(crbug.com/933034) Use decode.DataFiles when the old VDA tests have been deprecated.
+		Data: []string{"switch_1080p_720p_240frames.h264", "switch_1080p_720p_240frames.h264.json"},
+	})
+}
+
+// DecodeAccelH264ResolutionSwitch runs the video_decode_accelerator_tests with switch_1080p_720p_240frames.h264.
+func DecodeAccelH264ResolutionSwitch(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoTestNew(ctx, s, "switch_1080p_720p_240frames.h264")
+}
