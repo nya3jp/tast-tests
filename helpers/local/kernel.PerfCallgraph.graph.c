0e7e473d3aa9a449b1448e3834f3604826113bb3
Daniel Erat <derat@chromium.org>
2019-04-29 23:42:23 -0700

    tast-tests: Add kernel.PerfCallgraph test.
    Port the hardware_PerfCallgraphVerification Autotest test to
    Tast as kernel.PerfCallgraph. The test uses a
    kernel.PerfCallgraph.graph helper executable, which I've
    rewritten to recurse and then spin to make it less likely
    that we'll sometimes fail to get any samples with the
    required depth.
    
    BUG=none
    TEST=manual: ran it on caroline
    
    Change-Id: Ia11abce9da4f1017486562f1516e88e2317e1d67

---
+++ helpers/local/kernel.PerfCallgraph.graph.c
@@ -0,0 +1,48 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+// Amount of time to spin before returning.
+const int kSpinNs = 1e8;  // 100 ms
+
+// Number of recursive calls to make.
+const int kRecursionDepth = 8;
+
+// Returns the monotonically-increasing time as nanoseconds since the epoch.
+int64_t GetTimeNs() {
+  struct timespec ts = {0};
+  if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
+    fprintf(stderr, "clock_gettime: %s", strerror(errno));
+    exit(1);
+  }
+  return 1e9 * ts.tv_sec + ts.tv_nsec;
+}
+
+// Calls itself n times. The innermost call spins for a short period of time
+// before returning.
+__attribute__((noinline)) void Recurse(int n) {
+  if (n > 0) {
+    Recurse(n-1);
+    return;
+  }
+
+  const int64_t start = GetTimeNs();
+  while (1) {
+    if (GetTimeNs() - start >= kSpinNs) {
+      break;
+    }
+  }
+}
+
+int main() {
+  Recurse(kRecursionDepth);
+  return 0;
+}
