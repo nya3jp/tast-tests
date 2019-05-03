8ba1f3274dc8090dd3bc167ee96280e8c501f72c
Daniel Erat <derat@chromium.org>
2019-05-03 09:30:15 -0700

    tast-tests: Promote arc.Sysctl to CQ.
    Remove the informational attribute from the arc.Sysctl test.
    Its only failure since April 19 appears to have been
    unrelated Android boot flakiness.
    
    BUG=chromium:948658
    TEST=looked at test results
    
    Change-Id: If80d5e6a2fc6af64654f9137ef7894a86395ef76
    Reviewed-on: https://chromium-review.googlesource.com/1593822
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/sysctl.go
+++ src/chromiumos/tast/local/bundles/cros/arc/sysctl.go
@@ -26,7 +26,6 @@ func init() {
 			"arc-eng@google.com",
 			"hidehiko@chromium.org", // Tast port author.
 		},
-		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"android", "chrome_login"},
 		Timeout:      4 * time.Minute,
 	})
