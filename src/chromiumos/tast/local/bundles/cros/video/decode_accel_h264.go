25bc40bcae900dac370165ea82f567ea908bfdc0
Pin-chih Lin <johnylin@google.com>
2019-05-06 20:20:10 +0800

    tast-tests: generate md5 files from json metadata for decode_accel_*
    We are currently in progress of moving video decode accelerator tests to
    new test binary, which utilizes json metadata files. In json metadata
    files md5 checksums are provided, therefore we could remove md5 and frames
    md5 files in video/data and generate them from json metadata if needed.
    This method would be also useful for arc_decode_accel_* tests in the next
    CL.
    
    TODO(crbug.com/953118): video_decode_accelerator_unittests will be
    deprecated in the near future. Most code introduced here is a temporary
    step in the migration.
    
    BUG=chromium:933034
    TEST=tast run <DUT> video.DecodeAccel*
    
    Change-Id: Ifa56b88427f6d3a7cbe9ecc8ba0df6c1f2838e07
    Reviewed-on: https://chromium-review.googlesource.com/1596768
    Commit-Ready: Pin-chih Lin <johnylin@chromium.org>
    Tested-by: Pin-chih Lin <johnylin@chromium.org>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_h264.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_h264.go
@@ -20,7 +20,7 @@ func init() {
 		Desc:         "Run Chrome video_decode_accelerator_unittest with an H.264 video",
 		Contacts:     []string{"keiichiw@chromium.org", "chromeos-video-eng@google.com"},
 		SoftwareDeps: []string{"chrome", caps.HWDecodeH264},
-		Data:         decode.DataFiles(videotype.H264Prof, decode.AllocateBuffer),
+		Data:         decode.DataFiles(videotype.H264Prof),
 		Timeout:      4 * time.Minute,
 	})
 }
