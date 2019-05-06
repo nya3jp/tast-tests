8b1d4523cf46f7911575f3a6acc3d62f8f9d610a
Pin-chih Lin <johnylin@google.com>
2019-05-06 21:01:00 +0800

    tast-tests: push frames md5 file for arc decoder test
    The arc decoder test binary has implemented frame validator to check
    decoding sanity. The corresponding frames md5 file, which is generated
    from json metadata file, should be pushed into arc environment then.
    
    BUG=b:112677333
    TEST=tast run <DUT_IP> video.ARCVideoAccel*
    
    Change-Id: I900b52a33fe0e0a9beb0375e53be62598410059d

--- src/chromiumos/tast/local/bundles/cros/video/arc_decode_accel_vp9_240p.go
+++ src/chromiumos/tast/local/bundles/cros/video/arc_decode_accel_vp9_240p.go
@@ -10,6 +10,7 @@ import (
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/bundles/cros/video/decode"
 	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
 	"chromiumos/tast/testing"
 )
 
@@ -20,7 +21,7 @@ func init() {
 		Contacts:     []string{"johnylin@chromium.org", "chromeos-video-eng@google.com"},
 		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"android", "chrome_login", caps.HWDecodeVP9},
-		Data:         []string{decode.Test25FPSVP9.Name},
+		Data:         decode.DataFiles(videotype.VP9Prof),
 		Pre:          arc.Booted(),
 	})
 }
