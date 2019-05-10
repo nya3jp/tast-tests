78fdb04a8496e9e275ee76e08ed11c83fb48147d
David Staessens <dstaessens@chromium.org>
2019-05-10 12:36:53 +0900

    video.DecodeAccelPerfH264: Add H.264 video decoder performance test.
    This CL adds a new video decoder performance test for H.264 using the new
    video_decode_accelerator_perf_tests binary.
    
    Note: Wait for the next Chrome uprev that contains CL:1610672 before submitting.
    
    TEST=DecodeAccelPerfH264 on eve
    
    BUG=chromium:953114
    
    Cq-Depend: chromium:1610672
    
    Change-Id: I95a484d4bfccc4598909aa4f14d4373eb539d45c

--- src/chromiumos/tast/local/bundles/cros/video/decode/accel_video.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode/accel_video.go
@@ -13,14 +13,18 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"syscall"
 	"time"
 
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/bundles/cros/video/lib/arctest"
+	"chromiumos/tast/local/bundles/cros/video/lib/cpu"
 	"chromiumos/tast/local/bundles/cros/video/lib/logging"
 	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
 	"chromiumos/tast/local/chrome/bintest"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
@@ -333,6 +337,97 @@ func RunAccelVideoTestNew(ctx context.Context, s *testing.State, filename string
 			s.Error(t, " failed")
 		}
 	}
+}
+
+// RunAccelVideoPerfTest runs video_decode_accelerator_perf_tests with the
+// specified video file.
+func RunAccelVideoPerfTest(ctx context.Context, s *testing.State, filename string) {
+	const (
+		// Name of the capped performance test.
+		cappedTestname = "MeasureCappedPerformance"
+		// Name of the uncapped performance test.
+		uncappedTestname = "MeasureUncappedPerformance"
+		// Time to wait for CPU to stabilize after launching test binary.
+		stabilizationDuration = 1 * time.Second
+		// Duration of the interval during which CPU usage will be measured.
+		measurementDuration = 20 * time.Second
+	)
+
+	shortCtx, cleanupBenchmark, err := cpu.SetUpBenchmark(ctx)
+	if err != nil {
+		s.Fatal("Failed to set up benchmark mode: ", err)
+	}
+	defer cleanupBenchmark()
+
+	// Only a single process can have access to the GPU, so we are required to
+	// call "stop ui" at the start of the test. This will shut down the chrome
+	// process and allow us to claim ownership of the GPU.
+	if err := upstart.StopJob(shortCtx, "ui"); err != nil {
+		s.Error("Failed to stop ui: ", err)
+	}
+	defer upstart.EnsureJobRunning(ctx, "ui")
+
+	// Test 1: Measure capped and uncapped performance.
+	args := []string{
+		"--output_folder=" + s.OutDir(),
+		s.DataPath(filename),
+		s.DataPath(filename + ".json"),
+	}
+
+	const exec = "video_decode_accelerator_perf_tests"
+	if ts, err := bintest.Run(shortCtx, exec, args, s.OutDir()); err != nil {
+		s.Errorf("Failed to run %v with video %s: %v", exec, filename, err)
+		for _, t := range ts {
+			s.Error(t, " failed")
+		}
+	}
+
+	p := perf.NewValues()
+	if err := parseUncappedPerformanceMetrics(s.OutDir()+"/"+uncappedTestname+".json", p); err != nil {
+		s.Fatal("Failed to parse uncapped performance metrics: ", err)
+	}
+	if err := parseCappedPerformanceMetrics(s.OutDir()+"/"+cappedTestname+".json", p); err != nil {
+		s.Fatal("Failed to parse capped performance metrics: ", err)
+	}
+
+	// Test2: Measure CPU usage while running capped performance test only.
+	// TODO(dstaessens@) Investigate collecting CPU usage during previous test.
+	cappedArgs := append(args, "--gtest_filter="+cappedTestname, "--gtest_repeat=-1")
+	cmd, err := bintest.RunAsync(ctx, exec, cappedArgs, nil, s.OutDir())
+	if err != nil {
+		s.Fatalf("Failed to run %v: %v", exec, err)
+	}
+
+	s.Logf("Sleeping %v to wait for CPU usage to stabilize", stabilizationDuration.Round(time.Second))
+	if err := testing.Sleep(ctx, stabilizationDuration); err != nil {
+		s.Fatal("Failed waiting for CPU usage to stabilize: ", err)
+	}
+
+	s.Logf("Sleeping %v to measure CPU usage", measurementDuration.Round(time.Second))
+	cpuUsage, err := cpu.MeasureUsage(ctx, measurementDuration)
+	if err != nil {
+		s.Fatal("Failed to measure CPU usage: ", err)
+	}
+
+	// We got our measurements, now kill the process. After killing a process we
+	// still need to wait for all resources to get released.
+	if err := cmd.Kill(); err != nil {
+		s.Fatalf("Failed to kill %v: %v", exec, err)
+	}
+	if err := cmd.Wait(); err != nil {
+		ws, _ := testexec.GetWaitStatus(err)
+		if !ws.Signaled() || ws.Signal() != syscall.SIGKILL {
+			s.Fatalf("Failed to run %v: %v", exec, err)
+		}
+	}
+
+	p.Set(perf.Metric{
+		Name:      "tast_cpu_usage",
+		Unit:      "ratio",
+		Direction: perf.SmallerIsBetter,
+	}, cpuUsage)
+
+	p.Save(s.OutDir())
 }
 
 // RunAllAccelVideoTest runs all tests in video_decode_accelerator_unittest with thumbnail stored in
