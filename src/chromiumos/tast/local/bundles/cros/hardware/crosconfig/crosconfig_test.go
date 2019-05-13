811531229ea7f785a1b1f974a384abf9a5614ff3
Mathew King <mathewk@chromium.org>
2019-05-13 16:27:34 -0600

    tast: Add sensor presence test
    This test ensures that all sensors that are defined in a boards
    model.yaml file are present as iio devices on the DUT.
    
    BUG=b:132282412
    TEST=tast run ${REMOTE_IP} hardware.SensorPresence
    
    Change-Id: I33bb4011263cd1043a3ed5d1addeaf8ff446f1fc

---
+++ src/chromiumos/tast/local/bundles/cros/hardware/crosconfig/crosconfig_test.go
@@ -0,0 +1,75 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crosconfig
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestCheckHardwareProperty(t *testing.T) {
+	origRunCrosConfig := runCrosConfig
+	runCrosConfig = fakeRunCrosConfig
+	defer func() { runCrosConfig = origRunCrosConfig }()
+
+	for _, tc := range []struct {
+		prop        HardwareProperty
+		expected    bool
+		expectError bool
+	}{
+		{HasBaseGyroscope, false, false},
+		{HasBaseMagnetometer, false, false},
+		{HasBaseAccelerometer, false, true},
+		{HasLidAccelerometer, true, false},
+		{HasLidGyroscope, false, true},
+	} {
+		out, err := CheckHardwareProperty(context.Background(), tc.prop)
+
+		if err != nil && !tc.expectError {
+			t.Errorf("[%v] Expected no error, got %v", tc.prop, err)
+		} else if out != tc.expected {
+			t.Errorf("[%v] Expected out to be %v, got %v", tc.prop, tc.expected, out)
+		}
+	}
+}
+
+func TestCrosConfigFake(t *testing.T) {
+	if os.Getenv("CROSCONFIG_CHILD") == "1" {
+		// child process
+		crosConfig(os.Args[3:]...)
+		panic("unreachable")
+	}
+}
+
+func fakeRunCrosConfig(ctx context.Context, arg ...string) ([]byte, error) {
+	cs := []string{"-test.run=TestCrosConfigFake", "--"}
+	cs = append(cs, arg...)
+	cmd := exec.CommandContext(ctx, os.Args[0], cs...)
+	cmd.Env = []string{"CROSCONFIG_CHILD=1"}
+	return cmd.Output()
+}
+
+// Fake cros_config
+func crosConfig(args ...string) {
+	prop := args[1]
+
+	switch prop {
+	case string(HasBaseGyroscope):
+		os.Exit(1)
+	case string(HasBaseMagnetometer):
+		fmt.Fprintf(os.Stdout, "false")
+	case string(HasBaseAccelerometer):
+		fmt.Fprintf(os.Stdout, "")
+	case string(HasLidGyroscope):
+		os.Exit(2)
+	default:
+		fmt.Fprintf(os.Stdout, "true")
+	}
+
+	os.Exit(0)
+}
