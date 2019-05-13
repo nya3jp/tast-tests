388f48ff1520216252410acdb0b35fb6e575914f
Mathew King <mathewk@chromium.org>
2019-05-13 16:27:34 -0600

    tast: Add sensor presence test
    This test ensures that all sensors that are defined in a boards
    model.yaml file are present as iio devices on the DUT.
    
    BUG=b:132282412
    TEST=tast run ${REMOTE_IP} hardware.SensorPresence
    
    Change-Id: I33bb4011263cd1043a3ed5d1addeaf8ff446f1fc

---
+++ src/chromiumos/tast/local/bundles/cros/hardware/iio/iio_test.go
@@ -0,0 +1,62 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package iio
+
+import (
+	"os"
+	"path"
+	"reflect"
+	"testing"
+
+	"chromiumos/tast/testutil"
+)
+
+type file struct {
+	name     string
+	contents string
+}
+
+type dir struct {
+	name  string
+	dirs  []dir
+	files []file
+}
+
+func TestGetSensors(t *testing.T) {
+	td := testutil.TempDir(t)
+	defer os.RemoveAll(td)
+
+	if err := testutil.WriteFiles(path.Join(td, "sys", "bus", "iio", "devices"),
+		map[string]string{
+			"bad:device/name":      "bad",
+			"iio:device0/name":     "cros-ec-accel",
+			"iio:device0/location": "lid",
+			"iio:device1/name":     "cros-ec-gyro",
+			"iio:device1/location": "base",
+			"iio:device2/name":     "cros-ec-unknown",
+			"iio:device3/name":     "cros-ec-ring",
+		}); err != nil {
+		t.Fatal(err)
+	}
+
+	oldBasePath := basePath
+	basePath = td
+	defer func() { basePath = oldBasePath }()
+
+	sensors, err := GetSensors()
+	if err != nil {
+		t.Fatal("Error getting sensors: ", err)
+	}
+
+	expected := []Sensor{
+		{Accel, Lid, "iio:device0"},
+		{Gyro, Base, "iio:device1"},
+		{Ring, None, "iio:device3"},
+	}
+
+	if !reflect.DeepEqual(expected, sensors) {
+		t.Errorf("Expected sensors %v but got %v", expected, sensors)
+	}
+}
