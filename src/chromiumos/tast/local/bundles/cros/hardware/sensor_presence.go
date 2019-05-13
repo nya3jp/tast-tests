9e1ff166bab8c380fb6b48dc4be7a347a68e7994
Mathew King <mathewk@chromium.org>
2019-05-13 16:27:34 -0600

    tast: Add sensor presence test
    This test ensures that all sensors that are defined in a boards
    model.yaml file are present as iio devices on the DUT.
    
    BUG=b:132282412
    TEST=tast run ${REMOTE_IP} hardware.SensorPresence
    
    Change-Id: I33bb4011263cd1043a3ed5d1addeaf8ff446f1fc
    Reviewed-on: https://chromium-review.googlesource.com/1611006
    Commit-Ready: Mathew King <mathewk@chromium.org>
    Tested-by: Mathew King <mathewk@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    (cherry picked from commit f5785adb4639fdc0d6e41ff920bad83b2c53c0ab)

---
+++ src/chromiumos/tast/local/bundles/cros/hardware/sensor_presence.go
@@ -0,0 +1,83 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hardware
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/hardware/crosconfig"
+	"chromiumos/tast/local/bundles/cros/hardware/iio"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SensorPresence,
+		Desc: "Tests that all sensors defined in model.yaml are present in the system",
+		Contacts: []string{
+			"gwendal@chromium.com", // Chrome OS sensors point of contact
+			"mathewk@chromium.org", // Test author
+			"jettrink@chromium.org",
+			"chromeos-sensors@google.com",
+		},
+		SoftwareDeps: []string{"cros_config"},
+		Attr:         []string{"informational"},
+	})
+}
+
+// SensorPresence verifies that sensors defined in a board's model.yaml file are
+// defined as iio devices on the DUT.
+func SensorPresence(ctx context.Context, s *testing.State) {
+	s.Log("Restarting ui job to so sensors are not claimed by ARC")
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Fatal("Failed to restart ui job: ", err)
+	}
+
+	dutSensors, err := iio.GetSensors()
+	if err != nil {
+		s.Fatal("Error reading sensors on DUT: ", err)
+	}
+
+	sensorDetected := func(name iio.SensorName, loc iio.SensorLocation) bool {
+		for _, sensor := range dutSensors {
+			if sensor.Name == name && sensor.Location == loc {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	for _, tc := range []struct {
+		prop     crosconfig.HardwareProperty
+		name     iio.SensorName
+		location iio.SensorLocation
+	}{
+		{crosconfig.HasBaseAccelerometer, iio.Accel, iio.Base},
+		{crosconfig.HasBaseGyroscope, iio.Gyro, iio.Base},
+		{crosconfig.HasBaseMagnetometer, iio.Mag, iio.Base},
+		{crosconfig.HasLidAccelerometer, iio.Accel, iio.Lid},
+		{crosconfig.HasLidGyroscope, iio.Gyro, iio.Lid},
+		{crosconfig.HasLidMagnetometer, iio.Mag, iio.Lid},
+	} {
+		val, err := crosconfig.CheckHardwareProperty(ctx, tc.prop)
+
+		if err != nil {
+			s.Errorf("Failed to check property %v: %v", tc.prop, err)
+			continue
+		}
+
+		hasSensor := sensorDetected(tc.name, tc.location)
+
+		if val && !hasSensor {
+			s.Errorf("Expected sensor %v %v to be present on the DUT but it is missing",
+				tc.location, tc.name)
+		} else if !val && hasSensor {
+			s.Logf("Extra sensor %v %v; it should be added to the board's model.yaml",
+				tc.location, tc.name)
+		}
+	}
+}
