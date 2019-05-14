b6347bd69d640b3442ff135c854b354e54bc4d63
Ben Chan <benchan@chromium.org>
2019-05-14 15:21:15 -0700

    tast-tests: remove 'wimax-manager' from security.SandboxedServices
    The 'wimax-manager' system service was removed from Chrome OS.
    
    BUG=chromium:954635
    TEST=Ran security.SandboxedServices.
    
    Change-Id: If1330536f96c1a483adfd6a493f425a5870918cc

--- src/chromiumos/tast/local/bundles/cros/security/sandboxed_services.go
+++ src/chromiumos/tast/local/bundles/cros/security/sandboxed_services.go
@@ -82,7 +82,6 @@ func SandboxedServices(ctx context.Context, s *testing.State) {
 		{"debugd", "root", "root", mntNS},
 		{"cras", "cras", "cras", mntNS | restrictCaps | noNewPrivs},
 		{"tcsd", "tss", "tss", restrictCaps},
-		{"wimax-manager", "root", "root", 0},
 		{"mtpd", "mtp", "mtp", pidNS | mntNS | restrictCaps | noNewPrivs | seccomp},
 		{"tlsdated", "tlsdate", "tlsdate", restrictCaps},
 		{"tlsdated-setter", "root", "root", noNewPrivs | seccomp},
