b13568297a995dc66b18d74005f83762d2167ddc
Garrick Evans <garrick@google.com>
2019-05-15 09:39:47 +0900

    arc: Update tast for arc-networkd adb proxy change.
    BUG=b:132213940
    TEST=passes
    Cq-Depend: chromium:1598989
    Cq-Depend: chromium:1598954
    Change-Id: I001c4b1164bc3bec53014fef8c177f32290bb419
    Reviewed-on: https://chromium-review.googlesource.com/1611589
    Commit-Ready: Garrick Evans <garrick@chromium.org>
    Tested-by: Garrick Evans <garrick@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>

--- src/chromiumos/tast/local/bundles/cros/security/network_listeners_arc.go
+++ src/chromiumos/tast/local/bundles/cros/security/network_listeners_arc.go
@@ -30,7 +30,9 @@ func init() {
 func NetworkListenersARC(ctx context.Context, s *testing.State) {
 	ls := netlisten.Common(s.PreValue().(arc.PreData).Chrome)
 	ls["127.0.0.1:5037"] = "/usr/bin/adb"
-	// sslh is installed on ARC-capable systems to multiplex port 22 traffic between sshd and adb.
+	// arc-networkd runs an ADB proxy server on port 5550.
+	ls["127.0.0.1:5550"] = "/usr/bin/arc-networkd"
+	// sslh is installed on ARC-capable systems to multiplex port 22 traffic between sshd and arc-networkd (for adb).
 	ls["*:22"] = "/usr/sbin/sslh-fork"
 	ls["*:2222"] = "/usr/sbin/sshd"
 	netlisten.CheckPorts(ctx, s, ls)
