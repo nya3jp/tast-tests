4a1e91594bb8efb179bb78d73a7e609be41f6ef3
jasongustaman <jasongustaman@google.com>
2019-05-15 09:19:55 +0900

    ARC: Net: Adds multi-networking connectivity test for ARC.
    Multi-networking connectivity checks ARC outbound and inbound connections.
    I accidentally deleted the last CL: 1611588. This CL addresses the
    comments from previous reviews.
    
    BUG=b:132735290
    TEST=tast run root@172.30.6.69 arc.MultiNetworkingConnectivity (on atlas and betty)
    
    Change-Id: I470a62f402336217cf2417dea134f5fdaafd4e6a
    Reviewed-on: https://chromium-review.googlesource.com/1611592
    Tested-by: Jason Jeremy Iman <jasongustaman@google.com>
    Commit-Ready: Jason Jeremy Iman <jasongustaman@google.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Garrick Evans <garrick@chromium.org>

---
+++ src/chromiumos/tast/local/arc/networking.go
@@ -0,0 +1,50 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"strings"
+)
+
+const (
+	// VethPrefix is a prefix for host veth interfaces name.
+	VethPrefix = "veth_"
+	// BridgePrefix is a prefix for host bridge interfaces name.
+	BridgePrefix = "arc_"
+	// VmtapPrefix is a prefix for host vmtap interfaces name.
+	VmtapPrefix = "vmtap"
+
+	// ARCBR0 refers to a host network bridge interface named arcbr0.
+	ARCBR0 = "arcbr0"
+	// ARC0 refers to an ARC network interface named arc0.
+	ARC0 = "arc0"
+	// Loopback refers to loopback interface named lo.
+	Loopback = "lo"
+
+	// Android interface prefixes
+	clatPrefix = "v4-"
+	vpnPrefix  = "tun"
+)
+
+// NetworkInterfaceNames filters Android interfaces and returns ARC related network interfaces.
+func NetworkInterfaceNames(ctx context.Context) ([]string, error) {
+	out, err := BootstrapCommand(ctx, "/system/bin/ls", "/sys/class/net/").Output()
+	if err != nil {
+		return nil, err
+	}
+
+	// Filter out non-arc android net interfaces
+	var ifnames []string
+	for _, ifname := range strings.Fields(string(out)) {
+		if !strings.HasPrefix(ifname, clatPrefix) &&
+			!strings.HasPrefix(ifname, vpnPrefix) &&
+			ifname != Loopback {
+			ifnames = append(ifnames, ifname)
+		}
+	}
+
+	return ifnames, nil
+}
