491e499d64d5ca3f4c7f0fe712577cb160729193
Po-Hsien Wang <pwang@chromium.org>
2019-05-15 19:09:08 -0700

    tast-tests: Graphics: add CrostiniTraceGlxgears test
    Add a basic Crostini test that uses apitrace to replay traces. The goal
    is to make sure that graphics performance in the VM is not regressed.
    
    BUG=b:132905401
    TEST=run `tast run $DUT graphics.CrostiniTraceGlxgears` and check result-chart.json
    Change-Id: I4c68652b909e3bff5934cf90cb45eaf50e209646

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/crostini_trace_glxgears.go
@@ -0,0 +1,29 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/graphics/trace"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CrostiniTraceGlxgears,
+		Desc:         "Replay graphics trace in Crostini VM",
+		Contacts:     []string{"chromeos-gfx@google.com"},
+		Attr:         []string{"disabled", "group:crosbolt", "crosbolt_perbuild"},
+		Data:         []string{"crostini_trace_glxgears.trace"},
+		Timeout:      3 * time.Minute,
+		SoftwareDeps: []string{"chrome", "vm_host"},
+	})
+}
+
+func CrostiniTraceGlxgears(ctx context.Context, s *testing.State) {
+	trace.RunTest(ctx, s, map[string]string{"crostini_trace_glxgears.trace": "glxgears"})
+}
