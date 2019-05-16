d5db7fe041d5aa5f73bcdec4e0ec46130067ca4a
Po-Hsien Wang <pwang@chromium.org>
2019-05-15 19:09:08 -0700

    tast-tests: Graphics: add CrostiniTraceGlxgears test
    Add a basic Crostini test that uses apitrace to replay traces. The goal
    is to make sure that graphics performance in the VM is not regressed.
    
    BUG=b:132905401
    TEST=run `tast run $DUT graphics.CrostiniTraceGlxgears` and check result-chart.json
    Change-Id: I4c68652b909e3bff5934cf90cb45eaf50e209646
    Reviewed-on: https://chromium-review.googlesource.com/1614794
    Commit-Ready: Po-Hsien Wang <pwang@chromium.org>
    Commit-Ready: Ilja H. Friedel <ihf@chromium.org>
    Tested-by: Ilja H. Friedel <ihf@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/trace/trace.go
@@ -0,0 +1,181 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package trace provides common code to run graphics trace files.
+package trace
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/local/vm"
+	"chromiumos/tast/testing"
+)
+
+const (
+	logDir  = "trace"
+	envFile = "glxinfo.txt"
+)
+
+func logInfo(ctx context.Context, cont *vm.Container, file string) error {
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	cmd := cont.Command(ctx, "glxinfo")
+	cmd.Stdout, cmd.Stderr = f, f
+	return cmd.Run()
+}
+
+// RunTest starts a VM and runs all traces in trace, which maps from filenames (passed to s.DataPath) to a human-readable name for the trace, that is used both for the output file's name and for the reported perf keyval.
+func RunTest(ctx context.Context, s *testing.State, traces map[string]string) {
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	// TODO(pwang): use crostini setup library once crbug.com/965398 is done.
+	s.Log("Enabling Crostini preference setting")
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+	if err = vm.EnableCrostini(ctx, tconn); err != nil {
+		s.Fatal("Failed to enable Crostini preference setting: ", err)
+	}
+
+	s.Log("Setting up component ", vm.LiveComponent)
+	err = vm.SetUpComponent(ctx, vm.LiveComponent)
+	if err != nil {
+		s.Fatal("Failed to set up component: ", err)
+	}
+	defer vm.UnmountComponent(ctx)
+
+	s.Log("Creating default container")
+	cont, err := vm.CreateDefaultContainer(ctx, s.OutDir(), cr.User(), vm.LiveImageServer)
+	if err != nil {
+		s.Fatal("Failed to set up default container: ", err)
+	}
+	defer func() {
+		if err := cont.DumpLog(ctx, s.OutDir()); err != nil {
+			s.Error("Failed to dump container log: ", err)
+		}
+		vm.StopConcierge(ctx)
+	}()
+
+	outDir := filepath.Join(s.OutDir(), logDir)
+	if err := os.MkdirAll(outDir, 0755); err != nil {
+		s.Fatalf("Failed to create output dir %v: %v", outDir, err)
+	}
+	file := filepath.Join(outDir, envFile)
+	s.Log("Logging container graphics environment to ", envFile)
+	if err := logInfo(ctx, cont, file); err != nil {
+		s.Log("Failed to log container information: ", err)
+	}
+
+	shortCtx, shortCancel := ctxutil.Shorten(ctx, 30*time.Second)
+	defer shortCancel()
+
+	// TODO(pwang): Install it in container image.
+	s.Log("Checking if apitrace installed")
+	cmd := cont.Command(shortCtx, "sudo", "dpkg", "-l", "apitrace")
+	if err := cmd.Run(); err != nil {
+		cmd.DumpLog(shortCtx)
+		s.Fatal("Failed to get apitrace: ", err)
+	}
+	for traceFile, traceName := range traces {
+		perfValues, err := runTrace(shortCtx, cont, s.DataPath(traceFile), traceName)
+		if err != nil {
+			s.Fatal("Failed running trace: ", err)
+		}
+		if err := perfValues.Save(s.OutDir()); err != nil {
+			s.Fatal("Failed saving perf data: ", err)
+		}
+	}
+}
+
+// runTrace runs a trace and writes output to ${traceName}.txt. traceFile should be absolute path.
+func runTrace(ctx context.Context, cont *vm.Container, traceFile, traceName string) (*perf.Values, error) {
+	containerPath := filepath.Join("/tmp", filepath.Base(traceFile))
+	if err := cont.PushFile(ctx, traceFile, containerPath); err != nil {
+		return nil, errors.Wrap(err, "failed copying trace file to container")
+	}
+
+	testing.ContextLog(ctx, "Replaying trace file ", filepath.Base(traceFile))
+	cmd := cont.Command(ctx, "apitrace", "replay", containerPath)
+	traceOut, err := cmd.CombinedOutput()
+	if err != nil {
+		cmd.DumpLog(ctx)
+		return nil, errors.Wrap(err, "failed to replay apitrace")
+	}
+
+	outDir, ok := testing.ContextOutDir(ctx)
+	if !ok {
+		return nil, errors.New("failed to get OutDir for writing trace result")
+	}
+	// Suggesting the file is human readable by appending txt extension.
+	file := filepath.Join(outDir, logDir, traceName+".txt")
+	testing.ContextLog(ctx, "Dumping trace output to file ", filepath.Base(file))
+	if err := ioutil.WriteFile(file, traceOut, 0644); err != nil {
+		return nil, errors.Wrap(err, "error writing tracing output")
+	}
+	return parseResult(traceName, string(traceOut))
+}
+
+// parseResult parses the output of apitrace and return the perfs.
+func parseResult(traceName, output string) (*perf.Values, error) {
+	re := regexp.MustCompile(`Rendered (\d+) frames in (\d*\.?\d*) secs, average of (\d*\.?\d*) fps`)
+	match := re.FindStringSubmatch(output)
+	if match == nil {
+		return nil, errors.New("result line can't be located")
+	}
+
+	frames, err := strconv.ParseUint(match[1], 10, 64)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to parse frames %q", match[1])
+	}
+	duration, err := strconv.ParseFloat(match[2], 64)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to parse duration %q", match[2])
+	}
+	fps, err := strconv.ParseFloat(match[3], 64)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to parse fps %q", match[3])
+	}
+
+	value := perf.NewValues()
+	value.Set(perf.Metric{
+		Name:      traceName,
+		Variant:   "time",
+		Unit:      "sec",
+		Direction: perf.SmallerIsBetter,
+	}, duration)
+	value.Set(perf.Metric{
+		Name:      traceName,
+		Variant:   "frames",
+		Unit:      "frame",
+		Direction: perf.BiggerIsBetter,
+	}, float64(frames))
+	value.Set(perf.Metric{
+		Name:      traceName,
+		Variant:   "fps",
+		Unit:      "fps",
+		Direction: perf.BiggerIsBetter,
+	}, fps)
+	return value, nil
+}
+
+// TODO(pwang): Write a func to cleans up disk in best effort.
