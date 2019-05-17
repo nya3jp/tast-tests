163fdf30cf88dd0a2fa666169fb9bd82c4710131
Qijiang Fan <fqj@chromium.org>
2019-05-17 11:29:25 +0900

    tast-tests: add security.SELinuxAuditSanity
    Add a new informational tast test security.SELinuxAuditSanity for
     - auditd is running
     - trigger an audit event
      - nothing is written into syslog
      - audit message is written into audit.log
    
    TEST=tast run betty security.SELinuxAuditSanity
    BUG=chromium:894029
    
    Cq-Depend: chromium:1616862
    Change-Id: I174f46989eee754f97505795d175c2aa8ab5f333

---
+++ src/chromiumos/tast/local/bundles/cros/security/selinux_audit_sanity.go
@@ -0,0 +1,110 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"bufio"
+	"context"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SELinuxAuditSanity,
+		Desc:         "Checks SELinux audit works as intended",
+		Contacts:     []string{"fqj@chromium.org", "kroot@chromium.org", "chromeos-security@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"selinux"},
+	})
+}
+
+func SELinuxAuditSanity(ctx context.Context, s *testing.State) {
+	// Directory name should keep in sync with platform2/sepolicy/policy/chromeos/dev/cros_ssh_session
+	const markerDirName = "cros_selinux_audit_sanity_test"
+
+	s.Log("Waiting for auditd job to be running")
+	if err := upstart.WaitForJobStatus(ctx, "auditd", upstart.StartGoal, upstart.RunningState, upstart.RejectWrongGoal, 30*time.Second); err != nil {
+		s.Fatal("Failed waiting for auditd to start: ", err)
+	}
+
+	randStr := func(l int) string {
+		var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+		s := make([]rune, l)
+		for i := range s {
+			s[i] = letters[rand.Intn(len(letters))]
+		}
+		return string(s)
+	}
+
+	findFirstMatchedLine := func(r io.Reader, re *regexp.Regexp) (string, error) {
+		rr := bufio.NewReader(r)
+		for {
+			line, err := rr.ReadString('\n')
+			if err == io.EOF {
+				return "", nil
+			}
+			if err != nil {
+				return "", err
+			}
+			if re.MatchString(line) {
+				return line, nil
+			}
+		}
+	}
+
+	// Generate an audit event by creating a file inside markerDirectory
+	td, err := ioutil.TempDir("/tmp/", "tast.security.SELinuxAuditSanity.")
+	defer os.RemoveAll(td)
+	if err != nil {
+		s.Fatal("Failed to create temporary directory for testing: ", err)
+	}
+	markerDirectory := filepath.Join(td, markerDirName)
+	if err := os.Mkdir(markerDirectory, 0700); err != nil {
+		s.Fatal("Failed to create marker directory for testing: ", err)
+	}
+	fileName := randStr(20)
+	fi, err := os.Create(filepath.Join(markerDirectory, fileName))
+	if err != nil {
+		s.Fatal("Failed to create marker file: ", err)
+	}
+	fi.Close()
+
+	// Checks no logs matching the file name in syslog.
+	badContent, err := exec.Command("journalctl", "-b", "0", "-t", "kernel", "--grep", fileName).Output()
+	if err != nil {
+		s.Fatal("Failed to read syslog from journald: ", err)
+	}
+	// journalctl adds a '-- No Entries --' line if --grep is not found.
+	// We need to check the output actually contains the file name.
+	if strings.Contains(string(badContent), fileName) {
+		s.Errorf("audit shouldn't be logged to syslog, but found: %q", badContent)
+	}
+
+	// Checks log can be found in audit.log for file name.
+	f, err := os.Open("/var/log/audit/audit.log")
+	if err != nil {
+		s.Fatal("Failed to open audit.log")
+	}
+	defer f.Close()
+	wantedLine := regexp.MustCompile("granted.*" + fileName)
+	foundLine, err := findFirstMatchedLine(f, wantedLine)
+	if err != nil {
+		s.Fatal("Failed to read audit.log: ", err)
+	}
+	if foundLine == "" {
+		s.Error("Expected audit message in audit.log but not found")
+	}
+}
