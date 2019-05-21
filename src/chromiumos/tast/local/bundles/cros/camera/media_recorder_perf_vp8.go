4d62e17cbcb586052452d1fac4cf0a3bf08ec3f5
Hirokazu Honda <hiroh@chromium.org>
2019-05-21 14:02:34 +0900

    video: Add VP8 and VP9 cases for camera.MediaRecorderPerf
    We only have H264 media recoder performance test. This CL adds VP8 and VP9 cases
    for camera.MediaRecorderPerf.
    
    BUG=None
    TEST=tast run camera.MediaRecorderPerf*
    Change-Id: I22ac129bd63b8bac5f93c1e3952b7cb683d20be9

--- src/chromiumos/tast/local/bundles/cros/camera/media_recorder_perf_vp8.go
+++ src/chromiumos/tast/local/bundles/cros/camera/media_recorder_perf_vp8.go
@@ -8,7 +8,6 @@ import (
 	"context"
 	"time"
 
-	// TODO(crbug.com/963772) Move libraries in video to camera or media folder.
 	"chromiumos/tast/local/bundles/cros/video/lib/videotype"
 	"chromiumos/tast/local/bundles/cros/video/mediarecorder"
 	"chromiumos/tast/testing"
@@ -16,27 +15,26 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func: MediaRecorderPerfH264,
-		Desc: "Captures performance data about MediaRecorder for SW and HW with H.264",
+		Func: MediaRecorderPerfVP8,
+		Desc: "Captures performance data about MediaRecorder for SW and HW with VP8",
 		Contacts: []string{
 			"hiroh@chromium.org",    // Video team
 			"shenghao@chromium.org", // Camera team
 			"chromeos-camera-eng@google.com",
 		},
-		Attr: []string{"group:crosbolt", "crosbolt_perbuild"},
-		// "chrome_internal" is needed because H.264 is a proprietary codec.
-		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome"},
 		Data:         []string{mediarecorder.PerfStreamFile, "loopback_media_recorder.html"},
 		Timeout:      3 * time.Minute,
 	})
 }
 
-// MediaRecorderPerfH264 captures the perf data of MediaRecorder for HW and SW cases with H.264 codec and uploads to server.
-func MediaRecorderPerfH264(ctx context.Context, s *testing.State) {
+// MediaRecorderPerfVP8 captures the perf data of MediaRecorder for HW and SW cases with VP8 codec and uploads to server.
+func MediaRecorderPerfVP8(ctx context.Context, s *testing.State) {
 	const (
 		fps = 30
 	)
-	if err := mediarecorder.MeasurePerf(ctx, s.DataFileSystem(), s.OutDir(), videotype.H264, s.DataPath(mediarecorder.PerfStreamFile), fps); err != nil {
+	if err := mediarecorder.MeasurePerf(ctx, s.DataFileSystem(), s.OutDir(), videotype.VP8, s.DataPath(mediarecorder.PerfStreamFile), fps); err != nil {
 		s.Error("Failed to measure performance: ", err)
 	}
 }
