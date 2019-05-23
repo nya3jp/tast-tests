10b5bcc43960502162354151031a6b86ad7d90ab
David Staessens <dstaessens@chromium.org>
2019-05-24 08:35:58 +0900

    video.DecodeAccelH264*: Add missing chrome dependency to video decoder tests.
    This cl adds the 'chrome' software dependency to the DecodeAccelH264New and
    DecodeAccelH264ResolutionSwitch Tast tests. While these tests don't directly
    make use of the Chrome binary, the required test binaries are deployed together
    with the Chrome target.
    
    TEST=DecodeAccelH264 on eve
    
    BUG=None
    
    Change-Id: I1de1b35576cb3fd6db82b7d275d30389f63b1d57
    Reviewed-on: https://chromium-review.googlesource.com/1627183
    Commit-Ready: David Staessens <dstaessens@chromium.org>
    Tested-by: David Staessens <dstaessens@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_h264_resolution_switch.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_h264_resolution_switch.go
@@ -18,7 +18,7 @@ func init() {
 		Desc:         "Runs Chrome video_decode_accelerator_tests with an H.264 resolution switching video",
 		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
 		Attr:         []string{"informational"},
-		SoftwareDeps: []string{caps.HWDecodeH264},
+		SoftwareDeps: []string{"chrome", caps.HWDecodeH264},
 		// TODO(crbug.com/933034) Use decode.DataFiles when the old VDA tests have been deprecated.
 		Data: []string{"switch_1080p_720p_240frames.h264", "switch_1080p_720p_240frames.h264.json"},
 	})
