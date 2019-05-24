98e0e97a3a6fa618572cf1e2fb6c2a3329132239
Brian Norris <briannorris@chromium.org>
2019-05-24 10:55:34 -0700

    tast-tests: add WifiSetTxPower
    Some basic integration testing, to make sure we do basic driver matching
    and device discovery properly.
    
    BUG=chromium:782924
    TEST=run it, on devices with and without support
    
    Cq-Depend: chromium:1632665
    Change-Id: I5c2298b5ced8bd23343827cc7cf16f55e55f8714

---
+++ src/chromiumos/tast/local/bundles/cros/network/wifi_set_tx_power.go
@@ -0,0 +1,52 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: WifiSetTXPower,
+		Desc: "Tests WiFi TX power helper's basic operation",
+		Contacts: []string{
+			"briannorris@chromium.org",        // Author
+			"chromeos-kernel-wifi@google.com", // WiFi team
+		},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"tablet_mode"},
+	})
+}
+
+func WifiSetTXPower(ctx context.Context, s *testing.State) {
+	const setTxPowerExe = "set_wifi_transmit_power"
+
+	cmd := testexec.CommandContext(ctx, "check_powerd_config", "--set_wifi_transmit_power")
+	if err := cmd.Run(); err != nil {
+		if ws, ok := testexec.GetWaitStatus(err); ok && ws.ExitStatus() == 1 {
+			s.Log("DUT does not support WiFi power table switching")
+			return
+		}
+		defer cmd.DumpLog(ctx)
+		s.Fatal("Failed to run check_powerd_config: ", err)
+	}
+
+	for _, tc := range []struct {
+		mode string
+		args []string
+	}{
+		// Run tablet mode first, then switch back to non-tablet mode.
+		{"tablet", []string{"--tablet"}},
+		{"non-tablet", []string{}},
+	} {
+		if err := testexec.CommandContext(ctx, setTxPowerExe, tc.args...).Run(testexec.DumpLogOnError); err != nil {
+			s.Errorf("Failed to set TX power for %s mode: %v", tc.mode, err)
+		}
+	}
+}
