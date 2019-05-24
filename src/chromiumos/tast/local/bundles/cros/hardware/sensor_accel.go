7c06bbddc0db4ab0dca7bd11a8e70c2a897dcb43
Mathew King <mathewk@chromium.org>
2019-05-24 10:47:07 -0600

    tast: Test for accelerometer
    Test that all accelerometers on the DUT can be read and the magnitue is
    close to 9.8 m/s^2. This test will only work if the DUT is on Earth and
    not moving ;)
    
    BUG=b:132282412
    TEST=tast run ${REMOTE_IP} hardware.SensorAccel
    
    Change-Id: If9f7f135034fdf0440d6a3e4b33b3a7626256d90

---
+++ src/chromiumos/tast/local/bundles/cros/hardware/sensor_accel.go
@@ -0,0 +1,66 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hardware
+
+import (
+	"context"
+	"math"
+
+	"chromiumos/tast/local/bundles/cros/hardware/iio"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SensorAccel,
+		Desc: "Tests that accelerometer sensors can be read and give sane data",
+		Contacts: []string{
+			"gwendal@chromium.com", // Chrome OS sensors point of contact
+			"mathewk@chromium.org", // Test author
+			"chromeos-sensors@google.com",
+		},
+		SoftwareDeps: []string{"cros_config"},
+		Attr:         []string{"informational"},
+	})
+}
+
+// SensorAccel gets the current sensor reading of all accelerometer sensors and
+// verifies that the data is within 25% of 1g.
+func SensorAccel(ctx context.Context, s *testing.State) {
+	const (
+		accel1g  = 9.8185
+		accelErr = accel1g * .25
+	)
+
+	sensors, err := iio.GetSensors()
+	if err != nil {
+		s.Fatal("Error reading sensors on DUT: ", err)
+	}
+
+	for _, sn := range sensors {
+		if sn.Name != iio.Accel {
+			continue
+		}
+
+		r, err := sn.Read()
+		if err != nil {
+			s.Errorf("Error reading data from %v %v: %v", sn.Location, sn.Name, err)
+			continue
+		}
+
+		if len(r.Data) != 3 {
+			s.Errorf("Unexpected amount of data: got %v; want 3", len(r.Data))
+			continue
+		}
+
+		mag := math.Sqrt(r.Data[0]*r.Data[0] + r.Data[1]*r.Data[1] + r.Data[2]*r.Data[2])
+		s.Logf("%v %v magnitude is %.3f", sn.Location, sn.Name, mag)
+
+		if math.Abs(mag-accel1g) > accelErr {
+			s.Errorf("Accelerometer %v data out of range: got %.3f; want %.3f +- %.3f",
+				sn.Path, mag, accel1g, accelErr)
+		}
+	}
+}
