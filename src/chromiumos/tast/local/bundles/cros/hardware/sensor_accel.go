d8e25d10f8689edf9bd0796b1a13233d3b89444a
Mathew King <mathewk@chromium.org>
2019-05-24 10:47:07 -0600

    tast: Test for accel
    Change-Id: If9f7f135034fdf0440d6a3e4b33b3a7626256d90

---
+++ src/chromiumos/tast/local/bundles/cros/hardware/sensor_accel.go
@@ -0,0 +1,63 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hardware
+
+import (
+	"context"
+	"math"
+
+	"chromiumos/tast/local/bundles/cros/hardware/iio"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SensorAccel,
+		Desc: "Tests that accelerometer sensors can be read and give sane data",
+		Contacts: []string{
+			"gwendal@chromium.com", // Chrome OS sensors point of contact
+			"mathewk@chromium.org", // Test author
+			"chromeos-sensors@google.com",
+		},
+		SoftwareDeps: []string{"cros_config"},
+		Attr:         []string{"informational"},
+	})
+}
+
+const accel1g = 9.8185
+const accelErr = accel1g * .25
+
+// SensorAccel gets the current sensor reading of all accelerometer sensors and
+// verifies that the data is within 25% of 1g.
+func SensorAccel(ctx context.Context, s *testing.State) {
+	s.Log("Restarting ui job to so sensors are not claimed by ARC")
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Fatal("Failed to restart ui job: ", err)
+	}
+
+	dutSensors, err := iio.GetSensors()
+	if err != nil {
+		s.Fatal("Error reading sensors on DUT: ", err)
+	}
+
+	for _, sensor := range dutSensors {
+		if sensor.Name != iio.Accel {
+			continue
+		}
+
+		reading, err := sensor.Reading()
+		if err != nil {
+			s.Errorf("Error reading data from %v %v", sensor.Location, sensor.Name)
+		}
+
+		mag := math.Sqrt(reading.X*reading.X + reading.Y*reading.Y + reading.Z*reading.Z)
+
+		if math.Abs(mag-accel1g) > accelErr {
+			s.Errorf("Accelerometer %v data out of range, expected %v +- %v got %v",
+				sensor.Path, accel1g, accelErr, mag)
+		}
+	}
+}
