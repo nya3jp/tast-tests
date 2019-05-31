a4b71475d47b81e228bd15404aade06dc445cd98
Brian Norris <briannorris@chromium.org>
2019-05-30 17:10:58 -0700

    tast-tests: network.ConfigureServiceForProfile: grab check_ethernet lock
    This script stops/starts shill a few times, to reset state and to test
    that its own state remains across restart. This causes us to lose
    connectivity briefly, and it gives the recover_duts/check_ethernet.hook
    infrastructure a chance to start screwing us up. This is particularly
    problematic if it:
    
     * decides to start disabling/re-enabling USB ethernet at the wrong
       time, as that may prevent our Ethernet Service (that this test looks
       for) from showing up promptly; or
     * restarts shill, which will cause us to lose our D-Bus connection and
       time out
    
    Solution: hold the check_ethernet lock for the duration of this test.
    
    See also http://crrev.com/c/1637489, which improves the locking on the
    recover_duts side.
    
    Note: even with this lock usage, there is still potentially some ramp-up
    time after this test, where we are no longer holding the lock but may
    have brief connectivity loss (while Ethernet is getting re-configured).
    That's a problem for the next test though ;)
    
    Note: shill.Safe{Stop,Start}() utilize the old
    /run/lock/shill-start.lock properly, but unfortunately this is all
    pretty racy too, since:
     (*) check_ethernet.hook doesn't currently acquire the lock -- it only
         reads it -- so there's still a race window
     (*) even if shill has started (in the Upstart sense), it may not have
         finished configuring Ethernet; so check_ethernet.hook may again
         find the lock "open", but connectivity not in good shape yet
    This is a problem for another day, requiring total
    revamp/replacement/removal of shill-start.lock. Fortunately,
    'autotest_pause_ethernet_hook' is a sufficient superset of that lock,
    and it satisfies the needs of this test.
    
    I also add the clean-profile-on-exit logic that is present in the
    Autotest version, but not the Tast version of this test.
    
    BUG=chromium:966965
    TEST=`touch /mnt/stateful_partition/.labmachine; start recover_duts`,
         run network.ConfigureServiceForProfile repeatedly
    
    Change-Id: I84a645591dff248547fd3d445ed89055beb83ea5

---
+++ src/chromiumos/tast/local/network/check_network_hook.go
@@ -0,0 +1,85 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package network provides general CrOS network goodies.
+package network
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"golang.org/x/sys/unix"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+const (
+	checkNetworkLockPath = "/run/autotest_pause_ethernet_hook"
+	// An estimate of the longest time 'check_ethernet.hook' needs for its
+	// connectivity checks.
+	checkNetworkLockTimeout = 20 * time.Second
+)
+
+// LockCheckNetworkHook prevents the 'check_ethernet' recovery hook (runs on
+// machines in the lab) from interrupting us (e.g., trying to restart shill or
+// forcibly reset the Ethernet device). Use this if your test is going to
+// perform operations that may interrupt the DUT's network connectivity (e.g.,
+// restarting shill; configuring non-standard network profiles; suspending the
+// system).
+func LockCheckNetworkHook(ctx context.Context) (unlock func(), e error) {
+	lockChan := make(chan error, 1) // To notify lock completion to main thread.
+	done := make(chan struct{})     // To notify main thread completion to the goroutine.
+
+	doUnlock := func() {
+		close(done)
+	}
+
+	succeeded := false
+	defer func() {
+		if !succeeded {
+			doUnlock()
+		}
+	}()
+
+	go func() {
+		f, err := os.Create(checkNetworkLockPath)
+		if err != nil {
+			lockChan <- err
+			return
+		}
+		defer f.Close()
+
+		// NOTE: if this lock is held for a "long time", the main
+		// thread may time out but we'll still be stuck here beyond the
+		// time of test completion. This is OK, but beware that (for
+		// example) cleanup logging may not go anywhere useful.
+		if err = unix.Flock(int(f.Fd()), unix.LOCK_SH); err != nil {
+			lockChan <- err
+			return
+		}
+		defer func() {
+			if err = unix.Flock(int(f.Fd()), unix.LOCK_UN); err != nil {
+				testing.ContextLogf(ctx, "Failed to unlock %s: %v", checkNetworkLockPath, err)
+			}
+		}()
+		lockChan <- nil
+		<-done // Wait for main thread.
+	}()
+
+	lctx, cancel := context.WithTimeout(ctx, checkNetworkLockTimeout)
+	defer cancel()
+	select {
+	case err := <-lockChan:
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to acquire lock %s", checkNetworkLockPath)
+		}
+	case <-lctx.Done():
+		return nil, errors.Wrapf(lctx.Err(), "timed out acquiring lock %s", checkNetworkLockPath)
+	}
+
+	succeeded = true
+	return doUnlock, nil
+}
