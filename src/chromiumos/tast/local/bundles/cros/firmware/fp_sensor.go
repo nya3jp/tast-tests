09929fc3d6d2b0cd3ca95998f1c4483023ea3f2a
Yicheng Li <yichengli@chromium.org>
2019-05-31 17:59:27 -0700

    tast-test: Add tast test for fpsensor under category "firmware"
    This test tests verifies that FPTPM seed is set by calling
    the API, ec_command and ectool command for checking FPMCU
    encryption status.
    
    BUG=chromium:952275
    TEST=tast -verbose run <DUT> firmware.FpSensor
         tested on Nocture.
    
    Cq-Depend: chromium:1633272
    Change-Id: I16947b82366eba3bfdeeb0b5085d29116ef67493
    Signed-off-by: Yicheng Li <yichengli@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1638848
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fp_sensor.go
@@ -0,0 +1,37 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FpSensor,
+		Desc: "Checks that ectool commands for fingerprint sensor behave as expected",
+		Contacts: []string{
+			"yichengli@chromium.org", // Test author
+			"norvez@chromium.org",
+		},
+		Attr: []string{"informational"},
+	})
+}
+
+func FpSensor(ctx context.Context, s *testing.State) {
+	const exp = "FPTPM seed set:1"
+	cmd := testexec.CommandContext(ctx, "ectool", "--name=cros_fp", "fpencstatus")
+	s.Logf("Running command: %q", shutil.EscapeSlice(cmd.Args))
+	if out, err := cmd.Output(testexec.DumpLogOnError); err != nil {
+		s.Errorf("%q failed: %v", shutil.EscapeSlice(cmd.Args), err)
+	} else if !strings.Contains(string(out), exp) {
+		s.Errorf("FPTPM seed is not set; output %q doesn't contain %q", string(out), exp)
+	}
+}
