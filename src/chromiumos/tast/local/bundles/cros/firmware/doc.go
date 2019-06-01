83f4ace2e31b1be457a7e768354a94ed07b2408d
Yicheng Li <yichengli@chromium.org>
2019-05-31 17:59:27 -0700

    tast-test: Add tast test for fpsensor under category "firmware"
    This test tests verifies that FPTPM seed is set by calling
    the API, ec_command and ectool command for checking FPMCU
    encryption status.
    
    Cq-Depend: chromium:1633272
    BUG=chromium:952275
    TEST=tast -verbose run <DUT> firmware.FpSensor
         tested on Nocture.
    
    Change-Id: I16947b82366eba3bfdeeb0b5085d29116ef67493
    Signed-off-by: Yicheng Li <yichengli@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package firmware contains local Tast tests that exercise the firmware portion
+// of Chrome OS, for the main EC as well as other EC's such as the fingerprint
+// MCU. This corresponds to the chromiumos/platform/ec repository.
+package firmware
