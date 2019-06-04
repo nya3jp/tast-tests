b52e99d7f7b16dc42ce5909e9ae595374b4bd24e
Hidehiko Abe <hidehiko@chromium.org>
2019-06-04 18:58:03 +0900

    tast: Port cheets_NotificationTest.
    BUG=chromium:950455
    TEST=Ran on DUT (eve).
    
    Change-Id: Ie95914e74282b19f5654bbc3a2109d455da9e454

---
+++ src/chromiumos/tast/local/chrome/notification.go
@@ -0,0 +1,41 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package chrome
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+)
+
+// Notification corresponds to the "Notification" defined in
+// autotest_private.idl.
+type Notification struct {
+	ID       string `json:"id"`
+	Type     string `json:"type"`
+	Title    string `json:"title"`
+	Message  string `json:"message"`
+	Priority int    `json:"priority"`
+	Progress int    `json:"progress"`
+}
+
+// VisibleNotifications returns an array of visible notifications in Chrome.
+// tconn must be the connection returned by TestAPIConn.
+func VisibleNotifications(ctx context.Context, tconn *Conn) ([]Notification, error) {
+	var ret []Notification
+	if err := tconn.EvalPromise(ctx,
+		`new Promise((resolve, reject) => {
+		  chrome.autotestPrivate.getVisibleNotifications(function(items) {
+		      if (chrome.runtime.lastError) {
+		        reject(new Error(chrome.runtime.lastError.message))
+		        return
+		      }
+		      resolve(items)
+		  })
+		})`, &ret); err != nil {
+		return nil, errors.Wrap(err, "failed to call getVisibleNotifications")
+	}
+	return ret, nil
+}
