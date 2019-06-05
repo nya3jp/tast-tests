a7a9405790e0b95be0dd4a9a7968f86f79bbef88
Mathew King <mathewk@chromium.org>
2019-06-05 13:45:28 -0600

    tast: Test for ec ring device
    This test will open the cros-ec-ring buffer for reading and for each
    sensor in the ring set the ODR of to the max frequency for the sensor
    and collect data for 2 seconds. The test will fail if there are
    timestamps out of order or there are less than half the number of
    expected readings.
    
    BUG=b:132282412
    TEST=go test
         tast run ${REMOTE_IP} hardware.SensorRing
    
    Change-Id: I225bd95e26643c09d04406d60e259fa1a4b1391a

---
+++ src/chromiumos/tast/local/bundles/cros/hardware/iio/ring_test.go
@@ -0,0 +1,137 @@
+package iio
+
+import (
+	"encoding/binary"
+	"os"
+	"os/exec"
+	"path"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestRing(t *testing.T) {
+	defer setupTestFiles(t, map[string]string{
+		"iio:device0/name":                                "cros-ec-ring",
+		"iio:device0/buffer/enable":                       "0",
+		"iio:device0/buffer/length":                       "2",
+		"iio:device0/scan_elements/in_accel_flag_en":      "0",
+		"iio:device0/scan_elements/in_accel_flag_index":   "1",
+		"iio:device0/scan_elements/in_accel_flag_type":    "le:u8/8>>0",
+		"iio:device0/scan_elements/in_accel_id_en":        "0",
+		"iio:device0/scan_elements/in_accel_id_index":     "0",
+		"iio:device0/scan_elements/in_accel_id_type":      "le:u8/8>>0",
+		"iio:device0/scan_elements/in_accel_x_ring_en":    "0",
+		"iio:device0/scan_elements/in_accel_x_ring_index": "2",
+		"iio:device0/scan_elements/in_accel_x_ring_type":  "le:s16/16>>0",
+		"iio:device0/scan_elements/in_accel_y_ring_en":    "0",
+		"iio:device0/scan_elements/in_accel_y_ring_index": "3",
+		"iio:device0/scan_elements/in_accel_y_ring_type":  "le:s16/16>>0",
+		"iio:device0/scan_elements/in_accel_z_ring_en":    "0",
+		"iio:device0/scan_elements/in_accel_z_ring_index": "4",
+		"iio:device0/scan_elements/in_accel_z_ring_type":  "le:s16/16>>0",
+		"iio:device0/scan_elements/in_timestamp_en":       "0",
+		"iio:device0/scan_elements/in_timestamp_index":    "5",
+		"iio:device0/scan_elements/in_timestamp_type":     "le:s64/64>>0",
+		"iio:device1/name":                                "cros-ec-accel",
+		"iio:device1/location":                            "lid",
+		"iio:device1/id":                                  "0",
+		"iio:device1/scale":                               "0.25",
+		"iio:device1/min_frequency":                       "100",
+		"iio:device1/max_frequency":                       "1000",
+		"iio:device2/name":                                "cros-ec-gyro",
+		"iio:device2/location":                            "base",
+		"iio:device2/id":                                  "1",
+		"iio:device2/scale":                               "0.01",
+		"iio:device2/min_frequency":                       "2000",
+		"iio:device2/max_frequency":                       "5000",
+	})()
+
+	ringData := []SensorReading{
+		// Sensor flush events
+		{[]float64{0, 0, 0}, 0, 0x3, 100},
+		{[]float64{0, 0, 0}, 1, 0x3, 200},
+		// Data events
+		{[]float64{.25, .5, 1}, 0, 0x0, 300},
+		{[]float64{.01, .02, .1}, 1, 0x0, 400},
+		{[]float64{1, 2, 3}, 1, 0x0, 500},
+		{[]float64{-4, -5, 6}, 1, 0x0, 600},
+		{[]float64{0, -.25, 1.75}, 0, 0x0, 700},
+		{[]float64{40, -50, 60}, 1, 0x0, 800},
+	}
+
+	dutSensors, err := GetSensors()
+	if err != nil {
+		t.Fatal("Error reading sensors on DUT: ", err)
+	}
+
+	ring, err := NewRing(dutSensors)
+	if err != nil {
+		t.Fatal("Sensor ring not found: ", err)
+	}
+
+	if err := os.MkdirAll(path.Join(basePath, "dev"), 0755); err != nil {
+		t.Fatal("Error making dev dir: ", err)
+	}
+
+	fifoFile := path.Join(basePath, "dev/iio:device0")
+
+	// Use mkfifo to simulate an iio buffer
+	cmd := exec.Command("mkfifo", fifoFile)
+	if err := cmd.Run(); err != nil {
+		t.Fatal("Error making buffer fifo: ", err)
+	}
+
+	go func() {
+		bytes := make([]byte, 16)
+
+		f, err := os.OpenFile(fifoFile, os.O_WRONLY, 0)
+		if err != nil {
+			t.Fatal("Error opening named pipe for writing: ", err)
+		}
+		defer f.Close()
+
+		for _, r := range ringData {
+			s := ring.Sensors[r.ID]
+			bytes[0] = uint8(r.ID)
+			bytes[1] = uint8(r.Flags)
+			binary.LittleEndian.PutUint16(bytes[2:4], uint16(r.Data[0]/s.Sensor.Scale))
+			binary.LittleEndian.PutUint16(bytes[4:6], uint16(r.Data[1]/s.Sensor.Scale))
+			binary.LittleEndian.PutUint16(bytes[6:8], uint16(r.Data[2]/s.Sensor.Scale))
+			binary.LittleEndian.PutUint64(bytes[8:], uint64(r.Timestamp))
+
+			_, err = f.Write(bytes)
+			if err != nil {
+				t.Fatalf("Error writing %v to named pipe: %v", bytes, err)
+			}
+		}
+	}()
+
+	readings, err := ring.Open()
+	if err != nil {
+		t.Fatal("Error opening ring: ", err)
+	}
+	defer ring.Close()
+
+	read := []SensorReading{}
+	timeout := time.After(5 * time.Second)
+l:
+	for {
+		select {
+		case r, ok := <-readings:
+			if !ok {
+				t.Fatal("Ring unexpectedly closed")
+			}
+			read = append(read, *r)
+			if len(read) == len(ringData)-2 {
+				break l
+			}
+		case <-timeout:
+			t.Fatal("Timeout reading from ring")
+		}
+	}
+
+	if !reflect.DeepEqual(ringData[2:], read) {
+		t.Fatalf("Error reading from ring buffer: got %v; want %v", read, ringData[2:])
+	}
+}
