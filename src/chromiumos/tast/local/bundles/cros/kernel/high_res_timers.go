00eb31b5894812fe481e902dcf659101e34db768
Katherine Threlkeld <kathrelkeld@google.com>
2019-06-06 16:44:59 -0700

    Port platform_HighResTimers to Tast
    Add a Tast test to eventually replace platform_HighResTimers.
    
    TEST=ran on a few different devices. Behavior is the same as the Autotest
    BUG=chromium:963325
    
    Change-Id: I890ca032ad90537f476587f667d16323d8af33c3

---
+++ src/chromiumos/tast/local/bundles/cros/kernel/high_res_timers.go
@@ -0,0 +1,55 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package kernel
+
+import (
+	"bufio"
+	"context"
+	"os"
+	"regexp"
+	"strconv"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: HighResTimers,
+		Desc: "Fails if timers have nanosecond resolution that is not 1 ns",
+		Contacts: []string{
+			"chromeos-kernel@google.com", // Kernel team list
+			"kathrelkeld@chromium.org",   // Test port author
+		},
+		Attr: []string{"informational"},
+	})
+}
+
+// HighResTimers reads from /proc/timer_list to verify that any resolution
+// listed in nsecs has a value of 1.
+func HighResTimers(ctx context.Context, s *testing.State) {
+	re := regexp.MustCompile(`^\s*\.resolution:\s(\d+)\s*nsecs$`)
+
+	f, err := os.Open("/proc/timer_list")
+	if err != nil {
+		s.Error("Failed to open timer list: ", err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if matches := re.FindStringSubmatch(scanner.Text()); matches != nil {
+			res, err := strconv.Atoi(matches[1])
+			if err != nil {
+				s.Error("Error convering resolution to int: ", err)
+			}
+			if res != 1 {
+				s.Errorf("Timer resoultion: got %d, want 1 ns", res)
+			}
+		}
+	}
+	if scanner.Err(); err != nil {
+		s.Error("Error reading timers file: ", err)
+	}
+}
