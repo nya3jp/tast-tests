fe7705b52ab4b2d2600cc90b2f05df4bee68e9c8
Sheng-Hao Tsao <shenghao@google.com>
2019-06-06 16:54:20 +0800

    Add CCAUIPreviewOptions tast test
    This CL adds the CCAUIPreviewOptions tast test according to the manual
    test sheet: go/cca-tests
    
    BUG=b:117817410
    TEST=Verified on reef and eve that the test passes
    
    Change-Id: I67b682ce7a7ef086b16de1c878e7e8f7b46b0334
    Reviewed-on: https://chromium-review.googlesource.com/1646680
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Sheng-hao Tsao <shenghao@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_preview_options.go
@@ -0,0 +1,101 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/camera/cca"
+	// TODO(crbug.com/963772): Move libraries in video to camera or media folder.
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUIPreviewOptions,
+		Desc:         "Opens CCA and verifies the use cases of preview options like grid and mirror",
+		Contacts:     []string{"shenghao@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome", caps.BuiltinCamera},
+		Data:         []string{"cca_ui.js", "cca_ui_preview_options.js", "cca_ui_multi_camera.js"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func CCAUIPreviewOptions(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	app, err := cca.New(ctx, cr, []string{
+		s.DataPath("cca_ui.js"),
+		s.DataPath("cca_ui_preview_options.js"),
+		s.DataPath("cca_ui_multi_camera.js")})
+	if err != nil {
+		s.Fatal("Failed to open CCA: ", err)
+	}
+	defer app.Close(ctx)
+
+	if err := app.CheckVideoActive(ctx); err != nil {
+		s.Fatal("Preview is inactive after launching app: ", err)
+	}
+	s.Log("Preview started")
+
+	if exist, err := app.MirrorButtonExists(ctx); err != nil {
+		s.Error("Failed to get mirroring button state: ", err)
+	} else if !exist {
+		s.Error("Mirroring button unexpectedly disappeared")
+	}
+
+	// The default camera should be front camera, and mirroring should be enabled.
+	if mirrored, err := app.Mirrored(ctx); err != nil {
+		s.Error("Failed to get mirrored state: ", err)
+	} else if !mirrored {
+		s.Error("Mirroring unexpectedly disabled")
+	}
+
+	err = app.ToggleMirroringOption(ctx)
+	if err != nil {
+		s.Fatal("Toggling mirror option failed: ", err)
+	}
+
+	numCameras, err := app.GetNumOfCameras(ctx)
+	if err != nil {
+		s.Fatal("Can't get number of cameras: ", err)
+	}
+
+	if numCameras > 1 {
+		s.Log("Testing multi-camera scenario")
+		// Switch camera.
+		if err := app.SwitchCamera(ctx); err != nil {
+			s.Fatal("Switching camera failed: ", err)
+		}
+
+		facing, err := app.GetFacing(ctx)
+		if err != nil {
+			s.Fatal("Geting facing failed: ", err)
+		}
+
+		// Front facing and external camera should turn on mirror by default.
+		// Back camera should not.
+		if mirrored, err := app.Mirrored(ctx); err != nil {
+			s.Error("Failed to get mirrored state: ", err)
+		} else if mirrored != (facing != cca.FacingBack) {
+			s.Errorf("Mirroring state is unexpected: got %v, want %v", mirrored, facing != cca.FacingBack)
+		}
+
+		// Switch camera.
+		if err := app.SwitchCamera(ctx); err != nil {
+			s.Fatal("Switching camera failed: ", err)
+		}
+
+		// Mirror state should persist for each camera respectively.
+		if mirrored, err := app.Mirrored(ctx); err != nil {
+			s.Error("Failed to get mirrored state: ", err)
+		} else if mirrored {
+			s.Error("Mirroring unexpectedly enabled")
+		}
+	}
+}
