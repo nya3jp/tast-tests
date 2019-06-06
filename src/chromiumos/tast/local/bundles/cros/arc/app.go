be48b61ef3bd536026075a05255e9d93255a8a9f
Keigo Oka <oka@chromium.org>
2019-06-06 16:24:45 +0900

    arc: add sanity test running minimal app
    TEST=Test passes if betty is not broken (tested against `cros chrome-sdk --board=betty --version=12247.0.0`)
    TEST=Test fails if betty is broken # (tested against `cros chrome-sdk --board=betty --version=12211.0.0`)
    BUG=chromium:967936
    
    Change-Id: I2dd7183e64dfd974330e6bd245bc455c5acb2c7c

---
+++ src/chromiumos/tast/local/bundles/cros/arc/app.go
@@ -0,0 +1,63 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         App,
+		Desc:         "Sanity check to starts a simple app",
+		Contacts:     []string{"oka@chromium.org", "arc-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "chrome"},
+		Data:         []string{"app_hello-world.apk"},
+		Pre:          arc.Booted(),
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func App(ctx context.Context, s *testing.State) {
+	const (
+		// This is a plain hello world app:
+		// https://codelabs.developers.google.com/codelabs/android-training-hello-world
+		apk = "app_hello-world.apk"
+		pkg = "android.example.com"
+		cls = ".MainActivity"
+	)
+
+	shortCtx, shortChancel := ctxutil.Shorten(ctx, 15*time.Second)
+	defer shortChancel()
+
+	a := s.PreValue().(arc.PreData).ARC
+
+	s.Log("Starting app")
+	if err := a.Install(shortCtx, s.DataPath(apk)); err != nil {
+		s.Fatal("Failed installing app: ", err)
+	}
+	act, err := arc.NewActivity(a, pkg, cls)
+	if err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	defer act.Close()
+
+	if err = act.Start(shortCtx); err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	bounds, err := act.SurfaceBounds(shortCtx)
+	if err != nil {
+		s.Fatal("Failed to get window bounds: ", err)
+	}
+	if bounds.Height <= 0 || bounds.Width <= 0 {
+		s.Errorf("Bounds should be positive but was %dx%d", bounds.Height, bounds.Width)
+	}
+}
