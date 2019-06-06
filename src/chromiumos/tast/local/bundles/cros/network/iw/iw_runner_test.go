71ab0224b05f8527a4cf804f3c08189d46b66d43
Billy Zhao <billyzhao@google.com>
2019-06-06 09:44:06 -0700

    Add iw_runner.go (and unit tests), a wrapper around the `iw` utility.
    Currently, only scan flow is supported.
    
    BUG=None
    TEST=unit tests
    
    Change-Id: I98bc4dde8cae62fe9917ef3febc7834a11fab17e

---
+++ src/chromiumos/tast/local/bundles/cros/network/iw/iw_runner_test.go
@@ -0,0 +1,93 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package iw contains utility functions to wrap around the iw program.
+package iw
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestGetAllLinkKeys(t *testing.T) {
+	const testStr = `Connected to 74:e5:43:10:4f:c0 (on wlan0)
+      SSID: PMKSACaching_4m9p5_ch1
+      freq: 5220
+      RX: 5370 bytes (37 packets)
+      TX: 3604 bytes (15 packets)
+      signal: -59 dBm
+      tx bitrate: 13.0 MBit/s MCS 1
+
+      bss flags:      short-slot-time
+      dtim period:    5
+      beacon int:     100`
+	cmpMap := map[string]string{
+		"SSID":        "PMKSACaching_4m9p5_ch1",
+		"freq":        "5220",
+		"TX":          "3604 bytes (15 packets)",
+		"signal":      "-59 dBm",
+		"bss flags":   "short-slot-time",
+		"dtim period": "5",
+		"beacon int":  "100",
+		"RX":          "5370 bytes (37 packets)",
+		"tx bitrate":  "13.0 MBit/s MCS 1",
+	}
+	linkMap := getAllLinkKeys(testStr)
+	if diff := cmp.Diff(linkMap, cmpMap); diff != "" {
+		t.Error("getAllLinkKeys: Map behavior differs from expected: Got ", linkMap, " but expected ", cmpMap)
+	}
+}
+
+func TestExtractBSSID(t *testing.T) {
+	const testStr1 = `Connected to 04:f0:21:03:7d:bb (on wlan0)`
+	const testStr2 = `Station 04:f0:21:03:7d:bb (on mesh-5000mhz)`
+	const cmpStr = `04:f0:21:03:7d:bb`
+	res := extractBSSID(testStr1, "wlan0", false)
+	if res != cmpStr {
+		t.Errorf("extractBSSID(%q) = %q; want %q", testStr1, res, cmpStr)
+	}
+	res = extractBSSID(testStr2, "mesh-5000mhz", true)
+	if res != cmpStr {
+
+		t.Errorf("extractBSSID(%q) = %q; want %q", testStr2, res, cmpStr)
+	}
+}
+
+func TestParseScanResults(t *testing.T) {
+	const testStr = `BSS 00:11:22:33:44:55(on wlan0)
+          freq: 2447
+          beacon interval: 100 TUs
+          signal: -46.00 dBm
+          Information elements from Probe Response frame:
+          SSID: my_open_network
+          Extended supported rates: 24.0 36.0 48.0 54.0
+          HT capabilities:
+          Capabilities: 0x0c
+          HT20
+          HT operation:
+          * primary channel: 8
+          * secondary channel offset: no secondary
+          * STA channel width: 20 MHz
+          RSN: * Version: 1
+          * Group cipher: CCMP
+          * Pairwise ciphers: CCMP
+          * Authentication suites: PSK
+          * Capabilities: 1-PTKSA-RC 1-GTKSA-RC (0x0000)`
+	l, err := parseScanResults(testStr)
+	if err != nil {
+		t.Fatal("parseScanResults failed: ", err.Error())
+	}
+	cmpBSS := BSSData{
+		BSS:       "00:11:22:33:44:55",
+		Frequency: 2447,
+		SSID:      "my_open_network",
+		Security:  "RSN",
+		HT:        "HT20",
+		Signal:    -46,
+	}
+	if diff := cmp.Diff(*l[0], cmpBSS); diff != "" {
+		t.Errorf("parseScanResults returned unexpected result; diff: %s\n", diff)
+	}
+}
