3f77fd3193465c5d4d68d4a90ef93c73a12dcd75
jasongustaman <jasongustaman@google.com>
2019-06-07 10:40:20 +0900

    arc: Add local tast arcvm support
    This patch differentiates arc++ and arcvm boot for Tast.
    It changes the boot flow due to the current state of arcvm
    which doesn't support direct access to cmdline like arc++.
    
    BUG=b:134100635
    TEST=tast run root@172.30.6.69 arc.Clipboard (with modified constants)
    on eve-arcvm and nocturne.
    
    Cq-Depend: chromium:1648539, chromium:1666679
    Change-Id: I0f8f236f5cd8d1ce697efbec2ea9b1f7ad72e030

--- src/chromiumos/tast/local/arc/proc.go
+++ src/chromiumos/tast/local/arc/proc.go
@@ -13,20 +13,34 @@ import (
 
 // InitPID returns the PID (outside the container) of the ARC init process.
 func InitPID() (int32, error) {
-	uid, err := sysutil.GetUID("android-root")
-	if err != nil {
-		return -1, err
-	}
+	if guest == container {
+		uid, err := sysutil.GetUID("android-root")
+		if err != nil {
+			return -1, err
+		}
 
-	procs, err := process.Processes()
-	if err != nil {
-		return -1, errors.Wrap(err, "failed to list processes")
-	}
+		procs, err := process.Processes()
+		if err != nil {
+			return -1, errors.Wrap(err, "failed to list processes")
+		}
 
-	const initPath = "/init"
-	for _, p := range procs {
-		if uids, err := p.Uids(); err == nil && uint32(uids[0]) == uid {
-			if exe, err := p.Exe(); err == nil && exe == initPath {
+		const initPath = "/init"
+		for _, p := range procs {
+			if uids, err := p.Uids(); err == nil && uint32(uids[0]) == uid {
+				if exe, err := p.Exe(); err == nil && exe == initPath {
+					return p.Pid, nil
+				}
+			}
+		}
+	} else {
+		procs, err := process.Processes()
+		if err != nil {
+			return -1, errors.Wrap(err, "failed to list processes")
+		}
+
+		const crosvmPath = "/usr/bin/crosvm"
+		for _, p := range procs {
+			if exe, err := p.Exe(); err == nil && exe == crosvmPath {
 				return p.Pid, nil
 			}
 		}
