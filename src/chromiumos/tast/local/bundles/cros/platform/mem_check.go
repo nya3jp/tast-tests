6f5ff8172d152ad26b1ac4849f96205c0a1edfb1
Hidehiko Abe <hidehiko@chromium.org>
2019-06-11 02:09:15 +0900

    tast-tests: Port platform_MemCheck.
    BUG=chromium:971027
    TEST=Ran on DUT.
    
    Change-Id: Ie829e0748de6df632ee6f98e9748a879fbf0815b

---
+++ src/chromiumos/tast/local/bundles/cros/platform/mem_check.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/platform/memcheck"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: MemCheck,
+		Desc: "Verifies memory usage looks correct",
+		Contacts: []string{
+			"puthik@chromium.org", // original test author.
+			"chromeos-memory@google.com",
+			"hidehiko@chromium.org", // Tast port author.
+		},
+		Attr: []string{"informational"},
+	})
+}
+
+func MemCheck(ctx context.Context, s *testing.State) {
+	// This test is ported from platform_MemCheck autotest.
+	// The test is also run as a part of sequence/control.memory_qual and
+	// its family. At the moment, those are not yet ported, so the value
+	// output feature is not implemented.
+	memcheck.RunTest(ctx, s)
+}
