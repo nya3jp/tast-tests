9cb18ba9e0f7dacf201a1f7296574a37a7fe9d17
David Staessens <dstaessens@chromium.org>
2019-06-12 17:00:07 +0900

    video.EncodeAccelPerf*: Increase timeout of the EncodeAccelPerf* Tast tests.
    This CL increases the timeout of the EncodeAccelPerf* Tast tests. The default
    timeout of 2 minutes is not always enough. On some platforms a successful run
    seems to take more then 1m30s, at which time the tests gets aborted to leave
    enough time for cleanup. Increased the timeout to 3 minutes.
    
    TEST=video.EncodeAccelPerf on eve
    
    BUG=chromium:973325
    Change-Id: Ica4be679183f7c53ae1c6dba487683c3d566ede8

--- src/chromiumos/tast/local/bundles/cros/video/encode_accel_perf_h264_720p_i420_vidyo.go
+++ src/chromiumos/tast/local/bundles/cros/video/encode_accel_perf_h264_720p_i420_vidyo.go
@@ -6,6 +6,7 @@ package video
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/local/bundles/cros/video/encode"
 	"chromiumos/tast/local/bundles/cros/video/lib/caps"
@@ -21,6 +22,8 @@ func init() {
 		Contacts:     []string{"johnylin@chromium.org", "chromeos-video-eng@google.com"},
 		SoftwareDeps: []string{"chrome", caps.HWEncodeH264},
 		Data:         []string{encode.Vidyo720P.Name},
+		// Default timeout (i.e. 2 minutes) is not enough.
+		Timeout: 3 * time.Minute,
 	})
 }
 
