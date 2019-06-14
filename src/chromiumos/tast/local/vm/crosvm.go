db9612d375e9e57ebd45dde2bb280212110f9362
Trent Begin <tbegin@google.com>
2019-06-14 12:04:53 -0600

    tast-tests: improve crosvm library to be more generic
    This was work I did for the vm.StartSludge test that ended up not being
    needed, however it should be applicable for additional VMs in the
    future.
    
    BUG=none
    TEST=tast run --build dut vm.StartCrosvm
    
    Change-Id: Id1ac608c217200e60f0004dfd93a3ece2b3ef67d

--- src/chromiumos/tast/local/vm/crosvm.go
+++ src/chromiumos/tast/local/vm/crosvm.go
@@ -10,7 +10,11 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
@@ -20,38 +24,63 @@ type Crosvm struct {
 	cmd        *testexec.Cmd // crosvm process
 	socketPath string        // crosvm control socket
 	stdin      io.Writer     // stdin for cmd
-	stdout     io.Reader     // stdout for cmd
+	stdout     *os.File      // stdout for cmd; uses os.File to set a read dealine
+}
+
+// CrosvmParams - Parameters for starting a crosvm instance.
+type CrosvmParams struct {
+	VMPath      string   // file path where VM rootfs and kernel are stored
+	DiskPaths   []string // paths that will be mounted read only
+	RWDiskPaths []string // paths that will be mounted read/write
+	KernelArgs  []string // string arguments to be passed to the VM kernel
 }
 
 // NewCrosvm starts a crosvm instance with the optional disk path as an additional disk.
-func NewCrosvm(ctx context.Context, diskPath string, kernelArgs []string) (*Crosvm, error) {
-	componentPath, err := LoadTerminaComponent(ctx)
-	if err != nil {
-		return nil, err
+func NewCrosvm(ctx context.Context, params *CrosvmParams) (*Crosvm, error) {
+	if _, err := os.Stat(params.VMPath); err != nil {
+		return nil, errors.Wrap(err, "failed to find VM")
 	}
 
 	vm := &Crosvm{}
 
+	var err error
 	if vm.socketPath, err = genSocketPath(); err != nil {
 		return nil, err
 	}
 	args := []string{"run", "--socket", vm.socketPath, "--root",
-		filepath.Join(componentPath, "vm_rootfs.img")}
-	if diskPath != "" {
-		args = append(args, "--rwdisk", diskPath)
+		filepath.Join(params.VMPath, "vm_rootfs.img")}
+
+	for _, path := range params.RWDiskPaths {
+		args = append(args, "--rwdisk", path)
 	}
-	args = append(args, kernelArgs...)
-	args = append(args, filepath.Join(componentPath, "vm_kernel"))
+
+	for _, path := range params.DiskPaths {
+		args = append(args, "-d", path)
+	}
+
+	for _, arg := range params.KernelArgs {
+		args = append(args, "-p", arg)
+	}
+
+	args = append(args, filepath.Join(params.VMPath, "vm_kernel"))
 
 	vm.cmd = testexec.CommandContext(ctx, "crosvm", args...)
 
 	if vm.stdin, err = vm.cmd.StdinPipe(); err != nil {
 		return nil, err
 	}
-	if vm.stdout, err = vm.cmd.StdoutPipe(); err != nil {
+
+	pr, pw, err := os.Pipe()
+	if err != nil {
 		return nil, err
 	}
+	vm.cmd.Stdout = pw
+	vm.stdout = pr
+
+	defer pw.Close()
+
 	if err = vm.cmd.Start(); err != nil {
+		pr.Close()
 		return nil, err
 	}
 	return vm, nil
@@ -59,6 +88,7 @@ func NewCrosvm(ctx context.Context, diskPath string, kernelArgs []string) (*Cros
 
 // Close stops the crosvm process (and underlying VM) started by NewCrosvm.
 func (vm *Crosvm) Close(ctx context.Context) error {
+	defer vm.stdout.Close()
 	cmd := testexec.CommandContext(ctx, "crosvm", "stop", vm.socketPath)
 	if err := cmd.Run(); err != nil {
 		testing.ContextLog(ctx, "Failed to exec stop: ", err)
@@ -70,6 +100,7 @@ func (vm *Crosvm) Close(ctx context.Context) error {
 		vm.cmd.DumpLog(ctx)
 		return err
 	}
+
 	return nil
 }
 
@@ -94,4 +125,62 @@ func genSocketPath() (string, error) {
 		return "", err
 	}
 	return file.Name(), nil
+}
+
+// WaitForOutput waits until a line matched by re has been written to stdout,
+// crosvm's stdout is closed, or the deadline is reached. It returns the full
+// line that was matched. This function will consume output from stdout until it
+// returns.
+func (vm *Crosvm) WaitForOutput(ctx context.Context, re *regexp.Regexp) (string, error) {
+	// Start a goroutine that reads bytes from crosvm and buffers them in a
+	// string builder. We can't do this with lines because then we will miss the
+	// initial prompt that comes up that doesn't have a line terminator. If a
+	// matching line is found, send it through the channel.
+	type result struct {
+		line string
+		err  error
+	}
+	ch := make(chan result, 1)
+	// Allow the blocking read call to stop when the deadline has been exceeded.
+	// Defer removing the deadline until this function has exited.
+	deadline, ok := ctx.Deadline()
+	// If no deadline is set, default to no timeout.
+	if !ok {
+		deadline = time.Time{}
+	}
+	vm.stdout.SetReadDeadline(deadline)
+	defer vm.stdout.SetReadDeadline(time.Time{})
+	go func() {
+		defer close(ch)
+		var line strings.Builder
+		var b [1]byte
+		for {
+			_, err := vm.stdout.Read(b[:])
+			if err != nil {
+				ch <- result{"", err}
+				return
+			}
+			if b[0] == '\n' {
+				line.Reset()
+				continue
+			}
+			line.WriteByte(b[0])
+			if re.MatchString(line.String()) {
+				ch <- result{line.String(), nil}
+				return
+			}
+		}
+	}()
+
+	select {
+	case r := <-ch:
+		if os.IsTimeout(r.err) {
+			// If the read times out, this means the deadline has passed
+			select {
+			case <-ctx.Done():
+				return "", errors.Wrap(ctx.Err(), "timeout out waiting for output")
+			}
+		}
+		return r.line, r.err
+	}
 }
