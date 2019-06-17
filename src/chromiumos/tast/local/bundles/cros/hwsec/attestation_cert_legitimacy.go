e8df69a43ef6ded940fd819e13df54e7b82e6544
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: everything else
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} hwec.*
    
    Change-Id: I201797fec1315d5a21ec97979d35003b51751ef9

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/attestation_cert_legitimacy.go
@@ -0,0 +1,159 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	libhwsec "chromiumos/tast/common/hwsec"
+	"chromiumos/tast/local/chrome"
+	libhwseclocal "chromiumos/tast/local/hwsec"
+	a9n "chromiumos/tast/local/hwsec/attestation"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AttestationCertLegitimacy,
+		Desc: "Verifies login is the pre-condition of cert and test expired cert can't be finished",
+		Attr: []string{"informational"},
+		Contacts: []string{
+			"cylai@chromium.org", // Nobody
+		},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func AttestationCertLegitimacy(ctx context.Context, s *testing.State) {
+	s.Log("Start test with creating a helper and a utility")
+	helper, err := libhwseclocal.NewHelperLocal()
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+	utility, err := libhwsec.NewUtility(ctx, helper, libhwsec.CryptohomeBinaryType)
+	if err != nil {
+		s.Error("Utilty creation error: ", err)
+		return
+	}
+	if err := libhwsec.EnsureTpmIsReady(ctx, utility, a9n.DefaultTakingOwnershipTimeout); err != nil {
+		s.Error("Failed to ensure tpm readiness: ", err)
+		return
+	}
+	s.Log("Tpm is ensured to be ready")
+	if err := libhwsec.EnsureIsPreparedForEnrollment(ctx,
+		utility, a9n.DefaultPreparationForEnrolmentTimeout); err != nil {
+		s.Error("Failed to prepare for enrollment: ", err)
+		return
+	}
+	s.Log("Attestation is prepared for enrollment")
+	// For now, just uses '0' as in "default CA"
+	req, err := utility.CreateEnrollRequest(a9n.DefaultACAType)
+	s.Log(len(req))
+	if err != nil {
+		s.Error("Failed to create enroll request: ", err)
+		return
+	}
+	//resp, err := a9n.SendPostRequestTo(req, "https://asbestos-qa.corp.google.com/enroll")
+	resp, err := a9n.SendPostRequestTo(req, "https://chromeos-ca.gstatic.com/enroll")
+	if err != nil {
+		s.Error("Failed to send request to CA: ", err)
+	}
+	// For now, just uses '0' as in "default CA"
+	err = utility.FinishEnroll(a9n.DefaultACAType, resp)
+	if err != nil {
+		s.Error("Failed to finish enrollment: ", err)
+	}
+	isEnrolled, err := utility.IsEnrolled()
+	if err != nil {
+		s.Error("Failed to get enrollment status: ", err)
+	}
+	if !isEnrolled {
+		s.Error("Inconsistent reported status: after enrollment, status shows 'not enrolled'")
+	}
+	s.Log("The DUT is enrolled")
+
+	s.Log("Creating ceritificate request")
+
+	username := "test@crashwsec.bigr.name"
+
+	req, err = utility.CreateCertRequest(
+		a9n.DefaultACAType,
+		a9n.DefaultCertProfile,
+		username,
+		a9n.DefaultCertOrigin)
+	if err != nil {
+		s.Error("Failed to create certificate request: ", err)
+	}
+	s.Log("Created certificate request")
+
+	s.Log("Sending sign request")
+	resp, err = a9n.SendPostRequestTo(req, "https://chromeos-ca.gstatic.com/sign")
+	//resp, err = a9n.SendPostRequestTo(req, "https://asbestos-qa.corp.google.com/sign")
+	if err != nil {
+		s.Error("Failed to send request to CA: ", err)
+	}
+	if len(resp) == 0 {
+		s.Fatal("Unexpected empty cert")
+	}
+
+	s.Log("Finishing certificate request w/o login")
+	err = utility.FinishCertRequest(resp, username, a9n.DefaultCertLabel)
+
+	if err == nil {
+		s.Error("Expecting failure during finishing user cert: ", err)
+		return
+	}
+	// auth := chrome.Auth("cros@crosdmsregtest.com", "testpass", "gaia-id")
+	auth := chrome.Auth("test@crashwsec.bigr.name", "testpass", "gaia-id")
+	cr, err := chrome.New(ctx, auth)
+	if err != nil {
+		s.Fatal("Failed to log in by Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+	if username != cr.User() {
+		s.Fatal("Inconsistent username for cert and login")
+	}
+	s.Log("Chrome user: ", username)
+
+	s.Log("Finishing expired certificate request")
+	err = utility.FinishCertRequest(resp, username, a9n.DefaultCertLabel)
+	if err == nil {
+		s.Error("Expecting failure of finishing expired cert: ", err)
+		return
+	}
+
+	s.Log("Finishing certificate request after login")
+	s.Log("Creating ceritificate request")
+
+	req, err = utility.CreateCertRequest(
+		a9n.DefaultACAType,
+		a9n.DefaultCertProfile,
+		username,
+		a9n.DefaultCertOrigin)
+	if err != nil {
+		s.Error("Failed to create certificate request: ", err)
+	}
+	s.Log("Created certificate request")
+
+	s.Log("Sending sign request")
+	resp, err = a9n.SendPostRequestTo(req, "https://chromeos-ca.gstatic.com/sign")
+	//resp, err = a9n.SendPostRequestTo(req, "https://asbestos-qa.corp.google.com/sign")
+	if err != nil {
+		s.Error("Failed to send request to CA: ", err)
+	}
+	if len(resp) == 0 {
+		s.Fatal("Unexpected empty cert")
+	}
+
+	s.Log("Finishing certificate request")
+	err = utility.FinishCertRequest(resp, username, a9n.DefaultCertLabel)
+
+	if err != nil {
+		s.Error("Failed to finish user cert: ", err)
+		return
+	}
+
+	s.Log("Finished certificate request")
+}
---
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/clear_ownership.go
@@ -0,0 +1,50 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	libhwsec "chromiumos/tast/common/hwsec"
+	libhwsecremote "chromiumos/tast/remote/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ClearOwnership,
+		Desc:         "Verifies that the TPM ownership can be cleared",
+		Contacts:     []string{"cylai@google.com"},
+		SoftwareDeps: []string{"reboot"},
+		Attr:         []string{"informational"},
+	})
+}
+
+func ClearOwnership(ctx context.Context, s *testing.State) {
+	s.Log("Start test with creating a helper and a utility")
+	helper, err := libhwsecremote.NewHelperRemote(s.DUT())
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+	utility, err := libhwsec.NewUtility(ctx, &helper.Helper, libhwsec.CryptohomeBinaryType)
+	if err != nil {
+		s.Fatal("Utilty creation error: ", err)
+	}
+	s.Log("Start resetting TPM if needed")
+	if err := helper.EnsureTpmIsReset(ctx, utility); err != nil {
+		s.Fatal("Failed to ensure resetting TPM: ", err)
+	}
+	s.Log("TPM is confirmed to be reset")
+
+	if result, err := utility.IsPreparedForEnrollment(); err != nil {
+		s.Fatal("Cannot check if enrollment preparation is reset")
+	} else if result {
+		s.Fatal("Enrollment preparation is not reset after clearing ownership")
+	}
+	s.Log("Enrolling with TPM not ready")
+	if _, err := utility.CreateEnrollRequest(0); err == nil {
+		s.Fatal("Enrollment should not happen w/o getting prepared")
+	}
+}
