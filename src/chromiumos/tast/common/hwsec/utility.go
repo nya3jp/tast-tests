22497a01205487a3a257d0f91332c0e6dbb601b3
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: utility interface and implementation
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} "FILTER_EVERYTHING_OUT"
    
    Change-Id: I56b3bb467062c111361b64013b7f3132578890c1

---
+++ src/chromiumos/tast/common/hwsec/utility.go
@@ -0,0 +1,202 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"time"
+
+	apb "chromiumos/system_api/attestation_proto"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// ClientType is an alias of string which is used as an enum to specify a
+// |Utility| implementation type.
+type ClientType string
+
+// The collection of all valid |ClientType|s.
+const (
+	// CryptohomeProxyLegacyType refers to the implementation that talks to
+	// cryptohomed via legacy cryptohome dbus interface.
+	CryptohomeProxyLegacyType ClientType = "CryptohomeProxyLegacy"
+	CryptohomeProxyNewType    ClientType = "CryptohomeProxyNew"
+	DistributedModeProxyType  ClientType = "DistributedModeProxy"
+	CryptohomeBinaryType      ClientType = "CryptohomeBinary"
+)
+
+const (
+	pollingIntervalMillis int = 10
+)
+
+// Utility is a collection of utility functions that run hwsec-related commands on the DUT.
+type Utility interface {
+	// IsTPMReady returns the flag to indicate if TPM is ready and any encounted error during the opeation.
+	IsTPMReady() (bool, error)
+
+	// IsPreparedForEnrollment returns the flag to indicate if the DUT is
+	// prepared for enrollment and any encounted error during the opeation.
+	IsPreparedForEnrollment() (bool, error)
+
+	// IsEnrolled returns the flag to indicate if the DUT is
+	// enrolled and any encounted error during the opeation.
+	IsEnrolled() (bool, error)
+
+	// Take TPM ownership on DUT and wait until either ownership is taken or the operation fails; returns the flag to tell if the the DUT's TPM is owned after the call and any error encounted during the operation.
+	EnsureOwnership() (bool, error)
+
+	// Checks if any currently mounted vault; If the operation succeeds, then error will be nil, and the bool will contain if any user vault is mounted (true if any vault is mounted). Otherwise, an error is returned.
+	IsMounted() (bool, error)
+
+	// Unmounts the vault of |username|
+	Unmount(username string) (bool, error)
+
+	// Creates the vault for |username| if not exist.
+	CreateVault(username string, password string) (bool, error)
+
+	// Removes the vault of |username|.
+	RemoveVault(username string) (bool, error)
+
+	// Reports if the vault key of |username| is TPM-backed.
+	IsTPMWrappedKeySet(username string) (bool, error)
+
+	// Creates an enroll request that is sent to the corresponding pca server of |PCAType|
+	// later, and any error encountered during the operation.
+	CreateEnrollRequest(PCAType int) (string, error)
+
+	// Finishes the enroll with |resp| from pca server of |PCAType|. Returns any
+	// encountered error during the operation.
+	FinishEnroll(PCAType int, resp string) error
+
+	// Creates a certificate request that is sent to the corresponding pca server
+	// of |PCAType| later, and any error encountered during the operation.
+	CreateCertRequest(PCAType int,
+		profile apb.CertificateProfile,
+		username string,
+		origin string) (string, error)
+
+	// Finishes the certified key creation with |resp| from PCA server. Returns any encountered error during the operation.
+	FinishCertRequest(response string, username string, label string) error
+
+	// Validates and then sign |challenge| from the VA server. See attestationd's dbus interface for document of the arguments.
+	SignEnterpriseVAChallenge(
+		VAType int,
+		username string,
+		label string,
+		domain string,
+		deviceID string,
+		includeSignedPublicKey bool,
+		challenge []byte) (string, error)
+
+	// Signs |challenge| with the attestation key of |username| with |label|. Uses system-level key if |username| is empty.
+	SignSimpleChallenge(
+		username string,
+		label string,
+		challenge []byte) (string, error)
+
+	// Gets the public key of |username| with |label|. Gets system-level key if |username| is empty.
+	GetPublicKey(
+		username string,
+		label string) (string, error)
+
+	// Gets the key payload of |username| with |label|. Gets system-level key payload if |username| is empty.
+	GetKeyPayload(
+		username string,
+		label string) (string, error)
+
+	// Sets the key payload of |username| with |label|. Sets system-level key payload  if |username| is empty.
+	SetKeyPayload(
+		username string,
+		label string,
+		payload string) (bool, error)
+
+	// Registers the key of |username| with |label|. Registers system-level key if |username| is empty.
+	RegisterKeyWithChapsToken(
+		username string,
+		label string) (bool, error)
+
+	// Sets the enrollment and cert request process to be handled in asynchronous way.
+	// Currently only applicable to implementation using cryptohome binary.
+	SetAttestationAsyncMode(async bool) error
+
+	// Gets the EID used for attestation.
+	GetEnrollmentID() (string, error)
+
+	// Gets the TPM owner password. Note that empty owner password doesn't causes errors.
+	GetOwnerPassword() (string, error)
+
+	// Clears the TPM owner password if the owner dependencies are all gone.
+	ClearOwnerPassword() error
+
+	// Delete all keys of |username| with label prefixed by |prefix| in attestation. Deletes system-level keys if |username| is empty.
+	DeleteKeys(username string, prefix string) error
+}
+
+// EnsureTPMIsReady ensures the TPM is ready when the function returns |nil|.
+// Otherwise, returns any encountered error, including timeout after
+// |timeoutInMs|.
+func EnsureTPMIsReady(ctx context.Context, utility Utility, timeoutInMs int) error {
+	isReady, err := utility.IsTPMReady()
+	if err != nil {
+		return errors.Wrap(err, "failed to ensure ownership due to error in |IsTPMReady|")
+	}
+	if isReady == false {
+		result, err := utility.EnsureOwnership()
+		if err != nil {
+			return errors.Wrap(err, "failed to ensure ownership due to error in |TakeOwnership|")
+		}
+		if result == false {
+			return errors.New("failed to take ownership")
+		}
+	}
+	return testing.Poll(ctx, func(context.Context) error {
+		isReady, _ := utility.IsTPMReady()
+		if isReady {
+			return nil
+		}
+		return errors.New("haven't confirmed to be owned")
+	}, &testing.PollOptions{
+		Timeout:  time.Duration(timeoutInMs) * time.Millisecond,
+		Interval: time.Duration(pollingIntervalMillis) * time.Millisecond,
+	})
+}
+
+// EnsureIsPreparedForEnrollment ensures the DUT is prepareed for enrollment
+// when the function returns |nil|. Otherwise, returns any encountered error,
+// including timeout after |timeoutInMs|.
+func EnsureIsPreparedForEnrollment(ctx context.Context, utility Utility, timeoutInMs int) error {
+	return testing.Poll(ctx, func(context.Context) error {
+		// intentionally ignores error; retry the operation until timeout.
+		isPrepared, err := utility.IsPreparedForEnrollment()
+		if err != nil {
+			return err
+		}
+		if !isPrepared {
+			return errors.New("not prepared yet")
+		}
+		return nil
+	}, &testing.PollOptions{
+		Timeout:  time.Duration(timeoutInMs) * time.Millisecond,
+		Interval: time.Duration(pollingIntervalMillis) * time.Millisecond,
+	})
+}
+
+// NewUtility returns the implementation corresponding to
+// |clientType|. See the document of each implementation for more details.
+func NewUtility(ctx context.Context, h *Helper, clientType ClientType) (Utility, error) {
+	switch clientType {
+	case CryptohomeBinaryType:
+		proxy, err := NewCryptohomeBinary(ctx, h)
+		if err != nil {
+			return nil, err
+		}
+		defaultAsynAttestationMode := true
+		return utilityCryptohomeBinary{utilityCommon{ctx}, proxy, &defaultAsynAttestationMode}, nil
+	case CryptohomeProxyLegacyType, CryptohomeProxyNewType, DistributedModeProxyType:
+		return nil, errors.New("not implemented")
+	default:
+		return nil, errors.New("unrecognized client type")
+	}
+}
---
+++ src/chromiumos/tast/common/hwsec/utility_cryptohome_binary.go
@@ -0,0 +1,338 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"strconv"
+	"strings"
+
+	apb "chromiumos/system_api/attestation_proto"
+	"chromiumos/tast/errors"
+)
+
+const (
+	tpmIsReadyString                  = "TPM Ready: true"
+	tpmIsNotReadyString               = "TPM Ready: false"
+	tpmIsAttestationPreparedString    = "Attestation Prepared: true"
+	tpmIsNotAttestationPreparedString = "Attestation Prepared: false"
+	tpmIsAttestationEnrolledString    = "Attestation Enrolled: true"
+	tpmIsNotAttestationEnrolledString = "Attestation Enrolled: false"
+	resultIsSuccessString             = "Result: Success"
+	resultIsFailureString             = "Result: Failure"
+	cryptohomeWrappedKeysetString     = "TPM_WRAPPED"
+)
+
+// utilityCryptohomeBinary implements |Utility| using
+// |CryptohomeProxyLegacy|.
+type utilityCryptohomeBinary struct {
+	utilityCommon
+	proxy                *CryptohomeBinary
+	attestationAsyncMode *bool
+}
+
+// IsTPMReady implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) IsTPMReady() (bool, error) {
+	out, err := utility.proxy.TPMStatus()
+	if err != nil {
+		return false, errors.Wrap(err, "failed to call tpm_status")
+	}
+	if strings.Contains(out, tpmIsReadyString) {
+		return true, nil
+	}
+	if strings.Contains(out, tpmIsNotReadyString) {
+		return false, nil
+	}
+	return false, errors.New("unexpected output from |cryptohome|")
+}
+
+// IsPreparedForEnrollment implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) IsPreparedForEnrollment() (bool, error) {
+	out, err := utility.proxy.TPMAttestationStatus()
+	if err != nil {
+		return false, errors.Wrap(err, "failed to call cryptohome")
+	}
+	if strings.Contains(out, tpmIsAttestationPreparedString) {
+		return true, nil
+	}
+	if strings.Contains(out, tpmIsNotAttestationPreparedString) {
+		return false, nil
+	}
+	return false, errors.New("unexpected output from |cryptohome|")
+}
+
+// IsEnrolled implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) IsEnrolled() (bool, error) {
+	out, err := utility.proxy.TPMAttestationStatus()
+	if err != nil {
+		return false, errors.Wrap(err, "failed to call cryptohome")
+	}
+	if strings.Contains(out, tpmIsAttestationEnrolledString) {
+		return true, nil
+	}
+	if strings.Contains(out, tpmIsNotAttestationEnrolledString) {
+		return false, nil
+	}
+	return false, errors.New("unexpected output from |cryptohome|")
+}
+
+// EnsureOwnership implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) EnsureOwnership() (bool, error) {
+	if err := utility.proxy.TPMTakeOwnership(); err != nil {
+		return false, errors.Wrap(err, "failed to take ownership")
+	}
+	if err := utility.proxy.TPMWaitOwnership(); err != nil {
+		return false, errors.Wrap(err, "failed to wait ownership")
+	}
+	return true, nil
+}
+
+// CreateEnrollRequest implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) CreateEnrollRequest(PCAType int) (string, error) {
+	return utility.proxy.TPMAttestationStartEnroll(PCAType, *utility.attestationAsyncMode)
+}
+
+// FinishEnroll implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) FinishEnroll(PCAType int, resp string) error {
+	result, err := utility.proxy.TPMAttestationFinishEnroll(PCAType, resp, *utility.attestationAsyncMode)
+	if err != nil {
+		return errors.Wrap(err, "Failed to enroll")
+	}
+	if result != true {
+		return errors.New("Failed to enroll")
+	}
+	return nil
+}
+
+// CreateCertRequest implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) CreateCertRequest(
+	PCAType int,
+	profile apb.CertificateProfile,
+	username string,
+	origin string) (string, error) {
+	return utility.proxy.TPMAttestationStartCertRequest(PCAType, int(profile), username, origin, *utility.attestationAsyncMode)
+}
+
+// FinishCertRequest implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) FinishCertRequest(resp string, username string, label string) error {
+	cert, err := utility.proxy.TPMAttestationFinishCertRequest(resp, username, label, *utility.attestationAsyncMode)
+	if err != nil {
+		return errors.Wrap(err, "Failed to finish cert request")
+	}
+	if cert == "" {
+		return errors.New("unexpected empty cert")
+	}
+	return nil
+}
+
+// SignEnterpriseVAChallenge implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) SignEnterpriseVAChallenge(
+	VAType int,
+	username string,
+	label string,
+	domain string,
+	deviceID string,
+	includeSignedPublicKey bool,
+	challenge []byte) (string, error) {
+	if !includeSignedPublicKey {
+		return "", errors.New("crytptohome binary always includes signed public key")
+	}
+	return utility.proxy.TPMAttestationEnterpriseVaChallenge(
+		VAType,
+		username,
+		label,
+		domain,
+		deviceID,
+		challenge)
+}
+
+// SignSimpleChallenge implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) SignSimpleChallenge(
+	username string,
+	label string,
+	challenge []byte) (string, error) {
+	return utility.proxy.TPMAttestationSimpleChallenge(
+		username,
+		label,
+		challenge)
+}
+
+// GetPublicKey implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) GetPublicKey(
+	username string,
+	label string) (string, error) {
+	result, _, err := utility.getKeyStatus(username, label)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get key status")
+	}
+	return result, nil
+}
+
+func (utility utilityCryptohomeBinary) getKeyStatus(
+	username string,
+	label string) (string, string, error) {
+	out, err := utility.proxy.TPMAttestationKeyStatus(
+		username,
+		label)
+	if err != nil {
+		return "", "", errors.Wrap(err, "failed to get key status")
+	}
+	arr := strings.Split(out, "\n")
+	// Sanity check on output format
+	if len(arr) < 5 || arr[0] != "Public Key:" || arr[3] != "Certificate:" {
+		return "", "", errors.New("Ill-formed output string format")
+	}
+	if len(arr[1]) == 0 {
+		return "", "", errors.New("empty public key")
+	}
+	cert := strings.Join(arr[4:], "\n")
+	if len(cert) == 0 {
+		return "", "", errors.New("empty cert")
+	}
+	return arr[1], cert, nil
+}
+
+// IsMounted implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) IsMounted() (bool, error) {
+	out, err := utility.proxy.IsMounted()
+	if err != nil {
+		return false, errors.Wrap(err, "failed to check if mounted")
+	}
+	result, err := strconv.ParseBool(string(out))
+	if err != nil {
+		return false, errors.Wrap(err, "failed to parse output from cryptohome: "+string(out))
+	}
+	return result, nil
+}
+
+// Unmount implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) Unmount(username string) (bool, error) {
+	out, err := utility.proxy.Unmount(username)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to unmount: "+string(out))
+	}
+	return true, nil
+}
+
+// CreateVault implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) CreateVault(username string, password string) (bool, error) {
+	out, err := utility.proxy.MountEx(username, password, true)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to mount: "+string(out))
+	}
+	return true, nil
+}
+
+// RemoveVault implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) RemoveVault(username string) (bool, error) {
+	out, err := utility.proxy.Remove(username)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to remove vault: "+string(out))
+	}
+	return true, nil
+}
+
+// IsTPMWrappedKeySet implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) IsTPMWrappedKeySet(username string) (bool, error) {
+	out, err := utility.proxy.DumpKeyset(username)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to dump keyset: "+string(out))
+	}
+	return strings.Contains(string(out), cryptohomeWrappedKeysetString), nil
+}
+
+// GetEnrollmentID implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) GetEnrollmentID() (string, error) {
+	out, err := utility.proxy.GetEnrollmentID()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get EID: "+string(out))
+	}
+	return strings.TrimSpace(string(out)), nil
+}
+
+// GetOwnerPassword implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) GetOwnerPassword() (string, error) {
+	out, err := utility.proxy.TPMStatus()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get tpm status")
+	}
+	lastLine := func() string {
+		lines := strings.Split(strings.TrimSpace(out), "\n")
+		return lines[len(lines)-1]
+	}()
+	fields := strings.Fields(lastLine)
+	// Output doesn't match our expectation.
+	if len(fields) < 2 || len(fields) > 3 || fields[0] != "TPM" || fields[1] != "Password:" {
+		return "", errors.New("bad form of owner password: " + lastLine)
+	}
+	// Special case when the password is empty.
+	if len(fields) == 2 {
+		return "", nil
+	}
+	return fields[2], nil
+}
+
+// ClearOwnerPassword implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) ClearOwnerPassword() error {
+	out, err := utility.proxy.TPMClearStoredPassword()
+	if err != nil {
+		return errors.Wrap(err, string(out))
+	}
+	return nil
+}
+
+// GetKeyPayload implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) GetKeyPayload(
+	username string,
+	label string) (string, error) {
+	out, err := utility.proxy.TPMAttestationGetKeyPayload(username, label)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get key payload: "+string(out))
+	}
+	return string(out), nil
+}
+
+// SetKeyPayload implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) SetKeyPayload(
+	username string,
+	label string,
+	payload string) (bool, error) {
+	out, err := utility.proxy.TPMAttestationSetKeyPayload(username, label, payload)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to set key payload: "+string(out))
+	}
+	return true, nil
+}
+
+// RegisterKeyWithChapsToken implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) RegisterKeyWithChapsToken(
+	username string,
+	label string) (bool, error) {
+	out, err := utility.proxy.TPMAttestationRegisterKey(username, label)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to register key: "+string(out))
+	}
+	if strings.Contains(string(out), resultIsSuccessString) {
+		return true, nil
+	}
+	if strings.Contains(string(out), resultIsFailureString) {
+		return false, nil
+	}
+	return false, errors.New("Unexpected output from cryptohome binary")
+}
+
+// SetAttestationAsyncMode implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) SetAttestationAsyncMode(async bool) error {
+	*utility.attestationAsyncMode = async
+	return nil
+}
+
+// DeleteKeys implements Utility interface using CryptohomeBinary; see utility.go for more information.
+func (utility utilityCryptohomeBinary) DeleteKeys(username string, prefix string) error {
+	out, err := utility.proxy.TPMAttestationDelete(username, prefix)
+	if err != nil {
+		return errors.Wrap(err, "failed to delete keys: "+string(out))
+	}
+	return nil
+}
