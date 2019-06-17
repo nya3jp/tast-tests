7456eb6de9d2e147334d069e119adead0c8dc92d
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: attestation flow
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} hwec.Attestation
    
    Change-Id: I412fb59478f97af8f991d95596c9c432d5020f64

--- src/chromiumos/tast/local/bundles/cros/main.go
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -22,7 +22,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/example"
 	_ "chromiumos/tast/local/bundles/cros/firmware"
 	_ "chromiumos/tast/local/bundles/cros/graphics"
-	_ "chromiumos/tast/local/bundles/cros/hardware"
+	//	_ "chromiumos/tast/local/bundles/cros/hardware"
 	_ "chromiumos/tast/local/bundles/cros/hwsec"
 	_ "chromiumos/tast/local/bundles/cros/kernel"
 	_ "chromiumos/tast/local/bundles/cros/meta"
