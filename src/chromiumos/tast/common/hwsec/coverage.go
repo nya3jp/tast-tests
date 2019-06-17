0903e06de84c790e13aafde83b2a5e8feb67e375
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: common library for hwsec testing
    I've heard that a XL CL usually gets  LGTM in no time. However, just to
    be safe let's make it XXL
    
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} "*Attestation*"
    
    Change-Id: I56b3bb467062c111361b64013b7f3132578890c1

---
+++ src/chromiumos/tast/common/hwsec/coverage.go
@@ -0,0 +1,110 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+const (
+	coverageDataDir  = "coverage_data/profraws"
+	coveredBinaryDir = "coverage_data/binaries"
+)
+
+var (
+	daemonNamesForCoverage = []string{"tpm_managerd", "attestationd", "cryptohomed"}
+	binaryNamesForCoverage = []string{"tpm_managerd", "attestationd", "cryptohomed", "local_data_migration"}
+)
+
+func flushCoverageData(ctx context.Context) error {
+	if !isRemoteTest(ctx) {
+		return errors.New("reboot operation only supported in remote test")
+	}
+	os.MkdirAll(coverageDataDir, 0777)
+	os.MkdirAll(coveredBinaryDir, 0777)
+	d, ok := dut.FromContext(ctx)
+	if !ok {
+		errors.New("Failed to get DUT")
+	}
+	for _, daemon := range daemonNamesForCoverage {
+		cmd := "restart " + daemon
+		if _, err := d.Run(ctx, cmd); err != nil {
+			testing.ContextLog(ctx, "Failed to restart "+cmd)
+			continue
+		}
+	}
+	if err := collectProfraws(ctx); err != nil {
+		return errors.Wrap(err, "failed to collect profraw files")
+	}
+	return nil
+}
+
+func collectProfraws(ctx context.Context) error {
+	if !isRemoteTest(ctx) {
+		return errors.New("reboot operation only supported in remote test")
+	}
+	d, ok := dut.FromContext(ctx)
+	if !ok {
+		errors.New("Failed to get DUT")
+	}
+	cmd := "find /tmp -path /tmp*profraw"
+	result, err := d.Run(ctx, cmd)
+	if err != nil {
+		return errors.Wrap(err, "Failed to check if any profraws in /tmp")
+	}
+	hasCollected := false
+	for _, filepath := range strings.Fields(string(result)) {
+		cmd := "cat " + string(filepath)
+		testing.ContextLog(ctx, "Running "+cmd)
+		result, err := d.Run(ctx, cmd)
+		// Don't stop just because of failure of a transfer
+		if err != nil {
+			testing.ContextLog(ctx, "Failed to run "+cmd)
+			continue
+		}
+		// Writes the content to the same file name
+		// Skip "/tmp/"
+		outputFilename := filepath[5:]
+		outputFilepath := coverageDataDir + "/" + string(outputFilename)
+		if err := ioutil.WriteFile(outputFilepath, result, 0644); err != nil {
+			testing.ContextLog(ctx, "Failed to store to "+outputFilepath)
+		} else {
+			hasCollected = true
+		}
+	}
+	// If any profile is collected, also fectches the binaries of interest.
+	if !hasCollected {
+		return nil
+	}
+	for _, binaryName := range binaryNamesForCoverage {
+		// Gets the binary path.
+		cmd := "which " + binaryName
+		binaryPath, err := d.Run(ctx, cmd)
+		if err != nil {
+			testing.ContextLog(ctx, "Failed to run "+cmd)
+			continue
+		}
+		cmd = "cat " + string(binaryPath)
+		content, err := d.Run(ctx, cmd)
+		if err != nil {
+			testing.ContextLog(ctx, "Failed to run "+cmd)
+			continue
+		}
+		// Writes the content to the same file name
+		outputFilepath := coveredBinaryDir + "/" + binaryName
+		if err := ioutil.WriteFile(outputFilepath, content, 0755); err != nil {
+			testing.ContextLog(ctx, "Failed to store to "+outputFilepath)
+		}
+	}
+
+	return nil
+}
