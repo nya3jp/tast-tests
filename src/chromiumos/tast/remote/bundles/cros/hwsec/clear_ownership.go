a07e69d1512bf6b5e4cdd03b8519efcc80e8800e
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: ClearOwnership
    This is the test entry that just clears the TPM followed by simple
    verification of TPM reset.
    
    It is the subset of 'RetakeOwnership`, and it is meant to be used by
    developers to verify the functionality or as a push-button solution of
    TPM reset.
    
    BUG=b:132657813
    TEST=tast run ${DUT_IP} hwsec.ClearOwnership
    
    Change-Id: Ifaccb989ac996a5a5b79ca4db8dd517a9ccc5a00

---
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/clear_ownership.go
@@ -0,0 +1,53 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	hwsecremote "chromiumos/tast/remote/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ClearOwnership,
+		Desc:         "Verifies that the TPM ownership can be cleared",
+		Contacts:     []string{"cylai@google.com"},
+		SoftwareDeps: []string{"reboot", "tmp"},
+		Attr:         []string{"disabled"},
+	})
+}
+
+func ClearOwnership(ctx context.Context, s *testing.State) {
+	r, err := hwsecremote.NewCmdRunner(s.DUT())
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Utilty creation error: ", err)
+	}
+	helper, err := hwsecremote.NewHelper(utility, r, s.DUT())
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+	s.Log("Start resetting TPM if needed")
+	if err := helper.EnsureTPMIsReset(ctx); err != nil {
+		s.Fatal("Failed to ensure resetting TPM: ", err)
+	}
+	s.Log("TPM is confirmed to be reset")
+
+	if result, err := utility.IsPreparedForEnrollment(ctx); err != nil {
+		s.Fatal("Cannot check if enrollment preparation is reset")
+	} else if result {
+		s.Fatal("Enrollment preparation is not reset after clearing ownership")
+	}
+	s.Log("Enrolling with TPM not ready")
+	if _, err := utility.CreateEnrollRequest(ctx, hwsec.DefaultPCA); err == nil {
+		s.Fatal("Enrollment should not happen w/o getting prepared")
+	}
+}
