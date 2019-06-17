154338dd45393e3925f4170eade186158a5c3906
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: attestation eid sanity check
    BUG=b:132657813
    TEST=tast run ${DUT_IP} hwsec.AttestationEID
    
    Change-Id: I550c021affbea8efc21919e3bc8213db52093726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2073304
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Wei-Cheng Xiao <garryxiao@chromium.org>
    Tested-by: Leo Lai <cylai@google.com>
    Commit-Queue: Leo Lai <cylai@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/attestation_eid.go
@@ -0,0 +1,48 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	hwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AttestationEID,
+		Desc:         "Verifies attestation-related functionality",
+		Attr:         []string{"group:mainline", "informational"},
+		Contacts:     []string{"cylai@chromium.org", "cros-hwsec@google.com"},
+		SoftwareDeps: []string{"tpm"},
+	})
+}
+
+func AttestationEID(ctx context.Context, s *testing.State) {
+	r, err := hwseclocal.NewCmdRunner()
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Utility creation error: ", err)
+	}
+
+	enc, err := utility.GetEnrollmentID(ctx)
+	if err != nil {
+		s.Fatal("Failed to get enrollment id: ", err)
+	}
+	dec, err := hwsec.HexDecode([]byte(enc))
+	if err != nil {
+		s.Fatal("Failed to decode eid: ", err)
+	}
+	// SHA-256 digest size in bytes.
+	if len(dec) != 32 {
+		// Prints hex-encoded data because the decoded result might not be readable.
+		s.Fatal("Expected size of EID in 32 bytes; hex-encoded EID: ", enc)
+	}
+}
