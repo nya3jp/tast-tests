979c3cb660b88191440b50408a83c4458597ae41
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: CryptohomeBinary
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} "FILTER_OUT_EVERYTHING"
    
    Change-Id: I890764ed38105addb41fc071508e1f8d128dc822

--- src/chromiumos/tast/common/hwsec/const.go
+++ src/chromiumos/tast/common/hwsec/const.go
@@ -8,6 +8,15 @@ import (
 	"time"
 )
 
+// VAType indicates the type VA server, of which the possible value are default and test; see the const definition below.
+type VAType int
+
+// ACAType indicates the type ACA server, of which the possible value are default and test; see the const definition below.
+type ACAType int
+
+// PCAType is basically an alias of ACAType from legacy cryptohome's terminology.
+type PCAType ACAType
+
 const (
 	// PollingInterval is the polling interval we use in this library and the libraries extending this.
 	PollingInterval = 100 * time.Millisecond
@@ -19,4 +28,23 @@ const (
 	AttestationDBPath = "/mnt/stateful_partition/unencrypted/preserve/attestation.epb"
 	// TpmManagerLocalDataPath is the path of tpm_manager local data (only applicable for distributed model).
 	TpmManagerLocalDataPath = "/var/lib/tpm_manager/local_tpm_data"
+
+	// DefaultACA indicates the default ACA server.
+	DefaultACA ACAType = 0
+	// TestACA indicates the test ACA server.
+	TestACA ACAType = 1
+	// DefaultACAType is the default ACAType value we use in the testing script.
+	DefaultACAType ACAType = DefaultACA
+
+	// DefaultPCA indicates the default PCA server.
+	DefaultPCA PCAType = 0
+	// TestPCA indicates the test PCA server.
+	TestPCA PCAType = 1
+	// DefaultPCAType is the default PCAType value we use in the testing script.
+	DefaultPCAType PCAType = DefaultPCA
+
+	// DefaultVA indicates the default VA server.
+	DefaultVA VAType = 0
+	// TestVA indicates the test VA server.
+	TestVA VAType = 1
 )
