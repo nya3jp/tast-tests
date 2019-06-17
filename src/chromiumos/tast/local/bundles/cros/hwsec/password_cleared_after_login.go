1bdd74693a69d057a000b902b7a02fd58579b2e6
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: everything else
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} hwec.*
    
    Change-Id: I201797fec1315d5a21ec97979d35003b51751ef9

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/password_cleared_after_login.go
@@ -0,0 +1,73 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	libhwsec "chromiumos/tast/common/hwsec"
+	"chromiumos/tast/local/chrome"
+	libhwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PasswordClearedAfterLogin,
+		Desc: "Verifies synchronous attestation APIs",
+		Attr: []string{"informational"},
+		Contacts: []string{
+			"cylai@chromium.org", // Nobody
+		},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func PasswordClearedAfterLogin(ctx context.Context, s *testing.State) {
+	s.Log("Start test with creating a helper and a utility")
+	helper, err := libhwseclocal.NewHelperLocal()
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+	utility, err := libhwsec.NewUtility(ctx, helper, libhwsec.CryptohomeBinaryType)
+
+	s.Log("Switching to use of synchronous attestaton APIs")
+	if err := utility.SetAttestationAsyncMode(false); err != nil {
+		s.Fatal("Failed to switch to sync mode")
+	}
+
+	// This pattern is so bad...smh. Need to find a better way to do the switch
+	if err != nil {
+		s.Error("Utilty creation error: ", err)
+		return
+	}
+	if err := libhwsec.EnsureTpmIsReady(ctx, utility, defaultTakingOwnershipTimeout); err != nil {
+		s.Error("Failed to ensure tpm readiness: ", err)
+		return
+	}
+	s.Log("Tpm is ensured to be ready")
+	if err := libhwsec.EnsureIsPreparedForEnrollment(ctx,
+		utility, defaultPreparationForEnrolmentTimeout); err != nil {
+		s.Error("Failed to prepare for enrollment: ", err)
+		return
+	}
+	auth := chrome.Auth("test@crashwsec.bigr.name", "testpass", "gaia-id")
+	cr, err := chrome.New(ctx, auth)
+	if err != nil {
+		s.Fatal("Failed to log in by Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+	username := cr.User()
+	s.Log("Chrome user: ", username)
+
+	s.Log("Checking if onwer password is gone...")
+	//Needed when using legacy cryptohome interface.
+	libhwsec.RestartCryptohome(ctx, helper)
+	if ownerPassword, err := utility.GetOwnerPassword(); err != nil {
+		s.Fatal("Failed to get owner password: ", err)
+	} else if ownerPassword != "" {
+		s.Fatal("Still have owner password: ", ownerPassword)
+	}
+}
---
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/login_during_taking_ownership.go
@@ -0,0 +1,100 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	libhwsec "chromiumos/tast/common/hwsec"
+	"chromiumos/tast/errors"
+	libhwsecremote "chromiumos/tast/remote/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         LoginDuringTakingOwnership,
+		Desc:         "Verifies that the TPM ownership can be cleared and taken",
+		Contacts:     []string{"cylai@google.com"},
+		SoftwareDeps: []string{"reboot"},
+	})
+}
+
+func LoginDuringTakingOwnership(ctx context.Context, s *testing.State) {
+
+	s.Log("Start test with creating a helper and a utility")
+	helper, err := libhwsecremote.NewHelperRemote(s.DUT())
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+	utility, err := libhwsec.NewUtility(ctx, helper, libhwsec.CryptohomeBinaryType)
+	if err != nil {
+		s.Fatal("Utilty creation error: ", err)
+	}
+
+	loginFunc := func(username, passwd string) error {
+		s.Log("Start creating vault")
+		result, err := utility.CreateVault(username, passwd)
+		if err != nil {
+			return errors.Wrap(err, "error during create vault w/o tpm ownership")
+		} else if !result {
+			return errors.New("Failed to create vault w/o tpm ownership")
+		}
+		s.Log("Checking if the vault is TPM-backed")
+		result, err = utility.IsTpmWrappedKeySet(username)
+		if err != nil {
+			return errors.Wrap(err, "error checking if vault key set is tpm wrapped")
+		}
+		if result {
+			return errors.New("vault key set is tpm wrapped what~~~~~")
+		}
+		s.Log("Finished creating non-TPM-backed vault")
+		return nil
+	}
+	loginRoutine := func(username, passwd string, err chan error) {
+		err <- loginFunc(username, passwd)
+	}
+	takeOwnershipFunc := func() error {
+		s.Log("Start taking ownership")
+		if err := libhwsec.EnsureTpmIsReady(ctx, utility, defaultTakingOwnershipTimeout); err != nil {
+			return errors.Wrap(err, "Failed to ensure ownership: ")
+		}
+		s.Log("Ownership is taken")
+		return nil
+	}
+	takeOwnershipRoutine := func(err chan error) {
+		err <- takeOwnershipFunc()
+	}
+
+	s.Log("Start resetting TPM if needed")
+	if err := libhwsec.EnsureTpmIsReset(ctx, helper, utility); err != nil {
+		s.Fatal("Failed to ensure resetting TPM: ", err)
+	}
+	s.Log("TPM is confirmed to be reset")
+	loginErr := make(chan error)
+	ownershipErr := make(chan error)
+	username := "unowned@gmail.com"
+	go loginRoutine(username, "passwd", loginErr)
+	go takeOwnershipRoutine(ownershipErr)
+	err1, err2 := <-loginErr, <-ownershipErr
+	if err1 != nil || err2 != nil {
+		s.Fatal("concurrenty error: ", err1, err2)
+	}
+	result, err := utility.Unmount(username)
+	if err != nil {
+		s.Fatal("Error unmounting user: ", err)
+	}
+	if !result {
+		s.Fatal("Failed to unmount user: ", err)
+	}
+	result, err = utility.RemoveVault(username)
+	if err != nil {
+		s.Fatal("Error removing vault: ", err)
+	}
+	if !result {
+		s.Fatal("Failed to remove vault: ", err)
+	}
+
+}
