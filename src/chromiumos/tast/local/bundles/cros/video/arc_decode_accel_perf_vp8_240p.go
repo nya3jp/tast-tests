e99a4d994dcb24f495dfc441e58d8b6427f40d3a
Pin-chih Lin <johnylin@google.com>
2019-06-17 22:18:04 +0800

    tast-tests: add ARCDecodeAccelPerf* tests
    ARCDecodeAccelPerf* tests TestFPS of calculating decoder FPS and
    saves as perf metrics.
    
    BUG=b:112677333
    TEST=tast run <DUT_IP> video.ARCDecodeAccelPerf*
    
    Change-Id: Icc66be0bacf8cc6115ed69a8f101a98e010d1ef4

---
+++ src/chromiumos/tast/local/bundles/cros/video/arc_decode_accel_perf_vp8_240p.go
@@ -0,0 +1,30 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/bundles/cros/video/lib/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ARCDecodeAccelPerfVP8240P,
+		Desc:         "Runs arcvideodecoder_test on ARC++ to measure the performance with an 240p VP8 video test-25fps.vp8",
+		Contacts:     []string{"johnylin@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"android", "chrome", caps.HWDecodeVP8},
+		Data:         []string{decode.Test25FPSVP8.Name, decode.Test25FPSVP8.Name + ".json"},
+		Pre:          arc.Booted(),
+	})
+}
+
+func ARCDecodeAccelPerfVP8240P(ctx context.Context, s *testing.State) {
+	decode.RunARCVideoPerfTest(ctx, s, decode.Test25FPSVP8.Name)
+}
