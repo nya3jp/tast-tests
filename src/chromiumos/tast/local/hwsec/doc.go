2ac99f48cb7c6c9b85c465f8fa6571282456ae30
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: create hwsec library package
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} "*Attestation*" along with CLs
    that follows
    
    Change-Id: Id583aea927397a096589fed633a4df71a04cbce8

---
+++ src/chromiumos/tast/local/hwsec/doc.go
@@ -0,0 +1,13 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.  Use of this
+// source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+
+package hwsec
+
+/*
+Package hwsec implements the libraries and utiltiies which are used for
+tast-test package with the same name 'hwsec'. The following files are meant to
+provide the public interfaces which are called by the top-level tast-test
+functions.
+
+*/
