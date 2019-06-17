97f8f168e18260969ca0ccd08e29023fbcfded18
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: retake ownership
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} hwsec.RetakeOwnership
    
    Change-Id: I0818eb1df6dd46bc48465f0db74da51ff2a29223

---
+++ src/chromiumos/tast/common/hwsec/const.go
@@ -0,0 +1,10 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+const (
+	// DefaultTakingOwnershipTimeout is the default timeout while taking TPM ownership.
+	DefaultTakingOwnershipTimeout int = 40 * 1000
+)
