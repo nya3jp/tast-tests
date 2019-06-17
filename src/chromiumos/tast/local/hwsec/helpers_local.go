17afb2d4365603231218440e5dc22faede36ea04
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: helpers, and daemon control logic
    BUG=b:132657813
    TEST=tast run ${DUT_IP} "FILTER_OUT_EVERYTHING"
    
    Cq-Depend: chromium:1887010
    Change-Id: I6b84fbeb719ae1c7ce874e0aefb1ab342e4e9241

---
+++ src/chromiumos/tast/local/hwsec/helpers_local.go
@@ -0,0 +1,35 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+/*
+This file implements miscellaneous and unsorted helpers.
+*/
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+// CmdRunnerLocal implements hwsec.CmdRunner in remote test.
+type CmdRunnerLocal struct {
+}
+
+// Run implements the one of hwsec.CmdRunner.
+func (r *CmdRunnerLocal) Run(ctx context.Context, cmd string, args ...string) ([]byte, error) {
+	testing.ContextLogf(ctx, "Running: %s", shutil.EscapeSlice(append([]string{cmd}, args...)))
+	return testexec.CommandContext(ctx, cmd, args...).Output()
+}
+
+// NewHelperLocal creates a new hwsec.Helper instance that make use of the functions
+// implemented by CmdRunnerLocal.
+func NewHelperLocal() (*hwsec.Helper, error) {
+	runner := &CmdRunnerLocal{}
+	return &hwsec.Helper{runner}, nil
+}
