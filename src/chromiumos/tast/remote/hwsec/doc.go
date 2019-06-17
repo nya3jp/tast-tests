b71ffe4e27ba3ba6f46a70ddd8ad3926c4d00c7a
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: helpers, and daemon control logic
    BUG=b:132657813
    TEST=tast run ${DUT_IP} "FILTER_OUT_EVERYTHING"
    
    Cq-Depend: chromium:1887010
    Change-Id: I6b84fbeb719ae1c7ce874e0aefb1ab342e4e9241

---
+++ src/chromiumos/tast/remote/hwsec/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package hwsec implements the libraries and utilities which are used for
+// remote bundles with the same name 'hwsec'.
+package hwsec
