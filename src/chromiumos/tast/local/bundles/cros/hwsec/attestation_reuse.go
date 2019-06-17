a861f30495efc170fa9a79dce479ab8cd7200c90
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: everything else
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} hwec.*
    
    Change-Id: I201797fec1315d5a21ec97979d35003b51751ef9

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/attestation_reuse.go
@@ -0,0 +1,135 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	libhwseclocal "chromiumos/tast/local/hwsec"
+	a9n "chromiumos/tast/local/hwsec/attestation"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AttestationReuse,
+		Desc: "Verifies certified system key",
+		Attr: []string{"informational"},
+		Contacts: []string{
+			"cylai@chromium.org", // Nobody
+		},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func AttestationReuse(ctx context.Context, s *testing.State) {
+	r, err := libhwseclocal.NewCmdRunner()
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Utilty creation error: ", err)
+	}
+	helper, err := libhwseclocal.NewHelper(utility)
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+	if err := helper.EnsureTPMIsReady(ctx, hwsec.DefaultTakingOwnershipTimeout); err != nil {
+		s.Error("Failed to ensure tpm readiness: ", err)
+		return
+	}
+	s.Log("TPM is ensured to be ready")
+	if err := helper.EnsureIsPreparedForEnrollment(ctx, hwsec.DefaultPreparationForEnrolmentTimeout); err != nil {
+		s.Error("Failed to prepare for enrollment: ", err)
+		return
+	}
+	s.Log("Attestation is prepared for enrollment")
+	// For now, just uses '0' as in "default CA"
+	req, err := utility.CreateEnrollRequest(ctx, a9n.DefaultACAType)
+	s.Log(len(req))
+	if err != nil {
+		s.Error("Failed to create enroll request: ", err)
+		return
+	}
+	//resp, err := a9n.SendPostRequestTo(req, "https://asbestos-qa.corp.google.com/enroll")
+	resp, err := a9n.SendPostRequestTo(req, "https://chromeos-ca.gstatic.com/enroll")
+	if err != nil {
+		s.Error("Failed to send request to CA: ", err)
+	}
+	// For now, just uses '0' as in "default CA"
+	err = utility.FinishEnroll(ctx, a9n.DefaultACAType, resp)
+	if err != nil {
+		s.Error("Failed to finish enrollment: ", err)
+	}
+	isEnrolled, err := utility.IsEnrolled(ctx)
+	if err != nil {
+		s.Error("Failed to get enrollment status: ", err)
+	}
+	if !isEnrolled {
+		s.Error("Inconsistent reported status: after enrollment, status shows 'not enrolled'")
+	}
+	s.Log("The DUT is enrolled")
+
+	err = utility.FinishEnroll(ctx, a9n.DefaultACAType, resp)
+	if err != nil {
+		s.Error("Failed to finish enrollment: ", err)
+	}
+	isEnrolled, err = utility.IsEnrolled(ctx)
+	if err != nil {
+		s.Error("Failed to get enrollment status: ", err)
+	}
+	if !isEnrolled {
+		s.Error("Inconsistent reported status: after enrollment, status shows 'not enrolled'")
+	}
+
+	s.Log("Using the same repsonse to enroll again")
+
+	s.Log("Creating ceritificate request")
+
+	// Empty username indicates the system-level key.
+	username := ""
+
+	req, err = utility.CreateCertRequest(
+		ctx,
+		a9n.DefaultACAType,
+		a9n.DefaultCertProfile,
+		username,
+		a9n.DefaultCertOrigin)
+	if err != nil {
+		s.Error("Failed to create certificate request: ", err)
+	}
+	s.Log("Created certificate request")
+
+	s.Log("Sending sign request")
+	resp, err = a9n.SendPostRequestTo(req, "https://chromeos-ca.gstatic.com/sign")
+	//resp, err = a9n.SendPostRequestTo(req, "https://asbestos-qa.corp.google.com/sign")
+	if err != nil {
+		s.Error("Failed to send request to CA: ", err)
+	}
+	if len(resp) == 0 {
+		s.Error("Unexpected empty cert")
+		return
+	}
+
+	s.Log("Finishing certificate request")
+	err = utility.FinishCertRequest(ctx, resp, username, a9n.DefaultCertLabel)
+
+	if err != nil {
+		s.Error("Failed to finish cert request: ", err)
+		return
+	}
+	s.Log("Finished certificate request")
+
+	s.Log("Checking if FinishCert process can be replayed")
+	err = utility.FinishCertRequest(ctx, resp, username, a9n.DefaultCertLabel)
+
+	if err == nil {
+		s.Error("FinishCert okay w/ the same response: ", err)
+		return
+	}
+	s.Log("Expected error for the reuse of the certificate response from server: ", err)
+}
