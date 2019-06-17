0026c73ff954fe59fb0e437c5e741e74a98d5b0a
Leo Lai <cylai@google.com>
2019-06-17 22:25:53 +0800

    tast-tests: hwsec: common library folder for hwsec testing
    This CL starts a new folder for common library for hwsec testing.
    
    BUG=b:132657813
    TEST=tast run -buildtype=local ${DUT_IP} "FILTER_OUT_EVERYTHING"
    
    Change-Id: I90166a6749bad606dbc5e778456d9561e8098249

---
+++ src/chromiumos/tast/common/hwsec/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+/*
+Package hwsec implements the libraries and utiltiies which are used for
+both local and remote bundles with the same name 'hwsec'.
+*/
