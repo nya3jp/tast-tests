3af36ea9ad42582cfb023216f591ddac1e8f7487
Ricardo Quesada <ricardoq@google.com>
2019-06-18 15:49:07 -0700

    tast-test: WM Default launch state for clamshell test for SDK23
    This CL verifies that the Window Manager default launch state is the
    expected one, as defined in: go/arc-wm-p "Clamshell: default launch
    behavior - Android MNC or below" (slide #7)
    
    (CL #2 of many)
    
    BUG=chromium:972897
    TEST=tast -verbose run $DUT arc.WindowManagerCUJ
    
    Change-Id: I16af9b940e53b96678293bb3ed553c01eef0f990

--- src/chromiumos/tast/local/bundles/cros/arc/window_manager_cuj.go
+++ src/chromiumos/tast/local/bundles/cros/arc/window_manager_cuj.go
@@ -21,6 +21,8 @@ import (
 )
 
 const (
+	// Apk compiled against target SDK 23 (Pre-N)
+	wmPkg23 = "org.chromium.arc.testapp.windowmanager23"
 	// Apk compiled against target SDK 24 (N)
 	wmPkg24 = "org.chromium.arc.testapp.windowmanager24"
 
@@ -31,6 +33,9 @@ const (
 	wmNonResizeableUnspecifiedActivity = "org.chromium.arc.testapp.windowmanager.NonResizeableUnspecifiedActivity"
 	wmResizeablePortraitActivity       = "org.chromium.arc.testapp.windowmanager.ResizeablePortraitActivity"
 	wmNonResizeablePortraitActivity    = "org.chromium.arc.testapp.windowmanager.NonResizeablePortraitActivity"
+	wmLandscapeActivity                = "org.chromium.arc.testapp.windowmanager.LandscapeActivity"
+	wmUnspecifiedActivity              = "org.chromium.arc.testapp.windowmanager.UnspecifiedActivity"
+	wmPortraitActivity                 = "org.chromium.arc.testapp.windowmanager.PortraitActivity"
 
 	// These values must match the strings from ArcWMTestApp defined in BaseActivity#parseCaptionButtons:
 	// http://cs/android/vendor/google_arc/packages/development/ArcWMTestApp/src/org/chromium/arc/testapp/windowmanager/BaseActivity.java?l=448
@@ -48,14 +53,17 @@ func init() {
 		Contacts:     []string{"ricardoq@chromium.org", "arc-framework@google.com"},
 		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"android_p", "chrome"},
-		Data:         []string{"ArcWMTestApp_24.apk"},
+		Data:         []string{"ArcWMTestApp_23.apk", "ArcWMTestApp_24.apk"},
 		Pre:          arc.Booted(),
 		Timeout:      5 * time.Minute,
 	})
 }
 
 func WindowManagerCUJ(ctx context.Context, s *testing.State) {
-	const apk24 = "ArcWMTestApp_24.apk"
+	const (
+		apk23 = "ArcWMTestApp_23.apk"
+		apk24 = "ArcWMTestApp_24.apk"
+	)
 
 	cr := s.PreValue().(arc.PreData).Chrome
 	a := s.PreValue().(arc.PreData).ARC
@@ -74,15 +82,26 @@ func WindowManagerCUJ(ctx context.Context, s *testing.State) {
 	if err := a.Install(ctx, s.DataPath(apk24)); err != nil {
 		s.Fatal("Failed installing app: ", err)
 	}
+	if err := a.Install(ctx, s.DataPath(apk23)); err != nil {
+		s.Fatal("Failed installing app: ", err)
+	}
 
 	type testFunc func(context.Context, *chrome.Conn, *arc.ARC, *ui.Device) error
 	for idx, test := range []struct {
 		name string
 		fn   testFunc
 	}{
+		// Ordered as they appear on go/arc-wm-p
 		{"Default Launch Clamshell SDK24", wmDefaultLaunchClamshell24},
+		{"Default Launch Clamshell SDK23", wmDefaultLaunchClamshell23},
 	} {
 		s.Logf("Running test %q", test.name)
+
+		// Reset WM state to default values.
+		if err := a.Command(ctx, "am", "broadcast", "-a", "android.intent.action.arc.cleartaskstate").Run(); err != nil {
+			s.Fatal("Failed to clear task states: ", err)
+		}
+
 		if err := test.fn(ctx, tconn, a, d); err != nil {
 			path := fmt.Sprintf("%s/screenshot-cuj-failed-test-%d.png", s.OutDir(), idx)
 			if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
@@ -112,10 +131,6 @@ func wmDefaultLaunchClamshell24(ctx context.Context, tconn *chrome.Conn, a *arc.
 		return errors.Wrap(err, "could not set tablet mode disabled")
 	}
 
-	// Reset WM state to default values.
-	if err := a.Command(ctx, "am", "broadcast", "-a", "android.intent.action.arc.cleartaskstate").Run(); err != nil {
-		return errors.Wrap(err, "could not clear task states")
-	}
 	for _, test := range []struct {
 		name          string
 		act           string
@@ -139,6 +154,77 @@ func wmDefaultLaunchClamshell24(ctx context.Context, tconn *chrome.Conn, a *arc.
 	} {
 		testing.ContextLogf(ctx, "Running subtest %q", test.name)
 		act, err := arc.NewActivity(a, wmPkg24, test.act)
+		if err != nil {
+			return err
+		}
+		defer act.Close()
+
+		if err := act.Start(ctx); err != nil {
+			return err
+		}
+		// Stop activity at exit time so that the next WM test can launch a different activity from the same package.
+		defer act.Stop(ctx)
+
+		if err := act.WaitForIdle(ctx, 10*time.Second); err != nil {
+			return err
+		}
+
+		state, err := act.GetWindowState(ctx)
+		if err != nil {
+			return err
+		}
+		if state != test.wantedState {
+			return errors.Errorf("invalid window state %v, want %v", state, test.wantedState)
+		}
+
+		bn, err := wmCaptionButtons(ctx, d)
+		if err != nil {
+			return errors.Wrap(err, "could not get caption buttons state")
+		}
+		if !reflect.DeepEqual(bn, test.wantedCaption) {
+			return errors.Errorf("invalid caption buttons %+v, want %+v", bn, test.wantedCaption)
+		}
+		// Stopping current activity in order to make it possible to launch a different from the same package.
+		if err := act.Stop(ctx); err != nil {
+			return errors.Wrapf(err, "could not stop activity %v", test.act)
+		}
+	}
+	return nil
+}
+
+// wmDefaultLaunchClamshell23 launches in clamshell mode six SDK-pre-N activities with different orientations
+// and resize conditions. And it verifies that their default launch state is the expected one, as defined in:
+// go/arc-wm-p "Clamshell: default launch behavior - Android MNC and below" (slide #7).
+func wmDefaultLaunchClamshell23(ctx context.Context, tconn *chrome.Conn, a *arc.ARC, d *ui.Device) error {
+	// Expected caption buttons.
+	wmCaptionBMMC := []string{wmBack, wmMinimize, wmMaximize, wmClose}
+
+	tabletModeEnabled, err := ash.TabletModeEnabled(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "could not get tablet mode")
+	}
+	// Be nice and restore tablet mode to its original state on exit.
+	defer ash.SetTabletModeEnabled(ctx, tconn, tabletModeEnabled)
+	if err := ash.SetTabletModeEnabled(ctx, tconn, false); err != nil {
+		return errors.Wrap(err, "could not set tablet mode disabled")
+	}
+
+	for _, test := range []struct {
+		name          string
+		act           string
+		wantedState   arc.WindowState
+		wantedCaption []string
+	}{
+		// The are two possible default states (windows #A to #B) from three possible different activities.
+		// Window #A.
+		{"Unspecified", wmUnspecifiedActivity, arc.WindowStateNormal, wmCaptionBMMC},
+		// Window #A.
+		{"Portrait", wmPortraitActivity, arc.WindowStateNormal, wmCaptionBMMC},
+		// Window #B.
+		{"Landscape", wmLandscapeActivity, arc.WindowStateNormal, wmCaptionBMMC},
+	} {
+		testing.ContextLogf(ctx, "Running subtest %q", test.name)
+		act, err := arc.NewActivity(a, wmPkg23, test.act)
 		if err != nil {
 			return err
 		}
