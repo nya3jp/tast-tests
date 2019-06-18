5e9702c345433c9011bbb36f0a5bb5c5b645b5a3
Billy Zhao <billyzhao@google.com>
2019-06-18 16:31:50 -0700

    Add ping_runner
    Change-Id: Icabae376bfd15a3eb1cf5f3512d65c6ef57bb8eb

---
+++ src/chromiumos/tast/local/bundles/cros/network/ping/ping_runner.go
@@ -0,0 +1,151 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package ping contains utility functions to wrap around the ping program.
+package ping
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+type pingConfig struct {
+	TargetIP    string
+	Count       int
+	Size        int
+	Interval    float64
+	QOS         string
+	SourceIface string
+}
+
+type pingResult struct {
+	Sent	int
+	Received 	int
+	Loss		float64
+	MinLatency float64
+	AvgLatency float64
+	MaxLatency float64
+	DevLatency float64
+}
+
+type runner struct {
+	PingCommand   string
+	LossThreshold int
+	Config        pingConfig
+	s             *testing.State
+	ctx           context.Context
+}
+
+func NewRunner(contxt context.Context, state *testing.State, cfg pingConfig) runner {
+	return runner{PingCommand: "ping",
+		LossThreshold: 20,
+		Config:        cfg,
+		s:             state,
+		ctx:           contxt}
+}
+
+func NewPingConfig(ip string) {
+	return pingConfig{TargetIP: ip,
+	Count : 10,
+	Size: 0,
+	Interval: 0
+	QOS: ""
+	SourceIface :""}
+}
+
+func NewPingResult(snt int, rcv int, loss rcv) pingResult{
+	return pingResult{Sent: snt, Received: rcv, Loss: loss} 
+}
+
+func NewFullPingResult(snt int, rcv int, loss int, minL float64, avgL float64, maxL float64, devL float64) {
+	return pingResult{
+		Sent: snt,
+		Received: rcv,
+		Loss: loss,
+		MinLatency: minL,
+		AvgLatency: avgL,
+		MaxLatency: maxL,
+		DevLatency: devL
+	}
+}
+
+func (pr runner) cfgToArgs() (string, error) {
+	args := []string	
+	args = append(args, fmt.Sprintf("-c %d",pr.Config.Count))
+	if pr.Config.Size != 0 {
+		args = append(args, fmt.Sprintf("-s %d",pr.Config.Size))
+	}
+	if pr.Config.Interval != 0 {
+		args = append(args, fmt.Sprintf("-i %f",pr.Config.Interval))
+	}
+	if pr.Config.Interval != 0 {
+		args = append(args, fmt.Sprintf("-i %f",pr.Config.Interval))
+	}
+	if pr.Config.SourceIface != "" {
+		args = append(args, fmt.Sprintf("-i %s",pr.Config.SourceIface))
+	}
+	if pr.Config.Interval != 0 {
+		args = append(args, fmt.Sprintf("-i %f",pr.Config.Interval))
+	}
+	if pr.Config.QOS != "" {
+		switch pr.Config.QOS {
+		case "be":
+			args = append(args, "-Q 0x04")
+		case "bk":
+			args = append(args, "-Q 0x02")
+		case "vi":
+			args = append(args, "-Q 0x08")
+		case "vo":
+			args = append(args, "-Q 0x10")
+		default:
+			return nil, errors.New(fmt.Sprintf("Unknown QOS value: %s", pr.Config.QOS))
+		}
+	}
+	args = append(args, pr.Config.TargetIP)
+	return args, nil
+}
+
+func getFloat(str string) float64 {
+	out, _ := strconv.ParseFloat(str, 64)
+	return out
+}
+func (pr runner) parseOutput(out string) (pintResult, error){
+	sentMatch := regexp.MustCompile(`([0-9]+) packets transmitted`)
+	matchGroup := sentMatch.FindStringSubmatch(out)
+	if len(matchGroup) != 2 {
+		return nil, errors.New("Parse error on sent packets")
+	}
+	sent, _ := strconv.Atoi(matchGroup[1])
+
+	rcvMatch := regexp.MustCompile(`([0-9]+) packets received`)
+	matchGroup = rcvMatch.FindStringSubmatch(out)
+	if len(matchGroup) != 2 {
+		return nil, errors.New("Parse error on received packets")
+	}
+	rcv, _ := strconv.Atoi(matchGroup[1])
+
+	lossMatch := regexp.MustCompile(`([0-9]+(\.[0-9]+)?)% packet loss`)
+	matchGroup = lossMatch.FindStringSubmatch(out)
+	if len(matchGroup) != 2 {
+		return nil, errors.New("Parse error on lost packets")
+	}
+	loss, _ := strconv.Atoi(matchGroup[1])
+	latMatch := regexp.MustCompile(`(round-trip|rtt) min[^=]*= ([0-9.]+)/([0-9.]+)/([0-9.]+)/([0-9.]+)`)
+	matchGroup = latMatch.FindStringSubmatch(out)
+	if len(matchGroup) == 1 {
+		return NewPingResult(sent,rcv,loss), nil
+	}
+	if len(matchGroup) != 6 {
+		return nil, errors.New("Parse error on latency statistics")
+	}
+	return NewFullPingResult(sent,rcv,loss, getFloat(matchGroup[2]), getFloat(matchGroup[3]), getFloat(matchGroup[4]),getFloat(matchGroup[5])), nil
+	
+}
