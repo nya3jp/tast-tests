8cb44f7099c884adf5bc36fa3ef440ce16b4d0d6
Po-Hsien Wang <pwang@chromium.org>
2019-06-19 15:14:00 -0700

    vm: Split CreateDefaultContainer into 2 functions
    CreateDefaultContainer created VM and container all at the same
    function. Split it into CreateDefaultContainer and CreateDefaultVM to
    allow more customization.
    
    BUG=b:132905401
    TEST=tast run DUT vm.CrostiniStartBasic
    
    Change-Id: I8bb7d1635be937f80a81158d6924b6f571a0a822

--- src/chromiumos/tast/local/vm/util.go
+++ src/chromiumos/tast/local/vm/util.go
@@ -310,3 +310,20 @@ func findIPv4(ips string) (string, error) {
 	}
 	return "", errors.Errorf("could not find IPv4 address in %q", ips)
 }
+
+// CreateDefaultVMContainer prepares a VM and container with default settings and
+// either the live or staging container versions. The directory dir may be used
+// to store logs on failure. If the container type is Tarball, then artifactPath
+// must be specified with the path to the tarball containing the termina VM
+// and container. Otherwise, artifactPath is ignored.
+func CreateDefaultVMContainer(ctx context.Context, dir, user string, t ContainerType, artifactPath string) (*Container, error) {
+	vmInstance, err := CreateDefaultVM(ctx, dir, user, t, artifactPath)
+	if err != nil {
+		stoperr := vmInstance.Stop(ctx)
+		if stoperr != nil {
+			return nil, errors.Wrap(err, stoperr.Error())
+		}
+		return nil, err
+	}
+	return CreateDefaultContainer(ctx, vmInstance, t, dir)
+}
