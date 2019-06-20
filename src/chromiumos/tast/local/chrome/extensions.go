da4e8a5463e826449c21549394a5b0510ff12feb
Shuhei Takahashi <nya@chromium.org>
2019-06-20 17:43:24 +0900

    chrome: Set the device region explicitly.
    Currently Chrome's UI language is determined by device-specific data
    (VPD): https://chromium.googlesource.com/chromiumos/platform/vpd/
    
    Because of this, Chrome's UI language can be non-English when running
    a test with non-US devices, which can be a source of flakiness.
    
    This change improves the chrome package to explicitly set the region
    by --cros-region flag. Also ui.ForceRegion test is added to make
    sure the UI language is actually set.
    
    BUG=chromium:977090
    TEST=tast run betty ui.ForceRegion
    
    Change-Id: I1dbd2694a55affbbfd6b19d7afc7d08a96a0e897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1669369
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/chrome/extensions.go
+++ src/chromiumos/tast/local/chrome/extensions.go
@@ -84,6 +84,7 @@ func writeTestExtension(dir string) (id string, err error) {
     "audio",
     "autotestPrivate",
     "fontSettings",
+    "i18n",
     "inputMethodPrivate",
     "languageSettingsPrivate",
     "management",
