b1f768c474f09d6a8c4a534d2291f0a94f53f27c
Will Bradley <wbbradley@chromium.org>
2019-06-21 12:49:40 -0600

    tast: exercise cros_healthd when diagnostics is available
    BUG=chromium:969746
    TEST=tast -verbose run -build=true $DUT_IP platform.CrosHealthd
    
    Cq-Depend: chromium:1634616
    Change-Id: I7e01337c59f0ef6eeab9aa1803b96acb6990403c
    Reviewed-on: https://chromium-review.googlesource.com/1671267
    Tested-by: Will Bradley <wbbradley@chromium.org>
    Commit-Ready: Will Bradley <wbbradley@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Kartik Hegde <khegde@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_block_devices.go
@@ -0,0 +1,54 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosHealthdProbeBlockDevices,
+		Desc: "Check that we can probe cros_healthd for various probe data points",
+		Contacts: []string{
+			"wbbradley@google.com",
+			"pmoy@google.com",
+			"khegde@google.com",
+		},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"diagnostics"},
+	})
+}
+
+func CrosHealthdProbeBlockDevices(ctx context.Context, s *testing.State) {
+	// For now we are only testing probe_block_devices because that is all
+	// that's currently implemented.
+	// TODO(wbbradley): narrow interface for testing https://crbug.com/979210
+	b, err := testexec.CommandContext(ctx, "cros_healthd", "--probe_block_devices").Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to run 'cros_healthd --probe_block_devices': ", err)
+	}
+
+	// The theory here is that every board should have at least one
+	// non-removable block device showing up in this list.  The output of
+	// cros_healthd probe commands is just a CSV with a line of headers.
+	lineCount := 0
+	for _, line := range strings.Split(string(b), "\n") {
+		if len(strings.TrimSpace(line)) > 0 {
+			lineCount++
+			if lineCount > 1 {
+				// We found at least 1 row of device information. End the test
+				// immediately. This implies success.
+				return
+			}
+		}
+	}
+
+	s.Fatal("Could not find any rows of device information")
+}
