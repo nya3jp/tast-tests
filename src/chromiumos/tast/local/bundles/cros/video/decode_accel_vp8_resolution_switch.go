3d9c6bec243b49d531a9439c0627f5e564ec2975
David Staessens <dstaessens@chromium.org>
2019-06-21 15:04:15 +0900

    video.DecodeAccelVP*ResolutionSwitch: Add VP8/9 resolution switching Tast tests.
    This CL adds VP8 and VP9 resolution switching video decoder tests to Tast. These
    tests run the new video decoder tests with a resolution switching video stream.
    
    This CL also removes a deprecated TODO message in the H264 resolution switch
    test which was added earlier.
    
    TEST=video.DecodeAccelVP*ResolutionSwitch on eve
    
    BUG=chromium:934065
    
    Change-Id: I5c119e0e46235077ebf5a03d2565823488f5576e

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vp8_resolution_switch.go
@@ -0,0 +1,29 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DecodeAccelVP8ResolutionSwitch,
+		Desc:         "Runs Chrome video_decode_accelerator_tests with an VP8 resolution switching video",
+		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome", caps.HWDecodeVP8},
+		Data:         []string{"resolution_change_500frames.vp8.ivf", "resolution_change_500frames.vp8.ivf.json"},
+	})
+}
+
+// DecodeAccelVP8ResolutionSwitch runs the video_decode_accelerator_tests with resolution_change_500frames.vp8.ivf.
+func DecodeAccelVP8ResolutionSwitch(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoTestNew(ctx, s, "resolution_change_500frames.vp8.ivf", decode.VDA)
+}
