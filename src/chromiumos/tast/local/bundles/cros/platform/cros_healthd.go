0504ca813877a221e899d025ffca2462478e08c7
Will Bradley <wbbradley@chromium.org>
2019-06-21 12:49:40 -0600

    tast: exercise cros_healthd when diagnostics is available
    BUG=chromium:969746
    TEST=tast -verbose run -build=true $DUT_IP platform.CrosHealthd
    
    Cq-Depend: chromium:1634616
    Change-Id: I7e01337c59f0ef6eeab9aa1803b96acb6990403c

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd.go
@@ -0,0 +1,59 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosHealthd,
+		Desc: "Check that we can probe cros_healthd for various probe data points",
+		Contacts: []string{
+			"wbbradley@google.com",
+			"pmoy@google.com",
+			"khegde@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"diagnostics"},
+	})
+}
+
+func calcOutputLines(output string) int {
+	// Look for non-empty lines to count
+	lines := strings.Split(output, "\n")
+	lineCount := 0
+	for _, line := range lines {
+		if len(strings.TrimSpace(line)) > 0 {
+			lineCount++
+		}
+	}
+	return lineCount
+}
+
+func CrosHealthd(ctx context.Context, s *testing.State) {
+	// For now we are only testing probe_block_devices because that is all that's currently implemented.
+	cmd := testexec.CommandContext(ctx, "cros_healthd", "--probe_block_devices")
+	b, err := cmd.Output()
+	if err != nil {
+		cmd.DumpLog(ctx)
+		s.Error("Failed to run cros_healthd --probe_block_devices")
+		return
+	}
+	output := string(b)
+	lineCount := calcOutputLines(output)
+
+	s.Logf("Number of lines of output is %d", lineCount)
+	if lineCount < 2 {
+		s.Error("cros_healthd --probe_block_devices did not find a non-removable block storage device")
+	} else {
+		s.Log("Found non-removable block storage devices")
+		s.Log(output)
+	}
+}
