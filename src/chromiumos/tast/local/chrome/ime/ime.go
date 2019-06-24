e3774587dba1e72134ae4bd9eafaaef094ede088
Keith Lee <keithlee@google.com>
2019-06-24 15:48:16 +1000

    Add a test for some M17N rulebased IME inputs
    Created a basic template to test different IME inputs and
    verify that the outputs match
    
    Change-Id: Iaf7fca4ac8c34e3634f278ec39f4f5f6f7d556aa

---
+++ src/chromiumos/tast/local/chrome/ime/ime.go
@@ -0,0 +1,34 @@
+
+
+// The IME package
+package ime
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/local/chrome"
+)
+
+// SetCurrentInputMethod sets the current input method used by the virtual
+// keyboard.
+// TODO(keithlee) - chrome.autotestPrivate.setWhitelistedPref seems to
+// not be synchronous. The promise returns before the input engine has been set
+// Here, we evaluate promises and get a result
+func SetCurrentInputMethod(ctx context.Context, tconn *chrome.Conn, inputMethod string) error {
+	return tconn.EvalPromise(ctx, fmt.Sprintf(`
+new Promise((resolve, reject) => {
+	chrome.autotestPrivate.setWhitelistedPref(
+		'settings.language.preload_engines', %[1]q, () => {
+			chrome.inputMethodPrivate.setCurrentInputMethod(%[1]q, () => {
+				if (chrome.runtime.lastError) {
+					reject(chrome.runtime.lastError.message);
+				} else {
+					resolve();
+				}
+			});
+		}
+	);
+})
+`, inputMethod), nil)
+}
