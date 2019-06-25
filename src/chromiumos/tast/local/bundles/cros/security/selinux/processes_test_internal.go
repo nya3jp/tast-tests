49dc1ac6a60cfdc3b5856732d7457a818e35145b
Qijiang Fan <fqj@chromium.org>
2019-06-25 16:29:11 +0900

    tast-tests: security.SELinux* to split testcases for cq and informational
    Let security.SELinuxFileSystems and security.SELinuxProcesses be able to
    split every different test case to either important(CQ) or informational.
    
    Add two more failing tests in informational process test to discover
    processes not componentized yet.
    
    BUG=b:114172200
    TEST=tast run security.SELinux*
    
    Change-Id: Iadff3391e35b5ce02ce0ae90b7735aa6b5941528
    Reviewed-on: https://chromium-review.googlesource.com/1675745
    Tested-by: Qijiang Fan <fqj@google.com>
    Commit-Ready: Qijiang Fan <fqj@google.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/security/selinux/processes_test_internal.go
+++ src/chromiumos/tast/local/bundles/cros/security/selinux/processes_test_internal.go
@@ -7,19 +7,42 @@ package selinux
 import (
 	"context"
 	"regexp"
+	"strings"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/testing"
 )
 
-// ProcessesTestInternal runs the test suite for SELinuxProcesses(Experimental)?
-func ProcessesTestInternal(ctx context.Context, s *testing.State) {
+// ProcessTestCaseSelector specifies what kind of test cases will be run.
+type ProcessTestCaseSelector int
+
+const (
+	// Stable to run test cases proven to be stable.
+	Stable ProcessTestCaseSelector = iota
+	// Unstable to run newly introduced test cases or flaky cases.
+	Unstable
+)
+
+// ProcessesTestInternal runs the test suite for SELinuxProcesses(Experimental|Informational)?
+func ProcessesTestInternal(ctx context.Context, s *testing.State, testSelector []ProcessTestCaseSelector) {
 	assertContext := func(processes []Process, expected *regexp.Regexp) {
 		for _, proc := range processes {
 			if !expected.MatchString(proc.SEContext) {
 				s.Errorf("Process %+v has context %q; want %q", proc, proc.SEContext, expected)
 			}
 		}
+	}
+	// noStr returns a regular expression to match all strings that's not equal to s.
+	// Golang regexp doesn't support lookahead, so we can't simply implement not in regexp.
+	// e.g. notStr("abc") => "(([^a].*)?|a([^b].*)?|ab([^c].*)?|abc.+)"
+	notStr := func(s string) string {
+		l := len(s)
+		rt := make([]string, 0)
+		for i := 0; i < l; i++ {
+			rt = append(rt, s[0:i]+"([^"+s[i:i+1]+"].*)?")
+		}
+		rt = append(rt, s+".+")
+		return "(" + strings.Join(rt, "|") + ")"
 	}
 
 	ps, err := GetProcesses()
@@ -30,6 +53,7 @@ func ProcessesTestInternal(ctx context.Context, s *testing.State) {
 	type searchType int
 	const (
 		exe     searchType = iota // absolute executable path
+		notExe                    // not absolute executable path
 		cmdline                   // partial regular expression matched against command line
 	)
 	const (
@@ -37,7 +61,7 @@ func ProcessesTestInternal(ctx context.Context, s *testing.State) {
 		oneProc       = 1
 		twoProcs      = 2
 	)
-	for _, testCase := range []struct {
+	type testCaseType struct {
 		field   searchType // field to search for processes
 		query   string     // search keyword for given field
 		context string     // expected SELinux process context (domain).
@@ -46,43 +70,61 @@ func ProcessesTestInternal(ctx context.Context, s *testing.State) {
 		// the processes will be there. The platform.CheckProcesses test is responsible for checking that processes
 		// are actually running.
 		minProcessCount int
-	}{
-		{cmdline, "/usr/bin/periodic_scheduler", "cros_periodic_scheduler", twoProcs},
-		{cmdline, "/usr/share/cros/init/activate_date.sh", "cros_activate_date", zeroProcs},
-		{exe, "/opt/google/chrome/chrome", "cros_browser", zeroProcs}, // Only when browser exists
-		{exe, "/sbin/auditd", "cros_auditd", oneProc},                 // auditd must be running on SELinux boards
-		{exe, "/sbin/debugd", "cros_debugd", zeroProcs},
-		{exe, "/sbin/init", "cros_init", oneProc},
-		{exe, "/sbin/session_manager", "cros_session_manager", zeroProcs},
-		{exe, "/sbin/udevd", "cros_udevd", oneProc},
-		{exe, "/sbin/upstart-socket-bridge", "cros_upstart_socket_bridge", oneProc},
-		{exe, "/usr/bin/anomaly_detector", "cros_anomaly_detector", zeroProcs},
-		{exe, "/usr/bin/cras", "cros_cras", zeroProcs},
-		{exe, "/usr/bin/cros-disks", "cros_disks", oneProc},
-		{exe, "/usr/bin/dbus-daemon", "cros_dbus_daemon", oneProc},
-		{exe, "/usr/bin/memd", "cros_memd", zeroProcs},
-		{exe, "/usr/bin/metrics_daemon", "cros_metrics_daemon", zeroProcs},
-		{exe, "/usr/bin/midis", "cros_midis", zeroProcs}, // Only after start-arc-instance
-		{exe, "/usr/bin/powerd", "cros_powerd", zeroProcs},
-		{exe, "/usr/bin/shill", "cros_shill", zeroProcs},
-		{exe, "/usr/bin/sslh", "cros_sslh", zeroProcs},
-		{exe, "/usr/bin/tlsdated", "cros_tlsdated", oneProc},
-		{exe, "/usr/libexec/bluetooth/bluetoothd", "cros_bluetoothd", zeroProcs},
-		{exe, "/usr/sbin/ModemManager", "cros_modem_manager", zeroProcs},
-		{exe, "/usr/sbin/avahi-daemon", "cros_avahi_daemon", zeroProcs},
-		{exe, "/usr/sbin/chapsd", "cros_chapsd", zeroProcs},
-		{exe, "/usr/sbin/conntrackd", "cros_conntrackd", zeroProcs},
-		{exe, "/usr/sbin/cryptohomed", "cros_cryptohomed", zeroProcs},
-		{exe, "/usr/sbin/rsyslogd", "cros_rsyslogd", oneProc},
-		{exe, "/usr/sbin/sshd", "cros_sshd", zeroProcs},
-		{exe, "/usr/sbin/update_engine", "cros_update_engine", zeroProcs},
-		{exe, "/usr/sbin/wpa_supplicant", "wpa_supplicant", zeroProcs},
-	} {
+	}
+
+	testCases := make([]testCaseType, 0)
+	for _, sel := range testSelector {
+		switch sel {
+		case Stable:
+			testCases = append(testCases, []testCaseType{
+				{cmdline, "/usr/bin/periodic_scheduler", "cros_periodic_scheduler", twoProcs},
+				{cmdline, "/usr/share/cros/init/activate_date.sh", "cros_activate_date", zeroProcs},
+				{exe, "/opt/google/chrome/chrome", "cros_browser", zeroProcs}, // Only when browser exists
+				{exe, "/sbin/auditd", "cros_auditd", oneProc},                 // auditd must be running on SELinux boards
+				{exe, "/sbin/debugd", "cros_debugd", zeroProcs},
+				{exe, "/sbin/init", "cros_init", oneProc},
+				{exe, "/sbin/session_manager", "cros_session_manager", zeroProcs},
+				{exe, "/sbin/udevd", "cros_udevd", oneProc},
+				{exe, "/sbin/upstart-socket-bridge", "cros_upstart_socket_bridge", oneProc},
+				{exe, "/usr/bin/anomaly_detector", "cros_anomaly_detector", zeroProcs},
+				{exe, "/usr/bin/cras", "cros_cras", zeroProcs},
+				{exe, "/usr/bin/cros-disks", "cros_disks", oneProc},
+				{exe, "/usr/bin/dbus-daemon", "cros_dbus_daemon", oneProc},
+				{exe, "/usr/bin/memd", "cros_memd", zeroProcs},
+				{exe, "/usr/bin/metrics_daemon", "cros_metrics_daemon", zeroProcs},
+				{exe, "/usr/bin/midis", "cros_midis", zeroProcs}, // Only after start-arc-instance
+				{exe, "/usr/bin/powerd", "cros_powerd", zeroProcs},
+				{exe, "/usr/bin/shill", "cros_shill", zeroProcs},
+				{exe, "/usr/bin/sslh", "cros_sslh", zeroProcs},
+				{exe, "/usr/bin/tlsdated", "cros_tlsdated", oneProc},
+				{exe, "/usr/libexec/bluetooth/bluetoothd", "cros_bluetoothd", zeroProcs},
+				{exe, "/usr/sbin/ModemManager", "cros_modem_manager", zeroProcs},
+				{exe, "/usr/sbin/avahi-daemon", "cros_avahi_daemon", zeroProcs},
+				{exe, "/usr/sbin/chapsd", "cros_chapsd", zeroProcs},
+				{exe, "/usr/sbin/conntrackd", "cros_conntrackd", zeroProcs},
+				{exe, "/usr/sbin/cryptohomed", "cros_cryptohomed", zeroProcs},
+				{exe, "/usr/sbin/rsyslogd", "cros_rsyslogd", oneProc},
+				{exe, "/usr/sbin/sshd", "cros_sshd", zeroProcs},
+				{exe, "/usr/sbin/update_engine", "cros_update_engine", zeroProcs},
+				{exe, "/usr/sbin/wpa_supplicant", "wpa_supplicant", zeroProcs},
+			}...)
+		case Unstable:
+			testCases = append(testCases, []testCaseType{
+				{notExe, "/sbin/minijail0", notStr("minijail"), zeroProcs}, // These processes shouldn't exist.
+				{cmdline, ".*", notStr("chromeos"), zeroProcs},             // These processes shouldn't exist.
+				{cmdline, ".*", notStr("minijailed"), zeroProcs},           // These processes shouldn't exist.
+			}...)
+		}
+	}
+
+	for _, testCase := range testCases {
 		var p []Process
 		var err error
 		switch testCase.field {
 		case exe:
-			p = FindProcessesByExe(ps, testCase.query)
+			p = FindProcessesByExe(ps, testCase.query, false)
+		case notExe:
+			p = FindProcessesByExe(ps, testCase.query, true)
 		case cmdline:
 			p, err = FindProcessesByCmdline(ps, testCase.query)
 		default:
