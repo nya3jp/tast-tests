cca7437961df1ed6e994f4ca9b6522da825cc317
Billy Zhao <billyzhao@google.com>
2019-06-27 16:50:16 -0700

    Add helpers
    Change-Id: I3a75713725c3b6f2bb76a24786c2cdca029b4ea5

---
+++ src/chromiumos/tast/local/bundles/cros/network/iw_ibss.go
@@ -0,0 +1,49 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/local/bundles/cros/network/iw"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     IWIBSS,
+		Desc:     "Verifies `iw` IBSS behavior",
+		Contacts: []string{"billyzhao@google.com", "chromeos-kernel-wifi@google.com"},
+		Attr:     []string{"informational"},
+	})
+}
+
+func IWIBSS(ctx context.Context, s *testing.State) {
+	phy := "phy0"
+	iface := "ah0"
+	defer func() {
+		s.Log("cleanup")
+		iw.IBSSLeave(ctx, iface)
+		iw.RemoveInterface(ctx, iface)
+	}()
+	err := iw.AddInterface(ctx, phy, iface, "ibss")
+	if err != nil {
+		s.Fatal("AddInterface failed: ", err)
+	}
+	_, err = testexec.CommandContext(ctx, "ifconfig", iface, "up").Output()
+	if err != nil {
+		s.Fatal(fmt.Sprintf("Could not bring up interface %s: ", iface), err)
+	}
+	err = iw.IBSSJoin(ctx, iface, "Test", 2412)
+	if err != nil {
+		s.Fatal("IBSSJoin failed: ", err)
+	}
+	err = iw.IBSSLeave(ctx, iface)
+	if err != nil {
+		s.Fatal("IBSSLeave failed: ", err)
+	}
+}
