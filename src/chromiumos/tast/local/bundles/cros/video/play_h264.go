bca6f12363072fb9cbc04c4bcba902fa7a0e595e
Shuo-Peng Liao <deanliao@chromium.org>
2019-06-27 17:19:46 +0800

    tast-tests: Isolate Chrome log for TestPlay- and TestSeek-based tests.
    TestPlay- and TestSeek-based tests are flaky now. During investigation,
    we found Chrome log could be trimmed because it is too large as many
    tests already run on an existing Chrome instance. A trimmed log is
    useless for debugging.
    
    To obtain a full Chrome log, we decide to spend around 10 seconds for
    a test to run it in a standalone Chrome session. By doing so, we can
    get a Chrome log that contains only one test.
    
    BUG=chromium:941390
    TEST=tast run $ip video.PlayVP8 video.PlayVP9 video.PlayH264 video.PlayDecodeAccelUsed*
        video.Seek*
    
    Change-Id: I73d3a2805ff6da4012a2c4505a1c4f3dcd19957d

--- src/chromiumos/tast/local/bundles/cros/video/play_h264.go
+++ src/chromiumos/tast/local/bundles/cros/video/play_h264.go
@@ -7,9 +7,7 @@ package video
 import (
 	"context"
 
-	"chromiumos/tast/local/bundles/cros/video/lib/pre"
 	"chromiumos/tast/local/bundles/cros/video/play"
-	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
 )
 
@@ -21,13 +19,11 @@ func init() {
 		Attr:     []string{"informational"},
 		// "chrome_internal" is needed because H.264 is a proprietary codec.
 		SoftwareDeps: []string{"chrome", "chrome_internal"},
-		Pre:          pre.ChromeVideo(),
 		Data:         []string{"720_h264.mp4", "video.html"},
 	})
 }
 
 // PlayH264 plays 720_h264.mp4 with Chrome.
 func PlayH264(ctx context.Context, s *testing.State) {
-	play.TestPlay(ctx, s, s.PreValue().(*chrome.Chrome),
-		"720_h264.mp4", play.NormalVideo, play.NoCheckHistogram)
+	play.TestPlay(ctx, s, "720_h264.mp4", play.NormalVideo, play.NoCheckHistogram)
 }
