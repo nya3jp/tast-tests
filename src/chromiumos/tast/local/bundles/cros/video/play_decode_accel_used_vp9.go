b289ab628cfcba508bd2f5b1bbe374ab34f710f1
Shuo-Peng Liao <deanliao@chromium.org>
2019-06-27 17:19:46 +0800

    tast-tests: Isolate Chrome log for TestPlay- and TestSeek-based tests.
    TestPlay- and TestSeek-based tests are flaky now. During investigation,
    we found Chrome log could be trimmed because it is too large as many
    tests already run on an existing Chrome instance. A trimmed log is
    useless for debugging.
    
    To obtain a full Chrome log, we decide to spend around 10 seconds for
    a test to run it in a standalone Chrome session. By doing so, we can
    get a Chrome log that contains only one test.
    
    BUG=chromium:941390
    TEST=tast run $ip video.PlayVP8 video.PlayVP9 video.PlayH264 video.PlayDecodeAccelUsed*
        video.Seek*
    
    Change-Id: I73d3a2805ff6da4012a2c4505a1c4f3dcd19957d

--- src/chromiumos/tast/local/bundles/cros/video/play_decode_accel_used_vp9.go
+++ src/chromiumos/tast/local/bundles/cros/video/play_decode_accel_used_vp9.go
@@ -8,9 +8,7 @@ import (
 	"context"
 
 	"chromiumos/tast/local/bundles/cros/video/lib/caps"
-	"chromiumos/tast/local/bundles/cros/video/lib/pre"
 	"chromiumos/tast/local/bundles/cros/video/play"
-	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
 )
 
@@ -21,7 +19,6 @@ func init() {
 		Contacts:     []string{"deanliao@chromium.org", "chromeos-video-eng@google.com"},
 		Attr:         []string{"informational"},
 		SoftwareDeps: []string{caps.HWDecodeVP9, "chrome"},
-		Pre:          pre.ChromeVideo(),
 		Data:         []string{"720_vp9.webm", "video.html"},
 	})
 }
@@ -29,6 +26,5 @@ func init() {
 // PlayDecodeAccelUsedVP9 plays 720_vp9.webm with Chrome and
 // checks if video decode accelerator was used.
 func PlayDecodeAccelUsedVP9(ctx context.Context, s *testing.State) {
-	play.TestPlay(ctx, s, s.PreValue().(*chrome.Chrome),
-		"720_vp9.webm", play.NormalVideo, play.CheckHistogram)
+	play.TestPlay(ctx, s, "720_vp9.webm", play.NormalVideo, play.CheckHistogram)
 }
