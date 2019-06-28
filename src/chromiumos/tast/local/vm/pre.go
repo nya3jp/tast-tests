5da4d6fc59684280fcacb503bfd108dc4be863b1
Nicholas Hollingum <hollingum@google.com>
2019-06-28 11:35:17 +1000

    tast-tests: vm: Extended CrostiniPre to allow for an image artifact
    To prevent network-based flake, we would like to migrate crostini tests
    so that they use a pre-built image artifact where possible, rather than
    downloading the image.
    
    We refactor the CrostiniPre so that it can source the image either from
    a download or from the data dependency. This is a workaround suggested
    by nya@ which still requires us to list the data dependency for every
    artifact-based crostini test.
    
    BUG=chromium:979074
    TEST=tast -verbose run $DUT "vm.CrostiniStart*"
    
    Change-Id: Ie67e8d5abbee5bad8f30784a2ea7b860ec807eae

--- src/chromiumos/tast/local/vm/pre.go
+++ src/chromiumos/tast/local/vm/pre.go
@@ -13,6 +13,11 @@ import (
 	"chromiumos/tast/timing"
 )
 
+// ContainerImageArtifact holds the name of the artifact which will be used to
+// boot crostini. When using the CrostiniStartedByArtifact precondition, you
+// must list this as one of the data dependencies of your test.
+const ContainerImageArtifact string = "crostini_start_basic_guest_images.tar"
+
 // The CrostiniPre object is made available to users of this precondition via:
 //
 //	func DoSomething(ctx context.Context, s *testing.State) {
@@ -25,23 +30,38 @@ type CrostiniPre struct {
 	Container   *Container
 }
 
-// CrostiniStarted returns a precondition which is used in tests to ensure that
-// the crostini instance has been started, and makes several useful object
-// available for running your tests.
-func CrostiniStarted() testing.Precondition { return crostiniStartedPre }
+// CrostiniStartedByDownload is a precondition that ensures a tast test will
+// begin after crostini has been started by downloading an image.
+func CrostiniStartedByDownload() testing.Precondition { return crostiniStartedByDownloadPre }
 
-// The precondition used for crostini. The 10 minute timeout is based
-// on what the tests themselves define, and is generous to allow for
-// downloading the container image.
-var crostiniStartedPre = &preImpl{
-	name:    "crostini_started",
+// CrostiniStartedByArtifact is similar to CrostiniStartedByDownload, but will
+// use a pre-built image as a data-dependency rather than downloading one. To
+// use this precondition you must have ContainerImageArtifact as a data
+// dependency.
+func CrostiniStartedByArtifact() testing.Precondition { return crostiniStartedByArtifactPre }
+
+// The precondition used for crostini when an image download is needed. The 10
+// minute timeout is based on what the tests themselves define, and is generous
+// to allow for downloading the container image.
+var crostiniStartedByDownloadPre = &preImpl{
+	name:    "crostini_started_by_download",
 	timeout: chrome.LoginTimeout + 10*time.Minute,
+	dlImage: true,
+}
+
+// The precondition used for crostini when an image will be provided as a data
+// dependency.
+var crostiniStartedByArtifactPre = &preImpl{
+	name:    "crostini_started_by_artifact",
+	timeout: chrome.LoginTimeout + 7*time.Minute,
+	dlImage: false,
 }
 
 // Implementation of crostini's precondition.
 type preImpl struct {
 	name    string
 	timeout time.Duration
+	dlImage bool
 	cr      *chrome.Chrome
 	tconn   *chrome.Conn
 	cont    *Container
@@ -74,16 +94,27 @@ func (p *preImpl) Prepare(ctx context.Context, s *testing.State) interface{} {
 	if err = EnableCrostini(ctx, p.tconn); err != nil {
 		s.Fatal("Failed to enable Crostini preference setting: ", err)
 	}
-	s.Log("Setting up component ", StagingComponent)
-	if err = SetUpComponent(ctx, StagingComponent); err != nil {
-		s.Fatal("Failed to set up component: ", err)
-	}
 
-	s.Log("Creating default container")
-	// TODO(crbug/979074): Use a pre-build image rather than downloading one, to prevent
-	// network-based flakiness.
-	if p.cont, err = CreateDefaultContainer(ctx, s.OutDir(), p.cr.User(), StagingImageServer, ""); err != nil {
-		s.Fatal("Failed to set up default container: ", err)
+	if p.dlImage {
+		s.Log("Setting up component ", StagingComponent)
+		if err = SetUpComponent(ctx, StagingComponent); err != nil {
+			s.Fatal("Failed to set up component: ", err)
+		}
+		s.Log("Creating default container (from download)")
+		if p.cont, err = CreateDefaultContainer(ctx, s.OutDir(), p.cr.User(), StagingImageServer, ""); err != nil {
+			s.Fatal("Failed to set up default container (from download): ", err)
+		}
+	} else {
+		s.Log("Setting up component (from artifact)")
+		artifactPath := s.DataPath(ContainerImageArtifact)
+		if err = MountArtifactComponent(ctx, artifactPath); err != nil {
+			s.Fatal("Failed to set up component: ", err)
+		}
+
+		s.Log("Creating default container (from artifact)")
+		if p.cont, err = CreateDefaultContainer(ctx, s.OutDir(), p.cr.User(), Tarball, artifactPath); err != nil {
+			s.Fatal("Failed to set up default container (from artifact): ", err)
+		}
 	}
 
 	locked = true
