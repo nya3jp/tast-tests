ba58d272318ef34dd7cccded9f4e66538e202705
Ricardo Quesada <ricardoq@google.com>
2019-06-28 08:03:51 -0700

    tast-test: Window Manager CUJs
    This CL contains many CUJ use cases.
    This is for refernce only. Will be split in multiple smaller CLs
    
    BUG=chromium:972897
    TEST=tast run $DUT arc.WindowManagerCUJ
    
    Change-Id: I06f351f05297b5d8e9f2ff83629d6f554b0f5c2a

---
+++ src/chromiumos/tast/local/bundles/cros/example/chrome_display.go
@@ -0,0 +1,58 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ChromeDisplay,
+		Desc:         "Demonstrates using the chrome.display API",
+		Contacts:     []string{"ricardoq@chromium.org", "tast-users@chromium.org"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func ChromeDisplay(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to open connection: ", err)
+	}
+
+	info, err := display.GetInternalInfo(ctx, tconn)
+	if err != nil {
+		s.Fatal("No display: ", err)
+	}
+	s.Logf("Display info: %+v", info)
+	s.Log("Supported display modes:")
+	for _, m := range info.Modes {
+		s.Logf("Mode: %+v", *m)
+	}
+
+	newZoom := info.AvailableDisplayZoomFactors[0]
+	newRot := 90
+
+	s.Logf("Setting display properties: rotation=%d, zoom=%v", newRot, newZoom)
+	p := display.DisplayProperties{DisplayZoomFactor: &newZoom, Rotation: &newRot}
+	if err := display.SetDisplayProperties(ctx, tconn, info.ID, p); err != nil {
+		s.Fatal("Failed to set display properties: ", err)
+	}
+
+	// Leave Chrome in a reasonable state. Restore zoom and rotation.
+	s.Logf("Restoring display properties: rotation=%d, zoom=%v", info.Rotation, info.DisplayZoomFactor)
+	p = display.DisplayProperties{DisplayZoomFactor: &info.DisplayZoomFactor, Rotation: &info.Rotation}
+	if err := display.SetDisplayProperties(ctx, tconn, info.ID, p); err != nil {
+		s.Fatal("Failed to set display properties: ", err)
+	}
+}
---
+++ src/chromiumos/tast/local/chrome/settings/settings.go
@@ -0,0 +1,55 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package settings implements a library used for communication with Chrome settings.
+// chrome.Conn must have the "settingsPrivate" permission.
+package settings
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/local/chrome"
+)
+
+// DefaultZoom returns the default page zoom factor. Possible values are currently between
+// 0.25 and 5. For a full list, see zoom::kPresetZoomFactors in:
+// https://cs.chromium.org/chromium/src/components/zoom/page_zoom_constants.cc
+func DefaultZoom(ctx context.Context, c *chrome.Conn) (float64, error) {
+	var zoom float64
+	if err := c.EvalPromise(ctx,
+		`new Promise(function(resolve, reject) {
+		  chrome.settingsPrivate.getDefaultZoom(function(zoom) {
+		    if (chrome.runtime.lastError) {
+		      reject(new Error(chrome.runtime.lastError.message));
+		      return;
+		    }
+		    resolve(zoom);
+		  })
+		})`, &zoom); err != nil {
+		return 0, err
+	}
+	return zoom, nil
+}
+
+// SetDefaultZoom sets the page zoom factor. Must be less than 0.001 different than a value
+// in zoom::kPresetZoomFactors. See:
+// https://cs.chromium.org/chromium/src/components/zoom/page_zoom_constants.cc
+func SetDefaultZoom(ctx context.Context, c *chrome.Conn, zoom float64) error {
+	expr := fmt.Sprintf(
+		`new Promise(function(resolve, reject) {
+		  chrome.settingsPrivate.setDefaultZoom(%f, function(success) {
+		    if (chrome.runtime.lastError) {
+		      reject(new Error(chrome.runtime.lastError.message));
+		      return;
+		    }
+		    if (!success) {
+		      reject(new Error("setDefaultZoom() failed"));
+		      return;
+		    }
+		    resolve();
+		  })
+		})`, zoom)
+	return c.EvalPromise(ctx, expr, nil)
+}
