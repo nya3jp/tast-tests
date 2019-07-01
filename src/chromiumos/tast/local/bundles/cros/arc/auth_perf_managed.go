65032e809cb674dbaf2ef9bb733e7239c487d95f
Satoshi Niwa <niwa@google.com>
2019-07-01 17:08:27 +0900

    Port AuthPerf to tast
    Ported from:
      third_party/autotest-tests-cheets/client/site_tests/cheets_AuthPerf/cheets_AuthPerf.py
      third_party/autotest/files/client/common_lib/cros/arc_util.py
    
    Sample output in log:
      play_store_shown_time - average: 16790, range 15035 - 19050 based on 10 samples
      account_check_time - average: 1, range 0 - 8 based on 10 samples
      ...
      10,15035,16790,19050,0,1,8,1789,2292,3014,0,0,0,3531,4698,6002
    
    Cq-Depend: chromium:1694844
    TEST=tast run my_crbook arc.AuthPerfUnmanaged
    BUG=b:135964304
    Change-Id: Iede4827d768c75057c6dc9eebb397a5dcb7021aa

---
+++ src/chromiumos/tast/local/bundles/cros/arc/auth_perf_managed.go
@@ -0,0 +1,38 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/arc/authperf"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AuthPerfManaged,
+		Desc: "Measure auth times in ARC for managed case",
+		Contacts: []string{
+			"khmel@chromium.org", // Original author.
+			"niwa@chromium.org",  // Tast port author.
+			"arc-eng@google.com",
+		},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"android", "chrome"},
+		// This test steps through opt-in flow 10 times and each iteration takes 20~40 seconds.
+		Timeout: 20 * time.Minute,
+	})
+}
+
+func AuthPerfManaged(ctx context.Context, s *testing.State) {
+	const (
+		username = "autotest-arc-enabled@cros1.managedchrome.com"
+		password = "Z=8buZ6T"
+	)
+
+	authperf.RunTest(ctx, s, username, password, "_managed")
+}
