6099a4afddaf7b68950077139b92a89b228f1618
Ricardo Quesada <ricardoq@google.com>
2019-07-02 14:22:31 -0700

    tast-test: arc.WindowManagerCUJ works on tablet devices
    arc.WindowmanagerCUJ is failing on certain tablet devices because the
    activity is being launched while the transition from tablet-to-clamshell
    mode is still in progress.
    
    At the moment we don't have a reliable way to "wait for transition to
    finish". So, this CL adds a timeout of 5 seconds (research in progress
    regarding how to get rid of the delay).
    
    This CL also cleans-up the code a bit, a make it more modular. Needed
    for a follow-up CL that will include more tests.
    
    BUG=chromium:980711
    TEST=Tested on nocture both in tablet and clamshell modes
        $ tast run $DUT arc.WindowManagerCUJ
    
    Change-Id: I8399e5ef430eb53d44e9c89f9735e466c2e7e754

--- src/chromiumos/tast/local/bundles/cros/arc/window_manager_cuj.go
+++ src/chromiumos/tast/local/bundles/cros/arc/window_manager_cuj.go
@@ -35,11 +35,15 @@ const (
 	// These values must match the strings from ArcWMTestApp defined in BaseActivity#parseCaptionButtons:
 	// http://cs/android/vendor/google_arc/packages/development/ArcWMTestApp/src/org/chromium/arc/testapp/windowmanager/BaseActivity.java?l=448
 	wmBack     = "back"
-	wmMinimize = "minimize"
-	wmMaximize = "maximize"
-	wmRestore  = "restore"
 	wmClose    = "close"
+	wmMaximize = "maximize"
+	wmMinimize = "minimize"
+	wmPortrait = "portrait"
+	wmRestore  = "restore"
 )
+
+// wmTestStateFunc represents a function that tests if the window is in a certain state.
+type wmTestStateFunc func(context.Context, *arc.Activity, *ui.Device) error
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -75,14 +79,38 @@ func WindowManagerCUJ(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed installing app: ", err)
 	}
 
+	tabletModeEnabled, err := ash.TabletModeEnabled(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get tablet mode: ", err)
+	}
+	if tabletModeEnabled {
+		// Be nice and restore tablet mode to its original state on exit.
+		defer ash.SetTabletModeEnabled(ctx, tconn, tabletModeEnabled)
+		if err := ash.SetTabletModeEnabled(ctx, tconn, false); err != nil {
+			s.Fatal("Failed to  set tablet mode disabled: ", err)
+		}
+		// TODO(ricardoq): Wait for "tablet mode animation is finished" in a reliable way.
+		// If an activity is launched while the tablet mode animation is active, the activity
+		// will be launched in un undefined state, making the test flaky.
+		if err := testing.Sleep(ctx, 5*time.Second); err != nil {
+			s.Fatal("Failed to wait until tablet-mode animation finished: ", err)
+		}
+	}
+
 	type testFunc func(context.Context, *chrome.Conn, *arc.ARC, *ui.Device) error
 	for idx, test := range []struct {
 		name string
 		fn   testFunc
 	}{
-		{"Default Launch Clamshell SDK24", wmDefaultLaunchClamshell24},
+		{"Default Launch Clamshell N", wmDefaultLaunchClamshell24},
 	} {
 		s.Logf("Running test %q", test.name)
+
+		// Reset WM state to default values.
+		if err := a.Command(ctx, "am", "broadcast", "-a", "android.intent.action.arc.cleartaskstate").Run(); err != nil {
+			s.Fatal("Failed to clear task states: ", err)
+		}
+
 		if err := test.fn(ctx, tconn, a, d); err != nil {
 			path := fmt.Sprintf("%s/screenshot-cuj-failed-test-%d.png", s.OutDir(), idx)
 			if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
@@ -97,45 +125,24 @@ func WindowManagerCUJ(ctx context.Context, s *testing.State) {
 // and resize conditions. And it verifies that their default launch state is the expected one, as defined in:
 // go/arc-wm-p "Clamshell: default launch behavior - Android NYC or above" (slide #6).
 func wmDefaultLaunchClamshell24(ctx context.Context, tconn *chrome.Conn, a *arc.ARC, d *ui.Device) error {
-	// Expected caption buttons.
-	wmCaptionBMRC := []string{wmBack, wmMinimize, wmRestore, wmClose}
-	wmCaptionBMMC := []string{wmBack, wmMinimize, wmMaximize, wmClose}
-	wmCaptionBMC := []string{wmBack, wmMinimize, wmClose}
-
-	tabletModeEnabled, err := ash.TabletModeEnabled(ctx, tconn)
-	if err != nil {
-		return errors.Wrap(err, "could not get tablet mode")
-	}
-	// Be nice and restore tablet mode to its original state on exit.
-	defer ash.SetTabletModeEnabled(ctx, tconn, tabletModeEnabled)
-	if err := ash.SetTabletModeEnabled(ctx, tconn, false); err != nil {
-		return errors.Wrap(err, "could not set tablet mode disabled")
-	}
-
-	// Reset WM state to default values.
-	if err := a.Command(ctx, "am", "broadcast", "-a", "android.intent.action.arc.cleartaskstate").Run(); err != nil {
-		return errors.Wrap(err, "could not clear task states")
-	}
 	for _, test := range []struct {
-		name          string
-		act           string
-		wantedState   arc.WindowState
-		wantedCaption []string
+		name        string
+		act         string
+		wantedState wmTestStateFunc
 	}{
 		// The are four possible default states (windows #A to #D) from six possible different activities.
 		// Window #A.
-		{"Landscape + Resize enabled", wmResizeableLandscapeActivity, arc.WindowStateMaximized, wmCaptionBMRC},
+		{"Landscape + Resize enabled", wmResizeableLandscapeActivity, checkMaximizeResizeable},
 		// Window #B.
-		{"Landscape + Resize disabled", wmNonResizeableLandscapeActivity, arc.WindowStateMaximized, wmCaptionBMC},
+		{"Landscape + Resize disabled", wmNonResizeableLandscapeActivity, checkMaximizeNonResizeable},
 		// Window #A.
-		{"Unspecified + Resize enabled", wmResizeableUnspecifiedActivity, arc.WindowStateMaximized, wmCaptionBMRC},
+		{"Unspecified + Resize enabled", wmResizeableUnspecifiedActivity, checkMaximizeResizeable},
 		// Window #B.
-		{"Unspecified + Resize disabled", wmNonResizeableUnspecifiedActivity, arc.WindowStateMaximized, wmCaptionBMC},
+		{"Unspecified + Resize disabled", wmNonResizeableUnspecifiedActivity, checkMaximizeNonResizeable},
 		// Window #C.
-		{"Portrait + Resized enabled", wmResizeablePortraitActivity, arc.WindowStateNormal, wmCaptionBMMC},
+		{"Portrait + Resized enabled", wmResizeablePortraitActivity, checkRestoreResizeable},
 		// Window #D.
-		// TODO(ricardoq): detect pillarbox mode.
-		{"Portrait + Resize disabled", wmNonResizeablePortraitActivity, arc.WindowStateMaximized, wmCaptionBMC},
+		{"Portrait + Resize disabled", wmNonResizeablePortraitActivity, checkPillarboxNonResizeable},
 	} {
 		testing.ContextLogf(ctx, "Running subtest %q", test.name)
 		act, err := arc.NewActivity(a, wmPkg24, test.act)
@@ -154,21 +161,10 @@ func wmDefaultLaunchClamshell24(ctx context.Context, tconn *chrome.Conn, a *arc.
 			return err
 		}
 
-		state, err := act.GetWindowState(ctx)
-		if err != nil {
+		if err := test.wantedState(ctx, act, d); err != nil {
 			return err
 		}
-		if state != test.wantedState {
-			return errors.Errorf("invalid window state %v, want %v", state, test.wantedState)
-		}
 
-		bn, err := wmCaptionButtons(ctx, d)
-		if err != nil {
-			return errors.Wrap(err, "could not get caption buttons state")
-		}
-		if !reflect.DeepEqual(bn, test.wantedCaption) {
-			return errors.Errorf("invalid caption buttons %+v, want %+v", bn, test.wantedCaption)
-		}
 		// Stopping current activity in order to make it possible to launch a different from the same package.
 		if err := act.Stop(ctx); err != nil {
 			return errors.Wrapf(err, "could not stop activity %v", test.act)
@@ -177,17 +173,90 @@ func wmDefaultLaunchClamshell24(ctx context.Context, tconn *chrome.Conn, a *arc.
 	return nil
 }
 
-// wmCaptionButtons returns the caption buttons that are present in the ArcWMTestApp window.
-func wmCaptionButtons(ctx context.Context, d *ui.Device) (buttons []string, err error) {
-	s, err := getWMState(ctx, d)
-	if err != nil {
-		return nil, err
+// Helper functions
+
+// checkMaximizeResizeable checks that the window is maximized and is resizeable.
+func checkMaximizeResizeable(ctx context.Context, act *arc.Activity, d *ui.Device) error {
+	if err := compareWMState(ctx, act, arc.WindowStateMaximized); err != nil {
+		return err
 	}
-	return s.Buttons, nil
+	caption := []string{wmBack, wmMinimize, wmRestore, wmClose}
+	return compareCaption(ctx, act, d, caption)
 }
 
-// wmState represents the state of ArcWMTestApp activity.
-type wmState struct {
+// checkMaximizeNonResizeable checks that the window is maximized and is not resizeable.
+func checkMaximizeNonResizeable(ctx context.Context, act *arc.Activity, d *ui.Device) error {
+	if err := compareWMState(ctx, act, arc.WindowStateMaximized); err != nil {
+		return err
+	}
+	caption := []string{wmBack, wmMinimize, wmClose}
+	return compareCaption(ctx, act, d, caption)
+}
+
+// checkRestoreResizeable checks that the window is in restore mode and is resizeable.
+func checkRestoreResizeable(ctx context.Context, act *arc.Activity, d *ui.Device) error {
+	if err := compareWMState(ctx, act, arc.WindowStateNormal); err != nil {
+		return err
+	}
+	caption := []string{wmBack, wmMinimize, wmMaximize, wmClose}
+	return compareCaption(ctx, act, d, caption)
+}
+
+// checkPillarboxNonResizeable checks that the window is in pillar-box mode and is resizeable.
+func checkPillarboxNonResizeable(ctx context.Context, act *arc.Activity, d *ui.Device) error {
+	if err := checkPillarbox(ctx, act, d); err != nil {
+		return err
+	}
+	caption := []string{wmBack, wmMinimize, wmClose}
+	return compareCaption(ctx, act, d, caption)
+}
+
+// checkPillarbox checks that the window is in pillar-box mode.
+func checkPillarbox(ctx context.Context, act *arc.Activity, d *ui.Device) error {
+	const wanted = wmPortrait
+	o, err := uiOrientation(ctx, act, d)
+	if err != nil {
+		return err
+	}
+	if o != wanted {
+		return errors.Errorf("invalid orientation %v; want %v", o, wanted)
+	}
+
+	return compareWMState(ctx, act, arc.WindowStateMaximized)
+}
+
+// compareWMState compares the activity window state  with the wanted one.
+// Returns nil only if they are equal.
+func compareWMState(ctx context.Context, act *arc.Activity, wanted arc.WindowState) error {
+	state, err := act.GetWindowState(ctx)
+	if err != nil {
+		return err
+	}
+	if state != wanted {
+		return errors.Errorf("invalid window state %v, want %v", state, wanted)
+	}
+	return nil
+}
+
+// compareCaption compares the activity caption buttons with the wanted one.
+// Returns nil only if they are equal.
+func compareCaption(ctx context.Context, act *arc.Activity, d *ui.Device, wantedCaption []string) error {
+	bn, err := uiCaptionButtons(ctx, act, d)
+	if err != nil {
+		return errors.Wrap(err, "could not get caption buttons state")
+	}
+	if !reflect.DeepEqual(bn, wantedCaption) {
+		return errors.Errorf("invalid caption buttons %+v, want %+v", bn, wantedCaption)
+	}
+	return nil
+}
+
+// Helper UI functions
+// These functions use UI Automator to get / change the state of ArcWMTest activity.
+
+// uiState represents the state of ArcWMTestApp activity. See:
+// http://cs/pi-arc-dev/vendor/google_arc/packages/development/ArcWMTestApp/src/org/chromium/arc/testapp/windowmanager/JsonHelper.java
+type uiState struct {
 	WindowState       string      `json:"windowState"`
 	Orientation       string      `json:"orientation"`
 	DeviceMode        string      `json:"deviceMode"`
@@ -199,10 +268,13 @@ type wmState struct {
 	Accel             interface{} `json:"accel"`
 }
 
-// getWMState returns the state from the ArcWMTest activity.
+// getUIState returns the state from the ArcWMTest activity.
 // The state is taken by parsing the activity's TextView which contains the state in JSON format.
-func getWMState(ctx context.Context, d *ui.Device) (*wmState, error) {
-	obj := d.Object(ui.ClassName("android.widget.TextView"), ui.ResourceIDMatches(".+?(/caption_text_view)$"))
+func getUIState(ctx context.Context, act *arc.Activity, d *ui.Device) (*uiState, error) {
+	obj := d.Object(
+		ui.PackageName(act.PackageName()),
+		ui.ClassName("android.widget.TextView"),
+		ui.ResourceIDMatches(".+?(/caption_text_view)$"))
 	if err := obj.WaitForExists(ctx, 10*time.Second); err != nil {
 		return nil, err
 	}
@@ -210,9 +282,27 @@ func getWMState(ctx context.Context, d *ui.Device) (*wmState, error) {
 	if err != nil {
 		return nil, err
 	}
-	var state wmState
+	var state uiState
 	if err := json.Unmarshal([]byte(s), &state); err != nil {
 		return nil, errors.Wrap(err, "failed unmarshalling state")
 	}
 	return &state, nil
+}
+
+// uiCaptionButtons returns the caption buttons that are present in the ArcWMTestApp window.
+func uiCaptionButtons(ctx context.Context, act *arc.Activity, d *ui.Device) (buttons []string, err error) {
+	s, err := getUIState(ctx, act, d)
+	if err != nil {
+		return nil, err
+	}
+	return s.Buttons, nil
+}
+
+// uiOrientation returns the current orientation of the ArcWMTestApp window.
+func uiOrientation(ctx context.Context, act *arc.Activity, d *ui.Device) (string, error) {
+	s, err := getUIState(ctx, act, d)
+	if err != nil {
+		return "", err
+	}
+	return s.Orientation, nil
 }
