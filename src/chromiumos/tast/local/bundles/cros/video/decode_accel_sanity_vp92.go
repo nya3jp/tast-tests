29f2cbb40e77ab54853540a7ce874f441c1ad4ae
Shuo-Peng Liao <deanliao@chromium.org>
2019-07-03 15:11:01 +0800

    tast-tests: Add "vp9_sanity" into SoftwareDeps for DecodeAccelSanityVP9(2|3).
    It is used to blacklist RK3399 device from running
    video.DecodeAccelSanityVP9(2|3) as RK3399 kernel is known to have kernel crash
    issue playing unsupported VP9 profile stream.
    
    Noted that though RK3399 does not support VP9 profile 1, it does not crash on
    video.DecodeAccelSanityVP91 test so we still run the test on RK3399 devices.
    
    BUG=chromium:971032
    TEST=Run the following command on RK3399 device, e.g. scarlet
    tast run --checktestdeps always --buildlocalrunner $DUT video.DecodeAccelSanityVP9*
    with CL: https://crrev.com/c/1669975
    Expect to see video.DecodeAccelSanityVP9(2|3) tests are skipped.
    
    Cq-Depend: chromium:1669975
    Change-Id: If2b2e07ebf8367d767c36584eff3334ee60819e9

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_sanity_vp92.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_sanity_vp92.go
@@ -14,11 +14,14 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         DecodeAccelSanityVP92,
-		Desc:         "Run Chrome video_decode_accelerator_unittest's NoCrash test on a VP9.2 video",
-		Contacts:     []string{"deanliao@chromium.org", "chromeos-video-eng@google.com"},
-		Attr:         []string{"informational"},
-		SoftwareDeps: []string{"chrome", caps.HWDecodeVP9},
+		Func:     DecodeAccelSanityVP92,
+		Desc:     "Run Chrome video_decode_accelerator_unittest's NoCrash test on a VP9.2 video",
+		Contacts: []string{"deanliao@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:     []string{"informational"},
+		// "vp9_sanity" is a whitelist of devices that stay alive playing unsupported VP9 profile stream.
+		// Currently RK3399 devices may crash playing the VP9 profile 2 stream, so they are excluded.
+		// See crbug.com/971032 for detail.
+		SoftwareDeps: []string{"chrome", caps.HWDecodeVP9, "vp9_sanity"},
 		Data:         []string{decode.DecodeAccelSanityVP92.Name},
 	})
 }
