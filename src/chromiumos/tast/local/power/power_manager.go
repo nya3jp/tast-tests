668952a7f4e0011265f77f70fd5e0c87cdb7f5f7
Yu-Hsuan Hsu <yuhsuan@chromium.org>
2019-07-05 15:58:06 +0800

    tast: Turn on a display before waiting for audio devices
    Some devices use their displays as an internal speaker, like monroe.
    When a display is closed, the internal speaker is removed, too. It
    causes that we can not find the usable speaker on monroe. To fix the
    problem, we need to turn on a display to re-enable a HDMI internal
    speaker.
    
    BUG=b:135298456
    TEST=tast run ${DUT_IP} audio.ALSAConformance
    It can pass on monroe even if a display is closed.
    
    Change-Id: I40c3761b1257d8119529a17d69e7274e004bcf8b
    Reviewed-on: https://chromium-review.googlesource.com/1689053
    Tested-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Ready: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/power/power_manager.go
+++ src/chromiumos/tast/local/power/power_manager.go
@@ -10,6 +10,7 @@ import (
 	"github.com/godbus/dbus"
 
 	pmpb "chromiumos/system_api/power_manager_proto"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/dbusutil"
 )
 
@@ -27,6 +28,19 @@ type PowerManager struct { // NOLINT
 	obj  dbus.BusObject
 }
 
+// UserActivityType is a status code for the PowerManager related D-Bus methods.
+type UserActivityType int32
+
+// Values are from src/platform2/system_api/dbus/power_manager/dbus-constants.h
+const (
+	UserActivityOther                  UserActivityType = 0
+	UserActivityBrightnessUpKeyPress   UserActivityType = 1
+	UserActivityBrightnessDownKeyPress UserActivityType = 2
+	UserActivityVolumeUpKeyPress       UserActivityType = 3
+	UserActivityVolumeDownKeyPress     UserActivityType = 4
+	UserActivityVolumeMuteKeyPress     UserActivityType = 5
+)
+
 // NewPowerManager connects to power_manager via D-Bus and returns a PowerManager object.
 func NewPowerManager(ctx context.Context) (*PowerManager, error) {
 	conn, obj, err := dbusutil.Connect(ctx, dbusName, dbusPath)
@@ -41,4 +55,22 @@ func (m *PowerManager) GetSwitchStates(ctx context.Context) (*pmpb.SwitchStates,
 	ret := &pmpb.SwitchStates{}
 	err := dbusutil.CallProtoMethod(ctx, m.obj, dbusInterface+".GetSwitchStates", nil, ret)
 	return ret, err
+}
+
+// HandleUserActivity calls PowerManager.HandleUserActivity D-Bus method.
+func (m *PowerManager) HandleUserActivity(ctx context.Context, ActivityType UserActivityType) error {
+	return m.obj.CallWithContext(ctx, dbusInterface+".HandleUserActivity", 0, ActivityType).Err
+}
+
+// TurnOnDisplay turns on a display by sending a user activity ping to PowerManager
+// to light up the display.
+func TurnOnDisplay(ctx context.Context) error {
+	powerd, err := NewPowerManager(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create a PowerManager object")
+	}
+	if err := powerd.HandleUserActivity(ctx, UserActivityOther); err != nil {
+		return errors.Wrap(err, "failed to call HandleUserActivity D-Bus method")
+	}
+	return nil
 }
