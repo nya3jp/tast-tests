635f599770bfea5eaf2c9266cc91576fc93dbf66
Kazuki Takise <takise@google.com>
2019-07-08 17:53:31 +0900

    tast-test: Add black flash test
    BUG=b:136977595
    TEST=tast -verbose run <DUT> arc.BlackFlash
    TEST=tast -verbose run <DUT> arc.ResizeActivity
    TEST=tast -verbose run <DUT> arc.WindowState
    Change-Id: I432816e9725b135dca30ce80300e85e3312d1eca

---
+++ src/chromiumos/tast/local/bundles/cros/arc/screenshot/screenshot.go
@@ -0,0 +1,61 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package screenshot provides functions to assist with taking screenshots and analyzing them
+// in ARC window management tests.
+package screenshot
+
+import (
+	"context"
+	"fmt"
+	"image"
+	"image/color"
+	"os"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/colorcmp"
+	"chromiumos/tast/local/screenshot"
+)
+
+// CountPixels returns how many pixels in the specified color are contained in image.
+func CountPixels(image image.Image, clr color.Color) int {
+	// TODO(ricardoq): At least on Eve, Nocturne, Caroline, Kevin and Dru the color
+	// in black flashes is RGBA(0,0,0,255). But it might be possible that on certain
+	// devices the color is slightly different. In that case we should adjust the colorMaxDiff.
+	const colorMaxDiff = 0
+	rect := image.Bounds()
+	numPixels := 0
+	for y := rect.Min.Y; y < rect.Max.Y; y++ {
+		for x := rect.Min.X; x < rect.Max.X; x++ {
+			if colorcmp.ColorsMatch(image.At(x, y), clr, colorMaxDiff) {
+				numPixels++
+			}
+		}
+	}
+	return numPixels
+}
+
+// GrabScreenshot creates a screenshot and returns an image.Image.
+// The path of the image is generated ramdomly in /tmp.
+func GrabScreenshot(ctx context.Context, cr *chrome.Chrome) (image.Image, error) {
+	path := fmt.Sprintf("/tmp/screenshot_%d.png", time.Now().UTC().UnixNano())
+
+	if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
+		return nil, errors.Wrap(err, "failed to capture screenshot")
+	}
+
+	fd, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "error opening screenshot file")
+	}
+	defer fd.Close()
+
+	img, _, err := image.Decode(fd)
+	if err != nil {
+		return nil, errors.Wrap(err, "error decoding image file")
+	}
+	return img, nil
+}
