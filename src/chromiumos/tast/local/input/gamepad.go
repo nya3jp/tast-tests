098fc3203bde34d57983225421254745cc61a963
Tetsui Ohkubo <tetsui@chromium.org>
2019-07-08 17:22:19 +0900

    tast-tests: Add arc.Gamepad test.
    This CL adds an integration test for ARC++ improved gamepad support
    http://go/arc-improved-gamepad. When the feature flag ExoRawGamepadInfo
    is disabled, the test will fail.
    
    This CL creates a virtual gamepad device and injects events by using
    uinput. The virtual gamepad mocks USB-connected DualShock v4.
    
    This CL tests the following features:
    * Gamepad connection/disconnection is properly notified by
      InputDeviceListener.
    * Gamepad buttons are properly mapped and notified as KeyEvent.
    
    The following part of the test will be added in upcoming CLs:
    * Gamepad axes are properly mapped an notified as MotionEvent.
    
    TEST=tast eve-DUT arc.Gamepad
    BUG=b:130597086
    
    Change-Id: Ia374525c96b3511ba26e0daaba48479648925079

---
+++ src/chromiumos/tast/local/input/gamepad.go
@@ -0,0 +1,85 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package input
+
+import (
+	"context"
+	"math/big"
+	"os"
+)
+
+// GamepadEventWriter supports injecting events into a virtual gamepad device.
+type GamepadEventWriter struct {
+	rw   *RawEventWriter
+	virt *os.File // if non-nil, used to hold a virtual device open
+	dev  string   // path to underlying device in /dev/input
+}
+
+// Gamepad creates a virtual gamepad device and returns and EventWriter that injects events into it.
+func Gamepad(ctx context.Context) (*GamepadEventWriter, error) {
+	gw := &GamepadEventWriter{}
+	const usbBus = 0x3 // BUS_USB from input.h
+	var err error
+	if gw.dev, gw.virt, err = createVirtual(
+		gw.DeviceName(),
+		devID{usbBus, gw.VendorID(), gw.ProductID(), 0x0111}, 0, 0x1b,
+		map[EventType]*big.Int{
+			EV_KEY: makeBigInt([]uint64{0x3fff000000000000, 0, 0, 0, 0}),
+			EV_ABS: big.NewInt(0x26081000003003f),
+			EV_MSC: big.NewInt(0x10)}); err != nil {
+		return nil, err
+	}
+
+	if gw.rw, err = Device(ctx, gw.dev); err != nil {
+		gw.Close()
+		return nil, err
+	}
+
+	return gw, nil
+}
+
+// Close closes the gamepad device.
+func (gw *GamepadEventWriter) Close() error {
+	var firstErr error
+	if gw.rw != nil {
+		firstErr = gw.rw.Close()
+	}
+	if gw.virt != nil {
+		if err := gw.virt.Close(); firstErr == nil {
+			firstErr = err
+		}
+	}
+	return firstErr
+}
+
+// Device returns the path of the underlying device, e.g. "/dev/input/event3".
+func (gw *GamepadEventWriter) Device() string { return gw.dev }
+
+// VendorID returns the vendor ID of the virtual gamepad device.
+func (gw *GamepadEventWriter) VendorID() uint16 { return 0x054c }
+
+// ProductID returns the product ID of the virtual gamepad device.
+func (gw *GamepadEventWriter) ProductID() uint16 { return 0x09cc }
+
+// DeviceName returns the device name of the virtual gamepad device.
+func (gw *GamepadEventWriter) DeviceName() string {
+	return "Sony Interactive Entertainment Wireless Controller"
+}
+
+// sendKey writes a EV_KEY event containing the specified code and value, followed by a EV_SYN event.
+func (gw *GamepadEventWriter) sendKey(ctx context.Context, ec EventCode, val int32) error {
+	if err := gw.rw.Event(EV_KEY, ec, val); err != nil {
+		return err
+	}
+	return gw.rw.Sync()
+}
+
+// Button presses and releases the gamepad button specified by EventCode.
+func (gw *GamepadEventWriter) Button(ctx context.Context, ec EventCode) error {
+	if err := gw.sendKey(ctx, ec, 1); err != nil {
+		return err
+	}
+	return gw.sendKey(ctx, ec, 0)
+}
