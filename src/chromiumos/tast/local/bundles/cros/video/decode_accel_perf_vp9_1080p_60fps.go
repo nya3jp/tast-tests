416bb4ad82e9a1532e83ebd18946819f6f4bc367
David Staessens <dstaessens@chromium.org>
2019-07-10 09:05:26 +0900

    tast.video.DecodeAccelPerf: Add VP8/9 1080P@60FPS video decoder perf test.
    This CL adds a new Tast video decoder performance test for a VP8 and VP9
    1080P@60FPS video using the new video_decode_accelerator_perf_tests binary.
    
    TEST=video.DecodeAccelPerfVP*1080P60FPS on eve
    
    BUG=chromium:953114
    
    Change-Id: I7499a4cd624661eaa96f01ba20fc78b16937d7a9
    Reviewed-on: https://chromium-review.googlesource.com/1694201
    Tested-by: David Staessens <dstaessens@chromium.org>
    Commit-Ready: David Staessens <dstaessens@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_perf_vp9_1080p_60fps.go
@@ -0,0 +1,29 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DecodeAccelPerfVP91080P60FPS,
+		Desc:         "Runs video_decode_accelerator_perf_tests with a VP9 1080p@60fps video",
+		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", caps.HWDecodeVP9},
+		Data:         []string{"1080p_60fps_600frames.vp9.ivf", "1080p_60fps_600frames.vp9.ivf.json"},
+	})
+}
+
+// DecodeAccelPerfVP91080P60FPS runs the video_decode_accelerator_perf_tests with 1080p_60fps_600frames.vp9.ivf.
+func DecodeAccelPerfVP91080P60FPS(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoPerfTest(ctx, s, "1080p_60fps_600frames.vp9.ivf")
+}
