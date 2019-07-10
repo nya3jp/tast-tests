0b1c6e8aaa8e42138ff3cfb28d72593f5d53f525
Kartik Hegde <khegde@chromium.org>
2019-07-10 10:32:29 -0600

    tast: Validate cros_healthd probe for battery metrics
    This test checks that output from the battery prober has a proper
    header and expected number of lines.
    
    BUG=chromium:978128
    TEST=tast -verbose run -build=true $DUT_IP platform.CrosHealthd
    
    Cq-Depend: chromium:1682268
    Change-Id: Ifd00c9bfc8f5f3c3ae0f8b8b877a782d4f0116b8

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_battery_metrics.go
@@ -0,0 +1,74 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosHealthdBatteryMetricsProbe,
+		Desc: "Check that we can probe cros_healthd for battery metrics",
+		Contacts: []string{
+			"wbbradley@google.com",
+			"pmoy@google.com",
+			"khegde@google.com",
+		},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"diagnostics"},
+	})
+}
+
+func CrosHealthdProbeBatteryMetrics(ctx context.Context, s *testing.State) {
+	b, err := testexec.CommandContext(ctx, "cros_healthd", "--probe_battery_metrics").Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to run 'cros_healthd --probe_battery_metrics': ", err)
+	}
+
+	lines := strings.Split(string(b), "\n")
+	if len(lines) != 2 {
+		s.Fatalf("Incorrect number of output lines: got %d; want 2", len(lines))
+	}
+	headerExpected := []string{"charge_full", "charge_full_design", "cycle_count", "serial_number", "vendor(manufacturer)", "voltage_now", "voltage_min_design"}
+	headerActualMap := map[string]struct{}{}
+	for _, actualKey := range strings.Split(lines[0], ",") {
+		headerActualMap[actualKey] = struct{}{}
+	}
+	if len(headerActual) != len(headerExpected) {
+		s.Fatalf("Incorrect number of metrics: got=%d, want=%d", len(headerActualMap), len(headerExpected))
+	}
+	missing := []string{}
+	for _, he := range headerExpected {
+		ok := true
+		if _, ok = headerActualMap[he]; ok {
+			delete(headerActualMap, he)
+		}
+		if !ok {
+			missing = append(missing, he)
+		}
+	}
+	// This is the case where expected elements are missing.
+	if len(missing) > 0 {
+		s.Fatalf("Missing keys: %s", strings.Join(missing, ","))
+	}
+	// This is the case where there are actually more elements that there should be.
+	if len(headerActualMap) > 0 {
+		extra := []string{}
+		for _, v := range headerActualMap {
+			extra = append(extra, v)
+		}
+		s.Fatalf("Found unexpected keys: %s", strings.Join(extra, ","))
+	}
+	metrics := strings.Split(lines[1], ",")
+	if len(metrics) != len(headerExpected) {
+		s.Fatalf("Incorrect number of battery metrics: got=%d, want=%d", len(metrics), len(headerExpected))
+	}
+}
