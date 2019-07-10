9c2f6be4c0650fe11ef80d0b7b2a908b8648cc35
Xiaoqian Dai <xdai@chromium.org>
2019-07-10 16:44:03 -0700

    tast-tests: add ash window state test for ARC app windows.
    BUG=chromium:983251
    TEST=tast -verbose run $DUT arc.AshWindowState
    
    Change-Id: Iaf98b6a0fdafa1ec2bf1ffbb5bbec2f99acff854
    Reviewed-on: https://chromium-review.googlesource.com/1698494
    Tested-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arc/ash_window_state.go
@@ -0,0 +1,82 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AshWindowState,
+		Desc:         "Checks that sending Ash WM event will change ARC app window state correctly",
+		Contacts:     []string{"xdai@chromium.org", "arc-framework+tast@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android_p", "chrome"},
+		Pre:          arc.Booted(),
+		Timeout:      5 * time.Minute,
+	})
+}
+
+func AshWindowState(ctx context.Context, s *testing.State) {
+	a := s.PreValue().(arc.PreData).ARC
+
+	cr := s.PreValue().(arc.PreData).Chrome
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	// Restore tablet mode to its original state on exit.
+	tabletModeEnabled, err := ash.TabletModeEnabled(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get tablet mode: ", err)
+	}
+	defer ash.SetTabletModeEnabled(ctx, tconn, tabletModeEnabled)
+
+	// Force Chrome to be in clamshell mode to test window state related functionalities.
+	// TODO(xdai): Test window state functionalities in tablet mode as well.
+	if err := ash.SetTabletModeEnabled(ctx, tconn, false); err != nil {
+		s.Fatal("Failed to disable tablet mode: ", err)
+	}
+
+	// Start the Settings app.
+	const pkg = "com.android.settings"
+	act, err := arc.NewActivity(a, pkg, ".Settings")
+	if err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx); err != nil {
+		s.Fatal("Failed to start the Settings activity: ", err)
+	}
+
+	for _, test := range []struct {
+		wmEvent             ash.WMEventType
+		expectedWindowState ash.WindowStateType
+	}{
+		{ash.WMEventNormal, ash.WindowStateNormal},
+		{ash.WMEventMaximize, ash.WindowStateMaximized},
+		{ash.WMEventMinimize, ash.WindowStateMinimized},
+		{ash.WMEventFullscreen, ash.WindowStateFullscreen},
+		{ash.WMEventSnapLeft, ash.WindowStateLeftSnapped},
+		{ash.WMEventSnapRight, ash.WindowStateRightSnapped},
+	} {
+		s.Logf("Sending event %s to Settings app", test.wmEvent)
+
+		if state, err := ash.SetARCAppWindowState(ctx, tconn, pkg, test.wmEvent); err != nil {
+			s.Errorf("Failed to set window state to %s for Settings app", test.expectedWindowState)
+		} else if state != test.expectedWindowState {
+			s.Errorf("Unexpected window state: got %s; want %s", state, test.expectedWindowState)
+		}
+	}
+}
