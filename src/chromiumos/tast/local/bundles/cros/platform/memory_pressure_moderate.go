ba78c4f6e33a5ec2b4062500d5efae34538b23be
Kuo-Hsin Yang <vovoy@chromium.org>
2019-07-10 15:54:29 +0800

    tast-tests: add platform.memoryPressureModerate
    It's hard to compare performance under pressure for different memory
    setup with platform.MemoryPressure as the open tab count is different.
    Add a test platform.MemoryPressureModerate to check performance under
    fixed tab count.
    
    BUG=chromium:982608
    TEST=ran this test
    
    Change-Id: Ib8f3be10c3946eada45d9c0079133ba257d8e9fb

---
+++ src/chromiumos/tast/local/bundles/cros/platform/memory_pressure_moderate.go
@@ -0,0 +1,59 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/platform/kernelmeter"
+	"chromiumos/tast/local/bundles/cros/platform/mempressure"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     MemoryPressureModerate,
+		Desc:     "Measure tab switching performance under moderate memory pressure.",
+		Contacts: []string{"vovoy@chromium.org", "chromeos-memory@google.com"},
+		Timeout:  180 * time.Minute,
+		Data: []string{
+			mempressure.CompressibleData,
+			mempressure.DormantCode,
+			mempressure.WPRArchiveName,
+		},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// MemoryPressureModerate is the main test function.
+func MemoryPressureModerate(ctx context.Context, s *testing.State) {
+	memInfo, err := kernelmeter.MemInfo()
+	if err != nil {
+		s.Fatal("Cannot obtain memory info: ", err)
+	}
+
+	// One tab consumes about 200 MB on average. Set a fixed tab count
+	// to consume about 1.25x of the total memory.
+	var maxTab int
+	if memInfo.Total < kernelmeter.NewMemSizeMiB(2 * 1024) {
+		maxTab = 13
+	} else if memInfo.Total < kernelmeter.NewMemSizeMiB(4 * 1024) {
+		maxTab = 25
+	} else if memInfo.Total < kernelmeter.NewMemSizeMiB(8 * 1024) {
+		maxTab = 50
+	} else {
+		maxTab = 100
+	}
+
+	p := &mempressure.RunParameters{
+		DormantCodePath:          s.DataPath(mempressure.DormantCode),
+		PageFilePath:             s.DataPath(mempressure.CompressibleData),
+		PageFileCompressionRatio: 0.40,
+		WPRArchivePath:           s.DataPath(mempressure.WPRArchiveName),
+		MaxTabCount:		  maxTab,
+	}
+	mempressure.Run(ctx, s, p)
+}
