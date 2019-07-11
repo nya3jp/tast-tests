e0f9c6d00ef20fb82efaa2d0c67be57343712063
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2019-07-11 18:24:01 +0900

    tast-test: Port _test_reporter_startup in logging_UserCrash.
    from //src/third_party/autotest/files/client/site_tests/logging_UserCrash/logging_UserCrash.py
    
    BUG=chromium:970930
    TEST=tast run $DUT platform.UserCrash
    
    Change-Id: I02b7cf6889b73a9fc8f00f2fc0bd9e8f4fa676ec

---
+++ src/chromiumos/tast/local/bundles/cros/platform/crash/crash.go
@@ -0,0 +1,172 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crash
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+const (
+	// CorePattern is the full path of the core pattern file.
+	CorePattern = "/proc/sys/kernel/core_pattern"
+
+	// CrashReporterPath is the full path of the crash reporter binary.
+	CrashReporterPath = "/sbin/crash_reporter"
+
+	fallbackUserCrashDir = "/home/chronos/crash"
+	pauseFile            = "/var/lib/crash_sender_paused"
+	systemCrashDir       = "/var/spool/crash"
+	userCrashDirs        = "/home/chronos/u-*/crash"
+)
+
+// enableSystemSending allows to run system crash_sender.
+func enableSystemSending() error {
+	if err := os.Remove(pauseFile); err != nil && !os.IsNotExist(err) {
+		return errors.Wrapf(err, "failed to remove pause file %s", pauseFile)
+	}
+	return nil
+}
+
+// disableSystemSending disallows to run system crash_sender.
+func disableSystemSending() error {
+	if f, err := os.Stat(pauseFile); err != nil {
+		if !os.IsNotExist(err) {
+			return errors.Wrapf(err, "failed to stat %s", pauseFile)
+		}
+		// Create policy file that enables metrics/consent.
+		if err := ioutil.WriteFile(pauseFile, []byte{}, 0644); err != nil {
+			return errors.Wrap(err, "failed to create pause file")
+		}
+	} else {
+		if !f.Mode().IsRegular() {
+			return errors.Errorf("%s was not a regular file", pauseFile)
+		}
+	}
+	return nil
+}
+
+// replaceCrashFilterIn replaces --filter_in= flag value of the crash reporter.
+// When param is an empty string, the flag will be removed.
+// The kernel is set up to call the crash reporter with the core dump as stdin
+// when a process dies. This function adds a filter to the command line used to
+// call the crash reporter. This is used to ignore crashes in which we have no
+// interest.
+func replaceCrashFilterIn(param string) error {
+	b, err := ioutil.ReadFile(CorePattern)
+	if err != nil {
+		return errors.Wrapf(err, "failed reading core pattern file %s", CorePattern)
+	}
+	pattern := string(b)
+	if !strings.HasPrefix(pattern, "|") {
+		return errors.Wrapf(err, "pattern should start with '|', but was: %s", pattern)
+	}
+	e := strings.Split(strings.TrimSpace(pattern), " ")
+	var newargs []string
+	replaced := false
+	for _, s := range e {
+		if !strings.HasPrefix(s, "--filter_in=") {
+			newargs = append(newargs, s)
+			continue
+		}
+		if len(param) == 0 {
+			// remove from list
+			continue
+		}
+		newargs = append(newargs, "--filter_in="+strconv.Quote(param))
+		replaced = true
+	}
+	if len(param) != 0 && !replaced {
+		newargs = append(newargs, "--filter_in="+strconv.Quote(param))
+	}
+	pattern = strings.Join(newargs, " ")
+
+	if err := ioutil.WriteFile(CorePattern, []byte(pattern), 0644); err != nil {
+		return errors.Wrapf(err, "failed writing core pattern file %s", CorePattern)
+	}
+	return nil
+}
+
+// DisableCrashFiltering removes the --filter_in argument from the kernel core dump cmdline.
+// Next time the crash reporter is invoked (due to a crash) it will not receive a
+// --filter_in paramter.
+func DisableCrashFiltering() error {
+	return replaceCrashFilterIn("")
+}
+
+// clearSpooledCrashes clears system and user crash directories.
+// This removes all crash reports which are waiting to be sent.
+func clearSpooledCrashes() error {
+	matches, err := filepath.Glob(userCrashDirs)
+	if err != nil {
+		return errors.Wrapf(err, "failed globing user crash dirs %s", userCrashDirs)
+	}
+	for _, dir := range append(matches, systemCrashDir, fallbackUserCrashDir) {
+		if err := os.RemoveAll(dir); err != nil && !os.IsNotExist(err) {
+			return errors.Wrapf(err, "failed cleaning a crash dir %s", dir)
+		}
+	}
+	return nil
+}
+
+// initializeCrashReporter starts up the crash reporter.
+func initializeCrashReporter(ctx context.Context) error {
+	if err := testexec.CommandContext(ctx, CrashReporterPath, "--init").Run(); err != nil {
+		return errors.Wrap(err, "failed to initialize crash reporter")
+	}
+	// Completely disable crash_reporter from generating crash dumps
+	// while any tests are running, otherwise a crashy system can make
+	// these tests flaky.
+	return replaceCrashFilterIn("none")
+}
+
+// RunCrashTest runs a crash test case after setting up crash reporter.
+func RunCrashTest(ctx context.Context, s *testing.State, testFunc func(context.Context, *testing.State)) error {
+	// Prepare crasher.
+	// user_crash_test.py : _prepare_crasher
+	// Instead we will use binary delivery mechanism by TAST.
+
+	if err := initializeCrashReporter(ctx); err != nil {
+		return err
+	}
+	// Disable crash_sender from running, kill off any running ones.
+	// We set a flag to crash_sender when invoking it manually to avoid
+	// our invocations being paused.
+	if err := disableSystemSending(); err != nil {
+		return err
+	}
+	defer enableSystemSending()
+	// Ignore process-not-found error.
+	// TODO(yamaguchi): Refactor to this after Go version >= 1.12
+	// (*cmd.ProcessState).ExitCode()
+	if err := testexec.CommandContext(ctx, "pkill", "-9", "-e", "crash_sender").Run(); err != nil {
+		e, ok := err.(*exec.ExitError)
+		if !ok {
+			return errors.Wrap(err, "failed to get exit status from crash_sender")
+		}
+		s, ok := e.Sys().(syscall.WaitStatus)
+		if !ok {
+			return errors.Wrap(err, "failed to get exit status from crash_sender")
+		}
+		if s.ExitStatus() != 1 {
+			return errors.Wrap(err, "failed to kill crash_sender")
+		}
+	}
+	testFunc(ctx, s)
+	if err := clearSpooledCrashes(); err != nil {
+		return errors.Wrap(err, "failed to clear spooled crashes")
+	}
+	return nil
+}
