5be5c8a116ef393b9951e5e1b841362ac3ad4acc
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2019-07-11 18:24:01 +0900

    tast-test: Port _test_reporter_starup in logging_UserCrash.
    from //src/third_party/autotest/files/client/site_tests/logging_UserCrash/logging_UserCrash.py
    
    BUG=chromium:970930
    TEST=tast run $DUT platform.UserCrash
    
    Change-Id: I02b7cf6889b73a9fc8f00f2fc0bd9e8f4fa676ec

--- src/chromiumos/tast/local/bundles/cros/platform/crash/crash.go
+++ src/chromiumos/tast/local/bundles/cros/platform/crash/crash.go
@@ -10,15 +10,28 @@ package crash
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/fsutil"
 	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
 const (
+	// CorePattern is the full path of the core pattern file.
+	CorePattern = "/proc/sys/kernel/core_pattern"
+
+	// CrashReporterPath is the full path of the crash reporter binary.
+	CrashReporterPath = "/sbin/crash_reporter"
+
 	// MockMetricsOnPolicyFile is the name of the mock data file to indicate
 	// having consent to send crash reports.
 	// A test which calls SetConsent should load this file.
@@ -28,7 +41,72 @@ const (
 	// policy blob.
 	// A test which calls SetConsent should load this file.
 	MockMetricsOwnerKeyFile = "crash_tests_mock_metrics_owner.key"
+
+	fallbackUserCrashDir = "/home/chronos/crash"
+	pauseFile            = "/var/lib/crash_sender_paused"
+	systemCrashDir       = "/var/spool/crash"
+	userCrashDirs        = "/home/chronos/u-*/crash"
 )
+
+func enableSystemSending() error {
+	if err := os.Remove(pauseFile); err != nil && !os.IsNotExist(err) {
+		return errors.Wrapf(err, "failed to remove pause file %s", pauseFile)
+	}
+	return nil
+}
+
+func disableSystemSending() error {
+	if f, err := os.Stat(pauseFile); err != nil {
+		if !os.IsNotExist(err) {
+			return errors.Wrapf(err, "failed to stat %s", pauseFile)
+		}
+		// Create policy file that enables metrics/consent.
+		f, err := os.Create(pauseFile)
+		if err != nil {
+			return errors.Wrap(err, "failed to create pause file")
+		}
+		f.Close()
+	} else {
+		if !f.Mode().IsRegular() {
+			return errors.Errorf("%s was not a regular file", pauseFile)
+		}
+	}
+	return nil
+}
+
+// replaceCrashFilterIn replaces --filter_in= flag value of the crash reporter.
+// When param is an empty string, the flag will be removed.
+// The kernel is set up to call the crash reporter with the core dump as stdin
+// when a process dies. This function adds a filter to the command line used to
+// call the crash reporter. This is used to ignore crashes in which we have no
+// interest. The order of the commandline arguments will not be preserved.
+func replaceCrashFilterIn(param string) error {
+	b, err := ioutil.ReadFile(CorePattern)
+	if err != nil {
+		return errors.Wrapf(err, "failed reading core pattern file %s", CorePattern)
+	}
+	pattern := string(b)
+	if !strings.HasPrefix(pattern, "|") {
+		return errors.Wrapf(err, "pattern should start with '|', but was: %s", pattern)
+	}
+	re := regexp.MustCompile(`--filter_in=\S*\s*`)
+	pattern = strings.TrimSpace(re.ReplaceAllString(pattern, ""))
+	if len(param) != 0 {
+		pattern = fmt.Sprintf("%s --filter_in=%s", pattern, strconv.Quote(param))
+	}
+
+	if err := ioutil.WriteFile(CorePattern, []byte(pattern), 0644); err != nil {
+		return errors.Wrapf(err, "failed writing core pattern file %s", CorePattern)
+	}
+	return nil
+}
+
+// DisableCrashFiltering removes the --filter_in argument from the kernel core dump cmdline.
+// Next time the crash reporter is invoked (due to a crash) it will not receive a
+// --filter_in paramter.
+func DisableCrashFiltering() error {
+	return replaceCrashFilterIn("")
+}
 
 // SetConsent emulates the state where we have consent to send crash reports.
 // This creates the file to control whether crash_sender will consider that it
@@ -68,5 +146,62 @@ func SetConsent(ctx context.Context, mockPolicyFilePath string, mockKeyFilePath 
 		return err
 	}
 	testing.ContextLog(ctx, "Created ", consentFile)
+	return nil
+}
+
+// clearSpooledCrashes clears system and user crash directories.
+// This removes all crash reports which are waiting to be sent.
+func clearSpooledCrashes() error {
+	matches, err := filepath.Glob(userCrashDirs)
+	if err != nil {
+		return errors.Wrapf(err, "failed globing user crash dirs %s", userCrashDirs)
+	}
+	for _, match := range matches {
+		if err := os.Remove(match); err != nil && !os.IsNotExist(err) {
+			return errors.Wrap(err, "failed cleaning a user crash dir")
+		}
+	}
+	if err := os.Remove(fallbackUserCrashDir); err != nil && !os.IsNotExist(err) {
+		return errors.Wrapf(err, "failed cleaning fallback user crash dir %s", fallbackUserCrashDir)
+	}
+	return nil
+}
+
+// initializeCrashReporter starts up the crash reporter.
+func initializeCrashReporter(ctx context.Context) error {
+	if err := testexec.CommandContext(ctx, CrashReporterPath, "--init").Run(); err != nil {
+		return errors.Wrap(err, "failed to initialize crash reporter")
+	}
+	// Completely disable crash_reporter from generating crash dumps
+	// while any tests are running, otherwise a crashy system can make
+	// these tests flaky.
+	return replaceCrashFilterIn("none")
+}
+
+// Setup configures systems related to crash reporter before running a test case.
+func Setup(ctx context.Context) error {
+	if err := initializeCrashReporter(ctx); err != nil {
+		return err
+	}
+	// Disable crash_sender from running, kill off any running ones.
+	// We set a flag to crash_sender when invoking it manually to avoid
+	// our invocations being paused.
+	if err := disableSystemSending(); err != nil {
+		return err
+	}
+	if err := testexec.CommandContext(ctx, "pkill", "-9", "-e", "crash_sennnnnnder").Run(); err != nil {
+		return err
+	}
+	if err := clearSpooledCrashes(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// TearDown resets some system state after test case run.
+func TearDown(ctx context.Context) error {
+	if err := enableSystemSending(); err != nil {
+		return err
+	}
 	return nil
 }
---
+++ src/chromiumos/tast/local/bundles/cros/platform/user_crash.go
@@ -0,0 +1,73 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"chromiumos/tast/local/bundles/cros/platform/crash"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+const (
+	leaveCorePath = "/root/.leave_core"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: UserCrash,
+		Desc: "Verifies crash reporting for user processes",
+		Contacts: []string{
+			"domlaskowski@chromium.org", // Original autotest author
+			"yamaguchi@chromium.org",    // Tast port author
+		},
+		Attr: []string{"informational"},
+	})
+}
+
+func umountRoot(ctx context.Context, s *testing.State) {
+	if err := testexec.CommandContext(ctx, "umount", "/root").Run(testexec.DumpLogOnError); err != nil {
+		s.Error("Failed to unmount: ", err)
+	}
+}
+
+// testReporterStartup tests that the core_pattern is set up by crash reporter.
+func testReporterStartup(ctx context.Context, s *testing.State) {
+	// Turn off crash filtering so we see the original setting.
+	if err := crash.DisableCrashFiltering(); err != nil {
+		s.Fatal("Failed to turn off crash filtering: ", err)
+	}
+	out, err := ioutil.ReadFile(crash.CorePattern)
+	if err != nil {
+		s.Fatal("Failed to read core pattern file: ", crash.CorePattern)
+	}
+	trimmed := strings.TrimSuffix(string(out), "\n")
+	expectedCorePattern := fmt.Sprintf("|%s --user=%%P:%%s:%%u:%%g:%%e", crash.CrashReporterPath)
+	if trimmed != expectedCorePattern {
+		s.Errorf("core pattern should have been %s, not %s", expectedCorePattern, trimmed)
+	}
+
+	// Find log line of crash_reporter during the last boot.
+	out, err = testexec.CommandContext(ctx, "journalctl", "-b", "0", "-q", "-t", "crash_reporter",
+		"-g", "Enabling user crash handling").Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to execute and get output result of journalctl: ", err)
+	}
+	if len(out) == 0 {
+		s.Error("user space crash handling was not started during last boot")
+	}
+}
+
+func UserCrash(ctx context.Context, s *testing.State) {
+	crash.Setup(ctx)
+	// TODO(crrev.com/970930): Restart UI if exist, when adding tests that
+	// require it.
+	testReporterStartup(ctx, s)
+	crash.TearDown(ctx)
+}
