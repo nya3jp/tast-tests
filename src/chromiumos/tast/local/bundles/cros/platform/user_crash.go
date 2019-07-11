18e520f6632cdcc32953a0c1dcd7b74ceb93cf95
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2019-07-11 18:24:01 +0900

    tast-test: Port _test_reporter_startup in logging_UserCrash.
    from //src/third_party/autotest/files/client/site_tests/logging_UserCrash/logging_UserCrash.py
    
    BUG=chromium:970930
    TEST=tast run $DUT platform.UserCrash
    
    Change-Id: I02b7cf6889b73a9fc8f00f2fc0bd9e8f4fa676ec

---
+++ src/chromiumos/tast/local/bundles/cros/platform/user_crash.go
@@ -0,0 +1,102 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/platform/crash"
+	// "chromiumos/tast/local/metrics"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+const (
+	leaveCorePath            = "/root/.leave_core"
+	crashReporterEnabledPath = "/var/lib/crash_reporter/crash-handling-enabled"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: UserCrash,
+		Desc: "Verifies crash reporting for user processes",
+		Contacts: []string{
+			"domlaskowski@chromium.org", // Original autotest author
+			"yamaguchi@chromium.org",    // Tast port author
+		},
+		Attr: []string{"informational"},
+	})
+}
+
+func umountRoot(ctx context.Context, s *testing.State) {
+	if err := testexec.CommandContext(ctx, "umount", "/root").Run(testexec.DumpLogOnError); err != nil {
+		s.Error("Failed to unmount: ", err)
+	}
+}
+
+func getUptime() (time.Duration, error) {
+	const uptimeFile = "/proc/uptime"
+	out, err := ioutil.ReadFile(uptimeFile)
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to read file: %s", uptimeFile)
+	}
+	seconds, err := strconv.ParseFloat(strings.Split(string(out), " ")[0], 64)
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to parse content of uptime file: %s", out)
+	}
+	return time.Duration(seconds * 1e9), nil // in nanoseconds
+}
+
+// testReporterStartup tests that the core_pattern is set up by crash reporter.
+func testReporterStartup(ctx context.Context, s *testing.State) {
+	// Turn off crash filtering so we see the original setting.
+	if err := crash.DisableCrashFiltering(); err != nil {
+		s.Fatal("Failed to turn off crash filtering: ", err)
+	}
+	out, err := ioutil.ReadFile(crash.CorePattern)
+	if err != nil {
+		s.Fatal("Failed to read core pattern file: ", crash.CorePattern)
+	}
+	trimmed := strings.TrimSuffix(string(out), "\n")
+	expectedCorePattern := fmt.Sprintf("|%s --user=%%P:%%s:%%u:%%g:%%e", crash.CrashReporterPath)
+	if trimmed != expectedCorePattern {
+		s.Errorf("Unexpected core_pattern: got %s, want %s", trimmed, expectedCorePattern)
+	}
+
+	// Check that we wrote out the file indicating that crash_reporter is
+	// enabled AFTER the system was booted. This replaces the old technique
+	// of looking for the log message which was flakey when the logs got
+	// flooded.
+	// NOTE: This technique doesn't need to be highly accurate, we are only
+	// verifying that the flag was written after boot and there are multiple
+	// seconds between those steps, and a file from a prior boot will almost
+	// always have been written out much further back in time than our
+	// current boot time.
+	f, err := os.Stat(crashReporterEnabledPath)
+	if err != nil || !f.Mode().IsRegular() {
+		s.Fatal("Crash reporter enabled file flag is not present at ", crashReporterEnabledPath)
+	}
+	flagTime := time.Since(f.ModTime())
+	uptime, err := getUptime()
+	if err != nil {
+		s.Fatal("Failed to get uptime: ", err)
+	}
+	if flagTime > uptime {
+		s.Error("User space crash handling was not started during last boot")
+	}
+}
+
+func UserCrash(ctx context.Context, s *testing.State) {
+	// TODO(crrev.com/970930): Restart UI if exist, when adding tests that
+	// require it.
+	crash.RunCrashTest(ctx, s, testReporterStartup)
+}
