0fc20af100696fecececd0ca73bc30b0760dfb5d
Kelsey Deuth <kelsey.deuth@intel.com>
2019-07-12 14:05:29 -0700

    camera.CCAUIPreview: Introduce preview performance measurement
    This commit introduces a new test to measure the overall CPU
    utilization during camera preview. Tracking a seemingly straightforward
    scenario helps establish baselines for further analyzing complex use
    cases. Measurements are taken with the Chrome Camera Application
    fullscreened.
    
    Example data collected on a dev test build for Chrome 77.0.3836 and
    platform version 12314.0.0.
    | Board    | CPU Utilization (%) |
    | nocturne | 42                  |
    | electro  | 52                  |
    
    BUG=chromium:989388
    TEST=`tast run ${DUT_IP} camera.CCAUIPreviewPerf`
         - Verify test passes.
         - Verify CPU utilization numbers are stored in
           /tmp/tast/results/latest/tests/camera.CCAUIPreviewPerf/results-chart.json
    
    Change-Id: I03651f3377e7544790ae0bc8d0c891edcf1ecf51
    Signed-off-by: Kelsey Deuth <kelsey.deuth@intel.com>

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_preview_perf.go
@@ -0,0 +1,115 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/camera/cca"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/cpu"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUIPreviewPerf,
+		Desc:         "Opens CCA and measures the CPU utilization",
+		Contacts:     []string{"shik@chromium.org", "kelsey.deuth@intel.com", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", caps.BuiltinCamera},
+		Data:         []string{"cca_ui.js"},
+		Pre:          chrome.LoggedIn(),
+		Timeout:      3 * time.Minute,
+	})
+
+}
+
+// CCAUIPreviewPerf launches the Chrome Camera App, waits for camera preview, fullscreens the
+// application and starts measuring system CPU utilization.
+func CCAUIPreviewPerf(ctx context.Context, s *testing.State) {
+	// Duration of the interval during which CPU usage will be measured.
+	const measureDuration = 20 * time.Second
+
+	cr := s.PreValue().(*chrome.Chrome)
+
+	if err := cpu.WaitUntilIdle(ctx); err != nil {
+		s.Fatal("Failed to idle", err)
+	}
+
+	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")})
+	if err != nil {
+		s.Fatal("Failed to open CCA: ", err)
+	}
+	defer app.Close(ctx)
+
+	if err := app.WaitForVideoActive(ctx); err != nil {
+		s.Fatal("Preview is inactive after launching App: ", err)
+	}
+	s.Log("Preview started")
+
+	cleanUpBenchmark, err := cpu.SetUpBenchmark(ctx)
+	if err != nil {
+		s.Fatal("Failed to set up benchmark mode: ", err)
+	}
+	defer cleanUpBenchmark(ctx)
+
+	cpuUsage, err := measureUtilization(ctx, app, measureDuration)
+	if err != nil {
+		s.Error("Failed in testScreen(): ", err)
+		if err := app.Restart(ctx); err != nil {
+			s.Fatal("Failed to restart CCA: ", err)
+		}
+	}
+	s.Log("Measured CPU utilization: ", cpuUsage)
+
+	pv := perf.NewValues()
+	pv.Set(perf.Metric{
+		Name:      "cpu_usage",
+		Unit:      "percent",
+		Direction: perf.SmallerIsBetter,
+	}, cpuUsage)
+	if err := pv.Save(s.OutDir()); err != nil {
+		s.Error("Failed to save perf data: ", err)
+	}
+}
+
+func measureUtilization(ctx context.Context, app *cca.App, measureDuration time.Duration) (float64, error) {
+	restore := func() error {
+		if err := app.RestoreWindow(ctx); err != nil {
+			return errors.Wrap(err, "failed to restore window")
+		}
+		if err := app.WaitForVideoActive(ctx); err != nil {
+			return errors.Wrap(err, "preview is inactive after restoring window")
+		}
+		return nil
+	}
+	if err := restore(); err != nil {
+		return 0, err
+	}
+
+	testing.ContextLog(ctx, "Fullscreening window")
+	if err := app.FullscreenWindow(ctx); err != nil {
+		return 0, errors.Wrap(err, "failed to fullscreen window")
+	}
+	if err := app.WaitForVideoActive(ctx); err != nil {
+		return 0, errors.Wrap(err, "preview is inactive after fullscreening window")
+	}
+
+	testing.ContextLog(ctx, "Measuring CPU utilization for ", measureDuration)
+	cpuUsage, err := cpu.MeasureUsage(ctx, measureDuration)
+	if err != nil {
+		return 0, errors.Wrap(err, "Failed to measure CPU usage")
+	}
+	if err := restore(); err != nil {
+		return 0, errors.Wrap(err, "failed in restore() after fullscreening window")
+	}
+
+	return cpuUsage, nil
+}
