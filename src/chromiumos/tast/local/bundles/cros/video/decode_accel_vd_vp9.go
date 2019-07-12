5223d1a5086499b8440b26c7ade6abb13f3f3046
David Staessens <dstaessens@chromium.org>
2019-07-12 15:25:38 +0900

    tast.video.DecodeAccelVP*: Add VP8/9 decoder tests for new VD implementations.
    This CL adds VP8, VP9 and VP9.2 tast tests for the new VD-based video decoder
    implementations.
    
    TEST=video.DecodeAccelVP* on eve
    
    BUG=chromium:954855
    
    Change-Id: Ie3fcbc305c10ef51b06763782bb5c02e4d1e42b7

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_vp9.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/videotype"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     DecodeAccelVDVP9,
+		Desc:     "Runs Chrome video_decode_accelerator_tests with an VP9 video on a media::VideoDecoder",
+		Contacts: []string{"acourbot@chromium.org", "dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:     []string{"informational"},
+		// TODO(b/137916185): Remove dependency on android capability. It's used here
+		// to guarantee import-mode support, which is required by the new VD's.
+		SoftwareDeps: []string{"android", "chrome", caps.HWDecodeVP9},
+		Data:         decode.DataFiles(videotype.VP9Prof),
+	})
+}
+
+// DecodeAccelVDVP9 runs the video_decode_accelerator_tests with test-25fps.vp9 against
+// the new video decoders based on the media::VideoDecoder interface (see go/vd-migration).
+func DecodeAccelVDVP9(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoTestNew(ctx, s, decode.Test25FPSVP9.Name, decode.VD)
+}
