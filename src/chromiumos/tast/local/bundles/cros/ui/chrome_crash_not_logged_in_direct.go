51e0918b05ab0914833035526566982dc3bbf36e
Ian Barkley-Yeung <iby@google.com>
2019-07-15 16:47:34 -0700

    crash: Tast test for crash loop reporting
    Add a tast test to verify that crash-loop crash reporting works
    correctly.
    
    BUG=chromium:857317
    TEST=Ran tast test
    
    Cq-Depend: chromium:1730245, chromium:1730246, chromium:1737722, chromium:1740512
    Change-Id: I8d556f427db0adbcb5790fee430c8352a24380fd

--- src/chromiumos/tast/local/bundles/cros/ui/chrome_crash_not_logged_in_direct.go
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_crash_not_logged_in_direct.go
@@ -6,7 +6,6 @@ package ui
 
 import (
 	"context"
-	"time"
 
 	"chromiumos/tast/local/bundles/cros/ui/chromecrash"
 	"chromiumos/tast/local/chrome"
@@ -33,14 +32,6 @@ func ChromeCrashNotLoggedInDirect(ctx context.Context, s *testing.State) {
 		s.Fatal("Chrome startup failed: ", err)
 	}
 	defer cr.Close(ctx)
-
-	// Sleep briefly as a speculative workaround for Chrome hangs that are occasionally seen
-	// when this test sends SIGSEGV to Chrome soon after it starts: https://crbug.com/906690
-	const delay = 3 * time.Second
-	s.Logf("Sleeping %v to wait for Chrome to stabilize", delay)
-	if err := testing.Sleep(ctx, delay); err != nil {
-		s.Fatal("Timed out while waiting for Chrome startup: ", err)
-	}
 
 	if dumps, err := chromecrash.KillAndGetCrashFiles(ctx); err != nil {
 		s.Fatal("Couldn't kill Chrome or get dumps: ", err)
