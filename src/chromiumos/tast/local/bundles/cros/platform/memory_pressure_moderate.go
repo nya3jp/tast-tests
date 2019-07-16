bfadd050554c5d4dc65c272944517c25ac7904c2
Kuo-Hsin Yang <vovoy@chromium.org>
2019-07-16 16:11:24 +0800

    platform.memoryPressureModerate: add runtime flag maxTab
    Runtime flag platform.memoryPressureModerate.maxTab sets the maximal tab
    count. It's for manual test only.
    
    BUG=chromium:982608
    TEST=tast run -var=platform.memoryPressureModerate.maxTab=8 DUT_IP platform.MemoryPressureModerate
    
    Change-Id: Ib3787a2a51df3e23135dda03bc738d40d9433408

--- src/chromiumos/tast/local/bundles/cros/platform/memory_pressure_moderate.go
+++ src/chromiumos/tast/local/bundles/cros/platform/memory_pressure_moderate.go
@@ -6,6 +6,7 @@ package platform
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"chromiumos/tast/local/bundles/cros/platform/kernelmeter"
@@ -26,28 +27,43 @@ func init() {
 			mempressure.WPRArchiveName,
 		},
 		SoftwareDeps: []string{"chrome"},
+		Vars:         []string{"platform.MemoryPressureModerate.maxTab"},
 	})
 }
 
-// MemoryPressureModerate is the main test function.
 func MemoryPressureModerate(ctx context.Context, s *testing.State) {
-	memInfo, err := kernelmeter.MemInfo()
-	if err != nil {
-		s.Fatal("Cannot obtain memory info: ", err)
-	}
-
-	// One tab consumes about 200 MB on average. Set a fixed tab count
-	// to consume about 1.25x of the total memory.
 	var maxTab int
-	if memInfo.Total < kernelmeter.NewMemSizeMiB(2*1024) {
-		maxTab = 13
-	} else if memInfo.Total < kernelmeter.NewMemSizeMiB(4*1024) {
-		maxTab = 25
-	} else if memInfo.Total < kernelmeter.NewMemSizeMiB(8*1024) {
-		maxTab = 50
+
+	// Check runtime flag maxTab to specify maximal tab count.
+	if val, ok := s.Var("platform.MemoryPressureModerate.maxTab"); ok {
+		s.Log("The number of maxTab is specified via runtime variable")
+
+		tabs, err := strconv.Atoi(val)
+		if err != nil {
+			s.Fatal("Cannot parse argument platform.MemoryPressureModerate.maxTab: ", err)
+		}
+		maxTab = tabs
 	} else {
-		maxTab = 100
+		s.Log("Inferring the number of maxTab from the memory size")
+
+		memInfo, err := kernelmeter.MemInfo()
+		if err != nil {
+			s.Fatal("Cannot obtain memory info: ", err)
+		}
+
+		// One tab consumes about 200 MB on average. Set a fixed tab count
+		// to consume about 1.25x of the total memory.
+		if memInfo.Total < kernelmeter.NewMemSizeMiB(2*1024) {
+			maxTab = 13
+		} else if memInfo.Total < kernelmeter.NewMemSizeMiB(4*1024) {
+			maxTab = 25
+		} else if memInfo.Total < kernelmeter.NewMemSizeMiB(8*1024) {
+			maxTab = 50
+		} else {
+			maxTab = 100
+		}
 	}
+	s.Log("Maximal tab count: ", maxTab)
 
 	p := &mempressure.RunParameters{
 		DormantCodePath:          s.DataPath(mempressure.DormantCode),
