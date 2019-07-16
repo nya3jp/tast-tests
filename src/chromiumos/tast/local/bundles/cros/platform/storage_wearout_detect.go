7eef162e726b651168a3966ec9a5ee93753ba277
Brooke Mylander <brooke.mylander@intel.com>
2019-07-16 13:14:13 -0700

    platform: Introduce storage utility and wearout test
    This commit ports functionality from Autotest's
    hardware_StorageWearoutDetect to a new storage package. As with the
    previous Autotest, information is scraped from SMART (SSD, NVMe) and
    the extended CSD register (eMMC). To test the new package, data was
    pulled from a lulu (SSD), nocturne (eMMC), and an i7 eve (NVMe).
    
    Health analysis summary:
      - SSD devices will trigger failures if the FAIL column in SMART
        attributes is "NOW".
      - eMMC devices (>5.0) will trigger failures if
        DEVICE_LIFE_TIME_EST_TYPE_B or TYPE_A is in the 90-100% band or
        higher.
      - NVMe devices will trigger failures if the SMART health information
        percentage used field is above 97%.
    
    BUG=chromium:990517
    TEST=`../platform/tast/fast_build.sh -T`
         - Verify all tests pass.
    TEST=`tast run ${DUT_IP} platform.StorageWearoutDetect`
         - Verify that test passes.
         - If it fails, your Chromebook drive may be approaching failure.
    Cq-Depend: chromium:1769740
    
    Change-Id: I848e22473f3f8f6660abc65ea1e39a4865ac697c
    Signed-off-by: Brooke Mylander <brooke.mylander@intel.com>

---
+++ src/chromiumos/tast/local/bundles/cros/platform/storage_wearout_detect.go
@@ -0,0 +1,36 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/platform/storage"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: StorageWearoutDetect,
+		Desc: "Fails if storage device information indicates impending failure",
+		Contacts: []string{
+			"puthik@chromium.org",       // Autotest author
+			"brooke.mylander@intel.com", // Migrated Autotest to Tast
+		},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"storage_wearout_detect"},
+	})
+}
+
+func StorageWearoutDetect(ctx context.Context, s *testing.State) {
+	info, err := storage.Get(ctx)
+	if err != nil {
+		s.Fatal("Failed to get storage info: ", err)
+	}
+
+	if info.Status == storage.Failing {
+		s.Error("Storage device is failing, consider removing from DUT farm")
+	}
+}
