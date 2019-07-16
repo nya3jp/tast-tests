2187a11312a85844d2ad4df3d639d21152d7c54a
Brooke Mylander <brooke.mylander@intel.com>
2019-07-16 13:14:13 -0700

    platform: Introduce storage utility and wearout test
    This commit ports functionality from Autotest's
    hardware_StorageWearoutDetect to a new storage package. As with the
    previous Autotest, information is scraped from SMART (SSD, NVMe) and
    the extended CSD register (eMMC). To test the new package, data was
    pulled from a lulu (SSD), nocturne (eMMC), and an i7 eve (NVMe).
    
    Health analysis summary:
      - SSD devices will trigger failures if the FAIL column in SMART
        attributes is "NOW".
      - eMMC devices (>5.0) will trigger failures if
        DEVICE_LIFE_TIME_EST_TYPE_B or TYPE_A is in the 90-100% band or
        higher.
      - NVMe devices will trigger failures if the SMART health information
        percentage used field is above 97%.
    
    BUG=chromium:990517
    TEST=`../platform/tast/fast_build.sh -T`
         - Verify all tests pass.
    TEST=`tast run ${DUT_IP} platform.StorageWearoutDetect`
         - Verify that test passes.
         - If it fails, your Chromebook drive may be approaching failure.
    
    Cq-Depend: chromium:1769740
    Change-Id: I848e22473f3f8f6660abc65ea1e39a4865ac697c
    Signed-off-by: Brooke Mylander <brooke.mylander@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1733745
    Tested-by: Edward Baker <edward.baker@intel.com>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/platform/storage/storage.go
@@ -0,0 +1,209 @@
+// Copyright 2019 The Chromium OS Authors.i All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package storage reports information retrieved from storage-info-common.sh on behalf of tests.
+package storage
+
+import (
+	"bytes"
+	"context"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+)
+
+// Type stands for various Chromebook storage devices.
+type Type int
+
+// LifeStatus stands for a simplified overview of device health.
+type LifeStatus int
+
+const (
+	// EMMC (Embedded Multi-Media Controller) devices are a single package flash storage and controller.
+	EMMC Type = iota
+	// NVMe (Non-Volatile Memory Express) interface. PCIe cards, but more commonly M.2 in Chromebooks.
+	NVMe
+	// SSD (Solid State Drive) devices connected through a SATA interface.
+	SSD
+)
+
+const (
+	// Healthy means that the device does not indicate failure or limited remaining life time.
+	Healthy LifeStatus = iota
+	// Failing indicates the storage device failed or will soon.
+	Failing
+)
+
+// Info contains information about a storage device.
+type Info struct {
+	// Device contains the underlying hardware device type.
+	Device Type
+	// Failing contains a final assessment that the device failed or will fail soon.
+	Status LifeStatus
+}
+
+// Get runs the storage info shell script and returns its info.
+func Get(ctx context.Context) (*Info, error) {
+	cmd := testexec.CommandContext(ctx, "sh", "-c", ". /usr/share/misc/storage-info-common.sh; get_storage_info")
+	out, err := cmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to run storage info command")
+	}
+	return parseGetStorageInfoOutput(out)
+}
+
+// parseGetStorageInfoOutput parses the storage information to find the device type and life status.
+func parseGetStorageInfoOutput(out []byte) (*Info, error) {
+	out = bytes.TrimSpace(out)
+	if len(out) == 0 {
+		return nil, errors.New("get storage info did not produce output")
+	}
+
+	lines := strings.Split(string(out), "\n")
+
+	deviceType, err := parseDeviceType(lines)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse storage info for device type")
+	}
+
+	var lifeStatus LifeStatus
+	switch deviceType {
+	case EMMC:
+		lifeStatus, err = parseDeviceHealtheMMC(lines)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse eMMC health")
+		}
+	case NVMe:
+		lifeStatus, err = parseDeviceHealthNVMe(lines)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse NVMe health")
+		}
+	case SSD:
+		lifeStatus = parseDeviceHealthSSD(lines)
+	default:
+		return nil, errors.Errorf("parsing device health for type %v is not supported", deviceType)
+	}
+
+	return &Info{Device: deviceType, Status: lifeStatus}, nil
+}
+
+var (
+	// nvmeDetect detects if storage device is NVME using a regex.
+	// Example NVMe SMART text: "   SMART/Health Information (NVMe Log 0x02, NSID 0xffffffff)"
+	nvmeDetect = regexp.MustCompile(`\s*SMART.*NVMe Log`)
+	// ssdDetect detects if storage device is SSD using a regex.
+	// Example SSD ATA text, "      ATA Version is:   ACS-2 T13/2015-D revision 3"
+	ssdDetect = regexp.MustCompile(`\s*ATA Version`)
+	// emmcDetect detects if storage device is eMMC using a regex.
+	// Example eMMC CSD text, "  Extended CSD rev 1.8 (MMC 5.1)"
+	emmcDetect = regexp.MustCompile(`\s*Extended CSD rev.*MMC`)
+
+	// emmcVersion finds eMMC version of device using a regex.
+	// Example eMMC CSD text, "  Extended CSD rev 1.8 (MMC 5.1)".
+	emmcVersion = regexp.MustCompile(`\s*Extended CSD rev.*MMC (?P<version>\d+.\d+)`)
+	// emmcFailing detects if eMMC device is failing using a regex.
+	// Example CSD text containing life time registers. 0x0a means 90-100% band,
+	// 0x0b means over 100% band. Find none digits.
+	//   "Device life time estimation type B [DEVICE_LIFE_TIME_EST_TYP_B: 0x01]
+	//     i.e. 0% - 10% device life time used
+	//    Device life time estimation type A [DEVICE_LIFE_TIME_EST_TYP_A: 0x00]"
+	emmcFailing = regexp.MustCompile(`.*(?P<param>DEVICE_LIFE_TIME_EST_TYP_.): 0x0\D`)
+	// nvmeFailing detects if nvme is failing using a regex.
+	// Example NVMe usage text: "	Percentage Used:                        0%"
+	nvmeFailing = regexp.MustCompile(`\s*Percentage Used:\s*(?P<percentage>\d*)`)
+	// ssdFailing detects if ssd device is failing using a regex.
+	ssdFailing = regexp.MustCompile(`\s*(?P<param>\S+\s\S+)` + // ID and attribute name
+		`\s+[P-][O-][S-][R-][C-][K-]` + // Flags
+		`(\s+\d{3}){3}` + // Three 3-digit numbers
+		`\s+NOW`) // Fail indicator
+)
+
+// parseDeviceType searches outlines for storage device type.
+func parseDeviceType(outLines []string) (Type, error) {
+	for _, line := range outLines {
+		if nvmeDetect.MatchString(line) {
+			return NVMe, nil
+		}
+
+		if ssdDetect.MatchString(line) {
+			return SSD, nil
+		}
+
+		if emmcDetect.MatchString(line) {
+			return EMMC, nil
+		}
+	}
+
+	return 0, errors.New("failed to detect a device type")
+}
+
+// parseDeviceHealtheMMC analyzes eMMC for indications of failure. For additional information,
+// refer to JEDEC standard 84-B50 which describes the extended CSD register. In this case,
+// we focus on DEVICE_LIFE_TIME_EST_TYPE_B and TYPE_A registers.
+func parseDeviceHealtheMMC(outLines []string) (LifeStatus, error) {
+	// Device life estimates were introduced in version 5.0
+	const emmcMinimumVersion = 5.0
+
+	for _, line := range outLines {
+		match := emmcVersion.FindStringSubmatch(line)
+		if match == nil {
+			continue
+		}
+
+		version, err := strconv.ParseFloat(match[1], 64)
+		if err != nil {
+			return 0, errors.Errorf("failed to parse eMMC version %v", match[1])
+		}
+
+		if version < emmcMinimumVersion {
+			return 0, errors.Errorf("eMMC version %v less than %v", version, emmcMinimumVersion)
+		}
+	}
+
+	for _, line := range outLines {
+		if emmcFailing.MatchString(line) {
+			return Failing, nil
+		}
+	}
+
+	return Healthy, nil
+}
+
+// parseDeviceHealthNVMe analyzes NVMe SMART attributes for indications of failure.
+func parseDeviceHealthNVMe(outLines []string) (LifeStatus, error) {
+	// Flag devices which report estimates approaching 100%
+	const percentageUsedThreshold = 97
+
+	for _, line := range outLines {
+		match := nvmeFailing.FindStringSubmatch(line)
+		if match == nil {
+			continue
+		}
+
+		percentageUsed, err := strconv.ParseInt(match[1], 10, 32)
+		if err != nil {
+			return 0, errors.Errorf("failed to parse NVMe percentage used %v", match[1])
+		}
+
+		if percentageUsed > percentageUsedThreshold {
+			return Failing, nil
+		}
+	}
+
+	return Healthy, nil
+}
+
+// parseDeviceHealthSSD analyzes storage information for indications of failure specific to SSDs.
+func parseDeviceHealthSSD(outLines []string) LifeStatus {
+	for _, line := range outLines {
+		if ssdFailing.MatchString(line) {
+			return Failing
+		}
+	}
+
+	return Healthy
+}
