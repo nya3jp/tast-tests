cfeebdcee6bc311513e7195444c365b94d5ff14f
Ian Barkley-Yeung <iby@google.com>
2019-07-17 18:15:10 -0700

    tast: Make Chrome crash tests use crash_reporter
    Have the Chrome crash tast tests actually invoke crash_reporter like
    real Chrome crashes do, instead of invoking breakpad workarounds. We
    want to actually test the Chrome->crash_reporter flow. "Test like you
    fly"
    
    NOTE: This is a big change, so I moved the tests back to "informational"
    status until they stablized. To avoid losing test coverage in the time
    between now and when the tests are promoted to critical, I copied the
    old tests, renaming them with a "Direct" suffix. I'll remove the
    "Direct" tests once I promote the new tests to critical.
    
    BUG=chromium:984807
    TEST=Ran tast tests while in various states (logged in, logged out)
    
    Cq-Depend: chromium:1717686
    Change-Id: I88f8e02509bdb78dc0e6779b5c3134ca0b866924

---
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_crash_not_logged_in_direct.go
@@ -0,0 +1,50 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/ui/chromecrash"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ChromeCrashNotLoggedInDirect,
+		Desc:         "Checks that Chrome writes crash dumps while not logged in; old version that does not invoke crash_reporter",
+		Contacts:     []string{"iby@chromium.org", "chromeos-ui@google.com"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func ChromeCrashNotLoggedInDirect(ctx context.Context, s *testing.State) {
+	// This is the old test, left here so that we don't lose test coverage while
+	// waiting for ChromeCrashNotLoggedIn to be stable enough to promote to a
+	// critical (non-informational) test.
+	// TODO(crbug.com/984807): Once ChromeCrashNotLoggedIn is no longer "informational",
+	// remove this test.
+	cr, err := chrome.New(ctx, chrome.NoLogin())
+	if err != nil {
+		s.Fatal("Chrome startup failed: ", err)
+	}
+	defer cr.Close(ctx)
+
+	// Sleep briefly as a speculative workaround for Chrome hangs that are occasionally seen
+	// when this test sends SIGSEGV to Chrome soon after it starts: https://crbug.com/906690
+	const delay = 3 * time.Second
+	s.Logf("Sleeping %v to wait for Chrome to stabilize", delay)
+	if err := testing.Sleep(ctx, delay); err != nil {
+		s.Fatal("Timed out while waiting for Chrome startup: ", err)
+	}
+
+	if dumps, err := chromecrash.KillAndGetCrashFiles(ctx); err != nil {
+		s.Fatal("Couldn't kill Chrome or get dumps: ", err)
+	} else if len(dumps) == 0 {
+		s.Error("No minidumps written after not-logged-in Chrome crash")
+	}
+}
