b10b29a54c50a481410ae0f358934ecc0f8a6bfb
Ian Barkley-Yeung <iby@google.com>
2019-07-17 18:15:10 -0700

    tast: Make Chrome crash tests use crash_reporter
    Have the Chrome crash tast tests actually invoke crash_reporter like
    real Chrome crashes do, instead of invoking breakpad workarounds. We
    want to actually test the Chrome->crash_reporter flow. "Test like you
    fly"
    
    NOTE: This is a big change, so I moved the tests back to "informational"
    status until they stablized. To avoid losing test coverage in the time
    between now and when the tests are promoted to critical, I copied the
    old tests, renaming them with a "Direct" suffix. I'll remove the
    "Direct" tests once I promote the new tests to critical.
    
    BUG=chromium:984807
    TEST=Ran tast tests while in various states (logged in, logged out)
    
    Cq-Depend: chromium:1717686
    Change-Id: I88f8e02509bdb78dc0e6779b5c3134ca0b866924

--- src/chromiumos/tast/local/bundles/cros/ui/chrome_crash_logged_in.go
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_crash_logged_in.go
@@ -9,6 +9,7 @@ import (
 
 	"chromiumos/tast/local/bundles/cros/ui/chromecrash"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
@@ -16,21 +17,32 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         ChromeCrashLoggedIn,
 		Desc:         "Checks that Chrome writes crash dumps while logged in",
-		Contacts:     []string{"chromeos-ui@google.com"},
+		Contacts:     []string{"iby@chromium.org", "chromeos-ui@google.com"},
 		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"informational"},
 	})
 }
 
 func ChromeCrashLoggedIn(ctx context.Context, s *testing.State) {
-	cr, err := chrome.New(ctx)
+	cr, err := chrome.New(ctx,
+		chrome.CrashNormalMode(),
+		chrome.ExtraArgs("--enable-crash-reporter-for-testing"))
 	if err != nil {
 		s.Fatal("Chrome login failed: ", err)
 	}
 	defer cr.Close(ctx)
 
-	if dumps, err := chromecrash.KillAndGetDumps(ctx); err != nil {
-		s.Fatal("Couldn't kill Chrome or get dumps: ", err)
-	} else if len(dumps) == 0 {
-		s.Error("No minidumps written after logged-in Chrome crash")
+	s.Log("Running metric_client to set up consent")
+	if err = testexec.CommandContext(ctx, "/usr/bin/metrics_client", "-C").Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Error setting metrics consent: ", err)
+	}
+
+	files, err := chromecrash.KillAndGetCrashFiles(ctx)
+	if err != nil {
+		s.Fatal("Couldn't kill Chrome or get files: ", err)
+	}
+
+	if err = chromecrash.FindCrashFilesIn(chromecrash.CryptohomeCrashPattern, files); err != nil {
+		s.Error("Crash files weren't written to cryptohome: ", err)
 	}
 }
