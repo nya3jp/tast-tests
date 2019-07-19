7556c88dbea8efa397f622bfbfa5c71315e1ed70
David Staessens <dstaessens@chromium.org>
2019-07-19 10:54:14 +0900

    tast.video.DecodeAccelVDPerfVP*1080P60FPS: Add VP8/9 VD performance test.
    This CL adds new VP8/9 performance tests for the new VD-based video decoder
    implementations.
    
    TEST=video.DecodeAccelVDPerfVP*1080P60FPS on eve
    
    BUG=chromium:954855
    
    Cq-Depend: chromium:1694201
    Change-Id: I8d7f7156155113626a6cb733fa70c73caedac32c

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_perf_vp8_1080p_60fps.go
@@ -0,0 +1,31 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     DecodeAccelVDPerfVP81080P60FPS,
+		Desc:     "Runs video_decode_accelerator_perf_tests with an VP8 1080p@60fps video on a VideoDecoder (see crbug.com/952730)",
+		Contacts: []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:     []string{"group:crosbolt", "crosbolt_perbuild"},
+		// TODO(b/137916185): The android capability is used to guarantee import-mode support, which is required by the new VD's.
+		SoftwareDeps: []string{"android", "chrome", caps.HWDecodeVP8},
+		Data:         []string{"1080p_60fps_600frames.vp8.ivf", "1080p_60fps_600frames.vp8.ivf.json"},
+	})
+}
+
+// DecodeAccelVDPerfVP81080P60FPS runs the video_decode_accelerator_perf_tests with 1080p_60fps_600frames.vp8.ivf
+// against the new video decoders based on the VideoDecoder interface.
+func DecodeAccelVDPerfVP81080P60FPS(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoPerfTest(ctx, s, "1080p_60fps_600frames.vp8.ivf", decode.VD)
+}
