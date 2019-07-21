53353e4b112089550ce562f924bc6def0a82d15e
Gil Dekel <gildekel@chromium.org>
2019-07-20 21:38:38 -0400

    camera.DecodeAccelJPEG: update test files for Tast
    This CL updates the files added in https://crrev.com/c/1672404
    for the new VaapiWebPDecoder.
    
    TEST=ran locally on a caroline board.
    BUG=chromium:877694
    
    Change-Id: I23b22a00c94b7958103662fc07935fab059d5446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1709947
    Tested-by: Gil Dekel <gildekel@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Gil Dekel <gildekel@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    (cherry picked from commit d8f33b50010032e49c0b5f422819a3dec0ca5cbb)

--- src/chromiumos/tast/local/bundles/cros/camera/decode_accel_jpeg.go
+++ src/chromiumos/tast/local/bundles/cros/camera/decode_accel_jpeg.go
@@ -22,12 +22,16 @@ func init() {
 		Contacts:     []string{"henryhsu@chromium.org", "chromeos-camera-eng@google.com"},
 		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"chrome", caps.HWDecodeJPEG},
-		Data:         jpegTestFiles,
+		Data:         imageFiles,
 	})
 }
 
-// jpegTestFiles lists the files required by the JPEG decode accelerator test.
-var jpegTestFiles = []string{
+// imageFiles lists the files required by the JPEG decode accelerator test.
+// TODO(crbug.com/986074): we should move the WebPs to a separate tast test.
+// For now we make the WebP tests run with the jpeg_decode_accelerator_unittest binary.
+// Also rename imageFiles back to jpegTestFiles once the tests are separated.
+var imageFiles = []string{
+	"BlackAndWhite_criss-cross_pattern_2015x2015.webp",
 	"peach_pi-1280x720.jpg",
 	"peach_pi-40x23.jpg",
 	"peach_pi-41x22.jpg",
@@ -36,6 +40,11 @@ var jpegTestFiles = []string{
 	"pixel-1280x720-grayscale.jpg",
 	"pixel-1280x720-yuv420.jpg",
 	"pixel-1280x720-yuv444.jpg",
+	"RGB_noise_2015x2015.webp",
+	"RGB_noise_large_pixels_115x115.webp",
+	"RGB_noise_large_pixels_2015x2015.webp",
+	"RGB_noise_large_pixels_4000x4000.webp",
+	"solid_green_2015x2015.webp",
 }
 
 // DecodeAccelJPEG runs a set of HW JPEG decode tests, defined in
@@ -50,7 +59,7 @@ func DecodeAccelJPEG(ctx context.Context, s *testing.State) {
 	// The JPEG decode test operates on all files in a single directory.
 	// testing.State doesn't guarantee that all data files will be stored in the same
 	// directory, so copy them to a temp dir.
-	tempDir := binsetup.CreateTempDataDir(s, "DecodeAccelJPEG.tast.", jpegTestFiles)
+	tempDir := binsetup.CreateTempDataDir(s, "DecodeAccelJPEG.tast.", imageFiles)
 	defer os.RemoveAll(tempDir)
 
 	// Execute the test binary.
