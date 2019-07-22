16bb58eb5e3903becc18acc208526f4f936352a1
Sergei Datsenko <dats@chromium.org>
2019-07-22 07:14:10 +0000

    Revert "tast-tests: Promote arc.Clipboard."
    This reverts commit c4fe7e1e7088ad3bcc0366df372ef09219352cab.
    
    Reason for revert: Broke PFQ. crbug.com/986171
    
    Exempt-From-Owner-Approval: Revert of a breaking change.
    
    Original change's description:
    > tast-tests: Promote arc.Clipboard.
    >
    > The test and arc.UIAutomator looks now stable.
    > Promote to CQ.
    >
    > BUG=chromium:911275
    > TEST=Made sure on tastboard.
    >
    > Change-Id: I431c1f5fdf12365397a7a169d907f97cd89ee028
    > Reviewed-on: https://chromium-review.googlesource.com/1708627
    > Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Ready: Hidehiko Abe <hidehiko@chromium.org>
    > Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    > Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    
    Bug: chromium:911275
    Change-Id: Icae40be88d9dbeb96153977b8a04db79dd8d6955

--- src/chromiumos/tast/local/bundles/cros/arc/clipboard.go
+++ src/chromiumos/tast/local/bundles/cros/arc/clipboard.go
@@ -26,6 +26,7 @@ func init() {
 		Func:         Clipboard,
 		Desc:         "Tests copying and pasting from Chrome to Android and vice versa",
 		Contacts:     []string{"ruanc@chromium.org", "niwa@chromium.org", "arc-eng@google.com"},
+		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"android_p", "chrome"},
 		Timeout:      4 * time.Minute,
 		Data: []string{
