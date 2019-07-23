c825c3acf9bf287824116dcbaa501b6197d95440
Hidehiko Abe <hidehiko@chromium.org>
2019-07-23 13:58:58 +0900

    tast-tests: Migrate hal3 to gtest library.
    BUG=chromium:984387
    TEST=Ran locally on grunt. Fail/success didn't change.
    
    Change-Id: I431048d88c8af045c8a243e17172416155213c51

--- src/chromiumos/tast/local/gtest/gtest.go
+++ src/chromiumos/tast/local/gtest/gtest.go
@@ -13,10 +13,12 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/shutil"
 	"chromiumos/tast/testing"
 )
 
@@ -66,6 +68,10 @@ type GTest struct {
 	// the log wouldn't be recorded to the current test log.
 	logfile string
 
+	// tempLogfile is true if the logfile should be created with
+	// ioutil.TestFile instead of os.Create().
+	tempLogfile bool
+
 	// filter specifies a subset of tests to run. If not empty, the value
 	// is passed to --gtest_filter=.
 	// Please see the gtest manual for the specification.
@@ -77,7 +83,11 @@ type GTest struct {
 	// in this.
 	extraArgs []string
 
-	// TODO(hidehiko): To migrate from bintest, support UID.
+	// uid specifies the user to run.
+	// -1 (by default) means unspecified, i.e., it runs as the current user
+	// running this Tast test (practically, root).
+	uid int
+
 	// TODO(hidehiko): To migrate from arctest, support ARC gtest run.
 }
 
@@ -87,6 +97,16 @@ type option func(t *GTest)
 // Logfile returns an option to set logfile path of GTest.
 func Logfile(path string) option {
 	return func(t *GTest) { t.logfile = path }
+}
+
+// TempLogfile returns an option to set logfile path of GTest. The file is
+// created by using ioutil.TempFile to avoid conflict, so its special pattern
+// is usable here.
+func TempLogfile(path string) option {
+	return func(t *GTest) {
+		t.logfile = path
+		t.tempLogfile = true
+	}
 }
 
 // Filter returns an option to set gtest_filter.
@@ -100,13 +120,45 @@ func ExtraArgs(args ...string) option {
 	return func(t *GTest) { t.extraArgs = args }
 }
 
+// UID returns an option to specify the user to run the gtest.
+func UID(uid int) option {
+	return func(t *GTest) { t.uid = uid }
+}
+
 // New creates GTest instance with given options.
 func New(exec string, opts ...option) *GTest {
-	ret := &GTest{exec: exec}
+	ret := &GTest{exec: exec, uid: -1}
 	for _, opt := range opts {
 		opt(ret)
 	}
 	return ret
+}
+
+// ToArgs returns an array of string for execution.
+func (t *GTest) ToArgs() ([]string, error) {
+	args := []string{t.exec}
+	if t.filter != "" {
+		args = append(args, "--gtest_filter="+t.filter)
+	}
+
+	// Verify extraArgs and append them.
+	var gtestArgs []string
+	for _, a := range t.extraArgs {
+		if strings.HasPrefix(a, "--gtest") {
+			gtestArgs = append(gtestArgs, a)
+		}
+	}
+	if len(gtestArgs) > 0 {
+		return nil, errors.Errorf("gtest.ExtraArgs should not contain --gtest prefixed flags: %v", gtestArgs)
+	}
+	args = append(args, t.extraArgs...)
+
+	// If user to run the test is given, run under sudo.
+	if t.uid >= 0 {
+		args = append([]string{"sudo", "--user=#" + strconv.Itoa(t.uid)}, args...)
+	}
+
+	return args, nil
 }
 
 // Run executes the gtest, and returns the parsed Report.
@@ -115,44 +167,40 @@ func New(exec string, opts ...option) *GTest {
 // return an error, but the report file should be created. This function
 // also handles the case, and returns it.
 func (t *GTest) Run(ctx context.Context) (*Report, error) {
-	args := []string{t.exec}
-	if t.filter != "" {
-		args = append(args, "--gtest_filter="+t.filter)
+	args, err := t.ToArgs()
+	if err != nil {
+		return nil, err
 	}
 
-	// Create report file.
-	output, err := createOutput()
+	// Create a report file.
+	output, err := createOutput(t.uid)
 	if err != nil {
 		return nil, err
 	}
 	defer os.Remove(output)
 	args = append(args, "--gtest_output=xml:"+output)
 
-	if t.extraArgs != nil {
-		for _, a := range t.extraArgs {
-			if strings.HasPrefix(a, "--gtest") {
-				return nil, errors.Errorf("gtest.ExtraArgs should not contain --gtest prefixed flags: %s", a)
-			}
+	// Set up log output.
+	var log *os.File
+	if t.logfile != "" {
+		var err error
+		log, err = openLogfile(t.logfile, t.tempLogfile, args)
+		if err != nil {
+			return nil, err
 		}
-		args = append(args, t.extraArgs...)
+		// log needs to be closed after cmd starts.
+		defer log.Close()
+
+		// At the beginning of the log file, write the command line
+		// to make debugging easier.
+		if err := writeArgs(log, args); err != nil {
+			return nil, err
+		}
 	}
 
 	cmd := testexec.CommandContext(ctx, args[0], args[1:]...)
-
-	// Set up log file.
-	if t.logfile != "" {
-		if err := os.MkdirAll(filepath.Dir(t.logfile), 0755); err != nil {
-			return nil, errors.Wrap(err, "failed to create log directory")
-		}
-		f, err := os.Create(t.logfile)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to create a log file")
-		}
-		// f needs to be closed after cmd starts.
-		defer f.Close()
-		cmd.Stdout = f
-		cmd.Stderr = f
-	}
+	cmd.Stdout = log
+	cmd.Stderr = log
 
 	retErr := cmd.Run()
 
@@ -173,7 +221,10 @@ func (t *GTest) Run(ctx context.Context) (*Report, error) {
 	return report, retErr
 }
 
-func createOutput() (string, error) {
+// createOutput creates the XML output file. If uid is given (i.e. not negative)
+// the file is owned by the user, because gtest process opens the file directly.
+// Returns the absolute path to the file.
+func createOutput(uid int) (string, error) {
 	f, err := ioutil.TempFile("", "gtest_output_*.xml")
 	if err != nil {
 		return "", errors.Wrap(err, "failed to create an output file")
@@ -188,6 +239,12 @@ func createOutput() (string, error) {
 		return "", errors.Wrap(err, "failed to close the output file")
 	}
 
+	if uid >= 0 {
+		if err := os.Chown(f.Name(), uid, -1); err != nil {
+			return "", errors.Wrap(err, "failed to set user for the output file")
+		}
+	}
+
 	abspath, err := filepath.Abs(f.Name())
 	if err != nil {
 		return "", errors.Wrap(err, "failed to resolve the path to abs")
@@ -195,4 +252,43 @@ func createOutput() (string, error) {
 
 	f = nil
 	return abspath, nil
+}
+
+// openLogfile creates and opens the log file at path. If tempfile is set true,
+// ioutil.TempFile is used. Specifically, some random string will be appended
+// at the end of path, or last '*' is expanded to a random string. Please see
+// also ioutil.TempFile's comment for details.
+func openLogfile(path string, tempfile bool, args []string) (*os.File, error) {
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, errors.Wrap(err, "failed to create a log directory")
+	}
+
+	var f *os.File
+	var err error
+	if tempfile {
+		f, err = ioutil.TempFile(dir, filepath.Base(path))
+	} else {
+		f, err = os.Create(path)
+	}
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create a log file")
+	}
+
+	return f, nil
+}
+
+// writeArgs writes the given args into the file stream, and flushes it.
+func writeArgs(f *os.File, args []string) error {
+	if _, err := fmt.Fprintf(f, "Running %s\n\n", shutil.EscapeSlice(args)); err != nil {
+		return errors.Wrap(err, "failed to write command line to a log file")
+	}
+
+	// Then flush, so that the stdout/stderr redirected from gtest executable
+	// should follow.
+	if err := f.Sync(); err != nil {
+		return errors.Wrap(err, "failed to flush log file")
+	}
+
+	return nil
 }
