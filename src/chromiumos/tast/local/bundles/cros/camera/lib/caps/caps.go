6f6ae18a38a897a010d2aa189194d4effc4e804a
Kuo Jen Wei <inker@google.com>
2019-07-24 13:10:06 +0800

    tast-test: Add CCAUIRecordVideo tast test
    This CL adds the CCAUIRecordVideo tast test to cover manual tests in
    go/cca-tests.
    
    BUG=b:117817410
    TEST=Passes "tast run <DUT> 'camera.CCAUI*'" on soraka and peppy.
    CQ-DEPEND=CL:1708608
    Change-Id: I2fdc607da0fb105f04f8b880a4690a48c543927c

---
+++ src/chromiumos/tast/local/bundles/cros/camera/lib/caps/caps.go
@@ -0,0 +1,59 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package caps is a package for capabilities used in autotest-capability.
+package caps
+
+// These are constant strings for capabilities in autotest-capability.
+// Tests may list these in SoftwareDeps.
+// See the below link for detail.
+// https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/master/chromeos-base/autotest-capability-default/.
+const (
+	// Prefix is the prefix of capability.
+	Prefix = "autotest-capability:"
+
+	// Video Decoding
+	HWDecodeH264      = Prefix + "hw_dec_h264_1080_30"
+	HWDecodeH264_60   = Prefix + "hw_dec_h264_1080_60"
+	HWDecodeH264_4K   = Prefix + "hw_dec_h264_2160_30"
+	HWDecodeH264_4K60 = Prefix + "hw_dec_h264_2160_60"
+
+	HWDecodeVP8      = Prefix + "hw_dec_vp8_1080_30"
+	HWDecodeVP8_60   = Prefix + "hw_dec_vp8_1080_60"
+	HWDecodeVP8_4K   = Prefix + "hw_dec_vp8_2160_30"
+	HWDecodeVP8_4K60 = Prefix + "hw_dec_vp8_2160_60"
+
+	HWDecodeVP9      = Prefix + "hw_dec_vp9_1080_30"
+	HWDecodeVP9_60   = Prefix + "hw_dec_vp9_1080_60"
+	HWDecodeVP9_4K   = Prefix + "hw_dec_vp9_2160_30"
+	HWDecodeVP9_4K60 = Prefix + "hw_dec_vp9_2160_60"
+
+	HWDecodeVP9_2      = Prefix + "hw_dec_vp9-2_1080_30"
+	HWDecodeVP9_2_60   = Prefix + "hw_dec_vp9-2_1080_60"
+	HWDecodeVP9_2_4K   = Prefix + "hw_dec_vp9-2_2160_30"
+	HWDecodeVP9_2_4K60 = Prefix + "hw_dec_vp9-2_2160_60"
+
+	// JPEG Decoding
+	HWDecodeJPEG = Prefix + "hw_dec_jpeg"
+
+	// Video Encoding
+	HWEncodeH264    = Prefix + "hw_enc_h264_1080_30"
+	HWEncodeH264_4K = Prefix + "hw_enc_h264_2160_30"
+
+	HWEncodeVP8    = Prefix + "hw_enc_vp8_1080_30"
+	HWEncodeVP8_4K = Prefix + "hw_enc_vp8_2160_30"
+
+	HWEncodeVP9    = Prefix + "hw_enc_vp9_1080_30"
+	HWEncodeVP9_4K = Prefix + "hw_enc_vp9_2160_30"
+
+	// JPEG Encoding
+	HWEncodeJPEG = Prefix + "hw_enc_jpeg"
+
+	// Camera
+	BuiltinUSBCamera     = Prefix + "builtin_usb_camera"
+	BuiltinMIPICamera    = Prefix + "builtin_mipi_camera"
+	VividCamera          = Prefix + "vivid_camera"
+	BuiltinCamera        = Prefix + "builtin_camera"
+	BuiltinOrVividCamera = Prefix + "builtin_or_vivid_camera"
+)
