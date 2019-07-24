a32ce9dbc987c12495f7a9d56250f49b731496b9
Meilin Wang <meilinw@google.com>
2019-07-24 16:56:23 -0700

    assistant: add temporary logic in tast test to handle API change.
    This change adds logic in test ui.AssistantTextQueries to handle the
    upcoming autotest API change for sendAssistantTextQuery. Some of these
    logic needs to be removed after the API change get checked in.
    
    BUG=None.
    TEST=run ui.AssistantTextQueries with the new implementation of the
    API.
    
    Change-Id: I8495e123c9e3d1df7a07337b25fff4a080026dd8

--- src/chromiumos/tast/local/assistant/assistant.go
+++ src/chromiumos/tast/local/assistant/assistant.go
@@ -12,13 +12,22 @@ import (
 	"chromiumos/tast/local/chrome"
 )
 
-// Response contains a subset of the results returned from the Assistant server when the
-// user emits a query.
-type Response struct {
+// QueryResponse contains a subset of the results returned from the Assistant server
+// when it received a query. Currently we only defined fields which are using in tests.
+type QueryResponse struct {
 	// Fallback contains text messages used as the "fallback" for HTML card rendering.
 	// Generally the fallback text is similar to transcribed TTS, e.g. "It's exactly 6
 	// o'clock." or "Turning bluetooth off.".
 	Fallback string `json:"htmlFallback"`
+}
+
+// QueryStatus contains a subset of the status of an interaction with Assistant started
+// by sending a query, e.g. query text, mic status, and query response. Currently we only
+// defined fields which are using in tests.
+type QueryStatus struct {
+	// TODO(meilinw): remove this entry once we replace with new autotest private API.
+	Fallback      string `json:"htmlFallback"`
+	QueryResponse `json:"queryResponse"`
 }
 
 // Enable brings up Google Assistant service and returns any errors.
@@ -37,22 +46,22 @@ func Enable(ctx context.Context, tconn *chrome.Conn) error {
 		  })`, nil)
 }
 
-// SendTextQuery sends text query to Assistant and gets the query response.
-func SendTextQuery(ctx context.Context, tconn *chrome.Conn, query string) (Response, error) {
+// SendTextQuery sends text query to Assistant and returns the query status.
+func SendTextQuery(ctx context.Context, tconn *chrome.Conn, query string) (QueryStatus, error) {
 	expr := fmt.Sprintf(
 		`new Promise((resolve, reject) => {
 		  chrome.autotestPrivate.sendAssistantTextQuery(%q,
 		      10 * 1000 /* timeout_ms */,
-		      function(response) {
+		      function(status) {
 		        if (chrome.runtime.lastError === undefined) {
-		          resolve(response);
+		          resolve(status);
 		        } else {
 		          reject(chrome.runtime.lastError.message);
 		        }
 		      });
 		  })`, query)
 
-	var re Response
-	err := tconn.EvalPromise(ctx, expr, &re)
-	return re, err
+	var status QueryStatus
+	err := tconn.EvalPromise(ctx, expr, &status)
+	return status, err
 }
