0741806518e088c6180cc8edff5db32820685ad3
Billy Zhao <billyzhao@google.com>
2019-07-24 16:29:08 -0700

    tast: port network_TwoShills to network/two_shills.go.
    The test will ensure that a second shill process cannot be spawned.
    The motivation behind the original design of the test was that there was
    an issue (listed in the bug below) where DBUS would by default issue a
    _queue_request for a name when an owner already existed.
    This test is being ported for the purpose of migrating the
    autotest network_nightly suite to Tast.
    
    TEST=two_shills.go , manual testing on octopus DUT for false positives.
    BUG=chromium:356874
    
    Change-Id: I56748a2fdf8283f97591f6f5b886a818c836b189

---
+++ src/chromiumos/tast/local/bundles/cros/network/two_shills.go
@@ -0,0 +1,46 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     TwoShills,
+		Desc:     "Verifies that only a single instance of shill can run",
+		Contacts: []string{"billyzhao@google.com", "chromeos-kernel-wifi@google.com"},
+		Attr:     []string{"informational"},
+	})
+}
+
+func TwoShills(ctx context.Context, s *testing.State) {
+	const timeout time.Duration = 10
+	_, _, pid, err := upstart.JobStatus(ctx, "shill")
+	if err != nil {
+		s.Fatal("Failed to find shill job: ", err)
+	} else if pid == 0 {
+		s.Fatal("Shill is not running")
+	}
+	out, err := testexec.CommandContext(ctx, "ip", "route", "show", "default", "match", "0/0", "table", "0").Output(testexec.DumpLogOnError)
+	netdev := strings.Split(string(out), " ")[4]
+	if len(netdev) < 1 {
+		s.Fatal("Unable to determine default network device")
+	}
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	if err := testexec.CommandContext(ctx, "shill", "--foreground").Run(testexec.DumpLogOnError); err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			s.Fatal("Request to start shill failed: ", err)
+		}
+	}
+}
