1db09b16c4745f5fb1a63cc19a74558c64673ce5
Kuo Jen Wei <inker@google.com>
2019-07-24 13:10:06 +0800

    tast-test: Add CCAUIRecordVideo tast test
    This CL adds the CCAUIRecordVideo tast test to cover manual tests in
    go/cca-tests.
    
    BUG=b:117817410
    TEST=Passes "tast run <DUT> 'camera.CCAUI*'" on soraka and peppy.
    CQ-DEPEND=CL:1708608
    Change-Id: I2fdc607da0fb105f04f8b880a4690a48c543927c

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_record_video.go
@@ -0,0 +1,163 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/camera/cca"
+	"chromiumos/tast/local/bundles/cros/camera/lib/caps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUIRecordVideo,
+		Desc:         "Opens CCA and verifies video recording related use cases",
+		Contacts:     []string{"inker@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome", caps.BuiltinCamera},
+		Data:         []string{"cca_ui.js", "cca_ui_capture.js"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func CCAUIRecordVideo(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js"), s.DataPath("cca_ui_capture.js")})
+	if err != nil {
+		s.Fatal("Failed to open CCA: ", err)
+	}
+	defer app.Close(ctx)
+	if err := app.WaitForVideoActive(ctx); err != nil {
+		s.Fatal("Preview is inactive after launching App: ", err)
+	}
+	restartApp := func() {
+		if err := app.Restart(ctx); err != nil {
+			s.Fatal("Failed to restart CCA: ", err)
+		}
+		if err := app.WaitForVideoActive(ctx); err != nil {
+			s.Fatal("Preview is inactive after restart App: ", err)
+		}
+	}
+
+	testing.ContextLog(ctx, "Switch to video mode")
+	if err := app.SwitchMode(ctx, cca.Video); err != nil {
+		s.Fatal("Failed to switch to video mode: ", err)
+	}
+	if err := app.WaitForVideoActive(ctx); err != nil {
+		s.Fatal("Preview is inactive after switch to video mode: ", err)
+	}
+
+	toggleTimer := func(active bool) func(context.Context, *cca.App) error {
+		return func(ctx context.Context, app *cca.App) error {
+			return app.SetTimerOption(ctx, active)
+		}
+	}
+
+	for _, action := range []struct {
+		name string
+		run  func(context.Context, *cca.App) error
+	}{
+		{"testRecordVideo", testRecordVideo},
+		{"toggleTimer(true)", toggleTimer(true)},
+		{"testRecordVideoWithTimer", testRecordVideoWithTimer},
+		{"testRecordCancelTimer", testRecordCancelTimer},
+		{"toggleTimer(false)", toggleTimer(false)},
+	} {
+		testing.ContextLog(ctx, "Start ", action.name)
+		if err := action.run(ctx, app); err != nil {
+			s.Errorf("Failed in %v(): %v", action.name, err)
+			restartApp()
+		}
+		testing.ContextLog(ctx, "Finish ", action.name)
+	}
+}
+
+func testRecordVideo(ctx context.Context, app *cca.App) error {
+	testing.ContextLog(ctx, "Click on start shutter")
+	if err := app.ClickShutter(ctx); err != nil {
+		return err
+	}
+	if err := testing.Sleep(ctx, time.Second); err != nil {
+		return err
+	}
+	testing.ContextLog(ctx, "Maximizing window")
+	if err := app.MaximizeWindow(ctx); err != nil {
+		return errors.Wrap(err, "failed to maximize window")
+	}
+	if err := testing.Sleep(ctx, time.Second); err != nil {
+		return err
+	}
+	testing.ContextLog(ctx, "Restore window")
+	if err := app.RestoreWindow(ctx); err != nil {
+		return errors.Wrap(err, "failed to restore window")
+	}
+	if err := testing.Sleep(ctx, time.Second); err != nil {
+		return err
+	}
+	start := time.Now()
+	testing.ContextLog(ctx, "Click on stop shutter")
+	if err := app.ClickShutter(ctx); err != nil {
+		return err
+	}
+	if err := app.WaitForState(ctx, "taking", false); err != nil {
+		return errors.Wrap(err, "shutter is not ended")
+	}
+	if _, err := app.WaitForFileSaved(ctx, cca.VideoPattern, start); err != nil {
+		return errors.Wrap(err, "cannot find result video")
+	}
+	return nil
+}
+
+func testRecordVideoWithTimer(ctx context.Context, app *cca.App) error {
+	start := time.Now()
+	testing.ContextLog(ctx, "Click on start shutter")
+	if err := app.ClickShutter(ctx); err != nil {
+		return err
+	}
+	testing.ContextLog(ctx, "Wait for a second of video recording")
+	if err := testing.Sleep(ctx, cca.TimerDelay+time.Second); err != nil {
+		return err
+	}
+	testing.ContextLog(ctx, "Click on stop shutter")
+	if err := app.ClickShutter(ctx); err != nil {
+		return err
+	}
+	if err := app.WaitForState(ctx, "taking", false); err != nil {
+		return errors.Wrap(err, "shutter is not ended")
+	}
+	if result, err := app.WaitForFileSaved(ctx, cca.VideoPattern, start); err != nil {
+		return errors.Wrap(err, "cannot find result video")
+	} else if elapsed := result.ModTime().Sub(start); elapsed < cca.TimerDelay {
+		return errors.Errorf("the capture should happen after timer of %v, actual elapsed time %v", cca.TimerDelay, elapsed)
+	}
+
+	return nil
+}
+
+func testRecordCancelTimer(ctx context.Context, app *cca.App) error {
+	testing.ContextLog(ctx, "Click on start shutter")
+	if err := app.ClickShutter(ctx); err != nil {
+		return err
+	}
+	if err := testing.Sleep(ctx, time.Second); err != nil {
+		return err
+	}
+	testing.ContextLog(ctx, "Click on cancel shutter")
+	if err := app.ClickShutter(ctx); err != nil {
+		return err
+	}
+	if taking, err := app.GetState(ctx, "taking"); err != nil {
+		return err
+	} else if taking {
+		return errors.New("shutter is not cancelled after clicking cancel shutter")
+	}
+	return nil
+}
