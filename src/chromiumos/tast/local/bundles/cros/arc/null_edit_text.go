2cf7b6a600e1906709211c25c84a6c47e14e0dc3
Tetsui Ohkubo <tetsui@chromium.org>
2019-07-25 12:16:08 +0900

    WIP: tast: Add arc.NullEditText
    TEST=tast run eve-DUT arc.NullEditText
    BUG=b:136527424
    
    Change-Id: Idef9927559db608a2621321cf85b0bdb3c7b4d4a

---
+++ src/chromiumos/tast/local/bundles/cros/arc/null_edit_text.go
@@ -0,0 +1,89 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NullEditText,
+		Desc:         "Checks TYPE_NULL EditText works on Android",
+		Contacts:     []string{"tetsui@chromium.org", "arc-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android_both", "chrome"},
+		Data:         []string{"ArcKeyboardTest.apk"},
+		Pre:          arc.Booted(),
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func NullEditText(ctx context.Context, s *testing.State) {
+	a := s.PreValue().(arc.PreData).ARC
+	d, err := ui.NewDevice(ctx, a)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	defer d.Close()
+
+	const (
+		apk = "ArcKeyboardTest.apk"
+		pkg = "org.chromium.arc.testapp.keyboard"
+		cls = "org.chromium.arc.testapp.keyboard.NullEditTextActivity"
+	)
+
+	s.Log("Installing app")
+	if err := a.Install(ctx, s.DataPath(apk)); err != nil {
+		s.Fatal("Failed installing app: ", err)
+	}
+
+	s.Log("Starting app")
+	if err := a.Command(ctx, "am", "start", "-W", pkg+"/"+cls).Run(); err != nil {
+		s.Fatal("Failed starting app: ", err)
+	}
+
+	const fieldID = "org.chromium.arc.testapp.keyboard:id/text"
+	field := d.Object(ui.ID(fieldID))
+	if err := field.WaitForExists(ctx, 30*time.Second); err != nil {
+		s.Fatal("Failed to find field: ", err)
+	}
+	if err := field.Click(ctx); err != nil {
+		s.Fatal("Failed to click field: ", err)
+	}
+	if err := field.SetText(ctx, ""); err != nil {
+		s.Fatal("Failed to empty field: ", err)
+	}
+
+	if err := d.Object(ui.ID(fieldID), ui.Focused(true)).WaitForExists(ctx, 30*time.Second); err != nil {
+		s.Fatal("Failed to focus on field: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	const keystrokes = "abcdef\b\b\b"
+	if err := kb.Type(ctx, keystrokes); err != nil {
+		s.Fatalf("Failed to type %q: %v", keystrokes, err)
+	}
+
+	// In order to use GetText() after timeout, we should have shorter timeout than ctx.
+	if err := d.Object(ui.ID(fieldID), ui.Text("abc")).WaitForExists(ctx, 2*time.Minute); err != nil {
+		if actual, err := field.GetText(ctx); err != nil {
+			s.Fatal("Failed to get text: ", err)
+		} else {
+			s.Fatalf("Got input %q from field after typing %q", actual, keystrokes)
+		}
+	}
+}
