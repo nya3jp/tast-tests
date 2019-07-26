f0a41416c7eda5f0ae495b1adaf6d435dd867f89
David Staessens <dstaessens@chromium.org>
2019-07-26 11:39:59 +0900

    tast.video.PlaybackVDPerf: Add H.264 1080p 60fps playback perf test for new VD's
    This CL adds an H.264 1080p 60fps video playback performance test for the new
    VD-based video decoder implementations. The test plays an H.264 video in the
    Chrome browser while measuring various performance metrics such as CPU usage.
    
    The video is played once with HW acceleration enabled and once using software
    decoding.
    
    TEST=video.PlaybackVDPerfH2641080P60FPS on eve
    
    BUG=chromium:954855
    
    Change-Id: I22c77195cd65c53dec5013daf58fd68d5b1edbab

---
+++ src/chromiumos/tast/local/bundles/cros/video/playback_vd_perf_h264_1080p_60fps.go
@@ -0,0 +1,34 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/video/playback"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     PlaybackVDPerfH2641080P60FPS,
+		Desc:     "Measures video playback performance with/without HW acceleration for H264 1080p@60fps video using a media::VideoDecoder",
+		Contacts: []string{"akahuang@chromium.org", "dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:     []string{"group:crosbolt", "crosbolt_perbuild"},
+		// "chrome_internal" is needed because H.264 is a proprietary codec.
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		Data:         []string{"1080p_60fps_600frames.h264.mp4"},
+		// Default timeout (i.e. 2 minutes) is not enough for low-end devices.
+		Timeout: 3 * time.Minute,
+	})
+}
+
+// PlaybackVDPerfH2641080P60FPS plays an H264 1080P 60FPS video and measures the performance values
+// with/without HW decoding acceleration, while using a media::VideoDecoder (see go/vd-migration).
+// The values are reported to the performance dashboard.
+func PlaybackVDPerfH2641080P60FPS(ctx context.Context, s *testing.State) {
+	playback.RunTest(ctx, s, "1080p_60fps_600frames.h264.mp4", "h264_1080p_60fps", playback.DefaultPerfDisabled, playback.VD)
+}
