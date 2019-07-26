ad9821c519242ff56ea2a9edd47bf76e3e1e2a98
khmel@chromium.org <khmel@chromium.org>
2019-07-26 14:59:14 -0700

    arc: Reorganize GamePerformance tests.
    This makes it possible to run subset of tests isolately. This increases
    stability of running these tests on bots plus makes it easy to integrate
    upcoming tests.
    
    BUG=b:134706327
    TEST=Locally, running new subset of tests.
    
    Change-Id: Icbb1a6c53259d77fc8ff6696e0f42011ebff0045

---
+++ src/chromiumos/tast/local/bundles/cros/arc/game_performance_buffers.go
@@ -0,0 +1,31 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/gameperformance"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GamePerformanceBuffers,
+		Desc:         "Captures set of performance metrics for buffers and upload it to the server",
+		Contacts:     []string{"khmel@chromium.org", "skuhne@chromium.org", "arc-performance@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"android_both", "chrome"},
+		Data:         []string{"ArcGamePerformanceTest.apk"},
+		Pre:          arc.Booted(),
+		Timeout:      10 * time.Minute,
+	})
+}
+
+func GamePerformanceBuffers(ctx context.Context, s *testing.State) {
+	gameperformance.RunTest(ctx, s, "BuffersTest")
+}
