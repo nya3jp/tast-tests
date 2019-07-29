fdf21283ee64b6b9f590b7b2657e259b51ddae1d
Billy Zhao <billyzhao@google.com>
2019-07-29 15:52:06 -0700

    tast: port network_UdevRename to network/udev_rename.go.
    This test verifies system stability when udev is restarted or when
    network interface drivers are reloaded. The interfaces should be able to
    recover properly throughout the restart lifecycle.
    
    network_UdevRename is part of the network_nightly suite and is part of
    the ongoing effort to port the suite to tast.
    
    I will cleanup the interface retrieval in a cleanup CL. (crbug.com/988894)
    TEST=udev_rename.go, manual testing on atlas DUT.
    BUG=None
    
    Change-Id: Ic098be9f9c1e59580ec6b11ad9ffb7b5924d1620

---
+++ src/chromiumos/tast/local/bundles/cros/network/udev_rename.go
@@ -0,0 +1,140 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     UdevRename,
+		Desc:     "Verifies that network interfaces don't disappear completely after udev or the interface drivers are restarted",
+		Contacts: []string{"billyzhao@google.com", "chromeos-kernel-wifi@google.com"},
+		Attr:     []string{"informational"},
+	})
+}
+
+func findWirelessInterface() (string, error) {
+	typeList := []string{"wlan", "mlan"}
+	ifaceList, err := getInterfaceList()
+	if err != nil {
+		return "", err
+	}
+	for _, pref := range typeList {
+		for _, iface := range ifaceList {
+			if strings.HasPrefix(iface, pref) {
+				return iface, nil
+			}
+		}
+	}
+	return "", errors.New("could not find an interface")
+}
+
+func getInterfaceList() ([]string, error) {
+	files, err := ioutil.ReadDir("/sys/class/net")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get interfaces")
+	}
+	toRet := make([]string, len(files), len(files))
+	for i, file := range files {
+		toRet[i] = file.Name()
+	}
+	return toRet, nil
+}
+
+func restartWirelessInterface(ctx context.Context) error {
+	iface, err := findWirelessInterface()
+	if err != nil {
+		return errors.Wrap(err, "could not find interface")
+	}
+
+	devicePath := fmt.Sprintf("/sys/class/net/%s/device", iface)
+	deviceRealPath, err := filepath.EvalSymlinks(devicePath)
+	if err != nil {
+		return errors.Wrapf(err, "could not evaluate symlink on payload %s", devicePath)
+	}
+	// The payload being written to the file is the device name gleaned
+	// from the baseDir of the deviceRealPath
+	payload := filepath.Base(deviceRealPath)
+
+	// The driver path is the directory where we can bind and release the device.
+	driverPath := filepath.Join(devicePath, "driver")
+	driverRealPath, err := filepath.EvalSymlinks(driverPath)
+	if err != nil {
+		return errors.Wrapf(err, "could not evaluate symlink on path %s", driverPath)
+	}
+
+	if err := ioutil.WriteFile(filepath.Join(driverRealPath, "unbind"), []byte(payload), 0200); err != nil {
+		return errors.Wrapf(err, "could not unbind driver, %s", iface)
+	}
+	if err := ioutil.WriteFile(filepath.Join(driverRealPath, "bind"), []byte(payload), 0200); err != nil {
+		return errors.Wrapf(err, "could not bind driver, %s", iface)
+	}
+	return nil
+}
+
+func restartUdev(ctx context.Context) error {
+	const service = "udev"
+
+	if !upstart.JobExists(ctx, service) {
+		return errors.Errorf("%s not running", service)
+	}
+	if err := upstart.RestartJob(ctx, service); err != nil {
+		return errors.Errorf("%s failed to restart", service)
+	}
+	if !upstart.JobExists(ctx, service) {
+		return errors.Errorf("%s failed to stay running", service)
+	}
+	return nil
+}
+
+// deviceRestarter is a function prototype that defines a first class function that would restart
+// a device or series of devices. restartUdev and restartWirelessInterface match the
+// function prototype.
+type deviceRestarter func(ctx context.Context) error
+
+func testUdevDeviceList(ctx context.Context, fn deviceRestarter) error {
+	iflistPre, err := getInterfaceList()
+	if err != nil {
+		return err
+	}
+	if err := fn(ctx); err != nil {
+		return err
+	}
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		iflistPost, err := getInterfaceList()
+		if err != nil {
+			return err
+		}
+		if !reflect.DeepEqual(iflistPre, iflistPost) {
+			return errors.Errorf("Interface changed %v != %v", iflistPre, iflistPost)
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 10 * time.Second, Interval: time.Second}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func UdevRename(ctx context.Context, s *testing.State) {
+	if err := testUdevDeviceList(ctx, restartUdev); err != nil {
+		s.Error("restartUdev failed: ", err)
+	}
+
+	if err := testUdevDeviceList(ctx, restartWirelessInterface); err != nil {
+		s.Error("restartWirelessInterface failed: ", err)
+	}
+}
