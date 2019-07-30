ce8f286816e6429d49f70f2106838f20673d6a7e
Billy Zhao <billyzhao@google.com>
2019-07-30 14:13:07 -0700

    tast: Fix iw_scan.go flakiness on stainless.
    IWScan was failing on stainless beacuse scans
    would intermittently fail when shill is enabled. This change serves to
    release the wireless interface from shill's responsibility before
    conducting the scan, and then enable shill's control over the interface
    afterwards.
    
    I will cleanup the interface retrieval in a cleanup CL. (crbug.com/988894)
    
    TEST=iw_scan.go, unit tests
    BUG=chromium:988609
    
    Change-Id: I5aa5d41ba9a90a8b3112ee4db2f78e515cbc1fff

--- src/chromiumos/tast/local/bundles/cros/network/iw_scan.go
+++ src/chromiumos/tast/local/bundles/cros/network/iw_scan.go
@@ -8,6 +8,8 @@ import (
 	"context"
 
 	"chromiumos/tast/local/bundles/cros/network/iw"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
@@ -21,8 +23,37 @@ func init() {
 }
 
 func IWScan(ctx context.Context, s *testing.State) {
-	_, err := iw.TimedScan(ctx, "wlan0", nil, nil)
+	const (
+		iface      = "wlan0"
+		technology = "wifi"
+	)
+	// In order to guarantee reliable execution of IWScan, we need to make sure
+	// shill doesn't interfere with the scan. We will disable shill's control
+	// on the wireless device while still maintaining ethernet connectivity.
+	manager, err := shill.NewManager(ctx)
 	if err != nil {
+		s.Fatal("Failed creating shill manager proxy: ", err)
+	}
+
+	// Make sure shill doesn't interfere with scans on suspend/resume.
+	if err := manager.DisableTechnology(ctx, technology); err != nil {
+		s.Fatal("Could not disable wifi from shill: ", err)
+	}
+
+	defer func() {
+		// Allow shill to take control of wireless device.
+		if err := manager.EnableTechnology(ctx, technology); err != nil {
+			s.Fatal("Could not enable wifi from shill: ", err)
+		}
+	}()
+
+	// Bring up wireless device after it's released from shill.
+	if err := testexec.CommandContext(ctx, "ip", "link", "set", iface, "up").Run(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("Could not bring up %s after disable", iface)
+	}
+
+	// Conduct scan
+	if _, err = iw.TimedScan(ctx, iface, nil, nil); err != nil {
 		s.Fatal("TimedScan failed: ", err)
 	}
 }
