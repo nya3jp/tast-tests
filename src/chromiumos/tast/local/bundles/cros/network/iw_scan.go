0fe233bd021b0d5e80edfbbe56921bebfb53c871
Billy Zhao <billyzhao@google.com>
2019-07-30 14:13:07 -0700

    tast: Fix iw_scan.go flakiness on stainless.
    IWScan was failing on stainless for multiple reasons. Firstly, scans
    will intermittently fail when shill is enabled. The current change will
    temporarily disable shill (which will affect wired connectivity) when
    conducting the scan. Additionally, this change will attempt to
    continuously scan for a few seconds before failing the test in case the
    wireless interface takes a while to come online.
    
    I will cleanup the interface retrieval in a cleanup CL. (crbug.com/988894)
    
    TEST=iw_scan.go, unit tests
    BUG=chromium:988609
    
    Change-Id: I5aa5d41ba9a90a8b3112ee4db2f78e515cbc1fff

--- src/chromiumos/tast/local/bundles/cros/network/iw_scan.go
+++ src/chromiumos/tast/local/bundles/cros/network/iw_scan.go
@@ -6,8 +6,12 @@ package network
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/local/bundles/cros/network/iw"
+	"chromiumos/tast/local/network"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
@@ -21,8 +25,35 @@ func init() {
 }
 
 func IWScan(ctx context.Context, s *testing.State) {
-	_, err := iw.TimedScan(ctx, "wlan0", nil, nil)
+	// We lose connectivity briefly. Tell recover_duts not to worry.
+	unlock, err := network.LockCheckNetworkHook(ctx)
 	if err != nil {
+		s.Fatal("Failed to lock the check network hook: ", err)
+	}
+	defer unlock()
+
+	// Stop shill temporarily and remove the default profile.
+	if err := shill.SafeStop(ctx); err != nil {
+		s.Fatal("Failed stopping shill: ", err)
+	}
+	defer func() {
+		if err := shill.SafeStart(ctx); err != nil {
+			s.Fatal("Failed starting shill: ", err)
+		}
+	}()
+	// Bring up wireless device after it's released from shill.
+	s.Log("Enable wifi manually")
+	if err := testexec.CommandContext(ctx, "ifconfig", "wlan0", "up").Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Could not bring up wlan0 after disable")
+	}
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		_, err := iw.TimedScan(ctx, "wlan0", nil, nil)
+		if err != nil {
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 10 * time.Second, Interval: time.Second}); err != nil {
 		s.Fatal("TimedScan failed: ", err)
 	}
+
 }
