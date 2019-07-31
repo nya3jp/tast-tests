9af46948da0db369d4f6791065db8ebe60c43d46
Nicholas Hollingum <hollingum@google.com>
2019-07-31 16:26:33 +1000

    tast-tests: crostini: Refactored verify_app and display_density
    These tests become parametric tests once refactored.
    
    This CL does not contain refactorings for the *_from_launcher variants,
    as those test have a more complicated setup.
    
    BUG=chromium:980048
    TEST=tast -verbose run localhost:33333 "crostini.Verify*" \
           "crostini.Display*"
    
    Change-Id: Id1f7ce04edabad87cb39ef344a57f0b92d0aa4e9

---
+++ src/chromiumos/tast/local/crostini/demo.go
@@ -0,0 +1,13 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+const (
+	// X11DemoAppPath is a path (in the container) to an application used for verifying simple X11 functionality.
+	X11DemoAppPath = "/opt/google/cros-containers/bin/x11_demo"
+
+	// WaylandDemoAppPath is a path (in the container) to an application used for verifying simple Wayland functionality.
+	WaylandDemoAppPath = "/opt/google/cros-containers/bin/wayland_demo"
+)
