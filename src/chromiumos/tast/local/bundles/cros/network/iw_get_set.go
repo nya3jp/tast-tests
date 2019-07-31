92c587b8ee7586ecd60f3b06c1088f602721e649
Billy Zhao <billyzhao@google.com>
2019-07-31 10:09:35 -0700

    tast: Add getters/setters for iw_runner.go dev and phy attributes.
    added getters/setters for future query/use:
    ScanDump, GetLinkValue, GetOperatingMode, GetRadioConfig, GetRegulatoryDomain, SetTxPower, SetFreq, SetAntennaBitmap
    
    Some getters and setters from autotest's iw_runner.py were not ported as
    they are not supported on chromebooks. For instance, adhoc IBSS interactions are not supported.
    It also seems like the intel chromebooks can't set their regulatory domains either.
    
    I will cleanup the interface retrieval in a cleanup CL. (crbug.com/988894)
    
    BUG=chromium:939214
    TEST=iw_get_set.go, tested on octopus, caroline
    
    Change-Id: I59b8a56db98273949aa3e9d2a81b55111e5a85f1

---
+++ src/chromiumos/tast/local/bundles/cros/network/iw_get_set.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/network/iw"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     IWGetSet,
+		Desc:     "Test IW getter and setter functions",
+		Contacts: []string{"billyzhao@google.com", "chromeos-kernel-wifi@google.com"},
+		Attr:     []string{"informational"},
+	})
+}
+
+func IWGetSet(ctx context.Context, s *testing.State) {
+	const iface = "wlan0"
+	res, err := iw.GetRegulatoryDomain(ctx)
+	if err != nil {
+		s.Fatal("GetRegulatoryDomain failed: ", err)
+	}
+	s.Log("Regulatory Domain: ", res)
+	// TODO: Flesh out the test and add unit tests to include more of the getters/setters. Tests
+	// can't really be made for them right now because they require a link to
+	// an AP.
+}
