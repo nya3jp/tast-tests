222da5fb6e7dcff247ab0241bb53190a940463f4
Billy Zhao <billyzhao@google.com>
2019-07-30 17:07:21 -0700

    tast: Add wireless interface pci rescan stability test.
    This test verifies PCI rescan functionality on devices that support
    iwlwifi_rescan. The test will remove the wireless interface and verifies
    that the device eventually shows up on lspci again.
    
    I will cleanup the interface retrieval in a cleanup CL. (crbug.com/988894)
    
    BUG=chromium:953497, chromium:871645
    TEST=pci_rescan.go, tested on caroline device with iwlwifi_rescan
    
    Change-Id: Id95c18a6679934eb9005f0feaa435b07f6129dab

---
+++ src/chromiumos/tast/local/bundles/cros/network/iwlwifi_pci_rescan.go
@@ -0,0 +1,89 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/network"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     IwlwifiPCIRescan,
+		Desc:     "Makes sure that the wireless interface can recover automatically if removed",
+		Contacts: []string{"billyzhao@google.com", "chromeos-kernel-wifi@google.com"},
+		Attr:     []string{"informational"},
+		// TODO(crbug/953497) add correct pci rescan dependency.
+		// SoftwareDeps: []string{"iwlwifi_rescan"},
+	})
+}
+
+// Reload Wifi Driver in the worst case.
+func restartInterface(ctx context.Context, iface string) error {
+	err := testexec.CommandContext(ctx, "modprobe", "-r", "iwlmvm", "iwlwifi").Run(testexec.DumpLogOnError)
+	if err2 := testexec.CommandContext(ctx, "modprobe", "iwlwifi").Run(testexec.DumpLogOnError); err2 != nil {
+		if err != nil {
+			return errors.Wrap(err2, err.Error())
+		}
+	}
+	if err2 := testexec.CommandContext(ctx, "ip", "link", "set", iface, "up").Run(testexec.DumpLogOnError); err != nil {
+		if err != nil {
+			return errors.Wrapf(err2, "Could not bring up %s after disable: %s", iface, err.Error())
+
+		}
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func IwlwifiPCIRescan(ctx context.Context, s *testing.State) {
+	iface, err := network.FindWirelessInterface()
+	if err != nil {
+		s.Fatal("Could not find valid wireless interface: ", err)
+	}
+	rescanFile := fmt.Sprintf("/sys/class/net/%s/device/driver/module/parameters/remove_when_gone", iface)
+	out, err := ioutil.ReadFile(rescanFile)
+	if err != nil {
+		s.Fatal("Could not read rescan file: ", err)
+	} else if string(out) != "Y\n" {
+		s.Fatalf("wifi rescan should be enabled, current mode is %s", string(out))
+	}
+	driverPath := fmt.Sprintf("/sys/class/net/%s/device/remove", iface)
+	driverRealPath, err := filepath.EvalSymlinks(driverPath)
+
+	if err := ioutil.WriteFile(driverRealPath, []byte("1"), 0200); err != nil {
+		s.Fatalf("Could not remove %s driver: %s", iface, err)
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		newIface, err := network.FindWirelessInterface()
+		if err != nil {
+			return errors.Wrap(err, "failed to find wireless interface")
+		}
+		if iface == newIface {
+			return nil
+		}
+		return errors.Errorf("looking for interface %s but got %s", iface, newIface)
+
+	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+		defer func() {
+			err := restartInterface(ctx, iface)
+			if err != nil {
+				s.Error("Failed to restart interface: ", err)
+			}
+		}()
+		s.Fatal("Device did not recover: ", err)
+	}
+}
