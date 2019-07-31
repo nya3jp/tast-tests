3eabda542a4df0fa411771474a19aa4ecd1cd23b
Billy Zhao <billyzhao@google.com>
2019-07-31 10:11:59 -0700

    tast: update iw_get_set.go to include setter tests.
    Update iw_get_set.go tests to better test setters. Previously, some
    iw_runner.go parsing was not implemented which made it hard to test some
    features.
    
    BUG=None
    TEST=IWGetSet
    
    Change-Id: Iad28e29f8e3b711013a922610e2b0a42e1bf0c77

--- src/chromiumos/tast/local/bundles/cros/network/iw_get_set.go
+++ src/chromiumos/tast/local/bundles/cros/network/iw_get_set.go
@@ -6,8 +6,10 @@ package network
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/local/bundles/cros/network/iw"
+	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
@@ -20,11 +22,44 @@ func init() {
 	})
 }
 
+// resetState will reset the intel wifi driver.
+// This function serves as our setup and cleanup routine.
+func resetState(ctx context.Context, s *testing.State) {
+	if err := testexec.CommandContext(ctx, "modprobe", "-r", "iwlmvm", "iwlwifi").Run(testexec.DumpLogOnError); err != nil {
+		s.Error("Remove wireless driver failed: ", err)
+	}
+	if err := testexec.CommandContext(ctx, "modprobe", "iwlwifi").Run(testexec.DumpLogOnError); err != nil {
+		s.Error("Start wireless driver failed: ", err)
+	}
+}
+
 func IWGetSet(ctx context.Context, s *testing.State) {
 	const iface = "wlan0"
+	resetState(ctx, s)
+	// Wait for device to fully be brought up.
+	s.Log("Waiting for device to be brought up")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		return testexec.CommandContext(ctx, "ip", "add", "sh", "dev", iface).Run()
+	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
+		s.Fatal("Failed to wait for device: ", err)
+	}
+
+	s.Log("Device brought up")
+
+	// Run tests.
+	if err := iw.SetTxPower(ctx, iface, "fixed", 1800); err != nil {
+		s.Fatal("SetTxPower failed: ", err)
+	}
+
+	defer resetState(ctx, s)
+
 	res, err := iw.GetRegulatoryDomain(ctx)
 	if err != nil {
 		s.Fatal("GetRegulatoryDomain failed: ", err)
 	}
 	s.Log("Regulatory Domain: ", res)
+	// TODO: merge in interface configuration code before I can test set freq.
+	// if err := iw.SetFreq(ctx, iface, 2412); err != nil {
+	// 	s.Fatal("SetFreq failed: ", err)
+	// }
 }
