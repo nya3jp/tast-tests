1f46ed359007ab1f73ca1e82d5579683dadcd4f4
Nicholas Hollingum <hollingum@google.com>
2019-08-01 12:56:44 +1000

    tast-tests: crostini: Refactoring the demo-based tast tests.
    This refactoring is useful to do in advance of factoring the more
    complicated _from_launcher tests out of CrostiniStartEverything. The
    refactorings involve:
     - Collated test parameters into a single struct, since these parameters
       were related anyway, this way we can pass them around more
       conveniently
     - Brought a refactoring of display_density.go forward from
       crrev.com/c/1728492 at the request of oka@. This combines two related
       demo invocations into one helper function.
    
    BUG=chromium:980048
    TEST=tast -verbose run localhost:33333 "crostini.Verify*" \
           "crostini.Display*"
    
    Change-Id: I402c80b8e77f7e41942f0ba9baf3d0e32fa11d22

--- src/chromiumos/tast/local/crostini/demo.go
+++ src/chromiumos/tast/local/crostini/demo.go
@@ -4,10 +4,40 @@
 
 package crostini
 
-const (
-	// X11DemoAppPath is a path (in the container) to an application used for verifying simple X11 functionality.
-	X11DemoAppPath = "/opt/google/cros-containers/bin/x11_demo"
+import (
+	"image/color"
 
-	// WaylandDemoAppPath is a path (in the container) to an application used for verifying simple Wayland functionality.
-	WaylandDemoAppPath = "/opt/google/cros-containers/bin/wayland_demo"
+	"chromiumos/tast/local/colorcmp"
 )
+
+// The DemoConfig object holds a configuration for running a tast test that
+// uses one of the demo applications.
+type DemoConfig struct {
+	// Name identifies the demo configuration (e.g. "x11" is the demo which
+	// verifies that we can render x11 windows).
+	Name string
+	// AppPath is the path to the demo's executable.
+	AppPath string
+	// DominantColot is used to paint the demo's window. By convention, we
+	// associate demo windows with colors (in order to easily identify
+	// screenshots).
+	DominantColor color.Color
+}
+
+// X11DemoConfig returns the configuration needed to run demo tests based on the x11_demo app.
+func X11DemoConfig() DemoConfig {
+	return DemoConfig{
+		Name:          "x11",
+		AppPath:       "/opt/google/cros-containers/bin/x11_demo",
+		DominantColor: colorcmp.RGB(0x99, 0xee, 0x44),
+	}
+}
+
+// WaylandDemoConfig returns the configuration needed to run demo tests based on the wayland_demo app.
+func WaylandDemoConfig() DemoConfig {
+	return DemoConfig{
+		Name:          "wayland",
+		AppPath:       "/opt/google/cros-containers/bin/wayland_demo",
+		DominantColor: colorcmp.RGB(0x33, 0x88, 0xdd),
+	}
+}
