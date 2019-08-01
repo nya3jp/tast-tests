c6bb103eee17531328cf1f18482b53a30cbcf0a7
David Staessens <dstaessens@chromium.org>
2019-08-01 14:46:55 +0900

    video.DecodeAccelPerf: Add H.264/VP8/VP9 2160P@30FPS video decoder perf test.
    This CL adds new video decoder performance tests for the H.264, VP8 and VP9
    codecs using an 2160P 30FPS video. The tests run directly on top of the video
    decoders using the video_decode_accelerator_perf_tests binary. These tests are
    different from the video.PlaybackPerf tests that measure the performance of the
    entire Chrome video stack.
    
    TEST=DecodeAccelPerf* on eve
    
    BUG=chromium:953114
    
    Change-Id: I39abedd0a7f6a9aeb3ef2740b6fa4f5d0c59e809

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_perf_vp9_2160p_30fps.go
@@ -0,0 +1,28 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DecodeAccelPerfVP92160P30FPS,
+		Desc:         "Runs video_decode_accelerator_perf_tests with a VP9 2160p@30fps video",
+		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", caps.HWDecodeVP9},
+		Data:         []string{"2160p_30fps_300frames.vp9.ivf", "2160p_30fps_300frames.vp9.ivf.json"},
+	})
+}
+
+func DecodeAccelPerfVP92160P30FPS(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoPerfTest(ctx, s, "2160p_30fps_300frames.vp9.ivf", decode.VDA)
+}
