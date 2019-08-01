22b4498e7c0106d94799970ca790e52930b7db83
Hidehiko Abe <hidehiko@chromium.org>
2019-08-02 01:55:57 +0900

    tast-tests: Add bundlemain.
    This is to share the bundle's main function across bundle executables.
    
    BUG=chromium:984390
    TEST=Ran locally.
    
    Change-Id: I54639a91b501fcf1da72db06d08d6221392ff361
    Reviewed-on: https://chromium-review.googlesource.com/1731410
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Ready: Hidehiko Abe <hidehiko@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/local/bundlemain/main.go
@@ -0,0 +1,22 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package bundlemain provides a main function implementation for a bundle
+// to share it from various local bundle executables.
+// The most of the frame implementation is in chromiumos/tast/bundle package,
+// but some utilities, which lives in support libraries for maintenance,
+// need to be injected.
+package bundlemain
+
+import (
+	"os"
+
+	"chromiumos/tast/bundle"
+	"chromiumos/tast/local/ready"
+)
+
+// Main is an entry point function for bundles.
+func Main() {
+	os.Exit(bundle.Local(os.Args[1:], os.Stdin, os.Stdout, os.Stderr, ready.Wait))
+}
