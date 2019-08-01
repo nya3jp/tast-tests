e508d5de0c6dd2e2a75695f716cf6e59782ccd46
David Staessens <dstaessens@chromium.org>
2019-08-01 14:45:42 +0900

    video.DecodeAccelPerf: Add H.264/VP8/VP9 1080P@30FPS video decoder perf test.
    This CL adds new video decoder performance tests for the H.264, VP8 and VP9
    codecs using an 1080P 30FPS video. The tests run directly on top of the video
    decoders using the video_decode_accelerator_perf_tests binary. These tests are
    different from the video.PlaybackPerf tests that measure the performance of the
    entire Chrome video stack.
    
    TEST=DecodeAccelPerf* on eve
    
    BUG=chromium:953114
    
    Change-Id: Idfb3bd2119f7de79571cb6a25f196e3abeac2395

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_perf_vp8_1080p_30fps.go
@@ -0,0 +1,28 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DecodeAccelPerfVP81080P30FPS,
+		Desc:         "Runs video_decode_accelerator_perf_tests with a VP8 1080p@30fps video",
+		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", caps.HWDecodeVP8},
+		Data:         []string{"1080p_30fps_300frames.vp8.ivf", "1080p_30fps_300frames.vp8.ivf.json"},
+	})
+}
+
+func DecodeAccelPerfVP81080P30FPS(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoPerfTest(ctx, s, "1080p_30fps_300frames.vp8.ivf", decode.VDA)
+}
