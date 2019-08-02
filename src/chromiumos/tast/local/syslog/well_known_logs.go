35fae433a61c1eb73d6634a21917e18a0f705a29
Ian Barkley-Yeung <iby@google.com>
2019-08-01 19:15:14 -0700

    tast: Add syslog.Watcher
    Add a set of functions useful for searching for certain messages in the
    system logs. Handles things like ignoring messages that were in the logs
    from previous tests, as well as logs rotating during the test.
    
    This will be used to determine the success of the CrashLoopTest. In test
    mode, crash_sender will print a particular message to /var/log/messages,
    and the test will look for that message to determine test success.
    
    BUG=chromium:857317
    TEST=Unit tests, ran ui.ChromeCrashLoop using this functionality
    
    Change-Id: I1111742e76149ae1f16432fde83318715f743af6

---
+++ src/chromiumos/tast/local/syslog/well_known_logs.go
@@ -0,0 +1,10 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package syslog
+
+const (
+	// MessageFile is the name of main system log.
+	MessageFile = "/var/log/messages"
+)
