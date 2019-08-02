0bce51db6fd736576bf6e099b46e6e888b52f8cf
Po-Hsien Wang <pwang@chromium.org>
2019-08-02 14:42:39 -0700

    graphics.CrostiniTrace: add gputest trace files
    Add few gputest traces.
    
    BUG=b:132905401
    TEST=tast run DUT graphics.CrostiniTraceG
    Change-Id: Ib94f2cdd785121c7b58147a87454792b9021d4c5

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/crostini_trace_gputest_pixmark_piano.go
@@ -0,0 +1,29 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/graphics/trace"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CrostiniTraceGputestPixmarkPiano,
+		Desc:         "Replay graphics trace in Crostini VM",
+		Contacts:     []string{"chromeos-gfx@google.com", "ddmail@google.com", "ihf@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		Data:         []string{"crostini_trace_gputest_pixmark_piano.trace.bz2"},
+		Timeout:      15 * time.Minute,
+		SoftwareDeps: []string{"chrome", "crosvm_gpu", "vm_host"},
+	})
+}
+
+func CrostiniTraceGputestPixmarkPiano(ctx context.Context, s *testing.State) {
+	trace.RunTest(ctx, s, map[string]string{"crostini_trace_gputest_pixmark_piano.trace.bz2": "gputest_pixmark_piano"})
+}
