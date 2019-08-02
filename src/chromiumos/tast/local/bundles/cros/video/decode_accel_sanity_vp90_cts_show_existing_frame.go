23bdca32a28ff5bf6ddfba9d663b959731bcec89
David Staessens <dstaessens@chromium.org>
2019-08-02 13:29:04 +0900

    video.DecodeAccelSanity: Adapt sanity tests to use the new video decoder tests.
    The DecodeAccelSanity* tests make use of the video_decode_accelerator_unittest
    that is being replaced by the video_decode_accelerator_tests. This CL adapts the
    DecodeAccelSanity* Tast tests to make use of the new test binary.
    
    TEST=video.DecodeAccelSanity* on eve
    
    BUG=chromium:990228
    
    Change-Id: Ia91663597d71db51912ba3b580719ea2d34ecbf9

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_sanity_vp90_cts_show_existing_frame.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_sanity_vp90_cts_show_existing_frame.go
@@ -15,16 +15,19 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         DecodeAccelSanityVP90CtsShowExistingFrame,
-		Desc:         "Run Chrome video_decode_accelerator_unittest's NoCrash test on a VP9 video from Android CTS video repository which fails elm",
+		Desc:         "Verify that the system doesn't crash when playing a VP9 video using the 'show existing frame' feature",
 		Contacts:     []string{"deanliao@chromium.org", "chromeos-video-eng@google.com"},
 		SoftwareDeps: []string{"chrome", caps.HWDecodeVP9},
-		Data:         []string{decode.DecodeAccelSanityVP90CtsShowExistingFrame.Name},
+		Data:         []string{"vda_sanity-vp90_2_17_show_existing_frame.vp9", "vda_sanity-vp90_2_17_show_existing_frame.vp9.json"},
 	})
 }
 
-// DecodeAccelSanityVP90CtsShowExistingFrame runs NoCrash test in video_decode_accelerator_unittest with
-// video defined in decode.DecodeAccelSanityVP90CtsShowExistingFrame.
+// DecodeAccelSanityVP90CtsShowExistingFrame runs the FlushAtEndOfStream test in the
+// video_decode_accelerator_tests. The vda_sanity-vp90_2_17_show_existing_frame.vp9 video is used
+// which makes use of the 'show existing frame' feature and comes from the Android CTS video
+// repository. The test doesn't look at the decode result, but verifies system robustness when
+// encountering the 'show existing frame' feature.
 // TODO(crbug.com/900467): This test is failing on elm and hana due to driver issue.
 func DecodeAccelSanityVP90CtsShowExistingFrame(ctx context.Context, s *testing.State) {
-	decode.RunAccelVideoSanityTest(ctx, s, decode.DecodeAccelSanityVP90CtsShowExistingFrame)
+	decode.RunAccelVideoSanityTest(ctx, s, "vda_sanity-vp90_2_17_show_existing_frame.vp9")
 }
