72a80c1bfdf3c3923e4c171550ab1d331b52d22e
David Staessens <dstaessens@chromium.org>
2019-08-02 13:29:04 +0900

    video.DecodeAccelSanity: Adapt sanity tests to use the new video decoder tests.
    The DecodeAccelSanity* tests make use of the video_decode_accelerator_unittest
    that is being replaced by the video_decode_accelerator_tests. This CL adapts the
    DecodeAccelSanity* Tast tests to make use of the new test binary.
    
    TEST=video.DecodeAccelSanity* on eve
    
    BUG=chromium:990228
    
    Change-Id: Ia91663597d71db51912ba3b580719ea2d34ecbf9

--- src/chromiumos/tast/local/bundles/cros/video/decode/accel_video.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode/accel_video.go
@@ -379,23 +379,44 @@ func RunAllAccelVideoTest(ctx context.Context, s *testing.State, testData TestVi
 	})
 }
 
-// RunAccelVideoSanityTest runs NoCrash test in video_decode_accelerator_unittest.
-// NoCrash test fails if video decoder's kernel driver crashes.
+// RunAccelVideoSanityTest runs the FlushAtEndOfStream test in the
+// video_decode_accelerator_tests. The test only fails if the video decoder's
+// kernel driver crashes.
 // The motivation of the sanity test: on certain devices, when playing VP9
 // profile 1 or 3, the kernel crashed. Though the profile was not supported
 // by the decoder, kernel driver should not crash in any circumstances.
 // Refer to https://crbug.com/951189 for more detail.
-func RunAccelVideoSanityTest(ctx context.Context, s *testing.State, testData TestVideoData) {
+func RunAccelVideoSanityTest(ctx context.Context, s *testing.State, filename string) {
+	const cleanupTime = 10 * time.Second
+
 	vl, err := logging.NewVideoLogger()
 	if err != nil {
-		s.Fatal("Failed to set values for verbose logging")
+		s.Fatal("Failed to set values for verbose logging: ", err)
 	}
 	defer vl.Close()
 
-	runAccelVideoTest(ctx, s, testConfig{
-		testData:   testData,
-		dataPath:   s.DataPath(testData.Name),
-		bufferMode: AllocateBuffer,
-		testFilter: "VideoDecodeAcceleratorTest.NoCrash",
-	})
+	// Only a single process can have access to the GPU, so we are required to
+	// call "stop ui" at the start of the test. This will shut down the chrome
+	// process and allow us to claim ownership of the GPU.
+	if err := upstart.StopJob(ctx, "ui"); err != nil {
+		s.Fatal("Failed to stop ui: ", err)
+	}
+	defer upstart.EnsureJobRunning(ctx, "ui")
+
+	// Reserve time to restart the ui job and perform cleanup at the end of the test.
+	ctx, cancel := ctxutil.Shorten(ctx, cleanupTime)
+	defer cancel()
+
+	// Run the FlushAtEndOfStream test, but ignore errors. The test only fails
+	// if the kernel crashes.
+	args := []string{s.DataPath(filename), s.DataPath(filename + ".json"),
+		"--output_folder=" + s.OutDir(), "--disable_validator"}
+	const exec = "video_decode_accelerator_tests"
+	_, _ = gtest.New(
+		filepath.Join(chrome.BinTestDir, exec),
+		gtest.Logfile(filepath.Join(s.OutDir(), exec+".log")),
+		gtest.Filter("*FlushAtEndOfStream"),
+		gtest.ExtraArgs(args...),
+		gtest.UID(int(sysutil.ChronosUID)),
+	).Run(ctx)
 }
