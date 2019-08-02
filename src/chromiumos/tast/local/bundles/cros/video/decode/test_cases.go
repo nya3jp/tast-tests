38bdd08882bd681cc930a269d5a31ad90ed701da
David Staessens <dstaessens@chromium.org>
2019-08-02 13:29:04 +0900

    video.DecodeAccelSanity: Adapt sanity tests to use the new video decoder tests.
    The DecodeAccelSanity* tests make use of the video_decode_accelerator_unittest
    that is being replaced by the video_decode_accelerator_tests. This CL adapts the
    DecodeAccelSanity* Tast tests to make use of the new test binary.
    
    Note: Wait for first Chrome uprev containing crrev.com/c/1772938.
    
    TEST=video.DecodeAccelSanity* on eve
    
    BUG=chromium:990228
    
    Cq-Depend: chromium:1772938
    Change-Id: Ia91663597d71db51912ba3b580719ea2d34ecbf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1731414
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Tested-by: David Staessens <dstaessens@chromium.org>
    Commit-Queue: David Staessens <dstaessens@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/decode/test_cases.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode/test_cases.go
@@ -49,36 +49,3 @@ var Test25FPSVP92 = TestVideoData{
 	MinFPSNoRender:   150,
 	Profile:          videotype.VP9_2Prof,
 }
-
-// DecodeAccelSanityVP91 is the test parameters of video_decode_accelerator_unittest for "vda_sanity-bear_profile1.vp9".
-var DecodeAccelSanityVP91 = TestVideoData{
-	Name:             "vda_sanity-bear_profile1.vp9",
-	Size:             videotype.NewSize(320, 180),
-	NumFrames:        30,
-	NumFragments:     30,
-	MinFPSWithRender: 0,
-	MinFPSNoRender:   0,
-	Profile:          videotype.VP9Prof,
-}
-
-// DecodeAccelSanityVP92 is the test parameters of video_decode_accelerator_unittest for "vda_sanity-bear_profile2.vp9".
-var DecodeAccelSanityVP92 = TestVideoData{
-	Name:             "vda_sanity-bear_profile2.vp9",
-	Size:             videotype.NewSize(320, 180),
-	NumFrames:        30,
-	NumFragments:     30,
-	MinFPSWithRender: 0,
-	MinFPSNoRender:   0,
-	Profile:          videotype.VP9Prof,
-}
-
-// DecodeAccelSanityVP93 is the test parameters of video_decode_accelerator_unittest for "vda_sanity-bear_profile3.vp9".
-var DecodeAccelSanityVP93 = TestVideoData{
-	Name:             "vda_sanity-bear_profile3.vp9",
-	Size:             videotype.NewSize(320, 180),
-	NumFrames:        30,
-	NumFragments:     30,
-	MinFPSWithRender: 0,
-	MinFPSNoRender:   0,
-	Profile:          videotype.VP9Prof,
-}
