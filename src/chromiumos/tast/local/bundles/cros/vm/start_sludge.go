15f4a4c722f983511d535af87e9350a13e6ca469
Trent Begin <tbegin@google.com>
2019-08-05 11:29:27 -0600

    vm.StartSludge: Add grpc server to synchronize starting the Sludge VM
    Previously, the test had to poll to know when the VM has started. Now
    when the VM starts, it sends a gRPC request to the test.
    
    BUG=b:138329362
    TEST=tast run --build dut vm.StartSludge
    
    Cq-Depend: chromium:1737396
    Change-Id: Ie9d8ea513f0f465f6a7b210a0e2f514b12613893

--- src/chromiumos/tast/local/bundles/cros/vm/start_sludge.go
+++ src/chromiumos/tast/local/bundles/cros/vm/start_sludge.go
@@ -11,6 +11,7 @@ import (
 
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/local/vm"
 	"chromiumos/tast/testing"
 )
 
@@ -26,36 +27,52 @@ func init() {
 
 func StartSludge(ctx context.Context, s *testing.State) {
 	const (
-		wilcoVMJob = "wilco_dtc"
-		wilcoVMCID = "512"
+		// Const values from /etc/init/wilco_dtc.conf on device
+		wilcoVMJob         = "wilco_dtc"
+		wilcoVMCID         = "512"
+		wilcoVMStartupPort = 7788
 	)
+
+	// Load the vhost-vsock module
+	if err := testexec.CommandContext(ctx, "modprobe", "-q", "vhost-vsock").Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Unable to load vhost-vsock module: ", err)
+	}
+
+	server, err := vm.NewStartupListenerServer(wilcoVMStartupPort)
+	if err != nil {
+		s.Fatal("Unable to start VM startup listener gRPC server: ", err)
+	}
+
+	if err := server.Start(); err != nil {
+		s.Fatal("Unable to start listening server: ", err)
+	}
+	defer server.Stop()
 
 	s.Log("Restarting Wilco DTC daemon")
 	if err := upstart.RestartJob(ctx, wilcoVMJob); err != nil {
 		s.Fatal("Wilco DTC process could not start: ", err)
 	}
 
+	startCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	if err := server.WaitReady(startCtx); err != nil {
+		s.Fatal("Error waiting for Wilco DTC VM to start: ", err)
+	}
+
 	for _, name := range []string{"ddv", "sa"} {
 		s.Logf("Checking %v process", name)
 
-		// Poll to check if the processes have started.
-		if err := testing.Poll(ctx, func(ctx context.Context) error {
-			cmd := testexec.CommandContext(ctx,
-				"vsh", "--cid="+wilcoVMCID, "--", "pgrep", name)
-			// Add a dummy buffer for stdin to force allocating a pipe. vsh uses
-			// epoll internally and generates a warning (EPERM) if stdin is /dev/null.
-			cmd.Stdin = &bytes.Buffer{}
+		cmd := testexec.CommandContext(ctx,
+			"vsh", "--cid="+wilcoVMCID, "--", "pgrep", name)
+		// Add a dummy buffer for stdin to force allocating a pipe. vsh uses
+		// epoll internally and generates a warning (EPERM) if stdin is /dev/null.
+		cmd.Stdin = &bytes.Buffer{}
 
-			out, err := cmd.Output()
-			if err != nil {
-				return err
-			}
-
-			s.Logf("Process %v started with PID %s", name, bytes.TrimSpace(out))
-			return nil
-
-		}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
+		out, err := cmd.Output(testexec.DumpLogOnError)
+		if err != nil {
 			s.Errorf("Process %v not found: %v", name, err)
+		} else {
+			s.Logf("Process %v started with PID %s", name, bytes.TrimSpace(out))
 		}
 	}
 
