68c342e4f979f566202593c678928524cde8675e
David Staessens <dstaessens@chromium.org>
2019-08-05 16:23:04 +0900

    video.DecodeAccelVP*OddDimensions: Add VP8/9 odd dimensions video decoder tests.
    This CL adds Tast tests for VP8 and VP9 odd dimensions videos. The tests make
    use of the video_decode_accelerator_tests.
    
    The videos are generated using ffmpeg:
    ffmpeg -i test-25fps.vp8 -vf scale=321:241 test-25fps-321x241.vp8.ivf
    ffmpeg -i test-25fps.vp9 -c:v libvpx-vp9 -vf scale=321:241 \
      test-25fps-321x241.vp9.ivf
    
    The checksums are generated using:
    ffmpeg -i test-25fps-321x241.vp8.ivf -f framemd5 test-25fps.vp8.frames.md5
    cat test-25fps.vp8.frames.md5 | awk '{print "    \""$NF"\","}
    
    TEST=video.DecodeAccelVP*OddDimension on eve
    
    BUG=chromium:990688
    
    Change-Id: Ic036aa718bbbc2f0f5a65624a0fb3eb35db22746

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vp8_odd_dimensions.go
@@ -0,0 +1,28 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DecodeAccelVP8OddDimensions,
+		Desc:         "Runs Chrome video_decode_accelerator_tests on a odd dimention VP8 video",
+		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome", caps.HWDecodeVP8},
+		Data:         []string{"test-25fps-321x241.vp8", "test-25fps-321x241.vp8.json"},
+	})
+}
+
+func DecodeAccelVP8OddDimensions(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoTestNew(ctx, s, "test-25fps-321x241.vp8", decode.VDA)
+}
---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vp9_odd_dimensions.go
@@ -0,0 +1,28 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DecodeAccelVP9OddDimensions,
+		Desc:         "Runs Chrome video_decode_accelerator_tests on a odd dimention VP9 video",
+		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome", caps.HWDecodeVP9},
+		Data:         []string{"test-25fps-321x241.vp9", "test-25fps-321x241.vp9.json"},
+	})
+}
+
+func DecodeAccelVP9OddDimensions(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoTestNew(ctx, s, "test-25fps-321x241.vp9", decode.VDA)
+}
