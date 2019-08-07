0feb95d1a87d025f510f375b11dc26d4764f1c47
David Staessens <dstaessens@chromium.org>
2019-08-07 16:18:12 +0900

    video.DecodeAccel*: Deprecate tast tests using video_decode_accelerator_unittest
    This CL deprecates all Tast tests using the video_decode_accelerator_unittest.
    These tests are replaced by tests using the new video_decode_accelerator_tests,
    that were already added using the 'New' suffix. Both tests have been running
    side-by-side for comparison for a while. The old tests can now be removed and
    the suffix can now be dropped from the new tests.
    
    Both the DecodeAccel* and DecodeAccel*Import tests are now combined in a single
    DecodeAccel* test.
    
    The following tests have been replaced:
    video.DecodeAccelH264(Import) -> video.DecodeAccelH264New
    video.DecodeAccelVP8(Import) -> video.DecodeAccelVP8New
    video.DecodeAccelVP9(Import) -> video.DecodeAccelVP9New
    video.DecodeAccelVP92(Import) -> video.DecodeAccelVP92New
    
    The following tests have been renamed:
    video.DecodeAccelH264New -> video.DecodeAccelH264
    video.DecodeAccelVP8New -> video.DecodeAccelVP8
    video.DecodeAccelVP9New -> video.DecodeAccelVP9
    video.DecodeAccelVP92New -> video.DecodeAccelVP2
    
    TEST=video.DecodeAccel* on eve
    
    BUG=chromium:933034
    
    Change-Id: I3795bfc3098b52eb181baa8fdaa918e1f32370ff

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_vp8.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vp8.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,27 +6,24 @@ package video
 
 import (
 	"context"
-	"time"
 
 	"chromiumos/tast/local/bundles/cros/video/decode"
 	"chromiumos/tast/local/media/caps"
-	"chromiumos/tast/local/media/videotype"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         DecodeAccelVP8,
-		Desc:         "Run Chrome video_decode_accelerator_unittest with a VP8 video",
+		Desc:         "Run Chrome video_decode_accelerator_tests with a VP8 video",
 		Contacts:     []string{"acourbot@chromium.org", "dstaessens@chromium.org", "chromeos-video-eng@google.com"},
 		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"chrome", caps.HWDecodeVP8},
-		Data:         decode.DataFiles(videotype.VP8Prof),
-		Timeout:      4 * time.Minute,
+		Data:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
 	})
 }
 
-// DecodeAccelVP8 runs video_decode_accelerator_unittest in ALLOCATE mode with test-25fps.vp8.
+// DecodeAccelVP8 runs the video_decode_accelerator_tests with test-25fps.vp8.
 func DecodeAccelVP8(ctx context.Context, s *testing.State) {
-	decode.RunAllAccelVideoTest(ctx, s, decode.Test25FPSVP8, decode.AllocateBuffer)
+	decode.RunAccelVideoTest(ctx, s, "test-25fps.vp8", decode.VDA)
 }
--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_vp9.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vp9.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,27 +6,24 @@ package video
 
 import (
 	"context"
-	"time"
 
 	"chromiumos/tast/local/bundles/cros/video/decode"
 	"chromiumos/tast/local/media/caps"
-	"chromiumos/tast/local/media/videotype"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         DecodeAccelVP9,
-		Desc:         "Run Chrome video_decode_accelerator_unittest with a VP9 video",
+		Desc:         "Run Chrome video_decode_accelerator_tests with a VP9 video",
 		Contacts:     []string{"acourbot@chromium.org", "dstaessens@chromium.org", "chromeos-video-eng@google.com"},
 		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"chrome", caps.HWDecodeVP9},
-		Data:         decode.DataFiles(videotype.VP9Prof),
-		Timeout:      4 * time.Minute,
+		Data:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
 	})
 }
 
-// DecodeAccelVP9 runs video_decode_accelerator_unittest in ALLOCATE mode with test-25fps.vp9.
+// DecodeAccelVP9 runs the video_decode_accelerator_tests with test-25fps.vp9.
 func DecodeAccelVP9(ctx context.Context, s *testing.State) {
-	decode.RunAllAccelVideoTest(ctx, s, decode.Test25FPSVP9, decode.AllocateBuffer)
+	decode.RunAccelVideoTest(ctx, s, "test-25fps.vp9", decode.VDA)
 }
