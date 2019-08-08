2f15eef90a997a1f423f77900024c7cec7d1a2bd
David Staessens <dstaessens@chromium.org>
2019-08-08 15:58:48 +0900

    video.DecodeAccelVDPerf: Add H.264/VP8/VP9 2160@30FPS VD performance test.
    This CL adds additional performance tests for the new video decoders based on
    the media::VideoDecoder interface. Tests are added for the H.264, VP8 and VP9
    codecs using an 2160 30FPS video. These tests run directly on top of the video
    decoders using the video_decode_accelerator_perf_tests binary. These tests are
    different from the video.PlaybackVDPerf tests that measure the performance of
    the entire Chrome video stack.
    
    TEST=video.DecodeAccelVDPerf*2160P30FPS on eve
    
    BUG=chromium:954855
    
    Change-Id: Ib065c871ca145d93be02fc78c64a6217742ee84f

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_perf_vp9_2160p_30fps.go
@@ -0,0 +1,30 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     DecodeAccelVDPerfVP92160P30FPS,
+		Desc:     "Runs video_decode_accelerator_perf_tests with a VP9 2160p@30fps video on a media::VideoDecoder (see go/vd-migration)",
+		Contacts: []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:     []string{"group:crosbolt", "crosbolt_perbuild"},
+		// TODO(b/137916185): Remove dependency on android capability. It's used here
+		// to guarantee import-mode support, which is required by the new VD's.
+		SoftwareDeps: []string{"android", "chrome", caps.HWDecodeVP9},
+		Data:         []string{"2160p_30fps_300frames.vp9.ivf", "2160p_30fps_300frames.vp9.ivf.json"},
+	})
+}
+
+func DecodeAccelVDPerfVP92160P30FPS(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoPerfTest(ctx, s, "2160p_30fps_300frames.vp9.ivf", decode.VD)
+}
