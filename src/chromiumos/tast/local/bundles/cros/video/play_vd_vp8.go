3104d8905cd5e254e8ef4abc8582d3b57dbe13ef
David Staessens <dstaessens@chromium.org>
2019-08-08 16:45:51 +0900

    tast.video.PlayVD: Add H.264, VP8 and VP9 play tests for the new VD's.
    This CL introduces additional tests for the new VD-based video decoder
    implementations. The tests play an H.264, VP8 and VP9 video respectively in the
    Chrome browser, and verify whether playback was successful.
    
    These tests are slightly different from the video.PlayDecodeAccelUsedVD tests,
    as they allow fallback to a software decoder, where the PlayDecodeAccelUsedVD
    tests only pass if HW acceleration was used.
    
    TEST=video.PlayVD* on eve
    
    BUG=chromium:954855
    
    Change-Id: I8568bf11cfe98c592f1212f6f5e44915e1347ba5

---
+++ src/chromiumos/tast/local/bundles/cros/video/play_vd_vp8.go
@@ -0,0 +1,31 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/play"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PlayVDVP8,
+		Desc:         "Checks whether VP8 video playback is working when using a media::VideoDecoder (see go/vd-migration)",
+		Contacts:     []string{"dstaessens@chromium.org", "akahuang@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          pre.ChromeVideoVD(),
+		Data:         []string{"720_vp8.webm", "video.html"},
+	})
+}
+
+func PlayVDVP8(ctx context.Context, s *testing.State) {
+	play.TestPlay(ctx, s, s.PreValue().(*chrome.Chrome),
+		"720_vp8.webm", play.NormalVideo, play.NoCheckHistogram)
+}
