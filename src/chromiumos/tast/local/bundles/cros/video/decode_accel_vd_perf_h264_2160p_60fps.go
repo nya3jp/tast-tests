4e979b781727e934091803c1155bafdfdfaa51e4
David Staessens <dstaessens@chromium.org>
2019-08-08 16:01:09 +0900

    video.DecodeAccelVDPerf: Add H.264/VP8/VP9 2160@60FPS VD performance test.
    This CL adds additional performance tests for the new video decoders based on
    the media::VideoDecoder interface. Tests are added for the H.264, VP8 and VP9
    codecs using an 2160 60FPS video. These tests run directly on top of the video
    decoders using the video_decode_accelerator_perf_tests binary. These tests are
    different from the video.PlaybackVDPerf tests that measure the performance of
    the entire Chrome video stack.
    
    TEST=video.DecodeAccelVDPerf*2160P60FPS on eve
    
    BUG=chromium:954855
    
    Change-Id: I4b126f7de91450fabfc8f40fe08deef757ad6c82

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_perf_h264_2160p_60fps.go
@@ -0,0 +1,30 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     DecodeAccelVDPerfH2642160P60FPS,
+		Desc:     "Runs video_decode_accelerator_perf_tests with an H.264 2160p@60fps video on a media::VideoDecoder (see go/vd-migration)",
+		Contacts: []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:     []string{"group:crosbolt", "crosbolt_perbuild"},
+		// TODO(b/137916185): Remove dependency on android capability. It's used here
+		// to guarantee import-mode support, which is required by the new VD's.
+		SoftwareDeps: []string{"android", "chrome", caps.HWDecodeH264},
+		Data:         []string{"2160p_60fps_600frames.h264", "2160p_60fps_600frames.h264.json"},
+	})
+}
+
+func DecodeAccelVDPerfH2642160P60FPS(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoPerfTest(ctx, s, "2160p_60fps_600frames.h264", decode.VD)
+}
