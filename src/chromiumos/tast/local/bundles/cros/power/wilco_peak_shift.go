fea58d57049ac6888a029b029ec926c2bf13b527
Nick Crews <ncrews@chromium.org>
2019-08-09 12:57:38 -0600

    Introduce power.WilcoPeakShift
    The Peak Shift policy on Wilco devices uses the EC to schedule when the
    DUT uses AC power. Specifically, by using the policy it is possible to
    schedule 3 different modes:
     - Use AC and charge the battery (max AC usage)
     - Use AC, but don't charge the battery (medium AC usage)
     - Don't use AC, and run off just the battery. (no AC usage)
    For more information, see
    https://www.chromium.org/administrators/policy-list-3#DevicePowerPeakShiftEnabled
    
    In order to test all aspects of the policy, it must be possible for the
    EC to charge the battery. That would require the battery level to be
    less than 100%, but since in the lab all devices are always plugged into
    AC, the batteries are kept at 100%. In order to test the charging
    behavior this test would need to drain the batteries somewhat, which
    could take ~10 minutes. Therefore, this test only verifies that the
    device can toggle AC usage on and off, and doesn't test for battery
    charging/no-charging.
    
    TODO(b/138940522): Add a more thorough, slower, manual test for
    all aspects of the policy.
    
    Also, the EC currently takes a minute to update its behavior after a
    policy change. This mean this test has a timeout of 4 minutes, but from
    test runs it usually takes about 2.5 minutes. I've filed a bug to try to
    fix this at b/138612166.
    
    BUG=b:138612166,b:134153286,b:138938829
    TEST=tast run ${DUT} power.WilcoPeakShift
    
    Change-Id: Icd33877ae71f2fbe9fc7f8c7a40425db0ddf528e

---
+++ src/chromiumos/tast/local/bundles/cros/power/wilco_peak_shift.go
@@ -0,0 +1,122 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/wilco"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WilcoPeakShift,
+		Desc:         "Checks that basic Peak Shift works on Wilco devices",
+		Contacts:     []string{"ncrews@chromium.org", "chromeos-power@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"wilco"},
+		// The EC seems to poll the Peak Shift settings in a ~60 second loop, so
+		// it can take up to 80 seconds for policy changes to take effect.
+		// See b/138612166 for a request to change the EC behavior.
+		Timeout: 4 * time.Minute,
+	})
+}
+
+// WilcoPeakShift performs basic tests of the Peak Shift behavior on Wilco devices.
+// The Peak Shift policy on Wilco devices uses the EC to schedule when the
+// DUT uses AC power. Specifically, by using the policy it is possible to
+// schedule 3 different modes:
+//  - Use AC and charge the battery (max AC usage)
+//  - Use AC, but don't charge the battery (medium AC usage)
+//  - Don't use AC, and run off just the battery. (no AC usage)
+// For more information, see
+// https://www.chromium.org/administrators/policy-list-3#DevicePowerPeakShiftEnabled
+//
+// In order to test all aspects of the policy, it must be possible for the
+// EC to charge the battery. That would require the battery level to be
+// less than 100%, but since in the lab all devices are always plugged into
+// AC, the batteries are kept at 100%. In order to test the charging
+// behavior this test would need to drain the batteries somewhat, which
+// could take ~10 minutes. Therefore, this test only verifies that the
+// device can toggle AC usage on and off, and doesn't test for battery
+// charging/no-charging. This serves mostly as an integration test, to check
+// that we can communicate with the EC.
+//
+// TODO(b/138940522): Add a more thorough, slower, manual test for
+// all aspects of the policy.
+func WilcoPeakShift(ctx context.Context, s *testing.State) {
+	const (
+		// Location of sysfs files that control Peak Shift
+		peakShiftDir = "/sys/bus/platform/devices/wilco-charge-schedule/peak_shift/"
+		// See the note on the test timeout in test declaration.
+		policyChangeTimeout = 80 * time.Second
+	)
+	// To be consistent, let's set the RTC to Jan 1, 2001, at noon, local time.
+	testingTime := time.Date(2001, time.January, 1, 12, 0, 0, 0, time.Now().Location())
+	// Should be "monday"
+	testingWeekdayString := strings.ToLower(testingTime.Weekday().String())
+	peakShiftEnablePath := filepath.Join(peakShiftDir, "enable")
+	peakShiftBatteryThresholdPath := filepath.Join(peakShiftDir, "battery_threshold")
+	peakShiftSchedulePath := filepath.Join(peakShiftDir, testingWeekdayString)
+
+	// Poll the current charging status for a while, and if it doesn't become
+	// what we expect before the timeout, then fail the test.
+	verifyACUsage := func(expected bool) {
+		pollPowerStatus := func(pollCtx context.Context) error {
+			status := wilco.GetPowerStatus(pollCtx, s)
+			// In order for Peak Shift to disconnect line power the battery must be
+			// appreciably above the 15% battery threshold.
+			if status.BatteryPercent < 20 {
+				s.Fatalf("Battery level is %v, must be at least %v to begin test", status.BatteryPercent, 20)
+			}
+			if status.LinePowerConnected != expected {
+				return errors.Errorf("AC presence is %v, but should be %v", status.LinePowerConnected, expected)
+			}
+			return nil
+		}
+		opts := testing.PollOptions{Timeout: policyChangeTimeout}
+		if err := testing.Poll(ctx, pollPowerStatus, &opts); err != nil {
+			s.Fatal("AC presence never became correct: ", err)
+		}
+	}
+
+	// Ensure (as best we can) that the DUT is back in it's original state after the test.
+	defer func() {
+		wilco.WriteECRTC(ctx, s, time.Now())
+	}()
+	defer wilco.WriteFileStrict(s, peakShiftEnablePath, wilco.ReadFileStrict(s, peakShiftEnablePath))
+	defer wilco.WriteFileStrict(s, peakShiftBatteryThresholdPath, wilco.ReadFileStrict(s, peakShiftBatteryThresholdPath))
+	defer wilco.WriteFileStrict(s, peakShiftSchedulePath, wilco.ReadFileStrict(s, peakShiftSchedulePath))
+
+	// Stop the upstart job that keeps the EC RTC in sync with local time.
+	wilco.StopSyncRTCJob(ctx, s)
+	defer func() {
+		wilco.StartSyncRTCJob(ctx, s)
+	}()
+
+	// Set the RTC time to a consistent dummy time.
+	wilco.WriteECRTC(ctx, s, testingTime)
+
+	s.Log("Enabling Peak Shift and setting lowest possible battery threshold")
+	wilco.WriteFileStrict(s, peakShiftEnablePath, "1")
+	wilco.WriteFileStrict(s, peakShiftBatteryThresholdPath, "15")
+
+	s.Log("Setting schedule to disable Peak Shift, waiting for the DUT to use AC")
+	wilco.WriteFileStrict(s, peakShiftSchedulePath, "01:00 02:00 03:00")
+	verifyACUsage(true)
+
+	s.Log("Setting schedule to use full Peak Shift, waiting for the DUT to not use AC")
+	wilco.WriteFileStrict(s, peakShiftSchedulePath, "01:00 22:00 23:00")
+	verifyACUsage(false)
+
+	s.Log("Setting schedule to disable Peak Shift, waiting for the DUT to use AC")
+	wilco.WriteFileStrict(s, peakShiftSchedulePath, "01:00 02:00 03:00")
+	verifyACUsage(true)
+}
