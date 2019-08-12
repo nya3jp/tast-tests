fdff8a80b70c4cd26c7d01c48ac5eee6443a6f58
Kartik Hegde <khegde@chromium.org>
2019-08-12 17:05:47 -0600

    tast: Validate cros_healthd cached vpd probe
    This test checks that the output from the cached vpd probe
    follows the format 'sku_number\n[$SKU_NUMBER]'.
    
    BUG=b:128683357
    TEST=tast run --build $DUT_IP
    platform.CrosHealthdProbeCachedVpd
    
    Cq-Depend:chromium:1913143
    Change-Id: Id6da383eb6071a82d0b242e358eedba386c94cbb
    Signed-off-by: Paul Moy <pmoy@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_cached_vpd.go
@@ -0,0 +1,48 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosHealthdProbeCachedVpd,
+		Desc: "Check that we can probe cros_healthd for cached vpd info",
+		Contacts: []string{
+			"khegde@google.com",
+			"pmoy@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"diagnostics"},
+	})
+}
+
+func CrosHealthdProbeCachedVpd(ctx context.Context, s *testing.State) {
+	b, err := testexec.CommandContext(ctx, "telem", "--category=cached_vpd").Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to run 'telem --category=cached_vpd': ", err)
+	}
+	lines := strings.Split(strings.TrimRight(string(b), "\n"), "\n")
+	if len(lines) != 2 {
+		s.Fatalf("Wrong number of lines: got %v, want 2", len(lines))
+	}
+	// Expect the keys to be just sku_number.
+	want := "sku_number"
+	got := lines[0]
+	if !(want == got) {
+		s.Fatalf("Header keys: got %v; want %v", got, want)
+	}
+
+	// All we want is a non-empty value for sku_number.
+	if lines[1] == "" {
+		s.Fatal("Empty sku_number")
+	}
+}
