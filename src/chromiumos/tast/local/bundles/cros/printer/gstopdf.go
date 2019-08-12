56af41c88c217f889187e6dc11ffd617e97e8ca6
David Valleau <valleau@chromium.org>
2019-08-12 11:04:20 -0700

    tast-test: Adding test for gstopdf
    This change adds a simple regression test for gstopdf which verifies
    that it produces the correct output.
    
    This change also introduces the "ghostscript" package which contains
    some shared functionality between the gstopdf and gstoraster tast tests.
    
    BUG=chromium:912789
    TEST=Passes all ghostscript tast tests on asuka device
    
    CQ-DEPEND=CL:1749027
    Change-Id: I9e38c1bf18ecd0935846f45a1e9a44d2ad8c0477

--- src/chromiumos/tast/local/bundles/cros/printer/gstopdf.go
+++ src/chromiumos/tast/local/bundles/cros/printer/gstopdf.go
@@ -14,21 +14,22 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         Gstoraster,
-		Desc:         "Tests that the gstoraster CUPS filter produces expected output",
+		Func:         Gstopdf,
+		Desc:         "Tests that the gstopdf CUPS filter produces expected output",
 		Contacts:     []string{"valleau@chromium.org"},
 		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"chrome", "cups"},
-		Data:         []string{"gstoraster_input.pdf", "gstoraster_golden.pwg"},
+		Data:         []string{"gstopdf_input.ps", "gstopdf_golden.pdf"},
 		Pre:          chrome.LoggedIn(),
 	})
 }
 
-func Gstoraster(ctx context.Context, s *testing.State) {
+func Gstopdf(ctx context.Context, s *testing.State) {
 	const (
-		gsFilter = "gstoraster"
-		input    = "gstoraster_input.pdf"
-		golden   = "gstoraster_golden.pwg"
+		gsFilter = "gstopdf"
+		input    = "gstopdf_input.ps"
+		golden   = "gstopdf_golden.pdf"
+		envVar   = "CUPS_SERVERBIN=/usr/libexec/cups"
 	)
-	ghostscript.RunTest(ctx, s, gsFilter, input, golden, "" /*envVar*/)
+	ghostscript.RunTest(ctx, s, gsFilter, input, golden, envVar)
 }
