0897efc992c42b0c197131f6deebc56eba748961
Nicholas Hollingum <hollingum@google.com>
2019-08-14 15:54:34 +1000

    tast-tests: crostini: Refactored package tests and removed CrostiniStartEverything
    This is the last of the tests to be refactored out of
    CrostiniStartEverything. We split this test into several sub-tests,
    though they all use the same dependency (the .deb file).
    
    Since this was the last piece of code tested by CrostiniStartEverything,
    we have removed that test 🎉.
    
    BUG=chromium:980048
    TEST=tast -verbose run $DUT "crostini.Package*"
    
    Change-Id: I47968ba7cda881d656bd5442c4030b659b4edb61

--- src/chromiumos/tast/local/bundles/cros/vm/subtest/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2018 The Chromium OS Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Package subtest contains subtests executed by the vm.CrostiniStartEverything test.
-//
-// Crostini initialization can be time-consuming, so this approach allows setup to be performed just once.
-package subtest
