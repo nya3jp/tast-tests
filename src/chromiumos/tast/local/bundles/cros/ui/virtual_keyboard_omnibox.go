063e61f8e4e7ef0816ba33daf8cf9c5671d7a33a
Hidehiko Abe <hidehiko@chromium.org>
2019-08-15 12:30:14 +0900

    tast-tests: Demote ui.VirtualKeyboardOmnibox to informational.
    Temporarily mark ui.VirtualKeyboardOmnibox to informational
    in order not to block the CQ.
    
    BUG=chromium:993852
    TEST=Built.
    
    Change-Id: I137ef6c72be3b5e8c0b705f9b79e252e4708c099

--- src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_omnibox.go
+++ src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_omnibox.go
@@ -18,6 +18,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         VirtualKeyboardOmnibox,
 		Desc:         "Checks that the virtual keyboard appears when clicking on the omnibox",
+		Attr:         []string{"informational"},
 		Contacts:     []string{"essential-inputs-team@google.com"},
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
 	})
