17f84e4758240324dfe981369fb6dfe0db646aa9
Miriam Zimmerman <mutexlox@google.com>
2019-08-15 16:15:47 -0700

    tast-tests: Add platform.ServiceFailure test.
    This tests that service failures result in log files being created in
    /var/spool/crash.
    
    BUG=chromium:983725
    TEST=tast -verbose run 100.127.92.23 platform.ServiceFailure
    
    Cq-Depend: chromium:1754862
    Change-Id: I0a39ff3202c20f0bbfb32f09b1adf73f34c92ca3

---
+++ src/chromiumos/tast/local/bundles/cros/platform/service_failure.go
@@ -0,0 +1,86 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/crash"
+	"chromiumos/tast/diff"
+	"chromiumos/tast/errors"
+	platform_crash "chromiumos/tast/local/bundles/cros/platform/crash"
+	"chromiumos/tast/local/metrics"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ServiceFailure,
+		Desc:     "Verify service failures are logged as expected",
+		Contacts: []string{"cros-monitoring-forensics@chromium.org"},
+		Attr:     []string{"informational"},
+		Data:     []string{platform_crash.TestCert},
+	})
+}
+
+func ServiceFailure(ctx context.Context, s *testing.State) {
+	const systemCrashDir = "/var/spool/crash"
+
+	if err := metrics.SetConsent(ctx, s.DataPath(platform_crash.TestCert)); err != nil {
+		s.Fatal("Failed to set consent: ", err)
+	}
+
+	oldFiles, err := crash.GetCrashes(systemCrashDir)
+	if err != nil {
+		s.Fatalf("Failed to get original crashes: %s", err)
+	}
+
+	// Restart anomaly detector to clear its cache of recently seen service
+	// failures and ensure this one is logged.
+	if err := upstart.RestartJob(ctx, "anomaly-detector"); err != nil {
+		s.Fatalf("Couldn't restart anomaly-detector: %s", err)
+	}
+
+	if err := upstart.StartJob(ctx, "failing-service"); err != nil {
+		s.Fatalf("Couldn't start job: %s", err)
+	}
+
+	err = testing.Poll(ctx, func(c context.Context) error {
+		newFiles, err := crash.GetCrashes(systemCrashDir)
+		if err != nil {
+			s.Fatalf("Failed to get new crashes: %s", err)
+		}
+
+		diffFiles := diff.GetNew(oldFiles, newFiles)
+		expectedRegexes := []string{`service_failure_failing_service\.\d{8}\.\d{6}\.0\.log`,
+			`service_failure_failing_service\.\d{8}\.\d{6}\.0\.meta`}
+		var missing []string
+		for _, re := range expectedRegexes {
+			match := false
+			for _, f := range diffFiles {
+				match, err = regexp.MatchString(re, f)
+				if err != nil {
+					s.Fatalf("Invalid regexp %s", re)
+				}
+				if match {
+					break
+				}
+			}
+			if !match {
+				missing = append(missing, re)
+			}
+		}
+		if len(missing) != 0 {
+			return errors.Errorf("Missing some files: %v", missing)
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 2 * time.Second})
+	if err != nil {
+		s.Errorf("Failed: %s", err)
+	}
+}
