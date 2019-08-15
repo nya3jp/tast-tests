7afa27b68c1805b1da8c8661eabd9399e8e424d5
Nicholas Hollingum <hollingum@google.com>
2019-08-15 14:18:59 +1000

    tast-tests: crostini: Added a test to check that GPU is enabled/disabled
    With the recent addition of the GPU precondition, we would like a simple
    test to verify if enabling the GPU actually gives you access to the
    device in the container.
    
    BUG=chromium:990993
    TEST=tast -verbose run $DUT "crostini.GPU*"
    
    Change-Id: I3a302d83d3c4cf6f8225388de6e447fbeea0d712

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/gpu_enabled_artifact.go
@@ -0,0 +1,32 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/crostini/gpuenabled"
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GPUEnabledArtifact,
+		Desc:         "Ensures that when crostini boots from an artifact, the GPU is not enabled",
+		Contacts:     []string{"hollingum@google.com", "cros-containers-dev@google.com"},
+		Attr:         []string{"informational"},
+		Timeout:      7 * time.Minute,
+		Data:         []string{crostini.ImageArtifact},
+		Pre:          crostini.StartedByArtifact(),
+		SoftwareDeps: []string{"chrome", "vm_host", "crosvm_gpu"},
+	})
+}
+
+func GPUEnabledArtifact(ctx context.Context, s *testing.State) {
+	// In tast, we do not initialize the VM the normal way, so even though the GPU is enabled by default on some boards, this precondition will still have the GPU disabled.
+	gpuenabled.RunTest(ctx, s, s.PreValue().(crostini.PreData).Container, "llvmpipe")
+}
