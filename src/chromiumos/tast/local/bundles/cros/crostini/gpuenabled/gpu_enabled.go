271deae1f609b5aca910bfefa2c9206923061567
Nicholas Hollingum <hollingum@google.com>
2019-08-15 14:18:59 +1000

    tast-tests: crostini: Added a test to check that GPU is enabled/disabled
    With the recent addition of the GPU precondition, we would like a simple
    test to verify if enabling the GPU actually gives you access to the
    device in the container.
    
    BUG=chromium:990993
    TEST=tast -verbose run $DUT "crostini.GPU*"
    
    Change-Id: I3a302d83d3c4cf6f8225388de6e447fbeea0d712

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/gpuenabled/gpu_enabled.go
@@ -0,0 +1,30 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package gpuenabled provides a test to place expectations on whether the GPU
+// is enabled for different crostini startup configurations
+package gpuenabled
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/vm"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+// RunTest runs a simple test to ensure the command line works.
+func RunTest(ctx context.Context, s *testing.State, cont *vm.Container, expectedDevice string) {
+	cmd := cont.Command(ctx, "sh", "-c", "glxinfo -B | grep Device:")
+	if out, err := cmd.Output(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("Failed to run %q: %v", shutil.EscapeSlice(cmd.Args), err)
+	} else {
+		output := string(out)
+		if !strings.Contains(output, expectedDevice) {
+			s.Fatalf("Failed to verify GPU device, expected %q got %q", expectedDevice, output)
+		}
+	}
+}
