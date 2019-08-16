06429e8a76797ded014b418ce7d77b0077c04799
Miriam Zimmerman <mutexlox@google.com>
2019-08-15 18:20:20 -0700

    tast-tests: Add test for crashing arc app.
    Note: this depends on https://crrev.com/c/1836661, but I can't use Cq-Depend to
    indicate that (since Chromium changes don't use the same infra)
    
    TEST=tast -verbose run --extrauseflags chrome_internal 100.127.92.23 arc.AppCrash
    BUG=chromium:983725
    
    Change-Id: Ia59906db034eb4b6a3d784e9995a234a585ca5e5

---
+++ src/chromiumos/tast/local/bundles/cros/arc/app_crash.go
@@ -0,0 +1,107 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"chromiumos/tast/crash"
+	"chromiumos/tast/local/arc"
+	localCrash "chromiumos/tast/local/crash"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AppCrash,
+		Desc:         "Test handling of a local app crash",
+		Contacts:     []string{"mutexlox@google.com", "cros-monitoring-forensics@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android", "chrome", "chrome_internal"},
+		Data:         []string{"app_sanity_hello_world.apk"},
+		Pre:          arc.Booted(),
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func AppCrash(ctx context.Context, s *testing.State) {
+	const (
+		// apk is the name of a plain hello world app:
+		// https://googleplex-android.googlesource.com/platform/vendor/google_arc/+/refs/heads/pi-arc/packages/development/ArcAppSanityTastTest
+		apk = "app_sanity_hello_world.apk"
+		pkg = "org.chromium.arc.testapp.appsanitytast"
+		cls = ".MainActivity"
+	)
+	if err := localCrash.SetUpCrashTest(); err != nil {
+		s.Fatal("Couldn't set up crash test: ", err)
+	}
+	defer localCrash.TearDownCrashTest()
+
+	a := s.PreValue().(arc.PreData).ARC
+	cr := s.PreValue().(arc.PreData).Chrome
+
+	s.Log("Attempting to install app")
+	if err := a.Install(ctx, s.DataPath(apk)); err != nil {
+		s.Fatal("Failed to install app: ", err)
+	}
+
+	act, err := arc.NewActivity(a, pkg, cls)
+	if err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	defer act.Close()
+
+	s.Log("Starting app")
+	if err = act.Start(ctx); err != nil {
+		s.Fatal("Failed to start app: ", err)
+	}
+
+	s.Log("Getting preexisting crashes")
+	user := cr.User()
+	path, err := cryptohome.UserPath(ctx, user)
+	if err != nil {
+		s.Fatal("Couldn't get user path: ", err)
+	}
+	crashDir := filepath.Join(path, "/crash")
+
+	oldCrashes, err := crash.GetCrashes(crashDir)
+	if err != nil {
+		s.Fatal("Couldn't get preexisting crashes: ", err)
+	}
+
+	s.Log("Getting PID")
+	var pid string
+	if out, err := arc.BootstrapCommand(ctx, "/bin/pgrep", "-f", pkg).Output(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Couldn't get pid: ", err)
+	} else {
+		pid = strings.TrimSpace(string(out))
+	}
+	s.Logf("PID is %s. Killing", pid)
+
+	if err := arc.BootstrapCommand(ctx, "/bin/kill", "-SIGSEGV", pid).Run(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("Couldn't kill app: %s", err)
+	}
+
+	s.Log("Waiting for crash files to become present")
+	files, err := localCrash.WaitForCrashFiles(ctx, crashDir, oldCrashes, []string{
+		`org_chromium_arc_testapp_appsanitytast.\d{8}.\d{6}.\d{6}.log`,
+		`org_chromium_arc_testapp_appsanitytast.\d{8}.\d{6}.\d{6}.meta`,
+	})
+	if err != nil {
+		s.Error("didn't find files: ", err)
+	}
+
+	for _, f := range files {
+		if err := os.Remove(f); err != nil {
+			s.Logf("Couldn't clean up %s: %v", f, err)
+		}
+	}
+}
