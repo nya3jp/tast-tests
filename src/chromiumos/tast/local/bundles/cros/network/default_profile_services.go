59dc7f8864589d2a8a805b6486f1682f67f95ed5
Keigo Oka <oka@chromium.org>
2019-08-21 16:42:33 +0900

    port network_DefaultProfileServices
    Ported from
    http://cs/chromeos_public/src/third_party/autotest/files/client/site_tests/network_DefaultProfileServices/network_DefaultProfileServices.py?l=12&rcl=1461a28598ccd88de9b1d9951b2a817e0c971ab0 .
    
    BUG=chromium:996087
    TEST=tast run $dut network.DefaultProfileServices # DUT is betty
    TEST=tast run $dut network.ConfigureServiceForProfile # It uses FindMatchingService
    
    Change-Id: I025304e6ec15cb8960e06109c188316c71dd478f

---
+++ src/chromiumos/tast/local/bundles/cros/network/default_profile_services.go
@@ -0,0 +1,91 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"os"
+
+	"chromiumos/tast/local/network"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DefaultProfileServices,
+		Desc: "Checks configured services persist across shill reboot",
+		Contacts: []string{
+			"briannorris@chromium.org",
+			"chromeos-kernel-wifi@google.com", // WiFi team
+			"oka@chromium.org",                // Tast port author
+		},
+		Attr: []string{"informational"},
+	})
+}
+
+func DefaultProfileServices(ctx context.Context, s *testing.State) {
+	const (
+		filePath = "/var/cache/shill/default.profile"
+		// ssid is a fake service name chosen unlikely to match any SSID present over-the-air.
+		ssid = "org.chromium.DfltPrflSrvcsTest"
+	)
+
+	// We lose connectivity along the way here, and if that races with the
+	// recover_duts network-recovery hooks, it may interrupt us.
+	unlock, err := network.LockCheckNetworkHook(ctx)
+	if err != nil {
+		s.Fatal("Failed to lock the check network hook: ", err)
+	}
+	defer unlock()
+
+	// Stop shill temporarily and remove the default profile.
+	if err := shill.SafeStop(ctx); err != nil {
+		s.Fatal("Failed stopping shill: ", err)
+	}
+	os.Remove(filePath)
+	if err := shill.SafeStart(ctx); err != nil {
+		s.Fatal("Failed starting shill: ", err)
+	}
+
+	manager, err := shill.NewManager(ctx)
+	if err != nil {
+		s.Fatal("Failed creating shill manager proxy: ", err)
+	}
+	if err := manager.PopAllUserProfiles(ctx); err != nil {
+		s.Fatal("Failed to pop user profiles: ", err)
+	}
+	if err := manager.ConfigureService(ctx, map[string]interface{}{
+		"Type":            "wifi",
+		"Mode":            "managed",
+		"SSID":            ssid,
+		"WiFi.HiddenSSID": true,
+		"SecurityClass":   "none",
+	}); err != nil {
+		s.Fatal("Failed to configure service: ", err)
+	}
+
+	// Restart shill to ensure that configurations persist across reboot.
+	if err := shill.SafeStop(ctx); err != nil {
+		s.Fatal("Failed stopping shill: ", err)
+	}
+	if err := shill.SafeStart(ctx); err != nil {
+		s.Fatal("Failed starting shill: ", err)
+	}
+
+	manager, err = shill.NewManager(ctx)
+	if err != nil {
+		s.Fatal("Failed creating shill manager proxy: ", err)
+	}
+	if err := manager.PopAllUserProfiles(ctx); err != nil {
+		s.Fatal("Failed to pop user profiles: ", err)
+	}
+
+	if _, err := manager.FindMatchingAnyService(ctx, map[string]interface{}{
+		"Name": ssid,
+	}); err != nil {
+		s.Error("Network not found after restart: ", err)
+	}
+}
