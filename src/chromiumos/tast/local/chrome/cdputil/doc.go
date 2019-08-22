9bafe2e228b20d0182a02ff933609b42eaa17ca5
Hidehiko Abe <hidehiko@chromium.org>
2019-08-23 04:16:05 +0900

    tast-tests: Extract Chrome DevTools Protocol connection part to a package.
    This is preparation to reuse CDP connection without chrome.Chrome
    instance, which requires to restart the Chrome.
    
    BUG=chromium:984390
    TEST=Ran locally.
    
    Change-Id: Ic5954ff42b9fd566cd02418b20e78191dccb3bf2

---
+++ src/chromiumos/tast/local/chrome/cdputil/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package cdputil provides utilities to communicate with Chrome via Chrome DevTools Protocol.
+package cdputil
