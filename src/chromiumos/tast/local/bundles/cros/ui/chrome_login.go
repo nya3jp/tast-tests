3bdcd51ff1ee254b3b1575cc99f320a4b8da1800
Hidehiko Abe <hidehiko@chromium.org>
2019-08-22 13:38:47 +0900

    tast-tests: Support ui.ChromeLogin.stress.
    This CL refactors ui.ChromeLogin to support stress test.
    
    BUG=chromium:995979
    TEST=Ran locally.
    
    Change-Id: Ibb90ed6e843ddae05a518c1793e2dd072efd96dc
    Reviewed-on: https://chromium-review.googlesource.com/1763616
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Ready: Hidehiko Abe <hidehiko@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/chrome_login.go
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_login.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"time"
 
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/session"
@@ -22,16 +23,59 @@ func init() {
 		Desc:         "Checks that Chrome supports login",
 		Contacts:     []string{"chromeos-ui@google.com"},
 		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val: 1,
+		}, {
+			Name:      "stress",
+			Val:       50,
+			ExtraAttr: []string{"group:stress"},
+		}},
+		Timeout: 50 * time.Minute,
 	})
 }
 
 func ChromeLogin(ctx context.Context, s *testing.State) {
-	// Start listening for a "started" SessionStateChanged D-Bus signal from session_manager.
-	sm, err := session.NewSessionManager(ctx)
-	if err != nil {
-		s.Fatal("Failed to connect session_manager: ", err)
+	sm := func() *session.SessionManager {
+		// Set up the test environment. Should be done quickly.
+		const setupTimeout = 30 * time.Second
+		setupCtx, cancel := context.WithTimeout(ctx, setupTimeout)
+		defer cancel()
+
+		// Ensures login screen.
+		if err := upstart.RestartJob(setupCtx, "ui"); err != nil {
+			s.Fatal("Chrome logout failed: ", err)
+		}
+
+		sm, err := session.NewSessionManager(setupCtx)
+		if err != nil {
+			s.Fatal("Failed to connect session_manager: ", err)
+		}
+		return sm
+	}()
+
+	const content = "Hooray, it worked!"
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, content)
+	}))
+	defer server.Close()
+
+	numTrial := s.Param().(int)
+	for i := 0; i < numTrial; i++ {
+		if numTrial > 1 {
+			s.Logf("Trial %d/%d", i+1, numTrial)
+		}
+
+		testChromeLogin(ctx, s, sm, server.URL, content)
 	}
+}
+
+func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionManager, url, expected string) {
+	const timeoutPerRun = time.Minute
+	ctx, cancel := context.WithTimeout(ctx, timeoutPerRun)
+	defer cancel()
+
 	func() {
+		// Start listening for a "started" SessionStateChanged D-Bus signal from session_manager.
 		sw, err := sm.WatchSessionStateChanged(ctx, "started")
 		if err != nil {
 			s.Fatal("Failed to watch for D-Bus signals: ", err)
@@ -52,13 +96,7 @@ func ChromeLogin(ctx context.Context, s *testing.State) {
 			s.Fatal("Didn't get SessionStateChanged signal: ", ctx.Err())
 		}
 
-		const expected = "Hooray, it worked!"
-		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			io.WriteString(w, expected)
-		}))
-		defer server.Close()
-
-		conn, err := cr.NewConn(ctx, server.URL)
+		conn, err := cr.NewConn(ctx, url)
 		if err != nil {
 			s.Fatal("Creating renderer failed: ", err)
 		}
@@ -69,12 +107,11 @@ func ChromeLogin(ctx context.Context, s *testing.State) {
 		}
 
 		var actual string
-		if err = conn.Eval(ctx, "document.documentElement.innerText", &actual); err != nil {
+		if err := conn.Eval(ctx, "document.documentElement.innerText", &actual); err != nil {
 			s.Fatal("Getting page content failed: ", err)
 		}
-		s.Logf("Got content %q", actual)
 		if actual != expected {
-			s.Fatalf("Expected page content %q, got %q", expected, actual)
+			s.Fatalf("Unexpected page content: got %q; want %q", actual, expected)
 		}
 	}()
 
