05276105804c60f088b6823666086d395e3bf999
Ian Barkley-Yeung <iby@google.com>
2019-08-26 16:12:22 -0700

    tast: Give syslog.Watcher unit tests unique file names
    Give each run of the unit tests in syslog.watcher_test.go different file
    and/or directory names.
    
    This is a speculative fix for crbug.com/996817. The theory is that
    multiple copies of the unit test might run at the same time and being
    overwriting each other's files.
    
    BUG=chromium:996817
    TEST=fast_build.sh -t chromiumos/tast/local/syslog
    
    Change-Id: Ieae29415c6b12149dc393ae37be0874f18f258e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1772348
    Tested-by: Ian Barkley-Yeung <iby@chromium.org>
    Auto-Submit: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>

--- src/chromiumos/tast/local/syslog/watcher_test.go
+++ src/chromiumos/tast/local/syslog/watcher_test.go
@@ -5,7 +5,9 @@ package syslog
 
 import (
 	"context"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 	"time"
@@ -41,20 +43,21 @@ var tests = []testStruct{
 
 func TestHasMessageBasic(t *testing.T) {
 	// Create a file, look for the target message it in.
-	const filename = "/tmp/TestHasMessageBasic"
+	const filename = "TestHasMessageBasic"
 	for _, tc := range tests {
 		// Go into a subfunction so that defers happen at the right time.
 		func() {
 			t.Logf("TestHasMessageBasic{%q, %v}\n", tc.contents, tc.expected)
-			file, err := os.Create(filename)
+			file, err := ioutil.TempFile("", filename)
 			if err != nil {
-				t.Fatalf("os.Create(%s) failed: %v", filename, err)
+				t.Fatalf("ioutil.TempFile(\"\", %s) failed: %v", filename, err)
 			}
+			defer os.Remove(file.Name())
 			defer file.Close()
 
-			w, err := NewWatcher(filename)
+			w, err := NewWatcher(file.Name())
 			if err != nil {
-				t.Fatalf("Could not create Watcher: %v", err)
+				t.Fatalf("Could not create Watcher on %s: %v", file.Name(), err)
 			}
 			defer w.Close()
 
@@ -80,16 +83,17 @@ func TestHasMessageBasic(t *testing.T) {
 
 func TestHasMessageAddToFile(t *testing.T) {
 	// Keep adding to a file, expect HasMessage to notice when the target appears.
-	const filename = "/tmp/TestHasMessageAddToFile"
-	file, err := os.Create(filename)
+	const filename = "TestHasMessageAddToFile"
+	file, err := ioutil.TempFile("", filename)
 	if err != nil {
-		t.Fatalf("os.Create(%s) failed: %v", filename, err)
+		t.Fatalf("ioutil.TempFile(\"\", %s) failed: %v", filename, err)
 	}
+	defer os.Remove(file.Name())
 	defer file.Close()
 
-	w, err := NewWatcher(filename)
+	w, err := NewWatcher(file.Name())
 	if err != nil {
-		t.Fatalf("Could not create Watcher: %v", err)
+		t.Fatalf("Could not create Watcher on %s: %v", file.Name(), err)
 	}
 	defer w.Close()
 
@@ -129,16 +133,17 @@ func TestHasMessageAddToFileNoUnsuccesfulReads(t *testing.T) {
 	// Basically the same as TestHasMessageAddToFile except that we always go
 	// straight from one "Target" to the next. Every HasMessage should return true;
 	// there are no unsuccessful reads to 'clear' the reader.
-	const filename = "/tmp/TestHasMessageAddToFileNoUnsuccesfulReads"
-	file, err := os.Create(filename)
+	const filename = "TestHasMessageAddToFileNoUnsuccesfulReads"
+	file, err := ioutil.TempFile("", filename)
 	if err != nil {
-		t.Fatalf("os.Create(%s) failed: %v", filename, err)
+		t.Fatalf("ioutil.TempFile(\"\", %s) failed: %v", filename, err)
 	}
+	defer os.Remove(file.Name())
 	defer file.Close()
 
-	w, err := NewWatcher(filename)
+	w, err := NewWatcher(file.Name())
 	if err != nil {
-		t.Fatalf("Could not create Watcher: %v", err)
+		t.Fatalf("Could not create Watcher on %s: %v", file.Name(), err)
 	}
 	defer w.Close()
 	for _, tc := range tests {
@@ -169,16 +174,17 @@ func TestHasMessageAddToFileNoUnsuccesfulReads(t *testing.T) {
 }
 
 func TestSplitAcrossWrites(t *testing.T) {
-	const filename = "/tmp/TestSplitAcrossWrites"
-	file, err := os.Create(filename)
+	const filename = "TestSplitAcrossWrites"
+	file, err := ioutil.TempFile("", filename)
 	if err != nil {
-		t.Fatalf("os.Create(%s) failed: %v", filename, err)
+		t.Fatalf("ioutil.TempFile(\"\", %s) failed: %v", filename, err)
 	}
+	defer os.Remove(file.Name())
 	defer file.Close()
 
-	w, err := NewWatcher(filename)
+	w, err := NewWatcher(file.Name())
 	if err != nil {
-		t.Fatalf("Could not create Watcher: %v", err)
+		t.Fatalf("Could not create Watcher on %s: %v", file.Name(), err)
 	}
 	defer w.Close()
 
@@ -210,8 +216,9 @@ func TestSplitAcrossWrites(t *testing.T) {
 
 func TestFileRotation(t *testing.T) {
 	const (
-		filename        = "/tmp/TestFileRotation"
-		rotatedFilename = filename + ".1"
+		dirName             = "TestFileRotation"
+		baseFilename        = "test_log"
+		rotatedBaseFilename = baseFilename + ".1"
 	)
 
 	for _, tc := range []struct {
@@ -235,8 +242,13 @@ func TestFileRotation(t *testing.T) {
 	} {
 		func() {
 			t.Logf("TestFileRotation{%q, %q, %v}\n", tc.before, tc.after, tc.expected)
-			os.Remove(rotatedFilename)
+			tmpDir, err := ioutil.TempDir("", dirName)
+			if err != nil {
+				t.Fatalf("ioutil.TempDir(\"\", %s) failed: %v", dirName, err)
+			}
+			defer os.RemoveAll(tmpDir)
 
+			filename := filepath.Join(tmpDir, baseFilename)
 			file, err := os.Create(filename)
 			if err != nil {
 				t.Fatalf("os.Create(%s) failed: %v", filename, err)
@@ -250,7 +262,7 @@ func TestFileRotation(t *testing.T) {
 
 			w, err := NewWatcher(filename)
 			if err != nil {
-				t.Fatalf("Could not create Watcher: %v", err)
+				t.Fatalf("Could not create Watcher on %s: %v", filename, err)
 			}
 			defer w.Close()
 
@@ -262,9 +274,10 @@ func TestFileRotation(t *testing.T) {
 			if err != nil {
 				t.Fatalf("First close failed: %v", err)
 			}
+			rotatedFilename := filepath.Join(tmpDir, rotatedBaseFilename)
 			err = os.Rename(filename, rotatedFilename)
 			if err != nil {
-				t.Fatalf("Rename failed: %v", err)
+				t.Fatalf("Rename %s -> %s failed: %v", filename, rotatedFilename, err)
 			}
 			file, err = os.Create(filename)
 			if err != nil {
@@ -292,11 +305,19 @@ func TestFileRaceCondition(t *testing.T) {
 	// Test the race condition where a log is rotated but the new file isn't
 	// created yet.
 	const (
-		filename        = "/tmp/TestFileRaceCondition"
-		rotatedFilename = filename + ".1"
+		dirName             = "TestFileRaceCondition"
+		baseFilename        = "test_log"
+		rotatedBaseFilename = baseFilename + ".1"
 	)
 
 	// Set up
+	tmpDir, err := ioutil.TempDir("", dirName)
+	if err != nil {
+		t.Fatalf("ioutil.TempDir(\"\", %s) failed: %v", dirName, err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	filename := filepath.Join(tmpDir, baseFilename)
 	file, err := os.Create(filename)
 	if err != nil {
 		t.Fatalf("os.Create(%s) failed: %v", filename, err)
@@ -310,7 +331,7 @@ func TestFileRaceCondition(t *testing.T) {
 
 	w, err := NewWatcher(filename)
 	if err != nil {
-		t.Fatalf("Could not create Watcher: %v", err)
+		t.Fatalf("Could not create Watcher on %s: %v", filename, err)
 	}
 	defer w.Close()
 
@@ -336,9 +357,10 @@ func TestFileRaceCondition(t *testing.T) {
 	}
 
 	// Rotate but don't create new file.
+	rotatedFilename := filepath.Join(tmpDir, rotatedBaseFilename)
 	err = os.Rename(filename, rotatedFilename)
 	if err != nil {
-		t.Fatalf("Rename failed: %v", err)
+		t.Fatalf("Rename %s -> %s failed: %v", filename, rotatedFilename, err)
 	}
 
 	if hasMessage, err := w.HasMessage(target); err != nil {
@@ -367,16 +389,17 @@ func TestFileRaceCondition(t *testing.T) {
 }
 
 func TestWaitForMessageMessageExistsBefore(t *testing.T) {
-	const filename = "/tmp/TestWaitForMessageMessageExistsBefore"
-	file, err := os.Create(filename)
+	const filename = "TestWaitForMessageMessageExistsBefore"
+	file, err := ioutil.TempFile("", filename)
 	if err != nil {
-		t.Fatalf("os.Create(%s) failed: %v", filename, err)
+		t.Fatalf("ioutil.TempFile(\"\", %s) failed: %v", filename, err)
 	}
+	defer os.Remove(file.Name())
 	defer file.Close()
 
-	w, err := NewWatcher(filename)
+	w, err := NewWatcher(file.Name())
 	if err != nil {
-		t.Fatalf("Could not create Watcher: %v", err)
+		t.Fatalf("Could not create Watcher on %s: %v", file.Name(), err)
 	}
 	defer w.Close()
 	if _, err = file.WriteString(target); err != nil {
@@ -392,16 +415,17 @@ func TestWaitForMessageMessageExistsBefore(t *testing.T) {
 }
 
 func TestWaitForMessageMessageAddedDuring(t *testing.T) {
-	const filename = "/tmp/TestWaitForMessageMessageAddedDuring"
-	file, err := os.Create(filename)
+	const filename = "TestWaitForMessageMessageAddedDuring"
+	file, err := ioutil.TempFile("", filename)
 	if err != nil {
-		t.Fatalf("os.Create(%s) failed: %v", filename, err)
+		t.Fatalf("ioutil.TempFile(\"\", %s) failed: %v", filename, err)
 	}
+	defer os.Remove(file.Name())
 	defer file.Close()
 
-	w, err := NewWatcher(filename)
+	w, err := NewWatcher(file.Name())
 	if err != nil {
-		t.Fatalf("Could not create Watcher: %v", err)
+		t.Fatalf("Could not create Watcher on %s: %v", file.Name(), err)
 	}
 	defer w.Close()
 
@@ -425,16 +449,17 @@ func TestWaitForMessageMessageAddedDuring(t *testing.T) {
 }
 
 func TestWaitForMessageFails(t *testing.T) {
-	const filename = "/tmp/TestWaitForMessageFails"
-	file, err := os.Create(filename)
+	const filename = "TestWaitForMessageFails"
+	file, err := ioutil.TempFile("", filename)
 	if err != nil {
-		t.Fatalf("os.Create(%s) failed: %v", filename, err)
+		t.Fatalf("ioutil.TempFile(\"\", %s) failed: %v", filename, err)
 	}
+	defer os.Remove(file.Name())
 	defer file.Close()
 
-	w, err := NewWatcher(filename)
+	w, err := NewWatcher(file.Name())
 	if err != nil {
-		t.Fatalf("Could not create Watcher: %v", err)
+		t.Fatalf("Could not create Watcher on %s: %v", file.Name(), err)
 	}
 	defer w.Close()
 
