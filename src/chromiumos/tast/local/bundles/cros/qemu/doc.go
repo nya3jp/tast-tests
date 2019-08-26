f658e96e77288e53fb565c77e7585eb825af7d10
Keigo Oka <oka@chromium.org>
2019-08-26 16:12:00 +0900

    Port crosvm_Sanity naming it qemu.Sanity
    Ported from
    http://cs/chromeos_public/src/third_party/autotest/files/client/site_tests/crosvm_Sanity/crosvm_Sanity.py?l=1&rcl=701205e5d9a453785fa94021413cce7d993362b0
    
    TEST=ran test on betty.
    TEST=confirmed it doesn't run on careena.
    TEST=fast_build.sh -T # also checked it fails if the deps is commented out.
    BUG=chromium:991147
    
    Cq-Depend: chromium:1772835
    Change-Id: Ib65878e76cfcdb3463aa86eae2d952039fee02f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1768308
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/qemu/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package qemu contains tests exclusive to Chrome OS QEMU images (amd64-generic, betty*) typically running via kvm in developer chroot or builders (usually GCE).
+// Notice that arm images are currently too slow.
+package qemu
