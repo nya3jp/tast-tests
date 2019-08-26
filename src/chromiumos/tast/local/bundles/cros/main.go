77eb853a09f45bd121a8b2cb6ae1f10c7b24b1e7
Keigo Oka <oka@chromium.org>
2019-08-26 16:12:00 +0900

    Port crosvm_Sanity naming it qemu.Sanity
    Ported from
    http://cs/chromeos_public/src/third_party/autotest/files/client/site_tests/crosvm_Sanity/crosvm_Sanity.py?l=1&rcl=701205e5d9a453785fa94021413cce7d993362b0
    
    TEST=ran test on betty.
    TEST=confirmed it doesn't run on careena.
    TEST=fast_build.sh -T # also checked it fails if the deps is commented out.
    BUG=chromium:991147
    
    Cq-Depend: chromium:1772835
    Change-Id: Ib65878e76cfcdb3463aa86eae2d952039fee02f6

--- src/chromiumos/tast/local/bundles/cros/main.go
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -28,6 +28,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/platform"
 	_ "chromiumos/tast/local/bundles/cros/power"
 	_ "chromiumos/tast/local/bundles/cros/printer"
+	_ "chromiumos/tast/local/bundles/cros/qemu"
 	_ "chromiumos/tast/local/bundles/cros/security"
 	_ "chromiumos/tast/local/bundles/cros/session"
 	_ "chromiumos/tast/local/bundles/cros/ui"
