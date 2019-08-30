ac56380bd384b9621edeac834a7d0efdfe694e21
Matthew Wang <matthewmwang@chromium.org>
2019-08-29 19:16:48 -0700

    tast: add network_EthernetStaticIP test
    Add network_EthernetStaticIP test for testing that static IP
    configurations on Ethernet behave as they should.
    
    BUG=None
    TEST=ran network_EthernetStaticIP test
    
    Change-Id: Ifedc243000213fd754a0865ed4a89cdf9762f1ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1777368
    Tested-by: Matthew Wang <matthewmwang@chromium.org>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Brian Norris <briannorris@chromium.org>

--- src/chromiumos/tast/local/shill/shill.go
+++ src/chromiumos/tast/local/shill/shill.go
@@ -24,6 +24,7 @@ const (
 // Property names defined in dbus-constants.h .
 const (
 	// IPConfig property names.
+	IPConfigPropertyAddress     = "Address"
 	IPConfigPropertyNameServers = "NameServers"
 )
 
