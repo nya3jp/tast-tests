e0f4230119ea81e7f7272b9526ecbb61482b22e8
Aseda Aboagye <aaboagye@google.com>
2019-08-30 22:16:33 -0700

    tast-tests: Add usbc/fluffy_interop_std.go.
    This commit adds the first tast test using fluffy.  It is a basic
    interoperability test and the config is specified in
    fluffy_interop_std_config.json.
    
    It simply connects each charger to the DUT and measures the voltage at
    the DUT and verify that the voltage is expected.
    
    BUG=b:140451137
    TEST=`tast -verbose run -checktestdeps=false -var=usbc.MaxPwrReqMW=45000
    <DUT IP> usbc.FluffyInteropStd`
    
    Change-Id: I39b7fcdea33569dd5a2290902c542e8019346658
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

---
+++ src/chromiumos/tast/remote/bundles/cros/usbc/fluffy_interop_std.go
@@ -0,0 +1,126 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package usbc
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"strconv"
+	"time"
+
+	"chromiumos/tast/remote/servo"
+	"chromiumos/tast/testing"
+)
+
+type fluffyPort struct {
+	Port     int
+	Device   string
+	MaxPwrMW float64
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     FluffyInteropStd,
+		Desc:     "Uses fluffy with the standard charger configuration to check that expected voltages are reached",
+		Contacts: []string{"aaboagye@chromium.org"},
+		Data:     []string{"fluffy_interop_std_config.json"},
+		Vars:     []string{"usbc.MaxPwrReqMW"},
+		Attr:     []string{"disabled", "informational"},
+	})
+}
+
+func FluffyInteropStd(c context.Context, s *testing.State) {
+	// Retrieve the maximum power that the DUT will request.
+	maxPwrReq := s.RequiredVar("usbc.MaxPwrReqMW")
+	maxPwr, err := strconv.ParseFloat(maxPwrReq, 32)
+	if err != nil {
+		s.Fatalf("Max Power Request (mW) %q was not a valid number", maxPwrReq)
+	}
+	s.Logf("Assuming DUT will request a max of %s mW", maxPwrReq)
+
+	// Read in the standard fluffy deployment config.
+	s.Log("Reading standard config file")
+	cfgfile, err := ioutil.ReadFile(s.DataPath("fluffy_interop_std_config.json"))
+	if err != nil {
+		s.Fatal("Failed to read standard config file: ", err)
+	}
+
+	var cfg []fluffyPort
+	err = json.Unmarshal(cfgfile, &cfg)
+	if err != nil {
+		s.Fatal("Error decoding JSON file: ", err)
+	}
+
+	// Setup a servo host connected to fluffy.  This connects to the servod running on localhost:9999
+	s.Log("Setting up connection to servod at localhost:9999")
+	fluffy, err := servo.Default(c)
+	if err != nil {
+		s.Fatal("Failed to connect to servod: ", err)
+	}
+
+	// For each charger, enable it, wait a bit and verify that the expected voltage is reached (with some margin).
+	for p := range cfg {
+		s.Logf("Connecting port %d (%s)", p, cfg[p].Device)
+		// Call to servod to enable the port.
+		_, err = fluffy.SetActChgPort(c, strconv.Itoa(cfg[p].Port))
+		if err != nil {
+			s.Error("Could not set active charge port: ", err)
+		}
+
+		// Wait 1.5 seconds to allow the DUT to select the right PDO; this should be plenty of time.
+		testing.Sleep(c, 1500*time.Millisecond)
+
+		// Verify that the voltage reached is as expected.
+		voltageMV, err := fluffy.DutVoltageMV(c)
+		if err != nil {
+			s.Error("Failed to read DUT voltage: ", err)
+		}
+		measuredVoltageMV, err := strconv.ParseFloat(voltageMV, 32)
+		if err != nil {
+			s.Error("Failed to convert string to float64: ", err)
+		}
+
+		// Limit the max power to either what the DUT can request or what the charger supports.
+		var expectedPwrMW float64
+		if maxPwr > cfg[p].MaxPwrMW {
+			expectedPwrMW = cfg[p].MaxPwrMW
+		} else {
+			expectedPwrMW = maxPwr
+		}
+
+		// Select the correct voltage level depending upon the maximum power.  All PD compliant chargers should satisfy this.
+		var expectedVoltageMV float64
+		if expectedPwrMW > 45000 {
+			expectedVoltageMV = 20000
+		} else if expectedPwrMW > 27000 {
+			expectedVoltageMV = 15000
+		} else if expectedPwrMW > 15000 {
+			expectedVoltageMV = 9000
+		} else {
+			expectedVoltageMV = 5000
+		}
+
+		// Set the min and max voltage per the USB PD spec.
+		maxExpectedVoltageMV := expectedVoltageMV * 1.05
+		minExpectedVoltageMV := (0.95 * expectedVoltageMV) - 750
+
+		// Fluffy seems to read a bit high (b/140393065), move the entire range up by 300mV.
+		maxExpectedVoltageMV += 300
+		minExpectedVoltageMV += 300
+
+		// Additionally, add a 200mV margin; this is wide but should still generally work.
+		maxExpectedVoltageMV += 200
+		minExpectedVoltageMV -= 200
+
+		if (measuredVoltageMV > maxExpectedVoltageMV) || (measuredVoltageMV < minExpectedVoltageMV) {
+			s.Errorf("FAIL! [Port %d: %s] (Measured %.3fV. Should be %.3fV < x < %.3fV)", p, cfg[p].Device, measuredVoltageMV/1000,
+				minExpectedVoltageMV/1000, maxExpectedVoltageMV/1000)
+		}
+	}
+
+	// Turn off the ports when finished testing.
+	fluffy.SetActChgPort(c, "off")
+}
