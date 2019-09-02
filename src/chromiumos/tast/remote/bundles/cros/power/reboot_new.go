a28f2ac7418826c613cd7a822a05e0f5a316de2a
Shuhei Takahashi <nya@chromium.org>
2019-09-02 16:31:07 +0900

    tast-tests: Experiment a new reboot approach.
    Add power.RebootNew test which exercises the new DUT.Reboot method.
    This test will replace power.Reboot once we ensure it is stable.
    
    BUG=chromium:971024
    TEST=tast run DUT power.RebootNew
    
    Cq-Depend: chromium:1781294
    Change-Id: I5c44a368173ecd872cd79090a6a63a94187ee413

---
+++ src/chromiumos/tast/remote/bundles/cros/power/reboot_new.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RebootNew,
+		Desc:         "Verifies that system comes back after rebooting (with a new reboot method)",
+		Contacts:     []string{"nya@chromium.org", "tast-owners@google.com"},
+		SoftwareDeps: []string{"reboot"},
+		Attr:         []string{"informational"},
+	})
+}
+
+func RebootNew(ctx context.Context, s *testing.State) {
+	d, ok := dut.FromContext(ctx)
+	if !ok {
+		s.Fatal("Failed to get DUT")
+	}
+
+	if err := d.Reboot(ctx); err != nil {
+		s.Fatal("Failed to reboot DUT: ", err)
+	}
+}
