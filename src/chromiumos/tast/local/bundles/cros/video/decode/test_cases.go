4fbc945160af13f2901cf1f2c343bee693ce9cfb
David Staessens <dstaessens@chromium.org>
2019-09-06 10:44:13 +0900

    video.DecodeAccelVP9ShowExistingFrame: Add VP9 show-existing-frame video test.
    The VP9 show-existing-frame feature is currently only tested by the
    video.DecodeAccelSanityVP90CtsShowExistingFrame test. But this sanity check only
    verifies whether the device doesn't crash, rather than checking whether decoding
    is successful.
    
    This CL turns the video.DecodeAccelSanityVP90CtsShowExistingFrame test into a
    normal video test that properly verifies correct behavior while decoding a VP9
    video using the show-existing-frame feature.
    
    TEST=video.DecodeAccelVP9ShowExistingFrame on eve
    
    BUG=b:140591559
    
    Change-Id: Icd478cc1ff8419503a72de21cb10f48b24160a70

--- src/chromiumos/tast/local/bundles/cros/video/decode/test_cases.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode/test_cases.go
@@ -82,18 +82,3 @@ var DecodeAccelSanityVP93 = TestVideoData{
 	MinFPSNoRender:   0,
 	Profile:          videotype.VP9Prof,
 }
-
-// DecodeAccelSanityVP90CtsShowExistingFrame is the test parameters of
-// video_decode_accelerator_unittest for "vda_sanity-vp90_2_17_show_existing_frame.vp9".
-// It is from Android CTS:
-// https://android.googlesource.com/platform/cts/+/master/tests/tests/media/res/raw/vp90_2_17_show_existing_frame.vp9
-// which causes elm kernel crash (crbug.com/900467).
-var DecodeAccelSanityVP90CtsShowExistingFrame = TestVideoData{
-	Name:             "vda_sanity-vp90_2_17_show_existing_frame.vp9",
-	Size:             videotype.NewSize(352, 288),
-	NumFrames:        30,
-	NumFragments:     30,
-	MinFPSWithRender: 0,
-	MinFPSNoRender:   0,
-	Profile:          videotype.VP9Prof,
-}
