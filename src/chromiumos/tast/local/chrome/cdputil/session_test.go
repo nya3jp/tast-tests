234d299a28ded384f43a8116a5b01faf07195151
Jun Mukai <mukai@google.com>
2019-09-06 11:00:40 -0700

    tast-test: allow Chrome.NewConn to create a new window
    This relies on cdp-0.24.2.
    
    BUG=chromium:1000356
    TEST=fast_build.sh -T, fast_build.sh -C
    
    Change-Id: Ib337de3ef541ff36abe42c61916e578ba8bd1d19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1790624
    Tested-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/chrome/cdputil/session_test.go
+++ src/chromiumos/tast/local/chrome/cdputil/session_test.go
@@ -5,11 +5,19 @@
 package cdputil
 
 import (
+	"context"
+	"encoding/json"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/mafredri/cdp"
+	"github.com/mafredri/cdp/protocol/target"
+	"github.com/mafredri/cdp/rpcc"
+
+	"chromiumos/tast/errors"
 	"chromiumos/tast/testutil"
 )
 
@@ -46,5 +54,157 @@ func TestReadDebuggingPort(t *testing.T) {
 
 	if _, err := readDebuggingPort(filepath.Join(td, "missing")); err == nil {
 		t.Error("readDebuggingPort didn't return expected error for missing file")
+	}
+}
+
+type dummyConn struct {
+	done   chan struct{}
+	cancel context.CancelFunc
+}
+
+func (dc *dummyConn) Read(p []byte) (n int, err error) {
+	return 0, errors.New("not implemented")
+}
+
+func (dc *dummyConn) Write(p []byte) (n int, err error) {
+	return 0, errors.New("not implemented")
+}
+
+func (dc *dummyConn) Close() error {
+	dc.cancel()
+	return nil
+}
+
+func newDummyConn(ctx context.Context, addr string) (io.ReadWriteCloser, error) {
+	ctx, cancel := context.WithCancel(ctx)
+	dc := &dummyConn{done: make(chan struct{}), cancel: cancel}
+	go func() {
+		select {
+		case <-ctx.Done():
+			close(dc.done)
+		}
+	}()
+	return dc, nil
+}
+
+type dummyCodec struct {
+	dconn         *dummyConn
+	ch            chan uint64
+	requestedArgs []interface{}
+	responses     []interface{}
+}
+
+func (dc *dummyCodec) ReadResponse(resp *rpcc.Response) error {
+	var id uint64
+	select {
+	case id = <-dc.ch:
+		break
+	case <-dc.dconn.done:
+		return errors.New("already closed")
+	}
+
+	if len(dc.responses) == 0 {
+		return errors.New("no expected responses found")
+	}
+	nextResponse := dc.responses[0]
+	dc.responses = dc.responses[1:]
+	var err error
+	if resp.Result, err = json.Marshal(nextResponse); err != nil {
+		return err
+	}
+	resp.ID = id
+	return nil
+}
+
+func (dc *dummyCodec) WriteRequest(req *rpcc.Request) error {
+	dc.requestedArgs = append(dc.requestedArgs, req.Args)
+	select {
+	case dc.ch <- req.ID:
+		return nil
+	case <-dc.dconn.done:
+		return errors.New("already closed")
+	}
+}
+
+func (dc *dummyCodec) AppendExpectedResponse(resp interface{}) {
+	dc.responses = append(dc.responses, resp)
+}
+
+func newDummySession() (sess *Session, dc *dummyCodec, err error) {
+	dc = &dummyCodec{ch: make(chan uint64)}
+	conn, err := rpcc.Dial("dummy", rpcc.WithDialer(newDummyConn), rpcc.WithCodec(func(conn io.ReadWriter) rpcc.Codec {
+		dc.dconn = conn.(*dummyConn)
+		return dc
+	}))
+	if err != nil {
+		return nil, nil, err
+	}
+	return &Session{client: cdp.NewClient(conn)}, dc, nil
+}
+
+func TestCreateTarget(t *testing.T) {
+	for _, c := range []struct {
+		name               string
+		opts               []CreateTargetOption
+		expectedNewWindow  bool
+		expectedBackground bool
+	}{
+		{
+			name: "no-options",
+		},
+		{
+			name:              "new-window",
+			opts:              []CreateTargetOption{WithNewWindow()},
+			expectedNewWindow: true,
+		},
+		{
+			name:               "background",
+			opts:               []CreateTargetOption{WithBackground()},
+			expectedBackground: true,
+		},
+		{
+			name:               "both",
+			opts:               []CreateTargetOption{WithNewWindow(), WithBackground()},
+			expectedNewWindow:  true,
+			expectedBackground: true,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			sess, dc, err := newDummySession()
+			if err != nil {
+				t.Fatal("Failed to dial: ", err)
+			}
+			dc.AppendExpectedResponse(&target.CreateTargetReply{TargetID: "dummy-id"})
+			id, err := sess.CreateTarget(ctx, "about:blank", c.opts...)
+			if err != nil {
+				t.Fatal("Failed to call CreateTarget: ", err)
+			}
+			if id != "dummy-id" {
+				t.Errorf("Expected dummy-id, got %s", id)
+			}
+			if len(dc.requestedArgs) != 1 {
+				t.Fatalf("Expected number of requests 1, got %d", len(dc.requestedArgs))
+			}
+			lastArgs, ok := dc.requestedArgs[0].(*target.CreateTargetArgs)
+			if !ok {
+				t.Fatalf("The lastRequestArgs expected to be a CreateTargetArgs instance, got %v", lastArgs)
+			}
+			gotNewWindow := false
+			if lastArgs.NewWindow != nil {
+				gotNewWindow = *lastArgs.NewWindow
+			}
+			if gotNewWindow != c.expectedNewWindow {
+				t.Errorf("NewWindow parameter expected %v, but got %v", c.expectedNewWindow, gotNewWindow)
+			}
+			gotBackground := false
+			if lastArgs.Background != nil {
+				gotBackground = *lastArgs.Background
+			}
+			if gotBackground != c.expectedBackground {
+				t.Errorf("Background parameter expected %v, but got %v", c.expectedBackground, gotBackground)
+			}
+		})
 	}
 }
