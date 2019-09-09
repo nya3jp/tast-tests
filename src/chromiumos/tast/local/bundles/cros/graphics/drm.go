b125a0283200d9298cd9433f80f8a0347792ce49
Po-Hsien Wang <pwang@chromium.org>
2019-09-09 15:11:07 -0700

    graphics: Use parameterize feature in Drm tests
    This fixes few issues on tast Drm Test to match the behavior in
    autotest.
    
    1. Change Drm.go to use the parameterize feature, which is much cleaner in my point of view.
    2. vgem_test not in graphics_Drm.bvt. Drop it from informational and add swrast_test to informational.
    3. Delete duplicate tests, e.g drm_*.go
    4. Add an DRM.all tests which runs all drm tests as of graphics_DRM.
    
    BUG=None
    TEST=tast run ${fizz} graphics.Drm.bvt  # Only swrast_test is run with connector disconnected
    TEST=tast run ${fizz} graphics.Drm.bvt  # Run all four tests with connected connected
    
    Change-Id: If00aa9965d65b9da797c18167f0407ca77fe4bae

--- src/chromiumos/tast/local/bundles/cros/graphics/drm.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/drm.go
@@ -6,13 +6,37 @@ package graphics
 
 import (
 	"context"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
-	"chromiumos/tast/local/bundles/cros/graphics/drm"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/graphics"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/shutil"
 	"chromiumos/tast/testing"
 )
 
+type option struct {
+	command string
+	timeout time.Duration
+	runDeps []string
+}
+
 func init() {
+	allTestOpts := map[string]option{
+		"atomic_test":        {command: "atomictest -a -t all", timeout: 5 * time.Minute, runDeps: []string{"display_backlight", "drm_atomic"}},
+		"drm_cursor_test":    {command: "drm_cursor_test", timeout: 20 * time.Second, runDeps: []string{"display_backlight"}},
+		"mmap_test":          {command: "mmap_test", timeout: 5 * time.Minute, runDeps: []string{"display_backlight"}},
+		"null_platform_test": {command: "null_platform_test", timeout: 20 * time.Second, runDeps: []string{"display_backlight"}},
+		"linear_bo_test":     {command: "linear_bo_test", timeout: 20 * time.Second, runDeps: []string{"display_backlight"}},
+		"swrast_test":        {command: "swrast_test", timeout: 20 * time.Second, runDeps: []string{}},
+		"vgem_test":          {command: "vgem_test", timeout: 20 * time.Second, runDeps: []string{"display_backlight"}},
+		"vk_glow":            {command: "vk_glow", timeout: 20 * time.Second, runDeps: []string{"display_backlight", "vulkan"}},
+	}
+
 	testing.AddTest(&testing.Test{
 		Func: DRM,
 		Desc: "Verifies DRM-related test binaries run successfully",
@@ -21,20 +45,155 @@ func init() {
 			"chromeos-gfx@google.com",
 			"hidehiko@chromium.org", // Tast port.
 		},
-		Attr:         []string{"informational"},
-		SoftwareDeps: []string{"display_backlight"},
+		Params: []testing.Param{{
+			Name: "all",
+			Val: []option{
+				allTestOpts["atomic_test"],
+				allTestOpts["drm_cursor_test"],
+				allTestOpts["mmap_test"],
+				allTestOpts["null_platform_test"],
+				allTestOpts["linear_bo_test"],
+				allTestOpts["swrast_test"],
+				allTestOpts["vgem_test"],
+				allTestOpts["vk_glow"],
+			},
+			ExtraAttr: []string{"group:crosbolt", "crosbolt_nightly"},
+		}, {
+			Name: "bvt",
+			Val: []option{
+				allTestOpts["drm_cursor_test"],
+				allTestOpts["linear_bo_test"],
+				allTestOpts["null_platform_test"],
+				allTestOpts["swrast_test"]},
+			ExtraAttr: []string{"informational"},
+		}, { // All the remaining tests are disabled and only can be run manually.
+			Name:              "atomic_test",
+			Val:               []option{allTestOpts["atomic_test"]},
+			ExtraSoftwareDeps: allTestOpts["atomic_test"].runDeps,
+			ExtraAttr:         []string{"disabled"},
+		}, {
+			Name:              "drm_cursor_test",
+			Val:               []option{allTestOpts["drm_cursor_test"]},
+			ExtraSoftwareDeps: allTestOpts["drm_cursor_test"].runDeps,
+			ExtraAttr:         []string{"disabled"},
+		}, {
+			Name:              "mmap_test",
+			Val:               []option{allTestOpts["mmap_test"]},
+			ExtraSoftwareDeps: allTestOpts["mmap_test"].runDeps,
+			ExtraAttr:         []string{"disabled"},
+		}, {
+			Name:              "null_platform_test",
+			Val:               []option{allTestOpts["null_platform_test"]},
+			ExtraSoftwareDeps: allTestOpts["null_platform_test"].runDeps,
+			ExtraAttr:         []string{"disabled"},
+		}, {
+			Name:              "linear_bo_test",
+			Val:               []option{allTestOpts["linear_bo_test"]},
+			ExtraSoftwareDeps: allTestOpts["linear_bo_test"].runDeps,
+			ExtraAttr:         []string{"disabled"},
+		}, {
+			Name:              "swrast_test",
+			Val:               []option{allTestOpts["swrast_test"]},
+			ExtraSoftwareDeps: allTestOpts["swrast_test"].runDeps,
+			ExtraAttr:         []string{"disabled"},
+		}, {
+			Name:              "vgem_test",
+			Val:               []option{allTestOpts["vgem_test"]},
+			ExtraSoftwareDeps: allTestOpts["vgem_test"].runDeps,
+			ExtraAttr:         []string{"disabled"},
+		}, {
+			Name:              "vk_glow",
+			Val:               []option{allTestOpts["vk_glow"]},
+			ExtraSoftwareDeps: allTestOpts["vk_glow"].runDeps,
+			ExtraAttr:         []string{"disabled"},
+		}},
+		Timeout: 5 * time.Minute,
 	})
 }
 
 func DRM(ctx context.Context, s *testing.State) {
-	if err := drm.SetUp(ctx); err != nil {
+	if err := setUp(ctx); err != nil {
 		s.Fatal("Failed to set up the DRM test: ", err)
 	}
-	defer drm.TearDown(ctx)
+	defer tearDown(ctx)
 
-	const timeout = 20 * time.Second
-	drm.RunTest(ctx, s, timeout, "/usr/local/bin/drm_cursor_test")
-	drm.RunTest(ctx, s, timeout, "/usr/local/bin/linear_bo_test")
-	drm.RunTest(ctx, s, timeout, "/usr/local/bin/null_platform_test")
-	drm.RunTest(ctx, s, timeout, "/usr/local/bin/vgem_test")
+	testOpts := s.Param().([]option)
+	for _, testOpt := range testOpts {
+		shouldRun, err := checkRunAttr(ctx, testOpt.runDeps)
+		if err != nil {
+			s.Error("Failed to check attribute: ", err)
+		}
+		if !shouldRun {
+			continue
+		}
+
+		timeout := 20 * time.Second
+		if testOpt.timeout > timeout {
+			timeout = testOpt.timeout
+		}
+		splitCommand := strings.Fields(testOpt.command)
+		cmd := "/usr/local/bin/" + splitCommand[0]
+		runTest(ctx, s, timeout, cmd, splitCommand[1:]...)
+	}
+}
+
+// checkRunAttr checks the runDeps config to decide should we run the test.
+func checkRunAttr(ctx context.Context, attrs []string) (bool, error) {
+	for _, attr := range attrs {
+		switch attr {
+		case "display_backlight":
+			connectCount, err := graphics.GetNumberOfOutputsConnected(ctx)
+			if err != nil {
+				return false, errors.Wrapf(err, "failed to check test attribute %s", attr)
+			}
+			if connectCount == 0 {
+				testing.ContextLog(ctx, "No connector detected. Skipping test")
+				return false, nil
+			}
+		case "vulkan":
+			hasVulkan, err := graphics.SupportsVulkanForDEQP(ctx)
+			if err != nil {
+				return false, errors.Wrapf(err, "failed to check test attribute %s", attr)
+			}
+			if !hasVulkan {
+				testing.ContextLog(ctx, "Vulkan is not available. Skipping test")
+				return false, nil
+			}
+		default:
+			testing.ContextLogf(ctx, "Unrocognized runAttr %s. Assuming it is guarded by tast SoftwareDeps", attr)
+		}
+	}
+	return true, nil
+}
+
+// setUp prepares the testing environment to run RunTest().
+func setUp(ctx context.Context) error {
+	testing.ContextLog(ctx, "Setting up DRM test")
+	return upstart.StopJob(ctx, "ui")
+}
+
+// tearDown restores the working environment after RunTest().
+func tearDown(ctx context.Context) error {
+	testing.ContextLog(ctx, "Tearing down DRM test")
+	return upstart.EnsureJobRunning(ctx, "ui")
+}
+
+// runTest runs the exe binary test. Before running, SetUp() must be called.
+func runTest(ctx context.Context, s *testing.State, t time.Duration, exe string, args ...string) {
+	s.Log("Running ", shutil.EscapeSlice(append([]string{exe}, args...)))
+
+	f, err := os.Create(filepath.Join(s.OutDir(), filepath.Base(exe)+".txt"))
+	if err != nil {
+		s.Fatal("Failed to create a log file: ", err)
+	}
+	defer f.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, t)
+	defer cancel()
+	cmd := testexec.CommandContext(ctx, exe, args...)
+	cmd.Stdout = f
+	cmd.Stderr = f
+	if err := cmd.Run(); err != nil {
+		s.Errorf("Failed to run %s: %v", exe, err)
+	}
 }
