3c0bb1395ac39c7ec2042b0f7698f729e3914f55
Satoshi Niwa <niwa@google.com>
2019-09-12 16:29:45 +0900

    tast: Add a test to verify ARC(VM) can run in parallel with Crostini
    TEST=tast run my_crbook crostini.RunWithARC
    BUG=b:139317656
    
    Change-Id: I68df1e3053ec683da12ec10f92ff20ae9f605761

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/run_with_arc.go
@@ -0,0 +1,61 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/crostini/sanity"
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RunWithARC,
+		Desc:         "Checks that ARC(VM) runs in parallel with Crostini",
+		Contacts:     []string{"niwa@chromium.org", "arcvm-eng@google.com"},
+		Attr:         []string{"informational"},
+		Timeout:      7 * time.Minute,
+		Data:         []string{crostini.ImageArtifact},
+		Pre:          crostini.StartedARCEnabled(),
+		SoftwareDeps: []string{"chrome", "vm_host", "android_both"},
+	})
+}
+
+func RunWithARC(ctx context.Context, s *testing.State) {
+	sanity.RunTest(ctx, s, s.PreValue().(crostini.PreData).Container)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	// Ensures package manager service is running by checking the existence of
+	// "android" package.
+	out, err :=
+	    a.Command(ctx, "pm", "list", "packages").Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("pm list failed: ", err)
+	}
+
+	pkgs := strings.Split(string(out), "\n")
+	found := false
+	for _, p := range pkgs {
+		if p == "package:android" {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		s.Fatal("android package not found: ", pkgs)
+	}
+}
