51bdaf7cec91888ac79bd3344e0574bde0fedc38
Miriam Zimmerman <mutexlox@google.com>
2019-09-12 20:15:02 -0700

    tast: Give crash tests a clean crash directory.
    If /var/spool/crash is full when a crash test starts, it will have
    unexpected failures due to being unable to save crashes. Instead, start
    it with an empty directory.  (It's still possible this will flake if
    another process is _very_ crashy to the point that it crashes 32 times
    during the test run, but that's relatively unlikely. If it becomes a
    problem, we can also edit core_pattern to use --filter_in to ignore
    other crashes during the test run.)
    
    TEST=tast -verbose run $DEVICE_IP platform.KernelWarning
    BUG=chromium:998986
    
    Change-Id: Idb1bd366d3573efde4d4e271b4ade54ad1adba61

--- src/chromiumos/tast/local/crash/crash_test.go
+++ src/chromiumos/tast/local/crash/crash_test.go
@@ -14,22 +14,72 @@ import (
 func TestSetUpAndTearDownCrashTest(t *testing.T) {
 	// We can't use the normal file location /run/crash_reporter; we don't have
 	// permission to write there. Instead write to a location under /tmp.
-	dir, err := ioutil.TempDir("", "TestSetUpAndTearDownCrashTest")
+	runDir, err := ioutil.TempDir("", "TestSetUpAndTearDownCrashTest")
 	if err != nil {
 		t.Fatalf("ioutil.TempDir: %v", err)
 	}
+	defer os.RemoveAll(runDir)
 
-	if err := setUpCrashTestWithDirectory(dir); err != nil {
-		t.Fatalf("setUpCrashTestWithDirectory(%s): %v", dir, err)
+	// Create and populate spool and stash directories.
+	sysCrashDir, err := ioutil.TempDir("", "TestSetUpAndTearDownCrashTest")
+	if err != nil {
+		t.Fatalf("ioutil.TempDir: %v", err)
+	}
+	defer os.RemoveAll(sysCrashDir)
+	if err := ioutil.WriteFile(filepath.Join(sysCrashDir, "sysCrash.log"), []byte(""), 0744); err != nil {
+		t.Fatalf("ioutil.WriteFile: %v", err)
+	}
+	sysStashDir, err := ioutil.TempDir("", "TestSetUpAndTearDownCrashTest")
+	if err != nil {
+		t.Fatalf("ioutil.TempDir: %v", err)
+	}
+	defer os.RemoveAll(sysStashDir)
+	if err := ioutil.WriteFile(filepath.Join(sysStashDir, "oldSysCrash.log"), []byte(""), 0744); err != nil {
+		t.Fatalf("ioutil.WriteFile: %v", err)
 	}
 
-	file := filepath.Join(dir, "crash-test-in-progress")
+	userCrashDir, err := ioutil.TempDir("", "TestSetUpAndTearDownCrashTest")
+	if err != nil {
+		t.Fatalf("ioutil.TempDir: %v", err)
+	}
+	defer os.RemoveAll(userCrashDir)
+	if err := ioutil.WriteFile(filepath.Join(userCrashDir, "userCrash.log"), []byte(""), 0744); err != nil {
+		t.Fatalf("ioutil.WriteFile: %v", err)
+	}
+	userStashDir, err := ioutil.TempDir("", "TestSetUpAndTearDownCrashTest")
+	defer os.RemoveAll(userStashDir)
+	if err != nil {
+		t.Fatalf("ioutil.TempDir: %v", err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(userStashDir, "oldUserCrash.log"), []byte(""), 0744); err != nil {
+		t.Fatalf("ioutil.WriteFile: %v", err)
+	}
+
+	if err := setUpCrashTestWithDirectories(runDir, sysCrashDir, sysStashDir, userCrashDir, userStashDir); err != nil {
+		t.Fatalf("setUpCrashTestWithDirectories(%s, %s, %s, %s, %s): %v", runDir, sysCrashDir, sysStashDir, userCrashDir, userStashDir, err)
+	}
+
+	if _, err := ioutil.ReadFile(filepath.Join(sysStashDir, "sysCrash.log")); err != nil {
+		t.Errorf("Didn't move sysCrash to stash: %v", err)
+	}
+	if _, err := ioutil.ReadFile(filepath.Join(sysStashDir, "oldSysCrash.log")); err != nil {
+		t.Errorf("Didn't preserve oldSysCrash in stash: %v", err)
+	}
+
+	if _, err := ioutil.ReadFile(filepath.Join(userStashDir, "userCrash.log")); err != nil {
+		t.Errorf("Didn't move userCrash to stash: %v", err)
+	}
+	if _, err := ioutil.ReadFile(filepath.Join(userStashDir, "oldUserCrash.log")); err != nil {
+		t.Errorf("Didn't preserve oldUserCrash in stash: %v", err)
+	}
+
+	file := filepath.Join(runDir, "crash-test-in-progress")
 	if _, err := os.Stat(file); err != nil {
 		t.Errorf("Cannot stat %s: %v", file, err)
 	}
 
-	if err := tearDownCrashTestWithDirectory(dir); err != nil {
-		t.Errorf("tearDownCrashTestWithDirectory(%s): %v", dir, err)
+	if err := tearDownCrashTestWithDirectories(runDir, sysCrashDir, sysStashDir, userCrashDir, userStashDir); err != nil {
+		t.Errorf("tearDownCrashTestWithDirectories(%s, %s, %s, %s, %s): %v", runDir, sysCrashDir, sysStashDir, userCrashDir, userStashDir, err)
 	}
 
 	if _, err := os.Stat(file); err == nil {
