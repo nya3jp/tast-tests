f9398fd3adf71c4e2a6e532e45ac84e2fa68476d
Po-Hsien Wang <pwang@chromium.org>
2019-09-17 11:50:27 -0700

    graphics: Use parameterize feature in Drm tests
    Change Drm.go to use the parameterize feature, which is much cleaner in my point of view.
    And delete duplicate tests, e.g drm_*.go
    
    BUG=chromium:1005408
    TEST=tast run ${fizz} graphics.Drm.*
    
    Change-Id: If00aa9965d65b9da797c18167f0407ca77fe4bae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1794102
    Tested-by: Po-Hsien Wang <pwang@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Auto-Submit: Po-Hsien Wang <pwang@chromium.org>
    Commit-Queue: Po-Hsien Wang <pwang@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/graphics/drm.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/drm.go
@@ -6,13 +6,34 @@ package graphics
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"time"
 
-	"chromiumos/tast/local/bundles/cros/graphics/drm"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/shutil"
 	"chromiumos/tast/testing"
 )
 
+// drmTest is used to describe the config used to run each drm_test.
+type drmTest struct {
+	command []string      // The command path to be run. This should be relative to /usr/local/bin.
+	timeout time.Duration // Timeout to run the drmTest.
+}
+
 func init() {
+	var (
+		atomic       = drmTest{command: []string{"atomictest", "-a", "-t", "all"}, timeout: 5 * time.Minute}
+		drmCursor    = drmTest{command: []string{"drm_cursor_test"}, timeout: 20 * time.Second}
+		linearBo     = drmTest{command: []string{"linear_bo_test"}, timeout: 20 * time.Second}
+		mmap         = drmTest{command: []string{"mmap_test"}, timeout: 5 * time.Minute}
+		nullPlatform = drmTest{command: []string{"null_platform_test"}, timeout: 20 * time.Second}
+		swrast       = drmTest{command: []string{"swrast_test"}, timeout: 20 * time.Second}
+		vgem         = drmTest{command: []string{"vgem_test"}, timeout: 20 * time.Second}
+		vkGlow       = drmTest{command: []string{"vk_glow"}, timeout: 20 * time.Second}
+	)
+
 	testing.AddTest(&testing.Test{
 		Func: DRM,
 		Desc: "Verifies DRM-related test binaries run successfully",
@@ -21,20 +42,79 @@ func init() {
 			"chromeos-gfx@google.com",
 			"hidehiko@chromium.org", // Tast port.
 		},
-		Attr:         []string{"informational"},
-		SoftwareDeps: []string{"display_backlight"},
+		Params: []testing.Param{{
+			Name: "bvt",
+			Val: []drmTest{
+				drmCursor,
+				linearBo,
+				nullPlatform,
+				vgem},
+			ExtraSoftwareDeps: []string{"display_backlight"},
+			ExtraAttr:         []string{"informational"},
+		}, {
+			Name:              "atomic_test",
+			Val:               []drmTest{atomic},
+			ExtraSoftwareDeps: []string{"display_backlight", "drm_atomic"},
+			ExtraAttr:         []string{"informational"},
+		}, {
+			Name:              "mmap_test",
+			Val:               []drmTest{mmap},
+			ExtraSoftwareDeps: []string{"display_backlight"},
+			ExtraAttr:         []string{"informational"},
+		}, {
+			Name:      "swrast_test",
+			Val:       []drmTest{swrast},
+			ExtraAttr: []string{"informational"},
+		}, {
+			Name:              "vk_glow",
+			Val:               []drmTest{vkGlow},
+			ExtraSoftwareDeps: []string{"display_backlight", "vulkan"},
+			ExtraAttr:         []string{"informational"},
+		}},
+		Timeout: 5 * time.Minute,
 	})
 }
 
 func DRM(ctx context.Context, s *testing.State) {
-	if err := drm.SetUp(ctx); err != nil {
+	if err := setUp(ctx); err != nil {
 		s.Fatal("Failed to set up the DRM test: ", err)
 	}
-	defer drm.TearDown(ctx)
+	defer tearDown(ctx)
 
-	const timeout = 20 * time.Second
-	drm.RunTest(ctx, s, timeout, "/usr/local/bin/drm_cursor_test")
-	drm.RunTest(ctx, s, timeout, "/usr/local/bin/linear_bo_test")
-	drm.RunTest(ctx, s, timeout, "/usr/local/bin/null_platform_test")
-	drm.RunTest(ctx, s, timeout, "/usr/local/bin/vgem_test")
+	testOpts := s.Param().([]drmTest)
+	for _, testOpt := range testOpts {
+		runTest(ctx, s, testOpt.timeout, testOpt.command[0], testOpt.command[1:]...)
+	}
+}
+
+// setUp prepares the testing environment to run runTest().
+func setUp(ctx context.Context) error {
+	testing.ContextLog(ctx, "Setting up DRM test")
+	return upstart.StopJob(ctx, "ui")
+}
+
+// tearDown restores the working environment after runTest().
+func tearDown(ctx context.Context) error {
+	testing.ContextLog(ctx, "Tearing down DRM test")
+	return upstart.EnsureJobRunning(ctx, "ui")
+}
+
+// runTest runs the exe binary test. This method may be called several times as long as setUp() has been invoked beforehand.
+func runTest(ctx context.Context, s *testing.State, t time.Duration, exe string, args ...string) {
+	s.Log("Running ", shutil.EscapeSlice(append([]string{exe}, args...)))
+
+	f, err := os.Create(filepath.Join(s.OutDir(), filepath.Base(exe)+".txt"))
+	if err != nil {
+		s.Fatal("Failed to create a log file: ", err)
+	}
+	defer f.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, t)
+	defer cancel()
+	cmd := testexec.CommandContext(ctx, exe, args...)
+	cmd.Stdout = f
+	cmd.Stderr = f
+	if err := cmd.Run(); err != nil {
+		s.Errorf("Failed to run %s: %v", exe, err)
+	}
 }
