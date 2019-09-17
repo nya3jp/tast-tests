d2a5a6ac38f52770c106daf128fad20dfef61dff
Po-Hsien Wang <pwang@chromium.org>
2019-09-17 12:03:54 -0700

    graphics: check runtime deps in drm tests
    drm tests only required a display connected. It does not need to be
    an internal display. Add runDeps to determine if we need to run it
    or not.
    
    BUG=chromium:1005408
    TEST=tast run ${fizz} graphics.DRM.* # All pass but only swrast_test is run with display disconnected
    TEST=tast run ${fizz} graphics.DRM.* # All pass and run with display connected
    
    Change-Id: I9e00183b1bd480cb71748bbbc2b16568ae56bbb3

--- src/chromiumos/tast/local/bundles/cros/graphics/drm.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/drm.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"time"
 
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/graphics"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/shutil"
@@ -20,6 +22,7 @@ import (
 type drmTest struct {
 	command []string      // The command path to be run. This should be relative to /usr/local/bin.
 	timeout time.Duration // Timeout to run the drmTest.
+	runDeps []string      // The dependencies that should be check in runtime.
 }
 
 func init() {
@@ -31,40 +34,43 @@ func init() {
 			"chromeos-gfx@google.com",
 			"hidehiko@chromium.org", // Tast port.
 		},
+		// TODO(pwang): Add Hardware dependency for ChromeOS lab to optimize scheduling once HWDeps is supported.
+		// Instead of SoftwareDep, we also check for runtime dependency for developer to run tests in various environment.
 		Params: []testing.Param{{
 			Name: "atomic_test",
 			Val: drmTest{
 				command: []string{"atomictest", "-a", "-t", "all"},
 				timeout: 5 * time.Minute,
+				runDeps: []string{"display_connected", "drm_atomic"},
 			},
-			ExtraSoftwareDeps: []string{"display_backlight", "drm_atomic"},
+			ExtraSoftwareDeps: []string{"drm_atomic"},
 		}, {
 			Name:              "drm_cursor_test",
-			Val:               drmTest{command: []string{"drm_cursor_test"}, timeout: 20 * time.Second},
-			ExtraSoftwareDeps: []string{"display_backlight"},
+			Val:               drmTest{command: []string{"drm_cursor_test"}, timeout: 20 * time.Second, runDeps: []string{"display_connected"}},
+			ExtraSoftwareDeps: []string{},
 		}, {
 			Name:              "linear_bo_test",
-			Val:               drmTest{command: []string{"linear_bo_test"}, timeout: 20 * time.Second},
-			ExtraSoftwareDeps: []string{"display_backlight"},
+			Val:               drmTest{command: []string{"linear_bo_test"}, timeout: 20 * time.Second, runDeps: []string{"display_connected"}},
+			ExtraSoftwareDeps: []string{},
 		}, {
 			Name:              "mmap_test",
-			Val:               drmTest{command: []string{"mmap_test"}, timeout: 5 * time.Minute},
-			ExtraSoftwareDeps: []string{"display_backlight"},
+			Val:               drmTest{command: []string{"mmap_test"}, timeout: 5 * time.Minute, runDeps: []string{"display_connected"}},
+			ExtraSoftwareDeps: []string{},
 		}, {
 			Name:              "null_platform_test",
-			Val:               drmTest{command: []string{"null_platform_test"}, timeout: 20 * time.Second},
-			ExtraSoftwareDeps: []string{"display_backlight"},
+			Val:               drmTest{command: []string{"null_platform_test"}, timeout: 20 * time.Second, runDeps: []string{"display_connected"}},
+			ExtraSoftwareDeps: []string{},
 		}, {
 			Name: "swrast_test",
 			Val:  drmTest{command: []string{"swrast_test"}, timeout: 20 * time.Second},
 		}, {
 			Name:              "vgem_test",
-			Val:               drmTest{command: []string{"vgem_test"}, timeout: 20 * time.Second},
-			ExtraSoftwareDeps: []string{"display_backlight"},
+			Val:               drmTest{command: []string{"vgem_test"}, timeout: 20 * time.Second, runDeps: []string{"display_connected"}},
+			ExtraSoftwareDeps: []string{},
 		}, {
 			Name:              "vk_glow",
-			Val:               drmTest{command: []string{"vk_glow"}, timeout: 20 * time.Second},
-			ExtraSoftwareDeps: []string{"display_backlight", "vulkan"},
+			Val:               drmTest{command: []string{"vk_glow"}, timeout: 20 * time.Second, runDeps: []string{"display_connected", "vulkan"}},
+			ExtraSoftwareDeps: []string{"vulkan"},
 		}},
 		Attr:    []string{"informational"},
 		Timeout: 5 * time.Minute,
@@ -78,7 +84,50 @@ func DRM(ctx context.Context, s *testing.State) {
 	defer tearDown(ctx)
 
 	testOpt := s.Param().(drmTest)
-	runTest(ctx, s, testOpt.timeout, testOpt.command[0], testOpt.command[1:]...)
+	shouldRun, err := checkRunAttr(ctx, s.SoftwareDeps(), testOpt.runDeps)
+	if err != nil {
+		s.Error("Failed to check attribute: ", err)
+	}
+	if !shouldRun {
+		s.Logf("Warning. Test doesn't pass runtime dependency check. Please make sure DUT meets the following dependencies: %q", testOpt.runDeps)
+	} else {
+		runTest(ctx, s, testOpt.timeout, testOpt.command[0], testOpt.command[1:]...)
+	}
+}
+
+// checkRunAttr checks the runDeps config to decide whether we should run the test.
+// TODO(pwang): runtime dependency is not recommended in tast. Hardware Dependency is currently blocked by crbug.com/950346. We should consider using it once it is done.
+func checkRunAttr(ctx context.Context, softDeps, attrs []string) (bool, error) {
+	for _, attr := range attrs {
+		switch attr {
+		case "display_connected":
+			connectCount, err := graphics.NumberOfOutputsConnected(ctx)
+			if err != nil {
+				return false, errors.Wrapf(err, "failed to check test attribute %s", attr)
+			}
+			if connectCount == 0 {
+				testing.ContextLog(ctx, "No connector detected. Skipping test")
+				return false, nil
+			}
+		case "vulkan":
+			hasVulkan, err := graphics.SupportsVulkanForDEQP(ctx)
+			if err != nil {
+				return false, errors.Wrapf(err, "failed to check test attribute %s", attr)
+			}
+			if !hasVulkan {
+				for _, softDep := range softDeps {
+					if softDep == "vulkan" {
+						return false, errors.New("vulkan is not available but softDeps is set")
+					}
+				}
+				testing.ContextLog(ctx, "Vulkan is not available. Skipping test")
+				return false, nil
+			}
+		default:
+			testing.ContextLogf(ctx, "Unrocognized runtime dependency %s. Assuming it is guarded by tast SoftwareDeps", attr)
+		}
+	}
+	return true, nil
 }
 
 // setUp prepares the testing environment to run runTest().
