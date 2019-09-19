93424ebf5202b18106ca536d783b0ad183c5cb0e
Trent Begin <tbegin@google.com>
2019-09-19 17:36:44 -0600

    tast-tests: add wilco.SludgeGRPC test
    This test exercises the boundary between the wilco_dtc VM and the
    support daemon on the host, wilco_dtc_supportd using the DPSL
    testing utility.
    
    BUG=b:138329362
    TEST=tast run --build dut wilco.SludgeGRPC
    
    Change-Id: I5c486e203c8c30ad48c8e3b5d49fb3b415b5c997

---
+++ src/chromiumos/tast/local/bundles/cros/wilco/sludge_grpc.go
@@ -0,0 +1,111 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wilco
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/golang/protobuf/jsonpb"
+
+	wvm "chromiumos/tast/local/bundles/cros/wilco/vm"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/vm"
+	"chromiumos/tast/testing"
+	dtc "chromiumos/wilco_dtc"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SludgeGRPC,
+		Desc: "Starts an instance of the Wilco DTC VM and exercises the gRPC boundary to a daemon on the host, wilco_dtc_supportd",
+		Contacts: []string{
+			"tbegin@chromium.org", // Test author, wilco_dtc author
+			"pmoy@chromium.org",   // wilco_dtc_supportd author
+			"lamzin@chromium.org", // wilco_dtc_supportd maintainer
+		},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"vm_host", "wilco"},
+	})
+}
+
+func SludgeGRPC(ctx context.Context, s *testing.State) {
+	startCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := wvm.StartSludge(startCtx, false); err != nil {
+		s.Fatal("Unable to Start Sludge VM: ", err)
+	}
+	defer wvm.StopSludge(ctx)
+
+	if err := wvm.StartWilcoSupportDaemon(startCtx); err != nil {
+		s.Fatal("Unable to start wilco_dtc_supportd: ", err)
+	}
+	defer wvm.StopWilcoSupportDaemon(ctx)
+
+	m := jsonpb.Marshaler{}
+
+	osMsg := dtc.GetOsVersionRequest{}
+	str, err := m.MarshalToString(&osMsg)
+	if err != nil {
+		s.Error("Unable to convert proto msg to JSON: ", err)
+	}
+
+	s.Log("Checking VM -> Host Communication")
+	out, err := dpslUtilSend(ctx, "GetOsVersion", str)
+	if err != nil {
+		s.Error("Unable to get OS version: ", err)
+	} else {
+		s.Logf("Successfully got OS version: %s", out)
+	}
+
+	ecMsg := dtc.GetEcTelemetryRequest{}
+	// Get EC firmware label following kernel driver
+	// https://chromium.googlesource.com/chromiumos/third_party/kernel/+/d145cca29f845e55e353cbb86fa9391a71f71dbb/drivers/platform/chrome/wilco_ec/sysfs.c?pli=1#48
+	ecMsg.Payload = []byte{0x38, 0x00, 0x00}
+	str, err = m.MarshalToString(&ecMsg)
+	if err != nil {
+		s.Error("Unable to Marshal EC Payload to String: ", err)
+	}
+
+	s.Log("Getting EC Telemetry")
+	out, err = dpslUtilSend(ctx, "GetEcTelemetry", str)
+	if err != nil {
+		s.Error("Unable to get EC Telemetry: ", err)
+	} else {
+		s.Logf("Successfully got EC Telemetry: %s", out)
+	}
+
+	var jsn map[string]interface{}
+	if err := json.Unmarshal(out, &jsn); err != nil {
+		s.Error("Unable to parse EC telemetry message to JSON: ", err)
+	}
+
+	ecRes := dtc.GetEcTelemetryResponse{}
+	jsnStr, err := json.Marshal(jsn["body"])
+	if err != nil {
+		s.Error("Unable to parse JSON body from EC Telemetry: ", err)
+	}
+	if err := jsonpb.UnmarshalString(fmt.Sprintf("%s", jsnStr), &ecRes); err != nil {
+		s.Error("Unable to parse EC telemetry JSON to proto: ", err)
+	}
+
+	if ecRes.Status != dtc.GetEcTelemetryResponse_STATUS_OK {
+		s.Errorf("Unexpected EC telemetry response status: got %s, want GetEcTelemetryResponse_STATUS_OK", ecRes.Status)
+	}
+}
+
+// dpslUtilSend is a helper function that creates and runs a
+// diagnostics_dpsl_test_requester command over vsh. It accepts the name of
+// command and the JSON body to pass to it. It returns the stdout of the
+// command. See https://crrev.com/c/1767044 for a description of the
+// diagnostics_dpsl_test_requester.
+func dpslUtilSend(ctx context.Context, name, body string) ([]byte, error) {
+	cmd := vm.CreateVSHCommand(ctx, wvm.WilcoVMCID, "diagnostics_dpsl_test_requester",
+		"--message_name="+name, fmt.Sprintf("--message_body=%s", body))
+	return cmd.Output(testexec.DumpLogOnError)
+}
--- src/chromiumos/tast/local/bundles/cros/wilco/vm/sludge.go
+++ src/chromiumos/tast/local/bundles/cros/wilco/vm/sludge.go
@@ -64,3 +64,19 @@ func StopSludge(ctx context.Context) error {
 	}
 	return nil
 }
+
+// StartWilcoSupportDaemon starts the upstart process wilco_dtc_supportd.
+func StartWilcoSupportDaemon(ctx context.Context) error {
+	if err := upstart.RestartJob(ctx, wilcoSupportJob); err != nil {
+		return errors.Wrap(err, "wilco DTC Support daemon could not start")
+	}
+	return nil
+}
+
+// StopWilcoSupportDaemon stops the upstart process wilco_dtc_supportd.
+func StopWilcoSupportDaemon(ctx context.Context) error {
+	if err := upstart.StopJob(ctx, wilcoSupportJob); err != nil {
+		return errors.Wrap(err, "unable to stop Wilco DTC Support daemon")
+	}
+	return nil
+}
