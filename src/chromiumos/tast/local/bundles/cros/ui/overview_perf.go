5b80dd4c51111a1d4beb8f21aed3b6dafda6974d
Jun Mukai <mukai@google.com>
2019-09-20 16:44:13 -0700

    tast-tests: Use 2/8 browser windows for ui.OverviewPerf
    * creates 2 windows first and checks the performance of overview
      animation
    * then create 6 more windows (i.e. 8 total), and then checks the
      performance of overview animation again
    
    Both smoothness data are collected separately and reported;
    separating by adding 2windows / 8windows suffixes.
    
    This CL also changes the content of the browser window; previously
    about:blank is used but this will be unrealistically lightweight.
    I chose chrome://settings page because it is complex enough, and
    the contents should be stable (chrome://newtab wouldn't be stable
    since the logo image might change on certain holidays. I'm afraid
    that skews the test results).
    
    BUG=chromium:1005991
    TEST=tast run <IP> ui.OverviewPerf
    
    Change-Id: Id00b98ef587d8f0d85055e8062b65d113a28dbdb

--- src/chromiumos/tast/local/bundles/cros/ui/overview_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/overview_perf.go
@@ -6,10 +6,12 @@ package ui
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/cdputil"
 	"chromiumos/tast/local/chrome/metrics"
 	"chromiumos/tast/local/perf"
 	"chromiumos/tast/testing"
@@ -27,7 +29,7 @@ func init() {
 }
 
 func OverviewPerf(ctx context.Context, s *testing.State) {
-	cr, err := chrome.New(ctx)
+	cr, err := chrome.New(ctx, chrome.ExtraArgs("--ash-debug-shortcuts"))
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
@@ -39,39 +41,50 @@ func OverviewPerf(ctx context.Context, s *testing.State) {
 	}
 	defer tconn.Close()
 
-	conn, err := cr.NewConn(ctx, "")
-	if err != nil {
-		s.Fatal("Failed to open a new connection: ", err)
-	}
-	defer conn.Close()
-
-	if err = ash.WaitForSystemUIStabilized(ctx); err != nil {
-		s.Fatal("Failed to wait for system UI to be stabilized: ", err)
-	}
-
-	for i := 0; i < 10; i++ {
-		if err = ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
-			s.Fatal("It does not appear to be in the overview mode: ", err)
-		}
-		if err = ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
-			s.Fatal("It does not appear to be in the overview mode: ", err)
-		}
-	}
+	// Use the settings page for the content of the browser window. We chose this
+	// since settings page has lots of elements which would complicate the
+	// overview animation. Hesitant to use a new-tab page since the content (logo
+	// image) can change.
+	const url = "chrome://settings"
 
 	pv := perf.NewValues()
-	for _, histName := range []string{
-		"Ash.Overview.AnimationSmoothness.Enter.SingleClamshellMode",
-		"Ash.Overview.AnimationSmoothness.Exit.SingleClamshellMode",
-	} {
-		histogram, err := metrics.GetHistogram(ctx, cr, histName)
-		if err != nil {
-			s.Fatalf("Failed to get histogram %s: %v", histName, err)
+	currentWindows := 0
+	for _, windows := range []int{2, 8} {
+		for ; currentWindows < windows; currentWindows++ {
+			conn, err := cr.NewConn(ctx, url, cdputil.WithNewWindow())
+			if err != nil {
+				s.Fatal("Failed to open a new connection: ", err)
+			}
+			defer conn.Close()
 		}
-		pv.Set(perf.Metric{
-			Name:      histName,
-			Unit:      "percent",
-			Direction: perf.BiggerIsBetter,
-		}, histogram.Mean())
+
+		if err = ash.WaitForSystemUIStabilized(ctx); err != nil {
+			s.Fatal("Failed to wait for system UI to be stabilized: ", err)
+		}
+
+		for i := 0; i < 10; i++ {
+			if err = ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+				s.Fatal("It does not appear to be in the overview mode: ", err)
+			}
+			if err = ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
+				s.Fatal("It does not appear to be in the overview mode: ", err)
+			}
+		}
+
+		for _, histName := range []string{
+			"Ash.Overview.AnimationSmoothness.Enter.SingleClamshellMode",
+			"Ash.Overview.AnimationSmoothness.Exit.SingleClamshellMode",
+		} {
+			histogram, err := metrics.GetHistogram(ctx, cr, histName)
+			if err != nil {
+				s.Fatalf("Failed to get histogram %s: %v", histName, err)
+			}
+			pv.Set(perf.Metric{
+				Name:      fmt.Sprintf("%s.%dwindows", histName, currentWindows),
+				Unit:      "percent",
+				Direction: perf.BiggerIsBetter,
+			}, histogram.Mean())
+		}
 	}
 
 	if err := pv.Save(s.OutDir()); err != nil {
