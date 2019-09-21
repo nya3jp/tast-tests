3509f7536ff57da1cb88eb889b298a19b413cda3
Miriam Zimmerman <mutexlox@google.com>
2019-09-20 17:07:29 -0700

    tast-tests: Don't use Params for service_failure.
    Per https://crrev.com/c/1816057, this isn't the kind of thing
    parameterized tests are intended for.
    
    BUG=None
    TEST=tast -verbose run $device_ip platform.ServiceFailure
    
    Change-Id: I78a191f8c814402f6bea1227eb5885271add3299

--- src/chromiumos/tast/local/bundles/cros/platform/service_failure.go
+++ src/chromiumos/tast/local/bundles/cros/platform/service_failure.go
@@ -20,9 +20,49 @@ import (
 )
 
 type failureParams struct {
+	name          string
 	envVar        string
 	logMessage    string
 	servicePrefix string
+}
+
+var testParams = []failureParams{
+	{
+		name:          "pre start",
+		envVar:        "PRE_START_EXIT",
+		logMessage:    "pre-start process",
+		servicePrefix: "",
+	},
+	{
+		name:          "main",
+		envVar:        "MAIN_EXIT",
+		logMessage:    "main process",
+		servicePrefix: "",
+	},
+	{
+		name:          "post start",
+		envVar:        "POST_START_EXIT",
+		logMessage:    "post-start process",
+		servicePrefix: "",
+	},
+	{
+		name:          "arc pre start",
+		envVar:        "PRE_START_EXIT",
+		logMessage:    "pre-start process",
+		servicePrefix: "arc-",
+	},
+	{
+		name:          "arc main",
+		envVar:        "MAIN_EXIT",
+		logMessage:    "main process",
+		servicePrefix: "arc-",
+	},
+	{
+		name:          "arc post exit",
+		envVar:        "POST_START_EXIT",
+		logMessage:    "post-start process",
+		servicePrefix: "arc-",
+	},
 }
 
 func init() {
@@ -32,59 +72,10 @@ func init() {
 		Contacts: []string{"mutexlox@google.com", "cros-monitoring-forensics@chromium.org"},
 		Attr:     []string{"informational"},
 		Data:     []string{platformCrash.TestCert},
-		Params: []testing.Param{{
-			Name: "pre_start",
-			Val: failureParams{
-				envVar:        "PRE_START_EXIT",
-				logMessage:    "pre-start process",
-				servicePrefix: "",
-			},
-		}, {
-			Name: "main",
-			Val: failureParams{
-				envVar:        "MAIN_EXIT",
-				logMessage:    "main process",
-				servicePrefix: "",
-			},
-		}, {
-			Name: "post_start",
-			Val: failureParams{
-				envVar:        "POST_START_EXIT",
-				logMessage:    "post-start process",
-				servicePrefix: "",
-			},
-		}, {
-			Name: "arc_pre_start",
-			Val: failureParams{
-				envVar:        "PRE_START_EXIT",
-				logMessage:    "pre-start process",
-				servicePrefix: "arc-",
-			},
-		}, {
-			Name: "arc_main",
-			Val: failureParams{
-				envVar:        "MAIN_EXIT",
-				logMessage:    "main process",
-				servicePrefix: "arc-",
-			},
-		}, {
-			Name: "arc_post_start",
-			Val: failureParams{
-				envVar:        "POST_START_EXIT",
-				logMessage:    "post-start process",
-				servicePrefix: "arc-",
-			},
-		}},
 	})
 }
 
 func ServiceFailure(ctx context.Context, s *testing.State) {
-	const systemCrashDir = "/var/spool/crash"
-
-	params := s.Param().(failureParams)
-
-	failingServiceName := params.servicePrefix + "failing-service"
-
 	if err := localCrash.SetUpCrashTest(); err != nil {
 		s.Fatal("SetUpCrashTest failed: ", err)
 	}
@@ -94,49 +85,54 @@ func ServiceFailure(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to set consent: ", err)
 	}
 
-	oldFiles, err := crash.GetCrashes(systemCrashDir)
-	if err != nil {
-		s.Fatal("Failed to get original crashes: ", err)
-	}
+	for _, tt := range testParams {
+		s.Logf("======================================= %s =======================================", tt.name)
+		failingServiceName := tt.servicePrefix + "failing-service"
 
-	// Restart anomaly detector to clear its cache of recently seen service
-	// failures and ensure this one is logged.
-	if err := localCrash.RestartAnomalyDetector(ctx); err != nil {
-		s.Fatal("Failed to restart anomaly detector: ", err)
-	}
-
-	if err := upstart.StartJob(ctx, failingServiceName, fmt.Sprintf("%s=1", params.envVar)); err != nil {
-		// Ignore error; it's expected.
-		// (upstart exits nonzero if a job fails in pre-start)
-	}
-
-	expectedLogMsg := fmt.Sprintf("%s %s", failingServiceName, params.logMessage)
-
-	// "base" gives the prefix of the expected crash files on disk, which
-	// will use underscores rather than dashes.
-	base := strings.Replace(params.servicePrefix+"service_failure_"+failingServiceName, "-", "_", -1)
-	expectedRegexes := []string{base + `\.\d{8}\.\d{6}\.0\.log`, base + `\.\d{8}\.\d{6}\.0\.meta`}
-
-	files, err := localCrash.WaitForCrashFiles(ctx, localCrash.SystemCrashDir, oldFiles, expectedRegexes)
-	if err != nil {
-		s.Error("Couldn't find expected files: ", err)
-	}
-
-	// Clean up files and check contents.
-	for _, f := range files {
-		if strings.HasSuffix(f, ".log") {
-			contents, err := ioutil.ReadFile(f)
-			if err != nil {
-				s.Error("Couldn't read log file: ", err)
-				continue
-			}
-			if !strings.Contains(string(contents), expectedLogMsg) {
-				s.Errorf("Didn't find expected log contents: `%s`. Leaving %s for debugging", expectedLogMsg, f)
-				continue
-			}
+		oldFiles, err := crash.GetCrashes(localCrash.SystemCrashDir)
+		if err != nil {
+			s.Fatal("Failed to get original crashes: ", err)
 		}
-		if err := os.Remove(f); err != nil {
-			s.Logf("Couldn't clean up %s: %v", f, err)
+
+		// Restart anomaly detector to clear its cache of recently seen service
+		// failures and ensure this one is logged.
+		if err := localCrash.RestartAnomalyDetector(ctx); err != nil {
+			s.Fatal("Failed to restart anomaly detector: ", err)
+		}
+
+		if err := upstart.StartJob(ctx, failingServiceName, fmt.Sprintf("%s=1", tt.envVar)); err != nil {
+			// Ignore error; it's expected.
+			// (upstart exits nonzero if a job fails in pre-start)
+		}
+
+		expectedLogMsg := fmt.Sprintf("%s %s", failingServiceName, tt.logMessage)
+
+		// "base" gives the prefix of the expected crash files on disk, which
+		// will use underscores rather than dashes.
+		base := strings.Replace(tt.servicePrefix+"service_failure_"+failingServiceName, "-", "_", -1)
+		expectedRegexes := []string{base + `\.\d{8}\.\d{6}\.0\.log`, base + `\.\d{8}\.\d{6}\.0\.meta`}
+
+		files, err := localCrash.WaitForCrashFiles(ctx, localCrash.SystemCrashDir, oldFiles, expectedRegexes)
+		if err != nil {
+			s.Error("Couldn't find expected files: ", err)
+		}
+
+		// Clean up files and check contents.
+		for _, f := range files {
+			if strings.HasSuffix(f, ".log") {
+				contents, err := ioutil.ReadFile(f)
+				if err != nil {
+					s.Error("Couldn't read log file: ", err)
+					continue
+				}
+				if !strings.Contains(string(contents), expectedLogMsg) {
+					s.Errorf("Didn't find expected log contents: `%s`. Leaving %s for debugging", expectedLogMsg, f)
+					continue
+				}
+			}
+			if err := os.Remove(f); err != nil {
+				s.Logf("Couldn't clean up %s: %v", f, err)
+			}
 		}
 	}
 }
