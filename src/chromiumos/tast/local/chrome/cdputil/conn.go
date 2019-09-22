538658aa1680392a24f714f3fd791e69b55394dd
Daniel Gonzalez <daniel.d.gonzalez@intel.corp-partner.google.com>
2019-09-22 16:22:25 -0700

    Added DispatchMouseEvent to CDP, which will allow for mouse moving and clicking events based on element location, similar to how puppeteer and selenium perform clicking.
    BUG=none
    TEST=Able to click on DOM button with DispatchMouseEventArgs. Location
    of the button can be found using:
    x += w/2
    y += h/2
    where values are found using JS code:
    x = element.getBoundingClientRect().left
    y = element.getBoundingClientRect().top
    w = element.getBoundingClientRect().width
    h = element.getBoundingClientRect().height
    
    Ref: https://github.com/mafredri/cdp/tree/master/protocol/input
    
    Change-Id: I1e20b0a2ae5c8c3914deeac672ccb1bd3e115c6d
    Signed-off-by: Daniel Gonzalez <daniel.d.gonzalez@intel.corp-partner.google.com>

--- src/chromiumos/tast/local/chrome/cdputil/conn.go
+++ src/chromiumos/tast/local/chrome/cdputil/conn.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/mafredri/cdp"
 	"github.com/mafredri/cdp/protocol/dom"
+	"github.com/mafredri/cdp/protocol/input"
 	"github.com/mafredri/cdp/protocol/page"
 	"github.com/mafredri/cdp/protocol/runtime"
 	"github.com/mafredri/cdp/protocol/target"
@@ -209,4 +210,9 @@ func (c *Conn) Navigate(ctx context.Context, url string) error {
 		return err
 	}
 	return nil
+}
+
+// Dispatches a mouse event to the page.
+func (c *Conn) DispatchMouseEvent(ctx context.Context, args *input.DispatchMouseEventArgs) error {
+	return c.cl.Input.DispatchMouseEvent(ctx, args)
 }
