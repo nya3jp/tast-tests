d75a467ac6c657c452215dd33279bb8852d69d19
Shik Chen <shik@chromium.org>
2019-09-25 00:31:26 +0800

    camera: Add camera.V4L2 test
    It's ported from Autotest camera_V4L2 [1].
    
    [1] https://chromium.git.corp.google.com/chromiumos/third_party/autotest/+/98e5298571eba442ce594f23d1855c0e6b0d16f2/client/site_tests/camera_V4L2/camera_V4L2.py
    
    BUG=b:135726808
    TEST=Run test on Eve (v1) and Nautilus (v3).
    
    Cq-Depend: chromium:1826101
    Change-Id: Id816ded18ffb5c7dd02d802384bc72f7cd052306

---
+++ src/chromiumos/tast/local/bundles/cros/camera/v4l2.go
@@ -0,0 +1,180 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"chromiumos/tast/local/gtest"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+const hwTimestampsPath = "/sys/module/uvcvideo/parameters/hwtimestamps"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: V4L2,
+		Desc: "Verifies required V4L2 operations on USB camera devices",
+		Contacts: []string{
+			"shik@chromium.org",
+			"henryhsu@chromium.org",
+			"chromeos-camera-eng@google.com",
+		},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{caps.BuiltinUSBCamera},
+	})
+}
+
+func V4L2(ctx context.Context, s *testing.State) {
+	hasHWTimestamps, err := pathExist(hwTimestampsPath)
+	if err != nil {
+		s.Fatal("Failed to check hardware timestamps: ", err)
+	}
+	if hasHWTimestamps {
+		origVal, err := setHWTimestamps("1")
+		if err != nil {
+			s.Fatal("Failed to set hardware timestamps: ", err)
+		}
+		defer setHWTimestamps(origVal)
+	}
+
+	testList, err := getTestList()
+	if err != nil {
+		s.Fatal("Failed to get test list: ", err)
+	}
+
+	devicePaths, err := filepath.Glob("/dev/video*")
+	if err != nil {
+		s.Fatal("Failed to glob /dev/video*: ", err)
+	}
+
+	foundAny := false
+
+	for _, devicePath := range devicePaths {
+		if !isV4L2CaptureDevice(ctx, devicePath) {
+			s.Logf("Skip %v because it's not a V4L2 capture device", devicePath)
+			continue
+		}
+
+		usbInfo, err := getUSBInfo(devicePath)
+		if os.IsNotExist(err) {
+			s.Logf("Skip %v because it's not an USB device", devicePath)
+			continue
+		}
+		if err != nil {
+			s.Fatalf("Failed to get USB info of %v: %v", devicePath, err)
+		}
+
+		foundAny = true
+
+		extraArgs := []string{
+			"--device_path=" + devicePath,
+			"--test_list=" + testList,
+			"--usb_info=" + usbInfo,
+		}
+
+		t := gtest.New("media_v4l2_test",
+			gtest.Logfile(filepath.Join(s.OutDir(), "gtest.log")),
+			gtest.ExtraArgs(extraArgs...))
+
+		if args, err := t.Args(); err == nil {
+			s.Log("Running " + shutil.EscapeSlice(args))
+		}
+
+		report, err := t.Run(ctx)
+		if err != nil {
+			if report != nil {
+				for _, name := range report.FailedTestNames() {
+					s.Error(name, " failed")
+				}
+			}
+			s.Fatal("Failed to run media_v4l2_test: ", err)
+		}
+	}
+
+	if !foundAny {
+		s.Fatal("Failed to find any valid device")
+	}
+}
+
+func pathExist(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func setHWTimestamps(newValue string) (oldValue string, err error) {
+	b, err := ioutil.ReadFile(hwTimestampsPath)
+	if err != nil {
+		return "", err
+	}
+	oldValue = string(b)
+
+	if err := ioutil.WriteFile(hwTimestampsPath, []byte(newValue), 0644); err != nil {
+		return "", err
+	}
+	return oldValue, err
+}
+
+func getTestList() (string, error) {
+	const v1Path = "/usr/bin/arc_camera_service"
+	const v3Path = "/usr/bin/cros_camera_service"
+
+	hasV1, err := pathExist(v1Path)
+	if err != nil {
+		return "", err
+	}
+
+	hasV3, err := pathExist(v3Path)
+	if err != nil {
+		return "", err
+	}
+
+	if hasV3 && !hasV1 {
+		return "halv3", nil
+	}
+
+	return "default", nil
+}
+
+func isV4L2CaptureDevice(ctx context.Context, path string) bool {
+	cmd := testexec.CommandContext(ctx, "media_v4l2_is_capture_device", path)
+	if err := cmd.Run(); err != nil {
+		return false
+	}
+	return true
+}
+
+func getUSBInfo(path string) (string, error) {
+	baseName := filepath.Base(path)
+	vidPath := fmt.Sprintf("/sys/class/video4linux/%s/device/../idVendor", baseName)
+	pidPath := fmt.Sprintf("/sys/class/video4linux/%s/device/../idProduct", baseName)
+
+	vid, err := ioutil.ReadFile(vidPath)
+	if err != nil {
+		return "", err
+	}
+
+	pid, err := ioutil.ReadFile(pidPath)
+	if err != nil {
+		return "", err
+	}
+
+	info := strings.TrimSpace(string(vid)) + ":" + strings.TrimSpace(string(pid))
+	return info, nil
+}
