fdcb7afe001c9d8d07470394b19579f5751790b6
Kazuki Takise <takise@google.com>
2019-09-25 09:02:03 +0900

    Add timeout to arc.PIP
    BUG=b:135956156
    BUG=b:141577889
    TEST=tast -verbose run my_crbook arc.PIP
    
    Change-Id: I5fcf191d6007eb0b54fac8ddc42a9fd6461715ae

--- src/chromiumos/tast/local/bundles/cros/arc/pip.go
+++ src/chromiumos/tast/local/bundles/cros/arc/pip.go
@@ -59,6 +59,7 @@ func init() {
 		SoftwareDeps: []string{"tablet_mode", "android_p", "chrome"},
 		Data:         []string{"ArcPipTastTest.apk"},
 		Pre:          arc.Booted(),
+		Timeout:      5 * time.Minute,
 	})
 }
 
