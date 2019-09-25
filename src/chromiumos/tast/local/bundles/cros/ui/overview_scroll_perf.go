be48e1dd2c9f1f7161fdaab5b0aad72138c03fbf
Sammie Quon <sammiequon@google.com>
2019-09-25 14:14:43 -0700

    tast-tests: Add ui.OverviewScrollPerf test.
    Porting [1] from interactive ui tests. This measures presentation time
    when scrolling the grid in overview mode.
    
    [1] https://cs.chromium.org/chromium/src/chrome/browser/ui/ash/overview_scroll_interactive_uitest.cc
    
    BUG=chromium:1007060
    TEST=tast run IP ui.OverviewScrollPerf
    
    Change-Id: Id68392f0f0152adc6c14f7652cd0de60c45ab8c5

---
+++ src/chromiumos/tast/local/bundles/cros/ui/overview_scroll_perf.go
@@ -0,0 +1,119 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/cdputil"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         OverviewScrollPerf,
+		Desc:         "Measures the presentation time of scrolling the overview grid in tablet mode",
+		Contacts:     []string{"sammiequon@chromium.org", "chromeos-wmp@google.com"},
+		Attr:         []string{"informational", "group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", "tablet_mode"},
+		Timeout:      time.Minute,
+	})
+}
+
+func OverviewScrollPerf(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ExtraArgs("--enable-features=NewOverviewLayout"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+	defer tconn.Close()
+
+	conn, err := cr.NewConn(ctx, "")
+	if err != nil {
+		s.Fatal("Failed to open a new connection: ", err)
+	}
+	defer conn.Close()
+
+	if err = ash.WaitForSystemUIStabilized(ctx); err != nil {
+		s.Fatal("Failed to wait for system UI to be stabilized: ", err)
+	}
+
+	tabletModeEnabled, err := ash.TabletModeEnabled(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get tablet mode: ", err)
+	}
+	// Be nice and restore tablet mode to its original state on exit.
+	defer ash.SetTabletModeEnabled(ctx, tconn, tabletModeEnabled)
+
+	// Overview scrolling is only available in tablet mode.
+	if err = ash.SetTabletModeEnabled(ctx, tconn, true); err != nil {
+		s.Fatal("Failed to enable tablet mode: ", err)
+	}
+
+	// Prepare the touch screen as this test requires touch scroll events.
+	tsew, err := input.Touchscreen(ctx)
+	if err != nil {
+		s.Fatal("Failed to create touch screen event writer: ", err)
+	}
+
+	stw, err := tsew.NewSingleTouchWriter()
+	if err != nil {
+		s.Fatal("Failed to create single touch writer: ", err)
+	}
+	defer stw.Close()
+
+	// Use a total of 16 windows for this test, so that scrolling can happen.
+	for i := 0; i < 15; i++ {
+		conn, err := cr.NewConn(ctx, "about:blank", cdputil.WithNewWindow())
+		if err != nil {
+			s.Fatal("Failed to open a new connection for a new window: ", err)
+		}
+		defer conn.Close()
+	}
+
+	if err = ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+		s.Fatal("It does not appear to be in the overview mode: ", err)
+	}
+
+	// Scroll from the top right of the screen to the top left.
+	if err := stw.Swipe(ctx, tsew.Width()-10, 10, 10, 10, 500*time.Millisecond); err != nil {
+		s.Fatal("Failed to execute a swipe gesture: ", err)
+	}
+
+	if err := stw.End(); err != nil {
+		s.Fatal("Failed to finish the swipe gesture: ", err)
+	}
+
+	if err = ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
+		s.Fatal("It does not appear to be in the overview mode: ", err)
+	}
+
+	pv := perf.NewValues()
+	histName := "Ash.Overview.Scroll.PresentationTime.TabletMode"
+	histogram, err := metrics.GetHistogram(ctx, cr, histName)
+	if err != nil {
+		s.Fatalf("Failed to get histogram %s: %v", histName, err)
+	}
+	pv.Set(perf.Metric{
+		Name:      histName,
+		Unit:      "ms",
+		Direction: perf.SmallerIsBetter,
+	}, histogram.Mean())
+
+	if err := pv.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
