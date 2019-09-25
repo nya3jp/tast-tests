05a7e03bc9666f7cf848450119e21fcff3b1e56c
Oleh Lamzin <lamzin@google.com>
2019-09-25 17:32:42 +0200

    tast: test SetUsbEthernetMacAddressSource D-Bus method
    Test SetUsbEthernetMacAddressSource D-Bus method.
    
    Reference CLs:
      * https://crrev.com/c/1730078
    
    BUG=b:130434424
    TEST=tast run ${DUT_IP_ADDRESS} network.SetUSBEthernetMACAddressSource
    
    Change-Id: I59ffd877d7e73480be435cc0b975b1c73377038e

---
+++ src/chromiumos/tast/local/shill/device.go
@@ -0,0 +1,69 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package shill
+
+import (
+	"context"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+)
+
+const (
+	dbusDeviceInterface = "org.chromium.flimflam.Device"
+)
+
+// DeviceProp is the type for device property names.
+type DeviceProp string
+
+// Device property names defined in dbus-constants.h .
+const (
+	// Device property names.
+	DevicePropName DeviceProp = "Interface"
+
+	// Ethernet device property names.
+	DevicePropEthernetBusType   DeviceProp = "Ethernet.DeviceBusType"
+	DevicePropEthernetLinkUp    DeviceProp = "Ethernet.LinkUp"
+	DevicePropEthernetMACSource DeviceProp = "Ethernet.UsbEthernetMacAddressSource"
+)
+
+// Device wraps a Device D-Bus object in shill.
+type Device struct {
+	obj  dbus.BusObject
+	path dbus.ObjectPath
+}
+
+// NewDevice connects to shill's Device.
+func NewDevice(ctx context.Context, path dbus.ObjectPath) (*Device, error) {
+	_, obj, err := dbusutil.Connect(ctx, dbusService, path)
+	if err != nil {
+		return nil, err
+	}
+	m := &Device{obj: obj, path: path}
+	return m, nil
+}
+
+func (d *Device) String() string {
+	return string(d.path)
+}
+
+// GetProps returns a list of properties provided by the device.
+func (d *Device) GetProps(ctx context.Context) (map[DeviceProp]interface{}, error) {
+	props := make(map[DeviceProp]interface{})
+	if err := call(ctx, d.obj, dbusDeviceInterface, "GetProperties").Store(&props); err != nil {
+		return nil, errors.Wrap(err, "failed getting properties")
+	}
+	return props, nil
+}
+
+// SetMACSource sets USB Ethernet MAC address source for the device.
+func (d *Device) SetMACSource(ctx context.Context, source string) error {
+	if err := call(ctx, d.obj, dbusDeviceInterface, "SetUsbEthernetMacAddressSource", source).Err; err != nil {
+		return errors.Wrap(err, "failed set USB Ethernet MAC address source")
+	}
+	return nil
+}
