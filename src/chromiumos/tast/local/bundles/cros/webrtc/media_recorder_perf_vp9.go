0dcae72baad9a92b56565cafc2e57117705fe8ff
Miguel Casas <mcasas@chromium.org>
2019-09-27 19:57:53 -0400

    tast: cleanup webrtc.MediaRecorderPerf{H264,VP8,VP9}
    This CL changes the owners of the mentioned tast test files
    bundles/cros/webrtc/media_recorder_perf_*.go.
    
    It also takes the chance to clean up media_recorder.go:
    - removes the parameter |fps| since it's ignored: the Y4M file
      that is streamed determines the frame rate.
    - Removes unnecessary audio muting, since there's no audio in
      the files reproduced by --use-file-for-fake-video-capture.
    
    It also corrects a stranded VP8 in the VP9 test file.
    
    It also renames s/processingTime/processingTimePerFrame/.
    
    BUG=chromium:1008080,b:140359777
    TEST=tast run -build=true papaya webrtc.MediaRecorderPerf
      on nocturne
    
    Cq-Depend: chromium:1829488
    Change-Id: If079faf461615142fa960c1ec2be24614fa643b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1829494
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Tested-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/webrtc/media_recorder_perf_vp9.go
+++ src/chromiumos/tast/local/bundles/cros/webrtc/media_recorder_perf_vp9.go
@@ -18,9 +18,9 @@ func init() {
 		Func: MediaRecorderPerfVP9,
 		Desc: "Captures performance data about MediaRecorder for SW and HW with VP9",
 		Contacts: []string{
-			"hiroh@chromium.org", // Video team
-			"wtlee@chromium.org", // Camera team
-			"chromeos-camera-eng@google.com",
+			"mcasas@chromium.org",
+			"chromeos-gfx-video@google.com",
+			"chromeos-video-eng@google.com",
 		},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome"},
@@ -31,8 +31,7 @@ func init() {
 
 // MediaRecorderPerfVP9 captures the perf data of MediaRecorder for HW and SW cases with VP9 codec and uploads to server.
 func MediaRecorderPerfVP9(ctx context.Context, s *testing.State) {
-	const fps = 30
-	if err := mediarecorder.MeasurePerf(ctx, s.DataFileSystem(), s.OutDir(), videotype.VP8, s.DataPath(mediarecorder.PerfStreamFile), fps); err != nil {
+	if err := mediarecorder.MeasurePerf(ctx, s.DataFileSystem(), s.OutDir(), videotype.VP9, s.DataPath(mediarecorder.PerfStreamFile)); err != nil {
 		s.Error("Failed to measure performance: ", err)
 	}
 }
