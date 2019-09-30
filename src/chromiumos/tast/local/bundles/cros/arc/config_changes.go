f4cfa2e509498d0637f4bc78d5cb9197f37897a3
Tetsui Ohkubo <tetsui@chromium.org>
2019-09-30 18:35:59 +0900

    WIP: tast: Add arc.ConfigChanges
    TEST=tast run eve-DUT arc.ConfigChanges
    BUG=None
    
    Change-Id: I48b96f34df5859ad388cf148257e04cf2f928c6d

---
+++ src/chromiumos/tast/local/bundles/cros/arc/config_changes.go
@@ -0,0 +1,87 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ConfigChanges,
+		Desc:         "Checks that configChanges works as intended",
+		Contacts:     []string{"tetsui@chromium.org", "arc-framework@google.com"},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"android_p", "chrome"},
+		Data:         []string{"ArcOrientationTest.apk"},
+		Pre:          arc.Booted(),
+		Timeout:      3 * time.Minute,
+	})
+}
+
+func ConfigChanges(ctx context.Context, s *testing.State) {
+	a := s.PreValue().(arc.PreData).ARC
+	d, err := ui.NewDevice(ctx, a)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	defer d.Close()
+
+	const (
+		apk = "ArcOrientationTest.apk"
+		pkg = "org.chromium.arc.testapp.orientation"
+		cls = ".MainActivity"
+	)
+
+	s.Log("Installing app")
+	if err := a.Install(ctx, s.DataPath(apk)); err != nil {
+		s.Fatal("Failed installing app: ", err)
+	}
+
+	s.Log("Starting app")
+	act, err := arc.NewActivity(a, pkg, cls)
+	if err != nil {
+		s.Fatal("Failed starting app: ", err)
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx); err != nil {
+		s.Fatal("Failed starting app: ", err)
+	}
+	defer act.Stop(ctx)
+
+	const (
+		textID   = "org.chromium.arc.testapp.orientation:id/resume_count"
+		buttonID = "org.chromium.arc.testapp.orientation:id/button"
+	)
+
+	text := d.Object(ui.ID(textID))
+	if err := text.WaitForExists(ctx, 30*time.Second); err != nil {
+		s.Fatal("Failed to find the button: ", err)
+	}
+
+	initCount, err := text.GetText(ctx)
+	if err != nil {
+		s.Fatal("Failed to get text: ", err)
+	}
+
+	if err := d.Object(ui.ID(buttonID)).Click(ctx); err != nil {
+		s.Fatal("Failed to click the button: ", err)
+	}
+
+	updatedCount, err := text.GetText(ctx)
+	if err != nil {
+		s.Fatal("Failed to get text: ", err)
+	}
+
+	if initCount != updatedCount {
+		s.Fatal("The activity relaunched between orientation change")
+	}
+}
