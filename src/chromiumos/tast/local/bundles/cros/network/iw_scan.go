f0f15e66a54c4e2ee9bccba67d2ac5517a0c590d
Shuo-Peng Liao <deanliao@chromium.org>
2019-10-03 23:19:12 +0800

    Tast: retrieve WiFi interface from shill.
    Add GetWifiInterface() in shill/wifi.go to get a managed WiFi interface
    from shill. Previously, the WiFi interface was hard-coded as "wlan0" and
    the test fails on certain devices. Now, test can dynamicall get the
    system's WiFi interface to test to solve the issue.
    
    BUG=chromium:988894
    TEST=tast run $DUT network.IWScan on elm, which was failed because its
    wifi interface is "mlan0", not the hard-coded "wlan0".
    
    Change-Id: Iacbd7ebe35affd3dd61e518f378a0adc27bb25be
    Signed-off-by: Shuo-Peng Liao <deanliao@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/network/iw_scan.go
+++ src/chromiumos/tast/local/bundles/cros/network/iw_scan.go
@@ -17,16 +17,21 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:     IWScan,
 		Desc:     "Verifies `iw` Timed Scan executes and is parsed properly",
-		Contacts: []string{"billyzhao@google.com", "chromeos-kernel-wifi@google.com"},
+		Contacts: []string{"deanliao@google.com", "chromeos-kernel-wifi@google.com"},
 		Attr:     []string{"group:mainline", "informational"},
 	})
 }
 
 func IWScan(ctx context.Context, s *testing.State) {
 	const (
-		iface      = "wlan0"
 		technology = "wifi"
 	)
+	iface, err := shill.GetWifiInterface(ctx)
+	if err != nil {
+		s.Fatal("Could not get a WiFi interface: ", err)
+	}
+	s.Log("WiFi interface: ", iface)
+
 	// In order to guarantee reliable execution of IWScan, we need to make sure
 	// shill doesn't interfere with the scan. We will disable shill's control
 	// on the wireless device while still maintaining ethernet connectivity.
@@ -37,19 +42,19 @@ func IWScan(ctx context.Context, s *testing.State) {
 
 	// Make sure shill doesn't interfere with scans on suspend/resume.
 	if err := manager.DisableTechnology(ctx, technology); err != nil {
-		s.Fatal("Could not disable wifi from shill: ", err)
+		s.Fatal("Could not disable WiFi from shill: ", err)
 	}
 
 	defer func() {
 		// Allow shill to take control of wireless device.
 		if err := manager.EnableTechnology(ctx, technology); err != nil {
-			s.Fatal("Could not enable wifi from shill: ", err)
+			s.Fatal("Could not enable WiFi from shill: ", err)
 		}
 	}()
 
 	// Bring up wireless device after it's released from shill.
 	if err := testexec.CommandContext(ctx, "ip", "link", "set", iface, "up").Run(testexec.DumpLogOnError); err != nil {
-		s.Fatalf("Could not bring up %s after disable", iface)
+		s.Fatalf("Could not bring up %s after shill released WiFi management", iface)
 	}
 
 	// Conduct scan
