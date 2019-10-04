ecdc90a4db522fcb7d6e75f93a76118316666a9f
David Staessens <dstaessens@chromium.org>
2019-10-04 12:49:06 +0900

    tast.video: Make VD tests use cros_video_decoder dependency instead of android.
    The decode_accel_vd*, play_decode_accel_used_vd*, play_vd*, playback_vd* and
    seek_vd* tests test the new media::VideoDecoder-based video decoders. These
    video decoders require import mode to be supported by the platform. The
    "android" sotfware dependency is currently used as android also requires
    import mode to be supported, but this is not always accurate.
    
    This CL changes these tests to use the newly-introduced "cros_video_decoder"
    dependency instead of "android".
    
    BUG=b:137916185
    
    TEST=tast run $HOST video.DecodeAccelVDH264
    
    Cq-Depend: 1839472
    Change-Id: I93d58bfcb1bbb7ae1dc1c1b943a6c6a4f469bbe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1839316
    Tested-by: David Staessens <dstaessens@chromium.org>
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_perf_vp8_1080p_30fps.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_perf_vp8_1080p_30fps.go
@@ -14,13 +14,11 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:     DecodeAccelVDPerfVP81080P30FPS,
-		Desc:     "Runs video_decode_accelerator_perf_tests with a VP8 1080p@30fps video on a media::VideoDecoder (see go/vd-migration)",
-		Contacts: []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
-		Attr:     []string{"group:crosbolt", "crosbolt_perbuild"},
-		// TODO(b/137916185): Remove dependency on android capability. It's used here
-		// to guarantee import-mode support, which is required by the new VD's.
-		SoftwareDeps: []string{"android", "chrome", caps.HWDecodeVP8},
+		Func:         DecodeAccelVDPerfVP81080P30FPS,
+		Desc:         "Runs video_decode_accelerator_perf_tests with a VP8 1080p@30fps video on a media::VideoDecoder (see go/vd-migration)",
+		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"cros_video_decoder", "chrome", caps.HWDecodeVP8},
 		Data:         []string{"1080p_30fps_300frames.vp8.ivf", "1080p_30fps_300frames.vp8.ivf.json"},
 	})
 }
