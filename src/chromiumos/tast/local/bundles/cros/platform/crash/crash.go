561e6a2f37dd67b46382069aa9faeacb2d4af731
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2019-10-04 12:26:24 +0900

    tast-tests: Verify contents of crash directory.
    This is a partial porting of logging_UserCrash test in autotest:
    src/third_party/autotest/files/client/site_tests/logging_UserCrash/logging_UserCras
    h.py
    
    Tests verify that expected set of files are created in crash directory
    after intentional crash.
    This also stashes and restores the crash directory while testing crash.
    
    BUG=chromium:970930
    TEST=tast run platform.UserCrash
    
    Change-Id: I822059c44a0ad41eb5c999d8da9527bc7c493e98

--- src/chromiumos/tast/local/bundles/cros/platform/crash/crash.go
+++ src/chromiumos/tast/local/bundles/cros/platform/crash/crash.go
@@ -10,12 +10,16 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"os/user"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/fsutil"
 	"chromiumos/tast/local/metrics"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
@@ -37,6 +41,12 @@ const (
 
 	crashSenderRateDir = "/var/lib/crash_sender"
 	pauseFile          = "/var/lib/crash_sender_paused"
+
+	systemCrashDir       = "/var/spool/crash"
+	fallbackUserCrashDir = "/home/chronos/crash"
+	userCrashDirs        = "/home/chronos/u-*/crash"
+	userCrashDirRegex    = "/home/chronos/u-([a-f0-9]+)/crash"
+	backupCrashDir       = "/tmp/crashTestBackup"
 )
 
 // CrasherOptions stores configurations for running crasher process.
@@ -48,9 +58,14 @@ type CrasherOptions struct {
 
 // CrasherResult stores result status and outputs from a crasher prcess execution.
 type CrasherResult struct {
-	ReturnCode int
-	Crashed    bool
-	Output     string
+	ReturnCode          int
+	Crashed             bool
+	CrashReporterCaught bool
+	Output              string
+	Minidump            string
+	Basename            string
+	Meta                string
+	Log                 string
 }
 
 // DefaultCrasherOptions creates a CrasherOptions which actually cause and catch crash.
@@ -84,6 +99,92 @@ func exitCode(err error) (int, error) {
 	return -int(s.Signal()), nil
 }
 
+func checkCrashDirectoryPermissions(path string) error {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	s, ok := fileInfo.Sys().(*syscall.Stat_t)
+	if !ok {
+		return errors.New("failed to cast to Stat_t")
+	}
+	usr, err := user.LookupId(strconv.FormatInt(int64(s.Uid), 10))
+	if err != nil {
+		return err
+	}
+	grp, err := user.LookupGroupId(strconv.FormatInt(int64(s.Gid), 10))
+	if err != nil {
+		return err
+	}
+	mode := fileInfo.Mode()
+
+	permittedModes := make(map[os.FileMode]struct{})
+	var expectedUser string
+	var expectedGroup string
+	if strings.HasPrefix(path, "/var/spool/crash") {
+		if fileInfo.IsDir() {
+			files, err := ioutil.ReadDir(path)
+			if err != nil {
+				return errors.Wrapf(err, "failed to read directory %s", path)
+			}
+			for _, f := range files {
+				if err := checkCrashDirectoryPermissions(filepath.Join(path, f.Name())); err != nil {
+					return err
+				}
+			}
+			permittedModes[os.FileMode(0770)|os.ModeDir|os.ModeSetgid] = struct{}{}
+		} else {
+			permittedModes[os.FileMode(0660)] = struct{}{}
+			permittedModes[os.FileMode(0640)] = struct{}{}
+			permittedModes[os.FileMode(0644)] = struct{}{}
+		}
+		expectedUser = "root"
+		expectedGroup = "crash-access"
+	} else {
+		permittedModes[os.ModeDir|os.FileMode(0700)] = struct{}{}
+		expectedUser = "chronos"
+		expectedGroup = "chronos"
+	}
+	if usr.Username != expectedUser || grp.Name != expectedGroup {
+		return errors.Errorf("ownership of %s got %s.%s; want %s.%s",
+			path, usr.Username, grp.Name, expectedUser, expectedGroup)
+	}
+	if _, found := permittedModes[mode]; !found {
+		extractKeys := func(m map[os.FileMode]struct{}) []os.FileMode {
+			var result []os.FileMode
+			for k := range m {
+				result = append(result, k)
+			}
+			return result
+		}
+		return errors.Errorf("mode of %s got %v; want either of %v",
+			path, mode, extractKeys(permittedModes))
+	}
+	return nil
+}
+
+func getCrashDir(username string) string {
+	if username == "root" || username == "crash" {
+		return systemCrashDir
+	}
+	p, _ := filepath.Glob(userCrashDirs)
+	// Omitting error handling because it happens only when userCrashDirs is malformed
+	if len(p) == 0 {
+		return fallbackUserCrashDir
+	}
+	return p[0]
+}
+
+// canonicalizeCrashDir converts /home/chronos crash directory to /home/user counterpart.
+func canonicalizeCrashDir(path string) string {
+	r := regexp.MustCompile(userCrashDirRegex)
+	m := r.FindStringSubmatch(path)
+	if m == nil {
+		return path
+	}
+	return filepath.Join("/home", m[1], "crash")
+}
+
 // enableSystemSending allows to run system crash_sender.
 func enableSystemSending() error {
 	if err := os.Remove(pauseFile); err != nil && !os.IsNotExist(err) {
@@ -108,6 +209,56 @@ func disableSystemSending() error {
 		}
 	}
 	return nil
+}
+
+// stashCrashFiles moves contents of crash directory to a temporary backup directory.
+// Those files can be restored by restoreCrashFiles later. Doesn't support recursive stashing.
+func stashCrashFiles(userName string) error {
+	path := getCrashDir(userName)
+	if err := os.RemoveAll(backupCrashDir); err != nil && !os.IsNotExist(err) {
+		return errors.Wrapf(err, "failed to remove %s, old backup of crash directory", path)
+	}
+	if err := os.Mkdir(backupCrashDir, os.FileMode(0770)); err != nil {
+		return errors.Wrapf(err, "failed to create directory %s for crash directory backup", path)
+	}
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return errors.Wrapf(err, "failed to read crash directory %s", path)
+	}
+	for _, f := range files {
+		if err := fsutil.MoveFile(filepath.Join(path, f.Name()), filepath.Join(backupCrashDir, f.Name())); err != nil {
+			return errors.Wrapf(err, "failed to move a file in crash directory: %s", f.Name())
+		}
+	}
+	if err := os.RemoveAll(path); err != nil {
+		return errors.Wrapf(err, "failed to remove crash directory %s", path)
+	}
+	return nil
+}
+
+// restoreCrashFiles removes all existing files in crash directory and restore stashed ones.
+func restoreCrashFiles(ctx context.Context, userName string) error {
+	crashDir := getCrashDir(userName)
+	files, err := ioutil.ReadDir(crashDir)
+	if err != nil {
+		return errors.Wrapf(err, "failed to read crash directory %s", crashDir)
+	}
+	for _, f := range files {
+		if err := os.Remove(filepath.Join(crashDir, f.Name())); err != nil {
+			return errors.Wrapf(err, "failed to delete a file in crash directory: %s", f)
+		}
+	}
+
+	bf, err := ioutil.ReadDir(backupCrashDir)
+	if err != nil {
+		return errors.Wrapf(err, "failed to read crash backup directory %s", backupCrashDir)
+	}
+	for _, f := range bf {
+		if err := fsutil.MoveFile(filepath.Join(backupCrashDir, f.Name()), filepath.Join(crashDir, f.Name())); err != nil {
+			return errors.Wrapf(err, "failed to delete a file in crash directory: %s", f.Name())
+		}
+	}
+	return os.RemoveAll(backupCrashDir)
 }
 
 // replaceCrashFilterIn replaces --filter_in= flag value of the crash reporter.
@@ -235,7 +386,7 @@ func runCrasherProcess(ctx context.Context, opts CrasherOptions) (CrasherResult,
 		}
 		return nil
 	}, &testing.PollOptions{Timeout: time.Duration(10) * time.Second}); err != nil {
-		// TODO(yamaguchi): include log reader message in this error.
+		// TODO(crbug.com/970930): include system log message in this error.
 		return CrasherResult{}, errors.Wrap(err, "timeout waiting for crash_reporter to finish: ")
 	}
 
@@ -256,6 +407,14 @@ func runCrasherProcess(ctx context.Context, opts CrasherOptions) (CrasherResult,
 	return result, nil
 }
 
+func addDotPrefix(s []string) []string {
+	var r []string
+	for _, e := range s {
+		r = append(r, "."+e)
+	}
+	return r
+}
+
 // RunCrasherProcessAndAnalyze executes a crasher process and extracts result data from dumps and logs.
 func RunCrasherProcessAndAnalyze(ctx context.Context, opts CrasherOptions) (CrasherResult, error) {
 	result, err := runCrasherProcess(ctx, opts)
@@ -266,12 +425,130 @@ func RunCrasherProcessAndAnalyze(ctx context.Context, opts CrasherOptions) (Cras
 	if !result.Crashed /* || !result.CrashReporterCaught */ {
 		return result, nil
 	}
-	// TODO(yamaguchi): Add logic to examine contents of crash dir and store them to result.
+	if !opts.Consent {
+		if _, err := os.Stat(getCrashDir(opts.Username)); err == nil || !os.IsNotExist(err) {
+			return result, errors.Wrap(err, "crash directory should not exist")
+		}
+		return result, nil
+	}
+
+	if info, err := os.Stat(getCrashDir(opts.Username)); err != nil || !info.IsDir() {
+		return result, errors.Wrap(err, "crash directory does not exist")
+	}
+
+	crashDir := canonicalizeCrashDir(getCrashDir(opts.Username))
+	crashContents, err := ioutil.ReadDir(crashDir)
+	if err != nil {
+		return result, errors.Wrapf(err, "failed to read crash directory %s", crasherPath)
+	}
+	basename := strings.Replace(filepath.Base(crasherPath), ".", "_", -1)
+
+	// A dict tracking files for each crash report.
+	crashReportFiles := make(map[string]string)
+	if err := checkCrashDirectoryPermissions(crashDir); err != nil {
+		return result, err
+	}
+
+	testing.ContextLogf(ctx, "Contents in %s: %s", crashDir, crashContents)
+
+	// Variables and their typical contents:
+	// basename: crasher_nobreakpad
+	// filename: crasher_nobreakpad.20181023.135339.16890.dmp
+	// ext: dmp
+	for _, f := range crashContents {
+		if strings.HasSuffix(f.Name(), ".core") {
+			// Ignore core files.  We'll test them later.
+			continue
+		}
+		if strings.HasPrefix(f.Name(), basename+".") {
+			c := strings.Split(f.Name(), ".")
+			ext := c[len(c)-1]
+			testing.ContextLogf(ctx, "Found crash report file (%s): %s", ext, f.Name())
+			if data, ok := crashReportFiles[ext]; ok {
+				return result, errors.Errorf("found multiple files with .%s: %s and %s",
+					ext, f.Name(), data)
+			}
+			crashReportFiles[ext] = f.Name()
+		} else {
+			// Flag all unknown files.
+			return result, errors.Errorf("crash reporter created an unknown file: %s / base=%s",
+				f.Name(), basename)
+		}
+	}
+
+	// Every crash report needs one of these to be valid.
+	reportRequiredFiletypes := []string{
+		"meta",
+	}
+	// Reports might have these and that's OK!
+	reportOptionalFiletypes := []string{
+		"dmp", "log", "proclog",
+	}
+
+	// Make sure we generated the exact set of files we expected.
+	foundFiletypes := crashReportFiles
+	var missingFileTypes []string
+	for _, rt := range reportRequiredFiletypes {
+		found := false
+		for k := range foundFiletypes {
+			if k == rt {
+				found = true
+				break
+			}
+		}
+		if !found {
+			missingFileTypes = append(missingFileTypes, rt)
+		}
+	}
+	if len(missingFileTypes) > 0 {
+		return result, errors.Errorf("crash report is missing files: %v", addDotPrefix(missingFileTypes))
+	}
+
+	find := func(target string, lst []string) bool {
+		for _, v := range lst {
+			if target == v {
+				return true
+			}
+		}
+		return false
+	}
+	findInKey := func(target string, m map[string]string) bool {
+		_, found := m[target]
+		return found
+	}
+
+	var unknownFileTypes []string
+	for k := range foundFiletypes {
+		if !find(k, append(reportRequiredFiletypes, reportOptionalFiletypes...)) {
+			unknownFileTypes = append(missingFileTypes, k)
+		}
+	}
+	if len(unknownFileTypes) > 0 {
+		return result, errors.Errorf("crash report includes unkown files: %v", addDotPrefix(unknownFileTypes))
+	}
+
+	// Create full paths for the logging code below.
+	for _, key := range append(reportRequiredFiletypes, reportOptionalFiletypes...) {
+		if findInKey(key, foundFiletypes) {
+			crashReportFiles[key] = filepath.Join(crashDir, crashReportFiles[key])
+		} else {
+			crashReportFiles[key] = ""
+		}
+	}
+
+	result.Minidump = crashReportFiles["dmp"]
+	result.Basename = basename
+	result.Meta = crashReportFiles["meta"]
+	result.Log = crashReportFiles["log"]
 	return result, nil
 }
 
 // CheckCrashingProcess runs crasher process and verifies that it's processed.
 func CheckCrashingProcess(ctx context.Context, opts CrasherOptions) error {
+	if err := stashCrashFiles(opts.Username); err != nil {
+		return errors.Wrap(err, "failed to stash crash files")
+	}
+	defer restoreCrashFiles(ctx, opts.Username)
 	result, err := RunCrasherProcessAndAnalyze(ctx, opts)
 	if err != nil {
 		return errors.Wrap(err, "failed to run and analyze crasher")
@@ -279,6 +556,19 @@ func CheckCrashingProcess(ctx context.Context, opts CrasherOptions) error {
 	if !result.Crashed {
 		return errors.Errorf("Crasher returned %d instead of crashing", result.ReturnCode)
 	}
+
+	// TODO(crbug.com/970930): Check if crash_reporter caught the crash.
+
+	if !opts.Consent {
+		return nil
+	}
+	if result.Minidump == "" {
+		return errors.New("crash reporter did not announce minidump")
+	}
+
+	// TODO(crbug.com/970930): Check minidump stack walk.
+	// TODO(crbug.com/970930): Check generated report sending.
+
 	return nil
 }
 
