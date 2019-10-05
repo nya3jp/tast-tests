2326a614557ae4b6cf722db05aa13f6fc469bfcf
Shuo-Peng Liao <deanliao@chromium.org>
2019-10-05 15:22:39 +0800

    Tast: promote three network Tasts to critical.
    network.DefaultProfile, DefaultProfileServices and TwoShills were green
    for 20 builds.
    Note that though IWGetSet is green for 20 build, as the test itself is
    quite imcomplete, I'd rather keep it in informational.
    
    BUG=chromium:1011597
    TEST=None
    
    Change-Id: Ibc9fea730b44bb5c5448c9c381e7bcf216c531ae

--- src/chromiumos/tast/local/bundles/cros/network/two_shills.go
+++ src/chromiumos/tast/local/bundles/cros/network/two_shills.go
@@ -18,7 +18,7 @@ func init() {
 		Func:     TwoShills,
 		Desc:     "Verifies that an attempt to spawn a second instance of shill while an instance is already running will fail",
 		Contacts: []string{"deanliao@google.com", "chromeos-kernel-wifi@google.com"},
-		Attr:     []string{"group:mainline", "informational"},
+		Attr:     []string{"group:mainline"},
 	})
 }
 
