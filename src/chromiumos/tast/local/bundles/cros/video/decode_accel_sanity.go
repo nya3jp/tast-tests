f3bf265ad54fe8f00ab9b4de13605ad99c46fad9
David Staessens <dstaessens@chromium.org>
2019-10-08 16:30:16 +0900

    video.DecodeAccelSanity*: Combine video sanity tests using parameterized tests.
    This CL combines all video decoder sanity tests into a single testcase.
    
    Using parameterized tests causes the tests to have slightly different names:
    video.DecodeAccelSanityVP91 -> video.DecodeAccelSanity.vp91
    video.DecodeAccelSanityVP92 -> video.DecodeAccelSanity.vp92
    video.DecodeAccelSanityVP93 -> video.DecodeAccelSanity.vp93
    
    TEST=video.DecodeAccelSanity.* on samus
    
    BUG=b:139327919
    
    Change-Id: Ib718bba93e2cc337c9d4adeb3752e5016bd2170d

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_sanity.go
@@ -0,0 +1,58 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DecodeAccelSanity,
+		Desc: "Verify that the system doesn't crash when playing a VP9 video with unexpected VP9 profile1/2/3 features",
+		Contacts: []string{
+			"dstaessens@chromium.org",
+			"chromeos-gfx-video@google.com",
+			"chromeos-video-eng@google.com",
+		},
+		Attr:         []string{"group:mainline"},
+		SoftwareDeps: []string{"chrome", caps.HWDecodeVP9},
+		Params: []testing.Param{{
+			Name:      "vp91",
+			Val:       "vda_sanity-bear_profile1.vp9",
+			ExtraData: []string{"vda_sanity-bear_profile1.vp9", "vda_sanity-bear_profile1.vp9.json"},
+		}, {
+			Name:      "vp92",
+			Val:       "vda_sanity-bear_profile2.vp9",
+			ExtraAttr: []string{"informational"},
+			ExtraData: []string{"vda_sanity-bear_profile2.vp9", "vda_sanity-bear_profile2.vp9.json"},
+			// "vp9_sanity" is a whitelist of devices that don't crash when playing a VP9 profile0
+			// stream with unexpected profile2 features. Currently RK3399 devices may crash so they
+			// are excluded. See crbug.com/971032 for details.
+			ExtraSoftwareDeps: []string{"vp9_sanity"},
+		}, {
+			Name:      "vp93",
+			Val:       "vda_sanity-bear_profile3.vp9",
+			ExtraAttr: []string{"informational"},
+			ExtraData: []string{"vda_sanity-bear_profile3.vp9", "vda_sanity-bear_profile3.vp9.json"},
+			// "vp9_sanity" is a whitelist of devices that don't crash when playing a VP9 profile0
+			// stream with unexpected profile3 features. Currently RK3399 devices may crash so they
+			// are excluded. See crbug.com/971032 for details.
+			ExtraSoftwareDeps: []string{"vp9_sanity"},
+		}},
+	})
+}
+
+// DecodeAccelSanity runs the FlushAtEndOfStream test in the video_decode_accelerator_tests. The
+// vda_sanity-bear_profile{1,2,3}.vp9 video is used with metadata that incorrectly initializes the
+// video decoder for VP9 profile0. The test doesn't look at the decode result, but verifies system
+// robustness when encountering unexpected features.
+func DecodeAccelSanity(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoSanityTest(ctx, s, s.Param().(string))
+}
