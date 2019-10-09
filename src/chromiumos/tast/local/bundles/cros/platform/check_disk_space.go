bc37964e4a8c0520973f9551f077f81f48797c17
Bernie Thompson <bhthompson@google.com>
2019-10-09 09:12:24 -0700

    check_disk_space: Increase required disk space margin
    As we are seeing build failures due to the rootfs growing larger than the
    filesystem on a more regular basis, it would be helpful if there was an
    earlier warning. If we increase the margin from 2%/11MB to 5%/40MB we are
    more likely to catch unexpected rootfs growth early and be able to take
    action before build failures start.
    
    BUG=chromium:998345
    TEST=None
    
    Change-Id: I8fc5fde082912e994852a3dd50333106f6deae3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1850012
    Reviewed-by: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Bernie Thompson <bhthompson@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/platform/check_disk_space.go
+++ src/chromiumos/tast/local/bundles/cros/platform/check_disk_space.go
@@ -35,9 +35,9 @@ func CheckDiskSpace(ctx context.Context, s *testing.State) {
 		s.Log("Not checking available space since it's always 0 with squashfs")
 		return
 	}
-	// Require the minimum of 11 MiB and 2% of total.
-	var req int64 = 11 * 1024 * 1024
-	if b := int64(0.02 * float64(info.Used+info.Avail)); b < req {
+	// Require the minimum of 40 MiB and 5% of total.
+	var req int64 = 40 * 1024 * 1024
+	if b := int64(0.05 * float64(info.Used+info.Avail)); b < req {
 		req = b
 	}
 	if info.Avail < req {
