7b259882fe3198e8006c2de81c1f9d8043cb5bb0
David Staessens <dstaessens@chromium.org>
2019-10-10 13:05:07 +0900

    video.DecodeAccelVD: Combine video decoder VD tests using parameterized tests.
    This CL combines the binary video decode accelerator VD tests based on the
    video_decode_accelerator_tests binary into a single testcase.
    
    Using parameterized tests causes the tests to have slightly different names:
    video.DecodeAccelVDH264 -> video.DecodeAccelVD.h264
    video.DecodeAccelVDVP8 -> video.DecodeAccelVD.vp8
    video.DecodeAccelVDVP9 -> video.DecodeAccelVD.vp9
    video.DecodeAccelVDVP2 -> video.DecodeAccelVD.vp92
    
    TEST=video.DecodeAccelVD.* on samus
    
    BUG=b:139327919
    
    Cq-Depend:1839316,1845616
    Change-Id: I990f466f809b91b08d6fbd44c2f7baaaeabb9656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1851565
    Tested-by: David Staessens <dstaessens@chromium.org>
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd.go
@@ -0,0 +1,50 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DecodeAccelVD,
+		Desc:         "Verifies hardware decode acceleration of media::VideoDecoders by running the video_decode_accelerator_tests binary (see go/vd-migration)",
+		Contacts:     []string{"acourbot@chromium.org", "dstaessens@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "cros_video_decoder"},
+		Params: []testing.Param{{
+			Name:              "h264",
+			Val:               "test-25fps.h264",
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
+		}, {
+			Name:              "vp8",
+			Val:               "test-25fps.vp8",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
+		}, {
+			Name:              "vp9",
+			Val:               "test-25fps.vp9",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
+		}, {
+			Name: "vp9_2",
+			Val:  "test-25fps.vp9_2",
+			// TODO(crbug.com/911754): reenable this test once HDR VP9.2 is implemented.
+			ExtraAttr:         []string{"disabled"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_2},
+			ExtraData:         []string{"test-25fps.vp9_2", "test-25fps.vp9_2.json"},
+		}},
+	})
+}
+
+func DecodeAccelVD(ctx context.Context, s *testing.State) {
+	decode.RunAccelVideoTest(ctx, s, s.Param().(string), decode.VD)
+}
