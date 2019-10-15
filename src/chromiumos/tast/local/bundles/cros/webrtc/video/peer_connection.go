589a84fb492be537f39099db6b514454a981a732
Miguel Casas <mcasas@chromium.org>
2019-10-15 17:53:14 -0400

    tast/webrtc: Use getStats for {En,De}codeAccelUsedVP8
    This CL migrates the tests webrtc.{Decode,encode}AccelUsedVP8
    to use RTCPeerConnection getStats() API instead of checking
    histograms.
    
    It takes the chance to:
    - Remove the use of a y4m file, which is not needed to exercise
      an RTCPeerConnection and slow the test down.
    - Move the supporting logic to video/peer_connection.go,
      separating it from the support code for DecodeAccelUsedJPEG
      (this is completely different and is left untouched waiting
      for further cleanup, it should be under the camera/ folder).
    
    Note that getStats() doesn't fill in the codec implementation
    immediately, so we Poll() for up to 20 second. Experimentally,
    it takes ~2 or 3 VideoFrames to decide that value.
    
    BUG=chromium:1012828
    TEST=tast -verbose run papaya webrtc.*AccelUsed* on kohaku, dru
    
    Change-Id: I127697bd899e5d112f748cf4661d319291204c82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1862669
    Tested-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/webrtc/video/peer_connection.go
@@ -0,0 +1,132 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package video provides common code for webrtc.* tests related to video.
+package video
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/logging"
+	"chromiumos/tast/local/webrtc"
+	"chromiumos/tast/testing"
+)
+
+// CodecType is the type of codec to check for.
+type CodecType int
+
+const (
+	// Encoding refers to WebRTC video encoding.
+	Encoding CodecType = iota
+	// Decoding refers to WebRTC video decoding.
+	Decoding
+)
+
+// RunPeerConnection launches a loopback RTCPeerConnection and inspects that the
+// CodecType codec is hardware accelerated.
+func RunPeerConnection(ctx context.Context, s *testing.State, codecType CodecType) {
+	vl, err := logging.NewVideoLogger()
+	if err != nil {
+		s.Fatal("Failed to set values for verbose logging")
+	}
+	defer vl.Close()
+
+	chromeArgs := webrtc.ChromeArgsWithFakeCameraInput(true)
+	cr, err := chrome.New(ctx, chrome.ExtraArgs(chromeArgs...))
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	conn, err := cr.NewConn(ctx, server.URL+"/"+webrtc.LoopbackPage)
+	if err != nil {
+		s.Fatal("Failed to open video page: ", err)
+	}
+	defer conn.Close()
+	defer conn.CloseTarget(ctx)
+
+	if err := conn.WaitForExpr(ctx, "streamReady"); err != nil {
+		s.Fatal("timed out waiting for stream ready: ", err)
+	}
+
+	if err := checkForCodecImplementation(ctx, s, conn, codecType); err != nil {
+		s.Fatal("Failed: ", err)
+	}
+}
+
+// checkForCodecImplementation parses the RTCPeerConnection and verifies that it
+// is using hardware acceleration for codecType. This method uses the
+// RTCPeerConnection getStats() API [1].
+// [1] https://w3c.github.io/webrtc-pc/#statistics-model
+func checkForCodecImplementation(ctx context.Context, s *testing.State, conn *chrome.Conn, codecType CodecType) error {
+	// See [1] and [2] for the statNames to use here. The values are browser
+	// specific, for Chrome, "External{Deco,Enco}der" means that WebRTC is using
+	// hardware acceleration and anything else (e.g. "libvpx", "ffmpeg",
+	// "unknown") means it is not.
+	// [1] https://w3c.github.io/webrtc-stats/#dom-rtcinboundrtpstreamstats-decoderimplementation
+	// [2] https://w3c.github.io/webrtc-stats/#dom-rtcoutboundrtpstreamstats-encoderimplementation
+	statName := "encoderImplementation"
+	peerConnectionName := "localPeerConnection"
+	expectedImplementation := "ExternalEncoder"
+	if codecType == Decoding {
+		statName = "decoderImplementation"
+		peerConnectionName = "remotePeerConnection"
+		expectedImplementation = "ExternalDecoder"
+	}
+
+	parseStatsJS :=
+		fmt.Sprintf(`new Promise(function(resolve, reject) {
+			%s.getStats(null).then(stats => {
+				if (stats == null) {
+					reject("getStats() failed");
+					return;
+				}
+				stats.forEach(report => {
+					Object.keys(report).forEach(statName => {
+						if (statName === '%s') {
+							resolve(report[statName]);
+							return;
+						}
+					})
+				})
+				reject("%s not found");
+			});
+		})`, peerConnectionName, statName, statName)
+
+	// Poll getStats() to wait until expectedImplementation gets filled in:
+	// RTCPeerConnection needs a few frames to start up encoding/decoding; in the
+	// meantime it returns "unknown".
+	const pollInterval = 100 * time.Millisecond
+	const pollTimeout = 200 * pollInterval
+	var implementation string
+	err := testing.Poll(ctx,
+		func(ctx context.Context) error {
+			if err := conn.EvalPromise(ctx, parseStatsJS, &implementation); err != nil {
+				return errors.Wrap(err, "failed to retrieve and/or parse RTCStatsReport")
+			}
+			if implementation == "unknown" {
+				return errors.New("getStats() didn't fill in the codec implementation (yet)")
+			}
+			return nil
+		}, &testing.PollOptions{Interval: pollInterval, Timeout: pollTimeout})
+
+	if err != nil {
+		return err
+	}
+	s.Logf("%s: %s", statName, implementation)
+
+	if implementation != expectedImplementation {
+		return errors.Errorf("unexpected implementation, got %v, expected %v", expectedImplementation, implementation)
+	}
+	return nil
+}
