86d4fa90f93fca34c1b919ca640b3733e950c08b
Yu-Hsuan Hsu <yuhsuan@chromium.org>
2019-10-17 15:20:21 +0800

    tast: Add audio.CrasPlay and audio.CrasRecord tests
    Add two more tasts to check the basic function of CRAS.
    
    1. audio.CrasPlay
    Play audio through cras_test_client and then check whether the opened
    output device is not the fallback device.
    
    2. audio.CrasRecord
    Record audio through cras_test_client and then check whether the opened
    input device is not the fallback device.
    
    BUG=b:142700227
    TEST=tast run {IP} audio.CrasPlay
    tast run {IP} audio.CrasRecord
    
    Change-Id: Ida203fe67f6ebf9f4d460d958f8916c2a9aef207

---
+++ src/chromiumos/tast/local/bundles/cros/audio/cras_record.go
@@ -0,0 +1,75 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package audio
+
+import (
+	"context"
+	"regexp"
+	"strings"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CrasRecord,
+		Desc:         "Verifies CRAS record function works correctly",
+		Contacts:     []string{"yuhsuan@chromium.org", "cychiang@chromium.org"},
+		SoftwareDeps: []string{"audio_record"},
+		Attr:         []string{"group:mainline", "informational"},
+	})
+}
+
+func CrasRecord(ctx context.Context, s *testing.State) {
+
+	// Return the first running input device by parsing audio thread logs.
+	// A divice may not be opened immediately so it will repeat a query until there is a running input device.
+	getRunningInputDevice := func(ctx context.Context, timeout time.Duration) string {
+		ctx, cancel := ctxutil.OptionalTimeout(ctx, timeout)
+		defer cancel()
+		for {
+			s.Log("Dump audio thread to check running devices")
+			dump, err := testexec.CommandContext(ctx, "cras_test_client", "--dump_audio_thread").Output()
+			if err != nil {
+				s.Fatal("Failed to dump audio thread: ", err)
+			}
+
+			re, _ := regexp.Compile("Input dev: (.*)")
+			dev := re.FindStringSubmatch(string(dump))
+			if dev != nil {
+				return dev[1]
+			}
+
+			select {
+			case <-time.After(100 * time.Millisecond):
+			case <-ctx.Done():
+				s.Fatal("Failed to detect running input device")
+			}
+		}
+	}
+
+	// Playback function by CRAS.
+	command := testexec.CommandContext(
+		ctx, "cras_test_client",
+		"--capture_file", "/dev/null",
+		"--duration", "1",
+		"--num_channels", "2",
+		"--rate", "48000")
+	command.Start()
+
+	dev := getRunningInputDevice(ctx, time.Second)
+	s.Log("Input device: ", dev)
+
+	if strings.Contains(dev, "Silent") {
+		s.Fatal("Fallback to the silent device")
+	}
+
+	if err := command.Wait(); err != nil {
+		s.Fatal("Failed to wait: ", err)
+	}
+}
