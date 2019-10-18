bb9cc73d0cfc2ed3a6b75096173b15940caf534a
Derek Beckett <dbeckett@google.com>
2019-10-18 14:16:05 -0700

    [Tast] Added ability to Enroll a device for Enterprise Device testing
    Adds the extra steps needed for enrolling a device. Tested locally and
    chrome.go can now walk through the enrollment flow, and a normal (non
    enrollment) flow.
    
    BUG=chromium:867313
    TEST=Local "Device" test run (enrollment/login sucessful with a DM
    server)
    
    Change-Id: Ie7d6df6e00627341ee0049776523d9b049a31e92

--- src/chromiumos/tast/local/chrome/chrome.go
+++ src/chromiumos/tast/local/chrome/chrome.go
@@ -170,6 +170,12 @@ func DMSPolicy(url string) Option {
 	}
 }
 
+// EnterpriseEnroll returns an Option that can be passed to New to enable Enterprise
+// Enrollment
+func EnterpriseEnroll() Option {
+	return func(c *Chrome) { c.enroll = true }
+}
+
 // ARCDisabled returns an Option that can be passed to New to disable ARC.
 func ARCDisabled() Option {
 	return func(c *Chrome) { c.arcMode = arcDisabled }
@@ -227,6 +233,7 @@ type Chrome struct {
 	region             string
 	policyEnabled      bool   // flag to enable policy fetch
 	dmsAddr            string // Device Management URL, or empty if using default
+	enroll             bool   // whether device should be enrolled
 	arcMode            arcMode
 	restrictARCCPU     bool // a flag to control cpu restrictions on ARC
 	// If breakpadTestMode is true, tell Chrome's breakpad to always write
@@ -271,6 +278,7 @@ func New(ctx context.Context, opts ...Option) (*Chrome, error) {
 		loginMode:        fakeLogin,
 		region:           "us",
 		policyEnabled:    false,
+		enroll:           false,
 		breakpadTestMode: true,
 		watcher:          newBrowserWatcher(),
 		logMaster:        jslog.NewMaster(),
@@ -530,6 +538,9 @@ func (c *Chrome) restartChromeForTesting(ctx context.Context) error {
 		"--cros-region=" + c.region,                  // Force the region.
 		"--cros-regions-mode=hide",                   // Ignore default values in VPD.
 	}
+	if c.enroll {
+		args = append(args, "--disable-policy-key-verification") // Remove policy key verification for fake enrollment
+	}
 
 	if c.loginMode != gaiaLogin {
 		args = append(args, "--disable-gaia-services")
@@ -771,6 +782,16 @@ func (c *Chrome) getFirstOOBETarget(ctx context.Context) (*target.Info, error) {
 	return targets[0], nil
 }
 
+// possibleEnterpriseEnrollTargets returns the Gaia WebView targets, which are used
+// to help enrollment on the device.
+// Returns nil if none are found.
+func (c *Chrome) possibleEnterpriseEnrollTargets(ctx context.Context) ([]*target.Info, error) {
+	isGAIAWebView := func(t *target.Info) bool {
+		return t.Type == "webview" && strings.HasPrefix(t.URL, "https://accounts.google.com/")
+	}
+	return c.devsess.FindTargets(ctx, isGAIAWebView)
+}
+
 // waitForOOBEConnection waits for that the OOBE page is shown, then returns
 // a connection to the page. The caller must close the returned connection.
 func (c *Chrome) waitForOOBEConnection(ctx context.Context) (*Conn, error) {
@@ -812,6 +833,79 @@ func (c *Chrome) waitForOOBEConnection(ctx context.Context) (*Conn, error) {
 	return connToRet, nil
 }
 
+// userDomain will return the "domain" section of the c.user.
+// e.g. something@managedchrome.com will return "managedchrome"
+func (c *Chrome) userDomain() string {
+	return strings.Split(strings.Split(c.user, "@")[1], ".")[0]
+}
+
+// waitForEnrollmentLoginScreen will wait for the Enrollment screen to complete
+// and the Enrollment login screen to appear. If the login screen does not appear
+// the testing.Poll will timeout.
+func (c *Chrome) waitForEnrollmentLoginScreen(ctx context.Context) error {
+	loginBanner := `document.querySelectorAll('span[title="managedchrome.com"]').length;`
+	loginStr := fmt.Sprintf("apps.googleusercontent.com&manageddomain=" + c.userDomain())
+
+	pollOpt := &testing.PollOptions{Timeout: 45 * time.Second}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		gaiaTargets, err := c.possibleEnterpriseEnrollTargets(ctx)
+		if err != nil {
+			return errors.Wrap(err, "no Enrollment webview targets")
+		}
+		for _, gaiaTarget := range gaiaTargets {
+			// It's common for multiple targets to be returned.
+			// We want to run the command specifically on the "apps" target.
+			if !strings.Contains(string(gaiaTarget.URL), loginStr) {
+				continue
+			}
+
+			testing.ContextLog(ctx, "Enrollment apps url: ", string(gaiaTarget.URL))
+			webViewConn, err := c.NewConnForTarget(ctx, MatchTargetURL(gaiaTarget.URL))
+			if err != nil {
+				// TODO: Determine if this should be a failure or pass.
+				// Leaning towards pass due to Telemetry implementation, and the fact
+				// if the "managedchrome" query never returns anything an error will occur anyways.
+				testing.ContextLog(ctx, "Enrollment screen connection err occured here: ", err)
+				continue
+			}
+			defer webViewConn.Close()
+			content := -1
+			if err := webViewConn.Eval(ctx, loginBanner, &content); err != nil {
+				return err
+			}
+			// Found the login screen.
+			if content == 1 {
+				return nil
+			}
+		}
+		return errors.New("Enterprise Enrollment login screen not found")
+	}, pollOpt); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// enterpriseOOBELogin will complete the oobe login after Enrollment completes.
+func (c *Chrome) enterpriseOOBELogin(ctx context.Context, conn *Conn) error {
+	if err := c.waitForEnrollmentLoginScreen(ctx); err != nil {
+		return errors.Wrap(c.chromeErr(err), "could not enroll")
+	}
+
+	if err := conn.WaitForExpr(ctx, "typeof Oobe == 'function' && Oobe.readyForTesting"); err != nil {
+		return err
+	}
+
+	// Now login like "normal".
+	if err := conn.Exec(ctx, fmt.Sprintf("Oobe.loginForTesting('%s', '%s', '%s', false)",
+		c.user, c.pass, c.gaiaID)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // logIn logs in to a freshly-restarted Chrome instance.
 // It waits for the login process to complete before returning.
 func (c *Chrome) logIn(ctx context.Context) error {
@@ -827,11 +921,18 @@ func (c *Chrome) logIn(ctx context.Context) error {
 
 	switch c.loginMode {
 	case fakeLogin:
-		if err = conn.Exec(ctx, fmt.Sprintf("Oobe.loginForTesting('%s', '%s', '%s', false)", c.user, c.pass, c.gaiaID)); err != nil {
+		if err = conn.Exec(ctx, fmt.Sprintf("Oobe.loginForTesting('%s', '%s', '%s', %t)",
+			c.user, c.pass, c.gaiaID, c.enroll)); err != nil {
 			return err
 		}
 	case gaiaLogin:
 		if err = c.performGAIALogin(ctx, conn); err != nil {
+			return err
+		}
+	}
+
+	if c.enroll {
+		if err = c.enterpriseOOBELogin(ctx, conn); err != nil {
 			return err
 		}
 	}
@@ -876,14 +977,14 @@ func (c *Chrome) performGAIALogin(ctx context.Context, oobeConn *Conn) error {
 		}
 	}
 
-	isGAIAWebview := func(t *target.Info) bool {
+	isGAIAWebView := func(t *target.Info) bool {
 		return t.Type == "webview" && strings.HasPrefix(t.URL, "https://accounts.google.com/")
 	}
 
 	testing.ContextLog(ctx, "Waiting for GAIA webview")
 	var target *target.Info
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		if targets, err := c.devsess.FindTargets(ctx, isGAIAWebview); err != nil {
+		if targets, err := c.devsess.FindTargets(ctx, isGAIAWebView); err != nil {
 			return err
 		} else if len(targets) != 1 {
 			return errors.Errorf("got %d GAIA targets; want 1", len(targets))
