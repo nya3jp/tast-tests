bba4c1bb5a52418fc26776668bfc918e6bd1f23b
Shuo-Peng Liao <deanliao@chromium.org>
2019-10-21 15:18:00 +0800

    Tast: Poll the WiFi interface name until timeout.
    Because shill service may be just restarted by a previous test, shill
    may not have a complete device list yet. Instead of querying once and
    reporting WiFi device not found, shill.GetWifiInterface() repeatedly
    queries for WiFi device when it receives a manager's property change
    signal until timeout is reached.
    
    Also, update UdevRename as interface of shill.GetWifiInterface() is
    changed.
    
    BUG=chromium:999707
    TEST=tast run $DUT network.EthernetStaticIP network.IWScan \
    network.UdevRename
    As network.EthernetStaticIP restarts shill, it triggers the issue. We
    can run it before network.IWScan to verify the fix. Also, running
    network.UdevRename to verify the change.
    
    Change-Id: Idf5a131b466658df6a82b1e0dc45945f95358476
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1866158
    Tested-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Commit-Queue: Shuo-Peng Liao <deanliao@chromium.org>

--- src/chromiumos/tast/local/shill/wifi.go
+++ src/chromiumos/tast/local/shill/wifi.go
@@ -8,43 +8,51 @@ package shill
 
 import (
 	"context"
+	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 )
 
-// GetWifiInterface returns the WiFi interface name.
+// GetWifiInterface polls the WiFi interface name with timeout.
 // It returns "" with error if no (or more than one) WiFi interface is found.
-// Obtained by querying WiFi device from shill.
-func GetWifiInterface(ctx context.Context) (string, error) {
-	m, err := NewManager(ctx)
-	if err != nil {
-		return "", errors.Wrap(err, "failed to create shill manager proxy")
-	}
-	devPaths, err := m.GetDevices(ctx)
-	if err != nil {
-		return "", errors.Wrap(err, "failed to obtain paths of shill's devices")
-	}
-	var wifis []string
-	for _, path := range devPaths {
-		dev, err := NewDevice(ctx, path)
+func GetWifiInterface(ctx context.Context, m *Manager, timeout time.Duration) (string, error) {
+	ctx, cancel := ctxutil.OptionalTimeout(ctx, timeout)
+	defer cancel()
+
+	getWifiIfaces := func() ([]string, error) {
+		devs, err := m.GetDevicesByTechnology(ctx, TechnologyWifi)
 		if err != nil {
+			return nil, err
+		}
+		var ifaces []string
+		for _, dev := range devs {
+			if iface, err := dev.Properties().GetString(DevicePropertyInterface); err == nil {
+				ifaces = append(ifaces, iface)
+			}
+		}
+		return ifaces, nil
+	}
+
+	pw, err := m.Properties().CreateWatcher(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create a PropertiesWatcher")
+	}
+	defer pw.Close(ctx)
+
+	for {
+		// If more than one WiFi interface is found, an error is raised.
+		// If there's no WiFi interface, probe again when manager's "Devices" property is changed.
+		if ifaces, err := getWifiIfaces(); err != nil {
 			return "", err
+		} else if len(ifaces) > 1 {
+			return "", errors.Errorf("more than one WiFi interface found: %q", ifaces)
+		} else if len(ifaces) == 1 {
+			return ifaces[0], nil
 		}
 
-		if devType, err := dev.Properties().GetString(DevicePropertyType); err != nil {
-			return "", err
-		} else if devType != "wifi" {
-			continue
-		}
-
-		devIface, err := dev.Properties().GetString(DevicePropertyInterface)
-		if err != nil {
+		if err := pw.WaitAll(ctx, ManagerPropertyDevices); err != nil {
 			return "", err
 		}
-		wifis = append(wifis, devIface)
 	}
-	if len(wifis) != 1 {
-		return "", errors.Errorf("expect only one WiFi interface, found: %q", wifis)
-	}
-	return wifis[0], nil
 }
