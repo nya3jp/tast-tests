c19b61aceefd7c099fad5399472a62c78d16d3a9
Shuo-Peng Liao <deanliao@chromium.org>
2019-10-21 15:18:00 +0800

    Tast: Poll the WiFi interface name until timeout.
    Because shill service may be just restarted by a previous test, shill
    may not have a complete device list yet. Instead of querying once and
    reporting WiFi device not found, shill.GetWifiInterface() repeatedly
    queries for WiFi device when it receives a manager's property change
    signal until timeout is reached.
    
    Also, update UdevRename as interface of shill.GetWifiInterface() is
    changed.
    
    BUG=chromium:999707
    TEST=tast run $DUT network.EthernetStaticIP network.IWScan \
    network.UdevRename
    As network.EthernetStaticIP restarts shill, it triggers the issue. We
    can run it before network.IWScan to verify the fix. Also, running
    network.UdevRename to verify the change.
    
    Change-Id: Idf5a131b466658df6a82b1e0dc45945f95358476

--- src/chromiumos/tast/local/shill/manager.go
+++ src/chromiumos/tast/local/shill/manager.go
@@ -35,6 +35,21 @@ type Manager struct {
 	props      *Properties
 }
 
+// Technology is the type of a shill device's technology
+type Technology string
+
+// Device technologies
+// Refer to Flimflam type options in
+// https://chromium.googlesource.com/chromiumos/platform2/+/refs/heads/master/system_api/dbus/shill/dbus-constants.h#334
+const (
+	TechnologyBluetooth Technology = "bluetooth"
+	TechnologyCellular  Technology = "cellular"
+	TechnologyEthernet  Technology = "ethernet"
+	TechnologyPPPoE     Technology = "pppoe"
+	TechnologyVPN       Technology = "vpn"
+	TechnologyWifi      Technology = "wifi"
+)
+
 // NewManager connects to shill's Manager.
 func NewManager(ctx context.Context) (*Manager, error) {
 	conn, obj, err := dbusutil.Connect(ctx, dbusService, dbusManagerPath)
@@ -211,11 +226,42 @@ func (m *Manager) PopAllUserProfiles(ctx context.Context) error {
 }
 
 // EnableTechnology enables a technology interface.
-func (m *Manager) EnableTechnology(ctx context.Context, technology string) error {
-	return m.dbusObject.Call(ctx, "EnableTechnology", technology).Err
+func (m *Manager) EnableTechnology(ctx context.Context, technology Technology) error {
+	return m.dbusObject.Call(ctx, "EnableTechnology", string(technology)).Err
 }
 
 // DisableTechnology disables a technology interface.
-func (m *Manager) DisableTechnology(ctx context.Context, technology string) error {
-	return m.dbusObject.Call(ctx, "DisableTechnology", technology).Err
+func (m *Manager) DisableTechnology(ctx context.Context, technology Technology) error {
+	return m.dbusObject.Call(ctx, "DisableTechnology", string(technology)).Err
+}
+
+// GetDevicesByTechnology returns list of Devices of the specified technology.
+func (m *Manager) GetDevicesByTechnology(ctx context.Context, technology Technology) ([]*Device, error) {
+	var devs []*Device
+	// Refresh properties first.
+	_, err := m.GetProperties(ctx)
+	if err != nil {
+		return nil, err
+	}
+	devPaths, err := m.GetDevices(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, path := range devPaths {
+		dev, err := NewDevice(ctx, path)
+		// It is forgivable as a device may go down anytime.
+		if err != nil {
+			testing.ContextLogf(ctx, "Error getting a device %q: %v", path, err)
+			continue
+		}
+		if devType, err := dev.Properties().GetString(DevicePropertyType); err != nil {
+			testing.ContextLogf(ctx, "Error getting the type of the device %q: %v", path, err)
+			continue
+		} else if devType != string(technology) {
+			continue
+		}
+		devs = append(devs, dev)
+	}
+	return devs, nil
 }
