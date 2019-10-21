a839c6535621114f133c682d430585ef6a024757
Victor-Gabriel Savu <vsavu@google.com>
2019-10-21 11:11:26 +0200

    tast-test: add test for GetAvailableRoutines
    Add test for GetAvailableRoutines. Depends on change in
    diagnostics_dpsl_test_requester.
    
    BUG=b:143065923
    TEST=tast run $IP wilco.APIGetAvailableRoutines
    
    Cq-Depend: chromium:1871669
    Change-Id: If2c5140922b6194daf9ecf4f2cefa8da9a3be7fd

---
+++ src/chromiumos/tast/local/bundles/cros/wilco/api_get_available_routines.go
@@ -0,0 +1,64 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wilco
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/wilco/pre"
+	"chromiumos/tast/local/wilco"
+	"chromiumos/tast/testing"
+	dtcpb "chromiumos/wilco_dtc"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: APIGetAvailableRoutines,
+		Desc: "Test sending GetAvailableRoutines gRPC request from Wilco DTC VM to the Wilco DTC Support Daemon",
+		Contacts: []string{
+			"vsavu@chromium.org",  // Test author
+			"pmoy@chromium.org",   // wilco_dtc_supportd author
+			"lamzin@chromium.org", // wilco_dtc_supportd maintainer
+			"chromeos-wilco@google.com",
+		},
+		Attr:         []string{"informational"},
+		SoftwareDeps: []string{"vm_host", "wilco"},
+		Pre:          pre.WilcoDtcSupportdAPI,
+	})
+}
+
+func APIGetAvailableRoutines(ctx context.Context, s *testing.State) {
+	request := dtcpb.GetAvailableRoutinesRequest{}
+	response := dtcpb.GetAvailableRoutinesResponse{}
+
+	if err := wilco.DPSLSendMessage(ctx, "GetAvailableRoutines", &request, &response); err != nil {
+		s.Fatal("unable to get Routines: ", err)
+	}
+
+	if len(response.Routines) == 0 {
+		s.Fatal("No routines available")
+	}
+
+	contains := func(all []dtcpb.DiagnosticRoutine, expected dtcpb.DiagnosticRoutine) bool {
+		for _, e := range all {
+			if expected == e {
+				return true
+			}
+		}
+		return false
+	}
+	// Error conditions defined by the proto definition.
+
+	for _, val := range []dtcpb.DiagnosticRoutine{
+		dtcpb.DiagnosticRoutine_ROUTINE_BATTERY,
+		dtcpb.DiagnosticRoutine_ROUTINE_BATTERY_SYSFS,
+		dtcpb.DiagnosticRoutine_ROUTINE_URANDOM,
+		dtcpb.DiagnosticRoutine_ROUTINE_SMARTCTL_CHECK,
+	} {
+		if !contains(response.Routines, val) {
+			s.Fatalf("Routine %s missing", val.String())
+		}
+	}
+}
