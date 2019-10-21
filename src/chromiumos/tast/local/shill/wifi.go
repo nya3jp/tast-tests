ecf5e7164be6495f970dbc6ecfa2b11f25658adc
Shuo-Peng Liao <deanliao@chromium.org>
2019-10-21 15:18:00 +0800

    Tast: poll the WiFi interface name until timeout.
    Because shill service may be just restarted by a previous test, shill
    may not be able to report the correct device list. Instead of querying
    once, shill.GetWifiInterface() tries to get a WiFi interface when shill
    manager's property change event is observed.
    
    BUG=chromium:999707
    TEST=tast run $DUT network.IWScan with the shill restart code
    at the beginning of IWScan():
            if err := shill.SafeStop(ctx); err != nil {
                    s.Fatal("Failed stopping shill: ", err)
            } else if err = shill.SafeStart(ctx); err != nil {
                    s.Fatal("Failed starting shill: ", err)
            }
    to reproduce shill restarting environment.
    
    Change-Id: Idf5a131b466658df6a82b1e0dc45945f95358476

--- src/chromiumos/tast/local/shill/wifi.go
+++ src/chromiumos/tast/local/shill/wifi.go
@@ -8,23 +8,24 @@ package shill
 
 import (
 	"context"
+	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 )
 
-// GetWifiInterface returns the WiFi interface name.
-// It returns "" with error if no (or more than one) WiFi interface is found.
-// Obtained by querying WiFi device from shill.
-func GetWifiInterface(ctx context.Context) (string, error) {
-	m, err := NewManager(ctx)
-	if err != nil {
-		return "", errors.Wrap(err, "failed to create shill manager proxy")
-	}
+// getWifiInterface gets the WiFi interface name from shill devices.
+// It returns "" if no WiFi interface is found.
+// It returns error if more than one WiFi interface is found.
+func (m *Manager) getWifiInterface(ctx context.Context) (string, error) {
+	var wifis []string
+	// Refresh properties first.
+	m.GetProperties(ctx)
 	devPaths, err := m.GetDevices(ctx)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to obtain paths of shill's devices")
+		return "", err
 	}
-	var wifis []string
+
 	for _, path := range devPaths {
 		dev, err := NewDevice(ctx, path)
 		if err != nil {
@@ -43,8 +44,49 @@ func GetWifiInterface(ctx context.Context) (string, error) {
 		}
 		wifis = append(wifis, devIface)
 	}
-	if len(wifis) != 1 {
+
+	if len(wifis) > 1 {
 		return "", errors.Errorf("expect only one WiFi interface, found: %q", wifis)
+	} else if len(wifis) < 1 {
+		return "", nil
 	}
+
 	return wifis[0], nil
+}
+
+// GetWifiInterface polls the WiFi interface name with timeout.
+// It returns "" with error if no (or more than one) WiFi interface is found.
+func GetWifiInterface(ctx context.Context, timeout time.Duration) (string, error) {
+	ctx, cancel := ctxutil.OptionalTimeout(ctx, timeout)
+	defer cancel()
+
+	m, err := NewManager(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create shill manager proxy")
+	}
+
+	if wifi, err := m.getWifiInterface(ctx); err == nil && wifi != "" {
+		return wifi, nil
+	}
+
+	// Failed getting WiFi interface, wait for shill's Device property change.
+	pw, err := m.Properties().CreateWatcher(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create a PropertiesWatcher")
+	}
+	defer func() {
+		pw.Close(ctx)
+	}()
+
+	for {
+		if err := pw.WaitAll(ctx, "Devices"); err != nil {
+			return "", err
+		}
+
+		if wifi, err := m.getWifiInterface(ctx); err != nil {
+			return "", errors.Wrap(err, "failed to get WiFi interface from shill")
+		} else if wifi != "" {
+			return wifi, nil
+		}
+	}
 }
