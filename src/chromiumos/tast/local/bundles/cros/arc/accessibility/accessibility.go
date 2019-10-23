fc94f780656d56e227298f120e2f7b0c90c42747
Sara Kato <sarakato@chromium.org>
2019-10-23 13:31:54 +0900

    tast-tests: clean accessibility tests
    Add RunTest function in accessibility.go, which contains common
    logic among accessibility tests.
    
    Also move tab logic (in a11yEvent), to its' own function.
    
    BUG=b:140696360
    TEST=arc.accessibility* tests passes
    Change-Id: I3882ffb7dbd98515fc36c1bd6d21a44062c7f429

--- src/chromiumos/tast/local/bundles/cros/arc/accessibility/accessibility.go
+++ src/chromiumos/tast/local/bundles/cros/arc/accessibility/accessibility.go
@@ -11,17 +11,23 @@ import (
 	"strings"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/arc/ui"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
 const (
-	// This is a build of an application containing a single activity and basic UI elements.
+	// ApkName the apk which will be used in accessibility tests.
+	// It is a build of an application containing a single activity and basic UI elements.
 	// The source code is in vendor/google_arc.
+	ApkName      = "ArcAccessibilityTest.apk"
 	packageName  = "org.chromium.arc.testapp.accessibilitytest"
 	activityName = "org.chromium.arc.testapp.accessibilitytest.AccessibilityActivity"
 
@@ -32,7 +38,49 @@ const (
 	webViewID         = "org.chromium.arc.testapp.accessibilitytest:id/webView"
 
 	extURL = "chrome-extension://mndnfokpggljbaajbnioimlmbfngpief/cvox2/background/background.html"
+
+	// CheckBox class for UI widget.
+	CheckBox = "android.widget.CheckBox"
+	// EditText class for UI widget.
+	EditText = "android.widget.EditText"
+	// SeekBar class for UI widget.
+	SeekBar = "android.widget.SeekBar"
+	// ToggleButton class for UI widget.
+	ToggleButton = "android.widget.ToggleButton"
 )
+
+// AutomationNode represents an accessibility struct, which contains properties from chrome.automation.Autotmation.
+// This is defined at:
+// https://developer.chrome.com/extensions/automation#type-AutomationNode
+// Only the properties which are used in tast tests are defined here.
+type AutomationNode struct {
+	ClassName     string
+	Tooltip       string
+	Checked       string
+	ValueForRange int
+}
+
+// FocusedNode returns the currently focused node of ChromeVox.
+// chrome.automation.AutomationNode properties are defined using getters
+// see: https://cs.chromium.org/chromium/src/extensions/renderer/resources/automation/automation_node.js?q=automationNode&sq=package:chromium&dr=CSs&l=1218
+// meaning that resolve(node) cannot be applied here.
+func FocusedNode(ctx context.Context, chromeVoxConn *chrome.Conn) (*AutomationNode, error) {
+	var automationNode AutomationNode
+	const script = `new Promise((resolve, reject) => {
+				chrome.automation.getFocus((node) => {
+					resolve({
+						Tooltip: node.tooltip,
+						Checked: node.checked,
+						ClassName: node.className,
+						ValueForRange: node.valueForRange
+					});
+				});
+			})`
+	if err := chromeVoxConn.EvalPromise(ctx, script, &automationNode); err != nil {
+		return nil, err
+	}
+	return &automationNode, nil
+}
 
 // Enabled checks if accessibility is enabled in Android.
 func Enabled(ctx context.Context, a *arc.ARC) (bool, error) {
@@ -43,10 +91,10 @@ func Enabled(ctx context.Context, a *arc.ARC) (bool, error) {
 	return strings.TrimSpace(string(res)) == "1", nil
 }
 
-// ChromeVoxExtConn returns a connection to the ChromeVox extension's background page.
+// chromeVoxExtConn returns a connection to the ChromeVox extension's background page.
 // If the extension is not ready, the connection will be closed before returning.
 // Otherwise the calling function will close the connection.
-func ChromeVoxExtConn(ctx context.Context, c *chrome.Chrome) (*chrome.Conn, error) {
+func chromeVoxExtConn(ctx context.Context, c *chrome.Chrome) (*chrome.Conn, error) {
 	testing.ContextLog(ctx, "Waiting for ChromeVox background page at ", extURL)
 	extConn, err := c.NewConnForTarget(ctx, chrome.MatchTargetURL(extURL))
 	if err != nil {
@@ -64,9 +112,9 @@ func ChromeVoxExtConn(ctx context.Context, c *chrome.Chrome) (*chrome.Conn, erro
 	return extConn, nil
 }
 
-// EnableSpokenFeedback will enable spoken feedback on Chrome.
+// enableSpokenFeedback will enable spoken feedback on Chrome.
 // Also checks that setting is reflected in Android as well.
-func EnableSpokenFeedback(ctx context.Context, cr *chrome.Chrome, a *arc.ARC) error {
+func enableSpokenFeedback(ctx context.Context, cr *chrome.Chrome, a *arc.ARC) error {
 	conn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		return errors.Wrap(err, "creating test API connection failed")
@@ -100,28 +148,8 @@ func EnableSpokenFeedback(ctx context.Context, cr *chrome.Chrome, a *arc.ARC) er
 	return nil
 }
 
-// NewChrome starts Chrome calling chrome.New() with accessibility enabled.
-// The calling function will close the connection.
-func NewChrome(ctx context.Context) (*chrome.Chrome, error) {
-	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs("--force-renderer-accessibility"))
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to connect to Chrome: ")
-	}
-	return cr, nil
-}
-
-// NewARC starts ARC by calling arc.New().
-// The calling function will close the connection.
-func NewARC(ctx context.Context, outDir string) (*arc.ARC, error) {
-	a, err := arc.New(ctx, outDir)
-	if err != nil {
-		return nil, errors.New("failed to start ARC")
-	}
-	return a, err
-}
-
-// InstallAndStartSampleApp starts the test application, and checks that UI components exist.
-func InstallAndStartSampleApp(ctx context.Context, a *arc.ARC, apkPath string) error {
+// installAndStartSampleApp starts the test application, and checks that UI components exist.
+func installAndStartSampleApp(ctx context.Context, a *arc.ARC, apkPath string) error {
 	testing.ContextLog(ctx, "Installing app")
 	// Install ArcAccessibilityTest.apk
 	if err := a.Install(ctx, apkPath); err != nil {
@@ -151,22 +179,16 @@ func InstallAndStartSampleApp(ctx context.Context, a *arc.ARC, apkPath string) e
 	return nil
 }
 
-// WaitForElementFocused polls until the specified UI element (focusClassName) has focus.
+// WaitForFocusedNode polls until the properties of the focused node matches node.
 // Returns an error after 30 seconds.
-func WaitForElementFocused(ctx context.Context, chromeVoxConn *chrome.Conn, focusClassName string) error {
-	const script = `new Promise((resolve, reject) => {
-			chrome.automation.getFocus((node) => {
-				resolve(node.className);
-			});
-		})`
+func WaitForFocusedNode(ctx context.Context, chromeVoxConn *chrome.Conn, node *AutomationNode) error {
 	// Wait for focusClassName to receive focus.
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		var currFocusClassName string
-		if err := chromeVoxConn.EvalPromise(ctx, script, &currFocusClassName); err != nil {
+		focusedNode, err := FocusedNode(ctx, chromeVoxConn)
+		if err != nil {
 			return err
-		}
-		if strings.TrimSpace(currFocusClassName) != focusClassName {
-			return errors.Errorf("%q does not have focus, %q has focus instead", focusClassName, currFocusClassName)
+		} else if !cmp.Equal(focusedNode, node, cmpopts.EquateEmpty()) {
+			return errors.Errorf("focused node is %q, want %q", focusedNode, node)
 		}
 		return nil
 	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
@@ -192,8 +214,8 @@ func WaitForChromeVoxStopSpeaking(ctx context.Context, chromeVoxConn *chrome.Con
 	return nil
 }
 
-// WaitForChromeVoxReady polls until ChromeVox connection finishes loading.
-func WaitForChromeVoxReady(ctx context.Context, chromeVoxConn *chrome.Conn) error {
+// waitForChromeVoxReady polls until ChromeVox connection finishes loading.
+func waitForChromeVoxReady(ctx context.Context, chromeVoxConn *chrome.Conn) error {
 	if err := chromeVoxConn.WaitForExpr(ctx, `document.readyState === "complete"`); err != nil {
 		return errors.Wrap(err, "timed out waiting for ChromeVox connection to be ready")
 	}
@@ -205,4 +227,69 @@ func WaitForChromeVoxReady(ctx context.Context, chromeVoxConn *chrome.Conn) erro
 
 	testing.ContextLog(ctx, "ChromeVox is ready")
 	return nil
+}
+
+// SpeechLog obtains the speech log of ChromeVox.
+func SpeechLog(ctx context.Context, chromeVoxConn *chrome.Conn) ([]string, error) {
+	// speechLog represents a log of accessibility speech.
+	type speechLog struct {
+		Text string `json:"textString_"`
+		// Other values are not used in test.
+	}
+	var logs []speechLog
+	if err := chromeVoxConn.Eval(ctx, "LogStore.instance.getLogsOfType(LogStore.LogType.SPEECH)", &logs); err != nil {
+		return nil, err
+	}
+	var gotLogs []string
+	for _, log := range logs {
+		if log.Text != "" {
+			gotLogs = append(gotLogs, log.Text)
+		}
+	}
+	return gotLogs, nil
+}
+
+// RunTest starts Chrome with the accessibility features enabled.
+// It install the ArcAccessibilityTestApplication, launches it, and waits
+// for it (and ChromeVox) to be ready.
+func RunTest(ctx context.Context, s *testing.State, f func(a *arc.ARC, conn *chrome.Conn, ew *input.KeyboardEventWriter)) {
+	const (
+		apkName = "ArcAccessibilityTest.apk"
+	)
+	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs("--force-renderer-accessibility"))
+	if err != nil {
+		s.Fatal(err) // NOLINT: arc/ui returns loggable errors
+	}
+	defer cr.Close(ctx)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal(err) // NOLINT: arc/ui returns loggable errors
+	}
+	defer a.Close()
+
+	if err := installAndStartSampleApp(ctx, a, s.DataPath(apkName)); err != nil {
+		s.Fatal("Setting up ARC environment with accessibility failed: ", err)
+	}
+
+	if err := enableSpokenFeedback(ctx, cr, a); err != nil {
+		s.Fatal(err) // NOLINT: arc/ui returns loggable errors
+	}
+
+	chromeVoxConn, err := chromeVoxExtConn(ctx, cr)
+	if err != nil {
+		s.Fatal("Creating connection to ChromeVox extension failed: ", err)
+	}
+	defer chromeVoxConn.Close()
+
+	if err := waitForChromeVoxReady(ctx, chromeVoxConn); err != nil {
+		s.Fatal("Could not wait for ChromeVox to be ready: ", err)
+	}
+
+	ew, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Error with creating EventWriter from keyboard: ", err)
+	}
+	defer ew.Close()
+	f(a, chromeVoxConn, ew)
 }
