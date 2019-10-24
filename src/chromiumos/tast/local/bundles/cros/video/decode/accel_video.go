6381af2090d0f1c27f093f641980a22be864e570
David Staessens <dstaessens@chromium.org>
2019-10-24 10:54:47 +0900

    tast.video: Drop 'tast_' prefix from video performance metrics.
    This CL drops the 'tast_' prefix from the video performance metrics reported to
    crosbolt. The 'tast_' prefix was used to differentiate between autotest and tast
    test results (this turned out to be not necessary).
    
    This change will affect all crosbolt metrics of the following tests:
    video.DecodeAccelPerf*
    video.ARCEncodeAccelPerf*
    video.ARCDecodeAccelPerf*
    video.EncodeAccelPerf*
    video.PlaybackPerf*
    
    TEST=a selection of affected tests on nocturne.
    
    BUG=chromium:955957
    
    Change-Id: If7834db5f5d7f045e7d9b66d45ffc5fa92a4fc5c

--- src/chromiumos/tast/local/bundles/cros/video/decode/accel_video.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode/accel_video.go
@@ -190,9 +190,8 @@ func RunAccelVideoPerfTest(ctx context.Context, s *testing.State, filename strin
 	}
 	cpuUsage := measurements["cpu"]
 
-	// TODO(dstaessens@): Remove "tast_" prefix after removing video_VDAPerf in autotest.
 	p.Set(perf.Metric{
-		Name:      "tast_cpu_usage",
+		Name:      "cpu_usage",
 		Unit:      "percent",
 		Direction: perf.SmallerIsBetter,
 	}, cpuUsage)
