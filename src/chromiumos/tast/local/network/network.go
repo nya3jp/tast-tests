ac242e35e5625bead734cd2c801c3afd0a733c25
Jason Jeremy Iman <jasongustaman@google.com>
2019-10-24 14:26:25 +0900

    tast: Fix ARC multi-networking connectivity test
    ARC multi-networking connectivity test is failing
    on multiple devices.
    
    This is happening due to:
    - multi-networking not available on N,
    - not checking for interfaces that are down,
    - not checking if actual the physical interfaces
      have outbound connectivity.
    
    This patch addresses those mistakes by:
    - testing only on P,
    - checking for interface status (up / down),
    - checking connectivity by pinging gateway address
      instead of 8.8.8.8.
    
    BUG=b:143106280
    TEST=tast run <DUT> arc.MultiNetworkingConnectivity
         with interface up, down, and no connectivity
         (nocturne).
    
    Change-Id: I2a596aa640c789728e42313421e4c5f5fcf13a31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1877793
    Tested-by: Jason Jeremy Iman <jasongustaman@chromium.org>
    Auto-Submit: Jason Jeremy Iman <jasongustaman@chromium.org>
    Commit-Queue: Sean Abraham <seanabraham@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>

---
+++ src/chromiumos/tast/local/network/network.go
@@ -0,0 +1,36 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"regexp"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+)
+
+// ipv4Regex is a regex that matches IPv4 address.
+var ipv4Regex = regexp.MustCompile(`(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
+
+// Gateway returns IPv4 gateway address of an interface by calling "ip route get 8.8.8.8".
+func Gateway(ctx context.Context, ifname string) (string, error) {
+	// Get the physical interface gateway address.
+	out, err := testexec.CommandContext(ctx, "/bin/ip", "route", "get", "8.8.8.8", "oif", ifname).Output(testexec.DumpLogOnError)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to get gateway address for interface %s", ifname)
+	}
+
+	// ip route get 8.8.8.8 returns address of the next hop to 8.8.8.8 with the following format
+	// 8.8.8.8 via "gateway_address" ...
+	// e.g. "8.8.8.8 via 100.87.84.254 ..."
+	// Gateway is the second valid IPv4 address.
+	m := ipv4Regex.FindAllString(string(out), 2)
+	if len(m) < 2 {
+		return "", errors.Errorf("failed to parse gateway address from 'ip route' for interface %s", ifname)
+	}
+	gateway := m[1]
+	return gateway, nil
+}
