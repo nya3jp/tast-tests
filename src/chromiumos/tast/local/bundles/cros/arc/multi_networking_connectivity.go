e7e8f3718ffd33332fde88e8d18b0d93d278b731
Jason Jeremy Iman <jasongustaman@google.com>
2019-10-24 14:26:25 +0900

    tast: Fix ARC multi-networking connectivity test
    ARC multi-networking connectivity test is failing
    on multiple devices.
    
    This is happening due to:
    - multi-networking not available on N,
    - not checking for interfaces that are down,
    - not checking if actual the physical interfaces
      have outbound connectivity.
    
    This patch addresses those mistakes by:
    - testing only on P,
    - checking for interface status (up / down),
    - checking connectivity by pinging gateway address
      instead of 8.8.8.8.
    
    BUG=b:143106280
    TEST=tast run <DUT> arc.MultiNetworkingConnectivity
         with interface up, down, and no connectivity
         (nocturne).
    
    Change-Id: I2a596aa640c789728e42313421e4c5f5fcf13a31

--- src/chromiumos/tast/local/bundles/cros/arc/multi_networking_connectivity.go
+++ src/chromiumos/tast/local/bundles/cros/arc/multi_networking_connectivity.go
@@ -16,6 +16,7 @@ import (
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/network"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
@@ -26,7 +27,7 @@ func init() {
 		Desc:         "Checks connectivity while multi-networking is enabled",
 		Contacts:     []string{"jasongustaman@google.com", "arc-eng@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"android", "chrome"},
+		SoftwareDeps: []string{"android_p", "chrome"},
 		Pre:          arc.Booted(),
 	})
 }
@@ -46,13 +47,28 @@ func MultiNetworkingConnectivity(ctx context.Context, s *testing.State) {
 	// Ensure that outbound networking works for each network interface inside ARC.
 	// For multinetwork, "lo" and "arc0" are not supposed to have outbound networking
 	// and as such skipped for the test.
+	// Interface that is not up should also be skipped.
 	for _, ifname := range ifnames {
 		if ifname == arc.Loopback || ifname == arc.ARC0 {
 			continue
 		}
+
+		if ifup, err := interfaceUp(ifname); err != nil {
+			s.Errorf("Failed checking if %s is up: %s", ifname, err)
+			continue
+		} else if !ifup {
+			continue
+		}
+
+		gateway, err := network.Gateway(ctx, ifname)
+		if err != nil {
+			s.Errorf("Failed to get gateway address for interface %s: %s", ifname, err)
+			continue
+		}
+
 		s.Log("Pinging using ", ifname)
 		if err := testing.Poll(ctx, func(ctx context.Context) error {
-			return arc.BootstrapCommand(ctx, "/system/bin/ping", "-c1", "-w1", "-I", ifname, "8.8.8.8").Run()
+			return arc.BootstrapCommand(ctx, "/system/bin/ping", "-c1", "-w1", "-I", ifname, gateway).Run()
 		}, &testing.PollOptions{Timeout: timeout}); err != nil {
 			s.Errorf("Failed outbound check for interface %s: %s", ifname, err)
 		}
@@ -92,7 +108,7 @@ func MultiNetworkingConnectivity(ctx context.Context, s *testing.State) {
 	}
 
 	// Get the Android (ARC) interfaces names and IPs.
-	out, err := s.PreValue().(arc.PreData).ARC.Command(ctx, "/system/bin/ip", "-o", "addr", "show", "scope", "global").Output()
+	out, err := s.PreValue().(arc.PreData).ARC.Command(ctx, "/system/bin/ip", "-o", "addr", "show", "scope", "global").Output(testexec.DumpLogOnError)
 	if err != nil {
 		s.Fatal("Failed to get Android interface list: ", err)
 	}
@@ -102,10 +118,7 @@ func MultiNetworkingConnectivity(ctx context.Context, s *testing.State) {
 	// "id" "interface_name" "protocol" "ip/netmask" ...
 	// e.g. "103: arc0    inet 100.115.92.2/30 ..."
 	// The following regex is used to extract interface name and IP
-	re, err := regexp.Compile(`^\d+:\s+(\S+)\s+\S+\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
-	if err != nil {
-		s.Fatal("Failed to compile regular expression: ", err)
-	}
+	re := regexp.MustCompile(`^\d+:\s+(\S+)\s+\S+\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
 
 	// Parse output of "ip -o addr show" to get interface name and ip.
 	for _, o := range strings.Split(strings.TrimSpace(string(out)), "\n") {
@@ -132,6 +145,14 @@ func MultiNetworkingConnectivity(ctx context.Context, s *testing.State) {
 		if ifc.arcIP == "" || ifname == arc.ARC0 {
 			continue
 		}
+
+		if ifup, err := interfaceUp(ifname); err != nil {
+			s.Errorf("Failed checking if %s is up: %s", ifname, err)
+			continue
+		} else if !ifup {
+			continue
+		}
+
 		ifname, ifc := ifname, ifc // https://golang.org/doc/faq#closures_and_goroutines
 		g.Go(func() error {
 			if err := checkNetInterface(watchCtx, ifname, ifc.arcIP, ifc.bridgeIP); err != nil {
@@ -189,4 +210,13 @@ func checkNetInterface(ctx context.Context, ifname, arcIP, bridgeIP string) erro
 			return nil
 		}
 	}
+}
+
+// interfaceUp returns true if the network interface is up.
+func interfaceUp(ifname string) (bool, error) {
+	iface, err := net.InterfaceByName(ifname)
+	if err != nil {
+		return false, err
+	}
+	return strings.Contains(iface.Flags.String(), "up"), nil
 }
