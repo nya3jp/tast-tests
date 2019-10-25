a92f4a69094aaa09cbb2141da6ce9d8507bc51b2
Miguel Casas <mcasas@chromium.org>
2019-10-25 00:15:17 -0400

    tast/video: decrease amount of seeks for Seek.* tests
    This CL extends the Seek.* parameters to define the total amount
    of seeks to be performed and reduces this number from 100 to 25,
    since last runs have shown that some small chromebooks have
    troubles performing so many and time out [1] (and it's unlikely
    that a device does e.g. 30 seeks and then chokes).
    
    [1] https://stainless.corp.google.com/search?view=list&first_date=2019-10-19&last_date=2019-10-25&test=%5Etast.video.Seek.*&build=R80-12621.0.0&status=WARN&status=FAIL&status=ERROR&status=NOT_RUN&exclude_cts=false&exclude_not_run=false&exclude_non_release=true&exclude_au=true&exclude_acts=true&exclude_retried=false&exclude_non_production=false
    
    BUG=chromium:1018045
    TEST=tast -verbose run papaya video.Seek.* on kohaku
    
    Change-Id: I399fc2b69a9566a77e18d147ba5b94bdd7f4aea3

--- src/chromiumos/tast/local/bundles/cros/video/seek.go
+++ src/chromiumos/tast/local/bundles/cros/video/seek.go
@@ -13,6 +13,12 @@ import (
 	"chromiumos/tast/testing"
 )
 
+// seekTest is used to describe the config used to run each Seek test.
+type seekTest struct {
+	filename string // File name to play back.
+	numSeeks int    // Amount of times to seek into the <video>.
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func: Seek,
@@ -29,27 +35,27 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		Params: []testing.Param{{
 			Name:      "h264",
-			Val:       "video_seek.mp4",
+			Val:       seekTest{filename: "video_seek.mp4", numSeeks: 25},
 			ExtraData: []string{"video_seek.mp4"},
 			// "chrome_internal" is needed because H.264 is a proprietary codec.
 			ExtraSoftwareDeps: []string{"chrome_internal"},
 		}, {
 			Name:      "vp8",
-			Val:       "video_seek.webm",
+			Val:       seekTest{filename: "video_seek.webm", numSeeks: 25},
 			ExtraData: []string{"video_seek.webm"},
 		}, {
 			Name:      "vp9",
-			Val:       "shaka_720.webm",
+			Val:       seekTest{filename: "shaka_720.webm", numSeeks: 25},
 			ExtraData: []string{"shaka_720.webm"},
 		}, {
 			Name:      "switch_h264",
-			Val:       "switch_1080p_720p.mp4",
+			Val:       seekTest{filename: "switch_1080p_720p.mp4", numSeeks: 25},
 			ExtraData: []string{"switch_1080p_720p.mp4"},
 			// "chrome_internal" is needed because H.264 is a proprietary codec.
 			ExtraSoftwareDeps: []string{"chrome_internal"},
 		}, {
 			Name:      "switch_vp8",
-			Val:       "frame_size_change.webm",
+			Val:       seekTest{filename: "frame_size_change.webm", numSeeks: 25},
 			ExtraData: []string{"frame_size_change.webm"},
 		}},
 	})
@@ -57,5 +63,6 @@ func init() {
 
 // Seek plays a file with Chrome and checks that it can safely be seeked into.
 func Seek(ctx context.Context, s *testing.State) {
-	play.TestSeek(ctx, s, s.PreValue().(*chrome.Chrome), s.Param().(string))
+	testOpt := s.Param().(seekTest)
+	play.TestSeek(ctx, s, s.PreValue().(*chrome.Chrome), testOpt.filename, testOpt.numSeeks)
 }
