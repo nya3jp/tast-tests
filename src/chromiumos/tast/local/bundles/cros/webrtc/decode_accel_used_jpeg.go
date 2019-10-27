f453d1de0446ff0fc8251d83e90f2b2f10d76b71
Miguel Casas <mcasas@chromium.org>
2019-10-27 13:39:28 -0400

    tast/webrtc: fix/simplify DecodeAccelUsedJPEG
    This CL simplifies DecodeAccelUsedJPEG to not use the code in
    loopback.html code, instead adding a tiny get_user_media.html
    and running the call to the promisified getUserMedia() in
    video/video.go (that will be renamed later to get_user_media.go
    to simplify review).
    
    The function checkError() in video.go is not used anymore for
    this test, so it's migrated to its only caller in perf.go.
    
    This test is notoriously flaky in small devices due to the
    large overhead of setting an RTCPeerConnection, with at least
    an encoder and a decoder; with this simplification the flakes
    are expected to be reduced.
    
    BUG=chromium:1018590
    
    TEST=tast -verbose run papaya webrtc.*AccelUsed* on kohaku
    
    Change-Id: I3f20ac2919c1ed39257936632c99e262c8f3d4be

--- src/chromiumos/tast/local/bundles/cros/webrtc/decode_accel_used_jpeg.go
+++ src/chromiumos/tast/local/bundles/cros/webrtc/decode_accel_used_jpeg.go
@@ -10,21 +10,24 @@ import (
 	"chromiumos/tast/local/bundles/cros/webrtc/video"
 	"chromiumos/tast/local/media/caps"
 	"chromiumos/tast/local/media/constants"
-	"chromiumos/tast/local/webrtc"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         DecodeAccelUsedJPEG,
-		Desc:         "Checks HW decoding used for MJPEG in WebRTC",
-		Contacts:     []string{"hiroh@chromium.org", "chromeos-video-eng@google.com"},
+		Func: DecodeAccelUsedJPEG,
+		Desc: "Checks HW decoding used for MJPEG in WebRTC",
+		Contacts: []string{
+			"mcasas@chromium.org", // Test author.
+			"chromeos-gfx-video@google.com",
+			"chromeos-video-eng@google.com",
+		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", caps.HWDecodeJPEG},
-		Data:         append(webrtc.LoopbackDataFiles(), "crowd720_25frames.mjpeg"),
+		Data:         []string{"get_user_media.html", "crowd720_25frames.mjpeg"},
 	})
 }
 
 func DecodeAccelUsedJPEG(ctx context.Context, s *testing.State) {
-	video.RunVideo(ctx, s, "crowd720_25frames.mjpeg", constants.RTCJPEGInitStatus, constants.RTCJPEGInitSuccess)
+	video.RunGetUserMedia(ctx, s, "get_user_media.html", "crowd720_25frames.mjpeg", constants.RTCJPEGInitStatus, constants.RTCJPEGInitSuccess)
 }
