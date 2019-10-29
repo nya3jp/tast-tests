6d3afe2845cb15873db111ad287e5067ea81a9f1
Shuhei Takahashi <nya@chromium.org>
2019-10-29 11:29:43 +0900

    tast-tests: Replace power.Reboot with power.RebootNew.
    The new reboot method turned out to be stable.
    
    BUG=chromium:1000505
    TEST=fast_build.sh -T
    TEST=tast run -checktestdeps=false betty power.Reboot
    
    Change-Id: I05c98b918c7bb0ea1932e35be6185fa363e89529

--- src/chromiumos/tast/remote/bundles/cros/power/reboot.go
+++ src/chromiumos/tast/remote/bundles/cros/power/reboot.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,9 +14,10 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         Reboot,
-		Desc:         "Verifies that system comes back after rebooting",
-		Contacts:     []string{"tast-owners@google.com"},
+		Desc:         "Verifies that system comes back after rebooting (with a new reboot method)",
+		Contacts:     []string{"nya@chromium.org", "tast-owners@google.com"},
 		SoftwareDeps: []string{"reboot"},
+		Attr:         []string{"group:mainline"},
 	})
 }
 
@@ -26,24 +27,7 @@ func Reboot(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to get DUT")
 	}
 
-	s.Log("Rebooting DUT")
-	// Run the reboot command in the background to avoid the DUT potentially going down before
-	// success is reported over the SSH connection. Redirect all I/O streams to ensure that the
-	// SSH exec request doesn't hang (see https://en.wikipedia.org/wiki/Nohup#Overcoming_hanging).
-	cmd := "nohup sh -c 'sleep 2; reboot' >/dev/null 2>&1 </dev/null &"
-	if err := d.Command("sh", "-c", cmd).Run(ctx); err != nil {
+	if err := d.Reboot(ctx); err != nil {
 		s.Fatal("Failed to reboot DUT: ", err)
 	}
-
-	s.Log("Waiting for DUT to become unreachable")
-	if err := d.WaitUnreachable(ctx); err != nil {
-		s.Fatal("Failed to wait for DUT to become unreachable: ", err)
-	}
-	s.Log("DUT became unreachable (as expected)")
-
-	s.Log("Reconnecting to DUT")
-	if err := d.WaitConnect(ctx); err != nil {
-		s.Fatal("Failed to reconnect to DUT: ", err)
-	}
-	s.Log("Reconnected to DUT")
 }
