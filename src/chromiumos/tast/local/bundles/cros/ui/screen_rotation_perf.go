7e3964795447254cbc777fd2408800afe47b9bc6
Sammie Quon <sammiequon@google.com>
2019-10-29 15:25:47 -0700

    tast-tests: Add ui.ScreenRotationPerf test.
    BUG=chromium:1007071
    TEST=manual
    
    Change-Id: I1f84793c27b012a34a8c11c77390be2424f4c658

---
+++ src/chromiumos/tast/local/bundles/cros/ui/screen_rotation_perf.go
@@ -0,0 +1,100 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/cdputil"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/media/cpu"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/local/ui"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ScreenRotationPerf,
+		Desc:         "Measures animation smoothness of screen rotation in tablet mode",
+		Contacts:     []string{"chromeos-wmp@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", "tablet_mode"},
+		Pre:          chrome.LoggedIn(),
+		Timeout:      2 * time.Minute,
+	})
+}
+
+func ScreenRotationPerf(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+	defer tconn.Close()
+
+	originalTabletMode, err := ash.TabletModeEnabled(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to obtain the tablet mode status: ", err)
+	}
+	defer ash.SetTabletModeEnabled(ctx, tconn, originalTabletMode)
+
+	// Enter tablet mode.
+	if err = ash.SetTabletModeEnabled(ctx, tconn, true); err != nil {
+		s.Fatal("Failed to enable tablet mode: ", err)
+	}
+
+	dispInfo, err := display.GetInternalInfo(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get internal display info: ", err)
+	}
+
+	pv := perf.NewValues()
+	currentWindows := 0
+	// Run the screen rotation in overview mode with 2 or 8 windows.
+	for _, windows := range []int{2, 8} {
+		for ; currentWindows < windows; currentWindows++ {
+			conn, err := cr.NewConn(ctx, ui.PerftestURL, cdputil.WithNewWindow())
+			if err != nil {
+				s.Fatal("Failed to open a new connection: ", err)
+			}
+			defer conn.Close()
+		}
+
+		if err = cpu.WaitUntilIdle(ctx); err != nil {
+			s.Fatal("Failed to wait for system UI to be stabilized: ", err)
+		}
+
+		if err = ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+			s.Fatal("Failed to enter into the overview mode: ", err)
+		}
+
+		for _, rotation := range []display.RotationAngle{display.Rotate90, display.Rotate180, display.Rotate270, display.Rotate0} {
+			if err := display.SetDisplayRotationSync(ctx, tconn, dispInfo.ID, rotation); err != nil {
+				s.Fatal("Failed to rotate display: ", err)
+			}
+		}
+
+		histName := "Ash.Rotation.AnimationSmoothness"
+		histogram, err := metrics.GetHistogram(ctx, cr, histName)
+		if err != nil {
+			s.Fatalf("Failed to get histogram %s: %v", histName, err)
+		}
+		pv.Set(perf.Metric{
+			Name:      histName,
+			Unit:      "ms",
+			Direction: perf.SmallerIsBetter,
+		}, histogram.Mean())
+	}
+
+	if err := pv.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
