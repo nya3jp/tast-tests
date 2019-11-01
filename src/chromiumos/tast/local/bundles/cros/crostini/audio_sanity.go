61b6b6a7b1109a21863e8af22bac87886a961c0a
Nicholas Hollingum <hollingum@google.com>
2019-11-01 15:40:49 +1100

    tast-tests: crostini: Add buster pre to AudioSanity, DisplayDensity
    We do this via test parameters since these tests were written before
    (while) tast parameters were being added. We went back to them and
    changed their implementation to use these new features.
    
    BUG=chromium:1020462
    TEST=tast run localhost:33333 crostini.DisplayDensity* \
                 crostini.AudioSanity*
    
    Change-Id: I94c9b8ae5cbec54327f429d657500d0dc7eb6ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1895091
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Tested-by: Fergus Dall <sidereal@google.com>

--- src/chromiumos/tast/local/bundles/cros/crostini/audio_sanity.go
+++ src/chromiumos/tast/local/bundles/cros/crostini/audio_sanity.go
@@ -2,21 +2,43 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package audiosanity provides methods to test the basic audio functionality
-// of a container, using alsa.
-package audiosanity
+package crostini
 
 import (
 	"context"
+	"time"
 
+	"chromiumos/tast/local/crostini"
 	"chromiumos/tast/local/testexec"
-	"chromiumos/tast/local/vm"
 	"chromiumos/tast/testing"
 )
 
-// RunTest tests the container's audio subsystem. Specifically it uses
-// alsa's aplay and arecord to list and use the audio devices.
-func RunTest(ctx context.Context, s *testing.State, cont *vm.Container) {
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     AudioSanity,
+		Desc:     "Runs a sanity test on the container's audio (through alsa) using a pre-built crostini image",
+		Contacts: []string{"paulhsia@chromium.org", "cros-containers-dev@google.com", "chromeos-audio-bugs@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Name:      "artifact",
+			Pre:       crostini.StartedByArtifact(),
+			ExtraData: []string{crostini.ImageArtifact},
+			Timeout:   7 * time.Minute,
+		}, {
+			Name:    "download",
+			Pre:     crostini.StartedByDownload(),
+			Timeout: 10 * time.Minute,
+		}, {
+			Name:    "download_buster",
+			Pre:     crostini.StartedByDownloadBuster(),
+			Timeout: 10 * time.Minute,
+		}},
+		SoftwareDeps: []string{"chrome", "vm_host"},
+	})
+}
+
+func AudioSanity(ctx context.Context, s *testing.State) {
+	cont := s.PreValue().(crostini.PreData).Container
 	s.Log("List alsa output devices")
 	if err := cont.Command(ctx, "aplay", "-l").Run(testexec.DumpLogOnError); err != nil {
 		s.Fatal("Failed to list alsa output devices: ", err)
