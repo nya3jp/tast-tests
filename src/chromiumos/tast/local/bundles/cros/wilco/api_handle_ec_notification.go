2983a98f5e602ca8e1ff279cbf62dfd747b7d4b1
Trent Begin <tbegin@google.com>
2019-11-04 12:00:42 -0700

    tast/wilco.ECVM: Add test to receive EC events inside wilco VM
    - Refactor common wilco ec code into new helper library
    - Add library function for receiving DPSL messages inside of the VM
    - Add test to receive EC events from DPSL inside of the VM
    
    BUG=b:143726722
    TEST=tast run ${DUT_IP} wilco.API*
    tast run ${DUT_IP} wilco.ECEvent
    
    Cq-Depend: chrome-internal:2099166
    Change-Id: I3e8270c9721bb7c7734c1553b2e4039c7589c88e

---
+++ src/chromiumos/tast/local/bundles/cros/wilco/api_handle_ec_notification.go
@@ -0,0 +1,59 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wilco
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/wilco/pre"
+	"chromiumos/tast/local/wilco"
+	"chromiumos/tast/testing"
+	dtcpb "chromiumos/wilco_dtc"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: APIHandleECNotification,
+		Desc: "Tests that the Wilco DTC VM receives EC events using the DPSL",
+		Contacts: []string{
+			"tbegin@chromium.org",       // Test author and Wilco DTC VM author.
+			"chromeos-wilco@google.com", // Possesses some more domain-specific knowledge.
+		},
+		SoftwareDeps: []string{"wilco"},
+		Timeout:      10 * time.Second,
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.WilcoDtcSupportdAPI,
+	})
+}
+
+func APIHandleECNotification(ctx context.Context, s *testing.State) {
+	const (
+		// Message type defined at http://issuetracker.google.com/139017129.
+		expectedRequestType = 19
+	)
+
+	rec, err := wilco.CreateDPSLMessageReceiver(ctx)
+	if err != nil {
+		s.Fatal("Unable to create DPSL Message Receiver")
+	}
+	defer rec.Stop()
+
+	if err := wilco.TriggerECEvent(); err != nil {
+		s.Fatal("Unable to trigger EC event: ", err)
+	}
+
+	s.Log("Waiting for EC Notification")
+	msg := dtcpb.HandleEcNotificationRequest{}
+	if err := rec.WaitForMessage(ctx, &msg); err != nil {
+		s.Fatal("Unable to receive EC response: ", err)
+	}
+	s.Log("Received EC Notification")
+
+	if msg.Type != expectedRequestType {
+		s.Fatalf("EC Notification Request is the wrong type. Got %v, expected %v",
+			msg.Type, expectedRequestType)
+	}
+}
