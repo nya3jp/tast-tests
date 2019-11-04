6744ea966d9ac7eee193fc50e8c3cfe13290e008
Victor-Gabriel Savu <vsavu@google.com>
2019-11-04 14:22:07 +0100

    tast-test: add test for GetDriveSystemData
    Add test for GetDriveSystemData. Depends on change in
    diagnostics_dpsl_test_requester.
    
    BUG=b:143065923
    TEST=tast run $IP wilco.API*
    
    Cq-Depend: chromium:1881127
    Change-Id: I9a6a45e0e3f7be261bdf81b42d53ed60c8c76e9d

---
+++ src/chromiumos/tast/local/bundles/cros/wilco/api_get_drive_system_data.go
@@ -0,0 +1,71 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wilco
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/local/bundles/cros/wilco/pre"
+	"chromiumos/tast/local/wilco"
+	"chromiumos/tast/testing"
+	dtcpb "chromiumos/wilco_dtc"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: APIGetDriveSystemData,
+		Desc: "Test sending GetDriveSystemData gRPC request from Wilco DTC VM to the Wilco DTC Support Daemon daemon",
+		Contacts: []string{
+			"vsavu@chromium.org",  // Test author
+			"pmoy@chromium.org",   // wilco_dtc_supportd author
+			"lamzin@chromium.org", // wilco_dtc_supportd maintainer
+			"chromeos-wilco@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"vm_host", "wilco"},
+		Pre:          pre.WilcoDtcSupportdAPI,
+	})
+}
+
+func APIGetDriveSystemData(ctx context.Context, s *testing.State) {
+	request := dtcpb.GetDriveSystemDataRequest{
+		Type: dtcpb.GetDriveSystemDataRequest_SMART_ATTRIBUTES,
+	}
+	response := dtcpb.GetDriveSystemDataResponse{}
+
+	if err := wilco.DPSLSendMessage(ctx, "GetDriveSystemData", &request, &response); err != nil {
+		s.Fatal("Unable to get drive data: ", err)
+	}
+
+	if response.Status != dtcpb.GetDriveSystemDataResponse_STATUS_OK {
+		s.Fatalf("Unexpected GetDriveSystemData response status; got %s, want STATUS_OK", response.Status)
+	}
+
+	if !strings.HasPrefix(string(response.Payload), "smartctl") {
+		s.Error("Payload is not smartctl output")
+	}
+
+	if !strings.Contains(string(response.Payload), "START OF SMART DATA SECTION") {
+		s.Error("Payload is not SMART data")
+	}
+
+	request = dtcpb.GetDriveSystemDataRequest{
+		Type: dtcpb.GetDriveSystemDataRequest_IDENTITY_ATTRIBUTES,
+	}
+	response = dtcpb.GetDriveSystemDataResponse{}
+
+	if err := wilco.DPSLSendMessage(ctx, "GetDriveSystemData", &request, &response); err != nil {
+		s.Fatal("Unable to get drive data: ", err)
+	}
+
+	if response.Status != dtcpb.GetDriveSystemDataResponse_STATUS_OK {
+		s.Fatalf("Unexpected GetDriveSystemData response status; got %s, want STATUS_OK", response.Status)
+	}
+
+	if !strings.HasPrefix(string(response.Payload), "NVME Identify Controller") {
+		s.Error("Payload is not nvme id-ctrl output")
+	}
+}
