9da2b029ef84943212d2484297f93fac7f3956c3
Miriam Zimmerman <mutexlox@google.com>
2019-11-04 16:28:51 -0800

    crash: Promote service_failure test to critical.
    There are only 3 failures on M80 builds in the last 7 days, all of which
    are attributable to infra failure:
    * One is a nil pointer dereference that many other tests shared in
    the same run
    * One is Chrome failing to start
    * One is an error "Failed to create output dir...read-only filesystem"
    
    BUG=chromium:983725
    TEST=Checked stainless; see above notes.
    
    Change-Id: Id3ed86ccabf2c61f849a7b2064f54e7bd8d95fa2

--- src/chromiumos/tast/local/bundles/cros/platform/service_failure.go
+++ src/chromiumos/tast/local/bundles/cros/platform/service_failure.go
@@ -69,7 +69,7 @@ func init() {
 		Func:         ServiceFailure,
 		Desc:         "Verify service failures are logged as expected",
 		Contacts:     []string{"mutexlox@google.com", "cros-monitoring-forensics@chromium.org"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome", "chrome_internal"},
 		Pre:          chrome.LoggedIn(),
 	})
