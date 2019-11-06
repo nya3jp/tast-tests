d99eb4611831c24e2ebccd5876d2ccbdc38bad87
Shuo-Peng Liao <deanliao@chromium.org>
2019-11-06 20:16:19 +0800

    tast-tests: Add debug logging on shill/manager.
    Add a flip on shill.Manager so that developer can enable verbose logging
    to make debugging easier.
    
    BUG=chromium:1021876
    TEST=On ethernet_static_ip.go and iw_scan.go under bundles/cros/network,
    add
      manager.Debug = true
    right after manager is created.
    tast run network.EthernetStaticIP network.IWScan
    to see verbose log from shill/manager.
    
    Change-Id: I46845a71e6bcdea6d35d500bef2cab2d70a98fd3

--- src/chromiumos/tast/local/shill/manager.go
+++ src/chromiumos/tast/local/shill/manager.go
@@ -6,6 +6,7 @@ package shill
 
 import (
 	"context"
+	"fmt"
 	"reflect"
 	"time"
 
@@ -14,6 +15,7 @@ import (
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/dbusutil"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/timing"
 )
 
 const (
@@ -30,9 +32,11 @@ const (
 )
 
 // Manager wraps a Manager D-Bus object in shill.
+// Users can set m.Debug = true to see verbose logging for developing/debugging purpose.
 type Manager struct {
 	dbusObject *DBusObject
 	props      *Properties
+	Debug      bool // Debug set to enable verbose logging. Default false.
 }
 
 // Technology is the type of a shill device's technology
@@ -61,7 +65,7 @@ func NewManager(ctx context.Context) (*Manager, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Manager{dbusObject: dbusObj, props: props}, nil
+	return &Manager{dbusObject: dbusObj, props: props, Debug: false}, nil
 }
 
 // Properties returns existing properties.
@@ -78,25 +82,39 @@ func (m *Manager) String() string {
 // GetProperties refreshes and returns properties.
 func (m *Manager) GetProperties(ctx context.Context) (*Properties, error) {
 	props, err := NewProperties(ctx, m.dbusObject)
-	if err != nil {
-		return nil, err
+	if err == nil {
+		m.props = props
 	}
-	m.props = props
-	return props, nil
+	// Note that we don't emit props here because it is too verbose.
+	m.logReturn(ctx, "GetProperties()", err)
+	return props, err
+}
+
+// findMatchingService is a wrapper to call m.findMatchingServiceInner with debug logging.
+func (m *Manager) findMatchingService(ctx context.Context, props map[string]interface{}, complete bool, name string) (dbus.ObjectPath, error) {
+	ctx, st := timing.Start(ctx, "manager."+name)
+	defer st.End()
+
+	p, err := m.findMatchingServiceInner(ctx, props, complete)
+	m.logReturn(ctx, name+"()", err, p)
+	return p, err
 }
 
 // FindMatchingService returns a service with matching properties.
+// Note that it also refreshes properties.
 func (m *Manager) FindMatchingService(ctx context.Context, props map[string]interface{}) (dbus.ObjectPath, error) {
-	return m.findMatchingServiceInner(ctx, props, false)
+	return m.findMatchingService(ctx, props, false, "FindMatchingService")
 }
 
 // FindMatchingAnyService returns any service including not visible with matching properties.
+// Note that it also refreshes properties.
 func (m *Manager) FindMatchingAnyService(ctx context.Context, props map[string]interface{}) (dbus.ObjectPath, error) {
-	return m.findMatchingServiceInner(ctx, props, true)
+	return m.findMatchingService(ctx, props, true, "FindMatchingAnyService")
 }
 
 // getServicePaths obtains a list of service paths of the manager.
 // If there's no service path, it'll wait until it is updated.
+// Note that it also refreshes properties.
 func (m *Manager) getServicePaths(ctx context.Context, complete bool) ([]dbus.ObjectPath, error) {
 	serviceListName := ManagerPropertyServices
 	if complete {
@@ -159,95 +177,121 @@ ForServicePaths:
 // WaitForServiceProperties polls FindMatchingService() for a service matching
 // the given properties.
 func (m *Manager) WaitForServiceProperties(ctx context.Context, props map[string]interface{}, timeout time.Duration) error {
+	ctx, st := timing.Start(ctx, "manager.WaitForServiceProperties")
+	defer st.End()
+
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
 		_, err := m.FindMatchingService(ctx, props)
 		return err
 	}, &testing.PollOptions{Timeout: timeout}); err != nil {
+		m.logReturn(ctx, "WaitForServiceProperties()", err)
 		return err
 	}
+	m.logReturn(ctx, "WaitForServiceProperties()", nil)
 	return nil
+}
+
+// getObjectPaths returns a list of dbus.ObjectPath of the given property.
+func (m *Manager) getObjectPaths(ctx context.Context, prop, method string) ([]dbus.ObjectPath, error) {
+	ps, err := m.props.GetObjectPaths(prop)
+	m.logReturn(ctx, method+"()", err, ps)
+	return ps, err
 }
 
 // GetProfiles returns a list of profiles.
 func (m *Manager) GetProfiles(ctx context.Context) ([]dbus.ObjectPath, error) {
-	return m.props.GetObjectPaths(ManagerPropertyProfiles)
+	return m.getObjectPaths(ctx, ManagerPropertyProfiles, "GetProfiles")
 }
 
 // GetDevices returns a list of devices.
 func (m *Manager) GetDevices(ctx context.Context) ([]dbus.ObjectPath, error) {
-	return m.props.GetObjectPaths(ManagerPropertyDevices)
+	return m.getObjectPaths(ctx, ManagerPropertyDevices, "GetDevices")
+}
+
+// call calls m.dbusObject.Call() and returns checked error.
+// Also performs debug logging.
+func (m *Manager) call(ctx context.Context, method string, args ...interface{}) error {
+	err := m.dbusObject.Call(ctx, method, args...).Err
+	m.logReturn(ctx, method+"()", err)
+	return err
 }
 
 // ConfigureService configures a service with the given properties.
 func (m *Manager) ConfigureService(ctx context.Context, props map[string]interface{}) error {
-	return m.dbusObject.Call(ctx, "ConfigureService", props).Err
+	return m.call(ctx, "ConfigureService", props)
+}
+
+// callStorePath calls m.dbusObject.Call() and returns ObjectPath.
+// It also performs debug logging.
+func (m *Manager) callStorePath(ctx context.Context, method string, args ...interface{}) (dbus.ObjectPath, error) {
+	var path dbus.ObjectPath
+	err := m.dbusObject.Call(ctx, method, args...).Store(&path)
+	if err != nil {
+		err = errors.Wrapf(err, "manager.%s() failed", method)
+	}
+	m.logReturn(ctx, method+"()", err, path)
+	return path, nil
 }
 
 // ConfigureServiceForProfile configures a service at the given profile path.
 func (m *Manager) ConfigureServiceForProfile(ctx context.Context, path dbus.ObjectPath, props map[string]interface{}) (dbus.ObjectPath, error) {
-	var service dbus.ObjectPath
-	if err := m.dbusObject.Call(ctx, "ConfigureServiceForProfile", path, props).Store(&service); err != nil {
-		return "", errors.Wrap(err, "failed to configure service")
-	}
-	return service, nil
+	return m.callStorePath(ctx, "ConfigureServiceForProfile", path, props)
 }
 
 // CreateProfile creates a profile.
 func (m *Manager) CreateProfile(ctx context.Context, name string) (dbus.ObjectPath, error) {
-	var profile dbus.ObjectPath
-	if err := m.dbusObject.Call(ctx, "CreateProfile", name).Store(&profile); err != nil {
-		return "", errors.Wrap(err, "failed to create profile")
-	}
-	return profile, nil
+	return m.callStorePath(ctx, "CreateProfile", name)
 }
 
 // PushProfile pushes a profile.
 func (m *Manager) PushProfile(ctx context.Context, name string) (dbus.ObjectPath, error) {
-	var profile dbus.ObjectPath
-	if err := m.dbusObject.Call(ctx, "PushProfile", name).Store(&profile); err != nil {
-		return "", errors.Wrap(err, "failed to create profile")
-	}
-	return profile, nil
+	return m.callStorePath(ctx, "PushProfile", name)
 }
 
 // RemoveProfile removes the profile with the given name.
 func (m *Manager) RemoveProfile(ctx context.Context, name string) error {
-	return m.dbusObject.Call(ctx, "RemoveProfile", name).Err
+	return m.call(ctx, "RemoveProfile", name)
 }
 
 // PopProfile pops the profile with the given name if it is on top of the stack.
 func (m *Manager) PopProfile(ctx context.Context, name string) error {
-	return m.dbusObject.Call(ctx, "PopProfile", name).Err
+	return m.call(ctx, "PopProfile", name)
 }
 
 // PopAllUserProfiles removes all user profiles from the stack of managed profiles leaving only default profiles.
 func (m *Manager) PopAllUserProfiles(ctx context.Context) error {
-	return m.dbusObject.Call(ctx, "PopAllUserProfiles").Err
+	return m.call(ctx, "PopAllUserProfiles")
 }
 
 // EnableTechnology enables a technology interface.
 func (m *Manager) EnableTechnology(ctx context.Context, technology Technology) error {
-	return m.dbusObject.Call(ctx, "EnableTechnology", string(technology)).Err
+	return m.call(ctx, "EnableTechnology", string(technology))
 }
 
 // DisableTechnology disables a technology interface.
 func (m *Manager) DisableTechnology(ctx context.Context, technology Technology) error {
-	return m.dbusObject.Call(ctx, "DisableTechnology", string(technology)).Err
+	return m.call(ctx, "DisableTechnology", string(technology))
 }
 
 // GetDevicesByTechnology returns list of Devices of the specified technology.
+// Note that it also refreshes properties.
 func (m *Manager) GetDevicesByTechnology(ctx context.Context, technology Technology) ([]*Device, error) {
-	var devs []*Device
+	tech := string(technology)
+	method := "GetDevicesByTechnology(" + tech + ")"
+
 	// Refresh properties first.
 	_, err := m.GetProperties(ctx)
 	if err != nil {
+		m.logReturn(ctx, method, err)
 		return nil, err
 	}
 	devPaths, err := m.GetDevices(ctx)
 	if err != nil {
+		m.logReturn(ctx, method, err)
 		return nil, err
 	}
 
+	var devs []*Device
 	for _, path := range devPaths {
 		dev, err := NewDevice(ctx, path)
 		// It is forgivable as a device may go down anytime.
@@ -258,10 +302,24 @@ func (m *Manager) GetDevicesByTechnology(ctx context.Context, technology Technol
 		if devType, err := dev.Properties().GetString(DevicePropertyType); err != nil {
 			testing.ContextLogf(ctx, "Error getting the type of the device %q: %v", path, err)
 			continue
-		} else if devType != string(technology) {
+		} else if devType != tech {
 			continue
 		}
 		devs = append(devs, dev)
 	}
+	m.logReturn(ctx, method, nil, devs)
 	return devs, nil
+}
+
+// logReturn logs function return status if m.Debug is set.
+func (m *Manager) logReturn(ctx context.Context, method string, err error, rets ...interface{}) {
+	if m.Debug {
+		if err != nil {
+			testing.ContextLogf(ctx, "manager.%s failed: %s", method, err)
+		} else if len(rets) > 0 {
+			testing.ContextLogf(ctx, "manager.%s returns %s", method, fmt.Sprint(rets...))
+		} else {
+			testing.ContextLogf(ctx, "manager.%s success", method)
+		}
+	}
 }
