976b5bf4bac8c7bccfbf8385020624d87e0e84da
Ahmed Fakhry <afakhry@google.com>
2019-11-05 19:07:49 -0800

    tast-test: Add ui.DesksAnimationPerf
    This CL adds a new test to measure the smoothness of the desk-activation
    and desk-removal animations.
    
    BUG=chromium:1016994
    TEST=tast -verbose run <IP> ui.DesksAnimationPerf
    
    Change-Id: Ie80af9f7f99eaf0f5f7812c5970a848a91fe710e

---
+++ src/chromiumos/tast/local/bundles/cros/ui/desks_animation_perf.go
@@ -0,0 +1,70 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DesksAnimationPerf,
+		Desc:         "Measures the smoothness of the desk-activation and removal animations",
+		Contacts:     []string{"afakhry@chromium.org", "chromeos-wmp@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+		Timeout:      3 * time.Minute,
+	})
+}
+
+func DesksAnimationPerf(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+	defer tconn.Close()
+
+	// Create a new desk other than the default desk, activate it, then remove it.
+	if err = ash.CreateNewDesk(ctx, tconn); err != nil {
+		s.Fatal("Failed to create a new desk: ", err)
+	}
+	if err = ash.ActivateDeskAtIndex(ctx, tconn, 1); err != nil {
+		s.Fatal("Failed to activate the second desk: ", err)
+	}
+	if err = ash.RemoveActiveDesk(ctx, tconn); err != nil {
+		s.Fatal("Failed to remove the active desk: ", err)
+	}
+
+	pv := perf.NewValues()
+	for _, histName := range []string{
+		"Ash.Desks.AnimationSmoothness.DeskActivation",
+		"Ash.Desks.AnimationSmoothness.DeskRemoval",
+	} {
+		histogram, err := metrics.GetHistogram(ctx, cr, histName)
+		if err != nil {
+			s.Fatalf("Failed to get histogram %s: %v", histName, err)
+		}
+
+		pv.Set(perf.Metric{
+			Name:      histName,
+			Unit:      "percent",
+			Direction: perf.BiggerIsBetter,
+		}, histogram.Mean())
+	}
+
+	if err := pv.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
