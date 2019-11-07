ab915ec3df1aa05772040c20b5fa3e323a1a467f
Jorge Lucangeli Obes <jorgelo@chromium.org>
2019-11-07 09:10:00 -0500

    tast-tests: Add cryptohome.GuestSession.
    We'll be changing how Guest sessions (initially, then all sessions) are
    mounted, so add a test that we can use during development. Once the CL
    below lands, we'll move this to 'informational'.
    
    BUG=chromium:985492
    TEST=Passes with and without https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1895909
    
    Change-Id: I42fa61dea86229a9a910a474d289b19cb24c28fa

---
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/guest_session.go
@@ -0,0 +1,54 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: GuestSession,
+		Desc: "Ensures that cryptohome correctly mounts guest sessions",
+		Contacts: []string{
+			"jorgelo@chromium.org",
+			"chromeos-security@google.com",
+		},
+		// The initial objective of this test is to aid ongoing development.
+		Attr: []string{"disabled"},
+	})
+}
+
+func GuestSession(ctx context.Context, s *testing.State) {
+	if err := cryptohome.MountGuest(ctx); err != nil {
+		s.Fatal("Failed to mount guest: ", err)
+	}
+	defer cryptohome.UnmountVault(ctx, cryptohome.GuestUser)
+
+	// Use a shortened context for subprocesses.
+	shortCtx, shortCancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer shortCancel()
+
+	// Get list of mounts.
+	cmd := testexec.CommandContext(shortCtx, "mount")
+	output, err := cmd.Output()
+	if err != nil {
+		s.Fatal("Failed to get list of mounts: ", err)
+	}
+
+	// Check that an ephemeral mount exists.
+	mounts := string(output)
+	const ephemeralMountPoint string = "/run/cryptohome/ephemeral_mount"
+	if !strings.Contains(mounts, ephemeralMountPoint) {
+		s.Fatalf("Could not find %q in mounts", ephemeralMountPoint)
+	}
+}
