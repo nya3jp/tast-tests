4f6bd70202be2886da0ba31b406be7650ee59065
Derek Basehore <dbasehore@chromium.org>
2019-11-07 00:56:29 +0000

    Revert "Stop anomaly detector from dropping reports."
    This reverts commit c927d31eb160c0a2fbe55aff73cc99a391f758e0.
    
    Reason for revert: The variable err's declaration was moved into an if statement in a post R79 branch change, but it wasn't cherry-picked back it seems.
    
    Original change's description:
    > Stop anomaly detector from dropping reports.
    > 
    > The linked change limits the number of service failures anomaly_detector
    > will report. This passes a flag to ignore that for tests.
    > 
    > BUG=chromium:1017491
    > TEST=tast -verbose run $dut platform.ServiceFailure
    > 
    > Cq-Depend: chromium:1898043
    > Change-Id: I8d0c773dd97126309cac0ff0c57ccf95881cbb9e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1881747
    > Tested-by: Miriam Zimmerman <mutexlox@chromium.org>
    > Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    > Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    > Auto-Submit: Miriam Zimmerman <mutexlox@chromium.org>
    > Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    > (cherry picked from commit 0886e6037c638a63189474494bc5de360004521f)
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1898246
    > Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    
    Bug: chromium:1017491
    Change-Id: I4ac01ba53dbb9dfe2d67a9ce64ffd48b751231d5

--- src/chromiumos/tast/local/bundles/cros/platform/service_failure.go
+++ src/chromiumos/tast/local/bundles/cros/platform/service_failure.go
@@ -85,9 +85,6 @@ func ServiceFailure(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to set consent: ", err)
 	}
 
-	// Restart anomaly detector to clear its --testonly-send-all flag at the end of execution.
-	defer localCrash.RestartAnomalyDetector(ctx)
-
 	for _, tt := range testParams {
 		// TODO(https://crbug.com/1007138): Avoid repetition of the tt.name parameter.
 		failingServiceName := tt.servicePrefix + "failing-service"
@@ -99,7 +96,7 @@ func ServiceFailure(ctx context.Context, s *testing.State) {
 
 		// Restart anomaly detector to clear its cache of recently seen service
 		// failures and ensure this one is logged.
-		if err := localCrash.RestartAnomalyDetectorWithSendAll(ctx, true); err != nil {
+		if err := localCrash.RestartAnomalyDetector(ctx); err != nil {
 			s.Fatalf("%s: failed to restart anomaly detector: %v", tt.name, err)
 		}
 
