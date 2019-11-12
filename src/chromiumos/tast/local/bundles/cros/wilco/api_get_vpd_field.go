9c308b03b6256732b47a5c31081c7eafb69ce6a2
Oleh Lamzin <lamzin@google.com>
2019-11-12 16:44:44 +0100

    tast-test: extend APIGetVPDField VPD fields list
    Extend APIGetVPDField VPD fields list for test since extending VPD
    fields in gRPC interface in crrev.com/1859422.
    
    BUG=b:140881996
    TEST=run updated test on device
    
    Change-Id: I2b160bfe96a370ce5aa07c2e37979ae96bd9c981

--- src/chromiumos/tast/local/bundles/cros/wilco/api_get_vpd_field.go
+++ src/chromiumos/tast/local/bundles/cros/wilco/api_get_vpd_field.go
@@ -9,19 +9,12 @@ import (
 	"io/ioutil"
 	"path"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/wilco/pre"
 	"chromiumos/tast/local/wilco"
 	"chromiumos/tast/testing"
 	dtcpb "chromiumos/wilco_dtc"
 )
-
-// getVPDFieldDataParam is the parameter to the APIGetVPDField test.
-type getVPDFieldDataParam struct {
-	// typeField is sent as the request type to GetVpdField.
-	typeField dtcpb.GetVpdFieldRequest_VpdField
-	// fileName is the name of the file in /sys/firmware/vpd/ro/ to compare the returned value to.
-	fileName string
-}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -36,38 +29,94 @@ func init() {
 		Attr:         []string{"informational"},
 		SoftwareDeps: []string{"vm_host", "wilco"},
 		Pre:          pre.WilcoDtcSupportdAPI,
-		Params: []testing.Param{{
-			Name: "serial_number",
-			Val: getVPDFieldDataParam{
-				typeField: dtcpb.GetVpdFieldRequest_FIELD_SERIAL_NUMBER,
-				fileName:  "serial_number",
-			},
-		}},
 	})
 }
 
 func APIGetVPDField(ctx context.Context, s *testing.State) {
-	param := s.Param().(getVPDFieldDataParam)
-
-	request := dtcpb.GetVpdFieldRequest{
-		VpdField: param.typeField,
-	}
-	response := dtcpb.GetVpdFieldResponse{}
-
-	if err := wilco.DPSLSendMessage(ctx, "GetVpdField", &request, &response); err != nil {
-		s.Fatal("Unable to get VPD field: ", err)
-	}
-
-	if response.Status != dtcpb.GetVpdFieldResponse_STATUS_OK {
-		s.Fatalf("Unexpected status response; got %s, want STATUS_OK", response.Status)
+	getVpdField := func(vpd_field string) (string, error) {
+		content, err := ioutil.ReadFile(path.Join("/sys/firmware/vpd/ro/", vpd_field))
+		if err == nil {
+			return string(content), nil
+		}
+		content, err = ioutil.ReadFile(path.Join("/sys/firmware/vpd/rw/", vpd_field))
+		if err == nil {
+			return string(content), nil
+		}
+		return "", errors.New("vpd field does not exist")
 	}
 
-	fileContent, err := ioutil.ReadFile(path.Join("/sys/firmware/vpd/ro/", param.fileName))
-	if err != nil {
-		s.Fatalf("Unable to read VPD file %s: %v", param.fileName, err)
+	// Try to read VPD field that is required for all boards.
+	// If it fails, then something is broken with VPD and/or related VPD driver.
+	if _, err := getVpdField("serial_number"); err != nil {
+		s.Fatal("Unable to read serial number VPD field: ", err)
 	}
 
-	if expectedData := string(fileContent); response.VpdFieldValue != expectedData {
-		s.Fatalf("Unexpected VPD field value; got %s, want %s", response.VpdFieldValue, expectedData)
+	for _, tc := range []struct {
+		// grpcVpdField is sent as the request type to GetVpdField.
+		grpcVpdField dtcpb.GetVpdFieldRequest_VpdField
+		// vpdField is the name of VPD field.
+		vpdField string
+	}{
+		{
+			grpcVpdField: dtcpb.GetVpdFieldRequest_FIELD_ACTIVATE_DATE,
+			vpdField:     "ActivateDate",
+		},
+		{
+			grpcVpdField: dtcpb.GetVpdFieldRequest_FIELD_ASSET_ID,
+			vpdField:     "asset_id",
+		},
+		{
+			grpcVpdField: dtcpb.GetVpdFieldRequest_FIELD_MANUFACTURE_DATE,
+			vpdField:     "mfg_date",
+		},
+		{
+			grpcVpdField: dtcpb.GetVpdFieldRequest_FIELD_MODEL_NAME,
+			vpdField:     "model_name",
+		},
+		{
+			grpcVpdField: dtcpb.GetVpdFieldRequest_FIELD_SERIAL_NUMBER,
+			vpdField:     "serial_number",
+		},
+		{
+			grpcVpdField: dtcpb.GetVpdFieldRequest_FIELD_SKU_NUMBER,
+			vpdField:     "sku_number",
+		},
+		{
+			grpcVpdField: dtcpb.GetVpdFieldRequest_FIELD_SYSTEM_ID,
+			vpdField:     "system_id",
+		},
+		{
+			grpcVpdField: dtcpb.GetVpdFieldRequest_FIELD_UUID_ID,
+			vpdField:     "uuid_id",
+		},
+	} {
+		s.Logf("Running gRPC API for getting %s VPD field", tc.vpdField)
+
+		expectedValue, err := getVpdField(tc.vpdField)
+		if err != nil {
+			s.Logf("Unable to get %s VPD field: %v", tc.vpdField, err)
+			continue
+		}
+
+		request := dtcpb.GetVpdFieldRequest{
+			VpdField: tc.grpcVpdField,
+		}
+		response := dtcpb.GetVpdFieldResponse{}
+
+		if err := wilco.DPSLSendMessage(ctx, "GetVpdField", &request, &response); err != nil {
+			s.Errorf("Unable to perform gRPC request to get %s VPD field: %v", tc.vpdField, err)
+			continue
+		}
+
+		if response.Status != dtcpb.GetVpdFieldResponse_STATUS_OK {
+			s.Errorf("Unexpected status response; got %s, want STATUS_OK", response.Status)
+			continue
+		}
+
+		if response.VpdFieldValue != expectedValue {
+			s.Errorf("Unexpected VPD field value; got %s, want %s", response.VpdFieldValue, expectedValue)
+			continue
+		}
 	}
+
 }
