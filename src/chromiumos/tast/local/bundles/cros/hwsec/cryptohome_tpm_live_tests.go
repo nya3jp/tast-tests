70018f95630543d1170d1a52a129a38d90470e68
Wei-Cheng Xiao <garryxiao@chromium.org>
2019-11-12 09:52:19 +0800

    DRAFT tast-test: soft-clear tpm utils + cryptohome tpm live test
    This CL provides utils for soft-clearing the TPM and demonstrates the utils by
    porting the autotest server test platform_CryptohomeTpmLiveTestServer to Tast
    as a local test.
    
    (DO NOT SUBMIT)
    This CL is a draft and not correctly formatted yet (e.g., with gofmt). The way
    CryptohomeTpmLiveTests takes TPM ownership and waits for TPM to be ready is also
    subjected to be replaced with hwsec utils in other pending CLs like
    chromium:1826587.
    
    Cq-Depend: chromium:1928008
    Change-Id: I68115ff833a2276cb33ee9bc2ca408e4be756e8b

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/cryptohome_tpm_live_tests.go
@@ -0,0 +1,45 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/hwsec"
+	"chromiumos/tast/local/ready"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CryptohomeTpmLiveTests,
+		Desc: "Runs cryptohome's TPM live tests, which test TPM keys, PCR, and NVRAM functionality.",
+		Contacts: []string{
+			"cros-hwsec@chromium.org",
+			"garryxiao@chromium.org",
+		},
+		SoftwareDeps: []string{"tpm2"},
+		Attr:         []string{"group:mainline", "informational"},
+		Timeout: 			10 * time.Minute,
+	})
+}
+
+func CryptohomeTpmLiveTests(ctx context.Context, s *testing.State) {
+	if errs := hwsec.ResetTpmAndSystemStates(ctx); len(errs) > 0 {
+		s.Fatal("Failed to reset TPM or system states: ", errs)
+	}
+
+	if err := ready.Wait(ctx, func(msg string) { s.Log(msg) }); err != nil {
+		s.Fatal("Failed to wait for system to be ready: ", err)
+	}
+
+	if err := testexec.CommandContext(
+		ctx, "cryptohome-tpm-live-test", "--tpm2_use_system_owner_password").Run(); err != nil {
+			s.Fatal("TPM live test failed: ", err)
+	}
+}
+
