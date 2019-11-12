ba626389f5d67ff930536b28efcc177d3a2ab736
Mitsuru Oshima <oshima@chromium.org>
2019-11-12 09:11:18 -0800

    Add Launcher Perf Test
    Bug: crbug.com/1007056
    Change-Id: I0a458f98d212d3f4c95f8cfdd8d753c1f4e22e7b

---
+++ src/chromiumos/tast/local/chrome/ash/launcher.go
@@ -0,0 +1,87 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ash
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+)
+
+// Launcher state as defined in
+// https://cs.chromium.org/chromium/src/ash/public/cpp/app_list/app_list_types.h
+type LauncherState string
+const (
+	Peeking           LauncherState = "Peeking"
+	FullscreenAllApps LauncherState = "FullscreenAllApps"
+	FullscreenSearch  LauncherState = "FullscreenSearch"
+	Half              LauncherState = "Half"
+	Closed            LauncherState = "Closed"
+)
+
+// Accelerator key used to trigger launcher state change.
+type Accelerator string
+const (
+	AccelSearch      Accelerator = "{keyCode: 'search', shift: false, control: false, alt: false, search: false, pressed: true}"
+	AccelShiftSearch Accelerator = "{keyCode: 'search', shift: true, control: false, alt: false, search: false, pressed: true}"
+)
+
+// Wait until the launcher state becomes |state|.
+func WaitForLauncherState(ctx context.Context, tconn *chrome.Conn, state LauncherState) error {
+	expr := fmt.Sprintf(
+		`new Promise(function(resolve, reject) {
+                   chrome.autotestPrivate.waitForLauncherState(
+                     '%s',
+                     function() {
+                       resolve(true);
+                     });
+                 })`, state)
+
+	finished := false
+	if err := tconn.EvalPromise(ctx, expr, &finished); err != nil {
+		return errors.Wrap(err, "failed to evaluate promise")
+	}
+	if !finished {
+		return errors.New("error")
+	}
+	return nil
+}
+
+// Wait until the launcher state becomes |state|.
+func TriggerLauncherStateChangeAndWait(ctx context.Context, tconn *chrome.Conn, accel Accelerator, state LauncherState) error {
+	expr := fmt.Sprintf(
+		`new Promise(function(resolve, reject) {
+                   var acceleratorKey=%s;
+                   chrome.autotestPrivate.activateAccelerator(
+                     acceleratorKey,
+                     function(ignored) {
+                       acceleratorKey.pressed = false;
+                       chrome.autotestPrivate.activateAccelerator(
+                         acceleratorKey,
+                         function(success) {
+                           if (!success) {
+                             reject(new Error(chrome.runtime.lastError.message));
+                           } else {
+                             chrome.autotestPrivate.waitForLauncherState(
+                               '%s',
+                               function() {
+                                 resolve(true);
+                               });
+                           }
+                         });
+                     });
+                })`, accel, state)
+
+	finished := false
+	if err := tconn.EvalPromise(ctx, expr, &finished); err != nil {
+		return errors.Wrap(err, "failed to evaluate promise")
+	}
+	if !finished {
+		return errors.New("error")
+	}
+	return nil
+}
