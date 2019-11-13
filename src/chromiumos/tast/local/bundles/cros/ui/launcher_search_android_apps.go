72a78edfb0ec70bf3d4d2c7903e3801d3b48d18b
Brendan Hansknecht <bhansknecht@chromium.org>
2019-11-13 12:04:42 -0800

    tast-test: Launch apps through Launcher
    Created tests for launching apps of various types through the launcher.
    Since it is testing launcher functionality, it does not simply call
    app.Launch(). Currently, launching a crostini app pops up a message to
    install crostini even is it is already installed. This issue will be
    looked into in another CL.
    
    Also adds a ui/launcher package for controlling the luancher directly
    through the UI.
    
    BUG=chromium:982527
    TEST=tast run <eve> ui.LauncherSearch*
    
    Depends-On: chromium-review/1849808
    Change-Id: I16ed3cd2315d5b94b839fe325ed0d02fb9e57bbd

---
+++ src/chromiumos/tast/local/bundles/cros/ui/launcher_search_android_apps.go
@@ -0,0 +1,59 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui/launcher"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LauncherSearchAndroidApps,
+		Desc: "Launches an app of various types through the launcher",
+		Contacts: []string{
+			"bhansknecht@chromium.org",
+			"dhaddock@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      5 * time.Minute,
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               arc.Booted(),
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               arc.VMBooted(),
+		}},
+	})
+}
+
+func LauncherSearchAndroidApps(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(arc.PreData).Chrome
+	a := s.PreValue().(arc.PreData).ARC
+	if err := a.WaitIntentHelper(ctx); err != nil {
+		s.Fatal("Failed to wait for ARC Intent Helper: ", err)
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	if err := launcher.SearchAndLaunch(ctx, tconn, apps.PlayStore.Name); err != nil {
+		s.Fatal("Failed to launch Play Store: ", err)
+	}
+
+	if err := ash.WaitForApp(ctx, tconn, apps.PlayStore.ID); err != nil {
+		s.Fatal("Failed to wait for Play Store: ", err)
+	}
+}
---
+++ src/chromiumos/tast/local/bundles/cros/ui/launcher_search_native_apps.go
@@ -0,0 +1,45 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui/launcher"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LauncherSearchNativeApps,
+		Desc: "Launches an app of various types through the launcher",
+		Contacts: []string{
+			"bhansknecht@chromium.org",
+			"dhaddock@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func LauncherSearchNativeApps(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	if err := launcher.SearchAndLaunch(ctx, tconn, apps.Settings.Name); err != nil {
+		s.Fatal("Failed to launch Settings: ", err)
+	}
+
+	if err := ash.WaitForApp(ctx, tconn, apps.Settings.ID); err != nil {
+		s.Fatal("Failed to wait for Settings: ", err)
+	}
+}
