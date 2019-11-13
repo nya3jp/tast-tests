6f386a1961793a03a3e79cf2403a7e816851e91f
Brendan Hansknecht <bhansknecht@chromium.org>
2019-11-13 12:04:42 -0800

    tast-test: Launch apps through Launcher
    Created tests for launching apps of various types through the launcher.
    Since it is testing launcher functionality, it does not simply call
    app.Launch(). Currently, launching a crostini app pops up a message to
    install crostini even is it is already installed. This issue will be
    looked into in another CL.
    
    Also adds a ui/launcher package for controlling the launcher directly
    through the UI.
    
    BUG=chromium:982527
    TEST=tast run <eve> ui.LauncherSearch*
    
    Depends-On: chromium-review/1849808
    Change-Id: I16ed3cd2315d5b94b839fe325ed0d02fb9e57bbd

---
+++ src/chromiumos/tast/local/chrome/ui/launcher/launcher.go
@@ -0,0 +1,66 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package launcher is used for controlling the launcher directly through the UI.
+package launcher
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/input"
+)
+
+// SearchAndLaunch searches a query in the launcher and executes it.
+func SearchAndLaunch(ctx context.Context, tconn *chrome.Conn, query string) error {
+	const defaultTimeout = 15 * time.Second
+	root, err := ui.Root(ctx, tconn)
+	if err != nil {
+		return err
+	}
+	defer root.Release(ctx)
+
+	// Open the launcher.
+	params := ui.FindParams{
+		Name: "Launcher",
+		Role: ui.RoleTypeButton,
+	}
+	launcherButton, err := root.DescendantWithTimeout(ctx, params, defaultTimeout)
+	if err != nil {
+		return errors.Wrap(err, "failed to find launcher button")
+	}
+	defer launcherButton.Release(ctx)
+	if err := launcherButton.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "failed to click launcher button")
+	}
+
+	// Click the search box.
+	searchBox, err := root.DescendantWithTimeout(ctx, ui.FindParams{ClassName: "SearchBoxView"}, defaultTimeout)
+	if err != nil {
+		return errors.Wrap(err, "failed to wait for launcher searchbox")
+	}
+	defer searchBox.Release(ctx)
+	if err := searchBox.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "failed to click launcher searchbox")
+	}
+
+	// Setup keyboard.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get keyboard")
+	}
+	defer kb.Close()
+
+	// Search for the app.
+	if err := kb.Type(ctx, query); err != nil {
+		return errors.Wrapf(err, "failed to type %q", query)
+	}
+	if err := kb.Accel(ctx, "Enter"); err != nil {
+		return errors.Wrap(err, "failed to type enter key")
+	}
+	return nil
+}
