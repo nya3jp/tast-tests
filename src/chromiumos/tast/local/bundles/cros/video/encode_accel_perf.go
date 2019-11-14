854d6e13f21c30bd33980bc09b7fa89d0e1c1884
David Staessens <dstaessens@chromium.org>
2019-11-14 09:35:48 +0900

    video.EncodeAccelPerf: Parameterize video encoder h264 perf tests.
    This CL combines the binary video encode accelerator H264 performance tests
    based on the video_encode_accelerator_unittest binary into a single testcase.
    
    Using parameterized tests causes the tests to have slightly different names:
    EncodeAccelPerfH264192PI420      -> EncodeAccelPerf.h264_192P_i420
    EncodeAccelPerfH264360PI420      -> EncodeAccelPerf.h264_360p_i420
    EncodeAccelPerfH264720PI420Tulip -> EncodeAccelPerf.h264_720p_i420_tulip
    EncodeAccelPerfH264720PI420Vidyo -> EncodeAccelPerf.h264_720p_i420_vidyo
    EncodeAccelPerfH2641080PI420     -> EncodeAccelPerf.h264_1080p_i420
    EncodeAccelPerfH2642160PI420     -> EncodeAccelPerf.h264_2160p_i420
    
    TEST=video.EncodeAccelPerf.* on hana
    
    BUG=b:139327919
    
    Change-Id: I4808f25becbaf0a48c3758bda05a5b9988c394ab

---
+++ src/chromiumos/tast/local/bundles/cros/video/encode_accel_perf.go
@@ -0,0 +1,92 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/video/encode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/videotype"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         EncodeAccelPerf,
+		Desc:         "Measures hardware video encode performance by running the video_encode_accelerator_unittest binary",
+		Contacts:     []string{"johnylin@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome"},
+		// Default timeout (i.e. 2 minutes) is not enough.
+		Timeout: 10 * time.Minute,
+		Params: []testing.Param{{
+			Name: "h264_192p_i420",
+			Val: encode.TestOptions{
+				Profile:     videotype.H264Prof,
+				Params:      encode.Bear192P,
+				PixelFormat: videotype.I420,
+				InputMode:   encode.SharedMemory,
+			},
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264},
+			ExtraData:         []string{encode.Bear192P.Name},
+		}, {
+			Name: "h264_360p_i420",
+			Val: encode.TestOptions{
+				Profile:     videotype.H264Prof,
+				Params:      encode.Tulip360P,
+				PixelFormat: videotype.I420,
+				InputMode:   encode.SharedMemory,
+			},
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264},
+			ExtraData:         []string{encode.Tulip360P.Name},
+		}, {
+			Name: "h264_720p_i420_tulip",
+			Val: encode.TestOptions{
+				Profile:     videotype.H264Prof,
+				Params:      encode.Tulip720P,
+				PixelFormat: videotype.I420,
+				InputMode:   encode.SharedMemory,
+			},
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264},
+			ExtraData:         []string{encode.Tulip720P.Name},
+		}, {
+			Name: "h264_720p_i420_vidyo",
+			Val: encode.TestOptions{
+				Profile:     videotype.H264Prof,
+				Params:      encode.Vidyo720P,
+				PixelFormat: videotype.I420,
+				InputMode:   encode.SharedMemory,
+			},
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264},
+			ExtraData:         []string{encode.Vidyo720P.Name},
+		}, {
+			Name: "h264_1080p_i420",
+			Val: encode.TestOptions{
+				Profile:     videotype.H264Prof,
+				Params:      encode.Crowd1080P,
+				PixelFormat: videotype.I420,
+				InputMode:   encode.SharedMemory,
+			},
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264},
+			ExtraData:         []string{encode.Crowd1080P.Name},
+		}, {
+			Name: "h264_2160p_i420",
+			Val: encode.TestOptions{
+				Profile:     videotype.H264Prof,
+				Params:      encode.Crowd2160P,
+				PixelFormat: videotype.I420,
+				InputMode:   encode.SharedMemory,
+			},
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264_4K},
+			ExtraData:         []string{encode.Crowd2160P.Name},
+		}},
+	})
+}
+
+func EncodeAccelPerf(ctx context.Context, s *testing.State) {
+	encode.RunAccelVideoPerfTest(ctx, s, s.Param().(encode.TestOptions))
+}
