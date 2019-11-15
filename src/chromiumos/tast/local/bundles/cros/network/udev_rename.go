232090d0e193d55a67fefc5bd4a26b7896502474
Shuo-Peng Liao <deanliao@chromium.org>
2019-11-15 08:05:19 +0800

    tast-tests: Disable network.UdevRename as it would affect other tests.
    network.UdevRename may crash device (on elm and hana) or make WiFi
    interface unable to spin up (on veyron_mickey, veyron_minnie and
    veyron_speedy). In those cases, other tests rely on WiFi interface would
    fail. (See crbug.com/1007252). So before UdevRename issue is fixed, we
    should disable it from running in the lab.
    
    BUG=chromium:1015239
    TEST=tast run $DUT
    
    Change-Id: Id78da060330bb037fea368f883fbdef3a2195b18

--- src/chromiumos/tast/local/bundles/cros/network/udev_rename.go
+++ src/chromiumos/tast/local/bundles/cros/network/udev_rename.go
@@ -26,7 +26,7 @@ func init() {
 		Func:     UdevRename,
 		Desc:     "Verifies that network interfaces remain intact after udev restart and WiFi driver rebind",
 		Contacts: []string{"yenlinlai@google.com", "chromeos-kernel-wifi@google.com"},
-		Attr:     []string{"group:mainline", "informational"},
+		Attr:     []string{"group:mainline", "disabled"},
 	})
 }
 
