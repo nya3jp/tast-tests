87ca026a289936b2e8fe2352bdc07560533d15a1
Stephen Barber <smbarber@chromium.org>
2019-11-15 14:17:27 -0800

    vm.StartCrosvm: stabilize test and remove use of imageloader
    The vast majority of failures with vm.StartCrosvm have been either due to
    timeouts (we don't boot as fast as we used to) or because of imageloader
    failing to download the cros-termina component.
    
    Update timeouts where appropriate to a reasonably conservative value. Switch
    the test to use external data files, which are quite reliable, rather than
    having imageloader on the DUT download the VM images. Remove the now unused
    imageloader code in the vm package as well.
    
    BUG=chromium:1025374
    TEST=vm.StartCrosvm
    
    Change-Id: Id58eaeb611ad0bfcc56a05c26b2e52c86f8572b0

--- src/chromiumos/tast/local/bundles/cros/vm/start_crosvm.go
+++ src/chromiumos/tast/local/bundles/cros/vm/start_crosvm.go
@@ -10,6 +10,8 @@ import (
 	"regexp"
 	"time"
 
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/sysutil"
 	"chromiumos/tast/local/vm"
 	"chromiumos/tast/testing"
 )
@@ -18,20 +20,31 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         StartCrosvm,
 		Desc:         "Checks that crosvm starts termina and runs commands through stdin",
-		Contacts:     []string{"jkardatzke@chromium.org", "cros-containers-dev@google.com"},
+		Contacts:     []string{"smbarber@chromium.org", "crosvm-dev@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"vm_host"},
+		// TODO(smbarber): Download only files for the current architecture.
+		Data: []string{
+			"termina_kernel_aarch64",
+			"termina_kernel_x86_64",
+			"termina_rootfs_aarch64.img",
+			"termina_rootfs_x86_64.img",
+		},
 	})
 }
 
 func StartCrosvm(ctx context.Context, s *testing.State) {
-	componentPath, err := vm.LoadTerminaComponent(ctx)
+	kernelName, rootfsName, err := vmDataName(ctx)
 	if err != nil {
-		s.Fatal("Unable to load component: ", err)
+		s.Fatal("Failed to find VM image: ", err)
 	}
+	kernelPath := s.DataPath(kernelName)
+	rootfsPath := s.DataPath(rootfsName)
+
 	cvm, err := vm.NewCrosvm(ctx, &vm.CrosvmParams{
+		VMKernel:   kernelPath,
+		RootfsPath: rootfsPath,
 		KernelArgs: []string{"init=/bin/bash"},
-		VMPath:     componentPath,
 	})
 	if err != nil {
 		s.Fatal("Failed to start crosvm: ", err)
@@ -39,7 +52,7 @@ func StartCrosvm(ctx context.Context, s *testing.State) {
 	defer cvm.Close(ctx)
 
 	testing.ContextLog(ctx, "Waiting for VM to boot")
-	startCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	startCtx, cancel := context.WithTimeout(ctx, 20*time.Second)
 	defer cancel()
 	line, err := cvm.WaitForOutput(startCtx, regexp.MustCompile("localhost\\b.*#"))
 	if err != nil {
@@ -52,11 +65,26 @@ func StartCrosvm(ctx context.Context, s *testing.State) {
 	if _, err = io.WriteString(cvm.Stdin(), cmd+"\n"); err != nil {
 		s.Fatalf("Failed to write %q command: %v", cmd, err)
 	}
-	cmdCtx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	cmdCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 	if line, err = cvm.WaitForOutput(cmdCtx, regexp.MustCompile("^sbin$")); err != nil {
 		s.Errorf("Didn't get expected %q output: %v", cmd, err)
 	} else {
 		s.Logf("Saw line %q", line)
 	}
+}
+
+// vmDataName returns the name of the VM kernel and rootfs files to use for the current architecture.
+func vmDataName(ctx context.Context) (string, string, error) {
+	u, err := sysutil.Uname()
+	if err != nil {
+		return "", "", errors.Wrap(err, "failed to get uname")
+	}
+	if u.Machine == "aarch64" {
+		return "termina_kernel_aarch64", "termina_rootfs_aarch64.img", nil
+	} else if u.Machine == "x86_64" {
+		return "termina_kernel_x86_64", "termina_rootfs_x86_64.img", nil
+	}
+
+	return "", "", errors.Errorf("no known VM image for architecture %q", u.Machine)
 }
