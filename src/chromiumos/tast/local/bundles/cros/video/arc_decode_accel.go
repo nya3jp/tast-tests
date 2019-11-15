b90f60cfa25042d6204b0a5664fd3e6138ccd72d
David Staessens <dstaessens@chromium.org>
2019-11-15 12:58:32 +0900

    video.ARCDecodeAccel: Combine ARC video decoder tests using parameterization.
    This CL combines the binary ARC video decode accelerator tests based on the
    arcvideodecoder_test binary into a single testcase.
    
    Using parameterized tests causes the tests to have slightly different names:
    video.ARCDecodeAccelH264240P -> video.ARCDecodeAccel.h264
    video.ARCDecodeAccelVP8240P  -> video.ARCDecodeAccel.vp8
    video.ARCDecodeAccelVP9240P  -> video.ARCDecodeAccel.vp9
    
    Note: The "240P" suffix was removed from the test names, as this is not very
    relevant for non-performance tests and there are no other resolutions tested.
    This also makes the test names more consistent with the video.DecodeAccel tests.
    
    TEST=video.ARCDecodeAccel.* on hana
    
    BUG=b:139327919
    
    Change-Id: I021f6e698b20a6d1ef065a466108349c0aeb3cd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1916773
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Tested-by: David Staessens <dstaessens@chromium.org>
    Reviewed-by: Pin-chih Lin <johnylin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/video/arc_decode_accel.go
@@ -0,0 +1,45 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ARCDecodeAccel,
+		Desc:         "Verifies ARC++ hardware decode acceleration by running the arcvideodecoder_test binary",
+		Contacts:     []string{"johnylin@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"android", "chrome"},
+		Pre:          arc.Booted(),
+		Params: []testing.Param{{
+			Name:              "h264",
+			Val:               "test-25fps.h264",
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
+		}, {
+			Name:              "vp8",
+			Val:               "test-25fps.vp8",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
+		}, {
+			Name:              "vp9",
+			Val:               "test-25fps.vp9",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
+		}},
+	})
+}
+
+func ARCDecodeAccel(ctx context.Context, s *testing.State) {
+	decode.RunAllARCVideoTests(ctx, s, s.Param().(string))
+}
