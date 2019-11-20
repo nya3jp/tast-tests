ac450a62abfa5db587515aa408ee7ee5f2ac6b3e
Wei Lee <wtlee@google.com>
2019-11-20 18:26:04 +0800

    Move measurement logic in CCAUIPreviewPerf to cca.go
    This CL moves the performance measurement codes to cca.go so that
    they could later be shared by CCAUIRecordVideoPerf which will be
    introduced in a following CL.
    
    BUG=b:141518806
    TEST=tast run [DUT] camera.CCAUIPreviewPerf
    
    Change-Id: Iab9ceb152a4f0c623ee144bec7981b4113bdbb51

--- src/chromiumos/tast/local/bundles/cros/camera/cca_ui_preview_perf.go
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_preview_perf.go
@@ -6,14 +6,10 @@ package camera
 
 import (
 	"context"
-	"time"
 
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/camera/cca"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/media/caps"
-	"chromiumos/tast/local/media/cpu"
-	"chromiumos/tast/local/perf"
 	"chromiumos/tast/testing"
 )
 
@@ -27,77 +23,15 @@ func init() {
 		Data:         []string{"cca_ui.js"},
 		Pre:          chrome.LoggedIn(),
 	})
-
 }
 
 // CCAUIPreviewPerf launches the Chrome Camera App, waits for camera preview, fullscreens the
 // application and starts measuring system CPU usage.
 func CCAUIPreviewPerf(ctx context.Context, s *testing.State) {
-	// Duration to wait for CPU to stabalize.
-	const stabilizationDuration = 5 * time.Second
-	// Duration of the interval during which CPU usage will be measured.
-	const measureDuration = 20 * time.Second
-
 	cr := s.PreValue().(*chrome.Chrome)
 
-	// Prevents the CPU usage measurements from being affected by any previous tests.
-	if err := cpu.WaitUntilIdle(ctx); err != nil {
-		s.Fatal("Failed to idle: ", err)
-	}
-
-	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")})
+	err := cca.MeasurePerformance(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir())
 	if err != nil {
-		s.Fatal("Failed to open CCA: ", err)
+		s.Fatal("Failed to measure performance: ", err)
 	}
-	defer app.Close(ctx)
-
-	if err := app.WaitForVideoActive(ctx); err != nil {
-		s.Fatal("Preview is inactive after launching App: ", err)
-	}
-	s.Log("Preview started")
-
-	cleanUpBenchmark, err := cpu.SetUpBenchmark(ctx)
-	if err != nil {
-		s.Fatal("Failed to set up benchmark mode: ", err)
-	}
-	defer cleanUpBenchmark(ctx)
-
-	cpuUsage, err := measureCPUUsage(ctx, app, measureDuration, stabilizationDuration)
-	if err != nil {
-		s.Fatal("Failed in measureCPUUsage(): ", err)
-	}
-	s.Log("Measured CPU usage: ", cpuUsage)
-
-	pv := perf.NewValues()
-	pv.Set(perf.Metric{
-		Name:      "cpu_usage",
-		Unit:      "percent",
-		Direction: perf.SmallerIsBetter,
-	}, cpuUsage)
-	if err := pv.Save(s.OutDir()); err != nil {
-		s.Error("Failed to save perf data: ", err)
-	}
-}
-
-// measureCPUUsage fullscreens the camera preview, starts measuring the CPU usage, and returns the percentage of the CPU used.
-func measureCPUUsage(ctx context.Context, app *cca.App, measureDuration, stabilizationDuration time.Duration) (float64, error) {
-	testing.ContextLog(ctx, "Fullscreening window")
-	if err := app.FullscreenWindow(ctx); err != nil {
-		return 0, errors.Wrap(err, "failed to fullscreen window")
-	}
-	if err := app.WaitForVideoActive(ctx); err != nil {
-		return 0, errors.Wrap(err, "preview is inactive after fullscreening window")
-	}
-
-	testing.ContextLog(ctx, "Sleeping to wait for CPU usage to stabilize for ", stabilizationDuration)
-	if err := testing.Sleep(ctx, stabilizationDuration); err != nil {
-		return 0, errors.Wrap(err, "failed to wait for CPU usage to stabilize")
-	}
-
-	testing.ContextLog(ctx, "Measuring CPU usage for ", measureDuration)
-	cpuUsage, err := cpu.MeasureCPUUsage(ctx, measureDuration)
-	if err != nil {
-		return 0, errors.Wrap(err, "failed to measure CPU usage")
-	}
-	return cpuUsage, nil
 }
