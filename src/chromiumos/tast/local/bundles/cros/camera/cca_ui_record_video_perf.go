fe1639c0ce0f4a0e0d36d779da51592d0391370a
Wei Lee <wtlee@google.com>
2019-11-21 14:41:50 +0800

    Add CCAUIRecordVideoPerf
    This test is mostly identical to CCAUIPreviewPerf but it measure the
    performance during video recording.
    
    BUG=b:141518806
    TEST=tast run [DUT] camera.CCAUIRecordVideoPerf
    
    Change-Id: I34cf20880c65c1e84a901688aed069755ef43387

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_record_video_perf.go
@@ -0,0 +1,37 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/camera/cca"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUIRecordVideoPerf,
+		Desc:         "Opens CCA and measures the CPU usage and power consumption during video recording",
+		Contacts:     []string{"wtlee@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", caps.BuiltinOrVividCamera},
+		Data:         []string{"cca_ui.js"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+// CCAUIRecordVideoPerf launches the Chrome Camera App, waits for camera preview, fullscreens the
+// application, start recording video and starts measuring system CPU usage and power consumption.
+func CCAUIRecordVideoPerf(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	err := cca.MeasurePerformance(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir(), true)
+	if err != nil {
+		s.Fatal("Failed to measure performance: ", err)
+	}
+}
