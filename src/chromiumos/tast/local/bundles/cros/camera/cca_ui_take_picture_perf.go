2632198b4d7305ab8d8eb0d8fc11c355e2575424
Wei Lee <wtlee@google.com>
2019-11-21 14:47:43 +0800

    Add CCAUITakePicturePerf
    This test is used to measure the app performance when taking picture.
    
    BUG=b:141518806
    TEST=tast run [DUT] camera.CCAUITakePicturePerf
    
    Change-Id: Ia759981846e358ebbcdbc3c0d415f8269e4e421f

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_take_picture_perf.go
@@ -0,0 +1,54 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/camera/cca"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/cpu"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUITakePicturePerf,
+		Desc:         "Opens CCA and measures the performance during photo taking",
+		Contacts:     []string{"wtlee@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", caps.BuiltinOrVividCamera},
+		Data:         []string{"cca_ui.js"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+// CCAUITakePicturePerf launches the Chrome Camera App, waits for camera preview, taking a picture
+// and collects the performance metrics.
+func CCAUITakePicturePerf(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	// Waits until CPU is idle so that the result won't affect by previous tests or initialization.
+	if err := cpu.WaitUntilIdle(ctx); err != nil {
+		s.Fatal("Failed to idle: ", err)
+	}
+
+	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")})
+	if err != nil {
+		s.Fatal("Failed to open CCA: ", err)
+	}
+	defer app.Close(ctx)
+
+	if err := app.WaitForVideoActive(ctx); err != nil {
+		s.Fatal("Failed to wait for video active")
+	}
+
+	app.TakeSinglePhoto(ctx, cca.TimerOff)
+
+	if err := app.CollectPerfEvents(ctx, s.OutDir()); err != nil {
+		s.Fatal("Failed to collect perf events: ", err)
+	}
+}
