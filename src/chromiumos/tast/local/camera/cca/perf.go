c9e6559993dcf0a7ec8ea8ddccc9a2660e42327d
Wei Lee <wtlee@google.com>
2019-11-21 17:06:20 +0800

    Add power measurement in CCAUIPerf
    BUG=b:141518806, b:180773888
    TEST=tast run [DUT] camera.CCAUIPerf
    
    Change-Id: I1a206be655b5520d44dcf3c117a73437a48a1404
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1928815
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Tested-by: Shik Chen <shik@chromium.org>
    Auto-Submit: Wei Lee <wtlee@chromium.org>

--- src/chromiumos/tast/local/camera/cca/perf.go
+++ src/chromiumos/tast/local/camera/cca/perf.go
@@ -111,15 +111,15 @@ func measureUIBehaviors(ctx context.Context, cr *chrome.Chrome, app *App, perfVa
 	})
 }
 
-// measureStablizedCPUUsage measures the CPU usage after it's cooled down for stabilizationDuration.
-func measureStablizedCPUUsage(ctx context.Context) (float64, error) {
+// measureStablizedUsage measures the CPU and power usage after it's cooled down for stabilizationDuration.
+func measureStablizedUsage(ctx context.Context) (map[string]float64, error) {
 	testing.ContextLog(ctx, "Sleeping to wait for CPU usage to stabilize for ", stabilizationDuration)
 	if err := testing.Sleep(ctx, stabilizationDuration); err != nil {
-		return 0, errors.Wrap(err, "failed to wait for CPU usage to stabilize")
+		return nil, errors.Wrap(err, "failed to wait for CPU usage to stabilize")
 	}
 
 	testing.ContextLog(ctx, "Measuring CPU usage for ", measureDuration)
-	return cpu.MeasureCPUUsage(ctx, measureDuration)
+	return cpu.MeasureUsage(ctx, measureDuration)
 }
 
 // measurePreviewPerformance measures the performance of preview with QR code detection on and off.
@@ -137,17 +137,36 @@ func measurePreviewPerformance(ctx context.Context, app *App, perfValues *perf.V
 		return errors.New("QR code detection should be off by default")
 	}
 
-	cpuUsage, err := measureStablizedCPUUsage(ctx)
+	usage, err := measureStablizedUsage(ctx)
 	if err != nil {
-		return errors.Wrap(err, "failed to measure CPU usage")
+		return errors.Wrap(err, "failed to measure CPU and power usage")
 	}
-	testing.ContextLogf(ctx, "Measured preview CPU usage: %.1f%%", cpuUsage)
 
-	perfValues.Set(perf.Metric{
-		Name:      fmt.Sprintf("cpu_usage_preview-facing-%s", facing),
-		Unit:      "percent",
-		Direction: perf.SmallerIsBetter,
-	}, cpuUsage)
+	var cpuUsage float64
+	if cpuUsage, exist := usage["cpu"]; exist {
+		testing.ContextLogf(ctx, "Measured preview CPU usage: %.1f%%", cpuUsage)
+
+		perfValues.Set(perf.Metric{
+			Name:      fmt.Sprintf("cpu_usage_preview-facing-%s", facing),
+			Unit:      "percent",
+			Direction: perf.SmallerIsBetter,
+		}, cpuUsage)
+	} else {
+		testing.ContextLog(ctx, "Failed to measure preview CPU usage")
+	}
+
+	var powerUsage float64
+	if powerUsage, exist := usage["power"]; exist {
+		testing.ContextLogf(ctx, "Measured preview power usage: %.1f Watts", powerUsage)
+
+		perfValues.Set(perf.Metric{
+			Name:      fmt.Sprintf("power_usage_preview-facing-%s", facing),
+			Unit:      "Watts",
+			Direction: perf.SmallerIsBetter,
+		}, powerUsage)
+	} else {
+		testing.ContextLog(ctx, "Failed to measure preview power usage")
+	}
 
 	// Enable QR code detection and measure the performance again.
 	enabled, err := app.ToggleQRCodeOption(ctx)
@@ -158,12 +177,10 @@ func measurePreviewPerformance(ctx context.Context, app *App, perfValues *perf.V
 		return errors.Wrap(err, "QR code detection is not enabled after toggling")
 	}
 
-	cpuUsageQR, err := measureStablizedCPUUsage(ctx)
+	usageQR, err := measureStablizedUsage(ctx)
 	if err != nil {
-		return errors.Wrap(err, "failed to measure CPU usage")
+		return errors.Wrap(err, "failed to measure CPU and power usage with QR code detection")
 	}
-	overhead := math.Max(0, cpuUsageQR-cpuUsage)
-	testing.ContextLogf(ctx, "Measured QR code detection CPU usage: %.1f%%, overhead = %.1f%%", cpuUsageQR, overhead)
 
 	enabled, err = app.ToggleQRCodeOption(ctx)
 	if err != nil {
@@ -173,18 +190,47 @@ func measurePreviewPerformance(ctx context.Context, app *App, perfValues *perf.V
 		return errors.Wrap(err, "QR code detection is not disabled after toggling")
 	}
 
-	perfValues.Set(perf.Metric{
-		Name:      fmt.Sprintf("cpu_usage_qrcode-facing-%s", facing),
-		Unit:      "percent",
-		Direction: perf.SmallerIsBetter,
-	}, cpuUsageQR)
+	if cpuUsageQR, exist := usageQR["cpu"]; exist {
+		perfValues.Set(perf.Metric{
+			Name:      fmt.Sprintf("cpu_usage_qrcode-facing-%s", facing),
+			Unit:      "percent",
+			Direction: perf.SmallerIsBetter,
+		}, cpuUsageQR)
 
-	perfValues.Set(perf.Metric{
-		Name:      fmt.Sprintf("cpu_overhead_qrcode-facing-%s", facing),
-		Unit:      "percent",
-		Direction: perf.SmallerIsBetter,
-	}, overhead)
+		if cpuUsage != 0 {
+			overhead := math.Max(0, cpuUsageQR-cpuUsage)
 
+			testing.ContextLogf(ctx, "Measured QR code detection CPU usage: %.1f%%, overhead = %.1f%%", cpuUsageQR, overhead)
+			perfValues.Set(perf.Metric{
+				Name:      fmt.Sprintf("cpu_overhead_qrcode-facing-%s", facing),
+				Unit:      "percent",
+				Direction: perf.SmallerIsBetter,
+			}, overhead)
+		}
+	} else {
+		testing.ContextLog(ctx, "Failed to measure preview CPU usage with QR code detection")
+	}
+
+	if powerUsageQR, exist := usageQR["power"]; exist {
+		perfValues.Set(perf.Metric{
+			Name:      fmt.Sprintf("power_usage_qrcode-facing-%s", facing),
+			Unit:      "Watts",
+			Direction: perf.SmallerIsBetter,
+		}, powerUsageQR)
+
+		if powerUsage != 0 {
+			overhead := math.Max(0, powerUsageQR-powerUsage)
+
+			testing.ContextLogf(ctx, "Measured QR code detection power usage: %.1f Watts, overhead = %.1f Watts", powerUsageQR, overhead)
+			perfValues.Set(perf.Metric{
+				Name:      fmt.Sprintf("power_overhead_qrcode-facing-%s", facing),
+				Unit:      "Watts",
+				Direction: perf.SmallerIsBetter,
+			}, overhead)
+		}
+	} else {
+		testing.ContextLog(ctx, "Failed to measure preview power usage with QR code detection")
+	}
 	return nil
 }
 
@@ -200,22 +246,38 @@ func measureRecordingPerformance(ctx context.Context, app *App, perfValues *perf
 		return errors.Wrap(err, "failed to start recording for performance measurement")
 	}
 
-	cpuUsage, err := measureStablizedCPUUsage(ctx)
+	usage, err := measureStablizedUsage(ctx)
 	if err != nil {
-		return errors.Wrap(err, "failed to measure CPU usage")
+		return errors.Wrap(err, "failed to measure CPU and power usage")
 	}
-	testing.ContextLogf(ctx, "Measured recording CPU usage: %.1f%%", cpuUsage)
 
 	if _, _, err := app.StopRecording(ctx, false, recordingStartTime); err != nil {
 		return errors.Wrap(err, "failed to stop recording for performance measurement")
 	}
 
-	perfValues.Set(perf.Metric{
-		Name:      fmt.Sprintf("cpu_usage_recording-facing-%s", facing),
-		Unit:      "percent",
-		Direction: perf.SmallerIsBetter,
-	}, cpuUsage)
+	if cpuUsage, exist := usage["cpu"]; exist {
+		testing.ContextLogf(ctx, "Measured recording CPU usage: %.1f%%", cpuUsage)
 
+		perfValues.Set(perf.Metric{
+			Name:      fmt.Sprintf("cpu_usage_recording-facing-%s", facing),
+			Unit:      "percent",
+			Direction: perf.SmallerIsBetter,
+		}, cpuUsage)
+	} else {
+		testing.ContextLog(ctx, "Failed to measure recording CPU usage")
+	}
+
+	if powerUsage, exist := usage["power"]; exist {
+		testing.ContextLogf(ctx, "Measured recording power usage: %.1f Watts", powerUsage)
+
+		perfValues.Set(perf.Metric{
+			Name:      fmt.Sprintf("power_usage_recording-facing-%s", facing),
+			Unit:      "Watts",
+			Direction: perf.SmallerIsBetter,
+		}, powerUsage)
+	} else {
+		testing.ContextLog(ctx, "Failed to measure recording power usage")
+	}
 	return nil
 }
 
