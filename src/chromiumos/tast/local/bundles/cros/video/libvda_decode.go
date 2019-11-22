c6cd17269958290c2ed5482dcd025ee816dd6202
David Staessens <dstaessens@chromium.org>
2019-11-22 09:47:47 +0900

    video.LibvdaDecode: Parameterize libvda video decode tests.
    This CL combines the libvda video decode tests into a single testcase using
    parameterization.
    
    Using parameterized tests causes the tests to have slightly different names:
    video.LibvdaDecodeH264 -> video.LibvdaDecode.h264
    video.LibvdaDecodeVP8  -> video.LibvdaDecode.vp8
    video.LibvdaDecodeVP9  -> video.LibvdaDecode.vp9
    
    TEST=video.LibvdaDecode.* on hana
    
    BUG=b:139327919
    
    Change-Id: I9fbd4ba1e571c3a627b2f036e655aadc3bbc3840

---
+++ src/chromiumos/tast/local/bundles/cros/video/libvda_decode.go
@@ -0,0 +1,47 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/libvda"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         LibvdaDecode,
+		Desc:         "Checks that video decoding using libvda's Mojo connection to GAVDA is working",
+		Contacts:     []string{"alexlau@chromium.org", "chromeos-video-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"android_vm", "chrome"},
+
+		Params: []testing.Param{{
+			Name: "h264",
+			Val:  "h264",
+			// "chrome_internal" is needed because H.264 is a proprietary codec
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "chrome_internal"},
+			ExtraData:         []string{"test-25fps.h264"},
+		}, {
+			Name:              "vp8",
+			Val:               "vp8",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+			ExtraData:         []string{"test-25fps.vp8"},
+		}, {
+			Name:              "vp9",
+			Val:               "vp9",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         []string{"test-25fps.vp9"},
+		}},
+	})
+}
+
+func LibvdaDecode(ctx context.Context, s *testing.State) {
+	logFileName := "output_libvda_" + s.Param().(string) + ".txt"
+	videoFile := "test-25fps." + s.Param().(string)
+	libvda.RunGPUFileDecodeTest(ctx, s, logFileName, videoFile)
+}
