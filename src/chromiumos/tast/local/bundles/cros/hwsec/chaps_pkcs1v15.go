8e18dd35e6ef7591a87185e6696c6b921d97958b
John L Chen <zuan@chromium.org>
2019-11-23 03:11:49 +0800

    tast-tests: hwsec: Add ChapsPKCS11V5
    This change add the test for chaps that exercises signing and verification
    of RSA PKCS#1 v1.5.
    
    BUG=none
    TEST=tast run $dut hwsec.ChapsPKCS1V15
    
    Change-Id: I0c3bb7b9a502a24bbd3f8a74f60a6a8e194042fa

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/chaps_pkcs1v15.go
@@ -0,0 +1,79 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/common/pkcs11"
+	"chromiumos/tast/common/pkcs11/pkcs11test"
+	libhwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ChapsPKCS1V15,
+		Desc: "Verifies PKCS#1 v1.5 works with RSA keys (sign, verify) in chaps",
+		Attr: []string{"informational"},
+		Contacts: []string{
+			"zuan@chromium.org",
+			"cros-hwsec@chromium.org",
+		},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func ChapsPKCS1V15(ctx context.Context, s *testing.State) {
+	r, err := libhwseclocal.NewCmdRunner()
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Cryptohome Utilty creation error: ", err)
+	}
+
+	pkcs11Util, err := pkcs11.NewUtil(ctx, r, utility)
+	if err != nil {
+		s.Fatal("Failed to create PKCS#11 Utility: ", err)
+	}
+
+	const scratchpadPath = "/tmp/ChapsPKCS1V15Test"
+
+	// Prepare the scratchpad.
+	f1, f2, err := pkcs11test.PrepareScratchpadAndTestFiles(ctx, r, scratchpadPath)
+	if err != nil {
+		s.Fatal("Failed to initialize the scratchpad space: ", err)
+	}
+	// Remove all keys/certs, if any at the end. i.e. Cleanup after ourselves.
+	defer pkcs11test.CleanupScratchpad(ctx, r, scratchpadPath)
+
+	// Create the software-generated, then imported key.
+	importedKey, err := pkcs11Util.CreateRSASoftwareKey(ctx, utility, scratchpadPath, "", "testkey1", "aaaaaa")
+	if err != nil {
+		s.Fatal("Failed to create software key: ", err)
+	}
+	defer func() {
+		if err := importedKey.DestroyKey(ctx, pkcs11Util); err != nil {
+			s.Error("Failed to clean up software key: ", err)
+		}
+	}()
+
+	keys := []*pkcs11.KeyInfo{importedKey}
+
+	// Test the various keys.
+	for _, k := range keys {
+		// Test the various mechanisms.
+		for _, n := range []string{"SHA1-RSA-PKCS", "SHA256-RSA-PKCS"} {
+			m := pkcs11.NewMechanism(n)
+			if err := pkcs11test.SignAndVerify(ctx, pkcs11Util, k, f1, f2, m); err != nil {
+				s.Error("SignAndVerify failed: ", err)
+			}
+		}
+	}
+}
