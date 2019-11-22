108b2ec4591fe4e0482e424cac20955d4dac04a1
John L Chen <zuan@chromium.org>
2019-11-23 03:11:49 +0800

    tast-tests: hwsec: Add ChapsPKCS11V5
    This change add the test for chaps that exercises signing and verification
    of RSA PKCS#1 1v5.
    
    BUG=none
    TEST=tast run $dut hwsec.ChapsPKCS11V5
    
    Change-Id: I0c3bb7b9a502a24bbd3f8a74f60a6a8e194042fa

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/chaps_pkcs1v15.go
@@ -0,0 +1,81 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"io/ioutil"
+
+	"chromiumos/tast/common/hwsec"
+	libhwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ChapsPKCS1V15,
+		Desc: "Verifies PKCS#1 v1.5 works with RSA keys (sign, verify, encrypt, decrypt) in chaps",
+		Attr: []string{"informational"},
+		Contacts: []string{
+			"zuan@chromium.org",
+		},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func ChapsPKCS1V15(ctx context.Context, s *testing.State) {
+	r, err := libhwseclocal.NewCmdRunner()
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Cryptohome Utilty creation error: ", err)
+	}
+
+	pkcs11, err := hwsec.NewPkcs11Util(r, utility)
+	if err != nil {
+		s.Fatal("PKCS#11 Utility creation error: ", err)
+	}
+
+	// Remove all keys/certs, if any at the end. i.e. Cleanup after ourselves.
+	defer r.Run(ctx, "sh", "-c", "rm -f "+hwsec.Pkcs11Scratchpad+"/testkey* "+hwsec.Pkcs11Scratchpad+"/testfile*")
+	// Note: This test expects a clean /tmp/ (Pkcs11Scratchpad) in the sense that there should be no files prefixed by testkey or testfile when the test start.
+	// Note: Also, this test expects a clean keystore, in the sense that there should be no object with the same ID as those used by this test.
+
+	// Create the test file
+	const testfile1 = "/tmp/testfile1.txt"
+	if err = ioutil.WriteFile(testfile1, []byte("test1"), 0644); err != nil {
+		s.Fatal("Failed to write " + testfile1)
+	}
+	const testfile2 = "/tmp/testfile2.txt"
+	if err = ioutil.WriteFile(testfile2, []byte("test2"), 0644); err != nil {
+		s.Fatal("Failed to write " + testfile1)
+	}
+
+	// Create the software-generated, then imported key
+	importedKey, err := pkcs11.Pkcs11CreateRsaSoftwareKey(ctx, utility, "", "testkey1", "aaaaaa")
+	if err != nil {
+		s.Fatal("Failed to create software key: ", err)
+	}
+	defer func() {
+		if err := pkcs11.Pkcs11DestroyKey(ctx, importedKey); err != nil {
+			s.Fatal("Failed to clean up software key: ", err)
+		}
+	}()
+
+	keys := []hwsec.Pkcs11KeyInfo{importedKey}
+
+	// Test the various keys
+	for _, k := range keys {
+		// Test the various mechanisms
+		for _, m := range []hwsec.Pkcs11MechanismInfo{pkcs11.Pkcs11SHA1RSAPKCS(), pkcs11.Pkcs11SHA256RSAPKCS()} {
+			if err = pkcs11.Pkcs11SignVerify(ctx, k, testfile1, testfile2, m); err != nil {
+				s.Fatal("SignVerify failed: ", err)
+			}
+		}
+	}
+}
