b597893333f41d9fc738f7731384af43a0bb625e
David Valleau <valleau@chromium.org>
2019-11-25 14:02:19 -0800

    printer: adding PPDCopies and PPDNoCopies tests
    These tests configure a virtual USB printer which supports IPP-over-USB
    and checks that the PPD file generated by CUPS contains the expected
    "cups*Copies" values.
    
    BUG=chromium:1027820
    TEST=Passes tast tests on asuka device
    
    Change-Id: I98007f54c59d4a4dec8acb076a627c8baf41d731

---
+++ src/chromiumos/tast/local/bundles/cros/printer/ppd_no_copies.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package printer
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/printer/usbprinter"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PPDNoCopies,
+		Desc:         "Verifies that the 'copies-supported' attribute of the printer is used to populate the cupsManualCopies and cupsMaxCopies values in the corresponding generated PPD",
+		Contacts:     []string{"valleau@chromium.org"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "cups", "virtual_usb_printer"},
+		Data:         []string{"ipp_no_copies.json"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func PPDNoCopies(ctx context.Context, s *testing.State) {
+	const descriptors = "/usr/local/etc/virtual-usb-printer/ippusb_printer.json"
+	usbprinter.RunPPDTest(ctx, s, descriptors, s.DataPath("ipp_no_copies.json"), map[string]string{
+		"*cupsManualCopies": "true",
+		"*cupsMaxCopies":    "1",
+	})
+}
