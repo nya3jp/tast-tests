d35e4d580a933a203708b6cb32724cca68e17c82
Yen-lin Lai <yenlinlai@google.com>
2019-11-26 16:00:37 +0800

    WIP: network: PoC for remote test SimpleConnect
    DONE:
    - gRPC PoC for DUT to connect to WiFi
      - inherit interface from xmlrpc in Autotest
    - port AP setup code from Brian's PoC
    - cleanup profile after disconnect
    - collect daemon logs
    - port WifiTestContext as wifi.TextFixture
    
    TODO:
    - AP setting
      - finish the config structure
    - somehow have sshd Signal working
      - what if router needs update?
    - split smaller cls for each independent components
      - after the structure is confirmed
    
    BUG=chromium:1034875
    TEST=tast run -var=router=$ROUTER $DUT network.SimpleConnect*
    
    Cq-Depend: chromium:1971654
    Change-Id: Ifb4007ce65ac957492bfd9eff147619ded94a44b

---
+++ src/chromiumos/tast/remote/wifi/hostap/config.go
@@ -0,0 +1,212 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hostap
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/wifi/utils"
+)
+
+// Ported from Brian's draft crrev.com/c/1733740.
+
+// ModeEnum is the type for specifying hostap mode.
+type ModeEnum string
+
+// Mode enums.
+const (
+	Mode80211a      ModeEnum = "a"
+	Mode80211b      ModeEnum = "b"
+	Mode80211g      ModeEnum = "g"
+	Mode80211nMixed ModeEnum = "n-mixed"
+	Mode80211nPure  ModeEnum = "n-only"
+)
+
+// HTCap is the type for specifying HT capabilities in hostapd config (ht_capab=).
+type HTCap int
+
+// HTCap enums, use bitmask for ease of checking existance.
+const (
+	// HTCapHT20 is the default when none of HT40* specified.
+	HTCapHT40       HTCap = 1 << iota // auto-detect supported "[HT40-]" or "[HT40+]"
+	HTCapHT40Minus                    // "[HT40-]"
+	HTCapHT40Plus                     // "[HT40+]"
+	HTCapSGI20                        // "[SHORT-GI-20]"
+	HTCapSGI40                        // "[SHORT-GI-40]"
+	HTCapGreenfield                   // "[GF]"
+)
+
+// Option is the function signature used to specify options of Config.
+type Option func(*Config)
+
+// SSID return an Option which sets ssid in hostapd config.
+func SSID(ssid string) Option {
+	return func(c *Config) {
+		c.Ssid = ssid
+	}
+}
+
+// Mode returns an Option which sets mode in hostapd config.
+func Mode(mode ModeEnum) Option {
+	return func(c *Config) {
+		c.Mode = mode
+	}
+}
+
+// Channel returns an Option which sets channel in hostapd config.
+func Channel(ch int) Option {
+	return func(c *Config) {
+		c.Channel = ch
+	}
+}
+
+// HTCaps returns an Option which sets ht capabilities in hostapd config.
+func HTCaps(caps ...HTCap) Option {
+	return func(c *Config) {
+		for _, ca := range caps {
+			c.HTCaps |= ca
+		}
+	}
+}
+
+// NewConfig creates a Config with given options.
+func NewConfig(ops ...Option) *Config {
+	// Default config.
+	conf := &Config{
+		Ssid:    RandomSSID("TAST_TEST_"),
+		Mode:    Mode80211g,
+		Channel: 1,
+	}
+	for _, op := range ops {
+		op(conf)
+	}
+	return conf
+}
+
+// Config is the config used to start hostapd on router.
+type Config struct {
+	Ssid    string
+	Mode    ModeEnum
+	Channel int
+	HTCaps  HTCap
+}
+
+// Format the config into hostapd.conf format.
+// iface is the interface for the hostapd to run. ctrlPath is the control
+// file path for hostapd to communicate with hostapd_cli.
+func (c *Config) Format(iface, ctrlPath string) (string, error) {
+	var builder strings.Builder
+	configure := func(k, v string) {
+		builder.WriteString(fmt.Sprintf("%s=%s\n", k, v))
+	}
+
+	configure("logger_syslog", "-1")
+	configure("logger_syslog_level", "0")
+	// Default RTS and frag threshold to "off".
+	configure("rts_threshold", "2347")
+	configure("fragm_threshold", "2346")
+	configure("driver", "nl80211")
+
+	// Configurable.
+	configure("ctrl_interface", ctrlPath)
+	configure("ssid", c.Ssid)
+	configure("interface", iface)
+	configure("channel", strconv.Itoa(c.Channel))
+
+	hwMode, err := c.hwMode()
+	if err != nil {
+		return "", err
+	}
+	configure("hw_mode", hwMode)
+
+	if c.is80211n() {
+		configure("ieee80211n", "1")
+		htCaps, err := c.htCapsString()
+		if err != nil {
+			return "", err
+		}
+		configure("ht_capab", htCaps)
+		if c.Mode == Mode80211nPure {
+			configure("require_ht", "1")
+		}
+	}
+	if c.HTCaps != 0 {
+		configure("wmm_enabled", "1")
+	}
+
+	return builder.String(), nil
+}
+
+// utilities for HostAPConfig to generate config map.
+
+func (c *Config) is80211n() bool {
+	return c.Mode == Mode80211nMixed || c.Mode == Mode80211nPure
+}
+
+func (c *Config) hwMode() (string, error) {
+	if c.Mode == Mode80211a || c.Mode == Mode80211b || c.Mode == Mode80211g {
+		return string(c.Mode), nil
+	}
+	if c.is80211n() {
+		f, err := utils.ChannelToFrequency(c.Channel)
+		if err != nil {
+			return "", err
+		}
+		if f > 5000 {
+			return string(Mode80211a), nil
+		}
+		return string(Mode80211g), nil
+	}
+	return "", errors.Errorf("invalid mode %s", string(c.Mode))
+}
+
+func channelIn(ch int, list []int) bool {
+	for _, c := range list {
+		if c == ch {
+			return true
+		}
+	}
+	return false
+}
+
+var ht40MinusChannels = []int{5, 6, 7, 8, 9, 10, 11, 12, 13, 40, 48, 56, 64, 104, 112, 120, 128, 136, 144, 153, 161}
+var ht40PlusChannels = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 36, 44, 52, 60, 100, 108, 116, 124, 132, 140, 149, 157}
+
+func (c *Config) htCapsString() (string, error) {
+	var caps []string
+	if c.HTCaps&(HTCapHT40|HTCapHT40Minus) > 0 && channelIn(c.Channel, ht40MinusChannels) {
+		caps = append(caps, "[HT40-]")
+	}
+	if c.HTCaps&(HTCapHT40|HTCapHT40Plus) > 0 && channelIn(c.Channel, ht40PlusChannels) {
+		caps = append(caps, "[HT40+]")
+	}
+	if c.HTCaps&HTCapSGI20 > 0 {
+		caps = append(caps, "[SHORT-GI-20]")
+	}
+	if c.HTCaps&HTCapSGI40 > 0 {
+		caps = append(caps, "[SHORT-GI-40]")
+	}
+	if c.HTCaps&HTCapGreenfield > 0 {
+		caps = append(caps, "[GF]")
+	}
+	return strings.Join(caps, ""), nil
+}
+
+// RandomSSID returns a random SSID of length 30 and given prefix.
+func RandomSSID(prefix string) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// Generate 30-char SSID, including prefix
+	n := 30 - len(prefix)
+	s := make([]byte, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return prefix + string(s)
+}
---
+++ src/chromiumos/tast/remote/wifi/hostap/server.go
@@ -0,0 +1,176 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hostap
+
+import (
+	"context"
+	"fmt"
+	"path"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/host"
+	"chromiumos/tast/remote/wifi/utils"
+	"chromiumos/tast/testing"
+)
+
+// Ported from Brian's draft crrev.com/c/1733740.
+
+const (
+	hostapdCmd = "hostapd"
+)
+
+// Server controls a hostapd on router.
+type Server struct {
+	host    *host.SSH // TODO(crbug.com/1019537): use a more suitable ssh object.
+	conf    *Config
+	iface   string
+	workDir string
+	cmd     *host.Cmd
+}
+
+// NewServer creates a new Server object and runs hostapd on iface of the given host with settings
+// specified in config. workDir is the dir on host for the server to put temporary files.
+func NewServer(ctx context.Context, host *host.SSH, iface, workDir string, config *Config) (*Server, error) {
+	server := &Server{
+		host:    host,
+		conf:    config,
+		iface:   iface,
+		workDir: workDir,
+	}
+	if err := server.start(ctx); err != nil {
+		return nil, err
+	}
+	return server, nil
+}
+
+func (ap *Server) confPath() string {
+	return path.Join(ap.workDir, fmt.Sprintf("hostapd-%s.conf", ap.iface))
+}
+
+func (ap *Server) ctrlPath() string {
+	return path.Join(ap.workDir, fmt.Sprintf("hostapd-%s.ctrl", ap.iface))
+}
+
+func (ap *Server) stdoutFile() string {
+	return fmt.Sprintf("hostapd-%s.stdout", ap.iface)
+}
+
+func (ap *Server) stderrFile() string {
+	return fmt.Sprintf("hostapd-%s.stderr", ap.iface)
+}
+
+func (ap *Server) start(ctx context.Context) (err error) {
+	// Cleanup on error.
+	defer func() {
+		if err != nil {
+			ap.Stop(ctx)
+		}
+	}()
+
+	conf, err := ap.conf.Format(ap.iface, ap.ctrlPath())
+	if err != nil {
+		return err
+	}
+	err = utils.WriteToHost(ctx, ap.host, ap.confPath(), []byte(conf))
+	if err != nil {
+		return errors.Wrap(err, "failed to write config")
+	}
+
+	testing.ContextLog(ctx, "Starting hostapd")
+	cmd := ap.host.Command(hostapdCmd, "-dd", "-t", ap.confPath())
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to obtain StdoutPipe of hostapd")
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to obtain StderrPipe of hostapd")
+	}
+	if err := cmd.Start(ctx); err != nil {
+		return errors.Wrap(err, "failed to start hostapd")
+	}
+	ap.cmd = cmd
+
+	// Collect stderr.
+	if err := utils.ReadToOutDir(ctx, ap.stderrFile(), stderr, nil); err != nil {
+		return errors.Wrap(err, "failed to spawn reader for stderr")
+	}
+
+	// Wait for hostapd to get ready.
+	done := make(chan error, 1)
+	go func() {
+		var msg []byte
+		buf := make([]byte, 1024)
+		for {
+			n, err := stdout.Read(buf)
+			if err != nil {
+				// If the program exits unexpectedly, it will also go here with
+				// err = io.EOF.
+				done <- errors.Wrap(err, "failed to read stdout of hostapd")
+				return
+			}
+			msg = append(msg, buf[:n]...)
+			s := string(msg)
+			if strings.Contains(s, "Setup of interface done") {
+				break
+			}
+			if strings.Contains(s, "Interface initialization failed") {
+				// Don't keep polling. We failed.
+				done <- errors.New("hostapd failed to initialize AP interface")
+				return
+			}
+		}
+		if err := utils.ReadToOutDir(ctx, ap.stdoutFile(), stdout, msg); err != nil {
+			done <- errors.Wrap(err, "failed to spawn reader for stdout")
+			return
+		}
+		// Service ready.
+		close(done)
+	}()
+
+	select {
+	case err = <-done:
+	case <-ctx.Done():
+		err = ctx.Err()
+	}
+
+	if err != nil {
+		return err
+	}
+	testing.ContextLog(ctx, "hostapd started")
+	return nil
+}
+
+// Stop hostapd and cleanup related resources.
+func (ap *Server) Stop(ctx context.Context) error {
+	testing.ContextLog(ctx, "Stoping hostapd")
+	var err error
+	if ap.cmd == nil {
+		err = errors.New("server not started")
+	} else {
+		ap.cmd.Abort()
+		// TODO(crbug.com/1030635): Abort might not work, use pkill to ensure the daemon is killed.
+		ap.host.Command("pkill", "-f", fmt.Sprintf("^%s.*%s", hostapdCmd, ap.confPath()))
+
+		// Skip the error in Wait as the process is aborted and always has error in wait.
+		ap.cmd.Wait(ctx)
+		ap.cmd = nil
+	}
+	if err2 := ap.host.Command("rm", ap.confPath()).Run(ctx); err2 != nil {
+		err = errors.Wrapf(err, "failed to remove config with err=%s", err2.Error())
+	}
+	return err
+}
+
+// Interface returns the interface used by the hostapd.
+func (ap *Server) Interface() string {
+	return ap.iface
+}
+
+// Config returns the config used by the hostapd.
+func (ap *Server) Config() Config {
+	return *ap.conf
+}
