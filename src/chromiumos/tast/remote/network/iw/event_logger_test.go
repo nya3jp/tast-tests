c5a44cb343a5af175c6226de2d2721d5361478dd
Yen-lin Lai <yenlinlai@google.com>
2019-11-26 16:00:37 +0800

    network: first version for remote test SimpleConnect port
    This commit ports some components from Autotest to construct the basic
    flow of a remote WiFi test, including:
    - router setup (inherit codes in crrev.com/c/1733740)
    - gRPC for client side dbus operation
    - connection verification with ping
    
    More precised port of SimpleConnect is planned to be built upon this
    base, including:
    - more testcases with different parameters
    - pcap, attenuator setup
    - collect system logs on companion devices
    - collect perf metrics (e.g. time used in every stage of connection)
    - setup/teardown on wifi client (ensuring DUT with clean state)
    - some setup/verification steps in inline TODOs
    
    Still other components will be ported when used, including:
    - avahi
    - bridging multiple hostap instance
    - frag_threshold
    - hostap configs
    - IBSS
    - pcap parsing
    - security config (WEP, WPA...)
    
    BUG=chromium:1034875
    TEST=tast run -var=router=$ROUTER $DUT network.SimpleConnect*
    
    Cq-Depend: chromium:1971654
    Change-Id: Ifb4007ce65ac957492bfd9eff147619ded94a44b

---
+++ src/chromiumos/tast/remote/network/iw/event_logger_test.go
@@ -0,0 +1,74 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package iw
+
+import (
+	"math"
+	"testing"
+	"time"
+)
+
+func TestParseEvent(t *testing.T) {
+	testcases := []struct {
+		input    string
+		expected *Event
+	}{
+		{
+			input: "1578622653.543296: wlan0: new station aa:bb:cc:dd:ee:ff",
+			expected: &Event{
+				Timestamp: time.Unix(1578622653, 543296000),
+				Interface: "wlan0",
+				Message:   "new station aa:bb:cc:dd:ee:ff",
+			},
+		},
+		{
+			input: "1578622656.590595: wlan0 (phy #5): assoc aa:bb:cc:dd:ee:ff -> 1a:2b:3c:4d:5e:6f status: 0: Successful",
+			expected: &Event{
+				Timestamp: time.Unix(1578622656, 590595000),
+				Interface: "wlan0",
+				Message:   "assoc aa:bb:cc:dd:ee:ff -> 1a:2b:3c:4d:5e:6f status: 0: Successful",
+			},
+		},
+		// Handcrafted testcase to test wdev case.
+		{
+			input: "1.000005: wdev 0x1: message",
+			expected: &Event{
+				Timestamp: time.Unix(1, 5),
+				Interface: "wdev 0x1",
+				Message:   "message",
+			},
+		},
+	}
+
+	// We may have float parsing error, so construct our own compare.
+	compare := func(actual, want *Event) bool {
+		if (actual == nil) != (want == nil) {
+			return false
+		}
+		if actual == nil {
+			return true
+		}
+		if actual.Interface != want.Interface {
+			return false
+		}
+		if actual.Message != want.Message {
+			return false
+		}
+		// Let's check accuracy to millisecond (i.e. 1e6 nano).
+		if math.Abs(float64(actual.Timestamp.UnixNano()-want.Timestamp.UnixNano())) > 1e6 {
+			return false
+		}
+		return true
+	}
+
+	for i, tc := range testcases {
+		ev, err := parseEvent(tc.input)
+		if err != nil {
+			t.Errorf("error on case %d. err=%s", i, err.Error())
+		} else if !compare(ev, tc.expected) {
+			t.Errorf("error on case %d. got %v, want %v", i, ev, tc.expected)
+		}
+	}
+}
