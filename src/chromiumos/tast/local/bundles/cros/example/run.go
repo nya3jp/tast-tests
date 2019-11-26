9e913a8df8d1168ebd450c4e6046f1b3c1f862bf
Victor-Gabriel Savu <vsavu@google.com>
2019-11-26 04:15:16 +0000

    tast-test: add example for s.Run
    Add example for s.Run to showcase the new functionality.
    
    TEST=tast run example.Run
    BUG=chromium:1007138
    
    Cq-Depend: chromium:1935527
    Change-Id: I701a54939abaf47758970987e5b959d416f731c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1935948
    Tested-by: Victor-Gabriel Savu <vsavu@google.com>
    Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/example/run.go
@@ -0,0 +1,46 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     Run,
+		Desc:     "Subtest example, always fails",
+		Contacts: []string{"vsavu@google.com", "tast-owners@google.com"},
+		Attr:     []string{"disabled"},
+	})
+}
+
+func Run(ctx context.Context, s *testing.State) {
+	s.Run(ctx, "ok", func(ctx context.Context, s *testing.State) {
+		s.Log("ok")
+	})
+
+	s.Run(ctx, "error", func(ctx context.Context, s *testing.State) {
+		s.Error("Here's an error")
+	})
+
+	s.Run(ctx, "fatal", func(ctx context.Context, s *testing.State) {
+		s.Fatal("Here's a fatal error")
+	})
+
+	s.Run(ctx, "still-ok", func(ctx context.Context, s *testing.State) {
+		s.Log("Still ok")
+	})
+
+	s.Run(ctx, "l1", func(ctx context.Context, s *testing.State) {
+		s.Log("Level 1")
+
+		s.Run(ctx, "l2", func(ctx context.Context, s *testing.State) {
+			s.Log("Level 2")
+		})
+	})
+}
