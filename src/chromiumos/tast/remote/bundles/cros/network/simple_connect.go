b5ba6ef336fad19bca579828f8dfac3e37d54f77
Yen-lin Lai <yenlinlai@google.com>
2019-11-26 16:00:37 +0800

    network: migrate network_WiFi_SimpleConnect to Tast
    This commit ports the following components in Autotest to build the
    basic flow of a remote WiFi Tast test:
    - router setup (inherit codes in crrev.com/c/1733740)
    - gRPC for D-Bus operation on a remote DUT
    - connection verification with ping
    
    This commit migrates basic 802.11 a/b/g tests. Based on the commit,
    upcoming commits are planned to complete the Tast migration of
    network_WiFi_SimpleConnect, including:
    - more testcases with different parameters
    - pcap, attenuator setup
    - collect system logs on companion devices
    - collect perf metrics (e.g. time used in every stage of connection)
    - setup/teardown on wifi client (ensuring DUT with clean state)
    - some setup/verification steps in inline TODOs
    
    Still other components will be ported when used, including:
    - avahi
    - attenuator control
    - bridging multiple hostap instance
    - frag_threshold
    - more hostap configs
    - IBSS
    - pcap parsing
    - security config (WEP, WPA...)
    
    BUG=chromium:1034875
    TEST=tast run -var=router=$ROUTER $DUT network.SimpleConnect*
    
    Cq-Depend: chromium:1971654
    Change-Id: Ifb4007ce65ac957492bfd9eff147619ded94a44b

---
+++ src/chromiumos/tast/remote/bundles/cros/network/simple_connect.go
@@ -0,0 +1,99 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+
+	"chromiumos/tast/remote/wifi"
+	"chromiumos/tast/remote/wifi/hostap"
+	"chromiumos/tast/testing"
+)
+
+type simpleConnectParm struct {
+	apOptions []hostap.Option
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        SimpleConnect,
+		Desc:        "PoC of Wifi SimpleConnect test using gRPC",
+		Contacts:    []string{"yenlinlai@google.com", "chromeos-kernel-wifi@google.com"},
+		Attr:        []string{"group:wificell", "wifi_func"},
+		ServiceDeps: []string{"tast.cros.network.Wifi"},
+		Vars:        []string{"router"},
+		Params: []testing.Param{
+			{
+				Name: "80211a",
+				Val: simpleConnectParm{
+					apOptions: []hostap.Option{
+						hostap.Mode(hostap.Mode80211a),
+						hostap.Channel(48),
+					},
+				},
+			}, {
+				Name: "80211b",
+				Val: simpleConnectParm{
+					apOptions: []hostap.Option{
+						hostap.Mode(hostap.Mode80211b),
+						hostap.Channel(1),
+					},
+				},
+			}, {
+				Name: "80211g",
+				Val: simpleConnectParm{
+					apOptions: []hostap.Option{
+						hostap.Mode(hostap.Mode80211g),
+						hostap.Channel(1),
+					},
+				},
+			},
+		},
+	})
+}
+
+func SimpleConnect(ctx context.Context, s *testing.State) {
+	router, _ := s.Var("router")
+	tf, err := wifi.NewTestFixture(ctx, s.DUT(), s.RPCHint(), router)
+	if err != nil {
+		s.Fatal("Failed to set up test fixture: ", err)
+	}
+	defer func() {
+		if err := tf.Close(ctx); err != nil {
+			s.Logf("Failed to tear down test fixture, err=%q", err.Error())
+		}
+	}()
+
+	parm := s.Param().(simpleConnectParm)
+	ap, err := tf.ConfigureAP(ctx, parm.apOptions...)
+	if err != nil {
+		s.Fatal("Failed to configure ap: ", err)
+	}
+	defer func() {
+		if err := tf.DeconfigAP(ctx, ap); err != nil {
+			s.Logf("Failed to deconfig ap, err=%q", err.Error())
+		}
+	}()
+	s.Log("AP setup done")
+
+	if err := tf.ConnectWifi(ctx, ap); err != nil {
+		s.Fatal("Failed to connect to wifi: ", err)
+	}
+	defer func() {
+		if err := tf.DisconnectWifi(ctx); err != nil {
+			s.Logf("Failed to disconnect wifi, err=%q", err.Error())
+		}
+	}()
+	s.Log("Connected")
+
+	if err := tf.AssertNoDisconnect(ctx, tf.PingFromDUT); err != nil {
+		s.Fatal("Failed to ping from DUT: ", err)
+	}
+	// TODO(yenlinlai): assert no deauth detected from the server side.
+	// TODO(yenlinlai): Maybe some more check on the wifi capabilities to verify
+	// we really have the settings as expected. (ref: crrev.com/c/1995105)
+
+	s.Log("Tearing down")
+}
