df3b224fdc6f5663b5c6ee5116ce1477d0456fec
Miguel Casas <mcasas@chromium.org>
2019-11-26 19:08:14 -0500

    tast/webrtc: parameterize webrtc.DecodePerf and add h264 case
    ToT webrtc.DecodePerf leaves undetermined the codec profile to
    be used; WebRTC will use whichever it feels best for the job,
    usually one with hardware enco/decoding support, or VP8. This
    CL forces it to be e.g. h264 since it's the most pervasive,
    wiring it down to the decodePerf() function like its sibling
    RunPeerConnection() [1] does, so measurements are comparable.
    
    This also fixes an issue inadvertently introduced by my CL
    crrev.com/c/1898721, where the logic changed: before the CL
    the hw decode was disabled, and after it was forced-enabled
    and expected; before the CL if there was no support for Hw
    decoding, we'd just measure twice the sw one, whereas after
    the CL, we'll try with both hw and sw, and skip the measure
    if the expectation is not met.
    
    [1] https://chromium.git.corp.google.com/chromiumos/platform/tast-tests/+/1a9cde86fd71ad0a8074674fbd82008069e9deac/src/chromiumos/tast/local/bundles/cros/webrtc/peerconnection/peer_connection.go#34
    
    BUG=chromium:1028414
    TEST=tast -verbose run papaya webrtc.DecodePerf.* on candy (BYT)
    
    Change-Id: Icfb3ab88e134bd0c4dc4819eacf18ca0f51da2ad

--- src/chromiumos/tast/local/bundles/cros/webrtc/decode_perf.go
+++ src/chromiumos/tast/local/bundles/cros/webrtc/decode_perf.go
@@ -18,10 +18,21 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         DecodePerf,
 		Desc:         "Measures WebRTC decode performance in terms of CPU usage and decode time with and without hardware acceleration",
-		Contacts:     []string{"hiroh@chromium.org", "mcasas@chromium.org", "chromeos-video-eng@google.com"},
+		Contacts:     []string{
+			"hiroh@chromium.org",
+			"mcasas@chromium.org",
+			"chromeos-gfx-video@google.com",
+			"chromeos-video-eng@google.com",
+		},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome"},
 		Data:         append(webrtc.LoopbackDataFiles(), "crowd720_25frames.y4m", webrtc.AddStatsJSFile),
+		Params: []testing.Param{{
+			Name:              "h264",
+			Val:               "VP8",
+			// "chrome_internal" is needed because H.264 is a proprietary codec.
+			ExtraSoftwareDeps: []string{"chrome_internal"},
+		}},
 		// Default timeout (i.e. 2 minutes) is not enough.
 		Timeout: 10 * time.Minute,
 	})
@@ -33,7 +44,7 @@ func DecodePerf(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to read JS for gathering decode time: ", err)
 	}
-	peerconnection.RunDecodePerf(ctx, s, "crowd720_25frames.y4m", peerconnection.MeasureConfig{
+	peerconnection.RunDecodePerf(ctx, s, s.Param().(string), "crowd720_25frames.y4m", peerconnection.MeasureConfig{
 		CPUStabilize:      10 * time.Second,
 		CPUMeasure:        30 * time.Second,
 		DecodeTimeTimeout: 30 * time.Second,
