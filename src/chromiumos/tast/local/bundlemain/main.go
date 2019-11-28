ed84d78848a91a9e9a53c56bd85a25c5b773609e
Victor-Gabriel Savu <vsavu@google.com>
2019-11-28 01:37:35 +0000

    tast-tests: split /var/log/messages for each test run
    Split /var/log/messages by copying the section that was generated during
    one test to its output directory. This makes it easy to check log output
    when multiple tests are executed.
    
    Since faillog was removed also handle it here.
    
    TEST=manual
    BUG=none
    
    Cq-Depend: chromium:1940322
    Change-Id: I7b43192be34e3df78ba0c2e8dc7a4431e77f06ce

--- src/chromiumos/tast/local/bundlemain/main.go
+++ src/chromiumos/tast/local/bundlemain/main.go
@@ -10,17 +10,115 @@
 package bundlemain
 
 import (
+	"context"
+	"io"
 	"os"
+	"path/filepath"
 
 	"chromiumos/tast/bundle"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/faillog"
 	"chromiumos/tast/local/ready"
+	"chromiumos/tast/testing"
 )
+
+const varLogMessages = "/var/log/messages"
+
+func copyLogs(ctx context.Context, oldInfo os.FileInfo, outDir string) error {
+	dp := filepath.Join(outDir, "messages")
+
+	df, err := os.Create(dp)
+	if err != nil {
+		return errors.Wrapf(err, "failed to write log: failed to create %s", dp)
+	}
+	defer df.Close()
+
+	sf, err := os.Open(varLogMessages)
+	if err != nil {
+		return errors.Wrapf(err, "failed to read log: failed to open %s", varLogMessages)
+	}
+	defer sf.Close()
+
+	info, err := os.Stat(varLogMessages)
+	if err != nil {
+		return errors.Wrapf(err, "failed reading log position: failed to stat %s", varLogMessages)
+	}
+
+	if os.SameFile(info, oldInfo) {
+		// If the file has not rotated just copy everything since the test started.
+		if _, err = sf.Seek(oldInfo.Size(), 0); err != nil {
+			return errors.Wrapf(err, "failed to read log: failed to seek %s", varLogMessages)
+		}
+
+		if _, err = io.Copy(df, sf); err != nil {
+			return errors.Wrapf(err, "failed to write log: failed to copy %s", varLogMessages)
+		}
+	} else {
+		// If the log has rotated copy the old file from where the test started and then copy the entire new file.
+		// We assume that the log does not rotate twice during one test.
+		// If we fail to open the older log, we still copy the newer one.
+		previousLog := varLogMessages + ".1"
+
+		sfp, err := os.Open(previousLog)
+		if err != nil {
+			_, _ = io.Copy(df, sf)
+
+			return errors.Wrapf(err, "failed to read log: failed to open %s", previousLog)
+		}
+		defer sfp.Close()
+
+		if _, err = sfp.Seek(oldInfo.Size(), 0); err != nil {
+			_, _ = io.Copy(df, sf)
+
+			return errors.Wrapf(err, "failed to read log: failed to seek %s", previousLog)
+		}
+
+		// Copy previous log
+		if _, err = io.Copy(df, sfp); err != nil {
+			_, _ = io.Copy(df, sf)
+
+			return errors.Wrapf(err, "failed to write log: failed to copy previous %s", previousLog)
+		}
+
+		// Copy current log
+		if _, err = io.Copy(df, sf); err != nil {
+			return errors.Wrapf(err, "failed to write log: failed to copy current %s", previousLog)
+		}
+	}
+
+	return nil
+}
+
+func preTestRun(ctx context.Context, s *testing.State) func(ctx context.Context, s *testing.State) {
+	// Store the current log state
+	oldInfo, err := os.Stat(varLogMessages)
+	if err != nil {
+		s.Logf("Saving log position: failed to stat %s: %v", varLogMessages, err)
+
+		// Call faillog even if saving the log position failed
+		return func(ctx context.Context, s *testing.State) {
+			if s.HasError() {
+				faillog.Save(ctx)
+			}
+		}
+	}
+
+	return func(ctx context.Context, s *testing.State) {
+		if s.HasError() {
+			faillog.Save(ctx)
+		}
+
+		if err := copyLogs(ctx, oldInfo, s.OutDir()); err != nil {
+			s.Log("Failed to copy logs: ", err)
+			return
+		}
+	}
+}
 
 // Main is an entry point function for bundles.
 func Main() {
 	os.Exit(bundle.Local(os.Args[1:], os.Stdin, os.Stdout, os.Stderr, bundle.LocalDelegate{
-		Ready:   ready.Wait,
-		Faillog: faillog.Save,
+		Ready:      ready.Wait,
+		PreTestRun: preTestRun,
 	}))
 }
