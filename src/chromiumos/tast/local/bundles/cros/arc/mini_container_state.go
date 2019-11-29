1e0008ac542157a22701ad0513f8cb792b47eb00
Shuhei Takahashi <nya@chromium.org>
2019-11-29 18:40:56 +0900

    arc.MiniContainerState: Promote to critical.
    We observe no failures specific to this test recently.
    
    BUG=chromium:1029442
    TEST=fast_build.sh -T
    
    Change-Id: If1d0044be50738e2761f040e9e11adf6f69cb202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1943873
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/mini_container_state.go
+++ src/chromiumos/tast/local/bundles/cros/arc/mini_container_state.go
@@ -21,7 +21,7 @@ func init() {
 			"arc-core@google.com",
 			"hidehiko@chromium.org", // Tast port.
 		},
-		Attr: []string{"group:mainline", "informational"},
+		Attr: []string{"group:mainline"},
 		// TODO(crbug.com/952125): Consider to relax the SoftwareDeps.
 		SoftwareDeps: []string{"android", "chrome"},
 		Timeout:      4 * time.Minute,
