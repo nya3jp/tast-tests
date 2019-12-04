5c6129851807e422afe852e086869918f15afc99
Hidehiko Abe <hidehiko@chromium.org>
2019-12-04 19:28:07 +0900

    Refactor arc.Shutdown to arc.StartStop.
    - Introduces startstop.Subtest, to support table-based like tests.
    - Because the subtest supports PreStart(), it is renamed to arc.StartStop.
    - Introduces Params to split VM and non-VM cases.
    
    Note that, this is simple test code clean up, rather than testing the system
    in a different approach. Thus, we do not need to demote the test once.
    
    BUG=chromium:1030636
    TEST=tast run ${DUT} arc.StartStop
    
    Change-Id: Ifc80f061d89dc97a6d41e47faf4026960a5325df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1950283
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arc/startstop/mount.go
@@ -0,0 +1,64 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package startstop
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/testing"
+)
+
+// TestMount runs inside arc.StartStop.
+type TestMount struct{}
+
+// PreStart implements Subtest.PreStart().
+func (*TestMount) PreStart(ctx context.Context, s *testing.State) {
+	// Do nothing.
+}
+
+// PostStart implements Subtest.PostStart().
+func (*TestMount) PostStart(ctx context.Context, s *testing.State) {
+	// Do nothing.
+}
+
+// PostStop implements Subtest.PostStop(). It makes sure that ARC related
+// mount points are released, except ones for Mini container.
+func (*TestMount) PostStop(ctx context.Context, s *testing.State) {
+	ms, err := sysutil.MountInfoForPID(sysutil.SelfPID)
+	if err != nil {
+		s.Error("Failed to get mount info: ", err)
+		return
+	}
+
+	isARCMount := func(path string) bool {
+		return strings.HasPrefix(path, "/opt/google/containers/android/") ||
+			strings.HasPrefix(path, "/opt/google/containers/arc-") ||
+			strings.HasPrefix(path, "/run/arc/")
+	}
+
+	miniContainerMounts := map[string]struct{}{
+		"/opt/google/containers/android/rootfs/root":                        {},
+		"/opt/google/containers/arc-obb-mounter/mountpoints/container-root": {},
+		"/opt/google/containers/arc-sdcard/mountpoints/container-root":      {},
+		"/run/arc/adbd":            {},
+		"/run/arc/debugfs/tracing": {},
+		"/run/arc/media":           {},
+		"/run/arc/obb":             {},
+		"/run/arc/oem":             {},
+		"/run/arc/sdcard":          {},
+		"/run/arc/shared_mounts":   {},
+	}
+
+	for _, m := range ms {
+		if !isARCMount(m.MountPath) {
+			continue
+		}
+		if _, ok := miniContainerMounts[m.MountPath]; !ok {
+			s.Error("Mountpoint leaked after logout: ", m.MountPath)
+		}
+	}
+}
