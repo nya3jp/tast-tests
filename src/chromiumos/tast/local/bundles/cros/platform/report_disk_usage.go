85c816c04680bc74ed5327e8c42e0c446b101bff
Nicolas Norvez <norvez@chromium.org>
2019-12-05 15:01:44 -0800

    tast-tests: add more metrics to ReportDiskUsage
    ReportDiskUsage is currently reporting only the overall size of the OS.
    This CL adds more metrics to allow us to monitor specific directories
    that tend to be the largest and the more prone to sudden growth, to make
    it easier to pinpoint which specific components have grown.
    
    Notably we now report the size of:
    - Chrome
    - ARC++ (if present)
    - Fonts
    - TTS
    - IME
    - system libraries and binaries
    
    BUG=chromium:1030975
    TEST=./platform/tast/fast_build.sh -T
    TEST=tast run $DUT_IP platform.ReportDiskUsage
    TEST=verify that
    tast/results/latest/tests/platform.ReportDiskUsage/results-chart.json
    reports the correct values
    TEST=run the test on both x86_64 and ARM devices (nocturne and hana)
    TEST=run the test on a DUT without ARC++ (banjo), verify that the
    'bytes_arc' metric is not present in results-chart.json
    TEST=run the test on eve-arcvm
    TEST=run the test with a typo in a directory name, verify that 'du'
    fails and its output is dumped to the log
    
    Change-Id: Ia7e9551f7a8c0addca4151313ddb1bdbd1d5061b

--- src/chromiumos/tast/local/bundles/cros/platform/report_disk_usage.go
+++ src/chromiumos/tast/local/bundles/cros/platform/report_disk_usage.go
@@ -8,10 +8,15 @@ import (
 	"bytes"
 	"context"
 	"io/ioutil"
+	"runtime"
 	"strconv"
+	"strings"
 
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/bundles/cros/platform/fsinfo"
 	"chromiumos/tast/local/perf"
+	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
@@ -55,5 +60,85 @@ func ReportDiskUsage(ctx context.Context, s *testing.State) {
 			Unit:      "bytes",
 			Direction: perf.SmallerIsBetter,
 		}, float64(info.Used))
+	}
+
+	// Report the size of specific directories that are particularly large.
+	metrics := map[string]string{
+		"/opt/":                       "bytes_opt",
+		"/opt/google/chrome/":         "bytes_chrome",
+		"/usr/bin":                    "bytes_bin",
+		"/usr/sbin":                   "bytes_sbin",
+		"/usr/share/chromeos-assets/": "bytes_assets",
+		"/usr/share/chromeos-assets/input_methods/":    "bytes_ime",
+		"/usr/share/chromeos-assets/speech_synthesis/": "bytes_tts",
+		"/usr/share/fonts/":                            "bytes_fonts",
+	}
+	switch arch := runtime.GOARCH; arch {
+	case "amd64":
+		metrics["/usr/lib64/"] = "bytes_lib"
+	case "arm64":
+		metrics["/usr/lib64/"] = "bytes_lib"
+	case "arm":
+		metrics["/usr/lib/"] = "bytes_lib"
+	default:
+		s.Errorf("unsupported architecture %q", arch)
+	}
+	if arc.Supported() {
+		t, ok := arc.Type()
+		if ok {
+			switch t {
+			case arc.Container:
+				metrics[arc.ARCPath] = "bytes_arc"
+			case arc.VM:
+				metrics[arc.ARCVMPath] = "bytes_arc"
+			default:
+				s.Errorf("unsupported ARC type %d", t)
+			}
+		} else {
+			s.Error("Failed to detect ARC type")
+		}
+	}
+
+	// Find the space used in a directory, in bytes.
+	//
+	// This function uses 'du' to find the size of directories on a live DUT.
+	// In practice this test will run on "test" images that are ever so
+	// slightly different from the "base" image, so the results could be
+	// slightly different from production images. However:
+	// - the difference is tiny, overall size difference between base and
+	// test images is <100KB
+	// - if anything the size will be slightly larger on the test image, so
+	// we're monitoring the worst case scenario anyway
+	dirSize := func(path string) (int64, error) {
+		cmd := testexec.CommandContext(ctx, "du", "-s", "-B1", "-x", path)
+		out, err := cmd.Output(testexec.DumpLogOnError)
+		if err != nil {
+			return 0, errors.Wrap(err, "du command failed")
+		}
+		// Parse the output from a "du -s -B1 -x <path>" command.
+		//
+		// The output is expected to have the following form:
+		// 419024896	/opt/google/chrome/
+		fields := strings.Fields(strings.TrimSpace(string(out)))
+		if len(fields) != 2 {
+			return 0, errors.Errorf("expected 2 fields in line %q", string(out))
+		}
+		size, err := strconv.ParseInt(fields[0], 10, 64)
+		if err != nil {
+			return 0, errors.Errorf("failed to parse value %q", fields[0])
+		}
+		return size, nil
+	}
+
+	for k, v := range metrics {
+		if size, err := dirSize(k); err != nil {
+			s.Errorf("Failed to get the size of directory %q: %v", k, err)
+		} else {
+			pv.Set(perf.Metric{
+				Name:      v,
+				Unit:      "bytes",
+				Direction: perf.SmallerIsBetter,
+			}, float64(size))
+		}
 	}
 }
