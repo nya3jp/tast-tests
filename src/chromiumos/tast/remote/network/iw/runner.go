e05b23f024e8aa318dfaaf0b523cde8433d1a8a4
Yen-lin Lai <yenlinlai@google.com>
2019-12-09 15:48:45 +0800

    network: iw: support iw utilities in both local and remote
    Move common parsing logic to chromiumos/tast/common. Also allow iw
    runner to run through CmdRunner interface and create local (testexec)
    and remote (dut.DUT or host.SSH) implementation in corresponding
    packages.
    
    BUG=chromium:972833
    TEST=fast_build.sh -T
    TEST=tast run $DUT network.IWScan network.IWGetSet network.WifiCaps
    
    Change-Id: Ib189b2749d84fd60cc4ba28ac421feefc7cc7f82

---
+++ src/chromiumos/tast/remote/network/iw/runner.go
@@ -0,0 +1,41 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package iw contains utility functions to wrap around the iw program.
+package iw
+
+import (
+	"context"
+
+	"chromiumos/tast/common/network/iw"
+	"chromiumos/tast/host"
+)
+
+// Commander is a temporary shared interface for dut and general ssh (e.g. router).
+// TODO(crbug.com/1019537): use a more suitable ssh object.
+type Commander interface {
+	Command(string, ...string) *host.Cmd
+}
+
+type remoteCmdRunner struct {
+	host Commander
+}
+
+func (r *remoteCmdRunner) Run(ctx context.Context, cmd string, args ...string) error {
+	return r.host.Command(cmd, args...).Run(ctx)
+}
+
+func (r *remoteCmdRunner) Output(ctx context.Context, cmd string, args ...string) ([]byte, error) {
+	return r.host.Command(cmd, args...).Output(ctx)
+}
+
+// Runner is an alias for common iw Runner but only for remote execution.
+type Runner = iw.Runner
+
+// NewRunner creates a iw runner for remote execution.
+func NewRunner(hst Commander) *Runner {
+	return iw.NewRunner(&remoteCmdRunner{
+		host: hst,
+	})
+}
