nya3jp/tast-tests

commit 24860050a0686c1bc8904185021c999d2b9027a7

tast: hardware: iio: Add support for cros_ec_sensorhub
Newer kernel adds a kernel module cros_ec_sensorhub that will replace
cros_ec_sensor_ring. These changes also include a sysfs ABI refresh to
conform with sysfs IIO ABI (CL:1834451, CL:1834449, CL:1834448).
cros_ec_sensor_ring is still presented (CL:1834452), but ultimately it
will be removed and the ring test will need another change.

BUG=chromium:510242
TEST=tast -verbose run <IP> hardware.SensorRing pass on eve and nocturne
with new kernel.

Change-Id: I8c2cead25652c1bdd048e097ba4d2702e54e5c8f
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1836018
Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

src/chromiumos/tast/local/bundles/cros/hardware/iio/ring.go
@@ -6,6 +6,7 @@ package iio
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 	"time"
 
@@ -187,7 +188,14 @@ func (cr *CrosRing) Close() error {
 // only be called after the ring buffer is open because all sensors will be disabled
 // when the ring is opened.
 func (s *ringSensor) Enable(sensorFreq, interruptFreq int) error {
-	err := s.Sensor.WriteAttr("frequency", strconv.Itoa(sensorFreq))
+	var err error
+
+	if s.Sensor.OldSysfsStyle {
+		err = s.Sensor.WriteAttr("frequency", strconv.Itoa(sensorFreq))
+	} else {
+		err = s.Sensor.WriteAttr("sampling_frequency", fmt.Sprintf(
+			"%d.%03d", sensorFreq/1000, sensorFreq%1000))
+	}
 	if err != nil {
 		return errors.Wrapf(err, "error setting frequency of %v %v to %v",
 			s.Sensor.Location, s.Sensor.Name, sensorFreq)
@@ -199,7 +207,13 @@ func (s *ringSensor) Enable(sensorFreq, interruptFreq int) error {
 		interruptPeriod = 1e6 / interruptFreq
 	}
 
-	err = s.Sensor.WriteAttr("sampling_frequency", strconv.Itoa(interruptPeriod))
+	if s.Sensor.OldSysfsStyle {
+		err = s.Sensor.WriteAttr("sampling_frequency", strconv.Itoa(interruptPeriod))
+	} else {
+		err = s.Sensor.WriteAttr("buffer/hwfifo_timeout", fmt.Sprintf(
+			"%d.%03d", interruptPeriod/1000, interruptPeriod%1000))
+	}
+
 	if err != nil {
 		return errors.Wrapf(err, "error setting sampling_frequency of %v %v to %v",
 			s.Sensor.Location, s.Sensor.Name, interruptPeriod)

src/chromiumos/tast/local/bundles/cros/hardware/iio/iio.go
@@ -28,13 +28,14 @@ type SensorLocation string
 
 // Sensor represents one sensor on the DUT.
 type Sensor struct {
-	Name         SensorName
-	Location     SensorLocation
-	Path         string
-	ID           uint
-	Scale        float64
-	MinFrequency int
-	MaxFrequency int
+	Name          SensorName
+	Location      SensorLocation
+	Path          string
+	ID            uint
+	Scale         float64
+	MinFrequency  int
+	MaxFrequency  int
+	OldSysfsStyle bool
 }
 
 // SensorReading is one reading from a sensor.
@@ -144,6 +145,7 @@ func parseSensor(devName string) (*Sensor, error) {
 	var name SensorName
 	var id, minFreq, maxFreq int
 	var scale float64
+	var zeroInt, zeroFrac, minInt, minFrac, maxInt, maxFrac int
 
 	re := regexp.MustCompile(`^iio:device[0-9]+$`)
 	if !re.MatchString(devName) {
@@ -193,19 +195,42 @@ func parseSensor(devName string) (*Sensor, error) {
 		}
 	}
 
-	f, err := sensor.ReadAttr("min_frequency")
-	if err == nil {
-		minFreq, err = strconv.Atoi(f)
-		if err != nil {
-			return nil, errors.Wrapf(err, "invalid min frequency %q", f)
-		}
-	}
+	_, err = sensor.ReadAttr("frequency")
+	sensor.OldSysfsStyle = err == nil
 
-	f, err = sensor.ReadAttr("max_frequency")
-	if err == nil {
-		maxFreq, err = strconv.Atoi(f)
-		if err != nil {
-			return nil, errors.Wrapf(err, "invalid max frequency %q", f)
+	if sensor.OldSysfsStyle {
+		f, err := sensor.ReadAttr("min_frequency")
+		if err == nil {
+			minFreq, err = strconv.Atoi(f)
+			if err != nil {
+				return nil, errors.Wrapf(err, "invalid min frequency %q", f)
+			}
+		}
+
+		f, err = sensor.ReadAttr("max_frequency")
+		if err == nil {
+			maxFreq, err = strconv.Atoi(f)
+			if err != nil {
+				return nil, errors.Wrapf(err, "invalid max frequency %q", f)
+			}
+		}
+	} else {
+		f, err := sensor.ReadAttr("sampling_frequency_available")
+		if err == nil {
+			_, err = fmt.Sscanf(f, "%d.%06d %d.%06d %d.%06d",
+				&zeroInt, &zeroFrac, &minInt, &minFrac, &maxInt, &maxFrac)
+			if err != nil {
+				return nil, errors.Wrapf(err, "invalid frequency range %q", f)
+			}
+			if zeroInt != 0 || zeroFrac != 0 {
+				return nil, errors.Wrapf(err, "frequency range must start with 0 %q", f)
+			}
+			// In this code, frequency unit is mHz. iio now reports frequency in Hz with
+			// 6 digits of precision.
+			// So 12.5Hz will be printed 12.500000.
+			// Int will be 12, Frac 500000.
+			minFreq = minInt*1000 + minFrac/1000
+			maxFreq = maxInt*1000 + maxFrac/1000
 		}
 	}
 

src/chromiumos/tast/local/bundles/cros/hardware/iio/iio_test.go
@@ -15,18 +15,20 @@ import (
 
 func TestGetSensors(t *testing.T) {
 	defer setupTestFiles(t, map[string]string{
-		"bad:device/name":           "bad",
-		"iio:device0/name":          "cros-ec-accel",
-		"iio:device0/location":      "lid",
-		"iio:device0/id":            "0",
-		"iio:device0/scale":         "0.25",
-		"iio:device0/min_frequency": "100",
-		"iio:device0/max_frequency": "1000",
-		"iio:device1/name":          "cros-ec-gyro",
-		"iio:device1/location":      "base",
-		"iio:device1/id":            "1",
-		"iio:device2/name":          "cros-ec-unknown",
-		"iio:device3/name":          "cros-ec-ring",
+		"bad:device/name":                   "bad",
+		"iio:device0/name":                  "cros-ec-accel",
+		"iio:device0/location":              "lid",
+		"iio:device0/id":                    "0",
+		"iio:device0/scale":                 "0.25",
+		"iio:device0/frequency":             "100",
+		"iio:device0/min_frequency":         "100",
+		"iio:device0/max_frequency":         "1000",
+		"iio:device1/name":                  "cros-ec-gyro",
+		"iio:device1/location":              "base",
+		"iio:device1/id":                    "1",
+		"iio:device1/buffer/hwfifo_timeout": "0",
+		"iio:device2/name":                  "cros-ec-unknown",
+		"iio:device3/name":                  "cros-ec-ring",
 	})()
 
 	sensors, err := GetSensors()
@@ -35,9 +37,9 @@ func TestGetSensors(t *testing.T) {
 	}
 
 	expected := []*Sensor{
-		{Accel, Lid, "iio:device0", 0, .25, 100, 1000},
-		{Gyro, Base, "iio:device1", 1, 0, 0, 0},
-		{Ring, None, "iio:device3", 0, 0, 0, 0},
+		{Accel, Lid, "iio:device0", 0, .25, 100, 1000, true},
+		{Gyro, Base, "iio:device1", 1, 0, 0, 0, false},
+		{Ring, None, "iio:device3", 0, 0, 0, 0, false},
 	}
 
 	if !reflect.DeepEqual(expected, sensors) {

src/chromiumos/tast/local/bundles/cros/hardware/iio/ring_test.go
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package iio
 
 import (
@@ -39,14 +43,15 @@ func TestRing(t *testing.T) {
 		"iio:device1/location":                            "lid",
 		"iio:device1/id":                                  "0",
 		"iio:device1/scale":                               "0.25",
+		"iio:device1/frequency":                           "100",
 		"iio:device1/min_frequency":                       "100",
 		"iio:device1/max_frequency":                       "1000",
 		"iio:device2/name":                                "cros-ec-gyro",
 		"iio:device2/location":                            "base",
 		"iio:device2/id":                                  "1",
 		"iio:device2/scale":                               "0.01",
-		"iio:device2/min_frequency":                       "2000",
-		"iio:device2/max_frequency":                       "5000",
+		"iio:device2/sampling_frequency_available":        "0.000000 2000.000000 5000.000000",
+		"iio:device2/buffer/hwfifo_timeout":               "0",
 	})()
 
 	ringData := []SensorReading{
@@ -116,7 +121,7 @@ func TestRing(t *testing.T) {
 	}
 	defer ring.Close()
 
-	read := []SensorReading{}
+	var read []SensorReading
 	timeout := time.After(5 * time.Second)
 l:
 	for {

commit 1140049e39f43407b342057a7648a11a57ddc3b9

tast: hardware: iio: keep err a short declaration operator
err should be short-declared as documented in
https://www.godesignpatterns.com/2014/04/assignment-vs-short-variable-declaration.html

Fixes: 24860050a0686 (tast: hardware: iio: Add support for cros_ec_sensorhub)

BUG=None
TEST=Run tast --verbose run <IP> hardware.SensorRing

Change-Id: Ic2ded7f26b930dccc9e7e18a2049d82c57542ff1

src/chromiumos/tast/local/bundles/cros/hardware/iio/ring.go
@@ -193,17 +193,17 @@ func (cr *CrosRing) Close() error {
 // only be called after the ring buffer is open because all sensors will be disabled
 // when the ring is opened.
 func (s *ringSensor) Enable(sensorFreq, interruptFreq int) error {
-	var err error
-
 	if s.Sensor.OldSysfsStyle {
-		err = s.Sensor.WriteAttr("frequency", strconv.Itoa(sensorFreq))
+		if err := s.Sensor.WriteAttr("frequency", strconv.Itoa(sensorFreq)); err != nil {
+			return errors.Wrapf(err, "error setting frequency of %v %v to %v",
+				s.Sensor.Location, s.Sensor.Name, sensorFreq)
+		}
 	} else {
-		err = s.Sensor.WriteAttr("sampling_frequency", fmt.Sprintf(
-			"%d.%03d", sensorFreq/1000, sensorFreq%1000))
-	}
-	if err != nil {
-		return errors.Wrapf(err, "error setting frequency of %v %v to %v",
-			s.Sensor.Location, s.Sensor.Name, sensorFreq)
+		if err := s.Sensor.WriteAttr("sampling_frequency", fmt.Sprintf(
+			"%d.%03d", sensorFreq/1000, sensorFreq%1000)); err != nil {
+			return errors.Wrapf(err, "error setting frequency of %v %v to %v",
+				s.Sensor.Location, s.Sensor.Name, sensorFreq)
+		}
 	}
 
 	// sampling_frequency takes ms
@@ -213,18 +213,17 @@ func (s *ringSensor) Enable(sensorFreq, interruptFreq int) error {
 	}
 
 	if s.Sensor.OldSysfsStyle {
-		err = s.Sensor.WriteAttr("sampling_frequency", strconv.Itoa(interruptPeriod))
+		if err := s.Sensor.WriteAttr("sampling_frequency", strconv.Itoa(interruptPeriod)); err != nil {
+			return errors.Wrapf(err, "error setting sampling_frequency of %v %v to %y3yv",
+				s.Sensor.Location, s.Sensor.Name, interruptPeriod)
+		}
 	} else {
-		err = s.Sensor.WriteAttr("buffer/hwfifo_timeout", fmt.Sprintf(
-			"%d.%03d", interruptPeriod/1000, interruptPeriod%1000))
+		if err := s.Sensor.WriteAttr("buffer/hwfifo_timeout", fmt.Sprintf(
+			"%d.%03d", interruptPeriod/1000, interruptPeriod%1000)); err != nil {
+			return errors.Wrapf(err, "error setting sampling_frequency of %v %v to %y3yv",
+				s.Sensor.Location, s.Sensor.Name, interruptPeriod)
+		}
 	}
-
-	if err != nil {
-		return errors.Wrapf(err, "error setting sampling_frequency of %v %v to %v",
-			s.Sensor.Location, s.Sensor.Name, interruptPeriod)
-	}
-
-	return nil
 }
 
 // Disable will stop a sensor from colleting data by setting the ODR and interrupt