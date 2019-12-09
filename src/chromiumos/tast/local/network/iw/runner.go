ca0c587500696393a4a64f56360335dda6f36abe
Yen-lin Lai <yenlinlai@google.com>
2019-12-09 15:48:45 +0800

    network: iw: support iw utilities in both local and remote
    Move common parsing logic to chromiumos/tast/common. Also allow iw
    runner to run through CmdRunner interface and create local (testexec)
    and remote (dut.DUT or host.SSH) implementation in corresponding
    packages.
    
    BUG=chromium:972833
    TEST=fast_build.sh -T
    TEST=tast run $DUT network.IWScan network.IWGetSet network.WifiCaps
    
    Change-Id: Ib189b2749d84fd60cc4ba28ac421feefc7cc7f82

---
+++ src/chromiumos/tast/local/network/iw/runner.go
@@ -0,0 +1,34 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package iw contains utility functions to wrap around the iw program.
+package iw
+
+import (
+	"context"
+
+	"chromiumos/tast/common/network/iw"
+	"chromiumos/tast/local/testexec"
+)
+
+// localCmdRunner implements iw.CmdRunner interface.
+type localCmdRunner struct{}
+
+var _ iw.CmdRunner = (*localCmdRunner)(nil)
+
+func (r *localCmdRunner) Run(ctx context.Context, cmd string, args ...string) error {
+	return testexec.CommandContext(ctx, cmd, args...).Run(testexec.DumpLogOnError)
+}
+
+func (r *localCmdRunner) Output(ctx context.Context, cmd string, args ...string) ([]byte, error) {
+	return testexec.CommandContext(ctx, cmd, args...).Output(testexec.DumpLogOnError)
+}
+
+// Runner is an alias for common iw Runner but only for local execution.
+type Runner = iw.Runner
+
+// NewRunner creates a iw runner for local execution.
+func NewRunner() *Runner {
+	return iw.NewRunner(&localCmdRunner{})
+}
