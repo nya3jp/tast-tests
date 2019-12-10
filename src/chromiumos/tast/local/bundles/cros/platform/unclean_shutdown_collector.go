1dfa83d9e3c144455d31d4d37beaf58def6b6b88
Joon Ahn <joonbug@chromium.org>
2019-12-10 15:47:14 -0800

    Add intergration test for UncleanShutdownCollector
    BUG=chromium:983725
    TEST=tast --verbose run $IP platform.UncleanShutdownCollector
    
    Change-Id: Ib34d73cde7be1f4c40ae61563f3052fb8f89972e

---
+++ src/chromiumos/tast/local/bundles/cros/platform/unclean_shutdown_collector.go
@@ -0,0 +1,94 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"encoding/binary"
+	"os"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     UncleanShutdownCollector,
+		Desc:     "Verify unclean shutdown produces collection",
+		Contacts: []string{"joonbug@chromium.org", "cros-monitoring-forensics@google.com"},
+		Attr:     []string{"informational"},
+	})
+}
+
+func getUncleanShutdownCount(ctx context.Context, s *testing.State) uint64 {
+	const metricsFile = "/var/lib/metrics/Platform.UncleanShutdownsDaily"
+	numUnclean := make([]byte, 8)
+
+	f, err := os.Open(metricsFile)
+	if err != nil {
+		s.Fatal("Failed to open metrics file: ", err)
+	}
+
+	f.Seek(4, 0)
+	f.Read(numUnclean)
+	f.Close()
+
+	return binary.LittleEndian.Uint64(numUnclean)
+}
+
+func UncleanShutdownCollector(ctx context.Context, s *testing.State) {
+	const uncleanShutdownDetectedFile = "/run/metrics/external/crash-reporter/unclean-shutdown-detected"
+
+	unclean := getUncleanShutdownCount(ctx, s)
+	s.Log("Current unclean count: ", unclean)
+
+	// check if file exists
+	var _, err = os.Stat(uncleanShutdownDetectedFile)
+
+	// create file if not exists
+	if os.IsNotExist(err) {
+		var f, err = os.Create(uncleanShutdownDetectedFile)
+		if err != nil {
+			s.Fatal("Failed to fake an unclean shutdown: ", err)
+		}
+
+		f.Close()
+	}
+
+	s.Log("Restarting metrics_daemon")
+	cmd := testexec.CommandContext(ctx, "pkill", "metrics_daemon")
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Failed to restart metrics_daemon: ", err)
+	}
+
+	if err := testing.Sleep(ctx, 10000); err != nil {
+		s.Fatal("Could not trigger sleep")
+	}
+
+	// Wait for uncleanShutdownDetectedFile to be consumed by metrics daemon
+	if err := testing.Poll(ctx, func(c context.Context) error {
+		// check if file exists
+		var _, err = os.Stat(uncleanShutdownDetectedFile)
+
+		if os.IsNotExist(err) {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+		return errors.New("Unclean shutdown file is still there")
+	}, &testing.PollOptions{Timeout: 10 * time.Second}); err != nil {
+		s.Fatal("Could not wait for unclean shutdown to be detected: ", err)
+	}
+
+	newUnclean := getUncleanShutdownCount(ctx, s)
+
+	if newUnclean != unclean+1 {
+		s.Fatal("Unclean shutdown was logged incorrectly. Count should be ", unclean+1, " but got ", newUnclean)
+	}
+}
