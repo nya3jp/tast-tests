b374b396d850c236afcd9f3d50716bb8a22f748e
Nicholas Hollingum <hollingum@google.com>
2019-12-11 09:13:36 +1100

    tast-tests: crostini: promote crostini.Sanity.artifact to critical
    We have made several fixes to the underlying infrastructure, currently
    the failures in the last 20 CQ runs are caused by:
     - Expected failures on boards which do not run in the cq (kevin 64
       bit).
     - Tast-related failures (lab outages?) e.g.:
       https://stainless.corp.google.com/browse/chromeos-autotest-results/swarming-49023c7ad1f7ae10
    
    We feel the crostini test is suitable for promotion to the CQ.
    
    BUG=none
    TEST=tast run localhost:33333 crostini.Sanity.artifact
    
    Change-Id: I34901f0537899868cb4cdb0bad0a18f2f853ef55

--- src/chromiumos/tast/local/bundles/cros/crostini/sanity.go
+++ src/chromiumos/tast/local/bundles/cros/crostini/sanity.go
@@ -18,7 +18,7 @@ func init() {
 		Desc:         "Tests basic Crostini startup only (where crostini was shipped with the build)",
 		Contacts:     []string{"smbarber@chromium.org", "cros-containers-dev@google.com"},
 		SoftwareDeps: []string{"chrome", "vm_host"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		Params: []testing.Param{
 			{
 				Name:      "artifact",
@@ -27,20 +27,23 @@ func init() {
 				ExtraData: []string{crostini.ImageArtifact},
 			},
 			{
-				Name:    "download",
-				Pre:     crostini.StartedByDownload(),
-				Timeout: 10 * time.Minute,
+				Name:      "download",
+				Pre:       crostini.StartedByDownload(),
+				Timeout:   10 * time.Minute,
+				ExtraAttr: []string{"informational"},
 			},
 			{
-				Name:    "download_buster",
-				Pre:     crostini.StartedByDownloadBuster(),
-				Timeout: 10 * time.Minute,
+				Name:      "download_buster",
+				Pre:       crostini.StartedByDownloadBuster(),
+				Timeout:   10 * time.Minute,
+				ExtraAttr: []string{"informational"},
 			},
 			{
 				Name:      "installer",
 				Pre:       crostini.StartedByInstaller(),
 				Timeout:   7 * time.Minute,
 				ExtraData: []string{crostini.ImageArtifact},
+				ExtraAttr: []string{"informational"},
 			},
 		},
 	})
