2bbfe2018fb91048aa139b84124d292913da71a5
Nicolas Norvez <norvez@chromium.org>
2019-12-10 10:11:19 -0800

    security.Mtab: expect Quickoffice mount
    To save space on the rootfs, we are now compressing the Quickoffice NaCl
    binaries in a squashfs file that gets mounted on Chrome startup, see
    crrev.com/c/1933934
    
    Add the new mountpoint to security.Mtab.
    
    BUG=chromium:1025939
    BUG=chromium:1013288
    TEST=./platform/tast/fast_build.sh -T
    TEST=tast run ${EVE_DUT_IP} security.Mtab
    
    Change-Id: Ib3c4483a927fb92f290376527aabaed076642fba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1959847
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Nicolas Norvez <norvez@chromium.org>
    Tested-by: Nicolas Norvez <norvez@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/security/mtab.go
+++ src/chromiumos/tast/local/bundles/cros/security/mtab.go
@@ -123,10 +123,11 @@ func Mtab(ctx context.Context, s *testing.State) {
 		"/sys/kernel/debug/tracing":          {nil, "tracefs", defaultRW},
 		"/sys/kernel/security":               {nil, "securityfs", defaultRW},
 		"/tmp":                               {nil, "tmpfs", defaultRW},
-		"/usr/share/oem":                     {nil, "ext4", defaultRO},
-		"/var":                               {nil, "ext4", defaultRW},
-		"/var/lock":                          {nil, "tmpfs", defaultRW},               // duplicate of /run/lock
-		"/var/run":                           {nil, "tmpfs", defaultRW + ",mode=755"}, // duplicate of /run
+		"/usr/share/chromeos-assets/quickoffice/_platform_specific": {loopDev, "squashfs", defaultRO},
+		"/usr/share/oem": {nil, "ext4", defaultRO},
+		"/var":           {nil, "ext4", defaultRW},
+		"/var/lock":      {nil, "tmpfs", defaultRW},               // duplicate of /run/lock
+		"/var/run":       {nil, "tmpfs", defaultRW + ",mode=755"}, // duplicate of /run
 	}
 
 	// Moblab devices mount external USB storage devices at several locations.
