f13805dca92b11911d4827485502bab4a2970ac8
Yen-lin Lai <yenlinlai@google.com>
2019-12-10 17:56:00 +0800

    network: ping: add ping runner for remote test
    Ping is a convenient tool to check for network connectivity. Add a
    utility for ping command. This will later be used in wifi connection
    tests.
    
    BUG=chromium:939214
    TEST=fast_build.sh -T
    
    Change-Id: I4cb3f8535ffaba7b5a35e330961867df9a7e485a

---
+++ src/chromiumos/tast/remote/network/ping/runner.go
@@ -0,0 +1,199 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package ping contains utility functions to wrap around the ping program.
+package ping
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+)
+
+const (
+	pingCmd = "ping"
+)
+
+// QOSType is a enum type for ping QOS option.
+type QOSType int
+
+// Enum of QOSType.
+const (
+	QOSBK QOSType = 0x02
+	QOSBE QOSType = 0x04
+	QOSVI QOSType = 0x08
+	QOSVO QOSType = 0x10
+)
+
+// config is a struct that contains the ping command parameters.
+type config struct {
+	BindAddress bool
+	Count       int
+	Size        int
+	Interval    float64
+	QOS         QOSType
+	SourceIface string
+}
+
+// Option is a function used to configure ping command.
+type Option func(c *config)
+
+// Result is a struct that contains a successful ping's statistics.
+type Result struct {
+	Sent       int
+	Received   int
+	Loss       float64
+	MinLatency float64
+	AvgLatency float64
+	MaxLatency float64
+	DevLatency float64
+}
+
+// BindAddress returns an Option that can be passed to Ping to disallow ping
+// from changing source address.
+func BindAddress(bind bool) Option {
+	return func(c *config) { c.BindAddress = bind }
+}
+
+// Count returns an Option that can be passed to Ping func to set ping count.
+func Count(count int) Option {
+	return func(c *config) { c.Count = count }
+}
+
+// Size returns an Option that can be passed to Ping to set packet size.
+func Size(size int) Option {
+	return func(c *config) { c.Size = size }
+}
+
+// Interval returns an Option that can be passed to Ping to set interval (in seconds).
+func Interval(interval float64) Option {
+	return func(c *config) { c.Interval = interval }
+}
+
+// QOS returns an Option that can be passed to Ping to set QOS type.
+func QOS(qos QOSType) Option {
+	return func(c *config) { c.QOS = qos }
+}
+
+// SourceIface returns an Option that can be passed to Ping to set source interface.
+func SourceIface(iface string) Option {
+	return func(c *config) { c.SourceIface = iface }
+}
+
+// Runner is the object used for run ping command.
+type Runner struct {
+	dut *dut.DUT // TODO(crbug.com/1019537): use a more suitable ssh object, as it'll also be used on router.
+}
+
+// NewRunner creates a ping Runner on the given dut.
+func NewRunner(dut *dut.DUT) *Runner {
+	return &Runner{dut: dut}
+}
+
+// Ping performs a shell ping with parameters specified in Options.
+// If no Option is specified, default config (count=3, interval=0.5s) is used.
+// Notice that when no reply is received, this function will return error as
+// ping exits with code=1 in this case.
+func (r *Runner) Ping(ctx context.Context, targetIP string, options ...Option) (*Result, error) {
+	cfg := &config{Count: 3, Interval: 0.5}
+	for _, opt := range options {
+		opt(cfg)
+	}
+	args, err := cfgToArgs(targetIP, cfg)
+	if err != nil {
+		return nil, err
+	}
+	output, err := r.dut.Command(pingCmd, args...).Output(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "ping command failed")
+	} else if len(output) == 0 {
+		return nil, errors.New("ping returns empty stdout")
+	}
+	return parseOutput(string(output))
+}
+
+// cfgToArgs converts a config into a string of arguments for the ping command.
+func cfgToArgs(targetIP string, cfg *config) ([]string, error) {
+	var args []string
+	if cfg.BindAddress {
+		args = append(args, "-B")
+	}
+	args = append(args, "-c", strconv.Itoa(cfg.Count))
+	if cfg.Size != 0 {
+		args = append(args, "-s", strconv.Itoa(cfg.Size))
+	}
+	if cfg.Interval != 0 {
+		args = append(args, "-i", fmt.Sprintf("%f", cfg.Interval))
+	}
+	if cfg.SourceIface != "" {
+		args = append(args, "-I", cfg.SourceIface)
+	}
+	if cfg.QOS != 0 {
+		args = append(args, "-Q", fmt.Sprintf("0x%x", cfg.QOS))
+	}
+	args = append(args, targetIP)
+	return args, nil
+}
+
+// parseOutput parses the output of `ping` commands into a single Result.
+func parseOutput(out string) (*Result, error) {
+	sentRE := regexp.MustCompile(`(\d+) packets transmitted`)
+	m := sentRE.FindStringSubmatch(out)
+	if len(m) != 2 {
+		return nil, errors.New("parse error on sent packets")
+	}
+	sent, err := strconv.Atoi(m[1])
+	if err != nil {
+		return nil, err
+	}
+
+	receivedRE := regexp.MustCompile(`(\d+) received`)
+	m = receivedRE.FindStringSubmatch(out)
+	if len(m) != 2 {
+		return nil, errors.Errorf("parse error on received packets: %s", out)
+	}
+	recv, err := strconv.Atoi(m[1])
+	if err != nil {
+		return nil, err
+	}
+
+	lossRE := regexp.MustCompile(`(\d+(?:\.\d+)?)% packet loss`)
+	m = lossRE.FindStringSubmatch(out)
+	if len(m) != 2 {
+		return nil, errors.Errorf("parse error on lost packets. matched groups : %d", len(m))
+	}
+	loss, err := strconv.ParseFloat(m[1], 64)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to parse loss=%q to float", m[1])
+	}
+
+	statRE := regexp.MustCompile(`(?:round-trip|rtt) min[^=]*= ` +
+		`(\d+(?:\.\d+)?)/(\d+(?:\.\d+)?)/` +
+		`(\d+(?:\.\d+)?)/(\d+(?:\.\d+)?)`)
+	m = statRE.FindStringSubmatch(out)
+	if len(m) != 5 {
+		return nil, errors.New("parse error on latency statistics")
+	}
+	var stats [4]float64
+	for i, str := range m[1:] {
+		stats[i], err = strconv.ParseFloat(str, 64)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to parse stat=%q to float", str)
+		}
+	}
+
+	return &Result{
+		Sent:       sent,
+		Received:   recv,
+		Loss:       loss,
+		MinLatency: stats[0],
+		AvgLatency: stats[1],
+		MaxLatency: stats[2],
+		DevLatency: stats[3],
+	}, nil
+}
---
+++ src/chromiumos/tast/remote/network/ping/runner_test.go
@@ -0,0 +1,93 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ping
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestOptions(t *testing.T) {
+	cfg := &config{}
+	opts := []Option{
+		BindAddress(true),
+		Count(3),
+		Interval(0.5),
+		Size(100),
+		SourceIface("eth0"),
+		QOS(QOSVI),
+	}
+	expected := &config{
+		BindAddress: true,
+		Count:       3,
+		Interval:    0.5,
+		Size:        100,
+		SourceIface: "eth0",
+		QOS:         QOSVI,
+	}
+	for _, opt := range opts {
+		opt(cfg)
+	}
+	if !reflect.DeepEqual(cfg, expected) {
+		t.Errorf("options apply failed. Got %v, expected %v", cfg, expected)
+	}
+}
+
+func TestCfgToArgs(t *testing.T) {
+	// The interval float parameter will display up to 6 decimal places
+	expected := []string{"-B", "-c", "7", "-s", "3", "-i", "0.123456", "-I", "wlan0", "-Q", "0x10", "1.2.3.4"}
+	cfg := &config{BindAddress: true, Count: 7, Size: 3, Interval: 0.123456, SourceIface: "wlan0", QOS: QOSVO}
+	args, err := cfgToArgs("1.2.3.4", cfg)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if !reflect.DeepEqual(args, expected) {
+		t.Errorf("cfgToArgs: outputs differ. Got %v, expected %v", args, expected)
+	}
+}
+
+func TestParseOutput(t *testing.T) {
+	testcases := []struct {
+		input  string
+		expect *Result
+	}{
+		// Output collected from rammus DUT.
+		{
+			input: `PING 8.8.8.8 (8.8.8.8) 56(84) bytes of data.
+64 bytes from 8.8.8.8: icmp_seq=1 ttl=58 time=2.81 ms
+ping: sendmsg: Network is unreachable
+ping: sendmsg: Network is unreachable
+64 bytes from 8.8.8.8: icmp_seq=4 ttl=58 time=2.82 ms
+64 bytes from 8.8.8.8: icmp_seq=5 ttl=58 time=1.71 ms
+
+--- 8.8.8.8 ping statistics ---
+5 packets transmitted, 3 received, 40% packet loss, time 12004ms
+rtt min/avg/max/mdev = 1.717/2.451/2.826/0.520 ms`,
+			expect: &Result{Sent: 5, Received: 3, Loss: 40, MinLatency: 1.717, AvgLatency: 2.451, MaxLatency: 2.826, DevLatency: .520},
+		},
+		// Output collected in chroot.
+		{
+			input: `PING 8.8.8.8 (8.8.8.8) 56(84) bytes of data.
+64 bytes from 8.8.8.8: icmp_seq=1 ttl=59 time=0.638 ms
+64 bytes from 8.8.8.8: icmp_seq=2 ttl=59 time=0.653 ms
+
+--- 8.8.8.8 ping statistics ---
+2 packets transmitted, 2 received, 0% packet loss, time 1028ms
+rtt min/avg/max/mdev = 0.638/0.645/0.653/0.026 ms`,
+			expect: &Result{Sent: 2, Received: 2, Loss: 0, MinLatency: 0.638, AvgLatency: 0.645, MaxLatency: 0.653, DevLatency: .026},
+		},
+	}
+	for i := range testcases {
+		output, err := parseOutput(testcases[i].input)
+		if err != nil {
+			t.Errorf("testcase %d failed with err=%s", i, err.Error())
+		}
+		if diff := cmp.Diff(output, testcases[i].expect); diff != "" {
+			t.Errorf("testcase %d returned unexpected result; diff=\n%s", i, diff)
+		}
+	}
+}
