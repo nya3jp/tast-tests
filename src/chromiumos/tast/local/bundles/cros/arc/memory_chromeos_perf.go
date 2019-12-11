929a01a122a427cb3aa9119969cc8d5987c631b9
Charles William Dick <cwd@google.com>
2019-12-11 15:48:12 +0900

    tast arc.MemoryChromeOSPerf test
    Test measures memory available to ChromeOS by allocating until the available memory is just inside critical margin. The test tries to keep available memory at the same level and waits for the system to stabalize.
    
    BUG=b:143262285
    TEST=tast run <DUT> arc.MemoryChromeOSPerf.arc arc.MemoryChromeOSPerf.noarc
    
    Change-Id: I749b042992157643d4e60ccac7634aab6927c4d0

---
+++ src/chromiumos/tast/local/bundles/cros/arc/memory_chromeos_perf.go
@@ -0,0 +1,69 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/memory"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: MemoryChromeOSPerf,
+		Desc: "How much memory can be allocated in ChromeOS before critical memory pressure",
+		Contacts: []string{
+			"cwd@chromium.org",
+			"arcvm-eng@google.com",
+		},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name: "noarc",
+			Pre:  chrome.LoggedIn(),
+		}, {
+			Name:              "arc", // TODO (b/141884011): when we have arc.VMBooted, split this into arcvm and arcpp.
+			Pre:               arc.Booted(),
+			ExtraSoftwareDeps: []string{"android_both"},
+		}},
+		Timeout: 10 * time.Minute,
+	})
+}
+
+func MemoryChromeOSPerf(ctx context.Context, s *testing.State) {
+	const bytesInMiB = 1048576.0
+	allocatedMetric := perf.Metric{Name: "allocated", Unit: "MB", Direction: perf.BiggerIsBetter, Multiple: true}
+	marginMetric := perf.Metric{Name: "critical_margin", Unit: "MB"}
+	margin, err := memory.ChromeOSCriticalMargin()
+	if err != nil {
+		s.Fatal("Failed to read critical margin: ", err)
+	}
+	p := perf.NewValues()
+	p.Set(marginMetric, float64(margin))
+	c := memory.NewChromeOSAllocator()
+	allocated, err := c.AllocateUntil(
+		ctx,
+		time.Second,
+		60,
+		margin-5, // NB: we want to be just inside the critical margin.
+	)
+	if err != nil {
+		s.Fatal("Failed to allocate to critical margin: ", err)
+	}
+	for _, a := range allocated {
+		p.Append(allocatedMetric, float64(a)/bytesInMiB)
+	}
+	if _, err := c.FreeAll(); err != nil {
+		s.Fatal("Failed to free allocated memory: ")
+	}
+	if err := p.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
