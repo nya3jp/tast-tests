0212750c9ed66281a6ee9ebd288fedcea05bf2b1
Gil Dekel <gildekel@chromium.org>
2019-12-11 16:57:25 -0500

    tast.camera: Clean up VA-API test separation
    This CL removes the last remnants of the VA-API test dependencies from
    tast.camera's jpeg_decode_accelerator_unittest.
    
    BUG=chromium:986074
    TEST=Existing test runs and passes locally on an Eve.
    Change-Id: I95f7ace021fd9ecd7d8eac3ada64f3b5ca33635c

--- src/chromiumos/tast/local/bundles/cros/camera/decode_accel_jpeg.go
+++ src/chromiumos/tast/local/bundles/cros/camera/decode_accel_jpeg.go
@@ -6,12 +6,11 @@ package camera
 
 import (
 	"context"
-	"os"
+	"path"
 	"path/filepath"
 
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/gtest"
-	"chromiumos/tast/local/media/binsetup"
 	"chromiumos/tast/local/media/caps"
 	"chromiumos/tast/local/media/logging"
 	"chromiumos/tast/local/sysutil"
@@ -25,30 +24,11 @@ func init() {
 		Contacts:     []string{"henryhsu@chromium.org", "chromeos-camera-eng@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", caps.HWDecodeJPEG},
-		Data:         imageFiles,
+		Data:         []string{decodeAccelJpegTestFile},
 	})
 }
 
-// imageFiles lists the files required by the JPEG decode accelerator test.
-// TODO(crbug.com/986074): we should move the WebPs to a separate tast test.
-// For now we make the WebP tests run with the jpeg_decode_accelerator_unittest binary.
-// Also rename imageFiles back to jpegTestFiles once the tests are separated.
-var imageFiles = []string{
-	"BlackAndWhite_criss-cross_pattern_2015x2015.webp",
-	"peach_pi-1280x720.jpg",
-	"pixel-1280x720.jpg",
-	"pixel-1280x720-grayscale.jpg",
-	"pixel-1280x720-yuv420.jpg",
-	"pixel-1280x720-yuv444.jpg",
-	"pixel-40x23-yuv420.jpg",
-	"pixel-41x22-yuv420.jpg",
-	"pixel-41x23-yuv420.jpg",
-	"RGB_noise_2015x2015.webp",
-	"RGB_noise_large_pixels_115x115.webp",
-	"RGB_noise_large_pixels_2015x2015.webp",
-	"RGB_noise_large_pixels_4000x4000.webp",
-	"solid_green_2015x2015.webp",
-}
+const decodeAccelJpegTestFile = "peach_pi-1280x720.jpg"
 
 // DecodeAccelJPEG runs a set of HW JPEG decode tests, defined in
 // jpeg_decode_accelerator_unittest.
@@ -59,25 +39,17 @@ func DecodeAccelJPEG(ctx context.Context, s *testing.State) {
 	}
 	defer vl.Close()
 
-	// The JPEG decode test operates on all files in a single directory.
-	// testing.State doesn't guarantee that all data files will be stored in the same
-	// directory, so copy them to a temp dir.
-	var srcs []string
-	for _, fn := range imageFiles {
-		srcs = append(srcs, s.DataPath(fn))
-	}
-	tempDir, err := binsetup.CreateTempDataDir("DecodeAccelJPEG.tast.", srcs)
-	if err != nil {
-		s.Fatal("Failed to create a temporary directory: ", err)
-	}
-	defer os.RemoveAll(tempDir)
+	testDir := path.Dir(s.DataPath(decodeAccelJpegTestFile))
 
 	// Execute the test binary.
 	const exec = "jpeg_decode_accelerator_unittest"
 	if report, err := gtest.New(
 		filepath.Join(chrome.BinTestDir, exec),
 		gtest.Logfile(filepath.Join(s.OutDir(), exec+".log")),
-		gtest.ExtraArgs(logging.ChromeVmoduleFlag(), "--test_data_path="+tempDir+"/"),
+		gtest.ExtraArgs(
+			logging.ChromeVmoduleFlag(),
+			"--test_data_path="+testDir+"/",
+			"--jpeg_filenames="+decodeAccelJpegTestFile),
 		gtest.UID(int(sysutil.ChronosUID)),
 	).Run(ctx); err != nil {
 		s.Errorf("Failed to run %v: %v", exec, err)
