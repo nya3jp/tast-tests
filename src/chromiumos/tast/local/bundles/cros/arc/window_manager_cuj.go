c39a299b89d9317b3bc34696e7d6f6c1afa7ef31
Ricardo Quesada <ricardoq@google.com>
2019-12-11 10:34:39 -0800

    tast-test: arc.WindowManagerCUJ does not hang on pre-N
    This CL calls activity.SetWindowState instead of
    ash.SetARCAppWindowState() to prevent "starvation".
    
    ash.SetARCappWindowState() won't receive a "window change" event
    when trying to resize a pre-N application since a pop-up dialog
    asking for confirmation will appear. And ash.SetARCAppWindowSTate()
    will never return.
    
    This CL works-around that by calling activity.SetWindowState() which
    is a non-blocking call.
    
    (this CL is part of the needed changes for arc-next)
    
    BUG=b:143908501
    TEST=tast run $DUT arc.WindowManagerCUJ
    
    Change-Id: I8bf59b06f6c49a956a19d9a43c6004600d5826d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1962667
    Tested-by: Ricardo Quesada <ricardoq@chromium.org>
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Ricardo Quesada <ricardoq@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/window_manager_cuj.go
+++ src/chromiumos/tast/local/bundles/cros/arc/window_manager_cuj.go
@@ -313,7 +313,10 @@ func wmMaximizeRestoreClamshell23(ctx context.Context, tconn *chrome.Conn, a *ar
 				return err
 			}
 
-			if _, err := ash.SetARCAppWindowState(ctx, tconn, act.PackageName(), ash.WMEventMaximize); err != nil {
+			// Calling ash.SetARCAppWindowState() doesn't work in this subtest, using act.SetWindowState instead.
+			// Pre-N applications trigger a pop-up dialog  asking for confirmation. ash.SetARCAppWindowState() will
+			// "wait forever" for a window event to occur, but this event won't occur due to the pop-up dialog.
+			if err := act.SetWindowState(ctx, arc.WindowStateMaximized); err != nil {
 				return err
 			}
 
@@ -327,7 +330,7 @@ func wmMaximizeRestoreClamshell23(ctx context.Context, tconn *chrome.Conn, a *ar
 				return err
 			}
 
-			if _, err := ash.SetARCAppWindowState(ctx, tconn, act.PackageName(), ash.WMEventNormal); err != nil {
+			if err := act.SetWindowState(ctx, arc.WindowStateNormal); err != nil {
 				return err
 			}
 
