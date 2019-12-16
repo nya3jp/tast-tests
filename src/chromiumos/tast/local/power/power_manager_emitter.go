7e093c436fdcbe581152242d1251b9888d7aba97
Victor-Gabriel Savu <vsavu@google.com>
2019-12-16 18:03:30 +0100

    tast-tests: move to dbus-send for APIHandlePowerEvent
    Use dbus-send for APIHandlePowerEvent to impersonate the powerd daemon
    running as the power daemon. This works without allowing root to
    own the DBus interface.
    
    BUG=None
    TEST=tast run <ip> wilco.APIHandlePowerEvent
    
    Change-Id: I42d850d0304462d5fcc884d3c30170e11b85c594

--- src/chromiumos/tast/local/power/power_manager_emitter.go
+++ src/chromiumos/tast/local/power/power_manager_emitter.go
@@ -6,83 +6,52 @@ package power
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
-	"github.com/godbus/dbus"
 	"github.com/golang/protobuf/proto"
 
 	pmpb "chromiumos/system_api/power_manager_proto"
 	"chromiumos/tast/errors"
-	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/local/testexec"
 )
 
-const powerdJob = "powerd"
-
-// PowerManagerEmitter is used to emit signals on behalf of power manager over D-Bus.
-// For detailed spec of each D-Bus signal, please find
-// src/platform2/power_manager/dbus_bindings/org.chromium.PowerManager.xml
-type PowerManagerEmitter struct { // NOLINT
-	conn *dbus.Conn
-}
-
-// NewPowerManagerEmitter stops real power manager, takes ownership of power
-// manager D-Bus service interface and returns a PowerManagerEmitter object.
-func NewPowerManagerEmitter(ctx context.Context) (*PowerManagerEmitter, error) {
-	if err := upstart.StopJob(ctx, powerdJob); err != nil {
-		return nil, errors.Wrapf(err, "unable to stop the %s service", powerdJob)
-	}
-
-	conn, err := dbus.SystemBus()
-	if err != nil {
-		upstart.RestartJob(ctx, powerdJob)
-		return nil, errors.Wrap(err, "unable to connect to the dbus")
-	}
-	if _, err := conn.RequestName(dbusInterface, dbus.NameFlagReplaceExisting); err != nil {
-		upstart.RestartJob(ctx, powerdJob)
-		return nil, errors.Wrapf(err, "unable to request ownership of %s dbus name", dbusInterface)
-	}
-
-	return &PowerManagerEmitter{conn}, nil
-}
-
-// Stop relaeases D-Bus power manager service interface ownership and starts
-// real power manager.
-func (p *PowerManagerEmitter) Stop(ctx context.Context) error {
-	if _, err := p.conn.ReleaseName(dbusInterface); err != nil {
-		return errors.Wrapf(err, "unable to release %s dbus name", dbusInterface)
-	}
-	if err := upstart.RestartJob(ctx, powerdJob); err != nil {
-		return errors.Wrapf(err, "unable to start the %s service", powerdJob)
-	}
-	return nil
-}
-
 // EmitPowerSupplyPoll emits PowerSupplyPoll D-Bus signal.
-func (p *PowerManagerEmitter) EmitPowerSupplyPoll(msg *pmpb.PowerSupplyProperties) error {
-	return p.emitEvent(msg, "PowerSupplyPoll")
+func EmitPowerSupplyPoll(ctx context.Context, msg *pmpb.PowerSupplyProperties) error {
+	return emitEvent(ctx, msg, "PowerSupplyPoll")
 }
 
 // EmitSuspendImminent emits SuspendImminent D-Bus signal.
-func (p *PowerManagerEmitter) EmitSuspendImminent(msg *pmpb.SuspendImminent) error {
-	return p.emitEvent(msg, "SuspendImminent")
+func EmitSuspendImminent(ctx context.Context, msg *pmpb.SuspendImminent) error {
+	return emitEvent(ctx, msg, "SuspendImminent")
 }
 
 // EmitDarkSuspendImminent emits DarkSuspendImminent D-Bus signal.
-func (p *PowerManagerEmitter) EmitDarkSuspendImminent(msg *pmpb.SuspendImminent) error {
-	return p.emitEvent(msg, "DarkSuspendImminent")
+func EmitDarkSuspendImminent(ctx context.Context, msg *pmpb.SuspendImminent) error {
+	return emitEvent(ctx, msg, "DarkSuspendImminent")
 }
 
 // EmitSuspendDone emits SuspendDone D-Bus signal.
-func (p *PowerManagerEmitter) EmitSuspendDone(msg *pmpb.SuspendDone) error {
-	return p.emitEvent(msg, "SuspendDone")
+func EmitSuspendDone(ctx context.Context, msg *pmpb.SuspendDone) error {
+	return emitEvent(ctx, msg, "SuspendDone")
 }
 
-func (p *PowerManagerEmitter) emitEvent(msg proto.Message, eventName string) error {
+func emitEvent(ctx context.Context, msg proto.Message, eventName string) error {
 	bytes, err := proto.Marshal(msg)
 	if err != nil {
 		return errors.Wrap(err, "unable to marshal proto to byte array")
 	}
-	if err := p.conn.Emit(dbus.ObjectPath(dbusPath), dbusInterface+"."+eventName, bytes); err != nil {
-		return errors.Wrapf(err, "unable to emit %s event", eventName)
+
+	var bytesAsStrings []string
+	for _, v := range bytes {
+		bytesAsStrings = append(bytesAsStrings, fmt.Sprintf("0x%02x", v))
 	}
+
+	data := "array:byte:" + strings.Join(bytesAsStrings, ",")
+	args := []string{"-u", "power", "--", "dbus-send", "--sender=" + dbusInterface, "--system", "--type=signal", dbusPath, dbusInterface + "." + eventName, data}
+	if err := testexec.CommandContext(ctx, "sudo", args...).Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrapf(err, "unable to run sudo %s", args)
+	}
+
 	return nil
 }
