7cc61af6bcba1db697e36b8caeb55b1e2f8cb24a
Oleh Lamzin <lamzin@google.com>
2019-12-16 13:19:12 +0100

    tast-test: wait for DTC gRPC server ready
    Wait until DTC gRPC server ready for incoming messages.
    
    BUG=b:145259332
    TEST=run wilco.VMStart test
    
    Change-Id: Iba5b10dae7827facc6fa37d3fdcbcfb23fff35b2

--- src/chromiumos/tast/local/wilco/vm.go
+++ src/chromiumos/tast/local/wilco/vm.go
@@ -27,6 +27,7 @@ const (
 	DDVDbusTopic                    = "com.dell.ddv"
 	wilcoVMJob                      = "wilco_dtc"
 	wilcoVMStartupPort              = 7788
+	wilcoVMDTCPort                  = 6667
 	wilcoVMUIMessageReceiverDTCPort = 6668
 )
 
@@ -86,7 +87,7 @@ func StartVM(ctx context.Context, config *VMConfig) error {
 	}
 
 	if config.StartProcesses {
-		for _, port := range []uint32{wilcoVMUIMessageReceiverDTCPort} {
+		for _, port := range []uint32{wilcoVMDTCPort, wilcoVMUIMessageReceiverDTCPort} {
 			if err := waitVMGRPCServerReady(ctx, port); err != nil {
 				return errors.Wrapf(err, "unable to wait for gRPC server to be ready on %d port", port)
 			}
