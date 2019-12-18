41618c4f2cc9fd101d2783b0c7d69cf58648b609
Miguel Casas <mcasas@chromium.org>
2019-12-18 18:16:38 -0500

    tast/video: merge video.PlayDecodeAccelUsed.* into video.Play
    This CL merges video.PlayDecodeAccelUsed.* into video.Play, renaming
    them minimally to make it so that the blended result makes sense:
    
    video.PlayDecodeAccelUsed.h264    --> video.Play.h264_hw
    video.PlayDecodeAccelUsed.mse_h264--> video.Play.h264_hw_mse
    video.PlayDecodeAccelUsed.vp8     --> video.Play.vp8_hw
    video.PlayDecodeAccelUsed.mse_vp8 --> video.Play.vp8_hw_mse
    video.PlayDecodeAccelUsed.vp9     --> video.Play.vp9_hw
    video.PlayDecodeAccelUsed.mse_vp9 --> video.Play.vp9_mse_mse
    
    (Note that the mse_* variants only makes sense with hw support).
    
    They are all temporarily gathered into the group:graphics suite
    (graphics_video + graphics_perbuild) to stabilise before moving
    whichever are appropriate to the CQ. (See bug).
    
    TEST=tast -verbose run ${IP} video.Play* on kohaku
    BUG=chromium:1035649
    
    Change-Id: I770a50b5430ca0b3a8a13695721bdf03a396313e

--- src/chromiumos/tast/local/bundles/cros/video/play.go
+++ src/chromiumos/tast/local/bundles/cros/video/play.go
@@ -7,55 +7,91 @@ package video
 import (
 	"context"
 
+	"chromiumos/tast/local/bundles/cros/video/decode"
 	"chromiumos/tast/local/bundles/cros/video/play"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/caps"
 	"chromiumos/tast/local/media/pre"
 	"chromiumos/tast/testing"
 )
 
+type playParams struct {
+	fileName   string
+	videoType  play.VideoType
+	verifyMode play.VerifyHWAcceleratorMode
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func: Play,
-		Desc: "Checks simple unrestricted (HW, SW) video playback in Chrome is working",
+		Desc: "Checks simple video playback in Chrome is working",
 		Contacts: []string{
-			"acourbot@chromium.org",
 			"mcasas@chromium.org",
 			"chromeos-gfx-video@google.com",
-			"chromeos-video-eng@google.com",
 		},
 		SoftwareDeps: []string{"chrome"},
 		Pre:          pre.ChromeVideo(),
-		Data:         []string{"video.html"},
-		Attr:         []string{"group:mainline"},
+		Data:         []string{decode.ChromeMediaInternalsUtilsJSFile},
+		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
 		Params: []testing.Param{{
 			Name:      "av1",
-			Val:       "720p_30fps_300frames.av1.mp4",
-			ExtraData: []string{"720p_30fps_300frames.av1.mp4"},
-			ExtraAttr: []string{"informational"},
+			Val:       playParams{fileName: "720p_30fps_300frames.av1.mp4", videoType: play.NormalVideo, verifyMode: play.NoVerifyHWAcceleratorUsed},
+			ExtraData: []string{"video.html", "720p_30fps_300frames.av1.mp4"},
 		}, {
-			Name:      "h264",
-			Val:       "720_h264.mp4",
-			ExtraData: []string{"720_h264.mp4"},
-			// "chrome_internal" is needed because H.264 is a proprietary codec.
-			ExtraSoftwareDeps: []string{"chrome_internal"},
-			// TODO(crbug.com/1029188): Promote to critical again.
-			// This test is a fallout of ui.AssistantStartup errors
-			ExtraAttr:    []string{"informational"},
+			Name:              "h264",
+			Val:               playParams{fileName: "720_h264.mp4", videoType: play.NormalVideo, verifyMode: play.NoVerifyHWAcceleratorUsed},
+			ExtraData:         []string{"video.html", "720_h264.mp4"},
+			ExtraSoftwareDeps: []string{"chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
 		}, {
 			Name:      "vp8",
-			Val:       "720_vp8.webm",
-			ExtraData: []string{"720_vp8.webm"},
+			Val:       playParams{fileName: "720_vp8.webm", videoType: play.NormalVideo, verifyMode: play.NoVerifyHWAcceleratorUsed},
+			ExtraData: []string{"video.html", "720_vp8.webm"},
 		}, {
 			Name:      "vp9",
-			Val:       "720_vp9.webm",
-			ExtraData: []string{"720_vp9.webm"},
+			Val:       playParams{fileName: "720_vp9.webm", videoType: play.NormalVideo, verifyMode: play.NoVerifyHWAcceleratorUsed},
+			ExtraData: []string{"video.html", "720_vp9.webm"},
+		}, {
+			Name:              "h264_hw",
+			Val:               playParams{fileName: "720_h264.mp4", videoType: play.NormalVideo, verifyMode: play.VerifyHWAcceleratorUsed},
+			ExtraData:         []string{"video.html", "720_h264.mp4"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
+		}, {
+			Name:              "vp8_hw",
+			Val:               playParams{fileName: "720_vp8.webm", videoType: play.NormalVideo, verifyMode: play.VerifyHWAcceleratorUsed},
+			ExtraData:         []string{"video.html", "720_vp8.webm"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+		}, {
+			Name:              "vp9_hw",
+			Val:               playParams{fileName: "720_vp9.webm", videoType: play.NormalVideo, verifyMode: play.VerifyHWAcceleratorUsed},
+			ExtraData:         []string{"video.html", "720_vp9.webm"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+		}, {
+			Name:              "h264_hw_mse",
+			Val:               playParams{fileName: "bear-320x240.h264.mpd", videoType: play.MSEVideo, verifyMode: play.VerifyHWAcceleratorUsed},
+			ExtraData:         append(play.MSEDataFiles(), "bear-320x240-video-only.h264.mp4", "bear-320x240-audio-only.aac.mp4", "bear-320x240.h264.mpd"),
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
+		}, {
+			Name:              "vp8_hw_mse",
+			Val:               playParams{fileName: "bear-320x240.vp8.mpd", videoType: play.MSEVideo, verifyMode: play.VerifyHWAcceleratorUsed},
+			ExtraData:         append(play.MSEDataFiles(), "bear-320x240-video-only.vp8.webm", "bear-320x240-audio-only.vorbis.webm", "bear-320x240.vp8.mpd"),
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+		}, {
+			Name:              "vp9_mse_mse",
+			Val:               playParams{fileName: "bear-320x240.vp9.mpd", videoType: play.MSEVideo, verifyMode: play.VerifyHWAcceleratorUsed},
+			ExtraData:         append(play.MSEDataFiles(), "bear-320x240-video-only.vp9.webm", "bear-320x240-audio-only.opus.webm", "bear-320x240.vp9.mpd"),
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 		}},
 	})
 }
 
-// Play plays a given file with Chrome.
+// Play plays a given file in Chrome and verifies that the playback happens
+// correctly; if verifyMode says so, it verifies if playback uses hardware
+// acceleration.
+// If videoType is NormalVideo, a simple <video> player is instantiated with the
+// input filename, whereas if it's MSEVideo,then we try to feed the media files
+// via a SourceBuffer (using MSE, the Media Source Extensions protocol, and a
+// DASH MPD file).
 func Play(ctx context.Context, s *testing.State) {
-	play.TestPlay(ctx, s, s.PreValue().(*chrome.Chrome),
-		s.Param().(string), play.NormalVideo,
-		play.NoVerifyHWAcceleratorUsed)
+	testOpt := s.Param().(playParams)
+	play.TestPlay(ctx, s, s.PreValue().(*chrome.Chrome), testOpt.fileName, testOpt.videoType, testOpt.verifyMode)
 }
