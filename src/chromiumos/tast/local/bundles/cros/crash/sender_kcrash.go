459881c0c7e65aa206daa672e09aaf1afa8c9fc5
Shuhei Takahashi <nya@chromium.org>
2019-12-18 13:35:38 +0900

    crash: Add SenderKcrash test.
    This is a part of the patches to port logging_CrashSender to Tast.
    
    crash.SenderKcrash corresponds to "sender_simple_kernel_crash"
    subtest in logging_CrashSender.
    
    BUG=chromium:978255
    TEST=fast_build.sh -T
    TEST=tast run kohaku crash.SenderKcrash
    
    Change-Id: I4679816c59a9775d8d4772dec3ba808790d7c8db

---
+++ src/chromiumos/tast/local/bundles/cros/crash/sender_kcrash.go
@@ -0,0 +1,59 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crash
+
+import (
+	"context"
+	"os"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"chromiumos/tast/local/bundles/cros/crash/sender"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SenderKcrash,
+		Desc: "Check that kernel crash dumps are uploaded",
+		Contacts: []string{
+			"nya@chromium.org", // ported to Tast
+			"cros-monitoring-forensics@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func SenderKcrash(ctx context.Context, s *testing.State) {
+	crashDir, err := sender.SetUp(ctx, s.PreValue().(*chrome.Chrome))
+	if err != nil {
+		s.Fatal("Setup failed: ", err)
+	}
+	defer sender.TearDown()
+	defer os.RemoveAll(crashDir)
+
+	const basename = "some_kernel.1.2.3"
+	exp, err := sender.AddFakeKernelCrash(ctx, crashDir, basename)
+	if err != nil {
+		s.Fatal("Failed to add a fake kernel crash: ", err)
+	}
+
+	got, err := sender.Run(ctx, crashDir)
+	if err != nil {
+		s.Fatal("Failed to run crash_sender: ", err)
+	}
+	want := []*sender.SendResult{{
+		Success: true,
+		Data:    *exp,
+	}}
+	if diff := cmp.Diff(got, want, cmpopts.IgnoreFields(sender.SendResult{}, "Schedule")); diff != "" {
+		s.Log("Results mismatch (-got +want): ", diff)
+		s.Errorf("crash_sender sent unexpected %d results; see logs for diff", len(got))
+	}
+}
