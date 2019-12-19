b829224c870bb5218130f213efbf7b1b1efbc1e6
Meilin Wang <meilinw@google.com>
2019-12-18 16:44:17 -0800

    tast-test: Add Launcher-embedded Assistant UI animation perf test.
    This CL adds a performance test for the open/close animation of embedded
    Assistant UI, so that we can measure the smoothness of the animations
    under different circumstances.
    
    BUG=b:145218971
    TEST=tast -verbose run DUT ui.AssistantEmbeddedUiAnimationPerf
    
    Change-Id: I3ec3539d1989b441044009899f4f14712aa6334e

--- src/chromiumos/tast/local/assistant/assistant.go
+++ src/chromiumos/tast/local/assistant/assistant.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"time"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
 )
@@ -70,4 +71,26 @@ func SetHotwordEnabled(ctx context.Context, tconn *chrome.Conn, enabled bool) er
 	expr := fmt.Sprintf(
 		`tast.promisify(chrome.autotestPrivate.setWhitelistedPref)('%s', %t)`, prefName, enabled)
 	return tconn.EvalPromise(ctx, expr, nil)
+}
+
+// ToggleUIWithHotkey mimics the Assistant key press to open/close the Assistant UI.
+func ToggleUIWithHotkey(ctx context.Context, tconn *chrome.Conn) error {
+	const accelerator = "{keyCode: 'assistant', shift: false, control: false, alt: false, search: false, pressed: true}"
+	expr := fmt.Sprintf(
+		`(async () => {
+		   var accel = %s;
+		   // Triggers the hotkey pressed event.
+		   await tast.promisify(chrome.autotestPrivate.activateAccelerator)(accel);
+		   // Releases the key for cleanup. This release event will not be handled as it is
+		   // not a registered accelerator, so we ignore the result and don't wait for this
+		   // async call returned.
+		   accel.pressed = false;
+		   chrome.autotestPrivate.activateAccelerator(accel, () => {});
+		 })()`, accelerator)
+
+	if err := tconn.EvalPromise(ctx, expr, nil); err != nil {
+		return errors.Wrap(err, "failed to execute accelerator")
+	}
+
+	return nil
 }
