3e7fe6d2ffd558684b73be3a986a47e295bcad83
Leo Lai <cylai@google.com>
2019-12-19 23:28:56 +0800

    everythin else
    BUG=None
    TEST=None
    
    Change-Id: Ia5a5f7f8f719162b0633b7f453f06acc66b23342

---
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/cryptohome_key_during_init.go
@@ -0,0 +1,102 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	hwsecremote "chromiumos/tast/remote/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CryptohomeKeyDuringInit,
+		Desc:         "Verifies that the TPM ownership can be cleared and taken",
+		Contacts:     []string{"cylai@google.com"},
+		SoftwareDeps: []string{"chrome", "reboot"},
+		Attr:         []string{"informational"},
+	})
+}
+
+func CryptohomeKeyDuringInit(ctx context.Context, s *testing.State) {
+	r, err := hwsecremote.NewCmdRunner(s.DUT())
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Utilty creation error: ", err)
+	}
+	helper, err := hwsecremote.NewHelper(utility, r, s.DUT())
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+	s.Log("Start resetting TPM if needed")
+	if err := helper.EnsureTPMIsReset(ctx); err != nil {
+		s.Fatal("Failed to ensure resetting TPM: ", err)
+	}
+	s.Log("TPM is confirmed to be reset")
+
+	username := "unowned@gmail.com"
+	passwd := "testpass"
+
+	mountTask := hwsec.NewStressTaskMount(utility, username, passwd)
+	ownershipTask := hwsec.NewStressTaskTakeOwnership(&helper.Helper)
+	hwsec.RegisterRunner(mountTask, "mount")
+	hwsec.RegisterRunner(ownershipTask, "ownership")
+	topJSON := `{
+		"primary": {
+			"names": ["ownership"],
+			"count": 1,
+			"cof":false
+		},
+		"secondary": [
+			{
+				"names": ["mount"],
+				"cof":true
+			}
+		]
+	}`
+	const maxRun int = 5
+	for i := 0; i < maxRun; i++ {
+		s.Log("fuck")
+		tester, err := hwsec.UnmarshalPSTaskModel(topJSON)
+		if err != nil {
+			s.Fatal("Failed to unmarshal primary-secondary task model: ", err)
+		}
+		tester.Run(ctx, nil)
+		if err := helper.EnsureTPMIsReset(ctx); err != nil {
+			s.Fatal("Failed to ensure resetting TPM: ", err)
+		}
+	}
+	checkTask := hwsec.NewStressTaskCheckKey(utility, username, passwd)
+	hwsec.RegisterRunner(checkTask, "check")
+	topJSON = `{
+		"primary": {
+			"names": ["ownership"],
+			"count": 1,
+			"cof":false
+		},
+		"secondary": [
+			{
+				"names": ["check"],
+				"cof":true
+			}
+		]
+	}`
+	for i := 0; i < maxRun; i++ {
+		s.Log("fuck")
+		tester, err := hwsec.UnmarshalPSTaskModel(topJSON)
+		if err != nil {
+			s.Fatal("Failed to unmarshal primary-secondary task model: ", err)
+		}
+		tester.Run(ctx, nil)
+		if err := helper.EnsureTPMIsReset(ctx); err != nil {
+			s.Fatal("Failed to ensure resetting TPM: ", err)
+		}
+	}
+}
---
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/retake_ownership_flush_password.go
@@ -0,0 +1,97 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	hwsecremote "chromiumos/tast/remote/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: RetakeOwnershipFlushPassword,
+		Desc: "Verifies that taking ownership produce a new owner password",
+		Contacts: []string{
+			"cylai@chromium.org", // Nobody
+		},
+		SoftwareDeps: []string{"reboot", "tpm"},
+		Attr:         []string{"informational"},
+	})
+}
+
+func RetakeOwnershipFlushPassword(ctx context.Context, s *testing.State) {
+	r, err := hwsecremote.NewCmdRunner(s.DUT())
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Utilty creation error: ", err)
+	}
+	helper, err := hwsecremote.NewHelper(utility, r, s.DUT())
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+	s.Log("Start resetting TPM if needed")
+	if err := helper.EnsureTPMIsReset(ctx); err != nil {
+		s.Fatal("Failed to ensure resetting TPM: ", err)
+	}
+	s.Log("TPM is confirmed to be reset")
+
+	if result, err := utility.IsPreparedForEnrollment(ctx); err != nil {
+		s.Fatal("Cannot check if enrollment preparation is reset")
+	} else if result {
+		s.Fatal("Enrollment preparation is not reset after clearing ownership")
+	}
+	s.Log("Enrolling with TPM not ready")
+	if _, err := utility.CreateEnrollRequest(ctx, hwsec.DefaultPCA); err == nil {
+		s.Fatal("Enrollment should not happen w/o getting prepared")
+	}
+
+	s.Log("Start taking ownership")
+	if err := helper.EnsureTPMIsReady(ctx, hwsec.DefaultTakingOwnershipTimeout); err != nil {
+		s.Fatal("Failed to ensure ownership: ", err)
+	}
+	s.Log("Onwership is taken")
+
+	passwd, err := utility.GetOwnerPassword(ctx)
+	if err != nil {
+		s.Fatal("Failed to get owner password")
+	} else if len(passwd) != 20 {
+		s.Fatal("Ill-formed owner password")
+	}
+	s.Log("Start resetting TPM again")
+	if err := helper.EnsureTPMIsReset(ctx); err != nil {
+		s.Fatal("Failed to ensure resetting TPM: ", err)
+	}
+	s.Log("TPM is confirmed to be reset")
+
+	passwd2, err := utility.GetOwnerPassword(ctx)
+	if err != nil {
+		s.Fatal("Failed to get owner password")
+	} else if len(passwd2) != 0 {
+		s.Fatal("Non-empty owner password after reset")
+	}
+
+	s.Log("Start taking ownership again")
+	if err := helper.EnsureTPMIsReady(ctx, hwsec.DefaultTakingOwnershipTimeout); err != nil {
+		s.Fatal("Failed to ensure ownership: ", err)
+	}
+	s.Log("Onwership is taken")
+
+	passwd2, err = utility.GetOwnerPassword(ctx)
+	if err != nil {
+		s.Fatal("Failed to get owner password")
+	} else if len(passwd) != 20 {
+		s.Fatal("Ill-formed owner password")
+	}
+	if passwd == passwd2 {
+		s.Fatal("Owner password doesn't get re-created")
+	}
+
+}
