030e08a4a39d6d5c8d8a9961b2f8f20ed0f5a9bc
Leo Lai <cylai@google.com>
2019-12-19 23:28:56 +0800

    everything else
    BUG=None
    TEST=None
    
    Change-Id: Ia5a5f7f8f719162b0633b7f453f06acc66b23342

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/concurrent_login_enroll.go
@@ -0,0 +1,53 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	hwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ConcurrentLoginEnroll,
+		Desc:         "Stress tests login and enroll",
+		Attr:         []string{"group:mainline", "informational"},
+		Contacts:     []string{"cylai@chromium.org", "cros-hwsec@google.com"},
+		SoftwareDeps: []string{"chrome", "tpm"},
+	})
+}
+
+// ConcurrentLoginEnroll verifies the concurrency of login and enrollment.
+func ConcurrentLoginEnroll(ctx context.Context, s *testing.State) {
+	r, err := hwseclocal.NewCmdRunner()
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Utilty creation error: ", err)
+	}
+	helper, err := hwseclocal.NewHelper(utility)
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+	if err := helper.EnsureTPMIsReady(ctx, hwsec.DefaultTakingOwnershipTimeout); err != nil {
+		s.Error("Failed to ensure tpm readiness: ", err)
+		return
+	}
+	s.Log("TPM is ensured to be ready")
+	if err := helper.EnsureIsPreparedForEnrollment(ctx, hwsec.DefaultPreparationForEnrolmentTimeout); err != nil {
+		s.Error("Failed to prepare for enrollment: ", err)
+		return
+	}
+	s.Log("Attestation is prepared for enrollment")
+
+	enrollTask := hwsec.NewStressTaskEnroll(utility)
+	enrollStressTester := hwsec.NewSimpleStressTester(enrollTask, "enroll", 5, true)
+	enrollStressTester.Run(ctx, nil)
+}
