19a9a39620d66d742be8dbf99723b1a70ce10c57
David Staessens <dstaessens@chromium.org>
2019-12-19 10:04:59 +0900

    video.ARCDecodeAccelPerf: Update videos used by the ARC video decode perf tests.
    This CL changes the videos used by the ARC video decoder performance tests.
    Currently simple 240p 25fps videos are used to measure performance, but these
    videos are not well suited to give a good idea of decoder performance. Instead
    we will use 1080p and 2160p videos, both in 30 and 60 FPS. These are the same
    videos used by the Chrome video performance tests.
    
    This change causes the names of the tests to change, and makes them consistent
    with all other video performance tests:
    
    video.ARCDecodeAccelPerf.h264_240p -> video.ARCDecodeAccelPerf.h264_1080p_30fps
                                       -> video.ARCDecodeAccelPerf.h264_1080p_60fps
                                       -> video.ARCDecodeAccelPerf.h264_2160p_30fps
                                       -> video.ARCDecodeAccelPerf.h264_2160p_60fps
    video.ARCDecodeAccelPerf.vp8_240p -> video.ARCDecodeAccelPerf.vp8_1080p_30fps
                                       -> video.ARCDecodeAccelPerf.vp8_1080p_60fps
                                       -> video.ARCDecodeAccelPerf.vp8_2160p_30fps
                                       -> video.ARCDecodeAccelPerf.vp8_2160p_60fps
    video.ARCDecodeAccelPerf.vp9_240p -> video.ARCDecodeAccelPerf.vp9_1080p_30fps
                                       -> video.ARCDecodeAccelPerf.vp9_1080p_60fps
                                       -> video.ARCDecodeAccelPerf.vp9_2160p_30fps
                                       -> video.ARCDecodeAccelPerf.vp9_2160p_60fps
    
    Note: The name of the tests very recently changed in CL:1916775, so changing it
    again shouldn't cause any additional inconvience.
    
    TEST=video.ARCDecodeAccelPerf.* on hana
    
    BUG=None
    
    Change-Id: I8eee59dcdab92d514e24d6843dfb25568ae8f363

--- src/chromiumos/tast/local/bundles/cros/video/arc_decode_accel_perf.go
+++ src/chromiumos/tast/local/bundles/cros/video/arc_decode_accel_perf.go
@@ -22,20 +22,65 @@ func init() {
 		SoftwareDeps: []string{"android", "chrome"},
 		Pre:          arc.Booted(),
 		Params: []testing.Param{{
-			Name:              "h264_240p",
-			Val:               "test-25fps.h264",
+			Name:              "h264_1080p_30fps",
+			Val:               "1080p_30fps_300frames.h264",
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
-			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
+			ExtraData:         []string{"1080p_30fps_300frames.h264", "1080p_30fps_300frames.h264.json"},
 		}, {
-			Name:              "vp8_240p",
-			Val:               "test-25fps.vp8",
+			Name:              "h264_1080p_60fps",
+			Val:               "1080p_60fps_600frames.h264",
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_60},
+			ExtraData:         []string{"1080p_60fps_600frames.h264", "1080p_60fps_600frames.h264.json"},
+		}, {
+			Name:              "h264_2160p_30fps",
+			Val:               "2160p_30fps_300frames.h264",
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K},
+			ExtraData:         []string{"2160p_30fps_300frames.h264", "2160p_30fps_300frames.h264.json"},
+		}, {
+			Name:              "h264_2160p_60fps",
+			Val:               "2160p_60fps_600frames.h264",
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K60},
+			ExtraData:         []string{"2160p_60fps_600frames.h264", "2160p_60fps_600frames.h264.json"},
+		}, {
+			Name:              "vp8_1080p_30fps",
+			Val:               "1080p_30fps_300frames.vp8.ivf",
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
-			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
+			ExtraData:         []string{"1080p_30fps_300frames.vp8.ivf", "1080p_30fps_300frames.vp8.ivf.json"},
 		}, {
-			Name:              "vp9_240p",
-			Val:               "test-25fps.vp9",
+			Name:              "vp8_1080p_60fps",
+			Val:               "1080p_60fps_600frames.vp8.ivf",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_60},
+			ExtraData:         []string{"1080p_60fps_600frames.vp8.ivf", "1080p_60fps_600frames.vp8.ivf.json"},
+		}, {
+			Name:              "vp8_2160p_30fps",
+			Val:               "2160p_30fps_300frames.vp8.ivf",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_4K},
+			ExtraData:         []string{"2160p_30fps_300frames.vp8.ivf", "2160p_30fps_300frames.vp8.ivf.json"},
+		}, {
+			Name:              "vp8_2160p_60fps",
+			Val:               "2160p_60fps_600frames.vp8.ivf",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_4K60},
+			ExtraData:         []string{"2160p_60fps_600frames.vp8.ivf", "2160p_60fps_600frames.vp8.ivf.json"},
+		}, {
+			Name:              "vp9_1080p_30fps",
+			Val:               "1080p_30fps_300frames.vp9.ivf",
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
-			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
+			ExtraData:         []string{"1080p_30fps_300frames.vp9.ivf", "1080p_30fps_300frames.vp9.ivf.json"},
+		}, {
+			Name:              "vp9_1080p_60fps",
+			Val:               "1080p_60fps_600frames.vp9.ivf",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_60},
+			ExtraData:         []string{"1080p_60fps_600frames.vp9.ivf", "1080p_60fps_600frames.vp9.ivf.json"},
+		}, {
+			Name:              "vp9_2160p_30fps",
+			Val:               "2160p_30fps_300frames.vp9.ivf",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_4K},
+			ExtraData:         []string{"2160p_30fps_300frames.vp9.ivf", "2160p_30fps_300frames.vp9.ivf.json"},
+		}, {
+			Name:              "vp9_2160p_60fps",
+			Val:               "2160p_60fps_600frames.vp9.ivf",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_4K60},
+			ExtraData:         []string{"2160p_60fps_600frames.vp9.ivf", "2160p_60fps_600frames.vp9.ivf.json"},
 		}},
 	})
 }
