4b6c5f768fed8b77306351d66499b2ded6dc4dff
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2019-12-20 17:26:49 +0900

    tast-tests: Fix handling of user consent for crash reporter.
    The old way to set consent for sending crash was not applied in all
    cases.
    This change also adds new 2 test cases emulating having no consent, so
    that we can test the consent setters.
    
    BUG=chromium:1021782,chromium:970930
    TEST=tast run $DUT platform.UserCrash
    Change-Id: I412aa399cbf1998ce2b7d81efd50cdba35772693

--- src/chromiumos/tast/local/bundles/cros/platform/user_crash.go
+++ src/chromiumos/tast/local/bundles/cros/platform/user_crash.go
@@ -129,12 +129,32 @@ func testChronosCrasher(ctx context.Context, cr *chrome.Chrome, s *testing.State
 	}
 }
 
+// testChronosCrasherNoConsent tests that crasher exits by SIGSEGV with user "chronos".
+func testChronosCrasherNoConsent(ctx context.Context, cr *chrome.Chrome, s *testing.State) {
+	opts := crash.DefaultCrasherOptions()
+	opts.Consent = false
+	opts.Username = "chronos"
+	if err := crash.CheckCrashingProcess(ctx, cr, opts); err != nil {
+		s.Error("testChronosCrasherNoConsent failed: ", err)
+	}
+}
+
 // testRootCrasher tests that crasher exits by SIGSEGV with the root user.
 func testRootCrasher(ctx context.Context, cr *chrome.Chrome, s *testing.State) {
 	opts := crash.DefaultCrasherOptions()
 	opts.Username = "root"
 	if err := crash.CheckCrashingProcess(ctx, cr, opts); err != nil {
 		s.Error("testRootCrasher failed: ", err)
+	}
+}
+
+// testRootCrasherNoConsent tests that crasher exits by SIGSEGV with the root user.
+func testRootCrasherNoConsent(ctx context.Context, cr *chrome.Chrome, s *testing.State) {
+	opts := crash.DefaultCrasherOptions()
+	opts.Consent = false
+	opts.Username = "root"
+	if err := crash.CheckCrashingProcess(ctx, cr, opts); err != nil {
+		s.Error("testRootCrasherNoConsent failed: ", err)
 	}
 }
 
@@ -345,7 +365,7 @@ func UserCrash(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to restart UI job")
 	}
 
-	cr, err := chrome.New(ctx, chrome.KeepState())
+	cr, err := chrome.New(ctx)
 	if err != nil {
 		s.Fatal("Chrome login failed: ", err)
 	}
@@ -366,5 +386,9 @@ func UserCrash(ctx context.Context, s *testing.State) {
 		testMaxEnqueuedCrash,
 		testCrashLogsCreation,
 		testCrashLogInfiniteRecursion,
-	}, true)
+	}, true, true)
+	crash.RunCrashTests(ctx, cr, s, []func(context.Context, *chrome.Chrome, *testing.State){
+		testChronosCrasherNoConsent,
+		testRootCrasherNoConsent,
+	}, false, true)
 }
