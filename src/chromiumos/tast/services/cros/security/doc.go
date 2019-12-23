ffa9d2bbfa69d8930da67c766809ad032ebb0f6f
Victor Hsieh <victorhsieh@chromium.org>
2019-12-23 10:33:51 -0800

    tast-tests: add security.BootLockbox test
    Boot lockbox is a key-value storage, which only allows writing before any login. As a result, the test requires a reboot.
    
    BUG=chromium:1041137
    TEST=tast run $DUT security.BootLockbox
    
    Change-Id: I16131bc9ec50852fda2ece2b11160b6eea5fa75f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1990427
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Vaibhav Raheja <vraheja@chromium.org>
    Tested-by: Vaibhav Raheja <vraheja@chromium.org>

---
+++ src/chromiumos/tast/services/cros/security/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package security contains Tast test services that exercise core security-related features.
+//
+// Some of these tests could arguably live elsewhere (e.g. the kernel or platform packages),
+// but we group them here based on their relevance to security.
+package security
