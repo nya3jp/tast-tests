fb216507a8d5fdf74f9d6150db47d79725e175a6
matterchen <matterchen@google.com>
2019-12-23 13:17:23 +1100

    Integration test for sharing fonts with GuestOS
    As apart of the new feature for sharing fonts with the GuestOS,
    this integration test will test if this sharing is successful. This
    comprises of two things:
    
    1. Check that the fonts are reachable from the GuestOS
    2. Check that the new fonts are part of the font-config path
    
    BUG=chromium:903631
    TEST=this test (TODO, pending on other changes)
    
    Change-Id: I70cfa9c70da841ab48e8cab458527db0948b6fed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1980095
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Matthew Chen <matterchen@google.com>
    Commit-Queue: Matthew Chen <matterchen@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/shared_font_files.go
@@ -0,0 +1,54 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     SharedFontFiles,
+		Desc:     "Checks that the hostOS font files are shared with the guestOS and they are accessible",
+		Contacts: []string{"matterchen@google.com", "cros-containers-dev@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Name:      "installer",
+			Pre:       crostini.StartedByInstaller(),
+			Timeout:   10 * time.Minute,
+			ExtraData: []string{crostini.ImageArtifact},
+		}},
+		SoftwareDeps: []string{"chrome", "vm_host"},
+	})
+}
+
+func SharedFontFiles(ctx context.Context, s *testing.State) {
+	pre := s.PreValue().(crostini.PreData)
+	cont := pre.Container
+
+	const sharedFonts = "/mnt/chromeos/fonts"
+	s.Log("1. Verifying mounted fonts dir exists")
+
+	cmd := cont.Command(ctx, "ls", sharedFonts)
+	if outBytes, err := cmd.Output(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Failed to list fonts directory : ", err)
+	} else if len(outBytes) == 0 {
+		s.Fatal("Fonts directory is empty")
+	}
+
+	s.Log("2. Verifying one of the available fonts comes from mounted fonts dir")
+	cmd = cont.Command(ctx, "fc-list")
+	if outBytes, err := cmd.Output(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Failed to call fc-list : ", err)
+	} else if !strings.Contains(string(outBytes), sharedFonts) {
+		s.Fatal("Host fonts not part of font-config path")
+	}
+}
