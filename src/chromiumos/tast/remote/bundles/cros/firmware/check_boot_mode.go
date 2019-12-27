84287c5aa869c27bf49fd2d52841d5260e75de49
Greg Edelston <gredelston@google.com>
2019-12-27 13:29:55 -0700

    Create RPC service to check DUT boot mode
    Context: We are implementing firmware-testing in Tast.
    Many firmware tests rely on the DUT being in dev mode or recovery mode.
    This new service lets us verify that the DUT is in the expected mode.
    It also creates a new test, CheckBootMode, to exercise this service.
    
    Later, when we are able to switch the DUT into dev/recovery mode, it
    will probably make sense to rename this new CheckBootMode test as
    SwitchBootMode. But for now, the test's name should accurately reflect
    what functionality it exercises.
    
    BUG=chromium:1038115, chromium:1037911
    TEST=tast run firmware.CheckBootMode $DUT_IP
    
    Change-Id: Id4fa8bdb56c02269207b5b5342c9b053c27ee27f

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/check_boot_mode.go
@@ -0,0 +1,60 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+
+	"chromiumos/tast/rpc"
+	fwpb "chromiumos/tast/services/cros/firmware"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        CheckBootMode,
+		Desc:        "Verifies that remote tests can check whether the DUT is in normal, dev, and recovery mode",
+		Contacts:    []string{"chromeos-engprod@google.com"},
+		ServiceDeps: []string{"tast.cros.firmware.UtilsService"},
+		Attr:        []string{"group:mainline", "informational"},
+	})
+}
+
+func CheckBootMode(ctx context.Context, s *testing.State) {
+	// Connect to the gRPC server on the DUT.
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC: ", err)
+	}
+	defer cl.Close(ctx)
+	utils := fwpb.NewUtilsServiceClient(cl.Conn)
+
+	// checkBootMode wraps an RPC call to check whether the DUT is in the specified mode.
+	checkBootMode := func(bootMode fwpb.BootMode) bool {
+		req := &fwpb.CheckBootModeRequest{BootMode: bootMode}
+		res, err := utils.CheckBootMode(ctx, req)
+		if err != nil {
+			s.Fatalf("Error when calling fwpb.CheckBootMode(%s): %s", bootMode, err)
+		}
+		return res.GetVerified()
+	}
+
+	// DUT should start in normal mode.
+	isNormal := checkBootMode(fwpb.BootMode_BOOT_MODE_NORMAL)
+	if !isNormal {
+		s.Error("DUT was not in Normal mode at start of test")
+	}
+	isDev := checkBootMode(fwpb.BootMode_BOOT_MODE_DEV)
+	if isDev {
+		s.Error("DUT was thought to be in Dev mode at start of test")
+	}
+	isRecovery := checkBootMode(fwpb.BootMode_BOOT_MODE_RECOVERY)
+	if isRecovery {
+		s.Error("DUT was thought to be in Recovery mode at start of test")
+	}
+
+	// TODO (gredelston): When we have the ability to reboot the DUT into dev/recovery mode,
+	// switch into each mode, and check whether we are in the expected state.
+}
