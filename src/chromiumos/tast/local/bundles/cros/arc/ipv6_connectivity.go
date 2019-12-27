7ae5123d6e205c4940669df61ead825f1c3928b2
Taoyu Li <taoyl@google.com>
2019-12-27 15:08:00 +0900

    arc: network: Add tast for basic IPv6 connectivity
    This patch adds a tast test case to verify IPv6 reachability to
    well-known literal IPv6 addresses and IPv6-only hostnames.
    
    BUG=b:142242490
    TEST=tast run device arc.IPv6Connectivity
    
    Change-Id: I2833336f5ed074b98dbd202dc037f7242a7fcefc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1982007
    Tested-by: Taoyu Li <taoyl@chromium.org>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Commit-Queue: Taoyu Li <taoyl@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arc/ipv6_connectivity.go
@@ -0,0 +1,57 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         IPv6Connectivity,
+		Desc:         "Checks IPv6 connectivity inside ARC",
+		Contacts:     []string{"taoyl@google.com", "cros-networking@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"android", "chrome"},
+		Pre:          arc.Booted(),
+	})
+}
+
+func IPv6Connectivity(ctx context.Context, s *testing.State) {
+	const (
+		pingTimeout      = 10 * time.Second
+		googleDNSIPv6    = "2001:4860:4860::8888"
+		googleDotComIPv6 = "ipv6.google.com"
+	)
+	a := s.PreValue().(arc.PreData).ARC
+
+	// Verify global IPv6 address is configured correctly.
+	out, err := a.Command(ctx, "/system/bin/ip", "-6", "addr", "show", "scope", "global").Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatalf("Failed to get address information: %s", err)
+	}
+	if len(out) == 0 {
+		s.Fatal("No global IPv6 address is configured")
+	}
+
+	// Verify connectivity to literal IPv6 address destination.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		return a.Command(ctx, "/system/bin/ping6", "-c1", "-w1", googleDNSIPv6).Run()
+	}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+		s.Errorf("Cannot ping %s: %s", googleDNSIPv6, err)
+	}
+
+	// Verify connectivity to IPv6-only host name.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		return arc.BootstrapCommand(ctx, "/system/bin/ping6", "-c1", "-w1", googleDotComIPv6).Run()
+	}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+		s.Errorf("Cannot ping %s: %s", googleDotComIPv6, err)
+	}
+}
