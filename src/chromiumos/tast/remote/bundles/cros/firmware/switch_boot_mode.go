9a66d33a9ac6c6c77f50e0ed2451c139bfb2eec8
Greg Edelston <gredelston@google.com>
2019-12-27 13:29:55 -0700

    Create RPC service to check DUT boot mode
    Context: We are implementing firmware-testing in Tast.
    Many firmware tests rely on the DUT being in dev mode or recovery mode.
    This CL allows us to verify that the DUT is in the expected mode.
    It also sets up a new SwitchBootMode test, which we will use when the
    mode-switcher has been implemented.
    
    BUG=chromium:1038115, chromium:1037911
    TEST=tast run firmware.SwitchBootMode $DUT_IP
    
    Change-Id: Id4fa8bdb56c02269207b5b5342c9b053c27ee27f

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/switch_boot_mode.go
@@ -0,0 +1,54 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+
+	"chromiumos/tast/rpc"
+	fwpb "chromiumos/tast/services/cros/firmware"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        SwitchBootMode,
+		Desc:        "Verifies that system can transition between normal, dev, and recovery mode",
+		Contacts:    []string{"tast-owners@google.com"},
+		ServiceDeps: []string{"tast.cros.firmware.UtilsService"},
+		Attr:        []string{"group:mainline", "informational"},
+	})
+}
+
+func SwitchBootMode(ctx context.Context, s *testing.State) {
+	// Connect to the gRPC server on the DUT.
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC: ", err)
+	}
+	defer cl.Close(ctx)
+	utils := fwpb.NewUtilsServiceClient(cl.Conn)
+
+	// checkBootMode wraps an RPC call to check whether the DUT is in the specified mode.
+	checkBootMode := func(bootMode fwpb.BootMode) bool {
+		req := &fwpb.CheckBootModeRequest{BootMode: bootMode}
+		res, err := utils.CheckBootMode(ctx, req)
+		if err != nil {
+			s.Fatalf("Error when calling fwpb.CheckBootMode(%s): %s", bootMode, err)
+		}
+		return res.GetVerified()
+	}
+
+	// DUT should start in normal mode.
+	isNormal := checkBootMode(fwpb.BootMode_BOOT_MODE_NORMAL)
+	if !isNormal {
+		s.Error("DUT was not in Normal mode at start of test")
+	}
+
+	// TODO (gredelston): Test state transitions:
+	// Normal > Dev
+	// Dev > Rec
+	// Rec > Normal
+}
