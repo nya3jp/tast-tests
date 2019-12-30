6148113bfb4aef56d6992e9c4346acb84459d70d
Yen-lin Lai <yenlinlai@google.com>
2019-12-30 13:10:55 +0800

    WIP: wifi: add wifi.TestContext
    Somehow further simplify the code in test body.
    
    BUG=chromium:1034875
    TEST=tast run -var=router=$ROUTER $DUT network.SimpleConnect*
    
    Cq-Depend: chromium:1971654
    Change-Id: I0d5e5197ecc908f23ec74c8255d716d02a5d8b39

---
+++ src/chromiumos/tast/remote/wifi/test_context.go
@@ -0,0 +1,153 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/host"
+	"chromiumos/tast/remote/network/ping"
+	"chromiumos/tast/remote/wifi/hostap"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/testing"
+)
+
+// TestContext sets up the context for a basic wifi test.
+type TestContext struct {
+	dut        *dut.DUT
+	rpc        *rpc.Client
+	routerHost *host.SSH
+	router     *Router
+	curService *network.Service
+	curHostAP  *HostAPHandle
+}
+
+const (
+	// VarRouter is the variable used for TestContext to get router hostname.
+	VarRouter = "router"
+)
+
+// NewTestContext connects to rotuer and gRPC on DUT and then returns a TestContext.
+// TODO: kind of awkward that tast-lint complains about passing testing.State,
+// and we'll always have to extract all the information outside.
+func NewTestContext(ctx context.Context, dut *dut.DUT, rpcHint *testing.RPCHint, routerHostname string) (ret *TestContext, retErr error) {
+	tc := &TestContext{}
+	defer func() {
+		if retErr != nil {
+			tc.Close(ctx)
+		}
+	}()
+	var err error
+	tc.dut = dut
+	tc.rpc, err = rpc.Dial(ctx, dut, rpcHint, "cros")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect rpc")
+	}
+	if routerHostname == "" {
+		tc.routerHost, err = dut.DefaultWifiRouterHost(ctx)
+	} else {
+		var sopt host.SSHOptions
+		sopt.Hostname = routerHostname
+		sopt.KeyDir = dut.KeyDir()
+		sopt.KeyFile = dut.KeyFile()
+		sopt.ConnectTimeout = 10 * time.Second
+		tc.routerHost, err = host.NewSSH(ctx, &sopt)
+	}
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect router")
+	}
+	tc.router, err = NewRouter(ctx, tc.routerHost)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create router object")
+	}
+	rand.Seed(time.Now().UnixNano())
+	return tc, nil
+}
+
+// Close the connections created by TestContext.
+func (tc *TestContext) Close(ctx context.Context) error {
+	var err error
+	if tc.router != nil {
+		if err2 := tc.router.Close(ctx); err2 != nil {
+			err = errors.Wrap(err2, "failed to close rotuer")
+		}
+	}
+	if tc.routerHost != nil {
+		if err2 := tc.routerHost.Close(ctx); err2 != nil {
+			err = errors.Wrap(err2, "failed to close router ssh")
+		}
+	}
+	// Ignore the error of rpc.Close as aborting rpc daemon will always have error.
+	if tc.rpc != nil {
+		tc.rpc.Close(ctx)
+	}
+	// Do not close DUT, it'll be closed by the framework.
+	return err
+}
+
+// ConfigureAP configures the router to provide a wifi service with the options specified.
+func (tc *TestContext) ConfigureAP(ctx context.Context, ops ...hostap.Option) (*HostAPHandle, error) {
+	apConf := hostap.NewConfig(ops...)
+	return tc.router.StartHostAP(ctx, apConf)
+}
+
+// DeconfigAP stops the wifi service on router.
+func (tc *TestContext) DeconfigAP(ctx context.Context, ap *HostAPHandle) error {
+	return tc.router.StopHostAP(ctx, ap)
+}
+
+// ConnectWifi asks DUT to connect to the given wifi service.
+func (tc *TestContext) ConnectWifi(ctx context.Context, ap *HostAPHandle) error {
+	wc := network.NewWifiClient(tc.rpc.Conn)
+
+	config := &network.Config{
+		Ssid: ap.Config().Ssid,
+	}
+	service, err := wc.Connect(ctx, config)
+	if err != nil {
+		return err
+	}
+	tc.curService = service
+	tc.curHostAP = ap
+	return nil
+}
+
+// DisconnectWifi asks DUT to disconnect from current wifi service and removes the configuration.
+func (tc *TestContext) DisconnectWifi(ctx context.Context) error {
+	var err error
+	wc := network.NewWifiClient(tc.rpc.Conn)
+	if _, err2 := wc.Disconnect(ctx, tc.curService); err2 != nil {
+		err = errors.Wrap(err2, "failed to disconnect")
+	}
+	ssid := tc.curHostAP.Config().Ssid
+	if _, err2 := wc.DeleteEntriesForSSID(ctx, &network.SSID{Ssid: ssid}); err2 != nil {
+		err = errors.Wrapf(err, "failed to delete entries for ssid=%q, err=%q", ssid, err2.Error())
+	}
+	tc.curService = nil
+	tc.curHostAP = nil
+	return err
+}
+
+// PingFromDUT tests the connectivity between DUT and router through currently connected wifi service.
+func (tc *TestContext) PingFromDUT(ctx context.Context) error {
+	if tc.curHostAP == nil {
+		return errors.New("not connected")
+	}
+	pr := ping.NewRunner(tc.dut)
+	res, err := pr.Ping(ctx, tc.curHostAP.ServerIP().String())
+	if err != nil {
+		return err
+	}
+	testing.ContextLogf(ctx, "ping statistics=%+v", res)
+	if res.Sent != res.Received {
+		return errors.New("Some packets are lost in ping")
+	}
+	return nil
+}
