a4f4bd3503150a15448838e9e119b8657c0d0ef1
Shuhei Takahashi <nya@chromium.org>
2020-01-06 12:44:33 +0900

    crash: Add SenderRetry test.
    This is a part of the patches to port logging_CrashSender to Tast.
    
    crash.SenderRetry tests that crash_sender retries uploading a crash
    dump if it encounters an error on upload.
    
    crash.SenderRetry corresponds to "sender_send_fails" subtest in
    logging_CrashSender. The original test used to not test the retry
    behavior, but the new test adds that.
    
    BUG=chromium:978255
    TEST=fast_build.sh -T
    TEST=tast run kohaku crash.SenderRetry
    
    Change-Id: I47b292f39908ded52d4fe6cd6aa99b9ebf2e8b05

---
+++ src/chromiumos/tast/local/bundles/cros/crash/sender_retry.go
@@ -0,0 +1,86 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crash
+
+import (
+	"context"
+	"os"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"chromiumos/tast/local/bundles/cros/crash/sender"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SenderRetry,
+		Desc: "Check that crash_sender failures are retried",
+		Contacts: []string{
+			"mutexlox@chromium.org",
+			"iby@chromium.org",
+			"cros-monitoring-forensics@google.com",
+			"nya@chromium.org", // ported to Tast
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "metrics_consent"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func SenderRetry(ctx context.Context, s *testing.State) {
+	crashDir, err := sender.SetUp(ctx, s.PreValue().(*chrome.Chrome))
+	if err != nil {
+		s.Fatal("Setup failed: ", err)
+	}
+	defer sender.TearDown()
+	defer os.RemoveAll(crashDir)
+
+	const basename = "some_program.1.2.3"
+	exp, err := sender.AddFakeMinidumpCrash(ctx, crashDir, basename)
+	if err != nil {
+		s.Fatal("Failed to add a fake minidump crash: ", err)
+	}
+
+	// Simulate a failed upload.
+	if err := sender.EnableMock(false); err != nil {
+		s.Fatal("Failed to set mock mode: ", err)
+	}
+
+	got, err := sender.Run(ctx, crashDir)
+	if err != nil {
+		s.Fatal("Failed to run crash_sender: ", err)
+	}
+	exp.ImageType = "mock-fail" // set by mock code
+	want := []*sender.SendResult{{
+		Success: false,
+		Data:    *exp,
+	}}
+	if diff := cmp.Diff(got, want, cmpopts.IgnoreFields(sender.SendResult{}, "Schedule")); diff != "" {
+		s.Log("Results mismatch (-got +want): ", diff)
+		s.Errorf("crash_sender sent unexpected %d results; see logs for diff", len(got))
+	}
+
+	// Simulate a successful upload.
+	if err := sender.EnableMock(true); err != nil {
+		s.Fatal("Failed to set mock mode: ", err)
+	}
+
+	got, err = sender.Run(ctx, crashDir)
+	if err != nil {
+		s.Fatal("Failed to run crash_sender: ", err)
+	}
+	exp.ImageType = ""
+	want = []*sender.SendResult{{
+		Success: true,
+		Data:    *exp,
+	}}
+	if diff := cmp.Diff(got, want, cmpopts.IgnoreFields(sender.SendResult{}, "Schedule")); diff != "" {
+		s.Log("Results mismatch (-got +want): ", diff)
+		s.Errorf("crash_sender sent unexpected %d results; see logs for diff", len(got))
+	}
+}
