bc27376494e9ccea6da7f3a9ccf16036d7056b10
Fergus Dall <sidereal@google.com>
2020-01-06 14:16:19 +1100

    test-tests: Add test for VM crash reporting
    This test triggers a crash inside the VM and checks that the crash report
    is generated. This is currently expected to fail as this feature is not yet
    implemented.
    
    TEST=ran locally
    BUG=chromium:703926
    
    Change-Id: I1c2aaa2ed6e38e8a5d524f03383dad6447b59055

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/crash_reporter.go
@@ -0,0 +1,70 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/crash"
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CrashReporter,
+		Desc:         "Check that crashes inside the VM produce crash reports",
+		Contacts:     []string{"sidereal@google.com", "mutexlox@google.com"},
+		SoftwareDeps: []string{"chrome", "metrics_consent", "vm_host"},
+		Attr:         []string{"group:mainline", "informational"},
+		Params: []testing.Param{
+			{
+				Name:      "artifact",
+				Pre:       crostini.StartedByArtifact(),
+				Timeout:   7 * time.Minute,
+				ExtraData: []string{crostini.ImageArtifact},
+			},
+			{
+				Name:    "download",
+				Pre:     crostini.StartedByDownload(),
+				Timeout: 10 * time.Minute,
+			},
+			{
+				Name:    "download_buster",
+				Pre:     crostini.StartedByDownloadBuster(),
+				Timeout: 10 * time.Minute,
+			},
+		},
+	})
+}
+
+func CrashReporter(ctx context.Context, s *testing.State) {
+	pre := s.PreValue().(crostini.PreData)
+
+	if err := crash.SetUpCrashTest(ctx, crash.WithConsent(pre.Chrome)); err != nil {
+		s.Fatal("Failed to set up crash test: ", err)
+	}
+	defer crash.TearDownCrashTest()
+
+	oldFiles, err := crash.GetCrashes(crash.SystemCrashDir)
+	if err != nil {
+		s.Fatal("Failed to get original crashes: ", err)
+	}
+
+	// Trigger a crash in the root namespace of the VM
+	cmd := pre.Container.VM.Command(ctx, "python3", "-c", "import os\nos.abort()")
+	// Ignore errors as this command is supposed to crash
+	_ = cmd.Run()
+	s.Log("Triggered a crash in the VM")
+
+	if _, err := crash.WaitForCrashFiles(ctx, []string{crash.UserCrashDir}, oldFiles, []string{`.*\.meta`, `.*\.dmp`}); err != nil {
+		s.Error("Couldn't find expected files: ", err)
+	}
+
+	// TODO: Check that the values in the metadata file make sense
+	// In paticular, check that the guest OS release (e.g. "buster") is
+	// available and that the board is set to "tatl" or "tael".
+}
