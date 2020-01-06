0d28fe0888462dbed3802bbcc45028a773255e69
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-01-06 18:44:53 +0900

    tast-tests: Mock sending generated report.
    This change only implements mock sending of a crash report, but does not
    verify the result from crash_sender. That will be added by following
    changes.
    The original code in autotest is:
    src/third_party/autotest/files/client/cros/crash/crash_test.py
    
    TEST=tast run $DUT_IP platform.UserCrash
    TEST=fast_build.sh -T
    BUG=chromium:970930
    
    Change-Id: I755f2e5c261bc9d5511dff8863107abbf8ea8bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2004628
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Tested-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- src/chromiumos/tast/local/crash/crash.go
+++ src/chromiumos/tast/local/crash/crash.go
@@ -49,6 +49,9 @@ const (
 	UserCrashDir = "/home/chronos/user/crash"
 	// userCrashStash is a directory to stash pre-existing crash reports of currently logged in user during crash tests.
 	userCrashStash = "/home/chronos/user/crash.real"
+	// userCrashDirs is used for finding the directory name containing a hash for current logged-in user,
+	// in order to compare it with crash reporter log.
+	userCrashDirs = "/home/chronos/u-*/crash"
 
 	// BIOSExt is the extension for bios crash files.
 	BIOSExt = ".bios_log"
@@ -125,6 +128,25 @@ func GetCrashes(dirs ...string) ([]string, error) {
 		}
 	}
 	return crashFiles, nil
+}
+
+// GetCrashDir gives the path to the crash directory for given username.
+func GetCrashDir(username string) (string, error) {
+	if username == "root" || username == "crash" {
+		return SystemCrashDir, nil
+	}
+	p, err := filepath.Glob(userCrashDirs)
+	if err != nil {
+		// This only happens when userCrashDirs is malformed.
+		return "", errors.Wrapf(err, "failed to list up files with pattern [%s]", userCrashDirs)
+	}
+	if len(p) == 0 {
+		return LocalCrashDir, nil
+	}
+	if len(p) > 1 {
+		return "", errors.Errorf("Wrong number of users logged in; got %d, want 1 or 0", len(p))
+	}
+	return p[0], nil
 }
 
 // WaitForCrashFiles waits for each regex in regexes to match a file in dirs that is not also in oldFiles.
