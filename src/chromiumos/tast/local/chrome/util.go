78064c8272943770637982ddbe09bc6560e8c105
Tetsui Ohkubo <tetsui@chromium.org>
2020-01-07 15:48:54 +0900

    tast: Copy user crashes to system crash dir
    Every time Chrome restarts, Tast clears user directory, which contains
    crashes during Tast test runs. In order to make them accessible from
    stainless, we need to copy them manually.
    
    Normally user crashes are written to /home/user/(hash)/crash as they
    contain PII, but for test images they are written to /home/chronos/crash
    . https://crrev.com/c/1986701
    
    TEST=manual(insert Log.wtf in system_server and triggered during running
    arc.ChromeVirtualKeyboard, and confirmed the log is properly copied to
    /var/spool/crash)
    BUG=chromium:1039567
    
    Change-Id: Ie59b052c801bbb36691bb1f0496626cab9c87569

--- src/chromiumos/tast/local/chrome/util.go
+++ src/chromiumos/tast/local/chrome/util.go
@@ -5,6 +5,8 @@
 package chrome
 
 import (
+	"io"
+	"io/ioutil"
 	"os"
 	"os/user"
 	"path/filepath"
@@ -35,4 +37,62 @@ func chownContents(dir string, username string) error {
 		}
 		return os.Chown(p, int(uid), int(gid))
 	})
+}
+
+// moveUserCrashDumps copies the contents of the user crash directory to the
+// system crash directory.
+func moveUserCrashDumps() error {
+	const (
+		userCrashDir   = "/home/chronos/crash"
+		systemCrashDir = "/var/spool/crash"
+	)
+
+	if err := os.MkdirAll(systemCrashDir, 02770); err != nil {
+		return err
+	}
+
+	fis, err := ioutil.ReadDir(userCrashDir)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	copyFile := func(dst, src string) error {
+		out, err := os.Create(dst)
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+
+		in, err := os.Open(src)
+		if err != nil {
+			return err
+		}
+		defer in.Close()
+
+		if _, err := io.Copy(out, in); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	for _, fi := range fis {
+		if !fi.Mode().IsRegular() {
+			continue
+		}
+
+		// As they are in different partitions, os.Rename() doesn't work.
+		dst := filepath.Join(systemCrashDir, fi.Name())
+		src := filepath.Join(userCrashDir, fi.Name())
+		if err := copyFile(dst, src); err != nil {
+			return err
+		}
+		if err := os.Remove(src); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
