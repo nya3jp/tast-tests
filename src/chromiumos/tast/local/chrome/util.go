0a66d937baa9f58a0223208001338bc912c9b80b
Tetsui Ohkubo <tetsui@chromium.org>
2020-01-07 15:48:54 +0900

    tast: Copy user crashes to system crash dir
    Every time Chrome restarts, Tast clears user directory, which contains
    crashes during Tast test runs. In order to make them accessible from
    stainless, we need to copy them manually.
    
    Normally user crashes are written to /home/user/(hash)/crash as they
    contain PII, but for test images they are written to /home/chronos/crash
    . https://crrev.com/c/1986701
    
    TEST=manual(insert Log.wtf in system_server and triggered during running
    arc.ChromeVirtualKeyboard, and confirmed the log is properly copied to
    /var/spool/crash)
    BUG=chromium:1039567
    
    Change-Id: Ie59b052c801bbb36691bb1f0496626cab9c87569

--- src/chromiumos/tast/local/chrome/util.go
+++ src/chromiumos/tast/local/chrome/util.go
@@ -5,12 +5,14 @@
 package chrome
 
 import (
+	"io/ioutil"
 	"os"
 	"os/user"
 	"path/filepath"
 	"strconv"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/fsutil"
 )
 
 // chownContents recursively chowns dir's contents to username's uid and gid.
@@ -35,4 +37,61 @@ func chownContents(dir string, username string) error {
 		}
 		return os.Chown(p, int(uid), int(gid))
 	})
+}
+
+// moveUserCrashDumps copies the contents of the user crash directory to the
+// system crash directory.
+func moveUserCrashDumps() error {
+	// Normally user crashes are written to /home/user/(hash)/crash as they
+	// contain PII, but for test images they are written to /home/chronos/crash.
+	// https://crrev.com/c/1986701
+	const (
+		userCrashDir   = "/home/chronos/crash"
+		systemCrashDir = "/var/spool/crash"
+		crashGroup     = "crash-access"
+	)
+
+	g, err := user.LookupGroup(crashGroup)
+	if err != nil {
+		return err
+	}
+	gid, err := strconv.ParseInt(g.Gid, 10, 32)
+	if err != nil {
+		return errors.Wrapf(err, "failed to parse gid %q", g.Gid)
+	}
+
+	if err := os.MkdirAll(systemCrashDir, 02770); err != nil {
+		return err
+	}
+
+	if err := os.Chown(systemCrashDir, 0, int(gid)); err != nil {
+		return err
+	}
+
+	fis, err := ioutil.ReadDir(userCrashDir)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	for _, fi := range fis {
+		if !fi.Mode().IsRegular() {
+			continue
+		}
+
+		// As they are in different partitions, os.Rename() doesn't work.
+		src := filepath.Join(userCrashDir, fi.Name())
+		dst := filepath.Join(systemCrashDir, fi.Name())
+		if err := fsutil.MoveFile(src, dst); err != nil {
+			return err
+		}
+
+		if err := os.Chown(dst, 0, int(gid)); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
