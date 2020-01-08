6c6d658ac451db6f87c8108823dd0b2ad7609d16
Nicolas Norvez <norvez@chromium.org>
2020-01-08 15:17:17 -0800

    tast-tests: ReportDiskUsage requires |chrome|
    Some directories (e.g. chromeos-assets, chrome) that are checked by the
    test should only be present when Chrome is installed, not on Chrome-less
    devices.
    Make the test require the |chrome| dependency.
    
    BUG=b:146900389
    BUG=chromium:537081
    TEST=./platform/tast/fast_build.sh -T
    TEST=tast run ${NOCTURNE_IP} platform.ReportDiskUsage
    TESST=run platform.ReportDiskUsage against an amd64-generic VM, as per
    http://go/cros-vm#Run-a-Tast-test-in-the-VM
    
    Change-Id: I8baae60cbde8333b07b8f93e74c6997ea70405b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1992541
    Tested-by: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Auto-Submit: Nicolas Norvez <norvez@chromium.org>
    Commit-Queue: Nicolas Norvez <norvez@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/platform/report_disk_usage.go
+++ src/chromiumos/tast/local/bundles/cros/platform/report_disk_usage.go
@@ -25,8 +25,8 @@ func init() {
 		Func:     ReportDiskUsage,
 		Desc:     "Reports available disk space in the root filesystem",
 		Contacts: []string{"norvez@chromium.org", "sarthakkukreti@chromium.org", "chromeos-storage@google.com"},
-		// chromeos-assets is only available on internal builds -> require |chrome_internal|
-		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		// chromeos-assets is not available on devices without Chrome, require |chrome|
+		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 	})
 }
