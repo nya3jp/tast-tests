67e2c8f2678a0a3bd4f4b31c807846c9e5c5ff5f
Meng-Huan Yu <menghuan@chromium.org>
2020-01-08 14:23:36 +0800

    tast-test: Add remote godoc for factory test
    BUG=b:147337631
    TEST=preCQ passed
    
    Change-Id: Ie4bc5c84fc5ae831f03021426b58bec47e2f887b

--- src/chromiumos/tast/remote/bundles/cros/main.go
+++ src/chromiumos/tast/remote/bundles/cros/main.go
@@ -16,6 +16,7 @@ import (
 	_ "chromiumos/tast/remote/bundles/cros/arc"
 	_ "chromiumos/tast/remote/bundles/cros/crash"
 	_ "chromiumos/tast/remote/bundles/cros/example"
+	_ "chromiumos/tast/remote/bundles/cros/factory"
 	_ "chromiumos/tast/remote/bundles/cros/firmware"
 	_ "chromiumos/tast/remote/bundles/cros/hwsec"
 	_ "chromiumos/tast/remote/bundles/cros/meta"
