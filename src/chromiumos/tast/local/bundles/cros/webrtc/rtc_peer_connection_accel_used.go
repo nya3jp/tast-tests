66db3cf26a00dd4cfe18f4ea16812abd95a42524
Miguel Casas <mcasas@chromium.org>
2020-01-08 16:15:04 -0500

    tast/webrtc: teach RTCPeerConnectionAccelUsed to use a precondition
    This CL teaches RTCPeerConnectionAccelUsed to use preconditions just
    like its sibling RTCPeerConnection. This makes the test functions
    more similar, so they can be merged in a subsequent CL.
    
    A recent CL added an explicit flag to enable H264 encoding in AMD, I
    had to move that up into another precondition.
    
    Reshuffled a few comments in that file.
    
    BUG=chromium:1040230
    TEST=tast -verbose run ${IP} webrtc.*  on grunt
    
    Change-Id: I8985a2e579e515295c01abe5d6867349028215d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1992540
    Tested-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/webrtc/rtc_peer_connection_accel_used.go
+++ src/chromiumos/tast/local/bundles/cros/webrtc/rtc_peer_connection_accel_used.go
@@ -8,7 +8,9 @@ import (
 	"context"
 
 	"chromiumos/tast/local/bundles/cros/webrtc/peerconnection"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/pre"
 	"chromiumos/tast/local/webrtc"
 	"chromiumos/tast/testing"
 )
@@ -32,33 +34,35 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		Data:         append(webrtc.DataFiles(), peerconnection.LoopbackFile),
 		Attr:         []string{"group:mainline"},
+		// TODO(crbug.com/1017374): add "enc_vp9".
 		Params: []testing.Param{{
 			Name:              "enc_vp8",
 			Val:               rtcTest{codec: peerconnection.Encoding, profile: "VP8"},
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
+			Pre:               pre.ChromeVideoWithFakeWebcam(),
 		}, {
 			Name:              "dec_vp8",
 			Val:               rtcTest{codec: peerconnection.Decoding, profile: "VP8"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
-		},
-		// TODO(crbug.com/1017374): add "enc_vp9".
-		{
+			Pre:               pre.ChromeVideoWithFakeWebcam(),
+		}, {
 			Name:              "dec_vp9",
 			Val:               rtcTest{codec: peerconnection.Decoding, profile: "VP9"},
 			ExtraAttr:         []string{"informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			Pre:               pre.ChromeVideoWithFakeWebcam(),
 		}, {
 			Name:              "enc_h264",
 			Val:               rtcTest{codec: peerconnection.Encoding, profile: "H264"},
 			ExtraAttr:         []string{"informational"},
-			// "chrome_internal" is needed because H.264 is a proprietary codec.
-			ExtraSoftwareDeps: []string{caps.HWEncodeH264, "chrome_internal"},
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
+			Pre:               pre.ChromeVideoWithFakeWebcamAndH264AMDEncoder(),
 		}, {
 			Name:              "dec_h264",
 			Val:               rtcTest{codec: peerconnection.Decoding, profile: "H264"},
 			ExtraAttr:         []string{"informational"},
-			// "chrome_internal" is needed because H.264 is a proprietary codec.
-			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "chrome_internal"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
+			Pre:               pre.ChromeVideoWithFakeWebcam(),
 		}},
 	})
 }
@@ -66,5 +70,5 @@ func init() {
 // RTCPeerConnectionAccelUsed verifies that a PeerConnection uses accelerated encoding / decoding.
 func RTCPeerConnectionAccelUsed(ctx context.Context, s *testing.State) {
 	testOpt := s.Param().(rtcTest)
-	peerconnection.RunRTCPeerConnectionAccelUsed(ctx, s, testOpt.codec, testOpt.profile)
+	peerconnection.RunRTCPeerConnectionAccelUsed(ctx, s, s.PreValue().(*chrome.Chrome), testOpt.codec, testOpt.profile)
 }
