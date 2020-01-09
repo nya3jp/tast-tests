cab56b2a629202a333eaa91b8df6d44392d2c08b
Victor-Gabriel Savu <vsavu@google.com>
2020-01-09 14:55:52 +0100

    tast: add policy.AllowDinosaurEasterEgg
    Add test for the AllowDinosaurEasterEgg user policy.
    
    BUG=None
    TEST=tast run <ip> policy.AllowDinosaurEasterEgg.*
    
    Change-Id: I1e6f539cf8266ecf1243fd2756933537f762a224

---
+++ src/chromiumos/tast/local/bundles/cros/policy/allow_dinosaur_easter_egg.go
@@ -0,0 +1,126 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/policy"
+	"chromiumos/tast/local/policy/fakedms"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AllowDinosaurEasterEgg,
+		Desc: "Behavior of AllowDinosaurEasterEgg policy",
+		Contacts: []string{
+			"vsavu@google.com", // Test author
+			"kathrelkeld@chromium.org",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+	})
+}
+
+func AllowDinosaurEasterEgg(ctx context.Context, s *testing.State) {
+	// Start FakeDMS.
+	fdms, err := fakedms.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start FakeDMS: ", err)
+	}
+	defer fdms.Stop(ctx)
+
+	pb := fakedms.NewPolicyBlob()
+	if err = fdms.WritePolicyBlob(pb); err != nil {
+		s.Fatal("Failed to write policies to FakeDMS: ", err)
+	}
+
+	// Start a Chrome instance that will fetch policies from the FakeDMS.
+	cr, err := chrome.New(ctx,
+		chrome.Auth("tast-user@managedchrome.com", "test0000", "gaia-id"),
+		chrome.DMSPolicy(fdms.URL))
+	if err != nil {
+		s.Fatal("Chrome login failed: ", err)
+	}
+	defer cr.Close(ctx)
+
+	// Set up Chrome Test API connection.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	checkAllowDinosaurEasterEgg := func(ctx context.Context, s *testing.State, value *policy.AllowDinosaurEasterEgg) {
+		// Create a policy blob and have the FakeDMS serve it.
+		pb := fakedms.NewPolicyBlob()
+		pb.AddPolicies([]policy.Policy{value})
+		if err = fdms.WritePolicyBlob(pb); err != nil {
+			s.Fatal("Failed to write policies to FakeDMS: ", err)
+		}
+
+		// Refresh policies
+		if err = tconn.EvalPromise(ctx, `tast.promisify(chrome.autotestPrivate.refreshEnterprisePolicies)();`, nil); err != nil {
+			s.Fatal("Failed to refresh policies: ", err)
+		}
+
+		// Run actual test
+		conn, err := cr.NewConn(ctx, "chrome://dino")
+		if err != nil {
+			s.Fatal("Failed to connect to chrome: ", err)
+		}
+		defer func() {
+			// Close window (to refresh policy)
+			windows, err := ash.GetAllWindows(ctx, tconn)
+			if err != nil {
+				s.Fatal("Failed to get windows: ", err)
+			}
+
+			for _, window := range windows {
+				if err := window.CloseWindow(ctx, tconn); err != nil {
+					s.Fatal("Failed to close window: ", err)
+				}
+			}
+		}()
+		defer conn.Close()
+
+		var isBlocked bool
+		if err = conn.Eval(ctx, `document.querySelector('* /deep/ #main-frame-error div.snackbar') === null`, &isBlocked); err != nil {
+			s.Fatal("Could not read from dino page: ", err)
+		}
+
+		expectedBlocked := !(value.Stat == policy.StatusUnset) && value.Val
+
+		if isBlocked != expectedBlocked {
+			s.Errorf("Unexpected blocked behavior: got %t; want %t", isBlocked, expectedBlocked)
+		}
+	}
+
+	for _, param := range []struct {
+		// name is the subtest name.
+		name string
+		// value is the policy value.
+		value *policy.AllowDinosaurEasterEgg
+	}{
+		{
+			name:  "true",
+			value: &policy.AllowDinosaurEasterEgg{Val: true},
+		},
+		{
+			name:  "false",
+			value: &policy.AllowDinosaurEasterEgg{Val: false},
+		},
+		{
+			name:  "unset",
+			value: &policy.AllowDinosaurEasterEgg{Stat: policy.StatusUnset},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			checkAllowDinosaurEasterEgg(ctx, s, param.value)
+		})
+	}
+}
