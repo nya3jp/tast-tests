6d8fb78bd38b62e43a4b29b98ee126d2157ad119
Joon Ahn <joonbug@chromium.org>
2020-01-10 13:18:57 -0800

    Deflake crash reporter crash integration test
    BUG=chromium:1040266
    TEST=tast --verbose run $IP platform.CrashReporterCrash
    
    Change-Id: Icec68d7af67c38bf4d23c7713ce684cb50fd4171

--- src/chromiumos/tast/local/bundles/cros/platform/crash_reporter_crash.go
+++ src/chromiumos/tast/local/bundles/cros/platform/crash_reporter_crash.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -95,10 +96,14 @@ func CrashReporterCrash(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to restart anomaly detector: ", err)
 	}
 
-	// Intentionally crash metrics_daemon so that (doomed) crash_reporter will
-	// be called.
-	s.Log("Crashing metrics_daemon")
-	cmd := testexec.CommandContext(ctx, "pkill", "-sigsegv", "metrics_daemon")
+	s.Log("Starting a dummy process")
+	dummy := testexec.CommandContext(ctx, "/usr/bin/yes", ">", "/dev/null")
+	if err := dummy.Start(); err != nil {
+		s.Fatal("Failed to start a dummy process to kill: ", err)
+	}
+
+	s.Log("Crashing the dummy process")
+	cmd := testexec.CommandContext(ctx, "kill", "-sigsegv", strconv.Itoa(dummy.Process.Pid))
 	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
 		s.Fatal("Failed to induce an artifical crash: ", err)
 	}
@@ -106,6 +111,7 @@ func CrashReporterCrash(ctx context.Context, s *testing.State) {
 	s.Log("Waiting for crash_reporter failure files")
 	expectedRegexes := []string{`crash_reporter_failure\.\d{8}\.\d{6}\.0\.meta`,
 		`crash_reporter_failure\.\d{8}\.\d{6}\.0\.log`}
+
 	files, err := crash.WaitForCrashFiles(ctx, []string{crash.SystemCrashDir},
 		oldFiles, expectedRegexes)
 	if err != nil {
