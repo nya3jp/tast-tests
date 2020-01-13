39bdc130bf48e81674563acb874d564ee40d84c5
Victor-Gabriel Savu <vsavu@google.com>
2020-01-13 12:43:04 +0100

    Test update to illustrate reconnect bug
    BUG=None
    TEST=NONE
    
    Change-Id: Iecffce60dc4e11d9d0c6cd89caf774a4e30c7f92

--- src/chromiumos/tast/local/bundles/cros/wilco/api_request_bluetooth_data_notification.go
+++ src/chromiumos/tast/local/bundles/cros/wilco/api_request_bluetooth_data_notification.go
@@ -35,10 +35,29 @@ func APIRequestBluetoothDataNotification(ctx context.Context, s *testing.State) 
 	if err != nil {
 		s.Fatal("Unable to create DPSL Message Receiver: ", err)
 	}
-	defer rec.Stop()
 
 	request := dtcpb.RequestBluetoothDataNotificationRequest{}
 	response := dtcpb.RequestBluetoothDataNotificationResponse{}
+
+	if err := wilco.DPSLSendMessage(ctx, "RequestBluetoothDataNotification", &request, &response); err != nil {
+		s.Fatal("Unable to request notification: ", err)
+	}
+
+	testing.Sleep(ctx, 1*time.Second)
+
+	rec.Stop()
+
+	s.Log("ps wait")
+	testing.Sleep(ctx, 5*time.Second)
+
+	rec, err = wilco.NewDPSLMessageReceiver(ctx)
+	if err != nil {
+		s.Fatal("Unable to create DPSL Message Receiver: ", err)
+	}
+	defer rec.Stop()
+
+	request = dtcpb.RequestBluetoothDataNotificationRequest{}
+	response = dtcpb.RequestBluetoothDataNotificationResponse{}
 
 	if err := wilco.DPSLSendMessage(ctx, "RequestBluetoothDataNotification", &request, &response); err != nil {
 		s.Fatal("Unable to request notification: ", err)
