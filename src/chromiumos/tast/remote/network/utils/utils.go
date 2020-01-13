8ae11d6cacffc2d0598611d04ea8bd5a03d806d2
Yen-lin Lai <yenlinlai@google.com>
2020-01-13 18:06:51 +0800

    network: utils: move Commander interface to utils
    The interface will be useful for sharing common code on DUT and router.
    Move it to utils so that network related components can use it. (e.g.
    ping/runner, wifi package in later commits)
    Also apply the interface on ping.Runner in this cl.
    
    BUG=chromium:1034875
    TEST=fast_build.sh -T
    
    Change-Id: I9f50b22dbf8471c9daa039c0f1d2d38d2523c05b

---
+++ src/chromiumos/tast/remote/network/utils/utils.go
@@ -0,0 +1,15 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package utils provides utilities for network related.
+package utils
+
+import "chromiumos/tast/host"
+
+// Commander is an interface for those who provides Command() function.
+// It is used to hold both dut.DUT and host.SSH object.
+// TODO(crbug.com/1019537): use a more suitable ssh object.
+type Commander interface {
+	Command(string, ...string) *host.Cmd
+}
