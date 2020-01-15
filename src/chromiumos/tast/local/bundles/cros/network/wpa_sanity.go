8ce825ccae5dd730f5ba96fb8771b6f63bd82d57
Shuo-Peng Liao <deanliao@chromium.org>
2020-01-15 12:32:29 +0800

    network: Add WPASanity test.
    network.WPASanity is a sanity test for wpa_supplicant service.
    It tries to ping wpa_supplicant using "wpa_cli -i $iface ping" command,
    where $iface is the default WiFi interface name. It can capture the
    issue that wpa_supplicant is totally dead.
    
    Noted that it polls for a successful wpa_cli ping for 5 seconds to avoid
    false negative (wpa_supplicant isn't ready yet).
    
    BUG=chromium:1038551,b:146758562
    TEST=Run "tast run $DUT network.WPASanity" on a Chromebook with an
    affected image, e.g. R81-12775.0.0. It returns:
      Error at wpa_sanity.go:65: Failed to ping wpa_supplicant: \
      Failed to connect to non-global ctrl_ifname: wlan0  \
      error: No such file or directory
    
    Run the test on a Chromebook with an unaffected image, e.g.
    R81-12817.0.0. The tests passes.
    
    Change-Id: I0bd089c18172f7aa8a9ab64d2a28f2197b960d46

---
+++ src/chromiumos/tast/local/bundles/cros/network/wpa_sanity.go
@@ -0,0 +1,71 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WPASanity,
+		Desc:         "Verifies wpa_supplicant is up and running",
+		Contacts:     []string{"deanliao@google.com", "chromeos-kernel-wifi@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"wifi"},
+	})
+}
+
+func WPASanity(ctx context.Context, s *testing.State) {
+	manager, err := shill.NewManager(ctx)
+	if err != nil {
+		s.Fatal("Failed creating shill manager proxy: ", err)
+	}
+
+	iface, err := shill.GetWifiInterface(ctx, manager, 5*time.Second)
+	if err != nil {
+		s.Fatal("Could not get a WiFi interface: ", err)
+	}
+	s.Log("WiFi interface: ", iface)
+
+	var output string
+	logfile := "wpa_cli.log"
+
+	// In case wpa_cli produces multiple line output, in log message we show
+	// the path of wpa_cli.log.
+	wpacliOutput := func() string {
+		if strings.Contains(output, "\n") {
+			return "check wpa_cli logfile: /tmp/tast/results/latest/" +
+				"tests/network.WPASanity/" + logfile
+		}
+		return output
+	}
+
+	// Even if we got a WiFi device, iface, from shill, that does not imply
+	// that wpa_supplicant of the WiFi device is up and running. Poll for a
+	// successful wpa_cli ping for 5 seconds to avoid false negative.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		cmdOut, err := testexec.CommandContext(
+			ctx, "sudo", "-u", "wpa", "-g",
+			"wpa", "wpa_cli", "-i", iface, "ping").CombinedOutput()
+		ioutil.WriteFile(filepath.Join(s.OutDir(), logfile), cmdOut, 0644)
+		output = strings.TrimSpace(string(cmdOut))
+		return err
+	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
+		s.Fatal("Failed to ping wpa_supplicant: " + wpacliOutput())
+	}
+
+	if !strings.Contains(output, "PONG") {
+		s.Fatal("Failed to see expected PONG reply: " + wpacliOutput())
+	}
+}
