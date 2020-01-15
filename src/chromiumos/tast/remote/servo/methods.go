e2a34f27ea0ac2b0c833a8387dc815543c5d7322
Greg Edelston <gredelston@google.com>
2020-01-15 16:17:02 -0700

    Add servo methods to get/set arbitrary controls
    Fingerprint testing will need to get/set the 'fw_wp_state' control;
    see attached bug.
    
    There are already a few methods in methods.go to get/set specific
    controls (setActChgPort() and DutVoltageMV()). Rather than repeating the
    getting and setting syntax for each control, it's cleaner to have a
    single Get() and Set() method which tests/packages can invoke as needed.
    
    I did not replace the two aforementioned get/set methods because I don't
    have the hardware necessary to test that change. I will follow up with
    aaboagye, who added those methods, separately.
    
    TEST=Run servo_echo.go with the following appended to the test body:
            orig_fw_wp_state, err := pxy.Servo().Get(ctx,
    		servo.CtrlFwWpState)
    	if err != nil {
    		s.Fatal("servo.Get() yielded error: ", err)
    	}
    	s.Logf("Got fw_wp_state %q", orig_fw_wp_state)
    	err = pxy.Servo().SetAndCheck(ctx, servo.CtrlFwWpState,
    		"force_on")
    	if err != nil {
    		s.Fatal("servo.SetAndCheck() yielded error: ", err)
    	}
    	pxy.Servo().Set(ctx, servo.CtrlFwWpState, "force_off")
    When I ran that test, I saw the following logged, with no errors:
    > Got fw_wp_state "force_off"
    BUG=chromium:991319
    
    Change-Id: I6936f816935e8b768fa738a7a7f4f4c51b58b8fd

--- src/chromiumos/tast/remote/servo/methods.go
+++ src/chromiumos/tast/remote/servo/methods.go
@@ -6,6 +6,18 @@ package servo
 
 import (
 	"context"
+
+	"chromiumos/tast/errors"
+)
+
+// A ServoControl contains the name of a gettable/settable control.
+type servoControl string
+
+// These are the Servo controls which can be get/set.
+const (
+	CtrlActiveChgPort servoControl = "active_chg_port"
+	CtrlDutVoltageMv  servoControl = "dut_voltage_mv"
+	CtrlFwWpState     servoControl = "fw_wp_state"
 )
 
 // Echo calls the Servo echo method.
@@ -29,13 +41,40 @@ func (s *Servo) SetActChgPort(ctx context.Context, port string) error {
 	// response.  However it seems strange because if the call didn't succeed, I believe run() will return an
 	// error in err anyways so the variable seems useless.  This is why I'm ignoring val and only returning err.
 	var val bool
-	err := s.run(ctx, newCall("set", "active_chg_port", port), &val)
+	err := s.run(ctx, newCall("set", CtrlActiveChgPort, port), &val)
 	return err
 }
 
 // DutVoltageMV reads the voltage present on the DUT port on fluffy.
 func (s *Servo) DutVoltageMV(ctx context.Context) (string, error) {
 	var voltageMV string
-	err := s.run(ctx, newCall("get", "dut_voltage_mv"), &voltageMV)
+	err := s.run(ctx, newCall("get", CtrlDutVoltageMv), &voltageMV)
 	return voltageMV, err
+}
+
+// Get returns the value of a specified control.
+func (s *Servo) Get(ctx context.Context, control servoControl) (string, error) {
+	var value string
+	err := s.run(ctx, newCall("get", string(control)), &value)
+	return value, err
+}
+
+// Set sets a specified control to a specified value.
+func (s *Servo) Set(ctx context.Context, control servoControl, value string) error {
+	var resp bool
+	err := s.run(ctx, newCall("set", string(control), value), &resp)
+	return err
+}
+
+// SetAndCheck sets a control to a specified value, and then verifies that it was set correctly.
+func (s *Servo) SetAndCheck(ctx context.Context, control servoControl, value string) error {
+	if err := s.Set(ctx, control, value); err != nil {
+		return errors.Wrapf(err, "failed to set %s to %s", control, value)
+	}
+	if checkedValue, err := s.Get(ctx, control); err != nil {
+		return errors.Wrapf(err, "failed to check %s", control)
+	} else if checkedValue != value {
+		return errors.Errorf("after attempting to set %s to %s, checked value was %s", control, value, checkedValue)
+	}
+	return nil
 }
