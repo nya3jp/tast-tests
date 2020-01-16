433e932a78de3370de6c6d4106ce1548b21d17d0
Ian Barkley-Yeung <iby@google.com>
2020-01-15 16:39:26 -0800

    Remove metrics.SetConsent
    metrics.SetConsent was the old way of getting metrics consent (mostly so
    that we could collect crashes). It wasn't very reliable.
    
    Replace the last use with SetUpCrashTest's consent-getter. This is a bit
    long-winded, because it requires us to log in and then log back out, but
    it should be more reliable, and mean we don't have to maintain 2 consent
    systems.
    
    It also fixes problems where the ExtraArgs being passed to Chrome were
    being ignored. (https://crbug.com/1042483)
    
    BUG=chromium:1042483
    Fixed: chromium:1042483
    TEST=tast -verbose run --extrauseflags chrome_internal  my_crbook ui.ChromeCrashNotLoggedIn.*
    
    Change-Id: I62029e78ab037c5fece8feeecf6d73d523c310cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2004088
    Tested-by: Ian Barkley-Yeung <iby@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/metrics/metrics.go
+++ src/chromiumos/tast/local/metrics/metrics.go
@@ -5,21 +5,12 @@
 package metrics
 
 import (
-	"context"
 	"io/ioutil"
 	"os"
 	"regexp"
 	"strings"
 
-	"github.com/golang/protobuf/proto"
-
-	"chromiumos/policy/enterprise_management"
 	"chromiumos/tast/errors"
-	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/cryptohome"
-	"chromiumos/tast/local/session"
-	"chromiumos/tast/local/sysutil"
-	"chromiumos/tast/testing"
 )
 
 const (
@@ -40,68 +31,4 @@ func HasConsent() (bool, error) {
 	}
 	s := strings.TrimRight(string(b), "\n")
 	return uuidPattern.MatchString(s), nil
-}
-
-// SetConsent sets up the system to have, or not to have metrics consent. Note
-// that if you use chrome.New() after this, you must use chrome.KeepState() as
-// one of the parameters. certFile should be the complete path to a PKCS #12
-// format file.
-func SetConsent(ctx context.Context, certFile string, consent bool) error {
-	if consent {
-		testing.ContextLog(ctx, "Setting up consent")
-	} else {
-		testing.ContextLog(ctx, "Removing consent")
-	}
-	privKey, err := session.ExtractPrivKey(certFile)
-	if err != nil {
-		return errors.Wrap(err, "failed to parse PKCS #12 file")
-	}
-	settings := &enterprise_management.ChromeDeviceSettingsProto{
-		MetricsEnabled: &enterprise_management.MetricsEnabledProto{
-			MetricsEnabled: proto.Bool(consent),
-		},
-	}
-	if err := session.SetUpDevice(ctx); err != nil {
-		return errors.Wrap(err, "failed to reset device ownership")
-	}
-	sm, err := session.NewSessionManager(ctx)
-	if err != nil {
-		return errors.Wrap(err, "could not bind to session manager")
-	}
-	if err := session.PrepareChromeForPolicyTesting(ctx, sm); err != nil {
-		return errors.Wrap(err, "failed to prepare Chrome for testing")
-	}
-	// Create clean vault for the test user, and start the session.
-	if err := cryptohome.RemoveVault(ctx, chrome.DefaultUser); err != nil {
-		return errors.Wrap(err, "failed to remove vault")
-	}
-	if err := cryptohome.CreateVault(ctx, chrome.DefaultUser, chrome.DefaultPass); err != nil {
-		return errors.Wrap(err, "failed to create vault")
-	}
-	if err := session.StoreSettings(ctx, sm, chrome.DefaultUser, privKey, nil, settings); err != nil {
-		return errors.Wrap(err, "failed to store user policy")
-	}
-	if consent {
-		// Create deprecated consent file.  This is created *after* the
-		// policy file in order to avoid a race condition where Chrome
-		// might remove the consent file if the policy's not set yet.
-		// We create it as a temp file first in order to make the creation
-		// of the consent file, owned by chronos, atomic.
-		// See crosbug.com/18413.
-		tempFile := legacyConsent + ".tmp"
-		if err := ioutil.WriteFile(tempFile, []byte("test-consent"), 0644); err != nil {
-			return errors.Wrapf(err, "failed to write to legacy consent file %s", tempFile)
-		}
-
-		if err := os.Chown(tempFile, int(sysutil.ChronosUID), int(sysutil.ChronosGID)); err != nil {
-			return errors.Wrapf(err, "failed to chown legacy consent file %s", tempFile)
-		}
-		if err := os.Rename(tempFile, legacyConsent); err != nil {
-			return errors.Wrapf(err, "failed to rename legacy consent file %s to %s", tempFile, legacyConsent)
-		}
-	} else {
-		os.Remove(legacyConsent)
-	}
-
-	return nil
 }
