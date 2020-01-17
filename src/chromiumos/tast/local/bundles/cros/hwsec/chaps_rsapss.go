0aee5f1ea2528dcb4462d6b2bb28f74495e228a1
John L Chen <zuan@chromium.org>
2020-01-18 02:43:05 +0800

    tast-tests: hwsec: Add test for testing RSA PSS in chaps
    BUG=chromium:924230
    TEST=tast run $dut hwsec.ChapsRSAPSS
    
    Change-Id: I42107c03b4fb93cf039d0abe323c55602425c81e

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/chaps_rsapss.go
@@ -0,0 +1,85 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/common/pkcs11"
+	libhwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ChapsRSAPSS,
+		Desc: "Verifies RSA PSS works with RSA keys (sign, verify, encrypt, decrypt) in chaps",
+		Attr: []string{"informational"},
+		Contacts: []string{
+			"zuan@chromium.org",
+			"cros-hwsec@chromium.org",
+		},
+		SoftwareDeps: []string{"chrome", "tpm2"},
+	})
+}
+
+func ChapsRSAPSS(ctx context.Context, s *testing.State) {
+	r, err := libhwseclocal.NewCmdRunner()
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Utilty creation error: ", err)
+	}
+	pkcs11Util, err := pkcs11.NewUtil(ctx, r, utility)
+	if err != nil {
+		s.Fatal("Failed to create PKCS#11 Utility: ", err)
+	}
+
+	// Prepare the scratchpad.
+	testFile1Path, testFile2Path, err := pkcs11Util.PrepareScratchpadAndTestFiles(ctx, "/tmp/ChapsRSAPSSTest")
+	if err != nil {
+		s.Fatal("Failed to initialize the scratchpad space: ", err)
+	}
+	// Remove all keys/certs, if any at the end. i.e. Cleanup after ourselves.
+	defer pkcs11Util.CleanupScratchpad(ctx)
+	// Note: Also, this test expects a clean keystore, in the sense that there should be no object with the same ID as those used by this test.
+
+	// Create the software-generated, then imported key.
+	importedKey, err := pkcs11Util.CreateRsaSoftwareKey(ctx, utility, "", "testkey1", "111111")
+	if err != nil {
+		s.Fatal("Failed to create software key: ", err)
+	}
+	defer func() {
+		if err := pkcs11Util.DestroyKey(ctx, importedKey); err != nil {
+			s.Error("Failed to clean up software key: ", err)
+		}
+	}()
+
+	// Create the TPM generated key.
+	generatedKey, err := pkcs11Util.CreateRsaGeneratedKey(ctx, utility, "", "testkey3", "333333")
+	if err != nil {
+		s.Fatal("Failed to create generated key: ", err)
+	}
+	defer func() {
+		if err := pkcs11Util.DestroyKey(ctx, generatedKey); err != nil {
+			s.Error("Failed to clean up generated key: ", err)
+		}
+	}()
+
+	keys := []*pkcs11.KeyInfo{importedKey, generatedKey}
+
+	// Test the various keys.
+	for _, k := range keys {
+		// Test the various mechanisms.
+		for _, m := range []pkcs11.MechanismInfo{pkcs11.GetMechanism("SHA1-RSA-PKCS-PSS"), pkcs11.GetMechanism("SHA256-RSA-PKCS-PSS"), pkcs11.GetMechanism("RSA-PKCS-PSS+SHA1"), pkcs11.GetMechanism("RSA-PKCS-PSS+SHA256")} {
+			if err = k.SignVerify(ctx, pkcs11Util, testFile1Path, testFile2Path, m); err != nil {
+				s.Fatal("SignVerify failed: ", err)
+			}
+		}
+	}
+}
