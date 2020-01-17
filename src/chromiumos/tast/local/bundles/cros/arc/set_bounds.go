60d42b69893349e660e05f490403e8b97fd87d9c
Hiroki Sato <hirokisato@google.com>
2020-01-17 15:55:03 +0900

    tast-tests: fix arc.SetBounds to start activities separately
    In arc.SetBounds, the test launches one activity, and clicking a button
    on the the app invokes to start another activity.
    
    This CL changes the test to start each activity separately.
    
    This change fixes current tast failure due to mismatch of activity name
    between arc.Activity instance held in test and actually running activity
    in ARC container.
    
    BUG=chromium:1042617
    TEST=tast run DUT arc.SetBounds
    Change-Id: Ia16dd021c3413e4af5ddb3d7ff22c48bb86feba8

--- src/chromiumos/tast/local/bundles/cros/arc/set_bounds.go
+++ src/chromiumos/tast/local/bundles/cros/arc/set_bounds.go
@@ -27,37 +27,28 @@ func init() {
 	})
 }
 
+const (
+	setBoundsApk = "ArcSetBoundsTest.apk"
+	setBoundsPkg = "org.chromium.arc.testapp.setbounds"
+
+	regularButtonID       = setBoundsPkg + ":id/regular_button"
+	smallerButtonID       = setBoundsPkg + ":id/smaller_button"
+	appControlledButtonID = setBoundsPkg + ":id/controlled_toggle_button"
+
+	// TODO(hirokisato) find a reliable way to share constants
+	setBoundsAppInitHeight = 600
+	setBoundsAppInitWidth  = 700
+)
+
+// The bounds below are specified in
+// pi-arc/vendor/google_arc/packages/development/ArcSetBoundsTest/src/org/chromium/arc/testapp/setbounds/BaseActivity.java
+var setBoundsAppRegularBounds = arc.Rect{Left: 100, Top: 100, Width: 800, Height: 800}
+
+// When the activity requests smaller bounds than its min-size, ARC framework expands the bounds to the its min-size.
+// The min-size is specified in AndroidManifest.xml.
+var setBoundsAppSmallBounds = arc.Rect{Left: 200, Top: 200, Width: 600, Height: 500}
+
 func SetBounds(ctx context.Context, s *testing.State) {
-	const (
-		apk = "ArcSetBoundsTest.apk"
-		pkg = "org.chromium.arc.testapp.setbounds"
-
-		resizableActivity   = ".ResizableActivity"
-		unresizableActivity = ".UnresizableActivity"
-
-		regularButtonID       = pkg + ":id/regular_button"
-		smallerButtonID       = pkg + ":id/smaller_button"
-		appControlledButtonID = pkg + ":id/controlled_toggle_button"
-		unresizableButtonID   = pkg + ":id/go_unresizable_button"
-		resizableButtonID     = pkg + ":id/go_resizable_button"
-
-		// TODO(hirokisato) find a reliable way to share constants
-		initialHeight = 600
-		initialWidth  = 700
-	)
-
-	// The bounds below are specified in
-	// pi-arc/vendor/google_arc/packages/development/ArcSetBoundsTest/src/org/chromium/arc/testapp/setbounds/BaseActivity.java
-	regularBounds := arc.Rect{
-		Left: 100, Top: 100, Width: 800, Height: 800,
-	}
-
-	// When the activity requests smaller bounds than its min-size, ARC framework expands the bounds to the its min-size.
-	// The min-size is specified in AndrodiManifest.xml.
-	smallerBounds := arc.Rect{
-		Left: 200, Top: 200, Width: 600, Height: 500,
-	}
-
 	// TODO(crbug.com/1002958) Replace with Ash API to enable clamshell mode once it gets fixed.
 	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs("--force-tablet-mode=clamshell"))
 	if err != nil {
@@ -71,18 +62,8 @@ func SetBounds(ctx context.Context, s *testing.State) {
 	}
 	defer a.Close()
 
-	if err := a.Install(ctx, s.DataPath(apk)); err != nil {
+	if err := a.Install(ctx, s.DataPath(setBoundsApk)); err != nil {
 		s.Fatal("Failed installing app: ", err)
-	}
-
-	act, err := arc.NewActivity(a, pkg, resizableActivity)
-	if err != nil {
-		s.Fatal("Failed to create new activity: ", err)
-	}
-	defer act.Close()
-
-	if err := act.Start(ctx); err != nil {
-		s.Fatal("Failed start Settings activity: ", err)
 	}
 
 	d, err := ui.NewDevice(ctx, a)
@@ -91,74 +72,89 @@ func SetBounds(ctx context.Context, s *testing.State) {
 	}
 	defer d.Close()
 
+	for _, test := range []struct {
+		act       string
+		resizable bool
+	}{
+		{".ResizableActivity", true},
+		{".UnresizableActivity", false},
+	} {
+		if err := setBoundsSubTest(ctx, a, d, test.act, test.resizable); err != nil {
+			s.Errorf("Subtest(%s) failed: %v", test.act, err)
+		}
+	}
+}
+
+func setBoundsSubTest(ctx context.Context, a *arc.ARC, d *ui.Device, actName string, resizable bool) error {
+	testing.ContextLogf(ctx, "Starting %s", actName)
+
+	act, err := arc.NewActivity(a, setBoundsPkg, actName)
+	if err != nil {
+		return errors.Wrapf(err, "failed to create %s", actName)
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx); err != nil {
+		return err
+	}
+	// Stop activity at exit time so that the next WM test can launch a different activity from the same package.
+	defer act.Stop(ctx)
+
 	if err := act.WaitForResumed(ctx, time.Second); err != nil {
-		s.Fatal("Failed to wait for activity to resume: ", err)
+		return errors.Wrap(err, "failed to wait for activity to resume")
 	}
 
 	// Validate initial window size.
 	activityBounds, err := act.SurfaceBounds(ctx)
 	if err != nil {
-		s.Fatal("Failed to get window bounds: ", err)
+		return errors.Wrap(err, "failed to get window bounds")
 	}
 
-	if activityBounds.Height != initialHeight || activityBounds.Width != initialWidth {
-		s.Fatalf("Unexpected window size: got (%d, %d); want (%d, %d)", activityBounds.Width, activityBounds.Height, initialWidth, initialHeight)
+	if activityBounds.Height != setBoundsAppInitHeight || activityBounds.Width != setBoundsAppInitWidth {
+		return errors.Errorf("unexpected initial window size: got (%d, %d); want (%d, %d)", activityBounds.Width, activityBounds.Height, setBoundsAppInitHeight, setBoundsAppInitWidth)
 	}
 
-	clickButtonAndValidateBounds := func(buttonId string, expected arc.Rect) {
-		// Touch button.
+	clickButtonAndValidateBounds := func(buttonId string, expected arc.Rect) error {
+		// Touch the button.
 		if err := d.Object(ui.ID(buttonId)).Click(ctx); err != nil {
-			s.Fatalf("Could not click the button with id %q", buttonId)
+			return errors.Wrapf(err, "could not click the button with id %q", buttonId)
 		}
 
 		// Wait until the bounds to be the expected one.
-		err := testing.Poll(ctx, func(ctx context.Context) error {
+		return testing.Poll(ctx, func(ctx context.Context) error {
 			bounds, err := act.SurfaceBounds(ctx)
 			if err != nil {
-				s.Fatal("Failed to get window bounds: ", err)
+				return err
 			}
 			if bounds != expected {
-				return errors.Errorf("window bounds has not changed yet: got %s; want %s", bounds.String(), expected.String())
+				return errors.Errorf("window bounds has not changed yet: got %v; want %v", &bounds, &expected)
 			}
 			return nil
 		}, &testing.PollOptions{Timeout: 4 * time.Second})
-
-		if err != nil {
-			s.Fatal("Error while waiting for bounds update: ", err)
-		}
 	}
 
-	for _, test := range []struct {
-		// resizable represents current window resizability.
-		resizable bool
-		// appControlled represents current appControlled flag of the task.
-		appControlled bool
-		// buttonIDs represents the id of buttons to be clicked in order to go to the next test state.
-		buttonIDs []string
-	}{
-		{resizable: true, appControlled: false, buttonIDs: []string{appControlledButtonID}},
-		{resizable: true, appControlled: true, buttonIDs: []string{appControlledButtonID, unresizableButtonID}},
-		{resizable: false, appControlled: false, buttonIDs: []string{appControlledButtonID}},
-		{resizable: false, appControlled: true, buttonIDs: nil},
-	} {
-		s.Logf("Testing resizable=%t, appControlled=%t", test.resizable, test.appControlled)
+	for _, appControlled := range []bool{false, true} {
+		testing.ContextLogf(ctx, "Testing resizable=%t, appControlled=%t", resizable, appControlled)
 
-		clickButtonAndValidateBounds(regularButtonID, regularBounds)
-		clickButtonAndValidateBounds(smallerButtonID, smallerBounds)
+		// Validate that behaviour of chaning bounds doesn't depends on window resizability or app-controlled state.
+		clickButtonAndValidateBounds(regularButtonID, setBoundsAppRegularBounds)
+		clickButtonAndValidateBounds(smallerButtonID, setBoundsAppSmallBounds)
 
-		// Even if app specified its bounds, the resizablity depends on its configuration.
+		// Validate that changing bounds from Companion lib doesn't change window resizability.
+		// TODO(hirokisato): Take Chrome-side value, instead of Android-side value.
 		actual, err := act.Resizable(ctx)
 		if err != nil {
-			s.Fatal("Failed to get isResizable state: ", err)
+			return errors.Wrap(err, "failed to get isResizable state")
 		}
-		if actual != test.resizable {
-			s.Fatalf("window resizability is not expected: got %t; want %t", actual, test.resizable)
+		if actual != resizable {
+			return errors.Errorf("window resizability is not expected: got %t; want %t", actual, resizable)
 		}
 
-		for _, id := range test.buttonIDs {
-			if err := d.Object(ui.ID(id)).Click(ctx); err != nil {
-				s.Fatalf("Could not click the button %q: %v", id, err)
-			}
+		// Toggle App-Controlled state.
+		// TODO(hirokisato): Wait for app controlled state to be updated. This should be done together with b/148116159.
+		if err := d.Object(ui.ID(appControlledButtonID)).Click(ctx); err != nil {
+			return errors.Wrap(err, "could not click the button")
 		}
 	}
+	return nil
 }
