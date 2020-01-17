e0486133e15c134c5e3ccff4364cd30cc54bc196
John L Chen <zuan@chromium.org>
2020-01-18 02:44:52 +0800

    tast-tests: hwsec: Add testing of software-backed keys
    BUG=chromium:855675
    TEST=tast run $dut hwsec.Chaps*
    
    Change-Id: Id71504ca2456d4af515e749ac835f72c507631e0

--- src/chromiumos/tast/local/bundles/cros/hwsec/chaps_pkcs1v15.go
+++ src/chromiumos/tast/local/bundles/cros/hwsec/chaps_pkcs1v15.go
@@ -53,12 +53,23 @@ func ChapsPKCS1V15(ctx context.Context, s *testing.State) {
 	// Note: Also, this test expects a clean keystore, in the sense that there should be no object with the same ID as those used by this test.
 
 	// Create the software-generated, then imported key.
-	importedKey, err := pkcs11Util.CreateRsaSoftwareKey(ctx, utility, "", "testkey1", "aaaaaa")
+	importedKey, err := pkcs11Util.CreateRsaSoftwareKey(ctx, utility, "", "testkey1", "aaaaaa", false, true)
 	if err != nil {
 		s.Fatal("Failed to create software key: ", err)
 	}
 	defer func() {
 		if err := pkcs11Util.DestroyKey(ctx, importedKey); err != nil {
+			s.Error("Failed to clean up software key: ", err)
+		}
+	}()
+
+	// Create the software-generated, then imported as software-backed key.
+	softwareKey, err := pkcs11Util.CreateRsaSoftwareKey(ctx, utility, "", "testkey2", "bbbbbb", true, true)
+	if err != nil {
+		s.Fatal("Failed to create software key: ", err)
+	}
+	defer func() {
+		if err := pkcs11Util.DestroyKey(ctx, softwareKey); err != nil {
 			s.Error("Failed to clean up software key: ", err)
 		}
 	}()
@@ -74,7 +85,7 @@ func ChapsPKCS1V15(ctx context.Context, s *testing.State) {
 		}
 	}()
 
-	keys := []*pkcs11.KeyInfo{importedKey, generatedKey}
+	keys := []*pkcs11.KeyInfo{importedKey, softwareKey, generatedKey}
 
 	// Create a copy of software key for every key.
 	var copiedKeys []*pkcs11.KeyInfo
