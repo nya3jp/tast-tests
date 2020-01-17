81722396103b91b0c062f960ded63a7179554b65
Brian Norris <briannorris@chromium.org>
2020-01-17 12:55:14 -0800

    network.TwoShills: guard against still-restarting Shill
    If Shill restarted just before this test, our 'shill' command might win
    the race against the system shill. Make sure the system Shill is fully
    ready before trying to start a second instance.
    
    Inline the timeout too, because it makes it clearer exactly where the
    timeout applies.
    
    NB: this race condition is very hard to hit in Tast, apparently. We used
    to hit it at some point (see $BUG) though. It's also easy for me to
    reproduce this race condition in concept, using basic shell commands:
    
      ## The --foreground instance should die quickly, but it does not.
      $ stop shill; start shill & shill --foreground
    
    BUG=chromium:1011881
    TEST=network.TwoShills, immediately after a Shill restart (e.g., after
         network.ConfigureServiceForProfile)
    
    Change-Id: Ie17ffca1995b1ef330cf84c0159275bcf167eb49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2013081
    Tested-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Alex Khouderchah <akhouderchah@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/network/two_shills.go
+++ src/chromiumos/tast/local/bundles/cros/network/two_shills.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"time"
 
+	"chromiumos/tast/local/shill"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
@@ -17,19 +18,27 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:     TwoShills,
 		Desc:     "Verifies that an attempt to spawn a second instance of shill while an instance is already running will fail",
-		Contacts: []string{"deanliao@google.com", "chromeos-kernel-wifi@google.com"},
+		Contacts: []string{"deanliao@google.com", "cros-networking@google.com"},
 		Attr:     []string{"group:mainline", "informational"},
 	})
 }
 
 func TwoShills(ctx context.Context, s *testing.State) {
-	const timeout time.Duration = 10
 	if _, _, pid, err := upstart.JobStatus(ctx, "shill"); err != nil {
 		s.Fatal("Failed to find shill job: ", err)
 	} else if pid == 0 {
 		s.Fatal("Shill is not running")
 	}
-	ctx, cancel := context.WithTimeout(ctx, timeout*time.Second)
+
+	// Make sure Shill is not only running, but that it's listening on
+	// D-Bus. Note that Shill could be restarting from a previous test, so
+	// we could have a race condition (e.g., where we acquire the Shill
+	// D-Bus namespace before the system Shill does).
+	if _, err := shill.NewManager(ctx); err != nil {
+		s.Fatal("Failed creating Manager proxy: ", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 	if err := testexec.CommandContext(ctx, "shill", "--foreground").Run(testexec.DumpLogOnError); err != nil {
 		s.Log("Shill errored with reason: ", err)
