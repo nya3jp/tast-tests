26f1e5a62aead8c8e3411d17ccd0ecfab2c09bf8
John L Chen <zuan@chromium.org>
2020-01-21 05:25:55 +0800

    tast-tests: hwsec: Add some constants for testing vault and keys related functionality
    BUG=b:147470934
    TEST=Tested with hwsec.CryptohomeKeysAndMountSanity
    
    Change-Id: Id2a0d83516571f2656281829519339149138f01e

---
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/util/constants.go
@@ -0,0 +1,24 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package util
+
+// This file contains some shared constants for vault/key related testing.
+const (
+	FirstUsername        = "PierreDeFermat@example.com"
+	FirstPassword        = "F131dTooSm@ll2C0nt@1nMyP@ssw0rd!!"
+	FirstChangedPassword = "a^n+b^n=c^n" // Got a great proof, but margin too small.
+	FirstPin             = "65537"       // 5th Fermat Number
+
+	SecondUsername = "LeonhardEuler@example.com"
+	SecondPassword = "e^(i*phi)=cos(phi)+i*sin(phi)"
+	SecondPin      = "271828" // e
+	ThirdUsername  = "Pythagoras@example.com"
+	ThirdPassword  = "a^2+b^2=c^2"
+
+	PasswordLabel        = "password"
+	ChangedPasswordLabel = "changed"
+	PinLabel             = "pin"
+	IncorrectPassword    = "ImJustGuessing~"
+)
