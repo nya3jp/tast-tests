f7360ccee0d9d1834b2b511a490afc21f2797114
Hsin-chen Chuang <chharry@google.com>
2020-01-21 09:48:27 +0800

    WIP: network: add wifi SimpleConnect WPA and hidden network testcases.
    DONE:
    - Add Hidden Option. It setups AP to hide SSID and sets DUT's property
      to connect to hidden network.
    - Add package security. It defines interfaces and utilities of security
      types.
      - Add package base: for open network.
      - Add package wep:  for WEP protected network.
      - Add package wpa:  for WPA protected network.
    
    BUG=chromium:1034875
    TEST=tast run ${DUT} wifi.SimpleConnect.\*
    
    Change-Id: Iedd34ac917538416863d16850b63110f97be243f

---
+++ src/chromiumos/tast/remote/wificell/security/wep/config.go
@@ -0,0 +1,182 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wep
+
+import (
+	"fmt"
+	"strconv"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/remote/wificell/security"
+	"chromiumos/tast/remote/wificell/security/base"
+)
+
+// AuthAlgsEnum is the type for specifying WEP authentication algorithms.
+type AuthAlgsEnum int
+
+// WEP authentication algorithms modes.
+const (
+	AuthAlgsOpen   = 1
+	AuthAlgsShared = 2
+)
+
+// Option is the function signature used to specify options of Config.
+type Option func(*Factory)
+
+// Keys returns an Option which sets keys in Config.
+func Keys(strs []string) Option {
+	return func(c *Factory) {
+		c.Keys = make([]string, len(strs))
+		copy(c.Keys, strs)
+	}
+}
+
+// DefaultKey returns an Option which sets default key in Config.
+func DefaultKey(d int) Option {
+	return func(c *Factory) {
+		c.DefaultKey = d
+	}
+}
+
+// AuthAlgs returns an Option which sets what authentication algorithm to use in Config.
+func AuthAlgs(a AuthAlgsEnum) Option {
+	return func(c *Factory) {
+		c.AuthAlgs = a
+	}
+}
+
+// Config is the security config used to start hostapd and set properties of DUT.
+type Config struct {
+	// Embed base.Config so we don't have to re-implement noop methods.
+	base.Config
+	Keys       []string
+	DefaultKey int
+	AuthAlgs   AuthAlgsEnum
+}
+
+var _ security.Config = (*Config)(nil)
+
+// Factory holds some Option and provide Gen method to build a new Config.
+type Factory struct {
+	Keys       []string
+	DefaultKey int
+	AuthAlgs   AuthAlgsEnum
+}
+
+// Gen builds a Config with the given Option stored in Factory.
+func (f *Factory) Gen() (security.Config, error) {
+	conf := &Config{
+		Keys:       make([]string, len(f.Keys)),
+		DefaultKey: f.DefaultKey,
+		AuthAlgs:   f.AuthAlgs,
+	}
+	copy(conf.Keys, f.Keys)
+	if err := conf.validate(); err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
+
+// NewFactory builds a Factory with the given Option.
+func NewFactory(ops ...Option) security.Factory {
+	// Default config.
+	fac := &Factory{AuthAlgs: AuthAlgsOpen}
+	for _, op := range ops {
+		op(fac)
+	}
+	return fac
+}
+
+var _ security.Factory = (*Factory)(nil)
+
+// Class returns security class of WEP network.
+func (c *Config) Class() string {
+	return "wep"
+}
+
+// HostapdConfig returns hostapd config of WEP network.
+func (c *Config) HostapdConfig() (map[string]string, error) {
+	ret := make(map[string]string)
+	quote := func(s string) string { return fmt.Sprintf("%q", s) }
+	if err := c.validateKeys(); err != nil {
+		return nil, err
+	}
+	for i, key := range c.Keys {
+		formatted, err := formatKey(key, quote)
+		if err != nil {
+			return nil, err
+		}
+		ret[fmt.Sprintf("wep_key%d", i)] = formatted
+	}
+	ret["wep_default_key"] = strconv.Itoa(c.DefaultKey)
+	ret["auth_algs"] = strconv.Itoa(int(c.AuthAlgs))
+	return ret, nil
+}
+
+// ShillServiceProperties returns shill properties of WEP network.
+func (c *Config) ShillServiceProperties() map[string]interface{} {
+	keyWithIndex := fmt.Sprintf("%d:%s", c.DefaultKey, c.Keys[c.DefaultKey])
+	return map[string]interface{}{shill.ServicePropertyPassphrase: keyWithIndex}
+}
+
+// formatKey is a helper function for generating hostapd and wpa_cli config.
+func formatKey(key string, formatter func(string) string) (string, error) {
+	switch len(key) {
+	case 5, 13, 16:
+		return formatter(key), nil
+	case 10, 26, 32:
+		return key, nil
+	default:
+		return "", errors.Errorf("invalid key length: %q", key)
+	}
+}
+
+// validate validates the Config.
+func (c *Config) validate() error {
+	if c.AuthAlgs & ^(AuthAlgsOpen|AuthAlgsShared) > 0 {
+		return errors.New("invalid WEP auth algorithm is set")
+	}
+	if c.AuthAlgs&(AuthAlgsOpen|AuthAlgsShared) == 0 {
+		return errors.New("no WEP auth algorithm is set")
+	}
+	if len(c.Keys) > 4 {
+		return errors.Errorf("at most 4 keys can be set, got %d keys", len(c.Keys))
+	}
+	if c.DefaultKey >= len(c.Keys) || c.DefaultKey < 0 {
+		return errors.Errorf("default key index %d out of range %d", c.DefaultKey, len(c.Keys))
+	}
+	if err := c.validateKeys(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// validateKeys validates the keys.
+func (c *Config) validateKeys() error {
+	isValidHexChar := func(ch rune) bool {
+		for _, v := range "0123456789abcdefABCDEF" {
+			if ch == v {
+				return true
+			}
+		}
+		return false
+	}
+	for _, key := range c.Keys {
+		switch len(key) {
+		case 5, 13, 16:
+			// No need to check.
+		case 10, 26, 32:
+			for _, ch := range key {
+				if !isValidHexChar(ch) {
+					return errors.Errorf("key with length 10, 26, or 32 should only contain hexadecimal digits: %q", key)
+				}
+			}
+		default:
+			return errors.Errorf("invalid key length: %q", key)
+		}
+	}
+	return nil
+}
---
+++ src/chromiumos/tast/remote/wificell/security/wpa/config_test.go
@@ -0,0 +1,282 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wpa
+
+import (
+	"reflect"
+	"testing"
+
+	"chromiumos/tast/remote/wificell/security"
+)
+
+type testGenStruct struct {
+	factory    security.Factory
+	expected   security.Config
+	shouldFail bool
+}
+
+func runTestGen(t *testing.T, testcases []testGenStruct) {
+	for i, tc := range testcases {
+		conf, err := tc.factory.Gen()
+		if tc.shouldFail {
+			if err == nil {
+				t.Errorf("testcase %d should not pass config validation", i)
+			}
+			continue
+		}
+		if err != nil {
+			t.Errorf("testcase %d Gen failed with err=%s", i, err.Error())
+		}
+		if !reflect.DeepEqual(conf, tc.expected) {
+			t.Errorf("testcase %d got %v but expect %v", i, conf, tc.expected)
+		}
+	}
+}
+
+type testGetStruct struct {
+	conf          security.Config
+	verifyHostapd map[string]string      // hostapd config fields to verify
+	verifyShill   map[string]interface{} // shill config fields to verify
+}
+
+func runTestGet(t *testing.T, testcases []testGetStruct) {
+	for i, tc := range testcases {
+		// Verify the requested hostapd fields.
+		h, err := tc.conf.HostapdConfig()
+		if err != nil {
+			t.Errorf("testcase %d failed with err=%s", i, err.Error())
+			continue
+		}
+		for k, v := range tc.verifyHostapd {
+			if v != h[k] {
+				t.Errorf("testcase %d has %q=%q, expect %q", i, k, h[k], v)
+			}
+		}
+
+		// Verify the requested shill fields.
+		s := tc.conf.ShillServiceProperties()
+		for k, v := range tc.verifyShill {
+			if !reflect.DeepEqual(v, s[k]) {
+				t.Errorf("testcase %d has %q=%q, expect %q", i, k, s[k], v)
+			}
+		}
+	}
+}
+
+func TestGen(t *testing.T) {
+	// Test Factory.Gen and Config.validate.
+	runTestGen(t, []testGenStruct{
+		{
+			factory: NewFactory(
+				Psk("chromeos"),
+				Mode(ModePure),
+			),
+			expected:   nil,
+			shouldFail: true, // missing cipher
+		}, {
+			factory: NewFactory(
+				Psk("chromeos"),
+				Mode(ModePure2),
+			),
+			expected:   nil,
+			shouldFail: true, // missing both cipher and cipher2
+		}, {
+			factory: NewFactory(
+				Psk("chromeos"),
+				Mode(ModePure),
+				Ciphers(CipherTKIP, CipherCCMP),
+				Ciphers2(CipherCCMP),
+			),
+			expected:   nil,
+			shouldFail: true, // using pure WPA but cipher2 is set
+		}, {
+			factory: NewFactory(
+				Mode(ModePure),
+				Ciphers(CipherTKIP),
+			),
+			expected:   nil,
+			shouldFail: true, // missing PSK
+		}, {
+			factory: NewFactory(
+				Psk("01234"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"),
+				Mode(ModePure),
+				Ciphers(CipherTKIP),
+			),
+			expected:   nil,
+			shouldFail: true, // passphrase with length over 64
+		}, {
+			factory: NewFactory(
+				Psk("zzzz"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"),
+				Mode(ModePure),
+				Ciphers(CipherTKIP),
+			),
+			expected:   nil,
+			shouldFail: true, // passphrase with length 64 but contains non-hex digits
+		}, { // Good cases.
+			factory: NewFactory(
+				Psk("chromeos"),
+				Mode(ModeMixed),
+				Ciphers(CipherTKIP, CipherCCMP),
+				Ciphers2(CipherCCMP),
+				FTMode(FTModeNone),
+				GmkRekeyPeriod(86400),
+				GtkRekeyPeriod(86400),
+				PtkRekeyPeriod(600),
+				UseStrictRekey(true),
+			),
+			expected: &Config{
+				Psk:            "chromeos",
+				Mode:           ModeMixed,
+				Ciphers:        []Cipher{CipherTKIP, CipherCCMP},
+				Ciphers2:       []Cipher{CipherCCMP},
+				FTMode:         FTModeNone,
+				GmkRekeyPeriod: 86400,
+				GtkRekeyPeriod: 86400,
+				PtkRekeyPeriod: 600,
+				UseStrictRekey: true,
+			},
+			shouldFail: false,
+		}, {
+			// 64 byte hex digits is ok.
+			factory: NewFactory(
+				Psk("0123"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"+
+					"0123456789"+
+					"abcdefaaaa"+
+					"ABCDEFAAAA"),
+				Mode(ModePure),
+				Ciphers(CipherTKIP),
+			),
+			expected: &Config{
+				Psk: "0123" +
+					"0123456789" +
+					"0123456789" +
+					"0123456789" +
+					"0123456789" +
+					"abcdefaaaa" +
+					"ABCDEFAAAA",
+				Mode:    ModePure,
+				Ciphers: []Cipher{CipherTKIP},
+				FTMode:  FTModeNone,
+			},
+			shouldFail: false,
+		}, {
+			// Have cipher but don't have cipher2 is ok to WPA2.
+			factory: NewFactory(
+				Psk("chromeos"),
+				Mode(ModePure2),
+				Ciphers(CipherTKIP),
+			),
+			expected: &Config{
+				Psk:     "chromeos",
+				Mode:    ModePure2,
+				Ciphers: []Cipher{CipherTKIP},
+				FTMode:  FTModeNone,
+			},
+			shouldFail: false,
+		},
+	})
+}
+
+func TestGet(t *testing.T) {
+	// Test Config.HostapdConfig and Config.ShillServiceProperties.
+	runTestGet(t, []testGetStruct{
+		{
+			// All set.
+			conf: &Config{
+				Psk:            "chromeos",
+				Mode:           ModeMixed,
+				Ciphers:        []Cipher{CipherTKIP, CipherCCMP},
+				Ciphers2:       []Cipher{CipherCCMP},
+				FTMode:         FTModeNone,
+				GmkRekeyPeriod: 86400,
+				GtkRekeyPeriod: 86400,
+				PtkRekeyPeriod: 600,
+				UseStrictRekey: true,
+			},
+			verifyHostapd: map[string]string{
+				"wpa_passphrase":   "chromeos",
+				"wpa":              "3",
+				"wpa_pairwise":     "TKIP CCMP",
+				"rsn_pairwise":     "CCMP",
+				"wpa_key_mgmt":     "WPA-PSK",
+				"wpa_gmk_rekey":    "86400",
+				"wpa_group_rekey":  "86400",
+				"wpa_ptk_rekey":    "600",
+				"wpa_strict_rekey": "1",
+			},
+			verifyShill: map[string]interface{}{
+				"Passphrase": "chromeos",
+			},
+		}, {
+			// Use 64 byte PSK.
+			conf: &Config{
+				Psk: "0123" +
+					"0123456789" +
+					"0123456789" +
+					"0123456789" +
+					"0123456789" +
+					"abcdefaaaa" +
+					"ABCDEFAAAA",
+				Mode:    ModePure,
+				Ciphers: []Cipher{CipherTKIP},
+				FTMode:  FTModeNone,
+			},
+			verifyHostapd: map[string]string{
+				"wpa_psk": "0123" +
+					"0123456789" +
+					"0123456789" +
+					"0123456789" +
+					"0123456789" +
+					"abcdefaaaa" +
+					"ABCDEFAAAA",
+				"wpa":          "1",
+				"wpa_pairwise": "TKIP",
+				"wpa_key_mgmt": "WPA-PSK",
+			},
+			verifyShill: map[string]interface{}{
+				"Passphrase": "0123" +
+					"0123456789" +
+					"0123456789" +
+					"0123456789" +
+					"0123456789" +
+					"abcdefaaaa" +
+					"ABCDEFAAAA",
+			},
+		}, {
+			// Mixed FT mode.
+			conf: &Config{
+				Psk:     "chromeos",
+				Mode:    ModePure,
+				Ciphers: []Cipher{CipherTKIP},
+				FTMode:  FTModeMixed,
+			},
+			verifyHostapd: map[string]string{
+				"wpa_passphrase": "chromeos",
+				"wpa":            "1",
+				"wpa_pairwise":   "TKIP",
+				"wpa_key_mgmt":   "WPA-PSK FT-PSK",
+			},
+			verifyShill: map[string]interface{}{
+				"Passphrase":     "chromeos",
+				"WiFi.FTEnabled": true,
+			},
+		},
+	})
+}
