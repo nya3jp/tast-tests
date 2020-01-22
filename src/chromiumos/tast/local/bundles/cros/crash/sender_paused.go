1a57ae315affb8d496b1b50b053eaeb224d89ea6
Shuhei Takahashi <nya@chromium.org>
2020-01-22 11:40:26 +0900

    crash: Add SenderPaused test.
    This is a part of the patches to port logging_CrashSender to Tast.
    
    crash.SenderPaused checks that crash_sender is paused during crash
    tests.
    
    crash.SenderPaused corresponds to "sender_pausing" subtest in
    logging_CrashSender.
    
    BUG=chromium:978255
    TEST=fast_build.sh -T
    TEST=tast run DUT crash.SenderPaused
    
    Change-Id: If4d4e8ed14cfb9cd08214adcdee864be426f541d

---
+++ src/chromiumos/tast/local/bundles/cros/crash/sender_paused.go
@@ -0,0 +1,69 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crash
+
+import (
+	"context"
+	"os"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"chromiumos/tast/local/bundles/cros/crash/sender"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SenderPaused,
+		Desc: "Check that crash_sender is paused during crash tests",
+		Contacts: []string{
+			"mutexlox@chromium.org",
+			"iby@chromium.org",
+			"cros-monitoring-forensics@google.com",
+			"nya@chromium.org", // ported to Tast
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "metrics_consent"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func SenderPaused(ctx context.Context, s *testing.State) {
+	crashDir, err := sender.SetUp(ctx, s.PreValue().(*chrome.Chrome))
+	if err != nil {
+		s.Fatal("Setup failed: ", err)
+	}
+	defer sender.TearDown()
+	defer os.RemoveAll(crashDir)
+
+	const basename = "some_program.1.2.3"
+	exp, err := sender.AddFakeMinidumpCrash(ctx, crashDir, basename)
+	if err != nil {
+		s.Fatal("Failed to add a fake minidump crash: ", err)
+	}
+
+	// Run crash_sender without ignoring the pause file. This should fail because
+	// sender.SetUp creates the pause file.
+	if _, err := sender.RunNoIgnorePauseFile(ctx, crashDir); err == nil {
+		s.Fatal("crash_sender succeeded unexpectedly")
+	}
+	s.Log("crash_sender failed as expected")
+
+	// Run crash_sender again, ignoring the pause file this time. This should pass.
+	got, err := sender.Run(ctx, crashDir)
+	if err != nil {
+		s.Fatal("Failed to run crash_sender: ", err)
+	}
+	want := []*sender.SendResult{{
+		Success: true,
+		Data:    *exp,
+	}}
+	if diff := cmp.Diff(got, want, cmpopts.IgnoreFields(sender.SendResult{}, "Schedule")); diff != "" {
+		s.Log("Results mismatch (-got +want): ", diff)
+		s.Errorf("crash_sender sent unexpected %d results; see logs for diff", len(got))
+	}
+}
