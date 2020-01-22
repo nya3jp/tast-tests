aea913973ae4efa8e1701ba287d9832bab1a55aa
Miriam Zimmerman <mutexlox@google.com>
2020-01-21 17:14:52 -0800

    crash: Add WatchdogCrash test
    BUG=chromium:1039381
    TEST=tast run crash.WatchdogCrash
    
    Change-Id: I6f0e77d8c0961c88fdc71df5b60f9cdda40ef4ef

---
+++ src/chromiumos/tast/remote/bundles/cros/crash/watchdog_crash.go
@@ -0,0 +1,148 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crash
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/rpc"
+	crash_service "chromiumos/tast/services/cros/crash"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WatchdogCrash,
+		Desc:         "Verify artificial watchdog crash creates crash files",
+		Contacts:     []string{"mutexlox@chromium.org", "cros-monitoring-forensics@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "metrics_consent", "pstore", "reboot"},
+		ServiceDeps:  []string{"tast.cros.crash.FixtureService"},
+	})
+}
+
+func WatchdogCrash(ctx context.Context, s *testing.State) {
+	const systemCrashDir = "/var/spool/crash"
+
+	d := s.DUT()
+
+	// TODO(crbug.com/950346): Remove the below check and add dependency on
+	// ARM chip when hardware dependencies are implemented.
+	if err := d.Command("/bin/grep", "-q", "model.*ARM", "/proc/cpuinfo").Run(ctx); err != nil {
+		s.Fatal("Can only run Watchdog test on ARM devices")
+	}
+
+	cl, err := rpc.Dial(ctx, d, s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+
+	fs := crash_service.NewFixtureServiceClient(cl.Conn)
+
+	if _, err := fs.SetUp(ctx, &empty.Empty{}); err != nil {
+		cl.Close(ctx)
+		s.Fatal("Failed to set up: ", err)
+	}
+
+	// Shorten deadline to leave time for cleanup
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	// This is a bit delicate. If the test fails _before_ we panic the machine,
+	// we need to do TearDown then, and on the same connection (so we can close Chrome).
+	//
+	// If it fails to reconnect, we do not need to clean these up.
+	//
+	// Otherwise, we need to re-establish a connection to the machine and
+	// run TearDown.
+	defer func() {
+		s.Log("Cleaning up")
+		if fs != nil {
+			if _, err := fs.TearDown(cleanupCtx, &empty.Empty{}); err != nil {
+				s.Error("Couldn't tear down: ", err)
+			}
+		}
+		if cl != nil {
+			cl.Close(cleanupCtx)
+		}
+	}()
+
+	// Sync filesystem to minimize impact of the crash on other tests
+	if out, err := d.Command("sync").CombinedOutput(ctx); err != nil {
+		s.Fatalf("Failed to sync filesystems: %s. err: %v", out, err)
+	}
+
+	// Trigger a watchdog reset
+	// Run the triggering command in the background to avoid the DUT potentially going down before
+	// success is reported over the SSH connection. Redirect all I/O streams to ensure that the
+	// SSH exec request doesn't hang (see https://en.wikipedia.org/wiki/Nohup#Overcoming_hanging).
+
+	// Daisydog is the watchdog service
+	cmd := `nohup sh -c 'sleep 2
+	stop daisydog
+	sleep 60 > /dev/watchdog' >/dev/null 2>&1 </dev/null &`
+	if err := d.Command("bash", "-c", cmd).Run(ctx); err != nil {
+		s.Fatal("Failed to panic DUT: ", err)
+	}
+
+	s.Log("Waiting for DUT to become unreachable")
+
+	if err := d.WaitUnreachable(ctx); err != nil {
+		s.Fatal("Failed to wait for DUT to become unreachable: ", err)
+	}
+	s.Log("DUT became unreachable (as expected)")
+
+	// When we lost the connection, these connections broke.
+	cl.Close(ctx)
+	cl = nil
+	fs = nil
+
+	s.Log("Reconnecting to DUT")
+	if err := d.WaitConnect(ctx); err != nil {
+		s.Fatal("Failed to reconnect to DUT: ", err)
+	}
+	s.Log("Reconnected to DUT")
+
+	cl, err = rpc.Dial(ctx, d, s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	fs = crash_service.NewFixtureServiceClient(cl.Conn)
+
+	base := `kernel\.\d{8}\.\d{6}\.0`
+	waitReq := &crash_service.WaitForCrashFilesRequest{
+		Dirs:    []string{systemCrashDir},
+		Regexes: []string{base + `\.kcrash`, base + `\.meta`},
+	}
+	s.Log("Waiting for files to become present")
+	res, err := fs.WaitForCrashFiles(ctx, waitReq)
+	if err != nil {
+		s.Error("Failed to find crash files: ", err.Error())
+	}
+	for _, m := range res.Matches {
+		if strings.HasSuffix(m.Regex, ".meta") {
+			if len(m.Files) != 1 {
+				s.Errorf("Unexpected number of kernel crashes: %d, want 1", len(m.Files))
+				continue
+			}
+			if err := d.Command("/bin/grep", "-q", "sig=kernel-(WATCHDOG)", m.Files[0]).Run(ctx); err != nil {
+				s.Error("Did not find correct pattern in meta file: ", err)
+			}
+		}
+	}
+
+	removeReq := &crash_service.RemoveAllFilesRequest{
+		Matches: res.Matches,
+	}
+	if _, err := fs.RemoveAllFiles(ctx, removeReq); err != nil {
+		s.Error("Error removing files: ", err)
+	}
+}
