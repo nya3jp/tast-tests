b06487bdddff1ed9207ae6d378f92629b463f514
Victor-Gabriel Savu <vsavu@google.com>
2020-01-22 15:54:13 +0100

    tast: add precondition for user policies
    Add precondition for user policies to share chrome and FakeDMS.
    
    BUG=None
    TEST=tast run <IP> policy.*
    
    Change-Id: I96b0ff1fb740da4c76406f703bf45c4647b64e5a

--- src/chromiumos/tast/local/bundles/cros/policy/url_blacklist.go
+++ src/chromiumos/tast/local/bundles/cros/policy/url_blacklist.go
@@ -8,10 +8,9 @@ import (
 	"context"
 	"strings"
 
-	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/bundles/cros/policy/policyutil"
+	"chromiumos/tast/local/bundles/cros/policy/pre"
 	"chromiumos/tast/local/policy"
-	"chromiumos/tast/local/policy/fakedms"
 	"chromiumos/tast/testing"
 )
 
@@ -25,35 +24,13 @@ func init() {
 		},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
 	})
 }
 
 func URLBlacklist(ctx context.Context, s *testing.State) {
-	// Start FakeDMS.
-	fdms, err := fakedms.New(ctx, s.OutDir())
-	if err != nil {
-		s.Fatal("Failed to start FakeDMS: ", err)
-	}
-	defer fdms.Stop(ctx)
-
-	if err := fdms.WritePolicyBlob(fakedms.NewPolicyBlob()); err != nil {
-		s.Fatal("Failed to write policies to FakeDMS: ", err)
-	}
-
-	// Start a Chrome instance that will fetch policies from the FakeDMS.
-	cr, err := chrome.New(ctx,
-		chrome.Auth("tast-user@managedchrome.com", "test0000", "gaia-id"),
-		chrome.DMSPolicy(fdms.URL))
-	if err != nil {
-		s.Fatal("Chrome login failed: ", err)
-	}
-	defer cr.Close(ctx)
-
-	// Set up Chrome Test API connection.
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to create Test API connection: ", err)
-	}
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
 
 	for _, param := range []struct {
 		// name is the subtest name.
@@ -91,30 +68,17 @@ func URLBlacklist(ctx context.Context, s *testing.State) {
 		},
 	} {
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
-			// Close windows.
-			windows, err := ash.GetAllWindows(ctx, tconn)
-			if err != nil {
-				s.Fatal("Failed to get windows: ", err)
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
 			}
 
-			for _, window := range windows {
-				if err := window.CloseWindow(ctx, tconn); err != nil {
-					s.Fatal("Failed to close window: ", err)
-				}
+			// Update policies.
+			if err := policyutil.ServeAndRefresh(ctx, fdms, cr, []policy.Policy{param.value}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
 			}
 
-			// Create a policy blob and have the FakeDMS serve it.
-			pb := fakedms.NewPolicyBlob()
-			pb.AddPolicies([]policy.Policy{param.value})
-			if err := fdms.WritePolicyBlob(pb); err != nil {
-				s.Fatal("Failed to write policies to FakeDMS: ", err)
-			}
-
-			// Refresh policies.
-			if err := tconn.EvalPromise(ctx, `tast.promisify(chrome.autotestPrivate.refreshEnterprisePolicies)();`, nil); err != nil {
-				s.Fatal("Failed to refresh policies: ", err)
-			}
-
+			// Run actual test.
 			urlBlocked := func(url string) bool {
 				conn, err := cr.NewConn(ctx, url)
 				if err != nil {
