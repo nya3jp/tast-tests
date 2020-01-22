b4c1775efbaab3cf7d90490a0f9f9d6fe7a0f8b1
Victor-Gabriel Savu <vsavu@google.com>
2020-01-22 15:54:13 +0100

    tast: add precondition for user policies
    Add precondition for user policies to share chrome and FakeDMS.
    
    BUG=None
    TEST=tast run <IP> policy.*
    
    Change-Id: I96b0ff1fb740da4c76406f703bf45c4647b64e5a

---
+++ src/chromiumos/tast/local/bundles/cros/policy/util/chrome.go
@@ -0,0 +1,26 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package util
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/policy/fakedms"
+)
+
+// ResetChromeAndPolicies resets chrome and removes all set policies.
+func ResetChromeAndPolicies(ctx context.Context, fdms *fakedms.FakeDMS, cr *chrome.Chrome) error {
+	if err := cr.ResetState(ctx); err != nil {
+		return errors.Wrap(err, "failed to reset chrome")
+	}
+
+	if err := UpdatePolicies(ctx, fdms, cr, fakedms.NewPolicyBlob()); err != nil {
+		return errors.Wrap(err, "failed to clear policies")
+	}
+
+	return nil
+}
