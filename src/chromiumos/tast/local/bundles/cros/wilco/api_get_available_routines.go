8fea4833f09cfa5a16f511fbccdffb0666c73c01
Edward Lin <edward_lin1@dell.com>
2020-01-22 14:07:49 +0800

    tast-tests: Add CPU cache/stress routines for APIRoutine
    Add the CPU cache/stress routines for the APIRoutine tast-tests.
    
    BUG=b:146513388
    TEST=tast run $DUT_IP wilco.APIRoutine
    
    Change-Id: I2fe2148f39ec91e3769432de04bb4c981a81b2b6

--- src/chromiumos/tast/local/bundles/cros/wilco/api_get_available_routines.go
+++ src/chromiumos/tast/local/bundles/cros/wilco/api_get_available_routines.go
@@ -54,6 +54,8 @@ func APIGetAvailableRoutines(ctx context.Context, s *testing.State) {
 		dtcpb.DiagnosticRoutine_ROUTINE_BATTERY_SYSFS,
 		dtcpb.DiagnosticRoutine_ROUTINE_URANDOM,
 		dtcpb.DiagnosticRoutine_ROUTINE_SMARTCTL_CHECK,
+		dtcpb.DiagnosticRoutine_ROUTINE_CPU_CACHE,
+		dtcpb.DiagnosticRoutine_ROUTINE_CPU_STRESS,
 	} {
 		if !contains(response.Routines, val) {
 			s.Fatalf("Routine %s missing", val.String())
