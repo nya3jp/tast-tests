985342290cba0051640ae089c0753f7809936646
Greg Edelston <gredelston@google.com>
2020-01-24 13:01:26 -0700

    Add BlockingSync() RPC
    Firmware tests need to sync the device before switching the boot mode.
    This CL adds a BlockingSync RPC based on Autotest logic:
        http://cs/chromeos_public/src/third_party/autotest/files/...
        server/cros/faft/firmware_test.py?l=1101
    
    BUG=chromium:993062
    TEST=firmware.CheckBootMode
    
    Change-Id: I9a2097853cb3a7bebd901227e88f08b1b6576e37

--- src/chromiumos/tast/local/bundles/cros/firmware/utils_service.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/utils_service.go
@@ -5,11 +5,15 @@
 package firmware
 
 import (
+	"strings"
+
+	"github.com/golang/protobuf/ptypes/empty"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/firmware"
+	"chromiumos/tast/local/testexec"
 	fwpb "chromiumos/tast/services/cros/firmware"
 	"chromiumos/tast/testing"
 )
@@ -47,4 +51,84 @@ func (*UtilsService) CheckBootMode(ctx context.Context, req *fwpb.CheckBootModeR
 		return nil, err
 	}
 	return &fwpb.CheckBootModeResponse{Verified: verified}, nil
+}
+
+// BlockingSync syncs the root device and internal device.
+func (*UtilsService) BlockingSync(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	var err error
+	var cmd *testexec.Cmd
+	var devices []string
+
+	// The double calls to sync fakes a blocking call
+	// since the first call returns before the flush is complete,
+	// but the second will wait for the first to finish.
+	for i := 0; i < 2; i++ {
+		cmd = testexec.CommandContext(ctx, "sync")
+		err = cmd.Run(testexec.DumpLogOnError)
+		if err != nil {
+			return nil, errors.Wrapf(err, "sending sync command #%d to DUT", i+1)
+		}
+	}
+
+	// Find the root device.
+	rootDevice, err := firmware.GetRootDevice(ctx)
+	if err != nil {
+		return nil, err
+	}
+	devices = append(devices, rootDevice)
+
+	// If booted from removable media, sync the internal device too.
+	isRemovable, err := firmware.IsBootDeviceRemovable(ctx)
+	if err != nil {
+		return nil, err
+	} else if isRemovable {
+		internalDevice, err := firmware.GetInternalDevice(ctx)
+		if err != nil {
+			return nil, err
+		}
+		devices = append(devices, internalDevice)
+	}
+
+	// sync only sends SYNCHRONIZE_CACHE but doesn't check the status.
+	// This function will perform a device-specific sync command.
+	for _, device := range devices {
+		if strings.Contains(device, "mmcblk") {
+			// For mmc devices, use `mmc status get` command to send an
+			// empty command to wait for the disk to be available again.
+			cmd = testexec.CommandContext(ctx, "mmc", "status", "get", device)
+			err = cmd.Run(testexec.DumpLogOnError)
+			if err != nil {
+				return nil, errors.Wrapf(err, "sending mmc command to device %s", device)
+			}
+		} else if strings.Contains(device, "nvme") {
+			// For NVMe devices, use `nvme flush` command to commit data
+			// and metadata to non-volatile media.
+			// Get a list of NVMe namespaces, and flush them individually.
+			// The output is assumed to be in the following format:
+			// [ 0]:0x1
+			// [ 1]:0x2
+			cmd = testexec.CommandContext(ctx, "nvme", "list-ns", device)
+			namespaces, err := cmd.Output(testexec.DumpLogOnError)
+			if err != nil {
+				return nil, errors.Wrapf(err, "listing namespaces for device %s", device)
+			} else if len(namespaces) == 0 {
+				return nil, errors.Errorf("Listing namespaces for device %s returned no output", device)
+			}
+			for _, namespace := range namespaces {
+				ns := strings.Split(string(namespace), ":")[1]
+				cmd = testexec.CommandContext(ctx, "nvme", "flush", device, "-n", ns)
+				if err = cmd.Run(testexec.DumpLogOnError); err != nil {
+					return nil, errors.Wrapf(err, "flushing namespace %s on device %s", ns, device)
+				}
+			}
+		} else {
+			// For other devices, hdparm sends TUR to check if
+			// a device is ready for transfer operation.
+			cmd = testexec.CommandContext(ctx, "hdparm", "-f", device)
+			if err = cmd.Run(testexec.DumpLogOnError); err != nil {
+				return nil, errors.Wrapf(err, "sending hdparm command to device %s", device)
+			}
+		}
+	}
+	return &empty.Empty{}, nil
 }
--- src/chromiumos/tast/local/firmware/utils.go
+++ src/chromiumos/tast/local/firmware/utils.go
@@ -6,6 +6,11 @@ package firmware
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"golang.org/x/net/context"
 
@@ -53,4 +58,93 @@ func CheckBootMode(ctx context.Context, mode BootMode) (bool, error) {
 		return false, errors.Errorf("unrecognized boot mode %d", mode)
 	}
 	return CheckCrossystemValues(ctx, crossystemValues), nil
+}
+
+// pathExists determines whether a filepath represents a valid file/dir.
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return os.IsNotExist(err)
+}
+
+// GetRootPart returns the name of the root device with the partition number.
+func GetRootPart(ctx context.Context) (string, error) {
+	cmd := testexec.CommandContext(ctx, "rootdev", "-s")
+	output, err := cmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(output)), nil
+}
+
+// GetRootDevice returns the name of the root device without the partition number.
+func GetRootDevice(ctx context.Context) (string, error) {
+	rootPart, err := GetRootPart(ctx)
+	if err != nil {
+		return "", err
+	}
+	rootDev := stripPart(rootPart)
+	return rootDev, nil
+}
+
+// stripPart takes the name of the root device with the partition number, and removes the partition number.
+func stripPart(rootPart string) string {
+	rePart := regexp.MustCompile("p?[0-9]+$")
+	return rePart.ReplaceAllString(rootPart, "")
+}
+
+// IsBootDeviceRemovable checks whether the current boot device is removable.
+func IsBootDeviceRemovable(ctx context.Context) (bool, error) {
+	rootPart, err := GetRootPart(ctx)
+	if err != nil {
+		return false, err
+	}
+	return IsDeviceRemovable(ctx, rootPart)
+}
+
+// IsDeviceRemovable checks whether a certain storage device is removable.
+func IsDeviceRemovable(ctx context.Context, devicePart string) (bool, error) {
+	device := stripPart(devicePart)
+	deviceBasename := strings.Split(device, "/")[2]
+	fp := fmt.Sprintf("/sys/block/%s/removable", deviceBasename)
+	content, err := ioutil.ReadFile(fp)
+	if err != nil {
+		return false, errors.Wrapf(err, "reading filepath %s", fp)
+	}
+	removable, err := strconv.Atoi(strings.TrimSpace(string(content)))
+	if err != nil {
+		return false, err
+	}
+	return removable == 1, nil
+}
+
+// GetInternalDisk determines the internal disk based on the current disk.
+// If devicePart is a removable device, then the internal disk is determined
+// based on the type of device (arm or x86).
+// Otherwise, return device itself.
+func GetInternalDisk(ctx context.Context, devicePart string) (string, error) {
+	if isRemovable, err := IsDeviceRemovable(ctx, devicePart); err != nil {
+		return "", err
+	} else if isRemovable {
+		for _, p := range []string{"/dev/mmcblk0", "/dev/mmcblk1", "/dev/nvme0n1"} {
+			if pathExists(p) {
+				return p, nil
+			}
+		}
+		return "/dev/sda", nil
+	} else {
+		return stripPart(devicePart), nil
+	}
+}
+
+// GetInternalDevice returns the internal disk based on the current disk.
+func GetInternalDevice(ctx context.Context) (string, error) {
+	rootPart, err := GetRootPart(ctx)
+	if err != nil {
+		return "", err
+	}
+	internalDisk, err := GetInternalDisk(ctx, rootPart)
+	if err != nil {
+		return "", err
+	}
+	return internalDisk, nil
 }
