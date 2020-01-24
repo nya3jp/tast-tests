d286ee123ecbeccea5a77679bed778e6e579be59
Greg Edelston <gredelston@google.com>
2020-01-24 13:01:26 -0700

    Add BlockingSync() RPC
    Firmware tests need to sync the device before switching the boot mode.
    This CL adds a BlockingSync RPC based on Autotest logic:
        http://cs/chromeos_public/src/third_party/autotest/files/...
        server/cros/faft/firmware_test.py?l=1101
    
    BUG=chromium:993062
    TEST=firmware.CheckBootMode
    
    Change-Id: I9a2097853cb3a7bebd901227e88f08b1b6576e37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2019573
    Tested-by: Greg Edelston <gredelston@google.com>
    Commit-Queue: Greg Edelston <gredelston@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Wai-Hong Tam <waihong@google.com>

--- src/chromiumos/tast/local/bundles/cros/firmware/utils_service.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/utils_service.go
@@ -5,11 +5,15 @@
 package firmware
 
 import (
+	"strings"
+
+	"github.com/golang/protobuf/ptypes/empty"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/firmware"
+	"chromiumos/tast/local/testexec"
 	fwpb "chromiumos/tast/services/cros/firmware"
 	"chromiumos/tast/testing"
 )
@@ -47,4 +51,75 @@ func (*UtilsService) CheckBootMode(ctx context.Context, req *fwpb.CheckBootModeR
 		return nil, err
 	}
 	return &fwpb.CheckBootModeResponse{Verified: verified}, nil
+}
+
+// BlockingSync syncs the root device and internal device.
+func (*UtilsService) BlockingSync(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	// The double calls to sync fakes a blocking call
+	// since the first call returns before the flush is complete,
+	// but the second will wait for the first to finish.
+	for i := 0; i < 2; i++ {
+		if err := testexec.CommandContext(ctx, "sync").Run(testexec.DumpLogOnError); err != nil {
+			return nil, errors.Wrapf(err, "sending sync command #%d to DUT", i+1)
+		}
+	}
+
+	// Find the root device.
+	rootDevice, err := firmware.RootDevice(ctx)
+	if err != nil {
+		return nil, err
+	}
+	devices := []string{rootDevice}
+
+	// If booted from removable media, sync the internal device too.
+	isRemovable, err := firmware.BootDeviceRemovable(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if isRemovable {
+		internalDevice, err := firmware.InternalDevice(ctx)
+		if err != nil {
+			return nil, err
+		}
+		devices = append(devices, internalDevice)
+	}
+
+	// sync only sends SYNCHRONIZE_CACHE but doesn't check the status.
+	// This function will perform a device-specific sync command.
+	for _, device := range devices {
+		if strings.Contains(device, "mmcblk") {
+			// For mmc devices, use `mmc status get` command to send an
+			// empty command to wait for the disk to be available again.
+			if err := testexec.CommandContext(ctx, "mmc", "status", "get", device).Run(testexec.DumpLogOnError); err != nil {
+				return nil, errors.Wrapf(err, "sending mmc command to device %s", device)
+			}
+		} else if strings.Contains(device, "nvme") {
+			// For NVMe devices, use `nvme flush` command to commit data
+			// and metadata to non-volatile media.
+			// Get a list of NVMe namespaces, and flush them individually.
+			// The output is assumed to be in the following format:
+			// [ 0]:0x1
+			// [ 1]:0x2
+			namespaces, err := testexec.CommandContext(ctx, "nvme", "list-ns", device).Output(testexec.DumpLogOnError)
+			if err != nil {
+				return nil, errors.Wrapf(err, "listing namespaces for device %s", device)
+			}
+			if len(namespaces) == 0 {
+				return nil, errors.Errorf("Listing namespaces for device %s returned no output", device)
+			}
+			for _, namespace := range namespaces {
+				ns := strings.Split(string(namespace), ":")[1]
+				if err := testexec.CommandContext(ctx, "nvme", "flush", device, "-n", ns).Run(testexec.DumpLogOnError); err != nil {
+					return nil, errors.Wrapf(err, "flushing namespace %s on device %s", ns, device)
+				}
+			}
+		} else {
+			// For other devices, hdparm sends TUR to check if
+			// a device is ready for transfer operation.
+			if err := testexec.CommandContext(ctx, "hdparm", "-f", device).Run(testexec.DumpLogOnError); err != nil {
+				return nil, errors.Wrapf(err, "sending hdparm command to device %s", device)
+			}
+		}
+	}
+	return &empty.Empty{}, nil
 }
