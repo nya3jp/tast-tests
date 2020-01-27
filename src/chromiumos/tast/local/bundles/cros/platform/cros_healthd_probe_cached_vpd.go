71dca539a847c8847989a158fa96f523fc57299c
Jesse Schettler <jschettler@chromium.org>
2020-01-27 11:23:17 -0700

    tast: Use cros_config to check if device has SKU number
    Use cros_config to check if a device has a SKU number. If it does,
    verify that the SKU number is printed. If it does not, verify that "NA"
    is printed.
    
    BUG=None
    TEST=tast run --build $DUT_IP platform.CrosHealthdProbeCachedVpd
    
    Change-Id: Idf7740b252eb44439136a9e420e20e0fdec5f1a1

--- src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_cached_vpd.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_cached_vpd.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"strings"
 
+	"chromiumos/tast/local/bundles/cros/platform/crosconfig"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
@@ -18,11 +19,12 @@ func init() {
 		Func: CrosHealthdProbeCachedVpd,
 		Desc: "Check that we can probe cros_healthd for cached vpd info",
 		Contacts: []string{
+			"jschettler@google.com",
 			"khegde@google.com",
 			"pmoy@google.com",
 		},
 		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"diagnostics"},
+		SoftwareDeps: []string{"cros_config", "diagnostics"},
 	})
 }
 
@@ -30,14 +32,17 @@ func CrosHealthdProbeCachedVpd(ctx context.Context, s *testing.State) {
 	if err := upstart.EnsureJobRunning(ctx, "cros_healthd"); err != nil {
 		s.Fatal("Failed to start cros_healthd: ", err)
 	}
+
 	b, err := testexec.CommandContext(ctx, "telem", "--category=cached_vpd").Output(testexec.DumpLogOnError)
 	if err != nil {
 		s.Fatal("Failed to run 'telem --category=cached_vpd': ", err)
 	}
+
 	lines := strings.Split(strings.TrimRight(string(b), "\n"), "\n")
 	if len(lines) != 2 {
 		s.Fatalf("Wrong number of lines: got %v, want 2", len(lines))
 	}
+
 	// Expect the keys to be just sku_number.
 	want := "sku_number"
 	got := lines[0]
@@ -45,8 +50,19 @@ func CrosHealthdProbeCachedVpd(ctx context.Context, s *testing.State) {
 		s.Fatalf("Header keys: got %v; want %v", got, want)
 	}
 
-	// All we want is a non-empty value for sku_number.
-	if lines[1] == "" {
-		s.Fatal("Empty sku_number")
+	// Check if the device has a SKU number. If it does, the SKU number should
+	// be printed. If it does not, "NA" should be printed.
+	hasSku, err := crosconfig.Get(ctx, "/cros-healthd/cached-vpd", "has-sku-number")
+	if err != nil {
+		s.Fatal("Failed to get has-sku-number property: ", err)
+	}
+
+	sku := lines[1]
+	if hasSku == "true" && sku == "" {
+		s.Fatal("Empty SKU number")
+	}
+
+	if hasSku != "true" && sku != "NA" {
+		s.Fatalf("Incorrect SKU number: got %v, want NA", sku)
 	}
 }
