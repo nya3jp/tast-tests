a5fbeaae322afbad060255da12b69348d180e994
Jesse Schettler <jschettler@chromium.org>
2020-01-27 11:23:17 -0700

    tast: Use cros_config to check if device has SKU number
    Use cros_config to check if a device has a SKU number. If it does,
    verify that the SKU number is printed. If it does not, verify that "NA"
    is printed.
    
    Also, add a new crosconfig support library for tests that use
    cros_config.
    
    BUG=None
    TEST=tast run --build $DUT_IP platform.CrosHealthdProbeCachedVpd
    TEST=tast run --build $DUT_IP hardware.SensorPresence
    TEST=fast_build.sh -T
    
    Change-Id: Idf7740b252eb44439136a9e420e20e0fdec5f1a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2023326
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Jesse Schettler <jschettler@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>

---
+++ src/chromiumos/tast/local/crosconfig/crosconfig.go
@@ -0,0 +1,44 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package crosconfig provides methods for using the cros_config command line
+// utility. See https://chromium.googlesource.com/chromiumos/platform2/+/HEAD/chromeos-config
+// for more information about cros_config.
+package crosconfig
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+)
+
+// ErrNotFound describes an error encountered when a property is not found by
+// cros_config.
+type ErrNotFound struct {
+	*errors.E
+}
+
+// IsNotFound returns true if the given error is of type errNotFound.
+func IsNotFound(err error) bool {
+	_, ok := err.(*ErrNotFound)
+	return ok
+}
+
+// Get returns the given property as a string if it is set and returns an empty
+// string if it is not.
+func Get(ctx context.Context, path, prop string) (string, error) {
+	b, err := testexec.CommandContext(ctx, "cros_config", path, prop).Output()
+	if err != nil {
+		status, ok := testexec.GetWaitStatus(err)
+		if ok && status.ExitStatus() == 1 {
+			return "", &ErrNotFound{E: errors.Errorf("property not found: %v", prop)}
+		}
+
+		return "", err
+	}
+
+	return strings.TrimSpace(string(b)), nil
+}
