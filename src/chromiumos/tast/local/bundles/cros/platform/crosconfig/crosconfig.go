f89a7eaa105f1a5036a51dea29b063649a028964
Jesse Schettler <jschettler@chromium.org>
2020-01-27 11:23:17 -0700

    tast: Use cros_config to check if device has SKU number
    Use cros_config to check if a device has a SKU number. If it does,
    verify that the SKU number is printed. If it does not, verify that "NA"
    is printed.
    
    BUG=None
    TEST=tast run --build $DUT_IP platform.CrosHealthdProbeCachedVpd
    
    Change-Id: Idf7740b252eb44439136a9e420e20e0fdec5f1a1

---
+++ src/chromiumos/tast/local/bundles/cros/platform/crosconfig/crosconfig.go
@@ -0,0 +1,33 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package crosconfig provides methods for interacting with the cros_config
+// command line utility.See https://chromium.googlesource.com/chromiumos/platform2/+/HEAD/chromeos-config
+// for more information about cros_config.
+package crosconfig
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/local/testexec"
+)
+
+// GetProperty returns the given property as a string if it is set and returns
+// an empty string if it is not.
+func GetProperty(ctx context.Context, path string, prop string) (string, error) {
+	output, err := testexec.CommandContext(ctx, "cros_config", path, prop).Output(testexec.DumpLogOnError)
+	status, ok := testexec.GetWaitStatus(err)
+	if !ok {
+		return "", err
+	}
+
+	// If cros_config exits with a code of 1, it means that the value was not
+	// present in the model.yaml, and the output will be an empty string.
+	if status.ExitStatus() == 0 || status.ExitStatus() == 1 {
+		return strings.TrimSpace(string(output)), nil
+	}
+
+	return "", err
+}
