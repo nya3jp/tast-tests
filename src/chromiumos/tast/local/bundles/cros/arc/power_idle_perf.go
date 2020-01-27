8f2e90cf00ad0c01465ebc83342c304a7c1c1801
Charles William Dick <cwd@google.com>
2020-01-27 09:29:28 +0900

    tast Idle perf test
    Introduces PowerIdlePerf, a performance test that measure idle power
    use.
    
    BUG=b:148350240
    TEST=tast run $DUT arc.PowerIdlePerf.*
    
    Change-Id: I16d3ef22c689ecb1a9ca5be3c06ec0151fa51045

---
+++ src/chromiumos/tast/local/bundles/cros/arc/power_idle_perf.go
@@ -0,0 +1,101 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/local/power"
+	"chromiumos/tast/local/power/setup"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PowerIdlePerf,
+		Desc: "Measures the battery drain of an idle system",
+		Contacts: []string{
+			"cwd@chromium.org",
+			"arcvm-eng@google.com",
+		},
+		SoftwareDeps: []string{"chrome", "android_both"},
+		Params: []testing.Param{{
+			Name:      "noarc",
+			ExtraAttr: []string{"group:crosbolt", "crosbolt_nightly"},
+			Pre:       chrome.LoggedIn(),
+		}, {
+			Name:              "",
+			ExtraAttr:         []string{"group:crosbolt", "crosbolt_nightly"},
+			ExtraSoftwareDeps: []string{"android"},
+			Pre:               arc.Booted(),
+		}, {
+			Name:              "vm",
+			ExtraAttr:         []string{"group:crosbolt", "crosbolt_nightly"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               arc.VMBooted(),
+		}},
+		Timeout: 15 * time.Minute,
+	})
+}
+
+func PowerIdlePerf(ctx context.Context, s *testing.State) {
+	// Give cleanup actions a minute to run, even if we fail by exceeding our
+	// deadline.
+	cleanupCtx := ctx
+	ctx, _ = ctxutil.Shorten(ctx, time.Minute)
+
+	sup, cleanup := setup.New()
+	defer func() {
+		if err := cleanup(cleanupCtx); err != nil {
+			s.Fatal("Cleanup failed: ", err)
+		}
+	}()
+
+	sup.Add(setup.PowerTest(ctx))
+	if err := sup.Check(ctx); err != nil {
+		s.Fatal("Setup failed: ", err)
+	}
+
+	p := perf.NewValues()
+	metrics, err := perf.NewTimeline(
+		ctx,
+		power.NewBatteryMetrics(2.0),
+		power.NewRAPLMetrics(),
+	)
+	if err != nil {
+		s.Fatal("Failed to build metrics: ", err)
+	}
+	s.Log("Finished setup")
+
+	if err := power.WaitUntilCPUCoolDown(ctx, power.CoolDownPreserveUI); err != nil {
+		s.Fatal("Failed to cool down: ", err)
+	}
+
+	if err := metrics.Start(ctx); err != nil {
+		s.Fatal("Failed to start metrics: ", err)
+	}
+	const (
+		iterationCount    = 30
+		iterationDuration = 10 * time.Second
+	)
+	for i := 0; i < iterationCount; i++ {
+		if err := testing.Sleep(ctx, iterationDuration); err != nil {
+			s.Fatal("Failed to sleep between metric snapshots: ", err)
+		}
+		s.Logf("Iteration %d snapshot", i)
+		if err := metrics.Snapshot(ctx, p); err != nil {
+			s.Fatal("Failed to snapshot metrics: ", err)
+		}
+	}
+
+	if err := p.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
