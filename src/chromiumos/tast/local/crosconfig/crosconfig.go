b45fa5855e20949ecc918390c90d42e411510d59
Jesse Schettler <jschettler@chromium.org>
2020-01-27 11:23:17 -0700

    tast: Use cros_config to check if device has SKU number
    Use cros_config to check if a device has a SKU number. If it does,
    verify that the SKU number is printed. If it does not, verify that "NA"
    is printed.
    
    Also, add a new crosconfig support library for tests that use
    cros_config.
    
    BUG=None
    TEST=tast run --build $DUT_IP platform.CrosHealthdProbeCachedVpd
    
    Change-Id: Idf7740b252eb44439136a9e420e20e0fdec5f1a1

---
+++ src/chromiumos/tast/local/crosconfig/crosconfig.go
@@ -0,0 +1,47 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package crosconfig provides methods for using the cros_config command line
+// utility. See https://chromium.googlesource.com/chromiumos/platform2/+/HEAD/chromeos-config
+// for more information about cros_config.
+package crosconfig
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+)
+
+type errNotFound struct {
+	*errors.E
+}
+
+// IsNotFound returns true if the given error is of type errNotFound.
+func IsNotFound(err error) bool {
+	_, ok := err.(*errNotFound)
+	return ok
+}
+
+// RunCrosConfig runs cros_config.
+var RunCrosConfig = func(ctx context.Context, args ...string) ([]byte, error) {
+	return testexec.CommandContext(ctx, "cros_config", args...).Output()
+}
+
+// Get returns the given property as a string if it is set and returns an empty
+// string if it is not.
+func Get(ctx context.Context, path string, prop string) (string, error) {
+	b, err := RunCrosConfig(ctx, path, prop)
+	if err != nil {
+		status, ok := testexec.GetWaitStatus(err)
+		if ok && status.ExitStatus() == 1 {
+			return "", &errNotFound{E: errors.Wrap(err, "Property not found")}
+		}
+
+		return "", err
+	}
+
+	return strings.TrimSpace(string(b)), nil
+}
