369f746e0460f18fb79692e53062b1ded5a03cee
Shuhei Takahashi <nya@chromium.org>
2020-01-28 14:24:16 +0900

    crash: Move most of sender-related utils to the crash package.
    This is a part of changes to merge sender-related utilities.
    
    This change just moves the code with some renaming to consider
    the new package name, e.g.
    
     sender.EnableMock -> crash.EnableMockSending
     sender.Run -> crash.RunSender
    
    This is a pure refactoring; no functional change is expected.
    
    BUG=chromium:1046647
    TEST=fast_build.sh -T
    TEST=tast run DUT crash.Sender*
    
    Cq-Depend: chromium:2029537
    Cq-Depend: chromium:2026815
    Change-Id: I7340fa2f1897606b086e3aa5e05f7f38e44db6c6

--- src/chromiumos/tast/local/bundles/cros/crash/sender_retry.go
+++ src/chromiumos/tast/local/bundles/cros/crash/sender_retry.go
@@ -12,6 +12,7 @@ import (
 
 	"chromiumos/tast/local/bundles/cros/crash/sender"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/crash"
 	"chromiumos/tast/testing"
 )
 
@@ -38,47 +39,47 @@ func SenderRetry(ctx context.Context, s *testing.State) {
 	defer sender.TearDown()
 
 	const basename = "some_program.1.2.3"
-	exp, err := sender.AddFakeMinidumpCrash(ctx, basename)
+	exp, err := crash.AddFakeMinidumpCrash(ctx, basename)
 	if err != nil {
 		s.Fatal("Failed to add a fake minidump crash: ", err)
 	}
 
 	// Simulate a failed upload.
-	if err := sender.EnableMock(false); err != nil {
+	if err := crash.EnableMockSending(false); err != nil {
 		s.Fatal("Failed to set mock mode: ", err)
 	}
 
-	got, err := sender.Run(ctx)
+	got, err := crash.RunSender(ctx)
 	if err != nil {
 		s.Fatal("Failed to run crash_sender: ", err)
 	}
 	// On simulating a failed upload, crash_sender sets the image type to "mock-fail".
 	// See GetImageType in crash_sender_util.cc.
 	exp.ImageType = "mock-fail"
-	want := []*sender.SendResult{{
+	want := []*crash.SendResult{{
 		Success: false,
 		Data:    *exp,
 	}}
-	if diff := cmp.Diff(got, want, cmpopts.IgnoreFields(sender.SendResult{}, "Schedule")); diff != "" {
+	if diff := cmp.Diff(got, want, cmpopts.IgnoreFields(crash.SendResult{}, "Schedule")); diff != "" {
 		s.Log("Results mismatch (-got +want): ", diff)
 		s.Errorf("crash_sender sent unexpected %d results; see logs for diff", len(got))
 	}
 
 	// Simulate a successful upload.
-	if err := sender.EnableMock(true); err != nil {
+	if err := crash.EnableMockSending(true); err != nil {
 		s.Fatal("Failed to set mock mode: ", err)
 	}
 
-	got, err = sender.Run(ctx)
+	got, err = crash.RunSender(ctx)
 	if err != nil {
 		s.Fatal("Failed to run crash_sender: ", err)
 	}
 	exp.ImageType = ""
-	want = []*sender.SendResult{{
+	want = []*crash.SendResult{{
 		Success: true,
 		Data:    *exp,
 	}}
-	if diff := cmp.Diff(got, want, cmpopts.IgnoreFields(sender.SendResult{}, "Schedule")); diff != "" {
+	if diff := cmp.Diff(got, want, cmpopts.IgnoreFields(crash.SendResult{}, "Schedule")); diff != "" {
 		s.Log("Results mismatch (-got +want): ", diff)
 		s.Errorf("crash_sender sent unexpected %d results; see logs for diff", len(got))
 	}
