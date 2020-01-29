066dbcc25a530594908e8c1917cf71e1b6aff8c7
Charles William Dick <cwd@google.com>
2020-01-29 18:07:11 +0900

    tast power keyboard backlight setting
    Introduces the SetKeyboardBrightness setup action, which sets a DUT's
    keyboard brightness.
    
    BUG=b:148820690
    TEST=tast run servo arc.PowerIdlePerf, at top of stack
    
    Change-Id: I76550060f96b0153c7cc88ab6526cccf445a0fee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2028330
    Tested-by: Charles Dueck <cwd@chromium.org>
    Commit-Queue: Charles Dueck <cwd@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/local/power/setup/setup_keyboard_backlight.go
@@ -0,0 +1,53 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package setup
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func keyboardBrightness(ctx context.Context) (uint, error) {
+	output, err := testexec.CommandContext(ctx, "backlight_tool", "--keyboard", "--get_brightness").Output(testexec.DumpLogOnError)
+	if err != nil {
+		return 0, errors.Wrap(err, "unable to get current keyboard brightness")
+	}
+	brightness, err := strconv.ParseUint(strings.TrimSpace(string(output)), 10, 64)
+	if err != nil {
+		return 0, errors.Wrapf(err, "unable to parse current keyboard brightness from %q", output)
+	}
+	return uint(brightness), nil
+}
+
+func setKeyboardBrightness(ctx context.Context, brightness uint) error {
+	brightnessArg := fmt.Sprintf("--set_brightness=%d", brightness)
+	if err := testexec.CommandContext(ctx, "backlight_tool", "--keyboard", brightnessArg).Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "unable to set keyboard brightness")
+	}
+	return nil
+}
+
+// SetKeyboardBrightness sets the keyboard brightness.
+func SetKeyboardBrightness(ctx context.Context, brightness uint) (CleanupCallback, error) {
+	prevBrightness, err := keyboardBrightness(ctx)
+	if err != nil {
+		return nil, err
+	}
+	testing.ContextLogf(ctx, "Setting keyboard backlight brightness to %d from %d", brightness, prevBrightness)
+	if err := setKeyboardBrightness(ctx, brightness); err != nil {
+		return nil, err
+	}
+
+	return func(ctx context.Context) error {
+		testing.ContextLogf(ctx, "Resetting keyboard backlight brightness to %d", prevBrightness)
+		return setKeyboardBrightness(ctx, prevBrightness)
+	}, nil
+}
