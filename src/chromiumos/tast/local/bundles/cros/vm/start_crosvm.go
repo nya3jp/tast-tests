0b7af255b9751f9ce16dd889cd48ead691aa807a
Daniel Verkamp <dverkamp@chromium.org>
2020-01-29 10:55:17 -0800

    tast: promote vm.StartCrosvm to critical
    The last issue causing failures has been resolved (old firmware on kukui
    boards in the lab), so let's remove the informational flag.
    
    BUG=chromium:1025374,chromium:1033681
    TEST=tast run $DUT vm.StartCrosvm
    
    Change-Id: I3e788b62012858fb7643f820c24da26205017fbc

--- src/chromiumos/tast/local/bundles/cros/vm/start_crosvm.go
+++ src/chromiumos/tast/local/bundles/cros/vm/start_crosvm.go
@@ -21,7 +21,7 @@ func init() {
 		Func:         StartCrosvm,
 		Desc:         "Checks that crosvm starts termina and runs commands through stdin",
 		Contacts:     []string{"smbarber@chromium.org", "crosvm-dev@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"vm_host"},
 		// TODO(smbarber): Download only files for the current architecture.
 		Data: []string{
