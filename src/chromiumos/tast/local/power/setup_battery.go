ab33cc65370a720e2abe261399d0fe25f7d750b4
Charles William Dick <cwd@google.com>
2020-01-29 18:14:17 +0900

    tast power battery setup
    Introduces SetBatteryDischarge, a setup action that forces a DUT's battery to
    discharge.
    
    TEST=tast run servo arc.PowerIdlePerf, at top of stack
    BUG=b:148820690
    
    Change-Id: I309e1bd4baf4edd8b7b20f3c96749c0251b53e0e

---
+++ src/chromiumos/tast/local/power/setup_battery.go
@@ -0,0 +1,63 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/power/ectool"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func setChargecontrolDischarge(ctx context.Context) error {
+	if err := testexec.CommandContext(ctx, "ectool", "chargecontrol", "discharge").Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "unable to force battery discharge")
+	}
+	return nil
+}
+
+func setChargecontrolNormal(ctx context.Context) error {
+	if err := testexec.CommandContext(ctx, "ectool", "chargecontrol", "normal").Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "unable to disable battery discharge")
+	}
+	return nil
+}
+
+// SetBatteryDischarge forces the battery to discharge. This will fail if the
+// remaining battery is within lowBatteryMargin of the low power shutdown
+// level.
+func SetBatteryDischarge(ctx context.Context, lowBatteryMargin float64, chain CleanupChain) (CleanupChain, error) {
+	setupFailed, guard := SetupFailureGuard(chain)
+	defer guard(ctx)
+
+	low, err := ectool.LowBatteryShutdownPercent(ctx)
+	if err != nil {
+		return nil, err
+	}
+	b, err := ectool.NewBatteryState(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if (low + lowBatteryMargin) >= b.ChargePercent() {
+		return nil, errors.Errorf("battery percent %.2f is too low to start discharging", b.ChargePercent())
+	}
+	if b.Discharging() {
+		testing.ContextLog(ctx, "WARNING Battery is already discharging")
+	}
+	if err := setChargecontrolDischarge(ctx); err != nil {
+		return nil, err
+	}
+	testing.ContextLog(ctx, "Set battery to discharge")
+
+	return SetupSucceeded(setupFailed, chain, func(ctx context.Context) error {
+		if err := setChargecontrolNormal(ctx); err != nil {
+			return err
+		}
+		testing.ContextLog(ctx, "Restored battery discharge to normal")
+		return nil
+	})
+}
