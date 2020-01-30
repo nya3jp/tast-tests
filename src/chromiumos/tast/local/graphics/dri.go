10f2508480d0d67e4caeb6c1fb053e93c420ad1c
Miguel Casas <mcasas@chromium.org>
2020-01-30 18:09:39 -0500

    tast/video: add Play cases and logic for memory comparison
    This CL adds three test cases and logic for comparing the amount
    of framebuffer objects of specific dimensions before-after playing
    a video, failing if those two are not the same, which would mean
    there's a leak in Chrome-minigbm allocated resources.
    
    A new file dri.go is added to interact with the DRI debugfs. A
    graphics.Backend interface is added to define what is needed from
    that debugfs, with a single I915Backend implementation. This is
    used for three methods: CompareGraphicsMemoryBeforeAfter(),
    readStableObjectCount() and waitForStableReadings(), that
    basically wait for stability of the readings and compare them.
    
    The latter two methods emulate the CPU.go wait-for-idle logic
    but they are _not_ the same: CPU goes to zero, here a moving
    average is used.
    
    TEST=tast run ${IP} video.Play.*memcheck on kohaku and atlas
    BUG=chromium:1047514
    
    Change-Id: I0307bbd18662372149372bb220c7d4edf1e069b3

---
+++ src/chromiumos/tast/local/graphics/dri.go
@@ -0,0 +1,187 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package graphics contains graphics-related utility functions for local tests.
+package graphics
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"os"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+const (
+	// The debugfs file with the information on allocated framebuffers.
+	i915FramebufferFile = "/sys/kernel/debug/dri/0/i915_gem_framebuffer"
+	// Immediately after login there's a lot of graphics activity; wait for a
+	// minute until it subsides.
+	coolDownTimeAfterLogin = 1 * time.Second
+)
+
+// Backend contains the necessary methods to interact with the platform
+// debug interface: checking if it's supported and getting readings.
+type Backend interface {
+	// SupportsFramebufferInfo returns true if the platform supports graphics
+	// memory allocation debugging and it's available.
+	SupportsFramebufferInfo() bool
+
+	// ReadFramebufferCount tries to retrieve the amount of framebuffers of width
+	// and height dimensions allocated by the Backend.
+	ReadFramebufferCount(ctx context.Context, width, height int) (framebuffers int, err error)
+}
+
+// I915Backend Implements Backend for the Intel i915 case.
+type I915Backend struct{}
+
+// SupportsFramebufferInfo returns true if the debug file for graphics
+// memory allocation readings is available.
+func (g I915Backend) SupportsFramebufferInfo() bool {
+	_, err := os.Stat(i915FramebufferFile)
+	return err == nil
+}
+
+// ReadFramebufferCount tries to open the i915FramebufferFile and count the
+// amount of lines of dimensions width x height, which corresponds to the amount
+// of framebuffers allocated in the system.
+// See https://dri.freedesktop.org/docs/drm/gpu/i915.html
+func (g I915Backend) ReadFramebufferCount(ctx context.Context, width, height int) (framebuffers int, err error) {
+	f, err := os.Open(i915FramebufferFile)
+	if err != nil {
+		return framebuffers, errors.Wrap(err, "failed to open dri file")
+	}
+	text, err := ioutil.ReadAll(f)
+	if err != nil {
+		return framebuffers, errors.Wrap(err, "failed to read dri file")
+	}
+	lines := strings.Split(string(text), "\n")
+	numFbs := 0
+	for _, line := range lines {
+		// The line we're looking for looks like "user size: 1920 x 1080,...
+		var fbWidth, fbHeight int
+		_, err := fmt.Sscanf(line, "user size: %d x %d", &fbWidth, &fbHeight)
+		if err != nil {
+			continue
+		}
+		if fbWidth == width && fbHeight == height {
+			numFbs++
+		}
+	}
+	framebuffers = numFbs
+	return
+}
+
+// CompareGraphicsMemoryBeforeAfter compares the graphics memory consumption
+// before and after running the payload function, using the backend.
+func CompareGraphicsMemoryBeforeAfter(ctx context.Context, payload func(), backend Backend, width, height int) (err error) {
+	var before int
+	var after int
+
+	testing.ContextLogf(ctx, "Cooling down %v after log in", coolDownTimeAfterLogin)
+	if err := testing.Sleep(ctx, coolDownTimeAfterLogin); err != nil {
+		return errors.Wrap(err, "error while cooling down after log in")
+	}
+
+	if before, err = readStableObjectCount(ctx, backend, width, height); err != nil {
+		return errors.Wrap(err, "failed to get the i915 framebuffer object count")
+	}
+
+	payload()
+
+	if after, err = readStableObjectCount(ctx, backend, width, height); err != nil {
+		return errors.Wrap(err, "failed to get the i915 framebuffer object count")
+	}
+	testing.ContextLogf(ctx, "i915 objects before=%d, after=%d, delta=%d", before, after, after-before)
+	if before != after {
+		return errors.Wrapf(err, "i915 objects before=%d, after=%d do not coincide! delta=%d", before, after, after-before)
+	}
+	return nil
+}
+
+// readStableObjectCount waits until a given graphics object count obtained with
+// backend is stable, up to a certain timeout, progressively relaxing a
+// similarity threshold criteria.
+func readStableObjectCount(ctx context.Context, backend Backend, width, height int) (objectCount int, err error) {
+	const (
+		pollingInterval = 1 * time.Second
+		// Time to wait for the object count to be stable.
+		waitTimeout = 120 * time.Second
+		// Threshold (in percentage) below which the object count is considered stable.
+		objectCountThresholdBase = 0.1
+		// Maximum threshold (in percentage) for the object count to be considered stable.
+		objectCountThresholdMax = 2.0
+		// Maximum steps of relaxing the object count similarity threshold.
+		relaxingThresholdSteps = 5
+	)
+
+	// TODO()mcasas) width and height need to be rounded up to closest multiple of 16.
+
+	startTime := time.Now()
+	delta := (objectCountThresholdMax - objectCountThresholdBase) / (relaxingThresholdSteps - 1)
+	testing.ContextLogf(ctx, "Waiting at most %v for stable graphics object count, threshold will be gradually relaxed from %.1f%% to %.1f%%",
+		waitTimeout, objectCountThresholdBase, objectCountThresholdMax)
+
+	for i := 0; i < relaxingThresholdSteps; i++ {
+		idlePercent := objectCountThresholdBase + (delta * float64(i))
+		timeout := waitTimeout / relaxingThresholdSteps
+		testing.ContextLogf(ctx, "Waiting up to %v for object count to settle within %.1f%% (%d/%d)",
+			timeout.Round(time.Second), idlePercent, i+1, relaxingThresholdSteps)
+
+		objectCount, err = waitForStableReadings(ctx, backend, width, height, timeout, pollingInterval, idlePercent)
+		if err == nil {
+			testing.ContextLogf(ctx, "Waiting for object count stabilisation took %v (value %d, threshold: %.1f%%)",
+				time.Now().Sub(startTime).Round(time.Second), objectCount, idlePercent)
+			return objectCount, nil
+		}
+	}
+	return objectCount, err
+}
+
+// waitForStableReadings reads values using backend and waits for up to timeout
+// for the moving average of the last numReadings to settle within threshold.
+func waitForStableReadings(ctx context.Context, backend Backend, width, height int, timeout, interval time.Duration, threshold float64) (reading int, err error) {
+	// Keep the last numReadings for moving average purposes. Make it half the
+	// size that the current timeout and interval would allow.
+	numReadings := int(math.Floor(float64(timeout / (2.0 * interval))))
+
+	var currentNumReadings int
+	var values = make([]int, numReadings)
+
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		var e error
+		reading, e = backend.ReadFramebufferCount(ctx, width, height)
+		if e != nil {
+			return testing.PollBreak(errors.Wrap(e, "failed measuring"))
+		}
+		values[currentNumReadings%numReadings] = reading
+		currentNumReadings++
+		if currentNumReadings < numReadings {
+			return errors.New("Need more values")
+		}
+		average := mean(values)
+
+		if math.Abs(float64(reading)-average) > threshold {
+			testing.ContextLogf(ctx, "Reading %d is not within %.1f of %.1f", reading, threshold, average)
+			return errors.Errorf("Reading %d is not within %.1f of %.1f", reading, threshold, average)
+		}
+		testing.ContextLogf(ctx, "Reading %d is within %.1f of %.1f", reading, threshold, average)
+		return nil
+	}, &testing.PollOptions{Timeout: timeout, Interval: interval})
+	return reading, err
+}
+
+// mean returns the average of values.
+func mean(values []int) float64 {
+	var sum float64
+	for _, v := range values {
+		sum += float64(v)
+	}
+	return sum / float64(len(values))
+}
