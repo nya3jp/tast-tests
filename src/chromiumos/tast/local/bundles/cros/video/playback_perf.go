bd9630439b53a2fdd15aa3e09271add5c0630a06
Hirokazu Honda <hiroh@chromium.org>
2020-01-30 13:45:07 +0900

    video: Add PlaybackPerf tests for Gav1VideoDecoder
    PlyabackGav1Perf is the test to measure performance of
    VideoDecoder using libgav1, called Gav1VideoDecoder.
    The purpose is to monitor and compare performance of
    Gav1VideoDecoder and Dav1dVideoDecoder (default Chrome AV1 SW
    decoder).
    
    Gav1VideoDecoder tunes especially for ARM platform today.
    Therefore, PlyabackGav1Perf runs on ARM boards only. The test
    doesn't measure HW decoder performance because the performance is
    measured in PlaybackPerf.
    
    BUG=chromium:1047051
    TEST=video.PlaybackPerf.*_gav1
    
    Change-Id: Icb096525727317a2dbca07c393b7f90999bc6c0f

--- src/chromiumos/tast/local/bundles/cros/video/playback_perf.go
+++ src/chromiumos/tast/local/bundles/cros/video/playback_perf.go
@@ -153,6 +153,46 @@ func init() {
 				decoderType: playback.VDA,
 			},
 			ExtraData: []string{"1080p_60fps_600frames.av1.mp4"},
+		}, {
+			Name: "av1_480p_30fps_gav1",
+			Val: playbackPerfParams{
+				fileName:    "480p_30fps_300frames.av1.mp4",
+				decoderType: playback.LibGAV1,
+			},
+			ExtraSoftwareDeps: []string{"arm"},
+			ExtraData:         []string{"480p_30fps_300frames.av1.mp4"},
+		}, {
+			Name: "av1_720p_30fps_gav1",
+			Val: playbackPerfParams{
+				fileName:    "720p_30fps_300frames.av1.mp4",
+				decoderType: playback.LibGAV1,
+			},
+			ExtraSoftwareDeps: []string{"arm"},
+			ExtraData:         []string{"720p_30fps_300frames.av1.mp4"},
+		}, {
+			Name: "av1_720p_60fps_gav1",
+			Val: playbackPerfParams{
+				fileName:    "720p_60fps_600frames.av1.mp4",
+				decoderType: playback.LibGAV1,
+			},
+			ExtraSoftwareDeps: []string{"arm"},
+			ExtraData:         []string{"720p_60fps_600frames.av1.mp4"},
+		}, {
+			Name: "av1_1080p_30fps_gav1",
+			Val: playbackPerfParams{
+				fileName:    "1080p_30fps_300frames.av1.mp4",
+				decoderType: playback.LibGAV1,
+			},
+			ExtraSoftwareDeps: []string{"arm"},
+			ExtraData:         []string{"1080p_30fps_300frames.av1.mp4"},
+		}, {
+			Name: "av1_1080p_60fps_gav1",
+			Val: playbackPerfParams{
+				fileName:    "1080p_60fps_600frames.av1.mp4",
+				decoderType: playback.LibGAV1,
+			},
+			ExtraSoftwareDeps: []string{"arm"},
+			ExtraData:         []string{"1080p_60fps_600frames.av1.mp4"},
 		}},
 	})
 }
