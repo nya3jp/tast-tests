e363c284b56dbd08969ca2cfefd944dfc98a99df
Miguel Casas <mcasas@chromium.org>
2020-01-30 18:09:39 -0500

    tast/video: add MemCheck test
    This CL adds a new MemCheck test and logic for comparing the amount
    of framebuffer objects of specific dimensions before-after playing
    a video, failing if those two are not the same, which would mean
    there's a leak in Chrome-minigbm allocated resources.
    
    A new file dri.go is added to interact with the DRI debugfs. A
    graphics.Backend interface is added to define what is needed from
    that debugfs, with a single I915Backend implementation. This is
    used for three methods: CompareGraphicsMemoryBeforeAfter(),
    readStableObjectCount() and waitForStableReadings(), that
    basically wait for stability of the readings and compare them.
    
    The latter two methods emulate the CPU.go wait-for-idle logic
    but they are _not_ the same: CPU goes to zero, here a moving
    average is used.
    
    BUG=chromium:1047514
    TEST=tast run ${IP} video.MemCheck.* on kohaku and atlas
    
    Change-Id: I0307bbd18662372149372bb220c7d4edf1e069b3

---
+++ src/chromiumos/tast/local/bundles/cros/video/mem_check.go
@@ -0,0 +1,80 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/video/decode"
+	"chromiumos/tast/local/bundles/cros/video/play"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/graphics"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/pre"
+	"chromiumos/tast/testing"
+)
+
+type memCheckParams struct {
+	fileName    string
+	videoWidth  int
+	videoHeight int
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: MemCheck,
+		Desc: "Checks video playback in Chrome has no leaks",
+		Contacts: []string{
+			"mcasas@chromium.org",
+			"chromeos-gfx-video@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Data:         []string{decode.ChromeMediaInternalsUtilsJSFile},
+		Params: []testing.Param{{
+			Name:              "h264_hw",
+			Val:               memCheckParams{fileName: "720_h264.mp4", videoWidth: 1280, videoHeight: 720},
+			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+			ExtraData:         []string{"video.html", "720_h264.mp4"},
+			ExtraSoftwareDeps: []string{"amd64", caps.HWDecodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
+			Pre:               pre.ChromeVideoWithGuestLogin(),
+			Timeout:           10 * time.Minute,
+		}, {
+			Name:              "vp8_hw",
+			Val:               memCheckParams{fileName: "720_vp8.webm", videoWidth: 1280, videoHeight: 720},
+			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+			ExtraData:         []string{"video.html", "720_vp8.webm"},
+			ExtraSoftwareDeps: []string{"amd64", caps.HWDecodeVP8},
+			Pre:               pre.ChromeVideoWithGuestLogin(),
+			Timeout:           10 * time.Minute,
+		}, {
+			Name:              "vp9_hw",
+			Val:               memCheckParams{fileName: "720_vp9.webm", videoWidth: 1280, videoHeight: 720},
+			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+			ExtraData:         []string{"video.html", "720_vp9.webm"},
+			ExtraSoftwareDeps: []string{"amd64", caps.HWDecodeVP9},
+			Pre:               pre.ChromeVideoWithGuestLogin(),
+			Timeout:           10 * time.Minute,
+		}},
+	})
+}
+
+// MemCheck plays a given fileName in Chrome and verifies there are no graphics
+// memory leaks by comparing its usage before, during and after.
+func MemCheck(ctx context.Context, s *testing.State) {
+	testOpt := s.Param().(memCheckParams)
+
+	testPlay := func() {
+		play.TestPlay(ctx, s, s.PreValue().(*chrome.Chrome), testOpt.fileName, play.NormalVideo, play.VerifyHWAcceleratorUsed)
+	}
+
+	// TODO(crbug.com/1047514): support AMD.
+	i915 := graphics.I915Backend{}
+	if i915.SupportsFramebufferInfo() {
+		if err := graphics.CompareGraphicsMemoryBeforeAfter(ctx, testPlay, i915, testOpt.videoWidth, testOpt.videoHeight); err != nil {
+			s.Fatal("Test failed: ", err)
+		}
+	}
+}
