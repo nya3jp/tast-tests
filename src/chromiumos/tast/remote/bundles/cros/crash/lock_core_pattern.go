c6c133af6c72f2f3d16df0c3430b3a22f305d04f
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-01-31 14:36:21 +0900

    tast-tests: Fix test for verifying core_pattern lock.
    The old test did not work because the commandline string was interpreted
    as the name of the program.
    Additionally, we can write to file with a native method.
    
    BUG=none
    TEST=tast run $DUT_IP crash.LockCorePattern
    
    Change-Id: If443621c68fb0ff86b2895e94d7a3f4902d06dc7

--- src/chromiumos/tast/remote/bundles/cros/crash/lock_core_pattern.go
+++ src/chromiumos/tast/remote/bundles/cros/crash/lock_core_pattern.go
@@ -6,6 +6,8 @@ package crash
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
 
 	"chromiumos/tast/dut"
 	"chromiumos/tast/testing"
@@ -14,7 +16,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         LockCorePattern,
-		Desc:         "Verify locked |core_pattern| after `crash_reporter --init` on kernels < 3.18",
+		Desc:         "Verify locked |core_pattern| after `crash_reporter --init` on kernels <= 3.18",
 		Contacts:     []string{"sarthakkukreti@chromium.org"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"lock_core_pattern", "reboot"},
@@ -35,10 +37,6 @@ func LockCorePattern(ctx context.Context, s *testing.State) {
 
 	// Reboot device: other tests may need to modify the |core_pattern|.
 	defer func() {
-		// Cleanup test in progress marker.
-		if err := d.Command("rm", "-f", "/run/crash_reporter/crash-test-in-progress").Run(ctx); err != nil {
-			s.Log("Failed to cleanup crash-reporter test-in-progress marker")
-		}
 		s.Log("Rebooting DUT")
 		if err := d.Reboot(ctx); err != nil {
 			s.Fatal("Failed to reboot: ", err)
@@ -50,10 +48,10 @@ func LockCorePattern(ctx context.Context, s *testing.State) {
 	}
 
 	// Try to modify |core_pattern|.
-	cmd := d.Command("echo 'hello' > /proc/sys/kernel/core_pattern")
-	if err := cmd.Run(ctx); err == nil {
-		s.Fatal("|core_pattern| writeable after crash_reporter initialization")
+	if err := ioutil.WriteFile("/proc/sys/kernel/core_pattern", []byte("hello"), os.FileMode(0644)); err == nil {
+		s.Fatal("|core_pattern| writable after crash_reporter initialization")
 	} else {
-		s.Log("Expected failure to write to |core_pattern|: ", err)
+		// TODO(sarthakkukreti): Verify it the error type was as expected, or remove this message.
+		s.Log("Write to |core_pattern| failed as expected: ", err)
 	}
 }
