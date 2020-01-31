051bc0afd3d801b1215e51abc3a6b2f6af1bf8e3
Sara Kato <sarakato@chromium.org>
2020-01-31 18:39:14 +0900

    tast-tests: Add test for changing density
    Per application density scaling was added in ag/9537749.
    
    This CL adds an e2e test that opens an application containing
    a 100x100 black square. The density is then changed using
    ctrl+/ctrl0/ctrl-, and the test confirms this by checking the
    size of the drawn square after each change.
    
    BUG=b:148040202
    TEST=tast run DUT_IP arc.PerAppDensity
    Change-Id: Ieb681165aa231196f9c750075e6e51bac9d1662a

---
+++ src/chromiumos/tast/local/bundles/cros/arc/per_app_density.go
@@ -0,0 +1,178 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"image/color"
+	"math"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/screenshot"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PerAppDensity,
+		Desc:         "Checks that density can be changed with Android applications",
+		Contacts:     []string{"sarakato@chromium.org", "arc-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"android_p", "chrome"},
+		Data:         []string{perAppDensityApk},
+		Timeout:      4 * time.Minute,
+		Pre:          arc.Booted(),
+	})
+}
+
+const perAppDensityApk = "ArcPerAppDensityTest.apk"
+
+// performAndConfirmDensityChange changes the density of the activity,
+// and confirms that the density was changed by validating the size of the square on the screen.
+func performAndConfirmDensityChange(ctx context.Context, cr *chrome.Chrome, ew *input.KeyboardEventWriter, a *arc.ARC, name string, keySequence string, blackPixelCount int) error {
+	testing.ContextLogf(ctx, "%s density using key %q", name, keySequence)
+	if err := ew.Accel(ctx, keySequence); err != nil {
+		return errors.Wrapf(err, "could not change scale factor using %q", keySequence)
+	}
+	if err := checkBlackPixels(ctx, cr, blackPixelCount); err != nil {
+		return errors.Wrap(err, "could not check number of black pixels")
+	}
+	return nil
+}
+
+// checkBlackPixels grabs a screenshots and checks that number of black pixels is equal to wantPixelCount.
+func checkBlackPixels(ctx context.Context, cr *chrome.Chrome, wantPixelCount int) error {
+	// Need to wait for relayout to complete, before grabbing new screenshot.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		img, err := screenshot.GrabScreenshot(ctx, cr)
+		if err != nil {
+			return testing.PollBreak(err)
+		}
+		gotPixelCount := screenshot.CountPixels(img, color.Black)
+		diff := math.Abs(float64(wantPixelCount-gotPixelCount) / float64(wantPixelCount))
+		if diff > 0.01 {
+			return errors.Errorf("wrong number of black pixels, got: %d, want: %f", gotPixelCount, wantPixelCount)
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 10 * time.Second}); err != nil {
+		return errors.Wrap(err, "timed out waiting for updated screen")
+	}
+	return nil
+}
+
+func PerAppDensity(ctx context.Context, s *testing.State) {
+	const (
+		setprop        = "/system/bin/setprop"
+		mainActivity   = ".MainActivity"
+		packageName    = "org.chromium.arc.testapp.perappdensitytest"
+		densitySetting = "persist.sys.enable_application_zoom"
+		// The following scale factors have been taken from TaskRecordArc.
+		increasedSF = 1.1
+		decreasedSF = 0.9
+		// Defined in vendor/google_arc/packages/developments/ArcPerAppDensityTest/res/layout/main_activity.xml.
+		squareSidePx = 100
+	)
+
+	type densityChange struct {
+		// The action that should be performed to the current density.
+		name string
+		// The corresponding key sequence to perform the action.
+		keySequence string
+		// The expected pixel count after performing the current action.
+		blackPixelCount float64
+	}
+	a := s.PreValue().(arc.PreData).ARC
+	cr := s.PreValue().(arc.PreData).Chrome
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	if err := arc.BootstrapCommand(ctx, setprop, densitySetting, "true").Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Failed to set developer option: ", err)
+	}
+	defer arc.BootstrapCommand(ctx, setprop, densitySetting, "false").Run(testexec.DumpLogOnError)
+
+	tabletModeEnabled, err := ash.TabletModeEnabled(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get tablet mode: ", err)
+	}
+	defer ash.SetTabletModeEnabled(ctx, tconn, tabletModeEnabled)
+
+	testing.ContextLog(ctx, "Installing app")
+	if err := a.Install(ctx, s.DataPath(perAppDensityApk)); err != nil {
+		s.Fatal("Failed to install app: ", err)
+	}
+
+	act, err := arc.NewActivity(a, packageName, mainActivity)
+	if err != nil {
+		s.Fatal("Failed to create new activity")
+	}
+	defer act.Close()
+
+	testing.ContextLog(ctx, "Starting activity")
+	if err := act.Start(ctx); err != nil {
+		s.Fatal("Failed to start the  activity: ", err)
+	}
+	if err := ash.SetTabletModeEnabled(ctx, tconn, false); err != nil {
+		s.Fatal("Failed to set tablet mode enabled to false: ", err)
+	}
+	if err := act.WaitForResumed(ctx, 10*time.Second); err != nil {
+		s.Fatal("Failed to wait for the activity to resume: ", err)
+	}
+	if err := act.SetWindowState(ctx, arc.WindowStateFullscreen); err != nil {
+		s.Fatal("Failed to set window state to Maximized: ", err)
+	}
+	if err := ash.WaitForARCAppWindowState(ctx, tconn, packageName, ash.WindowStateFullscreen); err != nil {
+		s.Fatal("Failed to wait for the activity to be Maximized: ", err)
+	}
+
+	ew, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Error creating keyboard: ", err)
+	}
+	defer ew.Close()
+
+	// To obtain the size of the expected black rectangle, it's necessary to obtain the dimensions of the rectangle
+	// as drawn on the screen. After changing the density, we then need to multiply by the square of the new scale
+	// factor (in order to account for changes to both width and height).
+	displayDensity, err := act.DisplayDensity(ctx)
+	if err != nil {
+		s.Fatal("Failed to get display density: ", err)
+	}
+	expectedInitialPixelCount := (displayDensity * squareSidePx) * (displayDensity * squareSidePx)
+	if err := checkBlackPixels(ctx, cr, int(expectedInitialPixelCount)); err != nil {
+		s.Fatal("Failed to check initial state: ", err)
+	}
+
+	for _, test := range []densityChange{
+		{
+			"increase",
+			"ctrl+=",
+			expectedInitialPixelCount * float64(increasedSF) * float64(increasedSF),
+		},
+		{
+			"reset",
+			"ctrl+0",
+			expectedInitialPixelCount,
+		},
+		{
+			"decrease",
+			"ctrl+-",
+			expectedInitialPixelCount * float64(decreasedSF) * float64(decreasedSF),
+		},
+	} {
+		if err := performAndConfirmDensityChange(ctx, cr, ew, a, test.name, test.keySequence, int(test.blackPixelCount)); err != nil {
+			s.Fatalf("Error with performing %s: %s", test.name, err)
+		}
+	}
+}
