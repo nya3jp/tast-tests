7ffb7253830b838adee9a93886a1050333e22c29
Jesse Schettler <jschettler@chromium.org>
2020-02-03 11:19:27 -0700

    tast: Use cros_config to check for a battery and Smart Battery info
    If a device has a battery, verify the reported battery metrics. If it
    does not, verify that the telem tool outputs no information.
    
    If a device has Smart Battery info, verify the reported Smart Battery
    metrics. If it does not, verify that "NA" is printed for these metrics.
    
    BUG=chromium:1044005
    TEST=tast run --build $DUT_IP platform.CrosHealthdProbeBatteryMetrics
    
    Change-Id: Ieae9e18780978e8c095293d4965c3acb39cbe24c

--- src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_battery_metrics.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_battery_metrics.go
@@ -9,10 +9,9 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"reflect"
-	"sort"
 	"strings"
 
-	"chromiumos/tast/local/power"
+	"chromiumos/tast/local/crosconfig"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
@@ -28,7 +27,7 @@ func init() {
 			"jschettler@google.com",
 		},
 		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"diagnostics"},
+		SoftwareDeps: []string{"cros_config", "diagnostics"},
 	})
 }
 
@@ -36,42 +35,59 @@ func CrosHealthdProbeBatteryMetrics(ctx context.Context, s *testing.State) {
 	if err := upstart.EnsureJobRunning(ctx, "cros_healthd"); err != nil {
 		s.Fatal("Failed to start cros_healthd: ", err)
 	}
-	status, err := power.GetStatus(ctx)
-	if err != nil {
-		s.Fatal("Failed to get battery status: ", err)
+
+	val, err := crosconfig.Get(ctx, "/hardware-properties", "psu-type")
+	if err != nil && !crosconfig.IsNotFound(err) {
+		s.Fatal("Failed to get psu-type property: ", err)
 	}
+
+	// If psu-type is not set to "AC_only", assume there is a battery.
+	hasBattery := !(err == nil && val == "AC_only")
+	s.Log("Device has battery: ", hasBattery)
+
 	b, err := testexec.CommandContext(ctx, "telem", "--category=battery").Output(testexec.DumpLogOnError)
 	if err != nil {
 		s.Fatal("Failed to run 'telem --category=battery': ", err)
 	}
+
 	if err := ioutil.WriteFile(filepath.Join(s.OutDir(), "command_output.txt"), b, 0644); err != nil {
 		s.Errorf("Failed to write output to %s: %v", filepath.Join(s.OutDir(), "command_output.txt"), err)
 	}
+
 	lines := strings.Split(strings.TrimRight(string(b), "\n"), "\n")
-	if status.BatteryPresent && len(lines) != 2 {
+	if !hasBattery {
+		if len(lines) != 1 {
+			s.Fatalf("Incorrect number of output lines: got %d; want 1", len(lines))
+		}
+		// If there is no battery, there is no output to verify.
+		return
+	}
+
+	if len(lines) != 2 {
 		s.Fatalf("Incorrect number of output lines: got %d; want 2", len(lines))
 	}
-	want := []string{"charge_full", "charge_full_design", "charge_now",
-		"cycle_count", "manufacture_date_smart", "model_name", "serial_number",
-		"temperature_smart", "vendor(manufacturer)", "voltage_min_design",
-		"voltage_now"}
-	header := strings.Split(lines[0], ",")
-	got := make([]string, len(header))
-	copy(got, header)
-	sort.Strings(got)
+
+	want := []string{"charge_full", "charge_full_design", "cycle_count",
+		"serial_number", "vendor(manufacturer)", "voltage_now",
+		"voltage_min_design", "manufacture_date_smart", "temperature_smart",
+		"model_name", "charge_now"}
+	got := strings.Split(lines[0], ",")
 	if !reflect.DeepEqual(want, got) {
-		s.Fatalf("header keys: got %v; want %v", got, want)
+		s.Fatalf("Incorrect headers: got %v, want %v", got, want)
 	}
+
 	metrics := strings.Split(lines[1], ",")
 	if len(metrics) != len(want) {
 		s.Fatalf("Incorrect number of battery metrics: got %d; want %d", len(metrics), len(want))
 	}
-	// Validate smart battery metrics
+
+	// Validate battery metrics.
+	// TODO(crbug.com/1048471): Update error values when bug is resolved.
 	contentsMap := make(map[string]string)
-	for i, elem := range header {
+	for i, elem := range got {
 		contentsMap[elem] = metrics[i]
 	}
-	for _, key := range []string{"charge_now", "cycle_count", "manufacture_date_smart", "temperature_smart"} {
+	for _, key := range []string{"charge_now", "cycle_count"} {
 		if value, ok := contentsMap[key]; !ok || value == "0" {
 			s.Errorf("Failed to collect %s", key)
 		}
@@ -84,6 +100,39 @@ func CrosHealthdProbeBatteryMetrics(ctx context.Context, s *testing.State) {
 	for _, key := range []string{"vendor(manufacturer)", "serial_number", "model_name"} {
 		if value, ok := contentsMap[key]; !ok || value == "" {
 			s.Errorf("Failed to collect %s", key)
+		}
+	}
+
+	// Validate Smart Battery metrics.
+	val, err = crosconfig.Get(ctx, "/cros-healthd/battery", "has-smart-battery-info")
+	if err != nil && !crosconfig.IsNotFound(err) {
+		s.Fatal("Failed to get has-smart-battery-info property: ", err)
+	}
+
+	hasSmartInfo := err == nil && val == "true"
+	s.Log("Device has Smart Battery info: ", hasSmartInfo)
+	for _, e := range []struct {
+		key     string
+		zeroFmt string
+	}{
+		{key: "manufacture_date_smart", zeroFmt: "0000-00-00"},
+		{key: "temperature_smart", zeroFmt: "0"},
+	} {
+		value, ok := contentsMap[e.key]
+		if !ok {
+			s.Errorf("Key %q not found", e.key)
+			continue
+		}
+
+		s.Logf("Value for %v: %v", e.key, value)
+		if hasSmartInfo {
+			if value == "NA" || value == e.zeroFmt {
+				s.Error("Invalid value for ", e.key)
+			}
+		} else {
+			if value != "NA" {
+				s.Error("Should not have value for ", e.key)
+			}
 		}
 	}
 }
