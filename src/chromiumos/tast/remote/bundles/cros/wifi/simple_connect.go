99a8b5d12bd4a1991dd50212ec76af30c44b1b1c
Yen-lin Lai <yenlinlai@google.com>
2020-02-03 14:28:22 +0800

    wifi: Migrate network_WiFi_SimpleConnect to Tast.
    This and previous commits port the following components in Autotest to
    build the basic flow of a remote WiFi Tast test:
    - router setup (inherit the codes in crrev.com/c/1733740)
    - gRPC for D-Bus operation on a remote DUT
    - connection verification with ping
    
    These commits have migrated basic 802.11 a/b/g tests. Based on this
    structure, upcoming commits are planned to complete the Tast migration
    of network_WiFi_SimpleConnect, including:
    - more testcases with different parameters
    - pcap, attenuator setup
    - collect system logs on companion devices
    - collect perf metrics (e.g. time used in every stage of connection)
    - setup/teardown on wifi client (ensuring DUT with clean state)
    - some setup/verification steps in inline TODOs
    
    Still other components will be ported when used, including:
    - avahi
    - attenuator control
    - bridging multiple hostap instance
    - more hostap configs setup (server/client side)
    - pcap parsing
    - security config (WEP, WPA...)
    
    BUG=chromium:1034875
    TEST=tast run -var=router=$ROUTER $DUT wifi.SimpleConnect*
    
    Cq-Depend: chromium:1971654
    Change-Id: Ifb4007ce65ac957492bfd9eff147619ded94a44b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1935952
    Tested-by: Yen-lin Lai <yenlinlai@google.com>
    Commit-Queue: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/simple_connect.go
@@ -0,0 +1,119 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/remote/wificell/hostapd"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/testing"
+)
+
+type simpleConnectTestcase struct {
+	apOptions []hostapd.Option
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        SimpleConnect,
+		Desc:        "Verifies that DUT can connect to the host via AP in different WiFi configuration",
+		Contacts:    []string{"yenlinlai@google.com", "chromeos-kernel-wifi@google.com"},
+		Attr:        []string{"group:wificell", "wifi_func"},
+		ServiceDeps: []string{"tast.cros.network.Wifi"},
+		Vars:        []string{"router"},
+		Params: []testing.Param{
+			{
+				// Verifies that DUT can connect to an open 802.11a network on channels 48, 64.
+				Name: "80211a",
+				Val: []simpleConnectTestcase{
+					{[]hostapd.Option{hostapd.Mode(hostapd.Mode80211a), hostapd.Channel(48)}},
+					{[]hostapd.Option{hostapd.Mode(hostapd.Mode80211a), hostapd.Channel(64)}},
+				},
+			}, {
+				// Verifies that DUT can connect to an open 802.11b network on channels 1, 6, 11.
+				Name: "80211b",
+				Val: []simpleConnectTestcase{
+					{[]hostapd.Option{hostapd.Mode(hostapd.Mode80211b), hostapd.Channel(1)}},
+					{[]hostapd.Option{hostapd.Mode(hostapd.Mode80211b), hostapd.Channel(6)}},
+					{[]hostapd.Option{hostapd.Mode(hostapd.Mode80211b), hostapd.Channel(11)}},
+				},
+			}, {
+				// Verifies that DUT can connect to an open 802.11g network on channels 1, 6, 11.
+				Name: "80211g",
+				Val: []simpleConnectTestcase{
+					{[]hostapd.Option{hostapd.Mode(hostapd.Mode80211g), hostapd.Channel(1)}},
+					{[]hostapd.Option{hostapd.Mode(hostapd.Mode80211g), hostapd.Channel(6)}},
+					{[]hostapd.Option{hostapd.Mode(hostapd.Mode80211g), hostapd.Channel(11)}},
+				},
+			},
+		},
+	})
+}
+
+func SimpleConnect(ctx context.Context, s *testing.State) {
+	router, _ := s.Var("router")
+	tf, err := wificell.NewTestFixture(ctx, s.DUT(), s.RPCHint(), router)
+	if err != nil {
+		s.Fatal("Failed to set up test fixture: ", err)
+	}
+	defer func() {
+		if err := tf.Close(ctx); err != nil {
+			s.Log("Failed to tear down test fixture, err: ", err)
+		}
+	}()
+
+	testOnce := func(ctx context.Context, s *testing.State, options []hostapd.Option) {
+		ap, err := tf.ConfigureAP(ctx, options...)
+		if err != nil {
+			s.Fatal("Failed to configure ap, err: ", err)
+		}
+		defer func() {
+			if err := tf.DeconfigAP(ctx, ap); err != nil {
+				s.Error("Failed to deconfig ap, err: ", err)
+			}
+		}()
+		s.Log("AP setup done")
+
+		if err := tf.ConnectWifi(ctx, ap); err != nil {
+			s.Fatal("Failed to connect to WiFi, err: ", err)
+		}
+		defer func() {
+			if err := tf.DisconnectWifi(ctx); err != nil {
+				s.Error("Failed to disconnect WiFi, err: ", err)
+			}
+			if _, err := tf.WifiClient().DeleteEntriesForSSID(ctx, &network.SSID{Ssid: ap.Config().Ssid}); err != nil {
+				s.Errorf("Failed to remove entries for ssid=%s, err: %v", ap.Config().Ssid, err)
+			}
+		}()
+		s.Log("Connected")
+
+		ping := func(ctx context.Context) error {
+			return tf.PingFromDUT(ctx)
+		}
+
+		if err := tf.AssertNoDisconnect(ctx, ping); err != nil {
+			s.Fatal("Failed to ping from DUT, err: ", err)
+		}
+		// TODO(crbug.com/1034875): Assert no deauth detected from the server side.
+		// TODO(crbug.com/1034875): Maybe some more check on the WiFi capabilities to
+		// verify we really have the settings as expected. (ref: crrev.com/c/1995105)
+		s.Log("Deconfiguring")
+	}
+
+	testcases := s.Param().([]simpleConnectTestcase)
+	for i, tc := range testcases {
+		subtest := func(ctx context.Context, s *testing.State) {
+			testOnce(ctx, s, tc.apOptions)
+		}
+		if !s.Run(ctx, fmt.Sprintf("Testcase #%d", i), subtest) {
+			// Stop if any sub-test failed.
+			return
+		}
+	}
+	s.Log("Tearing down")
+}
