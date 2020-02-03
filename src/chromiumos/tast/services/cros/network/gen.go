f5b10e539620972a00ce23b1576b4aaa6b81e914
Yen-lin Lai <yenlinlai@google.com>
2020-02-03 13:27:33 +0800

    service: network: Add a gRPC service for basic WiFi operations.
    Add a gRPC service, tast.cros.network.Wifi, for connecting or
    disconnecting WiFi. This is used for the first remote Tast migration of
    network_WiFi_SimpleConnect. Currently, the service provides interface
    to connect to an open WiFi with SSID and disconnect. It will extend the
    interface when needed. (e.g. hidden, security setting)
    
    gRPC service is defined in services/cros/network/wifi.proto. And
    services/cros/network/gen.go is a go generate directive to generate
    wifi.pb.go file. The service implementation in DUT:
    local/bundles/cros/network/wifi_service.go
    
    BUG=chromium:1034875
    TEST=None
    
    Change-Id: Icc534ae8b5532a3f222c5c6d73053c2c70d54a21

---
+++ src/chromiumos/tast/services/cros/network/gen.go
@@ -0,0 +1,11 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. wifi.proto
+
+package network
+
+// Run the following command in CrOS chroot to regenerate protocol buffer bindings:
+//
+// ~/trunk/src/platform/tast/tools/go.sh generate chromiumos/tast/services/cros/network
