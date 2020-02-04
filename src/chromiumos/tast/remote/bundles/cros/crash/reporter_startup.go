8dcc2ed50c86e102d27cafe82909e83e97128ca4
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-02-04 14:37:40 +0900

    tast-tests: Verify crash_reporter setup after init.
    This is a replacement of the first part of logging_UserCrash autotest.
    reporter_startup in logggin_UserCrash is invoked without re-initializing
    crash_reporter to catch problems with the default crash reporting setup.
    https://chromium.googlesource.com/chromiumos/third_party/autotest/+/0.12.433.B/client/site_tests/logging_UserCrash/logging_UserCrash.py#676
    
    Some test setup such as stashing crash files and setting the
    test-in-progress flag file is omitted, because those will not affect
    this specific test checkpoints.
    
    BUG=chromium:1047059
    TEST=tast run $DUT crash.ReporterStartup
    
    Change-Id: If7356a862115188de28076fd708d88fc0231e186

---
+++ src/chromiumos/tast/remote/bundles/cros/crash/reporter_startup.go
@@ -0,0 +1,100 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crash
+
+import (
+	"context"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	commoncrash "chromiumos/tast/common/crash"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/baserpc"
+	crashservice "chromiumos/tast/services/cros/crash"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ReporterStartup,
+		Desc: "Verifies crash reporter after reboot",
+		Contacts: []string{
+			"domlaskowski@chromium.org", // Original autotest author
+			"yamaguchi@chromium.org",    // Tast port author
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "metrics_consent"},
+		ServiceDeps: []string{
+			"tast.cros.crash.FixtureService",
+			"tast.cros.baserpc.FileSystem",
+		},
+	})
+}
+
+// ReporterStartup tests crash reporter is set up correctly after reboot.
+// Equivlaent to the local test in testReporterStartup, but without
+// re-initializing to catch problems with the default crash reporting setup.
+// See src/chromiumos/tast/local/bundles/cros/platform/user_crash.go
+func ReporterStartup(ctx context.Context, s *testing.State) {
+	d := s.DUT()
+	s.Log("Rebooting DUT")
+	if err := d.Reboot(ctx); err != nil {
+		s.Fatal("Failed to reboot DUT: ", err)
+	}
+
+	cl, err := rpc.Dial(ctx, d, s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	// Turn off crash filtering so we see the original setting.
+	fixture := crashservice.NewFixtureServiceClient(cl.Conn)
+	if _, err := fixture.DisableCrashFilter(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Failed to turn off crash filtering: ", err)
+	}
+
+	// Check that core_pattern is set up by crash reporter.
+	fileSystem := baserpc.NewFileSystemClient(cl.Conn)
+	data, err := fileSystem.ReadFile(ctx, &baserpc.ReadFileRequest{Name: commoncrash.CorePattern})
+	if err != nil {
+		s.Fatal("Failed to read core pattern file: ", commoncrash.CorePattern)
+	}
+	trimmed := strings.TrimSuffix(string(data.Content), "\n")
+	expectedCorePattern := commoncrash.ExpectedCorePattern()
+	if trimmed != expectedCorePattern {
+		s.Errorf("Unexpected core_pattern: got %s, want %s", trimmed, expectedCorePattern)
+	}
+
+	// Check that we wrote out the file indicating that crash_reporter is
+	// enabled AFTER the system was booted. This replaces the old technique
+	// of looking for the log message which was flaky when the logs got
+	// flooded.
+	// NOTE: This technique doesn't need to be highly accurate, we are only
+	// verifying that the flag was written after boot and there are multiple
+	// seconds between those steps, and a file from a prior boot will almost
+	// always have been written out much further back in time than our
+	// current boot time.
+	flagInfo, err := fileSystem.Stat(ctx, &baserpc.StatRequest{Name: commoncrash.CrashReporterEnabledPath})
+	if err != nil {
+		s.Error("Failed to open crash reporter enabled file flag: ", err)
+	} else if !os.FileMode(flagInfo.Mode).IsRegular() {
+		s.Error("Crash reporter enabled file flag is not a regular file: ", commoncrash.CrashReporterEnabledPath)
+	}
+
+	uptimeInfo, err := fileSystem.Stat(ctx, &baserpc.StatRequest{Name: "/proc/uptime"})
+	if err != nil {
+		s.Fatal("Failed to read uptime file: ", err)
+	}
+
+	flagTime := time.Unix(flagInfo.Modified.Seconds, int64(flagInfo.Modified.Nanos))
+	bootTime := time.Unix(uptimeInfo.Modified.Seconds, int64(uptimeInfo.Modified.Nanos))
+	if flagTime.Before(bootTime) {
+		s.Error("User space crash handling was not started during last boot")
+	}
+}
