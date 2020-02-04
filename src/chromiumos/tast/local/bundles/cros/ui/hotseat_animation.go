8822513813c63e1505a03ee162c716616db0b1d8
Alex Newcomer <newcomer@chromium.org>
2020-02-04 15:53:50 -0800

    add performance tests for the hotseat
    Add a test which drags the hotseat, and another test
    which records all hotseat animations.
    
    Hotseat animations should be recorded in all major transitions
    to/from:
     - in-app shelf
     - Home Launcher
     - Overview
    
    BUG=chromium:1044289
    TEST=manual
    
    Change-Id: Ie6bff2b2f4f1e8fe1641d77bc1d7d6f2f967d3eb

---
+++ src/chromiumos/tast/local/bundles/cros/ui/hotseat_animation.go
@@ -0,0 +1,210 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/local/ui"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HotseatAnimation,
+		Desc:         "Measures the framerate of the hotseat animation in tablet mode",
+		Contacts:     []string{"newcomer@chromium.org", "manucornet@chromium.org", "cros-shelf-prod-notifications@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", "tablet_mode"},
+		Pre:          chrome.LoggedIn(),
+		Timeout:      8 * time.Minute,
+	})
+}
+
+func HotseatAnimation(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	orientation, err := display.GetOrientation(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to obtain the display rotation: ", err)
+	}
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, true)
+	if err != nil {
+		s.Fatal("Failed to ensure in tablet mode: ", err)
+	}
+	defer cleanup(ctx)
+
+	// Prepare the touch screen as this test requires touch scroll events.
+	tsw, err := input.Touchscreen(ctx)
+	if err != nil {
+		s.Fatal("Failed to create touch screen event writer: ", err)
+	}
+	if err = tsw.SetRotation(-orientation.Angle); err != nil {
+		s.Fatal("Failed to set rotation: ", err)
+	}
+
+	stw, err := tsw.NewSingleTouchWriter()
+	if err != nil {
+		s.Fatal("Failed to create single touch writer: ", err)
+	}
+	defer stw.Close()
+
+	histograms, err := metrics.Run(ctx, cr, func() error {
+
+		// Open a window to hide the launcher and animate the hotseat to Hidden.
+		const numWindows = 1
+		conns, err := ash.CreateWindows(ctx, cr, ui.PerftestURL, numWindows)
+		if err != nil {
+			return errors.Wrap(err, "failed to open browser windows: ")
+		}
+		defer conns.Close()
+
+		// Wait for the animations to complete.
+		if err := testing.Sleep(ctx, time.Second*2); err != nil {
+			return errors.Wrap(err, "failed to sleep")
+		}
+
+		startX := tsw.Width() / 2
+		startY := tsw.Height() - 1
+
+		endX := startX
+		endY := tsw.Height() / 2
+
+		s.Log("Dragging from the bottom slowly to open overview")
+		if err := stw.Swipe(ctx, startX, startY, endX, endY, 500*time.Millisecond); err != nil {
+			return errors.Wrap(err, "failed to swipe")
+		}
+		// Wait for overview to trigger.
+		const overviewWaitDuration = time.Second
+		if err := testing.Sleep(ctx, overviewWaitDuration); err != nil {
+			return errors.Wrap(err, "failed to sleep while waiting for overview to trigger")
+		}
+
+		if err := stw.End(); err != nil {
+			return errors.Wrap(err, "failed to finish the swipe gesture")
+		}
+
+		// After overview has triggered, wait a bit for things to calm down.
+		if err := testing.Sleep(ctx, time.Second*2); err != nil {
+			return errors.Wrap(err, "failed to sleep")
+		}
+
+		s.Log("Tapping an empty space in overview to open the launcher")
+		pressX := tsw.Width() * 5 / 6
+		pressY := tsw.Height() / 2
+		if err := stw.Swipe(ctx, pressX, pressY, pressX+5, pressY-5, 200*time.Millisecond); err != nil {
+			return errors.Wrap(err, "failed to tap")
+		}
+		if err := stw.End(); err != nil {
+			return errors.Wrap(err, "failed to finish the tap gesture")
+		}
+
+		// After opening the launcher, wait for things to calm down.
+		if err := testing.Sleep(ctx, time.Second*2); err != nil {
+			return errors.Wrap(err, "failed to sleep")
+		}
+
+		// Open overview to animate the hotseat to Extended.
+		startX = tsw.Width() / 2
+		startY = tsw.Height() - 1
+
+		endX = startX
+		endY = tsw.Height() / 2
+		s.Log("Dragging up slowly to go to overview")
+		if err := stw.Swipe(ctx, startX, startY, endX, endY, 500*time.Millisecond); err != nil {
+			return errors.Wrap(err, "failed to swipe")
+		}
+
+		// Wait for overview to trigger.
+		if err := testing.Sleep(ctx, overviewWaitDuration); err != nil {
+			return errors.Wrap(err, "failed to sleep while waiting for overview to trigger")
+		}
+
+		if err := stw.End(); err != nil {
+			return errors.Wrap(err, "failed to finish the swipe gesture")
+		}
+
+		// After overview has triggered, wait a bit for things to calm down.
+		if err := testing.Sleep(ctx, time.Second*2); err != nil {
+			return errors.Wrap(err, "failed to sleep")
+		}
+
+		s.Log("Tap the item to go back to in-app")
+		pressX = tsw.Width() / 3
+		pressY = tsw.Height() / 3
+
+		if err := stw.Swipe(ctx, pressX, pressY, pressX+5, pressY-5, 200*time.Millisecond); err != nil {
+			return errors.Wrap(err, "failed to tap")
+		}
+		if err := stw.End(); err != nil {
+			return errors.Wrap(err, "failed to finish the tap gesture")
+		}
+
+		// After going to in-app, wait a bit to calm down.
+		if err := testing.Sleep(ctx, time.Second*2); err != nil {
+			return errors.Wrap(err, "failed to sleep")
+		}
+
+		s.Log("Go to home launcher by swiping up")
+		startX = tsw.Width() / 2
+		startY = tsw.Height() - 1
+
+		endX = startX
+		endY = tsw.Height() / 2
+
+		if err := stw.Swipe(ctx, startX, startY, endX, endY, 200*time.Millisecond); err != nil {
+			return errors.Wrap(err, "failed to swipe")
+		}
+
+		if err := stw.End(); err != nil {
+			return errors.Wrap(err, "failed to finish the swipe gesture")
+		}
+
+		// After going to home launcher, wait for things to calm down and the
+		// animation to complete.
+		if err := testing.Sleep(ctx, time.Second*2); err != nil {
+			return errors.Wrap(err, "failed to sleep")
+		}
+		return nil
+	},
+		"Ash.HotseatTransition.AnimationSmoothness.TransitionToHiddenHotseat",
+		"Ash.HotseatTransition.AnimationSmoothness.TransitionToShownHotseat",
+		"Ash.HotseatTransition.AnimationSmoothness.TransitionToExtendedHotseat")
+	if err != nil {
+		s.Fatal("Failed to swipe or get histogram: ", err)
+	}
+
+	pv := perf.NewValues()
+	for _, h := range histograms {
+		mean, err := h.Mean()
+		if err != nil {
+			s.Fatalf("Failed to get mean for histogram %s: %v", h.Name, err)
+		}
+
+		pv.Set(perf.Metric{
+			Name:      h.Name,
+			Unit:      "percent",
+			Direction: perf.BiggerIsBetter,
+		}, mean)
+	}
+
+	if err := pv.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
