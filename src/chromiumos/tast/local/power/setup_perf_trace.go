b81987a4c765e5f6fa3fd27b1ed87e1662941ea5
Charles William Dick <cwd@google.com>
2020-02-04 10:17:53 +0900

    tast linux perf
    Collect Linux perf traces in a setup action. PowerIdlePerf uses a
    variable to enable tracing a specify extra arguments to perf.
    
    TEST=tast run -var=perf="-e cycles -a" $DUT arc.PowerIdlePerf.arcvm
    BUG=b:148821392
    
    Change-Id: Id415c0b74dc6b064dd0fee6f20db2f5e081f4788

---
+++ src/chromiumos/tast/local/power/setup_perf_trace.go
@@ -0,0 +1,58 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"syscall"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+// PerfTrace starts a performance trace and returns a callback that stop the
+// trace and dumps the results.
+func PerfTrace(ctx context.Context, dir string, extraArgs []string, chain CleanupChain) (CleanupChain, error) {
+	setupFailed, guard := SetupFailureGuard(chain)
+	defer guard(ctx)
+
+	const perfDataFileName = "perf.data"
+	perfDataFile := filepath.Join(dir, perfDataFileName)
+	args := append([]string{"record", "-o", perfDataFile}, extraArgs...)
+	perf := testexec.CommandContext(ctx, "perf", args...)
+	if err := perf.Start(); err != nil {
+		return nil, errors.Wrap(err, "failed to start perf trace")
+	}
+	testing.ContextLog(ctx, "Starting Linux perf trace")
+
+	return SetupSucceeded(setupFailed, chain, func(ctx context.Context) error {
+		// Send ctrl-c to stop the perf trace.
+		if err := perf.Signal(syscall.SIGINT); err != nil {
+			return errors.Wrap(err, "failed to interrupt perf trace")
+		}
+		const sigintError = "signal: interrupt"
+		if err := perf.Wait(testexec.DumpLogOnError); err != nil && err.Error() != sigintError {
+			return errors.Wrap(err, "failed to wait for perf to stop")
+		}
+
+		// Get symbols for the perf.data file by calling 'perf script'
+		const scriptDataFileName = "perf.data.dump"
+		scriptDataFile := filepath.Join(dir, scriptDataFileName)
+		dumpFile, err := os.Create(scriptDataFile)
+		if err != nil {
+			return errors.Wrap(err, "failed to create perf script dump file")
+		}
+		script := testexec.CommandContext(ctx, "perf", "script", "-i", perfDataFile)
+		script.Stdout = dumpFile
+		if err := script.Run(testexec.DumpLogOnError); err != nil {
+			return errors.Wrap(err, "failed run perf script")
+		}
+		testing.ContextLog(ctx, "Stopped Linux perf trace")
+		return nil
+	})
+}
