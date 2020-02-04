cb940802c4877b60ed1670df23d332d1fe071957
Charles William Dick <cwd@google.com>
2020-02-04 15:40:55 +0900

    WIP: tast setup android app
    Setup actions that can install and launch an android app.
    
    TEST=tbd
    BUG=b:146393576
    
    Change-Id: I40c6b072a9c74003f78f1f0c38199ec93f36d53a

---
+++ src/chromiumos/tast/local/power/setup/setup_app.go
@@ -0,0 +1,50 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package setup
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/testing"
+)
+
+// InstallApp installs an Android APK.
+func InstallApp(ctx context.Context, a *arc.ARC, apkDataPath string, pkg string) Result {
+	if err := a.Install(ctx, apkDataPath); err != nil {
+		return ResultFailed(errors.Wrapf(err, "failed to install apk %q", apkDataPath))
+	}
+	testing.ContextLogf(ctx, "Installed Android app %q", pkg)
+
+	return ResultSucceeded(func(ctx context.Context) error {
+		if err := a.Uninstall(ctx, pkg); err != nil {
+			return errors.Wrapf(err, "failed to uninstall package %q", pkg)
+		}
+		testing.ContextLogf(ctx, "Uninstalled Android app %q", pkg)
+		return nil
+	})
+}
+
+// StartActivity starts an Android activity.
+func StartActivity(ctx context.Context, a *arc.ARC, pkg string, activityName string) Result {
+	activity, err := arc.NewActivity(a, pkg, activityName)
+	if err != nil {
+		return ResultFailed(errors.Wrapf(err, "failed to create activity %q in package %q", activityName, pkg))
+	}
+	if err := activity.Start(ctx); err != nil {
+		return ResultFailed(errors.Wrapf(err, "failed to start activity %q in package %q", activityName, pkg))
+	}
+	testing.ContextLogf(ctx, "Started activity %s/%s", pkg, activityName)
+
+	return ResultSucceeded(func(ctx context.Context) error {
+		defer activity.Close()
+		if err := activity.Stop(ctx); err != nil {
+			errors.Wrapf(err, "failed to stop activity in package %q", pkg)
+		}
+		testing.ContextLogf(ctx, "Stopped activities in package %s", pkg)
+		return nil
+	})
+}
