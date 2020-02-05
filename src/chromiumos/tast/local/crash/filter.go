e8eb40a27b084e90a7dc651f92828ccc20297c58
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-02-05 21:52:17 +0900

    Add SetCrashFilter to fixture_serivce rpc.
    Some helper functions in bundles/cros/platform are moved to local/crash,
    because the service is implemented in bundles/cros/crash.
    
    BUG=chromium:1048530
    TEST=../platform/tast/fast_build.sh -T
    
    Change-Id: I62ec67372ae2f1997486a91374aeb31493f7b4c4

--- src/chromiumos/tast/local/crash/filter.go
+++ src/chromiumos/tast/local/crash/filter.go
@@ -1,13 +1,11 @@
-// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package crash contains functionality shared by tests that exercise the crash reporter.
 package crash
 
 import (
 	"io/ioutil"
-	"strconv"
 	"strings"
 
 	"chromiumos/tast/errors"
@@ -19,7 +17,6 @@ const (
 )
 
 // replaceArgs finds commandline arguments that match by prefix and replaces it with newarg.
-// TODO(yamaguchi): Deduplicate with the one in chromiumos/tast/local/crash/filter.go
 func replaceArgs(orig string, prefix string, newarg string) string {
 	e := strings.Fields(strings.TrimSpace(orig))
 	var newargs []string
@@ -42,24 +39,40 @@ func replaceArgs(orig string, prefix string, newarg string) string {
 	return strings.Join(newargs, " ")
 }
 
-// ReporterVerboseLevel sets the output log verbose level of the crash_reporter.
-// When level is set to 0, it will clear the -v=* flag because 0 is the default value.
-func ReporterVerboseLevel(level int) error {
-	if level < 0 {
-		return errors.Errorf("verbose level must be 0 or larger, got %d", level)
-	}
+// ReplaceCrashFilterIn replaces --filter_in= flag value of the crash reporter.
+// When param is an empty string, the flag will be removed.
+// The kernel is set up to call the crash reporter with the core dump as stdin
+// when a process dies. This function adds a filter to the command line used to
+// call the crash reporter. This is used to ignore crashes in which we have no
+// interest.
+func ReplaceCrashFilterIn(param string) error {
 	b, err := ioutil.ReadFile(CorePattern)
 	if err != nil {
 		return errors.Wrapf(err, "failed reading core pattern file %s", CorePattern)
 	}
 	pattern := string(b)
-	newarg := ""
-	if level > 0 {
-		newarg = "-v=" + strconv.Itoa(level)
+	if !strings.HasPrefix(pattern, "|") {
+		return errors.Wrapf(err, "pattern should start with '|', but was: %s", pattern)
 	}
-	pattern = replaceArgs(pattern, "-v=", newarg)
+	if len(param) == 0 {
+		pattern = replaceArgs(pattern, "--filter_in=", "")
+	} else {
+		pattern = replaceArgs(pattern, "--filter_in=", "--filter_in="+param)
+	}
 	if err := ioutil.WriteFile(CorePattern, []byte(pattern), 0644); err != nil {
 		return errors.Wrapf(err, "failed writing core pattern file %s", CorePattern)
 	}
 	return nil
+}
+
+// EnableCrashFiltering enables crash filtering with the specified process.
+func EnableCrashFiltering(s string) error {
+	return ReplaceCrashFilterIn(s)
+}
+
+// DisableCrashFiltering removes the --filter_in argument from the kernel core dump cmdline.
+// Next time the crash reporter is invoked (due to a crash) it will not receive a
+// --filter_in parameter.
+func DisableCrashFiltering() error {
+	return ReplaceCrashFilterIn("")
 }
