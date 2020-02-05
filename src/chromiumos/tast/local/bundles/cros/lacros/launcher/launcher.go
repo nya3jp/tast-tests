6e1488f17b57b97f8f2125ff37af169863672a0e
Erik Chen <erikchen@chromium.org>
2020-02-05 12:53:12 -0800

    Update launcher to support launching chrome multiple times.
    TEST=tast run lacros.Sanity
    BUG=chromium:1046885
    Change-Id: I1463db130b3340489cc8a00eba6ad50d239c5b6d

--- src/chromiumos/tast/local/bundles/cros/lacros/launcher/launcher.go
+++ src/chromiumos/tast/local/bundles/cros/lacros/launcher/launcher.go
@@ -6,7 +6,9 @@
 package launcher
 
 import (
+	"bytes"
 	"context"
+	"io/ioutil"
 	"os"
 	"strings"
 
@@ -15,6 +17,9 @@ import (
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
+
+// BinaryPath is the root directory for linux-chrome related binaries.
+const BinaryPath = LacrosTestPath + "/lacros_binary"
 
 // linuxChrome contains all state associated with a linux-chrome instance
 // that has been launched. Must call Close() to release resources.
@@ -39,18 +44,54 @@ func (l *linuxChrome) Close(ctx context.Context) error {
 	return nil
 }
 
+// PidsFromPath returns the pids of all processes with a given path in their
+// command line. This is typically used to find all chrome-related binaries,
+// e.g. chrome, nacl_helper, etc. They typically share paths, even though their
+// binary names differ. The output is a new-line separated list of pids. It's
+// returned as a bytes.Buffer to facilitate piping as input to another bash
+// command.
+func PidsFromPath(ctx context.Context, path string) bytes.Buffer {
+	psCmd := testexec.CommandContext(ctx, "ps", "aux")
+	grepCmd := testexec.CommandContext(ctx, "grep", path)
+	awkCmd := testexec.CommandContext(ctx, "awk", "{print $2}")
+
+	grepCmd.Cmd.Stdin, _ = psCmd.Cmd.StdoutPipe()
+	awkCmd.Cmd.Stdin, _ = grepCmd.Cmd.StdoutPipe()
+
+	var b bytes.Buffer
+	awkCmd.Stdout = &b
+
+	psCmd.Start()
+	grepCmd.Start()
+	awkCmd.Start()
+
+	psCmd.Wait()
+	grepCmd.Wait()
+	awkCmd.Wait()
+
+	return b
+}
+
 // LaunchLinuxChrome launches a fresh instance of linux-chrome.
 func LaunchLinuxChrome(ctx context.Context, p PreData) (*linuxChrome, error) {
-	const (
-		binaryPath = LacrosTestPath + "/lacros_binary"
+	// Kills all instances of linux-chrome and other related executables.
+	// Ignore errors, since they can occur for several expected reasons.
+	// e.g. the process to kill has died as a response to another process
+	// being killed.
+	b := PidsFromPath(ctx, BinaryPath)
+	killCmd := testexec.CommandContext(ctx, "xargs", "kill", "-9")
+	killCmd.Cmd.Stdin = &b
+	killCmd.Run()
 
-		// TODO: How do we kill a previously running instance of
-		// linux-chrome if we don't know its PID?
-		// Use a fixed user data dir, which makes debugging easier. We
-		// may wish to switch to a temp dir in the future to avoid disk
-		// contamination in the future.
-		userDataDir = binaryPath + "/user_data"
-	)
+	// Create a new temporary directory for user data dir. We don't bother
+	// clearing it on shutdown, since it's a subdirectory of the binary
+	// path, which is cleared by pre.go. We need to use a new temporary
+	// directory for each invocation so that successive calls to
+	// LaunchLinuxChrome don't interfere with each other.
+	userDataDir, err := ioutil.TempDir(BinaryPath, "")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create temp dir")
+	}
 
 	args := []string{
 		"--ozone-platform=wayland",                                  // Use wayland to connect to exo wayland server.
@@ -62,19 +103,19 @@ func LaunchLinuxChrome(ctx context.Context, p PreData) (*linuxChrome, error) {
 		"--no-first-run",                                            // Prevent showing up offer pages, e.g. google.com/chromebooks.
 		"--user-data-dir=" + userDataDir,                            // Specify a --user-data-dir, which holds on-disk state for Chrome.
 		"--long=en-US",                                              // Language
-		"--breakpad-dump-location=" + binaryPath,                    // Specify location for breakpad dump files.
+		"--breakpad-dump-location=" + BinaryPath,                    // Specify location for breakpad dump files.
 		"about:blank",                                               // Specify first tab to load.
 	}
 
 	l := &linuxChrome{}
-	l.cmd = testexec.CommandContext(ctx, binaryPath+"/chrome", args...)
-	l.cmd.Cmd.Env = append(os.Environ(), "XDG_RUNTIME_DIR=/run/chrome", "LD_LIBRARY_PATH="+binaryPath)
+	l.cmd = testexec.CommandContext(ctx, BinaryPath+"/chrome", args...)
+	l.cmd.Cmd.Env = append(os.Environ(), "XDG_RUNTIME_DIR=/run/chrome", "LD_LIBRARY_PATH="+BinaryPath)
+	testing.ContextLog(ctx, "Starting chrome: ", strings.Join(args, " "))
 	if err := l.cmd.Cmd.Start(); err != nil {
 		return nil, errors.Wrap(err, "failed to launch linux-chrome")
 	}
 
 	debuggingPortPath := userDataDir + "/DevToolsActivePort"
-	var err error
 	if l.Devsess, err = cdputil.NewSession(ctx, debuggingPortPath); err != nil {
 		l.Close(ctx)
 		return nil, errors.Wrap(err, "failed to connect to debugging port")
