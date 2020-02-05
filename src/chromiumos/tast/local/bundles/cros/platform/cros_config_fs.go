6257687714eff70562d0846370e016427d2c5a01
Jack Rosenthal <jrosenth@chromium.org>
2020-02-05 15:43:58 -0700

    Add platform.CrosConfigFS test
    Initial add of a test of the functionality of ChromeOS
    ConfigFS (go/configfs), beyond what is covered from the package unit
    tests.
    
    BUG=chromium:1041544
    TEST=tast run dut platform.CrosConfigFS, for both octopus
        dut (unibuild) and samus dut (non-unibuild)
    
    Change-Id: Ib48b9f4213e517098ce97c934c615d0b7922a17c

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_config_fs.go
@@ -0,0 +1,74 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     CrosConfigFS,
+		Desc:     "Check functionality of cros_config to mount ConfigFS",
+		Contacts: []string{"jrosenth@chromium.org"},
+		Attr:     []string{"group:mainline", "informational"},
+	})
+}
+
+func CrosConfigFS(ctx context.Context, s *testing.State) {
+	const configFSImage = "/usr/share/chromeos-config/configfs.img"
+
+	mountpoint, err := ioutil.TempDir("", "tast.platform.CrosConfigFS")
+	if err != nil {
+		s.Fatal("Failed to make temporary directory for mountpoint: ", err)
+	}
+	defer os.RemoveAll(mountpoint)
+
+	var args []string
+	if _, err := os.Stat(configFSImage); os.IsNotExist(err) {
+		// This is a non-unibuild board. Use the fallback mount.
+		args = []string{"--mount_fallback", mountpoint}
+	} else {
+		// This is a unibuild board. Mount the configfs image.
+		args = []string{"--mount", configFSImage, mountpoint}
+	}
+
+	if err := testexec.CommandContext(ctx, "cros_config", args...).Run(); err != nil {
+		s.Fatal("Failed to mount ChromeOS ConfigFS: ", err)
+	}
+
+	// The private directory is where the SquashFS image for
+	// ConfigFS got mounted, and the v1 directory is the
+	// corresponding bind mount for users.
+	privateDir := filepath.Join(mountpoint, "private")
+	v1Dir := filepath.Join(mountpoint, "v1")
+
+	for _, mount := range []string{privateDir, v1Dir} {
+		defer testexec.CommandContext(ctx, "umount", mount).Run()
+	}
+
+	// The purpose of this test is to make sure the mount
+	// succeeds. The unit tests in cros_config_host already check
+	// the filesystem contents are correct for unibuild, and the
+	// unit tests in libcros_config test probing works for
+	// unibuild, and the structure is correct for non-unibuild. So
+	// spot-checking that a few (required) files exist should be
+	// more than sufficient for this test.
+	for _, key := range []string{
+		"/name",
+		"/brand-code",
+		"/identity/platform-name",
+	} {
+		if _, err := os.Stat(filepath.Join(v1Dir, key)); err != nil {
+			s.Errorf("Cannot stat required key %s: %s", key, err)
+		}
+	}
+}
