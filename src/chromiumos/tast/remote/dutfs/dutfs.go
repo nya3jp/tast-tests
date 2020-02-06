eb1eb6b45351f7154ee3771dfe0e8cbf5b2efb08
Shuhei Takahashi <nya@chromium.org>
2020-02-06 18:11:03 +0900

    tast-tests: Define a common file system service.
    Many remote tests may want to do basic file system operations on DUT
    without bothering to define their own gRPC services. This new gRPC
    service provides a few basic file system operations.
    
    BUG=chromium:969627
    TEST=fast_build.sh -T
    
    Change-Id: I473e313fa09866e52ab59fa6c0cc764443db6a51

---
+++ src/chromiumos/tast/remote/dutfs/dutfs.go
@@ -0,0 +1,95 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package dutfs provides remote file system operations on DUT.
+package dutfs
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/baserpc"
+	"chromiumos/tast/testing"
+)
+
+// ServiceName is the name of the gRPC service this package uses to access remote
+// file system on DUT.
+const ServiceName = "tast.cros.baserpc.FileSystem"
+
+// Client provides remote file system operations on DUT.
+type Client struct {
+	cl *rpc.Client
+	fs baserpc.FileSystemClient
+}
+
+// Dial establishes a gRPC connection to the DUT for remote filesystem operations.
+// Tests calling this function must declare "tast.cros.baserpc.FileSystem" service
+// dependency. Close must be called after use to close the gRPC connection.
+func Dial(ctx context.Context, d *dut.DUT, h *testing.RPCHint) (*Client, error) {
+	cl, err := rpc.Dial(ctx, d, h, "cros")
+	if err != nil {
+		return nil, err
+	}
+
+	fs := baserpc.NewFileSystemClient(cl.Conn)
+	return &Client{cl, fs}, nil
+}
+
+// Close closes the underlying gRPC connection.
+func (c *Client) Close(ctx context.Context) error {
+	return c.cl.Close(ctx)
+}
+
+// ReadDir reads the directory named by dirname and returns a list of directory
+// entries sorted by filename.
+func (c *Client) ReadDir(ctx context.Context, dirname string) ([]os.FileInfo, error) {
+	res, err := c.fs.ReadDir(ctx, &baserpc.ReadDirRequest{Dir: dirname})
+	if err != nil {
+		return nil, err
+	}
+
+	var fis []os.FileInfo
+	for _, pb := range res.Files {
+		fis = append(fis, fileInfo{pb})
+	}
+	return fis, nil
+}
+
+// fileInfo wraps base.FileInfo to implement os.FileInfo interface.
+type fileInfo struct {
+	pb *baserpc.FileInfo
+}
+
+func (fi fileInfo) Name() string {
+	return fi.pb.Name
+}
+
+func (fi fileInfo) Size() int64 {
+	return int64(fi.pb.Size)
+}
+
+func (fi fileInfo) Mode() os.FileMode {
+	return os.FileMode(fi.pb.Mode)
+}
+
+func (fi fileInfo) ModTime() time.Time {
+	ts, err := ptypes.Timestamp(fi.pb.Modified)
+	if err != nil {
+		return time.Time{}
+	}
+	return ts
+}
+
+func (fi fileInfo) IsDir() bool {
+	return fi.Mode().IsDir()
+}
+
+func (fi fileInfo) Sys() interface{} {
+	return nil
+}
