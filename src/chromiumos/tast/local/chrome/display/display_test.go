60d832cfbf3001cffb14d598ae4f58e7925f84a4
Jun Mukai <mukai@google.com>
2020-02-10 14:21:58 -0800

    Add GetEffectiveDeviceScaleFactor to display package
    This is just a utility to return the current scale factor of a
    display.
    
    BUG=none
    TEST=fast_build.sh -T
    
    Change-Id: I2237c3f6e79f421fc77c769ea13a4b340ab5cede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2048206
    Tested-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>

---
+++ src/chromiumos/tast/local/chrome/display/display_test.go
@@ -0,0 +1,143 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package display
+
+import (
+	"testing"
+)
+
+func TestSelectedMode(t *testing.T) {
+	for _, c := range []struct {
+		name string
+		info *Info
+		// The index in 'Modes' to be returned, or -1 if error is expected.
+		expectedIndex int
+	}{
+		{
+			"multi",
+			&Info{
+				Modes: []*DisplayMode{
+					&DisplayMode{Width: 640, Height: 480, IsNative: true, IsSelected: false},
+					&DisplayMode{Width: 320, Height: 240, DeviceScaleFactor: 2.0, IsNative: false, IsSelected: true},
+				},
+			},
+			1,
+		},
+		{
+			"error-no-selected",
+			&Info{
+				Modes: []*DisplayMode{
+					&DisplayMode{Width: 640, Height: 480, IsNative: true, IsSelected: false},
+					&DisplayMode{Width: 320, Height: 240, DeviceScaleFactor: 2.0, IsNative: false, IsSelected: false},
+				},
+			},
+			-1,
+		},
+		{
+			"error-empty",
+			&Info{},
+			-1,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			gotMode, err := c.info.GetSelectedMode()
+			if c.expectedIndex < 0 {
+				if err == nil {
+					t.Errorf("Expected to fail, but successfully got %+v", gotMode)
+				}
+			} else {
+				if err != nil {
+					t.Error("Failed to get the selected info: ", err)
+				}
+				if gotMode != c.info.Modes[c.expectedIndex] {
+					t.Errorf("Unexpected mode is selected: got %+v, want %+v", gotMode, c.info.Modes[c.expectedIndex])
+				}
+			}
+		})
+	}
+}
+
+func TestEffectiveDeviceScaleFactor(t *testing.T) {
+	for _, c := range []struct {
+		name          string
+		info          *Info
+		shouldSucceed bool
+		expectedDSF   float64
+	}{
+		{
+			"normal",
+			&Info{
+				DisplayZoomFactor: 1.0,
+				Modes: []*DisplayMode{
+					&DisplayMode{DeviceScaleFactor: 1.0, IsSelected: true},
+				},
+			},
+			true, 1.0,
+		},
+		{
+			"display-zoomed",
+			&Info{
+				DisplayZoomFactor: 1.2,
+				Modes: []*DisplayMode{
+					&DisplayMode{DeviceScaleFactor: 1.0, IsSelected: true},
+				},
+			},
+			true, 1.2,
+		},
+		{
+			"hidpi",
+			&Info{
+				DisplayZoomFactor: 1.0,
+				Modes: []*DisplayMode{
+					&DisplayMode{DeviceScaleFactor: 2.0, IsSelected: true},
+				},
+			},
+			true, 2.0,
+		},
+		{
+			"hidpi-zoomed",
+			&Info{
+				DisplayZoomFactor: 1.2,
+				Modes: []*DisplayMode{
+					&DisplayMode{DeviceScaleFactor: 2.0, IsSelected: true},
+				},
+			},
+			true, 2.4,
+		},
+		{
+			"error-empty",
+			&Info{
+				DisplayZoomFactor: 1.0,
+			},
+			false, 1.0,
+		},
+		{
+			"error-not-selected",
+			&Info{
+				DisplayZoomFactor: 1.0,
+				Modes: []*DisplayMode{
+					&DisplayMode{DeviceScaleFactor: 2.0, IsSelected: false},
+				},
+			},
+			false, 1.0,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			gotDSF, err := c.info.GetEffectiveDeviceScaleFactor()
+			if c.shouldSucceed {
+				if err != nil {
+					t.Fatal("Failed to get DSF: ", err)
+				}
+				if gotDSF != c.expectedDSF {
+					t.Errorf("Incorrect DSF: got %f, want %f", gotDSF, c.expectedDSF)
+				}
+			} else {
+				if err == nil {
+					t.Errorf("Expected to fail, but successfully got DSF %f", gotDSF)
+				}
+			}
+		})
+	}
+}
