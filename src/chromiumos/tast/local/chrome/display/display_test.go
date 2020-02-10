f86b16e0fa890848ca7cfeb4fe11f1ca81b4d7ed
Jun Mukai <mukai@google.com>
2020-02-10 14:21:58 -0800

    Add GetEffectiveDeviceScaleFactor to display package
    This is just a utility to return the current scale factor of a
    display.
    
    BUG=none
    TEST=fast_build.sh -T
    
    Change-Id: I2237c3f6e79f421fc77c769ea13a4b340ab5cede

---
+++ src/chromiumos/tast/local/chrome/display/display_test.go
@@ -0,0 +1,153 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package display
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSelectedMode(t *testing.T) {
+	for _, c := range []struct {
+		name     string
+		info     *Info
+		expected *DisplayMode
+	}{
+		{
+			"multi",
+			&Info{
+				Modes: []*DisplayMode{
+					&DisplayMode{Width: 640, Height: 480, IsNative: true, IsSelected: false},
+					&DisplayMode{Width: 320, Height: 240, DeviceScaleFactor: 2.0, IsNative: false, IsSelected: true},
+				},
+			},
+			&DisplayMode{Width: 320, Height: 240, DeviceScaleFactor: 2.0, IsNative: false, IsSelected: true},
+		},
+		{
+			"error-no-selected",
+			&Info{
+				Modes: []*DisplayMode{
+					&DisplayMode{Width: 640, Height: 480, IsNative: true, IsSelected: false},
+					&DisplayMode{Width: 320, Height: 240, DeviceScaleFactor: 2.0, IsNative: false, IsSelected: false},
+				},
+			},
+			nil,
+		},
+		{
+			"error-empty",
+			&Info{},
+			nil,
+		},
+		{
+			"error-multiple-selected",
+			&Info{
+				Modes: []*DisplayMode{
+					&DisplayMode{Width: 640, Height: 480, IsNative: true, IsSelected: true},
+					&DisplayMode{Width: 320, Height: 240, DeviceScaleFactor: 2.0, IsNative: false, IsSelected: true},
+				},
+			},
+			&DisplayMode{Width: 640, Height: 480, IsNative: true, IsSelected: true},
+		},
+		{
+			"error-multiple-selected2",
+			&Info{
+				Modes: []*DisplayMode{
+					&DisplayMode{Width: 320, Height: 240, DeviceScaleFactor: 2.0, IsNative: false, IsSelected: true},
+					&DisplayMode{Width: 640, Height: 480, IsNative: true, IsSelected: true},
+				},
+			},
+			&DisplayMode{Width: 320, Height: 240, DeviceScaleFactor: 2.0, IsNative: false, IsSelected: true},
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			mode, err := c.info.GetSelectedMode()
+			if c.expected == nil {
+				if err == nil {
+					t.Errorf("Expected to fail, but successfully got %+v", mode)
+				}
+			} else {
+				if err != nil {
+					t.Error("Failed to get the selected info: ", err)
+				}
+				if !reflect.DeepEqual(c.expected, mode) {
+					t.Errorf("Unexpected mode is selected: want %+v, got %+v", c.expected, mode)
+				}
+			}
+		})
+	}
+}
+
+func TestEffectiveDeviceScaleFactor(t *testing.T) {
+	for _, c := range []struct {
+		name          string
+		info          *Info
+		shouldSuccess bool
+		expectedDSF   float64
+	}{
+		{
+			"normal",
+			&Info{
+				DisplayZoomFactor: 1.0,
+				Modes: []*DisplayMode{
+					&DisplayMode{DeviceScaleFactor: 1.0, IsSelected: true},
+				},
+			},
+			true, 1.0,
+		},
+		{
+			"display-zoomed",
+			&Info{
+				DisplayZoomFactor: 1.2,
+				Modes: []*DisplayMode{
+					&DisplayMode{DeviceScaleFactor: 1.0, IsSelected: true},
+				},
+			},
+			true, 1.2,
+		},
+		{
+			"hidpi",
+			&Info{
+				DisplayZoomFactor: 1.0,
+				Modes: []*DisplayMode{
+					&DisplayMode{DeviceScaleFactor: 2.0, IsSelected: true},
+				},
+			},
+			true, 2.0,
+		},
+		{
+			"hidpi-zoomed",
+			&Info{
+				DisplayZoomFactor: 1.2,
+				Modes: []*DisplayMode{
+					&DisplayMode{DeviceScaleFactor: 2.0, IsSelected: true},
+				},
+			},
+			true, 2.4,
+		},
+		{
+			"error",
+			&Info{
+				DisplayZoomFactor: 1.0,
+			},
+			false, 1.0,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			dsf, err := c.info.GetEffectiveDeviceScaleFactor()
+			if c.shouldSuccess {
+				if err != nil {
+					t.Fatal("Failed to get DSF: ", err)
+				}
+				if c.expectedDSF != dsf {
+					t.Errorf("Incorrect DSF: want %f got %f", c.expectedDSF, dsf)
+				}
+			} else {
+				if err == nil {
+					t.Errorf("Expected to fail, but successfully got DSF %f", dsf)
+				}
+			}
+		})
+	}
+}
