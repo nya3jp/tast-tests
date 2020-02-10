35bfc13136e0ef68d0abe79b0697e0a126120469
Miguel Casas <mcasas@chromium.org>
2020-02-10 14:35:52 -0500

    tast/video: add new Play variants with Guest Login
    This CL adds new video.Play.{av1,h264,vp8,vp9}_guest variants
    to run the homonymous Play test cases with a Guest Login (this
    has broken in the past e.g. https://crbug.com/1049585#c4). For
    this, it adds a new  precondition ChromeVideoWithGuestLogin.
    
    BUG=chromium:1050248
    TEST=tast run ${IP} video.Play.*guest on kohaku and atlas
    
    Change-Id: I818fceb0f89c3f08ca9d29ad374c9c06c22e83f7

--- src/chromiumos/tast/local/media/pre/pre.go
+++ src/chromiumos/tast/local/media/pre/pre.go
@@ -25,6 +25,14 @@ func ChromeVideo() testing.Precondition { return chromeVideoPre }
 var chromeVideoPre = chrome.NewPrecondition("video", chromeVModuleArgs,
 	chrome.ExtraArgs("--disable-features=ChromeosVideoDecoder"))
 
+// ChromeVideoWithGuestLogin returns a precondition equal to ChromeVideo but
+// forcing login as a guest, which is known to be different from a "normal"
+// user in, at least, the flag set used.
+func ChromeVideoWithGuestLogin() testing.Precondition { return chromeVideoWithGuestLoginPre }
+
+var chromeVideoWithGuestLoginPre = chrome.NewPrecondition("videoWithGuestLogin", chromeVModuleArgs,
+	chrome.ExtraArgs("--disable-features=ChromeosVideoDecoder"), chrome.GuestLogin())
+
 // ChromeVideoWithFakeWebcam returns precondition equal to ChromeVideo above,
 // supplementing it with the use of a fake video/audio capture device (a.k.a.
 // "fake webcam"), see https://webrtc.org/testing/.
