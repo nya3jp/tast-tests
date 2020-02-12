8acb922787428f345cce7ede9995dd7893c650c4
Arowa Suliman <arowa@google.com>
2020-02-11 22:09:51 -0800

    tast-test: Add ShillInitLoginGuestScript test
    The Tast test is migrated from network_ShillInitScripts.py, an Autotest.
    In the original version, it uses MockFlimflam instead of shill to run
    the login guest test. MockFlimflam provides a subset of shill DBus API
    so that we can track "dbus-send" invocations sent by the shill init
    scripts.  These tests are moved to run on real shill in the new Tast
    test, because it's possible to observe the expected behaviors without
    the mock and that allows for checking the status of the profile stack as
    part of these tests.
    
    BUG=chromium:1026901
    TEST=run tast network.ShillInitLoginGuestScript
    
    Change-Id: I2af2350f13db7f9237fa57671167dbcd1f9c45b8

---
+++ src/chromiumos/tast/local/bundles/cros/network/shill_init_login_guest_script.go
@@ -0,0 +1,109 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/network/shillscript"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ShillInitLoginGuestScript,
+		Desc:     "Test that shill init login guest script perform as expected",
+		Contacts: []string{"arowa@google.com", "cros-networking@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+	})
+}
+
+func ShillInitLoginGuestScript(ctx context.Context, s *testing.State) {
+	if err := shillscript.RunTest(ctx, testLoginGuest); err != nil {
+		s.Fatal("Failed running testLoginGuest: ", err)
+	}
+}
+
+// testLoginGuest tests the guest login process.
+// Login should create a temporary profile directory in /run,
+// instead of using one within the root directory for normal users.
+func testLoginGuest(ctx context.Context, env *shillscript.TestEnv) error {
+	// Simulate guest login.
+	// For guest login, session-manager passes an empty CHROMEOS_USER arg.
+	if err := upstart.StartJob(ctx, "shill"); err != nil {
+		return errors.Wrap(err, "failed starting shill")
+	}
+
+	timeoutCtx, cancel := context.WithTimeout(ctx, shillscript.DbusMonitorTimeout)
+	defer cancel()
+
+	stop, err := shillscript.DbusEventMonitor(timeoutCtx)
+	if err != nil {
+		return err
+	}
+
+	if err := shillscript.Login(ctx, ""); err != nil {
+		_, _ = stop()
+		return errors.Wrap(err, "failed logging in")
+	}
+
+	calledMethods, err := stop()
+	if err != nil {
+		return err
+	}
+
+	expectedCalls := []string{shillscript.CreateUserProfile, shillscript.InsertUserProfile}
+	if err := shillscript.AssureMethodCalls(ctx, expectedCalls, calledMethods); err != nil {
+		return err
+	}
+
+	if err := shillscript.AssureNotExists(env.ShillUserProfile); err != nil {
+		return errors.Wrapf(err, "shill user profile %s does exist", env.ShillUserProfile)
+	}
+
+	if err := shillscript.AssureNotExists(env.ShillUserProfileDir); err != nil {
+		return errors.Wrapf(err, "shill user profile directory %s does exist", env.ShillUserProfileDir)
+	}
+
+	if err := shillscript.AssureIsDir(shillscript.GuestShillUserProfileDir); err != nil {
+		return errors.Wrapf(err, "failed asserting that %v is a directory", shillscript.GuestShillUserProfileDir)
+	}
+
+	if err := shillscript.AssureIsDir(shillscript.ShillUserProfilesDir); err != nil {
+		return errors.Wrapf(err, "failed asserting that %s is a directory", shillscript.ShillUserProfilesDir)
+	}
+
+	if err := shillscript.AssureIsLinkTo("/run/shill/user_profiles/chronos", shillscript.GuestShillUserProfileDir); err != nil {
+		return err
+	}
+
+	if err := shillscript.AssureIsDir(shillscript.GuestShillUserLogDir); err != nil {
+		return errors.Wrapf(err, "failed asserting that %v is a directory", shillscript.GuestShillUserLogDir)
+	}
+
+	if err := shillscript.AssureIsLinkTo("/run/shill/log", shillscript.GuestShillUserLogDir); err != nil {
+		return err
+	}
+
+	profiles, err := shillscript.GetProfileList(ctx)
+	if err != nil {
+		return err
+	}
+
+	if len(profiles) == 0 {
+		return errors.Wrap(err, "profile list is empty")
+	}
+
+	// The last profile should be the one we just created.
+	profilePath := profiles[len(profiles)-1].String()
+
+	if profilePath != shillscript.ExpectedProfileName {
+		return errors.Wrapf(err, "found unexpected profile path: got %s, want %s", profilePath, shillscript.ExpectedProfileName)
+	}
+
+	return nil
+}
