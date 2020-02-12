13f63102a2f42458b04d2d94a9026871817999db
Arowa Suliman <arowa@google.com>
2020-02-11 22:14:59 -0800

    tast-test: Add ShillInitLogoutScript test
    The Tast test is migrated from network_ShillInitScripts.py, an Autotest.
    In the original version, it uses MockFlimflam instead of shill to run
    the logout test. MockFlimflam provides a subset of shill DBus API
    so that we can track "dbus-send" invocations sent by the shill init
    scripts.  These tests are moved to run on real shill in the new Tast
    test, because it's possible to observe the expected behaviors without
    the mock and that allows for checking the status of the profile stack as
    part of these tests.
    
    BUG=chromium:1026901
    TEST=run tast network.ShillInitLogoutScript
    
    Change-Id: I31f9031d3c021f465c647bc290f8f6c2e0379cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2050727
    Tested-by: Arowa Suliman <arowa@chromium.org>
    Commit-Queue: Arowa Suliman <arowa@chromium.org>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/network/shill_init_logout_script.go
@@ -0,0 +1,99 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"os"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/network/shillscript"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ShillInitLogoutScript,
+		Desc:     "Test that shill init logout script perform as expected",
+		Contacts: []string{"arowa@google.com", "cros-networking@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+	})
+}
+
+func ShillInitLogoutScript(ctx context.Context, s *testing.State) {
+	if err := shillscript.RunTest(ctx, testLogout); err != nil {
+		s.Fatal("Failed running testLogout: ", err)
+	}
+}
+
+// testLogout tests the logout process.
+func testLogout(ctx context.Context, env *shillscript.TestEnv) error {
+	if err := upstart.StartJob(ctx, "shill"); err != nil {
+		return errors.Wrap(err, "failed starting shill")
+	}
+
+	if err := os.MkdirAll(shillscript.ShillUserProfilesDir, 0777); err != nil {
+		return errors.Wrapf(err, "failed creating the directory: %s", shillscript.ShillUserProfilesDir)
+	}
+
+	if err := os.MkdirAll(shillscript.GuestShillUserProfileDir, 0777); err != nil {
+		return errors.Wrapf(err, "failed creating the directory: %s", shillscript.GuestShillUserProfileDir)
+	}
+
+	if err := os.MkdirAll(shillscript.GuestShillUserLogDir, 0777); err != nil {
+		return errors.Wrapf(err, "failed creating the directory: %s", shillscript.GuestShillUserLogDir)
+	}
+
+	if err := shillscript.Touch("/run/state/logged-in"); err != nil {
+		return err
+	}
+
+	timeoutCtx, cancel := context.WithTimeout(ctx, shillscript.DbusMonitorTimeout)
+	defer cancel()
+
+	stop, err := shillscript.DbusEventMonitor(timeoutCtx)
+	if err != nil {
+		return err
+	}
+
+	if err := shillscript.Logout(ctx); err != nil {
+		_, _ = stop()
+		return errors.Wrap(err, "failed logging out")
+	}
+
+	calledMethods, err := stop()
+	if err != nil {
+		return err
+	}
+
+	expectedCalls := []string{shillscript.PopAllUserProfiles}
+	if err := shillscript.AssureMethodCalls(ctx, expectedCalls, calledMethods); err != nil {
+		return err
+	}
+
+	if err := shillscript.AssureNotExists(shillscript.ShillUserProfilesDir); err != nil {
+		return err
+	}
+
+	if err := shillscript.AssureNotExists(shillscript.GuestShillUserProfileDir); err != nil {
+		return err
+	}
+
+	if err := shillscript.AssureNotExists(shillscript.GuestShillUserLogDir); err != nil {
+		return err
+	}
+
+	profiles, err := shillscript.GetProfileList(ctx)
+	if err != nil {
+		return err
+	}
+
+	if len(profiles) > 1 {
+		return errors.Wrapf(err, "found unexpected number of profiles in the profile stack: got %d, want 1", len(profiles))
+	}
+
+	return nil
+}
