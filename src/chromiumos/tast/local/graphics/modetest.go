47b14e0c33bd54c773ca58573107d2a53a37a8ae
Po-Hsien Wang <pwang@chromium.org>
2020-02-12 11:57:24 -0800

    graphics: Remove unused code from modetest
    Some DUT in lab has display connected but no configuration setup for
    modetest. It causes graphics.Sanity to fail since we can't figure out
    the preferred configuration. As graphics.Sanity only needs to know if
    the target monitor is connected or not, remove the unused code from
    modetest.go.
    
    id	encoder	status		name		size (mm)	modes	encoders
    49	48	connected	eDP-1          	217x136		2	48
      modes:
    	name refresh (Hz) hdisp hss hse htot vdisp vss vse vtot)
      1280x800 60 1280 1328 1360 1443 800 803 808 840 72728 flags: ; type:
      1280x800 48 1280 1328 1360 1440 800 803 808 832 57500 flags: ; type:
    
    BUG=chromium:1039042
    TEST=tast run DUT graphics.Sanity [PASS]
    
    Change-Id: I6b3b220992e71d85423897a3b9dc41636232b8e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2063575
    Tested-by: Po-Hsien Wang <pwang@chromium.org>
    Auto-Submit: Po-Hsien Wang <pwang@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Ricardo Quesada <ricardoq@chromium.org>
    Commit-Queue: Ricardo Quesada <ricardoq@chromium.org>

--- src/chromiumos/tast/local/graphics/modetest.go
+++ src/chromiumos/tast/local/graphics/modetest.go
@@ -22,18 +22,11 @@ type bound struct {
 
 // connector identifies the attributes related to display connector.
 type connector struct {
-	cid       int    // connector id
-	ctype     string // connector type, e.g. 'eDP', 'HDMI-A', 'DP'
-	connected bool   // true if the connector is connected
-	size      bound  // current screen size
+	cid       int  // connector id
+	connected bool // true if the connector is connected
 }
 
-var (
-	modesetConnectorPattern = regexp.MustCompile(`^(\d+)\s+\d+\s+(connected|disconnected)\s+(\S+)\s+\d+x\d+\s+\d+\s+\d+`)
-
-	// Group names match the drmModeModeInfo struct
-	modesetModePattern = regexp.MustCompile(`\s+(?P<name>.+)\s+(?P<vrefresh>\d+)\s+(?P<hdisplay>\d+)\s+(?P<hsync_start>\d+)\s+(?P<hsync_end>\d+)\s+(?P<htotal>\d+)\s+(?P<vdisplay>\d+)\s+(?P<vsync_start>\d+)\s+(?P<vsync_end>\d+)\s+(?P<vtotal>\d+)\s+(?P<clock>\d+)\s+flags:.+type: preferred`)
-)
+var modesetConnectorPattern = regexp.MustCompile(`^(\d+)\s+\d+\s+(connected|disconnected)\s+(\S+)\s+\d+x\d+\s+\d+\s+\d+`)
 
 // modetestConnectors retrieves a list of connectors using modetest.
 func modetestConnectors(ctx context.Context) ([]*connector, error) {
@@ -50,49 +43,11 @@ func modetestConnectors(ctx context.Context) ([]*connector, error) {
 			if err != nil {
 				return nil, errors.Wrapf(err, "failed to parse cid %s", matches[1])
 			}
-			connected := false
-			if matches[2] == "connected" {
-				connected = true
-			}
-			ctype := matches[3]
-			connectors = append(connectors, &connector{cid: cid, ctype: ctype, connected: connected})
+			connected := (matches[2] == "connected")
+			connectors = append(connectors, &connector{cid: cid, connected: connected})
 			continue
-		}
-
-		matches = modesetModePattern.FindStringSubmatch(line)
-		if matches == nil {
-			continue
-		}
-		var size bound
-		var err error
-		for i, name := range modesetModePattern.SubexpNames() {
-			if name == "hdisplay" {
-				size.width, err = strconv.Atoi(matches[i])
-			} else if name == "vdisplay" {
-				size.height, err = strconv.Atoi(matches[i])
-			}
-			if err != nil {
-				return connectors, errors.Wrapf(err, "failed to parse %s", name)
-			}
-		}
-		if len(connectors) == 0 {
-			return connectors, errors.Wrap(err, "failed to update last connector")
-		}
-		if size.width > 0 && size.height > 0 {
-			// Update the last connector in the list.
-			connectors[len(connectors)-1].size = size
 		}
 	}
-	// Check if all connected connectors have size set.
-	for _, display := range connectors {
-		if !display.connected {
-			continue
-		}
-		if display.size.width <= 0 || display.size.height <= 0 {
-			return nil, errors.Wrapf(err, "failed to fetch reasonable connector size: %q", display.size)
-		}
-	}
-
 	return connectors, nil
 }
 
