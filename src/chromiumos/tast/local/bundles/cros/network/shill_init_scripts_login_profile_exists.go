7b78cc5275c12ceb2c91a440c2b910a61a3547bd
Arowa Suliman <arowa@google.com>
2020-02-11 22:26:04 -0800

    tast-test: Add ShillInitScriptsLoginProfileExists
    The Tast test is migrated from network_ShillInitScripts.py, an Autotest.
    In the original version, it uses MockFlimflam instead of shill to run
    the login profile exists test. MockFlimflam provides a subset of shill DBus API
    so that we can track "dbus-send" invocations sent by the shill init
    scripts.  These tests are moved to run on real shill in the new Tast
    test, because it's possible to observe the expected behaviors without
    the mock and that allows for checking the status of the profile stack as
    part of these tests.
    
    BUG=chromium:1026901
    TEST=run tast network.ShillInitScriptsLoginProfileExists
    
    Change-Id: I5ae125235dc695d9d832abf0973d87ac8342c2f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2050355
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Arowa Suliman <arowa@chromium.org>
    Commit-Queue: Arowa Suliman <arowa@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/network/shill_init_scripts_login_profile_exists.go
@@ -0,0 +1,107 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"os"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/network/shillscript"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ShillInitScriptsLoginProfileExists,
+		Desc:     "Test that shill init scripts perform as expected",
+		Contacts: []string{"arowa@google.com", "cros-networking@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+	})
+}
+
+func ShillInitScriptsLoginProfileExists(ctx context.Context, s *testing.State) {
+	if err := shillscript.RunTest(ctx, testLoginProfileExists); err != nil {
+		s.Fatal("Failed running testLoginProfileExists: ", err)
+	}
+}
+
+// testLoginProfileExists tests logging in a user whose profile already exists.
+// Login script should only push (and not create) the user profile
+// if a user profile already exists.
+func testLoginProfileExists(ctx context.Context, env *shillscript.TestEnv) error {
+	if err := upstart.StartJob(ctx, "shill"); err != nil {
+		return errors.Wrap(err, "failed starting shill")
+	}
+
+	if err := os.Mkdir(env.ShillUserProfileDir, 0700); err != nil {
+		return errors.Wrapf(err, "failed creating the directory: %s", env.ShillUserProfileDir)
+	}
+
+	if err := testexec.CommandContext(ctx, "chown", "shill:shill", env.ShillUserProfileDir).Run(); err != nil {
+		return errors.Wrapf(err, "failed changing the owner of the directory %s to shill", shillscript.ShillUserProfilesDir)
+	}
+
+	if err := os.Mkdir(shillscript.ShillUserProfilesDir, 0700); err != nil {
+		return errors.Wrapf(err, "failed creating the directory: %s", shillscript.ShillUserProfilesDir)
+	}
+
+	if err := testexec.CommandContext(ctx, "chown", "shill:shill", shillscript.ShillUserProfilesDir).Run(); err != nil {
+		return errors.Wrapf(err, "failed changing the owner of the directory %s to shill", shillscript.ShillUserProfilesDir)
+	}
+
+	if err := os.Symlink(env.ShillUserProfileDir, shillscript.ShillUserProfileChronosDir); err != nil {
+		return errors.Wrapf(err, "failed to symlink %s to %s", env.ShillUserProfileDir, shillscript.ShillUserProfileChronosDir)
+	}
+
+	if err := shillscript.CreateProfile(ctx, shillscript.ChronosProfileName); err != nil {
+		return err
+	}
+
+	// When CreateProfile(), it also creates a symlink for chronos@
+	// (shillscript.ShillUserProfileChronosDir). When the symlink presents,
+	// it means there is an active network profile. Because shill shouldn't
+	// load multiple network profiles, shill skips login script if the symlink
+	// exists. We have to remove the symlink to mimic a wait-for-login scenario.
+	if err := os.RemoveAll(shillscript.ShillUserProfileChronosDir); err != nil {
+		return errors.Wrapf(err, "failed removing %s", shillscript.ShillUserProfileChronosDir)
+	}
+
+	timeoutCtx, cancel := context.WithTimeout(ctx, shillscript.DbusMonitorTimeout)
+	defer cancel()
+
+	stop, err := shillscript.DbusEventMonitor(timeoutCtx)
+	if err != nil {
+		return err
+	}
+
+	if err := shillscript.Login(ctx, shillscript.FakeUser); err != nil {
+		_, _ = stop()
+		return errors.Wrap(err, "failed logging in")
+	}
+
+	calledMethods, err := stop()
+	if err != nil {
+		return err
+	}
+
+	expectedCalls := []string{shillscript.InsertUserProfile}
+	if err := shillscript.AssureMethodCalls(ctx, expectedCalls, calledMethods); err != nil {
+		return err
+	}
+
+	profiles, err := shillscript.GetProfileList(ctx)
+	if err != nil {
+		return err
+	}
+
+	if len(profiles) != 2 {
+		return errors.Wrapf(err, "found unexpected number of profiles in the profile stack: got %d, want 2", len(profiles))
+	}
+
+	return nil
+}
