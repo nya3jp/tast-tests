8984c6ff770babc5770297e7f3e412623f00f8f5
khmel@chromium.org <khmel@chromium.org>
2020-02-13 12:39:03 -0800

    arc: Move DataCollector test to group:android-pfq
    To run this test only in AndroidPFQ
    
    Related CL:crrev.com/c/2055644
    
    BUG=b:148557896
    TEST=N/A
    
    Change-Id: I238369a0fdd900fbc4090b37d90d0b5abdcc6dfc

--- src/chromiumos/tast/remote/bundles/cros/arc/data_collector.go
+++ src/chromiumos/tast/remote/bundles/cros/arc/data_collector.go
@@ -29,7 +29,7 @@ func init() {
 			"khmel@chromium.org", // Original author.
 			"arc-performance@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:android-pfq"},
 		SoftwareDeps: []string{"android_both", "chrome"},
 		ServiceDeps:  []string{"tast.cros.arc.UreadaheadPackService"},
 		Timeout:      5 * time.Minute,
