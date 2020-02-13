df5c524976d7f063e81f8ad81298e17bcc24e512
Kyle Shimabukuro <kyleshima@chromium.org>
2020-02-13 09:45:54 -0800

    Add test to open Settings app from Ubertray.
    Added a simple test to open the Settings app from the Ubertray by
    clicking on the UI. The test clicks the status area in the bottom right
    to open the Ubertray, and clicks the settings button to open up the
    Settings app.
    
    BUG=chromium:1048375
    TEST=tast -verbose run 100.90.26.151 ui.UbertrayOpenSettings
    
    Change-Id: If751bd30fcb87ded9409fbeaeb3e60c57a5369ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2054753
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Tested-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: Kyle Shimabukuro <kyleshima@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/ui/ubertray_open_settings.go
@@ -0,0 +1,88 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: UbertrayOpenSettings,
+		Desc: "Checks that settings can be opened from the Ubertray",
+		Contacts: []string{
+			"bhansknecht@chromium.org",
+			"kyleshima@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+// UbertrayOpenSettings tests that we can open the settings app from the Ubertray.
+func UbertrayOpenSettings(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	root, err := ui.Root(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get UI automation root: ", err)
+	}
+	defer root.Release(ctx)
+
+	// Find and click the StatusArea via UI. Clicking it opens the Ubertray.
+	params := ui.FindParams{
+		ClassName: "ash/StatusAreaWidgetDelegate",
+	}
+	statusArea, err := root.DescendantWithTimeout(ctx, params, 10*time.Second)
+	if err != nil {
+		s.Fatal("Failed to find the status area (time, battery, etc.): ", err)
+	}
+	defer statusArea.Release(ctx)
+
+	if err := statusArea.LeftClick(ctx); err != nil {
+		s.Fatal("Failed to click status area: ", err)
+	}
+
+	// Find and click the Settings button in the Ubertray via UI.
+	params = ui.FindParams{
+		Name:      "Settings",
+		ClassName: "TopShortcutButton",
+	}
+	settingsBtn, err := root.DescendantWithTimeout(ctx, params, 10*time.Second)
+	if err != nil {
+		s.Fatal("Failed to find Ubertray Settings button: ", err)
+	}
+	defer settingsBtn.Release(ctx)
+
+	if err := settingsBtn.LeftClick(ctx); err != nil {
+		s.Fatal("Failed to click Ubertray Settings button: ", err)
+	}
+
+	// Wait for Settings app to open by checking if it's in the shelf.
+	if err := ash.WaitForApp(ctx, tconn, apps.Settings.ID); err != nil {
+		s.Fatal("Settings app did not appear in the shelf: ", err)
+	}
+
+	// Confirm that the Settings app is open by checking for the "Settings" heading.
+	params = ui.FindParams{
+		Role: ui.RoleTypeHeading,
+		Name: "Settings",
+	}
+	if err := root.WaitForDescendant(ctx, params, true, 10*time.Second); err != nil {
+		s.Fatal("Waiting for Settings app heading failed: ", err)
+	}
+}
