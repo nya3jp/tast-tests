b16214295569b94586eeb5d9afd80f3849e9860b
Yen-lin Lai <yenlinlai@google.com>
2020-02-13 12:55:24 +0800

    network: move IwlwifiPCIRescan to remote for better recovery
    When the test failed, WiFi interface will be removed and this affects
    later tests. We have a basic recovery mechanism (reload the driver) but
    it doesn't seem to work very well. In the worst case, we might have to
    reboot DUT to have the interface back. With current Tast design, we
    cannot do this in a local test. Move this test to remote and add
    recovery as suggested in [1].
    
    Have a feature request in crbug.com/1051801. We might move this back to
    local test if we can do similar recovery there.
    
    The local version is still kept and disabled for debugging purpose.
    
    [1] https://groups.google.com/a/chromium.org/g/tast-users/c/9ohr0Z9C8Nw
    
    BUG=chromium:1051352
    TEST=`tast run $DUT network.RemoteIwlwifiPCIRescan` on cyan DUT.
    Also manually modify the rescan script to noop and verify the reboot
    recovery does work.
    
    Change-Id: Ide4c06cae5d1db1a973c64e15d9155a29679f4ec

--- src/chromiumos/tast/local/bundles/cros/network/iwlwifi_pci_rescan.go
+++ src/chromiumos/tast/local/bundles/cros/network/iwlwifi_pci_rescan.go
@@ -6,180 +6,24 @@ package network
 
 import (
 	"context"
-	"fmt"
-	"io/ioutil"
-	"time"
 
-	"github.com/godbus/dbus"
-
-	"chromiumos/tast/errors"
-	"chromiumos/tast/local/dbusutil"
-	"chromiumos/tast/local/network"
-	"chromiumos/tast/local/shill"
-	"chromiumos/tast/local/testexec"
-	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/local/bundles/cros/network/iwlwifirescan"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         IwlwifiPCIRescan,
-		Desc:         "Verifies that the WiFi interface will recover if removed when the device has iwlwifi_rescan",
-		Contacts:     []string{"yenlinlai@google.com", "chromeos-kernel-wifi@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Func:     IwlwifiPCIRescan,
+		Desc:     "Verifies that the WiFi interface will recover if removed when the device has iwlwifi_rescan",
+		Contacts: []string{"yenlinlai@google.com", "chromeos-kernel-wifi@google.com"},
+		// For now, we prefer the remote version. Disable and keep the test to reproduce issue locally.
+		Attr:         []string{"group:mainline", "disabled"},
 		SoftwareDeps: []string{"iwlwifi_rescan"},
 	})
 }
 
-// restartInterface tries to reload Wifi driver when the device does not recover.
-func restartInterface(ctx context.Context) error {
-	err := testexec.CommandContext(ctx, "modprobe", "-r", "iwlmvm", "iwlwifi").Run(testexec.DumpLogOnError)
-	if err != nil {
-		err = errors.Wrap(err, "could not remove module iwlmvm and iwlwifi")
-	}
-	if err2 := testexec.CommandContext(ctx, "modprobe", "iwlwifi").Run(testexec.DumpLogOnError); err2 != nil {
-		return errors.Wrapf(err, "could not load iwlwifi module: %s", err2.Error())
-	}
-	return err
-}
-
-// waitIfaceRemoval waits until the interface removal from shill.
-func waitIfaceRemoval(ctx context.Context, pw *shill.PropertiesWatcher, iface string) error {
-	for {
-		v, err := pw.WaitAll(ctx, shill.ManagerPropertyDevices)
-		if err != nil {
-			return err
-		}
-		devicePaths, ok := v[0].([]dbus.ObjectPath)
-		if !ok {
-			return errors.Errorf("unexpected value for devices property: %v", v)
-		}
-		exist := false
-		for _, dPath := range devicePaths {
-			dev, err := shill.NewDevice(ctx, dPath)
-			if err != nil {
-				return err
-			}
-			devProps, err := dev.GetProperties(ctx)
-			if err != nil {
-				if dbusutil.IsDBusError(err, dbusutil.DBusErrorUnknownObject) {
-					// This error is forgivable as a device may go down anytime.
-					continue
-				}
-				return err
-			}
-			devIface, err := devProps.GetString(shill.DevicePropertyInterface)
-			if err != nil {
-				// Skip the devices without valid DevicePropertyInterface
-				testing.ContextLogf(ctx, "Skip device %s without valid interface, err=%s",
-					dPath, err.Error())
-				continue
-			}
-			if devIface != iface {
-				continue
-			}
-			exist = true
-			break
-		}
-		if !exist {
-			return nil
-		}
-	}
-}
-
-// removeIfaceAndWait removes the interface and wait until shill does remove it.
-func removeIfaceAndWait(ctx context.Context, m *shill.Manager, iface string) error {
-	pw, err := m.CreateWatcher(ctx)
-	if err != nil {
-		return err
-	}
-	defer pw.Close(ctx)
-
-	// Spawn watcher before removal.
-	done := make(chan error, 1)
-	watcherCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-	go func() {
-		done <- waitIfaceRemoval(watcherCtx, pw, iface)
-	}()
-
-	driverPath := fmt.Sprintf("/sys/class/net/%s/device/remove", iface)
-	if err := ioutil.WriteFile(driverPath, []byte("1"), 0200); err != nil {
-		return errors.Wrapf(err, "could not remove %s driver: %s", iface, err.Error())
-	}
-
-	return <-done
-}
-
 func IwlwifiPCIRescan(ctx context.Context, s *testing.State) {
-	manager, err := shill.NewManager(ctx)
-	if err != nil {
-		s.Fatal("Failed to create shill manager: ", err)
-	}
-	iface, err := shill.GetWifiInterface(ctx, manager, 10*time.Second)
-	if err != nil {
-		s.Fatal("Could not get a WiFi interface: ", err)
-	}
-	rescanFile := fmt.Sprintf("/sys/class/net/%s/device/driver/module/parameters/remove_when_gone", iface)
-	out, err := ioutil.ReadFile(rescanFile)
-	if err != nil {
-		s.Fatal("Could not read rescan file: ", err)
-	} else if string(out) != "Y\n" {
-		s.Fatalf("wifi rescan should be enabled, current mode is %q", string(out))
-	}
-
-	// TODO(crbug.com/1048366): We now have a shill restart in pci-rescan, so we have
-	// to wait until shill does restart after removing the interface in order to avoid
-	// any potential restart in later code.
-	unlock, err := network.LockCheckNetworkHook(ctx)
-	if err != nil {
-		s.Fatal("Failed to lock the check network hook: ", err)
-	}
-	defer unlock()
-
-	// Get shill pid.
-	_, _, shillPid, err := upstart.JobStatus(ctx, "shill")
-	if err != nil {
-		s.Fatal("Failed to get upstart status of shill: ", err)
-	} else if shillPid == 0 {
-		s.Fatal("Failed to get valid shill pid")
-	}
-
-	testing.ContextLog(ctx, "Remove the interface and wait for shill to update")
-	if err := removeIfaceAndWait(ctx, manager, iface); err != nil {
-		s.Fatal("Failed to remove interface: ", err)
-	}
-
-	// Wait for shill to be "running" with another pid.
-	testing.ContextLog(ctx, "Wait for shill restart")
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		_, _, pid, err := upstart.JobStatus(ctx, "shill")
-		if err != nil {
-			return testing.PollBreak(errors.Wrap(err, "cannot get upstart status of shill"))
-		}
-		if pid == 0 {
-			return errors.New("cannot get valid shill pid")
-		}
-		if pid == shillPid {
-			return errors.New("shill not yet restarted")
-		}
-		return nil
-	}, &testing.PollOptions{Timeout: 10 * time.Second}); err != nil {
-		s.Fatal("Failed to wait for shill restart: ", err)
-	}
-
-	testing.ContextLog(ctx, "Checking the interface recovery")
-	// Create a new manager to wait for shill being ready for dbus.
-	manager, err = shill.NewManager(ctx)
-	if err != nil {
-		s.Fatal("Failed to create Manager object after shill restart: ", err)
-	}
-	newIface, err := shill.GetWifiInterface(ctx, manager, 30*time.Second)
-	if err != nil {
-		restartInterface(ctx)
-		s.Fatal("Device did not recover: ", err)
-	} else if iface != newIface {
-		restartInterface(ctx)
-		s.Fatalf("looking for interface %s but got %s", iface, newIface)
+	if err := iwlwifirescan.RemoveIfaceAndWaitForRecovery(ctx); err != nil {
+		s.Fatal("Test failed with reason: ", err)
 	}
 }
