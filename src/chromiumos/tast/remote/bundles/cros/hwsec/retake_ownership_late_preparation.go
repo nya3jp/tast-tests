55652bcb766e9ad249fbd88afe21b272162a6837
Victor-Gabriel Savu <vsavu@google.com>
2020-02-13 11:33:28 +0100

    tast-tests: add helper to reset owner
    Add helper to reset the owner of the device to be used by device
    policy tests.
    
    BUG=None
    TEST=tast run <ip> policy.DeviceWilcoDtcAllowed
    TEST=tast run <ip> hwsec.ClearOwnership
    
    Change-Id: I47eb32a6623e253da9cb9783d80cb349ed507800

--- src/chromiumos/tast/remote/bundles/cros/hwsec/retake_ownership_late_preparation.go
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/retake_ownership_late_preparation.go
@@ -42,7 +42,7 @@ func RetakeOwnershipLatePreparation(ctx context.Context, s *testing.State) {
 	}
 
 	s.Log("Start resetting TPM if needed")
-	if err := helper.EnsureTPMIsReset(ctx); err != nil {
+	if err := helper.EnsureTPMIsReset(ctx, false); err != nil {
 		s.Fatal("Failed to ensure resetting TPM: ", err)
 	}
 	s.Log("TPM is confirmed to be reset")
