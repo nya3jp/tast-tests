8390e75cba0411a22332ec00363d90c66438afe6
Yen-lin Lai <yenlinlai@google.com>
2020-02-13 12:55:24 +0800

    network: move IwlwifiPCIRescan to remote for better recovery
    When the test failed, WiFi interface will be removed and this affects
    later tests. We have a basic recovery mechanism (reload the driver) but
    it doesn't seem to work very well. In the worst case, we might have to
    reboot DUT to have the interface back. With current Tast design, we
    cannot do this in a local test. Move this test to remote and add
    recovery as suggested in [1].
    
    Have a feature request in crbug.com/1051801. We might move this back to
    local test if we can do similar recovery there.
    
    The local version is still kept and disabled for debugging purpose.
    
    [1] https://groups.google.com/a/chromium.org/g/tast-users/c/9ohr0Z9C8Nw
    
    BUG=chromium:1051352
    TEST=`tast run $DUT network.RemoteIwlwifiPCIRescan` on cyan DUT.
    Also manually modify the rescan script to noop and verify the reboot
    recovery does work.
    
    Change-Id: Ide4c06cae5d1db1a973c64e15d9155a29679f4ec

---
+++ src/chromiumos/tast/local/bundles/cros/network/iwlwifi_pci_rescan_service.go
@@ -0,0 +1,52 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/network/iwlwifirescan"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddService(&testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			network.RegisterIwlwifiPCIRescanServer(srv, &IwlwifiPCIRescanService{})
+		},
+	})
+}
+
+// IwlwifiPCIRescanService implements tast.cros.network.IwlwifiPCIRescan gRPC service.
+type IwlwifiPCIRescanService struct{}
+
+// RemoveIfaceAndWaitForRecovery triggers iwlwifi-rescan rule by removing the WiFi device.
+// iwlwifi-rescan will rescan PCI bus and bring the WiFi device back.
+func (s *IwlwifiPCIRescanService) RemoveIfaceAndWaitForRecovery(ctx context.Context, _ *empty.Empty) (*empty.Empty, error) {
+	if err := iwlwifirescan.RemoveIfaceAndWaitForRecovery(ctx); err != nil {
+		return nil, err
+	}
+	return &empty.Empty{}, nil
+}
+
+// HealthCheck checks if the DUT has a WiFi device. If not, we may need to reboot the DUT.
+func (s *IwlwifiPCIRescanService) HealthCheck(ctx context.Context, _ *empty.Empty) (*empty.Empty, error) {
+	manager, err := shill.NewManager(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create shill manager")
+	}
+	_, err = shill.GetWifiInterface(ctx, manager, 5*time.Second)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get a WiFi interface")
+	}
+	return &empty.Empty{}, nil
+}
