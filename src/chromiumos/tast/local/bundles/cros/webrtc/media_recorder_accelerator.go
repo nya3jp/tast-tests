1fc7030e81bd587adbe4c5d46797f999eefd2259
Madhulika <madhulika.dolke@intel.com>
2020-02-14 15:59:36 -0800

    tast-tests: Changing software dependency to chrome_media from chrome_internal
    Chrome_internal is a USE flag that can be built with only internally
    by Google. As a partner, when we build Chromium OS images to test
    proprietary codecs such as H.264 we set USE=”chrome_media”.
    These are focused development images and not distributed.
    However, some tests like arc.LibvdaDecde.h264 depend on chrome_internal
    as a software dependency. This test checks video decoding using libvda's
    Mojo connection to GAVDA is working. We enable the intel-media-driver
    on X86 platforms and hence need to test H.264 codecs.
    
    BUG=chromium:1084463
    TEST=tast run <DUT IP> arc.LibvdaDecode.h264
    
    Cq-Depend: chromium:2207558
    Change-Id: Ia78f532696d6056a9ddaabbc0d720be6b5dfb5a7

--- src/chromiumos/tast/local/bundles/cros/webrtc/media_recorder_accelerator.go
+++ src/chromiumos/tast/local/bundles/cros/webrtc/media_recorder_accelerator.go
@@ -30,8 +30,8 @@ func init() {
 		Params: []testing.Param{{
 			Name: "h264",
 			Val:  videotype.H264,
-			// "chrome_internal" is needed because H.264 is a proprietary codec.
-			ExtraSoftwareDeps: []string{caps.HWEncodeH264, "chrome_internal"},
+			// "supports_proprietary_codecs" is needed because H.264 is a proprietary codec.
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264, "supports_proprietary_codecs"},
 			Pre:               pre.ChromeVideoWithFakeWebcam(),
 		}, {
 			Name:              "vp8",
