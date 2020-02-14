4a033138d0a7340fa1c0bbba40cc1452028f4620
Yusuke Sato <yusukes@google.com>
2020-02-14 15:15:51 -0800

    tast-test: Enable arc.BlackFlash on ARCVM
    BUG=b:135880969
    TEST=tast run DUT_EVE arc.BlackFlash.vm
    TEST=tast run DUT_EVE arc.BlackFlash
    
    Change-Id: Id6969ef79302451d435aed00f34489761f584568

--- src/chromiumos/tast/local/bundles/cros/arc/black_flash.go
+++ src/chromiumos/tast/local/bundles/cros/arc/black_flash.go
@@ -27,9 +27,16 @@ func init() {
 		Desc:         "Checks that Black flashes don't appear when ARC applications change window states",
 		Contacts:     []string{"takise@chromium.org", "arc-framework+tast@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"android_p", "chrome"},
-		Pre:          arc.Booted(),
+		SoftwareDeps: []string{"chrome"},
 		Data:         []string{"ArcBlackFlashTest.apk"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               arc.Booted(),
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               arc.VMBooted(),
+		}},
 	})
 }
 
