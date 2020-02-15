9b2be4fc6ec2f1ac8694f666ec6ddc3a84248f20
khmel@chromium.org <khmel@chromium.org>
2020-02-14 16:16:37 -0800

    arc: Add GMS Core and GSF caches to data collector.
    This CL:
      * add GMS Core and GSF caches generation.
      * Make upload resources to server optional.
      * Change root of generated resources
      * Use separate baskets to upload packs and GMS Core caches
      * ureadahead packs depend on Android build id and ABI. So these is
        used as a key for ureadahead packs upload.
      * ureadahead packs are uploaded if this is new Android build id and
        build is official.
      * GMS Core caches depend on GMS Core version and ABI only. So these is
        used as a key for GMS Core caches upload.
      * GMS Core caches are uploaded only for new GMS Core version.
      * Note, there is no check for official build for GMS Core upload. We
        uprev GMS Core manually quite rare and it is dev responsibility to
        run this test manually that make caches ready for the builder.
    
    BUG=b:148766837
    TEST=tast run DEVICE arc.DataCollector
    
    Cq-Depend: chromium:2055607
    Change-Id: I4f213015dfe159989e56c9ca89f20029df77737b

---
+++ src/chromiumos/tast/local/bundles/cros/arc/gmscore_cache_service.go
@@ -0,0 +1,71 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/arc/cache"
+	arcpb "chromiumos/tast/services/cros/arc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddService(&testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			arcpb.RegisterGmsCoreCacheServiceServer(srv, &GmsCoreCacheService{s: s})
+		},
+	})
+}
+
+// GmsCoreCacheService implements tast.cros.arc.GmsCoreCacheService.
+type GmsCoreCacheService struct {
+	s *testing.ServiceState
+}
+
+// Generate generates GMS Core and GFS caches.
+func (c *GmsCoreCacheService) Generate(ctx context.Context, req *empty.Empty) (res *arcpb.GmsCoreCacheResponse, retErr error) {
+	// Boot ARC without existing GMS caches enabled to let GMS Core generate genuine caches.
+	testing.ContextLog(ctx, "Starting ARC, with existing GMS caches disabled")
+
+	targetDir, err := ioutil.TempDir("", "gms_core_caches")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to created target dir for GMS Core caches")
+	}
+	defer func() {
+		if retErr != nil {
+			os.RemoveAll(targetDir)
+		}
+	}()
+
+	cr, a, err := cache.OpenSession(ctx, cache.PackagesCopy, cache.GMSCoreDisabled, nil, targetDir)
+	if err != nil {
+		os.RemoveAll(targetDir)
+		return nil, errors.Wrap(err, "failed to generage GMS Core caches")
+	}
+
+	defer cr.Close(ctx)
+	defer a.Close()
+
+	if err := cache.CopyCaches(ctx, a, targetDir); err != nil {
+		os.RemoveAll(targetDir)
+		return nil, errors.Wrap(err, "failed to generage GMS Core caches")
+	}
+
+	response := arcpb.GmsCoreCacheResponse{
+		TargetDir:           targetDir,
+		PackagesCacheName:   cache.PackagesCacheXML,
+		GmsCoreCacheName:    cache.GMSCoreCacheArchive,
+		GmsCoreManifestName: cache.GMSCoreManifest,
+		GsfCacheName:        cache.GSFCache,
+	}
+	return &response, nil
+}
--- src/chromiumos/tast/remote/bundles/cros/arc/data_collector.go
+++ src/chromiumos/tast/remote/bundles/cros/arc/data_collector.go
@@ -7,10 +7,14 @@ package arc
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
 	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
 
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/dut"
@@ -32,8 +36,9 @@ func init() {
 		Attr: []string{"group:mainline", "informational"},
 		// TODO(b/150012956): Stop using 'arc' here and use ExtraSoftwareDeps instead.
 		SoftwareDeps: []string{"arc", "chrome"},
-		ServiceDeps:  []string{"tast.cros.arc.UreadaheadPackService"},
-		Timeout:      10 * time.Minute,
+		ServiceDeps: []string{"tast.cros.arc.UreadaheadPackService",
+			"tast.cros.arc.GmsCoreCacheService"},
+		Timeout: 10 * time.Minute,
 		Vars: []string{
 			"arc.DataCollector.UreadaheadService_username",
 			"arc.DataCollector.UreadaheadService_password",
@@ -42,11 +47,11 @@ func init() {
 }
 
 // getArcVersionRemotely gets ARC build properties from the device, parses for build ID, ABI, and
-// returns these fields as a combined string.
-func getArcVersionRemotely(ctx context.Context, s *testing.State, dut *dut.DUT) (string, error) {
+// returns these fields as a combined string. It also return weither this is official build or not
+func getArcVersionRemotely(ctx context.Context, dut *dut.DUT) (bool, string, error) {
 	isARCVM, err := dut.Command("cat", "/run/chrome/is_arcvm").Output(ctx)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to check ARCVM status remotely")
+		return false, "", errors.Wrap(err, "failed to check ARCVM status remotely")
 	}
 
 	var propertyFile string
@@ -58,54 +63,40 @@ func getArcVersionRemotely(ctx context.Context, s *testing.State, dut *dut.DUT) 
 
 	buildProp, err := dut.Command("cat", propertyFile).Output(ctx)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to read ARC build property file remotely")
+		return false, "", errors.Wrap(err, "failed to read ARC build property file remotely")
 	}
 	buildPropStr := string(buildProp)
 
 	mArch := regexp.MustCompile(`(\n|^)ro.product.cpu.abi=(.+)(\n|$)`).FindStringSubmatch(buildPropStr)
 	if mArch == nil {
-		return "", errors.Errorf("ro.product.cpu.abi is not found in %q", buildPropStr)
+		return false, "", errors.Errorf("ro.product.cpu.abi is not found in %q", buildPropStr)
 	}
 
 	// Note, this should work on official builds only. Custom built Android image contains the
 	// version in different format.
-	mVersion := regexp.MustCompile(`(\n|^)ro.build.version.incremental=(\d+)(\n|$)`).FindStringSubmatch(buildPropStr)
+	mVersion := regexp.MustCompile(`(\n|^)ro.build.version.incremental=(.+)(\n|$)`).FindStringSubmatch(buildPropStr)
 	if mVersion == nil {
-		return "", errors.Errorf("Valid ro.build.version.incremental is not found in %q", buildPropStr)
+		return false, "", errors.Errorf("ro.build.version.incremental is not found in %q", buildPropStr)
 	}
 
+	official := regexp.MustCompile(`^\d+$`).MatchString(mVersion[2])
 	result := fmt.Sprintf("%s_%s", mArch[2], mVersion[2])
-	return result, nil
-}
-
-// uploadUreadaheadPack gets ureadahead pack from the target device and uploads it to the server.
-func uploadUreadaheadPack(ctx context.Context, s *testing.State, dut *dut.DUT, version, src, dst string) error {
-	// Base path for uploaded ureadahead packs.
-	const serverUreadaheadPackRoot = "gs://chromeos-arc-images/ureadahead_packs"
-
-	packPath := filepath.Join(s.OutDir(), dst)
-	if err := dut.GetFile(ctx, src, packPath); err != nil {
-		return errors.Wrap(err, "failed to get ARC ureadahead pack from the device")
-	}
-
-	gsURL := fmt.Sprintf("%s/%s/%s", serverUreadaheadPackRoot, version, dst)
-
-	// Use gsutil command to upload the pack to the server.
-	s.Logf("Uploading ARC ureadahead pack to the server: %q", gsURL)
-	cmd := exec.Command("gsutil", "copy", packPath, gsURL)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return errors.Wrapf(err, "failed to upload ARC ureadahead pack to the server %q", out)
-	}
-
-	s.Logf("Uploaded ARC ureadahead pack to the server: %q", gsURL)
-	return nil
+	return official, result, nil
 }
 
 // DataCollector performs ARC++ boots in various conditions, grabs required data and uploads it to
 // the binary server.
 func DataCollector(ctx context.Context, s *testing.State) {
 	const (
+		// Base path for uploaded resources.
+		runtimeArtefactsRoot = "gs://chromeos-arc-images/runtime_artefacts"
+
+		// ureadahed packs bucket
+		ureadAheadPacks = "ureadahead_packs"
+
+		// GMS Core caches bucket
+		gmsCoreCaches = "gms_core_caches"
+
 		// Name of the pack in case of initial boot.
 		initialPack = "initial_pack"
 
@@ -122,48 +113,160 @@ func DataCollector(ctx context.Context, s *testing.State) {
 	}
 	defer cl.Close(ctx)
 
-	v, err := getArcVersionRemotely(ctx, s, d)
-	if err != nil {
-		s.Fatal("Failed to get ARC version: ", err)
-	}
-	s.Logf("Detected version: %s", v)
-
-	service := arc.NewUreadaheadPackServiceClient(cl.Conn)
-	// First boot is needed to be initial boot with removing all user data.
-	request := arcpb.UreadaheadPackRequest{
-		InitialBoot: true,
-		Username:    s.RequiredVar("arc.DataCollector.UreadaheadService_username"),
-		Password:    s.RequiredVar("arc.DataCollector.UreadaheadService_password"),
+	remoteURL := func(bucket, version, dst string) string {
+		return fmt.Sprintf("%s/%s/%s/%s", runtimeArtefactsRoot, bucket, version, dst)
 	}
 
-	// Shorten the total context by 5 seconds to allow for cleanup.
-	shortCtx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
-	defer cancel()
+	// upload gets file from the target device and uploads it to the server.
+	upload := func(ctx context.Context, dut *dut.DUT, src, bucket, version, dst string) error {
+		temp, err := ioutil.TempFile("", filepath.Base(src))
+		if err != nil {
+			return errors.Wrapf(err, "failed to create temp file for %q", src)
+		}
+		defer os.Remove(temp.Name())
 
-	// Due to race condition of using ureadahead in various parts of Chrome,
-	// first generation might be incomplete. Just pass it without analyzing.
-	if _, err := service.Generate(shortCtx, &request); err != nil {
-		s.Fatal("UreadaheadPackService.Generate returned an error for warm-up pass: ", err)
+		if err := dut.GetFile(ctx, src, temp.Name()); err != nil {
+			return errors.Wrapf(err, "failed to get %q from the device", src)
+		}
+
+		gsURL := remoteURL(bucket, version, dst)
+
+		// Use gsutil command to upload the file to the server.
+		testing.ContextLogf(ctx, "Uploading %q to the server", gsURL)
+		if out, err := exec.Command("gsutil", "copy", temp.Name(), gsURL).CombinedOutput(); err != nil {
+			return errors.Wrapf(err, "failed to upload ARC ureadahead pack to the server %q", out)
+		}
+
+		testing.ContextLogf(ctx, "Uploaded %q to the server", gsURL)
+		return nil
 	}
 
-	// Pass initial boot and capture results.
-	response, err := service.Generate(shortCtx, &request)
-	if err != nil {
-		s.Fatal("UreadaheadPackService.Generate returned an error for initial boot pass: ", err)
-	}
+	genUreadaheadPack := func() {
+		official, v, err := getArcVersionRemotely(ctx, d)
+		if err != nil {
+			s.Fatal("Failed to get ARC version: ", err)
+		}
+		s.Logf("Detected version: %s", v)
 
-	if err = uploadUreadaheadPack(shortCtx, s, d, v, response.PackPath, initialPack); err != nil {
-		s.Fatal("Failed to upload initial boot pack: ", err)
-	}
+		service := arc.NewUreadaheadPackServiceClient(cl.Conn)
+		// First boot is needed to be initial boot with removing all user data.
+		request := arcpb.UreadaheadPackRequest{
+			InitialBoot: true,
+			Username:    s.RequiredVar("arc.DataCollector.UreadaheadService_username"),
+			Password:    s.RequiredVar("arc.DataCollector.UreadaheadService_password"),
+		}
 
-	// Now pass provisioned boot and capture results.
-	request.InitialBoot = false
-	response, err = service.Generate(shortCtx, &request)
-	if err != nil {
-		s.Fatal("UreadaheadPackService.Generate returned an error for second boot pass: ", err)
-	}
+		// Checks if generated packs need to be uploaded to the server.
+		needUpload := func() bool {
+			if !official {
+				s.Logf("Version: %s is not official version and generated ureadahead packs won't be uploaded to the server", v)
+				return false
+			}
 
-	if err = uploadUreadaheadPack(shortCtx, s, d, v, response.PackPath, provisionedPack); err != nil {
-		s.Fatal("Failed to upload provisioned boot pack: ", err)
+			packs := []string{initialPack, provisionedPack}
+			for _, pack := range packs {
+				gsURL := remoteURL(ureadAheadPacks, v, pack)
+				if err := exec.Command("gsutil", "stat", gsURL).Run(); err != nil {
+					return true
+				}
+			}
+			// This test is scheduled to run once per build id and ARCH. So race should never happen.
+			s.Logf("Version: %s has all packs uploaded and generated ureadahead packs won't be uploaded to the server", v)
+			return false
+		}
+
+		needUploadPacks := needUpload()
+
+		// Shorten the total context by 5 seconds to allow for cleanup.
+		shortCtx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+		defer cancel()
+
+		// Due to race condition of using ureadahead in various parts of Chrome,
+		// first generation might be incomplete. Just pass it without analyzing.
+		if _, err := service.Generate(shortCtx, &request); err != nil {
+			s.Fatal("UreadaheadPackService.Generate returned an error for warm-up pass: ", err)
+		}
+
+		// Pass initial boot and capture results.
+		response, err := service.Generate(shortCtx, &request)
+		if err != nil {
+			s.Fatal("UreadaheadPackService.Generate returned an error for initial boot pass: ", err)
+		}
+
+		if needUploadPacks {
+			if err = upload(shortCtx, d, response.PackPath, ureadAheadPacks, v, initialPack); err != nil {
+				s.Fatal("Failed to upload initial boot pack: ", err)
+			}
+		}
+
+		// Now pass provisioned boot and capture results.
+		request.InitialBoot = false
+		response, err = service.Generate(shortCtx, &request)
+		if err != nil {
+			s.Fatal("UreadaheadPackService.Generate returned an error for second boot pass: ", err)
+		}
+
+		if needUploadPacks {
+			if err = upload(shortCtx, d, response.PackPath, ureadAheadPacks, v, provisionedPack); err != nil {
+				s.Fatal("Failed to upload provisioned boot pack: ", err)
+			}
+		}
 	}
+	genUreadaheadPack()
+
+	genGmsCoreCache := func() {
+		service := arc.NewGmsCoreCacheServiceClient(cl.Conn)
+
+		// Shorten the total context by 5 seconds to allow for cleanup.
+		shortCtx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+		defer cancel()
+
+		response, err := service.Generate(shortCtx, &empty.Empty{})
+		if err != nil {
+			s.Fatal("GmsCoreCacheService.Generate returned an error: ", err)
+		}
+		defer d.Command("rm", "-rf", response.TargetDir).Output(ctx)
+
+		packages, err := d.Command("cat", filepath.Join(response.TargetDir, response.PackagesCacheName)).Output(ctx)
+		if err != nil {
+			s.Fatal("Failed to read packages_cache.xml: ", err)
+		}
+
+		gmsCoreVersion := regexp.MustCompile(`<package name=\"com\.google\.android\.gms\".+primaryCpuAbi=\"(\S+)\".+version=\"(\d+)\".+>`).FindStringSubmatch(string(packages))
+		if gmsCoreVersion == nil {
+			s.Fatal("Failed to parse GMS Core version from packages_cache.xml")
+		}
+
+		v := fmt.Sprintf("%s_%s", gmsCoreVersion[1], gmsCoreVersion[2])
+		s.Logf("Detected GMS core version: %s", v)
+
+		// Checks if generated GMS Core caches need to be uploaded to the server.
+		resources := []string{response.GmsCoreCacheName, response.GmsCoreManifestName, response.GsfCacheName}
+		needUpload := func() bool {
+			for _, resource := range resources {
+				gsURL := remoteURL(gmsCoreCaches, v, resource)
+				if err := exec.Command("gsutil", "stat", gsURL).Run(); err != nil {
+					return true
+				}
+			}
+
+			s.Logf("GMS Core: %s has all resources uploaded and generated caches won't be uploaded to the server", v)
+			return false
+		}
+
+		if needUpload() {
+			for _, resource := range resources {
+				if err = upload(
+					shortCtx,
+					d,
+					filepath.Join(response.TargetDir, resource),
+					gmsCoreCaches,
+					v,
+					resource); err != nil {
+					s.Fatalf("Failed to upload %q: %v", resource, err)
+				}
+			}
+		}
+	}
+	genGmsCoreCache()
 }
