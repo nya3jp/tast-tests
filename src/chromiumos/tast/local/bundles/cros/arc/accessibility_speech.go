4b0c68d3fd75172fa4bfa34d56651066525af3db
Sara Kato <sarakato@google.com>
2020-02-17 15:55:16 +0900

    Add accessibility speech log test
    This test was reverted in crrev/c/1875169, however changes have since
    been made which should make this more stable.
    
    BUG=b:144139889
    TEST=tast run DUT_IP arc.AccessibilitySpeech
    Change-Id: I8c90946e2a57b181c994db783c301073b429f4d7

---
+++ src/chromiumos/tast/local/bundles/cros/arc/accessibility_speech.go
@@ -0,0 +1,101 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/accessibility"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AccessibilitySpeech,
+		Desc:         "Checks ChromeVox reads Android elements as expected",
+		Contacts:     []string{"sarakato@chromium.org", "dtseng@chromium.org", "hirokisato@chromium.org", "arc-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"android_p", "chrome"},
+		Data:         []string{accessibility.ApkName},
+		Timeout:      4 * time.Minute,
+		Pre:          arc.Booted(),
+	})
+}
+
+func checkSpeechLog(ctx context.Context, chromeVoxConn *chrome.Conn, wantLog string) error {
+	if err := accessibility.WaitForChromeVoxStopSpeaking(ctx, chromeVoxConn); err != nil {
+		return errors.Wrap(err, "could not check if ChromeVox is speaking")
+	}
+
+	gotLogs, err := accessibility.SpeechLog(ctx, chromeVoxConn)
+	if err != nil {
+		return errors.Wrap(err, "could not get speech log")
+	}
+
+	if gotLogString := strings.Join(gotLogs, " "); gotLogString != wantLog {
+		return errors.Errorf("Speech log was not as expected for. Got: %q, Want: %q", gotLogString, wantLog)
+	}
+
+	// Ensure that ChromeVox log is cleared before proceeding.
+	if err := chromeVoxConn.Exec(ctx, "LogStore.instance.clearLog()"); err != nil {
+		return errors.Wrap(err, "error with clearing ChromeVox Log")
+	}
+	return nil
+}
+
+func AccessibilitySpeech(ctx context.Context, s *testing.State) {
+	const (
+		appName                     = "Accessibility Test App"
+		seekBarInitialValue         = 25
+		seekBarDiscreteInitialValue = 3
+	)
+
+	accessibility.RunTest(ctx, s, func(ctx context.Context, a *arc.ARC, chromeVoxConn *chrome.Conn, ew *input.KeyboardEventWriter) error {
+		// Array containg expected speech logs from ChromeVox.
+		expectedSpeechLogs := []string{
+			"OFF Toggle Button Not pressed Press Search+Space to toggle",
+			"CheckBox Check box Not checked Press Search+Space to toggle",
+			"seekBar Slider 25 Min 0 Max 100",
+			"seekBarDiscrete Slider 3 Min 0 Max 10",
+			"ANNOUNCE Button Press Search+Space to activate",
+		}
+		if err := accessibility.WaitForChromeVoxStopSpeaking(ctx, chromeVoxConn); err != nil {
+			s.Fatal("Could not check if ChromeVox is speaking: ", err)
+		}
+
+		// Enable speech logging.
+		if err := chromeVoxConn.Exec(ctx, `CommandHandler.onCommand("enableLogging")`); err != nil {
+			s.Fatal("Could not enable speech logging: ", err)
+		}
+
+		// Ensure that ChromeVox log is cleared before proceeding.
+		if err := chromeVoxConn.Exec(ctx, "LogStore.instance.clearLog()"); err != nil {
+			s.Fatal("error with clearing ChromeVox Log")
+		}
+
+		// Move focus to each of the UI elements, and check that ChromeVox log speaks as expected.
+		for _, expectedLog := range expectedSpeechLogs {
+			if err := ew.Accel(ctx, "Tab"); err != nil {
+				s.Fatal(err, "Accel(Tab) returned error")
+			}
+
+			if err := testing.Poll(ctx, func(ctx context.Context) error {
+				if err := checkSpeechLog(ctx, chromeVoxConn, expectedLog); err != nil {
+					return testing.PollBreak(err)
+				}
+				return nil
+			}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+				s.Fatal(err, "failed to check speech log")
+			}
+		}
+		return nil
+	})
+}
