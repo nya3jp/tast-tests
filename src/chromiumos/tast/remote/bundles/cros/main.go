8952f5e36493929381e17e2e730e68ae197e9770
Victor-Gabriel Savu <vsavu@google.com>
2020-02-17 13:58:05 +0100

    tast-tests: add test for DeviceWilcoDtcAllowed
    Add test for DeviceWilcoDtcAllowed.
    
    BUG=b:143065923
    TEST=tast run <ip> wilco.DeviceWilcoDtcAllowed
    
    Cq-Depend: chromium:2058956
    Change-Id: I6cb806ba5d6f5b66e37df37bb289fda7af209e3b

--- src/chromiumos/tast/remote/bundles/cros/main.go
+++ src/chromiumos/tast/remote/bundles/cros/main.go
@@ -20,6 +20,7 @@ import (
 	_ "chromiumos/tast/remote/bundles/cros/firmware"
 	_ "chromiumos/tast/remote/bundles/cros/hwsec"
 	_ "chromiumos/tast/remote/bundles/cros/meta"
+	_ "chromiumos/tast/remote/bundles/cros/policy"
 	_ "chromiumos/tast/remote/bundles/cros/power"
 	_ "chromiumos/tast/remote/bundles/cros/usbc"
 )
