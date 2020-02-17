0525bc4c2b3dcae186ef65f06e1826d32eef9a1f
Victor-Gabriel Savu <vsavu@google.com>
2020-02-17 13:48:54 +0100

    tast-tests: add policy service
    Add policy service to allow remote tests to enroll and set policies
    
    BUG=chromium:1052826
    TEST=as part of crrev.com/c/2011847
    
    Change-Id: I507c078b0fbb9635cd9852300f2a16944b035a60

---
+++ src/chromiumos/tast/local/bundles/cros/policy/policy_service.go
@@ -0,0 +1,211 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/policy/externaldata"
+	ppb "chromiumos/tast/services/cros/policy"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddService(&testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			ppb.RegisterPolicyServiceServer(srv, &PolicyService{
+				s:   s,
+				eds: nil,
+			})
+		},
+	})
+}
+
+// PolicyService implements tast.cros.policy.PolicyService.
+type PolicyService struct { // NOLINT
+	s *testing.ServiceState
+
+	chrome  *chrome.Chrome
+	fakeDMS *fakedms.FakeDMS
+
+	eds *externaldata.Server
+}
+
+func (c *PolicyService) EnrollUsingChrome(ctx context.Context, req *ppb.PolicyBlob) (*empty.Empty, error) {
+	testing.ContextLogf(ctx, "Enrolling using Chrome with policy %s", string(req.PolicyBlob))
+
+	tmpdir, err := ioutil.TempDir("", "fdms-")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create temp dir")
+	}
+
+	testing.ContextLogf(ctx, "fakedms using dir %s", tmpdir)
+
+	fdms, err := fakedms.New(ctx, tmpdir)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start FakeDMS")
+	}
+
+	c.fakeDMS = fdms
+
+	if err := fdms.WritePolicyBlobRaw(req.PolicyBlob); err != nil {
+		return nil, errors.Wrap(err, "failed to write policy blob")
+	}
+
+	authOpt := chrome.Auth("tast-user@managedchrome.com", "test0000", "gaia-id")
+	cr, err := chrome.New(ctx, authOpt, chrome.DMSPolicy(fdms.URL), chrome.EnterpriseEnroll())
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start chrome")
+	}
+
+	c.chrome = cr
+
+	return &empty.Empty{}, nil
+}
+
+func (c *PolicyService) UpdatePolicies(ctx context.Context, req *ppb.PolicyBlob) (*empty.Empty, error) {
+	// Write policies
+	if err := c.fakeDMS.WritePolicyBlobRaw(req.PolicyBlob); err != nil {
+		return nil, errors.Wrap(err, "failed to write policy blob")
+	}
+
+	tconn, err := c.chrome.TestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create test API connection")
+	}
+
+	// Refresh policies.
+	if err := tconn.EvalPromise(ctx, `tast.promisify(chrome.autotestPrivate.refreshEnterprisePolicies)();`, nil); err != nil {
+		return nil, errors.Wrap(err, "failed to refresh policies")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+func (c *PolicyService) CheckChrome(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	tconn, err := c.chrome.TestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create test API connection")
+	}
+
+	// Check Chrome
+	result := false
+	if err := tconn.Eval(ctx, "true", &result); err != nil {
+		return nil, errors.Wrap(err, "failed to interact with Chrome")
+	}
+	if !result {
+		return nil, errors.New("eval 'true' returned false")
+	}
+
+	// Check FakeDMS
+	if err := c.fakeDMS.Ping(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to ping FakeDMS")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+func (c *PolicyService) StopChrome(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	c.fakeDMS.Stop(ctx)
+
+	if err := c.chrome.Close(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to close chrome")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+func (c *PolicyService) StartExternalDataServer(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	if c.eds != nil {
+		return nil, errors.New("URL Policy server already started")
+	}
+
+	eds, err := externaldata.NewExternalDataServer(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start policy HTTP server")
+	}
+
+	c.eds = eds
+
+	return &empty.Empty{}, nil
+}
+
+func (c *PolicyService) StopExternalDataServer(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	if c.eds == nil {
+		return nil, errors.New("URL Policy server not started")
+	}
+
+	if err := c.eds.Stop(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to stop URL Policy server")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+func (c *PolicyService) ServePolicyData(ctx context.Context, req *ppb.ServePolicyDataRequest) (*ppb.ServePolicyDataResponse, error) {
+	if c.eds == nil {
+		return nil, errors.New("URL Policy server not started")
+	}
+
+	url, hash, err := c.eds.ServePolicyData(req.Contents)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to serve policy")
+	}
+
+	return &ppb.ServePolicyDataResponse{
+		Url:  url,
+		Hash: hash,
+	}, nil
+}
+
+func (c *PolicyService) EvalOnTestAPIConn(ctx context.Context, req *ppb.EvalOnTestAPIConnRequest) (*ppb.EvalOnTestAPIConnResponse, error) {
+	tconn, err := c.chrome.TestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create test API connection")
+	}
+
+	var result json.RawMessage
+	if err := tconn.Eval(ctx, req.Expression, &result); err != nil {
+		return nil, errors.Wrap(err, "failed to run javascript")
+	}
+
+	encoded, err := json.Marshal(result)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to encode result")
+	}
+
+	return &ppb.EvalOnTestAPIConnResponse{
+		Result: encoded,
+	}, nil
+}
+
+func (c *PolicyService) EvalPromiseOnTestAPIConn(ctx context.Context, req *ppb.EvalOnTestAPIConnRequest) (*ppb.EvalOnTestAPIConnResponse, error) {
+	tconn, err := c.chrome.TestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create test API connection")
+	}
+
+	var result json.RawMessage
+	if err := tconn.EvalPromise(ctx, req.Expression, &result); err != nil {
+		return nil, errors.Wrap(err, "failed to run javascript")
+	}
+
+	encoded, err := json.Marshal(result)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to encode result")
+	}
+
+	return &ppb.EvalOnTestAPIConnResponse{
+		Result: encoded,
+	}, nil
+}
