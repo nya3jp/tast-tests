23e53050ada6503010dcf8490af5c9bb6b59093a
Ian Barkley-Yeung <iby@google.com>
2020-02-18 10:06:34 -0800

    Get metrics consent via metrics_client
    Remove flakiness around setting metrics consent.
    
    When we set metrics consent in Chrome, it does (at least) two things --
    it touches the legacy consent file (/home/chronos/Consent To Send Stats)
    and it updates the policy file. The first is done instantly; the second
    is done asynchronously by queuing a task on a separate thread which then
    makes a dbus call to session manager.
    
    Before this change, the tast tests only checked that the legacy consent
    file had been created before proceeding with the test. But
    crash_reporter checks the policy files if they exist in preference to
    checking for the legacy consent files. This created a race condition.
    
    To avoid that, have the tests rely on the same consent checking code
    that crash_reporter relies on, instead of our own checks.
    "metrics_client -c" already runs the check we want, so just invoke that.
    
    BUG=chromium:1041062
    TEST=tast tested ui.ChromeCrash* platform.*Crash* and crash.*.
    Everything passed except some unrelated issues with
    ui.ChromeCrashLoop.breakpad and platform.UserCrash
    
    Change-Id: I103d637230123bea644d1f57f0cbf9fe7e2ab291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2063831
    Tested-by: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>

--- src/chromiumos/tast/local/metrics/metrics.go
+++ src/chromiumos/tast/local/metrics/metrics.go
@@ -5,30 +5,41 @@
 package metrics
 
 import (
-	"io/ioutil"
-	"os"
-	"regexp"
-	"strings"
+	"context"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
 )
 
 const (
-	legacyConsent = "/home/chronos/Consent To Send Stats"
+	metricsClientPath = "/usr/bin/metrics_client"
+	// metricsClientArg: "-c: return exit status 0 if user consents to stats,""
+	//                   "1 otherwise, in guest mode always return 1"
+	metricsClientArg = "-c"
+	// userConsents is the exit status if the user consents to stats.
+	userConsents = 0
+	// userDoesNotConsent is the exit status if the user does not consent to stats.
+	userDoesNotConsent = 1
 )
 
-var uuidPattern = regexp.MustCompile(`^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$`)
-
 // HasConsent checks if the system has metrics consent.
-func HasConsent() (bool, error) {
-	// Logic here should be in sync with ConsentId in libmetrics.
-	b, err := ioutil.ReadFile(legacyConsent)
-	if os.IsNotExist(err) {
+func HasConsent(ctx context.Context) (bool, error) {
+	// The C++ code reads the (possibly multiple) device policy files, and then
+	// falls back to previous files if the latest file isn't valid, and then falls
+	// back to enterprise enrollments and legacy consent files.
+	// Rather than try to reproduce all that in Go, call a C++ program that runs
+	// the exact same code that crash_reporter & crash_sender does.
+	err := testexec.CommandContext(ctx, metricsClientPath, metricsClientArg).Run()
+	code, ok := testexec.ExitCode(err)
+	if !ok {
+		return false, errors.Wrapf(err, "could not exec %s %s", metricsClientPath, metricsClientArg)
+	}
+	switch code {
+	case userConsents:
+		return true, nil
+	case userDoesNotConsent:
 		return false, nil
+	default:
+		return false, errors.Errorf("unexpected exit code from %s %s: %d", metricsClientPath, metricsClientArg, code)
 	}
-	if err != nil {
-		return false, errors.Wrapf(err, "failed to examine legacy consent file: %s", legacyConsent)
-	}
-	s := strings.TrimRight(string(b), "\n")
-	return uuidPattern.MatchString(s), nil
 }
