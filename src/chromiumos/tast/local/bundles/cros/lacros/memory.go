1bc43a873fe7b1071c9977adb0e916f7e56f731f
Erik Chen <erikchen@chromium.org>
2020-02-19 14:42:32 -0800

    lacros: Add youtube memory tast test.
    BUG=chromium:1049697
    TEST=tast run lacros.Memory.youtube
    Change-Id: I65e0ea155ff746a16c964b0f94ee54ea73fe421b

--- src/chromiumos/tast/local/bundles/cros/lacros/memory.go
+++ src/chromiumos/tast/local/bundles/cros/lacros/memory.go
@@ -40,6 +40,9 @@ func init() {
 		}, {
 			Name: "reddit",
 			Val:  "https://old.reddit.com/",
+		}, {
+			Name: "youtube",
+			Val:  "https://www.youtube.com/watch?v=uS33jC2VYNU",
 		},
 		},
 	})
