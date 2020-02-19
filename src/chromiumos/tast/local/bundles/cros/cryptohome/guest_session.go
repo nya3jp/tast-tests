2cb1a469f56fe6a59450a98f0b29fad8b3ffbe70
Jorge Lucangeli Obes <jorgelo@chromium.org>
2020-02-19 13:40:47 -0500

    GuestSession: Remove superfluous checks.
    The MountGuest function will actually check that the Guest vault was
    successfully mounted, so the checks here are 1. unnecessary and 2.
    break encapsulation. The test shouldn't know what the ephemeral mount
    path is, that knowledge should be encapsulated in the cryptohome
    module.
    
    Even without the extra checks this is a very useful test to ensure
    Guest sessions don't break, especially since we're making changes in
    this area.
    
    BUG=chromium:985492
    TEST=Still passes.
    
    Change-Id: I20fc2cf2dad1c6159141ff479307bfd820e568e3

--- src/chromiumos/tast/local/bundles/cros/cryptohome/guest_session.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/guest_session.go
@@ -6,12 +6,8 @@ package cryptohome
 
 import (
 	"context"
-	"strings"
-	"time"
 
-	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/cryptohome"
-	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
@@ -32,22 +28,4 @@ func GuestSession(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to mount guest: ", err)
 	}
 	defer cryptohome.UnmountVault(ctx, cryptohome.GuestUser)
-
-	// Use a shortened context for subprocesses.
-	shortCtx, shortCancel := ctxutil.Shorten(ctx, 5*time.Second)
-	defer shortCancel()
-
-	// Get list of mounts.
-	cmd := testexec.CommandContext(shortCtx, "mount")
-	output, err := cmd.Output()
-	if err != nil {
-		s.Fatal("Failed to get list of mounts: ", err)
-	}
-
-	// Check that an ephemeral mount exists.
-	mounts := string(output)
-	const ephemeralMountPoint string = "/run/cryptohome/ephemeral_mount"
-	if !strings.Contains(mounts, ephemeralMountPoint) {
-		s.Fatalf("Could not find %q in mounts", ephemeralMountPoint)
-	}
 }
