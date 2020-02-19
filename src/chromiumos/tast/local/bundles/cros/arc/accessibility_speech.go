2f95e92d9ec8706c94526b1ad37844433baf86d6
Sara Kato <sarakato@google.com>
2020-02-19 13:40:10 +0900

    tast-tests: Add ChromeVox+Next Key to a11ySpeech
    ChromeVox focus may moved using either Tab or Search+Right arrow.
    Previously, only the Tab key was used in the test, however both types
    of navigation should be used.
    
    BUG=b:149719277
    TEST=tast run DUT_IP arc.AccessibilitySpeech
    Change-Id: Ib79dda3981e6bbbe2c4893c1d8e7ab108d9e72fc

--- src/chromiumos/tast/local/bundles/cros/arc/accessibility_speech.go
+++ src/chromiumos/tast/local/bundles/cros/arc/accessibility_speech.go
@@ -44,7 +44,7 @@ func speechLog(ctx context.Context, chromeVoxConn *chrome.Conn) ([]string, error
 	}
 	var gotLogs []string
 	for _, log := range logs {
-		// TODO (crbug/i1053374):Investigate cause of empty string.
+		// TODO (crbug/1053374):Investigate cause of empty string.
 		if log.Text != "" {
 			gotLogs = append(gotLogs, log.Text)
 		}
@@ -68,27 +68,33 @@ func AccessibilitySpeech(ctx context.Context, s *testing.State) {
 			return errors.Wrap(err, "could not enable speech logging")
 		}
 
-		// Move focus to each of the UI elements, and check that ChromeVox log speaks as expected.
-		for _, wantLogs := range expectedSpeechLogs {
-			// Ensure that ChromeVox log is cleared before proceeding.
-			if err := chromeVoxConn.Exec(ctx, "LogStore.instance.clearLog()"); err != nil {
-				return errors.Wrap(err, "error with clearing ChromeVox log")
-			}
-			if err := ew.Accel(ctx, "Tab"); err != nil {
-				return errors.Wrap(err, "accel(Tab) returned error")
-			}
-			if err := testing.Poll(ctx, func(ctx context.Context) error {
-				gotLogs, err := speechLog(ctx, chromeVoxConn)
-				if err != nil {
-					return testing.PollBreak(err)
+		for _, chromeVoxNextKey := range []string{
+			"Search+Right",
+			"Tab",
+		} {
+			// Move focus to each of the UI elements, and check that ChromeVox log speaks as expected.
+			for _, wantLogs := range expectedSpeechLogs {
+				// Ensure that ChromeVox log is cleared before proceeding.
+				if err := chromeVoxConn.Exec(ctx, "LogStore.instance.clearLog()"); err != nil {
+					return errors.Wrap(err, "error with clearing ChromeVox log")
 				}
-				if diff := cmp.Diff(wantLogs, gotLogs); diff != "" {
-					return errors.Errorf("speech log was not as expected, diff is %q", diff)
+				if err := ew.Accel(ctx, chromeVoxNextKey); err != nil {
+					return errors.Wrapf(err, "accel(%s) returned error", chromeVoxNextKey)
 				}
-				return nil
-			}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
-				return errors.Wrap(err, "failed to check speech log")
+				if err := testing.Poll(ctx, func(ctx context.Context) error {
+					gotLogs, err := speechLog(ctx, chromeVoxConn)
+					if err != nil {
+						return testing.PollBreak(err)
+					}
+					if diff := cmp.Diff(wantLogs, gotLogs); diff != "" {
+						return errors.Errorf("speech log was not as expected, diff is %q", diff)
+					}
+					return nil
+				}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+					return errors.Wrap(err, "failed to check speech log")
+				}
 			}
+
 		}
 		return nil
 	})
