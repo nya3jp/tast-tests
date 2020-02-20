88caef06b4c1c7682c4807e247c845829fbfa5f1
Clark Chung <clarkchung@google.com>
2020-02-20 20:00:07 +0800

    tast-tests: add network tests for runtime probe
    This CL tests if the network component name in probe result matches the
    cros-label which is decoded by hwid service.  For short-term plan, it is
    integrated with test_that cmdline that will pass the filename of
    cros-labels by |-varsfile| option.
    
    BUG=b:143126249,b:148995867
    TEST=(for wilco device) $ tast -verbose run \
         >'-var=autotest_host_info_labels='\
         >'["hwid_component:wireless/<WIRELESS COMPONENT NAME>",'\
         >'"hwid_component:cellular/<CELLULAR NAME>",'\
         >'"model:<MODEL NAME>"]' \
         >$ACTIVE_DUT platform.CrosRuntimeProbeNetwork
    
    Cq-Depend: chromium:2064647,chromium:2043627
    Change-Id: Iae2e38035b1edaed6154b3e4c9a72e4e08491924

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_runtime_probe_network.go
@@ -0,0 +1,166 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"encoding/json"
+	"sort"
+	"strings"
+
+	"github.com/godbus/dbus"
+
+	rppb "chromiumos/system_api/runtime_probe_proto"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+var networkTypes = []string{"wireless", "cellular", "ethernet"}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosRuntimeProbeNetwork,
+		Desc: "Checks that network probe results are expected",
+		Contacts: []string{
+			"ckclark@chromium.org",
+			"chromeos-runtime-probe@google.com",
+		},
+		Attr:         []string{"group:runtime_probe"},
+		SoftwareDeps: []string{"wilco"},
+		Vars:         []string{"autotest_host_info_labels"},
+	})
+}
+
+// networkNameMapping will extract the model and network names (prefixed with
+// "hwid_component:<network type>/") from autotest_host_info_labels var which
+// is a json string of list of cros-labels.  After collecting network names,
+// this function will return a map of set containing them by network type.
+func networkNameMapping(jsonStr string) (map[string]map[string]struct{}, error) {
+	const modelPrefix = "model:"
+	mapping := make(map[string]map[string]struct{})
+	for _, networkType := range networkTypes {
+		mapping[networkType] = make(map[string]struct{})
+	}
+
+	var labels []string
+	if err := json.Unmarshal([]byte(jsonStr), &labels); err != nil {
+		return nil, err
+	}
+	// Find the model name of this DUT.
+	var model string
+	for _, label := range labels {
+		if strings.HasPrefix(label, modelPrefix) {
+			model = strings.TrimPrefix(label, modelPrefix)
+			break
+		}
+	}
+	if len(model) == 0 {
+		return nil, errors.New("no model found")
+	}
+
+	// Filter labels with prefix "hwid_component:<network type>/" and trim them.
+	for _, label := range labels {
+		for _, networkType := range networkTypes {
+			networkPrefix := "hwid_component:" + networkType + "/"
+			if strings.HasPrefix(label, networkPrefix) {
+				label := strings.TrimPrefix(label, networkPrefix)
+				mapping[networkType][model+"_"+label] = struct{}{}
+				break
+			}
+		}
+	}
+
+	return mapping, nil
+}
+
+// networkComponents uses D-Bus call to get network components from runtime_probe.
+// Currently only users |chronos| and |debugd| are allowed to call this D-Bus function.
+func networkComponents(ctx context.Context) ([]*rppb.Network, error) {
+	const (
+		// Define the D-Bus constants here.
+		// Note that this is for the reference only to demonstrate how
+		// to use dbusutil. For actual use, session_manager D-Bus call
+		// should be performed via
+		// chromiumos/tast/local/session_manager package.
+		jobName       = "runtime_probe"
+		dbusName      = "org.chromium.RuntimeProbe"
+		dbusPath      = "/org/chromium/RuntimeProbe"
+		dbusInterface = "org.chromium.RuntimeProbe"
+		dbusMethod    = dbusInterface + ".ProbeCategories"
+	)
+
+	if err := upstart.EnsureJobRunning(ctx, jobName); err != nil {
+		return nil, errors.Wrap(err, "runtime probe is not running")
+	}
+	defer upstart.StopJob(ctx, jobName)
+
+	conn, obj, err := dbusutil.ConnectPrivateWithAuth(ctx, sysutil.ChronosUID, dbusName, dbus.ObjectPath(dbusPath))
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	request := rppb.ProbeRequest{
+		Categories: []rppb.ProbeRequest_SupportCategory{
+			rppb.ProbeRequest_network,
+		},
+	}
+	result := rppb.ProbeResult{}
+
+	if err := dbusutil.CallProtoMethod(ctx, obj, dbusMethod, &request, &result); err != nil {
+		return nil, errors.Wrapf(err, "failed to call method %s", dbusMethod)
+	}
+	return result.GetNetwork(), nil
+}
+
+// CrosRuntimeProbeNetwork checks if the network names in cros-label are
+// consistent with probed names from runtime_probe.
+func CrosRuntimeProbeNetwork(ctx context.Context, s *testing.State) {
+	labelsStr, ok := s.Var("autotest_host_info_labels")
+	if !ok {
+		s.Fatal("No network labels")
+	}
+
+	mapping, err := networkNameMapping(labelsStr)
+	if err != nil {
+		s.Fatal("Unable to decode autotest_host_info_labels: ", err)
+	}
+
+	probedNetworkComponents, err := networkComponents(ctx)
+	if err != nil {
+		s.Fatal("Cannot get network components: ", err)
+	}
+
+	for _, component := range probedNetworkComponents {
+		name := component.GetName()
+		values := component.GetValues()
+		networkType := values.GetType()
+		if name == "generic" {
+			s.Log("Skip known generic probe result")
+		} else {
+			s.Log("Probed network:", name)
+			if _, exists := mapping[networkType][name]; !exists {
+				s.Logf("Extra network component %v of type %v is probed", name, networkType)
+			} else {
+				delete(mapping[networkType], name)
+			}
+		}
+	}
+	var unprobedNetworks []string
+	for networkType := range mapping {
+		if len(mapping[networkType]) > 0 {
+			for name := range mapping[networkType] {
+				unprobedNetworks = append(unprobedNetworks, networkType+"/"+name)
+			}
+		}
+	}
+	if len(unprobedNetworks) > 0 {
+		sort.Strings(unprobedNetworks)
+		s.Fatal("Some expected network components are not probed: ", unprobedNetworks)
+	}
+}
