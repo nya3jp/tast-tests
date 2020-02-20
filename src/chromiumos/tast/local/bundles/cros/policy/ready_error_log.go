8efcaf760be433352d7c7499c603f4a04a77458b
Victor-Gabriel Savu <vsavu@google.com>
2020-02-20 15:22:19 +0100

    tast: add clearPolicies to ready.go
    Add clearPolicies to ready.go to make sure no policies are set when
    tast starts running tests. Also adds test to check if the removal
    failed.
    
    BUG=chromium:1054004
    TEST=test_that <ip> policy_DeviceServer.EphemeralUser_true && tast run <ip> hwsec.ChallengeResponseMount
    
    Change-Id: I31dc8cf3b034ae4c81bcd246d67b97b0970a9cf0

---
+++ src/chromiumos/tast/local/bundles/cros/policy/ready_error_log.go
@@ -0,0 +1,44 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+
+	"chromiumos/tast/local/ready"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ReadyErrorLog,
+		Desc: "Check that ready.go successfully removed existing policies",
+		Contacts: []string{
+			"vsavu@google.com", // Test author
+			"nya@chromium.org",
+		},
+		SoftwareDeps: []string{},
+		Attr:         []string{"group:mainline", "informational"},
+	})
+}
+
+func ReadyErrorLog(ctx context.Context, s *testing.State) {
+	if _, err := os.Stat(ready.ClearPoliciesLogLocation); err != nil {
+		if os.IsNotExist(err) {
+			return // missing file means no error
+		}
+
+		s.Fatalf("Failed to check for the existence of %q", ready.ClearPoliciesLogLocation)
+	}
+
+	contents, err := ioutil.ReadFile(ready.ClearPoliciesLogLocation)
+	if err != nil {
+		s.Fatalf("Failed to open %q: %v", ready.ClearPoliciesLogLocation, err)
+	}
+
+	s.Errorf("Found error log for ready.go: %s", string(contents))
+}
