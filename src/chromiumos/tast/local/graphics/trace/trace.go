f15bbb767da136f08e0b604e18ca40d11b87c00f
Po-Hsien Wang <pwang@chromium.org>
2020-02-21 16:57:43 -0800

    graphics: trace: Fix perf issue when running multiple test
    perf.Save will override existing perf file whenever it is invoked.
    Move perf.Save out of the loop so that we only write the file at the end
    of the test.
    
    BUG=None
    TEST=Manually change the test to run twice and check result-chart.json
    
    Change-Id: I491a45b5724f318d23f68a0c12a6720883d3ee70

--- src/chromiumos/tast/local/graphics/trace/trace.go
+++ src/chromiumos/tast/local/graphics/trace/trace.go
@@ -63,29 +63,54 @@ func RunTest(ctx context.Context, s *testing.State, cont *vm.Container, traces m
 		cmd.DumpLog(shortCtx)
 		s.Fatal("Failed to get apitrace: ", err)
 	}
+
+	perfValues := perf.NewValues()
 	for traceFile, traceName := range traces {
 		shortCtx, st := timing.Start(shortCtx, "trace:"+traceName)
 		defer st.End()
-		perfValues, err := runTrace(shortCtx, cont, s.DataPath(traceFile), traceName)
+		output, err := runTrace(shortCtx, cont, s.DataPath(traceFile), traceName)
 		if err != nil {
 			s.Fatal("Failed running trace: ", err)
 		}
-		if err := perfValues.Save(s.OutDir()); err != nil {
-			s.Fatal("Failed saving perf data: ", err)
+		frames, duration, fps, err := parseResult(output)
+		if err != nil {
+			s.Fatal("Failed to parse trace replay output: ", err)
 		}
+
+		perfValues.Set(perf.Metric{
+			Name:      traceName,
+			Variant:   "time",
+			Unit:      "sec",
+			Direction: perf.SmallerIsBetter,
+		}, duration)
+		perfValues.Set(perf.Metric{
+			Name:      traceName,
+			Variant:   "frames",
+			Unit:      "frame",
+			Direction: perf.BiggerIsBetter,
+		}, float64(frames))
+		perfValues.Set(perf.Metric{
+			Name:      traceName,
+			Variant:   "fps",
+			Unit:      "fps",
+			Direction: perf.BiggerIsBetter,
+		}, fps)
+	}
+	if err := perfValues.Save(s.OutDir()); err != nil {
+		s.Fatal("Failed saving perf data: ", err)
 	}
 }
 
 // runTrace runs a trace and writes output to ${traceName}.txt. traceFile should be absolute path.
-func runTrace(ctx context.Context, cont *vm.Container, traceFile, traceName string) (*perf.Values, error) {
+func runTrace(ctx context.Context, cont *vm.Container, traceFile, traceName string) (string, error) {
 	containerPath := filepath.Join("/tmp", filepath.Base(traceFile))
 	if err := cont.PushFile(ctx, traceFile, containerPath); err != nil {
-		return nil, errors.Wrap(err, "failed copying trace file to container")
+		return "", errors.Wrap(err, "failed copying trace file to container")
 	}
 
 	containerPath, err := decompressTrace(ctx, cont, containerPath)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	testing.ContextLog(ctx, "Replaying trace file ", filepath.Base(containerPath))
@@ -105,15 +130,15 @@ func runTrace(ctx context.Context, cont *vm.Container, traceFile, traceName stri
 
 	outDir, ok := testing.ContextOutDir(ctx)
 	if !ok {
-		return nil, errors.New("failed to get OutDir for writing trace result")
+		return "", errors.New("failed to get OutDir for writing trace result")
 	}
 	// Suggesting the file is human readable by appending txt extension.
 	file := filepath.Join(outDir, logDir, traceName+".txt")
 	testing.ContextLog(ctx, "Dumping trace output to file ", filepath.Base(file))
 	if err := ioutil.WriteFile(file, traceOut, 0644); err != nil {
-		return nil, errors.Wrap(err, "error writing tracing output")
+		return "", errors.Wrap(err, "error writing tracing output")
 	}
-	return parseResult(traceName, string(traceOut))
+	return string(traceOut), nil
 }
 
 // decompressTrace trys to decompress the trace into trace format if possible. If the input is uncompressed, this function will do nothing.
@@ -144,47 +169,27 @@ func decompressTrace(ctx context.Context, cont *vm.Container, traceFile string) 
 	return decompressFile, nil
 }
 
-// parseResult parses the output of apitrace and return the perfs.
-func parseResult(traceName, output string) (*perf.Values, error) {
+// parseResult parses the output of apitrace and return the statistic.
+func parseResult(output string) (uint64, float64, float64, error) {
 	re := regexp.MustCompile(`Rendered (\d+) frames in (\d*\.?\d*) secs, average of (\d*\.?\d*) fps`)
 	match := re.FindStringSubmatch(output)
 	if match == nil {
-		return nil, errors.New("result line can't be located")
+		return 0, 0, 0, errors.New("result line can't be located")
 	}
 
 	frames, err := strconv.ParseUint(match[1], 10, 64)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to parse frames %q", match[1])
+		return 0, 0, 0, errors.Wrapf(err, "failed to parse frames %q", match[1])
 	}
 	duration, err := strconv.ParseFloat(match[2], 64)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to parse duration %q", match[2])
+		return 0, 0, 0, errors.Wrapf(err, "failed to parse duration %q", match[2])
 	}
 	fps, err := strconv.ParseFloat(match[3], 64)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to parse fps %q", match[3])
+		return 0, 0, 0, errors.Wrapf(err, "failed to parse fps %q", match[3])
 	}
-
-	value := perf.NewValues()
-	value.Set(perf.Metric{
-		Name:      traceName,
-		Variant:   "time",
-		Unit:      "sec",
-		Direction: perf.SmallerIsBetter,
-	}, duration)
-	value.Set(perf.Metric{
-		Name:      traceName,
-		Variant:   "frames",
-		Unit:      "frame",
-		Direction: perf.BiggerIsBetter,
-	}, float64(frames))
-	value.Set(perf.Metric{
-		Name:      traceName,
-		Variant:   "fps",
-		Unit:      "fps",
-		Direction: perf.BiggerIsBetter,
-	}, fps)
-	return value, nil
+	return frames, duration, fps, nil
 }
 
 // TODO(pwang): Write a func to cleans up disk in best effort.
