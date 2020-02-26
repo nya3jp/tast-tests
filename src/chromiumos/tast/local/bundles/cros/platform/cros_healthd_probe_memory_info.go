9b4481362cb275aa7c3ee58deed8884c8a0ac793
Paul Moy <pmoy@chromium.org>
2020-02-26 11:55:52 -0700

    tast: add CrosHealthdProbeMemoryInfo
    Add a new tast test that exercises cros_healthd's memory
    metric collection.
    
    BUG=chromium:1054198
    TEST=tast run --build $IP platform.CrosHealthdProbeMemoryInfo
    
    Change-Id: I86725566c82c405e30b28ea46a3798efed9fd0e1

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_memory_info.go
@@ -0,0 +1,77 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosHealthdProbeMemoryInfo,
+		Desc: "Check that we can probe cros_healthd for memory info",
+		Contacts: []string{
+			"jschettler@google.com",
+			"pmoy@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"diagnostics"},
+	})
+}
+
+func CrosHealthdProbeMemoryInfo(ctx context.Context, s *testing.State) {
+	if err := upstart.EnsureJobRunning(ctx, "cros_healthd"); err != nil {
+		s.Fatal("Failed to start cros_healthd: ", err)
+	}
+
+	b, err := testexec.CommandContext(ctx, "telem", "--category=memory").Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to run 'telem --category=memory': ", err)
+	}
+
+	// Log output to file for debugging.
+	if err := ioutil.WriteFile(filepath.Join(s.OutDir(), "command_output.txt"), b, 0644); err != nil {
+		s.Errorf("Failed to write output to %s: %v", filepath.Join(s.OutDir(), "command_output.txt"), err)
+	}
+
+	lines := strings.Split(strings.TrimRight(string(b), "\n"), "\n")
+	if len(lines) != 2 {
+		s.Fatalf("Wrong number of lines: got %v, want 2", len(lines))
+	}
+
+	want := []string{"total_memory_kib", "free_memory_kib", "available_memory_kib",
+		"page_faults_since_last_boot"}
+	got := strings.Split(lines[0], ",")
+	if !reflect.DeepEqual(want, got) {
+		s.Fatalf("Incorrect headers: got %v, want %v", got, want)
+	}
+
+	metrics := strings.Split(lines[1], ",")
+	if len(metrics) != len(want) {
+		s.Fatalf("Incorrect number of memory metrics: got %d; want %d", len(metrics), len(want))
+	}
+
+	// Each memory metric should be a positive integer. This assumes that all
+	// machines will always have at least 1 free KiB of memory, and all machines
+	// will have page faulted at least once between boot and the time this test
+	// finishes executing.
+	for i := 0; i < len(metrics); i++ {
+		val, err := strconv.Atoi(metrics[i])
+		if err != nil {
+			s.Errorf("Failed to convert %s to integer: %v", want[i], err)
+		} else if val <= 0 {
+			s.Errorf("Invalid %s", want[i])
+		}
+	}
+}
