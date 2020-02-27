ef71998642671e57f36e23952428bb0968eb3b50
Yuichiro Hanada <yhanada@google.com>
2020-02-27 22:27:18 +0900

    [WIP] Add a tast test for VK with focus switching.
    When the input focus moves between EditTexts, Chrome OS virtual keyboard
    should remain shown.
    
    BUG=b:148483295
    TEST=tast run ${DUT} arc.ChromeVirtualKeyboard
    
    Change-Id: I2f33d9213b43032e3c59d4ed333ae0d4136bb682

---
+++ android/ArcKeyboardTest/src/org/chromium/arc/testapp/keyboard/FocusChangeTestActivity.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.keyboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FocusChangeTestActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.focus_change_test_activity);
+
+        final Button focus_switch_button = findViewById(R.id.focus_switch_button);
+        focus_switch_button.setOnClickListener(v -> {
+            View text1 = findViewById(R.id.text1);
+            if (!text1.isFocused()) {
+                text1.requestFocus();
+                return;
+            }
+
+            View text2 = findViewById(R.id.text2);
+            if (!text2.isFocused()) {
+                text2.requestFocus();
+                return;
+            }
+        });
+    }
+}
