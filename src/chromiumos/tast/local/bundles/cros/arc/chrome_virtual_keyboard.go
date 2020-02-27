c1ae0ba8f61e5a8d735249c7ee454368937211c8
Yuichiro Hanada <yhanada@google.com>
2020-02-27 22:27:18 +0900

    Add a tast test for VK with focus switching.
    When the input focus moves between EditTexts, Chrome OS virtual keyboard
    should remain shown.
    
    BUG=b:148483295
    TEST=tast run ${DUT} arc.ChromeVirtualKeyboard
    
    Change-Id: I2f33d9213b43032e3c59d4ed333ae0d4136bb682

--- src/chromiumos/tast/local/bundles/cros/arc/chrome_virtual_keyboard.go
+++ src/chromiumos/tast/local/bundles/cros/arc/chrome_virtual_keyboard.go
@@ -10,6 +10,7 @@ import (
 
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/vkb"
 	"chromiumos/tast/testing"
 )
@@ -32,41 +33,28 @@ func init() {
 	})
 }
 
-func ChromeVirtualKeyboard(ctx context.Context, s *testing.State) {
+// editingTest tests basic editing on a EditText on an ARC++ app by Chrome's virtual keyboard.
+func editingTest(ctx context.Context, tconn *chrome.TestConn, d *ui.Device, s *testing.State) {
 	const (
-		apk = "ArcKeyboardTest.apk"
-		pkg = "org.chromium.arc.testapp.keyboard"
-		cls = "org.chromium.arc.testapp.keyboard.MainActivity"
+		pkg          = "org.chromium.arc.testapp.keyboard"
+		activityName = ".MainActivity"
 
 		fieldID = "org.chromium.arc.testapp.keyboard:id/text"
 	)
+	a := s.PreValue().(arc.PreData).ARC
+	cr := s.PreValue().(arc.PreData).Chrome
 
-	p := s.PreValue().(arc.PreData)
-	cr := p.Chrome
-	a := p.ARC
-
-	tconn, err := cr.TestAPIConn(ctx)
+	act, err := arc.NewActivity(a, pkg, activityName)
 	if err != nil {
-		s.Fatal("Creating test API connection failed: ", err)
+		s.Fatalf("Failed to create a new activity %q", activityName)
 	}
+	defer act.Close()
 
-	d, err := ui.NewDevice(ctx, a)
-	if err != nil {
-		s.Fatal("Failed initializing UI Automator: ", err)
+	if err := act.Start(ctx); err != nil {
+		s.Fatalf("Failed to start the activity %q", activityName)
 	}
-	defer d.Close()
+	defer act.Stop(ctx)
 
-	s.Log("Starting app")
-
-	if err := a.Install(ctx, arc.APKPath(apk)); err != nil {
-		s.Fatal("Failed installing app: ", err)
-	}
-
-	if err := a.Command(ctx, "am", "start", "-W", pkg+"/"+cls).Run(); err != nil {
-		s.Fatal("Failed starting app: ", err)
-	}
-
-	s.Log("Setting up app's initial state")
 	field := d.Object(ui.ID(fieldID))
 	if err := field.WaitForExists(ctx, 30*time.Second); err != nil {
 		s.Fatal("Failed to find field: ", err)
@@ -125,4 +113,109 @@ func ChromeVirtualKeyboard(ctx context.Context, s *testing.State) {
 			}
 		}
 	}
+
+	if err := vkb.HideVirtualKeyboard(ctx, tconn); err != nil {
+		s.Fatal("Failed to hide the virtual keyboard: ", err)
+	}
+}
+
+// focusChangeTest tests the virtual keyboard behavior when the focus moves programmatically.
+func focusChangeTest(ctx context.Context, tconn *chrome.TestConn, d *ui.Device, s *testing.State) {
+	const (
+		pkg          = "org.chromium.arc.testapp.keyboard"
+		activityName = ".FocusChangeTestActivity"
+
+		buttonID = "org.chromium.arc.testapp.keyboard:id/focus_switch_button"
+		fieldID1 = "org.chromium.arc.testapp.keyboard:id/text1"
+		fieldID2 = "org.chromium.arc.testapp.keyboard:id/text2"
+	)
+	a := s.PreValue().(arc.PreData).ARC
+
+	act, err := arc.NewActivity(a, pkg, activityName)
+	if err != nil {
+		s.Fatalf("Failed to create a new activity %q", activityName)
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx); err != nil {
+		s.Fatalf("Failed to start the activity %q", activityName)
+	}
+	defer act.Stop(ctx)
+
+	// Focusing on the text field programmatically should not show the virtual keyboard.
+	button := d.Object(ui.ID(buttonID))
+	if err := button.WaitForExists(ctx, 30*time.Second); err != nil {
+		s.Fatal("Failed to find the button: ", err)
+	}
+	if err := button.Click(ctx); err != nil {
+		s.Fatal("Failed to click the button: ", err)
+	}
+	if err := d.Object(ui.ID(fieldID1), ui.Focused(true)).WaitForExists(ctx, 30*time.Second); err != nil {
+		s.Fatal("Pressing the button didn't cause focusing on the field: ", err)
+	}
+	if shown, err := vkb.IsShown(ctx, tconn); shown || err != nil {
+		s.Fatal("The virtual keyboard is shown without any user action: ", err)
+	}
+
+	// Clicking on the text field should show the virtual keyboard.
+	field1 := d.Object(ui.ID(fieldID1))
+	if err := field1.WaitForExists(ctx, 30*time.Second); err != nil {
+		s.Fatal("Failed to find the field: ", err)
+	}
+	if err := field1.Click(ctx); err != nil {
+		s.Fatal("Failed to click the field: ", err)
+	}
+
+	s.Log("Waiting for the virtual keyboard to be ready")
+	if err := vkb.WaitUntilShown(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for the virtual keyboard to show: ", err)
+	}
+	if err := vkb.WaitUntilButtonsRender(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for the virtual keyboard to render: ", err)
+	}
+
+	// Thanks to TransienBlur feature, the virtual keyboard should keep showing
+	// when the focus is moved between the text fields programmatically.
+	s.Log("Clicking the button to switch the focus")
+	if err := button.Click(ctx); err != nil {
+		s.Fatal("Failed to click the button: ", err)
+	}
+	if err := d.Object(ui.ID(fieldID2), ui.Focused(true)).WaitForExists(ctx, 30*time.Second); err != nil {
+		s.Fatal("Clicking the button didn't cause the focus move: ", err)
+	}
+	if shown, err := vkb.IsShown(ctx, tconn); !shown || err != nil {
+		s.Fatal("The focus move makes the virtual keyboard to be hidden: ", err)
+	}
+
+	if err := vkb.HideVirtualKeyboard(ctx, tconn); err != nil {
+		s.Fatal("Failed to hide the virtual keyboard: ", err)
+	}
+}
+
+func ChromeVirtualKeyboard(ctx context.Context, s *testing.State) {
+	const (
+		apk = "ArcKeyboardTest.apk"
+	)
+
+	p := s.PreValue().(arc.PreData)
+	a := p.ARC
+
+	tconn, err := p.Chrome.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+	defer tconn.Close()
+
+	d, err := ui.NewDevice(ctx, a)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	defer d.Close()
+
+	if err := a.Install(ctx, arc.APKPath(apk)); err != nil {
+		s.Fatal("Failed installing app: ", err)
+	}
+
+	editingTest(ctx, tconn, d, s)
+	focusChangeTest(ctx, tconn, d, s)
 }
