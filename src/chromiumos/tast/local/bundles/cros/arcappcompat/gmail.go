1db3ec2fe4df2f2a9ee40b94ac63a8fc3cac8b67
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-02-28 16:28:45 -0800

    Run UIAutomator Test Using Tast For Gmail app
    BUG=b:150012230
    TEST=None
    
    Tast Test for Gmail app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: Iae5441f1fb11bcedeeb8c814ff28363dca06d70c

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/gmail.go
@@ -0,0 +1,157 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForGmail = []testutil.TestSuite{
+	{"Launch app in Clamshell", launchAppForGmail},
+	{"Clamshell: Fullscreen app", testutil.ClamshellFullscreenApp},
+	{"Clamshell: Minimise and Restore", testutil.MinimizeRestoreApp},
+	{"Clamshell: Resize window", testutil.ClamshellResizeWindow},
+	{"Clamshell: Reopen app", testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForGmail = []testutil.TestSuite{
+	{"Launch app in Touchview", launchAppForGmail},
+	{"Touchview: Minimise and Restore", testutil.MinimizeRestoreApp},
+	{"Touchview: Reopen app", testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Gmail,
+		Desc:         "Functional test for Gmail that installs the app also verifies it is logged in and that the main page is open, checks Gmail correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val: testutil.TestParams{
+				false,
+				clamshellTestsForGmail,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               arc.BootedAppCompat(),
+		}, {
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				true,
+				touchviewTestsForGmail,
+			},
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               arc.BootedInTabletModeAppCompat(),
+		}, {
+			Name: "vm",
+			Val: testutil.TestParams{
+				false,
+				clamshellTestsForGmail,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               arc.VMBootedAppCompat(),
+		}, {
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				true,
+				touchviewTestsForGmail,
+			},
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               arc.VMBootedInTabletModeAppCompat(),
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// Gmail test uses library for opting into the playstore and installing app.
+// Checks Gmail correctly changes the window states in both clamshell and touchview mode.
+func Gmail(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.google.android.gm"
+		appActivity = "com.google.android.gm.ConversationListActivityGmail"
+	)
+
+	// StepUp chrome on Chromebook.
+	cr, tconn, a, d := testutil.SetUpDevice(ctx, s, appPkgName, appActivity)
+
+	testSet := s.Param().(testutil.TestParams)
+	// Run the different test cases.
+	for idx, test := range testSet.Tests {
+		// Run subtests.
+		s.Run(ctx, test.Name, func(ctx context.Context, s *testing.State) {
+			defer func() {
+				if s.HasError() {
+					path := fmt.Sprintf("%s/screenshot-arcappcompat-failed-test-%d.png", s.OutDir(), idx)
+					if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
+						s.Log("Failed to capture screenshot: ", err)
+					}
+				}
+			}()
+			s.Log("Launch the app")
+			testutil.LaunchHelper(ctx, s, a, d, appPkgName, appActivity)
+			test.Fn(ctx, s, tconn, a, d, appPkgName, appActivity)
+		})
+	}
+}
+
+// launchAppForGmail verifies Gmail is logged in and
+// verify Gmail reached main activity page of the app.
+func launchAppForGmail(ctx context.Context, s *testing.State, tconn *chrome.Conn, a *arc.ARC, d *ui.Device, appPkgName string, appActivity string) {
+
+	const (
+		composeIconClassName    = "android.widget.ImageButton"
+		composeIconDescription  = "Compose"
+		textViewClassName       = "android.widget.TextView"
+		gotItButtonText         = "GOT IT"
+		takeMeToGmailButtonText = "TAKE ME TO GMAIL"
+		userNameID              = "com.google.android.gm:id/account_address"
+	)
+
+	// Click on Got It button.
+	//testutil.CurrentAppPackage(ctx, s, d)
+	GotItButton := d.Object(ui.ClassName(textViewClassName), ui.Text(gotItButtonText))
+	if err := GotItButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("GotIt Button doesn't exist: ", err)
+	} else {
+		if err := GotItButton.Click(ctx); err != nil {
+			s.Fatal("Failed to click on GotItButton: ", err)
+		}
+	}
+
+	// Check app is logged in with username.
+	userName := d.Object(ui.ID(userNameID))
+	if err := userName.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("Username does not exist: ", err)
+	}
+
+	// Click on TAKE ME TO GMAIL button.
+	takeMeToGmailButton := d.Object(ui.ClassName(textViewClassName), ui.Text(takeMeToGmailButtonText))
+	if err := takeMeToGmailButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Log("TAKE ME TO GMAIL Button doesn't exist: ", err)
+	} else {
+		if err := takeMeToGmailButton.Click(ctx); err != nil {
+			s.Fatal("Failed to click on takeMeToGmailButton: ", err)
+		}
+	}
+
+	// Check for compose icon in home page.
+	composeIcon := d.Object(ui.ClassName(composeIconClassName), ui.DescriptionContains(composeIconDescription))
+	if err := composeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("Failed to click on composeIcon: ", err)
+	}
+}
---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/testutil/testutil.go
@@ -0,0 +1,346 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package testutil contains functionality shared by tast tests for android apps on Chromebooks.
+package testutil
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/playstore"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+// Variables used by other tast tests
+const (
+	AndroidButtonClassName = "android.widget.Button"
+
+	DefaultUITimeout = 20 * time.Second
+	LongUITimeout    = 5 * time.Minute
+)
+
+// TestFunc represents the "test" function.
+type TestFunc func(ctx context.Context, s *testing.State, tconn *chrome.Conn, a *arc.ARC, d *ui.Device, appPkgName string, appActivity string)
+
+// TestSuite represents the  name of test, and the function to call.
+type TestSuite struct {
+	Name string
+	Fn   TestFunc
+}
+
+// TestParams represents the collection of tests to run in tablet mode or clamshell mode.
+type TestParams struct {
+	TabletMode bool
+	Tests      []TestSuite
+}
+
+// SetUpDevice func setup Chrome on Chromebook.
+func SetUpDevice(ctx context.Context, s *testing.State, appPkgName string, appActivity string) (*chrome.Chrome, *chrome.Conn, *arc.ARC, *ui.Device) {
+
+	// Setup Chrome.
+	cr := s.PreValue().(arc.PreData).Chrome
+	a := s.PreValue().(arc.PreData).ARC
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+	d, err := ui.NewDevice(ctx, a)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	s.Log("Enable showing ANRs")
+	if err := a.Command(ctx, "settings", "put", "secure", "anr_show_background", "1").Run(testexec.DumpLogOnError); err != nil {
+		s.Error("Failed to enable showing ANRs: ", err)
+	}
+	s.Log("Enable crash dialog")
+	if err := a.Command(ctx, "settings", "put", "secure", "show_first_crash_dialog_dev_option", "1").Run(testexec.DumpLogOnError); err != nil {
+		s.Error("Failed to enable crash dialog: ", err)
+	}
+
+	s.Log("Installing app")
+	if err := apps.Launch(ctx, tconn, apps.PlayStore.ID); err != nil {
+		s.Fatal("Failed to launch Play Store: ", err)
+	}
+	if err := playstore.InstallApp(ctx, a, d, appPkgName); err != nil {
+		s.Fatal("Failed to install app: ", err)
+	}
+	return cr, tconn, a, d
+}
+
+// LaunchHelper helps to launch the installed app.
+func LaunchHelper(ctx context.Context, s *testing.State, a *arc.ARC, d *ui.Device, appPkgName string, appActivity string) {
+	act, err := arc.NewActivity(a, appPkgName, appActivity)
+	if err != nil {
+		s.Fatal("Failed to create new app activity: ", err)
+	} else {
+		s.Log("Created new app activity")
+	}
+	defer act.Close()
+	if err := act.Start(ctx); err != nil {
+
+		s.Fatal("Failed start app: ", err)
+	} else {
+		s.Log("App launched successfully")
+	}
+}
+
+// ClamshellFullscreenApp Test launches the app in full screen window and verifies launch successfully without crash or ANR.
+func ClamshellFullscreenApp(ctx context.Context, s *testing.State, tconn *chrome.Conn, a *arc.ARC, d *ui.Device, appPkgName string, appActivity string) {
+	const (
+		restartButtonResourceID = "android:id/button1"
+	)
+	s.Log("Set the window to fullscreen")
+	if _, err := ash.SetARCAppWindowState(ctx, tconn, appPkgName, ash.WMEventFullscreen); err != nil {
+		s.Error(" Failed to set the window to fullscreen: ", err)
+	}
+	if err := ash.WaitForARCAppWindowState(ctx, tconn, appPkgName, ash.WindowStateFullscreen); err != nil {
+		s.Error("The window is not in fullscreen: ", err)
+	}
+
+	nApp := isNApp(ctx, s, tconn, a, d, appPkgName)
+	if !nApp {
+		s.Log("It's a pre N-app")
+		// Click on restart button.
+		restartButton := d.Object(ui.ResourceID(restartButtonResourceID))
+		if err := restartButton.WaitForExists(ctx, LongUITimeout); err != nil {
+			s.Fatal("Restart button does not exist: ", err)
+		}
+		if err := restartButton.Click(ctx); err != nil {
+			s.Fatal("Failed to click on restart button: ", err)
+		}
+
+		if _, err := d.WaitForWindowUpdate(ctx, appPkgName, LongUITimeout); err != nil {
+			s.Fatal("Failed to wait for the window to update: ", err)
+		}
+	} else {
+		s.Log("It's an N-app")
+	}
+	DetectAndCloseCrashOrAppNotResponding(ctx, s, tconn, a, d, appPkgName)
+	CurrentAppPackage(ctx, s, d)
+}
+
+// MinimizeRestoreApp Test "minimize and relaunch the app" and verifies app relaunch successfully without crash or ANR.
+func MinimizeRestoreApp(ctx context.Context, s *testing.State, tconn *chrome.Conn, a *arc.ARC, d *ui.Device, appPkgName string, appActivity string) {
+	s.Log("Minimize the window")
+	if _, err := ash.SetARCAppWindowState(ctx, tconn, appPkgName, ash.WMEventMinimize); err != nil {
+		s.Error("Failed to minimize the window: ", err)
+	}
+	if err := ash.WaitForARCAppWindowState(ctx, tconn, appPkgName, ash.WindowStateMinimized); err != nil {
+		s.Error("The window is not minimized: ", err)
+	}
+	DetectAndCloseCrashOrAppNotResponding(ctx, s, tconn, a, d, appPkgName)
+	// Create a gmail app activity handle.
+	act, err := arc.NewActivity(a, appPkgName, appActivity)
+	if err != nil {
+		s.Fatal("Failed to create new app activity: ", err)
+	} else {
+		s.Log("Created new app activity")
+	}
+	defer act.Close()
+	// Launch the activity.
+	if err := act.Start(ctx); err != nil {
+		s.Fatal("Failed start app: ", err)
+	} else {
+		s.Log("App relaunched successfully")
+	}
+	CurrentAppPackage(ctx, s, d)
+}
+
+// ClamshellResizeWindow Test "resize and restore back to original state of the app" and verifies app launch successfully without crash or ANR.
+func ClamshellResizeWindow(ctx context.Context, s *testing.State, tconn *chrome.Conn, a *arc.ARC, d *ui.Device, appPkgName string, appActivity string) {
+	const (
+		restartButtonResourceID = "android:id/button1"
+	)
+
+	nApp := isNApp(ctx, s, tconn, a, d, appPkgName)
+	if !nApp {
+		s.Log("It's a pre N-app")
+		s.Log("Maximize the window")
+		if _, err := ash.SetARCAppWindowState(ctx, tconn, appPkgName, ash.WMEventMaximize); err != nil {
+			s.Log("Failed to maximize the window: ", err)
+		}
+		if err := ash.WaitForARCAppWindowState(ctx, tconn, appPkgName, ash.WindowStateMaximized); err != nil {
+			s.Log("The window is not maximized: ", err)
+		}
+		// Click on restart button.
+		restartButton := d.Object(ui.ResourceID(restartButtonResourceID))
+		if err := restartButton.WaitForExists(ctx, LongUITimeout); err != nil {
+			s.Fatal("Restart button does not exist: ", err)
+		}
+		if err := restartButton.Click(ctx); err != nil {
+			s.Fatal("Failed to click on restart button: ", err)
+		}
+
+		if _, err := d.WaitForWindowUpdate(ctx, appPkgName, LongUITimeout); err != nil {
+			s.Fatal("Failed to wait window updated: ", err)
+		}
+		DetectAndCloseCrashOrAppNotResponding(ctx, s, tconn, a, d, appPkgName)
+		CurrentAppPackage(ctx, s, d)
+	} else {
+		s.Log("It's an N-app")
+		s.Log("Get to Normal size of the window")
+		if _, err := ash.SetARCAppWindowState(ctx, tconn, appPkgName, ash.WMEventNormal); err != nil {
+			s.Error("Normal size of the window is failed: ", err)
+		}
+		if err := ash.WaitForARCAppWindowState(ctx, tconn, appPkgName, ash.WindowStateNormal); err != nil {
+			s.Error("The window is not normalized: ", err)
+		}
+		DetectAndCloseCrashOrAppNotResponding(ctx, s, tconn, a, d, appPkgName)
+		CurrentAppPackage(ctx, s, d)
+		s.Log("Get back to maximized window state")
+		if _, err := ash.SetARCAppWindowState(ctx, tconn, appPkgName, ash.WMEventMaximize); err != nil {
+			s.Log("Maximize the window is failed: ", err)
+		}
+		if err := ash.WaitForARCAppWindowState(ctx, tconn, appPkgName, ash.WindowStateMaximized); err != nil {
+			s.Log("The window is not maximized: ", err)
+		}
+	}
+}
+
+// isNApp func to check if it is an N or pre-N app
+func isNApp(ctx context.Context, s *testing.State, tconn *chrome.Conn, a *arc.ARC, d *ui.Device, appPkgName string) bool {
+	var nApp bool
+
+	info, err := d.GetInfo(ctx)
+	if err != nil {
+		s.Log("Failed to get app sdk version: ", err)
+	} else {
+		s.Logf("App sdk version %+v", info.SDKInt)
+	}
+	if info.SDKInt >= 24 {
+		nApp = true
+	}
+	return nApp
+}
+
+// ReOpenWindow Test "close and relaunch the app" and verifies app launch successfully without crash or ANR.
+func ReOpenWindow(ctx context.Context, s *testing.State, tconn *chrome.Conn, a *arc.ARC, d *ui.Device, appPkgName string, appActivity string) {
+
+	// Create a gmail app activity handle.
+	act, err := arc.NewActivity(a, appPkgName, appActivity)
+	if err != nil {
+		s.Fatal("Failed to create new app activity: ", err)
+	} else {
+		s.Log("Created new app activity")
+	}
+	defer act.Close()
+	// Launch the activity.
+	if err := act.Start(ctx); err != nil {
+		s.Fatal("Failed start app: ", err)
+	} else {
+		s.Log("App launched successfully")
+	}
+
+	s.Log("Close the app")
+	if err := act.Stop(ctx); err != nil {
+		s.Fatal("Failed to close the app: ", err)
+	}
+	DetectAndCloseCrashOrAppNotResponding(ctx, s, tconn, a, d, appPkgName)
+	// ReLaunch the activity.
+	if err := act.Start(ctx); err != nil {
+		s.Fatal("Failed start app: ", err)
+	} else {
+		s.Log("App relaunched successfully")
+	}
+	CurrentAppPackage(ctx, s, d)
+}
+
+// CurrentAppPackage func to get info on current package name
+func CurrentAppPackage(ctx context.Context, s *testing.State, d *ui.Device) {
+	info, err := d.GetInfo(ctx)
+	if err != nil {
+		s.Log("Failed to get app package name: ", err)
+	} else {
+		s.Logf("Current app package name %+v", info.CurrentPackagename)
+	}
+}
+
+// DetectAndCloseCrashOrAppNotResponding func to handle Crash or ANR.
+func DetectAndCloseCrashOrAppNotResponding(ctx context.Context, s *testing.State, tconn *chrome.Conn, a *arc.ARC, d *ui.Device, appPkgName string) {
+	const (
+		alertTitleCanNotDownloadText = "Can't download app"
+		alertTitleHasStoppedText     = "has stopped"
+		alertTitleKeepsStoppingText  = "keeps stopping"
+		alertTitleNotRespondingText  = "isn't responding"
+		alertTitleOpenAppAgainText   = "Open app again"
+		shortUITimeout               = 2 * time.Second
+	)
+
+	// Check for isn't responding alert title
+	alertTitleCanNotDownload := d.Object(ui.TextContains(alertTitleCanNotDownloadText))
+	alertTitleHasStopped := d.Object(ui.TextContains(alertTitleHasStoppedText))
+	alertTitleKeepsStopping := d.Object(ui.TextContains(alertTitleKeepsStoppingText))
+	alertTitleNotResponding := d.Object(ui.TextContains(alertTitleNotRespondingText))
+	alertTitleOpenAppAgain := d.Object(ui.TextContains(alertTitleOpenAppAgainText))
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+
+		if err := alertTitleNotResponding.Exists(ctx); err == nil {
+			return testing.PollBreak(errors.Wrap(err, "NotResponding does exist"))
+		}
+		if err := alertTitleHasStopped.Exists(ctx); err == nil {
+			return testing.PollBreak(errors.Wrap(err, "HasStopped does exist"))
+		}
+		if err := alertTitleKeepsStopping.Exists(ctx); err == nil {
+			return testing.PollBreak(errors.Wrap(err, "KeepsStopping does exist"))
+		}
+		if err := alertTitleOpenAppAgain.Exists(ctx); err == nil {
+			return testing.PollBreak(errors.Wrap(err, "OpenAppAgain does exist"))
+		}
+		if err := alertTitleCanNotDownload.Exists(ctx); err == nil {
+			return testing.PollBreak(errors.Wrap(err, "CanNotDownload does exist"))
+		}
+		return errors.New("waiting for crash")
+	}, &testing.PollOptions{Timeout: shortUITimeout}); err != nil && !strings.Contains(err.Error(), "waiting for crash") {
+		s.Error("The application crashed: ", err)
+		handleCrashOrANRDialog(ctx, s, d)
+	}
+}
+
+// handleCrashOrANRDialog func will handle the crash or ANR dialog box
+func handleCrashOrANRDialog(ctx context.Context, s *testing.State, d *ui.Device) {
+	const (
+		closeAppText     = "Close"
+		okText           = "ok"
+		OpenAppAgainText = "Open app again"
+	)
+	// Click on Open App Again
+	openAppAgainButton := d.Object(ui.ClassName(AndroidButtonClassName), ui.TextContains(OpenAppAgainText))
+	if err := openAppAgainButton.WaitForExists(ctx, DefaultUITimeout); err != nil {
+		s.Error("OpenAppAgainButton doesn't exist: ", err)
+	} else {
+		if err := openAppAgainButton.Click(ctx); err != nil {
+			s.Fatal("Failed to click on OpenAppAgainButton: ", err)
+		}
+	}
+
+	// Click on close app
+	closeButton := d.Object(ui.ClassName(AndroidButtonClassName), ui.TextContains(closeAppText))
+	if err := closeButton.WaitForExists(ctx, DefaultUITimeout); err != nil {
+		s.Error("CloseButton doesn't exist: ", err)
+	} else {
+		if err := closeButton.Click(ctx); err != nil {
+			s.Fatal("Failed to click on CloseButton: ", err)
+		}
+	}
+	// Click on ok button
+	okButton := d.Object(ui.ClassName(AndroidButtonClassName), ui.TextContains(okText))
+	if err := okButton.WaitForExists(ctx, DefaultUITimeout); err != nil {
+		s.Error("OkButton doesn't exist: ", err)
+	} else {
+		if err := okButton.Click(ctx); err != nil {
+			s.Fatal("Failed to click on OkButton: ", err)
+		}
+	}
+}
