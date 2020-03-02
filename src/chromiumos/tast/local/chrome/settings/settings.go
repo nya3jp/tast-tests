29b07014669e8f5663ca62b7ca31173a5fa65590
Charles William Dick <cwd@google.com>
2020-03-02 21:06:27 +0900

    Tast power setup disable Night Light
    Introduces a new setup item, TurnOffNightLight, which disables Night
    Light and sets the schedule to Never, so it won't turn back on.
    
    BUG=b:150572487
    TEST=tast run <DUT> arc.PowerIdlePerf.noarc
    
    Change-Id: Ie8ecf0d96e44e23b07df921003fd3be5bdb3bd09

--- src/chromiumos/tast/local/chrome/settings/settings.go
+++ src/chromiumos/tast/local/chrome/settings/settings.go
@@ -10,6 +10,7 @@ import (
 	"context"
 	"fmt"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 )
 
@@ -52,4 +53,71 @@ func SetDefaultZoom(ctx context.Context, tconn *chrome.TestConn, zoom float64) e
 		  })
 		})`, zoom)
 	return tconn.EvalPromise(ctx, expr, nil)
+}
+
+// NightLightScheduleValue provides available values for the Night Light
+// schedule.
+type NightLightScheduleValue uint
+
+// The following constants are from NightLightController::ScheduleType in
+// chromium/src/ash/public/cpp/night_light_controller.h.
+const (
+	// NightLightScheduleNever means Night Light is never enabled.
+	NightLightScheduleNever NightLightScheduleValue = 0
+	// NightLightScheduleSunsetToSunrise means Night Light is enabled at night.
+	NightLightScheduleSunsetToSunrise NightLightScheduleValue = 1
+	// NightLightScheduleCustom means Night Light has a custom schedule.
+	NightLightScheduleCustom NightLightScheduleValue = 2
+)
+
+const schedulePref = "ash.night_light.schedule_type"
+
+// NightLightSchedule gets the current Night Light schedule. See the above
+// constants for possible values.
+func NightLightSchedule(ctx context.Context, c *chrome.TestConn) (NightLightScheduleValue, error) {
+	var schedule struct {
+		value uint
+	}
+	if err := c.Call(ctx, &schedule, "tast.promisify(chrome.settingsPrivate.getPref)", schedulePref); err != nil {
+		return 0, err
+	}
+	switch schedule.value {
+	case uint(NightLightScheduleNever):
+		return NightLightScheduleNever, nil
+	case uint(NightLightScheduleSunsetToSunrise):
+		return NightLightScheduleSunsetToSunrise, nil
+	case uint(NightLightScheduleCustom):
+		return NightLightScheduleCustom, nil
+	default:
+		return 0, errors.Errorf("unrecognized Night Light schedule %d", schedule)
+	}
+}
+
+// SetNightLightSchedule sets the current Night Light schedule.
+func SetNightLightSchedule(ctx context.Context, c *chrome.TestConn, schedule NightLightScheduleValue) error {
+	if err := c.Call(ctx, nil, "tast.promisify(chrome.settingsPrivate.setPref)", schedulePref, schedule); err != nil {
+		return err
+	}
+	return nil
+}
+
+const enabledPref = "ash.night_light.enabled"
+
+// NightLightEnabled returns true if Night Light is currently enabled.
+func NightLightEnabled(ctx context.Context, c *chrome.TestConn) (bool, error) {
+	var enabled struct {
+		value bool
+	}
+	if err := c.Call(ctx, &enabled, "tast.promisify(chrome.settingsPrivate.getPref)", enabledPref); err != nil {
+		return false, err
+	}
+	return enabled.value, nil
+}
+
+// SetNightLightEnabled enables or disables Night Light.
+func SetNightLightEnabled(ctx context.Context, c *chrome.TestConn, enabled bool) error {
+	if err := c.Call(ctx, nil, "tast.promisify(chrome.settingsPrivate.setPref)", enabledPref, enabled); err != nil {
+		return err
+	}
+	return nil
 }
