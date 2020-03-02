69ed149c8c51eeacddc2a6f22ffd2d4925b14207
Charles William Dick <cwd@google.com>
2020-03-02 21:06:27 +0900

    Tast power setup disable Night Light
    Introduces a new setup item, TurnOffNightLight, which disables Night
    Light and sets the schedule to Never, so it won't turn back on.
    
    BUG=b:150572487
    TEST=tast run <DUT> arc.PowerIdlePerf.noarc
    
    Change-Id: Ie8ecf0d96e44e23b07df921003fd3be5bdb3bd09

--- src/chromiumos/tast/local/chrome/settings/settings.go
+++ src/chromiumos/tast/local/chrome/settings/settings.go
@@ -53,3 +53,87 @@ func SetDefaultZoom(ctx context.Context, c *chrome.Conn, zoom float64) error {
 		})`, zoom)
 	return c.EvalPromise(ctx, expr, nil)
 }
+
+const (
+	// NightLightScheduleNever means Night Light is never enabled.
+	NightLightScheduleNever = 0
+	// NightLightScheduleSunsetToSunrise means Night Light is enabled at night.
+	NightLightScheduleSunsetToSunrise = 1
+	// NightLightScheduleCustom means Night Light has a custom schedule.
+	NightLightScheduleCustom = 2
+)
+
+// NightLightSchedule gets the current Night Light schedule. See the above
+// constants for possible values.
+func NightLightSchedule(ctx context.Context, c *chrome.Conn) (uint, error) {
+	var schedule uint
+	if err := c.EvalPromise(ctx,
+		`new Promise(function(resolve, reject) {
+		  chrome.settingsPrivate.getPref("ash.night_light.schedule_type", function(schedule) {
+		    if (chrome.runtime.lastError) {
+		      reject(new Error(chrome.runtime.lastError.message));
+		      return;
+		    }
+		    resolve(schedule.value);
+		  })
+		})`, &schedule); err != nil {
+		return 0, err
+	}
+	return schedule, nil
+}
+
+// SetNightLightSchedule sets the current Night Light schedule.
+func SetNightLightSchedule(ctx context.Context, c *chrome.Conn, schedule uint) error {
+	expr := fmt.Sprintf(
+		`new Promise(function(resolve, reject) {
+		  chrome.settingsPrivate.setPref("ash.night_light.schedule_type", %d, function(success) {
+		    if (chrome.runtime.lastError) {
+		      reject(new Error(chrome.runtime.lastError.message));
+		      return;
+		    }
+		    if (!success) {
+		      reject(new Error("set ash.night_light.schedule_type failed"));
+		      return;
+		    }
+		    resolve();
+		  })
+		})`, schedule)
+	return c.EvalPromise(ctx, expr, nil)
+}
+
+// NightLightEnabled returns true if Night Light is currenlty enabled.
+func NightLightEnabled(ctx context.Context, c *chrome.Conn) (bool, error) {
+	var enabled bool
+	if err := c.EvalPromise(ctx,
+		`new Promise(function(resolve, reject) {
+		  chrome.settingsPrivate.getPref("ash.night_light.enabled", function(enabled) {
+		    if (chrome.runtime.lastError) {
+		      reject(new Error(chrome.runtime.lastError.message));
+		      return;
+		    }
+		    resolve(enabled.value);
+		  })
+		})`, &enabled); err != nil {
+		return false, err
+	}
+	return enabled, nil
+}
+
+// SetNightLightEnabled enables or disables Night Light.
+func SetNightLightEnabled(ctx context.Context, c *chrome.Conn, enabled bool) error {
+	expr := fmt.Sprintf(
+		`new Promise(function(resolve, reject) {
+		  chrome.settingsPrivate.setPref("ash.night_light.enabled", %t, function(success) {
+		    if (chrome.runtime.lastError) {
+		      reject(new Error(chrome.runtime.lastError.message));
+		      return;
+		    }
+		    if (!success) {
+		      reject(new Error("set ash.night_light.enabled failed"));
+		      return;
+		    }
+		    resolve();
+		  })
+		})`, enabled)
+	return c.EvalPromise(ctx, expr, nil)
+}
