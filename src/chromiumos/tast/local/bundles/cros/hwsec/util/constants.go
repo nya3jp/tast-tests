669f480d5c8a9d6ca0ba32491602a14526a24829
John L Chen <zuan@chromium.org>
2020-03-02 22:37:01 +0800

    tast-tests: hwsec: Move vault related constants out of individual test
    BUG=none
    TEST=tast run $dut hwsec.RecreateUserVault
    
    Change-Id: Id9cd1b1266df30a264cb2436304dde79fde69b33

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/util/constants.go
@@ -0,0 +1,12 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package util
+
+// This file contains some shared constants for local hwsec bundle tests.
+const (
+	FirstUsername = "fakeuser1@example.com"
+	FirstPassword = "FakePasswordForFakeUser1"
+	PasswordLabel = "PasswordLabel"
+)
