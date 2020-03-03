ddd46f70702105a15c9aa5f9c224c169028d2df8
Mike Wiitala <mwiitala@google.com>
2020-03-03 15:22:29 -0700

    tast-tests: Add test to verify Drallion power key behavior
    On Drallion360, the power button is on the keyboard rather than the side
    of the device.  This makes inadvertent keypresses while in tablet mode
    more common. To account for this, the power button requires a long press
    (>1.5s) beore bringing up the power-down menus. This is the behavior
    that we must verify with this test.
    
    BUG=b:149035007
    TEST=Run test on lab DUT, verify expected behavior:
    tast -verbose run -var servo={servo_addr}.cros:{servo_port} {dut_name}.cros power.DrallionTabletPower
    
    Change-Id: I84df7f6792d16e8bbb81701f182c3e3f1d495eb4

---
+++ src/chromiumos/tast/local/bundles/cros/wilco/power_menu_service.go
@@ -0,0 +1,82 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wilco
+
+import (
+	"github.com/golang/protobuf/ptypes/empty"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	pb "chromiumos/tast/services/cros/wilco"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddService(&testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			pb.RegisterPowerMenuServiceServer(srv, &PowerMenuService{s: s})
+		},
+	})
+}
+
+// PowerMenuService implements tast.cros.power.PowerMenuService.
+type PowerMenuService struct {
+	s *testing.ServiceState
+
+	cr *chrome.Chrome
+}
+
+func (p *PowerMenuService) NewChrome(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	if p.cr != nil {
+		return nil, errors.New("Chrome already available")
+	}
+
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		return nil, err
+	}
+	p.cr = cr
+	return &empty.Empty{}, nil
+}
+
+func (p *PowerMenuService) CloseChrome(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	if p.cr == nil {
+		return nil, errors.New("Chrome not available")
+	}
+	err := p.cr.Close(ctx)
+	p.cr = nil
+	return &empty.Empty{}, err
+}
+
+func (p *PowerMenuService) IsPowerMenuPresent(ctx context.Context, req *empty.Empty) (*pb.IsPowerMenuPresentResponse, error) {
+	if p.cr == nil {
+		return nil, errors.New("Chrome not available")
+	}
+
+	tconn, err := p.cr.TestAPIConn(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	root, err := ui.Root(ctx, tconn)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get root node")
+	}
+
+	// Check if the power menu is displayed
+	params := ui.FindParams{
+		ClassName: "PowerButtonMenuView",
+		Role:      ui.RoleTypeMenu,
+	}
+	exists, err := root.DescendantExists(ctx, params)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find power menu")
+	}
+
+	return &pb.IsPowerMenuPresentResponse{IsMenuPresent: exists}, nil
+}
