99f7ccd121a48e2d60d563019b03fdb6c299c6be
Keiichi Watanabe <keiichiw@chromium.org>
2020-03-04 19:32:33 +0900

    video.ARCDecode: Enable arc decoder tests for arcvm
    BUG=b:144681449, b:143584325, b:142948390, b:142423642
    TEST=tast run arcvm-DUT video.ARCDecode*
    
    Change-Id: I77d14a52fe0c8188ebc95190d1f8ad6d5b1f1045

--- src/chromiumos/tast/local/bundles/cros/video/arc_decode_accel.go
+++ src/chromiumos/tast/local/bundles/cros/video/arc_decode_accel.go
@@ -21,22 +21,37 @@ func init() {
 		Contacts:     []string{"akahuang@chromium.org", "chromeos-video-eng@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		Data:         []string{c2e2etest.X86ApkName, c2e2etest.ArmApkName},
-		SoftwareDeps: []string{"android_p", "chrome"},
+		SoftwareDeps: []string{"chrome"},
 		Pre:          arc.BootedWithVideoLogging(),
 		Params: []testing.Param{{
 			Name:              "h264",
 			Val:               "test-25fps.h264",
-			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "android_p"},
+			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
+		}, {
+			Name:              "h264_vm",
+			Val:               "test-25fps.h264",
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "android_vm"},
 			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
 		}, {
 			Name:              "vp8",
 			Val:               "test-25fps.vp8",
-			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8, "android_p"},
+			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
+		}, {
+			Name:              "vp8_vm",
+			Val:               "test-25fps.vp8",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8, "android_vm"},
 			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
 		}, {
 			Name:              "vp9",
 			Val:               "test-25fps.vp9",
-			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "android_p"},
+			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
+		}, {
+			Name:              "vp9_vm",
+			Val:               "test-25fps.vp9",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "android_vm"},
 			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
 		}},
 	})
