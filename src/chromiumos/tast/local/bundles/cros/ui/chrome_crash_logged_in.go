6d7e35b2155a3ba660f6a679edd0cfeca689e308
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-03-04 14:31:00 +0900

    tast: Report errors upon TearDownCrashReporter in all tests.
    TearDownCrashTest is often called by defer, but it may report some
    errors. The utility method reports the first error that happened during
    tear down. It can reduce boilerplate code in tests catching such
    errors.
    
    BUG=chromium:978255
    TEST=tast run $DUT platform.UserCrash
    
    Cq-Depend: chromium:2089155
    Change-Id: I14b5933ca2e85bfc3fbe2e025f6adbdb736db588

--- src/chromiumos/tast/local/bundles/cros/ui/chrome_crash_logged_in.go
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_crash_logged_in.go
@@ -69,7 +69,7 @@ func ChromeCrashLoggedIn(ctx context.Context, s *testing.State) {
 	if err := crash.SetUpCrashTest(ctx, crash.WithConsent(cr)); err != nil {
 		s.Fatal("SetUpCrashTest failed: ", err)
 	}
-	defer crash.TearDownCrashTest()
+	defer crash.TearDownCrashTestReportingError(s)
 
 	files, err := ct.KillAndGetCrashFiles(ctx)
 	if err != nil {
