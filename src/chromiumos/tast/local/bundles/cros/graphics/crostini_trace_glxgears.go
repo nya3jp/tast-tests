714194c307ec5528ab3b50f1cce50cf6490fbf06
Po-Hsien Wang <pwang@chromium.org>
2020-03-04 12:15:04 -0800

    graphics: Change glxgears to use Large disk VM.
    BUG=chromium:1054692
    TEST=tast run DUT graphics.CrostiniTraceGlxgears
    
    Change-Id: Id9d38d4c386cc6001cc89b01a9837bcbb5465efe

--- src/chromiumos/tast/local/bundles/cros/graphics/crostini_trace_glxgears.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/crostini_trace_glxgears.go
@@ -20,7 +20,7 @@ func init() {
 		Contacts:     []string{"chromeos-gfx@google.com"},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		Data:         []string{"crostini_trace_glxgears.trace"},
-		Pre:          crostini.StartedGPUEnabledBuster(),
+		Pre:          crostini.StartedTraceVM(),
 		Timeout:      5 * time.Minute,
 		SoftwareDeps: []string{"chrome", "crosvm_gpu", "vm_host"},
 	})
