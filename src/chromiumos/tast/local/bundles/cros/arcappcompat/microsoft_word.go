d6c2b970c97f9ff10c3d3f512a8e22a1ed388fd7
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-03-04 13:24:53 -0800

    Run UIAutomator Test Using Tast For Microsoft Word app
    BUG=b:150012353
    TEST=None
    
    Tast Test for Microsoft Word app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: Ic437636baf06f27ba3aefb04908e7bd916873cfe

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/microsoft_word.go
@@ -0,0 +1,255 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForMicrosoftWord = []testutil.TestSuite{
+	{Name: "Launch app in Clamshell", Fn: launchAppForMicrosoftWord},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForMicrosoftWord = []testutil.TestSuite{
+	{Name: "Launch app in Touchview", Fn: launchAppForMicrosoftWord},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MicrosoftWord,
+		Desc:         "Functional test for MicrosoftWord that installs the app also verifies it is logged in and that the main page is open, checks MicrosoftWord correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForMicrosoftWord,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               arc.BootedAppCompat(),
+		}, {
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForMicrosoftWord,
+			},
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               arc.BootedInTabletModeAppCompat(),
+		}, {
+			Name: "vm",
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForMicrosoftWord,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               arc.VMBootedAppCompat(),
+		}, {
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForMicrosoftWord,
+			},
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               arc.VMBootedInTabletModeAppCompat(),
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// MicrosoftWord test uses library for opting into the playstore and installing app.
+// Checks MicrosoftWord correctly changes the window states in both clamshell and touchview mode.
+func MicrosoftWord(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.microsoft.office.word"
+		appActivity = "com.microsoft.office.apphost.LaunchActivity"
+	)
+
+	// Step up chrome on Chromebook.
+	cr, tconn, a, d := testutil.SetUpDevice(ctx, s, appPkgName, appActivity)
+	defer d.Close()
+
+	testSet := s.Param().(testutil.TestParams)
+	// Run the different test cases.
+	for idx, test := range testSet.Tests {
+		// Run subtests.
+		s.Run(ctx, test.Name, func(ctx context.Context, s *testing.State) {
+			defer func() {
+				if s.HasError() {
+					path := fmt.Sprintf("%s/screenshot-arcappcompat-failed-test-%d.png", s.OutDir(), idx)
+					if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
+						s.Log("Failed to capture screenshot: ", err)
+					}
+				}
+			}()
+			// Launch the app.
+			act, err := arc.NewActivity(a, appPkgName, appActivity)
+			if err != nil {
+				s.Fatal("Failed to create new app activity: ", err)
+			}
+			s.Log("Created new app activity")
+
+			defer act.Close()
+			if err := act.Start(ctx); err != nil {
+				s.Fatal("Failed start app: ", err)
+			}
+			s.Log("App launched successfully")
+
+			defer act.Stop(ctx)
+			test.Fn(ctx, s, tconn, a, d, appPkgName, appActivity)
+		})
+	}
+}
+
+// launchAppForMicrosoftWord verifies MicrosoftWord is logged in and
+// verify MicrosoftWord reached main activity page of the app.
+func launchAppForMicrosoftWord(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName string, appActivity string) {
+	const (
+		allowButtonText       = "ALLOW"
+		enterEmailAddressID   = "com.microsoft.office.word:id/OfcEditText"
+		nextButtonDescription = "Next"
+		passwordClassName     = "android.widget.EditText"
+		passwordID            = "i0118"
+		passwordText          = "Password"
+		emailAddress          = "arcplusplusappcompat6@gmail.com"
+		signInClassName       = "android.widget.Button"
+		signInText            = "Sign in"
+		newID                 = "com.microsoft.office.word:id/docsui_landing_pane_header_heading"
+	)
+
+	// Click on allow button to access your photos, media and files.
+	allowButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.Text(allowButtonText))
+	if err := allowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("Allow Button doesn't exists: ", err)
+	} else if err := allowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on allowButton: ", err)
+	}
+
+	// Enter email id.
+	enterEmailAddress := d.Object(ui.ID(enterEmailAddressID))
+	if err := enterEmailAddress.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("EnterEmailAddress doesn't exists: ", err)
+	} else if err := enterEmailAddress.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterEmailAddress: ", err)
+	}
+
+	if err := enterEmailAddress.SetText(ctx, emailAddress); err != nil {
+		s.Fatal("Doesn't enter EmailAddress: ", err)
+	}
+	s.Log("Entered EmailAddress")
+
+	// Click on next button
+	nextButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.Description(nextButtonDescription))
+	if err := nextButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("Next Button doesn't exists: ", err)
+	} else if err := nextButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on nextButton: ", err)
+	}
+
+	// Enter password.
+	enterPassword := d.Object(ui.ID(passwordID))
+	if err := enterPassword.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("EnterPassword doesn't exists: ", err)
+	} else if err := enterPassword.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterPassword: ", err)
+	}
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_M, ui.META_SHIFT_ON); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_M: ", err)
+	}
+	s.Log("Entered KEYCODE_M")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_I, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_I: ", err)
+	}
+	s.Log("Entered KEYCODE_I")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_C, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_C: ", err)
+	}
+	s.Log("Entered KEYCODE_C")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_R, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_R: ", err)
+	}
+	s.Log("Entered KEYCODE_R")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_O, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_O: ", err)
+	}
+	s.Log("Entered KEYCODE_O")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_S, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_S: ", err)
+	}
+	s.Log("Entered KEYCODE_S")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_O, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_O: ", err)
+	}
+	s.Log("Entered KEYCODE_O")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_F, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_F: ", err)
+	}
+	s.Log("Entered KEYCODE_F")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_T, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_T: ", err)
+	}
+	s.Log("Entered KEYCODE_T")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_1, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_1: ", err)
+	}
+	s.Log("Entered KEYCODE_1")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_2, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_2: ", err)
+	}
+	s.Log("Entered KEYCODE_2")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_3, 0); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_3: ", err)
+	}
+	s.Log("Entered KEYCODE_3")
+
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_4, ui.META_SHIFT_ON); err != nil {
+		s.Fatal("Doesn't enter KEYCODE_4: ", err)
+	}
+	s.Log("Entered KEYCODE_4")
+
+	// Click on Sign in button.
+	signInButton := d.Object(ui.ClassName(signInClassName), ui.Text(signInText))
+	if err := signInButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("SignInButton doesn't exists: ", err)
+	} else if err := signInButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on signInButton: ", err)
+	}
+
+	// Check for newIcon on homePage.
+	newIcon := d.Object(ui.ID(newID))
+	if err := newIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("NewIcon doesn't exists: ", err)
+	}
+}
