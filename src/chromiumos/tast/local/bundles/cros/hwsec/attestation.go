ff24475bc915b231018944bea1e92d3034cf21b4
Leo Lai <cylai@google.com>
2020-03-04 14:01:12 +0800

    tast-tests: hwsec: subtest of various attestation flows
    This CL extends the original attestatin flow (async, user cert) to
    testing of sync/async methods for enrollment and cert; also, test the
    flow for system cert (i.e., username == "")
    
    BUG=b:132657813
    TEST=tast run ${DUT_IP} hwsec.Attestation
    
    Change-Id: Ieea3c743123d7facad5cbef8da862c6e79215dfa

--- src/chromiumos/tast/local/bundles/cros/hwsec/attestation.go
+++ src/chromiumos/tast/local/bundles/cros/hwsec/attestation.go
@@ -48,8 +48,26 @@ func Attestation(ctx context.Context, s *testing.State) {
 	}
 
 	at := hwsec.NewAttestaionTest(utility, hwsec.DefaultPCA)
-	if err := at.Enroll(ctx); err != nil {
-		s.Fatal("Failed to enroll device: ", err)
+	for _, param := range []struct {
+		name  string
+		async bool
+	}{
+		{
+			name:  "async_enroll",
+			async: true,
+		}, {
+			name:  "sync_enroll",
+			async: false,
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			if err := utility.SetAttestationAsyncMode(ctx, param.async); err != nil {
+				s.Fatal("Failed to switch to sync mode: ", err)
+			}
+			if err := at.Enroll(ctx); err != nil {
+				s.Fatal("Failed to enroll device: ", err)
+			}
+		})
 	}
 
 	auth := chrome.Auth("test@crashwsec.bigr.name", "testpass", "gaia-id")
@@ -65,69 +83,96 @@ func Attestation(ctx context.Context, s *testing.State) {
 	}()
 	username := cr.User()
 
-	if err := at.GetCertificate(ctx, username, hwsec.DefaultCertLabel); err != nil {
-		s.Fatal("Failed to enroll device: ", err)
-	}
+	for _, param := range []struct {
+		name     string
+		async    bool
+		username string
+	}{
+		{
+			name:     "async_system_cert",
+			async:    true,
+			username: "",
+		},
+		{
+			name:     "async_user_cert",
+			async:    true,
+			username: username,
+		}, {
+			name:     "sync_user_cert",
+			async:    false,
+			username: username,
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			if err := utility.SetAttestationAsyncMode(ctx, param.async); err != nil {
+				s.Fatal("Failed to switch to sync mode: ", err)
+			}
+			username := param.username
+			if err := at.GetCertificate(ctx, username, hwsec.DefaultCertLabel); err != nil {
+				s.Fatal("Failed to enroll device: ", err)
+			}
 
-	if err := at.SignEnterpriseChallenge(ctx, username, hwsec.DefaultCertLabel); err != nil {
-		s.Fatal("Failed to sign enterprise challenge: ", err)
-	}
+			if err := at.SignEnterpriseChallenge(ctx, username, hwsec.DefaultCertLabel); err != nil {
+				s.Fatal("Failed to sign enterprise challenge: ", err)
+			}
 
-	if err := at.SignSimpleChallenge(ctx, username, hwsec.DefaultCertLabel); err != nil {
-		s.Fatal("Failed to sign simple challenge: ", err)
-	}
+			if err := at.SignSimpleChallenge(ctx, username, hwsec.DefaultCertLabel); err != nil {
+				s.Fatal("Failed to sign simple challenge: ", err)
+			}
 
-	s.Log("Start key payload closed-loop testing")
-	s.Log("Setting key payload")
-	expectedPayload := hwsec.DefaultKeyPayload
-	_, err = utility.SetKeyPayload(ctx, username, hwsec.DefaultCertLabel, expectedPayload)
-	if err != nil {
-		s.Fatal("Failed to set key payload: ", err)
-	}
-	s.Log("Getting key payload")
-	resultPayload, err := utility.GetKeyPayload(ctx, username, hwsec.DefaultCertLabel)
-	if err != nil {
-		s.Fatal("Failed to get key payload: ", err)
-	}
-	if resultPayload != expectedPayload {
-		s.Fatalf("Inconsistent paylaod -- result: %s / expected: %s", resultPayload, expectedPayload)
-	}
-	s.Log("Start key payload closed-loop done")
+			s.Log("Start key payload closed-loop testing")
+			s.Log("Setting key payload")
+			expectedPayload := hwsec.DefaultKeyPayload
+			_, err = utility.SetKeyPayload(ctx, username, hwsec.DefaultCertLabel, expectedPayload)
+			if err != nil {
+				s.Fatal("Failed to set key payload: ", err)
+			}
+			s.Log("Getting key payload")
+			resultPayload, err := utility.GetKeyPayload(ctx, username, hwsec.DefaultCertLabel)
+			if err != nil {
+				s.Fatal("Failed to get key payload: ", err)
+			}
+			if resultPayload != expectedPayload {
+				s.Fatalf("Inconsistent paylaod -- result: %s / expected: %s", resultPayload, expectedPayload)
+			}
+			s.Log("Start key payload closed-loop done")
 
-	s.Log("Start verifying key registration")
-	isSuccessful, err := utility.RegisterKeyWithChapsToken(ctx, username, hwsec.DefaultCertLabel)
-	if err != nil {
-		s.Fatal("Failed to register key with chaps token due to error: ", err)
-	}
-	if !isSuccessful {
-		s.Fatal("Failed to register key with chaps token")
-	}
-	// Now the key has been registered and remove from the key store
-	_, err = utility.GetPublicKey(ctx, username, hwsec.DefaultCertLabel)
-	if err == nil {
-		s.Fatal("unsidered successful operation -- key should be removed after registration")
-	}
-	// Well, actually we need more on system key so the key registration is validated.
-	s.Log("Key registration verified")
+			s.Log("Start verifying key registration")
+			isSuccessful, err := utility.RegisterKeyWithChapsToken(ctx, username, hwsec.DefaultCertLabel)
+			if err != nil {
+				s.Fatal("Failed to register key with chaps token due to error: ", err)
+			}
+			if !isSuccessful {
+				s.Fatal("Failed to register key with chaps token")
+			}
+			// Now the key has been registered and remove from the key store
+			_, err = utility.GetPublicKey(ctx, username, hwsec.DefaultCertLabel)
+			if err == nil {
+				s.Fatal("unsidered successful operation -- key should be removed after registration")
+			}
+			// Well, actually we need more on system key so the key registration is validated.
+			s.Log("Key registration verified")
 
-	s.Log("Verifying deletion of keys by prefix")
-	for _, label := range []string{"label1", "label2", "label3"} {
-		if err := at.GetCertificate(ctx, username, label); err != nil {
-			s.Fatalf("Failed to create certificate request for label %q: %v", label, err)
-		}
-		_, err = utility.GetPublicKey(ctx, username, label)
-		if err != nil {
-			s.Fatalf("Failed to get public key for label %q: %v", label, err)
-		}
+			s.Log("Verifying deletion of keys by prefix")
+			for _, label := range []string{"label1", "label2", "label3"} {
+				if err := at.GetCertificate(ctx, username, label); err != nil {
+					s.Fatalf("Failed to create certificate request for label %q: %v", label, err)
+				}
+				_, err = utility.GetPublicKey(ctx, username, label)
+				if err != nil {
+					s.Fatalf("Failed to get public key for label %q: %v", label, err)
+				}
+			}
+			s.Log("Deleting keys just created")
+			if err := utility.DeleteKeys(ctx, username, "label"); err != nil {
+				s.Fatal("Failed to remove the key group: ", err)
+			}
+			for _, label := range []string{"label1", "label2", "label3"} {
+				if _, err := utility.GetPublicKey(ctx, username, label); err == nil {
+					s.Fatalf("key with label %q still found: %v", label, err)
+				}
+			}
+			s.Log("Deletion of keys by prefix verified")
+		})
 	}
-	s.Log("Deleting keys just created")
-	if err := utility.DeleteKeys(ctx, username, "label"); err != nil {
-		s.Fatal("Failed to remove the key group: ", err)
-	}
-	for _, label := range []string{"label1", "label2", "label3"} {
-		if _, err := utility.GetPublicKey(ctx, username, label); err == nil {
-			s.Fatalf("key with label %q still found: %v", label, err)
-		}
-	}
-	s.Log("Deletion of keys by prefix verified")
 }
