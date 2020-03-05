2b2852236fdbbfc5b0e3de7d699b420d12f8c6a8
Jun Mukai <mukai@google.com>
2020-03-04 17:41:06 -0800

    Add ui.MeetCUJ test
    BUG=chromium:1085265
    TEST=tast run ui.MeetCUJ
    
    Change-Id: I4ba8accf0d20d67f9c2a8ee62e68f6c9bbaef6f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2211230
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Tested-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/ui/meet_cuj.go
@@ -0,0 +1,198 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
+	"chromiumos/tast/local/bundles/cros/ui/faillog"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MeetCUJ,
+		Desc:         "Measures the performance of critical user journey for Google Meet",
+		Contacts:     []string{"mukai@chromium.org", "tclaiborne@chromium.org"},
+		Attr:         []string{"group:crosbolt", "crosbolt_nightly"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      3 * time.Minute,
+		Vars: []string{
+			"mute",
+			"ui.MeetCUJ.username",
+			"ui.MeetCUJ.password",
+			"ui.MeetCUJ.meet_code",
+		},
+	})
+}
+
+func MeetCUJ(ctx context.Context, s *testing.State) {
+	const timeout = 10 * time.Second
+
+	username := s.RequiredVar("ui.MeetCUJ.username")
+	password := s.RequiredVar("ui.MeetCUJ.password")
+	code := s.RequiredVar("ui.MeetCUJ.meet_code")
+
+	cr, err := chrome.New(ctx, chrome.GAIALogin(), chrome.Auth(username, password, "gaia-id"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to the test API connection: ", err)
+	}
+
+	conn, err := cr.NewConn(ctx, "https://meet.google.com/")
+	if err != nil {
+		s.Fatal("Failed to open the hangout meet website: ", err)
+	}
+	defer conn.Close()
+	defer faillog.DumpUITreeOnError(ctx, s, tconn)
+
+	// Make it into a normal window if it is in clamshell-mode; so that the
+	// desktop needs to compose the browser window with the wallpaper.
+	inTabletMode, err := ash.TabletModeEnabled(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to detect it is in tablet-mode or not: ", err)
+	}
+	if !inTabletMode {
+		ws, err := ash.GetAllWindows(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to obtain the window list: ", err)
+		}
+		for _, w := range ws {
+			if _, err := ash.SetWindowState(ctx, tconn, w.ID, ash.WMEventNormal); err != nil {
+				s.Fatalf("Failed to change the window %s (%d) to normal: %v", w.Name, w.ID, err)
+			}
+		}
+	}
+
+	webview, err := ui.FindWithTimeout(ctx, tconn, ui.FindParams{Role: ui.RoleTypeWebView, ClassName: "WebView"}, timeout)
+	if err != nil {
+		s.Fatal("Failed to find webview: ", err)
+	}
+	defer webview.Release(ctx)
+
+	enter, err := webview.DescendantWithTimeout(ctx, ui.FindParams{Name: "Enter meeting code"}, timeout)
+	if err != nil {
+		s.Fatal("Failed to find the meeting code: ", err)
+	}
+	defer enter.Release(ctx)
+	if err := enter.LeftClick(ctx); err != nil {
+		s.Fatal("Failed to click the input form: ", err)
+	}
+	kw, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to create a keyboard: ", err)
+	}
+	defer kw.Close()
+	if err := kw.Type(ctx, code); err != nil {
+		s.Fatal("Failed to type the meeting code: ", err)
+	}
+	if err := kw.Accel(ctx, "Enter"); err != nil {
+		s.Fatal("Failed to type the enter key: ", err)
+	}
+
+	if err := func() error {
+		// Meet will ask the permission: wait for the permission bubble to appear.
+		// Note that there may be some other bubbles, so find only within the main
+		// container -- which should be named as "Desk_Container_A", the primary
+		// desk.
+		container, err := ui.Find(ctx, tconn, ui.FindParams{ClassName: "Desk_Container_A"})
+		if err != nil {
+			return errors.Wrap(err, "failed to find the container")
+		}
+		defer container.Release(ctx)
+		for i := 0; i < 5; i++ {
+			bubble, err := container.DescendantWithTimeout(ctx, ui.FindParams{ClassName: "BubbleDialogDelegateView"}, timeout)
+			if err != nil {
+				// It is fine not finding the bubble.
+				return nil
+			}
+			defer bubble.Release(ctx)
+			allowButton, err := bubble.Descendant(ctx, ui.FindParams{Name: "Allow", Role: ui.RoleTypeButton})
+			if err != nil {
+				return errors.Wrap(err, "failed to find the allow button")
+			}
+			defer allowButton.Release(ctx)
+			if err := allowButton.LeftClick(ctx); err != nil {
+				return errors.Wrap(err, "failed to click the allow button")
+			}
+			if err := testing.Sleep(ctx, time.Second); err != nil {
+				return errors.Wrap(err, "failed to wait for the next cycle of permission")
+			}
+		}
+		return errors.New("too many permission requests")
+	}(); err != nil {
+		s.Fatal("Failed to skip the permission requests: ", err)
+	}
+
+	joinNow, err := webview.DescendantWithTimeout(ctx, ui.FindParams{Name: "Join now"}, timeout)
+	if err != nil {
+		s.Fatal("Failed to find join-now button: ", err)
+	}
+	defer joinNow.Release(ctx)
+
+	configs := []cuj.MetricConfig{cuj.NewCustomMetricConfig(
+		"Graphics.Smoothness.PercentDroppedFrames.CompositorThread.Video",
+		"percent", perf.SmallerIsBetter, []int64{50, 80})}
+	for _, suffix := range []string{"Capturer", "Encoder", "EncoderQueue", "RateLimiter"} {
+		configs = append(configs, cuj.NewCustomMetricConfig(
+			"WebRTC.Video.DroppedFrames."+suffix, "percent", perf.SmallerIsBetter,
+			[]int64{50, 80}))
+	}
+	recorder, err := cuj.NewRecorder(ctx, configs...)
+	if err != nil {
+		s.Fatal("Failed to create the recorder: ", err)
+	}
+	if err := recorder.Run(ctx, tconn, func() error {
+		if err := joinNow.LeftClick(ctx); err != nil {
+			return errors.Wrap(err, `failed to click "Join now" button`)
+		}
+		shareMessage := "Share this info with people you want in the meeting"
+		if err := webview.WaitUntilDescendantExists(ctx, ui.FindParams{Name: shareMessage}, timeout); err == nil {
+			// "Share this code" popup appears, dismissing by close button.
+			// Close button
+			closeButton, err := webview.DescendantWithTimeout(ctx, ui.FindParams{Role: ui.RoleTypeButton, Name: "Close"}, timeout)
+			if err != nil {
+				return errors.Wrap(err, "close button should be in the popup")
+			}
+			if err := closeButton.LeftClick(ctx); err != nil {
+				return errors.Wrap(err, "failed to click the close button")
+			}
+			if err := webview.WaitUntilDescendantGone(ctx, ui.FindParams{Name: shareMessage}, timeout); err != nil {
+				return errors.Wrap(err, "popup does not disappear")
+			}
+		}
+		if err := testing.Sleep(ctx, 30*time.Second); err != nil {
+			return errors.Wrap(err, "failed to wait")
+		}
+		// Close the browser window to finish it.
+		if err := conn.CloseTarget(ctx); err != nil {
+			return errors.Wrap(err, "failed to close")
+		}
+		return nil
+	}); err != nil {
+		s.Fatal("Failed to conduct the recorder task: ", err)
+	}
+
+	pv := perf.NewValues()
+	if err := recorder.Record(pv); err != nil {
+		s.Fatal("Failed to record the data: ", err)
+	}
+	if pv.Save(s.OutDir()); err != nil {
+		s.Error("Failed to save the perf data: ", err)
+	}
+}
