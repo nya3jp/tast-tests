80ed96756a2c576f9b1b6de80ce0bd0330a8df72
Shuhei Takahashi <nya@chromium.org>
2020-03-06 15:53:11 +0900

    tast-tests: Protect the control message channel.
    Test bundles communicate with the tast command on the host with the
    control message channel established on stdin/stdout. Consequently,
    if a test writes something to stdout or reads from stdin, the
    control message channel is disturbed.
    
    This change modifies bundle main functions to call new
    bundle.{LocalDefault,RemoteDefault} which closes the standard IO
    before running tests.
    
    Also add a test (meta.Stdio) that verifies the new protection.
    
    BUG=chromium:1041131
    TEST=fast_build.sh -T
    TEST=tast run DUT meta.Stdio
    
    Cq-Depend: chromium:2089160
    Change-Id: I5ca22e4502bc4943d8e29460efb72b2f5588edec

---
+++ src/chromiumos/tast/local/bundles/cros/meta/stdio.go
@@ -0,0 +1,35 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package meta
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     Stdio,
+		Desc:     "Ensures that accessing stdin/stdout does not harm test execution",
+		Contacts: []string{"tast-owners@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+	})
+}
+
+func Stdio(ctx context.Context, s *testing.State) {
+	var r string
+	if _, err := fmt.Scan(&r); err == nil {
+		s.Error("fmt.Scan succeeded unexpectedly")
+	}
+	if _, err := fmt.Print("foo"); err == nil {
+		s.Error("fmt.Print succeeded unexpectedly")
+	}
+	if _, err := fmt.Fprint(os.Stderr, "foo"); err == nil {
+		s.Error("fmt.Fprint succeeded unexpectedly")
+	}
+}
