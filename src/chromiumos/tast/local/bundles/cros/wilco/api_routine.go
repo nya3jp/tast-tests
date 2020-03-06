7173cc5770cf0140cff204e138354e790b408c8d
Wayne Kung <wayne_kung@dell.corp-partner.google.com>
2020-03-06 14:13:21 +0800

    tast-tests: Add NVMe self-test routine for APIRoutine
    Add NVMe self-test routine for the APIRoutine tast-tests.
    
    BUG=b:146513388
    TEST=tast run $DUT_IP wilco.APIRoutine
    
    Change-Id: I6afdf78699d368261f475c973d90d4ea86b35c94

--- src/chromiumos/tast/local/bundles/cros/wilco/api_routine.go
+++ src/chromiumos/tast/local/bundles/cros/wilco/api_routine.go
@@ -36,9 +36,11 @@ func init() {
 
 func APIRoutine(ctx context.Context, s *testing.State) {
 	// executeRoutine sends the request in rrRequest, executing the routine, and
-	// checks the result against shouldFail.
+	// checks the result against expectedStatus. Routine would be cancelled,
+	// while.expectedStatus is specified as ROUTINE_STATUS_CANCELLED.
 	executeRoutine := func(ctx context.Context,
-		rrRequest dtcpb.RunRoutineRequest, shouldFail bool) error {
+		rrRequest dtcpb.RunRoutineRequest,
+		expectedStatus dtcpb.DiagnosticRoutineStatus) error {
 
 		ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
 		defer cancel()
@@ -52,6 +54,15 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 		uuid := rrResponse.Uuid
 		response := dtcpb.GetRoutineUpdateResponse{}
 
+		// Some routines would not get back to service right away, so shortening
+		// test time by cancelling them then check the result if as expected.
+		if expectedStatus == dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_CANCELLED {
+			err = routines.CancelRoutine(ctx, uuid)
+			if err != nil {
+				return errors.Wrap(err, "routine not cancelled")
+			}
+		}
+
 		err = routines.WaitUntilRoutineChangesState(ctx, uuid, dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_RUNNING, 2*time.Second)
 		if err != nil {
 			return errors.Wrap(err, "routine not finished")
@@ -63,14 +74,8 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 		}
 
 		s.Log("Routine status message: ", response.StatusMessage)
-		if shouldFail {
-			if response.Status != dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_FAILED {
-				return errors.Errorf("invalid status; got %s, want ROUTINE_STATUS_FAILED", response.Status)
-			}
-		} else {
-			if response.Status != dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_PASSED {
-				return errors.Errorf("invalid status; got %s, want ROUTINE_STATUS_PASSED", response.Status)
-			}
+		if response.Status != expectedStatus {
+			return errors.Errorf("invalid status; got %s, want %s", response.Status, expectedStatus)
 		}
 
 		err = routines.RemoveRoutine(ctx, uuid)
@@ -82,9 +87,9 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 	}
 
 	for _, param := range []struct {
-		name       string
-		request    dtcpb.RunRoutineRequest
-		shouldFail bool
+		name           string
+		request        dtcpb.RunRoutineRequest
+		expectedStatus dtcpb.DiagnosticRoutineStatus
 	}{
 		{
 			name: "urandom",
@@ -96,7 +101,7 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 					},
 				},
 			},
-			shouldFail: false,
+			expectedStatus: dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_PASSED,
 		},
 		{
 			name: "battery",
@@ -109,7 +114,7 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 					},
 				},
 			},
-			shouldFail: false,
+			expectedStatus: dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_PASSED,
 		},
 		{
 			name: "battery_fail",
@@ -124,7 +129,7 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 			},
 			// HighMah is 100 (all devices should have a battery larger than
 			// this).
-			shouldFail: true,
+			expectedStatus: dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_FAILED,
 		},
 		{
 			name: "battery_sysfs",
@@ -137,7 +142,7 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 					},
 				},
 			},
-			shouldFail: false,
+			expectedStatus: dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_PASSED,
 		},
 		{
 			name: "smartctl",
@@ -147,7 +152,7 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 					SmartctlCheckParams: &dtcpb.SmartctlCheckRoutineParameters{},
 				},
 			},
-			shouldFail: false,
+			expectedStatus: dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_PASSED,
 		},
 		{
 			name: "nvme_wear_level",
@@ -159,7 +164,7 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 					},
 				},
 			},
-			shouldFail: false,
+			expectedStatus: dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_PASSED,
 		},
 		{
 			name: "nvme_wear_level_failed",
@@ -171,14 +176,34 @@ func APIRoutine(ctx context.Context, s *testing.State) {
 					},
 				},
 			},
-			shouldFail: true,
+			expectedStatus: dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_FAILED,
+		},
+		{
+			name: "nvme_short_self_test",
+			request: dtcpb.RunRoutineRequest{
+				Routine: dtcpb.DiagnosticRoutine_ROUTINE_NVME_SHORT_SELF_TEST,
+				Parameters: &dtcpb.RunRoutineRequest_NvmeShortSelfTestParams{
+					NvmeShortSelfTestParams: &dtcpb.NvmeShortSelfTestRoutineParameters{},
+				},
+			},
+			expectedStatus: dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_CANCELLED,
+		},
+		{
+			name: "nvme_long_self_test",
+			request: dtcpb.RunRoutineRequest{
+				Routine: dtcpb.DiagnosticRoutine_ROUTINE_NVME_LONG_SELF_TEST,
+				Parameters: &dtcpb.RunRoutineRequest_NvmeLongSelfTestParams{
+					NvmeLongSelfTestParams: &dtcpb.NvmeLongSelfTestRoutineParameters{},
+				},
+			},
+			expectedStatus: dtcpb.DiagnosticRoutineStatus_ROUTINE_STATUS_CANCELLED,
 		},
 	} {
 		// Here we time how long the execution of each routine takes as they are
 		// run in the same test.
 
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
-			if err := executeRoutine(ctx, param.request, param.shouldFail); err != nil {
+			if err := executeRoutine(ctx, param.request, param.expectedStatus); err != nil {
 				s.Error("Routine test failed: ", err)
 			}
 		})
