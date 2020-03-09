3016363cfd0610bffd6a13fa41ea24d6a6ac020f
Sammie Quon <sammiequon@chromium.org>
2020-03-09 14:50:18 -0700

    tast-tests: Add DragWindowFromShelfPerf test.
    Test dragging into overview for now, additional tests may be
    added later.
    
    BUG=chromium:1048248
    TEST=tast -vebose run <IP> ui.DragWindowFromShelfPerf
    
    Change-Id: Iba44b37f4e912ca06b6953de6235503146833e4d

---
+++ src/chromiumos/tast/local/bundles/cros/ui/drag_window_from_shelf_perf.go
@@ -0,0 +1,115 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/media/cpu"
+	"chromiumos/tast/local/perf"
+	"chromiumos/tast/local/ui"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DragWindowFromShelfPerf,
+		Desc:         "Measures the presentation time of dragging a window from the shelf in tablet mode",
+		Contacts:     []string{"tbarzic@chromium.org", "xdai@chromium.org", "chromeos-wmp@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", "tablet_mode"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func DragWindowFromShelfPerf(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	orientation, err := display.GetOrientation(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to obtain the display rotation: ", err)
+	}
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, true)
+	if err != nil {
+		s.Fatal("Failed to ensure in tablet mode: ", err)
+	}
+	defer cleanup(ctx)
+
+	// Prepare the touch screen as this test requires touch scroll events.
+	tsw, err := input.Touchscreen(ctx)
+	if err != nil {
+		s.Fatal("Failed to create touch screen event writer: ", err)
+	}
+	defer tsw.Close()
+
+	if err = tsw.SetRotation(-orientation.Angle); err != nil {
+		s.Fatal("Failed to set rotation: ", err)
+	}
+
+	stw, err := tsw.NewSingleTouchWriter()
+	if err != nil {
+		s.Fatal("Failed to create single touch writer: ", err)
+	}
+	defer stw.Close()
+
+	const numWindows = 8
+	conns, err := ash.CreateWindows(ctx, cr, ui.PerftestURL, numWindows)
+	if err != nil {
+		s.Fatal("Failed to open browser windows: ", err)
+	}
+	defer conns.Close()
+
+	if err := cpu.WaitUntilIdle(ctx); err != nil {
+		s.Fatal("Failed waiting for CPU to become idle: ", err)
+	}
+
+	histograms, err := metrics.Run(ctx, tconn, func() error {
+		if err := ash.DragToShowOverview(ctx, tsw, stw, tconn); err != nil {
+			return errors.Wrap(err, "failed to drag from bottom of the screen to show overview")
+		}
+
+		return nil
+	},
+		"Ash.DragWindowFromShelf.PresentationTime",
+		"Ash.DragWindowFromShelf.PresentationTime.MaxLatency")
+	if err != nil {
+		s.Fatal("Failed to swipe or get histogram: ", err)
+	}
+
+	// Return the device back to non-overview mode.
+	if err := ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
+		s.Fatal("It does not appear to be in the overview mode: ", err)
+	}
+
+	pv := perf.NewValues()
+	for _, h := range histograms {
+		mean, err := h.Mean()
+		if err != nil {
+			s.Fatalf("Failed to get mean for histogram %s: %v", h.Name, err)
+		}
+
+		pv.Set(perf.Metric{
+			Name:      h.Name,
+			Unit:      "ms",
+			Direction: perf.SmallerIsBetter,
+		}, mean)
+	}
+
+	if err := pv.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
--- src/chromiumos/tast/local/chrome/ash/wm.go
+++ src/chromiumos/tast/local/chrome/ash/wm.go
@@ -19,6 +19,7 @@ import (
 	"chromiumos/tast/local/chrome/cdputil"
 	"chromiumos/tast/local/chrome/display"
 	"chromiumos/tast/local/coords"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
 
@@ -454,4 +455,74 @@ func FindFirstWindowInOverview(ctx context.Context, tconn *chrome.TestConn) (*Wi
 		return nil, errors.New("no windows are in overview mode")
 	}
 	return result, nil
+}
+
+// DragToShowOverview shows overview by dragging up, pausing for the gesture to be recognized, then ending the gesture.
+// Note that this action only works in tablet mode.
+func DragToShowOverview(ctx context.Context, tsw *input.TouchscreenEventWriter, stw *input.SingleTouchEventWriter, tconn *chrome.TestConn) error {
+	windows, err := GetAllWindows(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to get all windows")
+	}
+	if len(windows) == 0 {
+		return errors.Wrap(err, "there must be at least one window to go to overview")
+	}
+
+	startX := tsw.Width() / 2
+	startY := tsw.Height() - 1
+
+	endX := startX
+	endY := tsw.Height() / 2
+
+	testing.ContextLog(ctx, "Dragging from the bottom slowly to open overview")
+	if err := stw.Swipe(ctx, startX, startY, endX, endY, 500*time.Millisecond); err != nil {
+		return errors.Wrap(err, "failed to swipe")
+	}
+	// Wait with the swipe paused so the overview mode gesture is recognized. Use 1 second because this is roughly the amount of time it takes for the 'swipe up and hold' overview gesture to trigger.
+	const pauseDuration = time.Second
+	if err := testing.Sleep(ctx, pauseDuration); err != nil {
+		return errors.Wrap(err, "failed to sleep while waiting for overview to trigger")
+	}
+	if err := stw.End(); err != nil {
+		return errors.Wrap(err, "failed to finish the swipe gesture")
+	}
+
+	// When the drag up ends overview is already fully shown. The only thing that remains is to wait for the windows to finish animating to their final point in the overview grid.
+	for _, window := range windows {
+		if err := WaitWindowFinishAnimating(ctx, tconn, window.ID); err != nil {
+			return errors.Wrap(err, "failed to wait for the dragged window to animate")
+		}
+	}
+
+	// Now that all windows are done animating, ensure overview is still shown.
+	if err := WaitForOverviewState(ctx, tconn, Shown); err != nil {
+		return errors.Wrap(err, "failed to wait for animation to finish")
+	}
+	return nil
+}
+
+// TouchCoordConverter manages the conversion between locations in DIP and
+// the TouchCoord of the touchscreen.
+type TouchCoordConverter struct {
+	ScaleX float64
+	ScaleY float64
+}
+
+// ConvertLocation converts a location to TouchCoord.
+func (tcc *TouchCoordConverter) ConvertLocation(l coords.Point) (x, y input.TouchCoord) {
+	return input.TouchCoord(tcc.ScaleX * float64(l.X)), input.TouchCoord(tcc.ScaleY * float64(l.Y))
+}
+
+// NewTouchCoordConverter creates a new TouchCoordConverter for the internal
+// display with the given TouchscreenEventWriter.
+func NewTouchCoordConverter(ctx context.Context, tconn *chrome.TestConn, tsew *input.TouchscreenEventWriter) (*TouchCoordConverter, error) {
+	info, err := display.GetInternalInfo(ctx, tconn)
+	if err != nil {
+		return nil, errors.Wrap(err, "no internal display found")
+	}
+
+	return &TouchCoordConverter{
+		ScaleX: float64(tsew.Width()) / float64(info.Bounds.Width),
+		ScaleY: float64(tsew.Height()) / float64(info.Bounds.Height),
+	}, nil
 }
