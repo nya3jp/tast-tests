98373a17367a63bffae78c3f5ad5580a981534db
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-03-10 12:14:40 -0700

    Run UIAutomator Test Using Tast For Slack and Hulu
    BUG=b:150013637
    BUG=b:150013142
    TEST=None
    
    Tast Test for Slack and Hulu app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: I69cbdbd451c3c498ec8ecf7c1925292fdc093bb3

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/hulu.go
@@ -0,0 +1,242 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForHulu = []testutil.TestSuite{
+	{Name: "Launch app in Clamshell", Fn: launchAppForHulu},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForHulu = []testutil.TestSuite{
+	{Name: "Launch app in Touchview", Fn: launchAppForHulu},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Hulu,
+		Desc:         "Functional test for Hulu that installs the app also verifies it is logged in and that the main page is open, checks Hulu correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForHulu,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               arc.BootedAppCompat(),
+		}, {
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForHulu,
+			},
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               arc.BootedInTabletModeAppCompat(),
+		}, {
+			Name: "vm",
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForHulu,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               arc.VMBootedAppCompat(),
+		}, {
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForHulu,
+			},
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               arc.VMBootedInTabletModeAppCompat(),
+		}},
+		Timeout: 10 * time.Minute,
+		Vars: []string{"arcappcompat.username", "arcappcompat.password",
+			"arcappcompat.Hulu.emailid", "arcappcompat.Hulu.password", "arcappcompat.Hulu.username"},
+	})
+}
+
+// Hulu test uses library for opting into the playstore and installing app.
+// Checks Hulu correctly changes the window states in both clamshell and touchview mode.
+func Hulu(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.hulu.plus"
+		appActivity = "com.hulu.features.splash.SplashActivity"
+	)
+
+	// Step up chrome on Chromebook.
+	cr, tconn, a, d := testutil.SetUpDevice(ctx, s, appPkgName, appActivity)
+	defer d.Close()
+
+	testSet := s.Param().(testutil.TestParams)
+	// Run the different test cases.
+	for idx, test := range testSet.Tests {
+		// Run subtests.
+		s.Run(ctx, test.Name, func(ctx context.Context, s *testing.State) {
+			defer func() {
+				if s.HasError() {
+					path := fmt.Sprintf("%s/screenshot-arcappcompat-failed-test-%d.png", s.OutDir(), idx)
+					if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
+						s.Log("Failed to capture screenshot: ", err)
+					}
+				}
+			}()
+			// Launch the app.
+			act, err := arc.NewActivity(a, appPkgName, appActivity)
+			if err != nil {
+				s.Fatal("Failed to create new app activity: ", err)
+			}
+			s.Log("Created new app activity")
+
+			defer act.Close()
+
+			if err := act.Start(ctx, tconn); err != nil {
+				s.Fatal("Failed to start app: ", err)
+			}
+			s.Log("App launched successfully")
+
+			defer act.Stop(ctx)
+
+			testutil.DetectAndCloseCrashOrAppNotResponding(ctx, s, tconn, a, d, appPkgName)
+			test.Fn(ctx, s, tconn, a, d, appPkgName, appActivity)
+		})
+	}
+}
+
+// launchAppForHulu verifies Hulu is logged in and
+// verify Hulu reached main activity page of the app.
+func launchAppForHulu(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		loginID         = "com.hulu.plus:id/secondary_button"
+		enterEmailID    = "com.hulu.plus:id/email"
+		enterPasswordID = "com.hulu.plus:id/password"
+		loginButtonID   = "com.hulu.plus:id/login_button"
+		homeIconID      = "com.hulu.plus:id/menu_home"
+	)
+
+	if currentAppPkg := testutil.CurrentAppPackage(ctx, s, d); currentAppPkg != appPkgName {
+		s.Fatal("Failed to launch the app: ", currentAppPkg)
+	}
+	s.Log("App is launched successfully in launchAppForHulu")
+
+	loginButton := d.Object(ui.ID(loginID))
+	enterEmailAddress := d.Object(ui.ID(enterEmailID))
+	// Keep clicking login Button until enterEmailAddress exist in the home page.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := enterEmailAddress.Exists(ctx); err != nil {
+			loginButton.Click(ctx)
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.LongUITimeout}); err != nil {
+		s.Log("EnterEmailAddress doesn't exist: ", err)
+	} else {
+		s.Log("EnterEmailAddress does exists")
+	}
+	// Enter email address.
+	huluEmailID := s.RequiredVar("arcappcompat.Hulu.emailid")
+	if err := enterEmailAddress.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("EnterEmailAddress doesn't exist: ", err)
+	} else if err := enterEmailAddress.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterEmailAddress: ", err)
+	} else if enterEmailAddress.SetText(ctx, huluEmailID); err != nil {
+		s.Fatal("Failed to enterEmailAddress: ", err)
+	}
+
+	// Enter Password.
+	huluPassword := s.RequiredVar("arcappcompat.Hulu.password")
+	enterPassword := d.Object(ui.ID(enterPasswordID))
+	if err := enterPassword.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("EnterPassword doesn't exist: ", err)
+	} else if err := enterPassword.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterPassword: ", err)
+	} else if enterPassword.SetText(ctx, huluPassword); err != nil {
+		s.Fatal("Failed to enterPassword: ", err)
+	}
+
+	testSet := s.Param().(testutil.TestParams)
+	deviceMode := "clamshell"
+	if testSet.TabletMode {
+		deviceMode = "tablet"
+		// Press back to make login button visible.
+		if err := d.PressKeyCode(ctx, ui.KEYCODE_BACK, 0); err != nil {
+			s.Log("Failed to enter KEYCODE_BACK: ", err)
+		} else {
+			s.Log("Entered KEYCODE_BACK")
+		}
+	}
+	s.Logf("device %v mode", deviceMode)
+
+	// Click on Login button again.
+	clickOnLoginButton := d.Object(ui.ID(loginButtonID))
+	if err := clickOnLoginButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("Login Button doesn't exist: ", err)
+	} else if err := clickOnLoginButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnLoginButton: ", err)
+	}
+
+	// Select User.
+	userText := s.RequiredVar("arcappcompat.Hulu.username")
+	selectUser := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.Text(userText))
+	if err := selectUser.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("SelectUser doesn't exist: ", err)
+	} else if err := selectUser.Click(ctx); err != nil {
+		s.Fatal("Failed to click on selectUser: ", err)
+	}
+
+	// Check for home icon.
+	homeIcon := d.Object(ui.ID(homeIconID))
+	if err := homeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("SelectUser doesn't exist: ", err)
+	}
+
+	signOutOfHulu(ctx, s, a, d, appPkgName, appActivity)
+
+}
+
+// signOutOfHulu verifies app is signed out.
+func signOutOfHulu(ctx context.Context, s *testing.State, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		accountIconID    = "com.hulu.plus:id/menu_account"
+		logOutOfHuluText = "Log out of Hulu"
+	)
+
+	// Click on account icon.
+	accountIcon := d.Object(ui.ID(accountIconID))
+	if err := accountIcon.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("AccountIcon doesn't exist: ", err)
+	} else if err := accountIcon.Click(ctx); err != nil {
+		s.Fatal("Failed to click on accountIcon: ", err)
+	}
+
+	// Click on log out of Hulu.
+	logOutOfHulu := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.Text(logOutOfHuluText))
+	if err := logOutOfHulu.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("LogOutOfHulu doesn't exist: ", err)
+	} else if err := logOutOfHulu.Click(ctx); err != nil {
+		s.Fatal("Failed to click on logOutOfHulu: ", err)
+	}
+}
---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/slack.go
@@ -0,0 +1,232 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForSlack = []testutil.TestSuite{
+	{Name: "Launch app in Clamshell", Fn: launchAppForSlack},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForSlack = []testutil.TestSuite{
+	{Name: "Launch app in Touchview", Fn: launchAppForSlack},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Slack,
+		Desc:         "Functional test for Slack that installs the app also verifies it is logged in and that the main page is open, checks Slack correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForSlack,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               arc.BootedAppCompat(),
+		}, {
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForSlack,
+			},
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               arc.BootedInTabletModeAppCompat(),
+		}, {
+			Name: "vm",
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForSlack,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               arc.VMBootedAppCompat(),
+		}, {
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForSlack,
+			},
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               arc.VMBootedInTabletModeAppCompat(),
+		}},
+		Timeout: 10 * time.Minute,
+		Vars: []string{"arcappcompat.username", "arcappcompat.password",
+			"arcappcompat.Slack.emailid", "arcappcompat.Slack.password", "arcappcompat.Slack.workspace"},
+	})
+}
+
+// Slack test uses library for opting into the playstore and installing app.
+// Checks Slack correctly changes the window states in both clamshell and touchview mode.
+func Slack(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.Slack"
+		appActivity = "com.Slack.ui.HomeActivity"
+	)
+
+	// Step up chrome on Chromebook.
+	cr, tconn, a, d := testutil.SetUpDevice(ctx, s, appPkgName, appActivity)
+	defer d.Close()
+
+	testSet := s.Param().(testutil.TestParams)
+	// Run the different test cases.
+	for idx, test := range testSet.Tests {
+		// Run subtests.
+		s.Run(ctx, test.Name, func(ctx context.Context, s *testing.State) {
+			defer func() {
+				if s.HasError() {
+					path := fmt.Sprintf("%s/screenshot-arcappcompat-failed-test-%d.png", s.OutDir(), idx)
+					if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
+						s.Log("Failed to capture screenshot: ", err)
+					}
+				}
+			}()
+			// Launch the app.
+			act, err := arc.NewActivity(a, appPkgName, appActivity)
+			if err != nil {
+				s.Fatal("Failed to create new app activity: ", err)
+			}
+			s.Log("Created new app activity")
+
+			defer act.Close()
+
+			if err := act.Start(ctx, tconn); err != nil {
+				s.Fatal("Failed to start app: ", err)
+			}
+			s.Log("App launched successfully")
+
+			defer act.Stop(ctx)
+
+			testutil.DetectAndCloseCrashOrAppNotResponding(ctx, s, tconn, a, d, appPkgName)
+			test.Fn(ctx, s, tconn, a, d, appPkgName, appActivity)
+		})
+	}
+}
+
+// launchAppForSlack verifies Slack is logged in and
+// verify Slack reached main activity page of the app.
+func launchAppForSlack(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		signInID         = "com.Slack:id/sign_in_button"
+		noneOfTheAboveID = "com.google.android.gms:id/cancel"
+		signInManuallyID = "com.Slack:id/sign_in_manually_button"
+		workSpaceURLID   = "com.Slack:id/team_url_edit_text"
+		nextText         = "Next"
+		enterEmailID     = "com.Slack:id/email_edit_text"
+		enterPasswordID  = "com.Slack:id/password_edit_text"
+		homeIconID       = "com.Slack:id/title"
+		homeIconText     = "Home"
+	)
+
+	if currentAppPkg := testutil.CurrentAppPackage(ctx, s, d); currentAppPkg != appPkgName {
+		s.Fatal("Failed to launch the app: ", currentAppPkg)
+	}
+	s.Log("App is launched successfully in launchAppForSlack")
+
+	// Click on sign in button.
+	signInButton := d.Object(ui.ID(signInID))
+	if err := signInButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("SignIn Button doesn't exist: ", err)
+	} else if err := signInButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on signInButton: ", err)
+	}
+
+	// Click on none of the above button.
+	noneOfTheButton := d.Object(ui.ID(noneOfTheAboveID))
+	if err := noneOfTheButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("NoneOfTheButton doesn't exist: ", err)
+	} else if err := noneOfTheButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on noneOfTheButton: ", err)
+	}
+
+	// Click on sign in manually.
+	signInManuallyButton := d.Object(ui.ID(signInManuallyID))
+	if err := signInManuallyButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("SignInManually Button doesn't exist: ", err)
+	} else if err := signInManuallyButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on signInManuallyButton: ", err)
+	}
+
+	// Click on workspace url.
+	workSpace := s.RequiredVar("arcappcompat.Slack.workspace")
+	enterWorkSpaceURL := d.Object(ui.ID(workSpaceURLID))
+	if err := enterWorkSpaceURL.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("EnterWorkSpaceURL doesn't exist: ", err)
+	} else if err := enterWorkSpaceURL.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterWorkSpaceURL: ", err)
+	} else if err := enterWorkSpaceURL.SetText(ctx, workSpace); err != nil {
+		s.Fatal("Failed to enterWorkSpaceURL: ", err)
+	}
+
+	// Click on next button
+	nextButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.Text("Next"))
+	if err := nextButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("next Button doesn't exist: ", err)
+	} else if err := nextButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on nextButton: ", err)
+	}
+
+	// Enter email address.
+	slackEmailID := s.RequiredVar("arcappcompat.Slack.emailid")
+	enterEmailAddress := d.Object(ui.ID(enterEmailID))
+	if err := enterEmailAddress.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("EnterEmailAddress doesn't exist: ", err)
+	} else if err := enterEmailAddress.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterEmailAddress: ", err)
+	} else if err := enterEmailAddress.SetText(ctx, slackEmailID); err != nil {
+		s.Fatal("Failed to enterEmailAddress: ", err)
+	}
+
+	// Click on next button.
+	if err := nextButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("Next Button doesn't exist: ", err)
+	} else if err := nextButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on nextButton: ", err)
+	}
+	// Enter Password.
+	slackPassword := s.RequiredVar("arcappcompat.Slack.password")
+	enterPassword := d.Object(ui.ID(enterPasswordID))
+	if err := enterPassword.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("EnterPassword doesn't exist: ", err)
+	} else if err := enterPassword.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterPassword: ", err)
+	} else if err := enterPassword.SetText(ctx, slackPassword); err != nil {
+		s.Fatal("Failed to enterPassword: ", err)
+	}
+
+	// Click on next button.
+	if err := nextButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("Next Button doesn't exist: ", err)
+	} else if err := nextButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on nextButton: ", err)
+	}
+
+	// Check for home icon.
+	homeIcon := d.Object(ui.ID(homeIconID), ui.Text(homeIconText))
+	if err := homeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("HomeIcon doesn't exist: ", err)
+	}
+}
