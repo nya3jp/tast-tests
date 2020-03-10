0b755d4618301f3ee7ef250eda6ec9a11ca32c4a
Tetsui Ohkubo <tetsui@chromium.org>
2020-03-10 16:13:34 +0900

    Add a Tast test moving an ARC window outside the display
    Previously there was regression that a window was cropped at the edge of
    the display. This test ensures an ARC window can be moved outside the
    display.
    
    BUG=b:144459779,1017254
    TEST=tast run eve-DUT arc.WindowOutsideDisplay
    TEST=checked reverting http://ag/9733260 should fail the test
    
    Change-Id: I1fdf58f127a2bbe34c4464213d6e66d2ba79abb1

---
+++ src/chromiumos/tast/local/bundles/cros/arc/window_outside_display.go
@@ -0,0 +1,140 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/coords"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WindowOutsideDisplay,
+		Desc:         "Ensures an ARC window can move outside the display",
+		Contacts:     []string{"tetsui@chromium.org", "arc-eng@google.com"},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+			Val:               []string{},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Val:               []string{"--enable-arcvm"},
+		}},
+	})
+}
+
+func WindowOutsideDisplay(ctx context.Context, s *testing.State) {
+	args := s.Param().([]string)
+	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs("--force-tablet-mode=clamshell"), chrome.ExtraArgs(args...))
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	disp, err := arc.NewDisplay(a, arc.DefaultDisplayID)
+	if err != nil {
+		s.Fatal("Could not create a new Display: ", err)
+	}
+	defer disp.Close()
+	sz, err := disp.Size(ctx)
+	if err != nil {
+		s.Fatal("Failed to get the display size: ", err)
+	}
+
+	w, h := sz.Width, sz.Height
+
+	const (
+		pkg          = "com.android.settings"
+		activityName = ".Settings"
+		swipeDur     = time.Second
+		marginPX     = 2
+	)
+
+	act, err := arc.NewActivity(a, pkg, activityName)
+	if err != nil {
+		s.Fatal("Failed to create the settings activity: ", err)
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx); err != nil {
+		s.Fatal("Failed to start the settings activity: ", err)
+	}
+	defer act.Stop(ctx)
+
+	if err := act.SetWindowState(ctx, arc.WindowStateNormal); err != nil {
+		s.Fatal("Failed to set the window state to normal: ", err)
+	}
+
+	// ResizeWindow and MoveWindow are implemented with touchscreen event injection.
+	// Due to its floating point precision, results of these operations are not strictly accurate.
+	nearlyEqual := func(a, b int) bool {
+		diff := a - b
+		if diff < 0 {
+			diff = -diff
+		}
+		return diff <= marginPX
+	}
+
+	waitForWindowBounds := func(ctx context.Context, expected coords.Rect) error {
+		return testing.Poll(ctx, func(ctx context.Context) error {
+			actual, err := act.WindowBounds(ctx)
+			if err != nil {
+				return testing.PollBreak(err)
+			}
+
+			if !nearlyEqual(expected.Left, actual.Left) ||
+				!nearlyEqual(expected.Top, actual.Top) ||
+				!nearlyEqual(expected.Width, actual.Width) ||
+				!nearlyEqual(expected.Height, actual.Height) {
+				return errors.Errorf("window bounds doesn't match: got %v, want %v", actual, expected)
+			}
+
+			return nil
+		}, &testing.PollOptions{Timeout: 10 * time.Second})
+	}
+
+	if err := act.MoveWindow(ctx, coords.Point{0, 0}, swipeDur); err != nil {
+		s.Fatal("Failed to move the activity: ", err)
+	}
+
+	if err := act.ResizeWindow(ctx, arc.BorderBottomRight, coords.Point{w / 2, h / 2}, swipeDur); err != nil {
+		s.Fatal("Failed to resize the activity: ", err)
+	}
+
+	if err := waitForWindowBounds(ctx, coords.Rect{0, 0, w / 2, h / 2}); err != nil {
+		s.Fatal("Failed to wait for the activity to resize: ", err)
+	}
+
+	outset := w / 8
+
+	left := -outset
+	top := 0
+	right := w/2 + outset
+	bottom := h/2 + outset
+
+	for _, origin := range []coords.Point{{left, top}, {right, top}, {right, bottom}, {left, bottom}} {
+		if err := act.MoveWindow(ctx, origin, swipeDur); err != nil {
+			s.Fatal("Failed to move the activity: ", err)
+		}
+
+		if err := waitForWindowBounds(ctx, coords.Rect{origin.X, origin.Y, w / 2, h / 2}); err != nil {
+			s.Fatal("Failed to wait for the activity to move: ", err)
+		}
+	}
+}
