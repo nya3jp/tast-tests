f1490fada6d44af755f50979c0c36ae2c0e9c1af
Matthias Springer <springerm@google.com>
2020-03-12 09:01:45 +0900

    tast GCA camera preview perf test
    Introduces PowerCameraPreviewPerf, a performance test that measures
    power use while running the Google Camera App in preview mode.
    
    BUG=b:150182688
    TEST=tast run $DUT arc.PowerCameraGcaPreviewPerf.*
    
    Change-Id: Ic685d53cbcbfb2830621b02e47d9da0c381af0e0

---
+++ src/chromiumos/tast/local/power/setup/setup_app.go
@@ -0,0 +1,58 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package setup
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+// GrantPermission grants an Android permission to an app.
+func GrantPermission(ctx context.Context, a *arc.ARC, pkg string, permission string) (CleanupCallback, error) {
+	testing.ContextLogf(ctx, "Granting permission %q to Android app %q", permission, pkg)
+	if err := a.Command(ctx, "pm", "grant", pkg, permission).Run(testexec.DumpLogOnError); err != nil {
+		return nil, errors.Wrapf(err, "failed to grant permission %q to %q", permission, pkg)
+	}
+
+	return func(ctx context.Context) error {
+		testing.ContextLogf(ctx, "Revoking permission %q from Android app %q", permission, pkg)
+		return a.Command(ctx, "pm", "revoke", pkg, permission).Run(testexec.DumpLogOnError)
+	}, nil
+}
+
+// InstallApp installs an Android APK.
+func InstallApp(ctx context.Context, a *arc.ARC, apkDataPath string, pkg string) (CleanupCallback, error) {
+	testing.ContextLogf(ctx, "Installing Android app %q", pkg)
+	if err := a.Install(ctx, apkDataPath); err != nil {
+		return nil, errors.Wrapf(err, "failed to install apk %q", apkDataPath)
+	}
+
+	return func(ctx context.Context) error {
+		testing.ContextLogf(ctx, "Uninstalling Android app %q", pkg)
+		return a.Uninstall(ctx, pkg)
+	}, nil
+}
+
+// StartActivity starts an Android activity.
+func StartActivity(ctx context.Context, a *arc.ARC, pkg string, activityName string) (CleanupCallback, error) {
+	testing.ContextLogf(ctx, "Starting activity %s/%s", pkg, activityName)
+	activity, err := arc.NewActivity(a, pkg, activityName)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create activity %q in package %q", activityName, pkg)
+	}
+	if err := activity.Start(ctx); err != nil {
+		return nil, errors.Wrapf(err, "failed to start activity %q in package %q", activityName, pkg)
+	}
+
+	return func(ctx context.Context) error {
+		testing.ContextLogf(ctx, "Stopping activities in package %s", pkg)
+		defer activity.Close()
+		return activity.Stop(ctx)
+	}, nil
+}
