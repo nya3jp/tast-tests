3d764d86e9380bd10e1efb20ed7ea1738c152bf5
Andrew Xu <andrewxu@chromium.org>
2020-03-11 18:47:15 -0700

    [Tast] Scroll perf for in-app hotseat
    Collect animation smoothness data for gesture scroll on in-app hotseat.
    It should be landed after https://crrev.com/c/2098337
    
    BUG=chromium:1045269
    TEST=manually
    
    Change-Id: I7490219c07f3189eb18c40f63b20782d6498d1d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2099987
    Tested-by: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/hotseat_scroll_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/hotseat_scroll_perf.go
@@ -9,10 +9,10 @@ import (
 	"strings"
 
 	"chromiumos/tast/errors"
-	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/chrome/ui/filesapp"
 	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/media/cpu"
 	"chromiumos/tast/local/perf"
@@ -175,13 +175,18 @@ func fetchShelfScrollSmoothnessHistogram(ctx context.Context, cr *chrome.Chrome,
 	}
 
 	if isInTabletMode && !isLauncherVisible {
-		// Hide launcher by launching an app.
-		app := apps.Chrome
-		if err := apps.Launch(ctx, tconn, app.ID); err != nil {
-			return nil, errors.Wrapf(err, "failed to launch %s", app.Name)
+		// Hide launcher by launching the file app.
+		files, err := filesapp.Launch(ctx, tconn)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to hide the home launcher by activating an app")
 		}
-		if err := ash.WaitForApp(ctx, tconn, app.ID); err != nil {
-			return nil, errors.Wrapf(err, "%s did not appear in shelf after launch", app.Name)
+
+		// App should be open until the animation smoothness data is collected for in-app shelf.
+		defer files.Close(ctx)
+
+		// Swipe up the hotseat.
+		if err := ash.SwipeUpHotseatAndWaitForCompletion(ctx, tconn); err != nil {
+			return nil, errors.Wrap(err, "failed to test the in-app shelf")
 		}
 	} else if !isInTabletMode && isLauncherVisible {
 		// Show launcher fullscreen.
@@ -240,6 +245,10 @@ func HotseatScrollPerf(ctx context.Context, s *testing.State) {
 		{
 			launcherVisibility: launcherIsVisible,
 			mode:               inClamshellMode,
+		},
+		{
+			launcherVisibility: launcherIsHidden,
+			mode:               inTabletMode,
 		},
 		{
 			launcherVisibility: launcherIsVisible,
