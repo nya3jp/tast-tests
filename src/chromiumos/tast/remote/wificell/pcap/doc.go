39156dd48576d4bc6a0412c36d8787dc79dff8f7
Yen-lin Lai <yenlinlai@google.com>
2020-03-13 18:25:42 +0800

    wificell: Add pcap package for packet capturing.
    Add pcap.Capturer to control tcpdump process. This will later be used
    to capture packets in remote WiFi tests.
    
    BUG=chromium:1034875
    TEST=fast_build.sh -T
    
    Change-Id: I6f4ced2dcd667f3f439887fc920d383b894d9633

---
+++ src/chromiumos/tast/remote/wificell/pcap/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package pcap provides utilities to capture WiFi packets.
+package pcap
