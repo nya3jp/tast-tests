1e431051fa39dcb9f4d1e32f69f698a2a469a6dd
Brian Norris <briannorris@chromium.org>
2020-03-13 17:09:07 -0700

    crypto: add test certificates
    For use in 802.1X tests, among other things.
    
    I ripped only a single set of certificates out of Autotest for now, as
    that's all we're using for 802.1X tests. Other sets (non-matching;
    subject-alternative support; etc.) can be ported later.
    
    Note that we currently allow and use MD5 (crbug.com/1047146). We should
    upgrade our test certificates as we properly phase support out in
    production.
    
    BUG=chromium:1041155, chromium:1047146
    TEST=unit tests
    
    Change-Id: I6380128dcb2a3aa4901d719aeaf3835c0592c087

---
+++ src/chromiumos/tast/common/crypto/certificate/cert_test.go
@@ -0,0 +1,91 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package certificate
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+
+	"chromiumos/tast/errors"
+)
+
+func pemDecode(s string) ([]byte, error) {
+	block, rest := pem.Decode([]byte(s))
+	if block == nil {
+		return nil, errors.New("Couldn't decode Cert PEM")
+	}
+	if len(rest) != 0 {
+		return nil, errors.Errorf("Found trailing data in cert: %q", string(rest))
+	}
+	return block.Bytes, nil
+}
+
+// validateCertSignature checks that cert is signed by its parent. Note that we allow MD5-based signatures for now
+// (crbug.com/1047146), and because Golang's x509 library rejects this weak crypto, we can't easily verify signatures
+// properly.
+func validateCertSignature(cert, parent *x509.Certificate) error {
+	err := cert.CheckSignatureFrom(parent)
+	if err != nil {
+		// TODO(crbug.com/1047146): MD5 certificates are rejected by Golang x509. We're still allowing them for now.
+		var insecureErr x509.InsecureAlgorithmError
+		if !errors.As(err, &insecureErr) {
+			return err
+		}
+	}
+	return nil
+}
+
+func TestCertificate(t *testing.T) {
+	c := GetTestCertificate()
+
+	// Parse certificates. They should all be X-509 certificates in PEM format.
+	var cert, caCert, clientCert *x509.Certificate
+	for i, it := range []struct {
+		pem string
+		out **x509.Certificate
+	}{
+		{
+			c.Cert, &cert,
+		},
+		{
+			c.CACert, &caCert,
+		},
+		{
+			c.ClientCert, &clientCert,
+		},
+	} {
+		pem, err := pemDecode(it.pem)
+		if err != nil {
+			t.Fatalf("Failed to decode PEM %d: %v", i, err)
+		}
+		cert, err := x509.ParseCertificate(pem)
+		if err != nil {
+			t.Fatalf("Failed to parse certificate %d: %v", i, err)
+		}
+		*it.out = cert
+	}
+
+	// Parse private key. It should be a PKCS#1 key in PEM format.
+	pem, err := pemDecode(c.PrivateKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	key, err := x509.ParsePKCS1PrivateKey(pem)
+	if err != nil {
+		t.Fatal("Failed to parse private key: ", err)
+	}
+	if err = key.Validate(); err != nil {
+		t.Fatal("Private key failed validation: ", err)
+	}
+
+	// Check cert signatures.
+	if err := validateCertSignature(cert, caCert); err != nil {
+		t.Error("Failed CA cert check: ", err)
+	}
+	if err = validateCertSignature(caCert, caCert); err != nil {
+		t.Error("Unexpeted: CA cert isn't self-signed")
+	}
+}
