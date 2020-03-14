dcdf11babd64ef11beaf1f2f2efd439dcc25902e
Brian Norris <briannorris@chromium.org>
2020-03-13 17:18:07 -0700

    network/wiredhostapd: new package for hostapd over Ethernet
    In preparation for Ethernet/802.1X authentication tests. Pieces of this
    are borrowed from existing network_8021xWiredAuthentication and
    network_8021xEapDetection Autotests.
    
    Note that we have an existing chromiumos/tast/remote/wificell/hostapd
    package, but that is primarily for WiFi and doesn't end up sharing a lot
    with this implementation. Among other things, hostapd for WiFi needs a
    lot more flexibility around WiFi configuration parameters, whereas for
    Ethernet, hostapd is used almost exclusively for its built-in EAP
    server. I believe it would harm the readability and utility of both to
    force them together at the moment.
    
    This hostapd instance runs on the DUT (local test) and will typically
    listen on one end of a veth pair, acting as if it's an off-DUT
    Authenticator.
    
    We support a minimal number of actions ("start EAP authentication",
    "check STA status") to support testing EAP authentication, without
    wiring up a realistic post-authentication setup (e.g., DHCP, bridge,
    etc.).
    
    BUG=chromium:1041155
    TEST=run with WIP `tast -verbose run ${HOST} 'network.Ethernet8021X*'`
    
    Change-Id: I89107e4416a6f833579e497c821da7701079941f

---
+++ src/chromiumos/tast/local/bundles/cros/network/wiredhostapd/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package wiredhostapd contains utilities for establishing a hostapd server for use with 'driver=wired'
+// (i.e., Ethernet or similar).
+package wiredhostapd
