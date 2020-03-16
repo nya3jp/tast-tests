ec6774e6e9d3dada592d8499cc1a66a0fbc46dec
Xibin Liu <xliu@cienet.com>
2020-03-16 14:51:04 -0700

    tast-tests: MTBF test 007: Video Playback | 2K & 4K Youtube videos
    Procedure:
    1. Play YouTube video in 2K & 4K (original) resolution.
    2. Right click on the video on select 'Stats for nerds'.
    Verification:
    2.1 Video is playing, and the "Viewport / Frames" stats under "Stats for nerds" shows 0 frame dropped.
    
    BUG=None
    TEST=tast -buildbundle=mtbf -buildworkspace=<workspace> <DUT> video.MTBF007*
    
    Change-Id: I7c5df85e27eaceefb3d899e6c3c359564a787621

---
+++ src/chromiumos/tast/local/bundles/mtbf/video/mtbf007_play_youtube_4k.go
@@ -0,0 +1,88 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/common/mtbferrors"
+	"chromiumos/tast/local/bundles/mtbf/video/youtube"
+	"chromiumos/tast/local/chrome"
+	mtbfchrome "chromiumos/tast/local/mtbf/chrome"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MTBF007PlayYoutube4K,
+		Desc:         "YouTube 2K & 4K videos play properly. Only run this test on devices supporting 2K & 4K videos",
+		Contacts:     []string{"xliu@cienet.com"},
+		SoftwareDeps: []string{"chrome", "chrome_internal", "cros_video_decoder"},
+		Pre:          chrome.LoginReuse(),
+		Attr:         []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Name: "first_video",
+			Val:  "https://www.youtube.com/watch?v=suWsd372pQE",
+		}, {
+			Name: "second_video",
+			Val:  "http://www.youtube.com/watch?v=PcXOnoSN0RE",
+		}},
+	})
+}
+
+// MTBF007PlayYoutube4K play 4K/2K youtube video.
+func MTBF007PlayYoutube4K(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+	videoURL := s.Param().(string)
+
+	conn, err := mtbfchrome.NewConn(ctx, cr, youtube.Add1SecondForURL(videoURL))
+	if err != nil {
+		s.Fatal("MTBF failed: ", err)
+	}
+	defer conn.Close()
+	defer conn.CloseTarget(ctx)
+
+	s.Log("Youtube video is now ready for 4K/2K playing")
+	testing.Sleep(ctx, 5*time.Second)
+
+	for _, quality := range []string{
+		"4k",
+		"2k",
+	} {
+		s.Log("Change video quality to ", quality)
+		if err = youtube.ChangeQuality(ctx, conn, youtube.Quality[quality]); err != nil {
+			s.Error(mtbferrors.New(mtbferrors.VideoChgQuality, err, quality))
+		}
+		testing.Sleep(ctx, 20*time.Second) // Wait for video to change quality...
+		if err = youtube.VerifyPlaying(ctx, conn, 3*time.Second); err != nil {
+			s.Error(mtbferrors.New(mtbferrors.VideoNoPlay, err, videoURL))
+			path := filepath.Join(s.OutDir(), "screenshot-youtube-failed-playing.png")
+			if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
+				s.Log("Failed to capture screenshot: ", err)
+			}
+		}
+	}
+
+	s.Log("Open stats for nerd")
+	if err = youtube.OpenStatsForNerd(ctx, conn); err != nil {
+		s.Error(mtbferrors.New(mtbferrors.VideoStatsNerd, err))
+	}
+	testing.Sleep(ctx, 5*time.Second)
+
+	s.Log("Verify frame drops is zero")
+	var framedrops int
+	framedrops, err = youtube.GetFrameDropsFromStatsForNerd(ctx, conn)
+	if err != nil {
+		s.Error(mtbferrors.New(mtbferrors.VideoGetFrmDrop, err))
+	}
+	if framedrops != 0 {
+		s.Error(mtbferrors.New(mtbferrors.VideoFrmDrop, err, framedrops))
+	}
+
+	testing.Sleep(ctx, 5*time.Second)
+}
