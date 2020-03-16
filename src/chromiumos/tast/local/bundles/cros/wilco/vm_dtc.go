49ae69fc63cffdf433a731609becaca633549806
Oleh Lamzin <lamzin@google.com>
2020-03-16 17:09:38 +0100

    tast-tests: add logs before running command in wilco.VMDTC
    Add logs before running command instead of logging when command finished
    in wilco.VMDTC.
    
    Due to Tast documentation, it's recommended to log before doing some
    long operation:
    https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#When-to-log
    
    BUG=none
    TEST=tast run DUT wilco.VMDTC
    
    Change-Id: I190971eabe1f1a96c5c3060babd42f7a2de4ad51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2105221
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Tested-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Trent Begin <tbegin@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/wilco/vm_dtc.go
+++ src/chromiumos/tast/local/bundles/cros/wilco/vm_dtc.go
@@ -85,18 +85,18 @@ func VMDTC(ctx context.Context, s *testing.State) {
 	// test-ddv -s (examine single and two summary alert)
 	// test-ddv -r (examine runtime summary alert)
 	for _, param := range []string{"-g", "-s", "-r"} {
+		s.Log("Running test-ddv ", param)
 		cmd := vm.CreateVSHCommand(ctx, wilco.WilcoVMCID, libraryPath, "test-ddv", param)
 		if out, err := cmd.Output(testexec.DumpLogOnError); err != nil {
 			s.Errorf("Error running test-ddv %v: %v", param, err)
 		} else if !strings.Contains(string(out), "success") {
 			s.Errorf("test-ddv %v output does not contain `success`: %s", param, out)
-		} else {
-			s.Logf("test-ddv %v successful", param)
 		}
 	}
 
 	// test-ddtm -cmd calls wilco_dtc_supportd outside of the VM to run a
 	// diagnostic test.
+	s.Log("Running test-ddtm -cmd")
 	cmd := vm.CreateVSHCommand(ctx, wilco.WilcoVMCID, libraryPath, "test-ddtm", "-cmd", testParams)
 	if out, err := cmd.Output(testexec.DumpLogOnError); err != nil {
 		s.Error("Error running test-ddtm -cmd: ", err)
@@ -104,7 +104,5 @@ func VMDTC(ctx context.Context, s *testing.State) {
 		s.Errorf("test-ddtm -cmd output does not contain `Finish DDTM test`: %s", out)
 	} else if strings.Contains(string(out), "fail") {
 		s.Errorf("test-ddtm -cmd output contains `fail` %s", out)
-	} else {
-		s.Log("test-ddtm -cmd successful")
 	}
 }
