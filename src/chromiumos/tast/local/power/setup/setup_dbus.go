daf2a022652d2b1f1686d72146c67b5e19089155
Charles William Dick <cwd@google.com>
2020-03-16 21:29:07 +0900

    Tast power setup fix dbus
    We should not close the DBus connection in bluetooth setup, since it is
    a shared connection.
    
    Running TEST before fix had all but the first test fail.
    
    BUG=b:151601929
    TEST=tast run <DUT> arc.PowerIdlePerf*, all three pass
    
    Change-Id: Ia2db54e2cdb546fd834e8d322291b626cb94ff20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2104851
    Tested-by: Charles Dueck <cwd@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Auto-Submit: Charles Dueck <cwd@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>

--- src/chromiumos/tast/local/power/setup/setup_dbus.go
+++ src/chromiumos/tast/local/power/setup/setup_dbus.go
@@ -12,13 +12,6 @@ import (
 	"chromiumos/tast/local/dbusutil"
 )
 
-// DBusCloseConnection closes a DBus connection when the test completes.
-func DBusCloseConnection(conn *dbus.Conn) (CleanupCallback, error) {
-	return func(_ context.Context) error {
-		return conn.Close()
-	}, nil
-}
-
 // DBusProperty sets a DBus property to a passed value, and resets it at the end
 // of the test.
 func DBusProperty(ctx context.Context, obj dbus.BusObject, p string, value interface{}) (CleanupCallback, error) {
