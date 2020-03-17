e464328d386c6d0918ea62fc357ca263eda200bd
Victor-Gabriel Savu <vsavu@google.com>
2020-03-17 09:48:04 +0100

    WIP
    Change-Id: I782680472471396017cd6338ac8b208afe91e109

--- src/chromiumos/tast/services/cros/wilco/gen.go
+++ src/chromiumos/tast/services/cros/wilco/gen.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. wilco_service.proto
+//go:generate protoc -I ../../../../../../../../platform2/diagnostics/grpc/ -I . --go_out=plugins=grpc:../../../../.. wilco_service.proto
 
 // Package wilco provides the WilcoService
 package wilco
