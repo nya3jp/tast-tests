5be9a0f0154160ca1e523e641088f1332c8c6a49
Charles William Dick <cwd@google.com>
2020-03-17 21:42:21 +0900

    Tast bluetooth adapter helpers
    Introduces the bluetooth package and Adapter struct, for managing
    adapter state.
    
    BUG=b:151710832
    TEST=tast run <DUT> arc.PowerIdlePerf
    
    Change-Id: I1407a0bb959428ac4b93f93aece40e2e2b79fd83

--- src/chromiumos/tast/local/dbusutil/property.go
+++ src/chromiumos/tast/local/dbusutil/property.go
@@ -18,7 +18,7 @@ import (
 func SetProperty(ctx context.Context, obj dbus.BusObject, p string, v interface{}) error {
 	i := strings.LastIndex(p, ".")
 	if i < 0 {
-		return errors.Errorf("invalid DBus property %q", p)
+		return errors.Errorf("invalid D-Bus property %q", p)
 	}
 
 	iface := p[:i]
@@ -26,7 +26,7 @@ func SetProperty(ctx context.Context, obj dbus.BusObject, p string, v interface{
 	const dbusMethod = "org.freedesktop.DBus.Properties.Set"
 	c := obj.CallWithContext(ctx, dbusMethod, 0, iface, prop, dbus.MakeVariant(v))
 	if c.Err != nil {
-		return errors.Wrapf(c.Err, "failed to set DBUS property %q on interface %q to value %v", prop, iface, v)
+		return errors.Wrapf(c.Err, "failed to set D-Bus property %q on interface %q to value %v", prop, iface, v)
 	}
 	return nil
 }
@@ -36,7 +36,7 @@ func SetProperty(ctx context.Context, obj dbus.BusObject, p string, v interface{
 func Property(ctx context.Context, obj dbus.BusObject, p string) (interface{}, error) {
 	i := strings.LastIndex(p, ".")
 	if i < 0 {
-		return dbus.MakeVariant(nil), errors.Errorf("invalid DBus property %q", p)
+		return dbus.MakeVariant(nil), errors.Errorf("invalid D-Bus property %q", p)
 	}
 
 	const dbusMethod = "org.freedesktop.DBus.Properties.Get"
@@ -44,12 +44,40 @@ func Property(ctx context.Context, obj dbus.BusObject, p string) (interface{}, e
 	prop := p[i+1:]
 	c := obj.CallWithContext(ctx, dbusMethod, 0, iface, prop)
 	if c.Err != nil {
-		return nil, errors.Wrapf(c.Err, "failed to get DBUS property %q on interface %q", prop, iface)
+		return nil, errors.Wrapf(c.Err, "failed to get D-Bus property %q on interface %q", prop, iface)
 	}
 
 	v := dbus.Variant{}
 	if err := c.Store(&v); err != nil {
-		return nil, errors.Wrapf(c.Err, "failed to extract DBUS property %q on interface %q", prop, iface)
+		return nil, errors.Wrapf(c.Err, "failed to extract D-Bus property %q on interface %q", prop, iface)
 	}
 	return v.Value(), nil
+}
+
+// ManagedObjects gets all the objects managed under the passed object. Returns
+// a map from interface name to a slice of ObjectPaths that have an object with
+// that interface.
+func ManagedObjects(ctx context.Context, obj dbus.BusObject) (map[string][]dbus.ObjectPath, error) {
+	const dbusMethod = "org.freedesktop.DBus.ObjectManager.GetManagedObjects"
+	c := obj.CallWithContext(ctx, dbusMethod, 0)
+	if c.Err != nil {
+		return nil, errors.Wrap(c.Err, "failed to get D-Bus managed objects")
+	}
+
+	v := dbus.Variant{}
+	if err := c.Store(&v); err != nil {
+		return nil, errors.Wrap(c.Err, "failed to extract D-Bus managed objects")
+	}
+	managed, ok := v.Value().(map[dbus.ObjectPath]map[string]map[string]dbus.Variant)
+	if !ok {
+		return nil, errors.New("cannot convert managed objects to map")
+	}
+
+	paths := map[string][]dbus.ObjectPath{}
+	for objPath, ifaceToProps := range managed {
+		for objIface := range ifaceToProps {
+			paths[objIface] = append(paths[objIface], objPath)
+		}
+	}
+	return paths, nil
 }
