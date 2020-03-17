c36440ffdfadc2378fcb36e57074103b2258cc54
Woody Chow <woodychow@google.com>
2020-03-17 17:13:13 +0900

    tast-tests: Add WaitForExpInLogcat to arc
    WaitForExpInLogcat waits for a regexp to appear in the logcat with timeout
    
    BUG=None
    TEST=Use the function is graphics.GFXBench
    
    Change-Id: Ic621bd2a225cf99bd098864cc50630cd601ba96b

--- src/chromiumos/tast/local/arc/commands.go
+++ src/chromiumos/tast/local/arc/commands.go
@@ -5,9 +5,13 @@
 package arc
 
 import (
+	"bufio"
 	"context"
+	"regexp"
 	"strings"
+	"time"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/shutil"
 )
@@ -60,4 +64,46 @@ func (a *ARC) GetProp(ctx context.Context, key string) (string, error) {
 		return "", err
 	}
 	return strings.TrimSpace(string(o)), nil
+}
+
+// WaitForExpInLogcat waits for a regexp to appear in the logcat with timeout.
+func (a *ARC) WaitForExpInLogcat(ctx context.Context, exp *regexp.Regexp, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	cmd := a.Command(ctx, "logcat")
+
+	pipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to open StdoutPipe")
+	}
+	defer pipe.Close()
+
+	if err := cmd.Start(); err != nil {
+		return errors.Wrapf(err, "failed to start %s", shutil.EscapeSlice(cmd.Args))
+	}
+	defer func() {
+		cmd.Kill()
+		cmd.Wait()
+	}()
+
+	done := make(chan struct{})
+
+	go func() {
+		scanner := bufio.NewScanner(pipe)
+		for scanner.Scan() {
+			l := scanner.Text()
+			if exp.MatchString(l) {
+				close(done)
+				return
+			}
+		}
+	}()
+
+	select {
+	case <-done:
+		return nil
+	case <-ctx.Done():
+		return errors.Wrapf(ctx.Err(), "cannot match regexp %s in logcat before timeout(%s)", exp, timeout)
+	}
 }
