546b05c06b694f96c10f04b89438734f092488de
Miguel Casas <mcasas@chromium.org>
2020-03-19 18:59:39 -0400

    tast/webrtc: add a GetDisplayMedia (screen/window/tab capture) test
    This CL adds the test and JS logic to exercise the getDisplayMedia() JS
    API that allows users to capture the contents of the screen / a window
    / a tab of their choice. The test relies on a Chrome flag to bypass the
    UI picker, and for the purpose of this CL this is forced to be the
    screen, which is then verified not to be all black. This CL only lands
    the "full screen" capture case, waiting for crrev.com/c/2113410 to roll.
    
    BUG=chromium:1063449
    TEST=tast -verbose run $IP webrtc.GetDisplayMedia.monitor
     on atlas, grunt, scarlet and kukui
    
    Change-Id: Ie5557756f80ee49bd1b5e1f0cf02275fc48cd8ef

---
+++ src/chromiumos/tast/local/bundles/cros/webrtc/getdisplaymedia/get_display_media.go
@@ -0,0 +1,42 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package getdisplaymedia provides common code for WebRTC's getDisplayMedia
+// tests; this API is used for screen, window and tab capture, see
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getDisplayMedia
+// and https://w3c.github.io/mediacapture-screen-share/.
+package getdisplaymedia
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+const (
+	// HTMLFile is the file containing the HTML+JS code exercising getDisplayMedia().
+	HTMLFile = "getdisplaymedia.html"
+)
+
+// RunGetDisplayMedia drives the code verifying the getDisplayMedia functionality.
+func RunGetDisplayMedia(ctx context.Context, s *testing.State, cr *chrome.Chrome, surfaceType string) error {
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	conn, err := cr.NewConn(ctx, server.URL+"/"+HTMLFile)
+	if err != nil {
+		return errors.Wrapf(err, "failed to open %v", HTMLFile)
+	}
+	defer conn.Close()
+	defer conn.CloseTarget(ctx)
+
+	if err := conn.Call(ctx, nil, "start", surfaceType); err != nil {
+		return errors.Wrap(err, "failed to run getDisplayMedia()")
+	}
+	return nil
+}
