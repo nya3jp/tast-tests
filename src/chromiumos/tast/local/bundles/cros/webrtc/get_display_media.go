32be06211376c0c36f0c5f0453fd9a8b18a36ed5
Miguel Casas <mcasas@chromium.org>
2020-03-19 18:59:39 -0400

    tast/webrtc: add first GetDisplayMedia (screen/window/tab capture) test
    This CL adds the test and JS logic to exercise the getDisplayMedia() JS API
    that allows users to capture the screen contents or a window or a tab of
    their choice. The test relies on a Chrome flag to bypass the UI picker,
    and for the purpose of this CL this is forced to be the screen, which is
    then verified not to be all black.
    
    BUG=chromium:1063449
    TEST=tast -verbose run $IP webrtc.GetDisplayMedia.monitor
     on atlas, grunt, scarlet and kukui
    
    Change-Id: Ie5557756f80ee49bd1b5e1f0cf02275fc48cd8ef

---
+++ src/chromiumos/tast/local/bundles/cros/webrtc/get_display_media.go
@@ -0,0 +1,44 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package webrtc
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/webrtc/getdisplaymedia"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/pre"
+	"chromiumos/tast/local/webrtc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: GetDisplayMedia,
+		Desc: "Verifies that WebRTC getDisplayMedia() (screen, window, tab capture) works",
+		Contacts: []string{
+			"mcasas@chromium.org", // Test author.
+			"chromeos-gfx-video@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Data:         append(webrtc.DataFiles(), getdisplaymedia.HTMLFile),
+		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
+		// See https://w3c.github.io/mediacapture-screen-share/#displaycapturesurfacetype
+		// for where the case names come from.
+		// TODO(crbug.com/1063449): add other cases when the adequate precondition is ready.
+		Params: []testing.Param{{
+			Name: "monitor",
+			Val:  "monitor",
+			Pre:  pre.ChromeScreenCapture(),
+		}},
+	})
+}
+
+// GetDisplayMedia verifies that the homonymous API works as expected.
+func GetDisplayMedia(ctx context.Context, s *testing.State) {
+	if err := getdisplaymedia.RunGetDisplayMedia(ctx, s, s.PreValue().(*chrome.Chrome), s.Param().(string)); err != nil {
+		s.Fatal("TestPlay failed: ", err)
+	}
+}
