41b8ce1a43ae8aff4384120602c3d8b9ed56ec91
Greg Edelston <gredelston@google.com>
2020-03-20 12:59:32 -0600

    Load config data from fw-testing-configs JSON
    Design doc: go/cros-fw-testing-configs
    
    This is an incremental step. Follow-on CL's will have the &testing.Test
    datafile inherit all JSON files (not just DEFAULTS), load the correct
    JSON file based on platform name, inherit attributes from the parent
    platform(s), and create a dedicated end-to-end test for loading configs.
    
    BUG=chromium:993062
    TEST=firmware.CheckBootMode
    
    Cq-Depend: chromium:2112680
    Change-Id: I123240139a5feed52c8b7e531d123de1ffb5a976

--- src/chromiumos/tast/remote/firmware/config.go
+++ src/chromiumos/tast/remote/firmware/config.go
@@ -6,6 +6,15 @@
 
 package firmware
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"chromiumos/tast/errors"
+)
+
 // ModeSwitcherType represents which methods the platform uses for switching between DUT boot modes.
 type ModeSwitcherType string
 
@@ -16,31 +25,43 @@ const (
 	KeyboardDevSwitcher      ModeSwitcherType = "keyboard_dev_switcher"
 )
 
+// ConfigDir is the basename of the directory within remote/firmware/data/ which contains the JSON files.
+const ConfigDir = "fw-testing-configs"
+
+// defaultName is the name of the JSON file containing abstract values, without the .JSON.
+const defaultName = "DEFAULTS"
+
 // Config contains platform-specific attributes.
 // Fields are documented in autotest/server/cros/faft/configs/DEFAULTS.json.
 type Config struct {
-	ModeSwitcherType     ModeSwitcherType
-	PowerButtonDevSwitch bool
-	RecButtonDevSwitch   bool
-	FirmwareScreen       int
-	DelayRebootToPing    int
-	ConfirmScreen        int
-	USBPlug              int
+	ModeSwitcherType     ModeSwitcherType `json:"mode_switcher_type"`
+	PowerButtonDevSwitch bool             `json:"power_button_dev_switch"`
+	RecButtonDevSwitch   bool             `json:"rec_button_dev_switch"`
+	FirmwareScreen       int              `json:"firmware_screen"`
+	DelayRebootToPing    int              `json:"delay_reboot_to_ping"`
+	ConfirmScreen        int              `json:"confirm_screen"`
+	USBPlug              int              `json:"usb_plug"`
 }
 
 // NewConfig creates a new Config matching the DUT platform.
-// Normally these attributes would come from JSON files, but those files have not been added to Tast yet.
-// For now, it instead returns default values.
-// TODO(b/151469239): Populate with real, platform-specific config data.
-func NewConfig() (*Config, error) {
-	cfg := &Config{
-		ModeSwitcherType:     KeyboardDevSwitcher,
-		PowerButtonDevSwitch: false,
-		RecButtonDevSwitch:   false,
-		FirmwareScreen:       10,
-		DelayRebootToPing:    30,
-		ConfirmScreen:        3,
-		USBPlug:              10,
+// For now, it only returns default values.
+// TODO(b/151469239): Populate with config data matching the DUT platform.
+func NewConfig(configDataDir string) (*Config, error) {
+
+	// loadConfigJSON reads '${platform}.json' and loads its contents into a Config struct.
+	loadConfigJSON := func(platform string) (*Config, error) {
+		fp := filepath.Join(configDataDir, fmt.Sprintf("%s.json", platform))
+		b, err := ioutil.ReadFile(fp)
+		if err != nil {
+			return nil, errors.Wrapf(err, "reading datafile %s", fp)
+		}
+		var cfg *Config
+		err = json.Unmarshal(b, &cfg)
+		if err != nil {
+			return nil, errors.Wrapf(err, "unmarshaling json %s from datafile %s", b, fp)
+		}
+		return cfg, nil
 	}
-	return cfg, nil
+
+	return loadConfigJSON(defaultName)
 }
