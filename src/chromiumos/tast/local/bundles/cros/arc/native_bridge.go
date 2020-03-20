638de8fb8d01b3bac3bf6b614af7b6473dcd1e26
Youkichi Hosoi <youkichihosoi@google.com>
2020-03-20 23:28:13 +0900

    tast-tests: arcvm: Add arc.NativeBridge Tast test
    This test checks whether native bridge is properly setup for ARCVM.
    
    BUG=b:142361182
    TEST=~/trunk/src/platform/tast/fast_build.sh -T
    TEST=tast run <DUT> arc.NativeBridge
    TEST=tast run <DUT> arc.MountOBB
    TEST=tast run <DUT> arc.StartStop
    TEST=tast run <DUT> arc.ContainerMount
    TEST=tast run <DUT> platform.RootPartitionsNotMounted
    
    Cq-Depend: chromium:2128170
    Change-Id: I874d0b6c0363995bc26dcb625022249bf64f8b9b

---
+++ src/chromiumos/tast/local/bundles/cros/arc/native_bridge.go
@@ -0,0 +1,109 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NativeBridge,
+		Desc:         "Checks whether native bridge is properly setup for ARCVM",
+		Contacts:     []string{"youkichihosoi@chromium.org", "arcvm-eng@google.com"},
+		Data:         []string{"hello_world_arm", "hello_world_arm64"},
+		SoftwareDeps: []string{"android_vm_r", "chrome", "native_bridge"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          arc.VMBooted(),
+		Timeout:      5 * time.Minute,
+	})
+}
+
+func NativeBridge(ctx context.Context, s *testing.State) {
+	a := s.PreValue().(arc.PreData).ARC
+
+	// Check whether binfmt_misc is successfully unmounted.
+	ms, err := mountInfoForARCVM(ctx, a)
+	if err != nil {
+		s.Fatal("Failed to get mount info for ARCVM: ", err)
+	}
+	const binfmtMiscPath = "/proc/sys/fs/binfmt_misc"
+	for _, m := range ms {
+		if m.MountPath == binfmtMiscPath {
+			s.Fatalf("Failure: %q is not unmounted", binfmtMiscPath)
+		}
+	}
+
+	// Check whether the property for native bridge library is properly set.
+	const nativeBridgeProp = "ro.boot.native_bridge"
+	nb, err := a.GetProp(ctx, nativeBridgeProp)
+	if err != nil {
+		s.Fatalf("Failed to getprop %q: %v", nativeBridgeProp, err)
+	}
+	if nb != "libhoudini.so" && nb != "libndk_translation.so" {
+		s.Fatalf("Failure: the property %q is set to %q", nativeBridgeProp, nb)
+	}
+
+	// Check whether an ARM 32-bit executable can be run successfully.
+	if err := pushAndExecute(ctx, a, s.DataPath("hello_world_arm")); err != nil {
+		s.Fatal("Failed to run ARM executable: ", err)
+	}
+
+	// Check whether 64-bit native bridge is supported.
+	// TODO(youkichihosoi): update the condition once a dedicated property for 64-bit support is implemented.
+	const cpuAbilist64Prop = "ro.product.cpu.abilist64"
+	abi, err := a.GetProp(ctx, cpuAbilist64Prop)
+	if err != nil {
+		s.Fatalf("Failed to getprop %q: %v", cpuAbilist64Prop, err)
+	}
+	if abi == "x86_64,arm64-v8a" {
+		// Check whether an ARM 64-bit executable can be run successfully.
+		if err := pushAndExecute(ctx, a, s.DataPath("hello_world_arm64")); err != nil {
+			s.Fatal("Failed to run ARM 64-bit executable: ", err)
+		}
+	}
+}
+
+// pushAndExecute pushes an executable to Android's temporary directory and executes it.
+func pushAndExecute(ctx context.Context, a *arc.ARC, execPath string) (retErr error) {
+	tmpExecPath, err := a.PushFileToTmpDir(ctx, execPath)
+	if err != nil {
+		return errors.Wrapf(err, "failed to push %q to tmpdir", execPath)
+	}
+	defer func() {
+		if err := a.Command(ctx, "rm", "-f", tmpExecPath).Run(testexec.DumpLogOnError); err != nil {
+			if retErr == nil {
+				retErr = errors.Wrapf(err, "failed to remove %q", tmpExecPath)
+			} else {
+				testing.ContextLogf(ctx, "Failed to remove %q: %v", tmpExecPath, err)
+			}
+		}
+	}()
+
+	if err := a.Command(ctx, "chmod", "0755", tmpExecPath).Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrapf(err, "failed to change the permission of %q", tmpExecPath)
+	}
+	if err := a.Command(ctx, tmpExecPath).Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrapf(err, "failed to execute %q", tmpExecPath)
+	}
+	return nil
+}
+
+// mountInfoForARCVM returns a list of mount point info for ARCVM.
+func mountInfoForARCVM(ctx context.Context, a *arc.ARC) ([]sysutil.MountInfo, error) {
+	const mountInfoPath = "/proc/1/mountinfo"
+	mi, err := a.ReadFile(ctx, mountInfoPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to read mount info file %q", mountInfoPath)
+	}
+	return sysutil.ParseMountInfo(mi)
+}
