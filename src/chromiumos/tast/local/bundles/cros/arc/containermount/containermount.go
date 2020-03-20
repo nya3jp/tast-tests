9fc706873bc4d120f6bd022b966b49bbf69ede53
Yusuke Sato <yusukes@google.com>
2020-03-19 21:29:06 -0700

    tast-tests: Add test cases to arc.ContainerMount
    The cpuset settings done in init.cheets.rc work fine for homogeneous
    systems, but heterogeneous systems require their own init.cpusets.rc
    to properly set up non-foreground cpusets. This CL verifies that
    init.cpusets.rc exists on heterogeneous systems.
    
    BUG=b:151990429
    TEST=tast run DUT arc.ContainerMount, verify the new test runs only
     on bit.LITTLE devices
    
    Change-Id: Ie481a2a673d61ba69c9e683b2db5d49371268d47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2112290
    Tested-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/containermount/containermount.go
+++ src/chromiumos/tast/local/bundles/cros/arc/containermount/containermount.go
@@ -321,6 +321,45 @@ func getRootPID() (int, error) {
 	return -1, errors.New("root not found")
 }
 
+// Intel does not have any 'CPU part' line. ARM does, and when it's
+// big.LITTLE, it has two different CPU parts (e.g. 0xd03 and 0xd09).
+var cpuPartRE = regexp.MustCompile(`^CPU part\s+:\s+(0x[0-9a-f]+)$`)
+
+// isHeterogeneousCores returns true if cores are heterogeneous ones
+// such as ARM's big.LITTLE.
+func isHeterogeneousCores(ctx context.Context) (bool, error) {
+	if arch := runtime.GOARCH; arch != "arm" && arch != "arm64" {
+		return false, nil
+	}
+
+	out, err := ioutil.ReadFile("/proc/cpuinfo")
+	if err != nil {
+		return false, errors.Wrap(err, "failed to read cpuinfo")
+	}
+	lines := strings.Split(string(out), "\n")
+
+	var lastCPUPart string
+	for _, line := range lines {
+		matches := cpuPartRE.FindAllStringSubmatch(strings.TrimSpace(line), -1)
+		if matches == nil {
+			continue
+		}
+		cpuPart := matches[0][1]
+		if lastCPUPart == "" {
+			lastCPUPart = cpuPart
+			continue
+		}
+		if lastCPUPart != cpuPart {
+			testing.ContextLog(ctx, "Detected heterogeneous cores")
+			return true, nil
+		}
+	}
+	if lastCPUPart == "" {
+		return false, errors.New("no CPU part information found")
+	}
+	return false, nil
+}
+
 func testCPUSet(ctx context.Context, s *testing.State, a *arc.ARC) {
 	s.Log("Running testCPUSet")
 
@@ -331,14 +370,8 @@ func testCPUSet(ctx context.Context, s *testing.State, a *arc.ARC) {
 	}
 
 	// Verify that /dev/cpuset is properly set up.
-	types := []string{"foreground", "background", "system-background", "top-app"}
-	if ver, err := arc.SDKVersion(); err != nil {
-		s.Error("Failed to find SDKVersion: ", err)
-		return
-	} else if ver >= arc.SDKP {
-		// In ARC P or later, restricted is added.
-		types = append(types, "restricted")
-	}
+	types := []string{"foreground", "background", "system-background", "top-app", "restricted"}
+	numOtherCores := 0
 
 	for _, t := range types {
 		// cgroup pseudo file cannot be "adb pull"ed. Additionally, it is not
@@ -369,6 +402,24 @@ func testCPUSet(ctx context.Context, s *testing.State, a *arc.ARC) {
 				s.Errorf("Unexpected CPU setting %q for %s: should not be %d CPUs", val, path,
 					runtime.NumCPU())
 			}
+			numOtherCores += len(cpusInUse)
+		}
+	}
+
+	heterogeneousCores, err := isHeterogeneousCores(ctx)
+	if err != nil {
+		s.Error("Failed to determine core type: ", err)
+		return
+	}
+	if heterogeneousCores {
+		// The cpuset settings done in init.{cheets,bertha}.rc work fine
+		// for homogeneous systems, but heterogeneous systems require
+		// their own init.cpusets.rc to properly set up non-foreground
+		// cpusets. Verify that init.cpusets.rc exists on heterogeneous
+		// systems.
+		s.Log("Running testCPUSet (for boards with heterogeneous cores)")
+		if numOtherCores == 3 {
+			s.Error("Unexpected CPU setting; found heterogeneous cores but lacks proper init.cpusets.rc file")
 		}
 	}
 }
