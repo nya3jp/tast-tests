9c85983cf82110067a00f428413302a7a3e6ca90
Jorge Lucangeli Obes <jorgelo@chromium.org>
2020-03-22 10:47:04 -0400

    UserFilesGuest: Perform check in the correct namespace.
    Guest sessions are mounted in a non-root mount namespace, so the test
    needs to check in that same namespace, so that it can find the files.
    
    Make the test robust by only checking in the non-root namespace if the
    non-root namespace functionality is being used.
    
    BUG=chromium:1056294
    TEST=Passes on eve.
    
    Change-Id: I7b18ec08445b3fff051be9ed31db2cddee05ee0d

--- src/chromiumos/tast/local/bundles/cros/security/user_files_guest.go
+++ src/chromiumos/tast/local/bundles/cros/security/user_files_guest.go
@@ -7,6 +7,10 @@ package security
 import (
 	"context"
 
+	"github.com/shirou/gopsutil/process"
+	"golang.org/x/sys/unix"
+
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/security/userfiles"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/upstart"
@@ -27,6 +31,25 @@ func init() {
 	})
 }
 
+func getCryptohomeNamespaceMounterPid() (int, error) {
+	const exePath = "/usr/sbin/cryptohome-namespace-mounter"
+
+	all, err := process.Pids()
+	if err != nil {
+		return -1, err
+	}
+
+	for _, pid := range all {
+		if proc, err := process.NewProcess(pid); err != nil {
+			// Assume that the process exited.
+			continue
+		} else if exe, err := proc.Exe(); err == nil && exe == exePath {
+			return int(pid), nil
+		}
+	}
+	return -1, errors.New("mounter process not found")
+}
+
 func UserFilesGuest(ctx context.Context, s *testing.State) {
 	cr, err := chrome.New(ctx, chrome.GuestLogin())
 	if err != nil {
@@ -34,6 +57,41 @@ func UserFilesGuest(ctx context.Context, s *testing.State) {
 	}
 	// chrome.Chrome.Close() will not log the user out.
 	defer upstart.RestartJob(ctx, "ui")
+
+	nsPath := "/run/namespaces/mnt_chrome"
+	if _, err := getCryptohomeNamespaceMounterPid(); err != nil {
+		nsPath = "/proc/1/ns/mnt"
+	}
+
+	// Guest sessions are mounted in a non-root mount namespace
+	// so the test needs to perform checks in that same namespace.
+	chromeNsFd, err := unix.Open(nsPath, unix.O_CLOEXEC, unix.O_RDWR)
+	if err != nil {
+		s.Fatal("Opening Chrome mount namespace failed: ", err)
+	}
+	defer unix.Close(chromeNsFd)
+
+	// Open root mount namespace to be able to switch back to it.
+	rootNsFd, err := unix.Open("/proc/1/ns/mnt", unix.O_CLOEXEC, unix.O_RDONLY)
+	if err != nil {
+		s.Fatal("Opening root mount namespace failed: ", err)
+	}
+	defer unix.Close(rootNsFd)
+
+	// Ensure we can successfully call setns(2) by first calling unshare(2)
+	// which will make this thread's view of mounts distinct from the root's.
+	err = unix.Unshare(unix.CLONE_NEWNS)
+	if err != nil {
+		s.Fatal("Unsharing mount namespace failed: ", err)
+	}
+	// As soon as we've successfully called unshare(2) ensure we switch back
+	// to the original namespace.
+	defer unix.Setns(rootNsFd, unix.CLONE_NEWNS)
+
+	err = unix.Setns(chromeNsFd, unix.CLONE_NEWNS)
+	if err != nil {
+		s.Fatal("Entering Chrome mount namespace failed: ", err)
+	}
 
 	userfiles.Check(ctx, s, cr.User())
 }
