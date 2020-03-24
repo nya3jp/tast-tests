d9785c65a4e9bf281e59ca7d358bb0e1d789537c
Jose Torreguitar <jtguitar@google.com>
2020-03-23 22:23:39 -0700

    UHID library
    BUG=None
    
    Uhid allows for the creation of nodes at /dev/hidraw* and
    /dev/input/event* by writing to the /dev/uhid file together with
    other operations such as injecting events. The format for the structs
    used to communicate with /dev/uhid can be found at linux/uhid.h
    
    TEST=None yet
    
    Change-Id: I787fb03cc753f676ec8d2afa274b0b3b7287526f

---
+++ src/chromiumos/tast/local/uhid/deviceNodes.go
@@ -0,0 +1,170 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package uhid
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// deviceNodes returns the nodes corresponding to this device.
+// these nodes refer to the /dev/input/event and /dev/hidraw nodes.
+// these are obtained from /sys/bus/hid/devices which contains hid
+// device information.
+func deviceNodes(ctx context.Context, d *Device) error {
+	devicePath, err := devicePath(d.infoString())
+	if err != nil {
+		return err
+	}
+	if d.hidrawNodes, err = hidRawNodes(ctx, devicePath); err != nil {
+		return err
+	}
+	d.eventNodes, err = eventNodes(devicePath)
+	return err
+}
+
+const devicesDirectory = "/sys/bus/hid/devices/"
+
+// devicePath returns the path corresponding to this device that
+// exists in /sys/bus/hid/devices/.
+// An example of a possible device path is
+// /sys/bus/hid/devices/0003:046D:C31C.0018 where 0003 is the bus,
+// 046D is the vendor id and C31C is the product id. 0018 is a unique
+// number given in case multiple devices exist with the same bus and
+// ids. In the case of this library we choose to take the path of the
+// most recently created device. That is, the one with the highest
+// unique number.
+func devicePath(infoString string) (string, error) {
+	files, err := ioutil.ReadDir(devicesDirectory)
+	if err != nil {
+		return "", err
+	}
+	devicePath := ""
+	newestDeviceID := -1
+	for _, f := range files {
+		var currentID int
+		if currentID, err = deviceID(f.Name()); err != nil {
+			return "", err
+		}
+		if currentID > newestDeviceID && strings.HasPrefix(f.Name(), infoString) {
+			newestDeviceID = currentID
+			devicePath = f.Name()
+		}
+	}
+	if devicePath == "" {
+		return "", errors.Errorf("device %p hasn't been created", infoString)
+	}
+	return devicesDirectory + devicePath + "/", nil
+}
+
+const hidRawDir = "hidraw"
+
+// hidRawNodes returns the hidraw nodes that exist under
+// <path>/hidraw.  Because the hidraw directory takes some time to be
+// createad we poll for it.
+func hidRawNodes(ctx context.Context, path string) ([]string, error) {
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		directories, err := ioutil.ReadDir(path)
+		if err != nil {
+			return err
+		}
+		for _, d := range directories {
+			if d.Name() == hidRawDir {
+				return nil
+			}
+		}
+		return errors.New("hidraw directory was not created")
+	}, &testing.PollOptions{Timeout: 10 * time.Second}); err != nil {
+		return nil, err
+	}
+	path = path + hidRawDir + "/"
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+	return hidRawPaths(files), nil
+}
+
+// eventNodes returns the event nodes under <path>/input/input*.
+// A device can have multiple directories like this. For example,
+// a dualshock 3 controller will have <path>/input/input<i> and
+// <path>/input/input<i+1> which represent the controller and
+// its motion sensors.
+func eventNodes(path string) ([]string, error) {
+	eventNodes := make([]string, 0)
+	directories, err := ioutil.ReadDir(path + "input/")
+	if err != nil {
+		return nil, err
+	}
+	for _, d := range directories {
+		if strings.HasPrefix(d.Name(), "input") {
+			eventNode, err := eventNode(path + "input/" + d.Name() + "/")
+			if err != nil {
+				return nil, err
+			}
+			eventNodes = append(eventNodes, eventNode)
+		}
+	}
+	if len(eventNodes) == 0 {
+		return nil, errors.New("the created device has no event nodes")
+	}
+	return eventNodes, nil
+}
+
+// infoString returns a string of the form
+// <d.Data.Bus>:<d.Data.VendorID>:<d.Data.ProductID>. Information
+// regarding this device will be found under
+// /sys/bus/hid/devices/<infoString>.<ID> where ID is a unique ID for
+// each device the kernel recognizes.
+func (d *Device) infoString() string {
+	return fmt.Sprintf("%04X:%04X:%04X", d.Data.Bus, d.Data.VendorID, d.Data.ProductID)
+}
+
+// deviceID returns the unique ID belonging to the device represented by the
+// directory in path.
+func deviceID(path string) (int, error) {
+	id, err := strconv.ParseInt(filepath.Ext(path)[1:], 16, 0)
+	if err != nil {
+		return -1, errors.New("the given path is not a sysfs device path")
+	}
+	return int(id), nil
+}
+
+// hidRawPaths returns the file names of the files in files prepended
+// with "/dev/" which creates their absolute path. It filters out of
+// files the none hidraw files
+func hidRawPaths(files []os.FileInfo) []string {
+	paths := make([]string, 0)
+	for _, f := range files {
+		if strings.HasPrefix(f.Name(), "hidraw") {
+			paths = append(paths, "/dev/"+f.Name())
+		}
+	}
+	return paths
+}
+
+// eventNode gets the event* node that exists inside path and prepends
+// to it "/dev/input/" to create its absolute path.
+func eventNode(path string) (string, error) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return "", err
+	}
+	for _, f := range files {
+		if strings.HasPrefix(f.Name(), "event") {
+			return "dev/input/" + f.Name(), nil
+		}
+	}
+	return "", nil
+}
---
+++ src/chromiumos/tast/local/uhid/parse.go
@@ -0,0 +1,111 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package uhid
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+// parseInfo receives a string containing a bus, vendor id and product
+// id and assigns them to the given DeviceData. The string should be
+// in the format "<bus> <vendorID> <productID>" where each number is
+// separated by a white space.
+func parseInfo(data *DeviceData, line string) error {
+	var err error
+	var bus uint64
+	var vendorID uint64
+	var productID uint64
+	nextWhiteSpace := strings.Index(line, " ")
+	if bus, err = strconv.ParseUint(line[:nextWhiteSpace], 16, 16); err != nil {
+		return err
+	}
+	line = line[nextWhiteSpace+1:]
+	nextWhiteSpace = strings.Index(line, " ")
+	if vendorID, err = strconv.ParseUint(line[:nextWhiteSpace], 16, 32); err != nil {
+		return err
+	}
+	if productID, err = strconv.ParseUint(line[nextWhiteSpace+1:], 16, 32); err != nil {
+		return err
+	}
+	data.Bus = uint16(bus)
+	data.VendorID = uint32(vendorID)
+	data.ProductID = uint32(productID)
+	return nil
+}
+
+// parseDescriptor receives a line containing the descriptor and its
+// size and then assigns it to data.Descriptor. The string must be of
+// the form "<size> <d_0> <d_1> ... <d_size>" where d_i is a
+// descriptor element separated from the others by whitespace at
+// both sides.
+func parseDescriptor(data *DeviceData, line string) error {
+	nextWhiteSpace := strings.Index(line, " ")
+	size, err := strconv.ParseInt(line[:nextWhiteSpace], 10, 0)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < int(size); i++ {
+		line = line[nextWhiteSpace+1:]
+		nextWhiteSpace = strings.Index(line, " ")
+		var n uint64
+		if nextWhiteSpace == -1 {
+			if n, err = strconv.ParseUint(line, 16, 8); err != nil {
+				return err
+			}
+		} else {
+			if n, err = strconv.ParseUint(line[:nextWhiteSpace], 16, 8); err != nil {
+				return err
+			}
+		}
+		data.Descriptor[i] = byte(n)
+	}
+	return nil
+}
+
+// parseTime returns a duration based on the received line that
+// represents a time stamp. The line must be of the form
+// "<seconds>.<microseconds>" where both seconds and microseconds are
+// six digit long decimal numbers separated by a dot.
+func parseTime(line string) (time.Duration, error) {
+	var seconds uint64
+	var microSeconds uint64
+	var err error
+	if seconds, err = strconv.ParseUint(line[:6], 10, 32); err != nil {
+		return time.Until(time.Now()), err
+	}
+	if microSeconds, err = strconv.ParseUint(line[7:13], 10, 32); err != nil {
+		return time.Until(time.Now()), err
+	}
+	return time.Duration(seconds)*time.Second + time.Duration(microSeconds)*time.Microsecond, nil
+}
+
+// parseData returns the device event information given in line. The
+// returned []byte can then be passed to d.InjectEvent where d is the
+// Device created based on this recording. The line received must
+// be of the form "<size> <d_1> <d_2> ... <d_size>" where size is the
+// size of the returned array and d_i corresponds to element i in the
+// returned array, separated by whitespace from the others ath both
+// sides.
+func parseData(line string) ([]byte, error) {
+	nextWhiteSpace := strings.Index(line, " ")
+	var size uint64
+	var err error
+	if size, err = strconv.ParseUint(line[:nextWhiteSpace], 10, 0); err != nil {
+		return nil, err
+	}
+	data := make([]byte, size)
+	for i := 0; i < int(size); i++ {
+		line = line[nextWhiteSpace+1:]
+		var n uint64
+		if n, err = strconv.ParseUint(line[:nextWhiteSpace], 16, 8); err != nil {
+			return nil, err
+		}
+		data[i] = byte(n)
+		nextWhiteSpace = strings.Index(line, " ")
+	}
+	return data, nil
+}
