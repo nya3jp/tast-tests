c312faa02a5a71fd3681bda2945e479b947950c1
Kirtika Ruchandani <kirtika@chromium.org>
2020-03-24 20:42:17 -0700

    tast-tests: network.WlanDriver:  Add HrP2 for 5.4 kernel
    This adds support for known HrP2 SKUs on JSL running kernel v5.4.
    
    BUG=b:151361744
    TEST=`tast run ${DUT_IP} network.WlanDriver` passes on Waddledoo board.
    
    Change-Id: Icef1fb79a69f87a5dd9d8b7c873cbc6d34c76655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2119752
    Reviewed-by: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Kirtika Ruchandani <kirtika@chromium.org>
    Commit-Queue: Kirtika Ruchandani <kirtika@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/network/wlan_driver.go
+++ src/chromiumos/tast/local/bundles/cros/network/wlan_driver.go
@@ -115,6 +115,7 @@ var wlanDeviceLookup = map[wlanDeviceInfo]string{
 	{vendor: "0x8086", device: "0x02f0", subsystem: "0x0034"}: intel9000,
 	{vendor: "0x8086", device: "0x02f0", subsystem: "0x4070"}: intel22560,
 	{vendor: "0x8086", device: "0x02f0", subsystem: "0x0074"}: intel22560,
+	{vendor: "0x8086", device: "0x4df0", subsystem: "0x0074"}: intel22560,
 	{vendor: "0x02d0", device: "0x4354"}:                      broadcomBCM4354SDIO,
 	{vendor: "0x14e4", device: "0x43ec"}:                      broadcomBCM4356PCIE,
 	{vendor: "0x14e4", device: "0x440d"}:                      broadcomBCM4371PCIE,
@@ -166,6 +167,7 @@ var expectedWLANDriver = map[string]map[string]string{
 	},
 	intel22560: {
 		"4.19": "wireless/iwl7000/iwlwifi/iwlwifi.ko",
+		"5.4":  "wireless/iwl7000/iwlwifi/iwlwifi.ko",
 	},
 	atherosAR9462: {
 		"3.4":  "wireless/ath/ath9k_btcoex/ath9k_btcoex.ko",
