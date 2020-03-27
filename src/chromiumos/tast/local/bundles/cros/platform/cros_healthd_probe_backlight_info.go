c93968a49d8faeebea365a31ef52f68343d0fa78
Jesse Schettler <jschettler@chromium.org>
2020-03-27 16:55:13 -0600

    tast: Promote CrosHealthdProbeBacklightInfo to CQ
    This test has passed in 20+ builds.
    
    BUG=chromium:1048377
    TEST=tast run --build $DUT_IP platform.CrosHealthdProbeBacklightInfo
    
    Change-Id: I6e11bb19e4aacadc8bad81ed0eee9616fb5ff4da

--- src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_backlight_info.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_backlight_info.go
@@ -26,7 +26,7 @@ func init() {
 			"jschettler@google.com",
 			"pmoy@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"cros_config", "diagnostics"},
 	})
 }
