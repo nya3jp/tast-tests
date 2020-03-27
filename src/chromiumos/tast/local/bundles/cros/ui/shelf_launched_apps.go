66af9061ff3642ebeff6701e2cba14afb2a46cde
Brendan Hansknecht <bhansknecht@chromium.org>
2020-03-27 11:18:21 -0700

    tast-tests: promote ui.FilesApp* and ui.ShelfLaunchedApps
    Promoting ui.FilesAppSmoke, ui.FilesAppQuickView, and
    ui.ShelfLaunchedApps to CQ. These tests only fail when tast/chrome have
    an issue. They should be good to promote to CQ.
    
    BUG=chromium:991248
    TEST=tast run <eve> ui.FilesApp* ui.ShelfLaunchedApps
    
    Change-Id: I48c92a49bbc089abf40a0dfd3a9e0d798560bc21

--- src/chromiumos/tast/local/bundles/cros/ui/shelf_launched_apps.go
+++ src/chromiumos/tast/local/bundles/cros/ui/shelf_launched_apps.go
@@ -22,7 +22,7 @@ func init() {
 		Contacts: []string{
 			"dhaddock@chromium.org",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Pre:          chrome.LoggedIn(),
 	})
