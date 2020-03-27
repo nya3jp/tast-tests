624da4d05d8d02cfdb759369219ab3642b5fe7b1
Miriam Zimmerman <mutexlox@google.com>
2020-03-27 13:46:05 -0700

    Add logging to CrashReporterCrash test
    BUG=chromium:1040266
    TEST=tast run $DUT platform.CrashReporterCrash.mock_consent
    
    Change-Id: I31dd3588ed09e748f9dd2dff63aeabe80c2107fe

--- src/chromiumos/tast/local/bundles/cros/platform/crash_reporter_crash.go
+++ src/chromiumos/tast/local/bundles/cros/platform/crash_reporter_crash.go
@@ -44,13 +44,14 @@ func init() {
 	})
 }
 
-func setCorePatternCrashTest(crashTest bool) error {
+func setCorePatternCrashTest(ctx context.Context, crashTest bool) error {
 	b, err := ioutil.ReadFile(commoncrash.CorePattern)
 	if err != nil {
 		return errors.Wrapf(err, "failed reading core pattern file %s",
 			commoncrash.CorePattern)
 	}
 
+	testing.ContextLogf(ctx, "Previous core pattern: %s", string(b))
 	// Reset any crash test flag
 	corePatternExpr := strings.TrimSpace(string(b))
 	corePatternExpr = strings.Replace(corePatternExpr, " --crash_test", "", -1)
@@ -59,6 +60,7 @@ func setCorePatternCrashTest(crashTest bool) error {
 		corePatternExpr = corePatternExpr + " --crash_test"
 	}
 
+	testing.ContextLogf(ctx, "Setting core pattern to: %s", corePatternExpr)
 	if err := ioutil.WriteFile(commoncrash.CorePattern,
 		[]byte(corePatternExpr), 0644); err != nil {
 		return errors.Wrapf(err, "failed writing core pattern file %s",
@@ -83,10 +85,10 @@ func CrashReporterCrash(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to get original crashes: ", err)
 	}
 
-	if err := setCorePatternCrashTest(true); err != nil {
+	if err := setCorePatternCrashTest(ctx, true); err != nil {
 		s.Fatal(err, "failed to replace core pattern")
 	}
-	defer setCorePatternCrashTest(false)
+	defer setCorePatternCrashTest(ctx, false)
 
 	// TODO(crbug.com/1011932): Investigate if this is necessary
 	st, err := os.Stat(commoncrash.CrashReporterEnabledPath)
