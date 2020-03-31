8595bb6ca9383d5a7a950401d37ebf651f2613c7
Meng-Huan Yu <menghuan@chromium.org>
2020-03-31 16:45:37 +0800

    tast-test: Make factory.Goofy test only run on VMs
    Since the factory_common change (b/112251287) makes upstream factory
    toolkit not compatible with factory branched private overlay, we would
    like to disable the tests run on real devices (which is also meaningless
    for us).
    
    BUG=b:150189948
    TEST=pass presubmit hook
    
    Change-Id: I26aeb173f83cb338f2de03d221c3ac3060f8a31b

--- src/chromiumos/tast/local/bundles/cros/factory/goofy.go
+++ src/chromiumos/tast/local/bundles/cros/factory/goofy.go
@@ -25,8 +25,21 @@ func init() {
 		Func:     Goofy,
 		Desc:     "Setup factory toolkit and exercise Goofy with custom TestList",
 		Contacts: []string{"menghuan@chromium.org", "chromeos-factory-eng@google.com"},
-		Attr:     []string{"group:mainline", "informational"},
-		Data:     []string{"factory_image.zip"},
+		Attr:     []string{"group:mainline"},
+		Params: []testing.Param{{
+			// Since the upstream factory toolkit could not backward compatible with
+			// factory-branched private overlay (b/112251287), we'd like to run test on
+			// upstream factory toolkit without device specific configuration.
+			// "qemu" implies VM tests (aka. "betty" boards)
+			Name:              "",
+			ExtraSoftwareDeps: []string{"qemu"},
+			ExtraAttr:         []string{"informational"},
+		}, {
+			Name:              "manual",
+			ExtraSoftwareDeps: []string{"no_qemu"},
+			ExtraAttr:         []string{"disabled"},
+		}},
+		Data: []string{"factory_image.zip"},
 	})
 }
 
