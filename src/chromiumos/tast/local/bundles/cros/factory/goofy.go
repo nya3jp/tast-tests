6503b5613c101eb686609d6b119dab75b5fcdaaa
Meng-Huan Yu <menghuan@chromium.org>
2020-03-31 16:45:37 +0800

    tast-test: Split stable/nonstable tests in Goofy to different parameters
    BUG=b:150189948
    TEST=pass presubmit hook
    
    Change-Id: I26aeb173f83cb338f2de03d221c3ac3060f8a31b

--- src/chromiumos/tast/local/bundles/cros/factory/goofy.go
+++ src/chromiumos/tast/local/bundles/cros/factory/goofy.go
@@ -25,8 +25,19 @@ func init() {
 		Func:     Goofy,
 		Desc:     "Setup factory toolkit and exercise Goofy with custom TestList",
 		Contacts: []string{"menghuan@chromium.org", "chromeos-factory-eng@google.com"},
-		Attr:     []string{"group:mainline", "informational"},
-		Data:     []string{"factory_image.zip"},
+		Attr:     []string{"group:mainline"},
+		Params: []testing.Param{{
+			Name:              "",
+			ExtraSoftwareDeps: []string{"qemu"},
+			// ExtraHardwareDeps: hwdep.D(hwdep.Model("betty", "betty-pi-arc"))
+			ExtraAttr: []string{"informational"},
+		}, {
+			// TODO(b/150189948): Fix the non-stable board due to buildbot deps issue.
+			Name:              "not_stable",
+			ExtraSoftwareDeps: []string{"no_qemu"},
+			ExtraAttr:         []string{"informational"},
+		}},
+		Data: []string{"factory_image.zip"},
 	})
 }
 
