559f75727fd19507582a17a1f8dd11a066976601
Shuhei Takahashi <nya@chromium.org>
2020-03-31 14:33:54 +0900

    tast-tests: Remove the explicit "disabled" attributes.
    Instead of the "disabled" attributes, we should remove group:*
    attributes to disable a test in a group.
    
    See go/tast-mainline-attr-transition for the overall plan of
    the transition. This change is for phase 3->4.
    
    BUG=chromium:1005041
    TEST=fast_build.sh -T
    
    Cq-Depend: chromium:2097622
    Change-Id: I4208a144482471c79b086949c37e3cc63389862a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2128210
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_vda.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_vda.go
@@ -17,43 +17,47 @@ func init() {
 		Func:         DecodeAccelVDVDA,
 		Desc:         "Verifies hardware decode acceleration using a media::VideoDecoder through the VDA interface, by running the video_decode_accelerator_tests binary (see go/vd-migration)",
 		Contacts:     []string{"akahuang@chromium.org", "dstaessens@chromium.org", "chromeos-video-eng@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "cros_video_decoder"},
 		Params: []testing.Param{{
 			Name:              "h264",
 			Val:               "test-25fps.h264",
+			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
 			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
 		}, {
 			Name:              "vp8",
 			Val:               "test-25fps.vp8",
+			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
 		}, {
 			Name:              "vp9",
 			Val:               "test-25fps.vp9",
+			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
 		}, {
 			Name: "vp9_2",
 			Val:  "test-25fps.vp9_2",
 			// TODO(crbug.com/911754): reenable this test once HDR VP9.2 is implemented.
-			ExtraAttr:         []string{"disabled"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_2},
 			ExtraData:         []string{"test-25fps.vp9_2", "test-25fps.vp9_2.json"},
 		}, {
 			Name:              "h264_resolution_switch",
 			Val:               "switch_1080p_720p_240frames.h264",
+			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
 			ExtraData:         []string{"switch_1080p_720p_240frames.h264", "switch_1080p_720p_240frames.h264.json"},
 		}, {
 			Name:              "vp8_resolution_switch",
 			Val:               "resolution_change_500frames.vp8.ivf",
+			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			ExtraData:         []string{"resolution_change_500frames.vp8.ivf", "resolution_change_500frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp9_resolution_switch",
 			Val:               "resolution_change_500frames.vp9.ivf",
+			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"resolution_change_500frames.vp9.ivf", "resolution_change_500frames.vp9.ivf.json"},
 		}, {
@@ -61,12 +65,14 @@ func init() {
 			// https://android.googlesource.com/platform/cts/+/master/tests/tests/media/res/raw/vp90_2_17_show_existing_frame.vp9
 			Name:              "vp9_show_existing_frame",
 			Val:               "vda_sanity-vp90_2_17_show_existing_frame.vp9",
+			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"vda_sanity-vp90_2_17_show_existing_frame.vp9", "vda_sanity-vp90_2_17_show_existing_frame.vp9.json"},
 		}, {
 			// H264 stream in which a profile changes from Baseline to Main.
 			Name:              "h264_profile_change",
 			Val:               "test-25fps_basemain.h264",
+			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
 			ExtraData:         []string{"test-25fps_basemain.h264", "test-25fps_basemain.h264.json"},
 		}, {
@@ -75,6 +81,7 @@ func init() {
 			// The structure is used in Hangouts Meet. go/vp9-svc-hangouts for detail.
 			Name:              "vp9_keyframe_spatial_layers",
 			Val:               "keyframe_spatial_layers_180p_360p.vp9.ivf",
+			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "vaapi"},
 			ExtraData:         []string{"keyframe_spatial_layers_180p_360p.vp9.ivf", "keyframe_spatial_layers_180p_360p.vp9.ivf.json"},
 		}},
