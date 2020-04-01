ba0c1da380c319c966c38bfa2c58a4357b598c13
Po-Hsien Wang <pwang@chromium.org>
2020-04-01 16:56:40 -0700

    graphics: Add crostini_stable to glxgears test
    Add crostini_stable/crostini_unstable SoftwareDeps to the glxgears.
    Also add group:mainline and graphics_av_analysis attribute.
    
    BUG=b:49255805, chromium:1067067
    TEST=tast run DUT graphics.TraceReplay.glxgears_stable
    
    Cq-Depend: chromium:2127316, chromium:2113833
    Change-Id: I3f0df89a4622ebd4b581fea93752e3a5de4847f2

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/trace_replay.go
@@ -0,0 +1,61 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/local/graphics/trace"
+	"chromiumos/tast/local/graphics/trace/comm"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TraceReplay,
+		Desc:         "Replay glxgears trace file in Crostini VM",
+		Contacts:     []string{"chromeos-gfx@google.com", "tutankhamen@google.com", "ddmail@google.com", "ihf@google.com"},
+		Pre:          crostini.StartedGPUEnabledBuster(),
+		SoftwareDeps: []string{"chrome", "crosvm_gpu", "vm_host"},
+		Attr:         []string{"group:mainline", "group:graphics", "graphics_trace", "graphics_perbuild", "graphics_av_analysis"},
+		Timeout:      15 * time.Minute,
+		Params: []testing.Param{
+			{
+				Name: "glxgears_stable",
+				Val: comm.TestGroupConfig{
+					Labels: []string{"short"},
+					Repository: comm.RepositoryInfo{
+						RootURL: "gs://chromiumos-test-assets-public/tast/cros/graphics/traces/repo",
+						Version: 1,
+					},
+				},
+				ExtraAttr:         []string{"informational"},
+				ExtraSoftwareDeps: []string{"crostini_stable"},
+			},
+			{
+				Name: "glxgears_unstable",
+				Val: comm.TestGroupConfig{
+					Labels: []string{"short"},
+					Repository: comm.RepositoryInfo{
+						RootURL: "gs://chromiumos-test-assets-public/tast/cros/graphics/traces/repo",
+						Version: 1,
+					},
+				},
+				ExtraAttr:         []string{"informational"},
+				ExtraSoftwareDeps: []string{"crostini_unstable"},
+			},
+		},
+	})
+}
+
+func TraceReplay(ctx context.Context, s *testing.State) {
+	pre := s.PreValue().(crostini.PreData)
+	config := s.Param().(comm.TestGroupConfig)
+	if err := trace.RunTraceReplayTest(ctx, s.OutDir(), s.CloudStorage(), pre.Container, &config); err != nil {
+		s.Fatal("Trace replay test failed: ", err)
+	}
+}
