2c75e3510d6d2e392f90bef47c2cbfe1a99c5a24
Miguel Casas <mcasas@chromium.org>
2020-04-03 14:40:16 -0400

    tast-tests/webrtc: add RTCPeerConnection VP8 simulcast test case
    This CL adds a new webrtc.RTCPeerConnection test case to verify the
    VP8 simulcast functionality, which is used e.g. on Meet/Hangouts.
    For that:
    - a new test param is added, which is wired through to the html file,
    - the JS code is modified in a few points to use addTransceiver()
     and to fix the offer/answer SDPs as per simulcast needs and
    - we pull two extra files from the Web Platform Tests (WPT)
    upstream repo.
    
    In this test case we don't care if it's implemented in hw or in sw
    (see the next CL for that).
    
    BUG=chromium:1067715
    TEST=tast run $IP webrtc.RTCPeerConnection* on atlas
    
    Change-Id: I09aa0cd59f1f5ab6da4b9b8b3bfbfa36029a2a0c

--- src/chromiumos/tast/local/bundles/cros/webrtc/rtc_peer_connection.go
+++ src/chromiumos/tast/local/bundles/cros/webrtc/rtc_peer_connection.go
@@ -11,7 +11,6 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/media/caps"
 	"chromiumos/tast/local/media/pre"
-	"chromiumos/tast/local/webrtc"
 	"chromiumos/tast/testing"
 )
 
@@ -19,6 +18,11 @@ import (
 type rtcTest struct {
 	codec   peerconnection.CodecType // Encoding, decoding, or don't care.
 	profile string                   // Codec to try, e.g. VP8, VP9.
+	// Simulcast is a technique to send multiple differently encoded versions
+	// of the same media source in different RTP streams; this is used for
+	// example for video conference services.
+	// See https://www.w3.org/TR/webrtc/#simulcast-functionality
+	simulcast bool
 }
 
 func init() {
@@ -31,54 +35,58 @@ func init() {
 			"chromeos-video-eng@google.com",
 		},
 		SoftwareDeps: []string{"chrome"},
-		Data:         append(webrtc.DataFiles(), peerconnection.LoopbackFile),
+		Data:         append(peerconnection.DataFiles(), peerconnection.LoopbackFile),
 		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
 		// TODO(crbug.com/1017374): add "vp9_enc".
 		Params: []testing.Param{{
 			Name:              "vp8_enc",
-			Val:               rtcTest{codec: peerconnection.Encoding, profile: "VP8"},
+			Val:               rtcTest{codec: peerconnection.Encoding, profile: "VP8", simulcast: false},
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
 			Pre:               pre.ChromeVideoWithFakeWebcam(),
 		}, {
 			Name:              "vp8_dec",
-			Val:               rtcTest{codec: peerconnection.Decoding, profile: "VP8"},
+			Val:               rtcTest{codec: peerconnection.Decoding, profile: "VP8", simulcast: false},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			Pre:               pre.ChromeVideoWithFakeWebcam(),
 		}, {
 			Name:              "vp9_dec",
-			Val:               rtcTest{codec: peerconnection.Decoding, profile: "VP9"},
+			Val:               rtcTest{codec: peerconnection.Decoding, profile: "VP9", simulcast: false},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			Pre:               pre.ChromeVideoWithFakeWebcam(),
 		}, {
 			Name: "vp9_enc",
-			Val:  rtcTest{codec: peerconnection.Encoding, profile: "VP9"},
+			Val:  rtcTest{codec: peerconnection.Encoding, profile: "VP9", simulcast: false},
 			// TODO(crbug.com/811912): Remove "vaapi" and pre.ChromeVideoWithFakeWebcamAndVP9VaapiEncoder()
 			// once the feature is enabled by default on VA-API devices.
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP9, "vaapi"},
 			Pre:               pre.ChromeVideoWithFakeWebcamAndVP9VaapiEncoder(),
 		}, {
 			Name:              "h264_enc",
-			Val:               rtcTest{codec: peerconnection.Encoding, profile: "H264"},
+			Val:               rtcTest{codec: peerconnection.Encoding, profile: "H264", simulcast: false},
 			ExtraSoftwareDeps: []string{caps.HWEncodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
 			Pre:               pre.ChromeVideoWithFakeWebcamAndH264AMDEncoder(),
 		}, {
 			Name:              "h264_dec",
-			Val:               rtcTest{codec: peerconnection.Decoding, profile: "H264"},
+			Val:               rtcTest{codec: peerconnection.Decoding, profile: "H264", simulcast: false},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
 			Pre:               pre.ChromeVideoWithFakeWebcam(),
 		}, {
 			Name: "vp8",
-			Val:  rtcTest{codec: peerconnection.DontCare, profile: "VP8"},
+			Val:  rtcTest{codec: peerconnection.DontCare, profile: "VP8", simulcast: false},
 			Pre:  pre.ChromeVideoWithFakeWebcam(),
 		}, {
 			Name: "vp9",
-			Val:  rtcTest{codec: peerconnection.DontCare, profile: "VP9"},
+			Val:  rtcTest{codec: peerconnection.DontCare, profile: "VP9", simulcast: false},
 			Pre:  pre.ChromeVideoWithFakeWebcam(),
 		}, {
 			Name:              "h264",
-			Val:               rtcTest{codec: peerconnection.DontCare, profile: "H264"},
+			Val:               rtcTest{codec: peerconnection.DontCare, profile: "H264", simulcast: false},
 			ExtraSoftwareDeps: []string{"chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
 			Pre:               pre.ChromeVideoWithFakeWebcam(),
+		}, {
+			Name: "vp8_simulcast",
+			Val:  rtcTest{codec: peerconnection.DontCare, profile: "VP8", simulcast: true},
+			Pre:  pre.ChromeVideoWithFakeWebcam(),
 		}},
 	})
 }
@@ -87,5 +95,5 @@ func init() {
 // specified, verifies it uses accelerated encoding / decoding.
 func RTCPeerConnection(ctx context.Context, s *testing.State) {
 	testOpt := s.Param().(rtcTest)
-	peerconnection.RunRTCPeerConnection(ctx, s, s.PreValue().(*chrome.Chrome), testOpt.codec, testOpt.profile)
+	peerconnection.RunRTCPeerConnection(ctx, s, s.PreValue().(*chrome.Chrome), testOpt.codec, testOpt.profile, testOpt.simulcast)
 }
