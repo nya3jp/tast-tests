cd38582ba561a2a7598d59b8c4d7a47e9c14f676
Hsin-chen Chuang <chharry@google.com>
2020-04-05 22:14:19 +0800

    wifi: security: Add WPA support.
    Implemented the security interfaces Config and Factory as well as
    NewFactory function of WPA.
    
    BUG=chromium:1034875
    TEST=~/trunk/src/platform/tast/fast_build.sh -t \
    chromiumos/tast/common/wifi/security/wpa
    
    Change-Id: Ida83b5f050283cc5c225e6a4ffa769d8e6a8dff9

---
+++ src/chromiumos/tast/common/wifi/security/wpa/options.go
@@ -0,0 +1,64 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wpa
+
+// Option is the function signature used to specify options of Config.
+type Option func(*Config)
+
+// Mode returns an Option which sets WPA mode in Config.
+func Mode(mode ModeEnum) Option {
+	return func(c *Config) {
+		c.mode = mode
+	}
+}
+
+// Ciphers returns an Option which sets the used WPA ciphers in Config.
+func Ciphers(ciphers ...Cipher) Option {
+	return func(c *Config) {
+		c.ciphers = append(c.ciphers, ciphers...)
+	}
+}
+
+// Ciphers2 returns an Option which sets the used WPA2 ciphers in Config.
+func Ciphers2(ciphers ...Cipher) Option {
+	return func(c *Config) {
+		c.ciphers2 = append(c.ciphers2, ciphers...)
+	}
+}
+
+// PTKRekeyPeriod returns an Option which sets maximum lifetime in seconds for PTK in Config.
+func PTKRekeyPeriod(period int) Option {
+	return func(c *Config) {
+		c.ptkRekeyPeriod = period
+	}
+}
+
+// GTKRekeyPeriod returns an Option which sets time interval in seconds for rekeying GTK in Config.
+func GTKRekeyPeriod(period int) Option {
+	return func(c *Config) {
+		c.gtkRekeyPeriod = period
+	}
+}
+
+// GMKRekeyPeriod returns an Option which sets time interval in seconds for rekeying GMK in Config.
+func GMKRekeyPeriod(period int) Option {
+	return func(c *Config) {
+		c.gmkRekeyPeriod = period
+	}
+}
+
+// UseStrictRekey returns an Option which sets strict rekey in Config.
+func UseStrictRekey(use bool) Option {
+	return func(c *Config) {
+		c.useStrictRekey = use
+	}
+}
+
+// FTMode returns an Option which sets fast transition mode in Config.
+func FTMode(ft FTModeEnum) Option {
+	return func(c *Config) {
+		c.ftMode = ft
+	}
+}
