e414e85451e7d14963b42ba73a1b6ecc431ae6b5
Miguel Casas <mcasas@chromium.org>
2020-04-06 15:08:32 -0400

    tast-tests/video: move common video/encode structs to media/
    This CL moves the logic for encoding tests from the video/encode
    to a shared location in media/encoding, and adapts the necessary
    callsites in video/ and arc/video*.
    
    More precisely it extracts StreamParams and TestOption structs
    to said location, sadly causing a flurry of small package name
    fixes. The test_cases.go that apply to arc are splintered to a
    new file as per reviewers' suggestion.
    
    No functional changes intended.
    
    BUG=chromium:1066463
    TEST=tast -verbose run IP video.Play.vp9* on atlas
    
    Change-Id: Iddbec8453f8a553f47c0556d9914078a77181243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2137952
    Tested-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Ricardo Quesada <ricardoq@chromium.org>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arc/video/test_cases.go
@@ -0,0 +1,24 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"chromiumos/tast/local/coords"
+	"chromiumos/tast/local/media/encoding"
+)
+
+// Bear192P is the test parameters of video_encode_accelerator_unittest for "bear_320x192_40frames.yuv".
+var Bear192P = encoding.StreamParams{
+	Name:    "bear-320x192.vp9.webm",
+	Size:    coords.NewSize(320, 192),
+	Bitrate: 200000,
+}
+
+// Crowd1080P is the test parameters of video_encode_accelerator_unittest for the raw data obtained by decoding "crowd1920x1080.webm".
+var Crowd1080P = encoding.StreamParams{
+	Name:    "crowd-1920x1080.vp9.webm",
+	Size:    coords.NewSize(1920, 1080),
+	Bitrate: 4000000,
+}
