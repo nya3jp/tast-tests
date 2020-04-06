3f41ddddf4e9d0addb2329944c50654829cd2752
Miguel Casas <mcasas@chromium.org>
2020-04-06 15:22:04 -0400

    tast-tests: move arc-specific video encode logic to arc/video
    This CL moves the arc-specific logic from the video/encode
    folder to arc/video, adapting the callsites.
    
    No functional changes intended.
    
    BUG=chromium:1066463
    TEST=tast -verbose run IP arc.VideoEncodeAccel* video.EncodeAccel*
      on atlas
    
    Change-Id: I5d0307fe2a508bd5d116ddc9dbd3279ab94ca74a

--- src/chromiumos/tast/local/bundles/cros/arc/video_encode_accel_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/video_encode_accel_perf.go
@@ -11,7 +11,6 @@ import (
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/arc/c2e2etest"
 	"chromiumos/tast/local/bundles/cros/arc/video"
-	"chromiumos/tast/local/bundles/cros/video/encode"
 	"chromiumos/tast/local/media/caps"
 	"chromiumos/tast/local/media/encoding"
 	"chromiumos/tast/local/media/videotype"
@@ -42,5 +41,5 @@ func init() {
 }
 
 func VideoEncodeAccelPerf(ctx context.Context, s *testing.State) {
-	encode.RunARCPerfVideoTest(ctx, s, s.PreValue().(arc.PreData).ARC, s.Param().(encoding.TestOptions))
+	video.RunARCPerfVideoTest(ctx, s, s.PreValue().(arc.PreData).ARC, s.Param().(encoding.TestOptions))
 }
