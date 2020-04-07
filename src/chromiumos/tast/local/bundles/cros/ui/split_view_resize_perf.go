1046f6e2521170f82d91c5491151eb05fdca5e68
Avery Musbach <amusbach@chromium.org>
2020-04-07 13:35:16 -0700

    tast-tests: Add ui.SplitViewResizePerf.clamshell_mode
    BUG=chromium:1059477
    TEST=ui.SplitViewResizePerf.clamshell_mode
    
    Change-Id: I9d210f70aca0c4ca94b395d9b1b4d5c8c104d2c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2140820
    Tested-by: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/split_view_resize_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/split_view_resize_perf.go
@@ -14,6 +14,7 @@ import (
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/display"
 	"chromiumos/tast/local/chrome/metrics"
+	chromeui "chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/media/cpu"
 	"chromiumos/tast/local/perf"
@@ -24,17 +25,39 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         SplitViewResizePerf,
-		Desc:         "Measures smoothness of resizing windows in the split view of the tablet mode",
-		Contacts:     []string{"mukai@chromium.org", "sammiequon@chromium.org", "chromeos-wmp@google.com"},
+		Desc:         "Measures smoothness of resizing split view windows",
+		Contacts:     []string{"mukai@chromium.org", "sammiequon@chromium.org", "amusbach@chromium.org", "chromeos-wmp@google.com"},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
-		SoftwareDeps: []string{"chrome", "tablet_mode"},
-		Pre:          chrome.LoggedIn(),
+		SoftwareDeps: []string{"chrome"},
 		Timeout:      5 * time.Minute,
+		Params: []testing.Param{
+			{
+				Name: "clamshell_mode",
+				Val:  false,
+			},
+			{
+				ExtraSoftwareDeps: []string{"tablet_mode"},
+				Val:               true,
+			},
+		},
 	})
 }
 
 func SplitViewResizePerf(ctx context.Context, s *testing.State) {
-	cr := s.PreValue().(*chrome.Chrome)
+	// Enables DragToSnapInClamshellMode when testing clamshell split view.
+	// TODO: When the feature is fully launched, just use chrome.Pre().
+	tabletMode := s.Param().(bool)
+	var cr *chrome.Chrome
+	var err error
+	if tabletMode {
+		cr, err = chrome.New(ctx)
+	} else {
+		cr, err = chrome.New(ctx, chrome.ExtraArgs("--enable-features=DragToSnapInClamshellMode"))
+	}
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
 
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
@@ -50,9 +73,13 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 		return
 	}
 
-	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, true)
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, tabletMode)
 	if err != nil {
-		s.Fatal("Failed to ensure in tablet mode: ", err)
+		if tabletMode {
+			s.Fatal("Failed to ensure in tablet mode: ", err)
+		} else {
+			s.Fatal("Failed to ensure in clamshell mode: ", err)
+		}
 	}
 	defer cleanup(ctx)
 
@@ -62,8 +89,10 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 	}
 	defer tew.Close()
 
-	// Ensures in the landscape orientation; the following test scenario won't
-	// succeed when the device is in the portrait mode.
+	// Ensures landscape orientation so this test can assume that windows snap on
+	// the left and right. Windows snap on the top and bottom in portrait-oriented
+	// tablet mode. They snap on the left and right in portrait-oriented clamshell
+	// mode, but there are active (although contentious) proposals to change that.
 	orientation, err := display.GetOrientation(ctx, tconn)
 	if err != nil {
 		s.Fatal("Failed to obtain the orientation info: ", err)
@@ -96,7 +125,7 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 	// Computing the coordinates for the gesture of resizing, see also the
 	// comments around metrics.Run.
 	// y: shares the same value (at the middle of the screen) to move horizontaly.
-	// splitX: in the middle of the screen (should have the splitter).
+	// splitX: in the middle of the screen.
 	// midX: in the center of the left-snapped window.
 	// endX: the right edge of the screen. Note that tew.Width() is outside of the
 	//   visible area, thus subtracted by 1.
@@ -105,46 +134,102 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 	midX := tew.Width() / 4
 	endX := tew.Width() - 1
 
-	currentWindows := 0
-	pv := perf.NewValues()
-
 	// Testing 3 patterns;
 	// SingleWindow: there's a single window which is snapped to the left.
 	// WithOverview: one window is snapped to the left, and the right area is
 	//   in the overview mode.
-	// MultiWindow: one window is snapped to the left and another window is
-	//   snapped to the right.
-	for _, testCase := range []struct {
+	// MultiWindow (tablet only): one window is snapped to the left and another
+	//   window is snapped to the right.
+	type testCaseSlice []struct {
 		name       string
 		numWindows int
 		customPrep func() error
-	}{
-		{"SingleWindow", 1, func() error { return nil }},
-		{"WithOverview", 8, func() error { return nil }},
-		{"MultiWindow", 8, func() error {
-			// Additional preparation for the multi-window; by default the right side
-			// should be in the overview mode, so here selects one of the windows.
-			// First, find the window which is in the overview and obtains the center
-			// point of its bounds.
-			w, err := ash.FindFirstWindowInOverview(ctx, tconn)
-			if err != nil {
-				return err
-			}
-			id1 := w.ID
-			centerX, centerY := tcc.ConvertLocation(w.OverviewInfo.Bounds.CenterPoint())
-			// Tap the center of the overview window, and wait it to be snapped to
-			// the right.
-			if err := stw.Move(centerX, centerY); err != nil {
-				return errors.Wrapf(err, "failed to tap the center of %d", id1)
-			}
-			if err := stw.End(); err != nil {
-				return errors.Wrapf(err, "failed to release the tap for %d", id1)
-			}
-			return ash.WaitForCondition(ctx, tconn, func(w *ash.Window) bool {
-				return w.ID == id1 && !w.IsAnimating && w.State == ash.WindowStateRightSnapped
-			}, &testing.PollOptions{Timeout: 5 * time.Second})
-		}},
-	} {
+	}
+	var testCases testCaseSlice
+	var modeName string
+	if tabletMode {
+		testCases = testCaseSlice{
+			{"SingleWindow", 1, func() error { return nil }},
+			{"WithOverview", 8, func() error { return nil }},
+			{"MultiWindow", 8, func() error {
+				// Additional preparation for the multi-window; by default the right side
+				// should be in the overview mode, so here selects one of the windows.
+				// First, find the window which is in the overview and obtains the center
+				// point of its bounds.
+				w, err := ash.FindFirstWindowInOverview(ctx, tconn)
+				if err != nil {
+					return err
+				}
+				id1 := w.ID
+				centerX, centerY := tcc.ConvertLocation(w.OverviewInfo.Bounds.CenterPoint())
+				// Tap the center of the overview window, and wait it to be snapped to
+				// the right.
+				if err := stw.Move(centerX, centerY); err != nil {
+					return errors.Wrapf(err, "failed to tap the center of %d", id1)
+				}
+				if err := stw.End(); err != nil {
+					return errors.Wrapf(err, "failed to release the tap for %d", id1)
+				}
+				return ash.WaitForCondition(ctx, tconn, func(w *ash.Window) bool {
+					return w.ID == id1 && !w.IsAnimating && w.State == ash.WindowStateRightSnapped
+				}, &testing.PollOptions{Timeout: 5 * time.Second})
+			}},
+		}
+		modeName = "TabletMode"
+	} else {
+		testCases = testCaseSlice{
+			{"SingleWindow", 2, func() error {
+				// Additional preparation for the empty overview grid. When we drag and
+				// snap an overview item to achieve clamshell split view in the first
+				// place, we need a second overview item so that overview stays nonempty;
+				// otherwise overview will end. After entering clamshell split view, we
+				// need to get rid of that extra overview item, but if we close it then
+				// overview will end. So we have to create a second virtual desk and drag
+				// the overview item thereto. This workflow may be too rare to warrant
+				// performance testing, but the results also apply to the potentially
+				// more frequent workflow where one display is in clamshell split view
+				// with an empty overview grid while another display has overview items.
+				if err := ash.CreateNewDesk(ctx, tconn); err != nil {
+					return errors.Wrap(err, "failed to create a new desk")
+				}
+				w, err := ash.FindFirstWindowInOverview(ctx, tconn)
+				if err != nil {
+					return errors.Wrap(err, "failed to find the window in the overview mode")
+				}
+				// Wait for the desk mini-view to finish animating before we get its
+				// bounds. The animation only takes 250 milliseconds, but we may need to
+				// wait a little longer because everything takes time.
+				testing.Sleep(ctx, 500*time.Millisecond)
+				deskMiniViews, err := chromeui.FindAll(ctx, tconn, chromeui.FindParams{ClassName: "DeskMiniView"})
+				if err != nil {
+					return errors.Wrap(err, "failed to get desk mini-views")
+				}
+				defer deskMiniViews.Release(ctx)
+				if deskMiniViewCount := len(deskMiniViews); deskMiniViewCount != 2 {
+					return errors.Wrapf(err, "expected 2 desk mini-views; found %v", deskMiniViewCount)
+				}
+				wCenterX, wCenterY := tcc.ConvertLocation(w.OverviewInfo.Bounds.CenterPoint())
+				dCenterX, dCenterY := tcc.ConvertLocation(deskMiniViews[1].Location.CenterPoint())
+				if err := stw.Swipe(ctx, wCenterX, wCenterY, dCenterX, dCenterY, time.Second); err != nil {
+					return errors.Wrap(err, "failed to swipe window to desk mini-view")
+				}
+				if err := stw.End(); err != nil {
+					return errors.Wrap(err, "failed to end the swipe")
+				}
+				if _, err := ash.FindFirstWindowInOverview(ctx, tconn); err == nil {
+					return errors.Wrap(err, "failed to arrange clamshell split view with empty overview grid")
+				}
+				return nil
+			}},
+			// 9 windows, including 1 on the extra virtual desk from the "SingleWindow" case.
+			{"WithOverview", 9, func() error { return nil }},
+		}
+		modeName = "ClamshellMode"
+	}
+
+	currentWindows := 0
+	pv := perf.NewValues()
+	for _, testCase := range testCases {
 		s.Run(ctx, testCase.name, func(ctx context.Context, s *testing.State) {
 			conns, err := ash.CreateWindows(ctx, cr, ui.PerftestURL, testCase.numWindows-currentWindows)
 			if err != nil {
@@ -157,9 +242,6 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 			// achieve ths split-view state. The operation is
 			// * long press at the center of the target window to start dragging
 			// * move the touch point to the middle-left of the screen to snap left
-			// Note that ash.SetWindowState(SnapLeft) is not available -- it's not yet
-			// working well on the tablet mode. See also: https://crbug.com/1045990.
-			// TODO(mukai): use ash.SetWindowState here once it becomes available.
 			if err := ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
 				s.Fatal("Failed to enter into the overview mode: ", err)
 			}
@@ -168,8 +250,10 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to find the window in the overview mode: ", err)
 			}
 			centerX, centerY := tcc.ConvertLocation(w.OverviewInfo.Bounds.CenterPoint())
-			if err := stw.LongPressAt(ctx, centerX, centerY); err != nil {
-				s.Fatal("Failed to long-press to select the target window: ", err)
+			if tabletMode {
+				if err := stw.LongPressAt(ctx, centerX, centerY); err != nil {
+					s.Fatal("Failed to long-press to select the target window: ", err)
+				}
 			}
 			if err := stw.Swipe(ctx, centerX, centerY, 0, tew.Height()/2, time.Second); err != nil {
 				s.Fatal("Failed to swipe for snapping window: ", err)
@@ -197,7 +281,7 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 			hists, err := metrics.Run(ctx, tconn, func() (err error) {
 				// The actual test scenario: first swipe to left so that the window
 				// shrinks slightly, and then swipe to the right-edge of the screen,
-				// so the left-side window should be maximized again.
+				// so the left-side window should be maximized.
 				if err := stw.Swipe(ctx, splitX, y, midX, y, time.Second); err != nil {
 					return errors.Wrap(err, "failed to swipe to the first point")
 				}
@@ -218,8 +302,8 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 					return w.ID == id0 && !w.IsAnimating && w.State == ash.WindowStateMaximized
 				}, &testing.PollOptions{Timeout: 2 * time.Second})
 			},
-				fmt.Sprintf("Ash.SplitViewResize.PresentationTime.TabletMode.%s", testCase.name),
-				fmt.Sprintf("Ash.SplitViewResize.PresentationTime.MaxLatency.TabletMode.%s", testCase.name))
+				fmt.Sprintf("Ash.SplitViewResize.PresentationTime.%s.%s", modeName, testCase.name),
+				fmt.Sprintf("Ash.SplitViewResize.PresentationTime.MaxLatency.%s.%s", modeName, testCase.name))
 			if err != nil {
 				s.Fatal("Failed to drag or get the histogram: ", err)
 			}
