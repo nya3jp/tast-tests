2cfffe0e297943835a2cb940f0fbfc49bd0df277
Sean Kau <skau@chromium.org>
2020-04-06 18:26:17 -0700

    printer: Test USB backend timeout
    Add a test that purposely tries to print to a non-existent
    USB device.  We expect the backend to stop with an exit
    code of 1.  We were seeing crashes on SIGSEGV.
    
    BUG=chromium:1068366
    TEST=tast run <> printer.USBPrinterTimeout
    
    Change-Id: Ib51365484b7840f1bf65536d4a7e9b8c32369bb9

---
+++ src/chromiumos/tast/local/bundles/cros/printer/usb_printer_timeout.go
@@ -0,0 +1,82 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package printer
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/printing/lp"
+	"chromiumos/tast/local/printing/printer"
+	"chromiumos/tast/local/syslog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         USBPrinterTimeout,
+		Desc:         "Tests that USB print jobs timeout if the device does not exist",
+		Contacts:     []string{"skau@chromium.org"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"cups"},
+		Data:         []string{"print_usb_ps.ppd.gz", "print_usb_to_print.pdf"},
+	})
+}
+
+func usbPrinterURI(vid, pid string) string {
+	return fmt.Sprintf("usb://%s/%s", vid, pid)
+}
+
+func USBPrinterTimeout(ctx context.Context, s *testing.State) {
+	if err := printer.ResetCups(ctx); err != nil {
+		s.Fatal("Failed to reset cupsd: ", err)
+	}
+
+	foundPrinterName := "broken_usb_printer"
+	// Add a non-existant Kodak (0x040a) printer.
+	if err := lp.CupsAddPrinter(ctx, foundPrinterName, usbPrinterURI("0x040a", "0xEEEE"), s.DataPath("print_usb_ps.ppd.gz")); err != nil {
+		s.Fatal("Failed to configure printer: ", err)
+	}
+	s.Log("Printer configured with name: ", foundPrinterName)
+
+	defer func() {
+		// Regardless of whether the printer was added automatically by Chrome, or
+		// explicitly by the test, it is safe to remove the printer using CUPS.
+		if err := lp.CupsRemovePrinter(ctx, foundPrinterName); err != nil {
+			s.Error("Failed to remove printer: ", err)
+		}
+	}()
+
+	logReader, readerErr := syslog.NewReader(syslog.Program("cupsd"))
+	if readerErr != nil {
+		s.Fatal("Failed to start log reader: ", readerErr)
+	}
+	defer logReader.Close()
+
+	job, err := lp.CupsStartPrintJob(ctx, foundPrinterName, s.DataPath("print_usb_to_print.pdf"))
+	if err != nil {
+		s.Fatal("Failed to start printer: ", err)
+	}
+
+	s.Log(ctx, " Waiting for ", job, " to complete")
+	var logEntry *syslog.Entry
+	re := regexp.MustCompile(`\[Job \d+\] PID \d+ \(/usr/libexec/cups/backend/usb\) .*`)
+	logEntry, err = logReader.Wait(ctx, time.Duration(30)*time.Second, func(entry *syslog.Entry) bool {
+		return re.MatchString(entry.Content)
+	})
+	if err != nil {
+		s.Fatal("Print job never completed")
+	}
+
+	if strings.Contains(logEntry.Content, "crashed") {
+		s.Fatal("USB Backend crashed: " + logEntry.Content)
+	} else if !strings.Contains(logEntry.Content, "stopped with status 1") {
+		s.Fatal("USB Backend exited with an unrecognized error: " + logEntry.Content)
+	}
+	// It's expected that the usb backend exited with "stopped on status 1" indicating a timeout because the printer was unreachable.
+}
