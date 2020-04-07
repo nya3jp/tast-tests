01069ff06ebf6bf109c0f1b507e761821a462528
Jorge Lucangeli Obes <jorgelo@chromium.org>
2020-04-07 10:43:16 -0400

    kernel.ConfigVerify: Add VMAP_STACK, INIT_STACK_ALL.
    BUG=chromium:1061514
    TEST=Passes on betty, amd64-generic.
    
    Change-Id: Ic2435f27293011a460af7bbbc202f8e6da11e645

--- src/chromiumos/tast/local/bundles/cros/kernel/config_verify.go
+++ src/chromiumos/tast/local/bundles/cros/kernel/config_verify.go
@@ -38,6 +38,7 @@ func ConfigVerify(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to get kernel version and arch: ", err)
 	}
+	s.Log("Arch is ", arch)
 
 	conf, err := readKernelConfig(ctx)
 	if err != nil {
@@ -213,13 +214,9 @@ func newKernelConfigCheck(ver *kernelVersion, arch string) *kernelConfigCheck {
 		// TODO(crbug.com/1061514): Start enabling these.
 		"HARDENED_USERCOPY",
 
-		// "VMAP_STACK",
-
 		// CONFIG_UNMAP_KERNEL_AT_EL0=y (aarch64)
 
 		// CONFIG_ARM64_SW_TTBR0_PAN=y (aarch64)
-
-		// CONFIG_INIT_STACK_ALL=y (since v5.2, with Clang)
 
 		// CONFIG_SHUFFLE_PAGE_ALLOCATOR=y (since v5.2)
 
@@ -346,6 +343,10 @@ func newKernelConfigCheck(ver *kernelVersion, arch string) *kernelConfigCheck {
 	if ver.isOrLater(4, 14) {
 		builtin = append(builtin, "SLAB_FREELIST_RANDOM")
 		builtin = append(builtin, "SLAB_FREELIST_HARDENED")
+		if arch != "arm32" {
+			builtin = append(builtin, "VMAP_STACK")
+			builtin = append(builtin, "INIT_STACK_ALL")
+		}
 	}
 
 	if ver.isOrLater(4, 19) {
