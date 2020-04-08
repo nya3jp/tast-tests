3c99e9fcfd47b3e5246fc183ead0294244df7dc5
Miguel Casas <mcasas@chromium.org>
2020-04-08 16:57:31 -0400

    tast-tests: move arc-specific DecodeAccelVDVDA(Perf) to arc/video
    This CL moves the tests
    video.DecodeAccelVDVDA.*     --> arc.VideoDecodeAcelVDVDA.*
    video.DecodeAccelVDVDAPerf.* --> arc.VideoDecodeAcceVDVDAlPerf.*
    
    The two files used for manipulating a command line binary are
    migrated wholesale to local/media/decoding, except for the use
    of testing.State that had to be removed (presubmit check),
    returning an error instead of doing s.Fatal() directly etc.
    Callsites are updated.
    
    No functional changes intended.
    
    Note that I had to add Timeout and "arc" as a dependency to
    the arc/ files due to a testcheck.go run.
    
    BUG=chromium:1066463
    TEST=tast -verbose run IP arc.DecodeAccelVD* video.DecodeAccel.*
      on kohaku
    
    Change-Id: I01bef64bce49332f33b8ba811383ef5d51f0c0d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2142588
    Tested-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd.go
@@ -7,8 +7,8 @@ package video
 import (
 	"context"
 
-	"chromiumos/tast/local/bundles/cros/video/decode"
 	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/decoding"
 	"chromiumos/tast/testing"
 )
 
@@ -89,5 +89,7 @@ func init() {
 }
 
 func DecodeAccelVD(ctx context.Context, s *testing.State) {
-	decode.RunAccelVideoTest(ctx, s, s.Param().(string), decode.VD)
+	if err := decoding.RunAccelVideoTest(ctx, s.OutDir(), s.DataPath(s.Param().(string)), decoding.VD); err != nil {
+		s.Fatal("test failed: ", err)
+	}
 }
