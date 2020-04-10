d405ecd9082f0ca43aaa78cc172fff049c37dd29
Leo Lai <cylai@google.com>
2020-04-10 14:28:25 +0800

    tast-test: hwsec: disable EMK VA challange
    Now this part of the test needs policy file on the disk. Until we sort out a
    better way to verify EMK we should just disable it.
    
    BUG=b:152525281
    BUG=b:165426637
    TEST=hwsec.Attestation
    
    Change-Id: Iec3ef8e0cfb0874064dc0939780209073c9f4fe7

--- src/chromiumos/tast/local/bundles/cros/hwsec/attestation.go
+++ src/chromiumos/tast/local/bundles/cros/hwsec/attestation.go
@@ -112,8 +112,11 @@ func Attestation(ctx context.Context, s *testing.State) {
 				s.Fatal("Faild to get certificate: ", enrollReply.Status.String())
 			}
 
-			if err := at.SignEnterpriseChallenge(ctx, username, hwsec.DefaultCertLabel); err != nil {
-				s.Fatal("Failed to sign enterprise challenge: ", err)
+			// TODO(b/165426637): Enable it after we inject the fake devive policy with customer ID.
+			if username != "" {
+				if err := at.SignEnterpriseChallenge(ctx, username, hwsec.DefaultCertLabel); err != nil {
+					s.Fatal("Failed to sign enterprise challenge: ", err)
+				}
 			}
 
 			if err := at.SignSimpleChallenge(ctx, username, hwsec.DefaultCertLabel); err != nil {
