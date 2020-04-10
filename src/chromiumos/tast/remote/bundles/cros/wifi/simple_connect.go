bfea03727c65009139b27d2ecb94f932e028d55e
Yen-lin Lai <yenlinlai@google.com>
2020-04-10 14:07:11 +0800

    wificell: Add precondition for TestFixture.
    The TestFixture setup takes a few seconds to finish. As we're going to
    having more testcases, move it to precondition to save testing time.
    
    This also allows sharing interfaces between tests and reduce interface
    creation/deletion.
    
    Run with my local setup, it can save ~5 seconds per extra test.
    (12 WiFi tests: 265.318 -> 210.558 seconds in run_remote_tests)
    
    BUG=chromium:1034875
    TEST=tast run $DUT wifi.SimpleConnect.\*
    
    Change-Id: I987cbaa6cd0fc1467b0cf1e9ccdbddcafd2e1500

--- src/chromiumos/tast/remote/bundles/cros/wifi/simple_connect.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/simple_connect.go
@@ -25,8 +25,9 @@ func init() {
 		Desc:        "Verifies that DUT can connect to the host via AP in different WiFi configuration",
 		Contacts:    []string{"yenlinlai@google.com", "chromeos-kernel-wifi@google.com"},
 		Attr:        []string{"group:wificell", "wificell_func", "wificell_unstable"},
-		ServiceDeps: []string{"tast.cros.network.Wifi"},
+		ServiceDeps: []string{wificell.TFServiceName},
 		Vars:        []string{"router"},
+		Pre:         wificell.TestFixturePre(),
 		Params: []testing.Param{
 			{
 				// Verifies that DUT can connect to an open 802.11a network on channels 48, 64.
@@ -132,14 +133,10 @@ func init() {
 }
 
 func SimpleConnect(ctx context.Context, s *testing.State) {
-	router, _ := s.Var("router")
-	tf, err := wificell.NewTestFixture(ctx, s.DUT(), s.RPCHint(), router)
-	if err != nil {
-		s.Fatal("Failed to set up test fixture: ", err)
-	}
+	tf := s.PreValue().(*wificell.TestFixture)
 	defer func() {
-		if err := tf.Close(ctx); err != nil {
-			s.Log("Failed to tear down test fixture, err: ", err)
+		if err := tf.CollectLogs(ctx); err != nil {
+			s.Log("Error collecting logs, err: ", err)
 		}
 	}()
 
