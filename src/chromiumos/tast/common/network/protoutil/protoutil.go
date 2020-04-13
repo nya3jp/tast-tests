3b1081a62ec8ae5aa74b7dc07f8e683e4a1436eb
Hsin-chen Chuang <chharry@google.com>
2020-04-13 17:14:23 +0800

    network: Add a protobuf ShillVal and its utility functions.
    Add a protobuf, ShillVal, which is a variant: it can hold one of the
    supported types. It is used to represent shill properties which are
    variants.
    
    Package protoutil implements converters between map[string]interface{}
    and ShillValMap.
    
    BUG=chromium:1034875
    TEST=~/trunk/src/platform/tast/fast_build.sh -t \
    chromiumos/tast/common/network/protoutil
    
    Change-Id: Ibf7b21443bdd227679f366adfeabc1622b078d85

---
+++ src/chromiumos/tast/common/network/protoutil/protoutil.go
@@ -0,0 +1,54 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package protoutil provides utils to deal with protobuf.
+package protoutil
+
+import (
+	"chromiumos/tast/errors"
+	"chromiumos/tast/services/cros/network"
+)
+
+// ShillValMap is a type alias of map[string]*network.ShillVal. It can be sent through protobuf.
+type ShillValMap map[string]*network.ShillVal
+
+// EncodeToShillValMap converts a map that contains supported value type to protocol buffer network.ShillVal.
+func EncodeToShillValMap(conf map[string]interface{}) (ShillValMap, error) {
+	ret := make(ShillValMap)
+	for k, v := range conf {
+		switch x := v.(type) {
+		case string:
+			ret[k] = &network.ShillVal{
+				Val: &network.ShillVal_Str{
+					Str: x,
+				},
+			}
+		case bool:
+			ret[k] = &network.ShillVal{
+				Val: &network.ShillVal_Bool{
+					Bool: x,
+				},
+			}
+		default:
+			return nil, errors.Errorf("unsupported type %T", x)
+		}
+	}
+	return ret, nil
+}
+
+// DecodeFromShillValMap converts a ShillValMap to a (key, value) map.
+func DecodeFromShillValMap(conf ShillValMap) (map[string]interface{}, error) {
+	ret := make(map[string]interface{})
+	for k, v := range conf {
+		switch x := v.Val.(type) {
+		case *network.ShillVal_Str:
+			ret[k] = x.Str
+		case *network.ShillVal_Bool:
+			ret[k] = x.Bool
+		default:
+			return nil, errors.Errorf("unsupported type %T", x)
+		}
+	}
+	return ret, nil
+}
