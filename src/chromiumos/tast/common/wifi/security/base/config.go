1c8a1f9d1688555a12d26c25decd81e0d1ac908c
Hsin-chen Chuang <chharry@google.com>
2020-04-13 17:03:11 +0800

    wifi: Add package security and package base.
    Package security defines interfaces for test authors to deal with the
    diverse WiFi security standards:
    - Interface Config defines methods to compose configuration files of AP
      and DUT.
    - Interface Factory lets the authors to register the security options in
      testing.AddTest and Gen the Config object later in test body.
    
    Package base implements the interfaces with noop and can be used for
    open network.
    
    BUG=chromium:1034875
    TEST=none
    
    Change-Id: I5958d586a345da37fb3af1f6ef624e8f68270ea5

---
+++ src/chromiumos/tast/common/wifi/security/base/config.go
@@ -0,0 +1,48 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package base provides a Config type for open network.
+package base
+
+import (
+	"chromiumos/tast/common/wifi/security"
+	"chromiumos/tast/local/shill"
+)
+
+// Config implements security.Config interface for open network, i.e., no security.
+type Config struct{}
+
+// Static check: Config implements security.Config interface.
+var _ security.Config = (*Config)(nil)
+
+// ConfigFactory provides Gen method to build a new Config.
+type ConfigFactory struct{}
+
+// Gen builds a Config.
+func (*ConfigFactory) Gen() (security.Config, error) {
+	return &Config{}, nil
+}
+
+// NewConfigFactory builds a ConfigFactory.
+func NewConfigFactory() *ConfigFactory {
+	return &ConfigFactory{}
+}
+
+// Static check: ConfigFactory implements security.ConfigFactory interface.
+var _ security.ConfigFactory = (*ConfigFactory)(nil)
+
+// Class returns the security class of open network.
+func (*Config) Class() string {
+	return shill.SecurityNone
+}
+
+// HostapdConfig returns hostapd config of open network.
+func (*Config) HostapdConfig() (map[string]string, error) {
+	return nil, nil
+}
+
+// ShillServiceProperties returns shill properties of open network.
+func (*Config) ShillServiceProperties() (map[string]interface{}, error) {
+	return nil, nil
+}
