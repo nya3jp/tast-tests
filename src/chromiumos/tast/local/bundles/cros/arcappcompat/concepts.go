7f79a05eeb3f3bee7eefcf017e669de00e6b6edb
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-04-14 14:42:40 -0700

    Run UIAutomator Test Using Tast For Concepts and Youtube
    BUG=b:150014489
    BUG=b:150013907
    TEST=None
    
    Tast Test for Concepts and Youtube app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: I6fa45a307365aadb7b9f6d50ba7a93a7250634c9

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/concepts.go
@@ -0,0 +1,149 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForConcepts = []testutil.TestSuite{
+	{Name: "Launch app in Clamshell", Fn: launchAppForConcepts},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForConcepts = []testutil.TestSuite{
+	{Name: "Launch app in Touchview", Fn: launchAppForConcepts},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Concepts,
+		Desc:         "Functional test for Concepts  that installs the app also verifies it is logged in and that the main page is open, checks Gmail correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForConcepts,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               arc.BootedAppCompat(),
+		}, {
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForConcepts,
+			},
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               arc.BootedInTabletModeAppCompat(),
+		}, {
+			Name: "vm",
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForConcepts,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               arc.VMBootedAppCompat(),
+		}, {
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForConcepts,
+			},
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               arc.VMBootedInTabletModeAppCompat(),
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// Concepts test uses library for opting into the playstore and installing app.
+// Checks  Concepts correctly changes the window states in both clamshell and touchview mode.
+func Concepts(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.tophatch.concepts"
+		appActivity = "com.tophatch.concepts.MainActivity"
+	)
+
+	// Step up chrome on Chromebook.
+	cr, tconn, a, d := testutil.SetUpDevice(ctx, s, appPkgName, appActivity)
+	defer d.Close()
+
+	testSet := s.Param().(testutil.TestParams)
+	// Run the different test cases.
+	for idx, test := range testSet.Tests {
+		// Run subtests.
+		s.Run(ctx, test.Name, func(ctx context.Context, s *testing.State) {
+			defer func() {
+				if s.HasError() {
+					path := fmt.Sprintf("%s/screenshot-arcappcompat-failed-test-%d.png", s.OutDir(), idx)
+					if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
+						s.Log("Failed to capture screenshot: ", err)
+					}
+				}
+			}()
+			// Launch the app.
+			act, err := arc.NewActivity(a, appPkgName, appActivity)
+			if err != nil {
+				s.Fatal("Failed to create new app activity: ", err)
+			}
+			s.Log("Created new app activity")
+
+			defer act.Close()
+			if err := act.Start(ctx, tconn); err != nil {
+				s.Fatal("Failed start app: ", err)
+			}
+			s.Log("App launched successfully")
+
+			defer act.Stop(ctx)
+
+			test.Fn(ctx, s, tconn, a, d, appPkgName, appActivity)
+		})
+	}
+}
+
+// launchAppForConcepts verifies app is logged in and
+// verify app reached main activity page of the app.
+func launchAppForConcepts(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+
+	const (
+		closeButtonClassName = "android.widget.ImageButton"
+		closeButtonID        = "com.tophatch.concepts:id/closeButton"
+		addButtonID          = "com.tophatch.concepts:id/addButton"
+	)
+
+	// Click on close button to launch home page of the app.
+	closeButton := d.Object(ui.ClassName(closeButtonClassName), ui.ID(closeButtonID))
+	if err := closeButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("Give Access Button doesn't exists: ", err)
+	} else if err := closeButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on closeButton: ", err)
+	}
+
+	// Check home page is launched.
+	addButton := d.Object(ui.ID(addButtonID))
+	if err := addButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("AddButton doesn't exists: ", err)
+	}
+}
