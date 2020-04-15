12243fbd79b1348f9bb06a966c5affc57ea5fc2e
Greg Edelston <gredelston@google.com>
2020-04-15 14:20:50 -0600

    Create new test: firmware.Config
    Previously, we were testing that we could correctly load
    fw-testing-configs as a side-effect of firmware.CheckBootMode. But it
    makes more sense to move this into its own test. This way we can verify
    that each board is able to load its configs.
    
    Generalized config functionality, such as inheritance, would be better
    handled in a config_test.go.
    
    BUG=b:151832522
    TEST=firmware.Config
    TEST=firmware.CheckBootMode
    
    Change-Id: Ic31622a731c884621a52f9aead9cbb3b8781da44

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/config.go
@@ -0,0 +1,55 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"strings"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/remote/firmware"
+	"chromiumos/tast/rpc"
+	fwpb "chromiumos/tast/services/cros/firmware"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        Config,
+		Desc:        "Verifies that remote tests can load fw-testing-configs properly",
+		Contacts:    []string{"chromeos-engprod@google.com"},
+		Data:        firmware.ConfigDatafiles(),
+		ServiceDeps: []string{"tast.cros.firmware.UtilsService"},
+		Attr:        []string{"group:mainline", "informational"},
+	})
+}
+
+func Config(ctx context.Context, s *testing.State) {
+	// Connect to the gRPC server on the DUT.
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC: ", err)
+	}
+	defer cl.Close(ctx)
+	utils := fwpb.NewUtilsServiceClient(cl.Conn)
+
+	// Platform-specific behavior:
+	// Verify that this DUT can load its config file by name.
+	platformResponse, err := utils.Platform(ctx, &empty.Empty{})
+	if err != nil {
+		s.Fatal("Error during Platform: ", err)
+	}
+	platform := strings.ToLower(platformResponse.Platform)
+	cfg, err := firmware.NewConfig(s.DataPath(firmware.ConfigDir), platform)
+	if err != nil {
+		s.Fatal("Error during NewConfig: ", err)
+	}
+
+	// Verify that the resulting config's "platform" attribute matches the platform returned by RPC.
+	if cfg.Platform != platform {
+		s.Fatalf("cfg has Platform %s, want %s: %+v", cfg.Platform, platform, cfg)
+	}
+}
