3a5830739fffe3f18e52b9907c2116368e15937b
Shik Chen <shik@chromium.org>
2020-04-15 14:02:40 +0800

    camera: Add camera.Connector Tast test
    BUG=b:151047930
    TEST=tast run <nocturne> camera.Connector
    
    Change-Id: I41aa72059a22c955269c982e92177a7d84491f3d

---
+++ src/chromiumos/tast/local/bundles/cros/camera/connector.go
@@ -0,0 +1,47 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"path/filepath"
+
+	"chromiumos/tast/local/gtest"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Connector,
+		Desc: "Verifies the camera service connector library works",
+		Contacts: []string{
+			"shik@chromium.org",
+			"lnishan@chromium.org",
+			"chromeos-camera-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"arc_camera3"},
+	})
+}
+
+func Connector(ctx context.Context, s *testing.State) {
+	if err := upstart.EnsureJobRunning(ctx, "cros-camera"); err != nil {
+		s.Fatal("Failed to start cros-camera: ", err)
+	}
+
+	const exec = "cros_camera_connector_test"
+	t := gtest.New(exec,
+		gtest.Logfile(filepath.Join(s.OutDir(), "gtest.log")))
+
+	if report, err := t.Run(ctx); err != nil {
+		if report != nil {
+			for _, name := range report.FailedTestNames() {
+				s.Error(name, " failed")
+			}
+		}
+		s.Errorf("Failed to run %v: %v", exec, err)
+	}
+}
