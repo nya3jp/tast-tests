4ac45157516728f7a255b645e088f4911b52dd01
Avery Musbach <amusbach@chromium.org>
2020-04-15 10:26:53 -0700

    tast-tests: Add subtests to arc.SplitView
    These subtests check that overview is not canceled when split view is
    started by snapping an ARC window (in a real workflow which triggers
    window activation along with the snap).
    
    BUG=b:143499564
    TEST=arc.SplitView
    
    Change-Id: I236ac293082f5e3196d04e92c6e721ff2ecf580c

--- src/chromiumos/tast/local/bundles/cros/arc/split_view.go
+++ src/chromiumos/tast/local/bundles/cros/arc/split_view.go
@@ -6,11 +6,15 @@ package arc
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/ui"
 	"chromiumos/tast/testing"
 )
 
@@ -18,10 +22,9 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         SplitView,
 		Desc:         "Tests split view works properly with ARC apps",
-		Contacts:     []string{"tetsui@chromium.org", "arc-framework+tast@google.com"},
+		Contacts:     []string{"tetsui@chromium.org", "amusbach@chromium.org", "arc-framework+tast@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"android_p", "chrome"},
-		Pre:          arc.BootedInTabletMode(),
 	})
 }
 
@@ -66,48 +69,164 @@ func showActivityForSplitViewTest(ctx context.Context, tconn *chrome.TestConn, a
 }
 
 func SplitView(ctx context.Context, s *testing.State) {
-	p := s.PreValue().(arc.PreData)
-	cr := p.Chrome
-	a := p.ARC
+	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs("--enable-features=DragToSnapInClamshellMode"))
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
 
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Creating test API connection failed: ", err)
 	}
 
-	// Show two activities. As the content of the activities doesn't matter,
-	// use two activities available by default.
-	rightAct, err := showActivityForSplitViewTest(
-		ctx, tconn, a, "com.android.storagemanager", ".deletionhelper.DeletionHelperActivity")
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, true)
 	if err != nil {
-		s.Fatal("Failed to show an activity: ", err)
+		s.Fatal("Failed to ensure in tablet mode: ", err)
 	}
-	defer rightAct.Close()
-	leftAct, err := showActivityForSplitViewTest(ctx, tconn, a, "com.android.settings", ".Settings")
+	defer cleanup(ctx)
+
+	s.Run(ctx, "WindowStateSync", func(ctx context.Context, s *testing.State) {
+		a, err := arc.New(ctx, s.OutDir())
+		if err != nil {
+			s.Fatal("Failed to start ARC: ", err)
+		}
+		defer a.Close()
+
+		// Show two activities. As the content of the activities doesn't matter,
+		// use two activities available by default.
+		rightAct, err := showActivityForSplitViewTest(
+			ctx, tconn, a, "com.android.storagemanager", ".deletionhelper.DeletionHelperActivity")
+		if err != nil {
+			s.Fatal("Failed to show an activity: ", err)
+		}
+		defer rightAct.Close()
+		defer rightAct.Stop(ctx)
+		leftAct, err := showActivityForSplitViewTest(ctx, tconn, a, "com.android.settings", ".Settings")
+		if err != nil {
+			s.Fatal("Failed to show an activity: ", err)
+		}
+		defer leftAct.Close()
+		defer leftAct.Stop(ctx)
+
+		// Snap activities to left and right.
+		if _, err := ash.SetARCAppWindowState(ctx, tconn, leftAct.PackageName(), ash.WMEventSnapLeft); err != nil {
+			s.Fatal("Failed to snap app in split view: ", err)
+		}
+		if _, err := ash.SetARCAppWindowState(ctx, tconn, rightAct.PackageName(), ash.WMEventSnapRight); err != nil {
+			s.Fatal("Failed to snap app in split view: ", err)
+		}
+
+		if err := waitUntilStateChangeInSplitView(ctx, tconn, leftAct, rightAct); err != nil {
+			s.Fatal("Failed to wait until window state change: ", err)
+		}
+
+		// Swap the left activity and the right activity.
+		if err := ash.SwapWindowsInSplitView(ctx, tconn); err != nil {
+			s.Fatal("Failed to swap windows in split view: ", err)
+		}
+		leftAct, rightAct = rightAct, leftAct
+
+		if err := waitUntilStateChangeInSplitView(ctx, tconn, leftAct, rightAct); err != nil {
+			s.Fatal("Failed to wait until window state change: ", err)
+		}
+	})
+
+	tew, err := input.Touchscreen(ctx)
 	if err != nil {
-		s.Fatal("Failed to show an activity: ", err)
+		s.Fatal("Failed to access to the touch screen: ", err)
 	}
-	defer leftAct.Close()
+	defer tew.Close()
 
-	// Snap activities to left and right.
-	if _, err := ash.SetARCAppWindowState(ctx, tconn, leftAct.PackageName(), ash.WMEventSnapLeft); err != nil {
-		s.Fatal("Failed to snap app in split view: ", err)
+	// Ensures landscape orientation so this test can assume that windows snap on
+	// the left and right. Windows snap on the top and bottom in portrait-oriented
+	// tablet mode. They snap on the left and right in portrait-oriented clamshell
+	// mode, but there are active (although contentious) proposals to change that.
+	orientation, err := display.GetOrientation(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to obtain the orientation info: ", err)
 	}
-	if _, err := ash.SetARCAppWindowState(ctx, tconn, rightAct.PackageName(), ash.WMEventSnapRight); err != nil {
-		s.Fatal("Failed to snap app in split view: ", err)
+	rotation := -orientation.Angle
+	if orientation.Type == display.OrientationPortraitPrimary {
+		info, err := display.GetInternalInfo(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to obtain internal display info: ", err)
+		}
+		if err = display.SetDisplayRotationSync(ctx, tconn, info.ID, display.Rotate90); err != nil {
+			s.Fatal("Failed to rotate display: ", err)
+		}
+		defer display.SetDisplayRotationSync(ctx, tconn, info.ID, display.Rotate0)
+		rotation += 90
+	}
+	tew.SetRotation(rotation)
+
+	info, err := display.GetInternalInfo(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get the internal display info: ", err)
+	}
+	tcc := tew.NewTouchCoordConverter(info.Bounds.Size())
+
+	stw, err := tew.NewSingleTouchWriter()
+	if err != nil {
+		s.Fatal("Failed to create a single touch writer: ", err)
 	}
 
-	if err := waitUntilStateChangeInSplitView(ctx, tconn, leftAct, rightAct); err != nil {
-		s.Fatal("Failed to wait until window state change: ", err)
+	conns, err := ash.CreateWindows(ctx, cr, ui.PerftestURL, 1)
+	if err != nil {
+		s.Fatal("Failed to open a non-ARC window: ", err)
+	}
+	conns.Close()
+
+	subtestNotEndOverview := func(ctx context.Context, s *testing.State) {
+		a, err := arc.New(ctx, s.OutDir())
+		if err != nil {
+			s.Fatal("Failed to start ARC: ", err)
+		}
+		defer a.Close()
+
+		act, err := showActivityForSplitViewTest(ctx, tconn, a, "com.android.settings", ".Settings")
+		if err != nil {
+			s.Fatal("Failed to show an activity: ", err)
+		}
+		defer act.Close()
+		defer act.Stop(ctx)
+
+		if err := ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+			s.Fatal("Failed to enter overview: ", err)
+		}
+		arcWin, err := ash.FindFirstWindowInOverview(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to find the ARC window in the overview grid: ", err)
+		}
+		centerX, centerY := tcc.ConvertLocation(arcWin.OverviewInfo.Bounds.CenterPoint())
+		if err := stw.LongPressAt(ctx, centerX, centerY); err != nil {
+			s.Fatal("Failed to long-press to start dragging the ARC window: ", err)
+		}
+		if err := stw.Swipe(ctx, centerX, centerY, 0, tew.Height()/2, time.Second); err != nil {
+			s.Fatal("Failed to swipe for snapping the ARC window: ", err)
+		}
+		if err := stw.End(); err != nil {
+			s.Fatal("Failed to end the swipe: ", err)
+		}
+		if err := ash.WaitForCondition(ctx, tconn, func(w *ash.Window) bool {
+			return w.ID == arcWin.ID && !w.IsAnimating && w.State == ash.WindowStateLeftSnapped
+		}, &testing.PollOptions{Timeout: 15 * time.Second}); err != nil {
+			s.Fatal("Failed to wait for the ARC window to be left-snapped: ", err)
+		}
+
+		// Check for https://b.corp.google.com/issues/143499564.
+		if _, err := ash.FindFirstWindowInOverview(ctx, tconn); err != nil {
+			s.Fatal("Failed to find the non-ARC window in the overview grid: ", err)
+		}
 	}
 
-	// Swap the left activity and the right activity.
-	if err := ash.SwapWindowsInSplitView(ctx, tconn); err != nil {
-		s.Fatal("Failed to swap windows in split view: ", err)
-	}
-	leftAct, rightAct = rightAct, leftAct
+	s.Run(ctx, "TabletSplitViewNotEndOverview", subtestNotEndOverview)
 
-	if err := waitUntilStateChangeInSplitView(ctx, tconn, leftAct, rightAct); err != nil {
-		s.Fatal("Failed to wait until window state change: ", err)
+	cleanup2, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
+	if err != nil {
+		s.Fatal("Failed to ensure in clamshell mode: ", err)
 	}
+	defer cleanup2(ctx)
+
+	s.Run(ctx, "ClamshellSplitViewNotEndOverview", subtestNotEndOverview)
 }
