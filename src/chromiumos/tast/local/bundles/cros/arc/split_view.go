d33608ac90994abe42c6bbc8ebd491794b63a640
Avery Musbach <amusbach@chromium.org>
2020-04-15 10:26:53 -0700

    tast-tests: Improve arc.SplitView so it catches b/143499564
    arc.SplitView shall effectively check that overview is not canceled when
    split view is started by snapping an ARC window (in a real workflow
    which triggers window activation along with the snap).
    
    BUG=b:143499564
    TEST=arc.SplitView
    
    Change-Id: I1ccbe91fefafa2263f4de6e73b06edd9c45f96d0

--- src/chromiumos/tast/local/bundles/cros/arc/split_view.go
+++ src/chromiumos/tast/local/bundles/cros/arc/split_view.go
@@ -6,11 +6,14 @@ package arc
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
 
@@ -18,17 +21,64 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         SplitView,
 		Desc:         "Tests split view works properly with ARC apps",
-		Contacts:     []string{"tetsui@chromium.org", "arc-framework+tast@google.com"},
+		Contacts:     []string{"tetsui@chromium.org", "amusbach@chromium.org", "arc-framework+tast@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"android_p", "chrome"},
-		Pre:          arc.BootedInTabletMode(),
+		Params: []testing.Param{
+			{
+				Name: "clamshell_mode",
+				Val:  false,
+			},
+			{
+				Name:              "tablet_mode",
+				ExtraSoftwareDeps: []string{"tablet_mode"},
+				Pre:               arc.BootedInTabletMode(),
+				Val:               true,
+			},
+		},
 	})
 }
 
-// waitUntilStateChangeInSplitView waits for window state changes on both Ash
-// and ARC sides. It assumes Ash is currently in split view mode, and ARC
-// activities passed as left and right are both shown side by side.
-func waitUntilStateChangeInSplitView(ctx context.Context, tconn *chrome.TestConn, left, right *arc.Activity) error {
+// dragToSnapFirstOverviewWindow finds the first window in overview, and drags
+// to snap it. This function assumes that overview is already active.
+func dragToSnapFirstOverviewWindow(ctx context.Context, tconn *chrome.TestConn, tew *input.TouchscreenEventWriter, targetX input.TouchCoord) error {
+	info, err := display.GetInternalInfo(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to get the internal display info")
+	}
+	tcc := tew.NewTouchCoordConverter(info.Bounds.Size())
+
+	stw, err := tew.NewSingleTouchWriter()
+	if err != nil {
+		return errors.Wrap(err, "failed to create a single touch writer")
+	}
+	defer stw.Close()
+
+	w, err := ash.FindFirstWindowInOverview(ctx, tconn)
+	if err != nil {
+		// If you see this error on the second window snap (to the right), check if
+		// b/143499564 has been reintroduced.
+		return errors.Wrap(err, "failed to find window in overview grid")
+	}
+
+	centerX, centerY := tcc.ConvertLocation(w.OverviewInfo.Bounds.CenterPoint())
+	if err := stw.LongPressAt(ctx, centerX, centerY); err != nil {
+		return errors.Wrap(err, "failed to long-press to start dragging window")
+	}
+	if err := stw.Swipe(ctx, centerX, centerY, targetX, tew.Height()/2, time.Second); err != nil {
+		return errors.Wrap(err, "failed to swipe for snapping window")
+	}
+	if err := stw.End(); err != nil {
+		return errors.Wrap(err, "failed to end swipe")
+	}
+
+	return nil
+}
+
+// waitUntilStateChange waits for window state changes on both Ash and ARC
+// sides. If left is not nil, it is expected to become left snapped.
+// Likewise, if right is not nil, it is expected to become right snapped.
+func waitUntilStateChange(ctx context.Context, tconn *chrome.TestConn, left, right *arc.Activity) error {
 	return testing.Poll(ctx, func(ctx context.Context) error {
 		for _, test := range []struct {
 			act      *arc.Activity
@@ -37,6 +87,10 @@ func waitUntilStateChangeInSplitView(ctx context.Context, tconn *chrome.TestConn
 		}{
 			{left, ash.WindowStateLeftSnapped, arc.WindowStatePrimarySnapped},
 			{right, ash.WindowStateRightSnapped, arc.WindowStateSecondarySnapped}} {
+			if test.act == nil {
+				continue
+			}
+
 			if actual, err := ash.GetARCAppWindowState(ctx, tconn, test.act.PackageName()); err != nil {
 				return testing.PollBreak(errors.Wrap(err, "failed to get Ash window state"))
 			} else if actual != test.ashState {
@@ -67,14 +121,73 @@ func showActivityForSplitViewTest(ctx context.Context, tconn *chrome.TestConn, a
 }
 
 func SplitView(ctx context.Context, s *testing.State) {
-	p := s.PreValue().(arc.PreData)
-	cr := p.Chrome
-	a := p.ARC
-
+	// Enable DragToSnapInClamshellMode when testing clamshell split view.
+	// TODO(https://crbug.com/1073508): When the feature is fully launched, just
+	// use s.PreValue().(arc.PreData).
+	tabletMode := s.Param().(bool)
+	var p arc.PreData
+	var cr *chrome.Chrome
+	var a *arc.ARC
+	var err error
+	if tabletMode {
+		p = s.PreValue().(arc.PreData)
+		cr = p.Chrome
+	} else {
+		cr, err = chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs("--enable-features=DragToSnapInClamshellMode"))
+		if err != nil {
+			s.Fatal("Failed to connect to Chrome: ", err)
+		}
+		defer cr.Close(ctx)
+	}
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Creating test API connection failed: ", err)
 	}
+	if tabletMode {
+		// The precondition for the tablet_mode subtest ensures tablet mode.
+		a = p.ARC
+	} else {
+		cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
+		if err != nil {
+			s.Fatal("Failed to ensure in clamshell mode: ", err)
+		}
+		defer cleanup(ctx)
+
+		a, err = arc.New(ctx, s.OutDir())
+		if err != nil {
+			s.Fatal("Failed to start ARC: ", err)
+		}
+		defer a.Close()
+	}
+
+	tew, err := input.Touchscreen(ctx)
+	if err != nil {
+		s.Fatal("Failed to access to the touch screen: ", err)
+	}
+	defer tew.Close()
+
+	// Ensure landscape orientation so this test can assume that windows snap on
+	// the left and right. Windows snap on the top and bottom in portrait-oriented
+	// tablet mode. They snap on the left and right in portrait-oriented clamshell
+	// mode, but there are active (although contentious) proposals to change that.
+	// TODO: Do this part through camera orientation when that is supported.
+	orientation, err := display.GetOrientation(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to obtain the orientation info: ", err)
+	}
+	rotation := -orientation.Angle
+	if orientation.Type == display.OrientationPortraitPrimary {
+		info, err := display.GetInternalInfo(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to obtain internal display info: ", err)
+		}
+		if err = display.SetDisplayRotationSync(ctx, tconn, info.ID, display.Rotate90); err != nil {
+			s.Fatal("Failed to rotate display: ", err)
+		}
+		defer display.SetDisplayRotationSync(ctx, tconn, info.ID, display.Rotate0)
+		rotation += 90
+	}
+	tew.SetRotation(rotation)
 
 	// Show two activities. As the content of the activities doesn't matter,
 	// use two activities available by default.
@@ -90,25 +203,32 @@ func SplitView(ctx context.Context, s *testing.State) {
 	}
 	defer leftAct.Close()
 
+	if err := ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+		s.Fatal("Failed to enter overview: ", err)
+	}
+
 	// Snap activities to left and right.
-	if _, err := ash.SetARCAppWindowState(ctx, tconn, leftAct.PackageName(), ash.WMEventSnapLeft); err != nil {
-		s.Fatal("Failed to snap app in split view: ", err)
+	if err := dragToSnapFirstOverviewWindow(ctx, tconn, tew, 0); err != nil {
+		s.Fatal("Failed to drag window from overview and snap left: ", err)
 	}
-	if _, err := ash.SetARCAppWindowState(ctx, tconn, rightAct.PackageName(), ash.WMEventSnapRight); err != nil {
-		s.Fatal("Failed to snap app in split view: ", err)
+	if err := waitUntilStateChange(ctx, tconn, leftAct, nil); err != nil {
+		s.Fatal("Failed to wait until window state change: ", err)
 	}
-
-	if err := waitUntilStateChangeInSplitView(ctx, tconn, leftAct, rightAct); err != nil {
+	if err := dragToSnapFirstOverviewWindow(ctx, tconn, tew, tew.Width()-1); err != nil {
+		s.Fatal("Failed to drag window from overview and snap right: ", err)
+	}
+	if err := waitUntilStateChange(ctx, tconn, nil, rightAct); err != nil {
 		s.Fatal("Failed to wait until window state change: ", err)
 	}
 
-	// Swap the left activity and the right activity.
-	if err := ash.SwapWindowsInSplitView(ctx, tconn); err != nil {
-		s.Fatal("Failed to swap windows in split view: ", err)
-	}
-	leftAct, rightAct = rightAct, leftAct
-
-	if err := waitUntilStateChangeInSplitView(ctx, tconn, leftAct, rightAct); err != nil {
-		s.Fatal("Failed to wait until window state change: ", err)
+	if tabletMode {
+		// Swap the left activity and the right activity.
+		if err := ash.SwapWindowsInSplitView(ctx, tconn); err != nil {
+			s.Fatal("Failed to swap windows in split view: ", err)
+		}
+		leftAct, rightAct = rightAct, leftAct
+		if err := waitUntilStateChange(ctx, tconn, leftAct, rightAct); err != nil {
+			s.Fatal("Failed to wait until window state change: ", err)
+		}
 	}
 }
