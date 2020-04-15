7a81d7320e203dffbb9f9e680a4a97ed56ff32a6
Chinglin Yu <chinglinyu@chromium.org>
2020-04-15 11:03:41 +0800

    tast-tests: add boot perf test platform.BootPerf.
    platform.BootPerf is the conversion of BootPerfServerCrosPerf autotest:
    * The server autotest is converted as a remote tast test
    platform.BootPerf in boot_perf.go.
    * The test invokes BootPerfService RPC methods to perform complex
    actions on the DUT:
     * Enable/disable bootchart.
     * Wait until boot complete and gather boot perf metrics.
     * Gather raw data files of boot perf metrics.
    
    Changes made during the conversion:
    * Fail the test if rootfs verification is disabled. In the autotest, we
    restore rootfs verification by reimage the DUT to the current version.
    In tast, we just assert that.
    * Perform a wait for CPU cool down before each iteration to reduce the
    noise from CPU temperature differences.
    
    BUG=chromium:993256
    TEST=tast run <dut> platform.BootPerf
    
    Change-Id: Ifc6245fd7245b2a0fd51b5a639a5279012d09bce

---
+++ src/chromiumos/tast/local/bundles/cros/platform/bootperf/bootchart_util.go
@@ -0,0 +1,119 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bootperf
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/google/uuid"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+)
+
+const (
+	bootchartKConfig = "cros_bootchart"
+)
+
+// getRootPartition returns root partition index by running `rootdev -s` and
+// converting the last digit to partition index.
+// For example, "/dev/mmcblk0p3" corresponds to root partition index 2 that is
+// used in modifying the kernel args.
+func getRootPartition(ctx context.Context) (string, error) {
+	out, err := testexec.CommandContext(ctx, "/usr/bin/rootdev", "-s").Output()
+	if err != nil {
+		return "", errors.Wrap(err, "failed in getting the root partition")
+	}
+
+	re := regexp.MustCompile(`dev/.*\dp(\d+)`)
+	groups := re.FindStringSubmatch(string(out))
+	if len(groups) != 2 {
+		return "", errors.Errorf("failed to parse root partition from %s", out)
+	}
+
+	i, _ := strconv.Atoi(groups[1]) // We captured \d+ and that shouldn't fail Atoi
+
+	return strconv.Itoa(i - 1), nil
+}
+
+// editKernelArgs is a helper function for editing kernel args. Function |f|
+// performs the editing action by transforming the content of saved config.
+func editKernelArgs(ctx context.Context, f func([]byte) []byte) error {
+	part, err := getRootPartition(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Save the current boot config to |prefix|.|part| (make_dev_ssd.sh saves the content to a file named |prefix|.|part|).
+	prefix := "/tmp/" + uuid.New().String()
+	err = testexec.CommandContext(ctx, "/usr/share/vboot/bin/make_dev_ssd.sh", "--save_config", prefix, "--partitions", part).Run()
+
+	if err != nil {
+		return errors.Wrap(err, "failed to save boot config")
+	}
+
+	savedKconf := fmt.Sprintf("%s.%s", prefix, part)
+	savedKConfstr, err := ioutil.ReadFile(savedKconf)
+	if err != nil {
+		return errors.Wrap(err, "failed to read saved kernel config")
+	}
+
+	// Transform the content.
+	savedKConfstr = f(savedKConfstr)
+	err = ioutil.WriteFile(savedKconf, savedKConfstr, 0644)
+	if err != nil {
+		return errors.Wrap(err, "failed to edit saved kernel config")
+	}
+
+	err = testexec.CommandContext(ctx, "/usr/share/vboot/bin/make_dev_ssd.sh", "--set_config", prefix, "--partitions", part).Run()
+	if err != nil {
+		return errors.Wrap(err, "failed to set boot config")
+	}
+
+	return nil
+}
+
+// EnableBootchart enables bootchart by adding "cros_bootchart" to kernel
+// arguments.
+func EnableBootchart(ctx context.Context) error {
+	if err := editKernelArgs(ctx, func(str []byte) []byte {
+		s := string(str)
+		if strings.Contains(s, bootchartKConfig) {
+			// Bootchart already enabled: leave the kernel args as is.
+			return str
+		}
+
+		// Append "cros_bootchart" to kernel args.
+		return []byte(fmt.Sprintf("%s %s", s, bootchartKConfig))
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DisableBootchart Disables bootchart by removing "cros_bootchart" from kernel
+// arguments.
+func DisableBootchart(ctx context.Context) error {
+	if err := editKernelArgs(ctx, func(str []byte) []byte {
+		s := string(str)
+		if !strings.Contains(s, bootchartKConfig) {
+			// Bootchart already disabled: leave the kernel args as is.
+			return str
+		}
+
+		// Remove "cros_bootchart" from kernel args.
+		return []byte(strings.ReplaceAll(s, " "+bootchartKConfig, ""))
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
