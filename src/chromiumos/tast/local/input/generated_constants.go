7bfb28261e0685dc309b7f92b15f31180c415446
Hidehiko Abe <hidehiko@chromium.org>
2020-04-16 12:11:48 +0900

    tast-tests: Introduce GenerateConstants method.
    To share the implementation.
    The method will be used in tast/ repository later
    (with more clean up in following CLs).
    
    BUG=chromium:1066363
    TEST=Ran go generate.
    
    Change-Id: Ia04d56a5e61f285e968f983154e2239e0bbcc862
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2152050
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/input/generated_constants.go
+++ src/chromiumos/tast/local/input/generated_constants.go
@@ -9,7 +9,7 @@ package input
 // Do not change the above line; see https://golang.org/pkg/cmd/go/internal/generate/
 //
 // This file contains constants from include/uapi/linux/input-event-codes.h
-// in the Linux kernel repository at revision 4559d8a514331df16f38d611fb10055928546fd8.
+// in the Linux kernel repository at revision 2eca55fc6a9240d9a723701b7a3b9b05da2849fe.
 // Run "go generate" to regenerate it.
 
 //go:generate ../../../../../../tast/tools/go.sh run gen/gen_constants.go gen/util.go ../../../../../../../third_party/kernel/v4.14/include/uapi/linux/input-event-codes.h generated_constants.go
