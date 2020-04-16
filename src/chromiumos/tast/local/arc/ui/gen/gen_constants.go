2fa648faf02a013cab2192b861592f72477f07ee
Hidehiko Abe <hidehiko@chromium.org>
2020-04-16 12:11:48 +0900

    tast-tests: Introduce GenerateConstants method.
    To share the implementation.
    The method will be used in tast/ repository later
    (with more clean up in following CLs).
    
    BUG=chromium:1066363
    TEST=Ran go generate.
    
    Change-Id: Ia04d56a5e61f285e968f983154e2239e0bbcc862

--- src/chromiumos/tast/local/arc/ui/gen/gen_constants.go
+++ src/chromiumos/tast/local/arc/ui/gen/gen_constants.go
@@ -24,69 +24,56 @@ func main() {
 	inputFile := args[0]
 	outputFile := args[1]
 
-	repoPath, err := gitRelPath(inputFile)
-	if err != nil {
-		log.Fatalf("Failed to get the relpath to the git root for %v: %v", inputFile, err)
-	}
-	repoRev, err := gitRev(inputFile)
-	if err != nil {
-		log.Fatalf("Failed to get the git revision for %v: %v", inputFile, err)
-	}
-
 	const (
+		// Relative path to go.sh from the generated file.
+		goSh = "../../../../../../../tast/tools/go.sh"
+
+		// Relative path to this file from the generated file.
+		thisFile = "gen/gen_constants.go"
+
 		keyCodeType   = "KeyCode"
 		metaStateType = "MetaState"
 	)
 
-	types := []typeInfo{
-		{keyCodeType, "uint16", "represents an Android key code."},
-		{metaStateType, "uint64", "represents a meta-key state. Each bit set to 1 represents a pressed meta key."},
-	}
-
-	// We only care about KEYCODE and META prefixes. We ignore the rest.
-	groups := []groupInfo{
-		{"KEYCODE", keyCodeType, "KeyCodes constants"},
-		{"META", metaStateType, "Meta-key constants"},
-	}
-
-	const (
-		exeName = "gen/gen_constants.go"
-		goSh    = "../../../../../../../tast/tools/go.sh"
-		goGen   = `// Assumes that Android repo is checked out at same folder level as Chrome OS. e.g: If Chrome OS sources are in:
+	params := Params{
+		PackageName: "ui",
+		RepoName:    "Android frameworks/base",
+		PreludeCode: `// Assumes that Android repo is checked out at same folder level as Chrome OS. e.g: If Chrome OS sources are in:
 // ~/src/chromeos/, then Android sources should be in ~/src/android/
-//go:generate ` + goSh + ` run ` + exeName + ` gen/util.go ../../../../../../../../../../android/frameworks/base/core/java/android/view/KeyEvent.java generated_constants.go
-//go:generate ` + goSh + ` fmt generated_constants.go`
-	)
+//go:generate ` + goSh + ` run ` + thisFile + ` gen/util.go ../../../../../../../../../../android/frameworks/base/core/java/android/view/KeyEvent.java generated_constants.go
+//go:generate ` + goSh + ` fmt generated_constants.go`,
+		CopyrightYear:  2019,
+		MainGoFilePath: thisFile,
 
-	a := tmplArgs{
-		RepoPath:       repoPath,
-		RepoRev:        repoRev,
-		RepoName:       "Android frameworks/base",
-		PackageName:    "ui",
-		PreludeCode:    goGen,
-		ExecutableName: exeName,
-		CopyrightYear:  "2019",
-		Types:          types,
+		Types: []TypeSpec{
+			{keyCodeType, "uint16", "represents an Android key code."},
+			{metaStateType, "uint64", "represents a meta-key state. Each bit set to 1 represents a pressed meta key."},
+		},
+
+		// We only care about KEYCODE and META prefixes. We ignore the rest.
+		Groups: []GroupSpec{
+			{"KEYCODE", keyCodeType, "KeyCodes constants"},
+			{"META", metaStateType, "Meta-key constants"},
+		},
+
+		// Read inputFile, a KeyEvent.java. Looking for lines like:
+		//   public static final int META_SELECTING = 0x800;
+		// TODO(ricardoq): Multiline, bitwise-or metas are not supported. Find a robust way to support them. e.g:
+		//   public static final int META_SHIFT_MASK = META_SHIFT_ON
+		//        | META_SHIFT_LEFT_ON | META_SHIFT_RIGHT_ON;
+		LineParser: func() LineParser {
+			re := regexp.MustCompile(`^\s+public static final int ([_A-Z0-9]+)\s*=\s*(0x[0-9a-fA-F]+|\d+);$`)
+			return func(line string) (name, sval string, ok bool) {
+				m := re.FindStringSubmatch(line)
+				if m == nil {
+					return "", "", false
+				}
+				return m[1], m[2], true
+			}
+		}(),
 	}
 
-	// Read inputFile, a KeyEvent.java. Looking for lines like:
-	//   public static final int META_SELECTING = 0x800;
-	// TODO(ricardoq): Multiline, bitwise-or metas are not supported. Find a robust way to support them. e.g:
-	//   public static final int META_SHIFT_MASK = META_SHIFT_ON
-	//        | META_SHIFT_LEFT_ON | META_SHIFT_RIGHT_ON;
-	re := regexp.MustCompile(`^\s+public static final int ([_A-Z0-9]+)\s*=\s*(0x[0-9a-fA-F]+|\d+);$`)
-	consts, err := readConstants(inputFile, func(line string) (name, sval string, ok bool) {
-		m := re.FindStringSubmatch(line)
-		if m == nil {
-			return "", "", false
-		}
-		return m[1], m[2], true
-	})
-	if err != nil {
-		log.Fatalf("Failed to read %v: %v", inputFile, err)
-	}
-
-	if err := writeConstants(classifyConstants(consts, groups), a, outputFile); err != nil {
-		log.Fatalf("Failed to write %v: %v", outputFile, err)
+	if err := GenerateConstants(inputFile, outputFile, params); err != nil {
+		log.Fatalf("Failed to generate %v: %v", outputFile, err)
 	}
 }
