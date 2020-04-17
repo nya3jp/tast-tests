afc3779d3ee113cf8e2c01384c966f706af2d47a
Yen-lin Lai <yenlinlai@google.com>
2020-04-17 15:13:21 +0800

    wificell: Add utility object wrapping send_management_frame tool
    Add framesender.Sender object to control send_management_frame object.
    This combines several related components in Autotest:
    - send_management_frame_on_ap
    - send_management_frame
    - FrameSender
    
    The tool will be used in upcoming port of network_WiFi_CSA.
    
    BUG=b:154879577
    TEST=fast_build.sh -T
    
    Change-Id: I2b3a7e08dcea91ca5306a7d790390e8129a9230b

---
+++ src/chromiumos/tast/remote/wificell/framesender/sender_test.go
@@ -0,0 +1,66 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package framesender
+
+import (
+	"context"
+	"reflect"
+	"testing"
+)
+
+func TestConfigToArgs(t *testing.T) {
+	const iface = "iface"
+
+	testcases := []struct {
+		c      *config
+		expect []string
+	}{
+		{
+			c: &config{
+				t:     TypeBeacon,
+				ch:    36,
+				count: 1,
+			},
+			expect: []string{"-i", iface, "-t", "beacon", "-c", "36", "-n", "1"},
+		},
+		{
+			c: &config{
+				t:          TypeBeacon,
+				ch:         36,
+				count:      3,
+				ssidPrefix: "prefix",
+				numBSS:     2,
+				delay:      10,
+				destMAC:    "01:02:03:04:05:06",
+			},
+			expect: []string{
+				"-i", iface,
+				"-t", "beacon",
+				"-c", "36",
+				"-n", "3",
+				"-s", "prefix",
+				"-b", "2",
+				"-d", "10",
+				"-a", "01:02:03:04:05:06",
+			},
+		},
+	}
+
+	// A dummy sender without a valid host (and also no workDir).
+	sender := &Sender{
+		iface: iface,
+	}
+
+	for i, tc := range testcases {
+		args, err := sender.configToArgs(context.Background(), tc.c)
+		if err != nil {
+			t.Errorf("case#%d failed with err=%v", i, err)
+			continue
+		}
+		if !reflect.DeepEqual(args, tc.expect) {
+			t.Errorf("case#%d returns unexpected result: got %v, want %v", i, args, tc.expect)
+		}
+	}
+}
