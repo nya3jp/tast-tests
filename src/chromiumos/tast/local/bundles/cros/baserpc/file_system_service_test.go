e254961c985ee60495e546e6abb3ec950281bf42
Shuhei Takahashi <nya@chromium.org>
2020-04-17 06:23:25 +0000

    dutfs: Add unit tests.
    I considered several options as for the package to put these unit
    tests to, but could not come up with a better choice.
    
    Option 1:
      Server impl: chromiumos/tast/local/bundles/cros/baserpc
      Client impl: chromiumos/tast/remote/dutfs
      Unit tests:  chromiumos/tast/remote/dutfs
      => Support packages must never import bundle packages in general
         because it may lead to unexpected testing.AddTest calls on init.
         It might be acceptable because this import appears only in unit
         tests, but it is still good to avoid.
    
    Option 2 [CHOSEN]:
      Server impl: chromiumos/tast/local/bundles/cros/baserpc
      Client impl: chromiumos/tast/remote/dutfs
      Unit tests:  chromiumos/tast/local/bundles/cros/baserpc
      => Better than option 1 because its problems are resolved. OTOH
         chromiumos/tast/local/bundles/cros/baserpc has to import
         chromiumos/tast/*remote*/dutfs in unit tests.
    
    Option 3:
      Server impl: chromiumos/tast/remote/dutfs
      Client impl: chromiumos/tast/remote/dutfs
      Unit tests:  chromiumos/tast/remote/dutfs
      => In this approach baserpc.FileSystemService would embed the server
         implementation provided by the dutfs package. A problem is that
         chromiumos/tast/local/bundles/cros/baserpc has to import
         chromiumos/tast/*remote*/dutfs, even in production code. Also
         the server implementation is for local binaries so it is odd to
         put it under chromiumos/tast/remote.
    
    Option 4:
      Server impl: chromiumos/tast/common/dutfs
      Client impl: chromiumos/tast/common/dutfs
      Unit tests:  chromiumos/tast/common/dutfs
      => Neither dutfs.Client nor dutfs.Server are common to local and
         remote tests. This option essentially gives up categorizing code
         to local or remote. If we choose this, we might want to reconsider
         go/tast-support-libs-merge-proposal.
    
    BUG=chromium:1071736
    TEST=fast_build.sh -T
    
    Change-Id: Ia9a35737cf9916acedaccbd598f613701d7e4c1c

---
+++ src/chromiumos/tast/local/bundles/cros/baserpc/file_system_service_test.go
@@ -0,0 +1,164 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package baserpc
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/remote/dutfs"
+	"chromiumos/tast/services/cros/baserpc"
+	"chromiumos/tast/testutil"
+)
+
+// startTestPair starts a pair of remote file system server and client.
+// It aborts the test when it encounters an error during setup. Callers are
+// responsible for releasing the returned resources.
+func startTestPair(t *testing.T) (*grpc.Server, *grpc.ClientConn) {
+	t.Helper()
+
+	s := grpc.NewServer()
+	// Note: We omit releasing s on setup errors because this function is for
+	// unit tests only and errors are rare.
+	baserpc.RegisterFileSystemServer(s, &FileSystemService{nil})
+
+	lis, err := net.ListenTCP("tcp", nil)
+	if err != nil {
+		t.Fatal("Failed to listen: ", err)
+	}
+	go s.Serve(lis)
+
+	conn, err := grpc.Dial(lis.Addr().String(), grpc.WithInsecure())
+	if err != nil {
+		t.Fatal("Failed to dial: ", err)
+	}
+	return s, conn
+}
+
+func TestReadDir(t *testing.T) {
+	srv, conn := startTestPair(t)
+	defer srv.Stop()
+	defer conn.Close()
+
+	cl := dutfs.NewClient(conn)
+
+	dir := testutil.TempDir(t)
+	defer os.RemoveAll(dir)
+
+	fis, err := cl.ReadDir(context.Background(), dir)
+	if err != nil {
+		t.Error("ReadDir failed for empty directory: ", err)
+	} else if len(fis) > 0 {
+		t.Errorf("ReadDir returned %d entries for empty directory; want 0", len(fis))
+	}
+
+	if err := testutil.WriteFiles(dir, map[string]string{
+		"foo": "12345678",
+		"bar": "12345",
+	}); err != nil {
+		t.Fatal("Failed to write files: ", err)
+	}
+
+	fis, err = cl.ReadDir(context.Background(), dir)
+	if err != nil {
+		t.Error("ReadDir failed for non-empty directory: ", err)
+	} else {
+		var got []string
+		for _, fi := range fis {
+			got = append(got, fmt.Sprintf("%s %d", fi.Name(), fi.Size()))
+		}
+		want := []string{
+			"bar 5",
+			"foo 8",
+		}
+		if diff := cmp.Diff(got, want); diff != "" {
+			t.Error("ReadDir returned unexpected entries for non-empty directory (-got +want):\n", diff)
+		}
+	}
+
+	_, err = cl.ReadDir(context.Background(), filepath.Join(dir, "no_such_dir"))
+	// TODO(nya): Ensure that ReadDir returns os.ErrNotExist.
+	if err == nil {
+		t.Error("ReadDir unexpectedly succeeded for non-existent directory")
+	}
+}
+
+func TestStat(t *testing.T) {
+	srv, conn := startTestPair(t)
+	defer srv.Stop()
+	defer conn.Close()
+
+	cl := dutfs.NewClient(conn)
+
+	dir := testutil.TempDir(t)
+	defer os.RemoveAll(dir)
+
+	path := filepath.Join(dir, "foo")
+	if err := ioutil.WriteFile(path, []byte("12345"), 0666); err != nil {
+		t.Fatal("Failed to create file: ", err)
+	}
+
+	fi, err := cl.Stat(context.Background(), dir)
+	if err != nil {
+		t.Error("Stat failed for directory: ", err)
+	} else {
+		if !fi.IsDir() {
+			t.Error("IsDir = false for directory")
+		}
+	}
+
+	fi, err = cl.Stat(context.Background(), path)
+	if err != nil {
+		t.Error("Stat failed for file: ", err)
+	} else {
+		if fi.IsDir() {
+			t.Error("IsDir = true for file")
+		}
+	}
+
+	_, err = cl.Stat(context.Background(), filepath.Join(dir, "no_such_file"))
+	// TODO(nya): Ensure that Stat returns os.ErrNotExist.
+	if err == nil {
+		t.Error("Stat unexpectedly succeeded for non-existent file")
+	}
+}
+
+func TestReadFile(t *testing.T) {
+	srv, conn := startTestPair(t)
+	defer srv.Stop()
+	defer conn.Close()
+
+	cl := dutfs.NewClient(conn)
+
+	dir := testutil.TempDir(t)
+	defer os.RemoveAll(dir)
+
+	path := filepath.Join(dir, "foo")
+	const content = "12345"
+	if err := ioutil.WriteFile(path, []byte(content), 0666); err != nil {
+		t.Fatal("Failed to create file: ", err)
+	}
+
+	data, err := cl.ReadFile(context.Background(), path)
+	if err != nil {
+		t.Error("ReadFile failed: ", err)
+	} else if s := string(data); s != content {
+		t.Errorf("ReadFile returned %q; want %q", s, content)
+	}
+
+	_, err = cl.ReadFile(context.Background(), filepath.Join(dir, "no_such_file"))
+	// TODO(nya): Ensure that ReadFile returns os.ErrNotExist.
+	if err == nil {
+		t.Error("ReadFile unexpectedly succeeded for non-existent file")
+	}
+}
