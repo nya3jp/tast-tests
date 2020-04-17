b868f45cadd28f63e4367dad91feb1ca5e0c849d
Andrew Xu <andrewxu@chromium.org>
2020-04-16 20:03:43 -0700

    [Tast] Fix flaky hotseat scroll test [part 2]
    ui.HotseatScrollPerf is flaky. It may be caused by unstablet UI right
    after switch between clamshell and tablet. In this CL, wait for one
    second to stablize UI. If it solves the problem, a following CL should
    be landed for the real fixing.
    
    BUG=chromium:1064459
    TEST=manually
    
    Change-Id: I6578979814bc92434a5b203134cd39e17559ba45

--- src/chromiumos/tast/local/bundles/cros/ui/hotseat_scroll_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/hotseat_scroll_perf.go
@@ -7,6 +7,7 @@ package ui
 import (
 	"context"
 	"strings"
+	"time"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
@@ -169,6 +170,11 @@ func fetchShelfScrollSmoothnessHistogram(ctx context.Context, cr *chrome.Chrome,
 		return nil, errors.Wrap(err, "failed to ensure in clamshell mode")
 	}
 	defer cleanup(ctx)
+
+	// Wait for 1 second to stablize UI after switch between clamshell and tablet.
+	if err := testing.Sleep(ctx, 1*time.Second); err != nil {
+		return nil, err
+	}
 
 	launcherTargetState := ash.Closed
 	if isLauncherVisible {
