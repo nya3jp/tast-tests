22703b5ee3b35a1f212ff1a1096549eb0da400a3
Avery Musbach <amusbach@chromium.org>
2020-04-17 16:51:13 -0700

    tast-tests: Improve performance and cleanup in split_view_resize_perf.go
    Performance improvement: ui.SplitViewResizePerf shall use a precondition
    to connect to Chrome.
    
    Cleanup improvement: Both ui.SplitViewResizePerf and
    ui.SplitViewResizePerf.clamshell_mode shall close the single touch
    writer when finished.
    
    BUG=chromium:1059477
    TEST=ui.SplitViewResizePerf,ui.SplitViewResizePerf.clamshell_mode
    
    Change-Id: I33407895920414383e60ae15042c42bc3b15b368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2155201
    Tested-by: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/split_view_resize_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/split_view_resize_perf.go
@@ -37,6 +37,7 @@ func init() {
 			},
 			{
 				ExtraSoftwareDeps: []string{"tablet_mode"},
+				Pre:               chrome.LoggedIn(),
 				Val:               true,
 			},
 		},
@@ -51,14 +52,14 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 	var cr *chrome.Chrome
 	var err error
 	if tabletMode {
-		cr, err = chrome.New(ctx)
+		cr = s.PreValue().(*chrome.Chrome)
 	} else {
 		cr, err = chrome.New(ctx, chrome.ExtraArgs("--enable-features=DragToSnapInClamshellMode"))
+		if err != nil {
+			s.Fatal("Failed to connect to Chrome: ", err)
+		}
+		defer cr.Close(ctx)
 	}
-	if err != nil {
-		s.Fatal("Failed to connect to Chrome: ", err)
-	}
-	defer cr.Close(ctx)
 
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
@@ -122,6 +123,7 @@ func SplitViewResizePerf(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to create a single touch writer: ", err)
 	}
+	defer stw.Close()
 
 	// Computing the coordinates for the gesture of resizing, see also the
 	// comments around metrics.Run.
