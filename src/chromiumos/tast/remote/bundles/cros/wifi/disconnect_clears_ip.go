4064ede3f33b4d5ec96143fbb4ffe424bdc577b3
Arowa Suliman <arowa@google.com>
2020-04-19 22:42:54 -0700

    tast-test: Add DisconnectClearsIP
    The Tast test is migrated from network_WiFi_DisconnectClearsIP.py, an Autotest.
    
    BUG=b:154444284
    TEST=run tast wifi.DisconnectClearsIP
    
    Change-Id: Ie62b8b26299d7e98ed5a87ebd0047468c0e657ba

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/disconnect_clears_ip.go
@@ -0,0 +1,105 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/timing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        DisconnectClearsIP,
+		Desc:        "Check that the DUT removes the IP after disconnecting from a WiFi network",
+		Contacts:    []string{"arowa@google.com", "chromeos-platform-connectivity@google.com"},
+		Attr:        []string{"group:wificell", "wificell_func", "wificell_unstable"},
+		ServiceDeps: []string{"tast.cros.network.Wifi"},
+		Vars:        []string{"router"},
+	})
+}
+
+func DisconnectClearsIP(ctx context.Context, s *testing.State) {
+	router, _ := s.Var("router")
+	tf, err := wificell.NewTestFixture(ctx, s.DUT(), s.RPCHint(), router)
+	if err != nil {
+		s.Fatal("Failed to set up test fixture: ", err)
+	}
+	defer func() {
+		if err := tf.Close(ctx); err != nil {
+			s.Log("Failed to tear down test fixture: ", err)
+		}
+	}()
+
+	ap, err := tf.DefaultOpenNetworkAP(ctx)
+	if err != nil {
+		s.Fatal("Failed to configure the AP: ", err)
+	}
+	defer func() {
+		if err := tf.DeconfigAP(ctx, ap); err != nil {
+			s.Error("Failed to deconfig the AP: ", err)
+		}
+	}()
+
+	if err := tf.ConnectWifi(ctx, ap); err != nil {
+		s.Fatal("DUT: failed to connect to WiFi: ", err)
+	}
+
+	if err := tf.PingFromDUT(ctx); err != nil {
+		s.Fatal("Failed to ping from the DUT: ", err)
+	}
+
+	iface, err := tf.NewClientInterface(ctx)
+	if err != nil {
+		s.Fatal("DUT: failed to get the client WiFi interface: ", err)
+	}
+
+	addr, err := iface.Addresses(ctx)
+	if err != nil {
+		s.Fatal("DUT: failed to get the IP addresses info: ", err)
+	}
+
+	if addr.IPv4 == "" {
+		s.Fatal("DUT: expect an IPv4 address")
+	}
+
+	s.Logf("Connected with IP address: %s. Disconnecting WiFi", addr.IPv4)
+
+	if err := tf.DisconnectWifi(ctx); err != nil {
+		s.Error("DUT: failed to disconnect WiFi: ", err)
+	}
+
+	if s.HasError() {
+		if _, err := tf.WifiClient().DeleteEntriesForSSID(ctx, &network.SSID{Ssid: ap.Config().Ssid}); err != nil {
+			s.Errorf("Failed to remove entries for ssid=%s, err: %v", ap.Config().Ssid, err)
+		}
+	}
+
+	// Wait for IP to be cleared.
+	s.Log("Disconnected. Wait for the IP address to be cleared")
+	waitIPGone := func() {
+		ctx, st := timing.Start(ctx, "waitIPGone")
+		defer st.End()
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			addr, err := iface.Addresses(ctx)
+			if err != nil {
+				s.Fatal("DUT: failed to get the IP addresses info: ", err)
+			}
+			if addr.IPv4 != "" {
+				return errors.Errorf("DUT: expect no IPv4 address, got: %s", addr.IPv4)
+			}
+			return nil
+		}, &testing.PollOptions{Timeout: 10 * time.Second, Interval: 200 * time.Millisecond}); err != nil {
+			s.Fatal("Failed to clear the IP after WiFi disconnected: ", err)
+		}
+	}
+
+	waitIPGone()
+}
