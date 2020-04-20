71b78c0c2010dc2c66ca5ee4ae756fa030443af2
Risan <risan@google.com>
2020-04-20 21:00:17 +0900

    Enable HAL3 tests in ARCVM
    BUG=b:150009665
    TEST=Run tast test.
    
    Cq-Depend: chrome-internal:2904283
    Change-Id: Ib229d535a6c97e5ba99e47e62d3a9c0d7e499c6d

--- src/chromiumos/tast/local/bundles/cros/camera/hal3_frame.go
+++ src/chromiumos/tast/local/bundles/cros/camera/hal3_frame.go
@@ -19,7 +19,7 @@ func init() {
 		Desc:         "Verifies camera frame function with HAL3 interface",
 		Contacts:     []string{"shik@chromium.org", "chromeos-camera-eng@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"android_p", "arc_camera3", caps.BuiltinCamera},
+		SoftwareDeps: []string{"arc", "arc_camera3", caps.BuiltinCamera},
 		// Default timeout (i.e. 2 minutes) is not enough for some devices to
 		// exercise all resolutions on all cameras. Currently the device that
 		// needs longest timeout is Krane, which supports many resolutions
