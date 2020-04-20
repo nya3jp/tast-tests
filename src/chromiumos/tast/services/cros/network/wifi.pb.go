1adc0669f82f1c43625d08113e1a58b73c24b868
Arowa Suliman <arowa@google.com>
2020-04-19 22:42:54 -0700

    tast-test: Add DisconnectClearsIP
    The Tast test is migrated from network_WiFi_DisconnectClearsIP.py, an Autotest.
    
    BUG=b:154444284
    TEST=run tast wifi.DisconnectClearsIP
    
    Change-Id: Ie62b8b26299d7e98ed5a87ebd0047468c0e657ba

--- src/chromiumos/tast/services/cros/network/wifi.pb.go
+++ src/chromiumos/tast/services/cros/network/wifi.pb.go
@@ -150,33 +150,159 @@ func (m *SSID) GetSsid() string {
 	return ""
 }
 
+type NetInterface struct {
+	NetInterface         string   `protobuf:"bytes,1,opt,name=netInterface,proto3" json:"netInterface,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *NetInterface) Reset()         { *m = NetInterface{} }
+func (m *NetInterface) String() string { return proto.CompactTextString(m) }
+func (*NetInterface) ProtoMessage()    {}
+func (*NetInterface) Descriptor() ([]byte, []int) {
+	return fileDescriptor_14343df069b9efbf, []int{3}
+}
+
+func (m *NetInterface) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_NetInterface.Unmarshal(m, b)
+}
+func (m *NetInterface) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_NetInterface.Marshal(b, m, deterministic)
+}
+func (m *NetInterface) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_NetInterface.Merge(m, src)
+}
+func (m *NetInterface) XXX_Size() int {
+	return xxx_messageInfo_NetInterface.Size(m)
+}
+func (m *NetInterface) XXX_DiscardUnknown() {
+	xxx_messageInfo_NetInterface.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_NetInterface proto.InternalMessageInfo
+
+func (m *NetInterface) GetNetInterface() string {
+	if m != nil {
+		return m.NetInterface
+	}
+	return ""
+}
+
+type Adds struct {
+	Adds                 string   `protobuf:"bytes,1,opt,name=adds,proto3" json:"adds,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Adds) Reset()         { *m = Adds{} }
+func (m *Adds) String() string { return proto.CompactTextString(m) }
+func (*Adds) ProtoMessage()    {}
+func (*Adds) Descriptor() ([]byte, []int) {
+	return fileDescriptor_14343df069b9efbf, []int{4}
+}
+
+func (m *Adds) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Adds.Unmarshal(m, b)
+}
+func (m *Adds) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Adds.Marshal(b, m, deterministic)
+}
+func (m *Adds) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Adds.Merge(m, src)
+}
+func (m *Adds) XXX_Size() int {
+	return xxx_messageInfo_Adds.Size(m)
+}
+func (m *Adds) XXX_DiscardUnknown() {
+	xxx_messageInfo_Adds.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Adds proto.InternalMessageInfo
+
+func (m *Adds) GetAdds() string {
+	if m != nil {
+		return m.Adds
+	}
+	return ""
+}
+
+type WifiIface struct {
+	Iface                string   `protobuf:"bytes,1,opt,name=iface,proto3" json:"iface,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *WifiIface) Reset()         { *m = WifiIface{} }
+func (m *WifiIface) String() string { return proto.CompactTextString(m) }
+func (*WifiIface) ProtoMessage()    {}
+func (*WifiIface) Descriptor() ([]byte, []int) {
+	return fileDescriptor_14343df069b9efbf, []int{5}
+}
+
+func (m *WifiIface) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_WifiIface.Unmarshal(m, b)
+}
+func (m *WifiIface) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_WifiIface.Marshal(b, m, deterministic)
+}
+func (m *WifiIface) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_WifiIface.Merge(m, src)
+}
+func (m *WifiIface) XXX_Size() int {
+	return xxx_messageInfo_WifiIface.Size(m)
+}
+func (m *WifiIface) XXX_DiscardUnknown() {
+	xxx_messageInfo_WifiIface.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_WifiIface proto.InternalMessageInfo
+
+func (m *WifiIface) GetIface() string {
+	if m != nil {
+		return m.Iface
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*Config)(nil), "tast.cros.network.Config")
 	proto.RegisterType((*Service)(nil), "tast.cros.network.Service")
 	proto.RegisterType((*SSID)(nil), "tast.cros.network.SSID")
+	proto.RegisterType((*NetInterface)(nil), "tast.cros.network.NetInterface")
+	proto.RegisterType((*Adds)(nil), "tast.cros.network.Adds")
+	proto.RegisterType((*WifiIface)(nil), "tast.cros.network.WifiIface")
 }
 
 func init() { proto.RegisterFile("wifi.proto", fileDescriptor_14343df069b9efbf) }
 
 var fileDescriptor_14343df069b9efbf = []byte{
-	// 271 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x90, 0xcf, 0x4a, 0xf3, 0x40,
-	0x14, 0xc5, 0x9b, 0x8f, 0x92, 0x7e, 0xde, 0x9d, 0x83, 0xd4, 0x1a, 0x11, 0x4a, 0x40, 0xec, 0x6a,
-	0x06, 0xd4, 0x27, 0x48, 0x53, 0x21, 0xdb, 0x66, 0x21, 0xb8, 0x6b, 0x93, 0x9b, 0xe4, 0x62, 0x33,
-	0x37, 0xcc, 0x4c, 0x2d, 0x3e, 0xaa, 0x6f, 0x23, 0xf9, 0xd3, 0x95, 0xd1, 0xdd, 0x1d, 0xe6, 0x9c,
-	0xdf, 0x39, 0x1c, 0x80, 0x13, 0x15, 0x24, 0x1b, 0xc3, 0x8e, 0xc5, 0xa5, 0xdb, 0x59, 0x27, 0x33,
-	0xc3, 0x56, 0x6a, 0x74, 0x27, 0x36, 0xef, 0xc1, 0x6d, 0xc9, 0x5c, 0x1e, 0x50, 0x75, 0x82, 0xfd,
-	0xb1, 0x50, 0x58, 0x37, 0xee, 0xb3, 0xd7, 0x87, 0xcf, 0xe0, 0xaf, 0x59, 0x17, 0x54, 0x0a, 0x01,
-	0x53, 0x6b, 0x29, 0x5f, 0x78, 0x4b, 0x6f, 0x75, 0xb1, 0xed, 0x6e, 0x31, 0x07, 0xbf, 0xa2, 0x3c,
-	0x47, 0xbd, 0xf8, 0xb7, 0xf4, 0x56, 0xff, 0xb7, 0xc3, 0x2b, 0xbc, 0x83, 0x59, 0x8a, 0xe6, 0x83,
-	0x32, 0x6c, 0x6d, 0xcd, 0xce, 0x55, 0x67, 0x5b, 0x7b, 0x87, 0x01, 0x4c, 0xd3, 0x34, 0x89, 0xc7,
-	0x90, 0x8f, 0x5f, 0x1e, 0x4c, 0x5f, 0xa9, 0x20, 0x11, 0xc1, 0x6c, 0xcd, 0x5a, 0x63, 0xe6, 0xc4,
-	0x8d, 0xfc, 0xd1, 0x5a, 0xf6, 0xad, 0x82, 0x60, 0xe4, 0x6b, 0x88, 0x0e, 0x27, 0x22, 0x02, 0x88,
-	0xc9, 0x66, 0x03, 0xe6, 0x0f, 0x6d, 0x30, 0x97, 0xfd, 0x0a, 0xf2, 0xbc, 0x82, 0xdc, 0xb4, 0x2b,
-	0x84, 0x13, 0x91, 0xc0, 0x55, 0x8c, 0x07, 0x74, 0xb8, 0xd1, 0xce, 0x10, 0xda, 0x17, 0x36, 0x5d,
-	0xf9, 0xeb, 0x31, 0x5a, 0x9a, 0xc4, 0xbf, 0xa3, 0xa2, 0x87, 0xb7, 0xfb, 0xac, 0x32, 0x5c, 0xd3,
-	0xb1, 0x66, 0xab, 0x5a, 0xbb, 0xb2, 0x7d, 0xbe, 0x55, 0x2d, 0x47, 0x0d, 0x9c, 0xbd, 0xdf, 0x59,
-	0x9f, 0xbe, 0x03, 0x00, 0x00, 0xff, 0xff, 0xd0, 0xc5, 0x29, 0xb6, 0xba, 0x01, 0x00, 0x00,
+	// 358 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x91, 0x51, 0x4f, 0xea, 0x30,
+	0x14, 0xc7, 0x81, 0x70, 0xe1, 0x72, 0xc2, 0x7d, 0xb8, 0x0d, 0x41, 0x9c, 0x1a, 0xb1, 0x89, 0x91,
+	0xa7, 0x2e, 0x41, 0xbf, 0x00, 0x30, 0x34, 0x7b, 0xf1, 0x81, 0x3d, 0x98, 0xf8, 0x36, 0xd6, 0x33,
+	0x68, 0x84, 0x96, 0xb4, 0x45, 0xe2, 0x67, 0xf6, 0x4b, 0x98, 0x6e, 0x83, 0x60, 0x9c, 0xbc, 0x9d,
+	0xb3, 0xfd, 0xff, 0xbf, 0xa6, 0xbf, 0x02, 0xec, 0x44, 0x2a, 0xd8, 0x46, 0x2b, 0xab, 0xc8, 0x7f,
+	0x1b, 0x1b, 0xcb, 0x12, 0xad, 0x0c, 0x93, 0x68, 0x77, 0x4a, 0xbf, 0x79, 0x17, 0x0b, 0xa5, 0x16,
+	0x2b, 0xf4, 0xb3, 0xc0, 0x7c, 0x9b, 0xfa, 0xb8, 0xde, 0xd8, 0x8f, 0x3c, 0x4f, 0x1f, 0xa0, 0x31,
+	0x51, 0x32, 0x15, 0x0b, 0x42, 0xa0, 0x6e, 0x8c, 0xe0, 0xbd, 0x6a, 0xbf, 0x3a, 0x68, 0xcd, 0xb2,
+	0x99, 0x74, 0xa1, 0xb1, 0x14, 0x9c, 0xa3, 0xec, 0xd5, 0xfa, 0xd5, 0xc1, 0xdf, 0x59, 0xb1, 0xd1,
+	0x2b, 0x68, 0x46, 0xa8, 0xdf, 0x45, 0x82, 0xae, 0xb6, 0x89, 0xed, 0x72, 0x5f, 0x73, 0x33, 0xf5,
+	0xa0, 0x1e, 0x45, 0x61, 0x50, 0x86, 0xa4, 0x43, 0x68, 0x3f, 0xa3, 0x0d, 0xa5, 0x45, 0x9d, 0xc6,
+	0x09, 0x12, 0x0a, 0x6d, 0x79, 0xb4, 0x17, 0xd9, 0x6f, 0xdf, 0x1c, 0x6f, 0xc4, 0xb9, 0x71, 0xbc,
+	0x98, 0x73, 0xb3, 0xe7, 0xb9, 0x99, 0xde, 0x40, 0xeb, 0x45, 0xa4, 0x22, 0xcc, 0x60, 0x1d, 0xf8,
+	0x23, 0x8e, 0x28, 0xf9, 0x32, 0xfc, 0xac, 0x41, 0xdd, 0x65, 0xc8, 0x18, 0x9a, 0x13, 0x25, 0x25,
+	0x26, 0x96, 0x9c, 0xb3, 0x1f, 0xa2, 0x58, 0x2e, 0xc2, 0xf3, 0x4a, 0x7e, 0x15, 0xb7, 0xa5, 0x15,
+	0x32, 0x06, 0x08, 0x84, 0x49, 0x0a, 0xcc, 0x89, 0xac, 0xd7, 0x65, 0xb9, 0x78, 0xb6, 0x17, 0xcf,
+	0xa6, 0x4e, 0x3c, 0xad, 0x90, 0x10, 0x3a, 0x01, 0xae, 0xd0, 0xe2, 0x54, 0x5a, 0x2d, 0xd0, 0x3c,
+	0x2a, 0x9d, 0xf9, 0x3a, 0x2b, 0xa3, 0x45, 0x61, 0x70, 0x02, 0xf5, 0x04, 0xad, 0x11, 0xe7, 0x1a,
+	0x8d, 0x41, 0x43, 0xae, 0x4b, 0xfa, 0xc7, 0xb2, 0xbd, 0xb2, 0x03, 0x9c, 0xd9, 0x0c, 0xf4, 0x2f,
+	0xf3, 0x78, 0x78, 0x98, 0x5f, 0xce, 0xf4, 0x2e, 0x4b, 0x18, 0x87, 0x17, 0xa0, 0x95, 0xf1, 0xdd,
+	0xeb, 0x6d, 0xb2, 0xd4, 0x6a, 0x2d, 0xb6, 0x6b, 0x65, 0x7c, 0x97, 0xf5, 0x4d, 0x6e, 0xc4, 0xf8,
+	0xae, 0xe4, 0x17, 0xa5, 0x79, 0x23, 0x03, 0xdf, 0x7f, 0x05, 0x00, 0x00, 0xff, 0xff, 0x5e, 0x00,
+	0x11, 0x9f, 0xbf, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -197,6 +323,10 @@ type WifiClient interface {
 	Disconnect(ctx context.Context, in *Service, opts ...grpc.CallOption) (*empty.Empty, error)
 	// DeleteEntriesForSSID deletes all WiFi profile entries for a given ssid.
 	DeleteEntriesForSSID(ctx context.Context, in *SSID, opts ...grpc.CallOption) (*empty.Empty, error)
+	// Addresses returns the addresses (MAC, IP) associated with interface.
+	Addresses(ctx context.Context, in *NetInterface, opts ...grpc.CallOption) (*Adds, error)
+	// WifiInterface returns the wirless device interface name (e.g., wlan0).
+	WifiInterface(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*WifiIface, error)
 }
 
 type wifiClient struct {
@@ -234,6 +364,24 @@ func (c *wifiClient) DeleteEntriesForSSID(ctx context.Context, in *SSID, opts ..
 	return out, nil
 }
 
+func (c *wifiClient) Addresses(ctx context.Context, in *NetInterface, opts ...grpc.CallOption) (*Adds, error) {
+	out := new(Adds)
+	err := c.cc.Invoke(ctx, "/tast.cros.network.Wifi/Addresses", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *wifiClient) WifiInterface(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*WifiIface, error) {
+	out := new(WifiIface)
+	err := c.cc.Invoke(ctx, "/tast.cros.network.Wifi/WifiInterface", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // WifiServer is the server API for Wifi service.
 type WifiServer interface {
 	// Connect attempts to connect WiFi network.
@@ -242,6 +390,10 @@ type WifiServer interface {
 	Disconnect(context.Context, *Service) (*empty.Empty, error)
 	// DeleteEntriesForSSID deletes all WiFi profile entries for a given ssid.
 	DeleteEntriesForSSID(context.Context, *SSID) (*empty.Empty, error)
+	// Addresses returns the addresses (MAC, IP) associated with interface.
+	Addresses(context.Context, *NetInterface) (*Adds, error)
+	// WifiInterface returns the wirless device interface name (e.g., wlan0).
+	WifiInterface(context.Context, *empty.Empty) (*WifiIface, error)
 }
 
 // UnimplementedWifiServer can be embedded to have forward compatible implementations.
@@ -256,6 +408,12 @@ func (*UnimplementedWifiServer) Disconnect(ctx context.Context, req *Service) (*
 }
 func (*UnimplementedWifiServer) DeleteEntriesForSSID(ctx context.Context, req *SSID) (*empty.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeleteEntriesForSSID not implemented")
+}
+func (*UnimplementedWifiServer) Addresses(ctx context.Context, req *NetInterface) (*Adds, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Addresses not implemented")
+}
+func (*UnimplementedWifiServer) WifiInterface(ctx context.Context, req *empty.Empty) (*WifiIface, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method WifiInterface not implemented")
 }
 
 func RegisterWifiServer(s *grpc.Server, srv WifiServer) {
@@ -316,6 +474,42 @@ func _Wifi_DeleteEntriesForSSID_Handler(srv interface{}, ctx context.Context, de
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Wifi_Addresses_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(NetInterface)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(WifiServer).Addresses(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/tast.cros.network.Wifi/Addresses",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(WifiServer).Addresses(ctx, req.(*NetInterface))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Wifi_WifiInterface_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(empty.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(WifiServer).WifiInterface(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/tast.cros.network.Wifi/WifiInterface",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(WifiServer).WifiInterface(ctx, req.(*empty.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Wifi_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "tast.cros.network.Wifi",
 	HandlerType: (*WifiServer)(nil),
@@ -331,6 +525,14 @@ var _Wifi_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "DeleteEntriesForSSID",
 			Handler:    _Wifi_DeleteEntriesForSSID_Handler,
+		},
+		{
+			MethodName: "Addresses",
+			Handler:    _Wifi_Addresses_Handler,
+		},
+		{
+			MethodName: "WifiInterface",
+			Handler:    _Wifi_WifiInterface_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
