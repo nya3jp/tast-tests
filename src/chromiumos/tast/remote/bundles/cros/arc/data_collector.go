08312c6be634d8577c85a5f837d83806e2cb9e33
Lepton Wu <lepton@chromium.org>
2020-04-21 13:14:07 -0700

    tast: Mark arc.DataCollector.vm as informational
    It keeps failing on RVC and blocked RVC pfq. Hope this can unblock
    RVC pfq. And also we don't plan to maintain Pie VM, so it should
    be fine just marking it as informational for now.
    
    BUG=b:154164168
    TEST=None
    
    Change-Id: Ib092c78c27ab3801c95ce7d5dadbc55cf1685545

--- src/chromiumos/tast/remote/bundles/cros/arc/data_collector.go
+++ src/chromiumos/tast/remote/bundles/cros/arc/data_collector.go
@@ -56,6 +56,7 @@ func init() {
 		}, {
 			Name:              "vm",
 			ExtraSoftwareDeps: []string{"android_vm"},
+			ExtraAttr:         []string{"informational"},
 			Val:               true,
 		}},
 		Vars: []string{
