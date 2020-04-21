ca77b3c6e2e4f3c9cc4d3d0c7a626adcac52d320
Yicheng Li <yichengli@chromium.org>
2020-04-21 09:53:51 -0700

    biod: Add test to ensure biod is running on boards with fingerprint
    This is an effort to port the autotest http://crrev/c/2080739 to Tast.
    
    BUG=b:153736350
    TEST=tast -verbose run <IP> biod.IsRunning
    
    Change-Id: If9dd1c07ce52f3cf22a3ff0a65c4758962930053

---
+++ src/chromiumos/tast/local/bundles/cros/biod/is_running.go
@@ -0,0 +1,36 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package biod
+
+import (
+	"context"
+
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: IsRunning,
+		Desc: "Checks that biod is running on devices with fingerprint sensor",
+		Contacts: []string{
+			"yichengli@chromium.org", // Test author
+			"tomhughes@chromium.org",
+			"chromeos-fingerprint@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"biometrics_daemon"},
+		HardwareDeps: hwdep.D(hwdep.Fingerprint()),
+	})
+}
+
+// IsRunning checks the biod job and fails if it isn't running or has a process
+// in the zombie state.
+func IsRunning(ctx context.Context, s *testing.State) {
+	if err := upstart.CheckJob(ctx, "biod"); err != nil {
+		s.Fatal("Test failed: ", err)
+	}
+}
