4855f054eccbbc4b292bafbb9a1bc3aef83834c4
Yen-lin Lai <yenlinlai@google.com>
2020-04-21 10:25:22 +0800

    network: Add WifiCaps80211ac test.
    Add a 802.11ac capability test to exclude the boards that does not
    support 802.11ac and check ac related capabilities.
    
    MUMIMO is also ac related, but for the test coverage, keep it in the
    critical WifiCaps test for now.
    
    BUG=chromium:1024554
    TEST=`tast run $DUT network.WifiCaps\*` on monroe and krane DUT
    
    Change-Id: I3e5ef9cb6fc01f469f67f6b3d25ea28f19e52f40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2156694
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Tested-by: Yen-lin Lai <yenlinlai@google.com>
    Commit-Queue: Shuo-Peng Liao <deanliao@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/network/wifi_caps_80211ac.go
@@ -0,0 +1,38 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+
+	"chromiumos/tast/local/network/iw"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WifiCaps80211ac,
+		Desc:         "Verifies DUT supports required 802.11ac capabilities",
+		Contacts:     []string{"yenlinlai@google.com", "chromeos-kernel-wifi@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"wifi"},
+		HardwareDeps: hwdep.D(hwdep.Wifi80211ac()),
+	})
+}
+
+func WifiCaps80211ac(ctx context.Context, s *testing.State) {
+	iwr := iw.NewRunner()
+	res, err := iwr.ListPhys(ctx)
+	if err != nil {
+		s.Fatal("ListPhys failed: ", err)
+	}
+	if !res[0].SupportVHT {
+		s.Error("Device doesn't support VHT")
+	}
+	if !res[0].SupportVHT80SGI {
+		s.Error("Device doesn't support VHT80 short guard interval")
+	}
+}
