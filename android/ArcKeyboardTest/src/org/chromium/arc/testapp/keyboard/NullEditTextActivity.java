530e577915b723aa56561cc3068589efed69efca
Yuichiro Hanada <yhanada@google.com>
2020-04-22 15:20:14 +0900

    Add a test case for editing on TYPE_NULL text field.
    In crrev.com/c/2157442 and ag/11176104, we are going to support typing
    with the Chrome OS IME's virtual keyboard on an EditText with
    InputType.TYPE_NULL.
    On such an EditText, the virtual keyboard should send only key events
    instead of normal editing commands according to the public Android API
    document[1].
    The new added test checks that behavior.
    
    [1]:
    https://developer.android.com/reference/android/text/InputType#TYPE_NULL
    
    BUG=b:134992623
    TEST=Ran arc.ChromeVirtualKeyboard
    
    Change-Id: I2987ee89d7ad9639f31a9707ca6c727fd88ce7f5

--- android/ArcKeyboardTest/src/org/chromium/arc/testapp/keyboard/NullEditTextActivity.java
+++ android/ArcKeyboardTest/src/org/chromium/arc/testapp/keyboard/NullEditTextActivity.java
@@ -7,16 +7,41 @@
 package org.chromium.arc.testapp.keyboard;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.InputType;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.view.KeyEvent;
+import android.view.inputmethod.InputMethodManager;
 
 public class NullEditTextActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        EditText text = (EditText) findViewById(R.id.text);
+
+        final EditText text = (EditText) findViewById(R.id.text);
         text.setInputType(InputType.TYPE_NULL);
+        text.setOnClickListener(v -> {
+            final InputMethodManager imm =
+                    (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm == null) {
+                return;
+            }
+            imm.showSoftInput(v, 0);
+            return;
+        });
+
+        final TextView lastKeyDown = (TextView) findViewById(R.id.last_key_down);
+        final TextView lastKeyUp = (TextView) findViewById(R.id.last_key_up);
+        text.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                lastKeyDown.setText("key down: keyCode=" + keyCode);
+            } else if (event.getAction() == KeyEvent.ACTION_UP) {
+                lastKeyUp.setText("key up: keyCode=" + keyCode);
+            }
+            return true;
+        });
     }
 }
