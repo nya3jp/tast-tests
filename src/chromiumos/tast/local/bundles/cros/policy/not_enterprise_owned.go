e3163577d3e46b3a58accbe030702450b1c8b097
Victor-Gabriel Savu <vsavu@google.com>
2020-04-23 12:00:39 +0200

    tast-tests: check if device is enterprise owned in ready.go
    Check if device is enterprise owned in ready.go and log if that
    is the case. Local tests assume that the device is not enrolled.
    Add a local test to fail if the DUT is enterprise owned to help
    find issues on DUTs.
    
    BUG=chromium:1070672
    TEST=tast run <ip> wilco.APIGetConfigurationData to enroll/unenroll
    TEST=tast run <ip> wiloc.APIGetConfigurationDataEmpty as local test
    TEST=tast run <ip> policy.NotEnterpriseOwned policy.ReadyErrorLog
    
    Change-Id: I0d6aae037983c5a75008b238f8147bed95a0ebe4

---
+++ src/chromiumos/tast/local/bundles/cros/policy/not_enterprise_owned.go
@@ -0,0 +1,43 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"chromiumos/tast/local/ready"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: NotEnterpriseOwned,
+		Desc: "Check that the DUT is not enterprise owned",
+		Contacts: []string{
+			"vsavu@google.com", // Test author
+			"nya@chromium.org",
+			"chromeos-commercial-stability@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func NotEnterpriseOwned(ctx context.Context, s *testing.State) {
+	contents, err := ioutil.ReadFile(ready.EnterpriseOwnedLogLocation)
+	if os.IsNotExist(err) {
+		return // Test passed as error log is missing.
+	}
+
+	if err != nil {
+		s.Fatalf("Failed to read error log %q: %v", ready.ClearPoliciesLogLocation, err)
+	}
+
+	for _, line := range strings.Split(string(contents), "\n") {
+		s.Errorf("Found error log for ready.go: %s", line)
+	}
+}
