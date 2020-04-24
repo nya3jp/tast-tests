16f966f8cb718de0e97ad7fbadb01b999018acdd
Matthias Springer <springerm@google.com>
2020-04-24 16:19:39 +0900

    tast-tests: Fixed-size power metric sample interval
    Power metrics should be sampled with a fixed size sample interval, so
    that metrics can be displayed correctly in the power dashboard.
    
    BUG=b:153027738
    TEST=e.g.: tast run DUT arc.PowerIdlePerf.vm
    
    Change-Id: I2fbd22b86bdf124d7fd30177ee7085683562126c

---
+++ src/chromiumos/tast/local/arc/logcat.go
@@ -0,0 +1,60 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"bufio"
+	"context"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/shutil"
+)
+
+// WaitForExpInLogcat waits for a regexp to appear in the logcat with timeout.
+func WaitForExpInLogcat(ctx context.Context, a *ARC, exp *regexp.Regexp, timeout time.Duration) error {
+	var cancel context.CancelFunc
+	if timeout > 0*time.Second {
+		ctx, cancel = context.WithTimeout(ctx, timeout)
+		defer cancel()
+	}
+
+	cmd := a.Command(ctx, "logcat")
+
+	pipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to open StdoutPipe")
+	}
+	defer pipe.Close()
+
+	if err := cmd.Start(); err != nil {
+		return errors.Wrapf(err, "failed to start %s", shutil.EscapeSlice(cmd.Args))
+	}
+	defer func() {
+		cmd.Kill()
+		cmd.Wait()
+	}()
+
+	done := make(chan struct{})
+
+	go func() {
+		scanner := bufio.NewScanner(pipe)
+		for scanner.Scan() {
+			l := scanner.Text()
+			if exp.MatchString(l) {
+				close(done)
+				return
+			}
+		}
+	}()
+
+	select {
+	case <-done:
+		return nil
+	case <-ctx.Done():
+		return errors.Wrapf(ctx.Err(), "cannot match regexp %s in logcat before timeout(%s)", exp, timeout)
+	}
+}
