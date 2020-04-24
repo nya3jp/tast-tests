d545f6b4dea39bb730a8b10941e112a032ea4394
Greg Edelston <gredelston@google.com>
2020-04-24 11:10:47 -0600

    firmware/config: Inherit parent and default cfgs
    Each platform should overwrite the values of its parent config,
    recursively, until no parent is found. At that time, it should overwrite
    the values of the default config.
    
    Not covered in this CL: model-level configs.
    
    BUG=b:154500336
    TEST=firmware.Config
    TEST=config_test.go
    
    Change-Id: I3ac3cc90e4f2fd3eb49ca4b053536f0cfa56b160

--- src/chromiumos/tast/remote/firmware/config_test.go
+++ src/chromiumos/tast/remote/firmware/config_test.go
@@ -16,16 +16,39 @@ import (
 	"chromiumos/tast/testutil"
 )
 
+// In order to tell more easily where each value is obtained from, each mock config sets all integer fields to the same value.
+const (
+	defaultValue       = 1
+	myGrandparentValue = 2
+	myParentValue      = 3
+	myPlatformValue    = 4
+)
+
 var mockData = map[string][]byte{
-	defaultName: []byte(`{
-		"platform": "DEFAULTS",
+	defaultName: []byte(fmt.Sprintf(`{
+		"platform": null,
 		"parent": null,
-		"firmware_screen": 1
-	}`),
-	"myplatform": []byte(`{
+		"firmware_screen": %d,
+		"delay_reboot_to_ping": %d,
+		"confirm_screen": %d,
+		"usb_plug": %d
+	}`, defaultValue, defaultValue, defaultValue, defaultValue)),
+	"myplatform": []byte(fmt.Sprintf(`{
 		"platform": "myplatform",
-		"firmware_screen": 2
-	}`),
+		"parent": "myparent",
+		"firmware_screen": %d
+	}`, myPlatformValue)),
+	"myparent": []byte(fmt.Sprintf(`{
+		"platform": "myparent",
+		"parent": "mygrandparent",
+		"confirm_screen": %d,
+		"firmware_screen": %d
+	}`, myParentValue, myParentValue)),
+	"mygrandparent": []byte(fmt.Sprintf(`{
+		"platform": "mygrandparent",
+		"usb_plug": %d,
+		"confirm_screen": %d
+	}`, myGrandparentValue, myGrandparentValue)),
 }
 
 // setupMockData creates a temporary directory containing .json files for each platform in mockData.
@@ -56,6 +79,7 @@ func TestLoadBytes(t *testing.T) {
 	}
 }
 
+// TestNewConfig verifies that we can create a new Config object with proper inheritance.
 func TestNewConfig(t *testing.T) {
 	mockConfigDir, err := setupMockData(t)
 	defer os.RemoveAll(mockConfigDir)
@@ -69,7 +93,44 @@ func TestNewConfig(t *testing.T) {
 	if cfg.Platform != "myplatform" {
 		t.Errorf(`unexpected Platform value; got %q, want "myplatform"`, cfg.Platform)
 	}
-	if cfg.FirmwareScreen != 2 {
-		t.Errorf("unexpected FirmwareScreen value; got %d, want 2", cfg.FirmwareScreen)
+	// Platform and parents do not set values; inherit defaults.
+	if cfg.DelayRebootToPing != defaultValue {
+		t.Errorf("unexpected DelayRebootToPing value; got %d, want %d", cfg.DelayRebootToPing, defaultValue)
+	}
+	// Platform overwrites defaults (even though parent also sets the value)
+	if cfg.FirmwareScreen != myPlatformValue {
+		t.Errorf("unexpected FirmwareScreen value; got %d, want %d", cfg.FirmwareScreen, myPlatformValue)
+	}
+	// Platform inherits from parent (even though grandparent also sets the value)
+	if cfg.ConfirmScreen != myParentValue {
+		t.Errorf("unexpected ConfirmScreen value; got %d, want %d", cfg.ConfirmScreen, myParentValue)
+	}
+	// Platform inherits from grandparent
+	if cfg.USBPlug != myGrandparentValue {
+		t.Errorf("unexpected USBPlug value; got %d, want %d", cfg.USBPlug, myGrandparentValue)
+	}
+}
+
+// TestNewConfigNoParent verifies that a new config with no parent value has proper inheritance.
+func TestNewConfigNoParent(t *testing.T) {
+	mockConfigDir, err := setupMockData(t)
+	defer os.RemoveAll(mockConfigDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cfg, err := NewConfig(mockConfigDir, "mygrandparent")
+	if err != nil {
+		t.Fatal("creating config for mygrandparent:", err)
+	}
+	if cfg.Platform != "mygrandparent" {
+		t.Errorf(`unexpected Platform value; got %q, want "mygrandparent"`, cfg.Platform)
+	}
+	// mygrandparent has a custom value for USBPlug, overwriting defaults.
+	if cfg.USBPlug != myGrandparentValue {
+		t.Errorf("unexpected USBPlug value; got %d, want %d", cfg.USBPlug, myGrandparentValue)
+	}
+	// mygrandparent does not set a value for FirmwareScreen, so should inherit defaults.
+	if cfg.FirmwareScreen != defaultValue {
+		t.Errorf("unexpected FirmwareScreen value; got %d, want %d", cfg.FirmwareScreen, defaultValue)
 	}
 }
