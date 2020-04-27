38073a2865bb99ac6144cade6e34f24cc035c0d1
John L Chen <zuan@chromium.org>
2020-04-28 01:38:46 +0800

    tast-tests: hwsec: Add chaps helper function that create all keys that should be covered
    Currently many tests needs to test different aspects of the same set of keys,
    so there are duplicate code that create the same set of keys in each of those
    tests.
    
    This change add the code that creates all those keys with a single function
    call.
    
    BUG=b:155088532
    TEST=tast run $dut hwsec.Chaps* with CLs later down the chain.
    
    Change-Id: I25302171171e198a634ad06a74466ff5f604d15f

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/util/chaps.go
@@ -0,0 +1,81 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package util
+
+import (
+	"context"
+	"fmt"
+	"path"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/common/pkcs11"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// createKeysForTestingByUser create all the possible keys that we should test that belongs to the given username (reside in the slot that is associated with the user).
+// Specify empty string for username to specify system token slot.
+func createKeysForTestingByUser(ctx context.Context, username string, pkcs11Util *pkcs11.Chaps, scratchpadPath string) (keys []*pkcs11.KeyInfo, retErr error) {
+	defer func() {
+		if retErr != nil {
+			// Function failed, we need to cleanup all created keys.
+			for _, k := range keys {
+				if err := pkcs11Util.DestroyKey(ctx, k); err != nil {
+					testing.ContextLogf(ctx, "Failed to destroy key %s during cleanup when createKeysForTestingByUser failed: %q", pkcs11Util.DumpKeyInfo(k), err)
+				}
+			}
+			keys = []*pkcs11.KeyInfo{}
+		}
+	}()
+
+	// Create the software-generated, then imported key.
+	importedKey, err := pkcs11Util.CreateRSASoftwareKey(ctx, scratchpadPath, username, "testkey1", "111111", false, true)
+	if err != nil {
+		return keys, errors.Wrap(err, "failed to create software key")
+	}
+	keys = append(keys, importedKey)
+
+	// Create the software-generated, then imported as software-backed key.
+	softwareKey, err := pkcs11Util.CreateRSASoftwareKey(ctx, scratchpadPath, username, "testkey2", "222222", true, true)
+	if err != nil {
+		return keys, errors.Wrap(err, "failed to create software key")
+	}
+	keys = append(keys, softwareKey)
+
+	// Create the TPM generated key.
+	generatedKey, err := pkcs11Util.CreateRsaGeneratedKey(ctx, scratchpadPath, username, "testkey3", "333333")
+	if err != nil {
+		return keys, errors.Wrap(err, "failed to create generated key")
+	}
+	keys = append(keys, generatedKey)
+
+	// Create a copy of software key for every key.
+	for i, k := range keys {
+		// Note: C0B1%02X format is just to avoid collision with other key ID. C0B1 => closest "hexspeak" for copy.
+		copiedKey, _, err := pkcs11Util.CreateKeyCopy(ctx, k, fmt.Sprintf("C0B1%02X", i), map[string]string{})
+		if err != nil {
+			return keys, errors.Wrap(err, "failed to copy key")
+		}
+		keys = append(keys, copiedKey)
+	}
+
+	return keys, nil
+}
+
+// CreateKeysForTesting will create the set of keys that we want to cover in our tests.
+func CreateKeysForTesting(ctx context.Context, r hwsec.CmdRunner, pkcs11Util *pkcs11.Chaps, cryptohomeUtil *hwsec.UtilityCryptohomeBinary, scratchpadPath string) (keys []*pkcs11.KeyInfo, retErr error) {
+	// Create the system keys.
+	systemScratchpadPath := path.Join(scratchpadPath, "system")
+	if _, err := r.Run(ctx, "mkdir", "-p", systemScratchpadPath); err != nil {
+		return keys, errors.Wrap(err, "failed to create scratchpad for system keys")
+	}
+	retKeys, err := createKeysForTestingByUser(ctx, "", pkcs11Util, scratchpadPath)
+	if err != nil {
+		return keys, errors.Wrap(err, "failed to create system key")
+	}
+	keys = append(keys, retKeys...)
+
+	return keys, nil
+}
