2f207d25c575b51f2f4b49168b54b51a872e837e
Kimiyuki Onaka <kimiyuki@google.com>
2020-04-28 19:50:24 +0900

    tast-tests: Port cheets_AppfuseTest_P to Tast
    Ports `cheets_AppfuseTest_P` autotest to Tast as `arc.Appfuse`.
    
    BUG=b:155135138
    TEST=run following commands in cros_sdk
      $ cros_workon --board=$BOARD start tast-local-tests-crosint
      $ cros_run_unit_tests --board $BOARD --packages tast-local-tests-crosint
      $ tast run $DUT arc.Appfuse
    
    Change-Id: I67340e6b12dc9570654ffa0b747636b91af992dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2169595
    Tested-by: Kimiyuki Onaka <kimiyuki@google.com>
    Commit-Queue: Kimiyuki Onaka <kimiyuki@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arc/appfuse.go
@@ -0,0 +1,58 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"syscall"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Appfuse,
+		Desc: "Make sure arc-appfuse-provider works",
+		Contacts: []string{
+			"hashimoto@google.com", // original author.
+			"arc-storage@google.com",
+			"kimiyuki@google.org", // Tast port.
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"android_p", "chrome"},
+		Pre:          arc.Booted(),
+	})
+}
+func Appfuse(ctx context.Context, s *testing.State) {
+	const (
+		dbusName      = "org.chromium.ArcAppfuseProvider"
+		dbusPath      = "/org/chromium/ArcAppfuseProvider"
+		dbusInterface = "org.chromium.ArcAppfuseProvider"
+	)
+
+	// We need to run the DBus methods as chronos because they are allowed only for chronos. See "arc/appfuse/org.chromium.ArcAppfuseProvider.conf".
+	conn, obj, err := dbusutil.ConnectPrivateWithAuth(ctx, sysutil.ChronosUID, dbusName, dbus.ObjectPath(dbusPath))
+	if err != nil {
+		s.Fatal("Failed to connect to ", dbusName, ": ", err)
+	}
+	defer conn.Close()
+
+	uid := uint32(12345)
+	mountID := int32(678)
+	var fd dbus.UnixFD
+	if err := obj.CallWithContext(ctx, dbusInterface+".Mount", 0, uid, mountID).Store(&fd); err != nil {
+		s.Fatal("Failed to mount Appfuse: ", err)
+	}
+	defer syscall.Close(int(fd))
+
+	if err := obj.CallWithContext(ctx, dbusInterface+".Unmount", 0, uid, mountID).Err; err != nil {
+		s.Fatal("Failed to mount Appfuse: ", err)
+	}
+}
