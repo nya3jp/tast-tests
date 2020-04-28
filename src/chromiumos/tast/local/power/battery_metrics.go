f72f0ede9a5e6e6bf3dcc5cd50eb4f7fa62c105a
Hikaru Nishida <hikalium@chromium.org>
2020-04-28 18:09:20 +0900

    tast-tests: Add 'system' power metric based on sysfs
    This CL adds 'system' metric to replace powerMetric which uses ectool.
    powerMetric and other metrics from ectool will be eventually removed and
    replaced by metrics which use sysfs.
    
    BUG=b:153027513
    TEST=tast run <host> arc.PowerIdlePerf reports 'system' power metric
    on DUT with batteries
    TEST=tast run <host> arc.PowerIdlePerf doesn't report 'system' power
    metric on DUT without batteries
    
    Change-Id: I88dff04851b7af4d0232cca066d49f9531b7b0cd

--- src/chromiumos/tast/local/power/battery_metrics.go
+++ src/chromiumos/tast/local/power/battery_metrics.go
@@ -6,6 +6,9 @@ package power
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
+	"path"
 	"regexp"
 	"strconv"
 	"strings"
@@ -13,6 +16,7 @@ import (
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/perf"
 	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
 )
 
 // BatteryState holds all interesting battery metrics.
@@ -122,19 +126,100 @@ func NewBatteryState(ctx context.Context) (*BatteryState, error) {
 	}, nil
 }
 
+// GetSystemPower returns system power consumption in Watt.
+// If there are no batteries support reporting voltage_now and current_now,
+// the return value is 0., otherwise sum of power consumption of each battery.
+func GetSystemPower(devPaths *[]string) (float64, error) {
+	readFloat64 := func(devPath, name string) (float64, error) {
+		strBytes, err := ioutil.ReadFile(path.Join(devPath, name))
+		if err != nil {
+			return 0., err
+		}
+		return strconv.ParseFloat(strings.TrimSuffix(string(strBytes), "\n"), 64)
+	}
+	systemPower := 0.
+	for _, devPath := range *devPaths {
+		supplyVoltage, err := readFloat64(devPath, "voltage_now")
+		if err != nil {
+			return 0., errors.Wrap(err, "failed to read voltage_now")
+		}
+		supplyCurrent, err := readFloat64(devPath, "current_now")
+		if err != nil {
+			return 0., errors.Wrap(err, "failed to read current_now")
+		}
+		// voltage_now and current_now reports their value in micro unit
+		// so adjust this to match with Watt.
+		systemPower += supplyVoltage * supplyCurrent * 1e-12
+	}
+	return systemPower, nil
+}
+
 // BatteryMetrics hold the results of once call to ectool and enough
 // information to compute battery drain over time.
 type BatteryMetrics struct {
-	voltageMetric perf.Metric
-	currentMetric perf.Metric
-	powerMetric   perf.Metric
-	energyMetric  perf.Metric
-	lowSys        float64
-	lowMargin     float64
-	prevState     *BatteryState
+	systemPowerMetric perf.Metric
+	voltageMetric     perf.Metric
+	currentMetric     perf.Metric
+	powerMetric       perf.Metric
+	energyMetric      perf.Metric
+	lowSys            float64
+	lowMargin         float64
+	prevState         *BatteryState
+	sysfsBatteryPaths []string
 }
 
 var _ perf.TimelineDatasource = &BatteryMetrics{}
+
+// ListSysfsBatteryPaths lists paths of batteries which supply power to the system
+func ListSysfsBatteryPaths(ctx context.Context) []string {
+	// TODO(hikarun): Remove ContextLogf()s after checking this function works on all platforms
+	const sysfsPowerSupplyPath = "/sys/class/power_supply"
+	testing.ContextLog(ctx, "Listing batteries in ", sysfsPowerSupplyPath)
+	files, err := ioutil.ReadDir(sysfsPowerSupplyPath)
+	if err != nil {
+		testing.ContextLog(ctx, "Failed to read sysfs dir: ", err)
+		return nil
+	}
+	readLine := func(devPath, name string) (string, error) {
+		strBytes, err := ioutil.ReadFile(path.Join(devPath, name))
+		if err != nil {
+			return "", err
+		}
+		return strings.TrimSuffix(string(strBytes), "\n"), nil
+	}
+	hasSysfsAttribute := func(devPath, name string) bool {
+		_, err := os.Stat(path.Join(devPath, name))
+		return err != nil
+	}
+	var sysfsBatteryPaths []string
+	for _, file := range files {
+		devPath := path.Join(sysfsPowerSupplyPath, file.Name())
+		supplyType, err := readLine(devPath, "type")
+		if err != nil || supplyType != "Battery" {
+			testing.ContextLogf(ctx, "%v is not a Battery. err=%v", devPath, err)
+			continue
+		}
+		testing.ContextLogf(ctx, "%v is a Battery", devPath)
+		supplyScope, err := readLine(devPath, "scope")
+		if err != nil && !os.IsNotExist(err) {
+			// Ignore NotExist error since /sys/class/power_supply/*/scope may not exist
+			testing.ContextLogf(ctx, "Failed to scope of %v: %v", devPath, err)
+			continue
+		}
+		if supplyScope == "Device" {
+			// Ignore batteries for peripheral devices.
+			testing.ContextLogf(ctx, "%v is a Battery with Device scope", devPath)
+			continue
+		}
+		if !hasSysfsAttribute(sysfsPowerSupplyPath, "voltage_now") ||
+			!hasSysfsAttribute(sysfsPowerSupplyPath, "current_now") {
+			testing.ContextLogf(ctx, "%v lacks voltage_now or current_now", devPath)
+			continue
+		}
+		sysfsBatteryPaths = append(sysfsBatteryPaths, devPath)
+	}
+	return sysfsBatteryPaths
+}
 
 // NewBatteryMetrics creates a struct to capture battery metrics with the
 // ectool command.
@@ -149,6 +234,17 @@ func NewBatteryMetrics(lowBatteryMargin float64) *BatteryMetrics {
 // Setup reads the low battery shutdown percent that that we can error out a
 // test if the battery is ever too low.
 func (b *BatteryMetrics) Setup(ctx context.Context, prefix string) error {
+	b.sysfsBatteryPaths = ListSysfsBatteryPaths(ctx)
+	if len(b.sysfsBatteryPaths) != 0 {
+		testing.ContextLogf(ctx, "BatteryMetrics uses %v batteries:", len(b.sysfsBatteryPaths))
+		for _, path := range b.sysfsBatteryPaths {
+			testing.ContextLog(ctx, path)
+		}
+		b.systemPowerMetric = perf.Metric{Name: prefix + "system", Unit: "W", Direction: perf.SmallerIsBetter, Multiple: true}
+	} else {
+		testing.ContextLog(ctx, "Not reporting system metric since no batteries found")
+	}
+
 	low, err := LowBatteryShutdownPercent(ctx)
 	if err != nil {
 		return err
@@ -175,6 +271,13 @@ func (b *BatteryMetrics) Start(ctx context.Context) error {
 // Snapshot takes a snapshot of battery metrics. It also checks that the
 // battery is not too low for the test to continue.
 func (b *BatteryMetrics) Snapshot(ctx context.Context, values *perf.Values) error {
+	if len(b.sysfsBatteryPaths) != 0 {
+		power, err := GetSystemPower(&b.sysfsBatteryPaths)
+		if err != nil {
+			return err
+		}
+		values.Append(b.systemPowerMetric, power)
+	}
 	state, err := NewBatteryState(ctx)
 	if err != nil {
 		return err
