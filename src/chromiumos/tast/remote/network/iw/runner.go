13db26da145564721f27760c607269c98cbc67d4
Arowa Suliman <arowa@google.com>
2020-04-28 23:03:41 -0700

    tast-test: Add local Ping runner
    Factored the remote ping runner
    (src/chromiumos/tast/remote/network/ping/runner.go) to remote, common,
    and local library.
    
    Also, added the option User to the Ping package to be able to run the
    ping command as a different user, such as:
    su chronos -c 'ping -c 3 -i 0.500000 192.168.1.99'
    
    BUG=b:152080743
    TEST=run tast network.NetworkVPNConnect.l2tp_ipsec_psk /
    wifi.SimpleConnect.80211a
    
    Change-Id: Idcadf86ae1e1f2b0873880043b26a01856ceef4b

--- src/chromiumos/tast/remote/network/iw/runner.go
+++ src/chromiumos/tast/remote/network/iw/runner.go
@@ -6,42 +6,15 @@
 package iw
 
 import (
-	"context"
-
 	"chromiumos/tast/common/network/iw"
+	"chromiumos/tast/remote/network/cmd"
 	"chromiumos/tast/remote/network/commander"
-	"chromiumos/tast/shutil"
-	"chromiumos/tast/testing"
 )
-
-// remoteCmdRunner implements iw.CmdRunner interface.
-type remoteCmdRunner struct {
-	host commander.Commander
-}
-
-var _ iw.CmdRunner = (*remoteCmdRunner)(nil)
-
-func (r *remoteCmdRunner) Run(ctx context.Context, cmd string, args ...string) error {
-	_, err := r.Output(ctx, cmd, args...)
-	return err
-}
-
-func (r *remoteCmdRunner) Output(ctx context.Context, cmd string, args ...string) ([]byte, error) {
-	out, err := r.host.Command(cmd, args...).Output(ctx)
-	// NB: the 'local' variant uses DumpLogOnError, which is not provided by Commander.
-	if err != nil {
-		testing.ContextLogf(ctx, "Command: %s %s", shutil.Escape(cmd), shutil.EscapeSlice(args))
-		testing.ContextLog(ctx, "Output:\n", string(out)) // NOLINT
-	}
-	return out, err
-}
 
 // Runner is an alias for common iw Runner but only for remote execution.
 type Runner = iw.Runner
 
 // NewRunner creates a iw runner for remote execution.
 func NewRunner(host commander.Commander) *Runner {
-	return iw.NewRunner(&remoteCmdRunner{
-		host: host,
-	})
+	return iw.NewRunner(&cmd.RemoteCmdRunner{Host: host})
 }
