aca1924e9e78f4544a6fbc3a75a089353c8bddec
Xibin Liu <xliu@cienet.com>
2020-04-29 13:53:40 -0700

    tast-tests: mtbf 068 - 802.11ac ( Wave 2 with Mu-MiMo) support
    Objective:
    To verify device can connect to 802.11ac (Wave 2 with Mu-MIMO) router on 5ghz channel.
    Precondition:
    An 802.11ac (Wave 2 with Mu-MIMO) router.
    Procedure:
    1. Connect to the router.
    Verification:
    1.1 Verify connection is successful.
    1.2 Verify WiFi network works by browsing the internet.
    
    BUG=None
    TEST=tast run -buildbundle=mtbf -buildworkspace=<your workspace> -var=userID=<login id> -var=userPasswd=<login password> <DUT> camera.MTBF054*
    
    Change-Id: Ieab14434e624cefd4e615af9e52f493828783b09

---
+++ src/chromiumos/tast/local/bundles/mtbf/wifi/mtbf068_wifi_connect.go
@@ -0,0 +1,60 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/mtbf/wifi/common"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/mtbf/wifi"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/timing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MTBF068WifiConnect,
+		Desc:         "MTBF068 to verify device can connect to 802.11ac (Wave2) router on 5ghz channel",
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoginReuse(),
+		Attr:         []string{"group:mainline"},
+		Contacts:     []string{"xliu@cienet.com"},
+		Vars: []string{
+			"wifi.802.11ac.wave2.ssid",
+			"wifi.802.11ac.wave2.pwd",
+			"dut.id",
+			"detach.status.server",
+			"allion.api.server",
+			"allion.deviceId"},
+	})
+}
+
+// MTBF068WifiConnect testing the test case TC61
+func MTBF068WifiConnect(ctx context.Context, s *testing.State) {
+	ctx, st := timing.Start(ctx, "mtbf068_test_wifi_connection_802.11ac_wave1")
+	caseName := "wifi.MTBF068WifiConnect"
+	dutID := common.GetVar(ctx, s, "dut.id")
+	detachStatusSvr := common.GetVar(ctx, s, "detach.status.server")
+	allionServerURL := common.GetVar(ctx, s, "allion.api.server")
+	deviceID := common.GetVar(ctx, s, "allion.deviceId")
+	wifiSsid := common.GetVar(ctx, s, "wifi.802.11ac.wave2.ssid")
+	wifiPwd := common.GetVar(ctx, s, "wifi.802.11ac.wave2.pwd")
+	common.InformStatusServlet(ctx, s, detachStatusSvr, "start", dutID, caseName)
+	defer common.InformStatusServlet(ctx, s, detachStatusSvr, "end", dutID, caseName)
+	cr := s.PreValue().(*chrome.Chrome)
+	defer st.End()
+	wifiConn, mtbferr := wifi.NewConn(ctx, cr, true, wifiSsid, wifiPwd, allionServerURL, deviceID)
+
+	if mtbferr != nil {
+		s.Fatal(mtbferr)
+	}
+
+	defer wifiConn.Close(true)
+
+	if mtbferr := wifiConn.TestConnected(); mtbferr != nil {
+		s.Fatal(mtbferr)
+	}
+}
