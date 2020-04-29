702b720c69fe6fef4837b9c1dbc9c73fc262357e
alexanderhartl <alexanderhartl@google.com>
2020-04-29 08:35:51 +0000

    tast-tests: add WebAppInstallForceList
    Add the policy.WebAppInstallForceList tast test.
    
    BUG=chromium:1067554
    TEST=tast run IP policy.WebAppInstallForceList
    
    Change-Id: I3a3cb6d21b86db67479e0d929e50b2cbeddfc1dd

---
+++ src/chromiumos/tast/local/bundles/cros/policy/web_app_install_force_list.go
@@ -0,0 +1,84 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/policy/pre"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui/launcher"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: WebAppInstallForceList,
+		Desc: "Behavior of WebAppInstallForceList policy",
+		Contacts: []string{
+			"alexanderhartl@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+		Data:         []string{"web_app_install_force_list_index.html", "web_app_install_force_list_manifest.json", "web_app_install_force_list_service-worker.js", "web_app_install_force_list_icon-192x192.png", "web_app_install_force_list_icon-512x512.png"},
+	})
+}
+
+func WebAppInstallForceList(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	name := "Test PWA"
+	value := &policy.WebAppInstallForceList{
+		Val: []*policy.WebAppInstallForceListValue{
+			{
+				Url:                    fmt.Sprintf("%s/web_app_install_force_list_index.html", server.URL),
+				DefaultLaunchContainer: "window",
+			},
+		},
+	}
+
+	// Update policies.
+	if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{value}); err != nil {
+		s.Fatal("Failed to update policies: ", err)
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Cannot get TestConn: ", err)
+	}
+
+	// Wait until the PWA is installed.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := launcher.SearchAndLaunch(ctx, tconn, name); err != nil {
+			return testing.PollBreak(errors.Wrapf(err, "couldn't launch %s", name))
+		}
+
+		windows, err := ash.GetAllWindows(ctx, tconn)
+		if err != nil {
+			return testing.PollBreak(errors.Wrap(err, "failed to get windows"))
+		}
+
+		for _, window := range windows {
+			if window.Title == name {
+				return nil
+			}
+		}
+		return errors.New("couldn't find a window with the PWA")
+	}, nil); err != nil {
+		s.Error("PWA wasn't installed: ", err)
+	}
+}
