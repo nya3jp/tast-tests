2cfc247dc0271bcc1952c77913a42b1e217f4682
Po-Hsien Wang <pwang@chromium.org>
2020-04-29 17:31:00 -0700

    graphics: trace: Change blacklist to whitelist
    BUG=chromium:1067067, b:155341147
    TEST=tast run
    
    Change-Id: Ic9354c387366fa6c83100f93fad2b0f9dc7b1f0e

--- src/chromiumos/tast/local/graphics/trace/dep.go
+++ src/chromiumos/tast/local/graphics/trace/dep.go
@@ -5,22 +5,14 @@
 package trace
 
 import (
-	"chromiumos/tast/local/crostini"
 	"chromiumos/tast/testing/hwdep"
 )
 
-// modelBlackList is a list of model that are too flaky for the CQ.
-// This is in addition to crostini.modelBlacklist.
-var modelBlackList = append(crostini.ModelBlacklist,
-	// Platform jecht
-	"guado",
-	"jecht",
-	"rikku",
-	"tidus",
-)
+// modelWhitelist is a list of model that is targeted to be able to reliably work in the lab for testing.
+var modelWhitelist = []string{"atlas", "eve", "drallion"}
 
 // HwDepsStable is hardwareDeps condition that stable to run trace tests.
-var HwDepsStable = hwdep.D(hwdep.SkipOnModel(modelBlackList...))
+var HwDepsStable = hwdep.D(hwdep.Model(modelWhitelist...))
 
 // HwDepsUnstable is hardwareDeps condition that unstable to run trace tests.
-var HwDepsUnstable = hwdep.D(hwdep.Model(modelBlackList...))
+var HwDepsUnstable = hwdep.D(hwdep.SkipOnModel(modelWhitelist...))
