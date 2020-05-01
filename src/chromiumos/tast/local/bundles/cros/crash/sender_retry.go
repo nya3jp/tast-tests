35f25896654656b2e350a4543454c17b78a1f25c
Miriam Zimmerman <mutexlox@google.com>
2020-05-01 12:12:09 -0700

    Promote crash.SenderRetry to critical.
    https://tastboard.corp.google.com/test/crash.SenderRetry shows all
    passing runs.
    
    BUG=None
    TEST=CQ
    
    Change-Id: I2e74e079de4fee36d0502c2f81ecbcab6fa8b726

--- src/chromiumos/tast/local/bundles/cros/crash/sender_retry.go
+++ src/chromiumos/tast/local/bundles/cros/crash/sender_retry.go
@@ -24,7 +24,7 @@ func init() {
 			"cros-telemetry@google.com",
 			"nya@chromium.org", // ported to Tast
 		},
-		Attr: []string{"group:mainline", "informational"},
+		Attr: []string{"group:mainline"},
 	})
 }
 
