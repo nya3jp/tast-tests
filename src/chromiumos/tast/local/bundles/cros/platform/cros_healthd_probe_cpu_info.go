3b89d50f42d26a65a4715b9ba0e9eb0b0651e6f4
Paul Moy <pmoy@chromium.org>
2020-05-01 18:10:02 -0600

    tast-tests: update CrosHealthdProbeCPUInfo
    The structure of the CPU data returned by cros_healthd has
    changed, so the tast test needs to be updated to check for the
    new values.
    
    BUG=chromium:1067660
    TEST=tast run --build $IP platform.CrosHealthdProbeCPUInfo
    
    Cq-Depend: chromium:2173439
    Change-Id: Ibbb464812aad648e579f950a375cdc8c3d3106aa

--- src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_cpu_info.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_cpu_info.go
@@ -12,6 +12,7 @@ import (
 	"strconv"
 	"strings"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
@@ -31,6 +32,128 @@ func init() {
 	})
 }
 
+func verifyPhysicalCPU(lines []string) error {
+	// Make sure we've received at least nine lines. The first should be the
+	// physical CPU header, followed by one line of keys, one line of values,
+	// and six or more lines of logical CPU data.
+	if len(lines) < 9 {
+		return errors.New("could not find any lines of physical CPU info")
+	}
+
+	// Verify the first line is the correct header.
+	actualHeader := lines[0]
+	expectedHeader := "Physical CPU:"
+	if actualHeader != expectedHeader {
+		return errors.Errorf("incorrect physical CPU header: got %v, want %v", actualHeader, expectedHeader)
+	}
+
+	// Verify the key is correct.
+	want := "model_name"
+	got := lines[1]
+	if want != got {
+		return errors.Errorf("incorrect physical CPU key: got %v; want %v", got, want)
+	}
+
+	// Verify the value is a non-empty string.
+	if lines[2] == "" {
+		return errors.New("empty model_name")
+	}
+
+	// Verify each logical CPU.
+	for start, i := 3, 4; i <= len(lines); i++ {
+		if i == len(lines) || lines[i] == "Logical CPU:" {
+			if err := verifyLogicalCPU(lines[start : i-1]); err != nil {
+				return errors.Wrap(err, "failed to verify logical CPU")
+			}
+			start = i
+		}
+	}
+
+	return nil
+}
+
+func verifyLogicalCPU(lines []string) error {
+	// Make sure we've received at least six lines. The first should be the
+	// logical CPU header, followed by one line of keys, one line of values, and
+	// three or more lines of C-state data.
+	if len(lines) < 6 {
+		return errors.New("could not find any lines of logical CPU info")
+	}
+
+	// Verify the first line is the correct header.
+	actualHeader := lines[0]
+	expectedHeader := "Logical CPU:"
+	if actualHeader != expectedHeader {
+		return errors.Errorf("incorrect logical CPU header: got %v, want %v", actualHeader, expectedHeader)
+	}
+
+	// Verify the keys are correct.
+	want := []string{"max_clock_speed_khz", "scaling_max_frequency_khz", "scaling_current_frequency_khz", "idle_time_user_hz"}
+	got := strings.Split(lines[1], ",")
+	if !reflect.DeepEqual(want, got) {
+		return errors.Errorf("incorrect logical CPU keys: got %v; want %v", got, want)
+	}
+
+	// Check for error values.
+	vals := strings.Split(lines[2], ",")
+	if len(vals) != len(want) {
+		return errors.Errorf("wrong number of logical CPU values: got %v, want 4", len(vals))
+	}
+
+	for i, val := range vals {
+		if parsed, err := strconv.Atoi(val); err != nil {
+			return errors.Wrapf(err, "failed to convert %q to integer: %q", want[i], val)
+		} else if parsed < 0 {
+			return errors.Errorf("invalid %q: %v", want[i], parsed)
+		}
+	}
+
+	return verifyCStates(lines[3:])
+}
+
+func verifyCStates(lines []string) error {
+	// Make sure we've received at least three lines. The first should be the
+	// C-state header, followed by one line of keys and one or more lines of
+	// C-states.
+	if len(lines) < 3 {
+		return errors.New("could not find any lines of C-state info")
+	}
+
+	// Verify the first line is the correct header.
+	actualHeader := lines[0]
+	expectedHeader := "C-states:"
+	if actualHeader != expectedHeader {
+		return errors.Errorf("incorrect C-state header: got %v, want %v", actualHeader, expectedHeader)
+	}
+
+	// Verify the keys are correct.
+	want := []string{"name", "time_in_state_since_last_boot_us"}
+	got := strings.Split(lines[1], ",")
+	if !reflect.DeepEqual(want, got) {
+		return errors.Errorf("incorrect C-state keys: got %v; want %v", got, want)
+	}
+
+	// Verify each C-state value.
+	for _, line := range lines[2:] {
+		vals := strings.Split(line, ",")
+		if len(vals) != 2 {
+			return errors.Errorf("wrong number of C-state values: got %v, want 2", len(vals))
+		}
+
+		if vals[0] == "" {
+			return errors.New("empty name")
+		}
+
+		if i, err := strconv.Atoi(vals[1]); err != nil {
+			return errors.Wrapf(err, "failed to convert time_in_state_since_last_boot_us to integer: %q", vals[1])
+		} else if i < 0 {
+			return errors.Errorf("invalid time_in_state_since_last_boot_us: %d", i)
+		}
+	}
+
+	return nil
+}
+
 func CrosHealthdProbeCPUInfo(ctx context.Context, s *testing.State) {
 	if err := upstart.EnsureJobRunning(ctx, "cros_healthd"); err != nil {
 		s.Fatal("Failed to start cros_healthd: ", err)
@@ -45,43 +168,52 @@ func CrosHealthdProbeCPUInfo(ctx context.Context, s *testing.State) {
 		s.Errorf("Failed to write output to %s: %v", filepath.Join(s.OutDir(), "command_output.txt"), err)
 	}
 
-	// Every board should have at least one CPU. The output of the command is a
-	// line of headers followed by a line of CPU info for each CPU. Verify at
-	// least one line of CPU info exists.
-	lines := strings.Split(strings.TrimRight(string(b), "\n"), "\n")
-	if len(lines) < 2 {
+	// Every board should have at least one physical CPU, which contains at
+	// least one logical CPU. That section is at least nine lines long, and is
+	// preceded by one line of keys and one line of values.
+	lines := strings.Split((strings.TrimRight(string(b), "\n")), "\n")
+	if len(lines) < 11 {
 		s.Fatal("Could not find any lines of CPU info")
 	}
 
-	// Verify the headers are correct.
-	want := []string{"model_name", "architecture", "max_clock_speed_khz"}
-	got := strings.Split(lines[0], ",")
-	if !reflect.DeepEqual(want, got) {
-		s.Fatalf("Incorrect headers: got %v; want %v", got, want)
+	for i := range lines {
+		lines[i] = strings.TrimSpace(lines[i])
 	}
 
-	// Verify each line of CPU info contains valid values.
-	for _, line := range lines[1:] {
-		s.Log("Checking line: ", line)
-		vals := strings.Split(line, ",")
-		if len(vals) != 3 {
-			s.Errorf("Wrong number of values: got %v, want 3", len(vals))
-			continue
-		}
+	// Verify the top-level CpuInfo keys are correct.
+	want := []string{"num_total_threads", "architecture"}
+	got := strings.Split(lines[0], ",")
+	if !reflect.DeepEqual(want, got) {
+		s.Fatalf("Incorrect CpuInfo keys: got %v; want %v", got, want)
+	}
 
-		if vals[0] == "" {
-			s.Error("Empty model_name")
-		}
+	// Verify the CpuInfo values are valid.
+	vals := strings.Split(lines[1], ",")
+	if len(vals) != 2 {
+		s.Fatalf("Wrong number of values: got %v, want 2", len(vals))
+	}
 
-		if vals[1] == "" {
-			s.Error("Empty architecture")
-		}
+	if numThreads, err := strconv.Atoi(vals[0]); err != nil {
+		s.Error("Failed to convert num_total_threads to integer: ", err)
+	} else if numThreads <= 0 {
+		s.Error("Invalid num_total_threads")
+	}
 
-		i, err := strconv.Atoi(vals[2])
-		if err != nil {
-			s.Error("Failed to convert max_clock_speed_khz to integer: ", err)
-		} else if i <= 0 {
-			s.Error("Invalid max_clock_speed_khz")
+	if vals[1] == "" {
+		s.Error("Empty architecture")
+	}
+
+	// Verify the output for each physical CPU. Start on the third line, because
+	// that should always be the first line of "Physical CPU:". If it isn't, the
+	// test will fail verifying the first physical CPU, so it's a safe
+	// assumption.
+	for start, i := 2, 3; i <= len(lines); i++ {
+		if i == len(lines) || lines[i] == "Physical CPU:" {
+			err := verifyPhysicalCPU(lines[start : i-1])
+			if err != nil {
+				s.Error("Failed to verify physical CPU: ", err)
+			}
+			start = i
 		}
 	}
 }
