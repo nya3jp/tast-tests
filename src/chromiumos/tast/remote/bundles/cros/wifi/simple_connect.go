0eb21df0b1e889e4b8ef500e6c811eb6b192859a
Arowa Suliman <arowa@google.com>
2020-05-04 22:33:55 -0700

    tast-test: Add network_WiFi_HiddenRemains
    The Tast test is migrated from network_WiFi_HiddenRemains.py, an
    Autotest. Merged this test as an extra step to the
    wifi_simple_connect.go Tast test.
    
    BUG=b:155709047
    TEST=All the SimpleConnect tests pass.
    wifi.SimpleConnect.80211a            [pass]
    wifi.SimpleConnect.80211b            [pass]
    wifi.SimpleConnect.80211g            [pass]
    wifi.SimpleConnect.80211nht20        [pass]
    wifi.SimpleConnect.80211n24ht40      [pass]
    wifi.SimpleConnect.80211n5ht20       [pass]
    wifi.SimpleConnect.80211n5ht40       [pass]
    wifi.SimpleConnect.80211acvht20      [pass]
    wifi.SimpleConnect.80211acvht40      [pass]
    wifi.SimpleConnect.80211acvht80mixed [pass]
    wifi.SimpleConnect.80211acvht80pure  [pass]
    wifi.SimpleConnect.hidden            [pass]
    
    Change-Id: Iaa654e6392dbf704b250eb212548ab5c8f12f0c9

--- src/chromiumos/tast/remote/bundles/cros/wifi/simple_connect.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/simple_connect.go
@@ -175,6 +175,17 @@ func SimpleConnect(ctx context.Context, s *testing.State) {
 		if err := tf.AssertNoDisconnect(ctx, ping); err != nil {
 			s.Fatal("Failed to ping from DUT, err: ", err)
 		}
+
+		s.Log("Checking the status of the SSID in the DUT")
+		isHidden, err := tf.IsSSIDHiddenWifi(ctx, ap.Config().Ssid)
+		if err != nil {
+			s.Fatal("Failed to get the status of the SSID from DUT, err: ", err)
+		}
+
+		if isHidden != ap.Config().Hidden {
+			s.Fatalf("Unexpected hidden SSID status: got %t, want %t ", isHidden, ap.Config().Hidden)
+		}
+
 		// TODO(crbug.com/1034875): Assert no deauth detected from the server side.
 		// TODO(crbug.com/1034875): Maybe some more check on the WiFi capabilities to
 		// verify we really have the settings as expected. (ref: crrev.com/c/1995105)
