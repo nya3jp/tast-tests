60cc4e036a4bd3bd299b411097fe4b5d62f006ab
Kyle Shimabukuro <kyleshima@chromium.org>
2020-05-04 20:11:33 -0700

    Automate test to open Settings app using Assistant
    Automated test to check the Settings app can be opened with the
    Assistant query "open settings". Part of the manual test:
    - 1.1.6 - Assistant Buddy Mode (https://testtracker.googleplex.com/testplans/testcase/detail/1573610?id=21604)
    
    BUG=chromium:1060198
    TEST=tast run localhost:2222 assistant.OpenSettings
    
    Change-Id: I7b5564039a8b151f526036394e89e91c8e6afe49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2182081
    Tested-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Commit-Queue: Kyle Shimabukuro <kyleshima@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/assistant/open_settings.go
@@ -0,0 +1,65 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package assistant
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/assistant"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         OpenSettings,
+		Desc:         "Tests opening the Settings app using an Assistant query",
+		Contacts:     []string{"kyleshima@chromium.org", "bhansknecht@chromium.org", "meilinw@chromium.org"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+// OpenSettings tests that the Settings app can be opened by the Assistant
+func OpenSettings(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	// Starts Assistant service.
+	if err := assistant.Enable(ctx, tconn); err != nil {
+		s.Fatal("Failed to enable Assistant: ", err)
+	}
+	s.Log("Waiting for Assistant to be ready to answer queries")
+	if err := assistant.WaitForServiceReady(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for Libassistant to become ready: ", err)
+	}
+
+	// Run query to open the Settings window.
+	// assistant.SendTextQuery returns an error even when Settings launches successfully,
+	// so check for that here instead of processing the returned error.
+	// todo (crbug/1080366): process the error from assistantSendTextQuery.
+	s.Log("Launching Settings app with Assistant query and waiting for it to open")
+	_, assistErr := assistant.SendTextQuery(ctx, tconn, "open settings")
+	if err := ash.WaitForApp(ctx, tconn, apps.Settings.ID); err != nil {
+		s.Fatalf("Settings app did not appear in the shelf: %v. Last assistant.SendTextQuery error: %v", err, assistErr)
+	}
+
+	// Confirm that the Settings app is open by checking for the "Settings" heading.
+	params := ui.FindParams{
+		Role: ui.RoleTypeHeading,
+		Name: "Settings",
+	}
+	if err := ui.WaitUntilExists(ctx, tconn, params, 10*time.Second); err != nil {
+		s.Fatal("Waiting for Settings app heading failed: ", err)
+	}
+}
