f1937cfd6b40902c9f1199bfe0a9920ecc9510a7
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-05-06 17:23:45 -0700

    Run UIAutomator Test Using Tast For Hearthstone app
    BUG=b:154836131
    TEST=None
    
    Tast Test for Hearthstone app uses unique gmail account
    to login the Chromebook. Also  verifies launch app
    from the playstore, app is logged in and reaches the
    main activity page of the app. Also for Clamshell Mode
    it verifies Clamshell Full Screen, Minimize and Restore,
    Clamshell Resize, Reopen tests and for Touchview Mode it
    verifies Minimize and Restore, Reopen tests.
    Made preconditions for Hearthstone in pre.go
    file.
    
    Change-Id: Ieac53c345ac692b9ea5bf3de09d97abeea896558

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/pre/pre.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/pre/pre.go
@@ -14,6 +14,11 @@ var appcompatGaia = &arc.GaiaVars{
 	PassVar: "arcappcompat.password",
 }
 
+var appcompatHearthstone = &arc.GaiaVars{
+	"arcappcompat.Hearthstone.username",
+	"arcappcompat.Hearthstone.password",
+}
+
 // AppCompatBooted is a precondition similar to arc.Booted(). The only difference from arc.Booted() is
 // that it will GAIA login with the app compat credentials, and opt-in to the Play Store.
 var AppCompatBooted = arc.NewPrecondition("arcappcompat_booted", false, appcompatGaia, "--arc-disable-app-sync", "--arc-disable-play-auto-install", "--arc-disable-locale-sync", "--arc-play-store-auto-update=off")
@@ -29,3 +34,19 @@ var AppCompatBootedInTabletMode = arc.NewPrecondition("arcappcompat_booted_in_ta
 // AppCompatVMBootedInTabletMode returns a precondition similar to BootedInTabletModeAppCompat(). The only difference from BootedInTabletModeAppCompat() is
 // that ARC VM, and not the ARC Container, is enabled in this precondition.
 var AppCompatVMBootedInTabletMode = arc.NewPrecondition("arcappcompat_vmbooted_in_tablet_mode", true, appcompatGaia, "--force-tablet-mode=touch_view", "--enable-virtual-keyboard", "--arc-disable-app-sync", "--arc-disable-play-auto-install", "--arc-disable-locale-sync", "--arc-play-store-auto-update=off")
+
+// AppCompatBootedForHearthstone is a precondition similar to arc.Booted(). The only difference from arc.Booted() is
+// that it will GAIA login with the Hearthstone credentials, and opt-in to the Play Store.
+var AppCompatBootedForHearthstone = arc.NewPrecondition("arcappcompat_booted", false, appcompatHearthstone, "--arc-disable-app-sync", "--arc-disable-play-auto-install", "--arc-disable-locale-sync", "--arc-play-store-auto-update=off")
+
+// AppCompatVMBootedForHearthstone is a precondition similar to BootedAppCompat(). The only difference from BootedAppCompat() is
+// that ARC VM, and not the ARC Container, is enabled in this precondition.
+var AppCompatVMBootedForHearthstone = arc.NewPrecondition("arcappcompat_vmbooted", true, appcompatHearthstone, "--arc-disable-app-sync", "--arc-disable-play-auto-install", "--arc-disable-locale-sync", "--arc-play-store-auto-update=off")
+
+// AppCompatBootedInTabletModeForHearthstone returns a precondition similar to BootedAppCompat(). The only difference from BootedAppCompat() is
+// that Chrome is launched in tablet mode in this precondition.
+var AppCompatBootedInTabletModeForHearthstone = arc.NewPrecondition("arcappcompat_booted_in_tablet_mode", false, appcompatHearthstone, "--force-tablet-mode=touch_view", "--enable-virtual-keyboard", "--arc-disable-app-sync", "--arc-disable-play-auto-install", "--arc-disable-locale-sync", "--arc-play-store-auto-update=off")
+
+// AppCompatVMBootedInTabletModeForHearthstone returns a precondition similar to BootedInTabletModeAppCompat(). The only difference from BootedInTabletModeAppCompat() is
+// that ARC VM, and not the ARC Container, is enabled in this precondition.
+var AppCompatVMBootedInTabletModeForHearthstone = arc.NewPrecondition("arcappcompat_vmbooted_in_tablet_mode", true, appcompatHearthstone, "--force-tablet-mode=touch_view", "--enable-virtual-keyboard", "--arc-disable-app-sync", "--arc-disable-play-auto-install", "--arc-disable-locale-sync", "--arc-play-store-auto-update=off")
