51e0f05d3f9a67940ba08bd2c3595c16a9ce401e
Shuo-Peng Liao <deanliao@chromium.org>
2020-05-08 23:22:13 +0800

    wifi: Add timing info for WifiService gRPC.
    Adding timing info for WifiService gRPC methods.
    
    BUG=b:154618948
    TEST=fast_build.sh -T
    tast run $DUT wifi.SimpleConnect.*
    Then inspect /tmp/tast/results/latest/timing.json
    
    Change-Id: I4d6fca9d12796493ee7078899d0154ce79219d91

--- src/chromiumos/tast/local/bundles/cros/network/wifi_service.go
+++ src/chromiumos/tast/local/bundles/cros/network/wifi_service.go
@@ -17,6 +17,7 @@ import (
 	"chromiumos/tast/local/shill"
 	"chromiumos/tast/services/cros/network"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/timing"
 )
 
 func init() {
@@ -32,12 +33,67 @@ type WifiService struct {
 	s *testing.ServiceState
 }
 
+func (s *WifiService) discoverService(ctx context.Context, m *shill.Manager, props map[string]interface{}) (*shill.Service, error) {
+	ctx, st := timing.Start(ctx, "discoverService")
+	defer st.End()
+	testing.ContextLog(ctx, "Discovering a WiFi service with properties: ", props)
+
+	// TODO(crbug.com/1034875): collect timing metrics, e.g. discovery time.
+	var service *shill.Service
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		var err error
+		service, err = m.FindMatchingService(ctx, props)
+		if err == nil {
+			return nil
+		}
+		// Scan WiFi AP again if the expected AP is not found.
+		if err2 := m.RequestScan(ctx, shill.TechnologyWifi); err2 != nil {
+			return testing.PollBreak(errors.Wrap(err2, "failed to request active scan"))
+		}
+		return err
+	}, &testing.PollOptions{
+		Timeout:  15 * time.Second,
+		Interval: 200 * time.Millisecond, // RequestScan is spammy, but shill handles that for us.
+	}); err != nil {
+		return nil, err
+	}
+	return service, nil
+}
+
+func (s *WifiService) connectService(ctx context.Context, service *shill.Service) error {
+	ctx, st := timing.Start(ctx, "connectService")
+	defer st.End()
+	testing.ContextLog(ctx, "Connecting to the service: ", service)
+
+	// Spawn watcher before connect.
+	pw, err := service.CreateWatcher(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create watcher")
+	}
+	defer pw.Close(ctx)
+
+	if err := service.Connect(ctx); err != nil {
+		return errors.Wrap(err, "failed to connect to service")
+	}
+
+	// Wait until connection established.
+	// According to previous Autotest tests, a reasonable timeout is
+	// 15 seconds for association and 15 seconds for configuration.
+	timeoutCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	if err := pw.Expect(timeoutCtx, shill.ServicePropertyIsConnected, true); err != nil {
+		return err
+	}
+	return nil
+}
+
 // Connect connects to a WiFi service with specific config.
 // This is the implementation of network.Wifi/Connect gRPC.
 func (s *WifiService) Connect(ctx context.Context, request *network.ConnectRequest) (*network.ConnectResponse, error) {
+	ctx, st := timing.Start(ctx, "wifi_service.Connect")
+	defer st.End()
 	testing.ContextLog(ctx, "Attempting to connect with config: ", request)
 
-	testing.ContextLog(ctx, "Discovering")
 	m, err := shill.NewManager(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create a manager object")
@@ -55,34 +111,16 @@ func (s *WifiService) Connect(ctx context.Context, request *network.ConnectReque
 			return nil, errors.Wrap(err, "failed to configure a hidden SSID")
 		}
 	}
-
 	props := map[string]interface{}{
 		shill.ServicePropertyType:          shill.TypeWifi,
 		shill.ServicePropertyName:          request.Ssid,
 		shill.ServicePropertySecurityClass: request.Security,
 	}
 
-	// TODO(crbug.com/1034875): collect timing metrics, e.g. discovery time.
-	testing.ContextLog(ctx, "Finding service with props: ", props)
-	var service *shill.Service
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		service, err = m.FindMatchingService(ctx, props)
-		if err == nil {
-			return nil
-		}
-		// Scan WiFi AP again if the expected AP is not found.
-		if err2 := m.RequestScan(ctx, shill.TechnologyWifi); err2 != nil {
-			return testing.PollBreak(errors.Wrap(err2, "failed to request active scan"))
-		}
-		return err
-	}, &testing.PollOptions{
-		Timeout:  15 * time.Second,
-		Interval: 200 * time.Millisecond, // RequestScan is spammy, but shill handles that for us.
-	}); err != nil {
+	service, err := s.discoverService(ctx, m, props)
+	if err != nil {
 		return nil, err
 	}
-
-	testing.ContextLog(ctx, "Connecting to service: ", service)
 
 	shillProps, err := protoutil.DecodeFromShillValMap(request.Shillprops)
 	if err != nil {
@@ -94,23 +132,7 @@ func (s *WifiService) Connect(ctx context.Context, request *network.ConnectReque
 		}
 	}
 
-	// Spawn watcher before connect.
-	pw, err := service.CreateWatcher(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create watcher")
-	}
-	defer pw.Close(ctx)
-
-	if err := service.Connect(ctx); err != nil {
-		return nil, errors.Wrap(err, "failed to connect to service")
-	}
-
-	// Wait until connection established.
-	// According to previous Autotest tests, a reasonable timeout is
-	// 15 seconds for association and 15 seconds for configuration.
-	timeoutCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
-	defer cancel()
-	if err := pw.Expect(timeoutCtx, shill.ServicePropertyIsConnected, true); err != nil {
+	if err := s.connectService(ctx, service); err != nil {
 		return nil, err
 	}
 
@@ -122,6 +144,9 @@ func (s *WifiService) Connect(ctx context.Context, request *network.ConnectReque
 // Disconnect disconnects from a WiFi service.
 // This is the implementation of network.Wifi/Disconnect gRPC.
 func (s *WifiService) Disconnect(ctx context.Context, request *network.DisconnectRequest) (*empty.Empty, error) {
+	ctx, st := timing.Start(ctx, "wifi_service.Disconnect")
+	defer st.End()
+
 	service, err := shill.NewService(ctx, dbus.ObjectPath(request.ServicePath))
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create service object")
@@ -145,8 +170,11 @@ func (s *WifiService) Disconnect(ctx context.Context, request *network.Disconnec
 	return &empty.Empty{}, nil
 }
 
-// DeleteEntriesForSSID deletes all WiFi profile entries for a given ssid.
+// DeleteEntriesForSSID deletes all WiFi profile entries for a given SSID.
 func (s *WifiService) DeleteEntriesForSSID(ctx context.Context, request *network.DeleteEntriesForSSIDRequest) (*empty.Empty, error) {
+	ctx, st := timing.Start(ctx, "wifi_service.DeleteEntriesForSSID")
+	defer st.End()
+
 	m, err := shill.NewManager(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create a manager object")
