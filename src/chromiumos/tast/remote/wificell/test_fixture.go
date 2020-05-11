ffc2bba4770fb8f55b528351e5c020c9a9f73821
Shuo-Peng Liao <deanliao@chromium.org>
2020-05-11 23:02:54 +0800

    wifi: Migrate network_WiFi_Prefer5Ghz to Tast.
    Add Tast test: wifi.Prefer5Ghz.
    Unlike Autotest version, it obtains active frequency by examining
    shill's service properties instead of looking at "iw scan" result.
    
    BUG=b:156249659
    TEST=tast run -var "router=$ROUTER" -var "pcap=$PCAP" $DUT  wifi.Prefer5Ghz
    
    Change-Id: I02f93f026a55a3dc0bf5dcc75aef78057bfcfd58

--- src/chromiumos/tast/remote/wificell/test_fixture.go
+++ src/chromiumos/tast/remote/wificell/test_fixture.go
@@ -20,6 +20,7 @@ import (
 	"chromiumos/tast/dut"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/remote/network/iw"
+	remote_iw "chromiumos/tast/remote/network/iw"
 	remoteping "chromiumos/tast/remote/network/ping"
 	"chromiumos/tast/remote/wificell/hostapd"
 	"chromiumos/tast/remote/wificell/pcap"
@@ -431,6 +432,11 @@ func (tf *TestFixture) Pcap() *Router {
 // WifiClient returns the gRPC WifiServiceClient of the DUT.
 func (tf *TestFixture) WifiClient() network.WifiServiceClient {
 	return tf.wifiClient
+}
+
+// DUTIwRunner returns a new IwRunner object for DUT.
+func (tf *TestFixture) DUTIwRunner() *iw.Runner {
+	return remote_iw.NewRunner(tf.dut.Conn())
 }
 
 // DefaultOpenNetworkAP configures the router to provide an 802.11n open network.
