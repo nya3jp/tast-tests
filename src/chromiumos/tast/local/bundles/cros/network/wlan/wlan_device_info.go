288756b29a000f1377f268c2fdeaf9485bfd4da6
Will Donnelly <wgd@chromium.org>
2020-05-12 16:50:01 -0700

    tast-tests: network.WLANDriver: Add PCI ID of new HrP2 SKU
    BUG=b:146212060, b:149045842
    TEST=`tast run network.WLANDriver` now passes on Volteer
    
    Change-Id: Idaa2351498e158d14efddef87074956b1c8afe51

--- src/chromiumos/tast/local/bundles/cros/network/wlan/wlan_device_info.go
+++ src/chromiumos/tast/local/bundles/cros/network/wlan/wlan_device_info.go
@@ -73,6 +73,7 @@ var lookupWLANDev = map[DevInfo]string{
 	{vendor: "0x8086", device: "0x02f0", subsystem: "0x0034"}: Intel9000,
 	{vendor: "0x8086", device: "0x02f0", subsystem: "0x4070"}: Intel22560,
 	{vendor: "0x8086", device: "0x02f0", subsystem: "0x0074"}: Intel22560,
+	{vendor: "0x8086", device: "0xa0f0", subsystem: "0x0074"}: Intel22560,
 	{vendor: "0x02d0", device: "0x4354"}:                      BroadcomBCM4354SDIO,
 	{vendor: "0x14e4", device: "0x43ec"}:                      BroadcomBCM4356PCIE,
 	{vendor: "0x14e4", device: "0x440d"}:                      BroadcomBCM4371PCIE,
