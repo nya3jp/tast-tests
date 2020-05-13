790e5b50fdcabea8ae20a477bd4fb9a2e8a111e8
Tetsui Ohkubo <tetsui@chromium.org>
2020-05-13 14:10:54 +0900

    tast-tests: Build ArcWMTestApp from source
    Moved from vendor/google_arc/packages/development/ArcWMTestApp
    (pi-arc e13a56642eb6eb16f38d5d0a5a4b5f3119da0d48).
    
    BUG=b:120452372
    TEST=tast run eve-DUT arc.WindowManagerCUJ
    
    Change-Id: Ied39f12050fd5de3905afde3f3b7819475c3960a

---
+++ android/ArcWMTestApp/src/org/chromium/arc/testapp/windowmanager/ResizeableUnspecifiedActivity.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.windowmanager;
+
+/** A {@link BaseActivity} subclass. It creates a resizable + unspecified orientation activity. */
+public class ResizeableUnspecifiedActivity extends BaseActivity {}
