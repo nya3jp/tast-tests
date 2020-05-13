cd11c49e711de8e9080e802279873e1bd69e5a37
Yen-lin Lai <yenlinlai@google.com>
2020-05-13 14:28:18 +0800

    wifi: Port more cases from network_WiFi_SimpleConnect
    Port sub-tests under network_WiFi_SimpleConnect to Tast.
    - wifi_checkDFS
    - wifi_checkSSIDLimits
    - wifi_checkSGI
    - wifi_checkRawPMK
    
    Also introduce a utility for generating hostapd options with common
    settings. It will be more helpful when there are more testcases that
    don't quite care the protocol and channel. e.g. upcoming NonAsciiSSID
    testcases. We can also apply the function to the security testcases in
    SimpleConnect later if it is suitable.
    
    BUG=chromium:1034875
    TEST=`tast run $DUT wifi.SimpleConnect.*`
    
    Change-Id: I806f45796c6561a14d3f56acfc71b5bf7b950a4a

--- src/chromiumos/tast/remote/wificell/util.go
+++ src/chromiumos/tast/remote/wificell/util.go
@@ -8,6 +8,7 @@ import (
 	"context"
 
 	"chromiumos/tast/caller"
+	"chromiumos/tast/remote/wificell/hostapd"
 	"chromiumos/tast/testing"
 )
 
@@ -22,4 +23,19 @@ func collectFirstErr(ctx context.Context, firstErr *error, err error) {
 	if *firstErr == nil {
 		*firstErr = err
 	}
+}
+
+// CommonAPOptions generates a set of options with common settings on protocol
+// and channel appended with given extra options.
+// This function is useful for the tests which don't quite care about the
+// protocol used but need to set some other options like SSID or security.
+func CommonAPOptions(extraOps ...hostapd.Option) []hostapd.Option {
+	// Common case: 80211n, 5GHz channel, 40 MHz width.
+	commonOps := []hostapd.Option{
+		hostapd.Mode(hostapd.Mode80211nMixed),
+		hostapd.Channel(48),
+		hostapd.HTCaps(hostapd.HTCapHT40),
+	}
+	// Append extra options.
+	return append(commonOps, extraOps...)
 }
