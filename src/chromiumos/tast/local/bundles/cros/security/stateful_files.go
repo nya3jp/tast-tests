649c4b561a0c3be112854072a1987898294ae576
Jorge Lucangeli Obes <jorgelo@chromium.org>
2020-05-13 20:46:24 -0400

    security.StatefulFiles: Add expectation for diagnostics.
    'cros_healthd' uses /var/cache/diagnostics. Add expectations for it:
    
    /mnt/stateful_partition/encrypted/var/cache/diagnostics: UID 20134 (want [0]), GID 20134 (want [0])
    
    BUG=chromium:1081518
    TEST=Passes on amd64-generic.
    
    Change-Id: If47d5f382085d50e6d7927d8e6e85c5a62c1c6f0

--- src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
+++ src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
@@ -208,6 +208,10 @@ func StatefulFiles(ctx context.Context, s *testing.State) {
 		prependPatterns(chk.NewPattern(chk.Path("encrypted/var/lib/wilco/storage.img"), users("wilco_dtc"), groups("wilco_dtc"), chk.NotMode(022)))
 	}
 
+	if _, err := user.Lookup("cros_healthd"); err == nil {
+		prependPatterns(chk.NewPattern(chk.Path("encrypted/var/cache/diagnostics"), users("cros_healthd"), groups("cros_healthd"), chk.SkipChildren()))
+	}
+
 	if moblab.IsMoblab() {
 		// On moblab devices, there are additional user dirs and tons of stuff (MySQL, etc.) in /var.
 		prependPatterns(
