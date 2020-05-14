ab1ab511bbd3ac308b5d2430266ea68f5a15545e
Brendan Hansknecht <bhansknecht@chromium.org>
2020-05-14 09:31:08 -0700

    tast-tests: Promote ui.{UbertrayOpenSettings,LauncherSearchNativeApps}
    Promotes the listed tests to critical. LauncherSearchNativeApps was
    failing recently, but the bug has been fixed and it is stable.
    
    BUG=chromium:991248
    TEST=None
    
    Change-Id: Ib8f83f7888c5891c2a5b92a00682785b8308d0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2202162
    Tested-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: David Haddock <dhaddock@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/ubertray_open_settings.go
+++ src/chromiumos/tast/local/bundles/cros/ui/ubertray_open_settings.go
@@ -24,7 +24,7 @@ func init() {
 			"bhansknecht@chromium.org",
 			"kyleshima@chromium.org",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Pre:          chrome.LoggedIn(),
 	})
