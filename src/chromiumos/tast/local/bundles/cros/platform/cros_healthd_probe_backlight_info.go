dc58ebbf1bfc6b09f5d2041a6c87a21e660bfbba
Jesse Schettler <jschettler@chromium.org>
2020-05-15 14:42:03 -0600

    tast-tests: Update CrosHealthdProbeBacklightInfo
    Update platform.CrosHealthdProbeBacklightInfo to use the croshealthd
    support library.
    
    BUG=chromium:1083323
    TEST=platform.CrosHealthdProbeBacklightInfo
    
    Change-Id: Ib8c1bd6f26f2feb894cc7ead9a49292f38d1da62

--- src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_backlight_info.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_backlight_info.go
@@ -6,15 +6,11 @@ package platform
 
 import (
 	"context"
-	"io/ioutil"
-	"path/filepath"
 	"reflect"
 	"strconv"
-	"strings"
 
 	"chromiumos/tast/local/crosconfig"
-	"chromiumos/tast/local/testexec"
-	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/local/croshealthd"
 	"chromiumos/tast/testing"
 )
 
@@ -32,22 +28,9 @@ func init() {
 }
 
 func CrosHealthdProbeBacklightInfo(ctx context.Context, s *testing.State) {
-	if err := upstart.EnsureJobRunning(ctx, "cros_healthd"); err != nil {
-		s.Fatal("Failed to start cros_healthd: ", err)
-	}
-
-	// Expected output when backlight is present:
-	// path,max_brightness,brightness
-	// some_string,some_uint,some_uint <- this line repeated per backlight.
-	b, err := testexec.CommandContext(ctx, "telem", "--category=backlight").Output(testexec.DumpLogOnError)
+	records, err := croshealthd.FetchTelemetry(ctx, croshealthd.TelemCategoryBacklight, s.OutDir())
 	if err != nil {
-		s.Fatal("Command failed: ", err)
-	}
-
-	// Log output to file for debugging.
-	path := filepath.Join(s.OutDir(), "command_output.txt")
-	if err := ioutil.WriteFile(path, b, 0644); err != nil {
-		s.Errorf("Failed to write output to %s: %v", path, err)
+		s.Fatal("Failed to get backlight telemetry info: ", err)
 	}
 
 	val, err := crosconfig.Get(ctx, "/cros-healthd/backlight", "has-backlight")
@@ -56,49 +39,44 @@ func CrosHealthdProbeBacklightInfo(ctx context.Context, s *testing.State) {
 	}
 
 	hasBacklight := !(err == nil && val == "false")
-	s.Log("Device has backlight: ", hasBacklight)
-
-	lines := strings.Split(strings.TrimRight(string(b), "\n"), "\n")
 	if !hasBacklight {
-		if len(lines) != 1 {
-			s.Fatalf("Incorrect number of output lines: got %d; want 1", len(lines))
+		if len(records) != 1 {
+			s.Fatalf("Incorrect number of records: got %d; want 1", len(records))
 		}
-		// If there is no backlight, there is no output to verify.
+		// If there is no backlight, there are no records to verify.
 		return
 	}
 
-	if len(lines) < 2 {
+	if len(records) < 2 {
 		s.Fatal("Could not find any lines of backlight info")
 	}
 
 	// Verify the headers are correct.
 	want := []string{"path", "max_brightness", "brightness"}
-	got := strings.Split(lines[0], ",")
+	got := records[0]
 	if !reflect.DeepEqual(want, got) {
 		s.Fatalf("Incorrect headers: got %v; want %v", got, want)
 	}
 
-	// Verify each line of backlight info contains valid values.
-	for _, line := range lines[1:] {
-		s.Log("Checking line: ", line)
-		vals := strings.Split(line, ",")
-		if len(vals) != 3 {
-			s.Errorf("Wrong number of values: got %v, want 3", len(vals))
+	// Verify each record contains valid values.
+	for _, record := range records[1:] {
+		if len(record) != 3 {
+			s.Errorf("Wrong number of values: got %d, want 3", len(record))
 			continue
 		}
 
-		if vals[0] == "" {
+		if record[0] == "" {
 			s.Error("Empty path")
 		}
 
-		maxBrightness, err := strconv.Atoi(vals[1])
+		maxBrightness, err := strconv.Atoi(record[1])
 		if err != nil {
 			s.Errorf("Failed to convert %q to integer: %v", want[1], err)
 		} else if maxBrightness < 0 {
 			s.Errorf("Invalid %s: %v", want[1], maxBrightness)
 		}
 
-		brightness, err := strconv.Atoi(vals[2])
+		brightness, err := strconv.Atoi(record[2])
 		if err != nil {
 			s.Errorf("Failed to convert %q to integer: %v", want[2], err)
 		} else if brightness < 0 {
