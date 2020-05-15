0f1a48c70291e9c7f5e97151ff463b5b80b1f8b0
Puthikorn Voravootivat <puthik@chromium.org>
2020-05-15 11:11:46 -0700

    power: Add tast test to check DPTF profile override
    We found that DPTF uprev may break DPTF profile override which
    make DUT load wrong DPTF profile. Add new test to ensure that
    this won't happen again in the future.
    
    BUG=b:156685874, b:155916111
    TEST=Tested on atlas, kohaku, helios, leona, robo360.
    
    Cq-Depend: chromium:2213050, chromium:2212950
    Change-Id: I634ad3e78e3408f5b990a4d0291101340184efe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2204310
    Tested-by: Puthikorn Voravootivat <puthik@chromium.org>
    Auto-Submit: Puthikorn Voravootivat <puthik@chromium.org>
    Reviewed-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Puthikorn Voravootivat <puthik@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/power/dptf_override_script.go
@@ -0,0 +1,42 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/power/dptf"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DptfOverrideScript,
+		Desc: "Check that dptf loads correct thermal profile from override script",
+		Contacts: []string{
+			"puthik@chromium.org",                // test author
+			"chromeos-platform-power@google.com", // CrOS platform power developers
+		},
+		Attr: []string{"group:mainline", "informational"},
+		// Only Atlas use override script, board developed later uses unibuild.
+		HardwareDeps: hwdep.D(hwdep.Platform("atlas")),
+	})
+}
+
+func DptfOverrideScript(ctx context.Context, s *testing.State) {
+	expectedProfile, err := dptf.GetProfileFromOverrideScript(ctx)
+	if err != nil {
+		s.Fatal("GetProfileFromOverrideScript failed: ", err)
+	}
+	actualProfile, err := dptf.GetProfileFromPgrep(ctx)
+	if err != nil {
+		s.Fatal("GetProfileFromPgrep failed: ", err)
+	}
+
+	if expectedProfile != actualProfile {
+		s.Errorf("Unexpected DPTF profile: got %q; want %q", actualProfile, expectedProfile)
+	}
+}
