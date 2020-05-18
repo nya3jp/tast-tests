2e538bd77fe545ca5f0441c937b93501d36c631a
Dhanya Ganesh <dhanyaganesh@chromium.org>
2020-05-18 18:44:05 +0000

    Demote ui.LauncherSearchNativeApps to informational
    BUG=b:156784983
    TEST=None
    Exempt-From-Owner-Approval: Flaky test
    
    Change-Id: Ib7c5fa814a9a3952d993e506792f785d7fa02cd1

--- src/chromiumos/tast/local/bundles/cros/ui/launcher_search_native_apps.go
+++ src/chromiumos/tast/local/bundles/cros/ui/launcher_search_native_apps.go
@@ -23,7 +23,7 @@ func init() {
 			"bhansknecht@chromium.org",
 			"dhaddock@chromium.org",
 		},
-		Attr:         []string{"group:mainline"},
+		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Pre:          chrome.LoggedIn(),
 	})
