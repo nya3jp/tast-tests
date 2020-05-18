aee3944b65790098230c5e215a193100435c76e5
Sara Kato <sarakato@google.com>
2020-05-18 09:20:28 +0900

    tast-tests: Fix a11yEvent flakiness with testing.Poll
    Ran for a few hours without failure.
    
    BUG=b:156715817
    TEST=tast run DUT_IP arc.AccessibilityEvent
    
    Change-Id: If352ba2053b99ad17a089bb16c4bc91bf05524c4

--- src/chromiumos/tast/local/bundles/cros/arc/accessibility_event.go
+++ src/chromiumos/tast/local/bundles/cros/arc/accessibility_event.go
@@ -100,11 +100,17 @@ func runTestStep(ctx context.Context, cvconn *chrome.Conn, tconn *chrome.TestCon
 		return err
 	}
 
-	// Initial action sometimes invokes additional events (like focusing the entire application).
-	// Latest logs should only be checked on the first iteration. (b/123397142#comment19)
-	// TODO(b/142093176) Find the root cause.
-	if err := verifyLog(ctx, cvconn, test.Event, isFirstStep); err != nil {
-		return errors.Wrap(err, "failed to verify the log")
+	// To reduce flakiness when switching activities, it's necessary to poll for the updated log.
+	if pollErr := testing.Poll(ctx, func(ctx context.Context) error {
+		// Initial action sometimes invokes additional events (like focusing the entire application).
+		// Latest logs should only be checked on the first iteration. (b/123397142#comment19)
+		// TODO(b/142093176) Find the root cause.
+		if err := verifyLog(ctx, cvconn, test.Event, isFirstStep); err != nil {
+			return errors.Wrap(err, "failed to verify the log")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 10 * time.Second}); pollErr != nil {
+		return errors.Wrap(pollErr, "timed out waiting for log")
 	}
 
 	return nil
