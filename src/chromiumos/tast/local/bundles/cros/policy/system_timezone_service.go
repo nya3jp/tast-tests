697e1fa3b25ebd5e79440ab389e8d48258d108de
alexanderhartl <alexanderhartl@google.com>
2020-05-19 09:34:54 +0000

    tast-tests: add SystemTimezone
    Add the policy.SystemTimezone tast-test.
    
    BUG=chromium:1084446
    TEST=tast run IP policy.SystemTimezone
    
    Change-Id: I692714f9eca3c42d739a427dbb14360c0dcd0a92

---
+++ src/chromiumos/tast/local/bundles/cros/policy/system_timezone_service.go
@@ -0,0 +1,69 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"strings"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	pb "chromiumos/tast/services/cros/policy"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddService(&testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			pb.RegisterSystemTimezoneServiceServer(srv, &SystemTimezoneService{s: s})
+		},
+	})
+}
+
+// SystemTimezoneService implements tast.cros.policy.SystemTimezoneService.
+type SystemTimezoneService struct { // NOLINT
+	s *testing.ServiceState
+}
+
+func (c *SystemTimezoneService) TestLogout(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		return nil, errors.Wrap(err, "failed to log out")
+	}
+
+	return nil, nil
+}
+
+func (c *SystemTimezoneService) GetSystemTimezone(ctx context.Context, req *pb.GetSystemTimezoneRequest) (*empty.Empty, error) {
+
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		return nil, errors.Wrap(err, "failed to log out")
+	}
+
+	// Wait until the timezone is set.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+
+		out, err := testexec.CommandContext(ctx, "date", "+%Z").Output()
+		if err != nil {
+			return errors.Wrap(err, "failed to get the timezone")
+		}
+		outStr := strings.TrimSpace(string(out))
+
+		if outStr != req.Timezone {
+			return errors.Errorf("unexpected timezone: got %q; want %q", outStr, req.Timezone)
+		}
+
+		return nil
+
+	}, nil); err != nil {
+		return nil, err
+	}
+
+	return &empty.Empty{}, nil
+}
