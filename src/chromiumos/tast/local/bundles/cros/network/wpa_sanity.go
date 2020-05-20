cf7f0a49ea519f5ec9cc88accaf166ccedeb421e
Shuo-Peng Liao <deanliao@chromium.org>
2020-05-21 02:01:50 +0800

    network: Move out wpa_cli command logic.
    As we'll have another test running wpa_cli command, let's move the logic
    running wpa_cli to wpacli package.
    
    Also, it is too verbose dumping logs on error for every failed run
    during the polling. I add a field in both local and remote cmd runner
    to suppress logging on error.
    
    BUG=b:156249659
    TEST=tast run $DUT network.WPASanity
    ./fast_build.sh -t chromiumos/tast/common/network/wpacli
    
    Change-Id: I0de4f8bdc39026e2bceb14143131580ad402fa4f

--- src/chromiumos/tast/local/bundles/cros/network/wpa_sanity.go
+++ src/chromiumos/tast/local/bundles/cros/network/wpa_sanity.go
@@ -6,13 +6,11 @@ package network
 
 import (
 	"context"
-	"io/ioutil"
-	"path/filepath"
-	"strings"
 	"time"
 
+	"chromiumos/tast/common/network/wpacli"
+	"chromiumos/tast/local/network/cmd"
 	"chromiumos/tast/local/shill"
-	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
@@ -38,33 +36,17 @@ func WPASanity(ctx context.Context, s *testing.State) {
 	}
 	s.Log("WiFi interface: ", iface)
 
-	var output string
-	logfile := "wpa_cli.log"
-
-	// In case wpa_cli produces multiple line output, in log message we show
-	// the path of wpa_cli.log.
-	wpacliOutput := func() string {
-		if strings.Contains(output, "\n") {
-			return "check wpa_cli logfile: /tmp/tast/results/latest/tests/network.WPASanity/" + logfile
-		}
-		return output
-	}
-
 	// Even if we got a WiFi device, iface, from shill, that does not imply
 	// that wpa_supplicant of the WiFi device is up and running. Poll for a
 	// successful wpa_cli ping for 5 seconds to avoid false negative.
+	wRunner := wpacli.NewRunner(&cmd.LocalCmdRunner{NoLogOnError: true})
+	var cmdOut []byte
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		cmdOut, err := testexec.CommandContext(
-			ctx, "sudo", "-u", "wpa", "-g", "wpa",
-			"wpa_cli", "-i", iface, "ping").CombinedOutput()
-		ioutil.WriteFile(filepath.Join(s.OutDir(), logfile), cmdOut, 0644)
-		output = strings.TrimSpace(string(cmdOut))
-		return err
+		var err2 error
+		cmdOut, err2 = wRunner.Ping(ctx, iface)
+		return err2
 	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
-		s.Fatal("Failed to ping wpa_supplicant: " + wpacliOutput())
-	}
-
-	if !strings.Contains(output, "PONG") {
-		s.Fatal("Failed to see expected PONG reply: " + wpacliOutput())
+		output := wpacli.MayOutputToFile(cmdOut, s.OutDir(), "wpa_cli.log")
+		s.Fatalf("Failed to ping wpa_supplicant: %s; output: %s", err, output)
 	}
 }
