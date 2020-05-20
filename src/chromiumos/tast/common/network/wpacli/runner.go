a040f34135373a04859582648c540301392e4daf
Shuo-Peng Liao <deanliao@chromium.org>
2020-05-21 02:01:50 +0800

    network: Move out wpa_cli command logic.
    As we'll have another test running wpa_cli command, let's move the logic
    running wpa_cli to wpacli package.
    
    Also, it is too verbose dumping logs on error for every failed run
    during the polling. I add a field in both local and remote cmd runner
    to suppress logging on error.
    
    BUG=b:156249659
    TEST=tast run $DUT network.WPASanity
    ./fast_build.sh -t chromiumos/tast/common/network/wpacli
    
    Change-Id: I0de4f8bdc39026e2bceb14143131580ad402fa4f

---
+++ src/chromiumos/tast/common/network/wpacli/runner.go
@@ -0,0 +1,44 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wpacli
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/common/network/cmd"
+	"chromiumos/tast/errors"
+)
+
+// Runner contains methods involving wpa_cli command.
+type Runner struct {
+	cmd cmd.Runner
+}
+
+// NewRunner creates a new wpa_cli command utility runner.
+func NewRunner(c cmd.Runner) *Runner {
+	return &Runner{cmd: c}
+}
+
+// sudoWpacli returns a sudo command args that runs wpa_cli with args under sudo.
+func sudoWpacli(args ...string) []string {
+	ret := []string{"-u", "wpa", "-g", "wpa", "wpa_cli"}
+	for _, arg := range args {
+		ret = append(ret, arg)
+	}
+	return ret
+}
+
+// Ping runs "wpa_cli -i iface ping" command and expects to see PONG.
+func (r *Runner) Ping(ctx context.Context, iface string) ([]byte, error) {
+	cmdOut, err := r.cmd.Output(ctx, "sudo", sudoWpacli("-i", iface, "ping")...)
+	if err != nil {
+		return cmdOut, errors.Wrapf(err, "failed running wpa_cli -i %s ping", iface)
+	}
+	if !strings.Contains(string(cmdOut), "PONG") {
+		return cmdOut, errors.New("failed to see 'PONG' in wpa_cli ping output")
+	}
+	return cmdOut, nil
+}
