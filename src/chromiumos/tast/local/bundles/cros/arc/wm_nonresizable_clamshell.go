7794009ab6c64d728577794437722ecf67847fd9
armenk <armenk@google.com>
2020-05-21 16:49:41 -0700

    Add NC01 Tast-test for WM ARC++ R
    Implement NC01: non-resizable/clamshell: default launch behavior tast-test
    described in ARC++ R WM specs located in go/arc-wm-r.
    BUG=b:140203139
    TEST=tast -verbose run $DUT arc.WMNonresizableClamshell
    
    Change-Id: I962442ff8e6af75f2a54acac685acbc90ffc1ab8

---
+++ src/chromiumos/tast/local/bundles/cros/arc/wm_nonresizable_clamshell.go
@@ -0,0 +1,105 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arc/wm"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WMNonresizableClamshell,
+		Desc:         "Verifies that Window Manager non-resizable clamshell use-cases behave as described in go/arc-wm-r",
+		Contacts:     []string{"armenk@google.com", "arc-framework+tast@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"android_vm", "chrome"},
+		Data:         []string{"ArcWMTestApp_24.apk"},
+		Pre:          arc.VMBooted(),
+		Timeout:      8 * time.Minute,
+	})
+}
+
+func WMNonresizableClamshell(ctx context.Context, s *testing.State) {
+
+	// testFunc represents a function that tests if the window is in a certain state.
+	type testFunc func(context.Context, *chrome.TestConn, *arc.ARC, *ui.Device) error
+
+	wmApkToInstall := []string{wm.APKNameArcWMTestApp24}
+	cr := s.PreValue().(arc.PreData).Chrome
+	a := s.PreValue().(arc.PreData).ARC
+
+	tconn, d, err := wm.CommonWMSetUp(ctx, s, wmApkToInstall)
+	if err != nil {
+		s.Fatal("Failed to setup test: ", err)
+	}
+	defer d.Close()
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
+
+	if err != nil {
+		s.Fatal("Failed to ensure if tablet mode is disabled: ", err)
+	}
+
+	defer cleanup(ctx)
+
+	for _, test := range []struct {
+		name string
+		fn   testFunc
+	}{
+		{"NC01_launch", wmNC01}, // non-resizable/clamshell: default launch behavior
+	} {
+		s.Logf("Running test %q", test.name)
+
+		if err := test.fn(ctx, tconn, a, d); err != nil {
+			path := fmt.Sprintf("%s/screenshot-cuj-failed-test-%s.png", s.OutDir(), test.name)
+			if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
+				s.Log("Failed to capture screenshot: ", err)
+			}
+			s.Errorf("%s test failed: %v", test.name, err)
+		}
+	}
+}
+
+// wmNC01 covers non-resizable/clamshell default launch behavior.
+// Expected behavior is defined in: go/arc-wm-r NC01: non-resizable/clamshell: default launch behavior.
+func wmNC01(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device) error {
+	for _, activityName := range []string{
+		wm.NonResizablePortraitActivity,
+		wm.NonResizableLandscapeActivity,
+	} {
+		if err := func() error {
+			act, err := arc.NewActivity(a, wm.Pkg24, activityName)
+			if err != nil {
+				return err
+			}
+
+			if err := act.Start(ctx, tconn); err != nil {
+				return err
+			}
+			defer act.Stop(ctx)
+
+			if err := wm.WaitUntilActivityIsReady(ctx, tconn, act, d); err != nil {
+				return err
+			}
+			defer act.Close()
+
+			return wm.CheckMaximizeNonResizable(ctx, tconn, act, d)
+		}(); err != nil {
+			return errors.Wrapf(err, "%q test failed", activityName)
+		}
+	}
+	return nil
+}
