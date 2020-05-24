0bb31729175ebda38c2ff9e7b5efde87508abcd7
Daniel Hung-yu Wu <hywu@google.com>
2020-05-24 21:23:05 +0800

    camera: add portrait mode performance test
    BUG=b:117817041
    TEST="tast run <DUT> camera.HAL3PortraitMode.perf"
    
    Change-Id: Iadfb6cbca96234932d36bb7a37c1764152c4093a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2212012
    Commit-Queue: Hung-yu Wu <hywu@chromium.org>
    Tested-by: Hung-yu Wu <hywu@chromium.org>
    Reviewed-by: Ren-Pei Zeng <kamesan@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/camera/hal3_portrait_mode.go
+++ src/chromiumos/tast/local/bundles/cros/camera/hal3_portrait_mode.go
@@ -17,16 +17,24 @@ func init() {
 		Func:         HAL3PortraitMode,
 		Desc:         "Verifies camera portrait mode function with HAL3 interface",
 		Contacts:     []string{"hywu@chromium.org", "chromeos-camera-eng@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"android_p", "arc_camera3", caps.BuiltinCamera},
 		Data:         []string{portraitModeTestFile},
+		Params: []testing.Param{{
+			Name:      "",
+			Val:       false, // generatePerfLog
+			ExtraAttr: []string{"group:mainline", "informational"},
+		}, {
+			Name:      "perf",
+			Val:       true, // generatePerfLog
+			ExtraAttr: []string{"group:crosbolt", "crosbolt_perbuild"},
+		}},
 	})
 }
 
 const portraitModeTestFile = "portrait_4096x3072.jpg"
 
 func HAL3PortraitMode(ctx context.Context, s *testing.State) {
-	if err := hal3.RunTest(ctx, hal3.PortraitModeTestConfig(s.OutDir(), s.DataPath(portraitModeTestFile))); err != nil {
+	if err := hal3.RunTest(ctx, hal3.PortraitModeTestConfig(s.OutDir(), s.Param().(bool), s.DataPath(portraitModeTestFile))); err != nil {
 		s.Error("Test failed: ", err)
 	}
 }
