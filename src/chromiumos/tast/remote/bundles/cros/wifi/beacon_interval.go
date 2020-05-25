c8eb73ef276b918655199abeb6d609ba60977153
Yen-lin Lai <yenlinlai@google.com>
2020-05-25 13:10:22 +0800

    Reland "wifi: Port network_WiFi_BeaconInterval from Autotest."
    This is a reland of 93dc78b9b16e1d07c96d555c25bb04f206ebde15
    
    The original cl conflict with crrev.com/c/2237017. Fix the conflict
    and reland.
    
    Original change's description:
    > wifi: Port network_WiFi_BeaconInterval from Autotest.
    >
    > BUG=b:157523097
    > TEST=fast_build.sh -T
    > TEST=tast run $DUT wifi.BeaconInterval
    >
    > Change-Id: Ibf04fc830574f27c3b0cd2b904e4bf33d800bf63
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2217277
    > Tested-by: Yen-lin Lai <yenlinlai@google.com>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    > Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    > Commit-Queue: Shuo-Peng Liao <deanliao@chromium.org>
    
    BUG=b:157523097
    TEST=fast_build.sh -T
    TEST=tast run $DUT wifi.BeaconInterval
    
    Bug: b:157523097
    Change-Id: I47d394240c18775a462f66c2f7c47d3e1cb08969

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/beacon_interval.go
@@ -0,0 +1,122 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/network/iw"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/remote/wificell/hostapd"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        BeaconInterval,
+		Desc:        "Verifies that the beacon interval set on the AP is successfully adopted by the DUT",
+		Contacts:    []string{"yenlinlai@google.com", "chromeos-platform-connectivity@google.com"},
+		Attr:        []string{"group:wificell", "wificell_func", "wificell_unstable"},
+		ServiceDeps: []string{"tast.cros.network.WifiService"},
+		Vars:        []string{"router", "pcap"},
+	})
+}
+
+func BeaconInterval(ctx context.Context, s *testing.State) {
+	var tfOps []wificell.TFOption
+	if router, ok := s.Var("router"); ok {
+		tfOps = append(tfOps, wificell.TFRouter(router))
+	}
+	if pcap, ok := s.Var("pcap"); ok {
+		tfOps = append(tfOps, wificell.TFPcap(pcap))
+	}
+
+	tf, err := wificell.NewTestFixture(ctx, ctx, s.DUT(), s.RPCHint(), tfOps...)
+	if err != nil {
+		s.Fatal("Failed to set up test fixture: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := tf.Close(ctx); err != nil {
+			s.Log("Failed to tear down test fixture: ", err)
+		}
+	}(ctx)
+	// Shorten ctx on the way for each clean up in defer.
+	ctx, cancel := tf.ReserveForClose(ctx)
+	defer cancel()
+
+	// The value of beacon interval to be set in hostapd config
+	// and checked from DUT.
+	const expectBeaconInt = 200
+
+	s.Log("Setting up AP")
+	apOps := wificell.CommonAPOptions(hostapd.BeaconInterval(expectBeaconInt))
+	ap, err := tf.ConfigureAP(ctx, apOps, nil)
+	if err != nil {
+		s.Fatal("Failed to configure ap: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := tf.DeconfigAP(ctx, ap); err != nil {
+			s.Error("Failed to deconfig ap: ", err)
+		}
+	}(ctx)
+	ctx, cancel = tf.ReserveForDeconfigAP(ctx, ap)
+	defer cancel()
+
+	s.Log("Connecting to WiFi")
+	if _, err := tf.ConnectWifiAP(ctx, ap); err != nil {
+		s.Fatal("Failed to connect to WiFi: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := tf.DisconnectWifi(ctx); err != nil {
+			s.Error("Failed to disconnect WiFi: ", err)
+		}
+		req := &network.DeleteEntriesForSSIDRequest{Ssid: []byte(ap.Config().SSID)}
+		if _, err := tf.WifiClient().DeleteEntriesForSSID(ctx, req); err != nil {
+			s.Errorf("Failed to remove entries for ssid=%s: %v", ap.Config().SSID, err)
+		}
+	}(ctx)
+	// Shorten a little bit for disconnect.
+	ctx, cancel = ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	s.Log("Start verification")
+	// Check the beacon interval setting.
+	iface, err := tf.ClientInterface(ctx)
+	if err != nil {
+		s.Fatal("Failed to get DUT's WiFi interface: ", err)
+	}
+	bi, err := ifaceBeaconInt(ctx, s.DUT(), iface)
+	if err != nil {
+		s.Fatal("Failed to get beacon interval: ", err)
+	}
+	if bi != expectBeaconInt {
+		s.Fatalf("Unexpected beacon interval, got %d, want %d", bi, expectBeaconInt)
+	}
+	// Check connectivity.
+	if err := tf.PingFromDUT(ctx, ap.ServerIP().String()); err != nil {
+		s.Fatal("Failed to ping from DUT: ", err)
+	}
+
+	s.Log("Verified; tearing down")
+}
+
+func ifaceBeaconInt(ctx context.Context, dut *dut.DUT, iface string) (int, error) {
+	iwr := iw.NewRemoteRunner(dut.Conn())
+	val, err := iwr.LinkValue(ctx, iface, "beacon int")
+	if err != nil {
+		return 0, err
+	}
+	bi, err := strconv.Atoi(val)
+	if err != nil {
+		return 0, errors.Wrapf(err, "beacon int %q not a number", val)
+	}
+	return bi, nil
+}
