ae583e4aff47b11943fa221fc78af9775f0699b3
Shuo-Peng Liao <deanliao@chromium.org>
2020-05-25 18:59:51 +0800

    wifi: Migrate network_WiFi_ScanPerformance to Tast.
    Add Tast test: wifi.ScanPerf.
    
    BUG=b:156249427
    TEST=tast run var "router=$ROUTER" $DUT wifi.ScanPerf
    Observe that scan performance result is in
    /tmp/tast/results/latest/tests/wifi.ScanPerf/results-chart.json
    
    Change-Id: Iea2280ba25841881d860c344b4c60b33be1c17dc

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/scan_perf.go
@@ -0,0 +1,144 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/network/iw"
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	remote_iw "chromiumos/tast/remote/network/iw"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/remote/wificell/hostapd"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        ScanPerf,
+		Desc:        "Measure BSS scan performance in various setup",
+		Contacts:    []string{"deanliao@google.com", "chromeos-platform-connectivity@google.com"},
+		Attr:        []string{"group:wificell", "wificell_func", "wificell_unstable"},
+		ServiceDeps: []string{"tast.cros.network.WifiService"},
+		Vars:        []string{"router"},
+	})
+}
+
+func ScanPerf(fullCtx context.Context, s *testing.State) {
+	var tfOps []wificell.TFOption
+	if router, _ := s.Var("router"); router != "" {
+		tfOps = append(tfOps, wificell.TFRouter(router))
+	}
+
+	tf, err := wificell.NewTestFixture(fullCtx, fullCtx, s.DUT(), s.RPCHint(), tfOps...)
+	if err != nil {
+		s.Fatal("Failed to set up test fixture: ", err)
+	}
+	defer func() {
+		if err := tf.Close(fullCtx); err != nil {
+			s.Log("Failed to tear down test fixture: ", err)
+		}
+	}()
+
+	tfCtx, cancel := tf.ReserveForClose(fullCtx)
+	defer cancel()
+
+	ap, err := tf.DefaultOpenNetworkAP(tfCtx)
+	if err != nil {
+		s.Fatal("Failed to configure the AP: ", err)
+	}
+	defer func() {
+		if err := tf.DeconfigAP(tfCtx, ap); err != nil {
+			s.Error("Failed to deconfig the AP: ", err)
+		}
+	}()
+	ctx, cancel := tf.ReserveForDeconfigAP(tfCtx, ap)
+	defer cancel()
+	s.Log("AP setup done")
+
+	ssid := ap.Config().Ssid
+	freq, err := hostapd.ChannelToFrequency(ap.Config().Channel)
+	if err != nil {
+		s.Logf("Failed to convert channel %d to frequency: %s", ap.Config().Channel, err)
+	}
+	iface, err := tf.ClientInterface(ctx)
+	if err != nil {
+		s.Log("Failed to get DUT's interface: ", err)
+	}
+	iwr := remote_iw.NewRunner(s.DUT().Conn())
+
+	pv := perf.NewValues()
+	defer func() {
+		if err := pv.Save(s.OutDir()); err != nil {
+			s.Log("Failed to save perf data: ", err)
+		}
+	}()
+
+	logDuration := func(label string, duration time.Duration) {
+		pv.Set(perf.Metric{
+			Name:      label,
+			Unit:      "seconds",
+			Direction: perf.SmallerIsBetter,
+		}, duration.Seconds())
+		s.Logf("%s: %s", label, duration)
+	}
+
+	pollTimedScan := func(ctx context.Context, freqs []int) (time.Duration, error) {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+		var scanResult *iw.TimedScanData
+		err := testing.Poll(ctx, func(ctx context.Context) error {
+			var err error
+			scanResult, err = iwr.TimedScan(ctx, iface, freqs, []string{ssid})
+			if err != nil {
+				return err
+			}
+			for _, bssList := range scanResult.BSSList {
+				if bssList.SSID == ssid {
+					return nil
+				}
+			}
+			return errors.Errorf("iw scan found no SSID %s", ssid)
+		}, &testing.PollOptions{Timeout: 10 * time.Second, Interval: 200 * time.Millisecond})
+		if err != nil {
+			return 0, err
+		}
+		return scanResult.Time, nil
+	}
+
+	// Single channel scan.
+	if duration, err := pollTimedScan(ctx, []int{freq}); err != nil {
+		s.Errorf("Failed to perform single channel scan at frequency %d: %s", freq, err)
+	} else {
+		logDuration("scan_time_foreground_single_scan", duration)
+	}
+
+	// Foreground full scan.
+	if duration, err := pollTimedScan(ctx, nil); err != nil {
+		s.Errorf("Failed to perform full channel scan at frequency %d: %s", freq, err)
+	} else {
+		logDuration("scan_time_foreground_full", duration)
+	}
+
+	// Background full scan.
+	// DUT connecting to the AP.
+	if err := tf.ConnectWifi(ctx, ap); err != nil {
+		s.Fatal("DUT: failed to connect to WiFi: ", err)
+	}
+	defer func() {
+		if _, err := tf.WifiClient().DeleteEntriesForSSID(ctx, &network.DeleteEntriesForSSIDRequest{Ssid: ssid}); err != nil {
+			s.Errorf("Failed to remove entries for ssid=%s, err: %v", ap.Config().Ssid, err)
+		}
+	}()
+	s.Log("Connected")
+	if duration, err := pollTimedScan(ctx, nil); err != nil {
+		s.Errorf("Failed to perform full channel scan at frequency %d: %s", freq, err)
+	} else {
+		logDuration("scan_time_foreground_full", duration)
+	}
+}
