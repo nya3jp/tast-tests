62ef48a0e5c8ddbe11ac9cdf8653c096bf94d298
Charles William Dick <cwd@google.com>
2020-05-25 17:12:39 +0900

    WIP arc.LifecycleAndroidPerf test
    Introduce a test that launches multiple Android processes that allocate
    memory, and collect statistics on how many of them are killed.
    
    BUG=b:136696325
    TEST=tast run <DUT> arc.LifecycleAndroidPerf.*
    
    Change-Id: Id2d968d03e1052a1f0ee46fdc23d4936c13c4d2e

---
+++ android/ArcMemoryAllocatorTest/src/org/chromium/arc/testapp/memoryallocator/AllocateActivity2.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.memoryallocator;
+
+import android.os.Bundle;
+
+public class AllocateActivity2 extends AllocateActivityBase {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    onCreateHelper(2);
+  }
+}
