268120179dbdded68771ef4d88e77bb4f1482a06
Jesse Schettler <jschettler@chromium.org>
2020-05-26 10:36:37 -0600

    tast-tests: Log loading text error without failing
    For some devices, print preview loads too fast for tast to find the
    print preview loading text. Log this error without failing the test.
    
    BUG=b:157473323
    TEST=tast run --build $DUT_IP arc.Print
    
    Change-Id: I1b36514e58eaa776c979167a7159dde21b6ff9b7

--- src/chromiumos/tast/local/bundles/cros/arc/print.go
+++ src/chromiumos/tast/local/bundles/cros/arc/print.go
@@ -39,12 +39,11 @@ func waitForPrintPreview(ctx context.Context, tconn *chrome.TestConn) error {
 	params := ui.FindParams{
 		Name: "Loading preview",
 	}
-	// Wait for the loading text to appear to indicate print preview has
-	// launched and is loading. There should be a sufficient amount of time
-	// between the text appearing and being removed, but the test may fail here
-	// if the text is removed too quickly.
+	// Wait for the loading text to appear to indicate print preview is loading.
+	// Since print preview can finish loading before the loading text is found,
+	// log the error without failing the test.
 	if err := ui.WaitUntilExists(ctx, tconn, params, 10*time.Second); err != nil {
-		return errors.Wrap(err, "failed to find loading text")
+		testing.ContextLog(ctx, "Did not find loading text: ", err)
 	}
 	// Wait for the loading text to be removed to indicate print preview is no
 	// longer loading.
