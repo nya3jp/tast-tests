1990e4b37be37d890a3dce8713b4fdd91fa5abe9
Jun Sheng <shengjun@google.com>
2020-05-27 08:03:21 +1000

    tast-tests: Add sanity check on float VK
    Add new test to check drag and resize function.
    New precondition for vk.
    
    BUG=b:157099240
    TEST=Ran all the box tests
    
    Change-Id: I98b7e2ffa81699a8301f8aec1a7b010872cccb04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2211108
    Auto-Submit: Jun Sheng <shengjun@chromium.org>
    Commit-Queue: Jun Sheng <shengjun@chromium.org>
    Tested-by: Jun Sheng <shengjun@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- src/chromiumos/tast/local/chrome/pre.go
+++ src/chromiumos/tast/local/chrome/pre.go
@@ -32,6 +32,9 @@ const resetTimeout = 15 * time.Second
 // The Chrome instance is also shared and cannot be closed by tests.
 func LoggedIn() testing.Precondition { return loggedInPre }
 
+// VKEnabled creates a new precondition can be shared by tests that require an already-started Chromeobject that enables virtual keyboard.
+func VKEnabled() testing.Precondition { return vkEnabledPre }
+
 // NewPrecondition creates a new precondition that can be shared by tests
 // that require an already-started Chrome object that was created with opts.
 // suffix is appended to precondition's name.
@@ -44,6 +47,8 @@ func NewPrecondition(suffix string, opts ...Option) testing.Precondition {
 }
 
 var loggedInPre = NewPrecondition("logged_in")
+
+var vkEnabledPre = NewPrecondition("virtual_keyboard_enabled", ExtraArgs("--enable-virtual-keyboard"))
 
 // preImpl implements both testing.Precondition and testing.preconditionImpl.
 type preImpl struct {
