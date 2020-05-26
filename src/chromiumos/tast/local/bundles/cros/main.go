14fe4b71691a90faedb30c73c09daa4976f6dc60
Fletcher Woodruff <fletcherw@chromium.org>
2020-05-26 17:39:47 -0600

    scanner: add ScanESCLIPP test
    Create a new tast test component, 'scanner', and add a new test,
    ScanESCLIPP, which tests scanning an image via lorgnette and the airscan
    SANE backend. The scanner is emulated using virtual-usb-printer.
    
    BUG=b:153544294
    TEST=run scanner.ScanESCLIPP test
    
    Change-Id: I783737365f6745de9827634ac9c177e7da208af3

--- src/chromiumos/tast/local/bundles/cros/main.go
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -40,6 +40,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/power"
 	_ "chromiumos/tast/local/bundles/cros/printer"
 	_ "chromiumos/tast/local/bundles/cros/qemu"
+	_ "chromiumos/tast/local/bundles/cros/scanner"
 	_ "chromiumos/tast/local/bundles/cros/security"
 	_ "chromiumos/tast/local/bundles/cros/session"
 	_ "chromiumos/tast/local/bundles/cros/ui"
