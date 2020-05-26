51f64848904c4370890528c410fb6ee400bd90e8
Greg Edelston <gredelston@google.com>
2020-05-26 16:04:21 -0600

    Servo: Get & set the USB key state
    Servo contains a USB multiplexer ("mux") which, if powered, points to
    either the servo's USB port or the DUT's USB port.
    This CL introduces three USB states to the servo package: USBKeyDUT,
    USBKeyHost, and USBKeyOff.
    It also introduces getter and setter methods for those states, which
    involve getting and setting multiple servod controls.
    
    BUG=b:155425293
    TEST=Manual: Cycle through the states, first calling SetUSBKeyState and
    then calling GetUSBKeyState to verify.
    
    Change-Id: I4aec4875ea02700366a6859417afc3ea52f643d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2216814
    Tested-by: Greg Edelston <gredelston@google.com>
    Commit-Queue: Greg Edelston <gredelston@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- src/chromiumos/tast/remote/servo/methods.go
+++ src/chromiumos/tast/remote/servo/methods.go
@@ -6,8 +6,10 @@ package servo
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
 )
 
 // A StringControl contains the name of a gettable/settable Control which takes a string value.
@@ -15,9 +17,11 @@ type StringControl string
 
 // These are the Servo controls which can be get/set with a string value.
 const (
-	ActiveChgPort StringControl = "active_chg_port"
-	DUTVoltageMV  StringControl = "dut_voltage_mv"
-	FWWPState     StringControl = "fw_wp_state"
+	ActiveChgPort        StringControl = "active_chg_port"
+	DUTVoltageMV         StringControl = "dut_voltage_mv"
+	FWWPState            StringControl = "fw_wp_state"
+	ImageUSBKeyDirection StringControl = "image_usbkey_direction"
+	ImageUSBKeyPwr       StringControl = "image_usbkey_pwr"
 )
 
 // A KeypressControl is a special type of Control which can take either a numerical value or a KeypressDuration.
@@ -46,6 +50,16 @@ const (
 	DurTab       KeypressDuration = "tab"
 	DurPress     KeypressDuration = "press"
 	DurLongPress KeypressDuration = "long_press"
+)
+
+// A USBMuxState indicates whether the servo's USB mux is on, and if so, which direction it is powering.
+type USBMuxState string
+
+// These are the possible states of the USB mux.
+const (
+	USBMuxOff  USBMuxState = "off"
+	USBMuxDUT  USBMuxState = "dut_sees_usbkey"
+	USBMuxHost USBMuxState = "servo_sees_usbkey"
 )
 
 // Echo calls the Servo echo method.
@@ -90,8 +104,10 @@ func (s *Servo) GetServoVersion(ctx context.Context) (string, error) {
 // GetString returns the value of a specified control.
 func (s *Servo) GetString(ctx context.Context, control StringControl) (string, error) {
 	var value string
-	err := s.run(ctx, newCall("get", string(control)), &value)
-	return value, err
+	if err := s.run(ctx, newCall("get", string(control)), &value); err != nil {
+		return "", errors.Wrapf(err, "getting value for servo control %q", control)
+	}
+	return value, nil
 }
 
 // SetString sets a specified control to a specified value.
@@ -101,8 +117,10 @@ func (s *Servo) SetString(ctx context.Context, control StringControl, value stri
 	// However, reading this value is unnecessary, because if the call fails
 	// then run() returns an error anyway.
 	var val bool
-	err := s.run(ctx, newCall("set", string(control), value), &val)
-	return err
+	if err := s.run(ctx, newCall("set", string(control), value), &val); err != nil {
+		return errors.Wrapf(err, "setting servo control %q to %q", control, value)
+	}
+	return nil
 }
 
 // SetStringAndCheck sets a control to a specified value, and then verifies that it was set correctly.
@@ -121,4 +139,56 @@ func (s *Servo) SetStringAndCheck(ctx context.Context, control StringControl, va
 // KeypressWithDuration sets a KeypressControl to a KeypressDuration value.
 func (s *Servo) KeypressWithDuration(ctx context.Context, control KeypressControl, value KeypressDuration) error {
 	return s.SetString(ctx, StringControl(control), string(value))
+}
+
+// GetUSBMuxState determines whether the servo USB mux is on, and if so, which direction it is pointed.
+func (s *Servo) GetUSBMuxState(ctx context.Context) (USBMuxState, error) {
+	pwr, err := s.GetString(ctx, ImageUSBKeyPwr)
+	if err != nil {
+		return "", err
+	}
+	if pwr == string(USBMuxOff) {
+		return USBMuxOff, nil
+	}
+	direction, err := s.GetString(ctx, ImageUSBKeyDirection)
+	if err != nil {
+		return "", err
+	}
+	if direction != string(USBMuxDUT) && direction != string(USBMuxHost) {
+		return "", errors.Errorf("%q has an unknown value: %q", ImageUSBKeyDirection, direction)
+	}
+	return USBMuxState(direction), nil
+}
+
+// SetUSBMuxState switches the servo's USB mux to the specified power/direction state.
+func (s *Servo) SetUSBMuxState(ctx context.Context, value USBMuxState) error {
+	curr, err := s.GetUSBMuxState(ctx)
+	if err != nil {
+		return errors.Wrap(err, "getting servo usb state")
+	}
+	if curr == value {
+		return nil
+	}
+	if value == USBMuxOff {
+		return s.SetString(ctx, ImageUSBKeyPwr, string(value))
+	}
+	// Servod ensures the following:
+	// * The port is power cycled if it is changing directions
+	// * The port ends up in a powered state after this call
+	// * If facing the host side, the call only returns once a USB device is detected, or after a generous timeout (10s)
+	if err := s.SetString(ctx, ImageUSBKeyDirection, string(value)); err != nil {
+		return err
+	}
+	// Because servod makes no guarantees when switching to the DUT side,
+	// add a detection delay here when facing the DUT.
+	// Polling until GetUSBMuxState returns USBMuxDUT is not sufficient, because
+	// servod will return USBMuxDUT immediately. We need to wait to ensure that the DUT
+	// has had time to enumerate the USB.
+	// TODO(b/157751281): Clean this up once servo_v3 has been removed.
+	if value == USBMuxDUT {
+		if err := testing.Sleep(ctx, 5*time.Second); err != nil {
+			return errors.Wrap(err, "sleeping after switching usbkey direction")
+		}
+	}
+	return nil
 }
