2a1e778a4939aff1e5d42cd3080d1322b47dcd3f
Yuichiro Hanada <yhanada@google.com>
2020-05-27 21:33:41 +0900

    [WIP] Add arc.PreImeKeyEvent test.
    This tast test checks whether View.onKeyPreIme() API works correctly on
    ARC++ apps. The support is currently under development behind
    "ArcPreImeKeyEventSupport" feature flag, so the test needs to append the
    feature flag for now.
    
    BUG=b:148193316
    TEST=tast run arc.PreImeKeyEvent
    
    Change-Id: I7f0571a2c1181eed2fd6a2b0510fa5fcc44d3bc7

---
+++ android/ArcKeyboardTest/src/org/chromium/arc/testapp/keyboard/CaptureKeyPreImeView.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.keyboard;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+class CaptureKeyPreImeView extends EditText {
+    private TextView mLastPreImeKey;
+    private TextView mLastKeyDown;
+    private boolean mConsumeEvents = false;
+
+    public CaptureKeyPreImeView(Context context) {
+        super(context);
+    }
+
+    public CaptureKeyPreImeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CaptureKeyPreImeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    void setLastKeyEventLabels(TextView lastPreImeKey, TextView lastKeyDown) {
+        mLastPreImeKey = lastPreImeKey;
+        mLastKeyDown = lastKeyDown;
+    }
+
+    void startConsumingEvents() {
+        mConsumeEvents = true;
+    }
+
+    @Override
+    public boolean onKeyPreIme(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            mLastPreImeKey.setText("key down: keyCode=" + keyCode);
+        }
+        return mConsumeEvents;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        mLastKeyDown.setText("key down: keyCode=" + keyCode);
+        return true;
+    }
+}
