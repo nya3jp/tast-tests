52a03b0da2c791ba8dcf0a3641909cfea2b0d40a
Fletcher Woodruff <fletcherw@chromium.org>
2020-05-27 14:30:18 -0600

    printer: promote printer.PrintUSB to CQ
    Since the PrintUSB test has been consistently stable, remove the
    informational attribute and promote it to the CQ.
    
    BUG=chromium:1083427
    TEST=run test
    
    Change-Id: I1dafe02ab9c00544c94885066e19cd2d97f9a534

--- src/chromiumos/tast/local/bundles/cros/printer/print_usb.go
+++ src/chromiumos/tast/local/bundles/cros/printer/print_usb.go
@@ -20,7 +20,7 @@ func init() {
 		Func:         PrintUSB,
 		Desc:         "Tests that USB print job can be successfully sent",
 		Contacts:     []string{"valleau@chromium.org"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome", "cups", "virtual_usb_printer"},
 		Data: []string{"print_usb_ps.ppd.gz", "print_usb_to_print.pdf",
 			"print_usb_golden.ps"},
