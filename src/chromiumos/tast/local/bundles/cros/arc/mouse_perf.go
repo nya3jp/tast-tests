846f77ec64a1eac6eea256c0860bc5451fbc8e4e
Ben Lin <linben@google.com>
2020-05-28 13:58:43 -0700

    Refactor ARC PIP Tast tests.
    This CL does a couple of things:
    - Separate P and R implementations of SetWindowState, MoveWindow and
    Resize Window
    - Use ash API for minimizing into PIP
    - Use mouse API for move and resize in R
    - Disable a couple of tests for ARCVM since they are not passing yet
    
    BUG=b:156685602
    BUG=b:161835330
    TEST=tast run arc.PIP.vm
    TEST=tast run arc.PIP.vm_unstable
    
    Change-Id: I8f1f8b4b02e329460aad1d504c04a8836add2192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2220491
    Tested-by: Jorge Gil <jorgegil@google.com>
    Commit-Queue: Jorge Gil <jorgegil@google.com>
    Reviewed-by: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Ricardo Quesada <ricardoq@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/mouse_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/mouse_perf.go
@@ -74,7 +74,7 @@ func MousePerf(ctx context.Context, s *testing.State) {
 	}
 	defer act.Stop(ctx, tconn)
 
-	if err := act.SetWindowState(ctx, arc.WindowStateFullscreen); err != nil {
+	if err := act.SetWindowState(ctx, tconn, arc.WindowStateFullscreen); err != nil {
 		s.Fatal("Could not maximize test app: ", err)
 	}
 
