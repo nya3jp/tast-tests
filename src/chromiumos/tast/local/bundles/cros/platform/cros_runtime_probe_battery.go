083687e9cd8eed2dd9c9b214fd57df78ef4511e1
Clark Chung <clarkchung@google.com>
2020-05-28 13:10:55 +0800

    tast-tests: return model name in parsing function
    Since comp_group needs model name to construct identifier to compare
    with the matched identifier of probed components, the parsing functions
    (batteryNames/networkNameMapping/storageNames) will also return the
    parsed model name as well.
    
    BUG=b:155030911
    TEST=tast run $DUT platform.CrosRuntimeProbe*  # with components
         having information->comp_group in HWID DB
    
    Change-Id: I2c667f84c3fe21558203424eafccfa3fe85d30dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2217654
    Tested-by: Clark Chung <ckclark@chromium.org>
    Commit-Queue: Clark Chung <ckclark@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/platform/cros_runtime_probe_battery.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_runtime_probe_battery.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 
 	rppb "chromiumos/system_api/runtime_probe_proto"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/platform/runtimeprobe"
 	"chromiumos/tast/testing"
 )
@@ -32,14 +33,15 @@ func init() {
 // batteryNames will extract the model and battery names (prefixed with
 // "hwid_component:battery/") from autotest_host_info_labels var which is a json string of list of cros-labels.
 // After collecting battery names, this function will return a set containing them.
-func batteryNames(jsonStr string) (map[string]struct{}, error) {
+// Since we need the model name for component group, here we return it as well.
+func batteryNames(jsonStr string) (map[string]struct{}, string, error) {
 	const (
 		batteryPrefix = "hwid_component:battery/"
 		modelPrefix   = "model:"
 	)
 	var labels []string
 	if err := json.Unmarshal([]byte(jsonStr), &labels); err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	// Filter labels with prefix "hwid_component:battery/" and trim them.
 	// Also find the model name of this DUT.
@@ -53,12 +55,15 @@ func batteryNames(jsonStr string) (map[string]struct{}, error) {
 			model = strings.TrimPrefix(label, modelPrefix)
 		}
 	}
+	if len(model) == 0 {
+		return nil, "", errors.New("no model found")
+	}
 	set := make(map[string]struct{})
 	for _, label := range names {
 		set[model+"_"+label] = struct{}{}
 	}
 
-	return set, nil
+	return set, model, nil
 }
 
 // CrosRuntimeProbeBattery checks if the battery names in cros-label are consistent with probed names from runtime_probe
@@ -68,7 +73,7 @@ func CrosRuntimeProbeBattery(ctx context.Context, s *testing.State) {
 		s.Fatal("No battery labels")
 	}
 
-	set, err := batteryNames(labelsStr)
+	set, model, err := batteryNames(labelsStr)
 	if err != nil {
 		s.Fatal("Unable to decode autotest_host_info_labels: ", err)
 	}
@@ -92,7 +97,7 @@ func CrosRuntimeProbeBattery(ctx context.Context, s *testing.State) {
 		name := component.GetName()
 		if info := component.GetInformation(); info != nil {
 			if compGroup := info.GetCompGroup(); compGroup != "" {
-				name = compGroup
+				name = model + "_" + compGroup
 			}
 		}
 		if name == "generic" {
