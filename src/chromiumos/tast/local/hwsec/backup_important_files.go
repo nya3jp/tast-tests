290db40e2fbfa75ddd2986232fdffd27442f52c5
Leo Lai <cylai@google.com>
2020-05-28 11:02:26 +0800

    tast-tests: hwsec: functions that backup/restore tpm manager data.
    This will used to back up tpm manager local data in ready.go and used to
    rewind the state machine of tpm manager whenever necessary.
    
    In order to test this file, the child CLs are necessary. Th validation
    is done by observing if the wiped owner password can actually come back
    by restoring the snapshot at the beginning of a test. In this case, we
    choose hwsec.AttestationNoExternalServer to see if the enrollment status
    gets wiped and performs enrollment again. Also, we clear the owner
    password between 2 tast runs and confirm that the second run can
    recover the tpm local data from the backup by the first run.
    
    BUG=b:154672602
    TEST=see above.
    
    Change-Id: Ie4406ea577bbbd726f2930f57118d64c3f22864a

---
+++ src/chromiumos/tast/local/hwsec/backup_important_files.go
@@ -0,0 +1,50 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/fsutil"
+	"chromiumos/tast/local/testexec"
+)
+
+const tpmManagerLocalDataBackupPath = "/tmp/tast-system-backup-local_tpm_data"
+
+// isTpmLocalDataIntact uses tpm_manager_client to check if local data still contains owner password,
+// which means the set of imporant secrets are still intact.
+func isTpmLocalDataIntact(ctx context.Context) (bool, error) {
+	out, err := testexec.CommandContext(ctx, "tpm_manager_client", "status").Output()
+	if err != nil {
+		return false, errors.Wrap(err, "failed to call tpm_manager_client")
+	}
+	return strings.Contains(string(out), "owner_password"), nil
+}
+
+// BackupTpmManagerDataIfIntact backs up a the tpm manager data if the important secrets is not cleared.
+func BackupTpmManagerDataIfIntact(ctx context.Context) error {
+	ok, err := isTpmLocalDataIntact(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to check tpm local data")
+	}
+	if !ok {
+		return errors.New("owner password not found")
+	}
+	if err := fsutil.CopyFile(hwsec.TpmManagerLocalDataPath, tpmManagerLocalDataBackupPath); err != nil {
+		return errors.Wrap(err, "failed to copy tpm manager local data")
+	}
+	return nil
+}
+
+// RestoreTpmManagerData copies the backup file back to the location of tpm manager local data.
+func RestoreTpmManagerData(ctx context.Context) error {
+	if err := fsutil.CopyFile(tpmManagerLocalDataBackupPath, hwsec.TpmManagerLocalDataPath); err != nil {
+		return errors.Wrap(err, "failed to copy tpm manager local data backup")
+	}
+	return nil
+}
