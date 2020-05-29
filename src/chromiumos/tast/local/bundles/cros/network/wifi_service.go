2652794aaaab67d88492d8ee12006f23faf7f242
Shuo-Peng Liao <deanliao@chromium.org>
2020-05-29 12:23:49 +0800

    wifi: Add wpacli.ClearBlacklist() and runs it in InitDUT.
    wpacli.ClearBlacklist() clears wpa_supplicant's blacklist.
    It should be called in InitDUT() to clear the DUT's state.
    
    BUG=b:156249659
    TEST=tast run -var "router=$ROUTER" -var "pcap=$PCAP" $DUT
    wifi.SimpleConnect.*
    
    Change-Id: I86421cf63ec630a928b63711655f44b3dfb13026

--- src/chromiumos/tast/local/bundles/cros/network/wifi_service.go
+++ src/chromiumos/tast/local/bundles/cros/network/wifi_service.go
@@ -17,7 +17,9 @@ import (
 	"google.golang.org/grpc"
 
 	"chromiumos/tast/common/network/protoutil"
+	"chromiumos/tast/common/network/wpacli"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/network/cmd"
 	"chromiumos/tast/local/shill"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/services/cros/network"
@@ -86,6 +88,11 @@ func (s *WifiService) reinitTestState(ctx context.Context, m *shill.Manager) err
 	// Push the test profile.
 	if _, err := m.PushProfile(ctx, wifiTestProfileName); err != nil {
 		return errors.Wrap(err, "failed to push the test profile")
+	}
+	// Clean up wpa_supplicant blacklist in case some BSSID cannot be scanned.
+	// See https://crrev.com/c/219844.
+	if err := wpacli.NewRunner(&cmd.LocalCmdRunner{}).ClearBlacklist(ctx); err != nil {
+		return errors.Wrap(err, "failed to clear wpa_supplicant blacklist")
 	}
 	return nil
 }
