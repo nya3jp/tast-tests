3b8919f18480f690823c0b1533ea0aec7e8e2b29
Greg Edelston <gredelston@google.com>
2020-05-29 13:41:00 -0600

    firmware: Add a function to safely poweroff DUT
    Firmware tests often need to poweroff a DUT as an intermediate step
    toward mode-aware reboots, such as booting into recovery mode.
    It is safer to do this with the `poweroff` command, rather than the more
    intrusive servod power_state:off.
    
    BUG=b:155425293
    TEST=manual:
    1. Change poweroff to Poweroff so that it's exported.
    2. Call Poweroff from a test, catching any errors.
    3. Ping the DUT to verify it's down (even though that should be handled
       by the WaitUnreachable.
    
    Change-Id: Ifde569fabbe3dc4266b399c376f05dace906de24

--- src/chromiumos/tast/remote/firmware/boot_mode.go
+++ src/chromiumos/tast/remote/firmware/boot_mode.go
@@ -10,6 +10,7 @@ This file implements functions to check or switch the DUT's boot mode.
 
 import (
 	"context"
+	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 
@@ -17,6 +18,7 @@ import (
 	"chromiumos/tast/dut"
 	"chromiumos/tast/errors"
 	fwpb "chromiumos/tast/services/cros/firmware"
+	"chromiumos/tast/testing"
 )
 
 // CheckBootMode forwards to the CheckBootMode RPC to check whether the DUT is in a specified boot mode.
@@ -51,4 +53,19 @@ func RebootToMode(ctx context.Context, d *dut.DUT, utils fwpb.UtilsServiceClient
 	default:
 		return errors.Errorf("unsupported firmware boot mode transition to %s", toMode)
 	}
+}
+
+// poweroff safely powers off the DUT with the "poweroff" command, then waits for the DUT to be unreachable.
+func poweroff(ctx context.Context, d *dut.DUT) error {
+	testing.ContextLog(ctx, "Powering off DUT")
+	poweroffCtx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	d.Command("poweroff").Run(poweroffCtx) // ignore the error
+
+	offCtx, cancel := context.WithTimeout(ctx, 3*time.Minute)
+	defer cancel()
+	if err := d.WaitUnreachable(offCtx); err != nil {
+		return errors.Wrap(err, "waiting for dut to be unreachable after sending poweroff command")
+	}
+	return nil
 }
