0a31be4a3a7daa5a11f71b5d85da4e459f5c5647
Brian Malcolm <bmalcolm@chromium.org>
2020-05-29 09:15:49 -0700

    tast tests for PPD Pin support for Ricoh Printers
    These tests validate that Change-Id: If2069b29fab2e7026f76c871702815018d64bd47Ieeca1c13dac5f7f951b99c3de01979c525099891 works as expected.
    
    BUG=chromium:1082308
    TEST=tast run printer.PinPrint*
    
    Change-Id: I6f585c69408e4dbb1d7c791435d2fe90259791cb

---
+++ src/chromiumos/tast/local/bundles/cros/printer/pin_print_ricoh.go
@@ -0,0 +1,77 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package printer
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/printer/pinprint"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+type pinPrintParams struct {
+	ppdFile    string
+	printFile  string
+	goldenFile string
+	diffFile   string
+	options    string
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PinPrintRicoh,
+		Desc: "Verifies that printers with Ricoh Pin printing support add the appropriate options for a variety of attributes",
+		Contacts: []string{
+			"bmalcolm@chromium.org",
+			"cros-printing-dev@chromium.org",
+		},
+		SoftwareDeps: []string{"chrome", "cups"},
+		Data:         []string{"to_print.pdf", "printer_pin_print_Ricoh_JobPassword.ppd", "printer_pin_print_Ricoh_LockedPrintPassword.ppd", "printer_pin_print_Ricoh_password.ppd"},
+		Params: []testing.Param{{
+			Name:      "jobpassword_no_pin",
+			Val:       pinPrintParams{ppdFile: "printer_pin_print_Ricoh_JobPassword.ppd", printFile: "to_print.pdf", goldenFile: "printer_pin_print_ricoh_JobPassword_no_pin_golden.ps", diffFile: "jobpassword_no-pin_diff.txt", options: ""},
+			ExtraData: []string{"printer_pin_print_ricoh_JobPassword_no_pin_golden.ps"},
+			ExtraAttr: []string{"group:mainline", "informational"},
+			Pre:       chrome.LoggedIn(),
+		}, {
+			Name:      "jobpassword_pin",
+			Val:       pinPrintParams{ppdFile: "printer_pin_print_Ricoh_JobPassword.ppd", printFile: "to_print.pdf", goldenFile: "printer_pin_print_ricoh_JobPassword_pin_golden.ps", diffFile: "jobpassword_pin_diff.txt", options: "job-password=1234"},
+			ExtraData: []string{"printer_pin_print_ricoh_JobPassword_pin_golden.ps"},
+			ExtraAttr: []string{"group:mainline", "informational"},
+			Pre:       chrome.LoggedIn(),
+		}, {
+			Name:      "lockedprintpassword_no_pin",
+			Val:       pinPrintParams{ppdFile: "printer_pin_print_Ricoh_LockedPrintPassword.ppd", printFile: "to_print.pdf", goldenFile: "printer_pin_print_ricoh_LockedPrintPassword_no_pin_golden.ps", diffFile: "lockedprintpassword_no-pin_diff.txt", options: ""},
+			ExtraData: []string{"printer_pin_print_ricoh_LockedPrintPassword_no_pin_golden.ps"},
+			ExtraAttr: []string{"group:mainline", "informational"},
+			Pre:       chrome.LoggedIn(),
+		}, {
+			Name:      "lockedprintpassword_pin",
+			Val:       pinPrintParams{ppdFile: "printer_pin_print_Ricoh_LockedPrintPassword.ppd", printFile: "to_print.pdf", goldenFile: "printer_pin_print_ricoh_LockedPrintPassword_pin_golden.ps", diffFile: "lockedprintpassword_pin_diff.txt", options: "job-password=1234"},
+			ExtraData: []string{"printer_pin_print_ricoh_LockedPrintPassword_pin_golden.ps"},
+			ExtraAttr: []string{"group:mainline", "informational"},
+			Pre:       chrome.LoggedIn(),
+		}, {
+			Name:      "password_no_pin",
+			Val:       pinPrintParams{ppdFile: "printer_pin_print_Ricoh_password.ppd", printFile: "to_print.pdf", goldenFile: "printer_pin_print_ricoh_password_no_pin_golden.ps", diffFile: "password_no-pin_diff.txt", options: ""},
+			ExtraData: []string{"printer_pin_print_ricoh_password_no_pin_golden.ps"},
+			ExtraAttr: []string{"group:mainline", "informational"},
+			Pre:       chrome.LoggedIn(),
+		}, {
+			Name:      "password_pin",
+			Val:       pinPrintParams{ppdFile: "printer_pin_print_Ricoh_password.ppd", printFile: "to_print.pdf", goldenFile: "printer_pin_print_ricoh_password_pin_golden.ps", diffFile: "password_pin_diff.txt", options: "job-password=1234"},
+			ExtraData: []string{"printer_pin_print_ricoh_password_pin_golden.ps"},
+			ExtraAttr: []string{"group:mainline", "informational"},
+			Pre:       chrome.LoggedIn(),
+		}},
+	})
+}
+
+func PinPrintRicoh(ctx context.Context, s *testing.State) {
+	testOpt := s.Param().(pinPrintParams)
+
+	pinprint.Run(ctx, s, testOpt.ppdFile, testOpt.printFile, testOpt.goldenFile, testOpt.diffFile, testOpt.options)
+}
