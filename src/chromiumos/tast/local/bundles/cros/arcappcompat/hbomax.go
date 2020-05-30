490fd8a9bbb5011798c25fda24059a21d2295e7b
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-05-29 17:09:03 -0700

    Run UIAutomator Test Using Tast For Hbomax app
    BUG=b:154837257
    TEST=tast run <eve> arcappcompat.Hbomax
    
    Tast Test for Hbomax app verifies launch app
    from the playstore, verifies main activity page
    of the app. Also for Clamshell Mode it verifies
    Clamshell Full Screen, Minimize and Restore,
    Clamshell Resize, Reopen tests and for Touchview
    Mode it verifies Minimize and Restore, Reopen tests.
    
    Change-Id: Ifcf5aeb301aadb3ec1a527710f5ee33c1125dab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2222897
    Tested-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Commit-Queue: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/hbomax.go
@@ -0,0 +1,149 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForHbomax = []testutil.TestSuite{
+	{Name: "Launch app in Clamshell", Fn: launchAppForHbomax},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForHbomax = []testutil.TestSuite{
+	{Name: "Launch app in Touchview", Fn: launchAppForHbomax},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Hbomax,
+		Desc:         "Functional test for Hbomax that install, launch the app and check that the main page is open, also checks Hbomax correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForHbomax,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForHbomax,
+			},
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name: "vm",
+			Val: testutil.TestParams{
+				TabletMode: false,
+				Tests:      clamshellTestsForHbomax,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				TabletMode: true,
+				Tests:      touchviewTestsForHbomax,
+			},
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// Hbomax test uses library for opting into the playstore and installing app.
+// Checks Hbomax correctly changes the window states in both clamshell and touchview mode.
+func Hbomax(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.hbo.hbonow"
+		appActivity = "com.hbo.go.LaunchActivity"
+	)
+
+	// Step up chrome on Chromebook.
+	cr, tconn, a, d := testutil.SetUpDevice(ctx, s, appPkgName, appActivity)
+	defer d.Close()
+
+	testSet := s.Param().(testutil.TestParams)
+
+	// Run the different test cases.
+	for idx, test := range testSet.Tests {
+		// Run subtests.
+		s.Run(ctx, test.Name, func(ctx context.Context, s *testing.State) {
+
+			// Launch the app.
+			act, err := arc.NewActivity(a, appPkgName, appActivity)
+			if err != nil {
+				s.Fatal("Failed to create new app activity: ", err)
+			}
+			s.Log("Created new app activity")
+			defer act.Close()
+
+			if err := act.Start(ctx, tconn); err != nil {
+				s.Fatal("Failed to start app: ", err)
+			}
+			s.Log("App launched successfully")
+
+			defer act.Stop(ctx, tconn)
+
+			defer func() {
+				if s.HasError() {
+					path := fmt.Sprintf("%s/screenshot-arcappcompat-failed-test-%d.png", s.OutDir(), idx)
+					if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
+						s.Log("Failed to capture screenshot: ", err)
+					}
+				}
+			}()
+
+			testutil.DetectAndCloseCrashOrAppNotResponding(ctx, s, tconn, a, d, appPkgName)
+			test.Fn(ctx, s, tconn, a, d, appPkgName, appActivity)
+		})
+	}
+}
+
+// launchAppForHbomax verifies Hbomax is launched and
+// verify Hbomax reached main activity page of the app.
+func launchAppForHbomax(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		frameClassName = "android.widget.FrameLayout"
+		profileIconDes = "My Profile"
+	)
+
+	if currentAppPkg := testutil.CurrentAppPackage(ctx, s, d); currentAppPkg != appPkgName {
+		s.Fatal("Failed to launch the app: ", currentAppPkg)
+	}
+	s.Log("App is launched successfully in launchAppForHbomax")
+
+	// Check for profile icon.
+	profileIcon := d.Object(ui.ClassName(frameClassName), ui.Description(profileIconDes))
+	if err := profileIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("profileIcon doesn't exist: ", err)
+	}
+}
