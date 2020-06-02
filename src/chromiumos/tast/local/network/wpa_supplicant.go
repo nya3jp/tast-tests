d8fbac75275e7af761ae2588b3169a712420b355
Yen-lin Lai <yenlinlai@google.com>
2020-06-02 16:30:33 +0800

    network: Add gRPCs for MAC randomization and request active scans.
    Add gRPC methods for later RandomMACAddress test:
    - RequestScans for requesting and waiting for scans
    - SetMACRandomize for setting MAC randomization through shill
    - WaitScanIdle for waiting for existing scan to end
    
    RequestScans is the port of RandomMACAddressTestBase.request_scans but
    using a watcher of scanning status instead of simple sleep so that we
    can be more certain that expected number of scans are triggered. (but
    pcap might still miss some of them)
    
    BUG=b:158061732
    TEST=fast_build.sh -T
    
    Change-Id: Ib2384b1cd3289a9e60c14780ed1dab3408147d4e

---
+++ src/chromiumos/tast/local/network/wpa_supplicant.go
@@ -0,0 +1,10 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+// Constants for interacting with wpa_supplicant via dbus.
+const (
+	DBusWPASupplicantInterface = "fi.w1.wpa_supplicant1.Interface"
+)
