e948cc0c0388793a991f50ff3c748c5ad1c1b5c6
Hsin-chen Chuang <chharry@google.com>
2020-06-02 20:33:49 +0800

    wifi: Add DisableEnable test.
    This is migrated from network_WiFi_DisableEnable test in Autotest.
    
    BUG=b:156249124
    TEST=tast run ${DUT} wifi.DisableEnable
    
    Change-Id: Ibe51b83fd46db029da1568adc43e7b44dcfdb44a

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/disable_enable.go
@@ -0,0 +1,91 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        DisableEnable,
+		Desc:        "Tests that disabling and enabling WiFi re-connects the system",
+		Contacts:    []string{"chharry@google.com", "chromeos-platform-connectivity@google.com"},
+		Attr:        []string{"group:wificell", "wificell_cq", "wificell_unstable", "wificell_func"},
+		ServiceDeps: []string{"tast.cros.network.WifiService"},
+		Vars:        []string{"router"},
+	})
+}
+
+func DisableEnable(fullCtx context.Context, s *testing.State) {
+	router, _ := s.Var("router")
+	tf, err := wificell.NewTestFixture(fullCtx, fullCtx, s.DUT(), s.RPCHint(), wificell.TFRouter(router))
+	if err != nil {
+		s.Fatal("Failed to set up test fixture: ", err)
+	}
+	defer func() {
+		if err := tf.Close(fullCtx); err != nil {
+			s.Log("Failed to tear down test fixture: ", err)
+		}
+	}()
+
+	tfCtx, cancel := tf.ReserveForClose(fullCtx)
+	defer cancel()
+
+	ap, err := tf.DefaultOpenNetworkAP(tfCtx)
+	if err != nil {
+		s.Fatal("Failed to configure the AP: ", err)
+	}
+	defer func() {
+		if err := tf.DeconfigAP(tfCtx, ap); err != nil {
+			s.Error("Failed to deconfig the AP: ", err)
+		}
+	}()
+	ctx, cancel := tf.ReserveForDeconfigAP(tfCtx, ap)
+	defer cancel()
+	s.Log("AP setup done")
+
+	connRes, err := tf.ConnectWifiAP(ctx, ap)
+	if err != nil {
+		s.Fatal("Failed to connect to WiFi: ", err)
+	}
+	defer func() {
+		if err := tf.DisconnectWifi(fullCtx); err != nil {
+			s.Error("Failed to disconnect WiFi: ", err)
+		}
+		req := &network.DeleteEntriesForSSIDRequest{Ssid: []byte(ap.Config().SSID)}
+		if _, err := tf.WifiClient().DeleteEntriesForSSID(fullCtx, req); err != nil {
+			s.Errorf("Failed to remove entries for ssid=%s: %v", ap.Config().SSID, err)
+		}
+	}()
+	s.Log("Connected")
+
+	if err := tf.PingFromDUT(ctx, ap.ServerIP().String()); err != nil {
+		s.Fatal("Failed to ping from the DUT: ", err)
+	}
+
+	iface, err := tf.ClientInterface(ctx)
+	if err != nil {
+		s.Fatal("Failed to get interface from the DUT: ", err)
+	}
+
+	// Start disabling and enabling WiFi interface.
+	if _, err := tf.WifiClient().DisableEnableTest(ctx, &network.DisableEnableTestRequest{
+		InterfaceName: iface,
+		ServicePath:   connRes.ServicePath,
+	}); err != nil {
+		s.Fatal("DisableEnableTest failed: ", err)
+	}
+
+	// Verify the connection.
+	// Note that we should have re-connected to the correct WiFi service after the DisableEnableTest call above.
+	if err := tf.VerifyConnection(ctx, ap); err != nil {
+		s.Fatal("Failed to verify connection: ", err)
+	}
+}
