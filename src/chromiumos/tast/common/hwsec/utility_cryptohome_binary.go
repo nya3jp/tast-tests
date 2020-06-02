e7373651857f243b963c7699fbab2e7b17d42b69
John L Chen <zuan@chromium.org>
2020-06-03 05:36:58 +0800

    tast-tests: hwsec: Add GetSanitizedUsername() to UtilityCryptohomeBinary
    BUG=b:157851829
    TEST=tested with hwsec.SanitizedUsernameAndSalt CL.
    
    Change-Id: Ia875af666a1651652779429bbde7d65e2606d93c

--- src/chromiumos/tast/common/hwsec/utility_cryptohome_binary.go
+++ src/chromiumos/tast/common/hwsec/utility_cryptohome_binary.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -382,6 +383,27 @@ func (u *UtilityCryptohomeBinary) MountVault(ctx context.Context, username, pass
 		return errors.Wrap(err, "failed to mount")
 	}
 	return nil
+}
+
+// GetSanitizedUsername computes the sanitized username for the given user.
+// If useDBus is true, the sanitized username will be computed by cryptohome (through dbus). Otherwise, it'll be computed directly by libbrillo (without dbus).
+func (u *UtilityCryptohomeBinary) GetSanitizedUsername(ctx context.Context, username string, useDBus bool) (string, error) {
+	binaryOutput, err := u.binary.GetSanitizedUsername(ctx, username, useDBus)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to call GetSanitizedUsername")
+	}
+	output := string(binaryOutput)
+	output = strings.Trim(output, " \n")
+	// A proper sanitized username should be a hex string of length 40.
+	matched, err := regexp.MatchString("[a-f0-9]{40}", output)
+	if err != nil {
+		// My regex is bad, shouldn't happen.
+		panic("bad regexp")
+	}
+	if !matched {
+		return "", errors.Errorf("invalid sanitized username %q", output)
+	}
+	return output, nil
 }
 
 // CheckVault checks the vault via |CheckKeyEx| dbus mehod.
