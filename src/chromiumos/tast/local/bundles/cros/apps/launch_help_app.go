9b711fd9e81996d2fae2aaa0a4436676d3b22cf1
Jun Sheng <shengjun@google.com>
2020-06-04 07:44:32 +1000

    tast-tests: enable Help app sanity check in different mode
    1. Enable tests in different mode.
    2. Collect loadTimeData for reference.
    
    BUG=b:156433022
    TEST=Ran all the box tests
    
    Change-Id: I4ff4e2f0637e76d13af3c6c57202459d2debc3b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2227725
    Tested-by: Jun Sheng <shengjun@chromium.org>
    Reviewed-by: Ricardo Quesada <ricardoq@chromium.org>
    Commit-Queue: Jun Sheng <shengjun@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/apps/launch_help_app.go
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_help_app.go
@@ -6,8 +6,12 @@ package apps
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
 	"time"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
@@ -15,7 +19,11 @@ import (
 	"chromiumos/tast/testing"
 )
 
-// TODO(b/156433022): Parameterize test to run on both clamshell and tablet.
+// testParameters contains all the data needed to run a single test iteration.
+type testParameters struct {
+	tabletMode bool
+	oobe       bool
+}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -28,15 +36,57 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		Vars:         []string{"apps.LaunchHelpApp.consumer_username", "apps.LaunchHelpApp.consumer_password"},
 		SoftwareDeps: []string{"chrome"},
-	})
+		Params: []testing.Param{{
+			Val: testParameters{
+				tabletMode: false,
+				oobe:       true,
+			},
+		}, {
+			Name: "tablet_oobe",
+			Val: testParameters{
+				tabletMode: true,
+				oobe:       true,
+			},
+		}, {
+			Name: "clamshell_logged_in",
+			Val: testParameters{
+				tabletMode: false,
+				oobe:       false,
+			},
+			Pre: chrome.LoggedIn(),
+		}, {
+			Name: "tablet_logged_in",
+			Val: testParameters{
+				tabletMode: true,
+				oobe:       false,
+			},
+			Pre: chrome.LoggedIn(),
+		},
+		}})
 }
 
 // LaunchHelpApp verifies launching Showoff after OOBE.
 func LaunchHelpApp(ctx context.Context, s *testing.State) {
+	if s.Param().(testParameters).oobe {
+		helpAppLaunchDuringOOBE(ctx, s, s.Param().(testParameters).tabletMode)
+	} else {
+		helpAppLaunchAfterLogin(ctx, s, s.Param().(testParameters).tabletMode)
+	}
+}
+
+// helpAppLaunchDuringOOBE verifies help app launch during OOBE stage. Help app only launches with real user login in clamshell mode.
+func helpAppLaunchDuringOOBE(ctx context.Context, s *testing.State, isTabletMode bool) {
 	username := s.RequiredVar("apps.LaunchHelpApp.consumer_username")
 	password := s.RequiredVar("apps.LaunchHelpApp.consumer_password")
 
-	cr, err := chrome.New(ctx, chrome.Auth(username, password, ""), chrome.GAIALogin(), chrome.DontSkipOOBEAfterLogin())
+	extraArgs := ""
+	if isTabletMode {
+		extraArgs = "--force-tablet-mode=touch_view"
+	} else {
+		extraArgs = "--force-tablet-mode=clamshell"
+	}
+	cr, err := chrome.New(ctx, chrome.Auth(username, password, ""), chrome.GAIALogin(), chrome.DontSkipOOBEAfterLogin(), chrome.ExtraArgs(extraArgs))
+
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
@@ -46,15 +96,53 @@ func LaunchHelpApp(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to connect Test API: ", err)
 	}
 
-	tabletEnabled, err := ash.TabletModeEnabled(ctx, tconn)
+	// Verify HelpApp (aka Explore) launched in Clamshell mode only.
+	if err := assertHelpAppLaunched(ctx, s, tconn, cr, !isTabletMode); err != nil {
+		s.Fatalf("Failed to verify help app launching during oobe in tablet mode enabled(%v): %v", isTabletMode, err)
+	}
+}
+
+// helpAppLaunchAfterLogin verifies help app launch after user login. It should be able to launch on devices in both clamshell and tablet mode.
+func helpAppLaunchAfterLogin(ctx context.Context, s *testing.State, isTabletMode bool) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
-		s.Fatal("Failed to get current ui mode: ", err)
+		s.Fatal("Failed to connect Test API: ", err)
 	}
 
-	// Verify HelpApp (aka Explore) launched in Clamshell mode.
-	if !tabletEnabled {
-		if _, err = ui.FindWithTimeout(ctx, tconn, ui.FindParams{Name: apps.Help.Name}, 20*time.Second); err != nil {
-			s.Error("Failed to wait for Help app launched: ", err)
+	s.Logf("Ensure tablet mode enabled(%v)", isTabletMode)
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, isTabletMode)
+	if err != nil {
+		s.Fatalf("Failed to ensure tablet mode enabled(%v): %v", isTabletMode, err)
+	}
+	defer cleanup(ctx)
+
+	app := apps.Help
+	s.Logf("Launching %s", app.Name)
+	if err := apps.Launch(ctx, tconn, app.ID); err != nil {
+		s.Fatalf("Failed to launch %s: %v", app.Name, err)
+	}
+	if err := ash.WaitForApp(ctx, tconn, app.ID); err != nil {
+		s.Fatalf("%s did not appear in shelf after launch: %v", app.Name, err)
+	}
+
+	if err := assertHelpAppLaunched(ctx, s, tconn, cr, true); err != nil {
+		s.Fatal("Failed to verify help app launching after user logged in: ", err)
+	}
+}
+
+// assertHelpAppLaunched asserts help app to be launched or not
+func assertHelpAppLaunched(ctx context.Context, s *testing.State, tconn *chrome.TestConn, cr *chrome.Chrome, isLaunched bool) error {
+	if isLaunched {
+		helpAppNode, err := ui.FindWithTimeout(ctx, tconn, ui.FindParams{Name: apps.Help.Name}, 20*time.Second)
+		if err != nil {
+			return errors.Wrap(err, "failed to launch help app")
+		}
+
+		// Collect loadTimeData once launched.
+		if err := logRuntimeData(ctx, s, cr); err != nil {
+			return errors.Wrap(err, "failed to collect loadTimeData")
 		}
 
 		// Find Overview tab to verify app rendering.
@@ -62,18 +150,50 @@ func LaunchHelpApp(ctx context.Context, s *testing.State) {
 			Name: "Overview",
 			Role: ui.RoleTypeTab,
 		}
-		if _, err = ui.FindWithTimeout(ctx, tconn, params, 20*time.Second); err != nil {
-			s.Error("Failed to render Help app: ", err)
+		if _, err := helpAppNode.DescendantWithTimeout(ctx, params, 20*time.Second); err != nil {
+			return errors.Wrap(err, "failed to render help app")
 		}
 	} else {
-		// Verify HelpApp (aka Explore) not to launch in Tablet mode.
 		isHelpAppLaunched, err := ui.Exists(ctx, tconn, ui.FindParams{Name: apps.Help.Name})
 		if err != nil {
-			s.Error("Failed to check HelpApp existence: ", err)
+			return errors.Wrap(err, "failed to check HelpApp existence")
 		}
 
 		if isHelpAppLaunched {
-			s.Error("Help app is launched in Tablet mode")
+			return errors.New("Help app is launched in Tablet mode")
 		}
 	}
+	return nil
+}
+
+// logRuntimeData logs the window.loadTimeData() info to a file.
+func logRuntimeData(ctx context.Context, s *testing.State, cr *chrome.Chrome) error {
+	const (
+		logFileName = "loadTimeData.json"
+		helpAppURL  = "chrome-untrusted://help-app/app.html"
+	)
+	helpConn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(helpAppURL))
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to help app")
+	}
+
+	var loadTimeData interface{}
+	if err := helpConn.Eval(ctx, "window.loadTimeData", &loadTimeData); err != nil {
+		return err
+	}
+
+	file := filepath.Join(s.OutDir(), logFileName)
+	testing.ContextLogf(ctx, "Write loadTimeData into file: %s", file)
+
+	bContent, err := json.Marshal(loadTimeData)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(file, bContent, 0644)
+}
+
+// shouldLaunchHelp returns a result to launch help app or not.
+func shouldLaunchHelp(isTabletMode, isOOBE bool) bool {
+	return !isOOBE || !isTabletMode
 }
