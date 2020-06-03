15ee66b9d27a1a3f70be919c0341d56889ed0f64
Greg Edelston <gredelston@google.com>
2020-06-03 11:51:20 -0600

    servo: Add PowerState setter method
    In order to switch the firmware boot mode to Recovery mode (or
    recovery_force_mrc) and back to Normal, we need to be able to switch the
    DUT boot mode through servod.
    This CL adds that capability to servo/methods.go.
    
    BUG=b:155425293
    TEST=firmware.BootMode
    
    Change-Id: I010d7ad345b53bc6bb1a01b60a04ef7915478fd7

--- src/chromiumos/tast/remote/bundles/cros/firmware/boot_mode.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/boot_mode.go
@@ -6,11 +6,13 @@ package firmware
 
 import (
 	"context"
+	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 
 	fwCommon "chromiumos/tast/common/firmware"
 	"chromiumos/tast/remote/firmware"
+	"chromiumos/tast/remote/servo"
 	"chromiumos/tast/rpc"
 	fwpb "chromiumos/tast/services/cros/firmware"
 	"chromiumos/tast/testing"
@@ -25,6 +27,7 @@ func init() {
 		ServiceDeps:  []string{"tast.cros.firmware.UtilsService"},
 		SoftwareDeps: []string{"crossystem"},
 		Attr:         []string{"group:mainline", "informational"},
+		Vars:         []string{"servo"},
 	})
 }
 
@@ -77,5 +80,30 @@ func BootMode(ctx context.Context, s *testing.State) {
 			s.Fatalf("DUT was in %s after transition from %s to %s", fwCommon.BootModeFromProto[r.BootMode], fromMode, toMode)
 		}
 		fromMode = toMode
+	}
+
+	// Exercise PowerState, which will later be used during firmware.RebootToMode()
+	dut := s.DUT()
+	pxy, err := servo.NewProxy(ctx, s.RequiredVar("servo"), dut.KeyFile(), dut.KeyDir())
+	if err != nil {
+		s.Fatal("Failed to connect to servo: ", err)
+	}
+	defer pxy.Close(ctx)
+	svo := pxy.Servo()
+	if err := svo.SetPowerState(ctx, servo.PowerStateOff); err != nil {
+		s.Error("Error from setting power state to Off: ", err)
+	}
+	offCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	if err := dut.WaitUnreachable(offCtx); err != nil {
+		s.Fatal("Error during dut.WaitUnreachable: ", err)
+	}
+	if err := svo.SetPowerState(ctx, servo.PowerStateOn); err != nil {
+		s.Error("Error from setting power state to On: ", err)
+	}
+	onCtx, cancel := context.WithTimeout(ctx, time.Minute)
+	defer cancel()
+	if err := dut.WaitConnect(onCtx); err != nil {
+		s.Fatal("Error during dut.WaitConnect: ", err)
 	}
 }
