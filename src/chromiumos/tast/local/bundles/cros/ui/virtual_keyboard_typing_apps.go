1be5b832b674a4df7e3d0a18af0b4582805624ec
Jun Sheng <shengjun@google.com>
2020-06-03 22:25:46 +1000

    tast-tests: refactor test to check vk works in different apps
    1. Change ui mode to tablet as most virtual keyboard users are using tablet.
    2. Using tap event to trigger vk and input instead of calling private api
    3. Add new coverage on Settings app (more type of apps coming later)
    
    BUG=b:157871369
    TEST=Ran all the box tests
    
    Change-Id: I26ab65c3357b31425d319b6ff9e8c9af2bad507b

--- src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_typing_apps.go
+++ src/chromiumos/tast/local/bundles/cros/ui/virtual_keyboard_typing_apps.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,13 +6,10 @@ package ui
 
 import (
 	"context"
-	"fmt"
-	"io"
-	"net/http"
-	"net/http/httptest"
 	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/bundles/cros/ui/faillog"
 	"chromiumos/tast/local/bundles/cros/ui/pointer"
 	"chromiumos/tast/local/chrome"
@@ -24,8 +21,8 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         VirtualKeyboardTyping,
-		Desc:         "Checks that the virtual keyboard works in chrome",
+		Func:         VirtualKeyboardTypingApps,
+		Desc:         "Checks that the virtual keyboard works in apps",
 		Contacts:     []string{"essential-inputs-team@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
@@ -34,7 +31,7 @@ func init() {
 	})
 }
 
-func VirtualKeyboardTyping(ctx context.Context, s *testing.State) {
+func VirtualKeyboardTypingApps(ctx context.Context, s *testing.State) {
 	// typingKeys indicates a key series that tapped on virtual keyboard.
 	var typingKeys = []string{"h", "e", "l", "l", "o", "space", "t", "a", "s", "t"}
 
@@ -65,27 +62,26 @@ func VirtualKeyboardTyping(ctx context.Context, s *testing.State) {
 	}
 	defer pc.Close()
 
-	s.Log("Start a local server to test chrome")
-	const identifier = "e14s-inputbox"
-	html := fmt.Sprintf(`<input type="text" id="text" autocorrect="off" aria-label=%q/>`, identifier)
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/html")
-		io.WriteString(w, html)
-	}))
-	defer server.Close()
-
-	conn, err := cr.NewConn(ctx, server.URL)
-	if err != nil {
-		s.Fatal("Creating renderer for test page failed: ", err)
+	app := apps.Settings
+	s.Logf("Launching %s", app.Name)
+	if err := apps.Launch(ctx, tconn, app.ID); err != nil {
+		s.Fatalf("Failed to launch %s: %c", app.Name, err)
 	}
-	defer conn.Close()
-
-	element, err := ui.FindWithTimeout(ctx, tconn, ui.FindParams{Name: identifier}, 5*time.Second)
-	if err != nil {
-		s.Fatalf("Failed to find input element %s: %v", identifier, err)
+	if err := ash.WaitForApp(ctx, tconn, app.ID); err != nil {
+		s.Fatalf("%s did not appear in shelf after launch: %v", app.Name, err)
 	}
 
-	s.Log("Click input field to trigger virtual keyboard shown up")
+	s.Log("Find searchbox input element")
+	params := ui.FindParams{
+		Role: ui.RoleTypeSearchBox,
+		Name: "Search settings",
+	}
+	element, err := ui.FindWithTimeout(ctx, tconn, params, 3*time.Second)
+	if err != nil {
+		s.Fatal("Failed to find searchbox input field in settings: ", err)
+	}
+
+	s.Log("Click searchbox to trigger virtual keyboard")
 	if err := pointer.Click(ctx, pc, element.Location.CenterPoint()); err != nil {
 		s.Fatal("Failed to click the input element: ", err)
 	}
@@ -109,6 +105,6 @@ func VirtualKeyboardTyping(ctx context.Context, s *testing.State) {
 	}
 
 	if err := assertInputValue(ctx, element, expectedTypingResult); err != nil {
-		s.Error("Failed to assert input result: ", err)
+		s.Fatal("Failed to assert input result: ", err)
 	}
 }
