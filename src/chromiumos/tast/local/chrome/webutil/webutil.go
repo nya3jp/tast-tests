0b7bcf68ab86d3617bfb5940aa87b3011a9c3aad
Xiyuan Xia <xiyuan@google.com>
2020-06-03 14:33:33 -0700

    tast-tests: Add QuickCheckCUJ test
    QuickCheckCUJ that measures performance from screen unlock until
    opening an email thread in gmail.
    
    BUG=chromium:1062133
    TEST=tast run IP ui.QuickCheckCUJ
    
    Change-Id: I2c59cbf4be76252a5af3a85848fe6e31bd4779f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2230335
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Tested-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>

--- src/chromiumos/tast/local/chrome/webutil/webutil.go
+++ src/chromiumos/tast/local/chrome/webutil/webutil.go
@@ -26,3 +26,29 @@ func WaitForYoutubeVideo(ctx context.Context, conn *chrome.Conn, timeout time.Du
 		      bounds.width > 0 && bounds.height > 0;
 		})()`, timeout)
 }
+
+// WaitForQuiescence waits for the given chrome.Conn gets quiescence.
+func WaitForQuiescence(ctx context.Context, conn *chrome.Conn, timeout time.Duration) error {
+	// Each resourceTimings element contains the load start time and load end time
+	// for a resource.  If a load has not completed yet, the end time is set to
+	// the current time.  Then we can tell that a load has completed by detecting
+	// that the end time diverges from the current time.
+	//
+	// resourceTimings is sorted by event start time, so we need to look through
+	// the entire array to find the latest activity.
+	return conn.WaitForExprFailOnErrWithTimeout(ctx, `
+		(function() {
+			if (document.readyState !== 'complete') {
+				return false;
+			}
+
+			const QUIESCENCE_TIMEOUT_MS = 2000;
+			let lastEventTime = performance.timing.loadEventEnd -
+					performance.timing.navigationStart;
+			const resourceTimings = performance.getEntriesByType('resource');
+			lastEventTime = resourceTimings.reduce(
+					(current, timing) => Math.max(current, timing.responseEnd),
+					lastEventTime);
+			return performance.now() >= lastEventTime + QUIESCENCE_TIMEOUT_MS;
+		})()`, timeout)
+}
