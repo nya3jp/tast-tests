825a7b511f98877369a7c4a6f11e074b4f7dcf8e
Ben Reich <benreich@chromium.org>
2020-06-09 08:17:50 +0000

    tast-tests: Refactor Drivefs wait to lib
    Extract out the logic required to identify that
    Drivefs is stable to a library function to allow
    for reuse.
    
    BUG=chromium:1092805
    TEST=tast run <soraka/eve> platform.Drivefs
    
    Change-Id: Iac10dc7160d22aacd54e87e85a663dd63dabe054

---
+++ src/chromiumos/tast/local/drivefs/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package drivefs contains helper functions to interact with Google Drive
+package drivefs
