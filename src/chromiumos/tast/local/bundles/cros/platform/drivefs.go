80dbde6bf256bb2682d3088635b2b6bc7b3a963f
Ben Reich <benreich@chromium.org>
2020-06-09 08:17:50 +0000

    tast-tests: Refactor Drivefs wait to lib
    Extract out the logic required to identify that
    Drivefs is stable to a library function to allow
    for reuse.
    
    BUG=chromium:1092805
    TEST=tast run <soraka/eve> platform.Drivefs
    
    Change-Id: Iac10dc7160d22aacd54e87e85a663dd63dabe054

--- src/chromiumos/tast/local/bundles/cros/platform/drivefs.go
+++ src/chromiumos/tast/local/bundles/cros/platform/drivefs.go
@@ -10,12 +10,9 @@ import (
 	"path"
 	"time"
 
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ui/filesapp"
-	"chromiumos/tast/local/cryptohome"
-	"chromiumos/tast/local/session"
-	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/local/drivefs"
 	"chromiumos/tast/testing"
 )
 
@@ -43,49 +40,8 @@ func init() {
 	})
 }
 
-func findMatchingMount(matcher func(sysutil.MountInfo) bool) (matches []sysutil.MountInfo, err error) {
-	info, err := sysutil.MountInfoForPID(sysutil.SelfPID)
-	if err != nil {
-		return
-	}
-	for i := range info {
-		if matcher(info[i]) {
-			matches = append(matches, info[i])
-		}
-	}
-	return
-}
-
-func waitForMatchingMount(ctx context.Context, timeout time.Duration, matcher func(sysutil.MountInfo) bool) error {
-	testing.ContextLogf(ctx, "Waiting %v for a matching mount to appear", timeout)
-	return testing.Poll(ctx, func(ctx context.Context) error {
-		matches, err := findMatchingMount(matcher)
-		if err != nil {
-			return errors.Wrap(err, "IO error trying to list mounts")
-		}
-		if len(matches) == 0 {
-			return errors.Wrap(os.ErrNotExist, "The matching mount was not found")
-		}
-		return nil
-	}, &testing.PollOptions{Timeout: timeout, Interval: time.Second})
-}
-
-func isDriveFs(info sysutil.MountInfo) bool {
-	return info.Fstype == "fuse.drivefs"
-}
-
-func waitForMountConnected(ctx context.Context, timeout time.Duration, path string) error {
-	testing.ContextLogf(ctx, "Waiting %v for mount to become connected", timeout)
-	return testing.Poll(ctx, func(ctx context.Context) error {
-		_, err := os.Stat(path)
-		return err
-	}, &testing.PollOptions{Timeout: timeout, Interval: time.Second})
-}
-
 func Drivefs(ctx context.Context, s *testing.State) {
 	const (
-		mountPointTimeout = 15 * time.Second
-		fuseIoTimeout     = 40 * time.Second
 		filesAppUITimeout = 15 * time.Second
 		testFileName      = "drivefs"
 	)
@@ -105,66 +61,13 @@ func Drivefs(ctx context.Context, s *testing.State) {
 	}
 	defer cr.Close(ctx)
 
-	normUser, err := session.NormalizeEmail(cr.User(), true)
+	mountPath, err := drivefs.WaitForDriveFs(ctx, cr)
 	if err != nil {
-		s.Fatal("Failed to normalize user name: ", err)
+		s.Fatal("Failed waiting for DriveFS to start: ", err)
 	}
-	s.Log("Logged in as ", normUser)
-
-	// Check that cache folder was created by cryptohome.
-	homePath, err := cryptohome.UserPath(ctx, normUser)
-	if err != nil {
-		s.Fatal("Failed to get home path: ", err)
-	}
-	cachePath := path.Join(homePath, "GCache", "v2")
-	if dir, err := os.Stat(cachePath); !dir.IsDir() {
-		s.Fatal("Cache dir ", cachePath, " is missing: ", err)
-	}
-
-	// It takes some time for request to mount Drive to be handled by CrosDisks
-	// that creates the mount point. Poll for a mount point until timeout.
-	if err := waitForMatchingMount(ctx, mountPointTimeout, isDriveFs); err != nil {
-		s.Fatal("Timeout while waiting for mountpoint creation: ", err)
-	}
-	mounts, err := findMatchingMount(isDriveFs)
-	if err != nil {
-		s.Fatal("Could not obtain mounts: ", err)
-	}
-	if len(mounts) != 1 {
-		s.Fatal("Expected only one drivefs mount but found ", len(mounts), ". Found mounts ", mounts)
-	}
-	mountPath := mounts[0].MountPath
-	s.Log("drivefs is mounted into ", mountPath)
 
 	// We expect to find at least this folder in the mount point.
 	drivefsRoot := path.Join(mountPath, "root")
-
-	// As drivefs may not be fully initialized yet all access to the mount point
-	// may fail inside FUSE driver until the daemon is ready.
-	// Poll for stat to succeed in case the drivefs daemon is never ready due to
-	// some bug.
-	if err := waitForMountConnected(ctx, fuseIoTimeout, drivefsRoot); err != nil {
-		s.Fatal("Failed while waiting for stat: ", err)
-	}
-	dir, err := os.Stat(drivefsRoot)
-	if err != nil {
-		s.Fatal("Could not stat ", drivefsRoot, ": ", err)
-	}
-	if !dir.IsDir() {
-		s.Fatal("Could not find root folder inside ", mountPath, ": ", err)
-	}
-	s.Log("drivefs fully started")
-
-	// Now we are relatively confident that drivefs started correctly.
-	// Check for team_drives.
-	drivefsTeamDrives := path.Join(mountPath, "team_drives")
-	dir, err = os.Stat(drivefsTeamDrives)
-	if err != nil {
-		s.Fatal("Could not stat ", drivefsTeamDrives, ": ", err)
-	}
-	if !dir.IsDir() {
-		s.Fatal("Could not find team_drives folder inside ", mountPath, ": ", err)
-	}
 
 	// Create a test file inside Drive.
 	testFile, err := os.Create(path.Join(drivefsRoot, testFileName))
