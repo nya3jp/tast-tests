a4368fa7bc03c0aa9f8300593d1f19f7aeafe9e2
Cherie Cheung <cherieccy@google.com>
2020-06-10 03:21:56 +0900

    tast-tests: App can read file on DriveFS/Downloads
    2 new tast tests are added. In these tests, FilesApp's "Open with"
    android app feature is used to open files on DriveFS / Downloads
    folder. It is a generalized test framework that can be extended to
    cover other FilesApp folders.
    
    BUG=b:147618164
    TEST=tast run <DUT> arc.Drivefs
         tast run <DUT> arc.DownloadsFolder
    
    Cq-Depend: chrome-internal:3104297
    Change-Id: Ieb03f00f5b04ca0f9f51fcad4e0a4f30d96a53a2

---
+++ src/chromiumos/tast/local/bundles/cros/arc/drivefs.go
@@ -0,0 +1,79 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"path"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/storage"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/drivefs"
+	"chromiumos/tast/testing"
+)
+
+const (
+	drivefsURI = "content://org.chromium.arc.volumeprovider/MyDrive/root/storage.txt"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Drivefs,
+		Desc: "Android app can read files on Drive FS (Google Drive) via FilesApp",
+		Contacts: []string{
+			"cherieccy@google.com",
+			"arc-storage@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Vars:         []string{"arc.Drivefs.user", "arc.Drivefs.password"},
+		Params: []testing.Param{
+			{
+				Val: []storage.Expectation{
+					{storage.ActionID, storage.ExpectedAction},
+					{storage.FileContentID, storage.ExpectedFileContent}},
+				ExtraSoftwareDeps: []string{"android_p"},
+			}, {
+				Name: "vm",
+				Val: []storage.Expectation{
+					{storage.ActionID, storage.ExpectedAction},
+					{storage.URIID, drivefsURI},
+					{storage.FileContentID, storage.ExpectedFileContent}},
+				ExtraSoftwareDeps: []string{"android_vm"},
+			},
+		},
+	})
+}
+
+func Drivefs(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(
+		ctx,
+		chrome.ARCEnabled(),
+		chrome.Auth(s.RequiredVar("arc.Drivefs.user"), s.RequiredVar("arc.Drivefs.password"), ""),
+		chrome.GAIALogin(),
+	)
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	expectations := s.Param().([]storage.Expectation)
+
+	mountPath, err := drivefs.WaitForDriveFs(ctx, cr.User())
+	if err != nil {
+		s.Fatal("Failed waiting for DriveFS to start: ", err)
+	}
+	drivefsRoot := path.Join(mountPath, "root")
+	dir := storage.Directory{Path: drivefsRoot, Name: "Google Drive", Title: "Files - My Drive"}
+
+	storage.TestOpenWithAndroidApp(ctx, s, a, cr, dir, expectations)
+}
---
+++ src/chromiumos/tast/local/bundles/cros/arc/storage/storage.go
@@ -0,0 +1,189 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package storage
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	arcui "chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/filesapp"
+	"chromiumos/tast/testing"
+)
+
+const (
+	// Test app for reading the test file.
+	apkName = "ArcFileReaderTest.apk"
+	// Filename of the test file.
+	testFile = "storage.txt"
+	// Time to wait for file creation.
+	fileWaitTime = 20 * time.Second
+	// Timeout to wait for UI item to appear.
+	uiTimeout = 10 * time.Second
+
+	// Labels to appear in the test app and their expected values.
+
+	// ActionID is the id of the action label.
+	ActionID = "org.chromium.arc.testapp.filereader:id/action"
+	// ExpectedAction should be VIEW intent.
+	ExpectedAction = "android.intent.action.VIEW"
+	// URIID is the id of the uri label.
+	URIID = "org.chromium.arc.testapp.filereader:id/uri"
+	// FileContentID is the id of the file content label.
+	FileContentID = "org.chromium.arc.testapp.filereader:id/file_content"
+	// ExpectedFileContent in the test file.
+	ExpectedFileContent = "this is a test"
+)
+
+// Expectation is used for validating app label contents.
+type Expectation struct {
+	LabelID string
+	Value   string
+}
+
+// Directory represents a FilesApp directory, e.g. Drive FS, Downloads.
+type Directory struct {
+	Path  string
+	Name  string
+	Title string
+}
+
+// TestOpenWithAndroidApp performs OpenWith operation on the test file in the specified directory |dir|.
+// |dir| needs to be one of the top folders in FilesApp, e.g. Google Drive, Downloads.
+func TestOpenWithAndroidApp(ctx context.Context, s *testing.State, a *arc.ARC, cr *chrome.Chrome, dir Directory, expectations []Expectation) {
+	testing.ContextLogf(ctx, "Performing TestOpenWithAndroidApp on: %s", dir.Path)
+
+	testing.ContextLog(ctx, "Installing ArcFileReaderTest app")
+	if err := a.Install(ctx, arc.APKPath(apkName)); err != nil {
+		s.Fatal("Failed to install ArcFileReaderTest app: ", err)
+	}
+
+	testing.ContextLog(ctx, "Setting up a test file")
+	testFileLocation := filepath.Join(dir.Path, testFile)
+	if err := ioutil.WriteFile(testFileLocation, []byte(ExpectedFileContent), 0666); err != nil {
+		s.Fatalf("Failed to create test file %s: %s", testFileLocation, err)
+	}
+	defer os.Remove(testFileLocation)
+	testing.Sleep(ctx, fileWaitTime)
+
+	if err := openWithReaderApp(ctx, cr, dir); err != nil {
+		s.Fatal("Could not open file with ArcFileReaderTest: ", err)
+	}
+
+	if err := validateResult(ctx, a, expectations); err != nil {
+		s.Fatal("ArcFileReaderTest's data is invalid: ", err)
+	}
+}
+
+// openWithReaderApp launches FilesApp and opens the test file with ArcFileReaderTest.
+func openWithReaderApp(ctx context.Context, cr *chrome.Chrome, dir Directory) error {
+	testing.ContextLog(ctx, "Opening the test file with ArcFileReaderTest")
+
+	// Open the test API.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return errors.Wrap(err, "creating test API connection failed")
+	}
+
+	// Open the Files App.
+	files, err := filesapp.Launch(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "launching the Files App failed")
+	}
+	// Instead of closing the Files App, just release the memory reference.
+	// Otherwise, when this test fails, the screenshot will be of an empty desktop/closing app.
+	defer files.Root.Release(ctx)
+
+	// Open the directory under testing.
+	if err := files.OpenDir(ctx, dir.Name, dir.Title); err != nil {
+		return errors.Wrapf(err, "could not open %s folder", dir.Name)
+	}
+
+	// Wait for and click the test file.
+	if err := files.WaitForFile(ctx, testFile, uiTimeout); err != nil {
+		return errors.Wrapf(err, "waiting for the test file %s failed", testFile)
+	}
+	if err := files.SelectFile(ctx, testFile); err != nil {
+		return errors.Wrapf(err, "selecting the test file %s failed", testFile)
+	}
+
+	// Wait for the Open menu button in the top bar.
+	params := ui.FindParams{
+		Name: "Open",
+		Role: ui.RoleTypeButton,
+	}
+	open, err := files.Root.DescendantWithTimeout(ctx, params, uiTimeout)
+	if err != nil {
+		return errors.Wrap(err, "failed to find the Open menu button")
+	}
+	defer open.Release(ctx)
+
+	// Click the Open menu button.
+	if err := open.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "clicking the Open menu button failed")
+	}
+
+	// Wait for 'Open with ArcFileReaderTest' to appear.
+	params = ui.FindParams{
+		Name: "ARC File Reader Test",
+		Role: ui.RoleTypeStaticText,
+	}
+	app, err := files.Root.DescendantWithTimeout(ctx, params, uiTimeout)
+	if err != nil {
+		return errors.Wrap(err, "waiting for 'Open with ArcFileReaderTest' failed")
+	}
+	defer app.Release(ctx)
+
+	// Click 'Open with ArcFileReaderTest'.
+	if err := app.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "clicking 'Open with ArcFileReaderTest' failed")
+	}
+
+	return nil
+}
+
+// validateResult validates the data read from ArcFileReaderTest app.
+func validateResult(ctx context.Context, a *arc.ARC, expectations []Expectation) error {
+	d, err := arcui.NewDevice(ctx, a)
+	if err != nil {
+		return errors.Wrap(err, "failed initializing UI Automator")
+	}
+	defer d.Close()
+
+	for _, e := range expectations {
+		if err := validateLabel(ctx, d, e); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// validateLabel is a helper function to load app label texts and compare it with expectation.
+func validateLabel(ctx context.Context, d *arcui.Device, expectation Expectation) error {
+	uiObj := d.Object(arcui.ID(expectation.LabelID))
+	if err := uiObj.WaitForExists(ctx, uiTimeout); err != nil {
+		return errors.Wrapf(err, "failed to find the label id %s", expectation.LabelID)
+	}
+
+	actual, err := uiObj.GetText(ctx)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get text from the label id %s", expectation.LabelID)
+	}
+
+	if actual != expectation.Value {
+		return errors.Errorf("label content mismatch for %s. Actual = %s. Expected = %s", expectation.LabelID, actual, expectation.Value)
+	}
+
+	testing.ContextLogf(ctx, "Label content of %s = %s", expectation.LabelID, actual)
+	return nil
+}
