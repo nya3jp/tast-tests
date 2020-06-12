945d53640755bddc72292d2d990f25b482514bb8
Jesse Schettler <jschettler@chromium.org>
2020-06-12 16:18:58 -0600

    tast-tests: Add RunTelem() function
    Some cros_healthd tast tests don't expect CSV output from the telem
    subtool. Add a RunTelem() function for these tests to run the subtool
    without trying to parse the output.
    
    BUG=None
    TEST=platform.CrosHealthdProbe*
    
    Change-Id: I6340a6d7204f620e38899ee9659ea75d1d7a9f56

--- src/chromiumos/tast/local/croshealthd/telem.go
+++ src/chromiumos/tast/local/croshealthd/telem.go
@@ -43,6 +43,24 @@ const (
 // there is a failure to obtain or parse telemetry info or if a line of output
 // has an unexpected number of fields.
 func FetchTelemetry(ctx context.Context, category TelemCategory, outDir string) ([][]string, error) {
+	b, err := RunTelem(ctx, category, outDir)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to run telem command")
+	}
+
+	records, err := csv.NewReader(strings.NewReader(string(b))).ReadAll()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse output")
+	}
+
+	return records, nil
+}
+
+// RunTelem runs cros-health-tool's telem command with the given category and
+// returns the output. It also dumps the output to a file for debugging. An
+// error is returned if there is a failure to run the command or save the output
+// to the file.
+func RunTelem(ctx context.Context, category TelemCategory, outDir string) ([]byte, error) {
 	if err := upstart.EnsureJobRunning(ctx, "cros_healthd"); err != nil {
 		return nil, errors.Wrap(err, "failed to start cros_healthd")
 	}
@@ -59,10 +77,5 @@ func FetchTelemetry(ctx context.Context, category TelemCategory, outDir string) 
 		return nil, errors.Wrapf(err, "failed to write output to %s", path)
 	}
 
-	records, err := csv.NewReader(strings.NewReader(string(b))).ReadAll()
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to parse output")
-	}
-
-	return records, nil
+	return b, nil
 }
