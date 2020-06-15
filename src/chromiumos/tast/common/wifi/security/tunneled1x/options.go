8df0ab4a7070ac350d419df65685dd98e0d147c2
Hsin-chen Chuang <chharry@google.com>
2020-06-15 17:30:12 +0800

    wifi: security: Add tunneled1x package.
    Package tunneled1x provides a Config type for TTLS/PEAP protected
    network.
    
    BUG=chromium:1034875
    TEST=~/trunk/src/platform/tast/fast_build.sh -t \
    chromiumos/tast/common/wifi/security/tunneled1x
    
    Change-Id: Ief8e84cdf0ec0a1770fafbcc846d8d4e211a101b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2247427
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Yen-lin Lai <yenlinlai@google.com>
    Tested-by: Hsin-chen Chuang <chharry@google.com>
    Commit-Queue: Hsin-chen Chuang <chharry@google.com>

---
+++ src/chromiumos/tast/common/wifi/security/tunneled1x/options.go
@@ -0,0 +1,51 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package tunneled1x
+
+import (
+	"chromiumos/tast/common/wifi/security/wpaeap"
+)
+
+// Option is the function signature used to specify options of Config.
+type Option func(*ConfigFactory)
+
+// OuterProtocol returns an Option which sets the outer (layer1) protocol in Config.
+func OuterProtocol(outer string) Option {
+	return func(c *ConfigFactory) {
+		c.outerProtocol = outer
+	}
+}
+
+// InnerProtocol returns an Option which sets the inner (layer2) protocol in Config.
+func InnerProtocol(inner string) Option {
+	return func(c *ConfigFactory) {
+		c.innerProtocol = inner
+	}
+}
+
+// ClientPassword returns an Option which sets the client password in Config.
+// Note that this is used for setting a bad password for testing, that is,
+// it will be set to be the same as server's by default.
+func ClientPassword(passwd string) Option {
+	return func(c *ConfigFactory) {
+		c.clientPassword = passwd
+	}
+}
+
+// Options below are re-wrapped from the options of package wpaeap.
+
+// AltSubjectMatch returns an Option which sets shill EAP.SubjectAlternativeNameMatch property in Config.
+func AltSubjectMatch(sans []string) Option {
+	return func(c *ConfigFactory) {
+		c.wpaeapOps = append(c.wpaeapOps, wpaeap.AltSubjectMatch(sans))
+	}
+}
+
+// FileSuffix returns an Option which sets the file suffix in Config.
+func FileSuffix(suffix string) Option {
+	return func(c *ConfigFactory) {
+		c.wpaeapOps = append(c.wpaeapOps, wpaeap.FileSuffix(suffix))
+	}
+}
