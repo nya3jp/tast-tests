51a61ee935e8d6b7080290d0fd184757344fff2a
Arowa Suliman <arowa@google.com>
2020-06-17 14:24:22 -0700

    tast-test: Add wifi.DuplicateBSSID
    The Tast test is migrated from network_WiFi_MaskedBSSID.py, an Autotest.
    Changed the name of the test to a positive form DuplicateBSSID, which
    describes the behaviour of the test more clearly.
    
    In addition, instead of running a scan to make sure both SSIDs appear in
    the scan, the test make sure that we can  connect to both SSIDs.
    
    BUG=b:158696331
    TEST=run the test wifi.MaskedBSSID
    
    Change-Id: I4b90a6b82b27d63db6ba6136706d2aa10076f0f6

--- src/chromiumos/tast/remote/wificell/hostapd/config_test.go
+++ src/chromiumos/tast/remote/wificell/hostapd/config_test.go
@@ -155,6 +155,26 @@ func TestNewConfig(t *testing.T) {
 			expected:   nil,
 			shouldFail: true, // BeaconInterval should be in 15...65535.
 		},
+		{
+			ops: []Option{
+				Mode(Mode80211a),
+				Channel(36),
+				DTIMPeriod(5),
+				BSSID("00:11:22:33:44:55:00"),
+			},
+			expected:   nil,
+			shouldFail: true, // should not set the BSSID if its length is over 17.
+		},
+		{
+			ops: []Option{
+				Mode(Mode80211a),
+				Channel(36),
+				DTIMPeriod(5),
+				BSSID("00:11:22:33:44"),
+			},
+			expected:   nil,
+			shouldFail: true, // should not set the BSSID if its length is under 17.
+		},
 		// Good cases.
 		{
 			ops: []Option{
@@ -162,6 +182,7 @@ func TestNewConfig(t *testing.T) {
 				Mode(Mode80211a),
 				Channel(36),
 				DTIMPeriod(5),
+				BSSID("00:11:22:33:44:55"),
 			},
 			expected: &Config{
 				SSID:           "ssid",
@@ -170,6 +191,7 @@ func TestNewConfig(t *testing.T) {
 				HTCaps:         0,
 				SecurityConfig: &base.Config{},
 				DTIMPeriod:     5,
+				BSSID:          "00:11:22:33:44:55",
 			},
 			shouldFail: false,
 		},
@@ -323,6 +345,7 @@ func TestConfigFormat(t *testing.T) {
 				Channel:        1,
 				SecurityConfig: &base.Config{},
 				DTIMPeriod:     200,
+				BSSID:          "00:11:22:33:44:55",
 			},
 			verify: map[string]string{
 				"ssid2":          `P"ssid000"`,
@@ -331,6 +354,7 @@ func TestConfigFormat(t *testing.T) {
 				"interface":      iface,
 				"ctrl_interface": ctrl,
 				"dtim_period":    "200",
+				"bssid":          "00:11:22:33:44:55",
 			},
 		},
 		// Check 802.11n pure.
