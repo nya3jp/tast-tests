71388138f87678021876051ce99fae479b5bbde0
Andres Calderon Jaramillo <andrescj@chromium.org>
2020-06-17 01:46:51 +0000

    video: Add test to check the contents of a video.
    This CL creates a test that:
    
    1) Starts playing a video.
    2) Switches to full screen by injecting the 'f' key.
    3) Takes a screenshot using the CLI tool.
    4) Analyzes the screenshot to check the color of a few interesting
       pixels.
    
    To analyze the screenshot, we first rotate it if necessary (this is
    needed on, e.g., kukui where the screen is rotated). Then, we locate the
    top and bottom of the video to exclude the black strips on top and
    bottom that are not part of the video. Finally, we sample the color of
    a) the centers of the four quadrants of the video, and b) the four
    corners of the video. We compare these against hard coded expectations.
    
    Note that the test videos consist of still images, so it doesn't matter
    exactly when the screen is captured. However, we wait a few seconds
    before taking the screenshot in order to exclude the "Press Esc to exit
    full screen" message from the screenshot and ensure the video is the
    only thing on the screen.
    
    This test is able to verify the screenshot regardless of whether or not
    HW overlays were used (because the screenshot tool captures them). Also,
    as far as I could tell, the night light mode does not affect the output
    of the screenshot tool.
    
    BUG=b:159158467
    TEST=passes on eve and kukui.
    
    Change-Id: If0938f320114bb7f2143e16f9d47be325b943910

---
+++ src/chromiumos/tast/local/bundles/cros/video/contents.go
@@ -0,0 +1,58 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/video/play"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/pre"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Contents,
+		Desc: "Verifies that a screenshot of a full screen is sane",
+		Contacts: []string{
+			"andrescj@chromium.org",
+			"chromeos-gfx-video@google.com",
+		},
+		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name:              "h264_360p_hw",
+			Val:               "still-colors-360p.h264.mp4",
+			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
+			ExtraData:         []string{"video.html", "still-colors-360p.h264.mp4"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
+			Pre:               pre.ChromeVideo(),
+		}, {
+			Name:              "h264_720p_hw",
+			Val:               "still-colors-720p.h264.mp4",
+			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
+			ExtraData:         []string{"video.html", "still-colors-720p.h264.mp4"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
+			Pre:               pre.ChromeVideo(),
+		}, {
+			Name:              "h264_1080p_hw",
+			Val:               "still-colors-1080p.h264.mp4",
+			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
+			ExtraData:         []string{"video.html", "still-colors-1080p.h264.mp4"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "chrome_internal"}, // "chrome_internal" is needed because H.264 is a proprietary codec.
+			Pre:               pre.ChromeVideo(),
+		}},
+		// TODO(andrescj): add tests for VP8 and VP9. Also, test forcing HW overlays off.
+	})
+}
+
+func Contents(ctx context.Context, s *testing.State) {
+	if err := play.TestPlayAndScreenshot(ctx, s, s.PreValue().(*chrome.Chrome), s.Param().(string)); err != nil {
+		s.Fatal("TestPlayAndScreenshot failed: ", err)
+	}
+}
