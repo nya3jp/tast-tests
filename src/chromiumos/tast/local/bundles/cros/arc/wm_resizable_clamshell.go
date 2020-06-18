9d9736c8261a4b6aa08e1290362b6e9e37eafd6a
Garfield Tan <xutan@google.com>
2020-06-18 15:56:44 -0700

    [ARC-R WM TAST] Extend RC01 for whitelisted apps
    There are a set of special apps we whitelist to have special launch
    bounds (phone size, tablet size and maximized) based on some UX
    experiment. This CL adds integration test to verify those 3 types of
    launch logic is correct. See go/wm-arc-r and b/150477393 for more
    details.
    
    BUG=b:158515748
    TEST=tast run $DUT arc.WMResizableClamshell
    
    Change-Id: I2add2d117a179e46d58ac217dcffc1db4a227b0d

--- src/chromiumos/tast/local/bundles/cros/arc/wm_resizable_clamshell.go
+++ src/chromiumos/tast/local/bundles/cros/arc/wm_resizable_clamshell.go
@@ -15,8 +15,15 @@ import (
 	"chromiumos/tast/local/bundles/cros/arc/wm"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/screenshot"
 	"chromiumos/tast/testing"
+)
+
+const (
+	pkgMaximized  = "org.chromium.arc.testapp.windowmanager24.maximized"
+	pkgPhoneSize  = "org.chromium.arc.testapp.windowmanager24.phonesize"
+	pkgTabletSize = "org.chromium.arc.testapp.windowmanager24.tabletsize"
 )
 
 func init() {
@@ -81,6 +88,12 @@ func WMResizableClamshell(ctx context.Context, s *testing.State) {
 // wmRC01 covers resizable/clamshell default launch behavior.
 // Expected behavior is defined in: go/arc-wm-r RC01: resizable/clamshell: default launch behavior.
 func wmRC01(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device) error {
+	// launchBoundsThreshold stores the launch bounds of the last activity launch. This test is
+	// written in the order // from small launch bounds to big launch bounds so this variable
+	// serves as the lower bound of launch bounds.
+	var launchBoundsThreshold coords.Rect
+
+	// Start with ordinary case where we are launching apps not in the whitelist.
 	for activityName, desiredOrientation := range map[string]string{
 		wm.ResizablePortraitActivity:  wm.Portrait,
 		wm.ResizableLandscapeActivity: wm.Landscape,
@@ -113,9 +126,135 @@ func wmRC01(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 				return errors.Errorf("invalid orientation %v; want %v", orientation, desiredOrientation)
 			}
 
+			window, err := ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24)
+			if err != nil {
+				return err
+			}
+			orientationFromBounds := wm.OrientationFromBounds(window.BoundsInRoot)
+			if orientationFromBounds != desiredOrientation {
+				return errors.Errorf("invalid bounds orientation %v; want %v", orientationFromBounds, desiredOrientation)
+			}
+
+			if desiredOrientation == wm.Portrait {
+				launchBoundsThreshold = window.BoundsInRoot
+			}
+
 			return nil
 		}(); err != nil {
 			return errors.Wrapf(err, "%q test failed", activityName)
+		}
+	}
+
+	// Then we verify the launch logic for whitelisted apps is correct.
+	apkPath := map[string]string{
+		pkgMaximized:  "ArcWMTestApp_24_Maximized.apk",
+		pkgPhoneSize:  "ArcWMTestApp_24_PhoneSize.apk",
+		pkgTabletSize: "ArcWMTestApp_24_TabletSize.apk",
+	}
+	for pkgName, verifyFunc := range map[string]func(*arc.Activity, *ash.Window) error{
+		pkgPhoneSize: func(act *arc.Activity, window *ash.Window) error {
+			if err := wm.CheckRestoreResizable(ctx, tconn, act, d); err != nil {
+				return err
+			}
+
+			orientation, err := wm.UIOrientation(ctx, act, d)
+			if err != nil {
+				return err
+			}
+			if orientation != wm.Portrait {
+				return errors.Errorf("invalid orientation %v; want portrait", orientation)
+			}
+			orientationFromBounds := wm.OrientationFromBounds(window.BoundsInRoot)
+			if orientationFromBounds != wm.Portrait {
+				return errors.Errorf("invalid bounds orientation %v; want portrait", orientationFromBounds)
+			}
+
+			if launchBoundsThreshold.Width > window.BoundsInRoot.Width {
+				return errors.Errorf("Phone size width shouldn't be smaller than %v, but it's %v",
+					launchBoundsThreshold.Width, window.BoundsInRoot.Width)
+			}
+			if launchBoundsThreshold.Height > window.BoundsInRoot.Height {
+				return errors.Errorf("Phone size height shouldn't be smaller than %v, but it's %v",
+					launchBoundsThreshold.Height, window.BoundsInRoot.Height)
+			}
+			return nil
+		},
+		pkgTabletSize: func(act *arc.Activity, window *ash.Window) error {
+			if window.State == ash.WindowStateMaximized {
+				// We might be running on a small device that can't hold a freeform window of tablet size.
+				// However we don't have concrete display value to verify it, so we won't check display size.
+				return wm.CheckMaximizeResizable(ctx, tconn, act, d)
+			}
+
+			// The majority of our devices is large enough to hold a freeform window of tablet size so it should
+			// come here.
+			if err := wm.CheckRestoreResizable(ctx, tconn, act, d); err != nil {
+				return err
+			}
+
+			orientation, err := wm.UIOrientation(ctx, act, d)
+			if err != nil {
+				return err
+			}
+			if orientation != wm.Landscape {
+				return errors.Errorf("invalid orientation %v; want landscape", orientation)
+			}
+			orientationFromBounds := wm.OrientationFromBounds(window.BoundsInRoot)
+			if orientationFromBounds != wm.Landscape {
+				return errors.Errorf("invalid bounds orientation %v; want landscape", orientationFromBounds)
+			}
+
+			if launchBoundsThreshold.Width > window.BoundsInRoot.Width {
+				return errors.Errorf("Tablet size width shouldn't be smaller than %v, but it's %v",
+					launchBoundsThreshold.Width, window.BoundsInRoot.Width)
+			}
+			if launchBoundsThreshold.Height > window.BoundsInRoot.Height {
+				return errors.Errorf("Tablet size height shouldn't be smaller than %v, but it's %v",
+					launchBoundsThreshold.Height, window.BoundsInRoot.Height)
+			}
+			return nil
+		},
+		pkgMaximized: func(act *arc.Activity, window *ash.Window) error {
+			return wm.CheckMaximizeResizable(ctx, tconn, act, d)
+		},
+	} {
+		if err := func() error {
+			if err := a.Install(ctx, arc.APKPath(apkPath[pkgName])); err != nil {
+				return err
+			}
+			defer a.Uninstall(ctx, pkgName)
+
+			act, err := arc.NewActivity(a, pkgName, wm.ResizableUnspecifiedActivity)
+			if err != nil {
+				return err
+			}
+			defer act.Close()
+
+			if err := act.Start(ctx, tconn); err != nil {
+				return err
+			}
+			defer act.Stop(ctx, tconn)
+
+			if err := wm.WaitUntilActivityIsReady(ctx, tconn, act, d); err != nil {
+				return err
+			}
+
+			if err := ash.WaitForVisible(ctx, tconn, pkgName); err != nil {
+				return err
+			}
+			window, err := ash.GetARCAppWindowInfo(ctx, tconn, pkgName)
+			if err != nil {
+				return err
+			}
+
+			if err := verifyFunc(act, window); err != nil {
+				return err
+			}
+
+			launchBoundsThreshold = window.BoundsInRoot
+			return nil
+		}(); err != nil {
+			return err
 		}
 	}
 	return nil
