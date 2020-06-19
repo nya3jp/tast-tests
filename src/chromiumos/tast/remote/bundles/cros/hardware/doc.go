c6d6564e10b4aa81cf8db4323c98d70e167fef04
Kamil Koczurek <kek@semihalf.corp-partner.google.com>
2020-06-19 10:56:41 +0200

    tast-tests: Add test for validating clocksource
    This checks the time interval measured by DUT
    and compares it to the measurement performed
    by the host.
    
    BUG=b:159031520, b:155500631
    TEST=1) emerge tast-local-helpers-cros
         2) tast run [DUT IP] hardware.VerifySleep
    
    Change-Id: I4492bb65d2a1a8da04798ac094b81ad22acb7918

---
+++ src/chromiumos/tast/remote/bundles/cros/hardware/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package hardware contains tests that verify proper operation of various
+// components of DUT
+package hardware
