6aef0221f0f20e4623a0462a1348254829390350
Jun Sheng <shengjun@google.com>
2020-06-24 09:28:40 +1000

    tast-tests: Stabilize OOBE vk input test
    1. Trigger OOBE virtual keyboard via JS function rather than Tap to improve stability.
    2. Attached bug b/159748349 to create reliable tap function to trigger vk in oobe.
    
    BUG=b:159265523
    TEST=Ran on VM and device.
    
    Change-Id: Ie0275a7d264464eee5d1a7684d0e21b07bfbc436

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_oobe.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_oobe.go
@@ -16,7 +16,6 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/vkb"
 	"chromiumos/tast/local/coords"
-	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
 
@@ -71,55 +70,9 @@ func VirtualKeyboardOOBE(ctx context.Context, s *testing.State) {
 func inputField(ctx context.Context, kconn, gaiaConn *chrome.Conn, cssSelector string, keys []string, expectedValue string) error {
 	// Wait for document to load and input field to appear.
 	if err := gaiaConn.WaitForExpr(ctx, fmt.Sprintf(
-		"document.readyState === 'complete' && document.querySelector(%q)", cssSelector)); err != nil {
+		"document.readyState === 'complete' && document.querySelector(%q)===document.activeElement", cssSelector)); err != nil {
 		return errors.Wrapf(err, "failed to wait for document ready or %q element", cssSelector)
 	}
-
-	// Get touch point on input field.
-	// Touch method works on screen coordinate rather than document related location. Element coordinate should be calculated with screen availHeight.
-	var inputFieldTouchPoint coords.Point
-	if err := gaiaConn.Eval(ctx, fmt.Sprintf(
-		`(function() {
-			var elements = document.querySelectorAll(%q);
-			for(var element of elements){
-				if(!element.hidden){
-					var b = element.getBoundingClientRect();
-					return {
-						'x': Math.round(b.left + b.width / 2),
-						'y': Math.round(b.top + b.height / 2) + (screen.height - screen.availHeight) / 2,
-					};
-				}
-			}
-		  })()`, cssSelector), &inputFieldTouchPoint); err != nil {
-		return errors.Wrapf(err, "failed to get location of %q element", cssSelector)
-	}
-
-	tsw, err := input.Touchscreen(ctx)
-	if err != nil {
-		return errors.Wrap(err, "failed to open touchscreen device")
-	}
-	defer tsw.Close()
-
-	// Touchscreen bounds: The size of the touchscreen might not be the same
-	// as the display size. In fact, might be even up to 4x bigger.
-	// NewTouchCoordConverter is used for convert pixel to touch point.
-	screenSize, err := getScreenSize(ctx, gaiaConn)
-	testing.ContextLog(ctx, "Full screen size:", screenSize)
-	if err != nil {
-		return errors.Wrap(err, "failed to get viewport size")
-	}
-
-	tcc := tsw.NewTouchCoordConverter(screenSize)
-	touchpointX, touchpointY := tcc.ConvertLocation(inputFieldTouchPoint)
-
-	testing.ContextLog(ctx, "Touch window size: ", tsw.Width(), tsw.Height())
-	testing.ContextLog(ctx, "Touch position: ", input.TouchCoord(touchpointX), input.TouchCoord(touchpointY))
-
-	stw, err := tsw.NewSingleTouchWriter()
-	if err != nil {
-		return errors.Wrap(err, "could not get a new TouchEventWriter")
-	}
-	defer stw.Close()
 
 	// Original view port size without virtual keyboard
 	originalViewPortSize, err := getViewPortSize(ctx, gaiaConn)
@@ -127,13 +80,10 @@ func inputField(ctx context.Context, kconn, gaiaConn *chrome.Conn, cssSelector s
 		return errors.Wrap(err, "failed to get viewport size")
 	}
 
-	//TODO(b/157685907): Investigate why single touch does not trigger virtual keyboard. It requires double touch to show virtual keyboard.
-	// In manual testing, single touch can trigger virtual keyboard shown.
-	stw.Move(input.TouchCoord(touchpointX), input.TouchCoord(touchpointY))
-	stw.End()
-	testing.Sleep(ctx, 50*time.Millisecond)
-	stw.Move(input.TouchCoord(touchpointX), input.TouchCoord(touchpointY))
-	stw.End()
+	//TODO(b/159748349): Tap input field to trigger virtual keyboard rather than JS function.
+	if err := kconn.Eval(ctx, "chrome.inputMethodPrivate.showInputView()", nil); err != nil {
+		return errors.Wrap(err, "failed to show virtual keyboard via JS")
+	}
 
 	// Wait for viewport shrink vertically because of vk showing up.
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
@@ -163,26 +113,6 @@ func inputField(ctx context.Context, kconn, gaiaConn *chrome.Conn, cssSelector s
 		return errors.Wrap(err, "failed to get the contents of the text field")
 	}
 
-	// Tap key to hide vk
-	vkb.TapKeyJS(ctx, kconn, "hide keyboard")
-
-	// Wait for viewport reverted to full screen because of vk hidden.
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		newViewPort, err := getViewPortSize(ctx, gaiaConn)
-		if err != nil {
-			return errors.Wrap(err, "failed to get viewport size")
-		}
-
-		if newViewPort.Height < originalViewPortSize.Height {
-			return errors.New("Viewport has not reverted to full screen yet")
-		} else if newViewPort.Height > originalViewPortSize.Height {
-			// This should not happen in theory
-			return testing.PollBreak(errors.Errorf(`View port is getting larger during test; got %v; want %v`, newViewPort, originalViewPortSize))
-		}
-		return nil
-	}, &testing.PollOptions{Timeout: 3 * time.Second}); err != nil {
-		return errors.Wrap(err, "viewport does not reverted back to full screen after hiding virtual keyboard")
-	}
 	return nil
 }
 
