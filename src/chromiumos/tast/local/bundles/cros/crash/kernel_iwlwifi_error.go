0d20e7bcca6ab2976222163a2eb12d5cf7d3a837
Arowa Suliman <arowa@google.com>
2020-06-23 11:08:03 -0700

    tast-tests: Add kernel iwlwifi error test.
    This tests the kernel iwlwifi error collector by inducing an artificial
    iwlwifi error and verifying that the expected files are created in the
    crash.SystemCrashDir.
    
    BUG=chromium:1015106
    TEST=run the tests:
    crash.KernelIwlwifiError.real_consent    [ PASS ]
    crash.KernelIwlwifiError.mock_consent    [ PASS ]
    
    Change-Id: Ic9d0f3d3da591065a478af861a0682b6593cada5

---
+++ src/chromiumos/tast/local/bundles/cros/crash/kernel_iwlwifi_error.go
@@ -0,0 +1,94 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crash
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/crash"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     KernelIwlwifiError,
+		Desc:     "Verify kernel iwlwifi errors are logged as expected",
+		Contacts: []string{"arowa@google.com", "cros-telemetry@google.com"},
+		Attr:     []string{"group:mainline"},
+		Params: []testing.Param{{
+			Name:              "real_consent",
+			ExtraSoftwareDeps: []string{"chrome", "metrics_consent"},
+			ExtraAttr:         []string{"informational"},
+			Pre:               crash.ChromePreWithVerboseConsent(),
+			Val:               crash.RealConsent,
+		}, {
+			Name:      "mock_consent",
+			ExtraAttr: []string{"informational"},
+			Val:       crash.MockConsent,
+		}},
+	})
+}
+
+func KernelIwlwifiError(ctx context.Context, s *testing.State) {
+	opt := crash.WithMockConsent()
+	useConsent := s.Param().(crash.ConsentType)
+	if useConsent == crash.RealConsent {
+		opt = crash.WithConsent(s.PreValue().(*chrome.Chrome))
+	}
+	if err := crash.SetUpCrashTest(ctx, opt); err != nil {
+		s.Fatal("SetUpCrashTest failed: ", err)
+	}
+	defer crash.TearDownCrashTest(ctx)
+
+	oldFiles, err := crash.GetCrashes(crash.SystemCrashDir)
+	if err != nil {
+		s.Fatal("Failed to get original crashes: ", err)
+	}
+
+	if err := crash.RestartAnomalyDetector(ctx); err != nil {
+		s.Fatal("Failed to restart anomaly detector: ", err)
+	}
+
+	output, err := testexec.CommandContext(ctx, "lshw", "-c", "network").Output()
+	if err != nil {
+		s.Fatal("Failed to get the pci name of the wifi chip: ", err)
+	}
+
+	// Find the network pci interface name.
+	pat := regexp.MustCompile("bus info: pci@(.*)")
+	pci := pat.FindStringSubmatch(string(output))
+
+	const iwlwifiPath = "/sys/kernel/debug/iwlwifi"
+	const fwnmiPath = "/iwlmvm/fw_nmi"
+	fwnmi := filepath.Join(filepath.Join(iwlwifiPath, pci[1]), fwnmiPath)
+	if _, err := os.Stat(fwnmi); err == nil {
+		s.Log("Inducing artificial iwlwifi error")
+		if err := ioutil.WriteFile(fwnmi, []byte("1"), 0); err != nil {
+			s.Fatal("Failed to induce iwlwifi error in lkdtm: ", err)
+		}
+		s.Log("Waiting for files")
+		const funcName = `NMI_INTERRUPT_UNKNOWN`
+		const baseName = `kernel_iwlwifi_error_` + funcName + `\.\d{8}\.\d{6}\.0`
+		expectedRegexes := []string{baseName + `\.kcrash`,
+			baseName + `\.log\.gz`,
+			baseName + `\.meta`}
+		files, err := crash.WaitForCrashFiles(ctx, []string{crash.SystemCrashDir}, oldFiles, expectedRegexes)
+		if err != nil {
+			s.Fatal("Couldn't find expected files: ", err)
+		}
+		if err := crash.RemoveAllFiles(ctx, files); err != nil {
+			s.Log("Couldn't clean up files: ", err)
+		}
+	} else {
+		s.Log("Failed to induce an artifical iwlwifi error: ", err)
+	}
+
+}
