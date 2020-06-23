f7f36ac84085383da3338a53d63103ce6a878bac
Jun Mukai <mukai@google.com>
2020-06-22 17:53:06 -0700

    Running tests multiple times on UI perftests
    This CL introduces the following things:
    - create perfutil package which provides utilities for
      UI perftests
    - add Values construct to perfutil. It can store multiple
      data points, but drop the best/worst data points to
      stabilize the data
    - Runner and RunMultiple: conducts the test scenario
      multiple times. Also, RunMultiple runs the test scenario
      once more with tracing and store the tracing result
      into a file.
    
    BUG=chromium:1098087
    TEST=tast run ui.OverviewPerf ui.OverviewScrollPerf ui.SnapPerf ui.TabletTransitionPerf
    
    Change-Id: Icc53290618138d3cb43aaa7b3985e0ac769df53b

--- src/chromiumos/tast/local/bundles/cros/ui/overview_scroll_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/overview_scroll_perf.go
@@ -8,12 +8,11 @@ import (
 	"context"
 	"time"
 
-	"chromiumos/tast/common/perf"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/ui/perfutil"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/display"
-	"chromiumos/tast/local/chrome/metrics"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/media/cpu"
 	"chromiumos/tast/local/ui"
@@ -84,9 +83,9 @@ func OverviewScrollPerf(ctx context.Context, s *testing.State) {
 		s.Fatal("It does not appear to be in the overview mode: ", err)
 	}
 
-	histograms, err := metrics.RunAndWaitAll(ctx, tconn, time.Second, func() error {
+	pv := perfutil.RunMultiple(ctx, s, cr, perfutil.RunAndWaitAll(tconn, func() error {
 		// Scroll from the top right of the screen to the top left.
-		if err := stw.Swipe(ctx, tsew.Width()-10, 10, 10, 10, 500*time.Millisecond); err != nil {
+		if err := stw.Swipe(ctx, tsew.Width()-10, 10, tsew.Width()/2-10, 10, 500*time.Millisecond); err != nil {
 			return errors.Wrap(err, "failed to execute a swipe gesture")
 		}
 
@@ -94,27 +93,19 @@ func OverviewScrollPerf(ctx context.Context, s *testing.State) {
 			return errors.Wrap(err, "failed to finish the swipe gesture")
 		}
 
+		if err := stw.Swipe(ctx, tsew.Width()/2-10, 10, tsew.Width()-10, 10, 500*time.Millisecond); err != nil {
+			return errors.Wrap(err, "failed to execute a swipe gesture")
+		}
+		if err := stw.End(); err != nil {
+			return errors.Wrap(err, "failed to finish the swipe gesture")
+		}
+
 		return nil
-	}, "Ash.Overview.Scroll.PresentationTime.TabletMode")
-	if err != nil {
-		s.Fatal("Failed to scroll or get histogram: ", err)
-	}
+	}, "Ash.Overview.Scroll.PresentationTime.TabletMode"), perfutil.StoreLatency)
 
 	if err = ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
 		s.Fatal("It does not appear to be in the overview mode: ", err)
 	}
-
-	latency, err := histograms[0].Mean()
-	if err != nil {
-		s.Fatalf("Failed to get mean for histogram %s: %v", histograms[0].Name, err)
-	}
-
-	pv := perf.NewValues()
-	pv.Set(perf.Metric{
-		Name:      histograms[0].Name,
-		Unit:      "ms",
-		Direction: perf.SmallerIsBetter,
-	}, latency)
 
 	if err := pv.Save(s.OutDir()); err != nil {
 		s.Error("Failed saving perf data: ", err)
