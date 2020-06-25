bd275bf0bea12f6dd1d215602a2925c6415f063f
Jack Rosenthal <jrosenth@chromium.org>
2020-06-25 08:51:42 -0600

    dptf_override_cros_config: skip atlas
    Atlas is receiving unibuild configuration, per go/unibuild-migration.
    
    Since Atlas differentiates dptf profile based on memory part instead
    of identifiable information in cros_config (which is an obscure design
    we don't want to repeat), we need to keep the legacy dptf_override.sh
    script in the overlay.
    
    If we don't skip this test for atlas, after the migration, we get this
    error message:
        Unexpected DPTF profile: got "/etc/dptf/1098_0987.bin"; want ""
    
    So just skip the test for atlas.
    
    BUG=chromium:1098942
    TEST=power.DptfOverrideCrosConfig skips on atlas
    
    Change-Id: I69b7839085a253e999ac0a619546f7b74227c93b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2267357
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Auto-Submit: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>
    Reviewed-by: Greg Edelston <gredelston@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/power/dptf_override_cros_config.go
+++ src/chromiumos/tast/local/bundles/cros/power/dptf_override_cros_config.go
@@ -9,6 +9,7 @@ import (
 
 	"chromiumos/tast/local/bundles/cros/power/dptf"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -21,6 +22,12 @@ func init() {
 		},
 		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"dptf", "cros_config"},
+
+		// Atlas is a migrated pre-unibuild device, and uses a
+		// script to override the DPTF profile instead of
+		// cros_config.  New projects should use cros_config
+		// only; please do not add new devices to this list.
+		HardwareDeps: hwdep.D(hwdep.SkipOnModel("atlas")),
 	})
 }
 
