8500863b59f84e6f0d351b2e8429347e8b558bb1
Yichen Zhou <yichenz@chromium.org>
2020-06-24 19:13:53 -0700

    Add tab crash detection for CUJ tests
    It has been observed that some CUJ tests (e.g. ui.StadiaCUJ and
    ui.MeetCUJ) could have tab crashes. Thus cuj.TabCrashTracker is
    added and introduced to detect tab crash for general CUJ tests.
    
    Change-Id: I882fd7210c7049556753342aff2c275caf28174f

---
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/tab_crash_checker.go
@@ -0,0 +1,44 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cuj
+
+import (
+	"context"
+  
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/metrics"
+)
+
+// TabCrashChecker is used to check if any Chrome tab is crashed during CUJ test.
+type TabCrashChecker struct {
+	recorder *metrics.Recorder
+	tconn    *chrome.TestConn
+}
+
+// NewTabCrashChecker creates a TabCrashChecker and starts recording tab-crash metrics.
+func NewTabCrashChecker(ctx context.Context, tconn *chrome.TestConn) (*TabCrashChecker, error) {
+	recorder, err := metrics.StartRecorder(ctx, tconn, "Tabs.SadTab.CrashCreated",
+		"Tabs.SadTab.OomCreated", "Tabs.SadTab.KillCreated.OOM", "Tabs.SadTab.KillCreated")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start histogram recorder for sad tabs")
+	}
+	return &TabCrashChecker{recorder: recorder, tconn: tconn}, nil
+}
+
+// Check checks if there is any tab crash after the TabCrashChecker was created.
+func (checker *TabCrashChecker) Check(ctx context.Context) error {
+	diffs, err := checker.recorder.Histogram(ctx, checker.tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to get diff of histograms")
+	}
+	// Check the diffs and return errors if any histogram has non-zero num.
+	for _, h := range diffs {
+		if h.TotalCount() != 0 {
+			return errors.New("sad tab showed up (histogram " + h.Name + ")")
+		}
+	}
+	return nil
+}
