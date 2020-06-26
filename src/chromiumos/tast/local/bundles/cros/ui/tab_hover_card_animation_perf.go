908f18423d8d72f904e756efb6f50f026c022a8f
Yichen Zhou <yichenz@chromium.org>
2020-06-26 18:03:40 +0000

    ui.TabHoverCardAnimationPerf run for active/inactive tab
    Make ui.TabHoverCardAnimationPerf collect hover card animation
    metrics for both active and inactive tab.
    
    BUG=chromium:1060816
    TEST=tast run IP ui.TabHoverCardAnimationPerf
    Change-Id: Iec3a85ec91fdd7dac76767287295edf3b92e2d39

--- src/chromiumos/tast/local/bundles/cros/ui/tab_hover_card_animation_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/tab_hover_card_animation_perf.go
@@ -12,8 +12,9 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/metrics"
+	chromeui "chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/faillog"
 	"chromiumos/tast/local/chrome/ui/mouse"
-	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/media/cpu"
 	"chromiumos/tast/local/ui"
 	"chromiumos/tast/testing"
@@ -46,62 +47,85 @@ func TabHoverCardAnimationPerf(ctx context.Context, s *testing.State) {
 	}
 	defer cleanup(ctx)
 
-	conn, err := cr.NewConn(ctx, ui.PerftestURL)
-	if err != nil {
-		s.Fatal("Failed to open the tab: ", err)
-	}
-	if err := conn.Close(); err != nil {
-		s.Fatal("Failed to close the connection to the tab: ", err)
-	}
-
-	ws, err := ash.GetAllWindows(ctx, tconn)
-	if err != nil {
-		s.Fatal("Failed to obtain the window list: ", err)
-	}
-	bounds := ws[0].BoundsInRoot
-	// Use a heuristic offset (30, 30) from the window origin for the first tab.
-	tab := coords.NewPoint(bounds.Left+30, bounds.Top+30)
-	center := bounds.CenterPoint()
-
-	// Stabilize CPU usage.
-	if err := cpu.WaitUntilIdle(ctx); err != nil {
-		s.Error("Failed to wait for system UI to be stabilized: ", err)
+	for i := 0; i < 2; i++ {
+		conn, err := cr.NewConn(ctx, ui.PerftestURL)
+		if err != nil {
+			s.Fatalf("Failed to open %d-th tab: %v", i, err)
+		}
+		if err := conn.Close(); err != nil {
+			s.Fatalf("Failed to close the connection to %d-th tab: %v", i, err)
+		}
 	}
 
-	hists, err := metrics.RunAndWaitAll(ctx, tconn, time.Second, func() error {
-		if err := mouse.Move(ctx, tconn, center, 0); err != nil {
-			s.Fatal("Failed to put mouse to the center: ", err)
-		}
-		if err := mouse.Move(ctx, tconn, tab, 5*time.Second); err != nil {
-			s.Fatal("Failed to move mouse to the first tab: ", err)
-		}
-		if err := testing.Sleep(ctx, 5*time.Second); err != nil {
-			s.Fatal("Failed to sleep for 5 seconds: ", err)
-		}
-		if err := mouse.Move(ctx, tconn, center, 5*time.Second); err != nil {
-			s.Fatal("Failed to move mouse back to the center: ", err)
-		}
-		return nil
-	},
-		"Chrome.Tabs.AnimationSmoothness.HoverCard.FadeIn",
-		"Chrome.Tabs.AnimationSmoothness.HoverCard.FadeOut")
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	webview, err := chromeui.FindWithTimeout(ctx, tconn, chromeui.FindParams{Role: chromeui.RoleTypeWebView, ClassName: "WebView"}, 10*time.Second)
 	if err != nil {
-		s.Fatal("Failed to move mouse or get the histogram: ", err)
+		s.Fatal("Failed to find webview: ", err)
+	}
+	defer webview.Release(ctx)
+	center := webview.Location.CenterPoint()
+
+	// Find tabs.
+	tabs, err := chromeui.FindAll(ctx, tconn, chromeui.FindParams{Role: chromeui.RoleTypeTab, ClassName: "Tab"})
+	if err != nil {
+		s.Fatal("Failed to find tabs: ", err)
+	}
+	defer tabs.Release(ctx)
+	if len(tabs) != 2 {
+		s.Fatalf("expected 2 tabs, only found %v tab(s)", len(tabs))
 	}
 
 	pv := perf.NewValues()
-	for _, h := range hists {
-		mean, err := h.Mean()
-		if err != nil {
-			s.Fatalf("Failed to get mean for histogram %s: %v", h.Name, err)
-		}
-		pv.Set(perf.Metric{
-			Name:      h.Name,
-			Unit:      "percent",
-			Direction: perf.BiggerIsBetter,
-		}, mean)
+	for _, data := range []struct {
+		tab    *chromeui.Node
+		suffix string
+	}{
+		{tabs[0], "inactive"},
+		{tabs[1], "active"},
+	} {
+		s.Run(ctx, data.suffix, func(ctx context.Context, s *testing.State) {
+			// Stabilize CPU usage.
+			if err := cpu.WaitUntilIdle(ctx); err != nil {
+				s.Error("Failed to wait for system UI to be stabilized: ", err)
+			}
+
+			hists, err := metrics.RunAndWaitAll(ctx, tconn, time.Second, func() error {
+				if err := mouse.Move(ctx, tconn, center, 0); err != nil {
+					s.Fatalf("Failed to put mouse to the center: ", err)
+				}
+				if err := mouse.Move(ctx, tconn, data.tab.Location.CenterPoint(), time.Second); err != nil {
+					s.Fatalf("Failed to move mouse to the %s tab: ", data.suffix, err)
+				}
+				// Hover on the tab.
+				if err := testing.Sleep(ctx, 5*time.Second); err != nil {
+					s.Fatal("Failed to sleep for 5 seconds: ", err)
+				}
+				if err := mouse.Move(ctx, tconn, center, time.Second); err != nil {
+					s.Fatal("Failed to move mouse back to the center: ", err)
+				}
+				return nil
+			},
+				"Chrome.Tabs.AnimationSmoothness.HoverCard.FadeIn",
+				"Chrome.Tabs.AnimationSmoothness.HoverCard.FadeOut")
+			if err != nil {
+				s.Fatal("Failed to move mouse or get the histogram: ", err)
+			}
+
+			for _, h := range hists {
+				mean, err := h.Mean()
+				if err != nil {
+					s.Fatalf("Failed to get mean for histogram %s (%s tab): %v ", h.Name, data.suffix, err)
+				}
+				pv.Set(perf.Metric{
+					Name:      h.Name + "." + data.suffix,
+					Unit:      "percent",
+					Direction: perf.BiggerIsBetter,
+				}, mean)
+			}
+		})
 	}
 	if err := pv.Save(s.OutDir()); err != nil {
-		s.Error("Failed saving perf data: ", err)
+		s.Fatal("Failed to save perf data: ", err)
 	}
 }
