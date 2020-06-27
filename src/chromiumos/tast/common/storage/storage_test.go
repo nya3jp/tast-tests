11351deeb6f36b97ecdd83753cfc6f3bf51e6721
Alexis Savery <asavery@chromium.org>
2020-06-26 22:30:36 -0700

    platform: storage: change fail case for SATA and NVMe
    Since tests show that devices can work beyond the manufacturer
    recommendation, we should be more stringent and not look at the
    Percentage Used. For SATA we should use attribute 160
    Uncorrectable_Error_Cnt or 187 Reported_Uncorrect. For NVMe we
    should look at Available Spare and fail if it is below Available
    Spare Threshold.
    
    BUG=b:154925099
    TEST=Ran platform.StorageWearoutDetect
    
    Change-Id: Iee545b15f1d22bdc36ae069ffbd6232c6e3bc034

--- src/chromiumos/tast/common/storage/storage_test.go
+++ src/chromiumos/tast/common/storage/storage_test.go
@@ -69,8 +69,9 @@ Device life time estimation type A [DEVICE_LIFE_TIME_EST_TYP_A: 0x00]
 
 func TestParseGetStorageInfoOutputSimpleHealthyNVMe(t *testing.T) {
 	const out = `
-	SMART/Health Information (NVMe Log 0x02, NSID 0xffffffff)
-	Percentage Used:                        25%
+        SMART/Health Information (NVMe Log 0x02, NSID 0xffffffff)
+        Available Spare:                        25%
+        Available Spare Threshold:              10%
 `
 
 	info, err := parseGetStorageInfoOutput([]byte(out))
@@ -91,8 +92,9 @@ func TestParseGetStorageInfoOutputSimpleHealthyNVMe(t *testing.T) {
 
 func TestParseGetStorageInfoOutputSimpleFailingNVMe(t *testing.T) {
 	const out = `
-	SMART/Health Information (NVMe Log 0x02, NSID 0xffffffff)
-	Percentage Used:                        100%
+        SMART/Health Information (NVMe Log 0x02, NSID 0xffffffff)
+        Available Spare:                        8%
+        Available Spare Threshold:              10%
 `
 
 	info, err := parseGetStorageInfoOutput([]byte(out))
@@ -113,7 +115,7 @@ func TestParseGetStorageInfoOutputSimpleFailingNVMe(t *testing.T) {
 
 func TestParseGetStorageInfoOutputSimpleHealthySSD(t *testing.T) {
 	const out = `
-	ATA Version is:   7
+        ATA Version is:   7
 ID# ATTRIBUTE_NAME          FLAGS    VALUE WORST THRESH FAIL RAW_VALUE
   5 Reallocated_Sector_Ct   -O----   100   100   000    -    0
   9 Power_On_Hours          -O----   100   100   000    -    333
@@ -139,7 +141,7 @@ ID# ATTRIBUTE_NAME          FLAGS    VALUE WORST THRESH FAIL RAW_VALUE
 
 func TestParseGetStorageInfoOutputSimpleFailingSSD(t *testing.T) {
 	const out = `
-	ATA Version is:   7
+        ATA Version is:   7
 ID# ATTRIBUTE_NAME          FLAGS    VALUE WORST THRESH FAIL RAW_VALUE
   5 Reallocated_Sector_Ct   -O----   100   100   000    -    0
   9 Power_On_Hours          -O----   100   100   000    -    333
@@ -165,25 +167,13 @@ ID# ATTRIBUTE_NAME          FLAGS    VALUE WORST THRESH FAIL RAW_VALUE
 
 func TestParseGetStorageInfoOutputSimpleHealthySSDPercentage(t *testing.T) {
 	const out = `
-	ATA Version is:   7
-Device Statistics (GP Log 0x04)
-Page  Offset Size        Value Flags Description
-0x05  =====  =               =  ===  == Temperature Statistics (rev 1) ==
-0x05  0x008  1              45  ---  Current Temperature
-0x05  0x010  1               -  ---  Average Short Term Temperature
-0x05  0x018  1               -  ---  Average Long Term Temperature
-0x05  0x020  1              57  ---  Highest Temperature
-0x05  0x028  1              26  ---  Lowest Temperature
-0x05  0x030  1               -  ---  Highest Average Short Term Temperature
-0x05  0x038  1               -  ---  Lowest Average Short Term Temperature
-0x05  0x040  1               -  ---  Highest Average Long Term Temperature
-0x05  0x048  1               -  ---  Lowest Average Long Term Temperature
-0x05  0x050  4               0  ---  Time in Over-Temperature
-0x05  0x058  1              95  ---  Specified Maximum Operating Temperature
-0x05  0x060  4               0  ---  Time in Under-Temperature
-0x05  0x068  1               0  ---  Specified Minimum Operating Temperature
-0x07  =====  =               =  ===  == Solid State Device Statistics (rev 1) ==
-0x07  0x008  1               2  N--  Percentage Used Endurance Indicator
+        ATA Version is:   7
+ID# ATTRIBUTE_NAME          FLAGS    VALUE WORST THRESH FAIL RAW_VALUE
+  5 Reallocated_Sector_Ct   -O----   100   100   000    -    0
+  9 Power_On_Hours          -O----   100   100   000    -    333
+ 12 Power_Cycle_Count       -O----   100   100   000    -    1758
+165 Total_Write/Erase_Count -O----   100   100   000    -    174113
+187 Reported_Uncorrect      -O--CK   100   100   000    -    0
 `
 
 	info, err := parseGetStorageInfoOutput([]byte(out))
@@ -204,25 +194,13 @@ Page  Offset Size        Value Flags Description
 
 func TestParseGetStorageInfoOutputSimpleFailingSSDPercentage(t *testing.T) {
 	const out = `
-	ATA Version is:   7
-Device Statistics (GP Log 0x04)
-Page  Offset Size        Value Flags Description
-0x05  =====  =               =  ===  == Temperature Statistics (rev 1) ==
-0x05  0x008  1              45  ---  Current Temperature
-0x05  0x010  1               -  ---  Average Short Term Temperature
-0x05  0x018  1               -  ---  Average Long Term Temperature
-0x05  0x020  1              57  ---  Highest Temperature
-0x05  0x028  1              26  ---  Lowest Temperature
-0x05  0x030  1               -  ---  Highest Average Short Term Temperature
-0x05  0x038  1               -  ---  Lowest Average Short Term Temperature
-0x05  0x040  1               -  ---  Highest Average Long Term Temperature
-0x05  0x048  1               -  ---  Lowest Average Long Term Temperature
-0x05  0x050  4               0  ---  Time in Over-Temperature
-0x05  0x058  1              95  ---  Specified Maximum Operating Temperature
-0x05  0x060  4               0  ---  Time in Under-Temperature
-0x05  0x068  1               0  ---  Specified Minimum Operating Temperature
-0x07  =====  =               =  ===  == Solid State Device Statistics (rev 1) ==
-0x07  0x008  1              99  N--  Percentage Used Endurance Indicator
+        ATA Version is:   7
+ID# ATTRIBUTE_NAME          FLAGS    VALUE WORST THRESH FAIL RAW_VALUE
+  5 Reallocated_Sector_Ct   -O----   100   100   000    -    0
+  9 Power_On_Hours          -O----   100   100   000    -    333
+ 12 Power_Cycle_Count       -O----   100   100   000    -    1758
+165 Total_Write/Erase_Count -O----   100   100   000    -    174113
+187 Reported_Uncorrect      -O--CK   100   100   000    -    102
 `
 
 	info, err := parseGetStorageInfoOutput([]byte(out))
