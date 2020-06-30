3c1da8f7b27cb31a5f061ea2b3d61010e1854df1
Drew Davenport <ddavenport@chromium.org>
2020-06-30 10:32:08 -0600

    Add graphics.IGT.* tests
    Initial framework to run igt-gpu-tools tests through tast
    
    The initial test are chosen somewhat arbitrarily and are expected to be
    tweaked as a list of important tests are better defined.
    
    BUG=b:161825407
    TEST=tast run $dut graphics.IGT.*
    
    Change-Id: I277e723699e2861502f09124b1cc9a072ed70d13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2278109
    Commit-Queue: Ilja H. Friedel <ihf@chromium.org>
    Tested-by: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/igt.go
@@ -0,0 +1,77 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+// igtTest is used to describe the config used to run each test.
+type igtTest struct {
+	exe string // The test executable name.
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: IGT,
+		Desc: "Verifies igt-gpu-tools test binaries run successfully",
+		Contacts: []string{
+			"ddavenport@chromium.org",
+			"chromeos-gfx@google.com",
+		},
+		Params: []testing.Param{{
+			Name: "kms_atomic",
+			Val: igtTest{
+				exe: "kms_atomic",
+			},
+			ExtraSoftwareDeps: []string{"drm_atomic"},
+			Timeout:           5 * time.Minute,
+		}, {
+			Name: "kms_addfb_basic",
+			Val: igtTest{
+				exe: "kms_addfb_basic",
+			},
+			ExtraSoftwareDeps: []string{"drm_atomic"},
+			Timeout:           5 * time.Minute,
+		}, {
+			Name: "kms_plane",
+			Val: igtTest{
+				exe: "kms_plane",
+			},
+			ExtraSoftwareDeps: []string{"drm_atomic"},
+			Timeout:           5 * time.Minute,
+		}},
+		Attr: []string{"group:graphics"},
+	})
+}
+
+func IGT(ctx context.Context, s *testing.State) {
+	if err := upstart.StopJob(ctx, "ui"); err != nil {
+		s.Fatal("Failed to stop ui job: ", err)
+	}
+
+	testOpt := s.Param().(igtTest)
+
+	f, err := os.Create(filepath.Join(s.OutDir(), filepath.Base(testOpt.exe)+".txt"))
+	if err != nil {
+		s.Fatal("Failed to create a log file: ", err)
+	}
+	defer f.Close()
+
+	exePath := filepath.Join("/usr/local/libexec/igt-gpu-tools", testOpt.exe)
+	cmd := testexec.CommandContext(ctx, exePath)
+	cmd.Stdout = f
+	cmd.Stderr = f
+	if err := cmd.Run(); err != nil {
+		s.Errorf("Failed to run %s: %v", exePath, err)
+	}
+}
