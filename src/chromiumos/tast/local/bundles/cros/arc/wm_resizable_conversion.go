8f23d9c42716e2a5fe4a29d84277774cf65ecb9b
armenk <armenk@google.com>
2020-06-30 13:24:40 -0700

    Add RV19 Tast-test for WM ARC++ R
    Add wm_resizable_conversion.go and implement RV19:
    resizable/conversion: landscape described in ARC++ R WM specs located
    in go/arc-wm-r. RV19 covers conversion between clamshell and tablet
    mode and checks the window states.
    First in clamshell mode, a resizable activity is launched and
    conversion to tablet mode happened, then it gets back to clamshell.
    In every mode the activity's state is asserted to the desired state.
    BUG=b:140203139
    TEST=tast run $DUT arc.WMResizableConversion
    
    Change-Id: I9dc53776148e24a4129f3ee4248e03a5f5587c0c

---
+++ src/chromiumos/tast/local/bundles/cros/arc/wm_resizable_conversion.go
@@ -0,0 +1,129 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arc/wm"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WMResizableConversion,
+		Desc:         "Verifies that Window Manager resizable/conversion use-cases behaves as described in go/arc-wm-r",
+		Contacts:     []string{"armenk@google.com", "arc-framework+tast@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"android_vm", "chrome"},
+		Pre:          arc.Booted(),
+		Timeout:      8 * time.Minute,
+	})
+}
+
+func WMResizableConversion(ctx context.Context, s *testing.State) {
+	wm.SetupAndRunTestCases(ctx, s, false, []wm.TestCase{
+		wm.TestCase{
+			// resizable/conversion: landscape
+			Name: "RV_conversion_landscape",
+			Func: wmRV19,
+		},
+	})
+}
+
+// wmRV19 covers resizable/conversion behavior in landscape mode.
+// Expected behavior is defined in: go/arc-wm-r RV19 resizable/conversion: landscape.
+func wmRV19(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device) error {
+	act, err := arc.NewActivity(a, wm.Pkg24, wm.ResizableLandscapeActivity)
+	if err != nil {
+		return err
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx, tconn); err != nil {
+		return err
+	}
+	defer act.Stop(ctx, tconn)
+
+	if err := wm.WaitUntilActivityIsReady(ctx, tconn, act, d); err != nil {
+		return err
+	}
+
+	// Store activity's original window info to be compared with after tablet mode disabled.
+	windowInfoBeforeTabletMode, err := ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24)
+	if err != nil {
+		return nil
+	}
+
+	if err := wm.CheckRestoreResizable(ctx, tconn, act, d); err != nil {
+		return err
+	}
+
+	// windowID will be used to wait on certain conditions.
+	var windowID = windowInfoBeforeTabletMode.ID
+
+	// Enable tablet mode, the activity should go to Maximized state.
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, true)
+	if err != nil {
+		return errors.Wrap(err, "failed to ensure if tablet mode is enabled")
+	}
+	defer cleanup(ctx)
+
+	if err := ash.WaitForARCAppWindowState(ctx, tconn, wm.Pkg24, ash.WindowStateMaximized); err != nil {
+		return err
+	}
+	if err := ash.WaitWindowFinishAnimating(ctx, tconn, windowID); err != nil {
+		return err
+	}
+	if err := ash.WaitForCondition(ctx, tconn, func(w *ash.Window) bool {
+		return w.ID == windowID && w.IsFrameVisible == false
+	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
+		return errors.Wrap(err, "failed to wait for frame to get hidden")
+	}
+
+	// Store activity's window info when tablet mode is enabled to make sure it is in Maximized state.
+	windowInfoAtTabletMode, err := ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24)
+	if err != nil {
+		return err
+	}
+
+	// Compare activity's window TargetBounds to primary display work area.
+	if err := wm.CheckMaximizeWindowInTabletMode(ctx, tconn, *windowInfoAtTabletMode); err != nil {
+		return err
+	}
+
+	// Disable tablet mode.
+	if err := ash.SetTabletModeEnabled(ctx, tconn, false); err != nil {
+		return errors.Wrap(err, "failed to disable tablet mode")
+	}
+
+	if err := ash.WaitForCondition(ctx, tconn, func(w *ash.Window) bool {
+		return w.ID == windowID && w.IsFrameVisible == true
+	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
+		return errors.Wrap(err, "failed to wait for frame to become visible")
+	}
+
+	windowInfoAfterTabletMode, err := ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24)
+	if err != nil {
+		return err
+	}
+
+	if err := wm.CheckRestoreResizable(ctx, tconn, act, d); err != nil {
+		return err
+	}
+
+	// Activity should have same TargetBounds that it had before enabling tablet mode.
+	if windowInfoBeforeTabletMode.TargetBounds != windowInfoAfterTabletMode.TargetBounds {
+		return errors.Errorf("failed to retrieve original window bounds after switching back from tablet mode, got: %s, want: %s", windowInfoAfterTabletMode.TargetBounds, windowInfoBeforeTabletMode.TargetBounds)
+	}
+
+	return nil
+}
