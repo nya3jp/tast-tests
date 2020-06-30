1a87cf6b9050f5fbbf37db76dca368ecbdcab6f6
Brendan Hansknecht <bhansknecht@chromium.org>
2020-06-30 11:28:12 -0700

    tast-tests: appcompat update to filepath
    Use filepath when calculating the path for screenshots instead of using
    fmt.Sprintf().
    
    BUG=None
    TEST=tast run <eve> arcappcompat.*
    
    Change-Id: I6d8cdbc0dcfdc4cefff21da865fa2e1bc8d4f8de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2275739
    Tested-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Auto-Submit: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Commit-Queue: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Commit-Queue: Brendan Hansknecht <bhansknecht@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/hbomax.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/hbomax.go
@@ -8,6 +8,7 @@ package arcappcompat
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 	"time"
 
 	"chromiumos/tast/local/arc"
@@ -115,7 +116,8 @@ func Hbomax(ctx context.Context, s *testing.State) {
 
 			defer func() {
 				if s.HasError() {
-					path := fmt.Sprintf("%s/screenshot-arcappcompat-failed-test-%d.png", s.OutDir(), idx)
+					filename := fmt.Sprintf("screenshot-arcappcompat-failed-test-%d.png", idx)
+					path := filepath.Join(s.OutDir(), filename)
 					if err := screenshot.CaptureChrome(ctx, cr, path); err != nil {
 						s.Log("Failed to capture screenshot: ", err)
 					}
