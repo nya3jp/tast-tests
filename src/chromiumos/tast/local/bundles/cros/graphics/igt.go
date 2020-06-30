db24c68fd5323dd859df017e35526b9d08cb9298
Drew Davenport <ddavenport@chromium.org>
2020-06-30 10:32:08 -0600

    Add graphics.IGT.* tests
    Initial framework to run igt-gpu-tools tests through tast
    
    BUG=none
    TEST=tast run $dut graphics.IGT.*
    
    Change-Id: I277e723699e2861502f09124b1cc9a072ed70d13

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/igt.go
@@ -0,0 +1,104 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/timing"
+)
+
+func uiStopped() testing.Precondition { return uiStoppedPre }
+
+var uiStoppedPre = &preImpl{
+	name:    "ui_stopped",
+	timeout: 30 * time.Second,
+}
+
+type preImpl struct {
+	name    string
+	timeout time.Duration
+}
+
+// Interface methods for a testing.Precondition.
+func (p *preImpl) String() string         { return p.name }
+func (p *preImpl) Timeout() time.Duration { return p.timeout }
+
+func (p *preImpl) Prepare(ctx context.Context, s *testing.PreState) interface{} {
+	ctx, st := timing.Start(ctx, "prepare_"+p.name)
+	defer st.End()
+
+	// This is a no-op if the job is not currently running.
+	err := upstart.StopJob(ctx, "ui")
+	if err != nil {
+		s.Fatal("Failed to stop ui: ", err)
+	}
+	return nil
+}
+
+func (p *preImpl) Close(ctx context.Context, s *testing.PreState) {
+	ctx, st := timing.Start(ctx, "close_"+p.name)
+	defer st.End()
+
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Log("Failed to restart ui job: ", err)
+	}
+}
+
+// drmTest is used to describe the config used to run each drm_test.
+type igtTest struct {
+	exe     string        // The command path to be run. This should be relative to /usr/local/bin.
+	timeout time.Duration // Timeout to run the drmTest.
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: IGT,
+		Desc: "Verifies DRM-related test binaries run successfully",
+		Contacts: []string{
+			"ddavenport@chromium.org",
+			"chromeos-gfx@google.com",
+		},
+		Params: []testing.Param{{
+			Name: "kms_atomic",
+			Val: igtTest{
+				exe:     "kms_atomic",
+				timeout: 5 * time.Minute,
+			},
+			ExtraSoftwareDeps: []string{"drm_atomic"},
+			ExtraAttr:         []string{"informational"},
+			Pre:               uiStopped(),
+		}},
+		Timeout: 5 * time.Minute,
+		Attr:    []string{"group:mainline"},
+	})
+}
+
+func IGT(ctx context.Context, s *testing.State) {
+	testOpt := s.Param().(igtTest)
+
+	f, err := os.Create(filepath.Join(s.OutDir(), filepath.Base(testOpt.exe)+".txt"))
+	if err != nil {
+		s.Fatal("Failed to create a log file: ", err)
+	}
+	defer f.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, testOpt.timeout)
+	defer cancel()
+
+	exePath := filepath.Join("/usr/local/libexec/igt-gpu-tools", testOpt.exe)
+	cmd := testexec.CommandContext(ctx, exePath)
+	cmd.Stdout = f
+	cmd.Stderr = f
+	if err := cmd.Run(); err != nil {
+		s.Errorf("Failed to run %s: %v", exePath, err)
+	}
+}
