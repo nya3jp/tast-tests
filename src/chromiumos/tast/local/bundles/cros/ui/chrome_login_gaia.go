f7f7b4c6e4cf9d96175b3fc63ffc6f7411748b22
Sergei Datsenko <dats@google.com>
2020-07-02 12:01:47 +1000

    tast-tests: Add a smoke test for real GAIA logins
    BUG=chromium:1101611
    TEST=ui.ChromeLoginGaia
    
    Cq-Depend: chrome-internal:3144356
    Change-Id: Ic836f89710943396f8bb959e18533cf51864c1a0

---
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_login_gaia.go
@@ -0,0 +1,142 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/session"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ChromeLoginGaia,
+		Desc: "Checks that Chrome can make real GAIA logins",
+		Contacts: []string{
+			"chromeos-ui@google.com",
+			"tast-owners@google.com",
+		},
+		SoftwareDeps: []string{
+			"chrome",
+			"chrome_internal",
+		},
+		Attr: []string{
+			"group:mainline",
+			"informational",
+		},
+		Vars: []string{
+			"ui.ChromeLoginGaia.user",
+			"ui.ChromeLoginGaia.password",
+		},
+	})
+}
+
+func ChromeLoginGaia(ctx context.Context, s *testing.State) {
+	sm := func() *session.SessionManager {
+		// Set up the test environment. Should be done quickly.
+		const setupTimeout = 30 * time.Second
+		setupCtx, cancel := context.WithTimeout(ctx, setupTimeout)
+		defer cancel()
+
+		// Ensures login screen.
+		if err := upstart.RestartJob(setupCtx, "ui"); err != nil {
+			s.Fatal("Chrome logout failed: ", err)
+		}
+
+		sm, err := session.NewSessionManager(setupCtx)
+		if err != nil {
+			s.Fatal("Failed to connect session_manager: ", err)
+		}
+		return sm
+	}()
+
+	const content = "Hooray, it worked!"
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, content)
+	}))
+	defer server.Close()
+
+	user := s.RequiredVar("ui.ChromeLoginGaia.user")
+	password := s.RequiredVar("ui.ChromeLoginGaia.password")
+
+	testChromeLogin(ctx, s, sm, server.URL, content, user, password)
+}
+
+func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionManager, url, expected, user, password string) {
+	const timeoutPerRun = time.Minute
+	ctx, cancel := context.WithTimeout(ctx, timeoutPerRun)
+	defer cancel()
+
+	func() {
+		// Start listening for a "started" SessionStateChanged D-Bus signal from session_manager.
+		sw, err := sm.WatchSessionStateChanged(ctx, "started")
+		if err != nil {
+			s.Fatal("Failed to watch for D-Bus signals: ", err)
+		}
+		defer sw.Close(ctx)
+
+		cr, err := chrome.New(
+			ctx,
+			chrome.Auth(user, password, ""),
+			chrome.GAIALogin(),
+		)
+		if err != nil {
+			s.Fatal("Chrome login failed: ", err)
+		}
+		defer cr.Close(ctx)
+
+		s.Log("Waiting for SessionStateChanged \"started\" D-Bus signal from session_manager")
+		select {
+		case <-sw.Signals:
+			s.Log("Got SessionStateChanged signal")
+		case <-ctx.Done():
+			s.Fatal("Didn't get SessionStateChanged signal: ", ctx.Err())
+		}
+
+		conn, err := cr.NewConn(ctx, url)
+		if err != nil {
+			s.Fatal("Creating renderer failed: ", err)
+		}
+		defer conn.Close()
+
+		if err := conn.WaitForExpr(ctx, "document.readyState === 'complete'"); err != nil {
+			s.Fatal("Waiting load failed: ", err)
+		}
+
+		var actual string
+		if err := conn.Eval(ctx, "document.documentElement.innerText", &actual); err != nil {
+			s.Fatal("Getting page content failed: ", err)
+		}
+		if actual != expected {
+			s.Fatalf("Unexpected page content: got %q; want %q", actual, expected)
+		}
+	}()
+
+	sw, err := sm.WatchSessionStateChanged(ctx, "stopped")
+	if err != nil {
+		s.Fatal("Failed to watch for D-Bus signals: ", err)
+	}
+	defer sw.Close(ctx)
+
+	// Emulate logout.
+	if err = upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Fatal("Chrome logout failed: ", err)
+	}
+
+	s.Log("Waiting for SessionStateChanged \"stopped\" D-Bus signal from session_manager")
+	select {
+	case <-sw.Signals:
+		s.Log("Got SessionStateChanged signal")
+	case <-ctx.Done():
+		s.Fatal("Didn't get SessionStateChanged signal: ", ctx.Err())
+	}
+}
