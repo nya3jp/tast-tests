372902bb381a7b1cbbb57abbcc666ad8ddaf9688
Kartik Hegde <khegde@chromium.org>
2020-07-03 07:30:41 -0600

    tast: Add CSV validation to croshealthd
    In order to reduce duplicate code across croshealthd tast tests, add
    logic to validate the CSV by using function validators.
    
    BUG=chromium:1085420
    TEST=Tested with SystemInfo tast test (chromium:2262419)
    
    Change-Id: I16dd2738be8bf8e83b0b21a8ba375d401f19454d

--- src/chromiumos/tast/local/croshealthd/telem.go
+++ src/chromiumos/tast/local/croshealthd/telem.go
@@ -11,10 +11,15 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"reflect"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/crosconfig"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
 )
@@ -28,12 +33,12 @@ const (
 	TelemCategoryBacklight         TelemCategory = "backlight"
 	TelemCategoryBattery           TelemCategory = "battery"
 	TelemCategoryBluetooth         TelemCategory = "bluetooth"
-	TelemCategoryCachedVPD         TelemCategory = "cached_vpd"
 	TelemCategoryCPU               TelemCategory = "cpu"
 	TelemCategoryFan               TelemCategory = "fan"
 	TelemCategoryMemory            TelemCategory = "memory"
 	TelemCategoryStatefulPartition TelemCategory = "stateful_partition"
 	TelemCategoryStorage           TelemCategory = "storage"
+	TelemCategorySystem            TelemCategory = "system"
 	TelemCategoryTimezone          TelemCategory = "timezone"
 )
 
@@ -77,4 +82,191 @@ func RunAndParseTelem(ctx context.Context, category TelemCategory, outDir string
 	}
 
 	return records, nil
+}
+
+// ValidCSV is responsible for validating headers and all values of the CSV
+// output. An error is returned if the headers are invalid or if any of the CSV
+// values are incorrect as determined by the validators provided to verify a
+// particular CSV value.
+func ValidCSV(csv [][]string, headers []string, columns ...func(string) error) error {
+	// Validate headers
+	if ok := reflect.DeepEqual(csv[0], headers); !ok {
+		return errors.Errorf("incorrect headers: got %v want %v", csv[0], headers)
+	}
+	// Validate columns
+	for i, column := range columns {
+		if err := column(csv[1][i]); err != nil {
+			return errors.Wrapf(err, "failed validation on %v (Column %v) with value %v", headers[i], i, csv[1][i])
+		}
+	}
+	return nil
+}
+
+// Headers returns a slice of the headers.
+func Headers(headers ...string) []string {
+	return headers
+}
+
+// Column takes in a list of validators that will verify different properties
+// of |csvValue|. Normally, |validators| will have a size of 1 here which will
+// be a top level validator (e.g. String, UInt64) that takes as input a list of
+// more specific validators. Column returns a function that will be invoked in
+// ValidCSV().
+func Column(validators ...func(interface{}) error) func(string) error {
+	return func(csvValue string) error {
+		for _, validator := range validators {
+			if err := validator(csvValue); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+// String returns a function that checks whether |actualValue| is of type string
+// and invokes any validators that further verify the properties of
+// |actualValue|.
+func String(validators ...func(interface{}) error) func(interface{}) error {
+	return func(actualValue interface{}) error {
+		if str, ok := actualValue.(string); ok {
+			// Invoke all the validators
+			for _, validator := range validators {
+				if err := validator(str); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+		return errors.Errorf("invalid parameter type in String: got %T, want string", actualValue)
+	}
+}
+
+// UInt64 returns a function that checks whether |actualValue| is of type uint64 and
+// invokes any validators that further verify the properties of |actualValue|.
+func UInt64(validators ...func(interface{}) error) func(interface{}) error {
+	return func(actualValue interface{}) error {
+		if str, ok := actualValue.(string); ok {
+			// Confirm |actualValue| type can be converted to uint64
+			_, err := strconv.ParseUint(str, 10, 64)
+			if err != nil {
+				return errors.Wrapf(err, "failed to convert %v to uint64", str)
+			}
+			// Invoke all the validators
+			for _, validator := range validators {
+				if err = validator(str); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+		return errors.Errorf("invalid parameter type in UInt64: got %T, want string", actualValue)
+	}
+}
+
+// CorrectFormat returns a function that checks whether |actualValue| matches
+// the regex pattern specified by |regex|.
+func CorrectFormat(regex interface{}) func(interface{}) error {
+	return func(actualValue interface{}) error {
+		var re *regexp.Regexp
+		if r, ok := regex.(*regexp.Regexp); ok {
+			re = r
+		} else {
+			return errors.Errorf("invalid parameter type in CorrectFormat: got %T, want *regexp.Regexp", regex)
+		}
+		if str, ok := actualValue.(string); ok {
+			// If the value does not exist, do not check the format.
+			if str == "NA" {
+				return nil
+			}
+			if matched := re.MatchString(str); !matched {
+				return errors.Errorf("failed to follow correct pattern: got %v, want %v", re, str)
+			}
+			return nil
+		}
+		return errors.Errorf("invalid parameter type in CorrectFormat: got %T, want string", actualValue)
+	}
+}
+
+// EqualToFileContent returns a function that checks whether |filePathAndName|
+// and exists. If it does, it compares the value at that location wih
+// |actualValue|. If it does not, it ensures that |actualValue| equals "NA".
+func EqualToFileContent(filePathAndName interface{}) func(interface{}) error {
+	return func(actualValue interface{}) error {
+		var file string
+		if str, ok := filePathAndName.(string); ok {
+			file = str
+		} else {
+			return errors.Errorf("invalid parameter type in EqualToFileContent: got %T, want string", filePathAndName)
+		}
+		if actual, ok := actualValue.(string); ok {
+			expectedValueByteArr, err := ioutil.ReadFile(file)
+			if os.IsNotExist(err) {
+				if actual != "NA" {
+					return errors.Errorf("failed to get correct value: got %v, want NA", actual)
+				}
+			} else if err != nil {
+				return errors.Wrapf(err, "failed to read from file %v", file)
+			} else {
+				expected := strings.TrimRight(string(expectedValueByteArr), "\n")
+				if actual != expected {
+					return errors.Errorf("invalid parameter type in EqualToFileContent: got %v, want %v", actual, expected)
+				}
+			}
+			return nil
+		}
+		return errors.Errorf("invalid parameter type in EqualToFileContent: got %T, want string", actualValue)
+	}
+}
+
+// CheckFileContentIfFileShouldExist returns a function that checks whether
+// |filePathAndName| should exist by using crosconfig and its two arguments,
+// |crosConfigProperty| and |crosConfigPath|. If the file should exist, it
+// attempts to read the value from the file. If it cannot, an error is
+// reported. If it can, it compares the read value with |actualValue|.
+func CheckFileContentIfFileShouldExist(ctx context.Context, crosConfigPath,
+	crosConfigProperty, filePathAndName string) func(interface{}) error {
+	return func(actualValue interface{}) error {
+		val, err := crosconfig.Get(ctx, crosConfigPath, crosConfigProperty)
+		if err != nil && !crosconfig.IsNotFound(err) {
+			errors.Wrapf(err, "failed to get crosconfig %v property", crosConfigProperty)
+		}
+		hasProperty := err == nil && val == "true"
+		if actual, ok := actualValue.(string); ok {
+			if !hasProperty {
+				if actual != "NA" {
+					return errors.Errorf("failed to get correct value: got %v, want NA", actual)
+				}
+				return nil
+			}
+			expectedValueByteArr, err := ioutil.ReadFile(filePathAndName)
+			if err != nil {
+				return errors.Wrapf(err, "failed to read file %v", filePathAndName)
+			}
+			expected := strings.TrimRight(string(expectedValueByteArr), "\n")
+			if actual != expected {
+				return errors.Errorf("failed to get correct value: got %v, want %v", actual, expected)
+			}
+			return nil
+		}
+		return errors.Errorf("invalid parameter type in CheckFileContentIfFileShouldExist: got %T, want string", actualValue)
+	}
+}
+
+// EqualToCrosConfigContent returns a function that uses crosconfig and its two
+// arguments, |crosConfigProperty| and |crosConfigPath| to obtain a value that
+// is compared with |actualValue|.
+func EqualToCrosConfigContent(ctx context.Context, crosConfigPath, crosConfigProperty string) func(interface{}) error {
+	return func(actualValue interface{}) error {
+		expected, err := crosconfig.Get(ctx, crosConfigPath, crosConfigProperty)
+		if err != nil && !crosconfig.IsNotFound(err) {
+			errors.Wrapf(err, "failed to get crosconfig %v property", crosConfigProperty)
+		}
+		if actual, ok := actualValue.(string); ok {
+			if actual != expected {
+				return errors.Errorf("failed to get correct value: got %v, want %v", actual, expected)
+			}
+			return nil
+		}
+		return errors.Errorf("invalid parameter type in EqualToCrosConfigContent: got %T, want string", actualValue)
+	}
 }
