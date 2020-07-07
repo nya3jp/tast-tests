dda30143f749541290698b51f8114147dea5f111
armenk <armenk@google.com>
2020-07-07 16:42:21 -0700

    Add RC02 Tast-test for WM ARC++ R
    Implement RC02: resizable/clamshell: maximize portrait app behavior
    described in ARC++ R WM specs located in go/arc-wm-r.
    In RC02 a resizable activity will start and the maximize button on the
    caption bar will be touched, the window will go to maximize state,
    then the restore button on the caption bar will be touched.The window
    will go to restore state. The window bounds will be compared before
    and after switching to maximize mode.
    BUG=b:140203139
    TEST=tast run $DUT arc.WMResizableClamshell
    
    Change-Id: If76862c4043538192b4a96cb9a5a3fced1231b46

--- src/chromiumos/tast/local/bundles/cros/arc/wm_resizable_clamshell.go
+++ src/chromiumos/tast/local/bundles/cros/arc/wm_resizable_clamshell.go
@@ -14,7 +14,10 @@ import (
 	"chromiumos/tast/local/bundles/cros/arc/wm"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/display"
+	crui "chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/local/coords"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
 
@@ -42,6 +45,11 @@ func WMResizableClamshell(ctx context.Context, s *testing.State) {
 			// resizable/clamshell: default launch behavior
 			Name: "RC01_launch",
 			Func: wmRC01,
+		},
+		wm.TestCase{
+			// resizable/clamshell: resizable/clamshell: maximize portrait app (pillarbox)
+			Name: "RC02_maximize_portrait",
+			Func: wmRC02,
 		},
 	})
 }
@@ -222,5 +230,127 @@ func wmRC01(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 			return err
 		}
 	}
+	return nil
+}
+
+func wmRC02(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device) error {
+	act, err := arc.NewActivity(a, wm.Pkg24, wm.ResizablePortraitActivity)
+	if err != nil {
+		return err
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx, tconn); err != nil {
+		return err
+	}
+	defer act.Stop(ctx, tconn)
+
+	if err := wm.WaitUntilActivityIsReady(ctx, tconn, act, d); err != nil {
+		return err
+	}
+
+	if err := wm.CheckRestoreResizable(ctx, tconn, act, d); err != nil {
+		return err
+	}
+
+	// Store windows info before maximizing the activity to compare it with after restoring it.
+	winInfoBeforeMax, err := ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24)
+	if err != nil {
+		return err
+	}
+
+	// Store windowID to wait for animating finishes.
+	windowID := winInfoBeforeMax.ID
+
+	// Touch maximize button on caption bar.
+	if err := touchCaptionButton(ctx, tconn, "Maximize"); err != nil {
+		return err
+	}
+
+	if err := ash.WaitForARCAppWindowState(ctx, tconn, wm.Pkg24, ash.WindowStateMaximized); err != nil {
+		return err
+	}
+	if err := ash.WaitWindowFinishAnimating(ctx, tconn, windowID); err != nil {
+		return err
+	}
+
+	if err := wm.CheckMaximizeResizable(ctx, tconn, act, d); err != nil {
+		return err
+	}
+
+	// Touch restore button on caption bar.
+	if err := touchCaptionButton(ctx, tconn, "Restore"); err != nil {
+		return err
+	}
+
+	if err := ash.WaitForARCAppWindowState(ctx, tconn, wm.Pkg24, ash.WindowStateNormal); err != nil {
+		return err
+	}
+	if err := ash.WaitWindowFinishAnimating(ctx, tconn, windowID); err != nil {
+		return err
+	}
+
+	if err := wm.CheckRestoreResizable(ctx, tconn, act, d); err != nil {
+		return err
+	}
+
+	// Get window info after restoring, this should be equal to winInfoBeforeMax.
+	winInfoAfterMax, err := ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24)
+	if err != nil {
+		return err
+	}
+
+	// Compare BoundsInRoot of the activity before and after switching to maximize and restore button on caption bar.
+	if winInfoBeforeMax.BoundsInRoot != winInfoAfterMax.BoundsInRoot {
+		return errors.Errorf("failed to validate window bounds after restoring from maximize state, got: %s, want: %s", winInfoAfterMax.BoundsInRoot, winInfoBeforeMax.BoundsInRoot)
+	}
+
+	return nil
+}
+
+func touchCaptionButton(ctx context.Context, tconn *chrome.TestConn, btnName string) error {
+	captionBtn, err := crui.Find(ctx, tconn, crui.FindParams{ClassName: "FrameCaptionButton", Name: btnName})
+	if err != nil {
+		return err
+	}
+	tsw, err := input.Touchscreen(ctx)
+	if err != nil {
+		return err
+	}
+	defer tsw.Close()
+
+	stw, err := tsw.NewSingleTouchWriter()
+	if err != nil {
+		return err
+	}
+
+	// Get display info for touch coords calculation.
+	info, err := display.GetInternalInfo(ctx, tconn)
+	if err != nil {
+		return err
+	}
+
+	touchWidth := tsw.Width()
+	touchHeight := tsw.Height()
+
+	displayWidth := float64(info.Bounds.Width)
+	displayHeight := float64(info.Bounds.Height)
+
+	pixelToTouchFactorX := float64(touchWidth) / displayWidth
+	pixelToTouchFactorY := float64(touchHeight) / displayHeight
+
+	// Calculate caption button center X.
+	cBCX := input.TouchCoord(float64(captionBtn.Location.Left+captionBtn.Location.Width/2) * pixelToTouchFactorX)
+	// Calculate caption button center Y.
+	cBCY := input.TouchCoord(float64(captionBtn.Location.Top+captionBtn.Location.Height/2) * pixelToTouchFactorY)
+
+	// Touch caption button center.
+	if err := stw.Move(cBCX, cBCY); err != nil {
+		return err
+	}
+	if err := stw.End(); err != nil {
+		return err
+	}
+
 	return nil
 }
