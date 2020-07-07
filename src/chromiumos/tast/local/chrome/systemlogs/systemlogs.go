e39001884a0395173084c435eb5788fa42122bce
Steven Bennetts <stevenjb@chromium.org>
2020-07-07 15:58:27 -0700

    Add cros/logs smoke test and systemlogs helper
    The logs package will provide tests related to system logs
    starting with a Smoke test.
    
    The systemlogs package uses autotestPrivate.writeSystemLogs
    to generate a system logs file and read the contents for testing.
    
    BUG=chromium:550539
    TEST=ast run {target} logs.Smoke
    
    Change-Id: Ibccd9ee199cc06dee1eb52b0ed3ed8cac3c48620

---
+++ src/chromiumos/tast/local/chrome/systemlogs/systemlogs.go
@@ -0,0 +1,46 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package systemlogs calls autotestPrivate.writeSystemLogs and parses the results.
+package systemlogs
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/testexec"
+)
+
+// GetSystemLogs returns a string containing the complete contents of the
+// system logs file exported by chrome.autotestPrivate.writeSystemLogs.
+// The logs are written to a file in the /tmp directory which is removed
+// after this returns.
+func GetSystemLogs(ctx context.Context, tconn *chrome.TestConn) (string, error) {
+	dest := "/tmp"
+	var err error
+	var filepath string
+	if err = tconn.Call(ctx, &filepath, `tast.promisify(chrome.autotestPrivate.writeSystemLogs)`, dest); err != nil {
+		return "", err
+	}
+	defer os.Remove(filepath)
+
+	if !strings.HasSuffix(filepath, ".zip") {
+		return "", errors.New("system_logs file is not zipped")
+	}
+	if err := testexec.CommandContext(ctx, "unzip", "-u", filepath, "-d", dest).Run(testexec.DumpLogOnError); err != nil {
+		return "", errors.Wrap(err, "failed to unzip")
+	}
+
+	txtFilepath := filepath[0 : len(filepath)-4]
+	var systemLogs []byte
+	if systemLogs, err = ioutil.ReadFile(txtFilepath); err != nil {
+		return "", err
+	}
+	defer os.Remove(txtFilepath)
+	return string(systemLogs), nil
+}
