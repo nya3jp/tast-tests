dc7ea9c4633a52ffa75fae107d9e3c3771960c56
Shuhei Takahashi <nya@chromium.org>
2020-07-08 16:18:45 +0900

    meta: Add some local meta tests rebooting the DUT.
    meta.LocalReboot reboots the DUT. This test is useful to manually
    test Tast on unintentional reboots.
    
    meta.LocalKernelPanic triggers an intentional kernel panic. This test
    is useful to manually test Tast on kernel panics.
    
    BUG=None
    TEST=tast run DUT meta.LocalReboot
    TEST=tast run DUT meta.LocalKernelPanic
    
    Change-Id: Id12305f60533a976aab8d1a6b69b045be48c0a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2286974
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/meta/local_kernel_panic.go
@@ -0,0 +1,34 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package meta
+
+import (
+	"context"
+	"io/ioutil"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     LocalKernelPanic,
+		Desc:     "Triggers an intentional kernel panic with sysrq",
+		Contacts: []string{"tast-owners@google.com"},
+	})
+}
+
+func LocalKernelPanic(ctx context.Context, s *testing.State) {
+	// Save changes to the disk before triggering a kernel panic to avoid losing files.
+	if err := testexec.CommandContext(ctx, "sync").Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("sync failed: ", err)
+	}
+
+	// Trigger a kernel panic. Don't try this at home.
+	ioutil.WriteFile("/proc/sysrq-trigger", []byte("c"), 0666)
+
+	// Wait until the test timeout is reached.
+	<-ctx.Done()
+}
