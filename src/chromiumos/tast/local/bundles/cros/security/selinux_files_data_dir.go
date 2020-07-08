12bb43790d2c2356a3dcc192705bf4155423d22c
Vaibhav Raheja <vraheja@google.com>
2020-07-08 12:21:06 -0700

    tast-tests: Adding tests for /data directory
    Making first set of changes for including selinux context check for
    /data directory
    
    BUG=b:150782430
    TEST=tast run $IP security.SELinuxFilesDataDir
    
    Change-Id: I071330777431125e8ccbebe8c0060261ca327084

---
+++ src/chromiumos/tast/local/bundles/cros/security/selinux_files_data_dir.go
@@ -0,0 +1,125 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SELinuxFilesDataDir,
+		Desc:         "Checks SELinux labels specifically for the data dir in android-data",
+		Contacts:     []string{"vraheja@chromium.org", "chromeos-security@google.com"},
+		SoftwareDeps: []string{"selinux", "chrome"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		Attr:    []string{"group:mainline", "informational"},
+		Timeout: 5 * time.Minute,
+	})
+}
+
+const (
+	androidFileContexts = "/etc/selinux/arc/contexts/files/android_file_contexts"
+	baseDataDirPath     = "/home/.shadow/[a-z0-9]*/mount/root/android-data/data/"
+	dataDirFileContexts = "/tmp/data_file_contexts"
+)
+
+func verifyDirSelinuxContext(ctx context.Context, s *testing.State, dirName string) error {
+	directoryPath := baseDataDirPath + dirName
+	// Count the total files and sub-dirs in the dir
+	filesCountCmd := fmt.Sprintf("find %s | wc -l", directoryPath)
+	filesCount, err := testexec.CommandContext(ctx,
+		"sh", "-c", filesCountCmd).Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to count total files and sub-dirs in the dir: ", err)
+	}
+	// Count the total number of SELinux context verified files and sub-dirs in the dir
+	verifyCountCmd := fmt.Sprintf("find %s | xargs matchpathcon -f %s -V | grep verified | wc -l", directoryPath, dataDirFileContexts)
+	verifiedCount, err := testexec.CommandContext(ctx,
+		"sh", "-c", verifyCountCmd).Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to count verified files and sub-dirs in the dir: ", err)
+	}
+	// All the files should be verified, else trigger an error
+	if !bytes.Equal(filesCount, verifiedCount) {
+		err := errors.Errorf("Selinux context verification failed for %v", directoryPath)
+		return err
+	}
+
+	return nil
+}
+
+func SELinuxFilesDataDir(ctx context.Context, s *testing.State) {
+	s.Log("Creating the temporary SELinux context file for matchpathcon command")
+	// Create a temporary SeLinux Policy File
+	cmd := fmt.Sprintf("grep '/data' %s > %s", androidFileContexts, dataDirFileContexts)
+	_, err := testexec.CommandContext(ctx,
+		"sh", "-c", cmd).Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to create temporary Selinux Policy File: ", err)
+	}
+	// Correct paths in temporary SELinux policy file
+	replaceRawString := `s/\/data/\/home\/.shadow\/[a-z0-9]*\/mount\/root\/android-data\/data/g`
+	_, err = testexec.CommandContext(ctx,
+		"sed", "-i", replaceRawString, dataDirFileContexts).Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to correct paths in the Selinux policy file: ", err)
+	}
+	// List of /android-data/data/ directories to be checked
+	dirList := []string{
+		"adb",
+		"anr",
+		"app",
+		"app-asec",
+		"app-ephemeral",
+		"app-lib",
+		"app-private",
+		"backup",
+		"bootchart",
+		"cache",
+		"dalvik-cache",
+		"cache",
+		"drm",
+		"local",
+		"lost+found",
+		"media-drm",
+		"misc",
+		"misc_ce",
+		"misc_de",
+		"nfc",
+		"ota",
+		"ota_package",
+		"property",
+		"resource-cache",
+		"ss",
+		"system",
+		"system_de",
+		"tombstones",
+		"user",
+		"vendor",
+		"vendor_ce",
+		"vendor_de",
+	}
+
+	// Verify each dir for correct SELinux contexts
+	for _, dir := range dirList {
+		if err := verifyDirSelinuxContext(ctx, s, dir); err != nil {
+			s.Fatal("Selinux verification error: ", err)
+		}
+
+	}
+}
