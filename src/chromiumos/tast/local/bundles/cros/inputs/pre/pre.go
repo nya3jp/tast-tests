8ff9bab0d724c3101ef813c976e8395e72556cb1
Jun Sheng <shengjun@google.com>
2020-07-10 07:55:27 +1000

    tast-tests: exclude experimental boards from apps and inputs tests.
    1. Exclude kevin64 from test. kevin64 is an experimental board and does not support nacl.
    2. Create package for apps and inputs separately, since they will soon differentiate.
    
    BUG=b:160830151
    TEST=Ran on VM and device.
    
    Change-Id: Id0771dca9e9aee6ef129409c7331c83a06022355

---
+++ src/chromiumos/tast/local/bundles/cros/inputs/pre/pre.go
@@ -0,0 +1,12 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package pre contains preconditions for inputs tests
+package pre
+
+// ExcludeModels is list of models are not supposed to run inputs tests.
+var ExcludeModels = []string{
+	// Platform kevin64. It's the experimental board to verify arm64 user land.
+	"kevin64",
+}
--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing.go
@@ -13,11 +13,13 @@ import (
 	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/local/chrome/ui/faillog"
 	"chromiumos/tast/local/chrome/vkb"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -27,6 +29,7 @@ func init() {
 		Contacts:     []string{"essential-inputs-team@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
+		HardwareDeps: hwdep.D(hwdep.SkipOnModel(pre.ExcludeModels...)),
 		Timeout:      5 * time.Minute,
 	})
 }
