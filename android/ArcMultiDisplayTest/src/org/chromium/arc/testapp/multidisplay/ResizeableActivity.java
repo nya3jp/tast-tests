42c2d25b915311f3773ba287787cd95f5bc50386
Daichi Hirono <hirono@google.com>
2020-07-10 10:22:22 +0900

    Add APK to record config changes during multidisplay behavior
    BUG=b:161093205
    TEST=emerge-${BOARD} tast-local-apks-cros
    
    Change-Id: I880edd56c020a6e773ec35102ccf9e5a6f2e0481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2294606
    Reviewed-by: Ricardo Quesada <ricardoq@chromium.org>
    Tested-by: Daichi Hirono <hirono@chromium.org>
    Auto-Submit: Daichi Hirono <hirono@chromium.org>
    Commit-Queue: Daichi Hirono <hirono@chromium.org>

---
+++ android/ArcMultiDisplayTest/src/org/chromium/arc/testapp/multidisplay/ResizeableActivity.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.multidisplay;
+
+/**
+ * Activity which is marked as resizeable.
+ */
+public class ResizeableActivity extends BaseActivity {
+}
