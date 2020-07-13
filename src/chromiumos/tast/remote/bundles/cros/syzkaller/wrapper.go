017f02f870a302ca11cf4ee9b2695dafe89ac1d7
Zubin Mithra <zsm@chromium.org>
2020-07-13 15:34:01 -0700

    tast-tests: syzkaller: allow enabled syscalls
    Allow syzkaller to focus on fuzzing device drivers by specifying which
    syscalls to fuzz. This CL enables DRM i915 specific ioctls and mmap's.
    
    Also disable the syz-manager flag "-debug" as the SSH issue has been
    diagnosed.
    
    BUG=b:156755711
    TEST=tast -verbose run localhost:9222 syzkaller.Wrapper
    
    Change-Id: Ic4865c41c388f838ac8f6165dfaac78b626d1c48

--- src/chromiumos/tast/remote/bundles/cros/syzkaller/wrapper.go
+++ src/chromiumos/tast/remote/bundles/cros/syzkaller/wrapper.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"chromiumos/tast/dut"
@@ -29,6 +30,7 @@ const startupScriptContents = `
 mount -o remount,rw -o exec /tmp
 sysctl -w kernel.panic_on_warn=1
 dmesg --clear
+ln -s /dev/dri/card0 /dev/i915
 `
 
 // dutConfig represents information related to the DUT configuration;
@@ -43,16 +45,17 @@ type dutConfig struct {
 }
 
 type syzkallerConfig struct {
-	Name      string    `json:"name"`
-	Target    string    `json:"target"`
-	Reproduce bool      `json:"reproduce"`
-	HTTP      string    `json:"http"`
-	Workdir   string    `json:"workdir"`
-	Syzkaller string    `json:"syzkaller"`
-	Type      string    `json:"type"`
-	SSHKey    string    `json:"sshkey"`
-	Procs     int       `json:"procs"`
-	DUTConfig dutConfig `json:"vm"`
+	Name           string    `json:"name"`
+	Target         string    `json:"target"`
+	Reproduce      bool      `json:"reproduce"`
+	HTTP           string    `json:"http"`
+	Workdir        string    `json:"workdir"`
+	Syzkaller      string    `json:"syzkaller"`
+	Type           string    `json:"type"`
+	SSHKey         string    `json:"sshkey"`
+	Procs          int       `json:"procs"`
+	DUTConfig      dutConfig `json:"vm"`
+	EnableSyscalls []string  `json:"enable_syscalls"`
 }
 
 func init() {
@@ -68,7 +71,7 @@ func init() {
 		// stopping. The overall test duration is 12 minutes.
 		Timeout: syzkallerRunDuration + 2*time.Minute,
 		Attr:    []string{"group:syzkaller"},
-		Data:    []string{"testing_rsa"},
+		Data:    []string{"testing_rsa", "enabled_syscalls.txt"},
 	})
 }
 
@@ -111,6 +114,13 @@ func Wrapper(ctx context.Context, s *testing.State) {
 		s.Fatalf("Unable to chmod sshkey to 0600: %v", err)
 	}
 
+	// Read enabled_syscalls.
+	enabledSyscalls, err := ioutil.ReadFile(s.DataPath("enabled_syscalls.txt"))
+	if err != nil {
+		s.Fatalf("Unable to read enabled syscalls: %v", err)
+	}
+	s.Logf("Enabled syscalls: %v", strings.Split(string(enabledSyscalls), "\n"))
+
 	// Create syzkaller configuration file.
 	// Generating reproducers is unlikely to work as :
 	// [1] Corpus is not shared across two runs of the test.
@@ -132,6 +142,7 @@ func Wrapper(ctx context.Context, s *testing.State) {
 			TargetReboot:  true,
 			StartupScript: startupScript,
 		},
+		EnableSyscalls: strings.FieldsFunc(string(enabledSyscalls), func(c rune) bool { return c == '\n' }),
 	}
 
 	configFile, err := os.Create(filepath.Join(syzkallerTastDir, "config"))
@@ -159,7 +170,7 @@ func Wrapper(ctx context.Context, s *testing.State) {
 
 	s.Logf("Starting syzkaller with logfile at %v", logFile.Name())
 	syzManager := filepath.Join(artifactsDir, "syz-manager")
-	managerCmd := testexec.CommandContext(ctx, syzManager, "-config", configFile.Name(), "-vv", "10", "-debug")
+	managerCmd := testexec.CommandContext(ctx, syzManager, "-config", configFile.Name(), "-vv", "10")
 	managerCmd.Stdout = logFile
 	managerCmd.Stderr = logFile
 
