8eb3fbe7daeaecaa176f7f067b55660c7aad7d37
Miriam Zimmerman <mutexlox@google.com>
2020-07-13 16:26:17 -0700

    Promote crash.Sender{Old,Paused} to critical
    These two tests show all passes on tastboard:
    https://tastboard.corp.google.com/test?testname=crash.Sender*
    
    BUG=None
    TEST=CQ
    
    Change-Id: Ifb0668a06512074cf76822bd090d90f7175f3549

--- src/chromiumos/tast/local/bundles/cros/crash/sender_old.go
+++ src/chromiumos/tast/local/bundles/cros/crash/sender_old.go
@@ -26,7 +26,7 @@ func init() {
 			"cros-telemetry@google.com",
 			"nya@chromium.org", // ported to Tast
 		},
-		Attr: []string{"group:mainline", "informational"},
+		Attr: []string{"group:mainline"},
 	})
 }
 
