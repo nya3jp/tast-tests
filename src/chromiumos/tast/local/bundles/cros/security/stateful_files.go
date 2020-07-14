cf012180cde1a2bb98b7d0a46a23b44f065f65cc
Jorge Lucangeli Obes <jorgelo@chromium.org>
2020-07-14 10:47:30 -0400

    security.StatefulFiles: Mark as critical.
    We've had 12 consecutive build with only two single failures, which is
    fixed by:
    https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2304253
    
    And the big block of failures between 13331 and 13342 is fixed by:
    https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2293248
    
    https://tastboard.corp.google.com/test/security.StatefulFiles
    
    Mark as critical now that we've had a few successful builds. This test
    prevents security issues by ensuring that modifiable, persisted files
    in stateful have the right permissions, but because of that it's also
    easily broken if not run in presubmit.
    
    BUG=chromium:1104857
    TEST=Still passes on betty-pi-arc.
    
    Change-Id: I6f234594723e6f25340206a86428449df85ec38f

--- src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
+++ src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
@@ -26,7 +26,7 @@ func init() {
 			"jorgelo@chromium.org", // Security team
 			"chromeos-security@google.com",
 		},
-		Attr: []string{"group:mainline", "informational"},
+		Attr: []string{"group:mainline"},
 	})
 }
 
