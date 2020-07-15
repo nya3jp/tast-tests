295703ca126a1d3d75bcb203938d2259b33edbee
Judy Hsiao <judyhsiao@google.com>
2020-07-15 11:04:16 +0800

    tast-tests: Build ArcAudioTest from source
    Moved from vendor/google_arc/packages/development/ArcAudioTestApp
    
    BUG=b:120452372
    BUG=b:158623069
    TEST=tast -verbose run ${DUT_IP} arc.AudioAEC
    TEST=tast -verbose run ${DUT_IP} arc.AudioSanity.*
    
    Change-Id: Iee21b50baeb0ea0727f3e29eba837669a269621c

---
+++ android/ArcAudioTest/src/org/chromium/arc/testapp/arcaudiotest/SampleRecorder.java
@@ -0,0 +1,75 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.arc.testapp.arcaudiotest;
+
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.util.Log;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+
+public class SampleRecorder {
+
+    private final int mSampleRate;
+    private final int mEncoding;
+    private final int mChannelConfig;
+    private final int mBufferSize;
+    protected AudioRecord mRecord;
+
+    SampleRecorder(int sampleRate, int encoding, int channelConfig) {
+        mSampleRate = sampleRate;
+        mEncoding = encoding;
+        mChannelConfig = channelConfig;
+        mBufferSize = AudioRecord.getMinBufferSize(mSampleRate, mChannelConfig, mEncoding) * 3;
+        mRecord =
+                new AudioRecord(
+                        MediaRecorder.AudioSource.DEFAULT,
+                        mSampleRate,
+                        mChannelConfig,
+                        mEncoding,
+                        mBufferSize);
+        Log.i(
+                Constant.TAG,
+                String.format(
+                        "recorder created using rate = %d, encoding = 0x%08x",
+                        mSampleRate, mEncoding));
+    }
+
+    public int getAudioSessionId() {
+        return mRecord.getAudioSessionId();
+    }
+
+    public void record(File file) throws Exception {
+        final long TEST_DURATION_MILLIS = 5000;
+        final ByteBuffer buffer = ByteBuffer.allocateDirect(mBufferSize);
+
+        mRecord.startRecording();
+        try (FileOutputStream outStream = new FileOutputStream(file)) {
+            long elapsedMillis = 0;
+            long startTime = System.currentTimeMillis();
+            while (elapsedMillis < TEST_DURATION_MILLIS) {
+                int result = mRecord.read(buffer, mBufferSize);
+                if (result < 0) {
+                    throw new RuntimeException("Reading of audio buffer failed: " + result);
+                }
+                Log.i(Constant.TAG, String.format("recorder read %d bytes", result));
+                outStream.write(buffer.array(), 0, result);
+                elapsedMillis = System.currentTimeMillis() - startTime;
+                buffer.clear();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Writing of recorded audio failed", e);
+        } finally {
+            mRecord.stop();
+            Log.i(Constant.TAG, "mRecord.stop()");
+        }
+    }
+
+    public void release() {
+        mRecord.release();
+        Log.i(Constant.TAG, "mRecord.release()");
+    }
+}
---
+++ android/ArcAudioTest/src/org/chromium/arc/testapp/arcaudiotest/TestInputActivity.java
@@ -0,0 +1,51 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.arc.testapp.arcaudiotest;
+
+import android.media.AudioFormat;
+import android.os.Bundle;
+import android.util.Log;
+import java.io.File;
+
+/** This Chrome OS ARC++ audio sanity autotest. */
+public class TestInputActivity extends MainActivity {
+
+    public void testRecordPCM() {
+        File file = null;
+        SampleRecorder recorder = null;
+        try {
+            recorder =
+                    new SampleRecorder(
+                            44100, AudioFormat.ENCODING_PCM_16BIT, AudioFormat.CHANNEL_OUT_STEREO);
+            file =
+                    File.createTempFile(
+                            "tmp", "recording.pcm", getApplicationContext().getCacheDir());
+            Log.d(Constant.TAG, "start recording :" + file.getAbsolutePath());
+            recorder.record(file);
+            Log.d(Constant.TAG, "finish recording");
+            markAsPassed();
+        } catch (Exception e) {
+            markAsFailed("testRecordPCM failed. " + e.toString());
+        } finally {
+            if (file != null && file.exists()) {
+                file.delete();
+            }
+            recorder.release();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Constant.TAG, "start testRecordPCM");
+        testRecordPCM();
+        Log.i(Constant.TAG, "finish testRecordPCM");
+    }
+}
