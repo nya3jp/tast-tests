be8eb3dfe664f6b26024d40c705aed1109915861
Arowa Suliman <arowa@google.com>
2020-07-16 17:04:39 -0700

    tast-test: Add wpa_supplicant dbus Interface.
    Added wpa_supplicant dbus interface for tests that need to access
    wpa_supplicant such as wifi.RoamDbus.
    
    BUG=b:159673983
    TEST=run tast wifi.RoamDbus
    
    Change-Id: I0dc6b0acc71f31473758b22c272ebc181eeef275

---
+++ src/chromiumos/tast/local/wpasupplicant/bss.go
@@ -0,0 +1,40 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wpasupplicant
+
+import (
+	"context"
+
+	"github.com/godbus/dbus"
+)
+
+const (
+	dbusWpasupplicantObjBSS   = "fi.w1.wpa_supplicant1.BSS"
+	dbusWpasupplicantObjBSSID = "BSSID"
+)
+
+// BSS is the object to interact with wpa_supplicant's
+// fi.w1.wpa_supplicant1.BSS interface.
+type BSS struct {
+	dbus *DBusObject
+}
+
+// NewBSS creates a BSS object.
+func NewBSS(ctx context.Context, path dbus.ObjectPath) (*BSS, error) {
+	d, err := NewDBusObject(ctx, path, dbusWpasupplicantObjBSS)
+	if err != nil {
+		return nil, err
+	}
+	return &BSS{dbus: d}, nil
+}
+
+// BSSID returns the BSSID of this BSS.
+func (b *BSS) BSSID(ctx context.Context) ([]byte, error) {
+	var bssid []byte
+	if err := b.dbus.Get(ctx, dbusWpasupplicantObjBSSID, &bssid); err != nil {
+		return nil, err
+	}
+	return bssid, nil
+}
---
+++ src/chromiumos/tast/local/wpasupplicant/interface.go
@@ -0,0 +1,51 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wpasupplicant
+
+import (
+	"context"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/testing"
+)
+
+const (
+	dbusWpasupplicantObjIface = "fi.w1.wpa_supplicant1.Interface"
+	dbusWpasupplicantObjBSSs  = "BSSs"
+)
+
+// Interface is the object to interact with wpa_supplicant's
+// fi.w1.wpa_supplicant1.Interface interface.
+type Interface struct {
+	dbus *DBusObject
+}
+
+// NewInterface creates an Interface object.
+func NewInterface(ctx context.Context, path dbus.ObjectPath) (*Interface, error) {
+	d, err := NewDBusObject(ctx, path, dbusWpasupplicantObjIface)
+	if err != nil {
+		return nil, err
+	}
+	return &Interface{dbus: d}, nil
+}
+
+// BSSs returns the BSSs property of the interface.
+func (iface *Interface) BSSs(ctx context.Context) ([]*BSS, error) {
+	var bssPaths []dbus.ObjectPath
+	if err := iface.dbus.Get(ctx, dbusWpasupplicantObjBSSs, &bssPaths); err != nil {
+		return nil, err
+	}
+	var ret []*BSS
+	for _, path := range bssPaths {
+		bss, err := NewBSS(ctx, path)
+		if err != nil {
+			testing.ContextLogf(ctx, "Failed to NewBSS(%s): %v", path, err)
+		} else {
+			ret = append(ret, bss)
+		}
+	}
+	return ret, nil
+}
