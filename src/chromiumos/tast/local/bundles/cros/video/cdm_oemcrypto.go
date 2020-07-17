0efc50c6ee6cc6e528c183a8e4b9ec2df28a8279
Jeffrey Kardatzke <jkardatzke@google.com>
2020-07-17 14:37:50 -0700

    tast-tests: Add tests for CdmOemcrypto
    Adds a tast tests for CdmOemcrypto by running two binaries that are
    built by the cdm-oemcrypto-hw-test target which runs the Widevine CE CDM
    unit tests and also the OEMCrypto ref tests.  These then both exercise
    the underlying OEMCrypto HW implementation.
    
    BUG=b:155512423,b:155509785
    TEST=tast run host video.CdmOemcrypto.*
    
    Cq-Depend: chromium:2302956
    Change-Id: Iad2a98282a1e18d5648486f71355dcac626cc081

---
+++ src/chromiumos/tast/local/bundles/cros/video/cdm_oemcrypto.go
@@ -0,0 +1,50 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"path/filepath"
+
+	"chromiumos/tast/local/gtest"
+	"chromiumos/tast/testing"
+)
+
+type execConfig struct {
+	exec      string
+	extraArgs []string
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CDMOEMCrypto,
+		Desc: "Verifies that Widevine CE CDM and OEMCrypto tests run successfully",
+		Contacts: []string{
+			"jkardatzke@google.com",
+			"chromeos-gfx-video@google.com",
+		},
+		Params: []testing.Param{{
+			Name: "ce_cdm",
+			Val:  execConfig{"widevine_ce_cdm_hw_tests", []string{"-n"}},
+		}, {
+			Name: "oemcrypto",
+			Val:  execConfig{"oemcrypto_hw_ref_tests", []string{}},
+		}},
+		// TODO(jkardatzke): Add SoftwareDeps for cdm_factory_daemon USE flag and
+		// condition the run on that.
+	})
+}
+
+func CDMOEMCrypto(ctx context.Context, s *testing.State) {
+	testOpt := s.Param().(execConfig)
+	logdir := filepath.Join(s.OutDir(), "gtest")
+	s.Log("Running ", testOpt.exec)
+	if _, err := gtest.New(testOpt.exec,
+		gtest.Logfile(filepath.Join(logdir, testOpt.exec+".log")),
+		gtest.ExtraArgs(testOpt.extraArgs...),
+	).Run(ctx); err != nil {
+		s.Errorf("%s failed: %v", testOpt.exec, err)
+	}
+}
