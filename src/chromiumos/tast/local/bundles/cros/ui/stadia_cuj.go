8c10ebf0f06229f0ff5929915764b9dcb5abdc59
Yichen Zhou <yichenz@chromium.org>
2020-07-17 08:18:21 +0000

    Extract the logic of starting the target game on Stadia
    StadiaCUJ ran the Worm Game which is usually featured on Stadia
    main page. However after claiming more games for testing, the featured
    game might change and it's better to enter the Worm Game explicitly
    from the all-games list.
    
    StartGameFromGameListsView will also be used for testing other Stadia
    games.
    
    BUG=chromium:1091976
    TEST=tast run IP ui.StadiaCUJ
    Change-Id: I111bf2554f7c594b64481cc4bace532ee77c1d5a

--- src/chromiumos/tast/local/bundles/cros/ui/stadia_cuj.go
+++ src/chromiumos/tast/local/bundles/cros/ui/stadia_cuj.go
@@ -11,9 +11,11 @@ import (
 	"chromiumos/tast/common/perf"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/ui/cuj"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/webutil"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
@@ -37,7 +39,10 @@ func init() {
 // StadiaCUJ test starts the default game 'Worm Game' and runs it for 5 minutes. It stays on the
 // main game menu instead of playing or interacting with the game.
 func StadiaCUJ(ctx context.Context, s *testing.State) {
-	const timeout = 10 * time.Second
+	const (
+		timeout  = 10 * time.Second
+		gameName = "Worm Game Edition"
+	)
 
 	cr := s.PreValue().(cuj.PreData).Chrome
 
@@ -51,18 +56,12 @@ func StadiaCUJ(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to create TabCrashChecker: ", err)
 	}
 
-	conn, err := cr.NewConn(ctx, "https://ggp-staging.sandbox.google.com")
+	conn, err := cr.NewConn(ctx, "https://ggp-staging.sandbox.google.com/store/list")
 	if err != nil {
 		s.Fatal("Failed to open the stadia staging instance: ", err)
 	}
 	defer conn.Close()
 	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
-	ws, err := ash.GetAllWindows(ctx, tconn)
-	if err != nil {
-		s.Fatal("Failed to obtain the window list: ", err)
-	}
-	id0 := ws[0].ID
 
 	webview, err := ui.FindWithTimeout(ctx, tconn, ui.FindParams{Role: ui.RoleTypeWebView, ClassName: "WebView"}, timeout)
 	if err != nil {
@@ -70,31 +69,11 @@ func StadiaCUJ(ctx context.Context, s *testing.State) {
 	}
 	defer webview.Release(ctx)
 
-	// Start the worm game for testing.
-	wormGame, err := webview.DescendantWithTimeout(ctx, ui.FindParams{Name: "Worm Game Edition Play game", Role: ui.RoleTypeButton}, timeout)
-	if err != nil {
-		s.Fatal("Failed to find the worm game: ", err)
-	}
-	defer wormGame.Release(ctx)
-	wormGame.FocusAndWait(ctx, timeout)
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		if err := wormGame.LeftClick(ctx); err != nil {
-			return errors.Wrap(err, "failed to click the worm game start button")
-		}
-		w0, err := ash.GetWindow(ctx, tconn, id0)
-		if err != nil {
-			s.Fatal("Failed to get the window: ", err)
-		}
-		// The window should turn into fullscreen mode when game starts.
-		if w0.State != ash.WindowStateFullscreen {
-			return errors.New("hasn't entered the game yet")
-		}
-		return nil
-	}, &testing.PollOptions{Timeout: timeout, Interval: time.Second}); err != nil {
-		s.Fatal("Failed to start the worm game: ", err)
+	if err := startGameFromGameListsView(ctx, tconn, conn, webview, gameName, timeout); err != nil {
+		s.Fatalf("Failed to start the game %s: %s", gameName, err)
 	}
 
-	// Wait for the game screen to show up.
+	// Wait for the game to be completely loaded.
 	// TODO(crbug.com/1091976): use signal from Stadia games instead.
 	if err := testing.Sleep(ctx, 30*time.Second); err != nil {
 		s.Fatal("Failed to sleep: ", err)
@@ -139,4 +118,77 @@ func StadiaCUJ(ctx context.Context, s *testing.State) {
 	if err := pv.Save(s.OutDir()); err != nil {
 		s.Fatal("Failed to save the perf data: ", err)
 	}
+}
+
+// startGameFromGameListsView locates the game by its name from the game list page
+// and starts the game.
+func startGameFromGameListsView(ctx context.Context, tconn *chrome.TestConn, conn *chrome.Conn, n *ui.Node, name string, timeout time.Duration) error {
+	gameView := "View " + name + "."
+	gamePlay := "Play"
+	gameStart := name + " Play game"
+
+	// Find the game view from the game list.
+	gameViewButton, err := n.DescendantWithTimeout(ctx, ui.FindParams{Name: gameView, Role: ui.RoleTypeButton}, timeout)
+	if err != nil {
+		return errors.Wrapf(err, "failed to find the game view button (%s)", gameView)
+	}
+	defer gameViewButton.Release(ctx)
+	if err := gameViewButton.FocusAndWait(ctx, timeout); err != nil {
+		return errors.Wrapf(err, "failed to focus on the game view button (%s)", gameView)
+	}
+	if err := gameViewButton.LeftClick(ctx); err != nil {
+		return errors.Wrapf(err, "failed to click the game view button (%s)", gameView)
+	}
+	if err := webutil.WaitForQuiescence(ctx, conn, timeout); err != nil {
+		return errors.Wrap(err, "failed to wait for game page to finish loading")
+	}
+
+	// Play the game.
+	gamePlayButton, err := n.DescendantWithTimeout(ctx, ui.FindParams{Name: gamePlay, Role: ui.RoleTypeButton}, timeout)
+	if err != nil {
+		return errors.Wrapf(err, "failed to find the game play button (%s)", gamePlay)
+	}
+	defer gamePlayButton.Release(ctx)
+	if err := gamePlayButton.FocusAndWait(ctx, timeout); err != nil {
+		return errors.Wrapf(err, "failed to focus on the game play button (%s)", gamePlay)
+	}
+	if err := gamePlayButton.LeftClick(ctx); err != nil {
+		return errors.Wrapf(err, "failed to click the game play button (%s)", gamePlay)
+	}
+	if err := webutil.WaitForQuiescence(ctx, conn, timeout); err != nil {
+		return errors.Wrap(err, "failed to wait for game page to finish loading")
+	}
+
+	// Start(enter) the game.
+	gameStartButton, err := n.DescendantWithTimeout(ctx, ui.FindParams{Name: gameStart, Role: ui.RoleTypeButton}, timeout)
+	if err != nil {
+		return errors.Wrapf(err, "failed to find the game start button (%s)", gameStart)
+	}
+	defer gameStartButton.Release(ctx)
+	if err := gameStartButton.FocusAndWait(ctx, timeout); err != nil {
+		return errors.Wrapf(err, "failed to focus on the game start button (%s)", gameStart)
+	}
+	// Make sure the game is fully launched.
+	ws, err := ash.GetAllWindows(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to obtain the window list")
+	}
+	id0 := ws[0].ID
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := gameStartButton.LeftClick(ctx); err != nil {
+			return errors.Wrapf(err, "failed to click the game start button (%s)", gameStart)
+		}
+		w0, err := ash.GetWindow(ctx, tconn, id0)
+		if err != nil {
+			return errors.Wrapf(err, "failed to get the window")
+		}
+		// The window should turn into fullscreen mode when game starts.
+		if w0.State != ash.WindowStateFullscreen {
+			return errors.New("hasn't entered the game yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: timeout, Interval: time.Second}); err != nil {
+		return errors.Wrapf(err, "failed to start the game %s", name)
+	}
+	return nil
 }
