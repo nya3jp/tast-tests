7836ebb74129041641e827f9b38a60d020816cce
Arowa Suliman <arowa@google.com>
2020-07-16 17:04:39 -0700

    tast-test: Add wpa_supplicant dbus Interface.
    Added wpa_supplicant dbus interface for tests that need to access
    wpa_supplicant such as wifi.RoamDbus.
    
    BUG=b:159673983
    TEST=run tast wifi.RoamDbus
    
    Change-Id: I0dc6b0acc71f31473758b22c272ebc181eeef275

---
+++ src/chromiumos/tast/local/wpasupplicant/bss.go
@@ -0,0 +1,40 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wpasupplicant
+
+import (
+	"context"
+
+	"github.com/godbus/dbus"
+)
+
+const (
+	dbusInterfaceBSS          = "fi.w1.wpa_supplicant1.BSS"
+	dbusInterfaceBSSPropBSSID = "BSSID"
+)
+
+// BSS is the object to interact with wpa_supplicant's
+// fi.w1.wpa_supplicant1.BSS interface.
+type BSS struct {
+	dbus *DBusObject
+}
+
+// NewBSS creates a BSS object.
+func NewBSS(ctx context.Context, path dbus.ObjectPath) (*BSS, error) {
+	d, err := NewDBusObject(ctx, path, dbusInterfaceBSS)
+	if err != nil {
+		return nil, err
+	}
+	return &BSS{dbus: d}, nil
+}
+
+// BSSID returns the BSSID of this BSS.
+func (b *BSS) BSSID(ctx context.Context) ([]byte, error) {
+	var bssid []byte
+	if err := b.dbus.Get(ctx, dbusInterfaceBSSPropBSSID, &bssid); err != nil {
+		return nil, err
+	}
+	return bssid, nil
+}
