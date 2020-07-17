ecda706968256db0101bca20d9a4c20e74cafa40
Greg Edelston <gredelston@google.com>
2020-07-17 17:01:20 -0600

    Write and test fw_tries setters, checkers
    fw_tries functionality is, at a low level, just calls to `crossystem`.
    But the behavior varies based on whether the DUT uses vboot1 or vboot2.
    This CL implements setters/checkers for fw_tries, and tests them.
    
    The test code is analogous to Autotest's firmware_FWTries test.
    The checking code, Autotest's faft_checkers.fw_tries_checker().
    
    BUG=b:161354762
    TEST=firmware.FWTries on a vboot1 DUT (samus)
    TEST=firmware.FWTries on a vboot2 DUT (coral/nasher)
    
    Change-Id: Ibfdd07573fc7cbad9b4ed2f5c27003fd358ab898

--- src/chromiumos/tast/remote/firmware/fw_tries.go
+++ src/chromiumos/tast/remote/firmware/fw_tries.go
@@ -8,6 +8,8 @@ package firmware
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 
 	"chromiumos/tast/dut"
 	"chromiumos/tast/errors"
@@ -25,4 +27,74 @@ func Vboot2(ctx context.Context, dut *dut.DUT) (bool, error) {
 		return false, nil
 	}
 	return false, errors.Errorf("unexpected fw_vboot2 value: %q", output)
+}
+
+// SetFWTries sets which firmware (A or B) the DUT should boot to next, and how many times it should try booting ino that firmware.
+// If count is 0, then fw_try_count will not be modified, only fw_try_next.
+func SetFWTries(ctx context.Context, dut *dut.DUT, next string, count int) error {
+	// Behavior varies based on whether DUT is running Vboot1 or Vboot2
+	if vboot2, err := Vboot2(ctx, dut); err != nil {
+		return err
+	} else if vboot2 {
+		// Validate params for vboot2
+		if next != "A" && next != "B" {
+			return errors.Errorf("unexpected param next: got %q; want A or B", next)
+		}
+		if count < 0 {
+			return errors.Errorf("unexpected param count: got %d; want >=0", count)
+		}
+		// Set fw_try_next
+		const fwTryNext = "fw_try_next"
+		if err := dut.Command("crossystem", fmt.Sprintf("%s=%s", fwTryNext, next)).Run(ctx); err != nil {
+			return errors.Wrapf(err, "setting crossystem %q to %q", fwTryNext, next)
+		}
+		// Set fw_try_count
+		if count > 0 {
+			const fwTryCount = "fw_try_count"
+			if err := dut.Command("crossystem", fmt.Sprintf("%s=%d", fwTryCount, count)).Run(ctx); err != nil {
+				return errors.Wrapf(err, "setting crossystem %q to %d", fwTryCount, count)
+			}
+		}
+		return nil
+	}
+	// Validate params for vboot1
+	if next != "B" {
+		return errors.New("vboot1 dut can only set fw tries to B")
+	}
+	if count <= 0 {
+		return errors.Errorf("unexpected param count: got %d; want >0", count)
+	}
+	// Set fwb_tries
+	const FWBTries = "fwb_tries"
+	if err := dut.Command("crossystem", fmt.Sprintf("%s=%d", FWBTries, count)).Run(ctx); err != nil {
+		return errors.Wrapf(err, "setting crossystem %q to %d", FWBTries, count)
+	}
+	return nil
+}
+
+// CheckFWTries verifies the DUT's currently booted firmware and try_count, and returns an error if the expected values are not found.
+// The actual behavior varies based on whether the DUT is running Vboot1 or Vboot2.
+func CheckFWTries(ctx context.Context, dut *dut.DUT, mainfwAct string, count int) error {
+	if mainfwAct != "A" && mainfwAct != "B" {
+		return errors.Errorf("unexpected param mainfwAct: got %q; want A or B", mainfwAct)
+	}
+	// Setup expected crossystem values
+	crossystemChecks := map[string]string{"mainfw_act": mainfwAct}
+	if vboot2, err := Vboot2(ctx, dut); err != nil {
+		return err
+	} else if vboot2 {
+		crossystemChecks["fw_try_count"] = strconv.Itoa(count)
+	} else {
+		crossystemChecks["fwb_tries"] = strconv.Itoa(count)
+		crossystemChecks["tried_fwb"] = map[string]string{"A": "0", "B": "1"}[mainfwAct]
+	}
+	// Check crossystem values
+	for k, v := range crossystemChecks {
+		if output, err := dut.Command("crossystem", k).Output(ctx); err != nil {
+			return errors.Wrapf(err, "checking crossystem value for key %s", k)
+		} else if string(output) != v {
+			return errors.Errorf("unexpected crossystem value for key %s: got %s; want %s", k, output, v)
+		}
+	}
+	return nil
 }
