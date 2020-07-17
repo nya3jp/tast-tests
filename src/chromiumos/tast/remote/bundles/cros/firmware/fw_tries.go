d3b07085b0ff5c1ec8e26e1df2e53f7045543667
Greg Edelston <gredelston@google.com>
2020-07-17 17:01:20 -0600

    Write and test fw_tries setters, checkers
    fw_tries functionality is, at a low level, just calls to `crossystem`.
    But the behavior varies based on whether the DUT uses vboot1 or vboot2.
    This CL implements setters/checkers for fw_tries, and tests them.
    
    The test code is analogous to Autotest's firmware_FWTries test.
    The checking code, Autotest's faft_checkers.fw_tries_checker().
    
    BUG=b:161354762
    TEST=firmware.FWTries on a vboot1 DUT (samus)
    TEST=firmware.FWTries on a vboot2 DUT (coral/nasher)
    
    Change-Id: Ibfdd07573fc7cbad9b4ed2f5c27003fd358ab898

--- src/chromiumos/tast/remote/bundles/cros/firmware/fw_tries.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/fw_tries.go
@@ -14,7 +14,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         FWTries,
-		Desc:         "Verify that the DUT can be specified to boot from A or B. Note: This test is still under active development",
+		Desc:         "Verify that the DUT can be specified to boot from A or B",
 		Contacts:     []string{"cros-fw-engprod@google.com"},
 		SoftwareDeps: []string{"crossystem"},
 		Attr:         []string{"group:mainline", "informational"},
@@ -22,7 +22,8 @@ func init() {
 }
 
 func FWTries(ctx context.Context, s *testing.State) {
-	vboot2, err := firmware.Vboot2(ctx, s.DUT())
+	d := s.DUT()
+	vboot2, err := firmware.Vboot2(ctx, d)
 	if err != nil {
 		s.Fatal("Failed to determine fw_vboot2: ", err)
 	}
@@ -30,5 +31,36 @@ func FWTries(ctx context.Context, s *testing.State) {
 		s.Log("DUT uses vboot2")
 	} else {
 		s.Log("DUT does not use vboot2")
+	}
+
+	if err := firmware.CheckFWTries(ctx, d, firmware.A, firmware.UnspecifiedRWSection, 0); err != nil {
+		s.Error("Error with DUT state at start of test: ", err)
+	}
+	if err := firmware.SetFWTries(ctx, d, firmware.B, 2); err != nil {
+		s.Fatal("Setting FWTries to B/2: ", err)
+	}
+	if err := firmware.CheckFWTries(ctx, d, firmware.A, firmware.B, 2); err != nil {
+		s.Error("After setting FWTries to B/2, before rebooting: ", err)
+	}
+
+	if err := d.Reboot(ctx); err != nil {
+		s.Fatal("Rebooting: ", err)
+	}
+	if err := firmware.CheckFWTries(ctx, d, firmware.B, firmware.B, 1); err != nil {
+		s.Error("After setting FWTries to B/2, then rebooting once: ", err)
+	}
+
+	if err := d.Reboot(ctx); err != nil {
+		s.Fatal("Rebooting: ", err)
+	}
+	if err := firmware.CheckFWTries(ctx, d, firmware.B, firmware.UnspecifiedRWSection, 0); err != nil {
+		s.Error("After setting FWTries to B/2, then rebooting twice: ", err)
+	}
+
+	if err := d.Reboot(ctx); err != nil {
+		s.Fatal("Rebooting: ", err)
+	}
+	if err := firmware.CheckFWTries(ctx, d, firmware.A, firmware.A, 0); err != nil {
+		s.Error("After setting FWTries to B/2, then rebooting three times: ", err)
 	}
 }
