2510d7c4cd22c9fffc238dcff150a3eef624be1e
Yong Hong <yhong@google.com>
2020-07-20 23:17:13 +0800

    tast-tests: add edid probing tests for runtime probe
    This CL tests if the probed results from runtime probe include the same
    edid component recorded in cros-labels, which is decoded from DUT's
    HWID.
    
    BUG=b:159197877
    TEST=tast -verbose run '-var=autotest_host_info_labels=['\
         '"hwid_component:display_panel/<DISPLAY_PANEL_COMPONENT_NAME>",'\
         '"model:<MODEL_NAME>"]' <DUT_IP> platform.CrosRuntimeProbeEdid
    
    Change-Id: I186f9c1c3479edf44de3e953121d6391f3e005ad

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_runtime_probe_edid.go
@@ -0,0 +1,78 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"sort"
+
+	rppb "chromiumos/system_api/runtime_probe_proto"
+	"chromiumos/tast/local/bundles/cros/platform/runtimeprobe"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosRuntimeProbeEdid,
+		Desc: "Checks that edid probe results are expected",
+		Contacts: []string{
+			"ckclark@chromium.org",
+			"chromeos-runtime-probe@google.com",
+		},
+		Attr:         []string{"group:runtime_probe"},
+		SoftwareDeps: []string{"wilco"},
+		Vars:         []string{"autotest_host_info_labels"},
+	})
+}
+
+// CrosRuntimeProbeEdid checks if the edid names in cros-label are
+// consistent with probed names from runtime_probe.
+func CrosRuntimeProbeEdid(ctx context.Context, s *testing.State) {
+	const category = "display_panel"
+	labelsStr, ok := s.Var("autotest_host_info_labels")
+	if !ok {
+		s.Fatal("No edid labels")
+	}
+
+	mapping, model, err := runtimeprobe.GetComponentCount(labelsStr, []string{category})
+	labels := mapping[category]
+	if err != nil {
+		s.Fatal("Unable to decode autotest_host_info_labels: ", err)
+	} else if len(labels) == 0 {
+		s.Fatal("No edid labels")
+	}
+
+	request := &rppb.ProbeRequest{
+		Categories: []rppb.ProbeRequest_SupportCategory{
+			rppb.ProbeRequest_display_panel,
+		},
+	}
+	result, err := runtimeprobe.Probe(ctx, request)
+	if err != nil {
+		s.Fatal("Cannot get edid components: ", err)
+	}
+	probedEdidComponents := result.GetDisplayPanel()
+
+	for _, component := range probedEdidComponents {
+		result, name := runtimeprobe.DecreaseComponentCount(labels, model, component)
+		s.Logf("Probed %s: %s", category, name)
+		if !result {
+			if name == "generic" {
+				s.Logf("Skip known generic %s probe result", category)
+			} else {
+				s.Fatalf("Unexpected %s %s is probed", category, name)
+			}
+		}
+	}
+
+	if len(labels) > 0 {
+		unprobedEdid := make([]string, 0, len(labels))
+		for k := range labels {
+			unprobedEdid = append(unprobedEdid, k)
+		}
+		sort.Strings(unprobedEdid)
+		s.Fatal("Some edid(s) are not probed: ", unprobedEdid)
+	}
+}
