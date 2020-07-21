da6ef0345e130cfa269c5ed017f408868fddfc8f
Kyle Shimabukuro <kyleshima@chromium.org>
2020-07-21 16:50:12 -0700

    Add Ubertray module
    We have a few tests that interact with the Ubertray now, so it makes
    sense to have an Ubertray module to help with maintenance and
    readability. Existing tests have been refactored to use the new module.
    
    BUG=chromium:1080414
    TEST=tast run <ip> assistant.BluetoothQueries ui.UbertrayOpenSettings
    lacros.GpuCUJ.maximized arc.PIP
    
    Change-Id: I4031b6aa643e2869a5ec527585fc0957aa862ee2

--- src/chromiumos/tast/local/bundles/cros/assistant/bluetooth_queries.go
+++ src/chromiumos/tast/local/bundles/cros/assistant/bluetooth_queries.go
@@ -15,6 +15,7 @@ import (
 	"chromiumos/tast/local/assistant"
 	"chromiumos/tast/local/bluetooth"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/testing"
 )
@@ -105,22 +106,10 @@ func BluetoothQueries(ctx context.Context, s *testing.State) {
 
 		// Check Bluetooth ubertray button as well.
 		s.Log("Checking bluetooth ubertray pod button status")
-		params := ui.FindParams{
-			ClassName: "ash/StatusAreaWidgetDelegate",
-		}
-		statusArea, err := ui.FindWithTimeout(ctx, tconn, params, 10*time.Second)
-		if err != nil {
-			s.Fatal("Failed to open ubertray: ", err)
-		}
-		defer statusArea.Release(ctx)
-
-		if err := statusArea.LeftClick(ctx); err != nil {
-			s.Fatal("Failed to open ubertray: ", err)
-		}
-
-		btnName := fmt.Sprintf("Toggle Bluetooth. Bluetooth is %v", onOff)
-		if err := ui.WaitUntilExists(ctx, tconn, ui.FindParams{Name: btnName}, 10*time.Second); err != nil {
-			s.Fatal("Bluetooth button (ubertray) was not toggled by the Assistant: ", err)
+		if btPodStatus, err := ash.QuickSettingEnabled(ctx, tconn, ash.Bluetooth); err != nil {
+			s.Fatal("Failed to get Bluetooth quick setting status: ", err)
+		} else if btPodStatus != status {
+			s.Fatal("Bluetooth button (ubertray) was not toggled by the Assistant")
 		}
 	}
 }
