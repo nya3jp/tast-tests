8397f858df47a6c2ba85d9b7a48361e7754d67dc
Steven Bennetts <stevenjb@chromium.org>
2020-07-21 16:19:47 -0700

    Use chrome.feedbackPrivate in systemlogs
    This modifies systemlogs to use chrome.feedbackPrivate instead of
    a not-yet-landed custom autotestPrivate call.
    
    This simplifes both the systemlogs code and the smoke test.
    
    BUG=chromium:550539
    TEST=tast run {target} logs.Smoke
    
    Change-Id: Ic22aa6846dc729366abc0da3be42e68de0722ef6

--- src/chromiumos/tast/local/chrome/systemlogs/systemlogs.go
+++ src/chromiumos/tast/local/chrome/systemlogs/systemlogs.go
@@ -7,43 +7,30 @@ package systemlogs
 
 import (
 	"context"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/testexec"
 )
+
+// systemInformation corresponds to feedbackPrivate.SystemInformation entries.
+type systemInformation struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
 
 // GetSystemLogs returns a string containing the complete contents of the
 // system logs file exported by chrome.autotestPrivate.writeSystemLogs.
 // The logs are written to a file in the /tmp directory which is removed
 // after this returns.
-func GetSystemLogs(ctx context.Context, tconn *chrome.TestConn) (string, error) {
-	// Use /tmp instead of ioutil.TempDir since chrome does not have permission
-	// to write to the tast temp dir.
-	const destPath = "/tmp"
-	var zipFilepath string
-	if err := tconn.Call(ctx, &zipFilepath, `tast.promisify(chrome.autotestPrivate.writeSystemLogs)`, destPath); err != nil {
-		return "", errors.Wrap(err, "Failed to write system logs to: "+destPath)
-	}
-	defer os.Remove(zipFilepath)
-
-	const zipExt = ".zip"
-	if filepath.Ext(zipFilepath) != zipExt {
-		return "", errors.New("system_logs file is not zipped")
-	}
-	if err := testexec.CommandContext(ctx, "unzip", "-u", zipFilepath, "-d", destPath).Run(testexec.DumpLogOnError); err != nil {
-		return "", errors.Wrap(err, "failed to unzip")
-	}
-
-	txtFilepath := zipFilepath[0 : len(zipFilepath)-len(zipExt)]
-	defer os.Remove(txtFilepath)
-
-	systemLogs, err := ioutil.ReadFile(txtFilepath)
-	if err != nil {
+func GetSystemLogs(ctx context.Context, tconn *chrome.TestConn, key string) (string, error) {
+	var systemInfo []*systemInformation
+	if err := tconn.Call(ctx, &systemInfo, `tast.promisify(chrome.feedbackPrivate.getSystemInformation)`); err != nil {
 		return "", err
 	}
-	return string(systemLogs), nil
+	for _, info := range systemInfo {
+		if info.Key == key {
+			return info.Value, nil
+		}
+	}
+	return "", errors.Errorf("Key not found: %s", key)
 }
