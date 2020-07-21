36c59ce86110ec3e2f4dd16ee0579205cfdb4376
Judy Wu <jinrongwu@google.com>
2020-07-21 12:35:51 +1000

    Add tast test for crostini icon and username
    Automate crostini test case: 1.11.1 - test crostini icon on shelf and username in Terminal window
    
    BUG=chromium:1101583
    TEST=Ran on hatch
    
    Change-Id: I3e77ec64c2ec79c0ce2d47ba17bca5d83972b28b

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/icon_and_username.go
@@ -0,0 +1,68 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome/ui/terminalapp"
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     IconAndUsername,
+		Desc:     "Test Terminal icon on shelf and username in Terminal window",
+		Contacts: []string{"jinrongwu@google.com", "cros-containers-dev@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Name:              "artifact",
+			Pre:               crostini.StartedByArtifact(),
+			ExtraData:         []string{crostini.ImageArtifact},
+			Timeout:           7 * time.Minute,
+			ExtraHardwareDeps: crostini.CrostiniStable,
+		}, {
+			Name:              "artifact_unstable",
+			Pre:               crostini.StartedByArtifact(),
+			ExtraData:         []string{crostini.ImageArtifact},
+			Timeout:           7 * time.Minute,
+			ExtraHardwareDeps: crostini.CrostiniUnstable,
+		}, {
+			Name:    "download_stretch",
+			Pre:     crostini.StartedByDownloadStretch(),
+			Timeout: 10 * time.Minute,
+		}, {
+			Name:    "download_buster",
+			Pre:     crostini.StartedByDownloadBuster(),
+			Timeout: 10 * time.Minute,
+		}},
+		SoftwareDeps: []string{"chrome", "vm_host"},
+	})
+}
+
+func IconAndUsername(ctx context.Context, s *testing.State) {
+	tconn := s.PreValue().(crostini.PreData).TestAPIConn
+	cr := s.PreValue().(crostini.PreData).Chrome
+	keyboard := s.PreValue().(crostini.PreData).Keyboard
+
+	// Use a shortened context for test operations to reserve time for cleanup.
+	shortCtx, shortCancel := ctxutil.Shorten(ctx, 15*time.Second)
+	defer shortCancel()
+
+	// Open Terminal app.
+	terminalApp, err := terminalapp.Launch(shortCtx, tconn, strings.Split(cr.User(), "@")[0])
+	if err != nil {
+		s.Fatal("Failed to open Terminal app: ", err)
+	}
+
+	// Exiting Terminal app.
+	if err = terminalApp.Close(shortCtx, keyboard); err != nil {
+		s.Fatal("Failed to exit Terminal window: ", err)
+	}
+}
