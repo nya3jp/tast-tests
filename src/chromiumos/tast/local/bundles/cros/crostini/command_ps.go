2f8c7728e9ad278d519eaa62f4658d4318c34194
Judy Wu <jinrongwu@google.com>
2020-07-21 21:10:23 +1000

    Add tast test to run command ps in container
    Automate crostini test case: 1.15.2 - run command ps and check result in container
    
    BUG=chromium:1101583
    TEST=Ran on hatch
    
    Change-Id: I17df4b39643ccef8acad39ae80425579498cefea

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/command_ps.go
@@ -0,0 +1,59 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     CommandPs,
+		Desc:     "Run command ps in container",
+		Contacts: []string{"jinrongwu@google.com", "cros-containers-dev@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Name:              "artifact",
+			Pre:               crostini.StartedByArtifact(),
+			ExtraData:         []string{crostini.ImageArtifact},
+			Timeout:           7 * time.Minute,
+			ExtraHardwareDeps: crostini.CrostiniStable,
+		}, {
+			Name:              "artifact_unstable",
+			Pre:               crostini.StartedByArtifact(),
+			ExtraData:         []string{crostini.ImageArtifact},
+			Timeout:           7 * time.Minute,
+			ExtraHardwareDeps: crostini.CrostiniUnstable,
+		}, {
+			Name:    "download_stretch",
+			Pre:     crostini.StartedByDownloadStretch(),
+			Timeout: 10 * time.Minute,
+		}, {
+			Name:    "download_buster",
+			Pre:     crostini.StartedByDownloadBuster(),
+			Timeout: 10 * time.Minute,
+		}},
+		SoftwareDeps: []string{"chrome", "vm_host"},
+	})
+}
+
+func CommandPs(ctx context.Context, s *testing.State) {
+	cont := s.PreValue().(crostini.PreData).Container
+
+	cmd := cont.Command(ctx, "ps")
+	result, err := cmd.Output()
+	if err != nil {
+		s.Fatal("Failed to run command ps in container: ", err)
+	}
+	s.Log(string(result))
+	if !strings.Contains(string(result), "ps") {
+		s.Fatal("Failed to run command ps in contaimer")
+	}
+}
