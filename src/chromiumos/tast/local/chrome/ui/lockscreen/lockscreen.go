2be15322152e5d9320b9e473d185a8bdf3a1d6d4
Gabor Zsolt Magda <gabormagda@google.com>
2020-07-22 16:39:47 +0200

    tast-tests: Move lockscreen waitStatus to a common package
    It had two copies in different tests, and it is also needed
    in policy testing.
    
    BUG=None
    TEST=tast run <ip> ui.ScreenLock
         tast run <ip> ui.QuickCheckCUJ
    
    Change-Id: I1305bff6ffbc093da1790a764a75e35f8d54a36f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2313196
    Tested-by: Gabor Zsolt Magda <gabormagda@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Commit-Queue: Gabor Zsolt Magda <gabormagda@google.com>

---
+++ src/chromiumos/tast/local/chrome/ui/lockscreen/lockscreen.go
@@ -0,0 +1,36 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package lockscreen is used to get information about the lock screen directly through the UI.
+package lockscreen
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// State contains a subset of the state returned by chrome.autotestPrivate.loginStatus.
+type State struct {
+	Locked bool `json:"isScreenLocked"`
+	Ready  bool `json:"isReadyForPassword"`
+}
+
+// WaitState repeatedly calls chrome.autotestPrivate.loginStatus and passes the returned
+// state to check until it returns true or timeout is reached. The last-seen state is returned.
+func WaitState(ctx context.Context, tconn *chrome.TestConn, check func(st State) bool, timeout time.Duration) (State, error) {
+	var st State
+	err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := tconn.EvalPromise(ctx, `tast.promisify(chrome.autotestPrivate.loginStatus)()`, &st); err != nil {
+			return err
+		} else if !check(st) {
+			return errors.New("wrong status")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: timeout})
+	return st, err
+}
