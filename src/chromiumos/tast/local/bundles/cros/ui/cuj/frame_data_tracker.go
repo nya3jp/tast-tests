3ba2f906fcec1128bca68058a49874e8e977774f
Xiyuan Xia <xiyuan@google.com>
2020-07-22 14:46:01 -0700

    tast-tests: Add FrameDataTracker
    Add a FrameDataTracker that collects animation frame data. Update
    cuj.Recorder to use it to store the data in perf values for TPS
    frame rate and dropped frames sub score calculation.
    
    BUG=chromium:1098555
    TEST=tast run IP ui.*CUJ
    
    Change-Id: I2e09d75b86ccb02e1d54b3945732701e13fd494f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2314859
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Tested-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/frame_data_tracker.go
@@ -0,0 +1,75 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cuj
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+)
+
+// AnimationFrameData stores frame data for an animation.
+type AnimationFrameData struct {
+	FramesExpected int `json:"framesExpected"`
+	FramesProduced int `json:"framesProduced"`
+}
+
+// FrameDataTracker is helper to get animation frame data from Chrome.
+type FrameDataTracker struct {
+	animationData []AnimationFrameData
+}
+
+// Start starts the animation data collection.
+func (t *FrameDataTracker) Start(ctx context.Context, tconn *chrome.TestConn) error {
+	if err := tconn.EvalPromise(ctx,
+		`tast.promisify(chrome.autotestPrivate.startThroughputTrackerDataCollection)()`,
+		nil); err != nil {
+		return errors.Wrap(err, "failed to start data collection")
+	}
+
+	return nil
+}
+
+// Stop stops the animation data collection and stores the collected data.
+func (t *FrameDataTracker) Stop(ctx context.Context, tconn *chrome.TestConn) error {
+	var data []AnimationFrameData
+	if err := tconn.EvalPromise(ctx,
+		`tast.promisify(chrome.autotestPrivate.stopThroughputTrackerDataCollection)()`,
+		&data); err != nil {
+		return errors.Wrap(err, "failed to stop data collection")
+	}
+
+	t.animationData = append(t.animationData, data...)
+	return nil
+}
+
+// Record stores the collected data into pv for further processing.
+func (t *FrameDataTracker) Record(pv *perf.Values) {
+	if len(t.animationData) == 0 {
+		return
+	}
+
+	for i, data := range t.animationData {
+		prefix := fmt.Sprintf("TPS.Animation.%d", i)
+		pv.Set(perf.Metric{
+			Name:      prefix + ".FramesExpected",
+			Unit:      "count",
+			Direction: perf.BiggerIsBetter,
+		}, float64(data.FramesExpected))
+		pv.Set(perf.Metric{
+			Name:      prefix + ".FramesProduced",
+			Unit:      "count",
+			Direction: perf.BiggerIsBetter,
+		}, float64(data.FramesProduced))
+	}
+}
+
+// NewFrameDataTracker creates a new instance for FrameDataTracker.
+func NewFrameDataTracker() (*FrameDataTracker, error) {
+	return &FrameDataTracker{}, nil
+}
