b4a88dbc0d805c595b9465f38922afd171ea9062
Jun Sheng <shengjun@google.com>
2020-07-22 16:01:37 +1000

    tast-tests: add test to help app on managed device
    1. Abstract helpapp for shared functions.
    2. Add test on managed device with enterprise login.
    3. Add perks check on existing consumer test as well.
    
    BUG=b:161658110
    TEST=Ran on VM and device.
    
    Change-Id: Ia7d2da0b03544311dcf19d96616edefe5b054d37

---
+++ src/chromiumos/tast/local/bundles/cros/apps/helpapp/helpapp.go
@@ -0,0 +1,79 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package helpapp contains common functions used in help app.
+package helpapp
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/testing"
+)
+
+var helpRootNodeParams = ui.FindParams{
+	Name: apps.Help.Name,
+	Role: ui.RoleTypeRootWebArea,
+}
+
+// WaitForApp waits for app shown and rendered.
+func WaitForApp(ctx context.Context, tconn *chrome.TestConn) error {
+	helpRootNode, err := HelpRootNode(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to find help app")
+	}
+	// Find Overview tab to verify app rendering.
+	params := ui.FindParams{
+		Name: "Overview",
+		Role: ui.RoleTypeTreeItem,
+	}
+	if _, err := helpRootNode.DescendantWithTimeout(ctx, params, 20*time.Second); err != nil {
+		return errors.Wrap(err, "failed to render help app")
+	}
+	return nil
+}
+
+// Launch launches help app and waits for it to present in shelf.
+func Launch(ctx context.Context, tconn *chrome.TestConn) error {
+	app := apps.Help
+	testing.ContextLogf(ctx, "Launching %s", app.Name)
+	if err := apps.Launch(ctx, tconn, app.ID); err != nil {
+		return errors.Wrapf(err, "failed to launch %s", app.Name)
+	}
+
+	testing.ContextLog(ctx, "Wait for app shown in shelf")
+	if err := ash.WaitForApp(ctx, tconn, app.ID); err != nil {
+		return errors.Wrapf(err, "%s did not appear in shelf after launch", app.Name)
+	}
+	return nil
+}
+
+// Exists verifies help app exists or not in accessibility tree.
+func Exists(ctx context.Context, tconn *chrome.TestConn) (bool, error) {
+	return ui.Exists(ctx, tconn, helpRootNodeParams)
+}
+
+// IsPerkShown verifies if the perks tab is displayed or not.
+func IsPerkShown(ctx context.Context, tconn *chrome.TestConn) (bool, error) {
+	helpRootNode, err := HelpRootNode(ctx, tconn)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to find help app")
+	}
+	// Find Overview tab to verify app rendering.
+	params := ui.FindParams{
+		Name: "Perks",
+		Role: ui.RoleTypeTreeItem,
+	}
+	return helpRootNode.DescendantExists(ctx, params)
+}
+
+// HelpRootNode returns the root ui node of Help app.
+func HelpRootNode(ctx context.Context, tconn *chrome.TestConn) (*ui.Node, error) {
+	return ui.FindWithTimeout(ctx, tconn, helpRootNodeParams, 20*time.Second)
+}
