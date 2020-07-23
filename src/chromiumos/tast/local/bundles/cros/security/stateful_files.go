e985b2cd0d1098f07ccab6c8f14ac547af98450b
Judy Hsiao <judyhsiao@google.com>
2020-07-23 10:21:48 +0800

    security.StatefulFiles: Add check for new folder /var/lib/sound_card_init
    This folder is used by sound_card_init to store the amp calibration
    results.
    
    BUG=chromium:1108265
    TEST=tast run ${DUT_IP} security.StatefulFiles
    
    Change-Id: I20601efe61a52f8fb3bd45429571a07a735e6067

--- src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
+++ src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
@@ -227,6 +227,10 @@ func StatefulFiles(ctx context.Context, s *testing.State) {
 		prependPatterns(chk.NewPattern(chk.Tree("encrypted/var/log/displaylink"), users("displaylink"), groups("displaylink"), chk.NotMode(022), chk.SkipChildren()))
 	}
 
+	if _, err := user.Lookup("sound_card_init"); err == nil {
+		prependPatterns(chk.NewPattern(chk.Tree("encrypted/var/lib/sound_card_init"), users("sound_card_init"), groups("sound_card_init"), chk.NotMode(022)))
+	}
+
 	if moblab.IsMoblab() {
 		// On moblab devices, there are additional user dirs and tons of stuff (MySQL, etc.) in /var.
 		prependPatterns(
