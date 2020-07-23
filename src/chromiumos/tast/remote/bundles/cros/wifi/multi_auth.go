a827d4d5fb3d5db62231547b049a2690fb06a7a5
Will Donnelly <wgd@chromium.org>
2020-07-23 14:27:19 -0700

    wifi: Add MultiAuth test
    This is a port of network_WiFi_MultiAuth from autotest.
    
    This test launches two hostapd instances on the same PHY, with
    identical SSID/Channel/Mode but different security settings,
    and then verifies that the client can connect to both.
    
    Porting this test required a couple of changes to the router
    configuration code, to permit reusing the same PHY and to
    ensure that distinct interfaces on the same PHY have unique
    MAC addresses (see site_linux_system.py:595 for the Python
    equivalent of this behavior).
    
    BUG=b:162017454
    TEST=tast run $DUT wifi.MultiAuth
    
    Change-Id: Ie28a882b53487909a5d3842c1cc14aeab73e341d

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/multi_auth.go
@@ -0,0 +1,92 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+
+	"chromiumos/tast/common/wifi/security/wpa"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/remote/wificell/hostapd"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        MultiAuth,
+		Desc:        "Tests the ability to select network correctly among APs with similar network configurations, by configuring two APs with the same SSID/channel/mode but different security config and connecting to each in turn",
+		Contacts:    []string{"wgd@google.com", "chromeos-platform-connectivity@google.com"},
+		Attr:        []string{"group:wificell", "wificell_func", "wificell_unstable"},
+		ServiceDeps: []string{wificell.TFServiceName},
+		Pre:         wificell.TestFixturePreWithCapture(),
+		Vars:        []string{"router", "pcap"},
+	})
+}
+
+func MultiAuth(ctx context.Context, s *testing.State) {
+	tf := s.PreValue().(*wificell.TestFixture)
+	defer func(ctx context.Context) {
+		if err := tf.CollectLogs(ctx); err != nil {
+			s.Log("Error collecting logs, err: ", err)
+		}
+	}(ctx)
+	ctx, cancel := tf.ReserveForCollectLogs(ctx)
+	defer cancel()
+
+	apOpts := []hostapd.Option{hostapd.SSID("an ssid"), hostapd.Mode(hostapd.Mode80211g), hostapd.Channel(1)}
+	wpaCfg := wpa.NewConfigFactory("chromeos", wpa.Mode(wpa.ModePureWPA), wpa.Ciphers(wpa.CipherCCMP))
+
+	s.Log("Configuring AP 0 (Open)")
+	ap0, err := tf.ConfigureAP(ctx, apOpts, nil)
+	if err != nil {
+		s.Fatal("Failed to configure AP 0: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := tf.DeconfigAP(ctx, ap0); err != nil {
+			s.Error("Failed to deconfig AP 0: ", err)
+		}
+	}(ctx)
+	ctx, cancel = tf.ReserveForDeconfigAP(ctx, ap0)
+	defer cancel()
+
+	s.Log("Configuring AP 1 (WPA)")
+	ap1, err := tf.ConfigureAP(ctx, apOpts, wpaCfg)
+	if err != nil {
+		s.Fatal("Failed to configure AP 1: ", err)
+	}
+	s.Log("Configured AP1: ", ap1)
+	defer func(ctx context.Context) {
+		if err := tf.DeconfigAP(ctx, ap1); err != nil {
+			s.Error("Failed to deconfig AP 1: ", err)
+		}
+	}(ctx)
+	ctx, cancel = tf.ReserveForDeconfigAP(ctx, ap1)
+	defer cancel()
+
+	s.Log("Connecting to AP 0")
+	if _, err := tf.ConnectWifiAP(ctx, ap0); err != nil {
+		s.Fatal("Failed to connect to AP 0: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := tf.CleanDisconnectWifi(ctx); err != nil {
+			s.Error("Failed to disconnect WiFi: ", err)
+		}
+	}(ctx)
+	ctx, cancel = tf.ReserveForDisconnect(ctx)
+	defer cancel()
+	s.Log("Verifying connection to AP 0")
+	if err := tf.VerifyConnection(ctx, ap0); err != nil {
+		s.Fatal("Failed to verify connection: ", err)
+	}
+
+	s.Log("Connecting to AP 1")
+	if _, err := tf.ConnectWifiAP(ctx, ap1); err != nil {
+		s.Fatal("Failed to connect to AP 1: ", err)
+	}
+	s.Log("Verifying connection to AP 1")
+	if err := tf.VerifyConnection(ctx, ap1); err != nil {
+		s.Fatal("Failed to verify connection: ", err)
+	}
+}
