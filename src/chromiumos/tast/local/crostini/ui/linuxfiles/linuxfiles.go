f0cdf009571f446e8a0513ff4f517c69c944755a
Judy Wu <jinrongwu@google.com>
2020-07-25 12:48:38 +1000

    Add tast test for renaming a file
    Automate crostini test case: 1.1.2 - rename a file in Linux files and
    container
    
    BUG=chromium:1101583
    TEST=Ran on hatch
    
    Cq-Depend: chromium:2303333
    Change-Id: Ib809672eee10aa0524fd53411fe0f3a8b696ad2d

---
+++ src/chromiumos/tast/local/crostini/ui/linuxfiles/linuxfiles.go
@@ -0,0 +1,12 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package linuxfiles supports actions on Linux files on Files app.
+package linuxfiles
+
+// Linux files name in Files app
+const (
+	DirName = "Linux files"
+	Title   = "Files - " + DirName
+)
