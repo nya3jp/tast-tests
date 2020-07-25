ce2c209bc5b97bbe0d8e6ed1e393e7944eee9197
Jun Mukai <mukai@google.com>
2020-07-24 17:15:23 -0700

    Update Thermal metrics for UI CUJ tests
    For recording the termal data for TPS perspective, the current
    mechanism of revealing all raw data isn't manageable. Rather
    it would be better for us to use the known thermal data points.
    The current candidate is "CPU" only. It will drop any other
    thermal data points.
    
    BUG=chromium:1109015
    TEST=manually
    
    Change-Id: I6bdde9a31addcb219884f491b1f77ca33f5421be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2318691
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Tested-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/thermal.go
@@ -0,0 +1,107 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cuj
+
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/power"
+	"chromiumos/tast/testing"
+)
+
+type thermalMetric struct {
+	paths  []string
+	metric perf.Metric
+}
+
+type thermalDataSource struct {
+	metrics map[string]*thermalMetric
+}
+
+func newThermalDataSource(ctx context.Context) *thermalDataSource {
+	return &thermalDataSource{}
+}
+
+// Setup implements perf.TimelineDatasource.Setup.
+func (ds *thermalDataSource) Setup(ctx context.Context, prefix string) error {
+	thermalSensors, err := power.ListSysfsThermalSensors(ctx)
+	if err != nil {
+		return err
+	}
+	if len(thermalSensors) == 0 {
+		testing.ContextLog(ctx, "No thermal sensors found")
+		return nil
+	}
+
+	// Only CPU thermal data is cared currently.
+	namePatterns := []struct {
+		name    string
+		pattern *regexp.Regexp
+	}{
+		{"CPU", regexp.MustCompile("x86_pkg_temp")}, // Intel devices
+		{"CPU", regexp.MustCompile("B0D4")},         // Intel devices
+		{"CPU", regexp.MustCompile("acpitz")},       // AMD devices
+		{"CPU", regexp.MustCompile("[cC][pP][uU]")}, // several arm devices, typically named cpu_thermal
+	}
+
+	ds.metrics = map[string]*thermalMetric{}
+	for name, path := range thermalSensors {
+		var groupName string
+		for _, data := range namePatterns {
+			if data.pattern.MatchString(name) {
+				groupName = data.name
+				break
+			}
+		}
+		if groupName == "" {
+			testing.ContextLogf(ctx, "Name %s (path %s) does not match to known patterns, skipping", name, path)
+			continue
+		}
+		testing.ContextLogf(ctx, "Group: %s, Name: %q Path: %s", groupName, name, path)
+		metric, ok := ds.metrics[groupName]
+		if !ok {
+			metric = &thermalMetric{metric: perf.Metric{
+				Name:      prefix + "Thermal." + groupName,
+				Unit:      "deg_C",
+				Direction: perf.SmallerIsBetter,
+				Multiple:  true}}
+			ds.metrics[groupName] = metric
+		}
+		metric.paths = append(metric.paths, filepath.Join(path, "temp"))
+	}
+	return nil
+}
+
+// Start implements perf.TimelineDatasource.Start.
+func (ds *thermalDataSource) Start(ctx context.Context) error {
+	return nil
+}
+
+// Snapshot implements perf.TimelineDatasource.Snapshot.
+func (ds *thermalDataSource) Snapshot(ctx context.Context, values *perf.Values) error {
+	for _, metric := range ds.metrics {
+		var sum float64
+		for _, path := range metric.paths {
+			bs, err := ioutil.ReadFile(path)
+			if err != nil {
+				return errors.Wrapf(err, "failed to read %s", path)
+			}
+			temp, err := strconv.ParseFloat(strings.TrimSpace(string(bs)), 64)
+			if err != nil {
+				return errors.Wrapf(err, "failed to parse data %s", string(bs))
+			}
+			sum += temp
+		}
+		values.Append(metric.metric, sum/1000/float64(len(metric.paths)))
+	}
+	return nil
+}
