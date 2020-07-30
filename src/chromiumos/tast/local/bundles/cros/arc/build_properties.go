5f830b1364ea95fbe897defbc4981fbb2effc3c1
Yusuke Sato <yusukes@google.com>
2020-07-29 21:14:25 -0700

    tast-tests: Verify ro.<partition>.product.* properties are properly set
    This CL verifies that ro.<partition>.product.X and ro.product.X have
    the same value.
    
    BUG=b:162024174
    TEST=try
    
    Cq-Depend: chrome-internal:3199948
    Change-Id: I592958bef26ebccd8af377d1153c810398aa8106

--- src/chromiumos/tast/local/bundles/cros/arc/build_properties.go
+++ src/chromiumos/tast/local/bundles/cros/arc/build_properties.go
@@ -162,25 +162,32 @@ func BuildProperties(ctx context.Context, s *testing.State) {
 		}
 	}
 
-	// Starting R, the images have ro.{system,system_ext,product,odm,vendor}.build.* properties
-	// by default to allow vendors to customize the values. ARC++ doesn't need the customization
-	// and uses the same value for all of them. This verifies that all properties share the same
-	// value. On P, the images have only ro.{system,vendor} ones.
+	// Starting R, the images have ro.{system,system_ext,product,odm,vendor}.{build,product}.*
+	// properties by default to allow vendors to customize the values. ARC++ doesn't need the
+	// customization and uses the same value for all of them. This verifies that all properties
+	// share the same value. On P, the images have only ro.{system,vendor} ones.
 	partitions := []string{"system", "system_ext", "product", "odm", "vendor"}
 	allProperties := getAllPropertiesMap()
 	for property := range allProperties {
-		if !strings.HasPrefix(property, "ro.build.") {
-			continue
-		}
-		value := getProperty(property)
-		for _, partition := range partitions {
-			propertyForPartition := strings.Replace(property, "ro.build.", fmt.Sprintf("ro.%s.build.", partition), 1)
-			if !allProperties[propertyForPartition] {
-				// ro.build.X exists, but ro.<partition>.build.X doesn't.
+		for _, category := range []string{"build", "product"} {
+			prefix := fmt.Sprintf("ro.%s.", category)
+			if !strings.HasPrefix(property, prefix) {
 				continue
 			}
-			if valueForPartition := getProperty(propertyForPartition); valueForPartition != value {
-				s.Fatalf("Unexpected %v property: got %q; want %q", propertyForPartition, valueForPartition, value)
+			value := getProperty(property)
+			for _, partition := range partitions {
+				propertyForPartition := strings.Replace(property, prefix, fmt.Sprintf("ro.%s.%s.", partition, category), 1)
+				if strings.HasPrefix(propertyForPartition, "ro.product.product.") {
+					// TODO(yusukes): Syncing ro.product.<partition>.X with ro.product.X is not implemented yet.
+					continue
+				}
+				if !allProperties[propertyForPartition] {
+					// ro.{build,product}.X exists, but ro.<partition>.{build,product}.X doesn't.
+					continue
+				}
+				if valueForPartition := getProperty(propertyForPartition); valueForPartition != value {
+					s.Fatalf("Unexpected %v property: got %q; want %q", propertyForPartition, valueForPartition, value)
+				}
 			}
 		}
 	}
