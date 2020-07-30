c8f136fc118ea4049bd15de1913e749267fd52e2
Brendan Hansknecht <bhansknecht@chromium.org>
2020-07-30 16:57:28 -0700

    tast-tests: use GAIA in ui.LauncherSearchAndroidApps
    Play Store crashes when it is launched from a test account. The crashes
    make this test flaky. Instead, use GAIA creds to login.
    
    BUG=chromium:1111428
    TEST=tast run <betty> ui.LauncherSearchAndroidApps
    
    Cq-Depend: chrome-internal:3193056
    Change-Id: Iddeeb57f6896f71e9e1d1252e308c9a4040f3b4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2330714
    Tested-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Auto-Submit: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: Brendan Hansknecht <bhansknecht@chromium.org>
    (cherry picked from commit 00bf3290ec9fef3294bcc5b864bc0d0d2761bb1a)

--- src/chromiumos/tast/local/bundles/cros/ui/launcher_search_android_apps.go
+++ src/chromiumos/tast/local/bundles/cros/ui/launcher_search_android_apps.go
@@ -10,7 +10,12 @@ import (
 
 	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/arc"
+<<<<<<< HEAD   (166d58 Tast: Stop Assistant after assistant tests)
 	"chromiumos/tast/local/bundles/cros/ui/faillog"
+=======
+	"chromiumos/tast/local/arc/optin"
+	"chromiumos/tast/local/chrome"
+>>>>>>> CHANGE (00bf32 tast-tests: use GAIA in ui.LauncherSearchAndroidApps)
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/ui/launcher"
 	"chromiumos/tast/testing"
@@ -35,22 +40,58 @@ func init() {
 			ExtraSoftwareDeps: []string{"android_vm"},
 			Pre:               arc.VMBooted(),
 		}},
+		Vars: []string{"ui.LauncherSearchAndroidApps.username", "ui.LauncherSearchAndroidApps.password"},
 	})
 }
 
 func LauncherSearchAndroidApps(ctx context.Context, s *testing.State) {
-	cr := s.PreValue().(arc.PreData).Chrome
-	a := s.PreValue().(arc.PreData).ARC
+	username := s.RequiredVar("ui.LauncherSearchAndroidApps.username")
+	password := s.RequiredVar("ui.LauncherSearchAndroidApps.password")
+
+	args := []string{"--arc-disable-app-sync", "--arc-disable-play-auto-install", "--arc-disable-locale-sync", "--arc-play-store-auto-update=off"}
+	cr, err := chrome.New(ctx, chrome.GAIALogin(), chrome.Auth(username, password, "gaia-id"), chrome.ARCSupported(),
+		chrome.ExtraArgs(args...))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// Optin to Play Store.
+	s.Log("Opting into Play Store")
+	if err := optin.Perform(ctx, cr, tconn); err != nil {
+		s.Fatal("Failed to optin to Play Store: ", err)
+	}
+	if err := optin.WaitForPlayStoreShown(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for Play Store: ", err)
+	}
+	if err := apps.Close(ctx, tconn, apps.PlayStore.ID); err != nil {
+		s.Fatal("Failed to close Play Store: ", err)
+	}
+
+	// Setup ARC.
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
 	if err := a.WaitIntentHelper(ctx); err != nil {
 		s.Fatal("Failed to wait for ARC Intent Helper: ", err)
 	}
 
+<<<<<<< HEAD   (166d58 Tast: Stop Assistant after assistant tests)
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to connect Test API: ", err)
 	}
 	defer faillog.DumpUITreeOnError(ctx, s, tconn)
 
+=======
+>>>>>>> CHANGE (00bf32 tast-tests: use GAIA in ui.LauncherSearchAndroidApps)
 	if err := launcher.SearchAndLaunch(ctx, tconn, apps.PlayStore.Name); err != nil {
 		s.Fatal("Failed to launch Play Store: ", err)
 	}
