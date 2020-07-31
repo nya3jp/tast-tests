2d9c79de364ebf8cf81302a5c8cba0fa1fcefeaf
Yichen Zhou <yichenz@chromium.org>
2020-07-31 05:58:24 +0000

    Add support for screen recording during Tast tests
    BUG=chromium:1107747
    TEST=tast run ui.'*CUJ*'
    Change-Id: Ibe6275d99f9af832cb75dbfc0b305de253f1ea25

---
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/screen_recorder.go
@@ -0,0 +1,164 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cuj
+
+import (
+	"context"
+	"io/ioutil"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+)
+
+const (
+	timeout = 10 * time.Second
+)
+
+// ScreenRecorder is a utility to record the screen during a test scenario.
+type ScreenRecorder struct {
+	isRecording   bool
+	videoRecorder *chrome.JSObject
+	result        string
+}
+
+// NewScreenRecorder creates a ScreenRecorder. It only needs to create one
+// ScreenRecorder during one test. It chooses the entire desktop as the media
+// stream.
+func NewScreenRecorder(ctx context.Context, tconn *chrome.TestConn) (*ScreenRecorder, error) {
+	expr := `(function() {
+		let videoRecorder = {
+			chunks: [],
+			recorder: null,
+			streamPromise: null,
+			request: function() {
+				this.streamPromise = navigator.mediaDevices.getDisplayMedia({
+					audio: false,
+					video: {
+						cursor: "always"
+					}
+				});
+			},
+			start: function() {
+				this.chunks = [];
+				this.streamPromise.then(
+					stream => {
+						this.recorder = new MediaRecorder(stream, {mimeType: 'video/webm;codecs=vp9'});
+						this.recorder.ondataavailable = function(e) {
+							this.chunks.push(e.data);
+						}.bind(this);
+						this.recorder.start();
+					}
+				);
+			},
+			stop: function() {
+				return new Promise((resolve, reject) => {
+					this.recorder.onstop = function() {
+						var blob = new Blob(this.chunks, {'type': 'video/webm'});
+						var reader = new FileReader();
+						reader.onload = function() {
+							resolve(reader.result);
+						}
+						reader.readAsDataURL(blob);
+					}.bind(this);
+					this.recorder.stop();
+				})
+			}
+		};
+		return videoRecorder;
+	})()
+	`
+	videoRecorder := &chrome.JSObject{}
+	if err := tconn.Eval(ctx, expr, videoRecorder); err != nil {
+		return nil, errors.Wrap(err, "failed to initialize video recorder")
+	}
+	sr := &ScreenRecorder{isRecording: false, videoRecorder: videoRecorder}
+
+	// Request to share the screen.
+	if err := sr.videoRecorder.Call(ctx, nil, `function() {this.request();}`); err != nil {
+		return nil, errors.Wrap(err, "failed to request display media")
+	}
+
+	// Choose to record the entire desktop/screen with no audio.
+	mediaview, err := ui.FindWithTimeout(ctx, tconn, ui.FindParams{Name: "Share your screen", ClassName: "DesktopMediaPickerDialogView"}, timeout)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find media picker view")
+	}
+	defer mediaview.Release(ctx)
+
+	desktopView, err := mediaview.DescendantWithTimeout(ctx, ui.FindParams{ClassName: "DesktopMediaPicker_DesktopMediaSourceView", Role: ui.RoleTypeButton}, timeout)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find the desktop view")
+	}
+	defer desktopView.Release(ctx)
+	if err := desktopView.FocusAndWait(ctx, timeout); err != nil {
+		return nil, errors.Wrap(err, "failed to focus on the desktop view")
+	}
+	if err := desktopView.LeftClick(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to click the desktop view")
+	}
+
+	shareButton, err := mediaview.DescendantWithTimeout(ctx, ui.FindParams{Name: "Share", Role: ui.RoleTypeButton}, timeout)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find the share button")
+	}
+	defer shareButton.Release(ctx)
+	if err := shareButton.FocusAndWait(ctx, timeout); err != nil {
+		return nil, errors.Wrap(err, "failed to focus on the share button")
+	}
+	if err := shareButton.LeftClick(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to click the share button")
+	}
+
+	return sr, nil
+}
+
+// Start creates a new media recorder and starts to record the screen. As long as ScreenRecorder
+// is not recording, it can start to record again.
+func (r *ScreenRecorder) Start(ctx context.Context) error {
+	if r.isRecording == true {
+		return errors.New("recorder already started")
+	}
+
+	if err := r.videoRecorder.Call(ctx, nil, `function() {this.start();}`); err != nil {
+		return errors.Wrap(err, "failed to start to record screen")
+	}
+	r.isRecording = true
+
+	return nil
+}
+
+// Stop ends the screen recording and stores the encoded base64 string.
+func (r *ScreenRecorder) Stop(ctx context.Context) error {
+	if r.isRecording == false {
+		return errors.New("recorder hasn't started yet")
+	}
+
+	var result string
+	if err := r.videoRecorder.Call(ctx, &result, `function() {return this.stop();}`); err != nil {
+		return errors.Wrap(err, "failed to stop recording screen")
+	}
+	r.result = result
+	r.isRecording = false
+
+	return nil
+}
+
+// Save saves the latest encoded string into a file.
+func (r *ScreenRecorder) Save(ctx context.Context, filepath string) error {
+	if err := ioutil.WriteFile(filepath, []byte(r.result), 0644); err != nil {
+		return errors.Wrapf(err, "failed to dump encoded string to %s", filepath)
+	}
+	return nil
+}
+
+// Release frees the reference to Javascript for this video recorder.
+func (r *ScreenRecorder) Release(ctx context.Context) {
+	if r.isRecording {
+		r.Stop(ctx)
+	}
+	r.videoRecorder.Release(ctx)
+}
