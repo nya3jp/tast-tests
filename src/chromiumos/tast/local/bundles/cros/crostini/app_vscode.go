d21ef4e431585475311c0f0fb25d9be02aeda770
Judy Wu <jinrongwu@google.com>
2020-07-31 23:03:34 +1000

    Add tast test to create file with visual studio in Terminal
    Automate crostini test case: 1.15.2 - test creating a file using visual studio code in Terminal and verify the content in container
    
    BUG=chromium:1101583
    TEST=Ran on hatch
    
    Cq-Depend: chromium:2331827
    Change-Id: I31025b5c73c34c0307033700cabb259f467fae03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2331831
    Tested-by: Judy Wu <jinrongwu@google.com>
    Reviewed-by: Matthew Warton <mwarton@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Judy Wu <jinrongwu@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/app_vscode.go
@@ -0,0 +1,140 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/mouse"
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/local/crostini/ui/terminalapp"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/vm"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     AppVscode,
+		Desc:     "Test Visual Studio Code in Terminal window",
+		Contacts: []string{"jinrongwu@google.com", "cros-containers-dev@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+		Vars:     []string{"keepState"},
+		Params: []testing.Param{{
+			Name:              "download_buster",
+			Pre:               crostini.StartedByDownloadBusterLargeContainer(),
+			ExtraHardwareDeps: crostini.CrostiniAppTest,
+			Timeout:           15 * time.Minute,
+		}},
+		SoftwareDeps: []string{"chrome", "vm_host", "amd64"},
+	})
+}
+func AppVscode(ctx context.Context, s *testing.State) {
+	tconn := s.PreValue().(crostini.PreData).TestAPIConn
+	cr := s.PreValue().(crostini.PreData).Chrome
+	keyboard := s.PreValue().(crostini.PreData).Keyboard
+	cont := s.PreValue().(crostini.PreData).Container
+
+	// Use a shortened context for test operations to reserve time for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 90*time.Second)
+	defer cancel()
+	defer crostini.RunCrostiniPostTest(cleanupCtx, s.PreValue().(crostini.PreData))
+
+	// Open Terminal app.
+	terminalApp, err := terminalapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to open Terminal app: ", err)
+	}
+
+	restartIfError := true
+
+	defer func() {
+		// Restart Crostini in the end in case any error in the middle and Visual Studio Code is not closed.
+		// This also closes the Terminal window.
+		if restartIfError {
+			if err := terminalApp.RestartCrostini(cleanupCtx, keyboard, cont, cr.User()); err != nil {
+				s.Fatal("Failed to restart Crostini: ", err)
+			}
+		} else {
+			terminalApp.Exit(cleanupCtx, keyboard)
+		}
+	}()
+
+	if err := testCreateFileWithVSCode(ctx, terminalApp, keyboard, tconn, cont); err != nil {
+		s.Fatal("Failed to create file with Visual Studio Code in Terminal: ", err)
+	}
+
+	restartIfError = false
+}
+
+func testCreateFileWithVSCode(ctx context.Context, terminalApp *terminalapp.TerminalApp, keyboard *input.KeyboardEventWriter, tconn *chrome.TestConn, cont *vm.Container) error {
+	const (
+		testFile   = "test.go"
+		testString = "//This is a test string."
+	)
+
+	// Launch Visual Studio Code.
+	cmd := fmt.Sprintf("code %s", testFile)
+	if err := terminalApp.RunCommand(ctx, keyboard, cmd); err != nil {
+		return errors.Wrapf(err, "failed to run command '%q' in Terminal window", cmd)
+	}
+
+	param := ui.FindParams{
+		Name: fmt.Sprintf("● %s - Visual Studio Code", testFile),
+		Role: ui.RoleTypeWindow,
+	}
+
+	// Find the app window.
+	appWindow, err := ui.FindWithTimeout(ctx, tconn, param, 15*time.Second)
+	if err != nil {
+		return errors.Wrap(err, "failed to find the Visual Studio Code window")
+	}
+
+	// Sometimes left click could not focus on the new window. Moving the mouse first to make sure the cursor goes to the new window.
+	if err = mouse.Move(ctx, tconn, appWindow.Location.CenterPoint(), 5*time.Second); err != nil {
+		return errors.Wrap(err, "failed to move to the center of the Visual Studio Code window")
+	}
+
+	// Left click the app window.
+	if err = appWindow.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "failed left click on Visual Studio Code window")
+	}
+
+	// Type test string into the new file.
+	if err = keyboard.Type(ctx, testString); err != nil {
+		return errors.Wrapf(err, "failed to type %q in Visual Studio Code window", testString)
+	}
+
+	// Press ctrl+S to save the file.
+	if err = keyboard.Accel(ctx, "ctrl+S"); err != nil {
+		return errors.Wrap(err, "failed to press ctrl+S in Visual Studio Code window")
+	}
+
+	// Press ctrl+W twice to exit window.
+	if err = keyboard.Accel(ctx, "ctrl+W"); err != nil {
+		return errors.Wrap(err, "failed to press ctrl+W in Visual Studio Code window")
+	}
+	if err = keyboard.Accel(ctx, "ctrl+W"); err != nil {
+		return errors.Wrap(err, "failed to press ctrl+W in Visual Studio Code window")
+	}
+
+	if err = ui.WaitUntilGone(ctx, tconn, param, 15*time.Second); err != nil {
+		return errors.Wrap(err, "failed to close Visual Studio Code window")
+	}
+
+	// Check the content of the test file.
+	if err := cont.CheckFileContent(ctx, testFile, testString); err != nil {
+		return errors.Wrap(err, "failed to verify the content of the file")
+	}
+
+	return nil
+}
