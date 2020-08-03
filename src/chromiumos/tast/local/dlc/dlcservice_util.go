21cb10d554ea526092e4c612057cefff1c4b64a2
Nicholas Hollingum <hollingum@google.com>
2020-08-04 08:09:32 +1000

    tast: Restructure dlc tests
    We restructure the DLC tests such that:
     - A subset of its utilities (particularly install, remove, etc) area
       made available to other tast tests. Previously they were private to
       tests in cros/platform.
     - The remaining utilities (which should only be used when testing DLC
       itself, not by tests that use DLC) are left in cros/platform and
       their package is renamed "dlctest" to avoid collision.
     - CopyFile is renamed to CopyFileWithPermissions to distinguish it
       from the one in fsutil.
     - RestartUpstartJobAndWait is moved to local/upstart.
    
    BUG=b:162564992
    TEST=tast -verbose run $DUT "*DLC*"
    
    Change-Id: Ide2f24cb131d5378242cf978a7474b1bb1e03668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2335833
    Tested-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Matthew Warton <mwarton@google.com>

---
+++ src/chromiumos/tast/local/dlc/dlcservice_util.go
@@ -0,0 +1,72 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package dlc provides ways to interact with dlcservice daemon and utilities.
+package dlc
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+// Dlcservice related constants.
+const (
+	CacheDir    = "/var/cache/dlc"
+	JobName     = "dlcservice"
+	LibDir      = "/var/lib/dlcservice/dlc"
+	PreloadDir  = "/mnt/stateful_partition/var_overlay/cache/dlc-images"
+	ServiceName = "org.chromium.DlcService"
+	User        = "dlcservice"
+)
+
+// Info holds the fields related to a DLC.
+type Info struct {
+	ID      string
+	Package string
+}
+
+// Install calls the DBus method to install a DLC.
+func Install(ctx context.Context, id, omahaURL string) error {
+	testing.ContextLog(ctx, "Installing DLC: ", id, " using ", omahaURL)
+	if err := testexec.CommandContext(ctx, "dlcservice_util", "--install", "--id="+id, "--omaha_url="+omahaURL).Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "failed to install")
+	}
+	return nil
+}
+
+// Purge calls the DBus method to Purge a DLC.
+func Purge(ctx context.Context, id string) error {
+	testing.ContextLog(ctx, "Purging DLC: ", id)
+	if err := testexec.CommandContext(ctx, "dlcservice_util", "--purge", "--id="+id).Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "failed to purge")
+	}
+	return nil
+}
+
+// Cleanup removes all DLC related states and restarts dlcservice.
+func Cleanup(ctx context.Context, infos ...Info) error {
+	for _, info := range infos {
+		// Unmount the DLC.
+		path := filepath.Join("/run/imageloader", info.ID, info.Package)
+		if err := testexec.CommandContext(ctx, "imageloader", "--unmount", "--mount_point="+path).Run(testexec.DumpLogOnError); err != nil {
+			return errors.Wrapf(err, "failed to unmount DLC (%s)", info.ID)
+		}
+		// Remove all related directories.
+		for _, dir := range []string{CacheDir, LibDir, PreloadDir} {
+			if err := os.RemoveAll(filepath.Join(dir, info.ID)); err != nil {
+				return errors.Wrapf(err, "failed to cleanup directory (%s)", dir)
+			}
+		}
+	}
+	if err := upstart.RestartJobAndWaitForDbusService(ctx, JobName, ServiceName); err != nil {
+		return errors.Wrap(err, "failed to restart dlcservice")
+	}
+	return nil
+}
---
+++ src/chromiumos/tast/local/upstart/dbus.go
@@ -0,0 +1,29 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package upstart
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+)
+
+// RestartJobAndWaitForDbusService is a utility for restarting jobs
+// that provide named dbus services. It restarts the |job| as normal
+// and waits for the |serviceName| service to be available.
+func RestartJobAndWaitForDbusService(ctx context.Context, job, serviceName string) error {
+	if err := RestartJob(ctx, job); err != nil {
+		return errors.Wrapf(err, "failed to restart %s", job)
+	}
+
+	// Wait for service to be ready.
+	if bus, err := dbusutil.SystemBus(); err != nil {
+		return errors.Wrap(err, "failed to connect to the message bus")
+	} else if err := dbusutil.WaitForService(ctx, bus, serviceName); err != nil {
+		return errors.Wrapf(err, "failed to wait for D-Bus service %s", serviceName)
+	}
+	return nil
+}
