f9417c52b2c560f960a8adc44e8b013f5fc04438
alexanderhartl <alexanderhartl@google.com>
2020-08-04 19:18:24 +0000

    tast-tests: add DefaultGeolocationSetting
    add policy.DefaultGeolocationSetting tast test.
    
    BUG=None
    TEST=tast run <ip> policy.DefaultGeolocationSetting
    
    Change-Id: I80144f04c5fc855d2620b4c81b6b2e76b4097323

---
+++ src/chromiumos/tast/local/bundles/cros/policy/default_geolocation_setting.go
@@ -0,0 +1,119 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/bundles/cros/policy/pre"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DefaultGeolocationSetting,
+		Desc: "Behavior of DefaultGeolocationSetting policy, checking the location site settings after setting the policy",
+		Contacts: []string{
+			"alexanderhartl@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+	})
+}
+
+// DefaultGeolocationSetting tests the DefaultGeolocationSetting policy.
+func DefaultGeolocationSetting(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	for _, param := range []struct {
+		name           string
+		nodeName       string                            // the name of the toggle button node we want to check.
+		wantRestricted bool                              // wantRestricted is the wanted restriction state of the toggle button in the location settings.
+		wantChecked    ui.CheckedState                   // wantChecked is the wanted checked state of the toggle button in the location settings.
+		value          *policy.DefaultGeolocationSetting // value is the value of the policy.
+	}{
+		{
+			name:           "unset",
+			nodeName:       "Ask before accessing (recommended)",
+			wantRestricted: false,
+			wantChecked:    ui.CheckedStateTrue,
+			value:          &policy.DefaultGeolocationSetting{Stat: policy.StatusUnset},
+		},
+		{
+			name:           "allow",
+			nodeName:       "Ask before accessing (recommended)",
+			wantRestricted: true,
+			wantChecked:    ui.CheckedStateTrue,
+			value:          &policy.DefaultGeolocationSetting{Val: 1},
+		},
+		{
+			name:           "deny",
+			nodeName:       "Blocked",
+			wantRestricted: true,
+			wantChecked:    ui.CheckedStateFalse,
+			value:          &policy.DefaultGeolocationSetting{Val: 2},
+		},
+		{
+			name:           "ask",
+			nodeName:       "Ask before accessing (recommended)",
+			wantRestricted: true,
+			wantChecked:    ui.CheckedStateTrue,
+			value:          &policy.DefaultGeolocationSetting{Val: 3},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndRefresh(ctx, fdms, cr, []policy.Policy{param.value}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			// Connect to Test API to use it with the ui library.
+			tconn, err := cr.TestAPIConn(ctx)
+			if err != nil {
+				s.Fatal("Failed to create Test API connection: ", err)
+			}
+
+			// Open settings page where the affected toggle button can be found.
+			conn, err := cr.NewConn(ctx, "chrome://settings/content/location")
+			if err != nil {
+				s.Fatal("Failed to connect to the settings page: ", err)
+			}
+			defer conn.Close()
+
+			// Select the tab if it is not selected already.
+			node, err := ui.FindWithTimeout(ctx, tconn, ui.FindParams{
+				Role: ui.RoleTypeToggleButton,
+				Name: param.nodeName,
+			}, 15*time.Second)
+			if err != nil {
+				s.Fatalf("Finding %s node failed: %v", param.nodeName, err)
+			}
+			defer node.Release(ctx)
+
+			// Check the restriction setting of the toggle button.
+			if restricted := (node.Restriction == ui.RestrictionDisabled || node.Restriction == ui.RestrictionReadOnly); restricted != param.wantRestricted {
+				s.Logf("The restriction attribute is %q", node.Restriction)
+				s.Errorf("Unexpected toggle button restriction in the settings: got %t; want %t", restricted, param.wantRestricted)
+			}
+
+			if node.Checked != param.wantChecked {
+				s.Errorf("Unexpected toggle button checked state in the settings: got %t; want %t", node.Checked, param.wantChecked)
+			}
+
+		})
+	}
+}
