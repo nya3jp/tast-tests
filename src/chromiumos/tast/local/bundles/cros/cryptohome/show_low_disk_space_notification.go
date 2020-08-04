b9d40037f164f2ec3d90c7ba82b0231bc797ecde
Victor-Gabriel Savu <vsavu@google.com>
2020-08-04 10:54:31 +0200

    tast-tests: add cryptohome.ShowLowDiskSpaceNotification
    Add a test that checks that the low disk space notification is
    shown to the user.
    
    BUG=chromium:1099604
    TEST=tast run <ip> cryptohome.ShowLowDiskSpaceNotification
    
    Change-Id: I24439bf9d2ec3f5f449068d09832f5ad7df49e14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2335441
    Tested-by: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Greg Edelston <gredelston@google.com>
    Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/show_low_disk_space_notification.go
@@ -0,0 +1,73 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"context"
+	"os"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/cryptohome/cleanup"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ShowLowDiskSpaceNotification,
+		Desc: "Test showing the low disk space notification",
+		Contacts: []string{
+			"vsavu@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func ShowLowDiskSpaceNotification(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get test API connection: ", err)
+	}
+
+	fillFile, err := cleanup.FillUntil(cleanup.UserHome, cleanup.MinimalFreeSpace)
+	if err != nil {
+		s.Fatal("Failed to fill disk space: ", err)
+	}
+	defer func() {
+		if err := os.Remove(fillFile); err != nil {
+			s.Errorf("Failed to remove fill file %s: %v", fillFile, err)
+		}
+	}()
+
+	s.Log("Waiting for notification")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		notifications, err := ash.VisibleNotifications(ctx, tconn)
+		if err != nil {
+			return testing.PollBreak(errors.Wrap(err, "failed to get notifications"))
+		}
+
+		for _, notification := range notifications {
+			// Hardcoded in Chrome.
+			if strings.Contains(notification.ID, "low_disk") {
+				return nil
+			}
+		}
+
+		return errors.New("could not find low disk space notification")
+
+	}, &testing.PollOptions{
+		Timeout: 80 * time.Second, // Checks for low disk space run once per minute.
+	}); err != nil {
+		s.Error("Notification not shown: ", err)
+	}
+}
