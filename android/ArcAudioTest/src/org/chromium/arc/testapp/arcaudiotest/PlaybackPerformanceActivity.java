d5bd05fb8c7baa4af8f3ffabcc48a8b8f349aef0
Judy Hsiao <judyhsiao@google.com>
2020-08-05 11:01:08 +0800

    ArcAudioTest: use USAGE_GAME in PlaybackPerformanceAct
    PlaybackPerformanaceActivity needs to play audio with different
    AudioTrack performance mode.
    This CL use AudioAttributes.USAGE_GAME in
    PlaybackPerformanaceActivity to prevent AudioTrack automatically
    change its performance mode from PERFORMANCE_MODE_NONE to
    PERFORMANCE_MODE_POWER_SAVING when it's created with default
    AudioAttribute.
    
    BUG=b:158805171
    BUG=b:155950426
    TEST=tast run ${DUT_IP} arc.PowerAudioPlaybackPerf.* and verify the
    output stream flag in audio hal.
    
    Change-Id: I25a2e552a52e6cd9717bf8e3984147628b78f145

--- android/ArcAudioTest/src/org/chromium/arc/testapp/arcaudiotest/PlaybackPerformanceActivity.java
+++ android/ArcAudioTest/src/org/chromium/arc/testapp/arcaudiotest/PlaybackPerformanceActivity.java
@@ -8,6 +8,7 @@ import static org.chromium.arc.testapp.arcaudiotest.Constant.TAG;
 
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioAttributes;
 import android.media.AudioFormat;
 import android.media.AudioTrack;
 import android.os.Bundle;
@@ -53,6 +54,12 @@ public class PlaybackPerformanceActivity extends MainActivity {
                     AudioFormat.CHANNEL_OUT_STEREO,
                     fd);
             player.setPerformanceMode(mPerformanceMode);
+            // Using any usage other than USAGE_MEDIA to prevent the performance mode from being
+            // automatically change from PERFORMANCE_MODE_NONE to PERFORMANCE_MODE_POWER_SAVING
+            // by AudioTrack.
+            player.setAudioAttributes(new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .build());
             mThread = new SamplePlayerThread(player, mDurationSecond * 1000);
             mThread.start();
         } catch (Exception e) {
