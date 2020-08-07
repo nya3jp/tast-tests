ebed3ab2246fc8b2f228e17946e5f811ecda94bf
Kartik Hegde <khegde@chromium.org>
2020-08-07 11:52:03 -0600

    tast-tests: Promote CrosHealthdProbeSystemInfo to CQ
    This test has passed in 20+ builds.
    
    BUG=chromium:1057495
    TEST=tast run --build $DUT_IP platform.CrosHealthdProbeSystemInfo
    
    Change-Id: I7aef8e2d5f34ef322b2ac9904c7869c7d5abf0da

--- src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_system_info.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_system_info.go
@@ -24,7 +24,7 @@ func init() {
 			"khegde@google.com",
 			"pmoy@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"cros_config", "diagnostics"},
 	})
 }
