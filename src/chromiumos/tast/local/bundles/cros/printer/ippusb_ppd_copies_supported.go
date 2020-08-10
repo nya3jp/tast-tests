68dc28a77430466ada966751fb08a175dfe180bf
Kalvin Lee <kdlee@chromium.org>
2020-08-10 13:45:53 -0700

    printing: promote printer.IPPUSBPPD* to CQ
    This change promotes
    * printer.IPPUSBPPDCopiesSupported,
    * printer.IPPUSBPPDCopiesUnsupported, and
    * printer.IPPUSBPPDNoCopies
    
    to the CQ. These tests have stabilized with the introduction of
    ippusb_bridge and only fail on certain development models for what look
    like unrelated reasons.
    
    BUG=chromium:1083421
    TEST=tast run <dut> 'printer.IPPUSBPPD*'
    
    Disallow-Recycled-Builds: amd64-generic-cq
    Change-Id: I63f130653c48cc86939682c56b012105f7ab33f5

--- src/chromiumos/tast/local/bundles/cros/printer/ippusb_ppd_copies_supported.go
+++ src/chromiumos/tast/local/bundles/cros/printer/ippusb_ppd_copies_supported.go
@@ -17,7 +17,7 @@ func init() {
 		Func:         IPPUSBPPDCopiesSupported,
 		Desc:         "Verifies that the 'copies-supported' attribute of the printer is used to populate the cupsManualCopies and cupsMaxCopies values in the corresponding generated PPD",
 		Contacts:     []string{"skau@chromium.org", "project-bolton@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome", "cups", "virtual_usb_printer"},
 		Data:         []string{"ippusb_copies_supported.json"},
 		Pre:          chrome.LoggedIn(),
