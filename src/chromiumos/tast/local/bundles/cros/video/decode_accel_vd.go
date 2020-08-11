609350148fc07783d25e53f2f3d358c1fffcd74b
Hirokazu Honda <hiroh@chromium.org>
2020-08-12 07:39:46 +0900

    video: Re-Enable vp9 profile 2 decoding test
    This enables vp9 profile2 decoding test running with
    VaapiVideoDecoder. On the other hand, this deletes the test
    case running with VaapiVideoDecodeAccelerator because it will
    never support vp9 profile 2.
    
    BUG=chromium:911754, b:155054799
    TEST=video.DecodeAccelVD.vp9_2 on rammus
    
    Change-Id: I11900aaa23ac5a6a0184d69c8ff35697167e595b

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd.go
@@ -37,9 +37,8 @@ func init() {
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
 		}, {
-			Name: "vp9_2",
-			Val:  "test-25fps.vp9_2",
-			// TODO(crbug.com/911754): reenable this test once HDR VP9.2 is implemented.
+			Name:              "vp9_2",
+			Val:               "test-25fps.vp9_2",
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_2},
 			ExtraData:         []string{"test-25fps.vp9_2", "test-25fps.vp9_2.json"},
 		}, {
