aa831fa9c0012a8f82661ff09f83efe0faccf07c
Billy Zhao <billyzhao@google.com>
2020-08-11 06:18:13 +0000

    tast-tests: Add coexistence persistence test
    PersistenceBluetoothSansWifi will verify that bluetooth operations are not affected
    when wifi is disabled on reboot.
    
    BUG=b:161902990
    TEST=test_that ${DUT} wifi.PersistenceBluetoothSansWifi(board: hatch)
    
    Change-Id: Ic9abd0bcaff67091afa092ea28acd9f8e952613d

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/persistence_bluetooth_sans_wifi.go
@@ -0,0 +1,138 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/remote/bundles/cros/wifi/wifiutil"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PersistenceBluetoothSansWifi,
+		Desc:         "Verifies that Bluetooth remains operational when Wifi is disabled on reboot",
+		Contacts:     []string{"billyzhao@google.com", "chromeos-platform-connectivity@google.com"},
+		Attr:         []string{"group:wificell", "wificell_func", "wificell_unstable"},
+		SoftwareDeps: []string{"reboot"},
+		ServiceDeps:  []string{wificell.TFServiceName, "tast.cros.network.BluetoothService"},
+		Vars:         []string{"router"},
+	})
+}
+
+func PersistenceBluetoothSansWifi(ctx context.Context, s *testing.State) {
+	// Cleanup on exit.
+	defer func(ctx context.Context) {
+		d := s.DUT()
+		r, err := rpc.Dial(ctx, d, s.RPCHint(), "cros")
+		if err != nil {
+			s.Fatal("Failed to connect rpc: ", err)
+		}
+		defer r.Close(ctx)
+		wifiClient := network.NewWifiServiceClient(r.Conn)
+		btClient := network.NewBluetoothServiceClient(r.Conn)
+		// Enable wifi device.
+		if _, err := wifiClient.SetWifiStatus(ctx, &network.SetWifiStatusRequest{Status: true}); err != nil {
+			s.Error("Could not enable Wifi through shill: ", err)
+		}
+		// Enable bluetooth device.
+		if _, err := btClient.SetBluetoothStatus(ctx, &network.SetBluetoothStatusRequest{State: true}); err != nil {
+			s.Error("Could not enable Bluetooth through dbus: ", err)
+		}
+
+		// Reboot the DUT.
+		if err := d.Reboot(ctx); err != nil {
+			s.Log("Reboot failed: ", err)
+		}
+	}(ctx)
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	func(ctx context.Context) {
+		d := s.DUT()
+		r, err := rpc.Dial(ctx, d, s.RPCHint(), "cros")
+		if err != nil {
+			s.Fatal("Failed to connect rpc: ", err)
+		}
+		defer r.Close(ctx)
+
+		// Assert WiFi is up.
+		router, _ := s.Var("router")
+		tf, err := wificell.NewTestFixture(ctx, ctx, d, s.RPCHint(), wificell.TFRouter(router))
+		if err != nil {
+			s.Fatal("Failed to set up test fixture: ", err)
+		}
+		defer func(ctx context.Context) {
+			if err := tf.Close(ctx); err != nil {
+				s.Error("Failed to properly take down test fixture: ", err)
+			}
+		}(ctx)
+		ctx, cancel := tf.ReserveForClose(ctx)
+		defer cancel()
+
+		if err := wifiutil.AssertWifiEnabled(ctx, tf); err != nil {
+			s.Fatal("Wifi not functioning: ", err)
+		}
+
+		// Assert bluetooth is up.
+		btClient := network.NewBluetoothServiceClient(r.Conn)
+		if response, err := btClient.GetBluetoothStatus(ctx, &empty.Empty{}); err != nil {
+			s.Fatal("Could not get Bluetooth status: ", err)
+		} else if !response.Status {
+			s.Fatal("Bluetooth is off, expected to be on ")
+		}
+
+		// Disable WiFi.
+		wifiClient := network.NewWifiServiceClient(r.Conn)
+		if _, err := wifiClient.SetWifiStatus(ctx, &network.SetWifiStatusRequest{Status: false}); err != nil {
+			s.Fatal("Could not disable Wifi: ", err)
+		}
+
+		// Assert WiFi is down.
+		if response, err := wifiClient.GetWifiStatus(ctx, &empty.Empty{}); err != nil {
+			s.Fatal("Could not get WiFi status: ", err)
+		} else if response.Status {
+			s.Fatal("Wifi is on, expected to be off ")
+		}
+
+	}(ctx)
+
+	// Reboot the DUT.
+	if err := s.DUT().Reboot(ctx); err != nil {
+		s.Fatal("Failed to reboot DUT: ", err)
+	}
+
+	// Reinitialize gRPC connection with DUT after reboot as the current session is now stale.
+	d := s.DUT()
+	r, err := rpc.Dial(ctx, d, s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect rpc: ", err)
+	}
+	defer r.Close(ctx)
+
+	// Assert WiFi is down.
+	wifiClient := network.NewWifiServiceClient(r.Conn)
+	if response, err := wifiClient.GetWifiStatus(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Could not get WiFi status: ", err)
+	} else if response.Status {
+		s.Fatal("Wifi is on, expected to be off ")
+	}
+
+	// Assert Bluetooth is up.
+	btClient := network.NewBluetoothServiceClient(r.Conn)
+	if response, err := btClient.GetBluetoothStatus(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Could not get Bluetooth status: ", err)
+	} else if !response.Status {
+		s.Fatal("Bluetooth is off, expected to be on ")
+	}
+}
