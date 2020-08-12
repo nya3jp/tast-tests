72d4282565df9966a421bfa39abbcc213c8dc271
Efi Mavroudi <emavroudi@google.com>
2020-08-12 21:38:52 +0000

    ocr: Add GenerateSearchablePDFFromImage tast test
    Create a new Tast test component, 'ocr', and add a new test,
    GenerateSearchablePDFFromImage, which tests generating a searchable PDF
    from an input image document via the Chrome OS OCR service.
    It compares the output PDF with a golden PDF file produced by the OCR
    service.
    
    BUG=b:159229207
    TEST=tast -verbose run 127.0.0.1:2222 ocr.GenerateSearchablePDFFromImage
    
    Cq-Depend: chromium:2354314,chromium:2354078,chromium:2337055
    Change-Id: I2746197998ec7fe4a5683cc92ae67ad1f26b4d3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2352851
    Tested-by: Efi Mavroudi <emavroudi@google.com>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/main.go
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -40,6 +40,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/logs"
 	_ "chromiumos/tast/local/bundles/cros/meta"
 	_ "chromiumos/tast/local/bundles/cros/network"
+	_ "chromiumos/tast/local/bundles/cros/ocr"
 	_ "chromiumos/tast/local/bundles/cros/platform"
 	_ "chromiumos/tast/local/bundles/cros/policy"
 	_ "chromiumos/tast/local/bundles/cros/power"
---
+++ src/chromiumos/tast/local/bundles/cros/ocr/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package ocr contains local Tast tests that exercise the Optical Character
+// Recognition Service for Chrome OS.
+package ocr
