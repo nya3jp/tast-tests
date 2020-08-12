567a907e3fa1df83159b70937a04bfa2380d05d0
Kevin Lin <kevinptt@chromium.org>
2020-08-12 17:50:58 +0800

    tast-tests: move label counting into independent function
    This CL moves repeated code in runtime_probe tast for counting
    cros-label arguments and verifying component existence into independent
    function.
    
    BUG=None
    TEST=tast run $DUT platform.CrosRuntimeProbe*
    
    Change-Id: Ibff7b96e50306f65be8459e513c3d3f7978349a3

--- src/chromiumos/tast/local/bundles/cros/platform/cros_runtime_probe_network.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_runtime_probe_network.go
@@ -6,17 +6,12 @@ package platform
 
 import (
 	"context"
-	"encoding/json"
 	"sort"
-	"strings"
 
 	rppb "chromiumos/system_api/runtime_probe_proto"
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/platform/runtimeprobe"
 	"chromiumos/tast/testing"
 )
-
-var networkTypes = []string{"wireless", "cellular", "ethernet"}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -32,58 +27,16 @@ func init() {
 	})
 }
 
-// networkNameMapping will extract the model and network names (prefixed with
-// "hwid_component:<network type>/") from autotest_host_info_labels var which
-// is a json string of list of cros-labels.  After collecting network names,
-// this function will return a map of set containing them by network type.
-// Since we need the model name for component group, here we return it as well.
-func networkNameMapping(jsonStr string) (map[string]map[string]struct{}, string, error) {
-	const modelPrefix = "model:"
-	mapping := make(map[string]map[string]struct{})
-	for _, networkType := range networkTypes {
-		mapping[networkType] = make(map[string]struct{})
-	}
-
-	var labels []string
-	if err := json.Unmarshal([]byte(jsonStr), &labels); err != nil {
-		return nil, "", err
-	}
-	// Find the model name of this DUT.
-	var model string
-	for _, label := range labels {
-		if strings.HasPrefix(label, modelPrefix) {
-			model = strings.TrimPrefix(label, modelPrefix)
-			break
-		}
-	}
-	if len(model) == 0 {
-		return nil, "", errors.New("no model found")
-	}
-
-	// Filter labels with prefix "hwid_component:<network type>/" and trim them.
-	for _, label := range labels {
-		for _, networkType := range networkTypes {
-			networkPrefix := "hwid_component:" + networkType + "/"
-			if strings.HasPrefix(label, networkPrefix) {
-				label := strings.TrimPrefix(label, networkPrefix)
-				mapping[networkType][model+"_"+label] = struct{}{}
-				break
-			}
-		}
-	}
-
-	return mapping, model, nil
-}
-
 // CrosRuntimeProbeNetwork checks if the network names in cros-label are
 // consistent with probed names from runtime_probe.
 func CrosRuntimeProbeNetwork(ctx context.Context, s *testing.State) {
+	var networkTypes = []string{"wireless", "cellular", "ethernet"}
 	labelsStr, ok := s.Var("autotest_host_info_labels")
 	if !ok {
 		s.Fatal("No network labels")
 	}
 
-	mapping, model, err := networkNameMapping(labelsStr)
+	mapping, model, err := runtimeprobe.GetComponentCount(labelsStr, networkTypes)
 	if err != nil {
 		s.Fatal("Unable to decode autotest_host_info_labels: ", err)
 	}
@@ -100,22 +53,15 @@ func CrosRuntimeProbeNetwork(ctx context.Context, s *testing.State) {
 	probedNetworkComponents := result.GetNetwork()
 
 	for _, component := range probedNetworkComponents {
-		name := component.GetName()
-		if info := component.GetInformation(); info != nil {
-			if compGroup := info.GetCompGroup(); compGroup != "" {
-				name = model + "_" + compGroup
-			}
-		}
 		values := component.GetValues()
 		networkType := values.GetType()
-		if name == "generic" {
-			s.Log("Skip known generic probe result")
-		} else {
-			s.Log("Probed network:", name)
-			if _, exists := mapping[networkType][name]; !exists {
-				s.Logf("Extra network component %v of type %v is probed", name, networkType)
+		result, name := runtimeprobe.DecreaseComponentCount(mapping[networkType], model, component)
+		s.Logf("Probed %s: %s", networkType, name)
+		if !result {
+			if name == "generic" {
+				s.Logf("Skip known generic %s probe result", networkType)
 			} else {
-				delete(mapping[networkType], name)
+				s.Logf("Extra network component %q of type %s is probed", name, networkType)
 			}
 		}
 	}
--- src/chromiumos/tast/local/bundles/cros/platform/cros_runtime_probe_storage.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_runtime_probe_storage.go
@@ -6,12 +6,9 @@ package platform
 
 import (
 	"context"
-	"encoding/json"
 	"sort"
-	"strings"
 
 	rppb "chromiumos/system_api/runtime_probe_proto"
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/platform/runtimeprobe"
 	"chromiumos/tast/testing"
 )
@@ -30,56 +27,21 @@ func init() {
 	})
 }
 
-// storageNames will extract the model and storage names (prefixed with
-// "hwid_component:storage/") from autotest_host_info_labels var which is a json string of list of cros-labels.
-// After collecting storage names, this function will return a set containing them.
-// Since we need the model name for component group, here we return it as well.
-func storageNames(jsonStr string) (map[string]struct{}, string, error) {
-	const (
-		storagePrefix = "hwid_component:storage/"
-		modelPrefix   = "model:"
-	)
-	var labels []string
-	if err := json.Unmarshal([]byte(jsonStr), &labels); err != nil {
-		return nil, "", err
-	}
-	// Filter labels with prefix "hwid_component:storage/" and trim them.
-	// Also find the model name of this DUT.
-	var names []string
-	var model string
-	for _, label := range labels {
-		if strings.HasPrefix(label, storagePrefix) {
-			label := strings.TrimPrefix(label, storagePrefix)
-			names = append(names, label)
-		} else if strings.HasPrefix(label, modelPrefix) {
-			model = strings.TrimPrefix(label, modelPrefix)
-		}
-	}
-	if len(model) == 0 {
-		return nil, "", errors.New("no model found")
-	}
-
-	set := make(map[string]struct{})
-	for _, label := range names {
-		set[model+"_"+label] = struct{}{}
-	}
-
-	return set, model, nil
-}
-
 // CrosRuntimeProbeStorage checks if the storage names in cros-label are consistent with probed names from runtime_probe
 func CrosRuntimeProbeStorage(ctx context.Context, s *testing.State) {
+	const category = "storage"
 	labelsStr, ok := s.Var("autotest_host_info_labels")
 	if !ok {
 		s.Fatal("No storage labels")
 	}
 
-	set, model, err := storageNames(labelsStr)
+	mapping, model, err := runtimeprobe.GetComponentCount(labelsStr, []string{category})
+	labels := mapping[category]
 	if err != nil {
 		s.Fatal("Unable to decode autotest_host_info_labels: ", err)
 	}
 
-	if len(set) == 0 {
+	if len(labels) == 0 {
 		s.Fatal("No storage labels")
 	}
 
@@ -95,25 +57,19 @@ func CrosRuntimeProbeStorage(ctx context.Context, s *testing.State) {
 	probedStorageComponents := result.GetStorage()
 
 	for _, component := range probedStorageComponents {
-		name := component.GetName()
-		if info := component.GetInformation(); info != nil {
-			if compGroup := info.GetCompGroup(); compGroup != "" {
-				name = model + "_" + compGroup
+		result, name := runtimeprobe.DecreaseComponentCount(labels, model, component)
+		s.Logf("Probed %s: %s", category, name)
+		if !result {
+			if name == "generic" {
+				s.Logf("Skip known generic %s probe result", category)
+			} else {
+				s.Fatalf("Unexpected %s %q is probed", category, name)
 			}
-		}
-		if name == "generic" {
-			s.Log("Skip known generic probe result")
-		} else {
-			s.Log("Probed storage:", name)
-			if _, exists := set[name]; !exists {
-				s.Fatalf("Unexpected storage %v is probed", name)
-			}
-			delete(set, name)
 		}
 	}
-	if len(set) > 0 {
-		unprobedStorages := make([]string, 0, len(set))
-		for k := range set {
+	if len(labels) > 0 {
+		unprobedStorages := make([]string, 0, len(labels))
+		for k := range labels {
 			unprobedStorages = append(unprobedStorages, k)
 		}
 		sort.Strings(unprobedStorages)
