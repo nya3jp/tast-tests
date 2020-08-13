a6520f7a7cdc5431f668acc6023f4aff20450e8a
alexanderhartl <alexanderhartl@google.com>
2020-08-13 12:39:54 +0000

    tast-tests: add RequiredClientCertificateForUser
    add policy.RequiredClientCertificateForUser tast test.
    
    BUG=None
    TEST=tast run <ip> policy.RequiredClientCertificateForUser
    and tast run <ip> policy.RequiredClientCertificateForDevice
    
    Change-Id: I3efe7f27ffc5966b6db928f14974ef95b1f10814

--- src/chromiumos/tast/services/cros/policy/client_certificate_service.pb.go
+++ src/chromiumos/tast/services/cros/policy/client_certificate_service.pb.go
@@ -25,22 +25,69 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
+type TestClientCertificateIsInstalledRequest struct {
+	Slot                 string   `protobuf:"bytes,1,opt,name=Slot,proto3" json:"Slot,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *TestClientCertificateIsInstalledRequest) Reset() {
+	*m = TestClientCertificateIsInstalledRequest{}
+}
+func (m *TestClientCertificateIsInstalledRequest) String() string { return proto.CompactTextString(m) }
+func (*TestClientCertificateIsInstalledRequest) ProtoMessage()    {}
+func (*TestClientCertificateIsInstalledRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_0d29485bc3caf52d, []int{0}
+}
+
+func (m *TestClientCertificateIsInstalledRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_TestClientCertificateIsInstalledRequest.Unmarshal(m, b)
+}
+func (m *TestClientCertificateIsInstalledRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_TestClientCertificateIsInstalledRequest.Marshal(b, m, deterministic)
+}
+func (m *TestClientCertificateIsInstalledRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_TestClientCertificateIsInstalledRequest.Merge(m, src)
+}
+func (m *TestClientCertificateIsInstalledRequest) XXX_Size() int {
+	return xxx_messageInfo_TestClientCertificateIsInstalledRequest.Size(m)
+}
+func (m *TestClientCertificateIsInstalledRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_TestClientCertificateIsInstalledRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_TestClientCertificateIsInstalledRequest proto.InternalMessageInfo
+
+func (m *TestClientCertificateIsInstalledRequest) GetSlot() string {
+	if m != nil {
+		return m.Slot
+	}
+	return ""
+}
+
+func init() {
+	proto.RegisterType((*TestClientCertificateIsInstalledRequest)(nil), "tast.cros.policy.TestClientCertificateIsInstalledRequest")
+}
+
 func init() { proto.RegisterFile("client_certificate_service.proto", fileDescriptor_0d29485bc3caf52d) }
 
 var fileDescriptor_0d29485bc3caf52d = []byte{
-	// 180 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0xcf, 0xb1, 0x0a, 0xc2, 0x40,
-	0x0c, 0x06, 0x60, 0x27, 0x87, 0x4e, 0xd2, 0x41, 0xa4, 0x2e, 0x45, 0xc4, 0xf1, 0x0e, 0xf4, 0x0d,
-	0x2c, 0x0e, 0x9d, 0xed, 0xe4, 0x52, 0xda, 0x98, 0xd6, 0x83, 0x6b, 0x73, 0x5c, 0x52, 0xa1, 0x6f,
-	0x2f, 0xed, 0x29, 0x0e, 0xe2, 0x9a, 0x3f, 0xf9, 0xf8, 0x13, 0xa5, 0x60, 0x0d, 0xf6, 0x52, 0x02,
-	0x7a, 0x31, 0x8d, 0x81, 0x4a, 0xb0, 0x64, 0xf4, 0x4f, 0x03, 0xa8, 0x9c, 0x27, 0xa1, 0x78, 0x25,
-	0x15, 0x8b, 0x02, 0x4f, 0xac, 0x1c, 0x59, 0x03, 0x63, 0xb2, 0x6d, 0x89, 0x5a, 0x8b, 0x7a, 0xce,
-	0xeb, 0xa1, 0xd1, 0xd8, 0x39, 0x19, 0xc3, 0xfa, 0xd1, 0x45, 0x9b, 0x6c, 0x26, 0xb3, 0xaf, 0x78,
-	0x0d, 0x60, 0x5c, 0x44, 0x69, 0x81, 0x2c, 0x3f, 0x79, 0xce, 0x79, 0xcf, 0x52, 0x59, 0x8b, 0xf7,
-	0x78, 0xad, 0x82, 0xae, 0x3e, 0xba, 0xba, 0x4c, 0x7a, 0xf2, 0x67, 0xbe, 0x5b, 0x9c, 0x0f, 0xb7,
-	0x3d, 0x3c, 0x3c, 0x75, 0x66, 0xe8, 0x88, 0xf5, 0xd4, 0x56, 0xbf, 0x3f, 0x60, 0x3d, 0xd5, 0xd6,
-	0xa1, 0x76, 0xbd, 0x9c, 0x2f, 0x4f, 0xaf, 0x00, 0x00, 0x00, 0xff, 0xff, 0xa4, 0xb2, 0x00, 0x8f,
-	0xf3, 0x00, 0x00, 0x00,
+	// 220 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x48, 0xce, 0xc9, 0x4c,
+	0xcd, 0x2b, 0x89, 0x4f, 0x4e, 0x2d, 0x2a, 0xc9, 0x4c, 0xcb, 0x4c, 0x4e, 0x2c, 0x49, 0x8d, 0x2f,
+	0x4e, 0x2d, 0x2a, 0xcb, 0x4c, 0x4e, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x28, 0x49,
+	0x2c, 0x2e, 0xd1, 0x4b, 0x2e, 0xca, 0x2f, 0xd6, 0x2b, 0xc8, 0xcf, 0xc9, 0x4c, 0xae, 0x94, 0x92,
+	0x4e, 0xcf, 0xcf, 0x4f, 0xcf, 0x49, 0xd5, 0x07, 0xcb, 0x27, 0x95, 0xa6, 0xe9, 0xa7, 0xe6, 0x16,
+	0x94, 0x54, 0x42, 0x94, 0x2b, 0xd9, 0x72, 0xa9, 0x87, 0xa4, 0x16, 0x97, 0x38, 0x83, 0x8d, 0x75,
+	0x46, 0x98, 0xea, 0x59, 0xec, 0x99, 0x57, 0x5c, 0x92, 0x98, 0x93, 0x93, 0x9a, 0x12, 0x94, 0x5a,
+	0x58, 0x9a, 0x5a, 0x5c, 0x22, 0x24, 0xc4, 0xc5, 0x12, 0x9c, 0x93, 0x5f, 0x22, 0xc1, 0xa8, 0xc0,
+	0xa8, 0xc1, 0x19, 0x04, 0x66, 0x1b, 0x4d, 0x66, 0xe4, 0x92, 0xc0, 0xd0, 0x1b, 0x0c, 0x71, 0x90,
+	0x50, 0x39, 0x97, 0x02, 0x21, 0xb3, 0x85, 0x2c, 0xf5, 0xd0, 0xdd, 0xab, 0x47, 0xa4, 0x7b, 0xa4,
+	0xc4, 0xf4, 0x20, 0x1e, 0xd3, 0x83, 0x79, 0x4c, 0xcf, 0x15, 0xe4, 0x31, 0x25, 0x06, 0x27, 0xb5,
+	0x28, 0x95, 0xe4, 0x8c, 0xa2, 0xfc, 0xdc, 0xcc, 0xd2, 0xdc, 0xfc, 0x62, 0x7d, 0x90, 0x05, 0xfa,
+	0xd0, 0x40, 0x2a, 0xd6, 0x07, 0xd9, 0xa4, 0x0f, 0xb1, 0x29, 0x89, 0x0d, 0xac, 0xd3, 0x18, 0x10,
+	0x00, 0x00, 0xff, 0xff, 0xa5, 0x54, 0xae, 0x1e, 0x56, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -55,7 +102,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ClientCertificateServiceClient interface {
-	TestClientCertificateIsInstalled(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*empty.Empty, error)
+	TestClientCertificateIsInstalled(ctx context.Context, in *TestClientCertificateIsInstalledRequest, opts ...grpc.CallOption) (*empty.Empty, error)
 }
 
 type clientCertificateServiceClient struct {
@@ -66,7 +113,7 @@ func NewClientCertificateServiceClient(cc *grpc.ClientConn) ClientCertificateSer
 	return &clientCertificateServiceClient{cc}
 }
 
-func (c *clientCertificateServiceClient) TestClientCertificateIsInstalled(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*empty.Empty, error) {
+func (c *clientCertificateServiceClient) TestClientCertificateIsInstalled(ctx context.Context, in *TestClientCertificateIsInstalledRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
 	out := new(empty.Empty)
 	err := c.cc.Invoke(ctx, "/tast.cros.policy.ClientCertificateService/TestClientCertificateIsInstalled", in, out, opts...)
 	if err != nil {
@@ -77,14 +124,14 @@ func (c *clientCertificateServiceClient) TestClientCertificateIsInstalled(ctx co
 
 // ClientCertificateServiceServer is the server API for ClientCertificateService service.
 type ClientCertificateServiceServer interface {
-	TestClientCertificateIsInstalled(context.Context, *empty.Empty) (*empty.Empty, error)
+	TestClientCertificateIsInstalled(context.Context, *TestClientCertificateIsInstalledRequest) (*empty.Empty, error)
 }
 
 // UnimplementedClientCertificateServiceServer can be embedded to have forward compatible implementations.
 type UnimplementedClientCertificateServiceServer struct {
 }
 
-func (*UnimplementedClientCertificateServiceServer) TestClientCertificateIsInstalled(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+func (*UnimplementedClientCertificateServiceServer) TestClientCertificateIsInstalled(ctx context.Context, req *TestClientCertificateIsInstalledRequest) (*empty.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method TestClientCertificateIsInstalled not implemented")
 }
 
@@ -93,7 +140,7 @@ func RegisterClientCertificateServiceServer(s *grpc.Server, srv ClientCertificat
 }
 
 func _ClientCertificateService_TestClientCertificateIsInstalled_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(empty.Empty)
+	in := new(TestClientCertificateIsInstalledRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -105,7 +152,7 @@ func _ClientCertificateService_TestClientCertificateIsInstalled_Handler(srv inte
 		FullMethod: "/tast.cros.policy.ClientCertificateService/TestClientCertificateIsInstalled",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ClientCertificateServiceServer).TestClientCertificateIsInstalled(ctx, req.(*empty.Empty))
+		return srv.(ClientCertificateServiceServer).TestClientCertificateIsInstalled(ctx, req.(*TestClientCertificateIsInstalledRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
