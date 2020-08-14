ecfc349aa7c84cd12599818f2c04f70cb7d614ff
Sara Kato <sarakato@google.com>
2020-08-14 09:10:46 +0900

    tast-tests: Stop arc.PerAppDensity
    This test is failing on nearly 100% of its runs.  Stop it from running
    until it can be fixed.
    
    BUG=b:150909711
    TEST=none
    
    Change-Id: I94c848fd4fbdcad23da0751924317fe195bf0cf6

--- src/chromiumos/tast/local/bundles/cros/arc/per_app_density.go
+++ src/chromiumos/tast/local/bundles/cros/arc/per_app_density.go
@@ -17,10 +17,10 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         PerAppDensity,
-		Desc:         "Checks that density can be changed with Android applications",
-		Contacts:     []string{"sarakato@chromium.org", "arc-eng@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Func:     PerAppDensity,
+		Desc:     "Checks that density can be changed with Android applications",
+		Contacts: []string{"sarakato@chromium.org", "arc-eng@google.com"},
+		// TODO(b/150909711): Enable this test after fix.
 		SoftwareDeps: []string{"android_p", "chrome"},
 		Timeout:      4 * time.Minute,
 		Pre:          arc.Booted(),
