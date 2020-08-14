5e533995381247016cf7d865b9e3a6bdd3c29e2b
Kyle Shimabukuro <kyleshima@chromium.org>
2020-08-13 19:32:52 -0700

    tast-tests: Automate Quick Settings lock screen tests
    Automate 2 of the quick settings test cases related to lock screen behavior:
    - https://testtracker.googleplex.com/testplans/testcase/detail/2827661?id=21607&revision=78
    - https://testtracker.googleplex.com/testplans/testcase/detail/2827660?id=21607&revision=78
    
    BUG=chromium:1116629
    BUG=chromium:1116630
    TEST=tast run <ip> ui.QuickSettingsLockScreen
    tast run <ip> ui.QuickSettingsLockScreen.no_battery on a chromebox and
    vm
    
    Change-Id: Ib4e69310c3e27a92f25723fa7e0dd3cf7f2bdfdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2357406
    Tested-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

---
+++ src/chromiumos/tast/local/chrome/ui/quicksettings/constants.go
@@ -0,0 +1,85 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package quicksettings
+
+import "chromiumos/tast/local/chrome/ui"
+
+// quickSettingsParams are the parameters to find the Quick Settings area in the UI.
+var quickSettingsParams ui.FindParams = ui.FindParams{
+	ClassName: "BubbleFrameView",
+}
+
+// CollapseBtnParams are the UI params for the collapse button, which collapses and expands Quick Settings.
+var CollapseBtnParams ui.FindParams = ui.FindParams{
+	Role:      ui.RoleTypeButton,
+	ClassName: "CollapseButton",
+}
+
+// LockBtnParams are the UI params for Quick Settings' lock button.
+var LockBtnParams ui.FindParams = ui.FindParams{
+	Name:      "Lock",
+	ClassName: "TopShortcutButton",
+}
+
+// SettingsBtnParams are the UI params for the Quick Settings' setting button.
+var SettingsBtnParams ui.FindParams = ui.FindParams{
+	Name:      "Settings",
+	ClassName: "TopShortcutButton",
+}
+
+// ShutdownBtnParams are the UI params for the shutdown button in Quick Settings.
+var ShutdownBtnParams ui.FindParams = ui.FindParams{
+	Name:      "Shut down",
+	ClassName: "TopShortcutButton",
+}
+
+// SignoutBtnParams are the UI params for the 'Sign out' Quick Settings button.
+var SignoutBtnParams ui.FindParams = ui.FindParams{
+	Role:      ui.RoleTypeButton,
+	Name:      "Sign out",
+	ClassName: "SignOutButton",
+}
+
+// BrightnessSliderParams are the UI params for the Quick Settings brightness slider.
+var BrightnessSliderParams ui.FindParams = ui.FindParams{
+	Name:      "Brightness",
+	ClassName: "Slider",
+	Role:      ui.RoleTypeSlider,
+}
+
+// VolumeSliderParams are the UI params for the Quick Settings volume slider.
+var VolumeSliderParams ui.FindParams = ui.FindParams{
+	Name:      "Volume",
+	ClassName: "Slider",
+	Role:      ui.RoleTypeSlider,
+}
+
+// BatteryViewParams are the UI params for the Quick Settings date/time display.
+var BatteryViewParams ui.FindParams = ui.FindParams{
+	Role:      ui.RoleTypeLabelText,
+	ClassName: "BatteryView",
+}
+
+// DateViewParams are the UI params for the Quick Settings date/time display.
+var DateViewParams ui.FindParams = ui.FindParams{
+	Role:      ui.RoleTypeButton,
+	ClassName: "DateView",
+}
+
+// SettingPod represents the name of a setting pod in Quick Settings.
+// These names are contained in the Name attribute of the automation node
+// for the corresponding pod icon button, so they can be used to find the
+// buttons in the UI.
+type SettingPod string
+
+// List of quick setting names, derived from the corresponding pod icon button node names.
+// Character case in the names should exactly match the pod icon button node Name attribute.
+const (
+	SettingPodAccessibility SettingPod = "accessibility"
+	SettingPodBluetooth     SettingPod = "Bluetooth"
+	SettingPodDoNotDisturb  SettingPod = "Do not disturb"
+	SettingPodNetwork       SettingPod = "network"
+	SettingPodNightLight    SettingPod = "Night Light"
+)
