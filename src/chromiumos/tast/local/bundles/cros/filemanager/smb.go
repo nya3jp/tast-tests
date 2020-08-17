876cdbe35caf6a45533bbdc4b04fb9cf50d2e589
Ben Reich <benreich@chromium.org>
2020-08-17 23:54:12 +0000

    tast-tests: Remove samba install for SMB test
    Samba is now default installed in the Crostini
    test image, so removing the steps that install
    it. Have also removed the steps to set up a
    user for password protected shares as we do not
    test that in this test.
    
    BUG=chromium:1084323
    TEST=tast run <DUT-IP> filemanager.SMB
    
    Change-Id: Iaa17d8b93de6894d2ce7594f415a969de11fa3b4

--- src/chromiumos/tast/local/bundles/cros/filemanager/smb.go
+++ src/chromiumos/tast/local/bundles/cros/filemanager/smb.go
@@ -127,18 +127,12 @@ func SMB(ctx context.Context, s *testing.State) {
 
 func setupSambaShare(ctx context.Context, cont *vm.Container) error {
 	setupCommands := []string{
-		"echo 'samba-common samba-common/workgroup string WORKGROUP' | sudo debconf-set-selections",
-		"echo 'samba-common samba-common/dhcp boolean false' | sudo debconf-set-selections",
-		"echo 'samba-common samba-common/do_debconf boolean false' | sudo debconf-set-selections",
-		"sudo apt -y install samba",
 		"sudo mkdir -p /pub/guestshare",
 		"echo 'test file contents' | sudo tee /pub/guestshare/test.txt",
 		"sudo chown -R nobody:nogroup /pub/guestshare",
 		"sudo chmod 0770 /pub/guestshare",
-		"sudo useradd -d /home/smbuser -m smbuser",
-		"echo 'password' > sudo smbpasswd -s -a smbuser",
 		"sudo cp -f /tmp/smb.conf /etc/samba/",
-		"sudo service smbd start",
+		"sudo service smbd restart",
 	}
 
 	for _, cmd := range setupCommands {
