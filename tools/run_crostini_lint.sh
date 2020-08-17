3d9288241a7a0685a8b949d9324317b6e9082885
David Munro <davidmunro@google.com>
2020-08-17 10:11:03 +1000

    tast-tests: Add a Crostini PRESUBMIT check
    So far we check that the Crostini post-submit hooks are correctly
    called, we skip any non-Crostini files.
    For non-Crostini tests we only run a single git command which on my
    desktop takes ~10ms.
    
    BUG=None
    TEST=Manual
    
    Change-Id: I2a34575b8a218d850d2a6fe68d4d50aa6ff9504a

---
+++ tools/run_crostini_lint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+failed=0
+files=$(git diff-tree --no-commit-id --name-only -r "$1" \
+    src/chromiumos/tast/local/bundles/cros/crostini/)
+for file in $files; do
+    if ! grep 'Pre:\s*crostini\.' "$file" &>/dev/null; then
+        # Doesn't look like it uses a Crostini precondition, ignore
+        continue
+    fi
+    if ! grep 'defer crostini.RunCrostiniPostTest' "$file" &>/dev/null; then
+        echo "$file: Missing call to Crostini post-test hooks"
+        echo -n "Try adding e.g. defer crostini.RunCrostiniPostTest(ctx, cont)"
+        echo -n " at the start of your test."
+        failed=1
+    fi
+done
+exit $failed
