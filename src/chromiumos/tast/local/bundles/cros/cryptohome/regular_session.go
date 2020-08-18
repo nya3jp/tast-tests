c880a3ffe56dd70b9b00e9cb05877136de34eaa9
Betul Soysal <betuls@google.com>
2020-08-18 08:18:22 -0700

    tast-tests: Add cryptohome.RegularSession
    We'll be changing how regular user sessions are mounted,
    so add a test that we can use during development. The changes in the
    regular session mount/unmount will be activated behind a compile time
    flag with the below mentioned CL. This test will help during the work
    to remove the flag. .
    
    BUG=chromium:1074735
    TEST=Build and deploy cryptohome with and without USE='mount_oop' with
    https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2321614
    TEST=tast run cryptohome.RegularSession passes.
    
    Change-Id: I604fcdd4d94de1c84122a26c854a1f1fcec09aac

---
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/regular_session.go
@@ -0,0 +1,44 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"context"
+
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: RegularSession,
+		Desc: "Ensures that cryptohome correctly mounts and unmounts regular user sessions",
+		Contacts: []string{
+			"betuls@chromium.org",
+			"jorgelo@chromium.org",
+			"chromeos-security@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func RegularSession(ctx context.Context, s *testing.State) {
+	const (
+		testUser = "cryptohome_test@chromium.org"
+		testPass = "testme"
+	)
+	// Unmount all user vaults before we start.
+	if err := cryptohome.UnmountAll(ctx); err != nil {
+		s.Log("Failed to unmount all before test starts: ", err)
+	}
+	// Mount user cryptohome for test user.
+	if err := cryptohome.CreateVault(ctx, testUser, testPass); err != nil {
+		s.Fatal("Failed to mount user vault: ", err)
+	}
+	// Unmount user vault directory and daemon-store directories.
+	if err := cryptohome.UnmountVault(ctx, testUser); err != nil {
+		s.Error("Failed to unmount user vault: ", err)
+	}
+}
