d7f1f29700abd625194a6c792c27e62f91b4c911
Miguel Casas <mcasas@chromium.org>
2020-08-18 14:10:02 -0400

    tast-tests/webrtc: rename MediaRecorder tests
    MediaRecorder and MediaRecorderAccelerator are incorrectly
    named: the former should be MediaRecorderAPI and the latter
    should take over as simply MediaRecorder, to be homogeneous
    with the other video.* and webrtc.* test cases.
    
    Renaming is somewhat traumatic because it prevent bisects
    and makes test monitoring less effective, so I waited until
    there's long stable streak, and we're about to branch, to
    post this CL.
    
    I took the chance to reduce the frequency of some of these
    tests to graphics_nightly ISO graphics_perbuild, since they
    are not so critical.
    
    BUG=chromium:1102417
    TEST=tast run DUT webrtc.MediaRecorder.* webrtc.MediaRecoderAPI
    on kohaku
    
    Change-Id: Ifaa108ffcc58c641da3fc26bef22f49790f2aa05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2363177
    Tested-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Po-Hsien Wang <pwang@chromium.org>
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/webrtc/media_recorder_api.go
@@ -0,0 +1,92 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package webrtc
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: MediaRecorderAPI,
+		Desc: "Verifies the MediaRecorder API",
+		Contacts: []string{
+			"mcasas@chromium.org",
+			"chromeos-gfx-video@google.com",
+		},
+		Attr: []string{"group:graphics", "graphics_video", "graphics_nightly"},
+		// "chrome_internal" is needed because H.264 is a proprietary codec.
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		Data:         []string{"media_recorder.html", "media_recorder.js"},
+		Pre:          pre.ChromeVideoWithFakeWebcam(),
+		Timeout:      3 * time.Minute,
+	})
+}
+
+// MediaRecorderAPI verifies the MediaRecorder API, e.g. functions such as
+// start, stop, pause, resume. The test fails if the media recorder cannot
+// exercise these basic functions.
+func MediaRecorderAPI(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	conn, err := cr.NewConn(ctx, server.URL+"/media_recorder.html")
+	if err != nil {
+		s.Error(err, "failed to open recorder page")
+	}
+	defer conn.Close()
+	defer conn.CloseTarget(ctx)
+
+	runTest := func(js string) error {
+		s.Logf("Running %s", js)
+		if err := conn.EvalPromise(ctx, js, nil); err != nil {
+			return errors.Wrap(err, "failed to evaluate test function")
+		}
+		return nil
+	}
+
+	for _, js := range []string{
+		// Test start and stop.
+		"testStartAndRecorderState()",
+		"testStartStopAndRecorderState()",
+		"testStartAndDataAvailable('video/webm; codecs=h264')",
+		"testStartAndDataAvailable('video/webm; codecs=vp9')",
+		"testStartAndDataAvailable('video/webm; codecs=vp8')",
+		"testStartWithTimeSlice()",
+
+		// Test resume and pause.
+		"testResumeAndRecorderState()",
+		"testResumeAndDataAvailable()",
+		"testPauseAndRecorderState()",
+		"testPauseStopAndRecorderState()",
+		"testPausePreventsDataavailableFromBeingFired()",
+
+		// Test illegal operations handling.
+		"testIllegalResumeThrowsDOMError()",
+		"testIllegalPauseThrowsDOMError()",
+		"testIllegalStopThrowsDOMError()",
+		"testIllegalStartInRecordingStateThrowsDOMError()",
+		"testIllegalStartInPausedStateThrowsDOMError()",
+		"testIllegalRequestDataThrowsDOMError()",
+
+		"testTwoChannelAudio()",
+		"testAddingTrackToMediaStreamFiresErrorEvent()",
+		"testRemovingTrackFromMediaStreamFiresErrorEvent()",
+	} {
+		if err := runTest(js); err != nil {
+			s.Errorf("%v failed: %v", js, err)
+		}
+	}
+}
