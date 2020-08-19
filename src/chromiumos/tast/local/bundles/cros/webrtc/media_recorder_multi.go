4462eef63695283be56d4adc10d8287ac7283379
Anand K Mistry <amistry@chromium.org>
2020-08-19 15:44:07 +1000

    tast-tests/webrtc: Add test for parallel hardware encoding
    It is possible for users to run apps/sites such that multiple encoders
    are active at the same time (ie. being on two video calls at the same
    time). Test that this is possible and does not cause an error.
    
    BUG=b:159371656
    TEST=tast run <DUT> 'webrtc.MediaRecorderMulti.*'
    
    Change-Id: I8b990d7a68245b68b379747be210b388ed9769f2

---
+++ src/chromiumos/tast/local/bundles/cros/webrtc/media_recorder_multi.go
@@ -0,0 +1,105 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package webrtc
+
+import (
+	"context"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"chromiumos/tast/local/bundles/cros/webrtc/mediarecorder"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/constants"
+	"chromiumos/tast/local/media/pre"
+	"chromiumos/tast/local/media/videotype"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: MediaRecorderMulti,
+		Desc: "Verifies that MediaRecorder can use multiple encoders in parallel",
+		Contacts: []string{
+			"mcasas@chromium.org",
+			"chromeos-gfx-video@google.com",
+			"chromeos-video-eng@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Data:         []string{"loopback_media_recorder.html"},
+		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
+		Params: []testing.Param{{
+			Name:              "vp8_vp8",
+			Val:               []videotype.Codec{videotype.VP8, videotype.VP8},
+			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
+			Pre:               pre.ChromeVideoWithFakeWebcam(),
+		}, {
+			Name: "vp8_h264",
+			Val:  []videotype.Codec{videotype.H264, videotype.VP8},
+			// "chrome_internal" is needed because H.264 is a proprietary codec.
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264, caps.HWEncodeVP8, "chrome_internal"},
+			Pre:               pre.ChromeVideoWithFakeWebcam(),
+		}, {
+			Name: "h264_h264",
+			Val:  []videotype.Codec{videotype.H264, videotype.H264},
+			// "chrome_internal" is needed because H.264 is a proprietary codec.
+			ExtraSoftwareDeps: []string{caps.HWEncodeH264, "chrome_internal"},
+			Pre:               pre.ChromeVideoWithFakeWebcam(),
+		}},
+	})
+}
+
+// MediaRecorderMulti verifies that multiple video encoders can be used
+// in parallel.
+func MediaRecorderMulti(ctx context.Context, s *testing.State) {
+	const (
+		// Record for several seconds to ensure the encodings are overlapping.
+		// Also, any interaction between the two encoders may take several
+		// seconds to occur.
+		recordDuration = 10 * time.Second
+	)
+
+	tconn, err := s.PreValue().(*chrome.Chrome).TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	initHistogram, err := metrics.GetHistogram(ctx, tconn, constants.MediaRecorderVEAUsed)
+	if err != nil {
+		s.Fatal("Failed to get initial histogram: ", err)
+	}
+
+	g, encCtx := errgroup.WithContext(ctx)
+	for _, codec := range s.Param().([]videotype.Codec) {
+		c := codec
+		g.Go(func() error {
+			s.Log("Running codec ", c)
+			return mediarecorder.VerifyMediaRecorderUsesEncodeAccelerator(
+				encCtx, s.PreValue().(*chrome.Chrome), s.DataFileSystem(), c, recordDuration)
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		s.Error("Failed to run VerifyMediaRecorderUsesEncodeAccelerator: ", err)
+	}
+
+	// Ensure that at least len(s.Param().([]videotype.Codec)) HW encoders have been used.
+	histogramDiff, err := metrics.WaitForHistogramUpdate(ctx, tconn, constants.MediaRecorderVEAUsed, initHistogram, time.Second)
+	if err != nil {
+		s.Fatal("Failed to wait for histogram update: ", err)
+	}
+	if len(histogramDiff.Buckets) > 1 {
+		s.Error("Unexpected histogram update: ", histogramDiff)
+	}
+	diff := histogramDiff.Buckets[0]
+	if diff.Min != constants.MediaRecorderVEAUsedSuccess || diff.Max != constants.MediaRecorderVEAUsedSuccess+1 {
+		s.Error("Unexpected bucket range: ", diff)
+	} else if diff.Count < int64(len(s.Param().([]videotype.Codec))) {
+		s.Errorf("Insufficient histogram updates, expected at least: %d, actual: %d",
+			len(s.Param().([]videotype.Codec)), diff.Count)
+	}
+}
