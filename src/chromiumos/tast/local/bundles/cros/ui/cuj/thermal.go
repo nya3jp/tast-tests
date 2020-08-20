adaedc9e8ad5e0a606a595c5ba610e1aee3cae12
Yichen Zhou <yichenz@chromium.org>
2020-08-20 05:09:40 +0000

    tast-tests: Add thermal throttling status to thermal subscore
    Computes thermal throttling status based on the assumption that cpu is
    thermal-throttled when cpu's temperature is higher than a fixed value.
    Stores it as "TPS.Thermal.ThrottlingStatus" metric for cuj tests.
    
    BUG=chromium:1113426
    TEST=tast run IP ui.'*CUJ*'
    Change-Id: I954e9a3166b1ac3b47b52f081d859763d0ae7a8b

--- src/chromiumos/tast/local/bundles/cros/ui/cuj/thermal.go
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/thermal.go
@@ -18,13 +18,17 @@ import (
 	"chromiumos/tast/testing"
 )
 
+// CPU's temperature > 75C is considered to be thermal throttling.
+const throttlingTemp = 75
+
 type thermalMetric struct {
 	paths  []string
 	metric perf.Metric
 }
 
 type thermalDataSource struct {
-	metrics map[string]*thermalMetric
+	metrics          map[string]*thermalMetric
+	throttlingStatus perf.Metric
 }
 
 func newThermalDataSource(ctx context.Context) *thermalDataSource {
@@ -78,6 +82,14 @@ func (ds *thermalDataSource) Setup(ctx context.Context, prefix string) error {
 		}
 		metric.paths = append(metric.paths, filepath.Join(path, "temp"))
 	}
+
+	// Metrics for CPU's thermal throttling status.
+	ds.throttlingStatus = perf.Metric{
+		Name:      prefix + "Thermal." + "ThrottlingStatus",
+		Unit:      "Boolean",
+		Direction: perf.SmallerIsBetter,
+		Multiple:  true}
+
 	return nil
 }
 
@@ -101,7 +113,15 @@ func (ds *thermalDataSource) Snapshot(ctx context.Context, values *perf.Values) 
 			}
 			sum += temp
 		}
-		values.Append(metric.metric, sum/1000/float64(len(metric.paths)))
+		temp := sum / 1000 / float64(len(metric.paths))
+		var throttled float64
+		if temp > throttlingTemp {
+			throttled = 1
+		} else {
+			throttled = 0
+		}
+		values.Append(metric.metric, temp)
+		values.Append(ds.throttlingStatus, throttled)
 	}
 	return nil
 }
