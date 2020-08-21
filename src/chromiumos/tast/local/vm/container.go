20efdb618698967477bb7298c0abdc55035e4039
Fergus Dall <sidereal@google.com>
2020-08-21 15:05:58 +1000

    crostini: Rename references to container arch
    These variables actually represent the debian version of the
    container, and this will be confusing when we start needing to
    represent the actual container architecture.
    
    BUG=chromium:953544
    TEST=Build tast tests
    
    Change-Id: I1c48f2d6a9e0dc63e8d1bfe215911f3ddc81c905

--- src/chromiumos/tast/local/vm/container.go
+++ src/chromiumos/tast/local/vm/container.go
@@ -50,22 +50,22 @@ const (
 	Tarball
 )
 
-// ContainerArchType represents the architecture+version of the container's image.
-type ContainerArchType int
+// ContainerDebianVersion represents the OS version of the container's image.
+type ContainerDebianVersion string
 
 const (
 	// DebianStretch refers to the "stretch" distribution of debian (a.k.a. debian 9).
-	DebianStretch ContainerArchType = iota
+	DebianStretch ContainerDebianVersion = "stretch"
 	// DebianBuster refers to the "buster" distribution of debian (a.k.a. debian 10).
-	DebianBuster
+	DebianBuster ContainerDebianVersion = "buster"
 )
 
 // ContainerType defines the type of container.
 type ContainerType struct {
 	// Image is the image source for this container.
 	Image ContainerImageType
-	// Arch is the architecture that the image has.
-	Arch ContainerArchType
+	// DebianVersion is the version of debian that the image has.
+	DebianVersion ContainerDebianVersion
 }
 
 // Container encapsulates a container running in a VM.
@@ -135,7 +135,7 @@ func (c *Container) Connect(ctx context.Context, user string) error {
 // ArchitectureAlias returns the alias subpath of the chosen container
 // architecture, i.e. part of the path used to compute the container's
 // gsutil URL.
-func ArchitectureAlias(t ContainerArchType) string {
+func ArchitectureAlias(t ContainerDebianVersion) string {
 	switch t {
 	case DebianStretch:
 		return "debian/stretch/test"
@@ -156,10 +156,10 @@ func (c *Container) Create(ctx context.Context, t ContainerType) error {
 	switch t.Image {
 	case LiveImageServer:
 		req.ImageServer = liveContainerImageServerFormat
-		req.ImageAlias = ArchitectureAlias(t.Arch)
+		req.ImageAlias = ArchitectureAlias(t.DebianVersion)
 	case StagingImageServer:
 		req.ImageServer = stagingContainerImageServerFormat
-		req.ImageAlias = ArchitectureAlias(t.Arch)
+		req.ImageAlias = ArchitectureAlias(t.DebianVersion)
 	case Tarball:
 		req.RootfsPath = tarballRootfsPath
 		req.MetadataPath = tarballMetadataPath
