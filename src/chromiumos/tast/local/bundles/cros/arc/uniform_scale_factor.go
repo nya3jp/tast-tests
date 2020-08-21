87bbc61c969e405cc7949541b7ff8987dd2df63e
Sara Kato <sarakato@google.com>
2020-08-21 15:13:22 +0900

    tast-tests: Add USF test for non-maxized (normal) state.
    Ensure that USF (uniform scale factor), is behaving correctly for
    non-maximized ARC++ applications.
    The application is first launched, to obtain a baseline for the number
    of black pixels. This value is then used to verify the state of the
    launched application, after enabling USF.
    
    This CL also does some refactoring, to clean up common logic among USF
    tests.
    
    BUG=b:163656122, b:164507920
    TEST=tast run DUT_IP arc.UniformScaleFactorNonMaximized
    
    Change-Id: I03f542413a12df6c0b35c973d0ae9c436e474d2e

--- src/chromiumos/tast/local/bundles/cros/arc/uniform_scale_factor.go
+++ src/chromiumos/tast/local/bundles/cros/arc/uniform_scale_factor.go
@@ -8,12 +8,8 @@ import (
 	"context"
 	"time"
 
-	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/bundles/cros/arc/perappdensity"
-	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/ash"
-	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
@@ -31,27 +27,19 @@ func init() {
 
 func UniformScaleFactor(ctx context.Context, s *testing.State) {
 	const (
-		cleanupTime              = 10 * time.Second // time reserved for cleanup.
-		squareSidePx             = 100
-		uniformScaleFactor       = 1.25
-		perAppDensityPackageName = "org.chromium.arc.testapp.perappdensitytest"
-
-		uniformScaleFactorSetting = "persist.sys.ui.uniform_app_scaling"
+		cleanupTime  = 10 * time.Second // time reserved for cleanup.
+		squareSidePx = 100
 	)
 
 	cr := s.PreValue().(arc.PreData).Chrome
 	a := s.PreValue().(arc.PreData).ARC
-
-	if err := arc.BootstrapCommand(ctx, perappdensity.Setprop, uniformScaleFactorSetting, "1").Run(testexec.DumpLogOnError); err != nil {
-		s.Fatal("Failed to set developer option: ", err)
-	}
 
 	dd, err := perappdensity.MeasureDisplayDensity(ctx, a)
 	if err != nil {
 		s.Fatal("Error obtaining initial display density: ", err)
 	}
 
-	expectedPixelCount := (int)((dd * squareSidePx * uniformScaleFactor) * (dd * squareSidePx * uniformScaleFactor))
+	expectedPixelCount := (int)((dd * squareSidePx) * (dd * squareSidePx))
 
 	if err := perappdensity.SetUpApk(ctx, a, perappdensity.DensityApk); err != nil {
 		s.Fatal("Failed to set up perAppDensityApk: ", err)
@@ -62,44 +50,7 @@ func UniformScaleFactor(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to create Test API connection: ", err)
 	}
 
-	act, err := arc.NewActivity(a, perAppDensityPackageName, ".ViewActivity")
-	if err != nil {
-		s.Fatal("Failed to create new activity: ", err)
-	}
-	defer act.Close()
-
-	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
-	if err != nil {
-		s.Fatal("Failed to set tablet mode to false: ", err)
-	}
-	defer func(ctx context.Context) {
-		cleanup(ctx)
-	}(ctx)
-	ctx, cancel := ctxutil.Shorten(ctx, cleanupTime)
-	defer cancel()
-
-	if err := act.Start(ctx, tconn); err != nil {
-		s.Fatal("Failed to start the activity: ", err)
-	}
-	defer func(ctx context.Context, tconn *chrome.TestConn) {
-		act.Stop(ctx, tconn)
-	}(ctx, tconn)
-	ctx, cancel = ctxutil.Shorten(ctx, cleanupTime)
-	defer cancel()
-
-	if err := ash.WaitForVisible(ctx, tconn, perAppDensityPackageName); err != nil {
-		s.Fatal("Failed to wait for visible app: ", err)
-	}
-
-	if err := act.SetWindowState(ctx, tconn, arc.WindowStateFullscreen); err != nil {
-		s.Fatal("Failed to set window state to fullscreen: ", err)
-	}
-
-	if err := ash.WaitForARCAppWindowState(ctx, tconn, perAppDensityPackageName, ash.WindowStateFullscreen); err != nil {
-		s.Fatal("Failed to wait for the activity to be fullscreen: ", err)
-	}
-
-	if err := perappdensity.CountBlackPixels(ctx, cr, expectedPixelCount); err != nil {
-		s.Fatal("Failed to confirm expected pixels: ", err)
+	if err := perappdensity.EnableUSFAndConfirmPixelState(ctx, a, tconn, cr, arc.WindowStateFullscreen, expectedPixelCount); err != nil {
+		s.Fatal("Failed to confirm state after enabling uniform scale factor: ", err)
 	}
 }
