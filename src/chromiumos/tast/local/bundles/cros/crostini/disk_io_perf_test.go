28b60f6864a9bc6d4cab6e3971aa2e53abbce64f
Fergus Dall <sidereal@google.com>
2020-08-21 17:58:21 +1000

    crostini: Migrate tests to using normal external deps
    BUG=chromium:953544
    TEST=none
    
    Cq-Depend: 2437656, chrome-internal:3299125
    Change-Id: I9437a8d8e1c07ed872c7346fac3ddface2212f4c

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/disk_io_perf_test.go
@@ -0,0 +1,26 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"testing"
+	"time"
+
+	"chromiumos/tast/common/genparams"
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/local/vm"
+)
+
+func TestDiskIoPerfParams(t *testing.T) {
+	params := crostini.MakeTestParamsFromList(t, []crostini.Param{{
+		Timeout: 60 * time.Minute,
+		Preconditions: map[vm.ContainerDebianVersion]string{
+			vm.DebianBuster: "crostini.StartedTraceVM()",
+		},
+		MinimalSet:    true,
+		IsNotMainline: true,
+	}})
+	genparams.Ensure(t, "disk_io_perf.go", params)
+}
