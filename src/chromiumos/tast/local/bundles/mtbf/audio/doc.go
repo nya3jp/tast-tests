43389cedfcfc852b6f70ce3cbf44f97d02a93292
Xibin Liu <xliu@cienet.com>
2020-08-21 09:35:56 -0700

    tast-tests: MTBF Audio test cases
    mtbf018: Audio Video | Corrupted files
    mtbf021: Support for GSM_MS, HE-AAC, LC-AAC
    mtbf022: Support for M4A audio file types on Chrome OS
    mtbf023: Support for MP3 audio file types
    mtbf025: Support for PCM, PCM_S16BE and PCM_S24BE
    
    BUG=None
    TEST=tast run -buildworkspace=<tast-tests repo dir> -buildbundle=mtbf -varsfile=<yaml file> <DUT> audio.*
    
    Change-Id: Ia696a810a68bea940e603415d2f77303269b6f60

---
+++ src/chromiumos/tast/local/bundles/mtbf/audio/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package audio contains local Tast tests that exercise audio playback and recording on Chrome OS.
+package audio
---
+++ src/chromiumos/tast/local/bundles/mtbf/main.go
@@ -0,0 +1,18 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package main implements the "mtbf" local test bundle.
+//
+// This executable contains standard Chrome OS tests.
+package main
+
+import (
+	"chromiumos/tast/local/bundlemain"
+	// Underscore-imported packages register their tests via init functions.
+	_ "chromiumos/tast/local/bundles/mtbf/audio"
+)
+
+func main() {
+	bundlemain.Main()
+}
