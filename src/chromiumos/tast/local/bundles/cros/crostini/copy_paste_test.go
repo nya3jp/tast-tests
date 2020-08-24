2f7522b4b922dc1465f4e0ba76d4f22401f73ddf
Fergus Dall <sidereal@google.com>
2020-08-24 21:56:11 +1000

    crostini: generate test parameters using new mechanism
    Crostini tests currently need to have many different parameterized
    sub-tests to cover all the different ways crostini can be downloaded
    and installed. A new mechanism has recently been introduced to
    generate these automatically and keep them from going out of date.
    
    This CL migrates most crostini tests to use this new test parameter
    generator. The remaining tests are special cases which will be handled
    in the next CL.
    
    BUG=chromium:953544
    TEST=none
    
    Change-Id: I2df897c6a7e808e8d3494e4d5054565e244a7a9a

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/copy_paste_test.go
@@ -0,0 +1,50 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+// To update test parameters after modifying this file, run:
+// TAST_GENERATE_UPDATE=1 ~/trunk/src/platform/tast/tools/go.sh test -count=1 chromiumos/tast/local/bundles/cros/crostini/
+
+// See src/chromiumos/tast/local/crostini/params.go for more documentation
+
+import (
+	"testing"
+
+	"chromiumos/tast/common/genparams"
+	"chromiumos/tast/local/crostini"
+)
+
+func TestCopyPasteParams(t *testing.T) {
+	params := crostini.MakeTestParamsFromList(t, []crostini.Param{
+		{
+			Name: "wayland_to_wayland",
+			Val: `testParameters{
+				Copy:  waylandCopyConfig,
+				Paste: waylandPasteConfig,
+			}`,
+		},
+		{
+			Name: "wayland_to_x11",
+			Val: `testParameters{
+				Copy:  waylandCopyConfig,
+				Paste: x11PasteConfig,
+			}`,
+		},
+		{
+			Name: "x11_to_wayland",
+			Val: `testParameters{
+				Copy:  x11CopyConfig,
+				Paste: waylandPasteConfig,
+			}`,
+		},
+		{
+			Name: "x11_to_x11",
+			Val: `testParameters{
+				Copy:  x11CopyConfig,
+				Paste: x11PasteConfig,
+			}`,
+		}})
+	genparams.Ensure(t, "copy_paste.go", params)
+}
---
+++ src/chromiumos/tast/local/bundles/cros/crostini/toolkit_test.go
@@ -0,0 +1,59 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+// To update test parameters after modifying this file, run:
+// TAST_GENERATE_UPDATE=1 ~/trunk/src/platform/tast/tools/go.sh test -count=1 chromiumos/tast/local/bundles/cros/crostini/
+
+// See src/chromiumos/tast/local/crostini/params.go for more documentation
+
+import (
+	"testing"
+
+	"chromiumos/tast/common/genparams"
+	"chromiumos/tast/local/crostini"
+)
+
+func TestToolkitParams(t *testing.T) {
+	params := crostini.MakeTestParamsFromList(t, []crostini.Param{
+		{
+			Name:      "gtk3_wayland",
+			ExtraData: []string{"toolkit_gtk3_demo.py"},
+			Val: `toolkitConfig{
+				data:    "toolkit_gtk3_demo.py",
+				deps:    []string{"python3-gi", "python3-gi-cairo", "gir1.2-gtk-3.0"},
+				command: []string{"env", "GDK_BACKEND=wayland", "python3", "toolkit_gtk3_demo.py"},
+				appID:   "crostini:toolkit_gtk3_demo.py",
+			}`,
+		}, {
+			Name:      "gtk3_x11",
+			ExtraData: []string{"toolkit_gtk3_demo.py"},
+			Val: `toolkitConfig{
+				data:    "toolkit_gtk3_demo.py",
+				deps:    []string{"python3-gi", "python3-gi-cairo", "gir1.2-gtk-3.0"},
+				command: []string{"env", "GDK_BACKEND=x11", "python3", "toolkit_gtk3_demo.py"},
+				appID:   "crostini:org.chromium.termina.wmclass.Toolkit_gtk3_demo.py",
+			}`,
+		}, {
+			Name:      "qt5",
+			ExtraData: []string{"toolkit_qt5_demo.py"},
+			Val: `toolkitConfig{
+				data:    "toolkit_qt5_demo.py",
+				deps:    []string{"python3-pyqt5"},
+				command: []string{"python3", "toolkit_qt5_demo.py"},
+				appID:   "crostini:org.chromium.termina.wmclass.toolkit_qt5_demo.py",
+			}`,
+		}, {
+			Name:      "tkinter",
+			ExtraData: []string{"toolkit_tkinter_demo.py"},
+			Val: `toolkitConfig{
+				data:    "toolkit_tkinter_demo.py",
+				deps:    []string{"python3-tk"},
+				command: []string{"python3", "toolkit_tkinter_demo.py"},
+				appID:   "crostini:org.chromium.termina.wmclass.Tkinter_demo",
+			}`,
+		}})
+	genparams.Ensure(t, "toolkit.go", params)
+}
