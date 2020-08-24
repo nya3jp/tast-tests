aa99ef7ed2b7766a583c256a35aed997deab3812
Meng-Huan Yu <menghuan@chromium.org>
2020-08-25 04:48:26 +0800

    tast-tests: factory.Goofy: Rewrote the error message
    In b/163007073, we had a case that ppl has wrong assumption when reading
    the error message. Rewrote the log a little bit.
    
    BUG=b:163007073
    TEST=tast run $DUT factory.Goofy
    
    Change-Id: If8c2c82bb577b477aa2483aa4cdf670972118a40

--- src/chromiumos/tast/local/bundles/cros/factory/goofy.go
+++ src/chromiumos/tast/local/bundles/cros/factory/goofy.go
@@ -58,11 +58,11 @@ func Goofy(fullCtx context.Context, s *testing.State) {
 
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
 		if _, err := os.Stat(finishFlagFilePath); err != nil {
-			return errors.Wrapf(err, "failed to access finished flag file %s", finishFlagFilePath)
+			return errors.Errorf("finished flag file %s is missing", finishFlagFilePath)
 		}
 		return nil
 	}, nil); err != nil {
-		s.Fatal("Failed to execute the TestList or running Goofy: ", err)
+		s.Fatal("Factory software fail to init and run. This high level and complicate tests is not a good choice to debug. Please check dumpped logs or other failed tests.  Failed to finish Goofy in time: ", err)
 	}
 }
 
