195b47a7aedbb1550375671a0cc17893da8789a5
Fergus Dall <sidereal@google.com>
2020-08-24 21:56:11 +1000

    crostini: generate test parameters using new mechanism
    Crostini tests currently need to have many different parameterized
    sub-tests to cover all the different ways crostini can be downloaded
    and installed. A new mechanism has recently been introduced to
    generate these automatically and keep them from going out of date.
    
    This CL migrates most crostini tests to use this new test parameter
    generator. The remaining tests are special cases which will be handled
    in the next CL.
    
    BUG=chromium:953544
    TEST=none
    
    Change-Id: I2df897c6a7e808e8d3494e4d5054565e244a7a9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2374266
    Tested-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Reviewed-by: David Munro <davidmunro@google.com>
    Reviewed-by: Matthew Warton <mwarton@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/gpu_enabled_test.go
@@ -0,0 +1,32 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+// To update test parameters after modifying this file, run:
+// TAST_GENERATE_UPDATE=1 ~/trunk/src/platform/tast/tools/go.sh test -count=1 chromiumos/tast/local/bundles/cros/crostini/
+
+// See src/chromiumos/tast/local/crostini/params.go for more documentation
+
+import (
+	"testing"
+
+	"chromiumos/tast/common/genparams"
+	"chromiumos/tast/local/crostini"
+)
+
+func TestGpuEnabledParams(t *testing.T) {
+	params := crostini.MakeTestParamsFromList(t, []crostini.Param{
+		{
+			Name:              "sw",
+			Val:               `"llvmpipe"`,
+			ExtraSoftwareDeps: []string{"crosvm_no_gpu"},
+		},
+		{
+			Name:              "gpu",
+			Val:               `"virgl"`,
+			ExtraSoftwareDeps: []string{"crosvm_gpu"},
+		}})
+	genparams.Ensure(t, "gpu_enabled.go", params)
+}
