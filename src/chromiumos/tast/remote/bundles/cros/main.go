1b03d4bafc40b616b2783a132234347196ba7263
Seewai Fu <seewaifu@google.com>
2020-08-25 00:35:02 +0000

    tast: Enable faillog for remote tests
    Enable faillog for remote tests
    One manual test was modified: meta.RunTests.faillog
    BUG=chromium:1094756
    TEST=One manual test
    
    Cq-Depend: chromium:2375912, chrome-internal:3240644, chrome-internal:3263385
    Change-Id: Ia56253fc16e186a4615814e301c46628b2870b99

--- src/chromiumos/tast/remote/bundles/cros/main.go
+++ src/chromiumos/tast/remote/bundles/cros/main.go
@@ -8,9 +8,7 @@
 package main
 
 import (
-	"os"
-
-	"chromiumos/tast/bundle"
+	"chromiumos/tast/local/bundlemain"
 
 	// These packages register their tests via init functions.
 	_ "chromiumos/tast/remote/bundles/cros/arc"
@@ -34,5 +32,5 @@ import (
 )
 
 func main() {
-	os.Exit(bundle.RemoteDefault())
+	bundlemain.RunRemote()
 }
