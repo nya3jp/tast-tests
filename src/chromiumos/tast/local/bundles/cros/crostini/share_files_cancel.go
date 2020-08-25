278d106638d5b5acad77dd23fd0d3ec099a44914
Judy Wu <jinrongwu@google.com>
2020-08-25 20:51:04 +1000

    Add tast test for cancel sharing My files
    Automate crostini test case: 1.7.1 - Sharing chromeos folders
    and files with linux - cancel sharing My files
    
    BUG=chromium:1101583
    TEST=Ran on hatch
    
    Change-Id: I4252b896a5c71d3d33e6138e0f525bc9569ff28f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2374906
    Tested-by: Judy Wu <jinrongwu@google.com>
    Reviewed-by: Matthew Warton <mwarton@google.com>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Judy Wu <jinrongwu@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/share_files_cancel.go
@@ -0,0 +1,89 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome/ui/filesapp"
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/local/crostini/ui/sharedfolders"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ShareFilesCancel,
+		Desc:     "Test clicking Cancel on share confirm dialog",
+		Contacts: []string{"jinrongwu@google.com", "cros-containers-dev@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+		Vars:     []string{"keepState"},
+		Params: []testing.Param{{
+			Name:              "artifact",
+			Pre:               crostini.StartedByArtifact(),
+			ExtraData:         []string{crostini.ImageArtifact},
+			Timeout:           7 * time.Minute,
+			ExtraHardwareDeps: crostini.CrostiniStable,
+		}, {
+			Name:              "artifact_unstable",
+			Pre:               crostini.StartedByArtifact(),
+			ExtraData:         []string{crostini.ImageArtifact},
+			Timeout:           7 * time.Minute,
+			ExtraHardwareDeps: crostini.CrostiniUnstable,
+		}, {
+			Name:    "download_stretch",
+			Pre:     crostini.StartedByDownloadStretch(),
+			Timeout: 10 * time.Minute,
+		}, {
+			Name:    "download_buster",
+			Pre:     crostini.StartedByDownloadBuster(),
+			Timeout: 10 * time.Minute,
+		}},
+		SoftwareDeps: []string{"chrome", "vm_host"},
+	})
+}
+func ShareFilesCancel(ctx context.Context, s *testing.State) {
+	tconn := s.PreValue().(crostini.PreData).TestAPIConn
+	cont := s.PreValue().(crostini.PreData).Container
+
+	// Use a shortened context for test operations to reserve time for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 30*time.Second)
+	defer cancel()
+	defer crostini.RunCrostiniPostTest(ctx, cont)
+
+	// Open Files app.
+	filesApp, err := filesapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to open Files app: ", err)
+	}
+	defer filesApp.Close(cleanupCtx)
+
+	sharedFolders := sharedfolders.NewSharedFolders()
+
+	// Clean up shared folders in the end.
+	defer func() {
+		if err := sharedFolders.UnshareAll(cleanupCtx, tconn, cont); err != nil {
+			s.Error("Failed to unshare all folders: ", err)
+		}
+	}()
+
+	// Share My files.
+	scd, err := sharedFolders.ShareMyFiles(ctx, tconn, filesApp, sharedfolders.MyFilesMsg)
+	if err != nil {
+		s.Fatal("Failed to share My files: ", err)
+	}
+	defer scd.Release(ctx)
+
+	if err := scd.ClickCancel(ctx, tconn); err != nil {
+		s.Fatal("Failed to click Cancel on the share confirm dialog: ", err)
+	}
+
+	if err := sharedFolders.CheckNoSharedFolders(ctx, tconn, cont); err != nil {
+		s.Fatal("Failed to check shared folders list by default: ", err)
+	}
+}
