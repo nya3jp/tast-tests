2328b6dd94250145ea9e316e1f328677cffad517
Harvey Yang <chenghaoyang@chromium.org>
2020-08-25 11:29:38 +0800

    hardware: Add SensorIioservice Tast test
    This commit adds a tast test SensorIioservice that reads samples from
    iioservice with iioservice_simpleclient. SensorIioservice will pass
    if reads are stable.
    
    BUG=chromium:1006141, b:146464155
    TEST=run on octopus and kukui
    
    Cq-Depend: chromium:1782139
    Change-Id: I12f50fc44fedf9f1a980d13f76dfa592ecf6eb38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/1983000
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Cheng-Hao Yang <chenghaoyang@chromium.org>
    Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/hardware/sensor_iioservice.go
@@ -0,0 +1,70 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hardware
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"chromiumos/tast/local/bundles/cros/hardware/iio"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+const onErrorOccurred = "OnErrorOccurred:"
+const succeedReadingSamples = "Number of success reads"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SensorIioservice,
+		Desc: "Tests that iioservice provides sensors' samples properly",
+		Contacts: []string{
+			"gwendal@chromium.com",      // Chrome OS sensors point of contact
+			"chenghaoyang@chromium.org", // Test author
+			"chromeos-sensors@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+// SensorIioservice reads all devices' samples from daemon iioservice.
+func SensorIioservice(ctx context.Context, s *testing.State) {
+	var maxFreq int
+	var strOut string
+
+	// Call libmems' functions directly here to read and verify samples
+	sensors, err := iio.GetSensors()
+	if err != nil {
+		s.Fatal("Error reading sensors on DUT: ", err)
+	}
+
+	for _, sn := range sensors {
+		maxFreq = sn.MaxFrequency
+
+		if sn.Name == iio.Ring || sn.Name == iio.Light {
+			continue
+		}
+
+		frequency := fmt.Sprintf("--frequency=%f", float64(maxFreq)/1000)
+
+		out, err := testexec.CommandContext(ctx, "iioservice_simpleclient",
+			fmt.Sprintf("--device_id=%s", sn.Path[10:]), "--channels=timestamp",
+			frequency).CombinedOutput()
+
+		if err != nil {
+			s.Error("Error reading samples on DUT: ", err)
+		}
+
+		strOut = string(out)
+		if strings.Contains(strOut, onErrorOccurred) {
+			s.Error("OnErrorOccurred: ", sn.Name)
+		} else if !strings.Contains(strOut, succeedReadingSamples) {
+			s.Error("Not enough successful readsamples on sensor: ", sn.Name)
+		} else {
+			s.Logf("Test passed on device name: %v, id: %v", sn.Name, sn.Path)
+		}
+	}
+}
