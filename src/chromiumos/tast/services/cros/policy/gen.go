cba00b562f318ba58c41bcbbe01ed86d0e58bbbf
Saurabh Nijhara <snijhara@google.com>
2020-08-26 16:46:59 +0200

    tast-tests: add test for DeviceMinimumVersion policy
    This CL adds a test for DeviceMinimumVersion policy for the case
    of critical updates i.e. zero warning time. The test verifies that
    the in case of critical updates the user is logged out of the
    session and an update required screen is shown on the login page.
    
    BUG=chromium:1121550
    TEST=tast run <ip> policy.DeviceMinimumVersion
    
    Change-Id: Ieaad0bbc7a16bfa88b6144387753164808ecc422

--- src/chromiumos/tast/services/cros/policy/gen.go
+++ src/chromiumos/tast/services/cros/policy/gen.go
@@ -5,6 +5,7 @@
 //go:generate protoc -I . --go_out=plugins=grpc:../../../../.. policy.proto
 //go:generate protoc -I . --go_out=plugins=grpc:../../../../.. system_timezone.proto
 //go:generate protoc -I . --go_out=plugins=grpc:../../../../.. client_certificate_service.proto
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. device_minimum_version_service.proto
 
 // Package policy provides the PolicyService
 package policy
