0016d014dc7e6cefa08091f556285f8c2d335668
Jun Sheng <shengjun@google.com>
2020-08-27 21:18:45 +1000

    tast-tests: add new test to launch help app from browser menu
    1. Add new function to launch help app from three dot menu.
    
    BUG=b:166526703
    TEST=apps.LaunchHelpAppInBrowserMenu*
    
    Change-Id: I86625f73eb8edd6ebe65051d501fd7bc4bcc5e8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2377012
    Tested-by: Jun Sheng <shengjun@chromium.org>
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Jun Sheng <shengjun@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_help_app_in_browser_menu.go
@@ -0,0 +1,59 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package apps
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/apps/helpapp"
+	"chromiumos/tast/local/bundles/cros/apps/pre"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LaunchHelpAppInBrowserMenu,
+		Desc: "Help app can be launched in browser menu",
+		Contacts: []string{
+			"showoff-eng@google.com",
+			"shengjun@chromium.org", // original test author
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+		Params: []testing.Param{
+			{
+				Name:              "stable",
+				ExtraHardwareDeps: pre.AppsStableModels,
+			}, {
+				Name:              "unstable",
+				ExtraHardwareDeps: pre.AppsUnstableModels,
+			},
+		},
+	})
+}
+
+// LaunchHelpAppInBrowserMenu verifies launching Help app in chrome browser three dot menu.
+func LaunchHelpAppInBrowserMenu(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	conn, err := cr.NewConn(ctx, chrome.BlankURL)
+	if err != nil {
+		s.Fatal("Failed to lunch a new browser: ", err)
+	}
+	defer conn.Close()
+
+	if err := helpapp.LaunchFromThreeDotMenu(ctx, tconn); err != nil {
+		s.Fatal("Failed to launch Help app from chrome three dot menu: ", err)
+	}
+}
