efabc69b8543be52a58564d3b05de7cb043e1ca7
Po-Hsien Wang <pwang@chromium.org>
2020-08-26 23:56:33 -0700

    graphics: Collect VM and container logs
    This is ported from crrev.com/c/2345889, which collects VM logs
    whenever test finished instead precondition closed.
    
    Note that as we have both private&public version of trace_replay tests. I made the
    defer call in RunTraceReplayTest function.
    
    BUG=None
    TEST=Check existing crostini tests log in stainless
    
    Change-Id: I6bb1228f2b71c79ae50dc247c3304aec6d875862

--- src/chromiumos/tast/local/graphics/trace/trace_replay.go
+++ src/chromiumos/tast/local/graphics/trace/trace_replay.go
@@ -214,6 +214,8 @@ func pushTraceReplayApp(ctx context.Context, cont *vm.Container) error {
 
 // RunTraceReplayTest starts a VM and replays all the traces in the test config.
 func RunTraceReplayTest(ctx context.Context, resultDir string, cloudStorage *testing.CloudStorage, cont *vm.Container, group *comm.TestGroupConfig) error {
+	defer crostini.RunCrostiniPostTest(ctx, cont)
+
 	// Guest is unable to use VM network interface to access it's host because of security reason,
 	// and the only to make such connectivity is to use host's outbound network interface.
 	outboundIP, err := getOutboundIP()
