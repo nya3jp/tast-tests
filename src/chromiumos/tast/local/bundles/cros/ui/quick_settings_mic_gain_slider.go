535ad001fcbe9d921580aee0b9428bebe5511870
Kyle Shimabukuro <kyleshima@chromium.org>
2020-08-28 14:02:45 -0700

    Add Quick Settings slider controls and a test for the mic gain slider
    Add the ability to control the Quick Settings sliders and a test for
    controlling the mic gain slider.
    
    BUG=chromium:1083465
    TEST=tast run <ip> ui.QuickSettingsMicGainSlider
    
    Change-Id: I2b0392c78549634ea9e11390775761316d988a5c

---
+++ src/chromiumos/tast/local/bundles/cros/ui/quick_settings_mic_gain_slider.go
@@ -0,0 +1,66 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/ui/quicksettings"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: QuickSettingsMicGainSlider,
+		Desc: "Checks that the Quick Settings mic gain slider can be adjusted",
+		Contacts: []string{
+			"kyleshima@chromium.org",
+			"bhansknecht@chromium.org",
+			"dhaddock@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "audio_record"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+// QuickSettingsMicGainSlider tests that the mic gain slider can be adjusted up and down.
+func QuickSettingsMicGainSlider(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// Set up the keyboard, which is used to increment/decrement the slider.
+	// TODO(crbug/1123231): use better slider automation controls if possible, instead of keyboard controls.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer kb.Close()
+
+	initial, err := quicksettings.SliderValue(ctx, tconn, quicksettings.SliderTypeMicGain)
+	if err != nil {
+		s.Fatal("Failed initial value check for mic gain slider: ", quicksettings.SliderTypeMicGain)
+	}
+	s.Log("Initial mic gain slider value: ", initial)
+
+	decrease, err := quicksettings.DecreaseSlider(ctx, tconn, kb, quicksettings.SliderTypeMicGain)
+	if err != nil {
+		s.Fatal("Failed to decrease mic gain slider: ", err)
+	}
+	s.Log("Decreased mic gain slider value: ", decrease)
+
+	increase, err := quicksettings.IncreaseSlider(ctx, tconn, kb, quicksettings.SliderTypeMicGain)
+	if err != nil {
+		s.Fatal("Failed to increase mic gain slider: ", err)
+	}
+	s.Log("Increased mic gain slider value: ", increase)
+}
