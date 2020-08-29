c06b718b041904a3fe0aa56d7ec951f0d09cde32
Andrew Luo <aluo@chromium.org>
2020-08-28 19:12:24 -0700

    Adding preconditions for firmware tests to support different boot modes.
    BUG=b:166162128
    TEST=tast run -var=servo=<labstation:port> <dut> firmware.Pre.normal firmware.Pre.dev
    firmware.Pre.rec; #make sure servod has already been started on the
    labstation.
    
    Change-Id: I4d72f1d5981c70fe0fc0c169dc0ca180a8142578

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/pre.go
@@ -0,0 +1,60 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+
+	common "chromiumos/tast/common/firmware"
+	"chromiumos/tast/remote/firmware"
+	"chromiumos/tast/remote/firmware/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        Pre,
+		Desc:        "Verifies firmware Preconditions",
+		Contacts:    []string{"cros-fw-engprod@google.com", "aluo@google.com"},
+		Data:        []string{firmware.ConfigFile},
+		ServiceDeps: []string{"tast.cros.firmware.BiosService", "tast.cros.firmware.UtilsService"},
+		Vars:        []string{"servo"},
+		Params: []testing.Param{{
+			Name:      "normal",
+			Val:       common.BootModeNormal,
+			Pre:       pre.NormalMode(),
+			ExtraAttr: []string{"group:mainline", "informational"},
+		}, {
+			Name: "dev",
+			Val:  common.BootModeDev,
+			Pre:  pre.DevMode(),
+			//TODO(aluo): Re-enable when b/169704069 is resolved.
+		}, {
+			Name: "rec",
+			Val:  common.BootModeRecovery,
+			Pre:  pre.RecMode(),
+			//TODO(aluo): Re-enable when b/169704069 is resolved.
+		}},
+	})
+}
+
+func Pre(ctx context.Context, s *testing.State) {
+	v := s.PreValue().(*pre.Value)
+	mode := s.Param().(common.BootMode)
+
+	if v.BootMode != mode {
+		s.Fatalf("Precondition boot mode unexpected, want %v, got %v", mode, v.BootMode)
+	}
+
+	h := v.Helper
+
+	curr, err := h.Reporter.CurrentBootMode(ctx)
+	if err != nil {
+		s.Fatal("Could not report DUT boot mode: ", err)
+	} else if curr != v.BootMode {
+		s.Fatalf("DUT boot mode unexpected, want %v, got %v", v.BootMode, curr)
+	}
+	s.Logf("Successfully verified that precondition set boot mode to %q", curr)
+}
