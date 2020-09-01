887d60449b4404857b54c4d72e74d3574f3057a0
Tetsui Ohkubo <tetsui@chromium.org>
2020-09-01 17:28:32 +0900

    tast-tests: Promote arc.KeyCharacterMap to CQ
    According to http://tastboard/test/arc.KeyCharacterMap this test seems
    quite stable for the last >20 builds and remaining failures seem to be
    infra-related. Also, this CL adds .vm variant as informational.
    
    BUG=None
    TEST=tast run eve-DUT arc.KeyCharacterMap
    
    Change-Id: I1642d9aa6adc5c85ad8efc5f9d1b31f599d7ab72

--- src/chromiumos/tast/local/bundles/cros/arc/key_character_map.go
+++ src/chromiumos/tast/local/bundles/cros/arc/key_character_map.go
@@ -21,7 +21,7 @@ func init() {
 		Func:         KeyCharacterMap,
 		Desc:         "Checks KeyCharacterMap working in non-US layouts",
 		Contacts:     []string{"tetsui@chromium.org", "arc-framework+tast@google.com"},
-		Attr:         []string{"informational", "group:mainline"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Pre:          arc.Booted(),
 		Timeout:      3 * time.Minute,
@@ -30,6 +30,7 @@ func init() {
 		}, {
 			Name:              "vm",
 			ExtraSoftwareDeps: []string{"android_vm"},
+			ExtraAttr:         []string{"informational"},
 		}},
 	})
 }
