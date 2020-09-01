4e36825ef6ad602eec90285d4d1ea6d593c28304
Wei Lee <wtlee@google.com>
2020-09-01 12:12:36 +0800

    camera: Supports camera.CCAUISmoke.* for SWA
    As the first step to enable integration tests for SWA version of CCA, we
    should adjust the communication between Tast tests and CCA and enable
    camera.CCAUISmoke.*
    
    BUG=chromium:980846
    TEST=tast run [DUT] camera.CCAUISmoke.*
    
    Change-Id: I3f8acd03fa68bc054e17936a14b5ef23a4a5661f

--- src/chromiumos/tast/local/bundles/cros/camera/cca_ui_preview.go
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_preview.go
@@ -9,7 +9,7 @@ import (
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/camera/cca"
-	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/bundles/cros/camera/testutil"
 	"chromiumos/tast/local/media/caps"
 	"chromiumos/tast/testing"
 )
@@ -22,18 +22,21 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", caps.BuiltinOrVividCamera},
 		Data:         []string{"cca_ui.js"},
-		Pre:          chrome.LoggedIn(),
+		Pre:          testutil.NewPrecondition(testutil.ChromeConfig{}),
 	})
 }
 
 func CCAUIPreview(ctx context.Context, s *testing.State) {
-	cr := s.PreValue().(*chrome.Chrome)
+	p := s.PreValue().(testutil.PreData)
+	cr := p.Chrome
+	tb := p.TestBridge
+	isSWA := p.Config.InstallSWA
 
 	if err := cca.ClearSavedDirs(ctx, cr); err != nil {
 		s.Fatal("Failed to clear saved directory: ", err)
 	}
 
-	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir())
+	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir(), tb, isSWA)
 	if err != nil {
 		s.Fatal("Failed to open CCA: ", err)
 	}
@@ -46,7 +49,10 @@ func CCAUIPreview(ctx context.Context, s *testing.State) {
 
 	restartApp := func() {
 		s.Log("Restarts CCA")
-		if err := app.Restart(ctx); err != nil {
+		if err := app.CheckJSError(ctx, s.OutDir()); err != nil {
+			s.Error("Failed with javascript errors: ", err)
+		}
+		if err := app.Restart(ctx, tb, isSWA); err != nil {
 			s.Fatal("Failed to restart CCA: ", err)
 		}
 	}
--- src/chromiumos/tast/local/bundles/cros/camera/cca_ui_resolutions.go
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_resolutions.go
@@ -19,7 +19,7 @@ import (
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/camera/cca"
-	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/bundles/cros/camera/testutil"
 	"chromiumos/tast/local/media/caps"
 	"chromiumos/tast/testing"
 )
@@ -32,7 +32,7 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "arc_camera3", caps.BuiltinOrVividCamera},
 		Data:         []string{"cca_ui.js"},
-		Pre:          chrome.LoggedIn(),
+		Pre:          testutil.NewPrecondition(testutil.ChromeConfig{}),
 		// Default timeout (i.e. 2 minutes) is not enough for some devices to
 		// exercise all resolutions on all cameras.
 		Timeout: 5 * time.Minute,
@@ -40,13 +40,16 @@ func init() {
 }
 
 func CCAUIResolutions(ctx context.Context, s *testing.State) {
-	cr := s.PreValue().(*chrome.Chrome)
+	p := s.PreValue().(testutil.PreData)
+	cr := p.Chrome
+	tb := p.TestBridge
+	isSWA := p.Config.InstallSWA
 
 	if err := cca.ClearSavedDirs(ctx, cr); err != nil {
 		s.Fatal("Failed to clear saved directory: ", err)
 	}
 
-	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir())
+	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir(), tb, isSWA)
 	if err != nil {
 		s.Fatal("Failed to open CCA: ", err)
 	}
@@ -59,7 +62,10 @@ func CCAUIResolutions(ctx context.Context, s *testing.State) {
 
 	restartApp := func() {
 		s.Log("Restarts CCA")
-		if err := app.Restart(ctx); err != nil {
+		if err := app.CheckJSError(ctx, s.OutDir()); err != nil {
+			s.Error("Failed with javascript errors: ", err)
+		}
+		if err := app.Restart(ctx, tb, isSWA); err != nil {
 			s.Fatal("Failed to restart CCA: ", err)
 		}
 	}
