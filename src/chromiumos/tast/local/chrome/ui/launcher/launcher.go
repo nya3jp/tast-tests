edf004cd95fc856dbfae55a431b0db47c9473035
Seewai Fu <seewaifu@google.com>
2020-09-02 13:19:06 -0700

    tast_tests: Pin Application onto shelf
    Test Chrome OS feature for pinning application onto shelf.
    TestTracker Link: https://testtracker.googleplex.com/testplans/testcase/detail/1763443
    Chromium Issue Link: https://bugs.chromium.org/p/chromium/issues/detail?id=1124415
    Command line: tast -verbose run -build=true <host> ui.LauncherPinAppToShelf.tablet_mode ui.LauncherPinAppToShelf.clamshell_mode
    Test Result:
    Code Name       Form Factor     Result
    octopus         Tablet mode     Pass
    hana            Chromebook      Pass
    karma           Chromebase      Pass
    monroe          Chromebase      N/A (Could not in as test user)
    faffy           Chromebox       Pass
    jax             Chromebox       Pass
    dru             Tablet          Pass
    kodama          Tablet          Pass
    
    BUG=chromium:1124415
    TEST=Manual test passed on octopus and hana
    
    Change-Id: I1d654def0b21c5e932d6bc45c31ae909a6898c1e

--- src/chromiumos/tast/local/chrome/ui/launcher/launcher.go
+++ src/chromiumos/tast/local/chrome/ui/launcher/launcher.go
@@ -10,13 +10,18 @@ import (
 	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
 
 const defaultTimeout = 15 * time.Second
+
+// ExpandedItemsClass define the class name of the expanded launcher view which is used as search parameters in ui.
+const ExpandedItemsClass = "ui/app_list/AppListItemView"
 
 // SearchAndLaunch searches a query in the launcher and executes it.
 func SearchAndLaunch(ctx context.Context, tconn *chrome.TestConn, query string) error {
@@ -110,4 +115,116 @@ func SearchAndWaitForApp(ctx context.Context, tconn *chrome.TestConn, appName st
 		return nil, errors.Wrapf(err, "%s app does not exist in search result", appName)
 	}
 	return appNode, err
+}
+
+// OpenExpandedView opens the Launcher to the Apps list page.
+func OpenExpandedView(ctx context.Context, tconn *chrome.TestConn) error {
+	// TODO: Call autotestPrivate API instead after https://bugs.chromium.org/p/chromium/issues/detail?id=1127384 is implemented.
+	params := ui.FindParams{ClassName: ExpandedItemsClass}
+	exist, err := ui.Exists(ctx, tconn, params)
+	if err != nil {
+		return errors.Wrap(err, "cannot check if ui/app_list/AppListItemView exists")
+	}
+	if exist {
+		// Even if it exist, active window may cover it in tablet mode. Check for active windows.
+		windows, err := ash.FindAllWindows(ctx, tconn, func(window *ash.Window) bool {
+			return window.IsVisible
+		})
+		if err != nil {
+			return errors.Wrap(err, "failed to get all windows")
+		}
+		// Do nothing if there are no windows. Homescreen should be there already.
+		if len(windows) == 0 {
+			return nil
+		}
+	}
+
+	if err := ash.TriggerLauncherStateChange(ctx, tconn, ash.AccelShiftSearch); err != nil {
+		return errors.Wrap(err, "failed to switch to fullscreen")
+	}
+	if err := ash.WaitForLauncherState(ctx, tconn, ash.FullscreenAllApps); err != nil {
+		return errors.Wrap(err, "failed to switch the state to 'FullscreenAllApps'")
+	}
+	return nil
+}
+
+// FindAppFromItemView finds the node handle of an application from the expanded launcher.
+func FindAppFromItemView(ctx context.Context, tconn *chrome.TestConn, app apps.App) (*ui.Node, error) {
+	if err := OpenExpandedView(ctx, tconn); err != nil {
+		return nil, errors.Wrapf(err, "failed to expand launcher while looking for app %q", app.Name)
+	}
+	params := ui.FindParams{Name: app.Name, ClassName: ExpandedItemsClass}
+	icon, err := ui.FindWithTimeout(ctx, tconn, params, defaultTimeout)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to find app %q", app.Name)
+	}
+	return icon, nil
+}
+
+// LaunchApp runs an app from the expanded launcher.
+func LaunchApp(ctx context.Context, tconn *chrome.TestConn, app apps.App) error {
+	icon, err := FindAppFromItemView(ctx, tconn, app)
+	if err != nil {
+		return err
+	}
+	defer icon.Release(ctx)
+
+	// To handle the case of icon is off screen, we first make sure the icon is in focus first.
+	if err := icon.FocusAndWait(ctx, 10*time.Second); err != nil {
+		return errors.Wrap(err, "failed to focus on icon")
+	}
+
+	if err := icon.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "failed to run app")
+	}
+	// Make sure app is pinned on the shelf.
+	if err := ash.WaitForApp(ctx, tconn, app.ID); err != nil {
+		return errors.Wrapf(err, "failed to wait for app %q: ", app.Name)
+	}
+	// Make sure all items on the shelf are done moving.
+	if err := ui.WaitForLocationChangeCompleted(ctx, tconn); err != nil {
+		return errors.Wrap(err, "failed to wait for location change to be completed")
+	}
+	return nil
+}
+
+// PinAppToShelf pins an app from the expanded launcher to shelf.
+func PinAppToShelf(ctx context.Context, tconn *chrome.TestConn, app apps.App) error {
+	// Find the icon from the the expanded launcher.
+	icon, err := FindAppFromItemView(ctx, tconn, app)
+	if err != nil {
+		return err
+	}
+	defer icon.Release(ctx)
+
+	// To handle the case of icon is off screen, we first make sure the icon is in focus first.
+	if err := icon.FocusAndWait(ctx, 10*time.Second); err != nil {
+		return errors.Wrap(err, "failed to focus on icon")
+	}
+
+	if err := ui.WaitForLocationChangeCompletedOnNode(ctx, tconn, icon); err != nil {
+		return errors.Wrap(err, "failed to wait for location change for node")
+	}
+
+	// Open context menu.
+	if err := icon.RightClick(ctx); err != nil {
+		return errors.Wrap(err, "failed to open context menu")
+	}
+	// Find option to pin app to shelf.
+	params := ui.FindParams{Name: "Pin to shelf", ClassName: "MenuItemView"}
+	option, err := ui.FindWithTimeout(ctx, tconn, params, 10*time.Second)
+	if err != nil {
+		// The option pin to shelf is not available for this icon
+		return errors.Wrap(err, `option "Pin to shelf" is not available`)
+	}
+	defer option.Release(ctx)
+	// Pin app to shelf.
+	if err := option.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, `failed to select option "Pin to shelf"`)
+	}
+	// Make sure all items on the shelf are done moving.
+	if err := ui.WaitForLocationChangeCompleted(ctx, tconn); err != nil {
+		return errors.Wrap(err, "failed to wait for location change to be completed")
+	}
+	return nil
 }
