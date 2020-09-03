9a31a5dcda49f8f32a15b66f9609a11c6a75fe19
Keiichi Watanabe <keiichiw@chromium.org>
2020-09-03 19:09:23 +0900

    tast-tests: crostini: Add VmcStart test
    Add tests that runs the `vmc` command to start a Linux VM.
    
    BUG=chromium:1068046
    TEST=tast run $DUT crostini.VmcStart.*
    
    Change-Id: I0dc02695f2be60e984736e71c1fc2a6efaadae7a

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/vmc/vmc.go
@@ -0,0 +1,41 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package vmc provides utilities for the vmc command.
+package vmc
+
+import (
+	"context"
+	"os"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/session"
+	"chromiumos/tast/local/testexec"
+)
+
+// UserIDHash returns a sanitized username of the primary session.
+// The return value can be used as "CROS_USER_ID_HASH".
+func UserIDHash(ctx context.Context) (string, error) {
+	sessionManager, err := session.NewSessionManager(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to start session manager")
+	}
+
+	_, hash, err := sessionManager.RetrievePrimarySession(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to retrieve the primary session")
+	}
+
+	return hash, nil
+}
+
+// Command creates an object for a vmc command.
+func Command(ctx context.Context, hash string, arg ...string) *testexec.Cmd {
+	cmd := testexec.CommandContext(ctx, "vmc", arg...)
+	cmd.Env = append(
+		os.Environ(),
+		"CROS_USER_ID_HASH="+hash,
+	)
+	return cmd
+}
