64b1185e0463a39336bb24a1c66d9ea8ec6b6e59
Rachel Carpenter <carpenterr@chromium.org>
2020-09-03 15:45:05 +1000

    Add new Test tests to test opening Help App from settings.
    There are two entry points here, one for Release Notes and one for
    general help. Create a test for each.
    
    BUG=b:166510008
    TEST=apps.LaunchHelpAppFromSettings*,
    apps.LaunchReleaseNotesFromSettings*
    
    Change-Id: I31e7a0a117849828b939d112082a90c9542f56b7

---
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_help_app_from_settings.go
@@ -0,0 +1,69 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package apps
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/apps/helpapp"
+	"chromiumos/tast/local/bundles/cros/apps/pre"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/ui/settings"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LaunchHelpAppFromSettings,
+		Desc: "Help app can be launched from Settings",
+		Contacts: []string{
+			"showoff-eng@google.com",
+			"carpenterr@chromium.org", // original test author
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+		Params: []testing.Param{
+			{
+				Name:              "stable",
+				ExtraHardwareDeps: pre.AppsStableModels,
+			}, {
+				Name:              "unstable",
+				ExtraHardwareDeps: pre.AppsUnstableModels,
+			},
+		},
+	})
+}
+
+// LaunchHelpAppFromSettings verifies launching Help app from Chrome OS settings.
+func LaunchHelpAppFromSettings(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	if err := settings.LaunchAtPage(ctx, tconn, settings.AboutChromeOS); err != nil {
+		s.Fatal("Failed to launch Settings: ", err)
+	}
+
+	// Find and click Get help with Chrome OS.
+	if err := ui.FindAndClick(ctx, tconn, ui.FindParams{
+		Name: "Get help with Chrome OS",
+		Role: ui.RoleTypeLink,
+	}, 10*time.Second); err != nil {
+		s.Fatal("Failed to click Get help with Chrome OS: ", err)
+	}
+
+	// App should be launched at the overview page.
+	if err := helpapp.WaitForApp(ctx, tconn); err != nil {
+		s.Fatal("Failed to launch help app: ", err)
+	}
+}
