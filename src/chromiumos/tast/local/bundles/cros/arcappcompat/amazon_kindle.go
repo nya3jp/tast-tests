db1ac854d09773c288c02baa94579094cd7180ac
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-09-04 17:30:32 -0700

    Run UIAutomator Test Using Tast For AmazonKindle app
    BUG=b:167704273
    TEST=tast run <eve> arcappcompat.AmazonKindle
    
    Tast Test for Amazon Kindle app verifies launch app
    from the playstore, check app reaches
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: If5b0de30b30052a6ed1309221995095aa3ef5c58

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/amazon_kindle.go
@@ -0,0 +1,92 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForAmazonKindle = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForAmazonKindle},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForAmazonKindle = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForAmazonKindle},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AmazonKindle,
+		Desc:         "Functional test for AmazonKindle that installs the app also verifies it reaches home page, also checks AmazonKindle correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForAmazonKindle,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForAmazonKindle,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForAmazonKindle,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForAmazonKindle,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// AmazonKindle test uses library for opting into the playstore and installing app.
+// Checks AmazonKindle correctly changes the window states in both clamshell and touchview mode.
+func AmazonKindle(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.amazon.kindle"
+		appActivity = ".UpgradePage"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForAmazonKindle verifies reached main activity page of the app.
+func launchAppForAmazonKindle(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		homePageID = "android:id/content"
+	)
+
+	// Check for home icon.
+	homeIcon := d.Object(ui.ID(homePageID), ui.PackageName(appPkgName))
+	if err := homeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("home icon doesn't exist: ", err)
+	} else if err := homeIcon.Click(ctx); err != nil {
+		s.Fatal("Failed to click on homeIcon: ", err)
+	}
+}
