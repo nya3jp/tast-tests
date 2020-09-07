7ce2ff778ba6f5e8ced3827b6349fc32fa96b59f
David Stevens <stevensd@chromium.org>
2020-09-07 17:51:00 +0900

    arc: Move c2e2e resources into arc directory
    The arc video tests were moved from video to arc awhile back, so it's
    no longer necessary to have a separate package to share the c2e2e
    resources between arc's video and power tests.
    
    BUG=None
    TEST=tast run DUT arc.PowerVideoPerf.* arc.VideoDecodeAccelPerf.*
    
    Change-Id: Ia4508db0d1619f77c1f2d2af848fdb48346a1015

--- src/chromiumos/tast/local/bundles/cros/arc/video/arc_accel_video.go
+++ src/chromiumos/tast/local/bundles/cros/arc/video/arc_accel_video.go
@@ -20,7 +20,7 @@ import (
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
-	"chromiumos/tast/local/arc/c2e2etest"
+	"chromiumos/tast/local/bundles/cros/arc/c2e2etest"
 	"chromiumos/tast/local/media/cpu"
 	"chromiumos/tast/local/media/logging"
 	"chromiumos/tast/local/testexec"
