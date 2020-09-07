970cb28420473b087edec94e3c591b25b3b275f8
Daniil Lunev <dlunev@chromium.org>
2020-09-07 10:43:09 +1000

    Improve disk_info utility for Storage Qualification
    * Add Secondary namespace detection
    * Be more restrictive on the allowed device regex
    * Filter devices early
    * Fix bug with returning reference to a temporary device
    * Remove unnecessary test case
    * Better parsing/filtering test (to cover all allowed)
    
    BUG=b:160193784
    TEST=cros_run_unit_tests  --packages chromeos-base/tast-local-tests-cros --board hatch -v
    
    Change-Id: I084bbad3f6e248ee836c9be08312753e96fa06ad

--- src/chromiumos/tast/local/bundles/cros/storage/stress/disk_info_test.go
+++ src/chromiumos/tast/local/bundles/cros/storage/stress/disk_info_test.go
@@ -10,12 +10,19 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestParseGetDiskInfo(t *testing.T) {
+func TestParseAndFilterGetDiskInfo(t *testing.T) {
 	const out = `
 {
-	"blockdevices": [
-		{"name": "sda", "type": "disk", "hotplug": "0", "size": "1073741824000", "state": "running"}
-	]
+  "blockdevices": [
+    {"name": "loop0", "type": "loop", "hotplug": "0", "size": "536870912000", "state": null},
+    {"name": "sda", "type": "disk", "hotplug": "0", "size": "1073741824000", "state": "running"},
+    {"name": "mmcblk0", "type": "disk", "hotplug": "1", "size": "31268536320", "state": null},
+    {"name": "mmcblk0boot0", "type": "disk", "hotplug": "1", "size": "4194304", "state": null},
+    {"name": "mmcblk0boot1", "type": "disk", "hotplug": "1", "size": "4194304", "state": null},
+    {"name":"nvme0n1", "type":"disk", "hotplug": "0", "size":"137438953472", "state":"live"},
+    {"name":"nvme0n2", "type":"disk", "hotplug": "0", "size":"17179869184", "state":"live"},
+    {"name":"nvme1n1", "type":"disk", "hotplug": "0", "size":"34359738368", "state":"live"}
+  ]
 }`
 
 	info, err := parseDiskInfo([]byte(out))
@@ -23,49 +30,52 @@ func TestParseGetDiskInfo(t *testing.T) {
 		t.Fatal("parseDiskInfo() failed: ", err)
 	}
 
-	exp := Blockdevice{
-		Name:    "sda",
-		Type:    "disk",
-		Hotplug: "0",
-		Size:    1073741824000,
-		State:   "running",
+	info = filterNotAllowedDevices(info)
+
+	exp := []*Blockdevice{
+		{
+			Name:    "sda",
+			Type:    "disk",
+			Hotplug: "0",
+			Size:    1073741824000,
+			State:   "running",
+		},
+		{
+			Name:    "mmcblk0",
+			Type:    "disk",
+			Hotplug: "1",
+			Size:    31268536320,
+			State:   "",
+		},
+		{
+			Name:    "nvme0n1",
+			Type:    "disk",
+			Hotplug: "0",
+			Size:    137438953472,
+			State:   "live",
+		},
+		{
+			Name:    "nvme0n2",
+			Type:    "disk",
+			Hotplug: "0",
+			Size:    17179869184,
+			State:   "live",
+		},
+		{
+			Name:    "nvme1n1",
+			Type:    "disk",
+			Hotplug: "0",
+			Size:    34359738368,
+			State:   "live",
+		},
 	}
 
-	if !cmp.Equal(info.Blockdevices[0], exp) {
-		t.Errorf("parseDiskInfo() = %+v; want %+v", info.Blockdevices[0], exp)
-	}
-}
-
-func TestParseAndFilterGetDiskInfo(t *testing.T) {
-	const out = `
-	{
-		"blockdevices": [
-			{"name": "loop0", "type": "loop", "hotplug": "0", "size": "536870912000", "state": null},
-			{"name": "sda", "type": "disk", "hotplug": "0", "size": "1073741824000", "state": "running"}
-		]
-	}`
-
-	info, err := parseDiskInfo([]byte(out))
-	if err != nil {
-		t.Fatal("parseDiskInfo() failed: ", err)
+	if len(info.Blockdevices) != 5 {
+		t.Errorf("filterNotAllowedDevices() returned %+v elements", len(info.Blockdevices))
 	}
 
-	info = filterNonDiskDevices(info)
-
-	exp := Blockdevice{
-		Name:    "sda",
-		Type:    "disk",
-		Hotplug: "0",
-		Size:    1073741824000,
-		State:   "running",
-	}
-
-	if len(info.Blockdevices) != 1 {
-		t.Errorf("filterNonDiskDevices() returned %+v elements", len(info.Blockdevices))
-	}
-
-	if !cmp.Equal(info.Blockdevices[0], exp) {
-		t.Errorf("parseDiskInfo() = %+v; want %+v", info.Blockdevices[0], exp)
+	if !cmp.Equal(info.Blockdevices, exp) {
+		t.Errorf("parseDiskInfo() = %+v; want %+v", info.Blockdevices, exp)
 	}
 }
 
@@ -90,27 +100,94 @@ func TestGetMainDeviceSize(t *testing.T) {
 	}
 }
 
-func TestMainDevice(t *testing.T) {
+func TestMainDeviceOnly(t *testing.T) {
 	const out = `
 {
 	"blockdevices": [
 		{"name": "loop0", "type": "loop", "hotplug": "0", "size": "536870912000", "state": null},
-		{"name": "mmcblk0d1", "type": "disk", "hotplug": "0", "size": "10000", "state": "running"},
+		{"name": "mmcblk0d1", "type": "disk", "hotplug": "1", "size": "536870912000", "state": null},
+		{"name": "mmcblk0", "type": "disk", "hotplug": "1", "size": "10000", "state": null},
 		{"name": "zram0", "type": "loop", "hotplug": "0", "size": "536870912000", "state": null}
 	]
 }`
+
+	exp := &Blockdevice{
+		Name:    "mmcblk0",
+		Type:    "disk",
+		Hotplug: "1",
+		Size:    10000,
+		State:   "",
+	}
 
 	info, err := parseDiskInfo([]byte(out))
 	if err != nil {
 		t.Fatal("parseDiskInfo() failed: ", err)
 	}
+	info = filterNotAllowedDevices(info)
 
 	dev, err := info.MainDevice()
 	if err != nil {
 		t.Fatal("MainDevice() didn't return a valid main device: ", err)
 	}
 
-	if dev.Name != "mmcblk0d1" {
-		t.Errorf("MainDevice() returned wrong device %+v", dev)
+	if !cmp.Equal(dev, exp) {
+		t.Errorf("MainDevice() = %+v; want %+v", dev, exp)
+	}
+
+	secondary, err := info.SecondaryDevice()
+	if secondary != nil {
+		t.Errorf("SecondaryDevice() returned unexpected device %+v", secondary)
+	}
+}
+
+func TestMainAndSecondaryDeviceOnly(t *testing.T) {
+	const out = `
+{
+	"blockdevices": [
+		{"name": "loop0", "type": "loop", "hotplug": "0", "size": "536870912000", "state": null},
+    {"name":"nvme0n1", "type":"disk", "hotplug": "0", "size":"17179869184", "state":"live"},
+    {"name":"nvme0n2", "type":"disk", "hotplug": "0", "size":"137438953472", "state":"live"},
+		{"name": "zram0", "type": "loop", "hotplug": "0", "size": "536870912000", "state": null}
+	]
+}`
+
+	mainDeviceExp := &Blockdevice{
+		Name:    "nvme0n2",
+		Type:    "disk",
+		Hotplug: "0",
+		Size:    137438953472,
+		State:   "live",
+	}
+
+	secondaryDeviceExp := &Blockdevice{
+		Name:    "nvme0n1",
+		Type:    "disk",
+		Hotplug: "0",
+		Size:    17179869184,
+		State:   "live",
+	}
+
+	info, err := parseDiskInfo([]byte(out))
+	if err != nil {
+		t.Fatal("parseDiskInfo() failed: ", err)
+	}
+	info = filterNotAllowedDevices(info)
+
+	main, err := info.MainDevice()
+	if err != nil {
+		t.Fatal("MainDevice() didn't return a valid main device: ", err)
+	}
+
+	if !cmp.Equal(main, mainDeviceExp) {
+		t.Errorf("MainDevice() = %+v; want %+v", main, mainDeviceExp)
+	}
+
+	secondary, err := info.SecondaryDevice()
+	if secondary == nil {
+		t.Fatal("SecondaryDevice() didn't return a valid secondary device: ", err)
+	}
+
+	if !cmp.Equal(secondary, secondaryDeviceExp) {
+		t.Errorf("SecondaryDevice() = %+v; want %+v", secondary, secondaryDeviceExp)
 	}
 }
