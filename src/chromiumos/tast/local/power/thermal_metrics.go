798b76c821af90250590e069d57baeea3190368c
Matthias Springer <springerm@google.com>
2020-09-09 12:46:30 +0900

    tast-tests: Monitor CPU metrics in power tests.
    Monitor and report CPU usage values in all power/performance tests. CPU
    usage is read from /proc/stat.
    
    BUG=b:168076231
    TEST=tast run DUT arc.PowerIdlePerf
    
    Change-Id: Idfd629c1b9d1d3a19f2de97bb990af68e834603e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2398753
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Tested-by: Matthias Springer <springerm@chromium.org>
    Commit-Queue: Matthias Springer <springerm@chromium.org>

--- src/chromiumos/tast/local/power/thermal_metrics.go
+++ src/chromiumos/tast/local/power/thermal_metrics.go
@@ -42,7 +42,7 @@ func ListSysfsThermalSensors(ctx context.Context) (map[string]string, error) {
 			continue
 		}
 
-		name, err := readLine(path.Join(devPath, "type"))
+		name, err := readFirstLine(path.Join(devPath, "type"))
 		if err != nil {
 			testing.ContextLogf(ctx, "%v is not readable", devPath)
 			continue
