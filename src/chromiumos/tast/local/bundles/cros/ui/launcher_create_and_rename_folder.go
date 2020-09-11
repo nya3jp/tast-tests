44178cb07b43504a177b5fe43d3b9d9b60412189
Seewai Fu <seewaifu@google.com>
2020-09-11 15:15:56 -0700

    tast_tests: Rename folder in the launcher
    Test Chrome OS feature for pinning application onto shelf.
    TestTracker Link: https://testtracker.googleplex.com/testplans/testcase/detail/1763536
    Chromium Issue Link: https://bugs.chromium.org/p/chromium/issues/detail?id=1127137
    
    Manual Test Log for a hana DUT:
    2020/09/11 16:24:26 Command line: tast -verbose run -build=true hana ui.LauncherRenameFolder
    ...
    2020/09/11 16:25:57 ui.LauncherCreateAndRenameFolder  [ PASS ]
    
    Manual Test Log for a octopus DUT:
    2020/09/11 16:26:07 Command line: tast -verbose run -build=true octopus ui.LauncherCreateAndRenameFolder
    ...
    2020/09/11 16:28:55 ui.LauncherCreateAndRenameFolder  [ PASS ]
    
    BUG=chromium:1127137
    TEST=Manual test passed on octopus and hana
    
    Change-Id: I2b02da2e1bb722605dab0b47c2c57699c0b0ead7

---
+++ src/chromiumos/tast/local/bundles/cros/ui/launcher_create_and_rename_folder.go
@@ -0,0 +1,70 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/ui/launcher"
+	"chromiumos/tast/local/chrome/ui/mouse"
+	"chromiumos/tast/testing"
+)
+
+// init adds the test LauncherCreateAndRenameFolder.
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LauncherCreateAndRenameFolder,
+		Desc: "Renaming Folder In Launcher",
+		Contacts: []string{
+			"seewaifu@chromium.org",
+			"kyleshima@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+// LauncherCreateAndRenameFolder tests if launcher handling rename folder correctly.
+func LauncherCreateAndRenameFolder(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// Open the Launcher and go to Apps list page.
+	if err := launcher.OpenExpandedView(ctx, tconn); err != nil {
+		s.Fatal("Failed to open Expanded Application list view: ", err)
+	}
+
+	// Create a folder in launcher by dragging one app on top of another.
+	params := ui.FindParams{ClassName: "ui/app_list/AppListItemView"}
+	icons, err := ui.FindAll(ctx, tconn, params)
+	if err != nil {
+		s.Fatal("Failed to find all in expanded launcher: ", err)
+	}
+	defer icons.Release(ctx)
+	if len(icons) < 2 {
+		s.Fatal("Not enough icons in expanded launcher to perform test")
+	}
+	point0 := icons[0].Location.CenterPoint()
+	point1 := icons[1].Location.CenterPoint()
+	// Drag one app on top of another.
+	if err := mouse.Drag(ctx, tconn, point0, point1, 10*time.Second); err != nil {
+		s.Fatalf("Failed to Drag app %q: %v", icons[0].Name, err)
+	}
+	ui.WaitForLocationChangeCompleted(ctx, tconn)
+
+	if err := launcher.RenameFolder(ctx, tconn, "Unnamed", "NewName"); err != nil {
+		s.Fatal("Failed to rename folder to NewName: ", err)
+	}
+
+}
