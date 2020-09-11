b715dbf4ccf83775e1124b1825c9613c67901081
Callistus <callistus@google.com>
2020-09-11 12:07:04 +1000

    Added loadTimeData checks to managed device tast test for the Help app.
    BUG=b:161658792
    TEST=apps.LaunchHelpAppOnManagedDevice
    
    Change-Id: I724ca91c09b5beb2002f1580d197e097b276f9d2

--- src/chromiumos/tast/local/bundles/cros/apps/helpapp/helpapp.go
+++ src/chromiumos/tast/local/bundles/cros/apps/helpapp/helpapp.go
@@ -7,6 +7,7 @@ package helpapp
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"chromiumos/tast/errors"
@@ -56,6 +57,41 @@ func Launch(ctx context.Context, tconn *chrome.TestConn) error {
 // Exists checks whether the help app exists in the accessiblity tree.
 func Exists(ctx context.Context, tconn *chrome.TestConn) (bool, error) {
 	return ui.Exists(ctx, tconn, helpRootNodeParams)
+}
+
+// LoadTimeData struct for the help app.
+type LoadTimeData struct {
+	IsManagedDevice bool
+}
+
+// GetLoadTimeData returns some of the LoadTimeData fields from the help app.
+func GetLoadTimeData(ctx context.Context, cr *chrome.Chrome) (*LoadTimeData, error) {
+	// Establish a Chrome connection to the Help app and wait for it to finish loading.
+	helpAppConn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL("chrome-untrusted://help-app/"))
+
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get connection to help app")
+	}
+	defer helpAppConn.Close()
+
+	if err := helpAppConn.WaitForExpr(ctx, `document.readyState === "complete"`); err != nil {
+		return nil, errors.Wrap(err, "failed to wait for help app to finish loading")
+	}
+
+	isManagedDevice, err := getLoadTimeBoolean(ctx, helpAppConn, "isManagedDevice")
+	if err != nil {
+		return nil, err
+	}
+
+	return &LoadTimeData{IsManagedDevice: isManagedDevice}, nil
+}
+
+func getLoadTimeBoolean(ctx context.Context, helpAppConn *chrome.Conn, key string) (bool, error) {
+	val := false
+	if err := helpAppConn.Eval(ctx, fmt.Sprintf("window.loadTimeData.getBoolean(%q)", key), &val); err != nil {
+		return false, errors.Wrapf(err, "failed to evaluate window.loadTimeData.getBoolean(%q)", key)
+	}
+	return val, nil
 }
 
 // IsPerkShown checks if the perks tab is displayed or not.
