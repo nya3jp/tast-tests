21f78ec4b1f3beb48b7ecfb175ebf777e8357abc
Drew Davenport <ddavenport@chromium.org>
2020-09-12 00:59:26 -0600

    security.SELinuxFilesSystem: Make some tests informational
    Recursive scans under /sys/devices/system/cpu/... are flaky on some
    boards using 5.4 kernel. Move these test cases to
    security.SELinuxFilesSystemInformational in the meantime.
    
    BUG=chromium:1096102
    TEST=tast run $dut security.SELinuxFilesSystem*
    
    Change-Id: Id6b891d8f570d81ef04f7c5d4cc973b0f82a83a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2407564
    Tested-by: Drew Davenport <ddavenport@chromium.org>
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Alex Levin <levinale@google.com>
    Reviewed-by: Alexander Hartl <alexanderhartl@google.com>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/security/selinux_files_system.go
+++ src/chromiumos/tast/local/bundles/cros/security/selinux_files_system.go
@@ -6,8 +6,6 @@ package security
 
 import (
 	"context"
-	"os"
-	"syscall"
 
 	"chromiumos/tast/local/bundles/cros/security/selinux"
 	"chromiumos/tast/local/chrome"
@@ -26,29 +24,6 @@ func init() {
 }
 
 func SELinuxFilesSystem(ctx context.Context, s *testing.State) {
-	type rwFilter int
-	const (
-		readonly rwFilter = iota
-		writable
-	)
-	systemCPUFilter := func(writableFilter rwFilter) selinux.FileLabelCheckFilter {
-		return func(p string, fi os.FileInfo) (skipFile, skipSubdir selinux.FilterResult) {
-			mode := fi.Mode()
-			// Domain has search to both sysfs and sysfs_devices_system_cpu.
-			if mode.IsDir() {
-				return selinux.Skip, selinux.Check
-			}
-
-			isWritable := mode.IsRegular() && ((mode.Perm() & (syscall.S_IWUSR | syscall.S_IWGRP | syscall.S_IWOTH)) > 0)
-			// Writable files
-			if isWritable != (writableFilter == writable) {
-				return selinux.Skip, selinux.Check
-			}
-
-			return selinux.Check, selinux.Check
-		}
-	}
-
 	gpuDevices, err := selinux.GpuDevices()
 	if err != nil {
 		// Error instead of Fatal to continue test other testcases .
@@ -119,8 +94,6 @@ func SELinuxFilesSystem(ctx context.Context, s *testing.State) {
 			// we don't have anything special of conntrack files than others. conntrack slab cache changes when connections established or closes, and may cause flakiness.
 			"/sys/kernel/slab/nf_conntrack_.*",
 		}, gpuDevices...), crosEcIioDevices...))},
-		{Path: "/sys/devices/system/cpu", Context: "sysfs", Recursive: true, Filter: systemCPUFilter(writable)},
-		{Path: "/sys/devices/system/cpu", Context: "sysfs_devices_system_cpu", Recursive: true, Filter: systemCPUFilter(readonly)},
 		{Path: "/sys/fs/cgroup", Context: "cgroup", Recursive: true, Filter: selinux.IgnorePathButNotContents("/sys/fs/cgroup")},
 		{Path: "/sys/fs/cgroup", Context: "tmpfs"},
 		{Path: "/sys/fs/pstore", Context: "pstorefs"},
