83002fa48389dd6a8ecab1a0c6f8fd2310393bc0
Greg Edelston <gredelston@google.com>
2020-09-14 11:37:26 -0600

    firmware.Config: Allow floats for duration values
    Previously, duration values were imported directly as time.Duration, an
    integer-based field. This meant that non-integer durations would not be
    marshalled properly. For example, the default value of
    ECBootToPwrButton, 0.5, would be truncated to 0.
    
    This CL enables floating-point values.
    See attached bug more more discussion of the design.
    
    BUG=b:168487916
    TEST=config_test.go
    TEST=firmware.Config
    
    Change-Id: I9f25a7aa6bd0d46a621ccb3b3dd58e4c58f8a74c

--- src/chromiumos/tast/remote/firmware/config.go
+++ src/chromiumos/tast/remote/firmware/config.go
@@ -179,17 +179,32 @@ func ConfigDatafiles() []string {
 // Config contains platform-specific attributes.
 // Fields are documented in autotest/server/cros/faft/configs/DEFAULTS.json.
 type Config struct {
-	Platform             string                     `json:"platform"`
-	Parent               string                     `json:"parent"`
-	ECCapability         []ECCapability             `json:"ec_capability"`
-	ModeSwitcherType     ModeSwitcherType           `json:"mode_switcher_type"`
-	PowerButtonDevSwitch bool                       `json:"power_button_dev_switch"`
-	RecButtonDevSwitch   bool                       `json:"rec_button_dev_switch"`
-	FirmwareScreen       time.Duration              `json:"firmware_screen"`
-	DelayRebootToPing    time.Duration              `json:"delay_reboot_to_ping"`
-	ConfirmScreen        time.Duration              `json:"confirm_screen"`
-	USBPlug              time.Duration              `json:"usb_plug"`
-	Models               map[string]json.RawMessage `json:"models"`
+	Platform             string           `json:"platform"`
+	Parent               string           `json:"parent"`
+	ECCapability         []ECCapability   `json:"ec_capability"`
+	ModeSwitcherType     ModeSwitcherType `json:"mode_switcher_type"`
+	PowerButtonDevSwitch bool             `json:"power_button_dev_switch"`
+	RecButtonDevSwitch   bool             `json:"rec_button_dev_switch"`
+
+	// Raw duration fields represent a quantity of seconds.
+	// They are used during NewConfig to populate actual duration fields, which are defined below.
+	// Generally, these raw fields should not be accessed by tests and libraries.
+  	// Nevertheless, the raw fields must be exported in order for them to be set by json.Unmarshal.
+	RawConfirmScreen     float64 `json:"confirm_screen"`
+	RawDelayRebootToPing float64 `json:"delay_reboot_to_ping"`
+	RawECBootToPwrButton float64 `json:"ec_boot_to_pwr_button"`
+	RawFirmwareScreen    float64 `json:"firmware_screen"`
+	RawUSBPlug           float64 `json:"usb_plug"`
+
+	// Actual duration fields are populated during NewConfig based on raw JSON duration values, defined above.
+	ConfirmScreen     time.Duration
+	DelayRebootToPing time.Duration
+	ECBootToPwrButton time.Duration
+	FirmwareScreen    time.Duration
+	USBPlug           time.Duration
+
+	// Models maps DUT model names to overriding config JSON objects.
+	Models map[string]json.RawMessage `json:"models"`
 }
 
 // CfgPlatformFromLSBBoard interprets a board name that would come from /etc/lsb-release, and returns the name of the platform whose config should be loaded.
@@ -259,6 +274,14 @@ func NewConfig(configDataDir, board, model string) (*Config, error) {
 			return nil, errors.Wrapf(err, "failed to unmarshal model-level config for %q", model)
 		}
 	}
+
+	// Populate actual durations based on raw JSON values.
+	cfg.ConfirmScreen = toSeconds(cfg.RawConfirmScreen)
+	cfg.DelayRebootToPing = toSeconds(cfg.RawDelayRebootToPing)
+	cfg.ECBootToPwrButton = toSeconds(cfg.RawECBootToPwrButton)
+	cfg.FirmwareScreen = toSeconds(cfg.RawFirmwareScreen)
+	cfg.USBPlug = toSeconds(cfg.RawUSBPlug)
+
 	return &cfg, nil
 }
 
@@ -270,4 +293,10 @@ func (cfg *Config) HasECCapability(ecc ECCapability) bool {
 		}
 	}
 	return false
+}
+
+// toSeconds casts a float64 to a time.Duration, in seconds.
+func toSeconds(f float64) time.Duration {
+  	// The 1000* factor enables a non-integer f to be cast as a time.Duration, an integer field.
+	return time.Duration(1000*f) * time.Millisecond
 }
