4c114e46b8e6144e72a1cd91fd46968e766596ff
Kimiyuki Onaka <kimiyuki@google.com>
2020-09-14 12:01:22 +0900

    tast-tests: Update arc.NativeCrash to check dump files for crash_sender
    The previous tests check only for dump files which are directly
    created by arc-native-crash-collector{32|64}, which is the first
    step of crash reporting. They did so because the later steps
    were not implemented yet at the time. This commit updates the
    tests to check the final dump files to test all steps of crash
    reporting.
    
    Now, some parts of these tests are shared with arc.AppCrash.
    
    BUG=b:136697350
    TEST=tast run my_crbook arc.NativeCrash.{mock,real}_consent arc.AppCrash.vm_mock_consent
    TEST=tast run my_crbook arc.AppCrash.{mock,real}_consent
    
    Change-Id: Ia31611b3255351de4ae6fb348a98fb4ede810784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2409112
    Tested-by: Kimiyuki Onaka <kimiyuki@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Commit-Queue: Kimiyuki Onaka <kimiyuki@google.com>

--- src/chromiumos/tast/local/bundles/cros/arc/app_crash.go
+++ src/chromiumos/tast/local/bundles/cros/arc/app_crash.go
@@ -6,13 +6,11 @@ package arc
 
 import (
 	"context"
-	"io/ioutil"
-	"os"
 	"path/filepath"
 	"strings"
 
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/arccrash"
 	"chromiumos/tast/local/crash"
 	"chromiumos/tast/local/cryptohome"
 	"chromiumos/tast/testing"
@@ -40,79 +38,6 @@ func init() {
 			Val:               crash.MockConsent,
 		}},
 	})
-}
-
-type buildProp struct {
-	device      string
-	board       string
-	cpuAbi      string
-	fingerprint string
-}
-
-func getProp(ctx context.Context, a *arc.ARC, key string) (string, error) {
-	val, err := a.GetProp(ctx, key)
-	if err != nil {
-		return "", errors.Wrapf(err, "failed to get %s", key)
-	}
-	if val == "" {
-		return "", errors.Errorf("%s is empty", key)
-	}
-	return val, err
-}
-
-func getBuildProp(ctx context.Context, a *arc.ARC) (*buildProp, error) {
-	device, err := getProp(ctx, a, "ro.product.device")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get device")
-	}
-	board, err := getProp(ctx, a, "ro.product.board")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get board")
-	}
-	cpuAbi, err := getProp(ctx, a, "ro.product.cpu.abi")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get cpu_abi")
-	}
-	fingerprint, err := getProp(ctx, a, "ro.build.fingerprint")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get fingerprint")
-	}
-
-	return &buildProp{
-		device:      device,
-		board:       board,
-		cpuAbi:      cpuAbi,
-		fingerprint: fingerprint,
-	}, nil
-}
-
-func validateBuildProp(ctx context.Context, meta string, bp *buildProp) (bool, error) {
-	f, err := os.Open(meta)
-	if err != nil {
-		return false, errors.Wrap(err, "failed to open meta file")
-	}
-	defer f.Close()
-
-	b, err := ioutil.ReadAll(f)
-	if err != nil {
-		return false, errors.Wrap(err, "failed to read meta file")
-	}
-
-	lines := strings.Split(string(b), "\n")
-	contains := func(x string) bool {
-		for _, l := range lines {
-			if x == l {
-				return true
-			}
-		}
-		testing.ContextLogf(ctx, "Missing %q", x)
-		return false
-	}
-
-	return contains("upload_var_device="+bp.device) &&
-		contains("upload_var_board="+bp.board) &&
-		contains("upload_var_cpu_abi="+bp.cpuAbi) &&
-		contains("upload_var_arc_version="+bp.fingerprint), nil
 }
 
 func AppCrash(ctx context.Context, s *testing.State) {
@@ -157,20 +82,9 @@ func AppCrash(ctx context.Context, s *testing.State) {
 		base + crash.LogExt, metaFileName, base + crash.InfoExt,
 	})
 	if err != nil {
-		s.Fatal("didn't find files: ", err)
+		s.Fatal("Didn't find files: ", err)
 	}
 	defer crash.RemoveAllFiles(ctx, files)
-
-	bp, err := getBuildProp(ctx, a)
-	if err != nil {
-		// Upload /system/build.prop to invetigate because getprop sometimes fails to get the device name
-		// even though the device name should always exists.
-		// See details in https://bugs.chromium.org/p/chromium/issues/detail?id=1039512#c16
-		if err := a.PullFile(ctx, "/system/build.prop", filepath.Join(s.OutDir(), "build.prop")); err != nil {
-			s.Error("Failed to get build.prop: ", err)
-		}
-		s.Fatal("Failed to get BuildProperty: ", err)
-	}
 
 	metaFiles := files[metaFileName]
 	if len(metaFiles) > 1 {
@@ -179,12 +93,22 @@ func AppCrash(ctx context.Context, s *testing.State) {
 	}
 	// WaitForCrashFiles guarantees that there will be a match for all regexes if it succeeds,
 	// so this must exist.
-	isValid, err := validateBuildProp(ctx, metaFiles[0], bp)
+	metaFile := metaFiles[0]
+
+	s.Log("Validating the meta file")
+	bp, err := arccrash.GetBuildProp(ctx, a)
+	if err != nil {
+		if err := arccrash.UploadSystemBuildProp(ctx, a, s.OutDir()); err != nil {
+			s.Error("Failed to get build.prop: ", err)
+		}
+		s.Fatal("Failed to get BuildProperty: ", err)
+	}
+	isValid, err := arccrash.ValidateBuildProp(ctx, metaFile, bp)
 	if err != nil {
 		s.Fatal("Failed to validate meta file: ", err)
 	}
 	if !isValid {
 		s.Error("validateBuildProp failed. Saving meta file")
-		crash.MoveFilesToOut(ctx, s.OutDir(), metaFiles[0])
+		crash.MoveFilesToOut(ctx, s.OutDir(), metaFile)
 	}
 }
