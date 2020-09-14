8eda16ebdd17ad7f8879b5f702ec51a7bfab4f0e
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-09-14 16:58:55 -0700

    Run UIAutomator Test Using Tast For Instagram app
    BUG=b:167704660
    TEST=tast run <eve> arcappcompat.Instagram
    
    Tast Test for Instagram app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Cq-Depend: chrome-internal:3269818
    Change-Id: Iadc1277d5e61ed4677680e7502f1adfa0fa75454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2411410
    Tested-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Commit-Queue: Meenakshi Thiyagarajan <mthiyagarajan@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/instagram.go
@@ -0,0 +1,190 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForInstagram = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForInstagram},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForInstagram = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForInstagram},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Instagram,
+		Desc:         "Functional test for Instagram that installs the app also verifies it is logged in and that the main page is open, checks Instagram correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForInstagram,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForInstagram,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForInstagram,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForInstagram,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password", "arcappcompat.Instagram.username", "arcappcompat.Instagram.password"},
+	})
+}
+
+// Instagram test uses library for opting into the playstore and installing app.
+// Checks Instagram correctly changes the window states in both clamshell and touchview mode.
+func Instagram(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.instagram.android"
+		appActivity = ".activity.MainTabActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForInstagram verifies Instagram is logged in and
+// verify Instagram reached main activity page of the app.
+func launchAppForInstagram(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		dismissButtonID     = "android:id/button2"
+		enterEmailAddressID = "com.instagram.android:id/login_username"
+		loginButtonID       = "com.instagram.android:id/log_in_button"
+		loginID             = "com.instagram.android:id/button_text"
+		notNowID            = "android:id/autofill_save_no"
+		passwordID          = "com.instagram.android:id/password"
+		profileID           = "com.instagram.android:id/profile_tab"
+	)
+
+	// Check for login button.
+	loginButton := d.Object(ui.ID(loginButtonID))
+	if err := loginButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("LoginButton doesn't exist: ", err)
+	} else if err := loginButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on loginButton: ", err)
+	}
+
+	// Enter email id.
+	enterEmailAddress := d.Object(ui.ID(enterEmailAddressID))
+	if err := enterEmailAddress.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("EnterEmailAddress does not exist: ", err)
+	} else if err := enterEmailAddress.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterEmailAddress: ", err)
+	}
+
+	// Click on emailid text field until the emailid text field is focused.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if emailIDFocused, err := enterEmailAddress.IsFocused(ctx); err != nil {
+			return errors.New("email text field not focused yet")
+		} else if !emailIDFocused {
+			enterEmailAddress.Click(ctx)
+			return errors.New("email text field not focused yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.LongUITimeout}); err != nil {
+		s.Fatal("Failed to focus EmailId: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	username := s.RequiredVar("arcappcompat.Instagram.username")
+	if err := kb.Type(ctx, username); err != nil {
+		s.Fatal("Failed to enter username: ", err)
+	}
+	s.Log("Entered username")
+
+	// Enter password.
+	enterPassword := d.Object(ui.ID(passwordID))
+	if err := enterPassword.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("EnterPassword does not exist: ", err)
+	} else if err := enterPassword.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterPassword: ", err)
+	}
+	// Click on password text field until the password text field is focused.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if pwdFocused, err := enterPassword.IsFocused(ctx); err != nil {
+			return errors.New("password text field not focused yet")
+		} else if !pwdFocused {
+			enterPassword.Click(ctx)
+			return errors.New("password text field not focused yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.LongUITimeout}); err != nil {
+		s.Fatal("Failed to focus password: ", err)
+	}
+
+	password := s.RequiredVar("arcappcompat.Instagram.password")
+	if err := kb.Type(ctx, password); err != nil {
+		s.Fatal("Failed to enter password: ", err)
+	}
+	s.Log("Entered password")
+
+	// Click on signIn Button until not now button exist.
+	signInButton := d.Object(ui.ID(loginID))
+	notNowButton := d.Object(ui.ID(notNowID))
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := notNowButton.Exists(ctx); err != nil {
+			signInButton.Click(ctx)
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.LongUITimeout}); err != nil {
+		s.Log("notNowButton doesn't exist: ", err)
+	} else if err := notNowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on notNowButton: ", err)
+	}
+
+	// Click on dimiss button to save password.
+	dimissButton := d.Object(ui.ID(dismissButtonID))
+	if err := dimissButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("dimissButton doesn't exists: ", err)
+	} else if err := dimissButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on dimissButton: ", err)
+	}
+
+	// Check for profile icon.
+	profileIcon := d.Object(ui.ID(profileID))
+	if err := profileIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("ProfileIcon doesn't exists: ", err)
+	}
+
+}
