517a3060037105fb2e5d5473c336952f7ef9cadd
Saurabh Nijhara <snijhara@google.com>
2020-09-14 17:29:59 +0200

    tast-tests: add test for URL popups allow/block lists
    This CL adds a tests for PopupsBlockedForUrls and
    PopupsAllowedForUrls user policies. The test verifies that
    when a URL is specified in the allow list, then it overrides the
    default block value from DefaultPopupsSetting policy and popups are
    allowed. Similarly when a URL is specified in the block list, then
    it overrides the default allow value and popups are blocked.
    
    BUG=chromium:1125587, chromium:1125586
    TEST=tast run <ip> policy.PopupsForURLCheck
    
    Change-Id: Icffa74d2566844e5a97092408035551f3b3b8bcf

---
+++ src/chromiumos/tast/local/bundles/cros/policy/popups_for_url_check.go
@@ -0,0 +1,171 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+type popupsSettingTestTable struct {
+	name      string          // name is the subtest name.
+	wantTitle string          // wantTitle is the expected title of the window after test is run with policies applied.
+	policies  []policy.Policy // policies is a list of PopupsBlockedForUrls, PopupsAllowedForUrls and DefaultPopupsSetting policies to update before checking popups.
+}
+
+// TODO(crbug.com/1125586): investigate using an easier filter like "*" in the allow/deny-listing policies along with DefaultPopupsSetting policy.
+const filterPopupsURL = "http://*/popups_for_url_check_index.html"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PopupsForURLCheck,
+		Desc: "Checks the behavior of popups on URL allow/deny-listing user policies",
+		Contacts: []string{
+			"snijhara@google.com", // Test author
+			"gabormagda@google.com",
+			"alexanderhartl@google.com",
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+		Data:         []string{"popups_for_url_check_index.html", "popups_for_url_check_popup.html"},
+		Params: []testing.Param{
+			{
+				Name: "default",
+				Val: []popupsSettingTestTable{
+					{
+						name:      "allowed",
+						wantTitle: "Popups allowed",
+						policies:  []policy.Policy{&policy.DefaultPopupsSetting{Val: 1}}, // 1: Popups are allowed
+					},
+					{
+						name:      "blocked",
+						wantTitle: "Popups blocked",
+						policies:  []policy.Policy{&policy.DefaultPopupsSetting{Val: 2}}, // 2: Popups are blocked
+					},
+					{
+						name:      "unset",
+						wantTitle: "Popups blocked",
+						policies:  []policy.Policy{&policy.DefaultPopupsSetting{Stat: policy.StatusUnset}},
+					},
+				},
+			},
+			{
+				Name: "allowlist",
+				Val: []popupsSettingTestTable{
+					{
+						name:      "blocklist_unset_default_block",
+						wantTitle: "Popups allowed",
+						policies: []policy.Policy{
+							&policy.PopupsBlockedForUrls{Stat: policy.StatusUnset},
+							&policy.PopupsAllowedForUrls{Val: []string{filterPopupsURL}},
+							&policy.DefaultPopupsSetting{Val: 2}, // 2: Popups are blocked by default
+						},
+					},
+					{
+						name:      "blocklist_set_default_block",
+						wantTitle: "Popups allowed",
+						policies: []policy.Policy{
+							&policy.PopupsBlockedForUrls{Val: []string{"https://chromium.org", "http://example.org"}},
+							&policy.PopupsAllowedForUrls{Val: []string{filterPopupsURL}},
+							&policy.DefaultPopupsSetting{Val: 2}, // 2: Popups are blocked by default
+						},
+					},
+				},
+			},
+			{
+				Name: "blocklist",
+				Val: []popupsSettingTestTable{
+					{
+						name:      "allowlist_unset_default_allow",
+						wantTitle: "Popups blocked",
+						policies: []policy.Policy{
+							&policy.PopupsBlockedForUrls{Val: []string{filterPopupsURL}},
+							&policy.PopupsAllowedForUrls{Stat: policy.StatusUnset},
+							&policy.DefaultPopupsSetting{Val: 1}, // 1: Popups are allowed by default
+						},
+					},
+					{
+						name:      "allowlist_set_default_allow",
+						wantTitle: "Popups blocked",
+						policies: []policy.Policy{
+							&policy.PopupsBlockedForUrls{Val: []string{filterPopupsURL}},
+							&policy.PopupsAllowedForUrls{Val: []string{"https://chromium.org", "http://example.org"}},
+							&policy.DefaultPopupsSetting{Val: 1}, // 1: Popups are allowed by default
+						},
+					},
+				},
+			},
+		},
+	})
+}
+
+func PopupsForURLCheck(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	tcs, ok := s.Param().([]popupsSettingTestTable)
+	if !ok {
+		s.Fatal("Failed to convert test cases to the desired type")
+	}
+
+	for _, tc := range tcs {
+		s.Run(ctx, tc.name, func(ctx context.Context, s *testing.State) {
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, tc.policies); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			conn, err := cr.NewConn(ctx, server.URL+"/popups_for_url_check_index.html")
+			if err != nil {
+				s.Fatal("Creating renderer failed: ", err)
+			}
+			defer conn.Close()
+
+			tconn, err := cr.TestAPIConn(ctx)
+			if err != nil {
+				s.Fatal("Failed to get TestConn: ", err)
+			}
+
+			// Wait until the popup window is opened.
+			if err := testing.Poll(ctx, func(ctx context.Context) error {
+				windows, err := ash.GetAllWindows(ctx, tconn)
+				if err != nil {
+					return testing.PollBreak(errors.Wrap(err, "failed to get windows"))
+				}
+
+				for _, window := range windows {
+					if strings.Contains(window.Title, tc.wantTitle) {
+						return nil
+					}
+				}
+				return errors.New("failed to find expected window title")
+			}, &testing.PollOptions{
+				Timeout: 15 * time.Second,
+			}); err != nil {
+				s.Errorf("Failed to find window title %q", tc.wantTitle)
+			}
+		})
+	}
+}
