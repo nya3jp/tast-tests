622f8a7eae10b111b216289c63184106f5b2bdc7
alexanderhartl <alexanderhartl@google.com>
2020-09-14 18:24:56 +0000

    tast-tests: add ShowLogoutButtonInTray
    Add the policy.ShowLogoutButtonInTray tast test.
    
    BUG=chromium:1125600
    TEST=tast run <ip> policy.ShowLogoutButtonInTray
    
    Change-Id: Iec5d04df3232732404b85bed61bc5615b4b0a924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2409965
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Reviewed-by: Yen-lin Lai <yenlinlai@google.com>
    Tested-by: Alexander Hartl <alexanderhartl@google.com>
    Commit-Queue: Alexander Hartl <alexanderhartl@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/policy/show_logout_button_in_tray.go
@@ -0,0 +1,86 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ShowLogoutButtonInTray,
+		Desc: "Behavior of ShowLogoutButtonInTray policy, check if a logout button is shown based on the value of the policy",
+		Contacts: []string{
+			"alexanderhartl@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+	})
+}
+
+func ShowLogoutButtonInTray(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	for _, param := range []struct {
+		name       string
+		wantButton bool                           // wantButton is the expected existence of the "Sign out" button.
+		policy     *policy.ShowLogoutButtonInTray // policy is the policy we test.
+	}{
+		{
+			name:       "unset",
+			wantButton: false,
+			policy:     &policy.ShowLogoutButtonInTray{Stat: policy.StatusUnset},
+		},
+		{
+			name:       "don't show",
+			wantButton: false,
+			policy:     &policy.ShowLogoutButtonInTray{Val: false},
+		},
+		{
+			name:       "show",
+			wantButton: true,
+			policy:     &policy.ShowLogoutButtonInTray{Val: true},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.policy}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			// Find the button node.
+			params := ui.FindParams{
+				Role: ui.RoleTypeButton,
+				Name: "Sign out",
+			}
+			if err := ui.WaitUntilExists(ctx, tconn, params, 30*time.Second); err != nil && !errors.Is(err, ui.ErrNodeDoesNotExist) {
+				s.Fatal("Failed to find sign out button: ", err)
+			} else if b := !errors.Is(err, ui.ErrNodeDoesNotExist); param.wantButton != b {
+				s.Errorf("Unexpected existence of sign out button: got %t; want %t", b, param.wantButton)
+			}
+		})
+	}
+}
