25525af89ce2c6fd866c5645f746992d7e0b62d5
David Staessens <dstaessens@google.com>
2020-09-15 16:31:31 +0900

    arc.PowerVideoEncodePerf_vm: Add video encode power test for ARCVM.
    This CL adds a new arc.PowerVideoEncodePerf.h264_1080p_i420_vm test
    for ARCVM that measures power drain while encoding a 1080p video.
    
    Note: Submit after V4L2 encoder is enabled by default.
    
    BUG=b:161659032
    TEST=tast run DUT arc.PowerVideoEncodePerf.h264_1080p_i420_vm on hatch-arc-r
    
    Change-Id: Idff0f7d53eb3fb2b3e6b1d3ad1f6710702eae518

--- src/chromiumos/tast/local/bundles/cros/arc/power_video_encode_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/power_video_encode_perf.go
@@ -55,6 +55,15 @@ func init() {
 			},
 			ExtraData:         []string{video.Crowd1080P.Name},
 			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name: "h264_1080p_i420_vm",
+			Val: encoding.TestOptions{
+				Profile:     videotype.H264Prof,
+				Params:      video.Crowd1080P,
+				PixelFormat: videotype.I420,
+			},
+			ExtraData:         []string{video.Crowd1080P.Name},
+			ExtraSoftwareDeps: []string{"android_vm"},
 		}},
 	})
 }
