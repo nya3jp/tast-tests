ad4904906bb2c9f34feba7723b566bf896c1ee9d
Andrew Luo <aluo@chromium.org>
2020-09-16 13:21:29 -0700

    Add ability to get GBB flags.
    BUG=b:166162128
    TEST=~/trunk/src/platform/tast/fast_build.sh -t chromiumos/tast/local/firmware/bios; tast run <DUT>  firmware.BiosService
    
    Change-Id: Ieea669426251b6417d53d20d4f4e1ec62a628a20

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/bios_service.go
@@ -0,0 +1,43 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+
+	//"github.com/golang/protobuf/ptypes/empty"
+	"chromiumos/tast/rpc"
+	pb "chromiumos/tast/services/cros/firmware"
+	"chromiumos/tast/testing"
+	empty "github.com/golang/protobuf/ptypes/empty"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        BiosService,
+		Desc:        "Verfies GBB flags state can be obtained from the DUT",
+		Contacts:    []string{"cros-fw-engprod@google.com"},
+		ServiceDeps: []string{"tast.cros.firmware.BiosService"},
+		Attr:        []string{"group:mainline", "informational"},
+	})
+}
+
+func BiosService(ctx context.Context, s *testing.State) {
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	bs := pb.NewBiosServiceClient(cl.Conn)
+
+	res, err := bs.GetGBBFlags(ctx, &empty.Empty{})
+
+	if err != nil {
+		s.Fatal("GetGBBFlags failed: ", err)
+	}
+
+	s.Log("Current GBB flags: ", res)
+}
