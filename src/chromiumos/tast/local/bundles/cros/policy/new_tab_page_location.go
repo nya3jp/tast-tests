5594505c25c945aa2a911de7969558d5f234fe55
Miriam Polzer <mpolzer@google.com>
2020-09-16 17:14:23 +0200

    tast-tests: add policy.NewTabPageLocation
    Add test for the NewTabPageLocation policy to replace the autotest.
    
    BUG=chromium:1125579
    TEST=tast run <ip> policy.NewTabPageLocation
    
    Change-Id: I8b6369c85c9adeaf478cee02234f5bac96892123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2414448
    Tested-by: Miriam Polzer <mpolzer@google.com>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Miriam Polzer <mpolzer@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/policy/new_tab_page_location.go
@@ -0,0 +1,79 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: NewTabPageLocation,
+		Desc: "Behavior of the NewTabPageLocation policy",
+		Contacts: []string{
+			"mpolzer@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+	})
+}
+
+func NewTabPageLocation(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+	for _, tc := range []struct {
+		name  string
+		value *policy.NewTabPageLocation
+	}{
+		{
+			name:  "set",
+			value: &policy.NewTabPageLocation{Val: "chrome://policy/"},
+		},
+		{
+			name:  "unset",
+			value: &policy.NewTabPageLocation{Stat: policy.StatusUnset},
+		},
+	} {
+		s.Run(ctx, tc.name, func(ctx context.Context, s *testing.State) {
+			// If the NewTabPageLocation policy is set, when a new tab is opened,
+			// the configured page should be loaded. Otherwise, the new tab page is
+			// loaded.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			if err := policyutil.ServeAndRefresh(ctx, fdms, cr, []policy.Policy{tc.value}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			conn, err := cr.NewConn(ctx, "chrome://newtab/")
+			if err != nil {
+				s.Fatal("Failed to connect to chrome: ", err)
+			}
+			defer conn.Close()
+
+			var url string
+			if err := conn.Eval(ctx, `document.URL`, &url); err != nil {
+				s.Fatal("Could not read URL: ", err)
+			}
+
+			if tc.value.Stat != policy.StatusUnset {
+				if url != tc.value.Val {
+					s.Errorf("New tab navigated to %s, expected %s", url, tc.value.Val)
+				}
+				// Depending on test flags the new tab page url might be one of the following.
+			} else if url != "chrome://new-tab-page/" && url != "chrome://newtab/" && url != "chrome-search://local-ntp/local-ntp.html" {
+				s.Errorf("New tab navigated to %s, expected the new tab page", url)
+			}
+		})
+	}
+}
