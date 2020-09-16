8e1c60b4ea2d54429c581fb23489c66623c74974
alexanderhartl <alexanderhartl@google.com>
2020-09-16 15:56:50 +0000

    tast-tests: add ScreenBrightnessPercent
    Add the policy.ScreenBrightnessPercent tast test.
    
    BUG=chromium:1125596
    TEST=tast run <ip> policy.ScreenBrightnessPercent
    
    Change-Id: I0473c5a6c9333e3f9d4cc2e642272913aeb68fda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2414449
    Tested-by: Alexander Hartl <alexanderhartl@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Alexander Hartl <alexanderhartl@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/policy/screen_brightness_percent.go
@@ -0,0 +1,120 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ScreenBrightnessPercent,
+		Desc: "Test behavior of ScreenBrightnessPercent policy: check if the screen brightness matches the value of the policy",
+		Contacts: []string{
+			"alexanderhartl@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+	})
+}
+
+func ScreenBrightnessPercent(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	for _, param := range []struct {
+		name           string
+		wantBrightness string                          // wantBrightness is the expected brightness value.
+		policy         *policy.ScreenBrightnessPercent // policy is the policy we test.
+	}{
+		{
+			name:           "brightness_1",
+			wantBrightness: "83%",
+			policy: &policy.ScreenBrightnessPercent{
+				Val: &policy.ScreenBrightnessPercentValue{
+					BrightnessAC:      83,
+					BrightnessBattery: 83,
+				},
+			},
+		},
+		{
+			name:           "brightness_2",
+			wantBrightness: "47%",
+			policy: &policy.ScreenBrightnessPercent{
+				Val: &policy.ScreenBrightnessPercentValue{
+					BrightnessAC:      47,
+					BrightnessBattery: 47,
+				},
+			},
+		},
+		{
+			name:           "unset",
+			wantBrightness: "47%", // In the unset case the last set brightness is expected.
+			policy:         &policy.ScreenBrightnessPercent{Stat: policy.StatusUnset},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.policy}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			// Find the Status tray node and click to open it.
+			paramsST := ui.FindParams{
+				ClassName: "ash/StatusAreaWidgetDelegate",
+			}
+			nodeST, err := ui.FindWithTimeout(ctx, tconn, paramsST, 15*time.Second)
+			if err != nil {
+				s.Fatal("Failed to find Status tray: ", err)
+			}
+			defer nodeST.Release(ctx)
+
+			if err := nodeST.LeftClick(ctx); err != nil {
+				s.Fatal("Failed to open Status tray: ", err)
+			}
+			defer func() {
+				// Close the Status tray again, otherwise the next subtest won't find it.
+				if err := nodeST.LeftClick(ctx); err != nil {
+					s.Fatal("Failed to close Status tray: ", err)
+				}
+			}()
+
+			// Find the Brightness slider.
+			paramsBS := ui.FindParams{
+				Role: ui.RoleTypeSlider,
+				Name: "Brightness",
+			}
+			nodeBS, err := ui.FindWithTimeout(ctx, tconn, paramsBS, 15*time.Second)
+			if err != nil {
+				s.Fatal("Failed to find Status tray: ", err)
+			}
+			defer nodeBS.Release(ctx)
+
+			if nodeBS.Value != param.wantBrightness {
+				s.Errorf("Unexpected brightness set: got %s; want %s", nodeBS.Value, param.wantBrightness)
+			}
+
+		})
+	}
+}
