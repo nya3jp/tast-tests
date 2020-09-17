b3dc7b58cff64b260e6fcb8575ca855ad4fd7589
Oleh Lamzin <lamzin@google.com>
2020-09-17 17:26:57 +0200

    Introduce DisableScreenshots policy test
    Port autotest test for DisableScreenshots policy. Check whether
    screenshot can be taken by pressing hotkeys.
    
    Introduce helper functions into ash packages:
      - CloseNotifications
      - WaitForNotification
    
    BUG=chromium:1125556
    TEST=in order to test new test itself, run:
         tast run localhost:9999 policy.DisableScreenshots
    TEST=in order to test that CloseNotifications can close multiple
         notifications, do:
         1. rename DisableScreenshots to Screenshots
         2. run DownloadRestrictions before Screenshots, because
            DownloadRestrictions lefts 2 notifications in the tray, so we
    	can try close them:
    	tast run localhost:9999 policy.DownloadRestrictions \
    	  policy.Screenshots
    
    Change-Id: I20f0ed1c75974fa5ff0e6653075ae58bfade0292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2416285
    Tested-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>

--- src/chromiumos/tast/local/chrome/ash/notification.go
+++ src/chromiumos/tast/local/chrome/ash/notification.go
@@ -6,12 +6,14 @@ package ash
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/local/chrome/ui/mouse"
+	"chromiumos/tast/testing"
 )
 
 // Notification corresponds to the "Notification" defined in
@@ -59,4 +61,101 @@ func HideAllNotifications(ctx context.Context, tconn *chrome.TestConn) error {
 		return errors.Wrap(err, "failed to click the tray button")
 	}
 	return nil
+}
+
+// CloseNotifications clicks on the tray button to show the system tray button,
+// clicks close button on each notification and clicks on the tray button
+// to hide the system tray button.
+func CloseNotifications(ctx context.Context, tconn *chrome.TestConn) error {
+	trayButton, err := ui.Find(ctx, tconn, ui.FindParams{Role: ui.RoleTypeButton, ClassName: "UnifiedSystemTray"})
+	if err != nil {
+		return errors.Wrap(err, "system tray button not found")
+	}
+	defer trayButton.Release(ctx)
+
+	if err := mouse.Click(ctx, tconn, trayButton.Location.CenterPoint(), mouse.LeftButton); err != nil {
+		return errors.Wrap(err, "failed to click the tray button")
+	}
+
+	if err := ui.WaitUntilExists(ctx, tconn, ui.FindParams{ClassName: "SettingBubbleContainer"}, 2*time.Second); err != nil {
+		return errors.Wrap(err, "quick settings does not appear")
+	}
+
+	params := ui.FindParams{
+		Name:      "Notification close",
+		ClassName: "ImageButton",
+		Role:      ui.RoleTypeButton,
+	}
+
+	for {
+		nodes, err := ui.FindAll(ctx, tconn, params)
+		if err != nil {
+			return errors.Wrap(err, "failed get list of all notifications close buttons")
+		}
+		if len(nodes) == 0 {
+			break
+		}
+
+		for _, node := range nodes {
+			// Here we intentionaly ignore errors, because we modify
+			// accesability tree by clicking close buttons.
+			// However it's not a problem since we repeat operation until there
+			// will be no close buttons.
+			node.LeftClick(ctx)
+		}
+	}
+
+	if err := mouse.Click(ctx, tconn, trayButton.Location.CenterPoint(), mouse.LeftButton); err != nil {
+		return errors.Wrap(err, "failed to click the tray button")
+	}
+
+	return nil
+}
+
+// waitPredicate is a function that returns true if notification satisfies some
+// conditions.
+type waitPredicate func(n *Notification) bool
+
+// WaitIDContains creates a predicate that checks whether notification ID
+// contains idContains.
+func WaitIDContains(idContains string) waitPredicate {
+	return func(n *Notification) bool {
+		return strings.Contains(n.ID, idContains)
+	}
+}
+
+// WaitTitle creates a predicate that checks whether notification has specific
+// title.
+func WaitTitle(title string) waitPredicate {
+	return func(n *Notification) bool {
+		return n.Title == title
+	}
+}
+
+// WaitForNotification waits for the first notification that satisfies all wait
+// predicates.
+func WaitForNotification(ctx context.Context, tconn *chrome.TestConn, timeout time.Duration, predicates ...waitPredicate) (*Notification, error) {
+	var result *Notification
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		notifications, err := Notifications(ctx, tconn)
+		if err != nil {
+			return testing.PollBreak(errors.Wrap(err, "failed to get list of notifications"))
+		}
+
+	NotificationLoop:
+		for _, notification := range notifications {
+			for _, predicate := range predicates {
+				if !predicate(notification) {
+					continue NotificationLoop
+				}
+			}
+			result = notification
+			return nil
+		}
+		return errors.New("no wanted notification")
+	}, &testing.PollOptions{Timeout: timeout}); err != nil {
+		return nil, errors.Wrap(err, "failed to wait for notification")
+	}
+	return result, nil
 }
