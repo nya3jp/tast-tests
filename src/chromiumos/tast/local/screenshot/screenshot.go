b56085485da7f0779ad2c698b793f21598123d90
Matt Stark <msta@google.com>
2020-09-17 23:58:00 +0000

    Add function to take screenshots of element with tast
    BUG=b:156040764
    TEST=local testing
    
    Change-Id: Iae5a01d4a07b79f26b914bd0c8f98186f17a7efd

--- src/chromiumos/tast/local/screenshot/screenshot.go
+++ src/chromiumos/tast/local/screenshot/screenshot.go
@@ -9,14 +9,23 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/cdputil"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
 )
 
 // Capture takes a screenshot and saves it as a PNG image to the specified file
@@ -27,6 +36,96 @@ func Capture(ctx context.Context, path string) error {
 		return errors.Errorf("failed running %q", strings.Join(cmd.Args, " "))
 	}
 	return nil
+}
+
+type skiaScreenshottable interface {
+	// SkiaScreenshot reports that a screenshot has been taken to send for skia for diff testing.
+	SkiaScreenshot(name string, screenshot *image.RGBA, keyValueMap map[string]string) error
+}
+
+// DiffTest takes a screenshot of the ui element described by `params` and sends it off to skia for diff testing.
+func DiffTest(ctx context.Context, s skiaScreenshottable, cr *chrome.Chrome, testName string, params ui.FindParams) error {
+	return DiffTestWithOptions(ctx, s, cr, testName, DiffTestOptions{Params: params})
+}
+
+// DiffTestWithOptions takes a screenshot of a particular ui element and sends it off to skia for diff testing.
+func DiffTestWithOptions(ctx context.Context, s skiaScreenshottable, cr *chrome.Chrome, testName string, options DiffTestOptions) error {
+	dir, ok := testing.ContextOutDir(ctx)
+	if !ok {
+		return errors.New("couldn't get output dir")
+	}
+	path := filepath.Join(dir, testName+".png")
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return err
+	}
+	if !options.LeaveNotifications {
+		ash.HideAllNotificationsAndWait(ctx, tconn)
+	}
+
+	node, err := ui.FindSingleton(ctx, tconn, options.Params)
+	if err != nil {
+		return err
+	}
+
+	boundsDp := node.Location
+	base64PNG, err := screenshotBase64PNG(func(code string, out interface{}) error {
+		return tconn.EvalPromise(ctx, code, out)
+	})
+	if err != nil {
+		return err
+	}
+
+	src, _, err := image.Decode(base64.NewDecoder(base64.StdEncoding, strings.NewReader(base64PNG)))
+	if err != nil {
+		return err
+	}
+
+	info, err := display.FindInfo(ctx, tconn, func(info *display.Info) bool {
+		return info.Bounds.Contains(boundsDp)
+	})
+	if err != nil {
+		return err
+	}
+	scale, err := info.GetEffectiveDeviceScaleFactor()
+	if err != nil {
+		return err
+	}
+	boundsPx := coords.ConvertBoundsFromDPToPX(boundsDp, scale)
+
+	// The screenshot returned is of the whole screen. Crop it to only contain the element requested by the user.
+	srcOffset := image.Point{X: boundsPx.Left, Y: boundsPx.Top}
+	dstSize := image.Rect(0, 0, boundsPx.Width, boundsPx.Height)
+	cropped := image.NewRGBA(dstSize)
+	draw.Draw(cropped, dstSize, src, srcOffset, draw.Src)
+
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	png.Encode(f, cropped)
+
+	displayMode, err := info.GetSelectedMode()
+	if err != nil {
+		return err
+	}
+
+	// TODO(msta): Determine an appropriate and useful set of parameters to use as key/value pairs.
+	return s.SkiaScreenshot(testName, cropped, map[string]string{
+		"resolution": fmt.Sprintf("%dx%d", displayMode.WidthInNativePixels, displayMode.HeightInNativePixels),
+		"scale":      fmt.Sprintf("%.2f", scale),
+	})
+}
+
+// DiffTestOptions provides all of the ways which you can configure the DiffTest function.
+type DiffTestOptions struct {
+	// By default, taking a screenshot will hide any notifications which might be overlaid on top of the element.
+	// Set to true if you don't want this behaviour.
+	LeaveNotifications bool
+
+	// The params used to find the node that we're looking for.
+	Params ui.FindParams
 }
 
 // CaptureChrome takes a screenshot of the primary display and saves it as a PNG
@@ -53,9 +152,9 @@ func CaptureCDP(ctx context.Context, conn *cdputil.Conn, path string) error {
 	})
 }
 
-func captureInternal(ctx context.Context, path string, eval func(code string, out interface{}) error) error {
+func screenshotBase64PNG(eval func(code string, out interface{}) error) (string, error) {
 	var base64PNG string
-	if err := eval(
+	err := eval(
 		`new Promise(function(resolve, reject) {
 		   chrome.autotestPrivate.takeScreenshot(function(base64PNG) {
 		     if (chrome.runtime.lastError === undefined) {
@@ -64,7 +163,13 @@ func captureInternal(ctx context.Context, path string, eval func(code string, ou
 		       reject(chrome.runtime.lastError.message);
 		     }
 		   });
-		 })`, &base64PNG); err != nil {
+		 })`, &base64PNG)
+	return base64PNG, err
+}
+
+func captureInternal(ctx context.Context, path string, eval func(code string, out interface{}) error) error {
+	base64PNG, err := screenshotBase64PNG(eval)
+	if err != nil {
 		return err
 	}
 
