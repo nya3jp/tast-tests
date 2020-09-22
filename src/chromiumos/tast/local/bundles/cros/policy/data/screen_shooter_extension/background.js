eb3276ba995528cf2e270b1d48ada86d820aacb0
Oleh Lamzin <lamzin@google.com>
2020-09-22 17:08:01 +0200

    Port screenshot restriction policy from extension
    Port autotest test to check screenshot restriction from Chrome
    extension.
    
    Copy ScreenShooter extension from autotest.
    
    BUG=chromium:1125556
    TEST=tast run localhost:7777 policy.DisableScreenshotsExtension
    
    Change-Id: I1e8df73adf38ac79caa57c9da293e6f5573d1922

---
+++ src/chromiumos/tast/local/bundles/cros/policy/data/screen_shooter_extension/background.js
@@ -0,0 +1,32 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function setTitle(capture) {
+  const title = capture ? "screen capture allowed" :
+    "screen capture not allowed";
+  chrome.tabs.executeScript({code: 'document.title = "' + title + '"'});
+}
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'takeScreenshot') {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {text: 'title'}, (method) => {
+        if (method === 'captureVisibleTab') {
+          chrome.tabs.captureVisibleTab((img) => {
+            setTitle(img);
+          });
+        } else if (method === 'tabCapture') {
+          chrome.tabCapture.capture({video: true}, (stream) => {
+            setTitle(stream);
+          });
+        } else if (method === 'desktopCapture') {
+          chrome.desktopCapture.chooseDesktopMedia(
+              ['screen', 'window', 'tab'], (streamId) => {
+                setTitle(streamId);
+              });
+        }
+      });
+    });
+  }
+});
