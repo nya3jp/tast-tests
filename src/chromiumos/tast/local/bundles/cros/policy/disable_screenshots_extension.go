463f071b77753a80c57c62fca82aaf909b4b1648
Oleh Lamzin <lamzin@google.com>
2020-09-22 17:08:01 +0200

    Test screenshot restriction policy from extension
    Add new test to check screenshot restriction from Chrome extension.
    
    BUG=chromium:1125556
    TEST=tast run localhost:7777 policy.DisableScreenshotsExtension
    
    Change-Id: I1e8df73adf38ac79caa57c9da293e6f5573d1922

---
+++ src/chromiumos/tast/local/bundles/cros/policy/disable_screenshots_extension.go
@@ -0,0 +1,151 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/fsutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/testing"
+)
+
+const (
+	backgroundJS = "screen_shooter_extension/background.js"
+	contentJS    = "screen_shooter_extension/content.js"
+	manifestJSON = "screen_shooter_extension/manifest.json"
+)
+
+var extensionFiles = []string{backgroundJS, contentJS, manifestJSON}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DisableScreenshotsExtension,
+		Desc: "Behavior of the DisableScreenshots policy, check whether screenshot can be taken by extensions APIs",
+		Contacts: []string{
+			"lamzin@google.com", // Test port author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Data:         extensionFiles,
+	})
+}
+
+func DisableScreenshotsExtension(ctx context.Context, s *testing.State) {
+	keyboard, err := input.VirtualKeyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer keyboard.Close()
+
+	extDir, err := ioutil.TempDir("", "disable_screenshot_extension")
+	if err != nil {
+		s.Fatal("Failed to create temp dir: ", err)
+	}
+	defer os.RemoveAll(extDir)
+
+	if err := os.Chown(extDir, int(sysutil.ChronosUID), int(sysutil.ChronosGID)); err != nil {
+		s.Fatalf("Failed to chown %q dir: %v", extDir, err)
+	}
+
+	for _, file := range extensionFiles {
+		dst := filepath.Join(extDir, filepath.Base(file))
+		if err := fsutil.CopyFile(s.DataPath(file), dst); err != nil {
+			s.Fatalf("Failed to copy %q file to %q: %v", file, extDir, err)
+		}
+	}
+
+	tmpdir, err := ioutil.TempDir("", "fdms-")
+	if err != nil {
+		s.Fatal("Failed to create fdms temp dir: ", err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	s.Logf("FakeDMS starting in %s", tmpdir)
+	fdms, err := fakedms.New(ctx, tmpdir)
+	if err != nil {
+		s.Fatal("Failed to start FakeDMS: ", err)
+	}
+	defer fdms.Stop(ctx)
+
+	if err := fdms.WritePolicyBlob(fakedms.NewPolicyBlob()); err != nil {
+		s.Fatal("Failed to write policies to FakeDMS: ", err)
+	}
+
+	cr, err := chrome.New(ctx,
+		chrome.UnpackedExtension(extDir),
+		chrome.Auth(pre.Username, pre.Password, pre.GaiaID),
+		chrome.DMSPolicy(fdms.URL))
+	if err != nil {
+		s.Fatal("Failed to create Chrome instance: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tabConn, err := cr.NewConn(ctx, "https://google.com")
+	if err != nil {
+		s.Fatal("Failed to create a tab: ", err)
+	}
+	defer tabConn.Close()
+
+	for _, tc := range []struct {
+		name      string
+		value     []policy.Policy
+		wantTitle string
+	}{
+		{
+			name:      "true",
+			value:     []policy.Policy{&policy.DisableScreenshots{Val: true}},
+			wantTitle: "screen capture not allowed",
+		},
+		{
+			name:      "false",
+			value:     []policy.Policy{&policy.DisableScreenshots{Val: false}},
+			wantTitle: "screen capture allowed",
+		},
+		{
+			name:      "unset",
+			value:     []policy.Policy{},
+			wantTitle: "screen capture allowed",
+		},
+	} {
+		// Update policies.
+		if err := policyutil.ServeAndVerify(ctx, fdms, cr, tc.value); err != nil {
+			s.Fatal("Failed to update policies: ", err)
+		}
+
+		// Here we check only `chrome.tabs.captureVisibleTab` extension API.
+		// TODO(crbug.com/839630, crbug.com/817497): check whether DisableScreenshots should affect
+		// `chrome.tabCapture.capture` and `chrome.desktopCapture.chooseDesktopMedia` extension APIs.
+		if err := tabConn.Eval(ctx, `document.title = "captureVisibleTab"`, nil); err != nil {
+			s.Fatal("Failed to execute JS in extension: ", err)
+		}
+
+		if err := keyboard.Accel(ctx, "Ctrl+Shift+Y"); err != nil {
+			s.Fatal("Failed to press Ctrl+Shift+Y: ", err)
+		}
+
+		if err := tabConn.WaitForExpr(ctx, `document.title != "captureVisibleTab"`); err != nil {
+			s.Fatal("Failed to execute JS in extension: ", err)
+		}
+
+		var title string
+		if err := tabConn.Eval(ctx, `document.title`, &title); err != nil {
+			s.Fatal("Failed to execute JS in extension: ", err)
+		}
+		if title != tc.wantTitle {
+			s.Fatalf("Unexpected title: get %q; want %q", title, tc.wantTitle)
+		}
+	}
+}
