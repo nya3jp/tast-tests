a4afb77cc96e915810a42814c7e39b75a3fbf497
alexanderhartl <alexanderhartl@google.com>
2020-09-22 09:33:25 +0000

    tast-tests: add ShowHomeButton
    Add the policy.ShowHomeButton tast test.
    
    BUG=chromium:1125599
    TEST=tast run <ip> policy.ShowHomeButton
    
    Change-Id: I787c73bf259a29aee53372e68075a8e954fcf00d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2409966
    Tested-by: Alexander Hartl <alexanderhartl@google.com>
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Commit-Queue: Alexander Hartl <alexanderhartl@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/policy/show_home_button.go
@@ -0,0 +1,96 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ShowHomeButton,
+		Desc: "Test the behavior of ShowHomeButton policy: check if a home button is shown based on the value of the policy",
+		Contacts: []string{
+			"alexanderhartl@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+	})
+}
+
+func ShowHomeButton(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	for _, param := range []struct {
+		name       string
+		wantButton bool                   // wantButton is the expected existence of the "Home" button.
+		policy     *policy.ShowHomeButton // policy is the policy we test.
+	}{
+		{
+			name:       "unset",
+			wantButton: false,
+			policy:     &policy.ShowHomeButton{Stat: policy.StatusUnset},
+		},
+		{
+			name:       "no_show",
+			wantButton: false,
+			policy:     &policy.ShowHomeButton{Val: false},
+		},
+		{
+			name:       "show",
+			wantButton: true,
+			policy:     &policy.ShowHomeButton{Val: true},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.policy}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			conn, err := cr.NewConn(ctx, "")
+			if err != nil {
+				s.Fatal("Failed to connect to chrome: ", err)
+			}
+			defer conn.Close()
+
+			// Find the button node.
+			params := ui.FindParams{
+				Role: ui.RoleTypeButton,
+				Name: "Home",
+			}
+			hasHomeButton := false
+			if err := ui.WaitUntilExists(ctx, tconn, params, 30*time.Second); err == nil {
+				hasHomeButton = true
+			} else if !errors.Is(err, ui.ErrNodeDoesNotExist) {
+				s.Fatal("Unexpected error retrieving home button: ", err)
+			}
+			if hasHomeButton != param.wantButton {
+				s.Errorf("Unexpected existence of home button: got %t; want %t", hasHomeButton, param.wantButton)
+			}
+		})
+	}
+}
