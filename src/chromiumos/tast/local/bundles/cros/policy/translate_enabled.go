b537349378fbcc9a73273c66fafafc2081b86317
Marc Grimme <marcgrimme@chromium.org>
2020-09-24 16:50:19 +0200

    tast-tests: add test for TranslateEnabled policy
    This CL adds a test for TranslateEnabledl policy migrated from autotest.
    This test validates the UI behaviour of the different states the policy
    introduces. When enabled/unset the translate widget appears otherwise
    it should not appear. The correct UI behaviours are checked.
    
    BUG=chromium:1125603
    TEST=tast run <ip> policy.TranslateEnabled
    
    Change-Id: I24db326bef265817cd2ce49d85331b63dc9cc15d

---
+++ src/chromiumos/tast/local/bundles/cros/policy/translate_enabled.go
@@ -0,0 +1,118 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: TranslateEnabled,
+		Desc: "Behavior of Translate policy",
+		Contacts: []string{
+			"marcgrimme@google.com", // Test author
+			"kathrelkeld@chromium.org",
+			"chromeos-commercial-managed-user-experience@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+		Data:         []string{"translate_page_fr.html"},
+	})
+}
+
+// TranslateEnabled validates the UI behaviour of the different
+// states the policy introduces. When enabled/unset the translate widget
+// appears otherwise it should not appear. The correct UI behaviours are
+// checked.
+func TranslateEnabled(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	// Setup and start webserver (implicitly provides data form above)
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	for _, param := range []struct {
+		// name is the subtest name.
+		name          string
+		wantTranslate bool
+		// value is the policy value.
+		value *policy.TranslateEnabled
+	}{
+		{
+			name:          "true",
+			wantTranslate: true,
+			value:         &policy.TranslateEnabled{Val: true},
+		},
+		{
+			name:          "false",
+			wantTranslate: false,
+			value:         &policy.TranslateEnabled{Val: false},
+		},
+		{
+			name:          "unset",
+			wantTranslate: true,
+			value:         &policy.TranslateEnabled{Stat: policy.StatusUnset},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			// Setup Chrome and enable policy
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndRefresh(ctx, fdms, cr, []policy.Policy{param.value}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			// Connect to Test API to use it with the ui library.
+			tconn, err := cr.TestAPIConn(ctx)
+			if err != nil {
+				s.Fatal("Failed to create Test API connection: ", err)
+			}
+
+			// Provide more data in artefacts if test fails.
+			defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+			// Open the browser and navigate to the to be translated page.
+			url := server.URL + "/translate_page_fr.html"
+			conn, err := cr.NewConn(ctx, url)
+			if err != nil {
+				s.Fatal("Failed to connect to chrome: ", err)
+			}
+			defer conn.Close()
+
+			if err := ui.WaitForLocationChangeCompleted(ctx, tconn); err != nil {
+				s.Fatal("Failed to wait for location change. Error: ", err)
+			}
+
+			// Find the translate node and validate against error.
+			isTranslate, err := ui.Exists(ctx, tconn, ui.FindParams{
+				Role: ui.RoleTypeButton,
+				Name: "Translate this page",
+			})
+			if err != nil {
+				s.Fatal("Error during checking for UI Compontent to translate. Error: ", err)
+			}
+
+			if isTranslate != param.wantTranslate {
+				s.Errorf("Wrong visibility for translated gadget: got %t; want %t", isTranslate, param.wantTranslate)
+			}
+		})
+	}
+}
