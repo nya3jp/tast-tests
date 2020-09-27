f2d4d59c215212070e308e541e9eabb2d6a181ec
Nicolas Boichat <drinkcat@chromium.org>
2020-09-27 15:28:33 +0800

    graphics.FPS: Expand margin to 0.3
    Allows us to make the test critical, as only elm/hana is above
    the current 0.2 threshold.
    
    BUG=chromium:1031054
    TEST=CQ, check tastboard
    
    Change-Id: Ic9613b8715fcc3867841c674dfd042640f8f69ee

--- src/chromiumos/tast/local/bundles/cros/graphics/fps.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/fps.go
@@ -107,8 +107,9 @@ func FPS(ctx context.Context, s *testing.State) {
 		trimPercent = 5
 		// Target fps (always 60 fps for now).
 		targetFPS = 60.0
-		// Accept up to 0.2 fps margin over the reference 60 fps.
-		margin = 0.2
+		// Accept up to 0.3 fps margin over the reference 60 fps.
+		// TODO(crbug.com/1031054): Restore to 0.2 after oak switches to 4.19
+		margin = 0.3
 		// Accept up to 0.2 fps standard deviation.
 		maxStddev = 0.2
 	)
