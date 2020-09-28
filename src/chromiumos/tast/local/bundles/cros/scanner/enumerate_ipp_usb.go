971a2749d5b3e42601fd14840b127093b7e0b53a
Benjamin Gordon <bmgordon@chromium.org>
2020-09-28 10:09:49 -0600

    scanner: Add EnumerateIPPUSB test
    This test sets up several virtual USB printers and checks that only the
    correct subset is included in lorgnette's ListScanners response.
    
    They have to be checked one at a time instead of all at once, because
    only one instance of virtual-usb-printer can be running at a time.
    However, this is done as a table-driven test instead of a parameterized
    test because they should all logically pass or fail together.
    
    BUG=b:159484077
    TEST=tast run scanner.EnumerateIPPUSB
    
    Change-Id: Ie668497ab19f24e411df4f94396b1ab2e7809abf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2437036
    Reviewed-by: Fletcher Woodruff <fletcherw@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Benjamin Gordon <bmgordon@chromium.org>
    Tested-by: Benjamin Gordon <bmgordon@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/scanner/enumerate_ipp_usb.go
@@ -0,0 +1,131 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package scanner
+
+import (
+	"context"
+	"regexp"
+	"time"
+
+	lpb "chromiumos/system_api/lorgnette_proto"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/scanner/lorgnette"
+	"chromiumos/tast/local/printing/usbprinter"
+	"chromiumos/tast/testing"
+)
+
+type scannerInfo struct {
+	name             string
+	descriptors      string
+	attributes       string
+	esclCapabilities string
+	platenImage      string
+	shouldEnumerate  bool
+}
+
+var ippUsbFormat = regexp.MustCompile("^ippusb:escl:.*:(....)_(....)/.*")
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         EnumerateIPPUSB,
+		Desc:         "Tests that IPP-USB devices are correctly found",
+		Contacts:     []string{"bmgordon@chromium.org", "project-bolton@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"virtual_usb_printer"},
+	})
+}
+
+// isMatchingScanner returns true iff scanner refers to an ippusb device with the same VID and PID as devInfo.
+func isMatchingScanner(scanner *lpb.ScannerInfo, devInfo usbprinter.DevInfo) bool {
+	if match := ippUsbFormat.FindStringSubmatch(scanner.Name); match != nil {
+		return devInfo.VID == match[1] && devInfo.PID == match[2]
+	}
+
+	return false
+}
+
+// runEnumerationTest sets up virtual-usb-printer to emulate the device specified in info,
+// calls lorgnette's ListScanners, and checks to see if the device was listed in the response.
+func runEnumerationTest(ctx context.Context, s *testing.State, info scannerInfo) {
+	s.Logf("Checking if %s is listed", info.name)
+
+	devInfo, err := usbprinter.LoadPrinterIDs(info.descriptors)
+	if err != nil {
+		s.Fatalf("Failed to load printer IDs from %v: %v", info.descriptors, err)
+	}
+
+	printer, err := usbprinter.StartScanner(ctx, devInfo, info.descriptors, info.attributes, info.esclCapabilities, "")
+	if err != nil {
+		s.Fatal("Failed to attach virtual printer: ", err)
+	}
+	defer func() {
+		printer.Kill()
+		printer.Wait()
+	}()
+
+	s.Log("Requesting scanner list")
+	l, err := lorgnette.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to lorgnette: ", err)
+	}
+	scanners, err := l.ListScanners(ctx)
+	if err != nil {
+		s.Fatal("Failed to call ListScanners: ", err)
+	}
+
+	found := false
+	for _, scanner := range scanners {
+		if isMatchingScanner(scanner, devInfo) {
+			found = true
+			break
+		}
+	}
+	if found != info.shouldEnumerate {
+		s.Errorf("%s enumerated=%v, want=%v", info.name, found, info.shouldEnumerate)
+	}
+}
+
+func EnumerateIPPUSB(ctx context.Context, s *testing.State) {
+	// Use cleanupCtx for any deferred cleanups in case of timeouts or
+	// cancellations on the shortened context.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := usbprinter.InstallModules(ctx); err != nil {
+		s.Fatal("Failed to install kernel modules: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := usbprinter.RemoveModules(ctx); err != nil {
+			s.Error("Failed to remove kernel modules: ", err)
+		}
+	}(cleanupCtx)
+
+	for _, info := range []scannerInfo{{
+		name:             "Non-IPP USB printer",
+		descriptors:      "/usr/local/etc/virtual-usb-printer/usb_printer.json",
+		attributes:       "",
+		esclCapabilities: "",
+		shouldEnumerate:  false,
+	}, {
+		name:             "IPP USB printer without eSCL",
+		descriptors:      "/usr/local/etc/virtual-usb-printer/ippusb_printer.json",
+		attributes:       "/usr/local/etc/virtual-usb-printer/ipp_attributes.json",
+		esclCapabilities: "",
+		shouldEnumerate:  false,
+	}, {
+		name:             "IPP USB printer with eSCL",
+		descriptors:      "/usr/local/etc/virtual-usb-printer/ippusb_printer.json",
+		attributes:       "/usr/local/etc/virtual-usb-printer/ipp_attributes.json",
+		esclCapabilities: "/usr/local/etc/virtual-usb-printer/escl_capabilities.json",
+		shouldEnumerate:  true,
+	}} {
+		runEnumerationTest(ctx, s, info)
+	}
+
+	// Lorgnette was auto started during testing.  Kill it to avoid
+	// affecting subsequent tests.
+	lorgnette.StopService(cleanupCtx)
+}
