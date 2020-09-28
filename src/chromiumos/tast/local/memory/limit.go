0eec0946c6850e44c60d2195ccde85dbf2ce9f97
Charles William Dick <cwd@google.com>
2020-09-28 10:51:43 +0900

    memory.Limit implementations for ChromeOS
    Introduces a Limit interface to abstract away low memory signals. Will
    be used in multi-VM tests, but as a first step convert the existing
    arc.MemoryChromsOSPerf test and evaluate the results.
    
    BUG=b:136696325
    TEST=run arc.MemoryChromeOSPerf.vm * 10, before and after change, see no
    large difference in results. arc.MemoryAndroidPerf.vm and
    arc.MemoryShiftingPerf.vm still work.
    
    Change-Id: I4b35e89d842e49c92cfadbef277f024ddbca12e0

---
+++ src/chromiumos/tast/local/memory/limit.go
@@ -0,0 +1,169 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package memory
+
+import (
+	"context"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+
+	"chromiumos/tast/errors"
+)
+
+// Limit allows tests to determine if memory use is close to a limit without
+// having to know the specific memory counters used.
+type Limit interface {
+	// Distance returns the amount of memory that can be allocated in bytes
+	// before the limit is reached. If negative, abs(Distance()) bytes must be
+	// freed to go below the limit.
+	Distance(ctx context.Context) (int64, error)
+}
+
+type availableLimit struct {
+	margin int64
+}
+
+// readFirstInt64 reads the first unsigned integer from a file.
+func readFirstInt64(f string) (int64, error) {
+	// Files will always just be a single line, so it's OK to read everything.
+	data, err := ioutil.ReadFile(f)
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to read file %q", f)
+	}
+	firstString := strings.Split(strings.TrimSpace(string(data)), " ")[0]
+	firstInt64, err := strconv.ParseInt(firstString, 10, 64)
+	if err != nil {
+		return 0, errors.Wrapf(err, "unable to convert %q to integer", data)
+	}
+	return firstInt64, nil
+}
+
+// Distance returns the difference between available memory and the critical
+// margin, in bytes. Result will be negative if available memory is below the
+// critical margin.
+func (l *availableLimit) Distance(_ context.Context) (int64, error) {
+	const availableMemorySysFile = "/sys/kernel/mm/chromeos-low_mem/available"
+	availableMiB, err := readFirstInt64(availableMemorySysFile)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to read ChromeOS available memory")
+	}
+	return (availableMiB * MiB) - l.margin, nil
+}
+
+// CriticalMargin returns the value of ChromeOS available memory below which
+// tabs are killed, in bytes.
+func CriticalMargin() (int64, error) {
+	const marginMemorySysFile = "/sys/kernel/mm/chromeos-low_mem/margin"
+	criticalMarginMiB, err := readFirstInt64(marginMemorySysFile)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to read ChromeOS critical margin")
+	}
+	return criticalMarginMiB * MiB, nil
+}
+
+// NewAvailableLimit creates a Limit that measures how far away ChromeOS
+// available memory is from a specified margin, in bytes.
+func NewAvailableLimit(margin int64) (Limit, error) {
+	return &availableLimit{margin}, nil
+}
+
+// NewAvailableCriticalLimit creates a Limit that measures how far away ChromeOS
+// is from the critical memory pressure margin. Unlike
+// NewAvailableIsCriticalLimit above, this it intended to keep ChromeOS memory
+// pressure below critical.
+func NewAvailableCriticalLimit() (Limit, error) {
+	criticalMargin, err := CriticalMargin()
+	if err != nil {
+		return nil, err
+	}
+	return &availableLimit{
+		margin: criticalMargin,
+	}, nil
+}
+
+type pageReclaimLimit struct {
+	largeZones map[string]bool
+}
+
+// Distance returns the smallest distance from a zone's free counter to half-way
+// between its min and low watermark. If <= 0, this means that page reclaim has
+// started and we are at risk of the Linux OOM Killer waking up.
+func (l *pageReclaimLimit) Distance(_ context.Context) (int64, error) {
+	infos, err := ReadZoneInfo()
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to read zone counters")
+	}
+	var minDistance int64 = math.MaxInt64
+	for _, info := range infos {
+		if _, ok := l.largeZones[info.Name]; !ok {
+			// Zone is not a large zone.
+			continue
+		}
+		distance := int64(info.Free) - int64((info.Low+info.Min)/2)
+		if distance < minDistance {
+			minDistance = distance
+		}
+	}
+	if minDistance == math.MaxInt64 {
+		return 0, errors.New("no large zones found")
+	}
+	return minDistance, nil
+}
+
+// NewPageReclaimLimit creates a Limit that measures how far away ChromeOS is
+// from any Linux memory zone's free pages being half-way between the min and
+// low watermarks. The intent is to trigger page reclaim by being below the low
+// watermark, but keep away from the low watermark to avoid invoking the Linux
+// OOM killer.
+func NewPageReclaimLimit() (Limit, error) {
+	infos, err := ReadZoneInfo()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read zone counters")
+	}
+	const largeZoneMinMin = 4 * MiB
+	largeZones := make(map[string]bool)
+	for _, info := range infos {
+		if info.Min > largeZoneMinMin {
+			largeZones[info.Name] = true
+		}
+	}
+	if len(largeZones) == 0 {
+		return nil, errors.New("no large zones found")
+	}
+	return &pageReclaimLimit{largeZones}, nil
+}
+
+type compositeLimit struct {
+	limits []Limit
+}
+
+// Distance returns the minimum Distance returned by any child Limit.
+func (l *compositeLimit) Distance(ctx context.Context) (int64, error) {
+	if len(l.limits) == 0 {
+		return 0, errors.New("empty compositeLimit")
+	}
+	minDistance, err := l.limits[0].Distance(ctx)
+	if err != nil {
+		return 0, err
+	}
+	for i := 1; i < len(l.limits); i++ {
+		distance, err := l.limits[i].Distance(ctx)
+		if err != nil {
+			return 0, err
+		}
+		if distance < minDistance {
+			minDistance = distance
+		}
+	}
+	return minDistance, nil
+}
+
+// NewCompositeLimit creates a Limit that returns the minimum Distance()
+// returned by all the passed limits.
+func NewCompositeLimit(limits ...Limit) Limit {
+	return &compositeLimit{limits}
+}
