b97a71199864b5a1297d18692337e580e425cbd3
Yen-lin Lai <yenlinlai@google.com>
2020-09-30 19:07:33 +0800

    wifi.SimpleConnect: Merge dfs_ch120 and dfs testcases.
    ch120 testcase was separated for a problem of ch120 on veyron_mickey.
    As veyron_mickey's AUE is passed (R86), we can merge the two testcases.
    If someone wants to debug the veyron_mickey problem, they can still run
    wifi.SimpleConnect.dfs to reproduce.
    
    BUG=b:154440798
    TEST=tast run $DUT wifi.SimpleConnect.dfs
    
    Change-Id: Icd288197b3bc359ff749aa08b4aaef3da4f03b2b

--- src/chromiumos/tast/remote/bundles/cros/wifi/simple_connect.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/simple_connect.go
@@ -500,16 +500,8 @@ func init() {
 				// See: https://en.wikipedia.org/wiki/Dynamic_frequency_selection, https://en.wikipedia.org/wiki/List_of_WLAN_channels
 				Name: "dfs",
 				Val: []simpleConnectTestcase{
-					{apOpts: []ap.Option{ap.Mode(ap.Mode80211nMixed), ap.Channel(136), ap.HTCaps(ap.HTCapHT40)}},
-				},
-			}, {
-				// Verifies that DUT can connect to an open network on the DFS channel 120 (5600MHz).
-				// TODO(b/154440798): Investigate why this fails on veyron_mickey and consider merge this with
-				// "dfs" case after resolution.
-				Name:      "dfs_ch120",
-				ExtraAttr: []string{"wificell_unstable"},
-				Val: []simpleConnectTestcase{
 					{apOpts: []ap.Option{ap.Mode(ap.Mode80211nMixed), ap.Channel(120), ap.HTCaps(ap.HTCapHT40)}},
+					{apOpts: []ap.Option{ap.Mode(ap.Mode80211nMixed), ap.Channel(136), ap.HTCaps(ap.HTCapHT40)}},
 				},
 			}, {
 				// Verifies that DUT can connect to a networks with the longest and shortest SSID.
