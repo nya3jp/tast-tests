2602d2a13eb1c5db35f0a6e052b3dbedb1280218
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-09-29 18:08:50 -0700

    Run UIAutomator Test Using Tast For Google Play Music app
    BUG=b:167707647
    TEST=tast run <eve> arcappcompat.GooglePlayMusic
    
    Tast Test for GooglePlayMusic launches the app
    and checks that the main page is open. Also
    for Clamshell Mode it verifies Clamshell Full
    Screen, Minimize and Restore,Clamshell Resize,
    Reopen tests and for Touchview Mode it verifies
    Minimize and Restore, Reopen tests.
    
    Change-Id: I0004f29035d5496fec4114efc3e360e6f345e5a2

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/google_play_music.go
@@ -0,0 +1,108 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForGooglePlayMusic = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForGooglePlayMusic},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForGooglePlayMusic = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForGooglePlayMusic},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GooglePlayMusic,
+		Desc:         "Functional test for GooglePlayMusic that install, launch the app and check that the main page is open, also checks GooglePlayMusic correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForGooglePlayMusic,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForGooglePlayMusic,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForGooglePlayMusic,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForGooglePlayMusic,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// GooglePlayMusic test uses library for opting into the playstore and installing app.
+// Checks GooglePlayMusic correctly changes the window states in both clamshell and touchview mode.
+func GooglePlayMusic(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.google.android.music"
+		appActivity = "com.android.music.activitymanagement.TopLevelActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForGooglePlayMusic verifies GooglePlayMusic is launched and
+// verify GooglePlayMusic reached main activity page of the app.
+func launchAppForGooglePlayMusic(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		homeID       = "com.google.android.music:id/navigation_button"
+		allowText    = "ALLOW"
+		noThanksText = "NO THANKS"
+	)
+	// Click on allow button.
+	clickOnAllowButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.Text(allowText))
+	if err := clickOnAllowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("clickOnAllowButton doesn't exist: ", err)
+	} else if err := clickOnAllowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnAllowButton: ", err)
+	}
+
+	// Click on noThanks button.
+	clickOnNoThanksButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.Text(noThanksText))
+	if err := clickOnNoThanksButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("clickOnNoThanksButton doesn't exist: ", err)
+	} else if err := clickOnNoThanksButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnNoThanksButton: ", err)
+	}
+
+	// Check for home icon.
+	homeIcon := d.Object(ui.ID(homeID))
+	if err := homeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("homeIcon doesn't exist: ", err)
+	}
+}
