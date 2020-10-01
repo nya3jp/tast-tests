3d07f30201f3153a098e924f0de2b462c7adb5a5
Woody Chow <woodychow@google.com>
2020-10-01 14:46:54 +0900

    tast-tests: Revert activity.IsRunning hack for R
    R support of dumpsysActivityActivities has been enabled.
    
    BUG=b:152576355
    TEST=Run tests using isRunning, and close the app intentionally
    
    Change-Id: Iee10e0dc9f58b423c85cce23101b601c58246762

--- src/chromiumos/tast/local/arc/activity.go
+++ src/chromiumos/tast/local/arc/activity.go
@@ -619,12 +619,6 @@ func (ac *Activity) IsRunning(ctx context.Context) (bool, error) {
 		return false, errors.Wrap(err, "unable to get Android SDK version")
 	}
 
-	if n == SDKR {
-		// TODO(b/152576355): activity.IsRunning is broken on Android R.
-		// As a workaround (to fix tests), return IsRunning = true for now.
-		return true, nil
-	}
-
 	if _, err := ac.getTaskInfo(ctx); err != nil {
 		if errors.Is(err, errNoTaskInfo) {
 			return false, nil
