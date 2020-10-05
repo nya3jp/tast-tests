73c6597a99a730a49321b7e03770cf81933337f1
Trent Begin <tbegin@google.com>
2020-10-05 09:30:50 -0600

    CrosHealthdProbeSystemInfo: sanitize marketing name to remove commas
    The cros_healthd diag tool was changed to remove commas from
    the marketing name of the device. This change updates the the test to
    remove the commas from the csv validation.
    
    BUG=chromium:1134667
    TEST=tast run coral-dut platform.CroshealthdProbeSystemInfo (pass)
    
    Change-Id: Id286ea5c464155e3ba9429861f3a34990327c2ae

--- src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_system_info.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_system_info.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 
 	"chromiumos/tast/local/bundles/cros/platform/csv"
+	"chromiumos/tast/local/crosconfig"
 	"chromiumos/tast/local/croshealthd"
 	"chromiumos/tast/lsbrelease"
 	"chromiumos/tast/testing"
@@ -66,6 +67,14 @@ func CrosHealthdProbeSystemInfo(ctx context.Context, s *testing.State) {
 		manufactureDateRegex = regexp.MustCompile("[0-9]{4}-[0-9]{2}-[0-9]{2}")
 	)
 
+	// Sanitize the marketing name value to remove commas. This matches the
+	// behavior of the telem tool.
+	marketingNameRaw, err := crosconfig.Get(ctx, arcBuildPropertiesPath, marketingNameProperty)
+	if err != nil {
+		s.Fatal("Unable to get marketing name from cros_config: ", err)
+	}
+	marketingName := strings.ReplaceAll(marketingNameRaw, ", ", "/")
+
 	lsbValues, err := lsbrelease.Load()
 	if err != nil {
 		s.Fatal("Failed to get lsb-release info: ", err)
@@ -91,7 +100,7 @@ func CrosHealthdProbeSystemInfo(ctx context.Context, s *testing.State) {
 			csv.MatchRegexOrNA(manufactureDateRegex)),
 		csv.Column("product_sku_number", csv.EqualToFileIfCrosConfigPropOrNA(ctx, crosHealthdCachedVpdPath,
 			skuNumberProperty, skuNumberPath)),
-		csv.Column("marketing_name", csv.EqualToCrosConfigProp(ctx, arcBuildPropertiesPath, marketingNameProperty)),
+		csv.Column("marketing_name", csv.MatchValue(marketingName)),
 		csv.Column("bios_version", csv.EqualToFileContentOrNA(biosVersionPath)),
 		csv.Column("board_name", csv.EqualToFileContentOrNA(boardNamePath)),
 		csv.Column("board_version", csv.EqualToFileContentOrNA(boardVersionPath)),
