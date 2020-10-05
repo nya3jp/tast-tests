68dbbfb5b3ab0c066ad65848ca02831913886a46
Wojciech Jakobczyk <wja@semihalf.com>
2020-10-05 12:10:37 +0000

    tast-test: wifi.RoamDiagnostics test cases
    The test is migrated from network_WiFi_RoamDiagnostics.py autotest.
    
    BUG=b:168881082
    TEST=run tast wifi.RoamDiagnostics
    
    Cq-Depend: chromium:2412333, chromium:2414450
    Change-Id: Iaa48c9000437bf245eea5d8d72f910b6a76a6ff7

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/wifiutil/ap_util.go
@@ -0,0 +1,58 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifiutil
+
+import (
+	"context"
+
+	"chromiumos/tast/common/wifi/security"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/remote/wificell/hostapd"
+	"chromiumos/tast/testing"
+)
+
+// ConfigureAP is a helper wrapper on TestFixture.ConfigureAP that additionally
+// handles error, returns deconfig function and provide channel frequency the AP is configured for
+func ConfigureAP(ctx context.Context, s *testing.State, apParams []hostapd.Option, routerIdx int,
+	secConfFac security.ConfigFactory) (ap *wificell.APIface, freq int, deconfig func(context.Context, *wificell.APIface)) {
+
+	tf := s.PreValue().(*wificell.TestFixture)
+
+	ap, err := tf.ConfigureAPOnRouterID(ctx, routerIdx, apParams, secConfFac)
+	if err != nil {
+		s.Fatalf("Failed to configure AP%d, err: %s", routerIdx, err)
+	}
+	s.Logf("AP%d setup done", routerIdx)
+
+	freq, err = hostapd.ChannelToFrequency(ap.Config().Channel)
+	if err != nil {
+		s.Fatalf("Failed to get frequency for channel %d: %v", ap.Config().Channel, err)
+	}
+
+	return ap, freq, func(ctx context.Context, ap *wificell.APIface) {
+		if err := tf.DeconfigAP(ctx, ap); err != nil {
+			s.Errorf("Failed to deconfig AP, err: %s", err)
+		}
+		s.Logf("AP%d teardown done", routerIdx)
+	}
+}
+
+// ConnectAP is a helper wrapper on TestFixture.ConnectWifiAP that additionally
+// handles errors and returns disconnect function
+func ConnectAP(ctx context.Context, s *testing.State, ap *wificell.APIface, apIdx int) (disconnect func(context.Context)) {
+	tf := s.PreValue().(*wificell.TestFixture)
+
+	if _, err := tf.ConnectWifiAP(ctx, ap); err != nil {
+		s.Fatal("Failed to connect to WiFi, err: ", err)
+	}
+
+	s.Log("Connected to AP", apIdx)
+
+	return func(ctx context.Context) {
+		if err := tf.CleanDisconnectWifi(ctx); err != nil {
+			s.Error("Failed to disconnect WiFi, err: ", err)
+		}
+	}
+}
