f27294fdcb0a63f6f8d63c4263036d776b751292
Mohamed Omar <mohamedaomar@google.com>
2020-10-06 11:18:13 +0200

    tast-tests: add PinUnlockMinimumLength
    add policy.PinUnlockMinimumLength tast test.
    
    BUG=None
    TEST=tast run <ip> policy.PinUnlockMinimumLength
    
    Change-Id: I93cf4044608c6d6b5bca0b510b753639eb47fcc1

---
+++ src/chromiumos/tast/local/bundles/cros/policy/pin_unlock_minimum_length.go
@@ -0,0 +1,147 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/ossettings"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PinUnlockMinimumLength,
+		Desc: "Follows the user flow to set unlock pin",
+		Contacts: []string{
+			"mohamedaomar@google.com", // Test author
+			"gabormagda@google.com",
+			"chromeos-commercial-stability@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          pre.User,
+	})
+}
+
+func PinUnlockMinimumLength(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	for _, param := range []struct {
+		name     string
+		pin      string
+		policies []policy.Policy
+	}{
+		{
+			name: "unset",
+			pin:  "123456",
+			policies: []policy.Policy{
+				&policy.PinUnlockMinimumLength{Stat: policy.StatusUnset},
+				&policy.QuickUnlockModeWhitelist{Val: []string{"PIN"}},
+			},
+		},
+		{
+			name: "min_4",
+			pin:  "12345",
+			policies: []policy.Policy{
+				&policy.PinUnlockMinimumLength{Val: 4},
+				&policy.QuickUnlockModeWhitelist{Val: []string{"PIN"}},
+			},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, param.policies); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			if err := ossettings.LaunchAtPage(ctx, tconn, ui.FindParams{
+				Name: "People",
+				Role: ui.RoleTypeLink,
+			}); err != nil {
+				s.Fatal("Failed to launch Settings: ", err)
+			}
+
+			nodeSSI, err := ui.FindWithTimeout(ctx, tconn, ui.FindParams{
+				Role: ui.RoleTypeLink,
+				Name: "Security and sign-in",
+			}, 15*time.Second)
+			if err != nil {
+				s.Fatal("Failed to find Security and sign-in option: ", err)
+			}
+			defer nodeSSI.Release(ctx)
+
+			if err := nodeSSI.StableLeftClick(ctx, &testing.PollOptions{Interval: 1 * time.Second, Timeout: 5 * time.Second}); err != nil {
+				s.Fatal("Failed to open the Security and sign-in option: ", err)
+			}
+
+			if err := ui.WaitUntilExists(ctx, tconn, ui.FindParams{
+				Role: ui.RoleTypeTextField,
+				Name: "Password",
+			}, 15*time.Second); err != nil {
+				s.Fatal("Could not find the password field: ", err)
+			}
+
+			kb, err := input.Keyboard(ctx)
+			if err != nil {
+				s.Fatal("Failed to get keyboard: ", err)
+			}
+			defer kb.Close()
+
+			if err := kb.Type(ctx, pre.Password+"\n"); err != nil {
+				s.Fatal("Failed to type password: ", err)
+			}
+
+			if err := ui.FindAndClick(ctx, tconn, ui.FindParams{
+				Role: ui.RoleTypeRadioButton,
+				Name: "PIN or password",
+			}, 15*time.Second); err != nil {
+				s.Fatal("Failed to find PIN or password radio button: ", err)
+			}
+
+			if err := ui.FindAndClick(ctx, tconn, ui.FindParams{
+				Role: ui.RoleTypeButton,
+				Name: "Set up PIN",
+			}, 15*time.Second); err != nil {
+				s.Fatal("Failed to find Set up PIN button: ", err)
+			}
+
+			if err := ui.WaitUntilExists(ctx, tconn, ui.FindParams{
+				Role: ui.RoleTypeTextField,
+				Name: "PIN",
+			}, 15*time.Second); err != nil {
+				s.Fatal("Could not find the PIN field: ", err)
+			}
+
+			if err := kb.Type(ctx, param.pin+"\n"); err != nil {
+				s.Fatal("Failed to type PIN: ", err)
+			}
+
+			if err := ui.WaitUntilExists(ctx, tconn, ui.FindParams{
+				Role: ui.RoleTypeStaticText,
+				Name: "Confirm your PIN",
+			}, 15*time.Second); err != nil {
+				s.Fatal("Could not find Confirm your PIN dialog: ", err)
+			}
+		})
+	}
+}
