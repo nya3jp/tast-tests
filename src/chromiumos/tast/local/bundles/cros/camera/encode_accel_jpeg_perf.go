e2928bc5677c6cc2d60fd6aa2a3ccd5929f3ff5a
Moja Hsu <mojahsu@google.com>
2020-10-07 15:46:46 +0800

    camera/JPEG: Only test DMA buffer case on kukui
    The jpeg encoder of kukui only supports DMA buffer usage. This CL skips
    the mmap test and only test DMA buffers on kukui.
    
    BUG=b:141516308, b:170055249, 170055249
    TEST=Test EncodeAccelJPEG* on a kodama
    
    Change-Id: I9d8add303712b3c4b6974aa8bac28b6798cd7b86

--- src/chromiumos/tast/local/bundles/cros/camera/encode_accel_jpeg_perf.go
+++ src/chromiumos/tast/local/bundles/cros/camera/encode_accel_jpeg_perf.go
@@ -24,6 +24,7 @@ import (
 	"chromiumos/tast/local/sysutil"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -35,6 +36,14 @@ func init() {
 		SoftwareDeps: []string{"chrome", caps.HWEncodeJPEG},
 		Data:         []string{"coast_3840x2160_P420.yuv"},
 		Timeout:      10 * time.Minute,
+		Params: []testing.Param{{
+			Name:              "mmap",
+			Val:               "JpegEncodeAcceleratorTest.SimpleEncode",
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform("kukui")),
+		}, {
+			Name: "dmabuf",
+			Val:  "JpegEncodeAcceleratorTest.SimpleDmaEncode",
+		}},
 	})
 }
 
@@ -42,8 +51,6 @@ func init() {
 // SimpleEncode test in jpeg_encode_accelerator_unittest.
 func EncodeAccelJPEGPerf(ctx context.Context, s *testing.State) {
 	const (
-		// GTest filter used to run JPEG encode tests.
-		filter = "JpegEncodeAcceleratorTest.SimpleEncode"
 		// Number of JPEG encodes.
 		perfJPEGEncodeTimes = 100
 		// Name of the test file used.
@@ -83,7 +90,7 @@ func EncodeAccelJPEGPerf(ctx context.Context, s *testing.State) {
 	if report, err := gtest.New(
 		filepath.Join(chrome.BinTestDir, exec),
 		gtest.Logfile(filepath.Join(s.OutDir(), exec+".log")),
-		gtest.Filter(filter),
+		gtest.Filter(s.Param().(string)),
 		gtest.ExtraArgs(
 			"--repeat="+strconv.Itoa(perfJPEGEncodeTimes),
 			"--output_log="+logPath,
