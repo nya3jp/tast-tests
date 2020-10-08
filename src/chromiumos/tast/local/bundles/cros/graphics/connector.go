c558594ad87f50c5e376e2993dc2b1db555d5159
Po-Hsien Wang <pwang@chromium.org>
2020-10-08 12:20:25 -0700

    graphics.Connector: check connector has a unique encoder.
    Instead of check HDMI-A for DP++, we check if any connector can be
    assigned a unique encoder concurrently.
    
    BUG=b:161194002
    TEST=tast run $DUT graphics.Connector
    TEST=go test
    
    Change-Id: Ia3eea0378acfd48034467b20b49be354f5bb25ea

--- src/chromiumos/tast/local/bundles/cros/graphics/connector.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/connector.go
@@ -6,17 +6,20 @@ package graphics
 
 import (
 	"context"
-	"strings"
+	"os"
+	"path/filepath"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/graph"
 	"chromiumos/tast/local/graphics"
+	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func: Connector,
-		Desc: "Checks the validity of display connector configurations.",
+		Desc: "Checks the validity of display connector configurations",
 		Contacts: []string{
 			"pwang@chromium.org",
 			"chromeos-gfx@google.com",
@@ -25,24 +28,50 @@ func init() {
 	})
 }
 
+func dumpModetestOnError(ctx context.Context, s *testing.State) {
+	if !s.HasError() {
+		return
+	}
+	file := filepath.Join(s.OutDir(), "modetest.txt")
+	f, err := os.Create(file)
+	if err != nil {
+		testing.ContextLogf(ctx, "Failed to create %s: %v", file, err)
+		return
+	}
+	defer f.Close()
+
+	cmd := testexec.CommandContext(ctx, "modetest", "-c")
+	cmd.Stdout, cmd.Stderr = f, f
+	if err := cmd.Run(); err != nil {
+		testing.ContextLog(ctx, "Failed to run modetest: ", err)
+	}
+}
+
+// Connector checks various attributes of the connectors settings via modetest.
 func Connector(ctx context.Context, s *testing.State) {
 	connectors, err := graphics.ModetestConnectors(ctx)
 	if err != nil {
 		s.Fatal("Failed to get connectors: ", err)
 	}
-	// We require that no HDMI-A connectors are exposed.
-	if err := checkHDMIA(ctx, connectors); err != nil {
-		s.Error("Failed to verify DP++: ", err)
-	}
+	defer dumpModetestOnError(ctx, s)
 
+	if err := checkUniqueEncoders(ctx, connectors); err != nil {
+		s.Error("Failed to have check unique encoders: ", err)
+	}
 }
 
-// checkHDMIA checks if any connector is named HDMI-A-*
-func checkHDMIA(ctx context.Context, connectors []*graphics.Connector) error {
+// checkUniqueEncoders checks if every connector can be assigned a unique encoder concurrently.
+func checkUniqueEncoders(ctx context.Context, connectors []*graphics.Connector) error {
+	g := graph.NewBipartite()
 	for _, connector := range connectors {
-		if strings.HasPrefix(connector.Name, "HDMI-A-") {
-			return errors.Errorf("found connector connected to HDMI-A: %v", connector)
+		for _, encoder := range connector.Encoders {
+			g.AddEdge(connector.Cid, encoder)
 		}
+	}
+
+	maxMatch := g.MaxMatching()
+	if maxMatch != len(connectors) {
+		return errors.Errorf("not all connector has a unqiue encoder matching (expect %d but got %d)", len(connectors), maxMatch)
 	}
 	return nil
 }
