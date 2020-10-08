bbfffb9b2b334e315ce96abeaa8f9388c0c37ab5
Sean Kau <skau@chromium.org>
2020-10-08 16:42:28 -0700

    printer: Add ljcolor tests
    Add a test case for an HP PPD using the ljcolor
    printer language.
    
    BUG=chromium:902142
    TEST=tast run ... printer.AddHpPrinter
    
    Change-Id: I5e3ed45c8692917b044c9049aff55f18a6f9975f

--- src/chromiumos/tast/local/bundles/cros/printer/add_hp_printer.go
+++ src/chromiumos/tast/local/bundles/cros/printer/add_hp_printer.go
@@ -22,26 +22,35 @@ func init() {
 		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "cups"},
-		Data:         []string{hpToPrintFile, hpPclmPpd, hpPclmGoldenFile},
+		Data:         []string{hpToPrintFile, hpPclmPpd, hpPclmGoldenFile, hpLjColorPpd, hpLjColorGoldenFile},
 		Pre:          chrome.LoggedIn(),
 	})
 }
 
 const (
-	// A PPD which uses the raster2dymolw filter.
+	// PDF for tests.
 	hpToPrintFile = "to_print.pdf"
 
+	// Tests using the hbpl1 printer language.
 	hpPclmPpd        = "printer_add_hp_printer_pclm.ppd.gz"
 	hpPclmGoldenFile = "printer_add_hp_printer_pclm_out.pclm"
+
+	// Tests using the ljcolor printer language.
+	hpLjColorPpd        = "printer_add_hp_ljcolor.ppd.gz"
+	hpLjColorGoldenFile = "printer_add_hp_printer_ljcolor_out.pcl"
 )
 
 func AddHpPrinter(ctx context.Context, s *testing.State) {
 	const (
 		// diffFile is the name of the file containing the diff between
 		// the golden data and actual request in case of failure.
-		pclmDiffFile = "pclm.diff"
+		pclmDiffFile    = "pclm.diff"
+		ljColorDiffFile = "ljcolor.diff"
 	)
 
-	// Tests printing with the old Ink PPDs.
+	// Tests printing with the hbpl1 PPD.
 	addtest.Run(ctx, s, hpPclmPpd, hpToPrintFile, hpPclmGoldenFile, pclmDiffFile)
+
+	// Tests printing with the ljcolor PPD.
+	addtest.Run(ctx, s, hpLjColorPpd, hpToPrintFile, hpLjColorGoldenFile, ljColorDiffFile)
 }
