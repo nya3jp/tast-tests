d3cc38aff49b95a27fc58ac7446a1e96bf4ab929
Sammie Quon <sammiequon@chromium.org>
2020-10-09 14:39:37 -0700

    tast-tests: Add DesksChainedAnimationPerf test.
    Add a test that measures performance of chaining multiple desk
    activation animations. This simulates behavior of when a user
    hits the keyboard shortcut to switch desks multiple times very
    quickly.
    
    BUG=chromium:1111445
    TEST=tast -vebose run <IP> ui.DesksChainedAnimationPerf
    
    Change-Id: I3f22a5b7778369a042f7660d24d8a5bac64b0239

---
+++ src/chromiumos/tast/local/bundles/cros/ui/desks_chained_animation_perf.go
@@ -0,0 +1,72 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/ui/perfutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DesksChainedAnimationPerf,
+		Desc:         "Measures the smoothness of a chained desk activation animation",
+		Contacts:     []string{"afakhry@chromium.org", "sammiequon@chromium.org", "chromeos-wmp@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome"},
+		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+	})
+}
+
+func DesksChainedAnimationPerf(ctx context.Context, s *testing.State) {
+	// TODO(sammiequon): When the feature is fully launched, use chrome.LoggedIn() precondition.
+	cr, err := chrome.New(ctx, chrome.ExtraArgs("--enable-features=EnhancedDeskAnimations"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	// Add three desks for a total of four. Remove them at the end of the test.
+	const numDesks = 3
+	for i := 0; i < numDesks; i++ {
+		if err = ash.CreateNewDesk(ctx, tconn); err != nil {
+			s.Error("Failed to create a new desk: ", err)
+		}
+		defer func() {
+			if err = ash.RemoveActiveDesk(ctx, tconn); err != nil {
+				s.Error("Failed to remove the active desk: ", err)
+			}
+		}()
+	}
+
+	pv := perfutil.RunMultiple(ctx, s, cr, perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
+		// Starting at desk 1, activate desk 4 by activating each adjacent desk until we reach it.
+		if err = ash.ActivateAdjacentDesksToTargetIndex(ctx, tconn, 3); err != nil {
+			return errors.Wrap(err, "failed to activate the fourth desk")
+		}
+		// Go back to desk 1.
+		if err = ash.ActivateAdjacentDesksToTargetIndex(ctx, tconn, 0); err != nil {
+			return errors.Wrap(err, "failed to activate the first desk")
+		}
+		return nil
+	},
+		"Ash.Desks.AnimationSmoothness.DeskActivation"),
+		perfutil.StoreSmoothness)
+
+	if err := pv.Save(ctx, s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
