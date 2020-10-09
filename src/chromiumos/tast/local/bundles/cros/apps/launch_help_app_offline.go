4e1b81a7243f7f60074c2d6a70779b0ea50457b3
Jun Sheng <shengjun@google.com>
2020-10-09 13:03:21 +1100

    tast-tests: promote LaunchHelpAppOffline.stable to CQ
    This test is stable in recent 20+ builds.
    https://tastboard.corp.google.com/test/apps.LaunchHelpAppOffline.stable
    
    BUG=b:170440021
    TEST=None
    
    Change-Id: I5b82764684bc193ec332f0019b1c5cb5fa5ca6a9

--- src/chromiumos/tast/local/bundles/cros/apps/launch_help_app_offline.go
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_help_app_offline.go
@@ -27,7 +27,7 @@ func init() {
 			"showoff-eng@google.com",
 			"shengjun@chromium.org", // Test author
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{
 			{
@@ -36,6 +36,7 @@ func init() {
 			}, {
 				Name:              "unstable",
 				ExtraHardwareDeps: pre.AppsUnstableModels,
+				ExtraAttr:         []string{"informational"},
 			},
 		},
 	})
