c3afa0de6e94d8485f45400bdba73e76a490cbe8
Jacek Siuda <jck@semihalf.com>
2020-10-12 09:43:17 +0000

    wificell: Add Attenuator to TestFixture and pre features
    Add multiple features to TestFixture precondition:
    multi-router and attenuator.
    
    Add automatic attenuator variable checking when Attenuator
    precondition is used.
    
    Add Attenuator support to TestFixture.
    
    BUG=chromium:1034875
    TEST=wifi.SimpleConnect.80211a, AttenuatedPerf(WiP)
    
    Change-Id: Icc7afb198c7b925c289179067f53cbb569b7015b

--- src/chromiumos/tast/remote/wificell/pre.go
+++ src/chromiumos/tast/remote/wificell/pre.go
@@ -6,17 +6,59 @@ package wificell
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"time"
 
 	"chromiumos/tast/testing"
 	"chromiumos/tast/timing"
 )
 
+// TFFeatures is enum for any extra features needed for precondition.
+type TFFeatures uint8
+
+const (
+	// NoFeatures reporesents a default value.
+	NoFeatures TFFeatures = 0
+	// Capture is a feature that spawns packet capturer in TestFixture.
+	Capture = 1 << iota
+	// MultiRouters allows to configure more than one router.
+	MultiRouters
+	// AttenuatorPresent feature facilitates attenuator handling.
+	AttenuatorPresent
+)
+
+// String returns name component corresponding to enum value(s).
+func (enum TFFeatures) String() string {
+	if enum == 0 {
+		return "default"
+	}
+	var ret string
+	if enum&Capture != 0 {
+		ret = "capture"
+		// Punch out the bit to check for weird values later.
+		enum ^= Capture
+	}
+	if enum&MultiRouters != 0 {
+		ret += "&routers"
+		enum ^= MultiRouters
+	}
+	if enum&AttenuatorPresent != 0 {
+		ret += "&attenuator"
+		enum ^= AttenuatorPresent
+	}
+	// Catch weird cases. Like when somebody extends enum, but forgets to extend this.
+	if enum != 0 {
+		panic(fmt.Sprintf("Invalid TFFeatures enum value :%d", enum))
+	}
+
+	return strings.TrimLeft(ret, "&")
+}
+
 type testFixturePreImpl struct {
 	name     string
-	extraOps []TFOption
-
-	tf *TestFixture
+	features TFFeatures
+	tf       *TestFixture
 }
 
 // String returns a short, underscore-separated name for the precondition.
@@ -58,13 +100,31 @@ func (p *testFixturePreImpl) Prepare(ctx context.Context, s *testing.PreState) i
 	var ops []TFOption
 	// Read router/pcap variable. If not available or empty, NewTestFixture
 	// will fall back to Default{Router,Pcap}Host.
-	if router, ok := s.Var("router"); ok && router != "" {
+	if p.features&MultiRouters != 0 {
+		if routers, ok := s.Var("routers"); ok && routers != "" {
+			s.Log("routers = ", routers)
+			slice := strings.Split(routers, ",")
+			ops = append(ops, TFRouter(slice...))
+		}
+	} else if router, ok := s.Var("router"); ok && router != "" {
+		s.Log("router = ", router)
 		ops = append(ops, TFRouter(router))
 	}
 	if pcap, ok := s.Var("pcap"); ok && pcap != "" {
 		ops = append(ops, TFPcap(pcap))
 	}
-	ops = append(ops, p.extraOps...)
+	// Read attenuator variable. If not available or empty, NewTestFixture
+	// will not initialize attenuator.
+	if p.features&AttenuatorPresent != 0 {
+		if atten, ok := s.Var("attenuator"); ok && atten != "" {
+			s.Log("attenuator = ", atten)
+			ops = append(ops, TFAttenuator(atten))
+		}
+	}
+	// Enable capturing.
+	if p.features&Capture != 0 {
+		ops = append(ops, TFCapture(true))
+	}
 	tf, err := NewTestFixture(ctx, s.PreCtx(), s.DUT(), s.RPCHint(), ops...)
 	if err != nil {
 		s.Fatal("Failed to set up test fixture: ", err)
@@ -88,27 +148,31 @@ func (p *testFixturePreImpl) Close(ctx context.Context, s *testing.PreState) {
 }
 
 // newTestFixturePre creates a testFixturePreImpl object to be used as Precondition.
-func newTestFixturePre(name string, extraOps ...TFOption) *testFixturePreImpl {
+func newTestFixturePre(name string, features TFFeatures) *testFixturePreImpl {
 	return &testFixturePreImpl{
 		name:     name,
-		extraOps: extraOps,
+		features: features,
 	}
 }
 
-// testFixturePre is the singleton of test fixture precondition.
-var testFixturePre = newTestFixturePre("default")
+var testFixturePre = make(map[TFFeatures]*testFixturePreImpl)
+
+// TestFixturePreWithFeatures returns a precondition of wificell TestFixture with
+// a set of features enabled.
+func TestFixturePreWithFeatures(features TFFeatures) testing.Precondition {
+	if _, ok := testFixturePre[features]; !ok {
+		testFixturePre[features] = newTestFixturePre(features.String(), features)
+	}
+	return testFixturePre[features]
+}
 
 // TestFixturePre returns a precondition of wificell TestFixture.
 func TestFixturePre() testing.Precondition {
-	return testFixturePre
+	return TestFixturePreWithFeatures(NoFeatures)
 }
-
-// testFixturePreWithCapture is the singleton of test fixture precondition with
-// TFCapture option.
-var testFixturePreWithCapture = newTestFixturePre("capture", TFCapture(true))
 
 // TestFixturePreWithCapture returns a precondition of wificell TestFixture with
 // TFCapture(true) option.
 func TestFixturePreWithCapture() testing.Precondition {
-	return testFixturePreWithCapture
+	return TestFixturePreWithFeatures(Capture)
 }
