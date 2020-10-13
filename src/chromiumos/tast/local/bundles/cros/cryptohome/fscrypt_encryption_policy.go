de3a9e3fd60cbc2620e13ec91af8452d947d1b81
Sarthak Kukreti <sarthakkukreti@chromium.org>
2020-10-13 15:28:10 -0700

    tast-tests: Mark cryptohome.FscryptEncryptionPolicy as critical
    The test has passed several builds; promote to CQ.
    
    BUG=chromium:1137811
    TEST=none
    
    Change-Id: I10d0a5e8176ab4ce5ee8944e2f433b20cdcf3f2e

--- src/chromiumos/tast/local/bundles/cros/cryptohome/fscrypt_encryption_policy.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/fscrypt_encryption_policy.go
@@ -25,7 +25,7 @@ func init() {
 			"sarthakkukreti@google.com",
 			"chromeos-storage@google.com",
 		},
-		Attr: []string{"group:mainline", "informational"},
+		Attr: []string{"group:mainline"},
 	})
 }
 
