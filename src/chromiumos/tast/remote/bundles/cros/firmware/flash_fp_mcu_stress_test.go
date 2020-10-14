1d0b172f2b9ffee4fc2c1d7c4d00a3d4de63b11b
Yicheng Li <yichengli@chromium.org>
2020-10-14 16:17:43 -0700

    flash_fp_mcu stress test
    Change-Id: I1932690d6040ab2c4354b5efcca818244c70476e

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/flash_fp_mcu_stress_test.go
@@ -0,0 +1,135 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"bytes"
+	"context"
+	"path/filepath"
+	"strings"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/lsbrelease"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+const (
+	// nocturne and nami are special cases and have "_fp" appended.
+	// Newer FPMCUs have unique names.
+	// See go/cros-fingerprint-firmware-branching-and-signing.
+	fingerprintBoardNameSuffix  = "_fp"
+	fingerprintFirmwarePathBase = "/opt/google/biod/fw/"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FlashFpMcuStressTest,
+		Desc: "Repeatedly run flash_fp_mcu, rebooting each time after flashing",
+		Contacts: []string{
+			"yichengli@chromium.org", // Test author
+			"tomhughes@chromium.org",
+			"chromeos-fingerprint@google.com",
+		},
+		Attr:         []string{"group:mainline"},
+		SoftwareDeps: []string{"biometrics_daemon"},
+		HardwareDeps: hwdep.D(hwdep.Fingerprint()),
+	})
+}
+
+func loadLsbRelease(ctx context.Context, d *dut.DUT) (map[string]string, error) {
+	out, err := d.Command("cat", "/etc/lsb-release").Output(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read /etc/lsb-release")
+	}
+	return lsbrelease.Parse(bytes.NewReader(out))
+}
+
+func getBoardByLsbRelease(ctx context.Context, d *dut.DUT) (string, error) {
+	m, err := loadLsbRelease(ctx, d)
+	if err != nil {
+		return "", err
+	}
+	return m[lsbrelease.Board], nil
+}
+
+func getBoardByCrosConfig(ctx context.Context, d *dut.DUT) (string, error) {
+	out, err := d.Command("cros_config", "/fingerprint", "board").Output(ctx)
+	return string(out), err
+}
+
+// getFpBoard returns the name of the fingerprint EC on the DUT
+func getFpBoard(ctx context.Context, d *dut.DUT) (string, error) {
+	// For devices that don't have unibuild support (which is required to
+	// use cros_config).
+	// TODO(https://crbug.com/1030862): remove when nocturne has cros_config
+	// support.
+	board, err := getBoardByLsbRelease(ctx, d)
+	if err != nil {
+		return "", err
+	}
+	if board == "nocturne" {
+		return board + fingerprintBoardNameSuffix, nil
+	}
+
+	// Use cros_config to get fingerprint board.
+	return getBoardByCrosConfig(ctx, d)
+}
+
+func getFpFirmwarePath(ctx context.Context, d *dut.DUT, fpBoard string) (string, error) {
+	cmd := "ls " + fingerprintFirmwarePathBase + fpBoard + "*.bin"
+	out, err := d.Command("bash", "-c", cmd).Output(ctx)
+	if err != nil {
+		return "", err
+	}
+	outStr := strings.TrimSpace(string(out))
+	if strings.Contains(outStr, "\n") {
+		return "", errors.Errorf("found multiple firmware files for %q", fpBoard)
+	}
+	return outStr, nil
+}
+
+func flashFpFirmware(ctx context.Context, d *dut.DUT, fpFirmwarePath string) error {
+	flashCmd := []string{"flash_fp_mcu", fpFirmwarePath}
+	testing.ContextLogf(ctx, "Running command: %q", shutil.EscapeSlice(flashCmd))
+	if err := d.Command(flashCmd[0], flashCmd[1:]...).Run(ctx); err != nil {
+		return errors.Wrap(err, "flash_fp_mcu failed")
+	}
+	testing.ContextLog(ctx, "Entropy initialized, now rebooting to rebind cros-ec-uart on zork")
+	if err := d.Reboot(ctx); err != nil {
+		return errors.Wrap(err, "failed to reboot DUT")
+	}
+	return nil
+}
+
+func FlashFpMcuStressTest(ctx context.Context, s *testing.State) {
+	d := s.DUT()
+	fpBoard, err := getFpBoard(ctx, d)
+	if err != nil {
+		return errors.Wrap(err, "failed to get fp board")
+	}
+	testing.ContextLogf(ctx, "fp board name: %q", fpBoard)
+
+	fpFirmwarePath, err := getFpFirmwarePath(ctx, d, fpBoard)
+	if err != nil {
+		return errors.Wrap(err, "failed to get fp firmware path")
+	}
+
+	for (i := 0; i < 10; i++) {
+		if err := flashFpFirmware(ctx, d, fpFirmwarePath); err != nil {
+			s.Fatalf("Failed to flash FP firmware: ", err)
+		}
+
+		// Check version and see if the FPMCU is in a good state.
+		versionCmd := []string{"ectool", "--name=cros_fp", "version"}
+		testing.ContextLogf(ctx, "Running command: %q", shutil.EscapeSlice(versionCmd))
+		out, err := d.Command(versionCmd[0], versionCmd[1:]...).Output(ctx)
+		if err != nil {
+			s.Fatalf("Failed to query FPMCU version after flashing (error: %v).", err)
+		}
+	}
+}
