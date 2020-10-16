24e8a6a81fbdbfd48c643aea77549d0c95bcbfcb
Jacek Siuda <jck@semihalf.com>
2020-10-16 09:02:49 +0000

    netperf: initial implementation
    Add netperf and netsession entities that is used
    for running netperf tests on target.
    
    Add simple firewall wrapper, used for punching holes in
    the system firewall (iptables) for passing netperf traffic.
    
    BUG=b:168441076
    TEST=AttenuatedPerf(WiP)
    
    Change-Id: I0c3d339c932ff499a0cce61f73db718146ab8adc

---
+++ src/chromiumos/tast/remote/network/cmd/util.go
@@ -0,0 +1,24 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cmd
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/ssh"
+)
+
+// GetCmdPath returns full path for the binary on the given device, defined
+// by its SSH connection.
+func GetCmdPath(ctx context.Context, conn *ssh.Conn, cmd string) (string, error) {
+	res, err := conn.Command("which", cmd).Output(ctx)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to run command 'which %s'", cmd)
+	}
+
+	return strings.TrimSpace(string(res)), nil
+}
