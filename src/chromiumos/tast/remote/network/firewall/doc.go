57ed1b0ee81c29392fc9172d4dfd6f21b12a89e7
Jacek Siuda <jck@semihalf.com>
2020-10-16 09:02:49 +0000

    netperf: initial implementation
    Add netperf and netsession entities that is used
    for running netperf tests on target.
    
    Add simple firewall wrapper, used for punching holes in
    the system firewall (iptables) for passing netperf traffic.
    
    BUG=b:168441076
    TEST=AttenuatedPerf(WiP)
    
    Change-Id: I0c3d339c932ff499a0cce61f73db718146ab8adc

---
+++ src/chromiumos/tast/remote/network/firewall/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package firewall wraps basic iptables call to control
+// filtering of incoming/outgoing traffic.
+package firewall
