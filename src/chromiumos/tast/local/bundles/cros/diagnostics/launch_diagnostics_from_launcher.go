0807453b349daaee831272f2b898a9b44228ae55
Joon Ahn <joonbug@chromium.org>
2020-10-16 15:48:00 -0700

    Diagnostics: diagnostics.LaunchDiagnosticsFromLauncher
    https://drive.google.com/file/d/1gY0-eP-5VNgr3I1OiPYYrb31fzyxWXB7/view?usp=sharing (requires Chrome PA)
    
    BUG=chromium:1125150
    TEST=tast --verbose run ${DUT}
    diagnostics.LaunchDiagnosticsFromLauncher
    
    Change-Id: I77dc3df3d5cbbd34abfc0de85f43a8a76d97a90b

---
+++ src/chromiumos/tast/local/bundles/cros/diagnostics/launch_diagnostics_from_launcher.go
@@ -0,0 +1,73 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package diagnostics
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui/diagnosticsapp"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/ui/launcher"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LaunchDiagnosticsFromLauncher,
+		Desc: "Diagnostics app can be found and launched from the launcher",
+		Contacts: []string{
+			"joonbug@chromium.org",
+			"cros-lax@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// LaunchDiagnosticsFromLauncher verifies launching diagnostics app from the launcher.
+func LaunchDiagnosticsFromLauncher(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("DiagnosticsApp"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	if err := launcher.OpenLauncher(ctx, tconn); err != nil {
+		s.Fatal("Failed to open launcher: ", err)
+	}
+
+	// Search for "diagnostic".
+	if err := launcher.Search(ctx, tconn, "diagnostic"); err != nil {
+		s.Fatal("Failed to search for diagnostics: ", err)
+	}
+
+	// Diagnostics app should be one of the search results.
+	appNode, err := launcher.WaitForAppResult(ctx, tconn, "Diagnostics", 15*time.Second)
+	if err != nil {
+		s.Fatal("Diagnostics app does not exist in search result: ", err)
+	}
+
+	// Clicking that result should open the Diagnostics app.
+	if err := appNode.LeftClick(ctx); err != nil {
+		s.Fatal("Failed to launch app from search result: ", err)
+	}
+
+	// App should be launched at the overview page.
+	if err := diagnosticsapp.WaitForApp(ctx, tconn); err != nil {
+		s.Fatal("Failed to launch diagnostics app: ", err)
+	}
+}
