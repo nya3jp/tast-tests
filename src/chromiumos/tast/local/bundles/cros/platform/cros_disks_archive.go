d0d58d77596bdefe015810dd84814bbafdec5894
Sergei Datsenko <dats@google.com>
2020-10-19 16:43:15 +1100

    cros-disks: Port archive autotests to tast
    BUG=chromium:955858
    TEST=platform.CrosDisksArchive
    
    Change-Id: I1e1eba1142faf55a583b58d005a7950100bb983b

---
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_disks_archive.go
@@ -0,0 +1,29 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/platform/crosdisks"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosDisksArchive,
+		Desc: "Verifies CrosDisks mounts various archive types",
+		Contacts: []string{
+			"fdegros@chromium.org",
+			"jboulic@chromium.org",
+		},
+		Attr: []string{"group:mainline", "informational"},
+		Data: crosdisks.PreparedArchives,
+	})
+}
+
+func CrosDisksArchive(ctx context.Context, s *testing.State) {
+	crosdisks.RunArchiveTests(ctx, s)
+}
