c6b007ca9ed036b92f41cb7a53a86c41085fcc12
Michael Checo <michaelcheco@google.com>
2020-10-21 17:54:45 +0000

    Print Management:printmanagement.LaunchPrintmanagementFromLauncher
    BUG=1053704
    TEST=tast --verbose run ${DUT} printmanagement.LaunchPrintManagementFromLauncher
    
    Change-Id: I04133a0d40989b04c144e6d80b790c5594b91340

---
+++ src/chromiumos/tast/local/bundles/cros/printmanagement/launch_printmanagement_from_launcher.go
@@ -0,0 +1,58 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package printmanagement
+
+import (
+        "context"
+        "time"
+
+        "chromiumos/tast/ctxutil"
+        "chromiumos/tast/local/apps"
+        "chromiumos/tast/local/chrome"
+        "chromiumos/tast/local/chrome/ui/faillog"
+        "chromiumos/tast/local/chrome/ui/launcher"
+        "chromiumos/tast/local/chrome/ui/printmanagementapp"
+        "chromiumos/tast/testing"
+)
+
+func init() {
+        testing.AddTest(&testing.Test{
+                Func: LaunchPrintmanagementFromLauncher,
+                Desc: "Print management app can be found and launched from the launcher",
+                Contacts: []string{
+                        "michaelcheco@google.com",
+                        "cros-peripherals@google.com",
+                },
+                Attr:         []string{"group:mainline", "informational"},
+                SoftwareDeps: []string{"chrome"},
+        })
+}
+
+// LaunchPrintmanagementFromLauncher verifies launching the print management app from the launcher.
+func LaunchPrintmanagementFromLauncher(ctx context.Context, s *testing.State) {
+        cr, err := chrome.New(ctx)
+        if err != nil {
+                s.Fatal("Failed to start Chrome: ", err)
+        }
+
+        cleanupCtx := ctx
+        ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+        defer cancel()
+
+        tconn, err := cr.TestAPIConn(ctx)
+        if err != nil {
+                s.Fatal("Failed to connect Test API: ", err)
+        }
+        defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+        if err := launcher.SearchAndLaunch(ctx, tconn, apps.PrintManagement.Name); err != nil {
+                s.Fatal("Failed to open launcher and search for Print Jobs: ", err)
+        }
+
+        // App should be launched.
+        if err := printmanagementapp.WaitForApp(ctx, tconn); err != nil {
+                s.Fatal("Failed to launch print management app: ", err)
+        }
+}
