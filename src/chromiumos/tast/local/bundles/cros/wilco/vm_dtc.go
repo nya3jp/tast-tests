7395a1dae339f855db63d2dcfc47c0475d30976e
Andrey Pronin <apronin@chromium.org>
2020-10-22 17:29:33 -0700

    tast: mark wilco.VMStart and wilco.VMDTC informational
    This CL makes the abovementioned tests informational temporarily due to
    BUG.
    
    BUG=chromium:1139749
    TEST=none
    
    Change-Id: I82968262daa1a49ff674680cad75d82441eb36ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2492915
    Tested-by: Andrey Pronin <apronin@chromium.org>
    Auto-Submit: Andrey Pronin <apronin@chromium.org>
    Reviewed-by: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Eric Caruso <ejcaruso@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/wilco/vm_dtc.go
+++ src/chromiumos/tast/local/bundles/cros/wilco/vm_dtc.go
@@ -26,7 +26,7 @@ func init() {
 			"lamzin@chromium.org", // wilco_dtc_supportd author
 			"chromeos-wilco@google.com",
 		},
-		Attr:         []string{"group:mainline"},
+		Attr:         []string{"group:mainline", "informational"}, // crbug.com/1139749
 		SoftwareDeps: []string{"vm_host", "wilco"},
 	})
 }
