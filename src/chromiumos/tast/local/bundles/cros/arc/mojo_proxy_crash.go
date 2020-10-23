3146f095c634226f291599cd18bbf60e072fa956
Ryo Hashimoto <hashimoto@google.com>
2020-10-23 19:01:03 +0900

    arc: Add a new test arc.MojoProxyCrash.vm
    This test verifies that stopping the mojo proxy results in stopping the
    existing VM and starting a new one.
    
    BUG=b:171456596
    TEST=tast run arc.MojoProxyCrash.vm
    
    Change-Id: I0fd199cc0e2739b03be83b8f9e240028c96d9803

---
+++ src/chromiumos/tast/local/bundles/cros/arc/mojo_proxy_crash.go
@@ -0,0 +1,90 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MojoProxyCrash,
+		Desc:         "Test mojo proxy crash handling",
+		Contacts:     []string{"hashimoto@chromium.org", "arc-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		Timeout: 10 * time.Minute,
+	})
+}
+
+func MojoProxyCrash(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ARCEnabled())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer func() {
+		if err := cr.Close(ctx); err != nil {
+			s.Fatal("Failed to close Chrome while (re)booting ARC: ", err)
+		}
+	}()
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer func() {
+		if a != nil {
+			a.Close()
+		}
+	}()
+
+	oldPID, err := arc.InitPID()
+	if err != nil {
+		s.Fatal("Failed to get init PID before reboot: ", err)
+	}
+
+	// Stopping the mojo proxy should result in Android reboot.
+	s.Log("Stopping the proxy process")
+	if err := upstart.StopJob(ctx, "arcvm-server-proxy"); err != nil {
+		s.Fatal("Failed to stop the proxy process: ", err)
+	}
+
+	s.Log("Waiting for old init process to exit")
+	if err = testing.Poll(ctx, func(ctx context.Context) error {
+		if r, _ := arc.InitExists(); r {
+			return errors.New("Init still exists")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 60 * time.Second}); err != nil {
+		s.Fatal("Failed to wait for old init process to exit: ", err)
+	}
+
+	a.Close()
+
+	a, err = arc.New(ctx, s.OutDir())
+	if err != nil {
+		// We can assume a == nil at this point.
+		s.Fatal("Failed to restart ARC: ", err)
+	}
+
+	newPID, err := arc.InitPID()
+	if err != nil {
+		s.Fatal("Failed to get init PID after reboot: ", err)
+	}
+	if newPID == oldPID {
+		s.Fatal("Failure: init PID did not change")
+	}
+}
