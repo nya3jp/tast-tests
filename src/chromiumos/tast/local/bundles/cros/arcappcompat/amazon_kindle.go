22a5a678ff3cb350ad7c2e1a1c879743f8f90d26
Ross Zwisler <zwisler@google.com>
2020-10-23 16:59:15 -0600

    tast-tests: stop arcappcompat.AmazonKindle
    This test is failing on the majority of its runs.
    Disable it until it can be fixed.
    
    BUG=b:171598061
    TEST='fast_build.sh -T'
    
    Change-Id: Idc1116d13b513cc35dfb29005238cba173a0fa48

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/amazon_kindle.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/amazon_kindle.go
@@ -37,10 +37,11 @@ var touchviewTestsForAmazonKindle = []testutil.TestCase{
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         AmazonKindle,
-		Desc:         "Functional test for AmazonKindle that installs the app also verifies it is logged in and that the main page is open, checks AmazonKindle correctly changes the window state in both clamshell and touchview mode",
-		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:appcompat"},
+		Func:     AmazonKindle,
+		Desc:     "Functional test for AmazonKindle that installs the app also verifies it is logged in and that the main page is open, checks AmazonKindle correctly changes the window state in both clamshell and touchview mode",
+		Contacts: []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		// TODO(http://b/171598061): Test is disabled until it can be fixed
+		// Attr:         []string{"group:appcompat"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Val:               clamshellTestsForAmazonKindle,
