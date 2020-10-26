4f6cd992e70c76d7128f0de0db09f7b7c5ac6b77
Kimiyuki Onaka <kimiyuki@google.com>
2020-10-26 15:58:16 +0900

    tast-tests: Add arc.NativeCrashStartup
    BUG=b:169627491
    TEST=tast run my_crbook arc.NativeCrashStartup
    
    Change-Id: If5454e14de55dc95d19a4443a77b13ac2d9ac5c4

---
+++ src/chromiumos/tast/local/bundles/cros/arc/native_crash_startup.go
@@ -0,0 +1,122 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"path/filepath"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/arccrash"
+	"chromiumos/tast/local/crash"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NativeCrashStartup,
+		Desc:         "Test handling of a native binary crash which happens before ArcCrashCollector starts",
+		Contacts:     []string{"kimiyuki@google.com", "arc-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          arc.Booted(),
+		Params: []testing.Param{{
+			Name:              "real_consent",
+			ExtraSoftwareDeps: []string{"android_vm", "metrics_consent"},
+			Val:               crash.RealConsent,
+		}, {
+			Name:              "mock_consent",
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Val:               crash.MockConsent,
+		}},
+	})
+}
+
+func NativeCrashStartup(ctx context.Context, s *testing.State) {
+	a := s.PreValue().(arc.PreData).ARC
+	cr := s.PreValue().(arc.PreData).Chrome
+
+	opt := crash.WithMockConsent()
+	if s.Param().(crash.ConsentType) == crash.RealConsent {
+		opt = crash.WithConsent(cr)
+	}
+
+	if err := crash.SetUpCrashTest(ctx, opt); err != nil {
+		s.Fatal("Failed to set up crash test: ", err)
+	}
+	defer crash.TearDownCrashTest(ctx)
+
+	// We need to get BuildProperty before rebooting ARCVM because the `a` struct is invalidated by rebooting.
+	s.Log("Getting BuildProperty")
+	bp, err := arccrash.GetBuildProp(ctx, a)
+	if err != nil {
+		if err := arccrash.UploadSystemBuildProp(ctx, a, s.OutDir()); err != nil {
+			s.Error("Failed to get build.prop: ", err)
+		}
+		s.Fatal("Failed to get BuildProperty: ", err)
+	}
+
+	// To make the situtation that there are some logs when ARCVM boots, stop ArcCrashCollector service, make crashes while it stops, and reboot ARCVM. We cannot kill the service because it restarts automatically.
+	s.Log("Stop ArcCrashCollector service")
+	if err := a.Command(ctx, "pkill", "-STOP", "-f", "org\\.chromium\\.arc\\.crash_collector").Run(); err != nil {
+		s.Fatal("Failed to stop ArcCrashCollector service: ", err)
+	}
+
+	s.Log("Making crash")
+	cmd := a.Command(ctx, "/system/bin/sh", "-c", "kill -SEGV $$")
+	if err := cmd.Run(); err != nil {
+		// The shell returns 139 (= 128 + 11) when it's terminated by SIGSEGV (= 11).
+		if cmd.ProcessState.ExitCode() != 139 {
+			s.Fatal("Failed to crash: ", err)
+		}
+	} else {
+		s.Fatal("Failed to crash: the process has successfully finished without crashing")
+	}
+
+	s.Log("Running reboot command via ADB")
+	if err := a.Command(ctx, "reboot").Run(); err != nil {
+		s.Fatal("Failed to run reboot command via ADB: ", err)
+	}
+
+	s.Log("Getting crash dir path")
+	user := cr.User()
+	path, err := cryptohome.UserPath(ctx, user)
+	if err != nil {
+		s.Fatal("Couldn't get user path: ", err)
+	}
+	crashDir := filepath.Join(path, "/crash")
+
+	s.Log("Waiting for crash files to become present")
+	// Wait files like sh.20200420.204845.664107.dmp in crashDir
+	const stem = `sh\.\d{8}\.\d{6}\.\d+`
+	metaFileName := stem + crash.MetadataExt
+	files, err := crash.WaitForCrashFiles(ctx, []string{crashDir}, []string{
+		stem + crash.MinidumpExt, metaFileName,
+	})
+	if err != nil {
+		s.Fatal("Failed to find files: ", err)
+	}
+	defer crash.RemoveAllFiles(ctx, files)
+
+	metaFiles := files[metaFileName]
+	if len(metaFiles) > 1 {
+		s.Errorf("Unexpectedly saw %d crashes. Saving for debugging", len(metaFiles))
+		crash.MoveFilesToOut(ctx, s.OutDir(), metaFiles...)
+	}
+	// WaitForCrashFiles guarantees that there will be a match for all regexes if it succeeds,
+	// so this must exist.
+	metaFile := metaFiles[0]
+
+	s.Log("Validating the meta file")
+	isValid, err := arccrash.ValidateBuildProp(ctx, metaFile, bp)
+	if err != nil {
+		s.Fatal("Failed to validate meta file: ", err)
+	}
+	if !isValid {
+		s.Error("validateBuildProp failed. Saving meta file")
+		crash.MoveFilesToOut(ctx, s.OutDir(), metaFile)
+	}
+}
