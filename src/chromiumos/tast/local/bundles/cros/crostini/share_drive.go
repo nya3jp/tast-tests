4249e1fcb4f812cfc02494a339d8857ad8f90501
Judy Wu <jinrongwu@google.com>
2020-10-27 20:43:41 +1100

    Test add files to Drive when sharing with Crostini
    Crostini should have access to subfolders and files in Drive when
    shared. Crostini should not have execution permission.
    
    BUG=chromium:1101583
    TEST=On Hatch
    
    Change-Id: I1f57bd746cde8edd7f9ae9431d36f13e5f91f153

--- src/chromiumos/tast/local/bundles/cros/crostini/share_drive.go
+++ src/chromiumos/tast/local/bundles/cros/crostini/share_drive.go
@@ -6,16 +6,24 @@ package crostini
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"time"
 
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/crostini/listset"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ui/filesapp"
 	"chromiumos/tast/local/crostini"
 	"chromiumos/tast/local/crostini/ui/settings"
 	"chromiumos/tast/local/crostini/ui/sharedfolders"
+	"chromiumos/tast/local/drivefs"
 	"chromiumos/tast/local/vm"
 	"chromiumos/tast/testing"
 )
@@ -45,24 +53,13 @@ func init() {
 func ShareDrive(ctx context.Context, s *testing.State) {
 	tconn := s.PreValue().(crostini.PreData).TestAPIConn
 	cont := s.PreValue().(crostini.PreData).Container
+	cr := s.PreValue().(crostini.PreData).Chrome
 
 	// Use a shortened context for test operations to reserve time for cleanup.
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 30*time.Second)
 	defer cancel()
 	defer crostini.RunCrostiniPostTest(ctx, s.PreValue().(crostini.PreData))
-
-	// Open Files app.
-	filesApp, err := filesapp.Launch(ctx, tconn)
-	if err != nil {
-		s.Fatal("Failed to open Files app: ", err)
-	}
-	// Close the Files app in the end.
-	defer func() {
-		if err := filesApp.Close(cleanupCtx); err != nil {
-			s.Error("Failed to close the Files app: ", err)
-		}
-	}()
 
 	sharedFolders := sharedfolders.NewSharedFolders()
 	// Clean up shared folders in the end.
@@ -72,13 +69,95 @@ func ShareDrive(ctx context.Context, s *testing.State) {
 		}
 	}()
 
-	if err := sharedFolders.ShareDriveOK(ctx, filesApp, tconn); err != nil {
-		s.Fatal("Failed to share Google Drive: ", err)
-	}
+	s.Run(ctx, "test_share_drive", func(ctx context.Context, s *testing.State) {
+		// Open Files app.
+		filesApp, err := filesapp.Launch(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to open Files app: ", err)
+		}
+		// Close the Files app in the end.
+		defer func() {
+			if err := filesApp.Close(cleanupCtx); err != nil {
+				s.Error("Failed to close the Files app: ", err)
+			}
+		}()
 
-	if err := checkDriveResults(ctx, tconn, cont); err != nil {
-		s.Fatal("Failed to verify sharing results: ", err)
-	}
+		if err := sharedFolders.ShareDriveOK(ctx, filesApp, tconn); err != nil {
+			s.Fatal("Failed to share Google Drive: ", err)
+		}
+
+		if err := checkDriveResults(ctx, tconn, cont); err != nil {
+			s.Fatal("Failed to verify sharing results: ", err)
+		}
+	})
+
+	s.Run(ctx, "add_files_to_Drive", func(ctx context.Context, s *testing.State) {
+		const (
+			testFile   = "testD.sh"
+			testString = "ls\n"
+		)
+		testFolder := fmt.Sprintf("testFolderD_%d", rand.Intn(1000000000))
+
+		mountPath, err := drivefs.WaitForDriveFs(ctx, cr.User())
+		if err != nil {
+			s.Fatal("Failed waiting for DriveFS to start: ", err)
+		}
+		drivefsRoot := filepath.Join(mountPath, "root")
+		folderPath := filepath.Join(drivefsRoot, testFolder)
+
+		// Delete newly created files in the end.
+		defer os.RemoveAll(folderPath)
+
+		// Add a file and a folder in Drive.
+		if err := os.MkdirAll(folderPath, 0755); err != nil {
+			s.Fatal("Failed to create test folder in Drive: ", err)
+		}
+		if err := ioutil.WriteFile(filepath.Join(folderPath, testFile), []byte(testString), 0644); err != nil {
+			s.Fatal("Failed to create file in Drive: ", err)
+		}
+
+		// Check file list in the container.
+		filesInCont, err := cont.GetFileList(ctx, sharedfolders.MountPathMyDrive)
+		if err != nil {
+			s.Fatal("Failed to get file list of /mnt/chromeos/MyFiles/MyDrive: ", err)
+		}
+		list, err := ioutil.ReadDir(drivefsRoot)
+		if err != nil {
+			s.Fatal("Failed to read files in Drive: ", err)
+		}
+		var filesInDrive []string
+		for _, f := range list {
+			filesInDrive = append(filesInDrive, f.Name())
+		}
+		if err := listset.CheckListsMatch(filesInCont, filesInDrive...); err != nil {
+			s.Fatal("Failed to verify the files list in container: ", err)
+		}
+
+		shared, err := cont.GetFileList(ctx, filepath.Join(sharedfolders.MountPathMyDrive, testFolder))
+		if err != nil {
+			s.Fatal("Failed to get file list of /mnt/chromeos/MyFiles/MyDrive: ", err)
+		}
+		if want := []string{testFile}; !reflect.DeepEqual(shared, want) {
+			s.Fatalf("Failed to verify shared folders list, got %s, want %s", shared, want)
+		}
+
+		sharedFilePath := filepath.Join(sharedfolders.MountPathMyDrive, testFolder, testFile)
+
+		// Check the content of the test file in the container.
+		if err := cont.CheckFileContent(ctx, sharedFilePath, testString); err != nil {
+			s.Fatal("Failed to verify the content of the test file: ", err)
+		}
+
+		// Check the file does not have execution permission.
+		result, err := cont.Command(ctx, "ls", "-l", sharedFilePath).Output()
+		if err != nil {
+			s.Fatal("Failed to run ls on the test file in the container: ", err)
+		}
+		permission := strings.Split(string(result), " ")[0]
+		if strings.Contains(permission, "x") {
+			s.Fatalf("Failed to verify the permission of shared file, got %s, want %s", permission, "-rw-rw----")
+		}
+	})
 }
 
 func checkDriveResults(ctx context.Context, tconn *chrome.TestConn, cont *vm.Container) error {
