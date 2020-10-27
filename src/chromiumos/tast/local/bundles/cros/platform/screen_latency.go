f5d7d60ef1b0f953aedb252ae8a2a909976f844c
Michael Pishchagin <mblsha@google.com>
2020-10-27 12:20:54 +0000

    tast-tests: Add platform.ScreenLatency test.
    Requires a companion app to be installed on an Android device with a slo-mo
    camera, and the device should be authorized in "adb devices" on the DUT.
    
    After running the test the shutter button needs to be manually pressed on an
    Android device.
    
    BUG=chromium:1143250
    TEST=tast run platform.ScreenLatency
    
    Change-Id: Ib2a7edf46dbab9d96a34af2454f2d330d5968fa9

---
+++ src/chromiumos/tast/local/bundles/cros/platform/screen_latency.go
@@ -0,0 +1,55 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"net"
+
+	"chromiumos/tast/local/bundles/cros/platform/screenlatency"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ScreenLatency,
+		Desc:         "Tests latency between pressing a key and having it shown on a screen",
+		Contacts:     []string{"mblsha@google.com"},
+		Attr:         []string{"group:input-tools"},
+		SoftwareDeps: []string{},
+		Params:       []testing.Param{},
+	})
+}
+
+// ScreenLatency uses a companion Android app to measure latency between a
+// key press being simulated and a character appearing on the screen.
+//
+// TODO(mblsha): See the http://go/project-slate-handover for future direction info.
+func ScreenLatency(ctx context.Context, s *testing.State) {
+	keyboard, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to create keyboard device: ", err)
+	}
+
+	ln, _ := net.Listen("tcp", "127.0.0.1:")
+	_, serverPort, _ := net.SplitHostPort(ln.Addr().String())
+	testing.ContextLog(ctx, "Listening on address: ", ln.Addr())
+
+	openAppCommand := testexec.CommandContext(ctx, "adb", "shell", "am", "start", "-n",
+		"com.android.example.camera2.slowmo/com.example.android.camera2.slowmo.CameraActivity",
+		"--es", "port "+serverPort)
+	if err := openAppCommand.Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Failed to start companion Android app using adb")
+	}
+
+	portForwardingCommand := testexec.CommandContext(ctx, "adb", "reverse", "tcp:"+serverPort, "tcp:"+serverPort)
+	if err := portForwardingCommand.Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Failed to initiate TCP connection with a companion Android app")
+	}
+
+	screenlatency.CommunicateWithCompanionApp(ctx, s, ln, keyboard)
+}
