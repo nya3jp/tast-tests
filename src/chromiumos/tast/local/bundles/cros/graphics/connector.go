d077cb57cbc363702481d24867c1a4de4a9f6726
Po-Hsien Wang <pwang@chromium.org>
2020-10-27 13:06:55 -0700

    graphics: update Connector test to use parameters
    Split the test into default one (run in CQ to prevent future
    board to support DP+) and unstable one.
    
    BUG=b:161194002
    TEST=tast run $DUT graphics.Connector
    
    Change-Id: I75b26d93da8c00062826a13c6e440813cae09716

--- src/chromiumos/tast/local/bundles/cros/graphics/connector.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/connector.go
@@ -14,7 +14,44 @@ import (
 	"chromiumos/tast/local/graphics"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
+
+// dpPlusModels are models that have DPPlus support in BIOS.
+var dpPlusModels = []string{
+	"asuka",
+	"chell",
+	"cave",
+	"caroline",
+	// drallion family
+	"drallion", "drallion360",
+	// TODO(b:16119031): Unmark dedede once it is fixed.
+	// dedede family
+	"drawlat", "drawman",
+	// fizz family
+	"teemo", "jax",
+	"guado",
+	"karma",
+	"lars",
+	"nautilus",
+	"nocturne",
+	// nami family
+	"vayne", "sona", "pantheon", "ekko", "bard", "akali", "akali360", "syndra",
+	// octopus family
+	"dorp",
+	// puff family
+	"wyvern", "puff", "nooibat", "kaisa", "faffy", "duffy",
+	// rammus family
+	"shyvana", "leona",
+	"rikku",
+	"sentry",
+	"soraka",
+	// sarien family
+	"sarien", "arcada",
+	"tidus",
+	// volteer family
+	"volteer", "volteer2", "delbin",
+}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -25,6 +62,15 @@ func init() {
 			"chromeos-gfx@google.com",
 		},
 		Attr: []string{"group:mainline", "informational"},
+		Params: []testing.Param{
+			{
+				Name:              "",
+				ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(dpPlusModels...)),
+			}, {
+				Name:              "unstable",
+				ExtraHardwareDeps: hwdep.D(hwdep.Model(dpPlusModels...)),
+			},
+		},
 	})
 }
 
