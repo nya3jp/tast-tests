285f677dfb8faea21e47be39d543839f64cc525c
Pranav Batra <batrapranav@chromium.org>
2020-10-30 00:32:48 +0000

    Move printer proxy tests from crosint to cros
    BUG=none
    TEST=tast run localhost:9222 printer.Proxy*
    
    Cq-Depend: chrome-internal:3371230
    Change-Id: Ia94ed1995b820a817115200fa29f559a05a89cdd

---
+++ src/chromiumos/tast/local/bundles/cros/printer/proxy_add_epson_printer.go
@@ -0,0 +1,61 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package printer
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/printer/proxylpprint"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ProxyAddEpsonPrinter,
+		Desc: "Verifies the lp command enqueues print jobs with Epson config",
+		Contacts: []string{
+			"batrapranav@chromium.org",
+			"cros-printing-dev@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "cups", "plugin_vm"},
+		Data:         []string{proxyEpsonPPDFile, proxyEpsonModPPD, proxyEpsonToPrintFile, proxyEpsonGoldenFile, proxyEpsonColorGoldenFile, proxyEpsonMonochromeGoldenFile},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+const (
+	// epsonPPDFile is ppd.gz file to be registered via debugd.
+	proxyEpsonPPDFile = "printer_add_epson_printer_EpsonWF3620.ppd"
+
+	proxyEpsonModPPD = "printer_add_epson_printer_EpsonGenericColorModel.ppd"
+
+	// epsonToPrintFile is a PDF file to be printed.
+	proxyEpsonToPrintFile = "to_print.pdf"
+
+	// epsonGoldenFile contains a golden LPR request data.
+	proxyEpsonGoldenFile = "printer_add_epson_printer_golden.ps"
+
+	proxyEpsonColorGoldenFile      = "printer_add_epson_printer_color_golden.bin"
+	proxyEpsonMonochromeGoldenFile = "printer_add_epson_printer_monochrome_golden.bin"
+)
+
+func ProxyAddEpsonPrinter(ctx context.Context, s *testing.State) {
+	const (
+		// diffFile is the name of the file containing the diff between
+		// the golden data and actual request in case of failure.
+		diffFile           = "printer_add_epson_printer_diff.txt"
+		colorDiffFile      = "color.diff"
+		monochromeDiffFile = "monochrome.diff"
+	)
+
+	// Tests printing with the old Ink PPDs.
+	proxylpprint.Run(ctx, s, proxyEpsonPPDFile, proxyEpsonToPrintFile, proxyEpsonGoldenFile, diffFile)
+
+	// Tests printing with the modified ColorModel PPD in color and monochrome.
+	proxylpprint.RunWithOptions(ctx, s, proxyEpsonModPPD, proxyEpsonToPrintFile, proxyEpsonColorGoldenFile, colorDiffFile, "print-color-mode=color")
+	proxylpprint.RunWithOptions(ctx, s, proxyEpsonModPPD, proxyEpsonToPrintFile, proxyEpsonMonochromeGoldenFile, monochromeDiffFile, "print-color-mode=monochrome")
+}
