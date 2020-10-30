61e6a633572f05d62f066dd79a590aab2d6c27b3
Billy Zhao <billyzhao@google.com>
2020-10-30 17:56:55 +0000

    tast-tests: Add tast.PersistenceWifiSansBluetooth
    PersistenceWifiSansBluetooth is the dual test of
    PersistenceBluetoothSansWifi. This test checks to make sure that wifi
    enables properly if bluetooth is disabled on boot.
    
    BUG=b:161902990
    TEST=test_that ${DUT} wifi.PersistenceWifiSansBluetooth (board: hatch)
    
    Cq-Depend: chromium:2500445
    Cq-Depend: chrome-internal:3205694
    Change-Id: Idc543bd62c9f110d644d2acfa26075d55d0cdf5d

--- src/chromiumos/tast/local/bundles/cros/network/bluetooth_service.go
+++ src/chromiumos/tast/local/bundles/cros/network/bluetooth_service.go
@@ -6,12 +6,15 @@ package network
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bluetooth"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/services/cros/network"
 	"chromiumos/tast/testing"
 )
@@ -29,36 +32,100 @@ type BluetoothService struct {
 	s *testing.ServiceState
 }
 
-// SetBluetoothPowered sets the Bluetooth adapter power status. This setting does not persist across reboots.
+// SetBluetoothPowered sets the Bluetooth adapter power status. This setting persists across reboots.
 func (s *BluetoothService) SetBluetoothPowered(ctx context.Context, req *network.SetBluetoothPoweredRequest) (*empty.Empty, error) {
-	adapters, err := bluetooth.Adapters(ctx)
+	cr, err := chrome.New(
+		ctx,
+		chrome.KeepState(),
+		chrome.NoLogin(),
+		chrome.EnableStartupWindow(), // TODO (billyzhao; crrev/c/2511791): This flag is necessary for the bluetooth preference to persist
+		// on startup. It is unclear why this flag is necessary.
+
+		chrome.LoadSigninProfileExtension(req.Credentials),
+	)
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to get Bluetooth adapters")
+		return nil, errors.Wrap(err, "failed to start Chrome")
 	}
-	if len(adapters) != 1 {
-		return nil, errors.Errorf("got %d adapters, expected 1 adapter", len(adapters))
+	defer cr.Close(ctx)
+	tLoginConn, err := cr.SigninProfileTestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create login test API connection")
 	}
-	if err := adapters[0].SetPowered(ctx, req.Powered); err != nil {
-		return nil, errors.Wrap(err, "couldn't set Bluetooth powered state")
+	defer tLoginConn.Close()
+
+	expr := fmt.Sprintf(
+		`new Promise(function(resolve, reject) {
+		  chrome.bluetoothPrivate.setAdapterState(
+		      {powered: %s}, function() {
+		    resolve(chrome.runtime.lastError ? chrome.runtime.lastError.message : "");
+		  });
+		})`, strconv.FormatBool(req.Powered))
+
+	msg := ""
+	if err = tLoginConn.EvalPromise(ctx, expr, &msg); err != nil {
+		return nil, errors.Wrap(err, "failed to get display info")
+	} else if msg != "" {
+		return nil, errors.New(msg)
+	}
+
+	if err := tLoginConn.Call(ctx, nil, `tast.promisify(chrome.settingsPrivate.setPref)`, "ash.system.bluetooth.adapter_enabled", req.Powered); err != nil {
+		return nil, err
+	}
+	var enabled struct {
+		Value bool `json:"value"`
+	}
+	if err := tLoginConn.Call(ctx, &enabled, "tast.promisify(chrome.settingsPrivate.getPref)", "ash.system.bluetooth.adapter_enabled"); err != nil {
+		return nil, err
+	}
+
+	if enabled.Value != req.Powered {
+		return nil, errors.Errorf("bad VALUE, wanted %s, got %s", strconv.FormatBool(req.Powered), strconv.FormatBool(enabled.Value))
 	}
 	return &empty.Empty{}, nil
 }
 
-// GetBluetoothPowered checks whether the Bluetooth adapter is enabled.
-func (s *BluetoothService) GetBluetoothPowered(ctx context.Context, _ *empty.Empty) (*network.GetBluetoothPoweredResponse, error) {
+// GetBluetoothPowered checks whether the Bluetooth adapter is enabled as well as the Bluetooth boot preference.
+func (s *BluetoothService) GetBluetoothPowered(ctx context.Context, req *network.GetBluetoothPoweredRequest) (*network.GetBluetoothPoweredResponse, error) {
+	cr, err := chrome.New(
+		ctx,
+		chrome.KeepState(),
+		chrome.NoLogin(),
+		chrome.EnableStartupWindow(), // TODO (billyzhao; crrev/c/2511791): This flag is necessary for the bluetooth preference to persist
+		// on startup. It is unclear why this flag is necessary.
+		chrome.LoadSigninProfileExtension(req.Credentials),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start Chrome")
+	}
+	defer cr.Close(ctx)
+	tLoginConn, err := cr.SigninProfileTestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create login test API connection")
+	}
+	defer tLoginConn.Close()
+
+	// Get Bluetooth pref.
+	var enabled struct {
+		Value bool `json:"value"`
+	}
+	if err := tLoginConn.Call(ctx, &enabled, "tast.promisify(chrome.settingsPrivate.getPref)", "ash.system.bluetooth.adapter_enabled"); err != nil {
+		return nil, err
+	}
+
+	// Get Bluetooth status.
 	adapters, err := bluetooth.Adapters(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to get Bluetooth adapters")
 	}
 
 	if len(adapters) != 1 {
-		return &network.GetBluetoothPoweredResponse{Powered: false}, nil
+		return &network.GetBluetoothPoweredResponse{Powered: false, Persistent: enabled.Value}, nil
 	}
 	res, err := adapters[0].Powered(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not get Bluetooth power state")
 	}
-	return &network.GetBluetoothPoweredResponse{Powered: res}, nil
+	return &network.GetBluetoothPoweredResponse{Powered: res, Persistent: enabled.Value}, nil
 }
 
 // ValidateBluetoothFunctional checks to see whether the Bluetooth device is usable.
