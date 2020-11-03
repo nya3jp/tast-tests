73ea8cbc609dba984f8c0155ac42a782806ab974
alexanderhartl <alexanderhartl@google.com>
2020-11-03 12:03:56 +0000

    tast-tests: add AutoclickEnabled
    Add the policy.AutoclickEnabled tast test.
    
    BUG=None
    TEST=tast run <ip> policy.AutoclickEnabled
    
    Change-Id: I1ea6fe42fef71f7f16d8bc6db5cc0320285a3336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2520784
    Tested-by: Alexander Hartl <alexanderhartl@google.com>
    Commit-Queue: Alexander Hartl <alexanderhartl@google.com>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/policy/autoclick_enabled.go
@@ -0,0 +1,153 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/ui/mouse"
+	"chromiumos/tast/local/coords"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AutoclickEnabled,
+		Desc: "Behavior of AutoclickEnabled policy: checking if autoclick is enabled or not",
+		Contacts: []string{
+			"alexanderhartl@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+	})
+}
+
+// AutoclickEnabled tests the AutoclickEnabled policy.
+func AutoclickEnabled(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	// Connect to Test API to use it with the UI library.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	for _, param := range []struct {
+		name           string
+		value          *policy.AutoclickEnabled
+		wantButton     bool
+		wantChecked    ui.CheckedState
+		wantRestricted bool
+	}{
+		{
+			name:           "unset",
+			value:          &policy.AutoclickEnabled{Stat: policy.StatusUnset},
+			wantButton:     false,
+			wantChecked:    ui.CheckedStateFalse,
+			wantRestricted: false,
+		},
+		{
+			name:           "disabled",
+			value:          &policy.AutoclickEnabled{Val: false},
+			wantButton:     false,
+			wantChecked:    ui.CheckedStateFalse,
+			wantRestricted: true,
+		},
+		{
+			name:           "enabled",
+			value:          &policy.AutoclickEnabled{Val: true},
+			wantButton:     true,
+			wantChecked:    ui.CheckedStateTrue,
+			wantRestricted: true,
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.value}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			// Find the system tray button node.
+			stNode, err := ui.FindWithTimeout(ctx, tconn, ui.FindParams{
+				Role:      ui.RoleTypeButton,
+				ClassName: "UnifiedSystemTray",
+			}, 15*time.Second)
+			if err != nil {
+				s.Fatal("Failed to find the system tray button: ", err)
+			}
+			defer stNode.Release(ctx)
+
+			// Move mouse to the middle of the system tray button.
+			c := coords.Point{
+				X: stNode.Location.Left + stNode.Location.Width/2,
+				Y: stNode.Location.Top + stNode.Location.Height/2,
+			}
+
+			if err := mouse.Move(ctx, tconn, c, 0); err != nil {
+				s.Fatal("Failed to move the mouse: ", err)
+			}
+
+			// Check if a click occurred by checking whether the Sign out button is visible or not.
+			if err := ui.WaitUntilExistsStatus(ctx, tconn, ui.FindParams{
+				Name:      "Sign out",
+				ClassName: "SignOutButton",
+			}, param.wantButton, 30*time.Second); err != nil {
+				s.Error("Could not confirm the desired status of the Sign out button: ", err)
+			}
+
+			// Open settings page where the affected toggle button can be found.
+			sconn, err := cr.NewConn(ctx, "chrome://os-settings/osAccessibility")
+			if err != nil {
+				s.Fatal("Failed to connect to the accessibility settings page: ", err)
+			}
+			defer sconn.Close()
+
+			// Find and click manage accessibility link.
+			if err := ui.StableFindAndClick(ctx, tconn, ui.FindParams{
+				Role: ui.RoleTypeLink,
+				Name: "Manage accessibility features",
+			}, &testing.PollOptions{Timeout: 15 * time.Second}); err != nil {
+				s.Fatal("Failed to find and click Manage accessibility features link: ", err)
+			}
+
+			// Find the toggle button node.
+			params := ui.FindParams{
+				Role: ui.RoleTypeToggleButton,
+				Name: "Automatically click when the cursor stops",
+			}
+			node, err := ui.FindWithTimeout(ctx, tconn, params, 15*time.Second)
+			if err != nil {
+				s.Fatal("Finding Automatically click when the cursor stops node failed: ", err)
+			}
+			defer node.Release(ctx)
+
+			// Check the restriction setting of the toggle button.
+			if restricted := (node.Restriction == ui.RestrictionDisabled || node.Restriction == ui.RestrictionReadOnly); restricted != param.wantRestricted {
+				s.Logf("The restriction attribute is %q", node.Restriction)
+				s.Errorf("Unexpected toggle button restriction in the settings: got %t; want %t", restricted, param.wantRestricted)
+			}
+
+			if node.Checked != param.wantChecked {
+				s.Errorf("Unexpected toggle button checked state in the settings: got %s; want %s", node.Checked, param.wantChecked)
+			}
+		})
+	}
+}
