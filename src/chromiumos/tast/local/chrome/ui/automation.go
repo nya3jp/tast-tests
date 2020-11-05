c0a0eea2f54969b3cef56a6274f3a086443a9891
Mohamed Omar <mohamedaomar@google.com>
2020-11-05 22:21:20 +0100

    ui: New method to find the selected radio button
    Add a new method to find the selected radio button under
    radio group.
    
    BUG=None
    TEST=tast run <ip> policy.SafeBrowsingProtectionLevel
    
    Change-Id: Ib5db87cc65a39b52a4786c6f7b4ecf6253249c10

--- src/chromiumos/tast/local/chrome/ui/automation.go
+++ src/chromiumos/tast/local/chrome/ui/automation.go
@@ -665,3 +665,46 @@ func LogRootDebugInfo(ctx context.Context, tconn *chrome.TestConn, filename stri
 	}
 	return ioutil.WriteFile(filename, []byte(debugInfo), 0644)
 }
+
+// ErrNoRadioButton is returned when there are no radio buttons under the radio group.
+var ErrNoRadioButton = errors.New("no radio buttons in this radio group")
+
+// ErrNoActiveRadioButton is returned when no active radio group is selected.
+var ErrNoActiveRadioButton = errors.New("no active radio button is selected")
+
+// FindSelectedRadioButton finds the selected radio button under a radio group node.
+// If no active radio button is selected, an error is returned.
+func (n *Node) FindSelectedRadioButton(ctx context.Context) (*Node, error) {
+	if n.Role != RoleTypeRadioGroup {
+		return nil, errors.New("the current node is not a radio group")
+	}
+
+	rbexists, err := n.DescendantExists(ctx, FindParams{Role: RoleTypeRadioButton})
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find radio buttons")
+	}
+	if rbexists == false {
+		return nil, ErrNoRadioButton
+	}
+
+	srbexists, err := n.DescendantExists(ctx, FindParams{
+		Role:       RoleTypeRadioButton,
+		Attributes: map[string]interface{}{"checked": CheckedStateTrue},
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find selected radio button")
+	}
+	if srbexists == false {
+		return nil, ErrNoActiveRadioButton
+	}
+
+	sb, err := n.Descendant(ctx, FindParams{
+		Role:       RoleTypeRadioButton,
+		Attributes: map[string]interface{}{"checked": CheckedStateTrue},
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't find selected radio buttons")
+	}
+	defer sb.Release(ctx)
+	return sb, nil
+}
