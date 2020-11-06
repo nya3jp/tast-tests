3fa2820a1ba0a98f277ee1d5616fc2ebf5fe3231
David Haddock <dhaddock@google.com>
2020-11-06 01:59:22 -0800

    Fix and reenable unicorn login test.
    Test was failing because the buttons no longer have role=button
    attribute. Changed to read the data-email attribute instead.
    The test used to live in the private repo but it doesn't need to be
    there so now the test will be in the public repo.
    
    BUG=chromium:1097949
    TEST=unicorn.Login
    
    Change-Id: Id770fe8a23b02b1e3f3762fd3e2dd617bb2a0ec0

--- src/chromiumos/tast/local/bundles/cros/main.go
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -53,6 +53,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/session"
 	_ "chromiumos/tast/local/bundles/cros/storage"
 	_ "chromiumos/tast/local/bundles/cros/ui"
+	_ "chromiumos/tast/local/bundles/cros/unicorn"
 	_ "chromiumos/tast/local/bundles/cros/video"
 	_ "chromiumos/tast/local/bundles/cros/vm"
 	_ "chromiumos/tast/local/bundles/cros/webrtc"
---
+++ src/chromiumos/tast/local/bundles/cros/unicorn/login.go
@@ -0,0 +1,39 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package unicorn is used for writing Unicorn tests.
+package unicorn
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Login,
+		Desc:         "Checks if Unicorn login is working",
+		Contacts:     []string{"chromeos-sw-engprod@google.com", "cros-oac@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Vars:         []string{"unicorn.parentUser", "unicorn.parentPassword", "unicorn.childUser", "unicorn.childPassword"},
+	})
+}
+
+func Login(ctx context.Context, s *testing.State) {
+	parentUser := s.RequiredVar("unicorn.parentUser")
+	parentPass := s.RequiredVar("unicorn.parentPassword")
+	childUser := s.RequiredVar("unicorn.childUser")
+	childPass := s.RequiredVar("unicorn.childPassword")
+
+	cr, err := chrome.New(ctx, chrome.GAIALogin(),
+		chrome.Auth(childUser, childPass, "gaia-id"),
+		chrome.ParentAuth(parentUser, parentPass))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+}
