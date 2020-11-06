49de779aad178f346a13d169d48d1a5bdd2aeaf8
Ben Reich <benreich@chromium.org>
2020-11-06 05:52:10 +0000

    tast-tests: Test Drag Drop from Files app
    Verify that a file dragged from the Files app can be dropped onto a
    target on a separate Chrome app. The Chrome app was modified from the
    arc.DragDrop test to listen for a drop event, interrogate the dropped
    file and pass it back via the document.window.title.
    
    BUG=chromium:1145322
    TEST=tast run <DUT-IP> filemanager.DragDrop
    
    Change-Id: Id32c92596bb47929841b8cc078df7df29f324b27

---
+++ src/chromiumos/tast/local/bundles/cros/filemanager/data/drag_drop_background.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+chrome.app.window.create("window.html", {
+  outerBounds: {
+    width: 300,
+    height: 300,
+    left: 0,
+    top: 0,
+  },
+  alwaysOnTop: true,
+  focused: true,
+});
