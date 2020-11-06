ea6bc1944022ebcbd19e6d9a783b31dfd894c7a6
meenakshi thiyagarajan <mthiyagarajan@google.com>
2020-11-05 19:03:50 -0800

    Run UIAutomator Test Using Tast For Pbs Kids Video app
    BUG=b:172087056
    TEST=tast run <eve> arcappcompat.PbsKidsVideo
    
    Tast Test for PbsKidsVideo app verifies launch app
    from the playstore, check app reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: I2ee4085df886c5cfc09a2260dae53df750a18d18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2522584
    Tested-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Commit-Queue: Meenakshi Thiyagarajan <mthiyagarajan@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/pbs_kids_video.go
@@ -0,0 +1,89 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForPbsKidsVideo = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForPbsKidsVideo},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForPbsKidsVideo = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForPbsKidsVideo},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PbsKidsVideo,
+		Desc:         "Functional test for Pbs Kids Video that install, launch the app and check that the main page is open, also checks Pbs Kids Video correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForPbsKidsVideo,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForPbsKidsVideo,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForPbsKidsVideo,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForPbsKidsVideo,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// PbsKidsVideo test uses library for opting into the playstore and installing app.
+// Checks PbsKidsVideo correctly changes the window states in both clamshell and touchview mode.
+func PbsKidsVideo(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "org.pbskids.video"
+		appActivity = ".activities.SplashActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForPbsKidsVideo verifies PbsKidsVideo is logged in and
+// verify PbsKidsVideo reached main activity page of the app.
+func launchAppForPbsKidsVideo(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const homeID = "org.pbskids.video:id/live_tv_container"
+
+	// Check for home icon.
+	homeIcon := d.Object(ui.ID(homeID))
+	if err := homeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("homeIcon doesn't exist: ", err)
+	}
+}
