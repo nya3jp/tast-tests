8fc02f11725fce60ba9655b4eb9a8c149b36d46e
Yen-lin Lai <yenlinlai@google.com>
2020-11-06 15:23:28 +0800

    wifi: Port network_WiFi_ChannelHop to Tast.
    Port the test to Tast as wifi.ChannelHop.
    
    BUG=b:172785860
    TEST=tast run $DUT wifi.ChannelHop
    
    Change-Id: I5309265ff60879b0f1a73afbbd47617532e65a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2525255
    Tested-by: Yen-lin Lai <yenlinlai@google.com>
    Commit-Queue: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/wifiutil/cleanup_util.go
@@ -0,0 +1,46 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifiutil
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/shillconst"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/services/cros/network"
+)
+
+const waitServiceIdleTime = 30 * time.Second
+
+// WaitServiceIdle waits for the service in shill on DUT going to idle state
+// for at most waitServiceIdleTime. This is useful for tests to ensure a clean
+// state when starting or to leaving one part of verification.
+// NOTE: If the test is to verify a specific disconnection, spawn watcher with
+// tf.ExpectShillProperty before the trigger might be preferred.
+func WaitServiceIdle(ctx context.Context, tf *wificell.TestFixture, servicePath string) error {
+	ctx, cancel := context.WithTimeout(ctx, waitServiceIdleTime)
+	defer cancel()
+	props := []*wificell.ShillProperty{{
+		Property:       shillconst.ServicePropertyState,
+		ExpectedValues: []interface{}{shillconst.ServiceStateIdle},
+		Method:         network.ExpectShillPropertyRequest_CHECK_WAIT,
+	}}
+	wait, err := tf.ExpectShillProperty(ctx, servicePath, props, nil)
+	if err != nil {
+		return errors.Wrap(err, "failed to watch service state")
+	}
+	if _, err := wait(); err != nil {
+		return errors.Wrap(err, "failed to wait for service idle")
+	}
+	return nil
+}
+
+// ReserveForWaitServiceIdle reserve time for WaitServiceIdle call.
+func ReserveForWaitServiceIdle(ctx context.Context) (context.Context, context.CancelFunc) {
+	return ctxutil.Shorten(ctx, waitServiceIdleTime)
+}
