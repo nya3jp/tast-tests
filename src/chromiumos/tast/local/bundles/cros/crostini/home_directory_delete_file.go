214be97f405f14e2675491aa1dc440b2cb24e10e
Matthew Warton <mwarton@google.com>
2020-11-10 10:40:00 +1100

    crostini: promote two tests to the CQ
    Both of these tests only have 3 failures showing on tastboard, none of
    which are related to these tests.  Two are exonerated by a tast-wide
    marking (shown in grey) and the other failure occurred before any
    crostini code started to run (and on a non-CQ board).
    
    https://tastboard.corp.google.com/test/crostini.GPUEnabled.gpu_artifact
    
    https: //tastboard.corp.google.com/test/crostini.HomeDirectoryDeleteFile.artifact
    
    BUG=None
    TEST=Ran both tests on hatch
    
    Change-Id: I240dcb98c4c808aaf6beda3615505c2395772ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2528549
    Tested-by: Matthew Warton <mwarton@google.com>
    Reviewed-by: Judy Wu <jinrongwu@google.com>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: Matthew Warton <mwarton@google.com>

--- src/chromiumos/tast/local/bundles/cros/crostini/home_directory_delete_file.go
+++ src/chromiumos/tast/local/bundles/cros/crostini/home_directory_delete_file.go
@@ -24,7 +24,7 @@ func init() {
 		Func:         HomeDirectoryDeleteFile,
 		Desc:         "Test deleting a file in Linux files and container using a pre-built crostini image",
 		Contacts:     []string{"jinrongwu@google.com", "cros-containers-dev@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		Vars:         []string{"keepState"},
 		SoftwareDeps: []string{"chrome", "vm_host"},
 		Params: []testing.Param{
