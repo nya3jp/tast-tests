37825aab2342fd5ffd5560ab5839f9859e712e6b
Yi Chou <yich@google.com>
2020-11-10 13:35:57 +0800

    tast-test: use EnsureTPMIsResetAndPowerwash to reset TPM
    These tast-tests need more stable TPM reset process.
    
    BUG=b:172876417
    TEST=tast run $DUT hwsec.*Perf
    TEST=tast run $DUT hwsec.KeysetTiedToTPM1
    TEST=tast run $DUT hwsec.CryptohomeKeysAndMountSanity
    TEST=tast run $DUT hwsec.InstallAttributes
    TEST=tast run $DUT hwsec.LoginDuringTakingOwnership
    
    Change-Id: Ia8d1c0e5f501e2c1b84db533745ec92548f1e698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2526755
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>
    Tested-by: joe Chou <yich@google.com>
    Commit-Queue: joe Chou <yich@google.com>

--- src/chromiumos/tast/remote/bundles/cros/hwsec/check_key_perf.go
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/check_key_perf.go
@@ -47,7 +47,7 @@ func CheckKeyPerf(ctx context.Context, s *testing.State) {
 
 	// Reset TPM
 	s.Log("Start resetting TPM if needed")
-	if err := helper.EnsureTPMIsReset(ctx); err != nil {
+	if err := helper.EnsureTPMIsResetAndPowerwash(ctx); err != nil {
 		s.Fatal("Failed to ensure resetting TPM: ", err)
 	}
 	s.Log("TPM is confirmed to be reset")
--- src/chromiumos/tast/remote/bundles/cros/hwsec/cryptohome_perf.go
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/cryptohome_perf.go
@@ -109,7 +109,7 @@ func clearOwnership(ctx context.Context, s *testing.State) {
 	}
 
 	s.Log("Start resetting TPM if needed")
-	if err := helper.EnsureTPMIsReset(ctx); err != nil {
+	if err := helper.EnsureTPMIsResetAndPowerwash(ctx); err != nil {
 		s.Fatal("Failed to ensure resetting TPM: ", err)
 	}
 	s.Log("TPM is confirmed to be reset")
