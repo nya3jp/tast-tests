bba812e35db2a16c1cdc93ec4dd3affb64456ee3
meenakshi thiyagarajan <mthiyagarajan@google.com>
2020-11-09 17:55:06 -0800

    Run UIAutomator Test Using Tast For Starz app
    BUG=b:172087102
    TEST=tast run <eve> arcappcompat.Starz
    
    Tast Test for Starz app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Cq-Depend: chrome-internal:3392571
    Change-Id: Iaeed44c47e2aa758d095763f567b0e8aa50c945b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2528693
    Tested-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Commit-Queue: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/starz.go
@@ -0,0 +1,239 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForStarz = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForStarz},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForStarz = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForStarz},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Starz,
+		Desc:         "Functional test for Starz that installs the app also verifies it is logged in and that the main page is open, checks Starz correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForStarz,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForStarz,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForStarz,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForStarz,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password", "arcappcompat.Starz.emailid", "arcappcompat.Starz.password"},
+	})
+}
+
+// Starz test uses library for opting into the playstore and installing app.
+// Checks Starz correctly changes the window states in both clamshell and touchview mode.
+func Starz(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.bydeluxe.d3.android.program.starz"
+		appActivity = "com.starz.handheld.SplashActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForStarz verifies Starz is logged in and
+// verify Starz reached main activity page of the app.
+func launchAppForStarz(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		dismissButtonID     = "android:id/button2"
+		enterEmailAddressID = "com.bydeluxe.d3.android.program.starz:id/email_et"
+		loginButtonID       = "com.bydeluxe.d3.android.program.starz:id/login_link"
+		loginID             = "com.bydeluxe.d3.android.program.starz:id/login_button"
+		menuID              = "com.bydeluxe.d3.android.program.starz:id/action_more"
+		notNowID            = "android:id/autofill_save_no"
+		passwordID          = "com.bydeluxe.d3.android.program.starz:id/password_et"
+		homeID              = "com.bydeluxe.d3.android.program.starz:id/action_home"
+	)
+
+	// Check for home icon.
+	homeIcon := d.Object(ui.ID(homeID))
+	if err := homeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("homeIcon doesn't exists: ", err)
+	}
+
+	// Check for login button.
+	loginButton := d.Object(ui.ID(loginButtonID))
+	if err := loginButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("LoginButton doesn't exist: ", err)
+	}
+
+	// Press until KEYCODE_DPAD_DOWN login button is focused.
+	// Press KEYCODE_DPAD_RIGHT and KEYCODE_ENTER to click on login button.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if loginBtnFocused, err := loginButton.IsFocused(ctx); err != nil {
+			return errors.New("login button not focused yet")
+		} else if !loginBtnFocused {
+			d.PressKeyCode(ctx, ui.KEYCODE_DPAD_DOWN, 0)
+			return errors.New("login button not focused yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Log("Failed to focus on login button: ", err)
+	} else if err := d.PressKeyCode(ctx, ui.KEYCODE_DPAD_RIGHT, 0); err != nil {
+		s.Fatal("Failed to click on KEYCODE_DPAD_RIGHT button: ", err)
+	} else if err := d.PressKeyCode(ctx, ui.KEYCODE_ENTER, 0); err != nil {
+		s.Fatal("Failed to click on KEYCODE_ENTER button: ", err)
+	}
+
+	// Click on enter email address.
+	enterEmailAddress := d.Object(ui.ID(enterEmailAddressID))
+	if err := enterEmailAddress.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("enterEmailAddress doesn't exist: ", err)
+	} else if err := enterEmailAddress.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterEmailAddress: ", err)
+	}
+
+	// Click on emailid text field until the emailid text field is focused.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if emailIDFocused, err := enterEmailAddress.IsFocused(ctx); err != nil {
+			return errors.New("email text field not focused yet")
+		} else if !emailIDFocused {
+			enterEmailAddress.Click(ctx)
+			return errors.New("email text field not focused yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Fatal("Failed to focus EmailId: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	username := s.RequiredVar("arcappcompat.Starz.emailid")
+	if err := kb.Type(ctx, username); err != nil {
+		s.Fatal("Failed to enter username: ", err)
+	}
+	s.Log("Entered username")
+
+	// Enter password.
+	enterPassword := d.Object(ui.ID(passwordID))
+	if err := enterPassword.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Error("EnterPassword does not exist: ", err)
+	} else if err := enterPassword.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterPassword: ", err)
+	}
+	// Click on password text field until the password text field is focused.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if pwdFocused, err := enterPassword.IsFocused(ctx); err != nil {
+			return errors.New("password text field not focused yet")
+		} else if !pwdFocused {
+			enterPassword.Click(ctx)
+			return errors.New("password text field not focused yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Fatal("Failed to focus password: ", err)
+	}
+
+	password := s.RequiredVar("arcappcompat.Starz.password")
+	if err := kb.Type(ctx, password); err != nil {
+		s.Fatal("Failed to enter password: ", err)
+	}
+	s.Log("Entered password")
+
+	// Click on signIn Button until not now button exist.
+	signInButton := d.Object(ui.ID(loginID))
+	notNowButton := d.Object(ui.ID(notNowID))
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := notNowButton.Exists(ctx); err != nil {
+			signInButton.Click(ctx)
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Log("notNowButton doesn't exist: ", err)
+	} else if err := notNowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on notNowButton: ", err)
+	}
+
+	// Click on dimiss button to save password.
+	dimissButton := d.Object(ui.ID(dismissButtonID))
+	if err := dimissButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("dimissButton doesn't exists: ", err)
+	} else if err := dimissButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on dimissButton: ", err)
+	}
+
+	// Check for menu icon.
+	menuIcon := d.Object(ui.ID(menuID))
+	if err := menuIcon.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("menuIcon doesn't exist: ", err)
+	} else {
+		signOutOfStarz(ctx, s, tconn, a, d, appPkgName, appActivity)
+	}
+
+}
+
+// signOutOfStarz verifies app is signed out.
+func signOutOfStarz(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		menuID   = "com.bydeluxe.d3.android.program.starz:id/action_more"
+		logoutID = "com.bydeluxe.d3.android.program.starz:id/settings_bttn"
+	)
+
+	// Click on menu icon.
+	menuIcon := d.Object(ui.ID(menuID))
+	if err := menuIcon.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("menuIcon doesn't exist: ", err)
+	} else if err := menuIcon.Click(ctx); err != nil {
+		s.Fatal("Failed to click on menuIcon: ", err)
+	}
+
+	// Click on log out of Starz.
+	logOutOfStarz := d.Object(ui.ID(logoutID))
+	if err := logOutOfStarz.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("logOutOfStarz doesn't exist: ", err)
+	} else if err := logOutOfStarz.Click(ctx); err != nil {
+		s.Fatal("Failed to click on logOutOfStarz: ", err)
+	}
+}
