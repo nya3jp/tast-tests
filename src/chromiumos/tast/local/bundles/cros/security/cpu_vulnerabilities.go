5cf5b5b7c5bdec9e92434fa7f67782974d054bb9
Stephen Boyd <swboyd@chromium.org>
2020-11-12 15:25:45 -0800

    security: Add a test for CPU vulnerabilities
    Add a test to make sure that all the files in
    /sys/devices/system/cpu/vulnerabilities don't contain the term
    "vulnerable".
    
    BUG=b:173225016
    TEST=tast run $IP security.CPUVulnerabilities
    
    Change-Id: I1db3d073597a1783d5ca0c364a30950711bf31c2

---
+++ src/chromiumos/tast/local/bundles/cros/security/cpu_vulnerabilities.go
@@ -0,0 +1,46 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"path/filepath"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CPUVulnerabilities,
+		Desc: "Confirm CPU vulnerabilities are mitigated",
+		Contacts: []string{
+			"swboyd@chromium.org", // Tast author
+			"chromeos-security@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func CPUVulnerabilities(ctx context.Context, s *testing.State) {
+	vulnDir := "/sys/devices/system/cpu/vulnerabilities/"
+	fileList, err := ioutil.ReadDir(vulnDir)
+	if err != nil {
+		s.Fatal("Failed to list vulnerability files: ", err)
+	}
+	for _, f := range fileList {
+		fName := f.Name()
+		contents, err := ioutil.ReadFile(filepath.Join(vulnDir, fName))
+		if err != nil {
+			s.Fatal("Can't read vulnerability file: ", err)
+		}
+		contents = bytes.TrimSpace(contents)
+		s.Logf("%s: %s", fName, contents)
+		if bytes.EqualFold(contents, []byte("vulnerable")) {
+			s.Errorf("File %q has CPU vulnerabilities", fName)
+		}
+	}
+}
