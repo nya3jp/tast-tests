1e6ca61414c652e897c3a41ec3e5a923c54f490e
Billy Zhao <billyzhao@google.com>
2020-11-13 00:56:01 +0000

    TAST: port network_WiFi_WMM to wifi.WMM
    Adds a test which verifies that the router and DUT supports various QOS
    levels.
    
    BUG=b:173155003
    TEST=tast run ${DUT} wifi.WMM (hatch)
    
    Change-Id: I51dae0aa29ca5ec29aee95c69bb81d6d46f34df4

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/wmm.go
@@ -0,0 +1,77 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+
+	"chromiumos/tast/common/network/ping"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/remote/wificell/hostapd"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        WMM,
+		Desc:        "Verifies that the router and the DUT can handle multiple QoS levels",
+		Contacts:    []string{"billyzhao@google.com", "chromeos-platform-connectivity@google.com"},
+		Attr:        []string{"group:wificell", "wificell_func", "wificell_unstable"},
+		ServiceDeps: []string{wificell.TFServiceName},
+		Pre:         wificell.TestFixturePre(),
+		Vars:        []string{"router", "pcap"},
+	})
+}
+
+func WMM(ctx context.Context, s *testing.State) {
+	tf := s.PreValue().(*wificell.TestFixture)
+	defer func(ctx context.Context) {
+		if err := tf.CollectLogs(ctx); err != nil {
+			s.Log("Error collecting logs, err: ", err)
+		}
+	}(ctx)
+	ctx, cancel := tf.ReserveForCollectLogs(ctx)
+	defer cancel()
+
+	s.Log("Setting up AP")
+	apOps := []hostapd.Option{hostapd.Channel(6), hostapd.Mode(hostapd.Mode80211g)}
+	ap, err := tf.ConfigureAP(ctx, apOps, nil)
+	if err != nil {
+		s.Fatal("Failed to configure ap: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := tf.DeconfigAP(ctx, ap); err != nil {
+			s.Error("Failed to deconfig ap: ", err)
+		}
+	}(ctx)
+	ctx, cancel = tf.ReserveForDeconfigAP(ctx, ap)
+	defer cancel()
+
+	s.Log("Connecting to WiFi")
+	if _, err := tf.ConnectWifiAP(ctx, ap); err != nil {
+		s.Fatal("Failed to connect to WiFi: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := tf.CleanDisconnectWifi(ctx); err != nil {
+			s.Error("Failed to disconnect WiFi: ", err)
+		}
+	}(ctx)
+	ctx, cancel = tf.ReserveForDisconnect(ctx)
+	defer cancel()
+
+	s.Log("Start verification")
+	// Check connectivity for each QOS configuration.
+	qosTypes := [4]ping.QOSType{ping.QOSBK, ping.QOSBE, ping.QOSVI, ping.QOSVO}
+	for _, qos := range qosTypes {
+		if err := tf.PingFromDUT(ctx, ap.ServerIP().String(), ping.QOS(qos)); err != nil {
+			s.Fatalf("Failed to ping from DUT with QoS configuration %x: %v", qos, err)
+		}
+
+		if err := tf.PingFromServer(ctx, ping.QOS(qos)); err != nil {
+			s.Fatalf("Failed to ping from the Server with QoS configuration %x: %v", qos, err)
+		}
+	}
+	s.Log("Verified; tearing down")
+}
