97c56b70f16f0124b8bc5b60f4f558f1ed2b0fbe
Anthony Hinton <hinton@google.com>
2020-11-13 11:52:33 -0800

    Create wifipeer.go to add wifipeer hosts to DUTs.
    This creates a wifipeer.go file that uses the DUT.WifiPeerHost() call to
    create wifipeer devices. It's mainly going to be used for mutli-device
    802.11ax testing.  But it could be used by anyone who needs multiple ChromeOS
    devices for a test.
    
    BUG=b:171728381
    TEST=local unittest using fast_builds.sh  doesn't mock out IPs, so tests on
    real hardware.
    
    Change-Id: I86c5afe660f9b6406813a1fde4068be96e2d7eb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2537182
    Tested-by: Anthony Hinton <hinton@google.com>
    Commit-Queue: Anthony Hinton <hinton@google.com>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

---
+++ src/chromiumos/tast/remote/wificell/wifipeer/wifipeer.go
@@ -0,0 +1,33 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package wifipeer builds and controls peer Chrome OS devices for Wi-Fi tests.
+package wifipeer
+
+import (
+	"context"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/ssh"
+)
+
+// MakePeers constructs the peer devices needed for the tests.
+func MakePeers(ctx context.Context, testdut *dut.DUT, count int) (peers []*ssh.Conn, retErr error) {
+	defer func() {
+		if retErr != nil {
+			for _, peer := range peers {
+				peer.Close(ctx)
+			}
+		}
+	}()
+	for i := 1; i <= count; i++ {
+		peer, err := testdut.WifiPeerHost(ctx, i)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to connet to the peer %d", i)
+		}
+		peers = append(peers, peer)
+	}
+	return peers, nil
+}
