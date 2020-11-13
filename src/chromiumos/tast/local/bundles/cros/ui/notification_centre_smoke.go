657f90558393c1b7edc62f31286ee355a6710e23
David Haddock <dhaddock@google.com>
2020-11-13 12:47:58 -0800

    Fix notificationcentresmoke.
    It is likely the mouse cursor on top of the notification causing it
    not to dismiss. I have tested locally by putting the cursor there and
    the test fails. And the screenshot doesn't include the cursor matching
    what we see in lab failure screenshots.
    
    BUG=chromium:1048435
    TEST=ui.NotificationCentreSmoke
    
    Change-Id: I8c4012c1f1bbbd4ae8f568b21b281f9698ee2805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2538077
    Tested-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Commit-Queue: David Haddock <dhaddock@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/notification_centre_smoke.go
+++ src/chromiumos/tast/local/bundles/cros/ui/notification_centre_smoke.go
@@ -13,7 +13,9 @@ import (
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/ui/mouse"
 	"chromiumos/tast/local/chrome/ui/quicksettings"
+	"chromiumos/tast/local/coords"
 	"chromiumos/tast/testing"
 )
 
@@ -42,6 +44,11 @@ func NotificationCentreSmoke(ctx context.Context, s *testing.State) {
 	}
 	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
 
+	// Move mouse cursor to top left corner so it doesn't interfere with notification dismissal.
+	if err := mouse.Click(ctx, tconn, coords.NewPoint(0, 0), mouse.LeftButton); err != nil {
+		s.Fatal("Failed to move mouse to top left hand corner: ", err)
+	}
+
 	s.Log("Creating notification")
 	if _, err := ash.CreateTestNotification(ctx, tconn, ash.NotificationTypeBasic, "TestNotification1", "blahhh"); err != nil {
 		s.Fatal("Failed to create test notification: ", err)
@@ -52,11 +59,11 @@ func NotificationCentreSmoke(ctx context.Context, s *testing.State) {
 		ClassName: "ash/message_center/MessagePopup",
 	}
 	if err := ui.WaitUntilExists(ctx, tconn, params, uiTimeout); err != nil {
-		s.Fatal("Failed to find notification center: ", err)
+		s.Fatal("Failed to find notification popup: ", err)
 	}
 	s.Log("Waiting for notification to auto-dismiss")
 	if err := ui.WaitUntilGone(ctx, tconn, params, uiTimeout); err != nil {
-		s.Fatal("Failed to find notification center: ", err)
+		s.Fatal("Failed waiting for notification to auto-dismiss: ", err)
 	}
 	s.Log("Open quick settings")
 	if err := quicksettings.Show(ctx, tconn); err != nil {
