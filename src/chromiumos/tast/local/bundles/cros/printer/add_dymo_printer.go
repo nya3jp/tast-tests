8655a7bf3a9c1c1bd4a3f0e0501d3a268073bbd5
Pranav Batra <batrapranav@chromium.org>
2020-11-14 01:02:15 +0000

    tast.printer.*: Promote informational tests into CQ
    BUG=none
    TEST=none
    
    Change-Id: I6f0d1d4c841e9a170b26c61f8565cd72fdc6267a

--- src/chromiumos/tast/local/bundles/cros/printer/add_dymo_printer.go
+++ src/chromiumos/tast/local/bundles/cros/printer/add_dymo_printer.go
@@ -19,7 +19,7 @@ func init() {
 			"batrapranav@chromium.org",
 			"cros-printing-dev@chromium.org",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"cros_internal", "cups"},
 		Data:         []string{dymolwPPD, dymolmPPD, dymoToPrintFile, dymolwGoldenFile, dymolmGoldenFile},
 	})
