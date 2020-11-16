1000b41db16a25eae1bfb3a724ec0db83f1f2bd8
Yury Khmel <khmel@chromium.org>
2020-11-16 17:24:51 +0000

    Revert "tast-tests: stop arc.AuthPerf.managed*"
    This reverts commit 74b64b01222e9d75568c338d31d7a45582879132.
    
    Reason for revert: arc.AuthPerf.managed is critical perf test and we do monitor. It fails on many bots however it still has green runs on
    reference boards.
    
    Original change's description:
    > tast-tests: stop arc.AuthPerf.managed*
    >
    > tast.arc.AuthPerf.managed and tast.arc.AuthPerf.managed_vm fail on 100%
    > of their runs. Disable them until they can be fixed.
    >
    > BUG=b:161260610
    > TEST='fast_build.sh -T'
    >
    > Change-Id: I4377018075f1dc1d725219af225672951e586fb2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2508873
    > Tested-by: Ross Zwisler <zwisler@google.com>
    > Auto-Submit: Ross Zwisler <zwisler@google.com>
    > Reviewed-by: Satoshi Niwa <niwa@chromium.org>
    > Reviewed-by: Keigo Oka <oka@chromium.org>
    > Commit-Queue: Ross Zwisler <zwisler@google.com>
    
    Bug: b:161260610
    Change-Id: I2f6c75a61f288e9addd6ed3e5b4af89b193bf2c6

--- src/chromiumos/tast/local/bundles/cros/arc/auth_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/auth_perf.go
@@ -46,14 +46,12 @@ func init() {
 			"niwa@chromium.org",  // Tast port author.
 			"arc-performance@google.com",
 		},
-		// TODO(http://b/161260610): Test is disabled until it can be fixed
-		// Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome", "chrome_internal"},
 		// This test steps through opt-in flow 10 times and each iteration takes 20~40 seconds.
 		Timeout: 20 * time.Minute,
 		Params: []testing.Param{{
 			Name:              "unmanaged",
-			ExtraAttr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 			ExtraSoftwareDeps: []string{"android_p"},
 			Val: testParam{
 				username:          "arc.AuthPerf.unmanaged_username",
@@ -63,7 +61,6 @@ func init() {
 			},
 		}, {
 			Name:              "unmanaged_vm",
-			ExtraAttr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 			ExtraSoftwareDeps: []string{"android_vm"},
 			Val: testParam{
 				username:          "arc.AuthPerf.unmanaged_username",
