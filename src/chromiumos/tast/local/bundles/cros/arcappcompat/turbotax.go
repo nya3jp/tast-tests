1fe35941af8ac2838979b753753dfdba679e4bab
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-11-17 12:12:26 -0800

    Run UIAutomator Test Using Tast For Turbotax app
    BUG=b:172087973
    TEST=tast run <eve> arcappcompat.Turbotax
    
    Tast Test for Turbotax app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Cq-Depend: chrome-internal:3411507
    Change-Id: Iaa308b271e32b5790ec728396f88ebd215af5e42

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/turbotax.go
@@ -0,0 +1,224 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForTurbotax = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForTurbotax},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForTurbotax = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForTurbotax},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Turbotax,
+		Desc:         "Functional test for Turbotax that installs the app also verifies it is logged in and that the main page is open, checks Turbotax correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForTurbotax,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForTurbotax,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForTurbotax,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForTurbotax,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars: []string{"arcappcompat.username", "arcappcompat.password",
+			"arcappcompat.Turbotax.emailid", "arcappcompat.Turbotax.password"},
+	})
+}
+
+// Turbotax test uses library for opting into the playstore and installing app.
+// Checks Turbotax correctly changes the window states in both clamshell and touchview mode.
+func Turbotax(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.intuit.turbotax.mobile"
+		appActivity = "com.intuit.turbotaxuniversal.startup.activity.StartUpActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForTurbotax verifies Turbotax is logged in and
+// verify Turbotax reached main activity page of the app.
+func launchAppForTurbotax(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		continueText             = "CONTINUE"
+		enterEmailAddressID      = "com.intuit.turbotax.mobile:id/emailOrUsernameEditText"
+		letUsConfirmText         = "Let's confirm it's you"
+		notNowID                 = "android:id/autofill_save_no"
+		iHaveAnAccountButtonText = "I have an account"
+		passwordClassName        = "android.widget.EditText"
+		passwordID               = "com.intuit.turbotax.mobile:id/passwordEditText"
+		passwordText             = "Password"
+		selectEmailClassName     = "androidx.appcompat.app.ActionBar$Tab"
+		selectEmailDes           = "Email or user ID"
+		skipID                   = "com.intuit.turbotax.mobile:id/skip_TV"
+		signInClassName          = "android.widget.Button"
+		signInText               = "SIGN IN"
+		homeClassName            = "android.widget.FrameLayout"
+	)
+
+	// Click on I have an account button.
+	clickOnIHavaAnAccountButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.Text(iHaveAnAccountButtonText))
+	if err := clickOnIHavaAnAccountButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Log("clickOnIHavaAnAccountButton doesn't exists: ", err)
+	} else if err := clickOnIHavaAnAccountButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnIHavaAnAccountButton: ", err)
+	}
+
+	// Select email.
+	selectEmail := d.Object(ui.ClassName(selectEmailClassName), ui.Description(selectEmailDes))
+	if err := selectEmail.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("selectEmail doesn't exists: ", err)
+	} else if err := selectEmail.Click(ctx); err != nil {
+		s.Fatal("Failed to click on selectEmail: ", err)
+	}
+
+	// Enter email id.
+	enterEmailAddress := d.Object(ui.ID(enterEmailAddressID))
+	if err := enterEmailAddress.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("EnterEmailAddress doesn't exists: ", err)
+	}
+
+	// Click on email address text field until the email address text field is focused.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if emailAddressFocused, err := enterEmailAddress.IsFocused(ctx); err != nil {
+			return errors.New("emailAddress text field not focused yet")
+		} else if !emailAddressFocused {
+			enterEmailAddress.Click(ctx)
+			return errors.New("emailAddress text field not focused yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Fatal("Failed to focus emailAddress: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	emailAddress := s.RequiredVar("arcappcompat.Turbotax.emailid")
+	if err := kb.Type(ctx, emailAddress); err != nil {
+		s.Fatal("Failed to enter emailAddress: ", err)
+	}
+	s.Log("Entered EmailAddress")
+
+	// Click on Sign in button.
+	signInButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+signInText))
+	if err := signInButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("SignInButton doesn't exists: ", err)
+	} else if err := signInButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on signInButton: ", err)
+	}
+
+	// Enter password.
+	enterPassword := d.Object(ui.ID(passwordID))
+	if err := enterPassword.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("EnterPassword doesn't exists: ", err)
+	} else if err := enterPassword.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterPassword: ", err)
+	}
+
+	// Click on password text field until the password text field is focused.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if pwdFocused, err := enterPassword.IsFocused(ctx); err != nil {
+			return errors.New("password text field not focused yet")
+		} else if !pwdFocused {
+			enterPassword.Click(ctx)
+			return errors.New("password text field not focused yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Fatal("Failed to focus password: ", err)
+	}
+
+	password := s.RequiredVar("arcappcompat.Turbotax.password")
+	if err := kb.Type(ctx, password); err != nil {
+		s.Fatal("Failed to enter password: ", err)
+	}
+	s.Log("Entered password")
+
+	// Click on continue button.
+	continueButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+continueText))
+	if err := continueButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("continueButton doesn't exists: ", err)
+	} else if err := continueButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on continueButton: ", err)
+	}
+
+	// Click on notNow button.
+	notNowButton := d.Object(ui.ID(notNowID))
+	if err := notNowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("notNowButton doesn't exist: ", err)
+	} else if err := notNowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on notNowButton: ", err)
+	}
+
+	// Click on skip button.
+	skipButton := d.Object(ui.ID(skipID))
+	if err := skipButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("skipButton doesn't exists: ", err)
+	} else if err := skipButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on skipButton: ", err)
+
+	}
+
+	// Check for let us confirm it is you message.
+	checkForletUsConfirmMessage := d.Object(ui.Text(letUsConfirmText))
+	if err := checkForletUsConfirmMessage.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("checkForletUsConfirmMessage doesn't exists: ", err)
+	} else {
+		s.Log("checkForletUsConfirmMessage does exists")
+		return
+	}
+
+	// Check for homeIcon on homePage.
+	homeIcon := d.Object(ui.ClassName(homeClassName), ui.PackageName(appPkgName))
+	if err := homeIcon.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("homeIcon doesn't exists: ", err)
+	}
+
+}
