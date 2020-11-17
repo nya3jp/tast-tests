318bac97b3d9dc0c1b9eb189bde9601c2dda37c4
Hanlin Chen <hanlinchen@google.com>
2020-11-17 19:29:55 +0800

    camera: Add HAL3 tests into group camera-libcamera
    Add HAL3 related tests into group camera-libcamera for the corresponding
    test suite.
    
    BUG=b:161688570
    TEST=tast run $HOST '("group:camera-libcamera")'
    
    Change-Id: I6eb2a6981e86422cebd7bdb4575d8d982f9d057a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2544288
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Greg Edelston <gredelston@google.com>
    Tested-by: Hanlin Chen <hanlinchen@google.com>
    Commit-Queue: Hanlin Chen <hanlinchen@google.com>

--- src/chromiumos/tast/local/bundles/cros/camera/hal3_still_capture.go
+++ src/chromiumos/tast/local/bundles/cros/camera/hal3_still_capture.go
@@ -19,7 +19,7 @@ func init() {
 		Func:         HAL3StillCapture,
 		Desc:         "Verifies camera still capture function with HAL3 interface",
 		Contacts:     []string{"hywu@chromium.org", "shik@chromium.org", "chromeos-camera-eng@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline", "informational", "group:camera-libcamera"},
 		SoftwareDeps: []string{"arc", "arc_camera3", "chrome", caps.BuiltinCamera},
 		Pre:          chrome.LoggedIn(),
 		// Krane needs 4 minutes and 30 seconds for whole dark environment(covering the camera lens).
