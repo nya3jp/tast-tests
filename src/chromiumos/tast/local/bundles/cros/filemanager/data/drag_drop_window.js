554ba003aaaed678819c3bc396621df467f7d9d8
nohe@chromium.org <nohe@chromium.org>
2020-11-16 17:15:09 -0800

    Added a test for copying from Files app. tast-tests: Add filemanager.CopyImage test
    This test creates a image file in the file manager, tests that copying
    between folders within the file manager works, and finally tests that a
    file object containing an image is pasted when invoked in a browser
    window. This test aims to ensure that pasting in a browser window as
    well as between files continues to work for images.
    
    BUG=chromium:827333
    TEST=tast run <DUT_IP> filemanager.CopyImage
    
    Change-Id: I1838b3aaab08f9738a69fb0f6bf471855c3aca08

--- src/chromiumos/tast/local/bundles/cros/filemanager/data/drag_drop_window.js
+++ src/chromiumos/tast/local/bundles/cros/filemanager/data/drag_drop_window.js
@@ -18,5 +18,26 @@ window.addEventListener('load', () => {
   }
 
   window.document.title = 'awaiting drop.';
+  document.addEventListener('paste', (e) => {
+    e.preventDefault();
+    if (e.clipboardData.types.includes('fs/sources')) {
+      const itemTitle = e.clipboardData.getData('fs/sources').split('/').pop();
+      window.document.title = 'paste registered:' + itemTitle;
+      window.myData = {};
+      window.myData["files"] = [];
+      window.myData["fileCount"] = e.clipboardData.files.length;
+      for (let i = 0; i < e.clipboardData.files.length; i++) {
+        let file = e.clipboardData.files.item(i);
+        window.myData["files"].push(file);
+      }
+      e.clipboardData.types.forEach((type) => {
+        window.myData[type] = e.clipboardData.getData(type);
+      })
+      dropArea.innerHTML = 'paste registered';
+    } else {
+      console.error("paste event doesn't contain fs/sources")
+    }
+  });
+
 }, { once: true });
 
