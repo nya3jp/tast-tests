cdf37a5a2444a09a4a41ea4f770912eabf51fac4
Saurabh Nijhara <snijhara@google.com>
2020-11-17 20:15:33 +0100

    tast-tests: add policy.ForceGoogleSafeSearch
    Add test for the ForceGoogleSafeSearch policy to replace the
    autotest.
    
    BUG=chromium:1125566
    TEST=tast run <ip> policy.ForceGoogleSafeSearch
    
    Change-Id: I2085b8c16186195941e028c0486438393a095e83

---
+++ src/chromiumos/tast/local/bundles/cros/policy/force_google_safe_search.go
@@ -0,0 +1,82 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ForceGoogleSafeSearch,
+		Desc: "Behavior of the ForceGoogleSafeSearch policy",
+		Contacts: []string{
+			"snijhara@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+	})
+}
+
+func ForceGoogleSafeSearch(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	for _, param := range []struct {
+		name     string
+		wantSafe bool
+		value    *policy.ForceGoogleSafeSearch
+	}{
+		{
+			name:     "enabled",
+			wantSafe: true,
+			value:    &policy.ForceGoogleSafeSearch{Val: true},
+		},
+		{
+			name:     "disabled",
+			wantSafe: false,
+			value:    &policy.ForceGoogleSafeSearch{Val: false},
+		},
+		{
+			name:     "unset",
+			wantSafe: false,
+			value:    &policy.ForceGoogleSafeSearch{Stat: policy.StatusUnset},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.value, &policy.DefaultSearchProviderEnabled{Stat: policy.StatusUnset}}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			conn, err := cr.NewConn(ctx, "https://www.google.com/search?q=kittens")
+			if err != nil {
+				s.Fatal("Failed to connect to chrome: ", err)
+			}
+			defer conn.Close()
+
+			var url string
+			if err := conn.Eval(ctx, `document.URL`, &url); err != nil {
+				s.Fatal("Could not read URL: ", err)
+			}
+
+			isSafe := strings.Contains(url, "&safe=active")
+			if param.wantSafe != isSafe {
+				s.Errorf("Unexpected safe search behavior: got %t; want %t", isSafe, param.wantSafe)
+			}
+		})
+	}
+}
