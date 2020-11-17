ed9f6cc45c329f036df1f22622a9b877aa0cd00d
Hardik Goyal <hardikgoyal@google.com>
2020-11-17 07:03:51 -0800

    tast:add AuthSession API integration test
    This CL introduces integration test for AuthSession API. The test first
    starts an AuthSession and then tries to authenticate that same
    authsession using auth_session_id.
    
    BUG=chromium:1161094
    TEST=tast run <dut> cryptohome.Auth*
    
    Cq-Depend: chromium:2750763
    Change-Id: I0ed1d63061a578c96c394781d7f8d0da985a4eb5

---
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/auth_session_mount.go
@@ -0,0 +1,74 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"context"
+
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AuthSessionMount,
+		Desc: "Ensures creates, authenticate and mount with an AuthSession",
+		Contacts: []string{
+			"hardikgoyal@chromium.org",
+			"chromeos-security@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+// AuthSessionMount ensures that an AuthSession can be used for mounting.
+// Here are the steps that this takes:
+// 1. Create AuthSession, which gives us back an authSessionID
+// 2. Use that authSessionID to create a new user
+// 3. Authenticate the newly created user
+// 4. Perform mount using AuthSession
+// 5. Unmount and remove the user
+func AuthSessionMount(ctx context.Context, s *testing.State) {
+	const (
+		testUser = "cryptohome_auth_session_test@chromium.org"
+		testPass = "testme"
+	)
+	// Start an Auth session and get an authSessionID.
+	authSessionID, err := cryptohome.StartAuthSession(ctx, testUser)
+	if err != nil {
+		s.Fatal("Failed to start Auth session: ", err)
+	}
+	testing.ContextLogf(ctx, "Auth session ID: %s", authSessionID)
+
+	if err = cryptohome.AddCredentialsWithAuthSession(ctx, testUser, testPass, authSessionID); err != nil {
+		s.Fatal("Failed to add credentials with AuthSession: ", err)
+	} else {
+		// Authenticate the same AuthSession using authSessionID.
+		// If we cannot authenticate, do not proceed with mount and unmount.
+		if err = cryptohome.AuthenticateAuthSession(ctx, testPass, authSessionID); err != nil {
+			s.Fatal("Failed to authenticate with AuthSession: ", err)
+		}
+		testing.ContextLog(ctx, "User authenticated successfully")
+
+		// Mounting with AuthSession now.
+		err = cryptohome.MountWithAuthSession(ctx, authSessionID)
+		if err != nil {
+			s.Fatal("Failed to mount user -: ", err)
+		}
+		testing.ContextLog(ctx, "User mounted successfully")
+
+		// Unmounting user vault.
+		err = cryptohome.UnmountVault(ctx, testUser)
+		if err != nil {
+			s.Fatal("Failed to unmount vault user -: ", err)
+		}
+	}
+
+	// Removing the user now despite if we could authenticate or not.
+	if err = cryptohome.RemoveVault(ctx, testUser); err != nil {
+		s.Fatal("Failed to remove user -: ", err)
+	}
+	testing.ContextLog(ctx, "User removed")
+}
