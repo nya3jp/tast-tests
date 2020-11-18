72ac9d6fb00c858039a90e00491a53bccbd4e9ad
Greg Edelston <gredelston@google.com>
2020-11-18 14:16:38 -0700

    firmware.FWTries: Branch test behavior for Vboot1
    Some boards have consistently failed the firmware.FWTries test: see
    tastboard/test/firmware.FWTries.
    This is because the test logic was fitted to the way Vboot2 works, but
    not Vboot1.
    This CL adds branching behavior to the test for Vboot1.
    
    BUG=b:162367972
    TEST=firmware.FWTries on a tidus (Vboot1)
    TEST=firmware.FWTries on a nasher (Vboot2)
    
    Change-Id: I67ac621605a321984a43177784bc5638eed9befe

--- src/chromiumos/tast/remote/bundles/cros/firmware/fw_tries.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/fw_tries.go
@@ -33,9 +33,16 @@ func FWTries(ctx context.Context, s *testing.State) {
 		s.Log("DUT does not use vboot2")
 	}
 
+	// Start the test at A/0 for consistency.
 	if err := firmware.CheckFWTries(ctx, d, firmware.A, firmware.UnspecifiedRWSection, 0); err != nil {
-		s.Error("Error with DUT state at start of test: ", err)
+		s.Log("Unexpected DUT state at start of test: ", err)
+		if err := firmware.SetFWTries(ctx, d, firmware.A, 0); err != nil {
+			s.Fatal("Setting FWTries to A/0: ", err)
+		}
+		s.Error("After setting FWTries to A/0 at start of test: ", err)
 	}
+
+	// Set next=B, tries=2.
 	if err := firmware.SetFWTries(ctx, d, firmware.B, 2); err != nil {
 		s.Fatal("Setting FWTries to B/2: ", err)
 	}
@@ -43,24 +50,44 @@ func FWTries(ctx context.Context, s *testing.State) {
 		s.Error("After setting FWTries to B/2, before rebooting: ", err)
 	}
 
+	// Reboot the DUT.
 	if err := d.Reboot(ctx); err != nil {
 		s.Fatal("Rebooting: ", err)
-	}
-	if err := firmware.CheckFWTries(ctx, d, firmware.B, firmware.B, 1); err != nil {
-		s.Error("After setting FWTries to B/2, then rebooting once: ", err)
 	}
 
-	if err := d.Reboot(ctx); err != nil {
-		s.Fatal("Rebooting: ", err)
-	}
-	if err := firmware.CheckFWTries(ctx, d, firmware.B, firmware.UnspecifiedRWSection, 0); err != nil {
-		s.Error("After setting FWTries to B/2, then rebooting twice: ", err)
-	}
-
-	if err := d.Reboot(ctx); err != nil {
-		s.Fatal("Rebooting: ", err)
-	}
-	if err := firmware.CheckFWTries(ctx, d, firmware.A, firmware.A, 0); err != nil {
-		s.Error("After setting FWTries to B/2, then rebooting three times: ", err)
+	// Post-reboot behavior changes based on the Vboot version.
+	if vboot2 {
+		// In Vboot2, booting into Firmware B should decrement fw_try_count.
+		// fw_try_next should not change unless fw_try_count reaches 0.
+		// So the DUT should now be in Firmware B with fw_try_count=1.
+		if err := firmware.CheckFWTries(ctx, d, firmware.B, firmware.B, 1); err != nil {
+			s.Fatal("After rebooting from Firmware A with fw_try_next=B, fw_try_count=2: ", err)
+		}
+		// Next reboot should use Firmware B, and decrement fw_try_count to 0.
+		if err := d.Reboot(ctx); err != nil {
+			s.Fatal("Rebooting: ", err)
+		}
+		if err := firmware.CheckFWTries(ctx, d, firmware.B, firmware.UnspecifiedRWSection, 0); err != nil {
+			s.Fatal("After rebooting from Firmware B with fw_try_next=B, fw_try_count=1: ", err)
+		}
+		// Next reboot should return to Firmware A.
+		if err := d.Reboot(ctx); err != nil {
+			s.Fatal("Rebooting: ", err)
+		}
+		if err := firmware.CheckFWTries(ctx, d, firmware.A, firmware.A, 0); err != nil {
+			s.Fatal("After rebooting from Firmware B with fw_try_next=A, fw_try_count=0: ", err)
+		}
+	} else {
+		// In Vboot1, booting into Firmware B should set fwb_tries=0.
+		if err := firmware.CheckFWTries(ctx, d, firmware.B, firmware.A, 0); err != nil {
+			s.Fatal("After rebooting from Firmware A with fwb_tries=2: ", err)
+		}
+		// Next reboot should return to Firmware A.
+		if err := d.Reboot(ctx); err != nil {
+			s.Fatal("Rebooting: ", err)
+		}
+		if err := firmware.CheckFWTries(ctx, d, firmware.A, firmware.A, 0); err != nil {
+			s.Fatal("After rebooting from firmware.B with fwb_tries=0: ", err)
+		}
 	}
 }
