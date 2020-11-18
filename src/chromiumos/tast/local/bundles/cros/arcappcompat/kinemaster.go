aa4becdecdae6b88f7575a475c8bfc318e6af32a
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-11-18 15:20:34 -0800

    Run UIAutomator Test Using Tast For kinemaster app
    BUG=b:172086812
    TEST=tast run <eve> arcappcompat.Kinemaster
    
    Tast Test for Kinemaster app verifies launch app
    from the playstore, check app reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: I93a51e2c2b08475de47c8f22a94da043290c330f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2548204
    Tested-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Commit-Queue: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/kinemaster.go
@@ -0,0 +1,171 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForKinemaster = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForKinemaster},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForKinemaster = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForKinemaster},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Kinemaster,
+		Desc:         "Functional test for Kinemaster that installs the app also verifies it is logged in and that the main page is open, checks Kinemaster correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForKinemaster,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForKinemaster,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForKinemaster,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForKinemaster,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// Kinemaster test uses library for opting into the playstore and installing app.
+// Checks Kinemaster correctly changes the window states in both clamshell and touchview mode.
+func Kinemaster(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.nexstreaming.app.kinemasterfree"
+		appActivity = "com.nextreaming.nexeditorui.MainActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForKinemaster verifies Kinemaster is logged in and
+// verify Kinemaster reached main activity page of the app.
+func launchAppForKinemaster(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		addButtonID             = "com.nexstreaming.app.kinemasterfree:id/addProject"
+		allowButtonText         = "ALLOW"
+		cancelText              = "Cancel"
+		continueToAppID         = "close-button"
+		closeID                 = "com.nexstreaming.app.kinemasterfree:id/tv_subscribe_skip"
+		remindMelaterButtonText = "Remind Me Later"
+		startText               = "Start"
+		selectLayoutID          = "com.nexstreaming.app.kinemasterfree:id/ratio16v9"
+		homeID                  = "com.nexstreaming.app.kinemasterfree:id/mediaListView"
+	)
+
+	// Click on allow button to access your photos, media and files.
+	allowButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+allowButtonText))
+	if err := allowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("Allow Button doesn't exist: ", err)
+	} else if err := allowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on allowButton: ", err)
+	}
+
+	// Press KEYCODE_DPAD_RIGHT to swipe until Start button exist.
+	startButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+startText))
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := startButton.Exists(ctx); err != nil {
+			s.Log(" Press KEYCODE_DPAD_RIGHT to swipe until Start button exist")
+			d.PressKeyCode(ctx, ui.KEYCODE_DPAD_RIGHT, 0)
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Log("startButton doesn't exist: ", err)
+	} else if err := startButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on startButton: ", err)
+	}
+
+	// Click on close button to skip subscription.
+	clickOnCloseButton := d.Object(ui.ID(closeID))
+	if err := clickOnCloseButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("clickOnCloseButton doesn't exists: ", err)
+		d.PressKeyCode(ctx, ui.KEYCODE_BACK, 0)
+	} else if err := clickOnCloseButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnCloseButton: ", err)
+	}
+
+	// Click on continue to app button.
+	continueToAppButton := d.Object(ui.ID(continueToAppID))
+	if err := continueToAppButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("continueToAppButton doesn't exists: ", err)
+	} else if err := continueToAppButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on continueToAppButton: ", err)
+	}
+
+	// Click on remind me later button.
+	clickOnRemindMeLaterButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+remindMelaterButtonText))
+	if err := clickOnRemindMeLaterButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("clickOnRemindMeLaterButton doesn't exists: ", err)
+	} else if err := clickOnRemindMeLaterButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnRemindMeLaterButton: ", err)
+	}
+
+	// Click on cancel button.
+	cancelButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+cancelText))
+	if err := cancelButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("cancelButton doesn't exists: ", err)
+	} else if err := cancelButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on cancelButton: ", err)
+	}
+
+	// Click on add button.
+	clickOnAddButton := d.Object(ui.ID(addButtonID))
+	if err := clickOnAddButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("clickOnAddButton doesn't exists: ", err)
+	} else if err := clickOnAddButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnAddButton: ", err)
+	}
+
+	// Click on select Layout.
+	clickOnSlectLayout := d.Object(ui.ID(selectLayoutID))
+	if err := clickOnSlectLayout.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("clickOnSlectLayout doesn't exists: ", err)
+	} else if err := clickOnSlectLayout.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnSlectLayout: ", err)
+	}
+
+	// Check for homeIcon on homePage.
+	homeIcon := d.Object(ui.ID(homeID))
+	if err := homeIcon.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("homeIcon doesn't exists: ", err)
+	}
+}
