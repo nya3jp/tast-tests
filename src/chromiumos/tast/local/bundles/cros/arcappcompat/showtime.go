de7ddc31585e317014fccc0c7d2c26e4cb05a86e
Archana Singh <archanasing@google.com>
2020-11-17 21:19:29 -0800

    Run UIAutomator Test Using Tast For Showtime app
    BUG=b:171982400
    TEST=tast run <eve> arcappcompat.Showtime
    
    Tast Test for Showtime app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Cq-Depend: chrome-internal:3411516
    Change-Id: I34942f46d074441c47ab2ee956492b250aca2251

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/showtime.go
@@ -0,0 +1,144 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForShowtime = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForShowtime},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForShowtime = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForShowtime},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Showtime,
+		Desc:         "Functional test for Showtime that installs the app also verifies it is logged in and that the main page is open, checks Showtime correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForShowtime,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForShowtime,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForShowtime,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForShowtime,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars: []string{"arcappcompat.username", "arcappcompat.password",
+			"arcappcompat.Showtime.emailid", "arcappcompat.Showtime.password"},
+	})
+}
+
+// Showtime test uses library for opting into the playstore and installing app.
+// Checks Showtime correctly changes the window states in both clamshell and touchview mode.
+func Showtime(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.showtime.standalone"
+		appActivity = "com.showtime.showtimeanytime.activities.IntroActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForShowtime verifies Showtime is logged in and
+// verify Showtime reached main activity page of the app.
+func launchAppForShowtime(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		signInID        = "com.showtime.standalone:id/loginButton"
+		enterEmailID    = "com.showtime.standalone:id/emailLogin"
+		enterPasswordID = "com.showtime.standalone:id/passwordLogin"
+		logInText       = "SIGN IN"
+		logInID         = "com.showtime.standalone:id/signInButton"
+		notNowButtonID  = "android:id/autofill_save_no"
+		homeIconID      = "com.showtime.standalone:id/text"
+		homeIconText    = "HOME"
+	)
+
+	// Click on sign in button.
+	signInButton := d.Object(ui.ID(signInID))
+	if err := signInButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("Sign in button doesn't exist: ", err)
+	} else if err := signInButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on Sign in button: ", err)
+	}
+
+	// Enter email address.
+	ShowtimeEmailID := s.RequiredVar("arcappcompat.Showtime.emailid")
+	enterEmailAddress := d.Object(ui.ID(enterEmailID))
+	if err := enterEmailAddress.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("EnterEmailAddress doesn't exist: ", err)
+	} else if err := enterEmailAddress.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterEmailAddress: ", err)
+	} else if err := enterEmailAddress.SetText(ctx, ShowtimeEmailID); err != nil {
+		s.Fatal("Failed to enterEmailAddress: ", err)
+	}
+
+	// Enter password.
+	ShowtimePassword := s.RequiredVar("arcappcompat.Showtime.password")
+	enterPassword := d.Object(ui.ID(enterPasswordID))
+	if err := enterPassword.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("EnterPassword doesn't exist: ", err)
+	} else if err := enterPassword.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterPassword: ", err)
+	} else if err := enterPassword.SetText(ctx, ShowtimePassword); err != nil {
+		s.Fatal("Failed to enterPassword: ", err)
+	}
+
+	// Click on log in button
+	logInButton := d.Object(ui.ID(logInID), ui.Text(logInText))
+	if err := logInButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("LogIn button doesn't exist: ", err)
+	} else if err := logInButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on LogIn button: ", err)
+	}
+
+	notNowButton := d.Object(ui.ID(notNowButtonID))
+	if err := notNowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("notNowButton button doesn't exist: ", err)
+	} else if err := notNowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on notNowButton button: ", err)
+	}
+
+	// Check for home icon.
+	homeIcon := d.Object(ui.ID(homeIconID), ui.Text(homeIconText))
+	if err := homeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("homeIcon doesn't exist: ", err)
+	}
+}
