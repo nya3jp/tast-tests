5931c0aee9a948001b4f061114d2503e333ef716
Angavai Shanmugam <angavai@google.com>
2020-11-19 11:53:33 -0800

    Quick Settings from SignIn Screen.
    The test verifies Quick Settings and its contents from the signin screen
    TestTracker: https://testtracker.googleplex.com/testplans/testcase/detail/3191705?id=21607&revision=88
    
    BUG=none
    TEST=tast run <ip> ui.QuickSettingsSignIn
    
    Change-Id: I446b60103ccf6ffdf6975109dbdbb409989a43ef

---
+++ src/chromiumos/tast/local/bundles/cros/ui/quick_settings_sign_in.go
@@ -0,0 +1,125 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/ui/quicksettings"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: QuickSettingsSignIn,
+		Desc: "Checks the Quick Settings from SignIn screen",
+		Contacts: []string{
+			"kyleshima@chromium.org",
+			"bhansknecht@chromium.org",
+			"dhaddock@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			ExtraHardwareDeps: hwdep.D(hwdep.Battery()),
+			Val:               true,
+		}, {
+			Name: "no_battery",
+			Val:  false,
+		}},
+		Vars: []string{
+			"ui.signinProfileTestExtensionManifestKey",
+		},
+	})
+}
+
+// QuickSettingsSignIn verifies Quick Settings and its contents from the signin screen.
+func QuickSettingsSignIn(ctx context.Context, s *testing.State) {
+
+	// NoLogin is used to land in signin screen.
+	cr, err := chrome.New(
+		ctx,
+		chrome.NoLogin(),
+		chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey")),
+	)
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.SigninProfileTestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating login test API connection failed: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+	defer tconn.Close()
+
+	// Skipping the OOBE and navigating directly to the Login screen.
+	oobeConn, err := cr.WaitForOOBEConnection(ctx)
+	if err != nil {
+		s.Fatal("Failed to create OOBE connection: ", err)
+	}
+
+	if err := oobeConn.Eval(ctx, "Oobe.skipToLoginForTesting()", nil); err != nil {
+		s.Fatal("Failed to skip to login: ", err)
+	}
+
+	// Explicitly show Quick Settings ShowWithRetry on the signin screen, so it will
+	// remain open for the UI verification steps.
+	if err := quicksettings.ShowWithRetry(ctx, tconn, 10*time.Second); err != nil {
+		s.Fatal("Failed to show quick settings : ", err)
+	}
+	defer quicksettings.Hide(ctx, tconn)
+
+	// Check the network UI elements are shown in Quick Settings.
+	networkParams, err := quicksettings.PodIconParams(quicksettings.SettingPodNetwork)
+	if err != nil {
+		s.Fatal("Failed to get params for network pod icon: ", err)
+	}
+
+	// Check the bluetooth UI elements are shown in Quick Settings.
+	bluetoothParams, err := quicksettings.PodIconParams(quicksettings.SettingPodBluetooth)
+	if err != nil {
+		s.Fatal("Failed to get params for bluetoothParams pod icon: ", err)
+	}
+
+	// Associate the params with a descriptive name for better error reporting.
+	checkNodes := map[string]ui.FindParams{
+		"NetWork pod":       networkParams,
+		"Bluetooth pod":     bluetoothParams,
+		"Brightness slider": quicksettings.BrightnessSliderParams,
+		"Volume slider":     quicksettings.VolumeSliderParams,
+		"Shutdown button":   quicksettings.ShutdownBtnParams,
+		"Collapse button":   quicksettings.CollapseBtnParams,
+		"Date/time display": quicksettings.DateViewParams,
+	}
+
+	// Only check the battery display if the DUT has a battery.
+	if s.Param().(bool) {
+		checkNodes["Battery display"] = quicksettings.BatteryViewParams
+	}
+
+	// Loop through all the Quick Settings nodes and verify if they exist.
+	for node, params := range checkNodes {
+		if shown, err := ui.Exists(ctx, tconn, params); err != nil {
+			s.Fatalf("Failed to check existence of %v: %v", node, err)
+		} else if !shown {
+			s.Errorf("%v was not found in the UI", node)
+		}
+	}
+
+	// Get the Microphone gain Slider params from Audio Settings subpage
+	initial, err := quicksettings.SliderValue(ctx, tconn, quicksettings.SliderTypeMicGain)
+	if err != nil {
+		s.Fatal("Failed to get the SliderValue in Audio Settings Page: ", err)
+	}
+	s.Log("Mic gain slider exist with the slider value : ", initial)
+
+}
