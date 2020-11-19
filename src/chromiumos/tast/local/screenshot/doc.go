5d13aaa0c4b370dc30ed488ad01248c495b9d568
Oleh Lamzin <lamzin@google.com>
2020-11-19 15:31:19 +0100

    Move screenshot helpers to screenshot package
    Move screenshot file utilities to screenshot package.
    
    Rename screenshot.go to capture.go for better screenshot package
    readability and navigation.
    
    BUG=chromium:1125556
    TEST=tast run localhost:7777 policy.DisableScreenshots
    
    Change-Id: I0b2a3ce8855f2fad6499dfde06503b7612acd7c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2550026
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Tested-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>

---
+++ src/chromiumos/tast/local/screenshot/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package screenshot supports taking and examining screenshots; contains helper
+// utils to work with screenshot files.
+package screenshot
