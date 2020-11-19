14d8b29b00c36a51425e002434edd9719eaa2280
Miriam Polzer <mpolzer@google.com>
2020-11-19 11:11:06 +0100

    tast-tests: Add policy.AlternateErrorPagesEnabled
    Port from autotest to tast.
    
    BUG=chromium:1125533
    TEST=tast run <device> policy.AlternateErrorPagesEnabled
    
    Change-Id: I78d698d3d227a82efe16e572977665e9258e2243

---
+++ src/chromiumos/tast/local/bundles/cros/policy/alternate_error_pages_enabled.go
@@ -0,0 +1,80 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AlternateErrorPagesEnabled,
+		Desc: "Behavior of the AlternateErrorPagesEnabled policy: check that an alternate set of error pages is shown based on the policy",
+		Contacts: []string{
+			"mpolzer@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+	})
+}
+
+func AlternateErrorPagesEnabled(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	for _, tc := range []struct {
+		name       string
+		value      *policy.AlternateErrorPagesEnabled
+		suggestion string // The first suggestion item on the error page shown when navigating to a nonexistent page.
+	}{
+		{
+			name:       "true",
+			value:      &policy.AlternateErrorPagesEnabled{Val: true},
+			suggestion: "If spelling is correct, try running Connectivity Diagnostics.",
+		},
+		{
+			name:       "false",
+			value:      &policy.AlternateErrorPagesEnabled{Val: false},
+			suggestion: "Checking the connection",
+		},
+		{
+			name:       "unset",
+			value:      &policy.AlternateErrorPagesEnabled{Stat: policy.StatusUnset},
+			suggestion: "If spelling is correct, try running Connectivity Diagnostics.",
+		},
+	} {
+		s.Run(ctx, tc.name, func(ctx context.Context, s *testing.State) {
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{tc.value}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			conn, err := cr.NewConn(ctx, "https://bogus")
+			if err != nil {
+				s.Fatal("Failed to connect to chrome: ", err)
+			}
+			defer conn.Close()
+
+			var line string
+			if err := conn.Eval(ctx, "document.querySelector('#suggestions-list li').innerText", &line); err != nil {
+				s.Fatal("Could not read error page suggestion: ", err)
+			}
+
+			if line != tc.suggestion {
+				s.Fatalf("Unexpected suggestion on the error page: got %q, want %q", line, tc.suggestion)
+			}
+		})
+	}
+}
