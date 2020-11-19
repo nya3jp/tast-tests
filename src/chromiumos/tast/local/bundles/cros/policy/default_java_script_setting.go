cf21e1b9ae13688fde4bea4cbbf836429fd6c370
Miriam Polzer <mpolzer@google.com>
2020-11-19 08:54:17 +0100

    tast-tests: Add policy.DefaultJavaScriptSetting
    Port autotest to tast.
    
    BUG=chromium:1125543
    TEST=tast run <device> policy.DefaultJavaScriptSetting
    
    Change-Id: Ia228617e086984d30387f1cf20fc9393a58cf09c

---
+++ src/chromiumos/tast/local/bundles/cros/policy/default_java_script_setting.go
@@ -0,0 +1,82 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DefaultJavaScriptSetting,
+		Desc: "Check that the DefaultJavaScript policy blocks or allows JavaScript",
+		Contacts: []string{
+			"mpolzer@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+		Data:         []string{"js_test.html"},
+	})
+}
+
+func DefaultJavaScriptSetting(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	for _, tc := range []struct {
+		name  string
+		value *policy.DefaultJavaScriptSetting
+		allow bool
+	}{
+		{
+			name:  "allow",
+			value: &policy.DefaultJavaScriptSetting{Val: 1},
+			allow: true,
+		},
+		{
+			name:  "block",
+			value: &policy.DefaultJavaScriptSetting{Val: 2},
+			allow: false,
+		},
+	} {
+		s.Run(ctx, tc.name, func(ctx context.Context, s *testing.State) {
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{tc.value}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			conn, err := cr.NewConn(ctx, server.URL+"/js_test.html")
+			if err != nil {
+				s.Fatal("Failed to connect to chrome: ", err)
+			}
+			defer conn.Close()
+
+			allowed := false
+			// Evaluating is going to fail if JavaScript is not allowed.
+			if err := conn.Eval(ctx, "jsAllowed", &allowed); err != nil && tc.allow {
+				s.Fatal("Failed to read jsAllowed although JavaScript should be allowed: ", err)
+			} else {
+				if allowed != tc.allow {
+					s.Fatalf("Unexpected jsAllowed value; got %v, want %v", allowed, tc.allow)
+				}
+			}
+		})
+	}
+}
