76d148e80ea0e6240e555ebed7b7fef21f8ea0f5
Charles William Dick <cwd@google.com>
2020-11-19 16:21:38 +0900

    multivm.LifecycleShifting test
    Introduces test that starts streaks of tabs and apps, and measure how
    many are alive at the end.
    
    BUG=b:154785124
    TEST=tast run dut multivm.LifecycleShifting.arc_host
    
    Change-Id: Id4b41d60f6c55a84d39974112e6f53afa96bfd76

--- src/chromiumos/tast/local/memory/memoryuser/still_alive_metric_task.go
+++ src/chromiumos/tast/local/memory/memoryuser/still_alive_metric_task.go
@@ -6,6 +6,7 @@ package memoryuser
 
 import (
 	"context"
+	"math"
 
 	"chromiumos/tast/common/perf"
 	"chromiumos/tast/testing"
@@ -19,8 +20,9 @@ type KillableTask interface {
 // StillAliveMetricTask is a MemoryTask that counts how many of a set of
 // KillableTask are still alive, and records it as a perf.Metric.
 type StillAliveMetricTask struct {
-	tasks []KillableTask
-	name  string
+	tasks      []KillableTask
+	name       string
+	stillAlive int
 }
 
 // StillAliveMetricTask is a MemoryTask.
@@ -28,18 +30,18 @@ var _ MemoryTask = (*StillAliveMetricTask)(nil)
 
 // Run actually computes the metric.
 func (t *StillAliveMetricTask) Run(ctx context.Context, testEnv *TestEnv) error {
-	var stillAlive float64
+	t.stillAlive = 0
 	for _, sa := range t.tasks {
 		if sa.StillAlive(ctx, testEnv) {
-			stillAlive += 1.0
+			t.stillAlive++
 		}
 	}
-	testing.ContextLogf(ctx, "Still alive metric %q: %f", t.name, stillAlive)
+	testing.ContextLogf(ctx, "Still alive metric %q: %d", t.name, t.stillAlive)
 	testEnv.p.Set(perf.Metric{
 		Name:      t.name,
 		Unit:      "count",
 		Direction: perf.BiggerIsBetter,
-	}, stillAlive)
+	}, float64(t.stillAlive))
 	return nil
 }
 
@@ -57,8 +59,62 @@ func (t *StillAliveMetricTask) NeedVM() bool {
 	return false
 }
 
+// StillAlive returns how many tasks were rerported to be still alive. Returns
+// -1 if this task hasn't run yet.
+func (t *StillAliveMetricTask) StillAlive() int {
+	return t.stillAlive
+}
+
 // NewStillAliveMetricTask creates a new MemoryTask that records a perf.Metric
 // for how many of a set of tasks are still alive.
 func NewStillAliveMetricTask(tasks []KillableTask, name string) *StillAliveMetricTask {
-	return &StillAliveMetricTask{tasks, name}
+	return &StillAliveMetricTask{tasks, name, -1}
+}
+
+// MinStillAliveMetricTask is a MemoryTask that reports the minimum value
+// reported by a list of StilAliveMetricTasks.
+type MinStillAliveMetricTask struct {
+	tasks []*StillAliveMetricTask
+	name  string
+}
+
+// MinStillAliveMetricTask is a MemoryTask.
+var _ MemoryTask = (*MinStillAliveMetricTask)(nil)
+
+// Run actually computes the metric.
+func (t *MinStillAliveMetricTask) Run(ctx context.Context, testEnv *TestEnv) error {
+	min := math.MaxInt64
+	for _, sa := range t.tasks {
+		stillAlive := sa.StillAlive()
+		if stillAlive < min {
+			min = stillAlive
+		}
+	}
+	testing.ContextLogf(ctx, "Min still alive metric %q: %d", t.name, min)
+	testEnv.p.Set(perf.Metric{
+		Name:      t.name,
+		Unit:      "count",
+		Direction: perf.BiggerIsBetter,
+	}, float64(min))
+	return nil
+}
+
+// Close does nothing.
+func (t *MinStillAliveMetricTask) Close(_ context.Context, _ *TestEnv) {
+}
+
+// String gives MemoryUser a friendly string for logging.
+func (t *MinStillAliveMetricTask) String() string {
+	return "Min Still Alive Metric " + t.name
+}
+
+// NeedVM is false because we do not need a new Crostini VM spun up.
+func (t *MinStillAliveMetricTask) NeedVM() bool {
+	return false
+}
+
+// NewMinStillAliveMetricTask creates a new MemoryTask that records a
+// perf.Metric for the minimum value of a set of StillAliveMetricsTasks.
+func NewMinStillAliveMetricTask(tasks []*StillAliveMetricTask, name string) *MinStillAliveMetricTask {
+	return &MinStillAliveMetricTask{tasks, name}
 }
