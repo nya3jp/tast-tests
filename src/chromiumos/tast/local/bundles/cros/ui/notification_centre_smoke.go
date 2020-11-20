09ca3e8138d7634b68838508b9f2aa647704639c
David Haddock <dhaddock@google.com>
2020-11-19 18:40:36 -0800

    Allow test to deal with notification being focused.
    The test passes 100% on some boards and fails 100% on others. Failures
    are due to the notification getting focused. When this happens the
    notification does not autodismiss.
    
    It never happens when run as the only test. But when certain tests run
    before this one, the notification gets focused. I cannot tab away
    from the notification because the tabs end up just cycling within the
    notification :) So instead I am just going to open the launcher. The
    notification will then autodismiss and the test continues.
    
    BUG=chromium:1048435
    TEST=ui.DesktopControl ui.LauncherSearchBuiltInApps ui.NotificationCentreSmoke
    
    Change-Id: I511c5140f83e80c062594e82d65180883ee42e94

--- src/chromiumos/tast/local/bundles/cros/ui/notification_centre_smoke.go
+++ src/chromiumos/tast/local/bundles/cros/ui/notification_centre_smoke.go
@@ -13,9 +13,8 @@ import (
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/local/chrome/ui/faillog"
-	"chromiumos/tast/local/chrome/ui/mouse"
+	"chromiumos/tast/local/chrome/ui/launcher"
 	"chromiumos/tast/local/chrome/ui/quicksettings"
-	"chromiumos/tast/local/coords"
 	"chromiumos/tast/testing"
 )
 
@@ -44,11 +43,6 @@ func NotificationCentreSmoke(ctx context.Context, s *testing.State) {
 	}
 	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
 
-	// Move mouse cursor to top left corner so it doesn't interfere with notification dismissal.
-	if err := mouse.Click(ctx, tconn, coords.NewPoint(0, 0), mouse.LeftButton); err != nil {
-		s.Fatal("Failed to move mouse to top left hand corner: ", err)
-	}
-
 	s.Log("Creating notification")
 	if _, err := ash.CreateTestNotification(ctx, tconn, ash.NotificationTypeBasic, "TestNotification1", "blahhh"); err != nil {
 		s.Fatal("Failed to create test notification: ", err)
@@ -62,8 +56,33 @@ func NotificationCentreSmoke(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to find notification popup: ", err)
 	}
 	s.Log("Waiting for notification to auto-dismiss")
-	if err := ui.WaitUntilGone(ctx, tconn, params, uiTimeout); err != nil {
-		s.Fatal("Failed waiting for notification to auto-dismiss: ", err)
+	// Depending on DUT state when test is run, the notification could be focused and will not autodismiss.
+	// Check for that condition while waiting for notification to dismiss.
+	subparams := ui.FindParams{
+		State: map[ui.StateType]bool{ui.StateTypeFocused: true},
+	}
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		notificationPopup, err := ui.Find(ctx, tconn, params)
+		if err != nil {
+			// Notification dismissed.
+			return nil
+		}
+		defer notificationPopup.Release(ctx)
+		// Notification still exists. Check if it has focus.
+		exists, err := notificationPopup.DescendantExists(ctx, subparams)
+		if err != nil {
+			return errors.Wrap(err, "failed to find notification with focus")
+		}
+		if !exists {
+			return errors.New("focused notification does not exist")
+		}
+		s.Log("Notification had focus. Opening launcher to change focus")
+		if err := launcher.OpenLauncher(ctx, tconn); err != nil {
+			s.Fatal("Failed to open launcher: ", err)
+		}
+		return errors.New("notification exists and was focused")
+	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+		s.Fatal("Failed waiting for notification to dismiss: ", err)
 	}
 	s.Log("Open quick settings")
 	if err := quicksettings.Show(ctx, tconn); err != nil {
