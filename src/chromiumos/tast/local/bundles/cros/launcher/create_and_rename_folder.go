1cace99d6e3f4a66a08c5fa7edb0844b00701807
Seewai Fu <seewaifu@google.com>
2020-11-20 14:38:36 -0800

    tast: ui.LauncherCreateAndRenameFolder and ui.LauncherPinAppToShelf failing a lot
    Touchscreen behavior on clamshell machine while running in tablet
    can be different from tablet and convertible. Reduce the use of
    touchscreen to avoid the issue.
    
    Manual testing has been done on on fleex (clamshell), kodama (tablet)
    drallion (clamshell), dru(tablet), hana (convertible), octopus (convertible),
    jax (chromebox), flaffy (chromebox), kaisa (chromebox) and sparky360 (convertible).
    
    Manual Test Log (kaisa)
    2020/11/25 09:55:41 Command line: tast -verbose run -build=true chromeos6-row16-rack11-host19.cros.corp.google.com launcher.CreateAndRenameFolder.* launcher.PinAppToShelf.*
    ...
    2020/11/25 09:57:21 --------------------------------------------------------------------------------
    2020/11/25 09:57:21 launcher.CreateAndRenameFolder.tablet_mode     [ SKIP ] DUT does not have an internal display
    2020/11/25 09:57:21 launcher.PinAppToShelf.tablet_mode             [ SKIP ] DUT does not have an internal display
    2020/11/25 09:57:21 launcher.CreateAndRenameFolder.clamshell_mode  [ PASS ]
    2020/11/25 09:57:21 launcher.PinAppToShelf.clamshell_mode          [ PASS ]
    2020/11/25 09:57:21 --------------------------------------------------------------------------------
    2020/11/25 09:57:21 Results saved to /tmp/tast/results/20201125-095541
    
    Manual Test Log (drallion)
    2020/11/25 10:07:42 Command line: tast -verbose run -build=true chromeos6-row6-rack17-host19.cros.corp.google.com launcher.CreateAndRenameFolder.* launcher.PinAppToShelf.*
    ...
    2020/11/25 10:10:01 --------------------------------------------------------------------------------
    2020/11/25 10:10:01 launcher.CreateAndRenameFolder.clamshell_mode  [ PASS ]
    2020/11/25 10:10:01 launcher.CreateAndRenameFolder.tablet_mode     [ PASS ]
    2020/11/25 10:10:01 launcher.PinAppToShelf.clamshell_mode          [ PASS ]
    2020/11/25 10:10:01 launcher.PinAppToShelf.tablet_mode             [ PASS ]
    2020/11/25 10:10:01 --------------------------------------------------------------------------------
    2020/11/25 10:10:01 Results saved to /tmp/tast/results/20201125-100742
    
    BUG=chromium:1145781
    TEST=Manual testing. Unit tests passed
    
    Change-Id: Ib01412d73150714921f92201f35171d0ed39a7d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2552991
    Tested-by: Seewai Fu <seewaifu@google.com>
    Commit-Queue: Seewai Fu <seewaifu@google.com>
    Reviewed-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/launcher/create_and_rename_folder.go
+++ src/chromiumos/tast/local/bundles/cros/launcher/create_and_rename_folder.go
@@ -17,6 +17,7 @@ import (
 	"chromiumos/tast/local/chrome/ui/pointer"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -39,6 +40,7 @@ func init() {
 				Name:              "tablet_mode",
 				Val:               true,
 				ExtraSoftwareDeps: []string{"tablet_mode"},
+				ExtraHardwareDeps: hwdep.D(hwdep.InternalDisplay()),
 			},
 		},
 	})
@@ -103,7 +105,7 @@ func CreateAndRenameFolder(ctx context.Context, s *testing.State) {
 	}
 
 	// Drag one icon to the top of another icon to create a folder.
-	if err := dragIcon(ctx, tconn, icons[0], icons[1], tabletMode); err != nil {
+	if err := dragIcon(ctx, tconn, icons[0], icons[1]); err != nil {
 		s.Fatalf("Failed to Drag app %q: %v", icons[0].Name, err)
 	}
 
@@ -120,7 +122,7 @@ func CreateAndRenameFolder(ctx context.Context, s *testing.State) {
 }
 
 // dragIcon is a helper function to cause a drag of the left button from start to end for tablet.
-func dragIcon(ctx context.Context, tconn *chrome.TestConn, srcIcon, targetIcon *ui.Node, tabletMode bool) error {
+func dragIcon(ctx context.Context, tconn *chrome.TestConn, srcIcon, targetIcon *ui.Node) error {
 	start := srcIcon.Location.CenterPoint()
 	end := targetIcon.Location.CenterPoint()
 
@@ -132,16 +134,8 @@ func dragIcon(ctx context.Context, tconn *chrome.TestConn, srcIcon, targetIcon *
 
 	var ctlr pointer.Controller
 	var err error
-	if tabletMode {
-		// Setting up touch control
-		ctlr, err = pointer.NewTouchController(ctx, tconn)
-		if err != nil {
-			return errors.Wrap(err, "failed to create the touch controller")
-		}
-	} else {
-		// Setting up mouse control
-		ctlr = pointer.NewMouseController(tconn)
-	}
+	ctlr = pointer.NewMouseController(tconn)
+
 	// Simulate a long press so that the icon is ready to be moved.
 	// It is based on the implementation on ui.LongPress, but this
 	// implementation works for both touch screen and mouse press.
