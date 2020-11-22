3b0a9144030de89bba6abc637262a08c73a8b2f8
Tomasz Dobrowolski <tomdobro@chromium.org>
2020-11-22 17:23:28 +0100

    tast: Add Chromad enrollment and login flow test
    BUG=chromium:1149687
    TEST=tast run <DUT> chromad.ChromadFlow
    
    Change-Id: I4c025e6b56d551aa5e49d2275162e401c20796e6

--- src/chromiumos/tast/local/bundles/cros/main.go
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -20,6 +20,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/baserpc"
 	_ "chromiumos/tast/local/bundles/cros/biod"
 	_ "chromiumos/tast/local/bundles/cros/camera"
+	_ "chromiumos/tast/local/bundles/cros/chromad"
 	_ "chromiumos/tast/local/bundles/cros/crash"
 	_ "chromiumos/tast/local/bundles/cros/crostini"
 	_ "chromiumos/tast/local/bundles/cros/cryptohome"
