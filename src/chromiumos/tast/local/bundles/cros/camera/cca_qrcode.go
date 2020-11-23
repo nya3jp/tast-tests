c2e0c6ec728df1910a9794621e47d8501e0d3d82
Shik Chen <shik@chromium.org>
2020-11-23 18:34:08 +0800

    camera.CCAQRCode: Add a test to check BarcodeDetector API
    This CL adds a functional test for the BarcodeDetector API used in CCA.
    
    The warmupTimes and average time calculation logic will be used in a
    subsequent new CCAQrcodePerf test.
    
    BUG=b:172879638
    TEST=tast run <DUT> camera.CCAQRCode
    
    Change-Id: I6e07e5d323822095f56ac15c39962f402457c66a

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_qrcode.go
@@ -0,0 +1,53 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAQRCode,
+		Desc:         "Verifies the BarcodeDetector API used in CCA",
+		Contacts:     []string{"shik@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Data:         []string{"cca_qrcode.html", "cca_qrcode.js", "qrcode_3024x3024.jpg"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func CCAQRCode(ctx context.Context, s *testing.State) {
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	cr := s.PreValue().(*chrome.Chrome)
+	conn, err := cr.NewConn(ctx, server.URL+"/cca_qrcode.html")
+	if err != nil {
+		s.Fatal("Failed to open testing page: ", err)
+	}
+	defer conn.Close()
+
+	// Name the arguments to make the function call more clear.
+	imageURL := "qrcode_3024x3024.jpg"
+	expectedCode := "https://www.google.com/chromebook/chrome-os/"
+	width := 720
+	height := 720
+	warmupTimes := 0
+	times := 1
+
+	code := fmt.Sprintf("Tast.scan(%q, %d, %d, %q, %d, %d)",
+		imageURL, width, height, expectedCode, warmupTimes, times)
+	if err := conn.Eval(ctx, code, nil); err != nil {
+		s.Error("Failed to scan: ", err)
+	}
+}
