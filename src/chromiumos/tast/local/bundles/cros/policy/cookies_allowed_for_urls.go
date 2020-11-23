76213993eb3288092086a056d830c5b05cc95e42
Nikita Podguzov <nikitapodguzov@chromium.org>
2020-11-23 17:00:34 +0100

    tast-tests: Add policy.CookiesAllowedForUrls
    Port autotest to tast.
    
    BUG=chromium:1125527
    TEST=tast run <device> policy.CookiesAllowedForUrls
    
    Change-Id: I2c585f17ad39de95ce554fd1b3c73751a5288e8b

---
+++ src/chromiumos/tast/local/bundles/cros/policy/cookies_allowed_for_urls.go
@@ -0,0 +1,95 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/pre"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CookiesAllowedForUrls,
+		Desc: "Check that the CookiesAllowedForUrls policy allows setting cookies only on the given sites",
+		Contacts: []string{
+			"nikitapodguzov@chromium.org", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Pre:          pre.User,
+		Data:         []string{"cookies_test.html"},
+	})
+}
+
+func CookiesAllowedForUrls(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*pre.PreData).Chrome
+	fdms := s.PreValue().(*pre.PreData).FakeDMS
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+	url := server.URL
+
+	for _, tc := range []struct {
+		name  string
+		value *policy.CookiesAllowedForUrls
+		allow bool
+	}{
+		{
+			name:  "unset",
+			value: &policy.CookiesAllowedForUrls{Stat: policy.StatusUnset},
+			allow: false,
+		},
+		{
+			name:  "allow_single",
+			value: &policy.CookiesAllowedForUrls{Val: []string{url}},
+			allow: true,
+		},
+		{
+			name:  "allow_multiple",
+			value: &policy.CookiesAllowedForUrls{Val: []string{"http://google.com", "http://doesnotmatter.com", url}},
+			allow: true,
+		},
+		{
+			name:  "block_multiple",
+			value: &policy.CookiesAllowedForUrls{Val: []string{"https://testingwebsite.html", "https://somewebsite.com", "http://doesnotmatter.com"}},
+			allow: false,
+		},
+	} {
+		s.Run(ctx, tc.name, func(ctx context.Context, s *testing.State) {
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to reset Chrome: ", err)
+			}
+
+			// For sites not included in allowlist Chrome falls back to default policy.
+			blockCookies := &policy.DefaultCookiesSetting{Val: 2}
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{tc.value, blockCookies}); err != nil {
+				s.Fatal("Failed to serve and verify policies: ", err)
+			}
+
+			conn, err := cr.NewConn(ctx, url+"/cookies_test.html")
+			if err != nil {
+				s.Fatal("Failed to connect to chrome: ", err)
+			}
+			defer conn.Close()
+
+			allowed := false
+			// Evaluating is going to fail if Cookies is not allowed.
+			if err := conn.Eval(ctx, "cookiesAllowed", &allowed); err != nil && tc.allow {
+				s.Fatal("Failed to read cookiesAllowed although cookies should be allowed: ", err)
+			} else {
+				if allowed != tc.allow {
+					s.Fatalf("Unexpected cookiesAllowed value; got %v, want %v", allowed, tc.allow)
+				}
+			}
+		})
+	}
+}
