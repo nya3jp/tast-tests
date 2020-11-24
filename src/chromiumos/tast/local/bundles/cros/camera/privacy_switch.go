6865f9886aade12ce38cd95c5876b34f977eaecf
Ricardo Ribalda <ribalda@chromium.org>
2020-11-24 21:28:35 +0100

    camera: Add camera.PrivacySwitch test
    Validate that the Privacy Switch is properly exposed as a V4L2 control.
    
    BUG=b:174165571
    TEST=tast run $DUT camera.PrivacySwitch
    
    Cq-Depend: chrome-internal:3527666 chrome-internal:3527247
    Change-Id: Iae600d2382fbaa258fbf04a4abc68eae05ff764c

---
+++ src/chromiumos/tast/local/bundles/cros/camera/privacy_switch.go
@@ -0,0 +1,105 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strconv"
+
+	"chromiumos/tast/local/bundles/cros/camera/testutil"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PrivacySwitch,
+		Desc: "Verifies the privacy switch",
+		Contacts: []string{
+			"ribalda@chromium.org",
+			"chromeos-camera-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{caps.BuiltinUSBCamera},
+	})
+}
+
+var ctrlBusy = regexp.MustCompile(`(?m)^VIDIOC_G_EXT_CTRLS: failed: Device or resource busy$`)
+
+func hasPrivacySwitchControl(ctx context.Context, s *testing.State) bool {
+
+	usbCams, err := testutil.GetUSBCamerasFromV4L2Test(ctx)
+	if err != nil {
+		s.Fatal("Failed to get USB cameras: ", err)
+	}
+	if len(usbCams) == 0 {
+		s.Fatal("Failed to find any valid device")
+	}
+	s.Log("USB cameras: ", usbCams)
+
+	for _, devicePath := range usbCams {
+
+		cmd := testexec.CommandContext(ctx, "v4l2-ctl", "-C", "privacy", "-d", devicePath)
+		out, err := cmd.Output(testexec.DumpLogOnError)
+
+		if err == nil {
+			s.Log("Device ", devicePath, " has a Privacy Control")
+			return true
+		}
+
+		if cmd.ProcessState.ExitCode() == 255 {
+			if ctrlBusy.Match(out) {
+				s.Log("Device ", devicePath, " has a Privacy Control")
+				return true
+			}
+		}
+	}
+
+	s.Log("No Device has a Privacy Control")
+	return false
+}
+
+func hasPrivacySwitchHardware(ctx context.Context, s *testing.State) bool {
+
+	out, err := exec.Command("cros_config", "/camera", "count").Output()
+	if err != nil {
+		s.Fatal("Failed to execute cros_config: ", err)
+	}
+
+	count, err := strconv.Atoi(string(out))
+	if err != nil {
+		s.Fatal("Failed to get number of cameras: ", err)
+	}
+
+	for i := 0; i < count; i++ {
+		out, err = exec.Command("cros_config", fmt.Sprintf("/camera/devices/%v", i), "has-privacy-switch").Output()
+		if err == nil && string(out) == "true" {
+			s.Log("Camera ", i, " supports Privacy Switch")
+			return true
+		}
+	}
+
+	s.Log("No Privacy Switch Found")
+	return false
+}
+
+func PrivacySwitch(ctx context.Context, s *testing.State) {
+
+	hasControl := hasPrivacySwitchControl(ctx, s)
+	hasHardware := hasPrivacySwitchHardware(ctx, s)
+
+	if hasHardware && !hasControl {
+		s.Error("PrivacySwitch present but no video device can access it")
+	}
+
+	if hasControl && !hasHardware {
+		s.Error("PrivacySwitch not present in hardware but accessible via v4l control")
+	}
+
+}
