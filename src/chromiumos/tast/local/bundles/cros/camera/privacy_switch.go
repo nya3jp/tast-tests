92d2c1161726a9d8c5b5dce204096cfe46555e65
Ricardo Ribalda <ribalda@chromium.org>
2020-11-24 21:28:35 +0100

    camera: Add camera.PrivacySwitch test
    Validate that the Privacy Switch is properly exposed as a V4L2 control.
    
    BUG=b:174165571
    TEST=tast run $DUT camera.PrivacySwitch
    
    Change-Id: Iae600d2382fbaa258fbf04a4abc68eae05ff764c

---
+++ src/chromiumos/tast/local/bundles/cros/camera/privacy_switch.go
@@ -0,0 +1,61 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"regexp"
+
+	"chromiumos/tast/local/bundles/cros/camera/testutil"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PrivacySwitch,
+		Desc: "Verifies the privacy switch",
+		Contacts: []string{
+			"ribalda@chromium.org",
+			"chromeos-camera-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{caps.BuiltinUSBCamera, caps.PrivacySwitch},
+	})
+}
+
+var ctrlBusy = regexp.MustCompile(`(?m)^VIDIOC_G_EXT_CTRLS: failed: Device or resource busy$`)
+
+func PrivacySwitch(ctx context.Context, s *testing.State) {
+
+	usbCams, err := testutil.GetUSBCamerasFromV4L2Test(ctx)
+	if err != nil {
+		s.Fatal("Failed to get USB cameras: ", err)
+	}
+	if len(usbCams) == 0 {
+		s.Fatal("Failed to find any valid device")
+	}
+	s.Log("USB cameras: ", usbCams)
+
+	for _, devicePath := range usbCams {
+
+		cmd := testexec.CommandContext(ctx, "v4l2-ctl", "-C", "privacy", "-d", devicePath)
+		out, err := cmd.Output(testexec.DumpLogOnError)
+
+		if err == nil {
+			continue
+		}
+
+		if cmd.ProcessState.ExitCode() == 255 {
+			if ctrlBusy.Match(out) {
+				continue
+			}
+		}
+
+		s.Error("Error accessing Privacy Control on ", devicePath, " ", err, " ", string(out))
+	}
+
+}
