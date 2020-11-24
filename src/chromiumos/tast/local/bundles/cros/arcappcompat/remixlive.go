569929986f0e5d115edeb63f13b8ef135f644ba3
Archana Singh <archanasing@google.com>
2020-11-24 11:09:44 -0800

    Run UIAutomator Test Using Tast For Remixlive app
    BUG=b:171984698
    TEST=tast run <eve> arcappcompat.Remixlive
    
    Tast Test for Remixlive app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,Reopen tests.
    
    Change-Id: I625f235735c756ec71aba59a0f3764d4c4a88568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2558852
    Tested-by: Archana Singh <archanasing@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Commit-Queue: Archana Singh <archanasing@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/remixlive.go
@@ -0,0 +1,104 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForRemixlive = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForRemixlive},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForRemixlive = []testutil.TestCase{
+	{Name: "Launch app in TouchvieStudiow", Fn: launchAppForRemixlive},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Remixlive,
+		Desc:         "Functional test for Remixlive that installs the app also verifies it is logged in and that the main page is open, checks Remixlive correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForRemixlive,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForRemixlive,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForRemixlive,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForRemixlive,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// Remixlive test uses library for opting into the playstore and installing app.
+// Checks Remixlive correctly changes the window states in both clamshell and touchview mode.
+func Remixlive(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.mixvibes.remixlive"
+		appActivity = ".app.RemixliveSplashScreenActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForRemixlive verifies Remixlive is logged in and
+// verify Remixlive reached main activity page of the app.
+func launchAppForRemixlive(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		allowText       = "ALLOW"
+		hamburgerIconID = "com.mixvibes.remixlive:id/open_drawer_btn"
+	)
+
+	// Keep clicking allow button until hamburger button exists.
+	hamburgerButton := d.Object(ui.ID(hamburgerIconID))
+	allowButton := d.Object(ui.Text(allowText))
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := hamburgerButton.Exists(ctx); err != nil {
+			allowButton.Click(ctx)
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.DefaultUITimeout}); err != nil {
+		s.Log("beLikeThemButton doesn't exists: ", err)
+	}
+
+	// Check for hamburger button in home page.
+	if err := hamburgerButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("Hamburger button doesn't exist: ", err)
+	}
+}
