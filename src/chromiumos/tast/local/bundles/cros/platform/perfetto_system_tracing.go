62d4a863d789321a3fef737e93a0066bf4cbda06
Chinglin Yu <chinglinyu@chromium.org>
2020-11-25 19:09:41 +0800

    tast-tests: Add test platform.PerfettoSystemTracing
    platform.PerfettoSystemTracing is a functional test against the perfetto
    system tracing service daemons. It starts the system tracing service
    daemon, traced (which brings up the system probes, traced_probes
    automatically) and then collect a system-wide trace to exercise
    traced_probes. It also checks crashes (e.g. seccomp policy violation)
    after trace collection.
    
    BUG=b:147789115
    TEST=./repeat-test.sh -s -r 100 -- $IP_ADDR \
          platform.PerfettoSystemTracing
    
    Change-Id: Ib94d45221b6692da1ff7a5d7298a5f01b95268af

---
+++ src/chromiumos/tast/local/bundles/cros/platform/perfetto_system_tracing.go
@@ -0,0 +1,154 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+const (
+	tracedJob       = "traced"
+	tracedProbesJob = "traced_probes"
+	waitDuration    = 10 * time.Second
+
+	traceConfigName = "perfetto/system_trace_cfg.pbtxt"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     PerfettoSystemTracing,
+		Desc:     "Verifies functions of Perfetto traced and traced_probes",
+		Contacts: []string{"chinglinyu@chromium.org"},
+		Data:     []string{traceConfigName},
+		Attr:     []string{"group:mainline", "informational"}, // TODO(chinglinyu) remove informational once perfetto is landed
+	})
+}
+
+// ensureJobsStopped stops traced and makes sure traced_probes is also stopped.
+func ensureJobsStopped(ctx context.Context) error {
+	// Use a short context for waiting job status.
+	wctx, wcancel := context.WithTimeout(ctx, 10*time.Second)
+	defer wcancel()
+
+	// Stop traced.
+	if err := upstart.StopJob(wctx, tracedJob); err != nil {
+		return errors.Wrapf(err, "failed to stop job %s", tracedJob)
+	}
+
+	// Check that traced_probes is also stopped with traced.
+	if err := upstart.WaitForJobStatus(wctx, tracedProbesJob, upstart.StopGoal, upstart.WaitingState, upstart.RejectWrongGoal, waitDuration); err != nil {
+		return errors.Wrapf(err, "job %s isn't stopped", tracedProbesJob)
+	}
+
+	return nil
+}
+
+// collectTraceData collect a system-wide trace using the perfetto command line
+// too.
+func collectTraceData(ctx context.Context, s *testing.State) error {
+	// Trace config specifies a 5-sec duration. Use a 10 sec
+	wctx, wcancel := context.WithTimeout(ctx, 10*time.Second)
+	defer wcancel()
+
+	// Start a trace session using the perfetto command line tool.
+	traceOutputPath := filepath.Join(s.OutDir(), "perfetto_trace.proto")
+	traceConfigPath := s.DataPath(traceConfigName)
+	cmd := testexec.CommandContext(wctx, "/usr/bin/perfetto", "-c", traceConfigPath, "--txt", "-o", traceOutputPath)
+	if err := cmd.Run(); err != nil {
+		return errors.Wrap(err, "failed to run the tracing session")
+	}
+
+	// Validate the trace data.
+	stat, err := os.Stat(traceOutputPath)
+	if err != nil {
+		return errors.Wrapf(err, "unexpected error stating %s", traceOutputPath)
+	}
+	s.Logf("Collected %d bytes of trace data", stat.Size())
+	// TODO(chinglinyu): really validate the trace data content.
+	return nil
+}
+
+// getRunningJobStatus checks the status of job traced and traced_probes.
+// Returns the traced and traced_probes process IDs on success or an error if
+// either job is not in the running state, or either jobs has crashed (and
+// remains in the zombie process status.
+func getRunningJobStatus(ctx context.Context) (int, int, error) {
+	// Use a short context for waiting job status.
+	wctx, wcancel := context.WithTimeout(ctx, 15*time.Second)
+	defer wcancel()
+
+	// Ensure traced is not in the zombie process state.
+	if err := upstart.CheckJob(wctx, tracedJob); err != nil {
+		return 0, 0, err
+	}
+	// Get the PID of traced.
+	_, _, tracedPid, err := upstart.JobStatus(wctx, tracedJob)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// Sleep briefly to give traced_probes time to start.
+	if err := testing.Sleep(wctx, 3*time.Second); err != nil {
+		return 0, 0, nil
+	}
+
+	// Ensure traced_probes is not in the zombie process state.
+	if err := upstart.CheckJob(wctx, tracedProbesJob); err != nil {
+		return 0, 0, err
+	}
+	// Get the PID of traced_probes.
+	_, _, tracedProbesPid, err := upstart.JobStatus(wctx, tracedProbesJob)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return tracedPid, tracedProbesPid, nil
+}
+
+// PerfettoSystemTracing tests perfetto system-wide trace collection.
+func PerfettoSystemTracing(ctx context.Context, s *testing.State) {
+	wctx, wcancel := context.WithTimeout(ctx, 10*time.Second)
+	defer wcancel()
+	// Make sure traced is running (and start it if not).
+	if err := upstart.EnsureJobRunning(wctx, tracedJob); err != nil {
+		s.Fatalf("Job %s isn't running", tracedJob)
+	}
+	defer func() {
+		if err := ensureJobsStopped(ctx); err != nil {
+			s.Fatal("Error in stopping the jobs: ", err)
+		}
+	}()
+
+	// Remember the PID of both jobs to verify that the jobs didn't have seccomp crash during trace collection.
+	tracedPid, tracedProbesPid, err := getRunningJobStatus(ctx)
+	if err != nil {
+		s.Fatal("Error in getting job status: ", err)
+	}
+
+	if err := collectTraceData(ctx, s); err != nil {
+		s.Fatal("Failed to collect trace data: ", err)
+	}
+
+	tracedPid2, tracedProbesPid2, err := getRunningJobStatus(ctx)
+	if err != nil {
+		s.Fatal("Error in getting job status after trace collection: ", err)
+	}
+
+	// Check thhat PID stays the same as a heuristic that the jobs didn't crash during the test.
+	if tracedPid != tracedPid2 {
+		s.Errorf("Unexpected respawn of job %s (PID changed from %d to %d)", tracedJob, tracedPid, tracedPid2)
+	}
+	if tracedProbesPid != tracedProbesPid2 {
+		s.Errorf("Unexpected respawn of job %s (PID changed from %d to %d)", tracedProbesJob, tracedProbesPid, tracedProbesPid2)
+	}
+}
