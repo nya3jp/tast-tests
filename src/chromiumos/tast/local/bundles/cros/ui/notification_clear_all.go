1519d50c72137ee67256dc7c96e2c9c1287fdc3c
Kyle Shimabukuro <kyleshima@chromium.org>
2020-11-24 22:51:29 -0800

    Add test for clearing all notifications
    Add a test to clear all notifications with the 'Clear all' button.
    Automates this P0 manual test:
    - https://testtracker.googleplex.com/testplans/testcase/detail/1973331?id=590&revision=34
    
    BUG=chromium:1133749
    TEST=tast run <ip> ui.NotificationClearAll
    
    Change-Id: Iaa6918eac33ea992074ab98f648eb792999c6791

---
+++ src/chromiumos/tast/local/bundles/cros/ui/notification_clear_all.go
@@ -0,0 +1,88 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/ui/quicksettings"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: NotificationClearAll,
+		Desc: "Checks that the 'Clear all' button dismisses all notifications",
+		Contacts: []string{
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+// NotificationClearAll tests that several notifications can be dismissed with the 'Clear all' button.
+func NotificationClearAll(ctx context.Context, s *testing.State) {
+	const uiTimeout = 30 * time.Second
+
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	const n = 10
+	for i := 0; i < n; i++ {
+		title := fmt.Sprintf("TestNotification%v", i)
+		if _, err := ash.CreateTestNotification(ctx, tconn, ash.NotificationTypeBasic, title, "blahhh"); err != nil {
+			s.Fatalf("Failed to create test notification %v: %v", i, err)
+		}
+		if _, err := ash.WaitForNotification(ctx, tconn, uiTimeout, ash.WaitTitle(title)); err != nil {
+			s.Fatalf("Failed waiting for %v: %v", title, err)
+		}
+	}
+
+	// Open Quick Settings to ensure the 'Clear all' button is available.
+	if err := quicksettings.Show(ctx, tconn); err != nil {
+		s.Fatal("Failed to open Quick Settings: ", err)
+	}
+	defer quicksettings.Hide(ctx, tconn)
+
+	// Find and click the button.
+	clearAllParams := ui.FindParams{Name: "Clear all", Role: ui.RoleTypeStaticText}
+	clearAll, err := ui.FindWithTimeout(ctx, tconn, clearAllParams, uiTimeout)
+	if err != nil {
+		s.Fatal("Failed to find 'Clear all' button: ", err)
+	}
+	defer clearAll.Release(ctx)
+	if err := clearAll.LeftClick(ctx); err != nil {
+		s.Fatal("Failed to click 'Clear all' button: ", err)
+	}
+
+	// Wait until all notifications and the 'Clear all' button are gone.
+	r, err := regexp.Compile("TestNotification")
+	if err != nil {
+		s.Fatal("Failed to compile notification regex: ", err)
+	}
+	notificationParams := ui.FindParams{
+		ClassName:  "MessageView",
+		Attributes: map[string]interface{}{"name": r},
+	}
+	if err := ui.WaitUntilGone(ctx, tconn, notificationParams, uiTimeout); err != nil {
+		s.Fatal("Failed waiting for notifications to be dismissed: ", err)
+	}
+	if err := ui.WaitUntilGone(ctx, tconn, clearAllParams, uiTimeout); err != nil {
+		s.Fatal("Failed waiting for 'Clear all' button to be gone: ", err)
+	}
+}
