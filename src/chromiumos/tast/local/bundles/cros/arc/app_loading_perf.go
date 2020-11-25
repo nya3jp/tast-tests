fc7b2d9bb74f87fbb4524b68e74e1038f5a5120d
Alan Ding <alanding@google.com>
2020-11-25 15:57:32 -0800

    tast-tests: Add tc-tbf support in arc.AppLoadingPerf and nethelper
    Add initial traffic shaping settings to network test (see bug for more
    details).  Comment from https://crrev.com/c/2549823 below for reference.
    
    -----------
    ARC++/ARCVM App Load Time Performance tast (on-device lab test) requires
    the use of network delays / delay distribution emulation as well as tc
    (traffic control) qdisc classless token bucket filter to shape traffic.
    Goal is for reproducible tcp receive testing that is closer to real
    home network situations.
    
    netem will be used in conjunction with iperf3 bandwidth and ping RTT
    latency measurements. General idea for tbf is if data packets >>> token
    rate queuing will ensue with possible packet loss, and if the data
    packets <<< token rate can allow sending data beyond current token rate
    or in bursts. Parameters such as burst size, latency, rate, peakrate,
    etc. can be used.
    
    Currently, ARC++ can reach really high bandwidth quickly on-device
    unlike ARCVM where there is more buffering and variances.  On-device
    test without tc exacerbates this. TBF limits the egress bandwidth and
    simulate situation where system produces packets faster than the medium
    can handle in the ARCVM case.
    -----------
    
    BUG=b:169947242
    TEST=tast run arc.AppLoadingPerf.* and tc qdisc show <iface> to verify.
    
    Change-Id: I22bbe632717dac93517e10e04beae0473d2852bb

--- src/chromiumos/tast/local/bundles/cros/arc/app_loading_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/app_loading_perf.go
@@ -34,11 +34,11 @@ var (
 
 	// arcAppLoadingBooted is a precondition similar to arc.Booted(). The only difference from arc.Booted() is
 	// that it disables some heavy post-provisioned Android activities that use system resources.
-	arcAppLoadingBooted = arc.NewPrecondition("arcapploading_booted", arcAppLoadingGaia, "--arc-disable-app-sync", "--arc-disable-play-auto-install", "--arc-disable-locale-sync", "--arc-play-store-auto-update=off")
+	arcAppLoadingBooted = arc.NewPrecondition("arcapploading_booted", arcAppLoadingGaia, arc.DisableSyncFlags()...)
 
 	// arcAppLoadingVMBooted is a precondition similar to arc.VMBooted(). The only difference from arc.VMBooted() is
 	// that it disables some heavy post-provisioned Android activities that use system resources.
-	arcAppLoadingVMBooted = arc.NewPrecondition("arcapploading_vmbooted", arcAppLoadingGaia, "--ignore-arcvm-dev-conf", "--arc-disable-app-sync", "--arc-disable-play-auto-install", "--arc-disable-locale-sync", "--arc-play-store-auto-update=off")
+	arcAppLoadingVMBooted = arc.NewPrecondition("arcapploading_vmbooted", arcAppLoadingGaia, append(arc.DisableSyncFlags(), "--ignore-arcvm-dev-conf")...)
 )
 
 func init() {
@@ -146,6 +146,17 @@ func AppLoadingPerf(ctx context.Context, s *testing.State) {
 			s.Logf("WARNING: Failed to close nethelper connection: %s", err)
 		}
 	}()
+
+	// TODO(b/169947243): Add initial traffic control queuing discipline settings
+	// for traffic shaping based on experiments with netem, RTT latency, and iperf3
+	// bandwidth measurements.  Only kernel version 4.4+ with ARCVM supports tc-tbf.
+	if kernelSupported, err := apploading.IsKernelVersionAtLeast(ctx, 4, 4); err != nil {
+		s.Fatal("Failed to check kernel version: ", err)
+	} else if kernelSupported {
+		if err := conn.AddTcTbf(ctx, 10, 18, 10); err != nil {
+			s.Fatal("Failed to add tc-tbf: ", err)
+		}
+	}
 
 	finalPerfValues := perf.NewValues()
 	param := s.Param().(testParameters)
