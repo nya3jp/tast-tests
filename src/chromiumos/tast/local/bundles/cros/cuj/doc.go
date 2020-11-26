accc01f772d903bb65a5b81cd416e57b5ee26c9f
Septem Li <septemli@cienet.com>
2020-11-27 07:57:58 +0800

    tast-tests: add LocalStoreService to create temporary dir on DUT to store service generated content
    BUG=None
    TEST=./fast_build -T
    
    Change-Id: I34beda4681fa758041b9b83d6d2b33ee59b1f1ce

---
+++ src/chromiumos/tast/local/bundles/cros/cuj/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package cuj contains implementations of gRPC services for CUJ cases.
+package cuj
