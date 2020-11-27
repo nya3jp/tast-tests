20785ad9c66af4fa0009ea705247cf98c131c35b
Hirokazu Honda <hiroh@chromium.org>
2020-11-27 11:08:30 +0900

    video: Add video.EncodeAccelNewPerf
    This CL adds new encode accelerator performance tests. It runs
    video_encode_accelerator_perf_tests.
    
    BUG=b:174322759
    TEST=tast run video.EncodeAccelNewPerf.*
    
    Change-Id: I53f05095d16a2d9ca8ac93cace387c1cdd6a81a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2562914
    Tested-by: Hirokazu Honda <hiroh@chromium.org>
    Auto-Submit: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Greg Edelston <gredelston@google.com>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/encode/accel_video_new.go
+++ src/chromiumos/tast/local/bundles/cros/video/encode/accel_video_new.go
@@ -13,12 +13,14 @@ import (
 	"strings"
 	"time"
 
+	"chromiumos/tast/common/perf"
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/fsutil"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/gtest"
+	"chromiumos/tast/local/media/cpu"
 	"chromiumos/tast/local/media/encoding"
 	"chromiumos/tast/local/media/logging"
 	"chromiumos/tast/local/media/videotype"
@@ -27,10 +29,18 @@ import (
 	"chromiumos/tast/testing"
 )
 
+// Duration of the interval during which CPU usage will be measured in the performance test.
+const measureInterval = 20 * time.Second
+
 // TestOptionsNew is the options for runNewAccelVideoTest.
 type TestOptionsNew struct {
 	WebMName string
 	Profile  videotype.CodecProfile
+}
+
+// TestData returns the files used in video.EncodeAccelNew(Perf), the webm file and the json file returned by encode.YUVJSONFileNameFor().
+func TestData(webmFileName string) []string {
+	return []string{webmFileName, YUVJSONFileNameFor(webmFileName)}
 }
 
 // YUVJSONFileNameFor returns the json file name used in video.EncodeAccelNew with |webmMFileName|.
@@ -57,6 +67,23 @@ func codecProfileToEncodeCodecOption(profile videotype.CodecProfile) (string, er
 	}
 }
 
+// prepareYUVFiles encodes webMName and creates yuv file whose format is I420.
+// Returns the paths of the YUV file and the associated JSON file used in the test.
+func prepareYUVFiles(ctx context.Context, webMPath, srcYUVJSONPath string) (string, string, error) {
+	yuvPath, err := encoding.PrepareYUV(ctx, webMPath, videotype.I420, coords.NewSize(0, 0) /* placeholder size */)
+	if err != nil {
+		return "", "", errors.Wrap(err, "failed to prepare YUVFile")
+	}
+
+	dstYUVJSONPath := yuvPath + ".json"
+	if err := fsutil.CopyFile(srcYUVJSONPath, dstYUVJSONPath); err != nil {
+		os.Remove(yuvPath)
+		return "", "", errors.Wrapf(err, "failed to copy json file: %v %v", srcYUVJSONPath, dstYUVJSONPath)
+
+	}
+	return yuvPath, dstYUVJSONPath, nil
+}
+
 // RunNewAccelVideoTest runs all tests in video_encode_accelerator_tests.
 func RunNewAccelVideoTest(ctxForDefer context.Context, s *testing.State, opts TestOptionsNew) {
 	vl, err := logging.NewVideoLogger()
@@ -73,23 +100,18 @@ func RunNewAccelVideoTest(ctxForDefer context.Context, s *testing.State, opts Te
 	}
 	defer upstart.EnsureJobRunning(ctx, "ui")
 
-	yuvPath, err := encoding.PrepareYUV(ctx, s.DataPath(opts.WebMName), videotype.I420, coords.NewSize(0, 0) /* placeholder size */)
+	yuvPath, yuvJSONPath, err := prepareYUVFiles(ctx, s.DataPath(opts.WebMName),
+		s.DataPath(YUVJSONFileNameFor(opts.WebMName)))
 	if err != nil {
-		s.Fatal("Failed to prepare YUV file: ", err)
+		s.Fatal("Failed to prepare YUV files: ", err)
 	}
 	defer os.Remove(yuvPath)
-
-	yuvJSONFileName := YUVJSONFileNameFor(opts.WebMName)
-	yuvJSONPath := yuvPath + ".json"
-	if err := fsutil.CopyFile(s.DataPath(yuvJSONFileName), yuvJSONPath); err != nil {
-		s.Fatal("Failed to copy json file: ", s.DataPath(yuvJSONFileName), yuvJSONPath)
-	}
 	defer os.Remove(yuvJSONPath)
+
 	codec, err := codecProfileToEncodeCodecOption(opts.Profile)
 	if err != nil {
 		s.Fatal("Failed to get codec option: ", err)
 	}
-
 	testArgs := []string{logging.ChromeVmoduleFlag(),
 		fmt.Sprintf("--codec=%s", codec),
 		yuvPath,
@@ -110,4 +132,131 @@ func RunNewAccelVideoTest(ctxForDefer context.Context, s *testing.State, opts Te
 		}
 		s.Fatalf("Failed to run %v: %v", exec, err)
 	}
+}
+
+// RunNewAccelVideoPerfTest runs video_encode_accelerator_perf_tests with the specified
+// video file.
+// - Uncapped performance: the specified test video is encoded for 300 frames by the hardware encoder as fast as possible.
+// This provides an estimate of the decoder's max performance (e.g. the maximum FPS).
+// - Capped performance: the specified test video is encoded for 300 frames by the hardware encoder at 30fps.
+// This is used to measure cpu usage and power consumption in the practical case.
+// - Quality performance: the specified test video is encoded for 300 frames and computes the SSIM and PSNR metrics of the encoded stream.
+// TODO(hiroh): Remove New once video.EncodeAccelNewPerf becomes video.EncodeAccelPerf.
+func RunNewAccelVideoPerfTest(ctxForDefer context.Context, s *testing.State, opts TestOptionsNew) error {
+	const (
+		// Name of the uncapped performance test.
+		uncappedTestname = "MeasureUncappedPerformance"
+		// Name of the capped performance test.
+		cappedTestname = "MeasureCappedPerformance"
+		// Name of the bitstream quality test.
+		qualityTestname = "MeasureProducedBitstreamQuality"
+		// The binary performance test.
+		exec = "video_encode_accelerator_perf_tests"
+	)
+
+	ctx, cancel := ctxutil.Shorten(ctxForDefer, 10*time.Second)
+	defer cancel()
+	// Setup benchmark mode.
+	cleanUpBenchmark, err := cpu.SetUpBenchmark(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to set up benchmark mode")
+	}
+	defer cleanUpBenchmark(ctx)
+
+	if err := upstart.StopJob(ctx, "ui"); err != nil {
+		s.Error("Failed to stop ui: ", err)
+	}
+	defer upstart.EnsureJobRunning(ctx, "ui")
+
+	yuvPath, yuvJSONPath, err := prepareYUVFiles(ctx, s.DataPath(opts.WebMName),
+		s.DataPath(YUVJSONFileNameFor(opts.WebMName)))
+	if err != nil {
+		return errors.Wrap(err, "failed to prepare YUV files")
+	}
+	defer os.Remove(yuvPath)
+	defer os.Remove(yuvJSONPath)
+
+	if err := cpu.WaitUntilIdle(ctx); err != nil {
+		return errors.Wrap(err, "failed to wait for CPU to become idle")
+	}
+
+	codec, err := codecProfileToEncodeCodecOption(opts.Profile)
+	if err != nil {
+		return errors.Wrap(err, "failed to get codec option")
+	}
+
+	// Test 1: Measure maximum FPS and the quality of the encoded bitstream.
+	testArgs := []string{
+		fmt.Sprintf("--codec=%s", codec),
+		fmt.Sprintf("--output_folder=%s", s.OutDir()),
+		yuvPath,
+		yuvJSONPath,
+	}
+	if report, err := gtest.New(
+		filepath.Join(chrome.BinTestDir, exec),
+		gtest.Logfile(filepath.Join(s.OutDir(), exec+".uncap_and_quality.log")),
+		gtest.Filter(fmt.Sprintf("*%s:*%s", uncappedTestname, qualityTestname)),
+		gtest.ExtraArgs(testArgs...),
+		gtest.UID(int(sysutil.ChronosUID)),
+	).Run(ctx); err != nil {
+		if report != nil {
+			for _, name := range report.FailedTestNames() {
+				s.Error(name, " failed")
+			}
+		}
+		return errors.Wrapf(err, "failed to run %v", exec)
+	}
+
+	p := perf.NewValues()
+	uncappedJSON := filepath.Join(s.OutDir(), "VideoEncoderTest", uncappedTestname+".json")
+	if _, err := os.Stat(uncappedJSON); os.IsNotExist(err) {
+		return errors.Wrap(err, "failed to find uncapped performance metrics file")
+	}
+	qualityJSON := filepath.Join(s.OutDir(), "VideoEncoderTest", qualityTestname+".json")
+	if _, err := os.Stat(qualityJSON); os.IsNotExist(err) {
+		return errors.Wrap(err, "failed to find quality performance metrics file")
+	}
+
+	if err := encoding.ParseUncappedPerfMetrics(uncappedJSON, p); err != nil {
+		return errors.Wrap(err, "failed to parse uncapped performance metrics")
+	}
+	if err := encoding.ParseQualityPerfMetrics(qualityJSON, p); err != nil {
+		return errors.Wrap(err, "failed to parse quality performance metrics")
+	}
+
+	// Test 2: Measure CPU usage and power consumption while running capped
+	// performance test only.
+	measurements, err := cpu.MeasureProcessUsage(ctx, measureInterval, cpu.KillProcess, gtest.New(
+		filepath.Join(chrome.BinTestDir, exec),
+		gtest.Logfile(filepath.Join(s.OutDir(), exec+".cap.log")),
+		gtest.Filter("*"+cappedTestname),
+		// Repeat enough times to run for full measurement duration. We don't
+		// use -1 here as this can result in huge log files (b/138822793).
+		gtest.Repeat(1000),
+		gtest.ExtraArgs(testArgs...),
+		gtest.UID(int(sysutil.ChronosUID)),
+	))
+	if err != nil {
+		return errors.Wrapf(err, "failed to measure CPU usage %v", exec)
+	}
+	p.Set(perf.Metric{
+		Name:      "cpu_usage",
+		Unit:      "percent",
+		Direction: perf.SmallerIsBetter,
+	}, measurements["cpu"])
+
+	// Power measurements are not supported on all platforms.
+	if power, ok := measurements["power"]; ok {
+		p.Set(perf.Metric{
+			Name:      "power_consumption",
+			Unit:      "watt",
+			Direction: perf.SmallerIsBetter,
+		}, power)
+	}
+
+	if err := p.Save(s.OutDir()); err != nil {
+		return errors.Wrap(err, "failed to save performance metrics")
+	}
+
+	return nil
 }
