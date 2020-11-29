9f35fe9bc8b2582f2884f17f39c88d9d0a9338ff
Jun Sheng <shengjun@google.com>
2020-11-30 09:09:43 +1100

    tast-tests: promote VirtualKeyboardTypingOmnibox to cq
    1. VirtualKeyboardTypingBrowser tests Chrome omnibox only, so rename the test to VirtualKeyboardTypingOmnibox for making it accurate.
    
    2. The test is stable for a while. Promote stable boards to CQ. https://tastboard.corp.google.com/test/inputs.VirtualKeyboardTypingBrowser.stable
    
    BUG=b:174429680
    TEST=None
    
    Change-Id: I63035c9692e6c0573b1f53aa1e940dfb77d31391

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_omnibox.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_omnibox.go
@@ -19,10 +19,10 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         VirtualKeyboardTypingBrowser,
-		Desc:         "Checks that the virtual keyboard works in Chrome browser",
+		Func:         VirtualKeyboardTypingOmnibox,
+		Desc:         "Checks that the virtual keyboard works in Chrome browser omnibox",
 		Contacts:     []string{"essential-inputs-team@google.com"},
-		Attr:         []string{"group:mainline", "informational", "group:input-tools"},
+		Attr:         []string{"group:mainline", "group:input-tools"},
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
 		Timeout:      5 * time.Minute,
 		Params: []testing.Param{{
@@ -34,6 +34,7 @@ func init() {
 			Name:              "unstable",
 			Pre:               pre.VKEnabledTablet(),
 			ExtraHardwareDeps: pre.InputsUnstableModels,
+			ExtraAttr:         []string{"informational"},
 		}, {
 			Name:              "mojo",
 			Pre:               pre.IMEServiceEnabled(pre.VKEnabledTablet()),
@@ -42,7 +43,7 @@ func init() {
 		}}})
 }
 
-func VirtualKeyboardTypingBrowser(ctx context.Context, s *testing.State) {
+func VirtualKeyboardTypingOmnibox(ctx context.Context, s *testing.State) {
 	// typingKeys indicates a key series that tapped on virtual keyboard.
 	const typingKeys = "go"
 	cr := s.PreValue().(pre.PreData).Chrome
