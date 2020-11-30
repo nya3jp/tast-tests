3ad2626672dbc099db10053160274a4123c05175
Mohamed Omar <mohamedaomar@google.com>
2020-11-30 13:16:33 +0100

    ui: New method to simplify UI nodes status checks
    Add a new helper function to simplify UI component status checks in policy tests
    
    BUG=chromium:1113188
    TEST=tast run <ip> policy.<test>
    
    Change-Id: I7e71aa3f613129c7b7ce1b5e69bfdf5e478a411c

--- src/chromiumos/tast/local/chrome/ui/automation.go
+++ src/chromiumos/tast/local/chrome/ui/automation.go
@@ -699,3 +699,24 @@ func (n *Node) FindSelectedRadioButton(ctx context.Context) (*Node, error) {
 		Attributes: map[string]interface{}{"checked": CheckedStateTrue},
 	})
 }
+
+// CheckNodeAttributes returns whether this node matches the given expected params.
+// It will return an error with the first non-matching attribute, nil otherwise.
+func (n *Node) CheckNodeAttributes(expectedParams FindParams) error {
+	if expectedRestriction, exist := expectedParams.Attributes["restriction"]; exist {
+		if n.Restriction != expectedRestriction {
+			return errors.Errorf("unexpected restriction: got %#v; want %#v", n.Restriction, expectedRestriction)
+		}
+	}
+	if expectedChecked, exist := expectedParams.Attributes["checked"]; exist {
+		if n.Checked != expectedChecked {
+			return errors.Errorf("unexpected checked: got %#v; want %#v", n.Checked, expectedChecked)
+		}
+	}
+	if expectedName, exist := expectedParams.Attributes["name"]; exist {
+		if n.Name != expectedName {
+			return errors.Errorf("unexpected name: got %#v; want %#v", n.Name, expectedName)
+		}
+	}
+	return nil
+}
