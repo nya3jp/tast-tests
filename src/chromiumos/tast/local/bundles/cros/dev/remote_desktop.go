a2afff444b0f74eced6e5e8ef6491ac7ac3ee8ec
Kuo Jen Wei <inker@google.com>
2020-11-30 17:31:14 +0800

    camera: Test for aligning chart screen and camera FOV in camerabox
    Add CameraboxAlign semi-manual test for guiding alignment of camera FOV
    and screen of chart tablet. The overall test sequence:
    1. Display alignment pattern on chart.
    2. Call AlignmentService methods to prepare chrome remote desktop on
       DUT.
    3. Open preview.html page on DUT for guiding alignment between camera
       preview FOV and chart pattern.
    4. Keep checking alignment until both 4x3 and 16x9 can pass.
    
    BUG=b:166370953
    TEST=tast run -var=user=<USERNAME> -var=pass=<PASSWORD>
    -var=chart=<CHART IP> -var=facing=<back or front> <DUT>
    camera.CameraboxAlign
    
    Change-Id: Ia3cb3f8471a4b5029349da4d511e28e48a135298

--- src/chromiumos/tast/local/bundles/cros/dev/remote_desktop.go
+++ src/chromiumos/tast/local/bundles/cros/dev/remote_desktop.go
@@ -8,21 +8,13 @@ import (
 	"context"
 	"strconv"
 	"strings"
-	"time"
 
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/crd"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
-
-const crdURL = "https://remotedesktop.google.com/support"
-
-// Do not poll things too fast to prevent triggering weird UI behaviors. The
-// share button will not work if we keep clicking it.
-var rdpPollOpts = &testing.PollOptions{Interval: time.Second}
 
 // rdpVars represents the configurable parameters for the remote desktop session.
 type rdpVars struct {
@@ -62,160 +54,6 @@ func init() {
 				"krane", "nocturne")),
 		}},
 	})
-}
-
-// ensureAppInstalled ensures the companion extension for the Chrome Remote
-// Desktop website https://remotedesktop.google.com is installed.
-func ensureAppInstalled(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn) error {
-	const appCWSURL = "https://chrome.google.com/webstore/detail/chrome-remote-desktop/inomeogfingihgjfjlpeplalcfajhgai?hl=en"
-
-	cws, err := cr.NewConn(ctx, appCWSURL)
-	if err != nil {
-		return err
-	}
-	defer cws.Close()
-	defer cws.CloseTarget(ctx)
-
-	// Click the add button at most once to prevent triggering
-	// weird UI behaviors in Chrome Web Store.
-	addClicked := false
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		// Check if Remote Desktop is installed.
-		params := ui.FindParams{
-			Name: "Remove from Chrome",
-			Role: ui.RoleTypeButton,
-		}
-		if installed, err := ui.Exists(ctx, tconn, params); err != nil {
-			return testing.PollBreak(err)
-		} else if installed {
-			return nil
-		}
-
-		if !addClicked {
-			// If Remote Desktop is not installed, install it now.
-			// Click on the add button, if it exists.
-			params = ui.FindParams{
-				Name: "Add to Chrome",
-				Role: ui.RoleTypeButton,
-			}
-			if addButtonExists, err := ui.Exists(ctx, tconn, params); err != nil {
-				return testing.PollBreak(err)
-			} else if addButtonExists {
-				addButton, err := ui.Find(ctx, tconn, params)
-				if err != nil {
-					return testing.PollBreak(err)
-				}
-				defer addButton.Release(ctx)
-
-				if err := addButton.LeftClick(ctx); err != nil {
-					return testing.PollBreak(err)
-				}
-				addClicked = true
-			}
-		}
-
-		// Click on the confirm button, if it exists.
-		params = ui.FindParams{
-			Name: "Add extension",
-			Role: ui.RoleTypeButton,
-		}
-		if confirmButtonExists, err := ui.Exists(ctx, tconn, params); err != nil {
-			return testing.PollBreak(err)
-		} else if confirmButtonExists {
-			confirmButton, err := ui.Find(ctx, tconn, params)
-			if err != nil {
-				return testing.PollBreak(err)
-			}
-			defer confirmButton.Release(ctx)
-
-			if err := confirmButton.LeftClick(ctx); err != nil {
-				return testing.PollBreak(err)
-			}
-		}
-		return errors.New("Remote Desktop still installing")
-	}, rdpPollOpts); err != nil {
-		return errors.Wrap(err, "failed to install Remote Desktop")
-	}
-	return nil
-}
-
-func launch(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn) (*chrome.Conn, error) {
-	// Use english version to avoid i18n differences of HTML element attributes.
-	conn, err := cr.NewConn(ctx, crdURL+"?hl=en")
-	if err != nil {
-		return nil, err
-	}
-
-	const waitIdle = "new Promise(resolve => window.requestIdleCallback(resolve))"
-	if err := conn.Eval(ctx, waitIdle, nil); err != nil {
-		return nil, err
-	}
-
-	return conn, nil
-}
-
-func getAccessCode(ctx context.Context, crd *chrome.Conn) (string, error) {
-	const genCodeBtn = `document.querySelector('[aria-label="Generate Code"]')`
-	if err := crd.WaitForExpr(ctx, genCodeBtn); err != nil {
-		return "", err
-	}
-
-	const clickBtn = genCodeBtn + ".click()"
-	if err := crd.Eval(ctx, clickBtn, nil); err != nil {
-		return "", err
-	}
-
-	const codeSpan = `document.querySelector('[aria-label^="Your access code is:"]')`
-	if err := crd.WaitForExpr(ctx, codeSpan); err != nil {
-		return "", err
-	}
-
-	var code string
-	const getCode = codeSpan + `.getAttribute('aria-label').match(/\d+/g).join('')`
-	if err := crd.Eval(ctx, getCode, &code); err != nil {
-		return "", err
-	}
-	return code, nil
-}
-
-func waitConnection(ctx context.Context, tconn *chrome.TestConn) error {
-	// The share button might not be clickable at first, so we keep retrying
-	// until we see "Stop Sharing".
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		// Check if sharing
-		params := ui.FindParams{
-			Name: "Stop Sharing",
-			Role: ui.RoleTypeButton,
-		}
-		if sharing, err := ui.Exists(ctx, tconn, params); err != nil {
-			return testing.PollBreak(err)
-		} else if sharing {
-			return nil
-		}
-
-		// Click on the share button, if it exists.
-		params = ui.FindParams{
-			Name: "Share",
-			Role: ui.RoleTypeButton,
-		}
-		if shareButtonExists, err := ui.Exists(ctx, tconn, params); err != nil {
-			return testing.PollBreak(err)
-		} else if shareButtonExists {
-			shareButton, err := ui.Find(ctx, tconn, params)
-			if err != nil {
-				return testing.PollBreak(err)
-			}
-			defer shareButton.Release(ctx)
-
-			if err := shareButton.LeftClick(ctx); err != nil {
-				return testing.PollBreak(err)
-			}
-		}
-		return errors.New("still enabling sharing")
-	}, rdpPollOpts); err != nil {
-		return errors.Wrap(err, "failed to enable sharing")
-	}
-	return nil
 }
 
 // getVars extracts the testing parameters from testing.State. The user
@@ -307,27 +145,13 @@ func RemoteDesktop(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to connect Test API: ", err)
 	}
 
-	if err := ensureAppInstalled(ctx, cr, tconn); err != nil {
-		s.Fatal("Failed to install CRD app: ", err)
+	if err := crd.PrepareCRD(ctx, cr, tconn); err != nil {
+		s.Fatal("Failed to PrepareCRD: ", err)
 	}
-
-	crd, err := launch(ctx, cr, tconn)
-	if err != nil {
-		s.Fatal("Failed to launch CRD: ", err)
-	}
-	defer crd.Close()
-
-	s.Log("Getting access code")
-	accessCode, err := getAccessCode(ctx, crd)
-	if err != nil {
-		s.Fatal("Failed to getAccessCode: ", err)
-	}
-	s.Log("Access code: ", accessCode)
-	s.Log("Please paste the code to \"Give Support\" section on ", crdURL)
 
 	if vars.wait {
 		s.Log("Waiting connection")
-		if err := waitConnection(ctx, tconn); err != nil {
+		if err := crd.WaitConnection(ctx, tconn); err != nil {
 			s.Fatal("No client connected: ", err)
 		}
 	} else {
---
+++ src/chromiumos/tast/local/chrome/ui/crd/crd.go
@@ -0,0 +1,200 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package crd provides utilities to set up chrome remote desktop.
+package crd
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/testing"
+)
+
+const crdURL = "https://remotedesktop.google.com/support"
+
+// Do not poll things too fast to prevent triggering weird UI behaviors. The
+// share button will not work if we keep clicking it.
+var rdpPollOpts = &testing.PollOptions{Interval: time.Second}
+
+// ensureAppInstalled ensures the companion extension for the Chrome Remote
+// Desktop website https://remotedesktop.google.com is installed.
+func ensureAppInstalled(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn) error {
+	const appCWSURL = "https://chrome.google.com/webstore/detail/chrome-remote-desktop/inomeogfingihgjfjlpeplalcfajhgai?hl=en"
+
+	cws, err := cr.NewConn(ctx, appCWSURL)
+	if err != nil {
+		return err
+	}
+	defer cws.Close()
+	defer cws.CloseTarget(ctx)
+
+	// Click the add button at most once to prevent triggering
+	// weird UI behaviors in Chrome Web Store.
+	addClicked := false
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		// Check if Remote Desktop is installed.
+		params := ui.FindParams{
+			Name: "Remove from Chrome",
+			Role: ui.RoleTypeButton,
+		}
+		if installed, err := ui.Exists(ctx, tconn, params); err != nil {
+			return testing.PollBreak(err)
+		} else if installed {
+			return nil
+		}
+
+		if !addClicked {
+			// If Remote Desktop is not installed, install it now.
+			// Click on the add button, if it exists.
+			params = ui.FindParams{
+				Name: "Add to Chrome",
+				Role: ui.RoleTypeButton,
+			}
+			if addButtonExists, err := ui.Exists(ctx, tconn, params); err != nil {
+				return testing.PollBreak(err)
+			} else if addButtonExists {
+				addButton, err := ui.Find(ctx, tconn, params)
+				if err != nil {
+					return testing.PollBreak(err)
+				}
+				defer addButton.Release(ctx)
+
+				if err := addButton.LeftClick(ctx); err != nil {
+					return testing.PollBreak(err)
+				}
+				addClicked = true
+			}
+		}
+
+		// Click on the confirm button, if it exists.
+		params = ui.FindParams{
+			Name: "Add extension",
+			Role: ui.RoleTypeButton,
+		}
+		if confirmButtonExists, err := ui.Exists(ctx, tconn, params); err != nil {
+			return testing.PollBreak(err)
+		} else if confirmButtonExists {
+			confirmButton, err := ui.Find(ctx, tconn, params)
+			if err != nil {
+				return testing.PollBreak(err)
+			}
+			defer confirmButton.Release(ctx)
+
+			if err := confirmButton.LeftClick(ctx); err != nil {
+				return testing.PollBreak(err)
+			}
+		}
+		return errors.New("Remote Desktop still installing")
+	}, rdpPollOpts); err != nil {
+		return errors.Wrap(err, "failed to install Remote Desktop")
+	}
+	return nil
+}
+
+func launch(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn) (*chrome.Conn, error) {
+	// Use english version to avoid i18n differences of HTML element attributes.
+	conn, err := cr.NewConn(ctx, crdURL+"?hl=en")
+	if err != nil {
+		return nil, err
+	}
+
+	const waitIdle = "new Promise(resolve => window.requestIdleCallback(resolve))"
+	if err := conn.Eval(ctx, waitIdle, nil); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func getAccessCode(ctx context.Context, crd *chrome.Conn) (string, error) {
+	const genCodeBtn = `document.querySelector('[aria-label="Generate Code"]')`
+	if err := crd.WaitForExpr(ctx, genCodeBtn); err != nil {
+		return "", err
+	}
+
+	const clickBtn = genCodeBtn + ".click()"
+	if err := crd.Eval(ctx, clickBtn, nil); err != nil {
+		return "", err
+	}
+
+	const codeSpan = `document.querySelector('[aria-label^="Your access code is:"]')`
+	if err := crd.WaitForExpr(ctx, codeSpan); err != nil {
+		return "", err
+	}
+
+	var code string
+	const getCode = codeSpan + `.getAttribute('aria-label').match(/\d+/g).join('')`
+	if err := crd.Eval(ctx, getCode, &code); err != nil {
+		return "", err
+	}
+	return code, nil
+}
+
+// PrepareCRD prepares chrome remote desktop and generates access code to be connected by.
+func PrepareCRD(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn) error {
+	if err := ensureAppInstalled(ctx, cr, tconn); err != nil {
+		return errors.Wrap(err, "failed to install CRD app")
+	}
+
+	crd, err := launch(ctx, cr, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to launch CRD")
+	}
+	defer crd.Close()
+
+	testing.ContextLog(ctx, "Getting access code")
+	accessCode, err := getAccessCode(ctx, crd)
+	if err != nil {
+		return errors.Wrap(err, "failed to getAccessCode")
+	}
+	testing.ContextLog(ctx, "Access code: ", accessCode)
+	testing.ContextLog(ctx, "Please paste the code to \"Give Support\" section on ", crdURL)
+
+	return nil
+}
+
+// WaitConnection waits for remote desktop client connecting to DUT.
+func WaitConnection(ctx context.Context, tconn *chrome.TestConn) error {
+	// The share button might not be clickable at first, so we keep retrying
+	// until we see "Stop Sharing".
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		// Check if sharing
+		params := ui.FindParams{
+			Name: "Stop Sharing",
+			Role: ui.RoleTypeButton,
+		}
+		if sharing, err := ui.Exists(ctx, tconn, params); err != nil {
+			return testing.PollBreak(err)
+		} else if sharing {
+			return nil
+		}
+
+		// Click on the share button, if it exists.
+		params = ui.FindParams{
+			Name: "Share",
+			Role: ui.RoleTypeButton,
+		}
+		if shareButtonExists, err := ui.Exists(ctx, tconn, params); err != nil {
+			return testing.PollBreak(err)
+		} else if shareButtonExists {
+			shareButton, err := ui.Find(ctx, tconn, params)
+			if err != nil {
+				return testing.PollBreak(err)
+			}
+			defer shareButton.Release(ctx)
+
+			if err := shareButton.LeftClick(ctx); err != nil {
+				return testing.PollBreak(err)
+			}
+		}
+		return errors.New("still enabling sharing")
+	}, rdpPollOpts); err != nil {
+		return errors.Wrap(err, "failed to enable sharing")
+	}
+	return nil
+}
