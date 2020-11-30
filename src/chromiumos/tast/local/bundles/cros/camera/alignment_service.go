dbbfbbe06893978e2ad16dec806c4b83390c125b
Kuo Jen Wei <inker@google.com>
2020-11-30 17:31:14 +0800

    camera: Test for aligning chart screen and camera FOV in camerabox
    Add CameraboxAlign semi-manual test for guiding alignment of camera FOV
    and screen of chart tablet. The overall test sequence:
    1. Display alignment pattern on chart.
    2. Call AlignmentService methods to prepare chrome remote desktop on
       DUT.
    3. Open preview.html page on DUT for guiding alignment between camera
       preview FOV and chart pattern.
    4. Keep checking alignment until both 4x3 and 16x9 can pass.
    
    BUG=b:166370953
    TEST=tast run -var=user=<USERNAME> -var=pass=<PASSWORD>
    -var=chart=<CHART IP> -var=facing=<back or front> <DUT>
    camera.CameraboxAlign
    
    Change-Id: Ia3cb3f8471a4b5029349da4d511e28e48a135298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2566531
    Tested-by: Inker Kuo <inker@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/camera/alignment_service.go
@@ -0,0 +1,75 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui/crd"
+	pb "chromiumos/tast/services/cros/camerabox"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddService(&testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			pb.RegisterAlignmentServiceServer(srv, &AlignmentService{s: s})
+		},
+	})
+}
+
+type AlignmentService struct {
+	s *testing.ServiceState
+}
+
+func (a *AlignmentService) ManualAlign(ctx context.Context, req *pb.ManualAlignRequest) (*empty.Empty, error) {
+	srv := httptest.NewServer(http.FileServer(http.Dir(req.DataPath)))
+	defer srv.Close()
+
+	cr, err := chrome.New(ctx, chrome.ARCDisabled(),
+		chrome.Auth(req.Username, req.Password, ""), chrome.GAIALogin(),
+		chrome.KeepState(),
+		// Avoid the need to grant camera/microphone permissions.
+		chrome.ExtraArgs("--use-fake-ui-for-media-stream"))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start chrome")
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect Test API")
+	}
+	// TODO(b/166370953): Handle CRD timeout.
+	if err := crd.Launch(ctx, cr, tconn); err != nil {
+		return nil, errors.Wrap(err, "failed to launch remote desktop")
+	}
+	testing.ContextLog(ctx, "Waiting connection")
+	if err := crd.WaitConnection(ctx, tconn); err != nil {
+		return nil, errors.Wrap(err, "no client connected")
+	}
+
+	conn, err := cr.NewConn(ctx, srv.URL+"/camerabox_align.html")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open camerabox_align.html")
+	}
+	defer func() {
+		conn.CloseTarget(ctx)
+		conn.Close()
+	}()
+
+	if err := conn.Call(ctx, nil, "Tast.manualAlign", req.Facing); err != nil {
+		return nil, errors.Wrap(err, "failed to call manualAlign on camerabox_align.html")
+	}
+
+	return &empty.Empty{}, nil
+}
