f202a2d53962598f525feb2b4940185d26b40155
Ricardo Quesada <ricardoq@google.com>
2020-11-30 20:25:18 -0800

    new test: ArcGamePerformanceTest
    This is a direct copy&paste from:
    http://cs/pi-arc-dev/frameworks/base/tests/GamePerformance/
    
    Even though this test lives in framewroks/base, this test was created for ARC.
    So moving it to ChromeOS Tast repo makes sense since it is being used by
    Tast test.
    
    BUG=None
    TEST=emerge-$BOARD tast-local-apks-cros
         Install APK. Verified that it works Ok.
    Change-Id: If62ad21269fe0a64d76c0e76966ba65ae142fa9e
    
    Change-Id: Ic4d42ec5f4616b481afbae77ee66bbac50470d85

---
+++ android/ArcGamePerformanceTest/src/org/chromium/arc/testapp/gameperformance/MainActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.gameperformance;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Instrumentation;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ android/ArcGamePerformanceTest/src/org/chromium/arc/testapp/gameperformance/Utils.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.gameperformance;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Utils {
+    public static void closeQuietly(Closeable closeable) {
+        try {
+            if (closeable != null) {
+                closeable.close();
+            }
+        } catch (IOException e) {
+            // Ignore
+        }
+    }
+}
