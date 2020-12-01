9b8df89ac2d562ebd1b6a1e210c20ea83f0d91b5
Archana Singh <archanasing@google.com>
2020-12-01 12:37:49 -0800

    Run UIAutomator Test Using Tast For PostIt app
    BUG=b:171984940
    TEST=tast run <eve> arcappcompat.PostIt
    
    Tast Test for PostIt app verifies launch app
    from the playstore, check app reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: Ib9eed5e8ef643e031e5643b27071b1a5aca260bf

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/post_it.go
@@ -0,0 +1,91 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForPostIt = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForPostIt},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForPostIt = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForPostIt},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PostIt,
+		Desc:         "Functional test for PostIt that installs the app also verifies it is logged in and that the main page is open, checks PostIt correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForPostIt,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForPostIt,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForPostIt,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForPostIt,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// PostIt test uses library for opting into the playstore and installing app.
+// Checks PostIt correctly changes the window states in both clamshell and touchview mode.
+func PostIt(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.mmm.postit"
+		appActivity = ".MainActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForPostIt verifies PostIt is logged in and
+// verify PostIt reached main activity page of the app.
+func launchAppForPostIt(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		addNoteText = "ADD NOTE"
+	)
+
+	// Check for add note icon.
+	addNoteButton := d.Object(ui.Text(addNoteText))
+	if err := addNoteButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("addNote button doesn't exist: ", err)
+	}
+}
