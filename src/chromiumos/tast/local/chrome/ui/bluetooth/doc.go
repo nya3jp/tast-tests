97b8a250bc7a405eed3f28a63f6d3c6f31b51d52
Jane Yang <jane.yang@cienet.com>
2020-12-02 18:03:44 +0800

    tast-tests: Add common utils for cuj tests
    Add support library to local/chrome/ash to allow clipboard get and set.
    Add support library to local/chrome/ui/blutooth to allow BT control from
    ui.
    Add gmail app, gmail web, and netflix web management package under cros
    ui package, as well as some common ui operations for CUJ.
    
    BUG=None
    TEST=./fast_build.sh -T
    TEST=tast run <DUT> ui.TaskSwitchCUJ
    
    Change-Id: I7c4bc507a5358f72d11478f42c14f55f1da865ae

---
+++ src/chromiumos/tast/local/chrome/ui/bluetooth/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package bluetooth provides the control of bluetooth settings from the OS settings window.
+package bluetooth
