9dda3469610b994474a468b5207baa208214b332
Archana Singh <archanasing@google.com>
2020-12-01 18:24:00 -0800

    Run UIAutomator Test Using Tast For EdjingMix app
    BUG=b:171985206
    TEST=tast run <eve> arcappcompat.EdjingMix
    
    Tast Test for EdjingMix app verifies launch app
    from the playstore, check app reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: Iabf30e95c502d808eed0ef3399a47e23a38edfcb

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/edjing_mix.go
@@ -0,0 +1,148 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForEdjingMix = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForEdjingMix},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForEdjingMix = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForEdjingMix},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         EdjingMix,
+		Desc:         "Functional test for EdjingMix that installs the app also verifies it is logged in and that the main page is open, checks EdjingMix correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForEdjingMix,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForEdjingMix,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForEdjingMix,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForEdjingMix,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// EdjingMix test uses library for opting into the playstore and installing app.
+// Checks EdjingMix correctly changes the window states in both clamshell and touchview mode.
+func EdjingMix(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.edjing.edjingdjturntable"
+		appActivity = ".activities.PlatineActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForEdjingMix verifies EdjingMix is logged in and
+// verify EdjingMix reached main activity page of the app.
+func launchAppForEdjingMix(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		nextText       = "NEXT"
+		closeClassName = "android.widget.ImageView"
+		allowText      = "ALLOW"
+		skipText       = "SKIP"
+		recordingText  = "REC"
+	)
+
+	// Click on next button.
+	nextButton := d.Object(ui.Text(nextText))
+	if err := nextButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("Next button doesn't exists: ", err)
+	} else if err := nextButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on next button: ", err)
+	}
+
+	// Click on close button.
+	closeButton := d.Object(ui.ClassName(closeClassName))
+	if err := closeButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("Close button doesn't exist: ", err)
+	} else if err := closeButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on close button: ", err)
+	}
+
+	pressKeysEdjingMix(ctx, s, d)
+
+	// Click on skip button
+	skipButton := d.Object(ui.Text(skipText))
+	if err := skipButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("Skip button doesn't exists: ", err)
+	} else if err := skipButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on skip button: ", err)
+	}
+
+	// Check for recording button in home page.
+	recButton := d.Object(ui.Text(recordingText))
+	if err := recButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("Recording button doesn't exist: ", err)
+	}
+}
+
+// pressKeysEdjingMix handles Try for free and Allow to access phots, media and files dialog boxes
+func pressKeysEdjingMix(ctx context.Context, s *testing.State, d *ui.Device) {
+	var count = 1
+
+	for i := 0; i <= count; i++ {
+
+		//Press tab and enter keys
+		if err := d.PressKeyCode(ctx, ui.KEYCODE_TAB, 0); err != nil {
+			s.Log("Failed to enter KEYCODE_TAB: ", err)
+		} else {
+			s.Log("Entered KEYCODE_TAB")
+		}
+
+		if err := d.PressKeyCode(ctx, ui.KEYCODE_TAB, 0); err != nil {
+			s.Log("Failed to enter KEYCODE_TAB: ", err)
+		} else {
+			s.Log("Entered KEYCODE_TAB")
+		}
+
+		if err := d.PressKeyCode(ctx, ui.KEYCODE_ENTER, 0); err != nil {
+			s.Log("Failed to enter KEYCODE_ENTER: ", err)
+		} else {
+			s.Log("Entered KEYCODE_ENTER")
+		}
+	}
+}
