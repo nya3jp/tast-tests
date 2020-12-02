c71863ae81db07eeb338b2935295b344f3bd3eb9
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-12-01 17:24:13 -0800

    Run UIAutomator Test Using Tast For AppleMusic app
    BUG=b:172088564
    TEST=tast run <eve> arcappcompat.AppleMusic
    
    Tast Test for AppleMusic app verifies launch app
    from the playstore, check app reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: I5dacdf440962516085ce45362b0cc97deaa2b19c

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/apple_music.go
@@ -0,0 +1,133 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForAppleMusic = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForAppleMusic},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForAppleMusic = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForAppleMusic},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AppleMusic,
+		Desc:         "Functional test for AppleMusic that installs the app also verifies that the main page is open, checks AppleMusic correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForAppleMusic,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForAppleMusic,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForAppleMusic,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForAppleMusic,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// AppleMusic test uses library for opting into the playstore and installing app.
+// Checks AppleMusic correctly changes the window states in both clamshell and touchview mode.
+func AppleMusic(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.apple.android.music"
+		appActivity = ".onboarding.activities.SplashActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForAppleMusic verifies AppleMusic reached main activity page of the app.
+func launchAppForAppleMusic(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		agreeButtonText       = "Agree"
+		continueButtonText    = "Continue"
+		cancelButtonText      = "Cancel"
+		donotSendButtonText   = "DON'T SEND"
+		scrollLayoutClassName = "android.webkit.WebView"
+		homeID                = "com.apple.android.music:id/action_listen_now"
+	)
+
+	// Check for scroll layout.
+	scrollLayout := d.Object(ui.ClassName(scrollLayoutClassName), ui.Scrollable(true))
+	if err := scrollLayout.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("scrollLayout doesn't exist: ", err)
+	}
+	// Scroll until Agree button is enabled.
+	agreeButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+agreeButtonText), ui.Enabled(true))
+	scrollLayout.ScrollTo(ctx, agreeButton)
+	if err := agreeButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("agreeButton doesn't exist: ", err)
+	} else if err := agreeButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on agreeButton: ", err)
+	}
+
+	// Click on continue button.
+	continueButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+continueButtonText))
+	if err := continueButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("continueButton doesn't exist: ", err)
+	} else if err := continueButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on continueButton: ", err)
+	}
+
+	// Click on cancel button to skip subscription.
+	cancelButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+cancelButtonText))
+	if err := cancelButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("cancelButton doesn't exist: ", err)
+	} else if err := cancelButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on cancelButton: ", err)
+	}
+
+	// Click on donot send button for diagnostics.
+	donotSendButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+donotSendButtonText))
+	if err := donotSendButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("donotSendButton doesn't exist: ", err)
+	} else if err := donotSendButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on donotSendButton: ", err)
+	}
+
+	// Check for homeIcon on homePage.
+	homeIcon := d.Object(ui.ID(homeID))
+	if err := homeIcon.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("homeIcon doesn't exists: ", err)
+	}
+}
