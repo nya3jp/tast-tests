6a1329691f01736749b3fa87371f24b9cccc9f38
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-12-02 11:47:30 -0800

    Run UIAutomator Test Using Tast For ArtRage app
    BUG=b:171985415
    TEST=tast run <eve> arcappcompat.ArtRage
    
    Tast Test for ArtRage app verifies launch app
    from the playstore, check app reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: I044edc25c03c9494267b2925d468ae84a157a554

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/artrage.go
@@ -0,0 +1,115 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForArtrage = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForArtrage},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForArtrage = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForArtrage},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Artrage,
+		Desc:         "Functional test for Artrage that installs the app also verifies it is logged in and that the main page is open, checks Artrage correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForArtrage,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBootedForArtrage,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForArtrage,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletModeForArtrage,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForArtrage,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBootedForArtrage,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForArtrage,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletModeForArtrage,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.Artrage.username", "arcappcompat.Artrage.password"},
+	})
+}
+
+// Artrage test uses library for opting into the playstore and installing app.
+// Checks Artrage correctly changes the window states in both clamshell and touchview mode.
+func Artrage(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.ambientdesign.artrage.playstore"
+		appActivity = "com.ambientdesign.artrage.MainActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForArtrage verifies Artrage is logged in and
+// verify Artrage reached main activity page of the app.
+func launchAppForArtrage(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		allowButtonText = "ALLOW"
+		homeID          = "com.ambientdesign.artrage.playstore:id/ic_system"
+		retryButtonText = "Retry"
+	)
+
+	// Click on allow button.
+	allowButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+allowButtonText))
+	if err := allowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("allowButton doesn't exist: ", err)
+	} else if err := allowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on allowButton: ", err)
+	}
+
+	// Click on retry button to check for licence.
+	retryButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+retryButtonText))
+	if err := retryButton.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Log("retryButton doesn't exist: ", err)
+	}
+
+	// Click on retry button to check for licence until home page exist.
+	retryButton = d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+retryButtonText))
+	homePage := d.Object(ui.ID(homeID))
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := homePage.Exists(ctx); err != nil {
+			s.Log(" Click on retry button until home page exist")
+			retryButton.Click(ctx)
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Error("homePage doesn't exist: ", err)
+	}
+}
