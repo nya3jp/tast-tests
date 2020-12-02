0955dc533cb923817a0fec81f64470582ad4c77d
Shuhei Takahashi <nya@chromium.org>
2020-12-02 12:09:09 +0900

    meta: Add meta tests for gRPC output support.
    BUG=chromium:1153599
    TEST=tast run DUT meta.ServiceOutput
    
    Cq-Depend: chromium:2566544
    Change-Id: Ic60d1d228f6d88ca9d177ca0b7d37f0de61dca59

---
+++ src/chromiumos/tast/remote/bundles/cros/meta/service_output.go
@@ -0,0 +1,53 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package meta
+
+import (
+	"context"
+
+	"github.com/google/go-cmp/cmp"
+
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/meta"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testutil"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        ServiceOutput,
+		Desc:        "Ensure OutDir works for gRPC services",
+		Contacts:    []string{"nya@chromium.org", "tast-owners@google.com"},
+		Attr:        []string{"group:mainline", "informational"},
+		ServiceDeps: []string{"tast.cros.meta.OutputService"},
+	})
+}
+
+func ServiceOutput(ctx context.Context, s *testing.State) {
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	want := map[string]string{
+		"a.txt":     "foo",
+		"dir/b.txt": "bar",
+	}
+
+	oc := meta.NewOutputServiceClient(cl.Conn)
+	if _, err := oc.SaveOutputs(ctx, &meta.SaveOutputsRequest{Files: want}); err != nil {
+		s.Fatal("SaveOutputs RPC failed: ", err)
+	}
+
+	got, err := testutil.ReadFiles(s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to read OutDir: ", err)
+	}
+
+	if diff := cmp.Diff(got, want); diff != "" {
+		s.Error("OutDir content mismatch: ", diff)
+	}
+}
