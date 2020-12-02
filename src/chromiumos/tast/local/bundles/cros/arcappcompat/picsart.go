7daaec614b2798d76cbab0954fb735081c20fdc6
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-12-02 12:51:02 -0800

    Run UIAutomator Test Using Tast For Picsart app
    BUG=b:171983243
    TEST=tast run <eve> arcappcompat.Picsart
    
    Tast Test for Inkredible app verifies launch app
    from the playstore, check app reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: I05d33f2db04c937cb6e9a9ff251e917efdb67417

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/picsart.go
@@ -0,0 +1,89 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForPicsart = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForPicsart},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForPicsart = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForPicsart},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Picsart,
+		Desc:         "Functional test for Picsart that install, launch the app and check that the main page is open, also checks Picsart correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForPicsart,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForPicsart,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForPicsart,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForPicsart,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// Picsart test uses library for opting into the playstore and installing app.
+// Checks Picsart correctly changes the window states in both clamshell and touchview mode.
+func Picsart(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.picsart.studio"
+		appActivity = "com.socialin.android.photo.picsinphoto.MainPagerActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForPicsart verifies Picsart is launched and
+// verify Picsart reached main activity page of the app.
+func launchAppForPicsart(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const homeText = "Home"
+
+	// Check for homeIcon on homePage.
+	homeIcon := d.Object(ui.Text(homeText))
+	if err := homeIcon.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("homeIcon doesn't exists: ", err)
+	}
+}
