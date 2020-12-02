9b140129dc3a796f670a4a2f3d34d666a2a35631
Archana Singh <archanasing@google.com>
2020-12-02 12:47:52 -0800

    Run UIAutomator Test Using Tast For Asana app
    BUG=b:171985958
    TEST=tast run <eve> arcappcompat.Asana
    
    Tast Test for Asana app verifies launch app
    from the playstore, check app reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Cq-Depend: chrome-internal:3442614
    Change-Id: I14bf8c0d6bcc912f891d1d08d009db51bb6f6408

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/asana.go
@@ -0,0 +1,145 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForAsana = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForAsana},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForAsana = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForAsana},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Asana,
+		Desc:         "Functional test for Asana that installs the app also verifies it is logged in and that the main page is open, checks Asana correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForAsana,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForAsana,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForAsana,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForAsana,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password", "arcappcompat.Asana.username", "arcappcompat.Asana.password"},
+	})
+}
+
+// Asana test uses library for opting into the playstore and installing app.
+// Checks Asana correctly changes the window states in both clamshell and touchview mode.
+func Asana(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.asana.app"
+		appActivity = "com.asana.ui.activities.LaunchActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForAsana verifies Asana is logged in and
+// verify Asana reached main activity page of the app.
+func launchAppForAsana(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		logInText         = "Log In"
+		emailID           = "com.asana.app:id/email"
+		continueEmailText = "Continue with email"
+		typePasswordText  = "Type password"
+		passwordID        = "com.asana.app:id/password"
+		myTaskDescription = "My Tasks"
+	)
+
+	// Click on log in button
+	logInButton := d.Object(ui.Text(logInText))
+	if err := logInButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("LogIn button doesn't exist: ", err)
+	} else if err := logInButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on LogIn button: ", err)
+	}
+
+	// Enter email.
+	AsanaEmailID := s.RequiredVar("arcappcompat.Asana.username")
+	enterEmailAddress := d.Object(ui.ID(emailId))
+	if err := enterEmailAddress.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("enterEmailAddress doesn't exist: ", err)
+	} else if err := enterEmailAddress.SetText(ctx, AsanaEmailID); err != nil {
+		s.Fatal("Failed to enterEmailAddress: ", err)
+	}
+
+	// Click on continue button
+	continueButton := d.Object(ui.Text(continueEmailText))
+	if err := continueButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("continue button doesn't exist: ", err)
+	} else if err := continueButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on continue button: ", err)
+	}
+
+	// Click on type password button.
+	typePasswordButton := d.Object(ui.Text(typePasswordText))
+	if err := typePasswordButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("typePassword button doesn't exists: ", err)
+	} else if err := typePasswordButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on typePassword button: ", err)
+	}
+
+	// Enter password.
+	AsanaPassword := s.RequiredVar("arcappcompat.Asana.password")
+	enterPassword := d.Object(ui.ID(passwordId))
+	if err := enterPassword.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("enterPassword doesn't exists: ", err)
+	} else if err := enterPassword.SetText(ctx, AsanaPassword); err != nil {
+		s.Fatal("Failed to enter password: ", err)
+	}
+
+	// Click log in button.
+	if err := logInButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("LogIn button doesn't exist: ", err)
+	} else if err := logInButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on LogIn button: ", err)
+	}
+
+	// Check for my task button.
+	myTaskButton := d.Object(ui.Description(myTaskDescription))
+	if err := myTaskButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("My task button doesn't exist: ", err)
+	}
+}
