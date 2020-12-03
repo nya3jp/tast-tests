9e02de1c1e30a1185f9975825c68087ade3c4f4e
Jesse Schettler <jschettler@chromium.org>
2020-12-03 09:53:43 -0700

    tast-tests: Create scanapp.Scan test
    Create a new test to verify that the Scan app can be used to
    successfully perform a scan.
    
    BUG=b:173556456
    TEST=scanapp.Scan
    
    Change-Id: Iedea0a0da77f3cca01a876437f7e7565263ec4bd

--- src/chromiumos/tast/local/chrome/ui/scanapp/scanapp.go
+++ src/chromiumos/tast/local/chrome/ui/scanapp/scanapp.go
@@ -13,33 +13,203 @@ import (
 	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/testing"
 )
+
+const uiTimeout = 15 * time.Second
+
+var defaultStablePollOpts = testing.PollOptions{Interval: 100 * time.Millisecond, Timeout: 15 * time.Second}
 
 var scanAppRootNodeParams = ui.FindParams{
 	Name: apps.Scan.Name,
 	Role: ui.RoleTypeWindow,
 }
 
-var scanButton = ui.FindParams{
+var scanButtonParams = ui.FindParams{
 	Name: "Scan",
 	Role: ui.RoleTypeButton,
 }
 
-// RootNode returns the root UI node of the Scan SWA.
-func RootNode(ctx context.Context, tconn *chrome.TestConn) (*ui.Node, error) {
-	return ui.FindWithTimeout(ctx, tconn, scanAppRootNodeParams, 20*time.Second)
+var doneButtonParams = ui.FindParams{
+	Name: "Done",
+	Role: ui.RoleTypeButton,
 }
 
-// WaitForApp waits for the Scan SWA to be shown and rendered.
+// ScanApp represents an instance of the Scan app.
+type ScanApp struct {
+	tconn          *chrome.TestConn
+	Root           *ui.Node
+	stablePollOpts *testing.PollOptions
+}
+
+func (s *ScanApp) expandDropdown(ctx context.Context, id string) error {
+	params := ui.FindParams{ClassName: "md-select"}
+	dropdowns, err := s.Root.Descendants(ctx, params)
+	if err != nil {
+		return errors.Wrap(err, "failed to find dropdowns")
+	}
+	defer dropdowns.Release(ctx)
+
+	for _, dropdown := range dropdowns {
+		if dropdown.HTMLAttributes["id"] == id {
+			if err := dropdown.LeftClick(ctx); err != nil {
+				return err
+			}
+
+			return nil
+		}
+	}
+
+	return errors.New("failed to find dropdown")
+}
+
+// ToggleMoreSettings toggles the More settings drawer.
+func (s *ScanApp) ToggleMoreSettings(ctx context.Context) error {
+	params := ui.FindParams{
+		Name: "More settings",
+		Role: ui.RoleTypeButton,
+	}
+	button, err := s.Root.DescendantWithTimeout(ctx, params, uiTimeout)
+	if err != nil {
+		return errors.Wrap(err, "failed to find More settings button")
+	}
+	defer button.Release(ctx)
+
+	if err := button.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "failed to click More settings button")
+	}
+
+	return nil
+}
+
+func (s *ScanApp) waitForScanButton(ctx context.Context) error {
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		scanButton, err := s.Root.DescendantWithTimeout(ctx, scanButtonParams, uiTimeout)
+		if err != nil {
+			return errors.Wrap(err, "failed to find scan button")
+		}
+		defer scanButton.Release(ctx)
+
+		if scanButton.Restriction == ui.RestrictionDisabled {
+			return errors.New("scan button is disabled")
+		}
+
+		return nil
+	}, s.stablePollOpts); err != nil {
+		return errors.Wrap(err, "failed to wait for scan button")
+	}
+
+	return nil
+}
+
+// Launch launches the Scan app and returns it. An error is returned if the app
+// fails to launch.
+func Launch(ctx context.Context, tconn *chrome.TestConn) (*ScanApp, error) {
+	// Launch the Scan app.
+	if err := apps.Launch(ctx, tconn, apps.Scan.ID); err != nil {
+		return nil, err
+	}
+
+	// Get the Scan app root node.
+	root, err := ui.FindWithTimeout(ctx, tconn, scanAppRootNodeParams, time.Minute)
+	if err != nil {
+		return nil, err
+	}
+
+	app := ScanApp{tconn: tconn, Root: root, stablePollOpts: &defaultStablePollOpts}
+
+	// Wait until the scan button is enabled to verify the app is loaded.
+	if err := app.waitForScanButton(ctx); err != nil {
+		return nil, err
+	}
+
+	return &app, nil
+}
+
+// Release releases the root node held by the Scan app.
+func (s *ScanApp) Release(ctx context.Context) {
+	s.Root.Release(ctx)
+}
+
+// SelectOption selects the option defined by name in the dropdown defined by
+// id.
+func (s *ScanApp) SelectOption(ctx context.Context, id, name string) error {
+	if err := s.expandDropdown(ctx, id); err != nil {
+		return errors.Wrap(err, "failed to click dropdown")
+	}
+
+	params := ui.FindParams{
+		Name: name,
+		Role: ui.RoleTypeListBoxOption,
+	}
+	option, err := s.Root.DescendantWithTimeout(ctx, params, uiTimeout)
+	if err != nil {
+		return errors.Wrap(err, "failed to find option")
+	}
+	defer option.Release(ctx)
+
+	if err := option.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "failed to click option")
+	}
+
+	// Wait for the app to start processing the selected option.
+	if err := testing.Sleep(ctx, time.Second); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Scan performs a scan by clicking the scan button once it's enabled.
+func (s *ScanApp) Scan(ctx context.Context) error {
+	if err := s.waitForScanButton(ctx); err != nil {
+		return err
+	}
+
+	scanButton, err := s.Root.DescendantWithTimeout(ctx, scanButtonParams, uiTimeout)
+	if err != nil {
+		return errors.Wrap(err, "failed to find scan button")
+	}
+	defer scanButton.Release(ctx)
+
+	if err := scanButton.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "failed to click scan button")
+	}
+
+	// Wait until the done button is displayed to verify the scan completed
+	// successfully.
+	if err := s.Root.WaitUntilDescendantExists(ctx, doneButtonParams, 30*time.Second); err != nil {
+		return errors.New("scan failed to complete")
+	}
+
+	return nil
+}
+
+// ClickDone clicks the done button to return to the first page of the app.
+func (s *ScanApp) ClickDone(ctx context.Context) error {
+	doneButton, err := s.Root.DescendantWithTimeout(ctx, doneButtonParams, uiTimeout)
+	if err != nil {
+		return errors.Wrap(err, "failed to find done button")
+	}
+	defer doneButton.Release(ctx)
+
+	if err := doneButton.LeftClick(ctx); err != nil {
+		return errors.Wrap(err, "failed to click done button")
+	}
+
+	return nil
+}
+
+// WaitForApp waits for the Scan app to be shown and rendered.
 func WaitForApp(ctx context.Context, tconn *chrome.TestConn) error {
-	appRootNode, err := RootNode(ctx, tconn)
+	appRootNode, err := ui.FindWithTimeout(ctx, tconn, scanAppRootNodeParams, time.Minute)
 	if err != nil {
 		return errors.Wrap(err, "failed to find Scan app")
 	}
 	defer appRootNode.Release(ctx)
 
 	// Find the scan button to verify the app is rendering.
-	if _, err := appRootNode.DescendantWithTimeout(ctx, scanButton, 20*time.Second); err != nil {
+	if err := appRootNode.WaitUntilDescendantExists(ctx, scanButtonParams, uiTimeout); err != nil {
 		return errors.Wrap(err, "failed to render Scan app")
 	}
 
