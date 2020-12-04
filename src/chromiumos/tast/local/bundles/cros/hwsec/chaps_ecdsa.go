f5fa60e636a491c379d628eaebeb7892a37dc966
John L Chen <zuan@chromium.org>
2020-12-05 01:55:00 +0800

    hwsec: Add hwsec.ChapsECDSA test
    BUG=b:174823242
    TEST=tast run $dut hwsec.ChapsECDSA
    
    Change-Id: I47ae7e1b15ba462b52195d774676a8839b0fce53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2575477
    Tested-by: John L Chen <zuan@chromium.org>
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>
    Commit-Queue: John L Chen <zuan@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/chaps_ecdsa.go
@@ -0,0 +1,88 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/common/pkcs11"
+	"chromiumos/tast/common/pkcs11/pkcs11test"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/hwsec/util"
+	hwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ChapsECDSA,
+		Desc: "Verifies PKCS#1 v1.5 works with ECDSA keys (sign, verify) in chaps",
+		Contacts: []string{
+			"zuan@chromium.org",
+			"cros-hwsec@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func ChapsECDSA(ctx context.Context, s *testing.State) {
+	r, err := hwseclocal.NewCmdRunner()
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Cryptohome Utilty creation error: ", err)
+	}
+
+	pkcs11Util, err := pkcs11.NewChaps(ctx, r, utility)
+	if err != nil {
+		s.Fatal("Failed to create PKCS#11 Utility: ", err)
+	}
+
+	const scratchpadPath = "/tmp/ChapsECDSATest"
+
+	// Remove all keys/certs before the test as well.
+	if err := pkcs11test.CleanupScratchpad(ctx, r, scratchpadPath); err != nil {
+		s.Fatal("Failed to clean scratchpad before the start of test: ", err)
+	}
+	util.CleanupKeysBeforeTest(ctx, pkcs11Util, utility)
+
+	// Prepare the scratchpad.
+	f1, f2, err := pkcs11test.PrepareScratchpadAndTestFiles(ctx, r, scratchpadPath)
+	if err != nil {
+		s.Fatal("Failed to initialize the scratchpad space: ", err)
+	}
+	// Remove all keys/certs, if any at the end. i.e. Cleanup after ourselves.
+	defer pkcs11test.CleanupScratchpad(ctx, r, scratchpadPath)
+
+	// Create the various keys.
+	keys, err := util.CreateKeysForTesting(ctx, r, pkcs11Util, utility, scratchpadPath, util.ECKey)
+	if err != nil {
+		s.Fatal("Failed to create keys for testing: ", err)
+	}
+	defer func() {
+		if err := util.CleanupTestingKeys(ctx, keys, pkcs11Util, utility); err != nil {
+			s.Error("Failed to cleanup testing keys: ", err)
+		}
+	}()
+	// Give the cleanup 10 seconds to finish.
+	shortenedCtx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	// Test the various keys.
+	for _, k := range keys {
+		// Test the various mechanisms.
+		for _, m := range []pkcs11.MechanismInfo{pkcs11.ECDSASHA1} {
+			if err := pkcs11test.SignAndVerify(shortenedCtx, pkcs11Util, k, f1, f2, &m); err != nil {
+				s.Error("SignAndVerify failed: ", err)
+			}
+		}
+	}
+}
