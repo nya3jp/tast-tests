35ae294ccf2f01ba7e7fe31151ab15c051b68420
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2020-12-04 17:35:14 -0800

    Update Tast Test for Snapcat
    BUG=b:174885731
    TEST=tast run <eve> arcappcompat.Snapchat
    
    Tiktok is getting failed in launch due to the different UI window for login the app. Made changes in the code to fix it.
    
    Change-Id: Ief6d743b300f9244bec2ad7e42945dc1b245ee30

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/snapchat.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/snapchat.go
@@ -92,6 +92,7 @@ func launchAppForSnapchat(ctx context.Context, s *testing.State, tconn *chrome.T
 		passwordID                  = "com.snapchat.android:id/password_field"
 		profileID                   = "com.snapchat.android:id/neon_header_avatar_container"
 		turnonText                  = "TURN ON"
+		userNameOrEmailID           = "com.snapchat.android:id/login_username_hint"
 		homeID                      = "com.bydeluxe.d3.android.program.Snapchat:id/action_home"
 		whileUsingThisAppButtonText = "WHILE USING THE APP"
 	)
@@ -102,7 +103,95 @@ func launchAppForSnapchat(ctx context.Context, s *testing.State, tconn *chrome.T
 		s.Error("LoginButton doesn't exist: ", err)
 	}
 
+	// To handle to multiple UI window for login to app.
+	// Check for username or email text.
+	checkForUsernameOrEmail := d.Object(ui.ID(userNameOrEmailID))
+	if err := checkForUsernameOrEmail.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Log("checkForUsernameOrEmail doesn't exist: ", err)
+		loginToSnapchat(ctx, s, tconn, a, d, appPkgName, appActivity)
+	} else {
+		s.Log("checkForUsernameOrEmail does exist")
+		loginToSnapchatWithOtherUI(ctx, s, tconn, a, d, appPkgName, appActivity)
+	}
+
+	// Click on signIn Button until not now button exist.
+	signInButton := d.Object(ui.ID(signInID))
+	notNowButton := d.Object(ui.ID(notNowID))
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := notNowButton.Exists(ctx); err != nil {
+			signInButton.Click(ctx)
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Log("notNowButton doesn't exist: ", err)
+	} else if err := notNowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on notNowButton: ", err)
+	}
+
+	// Click on turnon button to save password.
+	turnonButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+turnonText))
+	if err := turnonButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("turnonButton doesn't exists: ", err)
+	} else if err := turnonButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on turnonButton: ", err)
+	}
+
+	// Click on allow button for accessing files.
+	allowButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+allowButtonText))
+	if err := allowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("allowButton Button doesn't exists: ", err)
+	} else if err := allowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on allowButton Button: ", err)
+	}
+
+	if err := allowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("allowButton Button doesn't exists: ", err)
+	} else if err := allowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on allowButton Button: ", err)
+	}
+
+	// Click on allow while using this app button to record audio.
+	clickOnWhileUsingThisAppButton := d.Object(ui.TextMatches("(?i)" + whileUsingThisAppButtonText))
+	if err := clickOnWhileUsingThisAppButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("clickOnWhileUsingThisApp Button doesn't exists: ", err)
+	} else if err := clickOnWhileUsingThisAppButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnWhileUsingThisApp Button: ", err)
+	}
+
+	// Click on allow button for accessing videos.
+	if err := allowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("allowButton Button doesn't exists: ", err)
+	} else if err := allowButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on allowButton Button: ", err)
+	}
+
+	// Click on allow while using this app button to record video.
+	clickOnWhileUsingThisAppButton = d.Object(ui.TextMatches("(?i)" + whileUsingThisAppButtonText))
+	if err := clickOnWhileUsingThisAppButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("clickOnWhileUsingThisApp Button doesn't exists: ", err)
+	} else if err := clickOnWhileUsingThisAppButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on clickOnWhileUsingThisApp Button: ", err)
+	}
+
+	// Check for profile icon.
+	profileIcon := d.Object(ui.ID(profileID))
+	if err := profileIcon.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("profileIcon doesn't exist: ", err)
+	}
+}
+
+// loginToSnapchat login to app.
+func loginToSnapchat(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		enterEmailAddressID  = "com.snapchat.android:id/username_or_email_field"
+		loginButtonClassName = "android.widget.TextView"
+		loginText            = "Log In"
+		passwordID           = "com.snapchat.android:id/password_field"
+	)
+
 	// click on login button until emailAddress exists.
+	loginButton := d.Object(ui.ClassName(loginButtonClassName), ui.TextMatches("(?i)"+loginText))
 	enterEmailAddress := d.Object(ui.ID(enterEmailAddressID))
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
 		if err := enterEmailAddress.Exists(ctx); err != nil {
@@ -143,7 +232,7 @@ func launchAppForSnapchat(ctx context.Context, s *testing.State, tconn *chrome.T
 
 	// Enter password.
 	enterPassword := d.Object(ui.ID(passwordID))
-	if err := enterPassword.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+	if err := enterPassword.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
 		s.Error("EnterPassword does not exist: ", err)
 	} else if err := enterPassword.Click(ctx); err != nil {
 		s.Fatal("Failed to click on enterPassword: ", err)
@@ -166,69 +255,83 @@ func launchAppForSnapchat(ctx context.Context, s *testing.State, tconn *chrome.T
 		s.Fatal("Failed to enter password: ", err)
 	}
 	s.Log("Entered password")
+}
 
-	// Click on signIn Button until not now button exist.
-	signInButton := d.Object(ui.ID(signInID))
-	notNowButton := d.Object(ui.ID(notNowID))
+// loginToSnapchatWithOtherUI login to app.
+func loginToSnapchatWithOtherUI(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		editTextClassName    = "com.snapchat.android:id/input_field_edit_text"
+		loginButtonClassName = "android.widget.TextView"
+		loginText            = "Log In"
+	)
+
+	// click on login button until emailAddress exists.
+	loginButton := d.Object(ui.ClassName(loginButtonClassName), ui.TextMatches("(?i)"+loginText))
+	enterEmailAddress := d.Object(ui.ClassName(editTextClassName))
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		if err := notNowButton.Exists(ctx); err != nil {
-			signInButton.Click(ctx)
+		if err := enterEmailAddress.Exists(ctx); err != nil {
+			loginButton.Click(ctx)
 			return err
 		}
 		return nil
 	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
-		s.Log("notNowButton doesn't exist: ", err)
-	} else if err := notNowButton.Click(ctx); err != nil {
-		s.Fatal("Failed to click on notNowButton: ", err)
+		s.Error("enterEmailAddress button doesn't exists: ", err)
+	} else if err := enterEmailAddress.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterEmailAddress: ", err)
 	}
 
-	// Click on turnon button to save password.
-	turnonButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+turnonText))
-	if err := turnonButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
-		s.Log("turnonButton doesn't exists: ", err)
-	} else if err := turnonButton.Click(ctx); err != nil {
-		s.Fatal("Failed to click on turnonButton: ", err)
+	// Click on emailid text field until the emailid text field is focused.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if emailIDFocused, err := enterEmailAddress.IsFocused(ctx); err != nil {
+			return errors.New("email text field not focused yet")
+		} else if !emailIDFocused {
+			enterEmailAddress.Click(ctx)
+			return errors.New("email text field not focused yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Fatal("Failed to focus EmailId: ", err)
 	}
 
-	// Click on allow button for accessing files.
-	allowButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+allowButtonText))
-	if err = allowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
-		s.Log("allowButton Button doesn't exists: ", err)
-	} else if err := allowButton.Click(ctx); err != nil {
-		s.Fatal("Failed to click on allowButton Button: ", err)
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	username := s.RequiredVar("arcappcompat.Snapchat.username")
+	if err := kb.Type(ctx, username); err != nil {
+		s.Fatal("Failed to enter username: ", err)
+	}
+	s.Log("Entered username")
+
+	// Enter password.
+	enterPassword := d.Object(ui.ClassName(editTextClassName))
+	if err := enterPassword.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		s.Error("EnterPassword does not exist: ", err)
+	} else if err := enterPassword.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterPassword: ", err)
+	}
+	// Click on password text field until the password text field is focused.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if pwdFocused, err := enterPassword.IsFocused(ctx); err != nil {
+			return errors.New("password text field not focused yet")
+		} else if !pwdFocused {
+			enterPassword.Click(ctx)
+			return errors.New("password text field not focused yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Fatal("Failed to focus password: ", err)
 	}
 
-	if err = allowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
-		s.Log("allowButton Button doesn't exists: ", err)
-	} else if err := allowButton.Click(ctx); err != nil {
-		s.Fatal("Failed to click on allowButton Button: ", err)
+	// Press tab to click on enter password.
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_TAB, 0); err != nil {
+		s.Log("Failed to enter KEYCODE_TAB: ", err)
 	}
-
-	// Click on allow while using this app button to record audio.
-	clickOnWhileUsingThisAppButton := d.Object(ui.TextMatches("(?i)" + whileUsingThisAppButtonText))
-	if err = clickOnWhileUsingThisAppButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
-		s.Log("clickOnWhileUsingThisApp Button doesn't exists: ", err)
-	} else if err := clickOnWhileUsingThisAppButton.Click(ctx); err != nil {
-		s.Fatal("Failed to click on clickOnWhileUsingThisApp Button: ", err)
+	password := s.RequiredVar("arcappcompat.Snapchat.password")
+	if err := kb.Type(ctx, password); err != nil {
+		s.Fatal("Failed to enter password: ", err)
 	}
-
-	if err = allowButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
-		s.Log("allowButton Button doesn't exists: ", err)
-	} else if err := allowButton.Click(ctx); err != nil {
-		s.Fatal("Failed to click on allowButton Button: ", err)
-	}
-
-	// Click on allow while using this app button to record video.
-	clickOnWhileUsingThisAppButton = d.Object(ui.TextMatches("(?i)" + whileUsingThisAppButtonText))
-	if err = clickOnWhileUsingThisAppButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
-		s.Log("clickOnWhileUsingThisApp Button doesn't exists: ", err)
-	} else if err := clickOnWhileUsingThisAppButton.Click(ctx); err != nil {
-		s.Fatal("Failed to click on clickOnWhileUsingThisApp Button: ", err)
-	}
-
-	// Check for profile icon.
-	profileIcon := d.Object(ui.ID(profileID))
-	if err := profileIcon.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
-		s.Error("profileIcon doesn't exist: ", err)
-	}
+	s.Log("Entered password")
 }
