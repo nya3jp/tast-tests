b7928358e114c67dc2d501977dd6ad91e49de190
Charles William Dick <cwd@google.com>
2020-12-08 11:38:27 +0900

    Add p90 metric to arc.Memory tests
    Crosbolt uses arithmetic mean to average multiple perf values. P90
    ignores outliers and should be more stable over time.
    
    BUG=None
    TEST=tast run dut arc.MemoryChromeOSPerf.vm arc.MemoryAndroidPerf.vm
    
    Change-Id: Ic03b4c48bb741788e86dc2f09483a400f1ccd44c

--- src/chromiumos/tast/local/bundles/cros/arc/memory_android_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/memory_android_perf.go
@@ -6,6 +6,7 @@ package arc
 
 import (
 	"context"
+	"sort"
 	"time"
 
 	"chromiumos/tast/common/perf"
@@ -40,6 +41,7 @@ func init() {
 
 func MemoryAndroidPerf(ctx context.Context, s *testing.State) {
 	allocatedMetric := perf.Metric{Name: "allocated", Unit: "MiB", Direction: perf.BiggerIsBetter, Multiple: true}
+	allocatedP90Metric := perf.Metric{Name: "allocated_p90", Unit: "MiB", Direction: perf.BiggerIsBetter}
 	marginMetric := perf.Metric{Name: "critical_margin", Unit: "MiB"}
 
 	a := arcMemory.NewAndroidAllocator(s.PreValue().(arc.PreData).ARC)
@@ -63,10 +65,17 @@ func MemoryAndroidPerf(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to allocate to critical margin: ", err)
 	}
-	const bytesInMiB = 1024 * 1024
-	for _, x := range allocated {
-		p.Append(allocatedMetric, float64(x)/bytesInMiB)
+	var allocatedFloat []float64
+	for _, a := range allocated {
+		const bytesInMiB = 1024 * 1024
+		aMiB := float64(a) / bytesInMiB
+		allocatedFloat = append(allocatedFloat, aMiB)
+		p.Append(allocatedMetric, aMiB)
 	}
+	sort.Float64s(allocatedFloat)
+	p90Index := int(float64(len(allocatedFloat))*0.9) - 1
+	p.Set(allocatedP90Metric, allocatedFloat[p90Index])
+
 	if err := a.FreeAll(ctx); err != nil {
 		s.Fatal("Failed to free memory: ", err)
 	}
