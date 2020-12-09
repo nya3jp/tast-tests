1f7e7674f06ff066700630192d2b513d90ebf55a
Yi Xie <yixie@chromium.org>
2020-12-09 03:50:00 +0000

    Add ARC install logging test
    enterprise.ARCInstallLogging test makes sure that when a package is
    force installed by policy, event logs are uploaded to the server.
    
    BUG=b:174803045
    TEST=tast run <IP> enterprise.ARCInstallLogging
    
    Change-Id: If3f15d96dc262edd5f28b2bdb4dc8c6c05734acc

---
+++ src/chromiumos/tast/local/bundles/cros/enterprise/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package enterprise contains local Tast tests which test enterprise-oriented features.
+package enterprise
