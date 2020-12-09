99bf34b023b8601cc4fcdfca19f5798c8019b35b
Shik Chen <shik@chromium.org>
2020-12-09 16:29:11 +0800

    camera.CCAQRCode: Promote it to CQ
    The test is stable for 20+ versions, and failures are Chrome
    precondition failures:
    https://tastboard.corp.google.com/test/camera.CCAQRCode
    
    This CL also adds the missing SoftwareDeps "chrome_internal" since
    BarcodeDetector API is only available on internal chrome build.
    
    BUG=b:172879638
    TEST=tast run <dut> camera.CCAQRCode
    
    Change-Id: Ie42cfbe2a23150d776f3ca24a11253a6996a732a

--- src/chromiumos/tast/local/bundles/cros/camera/cca_qrcode.go
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_qrcode.go
@@ -19,8 +19,8 @@ func init() {
 		Func:         CCAQRCode,
 		Desc:         "Verifies the BarcodeDetector API used in CCA",
 		Contacts:     []string{"shik@chromium.org", "chromeos-camera-eng@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
 		Data:         []string{"cca_qrcode.html", "cca_qrcode.js", "qrcode_3024x3024.jpg"},
 		Pre:          chrome.LoggedIn(),
 	})
