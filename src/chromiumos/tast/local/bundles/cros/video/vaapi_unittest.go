28aee1ca37298e8062c84469ed2ec77f8e6808fa
Steve Cho <stevecho@google.com>
2020-12-09 17:07:01 -0800

    tast-tests/video: add test_va_api tests
    This CL adds test_va_api tests in libva-utils to tast-tests.
    tast reported PASS and log file showed all 17412 tests passed.
    
    BUG=b:172495800
    TEST=video.VAAPIUnittest on drawlat (dedede)
    
    Change-Id: I377fe12da24c1a65f9d62789c2d1213b0f3dad5c

---
+++ src/chromiumos/tast/local/bundles/cros/video/vaapi_unittest.go
@@ -0,0 +1,48 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+	"path/filepath"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/gtest"
+	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: VAAPIUnittest,
+		Desc: "Runs test_va_api, a shallow libva API test",
+		Contacts: []string{
+			"stevecho@chromium.org",
+			"chromeos-gfx-video@google.com",
+		},
+		Attr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+		SoftwareDeps: []string{"vaapi"},
+	})
+}
+
+// VAAPIUnittest runs the "test_va_api" GTtest binary from the libva-test
+// package, see https://github.com/intel/libva-utils.
+func VAAPIUnittest(ctx context.Context, s *testing.State) {
+	const exec = "test_va_api"
+	if report, err := gtest.New(
+		filepath.Join(chrome.BinTestDir, exec),
+		gtest.Logfile(filepath.Join(s.OutDir(), exec+".log")),
+		gtest.UID(int(sysutil.ChronosUID)),
+	).Run(ctx); err != nil {
+		s.Errorf("%v failed: %v", exec, err)
+		if report != nil {
+			for _, name := range report.FailedTestNames() {
+				s.Error(name, " failed")
+			}
+		} else {
+			s.Error("No additional information is available for this failure")
+		}
+	}
+}
