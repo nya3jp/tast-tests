2ae1512a2b1eba22abda7a2ba5a1fa99d384de21
Ricardo Quesada <ricardoq@google.com>
2020-12-14 10:46:32 -0800

    ArcGamePerformanceTest: add GL Min requirement test
    This CL adds a "OpenGL ES minimum requirement" test.
    This test it just returns via Instruments the GL Version,
    GL Vendor, GL Extensions and whether GL Extension Pack is supported.
    
    The caller (Tast test) will decided whether or not the test passed.
    
    BUG=b:175603972
    TEST=emerge-$BOARD tast-local-apks-cros
         adb install ArcGamePerformanceTest.apk
         adb shell am instrument -w -e class \
           org.chromium.arc.testapp.gameperformance.GLESMinReqTest \
           org.chromium.arc.testapp.gameperformance
    
    Change-Id: I2075ad1a091e635870c77324419c8144daa717a1

---
+++ android/ArcGamePerformanceTest/src/org/chromium/arc/testapp/gameperformance/GLESMinReqTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.gameperformance;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.chromium.arc.testapp.gameperformance.CustomOpenGLView.FrameDrawer;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+import javax.microedition.khronos.opengles.GL10;
+
+// Simple class that gathers some OpenGL ES related data, and returns it to the caller.
+public class GLESMinReqTest extends ActivityInstrumentationTestCase2<GamePerformanceActivity> {
+    // Shared variables between this class, and the inner class that runs in the GL thread.
+    String mGLExtensions = null;
+    String mGLVersion = null;
+    String mGLVendor = null;
+
+    public GLESMinReqTest() {
+        super(GamePerformanceActivity.class);
+    }
+
+    public void testPerformanceMetrics() throws IOException, InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final GamePerformanceActivity activity = getActivity();
+
+        // Fetch GL variables from the GL thread
+        activity.attachOpenGLView();
+        activity.getOpenGLView().waitRenderReady();
+        activity.getOpenGLView().setFrameDrawer(new FrameDrawer() {
+            @Override
+            public void drawFrame(GL10 gl) {
+                // Runs in UI thread
+                mGLExtensions = gl.glGetString(GL10.GL_EXTENSIONS);
+                mGLVersion = gl.glGetString(GL10.GL_VERSION);
+                mGLVendor = gl.glGetString(GL10.GL_VENDOR);
+
+                // Remove FrameDrawer, since this is a one-time only thing
+                activity.getOpenGLView().setFrameDrawer(null);
+
+                latch.countDown();
+            }
+        });
+
+        latch.await();
+
+        boolean deviceSupportsAEP = activity.getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_OPENGLES_EXTENSION_PACK);
+
+        final Bundle status = new Bundle();
+        status.putString("gl_version", mGLVersion);
+        status.putString("gl_vendor", mGLVendor);
+        status.putString("gl_extensions", mGLExtensions);
+        status.putBoolean("supports_AEP", deviceSupportsAEP);
+        getInstrumentation().sendStatus(Activity.RESULT_OK, status);
+    }
+}
