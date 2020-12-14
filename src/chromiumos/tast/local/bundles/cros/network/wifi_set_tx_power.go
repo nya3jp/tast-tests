4d88025f387df15a49c86589b1113a5fef7e39ec
YH Lin <yueherngl@google.com>
2020-12-14 14:44:00 -0800

    wifi_set_tx_power: not skipping bard/ekko/sona/syndra
    Remove bard/ekko/sona/syndra from the list to be skipped by the test.
    
    BUG=b:171748383,b:171737478
    TEST=Run test locally.
    
    Change-Id: I3e06e0f5b9a28b93fb9c9483beb248429b3f56fc

--- src/chromiumos/tast/local/bundles/cros/network/wifi_set_tx_power.go
+++ src/chromiumos/tast/local/bundles/cros/network/wifi_set_tx_power.go
@@ -25,18 +25,18 @@ func init() {
 		Params: []testing.Param{
 			{
 				ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(
-					"ezkinil",                                                                             // TODO(b/162258095): Ezkinil lab DUTs have Qualcomm chip
-					"dalboz",                                                                              // TODO(b/162258095): Dalboz lab DUTs have Qualcomm chip
-					"bard", "ekko", "sona", "syndra", "electro", "ampton", "dood", "garg360", "phaser360", // TODO(b/171737478): Re-enable once models have been audited/fixed.
+					"ezkinil",                                           // TODO(b/162258095): Ezkinil lab DUTs have Qualcomm chip
+					"dalboz",                                            // TODO(b/162258095): Dalboz lab DUTs have Qualcomm chip
+					"electro", "ampton", "dood", "garg360", "phaser360", // TODO(b/171737478): Re-enable once models have been audited/fixed.
 				)),
 			},
 			{
 				Name:      "informational",
 				ExtraAttr: []string{"informational"},
 				ExtraHardwareDeps: hwdep.D(hwdep.Model(
-					"ezkinil",                                                                             // TODO(b/162258095): Ezkinil lab DUTs have Qualcomm chip
-					"dalboz",                                                                              // TODO(b/162258095): Dalboz lab DUTs have Qualcomm chip
-					"bard", "ekko", "sona", "syndra", "electro", "ampton", "dood", "garg360", "phaser360", // TODO(b/171737478): Re-enable once models have been audited/fixed.
+					"ezkinil",                                           // TODO(b/162258095): Ezkinil lab DUTs have Qualcomm chip
+					"dalboz",                                            // TODO(b/162258095): Dalboz lab DUTs have Qualcomm chip
+					"electro", "ampton", "dood", "garg360", "phaser360", // TODO(b/171737478): Re-enable once models have been audited/fixed.
 				)),
 			},
 		},
