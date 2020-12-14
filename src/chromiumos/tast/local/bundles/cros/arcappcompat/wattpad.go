a90430568b304314bfa038c205a154622321a15b
Archana Singh <archanasing@google.com>
2020-12-14 12:51:24 -0800

    Run UIAutomator Test Using Tast For Wattpad app
    BUG=b:175393513
    TEST=tast run <eve> arcappcompat.Wattpad
    
    Tast Test for Wattpad app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,Reopen tests.
    
    Change-Id: Icbf710fda68d5bf367f9c60ac979252c94280651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2590667
    Tested-by: Archana Singh <archanasing@chromium.org>
    Commit-Queue: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/wattpad.go
@@ -0,0 +1,109 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForWattpad = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForWattpad},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForWattpad = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForWattpad},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Wattpad,
+		Desc:         "Functional test for Wattpad that installs the app also verifies it is logged in and that the main page is open, checks Wattpad correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForWattpad,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForWattpad,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForWattpad,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForWattpad,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// Wattpad test uses library for opting into the playstore and installing app.
+// Checks Wattpad correctly changes the window states in both clamshell and touchview mode.
+func Wattpad(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "wp.wattpad"
+		appActivity = ".ui.activities.WelcomeActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForWattpad verifies Wattpad is logged in and
+// verify Wattpad reached main activity page of the app.
+func launchAppForWattpad(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		signInID        = "wp.wattpad:id/log_in_button"
+		googleID        = "wp.wattpad:id/authentication_view_google_button"
+		homeDescription = "Home"
+	)
+
+	// Click on sign in button.
+	signInButton := d.Object(ui.ID(signInID))
+	if err := signInButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("signIn button doesn't exist: ", err)
+	} else if err := signInButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on signIn button: ", err)
+	}
+
+	// Click on google button.
+	googleButton := d.Object(ui.ID(googleID))
+	if err := googleButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("google button doesn't exist: ", err)
+	} else if err := googleButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on google button: ", err)
+	}
+
+	// Check for home icon.
+	homeDescriptionButton := d.Object(ui.Description(homeDescription))
+	if err := homeDescriptionButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("Home icon doesn't exist: ", err)
+	}
+}
