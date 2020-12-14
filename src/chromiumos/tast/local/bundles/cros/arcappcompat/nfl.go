b5b7d1847da3b9488d8455bbe3c5450de1785d0a
Archana Singh <archanasing@google.com>
2020-12-14 15:14:50 -0800

    Run UIAutomator Test Using Tast For NFL app
    BUG=b:175394382
    TEST=tast run <eve> arcappcompat.NFL
    
    Tast Test for NFL app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,Reopen tests.
    
    Change-Id: Ib72313ef8973ca6f65611962c484729779254c99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2590501
    Tested-by: Archana Singh <archanasing@chromium.org>
    Commit-Queue: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/nfl.go
@@ -0,0 +1,115 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForNFL = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForNFL},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForNFL = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForNFL},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NFL,
+		Desc:         "Functional test for NFL that installs the app also verifies it is logged in and that the main page is open, checks NFL correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForNFL,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForNFL,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForNFL,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForNFL,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// NFL test uses library for opting into the playstore and installing app.
+// Checks NFL correctly changes the window states in both clamshell and touchview mode.
+func NFL(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.gotv.nflgamecenter.us.lite"
+		appActivity = "com.nfl.mobile.SplashActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForNFL verifies NFL is logged in and
+// verify NFL reached main activity page of the app.
+func launchAppForNFL(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		skipText = "Skip"
+		googleID = "wp.NFL:id/authentication_view_google_button"
+		homeText = "Home"
+	)
+
+	// Click on skip button.
+	skipButton := d.Object(ui.Text(skipText))
+	if err := skipButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("skip button doesn't exist: ", err)
+	} else if err := skipButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on skip button: ", err)
+	}
+
+	// Click on skip button to skip location access.
+	if err := skipButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("skip button doesn't exist: ", err)
+	} else if err := skipButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on skip button: ", err)
+	}
+
+	// Click on skip button to skip favorite team.
+	if err := skipButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("skip button doesn't exist: ", err)
+	} else if err := skipButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on skip button: ", err)
+	}
+
+	// Check for home icon.
+	homeButton := d.Object(ui.Text(homeText))
+	if err := homeButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("Home icon doesn't exist: ", err)
+	}
+}
