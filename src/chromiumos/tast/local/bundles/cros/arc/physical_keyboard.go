72359172a6cf0c7ab78b66e5ff60ffb8ffe4dd50
Yuichiro Hanada <yhanada@google.com>
2020-12-14 15:04:26 +0900

    tast-tests: arc: Parameterize test functions of physical_keyboard.go.
    This is a clean up CL and preparation for introducing unstable tests to
    physical_keyboard.go in future.
    This CL should not change any testing behavior.
    
    BUG=b:174259561
    TEST=tast run $DUT arc.PhysicalKeyboard
    
    Change-Id: Ib2d26fcc80c73fa29ef5a70159a73dfa604fabbe

--- src/chromiumos/tast/local/bundles/cros/arc/physical_keyboard.go
+++ src/chromiumos/tast/local/bundles/cros/arc/physical_keyboard.go
@@ -11,9 +11,29 @@ import (
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/android/ui"
 	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
+
+// pkTestState is a collection of objects needs to run the physical keyboard tests.
+type pkTestState struct {
+	tconn *chrome.TestConn
+	a     *arc.ARC
+	d     *ui.Device
+	kb    *input.KeyboardEventWriter
+}
+
+// pkTestParams represents the name of the test and the function to call.
+type pkTestParams struct {
+	name string
+	fn   func(context.Context, pkTestState, *testing.State)
+}
+
+var stablePkTests = []pkTestParams{
+	{"Basic editing", physicalKeyboardBasicEditingTest},
+	{"Editing on TYPE_NULL", physicalKeyboardOnTypeNullTextFieldTest},
+}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -24,12 +44,76 @@ func init() {
 		Pre:          arc.Booted(),
 		Attr:         []string{"group:mainline"},
 		Params: []testing.Param{{
+			Val:               stablePkTests,
 			ExtraSoftwareDeps: []string{"android_p"},
 		}, {
 			Name:              "vm",
+			Val:               stablePkTests,
 			ExtraSoftwareDeps: []string{"android_vm"},
 		}},
 	})
+}
+
+func testTextField(ctx context.Context, st pkTestState, s *testing.State, activity, keystrokes, expectedResult string) error {
+	const (
+		pkg      = "org.chromium.arc.testapp.keyboard"
+		fieldID  = pkg + ":id/text"
+		initText = "hello"
+	)
+
+	a := st.a
+	tconn := st.tconn
+	d := st.d
+	kb := st.kb
+
+	act, err := arc.NewActivity(a, pkg, activity)
+	if err != nil {
+		return errors.Wrapf(err, "failed to create a new activity %q", activity)
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx, tconn); err != nil {
+		return errors.Wrapf(err, "failed to start the activity %q", activity)
+	}
+	defer act.Stop(ctx, tconn)
+
+	if err := d.Object(ui.ID(fieldID), ui.Text(initText)).WaitForExists(ctx, 30*time.Second); err != nil {
+		return errors.Wrap(err, "failed to find field")
+	}
+
+	field := d.Object(ui.ID(fieldID))
+	if err := field.Click(ctx); err != nil {
+		return errors.Wrap(err, "failed to click field")
+	}
+	if err := field.SetText(ctx, ""); err != nil {
+		return errors.Wrap(err, "failed to empty field")
+	}
+
+	if err := d.Object(ui.ID(fieldID), ui.Focused(true)).WaitForExists(ctx, 30*time.Second); err != nil {
+		return errors.Wrap(err, "failed to focus on field")
+	}
+
+	if err := kb.Type(ctx, keystrokes); err != nil {
+		return errors.Wrapf(err, "failed to type %q", keystrokes)
+	}
+
+	if err := d.Object(ui.ID(fieldID)).WaitForText(ctx, expectedResult, 30*time.Second); err != nil {
+		return errors.Wrap(err, "failed to wait for text")
+	}
+
+	return nil
+}
+
+func physicalKeyboardBasicEditingTest(ctx context.Context, st pkTestState, s *testing.State) {
+	if err := testTextField(ctx, st, s, ".MainActivity", "google", "google"); err != nil {
+		s.Error("Failed to type in normal text field: ", err)
+	}
+}
+
+func physicalKeyboardOnTypeNullTextFieldTest(ctx context.Context, st pkTestState, s *testing.State) {
+	if err := testTextField(ctx, st, s, ".NullEditTextActivity", "abcdef\b\b\bghi", "abcghi"); err != nil {
+		s.Error("Failed to type in TYPE_NULL text field: ", err)
+	}
 }
 
 func PhysicalKeyboard(ctx context.Context, s *testing.State) {
@@ -63,55 +147,10 @@ func PhysicalKeyboard(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed installing app: ", err)
 	}
 
-	testTextField := func(activityName, keystrokes, expectedResult string) error {
-		act, err := arc.NewActivity(a, pkg, activityName)
-		if err != nil {
-			return errors.Wrapf(err, "failed to create a new activity %q", activityName)
-		}
-		defer act.Close()
-
-		if err := act.Start(ctx, tconn); err != nil {
-			return errors.Wrapf(err, "failed to start the activity %q", activityName)
-		}
-		defer act.Stop(ctx, tconn)
-
-		const (
-			fieldID  = "org.chromium.arc.testapp.keyboard:id/text"
-			initText = "hello"
-		)
-
-		if err := d.Object(ui.ID(fieldID), ui.Text(initText)).WaitForExists(ctx, 30*time.Second); err != nil {
-			return errors.Wrap(err, "failed to find field")
-		}
-
-		field := d.Object(ui.ID(fieldID))
-		if err := field.Click(ctx); err != nil {
-			return errors.Wrap(err, "failed to click field")
-		}
-		if err := field.SetText(ctx, ""); err != nil {
-			return errors.Wrap(err, "failed to empty field")
-		}
-
-		if err := d.Object(ui.ID(fieldID), ui.Focused(true)).WaitForExists(ctx, 30*time.Second); err != nil {
-			return errors.Wrap(err, "failed to focus on field")
-		}
-
-		if err := kb.Type(ctx, keystrokes); err != nil {
-			return errors.Wrapf(err, "failed to type %q", keystrokes)
-		}
-
-		if err := d.Object(ui.ID(fieldID)).WaitForText(ctx, expectedResult, 30*time.Second); err != nil {
-			return errors.Wrap(err, "failed to wait for text")
-		}
-
-		return nil
-	}
-
-	if err := testTextField(".MainActivity", "google", "google"); err != nil {
-		s.Error("Failed to type in normal text field: ", err)
-	}
-
-	if err := testTextField(".NullEditTextActivity", "abcdef\b\b\bghi", "abcghi"); err != nil {
-		s.Error("Failed to type in TYPE_NULL text field: ", err)
+	testState := pkTestState{tconn, a, d, kb}
+	for _, test := range s.Param().([]pkTestParams) {
+		s.Run(ctx, test.name, func(ctx context.Context, s *testing.State) {
+			test.fn(ctx, testState, s)
+		})
 	}
 }
