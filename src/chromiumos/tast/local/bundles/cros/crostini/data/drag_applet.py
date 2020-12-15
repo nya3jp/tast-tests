2d834a0a43b83906d2ee6c1c52e59e606310865c
Joel Hockey <joelhockey@chromium.org>
2020-12-14 18:12:19 -0800

    tast: crostini: drag from crostini and drop in FilesApp
    BUG=chromium:1144138
    TEST=crostini.DragDrop
    
    Change-Id: Ia2798b1d440bc2561e5ed8afe466a3b5f76e0cb1

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/data/drag_applet.py
@@ -0,0 +1,30 @@
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This applications brings up a window which will receive a file drop.
+# It prints the names of the files and closes.
+
+import gi
+import os
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk
+
+class DragWindow(Gtk.Window):
+  def __init__(self):
+    super().__init__(title="gtk3_drag_demo")
+    self.connect("drag-data-get", self.on_drag_data_get)
+    self.connect("drag-end", self.on_drag_end)
+    self.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [], Gdk.DragAction.COPY)
+    self.drag_source_add_uri_targets()
+
+  def on_drag_data_get(self, widget, drag_context, data, info, time):
+    data.set_uris(["file://" + os.path.abspath("crostini.txt")])
+
+  def on_drag_end(self, drag_context, data):
+    self.close()
+
+window = DragWindow()
+window.show_all()
+window.connect('delete-event', Gtk.main_quit)
+Gtk.main()
