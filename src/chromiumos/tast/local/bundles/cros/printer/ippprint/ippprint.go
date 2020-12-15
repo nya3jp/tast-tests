6787c4f5b80301453211f7cb1a0d7aab5b3af06d
Pranav Batra <batrapranav@chromium.org>
2020-12-15 03:19:44 +0000

    Refactor printer Tast tests
    Also add printer.Add.hp_pwg_raster_monochrome test.
    
    BUG=b:172622159
    TEST=tast run localhost:9222 printer.Add*
    
    Cq-Depend: chromium:2591914, chrome-internal:3467627
    Change-Id: I0701f7a2d3cc0e79b922462bbc4133d597637cfe

--- src/chromiumos/tast/local/bundles/cros/printer/ippprint/ippprint.go
+++ src/chromiumos/tast/local/bundles/cros/printer/ippprint/ippprint.go
@@ -15,52 +15,45 @@ import (
 	"chromiumos/tast/testing"
 )
 
-// Option is for supplying filter options.
-type Option string
-
 // Params struct used by all ipp print tests for parameterized tests.
 type Params struct {
 	PpdFile      string   // Name of the ppd used to print the job.
 	PrintFile    string   // PS file to print.
 	ExpectedFile string   // PS file output should be compared to.
-	Options      []Option // Options to be passed to the filter to change output.
+	Options      []string // Options to be passed to the filter to change output.
+	UseProxy     bool     // If enabled, print via the CUPS proxy.
 }
 
 // Collate enables collation.
-func Collate() Option {
-	return Option("multiple-document-handling=separate-documents-collated-copies")
+func Collate() string {
+	return "multiple-document-handling=separate-documents-collated-copies"
 }
 
 // WithCopies properly formats a copies option.
-func WithCopies(n int) Option {
-	return Option(fmt.Sprintf("copies=%d", n))
+func WithCopies(n int) string {
+	return fmt.Sprintf("copies=%d", n)
 }
 
 // WithJobPassword properly formats a job-password option.
-func WithJobPassword(pass string) Option {
-	return Option(fmt.Sprintf("job-password=%s", pass))
+func WithJobPassword(pass string) string {
+	return fmt.Sprintf("job-password=%s", pass)
 }
 
 // WithResolution properly formats a printer-resolution option.
-func WithResolution(res string) Option {
-	return Option(fmt.Sprintf("printer-resolution=%s", res))
+func WithResolution(res string) string {
+	return (fmt.Sprintf("printer-resolution=%s", res))
 }
 
 // optionsToString turns an array of options into a space-delimited string.
-func optionsToString(options []Option) string {
-	var arr []string
-	for _, o := range options {
-		arr = append(arr, string(o))
-	}
-	return strings.Join(arr, " ")
+func optionsToString(options []string) string {
+	return strings.Join(options, " ")
 }
 
 // Run executes the main test logic with |p.Options| included in the lp command.
 func Run(ctx context.Context, s *testing.State, p *Params) {
-	lpprint.RunWithOptions(ctx, s, p.PpdFile, p.PrintFile, p.ExpectedFile, optionsToString(p.Options))
-}
-
-// ProxyRun is similar to Run but uses proxylppprint instead of lpprint.
-func ProxyRun(ctx context.Context, s *testing.State, p *Params) {
-	proxylpprint.RunWithOptions(ctx, s, p.PpdFile, p.PrintFile, p.ExpectedFile, optionsToString(p.Options))
+	run := lpprint.RunWithOptions
+	if p.UseProxy {
+		run = proxylpprint.RunWithOptions
+	}
+	run(ctx, s, p.PpdFile, p.PrintFile, p.ExpectedFile, optionsToString(p.Options))
 }
