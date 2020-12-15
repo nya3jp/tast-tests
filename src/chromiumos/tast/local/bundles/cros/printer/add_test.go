9dc0301f2c36daf36615ba5ac9e052e2d31e18a7
Pranav Batra <batrapranav@chromium.org>
2020-12-15 03:19:44 +0000

    Refactor printer Tast tests
    Also add printer.Add.hp_pwg_raster_monochrome test.
    
    BUG=b:172622159
    TEST=tast run localhost:9222 printer.Add* printer.ProxyAdd*
    TEST=TAST_GENERATE_UPDATE=1 ~/trunk/src/platform/tast/tools/go.sh test add_test.go
    
    Cq-Depend: chromium:2591914, chrome-internal:3467627
    Change-Id: I0701f7a2d3cc0e79b922462bbc4133d597637cfe

---
+++ src/chromiumos/tast/local/bundles/cros/printer/add_test.go
@@ -0,0 +1,104 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package printer
+
+import (
+	"testing"
+
+	"chromiumos/tast/common/genparams"
+	"chromiumos/tast/local/bundles/cros/printer/ippprint"
+)
+
+// base adds two parameterized tests, one prefixed with "proxy_" that uses
+// the CUPS proxy for printing and the other that does not.
+type base struct {
+	ExtraAttr                            []string
+	PrintFile, Name, PpdFile, GoldenFile string
+	Options                              []string
+}
+
+// test adds non-informational parametrized tests (one proxy, one regular)
+// that use "to_print.pdf" for printing.
+func test(name, ppdFile, goldenFile string, options ...string) base {
+	return base{nil, "to_print.pdf", name, ppdFile, goldenFile, options}
+}
+
+// iTest adds informational parametrized tests (one proxy, one regular)
+// that use "to_print.pdf" for printing.
+func iTest(name, ppdFile, goldenFile string, options ...string) base {
+	return base{[]string{"informational"}, "to_print.pdf", name, ppdFile, goldenFile, options}
+}
+
+// test2 adds non-informational parametrized tests (one proxy, one regular)
+// that use "2page.pdf" for printing.
+func test2(name, ppdFile, goldenFile string, options ...string) base {
+	return base{nil, "2page.pdf", name, ppdFile, goldenFile, options}
+}
+
+// iTest2 adds informational parametrized tests (one proxy, one regular)
+// that use "2page.pdf" for printing.
+func iTest2(name, ppdFile, goldenFile string, options ...string) base {
+	return base{[]string{"informational"}, "2page.pdf", name, ppdFile, goldenFile, options}
+}
+
+func TestAddParams(t *testing.T) {
+	code := genparams.Template(t, `{{ range . }} {
+        Name: {{ .Name | fmt }},
+        Val: &ippprint.Params{
+                PpdFile:      {{ .PpdFile | fmt }},
+                PrintFile:    {{ .PrintFile | fmt }},
+                ExpectedFile: {{ .GoldenFile | fmt }},
+                {{ if .Options }}
+                Options:      {{ .Options | fmt }},
+                {{ end }}
+        },
+        ExtraData: []string{ {{ .PrintFile | fmt }}, {{ .PpdFile | fmt }}, {{ .GoldenFile | fmt }} },
+        {{ if .ExtraAttr }}
+        ExtraAttr: {{ .ExtraAttr | fmt }},
+        {{ end }}
+}, {{ end }}`, []base{
+		// Collate
+		iTest2("epson_software_collate", "printer_EpsonWF3620.ppd", "printer_collate_epson_software_collate_golden.bin", ippprint.WithCopies(2), ippprint.Collate()),
+		iTest2("epson_software_uncollated", "printer_EpsonWF3620.ppd", "printer_collate_epson_software_uncollated_golden.bin", ippprint.WithCopies(2)),
+		iTest2("epson_hardware_collate", "printer_EpsonWFC20590.ppd", "printer_collate_epson_hardware_collate_golden.ps", ippprint.WithCopies(2), ippprint.Collate()),
+		iTest2("epson_hardware_uncollated", "printer_EpsonWFC20590.ppd", "printer_collate_epson_hardware_uncollated_golden.ps", ippprint.WithCopies(2)),
+
+		// Resolution
+		iTest("lexmark_600dpi", "printer_Lexmark.ppd", "printer_resolution_lexmark_600dpi_golden.ps", ippprint.WithResolution("600dpi")),
+		iTest("lexmark_1200dpi", "printer_Lexmark.ppd", "printer_resolution_lexmark_1200dpi_golden.ps", ippprint.WithResolution("1200dpi")),
+		iTest("lexmark_2400x600dpi", "printer_Lexmark.ppd", "printer_resolution_lexmark_2400x600dpi_golden.ps", ippprint.WithResolution("2400x600dpi")),
+
+		// Add
+		iTest("dymo_lw", "printer_add_dymo_printer_lw450.ppd", "printer_add_dymo_lw_printer_golden.bin"),
+		iTest("dymo_lm", "printer_add_dymo_printer_lm450.ppd", "printer_add_dymo_lm_printer_golden.bin"),
+		iTest("epson", "printer_EpsonWF3620.ppd", "printer_add_epson_printer_golden.ps"),
+		iTest("epson_color", "printer_EpsonGenericColorModel.ppd", "printer_add_epson_printer_color_golden.bin", "print-color-mode=color"),
+		iTest("epson_monochrome", "printer_EpsonGenericColorModel.ppd", "printer_add_epson_printer_monochrome_golden.bin", "print-color-mode=monochrome"),
+		test("generic", "printer_add_generic_printer_GenericPostScript.ppd.gz", "printer_add_generic_printer_golden.ps"),
+		iTest("hp", "printer_add_hp_printer_pclm.ppd.gz", "printer_add_hp_printer_pclm_out.pclm"),
+		iTest("hp_pwg_raster_color", "hp_ipp_everywhere.ppd", "printer_add_hp_ipp_everywhere_golden.pwg"),
+		iTest("hp_pwg_raster_monochrome", "hp_ipp_everywhere.ppd", "printer_add_hp_pwg_raster_monochrome_golden.pwg", "print-color-mode=monochrome"),
+		test("star", "printer_add_star_printer_rastertostar.ppd.gz", "printer_add_star_printer_rastertostar.bin"),
+		test("star_lm", "printer_add_star_printer_rastertostarlm.ppd.gz", "printer_add_star_printer_rastertostarlm.bin"),
+
+		// Pin print
+		test("hp_no_pin", "printer_HP.ppd", "printer_pin_print_hp_no_pin_golden.ps"),
+		test("hp_pin", "printer_HP.ppd", "printer_pin_print_hp_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("lexmark_no_pin", "printer_Lexmark.ppd", "printer_pin_print_lexmark_no_pin_golden.ps"),
+		test("lexmark_pin", "printer_Lexmark.ppd", "printer_pin_print_lexmark_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("ricoh_jobpassword_no_pin", "printer_Ricoh_JobPassword.ppd", "printer_pin_print_ricoh_JobPassword_no_pin_golden.ps"),
+		test("ricoh_jobpassword_pin", "printer_Ricoh_JobPassword.ppd", "printer_pin_print_ricoh_JobPassword_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("ricoh_lockedprintpassword_nopin", "printer_Ricoh_LockedPrintPassword.ppd", "printer_pin_print_ricoh_LockedPrintPassword_no_pin_golden.ps"),
+		test("ricoh_lockedprintpassword_pin", "printer_Ricoh_LockedPrintPassword.ppd", "printer_pin_print_ricoh_LockedPrintPassword_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("ricoh_password_no_pin", "printer_Ricoh_password.ppd", "printer_pin_print_ricoh_password_no_pin_golden.ps"),
+		test("ricoh_password_pin", "printer_Ricoh_password.ppd", "printer_pin_print_ricoh_password_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("sharp_no_pin", "printer_Sharp.ppd", "printer_pin_print_sharp_no_pin_golden.ps"),
+		test("sharp_pin", "printer_Sharp.ppd", "printer_pin_print_sharp_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("unsupported_no_pin", "printer_unsupported_GenericPostScript.ppd.gz", "printer_pin_print_unsupported_golden.ps"),
+		test("unsupported_pin", "printer_unsupported_GenericPostScript.ppd.gz", "printer_pin_print_unsupported_golden.ps", ippprint.WithJobPassword("1234")),
+	})
+	genparams.Ensure(t, "add.go", code)
+	genparams.Ensure(t, "proxy_add.go", code)
+}
