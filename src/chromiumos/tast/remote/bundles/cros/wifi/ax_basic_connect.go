a59d14152e3eb2c542e0d9e72d117e23ea0c0462
Anthony Hinton <hinton@google.com>
2020-12-15 13:50:59 -0800

    Adding AxBasicConnect tast test.
    This connects one of our 802.11ax DUTs to each of the four routeres we
    have. It will be added to a new ax suite, since it can only run in our
    ax Lab.
    BUG=b:175713339
    TEST=tast run chromeos7-row1-rack2-host12.cros wifi.AxBasicConnect
    
    Change-Id: I7591495d6d82e9ac83b1a82026c4d0b718f10aa3

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/ax_basic_connect.go
@@ -0,0 +1,93 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/common/network/protoutil"
+	"chromiumos/tast/common/wifi/security/wpa"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        AxBasicConnect,
+		Desc:        "Tests our ability to connect to our third-party ax routers",
+		Contacts:    []string{"hinton@google.com", "chromeos-platform-connectivity@google.com"},
+		ServiceDeps: []string{"tast.cros.network.WifiService"},
+		Attr:        []string{"group:wificell"},
+	})
+}
+
+func AxBasicConnect(ctx context.Context, s *testing.State) {
+	axConnect(ctx, s, "Velop-5G", "chromeos")
+	axConnect(ctx, s, "Rapture-5G-1", "chromeos")
+	axConnect(ctx, s, "Juplink-RX4-1500", "chromeos")
+	axConnect(ctx, s, "NETGEAR69-5G", "chromeos")
+}
+
+func axConnect(ctx context.Context, s *testing.State, ssid, passphrase string) {
+	defer func(ctx context.Context) {
+		rpc, _ := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+		wificlient := network.NewWifiServiceClient(rpc.Conn)
+		disconnectWifi(ctx, wificlient, false)
+	}(ctx)
+	s.Log("Connecting to Ax Router ", ssid)
+	rpc, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Error("Error trying to create RPC Dial: ", err)
+	}
+	wificlient := network.NewWifiServiceClient(rpc.Conn)
+	secConf := &wpa.Config{}
+	secProps, err := secConf.ShillServiceProperties()
+	if err != nil {
+		s.Error("Error Creating Security Properties: ", err)
+	}
+	secProps["Passphrase"] = "chromeos"
+	propsEnc, err := protoutil.EncodeToShillValMap(secProps)
+	if err != nil {
+		s.Error("Error encoding Security Properties to ShillValMaps: ", err)
+	}
+
+	err = disconnectWifi(ctx, wificlient, false)
+	if err == nil {
+		s.Log("Previous test did not clean up, Wifi was already connected. Disconnected it")
+	}
+
+	request := &network.ConnectRequest{
+		Ssid:       []byte("NETGEAR69"),
+		Hidden:     false,
+		Security:   secConf.Class(),
+		Shillprops: propsEnc,
+	}
+
+	response, err := wificlient.Connect(ctx, request)
+	if err != nil {
+		s.Error("Error connecting DUT to AP: ", err)
+	}
+	s.Log("Connection establish. Response was ", response)
+}
+
+func disconnectWifi(ctx context.Context, wificlient network.WifiServiceClient, removeProfile bool) error {
+	resp, err := wificlient.SelectedService(ctx, &empty.Empty{})
+	if err != nil {
+		return errors.Wrap(err, "failed to get selected service")
+	}
+
+	req := &network.DisconnectRequest{
+		ServicePath:   resp.ServicePath,
+		RemoveProfile: removeProfile,
+	}
+	if _, err := wificlient.Disconnect(ctx, req); err != nil {
+		return errors.Wrap(err, "failed to disconnect")
+	}
+	return nil
+}
