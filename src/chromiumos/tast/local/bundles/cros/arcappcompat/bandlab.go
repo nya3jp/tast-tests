d9d0a78f9b5b0b9560598585e0c7a8ac571f451f
Archana Singh <archanasing@google.com>
2020-12-16 11:40:46 -0800

    Run UIAutomator Test Using Tast For Bandlab app
    BUG=b:171982408
    TEST=tast run <eve> arcappcompat.Bandlab
    
    Tast Test for Bandlab app verifies launch app
    from the playstore, app is logged in and reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,Reopen tests.
    
    Cq-Depend: chrome-internal:3414045
    Change-Id: I92ea2799c5f6cc59f174cc7263949c31474e42cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2547524
    Tested-by: Archana Singh <archanasing@chromium.org>
    Commit-Queue: Archana Singh <archanasing@chromium.org>
    Reviewed-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/bandlab.go
@@ -0,0 +1,145 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForBandlab = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForBandlab},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForBandlab = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForBandlab},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Bandlab,
+		Desc:         "Functional test for Bandlab that installs the app also verifies it is logged in and that the main page is open, checks Bandlab correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val:               clamshellTestsForBandlab,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForBandlab,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForBandlab,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForBandlab,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars: []string{"arcappcompat.username", "arcappcompat.password",
+			"arcappcompat.Bandlab.emailid", "arcappcompat.Bandlab.password"},
+	})
+}
+
+// Bandlab test uses library for opting into the playstore and installing app.
+// Checks Bandlab correctly changes the window states in both clamshell and touchview mode.
+func Bandlab(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.bandlab.bandlab"
+		appActivity = ".core.activity.navigation.NavigationActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForBandlab verifies Bandlab is logged in and
+// verify Bandlab reached main activity page of the app.
+func launchAppForBandlab(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	const (
+		logInID          = "com.bandlab.bandlab:id/join_go_log_in"
+		enterEmailID     = "com.bandlab.bandlab:id/login_user_name"
+		enterPasswordID  = "com.bandlab.bandlab:id/login_password"
+		logInText        = "Log In"
+		logInButtonID    = "com.bandlab.bandlab:id/login_btn"
+		noThanksButtonID = "android:id/autofill_save_no"
+		createIconID     = "com.bandlab.bandlab:id/add"
+		createIconDesc   = "Create"
+	)
+
+	// Click on log in button.
+	logInButton := d.Object(ui.ID(logInID))
+	if err := logInButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("Log in button doesn't exist: ", err)
+	} else if err := logInButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on Log in button: ", err)
+	}
+
+	// Enter email address.
+	BandlabEmailID := s.RequiredVar("arcappcompat.Bandlab.emailid")
+	enterEmailAddress := d.Object(ui.ID(enterEmailID))
+	if err := enterEmailAddress.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("EnterEmailAddress doesn't exist: ", err)
+	} else if err := enterEmailAddress.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterEmailAddress: ", err)
+	} else if err := enterEmailAddress.SetText(ctx, BandlabEmailID); err != nil {
+		s.Fatal("Failed to enterEmailAddress: ", err)
+	}
+
+	// Enter password.
+	BandlabPassword := s.RequiredVar("arcappcompat.Bandlab.password")
+	enterPassword := d.Object(ui.ID(enterPasswordID))
+	if err := enterPassword.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("EnterPassword doesn't exist: ", err)
+	} else if err := enterPassword.Click(ctx); err != nil {
+		s.Fatal("Failed to click on enterPassword: ", err)
+	} else if err := enterPassword.SetText(ctx, BandlabPassword); err != nil {
+		s.Fatal("Failed to enterPassword: ", err)
+	}
+
+	// Click on log in button
+	logInBtn := d.Object(ui.ID(logInButtonID), ui.Text(logInText))
+	if err := logInBtn.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("LogIn button doesn't exist: ", err)
+	} else if err := logInBtn.Click(ctx); err != nil {
+		s.Fatal("Failed to click on LogIn button: ", err)
+	}
+
+	// Click no thanks button
+	noThanksButton := d.Object(ui.ID(noThanksButtonID))
+	if err := noThanksButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Error("noThanks button doesn't exist: ", err)
+	} else if err := noThanksButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on noThanks button: ", err)
+	}
+
+	// Check for create icon.
+	createIcon := d.Object(ui.ID(createIconID), ui.Description(createIconDesc))
+	if err := createIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("createIcon doesn't exist: ", err)
+	}
+}
