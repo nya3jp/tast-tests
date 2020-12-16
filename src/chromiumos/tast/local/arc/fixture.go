692a6980af631a4277af9a6c9ec43c136de83354
Kimiyuki Onaka <kimiyuki@google.com>
2020-12-16 19:43:26 +0900

    tast-tests: Add ARCVM console output to test results with fixtures
    This commit makes the "arcBooted" fixture save console output of ARCVM
    guest kernel as "arcvm-console.txt" in Tast result logs.
    
    We need to use fixtures to work even when two or more test cases are run
    at once. Preconditions have per-start hooks but don't have per-test hooks.
    
    BUG=b:149921859
    TEST=tast run my_crbook arc.BuildProperties.vm arc.CheckAndroidVersion.vm
    
    Cq-Depend: chromium:2617165
    Change-Id: Ia01edc2e2de56a15a3a86fdb3223ec6d4e22a775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2594929
    Tested-by: Kimiyuki Onaka <kimiyuki@google.com>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Kimiyuki Onaka <kimiyuki@google.com>

--- src/chromiumos/tast/local/arc/fixture.go
+++ src/chromiumos/tast/local/arc/fixture.go
@@ -6,8 +6,11 @@ package arc
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/testing"
 )
 
@@ -19,6 +22,7 @@ func init() {
 		SetUpTimeout:    chrome.LoginTimeout + BootTimeout,
 		ResetTimeout:    resetTimeout,
 		TearDownTimeout: resetTimeout,
+		PostTestTimeout: resetTimeout,
 	})
 }
 
@@ -98,4 +102,42 @@ func (f *bootedFixture) PreTest(ctx context.Context, s *testing.FixtTestState) {
 func (f *bootedFixture) PostTest(ctx context.Context, s *testing.FixtTestState) {
 	// TODO(crbug.com/1136382): Support per-test logcat once we get pre/post-test
 	// hooks in fixtures.
+
+	if err := saveARCVMConsole(ctx, s.OutDir()); err != nil {
+		s.Error("Failed to to save ARCVM console output: ", err)
+	}
+}
+
+// saveARCVMConsole saves the console output of ARCVM Kernel to the output directory using vm_pstore_dump command.
+func saveARCVMConsole(ctx context.Context, outDir string) error {
+	const (
+		pstoreCommandPath = "/usr/bin/vm_pstore_dump"
+		arcvmConsoleName  = "arcvm-console.txt"
+	)
+
+	// Do nothing for containers. The console output is already captured for containers.
+	isVMEnabled, err := VMEnabled()
+	if err != nil {
+		return err
+	}
+	if !isVMEnabled {
+		return nil
+	}
+
+	// TODO(b/153934386): Remove this check when pstore is enabled on ARM.
+	// The pstore feature is enabled only on x86_64. It's not enabled on some architectures, and this `vm_pstore_dump` command doesn't exist on such architectures.
+	if _, err := os.Stat(pstoreCommandPath); os.IsNotExist(err) {
+		return nil
+	}
+
+	path := filepath.Join(outDir, arcvmConsoleName)
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	cmd := testexec.CommandContext(ctx, pstoreCommandPath)
+	cmd.Stdout = file
+	return cmd.Run()
 }
