47092a6825b46fb00994bf6d475bf8f56e62e54f
Archana Singh <archanasing@google.com>
2020-12-17 14:28:01 -0800

    Run UIAutomator Test Using Tast For ESPN app
    BUG=b:172088569
    TEST=tast run <eve> arcappcompat.ESPN
    
    Tast Test for ESPN app verifies launch app
    from the playstore, check app reached
    main activity page of the app. Also for Clamshell
    Mode it verifies Clamshell Full Screen, Minimize
    and Restore, Clamshell Resize, Reopen tests and
    for Touchview Mode it verifies Minimize and Restore,
    Reopen tests.
    
    Change-Id: I108b3ea8798b76071f58c8501da0b78c2cd0f1be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2597066
    Tested-by: Archana Singh <archanasing@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Commit-Queue: Archana Singh <archanasing@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/espn.go
@@ -0,0 +1,154 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+// ClamshellTests are placed here.
+var clamshellTestsForESPN = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForESPN},
+	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
+	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
+	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+// TouchviewTests are placed here.
+var touchviewTestsForESPN = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForESPN},
+	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
+	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ESPN,
+		Desc:         "Functional test for ESPN that installs the app also verifies it is logged in and that the main page is open, checks ESPN correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		HardwareDeps: hwdep.D(hwdep.SkipOnModel("elm")),
+		Params: []testing.Param{{
+			Val:               clamshellTestsForESPN,
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "tablet_mode",
+			Val:               touchviewTestsForESPN,
+			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}, {
+			Name:              "vm",
+			Val:               clamshellTestsForESPN,
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               pre.AppCompatBooted,
+		}, {
+			Name:              "vm_tablet_mode",
+			Val:               touchviewTestsForESPN,
+			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
+			Pre:               pre.AppCompatBootedInTabletMode,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.username", "arcappcompat.password"},
+	})
+}
+
+// ESPN test uses library for opting into the playstore and installing app.
+// Checks  ESPN correctly changes the window states in both clamshell and touchview mode.
+func ESPN(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.espn.score_center"
+		appActivity = "com.espn.sportscenter.ui.EspnLaunchActivity"
+	)
+	testCases := s.Param().([]testutil.TestCase)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+}
+
+// launchAppForESPN verifies app is logged in and
+// verify app reached main activity page of the app.
+func launchAppForESPN(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+
+	const (
+		allowText       = "ALLOW"
+		signUpLaterText = "Sign Up Later"
+		skipText        = "Skip"
+		finishText      = "Finish"
+		okText          = "OK"
+		homeID          = "com.espn.score_center:id/largeLabel"
+	)
+
+	// Click on allow button until log in button exists.
+	allowButton := d.Object(ui.Text(allowText))
+	signUpButton := d.Object(ui.Text(signUpLaterText))
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := signUpButton.Exists(ctx); err != nil {
+			s.Log(" Click on allow button until sign up button exist")
+			allowButton.Click(ctx)
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Error("Sign up button doesn't exist: ", err)
+	} else if err := signUpButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on sign up button: ", err)
+	}
+
+	// Click on skip button.
+	skipButton := d.Object(ui.Text(skipText))
+	if err := skipButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("skip button doesn't exists: ", err)
+	} else if err := skipButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on skip button: ", err)
+	}
+
+	// Click on finish button.
+	finishButton := d.Object(ui.Text(finishText))
+	if err := finishButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("finish button doesn't exists: ", err)
+	} else if err := finishButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on finish button: ", err)
+	}
+
+	// Click on ok button.
+	okButton := d.Object(ui.Text(okText))
+	if err := okButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("ok button doesn't exists: ", err)
+	} else if err := okButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on ok button: ", err)
+	}
+
+	// Press back key  until home screen exists.
+	homeButton := d.Object(ui.ID(homeID))
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := homeButton.Exists(ctx); err != nil {
+			if err := d.PressKeyCode(ctx, ui.KEYCODE_BACK, 0); err != nil {
+				s.Log("Failed to enter KEYCODE_BACK: ", err)
+			} else {
+				s.Log("Entered KEYCODE_BACK")
+			}
+			return err
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: testutil.ShortUITimeout}); err != nil {
+		s.Fatal("Failed to enter KEYCODE_BACK: ", err)
+	}
+
+	// Check home page is launched.
+	if err := homeButton.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal("home button doesn't exists: ", err)
+	}
+}
