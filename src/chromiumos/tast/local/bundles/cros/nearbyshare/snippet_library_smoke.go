b801372a8c2f2bf0c6e98785e17f76ba4903bd36
Kyle Shimabukuro <kyleshima@chromium.org>
2020-12-17 03:09:56 -0800

    Add controls for Android Nearby Share
    Add controls for Android Nearby Share. The Android NS team in Taiwan
    develops the Nearby snippet APK, an RPC server that provides APIs to
    interact with Nearby Share functionality. See go/nearby-e2e-exp-doc and
    go/nearby-e2e-android-snippet for additional details. This CL adds the
    basic setup for initializing the APK and making RPCs to it. A followup
    CL will have more detailed functionality for actually driving major
    Nearby Share flows.
    
    BUG=chromium:1136365
    TEST=tast run <ip> nearbyshare.SnippetLibrarySmoke (using
    nearby_snippet_20201217.zip)
    
    Change-Id: I8e9b2c61de402e3a1125fcc74eef0c52754be22f

---
+++ src/chromiumos/tast/local/bundles/cros/nearbyshare/snippet_library_smoke.go
@@ -0,0 +1,71 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package nearbyshare
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"chromiumos/tast/local/android/adb"
+	"chromiumos/tast/local/bundles/cros/nearbyshare/nearbysnippet"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SnippetLibrarySmoke,
+		Desc: "Checks that we can successfully run the Nearby Snippet on the Android device",
+		Contacts: []string{
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:nearby-share"},
+		SoftwareDeps: []string{"chrome"},
+		Data:         []string{nearbysnippet.ZipName},
+		// This var can be used when running locally on non-rooted devices which
+		// have already overridden the GMS Core flags by other means.
+		Vars: []string{"overrideGMS"},
+	})
+}
+
+// SnippetLibrarySmoke tests that we can successfully start and interact with the Nearby Snippet on the Android device.
+func SnippetLibrarySmoke(ctx context.Context, s *testing.State) {
+	// This loads the ARC adb vendor key, which must be pre-loaded on the Android device to allow adb over usb without requiring UI interaction.
+	if err := adb.LaunchServer(ctx); err != nil {
+		s.Fatal("Failed to launch adb server: ", err)
+	}
+
+	// Wait for the first available device, since we are assuming only a single device is connected.
+	testDevice, err := adb.WaitForDevice(ctx, func(device *adb.Device) bool { return true }, 10*time.Second)
+	if err != nil {
+		s.Fatal("Failed to list adb devices: ", err)
+	}
+
+	// Launch and start the Snippet. Don't override GMS Core flags if specified in the runtime vars.
+	override := true
+	if val, ok := s.Var("overrideGMS"); ok {
+		b, err := strconv.ParseBool(val)
+		if err != nil {
+			s.Fatal("Unable to convert overrideGMS var to bool: ", err)
+		}
+		override = b
+	}
+
+	androidNearby, err := nearbysnippet.New(ctx, testDevice, s.DataPath(nearbysnippet.ZipName), override)
+	if err != nil {
+		s.Fatal("Failed to set up the snippet server: ", err)
+	}
+	defer androidNearby.Cleanup(ctx)
+
+	if err := androidNearby.Initialize(ctx); err != nil {
+		s.Fatal("Failed to initialize snippet server: ", err)
+	}
+
+	version, err := androidNearby.GetNearbySharingVersion(ctx)
+	if err != nil {
+		s.Fatal("Failed to get Android's Nearby Share version: ", err)
+	}
+	s.Log("Successfully got Android Nearby Share version: ", version)
+}
