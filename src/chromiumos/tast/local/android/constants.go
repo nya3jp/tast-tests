ce72f6e15d84cb8b288b51ac48bb2978518097eb
Kyle Shimabukuro <kyleshima@chromium.org>
2020-12-17 03:09:56 -0800

    Add controls for Android Nearby Share
    Add controls for Android Nearby Share. The Android NS team in Taiwan
    develops the Nearby snippet APK, an RPC server that provides APIs to
    interact with Nearby Share functionality. See go/nearby-e2e-exp-doc and
    go/nearby-e2e-android-snippet for additional details. This CL adds the
    basic setup for initializing the APK and making RPCs to it. A followup
    CL will have more detailed functionality for actually driving major
    Nearby Share flows.
    
    BUG=chromium:1136365
    TEST=tast run <ip> nearbyshare.SnippetLibrarySmoke (using
    nearby_snippet_20201217.zip)
    
    Change-Id: I8e9b2c61de402e3a1125fcc74eef0c52754be22f

---
+++ src/chromiumos/tast/local/android/constants.go
@@ -0,0 +1,8 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package android
+
+// DefaultUser is the default Android user ID, corresponding to the system user that adb runs as.
+const DefaultUser = "0"
