45b41af0bb24c2441414f7e2149b3713e970d647
Trent Begin <tbegin@google.com>
2020-12-17 12:09:42 -0700

    tast-tests: move cros_healthd tests to separate folder
    This change moves all cros_healthd tests out of platform and to a new
    folder, cros/health. This shortens the name and cleans up the platform
    namespace.
    
    BUG=b:173062841
    TEST=tast run hatch-dut health.* (pass)
    
    Change-Id: I02e4e372781f44bc7fc6b51631965fad0cc9b926

--- src/chromiumos/tast/local/bundles/cros/health/diagnostics.go
+++ src/chromiumos/tast/local/bundles/cros/health/diagnostics.go
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package platform
+// Package health tests the system daemon cros_healthd to ensure that telemetry
+// and diagnostics calls can be completed successfully.
+package health
 
 import (
 	"context"
