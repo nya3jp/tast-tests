edb68f5568525f01dcb9529917bf9e8c1673b7ec
Avery Musbach <amusbach@chromium.org>
2020-12-17 10:33:21 -0800

    tast-tests: Nix visible browser window when measuring power usage of PIP
    The present CL is an updated version of [1]. I do not intend to land it.
    I only intend to run tests locally with it, for the purpose of
    investigating unnecessary compositing (as evidenced by viz.triangles
    trace data) which could be related to the visible browser window.
    
    [1] https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2410726
    
    BUG=chromium:1093933
    TEST=*PIPEnergyAndPower.*
    
    Change-Id: I4f1079471b0a9a53ffc970b8481b29090f69fbe0

--- src/chromiumos/tast/local/bundles/cros/ui/chrome_pip_energy_and_power.go
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_pip_energy_and_power.go
@@ -157,6 +157,19 @@ func ChromePIPEnergyAndPower(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to move mouse to PIP window: ", err)
 	}
 
+	windows, err := ash.GetAllWindows(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get windows: ", err)
+	}
+
+	if windowsCount := len(windows); windowsCount != 1 {
+		s.Fatal("Expected 1 window; found ", windowsCount)
+	}
+
+	if _, err := ash.SetWindowState(ctx, tconn, windows[0].ID, ash.WMEventMinimize); err != nil {
+		s.Fatal("Failed to minimize browser window: ", err)
+	}
+
 	resizeHandle, err := chromeui.Find(ctx, tconn, chromeui.FindParams{Name: "Resize", ClassName: "ResizeHandleButton"})
 	if err != nil {
 		s.Fatal("Failed to get PIP resize handle: ", err)
@@ -241,22 +254,6 @@ func ChromePIPEnergyAndPower(ctx context.Context, s *testing.State) {
 		if err := mouse.Move(ctx, tconn, workAreaTopLeft.Add(coords.NewPoint(20, 20)), time.Second); err != nil {
 			s.Fatal("Failed to move mouse: ", err)
 		}
-	}
-
-	extraConn, err := cr.NewConn(ctx, "chrome://settings")
-	if err != nil {
-		s.Fatal("Failed to load chrome://settings: ", err)
-	}
-	defer extraConn.Close()
-
-	if err := webutil.WaitForQuiescence(ctx, extraConn, 10*time.Second); err != nil {
-		s.Fatal("Failed to wait for chrome://settings to achieve quiescence: ", err)
-	}
-
-	// Tab away from the search box of chrome://settings, so that
-	// there will be no blinking cursor.
-	if err := kw.Accel(ctx, "Tab"); err != nil {
-		s.Fatal("Failed to send Tab: ", err)
 	}
 
 	// triedToStopTracing means that cr.StopTracing(cleanupCtx)
