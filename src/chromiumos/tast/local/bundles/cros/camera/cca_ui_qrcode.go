1a3ac4098d958aab94ead1a1715ee41603ceea83
Shik Chen <shik@chromium.org>
2020-12-17 23:20:48 +0800

    camera.CCAUIQRCode: Add a test for QR code detection
    This CL adds a test to check that an URL in QR code can be detected,
    copied, and opened from the camera app.
    
    BUG=b:172879638
    TEST=tast run <dut> 'camera.CCAUIQRCode*'
    
    Change-Id: I7f14b39325742273422a25f527e7528d35badafd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2596942
    Tested-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_qrcode.go
@@ -0,0 +1,131 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/camera/cca"
+	"chromiumos/tast/local/bundles/cros/camera/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUIQRCode,
+		Desc:         "Checks QR code detection in CCA",
+		Contacts:     []string{"shik@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		Data:         []string{"cca_ui.js", "qrcode_1280x960.y4m"},
+		Params: []testing.Param{{
+			Val: testutil.PlatformApp,
+		}, {
+			Name: "swa",
+			Val:  testutil.SWA,
+		}},
+	})
+}
+
+func CCAUIQRCode(ctx context.Context, s *testing.State) {
+	const expectedURL = "https://www.google.com/chromebook/chrome-os/"
+	useSWA := s.Param().(testutil.CCAAppType) == testutil.SWA
+
+	feature := chrome.DisableFeatures("CameraSystemWebApp")
+	if useSWA {
+		feature = chrome.EnableFeatures("CameraSystemWebApp")
+	}
+	cr, err := chrome.New(ctx,
+		feature,
+		chrome.ExtraArgs(
+			"--use-fake-device-for-media-stream",
+			"--use-file-for-fake-video-capture="+s.DataPath("qrcode_1280x960.y4m")))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get test connection: ", err)
+	}
+
+	tb, err := testutil.NewTestBridge(ctx, cr, useSWA)
+	if err != nil {
+		s.Fatal("Failed to construct test bridge: ", err)
+	}
+	defer tb.TearDown(ctx)
+
+	if err := cca.ClearSavedDirs(ctx, cr); err != nil {
+		s.Fatal("Failed to clear saved directory: ", err)
+	}
+
+	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir(), tb, useSWA)
+	if err != nil {
+		s.Fatal("Failed to open CCA: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := app.Close(ctx); err != nil {
+			s.Error("Failed to close app: ", err)
+		}
+	}(ctx)
+
+	enabled, err := app.ToggleQRCodeOption(ctx)
+	if err != nil {
+		s.Fatal("Failed to enable QR code detection: ", err)
+	}
+	if !enabled {
+		s.Fatal("QR code detection is not enabled after toggling")
+	}
+	s.Log("Start scanning QR Code")
+
+	if err := app.WaitForVisibleState(ctx, cca.BarcodeChipURL, true); err != nil {
+		s.Fatal("Failed to detect url from barcode: ", err)
+	}
+	s.Log("URL detected")
+
+	// Copy the url.
+	if err := app.Click(ctx, cca.BarcodeCopyURLButton); err != nil {
+		s.Fatal("Failed to click copy button: ", err)
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		var clipData string
+		if err := tconn.Eval(ctx, `tast.promisify(chrome.autotestPrivate.getClipboardTextData)()`, &clipData); err != nil {
+			return testing.PollBreak(err)
+		}
+		if clipData != expectedURL {
+			return errors.Errorf("unexpected clipboard data: got %q, want %q", clipData, expectedURL)
+		}
+		s.Log("URL copied successfully")
+		return nil
+	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
+		s.Fatal("Failed to get expected clipboard data: ", err)
+	}
+
+	// Open the url.
+	if err := app.Click(ctx, cca.BarcodeChipURL); err != nil {
+		s.Fatal("Failed to click url: ", err)
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		ok, err := cr.IsTargetAvailable(ctx, chrome.MatchTargetURL(expectedURL))
+		if err != nil {
+			return testing.PollBreak(err)
+		}
+		if !ok {
+			return errors.Errorf("no match target for %v", expectedURL)
+		}
+		s.Log("URL opened successfully")
+		return nil
+	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
+		s.Fatal("Failed to open url: ", err)
+	}
+
+	// TODO(b/172879638): Test text chip.
+	// TODO(b/172879638): Test invalid binary content.
+}
