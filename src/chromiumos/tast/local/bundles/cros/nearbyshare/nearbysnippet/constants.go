b811b71f189a30d6856fdb33fac13fb335643f79
Kyle Shimabukuro <kyleshima@chromium.org>
2020-12-17 03:09:56 -0800

    Add controls for Android Nearby Share
    Add controls for Android Nearby Share. The Android NS team in Taiwan
    develops the Nearby snippet APK, an RPC server that provides APIs to
    interact with Nearby Share functionality. See go/nearby-e2e-exp-doc and
    go/nearby-e2e-android-snippet for additional details. This CL adds the
    basic setup for initializing the APK and making RPCs to it. A followup
    CL will have more detailed functionality for actually driving major
    Nearby Share flows.
    
    BUG=chromium:1136365
    TEST=tast run <ip> nearbyshare.SnippetLibrarySmoke (using
    nearby_snippet_20201217.zip)
    
    Change-Id: I8e9b2c61de402e3a1125fcc74eef0c52754be22f

---
+++ src/chromiumos/tast/local/bundles/cros/nearbyshare/nearbysnippet/constants.go
@@ -0,0 +1,54 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package nearbysnippet
+
+// Constants used in the adb commands for installing and launching the Nearby Snippet.
+const (
+	ZipName                      = "nearby_snippet.zip"
+	ApkName                      = "nearby_snippet.apk"
+	protocolVersion              = "1"
+	moblyPackage                 = "com.google.android.gmscore.integ.modules.nearby.mobly.snippets"
+	instrumentationRunnerPackage = "com.google.android.mobly.snippet.SnippetRunner"
+)
+
+// DataUsage are data usage values for the Nearby Snippet's setupDevice and getDataUsage methods.
+type DataUsage int
+
+// These are the 3 values defined by the Nearby Snippet API.
+const (
+	DataUsageOffline DataUsage = iota + 1
+	DataUsageOnline
+	DataUsageWifiOnly
+)
+
+// Visibility are values for the Nearby Snippet's setupDevice and getVisibility methods, corresponding to different contact visibility settings.
+type Visibility int
+
+// These are the 5 values defined by the Nearby Snippet API.
+const (
+	VisibilityUnknown Visibility = iota - 1
+	VisibilityNoOne
+	VisibilityAllContacts
+	VisibilitySelectedContacts
+	VisibilityEveryone
+)
+
+// SnippetEvent are the event names posted by the Nearby Snippet to its event cache after initiating receiving.
+// The host CrOS device can monitor the sharing state by awaiting these events using the Nearby Snippet's eventWaitAndGet RPC.
+type SnippetEvent string
+
+// Event names defined by the Nearby Snippet.
+const (
+	// Snippet events when Android is the receiver.
+	SnippetEventOnLocalConfirmation SnippetEvent = "onLocalConfirmation"
+	SnippetEventOnReceiveStatus     SnippetEvent = "onReceiveStatus"
+	// Snippet events when Android is the sender.
+	SnippetEventOnReceiverFound          SnippetEvent = "onReceiverFound"
+	SnippetEventOnAwaitingReceiverAccept SnippetEvent = "onAwaitingReceiverAccept"
+	SnippetEventOnTransferStatus         SnippetEvent = "onTransferStatus"
+	// Shared Snippet event when Android is sender and receiver.
+	// The onStop event indicates that the transfer is complete and all teardown tasks for Android Nearby are complete.
+	SnippetEventOnStop SnippetEvent = "onStop"
+)
