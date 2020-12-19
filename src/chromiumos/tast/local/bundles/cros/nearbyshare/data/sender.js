a8627dca8649d6c5bb8fdc5aa1dc47597ee1c2e4
Kyle Shimabukuro <kyleshima@chromium.org>
2020-12-18 22:54:37 -0800

    (Nearby Share) Add test for CB->Phone file transfer
    Automated test for CB->phone Nearby Share file transfer using the API
    approach (no UI interaction). The CrOS side is controlled by running
    Javascript on a conn to chrome://nearby. The JS implements the necessary
    interfaces to drive the sending flow, mostly copying from the actual
    page's JS implementation at
    https://chromium.googlesource.com/chromium/src/+/master/chrome/browser/resources/nearby_share.
    The Android side is controlled via snippet RPC as described in
    go/nearby-e2e-android-snippet. The RPCs required for the receiving flow
    have been added here. The test has been implemented as a parameterized
    test so we can extend it with different file types/combinations in the
    future, the test data files simply need to be zipped and uploaded to the
    private test assets gsbucket.
    
    BUG=chromium:1129232
    TEST=tast run <dut> nearbyshare.CrosToAndroidFiles.small_png
    TEST=tast run <dut> -var=rooted=false
    nearbyshare.CrosToAndroidFiles.small_jpg (passes on local unrooted
    device)
    
    Change-Id: I9daab09334af425ff57111b157298eeaaee8aedf

---
+++ src/chromiumos/tast/local/bundles/cros/nearbyshare/data/sender.js
@@ -0,0 +1,93 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview JavaScript to run on chrome://nearby to control the sending
+ * flow. This is largely based on chrome://nearby's discovery_manager.js and
+ * nearby_discovery_page.js with minor adjustments made to ease testing.
+ * See https://chromium.googlesource.com/chromium/src/+/master/chrome/browser/resources/nearby_share
+ */
+function () {
+  return {
+    // Current transfer status once the share starts
+    // (nearbyShare.mojom.TransferStatus).
+    currentTransferStatus: null,
+
+    // Result of selectShareTarget (nearbyShare.mojom.SelectShareTargetResult).
+    selectShareTargetRes: null,
+
+    // Secure confirmation token shown to both parties. Becomes a valid value
+    // once transferUpdateListener_ starts getting updates.
+    confirmationToken: null,
+
+    // ConfirmationManager interface for the share target.
+    // Becomes a valid object once we have successfully selected a share target.
+    confirmationManager_: null,
+
+    // TransferUpdateListener interface for the share target.
+    // Becomes a valid object once we have successfully selected a share target.
+    transferUpdateListener_: null,
+
+    // Find the first share target with the given name within the discovery
+    // page's iron-list, where share target info is exposed to the page.
+    getShareTarget(name) {
+      shareTargets = document.querySelector("nearby-share-app").shadowRoot
+        .querySelector("nearby-discovery-page")
+        .shadowRoot.querySelectorAll("nearby-device");
+      target = null;
+      shareTargets.forEach(element => {
+        t = element.__data.shareTarget;
+        if (t.name == name) {
+          target = t;
+        }
+      });
+      return target;
+    },
+
+    selectShareTarget(name) {
+      shareTarget = this.getShareTarget(name);
+      if (!shareTarget) {
+        return;
+      }
+      import('./discovery_manager.js')
+        .then(m => m.getDiscoveryManager().selectShareTarget(shareTarget.id))
+        .then(response => {
+          const {
+            result,
+            transferUpdateListener,
+            confirmationManager
+          } = response;
+          this.selectShareTargetRes = result;
+          if (result !== nearbyShare.mojom.SelectShareTargetResult.kOk) {
+            return;
+          }
+
+          this.confirmationManager_ = confirmationManager;
+          this.transferUpdateListener_ =
+            new nearbyShare.mojom.TransferUpdateListenerReceiver(this);
+          this.transferUpdateListener_.$.bindHandle(
+            transferUpdateListener.handle);
+        });
+    },
+
+    onTransferUpdate(status, token) {
+      if (token) {
+        this.confirmationToken = token;
+      }
+      this.currentTransferStatus = status;
+    },
+
+    accept() {
+      this.confirmationManager_.accept();
+    },
+
+    reject() {
+      this.confirmationManager_.reject();
+    },
+
+    cancel() {
+      this.confirmationManager_.cancel();
+    },
+  }
+}
--- src/chromiumos/tast/local/bundles/cros/nearbyshare/nearbysetup/nearbysetup.go
+++ src/chromiumos/tast/local/bundles/cros/nearbyshare/nearbysetup/nearbysetup.go
@@ -7,12 +7,25 @@ package nearbysetup
 
 import (
 	"context"
+	"strconv"
+	"strings"
+	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/android"
+	"chromiumos/tast/local/android/adb"
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/bundles/cros/nearbyshare/nearbysnippet"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/nearbyshare"
 	"chromiumos/tast/local/chrome/ui/ossettings"
+	"chromiumos/tast/testing"
 )
+
+// DefaultScreenTimeout is the default screen-off timeout for the Android device.
+// It is a sufficiently large value to guarantee most transfers can complete without the screen turning off,
+// since Nearby Share on Android requires the screen to be on.
+const DefaultScreenTimeout = 10 * time.Minute
 
 // CrOSSetup enables Chrome OS Nearby Share and configures its settings through OS Settings. This allows tests to bypass onboarding.
 // If deviceName is empty, the device display name will not be set and the default will be used.
@@ -65,4 +78,78 @@ func CrOSSetup(ctx context.Context, tconn *chrome.TestConn, cr *chrome.Chrome, d
 		}
 	}
 	return nil
+}
+
+// AndroidSetup prepares the connected Android device for Nearby Share tests.
+func AndroidSetup(ctx context.Context, apkZipPath string, rooted bool, screenOff time.Duration, dataUsage nearbysnippet.DataUsage, visibility nearbysnippet.Visibility, name string) (*nearbysnippet.AndroidNearbyDevice, error) {
+	// This loads the ARC adb vendor key, which must be pre-loaded on the Android device to allow adb over usb without requiring UI interaction.
+	if err := adb.LaunchServer(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to launch adb server")
+	}
+
+	// Wait for the first available device, since we are assuming only a single device is connected.
+	testDevice, err := adb.WaitForDevice(ctx, func(device *adb.Device) bool { return !strings.HasPrefix(device.Serial, "emulator-") }, 10*time.Second)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to list adb devices")
+	}
+
+	// Clear the Android's default directory for receiving shares.
+	if err := testDevice.RemoveAll(ctx, android.DownloadDir); err != nil {
+		return nil, errors.Wrap(err, "failed to clear Android downloads directory")
+	}
+
+	// Prepare the device for Nearby Sharing by waking the screen, enabling bluetooth, and extending the screen-off timeout.
+	if err := testDevice.PressKeyCode(ctx, strconv.Itoa(int(ui.KEYCODE_WAKEUP))); err != nil {
+		return nil, errors.Wrap(err, "failed to wake screen")
+	}
+
+	if rooted {
+		if err := testDevice.EnableBluetooth(ctx); err != nil {
+			return nil, errors.Wrap(err, "failed to enable bluetooth")
+		}
+		if err := testDevice.SetScreenOffTimeout(ctx, screenOff); err != nil {
+			return nil, errors.Wrap(err, "failed to extend screen-off timeout")
+		}
+	}
+
+	// Launch and start the snippet server. Don't override GMS Core flags if specified in the runtime vars.
+	androidNearby, err := nearbysnippet.New(ctx, testDevice, apkZipPath, rooted)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to set up the snippet server")
+	}
+
+	if err := androidNearby.Initialize(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to initialize snippet server")
+	}
+
+	if err := androidNearby.SetupDevice(ctx, dataUsage, visibility, name); err != nil {
+		return nil, errors.Wrap(err, "failed to configure Android Nearby Share settings")
+	}
+
+	// androidNearby.SetupDevice is asynchronous, so we need to poll until the settings changes have taken effect.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if n, err := androidNearby.GetDeviceName(ctx); err != nil {
+			return testing.PollBreak(err)
+		} else if n != name {
+			return errors.Errorf("current device name (%v) not yet updated to %v", n, name)
+		}
+
+		if v, err := androidNearby.GetVisibility(ctx); err != nil {
+			return testing.PollBreak(err)
+		} else if v != visibility {
+			return errors.Errorf("current visibility (%v) not yet updated to %v", v, visibility)
+		}
+
+		if d, err := androidNearby.GetDataUsage(ctx); err != nil {
+			return testing.PollBreak(err)
+		} else if d != dataUsage {
+			return errors.Errorf("current data usage (%v) not yet updated to %v", d, dataUsage)
+		}
+
+		return nil
+	}, &testing.PollOptions{Interval: 2 * time.Second, Timeout: 10 * time.Second}); err != nil {
+		return nil, errors.Wrap(err, "timed out waiting for Nearby Share settings to update")
+	}
+
+	return androidNearby, nil
 }
