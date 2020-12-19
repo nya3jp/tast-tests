8a554b835e8e080369b217ad60aed31489ed9dd1
Kyle Shimabukuro <kyleshima@chromium.org>
2020-12-18 22:54:37 -0800

    (Nearby Share) Add test for CB->Phone file transfer
    Automated test for CB->phone Nearby Share file transfer using the API
    approach (no UI interaction). The CrOS side is controlled by running
    Javascript on a conn to chrome://nearby. The JS implements the necessary
    interfaces to drive the sending flow, mostly copying from the actual
    page's JS implementation at
    https://chromium.googlesource.com/chromium/src/+/master/chrome/browser/resources/nearby_share.
    The Android side is controlled via snippet RPC as described in
    go/nearby-e2e-android-snippet. The RPCs required for the receiving flow
    have been added here. The test has been implemented as a parameterized
    test so we can extend it with different file types/combinations in the
    future, the test data files simply need to be zipped and uploaded to the
    private test assets gsbucket.
    
    BUG=chromium:1136365
    TEST=tast run <dut> nearbyshare.CrosToAndroidFiles.small_png
    TEST=tast run <dut> -var=rooted=false
    nearbyshare.CrosToAndroidFiles.small_jpg (passes on local unrooted
    device)
    
    Change-Id: I9daab09334af425ff57111b157298eeaaee8aedf

--- src/chromiumos/tast/local/android/constants.go
+++ src/chromiumos/tast/local/android/constants.go
@@ -6,3 +6,14 @@ package android
 
 // DefaultUser is the default Android user ID, corresponding to the system user that adb runs as.
 const DefaultUser = "0"
+
+// DownloadDir is Android's default downloads directory.
+const DownloadDir = "/sdcard/Download/"
+
+// KeyCode is used to specify a key event on the Android device using the `adb shell input keyevent` command.
+type KeyCode string
+
+// Derived from https://developer.android.com/reference/android/view/KeyEvent.html.
+const (
+	KeyCodeWakeup KeyCode = "KEYCODE_WAKEUP"
+)
