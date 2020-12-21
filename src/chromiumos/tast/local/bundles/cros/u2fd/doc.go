f97a286f4b40ae97908e3c71624c5d30450b1de8
Yicheng Li <yichengli@chromium.org>
2020-12-21 10:06:52 -0800

    Add WebAuthn end-to-end test using external website
    This test exercise the WebAuthn feature, which involves implementation
    in Chrome, cryptohome, Ash, u2fd and cr50. The WebAuthn flow starts
    from the browser receiving a WebAuthn request. The browser then talks
    to u2fd to perform MakeCredential or GetAssertion.
    
    Why this is not using a local js file: WebAuthn has security checks that
    disallow using an IP address as host, or other "untrustworthy" domains.
    Thus only an external website can invoke WebAuthn. This test uses
    https://securitykeys.info, which is controlled by Google.
    
    BUG=b:169976548
    TEST=tast -verbose run <IP> u2fd.WebauthnUsingPIN
    
    Change-Id: Ic41457ac9a144f51188a5b0f3003a2d2561557e9

---
+++ src/chromiumos/tast/local/bundles/cros/u2fd/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package u2fd contains local Tast tests that exercise the Universal Second Factor Daemon of Chrome OS.
+package u2fd
