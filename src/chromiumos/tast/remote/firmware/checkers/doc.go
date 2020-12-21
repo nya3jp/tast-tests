554372aa73a9e89e42848c8010e583ab00ea1dcd
Andrew Luo <aluo@google.com>
2020-12-21 14:26:26 -0800

    Factor out GBB flags check logic into firmware checkers.
    BUG=b:166162128
    TEST=tast run <DUT> firmware.GBBFlags
    
    Change-Id: Ie67e21def31f2f8d0d97eb0321cf692384f3320e

---
+++ src/chromiumos/tast/remote/firmware/checkers/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package checkers is a collection of canonical methods to verify test-relevant information about a DUT.
+// It should not modify the DUT state.
+package checkers
