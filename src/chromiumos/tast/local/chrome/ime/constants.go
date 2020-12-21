907856893ebf8709760515abc42706447c4fff4a
Jun Sheng <shengjun@google.com>
2020-12-21 13:13:19 +1100

    tast-tests: refactor VirtualKeyboardInputFields
    1. enable IME tests on mainline after tested them locally.
    2. Enable screenshot on each subtest.
    3. Keep exp off mainline as they are broken, and should be fixed in 89.0.4358.0
    
    BUG=b:173158685
    TEST=inputs.*
    
    Change-Id: I4ad57838f0f114bd656fe9a114b4eb19df883037

--- src/chromiumos/tast/local/chrome/ime/constants.go
+++ src/chromiumos/tast/local/chrome/ime/constants.go
@@ -9,12 +9,15 @@ type InputMethodCode string
 
 // List of input tool codes.
 const (
-	INPUTMETHOD_NACL_MOZC_US              InputMethodCode = "nacl_mozc_us"    // Japanese input (for US keyboard)
-	INPUTMETHOD_NACL_MOZC_JP              InputMethodCode = "nacl_mozc_jp"    // Japanese input (for JP keyboard)
-	INPUTMETHOD_PINYIN_CHINESE_SIMPLIFIED InputMethodCode = "zh-t-i0-pinyin"  // Chinese Pinyin input method
-	INPUTMETHOD_XKB_US_ENG                InputMethodCode = "xkb:us::eng"     // English US keyboard
-	INPUTMETHOD_XKB_US_INTL               InputMethodCode = "xkb:us:intl:eng" // US International keyboard
-	INPUTMETHOD_XKB_FR_FRA                InputMethodCode = "xkb:fr::fra"     // FR France keyboard
-	INPUTMETHOD_XKB_ES_SPA                InputMethodCode = "xkb:es::spa"     // MSG Keyboard Spanish
-	INPUTMETHOD_XKB_JP_JPN                InputMethodCode = "xkb:jp::jpn"     // MSG Keyboard Japanese
+	INPUTMETHOD_NACL_MOZC_US                  InputMethodCode = "nacl_mozc_us"              // Japanese input (for US keyboard)
+	INPUTMETHOD_NACL_MOZC_JP                  InputMethodCode = "nacl_mozc_jp"              // Japanese input (for JP keyboard)
+	INPUTMETHOD_PINYIN_CHINESE_SIMPLIFIED     InputMethodCode = "zh-t-i0-pinyin"            // Chinese Pinyin input method
+	INPUTMETHOD_CANTONESE_CHINESE_TRADITIONAL InputMethodCode = "yue-hant-t-i0-und"         // Chinese Cantonese input method
+	INPUTMETHOD_CANGJIE87_CHINESE_TRADITIONAL InputMethodCode = "zh-hant-t-i0-cangjie-1987" // Chinese Cangjie input method
+	INPUTMETHOD_HANGUL_KOREAN                 InputMethodCode = "ko-t-i0-und"               // Korean input method
+	INPUTMETHOD_XKB_US_ENG                    InputMethodCode = "xkb:us::eng"               // English US keyboard
+	INPUTMETHOD_XKB_US_INTL                   InputMethodCode = "xkb:us:intl:eng"           // US International keyboard
+	INPUTMETHOD_XKB_FR_FRA                    InputMethodCode = "xkb:fr::fra"               // FR France keyboard
+	INPUTMETHOD_XKB_ES_SPA                    InputMethodCode = "xkb:es::spa"               // MSG Keyboard Spanish
+	INPUTMETHOD_XKB_JP_JPN                    InputMethodCode = "xkb:jp::jpn"               // MSG Keyboard Japanese
 )
---
+++ src/chromiumos/tast/local/chrome/ui/faillog/screenshot.go
@@ -0,0 +1,45 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package faillog provides helper functions for dumping UI data on test failures.
+package faillog
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+// Default screenshot file name.
+const screenshotFileName = "screenshot.png"
+
+// SaveScreenshotOnError takes screenshot when the test fails.
+// Use SaveScreenshotToFile, if you want to specify the fileName.
+func SaveScreenshotOnError(ctx context.Context, cr *chrome.Chrome, outDir string, hasError func() bool, tconn *chrome.TestConn) {
+	SaveScreenshotToFile(ctx, cr, outDir, hasError, tconn, screenshotFileName)
+}
+
+// SaveScreenshotToFile checks the given hasError function and takes screenshot into a file 'fileName' when the test fails.
+// It does nothing when the test succeeds.
+func SaveScreenshotToFile(ctx context.Context, cr *chrome.Chrome, outDir string, hasError func() bool, tconn *chrome.TestConn, fileName string) {
+	if !hasError() {
+		return
+	}
+
+	dir := filepath.Join(outDir, faillogDir)
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		testing.ContextLogf(ctx, "Failed to create directory %s: %v", dir, err)
+		return
+	}
+
+	screetshotFile := filepath.Join(dir, "screenshot.png")
+	testing.ContextLog(ctx, "Test failed. Saving screenshot to ", screetshotFile)
+	if err := screenshot.CaptureChrome(ctx, cr, screetshotFile); err != nil {
+		testing.ContextLog(ctx, "Failed to take screenshot: ", err)
+	}
+}
