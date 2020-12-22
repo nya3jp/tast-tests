4e9b630e39870a1056711008d664ab41c5413a8f
Wei Lee <wtlee@google.com>
2020-12-22 16:24:26 +0800

    camera: Add CCAUIRefresh
    This test is for testing if CCA still works after refresh. Currently
    only enable it for swa since on platform app version the app won't work
    after refreshing.
    
    BUG=b:176042324
    TEST=tast run [DUT] camera.CCAUIRefresh.*
    
    Change-Id: I13814c98ba084b17db18d58f66458553b4671bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2599655
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Yen-lin Lai <yenlinlai@google.com>
    Tested-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_refresh.go
@@ -0,0 +1,64 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/camera/cca"
+	"chromiumos/tast/local/bundles/cros/camera/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUIRefresh,
+		Desc:         "Test for checking Chrome Camera App still works after refreshing",
+		Contacts:     []string{"wtlee@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", caps.BuiltinOrVividCamera},
+		Data:         []string{"cca_ui.js"},
+		Params: []testing.Param{{
+			Name: "swa",
+			Pre:  testutil.ChromeWithSWA(),
+			Val:  testutil.SWA,
+		}},
+	})
+}
+
+func CCAUIRefresh(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+	useSWA := s.Param().(testutil.CCAAppType) == testutil.SWA
+	tb, err := testutil.NewTestBridge(ctx, cr, useSWA)
+	if err != nil {
+		s.Fatal("Failed to construct test bridge: ", err)
+	}
+	defer tb.TearDown(ctx)
+
+	if err := cca.ClearSavedDirs(ctx, cr); err != nil {
+		s.Fatal("Failed to clear saved directory: ", err)
+	}
+
+	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir(), tb, useSWA)
+	if err != nil {
+		s.Fatal("Failed to open CCA: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := app.Close(ctx); err != nil {
+			s.Error("Failed to close app: ", err)
+		}
+	}(ctx)
+
+	s.Log("Refreshing CCA")
+	if err := app.Refresh(ctx, tb); err != nil {
+		s.Fatal("Failed to complete refresh: ", err)
+	}
+
+	if err := app.WaitForVideoActive(ctx); err != nil {
+		s.Fatal("Preview is not shown after refreshing: ", err)
+	}
+}
