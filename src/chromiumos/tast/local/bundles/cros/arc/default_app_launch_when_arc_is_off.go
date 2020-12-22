57ecca27a5495b8586e36032bc8ba39ede5b81f7
Renuga Nanjappan <rnanjappan@chromium.org>
2020-12-22 08:17:48 +0000

    Tast Tests:Default Arc-Apps Launch Opt-In flow when PlayStore is Off.
    BUG=b:174260106
    TEST=tast -verbose run $DUT arc.DefaultAppsArcOff.vm
    
    Add a test to verify that default arc icons launch play store
    opt-in flow when Playstore is disabled.
    
    Change-Id: I52468a9d96e5a3e610b9ad09d637c2bdda49ac68

---
+++ src/chromiumos/tast/local/bundles/cros/arc/default_app_launch_when_arc_is_off.go
@@ -0,0 +1,97 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/optin"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/launcher"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DefaultAppLaunchWhenArcIsOff,
+		Desc:         "Verify Default App Icons Launch Opt In Flow When PlayStore is Off ",
+		Contacts:     []string{"rnanjappan@google.com", "cros-arc-te@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		Timeout: chrome.LoginTimeout + arc.BootTimeout + 120*time.Second,
+		Vars:    []string{"arc.username", "arc.password"},
+	})
+}
+
+func DefaultAppLaunchWhenArcIsOff(ctx context.Context, s *testing.State) {
+	const (
+		defaultTimeout = 20 * time.Second
+	)
+
+	username := s.RequiredVar("arc.username")
+	password := s.RequiredVar("arc.password")
+
+	cr, err := chrome.New(ctx, chrome.GAIALogin(), chrome.Auth(username, password, "gaia-id"), chrome.ARCSupported(), chrome.ExtraArgs(arc.DisableSyncFlags()...))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	// Launch Play Games App.
+	if err := launcher.LaunchApp(ctx, tconn, apps.PlayGames); err != nil {
+		s.Log("Failed to Launch the Play Games: ", err)
+	}
+
+	// Find  "More" button and click
+	moreParams := ui.FindParams{
+		Role: ui.RoleTypeButton,
+		Name: "More",
+	}
+	more, err := ui.FindWithTimeout(ctx, tconn, moreParams, defaultTimeout)
+	if err != nil {
+		s.Fatal("Failed to find More Button: ", err)
+	}
+	defer more.Release(ctx)
+
+	if err := more.LeftClick(ctx); err != nil {
+		s.Fatal("Failed to click More button: ", err)
+	}
+
+	// Find the "Accept" button and click
+	acceptParams := ui.FindParams{
+		Role: ui.RoleTypeButton,
+		Name: "Accept",
+	}
+	accept, err := ui.FindWithTimeout(ctx, tconn, acceptParams, defaultTimeout)
+	if err != nil {
+		s.Fatal("Failed to find Accept Button: ", err)
+	}
+	defer accept.Release(ctx)
+
+	if err := accept.LeftClick(ctx); err != nil {
+		s.Fatal("Failed to click Accept button: ", err)
+	}
+
+	// Verify Play Store is Enabled.
+	if err = optin.WaitForPlayStoreReady(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for Play Store to be ready: ", err)
+	}
+
+}
