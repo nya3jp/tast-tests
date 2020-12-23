0237a65528d22d35d3d8378bed1355bf3e14309b
Satoshi Niwa <niwa@google.com>
2020-12-23 18:06:45 +0900

    tast: Add arc.DataMigration.p_to_r
    Core logic has been ported from cheets_DataMigrationPlayStore_P.py
    and cheets_data_migration_arc_base.py
    
    BUG=b:178335807
    BUG=b:155123165
    TEST=tast run DUT arc.DataMigration.p_to_r
    
    Change-Id: I0168000be40fbfbd405f533e65817ec88dec5704

---
+++ src/chromiumos/tast/local/bundles/cros/arc/data_migration.go
@@ -0,0 +1,163 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/playstore"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+const (
+	homeDataNamePiX86 = "data_migration_pi_x86_64"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DataMigration,
+		Desc:         "Boots ARC with /data created on the previous version of ARC and verifies Play Store can install an app",
+		Contacts:     []string{"niwa@google.com", "arc-storage@google.com", "cros-arc-te@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      10 * time.Minute,
+		Vars:         []string{"arc.DataMigration.username", "arc.DataMigration.password"},
+		Params: []testing.Param{{
+			// Launch ARC R with /data created on ARC P.
+			Name: "p_to_r",
+			// TODO(b/155123165): Add ARM support.
+			ExtraData:         []string{homeDataNamePiX86},
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+	})
+}
+
+func DataMigration(ctx context.Context, s *testing.State) {
+	const (
+		// One of the apps reported by
+		// b/173835269 "Can't download or install some apps after P->R data migration"
+		appToInstall = "com.roblox.client"
+	)
+
+	// Ensure that user is logged out before executing mountVaultWithArchivedData().
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Fatal("Failed to log out: ", err)
+	}
+
+	homeDataPath := s.DataPath(homeDataNamePiX86)
+	username := s.RequiredVar("arc.DataMigration.username")
+	password := s.RequiredVar("arc.DataMigration.password")
+	if err := mountVaultWithArchivedData(ctx, homeDataPath, username, password); err != nil {
+		s.Fatal("Failed to mount home with archived data: ", err)
+	}
+
+	args := append(arc.DisableSyncFlags(), "--disable-arc-data-wipe")
+	cr, err := chrome.New(ctx,
+		chrome.GAIALogin(), chrome.Auth(username, password, "gaia-id"), chrome.ARCSupported(),
+		chrome.KeepState(), chrome.ExtraArgs(args...))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close()
+
+	if err := checkVersionUpgradeInArcLog(ctx); err != nil {
+		s.Fatal("Failed to check version upgrade in arc.log: ", err)
+	}
+
+	d, err := a.NewUIDevice(ctx)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	defer d.Close(ctx)
+
+	s.Log("Installing app " + appToInstall)
+	if err := playstore.InstallApp(ctx, a, d, appToInstall, -1); err != nil {
+		s.Fatal("Failed to install app: ", err)
+	}
+}
+
+func mountVaultWithArchivedData(ctx context.Context, homeDataPath, username, password string) error {
+	// Unmount and mount vault for the user.
+	if err := cryptohome.UnmountVault(ctx, username); err != nil {
+		return err
+	}
+	if err := cryptohome.RemoveVault(ctx, username); err != nil {
+		return err
+	}
+	if err := cryptohome.CreateVault(ctx, username, password); err != nil {
+		return err
+	}
+
+	vaultPath, err := cryptohome.MountedVaultPath(ctx, username)
+	if err != nil {
+		return err
+	}
+
+	testing.ContextLogf(ctx, "Unarchiving home data %q under %q", homeDataPath, vaultPath)
+	if err := testexec.CommandContext(
+		ctx, "tar", "--xattrs", "--selinux", "-C", vaultPath, "-xjf", homeDataPath).Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "failed to unarchive home data under vault")
+	}
+	return nil
+}
+
+func checkVersionUpgradeInArcLog(ctx context.Context) error {
+	const (
+		arcLogPath = "/var/log/arc.log"
+	)
+
+	// SDK version of ARC running curently
+	systemVersionRegexp := regexp.MustCompile(`System SDK version: (\d+)`)
+	systemVersion := 0
+
+	// SDK version of ARC on which /data was created
+	dataVersionRegexp := regexp.MustCompile(`Data SDK version: (\d+)`)
+	dataVersion := 0
+
+	testing.ContextLogf(ctx, "Scanning %s to check SDK versions", arcLogPath)
+	out, err := testexec.CommandContext(ctx, "tail", arcLogPath, "-n", "500").Output()
+	if err != nil {
+		return errors.Wrapf(err, "failed to open %s", arcLogPath)
+	}
+	for _, l := range strings.Split(string(out), "\n") {
+		m := systemVersionRegexp.FindStringSubmatch(l)
+		if m != nil {
+			systemVersion, _ = strconv.Atoi(m[1])
+			continue
+		}
+		m = dataVersionRegexp.FindStringSubmatch(l)
+		if m != nil {
+			dataVersion, _ = strconv.Atoi(m[1])
+			continue
+		}
+	}
+	if systemVersion <= 0 {
+		return errors.Wrapf(err, "failed to get System SDK version in %s", arcLogPath)
+	}
+	if dataVersion <= 0 {
+		return errors.Wrapf(err, "failed to get Data SDK version in %s", arcLogPath)
+	}
+	testing.ContextLogf(ctx, "System SDK version: %d, Data SDK verson: %d", systemVersion, dataVersion)
+	if systemVersion <= dataVersion {
+		return errors.Wrap(err, "System SDK version should be higher than Data SDK version")
+	}
+	return nil
+}
