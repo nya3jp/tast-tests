19c2a3a4fb3dd3dcefff225e3da2db92c8869572
Maksim Ivanov <emaxx@chromium.org>
2020-12-28 23:40:49 +0100

    Add PNaCl module execution test
    Add a Tast test that verifies the basic functionality of PNaCl (Portable
    Native Client).
    
    The test loads the PNaCl binary module in the context of a Chrome App
    (since [P]NaCl has been put behind an Origin Trial on the open web). The
    test then verifies that a message can be sent to/from the module.
    
    Despite that the [p]NaCl technology has been deprecated for a long time,
    it's still used for some crucial applications and will stay so for a
    while (until the final deprecation deadline comes), therefore the new
    test should help preventing regressions in this area. This is becoming
    more important now that the NaCl's overall test coverage decreases due
    applications being slowly migrated away from it.
    
    BUG=none
    TEST=tast run $DUT nacl.Pnacl
    
    Change-Id: I63a6762fa780d30b8f1a21bf220bd0efc1085883

---
+++ src/chromiumos/tast/local/bundles/cros/nacl/pnacl.go
@@ -0,0 +1,78 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package nacl
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/fsutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+var extensionFiles = []string{
+	"chrome_nacl_app/background.js",
+	"chrome_nacl_app/manifest.json",
+	"chrome_nacl_app/nacl_module.nmf",
+	"chrome_nacl_app/nacl_module.pexe",
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Pnacl,
+		Desc:         "Tests running a PNaCl module",
+		Contacts:     []string{"emaxx@chromium.org", "fabiansommer@chromium.org"},
+		Data:         extensionFiles,
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+	})
+}
+
+func Pnacl(ctx context.Context, s *testing.State) {
+	extDir, err := ioutil.TempDir("", "tast.nacl.PNaclApp.")
+	if err != nil {
+		s.Fatal("Failed to create temp dir: ", err)
+	}
+	defer os.RemoveAll(extDir)
+
+	for _, file := range extensionFiles {
+		dst := filepath.Join(extDir, filepath.Base(file))
+		if err := fsutil.CopyFile(s.DataPath(file), dst); err != nil {
+			s.Fatalf("Failed to copy %q file to %q: %v", file, extDir, err)
+		}
+	}
+
+	extID, err := chrome.ComputeExtensionID(extDir)
+	if err != nil {
+		s.Fatalf("Failed to compute extension ID for %v: %v", extDir, err)
+	}
+
+	cr, err := chrome.New(ctx, chrome.UnpackedExtension(extDir))
+	if err != nil {
+		s.Fatal("Chrome login failed: ", err)
+	}
+	defer cr.Close(ctx)
+
+	s.Log("Connecting to background page")
+	bgURL := chrome.ExtensionBackgroundPageURL(extID)
+	conn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(bgURL))
+	if err != nil {
+		s.Fatalf("Failed to connect to background page at %v: %v", bgURL, err)
+	}
+	defer conn.Close()
+
+	s.Log("Waiting for JS test function to become available")
+	if err := conn.WaitForExpr(ctx, "runTest"); err != nil {
+		s.Fatal("JS test function unavailable: ", err)
+	}
+
+	s.Log("Executing JS test function")
+	if err := conn.Eval(ctx, "runTest()", nil); err != nil {
+		s.Fatal("Failed to call JS test function: ", err)
+	}
+}
