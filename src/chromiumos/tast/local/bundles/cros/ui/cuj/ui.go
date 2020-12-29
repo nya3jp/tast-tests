5ae0965e52e971555ebc615c4c219d6f4277026e
Jane Yang <jane.yang@cienet.com>
2020-12-29 14:56:47 +0800

    cuj: add extended display for GoogleMeet conference
    Add remote case for GoogleMeet large conference room (at least 17
    participants) with extended display.
    
    BUG=chromium:1196849
    TEST=tast run -var='ui.meet_url=$MEET_ROOM_URL' -var='chameleon=$CHAMELEON_IP' <other vars> $DUT ui.ExtendedDisplayCUJ.premium__meet_large
    
    Change-Id: Ib2df43576abcf939e02f0ec6be1de8005c8c918e

--- src/chromiumos/tast/local/bundles/cros/ui/cuj/ui.go
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/ui.go
@@ -15,6 +15,8 @@ import (
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/chrome/uiauto/touch"
 	"chromiumos/tast/testing"
 )
@@ -92,9 +94,13 @@ func CloseBrowserTabs(ctx context.Context, tconn *chrome.TestConn) error {
 // Which it is also support multiple names for a single app (e.g. "Chrome"||"Chromium" for Google Chrome, the browser).
 // It returns the time when a mouse click event is injected to the app icon.
 func LaunchAppFromShelf(ctx context.Context, tconn *chrome.TestConn, appName string, appOtherPossibleNames ...string) (time.Time, error) {
-	params := nodewith.Name(appName).ClassName("ash/ShelfAppButton")
+	// If the extended display is enabled, the icon will appear on both internal and external display,
+	// Find node on internal display.
+	nativeDisplay := nodewith.ClassName("RootWindow-0").Role(role.Window)
+
+	params := nodewith.Name(appName).ClassName("ash/ShelfAppButton").Ancestor(nativeDisplay)
 	if len(appOtherPossibleNames) > 0 {
-		params = paramsOfAppNames(append(appOtherPossibleNames, appName))
+		params = paramsOfAppNames(append(appOtherPossibleNames, appName)).Ancestor(nativeDisplay)
 	}
 
 	ac := uiauto.New(tconn)
@@ -137,9 +143,13 @@ func LaunchAppFromHotseat(ctx context.Context, tconn *chrome.TestConn, appName s
 		return startTime, errors.Wrap(err, "failed to show hotseat")
 	}
 
-	params := nodewith.Name(appName).ClassName("ash/ShelfAppButton")
+	// If the extended display is enabled, the icon will appear on both internal and external display,
+	// Find node on internal display.
+	nativeDisplay := nodewith.ClassName("RootWindow-0").Role(role.Window)
+
+	params := nodewith.Name(appName).ClassName("ash/ShelfAppButton").Ancestor(nativeDisplay)
 	if len(appOtherPossibleNames) > 0 {
-		params = paramsOfAppNames(append(appOtherPossibleNames, appName))
+		params = paramsOfAppNames(append(appOtherPossibleNames, appName)).Ancestor(nativeDisplay)
 	}
 
 	ac := uiauto.New(tconn)
@@ -167,4 +177,38 @@ func paramsOfAppNames(names []string) *nodewith.Finder {
 	pattern += ")"
 
 	return nodewith.NameRegex(regexp.MustCompile(pattern)).ClassName("ash/ShelfAppButton")
+}
+
+// UnsetMirrorDisplay unsets the mirror display settings.
+func UnsetMirrorDisplay(ctx context.Context, tconn *chrome.TestConn) error {
+	ui := uiauto.New(tconn)
+
+	testing.ContextLog(ctx, "Launch os-settings to disable mirror")
+	settings, err := ossettings.LaunchAtPage(ctx, tconn, nodewith.Name("Device").Role(role.Link))
+	if err != nil {
+		return errors.Wrap(err, "failed to launch os-settings Device page")
+	}
+
+	displayFinder := nodewith.Name("Displays").Role(role.Link).Ancestor(ossettings.WindowFinder)
+	if err := ui.LeftClick(displayFinder)(ctx); err != nil {
+		return errors.Wrap(err, "failed to launch display page")
+	}
+
+	mirrorFinder := nodewith.Name("Mirror Built-in display").Role(role.CheckBox).Ancestor(ossettings.WindowFinder)
+	// Find the node info for the mirror checkbox
+	nodeInfo, err := ui.Info(ctx, mirrorFinder)
+	if err != nil {
+		return errors.Wrap(err, "failed to get info for the mirror checkbox")
+	}
+	if nodeInfo.Checked == "true" {
+		testing.ContextLog(ctx, "Click 'Mirror Built-in display' checkbox")
+		if err := ui.LeftClick(mirrorFinder)(ctx); err != nil {
+			return errors.Wrap(err, "failed to click mirror display")
+		}
+	}
+
+	if err := settings.Close(ctx); err != nil {
+		return errors.Wrap(err, "failed to close settings")
+	}
+	return nil
 }
