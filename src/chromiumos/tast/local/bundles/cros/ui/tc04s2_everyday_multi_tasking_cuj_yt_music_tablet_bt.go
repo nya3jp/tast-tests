22696bc65d7d4d300a4d91284817370c840e7701
Jane Yang <jane.yang@cienet.com>
2020-12-29 14:56:28 +0800

    add TC04S2EverydayMultiTaskingCUJYtMusicTabletBT case
    BUG=None
    TEST=tast run -var=ui.cuj_username=$ACCOUNT -var=ui.cuj_password=$PASSWORD -var=ui.bt_devicename=$DEVNAME $DUT ui.TC04S2EverydayMultiTaskingCUJYtMusicTabletBT
    
    Change-Id: I74145dfb4c42cbd18bcd01f8e6fbcfef82b884e8

---
+++ src/chromiumos/tast/local/bundles/cros/ui/tc04s2_everyday_multi_tasking_cuj_yt_music_tablet_bt.go
@@ -0,0 +1,48 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
+	et "chromiumos/tast/local/bundles/cros/ui/everydaymultitaskingcuj"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TC04S2EverydayMultiTaskingCUJYtMusicTabletBT,
+		Desc:         "Measures the performance of everyday multi-tasking CUJ",
+		Contacts:     []string{"mukai@chromium.org", "tclaiborne@chromium.org", "jane.yang@cienet.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_nightly"},
+		SoftwareDeps: []string{"android_p", "chrome"},
+		HardwareDeps: hwdep.D(hwdep.InternalDisplay(), hwdep.TouchScreen()),
+		Timeout:      15 * time.Minute,
+		Vars: []string{
+			"mute",
+			"ui.cuj_username",
+			"ui.cuj_password",
+			"ui.bt_devicename",
+		},
+		Pre: cuj.LoginKeepState(),
+		Params: []testing.Param{
+			{
+				ExtraSoftwareDeps: []string{"tablet_mode"},
+				Val: et.TestParameters{
+					TabletMode: true,
+					Bluetooth:  true,
+				},
+			},
+		},
+		Data: []string{"cca_ui.js"},
+	})
+}
+
+func TC04S2EverydayMultiTaskingCUJYtMusicTabletBT(ctx context.Context, s *testing.State) {
+	et.Run(ctx, s, et.YoutubeMusic)
+}
