8d25f879d71cf4c48e2fefbbedcfdd00e5b05a9b
Tim Chang <tim.chang@cienet.com>
2020-12-29 14:56:47 +0800

    cuj: add video cuj (YT App implementation)
    Video CUJ plays video in fullscreen, opens gmail browser, switches back to
    video and verifies pause and resume of video playback.
    The test scenario designed in this CL is:
        -Youtube app video
    
    This CL provides Youtube APP implementation details.
    
    BUG=chromium:1196849
    TEST=tast run $DUT ui.VideoCUJ2.*
    
    Change-Id: I25a038c51fec9180a5e782df5542767ce3510124

--- src/chromiumos/tast/local/bundles/cros/ui/videocuj/youtube_app.go
+++ src/chromiumos/tast/local/bundles/cros/ui/videocuj/youtube_app.go
@@ -6,11 +6,16 @@ package videocuj
 
 import (
 	"context"
+	"strings"
 	"time"
 
+	"chromiumos/tast/errors"
 	androidui "chromiumos/tast/local/android/ui"
 	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
@@ -50,29 +55,234 @@ func NewYtApp(tconn *chrome.TestConn, kb *input.KeyboardEventWriter, a *arc.ARC,
 func (y *YtApp) OpenAndPlayVideo(ctx context.Context) (err error) {
 	testing.ContextLog(ctx, "Open Youtube app")
 
-	// TODO: provide implementation details.
+	const (
+		youtubeApp              = "Youtube App"
+		youtubeAct              = "com.google.android.apps.youtube.app.WatchWhileActivity"
+		accountImageID          = "com.google.android.youtube:id/image"
+		searchButtonID          = "com.google.android.youtube:id/menu_item_1"
+		searchEditTextID        = "com.google.android.youtube:id/search_edit_text"
+		moreOptions             = "com.google.android.youtube:id/player_overflow_button"
+		noThanksID              = "com.google.android.youtube:id/dismiss"
+		accountImageDescription = "Account"
+	)
+
+	if appStartTime, y.act, err = cuj.OpenAppAndGetStartTime(ctx, y.tconn, y.a, youtubePkg, youtubeApp, youtubeAct); err != nil {
+		return errors.Wrap(err, "failed to get app start time")
+	}
+
+	accountImage := y.d.Object(androidui.ID(accountImageID), androidui.DescriptionContains(accountImageDescription))
+	if err := accountImage.WaitForExists(ctx, uiWaitTime); err != nil {
+		return errors.Wrap(err, "failed to check for Youtube app launched")
+	}
+
+	// Clear notification prompt if it exists.
+	noThanksEle := y.d.Object(androidui.ID(noThanksID), androidui.Text("NO THANKS"))
+	if err := cuj.ClickIfExist(ctx, noThanksEle, 5*time.Second); err != nil {
+		return errors.Wrap(err, `failed to click "NO THANKS" to clear notification prompt`)
+	}
+
+	playVideo := func() error {
+		testing.ContextLog(ctx, "Search and play video")
+
+		searchButton := y.d.Object(androidui.ID(searchButtonID))
+		if err := searchButton.Click(ctx); err != nil {
+			return err
+		}
+
+		searchEditText := y.d.Object(androidui.ID(searchEditTextID))
+		if err := cuj.FindAndClick(ctx, searchEditText, uiWaitTime); err != nil {
+			return errors.Wrap(err, `failed to find "searchTextfield"`)
+		}
+
+		if err := uiauto.Combine("type video url",
+			y.kb.TypeAction(y.video.url),
+			y.kb.AccelAction("enter"),
+		)(ctx); err != nil {
+			return err
+		}
+
+		firstVideo := y.d.Object(androidui.ClassName("android.view.ViewGroup"), androidui.Index(1))
+		startTime := time.Now()
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+
+			if err := cuj.FindAndClick(ctx, firstVideo, uiWaitTime); err != nil {
+				if strings.Contains(err.Error(), "click") {
+					return testing.PollBreak(err)
+				}
+				return errors.Wrap(err, `failed to find "First Video"`)
+			}
+
+			testing.ContextLogf(ctx, "Elapsed time when waiting the video list: %.3f s", time.Since(startTime).Seconds())
+			return nil
+		}, &testing.PollOptions{Interval: 3 * time.Second, Timeout: 30 * time.Second}); err != nil {
+			return errors.Wrap(err, "failed to click first video")
+		}
+		return nil
+	}
+
+	// Switch quality is a continuous action.
+	// Dut to the different response time of DUTs.
+	// We need to combine these actions in Poll to make switch quality works smoothly.
+	switchQuality := func(resolution string) error {
+		testing.ContextLogf(ctx, "Switch Quality to %q", resolution)
+		startTime := time.Now()
+		if err := testing.Poll(ctx, func(context.Context) error {
+			playerView := y.d.Object(androidui.ID(playerViewID))
+			if err := cuj.FindAndClick(ctx, playerView, uiWaitTime); err != nil {
+				return errors.Wrap(err, "failed to find/click the player view on switch quality")
+			}
+
+			moreBtn := y.d.Object(androidui.ID(moreOptions))
+			if err := cuj.FindAndClick(ctx, moreBtn, waitTimeAfterClickPlayerView); err != nil {
+				return errors.Wrap(err, `failed to find/click the "More options"`)
+			}
+
+			qualityBtn := y.d.Object(androidui.Text("Quality"))
+			if err := cuj.FindAndClick(ctx, qualityBtn, uiWaitTime); err != nil {
+				return errors.Wrap(err, "failed to find/click the Quality")
+			}
+
+			advancedBtn := y.d.Object(androidui.Text("Advanced"))
+			if err := cuj.FindAndClick(ctx, advancedBtn, uiWaitTime); err != nil {
+				return errors.Wrap(err, "failed to find/click the advanced option")
+			}
+
+			targetQuality := y.d.Object(androidui.Text(resolution), androidui.ID(qualityListItemID))
+			if err := targetQuality.WaitForExists(ctx, 2*time.Second); err != nil {
+				return errors.Wrap(err, "failed to find the target quality")
+			}
+
+			// Immediately clicking the target button sometimes doesn't work.
+			if err := testing.Sleep(ctx, time.Second); err != nil {
+				return errors.Wrap(err, "failed to sleep and wait before click resolution")
+			}
+			if err := targetQuality.Click(ctx); err != nil {
+				return errors.Wrap(err, "failed to click the target quality")
+			}
+
+			testing.ContextLogf(ctx, "Elapsed time when switching quality: %.3f s", time.Since(startTime).Seconds())
+			return nil
+		}, &testing.PollOptions{Interval: time.Second, Timeout: time.Minute}); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := playVideo(); err != nil {
+		return errors.Wrap(err, "failed to play video")
+	}
+
+	if err := switchQuality(y.video.quality); err != nil {
+		return errors.Wrap(err, "failed to switch Quality")
+	}
+
 	return nil
 }
 
-func checkYoutubeAppPIP(ctx context.Context, tconn *chrome.TestConn) error {
-	// TODO: provide implementation details.
-	return nil
+func (y *YtApp) checkYoutubeAppPIP(ctx context.Context) error {
+	testing.ContextLog(ctx, "Check window state should be PIP")
+	startTime := time.Now()
+
+	ws, err := ash.GetARCAppWindowState(ctx, y.tconn, youtubePkg)
+	if err != nil {
+		return errors.Wrap(err, "can not get ARC App Window State")
+	}
+	if ws == ash.WindowStatePIP {
+		testing.ContextLogf(ctx, "Elapsed time when checking PIP mode: %.3f s", time.Since(startTime).Seconds())
+		return nil
+	}
+
+	waitForPipMode := func(ctx context.Context) error {
+		return ash.WaitForARCAppWindowState(ctx, y.tconn, youtubePkg, ash.WindowStatePIP)
+	}
+
+	// Checking PIP mode sometimes doesn't work (e.g. if chrome window is not in fullscreen),
+	// retry a few times to enable PIP mode.
+	return uiauto.Retry(3,
+		uiauto.Combine("change to pip mode",
+			y.kb.AccelAction("Alt+="),
+			waitForPipMode,
+		),
+	)(ctx)
 }
 
 // EnterFullscreen switches youtube video to fullscreen.
 func (y *YtApp) EnterFullscreen(ctx context.Context) error {
 	testing.ContextLog(ctx, "Make Youtube app fullscreen")
 
-	// TODO: provide implementation details.
-	return nil
+	const fullscreenBtnID = "com.google.android.youtube:id/fullscreen_button"
+
+	playerView := y.d.Object(androidui.ID(playerViewID))
+
+	startTime := time.Now()
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := cuj.FindAndClick(ctx, playerView, uiWaitTime); err != nil {
+			return errors.Wrap(err, "failed to find/click the player view")
+		}
+
+		fsBtn := y.d.Object(androidui.ID(fullscreenBtnID))
+		if err := cuj.FindAndClick(ctx, fsBtn, waitTimeAfterClickPlayerView); err != nil {
+			return errors.Wrap(err, "failed to find/click the fullscreen button")
+		}
+
+		testing.ContextLogf(ctx, "Elapsed time when doing enter fullscreen %.3f s", time.Since(startTime).Seconds())
+		return nil
+	}, &testing.PollOptions{Interval: time.Second, Timeout: 30 * time.Second}); err != nil {
+		return err
+	}
+
+	return ash.WaitForARCAppWindowState(ctx, y.tconn, youtubePkg, ash.WindowStateFullscreen)
 }
 
 // PauseAndPlayVideo verifies video playback on youtube app.
 func (y *YtApp) PauseAndPlayVideo(ctx context.Context) error {
 	testing.ContextLog(ctx, "Pause and play video")
 
-	// TODO: provide implementation details.
-	return nil
+	const (
+		playPauseBtnID = "com.google.android.youtube:id/player_control_play_pause_replay_button"
+		playBtnDesc    = "Play video"
+		pauseBtnDesc   = "Pause video"
+		sleepTime      = 3 * time.Second
+	)
+
+	playerView := y.d.Object(androidui.ID(playerViewID))
+	pauseBtn := y.d.Object(androidui.ID(playPauseBtnID), androidui.Description(pauseBtnDesc))
+	playBtn := y.d.Object(androidui.ID(playPauseBtnID), androidui.Description(playBtnDesc))
+
+	startTime := time.Now()
+	return testing.Poll(ctx, func(ctx context.Context) error {
+
+		if err := cuj.FindAndClick(ctx, playerView, uiWaitTime); err != nil {
+			return errors.Wrap(err, "failed to find/click the player view")
+		}
+
+		if err := cuj.FindAndClick(ctx, pauseBtn, waitTimeAfterClickPlayerView); err != nil {
+			return errors.Wrap(err, "failed to find/click the pause button")
+		}
+
+		if err := playBtn.WaitForExists(ctx, 2*time.Second); err != nil {
+			return errors.Wrap(err, "failed to find the play button")
+		}
+
+		// Immediately clicking the target button sometimes doesn't work.
+		if err := testing.Sleep(ctx, sleepTime); err != nil {
+			return errors.Wrap(err, "failed to sleep before clicking play button")
+		}
+		if err := playBtn.Click(ctx); err != nil {
+			return errors.Wrap(err, "failed to click the play button")
+		}
+		if err := pauseBtn.WaitForExists(ctx, uiWaitTime); err != nil {
+			return errors.Wrap(err, "failed to find the pause button")
+		}
+
+		// Keep the video playing for a short time.
+		if err := testing.Sleep(ctx, sleepTime); err != nil {
+			return errors.Wrap(err, "failed to sleep while video is playing")
+		}
+
+		testing.ContextLogf(ctx, "Elapsed time when checking the playback status of youtube app: %.3f s", time.Since(startTime).Seconds())
+		return nil
+	}, &testing.PollOptions{Interval: time.Second, Timeout: 2 * time.Minute})
 }
 
 // Close closes the resources related to video.
