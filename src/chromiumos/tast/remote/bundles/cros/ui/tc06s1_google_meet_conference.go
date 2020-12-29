ebbf6c56ca1c4b6930a2ed2e7f1978855f49ec47
Septem Li <septemli@cienet.com>
2020-12-29 14:59:30 +0800

    cuj: add video conference related cases, includes Google Meet and Zoom
    add TC06S1GoogleMeetConference case
    add TC06T1GoogleMeetConference case
    add TC08S1ZoomVideoChat case
    add TC08T1ZoomVideoChat case
    
    Cases with TCxxSy prefix are designed for standard non-touch models, and
    the ones with TCxxTy prefix are for tablets.
    
    Video chat cases are designed as remote tests, considering there are
    possibilities to manipulate multiple DUTs and manage external test
    servers.
    
    BUG=None
    TEST=tast run -var=ui.cuj_username=$ACCOUNT -var=ui.cuj_password=$PASSWORD -var=ui.meet_account=$MEET_ACCOUNT -var=ui.meet_url=$MEET_URL $DUT ui.TC06S1GoogleMeetConference
    
    Change-Id: Id80cd003c82e8a5e901fdb86a92a4c3c31dce593

---
+++ src/chromiumos/tast/remote/bundles/cros/ui/tc06s1_google_meet_conference.go
@@ -0,0 +1,32 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/remote/bundles/cros/ui/conference"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TC06S1GoogleMeetConference,
+		Desc:         "Using Google Meet host a conference and presentation with participants",
+		Contacts:     []string{"jane.yang@cienet.com"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		ServiceDeps: []string{
+			"tast.cros.ui.GoogleMeetService",
+		},
+		Vars:    []string{"ui.cuj_username", "ui.cuj_password", "ui.meet_url", "perf_level"},
+		Timeout: time.Minute * 10,
+	})
+}
+
+func TC06S1GoogleMeetConference(ctx context.Context, s *testing.State) {
+	perfLevel := s.RequiredVar("perf_level")
+	conference.Run(ctx, s, conference.GoogleMeet, perfLevel, conference.OneRoomSize)
+}
