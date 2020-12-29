e33cd74df1350f30130263b16ac1ba353fb1c5cc
Jane Yang <jane.yang@cienet.com>
2020-12-29 14:59:37 +0800

    cuj: add everyday multi tasking cuj
    Everyday multi tasking CUJ plays music, opens browsers, cycle through tabs.
    Three test cases are designed:
    Test case 3.1 (TC03S1): Play YouTube Music
    Test case 3.2 (TC03S2): Play YouTube Music through BT speaker
    Test case 3.3 (TC03S3): Play Spotify Music through BT speaker
    
    For each test case, there is a non-touch version TCxxSy and a touch
    version TCxxTy.
    
    BUG=None
    TEST=tast run $DUT ui.TC03S1EverydayMultiTaskingCUJYtMusic
    TEST=tast run $DUT ui.TC03S2EverydayMultiTaskingCUJYtMusicBT
    TEST=tast run $DUT ui.TC03S3EverydayMultiTaskingCUJSpotifyBT
    TEST=tast run $DUT ui.TC03T1EverydayMultiTaskingCUJYtMusic
    TEST=tast run $DUT ui.TC03T2EverydayMultiTaskingCUJYtMusicBT
    TEST=tast run $DUT ui.TC03T3EverydayMultiTaskingCUJSpotifyBT
    
    Change-Id: Iae4eaa689a843e3b0bb22b2e76f105502e9f4d22

---
+++ src/chromiumos/tast/local/bundles/cros/ui/tc03t1_everyday_multi_tasking_cuj_yt_music.go
@@ -0,0 +1,43 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
+	et "chromiumos/tast/local/bundles/cros/ui/everydaymultitaskingcuj"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TC03T1EverydayMultiTaskingCUJYtMusic,
+		Desc:         "Measures the performance of everyday multi-tasking CUJ",
+		Contacts:     []string{"mukai@chromium.org", "tclaiborne@chromium.org", "jane.yang@cienet.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_nightly"},
+		SoftwareDeps: []string{"android_p", "chrome", "tablet_mode"},
+		HardwareDeps: hwdep.D(hwdep.InternalDisplay(), hwdep.TouchScreen()),
+		Timeout:      15 * time.Minute,
+		Vars: []string{
+			"mute",
+			"perf_level",
+		},
+		Fixture: "loggedInToCUJUserKeepState",
+		Data:    []string{"cca_ui.js"},
+	})
+}
+
+func TC03T1EverydayMultiTaskingCUJYtMusic(ctx context.Context, s *testing.State) {
+	const (
+		tabletMode    = true
+		openBluetooth = false
+	)
+	cr := s.FixtValue().(cuj.FixtureData).Chrome
+	a := s.FixtValue().(cuj.FixtureData).ARC
+	et.Run(ctx, s, cr, a, et.YoutubeMusic, tabletMode, openBluetooth)
+}
