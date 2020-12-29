55b8c253d91917228f3bc127908c5124eae82ed0
Septem Li <septemli@cienet.com>
2020-12-29 14:56:47 +0800

    cuj: add video cuj
    Video CUJ plays video in fullscreen, opens gmail browser, switches back to
    video and verifies pause and resume of video playback.
    Three test cases are designed:
    Test case 5.1 (TC05S1): Play YouTube web video
    Test case 5.2 (TC05S2): Play Netflix web video
    Test case 5.3 (TC05S3): Play Youtube app video
    
    For each test case, there is a non-touch version TCxxSy and a touch
    version TCxxTy.
    
    BUG=None
    TEST=tast run $DUT ui.TC05S1YoutubeWebVideoCUJ
    TEST=tast run $DUT ui.TC05S2NetflixVideoCUJ
    TEST=tast run $DUT ui.TC05S3YoutubeAppVideoCUJ
    TEST=tast run $DUT ui.TC05T1YoutubeWebVideoCUJ
    TEST=tast run $DUT ui.TC05T2NetflixVideoCUJ
    TEST=tast run $DUT ui.TC05T3YoutubeAppVideoCUJ
    
    Change-Id: Ia4b56a3b1d883ee8212373f6bc952f2cea2a4d82

---
+++ src/chromiumos/tast/local/bundles/cros/ui/tc05s1_youtube_web_video_cuj.go
@@ -0,0 +1,37 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
+	"chromiumos/tast/local/bundles/cros/ui/videocuj"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TC05S1YoutubeWebVideoCUJ,
+		Desc:         "Measures the smoothess of switch between full screen YouTube video and another browser window",
+		Contacts:     []string{"xiyuan@chromium.org", "chromeos-wmp@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_nightly"},
+		SoftwareDeps: []string{"chrome", "arc"},
+		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+		Timeout:      6 * time.Minute,
+		Fixture:      "loggedInToCUJUserKeepState",
+		Vars: []string{
+			"perf_level",
+		},
+	})
+}
+
+func TC05S1YoutubeWebVideoCUJ(ctx context.Context, s *testing.State) {
+	const tabletMode = false
+	cr := s.FixtValue().(cuj.FixtureData).Chrome
+	videocuj.Run(ctx, s, cr, nil, videocuj.YoutubeWeb, tabletMode)
+}
