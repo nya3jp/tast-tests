f85fc71653de798695c070b926e41bdedc82023e
Septem Li <septemli@cienet.com>
2020-12-29 14:56:47 +0800

    cuj: add video cuj
    Video CUJ plays video in fullscreen, opens gmail browser, switches back to
    video and verifies pause and resume of video playback.
    Three test scenarios are designed:
    -YouTube web video
    -Netflix web video
    -Youtube app video
    
    For each test case, there is a clamshell version and a tablet version.
    
    BUG=None
    TEST=tast run $DUT ui.VideoCUJ2.*
    
    Change-Id: Ia4b56a3b1d883ee8212373f6bc952f2cea2a4d82

---
+++ src/chromiumos/tast/local/bundles/cros/ui/video_cuj2_test.go
@@ -0,0 +1,58 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+// Refer to cuj/test_params.go for the details of how to use this unit test.
+
+import (
+	"testing"
+	"time"
+
+	"chromiumos/tast/common/genparams"
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
+)
+
+func TestVideoCUJ2Params(t *testing.T) {
+	// <tier>_<screen model>_<scenario>_<app name>_<app type>_<bluetooth>
+	params := []cuj.TestParam{
+		cuj.TestParam{
+			Tier:            cuj.Basic,
+			ApplicationName: "youtube",
+			ApplicationType: cuj.Web,
+			Timeout:         10 * time.Minute,
+		},
+		cuj.TestParam{
+			Tier:            cuj.Plus,
+			ApplicationName: "youtube",
+			ApplicationType: cuj.Web,
+			Timeout:         10 * time.Minute,
+		},
+		cuj.TestParam{
+			Tier:            cuj.Basic,
+			ApplicationName: "netflix",
+			ApplicationType: cuj.Web,
+			Timeout:         10 * time.Minute,
+		},
+		cuj.TestParam{
+			Tier:            cuj.Plus,
+			ApplicationName: "netflix",
+			ApplicationType: cuj.Web,
+			Timeout:         10 * time.Minute,
+		},
+		cuj.TestParam{
+			Tier:            cuj.Basic,
+			ApplicationName: "youtube",
+			ApplicationType: cuj.APP,
+			Timeout:         10 * time.Minute,
+		},
+		cuj.TestParam{
+			Tier:            cuj.Plus,
+			ApplicationName: "youtube",
+			ApplicationType: cuj.APP,
+			Timeout:         10 * time.Minute,
+		},
+	}
+	genparams.Ensure(t, "video_cuj2.go", cuj.MakeCUJCaseParam(t, params))
+}
