9a912d0781577df4c4dd051fe13b967eb9cab7a1
Jane Yang <jane.yang@cienet.com>
2020-12-29 14:57:53 +0800

    cuj: add tab switch CUI test cases
    add tab_switch_cuj2.go. It is assumed that an external WPR server needs
    to be set up with the WPR address provided to the tests via runtime
    variables.
    
    BUG=None
    TEST=tast run -build=false -var=ui.cuj_username=$ACCOUNT -var=ui.cuj_password=$PASSWORD -var=wpr_http_addr=$WPR_ADDR -var=wpr_https_addr=$WPR_ADDR ui.TabSwitchCUJ2.*
    
    Change-Id: Id34c226e2d3f89ef1cf6dc83377bf187cbf9f1e4

---
+++ src/chromiumos/tast/local/bundles/cros/ui/tab_switch_cuj2_test.go
@@ -0,0 +1,33 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+// Refer to cuj/test_params.go for the details of how to use this unit test.
+
+import (
+	"testing"
+	"time"
+
+	"chromiumos/tast/common/genparams"
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
+)
+
+func TestTabSwitchCUJ2Params(t *testing.T) {
+	params := []cuj.TestParam{
+		cuj.TestParam{
+			Tier:    cuj.Basic,
+			Timeout: 30 * time.Minute,
+		},
+		cuj.TestParam{
+			Tier:    cuj.Plus,
+			Timeout: 35 * time.Minute,
+		},
+		cuj.TestParam{
+			Tier:    cuj.Premium,
+			Timeout: 40 * time.Minute,
+		},
+	}
+	genparams.Ensure(t, "tab_switch_cuj2.go", cuj.MakeCUJCaseParam(t, params))
+}
