b9c740e6a4050a6cb09651c50d68dd309f31a237
Matthew Warton <mwarton@google.com>
2020-12-31 09:38:07 +1100

    multivm: Add Crostini precondition
    Mostly based off of crrev.com/c/2573718 by cwd@
    
    Main change to get it working was to split tests into arm and amd64
    architectures, and add the appropriate external data files.
    
    BUG=b:154785124
    TEST=run multivm.Login.crostini_amd64 on hatch
    
    Change-Id: Icc77bcde2466b87d31d48b14e6359c378c759087

--- src/chromiumos/tast/local/bundles/cros/multivm/login.go
+++ src/chromiumos/tast/local/bundles/cros/multivm/login.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"time"
 
+	"chromiumos/tast/local/crostini"
 	"chromiumos/tast/local/multivm"
 	"chromiumos/tast/testing"
 )
@@ -24,9 +25,33 @@ func init() {
 			Name: "novm",
 			Pre:  multivm.NoVMStarted(),
 		}, {
+			Name:              "arc_crostini_amd64",
+			Pre:               multivm.ArcCrostiniStarted(),
+			ExtraData:         []string{"crostini_vm_amd64.zip", "crostini_test_container_metadata_buster_amd64.tar.xz", "crostini_test_container_rootfs_buster_amd64.tar.xz"},
+			ExtraHardwareDeps: crostini.CrostiniStable,
+			ExtraSoftwareDeps: []string{"vm_host", "android_vm", "amd64"},
+		}, {
+			Name:              "arc_crostini_arm",
+			Pre:               multivm.ArcCrostiniStarted(),
+			ExtraData:         []string{"crostini_vm_arm.zip", "crostini_test_container_metadata_buster_arm.tar.xz", "crostini_test_container_rootfs_buster_arm.tar.xz"},
+			ExtraHardwareDeps: crostini.CrostiniStable,
+			ExtraSoftwareDeps: []string{"vm_host", "android_vm", "arm"},
+		}, {
 			Name:              "arc",
 			Pre:               multivm.ArcStarted(),
 			ExtraSoftwareDeps: []string{"android_vm"},
+		}, {
+			Name:              "crostini_amd64",
+			Pre:               multivm.CrostiniStarted(),
+			ExtraData:         []string{"crostini_vm_amd64.zip", "crostini_test_container_metadata_buster_amd64.tar.xz", "crostini_test_container_rootfs_buster_amd64.tar.xz"},
+			ExtraHardwareDeps: crostini.CrostiniStable,
+			ExtraSoftwareDeps: []string{"vm_host", "amd64"},
+		}, {
+			Name:              "crostini_arm",
+			Pre:               multivm.CrostiniStarted(),
+			ExtraData:         []string{"crostini_vm_arm.zip", "crostini_test_container_metadata_buster_arm.tar.xz", "crostini_test_container_rootfs_buster_arm.tar.xz"},
+			ExtraHardwareDeps: crostini.CrostiniStable,
+			ExtraSoftwareDeps: []string{"vm_host", "arm"},
 		}},
 	})
 }
@@ -47,6 +72,12 @@ func Login(ctx context.Context, s *testing.State) {
 
 		if _, ok := pkgs["android"]; !ok {
 			s.Fatal("Android package not found: ", pkgs)
+		}
+	}
+
+	if pre.Crostini != nil {
+		if err := crostini.BasicCommandWorks(ctx, pre.Crostini); err != nil {
+			s.Fatal("Crostini basic commands don't work: ", err)
 		}
 	}
 }
