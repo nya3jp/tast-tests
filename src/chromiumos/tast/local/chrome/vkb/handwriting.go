85453731f2c608d85a5ac7065059c22bee22f90e
Hyun Shim <hyunshim@google.com>
2020-12-30 23:44:40 +0000

    tast-tests: add handwriting input test functionality
    1) Add new test VirtualKeyboardHandwriting with English, Chinese, and Japanese parameterized tests.
    
    2) Add multiple functions in vkb to switch between handwriting and keyboard inputs, and other supporting functions to help set up the environment for each new test case.
    
    3) Add function in vkb to read in mouse trajectories that form the handwritten text from data file, transform the points into the correct scale, and populate the data into the struct.
    
    4) Add function in vkb to draw the mouse trajectories into the handwriting input.
    
    5) Separate handwriting only related functions into a separate file for vkb.
    
    BUG=chromium:1162513
    TEST=inputs.VirtualKeyboardHandwriting*
    
    Change-Id: I8e47fdd5a7cbe8bcb00b657ea986ae92eca56e3d

---
+++ src/chromiumos/tast/local/chrome/vkb/handwriting.go
@@ -0,0 +1,139 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package vkb
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/ui/mouse"
+	"chromiumos/tast/local/coords"
+	"chromiumos/tast/testing"
+)
+
+// point is a single coordinate on the canvas.
+type point struct {
+	x float64
+	y float64
+}
+
+// path contains an array of Points that form a stroke.
+type path struct {
+	points []point
+}
+
+// pathGroup contains an array of strokes that form the expectedText.
+type pathGroup struct {
+	width  float64
+	height float64
+	paths  []path
+}
+
+// findHandwritingCanvas finds the canvas for the handwriting input which will be used to draw the handwriting.
+func findHandwritingCanvas(ctx context.Context, tconn *chrome.TestConn) (*ui.Node, error) {
+	params := ui.FindParams{
+		Role:      ui.RoleTypeCanvas,
+		ClassName: "ita-hwt-canvas",
+	}
+	opts := testing.PollOptions{Timeout: 2 * time.Second, Interval: 200 * time.Millisecond}
+	return ui.StableFind(ctx, tconn, params, &opts)
+}
+
+// toCoord converts a point in float64 format into a coordinate in int format.
+func toCoord(_point point) coords.Point {
+	// 0.5 is added to both x and y values as golang discards the fraction when converting from float to int.
+	// Adding 0.5 will allow values with fractions over 0.5 to round up instead.
+	return coords.Point{int(_point.x + 0.5), int(_point.y + 0.5)}
+}
+
+// ReadFileAndDrawHandwriting reads the handwriting file, transforms the Ppints into the correct scale,
+// populates the data into the struct, and draws the strokes into the handwriting input.
+func ReadFileAndDrawHandwriting(ctx context.Context, tconn *chrome.TestConn, filePath string) error {
+	_pathGroup := &pathGroup{}
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return errors.Wrap(err, "failed to read handwriting file")
+	}
+
+	// Create a scanner to scan through each line of the input file.
+	scanner := bufio.NewScanner(strings.NewReader(string(file)))
+
+	// Read in the width and height located in the first line of the input file.
+	scanner.Scan()
+	lineReader := strings.NewReader(scanner.Text())
+	_, err = fmt.Fscanf(lineReader, "%f%f", &_pathGroup.width, &_pathGroup.height)
+	if err != nil {
+		return errors.Wrap(err, "failed to read canvas width and height from handwriting file")
+	}
+
+	// Find the handwriting canvas location.
+	canvas, err := findHandwritingCanvas(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to find handwriting canvas")
+	}
+	canvasLocation := canvas.Location
+
+	// Scale the width and height of PathGroup according to the handwriting canvas size.
+	scale := math.Min(float64(canvasLocation.Width)*0.6/_pathGroup.width, float64(canvasLocation.Height)*0.6/_pathGroup.height)
+	_pathGroup.width *= scale
+	_pathGroup.height *= scale
+
+	// Initialise offset values for width and height so that the Points are within the canvas.
+	widthOffset := float64(canvasLocation.Left) + (float64(canvasLocation.Width)-_pathGroup.width)/2.0
+	heightOffset := float64(canvasLocation.Top) + (float64(canvasLocation.Height)-_pathGroup.height)/2.0
+
+	// Process the rest of the lines to populate the Points into the Paths.
+	for scanner.Scan() {
+		var _point point
+		var _path path
+
+		// Read in the current line and populate a single Path.
+		lineReader := strings.NewReader(scanner.Text())
+		for {
+			_, err = fmt.Fscanf(lineReader, "%f%f", &_point.x, &_point.y)
+			if err != nil {
+				break
+			}
+			// Points are scaled as they should all be contained within the canvas.
+			_point.x = _point.x*scale + widthOffset
+			_point.y = _point.y*scale + heightOffset
+			_path.points = append(_path.points, _point)
+		}
+		if len(_path.points) > 0 {
+			_pathGroup.paths = append(_pathGroup.paths, _path)
+		}
+	}
+
+	// Draw the strokes into the handwriting input.
+	for _, _path := range _pathGroup.paths {
+		_points := _path.points
+		for i, _point := range _points {
+			// Mouse will be moved to each of the Points to draw the stroke.
+			if err := mouse.Move(ctx, tconn, toCoord(_point), 30*time.Millisecond); err != nil {
+				return errors.Wrap(err, "failed to move mouse")
+			}
+			// Left mouse button should be pressed on only the first Point of every Path to start the stroke.
+			if i == 0 {
+				if err := mouse.Press(ctx, tconn, mouse.LeftButton); err != nil {
+					return errors.Wrap(err, "failed to click mouse")
+				}
+			}
+		}
+		// After going through all the Points for a single stroke, release the left mouse button.
+		if err := mouse.Release(ctx, tconn, mouse.LeftButton); err != nil {
+			return errors.Wrap(err, "failed to release mouse")
+		}
+	}
+
+	return nil
+}
