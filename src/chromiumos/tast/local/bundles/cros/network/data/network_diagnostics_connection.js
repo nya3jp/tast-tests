eb29c91b341c616d1b8284b8e8307eb15970d49e
Kartik Hegde <khegde@chromium.org>
2020-12-31 17:35:35 -0700

    WIP: Add tast testing for network diagnostics routines
    Test the NetworkDiagnosticsRoutines Mojo API through tast.
    
    TEST=TODO(khegde): Add testing description
    BUG=TODO(khegde): Add bug number
    
    Change-Id: If83e6eb77242351add0ec33fbbf01e34a9098e3d

---
+++ src/chromiumos/tast/local/bundles/cros/network/data/network_diagnostics_connection.js
@@ -0,0 +1,59 @@
+/**
+ * @fileoverview Contains the logic for interacting with the
+ * NetworkDiagnosticsRoutines Mojo API.
+ */
+function() {
+  return {
+    publicVariable : null,
+
+    privateVariable_ : null,
+
+    publicMethod() {},
+
+    privateMethod_() {},
+
+    onPublicCallback() {},
+
+    onPrivateCallback_() {},
+
+    networkDiagnosticsRoutines_ : null,
+
+    createNetworkDiagnosticsRoutinesRemote() {
+      this.networkDiagnosticsRoutines_ = chromeos.networkDiagnostics.mojom.NetworkDiagnosticsRoutines.getRemote();
+      /* Check for existence of onConnectionError callback. (Is this part of the
+       * JS Mojo bindings)*/
+      //this.networkDiagnosticsRoutines_.onConnectionError.addListener(() => this.networkDiagnosticsRoutines_ = null);
+    },
+
+    networkDiagnosticsRoutines() {
+      return this.networkDiagnosticsRoutines_;
+    },
+
+    evaluateRoutine_(response) {
+      return true;
+    },
+
+    lanConnectivity() {
+      if (!this.networkDiagnosticsRoutines()) {
+        this.createNetworkDiagnosticsRoutinesRemote();
+      }
+      //var func = () => this.networkDiagnosticsRoutines_.lanConnectivity();
+      var verdict = this.networkDiagnosticsRoutines_.lanConnectivity();
+      return verdict;
+      /*var promise = new Promise(function(resolve, reject) {
+        // do a thing, possibly async, then…
+        if (true) {
+          resolve("Stuff worked!");
+        } else {
+          reject(Error("It broke"));
+        }
+      });
+      promise.then(function(result) {
+        console.log(result); // "Stuff worked!"
+      }, function(err) {
+        console.log(err); // Error: "It broke"
+      });*/
+      //func.then(response => this.evaluateRoutine_(response));
+    },
+  }
+}
