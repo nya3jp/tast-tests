2bc28d30bf3310f7961e94e3163a049dd39e7d8e
Trent Begin <tbegin@google.com>
2021-01-05 10:04:35 -0700

    platform.CrosHealthdProbeNetworkInfo: renable test
    This test was disabled since it was failing in the CQ due to
    https://crbug.com/1161107. On device startup Chromevox would pop up a
    dialog that would hang Chrome. This tast test relies on Chrome being
    present and running. This Chromevox features has been disabled until it
    is fixed.
    
    BUG=b:175940130
    TEST=flash device with prebuilt after 13694.0
    tast run hatch-dut platform.CrosHealthdProbeNetworkInfo (pass)
    
    Change-Id: If13fe9a390d5ffb82a91931b6e242e767289436e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2611071
    Tested-by: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Trent Begin <tbegin@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_network_info.go
+++ src/chromiumos/tast/local/bundles/cros/platform/cros_healthd_probe_network_info.go
@@ -24,7 +24,7 @@ func init() {
 			"pmoy@google.com",
 			"khegde@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"diagnostics"},
 	})
 }
