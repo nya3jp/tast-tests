fd59361f04aef4ed4091641f7ba491b7329b1725
Renuga Nanjappan <rnanjappan@chromium.org>
2021-01-07 03:04:19 +0000

    Tast Tests : Test to Verify ARC is On for Unicorn Account.
    BUG=b:159957340
    TEST=tast -verbose run <DUT> arc.UnicornPlaystoreOn
    
    Add a test to verify that ARC is on for child account after sign in
    
    Change-Id: Icb5f54c2ef1436db2bf653062a2c4d4c360b4a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2614117
    Tested-by: Renuga Nanjappan <rnanjappan@chromium.org>
    Commit-Queue: Renuga Nanjappan <rnanjappan@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arc/unicorn_playstore_on.go
@@ -0,0 +1,61 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc/optin"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         UnicornPlaystoreOn,
+		Desc:         "Checks if PlayStore is Enabled for Unicorn Acccount",
+		Contacts:     []string{"rnanjappan@chromium.org", "cros-arc-te@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      5 * time.Minute,
+		Vars:         []string{"arc.parentUser", "arc.parentPassword", "arc.childUser", "arc.childPassword"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+	})
+}
+
+func UnicornPlaystoreOn(ctx context.Context, s *testing.State) {
+	parentUser := s.RequiredVar("arc.parentUser")
+	parentPass := s.RequiredVar("arc.parentPassword")
+	childUser := s.RequiredVar("arc.childUser")
+	childPass := s.RequiredVar("arc.childPassword")
+
+	cr, err := chrome.New(ctx, chrome.GAIALogin(),
+		chrome.Auth(childUser, childPass, "gaia-id"),
+		chrome.ParentAuth(parentUser, parentPass), chrome.ARCSupported())
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+
+	// Optin to Play Store.
+	s.Log("Opting into Play Store")
+	if err := optin.Perform(ctx, cr, tconn); err != nil {
+		s.Fatal("Failed to optin to Play Store: ", err)
+	}
+	if err := optin.WaitForPlayStoreShown(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for Play Store: ", err)
+	}
+}
