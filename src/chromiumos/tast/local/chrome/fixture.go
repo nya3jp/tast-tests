6f9dd179802d21d8a599d6784ff61b0c3124a352
Xibin Liu <xliu@cienet.com>
2021-01-07 11:57:22 -0800

    tast-tests: Chrome loggedIn fixture extension
    The existing Chrome loggedIn fixture will always use the default test
    user credentials. Often test cases want to specify their own user name
    and password obtained from runtime variables. This CL extends loggedIn
    fixture with closures, which return chrome option at runtime, and
    allows runtime variables (including username/password) to be given and
    used when starting new Chrome.
    
    BUG=None
    TEST=fast_build.sh
    
    Change-Id: Id9b7e56d045d55f4477af95f3e75c0b2e0069e13

--- src/chromiumos/tast/local/chrome/fixture.go
+++ src/chromiumos/tast/local/chrome/fixture.go
@@ -45,17 +45,43 @@ func init() {
 	})
 }
 
+// FixtureOption is the function used to set up the fixture by returning Chrome option.
+type FixtureOption func(ctx context.Context, s *testing.FixtState) (Option, error)
+
+// LoggedInUser returns a fixture option to set up Chrome user.
+func LoggedInUser(userVar, passwdVar string) FixtureOption {
+	return func(ctx context.Context, s *testing.FixtState) (Option, error) {
+		var userID, userPasswd string
+		var ok bool
+		userID, ok = s.Var(userVar)
+		if !ok {
+			s.Fatalf("create new Chrome - %s not provided. Please specify it in your test vars configuration", userVar)
+		}
+		userPasswd, ok = s.Var(passwdVar)
+		if !ok {
+			s.Fatalf("create new Chrome - %s not provided. Please specify it in your test vars configuration", passwdVar)
+		}
+		return Auth(userID, userPasswd, ""), nil
+	}
+}
+
 // loggedInFixture is a fixture to start Chrome with the given options.
 // If the parent is specified, and the parent returns a value of []Option, it
 // will also add those options when starting Chrome.
 type loggedInFixture struct {
-	cr   *Chrome
-	opts []Option
+	cr    *Chrome
+	opts  []Option        // Chrome Options
+	fOpts []FixtureOption // Fixture Options
 }
 
 // NewLoggedInFixture returns a FixtureImpl of creating a Chrome instance with the given options.
 func NewLoggedInFixture(opts ...Option) testing.FixtureImpl {
 	return &loggedInFixture{opts: opts}
+}
+
+// NewLoggedInFixtureWithOptions returns a FixtureImpl with both Chrome Options and Fixture Options given.
+func NewLoggedInFixtureWithOptions(opts []Option, fOpts []FixtureOption) testing.FixtureImpl {
+	return &loggedInFixture{opts: opts, fOpts: fOpts}
 }
 
 func (f *loggedInFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
@@ -64,6 +90,15 @@ func (f *loggedInFixture) SetUp(ctx context.Context, s *testing.FixtState) inter
 	if extraOpts, ok := s.ParentValue().([]Option); ok {
 		opts = append(opts, extraOpts...)
 	}
+
+	for _, fOpt := range f.fOpts {
+		crOpt, err := fOpt(ctx, s)
+		if err != nil {
+			s.Fatal("Failed to call fixture option: ", err)
+		}
+		opts = append(opts, crOpt)
+	}
+
 	cr, err := New(ctx, opts...)
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
