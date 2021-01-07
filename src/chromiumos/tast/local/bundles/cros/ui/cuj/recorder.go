98cf5ad73736d4451fd1ee499abd49bd839fb226
Jun Mukai <mukai@google.com>
2021-01-06 16:18:24 -0800

    Split power-related data collector from cuj.Recorder
    The error reports that a single 'snapshot' of timeline data can
    take more than 1 second (the snapshot interval), which is treated
    as an error.
    
    One of the potential problem is actually addition of power metrics.
    I suggested to simply merge them into cuj.Recorder, but this could
    be a bad decision.  It is better to be split and recorded
    separately.
    
    Now they are recorded in a different timeline with 'Power' prefix,
    so they won't interfere with each other.
    
    BUG=chromium:1162174
    TEST=tast run ui.MeetCUJ.4p ui.MeetCUJ.power_4p ui.TabSwitchCUJ
    
    Change-Id: I239dcbb85a250ec7a1224b09528a1aee74bb4f4b

--- src/chromiumos/tast/local/bundles/cros/ui/cuj/recorder.go
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/recorder.go
@@ -14,7 +14,6 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/metrics"
 	"chromiumos/tast/local/load"
-	"chromiumos/tast/local/power"
 	"chromiumos/tast/testing"
 )
 
@@ -127,8 +126,6 @@ func NewRecorder(ctx context.Context, tconn *chrome.TestConn, configs ...MetricC
 		gpuDS,
 		memDiff,
 	}
-	// Note setup.PowerTest() is needed to for stable/reliable power metrics.
-	sources = append(sources, power.TestMetrics()...)
 	timeline, err := perf.NewTimeline(ctx, sources, perf.Interval(checkInterval), perf.Prefix("TPS."))
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to start perf.Timeline")
