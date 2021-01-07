8d1e4335dbffeed3f168b6ac52fd6527245fc9f9
Xibin Liu <xliu@cienet.com>
2021-01-07 11:57:22 -0800

    tast-tests: Chrome loggedIn fixture extension
    The existing Chrome loggedIn fixture will always use the default test
    user credentials. Often test cases want to specify their own user name
    and password obtained from runtime variables. This CL extends loggedIn
    fixture with a closure, which return chrome options at runtime, and
    allows runtime variables (including username/password) to be given and
    used when starting new Chrome.
    
    BUG=None
    TEST=fast_build.sh
    
    Change-Id: Id9b7e56d045d55f4477af95f3e75c0b2e0069e13

--- src/chromiumos/tast/local/chrome/fixture.go
+++ src/chromiumos/tast/local/chrome/fixture.go
@@ -13,20 +13,24 @@ import (
 
 func init() {
 	testing.AddFixture(&testing.Fixture{
-		Name:            "chromeLoggedIn",
-		Desc:            "Logged into a user session",
-		Contacts:        []string{"nya@chromium.org", "oka@chromium.org"},
-		Impl:            NewLoggedInFixture(),
+		Name:     "chromeLoggedIn",
+		Desc:     "Logged into a user session",
+		Contacts: []string{"nya@chromium.org", "oka@chromium.org"},
+		Impl: NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]Option, error) {
+			return nil, nil
+		}),
 		SetUpTimeout:    LoginTimeout,
 		ResetTimeout:    ResetTimeout,
 		TearDownTimeout: ResetTimeout,
 	})
 
 	testing.AddFixture(&testing.Fixture{
-		Name:            "chromeLoggedInWith100DummyApps",
-		Desc:            "Logged into a user session with 100 dummy apps",
-		Contacts:        []string{"mukai@chromium.org"},
-		Impl:            NewLoggedInFixture(),
+		Name:     "chromeLoggedInWith100DummyApps",
+		Desc:     "Logged into a user session with 100 dummy apps",
+		Contacts: []string{"mukai@chromium.org"},
+		Impl: NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]Option, error) {
+			return nil, nil
+		}),
 		Parent:          "install100Apps",
 		SetUpTimeout:    LoginTimeout,
 		ResetTimeout:    ResetTimeout,
@@ -34,36 +38,48 @@ func init() {
 	})
 
 	testing.AddFixture(&testing.Fixture{
-		Name:            "chromeLoggedInWith100DummyAppsSkiaRenderer",
-		Desc:            "Logged into a user session with 100 dummy apps",
-		Contacts:        []string{"mukai@chromium.org"},
-		Impl:            NewLoggedInFixture(EnableFeatures("UseSkiaRenderer")),
+		Name:     "chromeLoggedInWith100DummyAppsSkiaRenderer",
+		Desc:     "Logged into a user session with 100 dummy apps",
+		Contacts: []string{"mukai@chromium.org"},
+		Impl: NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]Option, error) {
+			return []Option{EnableFeatures("UseSkiaRenderer")}, nil
+		}),
 		Parent:          "install100Apps",
 		SetUpTimeout:    LoginTimeout,
 		ResetTimeout:    ResetTimeout,
 		TearDownTimeout: ResetTimeout,
 	})
 }
+
+// OptionsCallback is the function used to set up the fixture by returning Chrome options.
+type OptionsCallback func(ctx context.Context, s *testing.FixtState) ([]Option, error)
 
 // loggedInFixture is a fixture to start Chrome with the given options.
 // If the parent is specified, and the parent returns a value of []Option, it
 // will also add those options when starting Chrome.
 type loggedInFixture struct {
 	cr   *Chrome
-	opts []Option
+	fOpt OptionsCallback // Function to generate Chrome Options
 }
 
-// NewLoggedInFixture returns a FixtureImpl of creating a Chrome instance with the given options.
-func NewLoggedInFixture(opts ...Option) testing.FixtureImpl {
-	return &loggedInFixture{opts: opts}
+// NewLoggedInFixture returns a FixtureImpl with a OptionsCallback function to provide Chrome options.
+func NewLoggedInFixture(fOpt OptionsCallback) testing.FixtureImpl {
+	return &loggedInFixture{fOpt: fOpt}
 }
 
 func (f *loggedInFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
-	opts := f.opts
+	var opts []Option
 	// If there's a parent fixture and the fixture supplies extra options, use them.
 	if extraOpts, ok := s.ParentValue().([]Option); ok {
 		opts = append(opts, extraOpts...)
 	}
+
+	crOpts, err := f.fOpt(ctx, s)
+	if err != nil {
+		s.Fatal("Failed to obtain Chrome options: ", err)
+	}
+	opts = append(opts, crOpts...)
+
 	cr, err := New(ctx, opts...)
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
