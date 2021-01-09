538cab6f988b9f55134a4aca574c2c09320795a1
Steven Bennetts <stevenjb@chromium.org>
2021-01-08 16:08:01 -0800

    shill: Add shill_cellular_ready test
    Adds a readiness test for Cellular.
    
    This includes a cellular_helper file that will be shared by
    other Cellular tests to simplify the tests themselves and avoid
    duplicate code and error checking.
    
    BUG=b:177103167
    TEST=tast --verbose run ${DUT} network.ShillCellularReady
    
    Change-Id: Ie32783bbc242d5cf715d60d1422ac7d1c1986622

---
+++ src/chromiumos/tast/local/bundles/cros/network/cellular/cellular_helper.go
@@ -0,0 +1,84 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package cellular provides functions for testing Cellular connectivity.
+package cellular
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/shillconst"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/testing"
+)
+
+// Helper fetches Cellular Device and Service properties.
+type Helper struct {
+	Manager *shill.Manager
+	Device  *shill.Device
+}
+
+// NewHelper creates a Helper object and ensures that a Cellular Device is present.
+func NewHelper(ctx context.Context) (*Helper, error) {
+	manager, err := shill.NewManager(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create Manager object")
+	}
+	available, err := manager.IsAvailable(ctx, shill.TechnologyCellular)
+	if err != nil || !available {
+		return nil, errors.Wrap(err, "Cellular Technology not available")
+	}
+	device, err := manager.DeviceByType(ctx, shillconst.TypeCellular)
+	if err != nil || device == nil {
+		return nil, errors.Wrap(err, "failed to get Cellular Device")
+	}
+	helper := Helper{Manager: manager, Device: device}
+
+	// Ensure Cellular is enabled.
+	if enabled, err := manager.IsEnabled(ctx, shill.TechnologyCellular); err != nil {
+		return nil, errors.Wrap(err, "error requesting enabled state")
+	} else if !enabled {
+		if err := helper.Enable(ctx); err != nil {
+			return nil, errors.Wrap(err, "unable to enable Cellular")
+		}
+	}
+
+	return &helper, nil
+}
+
+// Enable enables Cellular and ensures that the enable succeeded, otherwise an error is returned.
+func (h *Helper) Enable(ctx context.Context) error {
+	h.Manager.EnableTechnology(ctx, shill.TechnologyCellular)
+
+	// It may take a few seconds for Cellular to become enabled.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		enabled, err := h.Manager.IsEnabled(ctx, shill.TechnologyCellular)
+		if err != nil {
+			return errors.Wrap(err, "failed to get enabled state")
+		}
+		if !enabled {
+			return errors.New("Cellular not enabled")
+		}
+		return nil
+	}, &testing.PollOptions{
+		Timeout:  30 * time.Second,
+		Interval: 100 * time.Millisecond,
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+// FindService returns the first connectable Cellular Service.
+// If no such Cellular Service is available, returns a nil service and an error.
+func (h *Helper) FindService(ctx context.Context) (*shill.Service, error) {
+	// Look for any connectable Cellular service.
+	cellularProperties := map[string]interface{}{
+		shillconst.ServicePropertyConnectable: true,
+		shillconst.ServicePropertyType:        shillconst.TypeCellular,
+	}
+	return h.Manager.WaitForServiceProperties(ctx, cellularProperties, 5*time.Second)
+}
