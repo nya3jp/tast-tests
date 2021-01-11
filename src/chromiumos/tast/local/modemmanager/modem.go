43c857674d2631b0ca694c629c3cb12ddf865c17
Steven Bennetts <stevenjb@chromium.org>
2021-01-11 14:47:43 -0800

    modemmanager: add ModemmanagerSlots test
    This includes a tast/local/modemmanager/ dir and package
    with a modem.go module for fetching modemmanager properties.
    
    BUG=b:177103167
    TEST=tast --verbose run ${DUT} network.ModemmanagerSlots
    
    Change-Id: Iab0dd4f64c5488307dfbdb2e472a6976abaf6934

---
+++ src/chromiumos/tast/local/modemmanager/modem.go
@@ -0,0 +1,54 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package modemmanager
+
+import (
+	"context"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+)
+
+const (
+	dbusModemmanagerPath           = "/org/freedesktop/ModemManager1"
+	dbusModemmanagerService        = "org.freedesktop.ModemManager1"
+	dbusModemmanagerModemInterface = "org.freedesktop.ModemManager1.Modem"
+)
+
+// Modem wraps a Modemmanager.Modem D-Bus object.
+type Modem struct {
+	*dbusutil.PropertyHolder
+}
+
+// NewModem creates a new PropertyHolder instance for the Modem object.
+func NewModem(ctx context.Context) (*Modem, error) {
+	_, obj, err := dbusutil.ConnectNoTiming(ctx, dbusModemmanagerService, dbusModemmanagerPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to connect to service %s", dbusModemmanagerService)
+	}
+	managed, err := dbusutil.ManagedObjects(ctx, obj)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get ManagedObjects")
+	}
+	modemPath := dbus.ObjectPath("")
+	for iface, paths := range managed {
+		if iface == dbusModemmanagerModemInterface {
+			if len(paths) > 0 {
+				modemPath = paths[0]
+			}
+			break
+		}
+	}
+	if modemPath == dbus.ObjectPath("") {
+		return nil, errors.Wrap(err, "failed to get Modem path")
+	}
+	ph, err := dbusutil.NewPropertyHolder(ctx, dbusModemmanagerService, dbusModemmanagerModemInterface, modemPath)
+	if err != nil {
+		return nil, err
+	}
+	return &Modem{ph}, nil
+}
