8cb194e90f7582e77da908e81e4c9233e3e9bdc9
Matthew Warton <mwarton@google.com>
2021-01-12 09:09:29 +1100

    crostini: uninstall Linux at end of tests
    Previously the precondition stopped Linux by killing the concierge
    service, however this service is now expected to always be running by
    ARCVM.
    
    This change uses the UI to uninstall Linux the same way the user would,
    which leaves concierge in the state defined by the product uninstall
    code.
    
    BUG=None
    TEST=Ran crostini.Basic on hatch 2 times
    
    Change-Id: I712851480a05e0f91fcd18dddf41e534be0ce086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2622135
    Reviewed-by: Matthew Warton <mwarton@google.com>
    Reviewed-by: Judy Wu <jinrongwu@google.com>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Tested-by: Matthew Warton <mwarton@google.com>
    Commit-Queue: Matthew Warton <mwarton@google.com>
    Auto-Submit: Matthew Warton <mwarton@google.com>

--- src/chromiumos/tast/local/crostini/pre.go
+++ src/chromiumos/tast/local/crostini/pre.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strconv"
 	"strings"
@@ -19,8 +20,11 @@ import (
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
 	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/uig"
 	cui "chromiumos/tast/local/crostini/ui"
+	"chromiumos/tast/local/crostini/ui/settings"
 	"chromiumos/tast/local/crostini/ui/terminalapp"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/testexec"
@@ -532,20 +536,42 @@ func (p *preImpl) cleanUp(ctx context.Context, s *testing.PreState) {
 		p.keyboard = nil
 	}
 
-	// Don't stop concierge or delete the image for keepState so that
+	// Don't uninstall crostini or delete the image for keepState so that
 	// crostini is still running after the test, and the image can be reused.
 	if keepState(s) && p.startedOK {
-		s.Log("keepState not stopping concierge or unmounting and deleting image in cleanUp")
+		s.Log("keepState not uninstalling Crostini and deleting image in cleanUp")
 	} else {
 		if p.cont != nil {
-			if err := vm.StopConcierge(ctx); err != nil {
-				s.Log("Failure stopping concierge: ", err)
+			// Open the Linux settings.
+			st, err := settings.OpenLinuxSettings(ctx, p.tconn)
+			if err != nil {
+				s.Log("Failed to open Linux Settings: ", err)
+			}
+
+			// Click Remove on Linux settings page.
+			removeDlg, err := st.ClickRemove(ctx, p.tconn)
+			if err != nil {
+				s.Log("Failed to click Remove button on Linux settings page: ", err)
+			}
+
+			// Click Delete on the confirm dialog.
+			if err := uig.Do(ctx, p.tconn, uig.WaitForLocationChangeCompleted(), removeDlg.Delete.LeftClick()); err != nil {
+				s.Log("Failed to click Delete button on remove Linux dialog: ", err)
+			}
+
+			var developersButton = ui.FindParams{Attributes: map[string]interface{}{"name": regexp.MustCompile(`Developers|Linux \(Beta\)`)}, Role: ui.RoleTypeButton}
+			turnOn, err := ui.FindWithTimeout(ctx, p.tconn, developersButton, 30*time.Second)
+			if err != nil {
+				s.Fatal("Failed to find turn on button after removing Linux: ", err)
+			}
+
+			turnOn.Release(ctx)
+
+			if err := st.Close(ctx); err != nil {
+				s.Log("Failed to close settings window after uninstalling Linux: ", err)
 			}
 			p.cont = nil
 		}
-		// It is always safe to unmount the component, which just posts some
-		// logs if it was never mounted.
-		vm.UnmountComponent(ctx)
 		if err := vm.DeleteImages(); err != nil {
 			s.Log("Error deleting images: ", err)
 		}
