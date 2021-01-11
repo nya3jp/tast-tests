d200be356fbee4aaff3133a067553b03c425326f
Xibin Liu <xliu@cienet.com>
2021-01-11 12:38:04 -0800

    tast-tests: extend ArcBooted fixture with closure
    A closure can be provided to ArcBooted fixture to provide chrome options
    that will be used to start Chrome.
    
    BUG=chromium:1171012
    TEST=fast_build.sh
    
    Change-Id: I408e585fa7f79cc30e0d8498f28e180846fac7ae

--- src/chromiumos/tast/local/bundles/cros/ui/screen_rotation_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/screen_rotation_perf.go
@@ -6,17 +6,10 @@ package ui
 
 import (
 	"context"
-	"fmt"
 	"time"
 
-	"chromiumos/tast/common/perf"
-	"chromiumos/tast/errors"
-	"chromiumos/tast/local/bundles/cros/ui/perfutil"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/ash"
-	"chromiumos/tast/local/chrome/display"
 	"chromiumos/tast/local/power"
-	"chromiumos/tast/local/ui"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
@@ -29,7 +22,7 @@ func init() {
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome", "tablet_mode"},
 		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
-		Fixture:      "chromeLoggedIn",
+		Fixture:      "arcBooted",
 		Timeout:      3 * time.Minute,
 	})
 }
@@ -42,50 +35,51 @@ func ScreenRotationPerf(ctx context.Context, s *testing.State) {
 
 	cr := s.FixtValue().(*chrome.Chrome)
 
-	tconn, err := cr.TestAPIConn(ctx)
+	_, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to connect to test API: ", err)
 	}
-
-	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, true)
-	if err != nil {
-		s.Fatal("Failed to ensure in tablet mode: ", err)
-	}
-	defer cleanup(ctx)
-
-	dispInfo, err := display.GetInternalInfo(ctx, tconn)
-	if err != nil {
-		s.Fatal("Failed to get internal display info: ", err)
-	}
-
-	currentWindows := 0
-	runner := perfutil.NewRunner(cr)
-	// Run the screen rotation in overview mode with 2 or 8 windows.
-	for _, windows := range []int{2, 8} {
-		conns, err := ash.CreateWindows(ctx, tconn, cr, ui.PerftestURL, windows-currentWindows)
+	/*
+		cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, true)
 		if err != nil {
-			s.Fatal("Failed to create browser windows: ", err)
+			s.Fatal("Failed to ensure in tablet mode: ", err)
 		}
-		defer conns.Close()
-		currentWindows = windows
+		defer cleanup(ctx)
 
-		if err = ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
-			s.Fatal("Failed to enter into the overview mode: ", err)
+		dispInfo, err := display.GetInternalInfo(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to get internal display info: ", err)
 		}
 
-		suffix := fmt.Sprintf("%dwindows", windows)
-		runner.RunMultiple(ctx, s, suffix, perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
-			for _, rotation := range []display.RotationAngle{display.Rotate90, display.Rotate180, display.Rotate270, display.Rotate0} {
-				if err := display.SetDisplayRotationSync(ctx, tconn, dispInfo.ID, rotation); err != nil {
-					return errors.Wrap(err, "failed to rotate display")
-				}
+		currentWindows := 0
+		runner := perfutil.NewRunner(cr)
+		// Run the screen rotation in overview mode with 2 or 8 windows.
+		for _, windows := range []int{2, 8} {
+			conns, err := ash.CreateWindows(ctx, tconn, cr, ui.PerftestURL, windows-currentWindows)
+			if err != nil {
+				s.Fatal("Failed to create browser windows: ", err)
 			}
-			return nil
-		}, "Ash.Rotation.AnimationSmoothness"),
-			perfutil.StoreAll(perf.BiggerIsBetter, "percent", suffix))
-	}
+			defer conns.Close()
+			currentWindows = windows
 
-	if err := runner.Values().Save(ctx, s.OutDir()); err != nil {
-		s.Error("Failed saving perf data: ", err)
-	}
+			if err = ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+				s.Fatal("Failed to enter into the overview mode: ", err)
+			}
+
+			suffix := fmt.Sprintf("%dwindows", windows)
+			runner.RunMultiple(ctx, s, suffix, perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
+				for _, rotation := range []display.RotationAngle{display.Rotate90, display.Rotate180, display.Rotate270, display.Rotate0} {
+					if err := display.SetDisplayRotationSync(ctx, tconn, dispInfo.ID, rotation); err != nil {
+						return errors.Wrap(err, "failed to rotate display")
+					}
+				}
+				return nil
+			}, "Ash.Rotation.AnimationSmoothness"),
+				perfutil.StoreAll(perf.BiggerIsBetter, "percent", suffix))
+		}
+
+		if err := runner.Values().Save(ctx, s.OutDir()); err != nil {
+			s.Error("Failed saving perf data: ", err)
+		}
+	*/
 }
