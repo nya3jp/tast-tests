a4cf952483424de0e6bfd48c7a8d1880a43177c9
Steven Bennetts <stevenjb@chromium.org>
2021-01-11 14:47:43 -0800

    modemmanager: add ModemmanagerSlots test
    This includes a tast/local/modemmanager/ dir and package
    with a modem.go module for fetching modemmanager properties.
    
    BUG=b:177103167
    TEST=tast --verbose run ${DUT} network.ModemmanagerSlots
    
    Change-Id: Iab0dd4f64c5488307dfbdb2e472a6976abaf6934

---
+++ src/chromiumos/tast/local/bundles/cros/network/modemmanager_slots.go
@@ -0,0 +1,51 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+
+	"chromiumos/tast/local/modemmanager"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ModemmanagerSlots,
+		Desc:     "Verifies that modemmanager reports multiple SIM slots",
+		Contacts: []string{"stevenjb@google.com", "cros-network-health@google.com"},
+		Attr:     []string{"group:cellular"},
+	})
+}
+
+// ModemmanagerSlots Test
+func ModemmanagerSlots(ctx context.Context, s *testing.State) {
+	modem, err := modemmanager.NewModem(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Modem: ", err)
+	}
+	props, err := modem.GetShillProperties(ctx)
+	if err != nil {
+		s.Fatal("Failed to call GetProperties on Modem: ", err)
+	}
+	simSlots, err := props.GetObjectPaths("SimSlots")
+	if err != nil {
+		s.Fatal("Missing SimSlots property: ", err)
+	}
+	primary, err := props.GetUint32("PrimarySimSlot")
+	if err != nil {
+		s.Fatal("Missing PrimarySimSlot property: ", err)
+	}
+	if int(primary) > len(simSlots) {
+		s.Fatalf("Invalid PrimarySimSlot: %d", primary)
+	}
+	sim, err := props.GetObjectPath("Sim")
+	if err != nil {
+		s.Fatal("Missing Sim property: ", err)
+	}
+	if sim != simSlots[primary-1] {
+		s.Fatalf("Sim property mismatch: %s", sim)
+	}
+}
