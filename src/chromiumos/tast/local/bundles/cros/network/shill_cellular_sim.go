a5514fe1c96728344fd444c1e909ab01a99c8025
Steven Bennetts <stevenjb@chromium.org>
2021-01-11 09:51:06 -0800

    shill: Add shill_cellular_sim
    Adds CellularHelper which ensures a matching Cellular
    Device and Service, and a simple test using the helper
    verifying that Cellular.SIMPresent is true.
    
    BUG=b:177103167
    TEST=tast --verbose run ${DUT} network.ShillCellularSim
    
    Change-Id: I82d8f7b7f091cdd41474ddf516d600fe3318e912

---
+++ src/chromiumos/tast/local/bundles/cros/network/shill_cellular_sim.go
@@ -0,0 +1,41 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+
+	"chromiumos/tast/common/shillconst"
+	"chromiumos/tast/local/bundles/cros/network/cellular"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ShillCellularSim,
+		Desc:     "Verifies that a Cellular Device and matching Service are present with a valid SIM card",
+		Contacts: []string{"stevenjb@google.com", "cros-network-health@google.com"},
+		Attr:     []string{"group:cellular"},
+	})
+}
+
+func ShillCellularSim(ctx context.Context, s *testing.State) {
+	helper, err := cellular.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create cellular.Helper")
+	}
+	_, err = helper.FindServiceForDevice()
+	if err != nil {
+		s.Fatal("Failed to get Cellular Service for Device: ", err)
+	}
+	properties, err := helper.Device.GetProperties(ctx)
+	if err != nil {
+		s.Fatal("Failed to get Device properties: ", err)
+	}
+	simPresent, err := properties.GetBool(shillconst.DevicePropertyCellularSIMPresent)
+	if !simPresent {
+		s.Fatal("SIMPresent = false, err: ", err)
+	}
+}
