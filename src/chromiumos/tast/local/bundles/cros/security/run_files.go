2d44960c0542fb650de3f4518dc028c6f945e734
Yusuke Sato <yusukes@google.com>
2021-01-13 14:16:36 -0800

    tast-tests: Add security.RunFiles local test
    The new test checks owners and permissions of files in /run.
    
    This depends on
    https://chromium-review.googlesource.com/c/chromium/src/+/2613917
    
    BUG=chromium:1163122
    TEST=tast run $IPADDR security.RunFiles (on eve and hatch-arc-r)
    
    Change-Id: Ibb7b39671e84ef521a34f0adaeee3d070a3de3bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2628183
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Tested-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/security/run_files.go
@@ -0,0 +1,46 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package security
+
+import (
+	"context"
+	"strings"
+
+	chk "chromiumos/tast/local/bundles/cros/security/filecheck"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: RunFiles,
+		Desc: "Checks ownership and permissions of files in /run",
+		Contacts: []string{
+			"jorgelo@chromium.org", // Security team
+			"yusukes@chromium.org", // Initial author
+			"chromeos-security@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func RunFiles(ctx context.Context, s *testing.State) {
+	const (
+		root = "/run"
+	)
+	patterns := []*chk.Pattern{
+		// ARC/ARCVM files (crbug.com/1163122)
+		chk.NewPattern(chk.PathRegexp("arc(vm)?/host_generated/.*\\.prop"), chk.UID(0), chk.GID(0), chk.Mode(0644)),
+		// ARCVM-specific files (ignored on ARC builds)
+		chk.NewPattern(chk.Path("arcvm/host_generated/fstab"), chk.UID(0), chk.GID(0), chk.Mode(0644)),
+	}
+
+	problems, _, err := chk.Check(ctx, root, patterns)
+	if err != nil {
+		s.Errorf("Failed to check %v: %v", root, err)
+	}
+	for path, msgs := range problems {
+		s.Errorf("%v: %v", path, strings.Join(msgs, ", "))
+	}
+}
