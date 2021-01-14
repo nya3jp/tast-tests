a0af15ec94f9f3d8d983926e780c1af716f14ecf
Kuo Jen Wei <inker@google.com>
2021-01-14 17:54:20 +0800

    camera: Check sampled frames in N seconds
    Check N seconds frames sampled in small time interval for judging if the
    DUT FOV is aligned with chart instead of N frames captured in 1 second
    interval for decreasing the time feedbacking check result to UI.
    
    BUG=b:166370953
    TEST=tast run -var=user=<USERNAME> -var=pass=<PASSWORD>
    -var=chart=<CHART IP> -var=facing=<back or front> <DUT>
    camera.CameraboxAlign
    
    Change-Id: Idb5f17765e42fe33bc897c1e3726c59ce2760311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2629127
    Tested-by: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Greg Edelston <gredelston@google.com>

--- src/chromiumos/tast/remote/bundles/cros/camera/data/camerabox_align.js
+++ src/chromiumos/tast/remote/bundles/cros/camera/data/camerabox_align.js
@@ -202,28 +202,37 @@ window.Tast = class Tast {
   };
 
   /**
-   * Waits for N consecutive frames captured from |facing| camera in
-   * |aspectRatio| FOV passing alignment check.
+   * Waits for all sampled frames captured in last N milliseconds from |facing|
+   * camera in |aspectRatio| FOV passing alignment check.
    * @param {!Facing} facing
    * @param {!AspectRatio} aspectRatio
-   * @param {number} times
+   * @param {number} ms
    * @return {!Promise}
    * @private
    */
-  static async waitForPassAlignN_(facing, aspectRatio, times) {
+  static async waitForPassAlignN_(facing, aspectRatio, ms) {
     const aspectRatioName = aspectRatio === AspectRatio.AR4X3 ? '4x3' : '16x9';
-    let i = 1;
-    while (i <= times) {
-      // TODO(b/166370953): Improve the check response time < 1 seconds.
-      await sleep(1000);
+    let startTime = null;
+    while (true) {
+      await sleep(200);
+      const currentTime = Date.now();
       if (!await Tast.checkAlign_(facing, aspectRatio)) {
         Tast.feedbackAlign_(false, `Check ${aspectRatioName} align failed`);
-        i = 1;
+        startTime = null;
         continue;
       }
+      if (startTime === null) {
+        startTime = currentTime;
+        Tast.feedbackAlign_(true, `Pass check ${aspectRatioName} align`);
+        continue;
+      }
+      const duration = currentTime - startTime;
       Tast.feedbackAlign_(
-          true, `Pass check ${aspectRatioName} align ${i} times`);
-      i++;
+          true,
+          `Pass check ${aspectRatioName} align ${duration / 1000} seconds`);
+      if (duration >= ms) {
+        break;
+      }
     }
   }
 
