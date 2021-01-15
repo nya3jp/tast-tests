fe6562f5ef58994d91ac654d13f5d3c97c8de73b
Steven Bennetts <stevenjb@chromium.org>
2021-01-14 16:38:29 -0800

    network: Add inhibited tests
    These tests ensure that Modem.InhibitDevice and the Shill
    Device.Inhibited property work as expected.
    
    This encudes ensuring that setting the Device.Inhibited property
    removes the Cellular Service, and that setting Device.Inhibited
    to false restores the Service and a Connect succeeds.
    
    BUG=b:172077101
    TEST=tast run ${DUT} network.ShillCellularInhibited
    
    Change-Id: Ifc2ddcc64aa3d48f728f3f1816e54d519eff9578

---
+++ src/chromiumos/tast/local/bundles/cros/network/modemmanager_inhibit_device.go
@@ -0,0 +1,62 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/mmconst"
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/local/modemmanager"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ModemmanagerInhibitDevice,
+		Desc:     "Verifies that ModemManager1.InhibitDevice succeeds",
+		Contacts: []string{"stevenjb@google.com", "cros-network-health@google.com"},
+		Attr:     []string{"group:cellular"},
+		Timeout:  5 * time.Minute,
+	})
+}
+
+func ModemmanagerInhibitDevice(ctx context.Context, s *testing.State) {
+	modem, err := modemmanager.NewModem(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Modem: ", err)
+	}
+	props, err := modem.GetDBusProperties(ctx)
+	if err != nil {
+		s.Fatal("Failed to call GetProperties on Modem: ", err)
+	}
+	device, err := props.GetString(mmconst.ModemPropertyDevice)
+	if err != nil {
+		s.Fatal("Missing Device property: ", err)
+	}
+
+	for i := 0; i < 3; i++ {
+		obj, err := dbusutil.NewDBusObject(ctx, modemmanager.DBusModemmanagerService, modemmanager.DBusModemmanagerInterface, modemmanager.DBusModemmanagerPath)
+		if err != nil {
+			s.Fatal("Unable to connect to ModemManager1: ", err)
+		}
+		if err = obj.Call(ctx, "InhibitDevice", device, true).Err; err != nil {
+			s.Fatal("InhibitDevice(true) failed: ", err)
+		}
+		if err = obj.Call(ctx, "InhibitDevice", device, false).Err; err != nil {
+			s.Fatal("InhibitDevice(false) failed: ", err)
+		}
+
+		modem2, err := modemmanager.NewModem(ctx)
+		if err != nil {
+			s.Fatal("Failed to create Modem after Inhibit: ", err)
+		}
+		if modem.ObjectPath() == modem2.ObjectPath() {
+			s.Fatalf("Modem path expected to change but did not: %v, err: %s", modem.ObjectPath(), err)
+		}
+		modem = modem2
+	}
+}
