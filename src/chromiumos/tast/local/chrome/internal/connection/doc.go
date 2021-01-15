4966b03adda6d8b2b6417c2644584841e582ab75
Shuhei Takahashi <nya@chromium.org>
2021-01-15 19:17:54 +0900

    WIP: chrome: Move Conn and JSObject to an internal package
    This is one of CLs to move components in the chrome package to internal
    subpackages. This would make component separation clearer.
    
    *** TODO: Is it really useful to separate this new package and the
    *** cdputil package? Think more carefully what are responsibilities of
    *** the two packages.
    
    BUG=chromium:1166043
    TEST=tast run DUT ui.ChromeLogin
    
    Change-Id: I39d16caf79c39f63087e62a6618e42fd04bedaaf

---
+++ src/chromiumos/tast/local/chrome/internal/connection/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package connection provides components to work with Chrome DevTools protocol,
+// wrapping the cdputil package.
+package connection
