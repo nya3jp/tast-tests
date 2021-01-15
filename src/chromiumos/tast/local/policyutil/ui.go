879329d7158f8d8cf20e20ddf70f4cd2a9117135
Victor-Gabriel Savu <vsavu@google.com>
2021-01-15 16:15:20 +0100

    policy: add more helpers for node existence testing
    Add the VerifyNotExists and the VerifyNodeState methods to correctly
    check the state of the UI in policy tests.
    
    BUG=None
    TEST=tast run <ip> policy.AudioOutputAllowed policy.AutoclickEnabled
    
    Change-Id: I68185be5d5ccffcf099ca677fb23c423467ab79c

--- src/chromiumos/tast/local/policyutil/ui.go
+++ src/chromiumos/tast/local/policyutil/ui.go
@@ -12,6 +12,40 @@ import (
 	"chromiumos/tast/local/chrome/ui"
 )
 
+// VerifyNotExists checks if the element does not appear during timeout.
+// The function first waits until the element disappears.
+// Note: this waits for the full timeout to check that the element does not appear.
+func VerifyNotExists(ctx context.Context, tconn *chrome.TestConn, params ui.FindParams, timeout time.Duration) error {
+	start := time.Now()
+
+	// Wait for element to disappear.
+	if err := ui.WaitUntilGone(ctx, tconn, params, timeout); err != nil {
+		return err
+	}
+
+	// Continue waiting for timeout.
+	var after = time.After(timeout - time.Since(start))
+
+	// Wait for the full timeout to see if the element shows up.
+	// Check periodically if it shows up.
+	for {
+		if exists, err := ui.Exists(ctx, tconn, params); err != nil {
+			return err
+		} else if exists {
+			return ui.ErrNodeExists
+		}
+
+		select {
+		case <-time.After(100 * time.Millisecond):
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-after:
+			// Node did not show up.
+			return nil
+		}
+	}
+}
+
 // WaitUntilExistsStatus repeatedly checks the existence of a node
 // until the desired status is found or the timeout is reached.
 // If the JavaScript fails to execute, an error is returned.
@@ -21,4 +55,16 @@ func WaitUntilExistsStatus(ctx context.Context, tconn *chrome.TestConn, params u
 	}
 
 	return ui.WaitUntilGone(ctx, tconn, params, timeout)
+}
+
+// VerifyNodeState repeatedly checks the existence of a node to make sure it
+// reaches the desired status and stays in that state.
+// Fully waits until the timeout expires to ensure non-existence.
+// If the JavaScript fails to execute, an error is returned.
+func VerifyNodeState(ctx context.Context, tconn *chrome.TestConn, params ui.FindParams, exists bool, timeout time.Duration) error {
+	if exists {
+		return ui.WaitUntilExists(ctx, tconn, params, timeout)
+	}
+
+	return VerifyNotExists(ctx, tconn, params, timeout)
 }
