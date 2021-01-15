c639cda17a4a6668d95c4fcdbdc1bc8f95f269a1
Shuhei Takahashi <nya@chromium.org>
2021-01-15 19:17:54 +0900

    WIP: chrome: Move Conn and JSObject to an internal package
    This is one of CLs to move components in the chrome package to internal
    subpackages. This would make component separation clearer.
    
    This is a refactoring change. No behavior change is expected.
    
    BUG=chromium:1166043
    TEST=tast run DUT ui.ChromeLogin
    
    Change-Id: I39d16caf79c39f63087e62a6618e42fd04bedaaf

---
+++ src/chromiumos/tast/local/chrome/internal/driver/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package driver provides components to interact with a locally-running Chrome
+// process with DevTools protocol.
+//
+// This package works with a properly set up Chrome processes. This package does
+// NOT support starting Chrome processes.
+package driver
