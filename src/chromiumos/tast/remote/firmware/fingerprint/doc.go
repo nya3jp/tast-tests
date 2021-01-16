8df25d601daeab000e3cae9e27884000ccb751f6
Yicheng Li <yichengli@chromium.org>
2021-01-15 16:26:05 -0800

    firmware: Extract fingerprint helper functions to fingerprint package
    This will allow reusing these functions in future fingerprint tests.
    
    BUG=b:169958710
    TEST=tast -verbose run -ephemeraldevserver=false <IP> firmware.FpSensor
    
    Change-Id: I4e64a74ed1fad98460f3a22478c16f56b2289e79

---
+++ src/chromiumos/tast/remote/firmware/fingerprint/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package fingerprint is a collection of utilities to work with the firmware
+// on the fingerprint microcontroller (FPMCU), e.g. toggle write protect and
+// flash firmware.
+package fingerprint
