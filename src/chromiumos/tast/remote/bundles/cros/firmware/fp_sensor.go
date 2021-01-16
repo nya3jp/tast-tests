bfd44ef332383d78678c0ca6cc61b22181e22c77
Yicheng Li <yichengli@chromium.org>
2021-01-15 16:26:05 -0800

    firmware: Extract fingerprint helper functions to fingerprint package
    This will allow reusing these functions in future fingerprint tests.
    
    BUG=b:169958710
    TEST=tast -verbose run -ephemeraldevserver=false <IP> firmware.FpSensor
    
    Change-Id: I4e64a74ed1fad98460f3a22478c16f56b2289e79

--- src/chromiumos/tast/remote/bundles/cros/firmware/fp_sensor.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/fp_sensor.go
@@ -5,17 +5,11 @@
 package firmware
 
 import (
-	"bytes"
 	"context"
-	"io/ioutil"
-	"path/filepath"
 	"regexp"
-	"strings"
 	"time"
 
-	"chromiumos/tast/dut"
-	"chromiumos/tast/errors"
-	"chromiumos/tast/lsbrelease"
+	"chromiumos/tast/remote/firmware/fingerprint"
 	"chromiumos/tast/rpc"
 	"chromiumos/tast/services/cros/platform"
 	"chromiumos/tast/shutil"
@@ -24,12 +18,7 @@ import (
 )
 
 const (
-	// nocturne and nami are special cases and have "_fp" appended.
-	// Newer FPMCUs have unique names.
-	// See go/cros-fingerprint-firmware-branching-and-signing.
-	fingerprintBoardNameSuffix  = "_fp"
-	fingerprintFirmwarePathBase = "/opt/google/biod/fw/"
-	waitForBiodToStartTimeout   = 30 * time.Second
+	waitForBiodToStartTimeout = 30 * time.Second
 )
 
 func init() {
@@ -48,103 +37,10 @@ func init() {
 	})
 }
 
-func loadLsbRelease(ctx context.Context, d *dut.DUT) (map[string]string, error) {
-	out, err := d.Command("cat", "/etc/lsb-release").Output(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to read /etc/lsb-release")
-	}
-	return lsbrelease.Parse(bytes.NewReader(out))
-}
-
-func getBoardByLsbRelease(ctx context.Context, d *dut.DUT) (string, error) {
-	m, err := loadLsbRelease(ctx, d)
-	if err != nil {
-		return "", err
-	}
-	return m[lsbrelease.Board], nil
-}
-
-func getBoardByCrosConfig(ctx context.Context, d *dut.DUT) (string, error) {
-	out, err := d.Command("cros_config", "/fingerprint", "board").Output(ctx)
-	return string(out), err
-}
-
-// getFpBoard returns the name of the fingerprint EC on the DUT
-func getFpBoard(ctx context.Context, d *dut.DUT) (string, error) {
-	// For devices that don't have unibuild support (which is required to
-	// use cros_config).
-	// TODO(https://crbug.com/1030862): remove when nocturne has cros_config
-	// support.
-	board, err := getBoardByLsbRelease(ctx, d)
-	if err != nil {
-		return "", err
-	}
-	if board == "nocturne" {
-		return board + fingerprintBoardNameSuffix, nil
-	}
-
-	// Use cros_config to get fingerprint board.
-	return getBoardByCrosConfig(ctx, d)
-}
-
-func getFpFirmwarePath(ctx context.Context, d *dut.DUT, fpBoard string) (string, error) {
-	cmd := "ls " + fingerprintFirmwarePathBase + fpBoard + "*.bin"
-	out, err := d.Command("bash", "-c", cmd).Output(ctx)
-	if err != nil {
-		return "", err
-	}
-	outStr := strings.TrimSpace(string(out))
-	if strings.Contains(outStr, "\n") {
-		return "", errors.Errorf("found multiple firmware files for %q", fpBoard)
-	}
-	return outStr, nil
-}
-
-func flashFpFirmware(ctx context.Context, d *dut.DUT) error {
-	fpBoard, err := getFpBoard(ctx, d)
-	if err != nil {
-		return errors.Wrap(err, "failed to get fp board")
-	}
-	testing.ContextLogf(ctx, "fp board name: %q", fpBoard)
-
-	fpFirmwarePath, err := getFpFirmwarePath(ctx, d, fpBoard)
-	if err != nil {
-		return errors.Wrap(err, "failed to get fp firmware path")
-	}
-	flashCmd := []string{"flash_fp_mcu", fpFirmwarePath}
-	testing.ContextLogf(ctx, "Running command: %q", shutil.EscapeSlice(flashCmd))
-	if err := d.Command(flashCmd[0], flashCmd[1:]...).Run(ctx); err != nil {
-		return errors.Wrap(err, "flash_fp_mcu failed")
-	}
-	testing.ContextLog(ctx, "Flashed FP firmware, now initializing the entropy")
-	if err := d.Command("bio_wash", "--factory_init").Run(ctx); err != nil {
-		return errors.Wrap(err, "failed to initialize entropy after flashing FPMCU")
-	}
-	testing.ContextLog(ctx, "Entropy initialized, now rebooting to get seed")
-	if err := d.Reboot(ctx); err != nil {
-		return errors.Wrap(err, "failed to reboot DUT")
-	}
-	return nil
-}
-
 func FpSensor(ctx context.Context, s *testing.State) {
 	d := s.DUT()
-	// Check version and see if the FPMCU is in a good state.
-	versionCmd := []string{"ectool", "--name=cros_fp", "version"}
-	testing.ContextLogf(ctx, "Running command: %q", shutil.EscapeSlice(versionCmd))
-	out, err := d.Command(versionCmd[0], versionCmd[1:]...).Output(ctx)
-
-	if err == nil {
-		versionOutputFile := "cros_fp_version.txt"
-		testing.ContextLogf(ctx, "Writing FP firmware version to %s", versionOutputFile)
-		if err := ioutil.WriteFile(filepath.Join(s.OutDir(), versionOutputFile), out, 0644); err != nil {
-			s.Error("Failed to write FP firmware version to file: ", err)
-		}
-	} else {
-		testing.ContextLogf(ctx, "Failed to query FPMCU version (error: %v). Trying re-flashing FP firmware", err)
-		if err := flashFpFirmware(ctx, d); err != nil {
-			s.Error("Failed to flash FP firmware: ", err)
-		}
+	if err := fingerprint.InitializeKnownState(ctx, d, s.OutDir()); err != nil {
+		s.Fatal("Initialization failed: ", err)
 	}
 
 	cl, err := rpc.Dial(ctx, d, s.RPCHint(), "cros")
@@ -168,7 +64,7 @@ func FpSensor(ctx context.Context, s *testing.State) {
 
 	fpencstatusCmd := []string{"ectool", "--name=cros_fp", "fpencstatus"}
 	testing.ContextLogf(ctx, "Running command: %q", shutil.EscapeSlice(fpencstatusCmd))
-	out, err = d.Command(fpencstatusCmd[0], fpencstatusCmd[1:]...).Output(ctx)
+	out, err := d.Command(fpencstatusCmd[0], fpencstatusCmd[1:]...).Output(ctx)
 
 	if err != nil {
 		s.Fatalf("%q failed: %v", shutil.EscapeSlice(fpencstatusCmd), err)
---
+++ src/chromiumos/tast/remote/firmware/fingerprint/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package fingerprint is a collection of utilities to work with the firmware
+// on the fingerprint MCU, e.g. toggle write protect and flash firmware.
+package fingerprint
