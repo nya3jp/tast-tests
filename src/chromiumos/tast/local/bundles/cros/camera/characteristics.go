f3888f0a27e1c3b9dbd4c00f0d72dcea0473518a
Ren-Pei Zeng <kamesan@chromium.org>
2021-01-18 12:22:33 +0800

    camera: Add camera.Characteristics Tast test
    The test validates the format of USB camera config file.
    
    BUG=b:177381783
    TEST=tast run $DUT camera.Characteristics
    
    Cq-Depend: chromium:2635636
    Change-Id: I7f960866d3e062ed6a9082c3a31dc27b617c8b6e

---
+++ src/chromiumos/tast/local/bundles/cros/camera/characteristics.go
@@ -0,0 +1,40 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"path/filepath"
+
+	"chromiumos/tast/local/gtest"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Characteristics,
+		Desc: "Verifies the format of camera characteristics file",
+		Contacts: []string{
+			"kamesan@chromium.org",
+			"chromeos-camera-eng@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func Characteristics(ctx context.Context, s *testing.State) {
+	const gtestExecutableName = "camera_characteristics_test"
+	t := gtest.New(gtestExecutableName,
+		gtest.Logfile(filepath.Join(s.OutDir(), gtestExecutableName+".log")))
+	args, err := t.Args()
+	if err != nil {
+		s.Fatal("Failed to get GTest execution args: ", err)
+	}
+	s.Log("Running ", shutil.EscapeSlice(args))
+	if _, err := t.Run(ctx); err != nil {
+		s.Fatalf("Failed to run %v: %v", gtestExecutableName, err)
+	}
+}
