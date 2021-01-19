3ed81aacd22112352545e301054967d74b473bcb
Ben Reich <benreich@chromium.org>
2021-01-19 01:18:04 +0000

    tast-tests: Add apps.LaunchSystemWebAppsFromURL
    Increasing the test coverage for System Web Apps (SWAs) we try to launch
    all the SWAs by typing their chrome:// URL into the Chrome omnibox. This
    does not use |conn.Navigate| or |cr.NewConn| to navigate as it utilises
    DevTools to perform the navigation which does not verify link capturing
    in SWAs.
    
    BUG=chromium:1146904
    TEST=tast run <DUT-IP> apps.LaunchSystemWebAppsFromURL
    
    Change-Id: I48b23bbc7408066ccf55a0b5588e1647acc3ccb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2635923
    Tested-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_system_web_apps_from_url.go
@@ -0,0 +1,139 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package apps
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LaunchSystemWebAppsFromURL,
+		Desc: "Verifies that System Web Apps can launch through their URL",
+		Contacts: []string{
+			"chrome-apps-platform-rationalization@google.com",
+			"benreich@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		Timeout:      5 * time.Minute,
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+// LaunchSystemWebAppsFromURL tries to navigate to System Web Apps from their chrome:// URL.
+func LaunchSystemWebAppsFromURL(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	systemWebApps, err := apps.GetListOfSystemWebApps(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get list of SWAs: ", err)
+	}
+
+	// Open up an empty Chrome browser window.
+	conn, err := cr.NewConn(ctx, "")
+	if err != nil {
+		s.Fatal("Failed to open a new renderer: ", err)
+	}
+	defer conn.Close()
+
+	// Get a handle to the input keyboard.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard handle: ", err)
+	}
+	defer kb.Close()
+
+	for _, app := range systemWebApps {
+		chromeURL := app.PublisherID
+
+		// Filter out apps with empty PublisherIDs / Chrome URLs e.g. Terminal.
+		if chromeURL == "" {
+			continue
+		}
+
+		s.Run(ctx, app.ShortName, func(ctx context.Context, s *testing.State) {
+			s.Log("Navigating to ", chromeURL)
+			if err := verifyAndLaunchSystemWebAppFromURL(ctx, cr, tconn, kb, s.OutDir(), app.Name, chromeURL); err != nil {
+				s.Fatalf("Failed navigating to %q: %v", chromeURL, err)
+			}
+		})
+	}
+}
+
+// verifyAndLaunchSystemWebAppFromURL types the URL into the Chrome omnibox and verifies the SWA page loads.
+func verifyAndLaunchSystemWebAppFromURL(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn, keyboard *input.KeyboardEventWriter, outDir, appName, appURL string) (retErr error) {
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	const uiTimeout = 20 * time.Second
+	pollOpts := testing.PollOptions{Interval: 2 * time.Second, Timeout: uiTimeout}
+
+	// Take a screenshot of the display before closing the SWA window (if it exists).
+	var conn *chrome.Conn
+	defer func() {
+		if retErr != nil {
+			screenshotFile := filepath.Join(outDir, appName+"_failed.png")
+			if err := screenshot.Capture(ctx, screenshotFile); err != nil {
+				testing.ContextLog(ctx, "Failed to take screenshot: ", err)
+			}
+		}
+
+		if conn != nil {
+			conn.Close()
+		}
+	}()
+
+	params := ui.FindParams{
+		Name: "Address and search bar",
+		Role: ui.RoleTypeTextField,
+	}
+
+	if err := ui.StableFindAndClick(ctxWithTimeout, tconn, params, &pollOpts); err != nil {
+		return errors.Wrap(err, "failed to click the omnibox")
+	}
+
+	if err := keyboard.Accel(ctxWithTimeout, "ctrl+a"); err != nil {
+		return errors.Wrap(err, "failed pressing enter into chrome omnibox")
+	}
+
+	if err := keyboard.Type(ctxWithTimeout, appURL); err != nil {
+		return errors.Wrap(err, "failed entering URL into chrome omnibox")
+	}
+
+	if err := keyboard.Accel(ctxWithTimeout, "Enter"); err != nil {
+		return errors.Wrap(err, "failed pressing enter into chrome omnibox")
+	}
+
+	conn, err := cr.NewConnForTarget(ctxWithTimeout, chrome.MatchTargetURL(appURL))
+	if err != nil {
+		return errors.Wrap(err, "failed getting connection to new target")
+	}
+
+	if conn.WaitForExpr(ctxWithTimeout, "document.readyState === 'complete'"); err != nil {
+		return errors.Wrap(err, "failed waiting for URL to load")
+	}
+
+	if err := conn.Eval(ctxWithTimeout, "window.close()", nil); err != nil {
+		return errors.Wrap(err, "failed closing the window")
+	}
+
+	return nil
+}
