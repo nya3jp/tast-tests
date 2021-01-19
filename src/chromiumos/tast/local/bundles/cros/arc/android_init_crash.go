c1223430a71f0aca992c0c3941fce293fe82fe80
Ryo Hashimoto <hashimoto@google.com>
2021-01-19 19:28:25 +0900

    arc: Add arc.AndroidInitCrash
    To verify that ARC restarts after Android init crash (crosvm if ARCVM).
    
    BUG=b:142281432
    TEST=tast run arc.AndroidInitCrash on kevin
    TEST=tast run arc.AndroidInitCrash on eve-arc-r
    
    Change-Id: I799db750d11ac6030d46240e7d9b7d2c2eb32fd8

---
+++ src/chromiumos/tast/local/bundles/cros/arc/android_init_crash.go
@@ -0,0 +1,90 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AndroidInitCrash,
+		Desc:         "Test Android init crash handling",
+		Contacts:     []string{"hashimoto@chromium.org", "arc-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		Timeout: 10 * time.Minute,
+	})
+}
+
+func AndroidInitCrash(ctx context.Context, s *testing.State) {
+	// Shorten the context to save time for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	cr, err := chrome.New(ctx, chrome.ARCEnabled())
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer func() {
+		if err := cr.Close(cleanupCtx); err != nil {
+			s.Fatal("Failed to close Chrome while (re)booting ARC: ", err)
+		}
+	}()
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer func() {
+		// Note: |a| can be the arc.New() result above, or the one below after reboot.
+		if a != nil {
+			a.Close()
+		}
+	}()
+
+	oldPID, err := arc.InitPID()
+	if err != nil {
+		s.Fatal("Failed to get init PID before reboot: ", err)
+	}
+
+	// Android init crash should result in Android reboot.
+	s.Log("Inducing Android init crash")
+	if err := testexec.CommandContext(ctx, "kill", fmt.Sprintf("%d", oldPID)).Run(); err != nil {
+		s.Fatal("Failed to kill Android init: ", err)
+	}
+
+	a.Close()
+	a = nil
+
+	// Make sure Android successfully boots.
+	a, err = arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to restart ARC: ", err)
+	}
+	// Additional check just to be sure.
+	newPID, err := arc.InitPID()
+	if err != nil {
+		s.Fatal("Failed to get init PID after reboot: ", err)
+	}
+	if newPID == oldPID {
+		s.Fatal("Failure: init PID did not change")
+	}
+}
