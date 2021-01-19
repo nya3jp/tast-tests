c4d9e1f75d81e35d79b34a7b55095814d711b1dd
Shuhei Takahashi <nya@chromium.org>
2021-01-19 18:51:03 +0900

    WIP: chrome: Isolate test extension logic to an internal package
    This patch isolates logic to prepare test extensions to a new internal
    package.
    
    This is a refactoring change. No behavior change is expected.
    
    BUG=chromium:1166043
    TEST=tast run DUT ui.ChromeLogin
    
    Change-Id: I8fee8ea70b81cfdd14a6967fab62fac119e1baeb

--- src/chromiumos/tast/local/chrome/util.go
+++ src/chromiumos/tast/local/chrome/util.go
@@ -13,38 +13,13 @@ import (
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/fsutil"
+	"chromiumos/tast/local/chrome/internal/extension"
 )
-
-const chromeUser = "chronos" // Chrome Unix username
 
 // ChownContentsToChrome recursively changes the ownership of the directory
 // contents to the uid and gid of the Chrome's browser process.
 func ChownContentsToChrome(dir string) error {
-	return chownContents(dir, chromeUser)
-}
-
-// chownContents recursively chowns dir's contents to username's uid and gid.
-func chownContents(dir, username string) error {
-	var u *user.User
-	var err error
-	if u, err = user.Lookup(username); err != nil {
-		return err
-	}
-
-	var uid, gid int64
-	if uid, err = strconv.ParseInt(u.Uid, 10, 32); err != nil {
-		return errors.Wrapf(err, "failed to parse uid %q", u.Uid)
-	}
-	if gid, err = strconv.ParseInt(u.Gid, 10, 32); err != nil {
-		return errors.Wrapf(err, "failed to parse gid %q", u.Gid)
-	}
-
-	return filepath.Walk(dir, func(p string, fi os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		return os.Chown(p, int(uid), int(gid))
-	})
+	return extension.ChownContentsToChrome(dir)
 }
 
 // moveUserCrashDumps copies the contents of the user crash directory to the
