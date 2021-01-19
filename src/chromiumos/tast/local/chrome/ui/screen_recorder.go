bf2bfe5e64945117da1f0f29d8a80b186db48581
Judy Wu <jinrongwu@google.com>
2021-01-19 15:02:33 +1100

    Update the screen record view name accordingly
    The item name of screen record has been changed in accessibility tree,
    update it. Also extend the timeout.
    
    BUG=None
    TEST=Ran on hatch
    
    Change-Id: I9ed0c2325fb142469d45a30d32e0ad6297bf9f67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2636695
    Tested-by: Judy Wu <jinrongwu@google.com>
    Commit-Queue: Judy Wu <jinrongwu@google.com>
    Reviewed-by: Matthew Warton <mwarton@google.com>

--- src/chromiumos/tast/local/chrome/ui/screen_recorder.go
+++ src/chromiumos/tast/local/chrome/ui/screen_recorder.go
@@ -92,7 +92,7 @@ func NewScreenRecorder(ctx context.Context, tconn *chrome.TestConn) (*ScreenReco
 		return nil, errors.Wrap(err, "failed to wait for animation finished")
 	}
 
-	desktopView, err := mediaview.DescendantWithTimeout(ctx, FindParams{ClassName: "DesktopMediaPicker_DesktopMediaSourceView", Role: RoleTypeButton}, timeout)
+	desktopView, err := mediaview.DescendantWithTimeout(ctx, FindParams{ClassName: "DesktopMediaSourceView", Role: RoleTypeButton}, 30*time.Second)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to find the desktop view")
 	}
