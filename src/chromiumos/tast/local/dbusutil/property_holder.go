8dc77dd07d68243fffaa8fc3a5c82da6bc1d22fa
Steven Bennetts <stevenjb@chromium.org>
2021-01-20 11:30:27 -0800

    dbusutil: Move shill PropertyHolder to dbusutil
    This will allow other dbus code to share these functions
    which are not shill specific. They will be used in a
    follow-up CL to support modemmanager tests.
    
    BUG=b:161486698
    TEST=tast wifi annd wpasupplicant tests
    
    Change-Id: I3eb0dea80ee90de823dd4094a854d58596ee875c

---
+++ src/chromiumos/tast/local/dbusutil/property_holder.go
@@ -0,0 +1,39 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package dbusutil
+
+import (
+	"context"
+
+	"github.com/godbus/dbus"
+)
+
+// PropertyHolder provides methods to access properties of a DBus object.
+// The DBus object must provides GetProperties and SetProperty methods, and a PropertyChanged signal.
+type PropertyHolder struct {
+	*DBusObject
+}
+
+// NewPropertyHolder creates a DBus object with the given service, interface and path
+// which can be used for accessing and setting properties.
+func NewPropertyHolder(ctx context.Context, service, iface string, path dbus.ObjectPath) (*PropertyHolder, error) {
+	dbusObject, err := NewDBusObject(ctx, service, iface, path)
+	if err != nil {
+		return nil, err
+	}
+	return &PropertyHolder{dbusObject}, nil
+}
+
+// GetProperties calls dbus GetProperties method on the interface and returns the result.
+// The dbus call may fail with DBusErrorUnknownObject if the ObjectPath is not valid.
+// Callers can check it with IsDBusError if it's expected.
+func (h *PropertyHolder) GetProperties(ctx context.Context) (*Properties, error) {
+	return NewProperties(ctx, h.DBusObject)
+}
+
+// SetProperty calls SetProperty on the interface to set property to the given value.
+func (h *PropertyHolder) SetProperty(ctx context.Context, prop string, value interface{}) error {
+	return h.Call(ctx, "SetProperty", prop, value).Err
+}
