f190eda916c0c9cabfc459f926dcb22666559f28
Akihiro Ota <akihiroota@chromium.org>
2021-01-20 14:34:25 -0800

    tast-tests: Write a spoken feedback test.
    This change creates a test for spoken feedback. To do so,
    this change:
    1. Adds a new GoogleTtsConn type, which represents a
    connection to the Google TTS background page.
    2. Adds an ExpectSpeech method, which can be used to
    verify speech output by the TTS engine.
    3. Adds a DoCommand helper method, which can be used to
    execute ChromeVox commands from JS.
    4. Adds a DoCommandAndExpectSpeech and PressKeysAndExpectSpeech
    convenience methods, which make it easy to write the test.
    
    BUG=chromium:1168327
    TEST=a11y.SpokenFeedback
    
    Change-Id: I314b7d574f23dd94bbb9288f81e4d4c4ced53a45

--- src/chromiumos/tast/local/a11y/a11y.go
+++ src/chromiumos/tast/local/a11y/a11y.go
@@ -8,16 +8,20 @@ package a11y
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/cdputil"
 	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
 
 const (
 	chromeVoxExtensionURL = "chrome-extension://mndnfokpggljbaajbnioimlmbfngpief/chromevox/background/background.html"
+	googleTtsExtensionID  = "gjjabgpgjpampikjhjpfhneeoapjbjaf"
 )
 
 // Feature represents an accessibility feature in Chrome OS.
@@ -36,8 +40,8 @@ const (
 // SetFeatureEnabled sets the specified accessibility feature enabled/disabled using the provided connection to the extension.
 func SetFeatureEnabled(ctx context.Context, tconn *chrome.TestConn, feature Feature, enable bool) error {
 	if err := tconn.Call(ctx, nil, `(feature, enable) => {
-		  return tast.promisify(tast.bind(chrome.accessibilityFeatures[feature], "set"))({value: enable});
-		}`, feature, enable); err != nil {
+      return tast.promisify(tast.bind(chrome.accessibilityFeatures[feature], "set"))({value: enable});
+    }`, feature, enable); err != nil {
 		return errors.Wrapf(err, "failed to toggle %v to %t", feature, enable)
 	}
 	return nil
@@ -111,5 +115,129 @@ func (cv *ChromeVoxConn) WaitForFocusedNode(ctx context.Context, tconn *chrome.T
 	}, &testing.PollOptions{Timeout: timeout}); err != nil {
 		return errors.Wrap(err, "failed to get current focus")
 	}
+	return nil
+}
+
+// doCommand performs a ChromeVox command.
+func (cv *ChromeVoxConn) doCommand(ctx context.Context, cmd string) error {
+	expr := fmt.Sprintf("CommandHandler.onCommand('%s');", cmd)
+	return cv.Eval(ctx, expr, nil)
+}
+
+// GoogleTtsConn represents a connection to the Google TTS extension background page.
+type GoogleTtsConn struct {
+	*chrome.Conn
+}
+
+// NewGoogleTtsConn returns a connection to the Google TTS extension's background page.
+// If the extension is not ready, the connection will be closed before returning.
+// Otherwise the calling function will close the connection.
+func NewGoogleTtsConn(ctx context.Context, c *chrome.Chrome) (*GoogleTtsConn, error) {
+	devsess, err := cdputil.NewSession(ctx, cdputil.DebuggingPortPath, cdputil.WaitPort)
+	if err != nil {
+		return nil, err
+	}
+	bgURL := chrome.ExtensionBackgroundPageURL(googleTtsExtensionID)
+	targets, err := devsess.FindTargets(ctx, chrome.MatchTargetURL(bgURL))
+	if err != nil {
+		return nil, err
+	}
+	if len(targets) > 1 {
+		for _, t := range targets[1:] {
+			// Close all but one instance of the Google TTS engine background page.
+			// We must do this because because trying to connect when there are multiple
+			// instances triggers the following error:
+			// Error: X targets matched while unique match was expected.
+			devsess.CloseTarget(ctx, t.TargetID)
+		}
+	}
+
+	var extConn *chrome.Conn
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		extConn, err = c.NewConnForTarget(ctx, chrome.MatchTargetURL(bgURL))
+		return err
+	}, &testing.PollOptions{Timeout: 20 * time.Second}); err != nil {
+		return nil, errors.Wrap(err, "failed to create a connection to the Google TTS background page")
+	}
+
+	if err := extConn.WaitForExpr(ctx, `document.readyState === "complete"`); err != nil {
+		extConn.Close()
+		return nil, errors.Wrap(err, "timed out waiting for the Google TTS engine background page to load")
+	}
+
+	if err := extConn.WaitForExpr(ctx, "window.engine.initialized_"); err != nil {
+		extConn.Close()
+		return nil, errors.Wrap(err, "timed out waiting for the Google TTS engine to initialize")
+	}
+
+	return &GoogleTtsConn{extConn}, nil
+}
+
+// expectSpeech verifies that the given utterances are spoken by the Google TTS
+// engine.
+func (tts *GoogleTtsConn) expectSpeech(ctx context.Context, ch1 chan bool, ch2 chan error, utterances []string) {
+	ch1 <- true
+	for _, utterance := range utterances {
+		expr := fmt.Sprintf(`window.engine.utterance_ === "%s"`, utterance)
+		if err := tts.WaitForExpr(ctx, expr); err != nil {
+			ch2 <- errors.Wrapf(err, "timed out waiting for utterance: %s", utterance)
+		}
+	}
+
+	ch2 <- nil
+}
+
+// DoCommandAndExpectSpeech performs a ChromeVox command and verifies that the
+// utterances are spoken by the Google TTS engine.
+func (cv *ChromeVoxConn) DoCommandAndExpectSpeech(ctx context.Context, tts *GoogleTtsConn, cmd string, utterances []string) error {
+	// Use a goroutine to verify that an action produces the correct speech.
+	// We need to begin waiting for utterances before speech is given to avoid
+	// any flakiness.
+	// The first channel is used to communicate that the goroutine is ready.
+	// The second channel is used to communicate errors, if any.
+	ch1 := make(chan bool)
+	ch2 := make(chan error)
+	go tts.expectSpeech(ctx, ch1, ch2, utterances)
+	if ready := <-ch1; ready != true {
+		return errors.New("expectSpeech goroutine is not ready")
+	}
+	if err := cv.doCommand(ctx, cmd); err != nil {
+		return errors.Wrapf(err, "failed to perform the ChromeVox command: %s", cmd)
+	}
+	if err := <-ch2; err != nil {
+		return errors.Wrap(err, "failed to verify speech from the Google TTS engine")
+	}
+
+	return nil
+}
+
+// PressKeysAndExpectSpeech presses keys and verifies that the utterances are
+// spoken by the Google TTS engine.
+func PressKeysAndExpectSpeech(ctx context.Context, tts *GoogleTtsConn, keys string, utterances []string) error {
+	// Open a connection to the keyboard.
+	ew, err := input.Keyboard(ctx)
+	if err != nil {
+		return errors.Wrap(err, "error with creating EventWriter from keyboard")
+	}
+	defer ew.Close()
+
+	// Use a goroutine to verify that an action produces the correct speech.
+	// We need to begin waiting for utterances before speech is given to avoid
+	// any flakiness.
+	// The first channel is used to communicate that the goroutine is ready.
+	// The second channel is used to communicate errors, if any.
+	ch1 := make(chan bool)
+	ch2 := make(chan error)
+	go tts.expectSpeech(ctx, ch1, ch2, utterances)
+	if ready := <-ch1; ready != true {
+		return errors.New("expectSpeech goroutine is not ready")
+	}
+	if err := ew.Accel(ctx, keys); err != nil {
+		return errors.Wrapf(err, "error when pressing the keys: %s", keys)
+	}
+	if err := <-ch2; err != nil {
+		return errors.Wrap(err, "failed to verify speech from the Google TTS engine")
+	}
+
 	return nil
 }
