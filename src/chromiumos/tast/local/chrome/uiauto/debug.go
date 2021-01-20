e778809b9d486e1e22f99788b71d7207d7298ee3
Brendan Hansknecht <bhansknecht@chromium.org>
2021-01-20 11:53:30 -0800

    tast-tests: add base of uiauto library
    This adds the base of the uiauto library. It adds the nodewith package to
    generate a way to find nodes. It adds packages for each type of
    constant. It still is missing many features to make it have parity with
    the old ui library. ui.ChangeWallpaper is ported to show what the new
    api looks like in use.
    
    BUG=b:178020071
    TEST=tast run <eve> ui.ChangeWallpaper
    
    Change-Id: Ia6763ba4541458010e5a500a6433ae4752011811

---
+++ src/chromiumos/tast/local/chrome/uiauto/debug.go
@@ -0,0 +1,29 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package uiauto
+
+import (
+	"context"
+	"io/ioutil"
+
+	"chromiumos/tast/local/chrome"
+)
+
+// RootDebugInfo returns the chrome.automation root as a string.
+// If the JavaScript fails to execute, an error is returned.
+func RootDebugInfo(ctx context.Context, tconn *chrome.TestConn) (string, error) {
+	var out string
+	err := tconn.EvalPromise(ctx, "tast.promisify(chrome.automation.getDesktop)().then(root => root+'');", &out)
+	return out, err
+}
+
+// LogRootDebugInfo logs the chrome.automation root debug info to a file.
+func LogRootDebugInfo(ctx context.Context, tconn *chrome.TestConn, filename string) error {
+	debugInfo, err := RootDebugInfo(ctx, tconn)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, []byte(debugInfo), 0644)
+}
