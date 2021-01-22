ee919c5a8d77ecbd14c3b679aad45ddd26c1dd75
Shuhei Takahashi <nya@chromium.org>
2021-01-22 14:57:13 +0900

    chrome: Isolate login-related logic to an internal package
    This is a pure code move. It changes no logic.
    
    BUG=chromium:1166043
    TEST=fast_build.sh -T
    
    Change-Id: Iff213e70edeadbf2a6b1001706b7940dac183e6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2642664
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>

---
+++ src/chromiumos/tast/local/chrome/internal/login/user.go
@@ -0,0 +1,76 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package login
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome/internal/config"
+	"chromiumos/tast/local/chrome/internal/driver"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/timing"
+)
+
+// loginUser logs in to a freshly-restarted Chrome instance.
+// It waits for the login process to complete before returning.
+func loginUser(ctx context.Context, cfg *config.Config, sess *driver.Session) error {
+	conn, err := WaitForOOBEConnection(ctx, sess)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	testing.ContextLogf(ctx, "Logging in as user %q", cfg.User)
+	ctx, st := timing.Start(ctx, "login")
+	defer st.End()
+
+	switch cfg.LoginMode {
+	case config.FakeLogin:
+		if err = conn.Exec(ctx, fmt.Sprintf("Oobe.loginForTesting('%s', '%s', '%s', %t)",
+			cfg.User, cfg.Pass, cfg.GAIAID, cfg.Enroll)); err != nil {
+			return err
+		}
+	case config.GAIALogin:
+		// GAIA login requires Internet connectivity.
+		if err := shill.WaitForOnline(ctx); err != nil {
+			return err
+		}
+		if err := performGAIALogin(ctx, cfg, sess, conn); err != nil {
+			return err
+		}
+	}
+
+	if cfg.Enroll {
+		if err := enterpriseOOBELogin(ctx, cfg, sess); err != nil {
+			return err
+		}
+	}
+
+	if err = cryptohome.WaitForUserMount(ctx, cfg.NormalizedUser); err != nil {
+		return err
+	}
+
+	if cfg.SkipOOBEAfterLogin {
+		testing.ContextLog(ctx, "Waiting for OOBE to be dismissed")
+		if err = testing.Poll(ctx, func(ctx context.Context) error {
+			if t, err := getFirstOOBETarget(ctx, sess); err != nil {
+				// This is likely Chrome crash. So there's no chance that
+				// waiting for the dismiss succeeds later. Quit the polling now.
+				return testing.PollBreak(err)
+			} else if t != nil {
+				return errors.Errorf("%s target still exists", oobePrefix)
+			}
+			return nil
+		}, pollOpts); err != nil {
+			return errors.Wrap(sess.Watcher().ReplaceErr(err), "OOBE not dismissed")
+		}
+	}
+
+	return nil
+}
