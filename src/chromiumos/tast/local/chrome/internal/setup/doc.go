4e9bc941a939e7561578192b2c079da9c76b1a9a
Shuhei Takahashi <nya@chromium.org>
2021-01-22 15:06:24 +0900

    chrome: Isolate setup-related logic to an internal package
    This is a pure code move. No logic is changed.
    
    BUG=chromium:1166043
    TEST=fast_build.sh -T
    
    Change-Id: I138dffdce2c9da3b4446264e6bf85f11b25178a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2642666
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>

---
+++ src/chromiumos/tast/local/chrome/internal/setup/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package setup implements setting up Chrome for testing.
+package setup
