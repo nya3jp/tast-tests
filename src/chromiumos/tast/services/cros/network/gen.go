a06737541eaff270d7324734ba3dba2745063edb
Andreea Costinas <acostinas@google.com>
2021-01-22 17:21:16 +0100

    service: network: Add a gRPC service for proxy server
    Add a gRPC service, tast.cros.network.ProxyService, to allow running
    a proxy server instance on the DUT for remote tests.
    
    BUG=b:178191961
    TEST=None
    
    Change-Id: I46fe6c57476ae3626cb2932df516459b8836a373

--- src/chromiumos/tast/services/cros/network/gen.go
+++ src/chromiumos/tast/services/cros/network/gen.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. iwlwifi_pci_rescan.proto wifi_service.proto bluetooth_service.proto
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. iwlwifi_pci_rescan.proto wifi_service.proto bluetooth_service.proto proxy_service.proto
 
 package network
 
