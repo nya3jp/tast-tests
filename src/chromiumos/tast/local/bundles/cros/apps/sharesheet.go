aed57813e8b472434403dff277c060cdd1454a63
Ben Reich <benreich@chromium.org>
2021-01-22 23:13:19 +0000

    tast-tests: Add apps.Sharesheet test
    Sharesheet allows sharing not only to Android apps but also to
    Progressive Web Apps (PWAs). This test installs a PWA that has a
    registered web shart target which should allow the Sharesheet to share a
    file to the app. It also verifies the data was received correctly.
    
    BUG=chromium:1169916
    TEST=tast run <DUT-IP> apps.Sharesheet
    
    Change-Id: Ic2811fc8ce455d2ef65d5413c72dae9bd4ce4896

---
+++ src/chromiumos/tast/local/bundles/cros/apps/sharesheet.go
@@ -0,0 +1,134 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package apps
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui/filesapp"
+	"chromiumos/tast/local/chrome/ui/sharesheet"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Sharesheet,
+		Desc: "Verify sharing a file to PWA works",
+		Contacts: []string{
+			"benreich@chromium.org",
+			"chromeos-apps-foundation-team@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		Data:         []string{"sharesheet_manifest.json", "sharesheet_service.js", "sharesheet_index.html", "sharesheet_icon.png"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func Sharesheet(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+
+	const (
+		appShareLabel        = "Web Share Target Test App"
+		expectedFileName     = "test.txt"
+		expectedFileContents = "test file contents"
+	)
+
+	// Setup the test file.
+	testFileLocation := filepath.Join(filesapp.DownloadPath, expectedFileName)
+	if err := ioutil.WriteFile(testFileLocation, []byte(expectedFileContents), 0644); err != nil {
+		s.Fatalf("Creating file %s failed: %s", testFileLocation, err)
+	}
+	defer os.Remove(testFileLocation)
+
+	fileChan := make(chan string)
+
+	fs := http.FileServer(s.DataFileSystem())
+	http.Handle("/", fs)
+	http.HandleFunc("/web-share-target/", func(w http.ResponseWriter, r *http.Request) {
+		parseErr := r.ParseMultipartForm(4096)
+		if parseErr != nil {
+			s.Fatal("Failed parsing multipart form: ", parseErr)
+			return
+		}
+
+		receivedFile := r.MultipartForm.File["received_file"][0]
+		fileChan <- receivedFile.Filename
+
+		multipartFile, err := receivedFile.Open()
+		if err != nil {
+			s.Fatal("Failed reading the received file: ", err)
+		}
+
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(multipartFile)
+
+		fileChan <- buf.String()
+	})
+
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			s.Fatal("Failed creating local server")
+		}
+	}()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	appID, err := apps.InstallPWAForURL(ctx, cr, filepath.Join("http://localhost:8080/sharesheet_index.html"), 5*time.Second)
+	if err != nil {
+		s.Fatal("Failed installing PWA for URL: ", err)
+	}
+
+	if err := ash.WaitForChromeAppInstalled(ctx, tconn, appID, 10*time.Second); err != nil {
+		s.Fatal("Failed waiting for PWA to be installed: ", err)
+	}
+
+	// Open the Files App.
+	files, err := filesapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed launching the Files app: ", err)
+	}
+	defer files.Release(ctx)
+
+	// Open the Downloads folder and check for the test file.
+	if err := files.OpenDownloads(ctx); err != nil {
+		s.Fatal("Failed opening the Downloads folder: ", err)
+	}
+
+	if err := files.SelectFile(ctx, expectedFileName); err != nil {
+		s.Fatalf("Failed selecting file %q: %v", expectedFileName, err)
+	}
+
+	if err := files.ClickShareButton(ctx); err != nil {
+		s.Fatal("Failed clicking share button: ", err)
+	}
+
+	if err := sharesheet.ClickApp(ctx, tconn, appShareLabel); err != nil {
+		s.Fatal("Failed clicking app on stable sharesheet: ", err)
+	}
+
+	fileName := <-fileChan
+	fileContents := <-fileChan
+
+	if fileContents != expectedFileContents {
+		s.Fatalf("File contents shared did not match: got %q; want %q", fileContents, expectedFileContents)
+	}
+
+	if fileName != expectedFileName {
+		s.Fatalf("File name shared did not match: got %q; want %q", fileName, expectedFileName)
+	}
+}
