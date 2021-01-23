2405925ac8e71710370a56867330ba1d19366d69
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-01-22 17:46:55 -0800

    Update Tast Test for Houseparty
    BUG=b:178242778
    TEST=tast run <eve> arcappcompat.Houseparty
    
    Disabled the test, as Houseparty android app is no longer available and it is migrated to PWA app.
    
    Change-Id: Idec582cdb108cfdf24822c196995550f34189161

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/houseparty.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/houseparty.go
@@ -37,10 +37,11 @@ var touchviewTestsForHouseparty = []testutil.TestCase{
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         Houseparty,
-		Desc:         "Functional test for Houseparty that installs the app also verifies it is logged in and that the main page is open, checks Houseparty correctly changes the window state in both clamshell and touchview mode",
-		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:appcompat"},
+		Func:     Houseparty,
+		Desc:     "Functional test for Houseparty that installs the app also verifies it is logged in and that the main page is open, checks Houseparty correctly changes the window state in both clamshell and touchview mode",
+		Contacts: []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		// Disabled this test as Houseparty android app is not available anymore and it is migrated to PWA app.
+		//Attr:         []string{"group:appcompat"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Val:               clamshellTestsForHouseparty,
