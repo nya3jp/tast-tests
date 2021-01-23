7b14da5de35e69d9b2fcd063bfc46792cb15d9db
Miguel Casas <mcasas@chromium.org>
2021-01-22 21:51:02 -0500

    camera,video: split camera-related Capabilities test
    This CL splits the camera-specific test part out of
    video.Capability and into a new file with the same
    name under camera/.
    
    I kept hiroh@ as the owner and changed the list to
    chromeos-camera-eng@.
    
    BUG=b:177261819
    TEST=tast run samus video.Capability camera.Capability no samus
    
    Change-Id: I5682375a6d4f732f7b931609adde502a67c75cb6

--- src/chromiumos/tast/local/bundles/cros/camera/capability.go
+++ src/chromiumos/tast/local/bundles/cros/camera/capability.go
@@ -1,8 +1,8 @@
-// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package video
+package camera
 
 import (
 	"context"
@@ -16,7 +16,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:     Capability,
 		Desc:     "Compare capabilities computed by autocaps package with ones detected by avtest_label_detect",
-		Contacts: []string{"hiroh@chromium.org", "chromeos-gfx-video@google.com"},
+		Contacts: []string{"hiroh@chromium.org", "chromeos-camera-eng@google.com"},
 		Attr:     []string{"group:mainline", "informational"},
 	})
 }
@@ -24,21 +24,11 @@ func init() {
 // capabilitiesToVerify is a map of capabilities to verify indexed by the
 // avtest_label_detect command line tool name.
 var capabilitiesToVerify = map[string]caps.Capability{
-	"hw_video_acc_h264":        {caps.HWDecodeH264, false},
-	"hw_video_acc_vp8":         {caps.HWDecodeVP8, false},
-	"hw_video_acc_vp9":         {caps.HWDecodeVP9, false},
-	"hw_video_acc_vp9_2":       {caps.HWDecodeVP9_2, false},
-	"hw_video_acc_h264_4k":     {caps.HWDecodeH264_4K, true},
-	"hw_video_acc_vp8_4k":      {caps.HWDecodeVP8_4K, true},
-	"hw_video_acc_vp9_4k":      {caps.HWDecodeVP9_4K, true},
-	"hw_jpeg_acc_dec":          {caps.HWDecodeJPEG, false},
-	"hw_video_acc_enc_h264":    {caps.HWEncodeH264, false},
-	"hw_video_acc_enc_vp8":     {caps.HWEncodeVP8, false},
-	"hw_video_acc_enc_vp9":     {caps.HWEncodeVP9, false},
-	"hw_video_acc_enc_h264_4k": {caps.HWEncodeH264_4K, false},
-	"hw_video_acc_enc_vp8_4k":  {caps.HWEncodeVP8_4K, false},
-	"hw_video_acc_enc_vp9_4k":  {caps.HWEncodeVP9_4K, false},
-	"hw_jpeg_acc_enc":          {caps.HWEncodeJPEG, false},
+	"builtin_usb_camera":      {caps.BuiltinUSBCamera, false},
+	"builtin_mipi_camera":     {caps.BuiltinMIPICamera, false},
+	"vivid_camera":            {caps.VividCamera, false},
+	"builtin_camera":          {caps.BuiltinCamera, false},
+	"builtin_or_vivid_camera": {caps.BuiltinOrVividCamera, false},
 }
 
 // Capability compares the static capabilities versus those detected in the DUT.
