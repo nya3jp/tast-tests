8b79f9197a92ce4bf7c12a7b21378c2f4bdb673c
Miguel Casas <mcasas@chromium.org>
2021-01-22 21:51:02 -0500

    camera,video: split camera-related Capabilities test
    This CL splits the camera-specific test part out of
    video.Capability and into a new file with the same
    name under camera/.
    
    I changed the owner to kamesan@ and changed the list to
    chromeos-camera-eng@.
    
    BUG=b:177261819
    TEST=tast run samus video.Capability camera.Capability no samus
    
    Change-Id: I5682375a6d4f732f7b931609adde502a67c75cb6

---
+++ src/chromiumos/tast/local/bundles/cros/camera/capability.go
@@ -0,0 +1,45 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/local/media/caps"
+	"chromiumos/tast/local/media/logging"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     Capability,
+		Desc:     "Compare capabilities computed by autocaps package with ones detected by avtest_label_detect",
+		Contacts: []string{"kamesan@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+	})
+}
+
+// capabilitiesToVerify is a map of capabilities to verify indexed by the
+// avtest_label_detect command line tool name.
+var capabilitiesToVerify = map[string]caps.Capability{
+	"builtin_usb_camera":      {Name: caps.BuiltinUSBCamera, Optional: false},
+	"builtin_mipi_camera":     {Name: caps.BuiltinMIPICamera, Optional: false},
+	"vivid_camera":            {Name: caps.VividCamera, Optional: false},
+	"builtin_camera":          {Name: caps.BuiltinCamera, Optional: false},
+	"builtin_or_vivid_camera": {Name: caps.BuiltinOrVividCamera, Optional: false},
+}
+
+// Capability compares the static capabilities versus those detected in the DUT.
+func Capability(ctx context.Context, s *testing.State) {
+	vl, err := logging.NewVideoLogger()
+	if err != nil {
+		s.Fatal("Failed to set values for verbose logging")
+	}
+	defer vl.Close()
+
+	if err := caps.VerifyCapabilities(ctx, s, capabilitiesToVerify); err != nil {
+		s.Fatal("Test failed: ", err)
+	}
+}
