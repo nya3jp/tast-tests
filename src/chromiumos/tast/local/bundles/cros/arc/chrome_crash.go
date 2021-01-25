f5d7251cf7b7257a3148869dd1e8b6c43b3e2ab1
Ryo Hashimoto <hashimoto@google.com>
2021-01-25 20:30:44 +0900

    arc: Add logged in tests to arc.ChromeCrash
    BUG=b:142281129
    TEST=tast run $EVE_ARC_R arc.ChromeCrash.vm arc.ChromeCrash.vm_logged_in
    TEST=tast run $NOCTURNE arc.ChromeCrash arc.ChromeCrash.logged_in
    
    Change-Id: I99e15223559e1790f611060e8e7e5c2ea244fed7

--- src/chromiumos/tast/local/bundles/cros/arc/chrome_crash.go
+++ src/chromiumos/tast/local/bundles/cros/arc/chrome_crash.go
@@ -9,6 +9,7 @@ import (
 	"syscall"
 	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
@@ -24,9 +25,19 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
+			Val:               false,
 			ExtraSoftwareDeps: []string{"android_p"},
 		}, {
 			Name:              "vm",
+			Val:               false,
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}, {
+			Name:              "logged_in",
+			Val:               true,
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm_logged_in",
+			Val:               true,
 			ExtraSoftwareDeps: []string{"android_vm"},
 		}},
 		Timeout: 10 * time.Minute,
@@ -34,17 +45,46 @@ func init() {
 }
 
 func ChromeCrash(ctx context.Context, s *testing.State) {
-	// Ensure login screen.
-	if err := upstart.RestartJob(ctx, "ui"); err != nil {
-		s.Fatal("Failed to restart ui: ", err)
-	}
+	loggedIn := s.Param().(bool)
 
-	s.Log("Waiting for Android init process")
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		_, err := arc.InitPID()
-		return err
-	}, &testing.PollOptions{Timeout: 60 * time.Second}); err != nil {
-		s.Fatal("Failed to wait for Android init process: ", err)
+	// Shorten the context to save time for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	if loggedIn {
+		func() {
+			cr, err := chrome.New(ctx, chrome.ARCEnabled())
+			if err != nil {
+				s.Fatal("Failed to connect to Chrome: ", err)
+			}
+			defer func() {
+				if err := cr.Close(cleanupCtx); err != nil {
+					s.Fatal("Failed to close Chrome: ", err)
+				}
+			}()
+			a, err := arc.New(ctx, s.OutDir())
+			if err != nil {
+				s.Fatal("Failed to start ARC: ", err)
+			}
+			defer func() {
+				if err := a.Close(); err != nil {
+					s.Fatal("Failed to close ARC connection: ", err)
+				}
+			}()
+		}()
+	} else {
+		// Ensure login screen.
+		if err := upstart.RestartJob(ctx, "ui"); err != nil {
+			s.Fatal("Failed to restart ui: ", err)
+		}
+		s.Log("Waiting for Android init process")
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			_, err := arc.InitPID()
+			return err
+		}, &testing.PollOptions{Timeout: 60 * time.Second}); err != nil {
+			s.Fatal("Failed to wait for Android init process: ", err)
+		}
 	}
 	oldPID, err := arc.InitPID()
 	if err != nil {
