d5674b569d3244aad96106dfb340d7039987ba0c
Hirokazu Honda <hiroh@chromium.org>
2021-01-25 03:45:48 +0000

    video: Split video.DecodeCompliance.av1_test_vectors
    The av1 film grain streams cannot be MD5-validated because
    decoded images can be different among decoders. Therefore, we
    need to validate the videos by computing SSIM. This CL separates
    video.DecodeCompliance.av1_test_vectors to following test cases,
    1.) av1_common: Validate non film grain streams in the MD5 way
    2.) av1_film_grain: Validate film grain streams in the MD5 way.
                        This test is skipped on TigerLake.
    3.) av1_ssim: Validate all streams in the SSIM way.
    
    BUG=b:176927551
    TEST=video.DecodeCompliance.av1_*
    
    Change-Id: I7e85d6c68151bc372c63ea811812eece6782b48a

--- src/chromiumos/tast/local/bundles/cros/video/decode_compliance.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_compliance.go
@@ -6,49 +6,46 @@ package video
 
 import (
 	"context"
-	"path/filepath"
 
 	"chromiumos/tast/local/media/caps"
 	"chromiumos/tast/local/media/decoding"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
-var videoTestFiles = []string{
+var av1CommonFiles = []string{
 	"test_vectors/av1/00000548.ivf",
-	"test_vectors/av1/00000548.ivf.json",
 	"test_vectors/av1/48_delayed.ivf",
-	"test_vectors/av1/48_delayed.ivf.json",
 	"test_vectors/av1/av1-1-b8-02-allintra.ivf",
-	"test_vectors/av1/av1-1-b8-02-allintra.ivf.json",
 	"test_vectors/av1/av1-1-b8-03-sizeup.ivf",
-	"test_vectors/av1/av1-1-b8-03-sizeup.ivf.json",
-	// TODO(b/176927551): Test with film grain streams if the intel driver fixes.
-	//	"test_vectors/av1/av1-1-b8-23-film_grain-50.ivf",
-	//	"test_vectors/av1/av1-1-b8-23-film_grain-50.ivf.json",
-	//	"test_vectors/av1/ccvb_film_grain.ivf",
-	//	"test_vectors/av1/ccvb_film_grain.ivf.json",
 	"test_vectors/av1/crosvideo_last_2sec.ivf",
-	"test_vectors/av1/crosvideo_last_2sec.ivf.json",
 	"test_vectors/av1/frames_refs_short_signaling.ivf",
-	"test_vectors/av1/frames_refs_short_signaling.ivf.json",
 	"test_vectors/av1/non_uniform_tiling.ivf",
-	"test_vectors/av1/non_uniform_tiling.ivf.json",
 	"test_vectors/av1/test-25fps-192x288-only-tile-cols-is-power-of-2.ivf",
-	"test_vectors/av1/test-25fps-192x288-only-tile-cols-is-power-of-2.ivf.json",
 	"test_vectors/av1/test-25fps-192x288-only-tile-rows-is-power-of-2.ivf",
-	"test_vectors/av1/test-25fps-192x288-only-tile-rows-is-power-of-2.ivf.json",
 	"test_vectors/av1/test-25fps-192x288-tile-rows-3-tile-cols-3.ivf",
-	"test_vectors/av1/test-25fps-192x288-tile-rows-3-tile-cols-3.ivf.json",
 }
 
-func testVectors() []string {
-	var tv []string
-	for _, file := range videoTestFiles {
-		if filepath.Ext(file) == ".ivf" {
-			tv = append(tv, file)
-		}
+var av1FilmGrainFiles = []string{
+	"test_vectors/av1/av1-1-b8-23-film_grain-50.ivf",
+	"test_vectors/av1/ccvb_film_grain.ivf",
+}
+
+var av1Files = append(av1CommonFiles, av1FilmGrainFiles...)
+
+func testFiles(videoFiles []string) []string {
+	var tf []string
+	for _, file := range videoFiles {
+		tf = append(tf, file)
+		tf = append(tf, file+".json")
 	}
-	return tv
+	return tf
+}
+
+// testOpt is used to describe the options used to run each test.
+type decodeComplianceTestParam struct {
+	videoFiles    []string               // The paths of video files to be tested.
+	validatorType decoding.ValidatorType // The frame validation type of video_decode_accelerator_tests.
 }
 
 func init() {
@@ -59,20 +56,46 @@ func init() {
 		SoftwareDeps: []string{"chrome", "video_decoder_direct"},
 		Attr:         []string{"group:mainline", "informational"},
 		Params: []testing.Param{{
-			Name:              "av1_test_vectors",
+			Name:              "av1_common",
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1},
-			ExtraData:         videoTestFiles,
+			ExtraData:         testFiles(av1CommonFiles),
+			Val: decodeComplianceTestParam{
+				videoFiles:    av1CommonFiles,
+				validatorType: decoding.MD5,
+			},
+		}, {
+			Name:              "av1_film_grain",
+			ExtraSoftwareDeps: []string{caps.HWDecodeAV1},
+			// Different decoders may use different film grain synthesis methods while producing
+			// a visually correct output (AV1 spec 7.2). Thus, for volteer, we don't validate
+			// the decoding of film-grain streams using MD5. Instead, we validate them using
+			// SSIM (see the av1_ssim test).
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform("volteer")),
+			ExtraData:         testFiles(av1FilmGrainFiles),
+			Val: decodeComplianceTestParam{
+				videoFiles:    av1FilmGrainFiles,
+				validatorType: decoding.MD5,
+			},
+		}, {
+			Name:              "av1_ssim",
+			ExtraSoftwareDeps: []string{caps.HWDecodeAV1},
+			ExtraData:         testFiles(av1Files),
+			Val: decodeComplianceTestParam{
+				videoFiles:    av1Files,
+				validatorType: decoding.SSIM,
+			},
 		}},
 	})
 }
 
 func DecodeCompliance(ctx context.Context, s *testing.State) {
 	var tv []string
-	for _, file := range testVectors() {
+	param := s.Param().(decodeComplianceTestParam)
+	for _, file := range param.videoFiles {
 		tv = append(tv, s.DataPath(file))
 	}
 
-	if err := decoding.RunAccelVideoTestWithTestVectors(ctx, s.OutDir(), tv); err != nil {
+	if err := decoding.RunAccelVideoTestWithTestVectors(ctx, s.OutDir(), tv, param.validatorType); err != nil {
 		s.Fatal("test failed: ", err)
 	}
 }
