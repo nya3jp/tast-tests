c354d92d383a0755b492aa9c3c346df955462abd
Shuhei Takahashi <nya@chromium.org>
2021-01-25 10:18:19 +0900

    chrome: Save test extensions to persistent directory
    Formerly Chrome.Close used to delete temporary directories where test
    extensions are saved. This may cause Chrome to malfunction on post-test
    manual inspection, as well as on reconnecting to Chrome, which is to be
    implemented soon.
    
    This patch changes the library to save extension files to the
    "persistent" directory, which is not deleted on Chrome.Close. It is
    instead cleared on chrome.New. Note that unpacked extensions specified
    with chrome.UnpackedExtension are also copied to this directory.
    
    BUG=chromium:1170186
    TEST=fast_build.sh -T
    TEST=tast run DUT example.ChromeExtension
    
    Change-Id: Ic576967afa63c1b5bde8bbdc3d0b831dfabee1ae

--- src/chromiumos/tast/local/chrome/internal/extension/files.go
+++ src/chromiumos/tast/local/chrome/internal/extension/files.go
@@ -5,11 +5,13 @@
 package extension
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/fsutil"
 )
 
 // Files manages local files of extensions to be installed to Chrome for
@@ -21,51 +23,59 @@ type Files struct {
 }
 
 // PrepareExtensions writes test extensions to the local disk.
+// destDir is a path to a directory under which extensions are written. The
+// directory should not exist at the beginning. Callers are responsible for
+// deleting the directory after they're done with it.
 // The user test extension is always created. If signinExtensionKey is a
 // non-empty string, the sign-in profile test extension is also created using
 // the key. extraExtDirs specifies directories of extra extensions to be
 // installed.
-func PrepareExtensions(extraExtDirs []string, signinExtensionKey string) (files *Files, retErr error) {
-	// Prepare the user test extension.
-	user, err := prepareTestExtension(testExtensionKey, TestExtensionID)
-	if err != nil {
+func PrepareExtensions(destDir string, extraExtDirs []string, signinExtensionKey string) (files *Files, retErr error) {
+	if err := os.MkdirAll(destDir, 0755); err != nil {
 		return nil, err
 	}
 	defer func() {
 		if retErr != nil {
-			user.RemoveAll()
+			os.RemoveAll(destDir)
 		}
 	}()
+
+	// Prepare the user test extension.
+	user, err := prepareTestExtension(filepath.Join(destDir, "test_api"), testExtensionKey, TestExtensionID)
+	if err != nil {
+		return nil, err
+	}
 
 	// Prepare the sign-in profile test extension if it is available.
 	var signin *testExtension
 	if signinExtensionKey != "" {
-		signin, err = prepareTestExtension(signinExtensionKey, SigninProfileTestExtensionID)
+		signin, err = prepareTestExtension(filepath.Join(destDir, "test_api_signin_profile"), signinExtensionKey, SigninProfileTestExtensionID)
 		if err != nil {
 			return nil, err
 		}
-		defer func() {
-			if retErr != nil {
-				signin.RemoveAll()
-			}
-		}()
 	}
 
 	// Prepare extra extensions.
-	for _, dir := range extraExtDirs {
-		manifest := filepath.Join(dir, "manifest.json")
+	var copiedExtraExtDirs []string
+	for i, src := range extraExtDirs {
+		manifest := filepath.Join(src, "manifest.json")
 		if _, err = os.Stat(manifest); err != nil {
 			return nil, errors.Wrap(err, "missing extension manifest")
 		}
-		if err := ChownContentsToChrome(dir); err != nil {
+		dst := filepath.Join(destDir, fmt.Sprintf("extra.%d", i))
+		if err := copyDir(src, dst); err != nil {
 			return nil, err
 		}
+		if err := ChownContentsToChrome(dst); err != nil {
+			return nil, err
+		}
+		copiedExtraExtDirs = append(copiedExtraExtDirs, dst)
 	}
 
 	return &Files{
 		user:         user,
 		signin:       signin,
-		extraExtDirs: extraExtDirs,
+		extraExtDirs: copiedExtraExtDirs,
 	}, nil
 }
 
@@ -94,17 +104,21 @@ func (f *Files) ChromeArgs() []string {
 	return args
 }
 
-// RemoveAll removes files for test extensions. It does not remove files for
-// extra extensions.
-func (f *Files) RemoveAll() error {
-	var firstErr error
-	if err := f.user.RemoveAll(); err != nil && firstErr == nil {
-		firstErr = err
-	}
-	if f.signin != nil {
-		if err := f.signin.RemoveAll(); err != nil && firstErr == nil {
-			firstErr = err
+// copyDir copies a directory recursively.
+func copyDir(srcDir, dstDir string) error {
+	return filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
-	}
-	return firstErr
+		rel, err := filepath.Rel(srcDir, path)
+		if err != nil {
+			return err
+		}
+		src := filepath.Join(srcDir, rel)
+		dst := filepath.Join(dstDir, rel)
+		if info.IsDir() {
+			return os.Mkdir(dst, 0755)
+		}
+		return fsutil.CopyFile(src, dst)
+	})
 }
