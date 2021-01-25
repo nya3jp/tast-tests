8187c0f2f733e302cdebf9d9808b3f361b97f177
Hirokazu Honda <hiroh@chromium.org>
2021-01-25 01:27:59 +0000

    webrtc: Set display to landscape orientation in peerconnection test
    The default resolution produced by camera in RTC peerconnection
    page depends on the display resolution. On scarlet, it is
    1920x1080 and 1080x1920 if the display is in landscape and
    portrait orientation, respectively. However, the maximum
    resolution supported by the hardware encoder on scarlet is
    1920x1088. Therefore, the hardware encoder is not used if
    the display is in portrait orientation.
    This causes a failure in webrtc.RTCPeerConnection that tests
    the hardware encoder is used during a rtc peer connection.
    This fixes the failure by setting the display orientation to
    the landscape orientation in the test.
    
    BUG=b:168059462
    TEST=video.RTCPeerConnection.vp8_enc_cam on scarlet
    
    Change-Id: I09a3af2c755b32310a33104bbe9ecf47d56c2915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2644590
    Tested-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Yen-lin Lai <yenlinlai@google.com>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@google.com>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/video/play/play.go
+++ src/chromiumos/tast/local/bundles/cros/video/play/play.go
@@ -23,7 +23,7 @@ import (
 	"chromiumos/tast/local/audio/crastestclient"
 	"chromiumos/tast/local/bundles/cros/video/decode"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/graphics"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/media/logging"
 	"chromiumos/tast/local/screenshot"
@@ -318,6 +318,7 @@ func TestPlayAndScreenshot(ctx context.Context, s *testing.State, cr *chrome.Chr
 		return errors.Wrapf(err, "failed to open %v", url)
 	}
 	defer conn.Close()
+
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		return errors.Wrap(err, "failed to connect to test API")
@@ -325,28 +326,8 @@ func TestPlayAndScreenshot(ctx context.Context, s *testing.State, cr *chrome.Chr
 	defer tconn.Close()
 
 	// For consistency across test runs, ensure that the device is in landscape-primary orientation.
-	dispInfo, err := display.GetInternalInfo(ctx, tconn)
-	if err != nil {
-		return errors.Wrap(err, "failed to get internal display info")
-	}
-	orient, err := display.GetOrientation(ctx, tconn)
-	if err != nil {
-		return errors.Wrap(err, "failed to get the display orientation")
-	}
-	s.Logf("Original display orientation = %q", orient.Type)
-	if orient.Type != display.OrientationLandscapePrimary {
-		s.Log("Rotating the display to get to 'landscape-primary'")
-		if err := display.SetDisplayRotationSync(ctx, tconn, dispInfo.ID, display.Rotate270); err != nil {
-			return errors.Wrap(err, "failed to rotate display")
-		}
-		// Make sure that the rotation worked.
-		orient, err = display.GetOrientation(ctx, tconn)
-		if err != nil {
-			return errors.Wrap(err, "failed to get the display orientation")
-		}
-		if orient.Type != display.OrientationLandscapePrimary {
-			return errors.New("the display is not in the expected landscape-primary orientation")
-		}
+	if err = graphics.RotateDisplayToLandscapePrimary(ctx, tconn); err != nil {
+		return errors.Wrap(err, "failed to set display to landscape-primary orientation")
 	}
 
 	// Make the video go to full screen mode by pressing 'f': requestFullScreen() needs a user gesture.
