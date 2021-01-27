aea79e22bb2270865691b0998975c611db3bbc24
Yi Chou <yich@google.com>
2021-01-27 20:14:42 +0800

    hwsec: add attestation client service
    BUG=b:175510138
    TEST=tast run $DUT hwsec.*
    
    Cq-Depend: chromium:2651739
    Change-Id: I820a172b6756324f9fca7842e302d59cec61a7dd

---
+++ src/chromiumos/tast/services/cros/hwsec/gen.go
@@ -0,0 +1,11 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//go:generate protoc -I . -I ../../../../../../../../platform2/system_api/dbus/attestation --go_out=plugins=grpc:../../../../.. attestation_client_service.proto
+
+package hwsec
+
+// Run the following command in CrOS chroot to regenerate protocol buffer bindings:
+//
+// ~/trunk/src/platform/tast/tools/go.sh generate chromiumos/tast/services/cros/hwsec
