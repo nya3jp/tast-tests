73cf74980e3df32a538b05d6f588597d56eb242b
Yi Chou <yich@google.com>
2021-01-27 21:25:16 +0800

    hwsec: using attestation client in helper
    BUG=b:175510138
    TEST=tast run $DUT hwsec.*
    
    Change-Id: I2c32464a5512a7a06262a99aeda4a1c61038ccdf

--- src/chromiumos/tast/local/bundles/cros/hwsec/attestation_no_external_server.go
+++ src/chromiumos/tast/local/bundles/cros/hwsec/attestation_no_external_server.go
@@ -46,7 +46,7 @@ func AttestationNoExternalServer(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("CmdRunner creation error: ", err)
 	}
-	helper, err := hwseclocal.NewHelper(r)
+	helper, err := hwseclocal.NewHelperWithAttestationClient(ctx, r)
 	if err != nil {
 		s.Fatal("Helper creation error: ", err)
 	}
