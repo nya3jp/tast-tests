1b221f759df3afbb0e274bb099acd5d1efb7e81c
Yi Chou <yich@google.com>
2021-01-27 20:54:39 +0800

    hwsec: add remote attestation client
    BUG=b:175510138
    TEST=tast run $DUT hwsec.*
    
    Change-Id: Ie6ea170251539006e6c78b1fd5255ce4c46ca7a2

---
+++ src/chromiumos/tast/remote/hwsec/attestation_client.go
@@ -0,0 +1,38 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	apb "chromiumos/system_api/attestation_proto"
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/rpc"
+	hwsecpb "chromiumos/tast/services/cros/hwsec"
+	"chromiumos/tast/testing"
+)
+
+// AttestationClient talks to attestation service via gRPC D-Bus APIs.
+type AttestationClient struct {
+	dut     *dut.DUT
+	rpcHint *testing.RPCHint
+}
+
+// NewAttestationClient use the dut and rpc hint objects to construct AttestationClient.
+func NewAttestationClient(d *dut.DUT, r *testing.RPCHint) (*AttestationClient, error) {
+	return &AttestationClient{d, r}, nil
+}
+
+// GetStatus calls "GetStatus" gRPC D-Bus Interface.
+func (a *AttestationClient) GetStatus(ctx context.Context, req *apb.GetStatusRequest) (*apb.GetStatusReply, error) {
+	cl, err := rpc.Dial(ctx, a.dut, a.rpcHint, "cros")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect to the RPC service on the DUT")
+	}
+	defer cl.Close(ctx)
+	ac := hwsecpb.NewAttestationClientServiceClient(cl.Conn)
+	return ac.GetStatus(ctx, req)
+}
