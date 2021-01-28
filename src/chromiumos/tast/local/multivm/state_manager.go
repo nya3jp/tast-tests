51b338b9ea761d35b9f9f6f886190eaad304755b
Patrick Meiring <meiring@google.com>
2021-01-28 14:12:20 +1100

    multivm: Refactor multivm precondition to allow plugging in additional VMs.
    Currently, StateManager and the multi-vm precondition is closed to
    extension by virtue of accepting only options for predefined (crostini
    or arc) VM types. This makes the addition of additional VM types
    difficult, especially for private VMs (i.e. Parallels) which reside
    in internal bundles and whose types cannot be accessed from public
    Tast code.
    
    Refactor StateManager to accept any VM type implementing a designated
    interface and provide public constructors for MultiVM preconditions (as
    well as ARC and Crostini VMs) so that further MultiVM preconditions can
    be defined in internal bundles.
    
    BUG=b:178534364
    TEST=tast run ${DUT_IP} multivm.Login.*
    
    Change-Id: Ifb90710a3819068a252b507efc4fac3e5002defb

--- src/chromiumos/tast/local/multivm/state_manager.go
+++ src/chromiumos/tast/local/multivm/state_manager.go
@@ -9,70 +9,73 @@ import (
 	"time"
 
 	"chromiumos/tast/errors"
-	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/crostini"
-	cui "chromiumos/tast/local/crostini/ui"
 	"chromiumos/tast/local/input"
-	"chromiumos/tast/local/vm"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/timing"
 )
 
 // ChromeOptions describe how to run chrome.New.
 type ChromeOptions struct {
-	extraArgs []string // passed to Chrome on initialization
-	timeout   time.Duration
+	ExtraArgs []string // passed to Chrome on initialization
+	Timeout   time.Duration
 }
 
-// ARCOptions describe how to start ARC. Pass nil if ARC is not needed.
-type ARCOptions struct {
-	timeout time.Duration
+// VMOptions describes how to start a VM.
+type VMOptions interface {
+	// Name returns a stable name for the VM. This is the same name that
+	// must be passed to StateManager.VM() to retrieve the VM instance.
+	Name() string
+	// ActivateTimeout returns the time needed to activate the VM.
+	ActivateTimeout() time.Duration
+	// Activate activates the requested VM. The operation should either
+	// succeed completely, or roll the VM back to a deactivated state.
+	Activate(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn, st StateManagerTestingState) (VMActivation, error)
 }
 
-// CrostiniOptions describe how to start Crostini. Pass nil if Crostini is not
-// needed.
-type CrostiniOptions struct {
-	mode           string                    // Where (download/build artifact) the container image comes from.
-	debianVersion  vm.ContainerDebianVersion // OS version of the container image.
-	minDiskSize    uint64                    // The minimum size of the VM image in bytes. 0 to use default disk size.
-	largeContainer bool
-	timeout        time.Duration
+// VMActivation represents an active VM instance. This interface is returned by
+// VMOptions to ensure the methods can only be called after a call to Activate.
+type VMActivation interface {
+	// CheckAndReset checks and cleans a VM, so it can be re-used for another test.
+	CheckAndReset(ctx context.Context, st StateManagerTestingState) error
+	// Deactivate the VM.
+	Deactivate(ctx context.Context) error
+	// VM returns a VM-specific object representing the active VM.
+	VM() interface{}
 }
 
 // StateManager allows Chrome and VMs to be activated, checked and cleaned
 // between tests, and deactivated.
 type StateManager struct {
 	// Activate options.
-	crOptions   ChromeOptions
-	useARC      *ARCOptions
-	useCrostini *CrostiniOptions
+	crOptions ChromeOptions
+	vmOptions map[string]VMOptions
 
-	// Managed Chrome and VM. Default zero values set inactive state.
-	cr          *chrome.Chrome
-	tconn       *chrome.TestConn
-	keyboard    *input.KeyboardEventWriter
-	arc         *arc.ARC
-	arcSnapshot *arc.Snapshot
-	crostini    *vm.Container
+	// Managed Chrome and VMs. Default zero values set inactive state.
+	cr       *chrome.Chrome
+	vms      map[string]VMActivation
+	tconn    *chrome.TestConn
+	keyboard *input.KeyboardEventWriter
 
 	active bool
 }
 
 // NewStateManager creates a state manager from ChromeOptions, and optional
-// ARCOptions and CrostiniOptions, if ARC and/or Crostini are to be launched.
-func NewStateManager(crOptions ChromeOptions, useARC *ARCOptions, useCrostini *CrostiniOptions) StateManager {
+// VMOptions, depending on the VMs to be launched.
+func NewStateManager(crOptions ChromeOptions, vms ...VMOptions) StateManager {
+	vmOpts := make(map[string]VMOptions)
+	for _, vmOpt := range vms {
+		vmOpts[vmOpt.Name()] = vmOpt
+	}
+
 	return StateManager{
-		crOptions:   crOptions,
-		useARC:      useARC,
-		useCrostini: useCrostini,
-		cr:          nil,
-		tconn:       nil,
-		keyboard:    nil,
-		arc:         nil,
-		arcSnapshot: nil,
-		crostini:    nil,
-		active:      false,
+		crOptions: crOptions,
+		vmOptions: vmOpts,
+		vms:       make(map[string]VMActivation),
+		cr:        nil,
+		tconn:     nil,
+		keyboard:  nil,
+		active:    false,
 	}
 }
 
@@ -82,8 +85,8 @@ type StateManagerTestingState interface {
 	DataPath(p string) string
 	OutDir() string
 	RequiredVar(name string) string
+	Var(name string) (val string, ok bool)
 	SoftwareDeps() []string
-	Fatal(...interface{})
 }
 
 // Chrome gets the active chrome.Chrome.
@@ -110,25 +113,30 @@ func (s *StateManager) Keyboard() *input.KeyboardEventWriter {
 	return s.keyboard
 }
 
-// ARC gets the active arc.ARC, or nil if not active.
-func (s *StateManager) ARC() *arc.ARC {
+// VMs returns the active VMs as map, keyed by a VM-defined name.
+func (s *StateManager) VMs() map[string]interface{} {
 	if !s.active {
-		panic("Do not call ARC when multivm.StateManager is not active")
+		panic("Do not call VMs when multivm.StateManager is not active")
 	}
-	return s.arc
-}
-
-// Crostini gets the active Crostini container, or nil if not active.
-func (s *StateManager) Crostini() *vm.Container {
-	if !s.active {
-		panic("Do not call Crostini when multivm.StateManager is not active")
+	result := make(map[string]interface{})
+	for k, v := range s.vms {
+		result[k] = v.VM()
 	}
-	return s.crostini
+	return result
 }
 
 // Active is true if Chrome and VMs are currently active.
 func (s *StateManager) Active() bool {
 	return s.active
+}
+
+// Timeout returns the total timeout needed to activate Chrome and all VMs.
+func (s *StateManager) Timeout() time.Duration {
+	duration := s.crOptions.Timeout
+	for _, v := range s.vmOptions {
+		duration += v.ActivateTimeout()
+	}
+	return duration
 }
 
 // Activate Chrome and any requested VMs.
@@ -149,19 +157,19 @@ func (s *StateManager) Activate(ctx context.Context, st StateManagerTestingState
 
 	// Chrome.
 	if err := func() error {
-		ctx, cancel := context.WithTimeout(ctx, s.crOptions.timeout)
+		ctx, cancel := context.WithTimeout(ctx, s.crOptions.Timeout)
 		defer cancel()
 
 		var opts []chrome.Option
-		if s.useARC != nil {
+		if _, ok := s.vmOptions[ARCName]; ok {
 			opts = append(opts, chrome.ARCEnabled())
 		} else {
 			opts = append(opts, chrome.ARCDisabled())
 		}
-		if s.useCrostini != nil {
+		if _, ok := s.vmOptions[CrostiniName]; ok {
 			opts = append(opts, chrome.ExtraArgs("--vmodule=crostini*=1"))
 		}
-		opts = append(opts, chrome.ExtraArgs(s.crOptions.extraArgs...))
+		opts = append(opts, chrome.ExtraArgs(s.crOptions.ExtraArgs...))
 
 		testing.ContextLog(ctx, "Creating Chrome")
 		var err error
@@ -181,53 +189,22 @@ func (s *StateManager) Activate(ctx context.Context, st StateManagerTestingState
 		return err
 	}
 
-	// ARC, if requested.
-	if s.useARC != nil {
+	for k, v := range s.vmOptions {
 		if err := func() error {
-			ctx, cancel := context.WithTimeout(ctx, s.useARC.timeout)
+			ctx, cancel := context.WithTimeout(ctx, v.ActivateTimeout())
 			defer cancel()
 
-			testing.ContextLog(ctx, "Creating ARC")
-			var err error
-			s.arc, err = arc.New(ctx, st.OutDir())
+			vm, err := v.Activate(ctx, s.cr, s.tconn, st)
 			if err != nil {
-				return errors.Wrap(err, "failed to start ARC")
+				return err
 			}
-			s.arcSnapshot, err = arc.NewSnapshot(ctx, s.arc)
-			if err != nil {
-				return errors.Wrap(err, "failed to take ARC state snapshot")
-			}
-			arc.Lock()
+			s.vms[k] = vm
 			return nil
 		}(); err != nil {
-			return err
+			return errors.Wrapf(err, "activating %s", k)
 		}
 	}
 
-	// Crostini, if requested.
-	if s.useCrostini != nil {
-		if err := func() error {
-			ctx, cancel := context.WithTimeout(ctx, s.useCrostini.timeout)
-			defer cancel()
-
-			testing.ContextLog(ctx, "Creating Crostini")
-			iOptions := crostini.GetInstallerOptions(st, true, s.useCrostini.debianVersion, s.useCrostini.largeContainer, s.cr.User())
-			iOptions.UserName = s.cr.User()
-			iOptions.MinDiskSize = s.useCrostini.minDiskSize
-			if _, err := cui.InstallCrostini(ctx, s.tconn, s.cr, iOptions); err != nil {
-				return errors.Wrap(err, "failed to install Crostini")
-			}
-			var err error
-			s.crostini, err = vm.DefaultContainer(ctx, s.cr.User())
-			if err != nil {
-				return errors.Wrap(err, "failed to connect to running container")
-			}
-			vm.Lock()
-			return nil
-		}(); err != nil {
-			return err
-		}
-	}
 	s.active = true
 	return nil
 }
@@ -250,19 +227,12 @@ func (s *StateManager) CheckAndReset(ctx context.Context, st StateManagerTesting
 		return errors.Wrap(err, "failed resetting Chrome")
 	}
 
-	// ARC.
-	if s.arc != nil {
-		if err := s.arcSnapshot.Restore(ctx, s.arc); err != nil {
-			return errors.Wrap(err, "failed to restore ARC")
+	for k, v := range s.vms {
+		if err := v.CheckAndReset(ctx, st); err != nil {
+			return errors.Wrapf(err, "checking and resetting %s", k)
 		}
 	}
 
-	// Crostini.
-	if s.crostini != nil {
-		if err := crostini.BasicCommandWorks(ctx, s.crostini); err != nil {
-			return errors.Wrap(err, "failed checking Crostini")
-		}
-	}
 	return nil
 }
 
@@ -310,32 +280,17 @@ func (s *StateManager) Deactivate(ctx context.Context) (errRet error) {
 		}()
 	}
 
-	if s.arc != nil {
-		defer func() {
-			arc.Unlock()
-			if err := s.arc.Close(); err != nil {
+	for k, v := range s.vms {
+		defer func(k string, v VMActivation) {
+			if err := v.Deactivate(ctx); err != nil {
 				if errRet == nil {
-					errRet = errors.Wrap(err, "failed to close ARC")
+					errRet = errors.Wrapf(err, "failed to deactivate %s", k)
 				} else {
-					testing.ContextLog(ctx, "Failed to close ARC")
+					testing.ContextLogf(ctx, "Failed to deactivate %s: %v", k, err)
 				}
 			}
-			s.arc = nil
-		}()
-	}
-
-	if s.crostini != nil {
-		defer func() {
-			vm.Unlock()
-			if err := s.crostini.VM.Stop(ctx); err != nil {
-				if errRet == nil {
-					errRet = errors.Wrap(err, "failed to deactivate Crostini")
-				} else {
-					testing.ContextLog(ctx, "Failed to deactivate Crostini: ", err)
-				}
-			}
-			s.crostini = nil
-		}()
+			delete(s.vms, k)
+		}(k, v)
 	}
 
 	return nil
