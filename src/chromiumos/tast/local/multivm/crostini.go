1f4cce645fc9a7b2408d716e4af4b946782903ff
Patrick Meiring <meiring@google.com>
2021-01-28 14:12:20 +1100

    multivm: Refactor multivm precondition to allow plugging in additional VMs.
    Currently, StateManager and the multi-vm precondition is closed to
    extension by virtue of accepting only options for predefined (crostini
    or arc) VM types. This makes the addition of additional VM types
    difficult, especially for private VMs (i.e. Parallels) which reside
    in internal bundles and whose types cannot be accessed from public
    Tast code.
    
    Refactor StateManager to accept any VM type implementing a designated
    interface and provide public constructors for MultiVM preconditions (as
    well as ARC and Crostini VMs) so that further MultiVM preconditions can
    be defined in internal bundles.
    
    BUG=b:178534364
    TEST=tast run ${DUT_IP} multivm.Login.*
    
    Change-Id: Ifb90710a3819068a252b507efc4fac3e5002defb

---
+++ src/chromiumos/tast/local/multivm/crostini.go
@@ -0,0 +1,116 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package multivm
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/crostini"
+	cui "chromiumos/tast/local/crostini/ui"
+	"chromiumos/tast/local/vm"
+	"chromiumos/tast/testing"
+)
+
+// CrostiniName is a stable name for the Crostini VM.
+const CrostiniName = "Crostini"
+
+// CrostiniOptions describe how to start Crostini.
+type CrostiniOptions struct {
+	Mode           string                    // Where (download/build artifact) the container image comes from.
+	DebianVersion  vm.ContainerDebianVersion // OS version of the container image.
+	MinDiskSize    uint64                    // The minimum size of the VM image in bytes. 0 to use default disk size.
+	LargeContainer bool
+}
+
+// crostiniActivation represents an instance of the Crostini VM used in a
+// multi-VM test.
+type crostiniActivation struct {
+	container *vm.Container
+}
+
+// Name returns a stable name for the Crostini VM.
+func (o CrostiniOptions) Name() string {
+	return CrostiniName
+}
+
+// ChromeOpts returns the Chrome option(s) that should be passed to
+// chrome.New().
+func (o CrostiniOptions) ChromeOpts() []chrome.Option {
+	return []chrome.Option{chrome.ExtraArgs("--vmodule=crostini*=1")}
+}
+
+// ActivateTimeout returns the time needed to setup the Crostini VM.
+func (o CrostiniOptions) ActivateTimeout() time.Duration {
+	return 7 * time.Minute
+}
+
+// Activate spins up the Crostini VM.
+func (o CrostiniOptions) Activate(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn, st StateManagerTestingState) (VMActivation, error) {
+	testing.ContextLog(ctx, "Creating Crostini")
+	iOptions := crostini.GetInstallerOptions(st, true, o.DebianVersion, o.LargeContainer, cr.User())
+	iOptions.UserName = cr.User()
+	iOptions.MinDiskSize = o.MinDiskSize
+	if _, err := cui.InstallCrostini(ctx, tconn, cr, iOptions); err != nil {
+		return nil, errors.Wrap(err, "installing Crostini")
+	}
+
+	var container *vm.Container
+	success := false
+	defer func() {
+		if !success {
+			if container != nil {
+				cleanupCrostini(ctx, container)
+			}
+		}
+	}()
+
+	// container may be set even when an error is returned. This must be cleaned
+	// up if there was an error.
+	container, err := vm.DefaultContainer(ctx, cr.User())
+	if err != nil {
+		return nil, errors.Wrap(err, "connecting to running container")
+	}
+	success = true
+	vm.Lock()
+	return &crostiniActivation{container: container}, nil
+}
+
+// CheckAndReset checks the Crostini VM between tests.
+func (c *crostiniActivation) CheckAndReset(ctx context.Context, st StateManagerTestingState) error {
+	if err := crostini.BasicCommandWorks(ctx, c.container); err != nil {
+		return errors.Wrap(err, "checking Crostini")
+	}
+	return nil
+}
+
+// Deactivate stops the Crostini VM.
+func (c *crostiniActivation) Deactivate(ctx context.Context) error {
+	vm.Unlock()
+	return cleanupCrostini(ctx, c.container)
+}
+
+func cleanupCrostini(ctx context.Context, container *vm.Container) error {
+	if err := container.VM.Stop(ctx); err != nil {
+		return errors.Wrap(err, "stopping Crostini")
+	}
+	return nil
+}
+
+// VM returns the underlying crostini container.
+func (c *crostiniActivation) VM() interface{} {
+	return c.container
+}
+
+// CrostiniFromPre returns the Crostini instance setup by the multi-vm
+// precondition (if available).
+func CrostiniFromPre(pre *PreData) *vm.Container {
+	if container, ok := pre.VMs[CrostiniName]; ok {
+		return container.(*vm.Container)
+	}
+	return nil
+}
