cd8cbdc708316ccf2eb1d96ab17a4b64be42ce01
Yi Chou <yich@google.com>
2021-01-30 02:08:48 +0800

    hwsec: add some utils to drop lockout permission in tast
    BUG=b:172358080
    TEST=tast run $DUT hwsec.*
    TEST=CQ
    
    Change-Id: I0da4d7a5d1d67b6215c609123afee3de7d72750a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2636703
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>

--- src/chromiumos/tast/common/hwsec/helpers.go
+++ src/chromiumos/tast/common/hwsec/helpers.go
@@ -10,9 +10,15 @@ This file implements miscellaneous and unsorted helpers.
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
 	"time"
 
+	"github.com/golang/protobuf/proto"
+
+	tmpb "chromiumos/system_api/tpm_manager_proto"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/shutil"
 	"chromiumos/tast/testing"
 )
 
@@ -24,9 +30,10 @@ type CmdRunner interface {
 // Helper provides various helper functions that could be shared across all
 // hwsec integration test regardless of run-type, i.e., remote or local.
 type Helper struct {
-	cmdRunner      CmdRunner
-	cryptohomeUtil *UtilityCryptohomeBinary
-	tpmManagerUtil *UtilityTpmManagerBinary
+	cmdRunner        CmdRunner
+	cryptohomeUtil   *UtilityCryptohomeBinary
+	tpmManagerUtil   *UtilityTpmManagerBinary
+	daemonController *DaemonController
 }
 
 // NewHelper creates a new Helper, with r responsible for CmdRunner.
@@ -39,10 +46,12 @@ func NewHelper(r CmdRunner) (*Helper, error) {
 	if err != nil {
 		return nil, err
 	}
+	daemonController := NewDaemonController(r)
 	return &Helper{
-		cmdRunner:      r,
-		cryptohomeUtil: cryptohomeUtil,
-		tpmManagerUtil: tpmManagerUtil,
+		cmdRunner:        r,
+		cryptohomeUtil:   cryptohomeUtil,
+		tpmManagerUtil:   tpmManagerUtil,
+		daemonController: daemonController,
 	}, nil
 }
 
@@ -54,6 +63,9 @@ func (h *Helper) CryptohomeUtil() *UtilityCryptohomeBinary { return h.cryptohome
 
 // TPMManagerUtil exposes the tpmManagerUtil of helper
 func (h *Helper) TPMManagerUtil() *UtilityTpmManagerBinary { return h.tpmManagerUtil }
+
+// DaemonController exposes the daemonController of helper
+func (h *Helper) DaemonController() *DaemonController { return h.daemonController }
 
 // EnsureTPMIsReady ensures the TPM is ready when the function returns |nil|.
 // Otherwise, returns any encountered error.
@@ -99,4 +111,112 @@ func (h *Helper) EnsureIsPreparedForEnrollment(ctx context.Context, timeout time
 		Timeout:  timeout,
 		Interval: PollingInterval,
 	})
+}
+
+// RemoveFile would delete the file
+func (h *Helper) RemoveFile(ctx context.Context, filename string) error {
+	_, err := h.cmdRunner.Run(ctx, "rm", "-f", "--", filename)
+	return err
+}
+
+// ReadFile would read data from the file
+func (h *Helper) ReadFile(ctx context.Context, filename string) ([]byte, error) {
+	return h.cmdRunner.Run(ctx, "cat", "--", filename)
+}
+
+// WriteFile would write data into the file
+func (h *Helper) WriteFile(ctx context.Context, filename string, data []byte) error {
+	// Because we may pass NULL('\0') byte in data, using base64 to encode the data could resolve the escape character issue.
+	// Using "echo" or "printf" would require a complex escaping rule to let it work correctly.
+	b64String := base64.StdEncoding.EncodeToString(data)
+	echoStrCmd := fmt.Sprintf("echo %s", shutil.Escape(b64String))
+	b64DecCmd := fmt.Sprintf("base64 -d > %s", shutil.Escape(filename))
+	cmd := fmt.Sprintf("%s | %s", echoStrCmd, b64DecCmd)
+	if _, err := h.cmdRunner.Run(ctx, "sh", "-c", cmd); err != nil {
+		return errors.Wrap(err, "failed to echo string")
+	}
+	return nil
+}
+
+// GetTPMManagerLocalData would read the tpm_manager local_tpm_data.
+// Note: Get the data without stopping tpm_managerd may result stale data.
+func (h *Helper) GetTPMManagerLocalData(ctx context.Context) ([]byte, error) {
+	return h.ReadFile(ctx, "/var/lib/tpm_manager/local_tpm_data")
+}
+
+// SetTPMManagerLocalData would write the local_tpm_data.
+// Because tpm_managerd may cache the local data in the memory, we would need to restart tpm_managerd after modifying the data.
+func (h *Helper) SetTPMManagerLocalData(ctx context.Context, data []byte) error {
+	return h.WriteFile(ctx, "/var/lib/tpm_manager/local_tpm_data", data)
+}
+
+// DropResetLockPermissions drops the reset lock permissions and return a callback to restore the permissions.
+func (h *Helper) DropResetLockPermissions(ctx context.Context) (restoreFunc func(ctx context.Context) error, retErr error) {
+	// Stop TPM Manager before modifying its local data.
+	if err := h.daemonController.StopTpmManager(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to stop TPM Manager")
+	}
+
+	// Restart it after finishing all operation.
+	defer func() {
+		if err := h.daemonController.StartTpmManager(ctx); err != nil {
+			if retErr == nil {
+				retErr = errors.Wrap(err, "failed to start TPM Manager")
+			} else {
+				testing.ContextLog(ctx, "Failed to take screenshot: ", err)
+			}
+		}
+	}()
+
+	rawData, err := h.GetTPMManagerLocalData(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get local TPM data")
+	}
+
+	var data tmpb.LocalData
+	if err := proto.Unmarshal(rawData, &data); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal local TPM data")
+	}
+
+	// Drop the owner password, so tpm_manager couldn't use it to create owner delegate on TPM1.2 device.
+	data.OwnerPassword = []byte{}
+	// Drop the owner delegate, so tpm_manager couldn't use it to reset DA counter on TPM1.2 device.
+	data.OwnerDelegate = &tmpb.AuthDelegate{}
+
+	// Drop the lockout password, so tpm_manager couldn't use it to reset DA counter on TPM2.0 device.
+	data.LockoutPassword = []byte{}
+
+	newData, err := proto.Marshal(&data)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal local TPM data")
+	}
+
+	// Write back the data into the local data of tpm_manager.
+	if err := h.SetTPMManagerLocalData(ctx, newData); err != nil {
+		return nil, errors.Wrap(err, "failed to set local TPM data")
+	}
+
+	return func(ctx context.Context) error {
+		// Stop TPM Manager before modifying its local data.
+		if err := h.daemonController.StopTpmManager(ctx); err != nil {
+			return errors.Wrap(err, "failed to stop TPM Manager")
+		}
+
+		// Restart it after finishing all operation.
+		defer func() {
+			if err := h.daemonController.StartTpmManager(ctx); err != nil {
+				if retErr == nil {
+					retErr = errors.Wrap(err, "failed to start TPM Manager")
+				} else {
+					testing.ContextLog(ctx, "Failed to take screenshot: ", err)
+				}
+			}
+		}()
+
+		// Restore the local data.
+		if err := h.SetTPMManagerLocalData(ctx, rawData); err != nil {
+			return errors.Wrap(err, "failed to restore local TPM data")
+		}
+		return nil
+	}, nil
 }
