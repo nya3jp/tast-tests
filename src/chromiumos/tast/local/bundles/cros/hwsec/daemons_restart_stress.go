746a1cd62d274128188fcf9f855a086cff40f53c
Yi Chou <yich@google.com>
2021-01-30 02:23:10 +0800

    hwsec: Add TPM daemons reset stress test
    BUG=b:172358080
    TEST=tast run $DUT hwsec.*
    TEST=revert crrev/c/2340830, and the tast would fail on chell
    
    Change-Id: I834e7d968683dda4b6d339736ec89325ade8118c

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/daemons_restart_stress.go
@@ -0,0 +1,122 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	hwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DaemonsRestartStress,
+		Desc: "Verifies that restarting hwsec daemons wouldn't cause problems",
+		Contacts: []string{
+			"cros-hwsec@chromium.org",
+			"yich@chromium.org",
+		},
+		SoftwareDeps: []string{"tpm"},
+		Attr:         []string{"group:mainline", "informational"},
+		Timeout:      10 * time.Minute,
+	})
+}
+
+// DaemonsRestartStress checks that restarting hwsec daemons wouldn't cause problems.
+func DaemonsRestartStress(ctx context.Context, s *testing.State) {
+	cmdRunner, err := hwseclocal.NewCmdRunner()
+	if err != nil {
+		s.Fatal("Failed to create CmdRunner: ", err)
+	}
+	helper, err := hwseclocal.NewHelper(cmdRunner)
+	if err != nil {
+		s.Fatal("Failed to create hwsec local helper: ", err)
+	}
+	tpmManagerUtil := helper.TPMManagerUtil
+	daemonController := helper.DaemonController
+
+	// Check that lockout shouldn't be in effect.
+	info, err := tpmManagerUtil.GetDAInfo(ctx)
+	if err != nil {
+		s.Fatal("Failed to get dictionary attack info: ", err)
+	}
+	if info.InEffect {
+		s.Fatal("Lockout in effect before testing")
+	}
+
+	ctxForResumeDaemons := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 2*time.Minute)
+	defer cancel()
+
+	// Drop the DA reset permission.
+	restorePermCall, err := helper.DropResetLockPermissions(ctx)
+	if err != nil {
+		s.Fatal("Failed to drop the DA reset permission: ", err)
+	}
+	defer func(ctx context.Context) {
+		// Restore the DA reset permission.
+		if err = restorePermCall(ctx); err != nil {
+			s.Log("Failed to restore lockout permission: ", err)
+		}
+	}(ctxForResumeDaemons)
+
+	tpmVer, err := helper.GetTPMVersion(ctx)
+	if err != nil {
+		s.Fatal("Failed to get TPM version: ", err)
+	}
+
+	// Restart TPM related daemons multiple times.
+	for i := 0; i < 10; i++ {
+		if err := daemonController.StopCryptohome(ctx); err != nil {
+			s.Fatal("Failed to stop cryptohomed: ", err)
+		}
+		if err := daemonController.StartAttestation(ctx); err != nil {
+			s.Fatal("Failed to stop attestationd: ", err)
+		}
+		if err := daemonController.StopTpmManager(ctx); err != nil {
+			s.Fatal("Failed to stop tpm_managerd: ", err)
+		}
+		switch tpmVer {
+		case "1.2":
+			if daemonController.RestartTcsd(ctx); err != nil {
+				s.Fatal("Failed to restart tcsd: ", err)
+			}
+		case "2.0":
+			if daemonController.RestartTrunks(ctx); err != nil {
+				s.Fatal("Failed to restart trunksd: ", err)
+			}
+		default:
+			s.Fatal("Unknown TPM version: ")
+		}
+		if err := daemonController.StartTpmManager(ctx); err != nil {
+			s.Fatal("Failed to start tpm_managerd: ", err)
+		}
+		if err := daemonController.StartAttestation(ctx); err != nil {
+			s.Fatal("Failed to start attestationd: ", err)
+		}
+		if err := daemonController.StartCryptohome(ctx); err != nil {
+			s.Fatal("Failed to start cryptohomed: ", err)
+		}
+	}
+
+	// Check counter didn't increase too much, and lockout shouldn't be in effect.
+	newInfo, err := tpmManagerUtil.GetDAInfo(ctx)
+	if err != nil {
+		s.Fatal("Failed to get dictionary attack info: ", err)
+	}
+
+	// The counter shouldn't constantly increase when we restart daemons multiple time.
+	// But it's acceptable to increase a little bit.
+	// For example: we need one quota to check the validity of empty password.
+	if newInfo.Counter > info.Counter+1 {
+		s.Fatalf("Unexpected counter increase, %d -> %d", info.Counter, newInfo.Counter)
+	}
+	if newInfo.InEffect {
+		s.Fatal("Lockout in effect after testing")
+	}
+}
