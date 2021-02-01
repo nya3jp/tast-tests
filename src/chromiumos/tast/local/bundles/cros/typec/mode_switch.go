d7df942d989cc99e804ec655fbfff8cd531b661b
Prashant Malani <pmalani@chromium.org>
2021-02-01 12:11:29 -0800

    tast-tests: typec: Add mode switch test
    Add a test which checks that USB Type C mode switching logic works as
    expected when connected to a Thunderbolt dock which supports USB+DP
    alternate mode.
    
    BUG=b:178774974
    TEST=Run the test on a voxel with an EC image support mode entry, and
    connected to a DP monitor via a TBT dock.
    
    Change-Id: I9aed1cde3ab5f0ac3de7231ad6a2104657c9c436

---
+++ src/chromiumos/tast/local/bundles/cros/typec/mode_switch.go
@@ -0,0 +1,165 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package typec
+
+import (
+	"context"
+	"io/ioutil"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/graphics"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ModeSwitch,
+		Desc:         "Checks USB Type C mode switch behaviour on login",
+		Contacts:     []string{"pmalani@chromium.org"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Vars:         []string{"ui.signinProfileTestExtensionManifestKey"},
+	})
+}
+
+// Since we can't pass the TestConn reference in the Poll functions, we stash it
+// a global variable.
+var testConn *chrome.TestConn
+
+// ModeSwitch does the following:
+// - Go to the login screen.
+// - Validate USB+DP alt mode is working correctly.
+// - Login.
+// - Validate that TBT alt mode is working correctly.
+//
+// This test requires the following H/W topology to run.
+//
+//
+//        DUT ------> Thunderbolt3 (>= Titan Ridge) dock -----> DP monitor.
+//      (USB4)
+//
+func ModeSwitch(ctx context.Context, s *testing.State) {
+	// Get to the Chrome login screen.
+	cr, err := chrome.New(ctx,
+		chrome.DeferLogin(),
+		chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey")))
+	if err != nil {
+		s.Fatal("Failed to start Chrome at login screen: ", err)
+	}
+	defer cr.Close(ctx)
+
+	testConn, err = cr.SigninProfileTestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get Test API connection: ", err)
+	}
+
+	s.Log("Verifying that no TBT devices enumerated")
+
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		files, err := ioutil.ReadDir("/sys/bus/thunderbolt/devices")
+		if err != nil {
+			return err
+		}
+		for _, file := range files {
+			// Check for built-in TBT devices.
+			if file.Name() == "domain0" || file.Name() == "0-0" {
+				continue
+			}
+			return errors.Errorf("found TBT device: %s", file.Name())
+		}
+		return nil
+	}, &testing.PollOptions{Interval: 100 * time.Millisecond, Timeout: 10 * time.Second})
+	if err != nil {
+		s.Fatal("Failed to verify no TBT devices connected at login screen: ", err)
+	}
+
+	s.Log("Verifying that DP monitor enumerated correctly")
+
+	err = testing.Poll(ctx, findConnectedDPMonitor,
+		&testing.PollOptions{Interval: 200 * time.Millisecond, Timeout: 20 * time.Second})
+	if err != nil {
+		s.Fatal("Failed to verify DP monitor working at login screen: ", err)
+	}
+
+	if err := cr.ContinueLogin(ctx); err != nil {
+		s.Fatal("Failed to login: ", err)
+	}
+
+	s.Log("Verifying that TBT device enumerated correctly")
+
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		files, err := ioutil.ReadDir("/sys/bus/thunderbolt/devices")
+		if err != nil {
+			return err
+		}
+		for _, file := range files {
+			// Check for non-built-in TBT devices.
+			if file.Name() != "domain0" && file.Name() != "0-0" {
+				return nil
+			}
+		}
+
+		return errors.New("no external TBT device found")
+	}, &testing.PollOptions{Interval: 100 * time.Millisecond, Timeout: 10 * time.Second})
+	if err != nil {
+		s.Fatal("Failed to verify TBT devices connected after login: ", err)
+	}
+
+	s.Log("Verifying that DP monitor enumerated correctly")
+
+	err = testing.Poll(ctx, findConnectedDPMonitor,
+		&testing.PollOptions{Interval: 200 * time.Millisecond, Timeout: 20 * time.Second})
+	if err != nil {
+		s.Fatal("Failed to verify DP monitor working after login: ", err)
+	}
+}
+
+// findConnectedDPMonitor checks the following two conditions:
+// - that modetest indicates a connected Display Port connector
+// - that there is a enabled "non-internal" display.
+//
+// These two signals are used as to determine whether a DP monitor is successfully connected and showing the extended screen.
+func findConnectedDPMonitor(ctx context.Context) error {
+	connectors, err := graphics.ModetestConnectors(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get connectors")
+	}
+
+	var foundConnected = false
+	for _, connector := range connectors {
+		// We're only interested in DP connectors.
+		matched, err := regexp.MatchString(`^DP-\d`, connector.Name)
+		if err != nil {
+			return err
+		}
+
+		if matched && connector.Connected {
+			foundConnected = true
+			break
+		}
+	}
+
+	if !foundConnected {
+		return errors.New("no connected DP connector found")
+	}
+
+	// Check the DisplayInfo from the Test API connection for a connected extended display.
+	infos, err := display.GetInfo(ctx, testConn)
+	if err != nil {
+		return errors.New("failed to get display info from test conn")
+	}
+
+	for _, info := range infos {
+		if !info.IsInternal && info.IsEnabled {
+			return nil
+		}
+	}
+
+	return errors.New("no enabled and working external display found")
+}
