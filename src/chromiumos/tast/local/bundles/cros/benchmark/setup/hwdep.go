18df3c4822b6cc340507c9a1153b6a5ed1ca92e0
Xibin Liu <xliu@cienet.com>
2021-02-01 15:32:31 -0800

    public benchmark - GeekBench Android version.
    This is one of the public benchmark tests - GeekBench Android version.
    The test installs GeekBench, run it and record the BeekBench test score.
    
    BUG=None
    TEST=tast run <DUT> benchmark.Geekbench5AndroidApp
    
    Change-Id: I36648ec3007725bd4ace4c3e385b6e57ca29ad60

---
+++ src/chromiumos/tast/local/bundles/cros/benchmark/setup/hwdep.go
@@ -0,0 +1,15 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package setup
+
+import (
+	"chromiumos/tast/testing/hwdep"
+)
+
+// PublicBenchmarkAllowed returns the DUT model dependency of running public benchmark tests.
+// Allowed hardware models will be white listed here.
+func PublicBenchmarkAllowed() hwdep.Condition {
+	return hwdep.Model("barla", "bluebird", "eve", "krane", "liara", "maple14", "pantheon", "kohaku")
+}
