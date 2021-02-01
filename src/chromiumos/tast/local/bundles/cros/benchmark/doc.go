585a7c09df5fbd9ff806ebd3c74106ee6b8fffd6
Xibin Liu <xliu@cienet.com>
2021-02-01 15:32:31 -0800

    public benchmark - GeekBench Android version.
    This is one of the public benchmark tests - GeekBench Android version.
    The test installs GeekBench, run it and record the BeekBench test score.
    
    BUG=None
    TEST=tast run <DUT> benchmark.Geekbench5AndroidApp
    
    Change-Id: I36648ec3007725bd4ace4c3e385b6e57ca29ad60

---
+++ src/chromiumos/tast/local/bundles/cros/benchmark/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package benchmark contains local Tast tests that exercise external
+// benchmark software on the DUT.
+package benchmark
