38a4aec7414e1ae1a77b56a3605a3c4d5399cc7b
Steven Bennetts <stevenjb@chromium.org>
2021-02-01 10:44:13 -0800

    shill: Add shill_cellular_smoke test
    Adds a smoke test for reading bytes over a Cellular network.
    
    BUG=b:177103167
    TEST=tast run ${DUT} network.ShillCellularSmoke
    
    Change-Id: Ifc60989ed102f10f3bae384a17ed666750a65f82

---
+++ src/chromiumos/tast/local/bundles/cros/network/shill_cellular_smoke.go
@@ -0,0 +1,159 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/shillconst"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/network/cellular"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+// Note: This test enables and connects to Cellular if not already enabled or connected.
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ShillCellularSmoke,
+		Desc: "Verifies that traffic can be sent over the Cellular network",
+		Contacts: []string{
+			"stevenjb@google.com",
+			"chromeos-cellular-team@google.com",
+		},
+		Attr: []string{"group:cellular"},
+	})
+}
+
+func ShillCellularSmoke(ctx context.Context, s *testing.State) {
+	helper, err := cellular.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create cellular.Helper: ", err)
+	}
+
+	// Disable Ethernet and/or WiFi if present and defer re-enabling.
+	ethernetEnabled, err := helper.Manager.IsEnabled(ctx, shill.TechnologyEthernet)
+	if err != nil {
+		s.Fatal("Unable to disable Ethernet: ", err)
+	}
+	if ethernetEnabled {
+		if err := helper.Manager.DisableTechnology(ctx, shill.TechnologyEthernet); err != nil {
+			s.Fatal("Unable to disable Ethernet: ", err)
+		}
+	}
+	wifiEnabled, err := helper.Manager.IsEnabled(ctx, shill.TechnologyWifi)
+	if err != nil {
+		s.Fatal("Unable to disable Wifi: ", err)
+	}
+	if wifiEnabled {
+		if err := helper.Manager.DisableTechnology(ctx, shill.TechnologyWifi); err != nil {
+			s.Fatal("Unable to disable WiFi: ", err)
+		}
+	}
+	if ethernetEnabled || wifiEnabled {
+		ctxForEnable := ctx
+		newCtx, cancel := ctxutil.Shorten(ctx, 1*time.Second)
+		ctx = newCtx
+		defer cancel()
+		defer func(ctx context.Context) {
+			if ethernetEnabled {
+				if err := helper.Manager.EnableTechnology(ctx, shill.TechnologyEthernet); err != nil {
+					s.Fatal("Unable to enable Ethernet: ", err)
+				}
+			}
+			if wifiEnabled {
+				if err := helper.Manager.EnableTechnology(ctx, shill.TechnologyWifi); err != nil {
+					s.Fatal("Unable to enable WiFi: ", err)
+				}
+			}
+		}(ctxForEnable)
+	}
+
+	// Verify that a connectable Cellular service exists and ensure it is connected.
+	service, err := helper.FindServiceForDevice(ctx)
+	if err != nil {
+		s.Fatal("Unable to find Cellular Service for Device: ", err)
+	}
+	isConnected, err := service.IsConnected(ctx)
+	if err != nil {
+		s.Fatal("Unable to find get IsConnected for Service: ", err)
+	}
+	if !isConnected {
+		if err := service.Connect(ctx); err != nil {
+			s.Fatal("Unable to Connect to Service: ", err)
+		}
+	}
+
+	// This URL comes from src/third_party/autotest/files/client/cros/network.py.
+	// Unfortunately it is not documented anywhere in the chromeos source or google3.
+	const hostName = "testing-chargen.appspot.com"
+
+	// Verify that traffic will be sent over Cellular by getting the routing interface for hostName
+	// and verifying that it matches the interface for the Cellular Device.
+
+	// LookupHost periodically fails, so try multiple times to prevent flakiness.
+	const lookupAttempts = 10
+	var addrs []string
+	for i := 0; i < lookupAttempts; i++ {
+		addrs, err = net.LookupHost(hostName)
+		if err == nil && len(addrs) >= 1 {
+			break
+		}
+		testing.Sleep(ctx, 100*time.Millisecond)
+	}
+	if err != nil || len(addrs) < 1 {
+		s.Fatalf("Unable to lookup address for host: %s: after %d attempts: %s", hostName, lookupAttempts, err)
+	}
+	s.Log("IP: ", addrs[0])
+
+	route, err := testexec.CommandContext(ctx, "ip", "route", "get", addrs[0]).Output()
+	if err != nil {
+		s.Fatal("Error running 'ip route': ", err)
+	}
+	// 'ip route get' returns strings in the format:
+	// <addr> via <gateway> dev <interface>
+	routeIface := strings.Split(string(route), " ")[4]
+
+	props, err := helper.Device.GetShillProperties(ctx)
+	if err != nil {
+		s.Fatal("Error getting Device properties: ", err)
+	}
+	deviceIface, err := props.GetString(shillconst.DevicePropertyInterface)
+	if err != nil {
+		s.Fatal("Error getting Device.Interface property: ", err)
+	}
+	if routeIface != deviceIface {
+		s.Fatalf("Wrong route interface: got %s, Device interface: %s", routeIface, deviceIface)
+	}
+
+	// This pattern also comes from src/third_party/autotest/files/client/cros/network.py
+	// and is undocumented.
+	const downloadBytes = 65536
+	fetchURL := fmt.Sprintf("http://%s/download?size=%d", hostName, downloadBytes)
+	s.Log("Fetch URL: ", fetchURL)
+
+	// Get data from |fetchURL| and confirm that the correct number of bytes are received.
+	resp, err := http.Get(fetchURL)
+	if err != nil {
+		s.Fatalf("Error fetching data from URL %q: %s", fetchURL, err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		s.Fatal("Error reading data: ", err)
+	}
+	bytesRead := len(body)
+	if bytesRead != downloadBytes {
+		s.Fatalf("Read wrong number of bytes: got %d, want %d", bytesRead, downloadBytes)
+	}
+}
