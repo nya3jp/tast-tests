f11e558765ef492c76aaa6f4fe36b50e2f2a9b73
Renuga Nanjappan <rnanjappan@chromium.org>
2021-02-01 19:54:18 +0000

    Tast Test: Add Test to Verify the OOBE Flow.
    Test navigates through OOBE via UI and verifies that ARC settings
    screen is launched at the end of OOBE flow.
    
    BUG=b:157842823
    TEST=tast -verbose run <DUT> arc.OobeArc
    
    Change-Id: Id3e90d3d821ed8db1cf863af8257f3a248262e06

---
+++ src/chromiumos/tast/local/bundles/cros/arc/oobe_arc.go
@@ -0,0 +1,97 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/optin"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui/faillog"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         OobeArc,
+		Desc:         "Navigate through OOBE and Verify that PlayStore Settings Screen is launched at the end",
+		Contacts:     []string{"rnanjappan@google.com", "cros-arc-te@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		Timeout: chrome.LoginTimeout + arc.BootTimeout + 120*time.Second,
+		Vars:    []string{"arc.username", "arc.password"},
+	})
+}
+
+func OobeArc(ctx context.Context, s *testing.State) {
+
+	username := s.RequiredVar("arc.username")
+	password := s.RequiredVar("arc.password")
+
+	cr, err := chrome.New(ctx, chrome.GAIALogin(), chrome.DontSkipOOBEAfterLogin(),
+		chrome.ARCSupported(),
+		chrome.Auth(username, password, "gaia-id"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	ui := uiauto.New(tconn)
+
+	// To tap on Accept and continue/Got it whichever the OOBE flow displays.
+	if err := ui.LeftClick(nodewith.Name("Accept and continue").Role(role.Button))(ctx); err != nil {
+		s.Log("Failed to click Accept and continue : ", err)
+		if err := ui.LeftClick(nodewith.Name("Got it").Role(role.Button))(ctx); err != nil {
+			s.Fatal("Failed to click Got it : ", err)
+		}
+	}
+
+	if err := uiauto.Run(ctx,
+		ui.LeftClick(nodewith.Name("More").Role(role.Button)),
+		ui.LeftClick(nodewith.Name("Review Google Play options following setup").Role(role.CheckBox)),
+		ui.LeftClick(nodewith.Name("Accept").Role(role.Button)),
+		ui.LeftClick(nodewith.Name("No thanks").Role(role.Button)),
+		ui.LeftClick(nodewith.Name("Get started").Role(role.Button)),
+	); err != nil {
+		s.Fatal("Failed to smoke test the Files App: ", err)
+	}
+
+	s.Log("Verify Play Store is On")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		playStoreState, err := optin.GetPlayStoreState(ctx, tconn)
+		if err != nil {
+			return errors.Wrap(err, "failed to get some playstore state")
+		}
+		if playStoreState["enabled"] == false {
+			return errors.New("playstore is off")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+		s.Fatal("Failed to verify Play Store is On: ", err)
+	}
+
+	s.Log("Verify Play Store Settings is Launched")
+	if err := ui.WaitUntilExists(nodewith.Name("Remove Google Play Store").Role(role.Button))(ctx); err != nil {
+		s.Fatal("Failed to Launch Android Settings After OOBE Flow : ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+}
