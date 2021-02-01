7d5b9687e152b429f604a5d000acbce332fb362c
Akihiro Ota <akihiroota@chromium.org>
2021-02-01 14:08:36 -0800

    tast-tests: Write a spoken feedback test.
    This change creates a test for spoken feedback. To do so,
    this change:
    1. Adds a new SpeechMonitor type, which connects to the
    Google TTS background page and is used to verify spoken
    utterances.
    2. Adds a PressKeysAndConsumeUtterances convenience
    method, which makes it easy to write the test.
    
    BUG=chromium:1168327
    TEST=a11y.SpokenFeedback
    
    Change-Id: I314b7d574f23dd94bbb9288f81e4d4c4ced53a45

---
+++ src/chromiumos/tast/local/bundles/cros/a11y/spoken_feedback.go
@@ -0,0 +1,69 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package a11y provides functions to assist with interacting with accessibility
+// features and settings.
+package a11y
+
+import (
+	"context"
+
+	"chromiumos/tast/local/a11y"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SpokenFeedback,
+		Desc: "A spoken feedback test that executes ChromeVox commands and keyboard shortcuts, and verifies that correct speech is given by the Google TTS engine",
+		Contacts: []string{
+			"akihiroota@chromium.org",      // Test author
+			"chromeos-a11y-eng@google.com", // Backup mailing list
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func SpokenFeedback(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	if err := a11y.SetFeatureEnabled(ctx, tconn, a11y.SpokenFeedback, true); err != nil {
+		s.Fatal("Failed to enable spoken feedback: ", err)
+	}
+	defer func() {
+		if err := a11y.SetFeatureEnabled(ctx, tconn, a11y.SpokenFeedback, false); err != nil {
+			s.Error("Failed to disable spoken feedback: ", err)
+		}
+	}()
+
+	cvconn, err := a11y.NewChromeVoxConn(ctx, cr)
+	if err != nil {
+		s.Fatal("Failed to connect to the ChromeVox background page: ", err)
+	}
+	defer cvconn.Close()
+
+	sm, err := a11y.NewSpeechMonitor(ctx, cr)
+	if err != nil {
+		s.Fatal("Failed to connect to the Google TTS background page: ", err)
+	}
+	defer sm.Close()
+
+	if err := a11y.PressKeysAndConsumeUtterances(ctx, sm, []string{"Search+O", "O"}, []string{"tab created", "ChromeVox Options"}); err != nil {
+		s.Error("Error when pressing keys and expecting speech: ", err)
+	}
+
+	if err := a11y.PressKeysAndConsumeUtterances(ctx, sm, []string{"Alt+Shift+L"}, []string{"Launcher", "Button", "Shelf", "Tool bar", "Press Search plus Space to activate"}); err != nil {
+		s.Error("Error when pressing keys and expecting speech: ", err)
+	}
+	if err := a11y.PressKeysAndConsumeUtterances(ctx, sm, []string{"Alt+Shift+S"}, []string{"Quick Settings, Press search plus left to access the notification center., window"}); err != nil {
+		s.Error("Error when pressing keys and expecting speech: ", err)
+	}
+}
