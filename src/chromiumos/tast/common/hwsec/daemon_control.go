77430873e6002bcd2e7c86bbc9a3a1571dcfc5fe
Yi Chou <yich@google.com>
2021-02-01 17:58:23 +0800

    hwsec: using gdbus to wait for dbus services
    So we don't need to use polling to check the services status.
    
    BUG=b:181183725
    TEST=tast run $DUT hwsec.*
    
    Change-Id: I80b585b686a0aa1e0163a729f68c2227a01e38c8

--- src/chromiumos/tast/common/hwsec/daemon_control.go
+++ src/chromiumos/tast/common/hwsec/daemon_control.go
@@ -13,11 +13,8 @@ instead of only (re)starting them.
 
 import (
 	"context"
-	"strings"
-	"time"
 
 	"chromiumos/tast/errors"
-	"chromiumos/tast/testing"
 )
 
 // DaemonInfo represents the information for a daemon.
@@ -111,23 +108,11 @@ func (dc *DaemonController) waitForDBusService(ctx context.Context, info *Daemon
 	if !info.HasDBus {
 		return errors.Errorf("%s doesn't have D-Bus interface", info.Name)
 	}
-	// Without quote, we might find something prefixed by name.
-	name := "\"" + info.DBusName + "\""
-	return testing.Poll(ctx, func(ctx context.Context) error {
-		if out, err := dc.r.Run(
-			ctx,
-			"dbus-send",
-			"--system",
-			"--dest=org.freedesktop.DBus",
-			"--print-reply",
-			"/org/freedesktop/DBus",
-			"org.freedesktop.DBus.ListNames"); err != nil {
-			return err
-		} else if strings.Contains(string(out), name) {
-			return nil
-		}
-		return errors.New("daemon not up")
-	}, &testing.PollOptions{Interval: 100 * time.Millisecond, Timeout: 15 * time.Second})
+	name := info.DBusName
+	if _, err := dc.r.Run(ctx, "gdbus", "wait", "--system", name); err != nil {
+		return errors.Wrapf(err, "failed to wait for D-Bus service %s", name)
+	}
+	return nil
 }
 
 // Start starts a daemon and waits until the D-Bus interface is responsive if it has D-Bus interface.
