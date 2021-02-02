775b9be8f86a1dde3b1cf323b4691ce8b27c1a88
Shao-Chuan Lee <shaochuan@chromium.org>
2021-02-02 17:38:24 +0900

    kmsvnc: add test to connect to server and verify initial handshake messages
    BUG=b:177965296
    TEST=tast run $DUT graphics.KmsvncConnect
    
    Change-Id: I9100d3e2d26508a8c5727efcd22dc0216ed3ba9e

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/kmsvnc_connect.go
@@ -0,0 +1,89 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+
+	"github.com/google/go-cmp/cmp"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/local/graphics"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         KmsvncConnect,
+		Desc:         "Connects to kmsvnc server and verifies server parameters",
+		Contacts:     []string{"shaochuan@chromium.org", "uekawa@chromium.org"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeGraphics",
+	})
+}
+
+// KmsvncConnect launches the kmsvnc server, connects to it, and verifies server parameters.
+func KmsvncConnect(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	k, err := graphics.NewKmsvnc(ctx)
+	if err != nil {
+		s.Fatal("Failed to start kmsvnc: ", err)
+	}
+	defer k.Stop()
+
+	serverInit, err := k.Connect(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to kmsvnc server: ", err)
+	}
+
+	// Verify server parameters.
+	gotW, gotH := int(serverInit.FramebufferWidth), int(serverInit.FramebufferHeight)
+	if wantW, wantH, err := findDisplayWidthHeight(ctx, cr); err != nil {
+		s.Error("Failed to find primary display size: ", err)
+	} else if gotW != wantW || gotH != wantH {
+		s.Errorf("Unexpected framebuffer size, got %dx%d, want %dx%d", gotW, gotH, wantW, wantH)
+	}
+
+	got := serverInit.PixelFormat
+	want := []byte{
+		0x20,       // bits-per-pixel
+		0x20,       // depth
+		0x00,       // big-endian-flag
+		0xff,       // true-color-flag
+		0x00, 0xff, // red-max
+		0x00, 0xff, // green-max
+		0x00, 0xff, // blue-max
+		0x10, // red-shift
+		0x08, // green-shift
+		0x00, // blue-shift
+	}
+	if !cmp.Equal(got, want) {
+		s.Errorf("Unexpected pixel format, got %v, want %v", got, want)
+	}
+}
+
+// findDisplayWidthHeight returns the width/height of the primary display, which should match the VNC framebuffer size.
+func findDisplayWidthHeight(ctx context.Context, cr *chrome.Chrome) (int, int, error) {
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "failed to connect to test API")
+	}
+
+	info, err := display.GetPrimaryInfo(ctx, tconn)
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "failed to get primary display info")
+	}
+
+	dm, err := info.GetSelectedMode()
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "failed to get selected display mode")
+	}
+	// TODO(b/177965296): handle rotation on tablet devices?
+	return dm.WidthInNativePixels, dm.HeightInNativePixels, nil
+}
---
+++ src/chromiumos/tast/local/graphics/kmsvnc.go
@@ -0,0 +1,202 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"bufio"
+	"context"
+	"encoding/binary"
+	"net"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+const (
+	kmsvncHostPort     = "localhost:5900"
+	kmsvncReadyMessage = "Listening for VNC connections"
+	kmsvncReadyTimeout = 1 * time.Second
+
+	rfbProtocolVersion = "RFB 003.008\n"
+)
+
+// Kmsvnc wraps a kmsvnc process used in tests.
+type Kmsvnc struct {
+	cmd  *testexec.Cmd
+	conn net.Conn
+}
+
+// NewKmsvnc launches kmsvnc as a subprocess and returns a handle.
+// Blocks until kmsvnc is ready to accept connections, so it's safe to connect to kmsvnc once this function returns.
+func NewKmsvnc(ctx context.Context) (*Kmsvnc, error) {
+	cmd := testexec.CommandContext(ctx, "kmsvnc")
+
+	// Create a pipe for stderr which we'll be listening later.
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	// Launch a separate goroutine to listen stderr and print as logs.
+	// Also detects when kmsvnc is ready to accept connections.
+	ready := make(chan struct{})
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			t := scanner.Text()
+			testing.ContextLog(ctx, "kmsvnc: ", t)
+			if ready != nil && strings.Contains(t, kmsvncReadyMessage) {
+				close(ready)
+				ready = nil
+			}
+		}
+	}()
+
+	// Block until kmsvnc is ready, or fail if context timed out / startup took too long.
+	// Make a child context so existing timeouts are taken into account.
+	tctx, cancel := context.WithTimeout(ctx, kmsvncReadyTimeout)
+	defer cancel()
+
+	select {
+	case <-tctx.Done():
+		if err := cmd.Kill(); err != nil {
+			testing.ContextLog(ctx, "Failed to kill kmsvnc process: ", err)
+		} else {
+			// Cleanup the process without blocking.
+			go cmd.Wait()
+		}
+		return nil, tctx.Err()
+	case <-ready:
+		return &Kmsvnc{cmd, nil}, nil
+	}
+}
+
+// Stop closes existing connections and terminates the kmsvnc process gracefully.
+func (k *Kmsvnc) Stop() error {
+	if k.conn != nil {
+		k.conn.Close()
+		k.conn = nil
+	}
+	// In case this fails, the watchdog routine created by cmd.Start() will kill it when the context expires.
+	if err := k.cmd.Signal(syscall.SIGTERM); err != nil {
+		return err
+	}
+	// Cleanup the process without blocking.
+	go k.cmd.Wait()
+	return nil
+}
+
+// RFBServerInit represents a ServerInit message as specified in RFB protocol. Only fields needed for testing are included.
+// https://tools.ietf.org/html/rfc6143#section-7.3.2
+type RFBServerInit struct {
+	FramebufferWidth  uint16
+	FramebufferHeight uint16
+	PixelFormat       []byte
+}
+
+// Connect connects to the kmsvnc server, completes the initial handshake as defined in RFC6143, and returns server parameters.
+// https://tools.ietf.org/html/rfc6143#section-7
+func (k *Kmsvnc) Connect(ctx context.Context) (*RFBServerInit, error) {
+	if k.conn != nil {
+		return nil, errors.New("already connected")
+	}
+
+	conn, err := net.Dial("tcp", kmsvncHostPort)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to connect to %s", kmsvncHostPort)
+	}
+	k.conn = conn
+
+	if err := k.expectProtocolVersionHandshake(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed ProtocolVersion handshake")
+	}
+	if err := k.expectSecurityHandshake(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed Security handshake")
+	}
+	serverInit, err := k.initHandshake(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed Initialization handshake")
+	}
+
+	return serverInit, nil
+}
+
+// expectProtocolVersionHandshake verifies ProtocolVersion handshake messages.
+func (k *Kmsvnc) expectProtocolVersionHandshake(ctx context.Context) error {
+	got := make([]byte, 12)
+	if _, err := k.conn.Read(got); err != nil {
+		return errors.Wrap(err, "error receiving ProtocolVersion message")
+	}
+	want := []byte(rfbProtocolVersion)
+	if !cmp.Equal(got, want) {
+		return errors.Errorf("unexpected ProtocolVersion message, got %v, want %v", got, want)
+	}
+
+	if _, err := k.conn.Write([]byte(rfbProtocolVersion)); err != nil {
+		return errors.Wrap(err, "error sending ProtocolVersion response")
+	}
+
+	return nil
+}
+
+// expectSecurityHandshake verifies security handshake messages.
+func (k *Kmsvnc) expectSecurityHandshake(ctx context.Context) error {
+	got := make([]byte, 2)
+	if _, err := k.conn.Read(got); err != nil {
+		return errors.Wrap(err, "error receiving security types")
+	}
+	// number-of-security-types = 1, security-types = [None]
+	want := []byte{0x1, 0x1}
+	if !cmp.Equal(got, want) {
+		return errors.Errorf("unexpected security types, got %v, want %v", got, want)
+	}
+
+	// Response: security-type = None
+	if _, err := k.conn.Write([]byte{0x1}); err != nil {
+		return errors.Wrap(err, "error sending security type response")
+	}
+
+	got = make([]byte, 4)
+	if _, err := k.conn.Read(got); err != nil {
+		return errors.Wrap(err, "error receiving SecurityResult message")
+	}
+	// SecurityResult = OK
+	want = []byte{0, 0, 0, 0}
+	if !cmp.Equal(got, want) {
+		return errors.Errorf("unexpected SecurityResult message, got %v, want %v", got, want)
+	}
+
+	return nil
+}
+
+// initHandshake sends a ClientInit message, and parses the ServerInit response.
+func (k *Kmsvnc) initHandshake(ctx context.Context) (*RFBServerInit, error) {
+	// ClientInit: shared-flag = 1
+	if _, err := k.conn.Write([]byte{0x1}); err != nil {
+		return nil, errors.Wrap(err, "error sending ClientInit message")
+	}
+
+	// ServerInit is of variable length, we only need the first 17 bytes.
+	buf := make([]byte, 17)
+	if _, err := k.conn.Read(buf); err != nil {
+		return nil, errors.Wrap(err, "error receiving ServerInit message")
+	}
+
+	return &RFBServerInit{
+		FramebufferWidth:  binary.BigEndian.Uint16(buf[0:2]),
+		FramebufferHeight: binary.BigEndian.Uint16(buf[2:4]),
+		PixelFormat:       buf[4:17],
+	}, nil
+}
