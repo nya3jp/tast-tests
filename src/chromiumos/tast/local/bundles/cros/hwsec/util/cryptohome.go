4e4e93a319fdac14a4a656e0ef0aff5319f2076e
Yi Chou <yich@google.com>
2021-02-02 15:58:41 +0800

    hwsec: Add CryptohomeBadPerms tast-tests
    This would migrate platform_CryptohomeBadPerms to tast-tests
    
    BUG=b:178163420
    TEST=tast run $DUT hwsec.CryptohomeBadPerms
    
    Change-Id: I5a91cb3ae7a7dfc15add331bb60c83ce9352db58

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/util/cryptohome.go
@@ -0,0 +1,20 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package util
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/errors"
+)
+
+// RequireMountFail would try to mount a user vault, and return error when it success.
+func RequireMountFail(ctx context.Context, utility *hwsec.UtilityCryptohomeBinary, user, pass, label string) error {
+	if err := utility.MountVault(ctx, user, pass, label, true, hwsec.NewVaultConfig()); err == nil {
+		return errors.Errorf("mount unexpectedly succeeded for %s", user)
+	}
+	return nil
+}
