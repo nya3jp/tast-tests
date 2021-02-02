0e199a0ed3e4207a89c4bddf91ad40e6986bde99
Trent Begin <tbegin@google.com>
2021-02-02 13:09:28 -0700

    chrome: add chromeRunning fixture
    This change adds a test fixture that does not log into a user session,
    but simply starts a chrome process.
    
    BUG=b:178274447
    TEST=tast run sarien-dut health.* (pass)
    
    Change-Id: I925e397235df8b335e02aa51da2e002cbc08c924

--- src/chromiumos/tast/local/chrome/fixture.go
+++ src/chromiumos/tast/local/chrome/fixture.go
@@ -43,22 +43,41 @@ func init() {
 		ResetTimeout:    ResetTimeout,
 		TearDownTimeout: ResetTimeout,
 	})
+
+	testing.AddFixture(&testing.Fixture{
+		Name:            "chromeRunning",
+		Desc:            "There is a Chrome process running, but no user session",
+		Contacts:        []string{"nya@chromium.org", "oka@chromium.org"},
+		Impl:            NewRunningFixture(),
+		SetUpTimeout:    LoginTimeout,
+		ResetTimeout:    ResetTimeout,
+		TearDownTimeout: ResetTimeout,
+	})
+
 }
 
-// loggedInFixture is a fixture to start Chrome with the given options.
+// chromeFixture is a fixture to start Chrome with the given options.
 // If the parent is specified, and the parent returns a value of []Option, it
 // will also add those options when starting Chrome.
-type loggedInFixture struct {
+type chromeFixture struct {
 	cr   *Chrome
 	opts []Option
 }
 
-// NewLoggedInFixture returns a FixtureImpl of creating a Chrome instance with the given options.
+// NewLoggedInFixture returns a FixtureImpl of creating a Chrome instance with
+// the given options. By default, this Chrome instance has a user session.
 func NewLoggedInFixture(opts ...Option) testing.FixtureImpl {
-	return &loggedInFixture{opts: opts}
+	return &chromeFixture{opts: opts}
 }
 
-func (f *loggedInFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+// NewRunningFixture returns a FixtureImpl of creating a Chrome instance with
+// the given options. This instance does not have a logged in user session.
+func NewRunningFixture(opts ...Option) testing.FixtureImpl {
+	opts = append(opts, NoLogin())
+	return &chromeFixture{opts: opts}
+}
+
+func (f *chromeFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
 	opts := f.opts
 	// If there's a parent fixture and the fixture supplies extra options, use them.
 	if extraOpts, ok := s.ParentValue().([]Option); ok {
@@ -73,7 +92,7 @@ func (f *loggedInFixture) SetUp(ctx context.Context, s *testing.FixtState) inter
 	return cr
 }
 
-func (f *loggedInFixture) TearDown(ctx context.Context, s *testing.FixtState) {
+func (f *chromeFixture) TearDown(ctx context.Context, s *testing.FixtState) {
 	Unlock()
 	if err := f.cr.Close(ctx); err != nil {
 		s.Log("Failed to close Chrome connection: ", err)
@@ -81,7 +100,7 @@ func (f *loggedInFixture) TearDown(ctx context.Context, s *testing.FixtState) {
 	f.cr = nil
 }
 
-func (f *loggedInFixture) Reset(ctx context.Context) error {
+func (f *chromeFixture) Reset(ctx context.Context) error {
 	if err := f.cr.Responded(ctx); err != nil {
 		return errors.Wrap(err, "existing Chrome connection is unusable")
 	}
@@ -91,6 +110,6 @@ func (f *loggedInFixture) Reset(ctx context.Context) error {
 	return nil
 }
 
-func (f *loggedInFixture) PreTest(ctx context.Context, s *testing.FixtTestState) {}
+func (f *chromeFixture) PreTest(ctx context.Context, s *testing.FixtTestState) {}
 
-func (f *loggedInFixture) PostTest(ctx context.Context, s *testing.FixtTestState) {}
+func (f *chromeFixture) PostTest(ctx context.Context, s *testing.FixtTestState) {}
