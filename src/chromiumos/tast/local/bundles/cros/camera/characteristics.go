851f9311878cc381ccae11326a9c67fcaf6d7079
Ren-Pei Zeng <kamesan@chromium.org>
2021-02-02 20:37:24 +0800

    camera.Characteristics: Check config file exists for built-in USB camera
    BUG=b:177381783
    TEST=tast run $DUT camera.Characteristics
    
    Cq-Depend: chromium:2666176
    Change-Id: I8c0593847a9c5d12ddb8936ef8a8effa720fa239

--- src/chromiumos/tast/local/bundles/cros/camera/characteristics.go
+++ src/chromiumos/tast/local/bundles/cros/camera/characteristics.go
@@ -7,7 +7,9 @@ package camera
 import (
 	"context"
 	"path/filepath"
+	"strconv"
 
+	"chromiumos/tast/autocaps"
 	"chromiumos/tast/local/gtest"
 	"chromiumos/tast/shutil"
 	"chromiumos/tast/testing"
@@ -26,8 +28,18 @@ func init() {
 }
 
 func Characteristics(ctx context.Context, s *testing.State) {
+	staticCaps, err := autocaps.Read(autocaps.DefaultCapabilityDir, nil)
+	if err != nil {
+		s.Fatal("Failed to read static capabilities: ", err)
+	}
+	hasBuiltinUsbCamera := false
+	if c, ok := staticCaps["builtin_usb_camera"]; ok && c == autocaps.Yes {
+		hasBuiltinUsbCamera = true
+	}
+
 	const gtestExecutableName = "camera_characteristics_test"
 	t := gtest.New(gtestExecutableName,
+		gtest.ExtraArgs("--skip_if_no_config="+strconv.FormatBool(!hasBuiltinUsbCamera)),
 		gtest.Logfile(filepath.Join(s.OutDir(), gtestExecutableName+".log")))
 	args, err := t.Args()
 	if err != nil {
