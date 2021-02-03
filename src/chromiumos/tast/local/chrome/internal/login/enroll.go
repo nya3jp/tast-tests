124bf87672a8e8573d6aa80969b7ce8f536b8fd4
Hidehiko Abe <hidehiko@chromium.org>
2021-02-04 02:46:01 +0900

    Replace EvalPromise/Exec by Eval/Call.
    EvalPromise and Exec are deprecated and Eval and Call are the mathods
    to replace.
    
    BUG=chromium:1042550
    TEST=./fast_build.sh -T
    
    Change-Id: If6cd47086a5f9d8258ffa027483b4a06b688b1a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2673280
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/chrome/internal/login/enroll.go
+++ src/chromiumos/tast/local/chrome/internal/login/enroll.go
@@ -152,8 +152,7 @@ func enterpriseOOBELogin(ctx context.Context, cfg *config.Config, sess *driver.S
 
 	testing.ContextLog(ctx, "Performing login after enrollment")
 	// Now login like "normal".
-	if err := conn.Exec(ctx, fmt.Sprintf("Oobe.loginForTesting('%s', '%s', '%s', false)",
-		cfg.User, cfg.Pass, cfg.GAIAID)); err != nil {
+	if err := conn.Call(ctx, nil, "Oobe.loginForTesting", cfg.User, cfg.Pass, cfg.GAIAID, false); err != nil {
 		return err
 	}
 
