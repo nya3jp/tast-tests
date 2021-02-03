f5c3818e080fd9d731dc0df51ae5a8ac807644ff
Matthias Springer <springerm@google.com>
2021-02-03 12:14:43 +0900

    tast-tests: Fix sysfs_battery_metrics on DUTs with no batteries
    Do not try to report battery metrics when the device has no batteries.
    This fixes a few power/performance tests.
    
    BUG=b:178979786
    BUG=b:166758232
    TEST=tast run DUT arc.PowerCameraPreviewPerf.30fps_nobatterymetrics
    
    Change-Id: I6286f14bda2e1ad6c6194af8dea17304eea225bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2670482
    Tested-by: Matthias Springer <springerm@chromium.org>
    Commit-Queue: Matthias Springer <springerm@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- src/chromiumos/tast/local/power/sysfs_battery_metrics.go
+++ src/chromiumos/tast/local/power/sysfs_battery_metrics.go
@@ -242,6 +242,10 @@ func (b *SysfsBatteryMetrics) Start(_ context.Context) error {
 // If there are no batteries can be used to report the metrics,
 // Snapshot does nothing and returns without error.
 func (b *SysfsBatteryMetrics) Snapshot(_ context.Context, values *perf.Values) error {
+	if len(b.batteryPath) == 0 {
+		return nil
+	}
+
 	power, err := ReadSystemPower(b.batteryPath)
 	if err != nil {
 		return err
