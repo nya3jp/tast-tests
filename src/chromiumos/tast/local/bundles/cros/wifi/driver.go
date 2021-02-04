6c26b89630edd72ea7b88cc1cea39d1127227e4f
YN Chen <yn.chen@mediatek.com>
2021-02-04 22:48:51 +0800

    wifi.Driver: add MediakTek MT7921 PCIE wlan device info
    Update to support Mediatek MT7921 PCIE WiFi
    Add vendor id 0x14c3 and device id 0x7961
    
    BUG=b:178999150
    TEST=tast run $DUT wifi.Driver
    
    Change-Id: I252e640bb5c9ba7706fea63b92250736c006f97b

--- src/chromiumos/tast/local/bundles/cros/wifi/driver.go
+++ src/chromiumos/tast/local/bundles/cros/wifi/driver.go
@@ -122,6 +122,9 @@ var expectedWLANDriver = map[string]map[string]string{
 		"5.4":  "wireless/realtek/rtw88/rtw88_8822ce.ko",
 		"5.10": "wireless/realtek/rtw88/rtw88_8822ce.ko",
 	},
+	wlan.MediaTekMT7921PCIE: {
+		"5.4": "wireless/mediatek/mt76/mt7921/mt7921e.ko",
+	},
 }
 
 func Driver(ctx context.Context, s *testing.State) {
