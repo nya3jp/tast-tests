bd35a2cf0c49b3d822e0b507e6d0750bf9c5b36d
Joon Ahn <joonbug@chromium.org>
2021-02-04 13:52:18 -0800

    Add peripherals.LaunchAppFromAccelerator
    Do not submit until https://chromium-review.googlesource.com/c/chromium/src/+/2657995/ uprev
    
    BUG=chromium:1125150
    TEST=tast run ${DUT} peripherals.LaunchAppFromAccelerator.diagnostics
    
    Change-Id: Ice93a16aeff52455703e6c45b565892b41b58334

---
+++ src/chromiumos/tast/local/bundles/cros/peripherals/launch_app_from_accelerator.go
@@ -0,0 +1,98 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package peripherals
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+// accelTestParams contains all the data needed to run a single test iteration.
+type accelTestParams struct {
+	app         apps.App
+	keystroke   string
+	featureFlag string
+}
+
+var launch = ui.FindParams{
+	Name: "STOPPED",
+	Role: ui.RoleTypeStaticText,
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LaunchAppFromAccelerator,
+		Desc: "Peripherals app can be found and launched with an accelerator",
+		Contacts: []string{
+			"joonbug@chromium.org",
+			"cros-peripherals@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{
+			{
+				Name: "diagnostics",
+				Val: accelTestParams{
+					app:         apps.Diagnostics,
+					keystroke:   "Ctrl+Search+Esc",
+					featureFlag: "DiagnosticsApp",
+				},
+			},
+		},
+	})
+}
+
+// LaunchAppFromAccelerator verifies launching an app with an accelerator.
+func LaunchAppFromAccelerator(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	params := s.Param().(accelTestParams)
+
+	cr, err := chrome.New(ctx, chrome.EnableFeatures(params.featureFlag))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx) // Close our own chrome instance.
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Error creating keyboard instance: ", err)
+	}
+	defer kb.Close()
+
+	// Briefly sleep before sending the keystroke so that the device will
+	// be completely past oobe and ready for input.
+	if err := testing.Sleep(ctx, 3*time.Second); err != nil {
+		s.Fatal("Error stablizing: ", err)
+	}
+
+	s.Logf("Sending keystroke: %s", params.keystroke)
+	err = kb.Accel(ctx, params.keystroke)
+	if err != nil {
+		s.Fatal("Failed to search and launch app: ", err)
+	}
+
+	err = ash.WaitForApp(ctx, tconn, params.app.ID)
+	if err != nil {
+		s.Fatal("Could not find app in shelf after launch: ", err)
+	}
+}
