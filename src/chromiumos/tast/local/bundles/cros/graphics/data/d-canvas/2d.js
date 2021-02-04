4d134daacb866fe2104ba1618f5b6ada9e7c23c4
Avery Musbach <amusbach@chromium.org>
2021-02-03 16:22:05 -0800

    CL for discussion only
    BUG=chromium:1070307
    TEST=graphics.FastInk
    
    Change-Id: Iff09080ac2ab477daa2a18f42d8d785477122660

--- src/chromiumos/tast/local/bundles/cros/graphics/data/d-canvas/2d.js
+++ src/chromiumos/tast/local/bundles/cros/graphics/data/d-canvas/2d.js
@@ -1,15 +1,14 @@
 // Copyright 2021 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-'use strict';
 
-let pixel_width;
-let pixel_height;
+var pixel_width;
+var pixel_height;
 
 function init2D() {
   let canvas = document.querySelector('canvas');
 
-  const setSizeAndRotation = () => {
+  let setSizeAndRotation = () => {
     let angle = screen.orientation.angle % 360;
     let dpr = devicePixelRatio;
     let dp_width = window.innerWidth;
@@ -20,16 +19,13 @@ function init2D() {
     if (angle % 180 == 90) {
       canvas.style.width = `${dp_height}px`;
       canvas.style.height = `${dp_width}px`;
-      let tmp = pixel_height;
+      var tmp = pixel_height;
       pixel_height = pixel_width;
       pixel_width = tmp;
     } else {
       canvas.style.width = `${dp_width}px`;
       canvas.style.height = `${dp_height}px`;
     }
-    canvas.width  = pixel_width;
-    canvas.height = pixel_height;
-
     canvas.style.transform = `rotateZ(${angle}deg)`;
     switch (angle) {
       case 0:
@@ -50,16 +46,22 @@ function init2D() {
         break;
     }
 
-    console.log("update1:" + angle + ", size=" + dp_width + "x" + dp_height +
+    canvas.width  = pixel_width;
+    canvas.height = pixel_height;
+
+    console.log("update1 size=" + window.innerWidth + "x" + window.innerHeight +
                 " angle=" + screen.orientation.angle);
   };
+
   screen.orientation.addEventListener('change', setSizeAndRotation);
-  window.addEventListener('resize',  setSizeAndRotation);
+  window.addEventListener('resize', setSizeAndRotation);
+
+  document.documentElement.addEventListener('click',  setSizeAndRotation);
   setSizeAndRotation();
   draw();
 }
 
-let deg = 0;
+var deg = 0;
 
 function draw() {
   let angle = screen.orientation.angle % 360;
@@ -78,7 +80,7 @@ function draw() {
   // Text
   c2.fillStyle = 'rgb(255,255,255)';
   c2.font = "40px Arial";
-  let text = `Pixel size=${pixel_width}x${pixel_height} \
+  var text = `Pixel size=${pixel_width}x${pixel_height} \
 dp size=${dp_width}x${dp_height} dpr=${dpr} angle=${angle}`;
   c2.fillText(text, 10, 50);
   c2.strokeStyle = 'rgb(0,0,0)';
--- src/chromiumos/tast/local/bundles/cros/graphics/fast_ink.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/fast_ink.go
@@ -31,7 +31,7 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		HardwareDeps: hwdep.D(hwdep.SupportsNV12Overlays(), hwdep.InternalDisplay(), hwdep.TouchScreen()),
-		Data:         []string{"d-canvas/main.html", "d-canvas/2d.js", "d-canvas/webgl.js"},
+		Data:         []string{"d-canvas/main.html", "d-canvas/2d.js"},
 		Fixture:      "chromeLoggedIn",
 		Timeout:      5 * time.Minute,
 	})
@@ -50,11 +50,17 @@ func FastInk(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to connect to test API: ", err)
 	}
 
-	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, true)
 	if err != nil {
-		s.Fatal("Failed to ensure clamshell mode: ", err)
+		s.Fatal("Failed to ensure tablet mode: ", err)
 	}
 	defer cleanup(cleanupCtx)
+
+	// The above call to ash.EnsureTabletModeEnabled may trigger a
+	// notification about gesture navigation.
+	if err := ash.HideVisibleNotifications(ctx, tconn); err != nil {
+		s.Fatal("Failed to hide notifications: ", err)
+	}
 
 	srv := httptest.NewServer(http.FileServer(s.DataFileSystem()))
 	defer srv.Close()
@@ -89,86 +95,54 @@ func FastInk(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to get the internal display info: ", err)
 	}
 
-	defer display.SetDisplayRotationSync(cleanupCtx, tconn, internalDisplayID, display.Rotate0)
-	for _, displayRotation := range []display.RotationAngle{display.Rotate0, display.Rotate90, display.Rotate180, display.Rotate270} {
-		s.Run(ctx, string(displayRotation), func(ctx context.Context, s *testing.State) {
-			if err := display.SetDisplayRotationSync(ctx, tconn, internalDisplayID, displayRotation); err != nil {
-				s.Fatal("Failed to rotate display: ", err)
-			}
+	if err := display.SetDisplayRotationSync(ctx, tconn, internalDisplayID, display.Rotate90); err != nil {
+		s.Fatal("Failed to rotate display: ", err)
+	}
 
-			for _, wState := range []ash.WindowStateType{ash.WindowStateNormal, ash.WindowStateMaximized, ash.WindowStateFullscreen} {
-				s.Run(ctx, string(wState), func(ctx context.Context, s *testing.State) {
-					if _, err := ash.SetWindowState(ctx, tconn, wID, ash.WMEventTypeForState(wState)); err != nil {
-						s.Fatalf("Failed to set window state to %v: %v", wState, err)
-					}
+	if _, err := ash.SetWindowState(ctx, tconn, wID, ash.WMEventFullscreen); err != nil {
+		s.Fatal("Failed to set window state to Fullscreen: ", err)
+	}
 
-					if wState == ash.WindowStateNormal {
-						internalInfo, err := display.GetInternalInfo(ctx, tconn)
-						if err != nil {
-							s.Fatal("Failed to get the internal display info: ", err)
-						}
+	w, err := ash.GetWindow(ctx, tconn, wID)
+	if err != nil {
+		s.Fatal("Failed to get window info: ", err)
+	}
 
-						// Make the window reasonably large, with no part behind the shelf.
-						desiredBounds := internalInfo.WorkArea.WithInset(10, 10)
-						bounds, displayID, err := ash.SetWindowBounds(ctx, tconn, wID, desiredBounds, internalInfo.ID)
-						if err != nil {
-							s.Fatal("Failed to set the window bounds: ", err)
-						}
-						if displayID != internalInfo.ID {
-							info, err := display.FindInfo(ctx, tconn, func(info *display.Info) bool { return info.ID == displayID })
-							if err != nil {
-								s.Fatal("Window not on internal display after setting bounds (also failed to get info on the display where it is)")
-							}
-							s.Fatalf("Window on %s; tried to put it on %s", info.Name, internalInfo.Name)
-						}
-						if bounds != desiredBounds {
-							s.Fatalf("Window bounds are %v; tried to set them to %v", bounds, desiredBounds)
-						}
-					}
+	// Move the mouse to the center of the window, where it will not
+	// bring extraneous UI stuff. Particularly, in full screen, a
+	// mouse near the bottom could make the shelf visible there, or
+	// a mouse near the top could make the browser frame visible
+	// there. We do this step even in tablet mode, because
+	// ash.HideVisibleNotifications causes the mouse cursor to show.
+	if err := mouse.Move(ctx, tconn, w.TargetBounds.CenterPoint(), time.Second); err != nil {
+		s.Fatal("Failed to move mouse: ", err)
+	}
 
-					w, err := ash.GetWindow(ctx, tconn, wID)
-					if err != nil {
-						s.Fatal("Failed to get window info: ", err)
-					}
+	if err := ui.WaitForLocationChangeCompleted(ctx, tconn); err != nil {
+		s.Fatal("Failed to wait for location change events to be completed: ", err)
+	}
 
-					// Move the mouse to the center of the window, where it will not
-					// bring extraneous UI stuff. Particularly, in full screen, a
-					// mouse near the bottom could make the shelf visible there, or
-					// a mouse near the top could make the browser frame visible
-					// there. Either of those scenarios could make this test fail.
-					if err := mouse.Move(ctx, tconn, w.TargetBounds.CenterPoint(), time.Second); err != nil {
-						s.Fatal("Failed to move mouse: ", err)
-					}
+	hists, err := metrics.Run(ctx, tconn, func(ctx context.Context) error {
+		if err := testing.Sleep(ctx, time.Second); err != nil {
+			return errors.Wrap(err, "failed to wait a second")
+		}
+		return nil
+	}, "Viz.DisplayCompositor.OverlayStrategy")
+	if err != nil {
+		s.Fatal("Error while recording histogram Viz.DisplayCompositor.OverlayStrategy: ", err)
+	}
 
-					if err := ui.WaitForLocationChangeCompleted(ctx, tconn); err != nil {
-						s.Fatal("Failed to wait for location change events to be completed: ", err)
-					}
+	hist := hists[0]
+	if len(hist.Buckets) == 0 {
+		s.Fatal("Got no overlay strategy data")
+	}
 
-					hists, err := metrics.Run(ctx, tconn, func(ctx context.Context) error {
-						if err := testing.Sleep(ctx, time.Second); err != nil {
-							return errors.Wrap(err, "failed to wait a second")
-						}
-						return nil
-					}, "Viz.DisplayCompositor.OverlayStrategy")
-					if err != nil {
-						s.Fatal("Error while recording histogram Viz.DisplayCompositor.OverlayStrategy: ", err)
-					}
-
-					hist := hists[0]
-					if len(hist.Buckets) == 0 {
-						s.Fatal("Got no overlay strategy data")
-					}
-
-					for _, bucket := range hist.Buckets {
-						// bucket.Min will be from enum OverlayStrategies as defined
-						// in tools/metrics/histograms/enums.xml in the chromium
-						// code base. Here we check for 1 which is "No overlay."
-						if bucket.Min == 1 {
-							s.Fatalf("%d of %d frame(s) had no overlay", bucket.Count, hist.TotalCount())
-						}
-					}
-				})
-			}
-		})
+	for _, bucket := range hist.Buckets {
+		// bucket.Min will be from enum OverlayStrategies as defined
+		// in tools/metrics/histograms/enums.xml in the chromium
+		// code base. Here we check for 1 which is "No overlay."
+		if bucket.Min == 1 {
+			s.Fatalf("%d of %d frame(s) had no overlay", bucket.Count, hist.TotalCount())
+		}
 	}
 }
