18c5c56b2679842b46da6a39c674de42f1ff1f83
Yi Chou <yich@google.com>
2021-02-05 18:58:10 +0800

    tast-tests: Ensure the TPM is in expected state after factory.Finalize
    BUG=chromium:1173572, b:178332041
    TEST=tast run $DUT factory.Finalize
    
    Change-Id: Idb5c3d008a7a7c3d14fbb4677f9c2fbd177e46b2

--- src/chromiumos/tast/remote/bundles/cros/factory/finalize.go
+++ src/chromiumos/tast/remote/bundles/cros/factory/finalize.go
@@ -10,8 +10,10 @@ import (
 	"strings"
 	"time"
 
+	"chromiumos/tast/common/hwsec"
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
+	hwsecremote "chromiumos/tast/remote/hwsec"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
@@ -116,5 +118,26 @@ func cleanup(ctx context.Context, s *testing.State) {
 	s.Log("Backing up the logs under /var/log")
 	if err := d.GetFile(ctx, "/var/log/upstart.log", filepath.Join(s.OutDir(), "upstart.log")); err != nil {
 		s.Error("Dump upstart.log fail: ", err)
+	}
+
+	r, err := hwsecremote.NewCmdRunner(s.DUT())
+	if err != nil {
+		s.Fatal("CmdRunner creation error: ", err)
+	}
+
+	utility, err := hwsec.NewUtilityCryptohomeBinary(r)
+	if err != nil {
+		s.Fatal("Utilty creation error: ", err)
+	}
+
+	helper, err := hwsecremote.NewHelper(utility, r, s.DUT())
+	if err != nil {
+		s.Fatal("Helper creation error: ", err)
+	}
+
+	// Because this tast may clobber the stateful partition without clearing TPM.
+	// Resetting the TPM to ensure the TPM become expected state.
+	if err := helper.EnsureTPMIsResetAndPowerwash(ctx); err != nil {
+		s.Fatal("Failed to ensure resetting TPM: ", err)
 	}
 }
