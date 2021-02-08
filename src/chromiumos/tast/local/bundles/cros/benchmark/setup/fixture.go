dd68368b3af2580c94f4527a7bae4f1a4611ce7b
Alfred Yu <alfredyu@cienet.com>
2021-02-08 19:04:58 +0800

    public benchmark - CrXPRT2.
    This is one of the public benchmark tests - CrXPRT2.
    The test installs CrXPRT2, runs it and records the CrXPRT2 test score.
    
    BUG=None
    TEST=tast run <DUT> benchmark.CRXPRT2
    
    Change-Id: I030d2267468147b2ff11a00b3b30c47c24851fca

--- src/chromiumos/tast/local/bundles/cros/benchmark/setup/fixture.go
+++ src/chromiumos/tast/local/bundles/cros/benchmark/setup/fixture.go
@@ -22,6 +22,9 @@ const (
 	// BenchmarkARCFixture is a fixture name that will be registered to tast.
 	// The fxture brings up Chrome and ARC with PlayStore.
 	BenchmarkARCFixture = "benchmarkARCFixture"
+	// BenchmarkChromeFixture is a fixture name that will be registered to tast.
+	// It brings up Chrome.
+	BenchmarkChromeFixture = "benchmarkChromeFixture"
 )
 
 func benchmarkARCFixtureOptions(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
@@ -48,6 +51,18 @@ func init() {
 		Impl:     arc.NewArcBootedWithPlayStoreFixture(benchmarkARCFixtureOptions),
 		// Add two minutes to setup time to allow extra Play Store UI operations.
 		SetUpTimeout:    chrome.GAIALoginTimeout + optin.OptinTimeout + arc.BootTimeout + 2*time.Minute,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		Vars:            []string{userVar, passwdVar},
+	})
+	testing.AddFixture(&testing.Fixture{
+		Name:     BenchmarkChromeFixture,
+		Desc:     "The fixture starts chrome with GAIA login and ARC Supported",
+		Contacts: []string{"xliu@cienet.com"},
+		// Although ARCSupported is provided as an option to bring up the ARC on DUT, we will not
+		// use ARC in the test so we don't need set up ARC/ADB. Use LoggedIn Fixture.
+		Impl:            chrome.NewLoggedInFixture(benchmarkARCFixtureOptions),
+		SetUpTimeout:    chrome.GAIALoginTimeout,
 		ResetTimeout:    chrome.ResetTimeout,
 		TearDownTimeout: chrome.ResetTimeout,
 		Vars:            []string{userVar, passwdVar},
