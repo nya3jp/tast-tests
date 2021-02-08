4064a3b78919b88b7ff548554e6c693baca8b9f1
Joon Ahn <joonbug@chromium.org>
2021-02-08 14:44:41 -0800

    Add peripherals.LaunchAppFromSettings to mainline group
    BUG=None
    TEST=tast run ${DUT} peripherals.LaunchAppFromSettings.*
    
    Change-Id: Ic07b999d4926fe79ca4797f222b609f7eaa615c4

--- src/chromiumos/tast/local/bundles/cros/peripherals/launch_app_from_settings.go
+++ src/chromiumos/tast/local/bundles/cros/peripherals/launch_app_from_settings.go
@@ -36,6 +36,7 @@ func init() {
 			"joonbug@chromium.org",
 			"cros-peripherals@google.com",
 		},
+		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{
 			{
