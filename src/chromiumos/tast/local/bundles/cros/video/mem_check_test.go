a02273f0677a45731d10b9255df9758f190eb1c6
Eliot Courtney <edcourtney@google.com>
2021-02-09 22:29:33 +0900

    video.MemCheck: Use genparam to generate variations.
    This is in preparation to add lacros variations. Generating the
    variations should help avoid bugs where either 1. lacros variations
    diverge from chromeos variations, or, 2. new chromeos variations are
    added without updating lacros variations.
    
    BUG=chromium:1140407
    TEST=tast run <dut> video.Seek.*
    
    Change-Id: I529ea9c491b168fc45bcf05e777b5e51e81e92da

---
+++ src/chromiumos/tast/local/bundles/cros/video/mem_check_test.go
@@ -0,0 +1,103 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"testing"
+
+	"chromiumos/tast/common/genparams"
+)
+
+var memCheckTestParams = []struct {
+	Name              string
+	FileName          string
+	Sizes             string
+	VideoType         string
+	MSEDataFiles      bool
+	ExtraSoftwareDeps []string
+	ExtraAttr         []string
+	ExtraData         []string
+	Fixture           string
+}{{
+	Name:              "av1_hw",
+	FileName:          "720_av1.mp4",
+	Sizes:             "[]graphics.Size{{Width: 1280, Height: 720}}",
+	VideoType:         "play.NormalVideo",
+	ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+	ExtraData:         []string{"video.html", "720_av1.mp4"},
+	ExtraSoftwareDeps: []string{`"amd64"`, `"video_overlays"`, "caps.HWDecodeAV1"},
+	Fixture:           "chromeVideoWithGuestLoginAndHWAV1Decoding",
+}, {
+	Name:              "h264_hw",
+	FileName:          "720_h264.mp4",
+	Sizes:             "[]graphics.Size{{Width: 1280, Height: 720}}",
+	VideoType:         "play.NormalVideo",
+	ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+	ExtraData:         []string{"video.html", "720_h264.mp4"},
+	ExtraSoftwareDeps: []string{`"amd64"`, `"video_overlays"`, "caps.HWDecodeH264", `"proprietary_codecs"`},
+	Fixture:           "chromeVideoWithGuestLogin",
+}, {
+	Name:              "vp8_hw",
+	FileName:          "720_vp8.webm",
+	Sizes:             "[]graphics.Size{{Width: 1280, Height: 720}}",
+	VideoType:         "play.NormalVideo",
+	ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+	ExtraData:         []string{"video.html", "720_vp8.webm"},
+	ExtraSoftwareDeps: []string{`"amd64"`, `"video_overlays"`, "caps.HWDecodeVP8"},
+	Fixture:           "chromeVideoWithGuestLogin",
+}, {
+	Name:              "vp9_hw",
+	FileName:          "720_vp9.webm",
+	Sizes:             "[]graphics.Size{{Width: 1280, Height: 720}}",
+	VideoType:         "play.NormalVideo",
+	ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+	ExtraData:         []string{"video.html", "720_vp9.webm"},
+	ExtraSoftwareDeps: []string{`"amd64"`, `"video_overlays"`, "caps.HWDecodeVP9"},
+	Fixture:           "chromeVideoWithGuestLogin",
+}, {
+	Name:              "av1_hw_switch",
+	FileName:          "dash_smpte_av1.mp4.mpd",
+	Sizes:             "[]graphics.Size{{Width: 256, Height: 144}, {Width: 426, Height: 240}}",
+	VideoType:         "play.MSEVideo",
+	MSEDataFiles:      true,
+	ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+	ExtraData:         []string{"dash_smpte_av1.mp4.mpd", "dash_smpte_144.av1.mp4", "dash_smpte_240.av1.mp4"},
+	ExtraSoftwareDeps: []string{`"amd64"`, `"video_overlays"`, "caps.HWDecodeAV1"},
+	Fixture:           "chromeVideoWithGuestLoginAndHWAV1Decoding",
+}, {
+	Name:              "h264_hw_switch",
+	FileName:          "cars_dash_mp4.mpd",
+	Sizes:             "[]graphics.Size{{Width: 256, Height: 144}, {Width: 426, Height: 240}}",
+	VideoType:         "play.MSEVideo",
+	MSEDataFiles:      true,
+	ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_nightly"},
+	ExtraData:         []string{"cars_dash_mp4.mpd", "cars_144_h264.mp4", "cars_240_h264.mp4"},
+	ExtraSoftwareDeps: []string{`"amd64"`, `"video_overlays"`, "caps.HWDecodeH264", `"proprietary_codecs"`},
+	Fixture:           "chromeVideoWithGuestLogin",
+}}
+
+func TestMemCheck(t *testing.T) {
+	code := genparams.Template(t, `{{ range . }}{
+	Name: {{ .Name | fmt }},
+	Val: memCheckParams{
+		fileName: {{ .FileName | fmt }},
+		sizes: {{ .Sizes }},
+		videoType: {{ .VideoType }},
+	},
+	{{ if .ExtraAttr }}
+	ExtraAttr: {{ .ExtraAttr | fmt }},
+	{{ end }}
+	{{ if .MSEDataFiles }}
+	ExtraData: append(play.MSEDataFiles(), {{ range .ExtraData }} {{ . | fmt }}, {{ end }}),
+	{{ else }}
+	ExtraData: {{ .ExtraData | fmt }},
+  {{ end }}
+	{{ if .ExtraSoftwareDeps }}
+	ExtraSoftwareDeps: []string{ {{ range .ExtraSoftwareDeps }} {{ . }}, {{ end }} },
+	{{ end }}
+	Fixture: {{ .Fixture | fmt }},
+}, {{ end }}`, memCheckTestParams)
+	genparams.Ensure(t, "mem_check.go", code)
+}
