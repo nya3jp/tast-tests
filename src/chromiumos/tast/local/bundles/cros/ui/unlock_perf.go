807922642a0c808378b1fb8eb8d8169869003660
Mitsuru Oshima <oshima@chromium.org>
2021-02-09 00:19:43 -0800

    Add unlock performance test
    BUG=None
    TEST=ui.UnlockPerf
    
    Change-Id: Ic1b38e7d5b69c9c04b9344701c338d403970e996

---
+++ src/chromiumos/tast/local/bundles/cros/ui/unlock_perf.go
@@ -0,0 +1,173 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/local/bundles/cros/ui/perfutil"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ui/lockscreen"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/lacros"
+	"chromiumos/tast/local/lacros/launcher"
+	"chromiumos/tast/local/power"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         UnlockPerf,
+		Desc:         "Measures animation smoothness of screen unlock",
+		Contacts:     []string{"mukai@chromium.org", "oshima@chromium.org", "chromeos-wmp@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome"},
+		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+		Timeout:      4 * time.Minute,
+		Vars:         []string{"lacrosDeployedBinary"},
+		Params: []testing.Param{{
+			Val:     lacros.ChromeTypeChromeOS,
+			Fixture: "chromeLoggedIn",
+		}, {
+			Name:    "skia_renderer",
+			Val:     lacros.ChromeTypeChromeOS,
+			Fixture: "chromeLoggedInWith100FakeAppsSkiaRenderer",
+		}, {
+			Name:              "lacros",
+			Val:               lacros.ChromeTypeLacros,
+			Pre:               launcher.StartedByData(),
+			ExtraData:         []string{launcher.DataArtifact},
+			ExtraSoftwareDeps: []string{"lacros"},
+		}},
+		Data: []string{"animation.html", "animation.js"},
+	})
+}
+
+func UnlockPerf(ctx context.Context, s *testing.State) {
+	const (
+		password    = "testpass"
+		lockTimeout = 30 * time.Second
+		authTimeout = 30 * time.Second
+	)
+
+	// Ensure display on to record ui performance correctly.
+	if err := power.TurnOnDisplay(ctx); err != nil {
+		s.Fatal("Failed to turn on display: ", err)
+	}
+
+	kb, err := input.VirtualKeyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed creating virtual keyboard: ", err)
+	}
+	defer kb.Close()
+
+	pv := s.FixtValue()
+	if pv == nil {
+		pv = s.PreValue()
+	}
+	cr, l, cs, err := lacros.Setup(ctx, pv, s.Param().(lacros.ChromeType))
+	if err != nil {
+		s.Fatal("Failed to initialize test: ", err)
+	}
+	defer lacros.CloseLacrosChrome(ctx, l)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	originalTabletMode, err := ash.TabletModeEnabled(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to obtain the tablet mode status: ", err)
+	}
+	defer ash.SetTabletModeEnabled(ctx, tconn, originalTabletMode)
+
+	// Run an http server to serve the test contents for accessing from the chrome browsers.
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+	url := server.URL + "/animation.html"
+
+	r := perfutil.NewRunner(cr)
+	currentWindows := 0
+	// Run the unlock flow for various situations.
+	// - change the number of browser windows, 2 or 8
+	// - the window system status; clamshell mode or tablet mode.
+	// If these window number values are changed, make sure to check lacros about:blank pages are closed correctly.
+	for i, windows := range []int{2, 8} {
+		conns, err := ash.CreateWindows(ctx, tconn, cs, url, windows-currentWindows)
+		if err != nil {
+			s.Fatal("Failed to create browser windows: ", err)
+		}
+		if err = conns.Close(); err != nil {
+			s.Fatal("Failed to close connections: ", err)
+		}
+
+		// This must be done after ash.CreateWindows to avoid terminating lacros-chrome.
+		if i == 0 && s.Param().(lacros.ChromeType) == lacros.ChromeTypeLacros {
+			if err := lacros.CloseAboutBlank(ctx, tconn, l.Devsess, 1); err != nil {
+				s.Fatal("Failed to close about:blank: ", err)
+			}
+		}
+
+		currentWindows = windows
+
+		for _, inTabletMode := range []bool{false, true} {
+			if err = ash.SetTabletModeEnabled(ctx, tconn, inTabletMode); err != nil {
+				s.Fatalf("Failed to set tablet mode %v: %v", inTabletMode, err)
+			}
+
+			if err = ash.ForEachWindow(ctx, tconn, func(w *ash.Window) error {
+				return ash.WaitWindowFinishAnimating(ctx, tconn, w.ID)
+			}); err != nil {
+				s.Fatal("Failed to wait: ", err)
+			}
+
+			var suffix string
+			if inTabletMode {
+				suffix = ".TabletMode"
+			} else {
+				suffix = ".ClamshellMode"
+			}
+
+			r.RunMultiple(ctx, s, fmt.Sprintf("%s-%dwindows", suffix, currentWindows), perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
+				// Lock screen
+				const accel = "Search+L"
+				if err := kb.Accel(ctx, accel); err != nil {
+					s.Fatalf("Typing %v failed: %v", accel, err)
+				}
+				if st, err := lockscreen.WaitState(ctx, tconn, func(st lockscreen.State) bool { return st.Locked && st.ReadyForPassword }, lockTimeout); err != nil {
+					s.Fatalf("Waiting for screen to be locked failed: %v (last status %+v)", err, st)
+				}
+
+				if err := kb.Type(ctx, password+"\n"); err != nil {
+					s.Fatal("Typing correct password failed: ", err)
+				}
+
+				if st, err := lockscreen.WaitState(ctx, tconn, func(st lockscreen.State) bool { return !st.Locked }, authTimeout); err != nil {
+					s.Fatalf("Waiting for screen to be unlocked failed: %v (last status %+v)", err, st)
+				}
+				// Wait extra 1 seconds so that all animations are finished.
+				// TODO(oshima): Add api to wait for animations.
+				if err = testing.Sleep(ctx, time.Second); err != nil {
+					s.Fatal("Failed to wait: ", err)
+				}
+
+				return nil
+			},
+				"Ash.UnlockAnimation.Smoothness"+suffix),
+				perfutil.StoreAll(perf.BiggerIsBetter, "percent", fmt.Sprintf("%dwindows", currentWindows)))
+		}
+	}
+
+	if err := r.Values().Save(ctx, s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
