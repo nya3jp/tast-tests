0de5c4b1c58dcc7c4d76379741b49a17b5543cf3
Daniil Lunev <dlunev@chromium.org>
2021-02-10 14:56:40 +1100

    tast: rollout control tests for fscrypt v2
    [1] Adds the SoftwareDeps, [2] and [3] provide context
    
    [1] https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2683946
    [2] go/fscryptv2-vs-uprev
    [3] https://chromium.googlesource.com/chromiumos/platform/tast/+/refs/heads/master/docs/test_dependencies.md
    
    BUG=chromium:1159863,chromium:1160982
    TEST=<pending>
    
    Change-Id: I042664fd9f268ce2a49861b8fa4d73d0705e110b

---
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/fscrypt_verify_disabled.go
@@ -0,0 +1,28 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"context"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FscryptCheckDisabled,
+		Desc: "Verifies the fscrypt v2 flag is disabled on the platforms where it should be disabled",
+		Contacts: []string{
+			"dlunev@google.com",
+			"chromeos-storage@google.com",
+		},
+		SoftwareDeps: []string{"no_fscrypt_v2_support", "use_fscrypt_v2"},
+		Attr:         []string{"group:mainline", "informational"},
+	})
+}
+
+func FscryptCheckDisabled(ctx context.Context, s *testing.State) {
+	s.Fatal("Fscrypt V2 is enabled via IUSE, but should not be")
+}
