b7e10da54521d75362e797596ca6f7105b28cb57
David Haddock <dhaddock@google.com>
2021-02-11 12:03:44 -0800

    Add Fixture (precondition v2) for Nearby Share local tests.
    This fixture will share the setup of the ChromeOS side in Nearby share
    tests. This will make test runs faster for eng and in the lab.
    
    The fixture could not read the nearbyshare/* libraries in
    local/bundles/cros/ so they had to be moved to the chrome/nearbyshare
    directory.
    
    Moving these folders caused import cycles so I have moved constants
    around to be with the packages that use them.
    
    Fixtures that call chrome.Lock() need to be added to a dictionary in
    chrome.go.
    
    BUG=chromium:1159975
    TEST=nearbyshare.* all passing locally and in the lab
    
    Change-Id: I52f0ec240da0805948fa639599ce2e6b46ebc26a

--- src/chromiumos/tast/local/bundles/cros/nearbyshare/high_visibility_ui_smoke.go
+++ src/chromiumos/tast/local/bundles/cros/nearbyshare/high_visibility_ui_smoke.go
@@ -7,8 +7,6 @@ package nearbyshare
 import (
 	"context"
 
-	"chromiumos/tast/local/bundles/cros/nearbyshare/nearbysetup"
-	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/nearbyshare"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/testing"
@@ -23,31 +21,15 @@ func init() {
 		},
 		Attr:         []string{"group:nearby-share"},
 		SoftwareDeps: []string{"chrome"},
+		Fixture:      "nearbyShareEnabledDataOfflineAllContacts",
 	})
 }
 
 // HighVisibilityUISmoke tests that we can open the receiving UI surface from Quick Settings.
 func HighVisibilityUISmoke(ctx context.Context, s *testing.State) {
-	// TODO(crbug/1159975): Remove flags (or use precondition) once the feature is enabled by default.
-	cr, err := chrome.New(
-		ctx,
-		chrome.EnableFeatures("IntentHandlingSharing", "NearbySharing", "Sharesheet"),
-	)
-	if err != nil {
-		s.Fatal("Failed to start Chrome: ", err)
-	}
-	defer cr.Close(ctx)
-
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Creating test API connection failed: ", err)
-	}
+	tconn := s.FixtValue().(*nearbyshare.FixtData).TestConn
+	deviceName := s.FixtValue().(*nearbyshare.FixtData).DeviceName
 	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
-	const deviceName = "device_HighVisibilityUISmoke"
-	if err := nearbysetup.CrOSSetup(ctx, tconn, cr, nearbyshare.DataUsageOnline, nearbyshare.VisibilityAllContacts, deviceName); err != nil {
-		s.Fatal("Failed to set up Nearby Share: ", err)
-	}
 	if err := nearbyshare.StartHighVisibilityMode(ctx, tconn, deviceName); err != nil {
 		s.Fatal("Failed to enable Nearby Share's high visibility mode: ", err)
 	}
