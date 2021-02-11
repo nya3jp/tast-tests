67700015771e1a78d5641948c2af011731e33364
Steven Bennetts <stevenjb@chromium.org>
2021-02-11 08:10:55 -0800

    ShillCellularSim: Compare Shill properties to ModemManager properties
    This increases the tast coverage to include the bulk of the
    Cellular_Identifiers autotest coverage.
    
    BUG=b:177103167
    TEST=tast run ${DUT} network.ShillCellularSim
    
    Change-Id: Ieb8a6fe99c5aae3b8a6d9ed55d7483df03cb7ba0

--- src/chromiumos/tast/local/bundles/cros/network/shill_cellular_sim.go
+++ src/chromiumos/tast/local/bundles/cros/network/shill_cellular_sim.go
@@ -7,32 +7,71 @@ package network
 import (
 	"context"
 
+	"chromiumos/tast/common/mmconst"
 	"chromiumos/tast/common/shillconst"
 	"chromiumos/tast/local/bundles/cros/network/cellular"
+	"chromiumos/tast/local/modemmanager"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:     ShillCellularSim,
-		Desc:     "Verifies that a Cellular Device and matching Service are present with a valid SIM card",
-		Contacts: []string{"stevenjb@google.com", "cros-network-health@google.com"},
+		Desc:     "Verifies that Cellular Device and Service properties match ModemManager SIM properties",
+		Contacts: []string{"stevenjb@google.com", "chromeos-cellular-team@google.com"},
 		Attr:     []string{"group:cellular"},
 	})
 }
 
 func ShillCellularSim(ctx context.Context, s *testing.State) {
+	// Gather ModemManager properties
+	modem, err := modemmanager.NewModem(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Modem: ", err)
+	}
+	modemProps, err := modem.GetDBusProperties(ctx)
+	if err != nil {
+		s.Fatal("Failed to call GetProperties on Modem: ", err)
+	}
+	simPath, err := modemProps.GetObjectPath(mmconst.ModemPropertySim)
+	if err != nil {
+		s.Fatal("Failed to get Modem.Sim property: ", err)
+	}
+	s.Log("SIM path = ", simPath)
+	simProps, err := modem.GetSimProperties(ctx, simPath)
+	if err != nil {
+		s.Fatalf("Failed to create Sim for path: %q: %s", simPath, err)
+	}
+	simIccid, err := simProps.GetString(mmconst.SimPropertySimIdentifier)
+	if err != nil {
+		s.Fatal("Failed to get Sim.SimIdentifier property: ", err)
+	}
+
+	// Gather Shill Device properties
 	helper, err := cellular.NewHelper(ctx)
 	if err != nil {
 		s.Fatal("Failed to create cellular.Helper")
 	}
-	if properties, err := helper.Device.GetShillProperties(ctx); err != nil {
+	deviceProps, err := helper.Device.GetShillProperties(ctx)
+	if err != nil {
 		s.Fatal("Failed to get Device properties: ", err)
-	} else if simPresent, err := properties.GetBool(shillconst.DevicePropertyCellularSIMPresent); err != nil {
-		s.Fatal("Failed to get SIMPresent property: ", err)
-	} else if !simPresent {
-		s.Fatal("SIM not present")
 	}
+	if simPresent, err := deviceProps.GetBool(shillconst.DevicePropertyCellularSIMPresent); err != nil {
+		s.Fatal("Failed to get Device.Cellular.SIMPresent property: ", err)
+	} else if !simPresent {
+		s.Fatal("SIMPresent property not set")
+	}
+	deviceIccid, err := deviceProps.GetString(shillconst.DevicePropertyCellularICCID)
+	if err != nil {
+		s.Fatal("Failed to get Device.Cellular.ICCID property: ", err)
+	}
+
+	// Ensure Shill Device ICCID and ModemManager ICCID match.
+	if deviceIccid != simIccid {
+		s.Fatalf("Device ICCID does not match SIM, got %q, want %q", deviceIccid, simIccid)
+	}
+
+	// Ensure that Shill creates a Service matching ICCID.
 	if _, err = helper.FindServiceForDevice(ctx); err != nil {
 		s.Fatal("Failed to get Cellular Service for Device: ", err)
 	}
