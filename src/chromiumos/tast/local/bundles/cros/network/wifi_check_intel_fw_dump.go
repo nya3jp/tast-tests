a943bbea850ae39280d8d6c620db0c30d593e0d3
Kevin Lund <kglund@google.com>
2021-02-11 17:47:54 -0700

    Tast: Add hwdep WifiIntel to tests
    Several tests are currently using the softwaredep "intel_wifi_chip" in
    combination with hwdep SkipOnModel to exclude devices with non-intel
    WiFi. This functionality has been added as a single hardwaredep
    "WifiIntel", so this CL replaces the instances of that usage with the
    WifiIntel hwdep. It also updates relevant comments and TODO's to be
    more up-to-date.
    
    BUG=b:180133528
    TEST=tast run ${DUT} ${TEST}
    Should pass on DUT with Intel WiFi, and skip on DUT without Intel Wifi
    Repeat for each affected test
    
    Cq-Depend: chromium:2689451
    Change-Id: I61934fdbcd3d8148321859d72c9d4b7fa07beb9a

--- src/chromiumos/tast/local/bundles/cros/network/wifi_check_intel_fw_dump.go
+++ src/chromiumos/tast/local/bundles/cros/network/wifi_check_intel_fw_dump.go
@@ -26,17 +26,11 @@ func init() {
 		Desc:         "Verifies that device coredumps are not empty",
 		Contacts:     []string{"arowa@chromium.org", "cros-telemetry@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"wifi", "intel_wifi_chip"},
-		// TODO(crbug.com/1070299): Remove the below hard-coded devices
-		// and the the software dependency "intel_wifi_chip" above.
-		// Instead, use the Intel WiFi dependency when wifi hardware
-		// dependencies are implemented.
-		// NB: These exclusions are somewhat overly broad; some
-		// (but not all) blooglet devices have WiFi chips that would
-		// work for this test. However, for now there is no better way
-		// to specify the exact hardware parameters needed for this
-		// test. (See linked bug.)
-		HardwareDeps: hwdep.D(hwdep.SkipOnModel("blooglet", "dirinboz", "ezkinil", "vilboz")),
+		SoftwareDeps: []string{"wifi"},
+		// NB: The WifiIntel dependency tracks a manually maintained list of devices.
+		// If the test is skipping when it should run or vice versa, check the hwdep
+		// to see if your board is incorrectly included/excluded.
+		HardwareDeps: hwdep.D(hwdep.WifiIntel()),
 	})
 }
 
