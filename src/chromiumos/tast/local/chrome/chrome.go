54837abf3be3427e8ae28cd9d8f2762a682453d4
Victor-Gabriel Savu <vsavu@google.com>
2021-02-12 09:49:22 +0100

    policy: Add the chromePolicyLoggedIn fixture and 2 test using it
    Add the chromePolicyLoggedIn fixture to complement the existing
    precondition. Add policy.AllowDinosaurEasterEggFixture and
    policy.Disable3DAPIsFixture as examples.
    
    This change should make it easier to later add a fixture
    for device policy tests.
    
    BUG=chromium:1143230
    TEST=tast run <ip> policy.AllowDinosaurEasterEggFixture policy.Disable3DAPIsFixture
    
    Change-Id: I7691e286d9e1bb091dc795f11b60464d7804686e

--- src/chromiumos/tast/local/chrome/chrome.go
+++ src/chromiumos/tast/local/chrome/chrome.go
@@ -64,6 +64,7 @@ var locked = false
 // prePackages lists packages containing preconditions that are allowed to call Lock and Unlock.
 var prePackages = []string{
 	"chromiumos/tast/local/arc",
+	"chromiumos/tast/local/policyutil/fixtures",
 	"chromiumos/tast/local/policyutil/pre",
 	"chromiumos/tast/local/bundles/cros/camera/testutil",
 	"chromiumos/tast/local/bundles/cros/ui/cuj",
---
+++ src/chromiumos/tast/local/policyutil/fixtures/chrome.go
@@ -0,0 +1,111 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fixtures
+
+import (
+	"context"
+
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name:            "chromePolicyLoggedIn",
+		Desc:            "Logged into a user session",
+		Contacts:        []string{"vsavu@google.com", "chromeos-commercial-stability@google.com"},
+		Impl:            &policyChromeFixture{},
+		SetUpTimeout:    chrome.LoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		Parent:          "fakeDMS",
+	})
+}
+
+type policyChromeFixture struct {
+	// Chrome is a connection to an already-started Chrome instance that loads policies from FakeDMS.
+	cr *chrome.Chrome
+	// FakeDMS is the already running DMS server from the parent fixture.
+	fdms *fakedms.FakeDMS
+}
+
+// FixtData is returned by the fixtures and used by tests to interact with Chrome and FakeDMS.
+type FixtData struct {
+	// FakeDMS is an already running DMS  server.
+	FakeDMS *fakedms.FakeDMS
+	// Chrome is a connection to an already-started Chrome instance that loads policies from FakeDMS.
+	Chrome *chrome.Chrome
+}
+
+// Credentials used for authenticating the test user.
+const (
+	Username = "tast-user@managedchrome.com"
+	Password = "test0000"
+	GaiaID   = "gaia-id"
+)
+
+func (p *policyChromeFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	fdms, ok := s.ParentValue().(*fakedms.FakeDMS)
+	if !ok {
+		s.Fatal("Parent is not a FakeDMS fixture")
+	}
+
+	p.fdms = fdms
+
+	// Start a Chrome instance that will fetch policies from the FakeDMS.
+	cr, err := chrome.New(ctx,
+		chrome.Auth(Username, Password, GaiaID),
+		chrome.DMSPolicy(fdms.URL))
+	if err != nil {
+		s.Fatal("Chrome login failed: ", err)
+	}
+
+	p.cr = cr
+
+	chrome.Lock()
+
+	return &FixtData{p.fdms, p.cr}
+}
+
+func (p *policyChromeFixture) TearDown(ctx context.Context, s *testing.FixtState) {
+	chrome.Unlock()
+
+	if p.cr == nil {
+		s.Fatal("Chrome not yet started")
+	}
+
+	if err := p.cr.Close(ctx); err != nil {
+		s.Error("Failed to close Chrome connection: ", err)
+	}
+
+	p.cr = nil
+}
+
+func (p *policyChromeFixture) Reset(ctx context.Context) error {
+	// Check the connection to Chrome.
+	if err := p.cr.Responded(ctx); err != nil {
+		return errors.Wrap(err, "existing Chrome connection is unusable")
+	}
+
+	// The policy blob has already been cleared.
+	if err := policyutil.RefreshChromePolicies(ctx, p.cr); err != nil {
+		return errors.Wrap(err, "failed to clear policies")
+	}
+
+	// Reset Chrome state.
+	if err := p.cr.ResetState(ctx); err != nil {
+		return errors.Wrap(err, "failed resetting existing Chrome session")
+	}
+
+	return nil
+}
+
+func (p *policyChromeFixture) PreTest(ctx context.Context, s *testing.FixtTestState) {}
+func (p *policyChromeFixture) PostTest(ctx context.Context, s *testing.FixtTestState) {
+	// TODO(crbug.com/1049532): Copy policy.json after each test finishes.
+}
