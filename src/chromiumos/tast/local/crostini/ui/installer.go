7b1b606022e92190b5a00bc38f502c33e4101743
Judy Wu <jinrongwu@google.com>
2021-02-15 11:15:17 +1100

    Crostini: installer
    Installer
    
    BUG=None
    TEST=Ran on hatch
    
    Change-Id: I021759870b0f26cbd350e4e3c5ad950d7160a97e

--- src/chromiumos/tast/local/crostini/ui/installer.go
+++ src/chromiumos/tast/local/crostini/ui/installer.go
@@ -168,7 +168,10 @@ func (p *Installer) Install(ctx context.Context) error {
 	// way and prevent the button from being clicked.
 	ui := uiauto.New(p.tconn)
 	installButton := nodewith.Name("Install").Role(role.Button)
-	if err := uiauto.Run(ctx, ui.LeftClick(installButton), ui.WithTimeout(8*time.Minute).WaitUntilGone(installWindow)); err != nil {
+	installCancel := nodewith.Name("Cancel").Role(role.Button)
+	if err := uiauto.Run(ctx,
+		ui.LeftClickUntil(installButton, ui.WithTimeout(2*time.Second).WaitUntilExists(installCancel)),
+		ui.WithTimeout(8*time.Minute).WaitUntilGone(installWindow)); err != nil {
 		// If the install fails, return any error message from the installer rather than a timeout error.
 		message, messageErr := p.checkErrorMessage(cleanupCtx)
 		if messageErr != nil {
