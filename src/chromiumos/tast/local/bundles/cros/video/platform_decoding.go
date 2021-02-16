397e8b7983233c7d85f3c8f45219b47c6d6287b7
Steve Cho <stevecho@google.com>
2021-02-16 13:25:43 -0800

    tast-tests/video: add video.PlatformDecoding.v4l2_vp9 test
    This CL wraps v4l2_stateful_decoder binary to run
    video.PlatformDecoding.v4l2_vp9 test with tast.
    
    BUG=b:180544893
    TEST=video.PlatformDecoding.v4l2_vp9 on trogdor
    
    Change-Id: I8e1ebfbf18886793cd8249c758705edd1d650dda

--- src/chromiumos/tast/local/bundles/cros/video/platform_decoding.go
+++ src/chromiumos/tast/local/bundles/cros/video/platform_decoding.go
@@ -18,33 +18,53 @@ import (
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 type failExpectedFn func(output []byte) bool
 
+// commandBuilderFn is the function type to generate the command line with arguments.
+type commandBuilderDecodeFn func(exe, filename string) (command []string)
+
 type platformDecodingParams struct {
-	filename     string
-	failExpected failExpectedFn
+	filename       string
+	failExpected   failExpectedFn
+	decoder        string                 // command line decoder binary
+	commandBuilder commandBuilderDecodeFn // Function to create the command line arguments.
 }
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func: PlatformDecoding,
-		Desc: "Smoke tests libva decoding by running the media/gpu/vaapi/test:decode_test binary",
+		Desc: "Smoke tests for vaapi libva decoding by running the media/gpu/vaapi/test:decode_test binary, for v4l2 decoding by running the drm-tests/v4l2_decode binary",
 		Contacts: []string{
 			"jchinlee@chromium.org",
+			"stevecho@chromium.org",
 			"chromeos-gfx-video@google.com",
 		},
-		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
-		SoftwareDeps: []string{"vaapi"},
+		Attr: []string{"group:graphics", "graphics_video", "graphics_perbuild"},
 		Params: []testing.Param{{
-			Name: "vp9",
+			Name: "vaapi_vp9",
 			Val: platformDecodingParams{
-				filename:     "resolution_change_500frames.vp9.ivf",
-				failExpected: nil,
+				filename:       "resolution_change_500frames.vp9.ivf",
+				failExpected:   nil,
+				decoder:        filepath.Join(chrome.BinTestDir, "decode_test"),
+				commandBuilder: vp9decodeVAAPIargs,
 			},
-			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraSoftwareDeps: []string{"vaapi", caps.HWDecodeVP9},
 			ExtraData:         []string{"resolution_change_500frames.vp9.ivf", "resolution_change_500frames.vp9.ivf.json"},
+		}, {
+			Name: "v4l2_vp9",
+			Val: platformDecodingParams{
+				filename:       "1080p_30fps_300frames.vp9.ivf",
+				failExpected:   nil,
+				decoder:        "v4l2_stateful_decoder",
+				commandBuilder: vp9decodeV4L2args,
+			},
+			ExtraHardwareDeps: hwdep.D(hwdep.Platform("trogdor")),
+			ExtraSoftwareDeps: []string{"v4l2_codec", caps.HWDecodeVP9},
+			// TODO(b/180615056): need Dynamic Resolution Change support to use resolution_change_500frames.vp9.ivf like vaapi
+			ExtraData: []string{"1080p_30fps_300frames.vp9.ivf", "1080p_30fps_300frames.vp9.ivf.json"},
 		}, {
 			// Attempt to decode an unsupported codec to ensure that the binary is not
 			// unconditionally succeeding, i.e. not crashing even when expected to.
@@ -54,18 +74,21 @@ func init() {
 				failExpected: func(output []byte) bool {
 					return strings.Contains(string(output), "Codec VP80 not supported.")
 				},
+				decoder:        filepath.Join(chrome.BinTestDir, "decode_test"),
+				commandBuilder: vp9decodeVAAPIargs,
 			},
-			ExtraData: []string{"resolution_change_500frames.vp8.ivf", "resolution_change_500frames.vp8.ivf.json"},
+			ExtraSoftwareDeps: []string{"vaapi"},
+			ExtraData:         []string{"resolution_change_500frames.vp8.ivf", "resolution_change_500frames.vp8.ivf.json"},
 		}},
 	})
 }
 
-// PlatformDecoding runs the media/gpu/vaapi/test:decode_test binary on the
-// file specified in the testing state. The test fails if any of the VAAPI calls
-// fail (or if the test is incorrectly invoked): notably, the binary does not
-// check for correctness of decoded output. This test is motivated by instances
-// in which libva uprevs may introduce regressions and cause decoding to break
-// for reasons unrelated to Chrome.
+// PlatformDecoding runs the media/gpu/vaapi/test:decode_test binary for vaapi
+// or drm-tests/v4l2_stateful_decoder binary on the file specified in the testing state.
+// The test fails if any of the VAAPI or V4L2 calls fail (or if the test is incorrectly invoked):
+// notably, the binary does not check for correctness of decoded output.
+// This test is motivated by instances in which libva uprevs may introduce regressions
+// and cause decoding to break for reasons unrelated to Chrome.
 func PlatformDecoding(ctx context.Context, s *testing.State) {
 	testOpt := s.Param().(platformDecodingParams)
 	const cleanupTime = 90 * time.Second
@@ -89,16 +112,17 @@ func PlatformDecoding(ctx context.Context, s *testing.State) {
 	}
 	defer upstart.EnsureJobRunning(cleanupCtx, "ui")
 
-	// Run the decode_test binary, propagating its errors: the decode_test binary
-	// fails if the VAAPI calls themselves error, the binary is called on
-	// unsupported inputs or could not open the DRI render node, or the binary
-	// otherwise crashes.
-	const exec = "decode_test"
+	// Run the decode_test binary for vaapi or the v4l2_stateful_decoder binary
+	// for v4l2, propagating its errors: the binary fails if the VAAPI or V4l2 calls
+	// themselves error, the binary is called on unsupported inputs or could not open
+	// the DRI render node, or the binary otherwise crashes.
+	exec := testOpt.decoder
+	command := testOpt.commandBuilder(exec, s.DataPath(testOpt.filename))
 	testing.ContextLog(ctx, "Running ", exec)
 	output, err := testexec.CommandContext(
 		ctx,
-		filepath.Join(chrome.BinTestDir, exec),
-		"--video="+s.DataPath(testOpt.filename),
+		command[0],
+		command[1],
 	).CombinedOutput(testexec.DumpLogOnError)
 
 	if err != nil && (testOpt.failExpected == nil || !testOpt.failExpected(output)) {
@@ -107,4 +131,18 @@ func PlatformDecoding(ctx context.Context, s *testing.State) {
 	if err == nil && testOpt.failExpected != nil {
 		s.Fatalf("%v passed when expected to fail", exec)
 	}
+}
+
+// vp9decodeV4L2args constructs the command line for the VP9 decoding binary exe for v4l2.
+func vp9decodeV4L2args(exe, filename string) (command []string) {
+	command = append(command, exe, "--file="+filename)
+
+	return
+}
+
+// vp9decodeVAAPIargs constructs the command line for the VP9 decoding binary exe for vaapi.
+func vp9decodeVAAPIargs(exe, filename string) (command []string) {
+	command = append(command, exe, "--video="+filename)
+
+	return
 }
