95eb6b8cf157a5ce38a3422a7fe93a52d10a2b01
Prashant Malani <pmalani@chromium.org>
2021-02-17 15:49:58 -0800

    tast-tests: typec: Add a test to check typecd crashing
    Add a test which verifies that if typecd crashes in the middle of the
    session, that it restarts and subsequent mode transitions (for example,
    now a later logout) get handled correctly.
    
    BUG=b:180055654
    TEST=typec.ModeCrash passes 15 times on a Voxel+DellWD19TB+DP monitor
    setup.
    
    Change-Id: I5c2b2dae0da53c9e0b87d89871555bda3222dad2

---
+++ src/chromiumos/tast/local/bundles/cros/typec/mode_crash.go
@@ -0,0 +1,143 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package typec
+
+import (
+	"context"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/typec/typecutils"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ModeCrash,
+		Desc:         "Checks USB Type C mode switch behaviour when typecd crashes",
+		Contacts:     []string{"pmalani@chromium.org"},
+		SoftwareDeps: []string{"chrome"},
+		Vars:         []string{"ui.signinProfileTestExtensionManifestKey"},
+		Params: []testing.Param{
+			// For running manually.
+			{},
+			// For automated testing.
+			{
+				Name:              "test",
+				ExtraAttr:         []string{"group:mainline", "informational"},
+				ExtraHardwareDeps: hwdep.D(hwdep.Model("volteer", "voxel")),
+			}},
+	})
+}
+
+// ModeCrash does the following:
+// - Login.
+// - Validate that TBT alt mode is working correctly.
+// - Kill typecd
+// - Logout.
+// - Validate that the dock in in USB+Dp alt mode.
+//
+// This test requires the following H/W topology to run.
+//
+//
+//        DUT ------> Thunderbolt3 (>= Titan Ridge) dock -----> DP monitor.
+//      (USB4)
+//
+func ModeCrash(ctx context.Context, s *testing.State) {
+	// This check is for test executions which take place on
+	// CQ (where TBT peripherals aren't connected).
+	if present, err := typecutils.CheckPortsForTBTPartner(ctx); err != nil {
+		s.Fatal("Couldn't determine TBT device from PD identity: ", err)
+	} else if !present {
+		s.Log("No TBT device connected to DUT")
+		return
+	}
+
+	cr, err := chrome.New(ctx,
+		chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey")))
+	if err != nil {
+		s.Fatal("Failed to start Chrome with login: ", err)
+	}
+
+	testConn, err := cr.SigninProfileTestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get Test API connection: ", err)
+	}
+
+	s.Log("Verifying that TBT device enumerated correctly")
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		return typecutils.CheckTBTDevice(true)
+	}, &testing.PollOptions{Interval: 100 * time.Millisecond, Timeout: 10 * time.Second})
+	if err != nil {
+		s.Fatal("Failed to verify TBT devices connected after login: ", err)
+	}
+
+	s.Log("Verifying that DP monitor enumerated correctly")
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		return typecutils.FindConnectedDPMonitor(ctx, testConn)
+	}, &testing.PollOptions{Interval: 200 * time.Millisecond, Timeout: 20 * time.Second})
+	if err != nil {
+		s.Fatal("Failed to verify DP monitor working after login: ", err)
+	}
+
+	s.Log("Killing typecd")
+	if err := testexec.CommandContext(ctx, "pkill", "typecd").Run(); err != nil {
+		s.Fatal("Failed to kill typecd: ", err)
+	}
+
+	// Wait for 2 seconds for typecd to restart.
+	if err := testing.Sleep(ctx, 2*time.Second); err != nil {
+		s.Fatal("Sleep after typecd kill failed: ", err)
+	}
+
+	s.Log("Checking that typecd restarted")
+	out, err := testexec.CommandContext(ctx, "pgrep", "typecd").Output()
+	if err != nil {
+		s.Fatal("Failed to run pgrep to check typecd restart: ", err)
+	}
+
+	// A valid PID is sufficient for us to know typecd is running again.
+	if pid, err := strconv.Atoi(strings.TrimSpace(string(out))); err != nil {
+		s.Fatal("Failed to convert pgrep output: ", err)
+	} else if pid < 0 {
+		s.Fatalf("typecd doesn't have a valid PID on restart: %d", pid)
+	}
+
+	// Log out;
+	cr.Close(ctx)
+	cr, err = chrome.New(ctx,
+		chrome.DeferLogin(),
+		chrome.KeepState(),
+		chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey")))
+	if err != nil {
+		s.Fatal("Failed to start Chrome with login: ", err)
+	}
+	defer cr.Close(ctx)
+
+	testConn, err = cr.SigninProfileTestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get pre-login Test API connection: ", err)
+	}
+
+	s.Log("Verifying that no TBT devices enumerated")
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		return typecutils.CheckTBTDevice(false)
+	}, &testing.PollOptions{Interval: 100 * time.Millisecond, Timeout: 10 * time.Second})
+	if err != nil {
+		s.Fatal("Failed to verify no TBT devices connected at login screen: ", err)
+	}
+
+	s.Log("Verifying that DP monitor enumerated correctly")
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		return typecutils.FindConnectedDPMonitor(ctx, testConn)
+	}, &testing.PollOptions{Interval: 200 * time.Millisecond, Timeout: 20 * time.Second})
+	if err != nil {
+		s.Fatal("Failed to verify DP monitor working at login screen: ", err)
+	}
+}
