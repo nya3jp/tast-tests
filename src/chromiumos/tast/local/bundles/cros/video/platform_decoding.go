78acb2a01d39447151e1423c3d16718ededebdcf
Jao-ke Chin-Lee <jchinlee@chromium.org>
2021-02-18 13:17:37 -0800

    video/platform_decoding: Add MD5 content verification.
    BUG=chromium:1062407,b:175909299
    TEST=video.PlatformDecoding.* on nautilus
    
    Change-Id: I882e607698f87458062f736b7ad84e47c81f902c

--- src/chromiumos/tast/local/bundles/cros/video/platform_decoding.go
+++ src/chromiumos/tast/local/bundles/cros/video/platform_decoding.go
@@ -6,6 +6,9 @@ package video
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"path/filepath"
 	"strings"
 	"time"
@@ -20,13 +23,6 @@ import (
 	"chromiumos/tast/testing"
 )
 
-type failExpectedFn func(output []byte) bool
-
-type platformDecodingParams struct {
-	filename     string
-	failExpected failExpectedFn
-}
-
 func init() {
 	testing.AddTest(&testing.Test{
 		Func: PlatformDecoding,
@@ -38,36 +34,74 @@ func init() {
 		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
 		SoftwareDeps: []string{"vaapi"},
 		Params: []testing.Param{{
-			Name: "vp9",
-			Val: platformDecodingParams{
-				filename:     "resolution_change_500frames.vp9.ivf",
-				failExpected: nil,
-			},
+			Name:              "vp9",
+			Val:               "resolution_change_500frames.vp9.ivf",
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"resolution_change_500frames.vp9.ivf", "resolution_change_500frames.vp9.ivf.json"},
-		}, {
-			// Attempt to decode an unsupported codec to ensure that the binary is not
-			// unconditionally succeeding, i.e. not crashing even when expected to.
-			Name: "unsupported_codec_fail",
-			Val: platformDecodingParams{
-				filename: "resolution_change_500frames.vp8.ivf",
-				failExpected: func(output []byte) bool {
-					return strings.Contains(string(output), "Codec VP80 not supported.")
-				},
-			},
-			ExtraData: []string{"resolution_change_500frames.vp8.ivf", "resolution_change_500frames.vp8.ivf.json"},
 		}},
 	})
 }
 
-// PlatformDecoding runs the media/gpu/vaapi/test:decode_test binary on the
-// file specified in the testing state. The test fails if any of the VAAPI calls
-// fail (or if the test is incorrectly invoked): notably, the binary does not
-// check for correctness of decoded output. This test is motivated by instances
-// in which libva uprevs may introduce regressions and cause decoding to break
-// for reasons unrelated to Chrome.
+// readMetadata reads metadata from metadata json.
+func readMetadata(metadataPath string) (meta map[string]interface{}, err error) {
+	metadataJSONBytes, err := ioutil.ReadFile(metadataPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to read metadata file at %s", metadataPath)
+	}
+
+	if err = json.Unmarshal(metadataJSONBytes, &meta); err != nil {
+		return nil, errors.Wrapf(err, "failed to read json from metadata file at %s", metadataPath)
+	}
+
+	return
+}
+
+// verifyContent compares expected per-frame hashes from metadata json to actual
+// hashes.
+func verifyContent(expectedHashesPath, actualOutput string) error {
+	meta, err := readMetadata(expectedHashesPath)
+	if err != nil {
+		return errors.Wrap(err, "failed to verify per-frame hashes")
+	}
+	expected, ok := meta["md5_checksums"].([]interface{})
+	if !ok {
+		return errors.Errorf("`md5_checksums` in metadata at %s not a slice; got %v", expectedHashesPath, meta["md5_checksums"])
+	}
+
+	actual := strings.Split(strings.TrimSpace(actualOutput), "\n")
+	if len(expected) != len(actual) {
+		return errors.Errorf("expected and actual number of frames mismatched (%d != %d)", len(expected), len(actual))
+	}
+
+	var first string
+	var count int
+	for i, ex := range expected {
+		if _, ok := ex.(string); !ok {
+			return errors.Errorf("failed to cast expected hash %v of type %T to string", ex, ex)
+		}
+		if wanted, got := strings.TrimSpace(ex.(string)), strings.TrimSpace(actual[i]); wanted != got {
+			count++
+			if first == "" {
+				first = fmt.Sprintf("frame %d (%s != %s)", i, wanted, got)
+			}
+		}
+	}
+
+	if count > 0 {
+		return errors.Errorf("%d mismatched hashes, first at %s", count, first)
+	}
+
+	return nil
+}
+
+// PlatformDecoding runs the media/gpu/vaapi/test:decode_test binary on the file
+// specified in the testing state. The test fails if any of the VAAPI calls fail
+// (or if the test is incorrectly invoked), or if any of the decoded frames have
+// hashes not matching reference values. This test is motivated by instances in
+// which libva uprevs may introduce regressions and cause decoding to break for
+// reasons unrelated to Chrome.
 func PlatformDecoding(ctx context.Context, s *testing.State) {
-	testOpt := s.Param().(platformDecodingParams)
+	filename := s.Param().(string)
 	const cleanupTime = 90 * time.Second
 
 	// Reserve time to restart the ui job and perform cleanup at the end of the test.
@@ -93,18 +127,25 @@ func PlatformDecoding(ctx context.Context, s *testing.State) {
 	// fails if the VAAPI calls themselves error, the binary is called on
 	// unsupported inputs or could not open the DRI render node, or the binary
 	// otherwise crashes.
+	// The test may also fail if the decoded results do not match expected MD5
+	// hashes.
 	const exec = "decode_test"
-	testing.ContextLog(ctx, "Running ", exec)
-	output, err := testexec.CommandContext(
+	stdout, stderr, err := testexec.CommandContext(
 		ctx,
 		filepath.Join(chrome.BinTestDir, exec),
-		"--video="+s.DataPath(testOpt.filename),
-	).CombinedOutput(testexec.DumpLogOnError)
+		"--video="+s.DataPath(filename),
+		"--md5",
+		// vpxdec is used to compute reference hashes, and outputs only those for
+		// visible frames
+		"--visible",
+	).SeparatedOutput(testexec.DumpLogOnError)
 
-	if err != nil && (testOpt.failExpected == nil || !testOpt.failExpected(output)) {
+	if err != nil {
+		output := append(stdout, stderr...)
 		s.Fatalf("%v failed unexpectedly: %v", exec, errors.Wrap(err, string(output)))
 	}
-	if err == nil && testOpt.failExpected != nil {
-		s.Fatalf("%v passed when expected to fail", exec)
+
+	if err := verifyContent(s.DataPath(filename+".json"), string(stdout)); err != nil {
+		s.Fatalf("%v failed to verify content: %v", exec, errors.Wrap(err, filename))
 	}
 }
