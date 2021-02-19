68e525a1576390a2d4c52123b47898a7b4bcaf60
Jao-ke Chin-Lee <jchinlee@chromium.org>
2021-02-19 02:10:34 -0800

    video/platform_decoding: Add tests for some of the webm VP9 test vectors.
    BUG=chromium:1062407,b:175909299
    TEST=video.PlatformDecoding.* on nautilus
    
    Change-Id: I05d86297f360b0025d490316e4bcacc15fd95b49

--- src/chromiumos/tast/local/bundles/cros/video/platform_decoding.go
+++ src/chromiumos/tast/local/bundles/cros/video/platform_decoding.go
@@ -25,8 +25,149 @@ import (
 type failExpectedFn func(stdout, stderr []byte) bool
 
 type platformDecodingParams struct {
-	filename     string
+	filenames    []string
 	failExpected failExpectedFn
+}
+
+var vp9Files = map[string]map[string][]string{
+	"profile_0": {
+		"buf": {
+			"test_vectors/vp9/crowd_run_1080X512_fr30_bd8_8buf_l3.ivf",
+			"test_vectors/vp9/crowd_run_1280X768_fr30_bd8_8buf_l31.ivf",
+			"test_vectors/vp9/crowd_run_2048X1088_fr30_bd8_8buf_l4.ivf",
+			"test_vectors/vp9/crowd_run_2048X1088_fr60_bd8_6buf_l41.ivf",
+			"test_vectors/vp9/crowd_run_256X144_fr15_bd8_8buf_l1.ivf",
+			"test_vectors/vp9/crowd_run_384X192_fr30_bd8_8buf_l11.ivf",
+			"test_vectors/vp9/crowd_run_4096X2176_fr30_bd8_4buf_l5.ivf",
+			"test_vectors/vp9/crowd_run_4096X2176_fr60_bd8_4buf_l51.ivf",
+			"test_vectors/vp9/crowd_run_480X256_fr30_bd8_8buf_l2.ivf",
+			"test_vectors/vp9/crowd_run_640X384_fr30_bd8_8buf_l21.ivf",
+			"test_vectors/vp9/grass_1_1080X512_fr30_bd8_8buf_l3.ivf",
+			"test_vectors/vp9/grass_1_1280X768_fr30_bd8_8buf_l31.ivf",
+			"test_vectors/vp9/grass_1_2048X1088_fr30_bd8_8buf_l4.ivf",
+			"test_vectors/vp9/grass_1_2048X1088_fr60_bd8_6buf_l41.ivf",
+			"test_vectors/vp9/grass_1_256X144_fr15_bd8_8buf_l1.ivf",
+			"test_vectors/vp9/grass_1_384X192_fr30_bd8_8buf_l11.ivf",
+			"test_vectors/vp9/grass_1_4096X2176_fr30_bd8_4buf_l5.ivf",
+			"test_vectors/vp9/grass_1_4096X2176_fr60_bd8_4buf_l51.ivf",
+			"test_vectors/vp9/grass_1_480X256_fr30_bd8_8buf_l2.ivf",
+			"test_vectors/vp9/grass_1_640X384_fr30_bd8_8buf_l21.ivf",
+			"test_vectors/vp9/street1_1_1080X512_fr30_bd8_8buf_l3.ivf",
+			"test_vectors/vp9/street1_1_1280X768_fr30_bd8_8buf_l31.ivf",
+			"test_vectors/vp9/street1_1_2048X1088_fr30_bd8_8buf_l4.ivf",
+			"test_vectors/vp9/street1_1_2048X1088_fr60_bd8_6buf_l41.ivf",
+			"test_vectors/vp9/street1_1_256X144_fr15_bd8_8buf_l1.ivf",
+			"test_vectors/vp9/street1_1_384X192_fr30_bd8_8buf_l11.ivf",
+			"test_vectors/vp9/street1_1_4096X2176_fr30_bd8_4buf_l5.ivf",
+			"test_vectors/vp9/street1_1_4096X2176_fr60_bd8_4buf_l51.ivf",
+			"test_vectors/vp9/street1_1_480X256_fr30_bd8_8buf_l2.ivf",
+			"test_vectors/vp9/street1_1_640X384_fr30_bd8_8buf_l21.ivf",
+		},
+		"gf_dist": {
+			"test_vectors/vp9/crowd_run_1080X512_fr30_bd8_gf_dist_4_l3.ivf",
+			"test_vectors/vp9/crowd_run_1280X768_fr30_bd8_gf_dist_4_l31.ivf",
+			"test_vectors/vp9/crowd_run_2048X1088_fr30_bd8_gf_dist_4_l4.ivf",
+			"test_vectors/vp9/crowd_run_2048X1088_fr60_bd8_gf_dist_5_l41.ivf",
+			"test_vectors/vp9/crowd_run_256X144_fr15_bd8_gf_dist_4_l1.ivf",
+			"test_vectors/vp9/crowd_run_384X192_fr30_bd8_gf_dist_4_l11.ivf",
+			"test_vectors/vp9/crowd_run_4096X2176_fr30_bd8_gf_dist_6_l5.ivf",
+			"test_vectors/vp9/crowd_run_4096X2176_fr60_bd8_gf_dist_10_l51.ivf",
+			"test_vectors/vp9/crowd_run_480X256_fr30_bd8_gf_dist_4_l2.ivf",
+			"test_vectors/vp9/crowd_run_640X384_fr30_bd8_gf_dist_4_l21.ivf",
+			"test_vectors/vp9/grass_1_1080X512_fr30_bd8_gf_dist_4_l3.ivf",
+			"test_vectors/vp9/grass_1_1280X768_fr30_bd8_gf_dist_4_l31.ivf",
+			"test_vectors/vp9/grass_1_2048X1088_fr30_bd8_gf_dist_4_l4.ivf",
+			"test_vectors/vp9/grass_1_2048X1088_fr60_bd8_gf_dist_5_l41.ivf",
+			"test_vectors/vp9/grass_1_256X144_fr15_bd8_gf_dist_4_l1.ivf",
+			"test_vectors/vp9/grass_1_384X192_fr30_bd8_gf_dist_4_l11.ivf",
+			"test_vectors/vp9/grass_1_4096X2176_fr30_bd8_gf_dist_6_l5.ivf",
+			"test_vectors/vp9/grass_1_4096X2176_fr60_bd8_gf_dist_10_l51.ivf",
+			"test_vectors/vp9/grass_1_480X256_fr30_bd8_gf_dist_4_l2.ivf",
+			"test_vectors/vp9/grass_1_640X384_fr30_bd8_gf_dist_4_l21.ivf",
+			"test_vectors/vp9/street1_1_1080X512_fr30_bd8_gf_dist_4_l3.ivf",
+			"test_vectors/vp9/street1_1_1280X768_fr30_bd8_gf_dist_4_l31.ivf",
+			"test_vectors/vp9/street1_1_2048X1088_fr30_bd8_gf_dist_4_l4.ivf",
+			"test_vectors/vp9/street1_1_2048X1088_fr60_bd8_gf_dist_5_l41.ivf",
+			"test_vectors/vp9/street1_1_256X144_fr15_bd8_gf_dist_4_l1.ivf",
+			"test_vectors/vp9/street1_1_384X192_fr30_bd8_gf_dist_4_l11.ivf",
+			"test_vectors/vp9/street1_1_4096X2176_fr30_bd8_gf_dist_6_l5.ivf",
+			"test_vectors/vp9/street1_1_4096X2176_fr60_bd8_gf_dist_10_l51.ivf",
+			"test_vectors/vp9/street1_1_480X256_fr30_bd8_gf_dist_4_l2.ivf",
+			"test_vectors/vp9/street1_1_640X384_fr30_bd8_gf_dist_4_l21.ivf",
+		},
+		"odd_size": {
+			"test_vectors/vp9/crowd_run_1080X504_fr30_bd8_odd_size_l3.ivf",
+			"test_vectors/vp9/crowd_run_1280X768_fr30_bd8_odd_size_l31.ivf",
+			"test_vectors/vp9/crowd_run_2040X1080_fr30_bd8_odd_size_l4.ivf",
+			"test_vectors/vp9/crowd_run_2040X1080_fr60_bd8_odd_size_l41.ivf",
+			"test_vectors/vp9/crowd_run_248X144_fr15_bd8_odd_size_l1.ivf",
+			"test_vectors/vp9/crowd_run_376X184_fr30_bd8_odd_size_l11.ivf",
+			"test_vectors/vp9/crowd_run_4088X2168_fr30_bd8_odd_size_l5.ivf",
+			"test_vectors/vp9/crowd_run_4088X2168_fr60_bd8_odd_size_l51.ivf",
+			"test_vectors/vp9/crowd_run_472X248_fr30_bd8_odd_size_l2.ivf",
+			"test_vectors/vp9/crowd_run_632X376_fr30_bd8_odd_size_l21.ivf",
+			"test_vectors/vp9/grass_1_1080X504_fr30_bd8_odd_size_l3.ivf",
+			"test_vectors/vp9/grass_1_1280X768_fr30_bd8_odd_size_l31.ivf",
+			"test_vectors/vp9/grass_1_2040X1080_fr30_bd8_odd_size_l4.ivf",
+			"test_vectors/vp9/grass_1_2040X1080_fr60_bd8_odd_size_l41.ivf",
+			"test_vectors/vp9/grass_1_248X144_fr15_bd8_odd_size_l1.ivf",
+			"test_vectors/vp9/grass_1_376X184_fr30_bd8_odd_size_l11.ivf",
+			"test_vectors/vp9/grass_1_4088X2168_fr30_bd8_odd_size_l5.ivf",
+			"test_vectors/vp9/grass_1_4088X2168_fr60_bd8_odd_size_l51.ivf",
+			"test_vectors/vp9/grass_1_472X248_fr30_bd8_odd_size_l2.ivf",
+			"test_vectors/vp9/grass_1_632X376_fr30_bd8_odd_size_l21.ivf",
+			"test_vectors/vp9/street1_1_1080X504_fr30_bd8_odd_size_l3.ivf",
+			"test_vectors/vp9/street1_1_1280X768_fr30_bd8_odd_size_l31.ivf",
+			"test_vectors/vp9/street1_1_2040X1080_fr30_bd8_odd_size_l4.ivf",
+			"test_vectors/vp9/street1_1_2040X1080_fr60_bd8_odd_size_l41.ivf",
+			"test_vectors/vp9/street1_1_248X144_fr15_bd8_odd_size_l1.ivf",
+			"test_vectors/vp9/street1_1_376X184_fr30_bd8_odd_size_l11.ivf",
+			"test_vectors/vp9/street1_1_4088X2168_fr30_bd8_odd_size_l5.ivf",
+			"test_vectors/vp9/street1_1_4088X2168_fr60_bd8_odd_size_l51.ivf",
+			"test_vectors/vp9/street1_1_472X248_fr30_bd8_odd_size_l2.ivf",
+			"test_vectors/vp9/street1_1_632X376_fr30_bd8_odd_size_l21.ivf",
+		},
+		"sub8X8": {
+			"test_vectors/vp9/crowd_run_1080X512_fr30_bd8_sub8X8_l3.ivf",
+			"test_vectors/vp9/crowd_run_1280X768_fr30_bd8_sub8X8_l31.ivf",
+			"test_vectors/vp9/crowd_run_2048X1088_fr30_bd8_sub8X8_l4.ivf",
+			"test_vectors/vp9/crowd_run_2048X1088_fr60_bd8_sub8X8_l41.ivf",
+			"test_vectors/vp9/crowd_run_256X144_fr15_bd8_sub8X8_l1.ivf",
+			"test_vectors/vp9/crowd_run_384X192_fr30_bd8_sub8X8_l11.ivf",
+			"test_vectors/vp9/crowd_run_4096X2176_fr30_bd8_sub8X8_l5.ivf",
+			"test_vectors/vp9/crowd_run_4096X2176_fr60_bd8_sub8X8_l51.ivf",
+			"test_vectors/vp9/crowd_run_480X256_fr30_bd8_sub8X8_l2.ivf",
+			"test_vectors/vp9/crowd_run_640X384_fr30_bd8_sub8X8_l21.ivf",
+			"test_vectors/vp9/grass_1_1080X512_fr30_bd8_sub8X8_l3.ivf",
+			"test_vectors/vp9/grass_1_1280X768_fr30_bd8_sub8X8_l31.ivf",
+			"test_vectors/vp9/grass_1_2048X1088_fr30_bd8_sub8X8_l4.ivf",
+			"test_vectors/vp9/grass_1_2048X1088_fr60_bd8_sub8X8_l41.ivf",
+			"test_vectors/vp9/grass_1_256X144_fr15_bd8_sub8X8_l1.ivf",
+			"test_vectors/vp9/grass_1_384X192_fr30_bd8_sub8X8_l11.ivf",
+			"test_vectors/vp9/grass_1_4096X2176_fr30_bd8_sub8X8_l5.ivf",
+			"test_vectors/vp9/grass_1_4096X2176_fr60_bd8_sub8X8_l51.ivf",
+			"test_vectors/vp9/grass_1_480X256_fr30_bd8_sub8X8_l2.ivf",
+			"test_vectors/vp9/grass_1_640X384_fr30_bd8_sub8X8_l21.ivf",
+			"test_vectors/vp9/street1_1_1080X512_fr30_bd8_sub8X8_l3.ivf",
+			"test_vectors/vp9/street1_1_1280X768_fr30_bd8_sub8X8_l31.ivf",
+			"test_vectors/vp9/street1_1_2048X1088_fr30_bd8_sub8X8_l4.ivf",
+			"test_vectors/vp9/street1_1_2048X1088_fr60_bd8_sub8X8_l41.ivf",
+			"test_vectors/vp9/street1_1_256X144_fr15_bd8_sub8X8_l1.ivf",
+			"test_vectors/vp9/street1_1_384X192_fr30_bd8_sub8X8_l11.ivf",
+			"test_vectors/vp9/street1_1_4096X2176_fr30_bd8_sub8X8_l5.ivf",
+			"test_vectors/vp9/street1_1_4096X2176_fr60_bd8_sub8X8_l51.ivf",
+			"test_vectors/vp9/street1_1_480X256_fr30_bd8_sub8X8_l2.ivf",
+			"test_vectors/vp9/street1_1_640X384_fr30_bd8_sub8X8_l21.ivf",
+		},
+	},
+}
+
+func testFiles(videoFiles []string) []string {
+	tf := make([]string, 0, 2*len(videoFiles))
+	for _, name := range videoFiles {
+		tf = append(tf, name, name+".json")
+	}
+	return tf
 }
 
 func init() {
@@ -40,19 +181,47 @@ func init() {
 		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
 		SoftwareDeps: []string{"vaapi"},
 		Params: []testing.Param{{
-			Name: "vp9",
+			Name: "vp9_0_buf",
 			Val: platformDecodingParams{
-				filename:     "resolution_change_500frames.vp9.ivf",
+				filenames:    vp9Files["profile_0"]["buf"],
 				failExpected: nil,
 			},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
-			ExtraData:         []string{"resolution_change_500frames.vp9.ivf", "resolution_change_500frames.vp9.ivf.json"},
+			ExtraData:         testFiles(vp9Files["profile_0"]["buf"]),
+			Timeout:           10 * time.Minute,
+		}, {
+			Name: "vp9_0_gf_dist",
+			Val: platformDecodingParams{
+				filenames:    vp9Files["profile_0"]["gf_dist"],
+				failExpected: nil,
+			},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         testFiles(vp9Files["profile_0"]["gf_dist"]),
+			Timeout:           10 * time.Minute,
+		}, {
+			Name: "vp9_0_odd_size",
+			Val: platformDecodingParams{
+				filenames:    vp9Files["profile_0"]["odd_size"],
+				failExpected: nil,
+			},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         testFiles(vp9Files["profile_0"]["odd_size"]),
+			Timeout:           10 * time.Minute,
+		}, {
+			Name: "vp9_0_sub8x8",
+			Val: platformDecodingParams{
+				filenames:    vp9Files["profile_0"]["sub8X8"],
+				failExpected: nil,
+			},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         testFiles(vp9Files["profile_0"]["sub8X8"]),
+			Timeout:           10 * time.Minute,
 		}, {
 			// Attempt to decode an unsupported codec to ensure that the binary is not
 			// unconditionally succeeding, i.e. not crashing even when expected to.
 			Name: "unsupported_codec_fail",
 			Val: platformDecodingParams{
-				filename: "resolution_change_500frames.vp8.ivf",
+				filenames: []string{"resolution_change_500frames.vp8.ivf"},
 				failExpected: func(stdout, stderr []byte) bool {
 					return strings.Contains(string(stderr), "Codec VP80 not supported.")
 				},
@@ -124,31 +293,34 @@ func PlatformDecoding(ctx context.Context, s *testing.State) {
 	}
 	defer upstart.EnsureJobRunning(cleanupCtx, "ui")
 
-	// Run the decode_test binary, propagating its errors: the decode_test binary
-	// fails if the VAAPI calls themselves error, the binary is called on
-	// unsupported inputs or could not open the DRI render node, or the binary
-	// otherwise crashes.
+	// Run the decode_test binary on all files, failing at the first error.
+	// The decode_test binary fails if the VAAPI calls themselves error, the
+	// binary is called on unsupported inputs or could not open the DRI render
+	// node, or the binary otherwise crashes.
 	// The test may also fail to verify the decode results (MD5 comparison).
 	const exec = "decode_test"
-	stdout, stderr, err := testexec.CommandContext(
-		ctx,
-		filepath.Join(chrome.BinTestDir, exec),
-		"--video="+s.DataPath(testOpt.filename),
-		"--md5",
-	).SeparatedOutput(testexec.DumpLogOnError)
+	for _, filename := range testOpt.filenames {
+		testing.ContextLogf(ctx, "Running %s on %s", exec, filename)
+		stdout, stderr, err := testexec.CommandContext(
+			ctx,
+			filepath.Join(chrome.BinTestDir, exec),
+			"--video="+s.DataPath(filename),
+			"--md5",
+		).SeparatedOutput(testexec.DumpLogOnError)
 
-	if err != nil && (testOpt.failExpected == nil || !testOpt.failExpected(stdout, stderr)) {
-		output := append(stdout, stderr...)
-		s.Fatalf("%v failed unexpectedly: %v", exec, errors.Wrap(err, string(output)))
-	}
-	if err == nil && testOpt.failExpected != nil {
-		s.Fatalf("%v passed on %s when expected to fail", exec, testOpt.filename)
-	}
-	if testOpt.failExpected != nil && testOpt.failExpected(stdout, stderr) {
-		return
-	}
+		if err != nil && (testOpt.failExpected == nil || !testOpt.failExpected(stdout, stderr)) {
+			output := append(stdout, stderr...)
+			s.Fatalf("%v failed unexpectedly: %v", exec, errors.Wrap(err, string(output)))
+		}
+		if err == nil && testOpt.failExpected != nil {
+			s.Fatalf("%v passed on %s when expected to fail", exec, filename)
+		}
+		if testOpt.failExpected != nil && testOpt.failExpected(stdout, stderr) {
+			continue
+		}
 
-	if err := verifyContent(s.DataPath(testOpt.filename+".json"), string(stdout)); err != nil {
-		s.Fatalf("%v failed to verify content: %v", exec, errors.Wrap(err, testOpt.filename))
+		if err := verifyContent(s.DataPath(filename+".json"), string(stdout)); err != nil {
+			s.Fatalf("%v failed to verify content: %v", exec, errors.Wrap(err, filename))
+		}
 	}
 }
