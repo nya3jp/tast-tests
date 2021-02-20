f5446284c8f3640cbad3dcd26fecb9ced8bd09b2
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-02-19 18:25:37 -0800

    Update Tast Test for Gmail
    BUG=b:180753739
    TEST=tast run <eve> arcappcompat.Gmail
    
    Gmail is getting failed in launch due to a new dialog box. Added a code to fix it.
    
    Change-Id: I24f4163ad16d61e4a4d3898fdeb1cdb7e1d3c317

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/gmail.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/gmail.go
@@ -20,20 +20,20 @@ import (
 // ClamshellTests are placed here.
 var clamshellTestsForGmail = []testutil.TestCase{
 	{Name: "Launch app in Clamshell", Fn: launchAppForGmail},
-	{Name: "Clamshell: Physical Keyboard", Fn: testutil.TouchAndTextInputs},
-	{Name: "Clamshell : Keyboard Critical Path", Fn: testutil.KeyboardNavigations},
 	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
 	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
 	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
 	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Clamshell: Physical Keyboard", Fn: testutil.TouchAndTextInputs},
+	{Name: "Clamshell: Keyboard Critical Path", Fn: testutil.KeyboardNavigations},
 }
 
 // TouchviewTests are placed here.
 var touchviewTestsForGmail = []testutil.TestCase{
 	{Name: "Launch app in Touchview", Fn: launchAppForGmail},
-	{Name: "Touchview: Virtual Keyboard", Fn: testutil.TouchAndTextInputs},
 	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
 	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
+	{Name: "Touchview: Virtual Keyboard", Fn: testutil.TouchAndTextInputs},
 }
 
 func init() {
@@ -84,8 +84,6 @@ func Gmail(ctx context.Context, s *testing.State) {
 func launchAppForGmail(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
 
 	const (
-		composeIconClassName    = "android.widget.ImageButton"
-		composeIconDescription  = "Compose"
 		textViewClassName       = "android.widget.TextView"
 		gotItButtonText         = "GOT IT"
 		takeMeToGmailButtonText = "TAKE ME TO GMAIL"
@@ -114,9 +112,11 @@ func launchAppForGmail(ctx context.Context, s *testing.State, tconn *chrome.Test
 		s.Fatal("Failed to click on takeMeToGmailButton: ", err)
 	}
 
-	// Check for compose icon in home page.
-	composeIcon := d.Object(ui.ClassName(composeIconClassName), ui.DescriptionContains(composeIconDescription))
-	if err := composeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
-		s.Fatal("Failed to click on composeIcon: ", err)
+	testutil.HandleDialogBoxes(ctx, s, d, appPkgName)
+	// Check for home icon.
+	homeIcon := d.Object(ui.PackageName(appPkgName))
+	if err := homeIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		testutil.DetectAndHandleCloseCrashOrAppNotResponding(ctx, s, d)
+		s.Fatal("homeIcon doesn't exists: ", err)
 	}
 }
