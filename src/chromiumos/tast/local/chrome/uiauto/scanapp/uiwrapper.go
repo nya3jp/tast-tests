971db6d6e51985b539dd3fc4897118a646eb417b
Jesse Schettler <jschettler@chromium.org>
2021-02-20 12:32:37 -0700

    tast-tests: Port scanapp to uiauto library
    Port the scanapp library to the new UI automation library. Update the
    scanapp.Scan test to use the ported scanapp library.
    
    BUG=b:173556456
    TEST=scanapp.Scan
    
    Change-Id: I76dd817b2bfacf0d5764784249f79ba0f4d9a867

---
+++ src/chromiumos/tast/local/chrome/uiauto/scanapp/uiwrapper.go
@@ -0,0 +1,39 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package scanapp
+
+import (
+	"time"
+
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+)
+
+// This file is used for methods on Scan App that just wrap methods on uiauto.Context.
+// Not all methods are necessarily added. Please add them if you need them.
+
+// WithTimeout returns a new ScanApp with the specified timeout.
+// This does not launch the Scan App again.
+func (s *ScanApp) WithTimeout(timeout time.Duration) *ScanApp {
+	return &ScanApp{
+		ui:    s.ui.WithTimeout(timeout),
+		tconn: s.tconn,
+	}
+}
+
+// WaitUntilExists calls ui.WaitUntilExists scoping the finder to the Scan App.
+func (s *ScanApp) WaitUntilExists(finder *nodewith.Finder) uiauto.Action {
+	return s.ui.WaitUntilExists(finder.FinalAncestor(WindowFinder))
+}
+
+// WaitUntilGone calls ui.WaitUntilGone scoping the finder to the Scan App.
+func (s *ScanApp) WaitUntilGone(finder *nodewith.Finder) uiauto.Action {
+	return s.ui.WaitUntilGone(finder.FinalAncestor(WindowFinder))
+}
+
+// LeftClick calls ui.LeftClick scoping the finder to the Scan App.
+func (s *ScanApp) LeftClick(finder *nodewith.Finder) uiauto.Action {
+	return s.ui.LeftClick(finder.FinalAncestor(WindowFinder))
+}
