ad351349bfbf5b4710e1459d07e6a80c193ec9d5
Vincent Lin <vincentlin@cienet.com>
2021-02-20 12:41:31 +0800

    tast-tests: add chameleon board support
    Add chameleon control methods to tast. Initially support a few extended
    display methods.
    
    BUG=chromium:1199988
    TEST=fast_build.sh -T
    
    Change-Id: Iae49db4c814a34d212fd09beaaeb663fd5e55ff4

--- src/chromiumos/tast/remote/servo/methods.go
+++ src/chromiumos/tast/remote/servo/methods.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	"chromiumos/tast/common/xmlrpc"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/testing"
 )
@@ -170,14 +171,14 @@ const ServoKeypressDelay = 100 * time.Millisecond
 // Echo calls the Servo echo method.
 func (s *Servo) Echo(ctx context.Context, message string) (string, error) {
 	var val string
-	err := s.run(ctx, newCall("echo", message), &val)
+	err := s.xmlrpc.Run(ctx, xmlrpc.NewCall("echo", message), &val)
 	return val, err
 }
 
 // PowerNormalPress calls the Servo power_normal_press method.
 func (s *Servo) PowerNormalPress(ctx context.Context) (bool, error) {
 	var val bool
-	err := s.run(ctx, newCall("power_normal_press"), &val)
+	err := s.xmlrpc.Run(ctx, xmlrpc.NewCall("power_normal_press"), &val)
 	return val, err
 }
 
@@ -196,7 +197,7 @@ func (s *Servo) GetServoVersion(ctx context.Context) (string, error) {
 	if s.version != "" {
 		return s.version, nil
 	}
-	err := s.run(ctx, newCall("get_version"), &s.version)
+	err := s.xmlrpc.Run(ctx, xmlrpc.NewCall("get_version"), &s.version)
 	return s.version, err
 }
 
@@ -231,7 +232,7 @@ func (s *Servo) GetServoV4Type(ctx context.Context) (V4TypeValue, error) {
 // GetString returns the value of a specified control.
 func (s *Servo) GetString(ctx context.Context, control StringControl) (string, error) {
 	var value string
-	if err := s.run(ctx, newCall("get", string(control)), &value); err != nil {
+	if err := s.xmlrpc.Run(ctx, xmlrpc.NewCall("get", string(control)), &value); err != nil {
 		return "", errors.Wrapf(err, "getting value for servo control %q", control)
 	}
 	return value, nil
@@ -242,7 +243,7 @@ func (s *Servo) SetString(ctx context.Context, control StringControl, value stri
 	// Servo's Set method returns a bool stating whether the call succeeded or not.
 	// This is redundant, because a failed call will return an error anyway.
 	// So, we can skip unpacking the output.
-	if err := s.run(ctx, newCall("set", string(control), value)); err != nil {
+	if err := s.xmlrpc.Run(ctx, xmlrpc.NewCall("set", string(control), value)); err != nil {
 		return errors.Wrapf(err, "setting servo control %q to %q", control, value)
 	}
 	return nil
@@ -250,7 +251,7 @@ func (s *Servo) SetString(ctx context.Context, control StringControl, value stri
 
 // SetInt sets a Servo control to an integer value.
 func (s *Servo) SetInt(ctx context.Context, control IntControl, value int) error {
-	if err := s.run(ctx, newCall("set", string(control), value)); err != nil {
+	if err := s.xmlrpc.Run(ctx, xmlrpc.NewCall("set", string(control), value)); err != nil {
 		return errors.Wrapf(err, "setting servo control %q to %d", control, value)
 	}
 	return nil
@@ -259,7 +260,7 @@ func (s *Servo) SetInt(ctx context.Context, control IntControl, value int) error
 // GetFloat returns the floating-point value of a specified control.
 func (s *Servo) GetFloat(ctx context.Context, control FloatControl) (float64, error) {
 	var value float64
-	if err := s.run(ctx, newCall("get", string(control)), &value); err != nil {
+	if err := s.xmlrpc.Run(ctx, xmlrpc.NewCall("get", string(control)), &value); err != nil {
 		return 0, errors.Wrapf(err, "getting value for servo control %q", control)
 	}
 	return value, nil
