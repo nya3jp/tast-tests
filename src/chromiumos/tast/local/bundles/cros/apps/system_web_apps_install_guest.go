1b00de0cf35e213e06be78ab54389fac82afa1f2
Ben Reich <benreich@chromium.org>
2021-02-21 21:17:28 +0000

    tast-tests: Add apps.SystemWebAppsInstallGuest tests
    Verify that System Web Apps (SWA) get installed correctly when a guest
    login is started by listing the installed SWAs and comparing to the
    number of internal names from the SWA manager.
    
    This tests both the enabled apps are installed and (by passing the
    EnabledAllSystemWebApps flag) that all SWAs are installed correctly. By
    correctly, ensuring Chrome doesn't crash is a sufficient signal.
    
    BUG=chromium:1122877
    TEST=tast run <DUT-IP> apps.SystemWebAppsInstallGuest*
    
    Change-Id: Ia6eb8803ff74b91c8a5ff344e6aeb29a127a3b3e

---
+++ src/chromiumos/tast/local/bundles/cros/apps/system_web_apps_install_guest.go
@@ -0,0 +1,84 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package apps
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SystemWebAppsInstallGuest,
+		Desc: "Checks that system web apps are installed in guest mode",
+		Contacts: []string{
+			"benreich@chromium.org",
+			"chrome-apps-platform-rationalization@google.com",
+		},
+		Timeout:      3 * time.Minute,
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Name: "all_apps",
+			Val:  []chrome.Option{chrome.EnableFeatures("EnableAllSystemWebApps")},
+		}, {
+			Name: "default_enabled_apps",
+			Val:  []chrome.Option{},
+		}},
+	})
+}
+
+// SystemWebAppsInstallGuest tests that system web apps are installed on a guest profile.
+func SystemWebAppsInstallGuest(ctx context.Context, s *testing.State) {
+	chromeOpts := s.Param().([]chrome.Option)
+	chromeOpts = append(chromeOpts, chrome.GuestLogin())
+
+	cr, err := chrome.New(ctx, chromeOpts...)
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	installedSystemWebApps, err := apps.ListSystemWebApps(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get installed system apps: ", err)
+	}
+
+	registeredInternalNames, err := apps.ListSystemWebAppsInternalNames(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get registered system apps: ", err)
+	}
+
+	for i, internalName := range registeredInternalNames {
+		// Terminal SWA is always installed but it is disabled (controlled by Crostini App Service publisher) in guest mode.
+		// Exclude it from the list as it does not show in installedSystemWebApps.
+		// https://source.chromium.org/chromium/chromium/src/+/HEAD:chrome/browser/apps/app_service/crostini_apps.cc;l=240
+		if internalName == "Terminal" {
+			registeredInternalNames = append(registeredInternalNames[:i], registeredInternalNames[i+1:]...)
+			break
+		}
+	}
+
+	if (len(registeredInternalNames) - terminal) != len(installedSystemWebApps) {
+		var installedNames []string
+		for _, app := range installedSystemWebApps {
+			installedNames = append(installedNames, app.Name)
+		}
+
+		s.Logf("Registered apps: %s", strings.Join(registeredInternalNames, ", "))
+		s.Logf("Installed apps: %s", strings.Join(installedNames, ", "))
+		s.Fatalf("Unexpected number of installed apps: want %d; got %d", len(registeredInternalNames), len(installedNames))
+	}
+}
