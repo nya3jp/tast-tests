f641e7f2a4c301927cae7804c1eec7627c8292ba
Youkichi Hosoi <youkichihosoi@google.com>
2021-02-22 17:22:53 +0900

    tast-tests: Add arc.MyFiles test
    The test checks whether (1) a file pushed to Android MyFiles directory
    appears in the Chrome OS MyFiles directory, and (2) a file put inside
    Chrome OS MyFiles directory can be read by Android apps.
    
    BUG=b:179656745
    TEST=tast run <betty/betty-arc-r> arc.MyFiles*
    
    Change-Id: I5faa47c5f79c57edde122a08d6d7436aafcbd999

--- src/chromiumos/tast/local/bundles/cros/arc/storage/doc.go
+++ src/chromiumos/tast/local/bundles/cros/arc/storage/doc.go
@@ -2,5 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package storage contains helper functions to test different FilesApp storage folders, e.g. Drive FS, Downloads
+// Package storage contains helper functions to test different FilesApp storage
+// folders that are shared from Chrome OS to ARC (e.g. Drive FS, Downloads).
+// They can be used to check whether a file put in a Chrome OS folder can be
+// read by Android apps, or to check whether a file pushed to an Android folder
+// appears in the corresponding Chrome OS folder.
 package storage
