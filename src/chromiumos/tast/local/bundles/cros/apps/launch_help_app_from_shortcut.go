6ed1e424aa74209c14e28114d67e23af07f8018d
Ben Reich <benreich@chromium.org>
2021-02-23 05:00:33 +0000

    tast-tests: Add apps.LaunchHelpAppFromShortcut
    Adds a test to exercise the path to open the Explore app using the
    Ctrl+Shift+/. There was also a P1 bug (chromium:1122480) for this
    path that happened in guest mode, so ensure we also verify this
    combination as well.
    
    BUG=chromium:1122480
    TEST=tast run <DUT-IP> apps.LaunchHelpAppFromShortcut*
    
    Change-Id: Ia973a4881c989a3efee93131845e62cc871f76ec

---
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_help_app_from_shortcut.go
@@ -0,0 +1,99 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package apps
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/bundles/cros/apps/helpapp"
+	"chromiumos/tast/local/bundles/cros/apps/pre"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LaunchHelpAppFromShortcut,
+		Desc: "Help app can be launched using shortcut Ctrl+Shift+/",
+		Contacts: []string{
+			"showoff-eng@google.com",
+			"benreich@chromium.org",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{
+			{
+				Name:              "stable",
+				Fixture:           "chromeLoggedIn",
+				ExtraHardwareDeps: pre.AppsStableModels,
+			}, {
+				Name:              "unstable",
+				Fixture:           "chromeLoggedIn",
+				ExtraHardwareDeps: pre.AppsUnstableModels,
+			},
+			{
+				Name:              "stable_guest",
+				Fixture:           "chromeLoggedInGuest",
+				ExtraHardwareDeps: pre.AppsStableModels,
+			}, {
+				Name:              "unstable_guest",
+				Fixture:           "chromeLoggedInGuest",
+				ExtraHardwareDeps: pre.AppsUnstableModels,
+			},
+		},
+	})
+}
+
+// LaunchHelpAppFromShortcut verifies launching Help app from Ctrl+Shift+/.
+func LaunchHelpAppFromShortcut(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	kw, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard handle: ", err)
+	}
+	defer kw.Close()
+
+	// On some low-end devices and guest mode sometimes Chrome is still
+	// initializing when the shortcut keys are emitted. Check that the
+	// app is showing up as installed before emitting the shortcut keys.
+	if err := ash.WaitForChromeAppInstalled(ctx, tconn, apps.Help.ID, 30*time.Second); err != nil {
+		s.Fatal("Failed to wait for Explore to be installed: ", err)
+	}
+
+	shortcuts := []string{"Ctrl+Shift+/", "Ctrl+/"}
+	for _, shortcut := range shortcuts {
+		if err := kw.Accel(ctx, shortcut); err != nil {
+			s.Errorf("Failed to press %q keys: %v", shortcut, err)
+		}
+
+		if err := helpapp.WaitForApp(ctx, tconn); err != nil {
+			s.Errorf("Failed to launch or render Help app by shortcut %q: %v", shortcut, err)
+		}
+
+		// Close the Help app, this may error if the app failed to
+		// open and we only use it to reset for the next shortcut.
+		// Simply log the error out instead of failing.
+		if err := apps.Close(ctx, tconn, apps.Help.ID); err != nil {
+			s.Log("Failed to close the app, may not have been opened: ", err)
+		}
+	}
+}
