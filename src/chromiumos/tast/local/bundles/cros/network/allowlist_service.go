42f02b78cc87d8b134a85fb53b8f608b6a13c89b
Andreea Costinas <acostinas@google.com>
2021-02-24 19:11:13 +0100

    allowlist: Implement TestArcConnectivity
    Add a test to check that the PlayStore is working as expected behind
    a firewall by logging in with a real managed user and checking that
    force installed apps are successfully installed.
    
    go/tast-for-allowlist
    
    BUG=b:181110031
    TEST=tast run $DUT allowlist.ArcConnectivity
    
    Change-Id: Ic95694986e9ccf01086a06d62165fdac3835acdf

--- src/chromiumos/tast/local/bundles/cros/network/allowlist_service.go
+++ src/chromiumos/tast/local/bundles/cros/network/allowlist_service.go
@@ -7,15 +7,19 @@ package network
 import (
 	"context"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/services/cros/network"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/timing"
 )
 
 const (
@@ -84,9 +88,43 @@ func (a *AllowlistService) GaiaLogin(ctx context.Context, req *network.GaiaLogin
 	return &empty.Empty{}, nil
 }
 
+// CheckArcAppInstalled verifies that ARC provisioning and installing ARC apps by checking that force installed apps are successfully installed.
 func (a *AllowlistService) CheckArcAppInstalled(ctx context.Context, req *network.CheckArcAppInstalledRequest) (*empty.Empty, error) {
-	return nil, errors.New("method CheckArcAppInstalled not implemented")
+	if a.cr == nil {
+		return nil, errors.New("Please start a new Chrome instance that uses the firewall by calling GaiaLogin()")
+	}
 
+	td, _ := testing.ContextOutDir(ctx)
+	arc, err := arc.New(ctx, td)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start ARC")
+	}
+	defer arc.Close()
+
+	// Ensure that Android packages are force-installed by ARC policy.
+	ctx, st := timing.Start(ctx, "wait_packages")
+	defer st.End()
+
+	testing.ContextLog(ctx, "Waiting for packages being installed")
+	if err = testing.Poll(ctx, func(ctx context.Context) error {
+		// List installed 3rd patry packages
+		cmd := arc.Command(ctx, "pm", "list", "packages", "-3")
+		out, err := cmd.Output()
+		if err != nil {
+			cmd.DumpLog(ctx)
+			return errors.Wrap(err, "pm list -3 failed")
+		}
+		pkgs := string(out)
+		if strings.Contains(pkgs, req.AppName) {
+			testing.ContextLogf(ctx, "Found packages %s", req.AppName)
+			return nil
+		}
+		return errors.New("failed to install 3rd party")
+	}, &testing.PollOptions{Interval: 1 * time.Second, Timeout: 180 * time.Second}); err != nil {
+		return nil, err
+	}
+
+	return &empty.Empty{}, nil
 }
 
 func (a *AllowlistService) CheckExtensionInstalled(ctx context.Context, req *network.CheckExtensionInstalledRequest) (*empty.Empty, error) {
