4905f24713e68483653a8a1c5eb4928b418302ce
Yi Chou <yich@google.com>
2021-02-24 19:57:16 +0800

    hwsec: combine the TPM reset in remote & local tast
    WIP
    
    BUG=None
    TEST=None
    
    Change-Id: I17e861f9eebacf7ec501b5d27d741981bb66fc81

---
+++ src/chromiumos/tast/remote/hwsec/tpm_clearer.go
@@ -0,0 +1,55 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+/*
+This file implements the TPM clear tool in remote tast.
+*/
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// TPMClearer clear the TPM via crossystem, this would work on both TPM1.2 and TPM2.0.
+type TPMClearer struct {
+	cmdRunner        hwsec.CmdRunner
+	daemonController *hwsec.DaemonController
+	dut              *dut.DUT
+}
+
+// NewTPMClearer creates a new TPMClearer object, where r is used to run the command internally.
+func NewTPMClearer(cmdRunner hwsec.CmdRunner, daemonController *hwsec.DaemonController, dut *dut.DUT) *TPMClearer {
+	return &TPMClearer{cmdRunner, daemonController, dut}
+}
+
+// ClearTPM would clear the TPM, reboot and ensure every TPM daemon is up.
+func (tc *TPMClearer) ClearTPM(ctx context.Context) error {
+	// Copy logs before TPM reset. Ignore errors on failure.
+	if outDir, ok := testing.ContextOutDir(ctx); ok {
+		dateString := time.Now().Format(time.RFC3339)
+		if err := tc.dut.GetFile(ctx, "/var/log/chrome/", filepath.Join(outDir, "chrome-"+dateString)); err != nil {
+			testing.ContextLog(ctx, "Failed to copy chrome logs: ", err)
+		}
+	}
+
+	// File clear TPM owner request to crossystem.
+	if _, err := tc.cmdRunner.Run(ctx, "crossystem", "clear_tpm_owner_request=1"); err != nil {
+		return errors.Wrap(err, "failed to file clear_tpm_owner_request")
+	}
+
+	// Reboot every the DUT.
+	if err := tc.dut.Reboot(ctx); err != nil {
+		return errors.Wrap(err, "failed to reboot")
+	}
+
+	return nil
+}
