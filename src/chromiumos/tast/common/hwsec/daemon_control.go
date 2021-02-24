7efcbf1e8e3829c54fc3b1b519cb36d9ddfbfc8f
Yi Chou <yich@google.com>
2021-02-24 19:57:16 +0800

    hwsec: add more function in daemon control
    WIP
    
    Change-Id: I17e861f9eebacf7ec501b5d27d741981bb66fc81

--- src/chromiumos/tast/common/hwsec/daemon_control.go
+++ src/chromiumos/tast/common/hwsec/daemon_control.go
@@ -10,19 +10,76 @@ It is meant to have our own implementation so we can support the control in
 both local and local test; also, we also wait for D-Bus interfaces to be responsive
 instead of only (re)starting them.
 
-Note that the design of this file is TBD and the current implementation is a temp
-solution; once more use cases in the test scripts these functions should be moved
-into the helper class properly.
+And we referenced this code:
+src/platform/tast-tests/src/chromiumos/tast/local/upstart/upstart.go
 */
 
 import (
 	"context"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/testing"
 )
+
+// DaemonGoal describes a job's goal. See Section 10.1.6.19, "initctl status", in the Upstart Cookbook.
+type DaemonGoal string
+
+// DaemonState describes a job's current state. See Section 4.1.2, "Job States", in the Upstart Cookbook.
+type DaemonState string
+
+const (
+	// startGoal indicates that a task or service job has been started.
+	startGoal DaemonGoal = "start"
+	// stopGoal indicates that a task job has completed or that a service job has been manually stopped or has
+	// a "stop on" condition that has been satisfied.
+	stopGoal DaemonGoal = "stop"
+
+	// waitingState is the initial state for a job.
+	waitingState DaemonState = "waiting"
+	// startingState indicates that a job is about to start.
+	startingState DaemonState = "starting"
+	// securityState indicates that a job is having its AppArmor security policy loaded.
+	securityState DaemonState = "security"
+	// preStartState indicates that a job's pre-start section is running.
+	preStartState DaemonState = "pre-start"
+	// spawnedState indicates that a job's script or exec section is about to run.
+	spawnedState DaemonState = "spawned"
+	// postStartState indicates that a job's post-start section is running.
+	postStartState DaemonState = "post-start"
+	// runningState indicates that a job is running (i.e. its post-start section has completed). It may not have a PID yet.
+	runningState DaemonState = "running"
+	// preStopState indicates that a job's pre-stop section is running.
+	preStopState DaemonState = "pre-stop"
+	// stoppingState indicates that a job's pre-stop section has completed.
+	stoppingState DaemonState = "stopping"
+	// killedState indicates that a job is about to be stopped.
+	killedState DaemonState = "killed"
+	// postStopState indicates that a job's post-stop section is running.
+	postStopState DaemonState = "post-stop"
+)
+
+var allGoals = map[DaemonGoal]struct{}{
+	startGoal: {},
+	stopGoal:  {},
+}
+
+var allStates = map[DaemonState]struct{}{
+	waitingState:   {},
+	startingState:  {},
+	securityState:  {},
+	preStartState:  {},
+	spawnedState:   {},
+	postStartState: {},
+	runningState:   {},
+	preStopState:   {},
+	stoppingState:  {},
+	killedState:    {},
+	postStopState:  {},
+}
 
 // DaemonInfo represents the information for a daemon
 type DaemonInfo struct {
@@ -86,6 +143,29 @@ var FakePCAAgentDaemonInfo = &DaemonInfo{
 	DaemonName: "fake_pca_agentd",
 	HasDBus:    true,
 	DBusName:   "org.chromium.PcaAgent",
+}
+
+// ChapsDaemonInfo represents the DaemonsInfo for chaps.
+var ChapsDaemonInfo = &DaemonInfo{
+	Name:       "chaps",
+	DaemonName: "chapsd",
+	HasDBus:    true,
+	DBusName:   "org.chromium.Chaps",
+}
+
+// BootLockboxDaemonInfo represents the DaemonsInfo for bootlockbox.
+var BootLockboxDaemonInfo = &DaemonInfo{
+	Name:       "bootlockbox",
+	DaemonName: "bootlockboxd",
+	HasDBus:    true,
+	DBusName:   "org.chromium.BootLockbox",
+}
+
+// U2fdDaemonInfo represents the DaemonsInfo for u2fd.
+var U2fdDaemonInfo = &DaemonInfo{
+	Name:       "u2fd",
+	DaemonName: "u2fd",
+	HasDBus:    false,
 }
 
 // UIDaemonInfo represents the DaemonsInfo for ui.
@@ -161,5 +241,66 @@ func (dc *DaemonController) Restart(ctx context.Context, info *DaemonInfo) error
 	if info.HasDBus {
 		return dc.waitForDBusService(ctx, info)
 	}
+	return nil
+}
+
+// parseStatus parses the output from "initctl status <job>", e.g. "ui start/running, process 28515".
+// The output may be multiple lines; see the example in Section 10.1.6.19.3,
+// "Single Job Instance Running with Multiple PIDs", in the Upstart Cookbook.
+func (dc *DaemonController) parseStatus(job, out string) (goal DaemonGoal, state DaemonState, pid int, err error) {
+	if !strings.HasPrefix(out, job+" ") {
+		return goal, state, pid, errors.Errorf("missing job prefix %q in %q", job, out)
+	}
+	// Matches a leading line of e.g. "ui start/running, process 3182" or "boot-splash stop/waiting".
+	statusRegexp := regexp.MustCompile(`(?m)^[^ ]+ ([-a-z]+)/([-a-z]+)(?:, process (\d+))?$`)
+	m := statusRegexp.FindStringSubmatch(out)
+	if m == nil {
+		return goal, state, pid, errors.Errorf("unexpected format in %q", out)
+	}
+
+	goal = DaemonGoal(m[1])
+	if _, ok := allGoals[goal]; !ok {
+		return goal, state, pid, errors.Errorf("invalid goal %q", m[1])
+	}
+
+	state = DaemonState(m[2])
+	if _, ok := allStates[state]; !ok {
+		return goal, state, pid, errors.Errorf("invalid state %q", m[2])
+	}
+
+	if m[3] != "" {
+		p, err := strconv.ParseInt(m[3], 10, 32)
+		if err != nil {
+			return goal, state, pid, errors.Errorf("bad PID %q", m[3])
+		}
+		pid = int(p)
+	}
+
+	return goal, state, pid, nil
+}
+
+// Ensure ensures a daemon is started and waits until the D-Bus interface is responsive if it has D-Bus interface.
+func (dc *DaemonController) Ensure(ctx context.Context, info *DaemonInfo) error {
+	out, err := dc.r.Run(ctx, "status", info.DaemonName)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get the status of %s", info.Name)
+	}
+	goal, _, _, err := dc.parseStatus(info.DaemonName, string(out))
+	if goal == stopGoal {
+		if _, err := dc.r.Run(ctx, "start", info.DaemonName); err != nil {
+			return errors.Wrapf(err, "failed to start %s", info.Name)
+		}
+	}
+	if info.HasDBus {
+		return dc.waitForDBusService(ctx, info)
+	}
+	return nil
+}
+
+func (dc *DaemonController) StopAllServices(ctx context.Context) error {
+	return nil
+}
+
+func (dc *DaemonController) EnsureAllServices(ctx context.Context) error {
 	return nil
 }
