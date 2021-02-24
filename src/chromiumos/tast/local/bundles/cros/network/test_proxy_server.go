b8464b89514c3b56e37cb44c9906a3ec4d7d7879
Andreea Costinas <acostinas@google.com>
2021-02-24 14:43:02 +0100

    tast-test: Allow tinyproxy to setup a filter
    This CL allows the tast proxy server to setup a filter via the
    tinyproxy config option `Filter`.
    
    This filter will be used in follow-up tests to test the Chrome OS
    allowlist from the official Google support page.
    
    Since tinyproxy will be used for "real" use cases, this CL also tweaks
    some of the config options to improve performance.
    
    Also (unrelated), it updates the documentation on the auto-generated
    file proxy_service.pb.go.
    
    BUG=b:181110031
    TEST=tast run $DUT network.TestProxyServer
    
    Change-Id: I77301379013294f2d7cd9f58912ef64cb31a6e3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2718174
    Tested-by: Andreea-Elena Costinas <acostinas@google.com>
    Commit-Queue: Andreea-Elena Costinas <acostinas@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/network/test_proxy_server.go
+++ src/chromiumos/tast/local/bundles/cros/network/test_proxy_server.go
@@ -23,8 +23,12 @@ func init() {
 }
 
 var (
-	authRequiredHeader = []byte("HTTP/1.0 407 Proxy Authentication Required")
-	pageMovedHeader    = []byte("HTTP/1.0 301 Moved Permanently")
+	authRequiredHeader   = []byte("HTTP/1.0 407 Proxy Authentication Required")
+	pageMovedHeader      = []byte("HTTP/1.0 301 Moved Permanently")
+	requestBlockedHeader = []byte("HTTP/1.0 403 Filtered")
+
+	allowedHost = "google.com"
+	blockedHost = "ssl.gstatic.com"
 )
 
 // TestProxyServer verifies that proxy.Server with authentication is working as expected.
@@ -32,13 +36,13 @@ func TestProxyServer(ctx context.Context, s *testing.State) {
 	Server := proxy.NewServer()
 
 	cred := &proxy.AuthCredentials{Username: "user", Password: "test0000"}
-	err := Server.Start(ctx, 3128, cred)
+	err := Server.Start(ctx, 3128, cred, []string{allowedHost})
 	if err != nil {
 		s.Fatal("Failed to setup server: ", err)
 	}
 
 	// curl request to the local proxy server without auth.
-	out, err := testexec.CommandContext(ctx, "curl", "-I", "-x", "http://"+Server.HostAndPort, "google.com").Output()
+	out, err := testexec.CommandContext(ctx, "curl", "-I", "-x", "http://"+Server.HostAndPort, allowedHost).Output()
 	if err != nil {
 		s.Error("Curl command without auth failed: ", err)
 	} else if !bytes.Contains(out, authRequiredHeader) {
@@ -46,11 +50,19 @@ func TestProxyServer(ctx context.Context, s *testing.State) {
 	}
 
 	// curl request to the local proxy server with credentials.
-	out, err = testexec.CommandContext(ctx, "curl", "-I", "-x", "http://user:test0000@"+Server.HostAndPort, "google.com").Output()
+	out, err = testexec.CommandContext(ctx, "curl", "-I", "-x", "http://user:test0000@"+Server.HostAndPort, allowedHost).Output()
 	if err != nil {
 		s.Error("Curl command with auth failed: ", err)
 	} else if !bytes.Contains(out, pageMovedHeader) {
 		s.Errorf("Unexpected curl result with auth: got %s; want %s", out, pageMovedHeader)
+	}
+
+	// curl request to the local proxy server with credentials but blocked hostname.
+	out, err = testexec.CommandContext(ctx, "curl", "-I", "-x", "http://user:test0000@"+Server.HostAndPort, blockedHost).Output()
+	if err != nil {
+		s.Error("Curl command with auth failed with blocked page: ", err)
+	} else if !bytes.Contains(out, requestBlockedHeader) {
+		s.Errorf("Unexpected curl result with auth: got %s; want %s", out, requestBlockedHeader)
 	}
 	Server.Stop(ctx)
 }
