4fda74f675725bddbaa27e2c9c7ebf93e9cfb24d
Jesse Schettler <jschettler@chromium.org>
2021-02-23 22:11:35 -0700

    tast-tests: Click done button instead of closing Scan app
    Now that the UI nodes are working correctly when clicking the done
    button, click the done button after a scan instead of closing the Scan
    app.
    
    BUG=b:173556456
    TEST=scanapp.Scan
    
    Change-Id: If0f0523350cb8e5206cac52361d89f1be9dd933d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2716854
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Commit-Queue: Brendan Hansknecht <bhansknecht@chromium.org>
    Tested-by: Jesse Schettler <jschettler@chromium.org>
    Auto-Submit: Jesse Schettler <jschettler@chromium.org>
    Reviewed-by: Brendan Hansknecht <bhansknecht@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/scanapp/scan.go
+++ src/chromiumos/tast/local/bundles/cros/scanapp/scan.go
@@ -177,6 +177,16 @@ func Scan(ctx context.Context, s *testing.State) {
 	}()
 	defer ippusbbridge.Kill(cleanupCtx, devInfo)
 
+	// Launch the Scan app, configure the settings, and perform scans.
+	app, err := scanapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch app: ", err)
+	}
+
+	if err := app.ClickMoreSettings()(ctx); err != nil {
+		s.Fatal("Failed to expand More settings: ", err)
+	}
+
 	for _, test := range tests {
 		s.Run(ctx, test.name, func(ctx context.Context, s *testing.State) {
 			defer func() {
@@ -185,28 +195,12 @@ func Scan(ctx context.Context, s *testing.State) {
 				}
 			}()
 
-			// Launch the Scan app, configure the settings, and perform a scan.
-			app, err := scanapp.Launch(ctx, tconn)
-			if err != nil {
-				s.Fatal("Failed to launch app: ", err)
-			}
-
-			if err := app.ClickMoreSettings()(ctx); err != nil {
-				s.Fatal("Failed to expand More settings: ", err)
-			}
-
 			if err := uiauto.Combine("scan",
 				app.SetScanSettings(test.settings),
 				app.Scan(),
+				app.ClickDone(),
 			)(ctx); err != nil {
 				s.Fatal("Failed to perform scan: ", err)
-			}
-
-			// TODO(jschettler): Click the done button instead of closing the
-			// app. Closing the app is a workaround for an issue where some
-			// nodes don't exist after leaving the done page.
-			if err := app.Close(ctx); err != nil {
-				s.Fatal("Failed to close app: ", err)
 			}
 
 			scan, err := getScan(defaultScanPattern)
