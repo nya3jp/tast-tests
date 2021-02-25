d8abc8e3e322f7360b3e570c060b57f6e8d4e5f9
Yi Chou <yich@google.com>
2021-02-25 11:28:07 +0800

    tast-tests: rename Poserwash to SystemStateReset
    EnsureTPMIsResetAndPowerwash -> EnsureTPMAndSystemStateAreReset
    
    BUG=b:181183725
    TEST=tast run $DUT hwsec.*
    
    Change-Id: I9d349e592883aa1246fefc6fb0540e53b7aad281

--- src/chromiumos/tast/remote/bundles/cros/wilco/api_perform_web_request.go
+++ src/chromiumos/tast/remote/bundles/cros/wilco/api_perform_web_request.go
@@ -40,7 +40,7 @@ func init() {
 
 func APIPerformWebRequest(ctx context.Context, s *testing.State) {
 	defer func(ctx context.Context) {
-		if err := policyutil.EnsureTPMIsResetAndPowerwash(ctx, s.DUT()); err != nil {
+		if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
 			s.Error("Failed to reset TPM: ", err)
 		}
 	}(ctx)
@@ -48,7 +48,7 @@ func APIPerformWebRequest(ctx context.Context, s *testing.State) {
 	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Minute)
 	defer cancel()
 
-	if err := policyutil.EnsureTPMIsResetAndPowerwash(ctx, s.DUT()); err != nil {
+	if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
 		s.Fatal("Failed to reset TPM: ", err)
 	}
 
