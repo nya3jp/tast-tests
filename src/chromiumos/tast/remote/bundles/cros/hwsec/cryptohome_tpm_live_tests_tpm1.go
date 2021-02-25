18ff170212e6e8bd568794e01ef8194510710861
Yi Chou <yich@google.com>
2021-02-25 11:28:07 +0800

    tast-tests: rename Poserwash to SystemStateReset
    EnsureTPMIsResetAndPowerwash -> EnsureTPMAndSystemStateAreReset
    
    Because "Powerwash" isn't accurate, rename it to SystemStateReset.
    
    BUG=b:181183725
    TEST=tast run $DUT hwsec.*
    TEST=CQ
    
    Change-Id: I9d349e592883aa1246fefc6fb0540e53b7aad281

--- src/chromiumos/tast/remote/bundles/cros/hwsec/cryptohome_tpm_live_tests_tpm1.go
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/cryptohome_tpm_live_tests_tpm1.go
@@ -41,7 +41,7 @@ func CryptohomeTPMLiveTestsTPM1(ctx context.Context, s *testing.State) {
 	tpmManager := helper.TPMManagerClient()
 
 	s.Log("Start resetting TPM if needed")
-	if err := helper.EnsureTPMIsResetAndPowerwash(ctx); err != nil {
+	if err := helper.EnsureTPMAndSystemStateAreReset(ctx); err != nil {
 		s.Fatal("Failed to ensure resetting TPM: ", err)
 	}
 	s.Log("TPM is confirmed to be reset")
@@ -51,7 +51,7 @@ func CryptohomeTPMLiveTestsTPM1(ctx context.Context, s *testing.State) {
 	defer cancel()
 	defer func(ctx context.Context) {
 		// Clean the TPM up, so that the TPM state clobbered by the TPM live tests doesn't affect subsequent tests.
-		if err := helper.EnsureTPMIsResetAndPowerwash(ctx); err != nil {
+		if err := helper.EnsureTPMAndSystemStateAreReset(ctx); err != nil {
 			s.Fatal("Failed to ensure resetting TPM: ", err)
 		}
 	}(ctxForResetTPM)
