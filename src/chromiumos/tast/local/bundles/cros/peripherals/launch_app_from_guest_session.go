e1f4aba4165df9ed219f84596addfb8ec576ea99
Joon Ahn <joonbug@chromium.org>
2021-02-25 15:08:13 -0800

    peripherals: launch app from guest mode
    BUG=chromium:1125150
    TEST=tast run ${DUT} peripherals.LaunchAppFromGuestSession.diagnostics
    
    Change-Id: I7b8e59aa6f44ea61dbdd3c9472f2000383610ec8

---
+++ src/chromiumos/tast/local/bundles/cros/peripherals/launch_app_from_guest_session.go
@@ -0,0 +1,66 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package peripherals
+
+import (
+	"context"
+
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/testing"
+)
+
+// guestModeTest contains all the data needed to run a single test iteration.
+type guestModeTest struct {
+	app apps.App
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LaunchAppFromGuestSession,
+		Desc: "Peripherals app can be found and launched from guest mode",
+		Contacts: []string{
+			"joonbug@chromium.org",
+			"cros-peripherals@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedInGuest",
+		Params: []testing.Param{
+			{
+				Name: "diagnostics",
+				Val: guestModeTest{
+					app: apps.Diagnostics,
+				},
+			},
+		},
+	})
+}
+
+// LaunchAppFromGuestSession verifies launching an app from guest mode.
+func LaunchAppFromGuestSession(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	// Attempt to open the Test API connection.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API Connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	params := s.Param().(guestModeTest)
+
+	err = apps.Launch(ctx, tconn, params.app.ID)
+	if err != nil {
+		s.Fatal("Failed to launch app: ", err)
+	}
+
+	err = ash.WaitForApp(ctx, tconn, params.app.ID)
+	if err != nil {
+		s.Fatal("Could not find app in shelf after launch: ", err)
+	}
+}
