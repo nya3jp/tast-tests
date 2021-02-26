c0e7ed363ffc70924bfe79d0e7d75dd75c8d0613
Cherie Cheung <cherieccy@google.com>
2021-02-27 00:53:50 +0900

    tast-tests: FSP tast test
    arc.Fsp / arc.Fsp.vm tast tests verify that Android app in Chrome OS ARC++/ARCVM can
    read files on FSP file system via FilesApp.
    
    BUG=b:179736612
    TEST=tast run <DUT> arc.Fsp
         tast run <DUT> arc.Fsp.vm
    
    Cq-Depend: chrome-internal:3647043
    Change-Id: I5a0b68cceb40a51152acb4b49f02c3fe41c54e27

--- src/chromiumos/tast/local/chrome/uiauto/crd/crd.go
+++ src/chromiumos/tast/local/chrome/uiauto/crd/crd.go
@@ -12,65 +12,19 @@ import (
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/cws"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/testing"
 )
 
 const crdURL = "https://remotedesktop.google.com/support"
+const appCWSURL = "https://chrome.google.com/webstore/detail/chrome-remote-desktop/inomeogfingihgjfjlpeplalcfajhgai?hl=en"
 
 // Do not poll things too fast to prevent triggering weird UI behaviors. The
 // share button will not work if we keep clicking it. Sets timeout to 5 minutes
 // according to timeout for CRD one time access code.
 var rdpPollOpts = &testing.PollOptions{Interval: time.Second, Timeout: 5 * time.Minute}
-
-// ensureAppInstalled ensures the companion extension for the Chrome Remote
-// Desktop website https://remotedesktop.google.com is installed.
-func ensureAppInstalled(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn) error {
-	const appCWSURL = "https://chrome.google.com/webstore/detail/chrome-remote-desktop/inomeogfingihgjfjlpeplalcfajhgai?hl=en"
-
-	cws, err := cr.NewConn(ctx, appCWSURL)
-	if err != nil {
-		return err
-	}
-	defer cws.Close()
-	defer cws.CloseTarget(ctx)
-
-	ui := uiauto.New(tconn)
-	remove := nodewith.Name("Remove from Chrome").Role(role.Button).First()
-	add := nodewith.Name("Add to Chrome").Role(role.Button).First()
-	confirm := nodewith.Name("Add extension").Role(role.Button)
-
-	// Click the add button at most once to prevent triggering
-	// weird UI behaviors in Chrome Web Store.
-	addClicked := false
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		// Check if Remote Desktop is installed.
-		if err := ui.Exists(remove)(ctx); err == nil {
-			return nil
-		}
-
-		if !addClicked {
-			// If Remote Desktop is not installed, install it now.
-			// Click on the add button, if it exists.
-			if err := ui.Exists(add)(ctx); err == nil {
-				if err := ui.LeftClick(add)(ctx); err != nil {
-					return testing.PollBreak(err)
-				}
-				addClicked = true
-			}
-		}
-
-		// Click on the confirm button, if it exists.
-		if err := ui.IfSuccessThen(ui.Exists(confirm), ui.LeftClick(confirm))(ctx); err != nil {
-			return testing.PollBreak(err)
-		}
-		return errors.New("Remote Desktop still installing")
-	}, rdpPollOpts); err != nil {
-		return errors.Wrap(err, "failed to install Remote Desktop")
-	}
-	return nil
-}
 
 func launch(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn) (*chrome.Conn, error) {
 	// Use english version to avoid i18n differences of HTML element attributes.
@@ -113,7 +67,11 @@ func getAccessCode(ctx context.Context, crd *chrome.Conn) (string, error) {
 
 // Launch prepares Chrome Remote Desktop and generates access code to be connected by.
 func Launch(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn) error {
-	if err := ensureAppInstalled(ctx, cr, tconn); err != nil {
+	// Ensures the companion extension for the Chrome Remote Desktop website
+	// https://remotedesktop.google.com is installed.
+	app := cws.App{Name: "Remote Desktop", URL: appCWSURL, InstalledTxt: "Remove from Chrome",
+		AddTxt: "Add to Chrome", ConfirmTxt: "Add extension"}
+	if err := cws.InstallApp(ctx, cr, tconn, app); err != nil {
 		return errors.Wrap(err, "failed to install CRD app")
 	}
 
