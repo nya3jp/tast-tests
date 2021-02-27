aeeb7a63dad9abd1d8caac18f720280aad07d433
Jun Mukai <mukai@google.com>
2021-02-26 18:11:00 -0800

    Split unstable boards for UI performance tests for CQ
    ui.DesktopControl and ui.TabletOperations are sometimes flaky
    on certain boards, but the goal of those tests are to detect the
    significant regressions and provides failures in CQ and chromium
    postsubmit bots. For now, the boards used in CQ are mostly
    stable, so I think it's reasonable to group the 'unstable'
    boards explicitly, and mark them as 'unstable'.
    
    For ui.TabletOperations, it's now stable, so this CL also
    promotes it to critical. for ui.DesktopControl, a failure
    is seen on kukui/krane. So this is not promoted yet.
    
    Tastboard links:
    https://tastboard.corp.google.com/test/ui.DesktopControl
    https://tastboard.corp.google.com/test/ui.TabletOperations
    
    BUG=chromium:1173315
    TEST=none
    
    Change-Id: I159c7e0bdca23629e9a21bbef660131aa058ac87

---
+++ src/chromiumos/tast/local/bundles/cros/ui/perfutil/constants.go
@@ -0,0 +1,15 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package perfutil
+
+// UnstableBoards is the list of the boards which appear to be not stable on
+// the UI performance tests.
+var UnstableBoards = []string{
+	"dedede",
+	"jacuzzi",
+	"ultima",
+	"veyron_tiger",
+	"zork",
+}
