5ff5d27e107ceb8e5bb67fb186b422d75de60542
Seewai Fu <seewaifu@google.com>
2021-02-26 19:34:29 -0800

    tast: Allow remote tests to use build artifacts as external data files
    Allow remote tests to use build artifacts
    
    One unit test was modified and one new test example.RemoteArtifactFiles
    was added.
    
    Manual Test Output
    2021-02-27T02:52:43.243483Z Command line: tast run hana example.RemoteArtifactFiles
    ...
    2021-02-27T02:53:27.805532Z [18:53:27.805] Read artifact external data file (1490 bytes)
    2021-02-27T02:53:28.354290Z Completed test example.RemoteArtifactFiles in 1.115s with 0 error(s)
    2021-02-27T02:53:28.354472Z [18:53:28.354] Disconnecting from DUT
    2021-02-27T02:53:28.360295Z Ran 1 remote test(s) in 2.401s
    2021-02-27T02:53:32.089347Z --------------------------------------------------------------------------------
    2021-02-27T02:53:32.089381Z example.RemoteArtifactFiles  [ PASS ]
    2021-02-27T02:53:32.089441Z --------------------------------------------------------------------------------
    2021-02-27T02:53:32.089471Z Results saved to /tmp/tast/results/20210226-185243
    
    BUG=chromium:1115853
    TEST=./fast_build.sh -T; tast run -build=true hana example.RemoteArtifactFiles
    
    Cq-Depend: chromium:2724980
    Change-Id: I06b838e9ef3daf50abfc688f7090202a88125127

---
+++ src/chromiumos/tast/remote/bundles/cros/example/remote_artifact_files.go
@@ -0,0 +1,34 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+	"io/ioutil"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     RemoteArtifactFiles,
+		Desc:     "Demonstrates how to use artifact data files for remote test",
+		Contacts: []string{"seewaifu@chromium.org", "tast-owners@chromium.org"},
+		Attr:     []string{"group:mainline", "informational"},
+		Data:     []string{"artifact_files_UPLOADED"},
+	})
+}
+
+func RemoteArtifactFiles(ctx context.Context, s *testing.State) {
+	// Build artifacts can be used as an external data file and read with
+	// s.DataPath just similarly as internal data files or static external data files.
+	// However, this works for Chrome OS images built on official builders only;
+	// on developer builds an error is raised.
+	if b, err := ioutil.ReadFile(s.DataPath("artifact_files_UPLOADED")); err != nil {
+		s.Error("Failed reading artifact external data file: ", err)
+	} else {
+		s.Logf("Read artifact external data file (%d bytes)", len(b))
+	}
+}
