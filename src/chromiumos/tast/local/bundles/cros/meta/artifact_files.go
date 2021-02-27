caf4063dcd3b2902e1bfdab4af0cfb98d6ce22be
Seewai Fu <seewaifu@google.com>
2021-02-26 19:34:29 -0800

    tast: Allow remote tests to use build artifacts as external data files
    Added a new test to test new feature which allow remote tests to use build artifacts as external data files
    move example.ArtifactFiles to meta.ArtifactFiles
    move example.RemoteArtifactFiles to meta.RemoteArtifactFiles
    
    BUG=chromium:1115853
    TEST=./fast_build.sh -T; tast run -build=true octopus meta.RemoteArtifactFiles meta.ArtifactFiles
    
    Cq-Depend: chromium:2724980
    Change-Id: I06b838e9ef3daf50abfc688f7090202a88125127

---
+++ src/chromiumos/tast/local/bundles/cros/meta/artifact_files.go
@@ -0,0 +1,35 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package meta
+
+import (
+	"context"
+	"io/ioutil"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ArtifactFiles,
+		Desc:     "Demonstrates how to use artifact data files",
+		Contacts: []string{"nya@chromium.org", "tast-owners@chromium.org"},
+		Attr:     []string{"group:mainline", "informational"},
+		Data:     []string{"artifact_files_UPLOADED"},
+	})
+}
+
+// ArtifactFiles tests if local test can download artifact files.
+func ArtifactFiles(ctx context.Context, s *testing.State) {
+	// Build artifacts can be used as an external data file and read with
+	// s.DataPath just similarly as internal data files or static external data files.
+	// However, this works for Chrome OS images built on official builders only;
+	// on developer builds an error is raised.
+	if b, err := ioutil.ReadFile(s.DataPath("artifact_files_UPLOADED")); err != nil {
+		s.Error("Failed reading artifact external data file: ", err)
+	} else {
+		s.Logf("Read artifact external data file (%d bytes)", len(b))
+	}
+}
