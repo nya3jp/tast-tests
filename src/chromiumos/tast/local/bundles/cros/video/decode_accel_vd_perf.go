ab2c028b93678a90036c15caeb437a066ec8d129
Jeffrey Kardatzke <jkardatzke@google.com>
2021-03-01 14:18:51 -0800

    tast-tests: Add DecodeAccelVDPerf tests for HEVC
    BUG=b:173736991
    TEST=tast run DUT video.DecodeAccelVDPerf.hevc*
    
    Change-Id: I16f8bbd71f5f981a57ac79594c5e1d005e734963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2728219
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Katherine Threlkeld <kathrelkeld@chromium.org>
    Tested-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Auto-Submit: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Reviewed-by: Steve Cho <stevecho@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@google.com>

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_perf.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_perf.go
@@ -42,23 +42,43 @@ func init() {
 		}, {
 			Name:              "h264_1080p_30fps",
 			Val:               "1080p_30fps_300frames.h264",
-			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			ExtraData:         []string{"1080p_30fps_300frames.h264", "1080p_30fps_300frames.h264.json"},
 		}, {
 			Name:              "h264_1080p_60fps",
 			Val:               "1080p_60fps_600frames.h264",
-			ExtraSoftwareDeps: []string{caps.HWDecodeH264_60},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_60, "proprietary_codecs"},
 			ExtraData:         []string{"1080p_60fps_600frames.h264", "1080p_60fps_600frames.h264.json"},
 		}, {
 			Name:              "h264_2160p_30fps",
 			Val:               "2160p_30fps_300frames.h264",
-			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K, "proprietary_codecs"},
 			ExtraData:         []string{"2160p_30fps_300frames.h264", "2160p_30fps_300frames.h264.json"},
 		}, {
 			Name:              "h264_2160p_60fps",
 			Val:               "2160p_60fps_600frames.h264",
-			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K60},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K60, "proprietary_codecs"},
 			ExtraData:         []string{"2160p_60fps_600frames.h264", "2160p_60fps_600frames.h264.json"},
+		}, {
+			Name:              "hevc_1080p_30fps",
+			Val:               "1080p_30fps_300frames.hevc",
+			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC, "proprietary_codecs", "protected_content"},
+			ExtraData:         []string{"1080p_30fps_300frames.hevc", "1080p_30fps_300frames.hevc.json"},
+		}, {
+			Name:              "hevc_1080p_60fps",
+			Val:               "1080p_60fps_600frames.hevc",
+			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC60, "proprietary_codecs", "protected_content"},
+			ExtraData:         []string{"1080p_60fps_600frames.hevc", "1080p_60fps_600frames.hevc.json"},
+		}, {
+			Name:              "hevc_2160p_30fps",
+			Val:               "2160p_30fps_300frames.hevc",
+			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC4K, "proprietary_codecs", "protected_content"},
+			ExtraData:         []string{"2160p_30fps_300frames.hevc", "2160p_30fps_300frames.hevc.json"},
+		}, {
+			Name:              "hevc_2160p_60fps",
+			Val:               "2160p_60fps_600frames.hevc",
+			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC4K60, "proprietary_codecs", "protected_content"},
+			ExtraData:         []string{"2160p_60fps_600frames.hevc", "2160p_60fps_600frames.hevc.json"},
 		}, {
 			Name:              "vp8_1080p_30fps",
 			Val:               "1080p_30fps_300frames.vp8.ivf",
