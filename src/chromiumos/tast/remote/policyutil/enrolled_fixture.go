48fc2e56e796fd8151098f2d7ace3f2d63b63394
Victor-Gabriel Savu <vsavu@google.com>
2021-03-02 11:35:17 +0100

    policy: add enrolled fixture
    Add the enrolled fixture for local tests using device policies.
    AllowDinosaurEasterEggEnrolled added as an example.
    
    BUG=chromium:1143230
    TEST=tast run <ip> policy.AllowDinosaurEasterEggEnrolled
    
    Change-Id: I3d72a97a5bc6fdb970990a7922cb39dd5090db9f

---
+++ src/chromiumos/tast/remote/policyutil/enrolled_fixture.go
@@ -0,0 +1,74 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policyutil
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/rpc"
+	ps "chromiumos/tast/services/cros/policy"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name:            "enrolled",
+		Desc:            "Fixture profviding enrollment",
+		Contacts:        []string{"vsavu@google.com", "chromeos-commercial-stability@google.com"},
+		Impl:            &enrolledFixt{},
+		SetUpTimeout:    5 * time.Minute,
+		TearDownTimeout: 5 * time.Minute,
+		ResetTimeout:    15 * time.Second,
+		ServiceDeps:     []string{"tast.cros.policy.PolicyService"},
+	})
+}
+
+type enrolledFixt struct{}
+
+func (*enrolledFixt) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	if err := EnsureTPMIsResetAndPowerwash(ctx, s.DUT()); err != nil {
+		s.Fatal("Failed to reset TPM: ", err)
+	}
+
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	pc := ps.NewPolicyServiceClient(cl.Conn)
+
+	pJSON, err := json.Marshal(fakedms.NewPolicyBlob())
+	if err != nil {
+		s.Fatal("Failed to serialize policies: ", err)
+	}
+
+	if _, err := pc.EnrollUsingChrome(ctx, &ps.EnrollUsingChromeRequest{
+		PolicyJson: pJSON,
+	}); err != nil {
+		s.Fatal("Failed to enroll using Chrome: ", err)
+	}
+
+	if _, err := pc.StopChromeAndFakeDMS(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Failed to stop Chrome: ", err)
+	}
+
+	return nil
+}
+
+func (*enrolledFixt) TearDown(ctx context.Context, s *testing.FixtState) {
+	if err := EnsureTPMIsResetAndPowerwash(ctx, s.DUT()); err != nil {
+		s.Fatal("Failed to reset TPM: ", err)
+	}
+}
+
+func (*enrolledFixt) Reset(ctx context.Context) error                        { return nil }
+func (*enrolledFixt) PreTest(ctx context.Context, s *testing.FixtTestState)  {}
+func (*enrolledFixt) PostTest(ctx context.Context, s *testing.FixtTestState) {}
