59e7795f912301dd8a07ffb7388203c9c6c509b9
Meilin Wang <meilinw@google.com>
2021-03-03 15:49:39 -0800

    [Tast-test] Step down assistant.VolumeQueries to informational.
    This test is failing constantly in builders, so we've decided to set it
    back to informational. We will re-enable it again once we figure out a
    proven fix for it.
    
    BUG=chromium:1183768
    TEST=CQ
    
    Change-Id: I54671a1977fe23e32521cbeef71a905e831ff0d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2733917
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Meilin Wang <meilinw@chromium.org>
    Commit-Queue: Meilin Wang <meilinw@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/assistant/volume_queries.go
+++ src/chromiumos/tast/local/bundles/cros/assistant/volume_queries.go
@@ -22,7 +22,7 @@ func init() {
 		Func:         VolumeQueries,
 		Desc:         "Tests setting and increasing volume actions via Assistant",
 		Contacts:     []string{"meilinw@chromium.org", "xiaohuic@chromium.org"},
-		Attr:         []string{"group:mainline"},
+		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "chrome_internal", "audio_play"},
 		Pre:          assistant.VerboseLoggingEnabled(),
 	})
