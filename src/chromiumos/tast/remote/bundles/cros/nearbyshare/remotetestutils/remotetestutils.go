63b057651c2e5cf6dc6e8e4872cf77fafb59f7ec
David Haddock <dhaddock@google.com>
2021-03-02 18:53:24 -0800

    (Nearby Share) Even more logging for CB -> CB test.
    There is a bug where the logs are sometimes not saved. I cannot repro it
    locally so log basically everything that happens at the end of the test
    to see where the files go missing.
    
    BUG=chromium:1182517
    TEST=nearbyshare.*
    Exempt-From-Owner-Approval: Just adding more logging.
    
    Change-Id: Ib3b4aef06a94fb969e2e30a11b72d893ddac458e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2730336
    Tested-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: David Haddock <dhaddock@chromium.org>

--- src/chromiumos/tast/remote/bundles/cros/nearbyshare/remotetestutils/remotetestutils.go
+++ src/chromiumos/tast/remote/bundles/cros/nearbyshare/remotetestutils/remotetestutils.go
@@ -8,9 +8,9 @@ package remotetestutils
 import (
 	"context"
 	"path/filepath"
+	"strings"
 
 	"chromiumos/tast/dut"
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome/nearbyshare"
 	"chromiumos/tast/ssh/linuxssh"
 	"chromiumos/tast/testing"
@@ -18,14 +18,23 @@ import (
 
 // SaveLogs is a helper function to save the relevant Nearby Share logs from each DUT during a remote test.
 func SaveLogs(ctx context.Context, dut *dut.DUT, tag, outDir string) error {
+	var firstErr error
 	logsToSave := []string{nearbyshare.ChromeLog, nearbyshare.MessageLog}
+	logFiles, err := dut.Conn().Command("ls", nearbyshare.NearbyLogDir).Output(ctx)
+	if err != nil {
+		testing.ContextLog(ctx, "Failed to get list of log files in remote DUTs nearby temp dir: ", err)
+	} else {
+		testing.ContextLog(ctx, "Files in remote DUTs nearby temp dir: ", strings.Replace(string(logFiles), "\n", " ", -1))
+	}
 	for _, log := range logsToSave {
 		logPathSrc := filepath.Join(nearbyshare.NearbyLogDir, log)
 		logPathDst := filepath.Join(outDir, log+"_"+tag)
 		if err := linuxssh.GetFile(ctx, dut.Conn(), logPathSrc, logPathDst); err != nil {
 			testing.ContextLogf(ctx, "Failed to save %s to %s. Error: %s", logPathSrc, logPathDst, err)
-			return errors.Wrapf(err, "failed to save %s to %s", logPathSrc, logPathDst)
+			if firstErr == nil {
+				firstErr = err
+			}
 		}
 	}
-	return nil
+	return firstErr
 }
