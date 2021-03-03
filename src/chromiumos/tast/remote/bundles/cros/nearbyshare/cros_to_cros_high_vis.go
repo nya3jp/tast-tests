dfb22809fcbc0131668b46ed57c4e8342171f2e6
David Haddock <dhaddock@google.com>
2021-03-03 09:36:12 -0800

    (Nearby Share) Add remote fixture for CB -> CB tests.
    Added fixture and updated both CB->CB tests to use it.
    Added additional parameterized png tests to each.
    
    Removed all log collection from the tests. Removed remotetestutils.
    Remove all file deletion in the tests.
    Fixture will now take care of all setup, log collection, and file
    deletion.
    
    BUG=chromium:1184030
    TEST=nearbyshare.*
    
    Change-Id: Ief7ba9dccaa70e44a14bbb0d28716900eebeb939

--- src/chromiumos/tast/remote/bundles/cros/nearbyshare/cros_to_cros_high_vis.go
+++ src/chromiumos/tast/remote/bundles/cros/nearbyshare/cros_to_cros_high_vis.go
@@ -7,20 +7,16 @@ package nearbyshare
 import (
 	"context"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome/nearbyshare"
-	"chromiumos/tast/local/chrome/nearbyshare/nearbysetup"
 	"chromiumos/tast/local/chrome/nearbyshare/nearbytestutils"
 	"chromiumos/tast/local/chrome/uiauto/filesapp"
-	"chromiumos/tast/remote/bundles/cros/nearbyshare/remotetestutils"
-	"chromiumos/tast/rpc"
+	remotenearby "chromiumos/tast/remote/cros/nearbyshare"
 	"chromiumos/tast/services/cros/nearbyservice"
-	"chromiumos/tast/ssh/linuxssh"
 	"chromiumos/tast/testing"
 )
 
@@ -32,8 +28,15 @@ func init() {
 		Attr:         []string{"group:nearby-share-remote"},
 		SoftwareDeps: []string{"chrome"},
 		ServiceDeps:  []string{"tast.cros.nearbyservice.NearbyShareService"},
+		Fixture:      "nearbyShareRemoteDataUsageOfflineAllContactsTestUser",
 		Vars:         []string{"secondaryTarget"},
 		Params: []testing.Param{
+			{
+				Name:      "dataoffline_allcontacts_png5kb",
+				Val:       nearbytestutils.TestData{Filename: "small_png.zip", Timeout: nearbyshare.SmallFileTimeout},
+				ExtraData: []string{"small_png.zip"},
+				Timeout:   nearbyshare.SmallFileTimeout,
+			},
 			{
 				Name:      "dataoffline_allcontacts_jpg11kb",
 				Val:       nearbytestutils.TestData{Filename: "small_jpg.zip", Timeout: nearbyshare.SmallFileTimeout},
@@ -46,71 +49,11 @@ func init() {
 
 // CrosToCrosHighVis tests file sharing between Chrome OS devices.
 func CrosToCrosHighVis(ctx context.Context, s *testing.State) {
-	d1 := s.DUT()
-	secondary, ok := s.Var("secondaryTarget")
-	if !ok {
-		secondary = ""
-	}
-	secondaryDUT, err := nearbytestutils.ChooseSecondaryDUT(d1.HostName(), secondary)
-	if err != nil {
-		s.Fatal("Failed to find hostname for DUT2: ", err)
-	}
-	s.Log("Ensuring we can connect to DUT2: ", secondaryDUT)
-	d2, err := d1.NewSecondaryDevice(secondaryDUT)
-	if err != nil {
-		s.Fatal("Failed to create secondary device: ", err)
-	}
-	if err := d2.Connect(ctx); err != nil {
-		s.Fatal("Failed to connect to secondary DUT: ", err)
-	}
-
-	s.Log("Preparing to move remote data files to DUT1 (Sender)")
-	tempdir, err := d1.Conn().Command("mktemp", "-d", "/tmp/nearby_share_XXXXXX").Output(ctx)
-	if err != nil {
-		s.Fatal("Failed to create remote data path directory: ", err)
-	}
-	dataPath := strings.TrimSpace(string(tempdir))
-	defer d1.Conn().Command("rm", "-r", dataPath).Run(ctx)
-	testData := s.Param().(nearbytestutils.TestData).Filename
-	s.Log("Data file path is: ", s.DataPath(testData))
-	remoteFilePath := filepath.Join(dataPath, testData)
-	s.Log("Moving data files to DUT1 (Sender): ", remoteFilePath)
-	if _, err := linuxssh.PutFiles(ctx, d1.Conn(), map[string]string{
-		s.DataPath(testData): remoteFilePath,
-	}, linuxssh.DereferenceSymlinks); err != nil {
-		s.Fatalf("Failed to send data to remote data path %v: %v", dataPath, err)
-	}
-
-	// Login and setup Nearby Share on DUT 1 (Sender).
-	cl1, err := rpc.Dial(ctx, d1, s.RPCHint(), "cros")
-	if err != nil {
-		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
-	}
-	defer cl1.Close(ctx)
-	const crosBaseName = "cros_test"
-	senderDisplayName := nearbytestutils.RandomDeviceName(crosBaseName)
-	s.Log("Enabling Nearby Share on DUT1 (Sender). Name: ", senderDisplayName)
-	sender, err := enableNearbyShare(ctx, s, cl1, senderDisplayName)
-	if err != nil {
-		s.Fatal("Failed to enable Nearby Share on DUT1 (Sender): ", err)
-	}
-	defer sender.CloseChrome(ctx, &empty.Empty{})
-	defer remotetestutils.SaveLogs(ctx, d1, "sender", s.OutDir())
-
-	// Login and setup Nearby Share on DUT 2 (Receiver).
-	cl2, err := rpc.Dial(ctx, d2, s.RPCHint(), "cros")
-	if err != nil {
-		s.Fatal("Failed to dial rpc service on DUT2: ", err)
-	}
-	defer cl2.Close(ctx)
-	receiverDisplayName := nearbytestutils.RandomDeviceName(crosBaseName)
-	s.Log("Enabling Nearby Share on DUT2 (Receiver). Name: ", receiverDisplayName)
-	receiver, err := enableNearbyShare(ctx, s, cl2, receiverDisplayName)
-	if err != nil {
-		s.Fatal("Failed to enable Nearby Share on DUT2 (Receiver): ", err)
-	}
-	defer receiver.CloseChrome(ctx, &empty.Empty{})
-	defer remotetestutils.SaveLogs(ctx, d2, "receiver", s.OutDir())
+	remoteFilePath := s.FixtValue().(*remotenearby.FixtData).RemoteFilePath
+	sender := s.FixtValue().(*remotenearby.FixtData).Sender
+	receiver := s.FixtValue().(*remotenearby.FixtData).Receiver
+	senderDisplayName := s.FixtValue().(*remotenearby.FixtData).SenderDisplayName
+	receiverDisplayName := s.FixtValue().(*remotenearby.FixtData).ReceiverDisplayName
 
 	s.Log("Starting receiving on DUT2 (Receiver)")
 	if _, err := receiver.StartReceiving(ctx, &empty.Empty{}); err != nil {
@@ -118,7 +61,8 @@ func CrosToCrosHighVis(ctx context.Context, s *testing.State) {
 	}
 
 	s.Log("Starting sending on DUT1 (Sender)")
-	fileReq := &nearbyservice.CrOSPrepareFileRequest{FileName: remoteFilePath}
+	remoteFile := filepath.Join(remoteFilePath, s.Param().(nearbytestutils.TestData).Filename)
+	fileReq := &nearbyservice.CrOSPrepareFileRequest{FileName: remoteFile}
 	fileNames, err := sender.PrepareFiles(ctx, fileReq)
 	if err != nil {
 		s.Fatal("Failed to prepare files for sending on DUT1 (Sender): ", err)
@@ -144,13 +88,6 @@ func CrosToCrosHighVis(ctx context.Context, s *testing.State) {
 	if senderShareToken.ShareToken != receiverShareToken.ShareToken {
 		s.Fatalf("Share tokens for sender and receiver do not match. Sender: %s, Receiver: %s", senderShareToken, receiverShareToken)
 	}
-	// Remove the files on the receiver after test run is complete.
-	defer func() {
-		for _, f := range fileNames.FileNames {
-			filesPath := filepath.Join(filesapp.DownloadPath, f)
-			d2.Conn().Command("rm", "-r", filesPath).Run(ctx)
-		}
-	}()
 
 	// Repeat the file hash check for a few seconds, as we have no indicator on the CrOS side for when the received file has been completely written.
 	// TODO(crbug/1173190): Remove polling when we can confirm the transfer status with public test functions.
@@ -179,21 +116,4 @@ func CrosToCrosHighVis(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed file hash comparison: ", err)
 	}
 	s.Log("Share completed and file hashes match on both DUTs")
-}
-
-// enableNearbyShare is a helper function to enable Nearby Share on each DUT.
-func enableNearbyShare(ctx context.Context, s *testing.State, cl *rpc.Client, deviceName string) (nearbyservice.NearbyShareServiceClient, error) {
-	// Connect to the Nearby Share Service so we can execute local code on the DUT.
-	ns := nearbyservice.NewNearbyShareServiceClient(cl.Conn)
-	loginReq := &nearbyservice.CrOSLoginRequest{}
-	if _, err := ns.NewChromeLogin(ctx, loginReq); err != nil {
-		s.Fatal("Failed to start Chrome: ", err)
-	}
-
-	// Setup Nearby Share on the DUT.
-	req := &nearbyservice.CrOSSetupRequest{DataUsage: int32(nearbysetup.DataUsageOnline), Visibility: int32(nearbysetup.VisibilityAllContacts), DeviceName: deviceName}
-	if _, err := ns.CrOSSetup(ctx, req); err != nil {
-		s.Fatal("Failed to setup Nearby Share: ", err)
-	}
-	return ns, nil
 }
