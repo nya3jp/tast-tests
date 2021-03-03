4cb46a8c263dfb348710e092a998d23472e55940
Gavin Williams <gavinwill@chromium.org>
2021-03-02 17:06:52 -0800

    tast-test: Install and save USB printer
    Installs and save USB printer from the Settings page.
    
    The follow up CLs will upgrade this test to eventually test the whole
    printing flow.
    
    BUG=chromium:1183580
    TEST=printer.Print
    
    Change-Id: I5118a5bd267b22a8f458a6e0af319590279aa84f

--- src/chromiumos/tast/local/bundles/cros/printer/print.go
+++ src/chromiumos/tast/local/bundles/cros/printer/print.go
@@ -15,6 +15,8 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/ossettings"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/printing/printer"
+	"chromiumos/tast/local/printing/usbprinter"
 	"chromiumos/tast/testing"
 )
 
@@ -29,11 +31,18 @@ func init() {
 			"group:paper-io",
 			"paper-io_printing",
 		},
-		SoftwareDeps: []string{"chrome"},
+		SoftwareDeps: []string{"chrome", "cups", "virtual_usb_printer"},
 	})
 }
 
 func Print(ctx context.Context, s *testing.State) {
+	const (
+		attributes    = "/usr/local/etc/virtual-usb-printer/ipp_attributes.json"
+		descriptors   = "/usr/local/etc/virtual-usb-printer/ippusb_printer.json"
+		settingsLabel = "Printers"
+		settingsPage  = "osPrinting"
+	)
+
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
 	defer cancel()
@@ -50,14 +59,54 @@ func Print(ctx context.Context, s *testing.State) {
 	}
 	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
 
-	// Open OS Settings and navigate to the Printing page
-	ui := uiauto.New(tconn)
-	entryFinder := nodewith.Name("Printers").Role(role.Link).Ancestor(ossettings.WindowFinder)
-	if _, err := ossettings.LaunchAtPageURL(ctx, tconn, cr, "osPrinting", ui.Exists(entryFinder)); err != nil {
-		s.Fatal("Failed to launch Settings page: ", err)
+	// Install virtual USB printer.
+	s.Log("Installing printer")
+	if err := printer.ResetCups(ctx); err != nil {
+		s.Fatal("Failed to reset cupsd: ", err)
 	}
 
+	devInfo, err := usbprinter.LoadPrinterIDs(descriptors)
+	if err != nil {
+		s.Fatalf("Failed to load printer IDs from %v: %v", descriptors, err)
+	}
+
+	// Use cleanupCtx for any deferred cleanups in case of timeouts or
+	// cancellations on the shortened context.
+	if err := usbprinter.InstallModules(ctx); err != nil {
+		s.Fatal("Failed to install kernel modules: ", err)
+	}
+	defer func() {
+		if err := usbprinter.RemoveModules(cleanupCtx); err != nil {
+			s.Error("Failed to remove kernel modules: ", err)
+		}
+	}()
+
+	printer, _, err := usbprinter.StartIPPUSB(ctx, devInfo, descriptors, attributes, "" /*record*/)
+	if err != nil {
+		s.Fatal("Failed to start IPP-over-USB printer: ", err)
+	}
+	defer func() {
+		printer.Kill()
+		printer.Wait()
+	}()
+
+	// Open OS Settings and navigate to the Printing page.
+	ui := uiauto.New(tconn)
+	entryFinder := nodewith.Name(settingsLabel).Role(role.Link).Ancestor(ossettings.WindowFinder)
+	if _, err := ossettings.LaunchAtPageURL(ctx, tconn, cr, settingsPage, ui.Exists(entryFinder)); err != nil {
+		s.Fatal("Failed to launch Settings page: ", err)
+	}
 	if err := ui.LeftClick(entryFinder)(ctx); err != nil {
 		s.Fatal("Failed to click entry: ", err)
+	}
+
+	// Save the USB printer.
+	savePrinterButton := nodewith.ClassName("save-printer-button").Ancestor(ossettings.WindowFinder)
+	if err := ui.LeftClick(savePrinterButton)(ctx); err != nil {
+		s.Fatal("Failed to click Save button: ", err)
+	}
+	editPrinterButton := nodewith.ClassName("icon-more-vert").Ancestor(ossettings.WindowFinder)
+	if err := ui.WithTimeout(time.Minute).WaitUntilExists(editPrinterButton)(ctx); err != nil {
+		s.Fatal("Failed to find edit printer button: ", err)
 	}
 }
