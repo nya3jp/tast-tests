e04020c1fe55b619c6e5ffc95afb7899614c94cc
Edward O'Callaghan <quasisec@google.com>
2021-03-03 11:30:07 +1100

    cros/firmware/flashrom.go: Flashrom perf test
    Implement a E2E test for flashrom to measure the time of
    execution. Some tasks such as enrollment are time sensitive
    and depend on Flashrom.
    
    BUG=b:179342220
    TEST=none
    
    Change-Id: I78d449aa3bc5318adacda8d5bfd41a676ccc1a2d

--- src/chromiumos/tast/local/bundles/cros/firmware/flashrom.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/flashrom.go
@@ -9,7 +9,9 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"regexp"
+	"time"
 
+	"chromiumos/tast/common/perf"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/shutil"
 	"chromiumos/tast/testing"
@@ -27,10 +29,21 @@ func init() {
 		Attr:         []string{"group:mainline", "group:labqual"},
 		SoftwareDeps: []string{"flashrom"},
 	})
+	testing.AddTest(&testing.Test{
+		Func: FlashromPerf,
+		Desc: "Flashrom perf metrics",
+		Contacts: []string{
+			"kmshelton@chromium.org",       // Test Author
+			"quasisec@chromium.org",        // CrOS Flashrom Maintainer
+			"chromeos-firmware@google.com", // CrOS Firmware Developers
+		},
+		Attr:         []string{"group:mainline", "group:labqual"},
+		SoftwareDeps: []string{"flashrom"},
+	})
 }
 
 // Flashrom runs the flashrom utility and confirms that flashrom was able to
-// communicate with a SPI ROM.
+// communicate with a SPI flash.
 func Flashrom(ctx context.Context, s *testing.State) {
 	// This test intentionally avoids SPI ROM read and write operations, so as not
 	// to stress devices-under-test.
@@ -47,4 +60,41 @@ func Flashrom(ctx context.Context, s *testing.State) {
 			"Output of %q did not contain %q (saved output to %s).",
 			shutil.EscapeSlice(cmd.Args), re, filepath.Base(path))
 	}
+}
+
+// FlashromPerf runs the flashrom utility and times its total execution time for
+// a complete probe and read of the SPI flash.
+func FlashromPerf(ctx context.Context, s *testing.State) {
+	var (
+		readTime = perf.Metric{
+			Name:      "flashrom_read_time",
+			Unit:      "ms",
+			Direction: perf.SmallerIsBetter,
+		}
+	)
+
+	p := perf.NewValues()
+	duration = testFlashromReadTime()
+	p.Set(readTime, duration)
+
+	if err := p.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
+
+func testFlashromReadTime() {
+	flashromStart := time.Now()
+
+	cmd := testexec.CommandContext(ctx, "flashrom", "-r", "dump.bin")
+	if out, err := cmd.Output(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("%q failed: %v", shutil.EscapeSlice(cmd.Args), err)
+	}
+	path := filepath.Join(s.OutDir(), "flashrom.txt")
+	if err := ioutil.WriteFile(path, out, 0644); err != nil {
+		s.Error("Failed to save flashrom output: ", err)
+	}
+	flashromElapsed = time.Since(flashromStart)
+
+	// Return duration.
+	return float64(flashromElapsed.Milliseconds())
 }
