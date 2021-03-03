182ced63337abee84c68ddc6dafbce5d1a68a1d1
Edward O'Callaghan <quasisec@google.com>
2021-03-03 11:30:07 +1100

    cros/firmware/flashrom.go: Flashrom perf test
    Implement a E2E test for flashrom to measure the time of
    execution. Some tasks such as enrollment are time sensitive
    and depend on Flashrom.
    
    BUG=b:179342220
    TEST=`$ tast -verbose run 100.114.231.236 firmware.Flashrom`
    
    Change-Id: I78d449aa3bc5318adacda8d5bfd41a676ccc1a2d

--- src/chromiumos/tast/local/bundles/cros/firmware/flashrom.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/flashrom.go
@@ -9,7 +9,9 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"regexp"
+	"time"
 
+	"chromiumos/tast/common/perf"
 	"chromiumos/tast/local/testexec"
 	"chromiumos/tast/shutil"
 	"chromiumos/tast/testing"
@@ -18,7 +20,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func: Flashrom,
-		Desc: "Checks that flashrom can find a SPI ROM",
+		Desc: "Flashrom SPI flash E2E tests",
 		Contacts: []string{
 			"kmshelton@chromium.org",       // Test Author
 			"quasisec@chromium.org",        // CrOS Flashrom Maintainer
@@ -29,9 +31,15 @@ func init() {
 	})
 }
 
-// Flashrom runs the flashrom utility and confirms that flashrom was able to
-// communicate with a SPI ROM.
+// Flashrom runs the flashrom utility to verify various expected behaviour is maintained.
 func Flashrom(ctx context.Context, s *testing.State) {
+	testFlashromDetect(ctx, s)
+	testFlashromPerf(ctx, s)
+}
+
+// testFlashromDetect runs the flashrom utility and confirms that flashrom was able to
+// communicate with a SPI flash.
+func testFlashromDetect(ctx context.Context, s *testing.State) {
 	// This test intentionally avoids SPI ROM read and write operations, so as not
 	// to stress devices-under-test.
 	cmd := testexec.CommandContext(ctx, "flashrom", "--verbose")
@@ -47,4 +55,37 @@ func Flashrom(ctx context.Context, s *testing.State) {
 			"Output of %q did not contain %q (saved output to %s).",
 			shutil.EscapeSlice(cmd.Args), re, filepath.Base(path))
 	}
+}
+
+// testFlashromPerf runs the flashrom utility and times its total execution time for
+// a complete probe and read of the SPI flash.
+func testFlashromPerf(ctx context.Context, s *testing.State) {
+	var (
+		readTime = perf.Metric{
+			Name:      "flashrom_read_time",
+			Unit:      "ms",
+			Direction: perf.SmallerIsBetter,
+		}
+	)
+
+	p := perf.NewValues()
+	duration := testFlashromReadTime(ctx, s)
+	p.Set(readTime, duration)
+
+	if err := p.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
+
+func testFlashromReadTime(ctx context.Context, s *testing.State) float64 {
+	flashromStart := time.Now()
+
+	cmd := testexec.CommandContext(ctx, "flashrom", "-r", "/tmp/dump.bin")
+	if _, err := cmd.Output(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("%q failed: %v", shutil.EscapeSlice(cmd.Args), err)
+	}
+	flashromElapsed := time.Since(flashromStart)
+
+	// Return duration.
+	return float64(flashromElapsed.Milliseconds())
 }
