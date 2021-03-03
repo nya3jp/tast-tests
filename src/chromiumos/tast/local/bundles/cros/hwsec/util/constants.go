6d4e6ea1529756f83cf6d326f302bed9f9385c07
Yi Chou <yich@google.com>
2021-03-03 16:31:34 +0800

    hwsec: Add CryptohomeMigrateKey tast-test
    This migrates platform_CryptohomeMigrateKey to tast-tests.
    
    https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/third_party/autotest/files/client/site_tests/platform_CryptohomeMigrateKey
    
    BUG=b:178163420
    TEST=tast run $DUT hwsec.CryptohomeMigrateKey
    
    Change-Id: I8ebbb0324b64918021106a9fed4a83af97084595

--- src/chromiumos/tast/local/bundles/cros/hwsec/util/constants.go
+++ src/chromiumos/tast/local/bundles/cros/hwsec/util/constants.go
@@ -6,21 +6,26 @@ package util
 
 // This file contains some shared constants for local hwsec bundle tests.
 const (
+	// Example user information
 	FirstUsername = "fakeuser1@example.com"
 	FirstPassword = "FakePasswordForFakeUser1"
 	FirstPin      = "123456"
 
+	// Example user information
 	SecondUsername = "fakeuser2@example.com"
 	SecondPassword = "FakePasswordForFakeUser2"
 	SecondPin      = "654321"
 
+	// Example user information
 	ThirdUsername = "fakeuser3@example.com"
 	ThirdPassword = "FakePasswordForFakeUser3"
 	ThirdPin      = "987654"
 
+	// Example key label
 	PasswordLabel = "PasswordLabel"
 	PinLabel      = "PinLabel"
 
+	// Example test file
 	TestFileName1   = "TESTFILE1"
 	TestFileName2   = "TESTFILE2"
 	TestFileContent = "TEST_CONTENT"
