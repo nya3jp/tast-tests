b03b004c6d138e306f0cce93144e839957feb74a
Yi Chou <yich@google.com>
2021-03-03 20:21:00 +0800

    hwsec: Add CryptohomeMount tast-test
    This migrates platform_CryptohomeMount to tast-tests.
    
    https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/third_party/autotest/files/client/site_tests/platform_CryptohomeMount
    
    BUG=b:178163420
    TEST=tast run $DUT hwsec.CryptohomeMount
    
    Change-Id: I638765294b347c9892aeec201dd56da8699e0573

---
+++ src/chromiumos/tast/common/hwsec/cryptohome_mount_info.go
@@ -0,0 +1,208 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/shirou/gopsutil/disk"
+
+	"chromiumos/tast/errors"
+)
+
+const (
+	// GuestUser is the name representing a guest user account.
+	// Defined in libbrillo/brillo/cryptohome.cc.
+	GuestUser = "$guest"
+
+	// mounterExe is the full executable for the out-of-process cryptohome
+	// mounter.
+	// Defined in cryptohome/BUILD.gn.
+	mounterExe = "/usr/sbin/cryptohome-namespace-mounter"
+
+	// cryptohomedExe is the full path of the daemon cryptohomed.
+	//
+	// TODO(crbug.com/1074735): Remove this and use mounterExe above for
+	// normal user mounts as well once the out-of-process cryptohome
+	// mounter is ready for that.
+	cryptohomedExe = "/usr/sbin/cryptohomed"
+)
+
+// matches a path to vault under /home/shadow.
+// Example: "/home/.shadow/118c4648065f5cd3660e17a53533ec7bc924d01f/vault"
+var shadowRegexp *regexp.Regexp
+
+// matches a path to /dev/*.
+// Example: "/dev/sda1"
+var devRegexp *regexp.Regexp
+
+// matches a path to /dev/loop\d+.
+// Example: "/dev/loop0"
+var devLoopRegexp *regexp.Regexp
+
+func init() {
+	shadowRegexp = regexp.MustCompile(`^/home/\.shadow/[^/]*/vault$`)
+	devRegexp = regexp.MustCompile(`^/dev(/[^/]*){1,2}$`)
+	devLoopRegexp = regexp.MustCompile(`^/dev/loop[0-9]+$`)
+}
+
+// CryptohomeMountInfo is a helper to get cryptohome mount information.
+type CryptohomeMountInfo struct {
+	runner     CmdRunner
+	cryptohome *CryptohomeClient
+}
+
+// NewCryptohomeMountInfo creates a new CryptohomeMountInfo
+func NewCryptohomeMountInfo(r CmdRunner, c *CryptohomeClient) *CryptohomeMountInfo {
+	return &CryptohomeMountInfo{r, c}
+}
+
+// IsMounted checks if the vault for the user is mounted.
+func (c *CryptohomeMountInfo) IsMounted(ctx context.Context, user string) (bool, error) {
+	mounter := cryptohomedExe
+	validatePartition := c.validatePermanentPartition
+
+	if user == GuestUser {
+		mounter = mounterExe
+		validatePartition = c.validateGuestPartition
+	}
+
+	userpath, err := c.cryptohome.GetHomeUserPath(ctx, user)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to get user home path")
+	}
+	systempath, err := c.cryptohome.GetRootUserPath(ctx, user)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to get user root path")
+	}
+	partitions, err := c.findMounts(ctx, mounter)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to find mount points")
+	}
+
+	up := c.findPartition(partitions, userpath)
+	if up == nil {
+		return false, nil
+	}
+	if err = validatePartition(up); err != nil {
+		return false, nil
+	}
+	sp := c.findPartition(partitions, systempath)
+	if sp == nil {
+		return false, nil
+	}
+	if err = validatePartition(sp); err != nil {
+		return false, nil
+	}
+	return true, nil
+}
+
+// findMounterPID finds the pid of the given mounter process.
+func (c *CryptohomeMountInfo) findMounterPID(ctx context.Context, mounter string) (int32, error) {
+	bs, err := c.runner.Run(ctx, "pidof", "-s", mounter)
+	if err != nil {
+		// Return -1 means we couldn't find the process.
+		return -1, nil
+	}
+
+	// Convert the result to pid
+	msg := string(bs)
+	pid, err := strconv.ParseInt(strings.TrimSpace(msg), 10, 32)
+	if err != nil {
+		return -1, errors.Wrapf(err, "failed to convert the pid %q", msg)
+	}
+	return int32(pid), nil
+}
+
+// findMountsForPID returns the list of mounts in pid's mount namespace.
+func (c *CryptohomeMountInfo) findMountsForPID(ctx context.Context, pid int32) ([]disk.PartitionStat, error) {
+	path := fmt.Sprintf("/proc/%d/mounts", pid)
+	bs, err := c.runner.Run(ctx, "cat", "--", path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get list of mounts for pid %d", pid)
+	}
+	output := strings.Trim(string(bs), "\n")
+	mounts := strings.Split(output, "\n")
+
+	var res []disk.PartitionStat
+	for _, mount := range mounts {
+		fields := strings.Fields(mount)
+		if len(fields) < 4 {
+			return nil, errors.Errorf("mount information doesn't have 4 fields: %q", mount)
+		}
+		d := disk.PartitionStat{
+			Device:     fields[0],
+			Mountpoint: fields[1],
+			Fstype:     fields[2],
+			Opts:       fields[3],
+		}
+		res = append(res, d)
+	}
+	return res, nil
+}
+
+// findMounts returns the list of mounts in the given mounter's mount namespace.
+func (c *CryptohomeMountInfo) findMounts(ctx context.Context, mounter string) ([]disk.PartitionStat, error) {
+	mounterPID, err := c.findMounterPID(ctx, mounter)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not list running processes")
+	} else if mounterPID == -1 {
+		// If the mounter process is not running, the list of mounts is
+		// empty.
+		return nil, nil
+	}
+	return c.findMountsForPID(ctx, mounterPID)
+}
+
+// findPartition returns a pointer to the entry in ps corresponding to path,
+// or nil if no matching entry is present.
+func (c *CryptohomeMountInfo) findPartition(ps []disk.PartitionStat, path string) *disk.PartitionStat {
+	for i := range ps {
+		if ps[i].Mountpoint == path {
+			return &ps[i]
+		}
+	}
+	return nil
+}
+
+// validatePermanentPartition checks if the given partition is valid for a
+// (non-guest) user mount.
+func (c *CryptohomeMountInfo) validatePermanentPartition(p *disk.PartitionStat) error {
+	switch p.Fstype {
+	case "ext4":
+		if !devRegexp.MatchString(p.Device) || devLoopRegexp.MatchString(p.Device) {
+			return errors.Errorf("ext4 device %q does not match %q excluding %q", p.Device, devRegexp, devLoopRegexp)
+		}
+	case "ecryptfs":
+		if !shadowRegexp.MatchString(p.Device) {
+			return errors.Errorf("ecryptfs device %q does not match %q", p.Device, shadowRegexp)
+		}
+	default:
+		return errors.Errorf("unexpected file system: %q", p.Fstype)
+	}
+	return nil
+}
+
+// validateGuestPartition checks if the given partition is valid for a guest
+// user mount.
+func (c *CryptohomeMountInfo) validateGuestPartition(p *disk.PartitionStat) error {
+	switch p.Fstype {
+	case "ext4":
+		if !devLoopRegexp.MatchString(p.Device) {
+			return errors.Errorf("ext4 device %q does not match %q", p.Device, devLoopRegexp)
+		}
+	case "tmpfs":
+		if p.Device != "guestfs" {
+			return errors.Errorf("tmpfs device %q should be guestfs", p.Device)
+		}
+	default:
+		return errors.Errorf("unexpected file system: %q", p.Fstype)
+	}
+	return nil
+}
