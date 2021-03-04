75771c3c4c7413ac5cd1aeb6b745bbf8273898ea
Yicheng Li <yichengli@chromium.org>
2021-03-03 18:18:27 -0800

    firmware: Let fingerprint tests verify firmware information
    This migrates the firmware information checks we do in autotests.
    ValidateBuildFwFile() will be called by all fingerprint firmware
    tests.
    
    BUG=b:177029702, b:153736350
    TEST=tast -verbose run -var servo=localhost:9999 <IP>
    	firmware.FpAddEntropy
    
    Change-Id: Ieb8f8143c083d31c0fe36d75eae50d05d1993ead

--- src/chromiumos/tast/remote/firmware/fingerprint/fingerprint.go
+++ src/chromiumos/tast/remote/firmware/fingerprint/fingerprint.go
@@ -39,6 +39,23 @@ const (
 	ImageTypeRW FWImageType = "RW"
 )
 
+type firmwareMetadata struct {
+	sha256sum string
+	roVersion string
+	rwVersion string
+	keyID     string
+}
+
+type fwInfoType int
+
+const (
+	// Elements of firmware information.
+	fwInfoTypeSha256sum fwInfoType = iota
+	fwInfoTypeRoVersion
+	fwInfoTypeRwVersion
+	fwInfoTypeKeyID
+)
+
 const (
 	// nocturne and nami are special cases and have "_fp" appended.
 	// Newer FPMCUs have unique names.
@@ -47,8 +64,225 @@ const (
 	fingerprintFirmwarePathBase = "/opt/google/biod/fw/"
 	// WaitForBiodToStartTimeout is the time to wait for biod to start.
 	WaitForBiodToStartTimeout = 30 * time.Second
+
+	// Types of signing keys.
+	keyTypeDev   = "dev"
+	keyTypePreMp = "premp"
+	keyTypeMp    = "mp"
+
+	// EC board names for FPMCUs.
+	fpBoardNameBloonchipper = "bloonchipper"
+	fpBoardNameDartmonkey   = "dartmonkey"
+	fpBoardNameNocturne     = "nocturne_fp"
+	fpBoardNameNami         = "nami_fp"
 )
 
+// Map from signing key ID to type of signing key.
+var keyIDMap = map[string]string{
+	// bloonchipper.
+	"61382804da86b4156d666cc9a976088f8b647d44": keyTypeDev,
+	"07b1af57220c196e363e68d73a5966047c77011e": keyTypePreMp,
+	"1c590ef36399f6a2b2ef87079c135b69ef89eb60": keyTypeMp,
+
+	// dartmonkey.
+	"257a0aa3ac9e81aa4bc3aabdb6d3d079117c5799": keyTypeMp,
+
+	// nocturne.
+	"8a8fc039a9463271995392f079b83ce33832d07d": keyTypeDev,
+	"6f38c866182bd9bf7a4462c06ac04fa6a0074351": keyTypeMp,
+	"f6f7d96c48bd154dbae7e3fe3a3b4c6268a10934": keyTypePreMp,
+
+	// nami.
+	"754aea623d69975a22998f7b97315dd53115d723": keyTypePreMp,
+	"35486c0090ca390408f1fbbf2a182966084fe2f8": keyTypeMp,
+}
+
+// Map of attributes for a given board's various firmware file releases.
+// Two purposes:
+//   1) Documents the exact versions and keys used for a given firmware file.
+//   2) Used to verify that files that end up in the build (and therefore
+//      what we release) is exactly what we expect.
+var firmwareVersionMap = map[string]map[string]firmwareMetadata{
+	fpBoardNameBloonchipper: map[string]firmwareMetadata{
+		"bloonchipper_v2.0.4277-9f652bb3-RO_v2.0.7314-3dfc5ff6-RW.bin": firmwareMetadata{
+			sha256sum: "2bac89c16ad71986fe37ed651fe7dd6d5a3d039678d4a5f1d03c5a65a9f3bc3c",
+			roVersion: "bloonchipper_v2.0.4277-9f652bb3",
+			rwVersion: "bloonchipper_v2.0.7314-3dfc5ff6",
+			keyID:     "1c590ef36399f6a2b2ef87079c135b69ef89eb60",
+		},
+		"bloonchipper_v2.0.5938-197506c1-RO_v2.0.7314-3dfc5ff6-RW.bin": firmwareMetadata{
+			sha256sum: "50ddcad558e1ded476a209946cabcddd6d9c1033890f1661d7ba8c183aa625ab",
+			roVersion: "bloonchipper_v2.0.5938-197506c1",
+			rwVersion: "bloonchipper_v2.0.7314-3dfc5ff6",
+			keyID:     "1c590ef36399f6a2b2ef87079c135b69ef89eb60",
+		},
+	},
+	fpBoardNameNocturne: map[string]firmwareMetadata{
+		"nocturne_fp_v2.2.64-58cf5974e-RO_v2.0.7304-441100b93-RW.bin": firmwareMetadata{
+			sha256sum: "569a191bd2ed25ce89b296f0ab8cd2ed567dbf6a8df3f6b3f82ad58c786d79a9",
+			roVersion: "nocturne_fp_v2.2.64-58cf5974e",
+			rwVersion: "nocturne_fp_v2.0.7304-441100b93",
+			keyID:     "6f38c866182bd9bf7a4462c06ac04fa6a0074351",
+		},
+	},
+	fpBoardNameNami: map[string]firmwareMetadata{
+		"nami_fp_v2.2.144-7a08e07eb-RO_v2.0.7304-441100b93-RW.bin": firmwareMetadata{
+			sha256sum: "e7b23f5e585c47d24fe3696139b48c0bac8c43b025669f74aafbff4aa9cbbebd",
+			roVersion: "nami_fp_v2.2.144-7a08e07eb",
+			rwVersion: "nami_fp_v2.0.7304-441100b93",
+			keyID:     "35486c0090ca390408f1fbbf2a182966084fe2f8",
+		},
+	},
+	fpBoardNameDartmonkey: map[string]firmwareMetadata{
+		"dartmonkey_v2.0.2887-311310808-RO_v2.0.7304-441100b93-RW.bin": firmwareMetadata{
+			sha256sum: "5127137655b4b13d7a86ba897b08a9957d36b74afb97558496c6fba98e808b7b",
+			roVersion: "dartmonkey_v2.0.2887-311310808",
+			rwVersion: "dartmonkey_v2.0.7304-441100b93",
+			keyID:     "257a0aa3ac9e81aa4bc3aabdb6d3d079117c5799",
+		},
+	},
+}
+
+// getExpectedFwInfo returns expected firmware info for a given firmware file name.
+func getExpectedFwInfo(fpBoard, buildFwFile string, infoType fwInfoType) (string, error) {
+	boardExpectedFwInfo, ok := firmwareVersionMap[fpBoard]
+	if !ok {
+		return "", errors.Errorf("failed to get firmware info for board %s", fpBoard)
+	}
+	expectedFwInfo, ok := boardExpectedFwInfo[filepath.Base(buildFwFile)]
+	if !ok {
+		return "", errors.Errorf("failed to get firmware info for file %s", buildFwFile)
+	}
+	switch infoType {
+	case fwInfoTypeSha256sum:
+		return expectedFwInfo.sha256sum, nil
+	case fwInfoTypeRwVersion:
+		return expectedFwInfo.rwVersion, nil
+	case fwInfoTypeRoVersion:
+		return expectedFwInfo.roVersion, nil
+	case fwInfoTypeKeyID:
+		return expectedFwInfo.keyID, nil
+	default:
+		return "", errors.Errorf("failed to get firmware info type %d", infoType)
+	}
+}
+
+// ValidateBuildFwFile checks that all attributes in the given firmware file match their expected values.
+func ValidateBuildFwFile(ctx context.Context, d *dut.DUT, fpBoard, buildFwFile string) error {
+	// Check hash on device.
+	actualHash, err := calculateSha256sum(ctx, d, buildFwFile)
+	if err != nil {
+		return err
+	}
+	expectedHash, err := getExpectedFwInfo(fpBoard, buildFwFile, fwInfoTypeSha256sum)
+	if err != nil {
+		return err
+	}
+	if actualHash != expectedHash {
+		return errors.Errorf("failed to validate the sha256 sum, got %s, want %s", actualHash, expectedHash)
+	}
+
+	// Check signing key ID.
+	actualKeyID, err := readFirmwareKeyID(ctx, d, buildFwFile)
+	if err != nil {
+		return err
+	}
+	expectedKeyID, err := getExpectedFwInfo(fpBoard, buildFwFile, fwInfoTypeKeyID)
+	if err != nil {
+		return err
+	}
+	if actualKeyID != expectedKeyID {
+		return errors.Errorf("failed to validate the key id, got %s, want %s", actualKeyID, expectedKeyID)
+	}
+
+	// Check the signing key type is allowed.
+	keyType, ok := keyIDMap[actualKeyID]
+	if !ok {
+		return errors.Errorf("failed to get key type for key id: %s", actualKeyID)
+	}
+	if keyType != keyTypePreMp && keyType != keyTypeMp {
+		return errors.Errorf("key type %s is not allowed", keyType)
+	}
+
+	// Check RO version.
+	actualRoVersion, err := readFmapSection(ctx, d, buildFwFile, "RO_FRID")
+	if err != nil {
+		return err
+	}
+	expectedRoVersion, err := getExpectedFwInfo(fpBoard, buildFwFile, fwInfoTypeRoVersion)
+	if err != nil {
+		return err
+	}
+	if actualRoVersion != expectedRoVersion {
+		return errors.Errorf("failed to validate the RO version, got %s, want %s", actualRoVersion, expectedRoVersion)
+	}
+
+	// Check RW version.
+	actualRwVersion, err := readFmapSection(ctx, d, buildFwFile, "RW_FWID")
+	if err != nil {
+		return err
+	}
+	expectedRwVersion, err := getExpectedFwInfo(fpBoard, buildFwFile, fwInfoTypeRwVersion)
+	if err != nil {
+		return err
+	}
+	if actualRwVersion != expectedRwVersion {
+		return errors.Errorf("failed to validate the RW version, got %s, want %s", actualRwVersion, expectedRwVersion)
+	}
+
+	testing.ContextLog(ctx, "Succeeded validating build firmware metadata")
+	return nil
+}
+
+// readFmapSection reads a section (e.g. RO_FRID) from a firmware file on device.
+func readFmapSection(ctx context.Context, d *dut.DUT, buildFwFile, section string) (string, error) {
+	// Prepare a temporary file because dump_map only writes the
+	// value read from a section to a file (will not just print it to
+	// stdout).
+	tempdir, err := d.Command("mktemp", "-d", "/tmp/fingerprint_dump_fmap_XXXXXX").Output(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create remote temp directory")
+	}
+	tempdirPath := strings.TrimSpace(string(tempdir))
+	defer d.Command("rm", "-r", tempdirPath).Run(ctx)
+
+	outputPath := filepath.Join(tempdirPath, section)
+	if err := d.Command("dump_fmap", "-x", buildFwFile, fmt.Sprintf("%s:%s", section, outputPath)).Run(ctx); err != nil {
+		return "", errors.Wrap(err, "failed to run dump_fmap")
+	}
+
+	out, err := d.Command("cat", outputPath).Output(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to read dump_fmap output")
+	}
+	// dump_fmap writes NULL characters at the end.
+	return strings.Trim(string(out), "\x00"), nil
+}
+
+// readFirmwareKeyID reads the key id of a firmware file on device.
+func readFirmwareKeyID(ctx context.Context, d *dut.DUT, buildFwFile string) (string, error) {
+	out, err := d.Command("futility", "show", buildFwFile).Output(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to run futility on device")
+	}
+	parsed := parseColonDelimitedOutput(string(out))
+	keyID, ok := parsed["ID"]
+	if !ok {
+		return "", errors.Errorf("failed to find key ID for %s", buildFwFile)
+	}
+	return keyID, nil
+}
+
+// calculateSha256sum calculates the sha256sum of a file on device.
+func calculateSha256sum(ctx context.Context, d *dut.DUT, buildFwFile string) (string, error) {
+	out, err := d.Command("sha256sum", buildFwFile).Output(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to calculate sha256sum on device")
+	}
+	return strings.Split(string(out), " ")[0], nil
+}
+
+// boardFromCrosConfig returns the fingerprint board name from cros_config.
 func boardFromCrosConfig(ctx context.Context, d *dut.DUT) (string, error) {
 	out, err := d.Command("cros_config", "/fingerprint", "board").Output(ctx)
 	return string(out), err
@@ -72,7 +306,8 @@ func Board(ctx context.Context, d *dut.DUT) (string, error) {
 	return boardFromCrosConfig(ctx, d)
 }
 
-func firmwarePath(ctx context.Context, d *dut.DUT, fpBoard string) (string, error) {
+// FirmwarePath returns the path to the fingerprint firmware file on device.
+func FirmwarePath(ctx context.Context, d *dut.DUT, fpBoard string) (string, error) {
 	cmd := fmt.Sprintf("ls %s%s*.bin", fingerprintFirmwarePathBase, fpBoard)
 	out, err := d.Command("bash", "-c", cmd).Output(ctx)
 	if err != nil {
@@ -93,7 +328,7 @@ func FlashFirmware(ctx context.Context, d *dut.DUT) error {
 	}
 	testing.ContextLogf(ctx, "fp board name: %q", fpBoard)
 
-	fpFirmwarePath, err := firmwarePath(ctx, d, fpBoard)
+	fpFirmwarePath, err := FirmwarePath(ctx, d, fpBoard)
 	if err != nil {
 		return errors.Wrap(err, "failed to get fp firmware path")
 	}
