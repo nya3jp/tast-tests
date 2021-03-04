aa0cc2ac0106b4acfa1b83512f272ee5aad597b1
Brian Norris <briannorris@chromium.org>
2021-03-03 18:13:57 -0800

    wifi: fixup doc strings
    BUG=none
    TEST=none
    
    Change-Id: I6a43ad29a7a644e09fdeca0f96becee0ba1316f8

--- src/chromiumos/tast/remote/bundles/cros/wifi/doc.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/doc.go
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package wifi contains remote Tast tests that exercise the Chrome OS wifi stack.
+// Package wifi contains remote Tast tests that exercise the Chrome OS WiFi stack
 package wifi
