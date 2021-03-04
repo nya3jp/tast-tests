841632b633cc36a47a049e32ac9a5548fb7ea103
Gavin Williams <gavinwill@chromium.org>
2021-03-04 14:49:46 -0800

    tast-tests: Create E2E printing test
    This test incorporates the end to end printing flow. The following
    objectives are covered:
    1. Save a printer from the OS Settings page
    2. Open print preview and print to that printer
    3. Open Print Management app and verify a print job is created
    
    BUG=chromium:1183580
    TEST=printer.Print
    
    Change-Id: Ibcbd2cf489f41070b958edb86c0bffc00049c728

--- src/chromiumos/tast/local/chrome/ui/printmanagementapp/printmanagementapp.go
+++ src/chromiumos/tast/local/chrome/ui/printmanagementapp/printmanagementapp.go
@@ -6,7 +6,13 @@
 package printmanagementapp
 
 import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/ui"
 )
 
@@ -18,4 +24,35 @@ var printManagementRootNodeParams = ui.FindParams{
 var printManagementClearHistoryButton = ui.FindParams{
 	Name: "Clear all history",
 	Role: ui.RoleTypeButton,
+}
+
+// PrintManagementHistoryLabel export is used to find the label for the History
+// section.
+var PrintManagementHistoryLabel = ui.FindParams{
+	Name: "History",
+	Role: ui.RoleTypeStaticText,
+}
+
+// PrintManagementPrintJobEntry export is used to find all print job entries.
+var PrintManagementPrintJobEntry = ui.FindParams{
+	ClassName: "list-item flex-center",
+}
+
+// Launch Print Management app via default method and return root node.
+func Launch(ctx context.Context, tconn *chrome.TestConn) (*ui.Node, error) {
+	err := apps.Launch(ctx, tconn, apps.PrintManagement.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to launch Print Management app")
+	}
+
+	err = ash.WaitForApp(ctx, tconn, apps.PrintManagement.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "Print Management app did not appear in shelf after launch")
+	}
+
+	rootnode, err := ui.FindWithTimeout(ctx, tconn, printManagementRootNodeParams, 20*time.Second)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find Print Management app")
+	}
+	return rootnode, nil
 }
