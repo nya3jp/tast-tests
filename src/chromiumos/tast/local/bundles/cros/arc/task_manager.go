74d88043a054a2683916082e1e38d07ad732ef1d
Renuga Nanjappan <rnanjappan@chromium.org>
2021-03-04 06:16:30 +0000

    tast tests: Add test to verify arc integration to task manager.
    The test installs an arc app and verifies that it shows up in the
    Task manager.
    
    BUG=b:184323848
    TEST=tast -verbose run <DUT> arc.TaskManager.vm
    
    Change-Id: I33ee1dc2ea6608f05ee4631bc0ff53454045406e

---
+++ src/chromiumos/tast/local/bundles/cros/arc/task_manager.go
@@ -0,0 +1,96 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TaskManager,
+		Desc:         "Checks Task Manager Integration with Arc",
+		Fixture:      "arcBooted",
+		Contacts:     []string{"rnanjappan@chromium.org", "cros-arc-te@google.com"},
+		Attr:         []string{"group:mainline", "informational", "group:arc-functional"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      arc.BootTimeout + 2*time.Minute,
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+	})
+}
+
+func TaskManager(ctx context.Context, s *testing.State) {
+
+	const (
+		// This is a plain hello world app.
+		apk = "ArcAppValidityTest.apk"
+		pkg = "org.chromium.arc.testapp.appvaliditytast"
+		cls = ".MainActivity"
+	)
+
+	a := s.FixtValue().(*arc.PreData).ARC
+	if err := a.Install(ctx, arc.APKPath(apk)); err != nil {
+		s.Fatal("Failed to install app: ", err)
+	}
+
+	cr := s.FixtValue().(*arc.PreData).Chrome
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	act, err := arc.NewActivity(a, pkg, cls)
+	if err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	defer act.Close()
+
+	s.Log("Starting app")
+	if err = act.Start(ctx, tconn); err != nil {
+		s.Fatal("Failed to start app: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	// This sleep is required for the ARC app to appear in first page to verify.
+	// TODO(b/184323848): Remove this when there is reliable way to wait for ARC entry.
+	if err := testing.Sleep(ctx, 30*time.Second); err != nil {
+		s.Fatal("Failed to wait: ", err)
+	}
+
+	taskManager := nodewith.Name("Task Manager").ClassName("TaskManagerView").First()
+	arcEntry := nodewith.Name("App: org.chromium.arc.testapp.appvaliditytast").Ancestor(taskManager)
+	ui := uiauto.New(tconn)
+	if err := uiauto.Combine("open Task Manager and look for ARC",
+		// Press Search + Esc to launch task manager.
+		kb.AccelAction("Search+Esc"),
+
+		// Check the ARC app in task manager.
+		ui.WaitUntilExists(arcEntry),
+
+		// Exit task manager.
+		kb.AccelAction("Ctrl+W"))(ctx); err != nil {
+		s.Fatal("Failed to close Task Manager: ", err)
+	}
+
+}
