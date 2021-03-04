20aa78d40a3f4239260c53133592aee95759d051
Gavin Williams <gavinwill@chromium.org>
2021-03-04 14:49:46 -0800

    tast-tests: Create E2E printing test
    This test tests the end to end printing flow. The following objectives
    are covered:
    1. Save a printer from the OS Settings page
    2. Open print preview and print to that printer
    3. Open Print Management app and verify a print job is created
    
    BUG=chromium:1183580
    TEST=printer.Print
    
    Change-Id: Ibcbd2cf489f41070b958edb86c0bffc00049c728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2737106
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Judy Wu <jinrongwu@google.com>
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Tested-by: Gavin Williams <gavinwill@chromium.org>

--- src/chromiumos/tast/local/chrome/ui/printmanagementapp/printmanagementapp.go
+++ src/chromiumos/tast/local/chrome/ui/printmanagementapp/printmanagementapp.go
@@ -6,16 +6,52 @@
 package printmanagementapp
 
 import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/apps"
-	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 )
 
-var printManagementRootNodeParams = ui.FindParams{
-	Name: apps.PrintManagement.Name,
-	Role: ui.RoleTypeWindow,
+// PrintManagementApp represents an instance of the Print Management app.
+type PrintManagementApp struct {
+	ui    *uiauto.Context
+	tconn *chrome.TestConn
 }
 
-var printManagementClearHistoryButton = ui.FindParams{
-	Name: "Clear all history",
-	Role: ui.RoleTypeButton,
+// printManagementHistoryLabel is used to find the label for the History
+// section.
+var printManagementHistoryLabel = nodewith.Name("History").Role(role.StaticText)
+
+// printManagementPrintJobEntry is used to find all print job entries.
+var printManagementPrintJobEntry = nodewith.ClassName("list-item flex-center")
+
+// Launch Print Management app via default method.
+func Launch(ctx context.Context, tconn *chrome.TestConn) (*PrintManagementApp, error) {
+	if err := apps.Launch(ctx, tconn, apps.PrintManagement.ID); err != nil {
+		return nil, errors.Wrap(err, "failed to launch Print Management app")
+	}
+
+	if err := ash.WaitForApp(ctx, tconn, apps.PrintManagement.ID); err != nil {
+		return nil, errors.Wrap(err, "Print Management app did not appear in shelf after launch")
+	}
+
+	return &PrintManagementApp{ui: uiauto.New(tconn), tconn: tconn}, nil
+}
+
+// VerifyHistoryLabel returns an action that verifies the History section of the
+// Print Management app is visible.
+func (p *PrintManagementApp) VerifyHistoryLabel() uiauto.Action {
+	return p.ui.WithTimeout(20 * time.Second).WaitUntilExists(printManagementHistoryLabel)
+}
+
+// VerifyPrintJob returns an action that verifies at least one print job is
+// visible in the Print Management app.
+func (p *PrintManagementApp) VerifyPrintJob() uiauto.Action {
+	return p.ui.WithTimeout(20 * time.Second).WaitUntilExists(printManagementPrintJobEntry)
 }
