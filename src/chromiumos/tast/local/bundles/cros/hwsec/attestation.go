55af843708753c05463376808208bfcea6cf3acb
Yi Chou <yich@google.com>
2021-03-08 23:06:15 +0800

    hwsec: Some style fixs
    BUG=b:175510138
    TEST=tast run $DUT hwsec.*
    
    Change-Id: I38726c1d8a747d931926bc6ed1a9139c79e51286

--- src/chromiumos/tast/local/bundles/cros/hwsec/attestation.go
+++ src/chromiumos/tast/local/bundles/cros/hwsec/attestation.go
@@ -40,6 +40,7 @@ func Attestation(ctx context.Context, s *testing.State) {
 	}
 	attestation := helper.AttestationClient()
 	cryptohome := helper.CryptohomeClient()
+	mountInfo := hwsec.NewCryptohomeMountInfo(r, cryptohome)
 	if err := helper.EnsureTPMIsReady(ctx, hwsec.DefaultTakingOwnershipTimeout); err != nil {
 		s.Fatal("Failed to ensure tpm readiness: ", err)
 	}
@@ -65,27 +66,22 @@ func Attestation(ctx context.Context, s *testing.State) {
 
 	const username = "test@crashwsec.bigr.name"
 
-	resetVault := func() {
-		if _, err := cryptohome.Unmount(ctx, username); err != nil {
-			s.Fatal("Failed to remove user vault: ", err)
-		}
-		if _, err := cryptohome.RemoveVault(ctx, username); err != nil {
-			s.Fatal("Failed to remove user vault: ", err)
-		}
-	}
-
 	s.Log("Resetting vault in case the cryptohome status is contaminated")
 	// Okay to call it even if the vault doesn't exist.
-	resetVault()
+	if err := mountInfo.CleanUpMount(ctx, username); err != nil {
+		s.Fatal("Failed to cleanup: ", err)
+	}
 
 	if err := cryptohome.MountVault(ctx, username, "testpass", "fake_label", true /* create */, hwsec.NewVaultConfig()); err != nil {
 		s.Fatal("Failed to create user vault: ", err)
 	}
 
-	defer func() {
+	defer func(ctx context.Context) {
 		s.Log("Resetting vault after use")
-		resetVault()
-	}()
+		if err := mountInfo.CleanUpMount(ctx, username); err != nil {
+			s.Error("Failed to cleanup: ", err)
+		}
+	}(ctx)
 
 	for _, param := range []struct {
 		name     string
