c5ba152368f0b59c13178a63f49822ee32816dd5
Tetsui Ohkubo <tetsui@chromium.org>
2021-03-08 15:46:51 +0900

    tast-tests: Click the input field twice for ARC R
    Since ARC R, a field should be focused first before it can trigger the
    virtual keyboard. With this CL, it clicks the input field first, wait
    until the field is focused, and then clicks the field again to show the
    virtual keyboard.
    
    BUG=b:179520931
    TEST=tast run rammus-arc-r-DUT arc.SoftInputMode.vm
    
    Change-Id: I0466b041afd950291603adbb24383f8b62077dbc

--- src/chromiumos/tast/local/bundles/cros/arc/soft_input_mode.go
+++ src/chromiumos/tast/local/bundles/cros/arc/soft_input_mode.go
@@ -148,13 +148,20 @@ func SoftInputMode(ctx context.Context, s *testing.State) {
 		const fieldID = "org.chromium.arc.testapp.softinputmode:id/text"
 		field := d.Object(ui.ID(fieldID))
 		if err := field.WaitForExists(ctx, 30*time.Second); err != nil {
-			s.Fatal("Failed to find field: ", err)
+			s.Fatal("Failed to find the field: ", err)
+		}
+		if err := field.Click(ctx); err != nil {
+			s.Fatal("Failed to click the field: ", err)
+		}
+		field = d.Object(ui.ID(fieldID), ui.Focused(true))
+		if err := field.WaitForExists(ctx, 30*time.Second); err != nil {
+			s.Fatal("Failed to focus the field: ", err)
 		}
 		if err := field.Click(ctx); err != nil {
 			s.Fatal("Failed to click the field: ", err)
 		}
 		if err := vkb.WaitLocationStable(ctx, tconn); err != nil {
-			s.Fatal("Failed to wait for the virtual keyboard to show: ", err)
+			s.Fatal("Failed to wait for the virtual keyboard to be shown: ", err)
 		}
 
 		if err := field.Exists(ctx); err != nil {
