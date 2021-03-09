37d87d57d472f2d32ea9282fe0483b262af42abd
Greg Edelston <gredelston@google.com>
2021-03-09 09:57:24 -0700

    firmware.Reporters: Move r.Now to new utils.go
    r.Now was defined in command_output.go. But that wasn't an appropriate
    home for it, since command_output.go was more intended for running
    arbitrary DUT commands and collecting output.
    
    This CL adds a new file, utils.go, for miscellaneous utility reporters;
    and it moves r.Now() to that file.
    
    BUG=b:174800291
    TEST=firmware.Eventlog
    
    Change-Id: I11b8f642f1b71339265d4087676457ecb9ac7a18

---
+++ src/chromiumos/tast/remote/firmware/reporters/utils.go
@@ -0,0 +1,24 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package reporters
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+// Now reports the output of the `date` command as a Go Time.
+func (r *Reporter) Now(ctx context.Context) (time.Time, error) {
+	const (
+		bashFormat = "%Y-%m-%d %H:%M:%S"
+		goFormat   = "2006-01-02 15:04:05"
+	)
+	res, err := r.CommandOutput(ctx, "date", fmt.Sprintf("+%s", bashFormat))
+	if err != nil {
+		return time.Time{}, err
+	}
+	return time.Parse(goFormat, res)
+}
