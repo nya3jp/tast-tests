ea9857d3cd40d71888011f1496e56052215bda2d
Jim Pollock <jmpollock@google.com>
2021-03-09 15:28:26 +0000

    platform: add nnapi hal driver tests
    BUG=b:182254380
    TEST=tast run localhost:8174 platform.NNAPIDriver.*
    
    Cq-Depend: chromium:2744147
    Change-Id: Id199598c433af285285fb0e6b31dc794a05ce813

---
+++ src/chromiumos/tast/local/bundles/cros/platform/nnapi_driver.go
@@ -0,0 +1,59 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:    NNAPIDriver,
+		Desc:    "Validates that the HAL driver conforms to the NNAPI specification",
+		Timeout: 10 * time.Minute,
+		Contacts: []string{
+			"jmpollock@google.com",
+			"slangley@google.com",
+			"chromeos-platform-ml@google.com",
+		},
+		Attr: []string{
+			"group:mainline", "informational",
+		},
+		SoftwareDeps: []string{"nnapi"},
+		Params: []testing.Param{{
+			Name: "cts",
+			// b/182264329: Fix SingleOperationTest.RESIZE_BILINEAR_V1_3
+			Val: []string{"cros_nnapi_cts", "--gtest_filter=-TestRandomGraph/SingleOperationTest.RESIZE_BILINEAR_V1_3/*"},
+		}, {
+			Name: "vts_1_0",
+			Val:  []string{"cros_nnapi_vts_1_0"},
+		}, {
+			Name: "vts_1_1",
+			Val:  []string{"cros_nnapi_vts_1_1"},
+		}, {
+			Name: "vts_1_2",
+			Val:  []string{"cros_nnapi_vts_1_2"},
+		}, {
+			Name: "vts_1_3",
+			Val:  []string{"cros_nnapi_vts_1_3"},
+		}},
+	})
+}
+
+func NNAPIDriver(ctx context.Context, s *testing.State) {
+	cmdArgs := s.Param().([]string)
+
+	cmd := testexec.CommandContext(ctx, cmdArgs[0], cmdArgs[1:]...)
+	cmd.Env = append(os.Environ(), "ANDROID_LOG_TAGS=*:f")
+
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		s.Error("Failed to run test suite: ", err)
+	}
+}
