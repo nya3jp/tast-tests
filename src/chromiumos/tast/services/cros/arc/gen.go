ccebf1dfa6d6ab8b08fdf8f7aebe2ad7723d66f4
Yury Khmel <khmel@chromium.org>
2021-03-09 23:22:52 +0000

    arc: Speculative fix for kevin failurs in DataCollector.
    This tries to use pooled accounts in order to reduce loading to GAIA
    during the data collector execution. Long term improvements would be
    discarding few reduntat flows when guest ureadahead generation is
    ready.
    
    BUG=b:182265039
    TEST=tast run DUT arc.DataCollector.local
    
    Change-Id: I4ed46d8ed83104d554407ac1bb1ac4b00cb3cd7e

--- src/chromiumos/tast/services/cros/arc/gen.go
+++ src/chromiumos/tast/services/cros/arc/gen.go
@@ -4,6 +4,7 @@
 
 //go:generate protoc -I . --go_out=plugins=grpc:../../../../.. perf_boot_service.proto
 //go:generate protoc -I . --go_out=plugins=grpc:../../../../.. gmscore_cache_service.proto
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. ureadahead_pack_service.proto
 
 package arc
 
