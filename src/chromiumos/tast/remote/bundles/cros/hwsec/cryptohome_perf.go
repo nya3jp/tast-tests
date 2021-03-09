02b93a0afbdb5936305b198d85a35affdebb8462
Yi Chou <yich@google.com>
2021-03-09 22:09:15 +0800

    hwsec: Cleanup shall-use-userdataauth in CryptohomePerf
    We don't need this switch anymore after we landed UserDataAuth.
    
    BUG=b:182255746, b:177880754
    TEST=tast run $DUT hwsec.CryptohomePerf
    
    Cq-Depend: chromium:2744973
    Change-Id: I276a774f12a8914de5ea2460d994aeaf2adcf44f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2745050
    Commit-Queue: Yi Chou <yich@google.com>
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: John L Chen <zuan@chromium.org>

--- src/chromiumos/tast/remote/bundles/cros/hwsec/cryptohome_perf.go
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/cryptohome_perf.go
@@ -11,7 +11,6 @@ import (
 	"strings"
 	"time"
 
-	"chromiumos/tast/common/hwsec"
 	"chromiumos/tast/common/perf"
 	"chromiumos/tast/errors"
 	hwsecremote "chromiumos/tast/remote/hwsec"
@@ -20,8 +19,7 @@ import (
 
 const (
 	resultDir                = "/tmp"
-	cryptohomeDbusTimeFile   = "cryptohome_dbus_time"
-	userdataauthDbusTimeFile = "userdataauth_dbus_time"
+	userDataAuthDbusTimeFile = "userdataauth_dbus_time"
 )
 
 func init() {
@@ -37,32 +35,14 @@ func init() {
 			"cros-hwsec@chromium.org",
 		},
 		SoftwareDeps: []string{"reboot", "tpm"},
-		ServiceDeps:  []string{"tast.cros.hwsec.AttestationDBusService"},
 	})
 }
 
-// waitUntilCryptohomeInit is a helper function to wait until cryptohome initialized.
-func waitUntilCryptohomeInit(ctx context.Context, remote *hwsecremote.CmdRunnerRemote) error {
+// waitUntilUserDataAuthInit is a helper function to wait until userdataauth initialized.
+func waitUntilUserDataAuthInit(ctx context.Context, remote *hwsecremote.CmdRunnerRemote) error {
 	return testing.Poll(ctx, func(context.Context) error {
-		file := filepath.Join(resultDir, cryptohomeDbusTimeFile)
-		_, err := remote.Run(ctx, "stat", file)
-		if err != nil {
-			return errors.Wrap(err, "stat file failed")
-		}
-
-		return nil
-	}, &testing.PollOptions{
-		Timeout:  60 * time.Second,
-		Interval: time.Second,
-	})
-}
-
-// waitUntilUserdataauthInit is a helper function to wait until userdataauth initialized.
-func waitUntilUserdataauthInit(ctx context.Context, remote *hwsecremote.CmdRunnerRemote) error {
-	return testing.Poll(ctx, func(context.Context) error {
-		file := filepath.Join(resultDir, userdataauthDbusTimeFile)
-		_, err := remote.Run(ctx, "stat", file)
-		if err != nil {
+		file := filepath.Join(resultDir, userDataAuthDbusTimeFile)
+		if _, err := remote.Run(ctx, "stat", file); err != nil {
 			return errors.Wrap(err, "stat file failed")
 		}
 
@@ -93,70 +73,24 @@ func getTime(ctx context.Context, remote *hwsecremote.CmdRunnerRemote, eventName
 	return stamp, nil
 }
 
-func clearOwnership(ctx context.Context, s *testing.State) {
+func CryptohomePerf(ctx context.Context, s *testing.State) {
 	r := hwsecremote.NewCmdRunner(s.DUT())
-
-	helper, err := hwsecremote.NewFullHelper(r, s.DUT(), s.RPCHint())
+	helper, err := hwsecremote.NewHelper(r, s.DUT())
 	if err != nil {
 		s.Fatal("Helper creation error: ", err)
 	}
 
-	attestation := helper.AttestationClient()
-
-	s.Log("Start resetting TPM if needed")
 	if err := helper.EnsureTPMIsResetAndPowerwash(ctx); err != nil {
 		s.Fatal("Failed to ensure resetting TPM: ", err)
 	}
-	s.Log("TPM is confirmed to be reset")
 
-	if result, err := attestation.IsPreparedForEnrollment(ctx); err != nil {
-		s.Fatal("Cannot check if enrollment preparation is reset: ", err)
-	} else if result {
-		s.Fatal("Enrollment preparation is not reset after clearing ownership")
+	if err := waitUntilUserDataAuthInit(ctx, r); err != nil {
+		s.Fatal("Failed to wait userdataauth init: ", err)
 	}
-	s.Log("Enrolling with TPM not ready")
-	if _, err := attestation.CreateEnrollRequest(ctx, hwsec.DefaultPCA); err == nil {
-		s.Fatal("Enrollment should not happen w/o getting prepared")
-	}
-}
 
-func CryptohomePerf(ctx context.Context, s *testing.State) {
-	r := hwsecremote.NewCmdRunner(s.DUT())
-
-	clearOwnership(ctx, s)
-
-	err := waitUntilCryptohomeInit(ctx, r)
+	userDataAuthDBusTime, err := getTime(ctx, r, userDataAuthDbusTimeFile)
 	if err != nil {
-		s.Fatal("Failed to wait cryptohome init: ", err)
-	}
-
-	enableUserdataauth := false
-	_, err = r.Run(ctx, "/usr/libexec/cryptohome/shall-use-userdataauth.sh")
-	if err == nil {
-		enableUserdataauth = true
-	}
-
-	if enableUserdataauth {
-		err = waitUntilUserdataauthInit(ctx, r)
-		if err != nil {
-			s.Fatal("Failed to wait userdataauth init: ", err)
-		}
-	}
-
-	cryptohomeDbusTime, err := getTime(ctx, r, cryptohomeDbusTimeFile)
-	if err != nil {
-		s.Fatal("Failed to parse cryptohome D-Bus startup time: ", err)
-	}
-
-	s.Log("start-up time of cryptohome D-Bus: ", cryptohomeDbusTime)
-
-	var userdataauthDbusTime float64
-	if enableUserdataauth {
-		userdataauthDbusTime, err = getTime(ctx, r, userdataauthDbusTimeFile)
-		if err != nil {
-			s.Fatal("Failed to parse userdataauth D-Bus startup time: ", err)
-		}
-		s.Log("start-up time of userdataauth D-Bus: ", userdataauthDbusTime)
+		s.Fatal("Failed to parse userdataauth D-Bus startup time: ", err)
 	}
 
 	// Record the perf measurements.
@@ -164,21 +98,11 @@ func CryptohomePerf(ctx context.Context, s *testing.State) {
 
 	value.Set(perf.Metric{
 		Name:      "crpytohome_start_time",
-		Variant:   "cryptohome_dbus",
+		Variant:   "userdataauth_dbus",
 		Unit:      "s",
 		Direction: perf.SmallerIsBetter,
 		Multiple:  false,
-	}, cryptohomeDbusTime)
-
-	if enableUserdataauth {
-		value.Set(perf.Metric{
-			Name:      "crpytohome_start_time",
-			Variant:   "userdataauth_dbus",
-			Unit:      "s",
-			Direction: perf.SmallerIsBetter,
-			Multiple:  false,
-		}, userdataauthDbusTime)
-	}
+	}, userDataAuthDBusTime)
 
 	value.Save(s.OutDir())
 }
