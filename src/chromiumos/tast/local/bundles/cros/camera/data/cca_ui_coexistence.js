97f9a8467ddf9c6c34fa040255b545dfc900dc09
Wei Lee <wtlee@google.com>
2021-03-10 16:31:23 +0800

    camera: Add CCAUICoexistence test
    Adding this test to verify it won't crash when opening CCA and web page
    with camera on at the same time.
    
    BUG=b:179330541
    TEST=tast run [DUT] camera.CCAUICoexistence
    
    Change-Id: Ic040cbfc615e619054a405a073ac434a24f34f59

---
+++ src/chromiumos/tast/local/bundles/cros/camera/data/cca_ui_coexistence.js
@@ -0,0 +1,29 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @typedef {{
+ *   hasEnded: boolean,
+ * }}
+ */
+let TrackState;
+
+// Namespace for calling from Tast.
+window.Tast = {
+  /**
+   * Starts a camera stream and returns a TrackState object to track the
+   * stream status.
+   * @return {!Promise<!TrackState>}
+   */
+  async startStream() {
+    const stream =
+        await navigator.mediaDevices.getUserMedia({audio: false, video: true});
+    const track = stream.getVideoTracks()[0];
+    const trackState = {hasEnded: false};
+    track.addEventListener('ended', () => {
+      trackState.hasEnded = true;
+    });
+    return trackState;
+  }
+};
