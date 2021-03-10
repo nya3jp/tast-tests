1e7f388e12719f3b5de0b5f7e40bc4ac5fb787cb
Oleh Lamzin <lamzin@google.com>
2021-03-10 19:29:58 +0100

    policy: Revert logic for DeveloperToolsAvailability
    Pressing all possible shortcuts to open/close DevTools is much faster
    way to test DeveloperToolsAvailability, but less stable. To back to the
    old slower approach where each shortcut is pressed on the new tab.
    
    This CL mostly reverts CL:2567703, but also increases test timeout and
    makes test cases more isolated.
    
    BUG=chromium:1153641
    BUG=chromium:1125548
    TEST=Run test on
           - arcada (fast local)
           - orco (slow device from lab, test never passed on this device)
         using command:
           tast run IP policy.DeveloperToolsAvailability
    
    Change-Id: I28bac2052cd05b13d449f6447be047657c4c9e6a

--- src/chromiumos/tast/local/bundles/cros/policy/developer_tools_availability.go
+++ src/chromiumos/tast/local/bundles/cros/policy/developer_tools_availability.go
@@ -30,6 +30,7 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline", "informational"},
 		Pre:          pre.User,
+		Timeout:      3 * time.Minute,
 	})
 }
 
@@ -76,18 +77,6 @@ func DeveloperToolsAvailability(ctx context.Context, s *testing.State) {
 		},
 	} {
 		s.Run(ctx, tc.name, func(ctx context.Context, s *testing.State) {
-			defer func(ctx context.Context) {
-				// Attempt to close DevTools and reload page.
-				if err := keyboard.Accel(ctx, "F12"); err != nil {
-					s.Fatal("Failed to press F12: ", err)
-				}
-				if err := keyboard.Accel(ctx, "Ctrl+R"); err != nil {
-					s.Fatal("Failed to press Ctrl+R: ", err)
-				}
-			}(ctx)
-
-			defer faillog.DumpUITreeOnErrorToFile(ctx, s.OutDir(), s.HasError, tconn, fmt.Sprintf("ui_tree_%s.txt", tc.name))
-
 			// Perform cleanup.
 			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
 				s.Fatal("Failed to clean up: ", err)
@@ -98,59 +87,53 @@ func DeveloperToolsAvailability(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to update policies: ", err)
 			}
 
-			// Open new tab and navigate to chrome://user-actions.
-			// Here we cannot use cr.Conn, because Chrome DevTools Protocol
-			// relies on DevTools.
-			if err := keyboard.Accel(ctx, "Ctrl+T"); err != nil {
-				s.Fatal("Failed to press Ctrl+T: ", err)
-			}
-			if err := keyboard.Type(ctx, "chrome://user-actions\n"); err != nil {
-				s.Fatal("Failed to type chrome://user-actions: ", err)
-			}
-
-			// The current hotkey order eventually leaves DevTools open. We have
-			// to go through all hotkeys to verify that none of them can open
-			// DevTools if it isn't allowed by policy.
 			for _, keys := range []string{
-				"Ctrl+Shift+C", // open
-				"Ctrl+Shift+I", // close
-				"Ctrl+Shift+J", // open
-				"F12",          // close
-				"F12",          // open
+				"Ctrl+Shift+C",
+				"Ctrl+Shift+I",
+				"F12",
+				"Ctrl+Shift+J",
 			} {
-				// Press keys combination to open DevTools.
-				if err := keyboard.Accel(ctx, keys); err != nil {
-					s.Fatalf("Failed to press %s: %v", keys, err)
-				}
+				s.Run(ctx, keys, func(ctx context.Context, s *testing.State) {
+					defer faillog.DumpUITreeOnErrorToFile(ctx, s.OutDir(), s.HasError, tconn, fmt.Sprintf("ui_tree_%s_%s.txt", tc.name, keys))
+
+					// Open new tab and navigate to chrome://user-actions.
+					// Here we cannot use cr.Conn, because Chrome DevTools Protocol
+					// relies on DevTools.
+					if err := keyboard.Accel(ctx, "Ctrl+T"); err != nil {
+						s.Fatal("Failed to press Ctrl+T: ", err)
+					}
+					if err := keyboard.Type(ctx, "chrome://user-actions\n"); err != nil {
+						s.Fatal("Failed to type chrome://user-actions: ", err)
+					}
+
+					// Check that we have access to chrome://user-actions accessability tree.
+					if err := ui.WaitUntilExists(ctx, tconn, ui.FindParams{
+						Name: "User Action",
+						Role: ui.RoleTypeColumnHeader,
+					}, 5*time.Second); err != nil {
+						s.Fatal("Failed to wait for page nodes: ", err)
+					}
+
+					// Press keys combination to open DevTools.
+					if err := keyboard.Accel(ctx, keys); err != nil {
+						s.Fatalf("Failed to press %s: %v", keys, err)
+					}
+
+					timeout := 5 * time.Second
+					elementsParams := ui.FindParams{Name: "Elements", Role: ui.RoleTypeTab}
+
+					switch tc.wantAllowed {
+					case false:
+						if err := policyutil.VerifyNotExists(ctx, tconn, elementsParams, timeout); err != nil {
+							s.Errorf("Failed to verify that DevTools are not available after %s: %s", timeout, err)
+						}
+					case true:
+						if err := ui.WaitUntilExists(ctx, tconn, elementsParams, timeout); err != nil {
+							s.Error("Failed to wait for DevTools: ", err)
+						}
+					}
+				})
 			}
-
-			// Check that we have access to chrome://user-actions accessability tree.
-			if err := ui.WaitUntilExists(ctx, tconn, ui.FindParams{
-				Name: "User Action",
-				Role: ui.RoleTypeColumnHeader,
-			}, 5*time.Second); err != nil {
-				s.Fatal("Failed to wait for page nodes: ", err)
-			}
-
-			elementsParams := ui.FindParams{Name: "Elements", Role: ui.RoleTypeTab}
-
-			switch tc.wantAllowed {
-			case false:
-				s.Log("Sleep to give a chance for DevTools to appear if policy does not work correctly")
-				if err := testing.Sleep(ctx, 5*time.Second); err != nil {
-					s.Fatal("Failed to sleep: ", err)
-				}
-				if exists, err := ui.Exists(ctx, tconn, elementsParams); err != nil {
-					s.Fatal("Failed to check whether DevTools are available: ", err)
-				} else if exists {
-					s.Error("Unexpected DevTools availability: get allowed; want disallowed")
-				}
-			case true:
-				if err := ui.WaitUntilExists(ctx, tconn, elementsParams, 5*time.Second); err != nil {
-					s.Error("Failed to wait for DevTools: ", err)
-				}
-			}
-
 		})
 	}
 }
