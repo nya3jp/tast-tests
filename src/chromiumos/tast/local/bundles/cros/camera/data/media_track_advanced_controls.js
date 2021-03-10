0b4effb13042e8c7bd716bf59dc38fb41e45c0f9
Moja Hsu <mojahsu@google.com>
2021-03-10 16:42:06 +0800

    camera: Add media track advanced control test
    BUG=b:151048287
    TEST=Test it on kukui and morphius
    
    Change-Id: I554225e8c2cec07e6e2771b312c3ec14f100fb3c

---
+++ src/chromiumos/tast/local/bundles/cros/camera/data/media_track_advanced_controls.js
@@ -0,0 +1,99 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get camera device info.
+ * @return {!Promise<!Array<!MediaDeviceInfo>>}
+ */
+async function getCameras() {
+  return (await navigator.mediaDevices.enumerateDevices())
+    .filter((device) => device.kind === 'videoinput')
+    .map(({deviceId, label}) => ({deviceId, label}))
+}
+
+/**
+ * @return {!HTMLVideoElement}
+ */
+function getPreviewVideo() {
+  return document.querySelector('#preview-video');
+}
+
+/**
+ * @return {!HTMLVideoElement}
+ */
+function getPreviewVideoWithStream() {
+  const video = getPreviewVideo();
+  if (video.srcObject === undefined) {
+    throw new Error('Stream is undefined');
+  }
+  return video;
+}
+
+/**
+ * Opens preview from specified camera.
+ *
+ * @param {deviceId}
+ * @throws {!Error}
+ */
+async function openPreview(deviceId) {
+  var video = getPreviewVideo();
+  const constraints = {
+    'audio': false,
+    'video': {deviceId},
+  };
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    video.srcObject = stream;
+    await video.play();
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Closes preview.
+ */
+function closePreview() {
+  const video = getPreviewVideoWithStream();
+  const stream = video.srcObject;
+  for (const track of stream.getTracks()) {
+    track.stop();
+  }
+
+  video.removeAttribute('srcObject');
+  video.load();
+}
+
+/**
+ * Gets media track capabilities of current active camera device.
+ * @return {!Promise<!MediaTrackCapabilities>}
+ */
+async function getMediaTrackCapabilities() {
+  const video = getPreviewVideoWithStream();
+  const track = video.srcObject.getVideoTracks()[0];
+  const capabilities = await track.getCapabilities();
+  return capabilities;
+}
+
+/**
+ * Gets media track settings of current active camera device.
+ * @return {!MediaTrackSettings}
+ */
+function getMediaTrackSettings() {
+  const video = getPreviewVideoWithStream();
+  const track = video.srcObject.getVideoTracks()[0];
+  const settings = track.getSettings();
+  return settings;
+}
+
+/**
+ * Applies media track constraints of current active camera device.
+ * @param {string} constraints
+ * @return  {!Promise}
+ */
+async function applyMediaTrackConstraints(constraints) {
+  const video = getPreviewVideoWithStream();
+  const track = video.srcObject.getVideoTracks()[0];
+  await track.applyConstraints(constraints);
+}
