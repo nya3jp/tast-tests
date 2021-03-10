a501fc693ab79f7bf496b3a859113b1f74893fff
Gwendal Grignou <gwendal@chromium.org>
2021-03-10 15:58:14 -0800

    hardware: iioservice: Do not test on activity or ring sensors
    Activity sensors are not producing event at a given frequency, do not
    use them to test iioservice.
    Light can also be a problem, exclude it for hard test.
    Fail if cros-ec-sensor-ring is present, it indicates the kernel has not
    been compiled properly.
    
    BUG=b:180449911
    TEST=Check that tast test hardware.SensorIioServiceHard and
    hardware.SensorIioService passes.
    
    Change-Id: Ibeff0286cb212c8a647613d787107b48807546e0

--- src/chromiumos/tast/local/bundles/cros/hardware/sensor_iioservice_hard.go
+++ src/chromiumos/tast/local/bundles/cros/hardware/sensor_iioservice_hard.go
@@ -49,6 +49,12 @@ func runSingleClient(ctx context.Context, s *testing.State, sn *iio.Sensor, i in
 		channels += " accel_x accel_y accel_z"
 	} else if sn.Name == iio.Gyro {
 		channels += " anglvel_x anglvel_y anglvel_z"
+	} else if sn.Name == iio.Mag {
+		channels += " magn_x magn_y magn_z"
+	} else if sn.Name == iio.Ring {
+		return errors.New("Kernel must be compiled with USE=iioservice")
+	} else {
+		return nil
 	}
 	cmd := testexec.CommandContext(
 		ctx, "iioservice_simpleclient", fmt.Sprintf("--frequency=%d.%03d",
