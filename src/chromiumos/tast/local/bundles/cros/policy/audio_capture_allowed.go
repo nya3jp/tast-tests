857908352ec448b7835c81e8a4975cad3023c75a
alexanderhartl <alexanderhartl@google.com>
2021-03-11 15:18:51 +0000

    Add policy.AudioCaptureAllowed tast test
    Add the policy.AudioCaptureAllowed tast test, which checks that
    whether a website is allowed to get access to the microphone
    based on the policy value.
    
    BUG=chromium:1187121
    TEST=tast run <DUT> policy.AudioCaptureAllowed
    
    Change-Id: I67665d9ce741b2224d4018d83716abfe4f80c49f

---
+++ src/chromiumos/tast/local/bundles/cros/policy/audio_capture_allowed.go
@@ -0,0 +1,139 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AudioCaptureAllowed,
+		Desc: "Checking if audio capture is allowed on websites or not, depending on the value of the policy",
+		Contacts: []string{
+			"alexanderhartl@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      "chromePolicyLoggedIn",
+		Data:         []string{"audio_capture_allowed.html"},
+	})
+}
+
+// AudioCaptureAllowed tests the AudioCaptureAllowed policy.
+func AudioCaptureAllowed(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+	fdms := s.FixtValue().(*fixtures.FixtData).FakeDMS
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	// Connect to Test API to use it with the UI library.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	for _, param := range []struct {
+		name      string
+		wantAsk   bool                        // wantAsk states whether a dialog to ask for permission should appear or not.
+		wantAllow bool                        // wantAllow states whether access to the microphone is allowed.
+		value     *policy.AudioCaptureAllowed // value is the value of the policy.
+	}{
+		{
+			name:      "unset",
+			wantAsk:   true,
+			wantAllow: true,
+			value:     &policy.AudioCaptureAllowed{Stat: policy.StatusUnset},
+		},
+		{
+			name:      "allow",
+			wantAsk:   false,
+			wantAllow: true,
+			value:     &policy.AudioCaptureAllowed{Val: true},
+		},
+		{
+			name:      "deny",
+			wantAsk:   false,
+			wantAllow: false,
+			value:     &policy.AudioCaptureAllowed{Val: false},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			defer faillog.DumpUITreeOnErrorToFile(ctx, s.OutDir(), s.HasError, tconn, "ui_tree_"+param.name+".txt")
+
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.value}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			// Open a website.
+			conn, err := cr.NewConn(ctx, server.URL+"/audio_capture_allowed.html")
+			if err != nil {
+				s.Fatal("Failed to open website: ", err)
+			}
+			defer conn.Close()
+
+			ui := uiauto.New(tconn)
+
+			// Start a go routine before requesting the microphone as the Eval()
+			// function will block when a dialog to ask for permission appears.
+			// The routine will then click the allow button in the dialog.
+			ch := make(chan error, 1)
+			go func() {
+				allowButton := nodewith.Name("Allow").Role(role.Button)
+
+				if err = ui.WaitUntilExists(allowButton)(ctx); err != nil {
+					if param.wantAsk {
+						s.Error("Allow button not found: ", err)
+					}
+					ch <- nil
+					return
+				}
+
+				if !param.wantAsk {
+					s.Error("Unexpected dialogue to ask for microphone permission found")
+				}
+
+				if err := ui.LeftClickUntil(allowButton, ui.Gone(allowButton))(ctx); err != nil {
+					s.Fatal("Failed to click the Allow button: ", err)
+				}
+
+				ch <- nil
+			}()
+
+			// Try to access the microphone.
+			var allowed bool
+			if err := conn.Eval(ctx, "requestMicrophone()", &allowed); err != nil {
+				s.Fatal("Failed to request microphone: ", err)
+			}
+			if err := <-ch; err != nil {
+				s.Error("Failed to execute the routine to click the allow button: ", err)
+			}
+
+			// Check if we were allowed to use the microphone.
+			if allowed != param.wantAllow {
+				s.Errorf("Unexpected access to microphone received: got %v; want %v", allowed, param.wantAllow)
+			}
+		})
+	}
+}
