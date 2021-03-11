65c19aeb1fa9aa3556028c8fcb9fb08d4149df46
Harvey Yang <chenghaoyang@chromium.org>
2021-03-11 16:11:59 +0800

    tast-tests: Make hardware.SensorIioservice(Hard) depend on "iioservice".
    Make hardware.SensorIioservice(Hard) depend on "iioservice" feature so
    it will be skipped on DUTs that don't have CrOS IIO Service running.
    It also makes hardware.SensorRing depend on "no_iioservice", as it won't
    be needed when CrOS IIO Service is running.
    
    BUG=b:182441449
    TEST=none
    
    Cq-Depend: chromium:2751498
    Change-Id: I0f9549bc7c105e4effcd8470831ae881ffcc1efd

--- src/chromiumos/tast/local/bundles/cros/hardware/sensor_ring.go
+++ src/chromiumos/tast/local/bundles/cros/hardware/sensor_ring.go
@@ -22,7 +22,8 @@ func init() {
 			"mathewk@chromium.org", // Test author
 			"chromeos-sensors-eng@google.com",
 		},
-		Attr: []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"no_iioservice"},
 	})
 }
 
