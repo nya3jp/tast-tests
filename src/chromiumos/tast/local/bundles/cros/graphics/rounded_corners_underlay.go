e06e8d6923394f447881aa6e05562757d0082907
Avery Musbach <amusbach@chromium.org>
2021-03-10 16:38:10 -0800

    tast-tests: Add test of rounded corners implemented as hardware underlay
    This is a Tast test for the main functionality of
    https://chromium-review.googlesource.com/c/chromium/src/+/2719610
    
    BUG=chromium:1107460
    TEST=graphics.RoundedCornersUnderlay
    
    Change-Id: I03144a6baaad059c85b67d7c960a68bb74dfe848

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/rounded_corners_underlay.go
@@ -0,0 +1,85 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/chrome/webutil"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RoundedCornersUnderlay,
+		Desc:         "Verifies that rounded corners are implemented with a hardware underlay",
+		Contacts:     []string{"amusbach@chromium.org", "oshima@chromium.org", "chromeos-wmp@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		HardwareDeps: hwdep.D(hwdep.SupportsNV12Overlays()),
+		Data:         []string{"bear-320x240.h264.mp4", "video_with_rounded_corners.html"},
+		Fixture:      "chromeLoggedIn",
+		Timeout:      90 * time.Second,
+	})
+}
+
+func RoundedCornersUnderlay(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	srv := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer srv.Close()
+
+	conn, err := cr.NewConn(ctx, srv.URL+"/video_with_rounded_corners.html")
+	if err != nil {
+		s.Fatal("Failed to load video_with_rounded_corners.html: ", err)
+	}
+	defer conn.Close()
+
+	if err := webutil.WaitForQuiescence(ctx, conn, 10*time.Second); err != nil {
+		s.Fatal("Failed to wait for video_with_rounded_corners.html to achieve quiescence: ", err)
+	}
+
+	hists, err := metrics.Run(ctx, tconn, func(ctx context.Context) error {
+		if err := testing.Sleep(ctx, time.Second); err != nil {
+			return errors.Wrap(err, "failed to wait a second")
+		}
+		return nil
+	}, "Viz.DisplayCompositor.OverlayStrategy")
+	if err != nil {
+		s.Fatal("Failed to record histogram Viz.DisplayCompositor.OverlayStrategy: ", err)
+	}
+
+	hist := hists[0]
+	if len(hist.Buckets) == 0 {
+		s.Fatal("Got no overlay strategy data")
+	}
+
+	var framesWithUnderlay int64
+	for _, bucket := range hist.Buckets {
+		// bucket.Min will be from enum OverlayStrategies as defined
+		// in tools/metrics/histograms/enums.xml in the chromium
+		// code base. Here we check for 4 which is "Underlay."
+		if bucket.Min == 4 {
+			framesWithUnderlay = bucket.Count
+			break
+		}
+	}
+
+	if totalFrames := hist.TotalCount(); framesWithUnderlay != totalFrames {
+		s.Errorf("Unexpected usage of underlay: got on only %d of %d frames; want on all frames", framesWithUnderlay, totalFrames)
+	}
+}
