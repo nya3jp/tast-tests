ab9c8e964a2713ae4750d40bf55c9a27281a667e
Jane Yang <jane.yang@cienet.com>
2021-03-12 10:10:47 -0800

    cuj: Google meet service
    The google meet service will be used by a remote test to conduct google
    meet testing.
    Some common structs and interfaces can be used to implement other video
    conf testing, like zoom.
    
    BUG=None
    TEST=./fast_build.sh -T
    
    Change-Id: Id80cd003c82e8a5e901fdb86a92a4c3c31dce593

---
+++ src/chromiumos/tast/local/bundles/cros/ui/conference/run.go
@@ -0,0 +1,144 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package conference
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/graphics"
+	"chromiumos/tast/local/power/setup"
+	"chromiumos/tast/testing"
+)
+
+// Cleanup releases the resources which the case used.
+type Cleanup func(context.Context) error
+
+// Prepare prepares conference room link before testing.
+type Prepare func(context.Context) (string, Cleanup, error)
+
+// Run runs the specified user scenario in conference room with different CUJ tiers.
+func Run(ctx context.Context, cr *chrome.Chrome, conf Conference, prepare Prepare, tier, outDir string, tabletMode, extendedDisplay bool) error {
+	inviteLink, cleanup, err := prepare(ctx)
+	if err != nil {
+		return err
+	}
+	if inviteLink == "" {
+		return errors.New("failed to get inviteLink")
+	}
+	defer cleanup(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to the test API connection")
+	}
+	defer tconn.Close()
+
+	testing.ContextLog(ctx, "Start to get browser start time")
+	browserStartTime, err := cuj.GetBrowserStartTime(ctx, cr, tconn, tabletMode)
+	if err != nil {
+		return errors.Wrap(err, "failed to get browser start time")
+	}
+
+	if err := conf.Join(ctx, inviteLink); err != nil {
+		return err
+	}
+
+	defer func() {
+		conf.End(ctx)
+	}()
+
+	setBatteryNormal, err := setup.SetBatteryDischarge(ctx, 50)
+	if err != nil {
+		return errors.Wrap(err, "failed to set battery discharge")
+	}
+	defer setBatteryNormal(ctx)
+
+	testing.ContextLog(ctx, "Start recording actions")
+
+	recorder, err := cuj.NewRecorder(ctx, tconn, cuj.MetricConfigs()...)
+	if err != nil {
+		return errors.Wrap(err, "failed to create the recorder")
+	}
+	defer recorder.Close(ctx)
+
+	pv := perf.NewValues()
+	if err := recorder.Run(ctx, func(ctx context.Context) error {
+		errc := make(chan error, 1)
+		go func() {
+			errc <- graphics.MeasureGPUCounters(ctx, 10*time.Second, pv)
+		}()
+
+		// Basic
+		if err := conf.SwitchTabs(ctx); err != nil {
+			return err
+		}
+
+		if err := conf.VideoAudioControl(ctx); err != nil {
+			return err
+		}
+
+		if err := conf.ChangeLayout(ctx); err != nil {
+			return err
+		}
+
+		// Plus and premium tier.
+		if tier == "plus" || tier == "premium" {
+			if err := newGoogleSlides(ctx, cr, extendedDisplay); err != nil {
+				return err
+			}
+			if extendedDisplay {
+				if err := conf.ExtendedDisplayPresenting(ctx, tabletMode); err != nil {
+					return err
+				}
+			} else {
+				if err := conf.PresentSlide(ctx); err != nil {
+					return err
+				}
+				if err := conf.StopPresenting(ctx); err != nil {
+					return err
+				}
+			}
+		}
+
+		// Premium tier.
+		if tier == "premium" {
+			if err := conf.BackgroundBlurring(ctx); err != nil {
+				return err
+			}
+		}
+
+		if err := <-errc; err != nil {
+			return errors.Wrap(err, "failed to collect GPU counters")
+		}
+		return nil
+	}); err != nil {
+		return errors.Wrap(err, "failed to conduct the recorder task")
+	}
+
+	if err := recorder.Record(ctx, pv); err != nil {
+		return errors.Wrap(err, "failed to record the data")
+	}
+
+	pv.Set(perf.Metric{
+		Name:      "Browser.StartTime",
+		Unit:      "ms",
+		Direction: perf.SmallerIsBetter,
+	}, float64(browserStartTime.Milliseconds()))
+
+	if err := pv.Save(outDir); err != nil {
+		return errors.Wrap(err, "failed to save perf data")
+	}
+
+	if err := recorder.SaveHistograms(outDir); err != nil {
+		return errors.Wrap(err, "failed to save histogram raw data")
+	}
+
+	return nil
+}
