328133e0ce358cf99292eb1641af551117c2a8c3
Jane Yang <jane.yang@cienet.com>
2021-03-12 10:19:46 -0800

    cuj: Google meet CUJ test case
    A remote test calling Google meet service to do the google meet testing.
    Different sizes of conference room are designed as different sub tests.
    
    To run Google Meet CUJ, an external static room server has to be set up
    and the test will conntect to join static meeting rooms.
    
    BUG=None
    TEST=tast run -var=ui.cuj_username=x -var=ui.cuj_password=x -var=ui.meet_account=x -var=ui.meet_url=x $DUT ui.GoogleMeetCUJ.*
    
    Change-Id: I9e433cb0e99c7e41fa65c70bce19d25b94405bdd

---
+++ src/chromiumos/tast/remote/bundles/cros/ui/google_meet_cuj.go
@@ -0,0 +1,99 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/remote/bundles/cros/ui/conference"
+	"chromiumos/tast/rpc"
+	pb "chromiumos/tast/services/cros/ui"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GoogleMeetCUJ,
+		Desc:         "Host a Google Meet video conference and do presentation to participants",
+		Contacts:     []string{"jane.yang@cienet.com", "xliu@cienet.com"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		ServiceDeps: []string{
+			"tast.cros.ui.ConferenceService",
+		},
+		Params: []testing.Param{
+			{
+				Name:    "basic_two",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier: "basic",
+					Size: conference.TwoRoomSize,
+				},
+			}, {
+				Name:    "basic_small",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier: "basic",
+					Size: conference.SmallRoomSize,
+				},
+			}, {
+				Name:    "basic_large",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier: "basic",
+					Size: conference.LargeRoomSize,
+				},
+			}, {
+				Name:    "basic_class",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier: "basic",
+					Size: conference.ClassRoomSize,
+				},
+			}, {
+				Name:    "plus_small",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier: "plus",
+					Size: conference.SmallRoomSize,
+				},
+			}, {
+				Name:    "plus_class",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier: "plus",
+					Size: conference.ClassRoomSize,
+				},
+			}, {
+				Name:    "premium_large",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier: "premium",
+					Size: conference.LargeRoomSize,
+				},
+			},
+		},
+	})
+}
+
+func GoogleMeetCUJ(ctx context.Context, s *testing.State) {
+	param := s.Param().(conference.TestParameters)
+
+	dut := s.DUT()
+	c, err := rpc.Dial(ctx, dut, s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to dial to remote dut: ", err)
+	}
+	defer c.Close(ctx)
+
+	client := pb.NewConferenceServiceClient(c.Conn)
+	if _, err := client.RunGoogleMeetScenario(ctx, &pb.MeetScenarioRequest{
+		Tier:            param.Tier,
+		RoomSize:        int64(param.Size),
+		ExtendedDisplay: false,
+	}); err != nil {
+		s.Fatal("Failed to run Meet Scenario: ", err)
+	}
+}
