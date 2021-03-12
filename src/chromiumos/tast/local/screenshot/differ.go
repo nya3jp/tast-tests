5347b0b671d06a41e1ca7f05f74df131e6a28ebb
Matt Stark <msta@google.com>
2021-03-12 02:03:37 +0000

    Make the screen diffing library call the gold backend.
    This CL adds the "diffing" aspect of screen diffing to the screendiff
    library (go/cros-screendiff).
    
    BUG=chromium:1137989
    TEST=Wrote and ran a tast test, and manually tested the code.
    
    Cq-Depend: chromium:2764175, chromium:2794071
    Change-Id: I70d4de97ad22bbede243278f8208fa7f4f8d2751

--- src/chromiumos/tast/local/screenshot/differ.go
+++ src/chromiumos/tast/local/screenshot/differ.go
@@ -15,24 +15,46 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"time"
 
+	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/display"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/coords"
+	"chromiumos/tast/lsbrelease"
 	"chromiumos/tast/testing"
 )
 
+// GoldServiceAccountKeyVar contains the name of the variable storing the service account key.
+const GoldServiceAccountKeyVar = "goldctl.GoldServiceAccountKey"
+
+const goldServiceAccountKeyFile = "/tmp/gold_service_account_key.json"
+
+// TODO(crbug.com/skia/10808): Change this once we have a production instance.
+const goldInstance = "cros-tast-dev"
+
+const goldctlWorkDir = "/tmp/goldctl"
 const keysFile = "keys.json"
 const screenshotFile = "screenshot.png"
+
+type testMode string
+
+const (
+	local      testMode = "local"
+	cq         testMode = "cq"
+	postsubmit testMode = "postsubmit"
+)
 
 type screenshotState interface {
 	Var(name string) (string, bool)
 	TestName() string
 	Fatal(...interface{})
+	Logf(string, ...interface{})
 }
 
 // DiffTestOptions provides all of the ways which you can configure the Diff method.
@@ -50,14 +72,18 @@ type Differ interface {
 }
 
 type differ struct {
-	ctx                context.Context
-	state              screenshotState
-	config             Config
-	chrome             *chrome.Chrome
-	tconn              *chrome.TestConn
-	dir                string
-	uiScale            float64
-	resetNormalization func() error
+	ctx         context.Context
+	state       screenshotState
+	config      Config
+	chrome      *chrome.Chrome
+	tconn       *chrome.TestConn
+	dir         string
+	uiScale     float64
+	reset       func() error
+	goldArgs    []string
+	failedTests []string
+	testMode    testMode
+	triageURL   string
 }
 
 // NewDiffer creates a differ for a new instance of chrome with default configuration.
@@ -92,6 +118,11 @@ func (d *differ) initialize() error {
 		d.chrome = cr
 	}
 
+	release, err := lsbrelease.Load()
+	if err != nil {
+		return err
+	}
+
 	tconn, err := d.chrome.TestAPIConn(d.ctx)
 	if err != nil {
 		return err
@@ -112,6 +143,7 @@ func (d *differ) initialize() error {
 		"deviceScaleFactor": fmt.Sprintf("%.2f", displayMode.DeviceScaleFactor),
 		"scale":             fmt.Sprintf("%.2f", uiScale),
 		"resolution":        fmt.Sprintf("%dx%d", displayMode.WidthInNativePixels, displayMode.HeightInNativePixels),
+		"board":             release[lsbrelease.Board],
 	}
 
 	dir, ok := testing.ContextOutDir(d.ctx)
@@ -132,6 +164,45 @@ func (d *differ) initialize() error {
 		return err
 	}
 
+	baseArgs := []string{
+		"--corpus", strings.Split(d.state.TestName(), ".")[0],
+	}
+
+	if strings.HasPrefix(release[lsbrelease.BuildType], "Continuous Builder") {
+		d.testMode = cq
+		builderMatch := regexp.MustCompile("-([0-9]+)$").FindStringSubmatch(release[lsbrelease.BuilderPath])
+		if builderMatch == nil {
+			return errors.Errorf("for a CQ build, the buildbucket ID should be filled in builder path (got %+v)", release[lsbrelease.BuilderPath])
+		}
+		d.goldArgs = append(baseArgs, []string{
+			"--cis", "buildbucket",
+			"--crs", "lookup",
+			"--changelist", "lookup",
+			"--patchset_id", "lookup",
+			"--jobid", builderMatch[1],
+			"--passfail"}...)
+		// TODO(crbug.com/skia/10808): once gold supports filtering by job id in the URL, set that.
+		d.triageURL = "Please look at the comment by the gold bot on the CL for a link to approve."
+
+		// Note: This will falsely pick up local builds that have been flashed with an official build.
+		// In the future, we may attempt to come up with a way to distinguish between these two.
+	} else if release[lsbrelease.BuildType] == "Official Build" {
+		build := release[lsbrelease.BuilderPath]
+		d.testMode = postsubmit
+		d.goldArgs = append(baseArgs, []string{
+			"--commit_id", strings.Split(build, "/")[1],
+			"--commit_metadata", fmt.Sprintf("gs://chromeos-image-archive/%s/manifest.xml", build)}...)
+	} else {
+		d.testMode = local
+		// TODO(crbug.com/skia/11815): once gold supports a local dev mode, replace the git hash field with that.
+		d.goldArgs = append(baseArgs, []string{
+			"--passfail",
+			"--git_hash", "f1d65adb1d7cd922f4677d0f9406a4083f5fdcbc"}...)
+	}
+
+	if err := d.authenticateGold(); err != nil {
+		return errors.Wrap(err, "failed to authenticate gold")
+	}
 	return nil
 }
 
@@ -173,7 +244,7 @@ func (d *differ) normalizeDisplayInfoAndMode() (*display.Info, *display.DisplayM
 				if err := display.SetDisplayProperties(d.ctx, d.tconn, info.ID, display.DisplayProperties{DisplayZoomFactor: &zoomFactor}); err != nil {
 					return nil, nil, errors.Wrap(err, "failed to normalize DPI")
 				}
-				d.resetNormalization = func() error {
+				d.reset = func() error {
 					return display.SetDisplayProperties(d.ctx, d.tconn, info.ID, display.DisplayProperties{DisplayZoomFactor: &oldZoomFactor})
 				}
 			}
@@ -193,23 +264,46 @@ func (d *differ) Diff(name string, finder *nodewith.Finder) uiauto.Action {
 func (d *differ) DiffWithOptions(name string, finder *nodewith.Finder, options DiffTestOptions) uiauto.Action {
 	return func(_ context.Context) error {
 		// Since elements of config are parameters that affect rendering, different configs should always have distinct diffs.
-		name = d.state.TestName() + "." + name
 		if configString := d.config.ToString(); len(configString) > 0 {
 			name += "." + configString
 		}
+		fullName := d.state.TestName() + "." + name
 		err := d.capture(name, finder, &options)
 		if err != nil {
 			return errors.Wrap(err, "failed to take screenshot")
 		}
+		if err := d.runGoldCommand("imgtest", append([]string{"add",
+			"--instance", goldInstance,
+			"--keys-file", filepath.Join(d.dir, keysFile),
+			"--test-name", fullName,
+			"--png-file", filepath.Join(d.dir, fullName, screenshotFile),
+		}, d.goldArgs...)...); err != nil {
+			d.failedTests = append(d.failedTests, name)
+			// In local dev, seeing the error early will help debug quicker.
+			if d.testMode == local {
+				d.state.Logf("Failed local test %s: %v", name, err)
+				// TODO(crbug.com/skia/10808): once we have a proper triage URL for CQ, delete this.
+			} else if d.testMode == cq {
+				d.state.Logf("Failed test on CQ %s: %v", name, err)
+			}
+		}
+
 		return nil
 	}
 }
 
 // GetFailedDiffs returns an error containing all of the diffs that failed, if any did, or nil if all passed.
 func (d *differ) GetFailedDiffs() error {
-	// TODO(crbug/1137989): Once we start using gold, this will become relevant.
-	if d.resetNormalization != nil {
-		return d.resetNormalization()
+	if d.reset != nil {
+		if err := d.reset(); err != nil {
+			return err
+		}
+	}
+
+	if len(d.failedTests) > 0 {
+		// Ignore the no newlines lint rule, because it makes it much easier to see what went wrong this way.
+		return errors.Errorf("failed screenshot tests:\n%s\n\n%s\n", // NOLINT
+			strings.Join(d.failedTests, "\n"), d.triageURL)
 	}
 	return nil
 }
@@ -217,7 +311,7 @@ func (d *differ) GetFailedDiffs() error {
 // DieOnFailedDiffs calls s.Fatal() if any diffs failed.
 func (d *differ) DieOnFailedDiffs() {
 	if err := d.GetFailedDiffs(); err != nil {
-		d.state.Fatal("diffs failed: ", err)
+		d.state.Fatal("screen diffs failed: ", err)
 	}
 }
 
@@ -273,4 +367,31 @@ func (d *differ) capture(screenshotName string, finder *nodewith.Finder, options
 	}
 	png.Encode(f, cropped)
 	return nil
+}
+
+func (d *differ) authenticateGold() error {
+	// If this file exists, then we've already authenticated, so there's no need to do it again.
+	if file, _ := os.Stat(filepath.Join(goldctlWorkDir, "auth_opt.json")); file != nil {
+		return nil
+	}
+	key, ok := d.state.Var(GoldServiceAccountKeyVar)
+	if !ok {
+		return errors.New("couldn't get the gold service account key. Please ensure you have access to tast-tests-private")
+	}
+	if err := ioutil.WriteFile(goldServiceAccountKeyFile, []byte(key), 0644); err != nil {
+		return err
+	}
+
+	return d.runGoldCommand("auth", "--service-account", goldServiceAccountKeyFile)
+}
+
+func (d *differ) runGoldCommand(subcommand string, args ...string) error {
+	args = append([](string){subcommand, "--work-dir", goldctlWorkDir}, args...)
+	cmd := testexec.CommandContext(d.ctx, "goldctl", args...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		// Ignore the no newlines lint rule, because it makes it much easier to see what went wrong this way.
+		err = errors.Errorf("while running \"goldctl %s\"\n%s", strings.Join(args, " "), out) // NOLINT
+	}
+	return err
 }
