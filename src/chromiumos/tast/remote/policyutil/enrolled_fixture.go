534aaed6f84ce12da4162eb118fac243da819b15
Victor-Gabriel Savu <vsavu@google.com>
2021-03-12 13:48:04 +0100

    policyutil: Add the enrolled fixture.
    Add the enrolled fixture that performs all required setup and teardown.
    Currently there is no way to forward state from this fixture to other
    fixtures or tests, use a fixed directory for the enrollment state in
    FakeDMS.
    
    BUG=chromium:1143230, chromium:1187473
    TEST=tast run <ip> policy.SystemTimezone
    
    Change-Id: Iec4cef1545eaa4903d907c00e981cc195de6ab5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2755296
    Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>
    Tested-by: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Alexander Hartl <alexanderhartl@google.com>

---
+++ src/chromiumos/tast/remote/policyutil/enrolled_fixture.go
@@ -0,0 +1,100 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policyutil
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/rpc"
+	ps "chromiumos/tast/services/cros/policy"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name:            "enrolled",
+		Desc:            "Fixture providing enrollment",
+		Contacts:        []string{"vsavu@google.com", "chromeos-commercial-stability@google.com"},
+		Impl:            &enrolledFixt{},
+		SetUpTimeout:    5 * time.Minute,
+		TearDownTimeout: 5 * time.Minute,
+		ResetTimeout:    15 * time.Second,
+		ServiceDeps:     []string{"tast.cros.policy.PolicyService"},
+	})
+}
+
+type enrolledFixt struct {
+	fdmsDir string
+}
+
+func (e *enrolledFixt) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	if err := EnsureTPMIsResetAndPowerwash(ctx, s.DUT()); err != nil {
+		s.Fatal("Failed to reset TPM: ", err)
+	}
+
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	pc := ps.NewPolicyServiceClient(cl.Conn)
+
+	// TODO(crbug.com/1187473): use a temporary directory.
+	e.fdmsDir = fakedms.EnrollmentFakeDMSDir
+
+	if _, err := pc.CreateFakeDMSDir(ctx, &ps.CreateFakeDMSDirRequest{
+		Path: e.fdmsDir,
+	}); err != nil {
+		s.Fatal("Failed to create temporary directory for FakeDMS: ", err)
+	}
+
+	pJSON, err := json.Marshal(fakedms.NewPolicyBlob())
+	if err != nil {
+		s.Fatal("Failed to serialize policies: ", err)
+	}
+
+	if _, err := pc.EnrollUsingChrome(ctx, &ps.EnrollUsingChromeRequest{
+		PolicyJson: pJSON,
+		FakedmsDir: e.fdmsDir,
+	}); err != nil {
+		s.Fatal("Failed to enroll using Chrome: ", err)
+	}
+
+	if _, err := pc.StopChromeAndFakeDMS(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Failed to stop Chrome: ", err)
+	}
+
+	return nil
+}
+
+func (e *enrolledFixt) TearDown(ctx context.Context, s *testing.FixtState) {
+	if err := EnsureTPMIsResetAndPowerwash(ctx, s.DUT()); err != nil {
+		s.Fatal("Failed to reset TPM: ", err)
+	}
+
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	pc := ps.NewPolicyServiceClient(cl.Conn)
+
+	if _, err := pc.RemoveFakeDMSDir(ctx, &ps.RemoveFakeDMSDirRequest{
+		Path: e.fdmsDir,
+	}); err != nil {
+		s.Fatal("Failed to remove temporary directory for FakeDMS: ", err)
+	}
+}
+
+func (*enrolledFixt) Reset(ctx context.Context) error                        { return nil }
+func (*enrolledFixt) PreTest(ctx context.Context, s *testing.FixtTestState)  {}
+func (*enrolledFixt) PostTest(ctx context.Context, s *testing.FixtTestState) {}
