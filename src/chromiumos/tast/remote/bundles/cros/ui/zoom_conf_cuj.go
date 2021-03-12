18a7ae05c0a93befecee2373a509d3fe2404cfe4
Septem Li <septemli@cienet.com>
2021-03-12 10:27:02 -0800

    cuj: Zoom conference test case
    Conduct zoom conference CUJ testing for various numbers of participants.
    
    To run Zoom CUJ, an external zoom bot server has to be set up. The CUJ
    will issue HTTP commnad to the zoom bot server to create zoom rooms
    dynamically.
    
    BUG=None
    TEST=tast run -var=ui.cuj_username=x -var=ui.cuj_password=x -var=ui.conference_server=x $DUT ui.ZoomConfCUJ.*
    
    Change-Id: I41552a169a97db2af32a3465d78e8fd5f5007a60

---
+++ src/chromiumos/tast/remote/bundles/cros/ui/zoom_conf_cuj.go
@@ -0,0 +1,177 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/remote/bundles/cros/ui/conference"
+	"chromiumos/tast/rpc"
+	pb "chromiumos/tast/services/cros/ui"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ZoomConfCUJ,
+		Desc:         "Host a Zoom video conference and do presentation to participants",
+		Contacts:     []string{"septemli@cienet.com", "jane.yang@cienet.com", "xliu@cienet.com"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		ServiceDeps: []string{
+			"tast.cros.ui.ZoomService",
+		},
+		Vars: []string{
+			"ui.cuj_username",
+			"ui.cuj_password",
+			// Zoom bot server proxy address
+			"ui.conference_server",
+		},
+		Params: []testing.Param{
+			{
+				Name:    "basic_clamshell_two",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "basic",
+					ScreenMode: "clamshell",
+					Size:       conference.TwoRoomSize,
+				},
+			},
+			{
+				Name:    "basic_clamshell_small",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "basic",
+					ScreenMode: "clamshell",
+					Size:       conference.SmallRoomSize,
+				},
+			},
+			{
+				Name:    "basic_clamshell_large",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "basic",
+					ScreenMode: "clamshell",
+					Size:       conference.LargeRoomSize,
+				},
+			},
+			{
+				Name:    "basic_clamshell_class",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "basic",
+					ScreenMode: "clamshell",
+					Size:       conference.ClassRoomSize,
+				},
+			},
+			{
+				Name:    "basic_tablet_two",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "basic",
+					ScreenMode: "tablet",
+					Size:       conference.TwoRoomSize,
+				},
+			},
+			{
+				Name:    "basic_tablet_small",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "basic",
+					ScreenMode: "tablet",
+					Size:       conference.SmallRoomSize,
+				},
+			},
+			{
+				Name:    "basic_tablet_large",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "basic",
+					ScreenMode: "tablet",
+					Size:       conference.LargeRoomSize,
+				},
+			},
+			{
+				Name:    "basic_tablet_class",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "basic",
+					ScreenMode: "tablet",
+					Size:       conference.ClassRoomSize,
+				},
+			},
+			{
+				Name:    "plus_clamshell_small",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "plus",
+					ScreenMode: "clamshell",
+					Size:       conference.SmallRoomSize,
+				},
+			},
+			{
+				Name:    "plus_clamshell_class",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "plus",
+					ScreenMode: "clamshell",
+					Size:       conference.ClassRoomSize,
+				},
+			},
+			{
+				Name:    "plus_tablet_small",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "plus",
+					ScreenMode: "tablet",
+					Size:       conference.SmallRoomSize,
+				},
+			},
+			{
+				Name:    "plus_tablet_class",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "plus",
+					ScreenMode: "tablet",
+					Size:       conference.ClassRoomSize,
+				},
+			},
+			{
+				Name:    "premium_clamshell_large",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "premium",
+					ScreenMode: "clamshell",
+					Size:       conference.LargeRoomSize,
+				},
+			},
+			{
+				Name:    "premium_tablet_large",
+				Timeout: time.Minute * 10,
+				Val: conference.TestParameters{
+					Tier:       "premium",
+					ScreenMode: "tablet",
+					Size:       conference.LargeRoomSize,
+				},
+			},
+		},
+	})
+}
+
+func ZoomConfCUJ(ctx context.Context, s *testing.State) {
+	param := s.Param().(conference.TestParameters)
+
+	tabletMode := param.ScreenMode == "tablet"
+
+	dut := s.DUT()
+	u1, err := rpc.Dial(ctx, dut, s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to dial to remote dut: ", err)
+	}
+	defer u1.Close(ctx)
+
+	client := pb.NewZoomServiceClient(u1.Conn)
+	conference.Run(ctx, s, client, param.Tier, param.Size, tabletMode, false)
+}
