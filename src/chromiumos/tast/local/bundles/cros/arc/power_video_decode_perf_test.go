db4df5d4dae93c7634d114b9400cd04dcef0efb3
Hikaru Nishida <hikalium@chromium.org>
2021-03-15 15:56:05 +0900

    tast-tests: Fix failure of PowerVideoDecodePerf on DUT without battery
    This CL adds _nobatterymetrics variants of PowerVideoDecodePerf tests.
    This CL also utilizes the Tast parameter generator to generate params
    of the test. The parameterization logic is in
    power_video_encode_perf_test.go and it automatically generates params
    of the test on its unittest.
    
    BUG=chromium:1065846,b:171533992
    TEST=~/trunk/src/platform/tast/tools/go.sh test \
         chromiumos/tast/local/bundles/cros/arc
    TEST=tast run $NO_BATTERY_DUT arc.PowerVideoDecodePerf* PASSed/SKIPped
    
    Change-Id: Iaec51eca687297e232e4b2be1075a3d4b0e30fae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2759609
    Tested-by: Hikaru Nishida <hikalium@chromium.org>
    Commit-Queue: Hikaru Nishida <hikalium@chromium.org>
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/arc/power_video_decode_perf_test.go
@@ -0,0 +1,126 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"testing"
+
+	"chromiumos/tast/common/genparams"
+)
+
+// To update test parameters after modifying this file, run:
+// TAST_GENERATE_UPDATE=1 ~/trunk/src/platform/tast/tools/go.sh test -count=1 chromiumos/tast/local/bundles/cros/arc
+
+func TestPowerVideoDecodePerfParamsAreGenerated(t *testing.T) {
+	type valMember struct {
+		Key   string
+		Value string
+	}
+	type paramData struct {
+		Name         string
+		Val          []valMember
+		SoftwareDeps []string
+		HardwareDeps []string
+		Data         []string
+	}
+	var params []paramData
+	for _, decoderParam := range []struct {
+		name         string
+		swdepLiteral string
+		dataName     string
+	}{
+		{
+			"h264_1080p_30fps",
+			"caps.HWDecodeH264",
+			"1080p_30fps_300frames.h264",
+		},
+		{
+			"vp8_1080p_30fps",
+			"caps.HWDecodeVP8",
+			"1080p_30fps_300frames.vp8.ivf",
+		},
+		{
+			"vp9_1080p_30fps",
+			"caps.HWDecodeVP9",
+			"1080p_30fps_300frames.vp9.ivf",
+		},
+		{
+			"vp9_1080p_60fps",
+			"caps.HWDecodeVP9_60",
+			"1080p_60fps_600frames.vp9.ivf",
+		},
+		{
+			"vp9_2160p_30fps",
+			"caps.HWDecodeVP9_4K",
+			"2160p_30fps_300frames.vp9.ivf",
+		},
+		{
+			"vp9_2160p_60fps",
+			"caps.HWDecodeVP9_4K60",
+			"2160p_60fps_600frames.vp9.ivf",
+		},
+	} {
+		for _, arcType := range []struct {
+			name  string
+			swdep string
+			attr  []string
+		}{
+			{
+				swdep: "android_p",
+			},
+			{
+				name:  "vm",
+				swdep: "android_vm",
+			},
+		} {
+			for _, batteryMode := range []struct {
+				name  string
+				hwdep string
+				val   []valMember
+			}{
+				{
+					hwdep: "hwdep.ForceDischarge()",
+					val:   []valMember{{"BatteryDischargeMode", "setup.ForceBatteryDischarge"}},
+				},
+				{
+					name:  "nobatterymetrics",
+					hwdep: "hwdep.NoForceDischarge()",
+					val:   []valMember{{"BatteryDischargeMode", "setup.NoBatteryDischarge"}},
+				},
+			} {
+				name := genTestName([]string{
+					decoderParam.name,
+					arcType.name,
+					batteryMode.name,
+				})
+				p := paramData{
+					Name:         string(name),
+					SoftwareDeps: []string{decoderParam.swdepLiteral, "\"" + arcType.swdep + "\""},
+					HardwareDeps: []string{batteryMode.hwdep},
+					Val: append([]valMember{
+						{"TestVideo", "\"" + decoderParam.dataName + "\""},
+					}, batteryMode.val...),
+					Data: []string{decoderParam.dataName, decoderParam.dataName + ".json"},
+				}
+				params = append(params, p)
+			}
+		}
+	}
+	code := genparams.Template(t, `{{ range . }}{
+		Name: {{ .Name | fmt }},
+		Val: video.DecodeTestOptions{
+			{{ range .Val }}{{ .Key }}: {{ .Value }},
+			{{ end }}
+		},
+		ExtraSoftwareDeps: []string{
+			{{ range .SoftwareDeps }}{{ . }},
+			{{ end }}
+		},
+		ExtraHardwareDeps: hwdep.D({{ range .HardwareDeps }}{{ . }},{{ end }}),
+		ExtraData: {{ .Data | fmt }},
+	},
+	{{ end }}`, params)
+	genparams.Ensure(t, "power_video_decode_perf.go", code)
+}
