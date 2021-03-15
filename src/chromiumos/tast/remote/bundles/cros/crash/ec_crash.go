c78e1d6909c24d9a51794ee7ba73738d363a5c80
Nicolas Boichat <drinkcat@chromium.org>
2021-03-15 19:37:23 +0800

    crash: ec: Fail when EC crash parsing reports WARNING/ERROR
    ec_parse_panicinfo now reports WARNING/ERROR when parsing EC crashes,
    and the crash reporter also output an ERROR if parsing fails, let's
    fail the tests when that happens.
    
    BUG=b:182122968
    TEST=```rm /usr/local/bin/ec_parse_panicinfo
      mv /usr/sbin/ec_parse_panicinfo /usr/sbin/ec_parse_panicinfo.orig
      echo '#!/bin/bash
    echo "WARNING: Cannot parse..." >&1
    exec /usr/sbin/ec_parse_panicinfo.orig "$@"
    ' > /usr/sbin/ec_parse_panicinfo
      chmod a+x /usr/sbin/ec_parse_panicinfo```
    
    Cq-Depend: chromium:2763543
    Change-Id: I163aff79ae5f894873f7bd4394b75a3c602fad4a

--- src/chromiumos/tast/remote/bundles/cros/crash/ec_crash.go
+++ src/chromiumos/tast/remote/bundles/cros/crash/ec_crash.go
@@ -6,7 +6,11 @@ package crash
 
 import (
 	"context"
+	"io/ioutil"
+	"path"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
@@ -139,6 +143,34 @@ func ECCrash(ctx context.Context, s *testing.State) {
 			s.Log("Failed to save messages log")
 		}
 		s.Fatal("Failed to find crash files: " + err.Error())
+	}
+
+	// Verify that parsed EC crash does not contain WARNING/ERROR
+	for _, match := range res.Matches {
+		if !strings.HasSuffix(match.Regex, ".eccrash") {
+			continue
+		}
+		if err := d.GetFile(cleanupCtx, match.Files[0],
+			filepath.Join(s.OutDir(), path.Base(match.Files[0]))); err != nil {
+			s.Error("Failed to save eccrash file")
+			continue
+		}
+		out, err := ioutil.ReadFile(filepath.Join(s.OutDir(), path.Base(match.Files[0])))
+		if err != nil {
+			s.Error("Failed to read eccrash file", match.Files[0])
+			continue
+		}
+
+		errorSigRegexp := regexp.MustCompile(`^ERROR:.*$`)
+		warningSigRegexp := regexp.MustCompile(`^WARNING:.*$`)
+		lines := strings.Split(string(out), "\n")
+		for _, line := range lines {
+			if err := errorSigRegexp.FindString(line); err != "" {
+				s.Error("Found error in EC crash parsing ", string(err))
+			} else if warn := warningSigRegexp.FindString(line); warn != "" {
+				s.Error("Found warning in EC crash parsing ", string(warn))
+			}
+		}
 	}
 
 	removeReq := &crash_service.RemoveAllFilesRequest{
