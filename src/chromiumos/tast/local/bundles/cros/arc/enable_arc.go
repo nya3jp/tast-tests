1ea685bc751c6c62e15a98bafae9b3461688fbfd
Toby Huang <tobyhuang@chromium.org>
2021-03-16 01:19:00 +0000

    Revert "Tast: Refactor common Family Link login code into fixture"
    This reverts commit 14025de33c45f4823b630f4e966ce79d14fbc0f4.
    
    Reason for revert: Causes Tast test failures with Arc Playstore.
    
    Original change's description:
    > Tast: Refactor common Family Link login code into fixture
    >
    > Now that several tests log in as a Family Link user in Tast, we should
    > refactor common code into a fixture so that the tests can share.
    >
    > BUG=chromium:1176991
    > TEST=Ran all affected Tast tests
    >
    > Change-Id: I1ebef5ae087a9e4b9d79730b1fdda530375a362c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2728562
    > Auto-Submit: Toby Huang <tobyhuang@chromium.org>
    > Reviewed-by: David Haddock <dhaddock@chromium.org>
    > Reviewed-by: Toby Huang <tobyhuang@chromium.org>
    > Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    > Reviewed-by: Renuga Nanjappan <rnanjappan@chromium.org>
    > Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    > Tested-by: Toby Huang <tobyhuang@chromium.org>
    
    Bug: chromium:1176991
    Change-Id: I3a860cf52e63ae1c0f8f536544fa0d9cb8620417

--- src/chromiumos/tast/local/bundles/cros/arc/enable_arc.go
+++ src/chromiumos/tast/local/bundles/cros/arc/enable_arc.go
@@ -12,13 +12,16 @@ import (
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/arc/optin"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/familylink"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/ossettings"
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/testing"
 )
+
+type accountTypeTestParam struct {
+	unicorn bool
+}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -29,19 +32,27 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			ExtraSoftwareDeps: []string{"android_p"},
-			Fixture:           "familyLinkParentArcLogin",
+			Val: accountTypeTestParam{
+				unicorn: false,
+			},
 		}, {
 			Name:              "unicorn",
 			ExtraSoftwareDeps: []string{"android_p"},
-			Fixture:           "familyLinkUnicornArcLogin",
+			Val: accountTypeTestParam{
+				unicorn: true,
+			},
 		}, {
 			Name:              "vm",
 			ExtraSoftwareDeps: []string{"android_vm"},
-			Fixture:           "familyLinkParentArcLogin",
+			Val: accountTypeTestParam{
+				unicorn: false,
+			},
 		}, {
 			Name:              "unicorn_vm",
 			ExtraSoftwareDeps: []string{"android_vm"},
-			Fixture:           "familyLinkUnicornArcLogin",
+			Val: accountTypeTestParam{
+				unicorn: true,
+			},
 		}},
 		Timeout: chrome.GAIALoginTimeout + arc.BootTimeout + 120*time.Second,
 		Vars:    []string{"arc.parentUser", "arc.parentPassword", "arc.childUser", "arc.childPassword"},
@@ -50,7 +61,44 @@ func init() {
 
 func EnableArc(ctx context.Context, s *testing.State) {
 
-	tconn := s.FixtValue().(*familylink.FixtData).TestConn
+	parentUser := s.RequiredVar("arc.parentUser")
+	parentPass := s.RequiredVar("arc.parentPassword")
+	childUser := s.RequiredVar("arc.childUser")
+	childPass := s.RequiredVar("arc.childPassword")
+	var cr *chrome.Chrome
+	var err error
+
+	accountType := s.Param().(accountTypeTestParam)
+	if accountType.unicorn {
+		cr, err = chrome.New(
+			ctx,
+			chrome.GAIALogin(chrome.Creds{
+				User:       childUser,
+				Pass:       childPass,
+				ParentUser: parentUser,
+				ParentPass: parentPass,
+			}),
+			chrome.ARCSupported())
+	} else {
+		cr, err = chrome.New(
+			ctx,
+			chrome.GAIALogin(chrome.Creds{
+				User: parentUser,
+				Pass: parentPass,
+			}),
+			chrome.ARCSupported(),
+			chrome.ExtraArgs(arc.DisableSyncFlags()...))
+	}
+
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
 
 	s.Log("Turn On Play Store from Settings")
 	if err := turnOnPlayStore(ctx, tconn); err != nil {
