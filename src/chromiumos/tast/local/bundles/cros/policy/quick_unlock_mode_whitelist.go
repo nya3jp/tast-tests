8b5aa18da0a7a0732f3496d55d1de94e47bdcdd9
Jana Grill <janagrill@google.com>
2021-03-18 20:01:16 +0100

    policy: Add QuickUnlockModeWhitelist tast test
    Add the policy.QuickUnlockModeWhitelist tast test, which verifies that
    the deprecated policy QuickUnlockModeWhitelist still behaves as
    expected.
    
    This policy was deprecated with M87 in favor of the more inclusive
    QuickUnlockModeAllowlist, however we will continue support for the
    deprecated policy and therefore want to verify that it still works
    properly.
    
    Once the support for this deprecated policy is stopped, this test can be
    removed.
    
    BUG=chromium:1187464
    TEST=tast run <DUT> policy.QuickUnlockModeWhitelist
    
    Change-Id: I4e4325faa5254abec3543569f0c642ccf55bfb37

---
+++ src/chromiumos/tast/local/bundles/cros/policy/quick_unlock_mode_whitelist.go
@@ -0,0 +1,129 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/restriction"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: QuickUnlockModeWhitelist,
+		Desc: "Checks that the deprecated policy still works",
+		Contacts: []string{
+			"janagrill@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromePolicyLoggedIn",
+	})
+}
+
+func QuickUnlockModeWhitelist(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+	fdms := s.FixtValue().(*fixtures.FixtData).FakeDMS
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer kb.Close()
+
+	for _, param := range []struct {
+		name            string
+		wantRestriction restriction.Restriction
+		policies        []policy.Policy
+	}{
+		{
+			name:            "unset",
+			wantRestriction: restriction.Disabled,
+			policies: []policy.Policy{
+				&policy.QuickUnlockModeWhitelist{Stat: policy.StatusUnset},
+			},
+		},
+		{
+			name:            "empty",
+			wantRestriction: restriction.Disabled,
+			policies: []policy.Policy{
+				&policy.QuickUnlockModeWhitelist{Val: []string{}},
+			},
+		},
+		{
+			name:            "all",
+			wantRestriction: restriction.None,
+			policies: []policy.Policy{
+				&policy.QuickUnlockModeWhitelist{Val: []string{"all"}},
+			},
+		},
+		{
+			name:            "pin",
+			wantRestriction: restriction.None,
+			policies: []policy.Policy{
+				&policy.QuickUnlockModeWhitelist{Val: []string{"PIN"}},
+			},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			defer faillog.DumpUITreeOnErrorToFile(ctx, s.OutDir(), s.HasError, tconn, "ui_tree_"+param.name+".txt")
+
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies. Use |ServeAndRefresh| instead of |ServeAndVerify| to
+			// avoid the error due to the policy being deprecated.
+			if err := policyutil.ServeAndRefresh(ctx, fdms, cr, param.policies); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			// Open the Lockscreen page where we can set a PIN.
+			conn, err := apps.LaunchOSSettings(ctx, cr, "chrome://os-settings/lockScreen")
+			if err != nil {
+				s.Fatal("Failed to connect to the settings page: ", err)
+			}
+			defer conn.Close()
+
+			ui := uiauto.New(tconn)
+
+			// Find and enter the password in the pop up window.
+			if err := ui.LeftClick(nodewith.Name("Password").Role(role.TextField))(ctx); err != nil {
+				s.Fatal("Could not find the password field: ", err)
+			}
+			if err := kb.Type(ctx, fixtures.Password+"\n"); err != nil {
+				s.Fatal("Failed to type password: ", err)
+			}
+
+			// // Find node info for the radio button group node.
+			rgNode, err := ui.Info(ctx, nodewith.Role(role.RadioGroup))
+			if err != nil {
+				s.Fatal("Finding radio group failed: ", err)
+			}
+
+			// Check that the radio button group has the expected restriction.
+			if rgNode.Restriction != param.wantRestriction {
+				s.Errorf("Unexpected Continue button state: got %v, want %v", rgNode.Restriction, param.wantRestriction)
+			}
+		})
+	}
+}
