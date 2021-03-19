716541723915e13d06c3588f7fb058ed3e0fe290
David Haddock <dhaddock@google.com>
2021-03-18 19:54:41 -0700

    (Nearby Share) Add png version of Phone -> CrOS test.
    BUG=chromium:1186069
    TEST=nearbyshare.PhoneToCrosHighVis.dataoffline_allcontacts_png5kb
    TEST=nearbyshare.PhoneToCrosInContacts.dataoffline_allcontacts_png5kb
    
    Change-Id: I0d8dd60ee2669ca375871d11313926c61ce2c189

--- src/chromiumos/tast/local/bundles/cros/nearbyshare/phone_to_cros_in_contacts.go
+++ src/chromiumos/tast/local/bundles/cros/nearbyshare/phone_to_cros_in_contacts.go
@@ -45,6 +45,18 @@ func init() {
 				Timeout:   nearbycommon.DetectionTimeout + nearbycommon.SmallFileTransferTimeout,
 			},
 			{
+				Name:    "dataoffline_allcontacts_png5kb",
+				Fixture: "nearbyShareDataUsageOfflineAllContactsGAIA",
+				Val: nearbytestutils.TestData{
+					Filename:        "small_png.zip",
+					TransferTimeout: nearbycommon.SmallFileTransferTimeout,
+					TestTimeout:     nearbycommon.DetectionTimeout + nearbycommon.SmallFileTransferTimeout,
+					MimeType:        nearbysnippet.MimeTypePng,
+				},
+				ExtraData: []string{"small_png.zip"},
+				Timeout:   nearbycommon.DetectionTimeout + nearbycommon.SmallFileTransferTimeout,
+			},
+			{
 				Name:    "dataonline_allcontacts_txt30mb",
 				Fixture: "nearbyShareDataUsageOnlineAllContactsGAIA",
 				Val: nearbytestutils.TestData{
