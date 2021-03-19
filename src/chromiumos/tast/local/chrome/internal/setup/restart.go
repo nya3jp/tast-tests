49ef265251d4f58ab98da0962d4cf23e1993da97
Victor-Gabriel Savu <vsavu@google.com>
2021-03-19 18:14:07 +0100

    policy: Perform enrollment before login
    Perform enrollment before login in tast to allow for more flexibility.
    No functional change.
    
    BUG=chromium:1190120
    TEST=tast run <ip> policy.SystemTimezone
    
    Change-Id: I1a170fa048ad909b3a123809a342a50d9e61ad26

--- src/chromiumos/tast/local/chrome/internal/setup/restart.go
+++ src/chromiumos/tast/local/chrome/internal/setup/restart.go
@@ -64,10 +64,6 @@ func RestartChromeForTesting(ctx context.Context, cfg *config.Config, exts *exte
 		args = append(args, "--no-startup-window") // Do not start up chrome://newtab by default to avoid unexpected patterns (doodle etc.)
 	}
 
-	if cfg.Enroll() {
-		args = append(args, "--disable-policy-key-verification") // Remove policy key verification for fake enrollment
-	}
-
 	if cfg.SkipOOBEAfterLogin() {
 		args = append(args, "--oobe-skip-postlogin")
 	}
