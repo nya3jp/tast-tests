1bbcf2ff475dddf05574bfc60a4a6736e1229c34
Srikanth Oruganti <srikanthkumar@google.com>
2021-03-18 21:52:51 -0700

    Add network.ModemmanagerSlotSwitch test
    This adds test for switching physical SIM slots and checks
    corresponding modem properties through modemmanager
    
    BUG=b:181016882
    TEST=tast --verbose run ${DUT} network.ModemmanagerSlotSwitch
    
    Change-Id: I25749bf321281393369879dcc07b0f2fef9c4b49

---
+++ src/chromiumos/tast/local/bundles/cros/network/modemmanager_slot_switch.go
@@ -0,0 +1,155 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/mmconst"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/modemmanager"
+	"chromiumos/tast/testing"
+)
+
+// This test runs on all carriers including esim. Slot switch is tested on boards with Modem,
+// SimSlots property and at least two SIM slots and one active profile on each slot.
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ModemmanagerSlotSwitch,
+		Desc:     "Verifies that modemmanager switches SIM slot",
+		Contacts: []string{"srikanthkumar@google.com", "cros-network-health@google.com"},
+		Attr:     []string{"group:cellular", "cellular_unstable"},
+		Fixture:  "cellular",
+		Timeout:  5 * time.Minute,
+	})
+}
+
+// ModemmanagerSlotSwitch Test
+func ModemmanagerSlotSwitch(ctx context.Context, s *testing.State) {
+	s.Log("Precheck Modem SIM properties")
+	primary, modem, err := checkModemSimProperties(ctx)
+
+	// Do graceful exit if dut has only one sim slot or no slot
+	if err == errorSingleSlot {
+		s.Log("Can not run this test: ", err)
+		return
+	} else if err != nil {
+		s.Fatal("Failed at checkModemSimProperties: ", err)
+	}
+
+	// Switch slot
+	var newslot uint32 = 2
+	if primary == newslot {
+		newslot = 1
+	}
+	s.Logf("Switching slot from: %d to %d", primary, newslot)
+	err = setPrimarySimSlot(ctx, newslot)
+	if err != nil {
+		s.Fatal("Switch failed in 1st run: ", err)
+	}
+	// Shorten the context to save time for cleanup.
+	ctx, cancel := ctxutil.Shorten(ctx, 30*time.Second)
+	defer cancel()
+
+	err = pollModem(ctx, modem)
+	if err != nil {
+		s.Fatal("Poll failed 1st run: ", err)
+	}
+	s.Log("Postcheck Modem SIM properties")
+	slot, modem, err := checkModemSimProperties(ctx)
+	if err != nil || slot != newslot {
+		s.Fatal("Failed to get primary slot properties in 1st run: ", err)
+	}
+
+	// Call setPrimarySimSlot again to set slot to original primary slot
+	s.Logf("Switching slot from: %d to %d", newslot, primary)
+	err = setPrimarySimSlot(ctx, primary)
+	if err != nil {
+		s.Fatal("Switch failed in 2nd run: ", err)
+	}
+	err = pollModem(ctx, modem)
+	if err != nil {
+		s.Fatal("Poll failed in 2nd run: ", err)
+	}
+	s.Log("Postcheck Modem SIM properties")
+	slot, modem, err = checkModemSimProperties(ctx)
+	if err != nil || slot != primary {
+		s.Fatal("Failed to get primary slot properties in 2nd run: ", err)
+	}
+}
+
+// errorSingleSlot thrown to do greaceful Exit from this test if DUT got single SIM slot error
+var errorSingleSlot = errors.New("Test requires two slots, exiting")
+
+// setPrimarySimSlot switches primary sim slot to given slot
+func setPrimarySimSlot(ctx context.Context, primary uint32) error {
+	modem, err := modemmanager.NewModem(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create Modem")
+	}
+	if modem.Call(ctx, "SetPrimarySimSlot", primary); err != nil {
+		return errors.Wrap(err, "failed at primary SIM Slot switch")
+	}
+	return nil
+}
+
+// pollModem waits for modem to load after every slot switch operation
+func pollModem(ctx context.Context, modem *modemmanager.Modem) error {
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		newModem, err := modemmanager.NewModem(ctx)
+		if err != nil {
+			return err
+		}
+		if modem.String() == newModem.String() {
+			return errors.New("Old modem still exists")
+		}
+		testing.ContextLogf(ctx, "Modem paths are Old: %s, New: %s ", modem, newModem)
+		return nil
+	}, &testing.PollOptions{Timeout: mmconst.ModemPollTime}); err != nil {
+		return errors.Wrap(err, "modem or its properties not up after switching SIM slot")
+	}
+	return nil
+}
+
+// checkModemSimProperties checks modem properties and returns primary SIM slot,
+// associated new modem object, and error for DBus failures & prerequisite failures
+func checkModemSimProperties(ctx context.Context) (uint32, *modemmanager.Modem, error) {
+	modem, err := modemmanager.NewModem(ctx)
+	if err != nil {
+		return 0, nil, errors.Wrap(err, "failed to create modem")
+	}
+	props, err := modem.GetDBusProperties(ctx)
+	if err != nil {
+		return 0, modem, errors.Wrap(err, "failed to call GetProperties on modem")
+	}
+	sim, err := props.GetObjectPath(mmconst.ModemPropertySim)
+	if err != nil {
+		return 0, modem, errors.Wrap(err, "missing sim property")
+	}
+	simSlots, err := props.GetObjectPaths(mmconst.ModemPropertySimSlots)
+	if err != nil {
+		return 0, modem, errors.Wrap(err, "failed to get simslots property")
+	}
+	numSlots := len(simSlots)
+	testing.ContextLogf(ctx, "Number of slots on dut: %d", numSlots)
+	if numSlots < 2 {
+		return 0, modem, errorSingleSlot
+	}
+	primary, err := props.GetUint32(mmconst.ModemPropertyPrimarySimSlot)
+	if err != nil {
+		return 0, modem, errors.Wrap(err, "missing PrimarySimSlot property")
+	}
+	if int(primary) > len(simSlots) {
+		return primary, modem, errors.Errorf("invalid PrimarySimSlot: %d", primary)
+	}
+	if sim != simSlots[primary-1] {
+		return primary, modem, errors.Errorf("Sim property mismatch: %s", sim)
+	}
+	testing.ContextLogf(ctx, "Modem Sim property: %s, PrimarySimSlot: %d", sim, primary)
+	return primary, modem, nil
+}
