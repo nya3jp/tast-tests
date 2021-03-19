064b4e26f7247057b063c311a09406de999dc0ed
Wei Lee <wtlee@google.com>
2021-03-19 13:36:48 +0800

    camera: Migrate to use Fixtures for CCA tests
    BUG=b:177800595
    TEST=tast run [DUT] camera.CCAUI*
    
    Change-Id: I6134180f30c7fdff4d26cbefbf7f8523b36fcf61

--- src/chromiumos/tast/local/bundles/cros/camera/cca_ui_take_picture.go
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_take_picture.go
@@ -9,10 +9,7 @@ import (
 	"time"
 
 	"chromiumos/tast/common/media/caps"
-	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/camera/cca"
-	"chromiumos/tast/local/camera/testutil"
-	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
 )
 
@@ -23,32 +20,12 @@ func init() {
 		Contacts:     []string{"inker@chromium.org", "chromeos-camera-eng@google.com"},
 		Attr:         []string{"group:mainline", "informational", "group:camera-libcamera"},
 		SoftwareDeps: []string{"camera_app", "chrome", caps.BuiltinOrVividCamera},
-		Data:         []string{"cca_ui.js"},
-		Pre:          chrome.LoggedIn(),
+		Fixture:      "ccaTestBridgeReady",
 	})
 }
 
 func CCAUITakePicture(ctx context.Context, s *testing.State) {
-	cr := s.PreValue().(*chrome.Chrome)
-	tb, err := testutil.NewTestBridge(ctx, cr, testutil.UseRealCamera)
-	if err != nil {
-		s.Fatal("Failed to construct test bridge: ", err)
-	}
-	defer tb.TearDown(ctx)
-
-	if err := cca.ClearSavedDir(ctx, cr); err != nil {
-		s.Fatal("Failed to clear saved directory: ", err)
-	}
-
-	app, err := cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir(), tb)
-	if err != nil {
-		s.Fatal("Failed to open CCA: ", err)
-	}
-	defer func(ctx context.Context) {
-		if err := app.Close(ctx); err != nil {
-			s.Error("Failed to close app: ", err)
-		}
-	}(ctx)
+	runSubTest := s.FixtValue().(cca.FixtureData).RunSubTest
 
 	subTestTimeout := 30 * time.Second
 	for _, tst := range []struct {
@@ -61,20 +38,10 @@ func CCAUITakePicture(ctx context.Context, s *testing.State) {
 	} {
 		subTestCtx, cancel := context.WithTimeout(ctx, subTestTimeout)
 		s.Run(subTestCtx, tst.name, func(ctx context.Context, s *testing.State) {
-			shortCtx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
-			defer cancel()
-
-			if err := cca.ClearSavedDir(ctx, cr); err != nil {
-				s.Fatal("Failed to clear saved directory: ", err)
-			}
-
-			if err := tst.testFunc(shortCtx, app); err != nil {
-				s.Fatalf("Failed in %v(): %v", tst.name, err)
-			}
-
-			// Restart app using non-shorten context.
-			if err := app.Restart(ctx, tb); err != nil {
-				s.Fatal("Failed to restart CCA: ", err)
+			if err := runSubTest(ctx, func(ctx context.Context, app *cca.App) error {
+				return tst.testFunc(ctx, app)
+			}, cca.SubTestParams{}); err != nil {
+				s.Errorf("Failed to pass %v subtest: %v", tst.name, err)
 			}
 		})
 		cancel()
