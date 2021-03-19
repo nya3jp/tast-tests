96164313f6a0e419d37deac6e636f6e1976c2457
Pranav Batra <batrapranav@chromium.org>
2021-03-19 01:09:04 +0000

    Add generate_external.sh
    This script can be used to generate external files from regular test data files.
    
    BUG=none
    TEST=manual
    
    Change-Id: I5892b38cf467fba6c7e670ee470278de38c71386

---
+++ src/chromiumos/tast/local/bundles/cros/printer/data/generate_external.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This script uploads files to the test server and generates corresponding
+# external files that can be used by the printer Tast tests.
+
+# Usage: ./generate_external.sh !(*.external|*.sh)
+# Uploads all non-external files and generates corresponding external files.
+
+# Remember to remove non-external files when finished: rm !(*.external|*.sh).
+
+U="gs://chromiumos-test-assets-public/tast/cros/printer/"
+#U="gs://chromeos-test-assets-private/tast/crosint/printer/"
+D="$(date +"%Y%m%d")"
+for A in "$@"; do
+F="${A##*/}"
+B="${U}${F%%.*}_${D}"
+if [[ "$F" == *.* ]]; then
+  B="${B}.${F#*.}"
+fi
+gsutil cp "$A" "$B"
+cat << EOF > "${F}.external"
+{
+  "url": "${B}",
+  "size": $(stat -c %s "${A}"),
+  "sha256sum": "$(sha256sum "${A}" | cut -d ' ' -f 1)"
+}
+EOF
+done
