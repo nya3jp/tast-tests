175f86b9651239165ef1928838ff8c4ffd36fb51
Avery Musbach <amusbach@chromium.org>
2021-03-22 16:59:11 -0700

    tast-tests: Measure Ash.Overview.AnimationSmoothness.*.SplitView
    BUG=chromium:1124599
    TEST=ui.OverviewPerf,ui.OverviewPerf.skia_renderer,ui.OverviewPerf.lacros
    
    Change-Id: I17aa8881af6eb33fc871390ca1bb20766aaca281

--- src/chromiumos/tast/local/bundles/cros/ui/overview_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/overview_perf.go
@@ -31,7 +31,7 @@ func init() {
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome"},
 		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
-		Timeout:      8 * time.Minute,
+		Timeout:      15 * time.Minute,
 		Params: []testing.Param{{
 			Val:     lacros.ChromeTypeChromeOS,
 			Fixture: "chromeLoggedIn",
@@ -61,12 +61,14 @@ func OverviewPerf(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to turn on display: ", err)
 	}
 
+	chromeType := s.Param().(lacros.ChromeType)
+	isLacros := chromeType == lacros.ChromeTypeLacros
 	// TODO(crbug.com/1127165): Remove the artifactPath argument when we can use Data in fixtures.
 	var artifactPath string
-	if s.Param().(lacros.ChromeType) == lacros.ChromeTypeLacros {
+	if isLacros {
 		artifactPath = s.DataPath(launcher.DataArtifact)
 	}
-	cr, l, cs, err := lacros.Setup(ctx, s.FixtValue(), artifactPath, s.Param().(lacros.ChromeType))
+	cr, l, cs, err := lacros.Setup(ctx, s.FixtValue(), artifactPath, chromeType)
 	if err != nil {
 		s.Fatal("Failed to initialize test: ", err)
 	}
@@ -83,22 +85,14 @@ func OverviewPerf(ctx context.Context, s *testing.State) {
 	}
 	defer ash.SetTabletModeEnabled(cleanupCtx, tconn, originalTabletMode)
 
-	// overviewAnimationType specifies the type of the animation of entering to or
-	// exiting from the overview mode.
-	type overviewAnimationType int
-	const (
-		// animationTypeMaximized is the animation when there are maximized windows
-		// in the clamshell mode.
-		animationTypeMaximized overviewAnimationType = iota
-		// animationTypeNormalWindow is the animation for normal windows in the
-		// clamshell mode.
-		animationTypeNormalWindow
-		// animationTypeTabletMode is the animation for windows in the tablet mode.
-		animationTypeTabletMode
-		// animationTypeTabletMode is the animation for windows in the tablet mode
-		// when they are all minimized
-		animationTypeMinimizedTabletMode
-	)
+	var blankWindowTitle string
+	if isLacros {
+		blankWindowTitle = "about:blank - Google Chrome"
+	} else {
+		blankWindowTitle = "Chrome - about:blank"
+	}
+
+	isBlank := func(w *ash.Window) bool { return w.Title == blankWindowTitle }
 
 	// Run an http server to serve the test contents for accessing from the chrome browsers.
 	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
@@ -106,42 +100,149 @@ func OverviewPerf(ctx context.Context, s *testing.State) {
 	url := server.URL + "/animation.html"
 
 	r := perfutil.NewRunner(cr)
-	currentWindows := 0
+	currentAnimationWindows := 0
+	haveBlankWindow := isLacros
 	// Run the overview mode enter/exit flow for various situations.
-	// - change the number of browser windows, 2 or 8
-	// - the window system status; clamshell mode with maximized windows or
-	//   tablet mode.
-	// If these window number values are changed, make sure to check lacros about:blank pages are closed correctly.
-	for i, windows := range []int{2, 8} {
-		if err := ash.CreateWindows(ctx, tconn, cs, url, windows-currentWindows); err != nil {
-			s.Fatal("Failed to create browser windows: ", err)
-		}
-
-		// This must be done after ash.CreateWindows to avoid terminating lacros-chrome.
-		if i == 0 && s.Param().(lacros.ChromeType) == lacros.ChromeTypeLacros {
-			if err := lacros.CloseAboutBlank(ctx, tconn, l.Devsess, 1); err != nil {
-				s.Fatal("Failed to close about:blank: ", err)
+	// - 2 or 8 overview windows
+	// - clamshell or tablet
+	// - overview windows normal, maximized, or minimized
+	// - tablet split view or not
+	// - split view window is showing about:blank or animation.html
+	for _, overviewWindows := range []int{2, 8} {
+		for _, test := range []struct {
+			histogramSuffix     string
+			extraDescriptionFmt string
+			animationWindows    int
+			tablet              bool
+			overviewWindowState ash.WindowStateType
+			splitView           bool
+			splitViewUseBlank   bool
+		}{
+			{
+				histogramSuffix:     "SingleClamshellMode",
+				extraDescriptionFmt: "%dwindows",
+				animationWindows:    overviewWindows,
+				tablet:              false,
+				overviewWindowState: ash.WindowStateMaximized,
+				splitView:           false,
+				splitViewUseBlank:   false,
+			},
+			{
+				histogramSuffix:     "ClamshellMode",
+				extraDescriptionFmt: "%dwindows",
+				animationWindows:    overviewWindows,
+				tablet:              false,
+				overviewWindowState: ash.WindowStateNormal,
+				splitView:           false,
+				splitViewUseBlank:   false,
+			},
+			{
+				histogramSuffix:     "TabletMode",
+				extraDescriptionFmt: "%dwindows",
+				animationWindows:    overviewWindows,
+				tablet:              true,
+				overviewWindowState: ash.WindowStateMaximized,
+				splitView:           false,
+				splitViewUseBlank:   false,
+			},
+			{
+				histogramSuffix:     "MinimizedTabletMode",
+				extraDescriptionFmt: "%dwindows",
+				animationWindows:    overviewWindows,
+				tablet:              true,
+				overviewWindowState: ash.WindowStateMinimized,
+				splitView:           false,
+				splitViewUseBlank:   false,
+			},
+			{
+				histogramSuffix:     "SplitView",
+				extraDescriptionFmt: "%dmaximizedoverviewwindows-lightsnappedwindow",
+				animationWindows:    overviewWindows,
+				tablet:              true,
+				overviewWindowState: ash.WindowStateMaximized,
+				splitView:           true,
+				splitViewUseBlank:   true,
+			},
+			{
+				histogramSuffix:     "SplitView",
+				extraDescriptionFmt: "%dminimizedoverviewwindows-lightsnappedwindow",
+				animationWindows:    overviewWindows,
+				tablet:              true,
+				overviewWindowState: ash.WindowStateMinimized,
+				splitView:           true,
+				splitViewUseBlank:   true,
+			},
+			{
+				histogramSuffix:     "SplitView",
+				extraDescriptionFmt: "%dmaximizedoverviewwindows-heavysnappedwindow",
+				animationWindows:    overviewWindows + 1,
+				tablet:              true,
+				overviewWindowState: ash.WindowStateMaximized,
+				splitView:           true,
+				splitViewUseBlank:   false,
+			},
+			{
+				histogramSuffix:     "SplitView",
+				extraDescriptionFmt: "%dminimizedoverviewwindows-heavysnappedwindow",
+				animationWindows:    overviewWindows + 1,
+				tablet:              true,
+				overviewWindowState: ash.WindowStateMinimized,
+				splitView:           true,
+				splitViewUseBlank:   false,
+			},
+		} {
+			if err := ash.CreateWindows(ctx, tconn, cs, url, test.animationWindows-currentAnimationWindows); err != nil {
+				s.Fatal("Failed to create browser windows: ", err)
 			}
-		}
+			currentAnimationWindows = test.animationWindows
 
-		currentWindows = windows
+			if test.splitViewUseBlank && !haveBlankWindow {
+				if err := ash.CreateWindows(ctx, tconn, cs, "about:blank", 1); err != nil {
+					s.Fatal("Failed to open about:blank: ", err)
+				}
+				haveBlankWindow = true
+			} else if !test.splitViewUseBlank && haveBlankWindow {
+				w, err := ash.FindWindow(ctx, tconn, isBlank)
+				if err != nil {
+					s.Fatal("Failed to find window with about:blank: ", err)
+				}
 
-		for _, state := range []overviewAnimationType{animationTypeMaximized, animationTypeNormalWindow, animationTypeTabletMode, animationTypeMinimizedTabletMode} {
-			inTabletMode := (state == animationTypeTabletMode || state == animationTypeMinimizedTabletMode)
-			if err = ash.SetTabletModeEnabled(ctx, tconn, inTabletMode); err != nil {
-				s.Fatalf("Failed to set tablet mode %v: %v", inTabletMode, err)
+				if err := w.CloseWindow(ctx, tconn); err != nil {
+					s.Fatal("Failed to close about:blank: ", err)
+				}
+				haveBlankWindow = false
 			}
 
-			windowState := ash.WindowStateNormal
-			if state == animationTypeMaximized || state == animationTypeTabletMode {
-				windowState = ash.WindowStateMaximized
-			} else if state == animationTypeMinimizedTabletMode {
-				windowState = ash.WindowStateMinimized
+			if err := ash.SetTabletModeEnabled(ctx, tconn, test.tablet); err != nil {
+				s.Fatalf("Failed to set tablet mode to %v: %v", test.tablet, err)
 			}
+
 			if err := ash.ForEachWindow(ctx, tconn, func(w *ash.Window) error {
-				return ash.SetWindowStateAndWait(ctx, tconn, w.ID, windowState)
+				return ash.SetWindowStateAndWait(ctx, tconn, w.ID, test.overviewWindowState)
 			}); err != nil {
-				s.Fatalf("Failed to set all windows to state %v: %v", windowState, err)
+				s.Fatalf("Failed to set all windows to state %v: %v", test.overviewWindowState, err)
+			}
+
+			if test.splitView {
+				var isWindowToSnap func(*ash.Window) bool
+				if test.splitViewUseBlank {
+					isWindowToSnap = isBlank
+				} else {
+					isWindowToSnap = func(*ash.Window) bool { return true }
+				}
+
+				w, err := ash.FindWindow(ctx, tconn, isWindowToSnap)
+				if err != nil {
+					s.Fatal("Failed to find window to snap: ", err)
+				}
+
+				if err := ash.SetWindowStateAndWait(ctx, tconn, w.ID, ash.WindowStateLeftSnapped); err != nil {
+					s.Fatal("Failed to snap window: ", err)
+				}
+
+				if err := ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
+					s.Fatal("Failed to exit overview: ", err)
+				}
 			}
 
 			// Wait for 3 seconds to stabilize the result. Note that this doesn't
@@ -152,19 +253,8 @@ func OverviewPerf(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to wait: ", err)
 			}
 
-			var suffix string
-			switch state {
-			case animationTypeMaximized:
-				suffix = "SingleClamshellMode"
-			case animationTypeNormalWindow:
-				suffix = "ClamshellMode"
-			case animationTypeTabletMode:
-				suffix = "TabletMode"
-			case animationTypeMinimizedTabletMode:
-				suffix = "MinimizedTabletMode"
-			}
-
-			r.RunMultiple(ctx, s, fmt.Sprintf("%s-%dwindows", suffix, currentWindows), perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
+			extraDescription := fmt.Sprintf(test.extraDescriptionFmt, overviewWindows)
+			r.RunMultiple(ctx, s, fmt.Sprintf("%s-%s", test.histogramSuffix, extraDescription), perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
 				if err = ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
 					return errors.Wrap(err, "failed to enter into the overview mode")
 				}
@@ -173,9 +263,9 @@ func OverviewPerf(ctx context.Context, s *testing.State) {
 				}
 				return nil
 			},
-				"Ash.Overview.AnimationSmoothness.Enter"+"."+suffix,
-				"Ash.Overview.AnimationSmoothness.Exit"+"."+suffix),
-				perfutil.StoreAll(perf.BiggerIsBetter, "percent", fmt.Sprintf("%dwindows", currentWindows)))
+				"Ash.Overview.AnimationSmoothness.Enter"+"."+test.histogramSuffix,
+				"Ash.Overview.AnimationSmoothness.Exit"+"."+test.histogramSuffix),
+				perfutil.StoreAll(perf.BiggerIsBetter, "percent", extraDescription))
 		}
 	}
 
