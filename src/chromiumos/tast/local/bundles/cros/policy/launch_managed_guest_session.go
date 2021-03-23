13e9f5a1943efbf5981a238ffc0422c52846c165
Jit Yao Yap <jityao@google.com>
2021-03-23 08:53:05 +0100

    Add chrome.login Extension API Tast test
    BUG=chromium:1187402
    TEST=TEST=tast run <ip> policy.LaunchManagedGuestSession
    
    Change-Id: Ib52e72710af24c6f324458cfa1afb60d3e549c0b

---
+++ src/chromiumos/tast/local/bundles/cros/policy/launch_managed_guest_session.go
@@ -0,0 +1,145 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/local/session"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LaunchManagedGuestSession,
+		Desc: "Test chrome.login.launchManagedGuestSession Extension API",
+		Contacts: []string{
+			"jityao@google.com", // Test author
+			"chromeos-commercial-identity@google.com",
+		},
+		Attr:         []string{"group:enrollment"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "fakeDMSEnrolled",
+	})
+}
+
+func LaunchManagedGuestSession(ctx context.Context, s *testing.State) {
+	fdms := s.FixtValue().(*fakedms.FakeDMS)
+
+	// Start a Chrome instance that will fetch policies from the FakeDMS.
+	cr, err := chrome.New(ctx,
+		chrome.FakeLogin(chrome.Creds{User: fixtures.Username, Pass: fixtures.Password}),
+		chrome.DMSPolicy(fdms.URL),
+		chrome.KeepState(),
+		chrome.ExtraArgs("--disable-policy-key-verification"))
+	if err != nil {
+		s.Fatal("Chrome login failed: ", err)
+	}
+
+	defer func(ctx context.Context) {
+		// Use cr as a reference to close the last started Chrome instance.
+		if err := cr.Close(ctx); err != nil {
+			s.Error("Failed to close Chrome connection: ", err)
+		}
+	}(ctx)
+
+	accountID := "foo@bar.com"
+	accountType := policy.AccountTypePublicSession
+
+	// These extensions are unlisted on the Chrome Web Store but can be
+	// downloaded directly using the extension IDs.
+	// ID for "Login screen APIs test extension".
+	loginScreenExtensionID := "oclffehlkdgibkainkilopaalpdobkan"
+	// ID for "Login screen APIs in-session test extension".
+	inSessionExtensionID := "ofcpkomnogjenhfajfjadjmjppbegnad"
+
+	policies := []policy.Policy{
+		&policy.DeviceLocalAccounts{
+			Val: []policy.DeviceLocalAccountInfo{
+				{
+					AccountID:   &accountID,
+					AccountType: &accountType,
+				},
+			},
+		},
+		&policy.DeviceLoginScreenExtensions{
+			Val: []string{loginScreenExtensionID},
+		},
+	}
+
+	pb := fakedms.NewPolicyBlob()
+	pb.AddPolicies(policies)
+	pb.AddPublicAccountPolicy(accountID, &policy.ExtensionInstallForcelist{
+		Val: []string{inSessionExtensionID},
+	})
+
+	if err := policyutil.ServeBlobAndRefresh(ctx, fdms, cr, pb); err != nil {
+		s.Fatal("Failed to kerve policies: ", err)
+	}
+
+	// Close the previous Chrome instance.
+	if err := cr.Close(ctx); err != nil {
+		s.Error("Failed to close Chrome connection: ", err)
+	}
+
+	// Restart Chrome.
+	cr, err = chrome.New(ctx,
+		chrome.NoLogin(),
+		chrome.DMSPolicy(fdms.URL),
+		chrome.KeepState(),
+		chrome.ExtraArgs("--disable-policy-key-verification"))
+	if err != nil {
+		s.Fatal("Chrome restart failed: ", err)
+	}
+
+	sm, err := session.NewSessionManager(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to session manager: ", err)
+	}
+
+	sw, err := sm.WatchSessionStateChanged(ctx, "started")
+	if err != nil {
+		s.Fatal("Failed to watch for D-Bus signals: ", err)
+	}
+	defer sw.Close(ctx)
+
+	loginScreenBGURL := chrome.ExtensionBackgroundPageURL(loginScreenExtensionID)
+	conn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(loginScreenBGURL))
+	if err != nil {
+		s.Fatal("Failed to connect to login screen background page: ", err)
+	}
+	defer conn.Close()
+
+	if err := conn.EvalPromise(ctx,
+		`new Promise((resolve, reject) => {
+		chrome.login.launchManagedGuestSession(() => {
+			if (chrome.runtime.lastError) {
+				reject(new Error(chrome.runtime.lastError.message));
+			}
+			resolve();
+		});
+	})`, nil); err != nil {
+		s.Fatal("Failed to launch MGS: ", err)
+	}
+
+	select {
+	case <-sw.Signals:
+		// Pass
+	case <-ctx.Done():
+		s.Fatal("Timeout before getting SessionStateChanged signal: ", err)
+	}
+
+	inSessionBGURL := chrome.ExtensionBackgroundPageURL(inSessionExtensionID)
+	conn2, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(inSessionBGURL))
+	if err != nil {
+		s.Fatal("Failed to connect to in-session background page: ", err)
+	}
+	defer conn2.Close()
+}
