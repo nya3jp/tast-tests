89766bf95e43999fa02289abd241d3852ab7ac36
Wei Lee <wtlee@google.com>
2021-03-24 12:15:17 +0800

    camera: Add GetUserMediaPerf.lacros test
    BUG=b:175168296
    TEST=tast run [DUT] camera.GetUserMediaPerf.lacros
    
    Change-Id: I01fe5894de5c2c95748a65cf842e20acfc814ffa

--- src/chromiumos/tast/local/bundles/cros/camera/get_user_media_perf.go
+++ src/chromiumos/tast/local/bundles/cros/camera/get_user_media_perf.go
@@ -11,6 +11,8 @@ import (
 	"chromiumos/tast/common/perf"
 	"chromiumos/tast/local/bundles/cros/camera/getusermedia"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/lacros"
+	"chromiumos/tast/local/lacros/launcher"
 	"chromiumos/tast/local/media/caps"
 	"chromiumos/tast/local/media/pre"
 	"chromiumos/tast/local/webrtc"
@@ -24,8 +26,20 @@ func init() {
 		Contacts:     []string{"shik@chromium.org", "chromeos-camera-eng@google.com"},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{caps.BuiltinOrVividCamera, "chrome", "camera_720p"},
-		Pre:          pre.ChromeCameraPerf(),
-		Data:         append(webrtc.DataFiles(), "getusermedia.html"),
+		Data:         append(webrtc.DataFiles(), launcher.DataArtifact, "getusermedia.html"),
+		Params: []testing.Param{
+			{
+				Pre: pre.ChromeCameraPerf(),
+				Val: lacros.ChromeTypeChromeOS,
+			},
+			{
+				Name:              "lacros",
+				Fixture:           "chromeCameraPerfLacros",
+				ExtraSoftwareDeps: []string{"lacros"},
+				Timeout:           7 * time.Minute, // A lenient limit for launching Lacros Chrome.
+				Val:               lacros.ChromeTypeLacros,
+			},
+		},
 	})
 }
 
@@ -38,9 +52,21 @@ func init() {
 // it uses "vivid" instead, which is the virtual video test driver and can be
 // used as an external USB camera.
 func GetUserMediaPerf(ctx context.Context, s *testing.State) {
+	var cr getusermedia.ChromeInterface
+	runLacros := s.Param().(lacros.ChromeType) == lacros.ChromeTypeLacros
+	if runLacros {
+		var err error
+		cr, err = launcher.LaunchLacrosChrome(ctx, s.FixtValue().(launcher.FixtData), s.DataPath(launcher.DataArtifact))
+		if err != nil {
+			s.Fatal("Failed to launch lacros-chrome: ", err)
+		}
+		defer cr.Close(ctx)
+	} else {
+		cr = s.PreValue().(*chrome.Chrome)
+	}
+
 	// Run tests for 20 seconds per resolution.
-	results := getusermedia.RunGetUserMedia(ctx, s, s.PreValue().(*chrome.Chrome), 20*time.Second,
-		getusermedia.NoVerboseLogging)
+	results := getusermedia.RunGetUserMedia(ctx, s, cr, 20*time.Second, getusermedia.NoVerboseLogging)
 
 	if !s.HasError() {
 		// Set and upload frame statistics below.
--- src/chromiumos/tast/local/media/pre/fixture.go
+++ src/chromiumos/tast/local/media/pre/fixture.go
@@ -52,6 +52,22 @@ func init() {
 		Vars:            launcher.LacrosFixtureVars,
 	})
 
+	testing.AddFixture(&testing.Fixture{
+		Name: "chromeCameraPerfLacros",
+		Desc: "Logged into a user session on Lacros without verbose logging that can affect the performance",
+		Impl: launcher.NewStartedByData(launcher.PreExist,
+			chrome.ExtraArgs(chromeBypassPermissionsArgs...),
+			chrome.LacrosExtraArgs(chromeBypassPermissionsArgs...),
+			chrome.ExtraArgs(chromeSuppressNotificationsArgs...),
+			chrome.LacrosExtraArgs(chromeSuppressNotificationsArgs...),
+		),
+		Parent:          "gpuWatchDog",
+		SetUpTimeout:    chrome.LoginTimeout + 7*time.Minute,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		Vars:            launcher.LacrosFixtureVars,
+	})
+
 	// Chrome has two said implementations: a "legacy" one and a Direct, VD-based on. Selecting one ore the other depends on the hardware and is ultimately determined by the overlays/ flags. Tests should be centered on what the users see, hence most of the testing should use chromeVideo, with a few test cases using this fixture.
 	testing.AddFixture(&testing.Fixture{
 		Name: "chromeAlternateVideoDecoder",
