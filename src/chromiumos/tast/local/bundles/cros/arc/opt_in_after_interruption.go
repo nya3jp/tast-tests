bed0cb7cfc952be69dcf23fd3681cbe380eaaa6a
Alan Ding <alanding@google.com>
2021-03-26 12:22:11 -0700

    tast-tests: Update timeout/delays based on UMA and refactor dumpLogCat
    There are upwards of 30 flakes per build for this test rendering it
    basically useless.  I did some research and realized our timeout and
    delays carried over from cheets autotest from NYC are outdated.
    
    Based on http://b/165923155 and latest UMA metrics:
    - ArcAuth.CheckinTime increases to 40 seconds or above for 90 percentile
    - ArcAuth.SignInTime increases to 80 seconds or above for 90 percentile
    
    https://uma.googleplex.com/p/chrome/timeline_v2/?sid=41a3f22ec65b8f3f6f64fd25f57cf493
    
    From this fact, timeout is increased to 90 seconds and extra set of
    pre-interruption delays above 40 seconds for unmanaged and managed.
    
    The 90 seconds provisioning timeout spec also matches arc.AuthPerf.*
    
    http://cs/chromeos_public/src/platform/tast-tests/src/chromiumos/tast/local/bundles/cros/arc/auth_perf.go?l=317&rcl=dd6612e2f9df9e70bb4ab5c8d679e07f57c41577
    
    Also refactor dumpLogCat out of optin test into arc optin lib for
    arc.OptInAfterInterruption to save logcat between attempts.
    
    BUG=b:179502877
    TEST=tast run $DUT arc.OptInAfterInterruption.*
    
    Change-Id: I742cfda937524dd8f0724655d9fd9fdf56521bbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2788848
    Reviewed-by: Muhammad Hasan Khan <mhasank@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Alan Ding <alanding@chromium.org>
    Commit-Queue: Alan Ding <alanding@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/opt_in_after_interruption.go
+++ src/chromiumos/tast/local/bundles/cros/arc/opt_in_after_interruption.go
@@ -6,17 +6,20 @@ package arc
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/arc/optin"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
 )
 
 const (
-	// Timeout to wait ARC provisioning is completed.
-	arcProvisionedWaitTimeOut = 60 * time.Second
+	// Timeout to wait ARC provisioning is completed. Based on UMA stats in released
+	// versions: http://uma/p/chrome/timeline_v2/?sid=206fdf1b0f02474ec11c6797f5668092
+	arcProvisionedWaitTimeOut = 90 * time.Second
 
 	// Interval to check ARC provisioning status.
 	arcProvisionedCheckInterval = 1 * time.Second
@@ -29,15 +32,11 @@ type optInTestParams struct {
 	chromeArgs []string        // Arguments to pass to Chrome command line.
 }
 
-// getUnmangedDelays is an initializer function that returns delays for unmanaged account tests
-func getUnmangedDelays() []time.Duration {
-	return []time.Duration{7 * time.Second, 17 * time.Second, 22 * time.Second, 32 * time.Second}
-}
+// unmangedDelays contains delays for unmanaged account tests
+var unmangedDelays = []time.Duration{7 * time.Second, 17 * time.Second, 22 * time.Second, 32 * time.Second, 42 * time.Second}
 
-// getManagedDelays is an initializer function that returns delays for managed account tests
-func getManagedDelays() []time.Duration {
-	return []time.Duration{10 * time.Second, 21 * time.Second, 26 * time.Second, 36 * time.Second}
-}
+// managedDelays contains delays for managed account tests
+var managedDelays = []time.Duration{10 * time.Second, 21 * time.Second, 26 * time.Second, 36 * time.Second, 46 * time.Second}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -57,7 +56,7 @@ func init() {
 			Val: optInTestParams{
 				username:   "arc.OptInAfterInterruption.unmanaged_username",
 				password:   "arc.OptInAfterInterruption.unmanaged_password",
-				delays:     getUnmangedDelays(),
+				delays:     unmangedDelays,
 				chromeArgs: []string{},
 			},
 		}, {
@@ -66,8 +65,8 @@ func init() {
 			Val: optInTestParams{
 				username:   "arc.OptInAfterInterruption.unmanaged_username",
 				password:   "arc.OptInAfterInterruption.unmanaged_password",
-				delays:     getUnmangedDelays(),
-				chromeArgs: []string{},
+				delays:     unmangedDelays,
+				chromeArgs: []string{"--ignore-arcvm-dev-conf"},
 			},
 		}, {
 			Name:              "managed",
@@ -75,7 +74,7 @@ func init() {
 			Val: optInTestParams{
 				username:   "arc.OptInAfterInterruption.managed_username",
 				password:   "arc.OptInAfterInterruption.managed_password",
-				delays:     getManagedDelays(),
+				delays:     managedDelays,
 				chromeArgs: []string{"--arc-force-show-optin-ui"},
 			},
 		}, {
@@ -84,8 +83,8 @@ func init() {
 			Val: optInTestParams{
 				username:   "arc.OptInAfterInterruption.managed_username",
 				password:   "arc.OptInAfterInterruption.managed_password",
-				delays:     getManagedDelays(),
-				chromeArgs: []string{"--arc-force-show-optin-ui"},
+				delays:     managedDelays,
+				chromeArgs: []string{"--arc-force-show-optin-ui", "--ignore-arcvm-dev-conf"},
 			},
 		}},
 		Vars: []string{
@@ -114,8 +113,7 @@ func OptInAfterInterruption(ctx context.Context, s *testing.State) {
 	password := s.RequiredVar(param.password)
 	extraArgs := param.chromeArgs
 
-	args := []string{"--arc-disable-app-sync", "--arc-disable-play-auto-install", "--ignore-arcvm-dev-conf"}
-	args = append(args, extraArgs...)
+	args := append(arc.DisableSyncFlags(), extraArgs...)
 
 	for _, delay := range param.delays {
 		s.Logf("Start iteration for %v delay", delay)
@@ -151,6 +149,9 @@ func OptInAfterInterruption(ctx context.Context, s *testing.State) {
 
 			s.Log("Wait for ARC to complete provisioning")
 			if err := waitForArcProvisioned(ctx, tconn); err != nil {
+				if err := optin.DumpLogCat(ctx, strconv.FormatInt(int64(delay), 10)); err != nil {
+					s.Logf("WARNING: Failed to dump logcat: %s", err)
+				}
 				s.Fatal("Failed to wait for ARC to complete provisioning: ", err)
 			}
 			s.Log("ARC is provisioned after restart")
