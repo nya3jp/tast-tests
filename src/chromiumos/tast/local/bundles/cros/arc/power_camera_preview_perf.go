f7113d4aa96265df0d67af5538d071ce0bc035cd
Michael Pishchagin <mblsha@google.com>
2021-03-29 12:20:09 +0000

    WIP: Testing ssh reconnection on kukui.
    https://crbug.com/1173390#c42 does some fixes, test it on CQ.
    
    BUG=b:180279505
    TEST=cq passes
    
    Change-Id: I0b243c19d45f55753d7396dd775194179e544dcc

--- src/chromiumos/tast/local/bundles/cros/arc/power_camera_preview_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/power_camera_preview_perf.go
@@ -6,23 +6,12 @@ package arc
 
 import (
 	"context"
-	"strconv"
 	"time"
 
-	"chromiumos/tast/common/perf"
-	"chromiumos/tast/ctxutil"
-	"chromiumos/tast/local/arc"
-	"chromiumos/tast/local/media/caps"
-	"chromiumos/tast/local/power"
-	"chromiumos/tast/local/power/setup"
+	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
-
-type powerCameraPreviewPerfArgs struct {
-	targetFPS     string
-	dischargeMode setup.BatteryDischargeMode
-}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -32,238 +21,36 @@ func init() {
 			"springerm@chromium.org",
 			"arcvm-eng@google.com",
 		},
-		SoftwareDeps: []string{"chrome", caps.BuiltinOrVividCamera},
-		Fixture:      "arcBootedWithDisableSyncFlags",
-		Attr:         []string{"group:crosbolt", "crosbolt_nightly"},
+		Attr: []string{"group:mainline"},
 		Params: []testing.Param{
 			{
-				Name:              "30fps",
-				ExtraSoftwareDeps: []string{"android_p"},
-				ExtraHardwareDeps: hwdep.D(hwdep.ForceDischarge()),
-				Val: powerCameraPreviewPerfArgs{
-					targetFPS:     "30",
-					dischargeMode: setup.ForceBatteryDischarge,
-				},
-			},
-			{
-				Name:              "60fps",
-				ExtraSoftwareDeps: []string{"android_p"},
-				ExtraHardwareDeps: hwdep.D(hwdep.ForceDischarge()),
-				Val: powerCameraPreviewPerfArgs{
-					targetFPS:     "60",
-					dischargeMode: setup.ForceBatteryDischarge,
-				},
-			},
-			{
-				Name:              "30fps_vm",
-				ExtraSoftwareDeps: []string{"android_vm"},
-				ExtraHardwareDeps: hwdep.D(hwdep.ForceDischarge()),
-				Val: powerCameraPreviewPerfArgs{
-					targetFPS:     "30",
-					dischargeMode: setup.ForceBatteryDischarge,
-				},
-			},
-			{
-				Name:              "60fps_vm",
-				ExtraSoftwareDeps: []string{"android_vm"},
-				ExtraHardwareDeps: hwdep.D(hwdep.ForceDischarge()),
-				Val: powerCameraPreviewPerfArgs{
-					targetFPS:     "60",
-					dischargeMode: setup.ForceBatteryDischarge,
-				},
-			},
-			{
-				Name:              "30fps_nobatterymetrics",
-				ExtraSoftwareDeps: []string{"android_p"},
-				ExtraHardwareDeps: hwdep.D(hwdep.NoForceDischarge()),
-				Val: powerCameraPreviewPerfArgs{
-					targetFPS:     "30",
-					dischargeMode: setup.NoBatteryDischarge,
-				},
-			},
-			{
-				Name:              "60fps_nobatterymetrics",
-				ExtraSoftwareDeps: []string{"android_p"},
-				ExtraHardwareDeps: hwdep.D(hwdep.NoForceDischarge()),
-				Val: powerCameraPreviewPerfArgs{
-					targetFPS:     "60",
-					dischargeMode: setup.NoBatteryDischarge,
-				},
-			},
-			{
-				Name:              "30fps_vm_nobatterymetrics",
-				ExtraSoftwareDeps: []string{"android_vm"},
-				ExtraHardwareDeps: hwdep.D(hwdep.NoForceDischarge()),
-				Val: powerCameraPreviewPerfArgs{
-					targetFPS:     "30",
-					dischargeMode: setup.NoBatteryDischarge,
-				},
-			},
-			{
-				Name:              "60fps_vm_nobatterymetrics",
-				ExtraSoftwareDeps: []string{"android_vm"},
-				ExtraHardwareDeps: hwdep.D(hwdep.NoForceDischarge()),
-				Val: powerCameraPreviewPerfArgs{
-					targetFPS:     "60",
-					dischargeMode: setup.NoBatteryDischarge,
-				},
+				Name:              "test_kukui_discharge",
+				ExtraHardwareDeps: hwdep.D(hwdep.Platform("kukui")),
+				Timeout:           15 * time.Minute,
 			},
 		},
-		Timeout: 10 * time.Minute,
 	})
 }
 
 func PowerCameraPreviewPerf(ctx context.Context, s *testing.State) {
-	const (
-		cameraAppActivity      = ".CameraActivity"
-		cameraAppApk           = "ArcCameraFpsTest.apk"
-		cameraAppPackage       = "org.chromium.arc.testapp.camerafps"
-		intentGetDroppedFrames = "org.chromium.arc.testapp.camerafps.ACTION_GET_NUM_DROPPED_FRAMES"
-		intentGetHistogram     = "org.chromium.arc.testapp.camerafps.ACTION_GET_HISTOGRAM"
-		intentGetPreviewSize   = "org.chromium.arc.testapp.camerafps.ACTION_GET_PREVIEW_SIZE"
-		intentGetTotalFrames   = "org.chromium.arc.testapp.camerafps.ACTION_GET_NUM_FRAMES"
-		intentResetData        = "org.chromium.arc.testapp.camerafps.ACTION_RESET_HISTOGRAM"
-		intentSetFps           = "org.chromium.arc.testapp.camerafps.ACTION_SET_TARGET_FPS"
-	)
-
-	// Give cleanup actions a minute to run, even if we fail by exceeding our
-	// deadline.
-	cleanupCtx := ctx
-	ctx, cancel := ctxutil.Shorten(ctx, time.Minute)
-	defer cancel()
-
-	cr := s.FixtValue().(*arc.PreData).Chrome
-
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to create Test API connection: ", err)
+	testing.ContextLog(ctx, "ectool chargecontrol discharge")
+	dischargeCmd := testexec.CommandContext(ctx,
+		"ectool", "chargecontrol", "discharge")
+	if err := dischargeCmd.Run(); err != nil {
+		s.Error("Failed to run discharge: ", err)
 	}
 
-	sup, cleanup := setup.New("camera preview power and fps")
-
-	defer func() {
-		if err := cleanup(cleanupCtx); err != nil {
-			s.Error("Cleanup failed: ", err)
-		}
-	}()
-
-	args := s.Param().(powerCameraPreviewPerfArgs)
-	sup.Add(setup.PowerTest(ctx, tconn, setup.PowerTestOptions{
-		Wifi: setup.DisableWifiInterfaces, Battery: args.dischargeMode, NightLight: setup.DisableNightLight}))
-
-	// Install camera testing app.
-	a := s.FixtValue().(*arc.PreData).ARC
-	sup.Add(setup.InstallApp(ctx, a, arc.APKPath(cameraAppApk), cameraAppPackage))
-
-	// Grant permissions to activity.
-	sup.Add(setup.GrantAndroidPermission(ctx, a, cameraAppPackage, "android.permission.CAMERA"))
-
-	// Wait until CPU is cooled down.
-	if _, err := power.WaitUntilCPUCoolDown(ctx, power.CoolDownPreserveUI); err != nil {
-		s.Fatal("CPU failed to cool down: ", err)
-	}
-
-	// Start camera testing app.
-	sup.Add(setup.StartActivity(ctx, tconn, a, cameraAppPackage, cameraAppActivity))
-
-	if err := sup.Check(ctx); err != nil {
-		s.Fatal("Setup failed: ", err)
-	}
-
-	const (
-		// TODO(springerm): Make iteration count an optional command line parameter.
-		iterationCount            = 30
-		iterationDuration         = 2 * time.Second
-		afterBootWarmupDuration   = 30 * time.Second
-		cameraResetWarmupDuration = 30 * time.Second
-	)
-
-	s.Log("Warmup: Waiting for Android to settle down")
-	if err := testing.Sleep(ctx, afterBootWarmupDuration); err != nil {
+	testing.ContextLog(ctx, "sleep 1")
+	if err := testing.Sleep(ctx, time.Second); err != nil {
 		s.Fatal("Failed to sleep: ", err)
 	}
 
-	s.Logf("Set target FPS: %v FPS", args.targetFPS)
-	if _, err = a.BroadcastIntent(ctx, intentSetFps, "--ei", "fps", args.targetFPS); err != nil {
-		s.Fatal("Could not send intent: ", err)
+	testing.ContextLog(ctx, "ectool chargecontrol normal")
+	chargeCmd := testexec.CommandContext(ctx,
+		"ectool", "chargecontrol", "normal")
+	if err := chargeCmd.Run(); err != nil {
+		s.Error("Failed to run charge: ", err)
 	}
 
-	resolution, err := a.BroadcastIntentGetData(ctx, intentGetPreviewSize)
-	if err != nil {
-		s.Fatal("Failed to query resolution from activity: ", err)
-	}
-	s.Log("Camera preview resolution: ", resolution)
-
-	// Create metrics. We report separately for each target FPS.
-	numFramesMetric := perf.Metric{Name: "total_num_frames", Unit: "frames", Direction: perf.BiggerIsBetter}
-	numDroppedFramesMetric := perf.Metric{Name: "num_dropped_frames", Unit: "frames", Direction: perf.SmallerIsBetter}
-	frameDropRatioMetric := perf.Metric{Name: "frame_drop_ratio", Unit: "ratio", Direction: perf.SmallerIsBetter}
-
-	powerMetrics, err := perf.NewTimeline(ctx, power.TestMetrics(), perf.Interval(iterationDuration))
-	if err != nil {
-		s.Fatal("Failed to build metrics: ", err)
-	}
-
-	if err := powerMetrics.Start(ctx); err != nil {
-		s.Fatal("Failed to start metrics: ", err)
-	}
-
-	s.Log("Warmup: Waiting a bit before starting the measurement")
-	if err := testing.Sleep(ctx, cameraResetWarmupDuration); err != nil {
-		s.Fatal("Failed to sleep: ", err)
-	}
-
-	s.Log("Starting measurement")
-
-	if _, err = a.BroadcastIntent(ctx, intentResetData); err != nil {
-		s.Fatal("Could not send intent: ", err)
-	}
-
-	// Keep camera running and record power usage.
-	if err := powerMetrics.StartRecording(ctx); err != nil {
-		s.Fatal("Failed to start recording: ", err)
-	}
-
-	if err := testing.Sleep(ctx, iterationCount*iterationDuration); err != nil {
-		s.Fatal("Failed to sleep: ", err)
-	}
-
-	p, err := powerMetrics.StopRecording(ctx)
-	if err != nil {
-		s.Fatal("Error while recording power metrics: ", err)
-	}
-
-	droppedFrames := 0
-	if o, err := a.BroadcastIntentGetData(ctx, intentGetDroppedFrames); err != nil {
-		s.Fatal("Could not send intent: ", err)
-	} else if droppedFrames, err = strconv.Atoi(o); err != nil {
-		s.Fatal("Unexpected result from intent " + intentGetDroppedFrames + ": " + o)
-	}
-
-	totalFrames := 0
-	if o, err := a.BroadcastIntentGetData(ctx, intentGetTotalFrames); err != nil {
-		s.Fatal("Could not send intent: ", err)
-	} else if totalFrames, err = strconv.Atoi(o); err != nil {
-		s.Fatal("Unexpected result from intent " + intentGetTotalFrames + ": " + o)
-	}
-
-	p.Set(numFramesMetric, float64(totalFrames))
-	p.Set(numDroppedFramesMetric, float64(droppedFrames))
-
-	if totalFrames == 0 {
-		s.Fatal("Camera app did not receive any frames")
-	} else {
-		p.Set(frameDropRatioMetric, float64(droppedFrames)/float64(totalFrames))
-	}
-
-	// Print frame duration histogram to log file.
-	o, err := a.BroadcastIntentGetData(ctx, intentGetHistogram)
-	if err != nil {
-		s.Fatal("Could not send intent: ", err)
-	}
-	s.Logf("Frame duration histogram: %q", o)
-
-	if err := p.Save(s.OutDir()); err != nil {
-		s.Error("Failed saving perf data: ", err)
-	}
+	testing.ContextLog(ctx, "done")
 }
