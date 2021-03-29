e702afdc3ba6016ca2e6ea188ee1512bdcb20d0c
tby <tby@chromium.org>
2021-03-29 17:27:33 +1100

    security.StatefulFules: add structured metrics directories
    BUG=chromium:1148168
    TEST=tast run ... security.StatefulFiles
    
    Change-Id: I72a3805c9e51dd69e486a9c5efc25a239b716fa4

--- src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
+++ src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
@@ -79,6 +79,10 @@ func StatefulFiles(ctx context.Context, s *testing.State) {
 		chk.NewPattern(chk.Tree("encrypted/var/lib/dhcpcd"), chk.Users("dhcp"), chk.Groups("dhcp"), chk.NotMode(022)),
 		chk.NewPattern(chk.Path("encrypted/var/lib/gentoo"), chk.Users("root"), chk.NotMode(022), chk.SkipChildren()),
 		chk.NewPattern(chk.Tree("encrypted/var/lib/imageloader"), chk.Users("imageloaderd"), chk.Groups("imageloaderd"), chk.NotMode(022)),
+		chk.NewPattern(chk.Path("encrypted/var/lib/metrics/structured/events"), chk.Users("metrics", "root"), chk.Groups("metrics", "root"), chk.Mode(0777)),              // directory itself
+		chk.NewPattern(chk.Tree("encrypted/var/lib/metrics/structured/events"), chk.Mode(0666), chk.SkipChildren()),                                                       // children
+		chk.NewPattern(chk.Path("encrypted/var/lib/metrics/structured"), chk.Users("metrics", "root"), chk.Groups("metrics", "root"), chk.Mode(0755)),                     // directory itself
+		chk.NewPattern(chk.Tree("encrypted/var/lib/metrics/structured"), chk.Users("metrics", "root"), chk.Groups("metrics", "root"), chk.Mode(0666), chk.SkipChildren()), // children
 		chk.NewPattern(chk.Path("encrypted/var/lib/metrics/uma-events"), chk.Users("metrics"), chk.Groups("metrics"), chk.Mode(0666)),
 		chk.NewPattern(chk.Path("encrypted/var/lib/metrics"), chk.Users("metrics"), chk.Groups("metrics"), chk.Mode(0755)),                                     // directory itself
 		chk.NewPattern(chk.Tree("encrypted/var/lib/metrics"), chk.Users("metrics", "root"), chk.Groups("metrics", "root"), chk.Mode(0644), chk.SkipChildren()), // children
