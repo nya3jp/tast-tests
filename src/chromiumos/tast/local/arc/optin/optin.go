7c34c82c21da75af23471f7f08eb65ad36f26477
Muhammad Hasan Khan <mhasank@chromium.org>
2021-03-29 02:07:58 +0000

    tast-tests:  add error for failing to load tos in arc.Optin
    BUG=b:177341225
    TEST=None
    
    Change-Id: I821507edde354b2d59fe72ae1a6f8e2525523dee

--- src/chromiumos/tast/local/arc/optin/optin.go
+++ src/chromiumos/tast/local/arc/optin/optin.go
@@ -77,7 +77,7 @@ func FindOptInExtensionPageAndAcceptTerms(ctx context.Context, cr *chrome.Chrome
 
 	var msg string
 	if err := conn.Eval(ctx, fmt.Sprintf("%s?%s:''", errorPageLoaded, errorMessage), &msg); err == nil && msg != "" {
-		return errors.New(msg)
+		return errors.New(fmt.Sprintf("failed to load terms of service page: %s", msg))
 	}
 
 	if err := conn.Eval(ctx, "termsPage.onAgree()", nil); err != nil {
