6a514f3aae1bf5b210b7b470b5bd2d2a64aaae7c
Xibin Liu <xliu@cienet.com>
2021-03-29 14:56:12 -0700

    tast-tests: add helper functions for shill wifi
    Add functions to allow WiFi enable/disable/connectAP/forgetAP/etc.
    The QuickCheckCUJ requires to connect to a given WiFi AP and checks
    the WiFi connectivity before and after suspend/resume or lock/unlock.
    With this CL, WiFi operations can be performed through shill DBUS
    instead of from system settings.
    
    BUG=None
    TEST=./fast_build.sh -T
    
    Change-Id: I0f9a1639f19387928d1407f337dddb7d3a9209ed

--- src/chromiumos/tast/local/shill/wifi.go
+++ src/chromiumos/tast/local/shill/wifi.go
@@ -8,11 +8,16 @@ package shill
 
 import (
 	"context"
+	"encoding/hex"
+	"strings"
 	"time"
 
 	"chromiumos/tast/common/shillconst"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
 )
+
+const wifiDefaultTimeout = 30 * time.Second
 
 // WifiInterface polls the WiFi interface name with timeout.
 // It returns "" with error if no (or more than one) WiFi interface is found.
@@ -55,4 +60,188 @@ func WifiInterface(ctx context.Context, m *Manager, timeout time.Duration) (stri
 			return "", err
 		}
 	}
+}
+
+// WiFiManager manages WiFi services, profiles, and devices through shill Manager.
+type WiFiManager struct {
+	m *Manager // shill manager
+}
+
+// NewWiFiManager makes sure WiFi interface is available and returns WiFi Manager.
+func NewWiFiManager(ctx context.Context) (*WiFiManager, error) {
+	m, err := NewManager(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create shill Manager object")
+	}
+	// Make sure WiFi interface is available.
+	if _, err := WifiInterface(ctx, m, 5*time.Second); err != nil {
+		return nil, errors.Wrap(err, "could not get system WiFi interface")
+	}
+	return &WiFiManager{m: m}, nil
+}
+
+// Enable enables or disables the WiFi network according to the given enable flag.
+func (wifi *WiFiManager) Enable(ctx context.Context, enable bool) error {
+	enabled, err := wifi.m.IsEnabled(ctx, TechnologyWifi)
+	if err != nil {
+		return errors.Wrap(err, "failed to get WiFi enabled state")
+	}
+	if enabled == enable {
+		return nil
+	}
+
+	if enable {
+		if err := wifi.m.EnableTechnology(ctx, TechnologyWifi); err != nil {
+			return errors.Wrap(err, "could not enable wifi via shill")
+		}
+	} else {
+		if err := wifi.m.DisableTechnology(ctx, TechnologyWifi); err != nil {
+			return errors.Wrap(err, "could not disable wifi via shill")
+		}
+	}
+
+	watcher, err := wifi.m.CreateWatcher(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create a shill manager watcher")
+	}
+	defer watcher.Close(ctx)
+
+	// Check enabled status change within the default timeout interval.
+	wCtx, cancel := context.WithTimeout(ctx, wifiDefaultTimeout)
+	defer cancel()
+	for {
+		enabled, err = wifi.m.IsEnabled(wCtx, TechnologyWifi)
+		if err != nil {
+			return errors.Wrap(err, "failed to get WiFi enabled state")
+		}
+		if enabled == enable {
+			return nil
+		}
+		if _, err := watcher.WaitAll(wCtx, shillconst.ManagerPropertyEnabledTechnologies); err != nil {
+			return errors.Wrap(err, "failed waiting for enabled status to change")
+		}
+	}
+}
+
+// Connected returns true if any WiFi AP is connected.
+func (wifi *WiFiManager) Connected(ctx context.Context) (bool, error) {
+	props := map[string]interface{}{
+		shillconst.ServicePropertyType:        shillconst.TypeWifi,
+		shillconst.ServicePropertyIsConnected: true,
+	}
+
+	if _, err := wifi.m.FindMatchingService(ctx, props); err != nil {
+		if err.Error() == shillconst.ErrorMatchingServiceNotFound {
+			return false, nil
+		}
+		return false, errors.Wrap(err, "failed to find the WiFi AP")
+	}
+
+	return true, nil
+}
+
+// APConnected returns true if the given WiFi AP is connected.
+func (wifi *WiFiManager) APConnected(ctx context.Context, ssid string) (bool, error) {
+	props := map[string]interface{}{
+		shillconst.ServicePropertyType:        shillconst.TypeWifi,
+		shillconst.ServicePropertyWiFiHexSSID: strings.ToUpper(hex.EncodeToString([]byte(ssid))),
+		shillconst.ServicePropertyVisible:     true, // only find visible APs.
+	}
+
+	service, err := wifi.m.FindMatchingService(ctx, props)
+	if err != nil {
+		if err.Error() == shillconst.ErrorMatchingServiceNotFound {
+			return false, nil
+		}
+		return false, errors.Wrap(err, "failed to find the WiFi AP")
+	}
+
+	connected, err := service.IsConnected(ctx)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to get the WiFi service connected status")
+	}
+	return connected, nil
+}
+
+// ConnectAP connects to a given WiFi AP identified by SSID.
+func (wifi *WiFiManager) ConnectAP(ctx context.Context, ssid, passphrase string) error {
+	props := map[string]interface{}{
+		shillconst.ServicePropertyType:        shillconst.TypeWifi,
+		shillconst.ServicePropertyWiFiHexSSID: strings.ToUpper(hex.EncodeToString([]byte(ssid))),
+		shillconst.ServicePropertyVisible:     true, // only find visible APs.
+	}
+	var service *Service
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		var err error
+		service, err = wifi.m.FindMatchingService(ctx, props)
+		if err == nil {
+			return nil
+		}
+		// Scan WiFi AP again if the expected AP is not found.
+		if err := wifi.m.RequestScan(ctx, TechnologyWifi); err != nil {
+			return testing.PollBreak(errors.Wrap(err, "failed to request wifi active scan"))
+		}
+		return err
+	}, &testing.PollOptions{
+		Timeout:  wifiDefaultTimeout,
+		Interval: 200 * time.Millisecond,
+	}); err != nil {
+		return errors.Wrap(err, "failed to find the WiFi AP")
+	}
+
+	connected, err := service.IsConnected(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get the WiFi service connected status")
+	}
+	if connected {
+		return nil
+	}
+
+	if err := service.SetProperty(ctx, shillconst.ServicePropertyPassphrase, passphrase); err != nil {
+		return errors.Wrap(err, "failed to set service passphrase")
+	}
+
+	if err := service.Connect(ctx); err != nil {
+		return errors.Wrap(err, "failed to connect to service")
+	}
+
+	watcher, err := service.CreateWatcher(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create service watcher")
+	}
+	defer watcher.Close(ctx)
+
+	// Check connected state change within the default timeout interval.
+	wCtx, cancel := context.WithTimeout(ctx, wifiDefaultTimeout)
+	defer cancel()
+	for {
+		connected, err = service.IsConnected(wCtx)
+		if err != nil {
+			return errors.Wrap(err, "failed to get WiFi connected state")
+		}
+		if connected {
+			return nil
+		}
+		if _, err := watcher.WaitAll(wCtx, shillconst.ServicePropertyState); err != nil {
+			return errors.Wrap(err, "failed waiting for service state to change")
+		}
+	}
+}
+
+// ForgetAP removes the WiFi AP from user profile.
+func (wifi *WiFiManager) ForgetAP(ctx context.Context, ssid string) error {
+	props := map[string]interface{}{
+		shillconst.ServicePropertyType:        shillconst.TypeWifi,
+		shillconst.ServicePropertyWiFiHexSSID: strings.ToUpper(hex.EncodeToString([]byte(ssid))),
+	}
+
+	service, err := wifi.m.FindMatchingService(ctx, props)
+	if err != nil {
+		return errors.Wrap(err, "cannot find the given WiFi AP service")
+	}
+
+	if err := service.Remove(ctx); err != nil {
+		return errors.Wrap(err, "failed to remove the service")
+	}
+	return nil
 }
