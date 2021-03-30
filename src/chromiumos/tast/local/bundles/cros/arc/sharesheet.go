283c402d0a3494697addb96fda8b94913166b052
Ben Reich <benreich@chromium.org>
2021-03-30 02:53:27 +0000

    tast-tests: Port arc.Sharesheet to uiauto
    On both the nocturne and kodama devices it's observed the old ui library
    is picking the incorrect position on the screen for the target app.
    Porting to uiauto as it seems to be more reliable in picking the right
    position.
    
    BUG=chromium:1156310
    TEST=tast run {kodama,nocturne} arc.Sharesheet
    
    Change-Id: I89b69d9cc0fe430a23aec1ea182688e038090fda

--- src/chromiumos/tast/local/bundles/cros/arc/sharesheet.go
+++ src/chromiumos/tast/local/bundles/cros/arc/sharesheet.go
@@ -11,13 +11,16 @@ import (
 	"path/filepath"
 	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/android/ui"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/ui/filesapp"
-	"chromiumos/tast/local/chrome/ui/sharesheet"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/testing"
 )
 
@@ -59,6 +62,10 @@ func Sharesheet(ctx context.Context, s *testing.State) {
 	username := s.RequiredVar("arc.Sharesheet.username")
 	password := s.RequiredVar("arc.Sharesheet.password")
 
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
 	// Setup Chrome.
 	cr, err := chrome.New(ctx,
 		chrome.GAIALogin(chrome.Creds{User: username, Pass: password}),
@@ -67,7 +74,7 @@ func Sharesheet(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
-	defer cr.Close(ctx)
+	defer cr.Close(cleanupCtx)
 
 	// Setup the test file.
 	testFileLocation := filepath.Join(filesapp.DownloadPath, expectedFileName)
@@ -81,15 +88,35 @@ func Sharesheet(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to create test API connection: ", err)
 	}
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	sr, err := uiauto.NewScreenRecorder(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to initialize the screen recorder: ", err)
+	}
+
+	if err := sr.Start(ctx, tconn); err != nil {
+		s.Fatal("Failed to start the screen recorder: ", err)
+	}
+	defer func() {
+		if err := sr.Stop(cleanupCtx); err != nil {
+			testing.ContextLog(cleanupCtx, "Failed to stop screen recorder: ", err)
+		}
+		if s.HasError() {
+			if err := sr.SaveInBytes(cleanupCtx, filepath.Join(s.OutDir(), "recording.webm")); err != nil {
+				testing.ContextLog(cleanupCtx, "Failed to save in bytes: ", err)
+			}
+		}
+		sr.Release(cleanupCtx)
+	}()
 
 	// Setup ARC device and UI Automator.
 	arcDevice, uiAutomator, err := setUpARC(ctx, cr, s.OutDir())
 	if err != nil {
 		s.Fatal("Failed to setup ARC: ", err)
 	}
-	defer arcDevice.Close(ctx)
-	defer uiAutomator.Close(ctx)
+	defer arcDevice.Close(cleanupCtx)
+	defer uiAutomator.Close(cleanupCtx)
 
 	if err := arcDevice.Install(ctx, arc.APKPath("ArcChromeSharesheetTest.apk")); err != nil {
 		s.Fatal("Failed to install the APK: ", err)
@@ -100,18 +127,19 @@ func Sharesheet(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to launch the Files app: ", err)
 	}
-	defer files.Release(ctx)
 
-	if err := files.OpenDownloads(ctx); err != nil {
-		s.Fatal("Failed to navigate to Downloads directory: ", err)
-	}
+	// The Sharesheet appears to not properly update the accessibility tree with
+	// the coordinates whilst animating. The total time to animate is currently 150ms
+	// so setting to 1s to ensure low-end devices are given enough time.
+	sharesheet := uiauto.New(tconn).WithInterval(time.Second)
+	sharesheetTargetButton := nodewith.Role(role.Button).NameContaining(appShareLabel).ClassName("SharesheetTargetButton")
 
-	if err := files.SelectContextMenu(ctx, expectedFileName, filesapp.Share); err != nil {
-		s.Fatal("Failed to click share button in context menu: ", err)
-	}
-
-	if err := sharesheet.ClickApp(ctx, tconn, appShareLabel); err != nil {
-		s.Fatal("Failed to click app on stable sharesheet: ", err)
+	if err := uiauto.Combine("Open Downloads and Click sharesheet",
+		files.OpenDownloads(),
+		files.ClickContextMenuItem(expectedFileName, filesapp.Share),
+		sharesheet.LeftClick(sharesheetTargetButton),
+	)(ctx); err != nil {
+		s.Fatal("Failed to open downloads and click share button: ", err)
 	}
 
 	// Wait for the file contents to show in the Android test app.
