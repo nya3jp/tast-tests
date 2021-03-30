e2797a118e6fc7ac7f82c4bb2649733468bd739c
Victor-Gabriel Savu <vsavu@google.com>
2021-03-30 15:02:03 +0200

    faillog: add DumpIOTreeWithScreenshotOnError
    Add DumpIOTreeWithScreenshotOnError to capture both the state of
    the UI and a Screenshot when a failure occurs.
    
    BUG=None
    TEST=tast run <ip> policy.ShowHomeButton
    
    Change-Id: If9b12d0c655ec505ebfbd72af35d73c975f8b6d1

--- src/chromiumos/tast/local/bundles/cros/policy/show_home_button.go
+++ src/chromiumos/tast/local/bundles/cros/policy/show_home_button.go
@@ -62,7 +62,7 @@ func ShowHomeButton(ctx context.Context, s *testing.State) {
 		},
 	} {
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
-			defer faillog.DumpUITreeOnErrorToFile(ctx, s.OutDir(), s.HasError, tconn, "ui_tree_"+param.name+".txt")
+			defer faillog.DumpIOTreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
 
 			// Perform cleanup.
 			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
