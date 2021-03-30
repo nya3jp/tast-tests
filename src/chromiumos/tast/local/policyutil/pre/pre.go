3c1e7fc77471be3390a36b4f8841985115847af6
Victor-Gabriel Savu <vsavu@google.com>
2021-03-30 11:24:51 +0200

    policyutil: document the precondition as deprecated
    Mark chromiumos/tast/local/policyutil/pre as deprecated since
    the fixture has landed and test have been migrated to it.
    
    BUG=chromium:1192497
    TEST=None
    
    Change-Id: I3f92b74a620dde64dfaa65572358fe9dd3fb99d5

--- src/chromiumos/tast/local/policyutil/pre/pre.go
+++ src/chromiumos/tast/local/policyutil/pre/pre.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Package pre contains preconditions for policy tests.
+// DEPRECATED: Use the "chromiumos/tast/local/policyutil/fixtures" package and
+//             the "chromePolicyLoggedIn" fixture instead.
 package pre
 
 import (
