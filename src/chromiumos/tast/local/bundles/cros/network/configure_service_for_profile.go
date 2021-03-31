734417eec86e6c159bd0aa2ef2a5b27059db6b75
Trent Begin <tbegin@google.com>
2021-03-31 10:50:12 -0600

    shill: add fixture to reset shill state after each test
    There is some repeated code in the network tests that ensure that any
    modifications to shill are reset between tests. This fixture ensures
    that shill is in a known default state on start and resets it after the
    test is completed. This change also adds the fixture to the
    network.ConfigureServiceForProfileTest.
    
    BUG=chromium:1178896
    TEST=tast run sarien-dut network.ConfigureServiceForProfileTest (pass)
    
    Change-Id: Ia0b241531e4f59a389b5d4c81a5161d7fbae2dc7

--- src/chromiumos/tast/local/bundles/cros/network/configure_service_for_profile.go
+++ src/chromiumos/tast/local/bundles/cros/network/configure_service_for_profile.go
@@ -6,11 +6,9 @@ package network
 
 import (
 	"context"
-	"os"
 	"time"
 
 	"chromiumos/tast/common/shillconst"
-	"chromiumos/tast/local/network"
 	"chromiumos/tast/local/shill"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
@@ -23,47 +21,19 @@ func init() {
 		Contacts: []string{
 			"matthewmwang@chromium.org",
 		},
-		Attr: []string{"group:mainline", "informational"},
+		Attr:    []string{"group:mainline", "informational"},
+		Fixture: "shillReset",
 	})
 }
 
 func ConfigureServiceForProfile(ctx context.Context, s *testing.State) {
 	const (
-		filePath   = shillconst.DefaultProfilePath
 		objectPath = shillconst.DefaultProfileObjectPath
 	)
-
-	// We lose connectivity along the way here, and if that races with the
-	// recover_duts network-recovery hooks, it may interrupt us.
-	unlock, err := network.LockCheckNetworkHook(ctx)
-	if err != nil {
-		s.Fatal("Failed to lock the check network hook: ", err)
-	}
-	defer unlock()
-
-	// Stop shill temporarily and remove the default profile.
-	if err := upstart.StopJob(ctx, "shill"); err != nil {
-		s.Fatal("Failed stopping shill: ", err)
-	}
-	os.Remove(filePath)
-	if err := upstart.RestartJob(ctx, "shill"); err != nil {
-		s.Fatal("Failed starting shill: ", err)
-	}
 
 	manager, err := shill.NewManager(ctx)
 	if err != nil {
 		s.Fatal("Failed creating shill manager proxy: ", err)
-	}
-
-	// Clean up custom services on exit.
-	defer func() {
-		upstart.StopJob(ctx, "shill")
-		os.Remove(filePath)
-		upstart.RestartJob(ctx, "shill")
-	}()
-
-	if err = manager.PopAllUserProfiles(ctx); err != nil {
-		s.Fatal("Failed to pop user profiles: ", err)
 	}
 
 	props := map[string]interface{}{
