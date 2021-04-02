4ee5c21da1e7a0d92ea5a0f2b9bcf41d7113b1f0
Tom Hughes <tomhughes@chromium.org>
2021-04-02 10:52:09 -0700

    firmware: Add FirmwareTest struct
    This is the first step in refactoring the common setup code into a
    single location.
    
    BUG=b:153736350, b:183151135
    TEST=tast run <IP> firmware.FpRDP0 firmware.FpAddEntropy
    
    Change-Id: I1b9d3af8c86e028600df47b8d2636737a5076215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2802692
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: David Stevens <stevensd@chromium.org>

--- src/chromiumos/tast/remote/bundles/cros/firmware/fp_add_entropy.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/fp_add_entropy.go
@@ -10,8 +10,6 @@ import (
 
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/remote/firmware/fingerprint"
-	"chromiumos/tast/remote/servo"
-	"chromiumos/tast/rpc"
 	"chromiumos/tast/services/cros/platform"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
@@ -36,7 +34,17 @@ func init() {
 }
 
 func FpAddEntropy(ctx context.Context, s *testing.State) {
-	d := s.DUT()
+	t, err := fingerprint.NewFirmwareTest(ctx, s.DUT(), s.RequiredVar("servo"), s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to create new firmware test: ", err)
+	}
+	ctxForCleanup := ctx
+	defer t.Close(ctxForCleanup)
+	ctx, cancel := ctxutil.Shorten(ctx, fingerprint.TimeForCleanup)
+	defer cancel()
+
+	d := t.DUT()
+	pxy := t.Servo()
 
 	fpBoard, err := fingerprint.Board(ctx, d)
 	if err != nil {
@@ -50,19 +58,6 @@ func FpAddEntropy(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to validate build firmware file: ", err)
 	}
 
-	pxy, err := servo.NewProxy(ctx, s.RequiredVar("servo"), d.KeyFile(), d.KeyDir())
-	if err != nil {
-		s.Fatal("Failed to connect to servo: ", err)
-	}
-	cleanupCtx := ctx
-	ctx, cancel := ctxutil.Shorten(ctx, 2*time.Minute)
-	defer cancel()
-	defer func(ctx context.Context) {
-		testing.ContextLog(ctx, "Tearing down")
-		defer pxy.Close(ctx)
-		fingerprint.ReimageFPMCU(ctx, d, pxy)
-	}(cleanupCtx)
-
 	if err := fingerprint.InitializeKnownState(ctx, d, s.OutDir(), pxy); err != nil {
 		s.Fatal("Initialization failed: ", err)
 	}
@@ -73,11 +68,7 @@ func FpAddEntropy(ctx context.Context, s *testing.State) {
 		s.Fatal("Initialization failed: ", err)
 	}
 
-	cl, err := rpc.Dial(ctx, d, s.RPCHint(), "cros")
-	if err != nil {
-		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
-	}
-	defer cl.Close(ctx)
+	cl := t.RPCClient()
 
 	upstartService := platform.NewUpstartServiceClient(cl.Conn)
 
