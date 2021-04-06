3b0cca08482a37aef96018fcf5371253b83d4aa8
Yi Chou <yich@google.com>
2021-04-06 11:15:35 +0800

    hwsec: Make DaemonsRestartStress critical.
    The test has been passing across 20+ revisions since 13869.0.0.
    
    https://tastboard.corp.google.com/test/hwsec.DaemonsRestartStress
    
    BUG=b:172297915
    TEST=CQ
    
    Change-Id: I00c18d7055ea25eadd468067e530fa3d64e934c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2807216
    Commit-Queue: Yi Chou <yich@google.com>
    Tested-by: Yi Chou <yich@google.com>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>

--- src/chromiumos/tast/local/bundles/cros/hwsec/daemons_restart_stress.go
+++ src/chromiumos/tast/local/bundles/cros/hwsec/daemons_restart_stress.go
@@ -23,8 +23,17 @@ func init() {
 			"yich@chromium.org",
 		},
 		SoftwareDeps: []string{"tpm"},
-		Attr:         []string{"group:mainline", "informational"},
-		Timeout:      10 * time.Minute,
+		Attr:         []string{"group:mainline"},
+		Params: []testing.Param{{
+			Name:              "tpm1",
+			ExtraSoftwareDeps: []string{"tpm1"},
+			ExtraAttr:         []string{"informational"},
+			Timeout:           4 * time.Minute,
+		}, {
+			Name:              "tpm2",
+			ExtraSoftwareDeps: []string{"tpm2"},
+			// No ExtraAttr; this test is critical.
+		}},
 	})
 }
 
@@ -48,7 +57,7 @@ func DaemonsRestartStress(ctx context.Context, s *testing.State) {
 	}
 
 	ctxForResumeDaemons := ctx
-	ctx, cancel := ctxutil.Shorten(ctx, 2*time.Minute)
+	ctx, cancel := ctxutil.Shorten(ctx, time.Minute)
 	defer cancel()
 
 	// Drop the DA reset permission.
