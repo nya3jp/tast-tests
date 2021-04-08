7293ca66970cebfe73bf91eade31adb996adc3a5
Trent Begin <tbegin@google.com>
2021-04-08 14:02:04 -0600

    upstart/fixture.go: add ensureNoUI fixture
    This change modifies the fixture ensureUI and adds a configuration
    option to ensure that the ui service is stopped.
    
    BUG=b:184284915
    TEST=CQ
    
    Change-Id: I67174d7afba08d8287662b62e6e243351d30a322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2815099
    Tested-by: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Po-Hsien Wang <pwang@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Trent Begin <tbegin@chromium.org>

--- src/chromiumos/tast/local/upstart/fixture.go
+++ src/chromiumos/tast/local/upstart/fixture.go
@@ -17,31 +17,54 @@ func init() {
 	testing.AddFixture(&testing.Fixture{
 		Name:            "ensureUI",
 		Desc:            "Ensure the ui service is running.",
-		Impl:            &ensureUIFixture{},
+		Impl:            &ensureUIFixture{running: true},
 		SetUpTimeout:    10 * time.Second,
 		TearDownTimeout: 10 * time.Second,
 		ResetTimeout:    10 * time.Second,
 	})
+
+	testing.AddFixture(&testing.Fixture{
+		Name:            "ensureNoUI",
+		Desc:            "Ensure the ui service is not running.",
+		Impl:            &ensureUIFixture{running: false},
+		SetUpTimeout:    10 * time.Second,
+		TearDownTimeout: 10 * time.Second,
+		ResetTimeout:    1 * time.Second,
+	})
 }
 
 type ensureUIFixture struct {
+	running bool
 }
 
 func (f *ensureUIFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
-	if err := EnsureJobRunning(ctx, "ui"); err != nil {
-		s.Fatal("Failed to start ui: ", err)
+	if f.running {
+		if err := EnsureJobRunning(ctx, "ui"); err != nil {
+			s.Fatal("Failed to start ui: ", err)
+		}
+	} else {
+		if err := StopJob(ctx, "ui"); err != nil {
+			s.Fatal("Failed to stop ui: ", err)
+		}
 	}
+
 	return nil
 }
 
 func (f *ensureUIFixture) TearDown(ctx context.Context, s *testing.FixtState) {
-	// We intentionally don't stop the ui as Chrome running is the expected clean.
+	// We intentionally don't stop the ui as Chrome running is the expected
+	// clean. If the ui is stopped, attempt to start it.
+	if err := EnsureJobRunning(ctx, "ui"); err != nil {
+		s.Log("Failed to start ui: ", err)
+	}
 }
 
 func (f *ensureUIFixture) Reset(ctx context.Context) error {
-	// Tried to ensure the ui job is still running between test. EnsureJobRunning is noop if Chrome is running.
-	if err := EnsureJobRunning(ctx, "ui"); err != nil {
-		return errors.Wrap(err, "failed to ensure ui is running")
+	// Tried to ensure the ui job is still running between tests. EnsureJobRunning is noop if Chrome is running.
+	if f.running {
+		if err := EnsureJobRunning(ctx, "ui"); err != nil {
+			return errors.Wrap(err, "failed to ensure ui is running")
+		}
 	}
 	return nil
 }
