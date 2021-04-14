2b169fa1c8fe2c8bf45b9d5c4a3ecc9df82f0652
Trent Begin <tbegin@google.com>
2021-04-14 15:24:36 -0600

    network.DiagFailFirewall: add test
    This test creates a firewall that blocks traffic on specified ports and
    runs the network diagnostic firewall routines to ensure the firewall is
    detected. It also modifies the CreateFirewall function to add a timeout
    that is passed to the iptables command. This allows the command to wait
    until other operations are complete.
    
    BUG=chromium:1178896
    TEST=tast run hatch-dut network.DiagFailFirewall* (pass)
    
    Change-Id: I6755aa4341f7ce2b487fff6572d9f9bac8beb77f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2826934
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Tested-by: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/network/diag_fail_firewall.go
@@ -0,0 +1,81 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/network/diag"
+	"chromiumos/tast/local/bundles/cros/network/firewall"
+	"chromiumos/tast/testing"
+)
+
+type failFirewallParams struct {
+	Routine    string
+	BlockPorts []string
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DiagFailFirewall,
+		Desc: "Tests that the http/s firewall network diagnostic test fails when traffic is not allowed on certain ports",
+		Contacts: []string{
+			"tbegin@chromium.org",            // test author
+			"khegde@chromium.org",            // network diagnostics author
+			"cros-network-health@google.com", // network-health team
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline"},
+		Fixture:      "networkDiagnosticsShillReset",
+		Params: []testing.Param{{
+			Name: "http",
+			Val: failFirewallParams{
+				Routine:    diag.RoutineHTTPFirewall,
+				BlockPorts: []string{"80"},
+			},
+			ExtraAttr: []string{"informational"},
+		}, {
+			Name: "https",
+			Val: failFirewallParams{
+				Routine:    diag.RoutineHTTPSFirewall,
+				BlockPorts: []string{"443"},
+			},
+			ExtraAttr: []string{"informational"},
+		}},
+	})
+}
+
+// DiagFailFirewall tests that when network traffic is blocked on port 80 and
+// 443 the network diagnostic routine can detect this condition.
+func DiagFailFirewall(ctx context.Context, s *testing.State) {
+	params := s.Param().(failFirewallParams)
+
+	createFirewallParams := firewall.CreateFirewallParams{
+		// Drop http and https traffic.
+		BlockPorts:     params.BlockPorts,
+		BlockProtocols: []string{"tcp", "udp"},
+		Timeout:        3 * time.Second,
+	}
+
+	if err := firewall.CreateFirewall(ctx, createFirewallParams); err != nil {
+		s.Fatal("Failed to create firewall: ", err)
+	}
+
+	mojo := s.FixtValue().(*diag.MojoAPI)
+	result, err := mojo.RunRoutine(ctx, params.Routine)
+	if err != nil {
+		s.Fatal("Failed to run routine: ", err)
+	}
+
+	const problemFirewallDetected = 1
+	expectedResult := &diag.RoutineResult{
+		Verdict:  diag.VerdictProblem,
+		Problems: []int{problemFirewallDetected},
+	}
+	if err := diag.CheckRoutineResult(result, expectedResult); err != nil {
+		s.Fatal("Routine result did not match: ", err)
+	}
+}
