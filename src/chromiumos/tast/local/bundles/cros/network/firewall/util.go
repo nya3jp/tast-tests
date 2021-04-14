a0d9c8ab3e9b28713f06166b8761dbb23072b795
Trent Begin <tbegin@google.com>
2021-04-14 15:00:46 -0600

    network/firewall: move SetupFirewall function to util file
    This changes moves the function call to create a firewall out of the
    gRPC service and into a util file so it can be used by local tests. It
    also modifies the function to be more general with parameterized values.
    
    BUG=chromium:1178896
    TEST=tast run hatch-dut network.ExtensionConnectivity (pass)
    
    Change-Id: Ieba998305f1f679c4ddd7c554793c8aecfac0647

---
+++ src/chromiumos/tast/local/bundles/cros/network/firewall/util.go
@@ -0,0 +1,78 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package firewall is a library with utilities for creating an on device firewall
+package firewall
+
+import (
+	"context"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+)
+
+const (
+	iptablesCmd  = "/sbin/iptables"
+	ip6tablesCmd = "/sbin/ip6tables"
+)
+
+// CreateFirewallParams is a list of optional parameters when creating a firewall.
+type CreateFirewallParams struct {
+	AllowPorts      []string
+	AllowInterfaces []string
+	AllowProtocols  []string
+	BlockPorts      []string
+	BlockProtocols  []string
+}
+
+// CreateFirewall modifies the iptables to allow traffic on specified ports and
+// interfaces and block traffic on specified ports and protocols.
+func CreateFirewall(ctx context.Context, params CreateFirewallParams) error {
+	cmds := []string{iptablesCmd, ip6tablesCmd}
+
+	// Allow each port and interface on all allowed protocols.
+	for _, pr := range params.AllowProtocols {
+		// Allow traffic from the specified ports through the firewall.
+		for _, po := range params.AllowPorts {
+			args := []string{"-I", "OUTPUT", "-p", pr, "-m", "tcp", "--sport", po, "-j", "ACCEPT"}
+			if err := executeIptables(ctx, cmds, args); err != nil {
+				return err
+			}
+		}
+
+		// Allow connections from the allowed interfaces.
+		for _, i := range params.AllowInterfaces {
+			args := []string{"-I", "FORWARD", "-p", pr, "-i", i, "-j", "ACCEPT"}
+			if err := executeIptables(ctx, cmds, args); err != nil {
+				return err
+			}
+		}
+	}
+
+	// Block each port on all blocked protocols.
+	for _, pr := range params.BlockProtocols {
+		for _, po := range params.BlockPorts {
+			// Add this rule with rule-number 2 so that the first rule above, which allows proxy traffic for the OUTPUT chain, has priority.
+			args := []string{"-I", "OUTPUT", "2", "-p", pr, "--dport", po, "-j", "REJECT"}
+			if err := executeIptables(ctx, cmds, args); err != nil {
+				return err
+			}
+			// Add this rule with rule-number 2 so that the second rule above, which allows proxy traffic for the FORWARD chain, has priority.
+			args = []string{"-I", "FORWARD", "2", "-p", pr, "--dport", po, "-j", "REJECT"}
+			if err := executeIptables(ctx, []string{iptablesCmd}, args); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func executeIptables(ctx context.Context, cmds, args []string) error {
+	for _, cmd := range cmds {
+		if err := testexec.CommandContext(ctx, cmd, args...).Run(testexec.DumpLogOnError); err != nil {
+			return errors.Wrapf(err, "failed to add iptables rule: %s %v", cmd, args)
+		}
+	}
+	return nil
+}
