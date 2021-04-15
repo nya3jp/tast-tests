8c9aeddf6596cc423879737c390c56ef504a06fc
Daniel Campello <campello@chromium.org>
2021-04-14 19:55:38 -0600

    tast-tests: add firmware.FwupdGetDevices
    This simple test makes sure that fwupdtool works and is able to
    get-devices in the system.
    
    BUG=b:185432355
    TEST=tast run dut firmware.FwupdGetDevices
    
    Cq-Depend: chromium:2829173
    Change-Id: I28729ec8e01f2afa13c9610c2217d46d460b5f56

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_get_devices.go
@@ -0,0 +1,36 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FwupdGetDevices,
+		Desc: "Checks that fwupd can detect device",
+		Contacts: []string{
+			"campello@chromium.org",     // Test Author
+			"chromeos-fwupd@google.com", // CrOS FWUPD
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"fwupd"},
+	})
+}
+
+// FwupdGetDevices runs the fwupdtool utility and verifies that it
+// detects devices in the system.
+func FwupdGetDevices(ctx context.Context, s *testing.State) {
+	cmd := testexec.CommandContext(ctx, "/usr/bin/fwupdtool", "get-devices", "--show-all")
+
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("%q failed: %v", shutil.EscapeSlice(cmd.Args), err)
+	}
+}
