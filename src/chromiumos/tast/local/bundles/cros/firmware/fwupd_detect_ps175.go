7668f206932a9a718b1758e3e2f072492659d88e
Peter Marheine <pmarheine@chromium.org>
2021-04-15 10:33:47 +1000

    tast-tests: convert PS175 firmware install to detect
    Changes to how fwupd uses flashrom mean it is no longer possible to mock
    flashrom as the test was doing, and the PS175 has different IDs. Change
    the test so it ensures fwupd detects the device, but does not attempt to
    update it.
    
    BUG=b:173742142
    TEST=Test now passes on Noibat
    
    Change-Id: I61d31790c498c11f7d34ce9994427cfea17d139e

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_detect_ps175.go
@@ -0,0 +1,69 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"regexp"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+const (
+	deviceInfoPattern = `PS175:
+      Device ID:          [0-9a-f]+
+      Current version:    \d+\.\d+
+      Vendor:             Parade \(I2C:1AF8\)
+      GUID:               f8887c5d-6236-50cd-bbc4-b188a4a3b198 \? FLASHROM-LSPCON-I2C-SPI\\VEN_1AF8&DEV_0175
+      Device Flags:       \? Internal device
+                          \? Updatable
+`
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FwupdDetectPS175,
+		Desc: "Checks that fwupd can detect device",
+		Contacts: []string{
+			"pmarheine@chromium.org",       // Test Author
+			"chromeos-firmware@google.com", // CrOS Firmware Developers
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		HardwareDeps: hwdep.D(hwdep.Platform("puff")),
+	})
+}
+
+// verifyPS175Detected verifies the fwupdtool output shows a PS175 was detected
+func verifyPS175Detected(output []byte) error {
+	matched, err := regexp.Match(deviceInfoPattern, output)
+	if err != nil {
+		return err
+	}
+
+	if !matched {
+		return errors.Errorf("get-devices output didn't match expected format: %q", output)
+	}
+
+	return nil
+}
+
+// FwupdDetectPS175 runs the fwupdtool utility and verifies that the PS175
+// device is recognized.
+func FwupdDetectPS175(ctx context.Context, s *testing.State) {
+	cmd := testexec.CommandContext(ctx, "/usr/bin/fwupdtool", "get-devices", "--plugins", "flashrom")
+
+	output, err := cmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatalf("%q failed: %v", shutil.EscapeSlice(cmd.Args), err)
+	}
+
+	if err := verifyPS175Detected(output); err != nil {
+		s.Fatal("fwupdtool failed to detect PS175: ", err)
+	}
+}
