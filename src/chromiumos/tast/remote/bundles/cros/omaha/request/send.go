3b3e3f24ab450e754dcde3b5bc87ba205ef08b57
Victor-Gabriel Savu <vsavu@google.com>
2021-04-16 10:39:45 +0200

    omaha: Add initial omaha helpers and a test
    Add helpers to check the current state of Omaha. Tests are in a
    separate group to limit the number of queries sent. omaha.Stable
    checks the currently served stable release.
    
    BUG=chromium:1165743
    TEST=tast run <ip> omaha.*
    
    Cq-Depend: chromium:2850925
    Change-Id: Ibbee923fb249980eff3177a80fd804a4d9ed9d6b

---
+++ src/chromiumos/tast/remote/bundles/cros/omaha/request/send.go
@@ -0,0 +1,67 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package request
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// Send sends a request to Omaha and parses the response.
+func Send(ctx context.Context, req *Request, filePrefix string) (*Response, error) {
+	reqData, err := xml.MarshalIndent(&req, "", "  ")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal request")
+	}
+
+	outDir, outDirOk := testing.ContextOutDir(ctx)
+	if outDirOk {
+		if err := ioutil.WriteFile(filepath.Join(outDir, filePrefix+"-request.xml"), reqData, 0644); err != nil {
+			testing.ContextLog(ctx, "Failed to dump request to file: ", err)
+		}
+	}
+
+	res, err := http.Post(OmahaRequestURL, "application/xml", bytes.NewReader(reqData))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to send request")
+	}
+
+	resData, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed read response body")
+	}
+
+	if outDirOk {
+		if err := ioutil.WriteFile(filepath.Join(outDir, filePrefix+"-response.xml"), resData, 0644); err != nil {
+			testing.ContextLog(ctx, "Failed to dump request to file: ", err)
+		}
+	}
+
+	var parsed Response
+	if err := xml.Unmarshal(resData, &parsed); err != nil {
+		return nil, errors.Wrap(err, "failed parse the response")
+	}
+
+	if outDirOk {
+		parsedRes, err := json.MarshalIndent(parsed, "", "  ")
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to marshal response")
+		}
+
+		if err := ioutil.WriteFile(filepath.Join(outDir, filePrefix+"-response-parsed.json"), parsedRes, 0644); err != nil {
+			testing.ContextLog(ctx, "Failed to dump request to file: ", err)
+		}
+	}
+
+	return &parsed, nil
+}
