e58205ea90375d2f62cca66a9436a3cae4d4d023
Victor-Gabriel Savu <vsavu@google.com>
2021-04-16 10:39:45 +0200

    omaha: Add initial omaha helpers and a test
    Add helpers to check the current state of Omaha. Tests are in a
    separate group to limit the number of queries sent. omaha.Stable
    checks the currently served stable release.
    
    BUG=chromium:1165743
    TEST=tast run <ip> omaha.*
    
    Cq-Depend: chromium:2850925
    Change-Id: Ibbee923fb249980eff3177a80fd804a4d9ed9d6b

---
+++ src/chromiumos/tast/remote/bundles/cros/omaha/params/params.go
@@ -0,0 +1,19 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package params contains parameters shared by the Omaha tests.
+// These values are by design maintaned manually and not obtained from an outside source.
+package params
+
+const (
+	// CurrentStableChrome is the current stable milestone.
+	CurrentStableChrome = "92"
+	// CurrentStableChromeOS is the current stable OS version.
+	CurrentStableChromeOS = "13982"
+
+	// NextStableChrome is the next stable milestone.
+	NextStableChrome = "93"
+	// NextStableChromeOS is the next stable OS version.
+	NextStableChromeOS = "14092"
+)
