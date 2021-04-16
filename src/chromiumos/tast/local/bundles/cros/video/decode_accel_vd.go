3d240057608670668de33a1392e70fc65a044c30
Miguel Casas <mcasas@chromium.org>
2021-04-16 02:02:30 +0000

    video: DecodeAccelVD: remove ExtraAttr from test cases
    All test cases have the same ExtraAttr:
      ExtraAttr:         []string{"group:mainline", "informational"}, 
    
    This CL moves those as simple test Attr.
    
    BUG=None
    TEST=Tast video.DecodeAccelVD.* on volteer
    
    Change-Id: Ia407911708feb96fb491fc774da30133e7c6d7f0

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd.go
@@ -18,82 +18,70 @@ func init() {
 		Desc:         "Verifies hardware decode acceleration of media::VideoDecoders by running the video_decode_accelerator_tests binary (see go/vd-migration)",
 		Contacts:     []string{"acourbot@chromium.org", "dstaessens@chromium.org", "chromeos-video-eng@google.com"},
 		SoftwareDeps: []string{"chrome", "video_decoder_direct"},
+        Attr:         []string{"group:mainline", "informational"},
 		Params: []testing.Param{{
 			Name:              "av1",
-			Val:               "test-25fps.av1.ivf",
-			ExtraAttr:         []string{"group:mainline", "informational"},
+			Val:               "test-25fps.av1.ivf",			
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1},
 			ExtraData:         []string{"test-25fps.av1.ivf", "test-25fps.av1.ivf.json"},
 		}, {
 			Name:              "av1_10bit",
 			Val:               "test-25fps-10bit.av1.ivf",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1_10BPP},
 			ExtraData:         []string{"test-25fps-10bit.av1.ivf", "test-25fps-10bit.av1.ivf.json"},
 		}, {
 			Name:              "h264",
 			Val:               "test-25fps.h264",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
 		}, {
 			Name:              "hevc",
 			Val:               "test-25fps.hevc",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC, "proprietary_codecs", "protected_content"},
 			ExtraData:         []string{"test-25fps.hevc", "test-25fps.hevc.json"},
 		}, {
 			Name:              "hevc_10bit",
 			Val:               "test-25fps.hevc10",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC10BPP, "proprietary_codecs", "protected_content"},
 			ExtraData:         []string{"test-25fps.hevc10", "test-25fps.hevc10.json"},
 		}, {
 			Name:              "vp8",
 			Val:               "test-25fps.vp8",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
 		}, {
 			Name:              "vp9",
 			Val:               "test-25fps.vp9",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
 		}, {
 			Name:              "vp9_2",
 			Val:               "test-25fps.vp9_2",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_2},
 			ExtraData:         []string{"test-25fps.vp9_2", "test-25fps.vp9_2.json"},
 		}, {
 			Name:              "av1_resolution_switch",
 			Val:               "resolution_change.av1.ivf",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1},
 			ExtraData:         []string{"resolution_change.av1.ivf", "resolution_change.av1.ivf.json"},
 		}, {
 			Name:              "h264_resolution_switch",
 			Val:               "switch_1080p_720p_240frames.h264",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			ExtraData:         []string{"switch_1080p_720p_240frames.h264", "switch_1080p_720p_240frames.h264.json"},
 		}, {
 			Name:              "hevc_resolution_switch",
 			Val:               "switch_1080p_720p_240frames.hevc",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC, "proprietary_codecs", "protected_content"},
 			ExtraData:         []string{"switch_1080p_720p_240frames.hevc", "switch_1080p_720p_240frames.hevc.json"},
 		}, {
 			Name:              "vp8_resolution_switch",
 			Val:               "resolution_change_500frames.vp8.ivf",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			ExtraData:         []string{"resolution_change_500frames.vp8.ivf", "resolution_change_500frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp9_resolution_switch",
 			Val:               "resolution_change_500frames.vp9.ivf",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"resolution_change_500frames.vp9.ivf", "resolution_change_500frames.vp9.ivf.json"},
 		}, {
@@ -101,14 +89,12 @@ func init() {
 			// https://android.googlesource.com/platform/cts/+/HEAD/tests/tests/media/res/raw/vp90_2_17_show_existing_frame.vp9
 			Name:              "vp9_show_existing_frame",
 			Val:               "vda_smoke-vp90_2_17_show_existing_frame.vp9",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"vda_smoke-vp90_2_17_show_existing_frame.vp9", "vda_smoke-vp90_2_17_show_existing_frame.vp9.json"},
 		}, {
 			// H264 stream in which a profile changes from Baseline to Main.
 			Name:              "h264_profile_change",
 			Val:               "test-25fps_basemain.h264",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			ExtraData:         []string{"test-25fps_basemain.h264", "test-25fps_basemain.h264.json"},
 		}, {
@@ -117,25 +103,21 @@ func init() {
 			// The structure is used in Hangouts Meet. go/vp9-svc-hangouts for detail.
 			Name:              "vp9_keyframe_spatial_layers",
 			Val:               "keyframe_spatial_layers_180p_360p.vp9.ivf",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "vaapi"},
 			ExtraData:         []string{"keyframe_spatial_layers_180p_360p.vp9.ivf", "keyframe_spatial_layers_180p_360p.vp9.ivf.json"},
 		}, {
 			Name:              "av1_odd_dimension",
 			Val:               "test-25fps-321x241.av1.ivf",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1},
 			ExtraData:         []string{"test-25fps-321x241.av1.ivf", "test-25fps-321x241.av1.ivf.json"},
 		}, {
 			Name:              "vp8_odd_dimension",
 			Val:               "test-25fps-321x241.vp8",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			ExtraData:         []string{"test-25fps-321x241.vp8", "test-25fps-321x241.vp8.json"},
 		}, {
 			Name:              "vp9_odd_dimension",
 			Val:               "test-25fps-321x241.vp9",
-			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"test-25fps-321x241.vp9", "test-25fps-321x241.vp9.json"},
 		}},
