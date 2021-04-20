13174d12b9664bd62eae6ceea1ab9bcce3c041ff
Jeremy Bettis <jbettis@google.com>
2021-04-20 11:54:53 -0600

    tast: Add controls for EC power state & cmd output
    Add EC control for ec_system_powerstate and GetECPowerState() method.
    
    Add method RunECCommandGetOutput that will send an EC command and then
    wait for a list of regexps.
    
    Add helper methods to parse the python encoded lists of strings that
    are returned by servo for some controls. This also makes it possible
    to just read all of the ec uart output by enabling ec_uart_capture and
    decoding the strings from ec_uart_stream.
    
    BUG=b:172227463
    TEST=Added unit test for parsing, and used new methods in a new test.
    
    Change-Id: Idec2b49e90f35294075fb3ff8465a07b8cdec3d0

---
+++ src/chromiumos/tast/remote/servo/methods_test.go
@@ -0,0 +1,59 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package servo
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// deepEquals is like reflect.DeepEqual, except that it works for []interface{}
+func deepEquals(t *testing.T, expected, actual interface{}) bool {
+	switch expected.(type) {
+	case []interface{}:
+		eVal := reflect.ValueOf(expected)
+		aVal := reflect.ValueOf(actual)
+		same := true
+		for i := 0; i < eVal.Len(); i++ {
+			same = same && deepEquals(t, eVal.Index(i).Interface(), aVal.Index(i).Interface())
+		}
+		return same
+	case string:
+		return expected.(string) == actual.(string)
+	default:
+		t.Errorf("Unhandled type %T of %v", expected, expected)
+		return false
+	}
+}
+
+func TestParseStringList(t *testing.T) {
+	if !cmp.Equal([]interface{}{}, []interface{}{}) {
+		t.Errorf("cmp.Equal is broken")
+	}
+	type testCase struct {
+		pslParam  string
+		expectErr bool
+		expected  interface{}
+	}
+	for _, tc := range []testCase{
+		{"[]", false, []interface{}(nil)},
+		{"", true, nil},
+		{`['foo', 'bar\'', 'ba\\z']`, false, []interface{}{"foo", "bar'", `ba\z`}},
+		{`[['one', 'two'], ['three']]`, false, []interface{}{[]interface{}{"one", "two"}, []interface{}{"three"}}},
+	} {
+		res, err := ParseStringList(tc.pslParam)
+		if tc.expectErr {
+			if err == nil {
+				t.Errorf("ParseStringList(%q) unexpectedly succeeded", tc.pslParam)
+			}
+		} else if err != nil {
+			t.Errorf("ParseStringList(%q) failed %s", tc.pslParam, err)
+		} else if !cmp.Equal(tc.expected, res) {
+			t.Errorf("ParseStringList(%q) %s", tc.pslParam, cmp.Diff(tc.expected, res))
+		}
+	}
+}
