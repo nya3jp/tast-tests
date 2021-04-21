a7d8484adefe22a9df8647af3b233bdacb492f91
Tom Hughes <tomhughes@chromium.org>
2021-04-21 16:46:32 -0700

    fingerprint: Add test to verify RO only boots valid RW images
    BUG=b:153736350
    TEST=tast run <IP> firmware.FpROOnlyBootsValidRW
    
    Change-Id: I3833d1fafce2a354db9a013882fbae6abc9b93f2

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/fp_ro_only_boots_valid_rw.go
@@ -0,0 +1,163 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/dutfs"
+	"chromiumos/tast/remote/firmware/fingerprint"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FpROOnlyBootsValidRW,
+		Desc: "Verify the RO fingerprint firmware only boots valid RW firmware",
+		Contacts: []string{
+			"tomhughes@chromium.org", // Test author
+			"chromeos-fingerprint@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		Timeout:      7 * time.Minute,
+		SoftwareDeps: []string{"biometrics_daemon"},
+		HardwareDeps: hwdep.D(hwdep.Fingerprint()),
+		ServiceDeps:  []string{"tast.cros.platform.UpstartService", dutfs.ServiceName},
+		Vars:         []string{"servo"},
+		Data: []string{
+			fingerprint.GenTestImagesScript,
+			fingerprint.Futility,
+			fingerprint.BloonchipperDevKey,
+			fingerprint.DartmonkeyDevKey,
+			fingerprint.NamiFPDevKey,
+			fingerprint.NocturneFPDevKey,
+		},
+	})
+}
+
+type testParams struct {
+	firmwarePath                string
+	expectedROVersion           string
+	expectedRWVersion           string
+	expectedRunningFirmwareCopy fingerprint.FWImageType
+}
+
+func testFlashingFirmwareVersion(ctx context.Context, d *dut.DUT, fs *dutfs.Client, params *testParams) error {
+	testing.ContextLog(ctx, "Flashing firmware: ", params.firmwarePath)
+	if err := fingerprint.FlashRWFirmware(ctx, d, fs, params.firmwarePath); err != nil {
+		return errors.Wrapf(err, "failed to flash firmware: %q", params.firmwarePath)
+	}
+
+	testing.ContextLog(ctx, "Checking for versions: RO: ", params.expectedROVersion, ", RW: ", params.expectedRWVersion)
+	if err := fingerprint.CheckRunningFirmwareVersionMatches(ctx, d, params.expectedROVersion, params.expectedRWVersion); err != nil {
+		return errors.Wrap(err, "unexpected firmware version")
+	}
+
+	testing.ContextLog(ctx, "Checking that ", params.expectedRunningFirmwareCopy, " firmware is running")
+	if err := fingerprint.CheckRunningFirmwareCopy(ctx, d, params.expectedRunningFirmwareCopy); err != nil {
+		return errors.Wrap(err, "not running RO firmware")
+	}
+
+	testing.ContextLog(ctx, "Checking that rollback remains unchanged")
+	if err := fingerprint.CheckRollbackSetToInitialValue(ctx, d); err != nil {
+		return errors.Wrap(err, "rollback not set to initial value")
+	}
+
+	return nil
+}
+
+func FpROOnlyBootsValidRW(ctx context.Context, s *testing.State) {
+	t, err := fingerprint.NewFirmwareTest(ctx, s.DUT(), s.RequiredVar("servo"), s.RPCHint(), s.OutDir(), true, true)
+	if err != nil {
+		s.Fatal("Failed to create new firmware test: ", err)
+	}
+	ctxForCleanup := ctx
+	defer func() {
+		if err := t.Close(ctxForCleanup); err != nil {
+			s.Fatal("Failed to clean up: ", err)
+		}
+	}()
+	ctx, cancel := ctxutil.Shorten(ctx, t.CleanupTime())
+	defer cancel()
+
+	d := t.DUT()
+
+	testImages, err := fingerprint.GenerateTestFirmwareImages(ctx, d, t.DutfsClient(), s.DataPath(fingerprint.GenTestImagesScript), t.FPBoard(), t.BuildFwFile(), t.DUTTempDir())
+	if err != nil {
+		s.Fatal("Failed to generate test images: ", err)
+	}
+
+	if err := fingerprint.CheckRunningFirmwareCopy(ctx, d, fingerprint.ImageTypeRW); err != nil {
+		s.Fatal("Test expects RW firmware copy to be running")
+	}
+
+	// Hardware write protect must be enabled for the test to work correctly.
+	if err := fingerprint.CheckWriteProtectStateCorrect(ctx, d, t.FPBoard(), fingerprint.ImageTypeRW, true, true); err != nil {
+		s.Fatal("Failed to validate write protect settings: ", err)
+	}
+
+	// Starts with MP-signed firmware. Then successively tries to flash three versions
+	// to RW: dev, corrupted first byte, and corrupted last byte. Each of these should
+	// flash successfully, but fail to boot (i.e., stay in RO mode). Finally,
+	// flash an MP-signed version, which should successfully boot to RW.
+
+	if err := testFlashingFirmwareVersion(ctx, d, t.DutfsClient(),
+		&testParams{
+			firmwarePath: testImages[fingerprint.TestImageTypeDev].Path,
+			// RO version should remain unchanged.
+			expectedROVersion: testImages[fingerprint.TestImageTypeOriginal].ROVersion,
+			// RW version should match what we requested to be flashed.
+			expectedRWVersion: testImages[fingerprint.TestImageTypeDev].RWVersion,
+			// Signature check will fail, so we should be running RO.
+			expectedRunningFirmwareCopy: fingerprint.ImageTypeRO,
+		}); err != nil {
+		s.Fatal("Dev key signed test failed: ", err)
+	}
+
+	// Note that the corrupted version has the same version string as the original version.
+	if err := testFlashingFirmwareVersion(ctx, d, t.DutfsClient(),
+		&testParams{
+			firmwarePath: testImages[fingerprint.TestImageTypeCorruptFirstByte].Path,
+			// RO version should remain unchanged.
+			expectedROVersion: testImages[fingerprint.TestImageTypeOriginal].ROVersion,
+			// RW version should match what we requested to be flashed.
+			expectedRWVersion: testImages[fingerprint.TestImageTypeCorruptLastByte].RWVersion,
+			// Signature check will fail, so we should be running RO.
+			expectedRunningFirmwareCopy: fingerprint.ImageTypeRO,
+		}); err != nil {
+		s.Fatal("Corrupt first byte test failed: ", err)
+	}
+
+	if err := testFlashingFirmwareVersion(ctx, d, t.DutfsClient(),
+		&testParams{
+			firmwarePath: testImages[fingerprint.TestImageTypeCorruptLastByte].Path,
+			// RO version should remain unchanged.
+			expectedROVersion: testImages[fingerprint.TestImageTypeOriginal].ROVersion,
+			// RW version should match what we requested to be flashed.
+			expectedRWVersion: testImages[fingerprint.TestImageTypeCorruptLastByte].RWVersion,
+			// Signature check will fail, so we should be running RO.
+			expectedRunningFirmwareCopy: fingerprint.ImageTypeRO,
+		}); err != nil {
+		s.Fatal("Corrupt last byte test failed: ", err)
+	}
+
+	if err := testFlashingFirmwareVersion(ctx, d, t.DutfsClient(),
+		&testParams{
+			firmwarePath: testImages[fingerprint.TestImageTypeOriginal].Path,
+			// RO version should remain unchanged.
+			expectedROVersion: testImages[fingerprint.TestImageTypeOriginal].ROVersion,
+			// RW version should match what we requested to be flashed.
+			expectedRWVersion: testImages[fingerprint.TestImageTypeOriginal].RWVersion,
+			// Signature check will succeed, so we should be running RW.
+			expectedRunningFirmwareCopy: fingerprint.ImageTypeRW,
+		}); err != nil {
+		s.Fatal("Good firmware test failed: ", err)
+	}
+}
