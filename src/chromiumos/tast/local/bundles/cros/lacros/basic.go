cc0fb8f74271c4f34d99f26505665f95ac3db79d
Hidehiko Abe <hidehiko@chromium.org>
2021-04-22 23:01:02 +0900

    Adds more lacros related logs.
    In order to identify the flakiness failure on bots,
    this CL adds more logs.
    We found some EPERM error on lacros-chrome execution.
    
    BUG=chromium:1198252
    TEST=Ran on DUT. New logs are stored in the test result.
    
    Change-Id: Ied1394b23c511aabac5f8a10bb8f86e202a19f11

--- src/chromiumos/tast/local/bundles/cros/lacros/basic.go
+++ src/chromiumos/tast/local/bundles/cros/lacros/basic.go
@@ -7,8 +7,11 @@ package lacros
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"time"
 
+	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/local/lacros/faillog"
 	"chromiumos/tast/local/lacros/launcher"
 	"chromiumos/tast/testing"
@@ -34,6 +37,35 @@ func init() {
 }
 
 func Basic(ctx context.Context, s *testing.State) {
+	// Temporarily save the lacros-chrome's execution stats.
+	// We found several "EPERM" errors on execution.
+	// TODO(crbug.com/1198252): Should be removed when the issue is fixed.
+	defer func() {
+		// Runs the given command with redirecting its stdout to outpath.
+		// On error, logging it then ignored.
+		run := func(outpath string, cmds ...string) {
+			out, err := os.Create(outpath)
+			if err != nil {
+				testing.ContextLogf(ctx, "Failed to create %q: %v", outpath, err)
+				return
+			}
+			defer out.Close()
+			cmd := testexec.CommandContext(ctx, cmds[0], cmds[1:]...)
+			cmd.Stdout = out
+			if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+				testing.ContextLogf(ctx, "Failed to run %q: %v", cmds[0], err)
+			}
+		}
+
+		// Dump current mount status. Specifically noexec on /mnt/stateful_partition
+		// is interesting.
+		run(filepath.Join(s.OutDir(), "mount.txt"), "mount")
+
+		// Also check the dearchived files.
+		run(filepath.Join(s.OutDir(), "lacros-ls.txt"),
+			"ls", "-l", s.FixtValue().(launcher.FixtData).LacrosPath)
+	}()
+
 	l, err := launcher.LaunchLacrosChrome(ctx, s.FixtValue().(launcher.FixtData), s.DataPath(launcher.DataArtifact))
 	if err != nil {
 		s.Fatal("Failed to launch lacros-chrome: ", err)
