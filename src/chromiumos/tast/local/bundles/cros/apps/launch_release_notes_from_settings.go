caff580a611369ef3c7023995a53caf4ec7cfbb7
Jun Sheng <shengjun@google.com>
2021-04-23 23:20:53 +1000

    tast-tests: fix apps.LaunchReleaseNotesFromSettings
    Fix the node finder which cause the test started to fail in M91.
    
    BUG=None
    TEST=tast run apps.LaunchReleaseNotesFromSettings*
    
    Change-Id: Icf979f8d38f0e7ae03784e656ccb7a3928262198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2848107
    Tested-by: Jun Sheng <shengjun@chromium.org>
    Auto-Submit: Jun Sheng <shengjun@chromium.org>
    Reviewed-by: Greg Edelston <gredelston@google.com>
    Commit-Queue: Jun Sheng <shengjun@chromium.org>
    (cherry picked from commit bcea2b3e294124d2ba30bef03a9b82fafedc6bd8)

--- src/chromiumos/tast/local/bundles/cros/apps/launch_release_notes_from_settings.go
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_release_notes_from_settings.go
@@ -64,7 +64,7 @@ func LaunchReleaseNotesFromSettings(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to launch Settings: ", err)
 	}
 
-	whatsNewTabFinder := nodewith.NameStartingWith("What’s New").Role(role.Heading).Ancestor(helpapp.RootFinder)
+	whatsNewTabFinder := nodewith.NameStartingWith("What's new").Role(role.Heading).Ancestor(helpapp.RootFinder)
 
 	if err := uiauto.Combine("launch WhatsNew and verify landing page",
 		settings.LaunchWhatsNew(),
