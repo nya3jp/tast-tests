b7a0d49f2a0d02b907a9f4da28186cda8bb27e3f
Kazuki Takise <takise@google.com>
2021-04-27 17:59:19 +0900

    Add Tast for Resize Lock feature
    This CL adds some test cases for Arc Resize Lock along with a new
    APK necessary for the test.
    
    More test cases will be coming as the features evolve.
    
    BUG=b:181614430
    TEST=tast -verbose run $DUT arc.ResizeLock
    
    Change-Id: Id213103e08255af1ca7dad99d4bc352859aa7258

---
+++ android/ArcResizeLockTest/src/org/chromium/arc/testapp/resizelock/MainActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.resizelock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_activity);
+    }
+}
