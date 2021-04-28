0e8059d723b05096a82af473ee1e18a8cabc42e3
Prashant Malani <pmalani@chromium.org>
2021-04-27 17:04:22 -0700

    tast-test: typec: Add Mode hotplug test
    Add a test which checks whether Thunderbolt alternate mode entry is
    behaving as expected when a dock is unplugged and then replugged.
    
    BUG=b:186581186
    TEST=Test passes 20/20 times on a voxel.
    
    Change-Id: Iea266d9c0b0a5830495f9e60bce16abdde68fbb4

---
+++ src/chromiumos/tast/local/bundles/cros/typec/mode_hotplug.go
@@ -0,0 +1,143 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package typec
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/typec/typecutils"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ModeHotplug,
+		Desc:         "Checks USB Type C mode switch behaviour when a Thunderbolt dock is unplugged/replugged",
+		Contacts:     []string{"pmalani@chromium.org", "chromeos-power@google.com"},
+		SoftwareDeps: []string{"chrome"},
+		Vars:         []string{"ui.signinProfileTestExtensionManifestKey"},
+		Data:         []string{"testcert.p12"},
+		Params: []testing.Param{
+			// For running manually.
+			{
+				Name: "manual",
+				Val:  false,
+			},
+			// For automated testing.
+			{
+				Name:              "smoke",
+				ExtraAttr:         []string{"group:mainline", "informational"},
+				ExtraHardwareDeps: hwdep.D(hwdep.Model("volteer", "voxel")),
+				Val:               true,
+			}},
+	})
+}
+
+// ModeHotplug does the following:
+// - Login.
+// - Validate that the Thunderbolt dock is enumerated correctly.
+// - Simulate unplug of the dock.
+// - Validate that the Thunderbolt dock is no longer enumerated.
+// - Simulate re-plug of the dock.
+// - Validate that the Thunderbolt dock is re-enumerated correctly.
+//
+// This test requires the following H/W topology to run.
+//
+//
+//        DUT ------> Thunderbolt3 (>= Titan Ridge) dock -----> DP monitor.
+//      (USB4)
+//
+// The Thunderbolt dock is assumed connected on port index 1.
+func ModeHotplug(ctx context.Context, s *testing.State) {
+	// This check is for test executions which take place on
+	// CQ (where TBT peripherals aren't connected).
+	present, err := typecutils.CheckPortsForTBTPartner(ctx)
+	if err != nil {
+		s.Fatal("Couldn't determine TBT device from PD identity: ", err)
+	}
+
+	// Return early for smoke testing (CQ).
+	if smoke := s.Param().(bool); smoke {
+		return
+	}
+
+	if !present {
+		s.Fatal("No TBT device connected to DUT")
+	}
+
+	// Get to the Chrome login screen.
+	loadSignInProfileOption := chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey"))
+	cr, err := chrome.New(ctx, chrome.DeferLogin(), loadSignInProfileOption)
+	if err != nil {
+		s.Fatal("Failed to start Chrome at login screen: ", err)
+	}
+
+	testConn, err := cr.SigninProfileTestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get Test API connection: ", err)
+	}
+	defer testConn.Close()
+
+	if err := typecutils.EnablePeripheralDataAccess(ctx, s.DataPath("testcert.p12")); err != nil {
+		s.Fatal("Failed to enable peripheral data access setting: ", err)
+	}
+
+	if err := cr.ContinueLogin(ctx); err != nil {
+		s.Fatal("Failed to login: ", err)
+	}
+
+	s.Log("Verifying TBT & DP functionality after login")
+	if err := checkTBTAndDP(ctx, testConn); err != nil {
+		s.Fatal("Failed to verify TBT & DP after login: ", err)
+	}
+
+	s.Log("Simulate unplug")
+	if err := testexec.CommandContext(ctx, "ectool", "pdcontrol", "suspend", "1").Run(); err != nil {
+		s.Fatal("Failed to simulate unplug: ", err)
+	}
+	defer testexec.CommandContext(ctx, "ectool", "pdcontrol", "resume", "1").Run()
+
+	s.Log("Verifying that no TBT devices enumerated")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		return typecutils.CheckTBTDevice(false)
+	}, &testing.PollOptions{Timeout: 10 * time.Second}); err != nil {
+		s.Fatal("Failed to verify no TBT devices connected after unplug: ", err)
+	}
+
+	s.Log("Simulate replug")
+	if err := testexec.CommandContext(ctx, "ectool", "pdcontrol", "resume", "1").Run(); err != nil {
+		s.Fatal("Failed to simulate replug: ", err)
+	}
+
+	s.Log("Verifying TBT & DP functionality after replug")
+	if err := checkTBTAndDP(ctx, testConn); err != nil {
+		s.Fatal("Failed to verify TBT & DP after replug: ", err)
+	}
+}
+
+// checkTBTAndDP is a convenience function that checks for TBT and DP enumeration.
+// It returns nil on success, and the relevant error otherwise.
+func checkTBTAndDP(ctx context.Context, tc *chrome.TestConn) error {
+	tbtPollOptions := testing.PollOptions{Timeout: 10 * time.Second}
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		return typecutils.CheckTBTDevice(true)
+	}, &tbtPollOptions); err != nil {
+		return errors.Wrap(err, "failed to verify TBT devices connected")
+	}
+
+	dpPollOptions := testing.PollOptions{Timeout: 20 * time.Second}
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		return typecutils.FindConnectedDPMonitor(ctx, tc)
+	}, &dpPollOptions); err != nil {
+		return errors.Wrap(err, "failed to verify DP monitor working")
+	}
+
+	return nil
+}
