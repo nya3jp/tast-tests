5eb7aae7f98a2bc3fb35747900ca4fdda3677ddf
Jun Sheng <shengjun@google.com>
2021-04-28 11:29:58 +1000

    tast-tests: promote apps.SearchInHelpApp.stable to CQ
    apps.SearchInHelpApp.stable has been stable for 20+ builds.
    
    https://tastboard.corp.google.com/test/apps.SearchInHelpApp.stable
    
    BUG=b:166047521
    TEST=none
    
    Change-Id: I7f57c89965f9638e19e3fd701a5d817f5e5b3c59

--- src/chromiumos/tast/local/bundles/cros/apps/search_in_help_app.go
+++ src/chromiumos/tast/local/bundles/cros/apps/search_in_help_app.go
@@ -30,7 +30,7 @@ func init() {
 			"callistus@chromium.org", // test author.
 			"showoff-eng@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{
 			{
@@ -39,6 +39,7 @@ func init() {
 			}, {
 				Name:              "unstable",
 				ExtraHardwareDeps: hwdep.D(pre.AppsUnstableModels),
+				ExtraAttr:         []string{"informational"},
 			},
 		},
 	})
