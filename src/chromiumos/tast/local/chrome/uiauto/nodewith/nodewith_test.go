90bba9ce0bfc3c30493ec815dcac9f06994c784c
Matt Stark <msta@google.com>
2021-04-28 15:25:35 +1000

    Support non-english languages for nodewith selectors.
    Existing functions such as ossettings.Launch use selectors like
    nodewith.NameStartingWith("Settings"). Most functions like this will
    error when running in a non-english language.
    
    This CL should make most functions run correctly in pseudolocales by
    default, and make it easy to support other languages. Eg. if someone
    decides they want to call this function in german, they just need to
    change the selector to the following:
    nodewith.MultilingualNameStartingWith("Settings", map[string]string{"de": "Einstellungen"})
    
    BUG=chromium:1173812
    TEST=Created unit tests, manually tested that it didn't modify existing
    behaviour, and worked in new languages.
    
    Change-Id: I49ff6569ba94aef59ee8e36b7028c7669eb6a2ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2853856
    Tested-by: Matt Stark <msta@google.com>
    Auto-Submit: Matt Stark <msta@google.com>
    Commit-Queue: Matt Stark <msta@google.com>
    Reviewed-by: Judy Wu <jinrongwu@google.com>

---
+++ src/chromiumos/tast/local/chrome/uiauto/nodewith/nodewith_test.go
@@ -0,0 +1,73 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package nodewith
+
+import (
+	gotesting "testing"
+
+	"chromiumos/tast/local/chrome/uiauto/role"
+)
+
+func TestArXbPseudolocale(t *gotesting.T) {
+	if pseudo := makeArXBString("abc"); pseudo != "cba" {
+		t.Errorf("Unexpected ar-XB string: got %+v, want \"cba\"", pseudo)
+	}
+	if pseudo := makeArXBString("abcd"); pseudo != "dcba" {
+		t.Errorf("Unexpected ar-XB string: got %+v, want \"dcba\"", pseudo)
+	}
+	if pseudo := makeArXBString("平仮名"); pseudo != "名仮平" {
+		t.Errorf("Unexpected ar-XB string: got %+v, want \"名仮平\"", pseudo)
+	}
+}
+
+func TestEnXaPseudolocale(t *gotesting.T) {
+	if pseudo := makeEnXAString("bbb", true); pseudo != "bbb one" {
+		t.Errorf("Unexpected en-XA string: got %+v, want \"bbb one\"", pseudo)
+	}
+
+	if pseudo := makeEnXAString("a", false); pseudo != "å" {
+		t.Errorf("Unexpected en-XA string: got %+v, want \"å\"", pseudo)
+	}
+
+	long := "b b b b b b b b b b b one two three four five six seven eight nine ten one"
+	if pseudo := makeEnXAString("b b b b b b b b b b b", true); pseudo != long {
+		t.Errorf("Unexpected en-XA string: got %+v, want %+v", pseudo, long)
+	}
+
+	allPseudoLetters := "åbçðéƒĝĥîĵķļmñöþqršţûvŵxýžÅBÇÐÉFĜĤÎĴĶĻMÑÖÞQ®ŠŢÛVŴXÝŽ¡@#€%^&*()¿"
+	if pseudo := makeEnXAString("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?", false); pseudo != allPseudoLetters {
+		t.Errorf("Unexpected en-XA string: got %+v, want %v", pseudo, allPseudoLetters)
+	}
+}
+
+func TestAttributeMatcher(t *gotesting.T) {
+	for _, tc := range []struct {
+		in  *Finder
+		out string
+	}{
+		{Role(role.Button), `{}`},
+		{ClassName("clsname"), `{"className":"clsname",}`},
+		{Name("hello"),
+			`{"name":selectName({"ar-XB":/^olleh$/,"en":/^hello$/,"en-XA":/^ĥéļļö one$/,})}`},
+		{MultilingualName("hello", map[string]string{"de": "hallo"}),
+			`{"name":selectName({"ar-XB":/^olleh$/,"de":/^hallo$/,"en":/^hello$/,"en-XA":/^ĥéļļö one$/,})}`},
+		{MultilingualName("hello", map[string]string{"en-XA": "hello a"}),
+			`{"name":selectName({"ar-XB":/^olleh$/,"en":/^hello$/,"en-XA":/^hello a$/,})}`},
+		{MultilingualNameStartingWith("hello", map[string]string{"de": "hallo", "ar": "abc"}),
+			`{"name":selectName({"ar":/^.*abc$/,"ar-XB":/^.*olleh$/,"de":/^hallo.*$/,"en":/^hello.*$/,"en-XA":/^ĥéļļö.*$/,})}`},
+		{MultilingualNameContaining("hello", map[string]string{"de": "hallo", "ar": "abc"}),
+			`{"name":selectName({"ar":/^.*abc.*$/,"ar-XB":/^.*olleh.*$/,"de":/^.*hallo.*$/,"en":/^.*hello.*$/,"en-XA":/^.*ĥéļļö.*$/,})}`},
+	} {
+		out, err := tc.in.attributesBytes()
+		if err != nil {
+			t.Errorf("%+v.attributesBytes() failed: %v", tc.in, err)
+		} else if string(out) != tc.out {
+			t.Errorf("%+v.attributesBytes() failed:\ngot:\n%v\nexpected:\n%v", tc.in, string(out), tc.out)
+		}
+	}
+	if _, err := MultilingualName("hello", map[string]string{"en.us": "hello"}).attributesBytes(); err == nil {
+		t.Error("MultilingualName should fail at converting to bytes when the language is invalid")
+	}
+}
