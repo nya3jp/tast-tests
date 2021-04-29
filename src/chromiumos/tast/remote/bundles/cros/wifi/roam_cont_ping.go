956c477ac3d7932ab580ba084b55b800f7cf89b9
Jacek Siuda <jck@semihalf.com>
2021-04-29 21:49:35 +0000

    wifi: Roaming Continuity ping test
    Adds new test for checking traffic continuity during WiFi roaming.
    
    Test is based on RoamFT, but designed to also run variants without FT.
    
    Adds verification framework for running arbitrary verification function
    in parallel to the main test.
    
    Adds the first continuity verification method (ping).
    
    Adds destructor stack to aid commonalizing code for multiple test.
    
    BUG=b:185142724
    TEST=tast run <host> wifi.RoamContPing.none
      Sample summary logs excerpt:
      Stop Verification
      Iteration 1: End Time=21:11:44.726, Packets lost=29
      Iteration 2: End Time=21:11:55.672, Packets lost=40
      Iteration 3: End Time=21:12:06.460, Packets lost=25
      Total packets lost=94/3000 (9 per round).
    
    Change-Id: Id8612b9ac580eb9ae120e2dc77bd085faa7966b5

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/roam_cont_ping.go
@@ -0,0 +1,210 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"chromiumos/tast/common/crypto/certificate"
+	"chromiumos/tast/common/network/ping"
+	"chromiumos/tast/common/wifi/security/wpa"
+	"chromiumos/tast/common/wifi/security/wpaeap"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/remote/bundles/cros/wifi/wifiutil"
+	remoteping "chromiumos/tast/remote/network/ping"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/remote/wificell/hostapd"
+	"chromiumos/tast/remote/wificell/verifier"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+type pingParam struct {
+	opts []ping.Option
+	// Max packets lost per roaming round.
+	maxLoss int
+}
+
+var (
+	contCert1 = certificate.TestCert1()
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: RoamContPing,
+		Desc: "Send ping every 10ms and check how many packets are lost on average",
+		Contacts: []string{
+			"jck@semihalf.com",                // Test author
+			"chromeos-wifi-champs@google.com", // WiFi oncall rotation; or http://b/new?component=893827
+		},
+		Attr:        []string{"group:wificell", "wificell_perf", "wificell_unstable"},
+		ServiceDeps: []string{wificell.TFServiceName},
+		Pre:         wificell.TestFixturePre(),
+		Vars:        []string{"router", "pcap", "wifi.RoamContPing.rounds"},
+		Params: []testing.Param{{
+			Name: "none",
+			Val: wifiutil.ContParam{
+				Rounds: 50,
+				ApOpts: [2][]hostapd.Option{{hostapd.Channel(1), hostapd.Mode(hostapd.Mode80211g)},
+					{hostapd.Channel(157), hostapd.Mode(hostapd.Mode80211acPure), hostapd.HTCaps(hostapd.HTCapHT40Plus), hostapd.VHTCaps(hostapd.VHTCapSGI80),
+						hostapd.VHTChWidth(hostapd.VHTChWidth80), hostapd.VHTCenterChannel(155)}},
+				Param: pingParam{
+					opts: []ping.Option{ping.Count(1000), ping.Interval(0.01)},
+					// With 10ms ping interval, abnormal time without service would be 25 packets = 250ms.
+					maxLoss: 25,
+				},
+			},
+		}, {
+			Name: "psk",
+			Val: wifiutil.ContParam{
+				Rounds: 50,
+				ApOpts: [2][]hostapd.Option{{hostapd.Channel(1), hostapd.Mode(hostapd.Mode80211g)},
+					{hostapd.Channel(157), hostapd.Mode(hostapd.Mode80211acPure), hostapd.HTCaps(hostapd.HTCapHT40Plus), hostapd.VHTCaps(hostapd.VHTCapSGI80),
+						hostapd.VHTChWidth(hostapd.VHTChWidth80), hostapd.VHTCenterChannel(155)}},
+				SecConfFac: wpa.NewConfigFactory("chromeos", wpa.Mode(wpa.ModePureWPA2), wpa.Ciphers2(wpa.CipherCCMP)),
+				Param: pingParam{
+					opts:    []ping.Option{ping.Count(1000), ping.Interval(0.01)},
+					maxLoss: 25,
+				},
+			},
+		}, {
+			Name: "ft_psk",
+			Val: wifiutil.ContParam{
+				Rounds: 50,
+				ApOpts: [2][]hostapd.Option{{hostapd.Channel(1), hostapd.Mode(hostapd.Mode80211g)},
+					{hostapd.Channel(157), hostapd.Mode(hostapd.Mode80211acPure), hostapd.HTCaps(hostapd.HTCapHT40Plus), hostapd.VHTCaps(hostapd.VHTCapSGI80),
+						hostapd.VHTChWidth(hostapd.VHTChWidth80), hostapd.VHTCenterChannel(155)}},
+				SecConfFac: wpa.NewConfigFactory("chromeos", wpa.Mode(wpa.ModePureWPA2), wpa.Ciphers2(wpa.CipherCCMP), wpa.FTMode(wpa.FTModePure)),
+				EnableFT:   true,
+				Param: pingParam{
+					opts:    []ping.Option{ping.Count(1000), ping.Interval(0.01)},
+					maxLoss: 25,
+				},
+			},
+			ExtraHardwareDeps: hwdep.D(hwdep.Wifi80211ac()),
+		}, {
+			Name: "eap",
+			Val: wifiutil.ContParam{
+				Rounds: 50,
+				ApOpts: [2][]hostapd.Option{{hostapd.Channel(1), hostapd.Mode(hostapd.Mode80211g)},
+					{hostapd.Channel(157), hostapd.Mode(hostapd.Mode80211acPure), hostapd.HTCaps(hostapd.HTCapHT40Plus), hostapd.VHTCaps(hostapd.VHTCapSGI80),
+						hostapd.VHTChWidth(hostapd.VHTChWidth80), hostapd.VHTCenterChannel(155)}},
+				SecConfFac: wpaeap.NewConfigFactory(
+					contCert1.CACred.Cert, contCert1.ServerCred,
+					wpaeap.ClientCACert(contCert1.CACred.Cert), wpaeap.ClientCred(contCert1.ClientCred),
+					wpaeap.Mode(wpa.ModePureWPA2),
+				),
+				Param: pingParam{
+					opts:    []ping.Option{ping.Count(1000), ping.Interval(0.01)},
+					maxLoss: 25,
+				},
+			},
+		}, {
+			Name: "ft_eap",
+			Val: wifiutil.ContParam{
+				Rounds: 50,
+				ApOpts: [2][]hostapd.Option{{hostapd.Channel(1), hostapd.Mode(hostapd.Mode80211g)},
+					{hostapd.Channel(157), hostapd.Mode(hostapd.Mode80211acPure), hostapd.HTCaps(hostapd.HTCapHT40Plus), hostapd.VHTCaps(hostapd.VHTCapSGI80),
+						hostapd.VHTChWidth(hostapd.VHTChWidth80), hostapd.VHTCenterChannel(155)}},
+				SecConfFac: wpaeap.NewConfigFactory(
+					contCert1.CACred.Cert, contCert1.ServerCred,
+					wpaeap.ClientCACert(contCert1.CACred.Cert), wpaeap.ClientCred(contCert1.ClientCred),
+					wpaeap.Mode(wpa.ModePureWPA2), wpaeap.FTMode(wpa.FTModePure),
+				),
+				EnableFT: true,
+				Param: pingParam{
+					opts:    []ping.Option{ping.Count(1000), ping.Interval(0.01)},
+					maxLoss: 25,
+				},
+			},
+			ExtraHardwareDeps: hwdep.D(hwdep.Wifi80211ac()),
+		}},
+	})
+}
+
+func RoamContPing(ctx context.Context, s *testing.State) {
+	tf := s.PreValue().(*wificell.TestFixture)
+	defer func(ctx context.Context) {
+		if err := tf.CollectLogs(ctx); err != nil {
+			s.Log("Error collecting logs, err: ", err)
+		}
+	}(ctx)
+	ctx, cancel := tf.ReserveForCollectLogs(ctx)
+	defer cancel()
+
+	param := s.Param().(wifiutil.ContParam)
+
+	// Allow override
+	var rounds int
+	roundsStr, _ := s.Var("wifi.RoamContPing.rounds")
+	if roundsStr == "" {
+		rounds = param.Rounds
+	} else {
+		var err error
+		rounds, err = strconv.Atoi(roundsStr)
+		if err != nil {
+			s.Fatal("Failed to convert value, err: ", err)
+		}
+	}
+
+	ctx, ct, finish := wifiutil.ContinuityTestInitialSetup(ctx, s, tf)
+	defer finish()
+	ctx, cancel = ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	var vf *verifier.Verifier
+	var resultAssertF func(context.Context, []verifier.ResultType)
+	pingF := func(ctx context.Context) (verifier.ResultType, error) {
+		// We need more result data than a simple tf.PingFromDUT(), so we use a separate runner.
+		pr := remoteping.NewRemoteRunner(s.DUT().Conn())
+		res, err := pr.Ping(ctx, wifiutil.ServerIP(), param.Param.(pingParam).opts...)
+		if err != nil {
+			testing.ContextLog(ctx, "ping error: ", err)
+			return verifier.ResultType{}, err
+		}
+		testing.ContextLogf(ctx, "Continuity: ping statistics=%+v", res)
+		return verifier.ResultType{Data: res, Timestamp: time.Now()}, nil
+	}
+	resultAssertF = func(ctx context.Context, results []verifier.ResultType) {
+		var sent, received int
+		for i, ret := range results {
+			pingData := ret.Data.(*ping.Result)
+			testing.ContextLogf(ctx, "Iteration %d: End Time=%s, Packets lost=%d",
+				i+1, ret.Timestamp.Format("15:04:05.000"), pingData.Sent-pingData.Received)
+			sent += pingData.Sent
+			received += pingData.Received
+		}
+		loss := sent - received
+		testing.ContextLogf(ctx, "Total packets lost=%d/%d (%d per round)",
+			loss, sent, loss/rounds)
+
+		if loss > rounds*param.Param.(pingParam).maxLoss {
+			s.Fatal("Loss threshold exceeded")
+		}
+	}
+	vf = verifier.NewVerifier(ctx, pingF)
+	defer vf.Finish()
+	ctx, cancel = ctxutil.Shorten(ctx, time.Second)
+	defer cancel()
+
+	ctx, destroy := ct.ContinuityTestSetupFinalize(ctx, s)
+	defer destroy()
+	ctx, cancel = ctxutil.Shorten(ctx, time.Second)
+	defer cancel()
+
+	vf.StartJob()
+
+	for i := 0; i < rounds; i++ {
+		ct.ContinuityRound(ctx, s, i)
+	}
+	results, err := vf.StopJob()
+	if err != nil {
+		testing.ContextLog(ctx, "Error while receiving verification results: ", err)
+		return
+	}
+	resultAssertF(ctx, results)
+}
