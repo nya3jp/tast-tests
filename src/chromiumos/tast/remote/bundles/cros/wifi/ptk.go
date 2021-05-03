36b769b727981e474a061784fef0924df3378c6d
Shuo-Peng Liao <deanliao@chromium.org>
2021-05-04 03:33:25 +0800

    tast-test: promote wifi.PTK to stable
    wifi.PTK is stable except for devices having Qualcomm QCA6174A-3 WiFi
    modules. Use HardwareDeps to run the test in suite:wifi_matfunc on
    devices without QCA6174A-3.
    
    BUG=b:160113424, b:183463918
    TEST=tast run wifi.PTK* on kukui and expect to see
    wifi.PTK.low_ping_loss and wifi.PTK.qualcomm runs, and
    wifi.PTK skips.
    
    Change-Id: Idcb9ec9b3d6c2039ccab0ed1bc0c60124d1ca328

--- src/chromiumos/tast/remote/bundles/cros/wifi/ptk.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/ptk.go
@@ -18,6 +18,7 @@ import (
 	"chromiumos/tast/remote/wificell/hostapd"
 	"chromiumos/tast/services/cros/network"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 type ptkParam struct {
@@ -26,6 +27,9 @@ type ptkParam struct {
 	pingInterval     float64
 	allowedLossCount int
 }
+
+// TODO(b/183463918): remove the restriction once the bug is solved.
+var ptkBuggyPlatform = []string{"kukui", "jacuzzi"}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -42,7 +46,22 @@ func init() {
 		Params: []testing.Param{
 			{
 				// Default case.
-				ExtraAttr: []string{"wificell_unstable"},
+				ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform(ptkBuggyPlatform...)),
+				// The ping configuration gives us around 75 seconds to ping,
+				// which covers around 15 rekeys with 5 seconds period.
+				Val: ptkParam{
+					rekeyPeriod:      5,
+					pingCount:        150,
+					pingInterval:     0.5,
+					allowedLossCount: 30, // Allow 20% ping loss.
+				},
+			},
+			{
+				// Qualcomm QCA6174A-3 case.
+				// TODO(b/183463918): remove this once the issue is fixed.
+				Name:              "qca6174a3",
+				ExtraHardwareDeps: hwdep.D(hwdep.Platform(ptkBuggyPlatform...)),
+				ExtraAttr:         []string{"wificell_unstable"},
 				// The ping configuration gives us around 75 seconds to ping,
 				// which covers around 15 rekeys with 5 seconds period.
 				Val: ptkParam{
