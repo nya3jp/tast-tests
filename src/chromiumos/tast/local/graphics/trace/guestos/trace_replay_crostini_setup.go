09f818298dbc50f842ae178c554773a38e88d8b2
Ryan Neph <ryanneph@google.com>
2021-05-03 19:05:37 +0000

    graphics: share TraceReplay setup with TraceReplayExtended
    Share test setup/execution among TraceReplay* tests.
    
    BUG=b:185822218
    TEST=tast run -buildbundle=cros                                 \
         -var=keepState=true                                        \
         $DUT graphics.TraceReplay.glxgears_unstable
    TEST=tast run -buildbundle=cros                                 \
         -var=keepState=true                                        \
         -var=PowerTest.resultDir=/tmp/results                      \
         -var=PowerTest.signalRunningFile=/tmp/signal_running       \
         -var=PowerTest.signalCheckpointFile=/tmp/signal_checkpoint \
         $DUT graphics.TraceReplayExtended.glxgears_1minute
    
    Change-Id: I43ecd8d2cebf24aedc3d23c580b496eba21becbb

---
+++ src/chromiumos/tast/local/graphics/trace/guestos/trace_replay_crostini_setup.go
@@ -0,0 +1,33 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package guestos
+
+import (
+	"context"
+
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/local/graphics/trace"
+	"chromiumos/tast/local/graphics/trace/comm"
+	"chromiumos/tast/testing"
+)
+
+// TraceReplayCrostiniSetup replays a graphics trace inside a crostini container.
+func TraceReplayCrostiniSetup(ctx context.Context, s *testing.State) {
+	pre := s.PreValue().(crostini.PreData)
+	config := s.Param().(comm.TestGroupConfig)
+	defer crostini.RunCrostiniPostTest(ctx, s.PreValue().(crostini.PreData))
+	guest := CrostiniGuestOS{
+		VMInstance: pre.Container,
+	}
+
+	var pTestVars *comm.TestVars
+	if config.ExtendedDuration > 0 {
+		pTestVars = &comm.TestVars{PowerTestVars: comm.GetPowerTestVars(s)}
+	}
+
+	if err := trace.RunTraceReplayTest(ctx, s.OutDir(), s.CloudStorage(), &guest, &config, pTestVars); err != nil {
+		s.Fatal("Trace replay test failed: ", err)
+	}
+}
