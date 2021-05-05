d0e3bc3018b27de16073a6896682c6603d7524a0
Xibin Liu <xliu@cienet.com>
2021-05-05 12:09:25 -0700

    tast-tests: allow background app installation
    In playstore.InstallApp(), instead of waiting for the "Open" or "Play" button as
    an indication of APP installation completes, use PackageInstalled() to
    verify if the app has been installed, in case the Play Store window has
    been closed and installation is in the background.
    
    BUG=chromium:1206005
    TEST=fast_build.sh -T
    
    Change-Id: I54d0bbb831a735380d1936e7f19a0266b6426baf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2874727
    Tested-by: Xibin Liu <xliu@cienet.com>
    Commit-Queue: Alex Bergman <abergman@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- src/chromiumos/tast/local/arc/playstore/playstore.go
+++ src/chromiumos/tast/local/arc/playstore/playstore.go
@@ -7,7 +7,6 @@ package playstore
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"chromiumos/tast/common/testexec"
@@ -36,8 +35,6 @@ func InstallApp(ctx context.Context, a *arc.ARC, d *ui.Device, pkgName string, t
 		gotItButtonText    = "got it"
 		installButtonText  = "install"
 		okButtonText       = "ok"
-		openButtonText     = "open"
-		playButtonText     = "play"
 		retryButtonText    = "retry"
 		skipButtonText     = "skip"
 
@@ -65,7 +62,7 @@ func InstallApp(ctx context.Context, a *arc.ARC, d *ui.Device, pkgName string, t
 	// Wait for the app to install.
 	testing.ContextLog(ctx, "Waiting for app to install")
 	tries := 0
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
+	return testing.Poll(ctx, func(ctx context.Context) error {
 		// Sometimes a dialog of "Can't download <app name>" pops up. Press Okay to
 		// dismiss the dialog. This check needs to be done before checking the
 		// install button since the install button exists underneath.
@@ -170,22 +167,15 @@ func InstallApp(ctx context.Context, a *arc.ARC, d *ui.Device, pkgName string, t
 			}
 		}
 
-		// Installation is complete once the open button or the play button is enabled.
-		if err := d.Object(ui.ClassName("android.widget.Button"), ui.TextMatches(fmt.Sprintf("(?i)(%s|%s)", openButtonText, playButtonText)), ui.Enabled(true)).Exists(ctx); err != nil {
-			return errors.Wrap(err, "failed to wait for enabled open button or play button")
+		// Verify if the package has been installed.
+		installed, err = a.PackageInstalled(ctx, pkgName)
+		if err != nil {
+			return testing.PollBreak(err)
 		}
-		return nil
-	}, &testing.PollOptions{Interval: time.Second}); err != nil {
-		return err
-	}
+		if !installed {
+			return errors.Errorf("package %s has not been installed", pkgName)
+		}
 
-	// Ensure that the correct package is installed, just in case the Play Store ui changes again.
-	installed, err = a.PackageInstalled(ctx, pkgName)
-	if err != nil {
-		return err
-	}
-	if !installed {
-		return errors.Errorf("failed to install %s", pkgName)
-	}
-	return nil
+		return nil
+	}, &testing.PollOptions{Interval: time.Second})
 }
