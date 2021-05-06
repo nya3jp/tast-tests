77b6598ae760277d1ac5cd432025b67a2f89f107
Matthew Wang <matthewmwang@chromium.org>
2021-05-05 19:06:28 -0700

    tast: add NonPrefChan tast test
    This tests the MBO non-preferred channel report functionality.
    
    BUG=b:180664188
    TEST=run test
    
    Change-Id: I38b27ee5e04a8e4472d94e17feb7a16a53ecd9b1

---
+++ src/chromiumos/tast/common/wifi/ieee80211/ieee80211.go
@@ -0,0 +1,10 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package ieee80211 defines some 802.11 constants not defined by
+// gopacket/layers/dot11.go
+package ieee80211
+
+// WFAOUI is the OUI corresponding to the WiFi Alliance
+var WFAOUI = []byte{0x50, 0x6F, 0x9A}
