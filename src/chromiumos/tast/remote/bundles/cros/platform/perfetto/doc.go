5d8070510e50e85c01dc065fe0d97f22e0cffbf6
Harvey Yang <chenghaoyang@chromium.org>
2021-05-06 17:31:22 +0800

    perfetto: Add remote tast test for commandline perfetto
    This commit adds an example remote tast test for commandline perfetto
    with android metrics: cpu and mem.
    
    BUG=b:187386092
    TEST=run on coachz
    
    Cq-Depend: chromium:2987603
    Change-Id: I3d7f2d67d562e5593e4f24e786eb384f73a3aa35

---
+++ src/chromiumos/tast/remote/bundles/cros/platform/perfetto/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package perfetto contains helper functions and constants to record traces
+// and run metrics with perfetto protobufs.
+package perfetto
---
+++ src/chromiumos/tast/remote/bundles/cros/platform/perfetto/traces.go
@@ -0,0 +1,77 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package perfetto
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+
+	tppb "chromiumos/perfetto/trace_processor"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/services/cros/platform"
+)
+
+const (
+	// TraceProcessor is retrieved from the script in
+	// https://get.perfetto.dev/trace_processor, with "os" being "linux"
+	// and "arch" being "x86_64".
+	// We download trace_processor_shell from gs bucket "perfetto".
+	// Update the external data file correspondingly when we need to
+	// uprev trace_processor_shell.
+	TraceProcessor = "trace_processor_shell-linux-a3ce2cbf4cbe4f86cc10b02957db727cecfafae8"
+)
+
+// RunPerfetto uses gRPC to run perfetto cmdline with
+// |traceConfigFile| in the DUT.
+func RunPerfetto(ctx context.Context, pc platform.PerfettoTraceBasedMetricsServiceClient, traceConfigPath string) (string, error) {
+	config, err := ioutil.ReadFile(traceConfigPath)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to read config file")
+	}
+
+	stream, err := pc.GeneratePerfettoTrace(ctx, &platform.GeneratePerfettoTraceRequest{Config: string(config)})
+	if err != nil {
+		return "", errors.Wrap(err, "failed to call gRPC GeneratePerfettoTrace")
+	}
+
+	response, err := stream.Recv()
+	if err != nil {
+		return "", err
+	}
+
+	tempFile, err := ioutil.TempFile("/tmp", "perfetto-trace-*.pb")
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create temp file")
+	}
+
+	if _, err := tempFile.Write(response.Result); err != nil {
+		os.Remove(tempFile.Name())
+		return "", errors.Wrap(err, "failed to write the result to temp file")
+	}
+
+	return tempFile.Name(), nil
+}
+
+// RunMetrics collects the result with trace_processor_shell.
+func RunMetrics(ctx context.Context, traceProcessorPath, outputPath string, metrics []string) (*tppb.TraceMetrics, error) {
+	metric := strings.Join(metrics, ",")
+	cmd := testexec.CommandContext(ctx, traceProcessorPath, outputPath, "--run-metrics", metric)
+	out, err := cmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to run metrics with trace_processor_shell")
+	}
+
+	tbm := &tppb.TraceMetrics{}
+	if err := proto.UnmarshalText(string(out), tbm); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal metrics result")
+	}
+
+	return tbm, nil
+}
