d59c2383fcd177ff7e1fabef7e1acd6ba47673c3
ChromeOS Developer <qjw@chromium.org>
2021-05-06 18:07:48 +1000

    tast-tests: add test for Web Handwriting Recognition for borads without ondevice_handwriting
    BUG=chromium:1186510
    TEST=Manual run on device, with ondevice_handwriting USE removed
    
    Change-Id: I3806c1471262f1a8fb26c885ba67b917362141f4

---
+++ src/chromiumos/tast/local/bundles/cros/mlservice/web_handwriting_recognition_not_supported.go
@@ -0,0 +1,74 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package mlservice
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: WebHandwritingRecognitionNotSupported,
+		Desc: "Checks Web Handwriting Recognition API works, even if ml_service doesn't support ondevice_handwriting",
+		Contacts: []string{
+			"qjw@chromium.org",               // Test author
+			"honglinyu@google.com",           // ml_service contact
+			"handwriting-web-api@google.com", // Backup mailing list
+		},
+		Timeout:      3 * time.Minute,
+		SoftwareDeps: []string{"chrome", "no_ondevice_handwriting"},
+		Attr:         []string{"group:mainline", "informational"},
+		Data: []string{
+			"web_handwriting_recognition_not_supported.html",
+		},
+	})
+}
+
+func WebHandwritingRecognitionNotSupported(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	// Setup test HTTP server.
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	// Launch chrome.
+	cr, err := chrome.New(
+		ctx,
+		chrome.ExtraArgs("--enable-experimental-web-platform-features"),
+		// TODO(https://crbug.com/1177374): Remove the below feature when no longer needed.
+		chrome.EnableFeatures("HandwritingRecognitionWebPlatformApi"),
+	)
+	if err != nil {
+		s.Fatal("Failed to start chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	// Open the test page.
+	conn, err := cr.NewConn(ctx, server.URL+"/web_handwriting_recognition_not_supported.html")
+	if err != nil {
+		s.Fatal("Failed to open test web page: ", err)
+	}
+	defer conn.Close()
+
+	// Wait for JavaScript test to start. The test exposes `resultPromise`, which either
+	// resolves to true (indicating PASS), or rejects with an error message (indicating
+	// FAILURE).
+	if err = conn.WaitForExpr(ctx, "'resultPromise' in window"); err != nil {
+		s.Fatal("Failed to wait for test page result promise: ", err)
+	}
+
+	if err = conn.Eval(ctx, "window.resultPromise", nil); err != nil {
+		s.Fatal("Failed to complete JS test: ", err)
+	}
+}
