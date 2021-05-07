d54c6ada98b7b4f9333ac5c2fcddd27da54addcb
Kamil Szarek <kamilszarek@google.com>
2021-05-07 16:05:17 +0000

    kiosk: Add test for starting app from sign in screen
    Add test that enables Kiosk applications and starts one by
    interacting from sign-in screen.
    Move code that is common to Kiosk tests into kiosk_mode.go and adjust
    autostart.go test to use kiosk_mode.go
    
    BUG=None
    TEST=tast run <ip> kiosk.StartAppFromSignInScreen
    
    Change-Id: I4ffebcf81fdd4a57f5256795539bc42b7c723100

--- src/chromiumos/tast/local/bundles/cros/kiosk/autostart.go
+++ src/chromiumos/tast/local/bundles/cros/kiosk/autostart.go
@@ -6,13 +6,10 @@ package kiosk
 
 import (
 	"context"
-	"strings"
-	"time"
 
-	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/kioskmode"
 	"chromiumos/tast/local/policyutil/fixtures"
 	"chromiumos/tast/local/syslog"
 	"chromiumos/tast/testing"
@@ -53,50 +50,11 @@ func Autostart(ctx context.Context, s *testing.State) {
 		}
 	}(ctx)
 
-	// DeviceLocalAccountInfo uses *string instead of string for internal data
-	// structure. That is needed since fields in json are marked as omitempty.
-	webKioskAccountID := "1"
-	webKioskAccountType := policy.AccountTypeKioskWebApp
-	webKioskIconURL := "https://www.google.com"
-	webKioskTitle := "TastKioskModeSetByPolicyGooglePage"
-	webKioskURL := "https://www.google.com"
-
-	kioskAppAccountID := "2"
-	kioskAppAccountType := policy.AccountTypeKioskApp
-	kioskAppID := "aajgmlihcokkalfjbangebcffdoanjfo"
-	// TODO: kamilszarek@ - turn that into a default Kiosk mode fixture
-	// Update policies and refresh. Here we wait for test API, hence login was
-	// required.
-	// ServerAndRefresh is used instead of ServerAndVerify since Verify part
-	// uses Autotest private api that returns only Enterprise policies values
-	// but not device policies values.
-	if err := policyutil.ServeAndRefresh(ctx, fdms, cr, []policy.Policy{
-		&policy.DeviceLocalAccounts{
-			Val: []policy.DeviceLocalAccountInfo{
-				{
-					AccountID:   &kioskAppAccountID,
-					AccountType: &kioskAppAccountType,
-					KioskAppInfo: &policy.KioskAppInfo{
-						AppId: &kioskAppID,
-					},
-				},
-				{
-					AccountID:   &webKioskAccountID,
-					AccountType: &webKioskAccountType,
-					WebKioskAppInfo: &policy.WebKioskAppInfo{
-						Url:     &webKioskURL,
-						Title:   &webKioskTitle,
-						IconUrl: &webKioskIconURL,
-					},
-				},
-			},
-		},
-		&policy.DeviceLocalAccountAutoLoginId{
-			Val: webKioskAccountID,
-		},
-	}); err != nil {
+	// Update policies.
+	if err := kioskmode.SetAutolaunch(ctx, fdms, cr, kioskmode.WebKioskAccountID); err != nil {
 		s.Fatal("Failed to update policies: ", err)
 	}
+
 	// Close the previous Chrome instance.
 	if err := cr.Close(ctx); err != nil {
 		s.Error("Failed to close Chrome connection: ", err)
@@ -122,24 +80,7 @@ func Autostart(ctx context.Context, s *testing.State) {
 		s.Fatal("Chrome restart failed: ", err)
 	}
 
-	const (
-		kioskStarting        = "Starting kiosk mode"
-		kioskLaunchSucceeded = "Kiosk launch succeeded"
-	)
-
-	if _, err := reader.Wait(ctx, 60*time.Second,
-		func(e *syslog.Entry) bool {
-			return strings.Contains(e.Content, kioskStarting)
-		},
-	); err != nil {
-		s.Fatal("Failed to verify starting of Kiosk mode: ", err)
-	}
-
-	if _, err := reader.Wait(ctx, 60*time.Second,
-		func(e *syslog.Entry) bool {
-			return strings.Contains(e.Content, kioskLaunchSucceeded)
-		},
-	); err != nil {
-		s.Fatal("Failed to verify successful launch of Kiosk mode: ", err)
+	if err := kioskmode.ConfirmKioskStarted(ctx, reader); err != nil {
+		s.Fatal("There was a problem while checking chrome logs for Kiosk related entries: ", err)
 	}
 }
---
+++ src/chromiumos/tast/local/bundles/cros/kiosk/start_app_from_sign_in_screen.go
@@ -0,0 +1,132 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package kiosk
+
+import (
+	"context"
+
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/kioskmode"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/local/syslog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: StartAppFromSignInScreen,
+		Desc: "Adds 2 Kiosk accounts, checks if both are available then starts one of them",
+		Contacts: []string{
+			"kamilszarek@google.com", // Test author
+			"alt-modalities-stability@google.com",
+		},
+		Vars: []string{"ui.signinProfileTestExtensionManifestKey"},
+		// Informational attribute can only be removed when
+		// https://crbug.com/1207293 is resolved.
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "fakeDMSEnrolled",
+	})
+}
+
+func StartAppFromSignInScreen(ctx context.Context, s *testing.State) {
+	fdms := s.FixtValue().(*fakedms.FakeDMS)
+	cr, err := chrome.New(
+		ctx,
+		chrome.FakeLogin(chrome.Creds{User: fixtures.Username, Pass: fixtures.Password}),
+		chrome.DMSPolicy(fdms.URL),
+		chrome.KeepEnrollment(),
+	)
+	if err != nil {
+		s.Error("Failed to start Chrome: ", err)
+	}
+
+	defer func(ctx context.Context) {
+		// Use cr as a reference to close the last started Chrome instance.
+		if cr != nil {
+			if err := cr.Close(ctx); err != nil {
+				s.Error("Failed to close Chrome connection: ", err)
+			}
+		}
+	}(ctx)
+
+	// Update policies.
+	if err := kioskmode.SetDefaultAppPolicies(ctx, fdms, cr); err != nil {
+		s.Fatal("Failed to update policies: ", err)
+	}
+
+	// Close the previous Chrome instance.
+	if err := cr.Close(ctx); err != nil {
+		s.Error("Failed to close Chrome connection: ", err)
+	}
+
+	reader, err := syslog.NewReader(ctx, syslog.Program("chrome"))
+	if err != nil {
+		s.Fatal("Failed to start log reader: ", err)
+	}
+	defer reader.Close()
+
+	// Restart Chrome.
+	cr, err = chrome.New(ctx,
+		chrome.DeferLogin(),
+		chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey")),
+		chrome.DMSPolicy(fdms.URL),
+		chrome.KeepEnrollment(),
+	)
+	if err != nil {
+		s.Fatal("Chrome restart failed: ", err)
+	}
+
+	testConn, err := cr.SigninProfileTestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, testConn)
+
+	localAccountsBtn := nodewith.Name("Apps").ClassName("MenuButton")
+	ui := uiauto.New(testConn)
+	if err := uiauto.Combine("open Kiosk application menu",
+		// It looks like UI is not stable to interact even when polling for
+		// elements. I was not able to find out how to overcome flakiness other
+		// than multiple waits or using hard sleep before interacting with UI.
+		ui.WaitUntilExists(localAccountsBtn),
+		ui.WaitUntilExists(localAccountsBtn),
+		ui.WaitUntilExists(localAccountsBtn),
+		ui.LeftClick(localAccountsBtn), // Often, after that click App (local accounts) buttton freezes.
+		ui.WaitUntilExists(kioskmode.KioskAppBtnNode),
+	)(ctx); err != nil {
+		s.Fatal("Failed to open menu with local accounts: ", err)
+	}
+
+	// Get applications that show up after clicking App button.
+	menuItems, err := ui.NodesInfo(ctx, nodewith.ClassName("MenuItemView"))
+	if err != nil {
+		s.Fatal("Failed to get local accounts: ", err)
+	}
+
+	const expectedLocalAccountsCount = 2
+	if len(menuItems) != expectedLocalAccountsCount {
+		s.Fatalf("Expected %d local accounts, but found %v app(s) %q", expectedLocalAccountsCount, len(menuItems), menuItems)
+	}
+
+	// When I had here only clicking the menu item that should be visible, the
+	// test failed at finding the menu item.
+	if err := uiauto.Combine("close and open Kiosk application menu then click on one menu item",
+		ui.LeftClick(localAccountsBtn),                // Close previously opened menu.
+		ui.LeftClick(localAccountsBtn),                // Open the menu again.
+		ui.WaitUntilExists(kioskmode.KioskAppBtnNode), // Wait for the menu item to be visible.
+		ui.LeftClick(kioskmode.KioskAppBtnNode),       // Launch the Kiosk app.
+	)(ctx); err != nil {
+		s.Fatal("Failed to start Kiosk application from Sign-in screen: ", err)
+	}
+
+	if err := kioskmode.ConfirmKioskStarted(ctx, reader); err != nil {
+		s.Fatal("There was a problem while checking chrome logs for Kiosk related entries: ", err)
+	}
+}
