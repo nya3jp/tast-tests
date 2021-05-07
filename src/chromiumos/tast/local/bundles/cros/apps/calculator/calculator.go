ba31c7ddb7cabeb7f5a1206e374bf36fe9b08af5
Jun Sheng <shengjun@google.com>
2021-05-07 13:09:07 +1000

    tast-tests: add a smoke test on calculator
    1. Add calculator info in apps.
    2. create calc package containing common functions.
    3. Add a new test CalculatorSmoke.
    
    BUG=b:187254857
    TEST=apps.CalculatorSmoke
    
    Change-Id: I366da31cede6147b216945221cd9418f7c75c230

---
+++ src/chromiumos/tast/local/bundles/cros/apps/calculator/calculator.go
@@ -0,0 +1,65 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package calc contains common functions used in the Calculator app.
+package calc
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/testing"
+)
+
+// UIConn returns a connection to the Calculator app HTML page,
+// where JavaScript can be executed to simulate interactions with the UI.
+// The caller should close the returned connection. e.g. defer calcConn.Close().
+func UIConn(ctx context.Context, cr *chrome.Chrome) (*chrome.Conn, error) {
+	// Establish a Chrome connection to the Calculator app and wait for it to finish loading.
+	targetURL := fmt.Sprintf(`chrome-extension://%s/index.html`, apps.Calculator.ID)
+	appConn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(targetURL))
+	if err != nil {
+		return nil, err
+	}
+	if err := appConn.WaitForExpr(ctx, `document.readyState === "complete"`); err != nil {
+		return nil, errors.Wrap(err, "failed to wait for Calculator app to finish loading")
+	}
+	return appConn, nil
+}
+
+// TapKey taps key on the app by executing click function on located web element.
+// keyName is aria-label of the element, not displayed text. e.g. Do not use '+' but 'plus'.
+func TapKey(appConn *chrome.Conn, keyName string) uiauto.Action {
+	script := fmt.Sprintf(`document.querySelector(".keypad canvas[aria-role='button'][aria-label='%s']").click()`, keyName)
+	return func(ctx context.Context) error {
+		if err := appConn.Eval(ctx, script, nil); err != nil {
+			return errors.Wrapf(err, "failed to tap key %q", keyName)
+		}
+		return nil
+	}
+}
+
+// WaitForCalculateResult waits until the calculation result is expected.
+func WaitForCalculateResult(appConn *chrome.Conn, expectedResult string) uiauto.Action {
+	script := `document.querySelector(".calculator-display").innerText`
+	var result string
+
+	return func(ctx context.Context) error {
+		return testing.Poll(ctx, func(c context.Context) error {
+			if err := appConn.Eval(ctx, script, &result); err != nil {
+				return errors.Wrap(err, "failed to get calculation result")
+			}
+
+			if result != expectedResult {
+				return errors.Errorf("Wrong calculation result: got %q; want %q", result, expectedResult)
+			}
+			return nil
+		}, &testing.PollOptions{Timeout: 10 * time.Second})
+	}
+}
