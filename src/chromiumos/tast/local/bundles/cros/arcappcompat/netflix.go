4aa3b25b0e7329c5709b6ff74eff6607b2c45ad3
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-05-10 12:04:27 -0700

    arcappcompat: add appcompat basic suite to few arcappcompat tests
    BUG=b:181041353
    TEST=test_that --board=eve <ip-address> tast.appcompat_basic
    
    Cq-Depend: chrome-internal:3821407
    Cq-Depend: chromium:2883483
    Cq-Depend: chromium:2876076
    Change-Id: I7dd43a3827caecdf329cc283baa95baf84e142b3

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/netflix.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/netflix.go
@@ -41,7 +41,7 @@ func init() {
 		Func:         Netflix,
 		Desc:         "Functional test for Netflix that installs the app also verifies it is logged in and that the main page is open, checks Netflix correctly changes the window state in both clamshell and touchview mode",
 		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:appcompat"},
+		Attr:         []string{"group:appcompat", "group:appcompat_basic"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Val:               clamshellTestsForNetflix,
