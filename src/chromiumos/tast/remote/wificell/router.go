5156129e38b2a908e481e7b1dc1b84ab6c97e6ab
Billy Zhao <billyzhao@chromium.org>
2021-05-10 06:53:43 +0000

    tast-tests: Add interface definitions to router
    We define the interface composition structure for the router module.
    This will allow us to efficiently develop code in this package to
    support multiple test topologies.
    
    This commit is mostly a noop since the interfaces aren't actually used.
    
    BUG=b:187719462
    TEST=ran wifi.WMM to see if it compiled
    
    Change-Id: I21d77cb45767b19ed55eb01273c5b3f6dd612c73

--- src/chromiumos/tast/remote/wificell/router.go
+++ src/chromiumos/tast/remote/wificell/router.go
@@ -32,6 +32,107 @@ import (
 	"chromiumos/tast/timing"
 )
 
+// RouterType is an enum indicating what type of router style a router is.
+type RouterType int
+
+const (
+	ax      RouterType = iota
+	openwrt RouterType = iota
+	legacy  RouterType = iota
+)
+
+// SuperRouter is a supertype of all routers. This is mostly used in tandem with a handler to see which methods are actually supported.
+type SuperRouter interface {
+	LegacyRouter
+	AxRouter
+	OpenwrtRouter
+}
+
+// AxRouter contains the funcionality that the ax testbed should support.
+type AxRouter interface {
+	baseRouter
+}
+
+// LegacyRouter contains the functionality the basic WiFi testing system should support.
+type LegacyRouter interface {
+	legacyOpenwrtShared
+}
+
+// OpenwrtRouter contains the functionality that the future openwrt testbed shall support.
+type OpenwrtRouter interface {
+	legacyOpenwrtShared
+}
+
+// baseRouter contains the basic functions shared across all routers.
+type baseRouter interface {
+	initialize(ctx, daemonCtx context.Context) error
+	Close(ctx context.Context) error
+	ReserveForClose(ctx context.Context) (context.Context, context.CancelFunc)
+}
+
+// legacyOpenwrtShared contains the functionality shared between legacy routers and openwrt routers.
+type legacyOpenwrtShared interface {
+	baseRouter
+	supportLogs
+	supportCapture
+	supportHostapd
+	supportDHCP
+	supportFrameSender
+	supportIfaceManipuation
+	supportVethBridgeBinding
+	supportBridge
+	supportVeth
+}
+
+type supportLogs interface {
+	CollectLogs(ctx context.Context) error
+}
+
+type supportCapture interface {
+	StartCapture(ctx context.Context, name string, ch int, freqOps []iw.SetFreqOption, pcapOps ...pcap.Option) (ret *pcap.Capturer, retErr error)
+	StartRawCapturer(ctx context.Context, name, iface string, ops ...pcap.Option) (*pcap.Capturer, error)
+	StopCapture(ctx context.Context, capturer *pcap.Capturer) error
+	StopRawCapturer(ctx context.Context, capturer *pcap.Capturer) error
+	ReserveForStopCapture(ctx context.Context, capturer *pcap.Capturer) (context.Context, context.CancelFunc)
+	ReserveForStopRawCapturer(ctx context.Context, capturer *pcap.Capturer) (context.Context, context.CancelFunc)
+}
+
+type supportHostapd interface {
+	StartHostapd(ctx context.Context, name string, conf *hostapd.Config) (_ *hostapd.Server, retErr error)
+	StopHostapd(ctx context.Context, hs *hostapd.Server) error
+	ReconfigureHostapd(ctx context.Context, hs *hostapd.Server, conf *hostapd.Config) (_ *hostapd.Server, retErr error)
+}
+
+type supportDHCP interface {
+	StartDHCP(ctx context.Context, name, iface string, ipStart, ipEnd, serverIP, broadcastIP net.IP, mask net.IPMask) (_ *dhcp.Server, retErr error)
+	StopDHCP(ctx context.Context, ds *dhcp.Server) error
+}
+
+type supportFrameSender interface {
+	CloseFrameSender(ctx context.Context, s *framesender.Sender) error
+	NewFrameSender(ctx context.Context, iface string) (ret *framesender.Sender, retErr error)
+	ReserveForCloseFrameSender(ctx context.Context) (context.Context, context.CancelFunc)
+}
+
+type supportIfaceManipuation interface {
+	SetAPIfaceDown(ctx context.Context, iface string) error
+	MAC(ctx context.Context, iface string) (net.HardwareAddr, error)
+}
+
+type supportVethBridgeBinding interface {
+	BindVethToBridge(ctx context.Context, veth, br string) error
+	UnbindVeth(ctx context.Context, veth string) error
+}
+type supportBridge interface {
+	NewBridge(ctx context.Context) (_ string, retErr error)
+	ReleaseBridge(ctx context.Context, br string) error
+}
+
+type supportVeth interface {
+	NewVethPair(ctx context.Context) (_, _ string, retErr error)
+	ReleaseVethPair(ctx context.Context, veth string) error
+}
+
 const (
 	// Autotest may be used on these routers too, and if it failed to clean up, we may be out of space in /tmp.
 	autotestWorkdirGlob = "/tmp/autotest-*"
