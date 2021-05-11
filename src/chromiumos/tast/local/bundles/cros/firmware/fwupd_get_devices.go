db24343867f61a4e14a72f87dd6fdfec4e31160f
Daniel Campello <campello@chromium.org>
2021-05-11 10:13:50 -0600

    firmware.fwupd*: promote out of informational
    These tests are stable now and we want them to be enforced on CQ runs.
    
    BUG=b:185432355
    TEST=none
    
    Cq-Depend: chromium:2903282
    Change-Id: I340625c5536ca0dbd18372457821bf09b8d16d77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2887967
    Auto-Submit: Daniel Campello <campello@chromium.org>
    Commit-Queue: Daniel Campello <campello@chromium.org>
    Tested-by: Daniel Campello <campello@chromium.org>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Reviewed-by: Benson Leung <bleung@google.com>

--- src/chromiumos/tast/local/bundles/cros/firmware/fwupd_get_devices.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_get_devices.go
@@ -20,7 +20,7 @@ func init() {
 			"campello@chromium.org",     // Test Author
 			"chromeos-fwupd@google.com", // CrOS FWUPD
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"fwupd"},
 	})
 }
