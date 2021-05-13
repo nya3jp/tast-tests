91d5cc9c98ac23a0f279aaac64577ffda0b845d7
Shijin Abraham <shijinabraham@google.com>
2021-05-12 20:05:37 -0700

    bluetooth: Add bluetooth ui test
    Add a test which enables/disables Bluetooth via UI button in
    minimized quick settings menu
    
    BUG=b:187993295
    TEST=Ran EnableDisableBluetooth test
    
    Change-Id: I3fa4dc7a2a116f6ff91c8c823a9eb8f5f669bed4

--- src/chromiumos/tast/local/bundles/cros/main.go
+++ src/chromiumos/tast/local/bundles/cros/main.go
@@ -20,6 +20,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/audio"
 	_ "chromiumos/tast/local/bundles/cros/baserpc"
 	_ "chromiumos/tast/local/bundles/cros/biod"
+	_ "chromiumos/tast/local/bundles/cros/bluetooth"
 	_ "chromiumos/tast/local/bundles/cros/camera"
 	_ "chromiumos/tast/local/bundles/cros/crash"
 	_ "chromiumos/tast/local/bundles/cros/crostini"
