e7c836308bb3ab2c4a8cd1fb412c6f84f2bc5ffd
Sherry Hu <shuanghu@google.com>
2021-05-14 14:36:14 -0700

    tast-test: Add arc.AdbOverUsb
    Add a remote tast test for adb-over-usb feature. The test checks if a CrOS job
    (arc(vm)-job) status  on all supported chromebook devices. Test pass if
    arc(vm)-job is up and running. One prerequisite of starting arc(vm)-adbd
    job is udc enabled(dev_enable_udc=1). This test enables udc and
    reboot device.
    
    BUG=b:159856231
    TEST=tast run DUT arc.AdbOverUsb
    TEST=tast run DUT arc.AdbOverUsb.vm
    
    Change-Id: Id286ec17b12b53ffa354b11715d355f42f8970bf

---
+++ src/chromiumos/tast/remote/bundles/cros/arc/adb_over_usb.go
@@ -0,0 +1,94 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/arc"
+	arcpb "chromiumos/tast/services/cros/arc"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ADBOverUSB,
+		Desc:     "Checks that arc(vm)-adbd job is up and running when adb-over-usb feature available",
+		Contacts: []string{"shuanghu@chromium.org", "arc-eng@google.com"},
+		HardwareDeps: hwdep.D(
+			// Available boards info, please refer to doc https://www.chromium.org/chromium-os/chrome-os-systems-supporting-adb-debugging-over-usb
+			hwdep.Model("eve", "atlas", "nocturne", "soraka"),
+		),
+		SoftwareDeps: []string{"reboot", "chrome", "crossystem"},
+		ServiceDeps:  []string{"tast.cros.arc.ADBOverUSBService"},
+		Attr:         []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		Timeout: 10 * time.Minute,
+	})
+}
+
+func ADBOverUSB(ctx context.Context, s *testing.State) {
+	d := s.DUT()
+
+	// Connect to the gRPC server on the DUT
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	service := arc.NewADBOverUSBServiceClient(cl.Conn)
+	enableUDCRequest := arcpb.EnableUDCRequest{
+		Enable: "1",
+	}
+	rsp, err := service.EnableUDC(ctx, &enableUDCRequest)
+	if err != nil {
+		s.Fatal("Failed to enable USB Device Controller on the DUT: ", err)
+	}
+
+	if rsp.Result {
+		s.Log("Rebooting")
+		if err := s.DUT().Reboot(ctx); err != nil {
+			s.Fatal("Failed to reboot DUT: ", err)
+		}
+
+		// Reconnect to the gRPC server after rebooting DUT.
+		cl, err = rpc.Dial(ctx, d, s.RPCHint(), "cros")
+		if err != nil {
+			s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+		}
+		defer cl.Close(ctx)
+
+		service = arc.NewADBOverUSBServiceClient(cl.Conn)
+	}
+
+	s.Log("Checking arc(vm)-adbd job on DUT")
+	if _, err = service.CheckADBDJobStatus(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Failed to check arc(vm)-adbd job on DUT: ", err)
+	}
+
+	if rsp.Result {
+		disableUDCRequest := arcpb.EnableUDCRequest{
+			Enable: "0",
+		}
+		if _, err := service.EnableUDC(ctx, &disableUDCRequest); err != nil {
+			s.Fatal("Failed to disable USB Device Controller on the DUT: ", err)
+		}
+		s.Log("Rebooting")
+		if err := s.DUT().Reboot(ctx); err != nil {
+			s.Fatal("Failed to reboot DUT: ", err)
+		}
+	}
+}
