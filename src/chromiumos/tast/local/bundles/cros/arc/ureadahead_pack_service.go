c4626c4d9d084e77b6bbe19a05748098c253caa6
Yury Khmel <khmel@chromium.org>
2021-05-14 17:44:38 +0000

    arc: Stabilize ureadahead environment for data collector
    This fixes HOST side ureadahead generation by:
    1) Explicitly clears tracing buffer in order to prevent ureadahead
    accesses previous session data.
    2) Explicitly open system and vendor images for ARCVM to let them appear
    in tracing buffer. Mounting block device may not be traced as file open.
    
    BUG=b:188000184
    BUG=b:188119522
    TEST=tast ${DUT} arc.DataCollector.vm_local using last failed PFQ kohaku
    image. Could repro the problem per environment b:188000184. After
    applying this fix ureadahead host generation is stable, multiple passes
    show no failure. Note, there is some flakiness for guest side ureadahead
    generation. However this is KI and should be resolved by retry (guest
    flakiness rate is relatively low, ~10%).
    
    Change-Id: I6a2999df21e5457f5a7eebfb78e06f1882e0c7d4

--- src/chromiumos/tast/local/bundles/cros/arc/ureadahead_pack_service.go
+++ src/chromiumos/tast/local/bundles/cros/arc/ureadahead_pack_service.go
@@ -179,6 +179,10 @@ func (c *UreadaheadPackService) Generate(ctx context.Context, request *arcpb.Ure
 		return nil, errors.Wrap(err, "failed to reset ureadahead flag")
 	}
 
+	if err := ioutil.WriteFile("/sys/kernel/debug/tracing/trace", []byte(""), 0644); err != nil {
+		return nil, errors.Wrap(err, "failed to reset tracing buffer")
+	}
+
 	logPath := filepath.Join(ureadaheadDataDir, logName)
 	log, err := os.Create(logPath)
 	if err != nil {
@@ -228,6 +232,21 @@ func (c *UreadaheadPackService) Generate(ctx context.Context, request *arcpb.Ure
 			testing.ContextLog(cleanCtx, "Failed to stop ureadahead tracing")
 		}
 	}()
+
+	if vmEnabled {
+		// In ARCVM we trace system and vendor images. They are mount as block devices
+		// and normally they would not appear in tracing open requests.
+		// Open images explicitly here in order to ensure tracing buffer has it.
+		images := []string{"system.raw.img", "vendor.raw.img"}
+		for _, image := range images {
+			imagePath := filepath.Join(arcvmRoot, image)
+			file, err := os.Open(imagePath)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to touch image %q", imagePath)
+			}
+			file.Close()
+		}
+	}
 
 	// Opt in.
 	testing.ContextLog(ctx, "Waiting for ARC opt-in flow to complete")
