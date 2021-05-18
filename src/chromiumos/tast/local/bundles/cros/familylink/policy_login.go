6a64eb94d573fbd6b4aa7f6751d39ca66585322b
Xiqi Ruan <xiqiruan@chromium.org>
2021-05-17 18:20:07 -0700

    Tast: Add policy test fixture for Family Link
    Family Link account use the domain gmail.com.
    Add a new fixture to fakedms and familylink
    to enable policy test for Family Link accounts.
    
    BUG=chromium:1222450
    TEST=Run familylink.PolicyLogin to verify it works.
    Cq-Depend: chromium:1222450
    
    Change-Id: I9107de2cd4cb5ab97486d3020b8b7a98545e6b46

---
+++ src/chromiumos/tast/local/bundles/cros/familylink/policy_login.go
@@ -0,0 +1,51 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package familylink
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/familylink"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PolicyLogin,
+		Desc:         "Checks if Unicorn login with policy setup is working",
+		Contacts:     []string{"xiqiruan@chromium.org", "cros-families-eng+test@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      chrome.GAIALoginTimeout + 5*time.Minute,
+		Fixture:      "familyLinkUnicornPolicyLogin",
+	})
+}
+
+func PolicyLogin(ctx context.Context, s *testing.State) {
+	fdms := s.FixtValue().(*familylink.FixtData).FakeDMS
+	cr := s.FixtValue().(*familylink.FixtData).Chrome
+	tconn := s.FixtValue().(*familylink.FixtData).TestConn
+
+	policies := []policy.Policy{
+		&policy.ForceGoogleSafeSearch{Val: true},
+	}
+
+	pb := fakedms.NewPolicyBlob()
+	pb.AddPolicies(policies)
+
+	if err := policyutil.ServeBlobAndRefresh(ctx, fdms, cr, pb); err != nil {
+		s.Fatal("Failed to serve policies: ", err)
+	}
+
+	s.Log("Verifying policies was delivered to device")
+	if err := policyutil.Verify(ctx, tconn, policies); err != nil {
+		s.Fatal("Failed to verify policies: ", err)
+	}
+}
