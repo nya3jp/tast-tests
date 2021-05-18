8a078a6f136c8dab2872444a5136aecd8b2d9884
Seewai Fu <seewaifu@google.com>
2021-05-17 18:58:27 -0700

    tast: making testing.State globally accessible
    This CL adds a new test to test a new feature which allows test
    libraries to access the state through context.
    
    BUG=b:187795655
    TEST=tast run -build=true hana example.AccessVars
    
    Cq-Depend: chromium:2902118
    Change-Id: I056795278a6f22d8af2045a459180c5202dff955

---
+++ src/chromiumos/tast/local/bundles/cros/example/access_vars.go
@@ -0,0 +1,33 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     AccessVars,
+		Desc:     "Access variables",
+		Contacts: []string{"tast-owners@google.com", "seewaifu@chromium.org"},
+		Attr:     []string{"group:mainline", "informational"},
+		// example.AccessVars.foo is defined in tast-tests/vars/example.AccessVars.yaml
+		VarDeps: []string{"example.AccessVars.foo"},
+	})
+}
+
+func AccessVars(ctx context.Context, s *testing.State) {
+	varName := "example.AccessVars.foo"
+	state, ok := testing.ContextTestState(ctx)
+	if !ok {
+		s.Error(`Got false while trying to get current state from context`)
+	}
+	if s.RequiredVar(varName) != state.RequiredVar(varName) {
+		s.Errorf(`Got %q for var %q, want %q`, state.RequiredVar(varName), varName, s.RequiredVar(varName))
+	}
+}
