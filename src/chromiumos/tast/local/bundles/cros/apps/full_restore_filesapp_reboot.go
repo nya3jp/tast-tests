b73b72a793250ec23a3bce78d7461d66730ca107
Judy Wu <jinrongwu@google.com>
2021-05-18 17:43:57 +1000

    Automate Full Restore
    Automate Full Restore test: restore Files app
    
    BUG=None
    TEST=Ran on hatch
    
    Change-Id: I75a7faeb736f21297340afa4529b17bafd7f1e66

---
+++ src/chromiumos/tast/local/bundles/cros/apps/full_restore_filesapp_reboot.go
@@ -0,0 +1,66 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package apps
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FullRestoreFilesappReboot,
+		Desc: "Test full restore files app",
+		Contacts: []string{
+			"jinrongwu@google.com",
+			"chromeos-apps-foundation-team@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		Vars:         []string{"ui.gaiaPoolDefault"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func FullRestoreFilesappReboot(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx,
+		chrome.GAIALoginPool(s.RequiredVar("ui.gaiaPoolDefault")),
+		chrome.ExtraArgs(`--feature-flags=["full-restore@1"]`))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	// Open the Files app.
+	_, err = filesapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch Files app: ", err)
+	}
+
+	cr, err = chrome.New(ctx,
+		chrome.GAIALogin(cr.Creds()),
+		chrome.KeepState())
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+
+	tconn, err = cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	// // Check Files app is restored.
+	// _, err = filesapp.App(ctx, tconn)
+	// if err != nil {
+	// 	s.Fatal("Failed to find Files app after reboot with full restore: ", err)
+	// }
+}
