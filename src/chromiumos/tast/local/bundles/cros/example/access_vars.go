33d19bc04910f838baf9a2e542b7210597f94558
Seewai Fu <seewaifu@google.com>
2021-05-17 18:58:27 -0700

    tast: Add a new test for accessing runtime variables globally (WIP)
    This CL adds a new test to test a new feature which allows test
    libraries to access runtime variables through context.
    
    BUG=b:187795655
    TEST=tast run -build=true hana example.AccessVars
    
    Cq-Depend: chromium:2902118
    Change-Id: I056795278a6f22d8af2045a459180c5202dff955

---
+++ src/chromiumos/tast/local/bundles/cros/example/access_vars.go
@@ -0,0 +1,40 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+
+	"chromiumos/tast/common/global"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     AccessVars,
+		Desc:     "Access variables",
+		Contacts: []string{"tast-owners@google.com", "seewaifu@chromium.org"},
+		Attr:     []string{"group:mainline", "informational"},
+	})
+}
+
+func AccessVars(ctx context.Context, s *testing.State) {
+	var boolVal bool
+	if ok := testing.ContextVar(ctx, global.ExampleBoolVar, &boolVal); boolVal != true || !ok {
+		s.Errorf("Got global variable value (%v, %v) from ContextVar want (%v, %v)", boolVal, ok, true, true)
+	}
+	var strVal string
+	if ok := testing.ContextVar(ctx, global.ExampleStrVar, &strVal); strVal != "test" || !ok {
+		s.Errorf("Got global variable value (%q, %v) from ContextVar want (%q, %v)", strVal, ok, "test", true)
+	}
+	var structVal global.ExampleStruct
+	expected := global.ExampleStruct{Name: "t1", Value: 8}
+	if ok := testing.ContextVar(ctx, global.ExampleStructVar, &structVal); !ok {
+		s.Fatal("Failed to find global variable ", global.ExampleStructVar)
+	}
+	if structVal.Name != expected.Name || structVal.Value != expected.Value {
+		s.Errorf("Got global variable value (%v) from ContextVar want (%v)", structVal, expected)
+	}
+}
