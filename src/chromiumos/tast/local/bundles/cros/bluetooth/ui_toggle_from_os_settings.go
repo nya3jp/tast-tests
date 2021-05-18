15995989c9d03b083739e32214af9318d78e1f66
Shijin Abraham <shijinabraham@google.com>
2021-05-18 16:23:18 -0700

    bluetooth: Add Bluetooth OS settings UI tests
    Add UI test to enable/disable Bluetooth from OS settings menu.
    Add UI test to enable/disable Bluetooth from Bluetooth settings menu.
    
    BUG=b:187993295
    TEST=Ran bluetooth.UIToggleFromOSSettings, bluetooth.UIToggleFromBTSettings,
         ~/trunk/src/platform/tast/fast_build.sh -T
    
    Change-Id: I939ec3c716a305b41c5571b84f06edf64177a505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2904932
    Reviewed-by: Po-Hsien Wang <pwang@chromium.org>
    Commit-Queue: Shijin Abraham <shijinabraham@google.com>
    Tested-by: Shijin Abraham <shijinabraham@google.com>
    Auto-Submit: Shijin Abraham <shijinabraham@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/bluetooth/ui_toggle_from_os_settings.go
@@ -0,0 +1,84 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bluetooth
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bluetooth"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: UIToggleFromOSSettings,
+		Desc: "Enable and disable Bluetooth from ChromeOS Settings UI",
+		Contacts: []string{
+			"chromeos-bluetooth-champs@google.com", // b/new?component=167317&template=1370210.
+			"chromeos-bluetooth-engprod@google.com",
+			"shijinabraham@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedIn",
+	})
+}
+
+// UIToggleFromOSSettings tests enabling/disabling Bluetooth from the ChromeOS settings UI.
+func UIToggleFromOSSettings(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	// Bluetooth button in the ChromeOS settings.
+	bluetoothToggleButton := nodewith.Name("Bluetooth enable").Role(role.ToggleButton)
+
+	// Power on Bluetooth adapter.
+	if err = bluetooth.Enable(ctx); err != nil {
+		s.Fatal("Failed to power on Bluetooth adapter: ", err)
+	}
+
+	// Launch the ChromeOS settings application.
+	if _, err = ossettings.LaunchAtPage(ctx, tconn, nodewith.Name("Bluetooth").Role(role.Heading)); err != nil {
+		s.Fatal("Failed to bring up Bluetooth os settings page: ", err)
+	}
+	if err = ui.Exists(bluetoothToggleButton)(ctx); err != nil {
+		s.Fatal("Failed to find Bluetooth button is ChromeOS setting UI: ", err)
+	}
+
+	const numIterations = 20
+	for i := 0; i < numIterations; i++ {
+		s.Logf("Iteration %d of %d", i, numIterations)
+
+		// Click on Bluetooth UI button and wait for button state to toggle.
+		if err := ui.LeftClick(bluetoothToggleButton)(ctx); err != nil {
+			s.Fatal("Failed to left click bluetooth toggle button: ", err)
+		}
+
+		// Confirm Bluetooth adapter is disabled.
+		if err = bluetooth.PollForBTDisabled(ctx); err != nil {
+			s.Fatal("Failed to verify Bluetooth status, got enabled, want disabled: ", err)
+		}
+
+		// Click on Bluetooth UI button and wait for button state to toggle.
+		if err := ui.LeftClick(bluetoothToggleButton)(ctx); err != nil {
+			s.Fatal("Failed to left click the bluetooth toggle button: ", err)
+		}
+
+		// Confirm Bluetooth adapter is enabled.
+		if err = bluetooth.PollForBTEnabled(ctx); err != nil {
+			s.Fatal("Failed to verify Bluetooth status, got disabled, want enabled: ", err)
+		}
+	}
+}
