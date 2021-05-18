5e6c2767bd2f0934b62b3587eb3a566522437484
Ben Reich <benreich@chromium.org>
2021-05-18 03:25:44 +0000

    tast-tests: Create Play Billing fixture
    Adds a new helper package specific for playbilling. Create a new fixture
    to isolate play billing tast tests, for now this is simply extending the
    arcBootedWithPlayStore fixture settings and logging in with the supplied
    username and password.
    
    BUG=chromium:1210330
    TEST=./fast_build.sh -T
    
    Change-Id: I043a3421711798b60ffdc47c619037363b91d151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2901980
    Tested-by: Ben Reich <benreich@chromium.org>
    Reviewed-by: David Stevens <stevensd@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>

---
+++ src/chromiumos/tast/local/playbilling/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package playbilling contains helper functions to interact with the Play Billing test PWA.
+package playbilling
