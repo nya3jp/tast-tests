b2caca2ff343dd4b0fcc2255e3ff8f333baf31e7
Ben Reich <benreich@chromium.org>
2021-05-18 11:24:18 +0000

    tast-tests: Add Play Billing helper package
    Implements a NewPWA method that wraps required connections to the Play
    Billing test PWA, the UIAutomator (to interact with the ARC Payments
    overlay) and the underlying server that hosts the PWA. The *TestPWA type
    will expose helper methods to interact with the Play Billing test PWA.
    
    The server must bind to port 80 (and not use httptest) as the
    HostsVerifier in ARC (for Trusted Web Activities) requires no port and
    an IP address, hence 127.0.0.1 is the only valid host to use.
    
    BUG=chromium:1210330
    TEST=./fast_build.sh -T
    
    Change-Id: Ibf3fc1dbce004b510fe772eaae378c68d82fb6c1

---
+++ src/chromiumos/tast/local/playbilling/playbilling.go
@@ -0,0 +1,103 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package playbilling
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+const (
+	appID              = "dlbmfdiobcnhnfocmenonncepnmhpckd"
+	localServerPort    = 80
+	localServerAddress = "http://127.0.0.1/"
+)
+
+// TestPWA holds references to the http.Server and the underlying
+// PWA CDP connection.
+type TestPWA struct {
+	server      *http.Server
+	pbConn      *chrome.Conn
+	uiAutomator *ui.Device
+}
+
+// NewTestPWA sets up a local HTTP server to serve the PWA for which the test
+// Android app points to.
+func NewTestPWA(ctx context.Context, cr *chrome.Chrome, arcDevice *arc.ARC, pwaDir string) (pwa *TestPWA, retErr error) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	uiAutomator, err := arcDevice.NewUIDevice(ctx)
+	if err != nil {
+		if err := arcDevice.Close(cleanupCtx); err != nil {
+			testing.ContextLog(cleanupCtx, "Failed to close ARC device: ", err)
+		}
+		return nil, errors.Wrap(err, "failed to initialize UI automator")
+	}
+
+	if err := arcDevice.WaitIntentHelper(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to wait for intent helper")
+	}
+
+	fs := http.FileServer(http.Dir(pwaDir))
+	server := &http.Server{Addr: fmt.Sprintf(":%v", localServerPort), Handler: fs}
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			testing.ContextLog(ctx, "Failed to create local server: ", err)
+		}
+	}()
+	defer func() {
+		if retErr != nil {
+			server.Shutdown(cleanupCtx)
+		}
+	}()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed getting Test API connection")
+	}
+	defer tconn.Close()
+
+	if err := apps.Launch(ctx, tconn, appID); err != nil {
+		return nil, errors.Wrapf(err, "failed launching app ID %q", appID)
+	}
+
+	pbConn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(localServerAddress))
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed getting connection for target: %q", localServerAddress)
+	}
+
+	return &TestPWA{
+		server:      server,
+		pbConn:      pbConn,
+		uiAutomator: uiAutomator,
+	}, nil
+}
+
+// Close performs cleanup actions for the PWA.
+func (p *TestPWA) Close(ctx context.Context) {
+	if err := p.shutdownServer(ctx); err != nil {
+		testing.ContextLog(ctx, "Failed to shutdown the PWA server: ", err)
+	}
+
+	if err := p.uiAutomator.Close(ctx); err != nil {
+		testing.ContextLog(ctx, "Failed to close the UIAutomator: ", err)
+	}
+}
+
+func (p *TestPWA) shutdownServer(ctx context.Context) error {
+	return p.server.Shutdown(ctx)
+}
