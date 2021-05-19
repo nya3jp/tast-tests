23051fb8a24633f969508e6fd113a1c891cec64a
Matt Stark <msta@google.com>
2021-05-19 15:17:51 +1000

    Add several features to screen diffing in tast
    * Allow off-by-one pixels in each channel
    * Add the subpixel antialiasing method as a parameter, since it was
      significantly affecting screenshots.
    * Add border pixels that aren't compared (for when there are curved
      corners)
    * Add the ability to remove specific elements from the image which are
      dynamic (eg. timestamps)
    * Remove multi-config diffing until we actually have pseudolocales.
    * Added some debugging features (eg. writing the whole screen as a
      screenshot, writing the UI tree with it)
    * Create a far more fully-featured test which tests various different
      things (text, images, dynamically sized elements, etc.)
    
    BUG=chromium:1137989
    TEST=Ran the test locally, produced expected images.
    
    Change-Id: Ia8f501c67c8d27f5bd6081924769f4205d31d438

--- src/chromiumos/tast/local/screenshot/differ.go
+++ src/chromiumos/tast/local/screenshot/differ.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
 	"io/ioutil"
@@ -25,6 +26,7 @@ import (
 	"chromiumos/tast/local/chrome/display"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/coords"
 	"chromiumos/tast/lsbrelease"
 	"chromiumos/tast/testing"
@@ -40,7 +42,15 @@ const goldInstance = "cros-tast-dev"
 
 const goldctlWorkDir = "/tmp/goldctl"
 const keysFile = "keys.json"
-const screenshotFile = "screenshot.png"
+const screenshotFile = "cropped.png"
+
+const fontConfigDir = "/etc/fonts/conf.d"
+
+var subPixelAAFiles = map[string]string{
+	"10-no-sub-pixel.conf":   "none",
+	"10-sub-pixel-bgr.conf":  "BGR",
+	"10-sub-pixel-vbgr.conf": "VGBR",
+	"10-sub-pixel-vrgb.conf": "VRGB"}
 
 type testMode string
 
@@ -61,10 +71,25 @@ type screenshotState interface {
 type DiffTestOptions struct {
 	// The time to spend looking for a node
 	Timeout time.Duration
+
+	// The minimum difference required to treat two pixels as different.
+	// Specifically, this is dr + dg + db (the sum of the difference in	each channel).
+	PixelDeltaThreshold int
+
+	// Pixels within this distance to a border (e.g. the top, the bottom, the side)
+	// will not be considered when determining difference.
+	IgnoredBorderThickness int
+
+	// Elements that will be removed from the screenshot. For example, if you have
+	// some dynamic content interlaced with static content (eg. file modification
+	// times in the files app).
+	RemoveElements []*nodewith.Finder
 }
 
 // Differ is a type for running screendiffs.
 type Differ interface {
+	Chrome() *chrome.Chrome
+	Tconn() *chrome.TestConn
 	Diff(string, *nodewith.Finder) uiauto.Action
 	DiffWithOptions(string, *nodewith.Finder, DiffTestOptions) uiauto.Action
 	GetFailedDiffs() error
@@ -87,17 +112,17 @@ type differ struct {
 }
 
 // NewDiffer creates a differ for a new instance of chrome with default configuration.
-func NewDiffer(ctx context.Context, state screenshotState) (Differ, *chrome.Chrome, error) {
+func NewDiffer(ctx context.Context, state screenshotState) (Differ, error) {
 	return NewDifferFromConfig(ctx, state, Config{})
 }
 
 // NewDifferFromConfig creates a differ for a new instance of chrome with configuration specified in cfg.
-func NewDifferFromConfig(ctx context.Context, state screenshotState, cfg Config) (Differ, *chrome.Chrome, error) {
+func NewDifferFromConfig(ctx context.Context, state screenshotState, cfg Config) (Differ, error) {
 	var d = &differ{ctx: ctx, state: state, config: cfg}
 	if err := d.initialize(); err != nil {
-		return nil, nil, errors.Wrap(err, "failed to initialize screen differ")
+		return nil, errors.Wrap(err, "failed to initialize screen differ")
 	}
-	return d, d.chrome, nil
+	return d, nil
 }
 
 // NewDifferFromChrome creates a differ for an existing chrome instance.
@@ -137,20 +162,28 @@ func (d *differ) initialize() error {
 	if err != nil {
 		return err
 	}
+
 	d.uiScale = uiScale
 	params := map[string]string{
-		"displayZoomFactor": fmt.Sprintf("%.2f", info.DisplayZoomFactor),
-		"deviceScaleFactor": fmt.Sprintf("%.2f", displayMode.DeviceScaleFactor),
-		"scale":             fmt.Sprintf("%.2f", uiScale),
-		"resolution":        fmt.Sprintf("%dx%d", displayMode.WidthInNativePixels, displayMode.HeightInNativePixels),
-		"board":             release[lsbrelease.Board],
+		"display_zoom_factor":      fmt.Sprintf("%.2f", info.DisplayZoomFactor),
+		"device_scale_factor":      fmt.Sprintf("%.2f", displayMode.DeviceScaleFactor),
+		"scale":                    fmt.Sprintf("%.2f", uiScale),
+		"resolution":               fmt.Sprintf("%dx%d", displayMode.WidthInNativePixels, displayMode.HeightInNativePixels),
+		"board":                    release[lsbrelease.Board],
+		"sub_pixel_antialiasing":   currentSubPixelAntialiasingMethod(),
+		"image_matching_algorithm": "fuzzy",
+		// Fuzzy matcher is a bit weird. Instead of "no more than <max different pixels> with difference of more than <delta>",
+		// it means "no more than <max different pixels> differing, and no individual pixel has more than <delta> difference."
+		// If we want to accept an image with all pixels off by one, this needs to be at least the number of pixels in the image.
+		"fuzzy_max_different_pixels": "999999999",
 	}
 
 	dir, ok := testing.ContextOutDir(d.ctx)
 	if !ok {
 		return errors.New("couldn't get output dir")
 	}
-	d.dir = filepath.Join(dir, "screenshots")
+	// Different configs may have different sets of keys.
+	d.dir = filepath.Join(dir, "screenshots"+d.config.Suffix())
 
 	jsonString, err := json.Marshal(params)
 	if err != nil {
@@ -262,22 +295,18 @@ func (d *differ) Diff(name string, finder *nodewith.Finder) uiauto.Action {
 // Collect all your diff results at the end with GetFailedDiffs() or DieOnFailedDiffs()
 func (d *differ) DiffWithOptions(name string, finder *nodewith.Finder, options DiffTestOptions) uiauto.Action {
 	return func(_ context.Context) error {
-		// Since elements of config are parameters that affect rendering, different configs should always have distinct diffs.
-		if configString := d.config.ToString(); len(configString) > 0 {
-			name += "." + configString
-		}
-		fullName := d.state.TestName() + "." + name
-		err := d.capture(name, finder, &options)
+		fullName := d.state.TestName() + "." + name + d.config.Suffix()
+		extraArgs, err := d.capture(name, finder, &options)
 		if err != nil {
 			return errors.Wrap(err, "failed to take screenshot")
 		}
-		if err := d.runGoldCommand("imgtest", append([]string{"add",
+		if err := d.runGoldCommand("imgtest", append(append([]string{"add",
 			"--instance", goldInstance,
 			"--keys-file", filepath.Join(d.dir, keysFile),
 			"--test-name", fullName,
 			"--png-file", filepath.Join(d.dir, name, screenshotFile),
-		}, d.goldArgs...)...); err != nil {
-			d.failedTests = append(d.failedTests, name)
+		}, d.goldArgs...), extraArgs...)...); err != nil {
+			d.failedTests = append(d.failedTests, name+d.config.Suffix())
 			// In case you don't have access to a filter by commit ID / release, output the logs directly.
 			d.state.Logf("Failed test %s: %v", name, err)
 		}
@@ -308,9 +337,19 @@ func (d *differ) DieOnFailedDiffs() {
 	}
 }
 
+// Chrome returns the chrome instance used in the screenshot test.
+func (d *differ) Chrome() *chrome.Chrome {
+	return d.chrome
+}
+
+// Tconn returns the tconn associated with this chrome.
+func (d *differ) Tconn() *chrome.TestConn {
+	return d.tconn
+}
+
 // DiffPerConfig takes a function that performs a set of screenshot diff tests, and a set of configurations to run it on,
 // and runs that screenshot test on each configuration.
-func DiffPerConfig(ctx context.Context, state screenshotState, configs []Config, fn func(Differ, *chrome.Chrome)) error {
+func DiffPerConfig(ctx context.Context, state screenshotState, configs []Config, fn func(Differ)) error {
 	var d = &differ{ctx: ctx, state: state}
 	for _, config := range configs {
 		d.config = config
@@ -319,47 +358,92 @@ func DiffPerConfig(ctx context.Context, state screenshotState, configs []Config,
 		if err := d.initialize(); err != nil {
 			return err
 		}
-		fn(d, d.chrome)
+		fn(d)
 	}
 	return d.GetFailedDiffs()
 }
 
-func (d *differ) capture(screenshotName string, finder *nodewith.Finder, options *DiffTestOptions) error {
+func (d *differ) capture(screenshotName string, finder *nodewith.Finder, options *DiffTestOptions) ([]string, error) {
+	var testArgs []string
 	if options.Timeout == 0 {
 		options.Timeout = time.Second * 2
 	}
-	location, err := uiauto.New(d.tconn).WithTimeout(options.Timeout).Location(d.ctx, finder)
+	if options.PixelDeltaThreshold < 3 {
+		// Allow off-by-one in each channel.
+		options.PixelDeltaThreshold = 3
+	}
+
+	ui := uiauto.New(d.tconn).WithTimeout(options.Timeout)
+	location, err := ui.Location(d.ctx, finder)
 	if err != nil {
-		return errors.Wrap(err, "failed to find node to take screenshot of")
+		return testArgs, errors.Wrap(err, "failed to find node to take screenshot of")
+	}
+
+	info, err := ui.Info(d.ctx, finder)
+	if err != nil {
+		return testArgs, errors.Wrap(err, "unable to get info for node")
+	}
+	if options.IgnoredBorderThickness == 0 && info.Role == role.Window {
+		// A window's corners are rounded, and unlike other elements, the background is inconsistent (since it's the wallpaper).
+		options.IgnoredBorderThickness = 1
 	}
 
 	dir := filepath.Join(d.dir, screenshotName)
 	if _, err := os.Stat(dir); err == nil {
-		return errors.Errorf("screenshot has already been taken for %s", screenshotName)
+		return testArgs, errors.Errorf("screenshot has already been taken for %s", screenshotName)
 	}
 	if err := os.Mkdir(dir, 0755); err != nil {
-		return err
+		return testArgs, err
+	}
+
+	if d.config.OutputUITrees {
+		uiauto.LogRootDebugInfo(d.ctx, d.tconn, filepath.Join(dir, "ui_tree.txt"))
 	}
 
 	img, err := CaptureChromeImage(d.ctx, d.chrome)
 	if err != nil {
-		return errors.Wrap(err, "failed to capture the chrome image")
+		return testArgs, errors.Wrap(err, "failed to capture the chrome image")
 	}
+
+	// This screenshot isn't used anywhere, but is useful for context to devs.
+	f, err := os.Create(filepath.Join(dir, "screenshot.png"))
+	if err != nil {
+		return testArgs, err
+	}
+	png.Encode(f, img)
 
 	boundsPx := coords.ConvertBoundsFromDPToPX(*location, d.uiScale)
 
 	// The screenshot returned is of the whole screen. Crop it to only contain the element requested by the user.
 	srcOffset := image.Point{X: boundsPx.Left, Y: boundsPx.Top}
 	dstSize := image.Rect(0, 0, boundsPx.Width, boundsPx.Height)
+	testArgs = append(testArgs,
+		"--add-test-optional-key", fmt.Sprintf("cropped_resolution:%dx%d", boundsPx.Height, boundsPx.Width),
+		"--add-test-optional-key", fmt.Sprintf("top_left_pixel:left_%d_top_%d", boundsPx.Left, boundsPx.Top),
+		"--add-test-optional-key", fmt.Sprintf("fuzzy_pixel_delta_threshold:%d", options.PixelDeltaThreshold),
+		"--add-test-optional-key", fmt.Sprintf("fuzzy_ignored_border_thickness:%d", options.IgnoredBorderThickness))
 	cropped := image.NewRGBA(dstSize)
 	draw.Draw(cropped, dstSize, img, srcOffset, draw.Src)
 
-	f, err := os.Create(filepath.Join(dir, screenshotFile))
+	for _, subelement := range options.RemoveElements {
+		location, err := ui.Location(d.ctx, subelement.Ancestor(finder))
+		if err != nil {
+			return testArgs, errors.Wrap(err, "unable to find element to remove from screenshot")
+		}
+		removedRect := coords.ConvertBoundsFromDPToPX(*location, d.uiScale)
+		removedRect.Left -= boundsPx.Left
+		removedRect.Top -= boundsPx.Top
+		draw.Draw(cropped,
+			image.Rect(removedRect.Left, removedRect.Top, removedRect.Left+removedRect.Width, removedRect.Top+removedRect.Height),
+			&image.Uniform{color.Transparent}, image.ZP, draw.Src)
+	}
+
+	f, err = os.Create(filepath.Join(dir, screenshotFile))
 	if err != nil {
-		return err
+		return testArgs, err
 	}
 	png.Encode(f, cropped)
-	return nil
+	return testArgs, nil
 }
 
 func (d *differ) authenticateGold() error {
@@ -387,4 +471,13 @@ func (d *differ) runGoldCommand(subcommand string, args ...string) error {
 		err = errors.Errorf("while running \"goldctl %s\"\n%s", strings.Join(args, " "), out) // NOLINT
 	}
 	return err
+}
+
+func currentSubPixelAntialiasingMethod() string {
+	for fname, method := range subPixelAAFiles {
+		if _, err := os.Stat(filepath.Join(fontConfigDir, fname)); !os.IsNotExist(err) {
+			return method
+		}
+	}
+	return "unknown"
 }
