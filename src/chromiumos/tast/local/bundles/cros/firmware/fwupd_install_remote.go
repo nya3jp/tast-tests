d8fd120729d93442aac037d0a7ecc3a4cb8e6828
Daniel Campello <campello@chromium.org>
2021-05-20 09:25:11 -0600

    firmware: Add firmware.FwupdInstallRemote
    BUG=b:188440695
    TEST=tast run dut firmware.FwupdInstallRemote
    
    Cq-Depend: chromium:2928967
    Change-Id: I010477cd60a6b130531253e79f08636dacd27422

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_install_remote.go
@@ -0,0 +1,35 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FwupdInstallRemote,
+		Desc: "Checks that fwupd can install using a remote repository",
+		Contacts: []string{
+			"campello@chromium.org",     // Test Author
+			"chromeos-fwupd@google.com", // CrOS FWUPD
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"fwupd"},
+	})
+}
+
+// FwupdInstallRemote runs the fwupdtool utility and verifies that it
+// can update a device in the system using a remote repository.
+func FwupdInstallRemote(ctx context.Context, s *testing.State) {
+	// b585990a-003e-5270-89d5-3705a17f9a43 is the GUID for a fake device.
+	cmd := testexec.CommandContext(ctx, "/usr/bin/fwupdtool", "update", "-v", "b585990a-003e-5270-89d5-3705a17f9a43")
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("%q failed: %v", cmd.Args, err)
+	}
+}
