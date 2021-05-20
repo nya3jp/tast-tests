76fb209212323a8c603546ad133ccee85884f6a8
Nicholas Verne <nverne@chromium.org>
2021-05-20 14:53:34 +1000

    crostini: automate resize tests under host space constraints
    Steps outlined in the bug. The tests fill host disk then check
    the resize operations are still viable.
    
    BUG=chromium:1211205
    TEST=tast run IP crostini.ResizeSpaceConstrained.*
    
    Change-Id: I092e7b971e6eb07bab70eab2226b6bbbbc1f2d63

--- src/chromiumos/tast/local/disk/fill.go
+++ src/chromiumos/tast/local/disk/fill.go
@@ -2,14 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package cleanup
+package disk
 
 import (
 	"io/ioutil"
 	"syscall"
 
 	"chromiumos/tast/errors"
-	"chromiumos/tast/local/disk"
 )
 
 // Fill creates a temporary file in a directory that fills the disk by
@@ -32,7 +31,7 @@ func Fill(path string, tofill uint64) (string, error) {
 // FillUntil reates a temporary file in a directory that fills the disk until
 // less than remaining bytes are available.
 func FillUntil(path string, remaining uint64) (string, error) {
-	freeSpace, err := disk.FreeSpace(path)
+	freeSpace, err := FreeSpace(path)
 	if err != nil {
 		return "", errors.Wrapf(err, "failed to read free space in %s", path)
 	}
