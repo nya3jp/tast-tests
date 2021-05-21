dfb7bb512318156a10d91a5d1562623d73a2e5de
Pi-Hsun Shih <pihsun@chromium.org>
2021-05-21 14:43:38 +0800

    camera: Add CCAUILauncherGuest to test opening camera app in guest mode
    BUG=b:175760318
    TEST=tast run [DUT] camera.CCAUILauncherGuest
    
    Change-Id: Ia8c48ce7ca79a8f15b0dcd538fcca21733d62e85

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_launcher_guest.go
@@ -0,0 +1,50 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUILauncherGuest,
+		Desc:         "Checks camera app can be launched via launcher in guest mode",
+		Contacts:     []string{"chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational", "group:camera-libcamera"},
+		SoftwareDeps: []string{"camera_app", "chrome", caps.BuiltinOrVividCamera},
+		Data:         []string{"cca_ui.js"},
+	})
+}
+
+func CCAUILauncherGuest(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.GuestLogin())
+	if err != nil {
+		s.Fatal("Login failed: ", err)
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+	if err := launcher.SearchAndWaitForAppOpen(tconn, kb, apps.Camera)(ctx); err != nil {
+		s.Fatal("Failed to launch camera app: ", err)
+	}
+}
