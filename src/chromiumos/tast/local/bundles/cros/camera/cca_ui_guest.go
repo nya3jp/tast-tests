7cc7b10530a12621d2cdb60552e7dd9b4f9911e8
Pi-Hsun Shih <pihsun@chromium.org>
2021-05-21 14:43:38 +0800

    camera: Add CCAUIGuest to test opening camera app in guest mode
    BUG=b:175760318
    TEST=tast run [DUT] camera.CCAUIGuest
    
    Change-Id: Ia8c48ce7ca79a8f15b0dcd538fcca21733d62e85

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_guest.go
@@ -0,0 +1,48 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/local/camera/cca"
+	"chromiumos/tast/local/camera/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUIGuest,
+		Desc:         "Checks camera app can be launched via launcher in guest mode",
+		Contacts:     []string{"chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational", "group:camera-libcamera"},
+		SoftwareDeps: []string{"camera_app", "chrome", caps.BuiltinOrVividCamera},
+		Data:         []string{"cca_ui.js"},
+	})
+}
+
+func CCAUIGuest(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.GuestLogin())
+	if err != nil {
+		s.Fatal("Login failed: ", err)
+	}
+
+	tb, err := testutil.NewTestBridge(ctx, cr, testutil.UseRealCamera)
+	if err != nil {
+		s.Fatal("Failed to construct test bridge: ", err)
+	}
+	defer tb.TearDown(ctx)
+
+	_, err = cca.New(ctx, cr, []string{s.DataPath("cca_ui.js")}, s.OutDir(), tb)
+	if err != nil {
+		s.Fatal("Failed to open CCA: ", err)
+	}
+
+	// TODO(pihsun): Test take a photo. Currently app.TakeSinglePhoto fails
+	// because it can't find the result photo, which is located in the guest
+	// ephermeral home directory.
+}
