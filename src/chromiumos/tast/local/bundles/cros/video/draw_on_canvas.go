ee056960bb32424fc60ce7cd9cda4f88a67179aa
Andres Calderon Jaramillo <andrescj@chromium.org>
2021-05-22 02:18:36 +0000

    video.DrawOnCanvas: Pass a reference image.
    This CL allows video.DrawOnCanvas to use a reference PNG file which
    contains the expected result of drawing the video onto a canvas. For
    now, we only use the reference file to set the size of the canvas equal
    to the size of the reference image. The reason is that we'd rather draw
    the video on the canvas without any scaling in order to prevent
    filtering artifacts from interfering with our color checks.
    
    We'll use the reference image for color checks in a follow-up CL.
    
    BUG=b:159158467
    TEST=video.DrawOnCanvas.h264_360p_hw on eldrid
    
    Change-Id: Ia47966f4c260077a86d7cd7c3fbd3f43f0d0a910

--- src/chromiumos/tast/local/bundles/cros/video/draw_on_canvas.go
+++ src/chromiumos/tast/local/bundles/cros/video/draw_on_canvas.go
@@ -6,14 +6,21 @@ package video
 
 import (
 	"context"
+	"image"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"path"
 
 	"chromiumos/tast/common/media/caps"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
 )
+
+type drawOnCanvasParams struct {
+	fileName    string
+	refFileName string
+}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -25,39 +32,54 @@ func init() {
 		},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
-			Name:              "h264_360p_hw",
-			Val:               "still-colors-360p.h264.mp4",
+			Name: "h264_360p_hw",
+			Val: drawOnCanvasParams{
+				fileName:    "still-colors-360p.h264.mp4",
+				refFileName: "still-colors-360p.ref.png",
+			},
 			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
-			ExtraData:         []string{"video-on-canvas.html", "still-colors-360p.h264.mp4"},
+			ExtraData:         []string{"video-on-canvas.html", "still-colors-360p.h264.mp4", "still-colors-360p.ref.png"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			Fixture:           "chromeVideo",
 		}, {
 			// TODO(andrescj): move to graphics_nightly after the test is stabilized.
-			Name:              "h264_360p_exotic_crop_hw",
-			Val:               "still-colors-720x480-cropped-to-640x360.h264.mp4",
+			Name: "h264_360p_exotic_crop_hw",
+			Val: drawOnCanvasParams{
+				fileName:    "still-colors-720x480-cropped-to-640x360.h264.mp4",
+				refFileName: "still-colors-360p.ref.png",
+			},
 			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
-			ExtraData:         []string{"video-on-canvas.html", "still-colors-720x480-cropped-to-640x360.h264.mp4"},
+			ExtraData:         []string{"video-on-canvas.html", "still-colors-720x480-cropped-to-640x360.h264.mp4", "still-colors-360p.ref.png"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			Fixture:           "chromeVideo",
 		}, {
-			Name:              "h264_480p_hw",
-			Val:               "still-colors-480p.h264.mp4",
+			Name: "h264_480p_hw",
+			Val: drawOnCanvasParams{
+				fileName:    "still-colors-480p.h264.mp4",
+				refFileName: "still-colors-480p.ref.png",
+			},
 			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
-			ExtraData:         []string{"video-on-canvas.html", "still-colors-480p.h264.mp4"},
+			ExtraData:         []string{"video-on-canvas.html", "still-colors-480p.h264.mp4", "still-colors-480p.ref.png"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			Fixture:           "chromeVideo",
 		}, {
-			Name:              "h264_720p_hw",
-			Val:               "still-colors-720p.h264.mp4",
+			Name: "h264_720p_hw",
+			Val: drawOnCanvasParams{
+				fileName:    "still-colors-720p.h264.mp4",
+				refFileName: "still-colors-720p.ref.png",
+			},
 			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
-			ExtraData:         []string{"video-on-canvas.html", "still-colors-720p.h264.mp4"},
+			ExtraData:         []string{"video-on-canvas.html", "still-colors-720p.h264.mp4", "still-colors-720p.ref.png"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			Fixture:           "chromeVideo",
 		}, {
-			Name:              "h264_1080p_hw",
-			Val:               "still-colors-1080p.h264.mp4",
+			Name: "h264_1080p_hw",
+			Val: drawOnCanvasParams{
+				fileName:    "still-colors-1080p.h264.mp4",
+				refFileName: "still-colors-1080p.ref.png",
+			},
 			ExtraAttr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
-			ExtraData:         []string{"video-on-canvas.html", "still-colors-1080p.h264.mp4"},
+			ExtraData:         []string{"video-on-canvas.html", "still-colors-1080p.h264.mp4", "still-colors-1080p.ref.png"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			Fixture:           "chromeVideo",
 		}},
@@ -76,7 +98,27 @@ func DrawOnCanvas(ctx context.Context, s *testing.State) {
 		s.Fatalf("Failed to open %v: %v", url, err)
 	}
 	defer conn.Close()
-	if err := conn.Call(ctx, nil, "playAndDrawOnCanvas", s.Param().(string)); err != nil {
+
+	// Open the reference file to set the canvas size equal to the expected size of the rendered video. This
+	// is done in order to prevent scaling filtering artifacts from interfering with our color checks later.
+	refPath := s.DataPath(s.Param().(drawOnCanvasParams).refFileName)
+	f, err := os.Open(refPath)
+	if err != nil {
+		s.Fatalf("Failed to open %v: %v", refPath, err)
+	}
+	defer f.Close()
+	refImg, _, err := image.Decode(f)
+	if err != nil {
+		s.Fatalf("Could not decode %v: %v", refPath, err)
+	}
+	videoW := refImg.Bounds().Dx()
+	videoH := refImg.Bounds().Dy()
+	if err := conn.Call(ctx, nil, "initializeCanvas", videoW, videoH); err != nil {
+		s.Fatal("initializeCanvas() failed: ", err)
+	}
+
+	// Now we can play the video and draw it on the canvas.
+	if err := conn.Call(ctx, nil, "playAndDrawOnCanvas", s.Param().(drawOnCanvasParams).fileName); err != nil {
 		s.Fatal("playAndDrawOnCanvas() failed: ", err)
 	}
 }
