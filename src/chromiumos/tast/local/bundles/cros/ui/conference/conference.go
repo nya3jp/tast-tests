ed16a853c81f614b616ce99523c987d3c2a1e271
Jane Yang <jane.yang@cienet.com>
2021-05-26 12:26:21 +0800

    cuj: Change google meet participant number check logic
    The number of participants check logic is changed to:
    - num participants (for small size meeting) == 4 or == 5
    - num participants (for large size meeting) == 15 or == 16
    
    BUG=b:189224674
    TEST=./fast_build.sh -T
    
    Change-Id: Icf1483c21bc2ab24a7d6365327fa6ffb2646640c

--- src/chromiumos/tast/local/bundles/cros/ui/conference/conference.go
+++ src/chromiumos/tast/local/bundles/cros/ui/conference/conference.go
@@ -13,8 +13,8 @@ const (
 	TwoRoomSize = 2
 	// SmallRoomSize creates a conference room with 5 participants.
 	SmallRoomSize = 5
-	// LargeRoomSize creates a conference room with 17 participants.
-	LargeRoomSize = 17
+	// LargeRoomSize creates a conference room with 16 participants.
+	LargeRoomSize = 16
 	// ClassRoomSize creates a conference room with 38 participants.
 	ClassRoomSize = 38
 )
