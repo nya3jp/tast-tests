46ebb789cff235553fba340b79c8dc6906d2776f
Andreea Costinas <acostinas@google.com>
2021-05-27 14:12:58 +0200

    tast-test: Run SystemProxySettings as a local test
    This CL moves the SystemProxySettings test from /remote to /local and
    adds a delay after applying the policies to allow the system-proxy
    daemon to start the worker processes.
    
    BUG=b:188626940, b:180302242
    TEST=multiple consecutive successful runs of SystemProxySettings*
    
    Change-Id: I663973044ec80522f3431370d4f90dcfb7b0b5f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2920575
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: Jun Sheng <shengjun@chromium.org>
    Tested-by: Andreea-Elena Costinas <acostinas@google.com>
    Commit-Queue: Andreea-Elena Costinas <acostinas@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/policy/system_proxy_settings.go
@@ -0,0 +1,106 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"github.com/shirou/gopsutil/process"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SystemProxySettings,
+		Desc: "Test setting the SystemProxySettings policy by checking if the System-proxy daemon and worker processes are running",
+		Contacts: []string{
+			"acostinas@google.com",
+			"hugobenichi@chromium.org",
+			"omorsi@chromium.org",
+			"pmarko@chromium.org",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      "chromeEnrolledLoggedIn",
+		Params: []testing.Param{{
+			Name: "enabled",
+			Val:  true,
+		}, {
+			Name: "disabled",
+			Val:  false,
+		}},
+	})
+}
+
+func SystemProxySettings(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+	fdms := s.FixtValue().(*fixtures.FixtData).FakeDMS
+
+	// Perform cleanup.
+	if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+		s.Fatal("Failed to clean up: ", err)
+	}
+
+	// Set the policy value.
+	systemProxyPolicy := &policy.SystemProxySettings{
+		Val: &policy.SystemProxySettingsValue{
+			SystemProxyEnabled:           s.Param().(bool),
+			SystemServicesPassword:       "********",
+			SystemServicesUsername:       "********",
+			PolicyCredentialsAuthSchemes: []string{},
+		},
+	}
+
+	// Update policies.
+	if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{systemProxyPolicy}); err != nil {
+		s.Fatal("Failed to update policies: ", err)
+	}
+
+	// Wait for 5 seconds to allow system-proxy to start the worker processe which authenticate OS level traffic.
+	testing.Sleep(ctx, 5*time.Second)
+
+	const (
+		mainBinPath   = "system_proxy"
+		workerBinPath = "system_proxy_worker"
+	)
+	if s.Param().(bool) {
+		if isRunning, err := processRunning(mainBinPath); err != nil {
+			s.Errorf("Failed to determine if %s is running: %v", mainBinPath, err)
+		} else if !isRunning {
+			s.Error("Main process is not running")
+		}
+
+		if isRunning, err := processRunning(workerBinPath); err != nil {
+			s.Errorf("Failed to determine if %s is running: %v", workerBinPath, err)
+		} else if !isRunning {
+			s.Error("Worker process is not running")
+		}
+	} else {
+		if isRunning, err := processRunning(mainBinPath); err != nil {
+			s.Errorf("Failed to determine if %s is running: %v", mainBinPath, err)
+		} else if isRunning {
+			s.Error("System-proxy running although disabled by policy")
+		}
+	}
+}
+
+// processRunning checks if a process named procName is running.
+func processRunning(procName string) (bool, error) {
+	ps, err := process.Processes()
+	if err != nil {
+		return false, err
+	}
+	for _, p := range ps {
+		if n, err := p.Name(); err == nil && n == procName {
+			return true, nil
+		}
+	}
+	return false, nil
+}
