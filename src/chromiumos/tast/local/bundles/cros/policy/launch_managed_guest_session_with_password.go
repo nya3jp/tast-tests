263b9f2c502a24436044129e181e298c445d419d
Jit Yao Yap <jityao@google.com>
2021-05-27 15:11:02 +0200

    Add Tast test for launchManagedGuestSession API with password
    BUG=chromium:1213875
    TEST=tast run <ip> policy.LaunchManagedGuestSessionWithPassword
    
    Change-Id: I583f8f3c836f0c8583c1ef7263db9601849aaee4

---
+++ src/chromiumos/tast/local/bundles/cros/policy/launch_managed_guest_session_with_password.go
@@ -0,0 +1,232 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/local/session"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: LaunchManagedGuestSessionWithPassword,
+		Desc: "Test chrome.login.launchManagedGuestSession Extension API",
+		Contacts: []string{
+			"jityao@google.com", // Test author
+			"chromeos-commercial-identity@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "fakeDMSEnrolled",
+	})
+}
+
+func LaunchManagedGuestSessionWithPassword(ctx context.Context, s *testing.State) {
+	fdms := s.FixtValue().(*fakedms.FakeDMS)
+
+	// Start a Chrome instance that will fetch policies from the FakeDMS.
+	cr, err := chrome.New(ctx,
+		chrome.FakeLogin(chrome.Creds{User: fixtures.Username, Pass: fixtures.Password}),
+		chrome.DMSPolicy(fdms.URL),
+		chrome.KeepState(),
+		chrome.ExtraArgs("--disable-policy-key-verification"))
+	if err != nil {
+		s.Fatal("Chrome login failed: ", err)
+	}
+
+	defer func(ctx context.Context) {
+		// Use cr as a reference to close the last started Chrome instance.
+		if err := cr.Close(ctx); err != nil {
+			s.Error("Failed to close Chrome connection: ", err)
+		}
+	}(ctx)
+
+	// Use a shortened context for test operations to reserve time for cleanup.
+	ctx, cancel := ctxutil.Shorten(ctx, 30*time.Second)
+	defer cancel()
+
+	accountID := "foo@bar.com"
+	accountType := policy.AccountTypePublicSession
+
+	// These extensions are unlisted on the Chrome Web Store but can be
+	// downloaded directly using the extension IDs.
+	// The code for the extensions can be found in the Chromium repo at
+	// chrome/test/data/extensions/api_test/login_screen_apis/.
+	// ID for "Login screen APIs test extension".
+	loginScreenExtensionID := "oclffehlkdgibkainkilopaalpdobkan"
+	// ID for "Login screen APIs in-session test extension".
+	inSessionExtensionID := "ofcpkomnogjenhfajfjadjmjppbegnad"
+
+	policies := []policy.Policy{
+		&policy.DeviceLocalAccounts{
+			Val: []policy.DeviceLocalAccountInfo{
+				{
+					AccountID:   &accountID,
+					AccountType: &accountType,
+				},
+			},
+		},
+		&policy.DeviceLoginScreenExtensions{
+			Val: []string{loginScreenExtensionID},
+		},
+	}
+
+	pb := fakedms.NewPolicyBlob()
+	pb.AddPolicies(policies)
+	pb.AddPublicAccountPolicy(accountID, &policy.ExtensionInstallForcelist{
+		Val: []string{inSessionExtensionID},
+	})
+
+	if err := policyutil.ServeBlobAndRefresh(ctx, fdms, cr, pb); err != nil {
+		s.Fatal("Failed to serve policies: ", err)
+	}
+
+	// Close the previous Chrome instance.
+	if err := cr.Close(ctx); err != nil {
+		s.Fatal("Failed to close Chrome connection: ", err)
+	}
+
+	// Restart Chrome, forcing Devtools to be available on the login screen.
+	cr, err = chrome.New(ctx,
+		chrome.NoLogin(),
+		chrome.DMSPolicy(fdms.URL),
+		chrome.KeepState(),
+		chrome.ExtraArgs("--force-devtools-available"),
+		chrome.ExtraArgs("--disable-policy-key-verification"))
+	if err != nil {
+		s.Fatal("Chrome restart failed: ", err)
+	}
+
+	sm, err := session.NewSessionManager(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to session manager: ", err)
+	}
+
+	swStart, err := sm.WatchSessionStateChanged(ctx, "started")
+	if err != nil {
+		s.Fatal("Failed to watch for D-Bus signals: ", err)
+	}
+	defer swStart.Close(ctx)
+
+	loginScreenBGURL := chrome.ExtensionBackgroundPageURL(loginScreenExtensionID)
+	conn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(loginScreenBGURL))
+	if err != nil {
+		s.Fatal("Failed to connect to login screen background page: ", err)
+	}
+	defer conn.Close()
+
+	pw := "password"
+	wrongPw := "wrong password"
+
+	// Launch a MGS with password.
+	if err := conn.EvalPromise(ctx,
+		fmt.Sprintf(`new Promise((resolve, reject) => {
+		chrome.login.launchManagedGuestSession("%s", () => {
+			if (chrome.runtime.lastError) {
+				reject(new Error(chrome.runtime.lastError.message));
+			}
+			resolve();
+		});
+	})`, pw), nil); err != nil {
+		s.Fatal("Failed to launch MGS: ", err)
+	}
+
+	select {
+	case <-swStart.Signals:
+		// Pass
+	case <-ctx.Done():
+		s.Fatal("Timeout before getting SessionStateChanged signal: ", err)
+	}
+
+	inSessionBGURL := chrome.ExtensionBackgroundPageURL(inSessionExtensionID)
+	inSessionConn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(inSessionBGURL))
+	if err != nil {
+		s.Fatal("Failed to connect to in-session background page: ", err)
+	}
+	defer inSessionConn.Close()
+
+	swLocked, err := sm.WatchScreenIsLocked(ctx)
+	if err != nil {
+		s.Fatal("Failed to watch for D-Bus signals: ", err)
+	}
+	defer swLocked.Close(ctx)
+
+	// Lock the session.
+	if err := inSessionConn.EvalPromise(ctx,
+		`new Promise((resolve, reject) => {
+		chrome.login.lockManagedGuestSession(() => {
+			if (chrome.runtime.lastError) {
+				reject(new Error(chrome.runtime.lastError.message));
+			}
+			resolve();
+		});
+	})`, nil); err != nil {
+		s.Fatal("Failed to lock session: ", err)
+	}
+
+	select {
+	case <-swLocked.Signals:
+		// Pass
+	case <-ctx.Done():
+		s.Fatal("Timeout before getting session locked signal: ", err)
+	}
+
+	swUnlocked, err := sm.WatchScreenIsUnlocked(ctx)
+	if err != nil {
+		s.Fatal("Failed to watch for D-Bus signals: ", err)
+	}
+	defer swUnlocked.Close(ctx)
+
+	// Create a new connection since login screen extensions are closed when
+	// the session is active.
+	conn, err = cr.NewConnForTarget(ctx, chrome.MatchTargetURL(loginScreenBGURL))
+	if err != nil {
+		s.Fatal("Failed to connect to login screen background page: ", err)
+	}
+	defer conn.Close()
+
+	// Unlock the session with wrong password.
+	if err := conn.EvalPromise(ctx,
+		fmt.Sprintf(`new Promise((resolve, reject) => {
+		chrome.login.unlockManagedGuestSession("%s", () => {
+			if (chrome.runtime.lastError) {
+				reject(new Error(chrome.runtime.lastError.message));
+			}
+			resolve();
+		});
+	})`, wrongPw), nil); err == nil {
+		s.Fatal("Unlocked session with wrong password: ", err)
+	}
+
+	// Unlock the session with the same password.
+	if err := conn.EvalPromise(ctx,
+		fmt.Sprintf(`new Promise((resolve, reject) => {
+		chrome.login.unlockManagedGuestSession("%s", () => {
+			if (chrome.runtime.lastError) {
+				reject(new Error(chrome.runtime.lastError.message));
+			}
+			resolve();
+		});
+	})`, pw), nil); err != nil {
+		s.Fatal("Failed to unlock session: ", err)
+	}
+
+	select {
+	case <-swUnlocked.Signals:
+		// Pass
+	case <-ctx.Done():
+		s.Fatal("Timeout before getting session unlocked signal: ", err)
+	}
+}
