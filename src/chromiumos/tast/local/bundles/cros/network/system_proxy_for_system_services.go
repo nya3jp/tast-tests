35c1b087468cc8313f0a3f2df191fa6195c9832b
Andreea Costinas <acostinas@google.com>
2021-05-27 14:24:29 +0200

    tast tests: Run SystemProxyForSystemServices as a local test
    This CL moves the SystemProxyForSystemServices test from /remote to
    /local and adds a delay after applying the policies to allow the
    Chrome Browser to process the signal that announces the system-proxy
    worker and forward the address of the worker to the proxy resolution
    service in Chrome.
    
    BUG=b:188626940, b:180302242
    TEST=multiple consecutive successful runs of
        SystemProxyForSystemServices
    
    Change-Id: I5e435809798a748948edaf1f11b29bd5cc0aba0b

--- src/chromiumos/tast/local/bundles/cros/network/system_proxy_for_system_services.go
+++ src/chromiumos/tast/local/bundles/cros/network/system_proxy_for_system_services.go
@@ -6,23 +6,18 @@ package network
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"regexp"
 	"strings"
 	"time"
 
-	"github.com/golang/protobuf/ptypes/empty"
-
 	"chromiumos/tast/common/policy"
-	"chromiumos/tast/common/policy/fakedms"
-	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
-	"chromiumos/tast/remote/policyutil"
-	"chromiumos/tast/rpc"
-	"chromiumos/tast/services/cros/network"
-	ps "chromiumos/tast/services/cros/policy"
-	"chromiumos/tast/ssh"
+	"chromiumos/tast/local/bundles/cros/network/proxy"
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
 	"chromiumos/tast/testing"
 )
 
@@ -34,91 +29,72 @@ func init() {
 			"acostinas@google.com", // Test author
 			"chromeos-commercial-networking@google.com",
 		},
-		Attr:         []string{"group:enrollment"},
-		SoftwareDeps: []string{"reboot", "vm_host", "chrome"},
-		ServiceDeps:  []string{"tast.cros.policy.PolicyService", "tast.cros.network.ProxyService"},
-		Timeout:      12 * time.Minute,
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      "chromeEnrolledLoggedIn",
 	})
 }
 
 func SystemProxyForSystemServices(ctx context.Context, s *testing.State) {
-	defer func(ctx context.Context) {
-		if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
-			s.Fatal("Failed to reset TPM: ", err)
-		}
-	}(ctx)
-
-	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Minute)
-	defer cancel()
-
-	if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
-		s.Fatal("Failed to reset TPM: ", err)
-	}
-
-	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
-	if err != nil {
-		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
-	}
-	defer cl.Close(ctx)
-
-	pc := ps.NewPolicyServiceClient(cl.Conn)
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+	fdms := s.FixtValue().(*fixtures.FixtData).FakeDMS
 
 	const username = "testuser"
 	const password = "testpwd"
 
 	// Start an HTTP proxy instance on the DUT which requires username and password authentication.
-	proxyClient := network.NewProxyServiceClient(cl.Conn)
-	response, err := proxyClient.StartServer(ctx,
-		&network.StartServerRequest{
-			Port:            3129,
-			AuthCredentials: &network.AuthCredentials{Username: username, Password: password},
-		})
+	ps := proxy.NewServer()
+
+	cred := &proxy.AuthCredentials{Username: username, Password: password}
+	err := ps.Start(ctx, 3128, cred, []string{})
 	if err != nil {
 		s.Fatal("Failed to start a local proxy on the DUT: ", err)
 	}
 
-	pb := fakedms.NewPolicyBlob()
 	// Configure the proxy on the DUT via policy to point to the local proxy instance started via the `ProxyService`.
-	pb.AddPolicy(&policy.ProxySettings{
+	proxyPolicy := &policy.ProxySettings{
 		Val: &policy.ProxySettingsValue{
 			ProxyMode:       "fixed_servers",
-			ProxyServer:     fmt.Sprintf("http://%s", response.HostAndPort),
+			ProxyServer:     fmt.Sprintf("http://%s", ps.HostAndPort),
 			ProxyBypassList: "",
-		}})
+		}}
 	// Start system-proxy and configure it with the credentials of the local proxy instance.
-	pb.AddPolicy(&policy.SystemProxySettings{
+	systemProxySettingsPolicy := &policy.SystemProxySettings{
 		Val: &policy.SystemProxySettingsValue{
 			SystemProxyEnabled:           true,
 			SystemServicesUsername:       username,
 			SystemServicesPassword:       password,
 			PolicyCredentialsAuthSchemes: []string{},
-		}})
+		}}
 
-	pJSON, err := json.Marshal(pb)
-	if err != nil {
-		s.Fatal("Failed to serialize policies: ", err)
+	// Perform cleanup.
+	if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+		s.Fatal("Failed to clean up: ", err)
 	}
 
-	if _, err := pc.EnrollUsingChrome(ctx, &ps.EnrollUsingChromeRequest{
-		PolicyJson: pJSON,
-	}); err != nil {
-		s.Fatal("Failed to enroll using chrome: ", err)
+	// Update policies.
+	if err := policyutil.ServeAndRefresh(ctx, fdms, cr, []policy.Policy{proxyPolicy, systemProxySettingsPolicy}); err != nil {
+		s.Fatal("Failed to update policies: ", err)
 	}
-	defer pc.StopChromeAndFakeDMS(ctx, &empty.Empty{})
 
-	if err = runTLSDate(ctx, s.DUT().Conn()); err != nil {
+	if err = waitForSignal(ctx); err != nil {
+		s.Fatal("Failed to observer event: ", err)
+	}
+
+	if err = runTLSDate(ctx); err != nil {
 		s.Fatal("Failed to run tlsdate with system-proxy: ", err)
 	}
+	ps.Stop(ctx)
 }
 
 // runTLSDate runs tlsdate once, in the foreground. Returns an error if tlsdate didn't use system-proxy to connect to the remote host
 // or if the certificate verification failed.
-func runTLSDate(ctx context.Context, conn *ssh.Conn) error {
+func runTLSDate(ctx context.Context) error {
 	// tlsdated is a CrOS daemon that runs the tlsdate binary periodically in the background and does proxy resolution through Chrome.
 	// The `-m <n>` option means tlsdate should run at most once every n seconds in steady state
 	// The `-p` option means dry run.
 	// The `-o` option means exit tlsdated after running once
-	out, err := conn.Command("/usr/bin/tlsdated", "-o", "-p", "-m", "60", "--", "/usr/bin/tlsdate", "-v", "-C", "/usr/share/chromeos-ca-certificates", "-l").CombinedOutput(ctx)
+	out, err := testexec.CommandContext(ctx, "/usr/bin/tlsdated", "-o", "-p", "-m", "60", "--", "/usr/bin/tlsdate", "-v", "-C", "/usr/share/chromeos-ca-certificates", "-l").CombinedOutput()
 
 	//  The exit code 124 indicates that timeout sent a SIGTERM to terminate tlsdate.
 	if err != nil && !strings.Contains(err.Error(), "Process exited with status 124") {
@@ -138,4 +114,19 @@ func runTLSDate(ctx context.Context, conn *ssh.Conn) error {
 	}
 
 	return nil
+}
+
+func waitForSignal(ctx context.Context) error {
+	match := dbusutil.MatchSpec{
+		Type:      "signal",
+		Path:      "/org/chromium/SystemProxy",
+		Interface: "org.chromium.SystemProxy",
+		Member:    "WorkerActive",
+	}
+	signal, err := dbusutil.NewSignalWatcherForSystemBus(ctx, match)
+	defer signal.Close(ctx)
+	// Wait for Chrome to process the signal.
+	testing.Sleep(ctx, 5*time.Second)
+
+	return err
 }
