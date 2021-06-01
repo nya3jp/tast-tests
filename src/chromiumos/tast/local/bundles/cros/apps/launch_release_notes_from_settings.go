a1e2d410be661163c61efb41060c8cd559636359
Jun Sheng <shengjun@google.com>
2021-06-02 09:47:40 +1000

    tast-tests: fix apps.LaunchReleaseNotesFromSettings
    The node info is changing between `What's new`and `What’s New`.
    
    It needs further investigation why it happens.
    
    BUG=b:189876057
    TEST=apps.LaunchReleaseNotesFromSettings*
    
    Change-Id: Iee193f53c436c217ef50b187e8bf1d60513aedf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2928888
    Tested-by: Jun Sheng <shengjun@chromium.org>
    Auto-Submit: Jun Sheng <shengjun@chromium.org>
    Reviewed-by: Judy Wu <jinrongwu@google.com>
    Commit-Queue: Jun Sheng <shengjun@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/apps/launch_release_notes_from_settings.go
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_release_notes_from_settings.go
@@ -6,6 +6,7 @@ package apps
 
 import (
 	"context"
+	"regexp"
 	"time"
 
 	"chromiumos/tast/ctxutil"
@@ -66,7 +67,7 @@ func LaunchReleaseNotesFromSettings(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to launch Settings: ", err)
 	}
 
-	whatsNewTabFinder := nodewith.NameStartingWith("What's new").Role(role.Heading).Ancestor(helpapp.RootFinder)
+	whatsNewTabFinder := nodewith.NameRegex(regexp.MustCompile("What's new|What’s New")).Role(role.Heading).Ancestor(helpapp.RootFinder)
 
 	if err := uiauto.Combine("launch WhatsNew and verify landing page",
 		settings.LaunchWhatsNew(),
