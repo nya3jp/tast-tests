ec45854a3a561256a49f706e4178f11ea41cb212
Ben Reich <benreich@chromium.org>
2021-06-01 05:22:18 +0000

    [play-billing] Add TestApp to interact with Play Store PWA
    All the tests that rely on the playBillingFixture will use a test
    Android application to exercise the play billing functionality. Create a
    wrapper around the TestApp so that all the difficult functionality can
    be abstracted into easier helper functions. Complete all that setup in
    the fixture to ensure small concise tests are written.
    
    BUG=chromium:1210330
    TEST=./fast_build.sh -T
    
    Change-Id: I2a8f41fbbf505f14638323a8437608a71f69d227

---
+++ src/chromiumos/tast/local/playbilling/testapp.go
@@ -0,0 +1,59 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package playbilling
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+)
+
+// TestApp represents the Play Billing test PWA and ARC Payments Overlay.
+type TestApp struct {
+	cr          *chrome.Chrome
+	pbconn      *chrome.Conn
+	tconn       *chrome.TestConn
+	uiAutomator *ui.Device
+}
+
+const (
+	appID              = "dlbmfdiobcnhnfocmenonncepnmhpckd"
+	localServerAddress = "http://127.0.0.1/"
+)
+
+// NewTestApp returns a reference to a new Play Billing Test App.
+func NewTestApp(ctx context.Context, cr *chrome.Chrome, arc *arc.ARC, uiAutomator *ui.Device) (*TestApp, error) {
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed getting Test API connection")
+	}
+
+	return &TestApp{
+		cr:          cr,
+		tconn:       tconn,
+		uiAutomator: uiAutomator,
+		pbconn:      nil,
+	}, nil
+}
+
+// Launch starts a new TestApp window.
+func (ta *TestApp) Launch(ctx context.Context) error {
+	if err := apps.Launch(ctx, ta.tconn, appID); err != nil {
+		return errors.Wrapf(err, "failed launching app ID %q", appID)
+	}
+
+	pbconn, err := ta.cr.NewConnForTarget(ctx, chrome.MatchTargetURL(localServerAddress))
+	if err != nil {
+		return errors.Wrapf(err, "failed getting connection for target: %q", localServerAddress)
+	}
+
+	ta.pbconn = pbconn
+
+	return nil
+}
