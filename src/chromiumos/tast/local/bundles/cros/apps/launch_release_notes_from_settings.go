b7ecff776ce8b99f8a1e21f86153064ba31aa94b
Federico 'Morg' Pareschi <morg@google.com>
2021-06-02 17:19:15 +0900

    tast-tests: Demote LaunchReleaseNotesFromSettings
    The test is still breaking and blocking CQ, marking it informational
    until we can get it back to a stable state
    
    We need to cherry pick this into the R92 release branch because it's breaking betty-release.
    
    BUG=b:189876057
    TEST=None
    
    Change-Id: Ice0ed9ad55ed39366e1a2efc7d6f643ec1553d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2932760
    Reviewed-by: Jun Sheng <shengjun@chromium.org>
    Commit-Queue: Jun Sheng <shengjun@chromium.org>
    Tested-by: Morg <morg@chromium.org>
    Owners-Override: Morg <morg@chromium.org>
    (cherry picked from commit 39875242fab618986ed51ca95268c9f9c76e89e2)

--- src/chromiumos/tast/local/bundles/cros/apps/launch_release_notes_from_settings.go
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_release_notes_from_settings.go
@@ -29,7 +29,7 @@ func init() {
 			"carpenterr@chromium.org", // test author.
 			"showoff-eng@google.com",
 		},
-		Attr:         []string{"group:mainline"},
+		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      "chromeLoggedInForEA",
 		Params: []testing.Param{
@@ -39,7 +39,6 @@ func init() {
 			}, {
 				Name:              "unstable",
 				ExtraHardwareDeps: hwdep.D(pre.AppsUnstableModels),
-				ExtraAttr:         []string{"informational"},
 			},
 		},
 	})
