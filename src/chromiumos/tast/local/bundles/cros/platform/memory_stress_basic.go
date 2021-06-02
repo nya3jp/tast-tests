5c64312085dbff33102d526ab813bf21e1a7e6d9
Kuo-Hsin Yang <vovoy@chromium.org>
2021-06-02 15:59:13 +0800

    tast-tests: Add platform.MemoryStressLacros
    This test opens some synthetic pages to allocate a lot of JavaScript
    objects to create memory pressure on Lacros . The test switches between
    the pages and reload crashed pages. It checks the count of reloading
    crashed pages.
    
    The common parts of platform.MemoryStressLacros and
    platform.MemoryStressBasic are moved to local/memory/stress/.
    
    BUG=b:190130454
    TEST=tast run $DUT_IP platform.MemoryStressLacros
    
    Change-Id: I5341eae63f4b66945eb75e428c1ed01c5e278b9b

--- src/chromiumos/tast/local/bundles/cros/platform/memory_stress_basic.go
+++ src/chromiumos/tast/local/bundles/cros/platform/memory_stress_basic.go
@@ -6,7 +6,6 @@ package platform
 
 import (
 	"context"
-	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
@@ -17,40 +16,26 @@ import (
 	"chromiumos/tast/common/perf"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/metrics"
-	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/lacros/launcher"
 	"chromiumos/tast/local/media/cpu"
-	"chromiumos/tast/local/memory/kernelmeter"
+	"chromiumos/tast/local/memory/stress"
 	"chromiumos/tast/testing"
 )
 
-type testCaseResult struct {
-	// jankyCount is the average janky count in 30 seconds histogram.
-	jankyCount *metrics.Histogram
-	// discardLatency is the discard latency histogram.
-	discardLatency *metrics.Histogram
-	// reloadCount is the tab reload count.
-	reloadCount int
-	// oomCount is the oom kill count.
-	oomCount uint64
+type testParams struct {
+	isLacros bool
 }
-
-const (
-	allocPageFilename  = "memory_stress.html"
-	javascriptFilename = "memory_stress.js"
-)
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:     MemoryStressBasic,
 		Desc:     "Create heavy memory pressure and check if oom-killer is invoked",
 		Contacts: []string{"vovoy@chromium.org", "chromeos-memory@google.com"},
-		Attr:     []string{"group:crosbolt", "crosbolt_memory_nightly"},
 		// This test takes 15-30 minutes to run.
 		Timeout: 45 * time.Minute,
 		Data: []string{
-			allocPageFilename,
-			javascriptFilename,
+			stress.AllocPageFilename,
+			stress.JavascriptFilename,
 		},
 		SoftwareDeps: []string{"chrome"},
 		Vars: []string{
@@ -60,21 +45,32 @@ func init() {
 			"platform.MemoryStressBasic.useHugePages",
 		},
 		Params: []testing.Param{{
+			ExtraAttr:         []string{"group:crosbolt", "crosbolt_memory_nightly"},
 			ExtraSoftwareDeps: []string{"android_p"},
+			Val: testParams{
+				isLacros: false,
+			},
 		}, {
 			Name:              "vm",
+			ExtraAttr:         []string{"group:crosbolt", "crosbolt_memory_nightly"},
 			ExtraSoftwareDeps: []string{"android_vm"},
+			Val: testParams{
+				isLacros: false,
+			},
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			ExtraData:         []string{launcher.DataArtifact},
+			Fixture:           "lacrosStartedByData",
+			Val: testParams{
+				isLacros: true,
+			},
 		}},
 	})
 
 }
 
 func MemoryStressBasic(ctx context.Context, s *testing.State) {
-	const (
-		mbPerTab    = 800
-		switchCount = 150
-	)
-
 	minFilelistKB := -1
 	if val, ok := s.Var("platform.MemoryStressBasic.minFilelistKB"); ok {
 		val, err := strconv.Atoi(val)
@@ -121,31 +117,19 @@ func MemoryStressBasic(ctx context.Context, s *testing.State) {
 	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
 	defer server.Close()
 
-	baseURL := server.URL + "/" + allocPageFilename
+	baseURL := server.URL + "/" + stress.AllocPageFilename
 
 	perfValues := perf.NewValues()
 
-	// Tests both the low compress ratio and high compress ratio cases.
-	// When there is more random data (67 percent random), the compress ratio is low,
-	// the low memory notification is triggered by low uncompressed anonymous memory.
-	// When there is less random data (33 percent random), the compress ratio is high,
-	// the low memory notification is triggered by low swap free.
-	label67 := "67_percent_random"
-	result67, err := stressTestCase(ctx, perfValues, localRand, mbPerTab, switchCount, minFilelistKB, 0.67, baseURL, label67, enableARC, useHugePages)
-	if err != nil {
-		s.Fatal("67_percent_random test case failed: ", err)
-	}
-	label33 := "33_percent_random"
-	result33, err := stressTestCase(ctx, perfValues, localRand, mbPerTab, switchCount, minFilelistKB, 0.33, baseURL, label33, enableARC, useHugePages)
-	if err != nil {
-		s.Fatal("33_percent_random test case failed: ", err)
-	}
-
-	if err := reportTestCaseResult(ctx, perfValues, result67, label67); err != nil {
-		s.Fatal("Reporting 67_percent_random failed: ", err)
-	}
-	if err := reportTestCaseResult(ctx, perfValues, result33, label33); err != nil {
-		s.Fatal("Reporting 33_percent_random failed: ", err)
+	const mbPerTab = 800
+	if s.Param().(testParams).isLacros {
+		if err := lacrosMain(ctx, s, localRand, mbPerTab, baseURL, perfValues); err != nil {
+			s.Fatal("lacrosMain failed: ", err)
+		}
+	} else {
+		if err := stressMain(ctx, localRand, mbPerTab, minFilelistKB, baseURL, enableARC, useHugePages, perfValues); err != nil {
+			s.Fatal("stressMain failed: ", err)
+		}
 	}
 
 	if err := perfValues.Save(s.OutDir()); err != nil {
@@ -153,7 +137,34 @@ func MemoryStressBasic(ctx context.Context, s *testing.State) {
 	}
 }
 
-func stressTestCase(ctx context.Context, perfValues *perf.Values, localRand *rand.Rand, mbPerTab, switchCount, minFilelistKB int, compressRatio float64, baseURL, label string, enableARC, useHugePages bool) (testCaseResult, error) {
+func stressMain(ctx context.Context, localRand *rand.Rand, mbPerTab, minFilelistKB int, baseURL string, enableARC, useHugePages bool, perfValues *perf.Values) error {
+	const switchCount = 150
+
+	// Tests both the low compress ratio and high compress ratio cases.
+	// When there is more random data (67 percent random), the compress ratio is low,
+	// the low memory notification is triggered by low uncompressed anonymous memory.
+	// When there is less random data (33 percent random), the compress ratio is high,
+	// the low memory notification is triggered by low swap free.
+	result67, err := stressTestCase(ctx, localRand, mbPerTab, switchCount, minFilelistKB, 0.67, baseURL, enableARC, useHugePages)
+	if err != nil {
+		return errors.Wrap(err, "67_percent_random test case failed")
+	}
+	result33, err := stressTestCase(ctx, localRand, mbPerTab, switchCount, minFilelistKB, 0.33, baseURL, enableARC, useHugePages)
+	if err != nil {
+		return errors.Wrap(err, "33_percent_random test case failed")
+	}
+
+	if err := stress.ReportTestCaseResult(ctx, perfValues, result67, "67_percent_random"); err != nil {
+		return errors.Wrap(err, "reporting 67_percent_random failed")
+	}
+	if err := stress.ReportTestCaseResult(ctx, perfValues, result33, "33_percent_random"); err != nil {
+		return errors.Wrap(err, "reporting 33_percent_random failed")
+	}
+
+	return nil
+}
+
+func stressTestCase(ctx context.Context, localRand *rand.Rand, mbPerTab, switchCount, minFilelistKB int, compressRatio float64, baseURL string, enableARC, useHugePages bool) (stress.TestCaseResult, error) {
 	var opts []chrome.Option
 	if enableARC {
 		opts = append(opts, chrome.ARCEnabled())
@@ -163,310 +174,43 @@ func stressTestCase(ctx context.Context, perfValues *perf.Values, localRand *ran
 	}
 	cr, err := chrome.New(ctx, opts...)
 	if err != nil {
-		return testCaseResult{}, errors.Wrap(err, "cannot start chrome")
+		return stress.TestCaseResult{}, errors.Wrap(err, "cannot start chrome")
 	}
 
 	if err := cpu.WaitUntilIdle(ctx); err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to wait for idle CPU")
+		return stress.TestCaseResult{}, errors.Wrap(err, "failed to wait for idle CPU")
 	}
 
 	// Setup min_filelist_kbytes after chrome start.
 	if minFilelistKB >= 0 {
 		if err := ioutil.WriteFile("/proc/sys/vm/min_filelist_kbytes", []byte(strconv.Itoa(minFilelistKB)), 0644); err != nil {
-			return testCaseResult{}, errors.Wrap(err, "could not write to /proc/sys/vm/min_filelist_kbytes")
+			return stress.TestCaseResult{}, errors.Wrap(err, "could not write to /proc/sys/vm/min_filelist_kbytes")
 		}
 	}
 
-	vmstatsStart, err := kernelmeter.VMStats()
-	if err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to get vmstat")
-	}
-
-	createTabCount, err := openTabCount(mbPerTab)
-	if err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to get open tab count")
-	}
-	testing.ContextLog(ctx, "Tab count to create: ", createTabCount)
-
-	if err := openTabs(ctx, cr, createTabCount, mbPerTab, compressRatio, baseURL); err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to open tabs")
-	}
-
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to connect to test API")
-	}
-
-	histogramNames := []string{"Browser.Responsiveness.JankyIntervalsPerThirtySeconds", "Memory.LowMemoryKiller.FirstKillLatency"}
-	startHistograms, err := metrics.GetHistograms(ctx, tconn, histogramNames)
-	if err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to get histograms")
-	}
-
-	reloadCount, err := switchTabs(ctx, cr, switchCount, localRand)
-	if err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to switch tabs")
-	}
-
-	endHistograms, err := metrics.GetHistograms(ctx, tconn, histogramNames)
-	if err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to get histograms")
-	}
-	histograms, err := metrics.DiffHistograms(startHistograms, endHistograms)
-	if err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to diff histograms")
-	}
-	jankyCount := histograms[0]
-	discardLatency := histograms[1]
-
-	vmstatsEnd, err := kernelmeter.VMStats()
-	if err != nil {
-		return testCaseResult{}, errors.Wrap(err, "failed to get vmstat")
-	}
-	oomCount := vmstatsEnd["oom_kill"] - vmstatsStart["oom_kill"]
-
-	return testCaseResult{
-		reloadCount:    reloadCount,
-		jankyCount:     jankyCount,
-		discardLatency: discardLatency,
-		oomCount:       oomCount,
-	}, nil
+	return stress.TestCase(ctx, cr, localRand, mbPerTab, switchCount, compressRatio, baseURL)
 }
 
-// waitAllocation waits for completion of JavaScript memory allocation.
-func waitAllocation(ctx context.Context, conn *chrome.Conn) error {
-	const timeout = 60 * time.Second
-	waitCtx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
-	// Waits for completion of JavaScript allocation.
-	// Checks completion only for the allocation page memory_stress.html.
-	// memory_stress.html saves the allocation result to document.out.
-	const expr = "!window.location.href.includes('memory_stress') || document.hasOwnProperty('out') == true"
-	if err := conn.WaitForExprFailOnErr(waitCtx, expr); err != nil {
-		if waitCtx.Err() == context.DeadlineExceeded {
-			testing.ContextLogf(ctx, "Ignoring tab quiesce timeout (%v)", timeout)
-			return nil
-		}
-		return errors.Wrap(err, "unexpected error waiting for tab quiesce")
-	}
-	return nil
-}
-
-// waitAllocationForURL waits for completion of JavaScript memory allocation on the tab with specified URL.
-func waitAllocationForURL(ctx context.Context, cr *chrome.Chrome, url string) error {
-	conn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(url))
+func lacrosMain(ctx context.Context, s *testing.State, localRand *rand.Rand, mbPerTab int, baseURL string, perfValues *perf.Values) error {
+	lacros, err := launcher.LaunchLacrosChrome(ctx, s.FixtValue().(launcher.FixtData), s.DataPath(launcher.DataArtifact))
 	if err != nil {
-		return errors.Wrap(err, "NewConnForTarget failed")
+		return errors.Wrap(err, "failed to launch lacros-chrome")
 	}
-	defer conn.Close()
 
-	return waitAllocation(ctx, conn)
-}
+	if err := cpu.WaitUntilIdle(ctx); err != nil {
+		return errors.Wrap(err, "failed to wait for idle CPU")
+	}
 
-// openAllocationPage opens a page to allocate many JavaScript objects.
-func openAllocationPage(ctx context.Context, url string, cr *chrome.Chrome) error {
-	conn, err := cr.NewConn(ctx, url)
+	const switchCount = 10
+	const compressRatio = 0.67
+	result, err := stress.TestCase(ctx, lacros, localRand, mbPerTab, switchCount, compressRatio, baseURL)
 	if err != nil {
-		return errors.Wrap(err, "cannot create new renderer")
-	}
-	defer conn.Close()
-
-	return waitAllocation(ctx, conn)
-}
-
-// openTabCount returns the count of tabs to open.
-func openTabCount(mbPerTab int) (int, error) {
-	memPerTab := kernelmeter.NewMemSizeMiB(mbPerTab)
-	memInfo, err := kernelmeter.MemInfo()
-	if err != nil {
-		return 0, errors.Wrap(err, "cannot obtain memory info")
-	}
-	// Allocates more than total memory and swap space size to trigger low memory.
-	return int((memInfo.Total+memInfo.SwapTotal)/memPerTab) + 1, nil
-}
-
-// openTabs opens tabs to create memory pressure.
-func openTabs(ctx context.Context, cr *chrome.Chrome, createTabCount, mbPerTab int, compressRatio float64, baseURL string) error {
-	for i := 0; i < createTabCount; i++ {
-		url := fmt.Sprintf("%s?alloc=%d&ratio=%.3f&id=%d", baseURL, mbPerTab, compressRatio, i)
-		if err := openAllocationPage(ctx, url, cr); err != nil {
-			return errors.Wrap(err, "cannot create tab")
-		}
-	}
-	return nil
-}
-
-// activeTabURL returns the URL of the acitve tab.
-func activeTabURL(ctx context.Context, cr *chrome.Chrome) (string, error) {
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		return "", errors.Wrap(err, "cannot create test connection")
+		return errors.Wrap(err, "stress test case failed")
 	}
 
-	var tabURL string
-	if err := tconn.Call(ctx, &tabURL, `async () => {
-		let tabs = await tast.promisify(chrome.tabs.query)({active: true});
-		return tabs[0].url;
-	}`); err != nil {
-		return "", errors.Wrap(err, "active tab URL not found")
+	if err := stress.ReportTestCaseResult(ctx, perfValues, result, "stress"); err != nil {
+		return errors.Wrap(err, "reporting result failed")
 	}
-	return tabURL, nil
-}
-
-// reloadActiveTab reloads the active tab.
-func reloadActiveTab(ctx context.Context, cr *chrome.Chrome) error {
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		return errors.Wrap(err, "cannot create test connection")
-	}
-
-	return tconn.Eval(ctx, "chrome.tabs.reload()", nil)
-}
-
-// reloadCrashedTab reload the active tab if it's crashed. Returns whether the tab is reloaded.
-func reloadCrashedTab(ctx context.Context, cr *chrome.Chrome) (bool, error) {
-	tabURL, err := activeTabURL(ctx, cr)
-	if err != nil {
-		return false, errors.Wrap(err, "cannot get active tab URL")
-	}
-
-	// If the active tab's URL is not in the devtools targets, the active tab is crashed.
-	targetAvailable, err := cr.IsTargetAvailable(ctx, chrome.MatchTargetURL(tabURL))
-	if err != nil {
-		return false, errors.Wrap(err, "IsTargetAvailable failed")
-	}
-
-	if !targetAvailable {
-		testing.ContextLog(ctx, "Reload tab:", tabURL)
-		if err := reloadActiveTab(ctx, cr); err != nil {
-			return false, errors.Wrap(err, "reloadActiveTab failed")
-		}
-		if err := waitAllocationForURL(ctx, cr, tabURL); err != nil {
-			return false, errors.Wrap(err, "waitAllocationForURL failed")
-		}
-		return true, nil
-	}
-	return false, nil
-}
-
-// waitMoveCursor moves the mouse cursor until after the specified waiting time.
-func waitMoveCursor(ctx context.Context, mw *input.MouseEventWriter, d time.Duration) error {
-	total := time.Duration(0)
-	sleepTime := 15 * time.Millisecond
-	i := 0
-
-	// Reset the cursor to the top left.
-	mw.Move(-10000, -10000)
-
-	for total < d {
-		// Moves mouse cursor back and forth diagonally.
-		if i%100 < 50 {
-			mw.Move(5, 5)
-		} else {
-			mw.Move(-5, -5)
-		}
-		// Sleeps briefly after each cursor move.
-		if err := testing.Sleep(ctx, sleepTime); err != nil {
-			return errors.Wrap(err, "sleep timeout")
-		}
-		i++
-		total += sleepTime
-	}
-
-	return nil
-}
-
-// switchTabs switches between tabs and reloads crashed tabs. Returns the reload count.
-func switchTabs(ctx context.Context, cr *chrome.Chrome, switchCount int, localRand *rand.Rand) (int, error) {
-	mouse, err := input.Mouse(ctx)
-	if err != nil {
-		return 0, errors.Wrap(err, "cannot initialize mouse")
-	}
-	defer mouse.Close()
-
-	keyboard, err := input.Keyboard(ctx)
-	if err != nil {
-		return 0, errors.Wrap(err, "cannot initialize keyboard")
-	}
-	defer keyboard.Close()
-
-	waitTime := 3 * time.Second
-	reloadCount := 0
-	for i := 0; i < switchCount; i++ {
-		if err := keyboard.Accel(ctx, "ctrl+tab"); err != nil {
-			return 0, errors.Wrap(err, "Accel(Ctrl+Tab) failed")
-		}
-		// Waits between tab switches.
-		// +/- within 1000ms from the previous wait time to cluster long and short wait times.
-		// On some settings, it's easier to trigger OOM with clustered short wait times.
-		// On other settings, it's easier with clustered long wait times.
-		waitTime += time.Duration(localRand.Intn(2001)-1000) * time.Millisecond
-		if waitTime < time.Second {
-			waitTime = time.Second
-		} else if waitTime > 5*time.Second {
-			waitTime = 5 * time.Second
-		}
-
-		if err := waitMoveCursor(ctx, mouse, waitTime); err != nil {
-			return 0, errors.Wrap(err, "error when moving mouse cursor")
-		}
-		testing.ContextLogf(ctx, "%3d, wait time: %v", i, waitTime)
-
-		reloaded, err := reloadCrashedTab(ctx, cr)
-		if err != nil {
-			return 0, errors.Wrap(err, "reloadCrashedTab failed")
-		}
-		if reloaded {
-			reloadCount++
-		}
-	}
-	return reloadCount, nil
-}
-
-// reportTestCaseResult writes the test case result to perfValues and prints the test case result.
-func reportTestCaseResult(ctx context.Context, perfValues *perf.Values, result testCaseResult, label string) error {
-	testing.ContextLog(ctx, "===== "+label+" test results =====")
-
-	jankMean, err := result.jankyCount.Mean()
-	if err != nil {
-		return errors.Wrap(err, "failed to get mean for tast_janky_count")
-	}
-	jankyMetric := perf.Metric{
-		Name:      "tast_janky_count_" + label,
-		Unit:      "count",
-		Direction: perf.SmallerIsBetter,
-	}
-	perfValues.Set(jankyMetric, jankMean)
-	testing.ContextLog(ctx, "Average janky count in 30s: ", jankMean)
-
-	killLatency, err := result.discardLatency.Mean()
-	if err != nil {
-		return errors.Wrap(err, "failed to get mean for tast_discard_latency")
-	}
-	killLatencyMetric := perf.Metric{
-		Name:      "tast_discard_latency_" + label,
-		Unit:      "ms",
-		Direction: perf.SmallerIsBetter,
-	}
-	perfValues.Set(killLatencyMetric, killLatency)
-	testing.ContextLog(ctx, "Average discard latency(ms): ", killLatency)
-
-	reloadTabMetric := perf.Metric{
-		Name:      "tast_reload_tab_count_" + label,
-		Unit:      "count",
-		Direction: perf.SmallerIsBetter,
-	}
-	perfValues.Set(reloadTabMetric, float64(result.reloadCount))
-	testing.ContextLog(ctx, "Reload tab count: ", result.reloadCount)
-
-	oomKillerMetric := perf.Metric{
-		Name:      "tast_oom_killer_count_" + label,
-		Unit:      "count",
-		Direction: perf.SmallerIsBetter,
-	}
-	perfValues.Set(oomKillerMetric, float64(result.oomCount))
-	testing.ContextLog(ctx, "OOM Kill count: ", result.oomCount)
 
 	return nil
 }
