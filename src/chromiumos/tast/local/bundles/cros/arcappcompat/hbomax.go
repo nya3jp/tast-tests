d0b4bfdb36abd8dec5a6f2f50f330386ac6eb602
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-06-02 15:13:25 -0700

    arcappcompat.Hbomax skip on Eve
    hbomax is blocked on eve by app developer.
    Do not try to run it on these devices.
    
    BUG=b:124517332
    TEST=tast run <eve> arcappcompat.Hbomax
    
    Change-Id: I0aa7a3f5158128d7084d9de23df7a5c054685abb

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/hbomax.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/hbomax.go
@@ -15,6 +15,7 @@ import (
 	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 // ClamshellTests are placed here.
@@ -40,6 +41,7 @@ func init() {
 		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
 		Attr:         []string{"group:appcompat", "appcompat_release"},
 		SoftwareDeps: []string{"chrome"},
+		HardwareDeps: hwdep.D(hwdep.SkipOnModel("eve")),
 		Params: []testing.Param{{
 			Val:               clamshellTestsForHbomax,
 			ExtraSoftwareDeps: []string{"android_p"},
