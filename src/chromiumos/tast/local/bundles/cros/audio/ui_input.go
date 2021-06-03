d20ae14e741ee910a27e3325eec3e42811e37577
Pin-chih Lin <johnylin@google.com>
2021-06-03 11:33:33 +0800

    tast: Add UIInput.mute to check input mute by UI control
    Add new tast "UIInput.mute" to check the end-to-end controllability of
    audio input node mute. Mute button is controlled by UI quicksettings
    API.
    
    The criteria is to use ALSA loopback path to record audio data on muted
    while playing the sine tone, and check whether the RMS amplitude is low
    enough.
    
    BUG=b:189749471
    BUG=b:189291363
    TEST=tast run ${IP} audio.UIInput.*
    
    Cq-Depend: chromium:2946493
    Change-Id: Id80d8642addee736266b8540bfbae2dc1f2207f2

--- src/chromiumos/tast/local/bundles/cros/audio/ui_input.go
+++ src/chromiumos/tast/local/bundles/cros/audio/ui_input.go
@@ -14,6 +14,7 @@ import (
 
 	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/audio"
 	"chromiumos/tast/local/audio/crastestclient"
 	"chromiumos/tast/local/chrome"
@@ -43,13 +44,71 @@ func init() {
 				Name: "gain",
 				Val:  gainSlider,
 			},
+			{
+				Name: "mute",
+				Val:  muteButton,
+			},
 		},
 	})
 }
 
-func UIInput(ctx context.Context, s *testing.State) {
+func setInputEnabled(ctx context.Context, tconn *chrome.TestConn, enabled bool) error {
+	if err := quicksettings.ToggleMic(ctx, tconn, enabled); err != nil {
+		return errors.Wrap(err, "set failed")
+	}
+
+	// Check the input is really enabled(unmuted) or disabled(muted).
+	currEnabled, err := quicksettings.MicEnabled(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "check setting failed")
+	}
+	if currEnabled != enabled {
+		return errors.New("setting is not as expectation")
+	}
+	return nil
+}
+
+func playAndCaptureToCalculateRms(ctx context.Context, input, output audio.TestRawData) (float64, error) {
+	// Playback function by CRAS.
+	playCmd := crastestclient.PlaybackFileCommand(
+		ctx, input.Path,
+		input.Duration,
+		input.Channels,
+		input.Rate)
+	playCmd.Start()
+
+	// Wait a short time to make sure playback command is working.
+	if err := testing.Sleep(ctx, 1*time.Second); err != nil {
+		if waitErr := playCmd.Wait(); waitErr != nil {
+			return 0.0, errors.Wrap(waitErr, "playback did not finish in time")
+		}
+		return 0.0, errors.Wrap(err, "wait 1sec failed")
+	}
+
+	// Capture function by CRAS.
+	captureErr := crastestclient.CaptureFileCommand(
+		ctx, output.Path,
+		output.Duration,
+		output.Channels,
+		output.Rate).Run(testexec.DumpLogOnError)
+
+	if err := playCmd.Wait(); err != nil {
+		return 0.0, errors.Wrap(err, "playback did not finish in time")
+	}
+
+	if captureErr != nil {
+		return 0.0, errors.Wrap(captureErr, "capture data failed")
+	}
+
+	rms, err := audio.GetRmsAmplitude(ctx, output)
+	if err != nil {
+		return 0.0, errors.Wrap(err, "get RMS failed")
+	}
+	return rms, nil
+}
+
+func testInputGain(ctx context.Context, s *testing.State, tconn *chrome.TestConn, kb *input.KeyboardEventWriter, input audio.TestRawData) {
 	const (
-		cleanupTime     = 10 * time.Second
 		captureDuration = 1 // second(s)
 		lowSliderGain   = 30
 		sliderStepValue = 10
@@ -62,62 +121,9 @@ func UIInput(ctx context.Context, s *testing.State) {
 		gainTolerance = 10.0
 	)
 
-	// system-tray-mic-gain is enabled as default on R86+ images.
-	cr := s.PreValue().(*chrome.Chrome)
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to create Test API connection: ", err)
-	}
-
-	// Set up the keyboard, which is used to increment/decrement the slider.
-	// TODO(crbug/1123231): use better slider automation controls if possible, instead of keyboard controls.
-	kb, err := input.Keyboard(ctx)
-	if err != nil {
-		s.Fatal("Failed to get keyboard: ", err)
-	}
-	defer kb.Close()
-
-	if err := quicksettings.Show(ctx, tconn); err != nil {
-		s.Fatal("Failed to show Quick Settings: ", err)
-	}
-	defer quicksettings.Hide(ctx, tconn)
-
-	// Defer this after deferring quicksettings.Hide to make sure quicksettings is still open when we
-	// get the failure info.
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
-	// Load ALSA loopback module.
-	unload, err := audio.LoadAloop(ctx)
-	if err != nil {
-		s.Fatal("Failed to load ALSA loopback module: ", err)
-	}
-	defer unload(ctx)
-
-	// Generate sine raw input file lasts 5 seconds.
-	audioInput := audio.TestRawData{
-		Path:          filepath.Join(s.OutDir(), "5SEC.raw"),
-		BitsPerSample: 16,
-		Channels:      2,
-		Rate:          48000,
-		Frequencies:   []int{440, 440},
-		Volume:        0.05,
-		Duration:      5,
-	}
-	if err := audio.GenerateTestRawData(ctx, audioInput); err != nil {
-		s.Fatal("Failed to generate audio test data: ", err)
-	}
-	defer os.Remove(audioInput.Path)
-
-	// Reserve time to remove input file and unload ALSA loopback at the end of the test.
-	shortCtx, cancel := ctxutil.Shorten(ctx, cleanupTime)
-	defer cancel()
-
-	// Select ALSA loopback output and input nodes as active nodes by UI.
-	if err := quicksettings.SelectAudioOption(shortCtx, tconn, kb, "Loopback Playback"); err != nil {
-		s.Fatal("Failed to select ALSA loopback output: ", err)
-	}
-	if err := quicksettings.SelectAudioOption(shortCtx, tconn, kb, "Loopback Capture"); err != nil {
-		s.Fatal("Failed to select ALSA loopback input: ", err)
+	// Set input enabled(unmuted) by UI quicksettings.
+	if err := setInputEnabled(ctx, tconn, true); err != nil {
+		s.Fatal("Failed to set mic unmuted: ", err)
 	}
 
 	// Use loopback path to play and record data with specified input gain levels.
@@ -125,7 +131,7 @@ func UIInput(ctx context.Context, s *testing.State) {
 	for _, gain := range []int{lowSliderGain, highSliderGain} {
 		s.Logf("Start testing loopback with expected mic gain slider value: %d", gain)
 
-		audioOutput := audio.TestRawData{
+		output := audio.TestRawData{
 			Path:          filepath.Join(s.OutDir(), fmt.Sprintf("cras_recorded_%d.raw", gain)),
 			BitsPerSample: 16,
 			Channels:      1,
@@ -133,7 +139,7 @@ func UIInput(ctx context.Context, s *testing.State) {
 			Duration:      captureDuration,
 		}
 
-		currVal, err := quicksettings.SliderValue(shortCtx, tconn, quicksettings.SliderTypeMicGain)
+		currVal, err := quicksettings.SliderValue(ctx, tconn, quicksettings.SliderTypeMicGain)
 		if err != nil {
 			s.Fatal("Failed initial value check for mic gain slider: ", err)
 		}
@@ -169,40 +175,9 @@ func UIInput(ctx context.Context, s *testing.State) {
 			}
 		}
 
-		// Playback function by CRAS.
-		playCmd := crastestclient.PlaybackFileCommand(
-			shortCtx, audioInput.Path,
-			audioInput.Duration,
-			audioInput.Channels,
-			audioInput.Rate)
-		playCmd.Start()
-
-		// Wait a short time to make sure playback command is working.
-		if err := testing.Sleep(shortCtx, 1*time.Second); err != nil {
-			if waitErr := playCmd.Wait(); waitErr != nil {
-				s.Log("Playback did not finish in time: ", waitErr)
-			}
-			s.Fatal("Failed to wait 1sec after playback command: ", err)
-		}
-
-		// Capture function by CRAS.
-		captureErr := crastestclient.CaptureFileCommand(
-			shortCtx, audioOutput.Path,
-			audioOutput.Duration,
-			audioOutput.Channels,
-			audioOutput.Rate).Run(testexec.DumpLogOnError)
-
-		if err := playCmd.Wait(); err != nil {
-			s.Fatal("Playback did not finish in time: ", err)
-		}
-
-		if captureErr != nil {
-			s.Fatal("Failed to capture data: ", captureErr)
-		}
-
-		rms, err := audio.GetRmsAmplitude(shortCtx, audioOutput)
+		rms, err := playAndCaptureToCalculateRms(ctx, input, output)
 		if err != nil {
-			s.Fatal("Failed to get RMS amplitude: ", err)
+			s.Fatal("Failed to playback and capture: ", err)
 		}
 		s.Logf("Signal RMS amplitude = %f", rms)
 		rmsValues[gain] = rms
@@ -213,5 +188,110 @@ func UIInput(ctx context.Context, s *testing.State) {
 	s.Logf("Calculated gain = %.4f", gain)
 	if math.Abs(gain-expectedGain) > gainTolerance {
 		s.Errorf("Gain is beyond expectation: got %.4f, expected %.4f, tolerance %.4f", gain, expectedGain, gainTolerance)
+	}
+}
+
+func testInputMute(ctx context.Context, s *testing.State, tconn *chrome.TestConn, input audio.TestRawData) {
+	const (
+		captureDuration = 1 // second(s)
+		rmsTolerance    = 0.00001
+	)
+
+	// Set input disabled(muted) by UI quicksettings, and enabled after test.
+	if err := setInputEnabled(ctx, tconn, false); err != nil {
+		s.Fatal("Failed to set mic muted: ", err)
+	}
+	defer setInputEnabled(ctx, tconn, true)
+
+	// Use loopback path to play and record data with input while muted.
+	s.Log("Start testing loopback with input while muted")
+
+	output := audio.TestRawData{
+		Path:          filepath.Join(s.OutDir(), "cras_recorded_mute.raw"),
+		BitsPerSample: 16,
+		Channels:      1,
+		Rate:          48000,
+		Duration:      captureDuration,
+	}
+
+	rms, err := playAndCaptureToCalculateRms(ctx, input, output)
+	if err != nil {
+		s.Fatal("Failed to playback and capture: ", err)
+	}
+	s.Logf("Signal RMS amplitude = %f", rms)
+
+	// Check the RMS value is small enough while muted.
+	if rms > rmsTolerance {
+		s.Errorf("RMS is too high for mute: got %f, tolerance %f", rms, rmsTolerance)
+	}
+}
+
+func UIInput(ctx context.Context, s *testing.State) {
+	const cleanupTime = 10 * time.Second
+
+	// system-tray-mic-gain is enabled as default on R86+ images.
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	// Set up the keyboard, which is used to increment/decrement the slider.
+	// TODO(crbug/1123231): use better slider automation controls if possible, instead of keyboard controls.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer kb.Close()
+
+	if err := quicksettings.Show(ctx, tconn); err != nil {
+		s.Fatal("Failed to show Quick Settings: ", err)
+	}
+	defer quicksettings.Hide(ctx, tconn)
+
+	// Defer this after deferring quicksettings.Hide to make sure quicksettings is still open when we
+	// get the failure info.
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// Load ALSA loopback module.
+	unload, err := audio.LoadAloop(ctx)
+	if err != nil {
+		s.Fatal("Failed to load ALSA loopback module: ", err)
+	}
+	defer unload(ctx)
+
+	testControl := s.Param().(testControl)
+
+	// Generate sine raw input file lasts 5 seconds.
+	audioInput := audio.TestRawData{
+		Path:          filepath.Join(s.OutDir(), "5SEC.raw"),
+		BitsPerSample: 16,
+		Channels:      2,
+		Rate:          48000,
+		Frequencies:   []int{440, 440},
+		Volume:        0.05,
+		Duration:      5,
+	}
+	if err := audio.GenerateTestRawData(ctx, audioInput); err != nil {
+		s.Fatal("Failed to generate audio test data: ", err)
+	}
+	defer os.Remove(audioInput.Path)
+
+	// Reserve time to remove input file and unload ALSA loopback at the end of the test.
+	shortCtx, cancel := ctxutil.Shorten(ctx, cleanupTime)
+	defer cancel()
+
+	// Select ALSA loopback output and input nodes as active nodes by UI.
+	if err := quicksettings.SelectAudioOption(shortCtx, tconn, kb, "Loopback Playback"); err != nil {
+		s.Fatal("Failed to select ALSA loopback output: ", err)
+	}
+	if err := quicksettings.SelectAudioOption(shortCtx, tconn, kb, "Loopback Capture"); err != nil {
+		s.Fatal("Failed to select ALSA loopback input: ", err)
+	}
+
+	if testControl == gainSlider {
+		testInputGain(shortCtx, s, tconn, kb, audioInput)
+	} else { // muteButton
+		testInputMute(shortCtx, s, tconn, audioInput)
 	}
 }
