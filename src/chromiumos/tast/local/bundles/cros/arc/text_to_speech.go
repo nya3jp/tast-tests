3237a1a9dfb39423f8cc6c9f55a970d5c132c246
Sara Kato <sarakato@google.com>
2021-06-03 10:55:17 +0900

    tast-tests: Add test to confirm Android TTS voice is used
    This tests adds an Anddroid TTS engine, and confirms that chrome.tts
    will use this engine(voice). When the Android TTS engine receives a
    speak request, it writes the text to file called ttsoutput.txt.
    The tast test will check the text inside of this file, to confirm
    that the Android engine is correctly used.
    
    BUG=b:168671331
    TEST=arc.TextToSpeech
    
    Change-Id: I1bae3d2a5ecc6a0019d36a1a178241d850bae44e

---
+++ src/chromiumos/tast/local/bundles/cros/arc/text_to_speech.go
@@ -0,0 +1,116 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/a11y"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TextToSpeech,
+		Desc:         "Checks that Android TTS voice is used when chosen",
+		Contacts:     []string{"sarakato@chromium.org", "sahok@chromium.org", "hirokisato@chromium.org", "arc-framework+tast@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "arcBooted",
+		Timeout:      4 * time.Minute,
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+	})
+}
+
+func TextToSpeech(ctx context.Context, s *testing.State) {
+	const (
+		apk           = "ArcTtsTest.apk"
+		enginePackage = "org.chromium.arc.testapp.tts"
+		speakText     = "hello world"
+		voiceName     = "Android org.chromium.arc.testapp.tts.ArcTtsService en"
+	)
+
+	cr := s.FixtValue().(*arc.PreData).Chrome
+	a := s.FixtValue().(*arc.PreData).ARC
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed creating test API connection: ", err)
+	}
+
+	s.Logf("Installing %s", apk)
+	if err := a.Install(ctx, arc.APKPath(apk)); err != nil {
+		s.Fatalf("Failed to install %s: %v", apk, err)
+	}
+
+	if err := a.Command(ctx, "settings", "put", "secure", "tts_default_synth", enginePackage).Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Failed to enable showing ANRs: ", err)
+	}
+
+	var ret []*a11y.VoiceData
+	const getVoices = `new Promise((resolve, reject) => {
+	chrome.tts.getVoices(
+		(voices)=> {
+			resolve(voices);}
+		)
+	})`
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := tconn.Eval(ctx, getVoices, &ret); err != nil {
+			return err
+		}
+
+		for _, voice := range ret {
+			if voice.Name == voiceName {
+				return nil
+			}
+		}
+		return errors.New("TTS engine is not loaded")
+	}, &testing.PollOptions{
+		Timeout: 15 * time.Second,
+	}); err != nil {
+		s.Fatal("Failed waiting for TTS engine to load: ", err)
+	}
+
+	speakScript := fmt.Sprintf("tast.promisify(chrome.tts.speak(%q, {'voiceName': %q, 'rate': 2.0, 'volume': 0}))", speakText, voiceName)
+	if err := tconn.Eval(ctx, speakScript, nil); err != nil {
+		s.Fatal("Failed to speak: ", err)
+	}
+
+	cryptohomeUserPath, err := cryptohome.UserPath(ctx, cr.User())
+	if err != nil {
+		s.Fatalf("Failed to get the cryptohome user path for %s: %v", cr.User(), err)
+	}
+	targetPathInCros := filepath.Join(cryptohomeUserPath, "MyFiles", "Downloads", "ttsoutput.txt")
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		actual, err := ioutil.ReadFile(targetPathInCros)
+		if err != nil {
+			return errors.Wrap(err, "failed to read TTS output file")
+		}
+		outputString := string(actual)
+		if outputString != speakText {
+			return errors.Errorf("TTS output was incorrect; got %q, want %q", outputString, speakText)
+		}
+		return nil
+	}, &testing.PollOptions{
+		Timeout: 15 * time.Second,
+	}); err != nil {
+		s.Fatal("Failed waiting for TTS output: ", err)
+	}
+
+}
