11f7eb3a673b16e972ca9771ef1c98a5499c1a07
Sara Kato <sarakato@google.com>
2021-06-03 10:55:17 +0900

    tast-tests: Add test to confirm Android TTS voice is used
    This tests adds an Anddroid TTS engine, and confirms that chrome.tts
    will use this engine(voice). When the Android TTS engine receives a
    speak request, it writes the text to file called ttsoutput.txt.
    The tast test will check the text inside of this file, to confirm
    that the Android engine is correctly used.
    
    BUG=b:168671331
    TEST=arc.AccessiblityTts
    
    Change-Id: I1bae3d2a5ecc6a0019d36a1a178241d850bae44e

---
+++ android/ArcTtsTest/src/org/chromium/arc/ttsengine/ArcTtsService.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.ttsengine;
+
+import android.os.Environment;
+import android.speech.tts.SynthesisCallback;
+import android.speech.tts.SynthesisRequest;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeechService;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+/**
+ * A text to speech engine that outputs the generated speech
+ * to a file called "ttsoutput.txt" under Downloads.
+ * {@link android.speech.tts.TextToSpeechService}.
+ */
+public class ArcTtsService extends TextToSpeechService {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected String[] onGetLanguage() {
+        return null;
+    }
+
+    @Override
+    protected int onIsLanguageAvailable(String lang, String country, String variant) {
+        return TextToSpeech.LANG_AVAILABLE;
+    }
+
+    @Override
+    protected synchronized int onLoadLanguage(String lang, String country, String variant) {
+          return TextToSpeech.LANG_AVAILABLE;
+    }
+
+    @Override
+    protected void onStop() {
+      return;
+    }
+
+    @Override
+    protected synchronized void onSynthesizeText(SynthesisRequest request,
+            SynthesisCallback callback) {
+      final String text = request.getText().toLowerCase();
+      final File logFile =
+          new File(Environment.getExternalStoragePublicDirectory(
+              Environment.DIRECTORY_DOWNLOADS), "ttsoutput.txt");
+      try (PrintStream out = new PrintStream(new FileOutputStream(logFile, true))) {
+          out.println(text);
+      } catch (FileNotFoundException e) {
+          throw new RuntimeException(e);
+      }
+
+      return;
+    }
+}
---
+++ src/chromiumos/tast/local/bundles/cros/arc/accessibility_tts.go
@@ -0,0 +1,114 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AccessibilityTts,
+		Desc:         "Checks that Android TTS voice is used when chosen",
+		Contacts:     []string{"sarakato@chromium.org", "dtseng@chromium.org", "hirokisato@chromium.org", "arc-framework+tast@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "arcBooted",
+		Timeout:      4 * time.Minute,
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+	})
+}
+
+// TtsVoice corresponds to the ttsVoice definition in chrome/common/extension/api/tts.json.
+type TtsVoice struct {
+	VoiceName string `json:"voiceName"`
+}
+
+func AccessibilityTts(ctx context.Context, s *testing.State) {
+	const (
+		apk           = "ArcTtsTest.apk"
+		enginePath    = "org.chromium.arc.ttsengine"
+		text          = "hello world"
+		ttsOutputPath = "/home/chronos/user/Downloads/ttsoutput.txt"
+		voiceName     = "Android org.chromium.arc.ttsengine.RobotSpeakTtsService en"
+	)
+
+	cr := s.FixtValue().(*arc.PreData).Chrome
+	a := s.FixtValue().(*arc.PreData).ARC
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed creating test API connection: ", err)
+	}
+
+	s.Logf("Installing %s", apk)
+	if err := a.Install(ctx, arc.APKPath(apk)); err != nil {
+		s.Fatalf("Failed to install %s: %v", apk, err)
+	}
+
+	if err := a.Command(ctx, "settings", "put", "secure", "tts_default_synth", enginePath).Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Failed to enable showing ANRs: ", err)
+	}
+
+	var ret []*TtsVoice
+	const getVoices = `new Promise((resolve, reject) => {
+	chrome.tts.getVoices(
+		(voices)=> {
+			resolve(voices);}
+		)
+	})`
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := tconn.Eval(ctx, getVoices, &ret); err != nil {
+			return err
+		}
+
+		for _, voice := range ret {
+			if voice.VoiceName == voiceName {
+				return nil
+			}
+		}
+		return errors.New("TTS engine is not loaded")
+	}, &testing.PollOptions{
+		Timeout: 15 * time.Second,
+	}); err != nil {
+		s.Fatal("Failed waiting for TTS engine to load: ", err)
+	}
+
+	speakScript := fmt.Sprintf("tast.promisify(chrome.tts.speak(%q, {'voiceName': %q, 'rate': 2.0, 'volume': 0}))", text, voiceName)
+	if err := tconn.Eval(ctx, speakScript, nil); err != nil {
+		s.Fatal("Failed to speak: ", err)
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		actual, err := ioutil.ReadFile(ttsOutputPath)
+		if err != nil {
+			return errors.Wrap(err, "failed to read TTS output file")
+		}
+		outputString := strings.TrimSuffix(string(actual[:]), "\n")
+		if outputString != text {
+			return errors.Errorf("TTS output was incorrect; got %q, want %q", outputString, text)
+		}
+		return nil
+	}, &testing.PollOptions{
+		Timeout: 15 * time.Second,
+	}); err != nil {
+		s.Fatal("Failed waiting for TTS output: ", err)
+	}
+
+}
