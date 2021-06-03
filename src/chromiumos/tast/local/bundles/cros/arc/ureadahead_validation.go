0805a66e5e880bb9d22f7eba43fb0d9446f35611
Yury Khmel <khmel@chromium.org>
2021-06-03 13:06:35 +0000

    arc: Relax memory check for uploading arc.DataCollector artifacts.
    This relax memory check introduced in crrev.com/c/2909725 by disabling
    uloading only for ureadahead caches. GMS Core caches are normally
    generated regardless of available memory.
    
    BUG=b:188579267
    TEST=tast run arc.DataCollector.vm
         tast run arc.UreadaheadValidation.vm
    
    Change-Id: Id9005c35b46546214cb82c97c9509b60d1e7b6f3

--- src/chromiumos/tast/local/bundles/cros/arc/ureadahead_validation.go
+++ src/chromiumos/tast/local/bundles/cros/arc/ureadahead_validation.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/testing"
 )
@@ -38,12 +39,41 @@ func init() {
 	})
 }
 
+// ureadaheadPackRequired returns true in case ureadahead is required for the current device.
+func ureadaheadPackRequired(ctx context.Context, isVMEnabled bool) (bool, error) {
+	const (
+		// please see arc.DataCollector for description.
+		minVMMemoryKb = 7500000
+	)
+
+	if !isVMEnabled {
+		// For container ureadahead pack is always required.
+		return true, nil
+	}
+
+	memInfo, err := testexec.CommandContext(ctx, "cat", "/proc/meminfo").Output()
+	if err != nil {
+		return false, errors.Wrap(err, "failed to read /proc/meminfo")
+	}
+
+	memTotal := regexp.MustCompile(`(\n|^)MemTotal:\s+(\d+)\s+kB(\n|$)`).FindSubmatch(memInfo)
+	if memTotal == nil {
+		return false, errors.Errorf("required MemTotal is not found in %q", memInfo)
+	}
+	memTotalInt, err := strconv.Atoi(string(memTotal[2]))
+	if err != nil || memTotalInt <= 0 {
+		return false, errors.Errorf("failed to parse %q", memTotal[2])
+	}
+
+	return memTotalInt >= minVMMemoryKb, nil
+}
+
 func UreadaheadValidation(ctx context.Context, s *testing.State) {
 	const (
 		// normally generated ureadahead pack covers >350MB of data.
 		minAcceptableUreadaheadPackizeKb = 300 * 1024
 
-		// name of ureadahead log
+		// name of ureadahead log.
 		ureadaheadLogName = "ureadahead.log"
 	)
 
@@ -58,6 +88,23 @@ func UreadaheadValidation(ctx context.Context, s *testing.State) {
 		packPath = "/opt/google/vms/android/ureadahead.pack"
 	} else {
 		packPath = "/opt/google/containers/android/ureadahead.pack"
+	}
+
+	if _, err := os.Stat(packPath); err != nil {
+		if os.IsNotExist(err) {
+			required, err := ureadaheadPackRequired(ctx, isVMEnabled)
+			if err != nil {
+				s.Fatalf("Failed to check if ureadahead pack %s required, %v : ", packPath, err)
+			}
+			if required {
+				s.Fatalf("ureadahead pack %s does not exist but required", packPath)
+			} else {
+				testing.ContextLogf(ctx, "ureadahead pack %s does not exist and is not required", packPath)
+				return
+			}
+		} else {
+			s.Fatalf("Failed to check ureadahead pack exists %s, %v : ", packPath, err)
+		}
 	}
 
 	logPath := filepath.Join(s.OutDir(), ureadaheadLogName)
