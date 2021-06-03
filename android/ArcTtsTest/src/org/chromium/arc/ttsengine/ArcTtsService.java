af2c72988bc9426b2f9d179cc40f52e95c6af0ba
Sara Kato <sarakato@google.com>
2021-06-03 10:55:17 +0900

    tast-tests: Add test to confirm Android TTS voice is used
    This tests adds an Anddroid TTS engine, and confirms that chrome.tts
    will use this engine(voice). When the Android TTS engine receives a
    speak request, it writes the text to file called ttsoutput.txt.
    The tast test will check the text inside of this file, to confirm
    that the Android engine is correctly used.
    
    BUG=b:168671331
    TEST=arc.TextToSpeech
    
    Change-Id: I1bae3d2a5ecc6a0019d36a1a178241d850bae44e

---
+++ android/ArcTtsTest/src/org/chromium/arc/ttsengine/ArcTtsService.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.tts;
+
+import android.os.Environment;
+import android.speech.tts.SynthesisCallback;
+import android.speech.tts.SynthesisRequest;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeechService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * A text to speech engine that outputs the generated speech to a file called "ttsoutput.txt" under
+ * Downloads. {@link android.speech.tts.TextToSpeechService}.
+ */
+public class ArcTtsService extends TextToSpeechService {
+    final static String OUTPUT_FILENAME = "ttsoutput.txt";
+
+    @Override
+    protected String[] onGetLanguage() {
+        return new String[] {"eng", "USA", ""};
+    }
+
+    @Override
+    protected int onIsLanguageAvailable(String lang, String country, String variant) {
+        if (lang.equals("eng")) return TextToSpeech.LANG_AVAILABLE;
+        return TextToSpeech.LANG_NOT_SUPPORTED;
+    }
+
+    @Override
+    protected synchronized int onLoadLanguage(String lang, String country, String variant) {
+        if (lang.equals("eng")) return TextToSpeech.LANG_AVAILABLE;
+        return TextToSpeech.LANG_NOT_SUPPORTED;
+    }
+
+    @Override
+    protected void onStop() {
+        return;
+    }
+
+    @Override
+    protected synchronized void onSynthesizeText(
+            SynthesisRequest request, SynthesisCallback callback) {
+        Path path =
+                Paths.get(
+                        Environment.getExternalStoragePublicDirectory(
+                                        Environment.DIRECTORY_DOWNLOADS)
+                                .getPath(),
+                        OUTPUT_FILENAME);
+
+        try {
+            Files.createFile(path);
+            byte[] bytes = request.getText().getBytes();
+            Files.write(path, bytes, java.nio.file.StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return;
+    }
+}
