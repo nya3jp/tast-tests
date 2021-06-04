bbb13ea36a8cf2144c70438d8e85dee53b130df3
Jeremy Bettis <jbettis@google.com>
2021-06-04 11:19:27 -0600

    tast: Changed the servo spec to include ssh port
    When connecting to a skylab machine, it is necessary to use ssh port
    forwarding, and the ssh port for connecting to the servo host will not
    be on port 22.
    
    Changed the servo string to be of the form host:9999:ssh:22, and allow
    for ipv6 addresses such as [::1]:9999:ssh:22, also the port and ssh port
    can be omitted, and the entire servo string can be omitted, and it
    defaults to localhost:9999:ssh:22.
    
    BUG=None
    TEST=Added unit tests, also connected to local DUT and skylab DUT.
    
    Change-Id: Ib00f7af82c55eae1c316a6d010b9a5afa6d507d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2940795
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Greg Edelston <gredelston@google.com>

---
+++ src/chromiumos/tast/remote/servo/proxy_test.go
@@ -0,0 +1,57 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package servo
+
+import (
+	"testing"
+)
+
+func TestSplitHostPort(t *testing.T) {
+	for _, tc := range []struct {
+		input           string
+		expectedHost    string
+		expectedPort    int
+		expectedSSHPort int
+		expectErr       bool
+	}{
+		{"", "localhost", 9999, 22, false},
+		{":ssh:", "", 0, 0, true},
+		{":ssh:33", "localhost", 9999, 33, false},
+		{"rutabaga", "rutabaga", 9999, 22, false},
+		{"rutabaga:ssh:33", "rutabaga", 9999, 33, false},
+		{"rutabaga:1234", "rutabaga", 1234, 22, false},
+		{"rutabaga:1234:ssh:33", "rutabaga", 1234, 33, false},
+		{"rutabaga:localhost:1234", "", 0, 0, true},
+		{":1234", "localhost", 1234, 22, false},
+		{":1234:ssh:", "", 0, 0, true},
+		{":1234:ssh:33", "localhost", 1234, 33, false},
+		{"[::2]", "::2", 9999, 22, false},
+		{"[::2]:ssh:33", "::2", 9999, 33, false},
+		{"[::2]:1234", "::2", 1234, 22, false},
+		{"[::2]:1234:ssh:33", "::2", 1234, 33, false},
+		{"[::2]:localhost:1234", "", 0, 0, true},
+		{"::2", "", 0, 0, true},
+		{"::2:1234", "", 0, 0, true},
+	} {
+		actualHost, actualPort, actualSSHPort, err := splitHostPort(tc.input)
+		if err != nil && !tc.expectErr {
+			t.Errorf("splitHostPort(%q) returned unexpected error: %v", tc.input, err)
+			continue
+		}
+		if err == nil && tc.expectErr {
+			t.Errorf("splitHostPort(%q) unexpectedly succeeded %s %d %d", tc.input, actualHost, actualPort, actualSSHPort)
+			continue
+		}
+		if actualHost != tc.expectedHost {
+			t.Errorf("splitHostPort(%q) returned host %q; want %q", tc.input, actualHost, tc.expectedHost)
+		}
+		if actualPort != tc.expectedPort {
+			t.Errorf("splitHostPort(%q) returned port %d; want %d", tc.input, actualPort, tc.expectedPort)
+		}
+		if actualSSHPort != tc.expectedSSHPort {
+			t.Errorf("splitHostPort(%q) returned port %d; want %d", tc.input, actualSSHPort, tc.expectedSSHPort)
+		}
+	}
+}
