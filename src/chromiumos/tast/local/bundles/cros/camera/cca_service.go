964b72bd762b2cf93ae0452e052c3f813e7d0805
Wei Lee <wtlee@google.com>
2021-06-04 15:24:24 +0800

    camera: Add CCARemote and document scanning remote test
    To test the document detection on preview, it needs to go through the
    CrOS VCD, which is unable to be covered by camera.CCAUIDocumentScanning
    since it uses file VCD.
    
    Therefore, this CL adds CCARemote test and have a document scanning test
    case to check if document detection on preview works expectedly via
    CameraBox.
    
    BUG=b:193196284
    TEST=tast run -var=chart=DUT1 DUT2 camera.CCARemote.document_scanning_back
    
    Change-Id: Ia32562f78c174868a46b06f776840f1570e50ad4

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_service.go
@@ -0,0 +1,106 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/camera/cca"
+	"chromiumos/tast/local/syslog"
+	cameraboxpb "chromiumos/tast/services/cros/camerabox"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddService(&testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			cameraboxpb.RegisterCCAServiceServer(srv, &CCAService{s: s})
+		},
+	})
+}
+
+type CCAService struct {
+	s *testing.ServiceState
+}
+
+type testFunc = func(ctx context.Context, scriptPaths []string, outDir string, facing cca.Facing) error
+
+var getTestFuncMap = map[cameraboxpb.CCATest]testFunc{
+	cameraboxpb.CCATest_DOCUMENT_SCANNING: cca.RunPreviewDocumentCornersDetection,
+}
+
+var facingMap = map[cameraboxpb.Facing]cca.Facing{
+	cameraboxpb.Facing_FACING_BACK:  cca.FacingBack,
+	cameraboxpb.Facing_FACING_FRONT: cca.FacingFront,
+}
+
+func tempFilePathForScriptContent(ctx context.Context, script []byte) (string, error) {
+	tempFile, err := ioutil.TempFile("", "Script_*")
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create temp file for script")
+	}
+	defer tempFile.Close()
+
+	_, err = tempFile.Write(script)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to write script into temp file")
+	}
+	return tempFile.Name(), nil
+}
+
+func (c *CCAService) RunTest(ctx context.Context, req *cameraboxpb.CCATestRequest) (_ *cameraboxpb.CCATestResponse, retErr error) {
+	testFunc, ok := getTestFuncMap[req.Test]
+	if !ok {
+		return nil, errors.Errorf("failed to run unknown test %v", req.Test)
+	}
+
+	outDir, ok := testing.ContextOutDir(ctx)
+	if !ok {
+		return nil, errors.New("failed to get remote output directory")
+	}
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	endLogFn, err := syslog.CollectSyslog()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start collecting syslog")
+	}
+	defer func(ctx context.Context) {
+		endLogFn(ctx, outDir)
+	}(cleanupCtx)
+
+	var tmpScriptPaths []string
+	for _, scriptContent := range req.ScriptContents {
+		tempFilePath, err := tempFilePathForScriptContent(ctx, scriptContent)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to put script contents into temp file")
+		}
+		tmpScriptPaths = append(tmpScriptPaths, tempFilePath)
+	}
+	defer func() {
+		for _, path := range tmpScriptPaths {
+			os.Remove(path)
+		}
+	}()
+
+	result := cameraboxpb.CCATestResponse{}
+	if testErr := testFunc(ctx, tmpScriptPaths, outDir, facingMap[req.Facing]); testErr == nil {
+		result.Result = cameraboxpb.TestResult_TEST_RESULT_PASSED
+	} else {
+		result.Result = cameraboxpb.TestResult_TEST_RESULT_FAILED
+		result.Error = testErr.Error()
+	}
+
+	return &result, nil
+}
