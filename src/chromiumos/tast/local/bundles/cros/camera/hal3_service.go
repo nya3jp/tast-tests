86f56bd2f2f85e7ed094414af29531fb502b93ae
Moja Hsu <mojahsu@google.com>
2021-06-04 15:33:02 +0800

    camera: Add face detection test running in camerabox
    Add face detection test. We use the face image from ITS scene2_a and
    check if DUT can detect 3 faces.
    
    BUG=b:189016786
    TEST=Pass it by forcing enable face detection on octopus
    
    Change-Id: I14fb9e48f486f83618f93333ee3c82fe315a74e4

--- src/chromiumos/tast/local/bundles/cros/camera/hal3_service.go
+++ src/chromiumos/tast/local/bundles/cros/camera/hal3_service.go
@@ -31,16 +31,17 @@ type HAL3Service struct {
 type getTestConfig = func() hal3.TestConfig
 
 var getTestConfigMap = map[cameraboxpb.HAL3CameraTest]getTestConfig{
-	cameraboxpb.HAL3CameraTest_DEVICE:        hal3.DeviceTestConfig,
-	cameraboxpb.HAL3CameraTest_FRAME:         hal3.FrameTestConfig,
-	cameraboxpb.HAL3CameraTest_JDA:           hal3.JDATestConfig,
-	cameraboxpb.HAL3CameraTest_JEA:           hal3.JEATestConfig,
-	cameraboxpb.HAL3CameraTest_MODULE:        hal3.ModuleTestConfig,
-	cameraboxpb.HAL3CameraTest_PERF:          hal3.PerfTestConfig,
-	cameraboxpb.HAL3CameraTest_PREVIEW:       hal3.PreviewTestConfig,
-	cameraboxpb.HAL3CameraTest_RECORDING:     hal3.RecordingTestConfig,
-	cameraboxpb.HAL3CameraTest_STILL_CAPTURE: hal3.StillCaptureTestConfig,
-	cameraboxpb.HAL3CameraTest_STREAM:        hal3.StreamTestConfig,
+	cameraboxpb.HAL3CameraTest_DEVICE:         hal3.DeviceTestConfig,
+	cameraboxpb.HAL3CameraTest_FRAME:          hal3.FrameTestConfig,
+	cameraboxpb.HAL3CameraTest_JDA:            hal3.JDATestConfig,
+	cameraboxpb.HAL3CameraTest_JEA:            hal3.JEATestConfig,
+	cameraboxpb.HAL3CameraTest_MODULE:         hal3.ModuleTestConfig,
+	cameraboxpb.HAL3CameraTest_PERF:           hal3.PerfTestConfig,
+	cameraboxpb.HAL3CameraTest_PREVIEW:        hal3.PreviewTestConfig,
+	cameraboxpb.HAL3CameraTest_RECORDING:      hal3.RecordingTestConfig,
+	cameraboxpb.HAL3CameraTest_STILL_CAPTURE:  hal3.StillCaptureTestConfig,
+	cameraboxpb.HAL3CameraTest_STREAM:         hal3.StreamTestConfig,
+	cameraboxpb.HAL3CameraTest_FACE_DETECTION: hal3.FaceDetectionTestConfig,
 }
 
 func (c *HAL3Service) RunTest(ctx context.Context, req *cameraboxpb.RunTestRequest) (_ *cameraboxpb.RunTestResponse, retErr error) {
@@ -57,6 +58,7 @@ func (c *HAL3Service) RunTest(ctx context.Context, req *cameraboxpb.RunTestReque
 	default:
 		return nil, errors.Errorf("unsupported facing: %v", req.Facing.String())
 	}
+	cfg.ExpectedNumFaces = req.ExpectedNumFaces
 	cfg.CameraHALs = []string{}
 
 	endLogFn, err := syslog.CollectSyslog()
---
+++ src/chromiumos/tast/remote/bundles/cros/camera/camerabox/camerabox.go
@@ -0,0 +1,72 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package camerabox provides utilities for camerabox environment.
+package camerabox
+
+import (
+	"context"
+	"path"
+	"strings"
+	"time"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	pb "chromiumos/tast/services/cros/camerabox"
+	"chromiumos/tast/ssh/linuxssh"
+	"chromiumos/tast/testing"
+)
+
+// LogTestScene takes a photo of test scene as log to debug scene related problem.
+func LogTestScene(ctx context.Context, d *dut.DUT, facing pb.Facing, outdir string) (retErr error) {
+	testing.ContextLog(ctx, "Capture scene log image")
+
+	// Release camera unique resource from cros-camera temporarily for taking a picture of test scene.
+	out, err := d.Command("status", "cros-camera").Output(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get initial state of cros-camera")
+	}
+	if strings.Contains(string(out), "start/running") {
+		if err := d.Command("stop", "cros-camera").Run(ctx); err != nil {
+			return errors.Wrap(err, "failed to stop cros-camera")
+		}
+		defer func() {
+			if err := d.Command("start", "cros-camera").Run(ctx); err != nil {
+				if retErr != nil {
+					testing.ContextLog(ctx, "Failed to start cros-camera")
+				} else {
+					retErr = errors.Wrap(err, "failed to start cros-camera")
+				}
+			}
+		}()
+	}
+
+	// Timeout for capturing scene image.
+	captureCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	// Take a picture of test scene.
+	facingArg := "back"
+	if facing == pb.Facing_FACING_FRONT {
+		facingArg = "front"
+	}
+	const sceneLog = "/tmp/scene.jpg"
+	if err := d.Command(
+		"sudo", "--user=arc-camera", "cros_camera_test",
+		"--gtest_filter=Camera3StillCaptureTest/Camera3DumpSimpleStillCaptureTest.DumpCaptureResult/0",
+		"--camera_facing="+facingArg,
+		"--dump_still_capture_path="+sceneLog,
+	).Run(captureCtx); err != nil {
+		return errors.Wrap(err, "failed to run cros_camera_test to take a scene photo")
+	}
+
+	// Copy result scene log image.
+	if err := linuxssh.GetFile(ctx, d.Conn(), sceneLog, path.Join(outdir, path.Base(sceneLog))); err != nil {
+		return errors.Wrap(err, "failed to pull scene log file from DUT")
+	}
+	if err := d.Command("rm", sceneLog).Run(ctx); err != nil {
+		return errors.Wrap(err, "failed to clean up scene log file from DUT")
+	}
+	return nil
+}
