749414ce12d4e07e3c9a1fc17d205a9f9c2c8003
Renuga Nanjappan <rnanjappan@chromium.org>
2021-06-04 23:48:55 +0000

    tast tests: Add test to verify the paid app installation for unicorn account
    Test verifies that paid app installation doesn't go through and
    triggers parent permission dialog when attempted.Parent account has been
    setup with payment method to enable this test.
    
    BUG=b:176872666
    TEST=tast run <DUT> arc.UnicornPaidAppParentPermission
    
    Change-Id: I0ba705d8c832b53763e6c953d7333be3c4574c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2941618
    Tested-by: Renuga Nanjappan <rnanjappan@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Renuga Nanjappan <rnanjappan@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arc/unicorn_paid_app_parent_permission.go
@@ -0,0 +1,143 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/optin"
+	"chromiumos/tast/local/chrome/familylink"
+	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         UnicornPaidAppParentPermission,
+		Desc:         "Checks if paid app installation triggers Parent Permission For Unicorn Account",
+		Contacts:     []string{"rnanjappan@chromium.org", "cros-arc-te@google.com"},
+		Attr:         []string{"group:mainline", "informational", "group:arc-functional"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      4 * time.Minute,
+		Vars:         []string{"arc.parentUser"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		Fixture: "familyLinkUnicornArcLogin",
+	})
+}
+
+func UnicornPaidAppParentPermission(ctx context.Context, s *testing.State) {
+	const (
+		askinMessageButtonText = "Ask in a message"
+		askinPersonButtonText  = "Ask in person"
+		playStoreSearchText    = "Search for apps & games"
+		gamesAppName           = "the wonder weeks"
+	)
+	parentUser := s.RequiredVar("arc.parentUser")
+	cr := s.FixtValue().(*familylink.FixtData).Chrome
+	tconn := s.FixtValue().(*familylink.FixtData).TestConn
+
+	st, err := arc.GetState(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get ARC state: ", err)
+	}
+	if st.Provisioned {
+		s.Log("ARC is already provisioned. Skipping the Play Store setup")
+	} else {
+		// Optin to Play Store.
+		s.Log("Opting into Play Store")
+		if err := optin.PerformAndClose(ctx, cr, tconn); err != nil {
+			s.Fatal("Failed to optin to Play Store and Close: ", err)
+		}
+	}
+	if err := launcher.LaunchApp(tconn, apps.PlayStore.Name)(ctx); err != nil {
+		s.Fatal("Failed to launch Play Store")
+	}
+
+	// Setup ARC.
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close(ctx)
+	defer func() {
+		if s.HasError() {
+			if err := a.Command(ctx, "uiautomator", "dump").Run(testexec.DumpLogOnError); err != nil {
+				s.Error("Failed to dump UIAutomator: ", err)
+			}
+			if err := a.PullFile(ctx, "/sdcard/window_dump.xml", filepath.Join(s.OutDir(), "uiautomator_dump.xml")); err != nil {
+				s.Error("Failed to pull UIAutomator dump: ", err)
+			}
+		}
+	}()
+
+	d, err := a.NewUIDevice(ctx)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	defer d.Close(ctx)
+
+	searchText := d.Object(ui.ClassName("android.widget.TextView"), ui.Text(playStoreSearchText))
+	if err := searchText.WaitForExists(ctx, 90*time.Second); err != nil {
+		s.Fatal("searchText doesn't exist: ", err)
+	}
+	if err := searchText.Click(ctx); err != nil {
+		s.Fatal("Failed to click on searchText: ", err)
+	}
+
+	searchTextEdit := d.Object(ui.ClassName("android.widget.EditText"), ui.Text(playStoreSearchText))
+	if err := searchTextEdit.SetText(ctx, gamesAppName); err != nil {
+		s.Fatal("Failed to set text to search: ", err)
+	}
+	if err := d.PressKeyCode(ctx, ui.KEYCODE_ENTER, 0); err != nil {
+		s.Fatal("Failed to click on KEYCODE_ENTER button: ", err)
+	}
+
+	installButton := d.Object(ui.ClassName("android.widget.Button"), ui.Text("$3.99"), ui.Enabled(true))
+	if err := installButton.WaitForExists(ctx, 10*time.Second); err != nil {
+		s.Fatal("Install Button doesn't exisit: ", err)
+	}
+	if err := installButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click  installButton: ", err)
+	}
+
+	buyButton := d.Object(ui.ClassName("android.widget.Button"), ui.Text("Buy"), ui.Enabled(true))
+	if err := buyButton.WaitForExists(ctx, 10*time.Second); err != nil {
+		s.Fatal("Buy Button doesn't exisit: ", err)
+	}
+	if err := buyButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click Buy Button: ", err)
+	}
+
+	// Verify Parent Permission Dialog is displayed.
+	askinPersonButton := d.Object(ui.ClassName("android.widget.Button"), ui.Text(askinPersonButtonText), ui.Enabled(true))
+	if err := askinPersonButton.WaitForExists(ctx, 90*time.Second); err != nil {
+		s.Fatal("Ask in person button doesn't Exists: ", err)
+	}
+
+	if err := d.Object(ui.TextMatches(askinMessageButtonText)).Exists(ctx); err != nil {
+		s.Fatal("Ask in a message button doesn't exist: ", err)
+	}
+
+	if err = askinPersonButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click  Ask in person: ", err)
+	}
+
+	parentPwd := d.Object(ui.ClassName("android.widget.EditText"), ui.Text(parentUser))
+	if err := parentPwd.WaitForExists(ctx, 90*time.Second); err != nil {
+		s.Fatal("parentPwd doesn't Exists: ", err)
+	}
+
+}
