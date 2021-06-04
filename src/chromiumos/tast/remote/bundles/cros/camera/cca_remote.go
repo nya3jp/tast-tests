b1edadf1e1c1416ac1de2239b8744cb953dfaedd
Wei Lee <wtlee@google.com>
2021-06-04 15:24:24 +0800

    camera: Add CCARemote and document scanning remote test
    To test the document detection on preview, it needs to go through the
    CrOS VCD, which is unable to be covered by camera.CCAUIDocumentScanning
    since it uses file VCD.
    
    Therefore, this CL adds CCARemote test and have a document scanning test
    case to check if document detection on preview works expectedly via
    CameraBox.
    
    BUG=b:193196284
    TEST=tast run -var=chart=DUT1 DUT2 camera.CCARemote.document_scanning_back
    
    Change-Id: Ia32562f78c174868a46b06f776840f1570e50ad4

---
+++ src/chromiumos/tast/remote/bundles/cros/camera/cca_remote.go
@@ -0,0 +1,73 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"io/ioutil"
+	"time"
+
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/remote/bundles/cros/camera/pre"
+	"chromiumos/tast/rpc"
+	pb "chromiumos/tast/services/cros/camerabox"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCARemote,
+		Desc:         "Verifies CCA behavior on remote DUT",
+		Contacts:     []string{"wtlee@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:camerabox"},
+		SoftwareDeps: []string{"arc", "arc_camera3", "chrome", caps.BuiltinCamera},
+		ServiceDeps:  []string{"tast.cros.camerabox.CCAService"},
+		Data:         []string{pre.DocumentScene().DataPath(), "cca_ui.js"},
+		Vars:         []string{"chart"},
+		Pre:          pre.DocumentScene(),
+		Params: []testing.Param{
+			testing.Param{
+				Name:      "document_scanning_back",
+				ExtraAttr: []string{"camerabox_facing_back"},
+				Val:       &pb.CCATestRequest{Test: pb.CCATest_DOCUMENT_SCANNING, Facing: pb.Facing_FACING_BACK},
+				Timeout:   2 * time.Minute,
+			},
+		},
+	})
+}
+
+func CCARemote(ctx context.Context, s *testing.State) {
+	d := s.DUT()
+	testRequest := s.Param().(*pb.CCATestRequest)
+
+	// Connect to the gRPC server on the DUT.
+	cl, err := rpc.Dial(ctx, d, s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the HAL3 service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	scriptContent, err := ioutil.ReadFile(s.DataPath("cca_ui.js"))
+	if err != nil {
+		s.Fatal("Failed to load script: ", err)
+	}
+	testRequest.ScriptContents = []string{string(scriptContent)}
+
+	// Run remote test on DUT.
+	ccaClient := pb.NewCCAServiceClient(cl.Conn)
+	testResponse, err := ccaClient.RunTest(ctx, testRequest)
+	if err != nil {
+		s.Fatal("Remote call RunTest() failed: ", err)
+	}
+
+	// Check test result.
+	switch testResponse.Result {
+	case pb.TestResult_TEST_RESULT_PASSED:
+	case pb.TestResult_TEST_RESULT_FAILED:
+		s.Error("Remote test failed with error message:", testResponse.Error)
+	case pb.TestResult_TEST_RESULT_UNSET:
+		s.Error("Remote test result is unset")
+	}
+}
