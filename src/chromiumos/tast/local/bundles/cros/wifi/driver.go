c50411f73c44791ae94571d78fabe7ade8335c46
Nicolas Norvez <norvez@chromium.org>
2021-06-04 11:24:29 -0700

    wifi.Driver: support atlas-kernelnext
    atlas-kernelnext was recently introduced, add support for it in the WiFi
    driver smoke test.
    
    BUG=b:190207240
    TEST=None
    
    Change-Id: I4ba6e4f1ae17790b33d947bf2cc5455613b1f551
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2940786
    Tested-by: Nicolas Norvez <norvez@chromium.org>
    Auto-Submit: Nicolas Norvez <norvez@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/wifi/driver.go
+++ src/chromiumos/tast/local/bundles/cros/wifi/driver.go
@@ -56,6 +56,7 @@ var expectedWLANDriver = map[string]map[string]string{
 		"4.4":  "wireless/iwl7000/iwlwifi/iwlwifi.ko",
 		"4.14": "wireless/iwl7000/iwlwifi/iwlwifi.ko",
 		"4.19": "wireless/iwl7000/iwlwifi/iwlwifi.ko",
+		"5.4":  "wireless/iwl7000/iwlwifi/iwlwifi.ko",
 	},
 	wlan.Intel22260: {
 		"4.4":  "wireless/iwl7000/iwlwifi/iwlwifi.ko",
