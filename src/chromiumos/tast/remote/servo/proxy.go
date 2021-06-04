14e79202f501d12ffb8df67070356829458ac4f3
Jeremy Bettis <jbettis@google.com>
2021-06-04 11:19:27 -0600

    tast: Changed the servo spec to include ssh port
    When connecting to a skylab machine, it is necessary to use ssh port
    forwarding, and the ssh port for connecting to the servo host will not
    be on port 22.
    
    Changed the servo string to be of the form host:9999:ssh:22, and allow
    for ipv6 addresses such as [::1]:9999:ssh:22, also the port and ssh port
    can be omitted, and the entire servo string can be omitted, and it
    defaults to localhost:9999:ssh:22.
    
    BUG=None
    TEST=Added unit tests, also connected to local DUT and skylab DUT.
    
    Change-Id: Ib00f7af82c55eae1c316a6d010b9a5afa6d507d5

--- src/chromiumos/tast/remote/servo/proxy.go
+++ src/chromiumos/tast/remote/servo/proxy.go
@@ -6,7 +6,9 @@ package servo
 
 import (
 	"context"
+	"fmt"
 	"net"
+	"strconv"
 	"strings"
 	"time"
 
@@ -25,6 +27,67 @@ type Proxy struct {
 	fwd *ssh.Forwarder // nil if servod is running locally
 }
 
+func splitHostPort(servoHostPort string) (string, int, int, error) {
+	host := "localhost"
+	port := 9999
+	sshPort := 22
+
+	hostport := servoHostPort
+	sshParts := strings.SplitN(hostport, ":ssh:", 2)
+	if len(sshParts) == 2 {
+		hostport = sshParts[0]
+		var err error
+		if sshPort, err = strconv.Atoi(sshParts[1]); err != nil {
+			return "", 0, 0, errors.Wrap(err, "parsing servo host ssh port")
+		}
+		if sshPort <= 0 {
+			return "", 0, 0, errors.New("invalid servo host ssh port")
+		}
+	}
+
+	// The port starts after the last colon.
+	i := strings.LastIndexByte(hostport, ':')
+	if i >= 0 {
+		if hostport[0] == '[' {
+			// Expect the first ']' just before the last ':'.
+			end := strings.IndexByte(hostport, ']')
+			if end < 0 {
+				return "", 0, 0, errors.New("missing ']' in address")
+			}
+			switch end + 1 {
+			case len(hostport): // No port
+				if hostport[1:end] != "" {
+					host = hostport[1:end]
+				}
+				return host, port, sshPort, nil
+			case i: // ] before :
+				if hostport[1:end] != "" {
+					host = hostport[1:end]
+				}
+			default:
+				return "", 0, 0, errors.New("servo arg must be of the form hostname:9999 or hostname:9999:ssh:22 or [::1]:9999")
+			}
+		} else {
+			if hostport[:i] != "" {
+				host = hostport[:i]
+			}
+			if strings.IndexByte(host, ':') >= 0 {
+				return "", 0, 0, errors.New("unexpected colon in hostname")
+			}
+		}
+		var err error
+		if port, err = strconv.Atoi(hostport[i+1:]); err != nil {
+			return "", 0, 0, errors.Wrap(err, "parsing servo port")
+		}
+		if port <= 0 {
+			return "", 0, 0, errors.New("invalid servo port")
+		}
+	} else if hostport != "" {
+		host = hostport
+	}
+	return host, port, sshPort, nil
+}
+
 // NewProxy returns a Proxy object for communicating with the servod instance at spec,
 // which takes the same form passed to New (i.e. "host:port").
 //
@@ -32,7 +95,7 @@ type Proxy struct {
 // to the host running servod and servod connections will be forwarded through it.
 // keyFile and keyDir are used for establishing the SSH connection and should
 // typically come from dut.DUT's KeyFile and KeyDir methods.
-func NewProxy(ctx context.Context, spec, keyFile, keyDir string) (newProxy *Proxy, retErr error) {
+func NewProxy(ctx context.Context, servoHostPort, keyFile, keyDir string) (newProxy *Proxy, retErr error) {
 	var pxy Proxy
 	toClose := &pxy
 	defer func() {
@@ -41,25 +104,23 @@ func NewProxy(ctx context.Context, spec, keyFile, keyDir string) (newProxy *Prox
 		}
 	}()
 
+	host, port, sshPort, err := splitHostPort(servoHostPort)
+	if err != nil {
+		return nil, err
+	}
 	// If the servod instance isn't running locally, assume that we need to connect to it via SSH.
-	if !strings.HasPrefix(spec, "localhost:") && !strings.HasPrefix(spec, "127.0.0.1:") {
-		hostname, port, err := net.SplitHostPort(spec)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to parse %q", spec)
-		}
-
+	if (host != "localhost" && host != "127.0.0.1" && host != "::1") || sshPort != 22 {
 		// First, create an SSH connection to the remote system running servod.
 		sopt := ssh.Options{
 			KeyFile:        keyFile,
 			KeyDir:         keyDir,
 			ConnectTimeout: proxyTimeout,
 			WarnFunc:       func(msg string) { testing.ContextLog(ctx, msg) },
+			Hostname:       net.JoinHostPort(host, fmt.Sprint(sshPort)),
+			User:           "root",
 		}
-		// Use the default SSH username and port.
-		if err := ssh.ParseTarget(hostname, &sopt); err != nil {
-			return nil, err
-		}
-		testing.ContextLogf(ctx, "Opening SSH connection to %s", sopt.Hostname)
+		testing.ContextLogf(ctx, "Opening Servo SSH connection to %s", sopt.Hostname)
+		var err error
 		if pxy.hst, err = ssh.New(ctx, &sopt); err != nil {
 			return nil, err
 		}
@@ -71,17 +132,22 @@ func NewProxy(ctx context.Context, spec, keyFile, keyDir string) (newProxy *Prox
 		}()
 		// Next, forward a local port over the SSH connection to the servod port.
 		testing.ContextLog(ctx, "Creating forwarded connection to port ", port)
-		pxy.fwd, err = pxy.hst.NewForwarder("localhost:0", "localhost:"+port,
+		pxy.fwd, err = pxy.hst.NewForwarder("localhost:0", fmt.Sprintf("localhost:%d", port),
 			func(err error) { testing.ContextLog(ctx, "Got servo forwarding error: ", err) })
 		if err != nil {
 			return nil, err
 		}
-		spec = pxy.fwd.LocalAddr().String()
+		var portstr string
+		if host, portstr, err = net.SplitHostPort(pxy.fwd.ListenAddr().String()); err != nil {
+			return nil, err
+		}
+		if port, err = strconv.Atoi(portstr); err != nil {
+			return nil, errors.Wrap(err, "parsing forwarded servo port")
+		}
 	}
 
-	var err error
-	testing.ContextLog(ctx, "Connecting to servod at ", spec)
-	pxy.svo, err = New(ctx, spec)
+	testing.ContextLogf(ctx, "Connecting to servod at %s:%d", host, port)
+	pxy.svo, err = New(ctx, host, port)
 	if err != nil {
 		return nil, err
 	}
@@ -90,16 +156,16 @@ func NewProxy(ctx context.Context, spec, keyFile, keyDir string) (newProxy *Prox
 }
 
 // logServoStatus logs the current servo status from the servo host.
-func logServoStatus(ctx context.Context, hst *ssh.Conn, port string) {
+func logServoStatus(ctx context.Context, hst *ssh.Conn, port int) {
 	// Check if servod is running of the servo host.
-	out, err := hst.Command("servodtool", "instance", "show", "-p", port).CombinedOutput(ctx)
+	out, err := hst.Command("servodtool", "instance", "show", "-p", fmt.Sprint(port)).CombinedOutput(ctx)
 	if err != nil {
 		testing.ContextLogf(ctx, "Servod process is not initialized on the servo-host: %v: %v", err, string(out))
 		return
 	}
 	testing.ContextLogf(ctx, "Servod instance is running on port %v of the servo host", port)
 	// Check if servod is busy.
-	if out, err = hst.Command("dut-control", "-p ", port, "serialname").CombinedOutput(ctx); err != nil {
+	if out, err = hst.Command("dut-control", "-p ", fmt.Sprint(port), "serialname").CombinedOutput(ctx); err != nil {
 		testing.ContextLogf(ctx, "The servod is not responsive or busy: %v: %v", err, string(out))
 		return
 	}
