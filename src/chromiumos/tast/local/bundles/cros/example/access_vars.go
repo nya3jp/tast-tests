aebf1c46572fb6dbf8005a26dc33c34b423bd199
Seewai Fu <seewaifu@google.com>
2021-06-03 17:13:31 -0700

    tast: Add a new test for accessing runtime variables globally --approach 2 (WIP)
    This CL adds a new test to test a new feature which allows test
    libraries to access runtime variables through context.
    In this approach, user can access any runtime variables whether it is
    declared or not.
    
    BUG=b:187795655
    TEST=tast run -build=true hana example.AccessVars
    
    Cq-Depend: chromium:2938405
    Change-Id: I20bc117e893c25c216d7a3d785ee39621d4f6557

---
+++ src/chromiumos/tast/local/bundles/cros/example/access_vars.go
@@ -0,0 +1,45 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+
+	"chromiumos/tast/local/example"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     AccessVars,
+		Desc:     "Access variables",
+		Contacts: []string{"tast-owners@google.com", "seewaifu@chromium.org"},
+		Attr:     []string{"group:mainline", "informational"},
+		// example.AccessVars.foo is defined in tast-tests/vars/example.AccessVars.yaml
+		VarDeps: []string{"example.AccessVars.foo"},
+		Fixture: "exampleFixtureWithVar",
+	})
+}
+
+func AccessVars(ctx context.Context, s *testing.State) {
+	// test for test variables.
+	varName := "example.AccessVars.foo"
+	val, ok := testing.ContextVar(ctx, varName)
+	expectedVal, expectedOk := s.Var(varName)
+	if val != expectedVal || ok != expectedOk {
+		s.Errorf("Got (%q, %v) from ContextVar want (%q, %v)", val, ok, expectedVal, expectedOk)
+	}
+
+	// test for fixture variables.
+	fixtureVarData := s.FixtValue().(*example.VarData)
+	val, ok = testing.ContextVar(ctx, fixtureVarData.Name)
+	if val != fixtureVarData.Val || ok != expectedOk {
+		s.Errorf("Got fixture variable value (%q, %v) from ContextVar want (%q, %v)", val, ok, fixtureVarData.Val, true)
+	}
+	// test for undelcared variables.
+	if val, ok := testing.ContextVar(ctx, "example.AccessVars.globalBoolean"); val != "true" || !ok {
+		s.Errorf("Got gloval variable value (%q, %v) from ContextVar want (%q, %v)", val, ok, "true", true)
+	}
+}
