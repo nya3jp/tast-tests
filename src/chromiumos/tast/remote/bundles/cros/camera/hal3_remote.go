9d3b1245651f15b9b7f9e246bb7e76601a75f273
Moja Hsu <mojahsu@google.com>
2021-06-04 15:33:02 +0800

    camera: Add face detection test running in camerabox
    Add face detection test. We use the face image from ITS scene2_a and
    check if DUT can detect 3 faces.
    
    BUG=b:189016786
    TEST=Pass it by forcing enable face detection on octopus
    
    Change-Id: I14fb9e48f486f83618f93333ee3c82fe315a74e4

--- src/chromiumos/tast/remote/bundles/cros/camera/hal3_remote.go
+++ src/chromiumos/tast/remote/bundles/cros/camera/hal3_remote.go
@@ -6,17 +6,13 @@ package camera
 
 import (
 	"context"
-	"path"
-	"strings"
 	"time"
 
 	"chromiumos/tast/common/media/caps"
-	"chromiumos/tast/dut"
-	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/bundles/cros/camera/camerabox"
 	"chromiumos/tast/remote/bundles/cros/camera/pre"
 	"chromiumos/tast/rpc"
 	pb "chromiumos/tast/services/cros/camerabox"
-	"chromiumos/tast/ssh/linuxssh"
 	"chromiumos/tast/testing"
 )
 
@@ -100,7 +96,7 @@ func HAL3Remote(ctx context.Context, s *testing.State) {
 	d := s.DUT()
 	runTestRequest := s.Param().(*pb.RunTestRequest)
 
-	if err := logTestScene(ctx, d, runTestRequest.Facing, s.OutDir()); err != nil {
+	if err := camerabox.LogTestScene(ctx, d, runTestRequest.Facing, s.OutDir()); err != nil {
 		s.Error("Failed to take a photo of test scene: ", err)
 	}
 
@@ -126,57 +122,4 @@ func HAL3Remote(ctx context.Context, s *testing.State) {
 	case pb.TestResult_TEST_RESULT_UNSET:
 		s.Error("Remote test result is unset")
 	}
-}
-
-// logTestScene takes a photo of test scene as log to debug scene related problem.
-func logTestScene(ctx context.Context, d *dut.DUT, facing pb.Facing, outdir string) (retErr error) {
-	testing.ContextLog(ctx, "Capture scene log image")
-
-	// Release camera unique resource from cros-camera temporarily for taking a picture of test scene.
-	out, err := d.Command("status", "cros-camera").Output(ctx)
-	if err != nil {
-		return errors.Wrap(err, "failed to get initial state of cros-camera")
-	}
-	if strings.Contains(string(out), "start/running") {
-		if err := d.Command("stop", "cros-camera").Run(ctx); err != nil {
-			return errors.Wrap(err, "failed to stop cros-camera")
-		}
-		defer func() {
-			if err := d.Command("start", "cros-camera").Run(ctx); err != nil {
-				if retErr != nil {
-					testing.ContextLog(ctx, "Failed to start cros-camera")
-				} else {
-					retErr = errors.Wrap(err, "failed to start cros-camera")
-				}
-			}
-		}()
-	}
-
-	// Timeout for capturing scene image.
-	captureCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
-	defer cancel()
-
-	// Take a picture of test scene.
-	facingArg := "back"
-	if facing == pb.Facing_FACING_FRONT {
-		facingArg = "front"
-	}
-	const sceneLog = "/tmp/scene.jpg"
-	if err := d.Command(
-		"sudo", "--user=arc-camera", "cros_camera_test",
-		"--gtest_filter=Camera3StillCaptureTest/Camera3DumpSimpleStillCaptureTest.DumpCaptureResult/0",
-		"--camera_facing="+facingArg,
-		"--dump_still_capture_path="+sceneLog,
-	).Run(captureCtx); err != nil {
-		return errors.Wrap(err, "failed to run cros_camera_test to take a scene photo")
-	}
-
-	// Copy result scene log image.
-	if err := linuxssh.GetFile(ctx, d.Conn(), sceneLog, path.Join(outdir, path.Base(sceneLog)), linuxssh.PreserveSymlinks); err != nil {
-		return errors.Wrap(err, "failed to pull scene log file from DUT")
-	}
-	if err := d.Command("rm", sceneLog).Run(ctx); err != nil {
-		return errors.Wrap(err, "failed to clean up scene log file from DUT")
-	}
-	return nil
 }
