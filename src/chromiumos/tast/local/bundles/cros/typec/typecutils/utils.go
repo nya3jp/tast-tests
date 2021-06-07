d4c4f9ae622fe62ae5fb438f3fa76ce721f31c53
Prashant Malani <pmalani@chromium.org>
2021-06-07 15:24:29 -0700

    typec: Update built-in TBT device checks
    Newer platforms have additional inbuilt devices, so update the
    corresponding checks to account for these. While doing so, make a slight
    modification so that the list of devices is maintained in an array.
    
    BUG=b:190430974
    TEST=typec.Mode*.manual passes on relevant platforms.
    
    Change-Id: Idc8b0939565e5a91a5b5336ea2c3e906e93065bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2946095
    Reviewed-by: Jun Sheng <shengjun@chromium.org>
    Tested-by: Prashant Malani <pmalani@chromium.org>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/typec/typecutils/utils.go
+++ src/chromiumos/tast/local/bundles/cros/typec/typecutils/utils.go
@@ -29,6 +29,20 @@ import (
 // The maximum number of USB Type C ports that a Chromebook supports.
 const maxTypeCPorts = 8
 
+// List of built-in Thunderbolt devices enumerated by the OS.
+var builtInTBTDevices = []string{"domain0", "domain1", "0-0", "1-0"}
+
+// BuiltInTBTDevice returns whether the specified name is a built-in Thunderbolt device or not.
+func BuiltInTBTDevice(name string) bool {
+	for _, device := range builtInTBTDevices {
+		if name == device {
+			return true
+		}
+	}
+
+	return false
+}
+
 // CheckTBTDevice is a helper function which checks for a TBT connected device.
 // |expected| specifies whether we want to check for the presence of a TBT device (true) or the
 // absence of one (false).
@@ -40,8 +54,7 @@ func CheckTBTDevice(expected bool) error {
 
 	found := ""
 	for _, file := range files {
-		// Check for non-built-in TBT devices.
-		if file.Name() == "domain0" || file.Name() == "0-0" {
+		if BuiltInTBTDevice(file.Name()) {
 			continue
 		}
 
