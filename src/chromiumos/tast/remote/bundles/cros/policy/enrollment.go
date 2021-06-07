276f16fa619157d23a1ea6fb8d34737a26eea84b
Victor-Gabriel Savu <vsavu@google.com>
2021-06-07 10:25:27 +0200

    policy: Add plain enrollment test.
    Add the policy.Enrollment test that just performs enrollment.
    
    BUG=chromium:1197004
    TEST=tast run <ip> policy.Enrollment
    
    Change-Id: Icc0005c06d7936709619f791f191160b059142bb

---
+++ src/chromiumos/tast/remote/bundles/cros/policy/enrollment.go
@@ -0,0 +1,70 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/remote/policyutil"
+	"chromiumos/tast/rpc"
+	ps "chromiumos/tast/services/cros/policy"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Enrollment,
+		Desc: "Enroll a device without checking policies",
+		Contacts: []string{
+			"vsavu@chromium.org", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		Attr:         []string{"group:enrollment"},
+		SoftwareDeps: []string{"reboot", "chrome"},
+		ServiceDeps:  []string{"tast.cros.policy.PolicyService"},
+		Timeout:      7 * time.Minute,
+	})
+}
+
+func Enrollment(ctx context.Context, s *testing.State) {
+	defer func(ctx context.Context) {
+		if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
+			s.Error("Failed to reset TPM after test: ", err)
+		}
+	}(ctx)
+
+	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Minute)
+	defer cancel()
+
+	if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
+		s.Fatal("Failed to reset TPM: ", err)
+	}
+
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	pJSON, err := json.Marshal(fakedms.NewPolicyBlob())
+	if err != nil {
+		s.Fatal("Failed to serialize policies: ", err)
+	}
+
+	pc := ps.NewPolicyServiceClient(cl.Conn)
+
+	if _, err := pc.EnrollUsingChrome(ctx, &ps.EnrollUsingChromeRequest{
+		PolicyJson: pJSON,
+	}); err != nil {
+		s.Fatal("Failed to enroll using chrome: ", err)
+	}
+	defer pc.StopChromeAndFakeDMS(ctx, &empty.Empty{})
+}
