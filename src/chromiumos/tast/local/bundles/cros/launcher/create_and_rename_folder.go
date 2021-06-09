64c090b8504696f569585e2df69ab888290e22de
Seewai Fu <seewaifu@google.com>
2021-06-08 21:00:19 -0700

    tast-test: Improve stability of launcher.CreateAndRenameFolder (WIP)
    This CL is to improve stability of launcher.CreateAndRenameFolder by
    retrying create folder if it fails to create folder at the first time.
    
    BUG=b:187794323
    TEST=./fast_build.sh -T; tast run -build=true  chromeos6-row17-rack15-host3.cros.corp.google.com launcher.CreateAndRenameFolder.*
    
    Change-Id: I4eb12193ef79d66efdf23ade02d9397d15292129

--- src/chromiumos/tast/local/bundles/cros/launcher/create_and_rename_folder.go
+++ src/chromiumos/tast/local/bundles/cros/launcher/create_and_rename_folder.go
@@ -6,15 +6,17 @@ package launcher
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
-	"chromiumos/tast/local/chrome/ui"
-	"chromiumos/tast/local/chrome/ui/pointer"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/local/chrome/uiauto/mouse"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
@@ -29,7 +31,8 @@ func init() {
 			"chromeos-sw-engprod@google.com",
 			"tbarzic@chromium.org",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr: []string{"group:mainline"},
+		// Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{
 			{
@@ -76,6 +79,8 @@ func CreateAndRenameFolder(ctx context.Context, s *testing.State) {
 	}
 	defer cleanup(ctx)
 
+	ui := uiauto.New(tconn)
+
 	// When a DUT switches from tablet mode to clamshell mode, sometimes it takes a while to settle down.
 	// Tablet mode's home screen has application icons.
 	// On the other hand, clamshell mode's home screen does not have any application icons and users
@@ -83,8 +88,8 @@ func CreateAndRenameFolder(ctx context.Context, s *testing.State) {
 	// Therefore, the following code waits for the icons to go away when changing from tablet mode
 	// to clamshell mode.
 	if originallyEnabled && !tabletMode {
-		params := ui.FindParams{ClassName: launcher.ExpandedItemsClass}
-		if err := ui.WaitUntilGone(ctx, tconn, params, 10*time.Second); err != nil {
+		launcherNode := nodewith.ClassName(launcher.ExpandedItemsClass)
+		if err := ui.EnsureGoneFor(launcherNode, 10*time.Second)(ctx); err != nil {
 			s.Fatal("Failed to wait tablet mode to clamshell mode transition complete: ", err)
 		}
 	}
@@ -94,83 +99,44 @@ func CreateAndRenameFolder(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to open Expanded Application list view: ", err)
 	}
 
-	if err := ui.WaitForLocationChangeCompleted(ctx, tconn); err != nil {
-		s.Fatal("Filed to wait for location changes: ", err)
-	}
-
-	// Create a folder in launcher by dragging one app on top of another.
-	params := ui.FindParams{ClassName: launcher.ExpandedItemsClass}
-	icons, err := ui.FindAll(ctx, tconn, params)
-	if err != nil {
-		s.Fatal("Failed to find all in expanded launcher: ", err)
-	}
-	defer icons.Release(ctx)
-	if len(icons) < 2 {
-		s.Fatal("Not enough icons in expanded launcher to perform test")
-	}
-
 	// Drag one icon to the top of another icon to create a folder.
-	if err := dragIcon(ctx, tconn, icons[0], icons[1]); err != nil {
-		s.Fatalf("Failed to Drag app %q: %v", icons[0].Name, err)
+	if err := createFolder(ctx, tconn, ui); err != nil {
+		s.Fatal("Failed to create folder app: ", err)
 	}
 
 	if err := launcher.RenameFolder(tconn, kb, "Unnamed", "NewName")(ctx); err != nil {
 		s.Fatal("Failed to rename folder to NewName: ", err)
 	}
-
 }
 
-// dragIcon is a helper function to cause a drag of the left button from start to end for tablet.
-func dragIcon(ctx context.Context, tconn *chrome.TestConn, srcIcon, targetIcon *ui.Node) error {
-	start := srcIcon.Location.CenterPoint()
-	end := targetIcon.Location.CenterPoint()
+// createFolder is a helper function to create a folder by dragging an icon on top of the other icon.
+func createFolder(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.Context) error {
+	// Create a folder in launcher by dragging one app on top of another.
+	srcIcon := nodewith.ClassName(launcher.ExpandedItemsClass).First()
+	targetIcon := nodewith.ClassName(launcher.ExpandedItemsClass).Nth(1)
 
-	// First make sure the icon is in focus first to make the test more stable.
-	// If the icon is not in focus, the drag and drop operations fail frequently.
-	if err := srcIcon.FocusAndWait(ctx, 2*time.Second); err != nil {
-		return errors.Wrapf(err, "failed to focus on icon %q", srcIcon.Name)
+	srcInfo, err := ui.Info(ctx, srcIcon)
+	if err != nil {
+		return errors.Wrap(err, "failed get informaton for the source icon")
 	}
 
-	var ctlr pointer.Controller
-	var err error
-	ctlr = pointer.NewMouseController(tconn)
-
-	// Simulate a long press so that the icon is ready to be moved.
-	// It is based on the implementation on ui.LongPress, but this
-	// implementation works for both touch screen and mouse press.
-	// The function ui.LongPress only works for touch screen.
-	// The wait time should be longer than
-	// chrome's default long press wait time, which is 500ms.
-	if err = ctlr.Press(ctx, start); err != nil {
-		return errors.Wrap(err, "failed to click icon")
-	}
-	if err = testing.Sleep(ctx, 1*time.Second); err != nil {
-		return errors.Wrap(err, "failed to long press")
-	}
-	// Move from one icon to the top of another icon.
-	if err = ctlr.Move(ctx, start, end, time.Second); err != nil {
-		return errors.Wrap(err, "failed to drag icon")
+	targetInfo, err := ui.Info(ctx, targetIcon)
+	if err != nil {
+		return errors.Wrap(err, "failed get informaton for the target icon")
 	}
 
-	// The following statement makes sure the icon was moved to the top of another one
-	// in order to prevent the early release of the mouse.
-	if err := ui.WaitForLocationChangeCompleted(ctx, tconn); err != nil {
-		return errors.Wrap(err, "failed to wait for location changes")
+	start, err := ui.Location(ctx, srcIcon)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get locaton for icon %v", srcInfo.Name)
+	}
+	end, err := ui.Location(ctx, targetIcon)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get locaton for icon %v", targetInfo.Name)
 	}
 
-	// Make sure we hold long enough to let the merge animation starts.
-	if err = ctlr.Release(ctx); err != nil {
-		return errors.Wrap(err, "failed to drop icon to target")
-	}
+	folder := nodewith.Name("Folder Unnamed").ClassName(launcher.ExpandedItemsClass)
 
-	// When an icon is dragged and dropped onto another icon, the two icons will
-	// become a single folder icon.
-	// The following code make sure the rearrangement of icons are done because
-	// the next step will click on the folder to rename the folder.
-	// We want to make sure we would click on the correct location of the icon.
-	if err := ui.WaitForLocationChangeCompleted(ctx, tconn); err != nil {
-		return errors.Wrap(err, "failed to wait for location changes")
-	}
-
-	return nil
+	return ui.Retry(3, uiauto.Combine(fmt.Sprintf("createFolder(%s, %s)", srcInfo.Name, targetInfo.Name),
+		mouse.Drag(tconn, start.CenterPoint(), end.CenterPoint(), time.Second*2),
+		ui.WaitUntilExists(folder)))(ctx)
 }
