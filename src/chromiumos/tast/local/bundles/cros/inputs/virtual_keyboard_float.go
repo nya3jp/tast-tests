973805a01d7f82c3e7d3d389cfafe0398d26eb8a
Jun Sheng <shengjun@google.com>
2021-06-10 09:47:26 +1000

    test
    Change-Id: I864e809d103d21facd5af0df403300e356e5c721

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_float.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_float.go
@@ -6,20 +6,13 @@ package inputs
 
 import (
 	"context"
-	"math"
 	"time"
 
 	"chromiumos/tast/ctxutil"
-	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
-	"chromiumos/tast/local/chrome/uiauto/mouse"
-	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/chrome/uiauto/vkb"
-	"chromiumos/tast/local/coords"
 	"chromiumos/tast/testing"
-	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -29,81 +22,35 @@ func init() {
 		Contacts:     []string{"essential-inputs-team@google.com"},
 		Attr:         []string{"group:mainline", "group:input-tools", "informational"},
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
-		Params: []testing.Param{{
-			Name:              "stable",
-			ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
-			ExtraAttr:         []string{"group:input-tools-upstream"},
-		}, {
-			Name:              "unstable",
-			ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
-		}},
+		Timeout:      5 * 60 * time.Minute,
 	})
 }
 
 func VirtualKeyboardFloat(ctx context.Context, s *testing.State) {
 	// Give 5 seconds to clean up and dump out UI tree.
-	cleanupCtx := ctx
-	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
-	defer cancel()
+	for i := 0; i < 10000; i++ {
+		testing.ContextLogf(ctx, "TESTING: %d", i)
+		cleanupCtx := ctx
+		ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+		defer cancel()
 
-	cr, err := chrome.New(ctx, chrome.EnableFeatures("VirtualKeyboardFloatingDefault"), chrome.VKEnabled(), chrome.ExtraArgs("--force-tablet-mode=touch_view"))
-	if err != nil {
-		s.Fatal("Failed to start Chrome: ", err)
-	}
-	defer cr.Close(cleanupCtx)
+		cr, err := chrome.New(ctx, chrome.VKEnabled(), chrome.ExtraArgs("--force-tablet-mode=touch_view"))
+		if err != nil {
+			s.Fatal("Failed to start Chrome: ", err)
+		}
+		defer cr.Close(cleanupCtx)
 
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to create test API connection: ", err)
-	}
-	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+		tconn, err := cr.TestAPIConn(ctx)
+		if err != nil {
+			s.Fatal("Failed to create test API connection: ", err)
+		}
+		defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+		vkbCtx := vkb.NewContext(cr, tconn)
 
-	vkbCtx := vkb.NewContext(cr, tconn)
+		testing.Sleep(ctx, 10*time.Second)
 
-	if err := vkbCtx.ShowVirtualKeyboard()(ctx); err != nil {
-		s.Fatal("Failed to show the virtual keyboard: ", err)
-	}
-
-	kconn, err := vkbCtx.UIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to create connection to virtual keyboard UI: ", err)
-	}
-	defer kconn.Close()
-
-	dragPointFinder := vkb.NodeFinder.Role(role.Button).Name("move keyboard, double tap then drag to reposition the keyboard")
-
-	// Get current center point of drag button.
-	dragLoc, err := uiauto.New(tconn).Location(ctx, dragPointFinder)
-	if err != nil {
-		s.Fatal("Failed to find drag point: ", err)
-	}
-	dragPoint := dragLoc.CenterPoint()
-
-	// Drag float vk to new position.
-	destinationPoint := coords.NewPoint(dragPoint.X-100, dragPoint.Y-100)
-	if err := mouse.Drag(tconn, dragPoint, destinationPoint, time.Second)(ctx); err != nil {
-		s.Fatal("Failed to drag float window: ", err)
-	}
-
-	// Get new center point of drag button.
-	newDragLoc, err := uiauto.New(tconn).Location(ctx, dragPointFinder)
-	if err != nil {
-		s.Fatal("Failed to find drag point: ", err)
-	}
-	newDragPoint := newDragLoc.CenterPoint()
-
-	// When dragging the virtual keyboard to a given location, the actual location it lands on can be slightly different.
-	// e.g. When dragging the virtual keyboard to (1016,762), it can end up at (1015, 762).
-	if math.Abs(float64(newDragPoint.X-destinationPoint.X)) > 3 || math.Abs(float64(newDragPoint.Y-destinationPoint.Y)) > 3 {
-		s.Fatalf("Failed to drag float VK or it did not land at desired location. got: %v, want: %v", newDragPoint, destinationPoint)
-	}
-
-	// Wait for resize handler to be shown.
-	resizeHandleFinder := vkb.NodeFinder.Name("resize keyboard, double tap then drag to resize the keyboard").Role(role.Button)
-
-	// Resizing float vk on some boards are flaky.
-	// Thus only check the handler is shown.
-	if err := uiauto.New(tconn).WaitUntilExists(resizeHandleFinder.First())(ctx); err != nil {
-		s.Fatal("Failed to wait for resize handler to be shown: ", err)
+		if err := vkbCtx.ShowVirtualKeyboard()(ctx); err != nil {
+			s.Fatal("Failed to show VK: ", err)
+		}
 	}
 }
--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_apps.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_apps.go
@@ -1,25 +1,14 @@
-// Copyright 2020 The Chromium OS Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 package inputs
 
 import (
 	"context"
-	"strings"
-	"time"
 
-	"chromiumos/tast/local/apps"
-	"chromiumos/tast/local/bundles/cros/inputs/pre"
-	"chromiumos/tast/local/bundles/cros/inputs/util"
-	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto"
-	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/chrome/uiauto/vkb"
 	"chromiumos/tast/testing"
-	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -29,49 +18,22 @@ func init() {
 		Contacts:     []string{"essential-inputs-team@google.com"},
 		Attr:         []string{"group:mainline", "group:input-tools", "informational"},
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
-		Timeout:      5 * time.Minute,
-		Params: []testing.Param{{
-			Name:              "stable",
-			Pre:               pre.VKEnabledTablet,
-			ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
-			ExtraAttr:         []string{"group:input-tools-upstream"},
-		}, {
-			Name:              "unstable",
-			Pre:               pre.VKEnabledTablet,
-			ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
-		}}})
+	})
 }
 
 func VirtualKeyboardTypingApps(ctx context.Context, s *testing.State) {
-	// typingKeys indicates a key series that tapped on virtual keyboard.
-	// Input string should start with lower case letter because VK layout is not auto-capitalized in the settings search bar.
-	const typingKeys = "language"
-
-	cr := s.PreValue().(pre.PreData).Chrome
-	tconn := s.PreValue().(pre.PreData).TestAPIConn
-
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
-	app := apps.Settings
-	s.Logf("Launching %s", app.Name)
-	if err := apps.Launch(ctx, tconn, app.ID); err != nil {
-		s.Fatalf("Failed to launch %s: %c", app.Name, err)
-	}
-	if err := ash.WaitForApp(ctx, tconn, app.ID, time.Minute); err != nil {
-		s.Fatalf("%s did not appear in shelf after launch: %v", app.Name, err)
+	cr, err := chrome.New(ctx, chrome.VKEnabled(), chrome.ExtraArgs("--force-tablet-mode=touch_view"))
+	if err != nil {
+		s.Fatal(err)
 	}
 
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
 	vkbCtx := vkb.NewContext(cr, tconn)
-	searchFieldFinder := nodewith.Role(role.SearchBox).Name("Search settings")
-	if err := uiauto.Combine("test virtual keyboard input in settings app",
-		vkbCtx.ClickUntilVKShown(searchFieldFinder),
-		vkbCtx.WaitForDecoderEnabled(true),
-		vkbCtx.TapKeys(strings.Split(typingKeys, "")),
-		// Hide virtual keyboard to submit candidate.
-		vkbCtx.HideVirtualKeyboard(),
-		// Validate text.
-		util.WaitForFieldTextToBe(tconn, searchFieldFinder, typingKeys),
-	)(ctx); err != nil {
-		s.Fatal("Failed to verify virtual keyboard input in settings: ", err)
+
+	if err := uiauto.Combine("Show VKB", vkbCtx.ClickUntilVKShown(nodewith.Role(role.TextField)), vkbCtx.WaitForDecoderEnabled(true), vkbCtx.TapKey("a"))(ctx); err != nil {
+		s.Fatal(err)
 	}
 }
