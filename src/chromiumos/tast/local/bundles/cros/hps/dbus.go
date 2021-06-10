dca915b0b7f235160989fe346edfe96f1a3a5ce3
Evan Benn <evanbenn@chromium.org>
2021-06-10 14:55:28 +1000

    hps: Add basic hps dbus method call test
    The hps dbus api is in early stages, but establish this test to set up
    the structure for future work.
    
    BUG=b:190570773
    TEST=tast run grunt hpsd.\*
    
    Change-Id: I2ef12be182f0612b32d4b582e8da0624b7d8fef1

---
+++ src/chromiumos/tast/local/bundles/cros/hps/dbus.go
@@ -0,0 +1,54 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hps
+
+import (
+	"context"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DBus,
+		Desc: "Check that hpsd can be connected to via dbus",
+		Contacts: []string{
+			"evanbenn@chromium.org", // Test author
+			"chromeos-hps-swe@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"hps"},
+	})
+}
+
+func DBus(ctx context.Context, s *testing.State) {
+	const (
+		dbusName      = "org.chromium.Hps"
+		dbusPath      = "/org/chromium/Hps"
+		dbusInterface = "org.chromium.Hps"
+
+		job = "hpsd"
+	)
+
+	s.Logf("Restarting %s job and waiting for %s service", job, dbusName)
+	if err := upstart.RestartJob(ctx, job); err != nil {
+		s.Fatalf("Failed to start %s: %v", job, err)
+	}
+	_, obj, err := dbusutil.Connect(ctx, dbusName, dbus.ObjectPath(dbusPath))
+	if err != nil {
+		s.Fatalf("Failed to connect to %s: %v", dbusName, err)
+	}
+
+	s.Log("Running EnableFeature(0) on hpsd")
+	if err := obj.CallWithContext(ctx, dbusInterface+".EnableFeature", 0, byte(0)).Store(); err != nil {
+		s.Error("EnableFeature failed")
+	} else {
+		s.Log("EnableFeature(0) success")
+	}
+}
