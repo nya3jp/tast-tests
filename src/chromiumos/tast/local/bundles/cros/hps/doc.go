3a382b21213c21d6584eeb0e70fedbe7eb8739b9
Evan Benn <evanbenn@chromium.org>
2021-06-10 10:30:21 +1000

    hps: Add the first hps daemon test
    Test copied from biod. Test checks that hpsd is running via upstart.
    
    BUG=b:190570773
    TEST=cros workon --board grunt start tast-use-flags
    TEST=cros_sdk -- env USE=hps emerge-grunt tast-use-flags
    TEST=cros deploy --root=/usr/local/ grunt tast-use-flags
    TEST=cros_sdk -- env USE=hps tast run grunt hps.\* # PASS
    TEST=ssh grunt stop hpsd
    TEST=cros_sdk -- env USE=hps tast run grunt hps.\* # FAIL
    
    Cq-Depend: chromium:2951601
    Change-Id: If555e463f12846ecc6c551db42624b798b1aa887
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2951463
    Reviewed-by: Andrew McRae <amcrae@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Tested-by: Evan Benn <evanbenn@chromium.org>
    Commit-Queue: Evan Benn <evanbenn@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/hps/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package hps contains local Tast tests that exercise the hps daemon and tool of Chrome OS.
+package hps
---
+++ src/chromiumos/tast/local/bundles/cros/hps/running.go
@@ -0,0 +1,33 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hps
+
+import (
+	"context"
+
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Running,
+		Desc: "Checks that hpsd is running on devices with hps enabled",
+		Contacts: []string{
+			"evanbenn@chromium.org", // Test author
+			"chromeos-hps-swe@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"hps"},
+	})
+}
+
+// Running checks the hpsd job and fails if it isn't running or has a process
+// in the zombie state.
+func Running(ctx context.Context, s *testing.State) {
+	if err := upstart.CheckJob(ctx, "hpsd"); err != nil {
+		s.Fatal("Test failed: ", err)
+	}
+}
