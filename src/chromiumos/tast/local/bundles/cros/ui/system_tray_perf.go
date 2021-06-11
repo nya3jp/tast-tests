aa8e581ac2be90f9ad04fabc172818e672eb5eb5
Andre Le <leandre@chromium.org>
2021-06-11 20:51:01 +0000

    Notification Perf: Record latency when clicking Status Area.
    Modify Tast test to record the latency of clicking Status Area to show
    up Quick Settings and notification bubble. We want to make sure that
    having lots of notification won't create a delay when showing the
    bubble.
    
    BUG=chromium:1219030
    TEST=tast run <IP> ui.SystemTrayPerf
    
    Change-Id: I31c82a450201b25665612b35310dcae5f08f7b07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2957191
    Reviewed-by: Kamil Szarek <kamilszarek@google.com>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Tested-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/system_tray_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/system_tray_perf.go
@@ -6,10 +6,13 @@ package ui
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/ui/perfutil"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/power"
@@ -21,7 +24,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         SystemTrayPerf,
 		Desc:         "Measures animation smoothness of system tray animations",
-		Contacts:     []string{"amehfooz@chromium.org", "tengs@chromium.org", "chromeos-wmp@google.com"},
+		Contacts:     []string{"amehfooz@chromium.org", "leandre@chromium.org", "chromeos-wmp@google.com"},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome"},
 		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
@@ -42,31 +45,54 @@ func SystemTrayPerf(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to connect to test API: ", err)
 	}
 
-	ac := uiauto.New(tconn)
-	// Find and click the StatusArea via UI. Clicking it opens the Ubertray.
-	statusArea := nodewith.ClassName("ash/StatusAreaWidgetDelegate")
-	collapseButton := nodewith.ClassName("CollapseButton")
-	if err := uiauto.Combine(
-		"open the uber tray",
-		ac.LeftClick(statusArea),
-		ac.WaitUntilExists(collapseButton),
-	)(ctx); err != nil {
-		s.Fatal("Failed to open the uber tray: ", err)
+	// Add some notifications so that notification centre shows up when opening Quick Settings.
+	const uiTimeout = 30 * time.Second
+	const n = 5
+	ts := []ash.NotificationType{
+		ash.NotificationTypeBasic,
+		ash.NotificationTypeImage,
+		ash.NotificationTypeProgress,
+		ash.NotificationTypeList,
+	}
+	for i := 0; i <= n; i++ {
+		for _, t := range ts {
+			if _, err := ash.CreateTestNotification(ctx, tconn, t, fmt.Sprintf("Test%sNotification%d", t, i), "test message"); err != nil {
+				s.Fatalf("Failed to create %d-th %s notification: %v", i, t, err)
+			}
+		}
 	}
 
-	// Toggle the collapsed state of the system tray.
+	// Wait for the last notification to finish creating.
+	if _, err := ash.WaitForNotification(ctx, tconn, uiTimeout, ash.WaitTitle(fmt.Sprintf("Test%sNotification%d", ts[len(ts)-1], n))); err != nil {
+		s.Fatal("Failed waiting for notification: ", err)
+	}
+
+	ac := uiauto.New(tconn)
+	statusArea := nodewith.ClassName("ash/StatusAreaWidgetDelegate")
+	collapseButton := nodewith.ClassName("CollapseButton")
+
+	// This includes toggle the Status Area button to record input latency of showing Quick Settings/notification centre
+	// and toggle the collapsed state of the system tray to record animation smoothness.
 	pv := perfutil.RunMultiple(ctx, s, cr, perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
-		return uiauto.Combine(
-			"collapse and expand",
+		if err := uiauto.Combine(
+			"open the uber tray, collapse and expand it, then close it",
+			ac.LeftClick(statusArea),
+			ac.WaitUntilExists(collapseButton),
 			ac.LeftClick(collapseButton),
 			ac.WaitForLocation(collapseButton),
 			ac.LeftClick(collapseButton),
 			ac.WaitForLocation(collapseButton),
-		)(ctx)
+			ac.LeftClick(statusArea),
+			ac.WaitUntilGone(collapseButton),
+		)(ctx); err != nil {
+			return errors.Wrap(err, "failed to open the uber tray, collapse and expand, then close")
+		}
+		return nil
 	},
+		"Ash.StatusAreaShowBubble.PresentationTime",
 		"ChromeOS.SystemTray.AnimationSmoothness.TransitionToCollapsed",
 		"ChromeOS.SystemTray.AnimationSmoothness.TransitionToExpanded"),
-		perfutil.StoreSmoothness)
+		perfutil.StoreAllWithHeuristics(""))
 
 	if err := pv.Save(ctx, s.OutDir()); err != nil {
 		s.Fatal("Failed saving perf data: ", err)
