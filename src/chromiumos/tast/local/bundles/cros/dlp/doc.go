0c9e36a92a39635cc88af6dd44b84c1e2976c76a
Vishal <vishal38785@gmail.com>
2021-06-11 14:37:21 +0530

    tast-tests: test for clipboard restriction of DLP
    Adds test to check whether clipboard restriction allows copy-paste and
    drag-drop of content if restricted in DLP policy
    
    Also, moved dlp tests in seprate dlp folder.
    
    BUG=chromium:1132208
    TEST=tast run <ip> dlp.DataLeakPreventionRulesListPrinting dlp.DataLeakPreventionRulesListScreenshot dlp.DataLeakPreventionRulesListClipboardMisc dlp.DataLeakPreventionRulesListClipboard dlp.DataLeakPreventionRulesListClipboardOmni dlp.DataLeakPreventionRulesListClipboardShelf dlp.DataLeakPreventionRulesListDragdrop
    
    Change-Id: I4637703e9a1c9030c073e2564440cf59708ac79d

---
+++ src/chromiumos/tast/local/bundles/cros/dlp/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package dlp contains local Tast tests that exercise DLP restrictions on confidential content.
+package dlp
