56352f8a590a834604479da9d463a6f259d8666e
Vishal <vishal38785@gmail.com>
2021-06-11 14:37:21 +0530

    tast-tests: test for clipboard restriction of DLP
    Adds test to check whether clipboard restriction allows copy-paste and
    drag-drop of content if restricted in DLP policy
    
    Also, moved dlp tests in separate dlp folder.
    
    BUG=chromium:1132208
    TEST=tast run <ip> dlp.DataLeakPreventionRulesListPrinting dlp.DataLeakPreventionRulesListScreenshot dlp.DataLeakPreventionRulesListClipboardMisc dlp.DataLeakPreventionRulesListClipboard dlp.DataLeakPreventionRulesListClipboardOmni dlp.DataLeakPreventionRulesListClipboardShelf dlp.DataLeakPreventionRulesListDragdrop
    
    Change-Id: I4637703e9a1c9030c073e2564440cf59708ac79d

---
+++ src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_clipboard.go
@@ -0,0 +1,172 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package dlp
+
+import (
+	"context"
+	"strings"
+
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/dlp/clipboard"
+	"chromiumos/tast/local/bundles/cros/dlp/policy"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DataLeakPreventionRulesListClipboard,
+		Desc: "Test behavior of DataLeakPreventionRulesList policy with clipboard blocked restriction by copy and paste",
+		Contacts: []string{
+			"vishal38785@gmail.com", // Test author
+			"chromeos-dlp@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      "chromePolicyLoggedIn",
+	})
+}
+
+func DataLeakPreventionRulesListClipboard(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+	fakeDMS := s.FixtValue().(*fixtures.FixtData).FakeDMS
+
+	// DLP policy with clipboard blocked restriction.
+	policyDLP := policy.GetStandardClipboardDlpPolicy()
+
+	// Update the policy blob.
+	pb := fakedms.NewPolicyBlob()
+	pb.AddPolicies(policyDLP)
+	if err := fakeDMS.WritePolicyBlob(pb); err != nil {
+		s.Fatal("Failed to write policies to FakeDMS: ", err)
+	}
+
+	// Update policy.
+	if err := policyutil.ServeBlobAndRefresh(ctx, fakeDMS, cr, pb); err != nil {
+		s.Fatal("Failed to serve and refresh: ", err)
+	}
+
+	// Connect to Test API.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	keyboard, err := input.VirtualKeyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer keyboard.Close()
+
+	s.Log("Waiting for chrome.clipboard API to become available")
+	if err := tconn.WaitForExpr(ctx, "chrome.clipboard"); err != nil {
+		s.Fatal("chrome.clipboard API unavailable: ", err)
+	}
+
+	for _, param := range []struct {
+		name        string
+		wantAllowed bool
+		url         string
+	}{
+		{
+			name:        "Example",
+			wantAllowed: false,
+			url:         "www.example.com",
+		},
+		{
+			name:        "Company",
+			wantAllowed: false,
+			url:         "www.company.com",
+		},
+		{
+			name:        "Chromium",
+			wantAllowed: true,
+			url:         "www.chromium.org",
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+
+			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
+
+			if _, err = cr.NewConn(ctx, "https://"+param.url); err != nil {
+				s.Error("Failed to open page: ", err)
+			}
+
+			if err := keyboard.Accel(ctx, "Ctrl+A"); err != nil {
+				s.Fatal("Failed to press Ctrl+A to select all content: ", err)
+			}
+
+			if err := keyboard.Accel(ctx, "Ctrl+C"); err != nil {
+				s.Fatal("Failed to press Ctrl+C to copy content: ", err)
+			}
+
+			copiedString, err := clipBoardText(ctx, tconn)
+			if err != nil {
+				s.Fatal("Failed to get clipboard content: ", err)
+			}
+
+			if _, err = cr.NewConn(ctx, "https://www.google.com/"); err != nil {
+				s.Error("Failed to open page: ", err)
+			}
+
+			// Google.com have property of autofocus for content to be pasted.
+			if err := keyboard.Accel(ctx, "Ctrl+V"); err != nil {
+				s.Fatal("Failed to press Ctrl+V to paste content: ", err)
+			}
+
+			ui := uiauto.New(tconn)
+
+			// Verify Notification Bubble.
+			notification := clipboard.CheckClipboardBubble(ctx, ui, param.url)
+
+			if !param.wantAllowed && notification != nil {
+				s.Fatal("Couldn't check for notification: ", notification)
+			}
+
+			// Check Pasted content.
+			pastedError := checkPastedContent(ctx, ui, copiedString)
+
+			if param.wantAllowed && pastedError != nil {
+				s.Fatal("Couldn't check for pasted content: ", pastedError)
+			}
+
+			if (!param.wantAllowed && pastedError == nil) || (param.wantAllowed && notification == nil) {
+				s.Fatal("Content pasted, expected restriction")
+			}
+
+		})
+	}
+}
+
+func clipBoardText(ctx context.Context, tconn *chrome.TestConn) (string, error) {
+	var clipData string
+	if err := tconn.Eval(ctx, `tast.promisify(chrome.autotestPrivate.getClipboardTextData)()`, &clipData); err != nil {
+		return "", errors.Wrap(err, "failed to get clipboard content: ")
+	}
+	return clipData, nil
+}
+
+func checkPastedContent(ctx context.Context, ui *uiauto.Context, content string) error {
+
+	words := strings.Fields(content)
+	content = strings.Join(words[:10], " ")
+
+	contentNode := nodewith.NameContaining(content).Role(role.InlineTextBox).First()
+
+	if err := uiauto.Combine("Pasted ",
+		ui.WaitUntilExists(contentNode))(ctx); err != nil {
+		return errors.Wrap(err, "failed to check for pasted content: ")
+	}
+
+	return nil
+}
