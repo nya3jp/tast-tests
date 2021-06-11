88e6bd76fcbbda9052956945bfd93b126f08f5a9
Vishal <vishal38785@gmail.com>
2021-06-11 14:37:21 +0530

    tast-tests: test for clipboard restriction of DLP
    Adds test to check whether clipboard restriction allows copy-paste and
    drag-drop of content if restricted in DLP policy
    
    BUG=chromium:1132208
    TEST=tast run <ip> policy.DataLeakPreventionRulesListDragdrop policy.DataLeakPreventionRulesListClipboard policy.DataLeakPreventionRulesListClipboardMisc policy.DataLeakPreventionRulesListClipboardShelf policy.DataLeakPreventionRulesListClipboardOmni
    
    Change-Id: I4637703e9a1c9030c073e2564440cf59708ac79d

---
+++ src/chromiumos/tast/local/bundles/cros/dlp/clipboard/clipboard.go
@@ -0,0 +1,61 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package clipboard contains functionality shared by tests that
+// exercise Clipboard restrictions of DLP.
+package clipboard
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+)
+
+// ClipText returns clipboard content.
+func ClipText(ctx context.Context, tconn *chrome.TestConn) (string, error) {
+	var clipData string
+	if err := tconn.Eval(ctx, `tast.promisify(chrome.autotestPrivate.getClipboardTextData)()`, &clipData); err != nil {
+		return "", errors.Wrap(err, "failed to get clipboard content: ")
+	}
+	return clipData, nil
+}
+
+// CheckPasteNode checks if paste node exists.
+func CheckPasteNode(ctx context.Context, tconn *chrome.TestConn) error {
+	ui := uiauto.New(tconn)
+
+	pasteNode := nodewith.Name("Paste Ctrl+V").Role(role.MenuItem)
+
+	if err := uiauto.Combine("Check paste node greyed",
+		ui.WaitUntilExists(pasteNode))(ctx); err != nil {
+		return errors.Wrap(err, "failed to check paste node greyed: ")
+	}
+
+	return nil
+}
+
+// CheckClipboardBubble checks if clipboard restriction bubble exists.
+func CheckClipboardBubble(ctx context.Context, tconn *chrome.TestConn, url string) error {
+
+	ui := uiauto.New(tconn)
+	bubbleView := nodewith.ClassName("ClipboardDlpBubble").Role(role.Window)
+	bubbleClass := nodewith.ClassName("ClipboardBlockBubble").Ancestor(bubbleView)
+	bubbleButton := nodewith.Name("Got it").Role(role.Button).Ancestor(bubbleClass)
+	messageBlocked := "Pasting from " + url + " to this location is blocked by administrator policy"
+	bubble := nodewith.Name(messageBlocked).Role(role.StaticText).Ancestor(bubbleClass)
+
+	if err := uiauto.Combine("Bubble ",
+		ui.WaitUntilExists(bubbleView),
+		ui.WaitUntilExists(bubbleButton),
+		ui.WaitUntilExists(bubbleClass),
+		ui.WaitUntilExists(bubble))(ctx); err != nil {
+		return errors.Wrap(err, "failed to check for notification bubble existence: ")
+	}
+
+	return nil
+}
