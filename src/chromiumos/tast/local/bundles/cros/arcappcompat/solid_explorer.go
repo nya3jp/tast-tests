8ae496a9e18abdd6b416e74a7dfff53c33070e4a
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-06-11 18:33:06 -0700

    arcappcompat: Implement test suite in tast.arcappcompat tests.
    Currently arcappcompat tests has a separate test suite for every
    app test. Implemented common Testsuite for arcappcompat tests
    which will be shared among 100 apps. Testsuite consists of
    launchApp test which handles app specific login code for every
    app, Common tests will have test cases that are common for all
    the apps and app specific tests will have test cases like Video
    Playback test case and sign out test case for required apps.
    
    Added the name as “clamshell_mode” for clamshell tests, renamed
    name of “vm” to “vm_clamshell_mode” for readability.Updated ID
    for checkUIElement for TouchScreenScroll and MouseClick tests.
    Considering the defaultTestCaseTimeout, reduced seed value for
    MouseScrollAction, TouchAndTextInputs,KeyboardNavigations and
    TouchAndPlayVideo tests.
    
    BUG=b:190231029
    
    TEST=test_that --board=eve <eve> tast.appcompat-shard-0
         test_that --board=eve <eve> tast.appcompat-shard-1
         test_that --board=eve <eve> tast.appcompat-shard-2
         test_that --board=hatch <helios> tast.appcompat-shard-0
         test_that --board=hatch <helios> tast.appcompat-shard-1
         test_that --board=hatch <helios> tast.appcompat-shard-2
    
    Change-Id: I9876d90e70433d02c3951c1e7e68877e85ec39b4

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/solid_explorer.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/solid_explorer.go
@@ -17,46 +17,53 @@ import (
 	"chromiumos/tast/testing"
 )
 
-// ClamshellTests are placed here.
-var clamshellTestsForSolidExplorer = []testutil.TestCase{
+// clamshellLaunchForSolidExplorer launches SolidExplorer in clamshell mode.
+var clamshellLaunchForSolidExplorer = []testutil.TestSuite{
 	{Name: "Launch app in Clamshell", Fn: launchAppForSolidExplorer},
-	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
-	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
-	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
-	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
 }
 
-// TouchviewTests are placed here.
-var touchviewTestsForSolidExplorer = []testutil.TestCase{
+// touchviewLaunchForSolidExplorer launches SolidExplorer in tablet mode.
+var touchviewLaunchForSolidExplorer = []testutil.TestSuite{
 	{Name: "Launch app in Touchview", Fn: launchAppForSolidExplorer},
-	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
-	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
 }
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         SolidExplorer,
 		Desc:         "Functional test for SolidExplorer that installs the app also verifies it is logged in and that the main page is open, checks SolidExplorer correctly changes the window state in both clamshell and touchview mode",
-		Contacts:     []string{"archanasing@chromium.org", "cros-appcompat-test-team@google.com"},
+		Contacts:     []string{"cros-appcompat-test-team@google.com"},
 		Attr:         []string{"group:appcompat"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
-			Val:               clamshellTestsForSolidExplorer,
+			Name: "clamshell_mode",
+			Val: testutil.TestParams{
+				Tests:      clamshellLaunchForSolidExplorer,
+				CommonTest: testutil.ClamshellCommonTests,
+			},
 			ExtraSoftwareDeps: []string{"android_p"},
 			Pre:               pre.AppCompatBooted,
 		}, {
-			Name:              "tablet_mode",
-			Val:               touchviewTestsForSolidExplorer,
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				Tests:      touchviewLaunchForSolidExplorer,
+				CommonTest: testutil.TouchviewCommonTests,
+			},
 			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
 			Pre:               pre.AppCompatBootedInTabletMode,
 		}, {
-			Name:              "vm",
-			Val:               clamshellTestsForSolidExplorer,
+			Name: "vm_clamshell_mode",
+			Val: testutil.TestParams{
+				Tests:      clamshellLaunchForSolidExplorer,
+				CommonTest: testutil.ClamshellCommonTests,
+			},
 			ExtraSoftwareDeps: []string{"android_vm"},
 			Pre:               pre.AppCompatBooted,
 		}, {
-			Name:              "vm_tablet_mode",
-			Val:               touchviewTestsForSolidExplorer,
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				Tests:      touchviewLaunchForSolidExplorer,
+				CommonTest: testutil.TouchviewCommonTests,
+			},
 			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
 			Pre:               pre.AppCompatBootedInTabletMode,
 		}},
@@ -72,8 +79,8 @@ func SolidExplorer(ctx context.Context, s *testing.State) {
 		appPkgName  = "pl.solidexplorer2"
 		appActivity = "pl.solidexplorer.SolidExplorer"
 	)
-	testCases := s.Param().([]testutil.TestCase)
-	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+	testSet := s.Param().(testutil.TestParams)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testSet)
 }
 
 // launchAppForSolidExplorer verifies SolidExplorer is logged in and
