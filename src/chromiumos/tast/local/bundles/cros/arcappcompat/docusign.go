ffeab0b557ff58b2fd8bcc4771d607a4868fc3d2
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-06-11 18:33:06 -0700

    arcappcompat: Implement test suite in tast.arcappcompat tests.
    Testsuite for arcappcompat tests consists of launchApp test
    which has app specific login code for every app, Common tests
    has test cases that are common for all the apps and app specific
    tests has Video Playback testcase and signout testcases for required
    apps.
    Added the name as “clamshell_mode” for clamshell tests, renamed
    name of “vm” to “vm_clamshell_mode” for readability.
    Updated ID for checkUIElement for TouchScreenScroll and MouseClick.
    Considering the defaultTestCaseTimeout, reduced seed value for
    MouseScrollAction, TouchAndTextInputs,KeyboardNavigations and
    TouchAndPlayVideo tests.
    
    BUG=b:190231029
    
    TEST=tast run <eve> arcappcompat.ABCKids.clamshell_mode
    tast run <eve> arcappcompat.ABCKids.tablet_mode
    tast run <helios> arcappcompat.ABCKids.vm_clamshell_mode
    tast run <helios> arcappcompat.ABCKids.vm_tablet_mode
    
    Change-Id: I9876d90e70433d02c3951c1e7e68877e85ec39b4

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/docusign.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/docusign.go
@@ -18,20 +18,14 @@ import (
 	"chromiumos/tast/testing"
 )
 
-// ClamshellTests are placed here.
-var clamshellTestsForDocusign = []testutil.TestCase{
+// clamshellLaunchForDocusign launches Docusign in clamshell mode.
+var clamshellLaunchForDocusign = []testutil.TestSuite{
 	{Name: "Launch app in Clamshell", Fn: launchAppForDocusign},
-	{Name: "Clamshell: Fullscreen app", Fn: testutil.ClamshellFullscreenApp},
-	{Name: "Clamshell: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
-	{Name: "Clamshell: Reopen app", Fn: testutil.ReOpenWindow},
-	{Name: "Clamshell: Resize window", Fn: testutil.ClamshellResizeWindow},
 }
 
-// TouchviewTests are placed here.
-var touchviewTestsForDocusign = []testutil.TestCase{
+// touchviewLaunchForDocusign launches Docusign in tablet mode.
+var touchviewLaunchForDocusign = []testutil.TestSuite{
 	{Name: "Launch app in Touchview", Fn: launchAppForDocusign},
-	{Name: "Touchview: Minimise and Restore", Fn: testutil.MinimizeRestoreApp},
-	{Name: "Touchview: Reopen app", Fn: testutil.ReOpenWindow},
 }
 
 func init() {
@@ -42,22 +36,35 @@ func init() {
 		Attr:         []string{"group:appcompat"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
-			Val:               clamshellTestsForDocusign,
+			Name: "clamshell_mode",
+			Val: testutil.TestParams{
+				Tests:      clamshellLaunchForDocusign,
+				CommonTest: testutil.ClamshellCommonTests,
+			},
 			ExtraSoftwareDeps: []string{"android_p"},
 			Pre:               pre.AppCompatBooted,
 		}, {
-			Name:              "tablet_mode",
-			Val:               touchviewTestsForDocusign,
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				Tests:      touchviewLaunchForDocusign,
+				CommonTest: testutil.TouchviewCommonTests,
+			},
 			ExtraSoftwareDeps: []string{"android_p", "tablet_mode"},
 			Pre:               pre.AppCompatBootedInTabletMode,
 		}, {
-			Name:              "vm",
-			Val:               clamshellTestsForDocusign,
+			Name: "vm_clamshell_mode",
+			Val: testutil.TestParams{
+				Tests:      clamshellLaunchForDocusign,
+				CommonTest: testutil.ClamshellCommonTests,
+			},
 			ExtraSoftwareDeps: []string{"android_vm"},
 			Pre:               pre.AppCompatBooted,
 		}, {
-			Name:              "vm_tablet_mode",
-			Val:               touchviewTestsForDocusign,
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				Tests:      touchviewLaunchForDocusign,
+				CommonTest: testutil.TouchviewCommonTests,
+			},
 			ExtraSoftwareDeps: []string{"android_vm", "tablet_mode"},
 			Pre:               pre.AppCompatBootedInTabletMode,
 		}},
@@ -74,8 +81,8 @@ func Docusign(ctx context.Context, s *testing.State) {
 		appPkgName  = "com.docusign.ink"
 		appActivity = ".HomeActivity"
 	)
-	testCases := s.Param().([]testutil.TestCase)
-	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testCases)
+	testSet := s.Param().(testutil.TestParams)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testSet)
 }
 
 // launchAppForDocusign verifies Docusign is logged in and
