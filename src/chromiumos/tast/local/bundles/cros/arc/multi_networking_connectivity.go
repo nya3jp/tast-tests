2c26b358de4a8591ae672187d6f70a93025f14ca
Jason Jeremy Iman <jasongustaman@google.com>
2021-06-13 01:55:07 +0900

    tast: multi_networking_connectivity: Rework the test for ARCVM
    Prior to this patch the test does:
    1. Ping gateway address of each physical interface from a
       specific ARC interface (using ping -I option).
    2. Ping ARC interface from the host and verify that the ping
       is coming from the right interface (using tcpdump).
    
    ARCVM currently does not support commands running from shell
    with root permission, without doing 'adb root' (which requires
    the build to be a test build).
    
    This patch removes "-I" from (1) and "tcpdump" from (2).
    This patch allows the test to be run for both ARC and ARCVM.
    
    BUG=b:168011514
    TEST=tast run <DUT> arc.MultiNetworkingConnectivity
    TEST=tast run <DUT> arc.MultiNetworkingConnectivity.vm
    
    Change-Id: Ie4538049a28ea2993fdd8aa9d1d86a8fee1e8801

--- src/chromiumos/tast/local/bundles/cros/arc/multi_networking_connectivity.go
+++ src/chromiumos/tast/local/bundles/cros/arc/multi_networking_connectivity.go
@@ -5,17 +5,12 @@
 package arc
 
 import (
-	"bufio"
 	"context"
-	"net"
 	"regexp"
 	"strings"
 	"time"
 
-	"golang.org/x/sync/errgroup"
-
 	"chromiumos/tast/common/testexec"
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/network"
 	"chromiumos/tast/testing"
@@ -27,46 +22,66 @@ func init() {
 		Desc:         "Checks connectivity while multi-networking is enabled",
 		Contacts:     []string{"jasongustaman@google.com", "arc-eng@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"android_p", "chrome"},
+		SoftwareDeps: []string{"chrome"},
 		Fixture:      "arcBooted",
+		Params: []testing.Param{
+			{
+				ExtraSoftwareDeps: []string{"android_p"},
+			}, {
+				Name:              "vm",
+				ExtraSoftwareDeps: []string{"android_vm"},
+			},
+		},
 	})
 }
 
 // ifnameIPRegex is a regex that extracts interface name and IP from 'ip -o addr show'.
-var ifnameIPRegex = regexp.MustCompile(`^\d+:\s+(\S+)\s+\S+\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
+var ifnameIPRegex = regexp.MustCompile(`^\d+:\s+\S+\s+\S+\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
 
 func MultiNetworkingConnectivity(ctx context.Context, s *testing.State) {
 	const (
-		// outTimeout defines the maximum allowed time for outbound connectivity check.
-		outTimeout = 10 * time.Second
-
-		// inTimeout defines the maximum allowed time for inbound connectivity check.
-		inTimeout = 30 * time.Second
+		// timeout defines the maximum allowed time for connectivity check.
+		timeout = 10 * time.Second
 	)
 
-	// This code tests outbound network from within Android (ARC).
-	// It fetches ARC network interfaces, and for each of the interface,
-	// test if a ping to Google DNS can be made.
-	ifnames, err := arc.NetworkInterfaceNames(ctx)
-	if err != nil {
-		s.Fatal("Failed to get ARC interface list: ", err)
+	// Hardcoded mapping of ARCVM interface name to host interface name.
+	ifMap := map[string]string{
+		"eth0":   "eth1",
+		"eth1":   "eth2",
+		"wlan0":  "eth3",
+		"wlan1":  "eth4",
+		"rmnet0": "eth5",
 	}
 
-	// Ensure that outbound networking works for each network interface inside ARC.
-	// We are skipping interfaces:
-	// - that is guest-only ("arc0" and "arc1"),
-	// - without outbound networking ("lo"),
-	// - that is not up.
-	for _, ifname := range ifnames {
-		if ifname == arc.Loopback || ifname == arc.ARC0 || ifname == arc.ARC1 {
-			continue
-		}
+	// Get the ARC instance.
+	a := s.FixtValue().(*arc.PreData).ARC
 
-		if ifup, err := interfaceUp(ifname); err != nil {
+	vmEnabled, err := arc.VMEnabled()
+	if err != nil {
+		s.Fatal("Unable to check install type of ARC: ", err)
+	}
+
+	ifnames, err := network.PhysicalInterfaces(ctx)
+	if err != nil {
+		s.Fatal("Failed to get physical interface list: ", err)
+	}
+
+	// Ensure that inbound and outbound networking works for each network interface inside ARC for each host's physical interfaces.
+	for _, ifname := range ifnames {
+		if ifup, err := interfaceUp(ctx, ifname); err != nil {
 			s.Errorf("Failed checking if %s is up: %s", ifname, err)
 			continue
 		} else if !ifup {
 			continue
+		}
+
+		arcIfname := ifname
+		if vmEnabled {
+			var ok bool
+			arcIfname, ok = ifMap[ifname]
+			if !ok {
+				continue
+			}
 		}
 
 		gateway, err := network.Gateway(ctx, ifname)
@@ -75,154 +90,48 @@ func MultiNetworkingConnectivity(ctx context.Context, s *testing.State) {
 			continue
 		}
 
-		s.Log("Pinging using ", ifname)
+		// This code tests outbound network from within Android (ARC).
+		// It fetches ARC network interfaces, and for each of the interface,
+		// test if a ping to Google DNS can be made.
+		s.Logf("Pinging from ARC interface %s through host interface %s", arcIfname, ifname)
 		if err := testing.Poll(ctx, func(ctx context.Context) error {
-			return arc.BootstrapCommand(ctx, "/system/bin/ping", "-c1", "-w1", "-I", ifname, gateway).Run()
-		}, &testing.PollOptions{Timeout: outTimeout}); err != nil {
+			return a.Command(ctx, "ping", "-c1", "-w1", gateway).Run()
+		}, &testing.PollOptions{Timeout: timeout}); err != nil {
 			s.Errorf("Failed outbound check for interface %s: %s", ifname, err)
 		}
-	}
 
-	// This code tests inbound network of Android (ARC). It checks if a ping from
-	// Chrome OS host can be made to each corresponding network interface of ARC.
-	// Currently, it does not check if ARC inbound network works from outside of
-	// the host.
-
-	// iface stores the IP of host bridge network interface ("arc_" prefix)
-	// and IP of ARC network interface (no prefix) of a corresponding device
-	// network interface (no prefix)
-	type iface struct {
-		bridgeIP string // Host bridge network interface IP address
-		arcIP    string // ARC network interface IP address
-	}
-
-	// Get host network interfaces.
-	h, err := net.Interfaces()
-	if err != nil {
-		s.Fatal("Failed to get host interface list: ", err)
-	}
-
-	// Get the bridge interfaces names and IPs (filters from host network interface).
-	ifaces := make(map[string]iface)
-	for _, ifc := range h {
-		n := strings.TrimPrefix(ifc.Name, arc.BridgePrefix)
-		if n == ifc.Name {
-			continue
+		// Get the Android (ARC) interface IP.
+		out, err := a.Command(ctx, "ip", "-o", "addr", "show", arcIfname, "scope", "global").Output(testexec.DumpLogOnError)
+		if err != nil {
+			s.Fatal("Failed to get Android interface list: ", err)
 		}
-		if addrs, err := ifc.Addrs(); err != nil {
-			s.Errorf("Failed to get %s interface address: %s", ifc.Name, err)
-		} else if len(addrs) > 0 {
-			ifaces[n] = iface{bridgeIP: addrs[0].(*net.IPNet).IP.String()}
-		}
-	}
-
-	// Get the Android (ARC) interfaces names and IPs.
-	out, err := s.FixtValue().(*arc.PreData).ARC.Command(ctx, "/system/bin/ip", "-o", "addr", "show", "scope", "global").Output(testexec.DumpLogOnError)
-	if err != nil {
-		s.Fatal("Failed to get Android interface list: ", err)
-	}
-
-	// ip -o addr show returns network interfaces with the following format
-	// space (' ') denotes whitespaces
-	// "id" "interface_name" "protocol" "ip/netmask" ...
-	// e.g. "103: arc0    inet 100.115.92.2/30 ..."
-	// Parse output of "ip -o addr show" to get interface name and ip.
-	for _, o := range strings.Split(strings.TrimSpace(string(out)), "\n") {
-		m := ifnameIPRegex.FindStringSubmatch(o)
-		if m == nil {
-			continue
-		}
-		name := m[1]
-		ifc := ifaces[name]
-		ifc.arcIP = m[2]
-		ifaces[name] = ifc
-	}
-
-	// Ping Android (ARC) interfaces from Chrome OS host and check for inbound traffic
-	// Ensures that ARC can receive each ping and it comes from the right interface.
-	s.Log("Pinging ARC interfaces")
-
-	// Create a shorter context for inbound traffic check.
-	watchCtx, watchCancel := context.WithTimeout(ctx, inTimeout)
-	defer watchCancel()
-
-	g, watchCtx := errgroup.WithContext(watchCtx)
-	for ifname, ifc := range ifaces {
-		if ifc.arcIP == "" || ifname == arc.ARC0 || ifname == arc.ARC1 {
-			continue
-		}
-
-		if ifup, err := interfaceUp(ifname); err != nil {
-			s.Errorf("Failed checking if %s is up: %s", ifname, err)
-			continue
-		} else if !ifup {
-			continue
-		}
-
-		ifname, ifc := ifname, ifc // https://golang.org/doc/faq#closures_and_goroutines
-		g.Go(func() error {
-			if err := checkNetInterface(watchCtx, ifname, ifc.arcIP, ifc.bridgeIP); err != nil {
-				return errors.Errorf("failed to get ping from the right interface for %s, %s", ifname, err)
+		var arcIP string
+		for _, o := range strings.Split(strings.TrimSpace(string(out)), "\n") {
+			m := ifnameIPRegex.FindStringSubmatch(o)
+			if m == nil {
+				continue
 			}
-			return nil
-		})
-	}
-	if err := g.Wait(); err != nil {
-		s.Fatal("Failed inbound traffic check: ", err)
-	}
-}
-
-// checkNetInterface runs a ping command from Chrome OS host to Android (ARC) for a network interface
-// "ifname" and checks if the right ping went through by checking the IP.
-// It ensures that the ARC and bridge network interface is created properly.
-func checkNetInterface(ctx context.Context, ifname, arcIP, bridgeIP string) error {
-	// Starts a ping command to "arcIP".
-	ping := testexec.CommandContext(ctx, "ping", "-i0.3", arcIP)
-	if err := ping.Start(); err != nil {
-		return errors.Wrap(err, "failed to start ping")
-	}
-	defer ping.Wait()
-	defer ping.Kill()
-
-	// Starts a tcpdump process that writes messages to stdout on new network messages.
-	tcpdump := arc.BootstrapCommand(ctx, "/system/xbin/tcpdump", "-i", ifname)
-
-	stdout, err := tcpdump.StdoutPipe()
-	if err != nil {
-		return err
-	}
-
-	if err := tcpdump.Start(); err != nil {
-		return errors.Wrap(err, "failed to start tcpdump")
-	}
-
-	// sc.Scan() below might block. Release bufio.Scanner by killing tcpdump if the
-	// process execution time exceeds context deadline.
-	go func() {
-		defer tcpdump.Wait()
-		defer tcpdump.Kill()
-
-		// Blocks until deadline is passed.
-		<-ctx.Done()
-	}()
-
-	// Watch and wait until tcpdump output "bridgeIP".
-	sc := bufio.NewScanner(stdout)
-	for {
-		if err := ctx.Err(); err != nil {
-			return err
+			arcIP = m[1]
+			break
 		}
-		if sc.Scan() && strings.Contains(sc.Text(), bridgeIP) {
-			return nil
+
+		// This code tests inbound network to Android (ARC).
+		// It fetches ARC network interfaces, and for each of the interface,
+		// test if a ping the interface is possible from the host.
+		s.Logf("Pinging from host interface %s to ARC interface %s", ifname, arcIfname)
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			return testexec.CommandContext(ctx, "ping", "-c1", "-w1", arcIP).Run()
+		}, &testing.PollOptions{Timeout: timeout}); err != nil {
+			s.Errorf("Failed inbound check for interface %s: %s", ifname, err)
 		}
 	}
 }
 
 // interfaceUp returns true if the network interface is up.
-func interfaceUp(ifname string) (bool, error) {
-	iface, err := net.InterfaceByName(ifname)
+func interfaceUp(ctx context.Context, ifname string) (bool, error) {
+	out, err := testexec.CommandContext(ctx, "cat", "/sys/class/net/"+ifname+"/operstate").Output()
 	if err != nil {
 		return false, err
 	}
-	return strings.Contains(iface.Flags.String(), "up"), nil
+	return strings.TrimSpace(string(out)) == "up", nil
 }
