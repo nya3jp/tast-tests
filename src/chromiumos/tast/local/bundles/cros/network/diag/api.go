4cb8c864bcc57acd3b4feba86d29bc15052d8867
Arowa Suliman <arowa@google.com>
2021-06-13 16:12:59 -0700

    Tast: [COIL] use inclusive terms in Tast.
    Replace oppressive terms in Tast with inclusive terms.
    
    BUG=b:187792819
    TEST=none
    
    Change-Id: Id5fb6584f19446db501d9d8ad75ef8506946a52f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2958781
    Tested-by: Arowa Suliman <arowa@chromium.org>
    Reviewed-by: Laurent Chavey <chavey@google.com>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Commit-Queue: Arowa Suliman <arowa@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/network/diag/api.go
+++ src/chromiumos/tast/local/bundles/cros/network/diag/api.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package diag is a library of functionality to utilize the native Chrome
+// Package diag is a library of functionality to utilize the built-in Chrome
 // network diagnostic routines.
 package diag
 
