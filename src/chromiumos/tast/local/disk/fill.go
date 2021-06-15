d98ed99b5fc1d2632c908ada6188f3b43ed46cc2
Jason Lin <lxj@google.com>
2021-06-15 16:43:22 +1000

    Add a disk.Refiller type to make (re-)adjusting disk space easier
    This is going to be used by https://crrev.com/i/3910284
    
    BUG=b:190687683
    TEST=tast run -buildbundle=crosint pita.LowDiskLaunchResume
    
    Change-Id: I8717e4b003abdc73d6f3f0734ae0eacb733f5b9c

--- src/chromiumos/tast/local/disk/fill.go
+++ src/chromiumos/tast/local/disk/fill.go
@@ -6,6 +6,7 @@ package disk
 
 import (
 	"io/ioutil"
+	"os"
 	"syscall"
 
 	"chromiumos/tast/errors"
@@ -13,10 +14,10 @@ import (
 
 // Fill creates a temporary file in a directory that fills the disk by
 // allocating it, but without performing any actual IO to write the content.
-func Fill(path string, tofill uint64) (string, error) {
-	file, err := ioutil.TempFile(path, "fill.*.dat")
+func Fill(dir string, tofill uint64) (string, error) {
+	file, err := ioutil.TempFile(dir, "fill.*.dat")
 	if err != nil {
-		return "", errors.Wrapf(err, "failed to create temp file in %s", path)
+		return "", errors.Wrapf(err, "failed to create temp file in %s", dir)
 	}
 	defer file.Close()
 
@@ -30,10 +31,10 @@ func Fill(path string, tofill uint64) (string, error) {
 
 // FillUntil reates a temporary file in a directory that fills the disk until
 // less than remaining bytes are available.
-func FillUntil(path string, remaining uint64) (string, error) {
-	freeSpace, err := FreeSpace(path)
+func FillUntil(dir string, remaining uint64) (string, error) {
+	freeSpace, err := FreeSpace(dir)
 	if err != nil {
-		return "", errors.Wrapf(err, "failed to read free space in %s", path)
+		return "", errors.Wrapf(err, "failed to read free space in %s", dir)
 	}
 
 	if freeSpace < remaining {
@@ -42,5 +43,58 @@ func FillUntil(path string, remaining uint64) (string, error) {
 
 	tofill := freeSpace - remaining
 
-	return Fill(path, tofill)
+	return Fill(dir, tofill)
+}
+
+// Refiller maintains the temporary fill file for you, so you can easily
+// (re-)adjust the disk space.
+type Refiller struct {
+	dir string
+	// The name of the temporary file, or empty if there isn't one.
+	filename string
+}
+
+// NewRefiller creates a new refiller. `dir` is the directory to put the
+// temporary file.
+func NewRefiller(dir string) Refiller {
+	return Refiller{dir: dir, filename: ""}
+}
+
+// Close removes the temporary file if there is one. You can call it as many
+// times as you want, and you probably want to also call it at the end of your
+// tast.
+func (r *Refiller) Close() error {
+	if r.filename != "" {
+		if err := os.Remove(r.filename); err != nil {
+			return errors.Wrapf(err, "Unable to remove the temporary file %v/%v", r.dir, r.filename)
+		}
+		r.filename = ""
+	}
+	return nil
+}
+
+// Refill removes the temporary file previously created, and then fill the
+// disk. See `Fill()`.
+func (r *Refiller) Refill(tofill uint64) error {
+	return r.refillImpl(Fill, tofill)
+}
+
+// RefillUntil removes the temporary file previously created, and then fill the disk. See
+// `FillUntil()`.
+func (r *Refiller) RefillUntil(remaining uint64) error {
+	return r.refillImpl(FillUntil, remaining)
+}
+
+func (r *Refiller) refillImpl(fillFunc func(string, uint64) (string, error), bytes uint64) error {
+	if err := r.Close(); err != nil {
+		return err
+	}
+
+	filename, err := fillFunc(r.dir, bytes)
+	if err != nil {
+		return err
+	}
+
+	r.filename = filename
+	return nil
 }
