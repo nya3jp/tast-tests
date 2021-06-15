257f31ddbd409e7b9b5d117e4fd533a6396c8bbe
David Stevens <stevensd@chromium.org>
2021-06-15 10:15:29 +0900

    Mark apps.LaunchGallery.stable as informational
    The test has been very flaky recently. Mark as informational until the
    regression is fixed.
    
    BUG=b:190742769
    TEST=tast run DUT apps.LaunchGallery.stable
    
    Change-Id: If1df30943aafedee440f932bcc117ff4062cf5e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2962033
    Tested-by: David Stevens <stevensd@chromium.org>
    Tested-by: Jun Sheng <shengjun@chromium.org>
    Reviewed-by: David Stevens <stevensd@chromium.org>
    Reviewed-by: Jun Sheng <shengjun@chromium.org>
    Commit-Queue: Jun Sheng <shengjun@chromium.org>
    Commit-Queue: David Stevens <stevensd@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/apps/launch_gallery.go
+++ src/chromiumos/tast/local/bundles/cros/apps/launch_gallery.go
@@ -43,6 +43,7 @@ func init() {
 		Params: []testing.Param{
 			{
 				Name:              "stable",
+				ExtraAttr:         []string{"informational"},
 				ExtraHardwareDeps: hwdep.D(pre.AppsStableModels),
 			}, {
 				Name:              "unstable",
