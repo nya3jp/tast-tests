ff39bbd6d0927bb1ea1823a42ee6fb6403b83335
Darren Shen <shend@chromium.org>
2021-06-17 10:02:10 +1000

    tast: Add candidate tests to Japanese PK.
    Test manipulating the candidate window via Tab, Arrow Keys and
    Number keys.
    
    BUG=b:190248965,b:190248867
    TEST=nocturne
    
    Change-Id: I1f7127bf49348372dfe06ffa6aed1baca0a87184

--- src/chromiumos/tast/local/bundles/cros/inputs/util/util.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/util/util.go
@@ -12,7 +12,11 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 )
+
+// PKCandidatesFinder is the finder for candidates in the IME candidates window.
+var PKCandidatesFinder = nodewith.Role(role.ImeCandidate)
 
 // InputEval is a data structure to define common input function and expected out.
 type InputEval struct {
@@ -40,4 +44,16 @@ func WaitForFieldTextToBe(tconn *chrome.TestConn, finder *nodewith.Finder, expec
 		}
 		return nil
 	})
+}
+
+// GetNthCandidateText returns the candidate text in the specified position in the candidates window.
+func GetNthCandidateText(ctx context.Context, tconn *chrome.TestConn, n int) (string, error) {
+	ui := uiauto.New(tconn)
+
+	candidate, err := ui.Info(ctx, PKCandidatesFinder.Nth(n))
+	if err != nil {
+		return "", err
+	}
+
+	return candidate.Name, nil
 }
