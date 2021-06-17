fb45d09dbc78a77607eb3be073f1b07638feffba
Swapnil <swapnilgupta@google.com>
2021-06-17 17:32:19 +0200

    tast-tests: add policy.ExtensionInstallEventLoggingEnabled
    Add test for the ExtensionInstallEventLoggingEnabled policy.
    
    BUG= chromium:1099749, b/170108629
    TEST=tast run <ip> policy.ExtensionInstallEventLoggingEnabled
    
    Change-Id: I985ee478869d5cea0ddd2e6ca73a187f7385fb45

---
+++ src/chromiumos/tast/local/bundles/cros/policy/extension_install_event_logging_enabled.go
@@ -0,0 +1,169 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/syslog"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/timing"
+)
+
+type eventType string
+
+const (
+	policyRequest      eventType = "POLICY_REQUEST"
+	success            eventType = "SUCCESS"
+	cancelled          eventType = "CANCELED"
+	connectivityChange eventType = "CONNECTIVITY_CHANGE"
+	sessionStateChange eventType = "SESSION_STATE_CHANGE"
+	installationFailed eventType = "INSTALLATION_FAILED"
+	unknown            eventType = "EXTENSION_INSTALL_STATUS_UNKNOWN"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ExtensionInstallEventLoggingEnabled,
+		Desc: "Behavior of ExtensionInstallEventLoggingEnabled policy",
+		Contacts: []string{
+			"swapnilgupta@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		Vars:         []string{"policy.ExtensionInstallEventLoggingEnabled.username", "policy.ExtensionInstallEventLoggingEnabled.password"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "enrolled",
+		Timeout:      chrome.GAIALoginTimeout + 3*time.Minute,
+	})
+}
+
+func ExtensionInstallEventLoggingEnabled(ctx context.Context, s *testing.State) {
+	// The user has the ExtensionInstallEventLoggingEnabled policy set.
+	username := s.RequiredVar("policy.ExtensionInstallEventLoggingEnabled.username")
+	password := s.RequiredVar("policy.ExtensionInstallEventLoggingEnabled.password")
+
+	cr, err := chrome.New(ctx,
+		chrome.GAIALogin(chrome.Creds{User: username, Pass: password}), chrome.ProdPolicy(),
+		chrome.KeepState(),
+		chrome.ExtraArgs("--enable-features=EncryptedReportingPipeline", "--install-log-fast-upload-for-tests", "--extension-install-event-chrome-log-for-tests"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	// Connect to Test API to use it with the UI library.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	const (
+		extensionID = "hoppbgdeajkagempifacalpdapphfoai"
+		downloadURL = "https://chrome.google.com/webstore/detail/platformkeys-test-extensi/" + extensionID
+	)
+
+	sconn, err := cr.NewConn(ctx, downloadURL)
+	if err != nil {
+		s.Fatal("Failed to connect to the extension page: ", err)
+	}
+	defer sconn.Close()
+
+	// If the extension is installed, the Installed button will be present which is not clickable.
+	installedButtonParams := ui.FindParams{
+		Role: ui.RoleTypeButton,
+		Name: "Installed",
+	}
+
+	node, err := ui.FindWithTimeout(ctx, tconn, installedButtonParams, 15*time.Second)
+	if err != nil {
+		s.Fatal("Finding button node failed: ", err)
+	}
+	defer node.Release(ctx)
+
+	// Check if required sequence appears in chrome log.
+	s.Log("Wait for events logged")
+	if err := waitForLoggedEvents(ctx, cr, extensionID); err != nil {
+		s.Fatal("Required events not logged: ", err)
+	}
+}
+
+// statusCodeToEvent converts status code to eventType. Should be in sync with device_management_backend.proto in chrome.
+func statusCodeToEvent(code string) eventType {
+	statusCodeMap := map[string]eventType{
+		"1": policyRequest,
+		"2": success,
+		"3": cancelled,
+		"4": connectivityChange,
+		"5": sessionStateChange,
+		"6": installationFailed,
+	}
+	event, ok := statusCodeMap[code]
+	if !ok {
+		event = unknown
+	}
+	return event
+}
+
+// readLoggedEvents reads logged events from /var/log/chrome/chrome file.
+func readLoggedEvents(extensionId string) ([]eventType, error) {
+	logContent, err := ioutil.ReadFile(syslog.ChromeLogFile)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read "+syslog.ChromeLogFile)
+	}
+
+	r := regexp.MustCompile(fmt.Sprintf(`Add extension install event: %s, (.*)`, extensionId))
+	matches := r.FindAllStringSubmatch(string(logContent), -1)
+	if matches == nil {
+		return nil, nil
+	}
+
+	var events []eventType
+	for _, m := range matches {
+		events = append(events, statusCodeToEvent(m[1]))
+	}
+	return events, nil
+}
+
+// waitForLoggedEvents waits for desired sequence to appear in chrome log.
+func waitForLoggedEvents(ctx context.Context, cr *chrome.Chrome, extensionId string) error {
+	var expectedEvents = []eventType{sessionStateChange, policyRequest, success}
+
+	ctx, st := timing.Start(ctx, "wait_logged_events")
+	defer st.End()
+
+	return testing.Poll(ctx, func(ctx context.Context) error {
+		loggedEvents, err := readLoggedEvents(extensionId)
+		if err != nil {
+			return testing.PollBreak(errors.Wrap(err, "failed to read chrome log"))
+		}
+
+		eventsMap := make(map[eventType]bool)
+		for _, e := range loggedEvents {
+			eventsMap[e] = true
+		}
+
+		for _, expected := range expectedEvents {
+			if !eventsMap[expected] {
+				var strEvents []string
+				for _, e := range loggedEvents {
+					strEvents = append(strEvents, string(e))
+				}
+				return errors.New("incomplete sequence: " + strings.Join(strEvents[:], ","))
+			}
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 60 * time.Second})
+}
