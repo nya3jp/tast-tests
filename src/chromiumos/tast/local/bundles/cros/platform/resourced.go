2ea1a9e87d32c913fc50415661839a1db824f3fc
Kuo-Hsin Yang <vovoy@chromium.org>
2021-06-17 17:37:14 +0800

    tast-tests: Add baseline variation to platform.Resourced
    The baseline variation only tests the minimal functionality of
    resourced. The baseline variation will be a mandatory test for CQ.
    
    BUG=b:178162453
    TEST=tast run $DUT_IP platform.Resourced.baseline
    
    Change-Id: Iedf5f282685407f182a508ac5a6c2bc4d0444e98

--- src/chromiumos/tast/local/bundles/cros/platform/resourced.go
+++ src/chromiumos/tast/local/bundles/cros/platform/resourced.go
@@ -13,6 +13,10 @@ import (
 	"chromiumos/tast/testing"
 )
 
+type resourcedTestParams struct {
+	isBaseline bool
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         Resourced,
@@ -21,6 +25,16 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Timeout:      3 * time.Minute,
+		Params: []testing.Param{{
+			Val: resourcedTestParams{
+				isBaseline: false,
+			},
+		}, {
+			Name: "baseline",
+			Val: resourcedTestParams{
+				isBaseline: true,
+			},
+		}},
 	})
 }
 
@@ -120,9 +134,16 @@ func checkMemoryPressureSignal(ctx context.Context) error {
 }
 
 func Resourced(ctx context.Context, s *testing.State) {
+	// Baseline checks.
 	if err := checkSetGameMode(ctx); err != nil {
 		s.Fatal("Checking SetGameMode failed: ", err)
 	}
+
+	if s.Param().(resourcedTestParams).isBaseline {
+		return
+	}
+
+	// Other checks.
 	if err := checkQueryMemoryStatus(ctx); err != nil {
 		s.Fatal("Querying memory status failed: ", err)
 	}
