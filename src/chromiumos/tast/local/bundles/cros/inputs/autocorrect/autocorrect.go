f096a29ac1eb436726a08c64eb933dbf6e5b926b
Bao-Duy Tran <tranbaoduy@chromium.org>
2021-06-17 18:44:07 +1000

    Extract autocorrect util from PK test for VK reuse.
    BUG=b:190791183,b:190791811,b:190790409,b:190789154
    TEST=Tast-on-VM
    
    Change-Id: Ie15e4f8d681e9b4d245219e1f4a38622a74309c9

---
+++ src/chromiumos/tast/local/bundles/cros/inputs/autocorrect/autocorrect.go
@@ -0,0 +1,24 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package autocorrect contains common defs shared by Autocorrect-related tests.
+package autocorrect
+
+// UndoMethod enum corresponds to ways autocorrect can be undone.
+type UndoMethod int
+
+// Possible entries of UndoMethod enum.
+const (
+	ViaBackspace UndoMethod = iota
+	ViaPopupUsingPK
+	ViaPopupUsingMouse
+)
+
+// TestCase struct encapsulates parameters for each Autocorrect test.
+type TestCase struct {
+	InputMethodID string
+	MisspeltWord  string
+	CorrectWord   string
+	UndoMethod    UndoMethod
+}
