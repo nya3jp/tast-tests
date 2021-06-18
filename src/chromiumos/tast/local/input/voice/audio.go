196bacbf0b554655a0b50748938e5dcf3f556d88
Alvin Jia <alvinjia@google.com>
2021-06-18 18:40:05 +1000

    Fix VK voice test issue caused by Aloop nodes
    Current VK voice tests are flaky because:
    
    1, It fails to activate Aloop output node.
    2. The Aloop nodes are not properly activated via cras API.
    
    Revise the tests to activate Aloop nodes for both input and output.
    
    Remove retry since the root cause has been identified.
    
    Refactor audio.go to activate Aloop nodes for input/output using
    quicksettings following the example at audio.UIInput* tests.
    
    Move audio.go file to tast/local/input/voice. Leaving it in the
    original place causes problem of cycle references,
    since it uses quicksettings.
    
    BUG=b:191086453, b:188489307
    TEST=tast run <test-device-ip> inputs.VirtualKeyboardSpeech*
    
    Change-Id: Iaeb45634c51fc7e28aed4c054f51f0c9ddf2b927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2972012
    Commit-Queue: Alvin Jia <alvinjia@google.com>
    Tested-by: Alvin Jia <alvinjia@google.com>
    Reviewed-by: Matt Stark <msta@google.com>
    Reviewed-by: Jun Sheng <shengjun@chromium.org>

---
+++ src/chromiumos/tast/local/input/voice/audio.go
@@ -0,0 +1,100 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package voice provides functionality related to voice inputs
+package voice
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/audio"
+	"chromiumos/tast/local/audio/crastestclient"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui/quicksettings"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+)
+
+// AudioFromFile inputs an audio file via active input node and waits for its completion.
+func AudioFromFile(ctx context.Context, audioFilePath string) error {
+	audioInput := audio.TestRawData{
+		Path:          audioFilePath,
+		BitsPerSample: 16,
+		Channels:      2,
+		Rate:          48000,
+		Frequencies:   []int{440, 440},
+		Volume:        0.05,
+	}
+
+	// Playback function by CRAS.
+	playCmd := crastestclient.PlaybackFileCommand(
+		ctx, audioInput.Path,
+		audioInput.Duration,
+		audioInput.Channels,
+		audioInput.Rate)
+	playCmd.Start()
+
+	return playCmd.Wait()
+}
+
+// EnableAloop loads and enables Aloop then sets it as active input/output node.
+// TODO(b/191602192): [CRAS] Issues in using CRAS api to activate Aloop nodes.
+// When the above issue is resolved, using CRAS api instead of ui interactions.
+func EnableAloop(ctx context.Context, tconn *chrome.TestConn) (func(ctx context.Context), error) {
+	// Load ALSA loopback module.
+	unload, err := audio.LoadAloop(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to load ALSA loopback module")
+	}
+
+	// Activate the Aloop nodes.
+	if err := activateAloopNodes(ctx, tconn); err != nil {
+		// Unload ALSA loopback if any following setups failed.
+		unload(ctx)
+		return nil, err
+	}
+
+	return unload, nil
+}
+
+func activateAloopNodes(ctx context.Context, tconn *chrome.TestConn) error {
+	cleanupCtx, shortCancel := ctxutil.Shorten(ctx, 2*time.Second)
+	defer shortCancel()
+	if err := quicksettings.Show(ctx, tconn); err != nil {
+		return errors.Wrap(err, "failed to show Quick Settings: ")
+	}
+	defer quicksettings.Hide(cleanupCtx, tconn)
+
+	if err := selectAudioOption(ctx, tconn, "Loopback Playback"); err != nil {
+		return errors.Wrap(err, "failed to select ALSA loopback output: ")
+	}
+
+	if err := selectAudioOption(ctx, tconn, "Loopback Capture"); err != nil {
+		return errors.Wrap(err, "failed to select ALSA loopback input: ")
+	}
+
+	return nil
+}
+
+func selectAudioOption(ctx context.Context, tconn *chrome.TestConn, device string) error {
+	if err := quicksettings.OpenAudioSettings(ctx, tconn); err != nil {
+		return err
+	}
+
+	ui := uiauto.New(tconn)
+	option := nodewith.Role(role.CheckBox).Name(device)
+
+	if err := ui.MakeVisible(option)(ctx); err != nil {
+		return errors.Wrapf(err, "failed to focus the %v audio option", device)
+	}
+	if err := ui.LeftClick(option)(ctx); err != nil {
+		return errors.Wrapf(err, "failed to click %v audio option", device)
+	}
+
+	return nil
+}
