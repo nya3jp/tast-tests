9df61b8d0a1b16494c189f6bbb42547372967d15
Tetsui Ohkubo <tetsui@chromium.org>
2021-06-21 17:16:02 +0900

    tast-tests: Add test for image copy & paste
    This CL adds arc.ImagePaste, which tests P0 CUJ of ARC image copy &
    paste app compat.
    
    * As the feature is only available on R, there is no .vm variant.
    * image_paste_sample.png is resized version of capybara.jpg in the same
      directory. The browser only supports image/png for clipboard.write().
    
    UX one pager: go/arc-image-cp-app-compat-onepager
    design proposal: go/arc-image-cp-app-compat-design-proposal
    
    BUG=b:188861982
    TEST=tast run rammus-arc-r arc.ImagePaste
    
    Change-Id: I28a6cfb71ffcd5b82b2c8e98672ee48158f9c481

---
+++ android/ArcImagePasteTest/src/org/chromium/arc/testapp/imagepaste/MainActivity.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.imagepaste;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends Activity implements ImageEditText.Listener {
+    private static final String TAG = "ArcImagePasteTest";
+
+    private TextView mCounterView;
+    private ImageView mImageView;
+    private ImageEditText mInputField;
+
+    private int mCounter = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        mCounterView = findViewById(R.id.counter);
+        mImageView = findViewById(R.id.image_view);
+        mInputField = findViewById(R.id.input_field);
+        mInputField.setListener(this);
+    }
+
+    @Override
+    public void onContentUri(Uri uri) {
+        // Alternatively, setImageURI can be used, but we don't want errors to be siletly ignored.
+        try (InputStream in = getContentResolver().openInputStream(uri)) {
+            mImageView.setImageBitmap(BitmapFactory.decodeStream(in));
+            mImageView.invalidate();
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "FileNotFoundException", e);
+            return;
+        } catch (IOException e) {
+            Log.e(TAG, "IOException", e);
+            return;
+        } catch (SecurityException e) {
+            Log.e(TAG, "SecurityException ", e);
+            return;
+        }
+        ++mCounter;
+        mCounterView.setText(Integer.toString(mCounter));
+    }
+}
