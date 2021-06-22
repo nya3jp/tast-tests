27b1cf4650ee6da4c4a111aba1b5ae51e896dbd3
David Haddock <dhaddock@google.com>
2021-06-22 16:14:49 -0700

    Remove duplicate lacros test.
    Seems like Yuke and I wrote the same test two weeks apart back in
    Decembeer. Mine was first :P but I'll get rid of it since the other one
    is more known :)
    
    BUG=None
    TEST=None
    
    Change-Id: I464e02f34895dedd62529cbb706e547487df02c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2981114
    Tested-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Hyung Tae Kim <hyungtaekim@chromium.org>
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Commit-Queue: David Haddock <dhaddock@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/launcher/search_built_in_apps.go
+++ src/chromiumos/tast/local/bundles/cros/launcher/search_built_in_apps.go
@@ -8,11 +8,10 @@ import (
 	"context"
 
 	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/launcher"
 	"chromiumos/tast/local/input"
-	"chromiumos/tast/local/lacros"
-	lacroslauncher "chromiumos/tast/local/lacros/launcher"
 	"chromiumos/tast/testing"
 )
 
@@ -30,31 +29,13 @@ func init() {
 			{
 				Val:     apps.Settings,
 				Fixture: "chromeLoggedIn",
-			},
-			{
-				Name:              "lacros",
-				Val:               apps.Lacros,
-				Fixture:           "lacrosStartedByDataUI",
-				ExtraAttr:         []string{"informational"},
-				ExtraData:         []string{lacroslauncher.DataArtifact},
-				ExtraSoftwareDeps: []string{"lacros"},
 			}},
 	})
 }
 
 // SearchBuiltInApps searches for the Settings app in the Launcher.
 func SearchBuiltInApps(ctx context.Context, s *testing.State) {
-	// TODO(crbug.com/1127165): Remove the artifactPath argument when we can use Data in fixtures.
-	if s.Param().(apps.App) == apps.Lacros {
-		if err := lacroslauncher.EnsureLacrosChrome(ctx, s.FixtValue().(lacroslauncher.FixtData), s.DataPath(lacroslauncher.DataArtifact)); err != nil {
-			s.Fatal("Failed to extract lacros binary: ", err)
-		}
-	}
-
-	cr, err := lacros.GetChrome(ctx, s.FixtValue())
-	if err != nil {
-		s.Fatal("Failed to get a Chrome instance: ", err)
-	}
+	cr := s.FixtValue().(*chrome.Chrome)
 	app := s.Param().(apps.App)
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
