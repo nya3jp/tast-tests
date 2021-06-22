15ed3e0a37ce423a9f727bc078e1aaa5d1d3acb2
John Palmer <jopalmer@chromium.org>
2021-06-22 10:58:47 +1000

    Make emoji test critical
    BUG=None
    TEST=CQ
    
    Change-Id: I29215198c7397349ab343f1bb629cae45523b0c7

--- src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_emoji.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_emoji.go
@@ -23,7 +23,7 @@ func init() {
 		Func:         PhysicalKeyboardEmoji,
 		Desc:         "Checks that right click input field and select emoji with physical keyboard",
 		Contacts:     []string{"jopalmer@chromium.org", "essential-inputs-team@google.com"},
-		Attr:         []string{"group:mainline", "group:input-tools", "informational"},
+		Attr:         []string{"group:mainline", "group:input-tools"},
 		SoftwareDeps: []string{"chrome"},
 		Pre:          pre.NonVKClamshell,
 		Params: []testing.Param{{
