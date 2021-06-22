4edef1260a5f237f37f881f80e39872009e18bec
Yen-lin Lai <yenlinlai@chromium.org>
2021-06-22 15:49:52 +0800

    wificell: Start migrating Precondition to Fixture.
    Add Fixture implementation and register one Fixture as example.
    
    In the following CLs, we'll gradually replace existing preconditions.
    
    BUG=b:187369955
    TEST=None
    
    Change-Id: If39f41501db9085bbe5c9d852bffd4c56f476c79

---
+++ src/chromiumos/tast/remote/wificell/fixture.go
@@ -0,0 +1,124 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wificell
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/remote/wificell/wifiutil"
+	"chromiumos/tast/testing"
+)
+
+// Timeout for methods of Tast fixture.
+const (
+	// We might need to reboot broken DUT in setup or teardown, give
+	// them enough time to do so.
+	setUpTimeout    = 5 * time.Minute
+	tearDownTimeout = 5 * time.Minute
+	resetTimeout    = 10 * time.Second
+	postTestTimeout = 5 * time.Second
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name: "wificellFixtWithCapture",
+		Desc: "Wificell setup with pcap for each configured AP",
+		Contacts: []string{
+			"chromeos-wifi-champs@google.com", // WiFi oncall rotation; or http://b/new?component=893827
+		},
+		Impl:            newTastFixture(TFFeaturesCapture),
+		SetUpTimeout:    setUpTimeout,
+		ResetTimeout:    resetTimeout,
+		PostTestTimeout: postTestTimeout,
+		TearDownTimeout: tearDownTimeout,
+		ServiceDeps:     []string{TFServiceName},
+		Vars:            []string{"router", "pcap"},
+	})
+}
+
+// tastFixtureImpl is the implementation of Tast fixture for wificell setup.
+// Notice the difference of tastFixture and TestFixture objects. The "fixture"
+// of former is the one in the Tast framework, while the one of the latter is
+// is for the fixtures in wificell setup.
+type tastFixtureImpl struct {
+	features TFFeatures
+	tf       *TestFixture
+}
+
+func newTastFixture(features TFFeatures) *tastFixtureImpl {
+	return &tastFixtureImpl{
+		features: features,
+	}
+}
+
+func (f *tastFixtureImpl) companionName(s *testing.FixtState, suffix string) string {
+	name, err := s.DUT().CompanionDeviceHostname(suffix)
+	if err != nil {
+		s.Fatal("Unable to synthesize name, err: ", err)
+	}
+	return name
+}
+
+// recoverUnhealthyDUT checks if the DUT is healthy. If not, try to recover it
+// with reboot.
+func (f *tastFixtureImpl) recoverUnhealthyDUT(ctx context.Context, s *testing.FixtState) error {
+	return recoverUnhealthyDUT(ctx, s.DUT(), s.RPCHint(), &f.tf)
+}
+
+func (f *tastFixtureImpl) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	if err := f.recoverUnhealthyDUT(ctx, s); err != nil {
+		s.Fatal("Failed to recover unhealthy DUT: ", err)
+	}
+
+	tf, err := setUpTestFixture(ctx, s.FixtContext(), s.DUT(), s.RPCHint(), f.features, s.Var)
+	if err != nil {
+		s.Fatal("Failed to set up test fixture: ", err)
+	}
+	f.tf = tf
+
+	return f.tf
+}
+
+func (f *tastFixtureImpl) TearDown(ctx context.Context, s *testing.FixtState) {
+	// Ensure DUT is healthy here again, so that we don't leave with
+	// bad state to later tests/tasks.
+	if err := f.recoverUnhealthyDUT(ctx, s); err != nil {
+		s.Fatal("Failed to recover unhealthy DUT: ", err)
+	}
+
+	if f.tf == nil {
+		return
+	}
+	if err := f.tf.Close(ctx); err != nil {
+		s.Log("Failed to tear down test fixture, err: ", err)
+	}
+	f.tf = nil
+}
+
+func (f *tastFixtureImpl) Reset(ctx context.Context) error {
+	var firstErr error
+	// Light-weight health check here. SetUp/TearDown will try to recover
+	// the DUT when anything goes wrong.
+	if _, err := f.tf.WifiClient().HealthCheck(ctx, &empty.Empty{}); err != nil {
+		wifiutil.CollectFirstErr(ctx, &firstErr, err)
+	}
+	if err := f.tf.Reinit(ctx); err != nil {
+		wifiutil.CollectFirstErr(ctx, &firstErr, err)
+	}
+	return firstErr
+}
+
+func (f *tastFixtureImpl) PreTest(ctx context.Context, s *testing.FixtTestState) {
+	// Nothing to do here for now.
+}
+
+func (f *tastFixtureImpl) PostTest(ctx context.Context, s *testing.FixtTestState) {
+	if err := f.tf.CollectLogs(ctx); err != nil {
+		s.Log("Error collecting logs, err: ", err)
+	}
+}
---
+++ src/chromiumos/tast/remote/wificell/fixture_util.go
@@ -0,0 +1,129 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wificell
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/wifi"
+	"chromiumos/tast/testing"
+)
+
+// This file contains the utilities shared by Precondition and Fixture.
+// We might merge these inline once we deprecated Precondition and improve the code structure.
+
+// dutHealthCheck checks if the DUT is healthy.
+func dutHealthCheck(ctx context.Context, d *dut.DUT, rpcHint *testing.RPCHint) error {
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	// We create a new gRPC session here to exclude broken gRPC case and save reboots when
+	// the DUT is healthy but the gRPC is broken.
+	rpcClient, err := rpc.Dial(ctx, d, rpcHint, "cros")
+	if err != nil {
+		return errors.Wrap(err, "cannot create gRPC client")
+	}
+	defer rpcClient.Close(ctx)
+
+	wifiClient := wifi.NewShillServiceClient(rpcClient.Conn)
+	if _, err := wifiClient.HealthCheck(ctx, &empty.Empty{}); err != nil {
+		return errors.Wrap(err, "health check failed")
+	}
+	return nil
+}
+
+// recoverUnhealthyDUT checks if the DUT is healthy. If not, close existing
+// TestFxiture and try to recover it with reboot.
+func recoverUnhealthyDUT(ctx context.Context, d *dut.DUT, rpcHint *testing.RPCHint, existingTF **TestFixture) error {
+	if err := dutHealthCheck(ctx, d, rpcHint); err != nil {
+		testing.ContextLog(ctx, "Rebooting the DUT due to health check err: ", err)
+		// As reboot will at least break tf.rpc, no reason to keep
+		// the existing p.tf. Close it before reboot.
+		if existingTF != nil && *existingTF != nil {
+			testing.ContextLog(ctx, "Close TestFixture before reboot")
+			if err := (*existingTF).Close(ctx); err != nil {
+				testing.ContextLog(ctx, "Failed to close TestFixture before DUT reboot recovery: ", err)
+			}
+			*existingTF = nil
+		}
+		if err := d.Reboot(ctx); err != nil {
+			return errors.Wrap(err, "reboot failed")
+		}
+	}
+	return nil
+}
+
+func companionName(d *dut.DUT, suffix string) (string, error) {
+	return d.CompanionDeviceHostname(suffix)
+}
+
+func setUpTestFixture(ctx, daemonCtx context.Context, d *dut.DUT, rpcHint *testing.RPCHint, features TFFeatures, getVar func(string) (string, bool)) (*TestFixture, error) {
+	// Create TestFixture.
+	var ops []TFOption
+	// Read router/pcap variable. If not available or empty, NewTestFixture
+	// will fall back to Default{Router,Pcap}Host.
+	if features&TFFeaturesRouters != 0 {
+		if routers, ok := getVar("routers"); ok && routers != "" {
+			testing.ContextLog(ctx, "routers: ", routers)
+			slice := strings.Split(routers, ",")
+			if len(slice) < 2 {
+				return nil, errors.New("must provide at least two router names when Routers feature is enabled")
+			}
+			ops = append(ops, TFRouter(slice...))
+		} else {
+			var routers []string
+			for _, suffix := range []string{dut.CompanionSuffixRouter, dut.CompanionSuffixPcap} {
+				name, err := companionName(d, suffix)
+				if err != nil {
+					return nil, err
+				}
+				routers = append(routers, name)
+
+			}
+			testing.ContextLog(ctx, "companion routers: ", routers)
+			ops = append(ops, TFRouter(routers...))
+		}
+	} else {
+		router, ok := getVar("router")
+		if ok && router != "" {
+			testing.ContextLog(ctx, "router: ", router)
+			ops = append(ops, TFRouter(router))
+		} // else: let TestFixture resolve the name.
+	}
+	pcap, ok := getVar("pcap")
+	if ok && pcap != "" {
+		testing.ContextLog(ctx, "pcap: ", pcap)
+		ops = append(ops, TFPcap(pcap))
+	} // else: let TestFixture resolve the name.
+	if features&TFFeaturesRouterAsCapture != 0 {
+		testing.ContextLog(ctx, "using router as pcap")
+		ops = append(ops, TFRouterAsCapture())
+	}
+	// Read attenuator variable.
+	if features&TFFeaturesAttenuator != 0 {
+		atten, ok := getVar("attenuator")
+		if !ok || atten == "" {
+			// Attenuator is not typical companion, so we synthesize its name here.
+			var err error
+			if atten, err = companionName(d, "-attenuator"); err != nil {
+				return nil, err
+			}
+		}
+		testing.ContextLog(ctx, "attenuator: ", atten)
+		ops = append(ops, TFAttenuator(atten))
+	}
+	// Enable capturing.
+	if features&TFFeaturesCapture != 0 {
+		ops = append(ops, TFCapture(true))
+	}
+	return NewTestFixture(ctx, daemonCtx, d, rpcHint, ops...)
+}
