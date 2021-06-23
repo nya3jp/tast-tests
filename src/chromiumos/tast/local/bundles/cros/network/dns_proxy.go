3bdbe98269507c0a0329584bf3e70b1cf0f36103
Jason Jeremy Iman <jasongustaman@google.com>
2021-06-23 12:07:15 +0900

    network: DNSProxy: Ensure DNS queries go through the proxy
    DNS proxy test check if DNS functionality are working
    properly when DNS proxies are running. This patch expects
    DNS queries to fail when the corresponding DNS query type
    (plaintext DNS or secure DNS) is blocked. This patch
    ensures:
    1. DNS proxy uses the correct DoH mode.
    2. DNS queries are routed through the proxy (except for
       systems).
    
    BUG=b:180551432
    TEST=tast run ${DUT} network.DNSProxy.*
    
    Change-Id: Ie8c8ec385753de3d59f24c7ca211f1eb8657335a

--- src/chromiumos/tast/local/bundles/cros/network/dns_proxy.go
+++ src/chromiumos/tast/local/bundles/cros/network/dns_proxy.go
@@ -6,9 +6,12 @@ package network
 
 import (
 	"context"
+	"strings"
 	"time"
 
+	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/network/dns"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
@@ -101,4 +104,157 @@ func DNSProxy(ctx context.Context, s *testing.State) {
 			s.Errorf("Successful DNS query for %s, but expected failure", dns.GetClientString(tc.c))
 		}
 	}
+
+	nss, err := dnsProxyNamespaces(ctx)
+	if err != nil {
+		s.Fatal("Failed to get DNS proxy's network namespaces: ", err)
+	}
+
+	physIfs, err := physicalInterfaces(ctx)
+	if err != nil {
+		s.Fatal(ctx, "Failed to get phyiscal interfaces: ", err)
+	}
+
+	// Block plain-text or secure DNS through iptables.
+	var unblock func()
+	var dnsBlockedTC []testCase
+	switch params.mode {
+	case dns.DoHOff:
+		unblock, err = blockPlaintextDNS(ctx, nss, physIfs)
+		dnsBlockedTC = []testCase{
+			testCase{c: dns.System, expectErr: true},
+			testCase{c: dns.User, expectErr: true},
+			testCase{c: dns.Chrome, expectErr: true}}
+	case dns.DoHAutomatic:
+		return
+	case dns.DoHAlwaysOn:
+		unblock, err = blockSecureDNS(ctx, nss, physIfs)
+		dnsBlockedTC = []testCase{
+			testCase{c: dns.System, expectErr: true},
+			testCase{c: dns.User, expectErr: true}}
+	}
+	defer unblock()
+	if err != nil {
+		s.Fatal(ctx, "Failed to block DNS: ", err)
+	}
+
+	// DNS queries should fail if corresponding DNS packets (plain-text or secure) are dropped.
+	for _, tc := range dnsBlockedTC {
+		err = dns.QueryDNS(ctx, tc.c, domainDNSBlocked)
+		if err != nil && !tc.expectErr {
+			s.Errorf("Failed DNS query check for %s: %v", dns.GetClientString(tc.c), err)
+		}
+		if err == nil && tc.expectErr {
+			s.Errorf("Successful DNS query for %s, but expected failure", dns.GetClientString(tc.c))
+		}
+	}
+}
+
+// dnsProxyNamespaces iterates through available network namespaces and return the namespaces with DNS proxy.
+// DNS proxy namespaces are identified by checking if the namespace contain a listening process named dnsproxyd.
+func dnsProxyNamespaces(ctx context.Context) ([]string, error) {
+	out, err := testexec.CommandContext(ctx, "ip", "netns", "list").Output()
+	if err != nil {
+		return nil, err
+	}
+
+	var nss []string
+	for _, o := range strings.Split(strings.TrimSpace(string(out)), "\n") {
+		ns := strings.Fields(o)[0]
+		ss, err := testexec.CommandContext(ctx, "ip", "netns", "exec", ns, "ss", "-lptun").Output()
+		if err != nil {
+			return nil, err
+		}
+		if strings.Contains(string(ss), "dnsproxyd") {
+			nss = append(nss, ns)
+		}
+	}
+	return nss, nil
+}
+
+func physicalInterfaces(ctx context.Context) ([]string, error) {
+	out, err := testexec.CommandContext(ctx, "/usr/bin/find", "/sys/class/net", "-type", "l", "-not", "-lname", "*virtual*", "-printf", "%f\n").Output()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get physical interfaces")
+	}
+	return strings.Split(strings.TrimSpace(string(out)), "\n"), nil
+}
+
+// blockPlaintextDNS blocks plaintext DNS outbound packets that go through the physical interfaces or DNS proxy namespace.
+// Blocking is done by dropping outbound UDP and TCP packets with port 53.
+func blockPlaintextDNS(ctx context.Context, nss, physIfs []string) (unblock func(), e error) {
+	doUnblock := func() {
+		for _, cmd := range []string{"iptables", "ip6tables"} {
+			for _, ns := range nss {
+				testexec.CommandContext(ctx, "ip", "netns", "exec", ns, cmd, "-F", "-w").Run()
+			}
+			for _, ifname := range physIfs {
+				testexec.CommandContext(ctx, cmd, "-D", "OUTPUT", "-p", "udp", "--dport", "53", "-o", ifname, "-j", "DROP", "-w").Run()
+				testexec.CommandContext(ctx, cmd, "-D", "OUTPUT", "-p", "tcp", "--dport", "53", "-o", ifname, "-j", "DROP", "-w").Run()
+			}
+			testexec.CommandContext(ctx, cmd, "-D", "OUTPUT", "-p", "udp", "--dport", "53", "-m", "owner", "--uid-owner", "chronos", "-j", "DROP", "-w").Run()
+			testexec.CommandContext(ctx, cmd, "-D", "OUTPUT", "-p", "tcp", "--dport", "53", "-m", "owner", "--uid-owner", "chronos", "-j", "DROP", "-w").Run()
+		}
+	}
+
+	for _, cmd := range []string{"iptables", "ip6tables"} {
+		for _, ns := range nss {
+			if err := testexec.CommandContext(ctx, "ip", "netns", "exec", ns, cmd, "-I", "OUTPUT", "-p", "udp", "--dport", "53", "-j", "DROP", "-w").Run(); err != nil {
+				return doUnblock, errors.Wrapf(err, "failed to block UDP plaintext DNS on %s", ns)
+			}
+			if err := testexec.CommandContext(ctx, "ip", "netns", "exec", ns, cmd, "-I", "OUTPUT", "-p", "tcp", "--dport", "53", "-j", "DROP", "-w").Run(); err != nil {
+				return doUnblock, errors.Wrapf(err, "failed to block TCP plaintext DNS on %s", ns)
+			}
+		}
+		for _, ifname := range physIfs {
+			if err := testexec.CommandContext(ctx, cmd, "-I", "OUTPUT", "-p", "udp", "--dport", "53", "-o", ifname, "-j", "DROP", "-w").Run(); err != nil {
+				return doUnblock, errors.Wrapf(err, "failed to block UDP plaintext DNS for %s", ifname)
+			}
+			if err := testexec.CommandContext(ctx, cmd, "-I", "OUTPUT", "-p", "tcp", "--dport", "53", "-o", ifname, "-j", "DROP", "-w").Run(); err != nil {
+				return doUnblock, errors.Wrapf(err, "failed to block TCP plaintext DNS for %s", ifname)
+			}
+		}
+		if err := testexec.CommandContext(ctx, cmd, "-I", "OUTPUT", "-p", "udp", "--dport", "53", "-m", "owner", "--uid-owner", "chronos", "-j", "DROP", "-w").Run(); err != nil {
+			return doUnblock, errors.Wrap(err, "failed to block UDP plaintext DNS for Chrome")
+		}
+		if err := testexec.CommandContext(ctx, cmd, "-I", "OUTPUT", "-p", "tcp", "--dport", "53", "-m", "owner", "--uid-owner", "chronos", "-j", "DROP", "-w").Run(); err != nil {
+			return doUnblock, errors.Wrap(err, "failed to block TCP plaintext DNS for Chrome")
+		}
+	}
+
+	return doUnblock, nil
+}
+
+// blockSecureDNS blocks secure DNS outbound packets that go through the physical interfaces or DNS proxy namespace.
+// Blocking is done by dropping outbound TCP packets with port 443 (HTTPS packets).
+func blockSecureDNS(ctx context.Context, nss, physIfs []string) (unblock func(), e error) {
+	doUnblock := func() {
+		for _, cmd := range []string{"iptables", "ip6tables"} {
+			for _, ns := range nss {
+				testexec.CommandContext(ctx, "ip", "netns", "exec", ns, cmd, "-F", "-w").Run()
+			}
+			for _, ifname := range physIfs {
+				testexec.CommandContext(ctx, cmd, "-D", "OUTPUT", "-p", "tcp", "--dport", "443", "-o", ifname, "-j", "DROP", "-w").Run()
+			}
+			testexec.CommandContext(ctx, cmd, "-D", "OUTPUT", "-p", "tcp", "--dport", "443", "-m", "owner", "--uid-owner", "chronos", "-j", "DROP", "-w").Run()
+		}
+	}
+
+	for _, cmd := range []string{"iptables", "ip6tables"} {
+		for _, ns := range nss {
+			if err := testexec.CommandContext(ctx, "ip", "netns", "exec", ns, cmd, "-I", "OUTPUT", "-p", "tcp", "--dport", "443", "-j", "DROP", "-w").Run(); err != nil {
+				return doUnblock, errors.Wrapf(err, "failed to block secure DNS on %s", ns)
+			}
+		}
+		for _, ifname := range physIfs {
+			if err := testexec.CommandContext(ctx, cmd, "-I", "OUTPUT", "-p", "tcp", "--dport", "443", "-o", ifname, "-j", "DROP", "-w").Run(); err != nil {
+				return doUnblock, errors.Wrapf(err, "failed to block secure DNS for %s", ifname)
+			}
+		}
+		if err := testexec.CommandContext(ctx, cmd, "-I", "OUTPUT", "-p", "tcp", "--dport", "443", "-m", "owner", "--uid-owner", "chronos", "-j", "DROP", "-w").Run(); err != nil {
+			return doUnblock, errors.Wrap(err, "failed to block secure DNS for Chrome")
+		}
+	}
+
+	return doUnblock, nil
 }
