fb1c5a32e5e5ec663ab8aa53c16221d5102e589b
David Haddock <dhaddock@google.com>
2021-06-22 17:32:38 -0700

    Add lock screen tests to lockscreen package.
    As part of the 30% automation project we will add more lockscreen tests so
    start the package name for future tests to use.
    
    BUG=b:187788829
    TEST=lockscreen.*
    
    Change-Id: Iea9eac64f7776a2a1ba2151d03cb3e4c9085b141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2981120
    Tested-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: David Haddock <dhaddock@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/lockscreen/pin_unlock.go
+++ src/chromiumos/tast/local/bundles/cros/lockscreen/pin_unlock.go
@@ -1,8 +1,8 @@
-// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package ui
+package lockscreen
 
 import (
 	"context"
