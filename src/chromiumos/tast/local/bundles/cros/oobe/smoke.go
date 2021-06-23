1c6291f1caf367baa2c5c2aa242f34e05f145063
David Haddock <dhaddock@google.com>
2021-06-22 17:07:25 -0700

    Create oobe package and move smoke test to it.
    As part of the 30% automation project we will add more oobe tests so
    start the package name for future tests to use.
    
    BUG=b:187788829
    TEST=oobe.*
    
    Change-Id: I430661d4f333eea909ff604bffbe5cf82f6b4f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2981117
    Tested-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: David Haddock <dhaddock@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/oobe/smoke.go
+++ src/chromiumos/tast/local/bundles/cros/oobe/smoke.go
@@ -1,8 +1,8 @@
-// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package ui
+package oobe
 
 import (
 	"context"
@@ -13,7 +13,7 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func: OOBESmoke,
+		Func: Smoke,
 		Desc: "Smoke test that clicks through OOBE",
 		Contacts: []string{
 			"chromeos-sw-engprod@google.com",
@@ -24,7 +24,7 @@ func init() {
 	})
 }
 
-func OOBESmoke(ctx context.Context, s *testing.State) {
+func Smoke(ctx context.Context, s *testing.State) {
 	cr, err := chrome.New(ctx, chrome.NoLogin())
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
