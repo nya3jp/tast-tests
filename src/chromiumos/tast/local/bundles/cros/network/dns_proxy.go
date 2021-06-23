add54871fca109aba4096dbf347f1963b0b8bd90
Jason Jeremy Iman <jasongustaman@google.com>
2021-06-23 11:23:20 +0900

    network: DNSProxy: Add DNS proxy test
    DNS proxy test check if DNS queries are successful
    when DNS proxies are running.
    
    The test is done on system, user, and chrome as its
    client.
    
    The test is also done with different DoH modes (Off,
    Automatic, and Always On).
    
    BUG=b:180551432
    TEST=tast run ${DUT} network.DNSProxy.*
    
    Change-Id: I671593060a95baa6b03a287cde5095d5f9d5197c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2981374
    Reviewed-by: Garrick Evans <garrick@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Jason Jeremy Iman <jasongustaman@chromium.org>
    Commit-Queue: Jason Jeremy Iman <jasongustaman@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/network/dns_proxy.go
@@ -0,0 +1,89 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/network/dns"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+type dnsProxyTestParams struct {
+	mode dns.DoHMode
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DNSProxy,
+		Desc:         "Ensure that DNS proxies are working correctly",
+		Contacts:     []string{"jasongustaman@google.com", "garrick@google.com", "cros-networking@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name: "doh_off",
+			Val: dnsProxyTestParams{
+				mode: dns.DoHOff,
+			},
+		}, {
+			Name: "doh_automatic",
+			Val: dnsProxyTestParams{
+				mode: dns.DoHAutomatic,
+			},
+		}, {
+			Name: "doh_always_on",
+			Val: dnsProxyTestParams{
+				mode: dns.DoHAlwaysOn,
+			},
+		}},
+	})
+}
+
+// DNSProxy tests DNS functionality with DNS proxy active by ensuring that DNS queries are successful.
+func DNSProxy(ctx context.Context, s *testing.State) {
+	const (
+		// Randomly generated domains to be resolved. Different domains are used to avoid caching.
+		domainDefault    = "a2ffec2cb85be5e7.com"
+		domainDNSBlocked = "da39a3ee5e6b4b0d.com"
+	)
+
+	// If the main body of the test times out, we still want to reserve a few
+	// seconds to allow for our cleanup code to run.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(cleanupCtx, 3*time.Second)
+	defer cancel()
+
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("EnableDnsProxy", "DnsProxyEnableDOH"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get test API connection: ", err)
+	}
+
+	// Toggle plain-text DNS or secureDNS depending on test parameter.
+	params := s.Param().(dnsProxyTestParams)
+	if err := dns.SetDoHMode(ctx, cr, tconn, params.mode, dns.GoogleDoHProvider); err != nil {
+		s.Fatal("Failed to set DNS-over-HTTPS mode: ", err)
+	}
+
+	// By default, DNS query should work.
+	var defaultTC = []dns.ProxyTestCase{
+		dns.ProxyTestCase{Client: dns.System},
+		dns.ProxyTestCase{Client: dns.User},
+		dns.ProxyTestCase{Client: dns.Chrome},
+	}
+	if errs := dns.TestQueryDNSProxy(ctx, defaultTC, domainDefault); len(errs) != 0 {
+		for _, err := range errs {
+			s.Error("Failed DNS query check: ", err)
+		}
+	}
+}
