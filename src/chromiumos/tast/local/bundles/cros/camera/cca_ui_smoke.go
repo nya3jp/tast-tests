3ecb3366272f9dd9e921bb1b92f5f1af61fa9b21
Wei Lee <wtlee@google.com>
2021-06-23 17:44:54 +0800

    camera: Add CCAUISmoke.{photo,video}_fake
    To cover the basic functionality of CCA.
    
    BUG=b:191846403
    TEST=tast run [DUT] camera.CCAUISmoke.{photo,video}_fake
    
    Change-Id: Ia89b975b15321fcb2592bd375a7c4abaf0570dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2982072
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Tested-by: Wei Lee <wtlee@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/camera/cca_ui_smoke.go
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_smoke.go
@@ -6,12 +6,26 @@ package camera
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/common/media/caps"
 	"chromiumos/tast/local/camera/cca"
 	"chromiumos/tast/local/camera/testutil"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
+)
+
+type smokeTestParams struct {
+	useCameraType testutil.UseCameraType
+	function      testFunctionality
+}
+
+type testFunctionality int
+
+const (
+	none testFunctionality = iota
+	photoTaking
+	videoRecoridng
 )
 
 func init() {
@@ -27,24 +41,52 @@ func init() {
 			ExtraSoftwareDeps: []string{caps.BuiltinCamera},
 			Pre:               chrome.LoggedIn(),
 			ExtraAttr:         []string{"informational"},
-			Val:               testutil.UseRealCamera,
+			Val: smokeTestParams{
+				useCameraType: testutil.UseRealCamera,
+				function:      none,
+			},
 		}, {
 			Name:              "vivid",
 			ExtraSoftwareDeps: []string{caps.VividCamera},
 			Pre:               chrome.LoggedIn(),
 			ExtraAttr:         []string{"group:camera-postsubmit"},
-			Val:               testutil.UseVividCamera,
+			Val: smokeTestParams{
+				useCameraType: testutil.UseVividCamera,
+				function:      none,
+			},
 		}, {
 			Name: "fake",
 			Pre:  testutil.ChromeWithFakeCamera(),
-			Val:  testutil.UseFakeCamera,
+			Val: smokeTestParams{
+				useCameraType: testutil.UseFakeCamera,
+				function:      none,
+			},
+		}, {
+			Name: "photo_fake",
+			Pre:  testutil.ChromeWithFakeCamera(),
+			// TODO(b/191846403): Promote the test to Chrome CQ once it is stable enough.
+			ExtraAttr: []string{"informational"},
+			Val: smokeTestParams{
+				useCameraType: testutil.UseFakeCamera,
+				function:      photoTaking,
+			},
+		}, {
+			Name: "video_fake",
+			Pre:  testutil.ChromeWithFakeCamera(),
+			// TODO(b/191846403): Promote the test to Chrome CQ once it is stable enough.
+			ExtraAttr: []string{"informational"},
+			Val: smokeTestParams{
+				useCameraType: testutil.UseFakeCamera,
+				function:      videoRecoridng,
+			},
 		}},
 	})
 }
 
 func CCAUISmoke(ctx context.Context, s *testing.State) {
 	cr := s.PreValue().(*chrome.Chrome)
-	tb, err := testutil.NewTestBridge(ctx, cr, s.Param().(testutil.UseCameraType))
+	testParams := s.Param().(smokeTestParams)
+	tb, err := testutil.NewTestBridge(ctx, cr, testParams.useCameraType)
 	if err != nil {
 		s.Fatal("Failed to construct test bridge: ", err)
 	}
@@ -63,4 +105,20 @@ func CCAUISmoke(ctx context.Context, s *testing.State) {
 			s.Error("Failed to close app: ", err)
 		}
 	}(ctx)
+
+	if testParams.function == photoTaking {
+		if err := app.SwitchMode(ctx, cca.Photo); err != nil {
+			s.Error("Failed to switch to photo mode: ", err)
+		}
+		if _, err := app.TakeSinglePhoto(ctx, cca.TimerOff); err != nil {
+			s.Error("Failed to take photo: ", err)
+		}
+	} else if testParams.function == videoRecoridng {
+		if err := app.SwitchMode(ctx, cca.Video); err != nil {
+			s.Error("Failed to switch to video mode: ", err)
+		}
+		if _, err := app.RecordVideo(ctx, cca.TimerOff, 3*time.Second); err != nil {
+			s.Error("Failed to record video: ", err)
+		}
+	}
 }
