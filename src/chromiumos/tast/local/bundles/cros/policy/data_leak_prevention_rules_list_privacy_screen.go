a9aeadab3fc8b6e08a004bf694f25c620acd78c2
Vishal Chaudhary <vishal38785@gmail.com>
2021-06-24 01:57:43 +0530

    tast-tests: test for screenshare restriction of DLP
    Adds test to check wether privacy screen restriction allows copy and paste of
    content if restricted in DLP policy
    
    BUG=chromium:1132208
    TEST=tast run <ip> policy.DataLeakPreventionRulesListPrivacyScreen
    
    Change-Id: Iba28899abc67bd8840ac97f7da60a33c13b18e84

---
+++ src/chromiumos/tast/local/bundles/cros/policy/data_leak_prevention_rules_list_privacy_screen.go
@@ -0,0 +1,102 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DataLeakPreventionPrivacyScreen,
+		Desc: "Test behavior of DataLeakPreventionRulesList policy with privacy screen blocked restriction",
+		Contacts: []string{
+			"vishal38785@gmail.com", // Test author
+			"chromeos-dlp@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      "fakeDMS",
+	})
+}
+
+func DataLeakPreventionPrivacyScreen(ctx context.Context, s *testing.State) {
+	fakeDMS := s.FixtValue().(*fakedms.FakeDMS)
+
+	// DLP policy with privacy screen blocked restriction.
+	policyDLP := []policy.Policy{&policy.DataLeakPreventionRulesList{
+		Val: []*policy.DataLeakPreventionRulesListValue{
+			{
+				Name:        "Enable privacy screen for confidential content in restricted source",
+				Description: "Privacy screen should be enabled when on restricted site",
+				Sources: &policy.DataLeakPreventionRulesListSources{
+					Urls: []string{
+						"chromium.org",
+						"google.com",
+					},
+				},
+				Restrictions: []*policy.DataLeakPreventionRulesListRestrictions{
+					{
+						Class: "PRIVACY_SCREEN",
+						Level: "BLOCK",
+					},
+				},
+			},
+		},
+	},
+	}
+
+	// Update the policy blob.
+	pb := fakedms.NewPolicyBlob()
+
+	pb.AddPolicies(policyDLP)
+	if err := fakeDMS.WritePolicyBlob(pb); err != nil {
+		s.Fatal("Failed to write policies to FakeDMS: ", err)
+	}
+
+	// Start a Chrome instance that will fetch policies from the FakeDMS.
+	// Policies are only updated after Chrome startup.
+	cr, err := chrome.New(ctx,
+		chrome.FakeLogin(chrome.Creds{User: fixtures.Username, Pass: fixtures.Password}),
+		chrome.DMSPolicy(fakeDMS.URL))
+	if err != nil {
+		s.Fatal("Chrome login failed: ", err)
+	}
+	defer cr.Close(ctx)
+
+	for _, param := range []struct {
+		name        string
+		wantAllowed bool
+		url         string
+	}{
+		{
+			name:        "Example",
+			wantAllowed: false,
+			url:         "www.example.com",
+		},
+		{
+			name:        "Chromium",
+			wantAllowed: true,
+			url:         "www.chromium.org",
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+
+			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
+
+			if _, err = cr.NewConn(ctx, "https://"+param.url); err != nil {
+				s.Error("Failed to open page: ", err)
+			}
+
+		})
+	}
+}
