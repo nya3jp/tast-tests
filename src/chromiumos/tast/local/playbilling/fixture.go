f6430875c48f29d93e959b0a74acb0e299fc1054
Jeevan Shikaram <jshikaram@google.com>
2021-06-24 14:09:12 +1000

    [Play Billing] Add test to check purchase of a sku.
    BUG=chromium:1223285
    TEST=tast run <DUT_IP> arc.CheckSkuPackage*
    
    Change-Id: Ib840cb3a8a0b4e8bc938b28033faa67377a1b129

--- src/chromiumos/tast/local/playbilling/fixture.go
+++ src/chromiumos/tast/local/playbilling/fixture.go
@@ -30,7 +30,7 @@ const (
 )
 
 // pwaFiles are data files required to serve the Play Billing PWA.
-// TODO(crbug/1127165): Move this to the fixture once data is supported.
+// TODO(b/187793648): Move this to the fixture once data is supported.
 var pwaFiles = []string{
 	"play_billing_icon.png",
 	"play_billing_index.html",
@@ -40,7 +40,7 @@ var pwaFiles = []string{
 }
 
 // DataFiles are the files required for each Play Billing tests.
-// TODO(crbug/1127165): Move this to the fixture once data is supported.
+// TODO(b/187793648): Move this to the fixture once data is supported.
 var DataFiles = append(pwaFiles, apk)
 
 func init() {
@@ -105,11 +105,24 @@ func (f *playBillingFixture) SetUp(ctx context.Context, s *testing.FixtState) in
 	arcDevice := s.ParentValue().(*arc.PreData).ARC
 	cr := s.ParentValue().(*arc.PreData).Chrome
 
-	// TODO(crbug/1127165): Move this to the fixture once data is supported.
-	const localDataPath = "/usr/local/share/tast/data_pushed/chromiumos/tast/local/bundles/cros/arc/data"
+	// TODO(b/187793648): Move this to the fixture once data is supported.
+	const prebuiltLocalDataPath = "/usr/local/share/tast/data/chromiumos/tast/local/bundles/cros/appsplatform/data"
+	const builtLocalDataPath = "/usr/local/share/tast/data_pushed/chromiumos/tast/local/bundles/cros/appsplatform/data"
+
+	// TODO(b/187793648): Replace with s.DataPath(apk) when data is supported in Fixtures.
+	// The data path changes based on whether -build=true or -build=false is supplied to `tast run`.
+	// Local test runs on your workstation use -build=true by default, while lab runs use -build=false.
+
+	// Use the built local data path if it exists, and fall back to the prebuilt data path otherwise.
+	apkPath := filepath.Join(builtLocalDataPath, apk)
+	if _, err := os.Stat(builtLocalDataPath); os.IsNotExist(err) {
+		apkPath = filepath.Join(prebuiltLocalDataPath, apk)
+	} else if err != nil {
+		s.Fatal("Failed to check if built local data path exists: ", err)
+	}
 
 	// Install the test APK.
-	if err := arcDevice.Install(ctx, filepath.Join(localDataPath, apk)); err != nil {
+	if err := arcDevice.Install(ctx, apkPath); err != nil {
 		s.Fatal("Failed to install the APK: ", err)
 	}
 
@@ -128,7 +141,13 @@ func (f *playBillingFixture) SetUp(ctx context.Context, s *testing.FixtState) in
 	}
 	f.pwaDir = pwaDir
 	for _, name := range pwaFiles {
-		dataFilePath := filepath.Join(localDataPath, name)
+		// TODO(b/187793648): Replace with s.DataPath(name) when data is supported in Fixtures.
+		dataFilePath := filepath.Join(builtLocalDataPath, name)
+		if _, err := os.Stat(builtLocalDataPath); os.IsNotExist(err) {
+			dataFilePath = filepath.Join(prebuiltLocalDataPath, name)
+		} else if err != nil {
+			s.Fatal("Failed to check if built local data path exists: ", err)
+		}
 		pwaFilePath := filepath.Join(f.pwaDir, strings.TrimPrefix(name, "play_billing_"))
 		if err := fsutil.CopyFile(dataFilePath, pwaFilePath); err != nil {
 			s.Fatalf("Failed to copy extension file %q: %v", name, err)
