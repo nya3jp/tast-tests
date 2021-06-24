a1540601669fdd0e3d9f2f6740bc8a41883151d8
Alfred Yu <alfredyu@cienet.com>
2021-06-24 17:28:34 +0800

    cuj: EverydayMultiTasking volume change fix and Android UI dump
    1. add method to dump arc ui tree
    2. Added procedures to wait for the cras active node change before
       change volume. Otherwise it won't be able to change volume due to
       active node has been changed.
    
    BUG=chromium:1196849
    TEST=tast run <vars> $DUT ui.EverydayMultiTaskingCUJ.*
    
    Cq-Depend: chromium:2987147, chromium:2987146
    Change-Id: Idfdc3147924a402dc58c75e4af1c54e85137375b

--- src/chromiumos/tast/local/arc/ui.go
+++ src/chromiumos/tast/local/arc/ui.go
@@ -6,7 +6,11 @@ package arc
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/android/ui"
 )
 
@@ -14,4 +18,29 @@ import (
 // Close must be called to clean up resources when a test is over.
 func (a *ARC) NewUIDevice(ctx context.Context) (*ui.Device, error) {
 	return ui.NewDevice(ctx, a.device)
+}
+
+// DumpUIHierarchyOnError dumps arc UI hierarchy to 'arc_uidump.xml', when the test fails.
+// Call this function after closing arc UI devices. Otherwise the uiautomator might exist with errors like
+// status 137.
+func (a *ARC) DumpUIHierarchyOnError(ctx context.Context, outDir string, hasError func() bool) error {
+	if !hasError() {
+		return nil
+	}
+
+	if err := a.Command(ctx, "uiautomator", "dump").Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "failed to dump arc UI")
+	}
+
+	dir := filepath.Join(outDir, "faillog")
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		return errors.Wrapf(err, "failed to create directory %s", dir)
+	}
+
+	file := filepath.Join(dir, "arc_uidump.xml")
+	if err := a.PullFile(ctx, "/sdcard/window_dump.xml", file); err != nil {
+		return errors.Wrap(err, "failed to pull UI dump to outDir")
+	}
+
+	return nil
 }
