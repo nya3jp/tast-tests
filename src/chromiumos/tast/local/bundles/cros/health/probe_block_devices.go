45db1aa9f74b39202f9948c92fe61194b8154e94
Yilin Yang <kerker@google.com>
2021-06-24 11:01:31 +0800

    health.ProbeBlockDevices: Parse output data in json
    BUG=b:191216628
    TEST=tast run ${IP} health.ProbeBlockDevices
    
    Cq-Depend: chromium:2984016
    Change-Id: Iee633bc8e0bf0f84c2da175a8de8d88aba3b3992

--- src/chromiumos/tast/local/bundles/cros/health/probe_block_devices.go
+++ src/chromiumos/tast/local/bundles/cros/health/probe_block_devices.go
@@ -6,10 +6,32 @@ package health
 
 import (
 	"context"
+	"encoding/json"
+	"strings"
 
 	"chromiumos/tast/local/croshealthd"
+	"chromiumos/tast/local/jsontypes"
 	"chromiumos/tast/testing"
 )
+
+type blockDeviceInfo struct {
+	BytesReadSinceLastBoot          jsontypes.Uint64  `json:"bytes_read_since_last_boot"`
+	BytesWrittenSinceLastBoot       jsontypes.Uint64  `json:"bytes_written_since_last_boot"`
+	IoTimeSecondsSinceLastBoot      jsontypes.Uint64  `json:"io_time_seconds_since_last_boot"`
+	Name                            string            `json:"name"`
+	Path                            string            `json:"path"`
+	ReadTimeSecondsSinceLastBoot    jsontypes.Uint64  `json:"read_time_seconds_since_last_boot"`
+	Serial                          jsontypes.Uint32  `json:"serial"`
+	Size                            jsontypes.Uint64  `json:"size"`
+	Type                            string            `json:"type"`
+	WriteTimeSecondsSinceLastBoot   jsontypes.Uint64  `json:"write_time_seconds_since_last_boot"`
+	DiscardTimeSecondsSinceLastBoot *jsontypes.Uint64 `json:"discard_time_seconds_since_last_boot"`
+	ManufacturerID                  uint8             `json:"manufacturer_id"`
+}
+
+type blockDeviceResult struct {
+	BlockDevices []blockDeviceInfo `json:"block_devices"`
+}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -28,13 +50,20 @@ func init() {
 
 func ProbeBlockDevices(ctx context.Context, s *testing.State) {
 	params := croshealthd.TelemParams{Category: croshealthd.TelemCategoryStorage}
-	records, err := croshealthd.RunAndParseTelem(ctx, params, s.OutDir())
+	rawData, err := croshealthd.RunTelem(ctx, params, s.OutDir())
 	if err != nil {
 		s.Fatal("Failed to get storage telemetry info: ", err)
 	}
 
-	// Every board should have at least one non-removable block device.
-	if len(records) < 2 {
-		s.Fatalf("Wrong number of output lines: got %d; want 2+", len(records))
+	dec := json.NewDecoder(strings.NewReader(string(rawData)))
+	dec.DisallowUnknownFields()
+
+	var blockDevice blockDeviceResult
+	if err := dec.Decode(&blockDevice); err != nil {
+		s.Fatalf("Failed to decode storage data [%q], err [%v]", rawData, err)
+	}
+
+	if len(blockDevice.BlockDevices) < 1 {
+		s.Fatalf("Wrong number of block device: got %d; want 1+", len(blockDevice.BlockDevices))
 	}
 }
