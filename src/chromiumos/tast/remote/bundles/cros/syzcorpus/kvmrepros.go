1d39833f7f6f6d77b154b2e0034ac54cae47967b
Zubin Mithra <zsm@chromium.org>
2021-06-25 20:54:50 -0700

    tast-tests: syzcorpus: run repros concurrently
    Instead of running repros one after another, run repros on the DUT in
    parallel, in batches of 5. This would be useful in keeping the overall test
    duration manageable as more subsystems are enabled.
    
    BUG=b:189825001
    TEST=tast -verbose run localhost:2200 syzcorpus.Kvmtest
    
    Change-Id: I878fbfdb619196fa6ab51b6821eaaa785eed19a4

--- src/chromiumos/tast/remote/bundles/cros/syzcorpus/kvmrepros.go
+++ src/chromiumos/tast/remote/bundles/cros/syzcorpus/kvmrepros.go
@@ -13,6 +13,8 @@ import (
 	"path/filepath"
 	"time"
 
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/remote/bundles/cros/syzcorpus/syzutils"
 	"chromiumos/tast/testing"
 )
@@ -85,45 +87,70 @@ func KVMRepros(ctx context.Context, s *testing.State) {
 		s.Fatal("Unable to clear dmesg: ", err)
 	}
 
-	cnt := 1
+	var repros []string
 	for _, f := range files {
 		fname := f.Name()
 		if _, ok := enabledRepros[fname]; !ok {
 			s.Log("Skipping ", fname)
 			continue
 		}
+		repros = append(repros, fname)
+	}
 
-		s.Logf("=> Using repro(%v/%v): %v", cnt, len(enabledRepros), fname)
-		localPath := filepath.Join(binDir, fname)
-		remotePath := filepath.Join("/usr/local/tmp", fname)
-
-		if err := syzutils.CopyRepro(ctx, d, localPath, remotePath); err != nil {
-			s.Fatal("Failed to copy repro: ", err)
+	count := 1
+	windowSize := 5
+	for start := 0; start < len(repros); start += windowSize {
+		// Take windowSize number of repros at a time.
+		end := start + windowSize
+		if end > len(repros) {
+			end = len(repros)
 		}
-
-		if out, err := syzutils.RunRepro(ctx, d, remotePath, 5*time.Second); err != nil {
-			s.Logf("RunRepro returned %v: with combined output: %v", err, string(out))
+		errChan := make(chan error, end-start)
+		// Execute windowSize number of repros in parallel.
+		for _, repro := range repros[start:end] {
+			s.Logf("=> Using repro(%v/%v): %v", count, len(repros), repro)
+			go worker(ctx, d, binDir, repro, errChan)
+			count++
 		}
-
+		// Wait for windowSize repros to finish, and check if any errors were
+		// encountered.
+		for i := 0; i < end-start; i++ {
+			err := <-errChan
+			if err != nil {
+				s.Fatal("Received error from worker: ", err)
+			}
+		}
+		// Check dmesg for any warnings or errors.
 		warning, err := syzutils.WarningInDmesg(ctx, d)
 		if err != nil {
-			s.Fatalf("warningInDmesg failed after running sample %v: %v", fname, err)
+			s.Fatalf("warningInDmesg failed after running repros %v: %v", repros[start:end], err)
 		} else if warning != nil {
-			crashFile := filepath.Join(crashesDir, fname)
+			crashFile := filepath.Join(crashesDir, "stacktrace")
 			if err := ioutil.WriteFile(crashFile, warning, 0755); err != nil {
-				s.Logf("Failed to save warning for sample %v: %v", fname, err)
+				s.Log("Failed to save warning: ", err)
 			}
 			if err := d.Reboot(ctx); err != nil {
 				s.Fatal("Failed to reboot DUT: ", err)
 			}
-			s.Fatalf("Warning found at sample %v, DUT reset", fname)
+			s.Fatalf("Warning found with repros %v, DUT reset", repros[start:end])
 		}
 		if err := syzutils.ClearDmesg(ctx, d); err != nil {
 			s.Fatal("Unable to clear dmesg: ", err)
 		}
-
-		cnt++
 	}
-
 	s.Log("Finished running all repros in ", time.Since(start))
+}
+
+func worker(ctx context.Context, d *dut.DUT, binDir, repro string, errChan chan error) {
+	localPath := filepath.Join(binDir, repro)
+	remotePath := filepath.Join("/usr/local/tmp", repro)
+	if err := syzutils.CopyRepro(ctx, d, localPath, remotePath); err != nil {
+		testing.ContextLog(ctx, "Failed to copy repro: ", err)
+		errChan <- errors.Wrapf(err, "failed to copy repro %v", repro)
+		return
+	}
+	if out, err := syzutils.RunRepro(ctx, d, remotePath, 5*time.Second); err != nil {
+		testing.ContextLogf(ctx, "RunRepro returned %v: with combined output: %v", err, string(out))
+	}
+	errChan <- nil
 }
