f9ded93a11c737233c5332c1af25eb857c27acbf
Kyra Moed <kmoed@google.com>
2021-06-28 18:41:35 +0000

    scanner: Update tests for VUP changes
    Changes in the Virtual USB Printer in CL:2985359 require an additional
    optional parameter for logging the settings. Update the current scanning
    tast tests to pass this additional parameter.
    
    BUG=b:181075256
    TEST=scanner.*, documentscanapi.*, scanapp.*
    
    Cq-Depend: chromium:2985359
    Change-Id: I91152534c563bd14cb363835cf6c6d745c456627

--- src/chromiumos/tast/local/bundles/cros/scanner/enumerate_ipp_usb.go
+++ src/chromiumos/tast/local/bundles/cros/scanner/enumerate_ipp_usb.go
@@ -67,7 +67,7 @@ func runEnumerationTest(ctx context.Context, s *testing.State, info scannerInfo)
 		s.Fatal("Failed to restart printing system: ", err)
 	}
 
-	printer, err := usbprinter.StartScanner(ctx, devInfo, info.descriptors, info.attributes, info.esclCapabilities, "")
+	printer, err := usbprinter.StartScanner(ctx, devInfo, info.descriptors, info.attributes, info.esclCapabilities, "", "")
 	if err != nil {
 		s.Fatal("Failed to attach virtual printer: ", err)
 	}
