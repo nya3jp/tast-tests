5f79c3297b21d5c03a7a6d2148c5bc07f4b08a3e
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-06-28 05:50:56 +0000

    tast-tests: Support for PowerService Battery Charge/Drain
    During remote testing, it requires DUT to be within a power range
    to test certain policies.
    
    EnsureBatteryPercentage implements necessay logic to put device within
    min_level and max_level by abstracting away the required charging
    and draining based on the current power level.
    
    BUG=None
    TEST=as a part of remote power policy (PeakShift, BatteryCharge etc.)
    
    Change-Id: Ice20e5a89d0d441638a2fe98bee08eaec307b251

---
+++ src/chromiumos/tast/remote/power/battery_control.go
@@ -0,0 +1,79 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package power is used to remotely operate power functionalities with DUTs.
+package power
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/servo"
+	"chromiumos/tast/rpc"
+	pb "chromiumos/tast/services/cros/power"
+	"chromiumos/tast/testing"
+)
+
+// EnsureBatteryPercentage puts the DUT battery within minLevel and maxLevel
+func EnsureBatteryPercentage(ctx context.Context, c *rpc.Client, s *servo.Servo, minLevel, maxLevel float64) error {
+	cl := pb.NewPowerServiceClient(c.Conn)
+	// Checking current status of dut battery percentage
+	status, err := cl.BatteryStatus(ctx, &empty.Empty{})
+	if err != nil {
+		return errors.Wrap(err, "unable to fetch battery status")
+	}
+	// Battery is already in required range
+	if p := status.Percentage; p > minLevel && p < maxLevel {
+		return nil
+	}
+
+	role, err := s.GetPDRole(ctx) // checking current power delivery role
+	if err != nil {
+		return err
+	}
+
+	if role == servo.PDRoleNA {
+		return errors.New("servo type v4 required for battery charge and drain through servo_pd_role")
+	}
+
+	defer func(ctx context.Context) {
+		if err := s.SetPDRole(ctx, role); err != nil {
+			testing.ContextLogf(ctx, "Failed to restore servo_pd_role to %s during cleanup: %v", role, err)
+		}
+	}(ctx)
+
+	ctx, cancel := ctxutil.Shorten(ctx, 20*time.Second)
+	defer cancel()
+
+	// If a range is provided, that means it is intended for some sort of tesing. So putting the battery into avg makes sense
+	// even if power source gets disconnected/reconnected to avoid flakiness
+	avgLevel := minLevel + (maxLevel-minLevel)/2.0
+	if status.Percentage < minLevel { // requires charging
+		testing.ContextLog(ctx, "Battery charging has been initiated")
+		if err := s.SetPDRole(ctx, servo.PDRoleSrc); err != nil {
+			return errors.Wrap(err, "unable to set servo_pd_role to src")
+		}
+		if _, err := cl.BatteryCharge(ctx, &pb.BatteryChargeRequest{
+			Percentage: avgLevel,
+		}); err != nil {
+			return err
+		}
+	} else { // discharging
+		if err := s.SetPDRole(ctx, servo.PDRoleSnk); err != nil {
+			return errors.Wrap(err, "unable to set servo_pd_role to snk")
+		}
+		testing.ContextLog(ctx, "Battery discharging has been initiated")
+		if _, err := cl.BatteryDrain(ctx, &pb.BatteryDrainRequest{
+			Percentage: avgLevel,
+		}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
