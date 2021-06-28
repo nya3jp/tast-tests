ff9ba4663be4b882696d500a407b8008e9e0eef4
Alvin Jia <alvinjia@google.com>
2021-06-28 16:55:20 +1000

    Parameterization of input method Tast tests
    Separation of test data for input method tests. This allows sharing
    test data among several tests, and enable devs to write test focusing
    on one input method and easily extend it to cover more input methods.
    
    Create shared inputs data type InputData, and define test data
    from four modalities: VK, PK, handwriting and voice.
    
    Refactor existing tests: handwriting test, voice test, input field
    test and type test.
    
    Automated import of test data is not considered in this CL.
    
    BUG=b:188488890
    TEST=tast run <DUT> inputs.VirtualKeyboard*
    
    Change-Id: Ibc6bba83a10090686e0cf863ac142bb9f90210c8

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_speech.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_speech.go
@@ -10,11 +10,11 @@ import (
 	"path/filepath"
 	"time"
 
-	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/fsutil"
+	"chromiumos/tast/local/bundles/cros/inputs/data"
+	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/testserver"
 	"chromiumos/tast/local/bundles/cros/inputs/util"
-	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ime"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
@@ -36,6 +36,12 @@ type speechTestParams struct {
 	imeID        ime.InputMethodCode
 }
 
+var voiceTestMessages = []data.Message{data.VoiceMessageHello}
+var voiceTestIMEs = []ime.InputMethodCode{
+	ime.INPUTMETHOD_PINYIN_CHINESE_SIMPLIFIED,
+	ime.INPUTMETHOD_XKB_US_ENG,
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         VirtualKeyboardSpeech,
@@ -43,67 +49,22 @@ func init() {
 		Contacts:     []string{"shengjun@chromium.org", "essential-inputs-team@google.com"},
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
 		Attr:         []string{"group:mainline", "informational", "group:input-tools"},
-		Params: []testing.Param{
-			{
-				Name:      "hello_en",
-				ExtraData: []string{audioFileEN},
-				Val: speechTestParams{
-					audioFile:    audioFileEN,
-					expectedText: "Hello",
-					imeID:        ime.INPUTMETHOD_XKB_US_ENG,
-				},
-			}, {
-				Name:      "hello_cn",
-				ExtraData: []string{audioFileCN},
-				Val: speechTestParams{
-					audioFile:    audioFileCN,
-					expectedText: "你好",
-					imeID:        ime.INPUTMETHOD_PINYIN_CHINESE_SIMPLIFIED,
-				},
-			},
-		},
+		Data:         data.ExtractExternalFiles(voiceTestMessages, voiceTestIMEs),
+		Pre:          pre.VKEnabledTablet,
+		Timeout:      time.Duration(len(voiceTestIMEs)) * time.Duration(len(voiceTestMessages)) * time.Minute,
 	})
 }
 
 func VirtualKeyboardSpeech(ctx context.Context, s *testing.State) {
-	cleanupCtx := ctx
-	// Use a shortened context for test operations to reserve time for cleanup.
-	ctx, shortCancel := ctxutil.Shorten(ctx, 10*time.Second)
-	defer shortCancel()
-
-	// TODO(crbug/1173252): Clean up states within Chrome using preconditions.
-	cr, err := chrome.New(ctx, chrome.VKEnabled(), chrome.ExtraArgs("--force-tablet-mode=touch_view"))
-	defer cr.Close(cleanupCtx)
-
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to connect Test API: ", err)
-	}
-
-	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+	cr := s.PreValue().(pre.PreData).Chrome
+	tconn := s.PreValue().(pre.PreData).TestAPIConn
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
 
 	cleanup, err := voice.EnableAloop(ctx, tconn)
 	if err != nil {
 		s.Fatal("Failed to load Aloop: ", err)
 	}
-	defer cleanup(cleanupCtx)
-
-	// Test parameters that are specific to the current test case.
-	audioFile := s.Param().(speechTestParams).audioFile
-	expectedText := s.Param().(speechTestParams).expectedText
-	testIME := ime.IMEPrefix + string(s.Param().(speechTestParams).imeID)
-
-	// Set up the test audio file.
-	testFileLocation := filepath.Join(filesapp.DownloadPath, audioFile)
-	if err := fsutil.CopyFile(s.DataPath(audioFile), testFileLocation); err != nil {
-		s.Fatalf("Failed to copy the test image to %s: %s", testFileLocation, err)
-	}
-	defer os.Remove(testFileLocation)
-
-	// Add the ime input being tested to the test device.
-	if err := ime.AddAndSetInputMethod(ctx, tconn, testIME); err != nil {
-		s.Fatalf("Failed to set input method to %s: %v: ", testIME, err)
-	}
+	defer cleanup(ctx)
 
 	// Launch inputs test web server.
 	its, err := testserver.Launch(ctx, cr, tconn)
@@ -116,15 +77,57 @@ func VirtualKeyboardSpeech(ctx context.Context, s *testing.State) {
 	inputField := testserver.TextAreaInputField
 	vkbCtx := vkb.NewContext(cr, tconn)
 
-	if err := uiauto.Combine("verify audio input",
-		its.ClickFieldUntilVKShown(inputField),
-		vkbCtx.SwitchToVoiceInput(),
-		func(ctx context.Context) error {
-			return voice.AudioFromFile(ctx, testFileLocation)
-		},
-		util.WaitForFieldTextToBe(tconn, inputField.Finder(), expectedText),
-	)(ctx); err != nil {
-		s.Fatal("Failed to validate voice input: ", err)
-	}
+	for _, inputMethod := range voiceTestIMEs {
+		for _, message := range voiceTestMessages {
+			inputData, ok := message.GetInputData(inputMethod)
+			if !ok {
+				s.Fatalf("Test Data for input method %v does not exist", inputMethod)
+			}
+			testName := string(inputMethod) + "-" + string(inputData.ExpectedText)
+			s.Run(ctx, testName, func(ctx context.Context, subS *testing.State) {
+				// IME code of the language currently being tested.
+				testIME := ime.IMEPrefix + string(inputMethod)
 
+				// Set up the test audio file.
+				testFileLocation := filepath.Join(filesapp.DownloadPath, inputData.VoiceFile)
+				if err := fsutil.CopyFile(s.DataPath(inputData.VoiceFile), testFileLocation); err != nil {
+					s.Fatalf("Failed to copy the test image to %s: %s", testFileLocation, err)
+				}
+				defer os.Remove(testFileLocation)
+
+				// Add the ime input being tested to the test device.
+				if err := ime.AddAndSetInputMethod(ctx, tconn, testIME); err != nil {
+					s.Fatalf("Failed to set input method to %s: %v: ", testIME, err)
+				}
+
+				if err := its.ClickFieldUntilVKShown(inputField)(ctx); err != nil {
+					s.Fatal("Failed to show VK: ", err)
+				}
+
+				defer func() {
+					if err := vkbCtx.ClearInputField(inputField.Finder())(ctx); err != nil {
+						s.Log("Failed to clear input field: ", err)
+					}
+
+					outDir := filepath.Join(s.OutDir(), testName)
+					faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, s.HasError, cr, "ui_tree_"+testName)
+
+					if err := vkbCtx.HideVirtualKeyboard()(ctx); err != nil {
+						s.Log("Failed to hide virtual keyboard: ", err)
+					}
+				}()
+
+				if err := uiauto.Combine("verify audio input",
+					vkbCtx.SwitchToVoiceInput(),
+					func(ctx context.Context) error {
+						return voice.AudioFromFile(ctx, testFileLocation)
+					},
+					util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), inputData.ExpectedText),
+					vkbCtx.SwitchFromVoiceToTyping(),
+				)(ctx); err != nil {
+					s.Fatal("Failed to validate voice input: ", err)
+				}
+			})
+		}
+	}
 }
