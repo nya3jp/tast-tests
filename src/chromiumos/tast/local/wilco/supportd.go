4b51a1093e4e1e00c440298ae0f7ad19502b71d3
Shuhei Takahashi <nya@chromium.org>
2021-06-29 16:06:45 +0900

    upstart: Pass extra arguments in a structured way
    StartJob and RestartJob accept extra arguments to pass to an upstart
    job. They are currently represented as ...string, each of which is
    formatted as "KEY=VALUE", an identical format as ones passed to
    initctl(1). This approach lacks a proper abstraction over upstart CLI.
    
    This patch modifies those functions to take ...Arg instead to be more
    conforming to Go API standards. Callers can call WithArg to construct
    Arg to be passed to functions, just like the following:
    
     upstart.StartJob(ctx, "foo", upstart.WithArg("key1", "value1"))
    
    Note: I also considered API of the following style:
    
     func WithArgs(kvs map[string]string) []Arg
    
    However, since it is rare to pass many arguments to upstart jobs,
    I found this style gets more verbose, e.g.
    
     upstart.StartJob(ctx, "foo", upstart.WithArgs(map[string]string{"key1": "value1"})...)
    
    BUG=b:192321435
    TEST=fast_build.sh -T
    
    Cq-Depend: chrome-internal:3943439
    Change-Id: I747ee09b1735982821fc046ec1bb8227f2b7e3b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2993944
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/wilco/supportd.go
+++ src/chromiumos/tast/local/wilco/supportd.go
@@ -24,7 +24,7 @@ func SupportdPID(ctx context.Context) (pid int, err error) {
 
 // StartSupportd starts the upstart process wilco_dtc_supportd.
 func StartSupportd(ctx context.Context) error {
-	if err := upstart.RestartJob(ctx, wilcoSupportdJob, "VMODULE_ARG=*=3"); err != nil {
+	if err := upstart.RestartJob(ctx, wilcoSupportdJob, upstart.WithArg("VMODULE_ARG", "*=3")); err != nil {
 		return errors.Wrapf(err, "unable to start the %s service", wilcoSupportdJob)
 	}
 	return nil
