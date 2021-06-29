78d67dda3ae9d77ff1e7693a47a16967f1ebbfbe
Toby Huang <tobyhuang@chromium.org>
2021-06-29 19:25:58 +0000

    Tast: Verify multiple sign-in disabled for Unicorn users
    This CL adds a Tast test to verify that multiple sign-in is disabled
    for Unicorn users.
    
    BUG=b:190654837
    TEST=All familylink.* tests pass locally on atlas device
    
    Change-Id: I857773961bc2399ed5e591479de03a7627b01544

---
+++ src/chromiumos/tast/local/bundles/cros/familylink/multiple_sign_in_disabled.go
@@ -0,0 +1,71 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package familylink
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"chromiumos/tast/local/chrome/familylink"
+	"chromiumos/tast/local/chrome/ui/quicksettings"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: MultipleSignInDisabled,
+		Desc: "Verifies that multiple sign-in is disabled for Unicorn users",
+		Contacts: []string{
+			"tobyhuang@chromium.org", "cros-families-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      5 * time.Minute,
+		VarDeps:      []string{"unicorn.childFirstName", "unicorn.childLastName"},
+		Fixture:      "familyLinkUnicornLoginNonOwner",
+	})
+}
+
+func MultipleSignInDisabled(ctx context.Context, s *testing.State) {
+	// After the fixture runs, the DUT has two users on the
+	// device: a regular owner and a Unicorn secondary user. The
+	// Unicorn user is logged in.
+	tconn := s.FixtValue().(*familylink.FixtData).TestConn
+
+	childFirstName := s.RequiredVar("unicorn.childFirstName")
+	childLastName := s.RequiredVar("unicorn.childLastName")
+
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+	ui := uiauto.New(tconn)
+
+	s.Log("Opening the system status tray")
+	if err := quicksettings.Show(ctx, tconn); err != nil {
+		s.Fatal("Failed to open Quick Settings: ", err)
+	}
+	defer quicksettings.Hide(ctx, tconn)
+
+	s.Log("Attempting to add multiple profiles")
+	userProfileName := fmt.Sprintf("%s%s", strings.ToLower(childFirstName), strings.ToLower(childLastName))
+	s.Logf("Looking for user profile name=%s", userProfileName)
+	userProfileIcon := nodewith.NameContaining(userProfileName).Role(role.Button)
+	if err := uiauto.Combine("Attempting to add multiple profiles",
+		ui.WaitUntilExists(userProfileIcon),
+		ui.LeftClick(userProfileIcon))(ctx); err != nil {
+		s.Fatal("Failed to click the user profile icon: ", err)
+	}
+
+	s.Log("Checking for error message preventing Unicorn users from adding multi-profiles")
+	// A regular user would see "Sign in another another user..."
+	// at this point instead of the expected error message.
+	if err := ui.WaitUntilExists(nodewith.Name("All available users have already been added to this session.").Role(role.StaticText))(ctx); err != nil {
+		s.Fatal("Failed to detect error message preventing Unicorn users from adding multi-profiles: ", err)
+	}
+}
