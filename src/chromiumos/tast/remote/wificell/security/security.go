6a24b1b2357dfcba7abbbab1e23a6c6dfde73187
Billy Zhao <billyzhao@chromium.org>
2021-06-29 08:26:16 +0000

    tast-tests: Add AxRouter security config wrapper
    This cl includes the wrappers on top of the hostapd security wrappers to
    enable the AxRouter to be configured with security factories while also
    leveraging the old security factories used in the legacy router system
    for the DUTs.
    
    BUG=b:187720951
    TEST=tast compiles
    
    Change-Id: I888a63a4a53064b7de0259aa1b5c5894d0f68470

---
+++ src/chromiumos/tast/remote/wificell/security/security.go
@@ -0,0 +1,35 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package security defines interfaces for test authors to deal with the
+// diverse WiFi security standards:
+// - Interface AxConfig defines methods to compose configuration files of AP or DUT.
+// - Interface AxConfigFactory lets the authors to register the security options
+//   in testing.AddTest and Gen the Config object later in test body.
+package security
+
+import (
+	sec "chromiumos/tast/common/wifi/security"
+	"chromiumos/tast/remote/wificell/router"
+)
+
+// AxConfig defines methods to generate hostapd and shill config of protected network.
+type AxConfig interface {
+	// RouterParams is a getter that returns the config's router params.
+	RouterParams() []router.AxRouterConfigParam
+	// SecConfig is a getter that returns the config's security config.
+	SecConfig() sec.Config
+}
+
+// AxConfigFactory defines a Gen() method to generate a Config instance.
+// A security protocol should implement its own AxConfigFactory interface as well as a NewConfigFactory()
+// function to compose a declarative factory object for generating a security config.
+// A factory, once created via NewConfigFactory, holds a list of options provided in testing.AddTest.
+// Gen() then uses the stored options to compose a Config.
+// Noted that the creation of a ConfigFactory object must not emit an error to satisfy the
+// requirement of declarative test registration.
+type AxConfigFactory interface {
+	// Gen builds a Config.
+	Gen() (AxConfig, error)
+}
