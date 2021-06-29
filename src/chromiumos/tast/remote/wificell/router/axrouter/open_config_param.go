6146775691cd9869a1069cac94dba113da231e9e
Billy Zhao <billyzhao@chromium.org>
2021-06-29 08:26:16 +0000

    tast-tests: Add AxRouter security config wrapper
    This cl includes the wrappers on top of the hostapd security wrappers to
    enable the AxRouter to be configured with security factories while also
    leveraging the old security factories used in the legacy router system
    for the DUTs.
    
    BUG=b:187720951
    TEST=tast compiles
    
    Change-Id: I888a63a4a53064b7de0259aa1b5c5894d0f68470

---
+++ src/chromiumos/tast/remote/wificell/router/axrouter/open_config_param.go
@@ -0,0 +1,21 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package axrouter
+
+// secOpen provides Gen method to build a new Config.
+type secOpen struct {
+	band BandEnum
+}
+
+// Gen builds a ConfigParam list to allow the router to support an open network flow.
+func (f *secOpen) Gen() ([]ConfigParam, error) {
+	routerConfigParams := []ConfigParam{{f.band, KeyAKM, ""}, {f.band, KeyAuthMode, "open"}}
+	return routerConfigParams, nil
+}
+
+// NewSecOpenConfigParamFac builds a SecConfigParamFac for open network for the 802.11ax AP.
+func NewSecOpenConfigParamFac(band BandEnum) SecConfigParamFac {
+	return &secOpen{band}
+}
