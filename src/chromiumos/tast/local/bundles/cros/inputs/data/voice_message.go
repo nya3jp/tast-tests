d91caf6fac9bb610660f96d61397d75bd1c4119d
Alvin Jia <alvinjia@google.com>
2021-06-30 18:39:25 +1000

    Parameterization of input method Tast tests
    Separation of test data for input method tests. This allows sharing
    test data among several tests, and enable devs to write test focusing
    on one input method and easily extend it to cover more input methods.
    
    Create shared inputs data type InputData, and define test data
    from four modalities: VK, PK, handwriting and voice.
    
    Automated import of test data is not considered in this CL.
    
    BUG=b:188488890
    TEST=None
    
    Change-Id: Ibc6bba83a10090686e0cf863ac142bb9f90210c8

---
+++ src/chromiumos/tast/local/bundles/cros/inputs/data/voice_message.go
@@ -0,0 +1,36 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package data
+
+import "chromiumos/tast/local/chrome/ime"
+
+type voiceMessage map[ime.Language]InputData
+
+// GetInputData returns two values given an input method: inputData and ok.
+// If the test data for the given input method can be found, ok is true.
+// If there is no match for the given input method, ok is false, and inputData is the zero value.
+func (message voiceMessage) GetInputData(inputMethodCode ime.InputMethodCode) (InputData, bool) {
+	var inputData InputData
+
+	languageCode, ok := ime.LanguageOfIME[inputMethodCode]
+	if !ok {
+		return inputData, false
+	}
+
+	inputData, ok = message[languageCode]
+	return inputData, ok
+}
+
+// VoiceMessageHello defines hello voice messages of input methods
+var VoiceMessageHello = voiceMessage{
+	ime.EN: {
+		VoiceFile:    "voice_en_hello_20201021.wav",
+		ExpectedText: "hello",
+	},
+	ime.ZH_HANS: {
+		VoiceFile:    "voice_zh_hans_hello_20201021.wav",
+		ExpectedText: "你好",
+	},
+}
