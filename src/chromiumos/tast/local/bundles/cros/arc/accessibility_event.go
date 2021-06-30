f3bed1b7f6b46f287ba3104e3915ccde30c20976
Akihiro Ota <akihiroota@chromium.org>
2021-06-30 15:41:06 -0700

    tast-tests: Port some a11y files to uiauto
    This change is a part of the tast-wide effort to port existing
    tests from chrome/ui to chrome/uiauto. We remove all uses of
    chrome/ui within */a11y/a11y.go. Some changes also had to be made
    to accessibility_event.go, but the file is still only partially
    converted.
    
    BUG=b:190614781
    TEST=tast -verbose run <IP> a11y.* arc.Accessibility*
    
    Change-Id: Ic6533fca884cbfcdcfccd6119dce895721fee36b

--- src/chromiumos/tast/local/bundles/cros/arc/accessibility_event.go
+++ src/chromiumos/tast/local/bundles/cros/arc/accessibility_event.go
@@ -13,14 +13,17 @@ import (
 	arca11y "chromiumos/tast/local/bundles/cros/arc/a11y"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
 
 type axEventTestStep struct {
-	keys      string        // a sequence of keys to invoke.
-	focus     ui.FindParams // expected params of focused node after the event.
-	eventType ui.EventType  // an expected event type from the focused node.
+	keys      string           // a sequence of keys to invoke.
+	focus     *nodewith.Finder // expected params of focused node after the ui.EventType
+	eventType ui.EventType     // an expected event type from the focused node.
 }
 
 func init() {
@@ -42,6 +45,9 @@ func init() {
 }
 
 func runTestStep(ctx context.Context, cvconn *a11y.ChromeVoxConn, tconn *chrome.TestConn, ew *input.KeyboardEventWriter, step axEventTestStep, isFirstStep bool) error {
+	uia := uiauto.New(tconn)
+	testing.ContextLog(ctx, "Running test step: ", step.keys)
+	testing.ContextLog(ctx, "Expecting node: ", step.focus.Pretty())
 	watcher, err := ui.NewRootWatcher(ctx, tconn, step.eventType)
 	if err != nil {
 		return errors.Wrap(err, "failed to create EventWatcher")
@@ -54,7 +60,7 @@ func runTestStep(ctx context.Context, cvconn *a11y.ChromeVoxConn, tconn *chrome.
 	}
 
 	// Wait for the focused element to match the expected.
-	if err := cvconn.WaitForFocusedNode(ctx, tconn, &step.focus, 10*time.Second); err != nil {
+	if err := cvconn.WaitForFocusedNode(ctx, tconn, step.focus, 10*time.Second); err != nil {
 		return err
 	}
 
@@ -69,145 +75,148 @@ func runTestStep(ctx context.Context, cvconn *a11y.ChromeVoxConn, tconn *chrome.
 			if e.Target == nil {
 				continue
 			}
-			if ok, err := e.Target.Matches(ctx, step.focus); err != nil {
-				return err
-			} else if ok {
-				return nil
+
+			// Convert ui.Node into uiauto.NodeInfo so that the target node can be
+			// matched against step.focus.
+			target, err := nodeToNodeInfo(ctx, tconn, e.Target)
+			if err != nil {
+				return errors.Wrap(err, "failed to convert Node into NodeInfo")
 			}
+
+			err = uia.Matches(ctx, target, step.focus)
+			if err != nil {
+				return errors.Wrap(err, "nodes did not match")
+			}
+
+			return nil
 		}
 		return errors.Errorf("expected event didn't occur. got: %+v", events)
 	}, &testing.PollOptions{Timeout: 10 * time.Second})
 }
 
+func nodeToNodeInfo(ctx context.Context, tconn *chrome.TestConn, node *ui.Node) (*uiauto.NodeInfo, error) {
+	uia := uiauto.New(tconn)
+	// 1. Create a finder with all properties in Node.
+	finder := nodewith.Name(node.Name)
+	// 2. Convert the finder into a NodeInfo
+	return uia.Info(ctx, finder)
+}
+
+/*
+// Manually compares a ui.Node and uiauto.NodeInfo, since there is no existing
+// way to compare them. This is a temporary method, which should be removed
+// once this file gets converted from chrome/ui to chrome/uiauto.
+func compareNodeAndNodeInfo(ctx context.Context, expected *uiauto.NodeInfo, actual *ui.Node) error {
+	if expected.Name != actual.Name {
+		return errors.Errorf("Expected name: %s, but got: %s", expected.Name, actual.Name)
+	}
+
+	if string(expected.Role) != string(actual.Role) {
+		return errors.Errorf("Expected role: %s, but got: %s", expected.Role, actual.Role)
+	}
+
+	if expected.Value != actual.Value {
+		return errors.Errorf("Expected value: %s, but got: %s", expected.Value, actual.Value)
+	}
+
+	// Convert the two maps into map[string]bool for comparison.
+	newExpected := make(map[string]bool)
+	for key, value := range expected.State {
+		newExpected[string(key)] = value
+	}
+
+	newActual := make(map[string]bool)
+	for key, value := range actual.State {
+		newActual[string(key)] = value
+	}
+
+	ok := reflect.DeepEqual(newExpected, newActual)
+	if !ok {
+		return errors.Errorf("Expected state: %s, but got: %s", newExpected, newActual)
+	}
+
+	if string(expected.Checked) != string(actual.Checked) {
+		return errors.Errorf("Expected checked: %s, but got: %s", expected.Checked, actual.Checked)
+	}
+
+	if expected.ClassName != actual.ClassName {
+		return errors.Errorf("Expected class name: %s, but got: %s", expected.ClassName, actual.ClassName)
+	}
+
+	if expected.Location != actual.Location {
+		return errors.Errorf("Expected location: %s, but got: %s", expected.Location, actual.Location)
+	}
+
+	if string(expected.Restriction) != string(actual.Restriction) {
+		return errors.Errorf("Expected restriction: %s, but got: %s", expected.Restriction, actual.Restriction)
+	}
+
+	ok = reflect.DeepEqual(expected.HTMLAttributes, actual.HTMLAttributes)
+	if !ok {
+		return errors.Errorf("Expected HTML attributes: %s, but got: %s", expected.HTMLAttributes, actual.HTMLAttributes)
+	}
+
+	return nil
+}
+*/
+
 func AccessibilityEvent(ctx context.Context, s *testing.State) {
 	MainActivityTestSteps := []axEventTestStep{
 		axEventTestStep{
 			"Tab",
-			ui.FindParams{
-				ClassName: arca11y.ToggleButton,
-				Name:      "OFF",
-				Role:      ui.RoleTypeToggleButton,
-				Attributes: map[string]interface{}{
-					"checked": ui.CheckedStateFalse,
-				},
-			},
+			nodewith.Name("OFF").Role(role.ToggleButton).ClassName(arca11y.ToggleButton).Attribute("checked", "false"),
 			ui.EventTypeFocus,
 		},
 		axEventTestStep{
 			"Search+Space",
-			ui.FindParams{
-				ClassName: arca11y.ToggleButton,
-				Name:      "ON",
-				Role:      ui.RoleTypeToggleButton,
-				Attributes: map[string]interface{}{
-					"checked": ui.CheckedStateTrue,
-				},
-			},
+			nodewith.Name("ON").Role(role.ToggleButton).ClassName(arca11y.ToggleButton).Attribute("checked", "true"),
 			ui.EventTypeCheckedStateChanged,
 		},
 		axEventTestStep{
 			"Tab",
-			ui.FindParams{
-				ClassName: arca11y.CheckBox,
-				Name:      "CheckBox",
-				Role:      ui.RoleTypeCheckBox,
-				Attributes: map[string]interface{}{
-					"checked": ui.CheckedStateFalse,
-				},
-			},
+			nodewith.Name("CheckBox").Role(role.CheckBox).ClassName(arca11y.CheckBox).Attribute("checked", "false"),
 			ui.EventTypeFocus,
 		},
 		axEventTestStep{
 			"Search+Space",
-			ui.FindParams{
-				ClassName: arca11y.CheckBox,
-				Name:      "CheckBox",
-				Role:      ui.RoleTypeCheckBox,
-				Attributes: map[string]interface{}{
-					"checked": ui.CheckedStateTrue,
-				},
-			},
+			nodewith.Name("CheckBox").Role(role.CheckBox).ClassName(arca11y.CheckBox).Attribute("checked", "true"),
 			ui.EventTypeCheckedStateChanged,
 		},
 		axEventTestStep{
 			"Tab",
-			ui.FindParams{
-				ClassName: arca11y.CheckBox,
-				Name:      "CheckBoxWithStateDescription",
-				Role:      ui.RoleTypeCheckBox,
-				Attributes: map[string]interface{}{
-					"checked": ui.CheckedStateFalse,
-				},
-			},
+			nodewith.Name("CheckBoxWithStateDescription").Role(role.CheckBox).ClassName(arca11y.CheckBox).Attribute("checked", "false"),
 			ui.EventTypeFocus,
 		},
 		axEventTestStep{
 			"Tab",
-			ui.FindParams{
-				ClassName: arca11y.SeekBar,
-				Name:      "seekBar",
-				Role:      ui.RoleTypeSlider,
-				Attributes: map[string]interface{}{
-					"valueForRange": 25,
-				},
-			},
+			nodewith.Name("seekBar").Role(role.Slider).ClassName(arca11y.SeekBar).Attribute("valueForRange", 25),
 			ui.EventTypeFocus,
 		},
 		axEventTestStep{
 			"=",
-			ui.FindParams{
-				ClassName: arca11y.SeekBar,
-				Name:      "seekBar",
-				Role:      ui.RoleTypeSlider,
-				Attributes: map[string]interface{}{
-					"valueForRange": 26,
-				},
-			},
+			nodewith.Name("seekBar").Role(role.Slider).ClassName(arca11y.SeekBar).Attribute("valueForRange", 26),
 			ui.EventTypeRangeValueChanged,
 		},
 		axEventTestStep{
 			"Tab",
-			ui.FindParams{
-				ClassName: arca11y.SeekBar,
-				Role:      ui.RoleTypeSlider,
-				Attributes: map[string]interface{}{
-					"valueForRange": 3,
-				},
-			},
+			nodewith.Role(role.Slider).ClassName(arca11y.SeekBar).Attribute("valueForRange", 3),
 			ui.EventTypeFocus,
 		},
 		axEventTestStep{
 			"-",
-			ui.FindParams{
-				ClassName: arca11y.SeekBar,
-				Role:      ui.RoleTypeSlider,
-				Attributes: map[string]interface{}{
-					"valueForRange": 2,
-				},
-			},
+			nodewith.Role(role.Slider).ClassName(arca11y.SeekBar).Attribute("valueForRange", 2),
 			ui.EventTypeRangeValueChanged,
 		},
 	}
 	EditTextActivityTestSteps := []axEventTestStep{
 		axEventTestStep{
 			"Tab",
-			ui.FindParams{
-				ClassName: arca11y.EditText,
-				Name:      "contentDescription",
-				Role:      ui.RoleTypeTextField,
-			},
+			nodewith.Name("contentDescription").Role(role.TextField).ClassName(arca11y.EditText),
 			ui.EventTypeFocus,
 		},
 		axEventTestStep{
 			"a",
-			ui.FindParams{
-				ClassName: arca11y.EditText,
-				Name:      "contentDescription",
-				Role:      ui.RoleTypeTextField,
-				Attributes: map[string]interface{}{
-					"value": "a",
-				},
-			},
+			nodewith.Name("contentDescription").Role(role.TextField).ClassName(arca11y.EditText).Attribute("value", "a"),
 			ui.EventTypeValueInTextFieldChanged,
 		},
 	}
