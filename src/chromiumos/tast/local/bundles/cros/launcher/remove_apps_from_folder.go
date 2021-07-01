61f6ca80b27c54c186291b9ae52d0c5f8c606ea2
Matthew Mourgos <mmourgos@chromium.org>
2021-07-01 13:50:01 -0700

    foldering tast test
    BUG=b:190654657
    TEST=Ran all the app list foldering test
    
    Change-Id: I64c7b6f11bac196b1d520f6ffa16ff1181a714a7

---
+++ src/chromiumos/tast/local/bundles/cros/launcher/remove_apps_from_folder.go
@@ -0,0 +1,95 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package launcher
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RemoveAppsFromFolder,
+		Desc:         "Test removing items from a folder in the launcher",
+		Contacts:     []string{"cros-launcher-prod-notifications@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedInWith100FakeApps",
+	})
+}
+
+// RemoveAppsFromFolder tests that items can be removed from a folder.
+func RemoveAppsFromFolder(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
+	if err != nil {
+		s.Fatal("Failed to ensure in clamshell mode: ", err)
+	}
+	defer cleanup(ctx)
+
+	// Open the Launcher and go to Apps list page.
+	ac := uiauto.New(tconn)
+	if err := ac.Retry(2, launcher.OpenExpandedView(tconn))(ctx); err != nil {
+		s.Fatal("Failed to open Expanded Application list view: ", err)
+	}
+
+	if err := ac.Retry(3, launcher.CreateFolder(ctx, tconn, ac))(ctx); err != nil {
+		s.Fatal("Failed to create colder app: ", err)
+	}
+
+	// Add 5 app items to the folder.
+	for i := 0; i < 5; i++ {
+		launcher.DragIconToIcon(ctx, tconn, ac)(ctx)
+	}
+
+	// Check that the folder has 7 items.
+	size, err := launcher.GetFolderSize(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get the folder size: ", err)
+	}
+	if size != 7 {
+		s.Fatalf("Unexpected number of items in folder, got %d, want %d", size, 7)
+	}
+
+	// Remove 3 items from the folder.
+	for i := 0; i < 3; i++ {
+		if err := launcher.RemoveIconFromFolder(tconn)(ctx); err != nil {
+			s.Fatal("Failed to remove icon from folder: ", err)
+		}
+	}
+
+	// Check that the folder has 4 items.
+	size, err = launcher.GetFolderSize(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get the folder size: ", err)
+	}
+	if size != 4 {
+		s.Fatalf("Unexpected number of items in folder, got %d, want %d", size, 4)
+	}
+
+	// Remove 3 items from the folder.
+	for i := 0; i < 3; i++ {
+		if err := launcher.RemoveIconFromFolder(tconn)(ctx); err != nil {
+			s.Fatal("Failed to remove icon from folder: ", err)
+		}
+	}
+
+	// Check that there is no longer a folder.
+	folder := nodewith.Name("Folder Unnamed").ClassName("AppListItemView")
+	if err := ac.Gone(folder)(ctx); err != nil {
+		s.Fatal("Folder exists when it should not.: ", err)
+	}
+}
--- src/chromiumos/tast/local/chrome/uiauto/launcher/launcher.go
+++ src/chromiumos/tast/local/chrome/uiauto/launcher/launcher.go
@@ -15,8 +15,10 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/mouse"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/input"
 )
 
@@ -173,4 +175,182 @@ func RenameFolder(tconn *chrome.TestConn, kb *input.KeyboardEventWriter, from, t
 		},
 		ui.WaitUntilExists(toFolder),
 	)
+}
+
+// CreateFolder is a helper function to create a folder by dragging the first icon on top of the second icon.
+func CreateFolder(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.Context) uiauto.Action {
+	// Create a folder in launcher by dragging the first icon on top of the second icon.
+	folder := nodewith.Name("Folder Unnamed").ClassName(ExpandedItemsClass)
+
+	return uiauto.Combine("createFolder",
+		DragIconToIcon(ctx, tconn, ui),
+		ui.WaitUntilExists(folder),
+	)
+}
+
+// DragIconToIcon drags from one icon to another icon.
+// We cannot use simple mouse.Drag because this CL https://crrev.com/c/2937656 changes the default UI behavior
+// which causes the location of icons to change after a mouse press on an icon.
+// This function will delay the calculation of the coordation of the destination until after mouse press.
+func DragIconToIcon(pctx context.Context, tconn *chrome.TestConn, ui *uiauto.Context) func(ctx context.Context) error {
+	const duration = time.Second
+	return func(ctx context.Context) error {
+		src := nodewith.ClassName(ExpandedItemsClass).Nth(1)
+		start, err := ui.Location(ctx, src)
+		if err != nil {
+			return errors.Wrap(err, "failed to get location for first icon")
+		}
+		if err := mouse.Move(tconn, start.CenterPoint(), 0)(ctx); err != nil {
+			return errors.Wrap(err, "failed to move to the start location")
+		}
+		if err := mouse.Press(tconn, mouse.LeftButton)(ctx); err != nil {
+			return errors.Wrap(err, "failed to press the button")
+		}
+
+		// Move a little bit first to trigger launcher-app-paging.
+		if err := mouse.Move(tconn, start.CenterPoint().Add(coords.Point{X: 1, Y: 1}), duration)(ctx); err != nil {
+			return errors.Wrap(err, "failed to move the mouse")
+		}
+
+		// Get destination location during drag.
+		dest := nodewith.ClassName(ExpandedItemsClass).Nth(0)
+		end, err := ui.Location(ctx, dest)
+		if err != nil {
+			return errors.Wrap(err, "failed to get location for second icon")
+		}
+		if err := mouse.Move(tconn, end.CenterPoint(), 200*time.Millisecond)(ctx); err != nil {
+			return errors.Wrap(err, "failed to move the mouse")
+		}
+		return mouse.Release(tconn, mouse.LeftButton)(ctx)
+	}
+}
+
+// RemoveIconFromFolder opens a folder and drags an icon out of the folder.
+func RemoveIconFromFolder(tconn *chrome.TestConn) uiauto.Action {
+	return func(ctx context.Context) error {
+		ui := uiauto.New(tconn)
+		src := nodewith.Name("Folder Unnamed").ClassName(ExpandedItemsClass)
+		folderLocation, err := ui.Location(ctx, src)
+		if err != nil {
+			return errors.Wrap(err, "failed to get location for first icon")
+		}
+
+		// Click to open the folder.
+		if err := mouse.Click(tconn, folderLocation.CenterPoint(), mouse.LeftButton)(ctx); err != nil {
+			return errors.Wrap(err, "failed to click the folder")
+		}
+
+		// Get the location for the first item in the folder.
+		folderItems := nodewith.ClassName(ExpandedItemsClass).Ancestor(nodewith.ClassName("AppListFolderView"))
+		start, err := ui.Location(ctx, folderItems.Nth(0))
+		if err != nil {
+			return errors.Wrap(err, "failed to get the location of the first folder item")
+		}
+
+		// Get a point outside of the folder view.
+		folderView := nodewith.ClassName("AppListFolderView")
+		folderViewLocation, err := ui.Location(ctx, folderView)
+		if err != nil {
+			return errors.Wrap(err, "failed to get folderViewLocation")
+		}
+		pointOutsideFolder := coords.NewPoint(folderViewLocation.Right()+50, folderViewLocation.CenterY()+50)
+
+		// Drag the first folder item outside of the folder.
+		mouse.Move(tconn, start.CenterPoint(), 0)(ctx)
+		mouse.Press(tconn, mouse.LeftButton)(ctx)
+		mouse.Move(tconn, pointOutsideFolder, time.Second)(ctx)
+
+		// Get the location of the folder during the drag.
+		folderLocation, err = ui.Location(ctx, src)
+		if err != nil {
+			return errors.Wrap(err, "failed to get location for folder")
+		}
+
+		// Drag and release the app to the right of the folder.
+		mouse.Move(tconn, folderLocation.CenterPoint().Add(coords.Point{X: folderLocation.Width, Y: 0}), time.Second)(ctx)
+		return mouse.Release(tconn, mouse.LeftButton)(ctx)
+
+	}
+}
+
+// DragIconToNextPage drags an icon to the next page of the app list.
+func DragIconToNextPage(tconn *chrome.TestConn) uiauto.Action {
+	return func(ctx context.Context) error {
+		ui := uiauto.New(tconn)
+		src := nodewith.ClassName(ExpandedItemsClass).Nth(0)
+		start, err := ui.Location(ctx, src)
+		if err != nil {
+			return errors.Wrap(err, "failed to get location for first icon")
+		}
+		if err := mouse.Move(tconn, start.CenterPoint(), 0)(ctx); err != nil {
+			return errors.Wrap(err, "failed to move to the start location")
+		}
+		if err := mouse.Press(tconn, mouse.LeftButton)(ctx); err != nil {
+			return errors.Wrap(err, "failed to press the icon")
+		}
+
+		// Move a little bit first to trigger launcher-app-paging.
+		if err := mouse.Move(tconn, start.CenterPoint().Add(coords.Point{X: 1, Y: 1}), time.Second)(ctx); err != nil {
+			return errors.Wrap(err, "failed to move the mouse")
+		}
+
+		// Get destination location during drag.
+		end, err := ui.Location(ctx, nodewith.ClassName("AppsGridView"))
+		if err != nil {
+			return errors.Wrap(err, "failed to get location for AppsGridView")
+		}
+		endPoint := coords.NewPoint(end.CenterPoint().X, end.Bottom())
+
+		// Drag icon to the bottom of the AppsGridView.
+		if err := mouse.Move(tconn, endPoint, 200*time.Millisecond)(ctx); err != nil {
+			return errors.Wrap(err, "failed to move the mouse")
+		}
+
+		// Move a little bit first to trigger page change.
+		if err := mouse.Move(tconn, endPoint.Add(coords.Point{X: 1, Y: 0}), time.Second)(ctx); err != nil {
+			return errors.Wrap(err, "failed to move the mouse")
+		}
+
+		// Move icon to near the top left of the AppsGridView and release to place the icon at the start of the page.
+		if err := mouse.Move(tconn, end.TopLeft().Add(coords.Point{X: 10, Y: 10}), 200*time.Millisecond)(ctx); err != nil {
+			return errors.Wrap(err, "failed to move to near the top left of the AppsGridView")
+		}
+		return mouse.Release(tconn, mouse.LeftButton)(ctx)
+	}
+}
+
+// GetFolderSize finds an existing folder and returns the number of apps inside
+func GetFolderSize(ctx context.Context, tconn *chrome.TestConn) (int, error) {
+	ui := uiauto.New(tconn)
+	src := nodewith.Name("Folder Unnamed").ClassName(ExpandedItemsClass)
+	folderLocation, err := ui.Location(ctx, src)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to get location for the folder")
+	}
+
+	// Click to open the folder.
+	if err := mouse.Click(tconn, folderLocation.CenterPoint(), mouse.LeftButton)(ctx); err != nil {
+		return 0, errors.Wrap(err, "failed to Click the folder")
+	}
+
+	folderView := nodewith.ClassName("AppListFolderView")
+
+	folderItems := nodewith.ClassName("AppListItemView").Ancestor(folderView)
+	folderItemsInfo, err := ui.NodesInfo(ctx, folderItems)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to find folderItemsInfo")
+	}
+
+	// Get and click on a location outside of the folder to close it.
+	folderViewLocation, err := ui.Location(ctx, folderView)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to get folderViewLocation")
+	}
+	pointOutsideFolder := coords.NewPoint(folderViewLocation.Right()+50, folderViewLocation.CenterY()+50)
+	// Click to open the folder.
+	if err := mouse.Click(tconn, pointOutsideFolder, mouse.LeftButton)(ctx); err != nil {
+		return 0, errors.Wrap(err, "failed to click outside of the folder")
+	}
+
+	return len(folderItemsInfo), nil
 }
