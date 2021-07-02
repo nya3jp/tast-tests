b72028bef9c4660c0732c04e091457021f7d4127
Alan Ding <alanding@google.com>
2021-07-01 19:16:46 -0700

    Refactor uses of sync and drop_caches for tast tests
    Currently, sync and drop_caches are used widely for performance
    and PFQ tast tests.  These are disk, IO, and memory operations used
    to clear as much clean file caches as possible for accurate benchmark.
    
    BUG=b:192615769
    TEST=tast run $DUT arc.AppLoadingPerf/AuthPerf/CachePerf/DataCollector.*
    
    Change-Id: I52eae98f851e336ae5feb41cac807457c021bbe5

--- src/chromiumos/tast/local/bundles/cros/arc/nethelper/nethelper.go
+++ src/chromiumos/tast/local/bundles/cros/arc/nethelper/nethelper.go
@@ -32,6 +32,7 @@ import (
 
 	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/disk"
 	"chromiumos/tast/testing"
 )
 
@@ -275,12 +276,8 @@ func handleClient(ctx context.Context, conn net.Conn) {
 }
 
 func handleDropCaches(ctx context.Context) string {
-	if err := testexec.CommandContext(ctx, "sync").Run(testexec.DumpLogOnError); err != nil {
-		testing.ContextLogf(ctx, "Failed to flush buffers: %s", err)
-		return failedResponse
-	}
-	if err := ioutil.WriteFile("/proc/sys/vm/drop_caches", []byte("3"), 0200); err != nil {
-		testing.ContextLogf(ctx, "Failed to drop caches: %s", err)
+	if err := disk.DropCaches(ctx, disk.ClearAllCaches); err != nil {
+		testing.ContextLog(ctx, "Failed to drop caches with error: ", err)
 		return failedResponse
 	}
 	return okResponse
---
+++ src/chromiumos/tast/local/disk/drop_caches.go
@@ -0,0 +1,46 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package disk
+
+import (
+	"context"
+	"io/ioutil"
+	"strconv"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+)
+
+// DropCachesOptions is an enum for `drop_caches` process options below.
+// - '1': Clears page caches
+// - '2': Clears dentries and inodes
+// - '3': Clears page caches, dentries and inodes
+type DropCachesOptions int
+
+// DropCachesOptions has three valid values 1-3.
+const (
+	ClearPageCaches = DropCachesOptions(1 << iota)
+	ClearDentriesInodes
+	ClearAllCaches
+)
+
+// DropCaches will write dirty pages to disks with 'sync' so that they are not
+// available for freeing and thus helps drop_caches to free more memory from
+// file system and IO operations. 'drop_caches' will clear clean page caches,
+// dentries (directory caches), and inodes (file caches).
+func DropCaches(ctx context.Context, value DropCachesOptions) error {
+	v := int(value)
+	if v < 1 || v > 3 {
+		return errors.Errorf("invalid drop_caches option: %d", v)
+	}
+
+	if err := testexec.CommandContext(ctx, "sync").Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "failed to flush buffers")
+	}
+	if err := ioutil.WriteFile("/proc/sys/vm/drop_caches", []byte(strconv.Itoa(v)), 0200); err != nil {
+		return errors.Wrap(err, "failed to clear caches")
+	}
+	return nil
+}
