18fee21da1ec19082b9e808d5d681d1aaf544b8e
Jun Sheng <shengjun@google.com>
2021-07-06 09:52:56 +1000

    tast-tests: run physical keyboard tests on stable boards
    Apply the same practice as other inputs tests, starting with stable(popular) boards first, then gradually extend to more.
    
    BUG=b:192612323
    TEST=None
    
    Change-Id: I87fac87af2dc13e5ef5d58dbf000fa06e6554411

--- src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_input_fields.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_input_fields.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,12 +9,14 @@ import (
 	"time"
 
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/testserver"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ime"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -26,6 +28,7 @@ func init() {
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
 		Fixture:      "chromeLoggedInForInputs",
 		Timeout:      5 * time.Minute,
+		HardwareDeps: hwdep.D(pre.InputsStableModels),
 		Params: []testing.Param{
 			{
 				Name: "us_en",
