38cf0cd2009c367bc06b7021836d568bd5cf3417
John Palmer <jopalmer@chromium.org>
2021-07-04 23:40:03 -0600

    [Inputs] Add test for new IME settings.
    At the same time, removes some hacky JS in the old test.
    
    Will require a chromium commit to fix up some stuff in the a11y tree.
    
    BUG=b:193580690
    TEST=DUT
    
    Change-Id: I6bca7be14ab66cb9d094fecfc988ba0c6c136e2c

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_english_settings.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_english_settings.go
@@ -6,7 +6,6 @@ package inputs
 
 import (
 	"context"
-	"fmt"
 	"strings"
 	"time"
 
@@ -14,8 +13,10 @@ import (
 	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/testserver"
 	"chromiumos/tast/local/bundles/cros/inputs/util"
+	"chromiumos/tast/local/chrome/ime"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/imesettings"
 	"chromiumos/tast/local/chrome/uiauto/vkb"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
@@ -52,7 +53,7 @@ func VirtualKeyboardEnglishSettings(ctx context.Context, s *testing.State) {
 	defer func() {
 		if err := tconn.Eval(cleanupCtx, `chrome.inputMethodPrivate.setSettings(
 			"xkb:us::eng",
-			{"virtualKeyboardEnableCapitalization": false,
+			{"virtualKeyboardEnableCapitalization": true,
 			"virtualKeyboardAutoCorrectionLevel": 1})`, nil); err != nil {
 			s.Log("Failed to revert language settings")
 		}
@@ -72,6 +73,7 @@ func VirtualKeyboardEnglishSettings(ctx context.Context, s *testing.State) {
 		capitalizationEnabled bool
 		keySeq                []string
 		expectedText          string
+		ime                   ime.InputMethod
 	}
 
 	subTests := []testData{
@@ -80,24 +82,31 @@ func VirtualKeyboardEnglishSettings(ctx context.Context, s *testing.State) {
 			capitalizationEnabled: true,
 			keySeq:                strings.Split("Hello", ""),
 			expectedText:          "Hello",
+			ime:                   ime.EnglishUS,
 		}, {
 			name:                  "capitalizationDisabled",
 			capitalizationEnabled: false,
 			keySeq:                strings.Split("hello", ""),
 			expectedText:          "hello",
+			ime:                   ime.EnglishUS,
 		},
 	}
 
 	for _, subTest := range subTests {
 		s.Run(ctx, subTest.name, func(ctx context.Context, s *testing.State) {
 			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+subTest.name)
-
-			// TODO(b/172498469): Change settings via Chrome OS settings page after we migrate settings to there.
-			if err := tconn.Eval(ctx, fmt.Sprintf(`chrome.inputMethodPrivate.setSettings(
-				"xkb:us::eng",
-				{"virtualKeyboardEnableCapitalization": %t,
-				"virtualKeyboardAutoCorrectionLevel": 1})`, subTest.capitalizationEnabled), nil); err != nil {
-				s.Fatal("Failed to set settings: ", err)
+			if !subTest.capitalizationEnabled {
+				settings, err := imesettings.LaunchAtInputsSettingsPage(ctx, tconn, cr)
+				if err != nil {
+					s.Fatal("Failed to launch OS settings and land at inputs setting page: ", err)
+				}
+				if err := uiauto.Combine("test input method settings change",
+					settings.OpenInputMethodSetting(tconn, subTest.ime),
+					settings.ClickAutoCap(),
+					settings.Close(),
+				)(ctx); err != nil {
+					s.Fatal("Failed to change IME settings: ", err)
+				}
 			}
 
 			vkbCtx := vkb.NewContext(cr, tconn)
