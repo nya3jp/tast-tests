31584d561dd1772dc126307db9c1a04ed9157ff7
Tim Sergeant <tsergeant@google.com>
2021-07-06 17:22:09 +1000

    [CrOS WebAPKs] Add Android app to test sharing to WebAPKs
    This Android app just shows a button which shares some text to any
    installed WebAPK that accepts text shares.
    
    This will be used in a test where we install a WebAPK and assert that an
    Android app is able to share content through the WebAPK to a Web App.
    
    BUG=chromium:1226730
    TEST=emerge-$BOARD tast-local-apks-cros (Then installed the APK
      manually and verified the behavior)
    
    Change-Id: Ie47439548b89e1ac4ef4cce7b11612b28c7d34c4

---
+++ android/ArcChromeWebApkTest/src/org/chromium/arc/testapp/chromewebapk/MainActivity.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.chromewebapk;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.content.pm.ResolveInfo;
+import android.content.pm.PackageManager;
+
+import java.util.List;
+
+/**
+ * Main activity for the ArcChromeWebApk test app.
+ *
+ * <p>Shows buttons to share content. If a single WebAPK is installed, will share the content to
+ * that WebAPK. Otherwise, opens the system sharesheet.
+ */
+public final class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.content_view);
+
+        findViewById(R.id.share_text_button).setOnClickListener(v -> {
+            Intent textShareIntent = new Intent(Intent.ACTION_SEND);
+            textShareIntent.setType("text/plain");
+            textShareIntent.putExtra(Intent.EXTRA_SUBJECT, "Shared title");
+            textShareIntent.putExtra(Intent.EXTRA_TEXT, "Shared text");
+
+            startActivity(getWebApkTargetedIntent(textShareIntent));
+        });
+    }
+
+    private Intent getWebApkTargetedIntent(Intent shareIntent) {
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(shareIntent, 0);
+
+        String packageName = null;
+
+        for (ResolveInfo info : resolveInfos) {
+            if (info.activityInfo.packageName.startsWith("org.chromium.webapk.")) {
+                if (packageName != null) {
+                    // There are multiple WebAPKs able to handle the intent, show a Chooser.
+                    return Intent.createChooser(shareIntent, null);
+                }
+                packageName = info.activityInfo.packageName;
+            }
+        }
+
+        if (packageName == null) {
+            // There are no WebAPKs able to handle the intent, show a Chooser.
+            return Intent.createChooser(shareIntent, null);
+        }
+
+        Intent targetedIntent = new Intent(shareIntent);
+        targetedIntent.setPackage(packageName);
+        return targetedIntent;
+    }
+}
