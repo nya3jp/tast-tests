6a042b714c2726dec6bbc225b40d17d07c9a0e81
Charles William Dick <cwd@google.com>
2021-07-06 16:38:46 +0900

    memoryuser FillMemory* helpers
    Refactor memoryuser.MemoryTasks used by Lifecycle tests to be runnable
    outside of memoryuser.RunTest. Add helpers that fill ChromeOS, ARCVM, or
    Crostini memory up with lifecycle units.
    
    This makes adding load to a system much easier. As used by borealis
    tests.
    
    BUG=b:186824043
    TEST=See https://crrev.com/i/3794769
    
    Change-Id: I808d5089da22d8bd49dc9f122a68d092bff6e596

--- src/chromiumos/tast/local/memory/memoryuser/arc_lifecycle_task.go
+++ src/chromiumos/tast/local/memory/memoryuser/arc_lifecycle_task.go
@@ -19,30 +19,31 @@ import (
 	"chromiumos/tast/testing"
 )
 
-// ArcLifecycleTask launches an Android app which allocates memory.
-type ArcLifecycleTask struct {
+// ArcLifecycleUnit launches an Android app which allocates memory.
+type ArcLifecycleUnit struct {
 	id            int
 	allocateBytes int64
 	ratio         float64
 	limit         memory.Limit
 }
 
-// ArcLifecycleTask is a MemoryTest.
-var _ MemoryTask = (*ArcLifecycleTask)(nil)
+// ArcLifecycleUnitCount is the number of different ARC lifecycle app packages,
+// which is the number of different lifecycle apps we can run at the same time.
+const ArcLifecycleUnitCount = 100
 
 const arcLifecycleTestPkg = "org.chromium.arc.testapp.lifecycle"
 
-func (t *ArcLifecycleTask) packageName() string {
+func (t *ArcLifecycleUnit) packageName() string {
 	// E.g. "org.chromium.arc.testapp.lifecycle00".
 	return fmt.Sprintf("%s%02d", arcLifecycleTestPkg, t.id)
 }
 
-func (t *ArcLifecycleTask) mainActivity() string {
+func (t *ArcLifecycleUnit) mainActivity() string {
 	// E.g. "org.chromium.arc.testapp.lifecycle00/org.chromium.arc.testapp.lifecycle.MainActivity".
 	return fmt.Sprintf("%s/%s.MainActivity", t.packageName(), arcLifecycleTestPkg)
 }
 
-func (t *ArcLifecycleTask) intentAction(action string) string {
+func (t *ArcLifecycleUnit) intentAction(action string) string {
 	// E.g. "org.chromium.arc.testapp.lifecycle00.ALLOC".
 	return fmt.Sprintf("%s.%s", t.packageName(), action)
 }
@@ -50,7 +51,7 @@ func (t *ArcLifecycleTask) intentAction(action string) string {
 var amStartRE = regexp.MustCompile("(?m)^Status: ok$")
 
 // Run starts the AndroidLifecycleTest app, and uses it to allocate memory.
-func (t *ArcLifecycleTask) Run(ctx context.Context, testEnv *TestEnv) error {
+func (t *ArcLifecycleUnit) Run(ctx context.Context, a *arc.ARC) error {
 	// Android doesn't like launching apps with the display asleep...
 	if err := power.TurnOnDisplay(ctx); err != nil {
 		return errors.Wrap(err, "failed to turn on display")
@@ -67,7 +68,7 @@ func (t *ArcLifecycleTask) Run(ctx context.Context, testEnv *TestEnv) error {
 	testing.ContextLogf(ctx, "Starting %s", t.mainActivity())
 	sizeString := strconv.FormatInt(t.allocateBytes, 10)
 	ratioString := strconv.FormatFloat(t.ratio, 'f', -1, 64)
-	startOut, err := testEnv.arc.Command(
+	startOut, err := a.Command(
 		ctx,
 		"am", "start", "-W", "-n", t.mainActivity(),
 		"--el", "size", sizeString,
@@ -82,7 +83,7 @@ func (t *ArcLifecycleTask) Run(ctx context.Context, testEnv *TestEnv) error {
 
 	// Wait for allocation to complete.
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		done, err := t.doneAllocating(ctx, testEnv.arc)
+		done, err := t.doneAllocating(ctx, a)
 		if err != nil {
 			return err
 		}
@@ -97,24 +98,14 @@ func (t *ArcLifecycleTask) Run(ctx context.Context, testEnv *TestEnv) error {
 }
 
 // Close kills the AndroidLifecycleTest app.
-func (t *ArcLifecycleTask) Close(ctx context.Context, testEnv *TestEnv) {
-	err := testEnv.arc.Command(ctx, "am", "force-stop", t.packageName()).Run(testexec.DumpLogOnError)
+func (t *ArcLifecycleUnit) Close(ctx context.Context, a *arc.ARC) {
+	err := a.Command(ctx, "am", "force-stop", t.packageName()).Run(testexec.DumpLogOnError)
 	if err != nil {
 		testing.ContextLogf(ctx, "Failed to Close %q: %v", t.packageName(), err)
 	}
 }
 
-// String returns a friendly string containing the
-func (t *ArcLifecycleTask) String() string {
-	return fmt.Sprintf("ARC lifecycle%02d", t.id)
-}
-
-// NeedVM returns false, because we don't need Crostini.
-func (t *ArcLifecycleTask) NeedVM() bool {
-	return false
-}
-
-func (t *ArcLifecycleTask) doneAllocating(ctx context.Context, arc *arc.ARC) (bool, error) {
+func (t *ArcLifecycleUnit) doneAllocating(ctx context.Context, arc *arc.ARC) (bool, error) {
 	data, err := arc.BroadcastIntentGetData(ctx, t.intentAction("DONE"))
 	if err != nil {
 		return false, err
@@ -126,10 +117,94 @@ func (t *ArcLifecycleTask) doneAllocating(ctx context.Context, arc *arc.ARC) (bo
 	return done, nil
 }
 
-// StillAlive uses ps to see if the App is still running.
-func (t *ArcLifecycleTask) StillAlive(ctx context.Context, testEnv *TestEnv) bool {
-	_, err := t.doneAllocating(ctx, testEnv.arc)
+// StillAlive checks to see if the AndroidLifecycleTest app is alive and
+// responding to intents.
+func (t *ArcLifecycleUnit) StillAlive(ctx context.Context, a *arc.ARC) bool {
+	_, err := t.doneAllocating(ctx, a)
 	return err == nil
+}
+
+// NewArcLifecycleUnit creates a helper to allocate memory inside an app in ARC.
+//  id            - Which copy of ArcLifecycleTest app to use.
+//  allocateBytes - mow much memory to allocate.
+//  ratio         - the compression ratio of allocated memory.
+//  limit         - if not nil, wait for Limit after allocation.
+func NewArcLifecycleUnit(id int, allocateBytes int64, ratio float64, limit memory.Limit) *ArcLifecycleUnit {
+	return &ArcLifecycleUnit{id, allocateBytes, ratio, limit}
+}
+
+// FillArcMemory installs, launches, and allocated in ArcLifecycleTest apps
+// until one is killed, filling up memory in ARC.
+func FillArcMemory(ctx context.Context, a *arc.ARC, unitBytes int64, ratio float64) (func(context.Context) error, error) {
+	var units []*ArcLifecycleUnit
+	cleanup := func(ctx context.Context) error {
+		var res error
+		for _, unit := range units {
+			// Uninstalling the APK will also kill the app, if it's running.
+			if err := a.Uninstall(ctx, unit.packageName()); err != nil {
+				testing.ContextLogf(ctx, "Failed to uninstall %q: %s", unit.packageName(), err)
+				if res == nil {
+					res = err
+				}
+			}
+		}
+		return res
+	}
+	for i := 0; i < ArcLifecycleUnitCount; i++ {
+		if err := a.Install(ctx, androidLifecycleTestAPKPath(i)); err != nil {
+			return cleanup, errors.Wrapf(err, "failed to install %q", androidLifecycleTestAPKPath(i))
+		}
+		unit := NewArcLifecycleUnit(i, unitBytes, ratio, nil)
+		units = append(units, unit)
+		if err := unit.Run(ctx, a); err != nil {
+			return cleanup, errors.Wrapf(err, "failed to run ArcLifecycleUnit %d", i)
+		}
+		for _, unit := range units {
+			if !unit.StillAlive(ctx, a) {
+				testing.ContextLogf(ctx, "FillArcMemory started %d units of %d MiB before first kill", len(units), unitBytes/memory.MiB)
+				return cleanup, nil
+			}
+		}
+	}
+	return cleanup, errors.Errorf("started %d ArcLifecycleUnits, but all are still alive", ArcLifecycleUnitCount)
+}
+
+// ArcLifecycleTask wraps ArcLifecycleUnit to conform to the MemoryTask and
+// KillableTask interfaces.
+type ArcLifecycleTask struct {
+	ArcLifecycleUnit
+}
+
+// ArcLifecycleTask is a MemoryTask.
+var _ MemoryTask = (*ArcLifecycleTask)(nil)
+
+// ArcLifecycleTask is a KillableTask.
+var _ KillableTask = (*ArcLifecycleTask)(nil)
+
+// String returns a friendly string containing the
+func (t *ArcLifecycleTask) String() string {
+	return fmt.Sprintf("ARC lifecycle%02d", t.id)
+}
+
+// NeedVM returns false, because we don't need Crostini.
+func (t *ArcLifecycleTask) NeedVM() bool {
+	return false
+}
+
+// Run starts the AndroidLifecycleTest app, and uses it to allocate memory.
+func (t *ArcLifecycleTask) Run(ctx context.Context, testEnv *TestEnv) error {
+	return t.ArcLifecycleUnit.Run(ctx, testEnv.arc)
+}
+
+// Close kills the AndroidLifecycleTest app.
+func (t *ArcLifecycleTask) Close(ctx context.Context, testEnv *TestEnv) {
+	t.ArcLifecycleUnit.Close(ctx, testEnv.arc)
+}
+
+// StillAlive checks to see if the AndroidLifecycleTest app is alive and
+// responding to intents.
+func (t *ArcLifecycleTask) StillAlive(ctx context.Context, testEnv *TestEnv) bool {
+	return t.ArcLifecycleUnit.StillAlive(ctx, testEnv.arc)
 }
 
 // NewArcLifecycleTask creates a MemoryTask that runs the ArcLifecycleTest app
@@ -139,7 +214,7 @@ func (t *ArcLifecycleTask) StillAlive(ctx context.Context, testEnv *TestEnv) boo
 //  ratio         - the compression ratio of allocated memory.
 //  limit         - if not nil, wait for Limit after allocation.
 func NewArcLifecycleTask(id int, allocateBytes int64, ratio float64, limit memory.Limit) *ArcLifecycleTask {
-	return &ArcLifecycleTask{id, allocateBytes, ratio, limit}
+	return &ArcLifecycleTask{*NewArcLifecycleUnit(id, allocateBytes, ratio, limit)}
 }
 
 func androidLifecycleTestAPKPath(id int) string {
@@ -148,6 +223,9 @@ func androidLifecycleTestAPKPath(id int) string {
 
 // InstallArcLifecycleTestApps installs 'howMany' copies of ArcLifecycleTest.
 func InstallArcLifecycleTestApps(ctx context.Context, a *arc.ARC, num int) error {
+	if num > ArcLifecycleUnitCount {
+		return errors.Errorf("requested %d number of apps to install, which is higher than the max of %d", num, ArcLifecycleUnitCount)
+	}
 	testing.ContextLogf(ctx, "Installing %d copies of AndroidLifecycleTest app", num)
 	for i := 0; i < num; i++ {
 		if err := a.Install(ctx, androidLifecycleTestAPKPath(i)); err != nil {
