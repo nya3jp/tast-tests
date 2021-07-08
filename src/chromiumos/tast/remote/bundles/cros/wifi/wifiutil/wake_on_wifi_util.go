36400410cc178ab08b84028b00e1557dbd562f0b
Yen-lin Lai <yenlinlai@chromium.org>
2021-07-08 17:36:54 +0800

    wifi: Port network_WiFi_ReconnectInDarkResume to Tast
    Port the old lucid_sleep test to Tast.
    
    Drop the dark resume count check as it highly depends on the internal
    implementation and the AP configuration time. Instead, we just expect
    the DUT to reconnect in a reasonable time. e.g. 1 min
    
    note: currently disconnect_before_suspend_* variation will fail, which
    might be related to b/188743774.
    
    BUG=b:189407934
    TEST=tast run -var "servo=$SERVO" $DUT wifi.ReconnectInDarkResume.*
    
    Change-Id: I4e6093415e7cb33a1b57838c1c57900055672a56

--- src/chromiumos/tast/remote/bundles/cros/wifi/wifiutil/wake_on_wifi_util.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/wifiutil/wake_on_wifi_util.go
@@ -8,10 +8,13 @@ import (
 	"context"
 	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/dut"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/servo"
 	"chromiumos/tast/remote/wificell"
 	"chromiumos/tast/services/cros/wifi"
+	"chromiumos/tast/testing"
 )
 
 // VerifyWakeOnWifiReason puts the DUT into suspend with timeout=duration,
@@ -76,4 +79,87 @@ func VerifyWakeOnWifiReason(
 		return err
 	}
 	return nil
+}
+
+// DUTActive access ex_system_powerstate to tell if the DUT is in active state.
+func DUTActive(ctx context.Context, servoInst *servo.Servo) (bool, error) {
+	state, err := servoInst.GetECSystemPowerState(ctx)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to get ec_system_power_state")
+	}
+	testing.ContextLog(ctx, "state: ", state)
+	if state == "S0" {
+		return true, nil
+	}
+	return false, nil
+}
+
+// WaitDUTActive uses servo to wait for DUT to reach/leave active (S0) state.
+func WaitDUTActive(ctx context.Context, servoInst *servo.Servo, expectActive bool, timeout time.Duration) error {
+	var expectedDesc string
+	if expectActive {
+		expectedDesc = "active"
+	} else {
+		expectedDesc = "inactive"
+	}
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		active, err := DUTActive(ctx, servoInst)
+		if err != nil {
+			return testing.PollBreak(err)
+		}
+		if active != expectActive {
+			return errors.Errorf("DUT is not yet %s", expectedDesc)
+		}
+		return nil
+	}, &testing.PollOptions{
+		Timeout:  timeout,
+		Interval: time.Second,
+	}); err != nil {
+		return errors.Wrapf(err, "failed to wait for DUT to become %s", expectedDesc)
+	}
+	return nil
+}
+
+// DarkResumeSuspend suspends the DUT with powerd_dbus_suspend with dark resume
+// enabled. On successful call, a shortened context and a cleanup function to
+// wake up the DUT are returned.
+func DarkResumeSuspend(fullCtx context.Context, d *dut.DUT, servoInst *servo.Servo) (context.Context, func() error, error) {
+	ctx, cancel := ctxutil.Shorten(fullCtx, 10*time.Second)
+
+	done := make(chan error, 1)
+	go func(ctx context.Context) {
+		defer close(done)
+		out, err := d.Command("powerd_dbus_suspend", "--disable_dark_resume=false", "--print_wakeup_type=true").CombinedOutput(ctx)
+		testing.ContextLog(ctx, "DEBUG: powerd_dbus_suspend output: ", string(out))
+		done <- err
+	}(ctx)
+
+	if err := WaitDUTActive(ctx, servoInst, false, 10*time.Second); err != nil {
+		cancel()
+		return ctx, nil, errors.Wrap(err, "failed to wait for DUT to be suspended")
+	}
+	cleanupFunc := func() error {
+		// Cancel the context used by the goroutine before leaving.
+		defer cancel()
+
+		active, err := DUTActive(fullCtx, servoInst)
+		if err != nil {
+			return err
+		}
+		if !active {
+			testing.ContextLog(ctx, "Try to wake up DUT with power_key:press")
+			if err := servoInst.KeypressWithDuration(fullCtx, servo.PowerKey, servo.DurPress); err != nil {
+				return errors.Wrap(err, "failed to trigger power key press with servo")
+			}
+			if err := WaitDUTActive(ctx, servoInst, true, 10*time.Second); err != nil {
+				return errors.New("failed to wake up DUT with servo")
+			}
+			testing.ContextLog(ctx, "DUT back to active")
+		}
+		if err := <-done; err != nil {
+			return errors.Wrap(err, "powerd_dbus_suspend failed")
+		}
+		return nil
+	}
+	return ctx, cleanupFunc, nil
 }
