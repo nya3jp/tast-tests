1dc46a7f006e2cb55afb5c3777f9a50c0bbdac92
George Popoola <gpopoola@google.com>
2021-07-08 19:06:16 +0000

    WIP: Create Tast tests for powerd plugin on fwupd
    This CL will create tests that run commands or processes to test the
    behavior and efficacy of a powerd plugin for fwupd.
    
    THe first test, written in fwupd_powerd_startup.go, runs the command
    "fwupdmgr get-plugins" and searches for the key word "powerd" in order
    to test whether it shows up on the list on enabled plugins.
    
    Currently, this test mentioned below fails due to an unused error,
    according to the output.
    
    BUG=b:193158345
    TEST= tast run [user@]host[:port] fwupd_powerd_startup.go
    
    Change-Id: I7490d457bc29132e940ab354823689a6351e5310

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_powerd_startup.go
@@ -0,0 +1,59 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"regexp"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+const (
+	targetStr = `.*powerd`
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FwupdPowerdEnabled,
+		Desc: "Checks that the powerd plugin is enabled",
+		Contacts: []string{
+			"gpopoola@google.com",       // Test Author
+			"chromeos-fwupd@google.com", // CrOS FWUPD
+		},
+		Attr:         []string{"group:mainline"},
+		SoftwareDeps: []string{"fwupd"},
+	})
+}
+
+/* checkForPowerdStr verifies that powerd was found among enabled plugins */
+func checkForPowerdStr(output []byte) error {
+	matched, err := regexp.Match(targetStr, output)
+	if err != nil {
+		return err
+	}
+	if !matched {
+		return errors.New("powerd was not found among enabled plugins")
+	}
+	return nil
+}
+
+/* FwupdPowerdEnabled runs fwupdmgr get-plugins, retrieves the output, and checks for powerd */
+func FwupdPowerdEnabled(ctx context.Context, s *testing.State) {
+	cmd := testexec.CommandContext(ctx, "/usr/bin/fwupdmgr", "get-plugins")
+
+	output, err := cmd.Output(testexec.DumpLogOnError)
+
+	if err != nil {
+		s.Fatalf("%q failed: %v", shutil.EscapeSlice(cmd.Args), err)
+	}
+
+	if err != checkForPowerdStr(output); err != nil {
+		s.Fatalf("powerd was not compiled: ", err)
+	}
+}
