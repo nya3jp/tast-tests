7af463c9f65aae51c0725a13a14483aee3676b42
Tim Sergeant <tsergeant@chromium.org>
2021-07-09 14:08:45 +1000

    [CrOS WebAPKs] Add test to verify sharing through a WebAPK
    This adds a new test which shares text data from an Android app to
    a Web app via a pregenerated WebAPK. In production the APK would
    automatically generated and installed when the PWA is installed, but
    here we do it manually since we're not sure how stable the generation
    code would be in a test environment.
    
    This test can also be extended to test sharing files, once this behavior
    is supported on the Chrome side.
    
    BUG=chromium:1226730
    TEST=tast run $DUT appsplatform.WebAPK
    
    Change-Id: I3ceba07509f746fb3eaa6aa8f2a8f3417a52a57f

---
+++ src/chromiumos/tast/local/bundles/cros/appsplatform/webapk.go
@@ -0,0 +1,218 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package appsplatform
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/testing"
+)
+
+type shareResult struct {
+	text  string
+	title string
+	err   error
+}
+
+const (
+	// A pre-generated WebAPK which points to a PWA installed from localhost:8000.
+	generatedWebAPK = "WebShareTargetTestWebApk_20210707.apk"
+
+	localServerAddr = "localhost:8000"
+
+	testPackage = "org.chromium.arc.testapp.chromewebapk"
+	testClass   = "org.chromium.arc.testapp.chromewebapk.MainActivity"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: WebAPK,
+		Desc: "Checks that a WebAPK can be used to share data to a web app",
+		Contacts: []string{
+			"tsergeant@chromium.org",
+			"jinrongwu@chromium.org",
+			"chromeos-apps-foundation-team@google.com",
+		},
+		Attr:    []string{"group:mainline", "informational"},
+		Fixture: "arcBootedWithWebAppSharing",
+		Data: []string{
+			"webshare_icon.png",
+			"webshare_index.html",
+			"webshare_manifest.json",
+			"webshare_service.js",
+			generatedWebAPK,
+		},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+	})
+}
+
+// WebAPK verifies that sharing to a WebAPK launches the corresponding Web App
+// with the shared data attached.
+func WebAPK(ctx context.Context, s *testing.State) {
+	p := s.FixtValue().(*arc.PreData)
+	cr := p.Chrome
+	a := p.ARC
+
+	// Reserve time for cleanup operations.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	s.Log("Starting test PWA Server")
+	server, shareChan := startTestPWAServer(ctx, s.DataFileSystem())
+	defer server.Shutdown(cleanupCtx)
+	defer close(shareChan)
+
+	s.Log("Installing test apps")
+	if err := installTestApps(ctx, cr, a, tconn, s.DataPath(generatedWebAPK)); err != nil {
+		s.Fatal("Error installing test apps: ", err)
+	}
+
+	s.Log("Launching test app")
+	device, activity, err := launchTestApp(ctx, a, tconn)
+	if err != nil {
+		s.Fatal("Error launching test app: ", err)
+	}
+	defer activity.Close()
+	defer activity.Stop(cleanupCtx, tconn)
+	defer device.Close(cleanupCtx)
+
+	s.Log("Sharing from Android to Web")
+	if err := shareTextAndVerify(ctx, device, shareChan); err != nil {
+		s.Fatal("Error sharing text from test app: ", err)
+	}
+}
+
+func startTestPWAServer(ctx context.Context, filesystem http.FileSystem) (*http.Server, chan shareResult) {
+	shareChan := make(chan shareResult)
+	mux := http.NewServeMux()
+	fs := http.FileServer(filesystem)
+	mux.Handle("/", fs)
+	mux.HandleFunc("/share", func(w http.ResponseWriter, r *http.Request) {
+		if parseErr := r.ParseMultipartForm(4096); parseErr != nil {
+			shareChan <- shareResult{err: errors.Wrap(parseErr, "failed to parse multipart form")}
+			return
+		}
+
+		sharedText := ""
+		if len(r.MultipartForm.Value["text"]) == 1 {
+			sharedText = r.MultipartForm.Value["text"][0]
+		}
+		sharedTitle := ""
+		if len(r.MultipartForm.Value["title"]) == 1 {
+			sharedTitle = r.MultipartForm.Value["title"][0]
+		}
+
+		shareChan <- shareResult{text: sharedText, title: sharedTitle}
+	})
+
+	server := &http.Server{Addr: localServerAddr, Handler: mux}
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			shareChan <- shareResult{err: errors.Wrap(err, "failed to start local server")}
+		}
+	}()
+
+	return server, shareChan
+}
+
+func installTestApps(ctx context.Context, cr *chrome.Chrome, a *arc.ARC, tconn *chrome.TestConn, webAPKPath string) error {
+	const (
+		localServerIndex = "http://" + localServerAddr + "/webshare_index.html"
+		installTimeout   = 15 * time.Second
+		testAPK          = "ArcChromeWebApkTest.apk"
+	)
+
+	appID, err := apps.InstallPWAForURL(ctx, cr, localServerIndex, installTimeout)
+	if err != nil {
+		return errors.Wrap(err, "failed to install PWA for URL")
+	}
+
+	if err := ash.WaitForChromeAppInstalled(ctx, tconn, appID, installTimeout); err != nil {
+		return errors.Wrap(err, "failed to wait for PWA to be installed")
+	}
+	if err := a.Install(ctx, webAPKPath); err != nil {
+		return errors.Wrap(err, "failed installing WebAPK")
+	}
+	if err := a.Install(ctx, arc.APKPath(testAPK)); err != nil {
+		return errors.Wrap(err, "failed installing test app")
+	}
+	return nil
+
+}
+
+func launchTestApp(ctx context.Context, a *arc.ARC, tconn *chrome.TestConn) (*ui.Device, *arc.Activity, error) {
+	activity, err := arc.NewActivity(a, testPackage, testClass)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to create a new activity")
+	}
+	if err := activity.Start(ctx, tconn); err != nil {
+		return nil, nil, errors.Wrap(err, "failed to start the test activity")
+	}
+
+	device, err := a.NewUIDevice(ctx)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed initializing UI Automator")
+	}
+
+	if err := device.WaitForIdle(ctx, 5*time.Second); err != nil {
+		return nil, nil, errors.Wrap(err, "failed to wait for device idle")
+	}
+	return device, activity, nil
+}
+
+func shareTextAndVerify(ctx context.Context, device *ui.Device, shareChan chan shareResult) error {
+	const (
+		shareTextButtonID = testPackage + ":id/share_text_button"
+
+		expectedSharedTitle = "Shared title"
+		expectedSharedText  = "Shared text"
+	)
+
+	// Clicking the "Share Text" button will send share data directly to
+	// any installed WebAPK.
+	if err := device.Object(ui.ID(shareTextButtonID)).Click(ctx); err != nil {
+		return errors.Wrap(err, "failed to click share button")
+	}
+
+	var receivedShare shareResult
+	select {
+	case receivedShare = <-shareChan:
+	case <-ctx.Done():
+		return errors.New("timeout waiting to receive shared text")
+	}
+
+	if receivedShare.err != nil {
+		return errors.Wrap(receivedShare.err, "error received from test server")
+	}
+
+	if receivedShare.title != expectedSharedTitle {
+		return errors.Errorf("shared title did not match: got %q, want %q", receivedShare.title, expectedSharedTitle)
+	}
+
+	if receivedShare.text != expectedSharedText {
+		return errors.Errorf("Shared title did not match: got %q, want %q", receivedShare.text, expectedSharedText)
+	}
+	return nil
+}
