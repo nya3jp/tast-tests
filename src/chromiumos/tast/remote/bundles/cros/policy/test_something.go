b8d54403bb685746a347a6fc02f20d76f2bdccc2
alexanderhartl <alexanderhartl@google.com>
2021-07-09 14:12:46 +0000

    tast: just a test for auth in luci
    This CL will just test luci auth in a CQ Dry run and will then be abandoned
    
    BUG=None
    TEST=None
    
    Change-Id: I5583a40641e29891a6e3038dfe7f4d262b8ef0fe

---
+++ src/chromiumos/tast/remote/bundles/cros/policy/test_something.go
@@ -0,0 +1,76 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"os"
+	"os/user"
+	"path/filepath"
+	"time"
+
+	"golang.org/x/oauth2/google"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: TestSomething,
+		Desc: "Behavior of TestSomething policy",
+		Contacts: []string{
+			"alexanderhartl@google.com", // Test author
+			"chromeos-commercial-stability@google.com",
+		},
+		Attr:         []string{"group:mainline"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      1 * time.Minute,
+	})
+}
+
+func TestSomething(ctx context.Context, s *testing.State) {
+
+	const f = "application_default_credentials.json"
+
+	fileName := ""
+	if v := os.Getenv("HOME"); v != "" {
+		fileName = filepath.Join(v, ".config", "gcloud", f)
+	}
+	// Else, fall back to user.Current:
+	if u, err := user.Current(); err == nil {
+		fileName = filepath.Join(u.HomeDir, ".config", "gcloud", f)
+	}
+
+	s.Log(fileName)
+
+	const envVar = "GOOGLE_APPLICATION_CREDENTIALS"
+	filename := os.Getenv(envVar)
+	s.Log(filename)
+
+	creds, err := google.FindDefaultCredentials(ctx, "https://www.googleapis.com/auth/userinfo.email")
+	if err != nil {
+		s.Fatal("Failed to find default credentials: ", err, " ", fileName, " ", filename)
+	}
+	s.Log(creds)
+
+	ts, err := google.DefaultTokenSource(ctx, "https://www.googleapis.com/auth/userinfo.email")
+	if err != nil {
+		s.Fatal("Failed to create default token source: ", err)
+	}
+	s.Log(ts)
+
+	client, err := google.DefaultClient(ctx, "https://www.googleapis.com/auth/userinfo.email")
+	if err != nil {
+		s.Fatal("Failed to create client with default account: ", err)
+	}
+	s.Log(client)
+
+	// resp, err := client.Get("https://test-dot-tape-307412.appspot.com/test")
+	// if err != nil {
+	// 	s.Fatal("calling TAPE failed: ", err)
+	// }
+	// s.Log(resp)
+	s.Fatal("Failed successfully")
+}
