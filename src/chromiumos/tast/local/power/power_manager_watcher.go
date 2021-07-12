3909bcd8b81db695b2127a053a3433246508f116
Yen-lin Lai <yenlinlai@chromium.org>
2021-07-12 16:51:04 +0800

    tast-tests: power: Add NewSignalWatcher utility.
    Wake on WiFi feature depends on dark resume and its tests will need to
    verify that dark resumes are [not] triggered on specific cases.
    
    Export the signal names with constants and extract a utility for
    spawning the D-Bus signal watcher, so that other tests can share the
    logic.
    
    BUG=b:189409165
    TEST=tast run $DUT wilco.APIHandlePowerEvent
    
    Change-Id: I8bf91e21d48404d578f3d74f435c870089ca439e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3034705
    Tested-by: Yen-lin Lai <yenlinlai@chromium.org>
    Commit-Queue: Yen-lin Lai <yenlinlai@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>

---
+++ src/chromiumos/tast/local/power/power_manager_watcher.go
@@ -0,0 +1,45 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+	"strings"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+)
+
+// NewSignalWatcher creates an D-Bus signal watcher on PowerManager interface.
+func NewSignalWatcher(ctx context.Context, signalNames ...string) (*dbusutil.SignalWatcher, error) {
+	if len(signalNames) == 0 {
+		return nil, errors.New("no signal to watch")
+	}
+	var matches []dbusutil.MatchSpec
+	for _, signalName := range signalNames {
+		matches = append(matches, dbusutil.MatchSpec{
+			Type:      "signal",
+			Path:      dbus.ObjectPath(dbusPath),
+			Interface: dbusInterface,
+			Member:    signalName,
+		})
+	}
+	watcher, err := dbusutil.NewSignalWatcherForSystemBus(ctx, matches...)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create dbus watcher")
+	}
+	return watcher, nil
+}
+
+// SignalName extracts the signal name from a dbus.Signal object.
+func SignalName(s *dbus.Signal) string {
+	parts := strings.Split(s.Name, ".")
+	if len(parts) == 0 {
+		return ""
+	}
+	return parts[len(parts)-1]
+}
