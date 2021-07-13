7c8a309df67c155d5565dd788b5c42d1c5df10f9
Roman Arora <romanarora@chromium.org>
2021-07-13 11:03:57 -0700

    WebUITabStrip Tast test: Touch drag & drop
    Adds touch related drag tests to DragTabInTabletPerf
    
    BUG=chromium:1228769
    TEST=ran ui.DragTabInTabletPerf
    Change-Id: I68858964c89cc503db9f487db81948340b95082e
    
    Change-Id: I6c480cb6db30de6c41a02cbfe643dcef2bc310df

--- src/chromiumos/tast/local/chrome/uiauto/pointer/pointer.go
+++ src/chromiumos/tast/local/chrome/uiauto/pointer/pointer.go
@@ -50,6 +50,8 @@ type Context interface {
 
 	// DragToNode returns a function to cause a drag to the specified node.
 	DragToNode(f *nodewith.Finder, duration time.Duration) uiauto.Action
+
+	Hold(duration time.Duration) uiauto.Action
 }
 
 // MouseContext is a Context with the mouse.
@@ -81,6 +83,10 @@ func (mc *MouseContext) ClickAt(loc coords.Point) uiauto.Action {
 // MenuClick implements Context.MenuClick.
 func (mc *MouseContext) MenuClick(finder *nodewith.Finder) uiauto.Action {
 	return mc.ac.RightClick(finder)
+}
+
+func (mc *MouseContext) Hold(duration time.Duration) uiauto.Action {
+	return nil
 }
 
 // Drag implements Context.Drag.
@@ -177,4 +183,8 @@ func (tc *TouchContext) DragTo(loc coords.Point, duration time.Duration) uiauto.
 // DragToNode implements COntext.DragToNode.
 func (tc *TouchContext) DragToNode(f *nodewith.Finder, duration time.Duration) uiauto.Action {
 	return tc.tc.SwipeToNode(f, duration)
+}
+
+func (tc *TouchContext) Hold(duration time.Duration) uiauto.Action {
+	return tc.tc.Hold(duration)
 }
