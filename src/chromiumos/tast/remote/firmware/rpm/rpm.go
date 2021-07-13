31e2f4e81fa6c7a3282ad3ccaa5a40d37f1104d0
Jeremy Bettis <jbettis@google.com>
2021-07-13 13:07:14 -0600

    tast: When removing the charger use rpm also
    Add rpm (remote power manager) client.
    Add Helper.SetDUTPower(), which will use servo_v4 pd role or rpm as
    needed to remove charger.
    
    BUG=b:190532175
    TEST=crosfleet dut lease --host chromeos6-row10-rack10-host9
    redirect ports with ssh
    tast run --var=servo=localhost:9999:ssh:2223 \
    --var=firmware.hibernate_time=2m \
    --var=dutHostname=chromeos6-row10-rack10-host9 \
    --var=powerunitHostname=chromeos6-row9_10-rack10-rpm1 \
    --var=powerunitOutlet=.A9 \
    localhost:2222 firmware.DeepSleep
    
    Change-Id: Icc1546d2944303c5912b01a9b71df245b96bb2b5

---
+++ src/chromiumos/tast/remote/firmware/rpm/rpm.go
@@ -0,0 +1,114 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package rpm
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+
+	"chromiumos/tast/common/servo"
+	"chromiumos/tast/common/xmlrpc"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// PowerState is a state to be passed to SetPowerViaPoe or SetPowerViaRPM.
+type PowerState string
+
+// States to be passed to SetPowerViaPoe or SetPowerViaRPM.
+const (
+	Off   PowerState = "OFF"
+	On    PowerState = "ON"
+	Cycle PowerState = "CYCLE"
+)
+
+// RPM is the client interface to the remote power management service.
+type RPM struct {
+	xmlrpc *xmlrpc.XMLRpc
+
+	// restoreRPMPower is set if we need to enable power on the plug on close.
+	restoreRPMPower bool
+
+	// dutHostname is the real name of the dut, even if tast is connected to a forwarded port.
+	dutHostname string
+
+	// powerunitHostname, powerunitOutlet, hydraHostname identify the managed power outlet for the DUT.
+	powerunitHostname, powerunitOutlet, hydraHostname string
+}
+
+// Use the RemoteRPMHost if you are outside of the lab, and LocalRPMHost if inside.
+const (
+	RemoteRPMHost  string = "chromeos-rpm-server.mtv.corp.google.com"
+	LocalRPMHost   string = "rpm-service"
+	DefaultRPMPort int    = 9999
+)
+
+// NewLabRPM creates a new RPM object for communicating with a RPM server in the lab.
+// `hydraHostname` is optional, the other params are required.
+func NewLabRPM(ctx context.Context, pxy *servo.Proxy, dutHostname, powerunitHostname, powerunitOutlet, hydraHostname string) (*RPM, error) {
+	rpmHost := LocalRPMHost
+	if _, err := net.ResolveIPAddr("ip", rpmHost); err != nil {
+		testing.ContextLogf(ctx, "Could not resolve %q: %s", rpmHost, err)
+		rpmHost = RemoteRPMHost
+	}
+	port := DefaultRPMPort
+	// When running outside of the lab, the rpm server is not accessible, so proxy through the servo host.
+	if pxy != nil {
+		fwd, err := pxy.NewForwarder(ctx, fmt.Sprintf("%s:%d", rpmHost, DefaultRPMPort))
+		if err != nil {
+			return nil, errors.Wrap(err, "forwarding rpm port")
+		}
+		var portstr string
+		rpmHost, portstr, err = net.SplitHostPort(fwd.ListenAddr().String())
+		if err != nil {
+			return nil, errors.Wrap(err, "splitting host port")
+		}
+		port, err = strconv.Atoi(portstr)
+		if err != nil {
+			return nil, errors.Wrap(err, "parsing forwarded servo port")
+		}
+	}
+
+	r := &RPM{
+		xmlrpc:            xmlrpc.New(rpmHost, port),
+		dutHostname:       dutHostname,
+		powerunitHostname: powerunitHostname,
+		powerunitOutlet:   powerunitOutlet,
+		hydraHostname:     hydraHostname,
+	}
+
+	return r, nil
+}
+
+// Close performs RPM cleanup.
+func (r *RPM) Close(ctx context.Context) error {
+	if r.restoreRPMPower {
+		testing.ContextLog(ctx, "Restoring RPM power")
+		if ok, err := r.SetPower(ctx, On); err != nil {
+			return errors.Wrap(err, "failed to restore rpm power")
+		} else if !ok {
+			return errors.New("failed to restore rpm power")
+		}
+	}
+	return nil
+}
+
+// SetPower sets the power state for a plug managed by RPM.
+// Returns the bool returned by the xml rpc call, or error if the call failed.
+// It is unclear under which situations the api will return false with no error.
+func (r *RPM) SetPower(ctx context.Context, state PowerState) (bool, error) {
+	var success bool
+	err := r.xmlrpc.Run(ctx, xmlrpc.NewCallTimeout("set_power_via_rpm", 2*time.Minute, r.dutHostname, r.powerunitHostname, r.powerunitOutlet, r.hydraHostname, string(state)), &success)
+	if err != nil {
+		return false, errors.Wrap(err, "set power via rpm")
+	}
+	if success {
+		r.restoreRPMPower = state == Off
+	}
+	return success, err
+}
