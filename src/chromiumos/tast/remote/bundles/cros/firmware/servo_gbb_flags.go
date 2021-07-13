0fb0d7eed8b98336afef1dcbf249f990fb3e4c71
Jeremy Bettis <jbettis@google.com>
2021-07-13 12:53:05 -0600

    faft: Firmware precondition vars for test params
    Every test that uses the firmware precondition to set the boot mode
    needs to specify Data, ServiceDeps, SoftwareDeps, and Vars. Adding a
    new required dep there is also painful as every test has to be modified.
    
    Instead add helper vars to provide the vars that the precondition
    needs but the test itself doesn't care about.
    
    BUG=b:190532175
    TEST=~/trunk/src/platform/tast/fast_build.sh -T
    
    Cq-Depend: chromium:3056832
    Change-Id: Iceda18f40ddc4c24f7187bfec10d61ddc6e90978

--- src/chromiumos/tast/remote/bundles/cros/firmware/servo_gbb_flags.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/servo_gbb_flags.go
@@ -16,7 +16,6 @@ import (
 
 	commonbios "chromiumos/tast/common/firmware/bios"
 	"chromiumos/tast/common/servo"
-	"chromiumos/tast/remote/firmware"
 	"chromiumos/tast/remote/firmware/bios"
 	"chromiumos/tast/remote/firmware/pre"
 	pb "chromiumos/tast/services/cros/firmware"
@@ -30,11 +29,11 @@ func init() {
 		Timeout:      8 * time.Minute,
 		Contacts:     []string{"cros-fw-engprod@google.com", "jbettis@google.com"},
 		Attr:         []string{"group:firmware", "firmware_cr50", "firmware_ccd"},
-		Data:         []string{firmware.ConfigFile},
 		Pre:          pre.NormalMode(),
-		ServiceDeps:  []string{"tast.cros.firmware.BiosService", "tast.cros.firmware.UtilsService"},
-		SoftwareDeps: []string{"crossystem", "flashrom"},
-		Vars:         []string{"servo"},
+		Data:         pre.Data,
+		ServiceDeps:  pre.ServiceDeps,
+		SoftwareDeps: pre.SoftwareDeps,
+		Vars:         pre.Vars,
 	})
 }
 
