388035c6b9e77e4f8337488ce245504a56b37a94
Roman Arora <romanarora@chromium.org>
2021-07-13 11:03:57 -0700

    WebUITabStrip Tast test: Touch drag & drop
    Adds touch related drag tests to DragTabInTabletPerf
    
    BUG=chromium:1228769
    TEST=ran ui.DragTabInTabletPerf
    
    Change-Id: I6c480cb6db30de6c41a02cbfe643dcef2bc310df

--- src/chromiumos/tast/local/bundles/cros/ui/drag_tab_in_tablet_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/drag_tab_in_tablet_perf.go
@@ -15,14 +15,21 @@ import (
 	"chromiumos/tast/local/chrome/display"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
-	"chromiumos/tast/local/chrome/uiauto/mouse"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/pointer"
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/power"
 	"chromiumos/tast/local/ui"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
+)
+
+type pointerTypeParam int
+
+const (
+	pointerMouse pointerTypeParam = iota
+	pointerTouch
 )
 
 func init() {
@@ -34,6 +41,12 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
 		Timeout:      5 * time.Minute,
+		Params: []testing.Param{{
+			Val: pointerMouse,
+		}, {
+			Name: "touch",
+			Val:  pointerTouch,
+		}},
 	})
 }
 
@@ -74,6 +87,18 @@ func DragTabInTabletPerf(ctx context.Context, s *testing.State) {
 
 	ac := uiauto.New(tconn)
 
+	var pc pointer.Context
+	pt := s.Param().(pointerTypeParam)
+	if pt == pointerTouch {
+		pc, err = pointer.NewTouch(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to set up the touch context: ", err)
+		}
+	} else {
+		pc = pointer.NewMouse(tconn)
+	}
+	defer pc.Close()
+
 	info, err := display.GetPrimaryInfo(ctx, tconn)
 	if err != nil {
 		s.Fatal("Failed to get the primary display info: ", err)
@@ -87,22 +112,18 @@ func DragTabInTabletPerf(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to click the tab strip button")
 	}
 
-	tab := nodewith.Role(role.Tab).First()
+	firstTab := nodewith.Role(role.Tab).First()
+	firstTabLocation, _ := ac.Location(ctx, firstTab)
 	tabList := nodewith.Role(role.TabList).First()
+	tabListLocation, _ := ac.Location(ctx, tabList)
 	pv := perfutil.RunMultiple(ctx, s, cr, perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
 		if err := uiauto.Combine("drag and move a tab",
-			// Drag the first tab in the tab strip.
-			ac.MouseMoveTo(tab, 0),
-			// Drag in tablet mode starts with a long press.
-			mouse.Press(tconn, mouse.LeftButton),
-			ac.Sleep(time.Second),
-			// Drag tab around work area.
-			mouse.Move(tconn, snapRightPoint, 3*time.Second),
-			mouse.Move(tconn, snapLeftPoint, 3*time.Second),
-			mouse.Move(tconn, workAreaCenterPoint, 3*time.Second),
-			// Snap the tab back to the tab strip.
-			ac.MouseMoveTo(tabList, 3*time.Second),
-			mouse.Release(tconn, mouse.LeftButton),
+			// Drag the first tab in the tab strip around work area, then snap back to the tab strip.
+			pc.Drag(firstTabLocation.CenterPoint(),
+				ac.Sleep(time.Second),
+				pc.DragTo(snapRightPoint, 3*time.Second), pc.DragTo(snapLeftPoint, 3*time.Second),
+				pc.DragTo(workAreaCenterPoint, 3*time.Second),
+				pc.DragTo(tabListLocation.CenterPoint(), 3*time.Second)),
 			// Sleep to ensure that the next run performs correctly.
 			ac.Sleep(time.Second),
 		)(ctx); err != nil {
