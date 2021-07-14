bd74fa9ede456742b783effcb1ce0e1efbf80af3
Kyle Shimabukuro <kyleshima@chromium.org>
2021-07-14 16:10:01 -0700

    nearbyshare: move btsnoop log collection to the local service
    Starting/stopping btmon with ssh command execution from within the
    remote fixture is causing ssh connectivity issues and consistently
    causing the tests to fail. We can prevent this by moving the command
    execution to the local service, which helps consolidate and simplify log
    collection anyways.
    
    BUG=b:187795916
    BUG=b:190102238
    TEST=tast run -var=secondaryTarget=<dut2> <dut1> nearbyshare.CrosToCros*
    
    Change-Id: I1c207468417aeab3bb55c18566c5cad8fdfd6aa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/2873192
    Tested-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Reviewed-by: David Stevens <stevensd@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    (cherry picked from commit b247a0cdc308fc03b819eccc70110ec8b445b8a0)

--- src/chromiumos/tast/local/bundles/cros/nearbyshare/nearby_share_service.go
+++ src/chromiumos/tast/local/bundles/cros/nearbyshare/nearby_share_service.go
@@ -17,8 +17,10 @@ import (
 	nearbycommon "chromiumos/tast/common/cros/nearbyshare"
 	"chromiumos/tast/common/cros/nearbyshare/nearbysetup"
 	"chromiumos/tast/common/cros/nearbyshare/nearbytestutils"
+	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/fsutil"
+	"chromiumos/tast/local/bluetooth"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/nearbyshare"
 	"chromiumos/tast/local/chrome/uiauto/filesapp"
@@ -50,6 +52,7 @@ type NearbyService struct {
 	username        string
 	dataUsage       nearbysetup.DataUsage
 	visibility      nearbysetup.Visibility
+	btsnoopCmd      *testexec.Cmd
 }
 
 // NewChromeLogin logs into Chrome with Nearby Share flags enabled.
@@ -125,6 +128,11 @@ func (n *NearbyService) StartLogging(ctx context.Context, req *empty.Empty) (*em
 	testing.ContextLog(ctx, "Started logging chrome and message logs")
 	n.chromeReader = chromeReader
 	n.messageReader = messageReader
+
+	n.btsnoopCmd = bluetooth.StartBTSnoopLogging(n.s.ServiceContext(), filepath.Join(os.TempDir(), nearbycommon.BtsnoopLog))
+	if err := n.btsnoopCmd.Start(); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to start btmon")
+	}
 	return &empty.Empty{}, err
 }
 
@@ -143,7 +151,11 @@ func (n *NearbyService) SaveLogs(ctx context.Context, req *empty.Empty) (*empty.
 	if err = nearbytestutils.SaveLogs(ctx, n.messageReader, filepath.Join(nearbycommon.NearbyLogDir, nearbycommon.MessageLog)); err != nil {
 		testing.ContextLog(ctx, "Failed to save message log: ", err)
 	}
-	if err := fsutil.CopyFile(filepath.Join("/tmp", nearbycommon.BtsnoopLog), filepath.Join(nearbycommon.NearbyLogDir, nearbycommon.BtsnoopLog)); err != nil {
+	if err := n.btsnoopCmd.Kill(); err != nil {
+		testing.ContextLog(ctx, "Failed to kill btmon: ", err)
+	}
+	n.btsnoopCmd.Wait()
+	if err := fsutil.CopyFile(filepath.Join(os.TempDir(), nearbycommon.BtsnoopLog), filepath.Join(nearbycommon.NearbyLogDir, nearbycommon.BtsnoopLog)); err != nil {
 		testing.ContextLog(ctx, "Failed to save btsnoop log: ", err)
 	}
 	return &empty.Empty{}, err
