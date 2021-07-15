22c35cb984577aa064f1f9fadc54d4263efb47d7
Alfred Yu <alfredyu@cienet.com>
2021-07-15 16:25:48 +0800

    cuj: change chrome "New tab" ClassName for tablet
    UIActionHandler change to fix the issue of failing to open
    new Chrome tab under tablet mode due to the UI change
    in R93-14078.0.0
    
    BUG=b:194111005
    TEST=tast run <vars> $DUT ui.TabSwitchCUJ2.*
    
    Change-Id: I9b25e29a1ef53b8851458c18a0b1c9dadcab317f

--- src/chromiumos/tast/local/bundles/cros/ui/cuj/ui_action_handler.go
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/ui_action_handler.go
@@ -253,7 +253,7 @@ func (t *TabletActionHandler) NewChromeTab(ctx context.Context, cr *chrome.Chrom
 	// There may be multiple browser windows under tablet mode, with one active and others invisible.
 	// The UI layout of different windows are the same and with the same coordinates. So tap the first
 	// found button will trigger the tap on the same button of the active window.
-	newTabFinder := nodewith.Name("New tab").Role(role.Button).ClassName("ToolbarButton").First()
+	newTabFinder := nodewith.Name("New tab").Role(role.Button).ClassName("WebUINewTabButton").First()
 	if err := t.tc.Tap(newTabFinder)(ctx); err != nil {
 		return nil, errors.Wrap(err, "failed to tap new tab button")
 	}
