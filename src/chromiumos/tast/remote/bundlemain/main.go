3c3db8e2a8a5f1a30edcc72befd05f429bffded3
Yi Chou <yich@google.com>
2021-07-16 01:40:13 +0800

    tast-test: ignore hwsecCheckTPMState in recovery mode
    The tpm_manager can not find the ownerpasswod when the owner password
    when the device is in recovery mode.
    The hwsecCheckTPMState is meaningless in this case, so we shuold skip it
    when "crossystem mainfw_type" returns "recovery".
    
    BUG=b:172358080
    TEST=tast run $DUT hwsec.*
    
    Change-Id: I7349a3173b992a8faddfbd9fa0733c4dfa5ff4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3032821
    Tested-by: Yi Chou <yich@google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Leo Lai <cylai@google.com>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>

--- src/chromiumos/tast/remote/bundlemain/main.go
+++ src/chromiumos/tast/remote/bundlemain/main.go
@@ -53,6 +53,16 @@ func hwsecGetTPMStatus(ctx context.Context, s *testing.TestHookState) (*hwsec.No
 }
 
 func hwsecCheckTPMState(ctx context.Context, s *testing.TestHookState, origStatus *hwsec.NonsensitiveStatusInfo, origCounter int) error {
+	cmdRunner := hwsecremote.NewLoglessCmdRunner(s.DUT())
+	fwType, err := cmdRunner.Run(ctx, "crossystem", "mainfw_type")
+	if err != nil {
+		testing.ContextLog(ctx, "Failed to get the firmware type")
+	}
+	if string(fwType) == "recovery" {
+		// Skipping this check when the device is in recovery mode.
+		return nil
+	}
+
 	status, err := hwsecGetTPMStatus(ctx, s)
 	if err != nil {
 		return errors.Wrap(err, "failed to get TPM status")
