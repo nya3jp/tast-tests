c5eb4fe8d6d8cb85bca54fda85b1fac21fd41151
Liên Hoàng <lienh@chromium.org>
2021-07-16 00:07:40 +0000

    Tast: First attempts to deflake the Edu coexistence tests
    Context in https://crbug.com/1222744. Changes include:
    - Make sure the Google Accounts button is scrolled into view before clicking.
    - Increase the UI wait timeout (15s to 20s)
    
    BUG=chromium:1222744
    TEST=familylink.* pass locally
    
    Change-Id: I6d49f36df0b8fa196f99c5f0206f3d12b97fd461

--- src/chromiumos/tast/local/chrome/familylink/familylink.go
+++ src/chromiumos/tast/local/chrome/familylink/familylink.go
@@ -29,7 +29,7 @@ func AddEduSecondaryAccount(ctx context.Context, cr *chrome.Chrome, tconn *chrom
 	parentFirstName, parentLastName, parentUser, parentPass,
 	secondUser, secondPass string) error {
 
-	ui := uiauto.New(tconn)
+	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
 
 	testing.ContextLog(ctx, "Checking logged in user is Family Link")
 	if err := ui.Exists(nodewith.Name("This account is managed by Family Link").Role(role.Image))(ctx); err != nil {
@@ -47,14 +47,15 @@ func AddEduSecondaryAccount(ctx context.Context, cr *chrome.Chrome, tconn *chrom
 	selectParentOption := nodewith.NameStartingWith(parentFirstName + " " + parentLastName).Role(role.ListBoxOption)
 	if err := uiauto.Combine("open in-session edu coexistence flow",
 		ui.WaitUntilExists(googleAccountsButton),
-		ui.LeftClickUntil(googleAccountsButton, ui.Exists(addSchoolAccountButton)),
+		ui.FocusAndWait(googleAccountsButton), // scroll the button into view
+		ui.WithInterval(200*time.Millisecond).LeftClickUntil(googleAccountsButton, ui.Exists(addSchoolAccountButton)),
 		ui.WithInterval(time.Second).LeftClickUntil(addSchoolAccountButton, ui.Exists(selectParentOption)),
 	)(ctx); err != nil {
 		return errors.Wrap(err, "failed to open in-session edu coexistence flow")
 	}
 
 	testing.ContextLog(ctx, "Clicking button that matches parent email: ", parentUser)
-	if err := ui.LeftClickUntil(selectParentOption, ui.Exists(nodewith.Name("Parent password").Role(role.TextField)))(ctx); err != nil {
+	if err := ui.WithInterval(200*time.Millisecond).LeftClickUntil(selectParentOption, ui.Exists(nodewith.Name("Parent password").Role(role.TextField)))(ctx); err != nil {
 		return errors.Wrap(err, "failed to click button that matches parent email")
 	}
 
@@ -102,7 +103,7 @@ func NavigateEduCoexistenceFlow(ctx context.Context, tconn *chrome.TestConn, par
 	enterSchoolEmailText := nodewith.Name("School email").Role(role.TextField)
 	if err := uiauto.Combine("Clicking next",
 		ui.WaitUntilExists(nextButton),
-		ui.LeftClickUntil(nextButton, ui.Exists(enterSchoolEmailText)))(ctx); err != nil {
+		ui.WithInterval(200*time.Millisecond).LeftClickUntil(nextButton, ui.Exists(enterSchoolEmailText)))(ctx); err != nil {
 		return errors.Wrap(err, "failed to click Next button")
 	}
 
