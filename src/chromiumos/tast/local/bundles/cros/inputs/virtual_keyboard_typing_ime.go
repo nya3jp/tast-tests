15cb5f129a40c0c82415675bd194c197ae6814d4
Alvin Jia <alvinjia@google.com>
2021-07-19 08:51:48 +1000

    Refacter typingIME as example of using shared data
    Revise typingIME test to use shared data, deprecate the use of
    parameterization prototype in vk_type_test_data.
    
    BUG=b:188488890
    TEST=tast run <DUT> inputs.VirtualKeyboardTypingI*
    
    Change-Id: Ia953bcc896c706a482531463b052f0c0fe753bfe

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_ime.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_ime.go
@@ -9,14 +9,38 @@ import (
 	"path/filepath"
 	"time"
 
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/inputs/data"
 	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/testserver"
+	"chromiumos/tast/local/bundles/cros/inputs/util"
+	"chromiumos/tast/local/chrome/ime"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/vkb"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
+
+var typingTestIMEs = []ime.InputMethodCode{
+	ime.INPUTMETHOD_XKB_US_ENG,
+	ime.INPUTMETHOD_NACL_MOZC_US,
+	ime.INPUTMETHOD_PINYIN_CHINESE_SIMPLIFIED,
+	ime.INPUTMETHOD_XKB_US_INTL,
+	ime.INPUTMETHOD_XKB_GB_EXTD_ENG,
+	ime.INPUTMETHOD_XKB_ES_SPA,
+	ime.INPUTMETHOD_XKB_SE_SWE,
+	ime.INPUTMETHOD_XKB_CA_ENG,
+	ime.INPUTMETHOD_XKB_JP_JPN,
+	ime.INPUTMETHOD_NACL_MOZC_JP,
+	ime.INPUTMETHOD_XKB_FR_FRA,
+	ime.INPUTMETHOD_CANTONESE_CHINESE_TRADITIONAL,
+	ime.INPUTMETHOD_CANGJIE87_CHINESE_TRADITIONAL,
+	ime.INPUTMETHOD_HANGEUL_HANJA_KOREAN,
+}
+
+var typingTestMessages = []data.Message{data.TypingMessageHello}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -25,9 +49,9 @@ func init() {
 		Contacts:     []string{"shengjun@chromium.org", "essential-inputs-team@google.com"},
 		Attr:         []string{"group:mainline", "informational", "group:input-tools-upstream", "group:input-tools"},
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
-		Pre:          pre.VKEnabledTablet,
+		Pre:          pre.VKEnabledTabletReset,
 		HardwareDeps: hwdep.D(pre.InputsStableModels),
-		Timeout:      time.Duration(len(data.VKInputMap)) * time.Minute,
+		Timeout:      time.Duration(len(typingTestIMEs)) * time.Duration(len(typingTestMessages)) * time.Minute,
 	})
 }
 
@@ -35,27 +59,53 @@ func VirtualKeyboardTypingIME(ctx context.Context, s *testing.State) {
 	cr := s.PreValue().(pre.PreData).Chrome
 	tconn := s.PreValue().(pre.PreData).TestAPIConn
 
+	vkbCtx := vkb.NewContext(cr, tconn)
+
 	its, err := testserver.Launch(ctx, cr, tconn)
 	if err != nil {
 		s.Fatal("Failed to launch inputs test server: ", err)
 	}
 	defer its.Close()
+	inputField := testserver.TextAreaNoCorrectionInputField
+	subtest := func(testName string, inputData data.InputData) func(ctx context.Context, s *testing.State) {
+		return func(ctx context.Context, s *testing.State) {
+			cleanupCtx := ctx
+			// Use a shortened context for test operations to reserve time for cleanup.
+			ctx, shortCancel := ctxutil.Shorten(ctx, 10*time.Second)
+			defer shortCancel()
 
-	for imeCode, testData := range data.VKInputMap {
-		if testData.SkipTest {
-			testing.ContextLog(ctx, "Skip testing in input method: ", string(imeCode))
-			continue
-		}
-		s.Run(ctx, string(imeCode), func(ctx context.Context, s *testing.State) {
-			defer func() {
-				outDir := filepath.Join(s.OutDir(), string(imeCode))
-				faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, s.HasError, cr, "ui_tree_"+string(imeCode))
-			}()
-			// 1 minute should be enough for the retries to avoid flakiness.
-			ui := uiauto.New(tconn).WithTimeout(time.Minute)
-			if err := ui.Retry(5, its.ValidateVKInputOnField(testserver.TextAreaNoCorrectionInputField, imeCode))(ctx); err != nil {
-				s.Fatalf("Failed to validate virtual keyboard input in %s: %v", string(imeCode), err)
+			defer func(ctx context.Context) {
+
+				outDir := filepath.Join(s.OutDir(), testName)
+				faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, s.HasError, cr, "ui_tree_"+testName)
+
+				if err := vkbCtx.HideVirtualKeyboard()(ctx); err != nil {
+					s.Log("Failed to hide virtual keyboard: ", err)
+				}
+			}(cleanupCtx)
+
+			if err := its.ClickFieldUntilVKShown(inputField)(ctx); err != nil {
+				s.Fatal("Failed to show VK: ", err)
 			}
-		})
+
+			if err := uiauto.Combine("validate vk input function on field "+string(inputField),
+				its.Clear(inputField),
+				func(ctx context.Context) error {
+					if err := vkbCtx.TapKeysIgnoringCase(inputData.CharacterKeySeq)(ctx); err != nil {
+						return errors.Wrapf(err, "failed to tap keys: %v", inputData.CharacterKeySeq)
+					}
+					if inputData.SubmitFromSuggestion {
+						return vkbCtx.SelectFromSuggestion(inputData.ExpectedText)(ctx)
+					}
+					return nil
+				},
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), inputData.ExpectedText),
+			)(ctx); err != nil {
+				s.Fatal("Failed to validate virtual keyboard input: ", err)
+			}
+
+		}
 	}
+	// Run defined subtest per input method and message combination.
+	util.RunSubtestsPerInputMethodAndMessage(ctx, tconn, s, typingTestIMEs, typingTestMessages, subtest)
 }
