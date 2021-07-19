9b9a3d945c9b9d86eaa47d189fe22d84ad9daf05
Vishal Chaudhary <vishal38785@gmail.com>
2021-07-19 14:38:34 +0530

    tast-tests: adds utility package for DLP
    Adds clipboard and policy package which provides utility functions
    for dlp policy related tests.
    
    BUG=chromium:1132208
    TEST=tast run <ip> dlp.DataLeakPreventionRulesListPrinting dlp.DataLeakPreventionRulesListScreenshot
    
    Change-Id: I3dcbc4279e5d052492aa893a7fcd89e10e36be41

---
+++ src/chromiumos/tast/local/bundles/cros/dlp/clipboard/clipboard.go
@@ -0,0 +1,48 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package clipboard contains functionality shared by tests that
+// exercise Clipboard restrictions of DLP.
+package clipboard
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+)
+
+// CheckGreyPasteNode checks if greyed paste node exists.
+func CheckGreyPasteNode(ctx context.Context, ui *uiauto.Context) error {
+	pasteNode := nodewith.Name("Paste Ctrl+V").Role(role.MenuItem)
+	pasteActiveNode := nodewith.Name("Paste Ctrl+V").Role(role.MenuItem).State("focusable", true)
+
+	if err := uiauto.Combine("Check paste node greyed ",
+		ui.WaitUntilExists(pasteNode),
+		ui.WaitUntilGone(pasteActiveNode))(ctx); err != nil {
+		return errors.Wrap(err, "failed to check paste node greyed: ")
+	}
+
+	return nil
+}
+
+// CheckClipboardBubble checks if clipboard restriction bubble exists.
+func CheckClipboardBubble(ctx context.Context, ui *uiauto.Context, url string) error {
+	bubbleView := nodewith.ClassName("ClipboardDlpBubble").Role(role.Window)
+	bubbleClass := nodewith.ClassName("ClipboardBlockBubble").Ancestor(bubbleView)
+	bubbleButton := nodewith.Name("Got it").Role(role.Button).Ancestor(bubbleClass)
+	messageBlocked := "Pasting from " + url + " to this location is blocked by administrator policy"
+	bubble := nodewith.Name(messageBlocked).Role(role.StaticText).Ancestor(bubbleClass)
+
+	if err := uiauto.Combine("Bubble ",
+		ui.WaitUntilExists(bubbleView),
+		ui.WaitUntilExists(bubbleButton),
+		ui.WaitUntilExists(bubble))(ctx); err != nil {
+		return errors.Wrap(err, "failed to check for notification bubble existence: ")
+	}
+
+	return nil
+}
