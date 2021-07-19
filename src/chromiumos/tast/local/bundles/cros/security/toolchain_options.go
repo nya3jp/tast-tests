44a2820b730aff7d425d7781c0498d5d606c94f4
Christopher Di Bella <cjdb@google.com>
2021-07-19 20:10:36 +0000

    removes libgcc expected failures
    Now that libunwind and compiler-rt are respectively pretending to be
    libgcc_s and libgcc, we have hardware failures that are getting upset
    when these libraries aren't failing the test as expected.
    
    BUG=b:194115264
    TEST=Tested using `bb` with go/croscl/2945947.
    
    Change-Id: I9c5bae355d9bf60771ec5adb94eca8fb297f0c98

--- src/chromiumos/tast/local/bundles/cros/security/toolchain_options.go
+++ src/chromiumos/tast/local/bundles/cros/security/toolchain_options.go
@@ -95,22 +95,6 @@ var stackAllowlist []string
 
 var loadwxAllowlist []string
 
-var libgccAllowlist = []string{
-	"/opt/google/chrome/nacl_helper",
-
-	// Files from flash player.
-	"/opt/google/chrome/pepper/libpepflashplayer.so",
-	// Prebuilt hdcp driver binary from Intel.
-	"/usr/sbin/hdcpd",
-	// Prebuilt binaries installed by Intel Camera HAL on kabylake boards.
-	"/usr/lib64/libSkyCamAIC.so",
-	"/usr/lib64/libSkyCamAICKBL.so",
-
-	// FIXME: Remove after mesa is fixed to not need libgcc_s. crbug.com/808264
-	"/usr/lib/dri/kms_swrast_dri.so",
-	"/usr/lib/dri/swrast_dri.so",
-}
-
 var libstdcAllowlist = []string{
 	// Flash player
 	"/opt/google/chrome/pepper/libpepflashplayer.so",
@@ -173,7 +157,6 @@ func ToolchainOptions(ctx context.Context, s *testing.State) {
 
 	// Condition: Verify all binaries are not linked with libgcc_s.so.
 	libgccVerify := toolchain.CreateNotLinkedVerify("libgcc_s.so*")
-	conds = append(conds, toolchain.NewELFCondition(libgccVerify, libgccAllowlist))
 
 	// Condition: Verify all binaries are not linked with libstdc++.so.
 	libstdcVerify := toolchain.CreateNotLinkedVerify("libstdc++.so*")
