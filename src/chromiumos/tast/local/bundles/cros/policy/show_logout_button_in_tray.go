c9e9dc81a5a64bdb7ed3aa05a3c0d6cd99c7a0a3
alexanderhartl <alexanderhartl@google.com>
2021-07-19 19:49:02 +0000

    policy: Migrate ShowLogoutButtonInTray test to uiauto
    BUG=b:190614781, b:194112304
    TEST=tast run <ip> policy.ShowLogoutButtonInTray
    
    Change-Id: Iab25525b67c97a99c4463c10594ef9977dc7f02a

--- src/chromiumos/tast/local/bundles/cros/policy/show_logout_button_in_tray.go
+++ src/chromiumos/tast/local/bundles/cros/policy/show_logout_button_in_tray.go
@@ -6,11 +6,12 @@ package policy
 
 import (
 	"context"
-	"time"
 
 	"chromiumos/tast/common/policy"
-	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/local/policyutil/fixtures"
 	"chromiumos/tast/testing"
@@ -75,11 +76,14 @@ func ShowLogoutButtonInTray(ctx context.Context, s *testing.State) {
 			}
 
 			// Confirm the status of the Sign out button node.
-			if err := policyutil.WaitUntilExistsStatus(ctx, tconn, ui.FindParams{
-				Role: ui.RoleTypeButton,
-				Name: "Sign out",
-			}, param.wantButton, 15*time.Second); err != nil {
-				s.Error("Could not confirm the desired status of the Sign out button: ", err)
+			ui := uiauto.New(tconn)
+			signOutButton := nodewith.Name("Sign out").Role(role.Button).First()
+			if err = ui.WaitUntilExists(signOutButton)(ctx); err != nil {
+				if param.wantButton {
+					s.Error("\"Sign out\" button not found: ", err)
+				}
+			} else if !param.wantButton {
+				s.Error("Unexpected \"Sign out\" button found: ", err)
 			}
 		})
 	}
