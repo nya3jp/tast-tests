770bb793b024e8c3be1c1a8a3e13a7f50521eddb
Alex Gimenez <raging@google.com>
2021-07-19 19:50:03 +0900

    tast-tests: add zram status and use it in a few tests
    BUG=b:184892486
    TEST=ran all affected tast tests, checked metrics
    
    Change-Id: I9725c655b8bc0eeeb910cf76275fbc06a6738261

--- src/chromiumos/tast/local/multivm/metrics.go
+++ src/chromiumos/tast/local/multivm/metrics.go
@@ -13,13 +13,33 @@ import (
 	"chromiumos/tast/local/memory/arc"
 )
 
+// BaseMetrics holds initial metrics, which can be used as a baseline
+// for ever-growing metrics (snapshot early in the test, subtract at the end).
+type BaseMetrics struct {
+	memstats *memory.ZramStats
+}
+
+// NewBaseMetrics gathers data on ever-growing metrics, so that they can
+// be a baseline to subtract from the same metrics at a later time.
+// A test will ideally perform this sequence:
+//     base := NewBaseMetrics()
+//     ..run the test..
+//     MemoryMetrics( ..., base, ...)
+func NewBaseMetrics() (*BaseMetrics, error) {
+	basezram, err := memory.NewZramStats()
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get baseline memory stats")
+	}
+	return &BaseMetrics{memstats: basezram}, nil
+}
+
 // MemoryMetrics generates log files detailing the memory from the following:
 // * smaps_rollup per process.
 // * fincore per file used as a disk by crsovm.
-// * zram compressed swap memory use.
+// * zram compressed swap memory use and zram overall usage stats
 // * Android adb dumpsys meminfo.
 // If p != nil, summaries are added as perf.Values.
-func MemoryMetrics(ctx context.Context, pre *PreData, p *perf.Values, outdir, suffix string) error {
+func MemoryMetrics(ctx context.Context, base *BaseMetrics, pre *PreData, p *perf.Values, outdir, suffix string) error {
 	if err := memory.SmapsMetrics(ctx, p, outdir, suffix); err != nil {
 		return errors.Wrap(err, "failed to collect smaps_rollup metrics")
 	}
@@ -28,6 +48,9 @@ func MemoryMetrics(ctx context.Context, pre *PreData, p *perf.Values, outdir, su
 	}
 	if err := memory.ZramMmStatMetrics(ctx, p, outdir, suffix); err != nil {
 		return errors.Wrap(err, "failed to collect zram mm_stats metrics")
+	}
+	if err := memory.ZramStatMetrics(ctx, base.memstats, p, outdir, suffix); err != nil {
+		return errors.Wrap(err, "failed to collect zram stats metrics")
 	}
 
 	preARC := ARCFromPre(pre)
