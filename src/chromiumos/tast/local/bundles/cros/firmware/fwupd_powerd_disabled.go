74c17aa6ed6aa5b998570e04a2f1d65300b40d3e
George Popoola <gpopoola@google.com>
2021-07-20 17:57:07 +0000

    fwupd: Tast test for Absence of Powerd
    This CL creates a new Tast test that first stops the power daemon if
    it is running. It then runs the command "fwupdmgr get-plugins --json" to get a
    list of plugins that were compiled for fwupd. Afterwards, this output is
    decoded into a struct for the test to parse. The test iterates through
    it, checking for powerd as the plugin name and "disabled" as one of its flags.
    
    The power daemon is the started up again after the output of "fwupdmgr
    get-plugins --json" has been searched.
    
    If it passes, then powerd was appropriately disabled
    upon compilation since the daemon was absent.
    
    BUG=b:194181934
    TEST=tast run [user@]host[:port] firmware.FwupdPowerdDisabled
    
    Change-Id: Id4b9187987a4e9f3b6837123d20c26db6e7748d2

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_powerd_disabled.go
@@ -0,0 +1,95 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FwupdPowerdDisabled,
+		Desc: "Checks that the powerd plugin is disabled in the absense of powerd",
+		Contacts: []string{
+			"gpopoola@google.com",       // Test Author
+			"chromeos-fwupd@google.com", // CrOS FWUPD
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"fwupd"},
+	})
+}
+
+// checkForPowerdDisabled verifies that powerd was found among disabled plugins */
+func checkForPowerdDisabled(output []byte) error {
+	type wrapper struct {
+		Plugins []struct {
+			Name  string
+			Flags []string
+		}
+	}
+
+	var w wrapper
+
+	if err := json.Unmarshal(output, &w); err != nil {
+		return errors.New("failed to parse command output")
+	}
+
+	for _, p := range w.Plugins {
+		if p.Name == "powerd" {
+			for _, f := range p.Flags {
+				if f == "disabled" {
+					return nil
+				}
+			}
+		}
+	}
+
+	return errors.New("powerd was not found among plugins (to be disabled)")
+}
+
+// FwupdPowerdDisabled calls stopPowerd to stop the daemon, runs fwupdmgr get-plugins,
+// retrieves the output, and checks that the powerd plugin was not enabled
+func FwupdPowerdDisabled(ctx context.Context, s *testing.State) {
+
+	if err := upstart.EnsureJobRunning(ctx, "fwupd"); err != nil {
+		s.Error("Failed to start fwupd (or ensure its already running): ", err)
+	}
+
+	if err := upstart.StopJob(ctx, "powerd"); err != nil {
+		s.Fatal("Failed to stop the power daemon: ", err)
+	}
+
+	defer func() {
+		if err := upstart.EnsureJobRunning(ctx, "powerd"); err != nil {
+			s.Error("Failed to restart the power daemon after testing: ", err)
+		}
+	}()
+
+	cmd := testexec.CommandContext(ctx, "fwupdmgr", "get-plugins", "--json")
+
+	output, err := cmd.Output(testexec.DumpLogOnError)
+
+	if err != nil {
+		s.Errorf("%s failed: %v", shutil.EscapeSlice(cmd.Args), err)
+	}
+
+	if err := ioutil.WriteFile(filepath.Join(s.OutDir(), "fwupdmgr.txt"), output, 0644); err != nil {
+		s.Error("Failed dumping fwupdmgr output: ", err)
+	}
+
+	if err := checkForPowerdDisabled(output); err != nil {
+		s.Error("match failed: ", err)
+	}
+
+}
