aa59ac35a078d462ac0caa6d69391444d404d668
Alvin Jia <alvinjia@google.com>
2021-07-20 11:04:11 +1000

    Divide handwriting tests on stable/unstable models
    The tests focusing on stable models will be prompted to CQ.
    
    BUG=b:179186595
    TEST=tast run <DUT> inputs.VirtualKeyboardHand*
    
    Change-Id: I41a311e8e42e368cb4d6ca026c54231a629c7e34

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_handwriting.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_handwriting.go
@@ -11,6 +11,7 @@ import (
 
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/bundles/cros/inputs/data"
+	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/testserver"
 	"chromiumos/tast/local/bundles/cros/inputs/util"
 	"chromiumos/tast/local/chrome"
@@ -49,14 +50,24 @@ func init() {
 		HardwareDeps: hwdep.D(hwdep.SkipOnModel("kevin1")),
 		Params: []testing.Param{
 			{
-				Name: "docked",
-				// false for docked-mode VK.
-				Val: false,
+				Name:              "docked_stable",
+				Val:               false, // false for docked-mode VK.
+				ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
 			},
 			{
-				Name: "floating",
-				// true for floating-mode VK.
-				Val: true,
+				Name:              "docked_unstable",
+				Val:               false, // false for docked-mode VK.
+				ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
+			},
+			{
+				Name:              "floating_stable",
+				Val:               true, // true for floating-mode VK.
+				ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
+			},
+			{
+				Name:              "floating_unstable",
+				Val:               true, // true for floating-mode VK.
+				ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
 			},
 		},
 	})
