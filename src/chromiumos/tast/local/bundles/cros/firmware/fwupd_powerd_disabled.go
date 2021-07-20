d0fa9db465e0519dcc4a6abc75a53b0da32ec9b0
George Popoola <gpopoola@google.com>
2021-07-20 17:57:07 +0000

    fwupd: Tast test for Absence of Powerd
    This CL creates a new Tast test that first stops the power daemon if
    it is running. It then runs the command "fwupdmgr get-plugins --json" to get a
    list of plugins that were compiled for fwupd. Afterwards, this output is
    decoded into a struct for the test to parse. The test iterates through
    it, checking for powerd as the plugin name and "disabled" as one of its flags.
    
    The power daemon is the started up again after the output of "fwupdmgr
    get-plugins --json" has been searched.
    
    If it passes, then powerd was appropriately disabled
    upon compilation since the daemon was absent.
    
    BUG=b:194181934
    TEST=tast run [user@]host[:port] firmware.FwupdPowerdDisabled
    
    Change-Id: Id4b9187987a4e9f3b6837123d20c26db6e7748d2

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_powerd_disabled.go
@@ -0,0 +1,120 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FwupdPowerdDisabled,
+		Desc: "Checks that the powerd plugin is disabled in the absense of powerd",
+		Contacts: []string{
+			"gpopoola@google.com",       // Test Author
+			"chromeos-fwupd@google.com", // CrOS FWUPD
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"fwupd"},
+	})
+}
+
+// stopPowerd runs the command "stop powerd" to halt the daemon
+func stopPowerd(ctx context.Context) error {
+	cmd := testexec.CommandContext(ctx, "stop", "powerd")
+
+	output, err := cmd.Output(testexec.DumpLogOnError)
+
+	if err != nil {
+		return err
+	}
+
+	if output == nil {
+		return errors.Errorf("stop powerd output indicates it didn't work as expected: %q", output)
+	}
+
+	return nil
+}
+
+// startupPowerd runs the command "start powerd" to make the daemon run again after testing
+func startupPowerd(ctx context.Context) error {
+	cmd := testexec.CommandContext(ctx, "start", "powerd")
+
+	output, err := cmd.Output(testexec.DumpLogOnError)
+
+	if err != nil {
+		return err
+	}
+
+	if output == nil {
+		return errors.Errorf("start powerd output indicates it didn't work as expected: %q", output)
+	}
+
+	return nil
+}
+
+// checkForPowerdDisabled verifies that powerd was found among disabled plugins */
+func checkForPowerdDisabled(output []byte, s *testing.State) error {
+	type wrapper struct {
+		Plugins []struct {
+			Name  string
+			Flags []string
+		}
+	}
+
+	var w wrapper
+
+	if err := json.Unmarshal(output, &w); err != nil {
+		s.Errorf("Failed parsing command output: ", err)
+	}
+
+	for i := range w.Plugins {
+		if w.Plugins[i].Name == "powerd" {
+			for j := range w.Plugins[i].Flags {
+				if w.Plugins[i].Flags[j] == "disabled" {
+					return nil
+				}
+			}
+		}
+	}
+
+	return errors.New("powerd was not found among plugins (to be disabled)")
+}
+
+// FwupdPowerdDisabled calls stopPowerd to stop the daemon, runs fwupdmgr get-plugins,
+// retrieves the output, and checks that the powerd plugin was not enabled
+func FwupdPowerdDisabled(ctx context.Context, s *testing.State) {
+	if err := stopPowerd(ctx); err != nil {
+		s.Fatalf("Failed to stop the power daemon: ", err)
+	}
+
+	cmd := testexec.CommandContext(ctx, "fwupdmgr", "get-plugins", "--json")
+
+	output, err := cmd.Output(testexec.DumpLogOnError)
+
+	if err != nil {
+		s.Errorf("%s failed: %v", shutil.EscapeSlice(cmd.Args), err)
+	}
+
+	if err := ioutil.WriteFile(filepath.Join(s.OutDir(), "fwupdmgr.txt"), output, 0644); err != nil {
+		s.Error("Failed dumping fwupdmgr output: ", err)
+	}
+
+	if err := checkForPowerdDisabled(output, s); err != nil {
+		s.Error("match failed: ", err)
+	}
+
+	if err := startupPowerd(ctx); err != nil {
+		s.Fatalf("Failed to restart the power daemon after testing: ", err)
+	}
+}
