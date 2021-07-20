de96a392a1284750cb3d2bffb316ad663a016950
Vishal Chaudhary <vishal38785@gmail.com>
2021-07-20 15:08:59 +0530

    tast-tests: test for clipboard ext of DLP
    Adds test to check whether clipboard restriction allows copy-paste and
    drag-drop of content if restricted in DLP policy
    
    BUG=chromium:1132208
    TEST=tast run <ip> dlp.DataLeakPreventionRulesListClipboardMisc
    
    Change-Id: Id69b7a5cf86f96324b4ef12197a3251c67636a12

---
+++ src/chromiumos/tast/local/bundles/cros/dlp/data/dlp_clipboard.js
@@ -0,0 +1,22 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var contentArea = document.getElementById('drag-drop');
+var scanButton = document.getElementById('scanButton');
+scanButton.addEventListener('click', function() {
+  let result = contentArea.innerText;
+  document.addEventListener('paste', (event) => {
+    let content = event.clipboardData.getData('text/plain').slice(0, 200);
+    contentArea.innerText = content;
+    event.preventDefault();
+  }, {once: true});
+  if (!document.execCommand('paste')) {
+    throw new Error('Failed to execute paste');
+  }
+  if (result != contentArea.innerText && contentArea.innerText != "") {
+    scanButton.innerText = "Extension able to access content";
+  } else {
+    scanButton.innerText = "Extension couldn't access content";
+  }
+});
