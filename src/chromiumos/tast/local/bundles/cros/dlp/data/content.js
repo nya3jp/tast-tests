ee8366fb5d015d7a3e41fff4a0b43def65de16fa
Vishal Chaudhary <vishal38785@gmail.com>
2021-07-20 15:08:59 +0530

    tast-tests: test for clipboard extension of DLP
    Adds test to check whether clipboard restriction allows access
    to confidential content using extension
    
    Also, check if opening files app with copied content shows notification
    
    BUG=chromium:1132208
    TEST=tast run <ip> dlp.DataLeakPreventionRulesListClipboardMisc
    
    Change-Id: Id69b7a5cf86f96324b4ef12197a3251c67636a12

---
+++ src/chromiumos/tast/local/bundles/cros/dlp/data/content.js
@@ -0,0 +1,33 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.text === "copy") {
+        document.addEventListener('copy', copyHandler);
+        if (!document.execCommand('copy')) {
+            throw new Error('Failed to execute paste');
+        }
+    }
+});
+
+
+// Copy event handler
+function copyHandler(e) {
+    var selection = document.getSelection();
+
+    // set clipboard string.
+    e.clipboardData.setData(
+      'text/plain',
+      selection.toString()
+    );
+    // stop default copy event.
+    e.preventDefault();
+
+    alertString = "Extension able to access content\n"
+    if (selection.toString() == "") {
+        alertString = "Extension couldn't access content\n"
+    }
+
+    alert(alertString + e.clipboardData.getData('text/plain').slice(0,30));
+  }
