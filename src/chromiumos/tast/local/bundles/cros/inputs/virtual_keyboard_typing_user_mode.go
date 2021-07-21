7d6f1298707c19c20c144e718592e964c9d5f5e8
Alvin Jia <alvinjia@google.com>
2021-07-21 16:35:28 +1000

    Refactor tyingUserMode test to use the shared data
    Deprecate the use of prototype test data.
    
    BUG=b:194255732
    TEST=tast run <DUT> inputs.VirtualKeyboardTypingUserMode*
    
    Change-Id: I9199e74ce90095341ad9e760b68af78584c330c4

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_user_mode.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_user_mode.go
@@ -6,20 +6,28 @@ package inputs
 
 import (
 	"context"
+	"path/filepath"
 	"time"
 
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/inputs/data"
 	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/testserver"
+	"chromiumos/tast/local/bundles/cros/inputs/util"
 	"chromiumos/tast/local/chrome/ime"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/vkb"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
 
-type testParams struct {
-	inputMethodID ime.InputMethodCode
-	userMode      string
+var typingModeTestIMEs = []ime.InputMethodCode{
+	ime.INPUTMETHOD_XKB_US_ENG,
+	ime.INPUTMETHOD_XKB_JP_JPN,
 }
+var typingModeTestMessages = []data.Message{data.TypingMessageHello}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -31,52 +39,68 @@ func init() {
 		HardwareDeps: hwdep.D(pre.InputsStableModels),
 		Timeout:      5 * time.Minute,
 		Params: []testing.Param{{
-			Name: "us_en_guest",
-			Val: testParams{
-				inputMethodID: ime.INPUTMETHOD_XKB_US_ENG,
-				userMode:      "guest",
-			},
-			Pre: pre.VKEnabledInGuest,
-		}, {
-			Name: "us_en_incognito",
-			Val: testParams{
-				inputMethodID: ime.INPUTMETHOD_XKB_US_ENG,
-				userMode:      "incognito",
-			},
-			Pre: pre.VKEnabledReset,
-		}, {
-			Name: "jp_jpn_guest",
-			Val: testParams{
-				inputMethodID: ime.INPUTMETHOD_XKB_JP_JPN,
-				userMode:      "guest",
-			},
-			Pre: pre.VKEnabledReset,
-		}, {
-			Name: "jp_jpn_incognito",
-			Val: testParams{
-				inputMethodID: ime.INPUTMETHOD_XKB_JP_JPN,
-				userMode:      "incognito",
-			},
-			Pre: pre.VKEnabledReset,
-		}},
+			Name: "guest",
+			Pre:  pre.VKEnabledInGuest,
+		},
+			{Name: "incognito",
+				Pre: pre.VKEnabledReset},
+		},
 	})
 }
 
 func VirtualKeyboardTypingUserMode(ctx context.Context, s *testing.State) {
-	testParams := s.Param().(testParams)
-
 	cr := s.PreValue().(pre.PreData).Chrome
 	tconn := s.PreValue().(pre.PreData).TestAPIConn
 
-	its, err := testserver.LaunchInMode(ctx, cr, tconn, testParams.userMode == "incognito")
+	its, err := testserver.LaunchInMode(ctx, cr, tconn, s.TestName() == "incognito")
 	if err != nil {
 		s.Fatal("Failed to launch inputs test server: ", err)
 	}
 	defer its.Close()
 
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+	inputField := testserver.TextAreaInputField
+	vkbCtx := vkb.NewContext(cr, tconn)
 
-	if err := its.ValidateVKInputOnField(testserver.TextAreaInputField, testParams.inputMethodID)(ctx); err != nil {
-		s.Fatalf("Failed to VK input in %q mode using input method %q", testParams.userMode, string(testParams.inputMethodID))
+	subtest := func(testName string, inputData data.InputData) func(ctx context.Context, s *testing.State) {
+		return func(ctx context.Context, s *testing.State) {
+			cleanupCtx := ctx
+			// Use a shortened context for test operations to reserve time for cleanup.
+			ctx, shortCancel := ctxutil.Shorten(ctx, 10*time.Second)
+			defer shortCancel()
+
+			defer func(ctx context.Context) {
+
+				outDir := filepath.Join(s.OutDir(), testName)
+				faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, s.HasError, cr, "ui_tree_"+testName)
+
+				if err := vkbCtx.HideVirtualKeyboard()(ctx); err != nil {
+					s.Log("Failed to hide virtual keyboard: ", err)
+				}
+			}(cleanupCtx)
+
+			if err := its.ClickFieldUntilVKShown(inputField)(ctx); err != nil {
+				s.Fatal("Failed to show VK: ", err)
+			}
+
+			if err := uiauto.Combine("validate vk input function on field "+string(inputField),
+				its.Clear(inputField),
+				func(ctx context.Context) error {
+					if err := vkbCtx.TapKeysIgnoringCase(inputData.CharacterKeySeq)(ctx); err != nil {
+						return errors.Wrapf(err, "failed to tap keys: %v", inputData.CharacterKeySeq)
+					}
+					if inputData.SubmitFromSuggestion {
+						return vkbCtx.SelectFromSuggestion(inputData.ExpectedText)(ctx)
+					}
+					return nil
+				},
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), inputData.ExpectedText),
+			)(ctx); err != nil {
+				s.Fatal("Failed to validate virtual keyboard input: ", err)
+			}
+
+		}
 	}
+
+	// Run defined subtest per input method and message combination.
+	util.RunSubtestsPerInputMethodAndMessage(ctx, tconn, s, typingModeTestIMEs, typingModeTestMessages, subtest)
 }
