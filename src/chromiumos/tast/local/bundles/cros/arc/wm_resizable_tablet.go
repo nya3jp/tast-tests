2e2f57e30abe70947a1bb033c60ce79f4bd51feb
Kazuki Takise <takise@google.com>
2021-07-21 18:20:18 +0900

    arc: Inject WM event instead of swiping in wmRT22
    Injecting input events tend to be flaky + the spec of the gesture
    changes frequently.
    
    As the arc.SplitView covers it, let's just simply inject WM events
    here.
    
    BUG=b:194156232
    TEST=arc.WMResizableTablet
    
    Change-Id: Id56192b521c9cbe847f75554a4122cc19ef3144f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3043471
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Tested-by: Kazuki Takise <takise@chromium.org>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Reviewed-by: Armen Keshishian <armenk@google.com>

--- src/chromiumos/tast/local/bundles/cros/arc/wm_resizable_tablet.go
+++ src/chromiumos/tast/local/bundles/cros/arc/wm_resizable_tablet.go
@@ -16,8 +16,6 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/display"
-	"chromiumos/tast/local/coords"
-	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
@@ -199,29 +197,6 @@ func wmRT17(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 // wmRT22 covers resizable/tablet: split screen.
 // Expected behavior is defined in: go/arc-wm-r RT22: resizable/tablet: split screen.
 func wmRT22(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device) (retErr error) {
-	// Test Steps:
-	// This test starts at landscape mode.
-	// 1- Start the first activity - Under activity.
-	// 2- Start the second activity - over activity.
-	// 3- Snap the over activity to the left:
-	//   3-1- Swipe the over activity from top-center to the middle of the screen.
-	//   3-2- Continue swiping the over activity from the middle of the screen to the top-left corner.
-	//   3-3- Make sure the over activity is snapped to the left.
-	// 4- Snap the under activity to the right:
-	//   4-1- Touch the middle of the right half of the screen.
-	//   4-2- Make sure the under activity is snapped to the right.
-	// 5- Get app window info for assertions:
-	//   5-1- Over activity must be snapped to the left.
-	//   5-2- Under activity must be snapped to the right.
-	// 6- Rotate the screen by 270 degrees - to portrait mode.
-	// 7- Get app window info for assertions:
-	//   7-1- Over activity must be snapped to the top.
-	//   7-2- Under activity must be snapped to the bottom.
-	// 8- Rotate the screen to 0 degrees - Back to landscape mode.
-	// 9- Get app window info for assertions:
-	//   9-1- Over activity must be snapped to the left.
-	//   9-2- Under activity must be snapped to the right.
-
 	ctxForStopOverActivity := ctx
 	ctx, cancelForStopOverActivity := ctxutil.Shorten(ctx, wm.TimeReservedForStop)
 	defer cancelForStopOverActivity()
@@ -242,7 +217,7 @@ func wmRT22(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 	}
 	defer underActivity.Close()
 
-	// 1- Start the first activity - Under activity.
+	// Start the first activity - Under activity.
 	if err := underActivity.Start(ctx, tconn); err != nil {
 		return errors.Wrap(err, "failed to start under activity")
 	}
@@ -268,7 +243,7 @@ func wmRT22(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 	}
 	defer overActivity.Close()
 
-	// 2- Start the second activity - over activity.
+	// Start the second activity - over activity.
 	if err := overActivity.Start(ctx, tconn); err != nil {
 		return errors.Wrap(err, "failed to start over activity")
 	}
@@ -286,57 +261,16 @@ func wmRT22(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 		return errors.Wrap(err, "failed to wait until over activity is ready")
 	}
 
-	pdInfo, err := display.GetPrimaryInfo(ctx, tconn)
-	if err != nil {
-		return err
-	}
-	if pdInfo == nil {
-		return errors.New("failed to find primary display info")
-	}
-
-	tew, err := input.Touchscreen(ctx)
-	if err != nil {
-		return errors.Wrap(err, "failed to create touch screen event writer")
-	}
-	defer tew.Close()
-
-	stw, err := tew.NewSingleTouchWriter()
-	if err != nil {
-		return errors.Wrap(err, "failed to create single touch screen writer")
-	}
-	defer stw.Close()
-
-	tcc := tew.NewTouchCoordConverter(pdInfo.Bounds.Size())
-
-	// 3- Snap the over activity to the left - Start.
-	from := coords.NewPoint(pdInfo.WorkArea.Width/2, 0)
-	to := coords.NewPoint(pdInfo.WorkArea.Width/2, pdInfo.WorkArea.Height/2)
-
-	x0, y0 := tcc.ConvertLocation(from)
-	x1, y1 := tcc.ConvertLocation(to)
-
-	//   3-1- Swipe the over activity from top-center to the middle of the screen.
-	if err := stw.Swipe(ctx, x0, y0, x1, y1, 500*time.Millisecond); err != nil {
-		return errors.Wrap(err, "failed to swipe")
-	}
-
-	//   3-2- Continue swiping the over activity from the middle of the screen to the top-left corner.
-	to = coords.NewPoint(1, 1)
-	x2, y2 := tcc.ConvertLocation(to)
-
-	if err := stw.Swipe(ctx, x1, y1, x2, y2, 500*time.Millisecond); err != nil {
-		return errors.Wrap(err, "failed to swipe")
-	}
-	if err := stw.End(); err != nil {
-		return errors.Wrap(err, "failed to finish the swipe gesture")
-	}
-
 	overActivityWInfo, err := ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24Secondary)
 	if err != nil {
 		return errors.Wrap(err, "failed to get arc app window info for over activity")
 	}
 
-	//   3-3- Make sure the over activity is snapped to the left.
+	if _, err := ash.SetARCAppWindowState(ctx, tconn, wm.Pkg24Secondary, ash.WMEventSnapLeft); err != nil {
+		return errors.Wrapf(err, "failed to left snap %s", wm.Pkg24Secondary)
+	}
+
+	//  Make sure the over activity is snapped to the left.
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
 		overActivityWInfo, err := ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24Secondary)
 		if err != nil {
@@ -350,27 +284,15 @@ func wmRT22(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
 		return err
 	}
-	// 3- Snap the over activity to the left - End.
+	// Snap the over activity to the left - End.
 
-	// 4- Snap the under activity to the right - Start.
-	overActivityWInfo, err = ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24Secondary)
-	if err != nil {
-		return errors.Wrap(err, "failed to get arc app window info for over activity")
+	// Snap the under activity to the right - Start.
+	if _, err := ash.SetARCAppWindowState(ctx, tconn, wm.Pkg24, ash.WMEventSnapRight); err != nil {
+		return errors.Wrapf(err, "failed to right snap %s", wm.Pkg24)
 	}
 
-	mid := coords.NewPoint(pdInfo.WorkArea.Width*3/4, pdInfo.WorkArea.Height/2)
-	x, y := tcc.ConvertLocation(mid)
-
-	//   4-1- Touch the middle of the right half of the screen.
-	if err := stw.Move(x, y); err != nil {
-		return errors.Wrap(err, "failed to move touch")
-	}
-	if err := stw.End(); err != nil {
-		return errors.Wrap(err, "failed to end touch")
-	}
-
-	//   4-2- Make sure the under activity is snapped to the right.
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		// Make sure the under activity is snapped to the right.
 		underActivityWInfo, err := ash.GetARCAppWindowInfo(ctx, tconn, wm.Pkg24)
 		if err != nil {
 			return testing.PollBreak(errors.Wrap(err, "failed to get arc app window info for under activity"))
@@ -383,33 +305,32 @@ func wmRT22(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
 		return err
 	}
-	// 4- Snap the under activity to the right - End.
+	// Snap the under activity to the right - End.
 
-	// 5- Get app window info for assertions. Over activity must be snapped to the left and under activity to the right.
+	// Get app window info for assertions. Over activity must be snapped to the left and under activity to the right.
 	if err := wm.CheckVerticalTabletSplit(ctx, tconn); err != nil {
 		return errors.Wrap(err, "failed to assert vertical split window bounds before rotation")
 	}
 
-	// 6- Rotate the screen by 270 degrees - to portrait mode.
+	// Rotate the screen by 270 degrees - to portrait mode.
 	cleanupRotation, err = wm.RotateDisplay(ctx, tconn, display.Rotate270)
 	if err != nil {
 		return err
 	}
 	defer cleanupRotation()
 
-	// 7- Get app window info for assertions. Over activity must be snapped to the top and under activity to the bottom.
+	// Get app window info for assertions. Over activity must be snapped to the top and under activity to the bottom.
 	if err := wm.CheckHorizontalTabletSplit(ctx, tconn); err != nil {
 		return errors.Wrap(err, "failed to assert horizontal split window bounds in portrait mode")
 	}
 
-	// 8- Rotate the screen to 0 degrees - Back to landscape mode.
+	// Rotate the screen to 0 degrees - Back to landscape mode.
 	cleanupRotation, err = wm.RotateDisplay(ctx, tconn, display.Rotate0)
 	if err != nil {
 		return err
 	}
 	defer cleanupRotation()
 
-	// 9- Get app window info for assertions. Over activity must be snapped to the left and under activity to the right.
 	if err := wm.CheckVerticalTabletSplit(ctx, tconn); err != nil {
 		return errors.Wrap(err, "failed to assert vertical split window bounds after rotation")
 	}
