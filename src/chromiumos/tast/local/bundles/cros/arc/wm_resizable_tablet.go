1fb02213fbcf52feee46a5ae9b36d6a93bc41fc3
Kazuki Takise <takise@google.com>
2021-07-21 18:23:05 +0900

    arc: Remove sleep() from wmRT22
    With the proper polling, we don't have to sleep()
    
    BUG=b:194156232
    TEST=arc.WMResizableTablet
    
    Change-Id: I598bb3a55401dbae27c90e8da3f9318456c67dcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3043472
    Tested-by: Kazuki Takise <takise@chromium.org>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Reviewed-by: Armen Keshishian <armenk@google.com>

--- src/chromiumos/tast/local/bundles/cros/arc/wm_resizable_tablet.go
+++ src/chromiumos/tast/local/bundles/cros/arc/wm_resizable_tablet.go
@@ -391,10 +391,6 @@ func wmRT22(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 	}
 	defer cleanupRotation()
 
-	if err := testing.Sleep(ctx, wm.RotationAnimationDuration); err != nil {
-		return errors.Wrap(err, "failed to sleep for rotation to portrait animation to finish")
-	}
-
 	// 7- Get app window info for assertions. Over activity must be snapped to the top and under activity to the bottom.
 	if err := wm.CheckHorizontalTabletSplit(ctx, tconn, pdInfo.WorkArea); err != nil {
 		return errors.Wrap(err, "failed to assert horizontal split window bounds in portrait mode")
@@ -406,10 +402,6 @@ func wmRT22(ctx context.Context, tconn *chrome.TestConn, a *arc.ARC, d *ui.Devic
 		return err
 	}
 	defer cleanupRotation()
-
-	if err := testing.Sleep(ctx, wm.RotationAnimationDuration); err != nil {
-		return errors.Wrap(err, "failed to sleep for rotation to landscape animation to finish")
-	}
 
 	// 9- Get app window info for assertions. Over activity must be snapped to the left and under activity to the right.
 	if err := wm.CheckVerticalTabletSplit(ctx, tconn, pdInfo.WorkArea); err != nil {
