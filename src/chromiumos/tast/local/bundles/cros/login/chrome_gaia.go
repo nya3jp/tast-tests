4232551c675539b4a14a884eaf126f1c904f3d4b
David Haddock <dhaddock@google.com>
2021-07-21 15:52:27 -0700

    oac: Keep owners consistent for OAC tests.
    This ensure we can have a single tastboard view.
    
    BUG=None
    TEST=None
    
    Change-Id: Ib6fdc74cb0d9dcc747694c7ee5de8b2e9bef22d9

--- src/chromiumos/tast/local/bundles/cros/login/chrome_gaia.go
+++ src/chromiumos/tast/local/bundles/cros/login/chrome_gaia.go
@@ -19,6 +19,8 @@ func init() {
 		Contacts: []string{
 			"chromeos-ui@google.com",
 			"tast-owners@google.com",
+			"cros-oac@google.com",
+			"chromeos-sw-engprod@google.com",
 		},
 		SoftwareDeps: []string{
 			"chrome",
