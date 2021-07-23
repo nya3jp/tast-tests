9874a5611d90a49df1994706d413472a2b6061b4
Jun Sheng <shengjun@google.com>
2021-07-23 15:17:55 +1000

    tast-tests: migrate input tests to new IME struct
    1. Add ActiveInputMethod to find current input method.
    2. Refactor common libs such as preconditions, inputData, testServer, utils.
    3. Refacor relevant input tests.
    
    ARC++ tests will be refactored later. Besides, old constants will be deprecated after all done.
    
    BUG=b:192819861
    TEST=tast run <DUT> inputs.*
    
    Change-Id: Icbaec8c5770c449dba2602870373c3387c48b041

--- src/chromiumos/tast/local/bundles/cros/inputs/data/inputs_data.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/data/inputs_data.go
@@ -30,27 +30,28 @@ type InputData struct {
 // Message is a generic type that provides a function of retrieving input data
 // by input methods.
 type Message interface {
-	GetInputData(inputMethodCode ime.InputMethodCode) (InputData, bool)
+	GetInputData(im ime.InputMethod) (InputData, bool)
 }
 
 // LanguageOfIME matches an input method to a language. This mapping is
 // intentionally for language-based handwriting and voice. Use with care
 // for other scenarios, as this mapping may not be suitable.
-var LanguageOfIME = map[ime.InputMethodCode]ime.Language{
-	ime.INPUTMETHOD_XKB_US_ENG:                ime.LANGUAGE_EN,      //NOLINT
-	ime.INPUTMETHOD_PINYIN_CHINESE_SIMPLIFIED: ime.LANGUAGE_ZH_HANS, //NOLINT
-	ime.INPUTMETHOD_NACL_MOZC_JP:              ime.LANGUAGE_JA,      //NOLINT
-	ime.INPUTMETHOD_NACL_MOZC_US:              ime.LANGUAGE_JA,      //NOLINT
+var LanguageOfIME = map[ime.InputMethod]ime.Language{
+	ime.EnglishUS:              ime.LANGUAGE_EN,      //NOLINT
+	ime.ChinesePinyin:          ime.LANGUAGE_ZH_HANS, //NOLINT
+	ime.Japanese:               ime.LANGUAGE_JA,      //NOLINT
+	ime.JapaneseWithUSKeyboard: ime.LANGUAGE_JA,      //NOLINT
 }
 
 // ExtractExternalFiles returns the file names contained in messages for
 // selected input methods.
-func ExtractExternalFiles(messages []Message, inputMethods []ime.InputMethodCode) []string {
+func ExtractExternalFiles(messages []Message, inputMethods []ime.InputMethod) []string {
 	var files = []string{}
 
 	for _, message := range messages {
-		for _, inputMethod := range inputMethods {
-			inputData, _ := message.GetInputData(inputMethod)
+		for _, im := range inputMethods {
+			//TODO(b/192819861): evaluate the OK result.
+			inputData, _ := message.GetInputData(im)
 			if inputData.HandwritingFile != "" {
 				files = append(files, inputData.HandwritingFile)
 			}
