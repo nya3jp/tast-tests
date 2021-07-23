a481da7be3fef87441fa37972c7d1dcf7290b63d
Craig Hesling <hesling@chromium.org>
2021-07-23 13:44:02 -0400

    firmware: Disable firmware.FpSensor from fingerprint-cq
    This test has become broken and is blocking some unrelated biod
    changes. This is a temporary disable.
    
    See b/194500081 for the fix.
    
    BUG=b:194500081, b:182596510
    TEST=none
    
    Change-Id: I6a3c8f0c89fd9ecde4aba6b76bb2bbffaa8cd249

--- src/chromiumos/tast/remote/bundles/cros/firmware/fp_sensor.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/fp_sensor.go
@@ -27,7 +27,7 @@ func init() {
 			"tomhughes@chromium.org",
 			"chromeos-fingerprint@google.com",
 		},
-		Attr:         []string{"group:mainline", "group:fingerprint-cq", "informational"},
+		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"biometrics_daemon"},
 		HardwareDeps: hwdep.D(hwdep.Fingerprint()),
 		ServiceDeps:  []string{"tast.cros.platform.UpstartService", dutfs.ServiceName},
