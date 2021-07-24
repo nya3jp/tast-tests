98aece6ff560a0d90048b79f3586796fede823ca
Twain Byrnes <binarynewts@google.com>
2021-07-24 00:18:38 +0000

    fwupd: Inhibit suspend during update test
    Ensures that the .lock file prohibiting system suspension does not exist
    before and after an update occurs, but does exist during the update.
    
    BUG=b:194512749
    TEST=tast run [user]@host:[port] firmware.FwupdInhibitSuspend
    
    Change-Id: I2e9f7b3cc4e28dcd1ff8fa2acbe74472cf0b6cb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3053842
    Reviewed-by: Daniel Campello <campello@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Twain Byrnes <binarynewts@google.com>
    Commit-Queue: Daniel Campello <campello@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_inhibit_suspend.go
@@ -0,0 +1,119 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"bufio"
+	"context"
+	"io"
+	"os"
+	"strings"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FwupdInhibitSuspend,
+		Desc: "Ensures .lock file does not exist before, after update, does exist during",
+		Contacts: []string{
+			"binarynewts@google.org",    // Test Author
+			"chromeos-fwupd@google.com", // CrOS FWUPD
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"fwupd"},
+	})
+}
+
+// updateChecker checks if an update is available and determines whether to update if there is or to reinstall one if there isn't
+func updateChecker(ctx context.Context) (string, error) {
+	cmd := testexec.CommandContext(ctx, "/usr/bin/fwupdmgr", "get-updates")
+	err := cmd.Run(testexec.DumpLogOnError)
+	if code, ok := testexec.ExitCode(err); ok {
+		if code == 0 {
+			return "update", nil
+		}
+		if code == 2 {
+			return "reinstall", nil
+		}
+	}
+	return "", err
+}
+
+// streamOutput sends back messages as they occur
+func streamOutput(rc io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	scanner := bufio.NewScanner(rc)
+	go func() {
+		for scanner.Scan() {
+			if s := scanner.Text(); s != "" {
+				ch <- s
+			}
+		}
+		close(ch)
+	}()
+
+	return ch
+}
+
+// FwupdInhibitSuspend runs the fwupdtool utility and makes sure
+// that the system can suspend before and after, but not during an update.
+func FwupdInhibitSuspend(ctx context.Context, s *testing.State) {
+	// make sure file does not exist before update
+	if _, err := os.Stat("/run/lock/power_override/fwupd.lock"); err == nil {
+		s.Fatal("System cannot suspend but no update has started")
+	}
+
+	// run the update
+	updtype, err := updateChecker(ctx)
+	if err != nil {
+		s.Fatal("update check failed: ", err)
+	}
+	// b585990a-003e-5270-89d5-3705a17f9a43 is the GUID for a fake device
+	cmd := testexec.CommandContext(ctx, "/usr/bin/fwupdmgr", updtype, "-v", "b585990a-003e-5270-89d5-3705a17f9a43")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		s.Fatalf("%q failed: %v", cmd.Args, err)
+	}
+
+	// watch output until update begins write phase
+	outch := streamOutput(stdout)
+	defer func() {
+		for range outch {
+		}
+	}()
+
+	if err := cmd.Start(); err != nil {
+		s.Fatalf("%q failed: %v", cmd.Args, err)
+	}
+	defer func() {
+		cmd.Kill()
+		cmd.Wait()
+	}()
+
+	// ensure write phase entered; stop reading output at this point
+	write := false
+	for str := range outch {
+		if strings.Contains(str, "Emitting ::status-changed() [device-write]") {
+			write = true
+			break
+		}
+	}
+	if !write {
+		s.Fatal("Write phase not entered by fwupd")
+	}
+
+	// ensure that file exists during update
+	if _, err := os.Stat("/run/lock/power_override/fwupd.lock"); os.IsNotExist(err) {
+		s.Fatal("System can suspend but update is in progress")
+	}
+
+	// make sure that file does not exist after update completed
+	cmd.Wait()
+	if _, err := os.Stat("/run/lock/power_override/fwupd.lock"); err == nil {
+		s.Fatal("System cannot suspend but update has finished")
+	}
+}
