c93925e4f1523cddd3bc6f7051774616720847de
Twain Byrnes <binarynewts@google.com>
2021-07-24 00:18:38 +0000

    fwupd: Inhibit suspend during update test
    Ensures that the .lock file prohibiting system suspension does not exist
    before and after an update occurs, but does exist for the course of the
    update.
    
    BUG=b:194512749
    TEST=tast run [user@]host[:port] firmware.FwupdInhibitSuspend
    
    Change-Id: I2e9f7b3cc4e28dcd1ff8fa2acbe74472cf0b6cb2

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_inhibit_suspend.go
@@ -0,0 +1,77 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"os"
+
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FwupdInhibitSuspend,
+		Desc: "Ensures .lock file does not exist before, after update, does exist during",
+		Contacts: []string{
+			"binarynewts@google.org",    // Test Author
+			"chromeos-fwupd@google.com", // CrOS FWUPD
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"fwupd"},
+	})
+}
+
+func RunUpdate(ctx context.Context, s *testing.State, finished chan bool) {
+	cmd := testexec.CommandContext(ctx, "/usr/bin/fwupdmgr", "update", "-v", "b585990a-003e-5270-89d5-3705a17f9a43")
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		s.Errorf("%q failed: %v", cmd.Args, err)
+	}
+	finished <- true
+}
+
+// FwupdInhibitSuspend runs the fwupdtool utility and makes sure
+// that the system can suspend before and after, but not during an update.
+func FwupdInhibitSuspend(ctx context.Context, s *testing.State) {
+	finished := make(chan bool)
+
+	if _, err := os.Stat("/run/lock/power_override/fwupd.lock"); err == nil {
+		s.Fatal("System cannot suspend but no update has started")
+	}
+
+	go RunUpdate(ctx, s, finished)
+
+	conn, err := dbus.ConnectSessionBus()
+	if err != nil {
+		s.Fatal("Failed to connect to session bus:", err)
+	}
+	defer conn.Close()
+
+	if err = conn.AddMatchSignal(
+		dbus.WithMatchObjectPath("/org/freedesktop/DBus"),
+		dbus.WithMatchInterface("org.freedesktop.DBus"),
+		dbus.WithMatchSender("org.freedesktop.DBus"),
+	); err != nil {
+		panic(err)
+	}
+
+	c := make(chan *dbus.Signal, 10)
+	conn.Signal(c)
+	/*for v := range c {
+		fmt.Println(v)
+	}*/
+
+	//if _, err := os.Stat("/run/lock/power_override/fwupd.lock"); os.IsNotExist(err) {
+	//       s.Fatalf("System can suspend but update is in progress")
+	//}
+
+	<-finished
+	if _, err := os.Stat("/run/lock/power_override/fwupd.lock"); err == nil {
+		s.Fatal("System cannot suspend but update has finished")
+	}
+}
