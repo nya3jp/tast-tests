c2f86b03553c65e45f0693bef256f9522a5eea04
Renuga Nanjappan <rnanjappan@chromium.org>
2021-07-24 02:02:23 +0000

    Tast Test with Sample Android Share App
    BUG=None
    TEST=tast run <DUT> arc.NearbyShare.vm
    
    Change-Id: If16dfa5e926cc8ca30c442159bdfaee0f11d5e56

---
+++ src/chromiumos/tast/local/bundles/cros/arc/nearby_share.go
@@ -0,0 +1,85 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/optin"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NearbyShare,
+		Desc:         "Verifies ARC removable media can be enabled from Chrome OS Settings",
+		Contacts:     []string{"rnanjappan@google.com", "cros-arc-te@google.com", "arc-storage@google.com"},
+		Attr:         []string{"group:mainline", "informational", "group:arc-functional"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		Timeout: chrome.GAIALoginTimeout + arc.BootTimeout + 1*time.Minute,
+		VarDeps: []string{"ui.gaiaPoolDefault"},
+	})
+}
+
+func NearbyShare(ctx context.Context, s *testing.State) {
+	// Set up Chrome.
+	cr, err := chrome.New(ctx,
+		chrome.GAIALoginPool(s.RequiredVar("ui.gaiaPoolDefault")),
+		chrome.ARCSupported(),
+		chrome.ExtraArgs(arc.DisableSyncFlags()...))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// Optin to PlayStore and close the app.
+	if err := optin.PerformAndClose(ctx, cr, tconn); err != nil {
+		s.Fatal("Failed to optin to Play Store and Close: ", err)
+	}
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close(ctx)
+
+	const (
+		// This is a plain app that triggers "Android" folder creation when external storage permission is ON.
+		apk = "ArcNearbyShareTest.apk"
+		pkg = "org.chromium.arc.testapp.nearbysharetast"
+		cls = ".MainActivity"
+	)
+
+	if err := a.Install(ctx, arc.APKPath(apk)); err != nil {
+		s.Fatal("Failed to install app: ", err)
+	}
+
+	act, err := arc.NewActivity(a, pkg, cls)
+	if err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	defer act.Close()
+
+	s.Log("Starting app")
+	if err = act.Start(ctx, tconn); err != nil {
+		s.Fatal("Failed to start app: ", err)
+	}
+}
