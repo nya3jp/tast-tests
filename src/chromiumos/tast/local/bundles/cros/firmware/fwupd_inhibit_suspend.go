f6cc1a589cac0bd40482cd5e16a79d5babcd9309
Twain Byrnes <binarynewts@google.com>
2021-07-24 00:18:38 +0000

    fwupd: Inhibit suspend during update test
    Ensures that the .lock file prohibiting system suspension does not exist
    before and after an update occurs, but does exist for the course of the
    update.
    
    BUG=b:194512749
    TEST=tast run [user@]host[:port] firmware.FwupdInhibitSuspend
    
    Change-Id: I2e9f7b3cc4e28dcd1ff8fa2acbe74472cf0b6cb2

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_inhibit_suspend.go
@@ -0,0 +1,55 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+        "context"
+
+        "chromiumos/tast/common/testexec"
+        "chromiumos/tast/testing"
+)
+
+func init() {
+        testing.AddTest(&testing.Test{
+                Func: FwupdInhibitSuspend,
+                Desc: "Ensures .lock file does not exist before, after update, does exist during",
+                Contacts: []string{
+                        "binarynewts@google.org",    // Test Author
+                        "chromeos-fwupd@google.com", // CrOS FWUPD
+                },
+                Attr:         []string{"group:mainline", "informational"},
+                SoftwareDeps: []string{"fwupd"},
+        })
+}
+
+func RunUpdate() chan int {
+        run := make(chan int)
+        go func() {
+                cmd := testexec.CommandContext(ctx, "/usr/bin/fwupdmgr", "update", "-v", "b585990a-003e-5270-89d5-3705a17f9a43")
+                if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+                        s.Fatalf("%q failed: %v", cmd.Args, err)
+                }
+		run <- 1
+        }()
+	return run
+}
+
+// FwupdInhibitSuspend runs the fwupdtool utility and makes sure
+// that the system can suspend before and after, but not during an update.
+func FwupdInhibitSuspend(ctx context.Context, s *testing.State) {
+        if _, err := os.Stat("/run/lock/power_override/fwupd.lock"); err == nil {
+                return errors.New("System cannot suspend but no update has started")
+        }
+
+        val := RunUpdate()
+        if _, err := os.Stat("/run/lock/power_override/fwupd.lock"); os.IsNotExist(err) {
+                return errors.New("System can suspend but update is in progress")
+        }
+
+        if _, err := os.Stat("/run/lock/power_override/fwupd.lock"); err == nil {
+                return errors.New("System cannot suspend but update has finished")
+        }
+}
+
