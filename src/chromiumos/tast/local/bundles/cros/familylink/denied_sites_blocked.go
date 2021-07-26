9c8c1758a181f5b5c2c4fc7a4cee74dd37441d7d
Danan S <danan@chromium.org>
2021-07-26 16:57:25 -0700

    Tast: Add familylink.DeniedSitesBlocked TAST test
    BUG=b:190654647
    TEST=Test passes on local caroline device
    
    Change-Id: I82fd9e5cd47e643d3aca635b86d34715d54c1d40

---
+++ src/chromiumos/tast/local/bundles/cros/familylink/denied_sites_blocked.go
@@ -0,0 +1,71 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package familylink is used for writing Family Link tests.
+package familylink
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome/familylink"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DeniedSitesBlocked,
+		Desc:         "Checks that parent-blocked sites are blocked for Unicorn users",
+		Contacts:     []string{"danan@chromium.org", "cros-families-eng+test@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      5 * time.Minute,
+		Vars:         []string{"unicorn.blockedSite"},
+		Fixture:      "familyLinkUnicornLogin",
+	})
+}
+
+func DeniedSitesBlocked(ctx context.Context, s *testing.State) {
+	tconn := s.FixtValue().(*familylink.FixtData).TestConn
+	cr := s.FixtValue().(*familylink.FixtData).Chrome
+
+	blockedSite := s.RequiredVar("unicorn.blockedSite")
+
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	ui := uiauto.New(tconn)
+
+	// The allow/block list can take a while to sync so infinitely loop checking
+	// for the website to be blocked.  If it doesn't happen within the test timeout
+	// period, this will fail via the timeout.
+	success := false
+	finalError := errors.New("foo")
+
+	defer func() {
+		if !success {
+			s.Fatal("Parent-blocked website is not blocked for Unicorn user", finalError)
+		}
+	}()
+
+	for attempts := 1; ; attempts++ {
+		conn, err := cr.NewConn(ctx, blockedSite)
+		if err != nil {
+			s.Fatal("Failed to open browser to website: ", err)
+		}
+		defer conn.Close()
+
+		if err := ui.WaitUntilExists(nodewith.Name("Ask your parent").Role(role.StaticText))(ctx); err != nil {
+			s.Logf("%d attempts to detect blocked site interstitial failed", attempts)
+			finalError = errors.Wrap(err, err.Error())
+			continue
+		}
+		success = true
+		break
+	}
+}
