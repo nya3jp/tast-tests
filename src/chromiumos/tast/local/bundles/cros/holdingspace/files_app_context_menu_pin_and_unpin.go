f18f83a4022fdf53b2049de3ee329a728887a737
Angus L. M. McLean IV <angusmclean@chromium.org>
2021-07-27 18:35:07 -0400

    Create tests for pinning files to Tote
    BUG=chromium:1187731
    TEST=Ran newly created tests locally and in trybot.
    
    Change-Id: I804a760b39416048b77146c5157eb293e05abcc6

---
+++ src/chromiumos/tast/local/bundles/cros/holdingspace/files_app_context_menu_pin_and_unpin.go
@@ -0,0 +1,70 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package holdingspace
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/chrome/uiauto/holdingspace"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FilesAppContextMenuPinAndUnpin,
+		Desc: "Checks that pinning to Holding Space from the Files app works",
+		Contacts: []string{
+			"angusmclean@chromium.org",
+			"dmblack@chromium.org",
+			"chromeos-sw-engprod@google.com",
+			"cros-system-ui-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// FilesAppContextMenuPinAndUnpin tests the functionality of pinning files to Holding Space
+// from the Files app.
+func FilesAppContextMenuPinAndUnpin(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatalf("Failed to log in to Chrome: %s", err)
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatalf("Failed to create Test API connection: %s", err)
+	}
+
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	fsapp, err := filesapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatalf("Could not open filesapp: %s", err)
+	}
+
+	const fileName = "test.txt"
+	filePath := filepath.Join(filesapp.MyFilesPath, fileName)
+	// Create our file, with appropriate permissions so we can delete later.
+	if err := ioutil.WriteFile(filePath, []byte("Per aspera, ad astra"), 0644); err != nil {
+		s.Fatalf("Creating file %s failed: %s", filePath, err)
+	}
+	defer os.Remove(filePath)
+
+	if err := holdingspace.PinViaFileContextMenu(ctx, tconn, fsapp, fileName); err != nil {
+		s.Fatalf("Failed to pin file: %s", err)
+	}
+
+	if err := holdingspace.UnpinItemViaContextMenu(ctx, tconn, fileName); err != nil {
+		s.Fatalf("Failed to unpin file: %s", err)
+	}
+}
--- src/chromiumos/tast/local/chrome/uiauto/holdingspace/holdingspace.go
+++ src/chromiumos/tast/local/chrome/uiauto/holdingspace/holdingspace.go
@@ -5,7 +5,15 @@
 package holdingspace
 
 import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/state"
 )
 
 // FindChip returns a finder which locates a holding space chip node with the
@@ -39,4 +47,87 @@ func FindPinnedFileChip(name string) *nodewith.Finder {
 // FindTray returns a finder which locates the holding space tray node.
 func FindTray() *nodewith.Finder {
 	return nodewith.ClassName("HoldingSpaceTray")
+}
+
+// UnpinItemViaContextMenu unpins an item from holding space.
+func UnpinItemViaContextMenu(ctx context.Context, tconn *chrome.TestConn,
+	name string) error {
+	uia := uiauto.New(tconn)
+
+	itemChipView := FindPinnedFileChip(name)
+	unpinMenuItem := FindContextMenuItem("Unpin")
+
+	if err := uiauto.Combine("Unpin holding space item via context menu",
+		uia.WaitUntilExists(itemChipView),
+		uia.RightClick(itemChipView),
+		uia.WaitUntilExists(unpinMenuItem),
+		uia.LeftClick(unpinMenuItem),
+	)(ctx); err != nil {
+		errors.Errorf("failed to unpin item from holding space: %s", err)
+	}
+
+	// If the tray doesn't exist, then we can return, since nothing is pinned.
+	if err := uia.EnsureGoneFor(FindTray(), time.Second)(ctx); err != nil {
+		return nil
+	}
+
+	if err := OpenBubble(ctx, tconn); err != nil {
+		return err
+	}
+
+	if err := uia.EnsureGoneFor(FindPinnedFileChip(name), 3*time.Second)(ctx); err != nil {
+		return errors.Errorf("Unpinning file %s failed: %s", name, err)
+	}
+
+	return nil
+}
+
+// PinViaFileContextMenu pins a given file visible in the given `FilesApp` to the
+// holding space via the context menu.
+func PinViaFileContextMenu(ctx context.Context, tconn *chrome.TestConn,
+	fsapp *filesapp.FilesApp, newFileName string) error {
+
+	// Pin the file to the shelf by using the context menu in the files app.
+	if err := fsapp.ClickContextMenuItem(newFileName, "Pin to shelf")(ctx); err != nil {
+		return errors.Errorf("Pinning file %s failed: %s", newFileName, err)
+	}
+
+	// Make sure the file got pinned.
+	if err := OpenBubble(ctx, tconn); err != nil {
+		return errors.Errorf("failed to open holding space bubble: %s", err)
+	}
+
+	uia := uiauto.New(tconn)
+	if err := uia.WaitUntilExists(FindPinnedFileChip(newFileName))(ctx); err != nil {
+		return errors.Errorf("failed to find pinned item %s: %s", newFileName, err)
+	}
+
+	return nil
+}
+
+// OpenBubble opens the holding space bubble.
+func OpenBubble(ctx context.Context, tconn *chrome.TestConn) error {
+	uia := uiauto.New(tconn)
+
+	pinnedFilesBubble := nodewith.ClassName("PinnedFilesBubble")
+
+	// If we find the bubble, it's already open. Return early.
+	if uia.Exists(pinnedFilesBubble)(ctx) == nil {
+		info, err := uia.Info(ctx, pinnedFilesBubble)
+		if err == nil && !info.State[state.Invisible] {
+			return nil
+		}
+	}
+
+	holdingSpaceTray := FindTray()
+
+	if err := uiauto.Combine("Open holding space bubble",
+		uia.WaitUntilExists(holdingSpaceTray),
+		uia.LeftClick(holdingSpaceTray),
+		uia.WaitUntilExists(pinnedFilesBubble),
+	)(ctx); err != nil {
+		return err
+	}
+
+	return nil
 }
