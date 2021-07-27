f1d2bfe5d7e2fcc66cd0f7c000136faf3c6a4a53
David Staessens <dstaessens@google.com>
2021-07-27 12:19:33 +0900

    arc.VideoEncodeAccelPerf: Add software video encoder performance tests.
    This CL adds the following software video encoder performance tests:
    - h264_1080p_i420_sw
    - h264_1080p_i420_sw_vm
    - vp8_1080p_i420_sw_vm
    - vp9_1080p_i420_sw_vm
    
    Note: These tests are disabled in the lab.
    
    BUG=b:169626661
    TEST=tast run DUT arc.VideoEncodeAccel.h264_1080p_i420_sw_vm
    
    Change-Id: Ia02044b439508e91e95de6d0f65eef7835ba1cac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3054754
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Judy Wu <jinrongwu@google.com>
    Tested-by: David Staessens <dstaessens@chromium.org>
    Commit-Queue: David Staessens <dstaessens@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/video_encode_accel_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/video_encode_accel_perf.go
@@ -26,7 +26,6 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnknown,
 		Desc:         "Measures ARC++ and ARCVM hardware video encode performance by running the arcvideoencoder_test binary",
 		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
-		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		Data:         []string{c2e2etest.X86ApkName, c2e2etest.ArmApkName},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      "arcBooted", // TODO(akahuang): Implement new precondition to boot ARC and enable verbose at chromium.
@@ -38,6 +37,17 @@ func init() {
 				Params:      video.Crowd1080P,
 				PixelFormat: videotype.I420,
 			},
+			ExtraAttr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+			ExtraData:         []string{video.Crowd1080P.Name},
+			ExtraSoftwareDeps: []string{"android_p", caps.HWEncodeH264},
+		}, {
+			Name: "h264_1080p_i420_sw",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Crowd1080P,
+				PixelFormat: videotype.I420,
+				EncoderType: video.SoftwareEncoder,
+			},
 			ExtraData:         []string{video.Crowd1080P.Name},
 			ExtraSoftwareDeps: []string{"android_p", caps.HWEncodeH264},
 		}, {
@@ -47,9 +57,20 @@ func init() {
 				Params:      video.Crowd1080P,
 				PixelFormat: videotype.I420,
 			},
+			ExtraAttr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 			ExtraData:         []string{video.Crowd1080P.Name},
 			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeH264},
 			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform(video.EncoderBlocklistVM...)),
+		}, {
+			Name: "h264_1080p_i420_sw_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Crowd1080P,
+				PixelFormat: videotype.I420,
+				EncoderType: video.SoftwareEncoder,
+			},
+			ExtraData:         []string{video.Crowd1080P.Name},
+			ExtraSoftwareDeps: []string{"android_vm"},
 		}, {
 			Name: "vp8_1080p_i420_vm",
 			Val: video.EncodeTestOptions{
@@ -57,17 +78,42 @@ func init() {
 				Params:      video.Crowd1080P,
 				PixelFormat: videotype.I420,
 			},
+			ExtraAttr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 			ExtraData:         []string{video.Crowd1080P.Name},
 			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP8},
 			ExtraHardwareDeps: hwdep.D(
 				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
 				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
 		}, {
+			Name: "vp8_1080p_i420_sw_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP8Prof,
+				Params:      video.Crowd1080P,
+				PixelFormat: videotype.I420,
+				EncoderType: video.SoftwareEncoder,
+			},
+			ExtraData:         []string{video.Crowd1080P.Name},
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}, {
 			Name: "vp9_1080p_i420_vm",
 			Val: video.EncodeTestOptions{
 				Profile:     videotype.VP9Prof,
 				Params:      video.Crowd1080P,
 				PixelFormat: videotype.I420,
+			},
+			ExtraAttr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+			ExtraData:         []string{video.Crowd1080P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP9},
+			ExtraHardwareDeps: hwdep.D(
+				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
+				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
+		}, {
+			Name: "vp9_1080p_i420_sw_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP9Prof,
+				Params:      video.Crowd1080P,
+				PixelFormat: videotype.I420,
+				EncoderType: video.SoftwareEncoder,
 			},
 			ExtraData:         []string{video.Crowd1080P.Name},
 			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP9},
