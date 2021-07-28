a22e9786401d261d9c89ab4dc6a13d2a0c709d9f
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-07-28 06:34:04 +0000

    tast-tests: DevicePowerPeakShift local policy test
    DevicePowerPeakShift tests the behaviour of DevicePowerPeakShiftEnabled
    as well as its subordinate policies. Peak Shift is a power management
    policy that reduces alternating current usage during peak hours.
    
    BUG=b:194873632
    TEST=tast run -var=servo=$SERVO $DUT policy.DevicePowerPeakShift
    
    Cq-Depend: chromium:2987941, chromium:3056633
    Change-Id: I2bb238e77e44b0b8b3dca26dffd146cdc4053b19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3058137
    Tested-by: Bisakh Mondal <bisakhmondal00@gmail.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>

---
+++ src/chromiumos/tast/local/wilco/rtc.go
@@ -0,0 +1,45 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wilco
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/local/rtc"
+	"chromiumos/tast/local/upstart"
+)
+
+// Job "wilco_sync_ec_rtc" continuously keeps the EC RTC in sync with local time (in wilco devices).
+const upstartJobName = "wilco_sync_ec_rtc"
+
+// RTC (wilco) is a thin wrapper around the rtc.RTC and implements the RTC features specific to the wilco devices.
+type RTC struct {
+	rtc.RTC
+}
+
+// MockECRTC mocks RTC time to the specified time by stopping necessary wilco specific job and returns a callback
+// to revert the state back to original.
+func (wrtc RTC) MockECRTC(ctx context.Context, t time.Time) (func(context.Context) error, error) {
+	// Stop the upstart job that keeps the wilco EC RTC in sync with local time.
+	if err := upstart.StopJob(ctx, upstartJobName); err != nil {
+		return nil, err
+	}
+	if err := wrtc.Write(ctx, t); err != nil {
+		return nil, err
+	}
+
+	return func(ctx context.Context) error {
+		if err := upstart.EnsureJobRunning(ctx, upstartJobName); err != nil {
+			return err
+		}
+		args := wrtc.HwclockArgs()
+		args = append([]string{"--systohc"}, args...)
+		ctx, cancel := context.WithTimeout(ctx, rtc.HwclockTimeout)
+		defer cancel()
+		return testexec.CommandContext(ctx, "hwclock", args...).Run(testexec.DumpLogOnError)
+	}, nil
+}
