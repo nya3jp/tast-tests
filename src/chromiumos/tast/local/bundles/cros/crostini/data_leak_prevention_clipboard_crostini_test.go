a39f3598314ab88559f2be5631521cf6324d5633
Vishal Chaudhary <vishal38785@gmail.com>
2021-07-28 15:51:51 +0530

    tast-tests: test for clipboard DLP restriction in Crostini
    Adds test to check whether clipboard restricition is restricted by DLP policy in Crostini
    
    BUG=chromium:1132208
    TEST=tast run <ip> crostini.DataLeakPreventionClipboardCrostini
    
    Change-Id: I42480a264bc23cbe2c6e6313e0e1e104a1b80048

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/data_leak_prevention_clipboard_crostini_test.go
@@ -0,0 +1,34 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+// To update test parameters after modifying this file, run:
+// TAST_GENERATE_UPDATE=1 ~/trunk/src/platform/tast/tools/go.sh test -count=1 chromiumos/tast/local/bundles/cros/crostini/
+
+// See src/chromiumos/tast/local/crostini/params.go for more documentation
+
+import (
+	"testing"
+
+	"chromiumos/tast/common/genparams"
+	"chromiumos/tast/local/crostini"
+)
+
+func TestDataLeakPreventionClipboardCrostiniParams(t *testing.T) {
+	params := crostini.MakeTestParamsFromList(t, []crostini.Param{
+		{
+			Name: "to_wayland",
+			Val: `testParameters{
+				Paste: waylandPasteConfig,
+			}`,
+		},
+		{
+			Name: "to_x11",
+			Val: `testParameters{
+				Paste: x11PasteConfig,
+			}`,
+		}})
+	genparams.Ensure(t, "data_leak_prevention_clipboard_crostini.go", params)
+}
