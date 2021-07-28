ad0fe9c29a09339ddae60b1a3b1f5341d6405ba7
Chung-Sheng Wu <chungsheng@google.com>
2021-07-28 16:33:27 +0800

    health: Add bus info probing tast
    Add tast test to run the bus info probling. Validation will be added in
    the following CL.
    
    BUG=b:194448732
    TEST=tast run $DUT health.ProbeBusInfo
    
    Change-Id: Ia99cfe8ef151eedcda28414d26ae8d2921192035

---
+++ src/chromiumos/tast/local/bundles/cros/health/probe_bus_info.go
@@ -0,0 +1,84 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package health
+
+import (
+	"context"
+
+	"chromiumos/tast/local/croshealthd"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ProbeBusInfo,
+		Desc: "Check that we can probe cros_healthd for bus info",
+		Contacts: []string{
+			"cros-tdm-tpe-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "diagnostics"},
+		Fixture:      "crosHealthdRunning",
+	})
+}
+
+func ProbeBusInfo(ctx context.Context, s *testing.State) {
+	params := croshealthd.TelemParams{Category: croshealthd.TelemCategoryBus}
+	var res busResult
+	if err := croshealthd.RunAndParseJSONTelem(ctx, params, s.OutDir(), &res); err != nil {
+		s.Fatal("Failed to get bus telemetry info: ", err)
+	}
+	for _, d := range res.Devices {
+		if d.BusInfo.PciBusInfo != nil {
+			// TODO(chungsheng): Add validation
+		} else if d.BusInfo.UsbBusInfo != nil {
+			// TODO(chungsheng): Add validation
+		} else {
+			s.Fatal("Unknown types of bus devices")
+		}
+	}
+}
+
+type pciBusInfo struct {
+	ClassID    uint8   `json:"class_id"`
+	SubClassID uint8   `json:"subclass_id"`
+	ProgIfID   uint8   `json:"prog_if_id"`
+	VendorID   uint16  `json:"vendor_id"`
+	DeviceID   uint16  `json:"device_id"`
+	Driver     *string `json:"driver"`
+}
+
+type usbInterfaceInfo struct {
+	InterfaceNumber uint8   `json:"interface_number"`
+	ClassID         uint8   `json:"class_id"`
+	SubClassID      uint8   `json:"subclass_id"`
+	ProtocolID      uint8   `json:"protocol_id"`
+	Driver          *string `json:"driver"`
+}
+
+type usbBusInfo struct {
+	ClassID    uint8              `json:"class_id"`
+	SubClassID uint8              `json:"subclass_id"`
+	ProtocolID uint8              `json:"protocol_id"`
+	VendorID   uint16             `json:"vendor_id"`
+	ProductID  uint16             `json:"product_id"`
+	Interfaces []usbInterfaceInfo `json:"interfaces"`
+}
+
+type busInfo struct {
+	PciBusInfo *pciBusInfo `json:"pci_bus_info"`
+	UsbBusInfo *usbBusInfo `json:"usb_bus_info"`
+}
+
+type busDevice struct {
+	VendorName  string  `json:"vendor_name"`
+	ProductName string  `json:"product_name"`
+	DeviceClass string  `json:"device_class"`
+	BusInfo     busInfo `json:"bus_info"`
+}
+
+type busResult struct {
+	Devices []busDevice `json:"devices"`
+}
