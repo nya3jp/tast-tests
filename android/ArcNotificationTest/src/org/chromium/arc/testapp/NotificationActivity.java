94725bea3b3ee34a91edd2e195737ee99ad5c2d9
Sara Kato <sarakato@google.com>
2021-07-29 15:12:11 +0900

    Allow ArcNotificationTest to generate high importance notifications.
    This CL adds a checkbox, which when checked will generate a notification
    with high importance. ARC++ notifications with high importance are
    shown as popup notifications.
    
    BUG=None.
    TEST=manual. Use test app to confirm that high important notifications
    are sent
    
    Change-Id: If8804d9d4c3beece49dff64737893d8bd4317b23

--- android/ArcNotificationTest/src/org/chromium/arc/testapp/NotificationActivity.java
+++ android/ArcNotificationTest/src/org/chromium/arc/testapp/NotificationActivity.java
@@ -8,6 +8,7 @@ package org.chromium.arc.testapp.notification;
 
 import android.app.Activity;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.CheckBox;
 import android.util.Log;
 
 public class NotificationActivity extends Activity implements View.OnClickListener {
@@ -71,12 +73,23 @@ public class NotificationActivity extends Activity implements View.OnClickListen
     }
 
     private void sendNotification(int id, String title, String text) {
-        Notification.Builder builder = new Notification.Builder(this)
-                .setSmallIcon(R.drawable.ic_adb_black_24dp)
-                .setContentTitle(title)
-                .setContentText(text);
+        Notification.Builder builder = new Notification.Builder(this);
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        boolean high_priority = ((CheckBox) findViewById(R.id.check_high_priority)).isChecked();
+        if (high_priority) {
+          // Create a high priority channel for notification to be sent on.
+          String channel_id = "ArcNotificationTest";
+          NotificationChannel channel = new NotificationChannel(channel_id, "high_priority_channel",
+                NotificationManager.IMPORTANCE_HIGH);
+          builder = new Notification.Builder(this, channel_id);
+          notificationManager.createNotificationChannel(channel);
+        }
+
+        builder.setSmallIcon(R.drawable.ic_adb_black_24dp)
+        .setContentTitle(title)
+        .setContentText(text);
+
         notificationManager.notify(id, builder.build());
     }
 
