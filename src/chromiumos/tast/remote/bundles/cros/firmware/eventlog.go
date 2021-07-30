c47c2821acfd5399b18cf644a4566562c96e0727
Jeremy Bettis <jbettis@google.com>
2021-07-30 12:25:43 -0600

    faft: Finish migrating eventlog to TAST
    Finish implementing the rest of the cases from firmware_EventLog.py.
    The old test has confusing behavior for suspend/result, but this test
    instead tests both with and without disable suspent to idle. We'll have
    to see what happens once this is running on all machines in skylab.
    
    BUG=b/174800291
    TEST=tast run $DUT firmware.Eventlog* # on lenoa
    
    Change-Id: Id536280f1d7d80e1d6313a0a5547cf10c7652022

--- src/chromiumos/tast/remote/bundles/cros/firmware/eventlog.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/eventlog.go
@@ -7,17 +7,31 @@ package firmware
 import (
 	"context"
 	"regexp"
+	"time"
 
+	fwCommon "chromiumos/tast/common/firmware"
 	"chromiumos/tast/remote/firmware"
 	"chromiumos/tast/remote/firmware/pre"
 	"chromiumos/tast/remote/firmware/reporters"
+	"chromiumos/tast/ssh"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
 
 // eventLogParams contains all the data needed to run a single test iteration.
 type eventLogParams struct {
-	additionalNormalEvents string
+	resetType     firmware.ResetType
+	bootToMode    fwCommon.BootMode
+	suspendResume bool
+	// All of the regexes in one of the sets must be present. Ex.
+	// [][]string{[]string{`Case 1A`, `Case 1B`}, []string{`Case 2A`, `Case 2[BC]`}}
+	// Any of these events would pass:
+	// Case 1A, Case 1B
+	// Case 2A, Case 2B
+	// Case 2A, Case 2C
+	requiredEventSets [][]string
+	prohibitedEvents  string
+	allowedEvents     string
 }
 
 func init() {
@@ -28,7 +42,7 @@ func init() {
 			"gredelston@google.com", // Test author
 			"cros-fw-engprod@google.com",
 		},
-		Attr: []string{"group:firmware", "firmware_experimental", "firmware_usb"},
+		Attr: []string{"group:firmware", "firmware_experimental"},
 		Data: []string{firmware.ConfigFile},
 		HardwareDeps: hwdep.D(
 			// Eventlog is broken/wontfix on veyron devices.
@@ -36,32 +50,97 @@ func init() {
 			hwdep.SkipOnPlatform("veyron_fievel"),
 			hwdep.SkipOnPlatform("veyron_tiger"),
 		),
-		Pre:          pre.NormalMode(),
 		ServiceDeps:  []string{"tast.cros.firmware.UtilsService", "tast.cros.firmware.BiosService"},
 		SoftwareDeps: []string{"crossystem", "flashrom"},
 		Vars:         []string{"servo"},
 		Params: []testing.Param{
+			// Test eventlog upon normal->normal reboot
 			{
+				Name:              "normal",
 				ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel("leona")),
-				Val:               eventLogParams{},
+				Pre:               pre.NormalMode(),
+				Val: eventLogParams{
+					resetType:         firmware.WarmReset,
+					requiredEventSets: [][]string{[]string{`System boot`}},
+					prohibitedEvents:  `Developer Mode|Recovery Mode|Sleep| Wake`,
+				},
 			},
 			{
 				// Allow some normally disallowed events on lenoa. b/184778308
-				Name:              "leona",
+				Name:              "leona_normal",
 				ExtraHardwareDeps: hwdep.D(hwdep.Model("leona")),
+				Pre:               pre.NormalMode(),
 				Val: eventLogParams{
-					additionalNormalEvents: `^ACPI Wake \| Deep S5$`,
+					resetType:         firmware.WarmReset,
+					requiredEventSets: [][]string{[]string{`System boot`}},
+					prohibitedEvents:  `Developer Mode|Recovery Mode|Sleep| Wake`,
+					allowedEvents:     `^ACPI Wake \| Deep S5$`,
 				},
 			},
+			// Test eventlog upon dev->dev reboot
+			{
+				Name:              "dev",
+				ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel("leona")),
+				Pre:               pre.DevModeGBB(),
+				Val: eventLogParams{
+					resetType:         firmware.WarmReset,
+					requiredEventSets: [][]string{[]string{`System boot`, `Chrome OS Developer Mode`}},
+					prohibitedEvents:  `Recovery Mode|Sleep| Wake`,
+				},
+			},
+			// Allow some normally disallowed events on lenoa. b/184778308
+			{
+				Name:              "leona_dev",
+				ExtraHardwareDeps: hwdep.D(hwdep.Model("leona")),
+				Pre:               pre.DevModeGBB(),
+				Val: eventLogParams{
+					resetType:         firmware.WarmReset,
+					requiredEventSets: [][]string{[]string{`System boot`, `Chrome OS Developer Mode`}},
+					prohibitedEvents:  `Recovery Mode|Sleep| Wake`,
+					allowedEvents:     `^ACPI Wake \| Deep S5$`,
+				},
+			},
+			// Test eventlog upon normal->rec reboot
+			{
+				Name:      "normal_rec",
+				Pre:       pre.NormalMode(),
+				ExtraAttr: []string{"firmware_usb"},
+				Val: eventLogParams{
+					bootToMode:        fwCommon.BootModeRecovery,
+					requiredEventSets: [][]string{[]string{`System boot`, `Chrome OS Recovery Mode \| Recovery Button`}},
+					prohibitedEvents:  `Developer Mode|Sleep|FW Wake|ACPI Wake \| S3`,
+				},
+			},
+			// Test eventlog upon rec->normal reboot
+			{
+				Name:      "rec_normal",
+				Pre:       pre.RecMode(),
+				ExtraAttr: []string{"firmware_usb"},
+				Val: eventLogParams{
+					bootToMode:        fwCommon.BootModeRecovery,
+					requiredEventSets: [][]string{[]string{`System boot`, `Chrome OS Recovery Mode \| Recovery Button`}},
+					prohibitedEvents:  `Developer Mode|Sleep|FW Wake|ACPI Wake \| S3`,
+				},
+			},
+			// Test eventlog upon suspend/resume
+			{
+				Name: "suspend_resume",
+				Pre:  pre.NormalMode(),
+				Val: eventLogParams{
+					suspendResume: true,
+					requiredEventSets: [][]string{
+						[]string{`^Wake`, `Sleep`},
+						[]string{`ACPI Enter \| S3`, `ACPI Wake \| S3`},
+						[]string{`S0ix Enter`, `S0ix Exit`},
+					},
+					prohibitedEvents: `System |Developer Mode|Recovery Mode`,
+				},
+			},
+			// TODO(b/174800291): Test eventlog upon suspend/resume w/ disable_suspend_to_idle
+			// TODO(b/174800291): Test eventlog with hardware watchdog
 		},
 	})
 }
-
-var (
-	// Expected and unexpected messages upon normal-mode reboot.
-	reSystemBootMessage    = regexp.MustCompile("System boot")
-	reNotSystemBootMessage = regexp.MustCompile("Developer Mode|Recovery Mode|Sleep| Wake")
-)
 
 // eventMessagesContainReMatch returns true if any event's message matches the regexp, and doesn't match the `exceptionRe`.
 func eventMessagesContainReMatch(ctx context.Context, events []reporters.Event, re, exceptionRe *regexp.Regexp) bool {
@@ -84,32 +163,48 @@ func Eventlog(ctx context.Context, s *testing.State) {
 	r := h.Reporter
 	param := s.Param().(eventLogParams)
 
-	// Check eventlog behavior on normal boot
 	cutoffTime, err := r.Now(ctx)
 	if err != nil {
 		s.Fatal("Reporting time at start of test: ", err)
 	}
-	if err := ms.ModeAwareReboot(ctx, firmware.WarmReset); err != nil {
-		s.Fatal("Error resetting DUT: ", err)
+	if param.resetType != "" {
+		if err := ms.ModeAwareReboot(ctx, param.resetType); err != nil {
+			s.Fatal("Error resetting DUT: ", err)
+		}
+	} else if param.bootToMode != "" {
+		if err = ms.RebootToMode(ctx, param.bootToMode); err != nil {
+			s.Fatalf("Error during transition to %s: %+v", param.bootToMode, err)
+		}
+	} else if param.suspendResume {
+		if err = h.DUT.Conn().CommandContext(ctx, "powerd_dbus_suspend", "-wakeup_timeout=10").Run(ssh.DumpLogOnError); err != nil {
+			s.Fatal("Failed to suspend: ", err)
+		}
+		if err = testing.Sleep(ctx, 5*time.Second); err != nil {
+			s.Fatal("Failed to sleep: ", err)
+		}
 	}
 	events, err := r.EventlogListSince(ctx, cutoffTime)
 	if err != nil {
 		s.Fatal("Gathering events after normal reboot: ", err)
 	}
-	if !eventMessagesContainReMatch(ctx, events, reSystemBootMessage /*exceptionRe=*/, nil) {
-		s.Error("No 'System boot' event on normal boot")
+	requiredEventsFound := false
+	for _, requiredEventSet := range param.requiredEventSets {
+		for _, requiredEvent := range requiredEventSet {
+			reRequiredEvent := regexp.MustCompile(requiredEvent)
+			if eventMessagesContainReMatch(ctx, events, reRequiredEvent /*exceptionRe=*/, nil) {
+				requiredEventsFound = true
+			}
+		}
 	}
-	var exceptionRe *regexp.Regexp
-	if param.additionalNormalEvents != "" {
-		exceptionRe = regexp.MustCompile(param.additionalNormalEvents)
+	if !requiredEventsFound {
+		s.Errorf("Required event missing: %+v", events)
 	}
-	if eventMessagesContainReMatch(ctx, events, reNotSystemBootMessage, exceptionRe) {
-		s.Errorf("Incorrect event logged on normal boot: %+v", events)
+	reProhibitedEvents := regexp.MustCompile(param.prohibitedEvents)
+	var allowedRe *regexp.Regexp
+	if param.allowedEvents != "" {
+		allowedRe = regexp.MustCompile(param.allowedEvents)
 	}
-
-	// TODO(b/174800291): Test eventlog upon dev->dev reboot
-	// TODO(b/174800291): Test eventlog upon normal->rec reboot
-	// TODO(b/174800291): Test eventlog upon rec->normal reboot
-	// TODO(b/174800291): Test eventlog upon suspend/resume
-	// TODO(b/174800291): Test eventlog with hardware watchdog
+	if eventMessagesContainReMatch(ctx, events, reProhibitedEvents, allowedRe) {
+		s.Errorf("Incorrect event logged: %+v", events)
+	}
 }
