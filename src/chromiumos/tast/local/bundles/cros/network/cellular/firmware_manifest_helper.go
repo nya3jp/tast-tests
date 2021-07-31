13ed5845376ba00194f89bc0d3f462094205eeaa
Andrew Lassalle <andrewlassalle@chromium.org>
2021-07-30 17:29:05 -0700

    cellular: Add test to verify firmware_manifest.prototxt
    Validate firmware_manifest.prototxt by checking that all listed FWs
    exist, and there are no conflicts between its multiple entries.
    
    BUG=b:195303946
    TEST=tast run boten network.ModemFWManifestVerification
    
    Change-Id: Icaf3f08ff1c51d0e932fe4efe57233495f4bf203

---
+++ src/chromiumos/tast/local/bundles/cros/network/cellular/firmware_manifest_helper.go
@@ -0,0 +1,45 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"path/filepath"
+
+	"github.com/golang/protobuf/proto"
+	// The contents of chromiumos/modemfwd are built and generated in platform2/modemfwd/.
+	mfv2 "chromiumos/modemfwd"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// ParseModemFWManifest Parses the modem firmware manifest and returns the FirmwareManifestV2 proto object.
+func ParseModemFWManifest(ctx context.Context, s *testing.State) (*mfv2.FirmwareManifestV2, error) {
+	modemFirmwareProtoPath := GetModemFirmwareManifestPath()
+	output, err := testexec.CommandContext(ctx, "cat", modemFirmwareProtoPath).Output()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to access the firmware manifest: %s", modemFirmwareProtoPath)
+	}
+
+	s.Log("Parsing modem firmware proto")
+	manifest := &mfv2.FirmwareManifestV2{}
+	if err := proto.UnmarshalText(string(output), manifest); err != nil {
+		return nil, errors.Wrapf(err, "failed to parse firmware manifest: %s", modemFirmwareProtoPath)
+	}
+	s.Log("Parsed successfully")
+
+	return manifest, nil
+}
+
+// GetModemFirmwarePath Get the path where the modem firmware files are located.
+func GetModemFirmwarePath() string {
+	return "/opt/google/modemfwd-firmware/"
+}
+
+// GetModemFirmwareManifestPath Get the path of the modem firmware manifest.
+func GetModemFirmwareManifestPath() string {
+	return filepath.Join(GetModemFirmwarePath(), "firmware_manifest.prototxt")
+}
