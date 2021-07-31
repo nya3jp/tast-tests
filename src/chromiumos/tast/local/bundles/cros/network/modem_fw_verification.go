b1de7d360811882a49cb24345724adbc1cac06ca
Andrew Lassalle <andrewlassalle@chromium.org>
2021-07-30 17:29:05 -0700

    cellular: Add test to verify firmware_manifest.prototxt
    Validate firmware_manifest.prototxt by checking that all listed FWs
    exist, and can be successfully installed.
    
    BUG=b:195303946
    TEST=tast run boten network.ModemFWVerification
    
    Change-Id: Icaf3f08ff1c51d0e932fe4efe57233495f4bf203

---
+++ src/chromiumos/tast/local/bundles/cros/network/modem_fw_verification.go
@@ -0,0 +1,109 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+
+	mfv2 "chromiumos/modemfwd"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+type sortableMessage []proto.Message
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ModemFWVerification,
+		Desc:     "Verifies that all modem FWs compatible with a device are installed",
+		Contacts: []string{"andrewlassalle@google.com", "chromeos-cellular-team@google.com"},
+		Attr:     []string{"group:cellular"},
+		// Fixture:  "cellular",
+	})
+}
+
+// ModemFWVerification Test
+func ModemFWVerification(ctx context.Context, s *testing.State) {
+	const modemFirmwarePath = "/opt/google/modemfwd-firmware/"
+	modemFirmwareProtoPath := filepath.Join(modemFirmwarePath, "firmware_manifest.prototxt")
+
+	if _, err := os.Stat(modemFirmwareProtoPath); err != nil {
+		s.Log("Skipping test on device. Modem FW manifest not available")
+		return
+	}
+
+	output, err := testexec.CommandContext(ctx, "cat", modemFirmwareProtoPath).Output()
+	if err != nil {
+		// TODO:Is there a way to skip the test instead of succeed?
+		s.Fatal("Failed to access the firmware manifest: ", err)
+		return
+	}
+
+	s.Log("Parsing modem firmware proto")
+	message := &mfv2.FirmwareManifestV2{}
+	if err := proto.UnmarshalText(string(output), message); err != nil {
+		s.Fatal("Failed to parse firmware manifest: ", err)
+	}
+	s.Log("Parsed successfully")
+
+	var missingFiles []string
+	var mainFirmwares map[string]bool
+	for _, device := range message.Device {
+		// Verify that we don't have repeated main FWs.
+		mainFirmwares = make(map[string]bool)
+		for _, mainFirmware := range device.MainFirmware {
+			if mainFirmwares[mainFirmware.Version] {
+				s.Fatal("Repeated value for main firmware: ", mainFirmware.Version, " in variant:", device.Variant)
+			} else {
+				mainFirmwares[mainFirmware.Version] = true
+			}
+		}
+		// Verify that all files exist.
+		for _, mainFW := range device.MainFirmware {
+			if !checkIfFirmwareExists(filepath.Join(modemFirmwarePath, mainFW.Filename)) {
+				missingFiles = append(missingFiles, mainFW.Filename)
+			}
+		}
+		for _, oemFW := range device.OemFirmware {
+			if !checkIfFirmwareExists(filepath.Join(modemFirmwarePath, oemFW.Filename)) {
+				missingFiles = append(missingFiles, oemFW.Filename)
+			}
+			// Verify if main FWs used by OEM FW exist.
+			for _, oemMainFirmwareVersion := range oemFW.MainFirmwareVersion {
+				if !mainFirmwares[oemMainFirmwareVersion] {
+					s.Fatal("Main firmware '", oemMainFirmwareVersion, "' referenced by OEM FW '", oemFW.Version, "' does not exist.")
+				}
+			}
+		}
+		for _, carrierFW := range device.CarrierFirmware {
+			if !checkIfFirmwareExists(filepath.Join(modemFirmwarePath, carrierFW.Filename)) {
+				missingFiles = append(missingFiles, carrierFW.Filename)
+			}
+			// Verify if main FWs used by carrier FW exist.
+			if carrierFW.MainFirmwareVersion != "" && !mainFirmwares[carrierFW.MainFirmwareVersion] {
+				s.Fatal("Main firmware '", carrierFW.MainFirmwareVersion, "' referenced by carrier FW '", carrierFW.Version, "' does not exist.")
+			}
+		}
+	}
+
+	if len(missingFiles) > 0 {
+		s.Fatal("The following firmwares files are specified in the manifest, but don't exist: '", strings.Join(missingFiles, ","))
+	}
+
+	// TODO: install images
+}
+
+// checkIfFirmwareExists Return true if directory exist, false otherwise
+func checkIfFirmwareExists(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
