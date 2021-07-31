be9930ca4d59aa84f2b7821ac06996b46e579bad
Manoj Gupta <manojgupta@google.com>
2021-07-30 18:45:11 -0700

    toolchain_options.go: Add more entries to libgcc allowlist
    Noticed that a few more files in mesa built for amd64
    need allowlisting. This is to fix failures in CL:3062381.
    
    BUG=b:187794119
    TEST=CL:3062381 passed CQ
    
    Change-Id: I4d600edee0eff9828f352a49dc60b8ca0df67d22

--- src/chromiumos/tast/local/bundles/cros/security/toolchain_options.go
+++ src/chromiumos/tast/local/bundles/cros/security/toolchain_options.go
@@ -109,6 +109,9 @@ var libgccAllowlist = []string{
 	// FIXME: Remove after mesa is fixed to not need libgcc_s. crbug.com/808264
 	"/usr/lib/dri/kms_swrast_dri.so",
 	"/usr/lib/dri/swrast_dri.so",
+	"/usr/lib64/dri/kms_swrast_dri.so",
+	"/usr/lib64/dri/swrast_dri.so",
+	"/usr/lib64/dri/virtio_gpu_dri.so",
 }
 
 var libstdcAllowlist = []string{
