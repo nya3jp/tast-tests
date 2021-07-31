683c42b72953481cb4dabad180865049930febeb
Miguel Casas <mcasas@chromium.org>
2021-07-30 19:22:56 -0700

    webrtc: add |enableHWEncoding| param to in RTCPeerConnectionPerf
    This CL adds a |enableHWEncoding| to RTCPeerConnectionPerf.
    It also extends the test cases' needed capabilities where
    applicable -- note that the existing "hw" imply both hw encoding
    and decoding, so this CL makes that explicit (other later CLs
    might add mixed hw/sw test cases).
    
    BUG=b:194895782
    TEST=tast run <volteer> webrtc.RTCPeerConnectionPerf.*
    
    Change-Id: I8d9762f04c3e4cb70fec3295204f42641c2fa846

--- src/chromiumos/tast/local/bundles/cros/webrtc/rtc_peer_connection_perf.go
+++ src/chromiumos/tast/local/bundles/cros/webrtc/rtc_peer_connection_perf.go
@@ -18,6 +18,7 @@ import (
 // rtcPerfTest is used to describe the config used to run each test case.
 type rtcPerfTest struct {
 	enableHWDecoding   bool   // Instruct to use hardware or software decoding.
+	enableHWEncoding   bool   // Instruct to use hardware or software encoding.
 	profile            string // Codec to try, e.g. VP8, VP9.
 	videoGridDimension int    // Dimension of the grid in which to embed the RTCPeerConnection <video>.
 	videoGridFile      string // Name of the video file to fill up the grid with, if needed.
@@ -37,71 +38,71 @@ func init() {
 		// TODO(crbug.com/1029548): Add more variations here, e.g. vp8.
 		Params: []testing.Param{{
 			Name:              "h264_hw",
-			Val:               rtcPerfTest{enableHWDecoding: true, profile: "H264"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeH264, "proprietary_codecs"},
+			Val:               rtcPerfTest{enableHWDecoding: true, enableHWEncoding: true, profile: "H264"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, caps.HWEncodeH264, "proprietary_codecs"},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "h264_sw",
-			Val:               rtcPerfTest{enableHWDecoding: false, profile: "H264"},
+			Val:               rtcPerfTest{profile: "H264"},
 			ExtraSoftwareDeps: []string{"proprietary_codecs"},
 			Fixture:           "chromeVideoWithFakeWebcamAndSWDecoding",
 		}, {
 			Name:              "vp8_hw",
-			Val:               rtcPerfTest{enableHWDecoding: true, profile: "VP8"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
+			Val:               rtcPerfTest{enableHWDecoding: true, enableHWEncoding: true, profile: "VP8"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8, caps.HWEncodeVP8},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:    "vp8_sw",
-			Val:     rtcPerfTest{enableHWDecoding: false, profile: "VP8"},
+			Val:     rtcPerfTest{profile: "VP8"},
 			Fixture: "chromeVideoWithFakeWebcamAndSWDecoding",
 		}, {
 			Name:              "vp9_hw",
-			Val:               rtcPerfTest{enableHWDecoding: true, profile: "VP9"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
+			Val:               rtcPerfTest{enableHWDecoding: true, enableHWEncoding: true, profile: "VP9"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, caps.HWEncodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:    "vp9_sw",
-			Val:     rtcPerfTest{enableHWDecoding: false, profile: "VP9"},
+			Val:     rtcPerfTest{profile: "VP9"},
 			Fixture: "chromeVideoWithFakeWebcamAndSWDecoding",
 		}, {
 			// This is a 3 temporal layers test.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_hw_force_l1t3",
-			Val:               rtcPerfTest{enableHWDecoding: true, profile: "VP9"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
+			Val:               rtcPerfTest{enableHWDecoding: true, enableHWEncoding: true, profile: "VP9"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, caps.HWEncodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcamAndForceVP9ThreeTemporalLayers",
 		}, {
 			// This is 3 spatial layers, 3 temporal layers (each) k-SVC.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_hw_force_l3t3_key",
-			Val:               rtcPerfTest{enableHWDecoding: true, profile: "VP9"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
+			Val:               rtcPerfTest{enableHWDecoding: true, enableHWEncoding: true, profile: "VP9"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, caps.HWEncodeVP9},
 			ExtraHardwareDeps: hwdep.D(hwdep.Platform("volteer", "dedede")),
 			Fixture:           "chromeVideoWithFakeWebcamAndForceVP9SVC3SL3TLKey",
 		}, {
 			// This is a 2 temporal layers test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_hw_svc_l1t2",
-			Val:               rtcPerfTest{enableHWDecoding: true, profile: "VP9", svc: "L1T2"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
+			Val:               rtcPerfTest{enableHWDecoding: true, enableHWEncoding: true, profile: "VP9", svc: "L1T2"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, caps.HWEncodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabled",
 		}, {
 			// This is a 3 temporal layers test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_hw_svc_l1t3",
-			Val:               rtcPerfTest{enableHWDecoding: true, profile: "VP9", svc: "L1T3"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
+			Val:               rtcPerfTest{enableHWDecoding: true, enableHWEncoding: true, profile: "VP9", svc: "L1T3"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, caps.HWEncodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabled",
 		}, {
 			Name:              "vp8_hw_multi_vp9_3x3",
-			Val:               rtcPerfTest{enableHWDecoding: true, profile: "VP8", videoGridDimension: 3, videoGridFile: "tulip2-320x180.vp9.webm"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
+			Val:               rtcPerfTest{enableHWDecoding: true, enableHWEncoding: true, profile: "VP8", videoGridDimension: 3, videoGridFile: "tulip2-320x180.vp9.webm"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, caps.HWDecodeVP8, caps.HWEncodeVP8},
 			ExtraData:         []string{"tulip2-320x180.vp9.webm"},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "vp8_hw_multi_vp9_4x4",
-			Val:               rtcPerfTest{enableHWDecoding: true, profile: "VP8", videoGridDimension: 4, videoGridFile: "tulip2-320x180.vp9.webm"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
+			Val:               rtcPerfTest{enableHWDecoding: true, enableHWEncoding: true, profile: "VP8", videoGridDimension: 4, videoGridFile: "tulip2-320x180.vp9.webm"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, caps.HWDecodeVP8, caps.HWEncodeVP8},
 			ExtraData:         []string{"tulip2-320x180.vp9.webm"},
 			Fixture:           "chromeVideoWithFakeWebcam",
 			// Trogdor doesn't have enough hardware contexts to pass this test.
@@ -115,7 +116,7 @@ func init() {
 // RTCPeerConnectionPerf opens a WebRTC loopback page that loops a given capture stream to measure decode time and CPU usage.
 func RTCPeerConnectionPerf(ctx context.Context, s *testing.State) {
 	testOpt := s.Param().(rtcPerfTest)
-	if err := peerconnection.RunDecodePerf(ctx, s.FixtValue().(*chrome.Chrome), s.DataFileSystem(), s.OutDir(), testOpt.profile, testOpt.enableHWDecoding, testOpt.videoGridDimension, testOpt.videoGridFile, testOpt.svc); err != nil {
+	if err := peerconnection.RunDecodePerf(ctx, s.FixtValue().(*chrome.Chrome), s.DataFileSystem(), s.OutDir(), testOpt.profile, testOpt.enableHWDecoding, testOpt.enableHWEncoding, testOpt.videoGridDimension, testOpt.videoGridFile, testOpt.svc); err != nil {
 		s.Error("Failed to measure performance: ", err)
 	}
 }
