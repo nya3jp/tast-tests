35678644747325c61c2d2ad17c850baacbbc06d3
Alvin Jia <alvinjia@google.com>
2021-08-01 22:32:08 +1000

    Generalize inputs validation based on modality
    Create methods for validating inputs based on modality: VK, handwriting a
    nd voice. These methods can be use for different input environments,
    as the underlying input-then-validate logic can be reused.
    
    BUG=b:195223184
    TEST=None
    
    Change-Id: I6e107a2a330ef68a6d7eb466101d835505ab85f7

--- src/chromiumos/tast/local/chrome/uiauto/vkb/vkb.go
+++ src/chromiumos/tast/local/chrome/uiauto/vkb/vkb.go
@@ -363,6 +363,19 @@ func (vkbCtx *VirtualKeyboardContext) ClickUntilVKShown(nodeFinder *nodewith.Fin
 	return uiauto.Retry(5, ac.LeftClickUntil(nodeFinder, vkbCtx.WaitLocationStable()))
 }
 
+// SwitchToKeyboard returns an action changing to keyboard layout.
+// TODO(b/195366402): Use test api for switching to keyboard/handwriting mode for VK.
+func (vkbCtx *VirtualKeyboardContext) SwitchToKeyboard() uiauto.Action {
+	showAccessPointsBtn := KeyFinder.Name("Show access points")
+	return uiauto.Combine("switch back to keyboard",
+		vkbCtx.ui.IfSuccessThen(
+			vkbCtx.ui.WithTimeout(500*time.Millisecond).WaitUntilExists(showAccessPointsBtn),
+			vkbCtx.ui.LeftClick(showAccessPointsBtn),
+		),
+		vkbCtx.ui.LeftClick(KeyFinder.Name("Back")),
+	)
+}
+
 // SwitchToVoiceInput returns an action changing virtual keyboard to voice input layout.
 func (vkbCtx *VirtualKeyboardContext) SwitchToVoiceInput() uiauto.Action {
 	// Call background API to switch.
