75ae3f0f0b8d63ba595b2d64e13f17476dcd170b
Madhav <madhavadas@google.com>
2021-08-01 08:11:43 +0000

    Cellular: Update fixture to stop/start modemmanager and shill
    Update SetUp to follow the initialization sequence
    in go/cellular-tast-test-design. Please check the InitDUT
    sequence.
    
    BUG=None
    TEST=tast -verbose run 192.168.86.30 '("group:cellular")'
    
    Change-Id: I39c0581953a620fdd99d38f0792644e9f85654e3

--- src/chromiumos/tast/local/bundles/cros/network/cellular/fixture.go
+++ src/chromiumos/tast/local/bundles/cros/network/cellular/fixture.go
@@ -6,14 +6,18 @@ package cellular
 
 import (
 	"context"
+	"os"
 	"time"
 
+	"chromiumos/tast/common/shillconst"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/shill"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
 
 // The Cellular test fixture ensures that modemfwd is stopped.
+const resetShillTimeout = 30 * time.Second
 
 func init() {
 	testing.AddFixture(&testing.Fixture{
@@ -42,6 +46,43 @@ func newCellularFixture() testing.FixtureImpl {
 }
 
 const modemfwdJobName = "modemfwd"
+const shillJobName = "shill"
+const shillLogLevel = "-4"
+const shillLogScope = "cellular+modem+connection+profile+portal"
+const modemmanagerJobName = "modemmanager"
+const modemmanagerLogLevel = "DEBUG"
+
+func resetShill(ctx context.Context) []error {
+	var errs []error
+	if err := upstart.StopJob(ctx, shillJobName); err != nil {
+		errs = append(errs, errors.Wrap(err, "failed to stop shill"))
+	}
+	if err := os.Remove(shillconst.DefaultProfilePath); err != nil && !os.IsNotExist(err) {
+		errs = append(errs, errors.Wrap(err, "failed to remove default profile"))
+	}
+	if err := upstart.RestartJob(ctx, shillJobName, upstart.WithArg("SHILL_LOG_LEVEL", shillLogLevel), upstart.WithArg("SHILL_LOG_SCOPES", shillLogScope)); err != nil {
+		// No more can be done if shill doesn't start
+		return append(errs, errors.Wrap(err, "failed to restart shill"))
+	}
+	manager, err := shill.NewManager(ctx)
+	if err != nil {
+		// No more can be done if a manger interface cannot be created
+		return append(errs, errors.Wrap(err, "failed to create new shill manager"))
+	}
+	if err = manager.PopAllUserProfiles(ctx); err != nil {
+		errs = append(errs, errors.Wrap(err, "failed to pop all user profiles"))
+	}
+
+	// Wait until a service is connected.
+	expectProps := map[string]interface{}{
+		shillconst.ServicePropertyIsConnected: true,
+	}
+	if _, err := manager.WaitForServiceProperties(ctx, expectProps, resetShillTimeout); err != nil {
+		errs = append(errs, errors.Wrap(err, "failed to wait for connected service"))
+	}
+
+	return errs
+}
 
 func (f *cellularFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
 	stopped, err := stopJob(ctx, modemfwdJobName)
@@ -54,6 +95,21 @@ func (f *cellularFixture) SetUp(ctx context.Context, s *testing.FixtState) inter
 		s.Logf("%q not running", modemfwdJobName)
 	}
 	f.modemfwdStopped = stopped
+
+	if err := upstart.StopJob(ctx, modemmanagerJobName); err != nil {
+		s.Fatalf("Failed to stop job: %q, %s", modemmanagerJobName, err)
+	}
+
+	if errs := resetShill(ctx); len(errs) != 0 {
+		for _, err := range errs {
+			s.Error("resetShill error: ", err)
+		}
+		s.Fatal("Failed resetting shill in PreTest")
+	}
+
+	if err := upstart.StartJob(ctx, modemmanagerJobName, upstart.WithArg("MM_LOGLEVEL", modemmanagerLogLevel)); err != nil {
+		s.Fatalf("Failed to start job: %q, %s", "modemmanager", err)
+	}
 	return nil
 }
 
