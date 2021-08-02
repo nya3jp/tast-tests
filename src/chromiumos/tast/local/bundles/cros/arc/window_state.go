43736c665080a759abd5f76e8b5e92fe68e7037c
Toshiki Kikuchi <toshikikikuchi@google.com>
2021-08-02 11:54:40 +0900

    tast-test: Re-enable freeform test tablet mode for R
    This CL brings back the freeform test in tablet mode.
    The test was disabled for R because of the Activity.setWindowStateR issue, but the issue was fixed by (https://crrev.com/c/3061864), so we can enable it again.
    
    BUG=b:179525662
    TEST=arc.WindowState.tablet_vm
    
    Change-Id: Ieb57b279f095a093132135456a550f1a161f220b

--- src/chromiumos/tast/local/bundles/cros/arc/window_state.go
+++ src/chromiumos/tast/local/bundles/cros/arc/window_state.go
@@ -47,13 +47,8 @@ var clamshellWindowStateTests = []windowStateTest{
 var tabletWindowStateTests = []windowStateTest{
 	{"MAXIMIZE <--> FULLSCREEN", arc.WindowStateMaximized, arc.WindowStateMaximized, ash.WindowStateMaximized, arc.WindowStateFullscreen, arc.WindowStateFullscreen, ash.WindowStateFullscreen},
 	{"MAXIMIZE <--> MINIMIZE", arc.WindowStateMaximized, arc.WindowStateMaximized, ash.WindowStateMaximized, arc.WindowStateMinimized, arc.WindowStateMinimized, ash.WindowStateMinimized},
+	{"MAXIMIZE <--> NORMAL", arc.WindowStateMaximized, arc.WindowStateMaximized, ash.WindowStateMaximized, arc.WindowStateNormal, arc.WindowStateMaximized, ash.WindowStateMaximized},
 	{"FULLSCREEN <--> MINIMIZE", arc.WindowStateFullscreen, arc.WindowStateFullscreen, ash.WindowStateFullscreen, arc.WindowStateMinimized, arc.WindowStateMinimized, ash.WindowStateMinimized},
-}
-
-// tabletWindowStateTestsForP contains list of tablet mode test cases that are only for P.
-// In R or later, freeform mode in tablet is no longer supported.
-var tabletWindowStateTestsForP = []windowStateTest{
-	{"MAXIMIZE <--> NORMAL", arc.WindowStateMaximized, arc.WindowStateMaximized, ash.WindowStateMaximized, arc.WindowStateMaximized, arc.WindowStateMaximized, ash.WindowStateMaximized},
 	{"FULLSCREEN <--> NORMAL", arc.WindowStateFullscreen, arc.WindowStateFullscreen, ash.WindowStateFullscreen, arc.WindowStateNormal, arc.WindowStateMaximized, ash.WindowStateMaximized},
 	{"NORMAL <--> MINIMIZE", arc.WindowStateNormal, arc.WindowStateMaximized, ash.WindowStateMaximized, arc.WindowStateMinimized, arc.WindowStateMinimized, ash.WindowStateMinimized},
 }
@@ -104,7 +99,7 @@ func init() {
 			Val: windowStateParams{
 				true, // Tablet Mode.
 				1,    // Num test iterations.
-				append(tabletWindowStateTests, tabletWindowStateTestsForP...),
+				tabletWindowStateTests,
 			},
 			ExtraSoftwareDeps: []string{"android_p"},
 		}, {
@@ -120,7 +115,7 @@ func init() {
 			Val: windowStateParams{
 				true, // Tablet Mode.
 				25,   // Num test iterations.
-				append(tabletWindowStateTests, tabletWindowStateTestsForP...),
+				tabletWindowStateTests,
 			},
 			ExtraSoftwareDeps: []string{"android_p"},
 		}, {
