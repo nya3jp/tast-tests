fceaf0a2bf2c2ca7db4c4600440d4ba00b396e8b
Chung-Sheng Wu <chungsheng@google.com>
2021-08-02 15:05:22 +0800

    health: Add osInfo validation
    BUG=b:194448979
    TEST=tast run $IP health.ProbeSystemInfoV2
    
    Change-Id: I9c9d37cc4c261137985dd56c191573515d114cd8

--- src/chromiumos/tast/local/bundles/cros/health/probe_system_info_v2.go
+++ src/chromiumos/tast/local/bundles/cros/health/probe_system_info_v2.go
@@ -6,8 +6,11 @@ package health
 
 import (
 	"context"
+	"strings"
 
+	"chromiumos/tast/local/bundles/cros/health/utils"
 	"chromiumos/tast/local/croshealthd"
+	"chromiumos/tast/lsbrelease"
 	"chromiumos/tast/testing"
 )
 
@@ -67,10 +70,66 @@ func init() {
 	})
 }
 
+func (info *osVersion) validate(ctx context.Context, s *testing.State) {
+	lsb, err := lsbrelease.Load()
+	if err != nil {
+		s.Fatal("Failed to get lsb-release info: ", err)
+	}
+	if err := utils.CompareString(lsb[lsbrelease.Milestone], info.ReleaseMilestone); err != nil {
+		s.Error("ReleaseMilestone: ", err)
+	}
+	if err := utils.CompareString(lsb[lsbrelease.BuildNumber], info.BuildNumber); err != nil {
+		s.Error("BuildNumber: ", err)
+	}
+	if err := utils.CompareString(lsb[lsbrelease.PatchNumber], info.PatchNumber); err != nil {
+		s.Error("PatchNumber: ", err)
+	}
+	if err := utils.CompareString(lsb[lsbrelease.ReleaseTrack], info.ReleaseChannel); err != nil {
+		s.Error("ReleaseChannel: ", err)
+	}
+}
+
+func (info *osInfo) validate(ctx context.Context, s *testing.State) {
+	const (
+		cfgCodeName      = "/name"
+		cfgMarketingName = "/arc/build-properties/marketing-name"
+	)
+	v, err := utils.GetCrosConfig(ctx, cfgCodeName)
+	if err != nil {
+		s.Fatal("Failed to get cros config: ", err)
+	}
+	if err := utils.CompareStringPtr(v, &info.CodeName); err != nil {
+		s.Error("CodeName: ", err)
+	}
+
+	v, err = utils.GetCrosConfig(ctx, cfgMarketingName)
+	if err != nil {
+		s.Fatal("Failed to get cros config: ", err)
+	}
+	if err := utils.CompareStringPtr(v, info.MarketingName); err != nil {
+		s.Error("MarketingName: ", err)
+	}
+
+	info.OsVersion.validate(ctx, s)
+
+	v, err = utils.ReadFile("/proc/cmdline")
+	if err != nil {
+		s.Fatal("Failed to read file: ", err)
+	}
+	if info.BootMode == "Unknown" || v == nil || !strings.Contains(*v, info.BootMode) {
+		s.Errorf("BootMode not in /proc/cmdline, got: %v, /proc/cmdline: %v", info.BootMode, utils.PtrToStr(v))
+	}
+}
+
+func (info *systemInfo) validate(ctx context.Context, s *testing.State) {
+	info.OsInfo.validate(ctx, s)
+}
+
 func ProbeSystemInfoV2(ctx context.Context, s *testing.State) {
 	params := croshealthd.TelemParams{Category: croshealthd.TelemCategorySystem2}
 	var info systemInfo
 	if err := croshealthd.RunAndParseJSONTelem(ctx, params, s.OutDir(), &info); err != nil {
 		s.Fatal("Failed to get storage telemetry info: ", err)
 	}
+	info.validate(ctx, s)
 }
