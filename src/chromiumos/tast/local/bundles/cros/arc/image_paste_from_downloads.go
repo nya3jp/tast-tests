71399672c2959dd820d559988ed1be018f0ad821
Tetsui Ohkubo <tetsui@chromium.org>
2021-08-04 15:36:52 +0900

    tast-tests: Add test for image copy & paste from Downloads
    This CL adds arc.ImagePasteFromDownloads, which tests a CUJ of image
    copy & paste app compat.
    
    arc.ImagePaste tests copy & paste from browser, while this one tests
    copy & paste from Files app (the Downloads directory in particular).
    
    UX one pager: go/arc-image-cp-app-compat-onepager
    design proposal: go/arc-image-cp-app-compat-design-proposal
    
    BUG=b:195379894
    TEST=tast run hatch-arc-r arc.ImagePasteFromDownloads
    
    Change-Id: I944da3c7ee16f6f65a3eca34637f397bb3edecb7

---
+++ src/chromiumos/tast/local/bundles/cros/arc/image_paste_from_downloads.go
@@ -0,0 +1,119 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"io/ioutil"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ImagePasteFromDownloads,
+		Desc:         "Checks image copy paste app compat from Files App",
+		Contacts:     []string{"tetsui@chromium.org", "arc-framework+tast@google.com"},
+		SoftwareDeps: []string{"chrome", "android_vm"},
+		Fixture:      "arcBooted",
+		Attr:         []string{"group:mainline", "informational"},
+		Data:         []string{"capybara.jpg"},
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func ImagePasteFromDownloads(ctx context.Context, s *testing.State) {
+	a := s.FixtValue().(*arc.PreData).ARC
+	cr := s.FixtValue().(*arc.PreData).Chrome
+
+	const (
+		filename = "capybara.jpg"
+		crosPath = "/home/chronos/user/Downloads/" + filename
+	)
+
+	expected, err := ioutil.ReadFile(s.DataPath(filename))
+	if err != nil {
+		s.Fatal("Could not read the test file: ", err)
+	}
+
+	if err = ioutil.WriteFile(crosPath, expected, 0666); err != nil {
+		s.Fatalf("Could not write to %s: %v", crosPath, err)
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed creating test API connection: ", err)
+	}
+
+	keyboard, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard")
+	}
+	defer keyboard.Close()
+
+	files, err := filesapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed launching the Files App: ", err)
+	}
+	if err := uiauto.Combine("copy capybara.jpg from Downloads",
+		files.OpenDownloads(),
+		files.SelectFile(filename),
+		keyboard.AccelAction("Ctrl+C"),
+	)(ctx); err != nil {
+		s.Fatal("Failed to open the Downloads directory: ", err)
+	}
+
+	const (
+		apk          = "ArcImagePasteTest.apk"
+		pkg          = "org.chromium.arc.testapp.imagepaste"
+		activityName = ".MainActivity"
+		fieldID      = pkg + ":id/input_field"
+		counterID    = pkg + ":id/counter"
+	)
+
+	d, err := a.NewUIDevice(ctx)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	defer d.Close(ctx)
+
+	if err := a.Install(ctx, arc.APKPath(apk)); err != nil {
+		s.Fatal("Failed to install the app: ", err)
+	}
+	act, err := arc.NewActivity(a, pkg, activityName)
+	if err != nil {
+		s.Fatalf("Failed to create a new activity %q", activityName)
+	}
+	defer act.Close()
+	if err := act.Start(ctx, tconn); err != nil {
+		s.Fatalf("Failed to start the activity %q", activityName)
+	}
+	defer act.Stop(ctx, tconn)
+
+	// Focus the input field and paste the image.
+	if err := d.Object(ui.ID(fieldID)).WaitForExists(ctx, 30*time.Second); err != nil {
+		s.Fatal("Failed to find the input field: ", err)
+	}
+	if err := d.Object(ui.ID(fieldID)).Click(ctx); err != nil {
+		s.Fatal("Failed to click the input field: ", err)
+	}
+	if err := d.Object(ui.ID(fieldID), ui.Focused(true)).WaitForExists(ctx, 30*time.Second); err != nil {
+		s.Fatal("Failed to focus on the input field: ", err)
+	}
+	if err := keyboard.Accel(ctx, "Ctrl+V"); err != nil {
+		s.Fatal("Failed to press Ctrl+V: ", err)
+	}
+
+	// Verify the image is pasted successfully by checking the counter.
+	if err := d.Object(ui.ID(counterID)).WaitForText(ctx, "1", 30*time.Second); err != nil {
+		s.Fatal("Failed to paste the image: ", err)
+	}
+}
