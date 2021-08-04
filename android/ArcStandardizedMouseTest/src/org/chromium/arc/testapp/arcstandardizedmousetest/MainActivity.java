df0b4046c88049a34801e5b85b9df91a1704479b
David Welling <davidwelling@google.com>
2021-08-04 21:51:03 +0000

    arcappcompat: Add standardized mouse left click test
    Adds an application for testing standardized mouse input and adds a
    mouse left click test for clamshell models.
    
    BUG=b:194900373
    TEST=push apk && tast run ${HOST} arc.StandardizedMouse*
    
    Change-Id: I38c5d44362de567ce6278c985f52f6b0010d8787

---
+++ android/ArcStandardizedMouseTest/src/org/chromium/arc/testapp/arcstandardizedmousetest/MainActivity.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.arcstandardizedmousetest;
+
+import android.app.Activity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+  private LinearLayout layoutMain;
+  private Button btnLeftClick;
+  private Integer btnLeftClickCounter = 1;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    this.layoutMain = findViewById(R.id.layoutMain);
+
+    // Add the text 'Mouse Left Click' when the left click button is pressed.
+    // Always add the click counter so the tast test can make sure a single click
+    // doesn't fire two events.
+    this.btnLeftClick = findViewById(R.id.btnLeftClick);
+    this.btnLeftClick.setOnClickListener((v) -> {
+      TextView el = new TextView(this);
+      el.setText(String.format("MOUSE LEFT CLICK (%d)", this.btnLeftClickCounter));
+      this.btnLeftClickCounter++;
+      this.layoutMain.addView(el);
+    });
+  }
+}
---
+++ src/chromiumos/tast/local/bundles/cros/arc/standardized_mouse.go
@@ -0,0 +1,81 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/bundles/cros/arc/standardizedtestutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         StandardizedMouse,
+		Desc:         "Functional test that installs an app and tests standard mouse input functionality. Tests are only performed in clamshell mode as tablets don't allow mice",
+		Contacts:     []string{"davidwelling@google.com", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      10 * time.Minute,
+		Params: []testing.Param{{
+			Val:               standardizedtestutil.GetStandardizedClamshellTests(runStandardizedMouseTests),
+			ExtraSoftwareDeps: []string{"android_p"},
+			Fixture:           "arcBooted",
+			ExtraHardwareDeps: standardizedtestutil.GetStandardizedClamshellHardwareDeps(),
+		}, {
+			Name:              "vm",
+			Val:               standardizedtestutil.GetStandardizedClamshellTests(runStandardizedMouseTests),
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Fixture:           "arcBooted",
+			ExtraHardwareDeps: standardizedtestutil.GetStandardizedClamshellHardwareDeps(),
+		}},
+	})
+}
+
+// StandardizedMouse runs all the provided test cases.
+func StandardizedMouse(ctx context.Context, s *testing.State) {
+	const (
+		apkName      = "ArcStandardizedMouseTest.apk"
+		appName      = "org.chromium.arc.testapp.arcstandardizedmousetest"
+		activityName = ".MainActivity"
+	)
+
+	testCases := s.Param().([]standardizedtestutil.StandardizedTestCase)
+	standardizedtestutil.RunStandardizedTestCases(ctx, s, apkName, appName, activityName, testCases)
+}
+
+// runStandardizedMouseTests runs all the tests that are part of the
+// standardized mouse input suite.
+func runStandardizedMouseTests(ctx context.Context, s *testing.State, testParameters standardizedtestutil.StandardizedTestFuncParams) {
+	runLeftClickTest(ctx, s, testParameters)
+}
+
+// runLeftClickTest ensures that the left mouse click works.
+func runLeftClickTest(ctx context.Context, s *testing.State, testParameters standardizedtestutil.StandardizedTestFuncParams) {
+	btnLeftClickID := testParameters.AppPkgName + ":id/btnLeftClick"
+	btnLeftClickSelector := testParameters.Device.Object(ui.ID(btnLeftClickID))
+
+	if err := btnLeftClickSelector.WaitForExists(ctx, standardizedtestutil.ShortUITimeout); err != nil {
+		s.Fatal("Unable to find the button to click, info: ", err)
+	}
+
+	if err := testParameters.Device.Object(ui.Text("MOUSE LEFT CLICK (1)")).WaitUntilGone(ctx, standardizedtestutil.ShortUITimeout); err != nil {
+		s.Fatal("The success label should not yet exist, info: ", err)
+	}
+
+	if err := standardizedtestutil.StandardizedMouseClickObject(ctx, testParameters.TestConn, btnLeftClickSelector, standardizedtestutil.LeftMouseButton); err != nil {
+		s.Fatal("Unable to click the button, info: ", err)
+	}
+
+	if err := testParameters.Device.Object(ui.Text("MOUSE LEFT CLICK (1)")).WaitForExists(ctx, standardizedtestutil.ShortUITimeout); err != nil {
+		s.Fatal("The success label should exist, info: ", err)
+	}
+
+	if err := testParameters.Device.Object(ui.Text("MOUSE LEFT CLICK (2)")).WaitUntilGone(ctx, standardizedtestutil.ShortUITimeout); err != nil {
+		s.Fatal("A single mouse click triggered two click events, info: ", err)
+	}
+}
