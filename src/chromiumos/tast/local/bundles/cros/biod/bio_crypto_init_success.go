02f42b5c1a05b1d0bee20ef422f02b2746c9355f
Josie Nordrum <JosieNordrum@google.com>
2021-08-06 13:03:16 -0600

    firmware: add bio_crypto_init smoke test
    Add test to check that bio_crypto_init finished with no violations and
    successfully set FPTPM seed. Add function to get audit logs since boot.
    Check that the string "bio_crypto_init" is not found in recent audit
    logs or in upstart logs.
    
    BUG=b:150468438
    TEST=tast -verbose run -ephemeraldevserver=false <IP> biod.BioCryptoInitSuccess
    	trigger bio_crypto_init error: delete "clone: 1" from
    		/usr/share/policy/bio-crypto-init-policy
    		sync
    	then generate fake TPM seed
    		SEED="/run/bio_crypto_init/seed"
    		dd if=/dev/urandom of="${SEED}" bs=32 count=1
    		chown biod:biod "${SEED}"
    	then run `start bio_crypto_init`
    	1. test should FAIL with violation in audit logs
    	next reboot computer
    	2. test should FAIL with violation in upstart logs
    	then re-add clone policy, sync, and reboot
    	3. test should PASS
    	last, generate fake TPM seed and start bio_crypto_init
    	4. test should PASS (upstart logs will have non-warning
    	   bio_crypto_init logs)
    
    Change-Id: I2fe3cc332d92ea903f36d352f28b59fa0e83a8f2

---
+++ src/chromiumos/tast/local/bundles/cros/biod/bio_crypto_init_success.go
@@ -0,0 +1,69 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package biod
+
+import (
+	"context"
+	"regexp"
+	"strings"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/local/syslog"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: BioCryptoInitSuccess,
+		Desc: "Checks that bio crypto init finishes gracefully without violations and that FPMCU seed is set",
+		Contacts: []string{
+			"josienordrum@google.org", // Test Author
+			"hesling@chromium.org",
+			"chromeos-fingerprint@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"biometrics_daemon"},
+		HardwareDeps: hwdep.D(hwdep.Fingerprint()),
+	})
+}
+
+const (
+	seedSuccessString = "FPTPM_seed_set"
+	violation         = "bio_crypto_init"
+)
+
+func BioCryptoInitSuccess(ctx context.Context, s *testing.State) {
+	auditLogs, err := syslog.GetAuditLogsSinceBoot(ctx)
+	if err != nil {
+		s.Fatal("Failed to get recent audit logs: ", err)
+	}
+	for _, l := range auditLogs {
+		if strings.Contains(l, violation) {
+			s.Fatal("bio_crypto_init warning found in audit logs, check syscall https://chromium.googlesource.com/chromiumos/docs/+/HEAD/constants/syscalls.md", l)
+		}
+	}
+
+	upstartlogs, err := syslog.GetUpstartLogsSinceBoot(ctx)
+	if err != nil {
+		s.Fatal("Failed to get recent upstart logs: ", err)
+	}
+	rx := regexp.MustCompile(`WARNING kernel: [ [0-9]*\.[0-9]*] init: bio_crypto_init`)
+	for _, l := range upstartlogs {
+		if rx.Find([]byte(l)) != nil {
+			s.Fatal("bio_crypto_init string found in upstart.log", l)
+		}
+	}
+
+	cmd := testexec.CommandContext(ctx, "ectool", "--name=cros_fp", "fpencstatus")
+	out, err := cmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to get FP Encryption Status: ", err)
+	}
+	if !strings.Contains(string(out), seedSuccessString) {
+		s.Fatal("FPTPM seet not set")
+	}
+
+}
