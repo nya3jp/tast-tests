604a82c984264fe244897fb86ef6d6fc932c875b
George Popoola <gpopoola@google.com>
2021-08-06 17:25:08 +0000

    Call updateCheck function in firmware.FwupdInstallRemote
    Calls an function that checks for available updates and runs the correct
    update command based on that. This is to bypass the need to restart
    fwupd after every test update.
    
    BUG=b:195759844
    TEST=tast run [user@]host[:port] firmware.FwupdInstallRemote
    
    Change-Id: Ia2ce30c3b6284e6176dc434858c3125665eb55ec

--- src/chromiumos/tast/local/bundles/cros/firmware/fwupd_install_remote.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_install_remote.go
@@ -14,7 +14,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func: FwupdInstallRemote,
-		Desc: "Checks that fwupd can install using a remote repository",
+		Desc: "Checks that fwcmd can install using a remote repository",
 		Contacts: []string{
 			"campello@chromium.org",     // Test Author
 			"chromeos-fwupd@google.com", // CrOS FWUPD
@@ -28,8 +28,21 @@ func init() {
 // can update a device in the system using a remote repository.
 func FwupdInstallRemote(ctx context.Context, s *testing.State) {
 	// b585990a-003e-5270-89d5-3705a17f9a43 is the GUID for a fake device.
-	cmd := testexec.CommandContext(ctx, "/usr/bin/fwupdmgr", "update", "-v", "b585990a-003e-5270-89d5-3705a17f9a43", "--ignore-power")
-	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
-		s.Fatalf("%q failed: %v", cmd.Args, err)
+	updateChecker(ctx)
+
+	var cmd *testexec.Cmd
+
+	if updtype := updateChecker(ctx); updtype == "update" {
+		cmd = testexec.CommandContext(ctx, "/usr/bin/fwupdmgr", "update", "-v", "b585990a-003e-5270-89d5-3705a17f9a43", "--ignore-power")
+		err := cmd.Run(testexec.DumpLogOnError)
+		if err != nil {
+			s.Fatalf("%s failed: %v", cmd.Args, err)
+		}
+	} else {
+		cmd = testexec.CommandContext(ctx, "/usr/bin/fwupdmgr", "reinstall", "-v", "b585990a-003e-5270-89d5-3705a17f9a43", "--ignore-power")
+		err := cmd.Run(testexec.DumpLogOnError)
+		if err != nil {
+			s.Fatalf("%s failed: %v", cmd.Args, err)
+		}
 	}
 }
