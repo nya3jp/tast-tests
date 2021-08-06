9ae3aadd90dc6bd08fdbc9f79a85cf1bf63e7f72
Josie Nordrum <JosieNordrum@google.com>
2021-08-06 13:03:16 -0600

    firmware: add bio_crypto_init smoke test
    Add test to check that bio_crypto_init finished with no violations and
    successfully set FPTPM seed. Add function to get audit logs since boot.
    Check that the string "bio_crypto_init" is not found in recent audit
    logs or in upstart logs.
    
    BUG=b:150468438
    TEST=tast -verboser run -ephemeraldevserver=false <IP> firmware.FpBioCryptoInitAudit
    
    Change-Id: I2fe3cc332d92ea903f36d352f28b59fa0e83a8f2

---
+++ src/chromiumos/tast/local/firmware/fp_read_logs_since_boot.go
@@ -0,0 +1,60 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+)
+
+const (
+	auditLogPath = "/var/log/audit/audit.log"
+)
+
+var rx = regexp.MustCompile(`msg=audit\([0-9]*\.[0-9]*`)
+
+// GetAuditLogsSinceBoot returns all audit logs since last boot.
+func GetAuditLogsSinceBoot(ctx context.Context) ([]string, error) {
+	// Get timestamp of last boot using uptime.
+	cmd := testexec.CommandContext(ctx, "uptime", "-s")
+	out, err := cmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		return make([]string, 0), errors.Wrap(err, "failed to get boot time")
+	}
+	bootTime, err := time.Parse("2006-01-02 15:04:05\n", string(out))
+	if err != nil {
+		return make([]string, 0), errors.Wrap(err, "failed to parse boot time")
+	}
+	auditLogs, err := ioutil.ReadFile(auditLogPath)
+	if err != nil {
+		return make([]string, 0), errors.Wrap(err, "failed to read audit log")
+	}
+
+	lines := strings.Split(string(auditLogs), "\n")
+	for i, l := range lines {
+		regMatch := string(rx.Find([]byte(l)))
+		unixTime := strings.TrimLeft(regMatch, "msg=audit(")
+		splitUnixTime := strings.Split(unixTime, ".")
+		secnds, err := strconv.Atoi(splitUnixTime[0])
+		nanosecnds, err := strconv.Atoi(splitUnixTime[1])
+		if err != nil {
+			return make([]string, 0), errors.Wrap(err, "error parsing or converting audit timestamp")
+		}
+		t := time.Unix(int64(secnds), int64(nanosecnds))
+
+		if bootTime.Before(t) {
+			recentLogs := lines[i:]
+			return recentLogs, nil
+		}
+	}
+	return make([]string, 0), nil
+}
