900dfcaf3960b79b2efb962d15a38e60dcf01b5d
Josie Nordrum <JosieNordrum@google.com>
2021-08-06 13:03:16 -0600

    firmware: add bio_crypto_init smoke test
    Add test to check that bio_crypto_init finished with no violations and
    successfully set FPTPM seed. Add function to get audit logs since boot.
    Check that the string "bio_crypto_init" is not found in recent audit
    logs or in upstart logs.
    
    BUG=b:150468438
    TEST=tast -verbose run -ephemeraldevserver=false <IP> firmware.FpBioCryptoInitSuccess
    	trigger bio_crypto_init error: delete "clone: 1" from
    		/usr/share/policy/bio-crypto-init-policy
    		sync
    	then generate fake TPM seed
    		SEED="/run/bio_crypto_init/seed"
    		dd if=/dev/urandom of="${SEED}" bs=32 count=1
    		chown biod:biod "${SEED}"
    	then run `start bio_crypto_init`
    	1. test should FAIL with violation in audit logs
    	next reboot computer
    	2. test should FAIL with violation in upstart logs
    	then re-add clone policy, sync, and reboot
    	3. test should PASS
    	last, generate fake TPM seed and start bio_crypto_init
    	4. test should PASS (upstart logs will have non-warning
    	   bio_crypto_init logs)
    
    Change-Id: I2fe3cc332d92ea903f36d352f28b59fa0e83a8f2

---
+++ src/chromiumos/tast/local/firmware/fp_read_logs_since_boot.go
@@ -0,0 +1,96 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+)
+
+const (
+	auditLogPath   = "/var/log/audit/audit.log"
+	upstartLogPath = "/var/log/upstart.log"
+	statLogPath    = "/proc/stat"
+)
+
+// GetBootTime returns boot time as a Time.
+func GetBootTime() (time.Time, error) {
+	statLogs, err := ioutil.ReadFile(statLogPath)
+	if err != nil {
+		return time.Time{}, errors.Wrap(err, "failed to read status logs")
+	}
+	rx := regexp.MustCompile(`(?:btime )([0-9]*)`)
+	tout := rx.FindStringSubmatch(string(statLogs))
+	toutint, _ := strconv.Atoi(tout[1])
+	if err != nil {
+		return time.Time{}, errors.Wrap(err, "error parsing boot time")
+	}
+	bootTime := time.Unix(int64(toutint), int64(0))
+	return bootTime, nil
+}
+
+// GetAuditLogsSinceBoot returns all audit logs since last boot.
+func GetAuditLogsSinceBoot(ctx context.Context) ([]string, error) {
+	bootTime, err := GetBootTime()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get boot time")
+	}
+	auditLogs, err := ioutil.ReadFile(auditLogPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read audit log")
+	}
+	rx := regexp.MustCompile(`(?:msg=audit\()([0-9]*)\.([0-9]*)`)
+	lines := strings.Split(string(auditLogs), "\n")
+	for i, l := range lines {
+		regMatch := rx.FindStringSubmatch(l)
+		secnds, err := strconv.Atoi(regMatch[1])
+		if err != nil {
+			return nil, errors.Wrap(err, "error parsing audit timestamp")
+		}
+		nanosecnds, err := strconv.Atoi(regMatch[2])
+		if err != nil {
+			return nil, errors.Wrap(err, "error parsing audit timestamp")
+		}
+		t := time.Unix(int64(secnds), int64(nanosecnds))
+
+		if bootTime.Before(t) {
+			recentLogs := lines[i:]
+			return recentLogs, nil
+		}
+	}
+	return nil, nil
+}
+
+// GetUpstartLogsSinceBoot returns all upstart logs since last boot.
+func GetUpstartLogsSinceBoot(ctx context.Context) ([]string, error) {
+	bootTime, err := GetBootTime()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get boot time")
+	}
+
+	upstartLogs, err := ioutil.ReadFile(upstartLogPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read upstart log")
+	}
+	lines := strings.Split(string(upstartLogs), "\n")
+	for i, l := range lines {
+		splitstr := strings.Split(l, " ")
+		t, err := time.Parse(time.RFC3339, splitstr[0])
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse upstart timestamp")
+		}
+		if bootTime.Before(t) {
+			recentLogs := lines[i:]
+			return recentLogs, nil
+		}
+	}
+	return nil, nil
+}
