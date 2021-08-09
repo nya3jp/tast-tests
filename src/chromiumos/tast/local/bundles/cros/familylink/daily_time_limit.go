a5f880ef5d59cdffbb096a97b62664d7f7429305
Xiqi Ruan <xiqiruan@chromium.org>
2021-08-09 16:37:21 -0700

    Tast test: Test daily limit works for Family link account
    This test sets the daily limit to be 1 minute on
    test day. After the dalily limit reaches, the
    screen locks. The screen will be unlock on reset
    time, which is 1 minute after screen locked.
    
    BUG=b:190654894
    TEST=tast -verbose run $DUT familylink.DailyTimeLimit
    
    Change-Id: Ib98442746ea797fc8f60669c3e416eb3454f43f8

--- src/chromiumos/tast/local/bundles/cros/familylink/daily_time_limit.go
+++ src/chromiumos/tast/local/bundles/cros/familylink/daily_time_limit.go
@@ -4,9 +4,9 @@
 
 package familylink
 
+//"reflect"
 import (
 	"context"
-	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -25,8 +25,8 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         BedTimeLimit,
-		Desc:         "Verify the bed time limit works correctly for Family Link account",
+		Func:         DailyTimeLimit,
+		Desc:         "Verify the daily time limit works correctly for Family Link account",
 		Contacts:     []string{"xiqiruan@chromium.org", "cros-families-eng+test@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
@@ -36,7 +36,7 @@ func init() {
 	})
 }
 
-func BedTimeLimit(ctx context.Context, s *testing.State) {
+func DailyTimeLimit(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(*familylink.FixtData).Chrome
 	tconn := s.FixtValue().(*familylink.FixtData).TestConn
 
@@ -50,36 +50,58 @@ func BedTimeLimit(ctx context.Context, s *testing.State) {
 	fdms := s.FixtValue().(*familylink.FixtData).FakeDMS
 
 	now := time.Now()
-	bedTimeDuration := time.Minute
-	timeBeforeLocked := 30 * time.Second
-	startAt := now.Add(timeBeforeLocked)
-	endAt := startAt.Add(bedTimeDuration)
-
 	usageLimitPolicy := familylink.CreateUsageTimeLimitPolicy()
-	bedTimeLimitEntry := &policy.UsageTimeLimitValueTimeWindowLimitEntries{
-		EffectiveDay:      strings.ToUpper(startAt.Weekday().String()),
-		LastUpdatedMillis: strconv.FormatInt(now.Unix(), 10 /*base*/),
-		EndsAt: &policy.RefTime{
-			Hour:   endAt.Local().Hour(),
-			Minute: endAt.Local().Minute(),
-		},
-		StartsAt: &policy.RefTime{
-			Hour:   startAt.Local().Hour(),
-			Minute: startAt.Local().Minute(),
-		},
+
+	// Set daily limit to be 1m to shorten the test.
+	dailyLimit := time.Minute
+	resetInMin := dailyLimit + time.Minute
+	// In real life, the reset time is 6:00 am. This test sets the reset time
+	// to be 1m after daily limit ends and the screen is locked (which is
+	// 2m after logged in) without changing the system clock. Family Link users
+	// have restrictions to prevent manipulating the system clock.
+	reset := now.Add(resetInMin)
+	usageLimitPolicy.Val.TimeUsageLimit.ResetAt = &policy.RefTime{
+		Hour:   reset.Local().Hour(),
+		Minute: reset.Local().Minute(),
 	}
 
-	usageLimitPolicy.Val.TimeWindowLimit.Entries =
-		append(usageLimitPolicy.Val.TimeWindowLimit.Entries, bedTimeLimitEntry)
+	dailyLimitEntry := &policy.RefTimeUsageLimitEntry{
+		LastUpdatedMillis: strconv.FormatInt(now.Unix(), 10 /*base*/),
+		UsageQuotaMins:    int(dailyLimit.Minutes()),
+	}
 
-	// Bed time duration is set by parent, which could be several hours starts at night
-	// and ends in the morning. This test locks the screen after 30 seconds and unlocks it
-	// after another minute. Family Link users have restrictions to prevent manipulating
-	// the system clock.
+	// The daily limit is applied between reset time of day N and day N+1. For example,
+	// if we set 1 minute daily limit on Tuesday with the reset time to be 17:00, the
+	// 1 minute daily limit will be applied between 17:00 Tuesday to 16:59 Wednesday.
+	// This line calculate which date in the week should be set limit on base on `reset`
+	// and set this date with `dailyLimitEntry`.
+	switch weekday := reset.Add(-time.Hour * 24).Weekday(); weekday {
+	case time.Sunday:
+		usageLimitPolicy.Val.TimeUsageLimit.Sunday = dailyLimitEntry
+		break
+	case time.Monday:
+		usageLimitPolicy.Val.TimeUsageLimit.Monday = dailyLimitEntry
+		break
+	case time.Tuesday:
+		usageLimitPolicy.Val.TimeUsageLimit.Tuesday = dailyLimitEntry
+		break
+	case time.Wednesday:
+		usageLimitPolicy.Val.TimeUsageLimit.Wednesday = dailyLimitEntry
+		break
+	case time.Thursday:
+		usageLimitPolicy.Val.TimeUsageLimit.Thursday = dailyLimitEntry
+		break
+	case time.Friday:
+		usageLimitPolicy.Val.TimeUsageLimit.Friday = dailyLimitEntry
+		break
+	case time.Saturday:
+		usageLimitPolicy.Val.TimeUsageLimit.Saturday = dailyLimitEntry
+		break
+	}
+
 	policies := []policy.Policy{
 		usageLimitPolicy,
 	}
-
 	pb := fakedms.NewPolicyBlob()
 	pb.PolicyUser = s.FixtValue().(*familylink.FixtData).PolicyUser
 	pb.AddPolicies(policies)
@@ -95,18 +117,21 @@ func BedTimeLimit(ctx context.Context, s *testing.State) {
 
 	ui := uiauto.New(tconn)
 	authTimeOut := 10 * time.Second
-	s.Log("Waiting for bed time starts in ", timeBeforeLocked)
+
+	// The tested account on DUT might have been active before this test. The screen might be
+	// locked less than `dailyLimit`.
+	s.Log("Waiting for daily limit reaches at most in ", dailyLimit)
 	if _, err := lockscreen.WaitState(ctx, tconn,
-		func(st lockscreen.State) bool { return st.Locked }, timeBeforeLocked+authTimeOut); err != nil {
+		func(st lockscreen.State) bool { return st.Locked }, dailyLimit+authTimeOut); err != nil {
 		s.Fatal("Waiting for screen to be locked failed: ", err)
 	}
-	if err := ui.WaitUntilExists(nodewith.Name("Time for bed").Role(role.StaticText))(ctx); err != nil {
-		s.Fatal("Time for bed message is missing: ", err)
+	if err := ui.WaitUntilExists(nodewith.Name("Time is up").Role(role.StaticText))(ctx); err != nil {
+		s.Fatal("Time is up message is missing: ", err)
 	}
 
-	s.Log("Waiting for bed time ends in ", bedTimeDuration)
+	s.Log("Waiting for daily limit reset at most in ", resetInMin)
 	childUser := strings.ToLower(s.RequiredVar("unicorn.childUser"))
-	if err := lockscreen.WaitForPasswordField(ctx, tconn, childUser, bedTimeDuration+authTimeOut); err != nil {
+	if err := lockscreen.WaitForPasswordField(ctx, tconn, childUser, resetInMin+authTimeOut); err != nil {
 		s.Error("Password text field did not appear in the UI: ", err)
 	}
 
@@ -122,7 +147,7 @@ func BedTimeLimit(ctx context.Context, s *testing.State) {
 	}
 	if st, err := lockscreen.WaitState(ctx, tconn,
 		func(st lockscreen.State) bool { return st.LoggedIn }, authTimeOut); err != nil {
-		s.Fatal(fmt.Sprintf("Waiting for screen to be unlocked failed (last status %+v): ", st), err)
+		s.Fatalf("Waiting for screen to be unlocked failed (last status %+v): %v", st, err)
 	}
 
 }
