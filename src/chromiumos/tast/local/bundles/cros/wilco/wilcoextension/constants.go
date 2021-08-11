8d70c36e2ea9c24f5a48794f73a7dacf41bd2bb7
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-08-11 10:20:43 +0000

    tast-tests: Wilco DTC fixtures
    New support for "wilcoDTCAllowed" and "wilcoDTCAllowedVMTestMode" fixtures
    to perform chrome, fakedms enrollment along with setting up and tearing
    down Wilco VM and Support daemon.
    
    BUG=b:189457904
    TEST=Manual:tast run $DUT wilco.APIGetStatefulPartitionAvailableCapacityEnrolled
    (Pass)
    
    Change-Id: Ia12e08a571edc1c18a4ec731aa1ef2e96cd612ce

---
+++ src/chromiumos/tast/local/bundles/cros/wilco/wilcoextension/constants.go
@@ -0,0 +1,27 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package wilcoextension contains constants and helpers to work with the
+// extension that can interact with the Wilco DTC VM.
+package wilcoextension
+
+// ID is hardcoded in Chrome to have access to the private API.
+const ID = "emelalhagcpibaiiiijjlkmhhbekaidg"
+
+// Manifest gives the extension permissions necessary to access the private API.
+const Manifest = `{
+    "key": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxcctELmluEjUimtjiCl44Z/U4iUm79M0lDBARqE0VmymkylGq4givCsZINZf2JyPJC1xM09UEpDvCyS/9qVISX8HzhuilCN1tNe8w473uiiIOHfgLwAYK5FO58F8G18LrkssKPM9OdCnvM+SVfgFxtcSZUxaiE0FRqs5hLmGSXnLSxDdFeVBUh9xQf2WdJL77inyBfP8pL9KnZ8CpKJ7HiK0ubAGBU4NkoDZRfWiGsKGYLIB/Lh3QWmymXrxVngoz+zqctoH/13Jt23civwyg7epnDwq48PurIU1zTs2bANtgsFc1sAEl2/bACAQHH7oX9ptgIL18Rt+8Khs+WghvwIDAQAB",
+    "description": "Wilco test extension",
+    "name": "Wilco test extension",
+    "background": {
+        "scripts": [
+            "background.js"
+        ]
+    },
+    "manifest_version": 2,
+    "version": "0.1",
+    "permissions": [
+        "nativeMessaging"
+    ]
+}`
