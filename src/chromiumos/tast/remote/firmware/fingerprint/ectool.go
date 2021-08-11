fdd027953cc72fd36f2fe55aef38d63fa5c6f347
Craig Hesling <hesling@chromium.org>
2021-08-11 14:09:27 -0700

    fingerprint: Refactor RollbackInfo functionality
    This factors out the RollbackInfo functionality to the RollbackState
    object itself.
    
    We also create a new ectool.go file to manage our specific ectool
    operations that will be migrated to the common ectool library.
    
    BUG=b:195989768, b:194500081, b:196182618
    TEST=../tast/fast_build.sh -T
    TEST=ssh localhost -p2222 -- flash_fp_mcu
         sleep 5 # biod restarting will init entropy
         ssh localhost -p2222 -- ectool --name=cros_fp rollbackinfo
         dut-control fw_wp_state:force_on
         ssh localhost -p2222 -- ectool --name=cros_fp flashprotect enable
         sleep 1
         ssh localhost -p2222 -- ectool --name=cros_fp reboot_ec
         # Block ID should be 1
         # Since the flash protect flags checking is broken, we
         # need to schedule in terms of keeping sw-wp
         time tast run localhost:2222 firmware.FpAddEntropy
         # Ensure all pass
    
    Change-Id: I5081fef1767ec57e70ad04965e1c862e4cb9caeb

---
+++ src/chromiumos/tast/remote/firmware/fingerprint/ectool.go
@@ -0,0 +1,84 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file implements functions to interact with the DUT's fingerprint MCU.
+
+package fingerprint
+
+import (
+	"context"
+	"strconv"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/ssh"
+	"chromiumos/tast/testing"
+)
+
+// RollbackState is the state of the anti-rollback block.
+type RollbackState struct {
+	BlockID    int
+	MinVersion int
+	RWVersion  int
+}
+
+// UnmarshalerEctool unmarshals part of ectool's output into a RollbackState.
+func (r *RollbackState) UnmarshalerEctool(data []byte) error {
+	rollbackInfoMap := parseColonDelimitedOutput(string(data))
+
+	var state RollbackState
+	blockID, err := strconv.Atoi(rollbackInfoMap["Rollback block id"])
+	if err != nil {
+		return errors.Wrap(err, "failed to convert rollback block id")
+	}
+	state.BlockID = blockID
+
+	minVersion, err := strconv.Atoi(rollbackInfoMap["Rollback min version"])
+	if err != nil {
+		return errors.Wrap(err, "failed to convert rollback min version")
+	}
+	state.MinVersion = minVersion
+
+	rwVersion, err := strconv.Atoi(rollbackInfoMap["RW rollback version"])
+	if err != nil {
+		return errors.Wrap(err, "failed to convert RW rollback version")
+	}
+	state.RWVersion = rwVersion
+
+	*r = state
+	return nil
+}
+
+// IsEntropySet checks that entropy has already been set based on the block ID.
+//
+// If the block ID is greater than 0, there is a very good chance that entropy
+// has been added. This is the same way that biod/bio_wash checks if entropy has
+// been set. That being said, this method can be fooled if some test simply
+// increments the anti-rollback version from a fresh flashing.
+func (r *RollbackState) IsEntropySet() bool {
+	return r.BlockID > 0
+}
+
+// IsAntiRollbackSet checks if version anti-rollback has been enabled.
+//
+// We currently do not have a minimum version number, thus this function
+// indicates if we are not in the normal rollback state.
+func (r *RollbackState) IsAntiRollbackSet() bool {
+	return r.MinVersion != 0 || r.RWVersion != 0
+}
+
+// RollbackInfo returns the rollbackinfo of the fingerprint MCU.
+func RollbackInfo(ctx context.Context, d *dut.DUT) (RollbackState, error) {
+	cmd := []string{"ectool", "--name=cros_fp", "rollbackinfo"}
+	testing.ContextLogf(ctx, "Running command: %s", shutil.EscapeSlice(cmd))
+	out, err := d.Conn().Command(cmd[0], cmd[1:]...).Output(ctx, ssh.DumpLogOnError)
+	if err != nil {
+		return RollbackState{}, errors.Wrap(err, "failed to query FPMCU rollbackinfo")
+	}
+
+	var state RollbackState
+	err = state.UnmarshalerEctool(out)
+	return state, err
+}
---
+++ src/chromiumos/tast/remote/firmware/fingerprint/ectool_test.go
@@ -0,0 +1,28 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file implements functions to interact with the DUT's fingerprint MCU.
+
+package fingerprint
+
+import "testing"
+
+func TestRollbackStateEctoolUnmarshaler(t *testing.T) {
+	// It should not matter that there are tabs to the left of the examples out.
+	var out = []byte(`
+	Rollback block id:    19
+	Rollback min version: 0
+	RW rollback version:  255
+	`)
+	var rExpect = RollbackState{BlockID: 19, MinVersion: 0, RWVersion: 255}
+
+	var r RollbackState
+	if err := r.UnmarshalerEctool(out); err != nil {
+		t.Fatal("Failed to unmarshal ")
+	}
+
+	if r != rExpect {
+		t.Fatalf("Unmarshaled rollback block %+v doesn't match expected block %+v.", r, rExpect)
+	}
+}
