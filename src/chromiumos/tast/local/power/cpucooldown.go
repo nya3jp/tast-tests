367e1ff22fdb3148cbe1b193c36eebf783eb5dbd
Po-Hsien Wang <pwang@chromium.org>
2021-08-11 16:58:39 -0700

    power: more temperature sensors to WaitCPUCoolDown
    Add readings from hwmon and ectools to WaitCPUCoolDown
    
    BUG=b:183221387
    BUG=b:196261267
    TEST=tast run
    
    Change-Id: If964032dcbc2dbc927d7715136e25ca85520ac1a

--- src/chromiumos/tast/local/power/cpucooldown.go
+++ src/chromiumos/tast/local/power/cpucooldown.go
@@ -8,11 +8,14 @@ package power
 import (
 	"context"
 	"io/ioutil"
+	"math"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
 
+	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
@@ -43,7 +46,7 @@ func DefaultCoolDownConfig(mode CoolDownMode) CoolDownConfig {
 	return CoolDownConfig{
 		PollTimeout:             300 * time.Second,
 		PollInterval:            2 * time.Second,
-		CPUTemperatureThreshold: 46000,
+		CPUTemperatureThreshold: 46,
 		CoolDownMode:            mode,
 	}
 }
@@ -52,21 +55,7 @@ func DefaultCoolDownConfig(mode CoolDownMode) CoolDownConfig {
 // took to cool down.
 // Ported from cheets_PerfBoot.wait_cpu_cool_down().
 func WaitUntilCPUCoolDown(ctx context.Context, config CoolDownConfig) (time.Duration, error) {
-	const (
-		// thermalZonePath is the path to thermal zone directories.
-		thermalZonePath = "/sys/class/thermal/thermal_zone*"
-	)
-
-	// thermalIgnoreTypes list thermal zones type to be ignored.
-	var thermalIgnoreTypes = []string{
-		// iwlwifi is the zone type of generic driver for WiFi adapters on most Intel platforms.
-		"iwlwifi",
-		// b/180696076: trogdor boards have a charger sensor which cools down extremely slowly when it charges the battery.
-		"charger-thermal",
-	}
-
 	timeBefore := time.Now()
-
 	switch config.CoolDownMode {
 	case CoolDownPreserveUI:
 	case CoolDownStopUI:
@@ -80,56 +69,15 @@ func WaitUntilCPUCoolDown(ctx context.Context, config CoolDownConfig) (time.Dura
 		return 0, errors.New("invalid cool down mode")
 	}
 
-	zonePaths, err := filepath.Glob(thermalZonePath)
-	if err != nil || len(zonePaths) == 0 {
-		return 0, errors.Wrapf(err, "failed to glob %s", thermalZonePath)
-	}
-
 	testing.ContextLog(ctx, "Waiting until CPU is cooled down")
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		for _, zonePath := range zonePaths {
-			b, err := ioutil.ReadFile(filepath.Join(zonePath, "mode"))
-			// No need to return on error because mode file doesn't always exist.
-			if err == nil && strings.TrimSpace(string(b)) == "disabled" {
-				continue
-			}
-
-			zoneTypePath := filepath.Join(zonePath, "type")
-			b, err = ioutil.ReadFile(zoneTypePath)
-			if err != nil {
-				return testing.PollBreak(
-					errors.Wrapf(err, "failed to read %q", zoneTypePath))
-			}
-			zoneType := strings.TrimSpace(string(b))
-			ignoreZone := false
-			for _, zoneToIgnore := range thermalIgnoreTypes {
-				if strings.Contains(zoneType, zoneToIgnore) {
-					ignoreZone = true
-					break
-				}
-			}
-			if ignoreZone {
-				continue
-			}
-
-			zoneTempPath := filepath.Join(zonePath, "temp")
-			b, err = ioutil.ReadFile(zoneTempPath)
-			if err != nil {
-				return testing.PollBreak(
-					errors.Wrapf(err, "failed to read %q", zoneTempPath))
-			}
-			zoneTemp, err := strconv.Atoi(strings.TrimSpace(string(b)))
-			if err != nil {
-				return testing.PollBreak(errors.Wrapf(err,
-					"failed to parse temperature value in %q", zoneTempPath))
-			}
-
-			if zoneTemp > config.CPUTemperatureThreshold {
-				testing.ContextLogf(ctx, "Waiting until %s temperature (%d) falls below %d",
-					zoneType, zoneTemp, config.CPUTemperatureThreshold)
-				return errors.Errorf("timed out while waiting until %s temperature (%d) falls below %d",
-					zoneType, zoneTemp, config.CPUTemperatureThreshold)
-			}
+		temperature, err := MaxObservedTemperature(ctx)
+		if err != nil {
+			return testing.PollBreak(err)
+		}
+		if int(math.Ceil(temperature)) > config.CPUTemperatureThreshold {
+			testing.ContextLogf(ctx, "Waiting until temperature (%d) falls below %d", temperature, config.CPUTemperatureThreshold)
+			return errors.Errorf("timed out while waiting until temperature (%d) falls below %d", temperature, config.CPUTemperatureThreshold)
 		}
 		return nil
 	}, &testing.PollOptions{Timeout: config.PollTimeout, Interval: config.PollInterval}); err != nil {
@@ -140,4 +88,163 @@ func WaitUntilCPUCoolDown(ctx context.Context, config CoolDownConfig) (time.Dura
 	duration := timeAfter.Sub(timeBefore)
 	testing.ContextLogf(ctx, "CPU is cooled down (took %f seconds)", duration.Seconds())
 	return duration, nil
+}
+
+// MaxObservedTemperature returns the maximum observed temperature reported by sensors.
+func MaxObservedTemperature(ctx context.Context) (float64, error) {
+	var temperatures []float64
+	// Not all temperature readings from ectool are monitoring CPU temperature. However, the cpu sensor should reports the highest temperature readings.
+	if temps, err := ectoolTemperatures(ctx); err != nil {
+		testing.ContextLog(ctx, "Failed to read ectool temeprature: ", err)
+	} else {
+		temperatures = append(temperatures, temps...)
+	}
+
+	if temps, err := hwmonTemperatures(ctx); err != nil {
+		testing.ContextLog(ctx, "Failed to read hwmon temeprature: ", err)
+	} else {
+		temperatures = append(temperatures, temps...)
+	}
+
+	if temps, err := thermalZoneTemperatures(ctx); err != nil {
+		testing.ContextLog(ctx, "Failed to read thermal_zone temeprature: ", err)
+	} else {
+		temperatures = append(temperatures, temps...)
+	}
+
+	if len(temperatures) == 0 {
+		return 0, errors.New("failed to read temperatures from all the sensors")
+	}
+
+	maxTemperature := temperatures[0]
+	for _, temperature := range temperatures {
+		if temperature > maxTemperature {
+			maxTemperature = temperature
+		}
+	}
+	return maxTemperature, nil
+}
+
+// ectoolTemperatures returns the temperatures in Celsius observed by ectool.
+func ectoolTemperatures(ctx context.Context) ([]float64, error) {
+	stdout, err := testexec.CommandContext(ctx, "ectool", "temps", "all").Output(testexec.DumpLogOnError)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to run ectool")
+	}
+
+	var res []float64
+	matchRegex := regexp.MustCompile(`.*: (\d+) K`)
+	for _, line := range strings.Split(string(stdout), "\n") {
+		matches := matchRegex.FindStringSubmatch(line)
+		if matches == nil {
+			continue
+		}
+
+		temps, err := strconv.Atoi(matches[1])
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to parse ectool output: %q", line)
+		}
+		// Convert to Celsius
+		temperature := float64(temps - 273)
+		if temperature < 10 || temperature > 150 {
+			testing.ContextLog(ctx, "Unreasonable ectool temperature", temperature)
+		}
+		res = append(res, temperature-273)
+	}
+	return res, nil
+
+}
+
+// thermalZoneTemperatures returns the temperatures in Celsius observed by thermal_zone.
+func thermalZoneTemperatures(ctx context.Context) ([]float64, error) {
+	const (
+		// thermalZonePath is the path to thermal zone directories.
+		thermalZonePath = "/sys/class/thermal/thermal_zone*"
+	)
+	// thermalIgnoreTypes list thermal zones type to be ignored.
+	var thermalIgnoreTypes = []string{
+		// iwlwifi is the zone type of generic driver for WiFi adapters on most Intel platforms.
+		"iwlwifi",
+		// b/180696076: trogdor boards have a charger sensor which cools down extremely slowly when it charges the battery.
+		"charger-thermal",
+	}
+
+	var res []float64
+	zonePaths, err := filepath.Glob(thermalZonePath)
+	if err != nil || len(zonePaths) == 0 {
+		return nil, errors.Wrapf(err, "failed to glob %s", thermalZonePath)
+	}
+	for _, zonePath := range zonePaths {
+		b, err := ioutil.ReadFile(filepath.Join(zonePath, "mode"))
+		// No need to return on error because mode file doesn't always exist.
+		if err == nil && strings.TrimSpace(string(b)) == "disabled" {
+			continue
+		}
+
+		zoneTypePath := filepath.Join(zonePath, "type")
+		b, err = ioutil.ReadFile(zoneTypePath)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to read %q", zoneTypePath)
+		}
+		zoneType := strings.TrimSpace(string(b))
+		ignoreZone := false
+		for _, zoneToIgnore := range thermalIgnoreTypes {
+			if strings.Contains(zoneType, zoneToIgnore) {
+				ignoreZone = true
+				break
+			}
+		}
+		if ignoreZone {
+			continue
+		}
+
+		zoneTempPath := filepath.Join(zonePath, "temp")
+		b, err = ioutil.ReadFile(zoneTempPath)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to read %q", zoneTempPath)
+		}
+		zoneTemp, err := strconv.Atoi(strings.TrimSpace(string(b)))
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to parse temperature value in %q", zoneTempPath)
+		}
+
+		temperature := float64(zoneTemp) / 1000
+		if temperature < 10 || temperature > 150 {
+			testing.ContextLogf(ctx, "Unreasonable thermal_zone(%v) temperature: %v", zoneType, temperature)
+		}
+		res = append(res, temperature)
+	}
+	return res, nil
+}
+
+// hwmonTemperatures returns the temperatures in Celsius observed by hwmon.
+func hwmonTemperatures(ctx context.Context) ([]float64, error) {
+	// The files contain temperature input value in millidegree Celsius.
+	// https://www.kernel.org/doc/Documentation/hwmon/sysfs-interface
+	const pattern = "/sys/class/hwmon/hwmon*/temp*_input"
+	fs, err := filepath.Glob(pattern)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to obtain list of temperature files")
+	}
+	if len(fs) == 0 {
+		return nil, errors.Errorf("no file matches %s", pattern)
+	}
+
+	var res []float64
+	for _, f := range fs {
+		b, err := ioutil.ReadFile(f)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to read temperature file")
+		}
+		c, err := strconv.ParseFloat(strings.TrimSpace(string(b)), 64)
+		if err != nil {
+			return nil, errors.Wrapf(err, "could not parse %s to get input temperature", f)
+		}
+		c = c / 1000
+		if c < 10 || c > 150 {
+			testing.ContextLog(ctx, "Unreasonable hwmon temperature", c)
+		}
+		res = append(res, c)
+	}
+	return res, nil
 }
