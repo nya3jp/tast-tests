660546a71d725b7a93b85c4f762b1415eb7010b5
Craig Hesling <hesling@chromium.org>
2021-08-11 14:28:54 -0700

    fingerprint: Refactor code into ectool file
    Move ectool related code into ectool.go. This is in preparation for
    converting these into more generic ectool functions.
    
    This has no functional change, since the move is within the same package.
    
    BUG=b:195989768, b:194500081, b:196182618
    TEST=../tast/fast_build.sh -T
    
    Change-Id: I18b224e71ad2950595b86e00995e10445dcbda5c

--- src/chromiumos/tast/remote/firmware/fingerprint/ectool.go
+++ src/chromiumos/tast/remote/firmware/fingerprint/ectool.go
@@ -9,13 +9,37 @@ package fingerprint
 import (
 	"context"
 	"strconv"
+	"strings"
+	"time"
 
 	"chromiumos/tast/dut"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/firmware"
 	"chromiumos/tast/shutil"
 	"chromiumos/tast/ssh"
 	"chromiumos/tast/testing"
 )
+
+// FWImageType is the type of firmware (RO or RW).
+type FWImageType string
+
+// These are the possible values of FWImageType.
+const (
+	ImageTypeRO FWImageType = "RO"
+	ImageTypeRW FWImageType = "RW"
+)
+
+const (
+	ectoolROVersion = "RO version"
+	ectoolRWVersion = "RW version"
+)
+
+// EctoolCommand constructs an "ectool" command for the FPMCU.
+func EctoolCommand(ctx context.Context, d *dut.DUT, args ...string) *ssh.Cmd {
+	cmd := firmware.NewECTool(d, firmware.ECToolNameFingerprint).Command(args...)
+	testing.ContextLogf(ctx, "Running command: %s", shutil.EscapeSlice(cmd.Args))
+	return cmd
+}
 
 // RollbackState is the state of the anti-rollback block.
 type RollbackState struct {
@@ -81,4 +105,97 @@ func RollbackInfo(ctx context.Context, d *dut.DUT) (RollbackState, error) {
 	var state RollbackState
 	err = state.UnmarshalerEctool(out)
 	return state, err
+}
+
+// AddEntropy adds entropy to the fingerprint MCU.
+func AddEntropy(ctx context.Context, d *dut.DUT, reset bool) error {
+	args := []string{"addentropy"}
+	if reset {
+		args = append(args, "reset")
+	}
+	return EctoolCommand(ctx, d, args[0:]...).Run(ctx)
+}
+
+// RebootFpmcu reboots the fingerprint MCU. It does not reboot the AP.
+func RebootFpmcu(ctx context.Context, d *dut.DUT, bootTo FWImageType) error {
+	testing.ContextLog(ctx, "Rebooting FPMCU")
+	// This command returns error even on success, so ignore error. b/116396469
+	_ = EctoolCommand(ctx, d, "reboot_ec").Run(ctx)
+	if bootTo == ImageTypeRO {
+		testing.Sleep(ctx, 500*time.Millisecond)
+		err := EctoolCommand(ctx, d, "rwsigaction", "abort").Run(ctx)
+		if err != nil {
+			return errors.Wrap(err, "failed to abort rwsig")
+		}
+	}
+
+	if err := WaitForRunningFirmwareImage(ctx, d, bootTo); err != nil {
+		return errors.Wrapf(err, "failed to boot to %q image", bootTo)
+	}
+
+	// Double check we are still in the expected image.
+	firmwareCopy, err := RunningFirmwareCopy(ctx, d)
+	if err != nil {
+		return err
+	}
+	if firmwareCopy != bootTo {
+		return errors.Errorf("FPMCU booted to %q, expected %q", firmwareCopy, bootTo)
+	}
+	return nil
+}
+
+// RunningFirmwareCopy returns the firmware copy on FPMCU (RO or RW).
+func RunningFirmwareCopy(ctx context.Context, d *dut.DUT) (FWImageType, error) {
+	out, err := EctoolCommand(ctx, d, "version").Output(ctx)
+	if err != nil {
+		return FWImageType(""), errors.Wrap(err, "failed to query FPMCU version")
+	}
+	versionInfoMap := parseColonDelimitedOutput(string(out))
+	firmwareCopy := versionInfoMap["Firmware copy"]
+	if firmwareCopy != string(ImageTypeRO) && firmwareCopy != string(ImageTypeRW) {
+		return FWImageType(""), errors.New("cannot find firmware copy string")
+	}
+	return FWImageType(firmwareCopy), nil
+}
+
+// CheckFirmwareIsFunctional checks that the AP can talk to the FPMCU and get the version.
+func CheckFirmwareIsFunctional(ctx context.Context, d *dut.DUT) ([]byte, error) {
+	testing.ContextLog(ctx, "Checking firmware is functional")
+	return EctoolCommand(ctx, d, "version").Output(ctx, ssh.DumpLogOnError)
+}
+
+// runningFirmwareVersion returns the current RO or RW firmware version on the FPMCU.
+func runningFirmwareVersion(ctx context.Context, d *dut.DUT, image FWImageType) (string, error) {
+	out, err := EctoolCommand(ctx, d, "version").Output(ctx, ssh.DumpLogOnError)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to query FPMCU version")
+	}
+	versionInfoMap := parseColonDelimitedOutput(string(out))
+	switch image {
+	case ImageTypeRW:
+		return versionInfoMap[ectoolRWVersion], nil
+	case ImageTypeRO:
+		return versionInfoMap[ectoolROVersion], nil
+	default:
+		return "", errors.Errorf("unrecognized image type: %q", image)
+	}
+}
+
+func rawFPFrameCommand(ctx context.Context, d *dut.DUT) *ssh.Cmd {
+	return EctoolCommand(ctx, d, "fpframe", "raw")
+}
+
+// parseColonDelimitedOutput parses colon delimited information to a map.
+func parseColonDelimitedOutput(output string) map[string]string {
+	ret := map[string]string{}
+	for _, line := range strings.Split(output, "\n") {
+		// Note that the ectool version build info line uses ':'s as time of
+		// date delimiters.
+		splits := strings.SplitN(line, ":", 2)
+		if len(splits) != 2 {
+			continue
+		}
+		ret[strings.TrimSpace(splits[0])] = strings.TrimSpace(splits[1])
+	}
+	return ret
 }
