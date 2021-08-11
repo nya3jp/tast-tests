3ebcc50e43f578ec8c853b21850cd771e281e023
Hikaru Nishida <hikalium@chromium.org>
2021-08-11 21:07:13 +0900

    tast-tests: Add suspend tests for ARC
    This test checks the behavior of guest clocks when the host goes into
    suspend.
    
    BUG=b:181617646
    TEST=USE="-cros-debug" emerge-${BOARD} tast-local-helpers-cros
    TEST=cros deploy ${DUT} --root=/usr/local tast-local-helpers-cros
    TEST=tast run ${DUT} arc.Suspend*
    
    Cq-Depend: chromium:3108946
    Change-Id: I0e98f4a9ccc0a408a8d1a637cb4e89da7a8b388b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3174895
    Tested-by: Hikaru Nishida <hikalium@chromium.org>
    Commit-Queue: Hikaru Nishida <hikalium@chromium.org>
    Reviewed-by: David Welling <davidwelling@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

---
+++ helpers/local/arc.Suspend.readclocks.c
@@ -0,0 +1,22 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <time.h>
+#include <stdio.h>
+
+int main() {
+  struct timespec clock_boottime;
+  struct timespec clock_monotonic;
+
+  clock_gettime(CLOCK_BOOTTIME, &clock_boottime);
+  clock_gettime(CLOCK_MONOTONIC, &clock_monotonic);
+
+  puts("{");
+  printf("  \"CLOCK_BOOTTIME\": { \"seconds\": %10ld, \"nanos\": %10ld },\n",
+    clock_boottime.tv_sec, clock_boottime.tv_nsec);
+  printf("  \"CLOCK_MONOTONIC\": { \"seconds\": %10ld, \"nanos\": %10ld }\n",
+    clock_monotonic.tv_sec, clock_monotonic.tv_nsec);
+  puts("}");
+  return 0;
+}
