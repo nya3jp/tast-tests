2cf1cac7eb93cd0e6a0345e8b68d83f6c07969fe
Craig Hesling <hesling@chromium.org>
2021-08-11 14:28:54 -0700

    fingerprint: Refactor code into ectool file
    Move ectool related code into ectool.go. This is in preparation for
    converting these into more generic ectool functions.
    
    This has no functional change, since the move is within the same package.
    
    BUG=b:195989768, b:194500081, b:196182618
    TEST=../tast/fast_build.sh -T
    
    Change-Id: I18b224e71ad2950595b86e00995e10445dcbda5c

--- src/chromiumos/tast/remote/firmware/fingerprint/ectool_test.go
+++ src/chromiumos/tast/remote/firmware/fingerprint/ectool_test.go
@@ -39,3 +39,43 @@ func TestRollbackStateEctoolUnmarshalerError(t *testing.T) {
 		t.Fatalf("Failed to detect error in rollback min version. Produced rollback block %+v.", r)
 	}
 }
+
+func TestParseColonDelimitedOutput(t *testing.T) {
+	const ectoolVersionOutput = `
+
+f1: A B C
+	 f2	 : D E F
+ f3 : G : H : I
+
+
+`
+
+	vals := parseColonDelimitedOutput(ectoolVersionOutput)
+	if keys := len(vals); keys != 3 {
+		t.Fatalf("Wrong number of keys. Expected 3, but received %d keys.", keys)
+	}
+	for k, v := range vals {
+		if v == "" {
+			t.Fatalf("Key %s had blank value.", k)
+		}
+	}
+
+	check := func(field, value string) {
+		v, ok := vals[field]
+		if !ok {
+			t.Fatal("Missing field '" + field + "'.")
+		}
+		if vals[field] != value {
+			t.Fatal("Field " + field + " contains invalid entry '" + v + "'.")
+		}
+		delete(vals, field)
+	}
+
+	check("f1", "A B C")
+	check("f2", "D E F")
+	check("f3", "G : H : I")
+
+	if len(vals) != 0 {
+		t.Fatal("Parsed extra values.")
+	}
+}
