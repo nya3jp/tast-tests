433c78f45bcc806d3f4f0d5983cea33d58df2ffd
Craig Hesling <hesling@chromium.org>
2021-08-11 21:00:46 -0700

    fingerprint: Add more robust mechanism to get flashprotect flags
    This also fixes an active flags regex issue, where it would omit
    any hex characters from the flash protect flag value.
    This is the difference between "\d+" and "[[:xdigit:]]+".
    
    BUG=b:196182618, b:195989768, b:181369478
    TEST=~/trunk/src/platform/tast/fast_build.sh -T
    TEST=time tast run localhost:2222 firmware.FpCheckWriteProtect
    
    Change-Id: I40a7413e29705999e3fc815ea45a4c91c431ce6e

---
+++ src/chromiumos/tast/remote/firmware/fingerprint/fingerprint_flash_protect_test.go
@@ -0,0 +1,33 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fingerprint
+
+import "testing"
+
+func TestFlashProtectEctoolUnmarshaler(t *testing.T) {
+	// It should not matter that there are tabs to the left of the examples out.
+	var out = []byte(`
+	Flash protect flags: 0x0000000b wp_gpio_asserted ro_at_boot ro_now
+	Valid flags:         0x0000003f wp_gpio_asserted ro_at_boot ro_now all_now STUCK INCONSISTENT
+	Writable flags:      0x00000004 all_now
+	`)
+	var expect = FlashProtect{
+		Active: FlashProtectGpioAsserted | FlashProtectRoAtBoot |
+			FlashProtectRoNow,
+		Valid: FlashProtectGpioAsserted | FlashProtectRoAtBoot |
+			FlashProtectRoNow | FlashProtectAllNow | FlashProtectErrorStuck |
+			FlashProtectErrorInconsistent,
+		Writeable: FlashProtectAllNow,
+	}
+
+	var actual FlashProtect
+	if err := actual.UnmarshalerEctool(out); err != nil {
+		t.Fatal("Failed to unmarshal flash protect info: ", err)
+	}
+
+	if actual != expect {
+		t.Fatalf("Unmarshaled flash protect block %+v doesn't match expected block %+v.", actual, expect)
+	}
+}
