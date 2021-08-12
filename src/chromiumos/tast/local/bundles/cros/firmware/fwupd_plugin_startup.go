d5f2db3e09674a8a399112ad743c04bc4016b861
Daniel Campello <campello@chromium.org>
2021-08-12 13:48:24 +0000

    fwupd: Expanding firmware.FwupdPowerdStartup to work with all plugins
    Changes the command run by this test so that the output comes in json
    format. This output is then parsed into a struct, which is iterated over
    until the target plugin (as determined by the name passed in the
    command) and flags (if applicable) are found.
    
    BUG=b:196631044
    TEST=tast run [user@]host[:port] firmware.FwupdPluginStartup
    
    Change-Id: I99a07e7d9ac887d6dec607d50ec76600e7159ae8

--- src/chromiumos/tast/local/bundles/cros/firmware/fwupd_plugin_startup.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd_plugin_startup.go
@@ -19,7 +19,7 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func: FwupdPowerdStartup,
+		Func: FwupdPluginStartup,
 		Desc: "Checks that the powerd plugin is enabled",
 		Contacts: []string{
 			"gpopoola@google.com",       // Test Author
@@ -30,8 +30,16 @@ func init() {
 	})
 }
 
-// checkForPowerdStr verifies that powerd was found among enabled plugins
-func checkForPowerdStr(output []byte) error {
+type pluginState int
+
+const (
+	stateEnabled pluginState = iota
+	stateDisabled
+)
+
+func parsePluginStates(output []byte) (map[string]pluginState, error) {
+	m := make(map[string]pluginState)
+
 	var wp struct {
 		Plugins []struct {
 			Name  string
@@ -40,25 +48,25 @@ func checkForPowerdStr(output []byte) error {
 	}
 
 	if err := json.Unmarshal(output, &wp); err != nil {
-		return errors.New("failed to parse command output")
+		return nil, errors.New("failed to parse command output")
 	}
 
 	for _, p := range wp.Plugins {
-		if p.Name == "powerd" {
-			for _, f := range p.Flags {
-				if f == "disabled" {
-					return errors.New("plugin was found to be disabled")
-				}
+		state := stateEnabled
+		for _, f := range p.Flags {
+			if f == "disabled" {
+				state = stateDisabled
 			}
 		}
+		m[p.Name] = state
 	}
 
-	return nil
+	return m, nil
 }
 
-// FwupdPowerdStartup runs fwupdmgr get-plugins, retrieves the output, and
-// checks for powerd
-func FwupdPowerdStartup(ctx context.Context, s *testing.State) {
+// FwupdPluginStartup runs fwupdmgr get-plugins, retrieves the output, and
+// checks that the expected plugins are enabled
+func FwupdPluginStartup(ctx context.Context, s *testing.State) {
 	if err := upstart.RestartJob(ctx, "fwupd"); err != nil {
 		s.Fatal("Failed to restart fwupd: ", err)
 	}
@@ -72,7 +80,39 @@ func FwupdPowerdStartup(ctx context.Context, s *testing.State) {
 		s.Error("Failed dumping fwupdmgr output: ", err)
 	}
 
-	if err := checkForPowerdStr(output); err != nil {
+	m, err := parsePluginStates(output)
+	if err != nil {
 		s.Fatal("search unsuccessful: ", err)
+	}
+
+	for _, plugin := range []string{
+		"analogix",
+		"ccgx",
+		"cros_ec",
+		"dell_dock",
+		"dfu",
+		"emmc",
+		"nvme",
+		"parade_lspcon",
+		"pixart_rf",
+		"powerd",
+		"realtek_mst",
+		"synaptics_cxaudio",
+		"synaptics_mst",
+		"test",
+		"thunderbolt",
+		"vli",
+		"wacom_raw",
+		"wacom_usb",
+	} {
+		s.Run(ctx, plugin, func(ctx context.Context, s *testing.State) {
+			state, prs := m[plugin]
+			if !prs {
+				s.Fatal("plugin was not found")
+			}
+			if state == stateDisabled {
+				s.Fatal("plugin was found to be disabled")
+			}
+		})
 	}
 }
