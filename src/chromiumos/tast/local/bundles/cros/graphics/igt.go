2e187610a62c629bb5c6bb4a57e8198cf80551fd
Mark Yacoub <markyacoub@google.com>
2021-08-12 13:51:01 -0400

    Graphics.IGT: Fix kms_flip and kms_vblank
    1. Extend the timeout of kms_vblank for 15 mins.
    
    2.Few IGT tests like kms_flip run tests in combination with suspend.
    For this, waking up is achieved using the RTC wake-up alarm.
    
    If any other service is holding /dev/rtc, the IGT test fails to take the lock and set a wake up alarm. Hence, it is required to stop the service which is currently holding the RTC lock before running the IGT test.
    In case of chrome, tlsdated holds rtc lock. This patch stops the
    service before IGT tests are started and stop it when it's done.
    
    BUG=b:193034528
    TEST=cros_sdk tast run VOLTEER graphics.IGT.kms_{flip,vblank}
    
    Change-Id: I5428d5aea1a091c3f4a94ffc6ea8ef6a2a0bf622

--- src/chromiumos/tast/local/bundles/cros/graphics/igt.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/igt.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
 
@@ -318,7 +319,7 @@ func init() {
 			Val: igtTest{
 				exe: "kms_vblank",
 			},
-			Timeout: 5 * time.Minute,
+			Timeout: 15 * time.Minute,
 		}, {
 			Name: "kms_vrr",
 			Val: igtTest{
@@ -386,6 +387,12 @@ func IGT(ctx context.Context, s *testing.State) {
 	cmd := testexec.CommandContext(ctx, exePath)
 	cmd.Stdout = f
 	cmd.Stderr = f
+	// Tests such as kms_flip requires Suspend and Wake-up which are achieved using the RTC wake-up alarm.
+	// tlsdated is holding /dev/rtc so IGT fails to take the lock and set a wake up alarm. Hence, it
+	// is required to stop the tlsdated before running the IGT test.
+	if err := upstart.StopJob(ctx, "tlsdated"); err != nil {
+		s.Fatal("Failed to stop tlsdated: ", err)
+	}
 	err = cmd.Run()
 	exitErr, isExitErr := err.(*exec.ExitError)
 
@@ -411,5 +418,10 @@ func IGT(ctx context.Context, s *testing.State) {
 		s.Error(summary)
 	} else {
 		s.Log(summary)
+	}
+
+	// Start again tlsdated which was stopped earlier.
+	if err := upstart.StartJob(ctx, "tlsdated"); err != nil {
+		s.Fatal("Failed to start tlsdated: ", err)
 	}
 }
