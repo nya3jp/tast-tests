3953cac896d15da652c63ff5f807e833cece94e1
Craig Hesling <hesling@chromium.org>
2021-08-12 19:07:04 -0700

    common: Add RPCDUT
    This is an extension of *dut.DUT that accounts for reconnecting
    a gRPC connection on Reboots.
    
    BUG=b:187795767
    TEST=fast_build.sh -T
    TEST=tast run localhost:2222 example.RPCDUTReboot
    
    Change-Id: I6e39e0c26ff17c56098d5b44529ae8a65d74393b

---
+++ src/chromiumos/tast/remote/bundles/cros/example/rpcdut_reboot.go
@@ -0,0 +1,81 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+	"strconv"
+	"strings"
+
+	"chromiumos/tast/common/rpcdut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/dutfs"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:        RPCDUTReboot,
+		Desc:        "Demonstrates how RPCDUT maintains an RPC connection across DUT reboots",
+		Contacts:    []string{"hesling@chromium.org"},
+		Attr:        []string{"group:mainline", "informational"},
+		ServiceDeps: []string{dutfs.ServiceName},
+	})
+}
+
+func readBootTime(ctx context.Context, d *rpcdut.RPCDUT) (int64, error) {
+	fs := dutfs.NewClient(d.RPC().Conn)
+	bytes, err := fs.ReadFile(ctx, "/proc/stat")
+	if err != nil {
+		return 0, err
+	}
+
+	// Search for "btime 1628806576".
+	for _, line := range strings.Split(string(bytes), "\n") {
+		parts := strings.Split(line, " ")
+		if len(parts) == 2 {
+			if parts[0] == "btime" {
+				return strconv.ParseInt(parts[1], 10, 64)
+			}
+		}
+	}
+
+	return 0, errors.New("failed to find boot time")
+}
+
+// RPCDUTReboot demonstrates how you'd use RPCDUT.
+func RPCDUTReboot(ctx context.Context, s *testing.State) {
+	d, err := rpcdut.NewRPCDUT(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect RPCDUT: ", err)
+	}
+	// We examine the error in this example simply to expose issues with Close.
+	defer func(ctx context.Context) {
+		if err := d.RPCClose(ctx); err != nil {
+			s.Fatal("Failed to close RPCDUT: ", err)
+		}
+	}(ctx)
+
+	bootTimeBefore, err := readBootTime(ctx, d)
+	if err != nil {
+		s.Fatal("Failed to read boot time: ", err)
+	}
+	testing.ContextLog(ctx, "Boot time before reboot: ", bootTimeBefore)
+
+	if err := d.Reboot(ctx); err != nil {
+		s.Fatal("Failed to reboot dut: ", err)
+	}
+
+	bootTimeAfter, err := readBootTime(ctx, d)
+	if err != nil {
+		s.Fatal("Failed to read boot time: ", err)
+	}
+	testing.ContextLog(ctx, "Boot time after reboot: ", bootTimeAfter)
+
+	if bootTimeBefore >= bootTimeAfter {
+		s.Fatal("We failed to reboot")
+	}
+
+}
