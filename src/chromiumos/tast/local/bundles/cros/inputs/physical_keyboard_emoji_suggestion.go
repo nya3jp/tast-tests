eba8ef741eed0f71853882fc0676f76879989671
Jun Sheng <shengjun@google.com>
2021-08-13 19:08:26 +1000

    tast-tests: add test on PK emoji suggestion
    1. Add New function to return OSSettings & IMESettings context.
    2. Add new lib to manage smart input settings.
    3. Use "e14s-test" as the test page path because some features only work on test pages.
    4. Add new test to validate emoji suggestion, including "enabled by default", "disabled" and "re-enabled" scenarios.
    
    BUG=b:196178320
    TEST=tast run <DUT> inputs.PhysicalKeyboardEmojiSuggestion
    
    Change-Id: I6940be91f05ca267b2de4c6aad7b1ac1b25e0464

---
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_emoji_suggestion.go
@@ -0,0 +1,202 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package inputs
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/inputs/pre"
+	"chromiumos/tast/local/bundles/cros/inputs/testserver"
+	"chromiumos/tast/local/bundles/cros/inputs/util"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ime"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/imesettings"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PhysicalKeyboardEmojiSuggestion,
+		Desc:         "Checks emoji suggestions with physical keyboard typing",
+		Contacts:     []string{"shengjun@chromium.org", "essential-inputs-team@google.com"},
+		Attr:         []string{"group:mainline", "group:input-tools", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			ExtraAttr:         []string{"group:input-tools-upstream"},
+			ExtraHardwareDeps: hwdep.D(hwdep.Model(pre.StableModels...)),
+		}, {
+			Name:              "informational",
+			ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
+		}}})
+}
+
+func PhysicalKeyboardEmojiSuggestion(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.ExtraArgs("--force-tablet-mode=clamshell"), chrome.EnableFeatures("EmojiSuggestAddition"), chrome.EnableFeatures("AssistEmojiEnhanced"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome in clamshell mode: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	// PK emoji suggestion only works in English(US).
+	inputMethod := ime.EnglishUS
+
+	// Activate function checks the current IME. It does nothing if the given input method is already in-use.
+	// It is called here just in case IME has been changed in last test.
+	if err := inputMethod.Activate(tconn)(ctx); err != nil {
+		s.Fatal("Failed to set IME: ", err)
+	}
+
+	its, err := testserver.Launch(ctx, cr, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch inputs test server: ", err)
+	}
+	defer its.Close()
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer kb.Close()
+
+	const inputField = testserver.TextInputField
+
+	const (
+		word  = "yum"
+		emoji = "🤤"
+	)
+
+	ui := uiauto.New(tconn)
+
+	emojiCandidateWindowFinder := nodewith.HasClass("SuggestionWindowView").Role(role.Window)
+	emojiCharFinder := nodewith.Name(emoji).Ancestor(emojiCandidateWindowFinder).HasClass("StyledLabel")
+
+	validateEmojiSuggestion := func(shouldSuggest bool) error {
+		return uiauto.Combine("validate emoji suggestion",
+			its.Clear(inputField),
+			its.ClickFieldAndWaitForActive(inputField),
+			kb.TypeAction(word),
+			kb.AccelAction("SPACE"),
+			func(ctx context.Context) error {
+				if shouldSuggest {
+					// Select emoji and wait for the candidate window disappear.
+					return uiauto.Combine("select emoji suggestion",
+						ui.LeftClick(emojiCharFinder),
+						ui.WaitUntilGone(emojiCandidateWindowFinder),
+						util.WaitForFieldTextToBe(tconn, inputField.Finder(), word+" "+emoji),
+					)(ctx)
+				}
+				// Otherwise check emoji suggestion window does not appear in 1s.
+				// Sleep is necessary here, otherwise it immediately returns success because of UI reflection delay.
+				testing.Sleep(ctx, 1*time.Second)
+				return uiauto.Combine("continue to input without emoji suggestion",
+					ui.WaitUntilGone(emojiCandidateWindowFinder),
+					kb.TypeAction(word),
+					util.WaitForFieldTextToBe(tconn, inputField.Finder(), word+" "+word),
+				)(ctx)
+			},
+		)(ctx)
+	}
+
+	testName := "suggestion"
+	s.Run(ctx, testName, func(ctx context.Context, s *testing.State) {
+		defer func(ctx context.Context) {
+			outDir := filepath.Join(s.OutDir(), testName)
+			faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, s.HasError, cr, "ui_tree_"+testName)
+		}(cleanupCtx)
+
+		if err := validateEmojiSuggestion(true); err != nil {
+			s.Fatal("Failed to validate enabled emoji suggestion: ", err)
+		}
+	})
+
+	// Test disabling emoji suggestion.
+	// Launch setting from emoji suggestion window and toggle it off.
+	testName = "disable"
+	s.Run(ctx, testName, func(ctx context.Context, s *testing.State) {
+		defer func(ctx context.Context) {
+			outDir := filepath.Join(s.OutDir(), testName)
+			faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, s.HasError, cr, "ui_tree_"+testName)
+		}(cleanupCtx)
+
+		if err := its.Clear(inputField)(ctx); err != nil {
+			s.Fatalf("Failed to clear input field %s: %v", string(inputField), err)
+		}
+
+		learnMoreFinder := nodewith.Name("Learn more").Ancestor(emojiCandidateWindowFinder).HasClass("ImageButton")
+
+		if err := uiauto.Combine("launch emoji suggestion setting",
+			its.ClickFieldAndWaitForActive(inputField),
+			// Use the first data to test "Learn more".
+			kb.TypeAction(word),
+			kb.AccelAction("SPACE"),
+			ui.LeftClick(learnMoreFinder),
+			ui.WaitUntilGone(emojiCandidateWindowFinder),
+		)(ctx); err != nil {
+			s.Fatal("Failed to validate emoji suggestion: ", err)
+		}
+
+		// User should be on suggestion setting page in OS settings.
+		imeSettings := imesettings.New(tconn)
+
+		if err := uiauto.Combine("toggle off emoji suggestion and close settings app",
+			imeSettings.WaitUntilEmojiSuggestion(cr, tconn, true),
+			imeSettings.ToggleEmojiSuggestions(tconn),
+			imeSettings.WaitUntilEmojiSuggestion(cr, tconn, false),
+			imeSettings.Close(),
+		)(ctx); err != nil {
+			s.Fatal("Failed to toggle off emoji suggestion: ", err)
+		}
+
+		if err := validateEmojiSuggestion(false); err != nil {
+			s.Fatal("Failed to validate disabing emoji suggestion: ", err)
+		}
+	})
+
+	// Test re-enabling emoji suggestion.
+	// Launch setting from OS Settings and toggle it on.
+	testName = "re-enable"
+	s.Run(ctx, testName, func(ctx context.Context, s *testing.State) {
+		defer func(ctx context.Context) {
+			outDir := filepath.Join(s.OutDir(), testName)
+			faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, s.HasError, cr, "ui_tree_"+testName)
+		}(cleanupCtx)
+
+		imeSettings, err := imesettings.LaunchAtSuggestionSettingsPage(ctx, tconn, cr)
+		if err != nil {
+			s.Fatal("Failed to launch OS settings: ", err)
+		}
+		if err := uiauto.Combine("toggle on emoji suggestion and close settings app",
+			imeSettings.ToggleEmojiSuggestions(tconn),
+			imeSettings.WaitUntilEmojiSuggestion(cr, tconn, true),
+			imeSettings.Close(),
+		)(ctx); err != nil {
+			s.Fatal("Failed to toggle on emoji suggestion: ", err)
+		}
+
+		if err := validateEmojiSuggestion(true); err != nil {
+			s.Fatal("Failed to validate re-enabled emoji suggestion: ", err)
+		}
+	})
+}
