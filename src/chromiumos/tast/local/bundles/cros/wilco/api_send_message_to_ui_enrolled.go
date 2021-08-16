60f4fb197d1d706dee097582968f054e0cb17b09
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-08-16 14:02:46 +0000

    tast-tests: Test Wilco DTC SendMessageToUi gRPC
    Local test wilco.APISendMessageToUIEnrolled tests wilco DTC
    SendMessageToUi gRPC method by sending message to DTC VM to
    wilco test extension.
    
    BUG=b:189457904
    TEST=tast run <ip> wilco.APISendMessageToUIEnrolled
    
    Cq-Depend: chromium:3088795
    Change-Id: I4d858c495cc8168f6044c5db047f6339f3ba7347

---
+++ src/chromiumos/tast/local/bundles/cros/wilco/api_send_message_to_ui_enrolled.go
@@ -0,0 +1,99 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wilco
+
+import (
+	"context"
+	"encoding/json"
+
+	"chromiumos/tast/local/bundles/cros/wilco/wilcoextension"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/local/wilco"
+	"chromiumos/tast/testing"
+	dtcpb "chromiumos/wilco_dtc"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: APISendMessageToUIEnrolled,
+		Desc: "Test sending a message from the Wilco DTC VM to the Chromium extension",
+		Contacts: []string{
+			"vsavu@chromium.org", // Test author
+			"lamzin@google.com",  // wilco_dtc_supportd maintainer
+			"chromeos-wilco@google.com",
+			"bisakhmondal00@gmail.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"vm_host", "wilco", "chrome"},
+		Fixture:      "wilcoDTCAllowed",
+	})
+}
+
+// APISendMessageToUIEnrolled tests Wilco DTC SendMessageToUi gRPC API.
+func APISendMessageToUIEnrolled(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+
+	conn, err := wilcoextension.NewConnectionToWilcoExtension(ctx, cr)
+	if err != nil {
+		s.Fatal("Failed to create connection to extension: ", err)
+	}
+	defer conn.Close()
+
+	if err := conn.CreatePort(ctx); err != nil {
+		s.Fatal("Failed to create port to built-in application: ", err)
+	}
+	if err := conn.StartListener(ctx); err != nil {
+		s.Fatal("Failed to start listener: ", err)
+	}
+
+	type testMsg struct {
+		Test int
+	}
+
+	uiResponse := testMsg{
+		Test: 8,
+	}
+
+	if err := conn.AddReply(ctx, &uiResponse); err != nil {
+		s.Fatal("Failed to set reply: ", err)
+	}
+
+	vmRequest := testMsg{
+		Test: 5,
+	}
+	marshaled, err := json.Marshal(vmRequest)
+	if err != nil {
+		s.Fatal("Failed to marshal message: ", err)
+	}
+
+	s.Log("Sending message to extension")
+	request := dtcpb.SendMessageToUiRequest{
+		JsonMessage: string(marshaled),
+	}
+	response := dtcpb.SendMessageToUiResponse{}
+
+	if err := wilco.DPSLSendMessage(ctx, "SendMessageToUi", &request, &response); err != nil {
+		s.Error("Failed to send message to UI: ", err)
+	}
+	var vmResponse testMsg
+	if err := json.Unmarshal([]byte(response.ResponseJsonMessage), &vmResponse); err != nil {
+		s.Logf("Response JSON message: %q", response.ResponseJsonMessage)
+		s.Fatal("Failed to unmarshal message: ", err)
+	}
+
+	if uiResponse != vmResponse {
+		s.Errorf("Unexpected reply received: got %v; want %v", vmResponse, uiResponse)
+	}
+
+	s.Log("Waiting for message")
+	var uiRequest testMsg
+	if err := conn.WaitForMessage(ctx, &uiRequest); err != nil {
+		s.Fatal("Failed to send message using extension: ", err)
+	}
+
+	if vmRequest != uiRequest {
+		s.Errorf("Unexpected request received: got %v; want %v", uiRequest, vmRequest)
+	}
+}
