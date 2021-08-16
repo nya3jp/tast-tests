3338d7884a34e3d989103178d46130c091117481
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-08-16 14:02:46 +0000

    tast-tests: Wilco DTC API Routine tests
    Local tests for wilco DTC RunRoutineRequest and GetRoutineUpdate gRPC
    API. Migration from wilco remote test suite to local tests.
    
    BUG=b:189457904
    TEST=tast run <ip> wilco.APIRoutineEnrolled
    
    Cq-Depend: chromium:3088795
    Change-Id: I8b183807b7c830b24b63655a18627f21e1c58dd6

---
+++ src/chromiumos/tast/remote/bundles/cros/wilco/wilcoextension/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package wilcoextension contains constants and helpers to work with the
+// extension that can interact with the Wilco DTC VM.
+package wilcoextension
+
+// TODO(b/189457904): remove once wilco.APISendMessageToUIEnrolled and
+// wilco.APIHandleMessageFromUIEnrolled will be stable enough.
