0ccdd7a28e44c72c74638e8bc4d4582ff1452f40
Mohamed Omar <mohamedaomar@google.com>
2021-08-18 17:40:07 +0200

    policy: Migrate ui radio button selected to uiauto
    Change the approach of checking selected radio button to new uiauto.
    
    BUG=b:190614781
    TEST=tast run <ip> policy.SafeBrowsingProtectionLevel
    TEST=tast run <ip> policy.SystemTimezone
    TEST=tast run <ip> policy.SystemTimezoneAutomaticDetection
    
    Change-Id: I5612072eb99beddbf61244c1d4c96f4cc24eb0ff

--- src/chromiumos/tast/local/bundles/cros/policy/safe_browsing_protection_level.go
+++ src/chromiumos/tast/local/bundles/cros/policy/safe_browsing_protection_level.go
@@ -6,11 +6,13 @@ package policy
 
 import (
 	"context"
-	"time"
 
 	"chromiumos/tast/common/policy"
-	"chromiumos/tast/local/chrome/ui"
+	"chromiumos/tast/local/chrome/uiauto/checked"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/restriction"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/local/policyutil/fixtures"
 	"chromiumos/tast/testing"
@@ -34,39 +36,33 @@ func SafeBrowsingProtectionLevel(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(*fixtures.FixtData).Chrome
 	fdms := s.FixtValue().(*fixtures.FixtData).FakeDMS
 
-	// Connect to Test API to use it with the UI library.
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to create Test API connection: ", err)
-	}
-
 	for _, param := range []struct {
 		name            string
-		wantRestriction ui.RestrictionState // wantRestriction is the wanted restriction state of the checkboxes in Safe Browsing settings page.
-		selectedOption  string              // selectedOption is the selected safety level in Safe Browsing settings page.
+		wantRestriction restriction.Restriction // wantRestriction is the wanted restriction state of the radio buttons in Safe Browsing settings page.
+		selectedOption  string                  // selectedOption is the selected safety level in Safe Browsing settings page.
 		value           *policy.SafeBrowsingProtectionLevel
 	}{
 		{
 			name:            "unset",
-			wantRestriction: ui.RestrictionNone,
+			wantRestriction: restriction.None,
 			selectedOption:  "Standard protection",
 			value:           &policy.SafeBrowsingProtectionLevel{Stat: policy.StatusUnset},
 		},
 		{
 			name:            "no_protection",
-			wantRestriction: ui.RestrictionDisabled,
+			wantRestriction: restriction.Disabled,
 			selectedOption:  "No protection (not recommended)",
 			value:           &policy.SafeBrowsingProtectionLevel{Val: 0},
 		},
 		{
 			name:            "standard_protection",
-			wantRestriction: ui.RestrictionDisabled,
+			wantRestriction: restriction.Disabled,
 			selectedOption:  "Standard protection",
 			value:           &policy.SafeBrowsingProtectionLevel{Val: 1},
 		},
 		{
 			name:            "enhanced_protection",
-			wantRestriction: ui.RestrictionDisabled,
+			wantRestriction: restriction.Disabled,
 			selectedOption:  "Enhanced protection",
 			value:           &policy.SafeBrowsingProtectionLevel{Val: 2},
 		},
@@ -84,34 +80,15 @@ func SafeBrowsingProtectionLevel(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to update policies: ", err)
 			}
 
-			// Open the security settings page where the affected radio buttons can be found.
-			conn, err := cr.NewConn(ctx, "chrome://settings/security")
-			if err != nil {
-				s.Fatal("Failed to connect to the settings page: ", err)
-			}
-			defer conn.Close()
-
-			// Find the radio group node.
-			rgNode, err := ui.FindWithTimeout(ctx, tconn, ui.FindParams{Role: ui.RoleTypeRadioGroup}, 15*time.Second)
-			if err != nil {
-				s.Fatal("Finding radio group failed: ", err)
-			}
-			defer rgNode.Release(ctx)
-
-			// Find the selected radio button under the radio group.
-			srbNode, err := rgNode.FindSelectedRadioButton(ctx)
-			if err != nil {
-				s.Fatal("Finding the selected radio button failed: ", err)
-			}
-			defer srbNode.Release(ctx)
-
-			if err := policyutil.CheckNodeAttributes(srbNode, ui.FindParams{
-				Attributes: map[string]interface{}{
-					"restriction": param.wantRestriction,
-					"name":        param.selectedOption,
-				},
-			}); err != nil {
-				s.Error("Unexpected settings state: ", err)
+			// Open the security settings page.
+			if err := policyutil.SettingsPage(ctx, cr, "security").
+				SelectNode(ctx, nodewith.
+					Role(role.RadioButton).
+					Name(param.selectedOption)).
+				Checked(checked.True).
+				Restriction(param.wantRestriction).
+				Verify(); err != nil {
+				s.Error("Unexpected OS settings state: ", err)
 			}
 		})
 	}
