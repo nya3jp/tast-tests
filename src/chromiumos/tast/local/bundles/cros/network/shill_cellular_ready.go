a6fdc4d10b7502c8858b787605c6687a88860eec
Pavan Holla <pholla@chromium.org>
2021-08-18 01:13:57 +0000

    cellular: Introduce sim_* subattribute
    We would like to be able to run all tast tests locally using the command
    tast run localhost:2200 '("group:cellular" && "cellular_sim_active")'
    
    Thus, we add the subattribute sim_active to all cellular tests that only
    require a single active sim. We also introduce sim_dual_active to
    identify tests that require an active sim on both slots. This makes
    cellular_multisim redundant, and thus we remove it.
    
    BUG=b:197028297
    TEST=tast run localhost:2200 '("group:cellular" && \
         "cellular_sim_active")'
    
    Cq-Depend: chromium:3101462, chromium:3101936
    Change-Id: I81048eba2c3c557a32cb4f4225c1f30f93dab580

--- src/chromiumos/tast/local/bundles/cros/network/shill_cellular_ready.go
+++ src/chromiumos/tast/local/bundles/cros/network/shill_cellular_ready.go
@@ -22,7 +22,7 @@ func init() {
 			"cros-network-health@google.com",
 			"chromeos-cellular-team@google.com",
 		},
-		Attr:    []string{"group:cellular"},
+		Attr:    []string{"group:cellular", "cellular_sim_active"},
 		Fixture: "cellular",
 	})
 }
