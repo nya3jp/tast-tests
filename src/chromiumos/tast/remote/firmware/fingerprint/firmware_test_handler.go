a6a926203fe87d6866c5594a46b729fc7a9571f9
Craig Hesling <hesling@chromium.org>
2021-08-17 18:34:40 -0700

    fingerprint: Cleanup FirmwareTest building
    BUG=none
    TEST=none
    
    Change-Id: I8e6717586a16ae60384189b2488830064136f7ea

--- src/chromiumos/tast/remote/firmware/fingerprint/firmware_test_handler.go
+++ src/chromiumos/tast/remote/firmware/fingerprint/firmware_test_handler.go
@@ -43,7 +43,7 @@ func NewFirmwareTest(ctx context.Context, d *rpcdut.RPCDUT, servoSpec, outDir st
 
 	t := &FirmwareTest{d: d, servo: pxy}
 
-	daemonState, err := stopDaemons(ctx, t.UpstartService(), []string{
+	t.daemonState, err = stopDaemons(ctx, t.UpstartService(), []string{
 		biodUpstartJobName,
 		// TODO(b/183123775): Remove when bug is fixed.
 		//  Disabling powerd to prevent the display from turning off, which kills
@@ -54,28 +54,28 @@ func NewFirmwareTest(ctx context.Context, d *rpcdut.RPCDUT, servoSpec, outDir st
 		return nil, err
 	}
 
-	fpBoard, err := Board(ctx, t.d)
+	t.fpBoard, err = Board(ctx, t.d)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get fingerprint board")
 	}
 
-	buildFwFile, err := FirmwarePath(ctx, t.d, fpBoard)
+	t.buildFwFile, err = FirmwarePath(ctx, t.d, t.fpBoard)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get build firmware file path")
 	}
 
-	if err := ValidateBuildFwFile(ctx, t.d, fpBoard, buildFwFile); err != nil {
+	if err := ValidateBuildFwFile(ctx, t.d, t.fpBoard, t.buildFwFile); err != nil {
 		return nil, errors.Wrap(err, "failed to validate build firmware file")
 	}
 
-	needsReboot, err := NeedsRebootAfterFlashing(ctx, t.d)
+	t.needsRebootAfterFlashing, err = NeedsRebootAfterFlashing(ctx, t.d)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to determine if reboot is needed")
 	}
 
-	cleanupTime := timeForCleanup
+	t.cleanupTime = timeForCleanup
 
-	if needsReboot {
+	if t.needsRebootAfterFlashing {
 		upstartService := t.UpstartService()
 		// Rootfs must be writable in order to disable the upstart job
 		if err := sysutil.MakeRootfsWritable(ctx, t.d.DUT, t.d.RPCHint()); err != nil {
@@ -94,11 +94,11 @@ func NewFirmwareTest(ctx context.Context, d *rpcdut.RPCDUT, servoSpec, outDir st
 		}
 
 		// Account for the additional time that rebooting adds.
-		cleanupTime += 3 * time.Minute
+		t.cleanupTime += 3 * time.Minute
 	} else {
 		// If we're not on a device that needs to be rebooted, the rootfs should not
 		// be writable.
-		rootfsIsWritable, err := sysutil.IsRootfsWritable(ctx, t.d.RPC())
+		rootfsIsWritable, err := sysutil.IsRootfsWritable(ctx, d.RPC())
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to check if rootfs is writable")
 		}
@@ -107,20 +107,9 @@ func NewFirmwareTest(ctx context.Context, d *rpcdut.RPCDUT, servoSpec, outDir st
 		}
 	}
 
-	dutTempDir, err := t.DutfsClient().TempDir(ctx, "", dutTempPathPattern)
+	t.dutTempDir, err = t.DutfsClient().TempDir(ctx, "", dutTempPathPattern)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create remote working directory")
-	}
-
-	t = &FirmwareTest{
-		d:                        t.d,
-		servo:                    t.servo,
-		fpBoard:                  fpBoard,
-		buildFwFile:              buildFwFile,
-		daemonState:              daemonState,
-		needsRebootAfterFlashing: needsReboot,
-		cleanupTime:              cleanupTime,
-		dutTempDir:               dutTempDir,
 	}
 
 	/*  Prepare the FPMCU FW */
@@ -139,7 +128,7 @@ func NewFirmwareTest(ctx context.Context, d *rpcdut.RPCDUT, servoSpec, outDir st
 	}
 
 	// Check FPMCU state and reflash if needed.
-	if err := InitializeKnownState(ctx, t.d, outDir, pxy, fpBoard, buildFwFile, needsReboot); err != nil {
+	if err := InitializeKnownState(ctx, t.d, outDir, t.servo, t.fpBoard, t.buildFwFile, t.needsRebootAfterFlashing); err != nil {
 		return nil, errors.Wrap(err, "initializing known state failed")
 	}
 
