22a72a852b88f7f9af182a35f5da92f8a013b9c9
Trent Begin <tbegin@google.com>
2021-08-18 16:52:04 -0600

    network.Diag: enable stable tests
    BUG=chromium:1178896
    TEST=none
    
    Change-Id: Icb0e33bf322a4c5af7b584f6077f72c703d8b4b6

--- src/chromiumos/tast/local/bundles/cros/network/diag_fail_dns_resolution.go
+++ src/chromiumos/tast/local/bundles/cros/network/diag_fail_dns_resolution.go
@@ -26,7 +26,7 @@ func init() {
 			"cros-network-health@google.com", // network-health team
 		},
 		SoftwareDeps: []string{"chrome"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		Fixture:      "networkDiagnosticsShillReset",
 	})
 }
