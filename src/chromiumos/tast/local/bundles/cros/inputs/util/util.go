6b8db0b1f32b0f3c254c261b73024ac353b6e1da
Jun Sheng <shengjun@google.com>
2021-08-20 13:24:42 +1000

    tast-tests: add new test on glide typing
    1. Add new test to validate glide typing.
    2. Add new function SelectFromSuggestionIgnoringCase.
    
    BUG=b:190653397
    TEST=tast run <DUT> inputs.VirtualKeyboardGlideTyping*
    
    Change-Id: Iba8f756dc91d18bb9c3fbe541d7476580bedfac5

--- src/chromiumos/tast/local/bundles/cros/inputs/util/util.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/util/util.go
@@ -7,16 +7,21 @@ package util
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 	"strings"
 	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/inputs/data"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ime"
 	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/chrome/uiauto/touch"
+	"chromiumos/tast/local/chrome/uiauto/vkb"
 	"chromiumos/tast/testing"
 )
 
@@ -158,4 +163,57 @@ func ExtractExternalFilesFromMap(messages map[InputModality]data.Message, inputM
 		messageList = append(messageList, message)
 	}
 	return data.ExtractExternalFiles(messageList, inputMethods)
+}
+
+// GlideTyping returns an action to simulate glide typing on virtual keyboard.
+// It works on both tablet VK and A11y VK.
+func GlideTyping(tconn *chrome.TestConn, keys []string) uiauto.Action {
+	return func(ctx context.Context) error {
+		if len(keys) < 2 {
+			return errors.New("glide typing only works on multiple keys")
+		}
+
+		touchCtx, err := touch.New(ctx, tconn)
+		if err != nil {
+			return errors.Wrap(err, "fail to get touch screen")
+		}
+		defer touchCtx.Close()
+
+		ui := uiauto.New(tconn)
+
+		initKeyLoc, err := ui.Location(ctx, vkb.KeyByNameIgnoringCase(keys[0]))
+		if err != nil {
+			return errors.Wrap(err, "fail to find the location of first key")
+		}
+
+		var gestures []uiauto.Action
+		for i := 1; i < len(keys); i++ {
+			// Perform a swipe in 50ms and stop 200ms on each key.
+			gestures = append(gestures, ui.Sleep(200*time.Millisecond))
+			if keys[i] == keys[i-1] {
+				keyLoc, err := ui.Location(ctx, vkb.KeyByNameIgnoringCase(keys[i]))
+				if err != nil {
+					return errors.Wrapf(err, "fail to find the location of key: %q", keys[i])
+				}
+				gestures = append(gestures, touchCtx.SwipeTo(keyLoc.TopLeft(), 50*time.Millisecond))
+			}
+			gestures = append(gestures, touchCtx.SwipeToNode(vkb.KeyByNameIgnoringCase(keys[i]), 50*time.Millisecond))
+		}
+		return touchCtx.Swipe(initKeyLoc.CenterPoint(), gestures...)(ctx)
+	}
+}
+
+// RunSubTest is designed to run an action as a subtest.
+// It reserves 5s for general cleanup, dumping ui tree and screenshot on error.
+func RunSubTest(ctx context.Context, s *testing.State, cr *chrome.Chrome, testName string, action uiauto.Action) {
+	s.Run(ctx, testName, func(ctx context.Context, s *testing.State) {
+		cleanupCtx := ctx
+		ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+		defer cancel()
+		defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, filepath.Join(s.OutDir(), testName), s.HasError, cr, "ui_tree_"+testName)
+
+		if err := action(ctx); err != nil {
+			s.Fatalf("Subtest %q failed: %v", testName, err)
+		}
+	})
 }
