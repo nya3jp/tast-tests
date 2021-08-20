2be6e0706aaccfe4fc98233b7b9c6c592f503fe7
Jun Sheng <shengjun@google.com>
2021-08-20 14:09:44 +1000

    tast-tests: add new test on PK caps lock
    Add new test to check that user can lock and unlock Caps on physical keyboard.
    
    BUG=None
    TEST=tast run <DUT> inputs.PhysicalKeyboardCapsLock*
    
    Change-Id: I1877c37a04d66d1fcde86087f6c9cb58d13fb9ba

---
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_caps_lock.go
@@ -0,0 +1,70 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package inputs
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/inputs/pre"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PhysicalKeyboardCapsLock,
+		Desc:         "Checks that user can lock Caps on physical keyboard",
+		Contacts:     []string{"shengjun@chromium.org", "essential-inputs-team@google.com"},
+		Attr:         []string{"group:mainline", "group:input-tools", "informational"},
+		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
+		Timeout:      2 * time.Minute,
+		Params: []testing.Param{{
+			Pre:               pre.NonVKClamshellReset,
+			ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
+			ExtraAttr:         []string{"group:input-tools-upstream"},
+		}, {
+			Name:              "informational",
+			Pre:               pre.NonVKClamshellReset,
+			ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
+		}},
+	})
+}
+
+func PhysicalKeyboardCapsLock(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(pre.PreData).Chrome
+	tconn := s.PreValue().(pre.PreData).TestAPIConn
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree")
+
+	keyboard, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer keyboard.Close()
+
+	ui := uiauto.New(tconn)
+	capsOnImageFinder := nodewith.Name("CAPS LOCK is on").Role(role.Image)
+
+	// TODO(b/196771467) Validate typing after changing caps lock.
+	if err := uiauto.Combine("caps lock and unlock",
+		keyboard.AccelAction("Alt+Search"),
+		ui.WaitUntilExists(capsOnImageFinder),
+		keyboard.AccelAction("Shift"),
+		ui.WaitUntilGone(capsOnImageFinder),
+	)(ctx); err != nil {
+		s.Fatal("Failed to validate caps lock: ", err)
+	}
+}
