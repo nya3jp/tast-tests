23c1d7fd7d4175308a96ad162f9d94c0ca2d4518
srok <srok@google.com>
2021-08-24 21:39:20 +0000

    Add ARC surface orientation tast test stub
    BUG=b:157503221
    TEST=Test passes
    
    Change-Id: I22089e18bffb49d6ae6989ef9158968eb1af84ac

---
+++ android/ArcSurfaceOrientationTest/src/org/chromium/arc/testapp/surfaceorientation/MainActivity.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.surfaceorientation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ src/chromiumos/tast/local/bundles/cros/arc/surface_orientation.go
@@ -0,0 +1,69 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SurfaceOrientation,
+		Desc:         "Test the various orientations of the display surface",
+		Contacts:     []string{"srok@google.com", "arc-framework+tast@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      4 * time.Minute,
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_vm_r"},
+		}},
+	})
+}
+
+func SurfaceOrientation(ctx context.Context, s *testing.State) {
+
+	// TODO(srok): Create this APK
+	const (
+		apk = "ArcSurfaceOrientationTest.apk"
+		pkg = "org.chromium.arc.testapp.surfaceorientation"
+		cls = ".MainActivity"
+	)
+
+	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs("--force-tablet-mode=clamshell"))
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to create ARC instance: ", err)
+	}
+	defer a.Close(ctx)
+
+	if err := a.Install(ctx, arc.APKPath(apk)); err != nil {
+		s.Fatal("Failed installing app: ", err)
+	}
+
+	act, err := arc.NewActivity(a, pkg, cls)
+	if err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx, tconn); err != nil {
+		s.Fatal("Failed to start activity: ", err)
+	}
+}
