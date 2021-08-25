b26522e857509a42fcb63d4491dc379f7612219c
David Welling <davidwelling@google.com>
2021-08-25 21:20:08 +0000

    arcappcompat: Add standardized touchscreen zoom test
    Update the Android application to handle zooming in, and zooming out on
    an element and add a corresponding test to handle the zoom functionality
    on the touchscreen. Additionally, added helper methods in the touch
    input file to support pathing between two points, and moving multiple
    touches at the same time.
    
    BUG=b:194900373
    TEST=push apk && tast run ${HOST} arc.StandardizedTouch*
    
    Change-Id: I2491fe78419ed1a18f832aacd3a0332d5ebccb37

---
+++ android/ArcStandardizedTouchscreenTest/src/org/chromium/arc/testapp/arcstandardizedtouchscreentest/ZoomGestureListener.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.arcstandardizedtouchscreentest;
+
+import android.view.ScaleGestureDetector;
+
+class ZoomGestureListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+    OnZoomListener mZoomInListener;
+    OnZoomListener mZoomOutListener;
+    OnZoomListener mZoomDebugListener;
+
+    public ZoomGestureListener(
+            OnZoomListener zoomInListener,
+            OnZoomListener zoomOutListener,
+            OnZoomListener zoomDebugListener) {
+        mZoomInListener = zoomInListener;
+        mZoomOutListener = zoomOutListener;
+        mZoomDebugListener = zoomDebugListener;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+        // Always fire the debug listener.
+        mZoomDebugListener.zoom(detector.getScaleFactor());
+
+        // Only fire the actual events if a change occurred.
+        if (detector.getScaleFactor() > 1) {
+            mZoomInListener.zoom(detector.getScaleFactor());
+        } else if (detector.getScaleFactor() < 1) {
+            mZoomOutListener.zoom(detector.getScaleFactor());
+        }
+    }
+}
