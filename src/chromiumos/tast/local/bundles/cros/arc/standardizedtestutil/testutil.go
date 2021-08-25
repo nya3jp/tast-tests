4f50b2e08f098c68cc2ddbf7096297568e5bceb6
David Welling <davidwelling@google.com>
2021-08-25 21:20:08 +0000

    arcappcompat: Add standardized touchscreen zoom test
    Update the Android application to handle zooming in, and zooming out on
    an element and add a corresponding test to handle the zoom functionality
    on the touchscreen. Additionally, added helper methods in the touch
    input file to support pathing between two points, and moving multiple
    touches at the same time.
    
    BUG=b:194900373
    TEST=push apk && tast run ${HOST} arc.StandardizedTouch*
    
    Change-Id: I2491fe78419ed1a18f832aacd3a0332d5ebccb37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3119707
    Tested-by: David Welling <davidwelling@google.com>
    Commit-Queue: David Welling <davidwelling@google.com>
    Reviewed-by: Kamil Szarek <kamilszarek@google.com>

--- src/chromiumos/tast/local/bundles/cros/arc/standardizedtestutil/testutil.go
+++ src/chromiumos/tast/local/bundles/cros/arc/standardizedtestutil/testutil.go
@@ -62,6 +62,15 @@ type StandardizedTestCase struct {
 	WindowStateType ash.WindowStateType
 }
 
+// StandardizedTouchscreenZoomType represents the touchscreen zoom type to perform.
+type StandardizedTouchscreenZoomType int
+
+// Holds all of the zoom types that can be performed on the touchscreen.
+const (
+	TouchscreenZoomIn StandardizedTouchscreenZoomType = iota
+	TouchscreenZoomOut
+)
+
 // StandardizedTouchscreenTapType represents the touch screen tap type to perform.
 type StandardizedTouchscreenTapType int
 
@@ -272,6 +281,54 @@ func StandardizedTouchscreenScroll(ctx context.Context, touchScreen *input.Touch
 
 	if err := touchScreenSingleEventWriter.End(); err != nil {
 		return errors.Wrap(err, "unable to end the scroll")
+	}
+
+	return nil
+}
+
+// StandardizedTouchscreenZoom performs a zoom on the touchscreen. Zoom in distance
+// varies per device but the function aims to zoom by 2x (i.e. a scale factor of 2.0
+// when zooming in, or .5 when zooming out).
+func StandardizedTouchscreenZoom(ctx context.Context, touchScreen *input.TouchscreenEventWriter, testParameters StandardizedTestFuncParams, selector *ui.Object, zoomType StandardizedTouchscreenZoomType) error {
+	const (
+		zoomDistancePerFinger = 600
+		zoomDuration          = 500 * time.Millisecond
+	)
+
+	// Zoom is implemented as a two finger pinch so it requires two touches.
+	mtw, err := touchScreen.NewMultiTouchWriter(2)
+	if err != nil {
+		return errors.Wrap(err, "unable to initialize event writer")
+	}
+	defer mtw.Close()
+
+	// Get the coordinates of the element to perform the zoom gesture on.
+	x, y, err := getTouchEventCoordinatesForElement(ctx, testParameters, touchScreen, selector)
+	if err != nil {
+		return errors.Wrap(err, "unable to get start coordinates")
+	}
+
+	// Perform the appropriate zoom.
+	switch zoomType {
+	case TouchscreenZoomIn:
+		if err := mtw.ZoomIn(ctx, *x, *y, zoomDistancePerFinger, zoomDuration); err != nil {
+			return errors.Wrap(err, "unable to zoom in")
+		}
+
+		break
+	case TouchscreenZoomOut:
+		if err := mtw.ZoomOut(ctx, *x, *y, zoomDistancePerFinger, zoomDuration); err != nil {
+			return errors.Wrap(err, "unable to zoom in")
+		}
+
+		break
+	default:
+		return errors.Errorf("invalid zoom type provided: %v", zoomType)
+	}
+
+	// End the gesture.
+	if err := mtw.End(); err != nil {
+		return errors.Wrap(err, "unable to end the zoom in")
 	}
 
 	return nil
