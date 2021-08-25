b409d0ee324f518679fedca98d21eb0e0eb034c3
David Welling <davidwelling@google.com>
2021-08-25 21:20:08 +0000

    arcappcompat: Add standardized touchscreen zoom test
    Update the Android application to handle zooming in, and zooming out on
    an element and add a corresponding test to handle the zoom functionality
    on the touchscreen. Additionally, added helper methods in the touch
    input file to support pathing between two points, and moving multiple
    touches at the same time.
    
    BUG=b:194900373
    TEST=push apk && tast run ${HOST} arc.StandardizedTouch*
    
    Change-Id: I2491fe78419ed1a18f832aacd3a0332d5ebccb37

---
+++ android/ArcStandardizedTouchscreenTest/src/org/chromium/arc/testapp/arcstandardizedtouchscreentest/ZoomTestActivity.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.arcstandardizedtouchscreentest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ScaleGestureDetector;
+import android.widget.TextView;
+
+public class ZoomTestActivity extends Activity {
+    private ScaleGestureDetector mZoomDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoom_test);
+
+        // Setup the zoom detector.
+        TextView txtZoom = findViewById(R.id.txtZoom);
+        mZoomDetector =
+                new ScaleGestureDetector(
+                        txtZoom.getContext(),
+                        new ZoomGestureListener(
+                                scaleFactor -> {
+                                    TextView txtZoomInState = findViewById(R.id.txtZoomInState);
+                                    txtZoomInState.setText("ZOOM IN: COMPLETE");
+                                },
+                                scaleFactor -> {
+                                    TextView txtZoomOutState = findViewById(R.id.txtZoomOutState);
+                                    txtZoomOutState.setText("ZOOM OUT: COMPLETE");
+                                },
+                                scaleFactor -> {
+                                    TextView txtDebugPreviousZoom =
+                                            findViewById(R.id.txtDebugPreviousZoom);
+                                    txtDebugPreviousZoom.setText(
+                                            String.format(
+                                                    "DEBUG PREVIOUS ZOOM: SCALE FACTOR: %s",
+                                                    scaleFactor));
+                                }));
+
+        // Send touch events to the zoom detector.
+        txtZoom.setOnTouchListener(
+                (v, event) -> {
+                    mZoomDetector.onTouchEvent(event);
+                    return true;
+                });
+    }
+}
