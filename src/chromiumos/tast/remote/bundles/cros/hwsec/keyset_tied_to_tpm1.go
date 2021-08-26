5eff0bf394fbcba66870e695a0202d5d4d2231ae
Howard Yang <hcyang@google.com>
2021-08-26 18:20:33 +0800

    hwsec: test login method with LECredential in LockToSingleUserMountUntilReboot
    Currently the test does not exercise the login method with LECredential.
    Add one more key that uses LECredential (PIN) for both users, and verify
    the same test logics work on LECredential too. On devices that do not
    support PinWeaer, only test the first and second key (which is same as
    the current test).
    
    Also, the constants FirstPin/SecondPin/PinLabel are currently used as a
    second set of password instead of PINS. Add constants
    FirstPassword2/SecondPassword2/Password2Label to replace the original usages,
    and use the pin-related constants as PINs. Fix related comments/messages.
    
    BUG=b:192822451
    TEST=tast run $DUT hwsec.*
    
    Change-Id: I4dfb5ecf61f96ac93f04a7ba287b2d1b91f771ed

--- src/chromiumos/tast/remote/bundles/cros/hwsec/keyset_tied_to_tpm1.go
+++ src/chromiumos/tast/remote/bundles/cros/hwsec/keyset_tied_to_tpm1.go
@@ -42,7 +42,7 @@ func loginTakeOwnershipAndCheckKeysetTiedToTPM(ctx context.Context, s *testing.S
 	s.Log("TPM is confirmed to be reset")
 
 	// Login+Logout.
-	if err := utility.MountVault(ctx, util.FirstUsername, util.FirstPassword, util.PasswordLabel, true, hwsec.NewVaultConfig()); err != nil {
+	if err := utility.MountVault(ctx, util.FirstUsername, util.FirstPassword1, util.Password1Label, true, hwsec.NewVaultConfig()); err != nil {
 		s.Fatal("Failed to create user vault when testing without reboot: ", err)
 	}
 	defer func() {
@@ -71,7 +71,7 @@ func loginTakeOwnershipAndCheckKeysetTiedToTPM(ctx context.Context, s *testing.S
 	}
 
 	// Login again.
-	if err := utility.MountVault(ctx, util.FirstUsername, util.FirstPassword, util.PasswordLabel, false, hwsec.NewVaultConfig()); err != nil {
+	if err := utility.MountVault(ctx, util.FirstUsername, util.FirstPassword1, util.Password1Label, false, hwsec.NewVaultConfig()); err != nil {
 		s.Fatal("Failed to login when testing without reboot: ", err)
 	}
 
