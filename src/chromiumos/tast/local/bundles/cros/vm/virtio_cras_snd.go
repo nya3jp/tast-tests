f0dbfab0d27c6587eb4db86b0dc6bfff90855ee7
Woody Chow <woodychow@google.com>
2021-08-26 14:10:44 +0900

    tast-tests: Add vm.VirtioCrasSnd
    to test the virtio CRAS sound device in crosvm.
    
    This runs the Alsa Conformance Test within the vm.
    
    BUG=b:195267672
    TEST=tast run $DUT_IP vm.VirtioCrasSnd
    
    Change-Id: If026bc1aca24af6c7ad17229749bdc0f62c2a38e

---
+++ src/chromiumos/tast/local/bundles/cros/vm/virtio_cras_snd.go
@@ -0,0 +1,109 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package vm
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/local/bundles/cros/vm/dlc"
+	"chromiumos/tast/testing"
+)
+
+const runAlsaConformanceTest string = "run-alsa-conformance-test.sh"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VirtioCrasSnd,
+		Desc:         "Tests that the crosvm CRAS virtio-snd device works correctly",
+		Contacts:     []string{"woodychow@google.com", "crosvm-core@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		Data:         []string{runAlsaConformanceTest},
+		Timeout:      20 * time.Minute,
+		SoftwareDeps: []string{"vm_host", "dlc"},
+		Fixture:      "vmDLC",
+	})
+}
+
+func VirtioCrasSnd(ctx context.Context, s *testing.State) {
+	// Create a temporary directory on the stateful partition rather than in memory.
+	td, err := ioutil.TempDir("/usr/local/tmp", "tast.vm.VirtioCrasSnd.")
+	if err != nil {
+		s.Fatal("Failed to create temporary directory: ", err)
+	}
+	defer os.RemoveAll(td)
+
+	// The test needs the execute bit set on every component in the test directory
+	// in order for rename(2) as a non-root user to succeed.
+	if err := os.Chmod(td, 0755); err != nil {
+		s.Fatal("Failed to change permissions on temporary directory: ", err)
+	}
+
+	data := s.FixtValue().(dlc.FixtData)
+	crosvmLogPath := filepath.Join(s.OutDir(), "crosvm.log")
+	kernelLogPath := filepath.Join(s.OutDir(), "kernel.log")
+
+	params := []string{
+		"root=/dev/root",
+		"rootfstype=virtiofs",
+		"rw",
+		fmt.Sprintf("init=%s", s.DataPath(runAlsaConformanceTest)),
+		"--",
+		td,
+	}
+
+	// The sandbox needs to be disabled because the test creates some device nodes, which is
+	// only possible when running as root in the initial namespace.
+	args := []string{
+		"run",
+		"-p", strings.Join(params, " "),
+		"-c", "1",
+		"-m", "256",
+		"-s", td,
+		"--serial", fmt.Sprintf("type=file,num=1,console=true,path=%s", kernelLogPath),
+		"--shared-dir", "/:/dev/root:type=fs:cache=always",
+		"--cras-snd",
+		"--disable-sandbox",
+		data.Kernel,
+	}
+
+	output, err := os.Create(crosvmLogPath)
+	if err != nil {
+		s.Fatal("Failed to create crosvm log file: ", err)
+	}
+	defer output.Close()
+
+	printLog := func() {
+		crosvmLog, err := ioutil.ReadFile(crosvmLogPath)
+		if err != nil {
+			s.Fatal("Failed to read serial log: ", err)
+		}
+		s.Log(string(crosvmLog))
+
+		kernelLog, err := ioutil.ReadFile(kernelLogPath)
+		if err != nil {
+			s.Fatal("Failed to read serial log: ", err)
+		}
+		s.Log(string(kernelLog))
+	}
+
+	s.Log("Running Alsa conformance test")
+	cmd := testexec.CommandContext(ctx, "crosvm", args...)
+	cmd.Stdout = output
+	cmd.Stderr = output
+
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		printLog()
+		s.Fatal("Failed to run crosvm: ", err)
+	}
+
+	printLog()
+}
