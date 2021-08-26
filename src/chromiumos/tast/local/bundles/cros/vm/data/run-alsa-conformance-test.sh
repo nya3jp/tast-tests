db68727ff682545874956b621c6e0540a9199391
Woody Chow <woodychow@google.com>
2021-08-26 14:10:44 +0900

    tast-tests: Add vm.AudioAlsaConformance.{virtio_cras_snd/ac97}
    to test the sound devices in crosvm.
    
    This runs the Alsa Conformance Test within the vm.
    
    BUG=b:195267672
    TEST=tast run $DUT_IP vm.AudioAlsaConformance.*
    
    Cq-Depend: chromium:3141086, chromium:3153087
    Change-Id: If026bc1aca24af6c7ad17229749bdc0f62c2a38e

---
+++ src/chromiumos/tast/local/bundles/cros/vm/data/run-alsa-conformance-test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This script is meant to be run as PID 1 inside a VM.
+set -ex
+
+die() {
+  echo "$1"
+  exit 1
+}
+
+usage() {
+  die "Usage: $(basename "$0")"
+}
+
+main() {
+  [[ "$$" == "1" ]] || die "Not runnnig as PID 1"
+
+  [[ $# -eq 2 ]] || \
+      die "Usage: $(basename "$0") <playback_log> <capture_log>"
+
+  local playback_log="$1"
+  local capture_log="$2"
+
+  # All the sample rates supported by virtio-snd
+  local rates=(5512 8000 11025 16000 22050 32000 44100 48000 64000 88200 96000
+      176400 192000 384000)
+
+  export PYTHONHOME=/usr/local
+  export PATH=${PATH}:/usr/local/bin
+  alsa_conformance_test.py \
+      --test-suites test_params test_rates test_all_pairs \
+      -P hw:0,0 --rate-criteria-diff-pct 0.1 \
+      --json-file "${playback_log}" \
+      --allow-rates "${rates[@]}"
+  alsa_conformance_test.py \
+      --test-suites test_params test_rates test_all_pairs \
+      -C hw:0,0 --rate-criteria-diff-pct 0.1 \
+      --json-file "${capture_log}" \
+      --allow-rates "${rates[@]}"
+}
+
+main "$@"
