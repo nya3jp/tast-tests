902c84a0e131305ad43d40bd9b0bdba3d428a92e
Kamil Szarek <kamilszarek@google.com>
2021-08-27 12:50:00 +0000

    policy: Add Kerberos tests
    Add Kerberos tests accessing website and file system protected by
    Kerberos. Test use two different ways to authenticate - using defined
    Kerberos account and integrated authentication.
    
    BUG=None
    TEST=tast run <ip> policy.Kerberos*
    
    Change-Id: I1209ff0d40985391db4f0e723270ec358f3e7519

---
+++ src/chromiumos/tast/local/bundles/cros/policy/kerberos_account_website.go
@@ -0,0 +1,104 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/kerberos"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: KerberosAccountWebsite,
+		Desc: "Checks the behavior of accessing website secured with kerberos using Kerberos account configuration",
+		Contacts: []string{
+			"kamilszarek@google.com", // Test author
+			"alexanderhartl@google.com",
+			"chromeos-commercial-remote-management@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      "chromePolicyLoggedIn",
+	})
+}
+
+func KerberosAccountWebsite(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+	fdms := s.FixtValue().(*fixtures.FixtData).FakeDMS
+
+	// Connect to Test API to use it with the UI library.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_kerberos_account")
+
+	// Perform cleanup.
+	if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+		s.Fatal("Failed to clean up: ", err)
+	}
+
+	kerberosAcc := policy.KerberosAccountsValue{
+		Principal: kerberos.KerberosUser,
+		Password:  kerberos.KerberosUserPass,
+		Krb5conf:  []string{},
+	}
+
+	// Update policies
+	if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{
+		&policy.KerberosEnabled{Val: true},
+		&policy.KerberosAccounts{
+			Val: []*policy.KerberosAccountsValue{
+				&kerberosAcc,
+			},
+		},
+	}); err != nil {
+		s.Fatal("Failed to update policies: ", err)
+	}
+
+	_, err = apps.LaunchOSSettings(ctx, cr, "chrome://os-settings/kerberos")
+	if err != nil {
+		s.Fatal("Could not open Kerberos section in OS settings: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	// Open Kerberos tickets section.
+	if err := ui.LeftClick(nodewith.Name("Kerberos tickets").Role(role.Link))(ctx); err != nil {
+		s.Fatal("Failed to open Kerberos tickets section: ", err)
+	}
+
+	// Wait for ticket to appear.
+	testing.ContextLog(ctx, "Waiting for Kerberos ticket to appear")
+	if err := ui.WithTimeout(10 * time.Second).WaitUntilExists(nodewith.NameStartingWith("croskertest@KER").Role(role.StaticText))(ctx); err != nil {
+		s.Fatal("Failed to see added Kerberos: ", err)
+	}
+
+	// Check that ticket is active.
+	if err := ui.Exists(nodewith.Name("Active").Role(role.StaticText))(ctx); err != nil {
+		s.Fatal("Kerberos ticket was not in Active state")
+	}
+
+	conn, err := cr.NewConn(ctx, kerberos.WebsiteAddress)
+	if err != nil {
+		s.Fatal("Failed to connect to chrome: ", err)
+	}
+	defer conn.Close()
+	if err := conn.Navigate(ctx, kerberos.WebsiteAddress); err != nil {
+		s.Fatalf("Failed to navigate to the server URL %q: %v", kerberos.WebsiteAddress, err)
+	}
+}
