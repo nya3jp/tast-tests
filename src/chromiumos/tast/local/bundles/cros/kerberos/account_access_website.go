c85183977896f670b146979359686c7a0041e060
Kamil Szarek <kamilszarek@google.com>
2021-08-27 12:50:00 +0000

    kerberos: Add Kerberos tests
    Add Kerberos tests accessing website and file system protected by
    Kerberos. Test use two different ways to authenticate - using defined
    Kerberos account and integrated authentication.
    
    BUG=None
    TEST=tast run <ip> kerberos.*
    
    Change-Id: I1209ff0d40985391db4f0e723270ec358f3e7519

---
+++ src/chromiumos/tast/local/bundles/cros/kerberos/account_access_website.go
@@ -0,0 +1,110 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package kerberos
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/kerberos"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AccountAccessWebsite,
+		Desc: "Checks the behavior of accessing website secured with kerberos using Kerberos account configuration",
+		Contacts: []string{
+			"kamilszarek@google.com", // Test author
+			"alexanderhartl@google.com",
+			"chromeos-commercial-remote-management@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		VarDeps:      []string{"kerberos.username", "kerberos.password", "kerberos.domain"},
+		Fixture:      "chromePolicyLoggedIn",
+	})
+}
+
+func AccountAccessWebsite(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+	fdms := s.FixtValue().(*fixtures.FixtData).FakeDMS
+	username := s.RequiredVar("kerberos.username")
+	password := s.RequiredVar("kerberos.password")
+	domain := s.RequiredVar("kerberos.domain")
+	config := kerberos.ConstructConfig(domain, username)
+
+	// Connect to Test API to use it with the UI library.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_kerberos_account")
+
+	// Perform cleanup.
+	if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+		s.Fatal("Failed to clean up: ", err)
+	}
+
+	kerberosAcc := policy.KerberosAccountsValue{
+		Principal: username,
+		Password:  password,
+		Krb5conf:  []string{},
+	}
+
+	// Update policies - changed to Refresh rather to verify as verify yielded
+	// error or policy mismatch.
+	if err := policyutil.ServeAndRefresh(ctx, fdms, cr, []policy.Policy{
+		&policy.KerberosEnabled{Val: true},
+		&policy.KerberosAccounts{
+			Val: []*policy.KerberosAccountsValue{
+				&kerberosAcc,
+			},
+		},
+	}); err != nil {
+		s.Fatal("Failed to update policies: ", err)
+	}
+
+	_, err = apps.LaunchOSSettings(ctx, cr, "chrome://os-settings/kerberos")
+	if err != nil {
+		s.Fatal("Could not open Kerberos section in OS settings: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	// Open Kerberos tickets section.
+	if err := ui.LeftClick(nodewith.Name("Kerberos tickets").Role(role.Link))(ctx); err != nil {
+		s.Fatal("Failed to open Kerberos tickets section: ", err)
+	}
+
+	// Wait for ticket to appear.
+	testing.ContextLog(ctx, "Waiting for Kerberos ticket to appear")
+	if err := ui.WithTimeout(10 * time.Second).WaitUntilExists(nodewith.NameStartingWith(username).Role(role.StaticText))(ctx); err != nil {
+		s.Fatal("Failed to see added Kerberos: ", err)
+	}
+
+	// Check that ticket is active.
+	if err := ui.Exists(nodewith.Name("Active").Role(role.StaticText))(ctx); err != nil {
+		s.Fatal("Kerberos ticket was not in Active state")
+	}
+
+	conn, err := cr.NewConn(ctx, config.WebsiteAddress)
+	if err != nil {
+		s.Fatal("Failed to connect to chrome: ", err)
+	}
+	defer conn.Close()
+	if err := conn.Navigate(ctx, config.WebsiteAddress); err != nil {
+		s.Fatalf("Failed to navigate to the server URL %q: %v", config.WebsiteAddress, err)
+	}
+}
