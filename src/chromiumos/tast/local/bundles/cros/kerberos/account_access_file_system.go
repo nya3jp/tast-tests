e6a96666d2f6c44b0531fff92beb2b69e9eda964
Kamil Szarek <kamilszarek@google.com>
2021-08-27 12:50:00 +0000

    kerberos: Add Kerberos tests
    Add Kerberos tests accessing website and file system protected by
    Kerberos. Test use two different ways to authenticate - using defined
    Kerberos account and integrated authentication.
    
    BUG=None
    TEST=tast run <ip> kerberos.*
    
    Cq-Depend: chrome-internal:4114491
    Change-Id: I1209ff0d40985391db4f0e723270ec358f3e7519

---
+++ src/chromiumos/tast/local/bundles/cros/kerberos/account_access_file_system.go
@@ -0,0 +1,134 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package kerberos
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/kerberos"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: AccountAccessFileSystem,
+		Desc: "Checks the behavior of accessing file system secured with Kerberos using Kerberos account",
+		Contacts: []string{
+			"kamilszarek@google.com", // Test author
+			"alexanderhartl@google.com",
+			"chromeos-commercial-remote-management@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		VarDeps:      []string{"kerberos.username", "kerberos.password", "kerberos.domain"},
+		Fixture:      "chromePolicyLoggedIn",
+	})
+}
+
+func AccountAccessFileSystem(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+	fdms := s.FixtValue().(*fixtures.FixtData).FakeDMS
+	username := s.RequiredVar("kerberos.username")
+	password := s.RequiredVar("kerberos.password")
+	domain := s.RequiredVar("kerberos.domain")
+	config := kerberos.ConstructConfig(domain, username)
+
+	// Connect to Test API to use it with the UI library.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// Perform cleanup.
+	if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+		s.Fatal("Failed to clean up: ", err)
+	}
+
+	kerberosAcc := policy.KerberosAccountsValue{
+		Principal: config.KerberosAccount,
+		Password:  password,
+		Krb5conf:  []string{},
+	}
+
+	// Update policies.
+	if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{
+		&policy.KerberosEnabled{Val: true},
+		&policy.KerberosAccounts{
+			Val: []*policy.KerberosAccountsValue{
+				&kerberosAcc,
+			},
+		},
+	}); err != nil {
+		s.Fatal("Failed to update policies: ", err)
+	}
+
+	_, err = apps.LaunchOSSettings(ctx, cr, "chrome://os-settings/kerberos")
+	if err != nil {
+		s.Fatal("Could not open kerberos section in OS settings: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	if err := ui.LeftClick(nodewith.Name("Kerberos tickets").Role(role.Link))(ctx); err != nil {
+		s.Fatal("Failed to open Kerberos tickets section: ", err)
+	}
+
+	testing.ContextLog(ctx, "Waiting for Kerberos ticket to appear")
+	if err := ui.WithTimeout(30 * time.Second).WaitUntilExists(nodewith.NameStartingWith(username).Role(role.StaticText))(ctx); err != nil {
+		s.Fatal("Failed to see added Kerberos: ", err)
+	}
+
+	// Check that ticket is not expired.
+	if err := ui.Exists(nodewith.Name("Expired").Role(role.StaticText))(ctx); err == nil {
+		s.Fatal("Kerberos ticket is expired")
+	}
+
+	// Open the Files App.
+	files, err := filesapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Launching the Files App failed: ", err)
+	}
+
+	// Get a handle to the input keyboard
+	keyboard, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard handle: ", err)
+	}
+	defer keyboard.Close()
+
+	testing.ContextLog(ctx, "Mounting SMB share")
+	fileShareURLTextBox := nodewith.Name("File share URL").Role(role.TextField)
+	if err := uiauto.Combine("Click add SMB file share",
+		files.ClickMoreMenuItem("Services", "SMB file share"),
+		ui.WaitForLocation(fileShareURLTextBox),
+		ui.LeftClick(fileShareURLTextBox),
+		keyboard.TypeAction(config.RemoteFileSystemURI),
+		// TODO: Need to select SSO option here to use created ticket.
+		ui.LeftClick(nodewith.Name("Add").HasClass("action-button")),
+		ui.WithInterval(500*time.Millisecond).LeftClickUntil(fileShareURLTextBox, ui.Gone(fileShareURLTextBox)),
+	)(ctx); err != nil {
+		s.Fatal("Failed to click add SMB share: ", err)
+	}
+
+	if err := uiauto.Combine("Wait for SMB to mount and open file",
+		files.OpenPath("Files - "+config.Folder, config.Folder),
+		files.WaitForFile(config.File),
+	)(ctx); err != nil {
+		s.Fatal("Failed to interact with SMB mount: ", err)
+	}
+}
---
+++ src/chromiumos/tast/local/kerberos/configuration.go
@@ -0,0 +1,48 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package kerberos contains details about Kerberos setup that is used in
+// testing.
+package kerberos
+
+const (
+	protocol  = "https"
+	subdomain = "dc1"
+	folder    = "kerberostest"
+	file      = "test.txt"
+)
+
+// ConstructConfig prepares all necessary constants that are used by tests.
+func ConstructConfig(kerberosDomain, username string) *Configuration {
+
+	return &Configuration{
+		KerberosDomain:      kerberosDomain,
+		ServerAllowList:     "*" + kerberosDomain,
+		WebsiteAddress:      protocol + "://" + subdomain + "." + kerberosDomain,
+		KerberosAccount:     username + "@" + kerberosDomain,
+		Folder:              folder,
+		RemoteFileSystemURI: "\\\\" + subdomain + "." + kerberosDomain + "\\" + folder,
+		File:                file,
+	}
+}
+
+// Configuration contains all necessary data needed for test to access
+// Kerberos infrastructure.
+type Configuration struct {
+	// KerberosDomain domain of the hosting server.
+	KerberosDomain string
+	// ServerAllowList server name for integrated authorization.
+	ServerAllowList string
+	// WebsiteAddress website address that is guarded by Kerberos.
+	WebsiteAddress string
+	// KerberosAccount is constructed in a following way username@domain.
+	KerberosAccount string
+	// Folder is the name of the directory that is part of RemoteFileSystemURI.
+	Folder string
+	// RemoteFileSystemURI is a identifier of remote file system that is
+	// guarded by Kerberos.
+	RemoteFileSystemURI string
+	// File is the name of the file that is expected on samba mount.
+	File string
+}
