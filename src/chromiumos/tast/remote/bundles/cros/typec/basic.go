ccc17b0e81c52416b724d0d1595b5675a598b93e
Prashant Malani <pmalani@chromium.org>
2021-08-27 16:29:04 -0700

    typec: Move common functions to separate file
    A few of the functions and constants in basic.go can be used in other
    tests, so move them into another file so that they can be re-used.
    
    No functional changes expected from this commit.
    
    BUG=b:197903975
    TEST=typec.Basic passes 10/10 times on delbin.
    
    Change-Id: I103e3215cb0e8cc1a85a830837c6a0b7d56593dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3126660
    Reviewed-by: Mohamed Omar <mohamedaomar@google.com>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>
    Tested-by: Prashant Malani <pmalani@chromium.org>

--- src/chromiumos/tast/remote/bundles/cros/typec/basic.go
+++ src/chromiumos/tast/remote/bundles/cros/typec/basic.go
@@ -5,41 +5,20 @@
 package typec
 
 import (
-	"bytes"
 	"context"
-	"path/filepath"
 	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
 	"chromiumos/tast/common/servo"
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/dut"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/bundles/cros/typec/typecutils"
 	"chromiumos/tast/ssh/linuxssh"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
-
-// Bitmasks for pin assignments C and D in the DisplayPort (DP) alternate mode Vendor-Defined Object (VDO) response.
-const (
-	pinCBitMask = 0x400
-	pinDBitMask = 0x800
-)
-
-// Wait for VBUS to discharge in Servo.
-// We do this after a CC off, so that a subsequent CC on (either directly or implicitly through src dts changes) doesn't
-// confuse certain TCPCs, causing Cr50 UART to be wedged on servo.
-// Request = tRequest(24ms) + tSenderResponse(24ms) + tSrcTransition(35ms) + PSTransitionTimer(550ms) = 633ms (rounded up to 650ms)
-// Hard Reset = 650ms + tSrcRecover(1s) + tSrcTurnOn(275ms) = 1925ms
-// Total time = 1925ms * 1.5 (50% buffer) = 2887ms (rounded up to 3s)
-const (
-	tVBusDischargeCcOff = 3 * time.Second
-)
-
-// Filepath on the DUT for the servo Type C partner device.
-const partnerPath = "/sys/class/typec/port0-partner"
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -129,7 +108,7 @@ func Basic(ctx context.Context, s *testing.State) {
 	}()
 
 	// Turn CC Off before modifying DTS Mode.
-	if err := ccOffAndWait(ctx, svo); err != nil {
+	if err := typecutils.CcOffAndWait(ctx, svo); err != nil {
 		s.Fatal("Failed CC off and wait: ", err)
 	}
 
@@ -171,7 +150,7 @@ func Basic(ctx context.Context, s *testing.State) {
 	}
 
 	// Turn CC Off before modifying DTS Mode in cleanup.
-	if err := ccOffAndWait(ctx, svo); err != nil {
+	if err := typecutils.CcOffAndWait(ctx, svo); err != nil {
 		s.Fatal("Failed CC off and wait: ", err)
 	}
 }
@@ -181,7 +160,7 @@ func Basic(ctx context.Context, s *testing.State) {
 func runDPTest(ctx context.Context, svo *servo.Servo, d *dut.DUT, s *testing.State, pinAssign string) error {
 
 	s.Log("Simulating servo disconnect")
-	if err := ccOffAndWait(ctx, svo); err != nil {
+	if err := typecutils.CcOffAndWait(ctx, svo); err != nil {
 		return errors.Wrap(err, "failed CC off and wait")
 	}
 
@@ -207,7 +186,7 @@ func runDPTest(ctx context.Context, svo *servo.Servo, d *dut.DUT, s *testing.Sta
 	}
 
 	// Check that the partner DP alternate mode is found.
-	if err := checkForDPAltMode(ctx, d, s, pinAssign); err != nil {
+	if err := typecutils.CheckForDPAltMode(ctx, d, s, pinAssign); err != nil {
 		return errors.Wrap(err, "failed to find the expected partner")
 	}
 
@@ -253,66 +232,6 @@ func setServoDPMode(ctx context.Context, svo *servo.Servo, pinAssign string) err
 
 	if err := svo.RunUSBCDPConfigCommand(ctx, "enable"); err != nil {
 		return errors.Wrap(err, "failed to enable DP support")
-	}
-
-	return nil
-}
-
-// checkForDPAltMode verifies that a partner was enumerated with the expected DP altmode with the
-// selected pin assignment setting.
-func checkForDPAltMode(ctx context.Context, d *dut.DUT, s *testing.State, pinAssign string) error {
-	// Servo is always on port 0.
-	out, err := d.Conn().CommandContext(ctx, "ls", partnerPath).Output()
-	if err != nil {
-		return errors.Wrap(err, "could not run ls command on DUT")
-	}
-
-	altModeDevice := regexp.MustCompile(`port0-partner\.\d`)
-	for _, device := range bytes.Split(out, []byte("\n")) {
-		// We're only interested in the alternate mode devices.
-		if !altModeDevice.Match(device) {
-			continue
-		}
-
-		modePath := filepath.Join(partnerPath, string(device))
-
-		// Check that the alt mode has the DP SVID: 0xff01.
-		svidPath := filepath.Join(modePath, "svid")
-		if out, err := linuxssh.ReadFile(ctx, d.Conn(), svidPath); err != nil {
-			return errors.Wrap(err, "couldn't read alt mode svid")
-		} else if !bytes.Contains(out, []byte("ff01")) {
-			continue
-		}
-
-		// Read the alt mode's VDO to determine the advertised pin assignment.
-		vdoPath := filepath.Join(modePath, "vdo")
-		out, err := linuxssh.ReadFile(ctx, d.Conn(), vdoPath)
-		if err != nil {
-			return errors.Wrap(err, "couldn't read alt mode vdo")
-		}
-
-		vdoVal, err := strconv.ParseInt(strings.TrimSpace(string(out)), 0, 64)
-		if err != nil {
-			errors.Wrap(err, "couldn't parse VDO content of alt mode into int")
-		}
-
-		if (pinAssign == "c" && vdoVal&pinCBitMask != 0) ||
-			(pinAssign == "d" && vdoVal&pinDBitMask != 0) {
-			return nil
-		}
-	}
-
-	return errors.Errorf("didn't find the right DP alternate mode registered for partner for pin assignment %s", pinAssign)
-}
-
-// ccOffAndWait performs a CC Off command, followed by a sleep to ensure VBus discharges safely before any further modification.
-func ccOffAndWait(ctx context.Context, svo *servo.Servo) error {
-	if err := svo.SetCC(ctx, servo.Off); err != nil {
-		return errors.Wrap(err, "failed to switch off CC")
-	}
-
-	if err := testing.Sleep(ctx, tVBusDischargeCcOff); err != nil {
-		return errors.Wrap(err, "failed to sleep after CC off")
 	}
 
 	return nil
