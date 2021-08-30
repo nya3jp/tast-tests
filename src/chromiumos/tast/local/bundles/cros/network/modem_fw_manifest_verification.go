b0b71f38bd4bee02bff02278e7c29a910a93c9c2
Andrew Lassalle <andrewlassalle@chromium.org>
2021-08-30 12:49:03 -0700

    cellular: Enable ModemFWManifestVerification and ModemHelperManifestVerification
    BUG=b:195303946
    TEST=None
    
    Change-Id: I1d2d97a9865985f26538db72566bce7d7e325cb2

--- src/chromiumos/tast/local/bundles/cros/network/modem_fw_manifest_verification.go
+++ src/chromiumos/tast/local/bundles/cros/network/modem_fw_manifest_verification.go
@@ -18,7 +18,7 @@ func init() {
 		Func:         ModemFWManifestVerification,
 		Desc:         "Verifies the validity of the firmware manifest",
 		Contacts:     []string{"andrewlassalle@google.com", "chromeos-cellular-team@google.com"},
-		Attr:         []string{"group:cellular", "cellular_unstable"},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_active"},
 		SoftwareDeps: []string{"modemfwd"},
 	})
 }
