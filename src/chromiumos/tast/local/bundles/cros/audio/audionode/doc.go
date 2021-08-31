71b3106576e355feb7a3ea48ed55ab44f6dc8a8e
Ambalavanan MM <ambalavanan.m.m@intel.com>
2021-08-31 08:59:18 +0530

    audio: Add LocalAudioPlayback test
    Play local audio file through default app and check if the audio is routing through expected device.
    
    Sample command to execute:
    tast run <DUT_IP> audio.LocalAudioPlayback.internal_speaker
    
    BUG=b:198226236
    TEST=Ran on DUT locally
    
    Change-Id: Ie71ea7645e03840e6028ecf6f48f938977a3f485

---
+++ src/chromiumos/tast/local/bundles/cros/audio/audionode/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package audionode provides the control of audionode settings from cras.
+package audionode
