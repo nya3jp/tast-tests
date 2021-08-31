1c0021568dcd96352cd8be714010733e6cc4dfc8
Junichi Uekawa <uekawa@google.com>
2021-08-31 11:39:34 +0900

    arc.AppLoadingPerf: Add a test with O_DIRECT on ARCVM.
    Always write arcvm_dev.conf with empty content and add a flag when
    needed, instead of ignoring arcvm_dev.conf.
    
    BUG=b:190337734
    TEST=tast run localhost:2231 arc.AppLoadingPerf.o_direct_vm
    
    Cq-Depend: chromium:3096896
    Change-Id: Ib9f0ba404d70ff18102f4fc94e0108c8cbb26f14

--- src/chromiumos/tast/local/arc/pre.go
+++ src/chromiumos/tast/local/arc/pre.go
@@ -6,6 +6,7 @@ package arc
 
 import (
 	"context"
+	"io/ioutil"
 	"time"
 
 	"chromiumos/tast/errors"
@@ -57,7 +58,7 @@ var bootedPre = &preImpl{
 }
 
 // NewPrecondition creates a new arc precondition for tests that need different args.
-func NewPrecondition(name string, gaia *GaiaVars, extraArgs ...string) testing.Precondition {
+func NewPrecondition(name string, gaia *GaiaVars, oDirect bool, extraArgs ...string) testing.Precondition {
 	timeout := resetTimeout + chrome.LoginTimeout + BootTimeout
 	if gaia != nil {
 		timeout = resetTimeout + chrome.GAIALoginTimeout + BootTimeout + optin.OptinTimeout
@@ -67,6 +68,7 @@ func NewPrecondition(name string, gaia *GaiaVars, extraArgs ...string) testing.P
 		timeout:   timeout,
 		gaia:      gaia,
 		extraArgs: extraArgs,
+		oDirect:   oDirect,
 	}
 	return pre
 }
@@ -84,9 +86,9 @@ type preImpl struct {
 
 	extraArgs []string  // passed to Chrome on initialization
 	gaia      *GaiaVars // a struct containing GAIA secret variables
-
-	cr  *chrome.Chrome
-	arc *ARC
+	oDirect   bool      // whether crosvm should use O_DIRECT.
+	cr        *chrome.Chrome
+	arc       *ARC
 
 	init *Snapshot
 }
@@ -143,6 +145,13 @@ func (p *preImpl) Prepare(ctx context.Context, s *testing.PreState) interface{} 
 		defer cancel()
 		extraArgs := p.extraArgs
 		var err error
+
+		if p.oDirect {
+			writeArcvmDevConf("O_DIRECT=true")
+		} else {
+			writeArcvmDevConf("")
+		}
+
 		if p.gaia != nil {
 			username := s.RequiredVar(p.gaia.UserVar)
 			password := s.RequiredVar(p.gaia.PassVar)
@@ -216,4 +225,10 @@ func (p *preImpl) closeInternal(ctx context.Context, s *testing.PreState) {
 		p.cr = nil
 	}
 	p.init = nil
+	// TODO(b/190337734): Restore old arcvm_dev.conf on completion / failure.
+	writeArcvmDevConf("")
+}
+
+func writeArcvmDevConf(text string) error {
+	return ioutil.WriteFile("/usr/local/vms/etc/arcvm_dev.conf", []byte(text), 0644)
 }
