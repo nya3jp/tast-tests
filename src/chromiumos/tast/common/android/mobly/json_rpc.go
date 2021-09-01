049fd9d977e2fb1af2a10fde25ae9b5c810d8e4a
Kyle Shimabukuro <kyleshima@chromium.org>
2021-09-01 16:10:12 -0700

    (BeTo) Refactor Mobly snippet client into its own package
    I implemented a Mobly snippet client as part of the Nearby Share
    automation effort, since we use a snippet to control Nearby Share on
    Android. We need snippets for testing other Better Together features
    such as Phone Hub and Smart Lock, so this CL extracts the Mobly snippet
    code from a Nearby-specific package to its own package so we can use it
    for other feature automation.
    
    BUG=b:192473324
    TEST=tast run <dut> nearbyshare.*Phone*
    
    Change-Id: I42979f0d60952c750ae1d8de3311feff924f643d

---
+++ src/chromiumos/tast/common/android/mobly/json_rpc.go
@@ -0,0 +1,145 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package mobly is for interacting with Mobly snippets on Android devices for rich Android automation controls.
+// See https://github.com/google/mobly-snippet-lib for more details.
+package mobly
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// DefaultRPCResponseTimeout is the default timeout for receiving an RPC response from the snippet.
+// Most RPCs should return a response within a short amount of time.
+// Some RPCs such as eventWaitAndGet may not respond until their specified timeouts are reached.
+const DefaultRPCResponseTimeout = 10 * time.Second
+
+// jsonRPCCmd is the command format required to initialize the RPC server.
+type jsonRPCCmd struct {
+	Cmd string `json:"cmd"`
+	UID int    `json:"uid"`
+}
+
+// jsonRPCCmdResponse is the corresponding response format to jsonRPCCmd. Only used when initializing the server.
+type jsonRPCCmdResponse struct {
+	Status bool `json:"status"`
+	UID    int  `json:"uid"`
+}
+
+// jsonRPCRequest is the primary request format for snippet RPCs.
+type jsonRPCRequest struct {
+	Method string        `json:"method"`
+	ID     int           `json:"id"`
+	Params []interface{} `json:"params"`
+}
+
+// JSONRPCResponse is the corresponding response format for jsonRPCRequest.
+// The Result field's format varies depending on which method is called by
+// the request, so it should be unmarshalled based on the request's API.
+type JSONRPCResponse struct {
+	ID       int             `json:"id"`
+	Result   json.RawMessage `json:"result"`
+	Callback string          `json:"callback"`
+	Error    string          `json:"error"`
+}
+
+// clientSend writes a request to the RPC server. A newline is appended
+// to the request body as it is required by the RPC server.
+func (sc *SnippetClient) clientSend(body []byte) error {
+	if _, err := sc.conn.Write(append(body, "\n"...)); err != nil {
+		return errors.Wrap(err, "failed to write to server")
+	}
+	return nil
+}
+
+// clientReceive reads the RPC server's response.
+func (sc *SnippetClient) clientReceive(timeout time.Duration) ([]byte, error) {
+	sc.conn.SetReadDeadline(time.Now().Add(timeout))
+	bufReader := bufio.NewReader(sc.conn)
+	res, err := bufReader.ReadBytes('\n')
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// RPC formats the provided RPC method and arguments as a jsonRPCRequest, sends it to the server, and returns the server's response.
+func (sc *SnippetClient) RPC(ctx context.Context, timeout time.Duration, method string, args ...interface{}) (*JSONRPCResponse, error) {
+	// Create and send the request.
+	reqID := sc.requestID
+	request := jsonRPCRequest{ID: reqID, Method: method, Params: make([]interface{}, 0)}
+	if len(args) > 0 {
+		request.Params = args
+	}
+	requestBytes, err := json.Marshal(&request)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal request to json")
+	}
+	testing.ContextLog(ctx, "\tRPC request: ", string(requestBytes))
+
+	if err := sc.clientSend(requestBytes); err != nil {
+		return nil, err
+	}
+	sc.requestID++
+
+	// Receive and process the response.
+	var res JSONRPCResponse
+	b, err := sc.clientReceive(timeout)
+	testing.ContextLog(ctx, "\tRPC response: ", string(b))
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(b, &res); err != nil {
+		return nil, err
+	}
+	if res.Error != "" {
+		return nil, errors.Errorf("response error %v", res.Error)
+	}
+	if res.ID != reqID {
+		return nil, errors.Errorf("response ID mismatch; expected %v, got %v", reqID, res.ID)
+	}
+	return &res, nil
+}
+
+// initialize initializes the snippet.
+func (sc *SnippetClient) initialize(ctx context.Context) error {
+	// Initialize the snippet server. Running the 'initiate' command with uid -1 is necessary to create a new session to the server.
+	reqCmd := jsonRPCCmd{UID: -1, Cmd: "initiate"}
+	reqCmdBody, err := json.Marshal(&reqCmd)
+	if err != nil {
+		return errors.Wrapf(err, "failed to marshal request (%+v) to json", reqCmd)
+	}
+	testing.ContextLog(ctx, "Initialize command request: ", string(reqCmdBody))
+	if err := sc.clientSend(reqCmdBody); err != nil {
+		return errors.Wrap(err, "failed to send initialize command")
+	}
+	b, err := sc.clientReceive(DefaultRPCResponseTimeout)
+	testing.ContextLog(ctx, "Initialize command response: ", string(b))
+	if err != nil {
+		return errors.Wrap(err, "failed to read response to initialize command")
+	}
+
+	// Unmarshal the response and check if the initialize command was successful.
+	var res jsonRPCCmdResponse
+	if err := json.Unmarshal(b, &res); err != nil {
+		return errors.Wrap(err, "failed to unmarshal initialize command response")
+	}
+	if !res.Status {
+		return errors.New("snippet RPC initialize command did not succeed")
+	}
+	return nil
+}
+
+// EventWaitAndGet waits for the specified event associated with the RPC that returned callbackID to appear in the snippet's event cache.
+func (sc *SnippetClient) EventWaitAndGet(ctx context.Context, callbackID, eventName string, timeout time.Duration) (*JSONRPCResponse, error) {
+	// Read the response with a slightly extended timeout. `eventWaitAndGet` won't respond until the event is posted in the snippet cache,
+	// or the timeout is reached. In the timeout case, we need to set the TCP read deadline a little later so we'll get the response before the conn times out.
+	return sc.RPC(ctx, timeout+time.Second, "eventWaitAndGet", callbackID, eventName, int(timeout.Milliseconds()))
+}
---
+++ src/chromiumos/tast/common/android/mobly/snippet_client.go
@@ -0,0 +1,285 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package mobly is for interacting with Mobly snippets on Android devices for rich Android automation controls.
+// See https://github.com/google/mobly-snippet-lib for more details.
+package mobly
+
+import (
+	"archive/zip"
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+
+	"chromiumos/tast/common/android/adb"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/android"
+	"chromiumos/tast/testing"
+)
+
+// SnippetClient is a client for a single Mobly snippet running on an Android device.
+// Mobly snippets run an on-device JSON RPC server, whose RPCs provide some custom controls on the Android device.
+// This client can be used by Tast tests and support libraries to interact with these snippets.
+type SnippetClient struct {
+	device        *adb.Device
+	conn          net.Conn
+	listeningPort int
+	hostPort      int
+	requestID     int
+	moblyPackage  string
+}
+
+// Constants for installing and running Mobly snippet APKs.
+const (
+	instrumentationRunnerClass = "com.google.android.mobly.snippet.SnippetRunner"
+	protocolVersion            = "1"
+)
+
+// NewSnippetClient initializes the snippet client by doing the following:
+//   1. Install and run the specified snippet APK to start the server.
+//   2. Forward the snippet's listening port to the host (CrOS device) and establish a TCP connection to it.
+// We can then send RPCs over the TCP connection to interact with the snippet.
+// The Android package containing the Snippet class must be provided in the moblyPackage argument.
+// Callers should defer Cleanup to ensure the resources used by the SnippetClient are freed.
+func NewSnippetClient(ctx context.Context, d *adb.Device, moblyPackage, apkZipPath, apkName string, apkPermissions ...string) (sc *SnippetClient, err error) {
+	sc = &SnippetClient{device: d, moblyPackage: moblyPackage}
+	if err := unzipAndInstallApk(ctx, d, apkZipPath, apkName); err != nil {
+		return sc, err
+	}
+
+	for _, p := range apkPermissions {
+		permissionsCmd := sc.device.ShellCommand(ctx, "appops", "set", "--uid", moblyPackage, p, "allow")
+		if err := permissionsCmd.Run(testexec.DumpLogOnError); err != nil {
+			return sc, errors.Wrap(err, "failed to grant external storage permissions to the snippet APK")
+		}
+	}
+
+	// Launch the snippet APK and connect to the RPC server.
+	if err := sc.launchSnippet(ctx); err != nil {
+		return sc, err
+	}
+	defer func() {
+		if err != nil {
+			sc.stopSnippet(ctx)
+		}
+	}()
+
+	// Forward the snippet's listening port to the host.
+	if err := sc.forwardPort(ctx); err != nil {
+		return sc, err
+	}
+	defer func() {
+		if err != nil {
+			sc.releasePort(ctx)
+		}
+	}()
+
+	// Create a TCP connection to the snippet.
+	if err := sc.tcpConn(ctx); err != nil {
+		return sc, err
+	}
+	defer func() {
+		if err != nil {
+			sc.closeTCPConn(ctx)
+		}
+	}()
+	return sc, sc.initialize(ctx)
+}
+
+// unzipAndInstallApk extracts and installs the specified APK from the given zip.
+func unzipAndInstallApk(ctx context.Context, d *adb.Device, apkZipPath, apkName string) error {
+	// Unzip the APK to a temp dir.
+	tempDir, err := ioutil.TempDir("", "snippet-apk")
+	if err != nil {
+		return errors.Wrap(err, "failed to create temp dir")
+	}
+	defer os.RemoveAll(tempDir)
+
+	r, err := zip.OpenReader(apkZipPath)
+	if err != nil {
+		return errors.Wrapf(err, "failed to unzip %v", apkZipPath)
+	}
+	defer r.Close()
+
+	var apkExists bool
+	for _, f := range r.File {
+		if f.Name == apkName {
+			src, err := f.Open()
+			if err != nil {
+				return errors.Wrap(err, "failed to open zip contents")
+			}
+			dstPath := filepath.Join(tempDir, f.Name)
+			dst, err := os.Create(dstPath)
+			if err != nil {
+				return errors.Wrap(err, "failed to create file for copying APK")
+			}
+			defer dst.Close()
+
+			if _, err := io.Copy(dst, src); err != nil {
+				return errors.Wrap(err, "failed to extract apk from zip")
+			}
+			apkExists = true
+			break
+		}
+	}
+	if !apkExists {
+		return errors.Errorf("failed to find %v in %v", apkName, apkZipPath)
+	}
+
+	if err := d.Install(ctx, filepath.Join(tempDir, apkName), adb.InstallOptionGrantPermissions); err != nil {
+		return errors.Wrap(err, "failed to install snippet APK on the device")
+	}
+
+	return nil
+}
+
+// launchSnippet loads the snippet on Android device, verifies it started successfully,
+// and stores its listening port that we can later forward to the host CrOS device.
+func (sc *SnippetClient) launchSnippet(ctx context.Context) (err error) {
+	launchSnippetCmd := sc.device.ShellCommand(ctx,
+		"am", "instrument", "--user", android.DefaultUser, "-w", "-e", "action", "start", sc.moblyPackage+"/"+instrumentationRunnerClass)
+	stdout, err := launchSnippetCmd.StdoutPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to create stdout pipe")
+	}
+	if err := launchSnippetCmd.Start(); err != nil {
+		return errors.Wrap(err, "failed to start command to launch snippet")
+	}
+	defer func() {
+		if err != nil {
+			sc.stopSnippet(ctx)
+		}
+	}()
+
+	// Confirm the protocol version and get the snippet's serving port by parsing the launch command's stdout.
+	reader := bufio.NewReader(stdout)
+	const (
+		protocolPattern = "SNIPPET START, PROTOCOL ([0-9]+) ([0-9]+)"
+		portPattern     = "SNIPPET SERVING, PORT ([0-9]+)"
+	)
+
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return errors.Wrap(err, "failed to read stdout while looking for the snippet protocol version")
+	}
+	testing.ContextLog(ctx, "snippet launch cmd stdout first line: ", line)
+	r, err := regexp.Compile(protocolPattern)
+	if err != nil {
+		return errors.Wrap(err, "failed to compile regexp for protocol match")
+	}
+	protocolMatch := r.FindStringSubmatch(line)
+	if len(protocolMatch) == 0 {
+		return errors.New("protocol version number not found in stdout")
+	} else if protocolMatch[1] != protocolVersion {
+		return errors.Errorf("incorrect protocol version; got %v, expected %v", protocolMatch[1], protocolVersion)
+	}
+
+	// Get the device port to forward to the CB.
+	line, err = reader.ReadString('\n')
+	if err != nil {
+		return errors.Wrap(err, "failed to read stdout while looking for the snippet port")
+	}
+	testing.ContextLog(ctx, "snippet launch cmd stdout second line: ", line)
+	r, err = regexp.Compile(portPattern)
+	if err != nil {
+		return errors.Wrap(err, "failed to compile regexp for port number match")
+	}
+	portMatch := r.FindStringSubmatch(line)
+	if len(portMatch) == 0 {
+		return errors.New("port number not found in stdout")
+	}
+	listeningPort, err := strconv.Atoi(portMatch[1])
+	if err != nil {
+		return errors.Wrap(err, "failed to convert port to int")
+	}
+	sc.listeningPort = listeningPort
+	return nil
+}
+
+// stopSnippet stops the snippet on Android device.
+func (sc *SnippetClient) stopSnippet(ctx context.Context) error {
+	stopSnippetCommand := sc.device.ShellCommand(ctx, "am", "instrument",
+		"--user", android.DefaultUser, "-w", "-e",
+		"action", "stop", sc.moblyPackage+"/"+instrumentationRunnerClass)
+	if err := stopSnippetCommand.Run(); err != nil {
+		return errors.Wrap(err, "failed to stop snippet on device")
+	}
+	return nil
+}
+
+// ReconnectToSnippet restarts a connection to the snippet on Android device.
+func (sc *SnippetClient) ReconnectToSnippet(ctx context.Context) error {
+	if err := sc.forwardPort(ctx); err != nil {
+		return errors.Wrap(err, "port forwarding failed")
+	}
+	if err := sc.tcpConn(ctx); err != nil {
+		return errors.Wrap(err, "failed to make tcp conn to snippet server")
+	}
+	if err := sc.initialize(ctx); err != nil {
+		return errors.Wrap(err, "failed to reinitialize the snippet server")
+	}
+
+	return nil
+}
+
+// forwardPort forwards the snippet's listening port to the host CrOS device.
+// Callers should defer releasePort to ensure it is freed on test completion or error.
+func (sc *SnippetClient) forwardPort(ctx context.Context) error {
+	hostPort, err := sc.device.ForwardTCP(ctx, sc.listeningPort)
+	if err != nil {
+		return errors.Wrap(err, "port forwarding failed")
+	}
+	sc.hostPort = hostPort
+	return nil
+}
+
+// releasePort removes the port forwarding from the snippet's listening port to the host CrOS port.
+func (sc *SnippetClient) releasePort(ctx context.Context) error {
+	if err := sc.device.RemoveForwardTCP(ctx, sc.hostPort); err != nil {
+		return errors.Wrap(err, "failed to remove port forwarding")
+	}
+	return nil
+}
+
+// tcpConn establishes a TCP connection from the host CrOS device to the snippet.
+// Callers should defer closeTCPConn to ensure the connection is closed on test completion or error.
+func (sc *SnippetClient) tcpConn(ctx context.Context) error {
+	address := fmt.Sprintf("localhost:%v", sc.hostPort)
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to snippet server")
+	}
+	sc.conn = conn
+	return nil
+}
+
+// closeTCPConn closes the TCP connection from the host CrOS device to the snippet.
+func (sc *SnippetClient) closeTCPConn(ctx context.Context) error {
+	if err := sc.conn.Close(); err != nil {
+		return errors.Wrap(err, "failed to close TCP connection to the snippet")
+	}
+	return nil
+}
+
+// Cleanup stops the snippet, removes port forwarding, and closes the TCP connection.
+// This should be deferred after calling NewSnippetClient to ensure the resources used by the SnippetClient are released at the end of tests.
+func (sc *SnippetClient) Cleanup(ctx context.Context) {
+	if err := sc.closeTCPConn(ctx); err != nil {
+		testing.ContextLog(ctx, "Failed to clean up TCP connection: ", err)
+	}
+	if err := sc.releasePort(ctx); err != nil {
+		testing.ContextLog(ctx, "Failed to clean up port forwarding: ", err)
+	}
+	if err := sc.stopSnippet(ctx); err != nil {
+		testing.ContextLog(ctx, "Failed to stop snippet: ", err)
+	}
+}
