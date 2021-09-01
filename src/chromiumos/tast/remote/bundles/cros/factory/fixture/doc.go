c20f18facac35acf710598314fdd7be1982b8fe6
lschyi <lschyi@google.com>
2021-09-01 10:02:47 +0000

    tast-tests: Add toolkit and kiosk tests
    Add 'Toolkit' and 'Kiosk' tests along with a Fixture 'ensureToolkit'.
    
    The Fixture ensureToolkit ensures the toolkit is installed with a reboot, and
    performs uninstallation of toolkit with a reboot at the end of all tests
    require this fixture. The Toolkit tests whether the toolkit is available in
    system and the Kiosk tests whether browser is running with the debug port 9222
    enabled, then checks is it displaying factory test page.
    
    BUG=b:191446203
    TEST=tast run <DUT> factory.Toolkit factory.Kiosk
    
    Change-Id: I15c8366cab3fe91c8f1c602392df2afc514f5562

---
+++ src/chromiumos/tast/remote/bundles/cros/factory/fixture/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package fixture contains remote Tast Fixture that used by factory related tests
+package fixture
