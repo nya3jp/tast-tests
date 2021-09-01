069e2431b52cbdf26f93c53b5721022a9b2f8cfe
Madhav <madhavadas@google.com>
2021-09-01 02:25:51 +0000

    tast-tests: Restart ModemManager after modem Inhibit
    Since the modem may not recover properly after un-inhibit,
    it is better if we restart modemmanager after this test.
    
    BUG=b:197785556
    TEST=tast run $DUT network.ModemmanagerInhibitDevice
    
    Change-Id: I0efa3892279066caafb9026f4e9daf66662e5e86

--- src/chromiumos/tast/local/bundles/cros/network/cellular/cellular_helper.go
+++ src/chromiumos/tast/local/bundles/cros/network/cellular/cellular_helper.go
@@ -12,6 +12,7 @@ import (
 	"chromiumos/tast/common/shillconst"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/shill"
+	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
 
@@ -262,4 +263,20 @@ func (h *Helper) SetDeviceProperty(ctx context.Context, prop string, value inter
 		return errors.Wrapf(err, "%s not set", prop)
 	}
 	return nil
+}
+
+// RestartModemManager  - restart modemmanager with debug logs enabled/disabled.
+// Return nil if restart succeeds, else return error.
+func (h *Helper) RestartModemManager(ctx context.Context, enableDebugLogs bool) error {
+	logLevel := "INFO"
+	if enableDebugLogs {
+		logLevel = "DEBUG"
+	}
+
+	if err := upstart.RestartJob(ctx, "modemmanager", upstart.WithArg("MM_LOGLEVEL", logLevel)); err != nil {
+		return errors.Wrap(err, "failed to restart modemmanager")
+	}
+
+	return nil
+
 }
