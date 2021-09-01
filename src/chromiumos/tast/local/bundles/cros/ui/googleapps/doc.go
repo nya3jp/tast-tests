db5ab5e113f75aaa66a824b5c7769cee30719810
Jane Yang <jane.yang@cienet.com>
2021-09-01 12:40:49 +0800

    video chat cuj: Google Docs and Google Slides operations
    1. Add function that operate on Google Docs, as what Google Slides does
    currently.
    2. Modify Google Slides functions
    
    BUG=b:198190014
    TEST=fast_build.sh -T
    
    Change-Id: I174c84b41d5b7c5766921e6ebf544f2e66645096

---
+++ src/chromiumos/tast/local/bundles/cros/ui/googleapps/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package googleapps provides the control of google apps, including google docs and google slides.
+package googleapps
---
+++ src/chromiumos/tast/local/bundles/cros/ui/googleapps/docs.go
@@ -0,0 +1,113 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package googleapps
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/action"
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/cdputil"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/chrome/webutil"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+// NewGoogleDocs create a new google document.
+func NewGoogleDocs(cr *chrome.Chrome, tconn *chrome.TestConn, newWindow bool) action.Action {
+	const newDocsURL = "https://docs.new"
+	var opts []cdputil.CreateTargetOption
+	if newWindow {
+		opts = append(opts, cdputil.WithNewWindow())
+	}
+	return func(ctx context.Context) error {
+		testing.ContextLog(ctx, "Start to create google document")
+		conn, err := cr.NewConn(ctx, newDocsURL, opts...)
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+		if err := webutil.WaitForQuiescence(ctx, conn, time.Minute); err != nil {
+			return errors.Wrap(err, "failed to wait for page to finish loading")
+		}
+		return nil
+	}
+}
+
+// RenameDoc rename the document.
+func RenameDoc(ctx context.Context, tconn *chrome.TestConn, kb *input.KeyboardEventWriter, title string) action.Action {
+	ui := uiauto.New(tconn)
+	docWebArea := nodewith.NameContaining("Google Docs").Role(role.RootWebArea)
+	documentSavedState := nodewith.NameContaining("Document status: Saved to Drive").Role(role.Button)
+	renameTextbox := nodewith.Name("Rename").ClassName("docs-title-input").Ancestor(docWebArea).Editable().Focusable()
+	testing.ContextLog(ctx, "Start to rename document")
+	return ui.Retry(5, uiauto.Combine("rename document",
+		ui.WaitUntilExists(docWebArea),
+		ui.LeftClickUntil(renameTextbox, ui.WithTimeout(5*time.Second).WaitUntilExists(renameTextbox.State("focused", true))),
+		kb.AccelAction("Ctrl+A"),
+		kb.TypeAction(title),
+		waitForFieldTextToBe(tconn, renameTextbox, title),
+		kb.AccelAction("Enter"),
+		ui.WaitUntilExists(documentSavedState),
+	))
+}
+
+// EditDoc edit the document.
+func EditDoc(ctx context.Context, tconn *chrome.TestConn, kb *input.KeyboardEventWriter, paragraph string) action.Action {
+	ui := uiauto.New(tconn)
+	docWebArea := nodewith.NameContaining("Google Docs").Role(role.RootWebArea)
+	content := nodewith.Name("Document content").Role(role.TextField).Ancestor(docWebArea).Editable().First()
+	documentSavedState := nodewith.NameContaining("Document status: Saved to Drive").Role(role.Button)
+	testing.ContextLog(ctx, "Start to edit document")
+	return uiauto.Combine("edit document",
+		ui.WaitUntilExists(content),
+		kb.TypeAction(paragraph),
+		ui.WaitUntilExists(documentSavedState),
+	)
+}
+
+// DeleteDoc delete the document.
+func DeleteDoc(ctx context.Context, tconn *chrome.TestConn) action.Action {
+	ui := uiauto.New(tconn)
+	docWebArea := nodewith.NameContaining("Google Docs").Role(role.RootWebArea)
+	docHomeWebArea := nodewith.Name("Google Docs").Role(role.RootWebArea)
+	application := nodewith.Role(role.Application).Ancestor(docWebArea) // Google Docs appliction node.
+	fileButton := nodewith.Name("File").Role(role.MenuItem).Ancestor(application)
+	menu := nodewith.Role(role.Menu).Ancestor(application)
+	moveToTrash := nodewith.NameContaining("Move to trash t").Role(role.MenuItem)
+	goToDocsHome := nodewith.Name("Go to Docs home screen").Role(role.Button)
+	leaveButton := nodewith.Name("Leave").Role(role.Button)
+	testing.ContextLog(ctx, "Start to delete document")
+	return uiauto.Combine("delete document",
+		cuj.ExpandMenu(tconn, fileButton, menu, 392),
+		ui.LeftClick(moveToTrash),
+		ui.LeftClick(goToDocsHome),
+		// When leaving the edit document, sometimes the "Leave Site?" dialog box will pop up.
+		// If it appears, click the leave button.
+		ui.IfSuccessThen(ui.WithTimeout(5*time.Second).WaitUntilExists(leaveButton), ui.LeftClick(leaveButton)),
+		ui.WithTimeout(time.Minute).WaitUntilExists(docHomeWebArea),
+	)
+}
+
+func waitForFieldTextToBe(tconn *chrome.TestConn, finder *nodewith.Finder, expectedText string) action.Action {
+	ui := uiauto.New(tconn)
+	return ui.WithInterval(400*time.Millisecond).Retry(5,
+		func(ctx context.Context) error {
+			nodeInfo, err := ui.Info(ctx, finder)
+			if err != nil {
+				return err
+			}
+			if nodeInfo.Value != expectedText {
+				return errors.Errorf("failed to validate input value: got: %s; want: %s", nodeInfo.Value, expectedText)
+			}
+			return nil
+		})
+}
