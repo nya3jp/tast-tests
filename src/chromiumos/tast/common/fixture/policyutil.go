bffaa8e65982cbd90328cd5533d10b8e5ddbbed4
Kamil Szarek <kamilszarek@google.com>
2021-09-02 12:44:03 +0000

    policy: Use fixtures names defined in Tast
    This change introduces compile check for fixtures used in policy
    testing. Fixtures names are defined in Tast common/fixture and test
    use those insted of using string literal in each policy test.
    
    BUG=None
    TEST=tast run <ip> policy.DictationEnabled policy.DeviceGuestModeEnabled
    
    Change-Id: Ic4072c36b0a9679784722245de09b32dd17f29d7

---
+++ src/chromiumos/tast/common/fixture/policyutil.go
@@ -0,0 +1,23 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fixture
+
+const (
+	// FakeDMS is a fixture name.
+	FakeDMS = "fakeDMS"
+	// FakeDMSEnrolled is a fixture name.
+	FakeDMSEnrolled = "fakeDMSEnrolled"
+	// FakeDMSFamilyLink is a fixture name.
+	FakeDMSFamilyLink = "fakeDMSFamilyLink"
+	// FakeDMSFamilyLinkArc is a fixture name.
+	FakeDMSFamilyLinkArc = "fakeDMSFamilyLinkArc"
+
+	// ChromePolicyLoggedIn is a fixture name.
+	ChromePolicyLoggedIn = "chromePolicyLoggedIn"
+	// ChromeEnrolledLoggedIn is a fixture name.
+	ChromeEnrolledLoggedIn = "chromeEnrolledLoggedIn"
+	// ChromeEnrolledLoggedInARC is a fixture name.
+	ChromeEnrolledLoggedInARC = "chromeEnrolledLoggedInARC"
+)
