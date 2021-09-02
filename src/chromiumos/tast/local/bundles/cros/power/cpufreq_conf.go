bcf6f4e41d0b4a7bfc7ba593e35b7c2e971d6037
Brian Norris <briannorris@chromium.org>
2021-09-02 12:58:02 -0700

    power.CpufreqConf: new test
    The 'cpufreq_config' tool reads /etc/cpufreq.conf and applies settings
    at various times (boot; battery/AC change). This files also needs
    updated on occasion, if we switch or deprecate various settings or
    cpufreq governor options.
    
    This test would have broken for BOARD=kevin-kernelnext, if we had such a
    test, because the listed governor had been replaced in newer kernels.
    
    There is a small limitation right now, in that we'll only test the
    current charging state (either discharging or charging).
    
    BUG=b:197685239
    TEST=run it
    
    Change-Id: I9d1354022166761a210f840fec389e1f17742333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3139989
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Brian Norris <briannorris@chromium.org>
    Commit-Queue: Brian Norris <briannorris@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/power/cpufreq_conf.go
@@ -0,0 +1,167 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"chromiumos/tast/errors"
+	localpower "chromiumos/tast/local/power"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CpufreqConf,
+		Desc: "Check that we respect the /etc/cpufreq.conf file",
+		Contacts: []string{
+			"briannorris@chromium.org",
+			"chromeos-platform-power@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func CpufreqConf(ctx context.Context, s *testing.State) {
+	parseConf := func(conf string) map[string]string {
+		m := make(map[string]string)
+		for _, l := range strings.Split(conf, "\n") {
+			pair := strings.SplitN(l, "=", 2)
+			if len(pair) < 2 {
+				continue
+			}
+			// Configuration files use shell-like quoting.
+			m[pair[0]] = strings.Trim(pair[1], "\"")
+		}
+		return m
+	}
+
+	testGovernor := func(expectedGovernor string) error {
+		paths, err := filepath.Glob("/sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_governor")
+		if err != nil {
+			return errors.Wrap(err, "failed to glob for governors")
+		}
+
+		for _, path := range paths {
+			out, err := ioutil.ReadFile(path)
+			if err != nil {
+				return errors.Wrap(err, "failed to read governor")
+			}
+
+			governor := strings.TrimSpace(string(out))
+			if governor != expectedGovernor {
+				return errors.Errorf("unexpected governor: got %q, want %q", governor, expectedGovernor)
+			}
+		}
+
+		return nil
+	}
+
+	testChargeGovernor := func(charging bool, expectedGovernor string) error {
+		batteryPath, err := localpower.SysfsBatteryPath(ctx)
+		if err != nil {
+			return errors.Wrap(err, "failed to get battery path")
+		}
+		status, err := localpower.ReadBatteryStatus(batteryPath)
+		if err != nil {
+			return errors.Wrap(err, "failed to read battery status")
+		}
+		if (status != localpower.BatteryStatusDischarging) == charging {
+			s.Logf("Charging status %q doesn't match charging=%t; not checking governor %q", status, charging, expectedGovernor)
+			return nil
+		}
+		return testGovernor(expectedGovernor)
+	}
+
+	testGovernorSetting := func(governor, setting, expected string) error {
+		path := filepath.Join("/sys/devices/system/cpu/cpufreq", governor, setting)
+		out, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		val := strings.TrimSpace(string(out))
+		if val != expected {
+			return errors.Errorf("unexpected setting for governor %q: got %q, want %q", governor, val, expected)
+		}
+
+		return nil
+	}
+
+	keyTests := map[string]func(string) error{
+		"CPUFREQ_GOVERNOR": testGovernor,
+		"CPUFREQ_GOVERNOR_BATTERY_CHARGE": func(expectedGovernor string) error {
+			return testChargeGovernor(true /* charging */, expectedGovernor)
+		},
+		"CPUFREQ_GOVERNOR_BATTERY_DISCHARGE": func(expectedGovernor string) error {
+			return testChargeGovernor(false /* charging */, expectedGovernor)
+		},
+	}
+
+	// Construct test map for governor-specific settings.
+	governorSettings := map[string][]string{
+		"interactive": []string{
+			"input_boost",
+			"above_hispeed_delay",
+			"go_hispeed_load",
+			"hispeed_freq",
+			"min_sample_time",
+			"target_loads",
+			"timer_rate",
+		},
+		"ondemand": []string{
+			"sampling_rate",
+			"up_threshold",
+			"ignore_nice_load",
+			"io_is_busy",
+			"sampling_down_factor",
+			"powersave_bias",
+		},
+	}
+	for gov, settings := range governorSettings {
+		for _, setting := range settings {
+			settingKey := "CPUFREQ_" + strings.ToUpper(setting)
+			keyTests[settingKey] = func(expectedSetting string) error {
+				return testGovernorSetting(gov, setting, expectedSetting)
+			}
+		}
+	}
+
+	out, err := ioutil.ReadFile("/etc/cpufreq.conf")
+	if os.IsNotExist(err) {
+		s.Log("No cpufreq.conf file")
+		return
+	}
+	if err != nil {
+		s.Fatal("Failed to read cpufreq.conf: ", err)
+	}
+
+	conf := parseConf(string(out))
+	s.Log("cpufreq.conf contents: ", conf)
+
+	// Force cpufreq job, in case previous tests modified the governor settings and didn't
+	// clean up.
+	if err := upstart.RestartJob(ctx, "cpufreq"); err != nil {
+		s.Fatal("Failed to run cpufreq job: ", err)
+	}
+
+	for key, val := range conf {
+		s.Logf("Checking configuration key %q, value %q", key, val)
+		keyTest, ok := keyTests[key]
+		if !ok {
+			s.Errorf("Unexpected key %q in cpufreq.conf (value %q)", key, val)
+			continue
+		}
+
+		if err := keyTest(val); err != nil {
+			s.Errorf("Key %q, value %q failed: %v", key, val, err)
+		}
+	}
+}
