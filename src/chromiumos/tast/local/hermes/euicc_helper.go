f1171288435868b8f160e301132e5e2b1727b82a
Pavan Holla <pholla@chromium.org>
2021-09-03 19:10:38 +0000

    network.HermesMultiProfile: Tast framework for hermes_multiprofile_test
    Basic test to enable/disable profiles on production eSIM's.
    
    BUG=b:198807690
    TEST=tast run dut network.HermesMultiProfile
    
    Change-Id: I537570078ed1b40003b4751d939b59441e425aba

---
+++ src/chromiumos/tast/local/hermes/euicc_helper.go
@@ -0,0 +1,90 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hermes
+
+import (
+	"context"
+
+	"chromiumos/tast/common/hermesconst"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+)
+
+// EUICC wraps a Hermes.EUICC DBus object.
+type EUICC struct {
+	*dbusutil.DBusObject
+}
+
+// NewEUICC returns a DBusObject for the euiccNum'th (zero based) eSIM.
+func NewEUICC(ctx context.Context, euiccNum int) (*EUICC, error) {
+	h, err := GetHermesManager(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get Hermes Manager DBus object")
+	}
+
+	props, err := dbusutil.NewDBusProperties(ctx, h.DBusObject)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get Hermes manager properties")
+	}
+	euiccPaths, err := props.GetObjectPaths(hermesconst.ManagerPropertyAvailableEuiccs)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get available euiccs")
+	}
+	if euiccNum >= len(euiccPaths) {
+		return nil, errors.Errorf("only %d eSIM's available, cannot find eSIM number %d", len(euiccPaths), euiccNum)
+	}
+	obj, err := dbusutil.NewDBusObject(ctx, DBusHermesService, DBusHermesEuiccInterface, euiccPaths[euiccNum])
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get EUICC object")
+	}
+	return &EUICC{obj}, nil
+}
+
+// InstalledProfiles reads the eSIM, and returns the installed profiles in the eSIM.
+func (e *EUICC) InstalledProfiles(ctx context.Context) ([]Profile, error) {
+	if err := e.DBusObject.Call(ctx, "RequestInstalledProfiles").Err; err != nil {
+		return nil, errors.Wrap(err, "unable to request installed profiles")
+	}
+	props, err := dbusutil.NewDBusProperties(ctx, e.DBusObject)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get euicc properties")
+	}
+	profilePaths, err := props.GetObjectPaths("InstalledProfiles")
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get installed profiles")
+	}
+	profiles := make([]Profile, len(profilePaths))
+	for i, profilePath := range profilePaths {
+		obj, err := dbusutil.NewDBusObject(ctx, DBusHermesService, DBusHermesProfileInterface, profilePath)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to get dbus object for profile")
+		}
+		profiles[i] = Profile{obj}
+	}
+	return profiles, nil
+}
+
+// EnabledProfile reads the eSIM, and returns the enabled Profile of the eSIM if found.
+func (e *EUICC) EnabledProfile(ctx context.Context) (*Profile, error) {
+	profiles, err := e.InstalledProfiles(ctx)
+	if err != nil {
+		return &Profile{nil}, errors.Wrap(err, "failed to get installed profiles")
+	}
+
+	for _, profile := range profiles {
+		props, err := dbusutil.NewDBusProperties(ctx, profile.DBusObject)
+		if err != nil {
+			return &Profile{nil}, errors.Wrap(err, "unable to get profile properties")
+		}
+		state, err := props.GetInt32(hermesconst.ProfilePropertyState)
+		if err != nil {
+			return &Profile{nil}, errors.Wrap(err, "failed to read profiles state")
+		}
+		if state == hermesconst.ProfileStateEnabled {
+			return &profile, nil
+		}
+	}
+	return nil, nil
+}
