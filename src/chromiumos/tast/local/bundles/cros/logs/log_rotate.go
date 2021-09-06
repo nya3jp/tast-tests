4c9c9750cac17161e7f67e67691dab3ef5cf66af
Yoshiki Iguchi <yoshiki@google.com>
2021-09-06 14:12:00 +0900

    logs.LogRotae: check a newly create base file after rotation
    The behavior of rog_rotator has been changed (*) and now the new
    base file is created by the rotator after rotation.
    
    This CL fixes the tast test of log rotation accordingly.
    
    *) crrev.com/de58cb6799c4461d865310697ee10cc1dea9973d
    
    BUG=chromium:1246788
    TEST=`tast test $DUT logs.LogRotate`
    
    Change-Id: I11426fa89e8f756742873ac4fa49ba90bdb43920

--- src/chromiumos/tast/local/bundles/cros/logs/log_rotate.go
+++ src/chromiumos/tast/local/bundles/cros/logs/log_rotate.go
@@ -67,10 +67,19 @@ func LogRotate(ctx context.Context, s *testing.State) {
 		s.Errorf("Executing log_rotator failed with output %q: %v", out, err)
 	}
 
-	// Ensures the base log file is gone by the rotation.
 	basePath := getLogFilePathWithIndex(0)
-	if _, err := os.Stat(basePath); !os.IsNotExist(err) {
-		s.Errorf("The log file should not exist %q: %v", basePath, err)
+
+	// Ensures the base log file is newly created by the rotation.
+	fileinfo, err := os.Stat(basePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			s.Errorf("A new log file doesn't exit %q", basePath)
+		} else {
+			s.Errorf("Stat of the new base file failed %q: %v", basePath, err)
+		}
+	}
+	if fileinfo.Size() != 0 {
+		s.Errorf("A new log file should be empty just after the rotation %q", basePath)
 	}
 
 	// Compares the hashes to check if the rotation is done correctly.
