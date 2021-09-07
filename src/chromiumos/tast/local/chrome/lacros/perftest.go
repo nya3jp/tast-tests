834210ecc903553f003484aed6e378509e10c3cf
Eriko Kurimoto <elkurin@chromium.org>
2021-09-07 03:25:32 +0000

    Disable automation in perftest set up
    This CL disables the accessibility automation feature in speedometer
    tast test.
    The automation feature is disabled by default in the product, so we
    should align the behavior in the performance test with the product.
    
    BUG=chromium:1237381, chromium:1232556
    TEST=tast run <DUT> lacros.Speedometer
    Change-Id: I15d8c42ead149f596316af85947fc5679891ad24

--- src/chromiumos/tast/local/chrome/lacros/perftest.go
+++ src/chromiumos/tast/local/chrome/lacros/perftest.go
@@ -15,6 +15,7 @@ import (
 	"chromiumos/tast/local/media/cpu"
 	"chromiumos/tast/local/power"
 	"chromiumos/tast/local/power/setup"
+	"chromiumos/tast/testing"
 )
 
 // CleanupCallback is a callback that should be deferred to clean up test resources.
@@ -58,6 +59,18 @@ func SetupPerfTest(ctx context.Context, tconn *chrome.TestConn, name string) (re
 	cleanup = CombineCleanup(ctx, cleanup, func(ctx context.Context) error {
 		return quicksettings.ToggleSetting(ctx, tconn, quicksettings.SettingPodDoNotDisturb, false)
 	}, "failed to re-enable notifications")
+
+	// Disable automation feature is it is enabled for perf test.
+	var isEnabled bool
+	if err := tconn.Eval(ctx, "tast.promisify(chrome.autotestPrivate.isAutomationEnabled)()", &isEnabled); err != nil {
+		return nil, errors.Wrap(err, "failed to get automation status")
+	}
+	if isEnabled {
+		testing.ContextLog(ctx, "Automation feature might be enabled unintentionally. Disabling automation")
+		if err := tconn.ResetAutomation(ctx); err != nil {
+			return nil, errors.Wrap(err, "failed to reset the automation feature")
+		}
+	}
 
 	return cleanup, nil
 }
