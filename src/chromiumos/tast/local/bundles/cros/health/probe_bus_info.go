b9741289b7ae994a36f294f87ab2a05e3bccb28a
Chung-Sheng Wu <chungsheng@google.com>
2021-09-07 16:39:16 +0800

    health: Add usb validation for ProbeBusInfo
    Validate the usb devices by lsusb and usb-devices results.
    
    BUG=b:194448732
    TEST=tast run $DUT health.ProbeBusInfo
    
    Change-Id: I9892b60fdddfc0e4219007490d25da77c2c6544a

--- src/chromiumos/tast/local/bundles/cros/health/probe_bus_info.go
+++ src/chromiumos/tast/local/bundles/cros/health/probe_bus_info.go
@@ -11,6 +11,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 
 	"chromiumos/tast/local/bundles/cros/health/pci"
+	"chromiumos/tast/local/bundles/cros/health/usb"
 	"chromiumos/tast/local/croshealthd"
 	"chromiumos/tast/testing"
 )
@@ -35,6 +36,7 @@ func ProbeBusInfo(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to get bus telemetry info: ", err)
 	}
 	var pciDevs []pci.Device
+	var usbDevs []usb.Device
 	for _, d := range res.Devices {
 		if d.BusInfo.PciBusInfo != nil {
 			pd := d.BusInfo.PciBusInfo
@@ -48,19 +50,46 @@ func ProbeBusInfo(ctx context.Context, s *testing.State) {
 				Driver:   pd.Driver,
 			})
 		} else if d.BusInfo.UsbBusInfo != nil {
-			// TODO(chungsheng): Add validation
+			udi := d.BusInfo.UsbBusInfo
+			udo := usb.Device{
+				VendorID:   fmt.Sprintf("%04x", udi.VendorID),
+				ProdID:     fmt.Sprintf("%04x", udi.ProductID),
+				DeviceName: d.VendorName + " " + d.ProductName,
+				Cls:        fmt.Sprintf("%02x", udi.ClassID),
+				Sub:        fmt.Sprintf("%02x", udi.SubClassID),
+				Prot:       fmt.Sprintf("%02x", udi.ProtocolID),
+			}
+			for _, ifc := range udi.Interfaces {
+				udo.Interfaces = append(udo.Interfaces, usb.Interface{
+					InterfaceNumber: fmt.Sprintf("%x", ifc.InterfaceNumber),
+					Cls:             fmt.Sprintf("%02x", ifc.ClassID),
+					Sub:             fmt.Sprintf("%02x", ifc.SubClassID),
+					Prot:            fmt.Sprintf("%02x", ifc.ProtocolID),
+					Driver:          ifc.Driver,
+				})
+			}
+			usbDevs = append(usbDevs, udo)
 		} else {
 			s.Fatal("Unknown types of bus devices")
 		}
 	}
 
-	e, err := pci.ExpectedDevices(ctx)
+	ePci, err := pci.ExpectedDevices(ctx)
 	if err != nil {
 		s.Fatal("Failed to get expected pci devices: ", err)
 	}
 	pci.Sorted(pciDevs)
-	if d := cmp.Diff(e, pciDevs); d != "" {
+	if d := cmp.Diff(ePci, pciDevs); d != "" {
 		s.Fatal("Pci devices validation failed (-expected + got): ", d)
+	}
+
+	eUsb, err := usb.ExpectedDevices(ctx)
+	if err != nil {
+		s.Fatal("Failed to get expected usb devices: ", err)
+	}
+	usb.Sorted(usbDevs)
+	if d := cmp.Diff(eUsb, usbDevs); d != "" {
+		s.Fatal("Usb devices validation failed (-expected + got): ", d)
 	}
 }
 
