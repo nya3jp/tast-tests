dc32e889821ca6b6b84d726d517f44e481230071
Jakub Skrzypnik <js@semihalf.com>
2021-09-08 01:18:21 +0200

    boot_mode/pre: Allow booting in usb_dev mode
    This CL adds an pre.UsbDevMode() precondition which allows booting
    the DUT in usb_dev mode where it can load a test image from USB
    device and operate from it, for example in tests invoking the
    chromeos-install and other internal disk operations.
    
    This CL is mostly focused on two parts of that process:
    * Setting GBB flag FORCE_DEV_BOOT_USB flag which allows the firmware
      to boot from USB device in developer mode
    * Pressing the Ctrl-U key combination at boot to actually proceed into
      booting straight from USB device
    
    There's also a change in reporters.CurrentBootMode() to make the new
    boot mode discoverable by checking if current boot device is removable.
    
    TEST=firmware.EMMCWriteload
    
    Change-Id: I0b7a0a42b6ac1de86ab2e42515aee48711a3f3a3

--- src/chromiumos/tast/remote/firmware/pre/pre.go
+++ src/chromiumos/tast/remote/firmware/pre/pre.go
@@ -51,6 +51,11 @@ func DevModeGBB() testing.Precondition {
 	return devModeGBB
 }
 
+// UsbDevMode boots to Developer mode from USB via keypress worklow and GBB flags.
+func UsbDevMode() testing.Precondition {
+	return usbDevMode
+}
+
 // RecMode boots to Recover Mode. Tests which use RecMode() need to use the Attr `firmware_usb` also.
 func RecMode() testing.Precondition {
 	return recMode
@@ -79,6 +84,7 @@ var (
 	devMode    = newPrecondition(common.BootModeDev, false)
 	devModeGBB = newPrecondition(common.BootModeDev, true)
 	recMode    = newPrecondition(common.BootModeRecovery, false)
+	usbDevMode = newPrecondition(common.BootModeUsbDev, false)
 )
 
 // Prepare ensures that the DUT is booted into the specified mode.
