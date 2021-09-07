5d6a930dcf35c6167c8b732bf7403aee9dd30863
Junichi Uekawa <uekawa@chromium.org>
2021-09-07 00:18:39 +0000

    Revert "arc.AuthPerf: Add a test with O_DIRECT on ARCVM."
    This reverts commit e27ec1994531aabb86fff711cb50f93d5834683d.
    
    Reason for revert: breaks non-arcvm runs.
    
    BUG=b:190337734
    TEST=None
    
    Original change's description:
    > arc.AuthPerf: Add a test with O_DIRECT on ARCVM.
    >
    > Always write arcvm_dev.conf with empty content and add a flag when
    > needed, instead of ignoring arcvm_dev.conf.
    >
    > BUG=b:190337734
    > TEST=tast run localhost:2229 arc.AuthPerf.unmanaged_o_direct_vm
    >
    > Cq-Depend: chromium:3096896
    > Change-Id: I21505b37e2641f136718c7b5b13905e437af4a89
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3131424
    > Commit-Queue: Junichi Uekawa <uekawa@chromium.org>
    > Tested-by: Junichi Uekawa <uekawa@chromium.org>
    > Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    > Reviewed-by: Yury Khmel <khmel@chromium.org>
    
    Bug: b:190337734
    Change-Id: Ib93986c4be23bb099288fae86a14b7e17fcdf2a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3144111
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Auto-Submit: Junichi Uekawa <uekawa@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/auth_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/auth_perf.go
@@ -8,7 +8,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"math"
 	"os"
 	"path/filepath"
@@ -38,7 +37,6 @@ type testParam struct {
 	maxErrorBootCount int
 	chromeArgs        []string
 	dropCaches        bool
-	oDirect           bool
 }
 
 var resultPropRegexp = regexp.MustCompile(`OK,(\d+)`)
@@ -84,14 +82,6 @@ func init() {
 			Val: testParam{
 				maxErrorBootCount: 3,
 				dropCaches:        true,
-			},
-		}, {
-			Name:              "unmanaged_o_direct_vm",
-			ExtraSoftwareDeps: []string{"android_vm"},
-			Val: testParam{
-				dropCaches:        true,
-				maxErrorBootCount: 3,
-				oDirect:           true,
 			},
 		}, {
 			Name:              "unmanaged_rt_vcpu_vm",
@@ -155,7 +145,7 @@ func AuthPerf(ctx context.Context, s *testing.State) {
 	param := s.Param().(testParam)
 	maxErrorBootCount := param.maxErrorBootCount
 
-	args := append(arc.DisableSyncFlags(), "--arc-force-show-optin-ui")
+	args := append(arc.DisableSyncFlags(), "--arc-force-show-optin-ui", "--ignore-arcvm-dev-conf")
 	if param.chromeArgs != nil {
 		args = append(args, param.chromeArgs...)
 	}
@@ -336,19 +326,6 @@ func bootARC(ctx context.Context, s *testing.State, cr *chrome.Chrome, tconn *ch
 		return v, err
 	}
 
-	// Enable O_DIRECT on ARCVM block device
-	// TODO(b/190337734): Restore old arcvm_dev.conf on completion / failure.
-	if s.Param().(testParam).oDirect {
-		if err := writeArcvmDevConf("O_DIRECT=true"); err != nil {
-			defer writeArcvmDevConf("")
-			return v, err
-		}
-	} else {
-		if err := writeArcvmDevConf(""); err != nil {
-			return v, err
-		}
-	}
-
 	// Drop host OS caches if test config requires it for predictable results.
 	if s.Param().(testParam).dropCaches {
 		if err := disk.DropCaches(ctx); err != nil {
@@ -509,8 +486,4 @@ func dumpLogcat(ctx context.Context, s *testing.State, filePath string) error {
 	cmd.Stdout = logFile
 	cmd.Stderr = logFile
 	return cmd.Run()
-}
-
-func writeArcvmDevConf(text string) error {
-	return ioutil.WriteFile("/usr/local/vms/etc/arcvm_dev.conf", []byte(text), 0644)
 }
