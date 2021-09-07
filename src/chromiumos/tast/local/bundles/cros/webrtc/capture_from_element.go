d5c3bc879bc1682651790aa74142b3a296807253
Miguel Casas <mcasas@chromium.org>
2021-09-07 14:07:25 -0700

    webrtc: add captureFromElement test and "canvas" test case
    This CL adds a webrtc.CaptureFromElement test to verify the W3C
    "Media Capture from DOM Elements" spec [1], a.k.a. as captureStream()
    and used for e.g. Meet post-effects video capture. The spec has
    two parts: one for <canvas> and one for <video>; this CL only
    covers the former.
    
    [1] https://w3c.github.io/mediacapture-fromelement/
    
    BUG=b:199174572
    TEST=tast run <kohaku IP> webrtc.CaptureFromElement.* works
    
    Change-Id: I688e84add73d9edf3f15e0570c66d42886f40d88

---
+++ src/chromiumos/tast/local/bundles/cros/webrtc/capture_from_element.go
@@ -0,0 +1,40 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package webrtc
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/webrtc/capturefromelement"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CaptureFromElement,
+		Desc: "Verifies that WebRTC captureStream() (canvas, video) works",
+		Contacts: []string{
+			"mcasas@chromium.org", // Test author.
+			"chromeos-gfx-video@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Data:         capturefromelement.DataFiles(),
+		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
+		Params: []testing.Param{{
+			Name:    "canvas",
+			Val:     "canvas",
+			Fixture: "chromeVideo",
+		}},
+		//TODO(b/199174572): add a test case for "video" capture.
+	})
+}
+
+// CaptureFromElement verifies that the homonymous API works as expected.
+func CaptureFromElement(ctx context.Context, s *testing.State) {
+	if err := capturefromelement.RunCaptureStream(ctx, s, s.FixtValue().(*chrome.Chrome)); err != nil {
+		s.Fatal("RunCaptureStream failed: ", err)
+	}
+}
