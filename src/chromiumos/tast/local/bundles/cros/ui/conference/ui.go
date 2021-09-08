895aaab4fe456c97267b12d8e252d4dc4f9764bf
Jane Yang <jane.yang@cienet.com>
2021-09-08 14:57:32 -0700

    video chat cuj: Add and move ui operations to ui.go
    Add and move common functions used by both zoom and google meet tests to ui.go
    
    BUG=b:198190014
    TEST=fast_build.sh -T
    
    Change-Id: I081cb462f1fc69b110f61b51c6de5693a58c222a

---
+++ src/chromiumos/tast/local/bundles/cros/ui/conference/ui.go
@@ -0,0 +1,54 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package conference
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/action"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/testing"
+)
+
+// doFullScreenAction returns an action that does the given fullScreenAction.
+func doFullScreenAction(tconn *chrome.TestConn, fullScreenAction action.Action, title string, isFullScreen bool) action.Action {
+	ui := uiauto.New(tconn)
+	actionDescription := "enter full screen"
+	if !isFullScreen {
+		actionDescription = "exit full screen"
+	}
+	return uiauto.NamedAction(actionDescription,
+		ui.Retry(3, uiauto.Combine(actionDescription,
+			fullScreenAction,
+			waitForFullscreenCondition(tconn, title, isFullScreen),
+		)))
+}
+
+// waitForFullscreenCondition returns an action that waits for the expected window to
+// be in full screen state if isFullScreen flag is true, or not in full screen state if
+// the flag is false.
+func waitForFullscreenCondition(tconn *chrome.TestConn, title string, isFullScreen bool) action.Action {
+	return func(ctx context.Context) error {
+		if err := ash.WaitForCondition(ctx, tconn, func(w *ash.Window) bool {
+			if !strings.Contains(w.Title, title) {
+				return false
+			}
+			if isFullScreen {
+				// Check the chrome window is in full screen.
+				return w.State == ash.WindowStateFullscreen && !w.IsAnimating
+			}
+			// Check the chrome window is not in full screen.
+			return w.State != ash.WindowStateFullscreen && !w.IsAnimating
+		}, &testing.PollOptions{Timeout: 10 * time.Second}); err != nil {
+			return errors.Wrap(err, "failed to wait for expected window state")
+		}
+		return nil
+	}
+}
