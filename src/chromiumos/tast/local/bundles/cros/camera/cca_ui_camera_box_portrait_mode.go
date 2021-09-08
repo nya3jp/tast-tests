9525568dc097b8fe31b597a1ac4071c95aec6db9
Wei Lee <wtlee@google.com>
2021-09-08 18:56:28 +0800

    camera: Migrate CCARemote.* to local tests
    We don't actually need to make them remote tests as long as we can
    connect to the chart tablet from local tests directly.
    
    BUG=b:177800595
    TEST=tast run [DUT] camera.CCAUICameraBox*
    
    Change-Id: I80fb3cbc23d4536103bba0b6df6b8f598a047bcc

---
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_camera_box_portrait_mode.go
@@ -0,0 +1,67 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/local/camera/cca"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CCAUICameraBoxPortraitMode,
+		Desc:         "Verifies that CCA can take portrait mode photo via CameraBox",
+		Contacts:     []string{"wtlee@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational", "group:camera-libcamera", "group:camerabox"},
+		SoftwareDeps: []string{"camera_app", "chrome", caps.BuiltinOrVividCamera},
+		Data:         []string{"testing_rsa", "human_face_scene.jpg"},
+		Vars:         []string{"chart"},
+		Fixture:      "ccaLaunched",
+		Params: []testing.Param{{
+			Name:      "back",
+			ExtraAttr: []string{"camerabox_facing_back"},
+			Val:       cca.FacingBack,
+		}, {
+			Name:      "front",
+			ExtraAttr: []string{"camerabox_facing_front"},
+			Val:       cca.FacingFront,
+		}},
+	})
+}
+
+// CCAUICameraBoxPortraitMode tests that portrait mode works expectedly.
+func CCAUICameraBoxPortraitMode(ctx context.Context, s *testing.State) {
+	prepareChart := s.FixtValue().(cca.FixtureData).PrepareChart
+	if err := prepareChart(ctx, s.RequiredVar("chart"), s.DataPath("testing_rsa"), s.DataPath("human_face_scene.jpg")); err != nil {
+		s.Fatal("Failed to prepare chart: ", err)
+	}
+	app := s.FixtValue().(cca.FixtureData).App()
+	facing := s.Param().(cca.Facing)
+
+	if curFacing, err := app.GetFacing(ctx); err != nil {
+		s.Fatal("Failed to get facing: ", err)
+	} else if curFacing != facing {
+		if err := app.SwitchCamera(ctx); err != nil {
+			s.Fatal("Failed to switch camera: ", err)
+		}
+		if err := app.CheckFacing(ctx, facing); err != nil {
+			s.Fatalf("Failed to switch to the target camera %v: %v", facing, err)
+		}
+	}
+
+	if supported, err := app.PortraitModeSupported(ctx); err != nil {
+		s.Fatal("Failed to determine whether portrait mode is supported: ", err)
+	} else if supported {
+		if err := app.SwitchMode(ctx, cca.Portrait); err != nil {
+			s.Fatal("Failed to switch to portrait mode: ", err)
+		}
+		if _, err = app.TakeSinglePhoto(ctx, cca.TimerOff); err != nil {
+			s.Fatal("Failed to take portrait photo: ", err)
+		}
+	}
+}
