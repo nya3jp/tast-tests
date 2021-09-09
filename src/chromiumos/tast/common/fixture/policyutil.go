c244c6881af27a68b433f4be461841406a92fef2
Kamil Szarek <kamilszarek@google.com>
2021-09-09 10:25:34 +0000

    policy: Use fixtures names defined in Tast
    This change introduces compile check for fixtures used in policy
    testing. Fixtures names are defined in Tast common/fixture and test
    and fixtures definitions will use those instead of using string literal.
    
    BUG=None
    TEST=fast_build.sh -T
    TEST=tast run <ip> policy.DictationEnabled
    
    Change-Id: Ic4072c36b0a9679784722245de09b32dd17f29d7

---
+++ src/chromiumos/tast/common/fixture/policyutil.go
@@ -0,0 +1,33 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fixture
+
+// Fixture defined in chromiumos/tast/remote/policyutil/enrolled_fixture.go.
+const (
+	// Enrolled is a fixture name.
+	Enrolled = "enrolled"
+)
+
+// Fixtures defined in chromiumos/tast/local/policyutil/fixtures/fakedms.go.
+const (
+	// FakeDMS is a fixture name.
+	FakeDMS = "fakeDMS"
+	// FakeDMSEnrolled is a fixture name.
+	FakeDMSEnrolled = "fakeDMSEnrolled"
+	// FakeDMSFamilyLink is a fixture name.
+	FakeDMSFamilyLink = "fakeDMSFamilyLink"
+	// FakeDMSFamilyLinkArc is a fixture name.
+	FakeDMSFamilyLinkArc = "fakeDMSFamilyLinkArc"
+)
+
+// Fixtures defined in chromiumos/tast/local/policyutil/fixtures/chrome.go.
+const (
+	// ChromePolicyLoggedIn is a fixture name.
+	ChromePolicyLoggedIn = "chromePolicyLoggedIn"
+	// ChromeEnrolledLoggedIn is a fixture name.
+	ChromeEnrolledLoggedIn = "chromeEnrolledLoggedIn"
+	// ChromeEnrolledLoggedInARC is a fixture name.
+	ChromeEnrolledLoggedInARC = "chromeEnrolledLoggedInARC"
+)
