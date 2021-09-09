623a4dd439974282c42853abb77f86c26cc6cdb8
Miguel Casas <mcasas@chromium.org>
2021-09-09 11:47:52 -0700

    webrtc: Add webrtc.CaptureFromElementPerf test
    This CL adds a new webrtc.CaptureFromElementPerf test, modifying the
    logic supporting the existing webrtc.CaptureFromElement so that:
    - The <canvas> is painted in two alternating colors.
    - The JS logic gets a parameter to toggle the validation of contents
    (perf tests don't validate).
    - The RunCaptureStream() function measures performance if a
    new parameter |measurementDuration| is specified.
    
    BUG=b:199174572
    TEST=tast run <kohaku IP> webrtc.CaptureFromElement*
    
    Change-Id: I60e26d6f384840f8dca8c84c85b31a51b6024f7f

--- src/chromiumos/tast/local/bundles/cros/webrtc/capturefromelement/capture_from_element.go
+++ src/chromiumos/tast/local/bundles/cros/webrtc/capturefromelement/capture_from_element.go
@@ -12,9 +12,14 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
+	"sync"
+	"time"
 
+	"chromiumos/tast/common/perf"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/graphics"
+	"chromiumos/tast/local/media/cpu"
 	"chromiumos/tast/testing"
 )
 
@@ -24,7 +29,10 @@ const (
 )
 
 // RunCaptureStream drives the code verifying the captureStream() functionality.
-func RunCaptureStream(ctx context.Context, s *testing.State, cr *chrome.Chrome) error {
+// measurementDuration, if != 0, specifies the time to collect performance
+// metrics; conversely if == 0 the test instructs JS code to verify the capture
+// correctness and finishes immediately.
+func RunCaptureStream(ctx context.Context, s *testing.State, cr *chrome.Chrome, measurementDuration time.Duration) error {
 	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
 	defer server.Close()
 
@@ -35,8 +43,72 @@ func RunCaptureStream(ctx context.Context, s *testing.State, cr *chrome.Chrome) 
 	defer conn.Close()
 	defer conn.CloseTarget(ctx)
 
-	if err := conn.Call(ctx, nil, "captureCanvasAndInspect"); err != nil {
+	// Only validate contents if we're not measuring perf.
+	validate := measurementDuration == 0
+	if err := conn.Call(ctx, nil, "captureCanvasAndInspect", validate); err != nil {
 		return errors.Wrap(err, "failed to run test HTML")
+	}
+
+	// If the caller specifies no measurementDuration, then return immediately.
+	if validate {
+		return nil
+	}
+
+	p := perf.NewValues()
+
+	var gpuErr, cStateErr, cpuErr error
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		gpuErr = graphics.MeasureGPUCounters(ctx, measurementDuration, p)
+	}()
+	go func() {
+		defer wg.Done()
+		cStateErr = graphics.MeasurePackageCStateCounters(ctx, measurementDuration, p)
+	}()
+	go func() {
+		defer wg.Done()
+		cpuErr = measureCPUAndPowerUsage(ctx, measurementDuration, p)
+	}()
+	wg.Wait()
+	if gpuErr != nil {
+		return errors.Wrap(gpuErr, "failed to measure GPU counters")
+	}
+	if cStateErr != nil {
+		return errors.Wrap(cStateErr, "failed to measure Package C-State residency")
+	}
+	if cpuErr != nil {
+		return errors.Wrap(cpuErr, "failed to measure CPU/Package power")
+	}
+
+	p.Save(s.OutDir())
+	return nil
+}
+
+// measureCPUAndPowerUsage obtains CPU usage and power consumption if supported.
+func measureCPUAndPowerUsage(ctx context.Context, measurementDuration time.Duration, p *perf.Values) error {
+	testing.ContextLog(ctx, "Measuring CPU usage for ", measurementDuration)
+	measurements, err := cpu.MeasureUsage(ctx, measurementDuration)
+	if err != nil {
+		return errors.Wrap(err, "failed to measure CPU usage and power consumption")
+	}
+
+	cpuUsage := measurements["cpu"]
+	testing.ContextLogf(ctx, "CPU usage: %f%%", cpuUsage)
+	p.Set(perf.Metric{
+		Name:      "cpu_usage",
+		Unit:      "percent",
+		Direction: perf.SmallerIsBetter,
+	}, cpuUsage)
+
+	if power, ok := measurements["power"]; ok {
+		testing.ContextLogf(ctx, "Avg pkg power usage: %fW", power)
+		p.Set(perf.Metric{
+			Name:      "pkg_power_usage",
+			Unit:      "W",
+			Direction: perf.SmallerIsBetter,
+		}, power)
 	}
 	return nil
 }
