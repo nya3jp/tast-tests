a1768883c5fded59ddd4c1927cb95fa018e88737
Miriam Zimmerman <mutexlox@google.com>
2021-09-09 16:58:58 -0700

    crash: Add missing variables for crash.ECCrash.
    crash.ECCrash recently started failing due to missing variable
    declarations. The variables were added in https://crrev.com/c/3050512.
    Use the "Var" constant that the precondition file declares.
    
    BUG=None
    TEST=tast run -var="servo=chromeos6-row4-rack12-labstation2.cros:9994" \
         chromeos6-row4-rack12-host12.cros crash.ECCrash
    
    Change-Id: Iaa32fa59f79b7fea5a98a1e015a46172b8dc5e4b

--- src/chromiumos/tast/remote/bundles/cros/crash/ec_crash.go
+++ src/chromiumos/tast/remote/bundles/cros/crash/ec_crash.go
@@ -38,8 +38,8 @@ func init() {
 		// no_qemu because the servo is not available in VMs, and tast does
 		// not (yet) support skipping tests if required vars are not provided.
 		// TODO(crbug.com/967901): Remove no_qemu dep once servo var is sufficient.
-		SoftwareDeps: []string{"device_crash", "ec_crash", "pstore", "reboot", "no_qemu", "crossystem", "flashrom"},
-		Vars:         []string{"servo"},
+		SoftwareDeps: append(pre.SoftwareDeps, []string{"device_crash", "ec_crash", "pstore", "reboot", "no_qemu"}...),
+		Vars:         pre.Vars,
 	})
 }
 
