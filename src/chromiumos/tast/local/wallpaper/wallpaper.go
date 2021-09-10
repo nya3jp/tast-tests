2c64c32e6071f84bb945a0a92e7f8a90cd79152c
Jason Thai <jasontt@chromium.org>
2021-09-10 19:04:38 +0000

    tast-tests: rapid online wallpapers switching test
    Validate the behavior of rapid switching of online wallpapers by
    making sure the last image selected is displayed. The test selects
    multiple online wallpapers and compares the final wallpaper with
    the given RGBA value.
    
    BUG=b:198327140
    TEST=tast run wallpaper.SwitchOnlineWallpapers
    
    Change-Id: Ic15ae736a546b7eeaeee90f629e1af04bee442cf

---
+++ src/chromiumos/tast/local/wallpaper/wallpaper.go
@@ -0,0 +1,52 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package wallpaper supports interaction with ChromeOS wallpaper app.
+package wallpaper
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+)
+
+// OpenWallpaperPicker opens the wallpaper app.
+func OpenWallpaperPicker(ctx context.Context, ui *uiauto.Context) error {
+	wallpaperView := nodewith.ClassName("WallpaperView")
+	// Wait for the wallpaper to be visible on the screen.
+	if err := ui.WaitUntilExists(wallpaperView)(ctx); err != nil {
+		return errors.Wrap(err, "failed to render wallpaper view")
+	}
+	// Right click to display the option to set wallpaper.
+	if err := ui.RightClick(wallpaperView)(ctx); err != nil {
+		return errors.Wrap(err, "failed to right click on wallpaper view")
+	}
+	setWallpaperMenu := nodewith.Name("Set wallpaper").Role(role.MenuItem)
+	// This button takes a bit before it is clickable.
+	// Keep clicking it until the click is received and the menu closes.
+	return ui.WithInterval(500*time.Millisecond).LeftClickUntil(setWallpaperMenu, ui.Gone(setWallpaperMenu))(ctx)
+}
+
+// SelectCollection selects the collection with the given |collection| name.
+func SelectCollection(ctx context.Context, ui *uiauto.Context, collection string) error {
+	collections := nodewith.Role(role.Button).ClassName("photo-inner-container")
+	// We should at least wait for a few collections to be loaded.
+	if err := ui.WaitUntilExists(collections.Nth(5))(ctx); err != nil {
+		return errors.Wrap(err, "failed to render collection view")
+	}
+	return ui.LeftClick(nodewith.NameContaining(collection).Role(role.Button))(ctx)
+}
+
+// SelectImage selects the image with the given |image| title.
+func SelectImage(ctx context.Context, ui *uiauto.Context, image string) error {
+	images := nodewith.Role(role.ListBoxOption).ClassName("photo-inner-container")
+	if err := ui.WaitUntilExists(images.First())(ctx); err != nil {
+		errors.Wrap(err, "failed to render images view")
+	}
+	return ui.LeftClick(nodewith.Name(image).Role(role.ListBoxOption))(ctx)
+}
