c8aed53c4a06d21748ca612bc095814b4b679a5c
Hidehiko Abe <hidehiko@chromium.org>
2021-09-11 00:19:28 +0900

    Refactor ChromeRespawn.
    - Introduce procutil.WaitForTerminate.
    - Introduce ashproc.WaitForRoot.
    - Inline the respawn.go to chrome_respawn.go, which is the only user.
    
    BUG=b:199480378, chromium:1237972
    TEST=./fast_build.sh -T, tast run ui.ChromeRespawn
    
    Change-Id: Icea9fd8c47e9cfe8c9b86d40979cd258341dc24c

--- src/chromiumos/tast/local/bundles/cros/ui/chrome_respawn.go
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_respawn.go
@@ -6,9 +6,10 @@ package ui
 
 import (
 	"context"
+	"time"
 
-	"chromiumos/tast/local/bundles/cros/ui/respawn"
-	"chromiumos/tast/local/chrome/chromeproc"
+	"chromiumos/tast/local/chrome/ash/ashproc"
+	"chromiumos/tast/local/procutil"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
@@ -30,5 +31,26 @@ func ChromeRespawn(ctx context.Context, s *testing.State) {
 	if err := upstart.EnsureJobRunning(ctx, "ui"); err != nil {
 		s.Fatal("Failed to ensure ui job is running: ", err)
 	}
-	respawn.TestRespawn(ctx, s, "Chrome", chromeproc.GetRootPID)
+
+	// Chrome process should be running while ui job is running.
+	proc, err := ashproc.WaitForRoot(ctx, 30*time.Second)
+	if err != nil {
+		s.Fatal("Failed to getting initial chrome process: ", err)
+	}
+	s.Logf("Initial chrome process is %d", proc.Pid)
+
+	// Forcibly terminate the chrome.
+	if err := proc.Kill(); err != nil {
+		s.Fatal("Failed to kill chrome: ", err)
+	}
+	if err := procutil.WaitForTerminated(ctx, proc, 10*time.Second); err != nil {
+		s.Fatal("Old chrome process was not terminated: ", err)
+	}
+
+	// New Chrome should be automatically respawned.
+	newProc, err := ashproc.WaitForRoot(ctx, 30*time.Second)
+	if err != nil {
+		s.Fatal("Failed waiting for chrome to respawn: ", err)
+	}
+	s.Logf("Respawned chrome process is %d", newProc.Pid)
 }
---
+++ src/chromiumos/tast/local/procutil/wait.go
@@ -0,0 +1,26 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package procutil
+
+import (
+	"context"
+	"time"
+
+	"github.com/shirou/gopsutil/process"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// WaitForTerminated waits for the process p to be terminated.
+func WaitForTerminated(ctx context.Context, p *process.Process, timeout time.Duration) error {
+	return testing.Poll(ctx, func(ctx context.Context) error {
+		r, err := p.IsRunning()
+		if err == nil && r {
+			return errors.Errorf("process %d is still running", p.Pid)
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: timeout})
+}
