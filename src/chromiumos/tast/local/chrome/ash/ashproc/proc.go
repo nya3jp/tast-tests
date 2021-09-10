4c3c96214caa759c017467cafd796d204456e7eb
Hidehiko Abe <hidehiko@chromium.org>
2021-09-11 00:19:28 +0900

    Refactor ChromeRespawn.
    - Introduce procutil.WaitForTerminate.
    - Introduce ashproc.WaitForRoot.
    - Inline the respawn.go to chrome_respawn.go, which is the only user.
    
    BUG=b:199480378, chromium:1237972
    TEST=./fast_build.sh -T, tast run ui.ChromeRespawn
    
    Change-Id: Icea9fd8c47e9cfe8c9b86d40979cd258341dc24c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3153776
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/local/chrome/ash/ashproc/proc.go
+++ src/chromiumos/tast/local/chrome/ash/ashproc/proc.go
@@ -6,9 +6,14 @@
 package ashproc
 
 import (
+	"context"
+	"time"
+
 	"github.com/shirou/gopsutil/process"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome/internal/chromeproc"
+	"chromiumos/tast/testing"
 )
 
 // ExecPath contains the path to the Chrome executable.
@@ -22,4 +27,17 @@ func Processes() ([]*process.Process, error) {
 // Root returns the Process instance of the root ash-chrome process.
 func Root() (*process.Process, error) {
 	return chromeproc.Root(ExecPath)
+}
+
+// WaitForRoot waits for ash-chrome's root process is launched.
+func WaitForRoot(ctx context.Context, timeout time.Duration) (*process.Process, error) {
+	var ret *process.Process
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		var err error
+		ret, err = Root()
+		return err
+	}, &testing.PollOptions{Timeout: timeout}); err != nil {
+		return nil, errors.Wrap(err, "waiting for ash-chrome root is timed out")
+	}
+	return ret, nil
 }
