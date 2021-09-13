288de17f136b1d53c28643e8d1758f4557bc63fa
Kyle Shimabukuro <kyleshima@chromium.org>
2021-09-13 16:10:27 -0700

    crossdevice: add crossdevice setup fixture and utilities
    Add fixtures and utilities for crossdevice tests (phone hub, smart lock,
    etc.). One fixture is for Android setup, which handles GAIA login,
    settings+logging configuration, and installing the Multidevice Snippet.
    The other is for logging in to CrOS and pairing the devices. Many of the
    setup steps are shared with the Nearby Share fixtures, so some common
    code has been copied to the crossdevice package. I will refactor the
    Nearby fixtures later (blocked by b/199110070, since many of the changes
    are in the common nearbyshare package).
    
    BUG=b:187793760
    TEST=tast run <dut> phonehub.SilencePhone
    
    Cq-Depend: chrome-internal:4150918
    Change-Id: I7e48e52f1e4a35fc5e768124494deef964b6cabe

---
+++ src/chromiumos/tast/local/chrome/crossdevice/android_setup.go
@@ -0,0 +1,177 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crossdevice
+
+import (
+	"archive/zip"
+	"context"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/android/adb"
+	"chromiumos/tast/errors"
+	localadb "chromiumos/tast/local/android/adb"
+	"chromiumos/tast/local/android/ui"
+	"chromiumos/tast/local/chrome/nearbyshare/nearbysnippet"
+	"chromiumos/tast/testing"
+)
+
+// AdbSetup configures adb and connects to the Android device with adb root if available.
+func AdbSetup(ctx context.Context) (*adb.Device, bool, error) {
+	// Load the ARC adb vendor key, which must be pre-loaded on the Android device to allow adb over usb without requiring UI interaction.
+	if err := localadb.LaunchServer(ctx); err != nil {
+		return nil, false, errors.Wrap(err, "failed to launch adb server")
+	}
+	// Wait for the first available device, since we are assuming only a single Android device is connected to each CrOS device.
+	adbDevice, err := adb.WaitForDevice(ctx, func(device *adb.Device) bool { return !strings.HasPrefix(device.Serial, "emulator-") }, 10*time.Second)
+	if err != nil {
+		return nil, false, errors.Wrap(err, "failed to list adb devices")
+	}
+	// Check if adb root is available.
+	rooted := true
+	if err := adbDevice.Root(ctx); err != nil {
+		testing.ContextLog(ctx, "ADB root access not available; operations requiring root access will be skipped")
+		rooted = false
+	}
+	return adbDevice, rooted, nil
+}
+
+// GAIALogin removes existing user accounts from the device and adds the specified one using the tradefed GoogleAccountUtil APK.
+// Note that only rooted Android devices can add/remove accounts in this way.
+func GAIALogin(ctx context.Context, d *adb.Device, accountUtilZipPath, username, password string) error {
+	// Unzip the APK to a temp dir.
+	tempDir, err := ioutil.TempDir("", "account-util-apk")
+	if err != nil {
+		return errors.Wrap(err, "failed to create temp dir")
+	}
+	defer os.RemoveAll(tempDir)
+
+	r, err := zip.OpenReader(accountUtilZipPath)
+	if err != nil {
+		return errors.Wrapf(err, "failed to unzip %v", accountUtilZipPath)
+	}
+	defer r.Close()
+
+	var apkExists bool
+	for _, f := range r.File {
+		if f.Name == nearbysnippet.AccountUtilApk {
+			src, err := f.Open()
+			if err != nil {
+				return errors.Wrap(err, "failed to open zip contents")
+			}
+			dstPath := filepath.Join(tempDir, f.Name)
+			dst, err := os.Create(dstPath)
+			if err != nil {
+				return errors.Wrap(err, "failed to create file for copying APK")
+			}
+			defer dst.Close()
+
+			if _, err := io.Copy(dst, src); err != nil {
+				return errors.Wrap(err, "failed to extract apk from zip")
+			}
+			apkExists = true
+			break
+		}
+	}
+	if !apkExists {
+		return errors.Errorf("failed to find %v in %v", nearbysnippet.AccountUtilApk, accountUtilZipPath)
+	}
+
+	// Install the GoogleAccountUtil APK.
+	if err := d.Install(ctx, filepath.Join(tempDir, nearbysnippet.AccountUtilApk), adb.InstallOptionGrantPermissions); err != nil {
+		return errors.Wrap(err, "failed to install GoogleAccountUtil APK on the device")
+	}
+
+	// Try to remove the user account before re-adding it.
+	testing.ContextLog(ctx, "Removing all GAIA users from the Android device")
+	if err := RemoveAccounts(ctx, d); err != nil {
+		return err
+	}
+
+	// TODO(b/187795521): Re-adding the account immediately after removing it is flaky.
+	// Waiting a few seconds fixes it, but we should find a deterministic way to tell when we can safely re-add the account.
+	testing.Sleep(ctx, 3*time.Second)
+	testing.ContextLog(ctx, "Adding Nearby GAIA user to the Android device")
+	if err := AddAccount(ctx, d, username, password); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// AddAccount adds the specified Google account to the Android device. Assumes the GoogleAccountUtil APK has already been installed.
+func AddAccount(ctx context.Context, d *adb.Device, username, password string) error {
+	addAccountCmd := d.ShellCommand(ctx, "am", "instrument", "-w",
+		"-e", "account", username, "-e", "password", password, "com.google.android.tradefed.account/.AddAccount",
+	)
+	if out, err := addAccountCmd.Output(); err != nil {
+		return errors.Wrap(err, "failed to add account from the device")
+	} else if !strings.Contains(string(out), "INSTRUMENTATION_RESULT: result=SUCCESS") {
+		return errors.Errorf("failed to add account to the device (%v)", string(out))
+	}
+	return nil
+}
+
+// RemoveAccounts removes accounts from the Android device. Assumes the GoogleAccountUtil APK has already been installed.
+func RemoveAccounts(ctx context.Context, d *adb.Device) error {
+	removeAccountsCmd := d.ShellCommand(ctx, "am", "instrument", "-w", "com.google.android.tradefed.account/.RemoveAccounts")
+	if out, err := removeAccountsCmd.Output(); err != nil {
+		return errors.Wrap(err, "failed to run remove accounts command")
+	} else if !strings.Contains(string(out), "INSTRUMENTATION_RESULT: result=SUCCESS") {
+		return errors.Errorf("failed to remove accounts from the device (%v)", string(out))
+	}
+	return nil
+}
+
+// EnableVerboseLogging enables verbose logging on the Android device for the specified tags, and bluetooth+wifi if the device is rooted.
+func EnableVerboseLogging(ctx context.Context, d *adb.Device, rooted bool, tags ...string) error {
+	for _, tag := range tags {
+		if err := d.EnableVerboseLoggingForTag(ctx, tag); err != nil {
+			return errors.Wrapf(err, "failed to enable verbose logging for tag %v", tag)
+		}
+	}
+	if rooted {
+		if err := d.EnableBluetoothHciLogging(ctx); err != nil {
+			return errors.Wrap(err, "failed to enable bluetooth hci logging")
+		}
+		if err := d.EnableVerboseWifiLogging(ctx); err != nil {
+			return errors.Wrap(err, "failed to enable verbose wifi logging")
+		}
+	}
+	return nil
+}
+
+// ConfigureDevice performs basic device preparation. This includes clearing logcat and waking the screen,
+// and if the device is rooted, enabling bluetooth and extending the screen-off timeout.
+func ConfigureDevice(ctx context.Context, d *adb.Device, rooted bool) error {
+	// Clear logcat so that logs start from this point on.
+	if err := d.ClearLogcat(ctx); err != nil {
+		return errors.Wrap(err, "failed to clear previous logcat logs")
+	}
+
+	// Prepare the device for Nearby Sharing by waking+unlocking the screen, enabling bluetooth, and extending the screen-off timeout.
+	if err := d.PressKeyCode(ctx, strconv.Itoa(int(ui.KEYCODE_WAKEUP))); err != nil {
+		return errors.Wrap(err, "failed to wake screen")
+	}
+	if err := d.PressKeyCode(ctx, strconv.Itoa(int(ui.KEYCODE_MENU))); err != nil {
+		return errors.Wrap(err, "failed to wake screen")
+	}
+
+	if rooted {
+		if err := d.EnableBluetooth(ctx); err != nil {
+			return errors.Wrap(err, "failed to enable bluetooth")
+		}
+		if err := d.SetScreenOffTimeout(ctx, 10*time.Minute); err != nil {
+			return errors.Wrap(err, "failed to extend screen-off timeout")
+		}
+	}
+
+	return nil
+}
---
+++ src/chromiumos/tast/local/chrome/crossdevice/fixture.go
@@ -0,0 +1,216 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crossdevice
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/crossdevice/phonehub"
+	"chromiumos/tast/testing"
+)
+
+// NewCrossDeviceOnboarded creates a fixture that logs in to CrOS, pairs it with an Android device,
+// and ensures the features in the "Connected devices" section of OS Settings are ready to use (Smart Lock, Phone Hub, etc.).
+// Note that crossdevice fixtures inherit from crossdeviceAndroidSetup.
+func NewCrossDeviceOnboarded() testing.FixtureImpl {
+	defaultOpts := []chrome.Option{
+		chrome.ExtraArgs("--enable-logging", "--vmodule=*blue*=1", "--vmodule=*nearby*=1"),
+	}
+	return &crossdeviceFixture{
+		opts: defaultOpts,
+	}
+}
+
+// Fixture runtime variables.
+const (
+	// These vars can be used from the command line when running tests locally to configure the tests to run on personal GAIA accounts.
+	// Use these vars to log in with your own GAIA credentials on CrOS. The Android device should be signed in with the same account.
+	customCrOSUsername = "cros_username"
+	customCrOSPassword = "cros_password"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name: "crossdeviceOnboarded",
+		Desc: "User is signed in (with GAIA) to CrOS and paired with an Android phone",
+		Contacts: []string{
+			"kyleshima@chromium.org",
+			"chromeos-sw-engprod@google.com",
+		},
+		Parent: "crossdeviceAndroidSetup",
+		Impl:   NewCrossDeviceOnboarded(),
+		Vars: []string{
+			defaultCrossDeviceUsername,
+			defaultCrossDevicePassword,
+			customCrOSUsername,
+			customCrOSPassword,
+			KeepStateVar,
+		},
+		SetUpTimeout:    2 * time.Minute,
+		ResetTimeout:    resetTimeout,
+		TearDownTimeout: resetTimeout,
+		PreTestTimeout:  resetTimeout,
+		PostTestTimeout: resetTimeout,
+	})
+}
+
+type crossdeviceFixture struct {
+	opts              []chrome.Option
+	cr                *chrome.Chrome
+	androidAttributes *AndroidAttributes
+	crosAttributes    *CrosAttributes
+}
+
+// FixtData holds information made available to tests that specify this Fixture.
+type FixtData struct {
+	// Chrome is the running chrome instance.
+	Chrome *chrome.Chrome
+
+	// TestConn is a connection to the test extension.
+	TestConn *chrome.TestConn
+
+	// AndroidDevice is an object for interacting with the connected Android device's Multidevice Snippet.
+	AndroidDevice *AndroidDevice
+}
+
+func (f *crossdeviceFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	// Android device from parent fixture.
+	androidDevice := s.ParentValue().(*FixtData).AndroidDevice
+
+	// Reset and save logcat so we have Android logs even if fixture setup fails.
+	if err := androidDevice.ClearLogcat(ctx); err != nil {
+		s.Fatal("Failed to clear logcat at start of fixture setup")
+	}
+	defer androidDevice.DumpLogs(ctx, s.OutDir(), "fixture_setup_logcat.txt")
+
+	crosUsername := s.RequiredVar(defaultCrossDeviceUsername)
+	crosPassword := s.RequiredVar(defaultCrossDevicePassword)
+	customUser, userOk := s.Var(customCrOSUsername)
+	customPass, passOk := s.Var(customCrOSPassword)
+	if userOk && passOk {
+		s.Log("Logging in with user-provided credentials")
+		crosUsername = customUser
+		crosPassword = customPass
+	} else {
+		s.Log("Logging in with default GAIA credentials")
+	}
+	f.opts = append(f.opts, chrome.GAIALogin(chrome.Creds{User: crosUsername, Pass: crosPassword}))
+
+	if val, ok := s.Var(KeepStateVar); ok {
+		b, err := strconv.ParseBool(val)
+		if err != nil {
+			s.Fatalf("Unable to convert %v var to bool: %v", KeepStateVar, err)
+		}
+		if b {
+			f.opts = append(f.opts, chrome.KeepState())
+		}
+	}
+
+	cr, err := chrome.New(
+		ctx,
+		f.opts...,
+	)
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+
+	f.cr = cr
+	// Lock chrome after all Setup is complete so we don't block other fixtures.
+	chrome.Lock()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+
+	// Sometimes during login the tcp connection to the snippet server on Android is lost.
+	// If the Pair RPC fails, reconnect to the snippet server and try again.
+	if err := androidDevice.Pair(ctx); err != nil {
+		s.Log("Lost connection to the Snippet server. Reconnecting")
+		if err := androidDevice.ReconnectToSnippet(ctx); err != nil {
+			s.Fatal("Failed to reconnect to the snippet server: ", err)
+		}
+		if err := androidDevice.Pair(ctx); err != nil {
+			s.Fatal("Failed to connect the Android device to CrOS: ", err)
+		}
+	}
+
+	// Wait for the "Smart Lock is turned on" notification to appear,
+	// since it will cause Phone Hub to close if it's open before the notification pops up.
+	if _, err := ash.WaitForNotification(ctx, tconn, 30*time.Second, ash.WaitTitleContains("Smart Lock is turned on")); err != nil {
+		s.Log("Smart Lock notification did not appear after 30 seconds, proceeding anyways")
+	}
+
+	if err := phonehub.Enable(ctx, tconn, cr); err != nil {
+		s.Fatal("Failed to enable Phone Hub: ", err)
+	}
+
+	// Store Android attributes for reporting.
+	androidAttributes, err := androidDevice.GetAndroidAttributes(ctx)
+	if err != nil {
+		s.Fatal("Failed to get Android attributes for reporting: ", err)
+	}
+	f.androidAttributes = androidAttributes
+
+	// Store CrOS test metadata for reporting.
+	crosAttributes, err := GetCrosAttributes(ctx, tconn, crosUsername)
+	if err != nil {
+		s.Fatal("Failed to get CrOS attributes for reporting: ", err)
+	}
+	f.crosAttributes = crosAttributes
+
+	return &FixtData{
+		Chrome:        cr,
+		TestConn:      tconn,
+		AndroidDevice: androidDevice,
+	}
+}
+
+func (f *crossdeviceFixture) TearDown(ctx context.Context, s *testing.FixtState) {
+	chrome.Unlock()
+	if err := f.cr.Close(ctx); err != nil {
+		s.Log("Failed to close Chrome connection: ", err)
+	}
+	f.cr = nil
+}
+func (f *crossdeviceFixture) Reset(ctx context.Context) error {
+	if err := f.cr.Responded(ctx); err != nil {
+		return errors.Wrap(err, "existing Chrome connection is unusable")
+	}
+	if err := f.cr.ResetState(ctx); err != nil {
+		return errors.Wrap(err, "failed resetting existing Chrome session")
+	}
+	return nil
+}
+func (f *crossdeviceFixture) PreTest(ctx context.Context, s *testing.FixtTestState) {
+	if err := saveDeviceAttributes(f.crosAttributes, f.androidAttributes, filepath.Join(s.OutDir(), "device_attributes.json")); err != nil {
+		s.Error("Failed to save device attributes: ", err)
+	}
+}
+func (f *crossdeviceFixture) PostTest(ctx context.Context, s *testing.FixtTestState) {}
+
+// saveDeviceAttributes saves the CrOS and Android device attributes as a formatted JSON at the specified filepath.
+func saveDeviceAttributes(crosAttrs *CrosAttributes, androidAttrs *AndroidAttributes, filepath string) error {
+	attributes := struct {
+		CrOS    *CrosAttributes
+		Android *AndroidAttributes
+	}{CrOS: crosAttrs, Android: androidAttrs}
+	crosLog, err := json.MarshalIndent(attributes, "", "\t")
+	if err != nil {
+		return errors.Wrap(err, "failed to format device metadata for logging")
+	}
+	if err := ioutil.WriteFile(filepath, crosLog, 0644); err != nil {
+		return errors.Wrap(err, "failed to write CrOS attributes to output file")
+	}
+	return nil
+}
