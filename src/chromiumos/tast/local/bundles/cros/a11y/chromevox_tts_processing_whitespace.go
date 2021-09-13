491881eb2d75d9706ed0274a7b91d2f4df6e7725
Katie Dektar <katie@chromium.org>
2021-09-13 13:21:41 -0700

    tast-tests: Adds tast tests for some ChromeVox TTS preprocessing.
    This adds tast tests for ChromeVox TTS preprocessing of
    symbols and whitespace, similar to
    ChromeVoxTtsBackgroundTest.Preprocess.
    
    Similar tests could be added in the future after changing
    ChromeVox options for punctuation, echo, and whether to
    announce capital letters. Since that involves changing options,
    that will be left for a follow-up.
    
    BUG=chromium:1242981
    TEST=tast --verbose run localhost:2222 a11y.ChromevoxTTSProcessing*
    
    Change-Id: Ib5754bea57596d6420c23ac7b6c532813478782c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3158897
    Reviewed-by: Ricardo Quesada <ricardoq@chromium.org>
    Reviewed-by: Alvin Jia <alvinjia@google.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Tested-by: Katie Dektar <katie@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/a11y/chromevox_tts_processing_whitespace.go
@@ -0,0 +1,113 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package a11y
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/a11y"
+	"chromiumos/tast/local/audio/crastestclient"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ChromevoxTTSProcessingWhitespace,
+		Desc: "A test that verifies the way ChromeVox processes whitespace for speech in text areas",
+		Contacts: []string{
+			"katie@chromium.org",           // Test author
+			"chromeos-a11y-eng@google.com", // Backup mailing list
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+func ChromevoxTTSProcessingWhitespace(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	cr := s.PreValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	// Mute the device to avoid noisiness.
+	if err := crastestclient.Mute(ctx); err != nil {
+		s.Fatal("Failed to mute: ", err)
+	}
+	defer crastestclient.Unmute(cleanupCtx)
+
+	c, err := a11y.NewTabWithHTML(ctx, cr, "<textarea></textarea>")
+	if err != nil {
+		s.Fatal("Failed to open a new tab with HTML: ", err)
+	}
+	defer c.Close()
+
+	if err := a11y.SetFeatureEnabled(ctx, tconn, a11y.SpokenFeedback, true); err != nil {
+		s.Fatal("Failed to enable spoken feedback: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := a11y.SetFeatureEnabled(ctx, tconn, a11y.SpokenFeedback, false); err != nil {
+			s.Error("Failed to disable spoken feedback: ", err)
+		}
+	}(cleanupCtx)
+
+	// Connect to ChromeVox.
+	cvconn, err := a11y.NewChromeVoxConn(ctx, cr)
+	if err != nil {
+		s.Fatal("Failed to connect to the ChromeVox background page: ", err)
+	}
+	defer cvconn.Close()
+
+	// Get a speech monitor for the Google TTS engine.
+	ed := a11y.TTSEngineData{
+		ExtID:                     a11y.GoogleTTSExtensionID,
+		UseOnSpeakWithAudioStream: false,
+	}
+	sm, err := a11y.RelevantSpeechMonitor(ctx, cr, tconn, ed)
+	if err != nil {
+		s.Fatal("Failed to connect to the TTS background page: ", err)
+	}
+	defer sm.Close()
+
+	// Wait for ChromeVox to focus the root web area.
+	rootWebArea := nodewith.Role(role.RootWebArea).First()
+	if err = cvconn.WaitForFocusedNode(ctx, tconn, rootWebArea); err != nil {
+		s.Error("Failed to wait for initial ChromeVox focus: ", err)
+	}
+
+	testSteps := []struct {
+		keyCommands  []string
+		expectations []a11y.SpeechExpectation
+	}{
+		{
+			[]string{"Search+Right"},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("Text area")},
+		},
+		{
+			[]string{"Space"},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("space")},
+		},
+		{
+			[]string{"Enter"},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("new line")},
+		},
+	}
+
+	for _, step := range testSteps {
+		if err := a11y.PressKeysAndConsumeExpectations(ctx, sm, step.keyCommands, step.expectations); err != nil {
+			s.Error("Error when pressing keys and expecting speech: ", err)
+		}
+	}
+}
