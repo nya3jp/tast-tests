72eb8f91e4eb7e0c7cf2889cffa00b02edaa56be
Kamil Szarek <kamilszarek@google.com>
2021-09-13 10:16:12 +0000

    kerberos: Add Kerberos test
    Add Kerberos test accessing website protected by Kerberos. Add
    configuration library that constructs necessary data for tests.
    
    BUG=chromium:1249427
    TEST=tast run <ip> kerberos.ManualTicketAccessWebsite
    
    Cq-Depend: chrome-internal:4114491
    Change-Id: I1209ff0d40985391db4f0e723270ec358f3e7519

---
+++ src/chromiumos/tast/local/bundles/cros/kerberos/manual_ticket_access_website.go
@@ -0,0 +1,151 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package kerberos
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/kerberos"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ManualTicketAccessWebsite,
+		Desc: "Checks the behavior of accessing website secured by Kerberos after adding Kerberos ticket",
+		Contacts: []string{
+			"kamilszarek@google.com", // Test author
+			"alexanderhartl@google.com",
+			"chromeos-commercial-remote-management@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		VarDeps:      []string{"kerberos.username", "kerberos.password", "kerberos.domain"},
+		Fixture:      "chromePolicyLoggedIn",
+	})
+}
+
+func ManualTicketAccessWebsite(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
+	fdms := s.FixtValue().(*fixtures.FixtData).FakeDMS
+	username := s.RequiredVar("kerberos.username")
+	password := s.RequiredVar("kerberos.password")
+	domain := s.RequiredVar("kerberos.domain")
+	config := kerberos.ConstructConfig(domain, username)
+
+	// Connect to Test API to use it with the UI library.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// Perform cleanup.
+	if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+		s.Fatal("Failed to clean up: ", err)
+	}
+
+	// Set Kerberos configuration.
+	if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{
+		&policy.KerberosEnabled{Val: true},
+		&policy.AuthServerAllowlist{Val: config.ServerAllowlist},
+	}); err != nil {
+		s.Fatal("Failed to update policies: ", err)
+	}
+
+	conn, err := cr.NewConn(ctx, config.WebsiteAddress)
+	if err != nil {
+		s.Fatal("Failed to connect to chrome: ", err)
+	}
+	defer conn.Close()
+
+	// The website does not have a valid certificate. We accept the warning and
+	// proceed to the content.
+	clickAdvance := fmt.Sprintf("document.getElementById(%q).click()", "details-button")
+	if err := conn.Eval(ctx, clickAdvance, nil); err != nil {
+		s.Fatal("Failed to click Advance button: ", err)
+	}
+
+	clickProceed := fmt.Sprintf("document.getElementById(%q).click()", "proceed-link")
+	if err := conn.Eval(ctx, clickProceed, nil); err != nil {
+		s.Fatal("Failed to click Advance button: ", err)
+	}
+
+	// Check that title is 401 - unauthorized.
+	var websiteTitle string
+	if err := conn.Eval(ctx, "document.title", &websiteTitle); err != nil {
+		s.Error("Failed to get the website title: ", err)
+	}
+	if strings.Contains(websiteTitle, "401") {
+		s.Error("Website title did not contain error 401")
+	}
+
+	// This section deals with OS Settings and cleans UI.
+	{
+		ui := uiauto.New(tconn)
+		_, err = apps.LaunchOSSettings(ctx, cr, "chrome://os-settings/kerberos")
+		if err != nil {
+			s.Fatal("Could not open kerberos section in OS settings: ", err)
+		}
+		defer apps.Close(ctx, tconn, apps.Settings.ID)
+		keyboard, err := input.Keyboard(ctx)
+		if err != nil {
+			s.Fatal("Failed to get a keyboard")
+		}
+		defer keyboard.Close()
+
+		// Add a Kerberos ticket.
+		if err := uiauto.Combine("add Kerberos ticket",
+			ui.LeftClick(nodewith.Name("Kerberos tickets").Role(role.Link)),
+			ui.LeftClick(nodewith.Name("Add a ticket").Role(role.Button)),
+			ui.LeftClick(nodewith.Name("Kerberos username").Role(role.TextField)),
+			keyboard.TypeAction(config.KerberosAccount),
+			ui.LeftClick(nodewith.Name("Password").Role(role.TextField)),
+			keyboard.TypeAction(password),
+			ui.LeftClick(nodewith.Name("Add").HasClass("action-button")),
+			ui.EnsureGoneFor(nodewith.Name("Add a ticket").Role(role.Button), 30*time.Second),
+		)(ctx); err != nil {
+			s.Fatal("Failed to add Kerberos ticket: ", err)
+		}
+
+		// Wait for ticket to appear.
+		s.Log(ctx, "Waiting for Kerberos ticket to appear")
+		if err := ui.WaitUntilExists(nodewith.Name(config.KerberosAccount).Role(role.StaticText))(ctx); err != nil {
+			s.Fatal("Failed to see added Kerberos: ", err)
+		}
+
+		// Check that ticket is not expired.
+		if err := ui.Exists(nodewith.Name("Expired").Role(role.StaticText))(ctx); err == nil {
+			s.Fatal("Kerberos ticket is expired")
+		}
+	}
+
+	// Needed as without the website is not refreshed.
+	testing.Sleep(ctx, 5*time.Second)
+	if err := conn.Navigate(ctx, config.WebsiteAddress); err != nil {
+		s.Fatalf("Failed to navigate to the server URL %q: %v", config.WebsiteAddress, err)
+	}
+
+	if err := conn.Eval(ctx, "document.title", &websiteTitle); err != nil {
+		s.Error("Failed to get the website title: ", err)
+	}
+
+	if !strings.Contains(websiteTitle, "KerberosTest") {
+		s.Error("Website title was not KerberosTest but ", websiteTitle)
+	}
+}
