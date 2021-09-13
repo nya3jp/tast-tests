8b14e4826561a2ce42aa2ba97f99ab84c0888306
Kyle Shimabukuro <kyleshima@chromium.org>
2021-09-13 16:10:27 -0700

    crossdevice: add crossdevice setup fixture and utilities
    Add fixtures and utilities for crossdevice tests (phone hub, smart lock,
    etc.). One fixture is for Android setup, which handles GAIA login,
    settings+logging configuration, and installing the Multidevice Snippet.
    The other is for logging in to CrOS and pairing the devices. Many of the
    setup steps are shared with the Nearby Share fixtures, so some common
    code has been copied to the crossdevice package. I will refactor the
    Nearby fixtures later (blocked by b/199110070, since many of the changes
    are in the common nearbyshare package).
    
    BUG=b:187793760
    TEST=tast run <dut> phonehub.DoNotDisturb
    
    Cq-Depend: chrome-internal:4150918
    Change-Id: I7e48e52f1e4a35fc5e768124494deef964b6cabe

--- src/chromiumos/tast/local/chrome/crossdevice/constants.go
+++ src/chromiumos/tast/local/chrome/crossdevice/constants.go
@@ -10,3 +10,12 @@ const (
 	MultideviceSnippetApkName      = "multidevice_snippet.apk"
 	MultideviceSnippetMoblyPackage = "com.google.android.gmscore.integ.modules.auth.proximity.mobly.snippet"
 )
+
+// AccountUtilZip is the filename for the .zip containing the GoogleAccountUtil APK.
+const AccountUtilZip = "google_account_util.zip"
+
+// AccountUtilApk is the filename for the GoogleAccountUtil APK.
+const AccountUtilApk = "GoogleAccountUtil.apk"
+
+// KeepStateVar is the runtime variable name used to specify the chrome.KeepState parameter to preserve the DUT's user accounts.
+const KeepStateVar = "keepState"
