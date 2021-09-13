222f90e0da3e5debe22542734a69e3bde8eda2d5
Ambalavanan MM <ambalavanan.m.m@intel.com>
2021-09-13 09:36:18 +0530

    audio: Add PlayAndRecordAudio test
    Play local audio file and record it simultaneously
    
    Sample command to execute:
    tast run <DUT_IP> audio.PlayAndRecordAudio
    
    BUG=b:198226236
    TEST=Ran on DUT locally
    
    Change-Id: I8f465fec79fc141cf6a9f90f65aa964eb4949c16

---
+++ src/chromiumos/tast/local/bundles/cros/audio/play_and_record_audio.go
@@ -0,0 +1,133 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package audio
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/local/audio"
+	"chromiumos/tast/local/audio/crastestclient"
+	"chromiumos/tast/local/bundles/cros/audio/audionode"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui/filesapp"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PlayAndRecordAudio,
+		Desc:         "Play local audio file and record it simultaneously",
+		Contacts:     []string{"ambalavanan.m.m@intel.com", "intel-chrome-system-automation-team@intel.com"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+	})
+}
+
+// PlayAndRecordAudio generates audio file, plays it and records it simultaneously.
+func PlayAndRecordAudio(ctx context.Context, s *testing.State) {
+	const (
+		expectedAudioNode = "INTERNAL_SPEAKER"
+		storageType       = "Downloads"
+		audioRate         = 48000
+		audioChannel      = 2
+		duration          = 30
+	)
+
+	cr := s.PreValue().(*chrome.Chrome)
+
+	// Generate sine raw input file that lasts 30 seconds.
+	rawFileName := "30SEC.raw"
+	rawTempFile, err := ioutil.TempFile("", rawFileName)
+	if err != nil {
+		s.Error("Failed to create raw temp file: ", err)
+	}
+	rawFile := audio.TestRawData{
+		Path:          rawTempFile.Name(),
+		BitsPerSample: 16,
+		Channels:      audioChannel,
+		Rate:          audioRate,
+		Frequencies:   []int{440, 440},
+		Volume:        0.05,
+		Duration:      duration,
+	}
+	if err := audio.GenerateTestRawData(ctx, rawFile); err != nil {
+		s.Fatal("Failed to generate audio test data: ", err)
+	}
+
+	wavFileName := "30SEC.wav"
+	wavFile := filepath.Join(filesapp.DownloadPath, wavFileName)
+	if err := audio.ConvertRawToWav(ctx, rawTempFile.Name(), wavFile, audioRate, audioChannel); err != nil {
+		s.Fatal("Failed to convert raw to wav: ", err)
+	}
+
+	// Open the test API.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+	files, err := filesapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch the Files App: ", err)
+	}
+	defer files.Release(ctx)
+	if err := files.OpenDir(ctx, storageType, "Files - "+storageType); err != nil {
+		s.Fatalf("Failed to open %v folder in files app: %v", storageType, err)
+	}
+
+	audioDeviceName, audioDeviceType, err := audionode.SetAudioNode(ctx, expectedAudioNode)
+	if err != nil {
+		s.Fatal("Failed to set the Audio node: ", err)
+	}
+	s.Logf("Selected audio device name: %s", audioDeviceName)
+	s.Logf("Selected audio device type: %s", audioDeviceType)
+
+	recWavFileName := "30SEC_REC.wav"
+	recWavFile := filepath.Join(filesapp.DownloadPath, recWavFileName)
+	cmd := fmt.Sprintf("play -c %d -r %d %s & rec -r %d -c %d %s trim 0 30", audioChannel, audioRate, wavFile, audioRate, audioChannel, recWavFile)
+	output := testexec.CommandContext(ctx, "sh", "-c", cmd)
+	if err := output.Run(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("Failed to execute %q: %v", cmd, err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to open the keyboard: ", err)
+	}
+	defer kb.Close()
+
+	if err := files.OpenFile(ctx, recWavFileName); err != nil {
+		s.Fatalf("Failed to open the audio file %q: %v", recWavFileName, err)
+	}
+	// Sample time for the audio to play for 5 seconds.
+	testing.Sleep(ctx, 5*time.Second)
+
+	// Deleting the files and closing the audio player.
+	defer func() {
+		os.Remove(rawFile.Path)
+		os.Remove(wavFile)
+		os.Remove(recWavFile)
+		if kb.Accel(ctx, "Ctrl+W"); err != nil {
+			s.Error("Failed to close Audio player: ", err)
+		}
+	}()
+
+	devName, err := crastestclient.FirstRunningDevice(ctx, audio.OutputStream)
+	if err != nil {
+		s.Fatal("Failed to detect running output device: ", err)
+	}
+
+	if audioDeviceName != devName {
+		s.Fatalf("Failed to route the audio through expected audio node: got %q; want %q", devName, audioDeviceName)
+	}
+}
