088e6c8740b02bd027e0370a2806d1e9f79fb069
Arowa Suliman <arowa@google.com>
2021-09-13 13:10:10 -0700

    Tast: Change owner ShillInitScriptStartLoggedin.
    Move the ownership of the test to one of the networking team.
    
    BUG=b:153427777
    TEST=none
    
    Change-Id: Ifc5f869ea9f4db497efa7ffe7e4388272be4f669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3158892
    Tested-by: Arowa Suliman <arowa@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Commit-Queue: Arowa Suliman <arowa@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/network/shill_init_scripts_start_loggedin.go
+++ src/chromiumos/tast/local/bundles/cros/network/shill_init_scripts_start_loggedin.go
@@ -18,7 +18,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         ShillInitScriptsStartLoggedin,
 		Desc:         "Test that shill init scripts perform as expected",
-		Contacts:     []string{"arowa@google.com", "cros-networking@google.com"},
+		Contacts:     []string{"hugobenichi@google.com", "cros-networking@google.com"},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline", "informational"},
 	})
