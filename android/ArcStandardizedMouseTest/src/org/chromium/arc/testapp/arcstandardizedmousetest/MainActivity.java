a912e9e22e0141effea6d53fe5c1813c642a8d9d
David Welling <davidwelling@google.com>
2021-09-14 17:19:23 +0000

    arcappcompat: move mouse click tests into generic app
    Moves the mouse left, and right click apps into the shared app so they
    can be used by additional input devices in the future.
    
    BUG=b:194900373
    TEST=push apk && tast run ${HOST} arc.Standardized*
    
    Change-Id: I3cf5e64c5e7335af1bfe3271b61cab0f74f05fa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3160423
    Tested-by: David Welling <davidwelling@google.com>
    Commit-Queue: David Welling <davidwelling@google.com>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>

--- android/ArcStandardizedMouseTest/src/org/chromium/arc/testapp/arcstandardizedmousetest/MainActivity.java
+++ android/ArcStandardizedMouseTest/src/org/chromium/arc/testapp/arcstandardizedmousetest/MainActivity.java
@@ -8,50 +8,11 @@ package org.chromium.arc.testapp.arcstandardizedmousetest;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
-    private LinearLayout mLayoutMain;
-    private Button mBtnLeftClick;
-    private int mBtnLeftClickCounter = 1;
-
-    private Button mBtnRightClick;
-    private int mBtnRightClickCounter = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mLayoutMain = findViewById(R.id.layoutMain);
-
-        // Add the text 'Mouse Left Click' when the left click button is pressed.
-        // Always add the click counter so the tast test can make sure a single click
-        // doesn't fire two events.
-        mBtnLeftClick = findViewById(R.id.btnLeftClick);
-        mBtnLeftClick.setOnClickListener(
-                (v) -> {
-                    TextView el = new TextView(this);
-                    el.setText(String.format("MOUSE LEFT CLICK (%d)", mBtnLeftClickCounter));
-                    mBtnLeftClickCounter++;
-                    mLayoutMain.addView(el);
-                });
-
-        // Add the text 'Mouse Right Click' when the right click button is pressed.
-        // Always add the click counter so the tast test can make sure a single click
-        // doesn't fire two events.
-        // 'OnContextClick' is fired natively when the user right clicks.
-        mBtnRightClick = findViewById(R.id.btnRightClick);
-        mBtnRightClick.setOnContextClickListener(
-                (v) -> {
-                    TextView el = new TextView(this);
-                    el.setText(String.format("MOUSE RIGHT CLICK (%d)", mBtnRightClickCounter));
-                    mBtnRightClickCounter++;
-                    mLayoutMain.addView(el);
-                    return true;
-                });
     }
 }
