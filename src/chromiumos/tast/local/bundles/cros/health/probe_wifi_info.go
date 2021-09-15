3fb61342dc468c680a4899bce73c03b9bb200aeb
Pathan Jilani <pathan.jilani@intel.com>
2021-09-15 11:08:34 +0530

    health: add ProbeWifiInfo
    Check that we can probe cros_healthd for WIFI info.
    
    Sample command to execute:
    tast run <DUT_IP> health.ProbeWifiInfo
    
    BUG=b:198745197
    TEST=Ran on DUT locally
    
    Change-Id: Id65d9cd95694b351c00001ac308f86cc14971e28

---
+++ src/chromiumos/tast/local/bundles/cros/health/probe_wifi_info.go
@@ -0,0 +1,61 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package health
+
+import (
+	"context"
+
+	"chromiumos/tast/local/croshealthd"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+type wirelessInteface struct {
+	IEEEStandard      string `json:"ieee_standard"`
+	InterfaceName     string `json:"interface_name"`
+	PowerManagementOn bool   `json:"power_management_on"`
+}
+
+type networkInterface struct {
+	WirelessInterfaces wirelessInteface `json:"wireless_interface"`
+}
+
+type wifiInfo struct {
+	NetworkInterfaces []networkInterface `json:"network_interfaces"`
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ProbeWifiInfo,
+		Desc: "Check that we can probe cros_healthd for WIFI info",
+		Contacts: []string{"pathan.jilani@intel.com",
+			"intel-chrome-system-automation-team@intel.com",
+			"cros-tdm-tpe-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "diagnostics"},
+		HardwareDeps: hwdep.D(hwdep.Model("brya")),
+		Fixture:      "crosHealthdRunning",
+	})
+}
+
+func ProbeWifiInfo(ctx context.Context, s *testing.State) {
+	params := croshealthd.TelemParams{Category: "network_interface"}
+	var wifi wifiInfo
+	if err := croshealthd.RunAndParseJSONTelem(ctx, params, s.OutDir(), &wifi); err != nil {
+		s.Fatal("Failed to get WIFI telemetry info: ", err)
+	}
+	for _, interfaces := range wifi.NetworkInterfaces {
+		if interfaces.WirelessInterfaces.IEEEStandard == "" {
+			s.Fatal("Failed to get IEEEStandard")
+		}
+		if interfaces.WirelessInterfaces.InterfaceName == "" {
+			s.Fatal("Failed to get InterfaceName")
+		}
+		if !interfaces.WirelessInterfaces.PowerManagementOn {
+			s.Fatal("Failed to get PowerManagementOn")
+		}
+	}
+}
