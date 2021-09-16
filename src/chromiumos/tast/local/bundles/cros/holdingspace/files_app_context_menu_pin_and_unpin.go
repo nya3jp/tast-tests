3b9775af2d42adc8bc915c582d1866ed958742ff
Sun Tsai <sun.tsai@cienet.com>
2021-09-16 18:06:48 +0800

    mta: improve holdingspace.FilesAppContextMenuPinAndUnpin.folder test coverage
    Improve test's coverage by add folders control.
    
    [MTA 0ed6d] Pin/Unpin a folder from Files app, verify that the folder's presence in holdingspace.
    
    BUG=b:200064364
    TEST=tast run <DUT> holdingspace.FilesAppContextMenuPinAndUnpin*
    
    Change-Id: Iee2197acea15cd0ff2f8c2c8e7d86b15c8a68263
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3297635
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Kamil Szarek <kamilszarek@google.com>
    Reviewed-by: Pranav Batra <batrapranav@chromium.org>
    Tested-by: Alfred Yu <alfred.yu@cienet.com>
    Auto-Submit: Alfred Yu <alfred.yu@cienet.com>
    Commit-Queue: Alfred Yu <alfred.yu@cienet.com>

--- src/chromiumos/tast/local/bundles/cros/holdingspace/files_app_context_menu_pin_and_unpin.go
+++ src/chromiumos/tast/local/bundles/cros/holdingspace/files_app_context_menu_pin_and_unpin.go
@@ -9,14 +9,25 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/filesapp"
 	"chromiumos/tast/local/chrome/uiauto/holdingspace"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/testing"
+)
+
+const (
+	fileName   = "test.txt"
+	folderName = "test"
 )
 
 func init() {
@@ -29,9 +40,20 @@ func init() {
 			"dmblack@chromium.org",
 			"chromeos-sw-engprod@google.com",
 			"cros-system-ui-eng@google.com",
+			"sun.tsai@cienet.com",
+			"cienet-development@googlegroups.com",
 		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{
+			{
+				Val: fileName,
+			},
+			{
+				Name: "folder",
+				Val:  folderName,
+			},
+		},
 	})
 }
 
@@ -55,29 +77,63 @@ func FilesAppContextMenuPinAndUnpin(ctx context.Context, s *testing.State) {
 		s.Fatal("Could not open filesapp: ", err)
 	}
 
-	const fileName = "test.txt"
-	filePath := filepath.Join(filesapp.MyFilesPath, fileName)
-
-	// Create our file, with appropriate permissions so we can delete later.
-	if err := ioutil.WriteFile(filePath, []byte("Per aspera, ad astra"), 0644); err != nil {
-		s.Fatalf("Creating file %q failed: %s", filePath, err)
+	if err := verifyTipInModes(ctx, tconn, true /* isTablet */); err != nil {
+		s.Fatal("Failed to verify tip in tablet mode: ", err)
 	}
-	defer os.Remove(filePath)
+	if err := verifyTipInModes(ctx, tconn, false /* isTablet */); err != nil {
+		s.Fatal("Failed to verify tip in clamshell mode: ", err)
+	}
+
+	targetName := s.Param().(string)
+	targetPath := filepath.Join(filesapp.MyFilesPath, targetName)
+
+	if err := createTarget(targetPath, strings.HasSuffix(targetName, "txt") /* isFile */); err != nil {
+		s.Fatalf("Failed to create %q: %v", targetPath, err)
+	}
+	defer os.Remove(targetPath)
 
 	uia := uiauto.New(tconn)
 
+	// To prevent the "Pin to shelf" option from being hidden, maximize the Files app.
+	if err := uia.LeftClick(nodewith.Name("Maximize").HasClass("FrameCaptionButton").Role(role.Button))(ctx); err != nil {
+		s.Fatal("Failed to maximize the Files app: ", err)
+	}
+
 	// Pin and unpin the item using the context menu in the files app. Unpinning
 	// the item should make the Tray disappear, since nothing else is pinned.
-	if err := uiauto.Combine(
-		"Pin and unpin item from shelf via Files app context menu",
-		fsapp.ClickContextMenuItem(fileName, "Pin to shelf"),
+	if err := uiauto.Combine("pin and unpin item from shelf via Files app context menu",
+		fsapp.ClickContextMenuItem(targetName, "Pin to shelf"),
 		uia.LeftClick(holdingspace.FindTray()),
-		uia.WaitUntilExists(holdingspace.FindPinnedFileChip().Name(fileName)),
-		fsapp.ClickContextMenuItem(fileName, "Unpin from shelf"),
+		uia.WaitUntilExists(holdingspace.FindPinnedFileChip().Name(targetName)),
+		fsapp.ClickContextMenuItem(targetName, "Unpin from shelf"),
 		uia.WaitUntilGone(holdingspace.FindTray()),
 		uia.EnsureGoneFor(holdingspace.FindTray(), time.Second),
 	)(ctx); err != nil {
-		s.Fatalf("Failed to pin and unpin item %q: %s", fileName, err)
+		s.Fatalf("Failed to pin and unpin item %q: %v", targetName, err)
+	}
+}
+
+// verifyTipInModes verifies that Files app displays the educational tip "Create a shortcut for your files" both in clamshell and tablet mode.
+func verifyTipInModes(ctx context.Context, tconn *chrome.TestConn, isTablet bool) error {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Second)
+	defer cancel()
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, isTablet)
+	if err != nil {
+		return errors.Wrapf(err, "failed to ensure tablet mode enabled is %t", isTablet)
+	}
+	defer cleanup(cleanupCtx)
+
+	return uiauto.New(tconn).WaitUntilExists(nodewith.Name("Create a shortcut for your files").Role(role.StaticText))(ctx)
+}
+
+// createTarget creates the target if it is not in the specific file path.
+func createTarget(targetPath string, isFile bool) error {
+	if isFile {
+		// Create our file, with appropriate permissions so we can delete later.
+		return ioutil.WriteFile(targetPath, []byte("Per aspera, ad astra"), 0644)
 	}
 
+	return os.Mkdir(targetPath, 0755)
 }
