0d2dc8bf95c1dc3be3970a34e9042ea1affa31ae
Josiah K <josiahk@google.com>
2021-09-15 22:13:27 -0500

    tast-tests: Add Chromevox number reading style test
    (Also refactors common code into chromevox/chromevox.go)
    
    BUG=chromium:1159107
    TEST=tast run a11y.ChromevoxNumberReadingStyle
    
    Change-Id: Idbaf083bb99372ed896132b97eb256c91df18973

--- src/chromiumos/tast/local/bundles/cros/a11y/chromevox_number_reading_style.go
+++ src/chromiumos/tast/local/bundles/cros/a11y/chromevox_number_reading_style.go
@@ -12,56 +12,28 @@ import (
 	"chromiumos/tast/local/a11y"
 	"chromiumos/tast/local/audio/crastestclient"
 	"chromiumos/tast/local/bundles/cros/a11y/chromevox"
-	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/policyutil/fixtures"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func: Chromevox,
-		Desc: "A spoken feedback test that executes ChromeVox commands and keyboard shortcuts, and verifies that correct speech is given by the Google and eSpeak TTS engines",
+		Func: ChromevoxNumberReadingStyle,
+		Desc: "Verifies ChromeVox honors its setting to read numbers as words or as digits",
 		Contacts: []string{
-			"akihiroota@chromium.org",      // Test author
+			"josiahk@chromium.org",         // Test author
 			"chromeos-a11y-eng@google.com", // Backup mailing list
 		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
-		Pre:          chrome.LoggedIn(),
-		Params: []testing.Param{{
-			Name: "google_tts",
-			Val: chromevox.VoiceData{
-				VoiceData: a11y.VoiceData{
-					ExtID:  a11y.GoogleTTSExtensionID,
-					Locale: "en-US",
-				},
-				EngineData: a11y.TTSEngineData{
-					ExtID:                     a11y.GoogleTTSExtensionID,
-					UseOnSpeakWithAudioStream: false,
-				},
-			},
-		}, {
-			Name: "espeak",
-			Val: chromevox.VoiceData{
-				VoiceData: a11y.VoiceData{
-					// eSpeak does not come with an English voice built-in, so we need to
-					// use another language. We use Greek here since the voice is built-in
-					// and capable of speaking English words.
-					ExtID:  a11y.ESpeakExtensionID,
-					Locale: "el",
-				},
-				EngineData: a11y.TTSEngineData{
-					ExtID:                     a11y.ESpeakExtensionID,
-					UseOnSpeakWithAudioStream: true,
-				},
-			},
-		}},
+		Fixture:      "chromePolicyLoggedIn",
 	})
 }
 
-func Chromevox(ctx context.Context, s *testing.State) {
-	cr := s.PreValue().(*chrome.Chrome)
+func ChromevoxNumberReadingStyle(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to create Test API connection: ", err)
@@ -73,7 +45,7 @@ func Chromevox(ctx context.Context, s *testing.State) {
 	}
 	defer crastestclient.Unmute(ctx)
 
-	c, err := a11y.NewTabWithHTML(ctx, cr, "<p>Start</p><p>This is a ChromeVox test</p><p>End</p>")
+	c, err := a11y.NewTabWithHTML(ctx, cr, "<p>123</p>")
 	if err != nil {
 		s.Fatal("Failed to open a new tab with HTML: ", err)
 	}
@@ -94,9 +66,10 @@ func Chromevox(ctx context.Context, s *testing.State) {
 	}
 	defer cvconn.Close()
 
-	td := s.Param().(chromevox.VoiceData)
-	vd := td.VoiceData
-	ed := td.EngineData
+	vd := a11y.VoiceData{
+		ExtID:  a11y.GoogleTTSExtensionID,
+		Locale: "en-US",
+	}
 	if err := cvconn.SetVoice(ctx, vd); err != nil {
 		s.Fatal("Failed to set the ChromeVox voice: ", err)
 	}
@@ -106,6 +79,10 @@ func Chromevox(ctx context.Context, s *testing.State) {
 	}
 	defer a11y.SetTTSRate(ctx, tconn, 1.0)
 
+	ed := a11y.TTSEngineData{
+		ExtID:                     a11y.GoogleTTSExtensionID,
+		UseOnSpeakWithAudioStream: false,
+	}
 	sm, err := a11y.RelevantSpeechMonitor(ctx, cr, tconn, ed)
 	if err != nil {
 		s.Fatal("Failed to connect to the TTS background page: ", err)
@@ -124,31 +101,35 @@ func Chromevox(ctx context.Context, s *testing.State) {
 	}{
 		{
 			[]string{chromevox.NextObject},
-			[]a11y.SpeechExpectation{a11y.NewStringExpectation("Start")},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("123")},
+		},
+		{
+			chromevox.OpenOptionsPage,
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("Untitled, tab")},
+		},
+		{
+			[]string{chromevox.Find},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("Find")},
+		},
+		{
+			[]string{"R", "E", "A", "D", chromevox.Space, "N", "U", "M", "B", "E", "R", "S", chromevox.Escape},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("Read numbers as:")},
 		},
 		{
 			[]string{chromevox.NextObject},
-			[]a11y.SpeechExpectation{a11y.NewStringExpectation("This is a ChromeVox test")},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("Words")},
 		},
 		{
-			[]string{chromevox.NextObject},
-			[]a11y.SpeechExpectation{a11y.NewStringExpectation("End")},
+			[]string{chromevox.Activate},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("has pop up")},
 		},
 		{
-			[]string{chromevox.PreviousObject},
-			[]a11y.SpeechExpectation{a11y.NewStringExpectation("This is a ChromeVox test")},
+			[]string{chromevox.ArrowDown},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("Digits")},
 		},
 		{
-			[]string{chromevox.PreviousObject},
-			[]a11y.SpeechExpectation{a11y.NewStringExpectation("Start")},
-		},
-		{
-			[]string{chromevox.JumpToLauncher},
-			[]a11y.SpeechExpectation{a11y.NewRegexExpectation("(Launcher|Back)")},
-		},
-		{
-			[]string{chromevox.JumpToStatusTray},
-			[]a11y.SpeechExpectation{a11y.NewRegexExpectation("Quick Settings*")},
+			[]string{chromevox.Activate, chromevox.PreviousTab, chromevox.NextObject, chromevox.NextObject},
+			[]a11y.SpeechExpectation{a11y.NewStringExpectation("1 2 3")},
 		},
 	}
 
