0f3e8bc8731ab58a56d9adb33c716da5827042a6
Eliot Courtney <edcourtney@google.com>
2021-09-17 13:59:18 +0900

    Implement fixture for WPR.
    BUG=chromium:1244445, b:200234875
    TEST=fast_build.sh -T
    
    Change-Id: Ic82efb7a436b22dc136eb355b7a25ec2368c0bd4

---
+++ src/chromiumos/tast/local/wpr/fixture.go
@@ -0,0 +1,77 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wpr
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+// FixtValue is an interface for accessing WPR FixtValue members.
+type FixtValue interface {
+	FOpt() chrome.OptionsCallback
+}
+
+// FixtValueImpl holds values necessary for composed fixtures to interoperate
+// with WPR.
+type FixtValueImpl struct {
+	fOpt chrome.OptionsCallback
+}
+
+// FOpt gets the Chrome options necessary to run with WPR.
+func (f *FixtValueImpl) FOpt() chrome.OptionsCallback {
+	return f.fOpt
+}
+
+type fixtImpl struct {
+	archiveName string // Specifies archive name for WPR. Must be non-empty.
+	mode        Mode
+	wpr         *WPR // WPR instance.
+}
+
+// NewFixture creates a new fixture that can launch Lacros chrome with the given setup mode,
+// Chrome options, and WPR archive.
+func NewFixture(wprAchiveName string, mode Mode) testing.FixtureImpl {
+	return &fixtImpl{
+		archiveName: wprAchiveName,
+		mode:        mode,
+	}
+}
+
+// SetUp is called by tast before each test is run. We use this method to initialize
+// the fixture data, or return early if the fixture is already active.
+func (f *fixtImpl) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	// Use fixture scoped context so that the WPR server isn't killed early.
+	var err error
+	f.wpr, err = New(s.FixtContext(), f.mode, f.archiveName)
+	if err != nil {
+		s.Fatal("Cannot start WPR")
+	}
+
+	return FixtValueImpl{
+		fOpt: func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return f.wpr.ChromeOptions, nil
+		},
+	}
+}
+
+// TearDown is called after all tests involving this fixture have been run,
+// (or failed to be run if the fixture itself fails).
+func (f *fixtImpl) TearDown(ctx context.Context, s *testing.FixtState) {
+	if f.wpr != nil {
+		f.wpr.Close(ctx)
+		f.wpr = nil
+	}
+}
+
+func (f *fixtImpl) Reset(ctx context.Context) error {
+	return nil
+}
+
+func (f *fixtImpl) PreTest(ctx context.Context, s *testing.FixtTestState) {}
+
+func (f *fixtImpl) PostTest(ctx context.Context, s *testing.FixtTestState) {}
