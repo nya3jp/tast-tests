142b2856a3536fb55d4a99e1a09ccbdd3e776827
John Palmer <jopalmer@chromium.org>
2021-09-17 12:01:37 +1000

    Add tast test for ccurrent time in JP IME
    Lots of compliations for setting time zone
    
    BUG=b:196748043
    TEST=DUT,CQ
    
    Change-Id: I0a186079bd3a37fba40295fccfa1f6be795f139f

--- src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_japanese_typing.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_japanese_typing.go
@@ -6,6 +6,7 @@ package inputs
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"chromiumos/tast/ctxutil"
@@ -51,6 +52,30 @@ func validateInputFieldFromNthCandidate(its *testserver.InputsTestServer, tconn 
 	})
 }
 
+// Auckland so should never be the same as GMT.
+var timezone = "Pacific/Auckland"
+
+func getCurrentTime() []string {
+	loc, err := time.LoadLocation(timezone)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to load timezone: %v", err))
+	}
+	now := time.Now().In(loc)
+
+	return []string{
+		now.Format(("15:04")),
+		now.Add(time.Minute * 1).Format(("15:04")),
+		now.Add(time.Minute * -1).Format(("15:04")),
+	}
+}
+
+// setTimezone ensures that we are not in the GMT timezone which ensures that the IME is taking the time zone offset into account.
+func setTimezone(ctx context.Context, tconn *chrome.TestConn) error {
+	tconn.Eval(ctx, fmt.Sprintf("chrome.chromeosInfoPrivate.set('timezone','%s')", timezone), nil)
+	// Sleep to ensure that the timezone has been changed before the IME is started.
+	return testing.Sleep(ctx, 5000*time.Millisecond)
+}
+
 func PhysicalKeyboardJapaneseTyping(ctx context.Context, s *testing.State) {
 	cr := s.PreValue().(pre.PreData).Chrome
 
@@ -63,6 +88,11 @@ func PhysicalKeyboardJapaneseTyping(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to connect Test API: ", err)
 	}
 	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	// Set the timezone for a test.
+	if err := setTimezone(ctx, tconn); err != nil {
+		s.Fatal("Failed to set time zone")
+	}
 
 	// Add IME for testing.
 	imeCode := ime.ChromeIMEPrefix + s.Param().(ime.InputMethod).ID
@@ -206,6 +236,29 @@ func PhysicalKeyboardJapaneseTyping(ctx context.Context, s *testing.State) {
 						util.WaitForFieldTextToBe(tconn, inputField.Finder(), text),
 					)
 				}),
+			),
+		},
+		{
+			Name: "Correct time returned",
+			Action: uiauto.Combine("get the current time from ime",
+				its.ClearThenClickFieldAndWaitForActive(inputField),
+				kb.TypeAction("ima"), // "now" in Japanese
+				kb.AccelAction("Space"),
+				kb.AccelAction("Space"),
+				kb.AccelAction("Down"), // Current time always seems to be slot #4
+				kb.AccelAction("Down"),
+				kb.AccelAction("Enter"),
+				ui.WaitUntilGone(util.PKCandidatesFinder),
+				util.WaitForFieldTextToSatisfy(tconn, inputField.Finder(), "not this string",
+					func(t string) bool {
+						time := getCurrentTime()
+						for i := 0; i < len(time); i++ {
+							if time[i] == t {
+								return true
+							}
+						}
+						return false
+					}),
 			),
 		},
 	}
