c2e2f2f128aedd2d08c8383c3f0ab9c742903f35
Sarthak Kukreti <sarthakkukreti@chromium.org>
2021-09-17 12:25:47 -0700

    tast-tests: Add query for root device size to spaced
    Additionally, cleanup the free and total disk space functions to use
    more precise measures for disk space.
    
    BUG=b:173086361
    TEST=tast run platform.Spaced
    
    Cq-Depend: chromium:3079086
    Change-Id: I98d2953f20fa50abc24e62bbddb3a395ec6d7a6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3169317
    Reviewed-by: Ricardo Quesada <ricardoq@chromium.org>
    Reviewed-by: Matt Stark <msta@google.com>
    Tast-Review: Ricardo Quesada <ricardoq@chromium.org>
    Tested-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
    Commit-Queue: Sarthak Kukreti <sarthakkukreti@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/platform/spaced.go
+++ src/chromiumos/tast/local/bundles/cros/platform/spaced.go
@@ -6,7 +6,15 @@ package platform
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
 
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/firmware"
 	"chromiumos/tast/local/spaced"
 	"chromiumos/tast/testing"
 )
@@ -20,10 +28,54 @@ func init() {
 	})
 }
 
+// sysfsRootDeviceSize fetches the root device size from /sys/block/<dev>/size.
+func sysfsRootDeviceSize(ctx context.Context) (uint64, error) {
+	// Check actual root device size.
+	rootdev, err := firmware.RootDevice(ctx)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to fetch root device")
+	}
+
+	fp := fmt.Sprintf("/sys/block/%s/size", filepath.Base(rootdev))
+	content, err := ioutil.ReadFile(fp)
+	if err != nil {
+		return 0, errors.Wrapf(err, "reading filepath %s", fp)
+	}
+	size, err := strconv.ParseUint(strings.TrimSpace(string(content)), 10, 64)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to parse root device size as uint64")
+	}
+
+	// Size is in sectors; return in bytes.
+	return size * 512, nil
+}
+
+// statFreeDiskSpace gets the free space on the filesystem using statfs().
+func statFreeDiskSpace(ctx context.Context, path string) (uint64, error) {
+	var stat syscall.Statfs_t
+	if err := syscall.Statfs(path, &stat); err != nil {
+		return 0, errors.Wrapf(err, "failed to get disk stats for %s", path)
+	}
+
+	return stat.Bavail * uint64(stat.Bsize), nil
+}
+
+// statTotalDiskSpace gets the total space on the filesystem using statfs().
+func statTotalDiskSpace(ctx context.Context, path string) (uint64, error) {
+	var stat syscall.Statfs_t
+	if err := syscall.Statfs(path, &stat); err != nil {
+		return 0, errors.Wrapf(err, "failed to get disk stats for %s", path)
+	}
+
+	return stat.Blocks * uint64(stat.Bsize), nil
+}
+
 func Spaced(ctx context.Context, s *testing.State) {
 	const (
-		statefulMount   = "/mnt/stateful_partition/"
-		maxStatefulSize = 1024 * 1024 * 1024 * 1024
+		// Path to check disk space queries on.
+		statefulMount = "/mnt/stateful_partition/"
+		// Disk space margin to consider when comparing against expected values.
+		spaceMarginBytes = 100 * 1024 * 1024
 	)
 
 	spaced, err := spaced.NewClient(ctx)
@@ -32,25 +84,45 @@ func Spaced(ctx context.Context, s *testing.State) {
 	}
 
 	// Check D-Bus queries.
-	freeDiskSpace, err := spaced.GetFreeDiskSpace(ctx, statefulMount)
+	rootDeviceSize, err := spaced.RootDeviceSize(ctx)
+	if err != nil {
+		s.Fatal("Failed to query root device size: ", err)
+	}
+
+	expectedRootDeviceSize, err := sysfsRootDeviceSize(ctx)
+	if err != nil {
+		s.Fatal("Failed to get actual root device size: ", err)
+	}
+
+	if rootDeviceSize != expectedRootDeviceSize {
+		s.Fatalf("Invalid root device size: got %d, want: 0 < size < %d", rootDeviceSize, expectedRootDeviceSize)
+	}
+
+	freeDiskSpace, err := spaced.FreeDiskSpace(ctx, statefulMount)
 	if err != nil {
 		s.Fatal("Failed to query free disk space: ", err)
 	}
 
-	if freeDiskSpace == 0 || freeDiskSpace > maxStatefulSize {
-		s.Fatal("Invalid free disk space: ", freeDiskSpace)
+	expectedFreeDiskSpace, err := statFreeDiskSpace(ctx, statefulMount)
+	if err != nil {
+		s.Fatal("Failed to get expected free disk space: ", err)
 	}
 
-	totalDiskSpace, err := spaced.GetTotalDiskSpace(ctx, statefulMount)
+	if freeDiskSpace <= 0 || freeDiskSpace > expectedFreeDiskSpace+spaceMarginBytes {
+		s.Fatalf("Invalid free disk space;  got %d, want: 0 < size < %d", freeDiskSpace, expectedFreeDiskSpace+spaceMarginBytes)
+	}
+
+	totalDiskSpace, err := spaced.TotalDiskSpace(ctx, statefulMount)
 	if err != nil {
 		s.Fatal("Failed to query total disk space: ", err)
 	}
 
-	if totalDiskSpace == 0 || totalDiskSpace > maxStatefulSize {
-		s.Fatal("Invalid total disk space: ", totalDiskSpace)
+	expectedTotalDiskSpace, err := statTotalDiskSpace(ctx, statefulMount)
+	if err != nil {
+		s.Fatal("Failed to get expected total disk space: ", err)
 	}
 
-	if freeDiskSpace >= totalDiskSpace {
-		s.Fatal("Free disk space: ", freeDiskSpace, " greater than the total disk space: ", totalDiskSpace)
+	if totalDiskSpace <= 0 || totalDiskSpace > expectedTotalDiskSpace+spaceMarginBytes {
+		s.Fatalf("Invalid total disk space;  got %d, want: 0 < size < %d", totalDiskSpace, expectedTotalDiskSpace+spaceMarginBytes)
 	}
 }
