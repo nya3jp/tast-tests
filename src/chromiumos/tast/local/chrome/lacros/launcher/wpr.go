9c090e1d0a7526304c720cc2c1820a2b1e63c185
Eliot Courtney <edcourtney@google.com>
2021-09-17 15:38:53 +0900

    Add composed lacros WPR fixture.
    This allows lacros to be run with WPR.
    
    BUG=chromium:1244445, b:200234875
    TEST=fast_build.sh -T
    
    Change-Id: Iece7624acafc4c042995ea972664e453dd12346a

---
+++ src/chromiumos/tast/local/chrome/lacros/launcher/wpr.go
@@ -0,0 +1,34 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package launcher
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/wpr"
+	"chromiumos/tast/testing"
+)
+
+// NewLacrosWPRFixture creates a new fixture that can launch Lacros chrome with the given setup mode,
+// Chrome options, and WPR archive. This should be the child of a WPR fixture.
+func NewLacrosWPRFixture(mode SetupMode, fOpt chrome.OptionsCallback) testing.FixtureImpl {
+	return NewComposedFixture(mode, func(v *FixtValueImpl, pv interface{}) interface{} {
+		return v
+	}, func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+		opts, err := s.ParentValue().(wpr.FixtValue).FOpt()(ctx, s)
+		if err != nil {
+			return nil, err
+		}
+
+		optsExtra, err := fOpt(ctx, s)
+		if err != nil {
+			return nil, err
+		}
+
+		opts = append(opts, optsExtra...)
+		return opts, nil
+	})
+}
