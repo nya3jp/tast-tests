ba8fa3a8dfdd995f2f8081d2ddf744eca1c72eb0
Andrew Luo <aluo@google.com>
2021-09-20 15:01:48 -0700

    Add serial port communication support.
    BUG=b:197998755
    TEST=fast_build.sh -t chromiumos/tast/common/firmware/serial
    
    Change-Id: Ia891f0901cc39f45284b9f1d6174a284a60fe198

---
+++ src/chromiumos/tast/common/firmware/serial/connected_port.go
@@ -0,0 +1,97 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package serial
+
+import (
+	"context"
+
+	"github.com/tarm/serial"
+
+	"chromiumos/tast/errors"
+)
+
+// ConnectedPort implements Port, delegates to tarm/serial.
+type ConnectedPort struct {
+	port *serial.Port
+}
+
+// Read reads bytes into provided buffer, returns number of bytes read.
+func (p *ConnectedPort) Read(ctx context.Context, buf []byte) (n int, err error) {
+	done := make(chan struct{}, 1)
+	go func() {
+		if p.port == nil {
+			err = errors.New("port already closed")
+		} else {
+			n, err = p.port.Read(buf)
+		}
+		done <- struct{}{}
+	}()
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	case <-done:
+		return n, err
+	}
+}
+
+// Write writes bytes from provided buffer, returns number of bytes written.
+func (p *ConnectedPort) Write(ctx context.Context, buf []byte) (n int, err error) {
+	done := make(chan struct{}, 1)
+	go func() {
+		if p.port == nil {
+			err = errors.New("port already closed")
+		} else {
+			n, err = p.port.Write(buf)
+		}
+		done <- struct{}{}
+	}()
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	case <-done:
+		return n, err
+	}
+}
+
+// Flush flushes un-read/written content from the port.
+func (p *ConnectedPort) Flush(ctx context.Context) error {
+	errC := make(chan error, 1)
+	go func() {
+		if p.port == nil {
+			errC <- errors.New("port already closed")
+		} else {
+			errC <- p.port.Flush()
+		}
+	}()
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-errC:
+		return err
+	}
+}
+
+// Close closes the port.
+func (p *ConnectedPort) Close(ctx context.Context) error {
+	errC := make(chan error, 1)
+	go func() {
+		if p.port == nil {
+			errC <- nil
+			return
+		}
+		err := p.port.Close()
+		if err == nil {
+			p.port = nil
+		}
+		errC <- err
+	}()
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-errC:
+		return err
+	}
+	return nil
+}
