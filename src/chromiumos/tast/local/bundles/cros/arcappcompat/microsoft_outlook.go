8570e0cab5fd6d61c27fed498a13ae0788784b8e
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-09-20 16:51:27 -0700

    Update Tast Test for Microsoft Outlook
    BUG=b:200216335
    TEST=tast run <eve> arcappcompat.MicrosoftOutlook
    
    Disabled the test, as Microsoft is ending it's
    support for the android version of Microsoft outlook
    on Chromebooks.
    
    Change-Id: I0e258ba2c91abaa19075dc3de6cd0b2f982bd182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3171945
    Tested-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Reviewed-by: Renuga Nanjappan <rnanjappan@chromium.org>
    Commit-Queue: Meenakshi Thiyagarajan <mthiyagarajan@google.com>

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/microsoft_outlook.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/microsoft_outlook.go
@@ -34,10 +34,11 @@ var touchviewLaunchForMicrosoftOutlook = []testutil.TestCase{
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         MicrosoftOutlook,
-		Desc:         "Functional test for MicrosoftOutlook that installs the app also verifies it is logged in and that the main page is open, checks MicrosoftOutlook correctly changes the window state in both clamshell and touchview mode",
-		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:appcompat"},
+		Func:     MicrosoftOutlook,
+		Desc:     "Functional test for MicrosoftOutlook that installs the app also verifies it is logged in and that the main page is open, checks MicrosoftOutlook correctly changes the window state in both clamshell and touchview mode",
+		Contacts: []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		// Disabled the test as Microsoft is ending it's support for android version of Microsoft outlook.
+		//Attr:         []string{"group:appcompat"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Name: "clamshell_mode",
