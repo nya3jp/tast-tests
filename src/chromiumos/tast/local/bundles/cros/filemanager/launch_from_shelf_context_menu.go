2fb6c3900ca6e11cc0cf6e0f7c6071ecb5db3ac0
Ben Reich <benreich@chromium.org>
2021-09-21 09:47:08 +1000

    tast-tests: Promote filemanager.LaunchFromShelfContextMenu to critical
    The test has been stable for the past 20+ builds [1] [2] and so
    promoting it to critical.
    
    [1] http://tastboard/test/filemanager.LaunchFromShelfContextMenu
    [2] http://shortn/_xadG4h6anc
    
    BUG=chromium:1238131
    TEST=tast run <DUT-IP> filemanager.LaunchFromShelfContextMenu
    
    Change-Id: I28d66ac591582632ef1749e6f82be48ba7124d42

--- src/chromiumos/tast/local/bundles/cros/filemanager/launch_from_shelf_context_menu.go
+++ src/chromiumos/tast/local/bundles/cros/filemanager/launch_from_shelf_context_menu.go
@@ -28,7 +28,7 @@ func init() {
 			"benreich@chromium.org",
 			"chromeos-files-syd@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      "chromeLoggedIn",
 	})
