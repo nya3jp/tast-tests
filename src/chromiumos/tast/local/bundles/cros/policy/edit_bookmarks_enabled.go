4b32820d73ee85894593f550867fa7905760bb19
Kamil Szarek <kamilszarek@google.com>
2021-09-20 16:24:31 +0000

    policy: Use staticaly defined fixtures names
    Adjust policy tests to use fixtures names that were defined in common
    package.
    
    BUG=None
    TEST=tast run <ip> policy.DictationEnabled
    
    Change-Id: Ide7e9bddebcb74661f4e1ea291875ffcf6c7ecc1

--- src/chromiumos/tast/local/bundles/cros/policy/edit_bookmarks_enabled.go
+++ src/chromiumos/tast/local/bundles/cros/policy/edit_bookmarks_enabled.go
@@ -7,6 +7,7 @@ package policy
 import (
 	"context"
 
+	"chromiumos/tast/common/fixture"
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
@@ -35,7 +36,7 @@ func init() {
 		},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline", "informational"},
-		Fixture:      "chromePolicyLoggedIn",
+		Fixture:      fixture.ChromePolicyLoggedIn,
 	})
 }
 
