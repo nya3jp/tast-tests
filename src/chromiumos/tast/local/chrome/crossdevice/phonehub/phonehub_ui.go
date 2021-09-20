a7c87a4c125309d2e9d7197696773bc3ce516f01
Kyle Shimabukuro <kyleshima@chromium.org>
2021-09-20 14:44:29 -0700

    phonehub: add UI controls for Phone Hub
    Add the necessary UI+JS controls for setting up Phone Hub, displaying it,
    and controlling the "Silence Phone" pod which toggles the Do Not Disturb
    setting on the connected Android phone.
    
    BUG=b:187793760
    TEST=tast run <dut> phonehub.SilencePhone
    
    Change-Id: I7427c95b9b2a97d278416e7fff88c0ce840615ce

---
+++ src/chromiumos/tast/local/chrome/crossdevice/phonehub/phonehub_ui.go
@@ -0,0 +1,187 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package phonehub
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/checked"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+const (
+	settingsURL                 = "chrome://os-settings/"
+	connectedDevicesSettingsURL = "multidevice/features"
+	multidevicePageJS           = `document.querySelector("os-settings-ui").shadowRoot` +
+		`.querySelector("os-settings-main").shadowRoot` +
+		`.querySelector("os-settings-page").shadowRoot` +
+		`.querySelector("settings-multidevice-page")`
+	multideviceSubpageJS = multidevicePageJS + `.shadowRoot` +
+		`.querySelector("settings-multidevice-subpage")`
+	phoneHubToggleJS = multideviceSubpageJS +
+		`.shadowRoot.getElementById("phoneHubItem")` +
+		`.shadowRoot.querySelector("settings-multidevice-feature-toggle")` +
+		`.shadowRoot.getElementById("toggle")`
+	featureCheckedJS               = `.checked`
+	connectedDeviceToggleVisibleJS = multidevicePageJS + `.shouldShowToggle_()`
+)
+
+// Enable enables Phone Hub from OS Settings using JS. Assumes a connected device has already been paired.
+func Enable(ctx context.Context, tconn *chrome.TestConn, cr *chrome.Chrome) error {
+	_, err := ossettings.Launch(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to launch OS settings")
+	}
+	settingsConn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(settingsURL))
+	if err != nil {
+		return errors.Wrap(err, "failed to start Chrome session to OS settings")
+	}
+	defer settingsConn.Close()
+
+	// Use JS to wait for a phone to be connected.
+	if err := settingsConn.WaitForExpr(ctx, multidevicePageJS); err != nil {
+		return errors.Wrap(err, "failed waiting for \"Connected devices\" subpage to load")
+	}
+	if err := settingsConn.WaitForExpr(ctx, connectedDeviceToggleVisibleJS); err != nil {
+		return errors.Wrap(err, "failed to wait for the \"Connected devices\" toggle is visible")
+	}
+
+	// Turn on Phone Hub in the "Connected devices" subpage. The easiest way to get there is to reopen OS Settings on that specific page.
+	_, err = ossettings.LaunchAtPageURL(ctx, tconn, cr, connectedDevicesSettingsURL, func(context.Context) error { return nil })
+	if err != nil {
+		return errors.Wrap(err, "failed to re-launch OS Settings to the multidevice feature page")
+	}
+
+	// Toggle Phone Hub on with JS.
+	if err := settingsConn.WaitForExpr(ctx, phoneHubToggleJS); err != nil {
+		return errors.Wrap(err, "failed to find the Phone Hub toggle")
+	}
+	var enabled bool
+	if err := settingsConn.Eval(ctx, phoneHubToggleJS+featureCheckedJS, &enabled); err != nil {
+		return errors.Wrap(err, "failed to get Phone Hub toggle status")
+	}
+	if !enabled {
+		if err := settingsConn.Eval(ctx, phoneHubToggleJS+`.click()`, nil); err != nil {
+			return errors.Wrap(err, "failed to enable Phone Hub")
+		}
+	}
+	// Check that the toggle was correctly enabled.
+	if err := settingsConn.WaitForExpr(ctx, phoneHubToggleJS+featureCheckedJS+`===true`); err != nil {
+		return errors.Wrap(err, "failed to toggle Phone Hub on using JS")
+	}
+	// Phone Hub is still not immediately ready to use after toggling it on from OS Settings,
+	// since it takes a short amount of time for it to connect to the phone and display anything.
+	// Wait for it to become usable by checking for the existence of a settings pod.
+	ui := uiauto.New(tconn).WithTimeout(30 * time.Second)
+	if err := Show(ctx, tconn); err != nil {
+		return errors.Wrap(err, "failed to show Phone Hub")
+	}
+	defer Hide(ctx, tconn)
+	if err := ui.WaitUntilExists(phoneHubSettingPod.First())(ctx); err != nil {
+		return errors.Wrap(err, "failed to find a Phone Hub setting pod")
+	}
+
+	return nil
+}
+
+// PhoneHubTray is the finder for the Phone Hub tray UI.
+var PhoneHubTray = nodewith.Name("Phone Hub").ClassName("Widget")
+
+// PhoneHubShelfIcon is the finder for the Phone Hub shelf icon.
+var PhoneHubShelfIcon = nodewith.Name("Phone Hub").Role(role.Button).ClassName("PhoneHubTray")
+
+// phoneHubSettingPod is the base UI finder for the individual setting pods.
+var phoneHubSettingPod = nodewith.Ancestor(PhoneHubTray).Role(role.ToggleButton)
+
+// SilencePhonePod is the finder for Phone Hub's Silence Phone pod.
+var SilencePhonePod = phoneHubSettingPod.NameContaining("Toggle Silence phone")
+
+// Show opens Phone Hub if it's not already open.
+func Show(ctx context.Context, tconn *chrome.TestConn) error {
+	ui := uiauto.New(tconn)
+	if err := ui.Exists(PhoneHubTray)(ctx); err == nil { // Phone Hub already open
+		return nil
+	}
+	if err := uiauto.Combine("click Phone Hub shelf icon and wait for it to open",
+		ui.LeftClick(PhoneHubShelfIcon),
+		ui.WaitUntilExists(PhoneHubTray),
+	)(ctx); err != nil {
+		return errors.Wrap(err, "failed to open Phone Hub")
+	}
+	return nil
+}
+
+// Hide hides Phone Hub if it's not already hidden.
+func Hide(ctx context.Context, tconn *chrome.TestConn) error {
+	ui := uiauto.New(tconn)
+	if err := ui.Exists(PhoneHubTray)(ctx); err != nil { // Phone Hub already hidden
+		return nil
+	}
+	if err := uiauto.Combine("click Phone Hub shelf icon and wait for it to close",
+		ui.LeftClick(PhoneHubShelfIcon),
+		ui.WaitUntilGone(PhoneHubTray),
+	)(ctx); err != nil {
+		return errors.Wrap(err, "failed to close Phone Hub")
+	}
+	return nil
+}
+
+// PhoneSilenced returns true if the "Silence phone" pod is active, and false otherwise.
+func PhoneSilenced(ctx context.Context, tconn *chrome.TestConn) (bool, error) {
+	ui := uiauto.New(tconn)
+	info, err := ui.Info(ctx, SilencePhonePod)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to get node info for Silence Phone pod")
+	}
+	if info.Checked == checked.True {
+		return true, nil
+	}
+	return false, nil
+}
+
+// WaitForPhoneSilenced waits for the Phone Silenced pod to be toggled on/off, since its state can be changed from the Android side.
+func WaitForPhoneSilenced(ctx context.Context, tconn *chrome.TestConn, silenced bool, timeout time.Duration) error {
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if curr, err := PhoneSilenced(ctx, tconn); err != nil {
+			return err
+		} else if curr != silenced {
+			return errors.New("current Silence Phone status does not match the desired status")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: timeout}); err != nil {
+		wanted := "off"
+		if silenced {
+			wanted = "on"
+		}
+		return errors.Wrapf(err, "failed waiting for Silence Phone to be turned %v", wanted)
+	}
+	return nil
+}
+
+// ToggleSilencePhonePod toggles Phone Hub's Silence Phone pod on/off.
+func ToggleSilencePhonePod(ctx context.Context, tconn *chrome.TestConn, silence bool) error {
+	ui := uiauto.New(tconn)
+	curr, err := PhoneSilenced(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to check current Silence Phone setting")
+	}
+	if curr == silence {
+		return nil
+	}
+	if err := ui.LeftClick(SilencePhonePod)(ctx); err != nil {
+		return errors.Wrap(err, "failed to click Silence Phone pod")
+	}
+	if err := WaitForPhoneSilenced(ctx, tconn, silence, 15*time.Second); err != nil {
+		return errors.Wrap(err, "failed waiting for Silence Phone pod to be toggled after clicking")
+	}
+	return nil
+}
