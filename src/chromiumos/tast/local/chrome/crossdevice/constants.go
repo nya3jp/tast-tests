f687adb39da0b9af9508e02fc5697512f5625037
Kyle Shimabukuro <kyleshima@chromium.org>
2021-09-20 14:39:18 -0700

    crossdevice: add wrapper for Multidevice Snippet
    Add a wrapper in Tast for the Multidevice Mobly Snippet. This snippet
    provides an RPC for pairing an Android phone with a Chromebook,
    bypassing the multidevice setup flow and enabling use of the "Connected
    devices" features such as Phone Hub, Smart Lock, WiFi Sync, etc. The
    wrapper also includes some basic utilities for logging, and wraps the
    ADB commands used to interact with the phone's Do Not Disturb setting,
    since we will test Phone Hub's interaction with this setting.
    
    BUG=b:187793760
    TEST=tast run <dut> phonehub.SilencePhone
    
    Change-Id: Ia087fc3ac94ba4a4e67a81fcc46621c1da220622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3171705
    Tested-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: David Haddock <dhaddock@chromium.org>
    Tast-Review: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: Kyle Shimabukuro <kyleshima@chromium.org>

---
+++ src/chromiumos/tast/local/chrome/crossdevice/constants.go
@@ -0,0 +1,12 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crossdevice
+
+// Constants used in the adb commands for installing and launching the Multidevice Snippet.
+const (
+	MultideviceSnippetZipName      = "multidevice_snippet.zip"
+	MultideviceSnippetApkName      = "multidevice_snippet.apk"
+	MultideviceSnippetMoblyPackage = "com.google.android.gmscore.integ.modules.auth.proximity.mobly.snippet"
+)
