bb02fdb45b680201ee21e52e2a266a33e9db88e4
Tom Hughes <tomhughes@chromium.org>
2021-09-21 18:05:23 +0000

    Revert "fingerprint: Temporarily make FpUpdaterSucceeded informational"
    The test failure was caused by a real bug, which was fixed in build
    14232.
    
    This reverts commit 726878928a85d74aafebf78df2198913d10f50b3.
    
    BUG=b:200075921
    TEST=CQ passes
    
    Change-Id: Icf0f17ee03ff770de5567ddf66421e15d35d4663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3173064
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Craig Hesling <hesling@chromium.org>
    Reviewed-by: Patryk Duda <patrykd@google.com>
    Tast-Review: Matt Stark <msta@google.com>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/firmware/fp_updater_succeeded.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/fp_updater_succeeded.go
@@ -21,7 +21,7 @@ func init() {
 			"tomhughes@chromium.org",
 			"chromeos-fingerprint@google.com",
 		},
-		Attr:         []string{"group:mainline", "group:fingerprint-cq", "informational"},
+		Attr:         []string{"group:mainline", "group:fingerprint-cq"},
 		SoftwareDeps: []string{"biometrics_daemon"},
 		HardwareDeps: hwdep.D(hwdep.Fingerprint()),
 	})
