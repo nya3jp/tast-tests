bb9d9829b403ef88f9d4ed1a90d62076ef413447
Hyung Tae Kim <hyungtaekim@chromium.org>
2021-09-22 04:09:09 -0700

    lacros-au: Add remote test with skeleton service
    This change adds the initial remote autoupdate test that verifies
    the Lacros update path of Rootfs => Stateful.
    The test will connect to TLS for provisioning, to a test service
    on a DUT for test execution.
    Currently, the test service is just created with skeleton code.
    It will be implemented in a follow-up CL.
    
    See https://sequencediagram.googleplex.com/view/6215323990622208
    for the workflow and go/lacros-autoupdate-test-proposal.
    
    BUG=chromium:1247311
    TEST=fast_build.sh -T (unittest)
    TEST=# Start TLW + TLS locally
    TEST=tast run \
    TEST=  -var=provision.TLSAddr=${ADDR} -var=lacros.UpdateRootfsToStateful.component=lacros-dogfood-dev ${DUT} \
    TEST=  lacros.UpdateRootfsToStateful
    
    Change-Id: I81f05415226aede1ee006fb39d9413e2eb7ab06b

---
+++ src/chromiumos/tast/common/cros/lacros/update.go
@@ -0,0 +1,12 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package lacros
+
+// Constants used by Lacros update remote test or test service.
+const (
+	LacrosRootComponentPath  = "/home/chronos/cros-components/"
+	RootfsLacrosImageFileURL = "file:///opt/google/lacros"
+	BogusComponentUpdaterURL = "http://localhost:12345"
+)
