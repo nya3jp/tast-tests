edca8f2d8f82dadc09cf4b9247ff4e86c247395d
Youkichi Hosoi <youkichihosoi@google.com>
2021-09-24 00:39:14 +0900

    A test to investigate "Too many open files" issue of virtio-fs
    A lot of file descriptors seem to be left open when Files.walkFileTree
    or Files.walk is performed on a directory exported by virtio-fs.
    This test is to locally reproduce the issue.
    arc.TooManyOpenFiles.sdcard creates a lot of files in Android's
    /storage/emulated/0/Documents/ArcTooManyOpenFiles, and launch an Android
    app. You can trigger Files.walk and Files.walkFileTree by pressing the
    corresponding button for each method.
    arc.TooManyOpenFiles.myfiles does the same thing for
    /storage/<MyFiles-UUID>/ArcTooManyOpenFiles. When virtio-fs is used on
    MyFiles, pressing any of the buttons causes "Too many open files"
    exception if numberOfFiles is large enough (~10,000). This is because
    mount-passthrough sets ulimit -n 8192.
    
    BUG=b:182870415
    TEST=(cr) $ cros_workon --board=${BOARD} start tast-local-apks-cros
    TEST=(cr) $ emerge-${BOARD} tast-local-apks-cros
    TEST=(cr) $ cros deploy --board=${BOARD} --root=/usr/local <DUT> \
    TEST=       tast-local-apks-cros
    TEST=(cr) $ tast run <DUT> arc.TooManyOpenFiles.sdcard
    TEST=$ ssh <DUT>
    TEST=# find /proc -name status -exec grep -H virtio-fs {} \;
    TEST=# strace -f -p ${PID-fs} -o ${file-1}
    TEST=Press the "FILES.WALKFILETREE" button on the app window.
    TEST=Wait for a while until Files.walkFileTree is finished.
    TEST=# lsof -p ${PID-fs} > ${file-2}
    TEST=(cr) $ tast run <DUT> arc.TooManyOpenFiles.myfiles
    TEST=# find /proc -name status -exec grep -H virtio_9p {} \;
    TEST=# strace -f -p ${PID-9p} -o ${file-3}
    TEST=Press the "FILES.WALKFILETREE" button on the app window.
    TEST=# lsof -p ${PID-9p} > ${file-4}
    
    Change-Id: Ie3d0aec1c78814027ffee74ef49e147ce4b9c2e3

---
+++ src/chromiumos/tast/local/bundles/cros/arc/too_many_open_files.go
@@ -0,0 +1,134 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/common/android/adb"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/testing"
+)
+
+type arcTooManyOpenFilesParams struct {
+	crosTargetDir    func(ctx context.Context, user, dirname string) (string, error)
+	androidTargetDir func(dirname string) string
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TooManyOpenFiles,
+		Desc:         "Reproduce the \"Too many open files\" error on virtio-fs",
+		Contacts:     []string{"youkichihosoi@chromium.org", "arc-storage@google.com"},
+		SoftwareDeps: []string{"chrome", "android_vm"},
+		Fixture:      "arcBooted",
+		Params: []testing.Param{{
+			Name: "sdcard",
+			Val: arcTooManyOpenFilesParams{
+				crosTargetDir:    crosSdcardTargetDir,
+				androidTargetDir: androidSdcardTargetDir,
+			},
+		}, {
+			Name: "myfiles",
+			Val: arcTooManyOpenFilesParams{
+				crosTargetDir:    crosMyFilesTargetDir,
+				androidTargetDir: androidMyFilesTargetDir,
+			},
+		}},
+	})
+}
+
+func crosSdcardTargetDir(ctx context.Context, user, dirname string) (string, error) {
+	androidDataDir, err := arc.AndroidDataDir(user)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to get android-data path for user %v", user)
+	}
+	documentsDir := filepath.Join(androidDataDir, "data", "media", "0", "Documents")
+	return filepath.Join(documentsDir, dirname), nil
+}
+
+func androidSdcardTargetDir(dirname string) string {
+	return filepath.Join("/storage", "emulated", "0", "Documents", dirname)
+}
+
+func crosMyFilesTargetDir(ctx context.Context, user, dirname string) (string, error) {
+	cryptohomeUserPath, err := cryptohome.UserPath(ctx, user)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to get cryptohome user path for user %v", user)
+	}
+	myfilesDir := filepath.Join(cryptohomeUserPath, "MyFiles")
+	return filepath.Join(myfilesDir, dirname), nil
+}
+
+func androidMyFilesTargetDir(dirname string) string {
+	return filepath.Join("/storage", arc.MyFilesUUID, dirname)
+}
+
+func TooManyOpenFiles(ctx context.Context, s *testing.State) {
+	a := s.FixtValue().(*arc.PreData).ARC
+	cr := s.FixtValue().(*arc.PreData).Chrome
+	params := s.Param().(arcTooManyOpenFilesParams)
+
+	const (
+		// numberOfFiles = 16384
+		numberOfFiles = 1024
+		targetDirName = "ArcTooManyOpenFiles"
+		apkName       = "ArcTooManyOpenFilesTest.apk"
+		packageName   = "org.chromium.arc.testapp.toomanyopenfiles"
+		targetPathKey = "target_path"
+	)
+
+	crosTargetDir, err := params.crosTargetDir(ctx, cr.NormalizedUser(), targetDirName)
+	if err != nil {
+		s.Fatal("Failed to get the target directory path: ", err)
+	}
+
+	if err := os.MkdirAll(crosTargetDir, 0755); err != nil {
+		s.Fatalf("Failed to create the target directory %v: %v", crosTargetDir, err)
+	}
+	// defer os.RemoveAll(crosTargetDir)
+
+	for i := 0; i < numberOfFiles; i++ {
+		dst := filepath.Join(crosTargetDir, fmt.Sprintf("file_%d.txt", i))
+		file, err := os.Create(dst)
+		if err != nil {
+			s.Fatalf("Failed to create file %v: %v", dst, err)
+		}
+		file.Close()
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	if err := a.Install(ctx, arc.APKPath(apkName), adb.InstallOptionGrantPermissions); err != nil {
+		s.Fatalf("Failed to install %v: %v", apkName, err)
+	}
+
+	if err := a.Command(ctx, "appops", "set", packageName, "MANAGE_EXTERNAL_STORAGE", "allow").Run(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("Failed to grant MANAGE_EXTERNAL_STORAGE to %v: %v", packageName, err)
+	}
+
+	act, err := arc.NewActivity(a, packageName, ".MainActivity")
+	if err != nil {
+		s.Fatalf("Failed to create the main activity for %v: %v", packageName, err)
+	}
+	defer act.Close()
+
+	androidTargetDir := params.androidTargetDir(targetDirName)
+	startCommandPrefixes := []string{"-S", "-W", "-n"}
+	startCommandSuffixes := []string{"--es", targetPathKey, androidTargetDir}
+
+	if err := act.StartWithArgs(ctx, tconn, startCommandPrefixes, startCommandSuffixes); err != nil {
+		s.Fatalf("Failed to start the main activity of %v: %v", packageName, err)
+	}
+}
