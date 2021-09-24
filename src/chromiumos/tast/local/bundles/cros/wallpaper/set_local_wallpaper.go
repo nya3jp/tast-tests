ede0744e4e074b56a74b665772559a8a533a9f95
Jason Thai <jasontt@chromium.org>
2021-09-24 16:50:11 +0000

    tast-tests: add set local wallpaper test
    Add a new light_pink.jpg as local image. Use it to test the local
    image flow. Also test out the functionality of the cropping buttons.
    
    BUG=b:198332032
    TEST=tast run wallpaper.SetLocalWallpaper
    
    Change-Id: Ib2f97e8f2bc63f54a0e49eef52922e72714bb656

--- src/chromiumos/tast/local/bundles/cros/wallpaper/set_local_wallpaper.go
+++ src/chromiumos/tast/local/bundles/cros/wallpaper/set_local_wallpaper.go
@@ -6,15 +6,17 @@ package wallpaper
 
 import (
 	"context"
+	"image/color"
+	"io/ioutil"
 	"path/filepath"
 	"time"
 
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ui/filesapp"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
-	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/media/imgcmp"
 	"chromiumos/tast/local/screenshot"
 	"chromiumos/tast/local/wallpaper"
@@ -23,31 +25,39 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func: DailyRefresh,
-		Desc: "Test enabling daily refresh in the new wallpaper app",
+		Func: SetLocalWallpaper,
+		Desc: "Test setting online wallpapers in the new wallpaper app",
 		Contacts: []string{
 			"jasontt@google.com",
-			"croissant-eng@google.com",
+			"chromeos-sw-engprod@google.com",
+			"assistive-eng@google.com",
 		},
 		Attr:         []string{"group:mainline", "informational"},
+		Data:         []string{"light_pink.jpg"},
 		SoftwareDeps: []string{"chrome"},
 	})
 }
 
-// DailyRefresh tests enabling daily refresh and compares the new wallpaper with the old one.
-func DailyRefresh(ctx context.Context, s *testing.State) {
+func SetLocalWallpaper(ctx context.Context, s *testing.State) {
+	const (
+		filename   = "light_pink.jpg"
+		collection = "My Images"
+	)
+	filePath := filepath.Join(filesapp.DownloadPath, filename)
+
 	cr, err := chrome.New(ctx, chrome.EnableFeatures("WallpaperWebUI"))
 	if err != nil {
 		s.Fatal("Failed to connect to Chrome: ", err)
 	}
 	defer cr.Close(ctx)
 
-	// Open a keyboard device.
-	kb, err := input.Keyboard(ctx)
+	expected, err := ioutil.ReadFile(s.DataPath(filename))
 	if err != nil {
-		s.Fatal("Failed to open keyboard device: ", err)
+		s.Fatal("Could not read the test file: ", err)
 	}
-	defer kb.Close()
+	if err = ioutil.WriteFile(filePath, expected, 0755); err != nil {
+		s.Fatalf("Could not write to %s: %v", filePath, err)
+	}
 
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
@@ -55,8 +65,34 @@ func DailyRefresh(ctx context.Context, s *testing.State) {
 	}
 	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
 
-	// The test has a dependency of network speed, so we give uiauto.Context ample time to wait for nodes to load.
+	// The test has a dependency of network speed, so we give uiauto.Context ample
+	// time to wait for nodes to load.
 	ui := uiauto.New(tconn).WithTimeout(30 * time.Second)
+
+	if err := wallpaper.OpenWallpaperPicker(ui)(ctx); err != nil {
+		s.Fatal("Failed to open wallpaper picker: ", err)
+	}
+	if err := wallpaper.SelectCollection(ui, collection)(ctx); err != nil {
+		s.Fatalf("Failed to select collection %q: %v", collection, err)
+	}
+	if err := wallpaper.SelectImage(ui, filename)(ctx); err != nil {
+		s.Fatalf("Failed to select image %q: %v", filename, err)
+	}
+
+	windowNode := nodewith.NameContaining("Wallpaper").Role(role.Window).First()
+	minimizeBtn := nodewith.Name("Minimize").Role(role.Button).Ancestor(windowNode)
+	// Minimize window to get the view of wallpaper image.
+	if ui.LeftClick(minimizeBtn)(ctx); err != nil {
+		s.Fatal("Failed to minimize window: ", err)
+	}
+
+	pink := color.RGBA{255, 203, 198, 255}
+	// percentage takes into account the center cropped image is similar to the filled
+	// one.
+	const expectedPercent = 85
+	if err := wallpaper.ValidateBackground(ctx, cr, pink, expectedPercent); err != nil {
+		s.Error("Failed to validate wallpaper background: ", err)
+	}
 
 	// Take a screenshot of the current wallpaper.
 	firstScreenshot, err := screenshot.GrabScreenshot(ctx, cr)
@@ -67,31 +103,24 @@ func DailyRefresh(ctx context.Context, s *testing.State) {
 	if err := wallpaper.OpenWallpaperPicker(ui)(ctx); err != nil {
 		s.Fatal("Failed to open wallpaper picker: ", err)
 	}
-
-	const collection = "Solid colors"
 	if err := wallpaper.SelectCollection(ui, collection)(ctx); err != nil {
 		s.Fatalf("Failed to select collection %q: %v", collection, err)
 	}
-
-	if err := uiauto.Combine("Enable daily refresh",
-		ui.LeftClick(nodewith.Name("Change wallpaper image daily").Role(role.ToggleButton)),
-		ui.WaitUntilExists(nodewith.Name("Refresh the current wallpaper image").Role(role.Button)),
-	)(ctx); err != nil {
-		s.Fatal("Failed to enable daily refresh: ", err)
+	if err := ui.LeftClick(nodewith.Name("Center").Role(role.Button))(ctx); err != nil {
+		s.Fatal("Failed to select center option : ", err)
 	}
 
 	if err := wallpaper.MinimizeWallpaperPicker(ui)(ctx); err != nil {
 		s.Fatal("Failed to minimize wallpaper picker: ", err)
 	}
 
-	// Take a screenshot of the new wallpaper.
+	// Take a screenshot of the same wallpaper with new layout.
 	secondScreenshot, err := screenshot.GrabScreenshot(ctx, cr)
 	if err != nil {
 		s.Fatal("Failed to grab screenshot: ", err)
 	}
 
 	// Verify that the wallpaper has indeed changed.
-	const expectedPercent = 90
 	if err = wallpaper.ValidateDiff(firstScreenshot, secondScreenshot, expectedPercent); err != nil {
 		firstScreenshotPath := filepath.Join(s.OutDir(), "screenshot_1.png")
 		secondScreenshotPath := filepath.Join(s.OutDir(), "screenshot_2.png")
