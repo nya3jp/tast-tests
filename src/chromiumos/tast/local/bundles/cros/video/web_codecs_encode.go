4936caf5e1104abfb95ff12b9ddbf57198d1ee45
Hirokazu Honda <hiroh@chromium.org>
2021-09-24 14:58:04 +0900

    video: Add encoder test in Webcodecs API
    This CL introduces the first test to verify WebCodecs.
    WebCodecs API enables a web app to encode a video frame using
    native encoders in Chrome. In the test, we encode the frames by
     software or hardware encoder using WebCodecs API and verifies
    the encoded bitstream by PSNR and SSIM.
    
    The webcodecs javascript code is inspired by webcodecs_common.js
    in chromium [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:content/test/data/gpu/webcodecs/webcodecs_common.js
    
    BUG=b:196307009
    TEST=video.WebcodecsEncode.*
    
    Change-Id: I57535af913a57fab62ab0ba9bc244eb636ca5fe7

---
+++ src/chromiumos/tast/local/bundles/cros/video/web_codecs_encode.go
@@ -0,0 +1,61 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package video
+
+import (
+	"context"
+
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/local/bundles/cros/video/webcodecs"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/media/videotype"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: WebCodecsEncode,
+		Desc: "Verifies that WebCodecs API works, maybe verifying use of a hardware accelerator",
+		Contacts: []string{
+			"hiroh@chromium.org", // Test author.
+			"chromeos-gfx-video@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Data:         append(append(webcodecs.MP4DemuxerDataFiles(), webcodecs.EncodeDataFiles()...), webcodecs.VideoDataFiles()...),
+		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
+		Fixture:      "chromeWebCodecs",
+		Params: []testing.Param{{
+			Name:              "h264_sw",
+			Val:               webcodecs.TestArgs{Codec: videotype.H264, Acceleration: webcodecs.PreferSoftware},
+			ExtraSoftwareDeps: []string{"proprietary_codecs"},
+		}, {
+			Name:              "h264_hw",
+			Val:               webcodecs.TestArgs{Codec: videotype.H264, Acceleration: webcodecs.PreferHardware},
+			ExtraSoftwareDeps: []string{"proprietary_codecs", caps.HWEncodeH264},
+		}, {
+			Name: "vp8_sw",
+			Val:  webcodecs.TestArgs{Codec: videotype.VP8, Acceleration: webcodecs.PreferSoftware},
+		}, {
+			Name:              "vp8_hw",
+			Val:               webcodecs.TestArgs{Codec: videotype.VP8, Acceleration: webcodecs.PreferHardware},
+			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
+		}, {
+			Name: "vp9_sw",
+			Val:  webcodecs.TestArgs{Codec: videotype.VP9, Acceleration: webcodecs.PreferSoftware},
+		}, {
+			Name:              "vp9_hw",
+			Val:               webcodecs.TestArgs{Codec: videotype.VP9, Acceleration: webcodecs.PreferHardware},
+			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
+		}},
+	})
+}
+
+func WebCodecsEncode(ctx context.Context, s *testing.State) {
+	args := s.Param().(webcodecs.TestArgs)
+	if err := webcodecs.RunEncodeTest(ctx, s.FixtValue().(*chrome.Chrome),
+		s.DataFileSystem(), args, s.DataPath(webcodecs.Crowd720p), s.OutDir()); err != nil {
+		s.Error("test failed: ", err)
+	}
+}
