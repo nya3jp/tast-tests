458e2eb112069af66e1e0b5c3cec91c498445a59
Haruka Kumagai <harukakumagai@google.com>
2021-09-24 16:04:21 +0900

    lockscreen: use uiauto/ instead of ui/
    The lockscreen package in the UI test support library has a newer version in
    uiauto/. Update all tests using it to use the newer version.
    
    BUG=b:191923912
    TEST=fast_build.sh -T
    
    Change-Id: I15e34e1d155f08c59ca5e3762c98e6b2645806d0

--- src/chromiumos/tast/local/bundles/cros/quicksettings/lock_screen.go
+++ src/chromiumos/tast/local/bundles/cros/quicksettings/lock_screen.go
@@ -9,10 +9,10 @@ import (
 	"time"
 
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/ui/lockscreen"
 	"chromiumos/tast/local/chrome/ui/quicksettings"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/lockscreen"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/input"
