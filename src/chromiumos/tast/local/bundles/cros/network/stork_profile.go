f6eba9a77043113c37a18f980ccd82626bfad8a5
Kyle Horimoto <khorimoto@chromium.org>
2021-09-25 23:09:38 +0000

    Add test for invoking Stork API from DUT
    This CL adds a small library for fetching an eSIM profile activation
    code using the Stork API, then adds a simple test which verifies this
    flow. In the future, additional tests will be added which use the
    library to fetch a profile, the install it on the device.
    
    BUG=None
    TEST=tast run ${DUT} network.StorkProfile
    
    Change-Id: I904e17bc5aab1333c73d09d0bbb467e2beb2af94

---
+++ src/chromiumos/tast/local/bundles/cros/network/stork_profile.go
@@ -0,0 +1,43 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/stork"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: StorkProfile,
+		Desc: "Verifies that the Stork API can be invoked by a device",
+		Contacts: []string{
+			"khorimoto@google.com",
+			"pholla@google.com",
+			"chromeos-cellular-team@google.com",
+		},
+		Attr:    []string{"group:cellular", "cellular_unstable", "cellular_sim_test_esim"},
+		Timeout: 5 * time.Minute,
+	})
+}
+
+func StorkProfile(ctx context.Context, s *testing.State) {
+	ctxForCleanup := ctx
+
+	ctx, cancel := ctxutil.Shorten(ctx, stork.CleanupProfileTime)
+	defer cancel()
+
+	activationCode, cleanupFunc, err := stork.FetchStorkProfile(ctx)
+	if err != nil {
+		s.Fatal("Failed to fetch Stork profile: ", err)
+	}
+
+	defer cleanupFunc(ctxForCleanup)
+	s.Log("Fetched Stork profile with activation code: ", activationCode)
+}
