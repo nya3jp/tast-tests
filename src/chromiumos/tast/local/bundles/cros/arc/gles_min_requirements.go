90ed422acec78e87d71ee1d4e116087d1112ab9b
Cici Ruan <cuicuiruan@google.com>
2021-09-27 11:42:31 -0700

    tast-tests/arc: Promote arc.GLESMinRequirements.vm except betty
    This is known fixture failure.
    Betty is still running on GLES 3.0 and not in priority to support for
    GLES 3.1.
    
    BUG=b:198994939
    TEST=tast run DUT arc.GLESMinRequirements.vm
    
    Change-Id: I4028433957f115eaa1a1f699982abf243e0a30d7

--- src/chromiumos/tast/local/bundles/cros/arc/gles_min_requirements.go
+++ src/chromiumos/tast/local/bundles/cros/arc/gles_min_requirements.go
@@ -30,9 +30,9 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         GLESMinRequirements,
 		Desc:         "Checks whether the OpenGL ES minimun requirements are satisfied",
-		Contacts:     []string{"ricardo@chromium.org", "arc-gaming+tast@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"chrome"},
+		Contacts:     []string{"ricardoq@chromium.org", "arc-gaming+tast@google.com"},
+		Attr:         []string{"group:mainline"},
+		SoftwareDeps: []string{"chrome", "no_qemu"},
 		Fixture:      "arcBooted",
 		Params: []testing.Param{{
 			ExtraSoftwareDeps: []string{"android_p"},
