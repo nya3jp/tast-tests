80ae991b27325e54448eaa9ecf56e29b5c6eddcb
Charles William Dick <cwd@google.com>
2021-09-27 13:55:02 +0900

    ChromeOS available memory metrics
    This CL introduces three new metrics:
    * chromeos_margin_critical
    * chromeos_margin_moderate
    * chromeos_available
    
    These values are queried from resourced via D-Bus.
    
    BUG=b:201241089
    TEST=tast run dut multivm.Login.arc
    
    Change-Id: I9f58311162dd1e28b728fbcf03b8fe005824b408

---
+++ src/chromiumos/tast/local/memory/available.go
@@ -0,0 +1,53 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package memory
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/resourced"
+)
+
+// ChromeOSAvailableMetrics logs performance metrics for ChromeOS memory margins
+// and ChromeOS's available memory counter.
+func ChromeOSAvailableMetrics(ctx context.Context, p *perf.Values, suffix string) error {
+	rm, err := resourced.NewClient(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create Resource Manager client")
+	}
+	margins, err := rm.MemoryMarginsKB(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get ChromeOS memory margins")
+	}
+	available, err := rm.AvailableMemoryKB(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get ChromeOS available memory")
+	}
+	p.Set(
+		perf.Metric{
+			Name: fmt.Sprintf("chromeos_margin_critical%s", suffix),
+			Unit: "MiB",
+		},
+		float64(margins.CriticalKB)/KiB,
+	)
+	p.Set(
+		perf.Metric{
+			Name: fmt.Sprintf("chromeos_margin_moderate%s", suffix),
+			Unit: "MiB",
+		},
+		float64(margins.ModerateKB)/KiB,
+	)
+	p.Set(
+		perf.Metric{
+			Name: fmt.Sprintf("chromeos_available%s", suffix),
+			Unit: "MiB",
+		},
+		float64(available)/KiB,
+	)
+	return nil
+}
