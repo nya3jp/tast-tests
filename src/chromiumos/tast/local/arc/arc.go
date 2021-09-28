6d82ebc592a1277e4a0eef0c4774f7d026dc1442
Yury Khmel <khmel@chromium.org>
2021-09-28 15:35:15 -0700

    arc: temporarily increase timeout for ARC boot.
    This is to let ARCVM passes critical test for zork/grunt and unblock
    PFQ.
    
    BUG=b:188941140
    TEST=Locally: tast run ${DUT} arc.Boot.vm starts passing
    
    Change-Id: I15d4eda3d8c53b4406b5600a53de047d1c645355

--- src/chromiumos/tast/local/arc/arc.go
+++ src/chromiumos/tast/local/arc/arc.go
@@ -32,7 +32,8 @@ import (
 const (
 	// BootTimeout is the maximum amount of time that ARC is expected to take to boot.
 	// Tests that call New should declare a timeout that's at least this long.
-	BootTimeout = 120 * time.Second
+	// TODO(b/188941140): temporarily increase timeout to let ARCVM passes criticial test for zork/grunt.
+	BootTimeout = 240 * time.Second
 
 	// Time Android init process takes to start. It should be smaller than BootTimeout.
 	androidInitTimeout = 60 * time.Second
