912dde152c89fa50d696ea3f02bd8c567b15b504
Alston Huang <alston.huang@cienet.com>
2021-09-30 18:05:11 +0800

    cuj: Productivity - MS Office sign in
    Implement the sign in procedure in MS Office. Check if it is signed into a Microsoft account. If not, try to sign in.
    
    BUG=b:201631935
    TEST=tast run <vars> <dut> ui.MicrosoftOfficeWebCUJ.*
    
    Change-Id: I43f01f25512d41390af1e1a1e51fff9dc329398e

--- src/chromiumos/tast/local/bundles/cros/ui/productivitycuj/microsoft_web.go
+++ src/chromiumos/tast/local/bundles/cros/ui/productivitycuj/microsoft_web.go
@@ -7,10 +7,15 @@ package productivitycuj
 import (
 	"context"
 
+	"chromiumos/tast/common/action"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/ui/cuj"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
 )
 
 // MicrosoftWebOffice implements the ProductivityApp interface.
@@ -78,6 +83,119 @@ func (app *MicrosoftWebOffice) VoiceToTextTesting(ctx context.Context, expectedT
 // End cleans up case. Removes the document and slide which we created in the test case and close all tabs.
 func (app *MicrosoftWebOffice) End(ctx context.Context) error {
 	return nil
+}
+
+// checkSignIn checks if it is logged in, if not, try to log in.
+func (app *MicrosoftWebOffice) checkSignIn(ctx context.Context) error {
+	testing.ContextLog(ctx, "Signing in to microsoft account")
+
+	// If the account manager exists, it means it has been logged in. Skip the login procedure.
+	accountManager := nodewith.NameContaining("Account manager for").Role(role.Button)
+	if err := app.ui.WithTimeout(longerUIWaitTime).WaitUntilExists(accountManager)(ctx); err != nil {
+		testing.ContextLog(ctx, "Clicking the sign in link")
+
+		msWebArea := nodewith.NameContaining("Microsoft Office").Role(role.RootWebArea)
+		signInLink := nodewith.NameContaining("Sign in").Role(role.Link).Ancestor(msWebArea).First()
+		if err := app.ui.IfSuccessThen(
+			app.ui.WithTimeout(defaultUIWaitTime).WaitUntilExists(signInLink),
+			app.uiHdl.Click(signInLink),
+		)(ctx); err != nil {
+			return err
+		}
+
+		accountLocked := nodewith.Name("Your account has been locked").Role(role.StaticText)
+		// If the message exists, it means the account has been locked. We can only recover it manually.
+		if err := app.ui.WithTimeout(defaultUIWaitTime).WaitUntilExists(accountLocked)(ctx); err != nil {
+			return app.signIn(ctx)
+		}
+
+		return errors.New("failed to sign in microsoft office, your account has been locked")
+	}
+
+	testing.ContextLog(ctx, "Account has been logged in")
+	return nil
+}
+
+// checkIfAccountFilled checks if the account field is automatically filled in.
+func (app *MicrosoftWebOffice) checkIfAccountFilled(expected string) action.Action {
+	return func(ctx context.Context) error {
+		accountField := nodewith.NameContaining("Enter your email").Role(role.TextField)
+		node, err := app.ui.Info(ctx, accountField)
+		if err != nil {
+			return err
+		}
+		if node.Value == "" {
+			return app.kb.TypeAction(app.username)(ctx)
+		}
+		if node.Value != expected {
+			// If it has filled in the wrong account information, select all first and then enter the information.
+			testing.ContextLog(ctx, "Filled in the wrong account information")
+			return uiauto.Combine("fill in the account",
+				app.kb.AccelAction("Ctrl+A"),
+				app.kb.TypeAction(app.username),
+			)(ctx)
+		}
+		// If it has filled in the correct account information, skip entering it again.
+		testing.ContextLog(ctx, "Filled in the correct account information")
+		return nil
+	}
+}
+
+// signIn signs in to Microsoft Office account.
+func (app *MicrosoftWebOffice) signIn(ctx context.Context) error {
+	testing.ContextLog(ctx, "Signing in to Microsoft Office")
+
+	nextButton := nodewith.Name("Next").Role(role.Button)
+
+	fillAccount := func(ctx context.Context) error {
+		testing.ContextLog(ctx, "Filling in the account field")
+		return uiauto.Combine("fill in the account",
+			app.checkIfAccountFilled(app.username),
+			app.uiHdl.Click(nextButton),
+		)(ctx)
+	}
+
+	passwordField := nodewith.Name("Enter the password for " + app.username).Role(role.TextField)
+	signInButton := nodewith.Name("Sign in").Role(role.Button)
+	staySignInHeading := nodewith.Name("Stay signed in?").Role(role.Heading)
+	yesButton := nodewith.Name("Yes").Role(role.Button)
+	closeButton := nodewith.Name("Close first run experience").Role(role.Button)
+
+	fillPassword := func(ctx context.Context) error {
+		testing.ContextLog(ctx, "Filling in the password field")
+		return uiauto.Combine("fill in the password",
+			app.ui.DoubleClick(passwordField),
+			app.kb.TypeAction(app.password),
+			app.uiHdl.Click(signInButton),
+			app.ui.IfSuccessThen(app.ui.WithTimeout(defaultUIWaitTime).WaitUntilExists(staySignInHeading), app.uiHdl.Click(yesButton)),
+			app.ui.IfSuccessThen(app.ui.WithTimeout(defaultUIWaitTime).WaitUntilExists(closeButton), app.uiHdl.Click(closeButton)),
+		)(ctx)
+	}
+
+	accountList := nodewith.Name("Pick an account").Role(role.List)
+	accountButton := nodewith.NameContaining(app.username).Role(role.Button).Ancestor(accountList)
+	// If we have logged in before, sometimes it will show a "Pick an account" list.
+	if err := app.ui.IfSuccessThen(
+		app.ui.WithTimeout(defaultUIWaitTime).WaitUntilExists(accountButton),
+		app.uiHdl.Click(accountButton),
+	)(ctx); err != nil {
+		return err
+	}
+
+	accountField := nodewith.NameContaining("Enter your email").Role(role.TextField)
+	// If we select the account option in the "Pick an account" list, there is no need to fill in the account field.
+	if err := app.ui.IfSuccessThen(
+		app.ui.WithTimeout(defaultUIWaitTime).WaitUntilExists(accountField),
+		fillAccount,
+	)(ctx); err != nil {
+		return err
+	}
+
+	// Sometimes it will login directly without filling in the password.
+	return app.ui.IfSuccessThen(
+		app.ui.WithTimeout(defaultUIWaitTime).WaitUntilExists(passwordField),
+		fillPassword,
+	)(ctx)
 }
 
 // NewMicrosoftWebOffice creates MicrosoftWebOffice instance which implements ProductivityApp interface.
