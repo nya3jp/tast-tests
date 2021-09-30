6840f3ae1c6e95dedbee21e7299e0d6b3c40435b
Jason Thai <jasontt@chromium.org>
2021-09-30 22:20:52 +0000

    tast-tests: skip new wallpaper tests for tablet mode
    In tablet mode, the wallpaper is behind the app launcher so it
    is trick to validate the similary between the wallpaper itself
    and the given rgba color. We should skip these tests when running
    in tablet mode.
    
    BUG=none
    TEST=locally run existing tests
    
    Change-Id: Ib7b218d182021c03cc5a875001f3ae7e2a130934

--- src/chromiumos/tast/local/bundles/cros/wallpaper/daily_refresh.go
+++ src/chromiumos/tast/local/bundles/cros/wallpaper/daily_refresh.go
@@ -10,11 +10,11 @@ import (
 	"time"
 
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
-	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/media/imgcmp"
 	"chromiumos/tast/local/screenshot"
 	"chromiumos/tast/local/wallpaper"
@@ -42,18 +42,21 @@ func DailyRefresh(ctx context.Context, s *testing.State) {
 	}
 	defer cr.Close(ctx)
 
-	// Open a keyboard device.
-	kb, err := input.Keyboard(ctx)
-	if err != nil {
-		s.Fatal("Failed to open keyboard device: ", err)
-	}
-	defer kb.Close()
-
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to create Test API connection: ", err)
 	}
 	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// The wallpaper in tablet mode is behind the view of app icons so it is tricky to
+	// compare the wallpaper and the given rgba color. Skipping the test in tablet mode
+	// for now.
+	if tabletModeEnabled, err := ash.TabletModeEnabled(ctx, tconn); err != nil {
+		s.Fatal("Failed to get tablet mode: ", err)
+	} else if tabletModeEnabled {
+		s.Log("Device is in tablet mode. Skipping test")
+		return
+	}
 
 	// The test has a dependency of network speed, so we give uiauto.Context ample time to wait for nodes to load.
 	ui := uiauto.New(tconn).WithTimeout(30 * time.Second)
