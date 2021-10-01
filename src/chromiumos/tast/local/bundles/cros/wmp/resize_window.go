3e2a3f2b86285536d062d9d8db415d802f58322d
Lance Wang <lance.wang@cienet.com>
2021-10-01 12:06:00 +0800

    mta: add test case of wmp.ResizeWindow
    [MTA e8bb4] Resize window by dragging 4 corners and 4 sides.
    
    BUG=b:200059312
    TEST=tast run <DUT> wmp.ResizeWindow*
    
    Change-Id: I049f21a87f5b4102c7b8b1a46cd47c5103a58fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3412579
    Reviewed-by: Yichen Zhou <yichenz@chromium.org>
    Reviewed-by: David Welling <davidwelling@google.com>
    Tested-by: Alfred Yu <alfred.yu@cienet.com>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Alfred Yu <alfred.yu@cienet.com>

---
+++ src/chromiumos/tast/local/bundles/cros/wmp/resize_window.go
@@ -0,0 +1,262 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wmp
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/bundles/cros/wmp/wmputils"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/cws"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ResizeWindow,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Resize different windows by dragging 4 corners and 4 sides",
+		Contacts: []string{
+			"lance.wang@cienet.com",
+			"cienet-development@googlegroups.com",
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Vars:         []string{"ui.gaiaPoolDefault"}, // GAIA is required to install an app from Chrome Webstore.
+		Params: []testing.Param{
+			{
+				Val:     false, // Expect not to test on ARC++ app.
+				Timeout: 5*time.Minute + cws.InstallationTimeout,
+			}, {
+				Name:              "arc",
+				Val:               true, // Expect to test on ARC++ app.
+				ExtraSoftwareDeps: []string{"arc"},
+				Timeout:           5 * time.Minute,
+			}},
+	})
+}
+
+// ResizeWindow tests that resizing windows by dragging 4 corners and 4 sides.
+func ResizeWindow(ctx context.Context, s *testing.State) {
+	isArc := s.Param().(bool)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	var opts []chrome.Option
+	if isArc {
+		opts = append(opts, chrome.ARCEnabled(), chrome.FakeLogin(chrome.Creds{User: "testuser@gmail.com", Pass: "test1234"}))
+	} else {
+		opts = append(opts, chrome.GAIALoginPool(s.RequiredVar("ui.gaiaPoolDefault")))
+	}
+
+	// Avoid share session with other tests to ensure the window size is in initial state.
+	cr, err := chrome.New(ctx, opts...)
+	if err != nil {
+		s.Fatal("Failed to Chrome login: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get the connection to the test API: ", err)
+	}
+
+	var appList []*wmputils.ResizeApp
+	if !isArc {
+		// Install CWS app: Text.
+		cwsApp := newCwsAppText(cr, tconn)
+
+		if err := cwsApp.install(ctx); err != nil {
+			s.Fatal("Failed to install CWS Text app: ", err)
+		}
+		defer cwsApp.uninstall(cleanupCtx)
+
+		// Find correct Chrome browser app.
+		chromeApp, err := apps.ChromeOrChromium(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to find Chrome or Chromium app: ", err)
+		}
+
+		appList = []*wmputils.ResizeApp{
+			{
+				Name:         cwsApp.App.Name,
+				ID:           cwsApp.id,
+				IsArcApp:     false,
+				WindowFinder: cwsApp.windowFinder,
+			}, {
+				Name:         chromeApp.Name,
+				ID:           chromeApp.ID,
+				IsArcApp:     false,
+				WindowFinder: nodewith.HasClass("NonClientView").NameContaining(chromeApp.Name),
+			}, {
+				Name:         apps.Files.Name,
+				ID:           apps.Files.ID,
+				IsArcApp:     false,
+				WindowFinder: filesapp.WindowFinder(apps.Files.ID),
+			},
+		}
+	} else {
+		appList = []*wmputils.ResizeApp{
+			// Choose PlayStore as an ARC++ app so that it won't have to install any extra ARC++ app.
+			{
+				Name: apps.PlayStore.Name,
+				// The app "Play Store" isn't unified depending on different models.
+				// e.g.: the app is apps.PlayStore.ID on Volteer,
+				// "acemnolionkahnbnbangggohjkggjfpl" on Hayato,
+				// "cnbgggchhmkkdmeppjobngjoejnihlei" on Berknip.
+				// Thus, the ID isn't specified in this test.
+				ID:           "",
+				IsArcApp:     true,
+				WindowFinder: nodewith.HasClass("RootView").NameContaining("Google Play Store"),
+			},
+		}
+	}
+
+	for _, app := range appList {
+		f := func(ctx context.Context, s *testing.State) {
+			if err := resizeSubTest(ctx, cr, tconn, app, s.OutDir()); err != nil {
+				s.Fatal("Failed to resize window: ", err)
+			}
+		}
+
+		if !s.Run(ctx, fmt.Sprintf("resize on app %s", app.Name), f) {
+			s.Error("Failed to test resize functionality on app ", app.Name)
+		}
+	}
+}
+
+func resizeSubTest(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn, resizeApp *wmputils.ResizeApp, ourDir string) (retErr error) {
+	cleanupSubTestCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	if err := launcher.LaunchApp(tconn, resizeApp.Name)(ctx); err != nil {
+		return errors.Wrapf(err, "failed to launch %s", resizeApp.Name)
+	}
+	defer closeApp(cleanupSubTestCtx, tconn, resizeApp)
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupSubTestCtx, ourDir, func() bool { return retErr != nil }, cr, resizeApp.Name)
+
+	// Ensure only one window is shown.
+	// When running ARC subcase, Google Play Store will display two windows, which will cause the test to fail.
+	// We will wait until one window is disappeared automatically.
+	var window *ash.Window
+	if err := testing.Poll(ctx, func(ctx context.Context) (err error) {
+		if window, err = ash.FindOnlyWindow(ctx, tconn, func(w *ash.Window) bool { return w.IsActive }); err != nil {
+			return errors.Wrap(err, "failed to get active window")
+		}
+
+		return nil
+	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+		return errors.Wrap(err, "failed to wait for window to be shown")
+	}
+
+	if err := ash.SetWindowStateAndWait(ctx, tconn, window.ID, ash.WindowStateNormal); err != nil {
+		return errors.Wrap(err, "failed to set window state to normal")
+	}
+
+	if err := uiauto.New(tconn).WaitForLocation(resizeApp.WindowFinder)(ctx); err != nil {
+		return errors.Wrap(err, "failed to wait for window to stabilize")
+	}
+
+	// Minimize the window in advance to avoid its border being off-screen after the window has been dragged to the center of screen.
+	if err := resizeApp.ResizeWindowToMin(tconn, resizeApp.WindowFinder)(ctx); err != nil {
+		return errors.Wrap(err, "failed to minimize window")
+	}
+	testing.ContextLog(ctx, "Window has been resized to minimize")
+
+	// Move the window to the center of the screen to observe the test.
+	if err := resizeApp.MoveWindowToCenter(tconn, resizeApp.WindowFinder, resizeApp.IsArcApp)(ctx); err != nil {
+		return errors.Wrap(err, "failed to move window to center")
+	}
+	testing.ContextLog(ctx, "Window has been moved to the center of screen")
+
+	// Perform resize operations.
+	for _, bound := range wmputils.AllBounds() {
+		if err := resizeApp.ResizeWindow(tconn, resizeApp.WindowFinder, bound)(ctx); err != nil {
+			return errors.Wrap(err, "failed to resize window")
+		}
+	}
+
+	return nil
+}
+
+type cwsAppText struct {
+	cws.App
+	id           string
+	windowFinder *nodewith.Finder
+
+	cr    *chrome.Chrome
+	tconn *chrome.TestConn
+}
+
+func newCwsAppText(cr *chrome.Chrome, tconn *chrome.TestConn) *cwsAppText {
+	return &cwsAppText{
+		App: cws.App{
+			Name: "Text",
+			URL:  "https://chrome.google.com/webstore/detail/text/mmfbcljfglbokpmkimbfghdkjmjhdgbg",
+		},
+		id:           "mmfbcljfglbokpmkimbfghdkjmjhdgbg",
+		windowFinder: nodewith.HasClass("RootView").Name("Text"),
+		cr:           cr,
+		tconn:        tconn,
+	}
+}
+
+func (app *cwsAppText) install(ctx context.Context) error {
+	isInstalled, err := ash.ChromeAppInstalled(ctx, app.tconn, app.id)
+	if err != nil {
+		return errors.Wrap(err, "failed to check CWS app's existence")
+	}
+
+	if isInstalled {
+		return nil
+	}
+	return cws.InstallApp(ctx, app.cr, app.tconn, app.App)
+}
+
+func (app *cwsAppText) uninstall(ctx context.Context) error {
+	defer func() {
+		settings := ossettings.New(app.tconn)
+		settings.Close(ctx)
+	}()
+	return ossettings.UninstallApp(ctx, app.tconn, app.cr, app.Name, app.id)
+}
+
+// closeApp closes the app id ID is provided; otherwise, will find the app ID first.
+func closeApp(ctx context.Context, tconn *chrome.TestConn, app *wmputils.ResizeApp) error {
+	// The app "Play Store" isn't unified depending on different models.
+	// Thus, the ID isn't specified in this test.
+	// Instead, we find the ID from running apps on the shelf.
+	if app.ID == "" {
+		// Finds the currently running app ID with specified name on the shelf.
+		shelfApps, err := ash.ShelfItems(ctx, tconn)
+		if err != nil {
+			return errors.Wrap(err, "failed to obtain all shelf apps")
+		}
+		for _, shelfapp := range shelfApps {
+			if shelfapp.Status == ash.ShelfItemRunning {
+				return apps.Close(ctx, tconn, shelfapp.AppID)
+			}
+		}
+		return errors.Errorf("failed to find app ID of app %s", app.Name)
+	}
+
+	return apps.Close(ctx, tconn, app.ID)
+}
---
+++ src/chromiumos/tast/local/bundles/cros/wmp/wmputils/window_resize.go
@@ -0,0 +1,261 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package wmputils contains utility functions for wmp tests.
+package wmputils
+
+import (
+	"context"
+	"math"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/wmp"
+	"chromiumos/tast/local/coords"
+	"chromiumos/tast/testing"
+)
+
+// defaultMargin indicates the distance in pixels outside the border of the "normal" window from which it should be grabbed.
+const defaultMargin = 5
+
+// WindowBound use incremental integers to represent window boundaries.
+type WindowBound int
+
+const (
+	// TopLeft represents app's TopLeft corner.
+	TopLeft WindowBound = iota
+	// TopRight represents app's TopRight corner.
+	TopRight
+	// BottomLeft represents app's BottomLeft corner.
+	BottomLeft
+	// BottomRight represents app's BottomRight corner.
+	BottomRight
+	// Left represents app's Left edge.
+	Left
+	// Right represents app's Right edge.
+	Right
+	// Top represents app's Top edge.
+	Top
+	// Bottom represents app's Bottom edge.
+	Bottom
+)
+
+// AllBounds returns all the bounds of the window.
+func AllBounds() []WindowBound {
+	return []WindowBound{
+		TopLeft, TopRight, BottomLeft, BottomRight, Left, Right, Top, Bottom,
+	}
+}
+
+// ResizeApp represents an app that will be resized.
+type ResizeApp struct {
+	Name         string
+	ID           string
+	IsArcApp     bool
+	WindowFinder *nodewith.Finder
+}
+
+// TurnOffWindowPreset sets the mode of ARC app from `Tablet` to `Resizable`.
+// Non-ARC app will be ignored.
+func (ra *ResizeApp) TurnOffWindowPreset(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn) error {
+	if !ra.IsArcApp {
+		return nil
+	}
+
+	testing.ContextLog(ctx, "Setting ARC app window mode to `Resizable`")
+	conn, err := apps.LaunchOSSettings(ctx, cr, "chrome://os-settings/app-management/detail?id="+ra.ID)
+	if err != nil {
+		return errors.Wrapf(err, "failed to open settings of ARC app %q", ra.Name)
+	}
+	defer conn.Close()
+	defer conn.CloseTarget(ctx)
+
+	ui := uiauto.New(tconn)
+
+	btnResizeLock := nodewith.Name("Preset window sizes")
+	if err := ui.WaitUntilExists(btnResizeLock)(ctx); err != nil {
+		testing.ContextLogf(ctx, "ARC app %q should be resizable already", ra.Name)
+		return nil
+	}
+	return ui.LeftClick(btnResizeLock)(ctx)
+}
+
+// ResizeWindowToMin minimizes window by dragging form top-left to bottom-right of the window.
+func (ra *ResizeApp) ResizeWindowToMin(tconn *chrome.TestConn, window *nodewith.Finder) uiauto.Action {
+	ui := uiauto.New(tconn)
+
+	return func(ctx context.Context) error {
+		if err := ui.WaitForLocation(window)(ctx); err != nil {
+			return errors.Wrap(err, "failed to wait for window to be stable")
+		}
+
+		rect, err := ui.Info(ctx, window)
+		if err != nil {
+			return errors.Wrap(err, "failed to get window info")
+		}
+
+		return wmp.StableDrag(tconn, window, rect.Location.TopLeft(), rect.Location.BottomRight())(ctx)
+	}
+}
+
+// MoveWindowToCenter places the window on the center of screen.
+// Window should NOT be maximized before this function.
+func (ra *ResizeApp) MoveWindowToCenter(tconn *chrome.TestConn, window *nodewith.Finder, isArcApp bool) uiauto.Action {
+	ui := uiauto.New(tconn)
+
+	return func(ctx context.Context) error {
+		if err := ui.WaitForLocation(window)(ctx); err != nil {
+			return errors.Wrap(err, "failed to wait for window to be stable")
+		}
+
+		windowInfo, err := ui.Info(ctx, window)
+		if err != nil {
+			return errors.Wrap(err, "failed to get window info")
+		}
+
+		src := coords.NewPoint(windowInfo.Location.CenterX(), windowInfo.Location.Top+defaultMargin)
+		if isArcApp {
+			centerBtnInfo, err := ui.Info(ctx, nodewith.Name("Resizable").HasClass("FrameCenterButton"))
+			if err != nil {
+				testing.ContextLog(ctx, "Failed to get center button of title bar info")
+			} else {
+				// Drag the left side of center button of the title bar to avoid moving failure.
+				src = coords.Point{X: centerBtnInfo.Location.Left - defaultMargin, Y: windowInfo.Location.Top + defaultMargin}
+			}
+		}
+
+		resizeArea, err := wmp.ResizableArea(ctx, tconn)
+		if err != nil {
+			return errors.Wrap(err, "failed to get resizeable area")
+		}
+
+		dest := coords.NewPoint(resizeArea.CenterX(), resizeArea.CenterY()-windowInfo.Location.Height/2)
+
+		return wmp.StableDrag(tconn, window, src, dest)(ctx)
+	}
+}
+
+// ResizeWindow resizes window by dragging corners/sides.
+// SetupResizableArea should be called prior to calling this function.
+func (ra *ResizeApp) ResizeWindow(tconn *chrome.TestConn, window *nodewith.Finder, dragBound WindowBound) uiauto.Action {
+	ui := uiauto.New(tconn)
+
+	return func(ctx context.Context) error {
+		testing.ContextLogf(ctx, "Resizing window by dragging %d", dragBound)
+		if err := ui.WaitForLocation(window)(ctx); err != nil {
+			return errors.Wrap(err, "failed to wait for window to be stable")
+		}
+
+		windowInfoBefore, err := ui.Info(ctx, window)
+		if err != nil {
+			return errors.Wrap(err, "failed to get window info before resize")
+		}
+
+		start, end, err := ra.dragDetail(ctx, tconn, dragBound, windowInfoBefore)
+		if err != nil {
+			return errors.Wrap(err, "failed to get drag detail")
+		}
+
+		if err := wmp.StableDrag(tconn, window, start, end)(ctx); err != nil {
+			return errors.Wrap(err, "failed to resize")
+		}
+
+		windowInfoAfter, err := ui.Info(ctx, window)
+		if err != nil {
+			return errors.Wrap(err, "failed to get window info after resize")
+		}
+		if !ra.verifyLocation(ctx, windowInfoAfter.Location, end, dragBound) {
+			return errors.New("failed to verify that the window has been resized")
+		}
+		testing.ContextLog(ctx, "Window has resized as expected")
+
+		if err := wmp.StableDrag(tconn, window, end, start)(ctx); err != nil {
+			return errors.Wrap(err, "failed to restore resize")
+		}
+
+		return nil
+	}
+}
+
+// verifyLocation verifies that the new position of window is correct.
+func (ra *ResizeApp) verifyLocation(ctx context.Context, windowLoc coords.Rect, expectedPos coords.Point, dragBound WindowBound) bool {
+	// The difference between each bound of target window's location and expected position must be not greater than a threshold.
+	const maxBias = defaultMargin
+
+	switch dragBound {
+	case TopLeft:
+		return (math.Abs(float64(windowLoc.Left)-float64(expectedPos.X)) <= maxBias) && (math.Abs(float64(windowLoc.Top)-float64(expectedPos.Y)) <= maxBias)
+	case TopRight:
+		return (math.Abs(float64(windowLoc.Right())-float64(expectedPos.X)) <= maxBias) && (math.Abs(float64(windowLoc.Top)-float64(expectedPos.Y)) <= maxBias)
+	case BottomLeft:
+		return (math.Abs(float64(windowLoc.Left)-float64(expectedPos.X)) <= maxBias) && (math.Abs(float64(windowLoc.Bottom())-float64(expectedPos.Y)) <= maxBias)
+	case BottomRight:
+		return (math.Abs(float64(windowLoc.Right())-float64(expectedPos.X)) <= maxBias) && (math.Abs(float64(windowLoc.Bottom())-float64(expectedPos.Y)) <= maxBias)
+	case Left:
+		return math.Abs(float64(windowLoc.Left)-float64(expectedPos.X)) <= maxBias
+	case Right:
+		return math.Abs(float64(windowLoc.Right())-float64(expectedPos.X)) <= maxBias
+	case Top:
+		return math.Abs(float64(windowLoc.Top)-float64(expectedPos.Y)) <= maxBias
+	case Bottom:
+		return math.Abs(float64(windowLoc.Bottom())-float64(expectedPos.Y)) <= maxBias
+	default:
+		testing.ContextLog(ctx, "Unexpected drag bound: ", dragBound)
+		return false
+	}
+}
+
+// dragDetail adjusts the position that should be dragged according to defaultMargin.
+func (ra *ResizeApp) dragDetail(ctx context.Context, tconn *chrome.TestConn, dragBound WindowBound, nodeToResize *uiauto.NodeInfo) (sourcePt, endPt coords.Point, err error) {
+	resizeArea, err := wmp.ResizableArea(ctx, tconn)
+	if err != nil {
+		return sourcePt, endPt, errors.Wrap(err, "failed to get resizable area")
+	}
+
+	var shift coords.Point
+	switch dragBound {
+	case TopLeft:
+		shift = coords.NewPoint(-defaultMargin, -defaultMargin)
+		sourcePt = nodeToResize.Location.TopLeft()
+		endPt = resizeArea.TopLeft()
+	case TopRight:
+		shift = coords.NewPoint(defaultMargin, -defaultMargin)
+		sourcePt = nodeToResize.Location.TopRight()
+		endPt = resizeArea.TopRight()
+	case BottomLeft:
+		shift = coords.NewPoint(-defaultMargin, defaultMargin)
+		sourcePt = nodeToResize.Location.BottomLeft()
+		endPt = resizeArea.BottomLeft()
+	case BottomRight:
+		shift = coords.NewPoint(defaultMargin, defaultMargin)
+		sourcePt = nodeToResize.Location.BottomRight()
+		endPt = resizeArea.BottomRight()
+	case Left:
+		shift = coords.NewPoint(-defaultMargin, 0)
+		sourcePt = nodeToResize.Location.LeftCenter()
+		endPt = resizeArea.LeftCenter()
+		endPt.Y = sourcePt.Y
+	case Right:
+		shift = coords.NewPoint(defaultMargin, 0)
+		sourcePt = nodeToResize.Location.RightCenter()
+		endPt = resizeArea.RightCenter()
+		endPt.Y = sourcePt.Y
+	case Top:
+		shift = coords.NewPoint(0, -defaultMargin)
+		sourcePt = coords.NewPoint(nodeToResize.Location.CenterX(), nodeToResize.Location.Top)
+		endPt = coords.NewPoint(resizeArea.CenterX(), resizeArea.Top)
+		endPt.X = sourcePt.X
+	case Bottom:
+		shift = coords.NewPoint(0, defaultMargin)
+		sourcePt = nodeToResize.Location.BottomCenter()
+		endPt = resizeArea.BottomCenter()
+		endPt.X = sourcePt.X
+	default:
+		return sourcePt, endPt, errors.Errorf("unexpected drag bound: %v", dragBound)
+	}
+	return sourcePt.Add(shift), endPt.Sub(shift), nil
+}
