1060558a08edc048e5d82a986f933c5da57bccba
Pathan Jilani <pathan.jilani@intel.com>
2021-10-01 14:09:13 +0530

    wifi:add WifiAPBrowseInternet
    Wi-Fi AP connect and browse internet.
    
    BUG=b:195902740
    
    TEST:tast run --build=true  <dut-ip> wifi.WifiAPBrowseInternet
    
    Change-Id: I0a92400bd7a194f6512e9ff1a97d7a4e630f11b5

---
+++ src/chromiumos/tast/local/bundles/cros/wifi/wifi_ap_browse_internet.go
@@ -0,0 +1,151 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/shillconst"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WifiAPBrowseInternet,
+		Desc:         "Wi-Fi AP connect and browse internet",
+		Contacts:     []string{"pathan.jilani@intel.com", "intel-chrome-system-automation-team@intel.com"},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Vars:         []string{"wifissid", "wifipassword"},
+	})
+}
+
+func WifiAPBrowseInternet(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatalf("Failed to start chrome: %s", err)
+	}
+	const browseweb = "https://www.google.com/"
+	ssid := s.RequiredVar("wifissid")
+	wifiPwd := s.RequiredVar("wifipassword")
+	manager, err := shill.NewManager(ctx)
+	if err != nil {
+		s.Fatal("Failed creating shill manager proxy: ", err)
+	}
+
+	expectProps := map[string]interface{}{
+		shillconst.ServicePropertyType:          shillconst.TypeWifi,
+		shillconst.ServicePropertyName:          ssid,
+		shillconst.ServicePropertySecurityClass: shillconst.SecurityPSK,
+	}
+
+	if enableFunc, err := manager.DisableTechnologyForTesting(ctx, shill.TechnologyEthernet); err != nil {
+		s.Fatal("Unable to disable Ethernet: ", err)
+	} else if enableFunc != nil {
+		// If ethernet is already enabled, no work is done and a nil callback is returned
+		ctx, cancel := ctxutil.Shorten(ctx, shill.EnableWaitTime)
+		cleanupCtx := ctx
+		defer cancel()
+		defer enableFunc(cleanupCtx)
+	}
+
+	if err := manager.EnableTechnology(ctx, shill.TechnologyWifi); err != nil {
+		s.Fatal("Failed to enable WiFi: ", err)
+	}
+
+	if enabled, err := manager.IsEnabled(ctx, shill.TechnologyWifi); err != nil {
+		s.Fatal("Failed to get WiFi enabled state: ", err)
+	} else if !enabled {
+		s.Fatal("Failed to enable wifi: ", err)
+	}
+
+	s.Log("Wi-Fi is enabled")
+	service, err := manager.FindMatchingService(ctx, expectProps)
+	if err != nil {
+		s.Fatal("Failed to find matching services: ", err)
+	}
+
+	if err := service.SetProperty(ctx, shillconst.ServicePropertyPassphrase, wifiPwd); err != nil {
+		s.Fatal("Failed to set service passphrase: ", err)
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := manager.RequestScan(ctx, shill.TechnologyWifi); err != nil {
+			return errors.Wrap(err, "failed to request wifi active scan")
+		}
+		return nil
+	}, &testing.PollOptions{
+		Timeout: 20 * time.Second,
+	}); err != nil {
+		s.Fatal("Failed to find the WiFi AP: ", err)
+	}
+	s.Log("Connecting AP")
+
+	if err := service.Connect(ctx); err != nil {
+		s.Fatal(err, "Failed to connect to service")
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		connected, err := service.IsConnected(ctx)
+		if err != nil {
+			return errors.Wrap(err, "failed to get WiFi connected state")
+		}
+		if !connected {
+			return errors.New("Wi-Fi is disconnected")
+		}
+		return nil
+	}, &testing.PollOptions{
+		Timeout: 10 * time.Second,
+	}); err != nil {
+		s.Fatalf("Failed to connect to %s Wi-Fi SSID: %v", ssid, err)
+	}
+
+	conn, err := cr.NewConn(ctx, browseweb)
+	if err != nil {
+		s.Fatal("Failed to connect to chrome: ", err)
+	}
+	defer conn.Close()
+	var actual string
+	expected := "Google Search"
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := conn.Eval(ctx, `document.querySelector('[aria-label="Google Search"]').value`, &actual); err != nil {
+			return errors.Wrap(err, "failed getting page content")
+		}
+		if actual != expected {
+			return errors.Errorf("unexpected page content: got %s; expecting %s", actual, expected)
+		}
+		return nil
+	}, &testing.PollOptions{
+		Timeout: 5 * time.Second,
+	}); err != nil {
+		s.Fatalf("Failed to open required url page: ", err)
+	}
+
+	s.Log("Disconnecting AP")
+	if err := service.Disconnect(ctx); err != nil {
+		s.Fatal("Failed to remove the service: ", err)
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		connected, err := service.IsConnected(ctx)
+		if err != nil {
+			return errors.Wrap(err, "failed to get WiFi connected state")
+		}
+		if connected {
+			return errors.New("Wi-Fi is connected")
+		}
+		return nil
+	}, &testing.PollOptions{
+		Timeout: 5 * time.Second,
+	}); err != nil {
+		s.Fatalf("Failed to disconnect from %s Wi-Fi SSID: %v", ssid, err)
+	}
+}
