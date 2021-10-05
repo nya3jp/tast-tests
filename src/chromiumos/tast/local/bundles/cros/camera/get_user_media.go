b4fc322bd85baa52dd559cabe0b5b8398635e9ab
Joseph Sussman <josephsussman@google.com>
2021-10-05 16:28:41 +0000

    Re-enable camera.GetUserMedia.fake on reven.
    We opened crrev.com/c/3017049 a while back to skip-over this test case
    because it failed on the kernel version we were using at the time. It
    appears this test case no longer fails against a reven-vmtest image,
    built from recent ToT.
    
    BUG=b:193796017
    TEST=tast run <DUT> camera.GetUserMedia.fake
    
    Change-Id: Ieeab549007f4cc8d6eef48d2783154c019d26c93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3206291
    Tested-by: Joseph Sussman <josephsussman@google.com>
    Auto-Submit: Joseph Sussman <josephsussman@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Matt Stark <msta@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/camera/get_user_media.go
+++ src/chromiumos/tast/local/bundles/cros/camera/get_user_media.go
@@ -17,7 +17,6 @@ import (
 	"chromiumos/tast/local/media/vm"
 	"chromiumos/tast/local/webrtc"
 	"chromiumos/tast/testing"
-	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -44,10 +43,9 @@ func init() {
 				Val:               lacros.ChromeTypeChromeOS,
 			},
 			{
-				Name:              "fake",
-				Pre:               pre.ChromeVideoWithFakeWebcam(),
-				ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel("reven")),
-				Val:               lacros.ChromeTypeChromeOS,
+				Name: "fake",
+				Pre:  pre.ChromeVideoWithFakeWebcam(),
+				Val:  lacros.ChromeTypeChromeOS,
 			},
 			{
 				Name:              "lacros",
