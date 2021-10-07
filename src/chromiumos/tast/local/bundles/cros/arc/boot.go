bad6bd53ad004bda6d0dce824837252091a6c5e2
Yusuke Sato <yusukes@google.com>
2021-10-07 15:19:38 -0700

    tast-tests: Add arc.Boot.vm_with_per_vcpu_core_scheduling
    Today, ARCVM on poppy, rammus, and fizz is started with 'crosvm
    --per-vm-core-scheduling' (aka per-VM core scheduling) by default.
    However, we want to keep the older mode (without the flag aka
    per-vCPU core scheduling) up and running too.
    
    arc.Boot.vm_with_per_vcpu_core_scheduling continuously runs ARCVM
    without the --per-vm-core-scheduling crosvm flag to archive that.
    
    BUG=b:194022819
    TEST=tast run EVE_DUT arc.Boot.vm, verify --cpu is 5. Then,
      tast run EVE_DUT arc.Boot.vm_with_per_vcpu_core_scheduling,
      verify --cpu is 3.
    
    Change-Id: Ie6419631fe44e2dc914c2cee82554d9bdda9abce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3213264
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Renuga Nanjappan <rnanjappan@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Tested-by: Yusuke Sato <yusukes@chromium.org>
    Tast-Review: Renuga Nanjappan <rnanjappan@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/boot.go
+++ src/chromiumos/tast/local/bundles/cros/arc/boot.go
@@ -19,6 +19,8 @@ type bootConfig struct {
 	numTrials int
 	// Use O_DIRECT in disk access for ARCVM
 	oDirect bool
+	// Extra Chrome command line options
+	chromeArgs []string
 }
 
 func init() {
@@ -57,6 +59,17 @@ func init() {
 			Name: "vm",
 			Val: bootConfig{
 				numTrials: 1,
+			},
+			ExtraAttr:         []string{"group:mainline"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Timeout:           5 * time.Minute,
+		}, {
+			Name: "vm_with_per_vcpu_core_scheduling",
+			Val: bootConfig{
+				numTrials: 1,
+				// Switch from per-VM core scheduling to per-vCPU core scheduling which
+				// is more secure but slow.
+				chromeArgs: []string{"--disable-features=ArcEnablePerVmCoreScheduling"},
 			},
 			ExtraAttr:         []string{"group:mainline"},
 			ExtraSoftwareDeps: []string{"android_vm"},
@@ -113,7 +126,8 @@ func runBoot(ctx context.Context, s *testing.State) {
 	}
 	defer reader.Close()
 
-	cr, err := chrome.New(ctx, chrome.ARCEnabled())
+	args := s.Param().(bootConfig).chromeArgs
+	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs(args...))
 	if err != nil {
 		s.Fatal("Failed to connect to Chrome: ", err)
 	}
