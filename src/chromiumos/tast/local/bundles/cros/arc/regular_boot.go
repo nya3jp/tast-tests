4fc08d3512bd6881dd63e0525294670d0e0256e2
Yury Khmel <khmel@chromium.org>
2021-10-08 15:41:42 -0700

    arc: Add test to measure regular boot performance.
    Regular boot means the user logs in Chrome where ARC is already
    provisioned and tries to use ARC app immediately.
    
    BUG=b:202559763
    TEST=tast run ${DUT} arc.RegularBoot[.vm]
         4G octopus(dorp) reports:
         6.5s for container and
         17.5s for ARCVM
    
    Change-Id: Ibc30ebfd59032db3022339db4a61757e934382a7

---
+++ src/chromiumos/tast/local/bundles/cros/arc/regular_boot.go
@@ -0,0 +1,183 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/optin"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/disk"
+	"chromiumos/tast/local/power"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: RegularBoot,
+		Desc: "This exercises a scenario when user logs in Chrome where ARC is already provisioned and tries to use ARC app immediately. App launch delay is reported for this case",
+		Contacts: []string{
+			"khmel@chromium.org", // Original author.
+			"arc-performance@google.com",
+		},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		Timeout:      15 * time.Minute,
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		VarDeps: []string{
+			"arc.perfAccountPool",
+		},
+	})
+}
+
+// RegularBoot steps through multiple ARC boots.
+func RegularBoot(ctx context.Context, s *testing.State) {
+	creds, err := performArcInitialBoot(ctx, s)
+	if err != nil {
+		s.Fatal("Failed to do initial optin: ", err)
+	}
+
+	const iterationCount = 4
+	perfValues := perf.NewValues()
+	for i := 0; i < iterationCount; i++ {
+		duration, err := performArcRegularBoot(ctx, creds)
+		if err != nil {
+			s.Fatal("Failed to do regular boot: ", err)
+		}
+		perfValues.Append(perf.Metric{
+			Name:      "app_launch_time",
+			Unit:      "seconds",
+			Direction: perf.SmallerIsBetter,
+			Multiple:  true,
+		}, duration.Seconds())
+	}
+
+	if err := perfValues.Save(s.OutDir()); err != nil {
+		s.Fatal("Failed saving perf data: ", err)
+	}
+}
+
+// performArcInitialBoot performs initial boot that includes ARC provisioning and returns GAIA
+// credentials to use for regular boot wih preserved state.
+func performArcInitialBoot(ctx context.Context, s *testing.State) (chrome.Creds, error) {
+	// Options are tuned for the fastest boot, we don't care about
+	// initial provisioning performance, which is monitored in other tests.
+	opts := []chrome.Option{
+		chrome.ARCSupported(),
+		chrome.GAIALoginPool(s.RequiredVar("arc.perfAccountPool")),
+		chrome.ExtraArgs(arc.DisableSyncFlags()...)}
+
+	testing.ContextLog(ctx, "Create initial Chrome")
+	cr, err := chrome.New(ctx, opts...)
+	if err != nil {
+		return chrome.Creds{}, errors.Wrap(err, "failed to connect to Chrome")
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return chrome.Creds{}, errors.Wrap(err, "failed to create test connection")
+	}
+	defer tconn.Close()
+
+	testing.ContextLog(ctx, "ARC is not enabled, perform optin")
+	if err := optin.Perform(ctx, cr, tconn); err != nil {
+		return chrome.Creds{}, errors.Wrap(err, "failed to optin")
+	}
+
+	return cr.Creds(), nil
+}
+
+// performArcRegularBoot performs ARC boot and starts Play Store app deferred and waits it is
+// actually shown. It returns time between the user session is created and and Play Store window is
+// shown. Note, it is not actually possible to measure this time directly from test due to tast
+// login is complex and ends after user session is actually created. Instead it uses existing ARC
+// histogram first app launch request and delay. Combined value is actual time that representss
+// the hardest case when user stars ARC app immideatly after login. First app launch request
+// represents here the overhead from tast Chrome login implementation.
+func performArcRegularBoot(ctx context.Context, creds chrome.Creds) (time.Duration, error) {
+	coolDownConfig := power.CoolDownConfig{
+		PollTimeout:             300 * time.Second,
+		PollInterval:            2 * time.Second,
+		CPUTemperatureThreshold: 52000,
+		CoolDownMode:            power.CoolDownStopUI,
+	}
+
+	if _, err := power.WaitUntilCPUCoolDown(ctx, coolDownConfig); err != nil {
+		return 0, errors.Wrap(err, "failed to wait until CPU is cooled down")
+	}
+
+	if err := disk.DropCaches(ctx); err != nil {
+		return 0, errors.Wrap(err, "failed to drop caches")
+	}
+
+	opts := []chrome.Option{
+		chrome.ARCSupported(),
+		chrome.ARCSupported(),
+		chrome.RestrictARCCPU(),
+		chrome.GAIALogin(creds),
+		chrome.KeepState(),
+		chrome.ExtraArgs(arc.DisableSyncFlags()...)}
+
+	testing.ContextLog(ctx, "Create Chrome")
+	cr, err := chrome.New(ctx, opts...)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to connect to Chrome")
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to create test connection")
+	}
+	defer tconn.Close()
+
+	testing.ContextLog(ctx, "Starting Play Store window deferred")
+	if err := apps.Launch(ctx, tconn, apps.PlayStore.ID); err != nil {
+		return 0, errors.Wrap(err, "failed to launch Play Store")
+	}
+
+	if err := optin.WaitForPlayStoreShown(ctx, tconn, time.Minute); err != nil {
+		return 0, errors.Wrap(err, "failed to wait Play Store shown")
+	}
+
+	delay, err := readFirstAppLaunchHistogram(ctx, tconn, "Arc.FirstAppLaunchDelay.TimeDelta")
+	if err != nil {
+		return 0, err
+	}
+
+	request, err := readFirstAppLaunchHistogram(ctx, tconn, "Arc.FirstAppLaunchRequest.TimeDelta")
+	if err != nil {
+		return 0, err
+	}
+
+	return request + delay, nil
+}
+
+//readFirstAppLaunchHistogram reads histogram and converts it to Duration.
+func readFirstAppLaunchHistogram(ctx context.Context, tconn *chrome.TestConn, name string) (time.Duration, error) {
+	metric, err := metrics.WaitForHistogram(ctx, tconn, name, 20*time.Second)
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to get %s histogram", name)
+	}
+
+	timeMs, err := metric.Mean()
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to read %s histogram", name)
+	}
+
+	return time.Duration(timeMs*1000000.0) * time.Nanosecond, nil
+}
