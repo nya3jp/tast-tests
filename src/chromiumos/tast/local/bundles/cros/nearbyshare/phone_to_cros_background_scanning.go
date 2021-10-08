3f47a1dcead1a91d6d539953aaca1473692bd095
Claudius van der Merwe <cvandermerwe@google.com>
2021-10-08 20:00:57 +0000

    [Nearby] Add basic background scanning e2e tests
    BUG=chromium:1223888
    TEST=manually verified
    
    Change-Id: I2f2c4d992c5cb7a06017eba977785c31fef0988c

---
+++ src/chromiumos/tast/local/bundles/cros/nearbyshare/phone_to_cros_background_scanning.go
@@ -0,0 +1,141 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package nearbyshare
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	nearbycommon "chromiumos/tast/common/cros/nearbyshare"
+	"chromiumos/tast/common/cros/nearbyshare/nearbytestutils"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/android"
+	"chromiumos/tast/local/chrome/nearbyshare"
+	"chromiumos/tast/local/chrome/nearbyshare/nearbysnippet"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/screenshot"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PhoneToCrosBackgroundScanning,
+		Desc: "Checks that Nearby Device is trying to share notification shows up, clicking the notification enables high-vis mode and the receive flow is successful",
+		Contacts: []string{
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:nearby-share"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{
+			{
+				Name:    "dataoffline_hidden_jpg11kb",
+				Fixture: "nearbyShareDataUsageOfflineNoOne",
+				Val: nearbytestutils.TestData{
+					Filename:        "small_jpg.zip",
+					TransferTimeout: nearbycommon.SmallFileTransferTimeout,
+					TestTimeout:     nearbycommon.DetectionTimeout + nearbycommon.SmallFileTransferTimeout,
+					MimeType:        nearbysnippet.MimeTypeJpeg,
+				},
+				ExtraData: []string{"small_jpg.zip"},
+				Timeout:   nearbycommon.DetectionTimeout + nearbycommon.SmallFileTransferTimeout,
+			},
+		},
+	})
+}
+
+// PhoneToCrosBackgroundScanning tests background scanning and file sharing with an Android device as sender and CrOS device as receiver.
+func PhoneToCrosBackgroundScanning(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*nearbyshare.FixtData).Chrome
+	tconn := s.FixtValue().(*nearbyshare.FixtData).TestConn
+	crosDisplayName := s.FixtValue().(*nearbyshare.FixtData).CrOSDeviceName
+	androidDevice := s.FixtValue().(*nearbyshare.FixtData).AndroidDevice
+	androidDisplayName := s.FixtValue().(*nearbyshare.FixtData).AndroidDeviceName
+
+	// Extract the test file to the staging directory on the Android device.
+	testData := s.Param().(nearbytestutils.TestData)
+	testDataZip := s.DataPath(testData.Filename)
+	testFile, err := nearbytestutils.ExtractAndroidTestFile(ctx, testDataZip, androidDevice)
+	if err != nil {
+		s.Fatal("Failed to extract test data files: ", err)
+	}
+
+	s.Log("Starting sending on the Android device")
+	testTimeout := testData.TestTimeout
+	mimetype := testData.MimeType
+	if err := androidDevice.SendFile(ctx, androidDisplayName, crosDisplayName, testFile, mimetype, testTimeout); err != nil {
+		s.Fatal("Failed to start sending on Android: ", err)
+	}
+	// Defer cancelling the share on the Android side if it does not succeed.
+	var shareCompleted bool
+	defer func() {
+		if !shareCompleted {
+			s.Log("Cancelling sending")
+			if err := screenshot.CaptureChrome(ctx, cr, filepath.Join(s.OutDir(), "after_sharing.png")); err != nil {
+				s.Log("Failed to capture a screenshot before cancelling sending")
+			}
+			if err := androidDevice.CancelSendingFile(ctx); err != nil {
+				s.Error("Failed to cancel sending after the share failed: ", err)
+			}
+			if err := androidDevice.AwaitSharingStopped(ctx, testTimeout); err != nil {
+				s.Error("Failed waiting for the Android device to signal that sharing has finished: ", err)
+			}
+		}
+	}()
+
+	s.Log("Waiting for fast init notification on CrOS receiver")
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+	if err := nearbyshare.AcceptFastInitiationNotification(ctx, tconn, nearbycommon.DetectShareTargetTimeout); err != nil {
+		s.Fatal("CrOS receiver failed to wait for Fast Init notification: ", err)
+	}
+	s.Log("Notification accepted. Waiting to enable High Viz Mode")
+
+	s.Log("Getting receive surface")
+	receiver, err := nearbyshare.GetReceiveSurface(ctx, tconn, cr)
+	if err != nil {
+		s.Fatal("Failed to set up control over the receiving surface: ", err)
+	}
+	defer receiver.Close(ctx)
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	s.Log("Waiting for CrOS receiver to detect incoming share from Android sender")
+	crosToken, err := receiver.WaitForSender(ctx, androidDisplayName, nearbycommon.DetectShareTargetTimeout)
+	if err != nil {
+		s.Fatal("CrOS receiver failed to find Android sender: ", err)
+	}
+
+	s.Log("Waiting for Android sender to see that CrOS receiver connected")
+	androidToken, err := androidDevice.AwaitReceiverAccept(ctx, nearbycommon.DetectShareTargetTimeout)
+	if err != nil {
+		s.Fatal("Failed waiting for the Android to connect to receiver: ", err)
+	}
+	if crosToken != androidToken {
+		s.Fatalf("Share tokens for Android and CrOS do not match. Android: %s, CrOS: %s", androidToken, crosToken)
+	}
+	s.Log("Accepting the share on the CrOS receiver")
+	if err := receiver.AcceptShare(ctx); err != nil {
+		s.Fatal("CrOs receiver failed to accept share from Android sender: ", err)
+	}
+
+	s.Log("Waiting for the Android sender to signal that sharing has completed")
+	if err := androidDevice.AwaitSharingStopped(ctx, testData.TransferTimeout); err != nil {
+		s.Fatal("Failed waiting for the Android device to signal that sharing has finished: ", err)
+	}
+	shareCompleted = true
+
+	// Repeat the file hash check for a few seconds, as we have no indicator on the CrOS side for when the received file has been completely written.
+	// TODO(crbug/1173190): Remove polling when we can confirm the transfer status with public test functions.
+	s.Log("Comparing Android and CrOS file hashes")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := nearbytestutils.FileHashComparison(ctx, []string{testFile}, filesapp.DownloadPath, filepath.Join(android.DownloadDir, nearbysnippet.SendDir), androidDevice); err != nil {
+			return errors.Wrap(err, "file hashes don't match yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
+		s.Fatal("Failed file hash comparison: ", err)
+	}
+	s.Log("Share completed and file hashes match on both sides")
+}
---
+++ src/chromiumos/tast/remote/bundles/cros/nearbyshare/cros_to_cros_background_scanning.go
@@ -0,0 +1,115 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package nearbyshare
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	nearbycommon "chromiumos/tast/common/cros/nearbyshare"
+	"chromiumos/tast/common/cros/nearbyshare/nearbytestutils"
+	"chromiumos/tast/errors"
+	remotenearby "chromiumos/tast/remote/cros/nearbyshare"
+	"chromiumos/tast/services/cros/nearbyservice"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CrosToCrosBackgroundScanning,
+		Desc:         "Checks that Nearby Device is trying to share notification shows up, clicking the notification enables high-vis mode and the receive flow is successful",
+		Contacts:     []string{"chromeos-sw-engprod@google.com"},
+		Attr:         []string{"group:nearby-share-remote"},
+		SoftwareDeps: []string{"chrome"},
+		ServiceDeps:  []string{"tast.cros.nearbyservice.NearbyShareService"},
+		Vars:         []string{"secondaryTarget"},
+		Params: []testing.Param{
+			{
+				Name:      "dataoffline_hidden_png5kb",
+				Fixture:   "nearbyShareRemoteDataUsageOfflineNoOne",
+				Val:       nearbytestutils.TestData{Filename: "small_png.zip", TransferTimeout: nearbycommon.SmallFileTransferTimeout},
+				ExtraData: []string{"small_png.zip"},
+				Timeout:   nearbycommon.DetectionTimeout + nearbycommon.SmallFileTransferTimeout,
+			},
+		},
+	})
+}
+
+// CrosToCrosBackgroundScanning tests background scanning and file sharing between Chrome OS devices.
+func CrosToCrosBackgroundScanning(ctx context.Context, s *testing.State) {
+	remoteFilePath := s.FixtValue().(*remotenearby.FixtData).RemoteFilePath
+	sender := s.FixtValue().(*remotenearby.FixtData).Sender
+	receiver := s.FixtValue().(*remotenearby.FixtData).Receiver
+	senderDisplayName := s.FixtValue().(*remotenearby.FixtData).SenderDisplayName
+	receiverDisplayName := s.FixtValue().(*remotenearby.FixtData).ReceiverDisplayName
+
+	s.Log("Starting sending on DUT1 (Sender)")
+	testData := s.Param().(nearbytestutils.TestData)
+	remoteFile := filepath.Join(remoteFilePath, testData.Filename)
+	fileReq := &nearbyservice.CrOSPrepareFileRequest{FileName: remoteFile}
+	fileNames, err := sender.PrepareFiles(ctx, fileReq)
+	if err != nil {
+		s.Fatal("Failed to prepare files for sending on DUT1 (Sender): ", err)
+	}
+	sendReq := &nearbyservice.CrOSSendFilesRequest{FileNames: fileNames.FileNames}
+	_, err = sender.StartSend(ctx, sendReq)
+	if err != nil {
+		s.Fatal("Failed to start send on DUT1 (Sender): ", err)
+	}
+
+	s.Log("Accepting the fast initiation notification on DUT2 (Receiver)")
+	_, err = receiver.AcceptFastInitiationNotification(ctx, &empty.Empty{})
+	if err != nil {
+		s.Fatal("Failed to accept fast initiation notification on DUT2 (Receiver): ", err)
+	}
+
+	s.Log("Selecting Receiver's (DUT2) share target on Sender (DUT1)")
+	targetReq := &nearbyservice.CrOSSelectShareTargetRequest{ReceiverName: receiverDisplayName, CollectShareToken: true}
+	senderShareToken, err := sender.SelectShareTarget(ctx, targetReq)
+	if err != nil {
+		s.Fatal("Failed to select share target on DUT1 (Sender): ", err)
+	}
+	s.Log("Accepting the share request on DUT2 (Receiver)")
+	transferTimeoutSeconds := int32(testData.TransferTimeout.Seconds())
+	receiveReq := &nearbyservice.CrOSReceiveFilesRequest{SenderName: senderDisplayName, TransferTimeoutSeconds: transferTimeoutSeconds}
+	receiverShareToken, err := receiver.WaitForSenderAndAcceptShare(ctx, receiveReq)
+	if err != nil {
+		s.Fatal("Failed to accept share on DUT2 (Receiver): ", err)
+	}
+	if senderShareToken.ShareToken != receiverShareToken.ShareToken {
+		s.Fatalf("Share tokens for sender and receiver do not match. Sender: %s, Receiver: %s", senderShareToken, receiverShareToken)
+	}
+
+	// Repeat the file hash check for a few seconds, as we have no indicator on the CrOS side for when the received file has been completely written.
+	// TODO(crbug/1173190): Remove polling when we can confirm the transfer status with public test functions.
+	s.Log("Comparing file hashes for all transferred files on both DUTs")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		senderFileReq := &nearbyservice.CrOSFileHashRequest{FileNames: fileNames.FileNames, FileDir: nearbytestutils.SendDir}
+		senderFileRes, err := sender.FilesHashes(ctx, senderFileReq)
+		if err != nil {
+			return errors.Wrap(err, "failed to get file hashes on DUT1 (Sender)")
+		}
+		receiverFileReq := &nearbyservice.CrOSFileHashRequest{FileNames: fileNames.FileNames, FileDir: nearbytestutils.DownloadPath}
+		receiverFileRes, err := receiver.FilesHashes(ctx, receiverFileReq)
+		if err != nil {
+			return errors.Wrap(err, "failed to get file hashes on DUT2 (Receiver)")
+		}
+		if len(senderFileRes.Hashes) != len(receiverFileRes.Hashes) {
+			return errors.Wrap(err, "length of hashes don't match")
+		}
+		for i := range senderFileRes.Hashes {
+			if senderFileRes.Hashes[i] != receiverFileRes.Hashes[i] {
+				return errors.Wrap(err, "hashes don't match")
+			}
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
+		s.Fatal("Failed file hash comparison: ", err)
+	}
+	s.Log("Share completed and file hashes match on both DUTs")
+}
