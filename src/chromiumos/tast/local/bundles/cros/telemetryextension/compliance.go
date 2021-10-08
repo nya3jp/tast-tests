d0599f9840d11f7e2d9c88e7be109d1bc6783d55
Oleh Lamzin <lamzin@google.com>
2021-10-08 12:10:19 +0200

    telemetryextension: Add Compliance test
    Compliance tests that DUT satisfies all requirements to run Telemetry
    Extension.
    
    BUG=b:188897824
    TEST=tast run <IP> telemetryextension.Compliance
    
    Change-Id: I0fd41dd077512f01335dafd5383c603a61167484

---
+++ src/chromiumos/tast/local/bundles/cros/telemetryextension/compliance.go
@@ -0,0 +1,72 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package telemetryextension
+
+import (
+	"context"
+	"io/ioutil"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/local/crosconfig"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Compliance,
+		Desc: "Verifies that DUT satisfies all requirements to run Telemetry Extension such as has all required VPD fields and correct CrOSConfig",
+		Contacts: []string{
+			"lamzin@google.com", // Test and Telemetry Extension author
+			"mgawad@google.com", // Telemetry Extension author
+			"cros-oem-services-team@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+// Compliance tests that DUT satisfies all requirements to run Telemetry Extension.
+func Compliance(ctx context.Context, s *testing.State) {
+	if vendorBytes, err := ioutil.ReadFile("/sys/devices/virtual/dmi/id/sys_vendor"); err != nil {
+		s.Error("Failed to read vendor name: ", err)
+	} else if got, want := string(vendorBytes), "HP\n"; got != want {
+		s.Errorf("Unexpected vendor name: got %q; want %q", got, want)
+	}
+
+	if oemDataBytes, err := testexec.CommandContext(ctx, "/usr/share/cros/oemdata.sh").Output(); err != nil {
+		s.Error("Failed to get OEM data: ", err)
+	} else if len(oemDataBytes) == 0 {
+		s.Error("OEM data is empty")
+	}
+
+	if activateDateBytes, err := ioutil.ReadFile("/sys/firmware/vpd/rw/ActivateDate"); err != nil {
+		s.Error("Failed to read ActivateDate VPD field: ", err)
+	} else if len(activateDateBytes) == 0 {
+		s.Error("ActivateDate VPD is empty")
+	}
+
+	if modelNameBytes, err := ioutil.ReadFile("/sys/firmware/vpd/ro/model_name"); err != nil {
+		s.Error("Failed to read model_name VPD field: ", err)
+	} else if len(modelNameBytes) == 0 {
+		s.Error("model_name VPD is empty")
+	}
+
+	if serialNumberBytes, err := ioutil.ReadFile("/sys/firmware/vpd/ro/serial_number"); err != nil {
+		s.Error("Failed to read serial_number VPD field: ", err)
+	} else if len(serialNumberBytes) == 0 {
+		s.Error("serial_number VPD is empty")
+	}
+
+	if skuNumberBytes, err := ioutil.ReadFile("/sys/firmware/vpd/ro/sku_number"); err != nil {
+		s.Error("Failed to read sku_number VPD field: ", err)
+	} else if len(skuNumberBytes) == 0 {
+		s.Error("sku_number VPD is empty")
+	}
+
+	if got, err := crosconfig.Get(ctx, "/cros-healthd/cached-vpd", "has-sku-number"); err != nil {
+		s.Error("Failed to get has-sku-number value from cros config: ", err)
+	} else if want := "true"; got != want {
+		s.Errorf("Unexpected vendor name: got %q; want %q", got, want)
+	}
+}
