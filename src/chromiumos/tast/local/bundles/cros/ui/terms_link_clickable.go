c57c8b681c326951b0f5fa141142f36fd0f7ae75
Ting Chen <ting.chen@cienet.com>
2021-10-08 17:49:47 +0800

    mta: add case to check the terms of service link
    [MTA 0650a] Checks the terms of service link is clickable within help page
    
    BUG=b:200987276
    TEST=tast run <DUT> ui.TermsLinkClickable
    
    Change-Id: Id1cbf722c2a69bcf8886bd8ec0d8d9ca49ab8bb8

---
+++ src/chromiumos/tast/local/bundles/cros/ui/terms_link_clickable.go
@@ -0,0 +1,134 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: TermsLinkClickable,
+		Desc: "Checks the terms of service link is clickable within help page",
+		Contacts: []string{
+			"cienet-development@googlegroups.com",
+			"chromeos-sw-engprod@google.com",
+			"ting.chen@cienet.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedIn",
+	})
+}
+
+// TermsLinkClickable checks the chrome://terms link is clickable within 'About Chrome OS' and chrome://help.
+func TermsLinkClickable(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	webPageTest := func(ctx context.Context, s *testing.State) {
+		conn, err := cr.NewConn(ctx, "chrome://help")
+		if err != nil {
+			s.Fatal("Failed to connect to chrome: ", err)
+		}
+		defer conn.Close()
+		defer conn.CloseTarget(cleanupCtx)
+		defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "web_page_dump")
+
+		if err := checkTermsOfService(ctx, cr, tconn, s.OutDir()); err != nil {
+			s.Fatal("Failed to click from web page: ", err)
+		}
+	}
+
+	ossettingsTest := func(ctx context.Context, s *testing.State) {
+		settings, err := ossettings.Launch(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to launch OS settings: ", err)
+		}
+		defer settings.Close(cleanupCtx)
+		defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ossettings_dump")
+
+		if err := uiauto.Combine("switch to `About Chrome OS page` page and check",
+			settings.WaitUntilExists(ossettings.AboutChromeOS),
+			settings.LeftClick(ossettings.AboutChromeOS),
+		)(ctx); err != nil {
+			s.Fatal("Failed to click from settings page: ", err)
+		}
+
+		if err := checkTermsOfService(ctx, cr, tconn, s.OutDir()); err != nil {
+			s.Fatal("Failed to click from settings page: ", err)
+		}
+	}
+
+	for _, subtest := range []struct {
+		name string
+		f    func(ctx context.Context, s *testing.State)
+	}{
+		{"check web page", webPageTest},
+		{"check os-settings", ossettingsTest},
+	} {
+		if !s.Run(ctx, subtest.name, subtest.f) {
+			// Stop if any sub-test failed.
+			s.Fatalf("Failed to run subtest %s", subtest.name)
+		}
+	}
+}
+
+func checkTermsOfService(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn, outDir string) error {
+	ui := uiauto.New(tconn)
+
+	termsOfServiceLink := nodewith.Name("Terms of Service").Role(role.Link)
+	if err := uiauto.Combine("click terms of service link",
+		ui.WaitUntilExists(termsOfServiceLink),
+		ui.LeftClick(termsOfServiceLink),
+	)(ctx); err != nil {
+		return err
+	}
+
+	return verifyContent(ctx, cr, outDir)
+}
+
+func verifyContent(ctx context.Context, cr *chrome.Chrome, outDir string) (err error) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	url := "chrome://terms/"
+	conn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(url))
+	if err != nil {
+		return errors.Wrapf(err, "failed to connect to window %s", url)
+	}
+	defer conn.Close()
+	defer conn.CloseTarget(cleanupCtx)
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, outDir, func() bool { return err != nil }, cr, "terms_dump")
+
+	expected := "Google Chrome and Chrome OS Additional Terms of Service"
+	expr := fmt.Sprintf(`document.querySelector('h2').innerText === '%s'`, expected)
+	if err := conn.WaitForExprWithTimeout(ctx, expr, 10*time.Second); err != nil {
+		return errors.Wrap(err, "unexpected page content")
+	}
+
+	return nil
+}
