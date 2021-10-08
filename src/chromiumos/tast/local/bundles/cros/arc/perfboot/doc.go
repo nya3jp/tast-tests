841545d4dfb851723e99584427e2ba58f652b668
Yury Khmel <khmel@chromium.org>
2021-10-08 15:41:42 -0700

    arc: Add test to measure regular boot performance.
    Regular boot means the user logs in Chrome where ARC is already
    provisioned and tries to use ARC app immediately.
    
    BUG=b:202559763
    TEST=tast run ${DUT} arc.RegularBoot[.vm]
         4G octopus(dorp) reports:
         6.5s for container and
         17.5s for ARCVM
    
    Change-Id: Ibc30ebfd59032db3022339db4a61757e934382a7

---
+++ src/chromiumos/tast/local/bundles/cros/arc/perfboot/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package perfboot contains helper functions to parse Android logs in order to extract metrics
+// related to ARC booting.
+package perfboot
