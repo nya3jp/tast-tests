85e5d36b71c9b2b2ccde2183bd2fedb99a1088b3
Shengsong Tan <sstan@google.com>
2021-10-11 12:46:25 +0900

    arc: Refactor arc.GhostWindow for reduce flaky and upcoming test
    1) The test will flaky if the gaia account has been optin the DUT
    and not clear correctly. This CL add launch op for PlayStore to
    make sure it popup.
    
    2) Refactor code for upcoming sub-tests.
    
    BUG=b:202077301
    TEST=tast run arc.GhostWindow
    
    Change-Id: Iaf5b4d33c23b4a220ccf49496559c3215b942b16

--- src/chromiumos/tast/local/bundles/cros/arc/ghost_window.go
+++ src/chromiumos/tast/local/bundles/cros/arc/ghost_window.go
@@ -8,6 +8,8 @@ import (
 	"context"
 	"time"
 
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/arc/optin"
 	"chromiumos/tast/local/chrome"
@@ -55,59 +57,11 @@ func waitPlayStoreGhostWindowShown(ctx context.Context, tconn *chrome.TestConn, 
 	}, &testing.PollOptions{Timeout: timeout})
 }
 
-func GhostWindow(ctx context.Context, s *testing.State) {
-	var creds chrome.Creds
-
-	func() {
-		// Setup Chrome.
+func loginChrome(ctx context.Context, s *testing.State, creds *chrome.Creds) (*chrome.Chrome, error) {
+	if creds != nil {
+		// Setup Chrome. Login by the creds.
 		cr, err := chrome.New(ctx,
-			chrome.GAIALoginPool(s.RequiredVar("ui.gaiaPoolDefault")),
-			chrome.ARCSupported(),
-			chrome.EnableFeatures("FullRestore"),
-			chrome.EnableFeatures("ArcGhostWindow"),
-			chrome.ExtraArgs(arc.DisableSyncFlags()...))
-		if err != nil {
-			s.Fatal("Failed to start Chrome: ", err)
-		}
-		defer cr.Close(ctx)
-
-		creds = cr.Creds()
-
-		// Optin to Play Store.
-		s.Log("Opting into Play Store")
-		maxAttempts := 1
-
-		if err := optin.PerformWithRetry(ctx, cr, maxAttempts); err != nil {
-			s.Fatal("Failed to optin to Play Store: ", err)
-		}
-
-		tconn, err := cr.TestAPIConn(ctx)
-		if err != nil {
-			s.Fatal("Failed to create test API connection: ", err)
-		}
-
-		// In this case we cannot use this func, since it inspect App by check shelf ID.
-		// After ghost window finish ash shelf integration, the ghost window will also
-		// carry the corresponding app's ID into shelf. Here we need to check actual
-		// aura window.
-		if err := waitPlayStoreShown(ctx, tconn, testTimeout); err != nil {
-			s.Fatal("Failed to wait for Play Store: ", err)
-		}
-
-		// According to the PRD of Full Restore go/chrome-os-full-restore-dd,
-		// it uses a throttle of 2.5s to save the app launching and window status
-		// information to the backend. Therefore, sleep 5 seconds here.
-		testing.Sleep(ctx, 5*time.Second)
-	}()
-
-	if err := upstart.RestartJob(ctx, "ui"); err != nil {
-		s.Fatal("Failed to log out: ", err)
-	}
-
-	func() {
-		// Setup Chrome. Login by the same account.
-		cr, err := chrome.New(ctx,
-			chrome.GAIALogin(creds),
+			chrome.GAIALogin(*creds),
 			chrome.ARCSupported(),
 			chrome.EnableFeatures("FullRestore"),
 			chrome.EnableFeatures("ArcGhostWindow"),
@@ -115,29 +69,137 @@ func GhostWindow(ctx context.Context, s *testing.State) {
 			chrome.KeepState(),
 			chrome.ExtraArgs(arc.DisableSyncFlags()...))
 		if err != nil {
-			s.Fatal("Failed to start Chrome: ", err)
+			return nil, errors.Wrap(err, "failed to start Chrome")
 		}
-		defer cr.Close(ctx)
+		return cr, nil
+	}
+	// Setup Chrome for a new cred.
+	cr, err := chrome.New(ctx,
+		chrome.GAIALoginPool(s.RequiredVar("ui.gaiaPoolDefault")),
+		chrome.ARCSupported(),
+		chrome.EnableFeatures("FullRestore"),
+		chrome.EnableFeatures("ArcGhostWindow"),
+		chrome.ExtraArgs(arc.DisableSyncFlags()...))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start Chrome")
+	}
+	return cr, nil
+}
 
-		tconn, err := cr.TestAPIConn(ctx)
-		if err != nil {
-			s.Fatal("Failed to create test API connection: ", err)
+func clickRestoreButtonNormalStatus(ctx context.Context, tconn *chrome.TestConn, s *testing.State) error {
+	alertDialog := nodewith.NameStartingWith("Restore apps?").Role(role.AlertDialog)
+	restoreButton := nodewith.Name("RESTORE").Role(role.Button).Ancestor(alertDialog)
+
+	ui := uiauto.New(tconn)
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	if err := uiauto.Combine("restore playstore",
+		// Click Restore on the restore alert.
+		ui.LeftClick(restoreButton))(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func clickRestoreButtonCrashedStatus(ctx context.Context, tconn *chrome.TestConn, s *testing.State) error {
+	// Full text is "Your *Chromebook* restarted unexpectedly".
+	alertDialog := nodewith.NameStartingWith("Your").Role(role.AlertDialog)
+	restoreButton := nodewith.Name("RESTORE").Role(role.Button).Ancestor(alertDialog)
+
+	ui := uiauto.New(tconn)
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	if err := uiauto.Combine("restore playstore",
+		// Click Restore on the restore alert.
+		ui.LeftClick(restoreButton))(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func optinAndLaunchPlayStore(ctx context.Context, s *testing.State, cr *chrome.Chrome) error {
+	// Optin to Play Store.
+	s.Log("Opting into Play Store")
+	maxAttempts := 1
+
+	if err := optin.PerformWithRetry(ctx, cr, maxAttempts); err != nil {
+		return errors.Wrap(err, "failed to optin to Play Store")
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create test API connection")
+	}
+
+	// The PlayStore only popup automatically on first optin of an account.
+	// Launch it here in case it's not the first optin.
+	if err := apps.Launch(ctx, tconn, apps.PlayStore.ID); err != nil {
+		return errors.Wrap(err, "failed to launch Play Store")
+	}
+
+	// In this case we cannot use this func, since it inspect App by check shelf ID.
+	// After ghost window finish ash shelf integration, the ghost window will also
+	// carry the corresponding app's ID into shelf. Here we need to check actual
+	// aura window.
+	if err := waitPlayStoreShown(ctx, tconn, testTimeout); err != nil {
+		return errors.Wrap(err, "failed to wait for Play Store")
+	}
+
+	// According to the PRD of Full Restore go/chrome-os-full-restore-dd,
+	// it uses a throttle of 2.5s to save the app launching and window status
+	// information to the backend. Therefore, sleep 5 seconds here.
+	testing.Sleep(ctx, 5*time.Second)
+
+	return nil
+}
+
+func verifyGhostWindow(ctx context.Context, s *testing.State, cr *chrome.Chrome, isCrash bool) error {
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create test API connection")
+	}
+
+	if isCrash {
+		if err := clickRestoreButtonCrashedStatus(ctx, tconn, s); err != nil {
+			return errors.Wrap(err, "failed to click Restore button on crash restore notification")
 		}
-
-		alertDialog := nodewith.NameStartingWith("Restore apps?").Role(role.AlertDialog)
-		restoreButton := nodewith.Name("RESTORE").Role(role.Button).Ancestor(alertDialog)
-
-		ui := uiauto.New(tconn)
-		defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
-		if err := uiauto.Combine("restore playstore",
-			// Click Restore on the restore alert.
-			ui.LeftClick(restoreButton))(ctx); err != nil {
-			s.Fatal("Failed to restore playstore: ", err)
+	} else {
+		if err := clickRestoreButtonNormalStatus(ctx, tconn, s); err != nil {
+			return errors.Wrap(err, "failed to click Restore button on normal restore notification")
 		}
-		// Make sure ARC Ghost Window of PlayStore has popup.
-		if err := waitPlayStoreGhostWindowShown(ctx, tconn, testTimeout); err != nil {
-			s.Fatal("Failed to wait for Play Store: ", err)
-		}
-	}()
+	}
+
+	// Make sure ARC Ghost Window of PlayStore has popup.
+	if err := waitPlayStoreGhostWindowShown(ctx, tconn, testTimeout); err != nil {
+		return errors.Wrap(err, "failed to wait for Play Store")
+	}
+	return nil
+}
+
+func GhostWindow(ctx context.Context, s *testing.State) {
+	// Test ghost window in logout case.
+	cr, err := loginChrome(ctx, s, nil)
+	if err != nil {
+		s.Fatal("Fail to optin: ", err)
+	}
+	defer cr.Close(ctx)
+	creds := cr.Creds()
+	if err := optinAndLaunchPlayStore(ctx, s, cr); err != nil {
+		s.Fatal("Failed to initial optin: ", err)
+	}
+
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Fatal("Failed to log out: ", err)
+	}
+
+	// Re-login
+	cr, err = loginChrome(ctx, s, &creds)
+	if err != nil {
+		s.Fatal("Fail to re-optin: ", err)
+	}
+	defer cr.Close(ctx)
+
+	if err := verifyGhostWindow(ctx, s, cr, false); err != nil {
+		s.Fatal("Failed to launch ghost window: ", err)
+	}
 }
