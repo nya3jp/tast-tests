f3bcd000c32a3a6785051e05e0056d5b04e73d67
Edward Hill <ecgh@chromium.org>
2021-10-11 14:09:41 -0600

    Add Ti50CcdUpdate test.
    This test updates Dauntless on Andreiboard first using spiflash,
    then using gsctool via CCD.
    
    BUG=none
    TEST=tast run -var=mode=workstation -var=heximage=<ti50 full_image.signed> \
    -var=binimage=<ti50 full_image.signed.bin> -ver=serial=<ti50 serial> \
    <fake dut ip> firmware.Ti50CCDUpdate
    
    Change-Id: Ie5bda288649e157e4921c24c1adf1faca249d223

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/ti50_ccd_update.go
@@ -0,0 +1,127 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/common/firmware/ti50"
+	"chromiumos/tast/common/testexec"
+	remoteTi50 "chromiumos/tast/remote/firmware/ti50"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:    Ti50CCDUpdate,
+		Desc:    "Ti50 firmware update over CCD using gsctool",
+		Timeout: 5 * time.Minute,
+		Vars:    []string{"mode", "spiflash", "heximage", "binimage", "serial"},
+		Contacts: []string{
+			"ecgh@chromium.org",
+			"ti50-core@google.com",
+		},
+		ServiceDeps: []string{"tast.cros.baserpc.FileSystem", "tast.cros.firmware.SerialPortService"},
+		Attr:        []string{"group:firmware"},
+	})
+}
+
+func Ti50CCDUpdate(ctx context.Context, s *testing.State) {
+	const (
+		Ti50USBID             = "18d1:504a"
+		UpdateSuccessExitCode = 1
+		UpdateErrorExitCode   = 3
+	)
+
+	mode, _ := s.Var("mode")
+	spiflash, _ := s.Var("spiflash")
+
+	board, rpcClient, err := remoteTi50.GetTi50TestBoard(ctx, s.DUT(), s.RPCHint(), mode, spiflash, 4096, 100*time.Millisecond)
+
+	if err != nil {
+		s.Fatal("GetTi50TestBoard: ", err)
+	}
+	if rpcClient != nil {
+		defer rpcClient.Close(ctx)
+	}
+	defer board.Close(ctx)
+
+	heximage := s.RequiredVar("heximage")
+	err = board.FlashImage(ctx, heximage)
+	if err != nil {
+		s.Fatal("FlashImage: ", err)
+	}
+
+	i := ti50.NewCrOSImage(board)
+
+	if err := i.WaitUntilBooted(ctx); err != nil {
+		s.Fatal("WaitUntilBooted after spiflash: ", err)
+	}
+
+	out, err := i.Command(ctx, "version")
+	if err != nil {
+		s.Fatal("console version: ", err)
+	}
+	testing.ContextLog(ctx, "version after spiflash: ")
+	testing.ContextLog(ctx, out)
+
+	cmd := testexec.CommandContext(ctx, "lsusb", "-d", Ti50USBID, "-v")
+	bytes, err := cmd.Output()
+	if err != nil {
+		s.Fatal("lsusb: ", err)
+	}
+	out = string(bytes)
+	serial := s.RequiredVar("serial")
+	re := regexp.MustCompile(`iSerial\s+\d\s` + serial)
+	m := re.FindStringSubmatch(out)
+	if m == nil {
+		s.Fatal("lsusb serial: ", out)
+	}
+
+	cmd = testexec.CommandContext(ctx, "/usr/sbin/gsctool", "-n", serial, "-f")
+	if err := cmd.Run(); err != nil {
+		s.Fatal("gsctool version: ", err)
+	}
+
+	binimage := s.RequiredVar("binimage")
+	cmd = testexec.CommandContext(ctx, "ls", binimage)
+	if err := cmd.Run(); err != nil {
+		s.Fatal("binimage: ", err)
+	}
+
+	// Ti50 will reject updates for 60 seconds.
+	testing.Sleep(ctx, 30*time.Second)
+
+	cmd = testexec.CommandContext(ctx, "/usr/sbin/gsctool", "-n", serial, binimage)
+	err = cmd.Run()
+	if c, _ := testexec.ExitCode(err); c != UpdateErrorExitCode {
+		s.Fatal("gsctool reject update: ", err)
+	}
+	_, err = board.ReadSerialSubmatch(ctx, regexp.MustCompile("Attempted update too soon"))
+	if err != nil {
+		s.Fatal("console reject update: ", err)
+	}
+
+	testing.Sleep(ctx, 30*time.Second)
+
+	cmd = testexec.CommandContext(ctx, "/usr/sbin/gsctool", "-n", serial, binimage)
+	err = cmd.Run()
+	if c, _ := testexec.ExitCode(err); c != UpdateSuccessExitCode {
+		s.Fatal("gsctool update: ", err)
+	}
+
+	if err := i.WaitUntilBooted(ctx); err != nil {
+		s.Fatal("WaitUntilBooted after gsctool: ", err)
+	}
+
+	out, err = i.Command(ctx, "version")
+	if err != nil {
+		s.Fatal("console version: ", err)
+	}
+	testing.ContextLog(ctx, "version after gsctool: ")
+	testing.ContextLog(ctx, out)
+}
