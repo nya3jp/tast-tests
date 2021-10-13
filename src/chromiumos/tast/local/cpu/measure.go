888d17461c53aa1560091984f97f0ce02db5feae
Hidehiko Abe <hidehiko@chromium.org>
2021-10-13 18:05:41 +0900

    Move MeasureCPUUsage from media to cpu package.
    As preparation to move WaitUntilIdle() to split the complex
    dependencies for richer features in the package.
    
    Along with the move, renamed
    - MeasureCPUUsage -> MeasureUsage, as now it is under cpu pacakge.
    - getStat -> timesStat, following go style.
    
    BUG=chromium:1257513
    TEST=./fast_build.sh -T
    
    Change-Id: Iff482af5a85d8e35e77f69e32e3ddf2ddaf38832

---
+++ src/chromiumos/tast/local/cpu/measure.go
@@ -0,0 +1,60 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cpu
+
+import (
+	"context"
+	"time"
+
+	"github.com/shirou/gopsutil/cpu"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// timesStat returns utilization stats across all CPUs as reported by /proc/stat.
+func timesStat() (*cpu.TimesStat, error) {
+	times, err := cpu.Times(false)
+	if err != nil {
+		return nil, err
+	}
+	return &times[0], nil
+}
+
+// MeasureUsage measures utilization across all CPUs during duration.
+// Returns a percentage in the range [0.0, 100.0].
+func MeasureUsage(ctx context.Context, duration time.Duration) (float64, error) {
+	// Get the total time the CPU spent in different states (read from
+	// /proc/stat on linux machines).
+	statBegin, err := timesStat()
+	if err != nil {
+		return 0, err
+	}
+
+	if err := testing.Sleep(ctx, duration); err != nil {
+		return 0, err
+	}
+
+	// Get the total time the CPU spent in different states again. By looking at
+	// the difference with the values we got earlier, we can calculate the time
+	// the processor was idle. The gopsutil library also has a function that
+	// does this directly, but unfortunately we can't use it as that function
+	// doesn't abort when the timeout in ctx is exceeded.
+	statEnd, err := timesStat()
+	if err != nil {
+		return 0, err
+	}
+
+	totalTimeBegin := statBegin.Total()
+	activeTimeBegin := totalTimeBegin - (statBegin.Idle + statBegin.Iowait)
+	totalTimeEnd := statEnd.Total()
+	activeTimeEnd := totalTimeEnd - (statEnd.Idle + statEnd.Iowait)
+
+	if totalTimeEnd <= totalTimeBegin {
+		return 0.0, errors.Errorf("total time went from %f to %f", totalTimeBegin, totalTimeEnd)
+	}
+
+	return (activeTimeEnd - activeTimeBegin) / (totalTimeEnd - totalTimeBegin) * 100.0, nil
+}
