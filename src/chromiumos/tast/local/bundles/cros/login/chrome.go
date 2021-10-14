c39bb4d1b627fae27fe3d3f201ad86fcae28a7ca
Jakub Flis <jaflis@google.com>
2021-10-14 12:43:38 +0000

    ui.ChromeLogin: Increase timeout to fix a flaky test
    Fix a flaky test by increasing the timeout. Currently the test uses
    a newly created context with a shorter timeout. Remove it, and use
    the default context instead.
    
    Extract the code running in an anonymous function into a ‘normal’ function.
    
    BUG=chromium:1257339
    TEST=ran this test on a hatch device and on a local VM
    
    Change-Id: I9e732aa881f7481edec0bf6078c981b873e7a1d0

--- src/chromiumos/tast/local/bundles/cros/login/chrome.go
+++ src/chromiumos/tast/local/bundles/cros/login/chrome.go
@@ -72,54 +72,12 @@ func Chrome(ctx context.Context, s *testing.State) {
 			s.Logf("Trial %d/%d", i+1, numTrial)
 		}
 
-		testChromeLogin(ctx, s, sm, server.URL, content)
+		testChromeLoginInSession(ctx, s, sm, server.URL, content)
 	}
 }
 
-func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionManager, url, expected string) {
-	ctx, cancel := context.WithTimeout(ctx, chrome.LoginTimeout)
-	defer cancel()
-
-	func() {
-		// Start listening for a "started" SessionStateChanged D-Bus signal from session_manager.
-		sw, err := sm.WatchSessionStateChanged(ctx, "started")
-		if err != nil {
-			s.Fatal("Failed to watch for D-Bus signals: ", err)
-		}
-		defer sw.Close(ctx)
-
-		cr, err := chrome.New(ctx, chrome.ExtraArgs("--vmodule=login_display_host*=4,oobe_ui=4"))
-		if err != nil {
-			s.Fatal("Chrome login failed: ", err)
-		}
-		defer cr.Close(ctx)
-
-		s.Log("Waiting for SessionStateChanged \"started\" D-Bus signal from session_manager")
-		select {
-		case <-sw.Signals:
-			s.Log("Got SessionStateChanged signal")
-		case <-ctx.Done():
-			s.Fatal("Didn't get SessionStateChanged signal: ", ctx.Err())
-		}
-
-		conn, err := cr.NewConn(ctx, url)
-		if err != nil {
-			s.Fatal("Creating renderer failed: ", err)
-		}
-		defer conn.Close()
-
-		if err := conn.WaitForExpr(ctx, "document.readyState === 'complete'"); err != nil {
-			s.Fatal("Waiting load failed: ", err)
-		}
-
-		var actual string
-		if err := conn.Eval(ctx, "document.documentElement.innerText", &actual); err != nil {
-			s.Fatal("Getting page content failed: ", err)
-		}
-		if actual != expected {
-			s.Fatalf("Unexpected page content: got %q; want %q", actual, expected)
-		}
-	}()
+func testChromeLoginInSession(ctx context.Context, s *testing.State, sm *session.SessionManager, url, expected string) {
+	testChromeLogin(ctx, s, sm, url, expected)
 
 	sw, err := sm.WatchSessionStateChanged(ctx, "stopped")
 	if err != nil {
@@ -138,5 +96,46 @@ func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionM
 		s.Log("Got SessionStateChanged signal")
 	case <-ctx.Done():
 		s.Fatal("Didn't get SessionStateChanged signal: ", ctx.Err())
+	}
+}
+
+func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionManager, url, expected string) {
+	// Start listening for a "started" SessionStateChanged D-Bus signal from session_manager.
+	sw, err := sm.WatchSessionStateChanged(ctx, "started")
+	if err != nil {
+		s.Fatal("Failed to watch for D-Bus signals: ", err)
+	}
+	defer sw.Close(ctx)
+
+	cr, err := chrome.New(ctx, chrome.ExtraArgs("--vmodule=login_display_host*=4,oobe_ui=4"))
+	if err != nil {
+		s.Fatal("Chrome login failed: ", err)
+	}
+	defer cr.Close(ctx)
+
+	s.Log("Waiting for SessionStateChanged \"started\" D-Bus signal from session_manager")
+	select {
+	case <-sw.Signals:
+		s.Log("Got SessionStateChanged signal")
+	case <-ctx.Done():
+		s.Fatal("Didn't get SessionStateChanged signal: ", ctx.Err())
+	}
+
+	conn, err := cr.NewConn(ctx, url)
+	if err != nil {
+		s.Fatalf("Failed to connect to %v: %v", url, err)
+	}
+	defer conn.Close()
+
+	if err := conn.WaitForExpr(ctx, "document.readyState === 'complete'"); err != nil {
+		s.Fatal("Waiting load failed: ", err)
+	}
+
+	var actual string
+	if err := conn.Eval(ctx, "document.documentElement.innerText", &actual); err != nil {
+		s.Fatal("Getting page content failed: ", err)
+	}
+	if actual != expected {
+		s.Fatalf("Unexpected page content: got %q; want %q", actual, expected)
 	}
 }
