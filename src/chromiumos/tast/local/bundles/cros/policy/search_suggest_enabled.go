e96f95684ff86d9bb3ca6c8fb223d829d38cd582
Georg Neis <neis@chromium.org>
2021-10-20 12:03:52 +0200

    policy: Refactor SearchSuggestEnabled tast for lacros
    Add a lacros variation and refactor the test code such that no branching
    on the chrome type is needed at the top-level.
    
    BUG=chromium:1259192
    TEST=tast run policy.SearchSuggestEnabled*
    
    Change-Id: Ic29fac889ede74e03bc2b371ec42fb326544297b

--- src/chromiumos/tast/local/bundles/cros/policy/search_suggest_enabled.go
+++ src/chromiumos/tast/local/bundles/cros/policy/search_suggest_enabled.go
@@ -12,7 +12,9 @@ import (
 	"chromiumos/tast/common/fixture"
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/lacros"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
@@ -32,13 +34,26 @@ func init() {
 		},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline", "informational"},
-		Fixture:      fixture.ChromePolicyLoggedIn,
+		Params: []testing.Param{{
+			Fixture: fixture.ChromePolicyLoggedIn,
+			Val:     lacros.ChromeTypeChromeOS,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val:               lacros.ChromeTypeLacros,
+		}},
 	})
 }
 
 func SearchSuggestEnabled(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	// Reserve ten seconds for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
 
 	// Connect to Test API to use it with the UI library.
 	tconn, err := cr.TestAPIConn(ctx)
@@ -87,8 +102,21 @@ func SearchSuggestEnabled(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to update policies: ", err)
 			}
 
+			// TODO(crbug.com/1259615): This should be part of the fixture.
+			_, l, br, err := lacros.Setup(ctx, s.FixtValue(), s.Param().(lacros.ChromeType))
+			if err != nil {
+				s.Fatal("Failed to setup chrome: ", err)
+			}
+			defer lacros.CloseLacrosChrome(cleanupCtx, l)
+
+			conn, err := br.NewConn(ctx, "")
+			if err != nil {
+				s.Fatal("Failed to connect to chrome: ", err)
+			}
+			defer conn.Close()
+
 			// Try to open a tab.
-			if err := keyboard.Accel(ctx, "ctrl+n"); err != nil {
+			if err := keyboard.Accel(ctx, "ctrl+t"); err != nil {
 				s.Fatal("Failed to write events: ", err)
 			}
 
