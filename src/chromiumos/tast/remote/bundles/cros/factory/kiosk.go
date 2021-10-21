96ae1a02fca0a175fb8d6d9df587175b184af955
lschyi <lschyi@google.com>
2021-10-21 16:44:36 +0800

    factory Kiosk: promote as critical test
    As Kiosk test is running stable more than a month, remove the
    informational tag. The nocturne can not pass this test but since it is
    not being manufactured, simply excluded this platform.
    
    BUG=b:203720382
    TEST=tast run $(DUT) factory.Kiosk
    
    Change-Id: I8743657d4a50cc9e15416111d16b898ffec52b90

--- src/chromiumos/tast/remote/bundles/cros/factory/kiosk.go
+++ src/chromiumos/tast/remote/bundles/cros/factory/kiosk.go
@@ -26,11 +26,13 @@ func init() {
 		Func:     Kiosk,
 		Desc:     "Test if factory UI is running",
 		Contacts: []string{"lschyi@google.com", "chromeos-factory-eng@google.com"},
-		Attr:     []string{"group:mainline", "informational"},
+		Attr:     []string{"group:mainline"},
 		Timeout:  time.Minute,
 		Fixture:  "ensureToolkit",
-		// Skip "nyan_kitty" due to slow reboot speed.
-		HardwareDeps: hwdep.D(hwdep.SkipOnModel("kitty")),
+		// Skip "nyan_kitty" due to slow reboot speed, skip nocturne as
+		// it can not open the Kiosk page and is not manufactured. Skip
+		// volteer as it is not stable.
+		HardwareDeps: hwdep.D(hwdep.SkipOnModel("kitty"), hwdep.SkipOnPlatform("nocturne"), hwdep.SkipOnPlatform("volteer")),
 	})
 }
 
