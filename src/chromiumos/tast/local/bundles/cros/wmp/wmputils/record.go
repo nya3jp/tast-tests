3dafba758fd686a3c8a7bec9919f55ca6755109f
Yichen Zhou <yichenz@chromium.org>
2021-10-25 04:41:33 +0000

    tast-tests: stable entering/exiting record mode
    OverviewMode test somehow got into record mode[1] and thus still failed
    after crrev/c/3230335 submitted. Failures in entering/exiting record
    mode in RecordFullScreen and RecordPartialScreen would cause other tests
    to fail. This cl added EnsureRecordModeActivated function to make sure
    entering/exiting record mode successfully in both RecordFullScreen and
    RecordPartialScreen.
    
    BUG=chromium:1261243, chromium:1261250
    TEST=tast run DUT wmp.RecordPartialScreen.* wmp.RecordFullScreen.*
    [1] https://screenshot.googleplex.com/3UjyWNL3gHVJCPQ
    Change-Id: I758d57f15403201d222cd793bebb54c24abb6e15

---
+++ src/chromiumos/tast/local/bundles/cros/wmp/wmputils/record.go
@@ -0,0 +1,67 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package wmputils contains utility functions for wmp tests.
+package wmputils
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/action"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+type recordModeCondition struct {
+	function action.Action
+	errorMsg string
+	key      string
+}
+
+// EnsureRecordModeActivated makes sure that the recording mode is activated.
+func EnsureRecordModeActivated(tconn *chrome.TestConn, activated bool) uiauto.Action {
+	return func(ctx context.Context) error {
+		return testing.Poll(ctx, func(ctx context.Context) error {
+			ac := uiauto.New(tconn)
+
+			kb, err := input.Keyboard(ctx)
+			if err != nil {
+				return errors.Wrap(err, "failed to create a keyboard")
+			}
+
+			screenRecordToggleButton := nodewith.ClassName("CaptureModeToggleButton").Name("Screen record")
+
+			var condition recordModeCondition
+			if activated {
+				condition = recordModeCondition{
+					function: ac.Exists(screenRecordToggleButton),
+					errorMsg: "it hasn't entered record mode yet",
+					key:      "Ctrl+Shift+F5",
+				}
+			} else {
+				condition = recordModeCondition{
+					function: ac.Gone(screenRecordToggleButton),
+					errorMsg: "it is still in record mode",
+					key:      "Esc",
+				}
+			}
+
+			if err := condition.function(ctx); err != nil {
+				if err := kb.AccelAction(condition.key)(ctx); err != nil {
+					return errors.Wrapf(err, "failed to press %s", condition.key)
+				}
+				return errors.Wrap(err, condition.errorMsg)
+			}
+			return nil
+		}, &testing.PollOptions{
+			Interval: 500 * time.Millisecond,
+			Timeout:  5 * time.Second,
+		})
+	}
+}
