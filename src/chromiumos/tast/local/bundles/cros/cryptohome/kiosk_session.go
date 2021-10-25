6ace50f47d00cfd4c509aec3f25d8fd0f3cc2634
Hardik Goyal <hardikgoyal@google.com>
2021-10-25 13:18:19 -0700

    tast: Add AuthSession kiosk mount test
    This CL adds tast tests to create a kiosk user with old API and
    authenticate it with new API and vice versa.
    
    BUG=b:203787920
    TEST=tast run <target> cryptohome.KioskSession
    
    Change-Id: I9bcec646a97a3a75c7feed93a46b649cfe4c8df9

---
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/kiosk_session.go
@@ -0,0 +1,38 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"context"
+
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: KioskSession,
+		Desc: "Ensures that cryptohome correctly mounts kiosk sessions",
+		Contacts: []string{
+			"hardikgoyal@chromium.org",
+			"chromeos-security@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func KioskSession(ctx context.Context, s *testing.State) {
+	if err := cryptohome.MountKiosk(ctx); err != nil {
+		s.Fatal("Failed to mount kiosk: ", err)
+	}
+	// Unmount Vault.
+	cryptohome.UnmountVault(ctx, cryptohome.KioskUser)
+
+	// Test if existing kiosk vault can be signed in with AuthSession
+	if err := cryptohome.AuthSessionMountFlow(ctx, true /*Kiosk User*/, cryptohome.KioskUser, "" /* Empty passkey*/, false /*Create User*/); err != nil {
+		s.Fatal("Failed to Mount with AuthSession -: ", err)
+	}
+	defer cryptohome.UnmountVault(ctx, cryptohome.KioskUser)
+}
