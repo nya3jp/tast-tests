2339b3186c31bd5059153f82c85bd0604fa13d65
Oleh Lamzin <lamzin@google.com>
2021-10-26 12:01:16 +0200

    Use protocmp.Transform to find proto messages diff
    BUG=b:187790687
    TEST=~/trunk/src/platform/tast/fast_build.sh -T
    
    Cq-Depend: chromium:3242484
    Change-Id: I4a710e1a8954a5aa06c49acf61a190fb2d6f64b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3245373
    Auto-Submit: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>

--- src/chromiumos/tast/local/bundles/cros/session/ownership_retaken.go
+++ src/chromiumos/tast/local/bundles/cros/session/ownership_retaken.go
@@ -11,8 +11,10 @@ import (
 	"io/ioutil"
 	"path/filepath"
 
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+
 	"chromiumos/tast/errors"
-	"chromiumos/tast/local/bundles/cros/session/cmp"
 	"chromiumos/tast/local/cryptohome"
 	"chromiumos/tast/local/session"
 	"chromiumos/tast/local/session/ownership"
@@ -124,7 +126,7 @@ func OwnershipRetaken(ctx context.Context, s *testing.State) {
 	}
 
 	// Verify that there's no diff between sent data and fetched data.
-	if diff := cmp.ProtoDiff(settings, ret); diff != "" {
+	if diff := cmp.Diff(settings, ret, protocmp.Transform()); diff != "" {
 		const diffName = "diff.txt"
 		if err = ioutil.WriteFile(filepath.Join(s.OutDir(), diffName), []byte(diff), 0644); err != nil {
 			s.Error("Failed to write diff: ", err)
