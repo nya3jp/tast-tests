7697d75b8987a315282f694adaf0f541a628935d
Mohamed Omar <mohamedaomar@google.com>
2021-10-26 09:08:51 +0200

    msg: Introduce MGS helper functions library
    Add mgs helpers that sets necessary policies and verifies MGS
    started.
    
    BUG=b:204177106
    TEST=fast_build.sh -T
    TEST=tast run <ip> policy.LaunchManagedGuestSession
    
    Change-Id: I95c1be1bb8a5b63e080e8c675248f4149481dc0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3275927
    Commit-Queue: Mohamed Omar <mohamedaomar@google.com>
    Tested-by: Mohamed Omar <mohamedaomar@google.com>
    Reviewed-by: Kamil Szarek <kamilszarek@google.com>

---
+++ src/chromiumos/tast/local/mgs/config.go
@@ -0,0 +1,54 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package mgs
+
+import (
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/chrome"
+)
+
+// Option is a self-referential function can be used to configure MGS mode.
+// See https://commandcenter.blogspot.com.au/2014/01/self-referential-functions-and-design.html
+// for details about this pattern.
+type Option func(cfg *MutableConfig) error
+
+// Config contains configurations for MGS mode. It holds the necessary
+// policies that will be set to enable MGS mode.
+// Once retrieved by NewConfig() it should be used to read from not to modify.
+type Config struct {
+	m MutableConfig
+}
+
+// MutableConfig holds pieces of configuration that are set with Options.
+type MutableConfig struct {
+	// MGSAccounts applies DeviceLocalAccountInfo custom DeviceLocalAccountInfo configuration.
+	MGSAccounts *policy.DeviceLocalAccounts
+	// ExtraPolicies holds extra policies that will be applied.
+	ExtraPolicies []policy.Policy
+	// PublicAccountPolicies holds public accounts' IDs with associated polices
+	// that will be applied to the them.
+	PublicAccountPolicies map[string][]policy.Policy
+	// AutoLaunch determines whether to set MGS mode to autolaunch. When true
+	// AutoLaunchMGSAppID id is set to autolaunch.
+	AutoLaunch bool
+	// AutoLaunchMGSAppID is an id of the autolaunched MGS account.
+	AutoLaunchMGSAppID *string
+	// ExtraChromeOptions holds all extra options that will be passed to Chrome
+	// instance that will run in MGS mode.
+	ExtraChromeOptions []chrome.Option
+}
+
+// NewConfig creates new configuration.
+func NewConfig(opts []Option) (*Config, error) {
+	cfg := &Config{
+		m: MutableConfig{},
+	}
+	for _, opt := range opts {
+		if err := opt(&cfg.m); err != nil {
+			return nil, err
+		}
+	}
+	return cfg, nil
+}
---
+++ src/chromiumos/tast/local/mgs/options.go
@@ -0,0 +1,77 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package mgs
+
+import (
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/chrome"
+)
+
+// DefaultAccount applies default local account configuration using one
+// arbitrary MGS definition in the mgs package.
+// MGS account id is exposed from the package.
+func DefaultAccount() Option {
+	return func(cfg *MutableConfig) error {
+		cfg.MGSAccounts = &accountsConfiguration
+		return nil
+	}
+}
+
+// Accounts creates DeviceLocalAccountInfo (of a type policy.AccountTypePublicSession)
+// for each passed accountID and adds them all to policy.DeviceLocalAccounts.
+func Accounts(accountIDs ...string) Option {
+	return func(cfg *MutableConfig) error {
+		var mgsAccountPolicies []policy.DeviceLocalAccountInfo
+		for _, accountID := range accountIDs {
+			mgsPolicy := policy.DeviceLocalAccountInfo{
+				AccountID:   &accountID,
+				AccountType: &AccountType,
+			}
+			mgsAccountPolicies = append(mgsAccountPolicies, mgsPolicy)
+		}
+		accounts := policy.DeviceLocalAccounts{
+			Val: mgsAccountPolicies,
+		}
+		cfg.MGSAccounts = &accounts
+		return nil
+	}
+}
+
+// ExtraPolicies adds policies to be applied with MGS.
+func ExtraPolicies(p []policy.Policy) Option {
+	return func(cfg *MutableConfig) error {
+		cfg.ExtraPolicies = p
+		return nil
+	}
+}
+
+// AddPublicAccountPolicies adds policies that will be applied to the account.
+func AddPublicAccountPolicies(accountID string, policies []policy.Policy) Option {
+	return func(cfg *MutableConfig) error {
+		if cfg.PublicAccountPolicies == nil {
+			cfg.PublicAccountPolicies = make(map[string][]policy.Policy)
+		}
+		cfg.PublicAccountPolicies[accountID] = append(cfg.PublicAccountPolicies[accountID], policies...)
+		return nil
+	}
+}
+
+// AutoLaunch sets MGS ID to be started after Chrome restart.
+func AutoLaunch(accountID string) Option {
+	return func(cfg *MutableConfig) error {
+		cfg.AutoLaunch = true
+		cfg.AutoLaunchMGSAppID = &accountID
+		return nil
+	}
+}
+
+// ExtraChromeOptions passes Chrome options that will be applied to the Chrome
+// instance running in MGS mode.
+func ExtraChromeOptions(opts ...chrome.Option) Option {
+	return func(cfg *MutableConfig) error {
+		cfg.ExtraChromeOptions = opts
+		return nil
+	}
+}
