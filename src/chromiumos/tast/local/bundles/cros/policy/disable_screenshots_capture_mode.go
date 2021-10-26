d68246d97edb0cd6c3a3f7acdc7f7d34ec600487
Oleh Lamzin <lamzin@google.com>
2021-10-26 16:49:52 +0200

    policy: promote DisableScreenshotsCaptureMode
    policy.DisableScreenshotsCaptureMode is stable enough to be promoted to the
    CQ:
    https://cros-health-prod.googleplex.com/test/policy.DisableScreenshotsCaptureMode
    
    BUG=none
    TEST=none
    
    Change-Id: I342d995ca36e3920d343ab48669d0da92418d6aa

--- src/chromiumos/tast/local/bundles/cros/policy/disable_screenshots_capture_mode.go
+++ src/chromiumos/tast/local/bundles/cros/policy/disable_screenshots_capture_mode.go
@@ -30,7 +30,7 @@ func init() {
 			"chromeos-commercial-remote-management@google.com",
 		},
 		SoftwareDeps: []string{"chrome"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		Fixture:      fixture.ChromePolicyLoggedIn,
 	})
 }
