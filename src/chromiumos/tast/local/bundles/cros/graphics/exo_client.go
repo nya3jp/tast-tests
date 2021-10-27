da967672247eb5066a0da2764aadea29c9da9467
Femi Adegunloye <mrfemi@google.com>
2021-10-27 00:19:01 +0000

    graphics: add ExoClient test
    Setup exoclient test to run wayland_client_integration_test for HDR
    content.
    
    BUG=b:204242327
    TEST=tast -verbose run -buildbundle=cros $DUT graphics.ExoClient
    
    Change-Id: I4edf5a5a8f6142fbb0975a8472db48f7b428e9cd

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/exo_client.go
@@ -0,0 +1,61 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/gtest"
+	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ExoClient,
+		Desc: "Attaches a client to exo and exercises the wayland APIs",
+		Contacts: []string{
+			"jshargo@chromium.org",
+			"chromeos-gfx-compositor@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      "chromeGraphics",
+		SoftwareDeps: []string{"chrome", "no_qemu"},
+		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+		Timeout:      5 * time.Minute,
+	})
+}
+
+func ExoClient(ctx context.Context, s *testing.State) {
+	testPath := filepath.Join(chrome.BinTestDir, "wayland_client_integration_tests")
+
+	testList, err := gtest.ListTests(ctx, testPath)
+	if err != nil {
+		s.Fatal("Failed to list gtest: ", err)
+	}
+
+	for _, testcase := range testList {
+		if report, err := gtest.New(
+			testPath,
+			gtest.Logfile(filepath.Join(s.OutDir(), testcase+".log")),
+			gtest.Filter(testcase),
+			gtest.ExtraArgs("--test-launcher-jobs=1", "--use-drm", "--wayland_socket=/var/run/chrome/wayland-0"),
+			gtest.UID(int(sysutil.ChronosUID)),
+		).Run(ctx); err != nil {
+			s.Errorf("%s failed: %v", testcase, err)
+			if report != nil {
+				for _, name := range report.FailedTestNames() {
+					s.Error(name, " failed")
+				}
+			} else {
+				s.Error("No additional information is available for this failure")
+			}
+		}
+	}
+}
