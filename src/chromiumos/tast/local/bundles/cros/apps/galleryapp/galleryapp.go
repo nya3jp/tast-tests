6e0b2f66f2a697eef3152da2b1e519d279279b26
Mitch McDermott <mcdermottm@google.com>
2021-10-27 14:11:16 +0000

    WIP: Update gallery root finder
    BUG=b:200898614, b:202214335
    TEST=Ran Gallery Tast tests
    
    Change-Id: I4a83e6411a12b42dad111135b128e72a23c5e576

--- src/chromiumos/tast/local/bundles/cros/apps/galleryapp/galleryapp.go
+++ src/chromiumos/tast/local/bundles/cros/apps/galleryapp/galleryapp.go
@@ -34,8 +34,11 @@ func NewContext(cr *chrome.Chrome, tconn *chrome.TestConn) *GalleryContext {
 	}
 }
 
-// RootFinder is the finder of Gallery app root window.
-var RootFinder = nodewith.Name(apps.Gallery.Name).Role(role.RootWebArea)
+// BrowserFinder is the finder of Gallery app browser window.
+var BrowserFinder = nodewith.NameStartingWith(apps.Gallery.Name).ClassName("BrowserFrame")
+
+// RootFinder is the finder of Gallery app root web area.
+var RootFinder = nodewith.Role(role.RootWebArea).Ancestor(BrowserFinder).First()
 
 // DialogFinder is the finder of popup dialog in Gallery app.
 var DialogFinder = nodewith.Role(role.AlertDialog).Ancestor(RootFinder)
