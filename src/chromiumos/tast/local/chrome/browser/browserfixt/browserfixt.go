386116efbea5ae45e75c6e4ab7a8de8d9aaffec5
Georg Neis <neis@chromium.org>
2021-10-27 14:09:06 +0200

    tast-tests: introduce 'browser' package
    ... to be used for testing both Ash and Lacros. As an example,
    make use of it in the policy.ShowHomeButton test.
    
    BUG=chromium:1254152
    TEST=tast run policy.ShowHomeButton*
    
    Change-Id: I01aad9f124245ec4dfaf53840a625de115648f5f

---
+++ src/chromiumos/tast/local/chrome/browser/browserfixt/browserfixt.go
@@ -0,0 +1,41 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package browserfixt provides a function for obtaining a Browser instance for
+// a given tast fixture and browser type.
+package browserfixt
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/lacros/launcher"
+)
+
+// SetUp returns a Browser instance for a given fixture value and browser type.
+// It also returns a closure to be called in order to close the browser instance,
+// after which the instance should not be used any further.
+// If browser type is TypeAsh, the fixture value must implement the HasChrome interface.
+// If browser type is TypeLacros, the fixture value must be of launcher.FixtValue type.
+func SetUp(ctx context.Context, f interface{}, typ browser.Type) (*browser.Browser, func(ctx context.Context), error) {
+	switch typ {
+	case browser.TypeAsh:
+		cr := f.(chrome.HasChrome).Chrome()
+		return cr.Browser(), func(context.Context) {}, nil
+	case browser.TypeLacros:
+		f := f.(launcher.FixtValue)
+		l, err := launcher.LaunchLacrosChrome(ctx, f)
+		if err != nil {
+			return nil, nil, errors.Wrap(err, "failed to launch lacros-chrome")
+		}
+		closeLacros := func(ctx context.Context) {
+			l.Close(ctx) // Ignore error.
+		}
+		return l.Browser(), closeLacros, nil
+	default:
+		return nil, nil, errors.Errorf("unrecognized browser type %s", string(typ))
+	}
+}
