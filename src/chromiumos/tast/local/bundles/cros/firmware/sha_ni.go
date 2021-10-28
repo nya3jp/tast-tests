9e0243e244f5d4b33d0f2fe3ebbdbf2c5ffef119
Kangheui Won <khwon@chromium.org>
2021-10-28 17:36:01 +1100

    firmware.SHANI: Add test
    Add tast test for vb2_sha256_x86_tests.
    The binary is part of test image as vboot_reference-tests ebuild.
    
    BUG=b:162551138
    TEST=tast run ${IP} firmware.SHANI
    
    Change-Id: I84168317aa6a0a002eb42ba77ccbb63671b12f58

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/sha_ni.go
@@ -0,0 +1,34 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SHANI,
+		Desc: "Run SHA-NI extension test on x86 platforms",
+		Contacts: []string{
+			"khwon@chromium.org",           // Test Author
+			"chromeos-firmware@google.com", // CrOS Firmware Developers
+		},
+		Attr:         []string{"group:firmware", "firmware_unstable"},
+		HardwareDeps: hwdep.D(hwdep.X86()),
+	})
+}
+
+func SHANI(ctx context.Context, s *testing.State) {
+	testBin := "/usr/local/share/vboot/tests/vb2_sha256_x86_tests"
+	err := testexec.CommandContext(ctx, testBin).Run(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Test failed: ", err)
+	}
+}
