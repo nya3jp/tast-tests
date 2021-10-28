17b109222a03a200a28a79acdb533ad9a8e44a9f
Jasmine Chen <lnishan@google.com>
2021-10-28 23:48:44 +0800

    camera: Replace or remove lnishan@ from owners in tests
    lnishan@ left the Chrome OS camera team in Aug, and this CL hands off
    the ownership of the tests lnishan@ previously owned.
    
    BUG=None
    TEST=Run tast tests.
    
    Change-Id: I77510d94df35db843771b627424f901a54bfb4a6

--- src/chromiumos/tast/local/bundles/cros/camera/hal3_still_capture_zsl.go
+++ src/chromiumos/tast/local/bundles/cros/camera/hal3_still_capture_zsl.go
@@ -18,7 +18,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         HAL3StillCaptureZSL,
 		Desc:         "Verifies camera still capture with ZSL function with HAL3 interface",
-		Contacts:     []string{"lnishan@chromium.org", "chromeos-camera-eng@google.com"},
+		Contacts:     []string{"hywu@chromium.org", "chromeos-camera-eng@google.com"},
 		Attr:         []string{"group:mainline", "informational", "group:camera-libcamera"},
 		SoftwareDeps: []string{"arc", "arc_camera3", "chrome", caps.BuiltinCamera},
 		Pre:          chrome.LoggedIn(),
