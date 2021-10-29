e65181d540b6b250079481c0b4723752ea082cf8
Ruben Zakarian <rzakarian@google.com>
2021-10-29 14:56:39 -0700

    ChromeOS Enterprise: Add Integration Test
    This CL adds a Tast integration test that covers the interaction between the
    ChromeOS client and the Reporting API server.
    
    This is a remote version of this crrev.com/c/3098394.
    
    Tested: https://paste.googleplex.com/5635097156911104
    
    BUG=None
    TEST=tested on local device
    
    Change-Id: Iafd5340cfa5d91595b3b411e8af87ccd01111b3d

--- src/chromiumos/tast/local/bundles/cros/policy/policy_service.go
+++ src/chromiumos/tast/local/bundles/cros/policy/policy_service.go
@@ -7,23 +7,30 @@ package policy
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 
+	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/local/policyutil/externaldata"
 	ppb "chromiumos/tast/services/cros/policy"
 	"chromiumos/tast/testing"
 )
+
+const heartbeatEventDestination = "HEARTBEAT_EVENTS"
 
 func init() {
 	testing.AddService(&testing.Service{
@@ -50,6 +57,18 @@ type PolicyService struct { // NOLINT
 	eds *externaldata.Server
 }
 
+type inputEvent struct {
+	APIEvent *struct {
+		ReportingRecordEvent *struct {
+			Destination string `json:"destination"`
+			Time        string `json:"timestampUs"`
+		} `json:"reportingRecordEvent"`
+	} `json:"apiEvent"`
+	ObfuscatedCustomerID string `json:"obfuscatedCustomerID"`
+	ObfuscatedGaiaID     string `json:"obfuscatedGaiaID"`
+	ClientID             string `json:"clientId"`
+}
+
 // GAIAEnrollUsingChrome enrolls the device using dmserver. Specified user is logged in after this function completes.
 func (c *PolicyService) GAIAEnrollUsingChrome(ctx context.Context, req *ppb.GAIAEnrollUsingChromeRequest) (*empty.Empty, error) {
 	testing.ContextLogf(ctx, "Enrolling using Chrome with username: %s, dmserver: %s", string(req.Username), string(req.DmserverURL))
@@ -65,6 +84,146 @@ func (c *PolicyService) GAIAEnrollUsingChrome(ctx context.Context, req *ppb.GAIA
 	}
 
 	c.chrome = cr
+
+	return &empty.Empty{}, nil
+}
+
+// getDeviceVirtualID Returns the device's virtual ID, which is used to
+// identify the enrolled device when communicating with enterprise servers.
+// TODO: For now, this function extract Chrome's device virtual ID from the
+// chrome://policy page. But with a little plumbing, we can also extract the
+// device virtual ID, as well as other enterprise related IDs, directly from
+// the browser process through the AutotestPrivate extension.
+func getDeviceVirtualID(ctx context.Context, cr *chrome.Chrome) (string, error) {
+	conn, err := cr.NewConn(ctx, "chrome://policy")
+	if err != nil {
+		return "", errors.Wrap(err, "failed to navigate to chrome://policy page")
+	}
+
+	defer conn.Close()
+
+	var deviceVirtualID string
+	xpath := `//fieldset[./legend[./text()='Device policies']]/div/div[@class='client-id']/text()`
+	evalExpr := fmt.Sprintf(`document.evaluate("%s", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent`, xpath)
+	if err := conn.Eval(ctx, evalExpr, &deviceVirtualID); err != nil {
+		return "", errors.Wrap(err, evalExpr)
+	}
+	return strings.TrimSpace(deviceVirtualID), nil
+}
+
+// lookupEvents Call the Reporting API Server's ChromeReportingDebugService.LookupEvents
+// endpoint to get a list of events received by the server from this user.
+func lookupEvents(ctx context.Context, reportingServerURL, userEmail, obfuscatedCustomerID, apiKey string) ([]inputEvent, error) {
+	type lookupEventsResponse struct {
+		Event []inputEvent `json:"event"`
+	}
+
+	reqPath := fmt.Sprintf("%v/test/events?key=%v&obfuscatedCustomerId=%v", reportingServerURL, apiKey, obfuscatedCustomerID)
+	req, err := http.NewRequestWithContext(ctx, "GET", reqPath, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to craft event query request to the Reporting Server")
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to issue debug query request to the Reporting Server")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, errors.Errorf("reporting server encountered an error with the event query %s %v %v", reqPath, resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read the response body")
+	}
+	var resData lookupEventsResponse
+	if err := json.Unmarshal(resBody, &resData); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal response")
+	}
+	return resData.Event, nil
+}
+
+// validateEventReception Given a list of events received by the Reporting
+// API Server, validate whether the list contains events sent by this test.
+func validateEventReception(events []inputEvent, deviceVirtualID, eventDestination string, testStartTime time.Time, containsEvent *bool) error {
+	*containsEvent = false
+	for _, event := range events {
+		// Filter events by the device virtual ID, which uniquely identifies
+		// this test device, and by the event destination, which identifies the
+		// event type.
+		if event.ClientID == deviceVirtualID && event.APIEvent.ReportingRecordEvent.Destination == eventDestination {
+			// Parse the timestamp and check if the server received the event
+			// after test started.
+			micros, err := strconv.ParseInt(event.APIEvent.ReportingRecordEvent.Time, 10, 64)
+			if err != nil {
+				return errors.Wrap(err, "failed to parse int64 Spanner timestamp from event")
+			}
+			enqueueTime := time.Unix(micros/(int64(time.Second)/int64(time.Microsecond)), 0)
+			if enqueueTime.After(testStartTime) {
+				// Found an event sent by this test device after the test start time.
+				// This proves that the server received events from this device.
+				*containsEvent = true
+				return nil
+			}
+		}
+	}
+	return nil
+}
+
+// GAIAEnrollUsingChromeAndCollectReporting enrolls the device using dmserver. Specified user is logged in after this function completes.
+func (c *PolicyService) GAIAEnrollUsingChromeAndCollectReporting(ctx context.Context, req *ppb.GAIAEnrollUsingChromeAndCollectReportingRequest) (*empty.Empty, error) {
+	testing.ContextLogf(ctx, "Enrolling using Chrome with username: %s, dmserver: %s", string(req.Username), string(req.DmserverURL))
+
+	testStartTime := time.Now()
+
+	cr, err := chrome.New(
+		ctx,
+		chrome.GAIAEnterpriseEnroll(chrome.Creds{User: req.Username, Pass: req.Password}),
+		chrome.GAIALogin(chrome.Creds{User: req.Username, Pass: req.Password}),
+		chrome.DMSPolicy(req.DmserverURL),
+		chrome.EnableFeatures("EncryptedReportingPipeline", "EncryptedReportingHeartbeatEvent"),
+		chrome.EncryptedReportingAddr(fmt.Sprintf("%v/record", req.ReportingserverURL)),
+		chrome.ExtraArgs("--login-manager"),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start chrome")
+	}
+
+	c.chrome = cr
+
+	tconn, err := cr.TestAPIConn(ctx)
+
+	// Check that the cloud reporting policy is enabled.
+	testing.ContextLog(ctx, "Verify that Cloud Reporting is enabled")
+	if err := policyutil.Verify(ctx, tconn, []policy.Policy{&policy.CloudReportingEnabled{Val: true}}); err != nil {
+		errors.Wrap(err, "failed to verify Real-Time Reporting policy")
+	}
+
+	deviceVirtualID, err := getDeviceVirtualID(ctx, cr)
+	if err != nil {
+		errors.Wrap(err, "cannot get Device virtual ID")
+	}
+
+	testing.ContextLog(ctx, "Sleep to allow the integration test event to trigger")
+	if err := testing.Sleep(ctx, 10*time.Second); err != nil {
+		errors.Wrap(err, "failed to sleep")
+	}
+
+	// Call the Reporting Server's lookupEvents API to verify that
+	// the server received the test event.
+	events, err := lookupEvents(ctx, req.ReportingserverURL, req.Username, req.ObfuscatedCustomerID, req.DebugServiceAPIKey)
+	if err != nil {
+		errors.Wrap(err, "failed to look up events")
+	}
+
+	receivedNewEvents := false
+	if err := validateEventReception(events, deviceVirtualID, heartbeatEventDestination, testStartTime, &receivedNewEvents); err != nil {
+		errors.Wrap(err, "failed to validate event reception")
+	}
+
+	if !receivedNewEvents {
+		errors.Wrap(err, "the Reporting Api Server did not receive any events")
+	}
 
 	return &empty.Empty{}, nil
 }
---
+++ src/chromiumos/tast/remote/bundles/cros/policy/gaia_reporting.go
@@ -0,0 +1,101 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/remote/policyutil"
+	"chromiumos/tast/rpc"
+	ps "chromiumos/tast/services/cros/policy"
+	"chromiumos/tast/testing"
+)
+
+type testParameters struct {
+	username             string // username for Chrome login
+	password             string // password to login
+	dmserver             string // device management server url
+	reportingserver      string // reporting api server url
+	obfuscatedcustomerid string // external customer id
+	debugservicekey      string // debug service key
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: GAIAReporting,
+		Desc: "GAIA Enroll a device and verify reporting functionality",
+		Contacts: []string{
+			"rzakarian@google.com", // Test author
+			"chromeos-commercial-remote-management@google.com",
+		},
+		Attr:         []string{"group:enrollment"},
+		SoftwareDeps: []string{"reboot", "chrome"},
+		ServiceDeps:  []string{"tast.cros.policy.PolicyService"},
+		Timeout:      7 * time.Minute,
+		Params: []testing.Param{
+			{
+				Name: "autopush",
+				Val: testParameters{
+					username:             "policy.GAIAReporting.user_name",
+					password:             "policy.GAIAReporting.password",
+					dmserver:             "https://crosman-alpha.sandbox.google.com/devicemanagement/data/api",
+					reportingserver:      "https://autopush-chromereporting-pa.sandbox.googleapis.com/v1",
+					obfuscatedcustomerid: "policy.GAIAReporting.obfuscated_customer_id",
+					debugservicekey:      "policy.GAIAReporting.lookup_events_api_key",
+				},
+			},
+		},
+		Vars: []string{
+			"policy.GAIAReporting.user_name",
+			"policy.GAIAReporting.password",
+			"policy.GAIAReporting.obfuscated_customer_id",
+			"policy.GAIAReporting.lookup_events_api_key",
+		},
+	})
+}
+
+func GAIAReporting(ctx context.Context, s *testing.State) {
+	param := s.Param().(testParameters)
+	username := s.RequiredVar(param.username)
+	password := s.RequiredVar(param.password)
+	dmServerURL := param.dmserver
+	reportingServerURL := param.reportingserver
+	obfuscatedCustomerID := s.RequiredVar(param.obfuscatedcustomerid)
+	debugServiceAPIKey := s.RequiredVar(param.debugservicekey)
+
+	defer func(ctx context.Context) {
+		if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
+			s.Error("Failed to reset TPM after test: ", err)
+		}
+	}(ctx)
+
+	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Minute)
+	defer cancel()
+
+	if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
+		s.Fatal("Failed to reset TPM: ", err)
+	}
+
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	pc := ps.NewPolicyServiceClient(cl.Conn)
+
+	if _, err := pc.GAIAEnrollUsingChromeAndCollectReporting(ctx, &ps.GAIAEnrollUsingChromeAndCollectReportingRequest{
+		Username:             username,
+		Password:             password,
+		DmserverURL:          dmServerURL,
+		ReportingserverURL:   reportingServerURL,
+		ObfuscatedCustomerID: obfuscatedCustomerID,
+		DebugServiceAPIKey:   debugServiceAPIKey,
+	}); err != nil {
+		s.Fatal("Failed to enroll using chrome: ", err)
+	}
+}
