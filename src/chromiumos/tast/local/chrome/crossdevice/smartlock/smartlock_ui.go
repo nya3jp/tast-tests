30563d5ac64169c634cc690c26e8f8afd7ca691c
David Haddock <dhaddock@google.com>
2021-11-01 13:35:42 -0700

    smartlock: Add Login test.
    This CL does a few things:
    
    1. Changed Feature from an int to a struct. This gives us greater
    flexibility over the GAIA accounts to use for a test. It means we can
    use different credentials for smart lock unlock and login cases.
    
    2. Made screen recordings and locking Chrome in the fixture optional.
    Disabled both for Smart Lock login. Screen lock only works while signed
    in so screen recordings dont make sense here as we do multiple
    login/logout in the test.
    
    3. Added ability to enable and disable Smart Lock login via javascript.
    Refactored the authentication token code a bit. PIN Unlock wanted just
    the token value froom the authToken_ object. Smart lock wants the
    whole object. Chrome crashes if you just give it the token :)
    
    4. Smart Lock login test.
    Enables the feature in OS settings. Signs out. Logs in with password.
    Logs out. Logins in with Smart Lock.
    
    BUG=b:198788091
    TEST=smartlock.*
    TEST=lockscreen.PINUnlock
    
    Change-Id: I9be1918ff18fb3d3fb38f342ff09feef3ea4a101

---
+++ src/chromiumos/tast/local/chrome/crossdevice/smartlock/smartlock_ui.go
@@ -0,0 +1,87 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package smartlock is for controlling ChromeOS Smart Lock functionality.
+package smartlock
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+)
+
+const (
+	connectedDevicesSettingsURL = "multidevice/features/"
+	smartLockSettingsURL        = "smartLock"
+	smartLockSubpage            = `document.querySelector("os-settings-ui").shadowRoot` +
+		`.querySelector("os-settings-main").shadowRoot` +
+		`.querySelector("os-settings-page").shadowRoot` +
+		`.querySelector("settings-multidevice-page").shadowRoot` +
+		`.querySelector("settings-multidevice-smartlock-subpage")`
+)
+
+// OpenSmartLockSubpage opens the Smart Lock sub page in OS Settings
+func OpenSmartLockSubpage(ctx context.Context, tconn *chrome.TestConn, cr *chrome.Chrome) (*ossettings.OSSettings, error) {
+	settings, err := ossettings.LaunchAtPageURL(ctx, tconn, cr, connectedDevicesSettingsURL+smartLockSettingsURL, func(context.Context) error { return nil })
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to launch OS Settings to the Smart Lock page")
+	}
+	return settings, nil
+}
+
+// DisableSmartLockLogin disables Smart Lock login functionality.
+// This means that only unlocking with Smart Lock is allowed.
+func DisableSmartLockLogin(ctx context.Context, tconn *chrome.TestConn, cr *chrome.Chrome) error {
+	settings, err := OpenSmartLockSubpage(ctx, tconn, cr)
+	if err != nil {
+		return errors.Wrap(err, "failed to open Smart Lock subpage in OS Settings")
+	}
+	settingsConn, err := settings.ChromeConn(ctx, cr)
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to OS settings target")
+	}
+	if err := settingsConn.WaitForExpr(ctx, smartLockSubpage); err != nil {
+		return errors.Wrap(err, "failed waiting for Smart Lock subpage to load")
+	}
+	if err := settingsConn.Eval(ctx, smartLockSubpage+`.updateSmartLockSignInEnabled_(false)`, nil); err != nil {
+		return errors.Wrap(err, "failed to toggle smart lock login button")
+	}
+	return nil
+}
+
+// EnableSmartLockLogin enables the ability to login with Smart Lock.
+func EnableSmartLockLogin(ctx context.Context, tconn *chrome.TestConn, cr *chrome.Chrome, password string) error {
+	settings, err := OpenSmartLockSubpage(ctx, tconn, cr)
+	if err != nil {
+		return errors.Wrap(err, "failed to open Smart Lock subpage in OS Settings")
+	}
+	settingsConn, err := settings.ChromeConn(ctx, cr)
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to OS settings target")
+	}
+	token, err := settings.AuthToken(ctx, settingsConn, password)
+	if err != nil {
+		return errors.Wrap(err, "failed to get auth token")
+	}
+	data, err := json.Marshal(token)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshall auth token to JSON")
+	}
+	if err := settingsConn.WaitForExpr(ctx, smartLockSubpage); err != nil {
+		return errors.Wrap(err, "failed waiting for Smart Lock subpage to load")
+	}
+	expr := fmt.Sprintf(`%s.authToken_ = %s`, smartLockSubpage, data)
+	if err := settingsConn.Eval(ctx, expr, nil); err != nil {
+		return errors.Wrap(err, "failed to set authToken_ property")
+	}
+	if err := settingsConn.Eval(ctx, smartLockSubpage+`.onEnableSignInDialogClose_()`, nil); err != nil {
+		return errors.Wrap(err, "failed to toggle smart lock login button")
+	}
+
+	return nil
+}
