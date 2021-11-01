8f22fa49bd9d8128c52a67a055e9200a1c6003b2
Arowa Suliman <arowa@google.com>
2021-11-01 16:44:16 -0700

    wifi: Validate Intel fw dump
    Update the test wifi.CheckIntelFWDump to read the fw dump and check the
    validity of the fw dump instead of checking the size directly.
    
    The following checks are implemented:
    1- Check the file size in the header.
    2- Check the magic signature in the header.
    
    For more info refer to:
    [1]  https://b.corp.google.com/issues/169152720#comment28
    
    BUG=b:201671511, b:169152720
    TEST=ran test on two fw dumps {YOYO_MAGIC, non_YOYO_MAGIC} and compared
    the results with the original script [1]
    1- [YOYO_MAGIC] tast run ${DUT} wifi.CheckIntelFWDump [[ PASS ]]
        The test pass and the results match the original script.
    2- [non_YOYO_MAGIC] tast run ${DUT} wifi.CheckIntelFWDump [[ FAIL ]]
        The test fails but the rsults match the original script.
    
    Change-Id: Ifd9985cce8d4ca00e99151bd8379cfb62a1196f8

---
+++ src/chromiumos/tast/local/bundles/cros/wifi/intelfwextractor/intel_fw_extractor.go
@@ -0,0 +1,303 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package intelfwextractor extracts the fw dump and validate its contents.
+package intelfwextractor
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"encoding/binary"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+const (
+	yoyoMagic    = 0x14789633 // StP2
+	validMagic   = 0x14789632 // HrP2, CcP2, JfP2, ThP2
+	dumpInfoType = 1 << 31
+	csrFile      = "csr"
+	monitorFile  = "monitor"
+	lmacDccmFile = "dccm_lmac"
+)
+
+// DevcoreHeader is a struct that contains the fw dump header information.
+type DevcoreHeader struct {
+	Magic    uint32
+	FileSize uint32
+}
+
+// TLHeader is a struct that contains the type and length of a TLV header.
+type TLHeader struct {
+	Type   uint32
+	Length uint32
+}
+
+// VHeader is a struct that contains the memory type and address of a value in a  TLV header.
+type VHeader struct {
+	MemType uint32
+	Addr    uint32
+}
+
+// VYoYoMemHeader is a struct for reading the value of a YoYo TLV header.
+type VYoYoMemHeader struct {
+	Version     uint32
+	RegionID    uint32
+	NumOfRanges uint32
+	NameLength  uint32
+}
+
+// MandatoryFWDumpFiles are the mandaotry files that must exists in the fw dump
+// to be valid. Refer to http://b/169152720#comment28 for more information.
+// csr.lst       (mandatory) header type 1
+// monitor.lst   (mandatory) header type 5
+// dccm_lmac.lst (mandatory) header type of 9
+type MandatoryFWDumpFiles struct {
+	Csr      bool
+	Monitor  bool
+	DccmLmac bool
+}
+
+// ValidateFWDump extracts and validate the contents of the fw dump.
+func ValidateFWDump(ctx context.Context, file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return errors.Wrapf(err, "failed to open the file %s", file)
+	}
+	defer f.Close()
+
+	var r io.ReadCloser
+	r, err = gzip.NewReader(f)
+	if err != nil {
+		return errors.Wrap(err, "failed to create gzip reader")
+	}
+	defer r.Close()
+
+	fwDumpData, err := ioutil.ReadAll(r)
+	if err != nil {
+		return errors.Wrap(err, "failed to read the fw_dump")
+	}
+
+	fwDumpBuffer := bytes.NewBuffer(fwDumpData)
+
+	// Validate the header.
+	fwDumpHeader := DevcoreHeader{}
+	err = binary.Read(fwDumpBuffer, binary.LittleEndian, &fwDumpHeader)
+	if err != nil {
+		return errors.Wrap(err, "failed to read the header of the fw dump")
+	}
+
+	// Check the fw dump size.
+	if len(fwDumpData) != int(fwDumpHeader.FileSize) {
+		return errors.Errorf("unexpected file size: got %d, want %d", len(fwDumpData), fwDumpHeader.FileSize)
+	}
+
+	var mFiles MandatoryFWDumpFiles
+
+	// Check the magic signature of the fw dump.
+	if fwDumpHeader.Magic == yoyoMagic {
+		testing.ContextLog(ctx, "FW magic Signature: YOYO_Magic")
+		if err := validateYoYoMagicFWDump(fwDumpBuffer, &mFiles); err != nil {
+			return err
+		}
+	} else if fwDumpHeader.Magic == validMagic {
+		testing.ContextLog(ctx, "FW magic Signature: not YOYO_Magic")
+		if err := validateNonYoYoMagicFWDump(fwDumpBuffer, &mFiles); err != nil {
+			return err
+		}
+	} else {
+		return errors.Errorf("invalid magic signature: got %x, want  {%x, %x}", fwDumpHeader.Magic, validMagic, yoyoMagic)
+	}
+
+	// Check if the mandatory files exists in the fw dump.
+	if !mFiles.Csr || !mFiles.Monitor || !mFiles.DccmLmac {
+		return errors.Errorf("One of the mandatory files id messing: got {csr: %t, monitor: %t, dccm_lmac: %t}, want {csr: True, monitor: True, dccm_lmac: True}", mFiles.Csr, mFiles.Monitor, mFiles.DccmLmac)
+	}
+
+	return nil
+}
+
+func validateYoYoMagicFWDump(fwDumpBuffer *bytes.Buffer, mandatoryFiles *MandatoryFWDumpFiles) error {
+	// Parse the fw dump.
+	tlHeader := TLHeader{}
+	vYoYoMemHeader := VYoYoMemHeader{}
+
+	for fwDumpBuffer.Len() > 0 {
+		// Read a TLV header.
+		err := binary.Read(fwDumpBuffer, binary.LittleEndian, &tlHeader)
+		if err != nil {
+			return errors.Wrap(err, "failed to read the header of the fw dump")
+		}
+
+		// Continue if the TLV header type is 0.
+		if tlHeader.Type == 0 {
+			fwDumpBuffer.Next(4)
+			continue
+		}
+
+		if tlHeader.Length > 0 {
+			// Read the TLV header value.
+			if tlHeader.Type != dumpInfoType {
+
+				// Extract the value from the buffer using the tlHeader.Length.
+				value := *fwDumpBuffer
+				value.Truncate(int(tlHeader.Length))
+
+				// Read the VYoYoMemHeader to get the file name length.
+				err = binary.Read(&value, binary.LittleEndian, &vYoYoMemHeader)
+				if err != nil {
+					return errors.Wrap(err, "failed to read the header of the fw dump")
+				}
+
+				// Extarct the file name from value using the vYoYoMemHeader.NameLength.
+				fileName := value
+				fileName.Truncate(int(vYoYoMemHeader.NameLength))
+
+				// Trim the leading null values in the file name bytes and converting to string type.
+				fileNameTrimed := string(bytes.TrimRight(fileName.Bytes(), "\x00"))
+
+				// Check if the file name is matching any of the mandatory files.
+				if fileNameTrimed == csrFile {
+					mandatoryFiles.Csr = true
+				} else if fileNameTrimed == monitorFile {
+					mandatoryFiles.Monitor = true
+				} else if fileNameTrimed == lmacDccmFile {
+					mandatoryFiles.DccmLmac = true
+				}
+			}
+		}
+		fwDumpBuffer.Next(int(tlHeader.Length))
+	}
+
+	return nil
+}
+
+func validateNonYoYoMagicFWDump(fwDumpBuffer *bytes.Buffer, mandatoryFiles *MandatoryFWDumpFiles) error {
+	// Check the type of the memory API.
+	newMemAPI, err := checkMemoryAPI(fwDumpBuffer)
+	if err != nil {
+		return err
+	}
+
+	tlHeader := TLHeader{}
+	vHeader := VHeader{}
+
+	sramFiles := false
+	wifiDeviceName := ""
+
+	// Parse the fw dump.
+	for fwDumpBuffer.Len() > 0 {
+		// Read the TLV header.
+		err := binary.Read(fwDumpBuffer, binary.LittleEndian, &tlHeader)
+		if err != nil {
+			return errors.Wrap(err, "failed to read the header of the fw dump")
+		}
+
+		// Continue if the TLV header type is 0.
+		if tlHeader.Type == 0 {
+			fwDumpBuffer.Next(4)
+			continue
+		}
+
+		if tlHeader.Length == 0 {
+			continue
+		}
+
+		// Check the type of the TLV header.
+		switch tlHeader.Type {
+		case 1:
+			mandatoryFiles.Csr = true
+		case 4:
+			// Read device name.
+			// Extract the TLV value from the buffer using the tlHeader.Length.
+			hValue := *fwDumpBuffer
+			hValue.Next(72)
+			hValue.Truncate(64)
+			wifiDeviceName = hValue.String()
+		case 5:
+			mandatoryFiles.Monitor = true
+		case 9:
+			// Extract the TLV value from the buffer using the tlHeader.Length.
+			hValue := *fwDumpBuffer
+			hValue.Truncate(int(tlHeader.Length))
+
+			// Read Memory Type and Address from the TLV value using vHeader.
+			err = binary.Read(&hValue, binary.LittleEndian, &vHeader)
+			if err != nil {
+				return errors.Wrap(err, "failed to read the header of the fw dump")
+			}
+			if newMemAPI == 0 {
+				if vHeader.MemType == 0 {
+					sramFiles = true
+				}
+			} else {
+				// Check the memory type of the header.
+				if vHeader.MemType == 0 {
+					mandatoryFiles.DccmLmac = true
+				}
+			}
+		}
+
+		fwDumpBuffer.Next(int(tlHeader.Length))
+	}
+
+	if newMemAPI == 0 {
+		// Check the name of the WiFi chip.
+		if strings.Contains(wifiDeviceName, "7260") || strings.Contains(wifiDeviceName, "7265") || strings.Contains(wifiDeviceName, "a620") {
+			// Check if sram files has been created.
+			if sramFiles {
+				mandatoryFiles.DccmLmac = true
+			}
+		} else if strings.Contains(wifiDeviceName, "8000") || strings.Contains(wifiDeviceName, "8260") {
+			mandatoryFiles.DccmLmac = true
+		}
+	}
+
+	return nil
+}
+
+func checkMemoryAPI(fwDumpBuffer *bytes.Buffer) (int, error) {
+	checkMemAPIData := *fwDumpBuffer
+	newMemAPI := 0
+	tlHeader := TLHeader{}
+	vHeader := VHeader{}
+	for checkMemAPIData.Len() > 0 {
+		// Read the TLV header.
+		err := binary.Read(&checkMemAPIData, binary.LittleEndian, &tlHeader)
+		if err != nil {
+			return 0, errors.Wrap(err, "failed to read the header of the fw dump")
+		}
+
+		// Check the TLV header type.
+		if tlHeader.Type == 0 {
+			checkMemAPIData.Next(4)
+			continue
+		}
+
+		if tlHeader.Type == 9 {
+			// Extract the header value from the buffer using the tlHeader.Length.
+			value := checkMemAPIData
+			value.Truncate(int(tlHeader.Length))
+
+			// Read Memory Type and Address from the TLV value using vHeader.
+			err = binary.Read(&value, binary.LittleEndian, &vHeader)
+			if err != nil {
+				return 0, errors.Wrap(err, "failed to read the header of the fw dump")
+			}
+
+			// Check if the memory type is 2.
+			if vHeader.MemType == 2 {
+				newMemAPI = 1
+			}
+		}
+		checkMemAPIData.Next(int(tlHeader.Length))
+	}
+	return newMemAPI, nil
+}
