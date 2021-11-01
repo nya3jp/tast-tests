f2c3d5bc8194180866aa3ac6cfde28163abbef45
Arowa Suliman <arowa@google.com>
2021-11-01 16:44:16 -0700

    Tast: Validate Intel fw dump.
    Update the test wifi.CheckIntelFWDump to read the fw dump and check the
    validity of the fw dump instead of checking the size directly.
    
    The following checks are implemented:
    1- Check the file size in the header.
    2- Check the magic signature in the header.
    
    For more info refer to:
    https://b.corp.google.com/issues/169152720#comment28
    
    BUG=b:201671511, b:169152720
    TEST=ran tast run ${DUT} wifi.CheckIntelFWDump
    
    Change-Id: Ifd9985cce8d4ca00e99151bd8379cfb62a1196f8

---
+++ src/chromiumos/tast/local/bundles/cros/wifi/intelfwextractor/intel_fw_extractor.go
@@ -0,0 +1,71 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package intelfwextractor extracts the fw dump and validate its contents.
+package intelfwextractor
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"encoding/binary"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"chromiumos/tast/errors"
+)
+
+const (
+	yoyoMagic  = 0x14789633 // StP2
+	validMagic = 0x14789632 // HrP2, CcP2, JfP2, ThP2
+)
+
+// DevcoreHeader is a struct that contains the fw dump header information.
+type DevcoreHeader struct {
+	Magic    uint32
+	FileSize uint32
+}
+
+// ValidateFWDump extracts and validate the contents of the fw dump.
+func ValidateFWDump(ctx context.Context, file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return errors.Wrapf(err, "failed to open the file %s: ", file)
+	}
+	defer f.Close()
+
+	var r io.ReadCloser
+	r, err = gzip.NewReader(f)
+	if err != nil {
+		return errors.Wrapf(err, "failed to create gzip reader for %s: ", f)
+	}
+	defer r.Close()
+
+	fwDumpData, err := ioutil.ReadAll(r)
+	if err != nil {
+		return errors.Wrap(err, "failed to read the fw_dump")
+	}
+
+	fwDumpBuffer := bytes.NewBuffer(fwDumpData)
+
+	// Validate the header
+	fwDumpHeader := DevcoreHeader{}
+	err = binary.Read(fwDumpBuffer, binary.LittleEndian, &fwDumpHeader)
+	if err != nil {
+		return errors.Wrap(err, "failed to read the header of the fw dump")
+	}
+
+	// Check the fw dump size.
+	if len(fwDumpData) != int(fwDumpHeader.FileSize) {
+		return errors.Errorf("unexpected file size: got %d, want %d", len(fwDumpData), fwDumpHeader.FileSize)
+	}
+
+	// Check the magic signature of the fw dump. The following are the expected signatures.
+	if (fwDumpHeader.Magic != yoyoMagic) && (fwDumpHeader.Magic != validMagic) {
+		return errors.Errorf("unexpected magic signature: got %x, want {%x, %x}", fwDumpHeader.Magic, validMagic, yoyoMagic)
+	}
+
+	return nil
+}
