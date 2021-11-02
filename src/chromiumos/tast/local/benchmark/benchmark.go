6b730fb2169981889ce9e3c2505c4bf612ef14ed
Ricky Liang <jcliang@chromium.org>
2021-11-02 14:43:37 +0800

    camera: Add HDRnet benchmark tast test
    The HDRnet benchmark can help us track the functionality and
    performance of the HDRnet pipeline on the supported platforms.
    
    BUG=b:203627041
    TEST=tast run <dut> camera.HdrnetProcessorBenchmark
    
    Cq-Depend: chromium:3233629
    Change-Id: I11fb1fd563dd1bd736542e0805f0b6d084ac567c

---
+++ src/chromiumos/tast/local/benchmark/benchmark.go
@@ -0,0 +1,185 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package benchmark provides utilities for running Google Benchmark binaries on device.
+package benchmark
+
+import (
+	"context"
+	"encoding/json"
+
+	"chromiumos/tast/common/testexec"
+)
+
+// Context is a struct representing the device context as probed by the benchmark binary.
+type Context struct {
+	Date              string `json:"date"`
+	HostName          string `json:"host_name"`
+	Executable        string `json:"executable"`
+	NumCPUs           int    `json:"num_cpus"`
+	MhzPerCPU         int    `json:"mhz_per_cpu"`
+	CPUScalingEnabled bool   `json:"cpu_scaling_enabled"`
+	Cached            []struct {
+		Type       string `json:"type"`
+		Level      int    `json:"level"`
+		Size       int    `json:"size"`
+		NumSharing int    `json:"num_sharing"`
+	} `json:"caches"`
+	LoadAvg          []float32 `json:"load_avg"`
+	LibraryBuildType string    `json:"library_build_type"`
+}
+
+// Output is a struct representing one benchmark execution output.
+type Output struct {
+	Name                   string  `json:"name"`
+	FamilyIndex            int     `json:"family_index"`
+	PerFamilyInstanceIndex int     `json:"per_family_instance_index"`
+	RunName                string  `json:"run_name"`
+	RunType                string  `json:"run_type"`
+	Repetitions            int     `json:"repetitions"`
+	RepetitionIndex        int     `json:"repetition_index"`
+	Threads                int     `json:"threads"`
+	Iterations             int     `json:"iterations"`
+	RealTime               float64 `json:"real_time"`
+	CPUTime                float64 `json:"cpu_time"`
+	TimeUnit               string  `json:"time_unit"`
+}
+
+// Result is a struct representing the complete result of one benchmark run.
+type Result struct {
+	Context    Context  `json:"context"`
+	Benchmarks []Output `json:"benchmarks"`
+}
+
+// Format defines the output formatting of the benchmark run.
+type Format int
+
+const (
+	// Console sets the output formatting to human-readable texts. This is the default used by Google Benchmark.
+	Console Format = iota
+
+	// JSON sets the output formatting to JSON.
+	JSON
+
+	// CSV sets the output format to CSV.
+	CSV
+)
+
+// Benchmark encapsulates all the context for running a benchmark binary.
+type Benchmark struct {
+	// executable is the path to the benchmark executable binary.
+	executable string
+
+	// filter is a string pattern as defined by the Google Benchmark for specifying the sub-benchmark(s) to run.
+	filter string
+
+	// resultFormat specifies the output formatting of the benchmark result written to stdout.
+	resultFormat Format
+
+	// outFile is an optional file path for storing another copy of the benchmark result in addition to the one written to stdout.
+	outFile string
+
+	// outResultFormat specifies the output formatting of the benchmark result writted to outFile.
+	outResultFormat Format
+
+	// extraArgs is a list of strings specifying the extra arguments that will be fed to the benchmark binary.
+	extraArgs []string
+}
+
+// options is a self-referencing closure for configuring Benchmark.
+type option func(b *Benchmark)
+
+// Filter sets the benchmark name filter pattern.
+func Filter(pattern string) option {
+	return func(b *Benchmark) { b.filter = pattern }
+}
+
+// ResultFormat sets the output formatting for stdout.
+func ResultFormat(format Format) option {
+	return func(b *Benchmark) { b.resultFormat = format }
+}
+
+// OutputFile sets the additional output file path.
+func OutputFile(file string) option {
+	return func(b *Benchmark) { b.outFile = file }
+}
+
+// OutputResultFormat sets the output formatting for the additional output file.
+func OutputResultFormat(format Format) option {
+	return func(b *Benchmark) { b.outResultFormat = format }
+}
+
+// ExtraArgs sets a list of arguments that will be passed to the benchmark binary.
+func ExtraArgs(args ...string) option {
+	return func(b *Benchmark) { b.extraArgs = args }
+}
+
+// New creates a Benchmark instance with the given options.
+func New(exec string, opts ...option) *Benchmark {
+	ret := &Benchmark{
+		executable: exec,
+	}
+	for _, opt := range opts {
+		opt(ret)
+	}
+	return ret
+}
+
+// Args returns an array of string for benchmark execution.
+func (b *Benchmark) Args() ([]string, error) {
+	args := []string{b.executable}
+
+	if b.filter != "" {
+		args = append(args, "--benchmark_filter="+b.filter)
+	}
+
+	switch b.resultFormat {
+	case Console:
+		args = append(args, "--benchmark_format=console")
+	case JSON:
+		args = append(args, "--benchmark_format=json")
+	case CSV:
+		args = append(args, "--benchmark_format=csv")
+	}
+
+	if b.outFile != "" {
+		args = append(args, "--benchmark_out="+b.outFile)
+		switch b.outResultFormat {
+		case Console:
+			args = append(args, "--benchmark_out_format=console")
+		case JSON:
+			args = append(args, "--benchmark_out_format=json")
+		case CSV:
+			args = append(args, "--benchmark_out_format=csv")
+		}
+	}
+
+	if len(b.extraArgs) > 0 {
+		args = append(args, b.extraArgs...)
+	}
+	return args, nil
+}
+
+// Run executes the benchmark and returns the benchmark result in a byte array.
+func (b *Benchmark) Run(ctx context.Context) ([]byte, error) {
+	args, err := b.Args()
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := testexec.CommandContext(ctx, args[0], args[1:]...)
+	output, err := cmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+// UnmarshalJSONBytes unmarshals the given byte array as JSON payload and returns the BenchmarkResult instance.
+func UnmarshalJSONBytes(bytes []byte) Result {
+	var ret Result
+	json.Unmarshal(bytes, &ret)
+	return ret
+}
