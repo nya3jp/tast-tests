1434ab2175d11356b9180e416370c318d08e1707
Hirokazu Honda <hiroh@chromium.org>
2021-11-03 04:00:26 +0900

    video: Verify decoded frames by md5 checksums in video.WebCodecsDecode
    This CL add frame content validation by comparing md5 checksums in
    video.WebCodecsDecode. However, if the format of decoded frames are
    RGB format, they are likely converted to the RGB format using GPU
    after decoding. So their contents are dependent on GPU. We skip the
    md5sum validation in the case.
    
    BUG=b:173164490
    TEST=video.WebCodecsDecode.*
    
    Change-Id: I693de5fb1758c5101f7b0a7e53c116844dfa8e9c

--- src/chromiumos/tast/local/bundles/cros/video/webcodecs/decode.go
+++ src/chromiumos/tast/local/bundles/cros/video/webcodecs/decode.go
@@ -5,9 +5,13 @@
 package webcodecs
 
 import (
+	"bufio"
 	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"time"
 
 	"chromiumos/tast/ctxutil"
@@ -15,6 +19,7 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/media/devtools"
 	"chromiumos/tast/local/media/logging"
+	"chromiumos/tast/testing"
 )
 
 // TestDecodeArgs is the arguments used in RunDecodeTest.
@@ -38,8 +43,76 @@ func DecodeDataFiles() []string {
 	}
 }
 
-// RunDecodeTest tests encoding in WebCodecs API. It verifies a specified encoder is used.
-func RunDecodeTest(ctx context.Context, cr *chrome.Chrome, fileSystem http.FileSystem, testArgs TestDecodeArgs, outDir string) error {
+type planeLayout struct {
+	Offset uint `json:"offset"`
+	Stride uint `json:"stride"`
+}
+
+type decodedFrameInfo struct {
+	Data    []byte        `json:"frameBuffer"`
+	Format  string        `json:"format"`
+	Layouts []planeLayout `json:"layouts"`
+}
+
+// readExpectedMD5s reads ".md5" file, where md5 values are listed, and returns
+// them. Fails if the number of md5 values is not numMD5s.
+func readExpectedMD5s(md5FilePath string, numMD5s int) ([]string, error) {
+	f, err := os.Open(md5FilePath)
+	if err != nil {
+		return []string{}, err
+	}
+	defer f.Close()
+
+	var md5s []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		md5s = append(md5s, scanner.Text())
+	}
+
+	if len(md5s) != numMD5s {
+		return md5s, errors.Errorf("the number of read md5 values mismatch, got=%d, expected=%d", len(md5s), numMD5s)
+	}
+	return md5s, nil
+}
+
+// computeMD5 returns the md5sum of a visible rectangle of the YUV frame. The
+// visible rectangle of the frame must be (0, 0, vw, vh).
+func computeMD5(frameInfo decodedFrameInfo, vw, vh int) (string, error) {
+	if frameInfo.Format != "I420" {
+		return "unsupported", errors.Errorf("unsupported pixel format %s", frameInfo.Format)
+	}
+
+	hasher := md5.New()
+	for i, layout := range frameInfo.Layouts {
+		offset := layout.Offset
+		stride := layout.Stride
+		cvw, cvh := vw, vh
+		if i > 0 {
+			cvw /= 2
+			cvh /= 2
+		}
+		for h := 0; h < cvh; h++ {
+			hasher.Write(frameInfo.Data[offset+uint(h)*stride : offset+uint(h)*stride+uint(cvw)])
+		}
+	}
+
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
+
+// isRGBFormat returns true if and only if the format is one of RGB formats defined in VideoPixelFormat.
+// https://www.w3.org/TR/webcodecs/#pixel-format
+func isRGBFormat(format string) bool {
+	switch format {
+	case "RGBA", "RGBX", "BGRA", "BGRX":
+		return true
+	default:
+		return false
+	}
+}
+
+// RunDecodeTest tests encoding in WebCodecs API. It verifies a specified encoder is used and
+// the decoded frames with md5 checksums in md5FilePath.
+func RunDecodeTest(ctx context.Context, cr *chrome.Chrome, fileSystem http.FileSystem, testArgs TestDecodeArgs, md5FilePath, outDir string) error {
 	vl, err := logging.NewVideoLogger()
 	if err != nil {
 		return errors.Wrap(err, "failed to set values for verbose logging")
@@ -86,6 +159,46 @@ func RunDecodeTest(ctx context.Context, cr *chrome.Chrome, fileSystem http.FileS
 		return errors.Errorf("video is decoded by a hardware decoder, %s", name)
 	}
 
-	// TODO(b/173164490): Add md5 check supports
-	return nil
+	var gotMD5s []string
+	for i := 0; i < numFrames; i++ {
+		var frameInfo decodedFrameInfo
+		// We can get a raw frame because the size (320 * 240 * 3 / 2 + base64 conversion) is
+		// less than the tast websocket limitation, 1MB declared in session.go in package cdputil.
+		if err := conn.Call(ctx, &frameInfo, "GetFrame", i); err != nil {
+			return outputJSLogAndError(cleanupCtx, conn, errors.Wrap(err, "failed getting frame"))
+		}
+
+		if isRGBFormat(frameInfo.Format) {
+			// Skip md5 validation.
+			return nil
+		}
+		md5, err := computeMD5(frameInfo, width, height)
+		if err != nil {
+			return errors.Wrap(err, "failed computing md5")
+		}
+		gotMD5s = append(gotMD5s, md5)
+	}
+
+	if len(gotMD5s) != numFrames {
+		return errors.Errorf("the number of md5 values mismatch: got=%d, expected=%d", len(gotMD5s), numFrames)
+	}
+
+	expectedMD5s, err := readExpectedMD5s(md5FilePath, numFrames)
+	if err != nil {
+		return errors.Wrapf(err, "failed reading expected MD5 values, %s", md5FilePath)
+	}
+
+	mismatchMD5cnt := 0
+	for i := 0; i < numFrames; i++ {
+		if gotMD5s[i] != expectedMD5s[i] {
+			mismatchMD5cnt++
+			testing.ContextLogf(ctx, "%d: %s (got), %s (expected)", i, gotMD5s[i], expectedMD5s[i])
+		}
+	}
+
+	if mismatchMD5cnt > 0 {
+		return errors.Errorf("%d frames are invalid", mismatchMD5cnt)
+	}
+
+	return outputJSLogAndError(cleanupCtx, conn, nil)
 }
