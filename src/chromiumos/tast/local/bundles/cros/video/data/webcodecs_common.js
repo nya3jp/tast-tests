66d7b6a51684a9e89b301f6a88d3fe1634d0cfc5
Hirokazu Honda <hiroh@chromium.org>
2021-11-03 06:34:46 +0900

    video: Don't hold decoded frames in JS
    Originally we hold VideoFrame instance output by WebCodecs video
    decoder. Some video decoder implementation needs to release the
    instance to proceed decoding because it recycles the limited
    VideoFrame resources for decoded outputs.
    
    This CL copies VideoFrame content and release the VideoFrame
    instance output by VideoDecoder immediately after copying.
    
    BUG=b:173164490
    TEST=video.WebCodecsDecode.*
    
    Change-Id: Id107ad3b91604e66fa0d78c573db07e2b7535b04

--- src/chromiumos/tast/local/bundles/cros/video/data/webcodecs_common.js
+++ src/chromiumos/tast/local/bundles/cros/video/data/webcodecs_common.js
@@ -141,12 +141,35 @@ async function CreateEncoder(
   return encoder;
 }
 
+async function createNewFrame(frame) {
+  let buffer = new Uint8Array(frame.allocationSize());
+  let layout = await frame.copyTo(buffer);
+
+  let bufferInit = {
+    format: frame.format,
+    codedWidth: frame.codedWidth,
+    codedHeight: frame.codedHeight,
+    timestamp: frame.timestamp,
+    duration: frame.duration,
+    layout : layout,
+    visibleRect: frame.visibleRect,
+    displayWidth: frame.displayWidth,
+    displayHeight: frame.displayHeight,
+    colorSpace: frame.colorSpace
+  }
+
+  return new VideoFrame(buffer, bufferInit)
+}
+
 async function decodeVideoInURL(videoURL, numFrames) {
   let demuxer = new MP4Demuxer(videoURL);
   let videoFrames = [];
   let decoder = new VideoDecoder({
     output(frame) {
-      videoFrames.push(frame);
+      createNewFrame(frame).then(newFrame => {
+        videoFrames.push(newFrame);
+        frame.close();
+      });
     },
     error(e){
       TEST.log("decoder error: " + e);
