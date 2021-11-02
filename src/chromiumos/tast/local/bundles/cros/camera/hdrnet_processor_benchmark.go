91b89b516292e6b16fcc096727a6871b75e6e4fc
Ricky Liang <jcliang@chromium.org>
2021-11-02 14:43:37 +0800

    camera: Add HDRnet benchmark
    The HDRnet benchmark can help us track the functionality and
    performance of the HDRnet pipeline on the supported platforms.
    
    BUG=b:203627041
    TEST=tast run <dut> camera.HdrnetProcessorBenchmark
    
    Cq-Depend: chromium:3233629
    Change-Id: I11fb1fd563dd1bd736542e0805f0b6d084ac567c

---
+++ src/chromiumos/tast/local/bundles/cros/camera/hdrnet_processor_benchmark.go
@@ -0,0 +1,71 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/local/benchmark"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     HdrnetProcessorBenchmark,
+		Desc:     "Runs the HDRnet processor benchmark and reports the measurements",
+		Contacts: []string{"jcliang@chromium.org", "chromeos-camera-eng@google.com"},
+		// HDRnet is currently only available on Intel TGL and ADL platforms.
+		HardwareDeps: hwdep.D(hwdep.Platform("volteer", "brya")),
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		Timeout:      5 * time.Minute,
+	})
+}
+
+// HdrnetProcessorBenchmark runs the HDRnet processor benchmark.
+func HdrnetProcessorBenchmark(ctx context.Context, s *testing.State) {
+	const exec = "hdrnet_processor_benchmark"
+	const consoleOutput = "console_output.txt"
+	result, err := benchmark.New(
+		exec,
+		// Also produce a human-readable output file for ease of debugging.
+		benchmark.OutputFile(filepath.Join(s.OutDir(), consoleOutput)),
+		benchmark.OutputResultFormat(benchmark.Console),
+	).Run(ctx)
+
+	if err != nil {
+		s.Fatal("Failed to run HDRnet processor benchmark: ", err)
+	}
+
+	perfValues := perf.NewValues()
+	for _, r := range result.Benchmarks {
+		// Converts raw benchmark name like: "BM_HdrNetProcessorFullProcessing/1280/720" to more
+		// readable perf metric name: "HdrNetProcessorFullProcessing_1280x720_{CpuTime,RealTime}".
+		args := strings.Split(r.RunName, "/")
+		nameSlice := []string{strings.TrimPrefix(args[0], "BM_"), strings.Join(args[1:], "x")}
+		cpuTime := perf.Metric{
+			Name:      strings.Join(append(nameSlice, "CpuTime"), "_"),
+			Unit:      r.TimeUnit,
+			Direction: perf.SmallerIsBetter,
+			Multiple:  false,
+		}
+		perfValues.Set(cpuTime, r.CPUTime)
+		realTime := perf.Metric{
+			Name:      strings.Join(append(nameSlice, "RealTime"), "_"),
+			Unit:      r.TimeUnit,
+			Direction: perf.SmallerIsBetter,
+			Multiple:  false,
+		}
+		perfValues.Set(realTime, r.RealTime)
+	}
+
+	if err := perfValues.Save(s.OutDir()); err != nil {
+		s.Fatal("Failed to save perf metrics: ", err)
+	}
+}
