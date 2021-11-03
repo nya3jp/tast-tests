1320e26edde1ef47d56d1266aa2637b21492ed2d
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-11-03 12:10:26 -0700

    arcappcompat: Add TestAccountPool for arcappcompat tests
    It is observed that arcappcompat tests are failing in install
     with Authentication error due to Playstore rate limit issue.
    Currently arcappcompat tests are using a single test account
    for most of the apps. Adding TestAccountPool for arcappcompat
    tests will fix the play store rate limit issue as the test
    could use any test account from the testaccount pool.Also
    in "pre" file created a struct GaiaLoginPoolVars for handling
    testaccountpool.
    
    BUG=b:197558039
    TEST=tast run <eve> arcappcompat.ABCKids.clamshell_mode
    tast run <eve> arcappcompat.ABCKids.tablet_mode
    tast run <helios> arcappcompat.ABCKids.vm_clamshell_mode
    tast run <helios> arcappcompat.ABCKids.vm_tablet_mode
    
    Cq-Depend: chrome-internal:4234621
    Change-Id: Ia16ba25b65c60f4e207116980117ba277dfe6c43

--- src/chromiumos/tast/local/arc/pre.go
+++ src/chromiumos/tast/local/arc/pre.go
@@ -60,15 +60,16 @@ var bootedPre = &preImpl{
 }
 
 // NewPrecondition creates a new arc precondition for tests that need different args.
-func NewPrecondition(name string, gaia *GaiaVars, oDirect bool, extraArgs ...string) testing.Precondition {
+func NewPrecondition(name string, gaia *GaiaVars, gaiapool *GaiaLoginPoolVars, oDirect bool, extraArgs ...string) testing.Precondition {
 	timeout := resetTimeout + chrome.LoginTimeout + BootTimeout
-	if gaia != nil {
+	if gaia != nil || gaiapool != nil {
 		timeout = resetTimeout + chrome.GAIALoginTimeout + BootTimeout + optin.OptinTimeout
 	}
 	pre := &preImpl{
 		name:      name,
 		timeout:   timeout,
 		gaia:      gaia,
+		gaiapool:  gaiapool,
 		extraArgs: extraArgs,
 		oDirect:   oDirect,
 	}
@@ -81,14 +82,20 @@ type GaiaVars struct {
 	PassVar string // the secret variable for the GAIA password
 }
 
+// GaiaLoginPoolVars holds the secret variable for a GAIA POOL.
+type GaiaLoginPoolVars struct {
+	PoolVar string // the secret variable for the GAIA pool
+}
+
 // preImpl implements testing.Precondition.
 type preImpl struct {
 	name    string        // testing.Precondition.String
 	timeout time.Duration // testing.Precondition.Timeout
 
-	extraArgs []string  // passed to Chrome on initialization
-	gaia      *GaiaVars // a struct containing GAIA secret variables
-	oDirect   bool      // whether crosvm should use O_DIRECT.
+	extraArgs []string           // passed to Chrome on initialization
+	gaia      *GaiaVars          // a struct containing GAIA secret variables
+	gaiapool  *GaiaLoginPoolVars // a struct containing GAIA LOGIN POOL variable
+	oDirect   bool               // whether crosvm should use O_DIRECT.
 	cr        *chrome.Chrome
 	arc       *ARC
 
@@ -159,9 +166,13 @@ func (p *preImpl) Prepare(ctx context.Context, s *testing.PreState) interface{} 
 		}
 
 		if p.gaia != nil {
+			// Login into the device, using GAIA login.
 			username := s.RequiredVar(p.gaia.UserVar)
 			password := s.RequiredVar(p.gaia.PassVar)
 			p.cr, err = chrome.New(ctx, chrome.GAIALogin(chrome.Creds{User: username, Pass: password}), chrome.ARCSupported(), chrome.ExtraArgs(extraArgs...))
+		} else if p.gaiapool != nil {
+			// Login into the device, using GAIA login pool.
+			p.cr, err = chrome.New(ctx, chrome.GAIALoginPool(s.RequiredVar(p.gaiapool.PoolVar)), chrome.ARCSupported(), chrome.ExtraArgs(extraArgs...))
 		} else {
 			p.cr, err = chrome.New(ctx, chrome.ARCEnabled(), chrome.ExtraArgs(extraArgs...))
 		}
@@ -170,8 +181,8 @@ func (p *preImpl) Prepare(ctx context.Context, s *testing.PreState) interface{} 
 		}
 	}()
 
-	// Opt-in if performing a GAIA login.
-	if p.gaia != nil {
+	// Opt-in to playstore using GAIA login or GAIA login pool.
+	if p.gaia != nil || p.gaiapool != nil {
 		func() {
 			ctx, cancel := context.WithTimeout(ctx, optin.OptinTimeout)
 			defer cancel()
