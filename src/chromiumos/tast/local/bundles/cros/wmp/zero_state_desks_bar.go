824a4ebbfd88e1d55d913550f16d419df476103a
minch <minch@chromium.org>
2021-11-03 15:21:45 -0700

    tast-tests: Zero state desks bar
    Tests the functionality of zero state desks bar in overview mode.
    - Zero state desks bar should be shown when there is only 1 desk.
    - Tab to the default desk button inside zero desks bar and enter should
      expand desks bar without creating new desk. And focus on the desk
      name view.
    - Tab to the new desk button inside the expanded desks bar and enter
      should create a new desk and focus on the desk name view.
    - The new desk button inside the expanded desks bar should have the
      same size as the desk mini view.
    - When there is only 1 desk, it should switch back to zero state desks
      bar.
    - Click the new desk button inside zero state desks bar should expand
      desks bar and create a new desk. And focus on the desk name view.
    
    BUG=b:190654803
    TEST=tast -verbose run * wmp.ZeroStateDesksBar
    
    Change-Id: I1d99e8d06ed8dd02c0bd2a56fb4ee6ff9e229974

---
+++ src/chromiumos/tast/local/bundles/cros/wmp/zero_state_desks_bar.go
@@ -0,0 +1,151 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wmp
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/pointer"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ZeroStateDesksBar,
+		Desc: "Checks that zero state desks bar in overview works correctly",
+		Contacts: []string{
+			"minch@chromium.org",
+			"chromeos-wmp@google.com",
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedIn",
+	})
+}
+
+func ZeroStateDesksBar(ctx context.Context, s *testing.State) {
+	// Reserve five seconds for various cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	cr := s.FixtValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
+	if err != nil {
+		s.Fatal("Failed to ensure clamshell mode: ", err)
+	}
+	defer cleanup(cleanupCtx)
+
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	ac := uiauto.New(tconn)
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to create a keyboard: ", err)
+	}
+	pc := pointer.NewMouse(tconn)
+	defer pc.Close()
+
+	// Enters overview mode.
+	if err := ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+		s.Fatal("Failed to set overview mode: ", err)
+	}
+
+	// Zero state desks bar is shown when there is only one desk. Tab to
+	// default desk button inside and press "Enter" should expand the desks
+	// bar but no new desk should be created.
+	desk1DeskNameView := nodewith.ClassName("DeskNameView").Name("Desk 1")
+	if err := uiauto.Combine(
+		"press the default desk button to expand desks bar",
+		kb.AccelAction("Tab"),
+		kb.AccelAction("Enter"),
+		// The desk name view of the default desk mini view should be focused.
+		ac.WaitUntilExists(desk1DeskNameView.Focused()),
+	)(ctx); err != nil {
+		s.Fatal("Failed to switch to expanded desks bar: ", err)
+	}
+	// Verifies that there is only 1 desk.
+	if _, err := ash.FindDeskMiniViews(ctx, ac, 1); err != nil {
+		s.Fatal("Failed to find desks: ", err)
+	}
+
+	// Tab to the new desk button inside expanded desks bar and press "Enter"
+	// should create a new desk.
+	desk2DeskNameView := nodewith.ClassName("DeskNameView").Name("Desk 2")
+	newDeskName := "new desk"
+	if err := uiauto.Combine(
+		"create a new desk through new desk button in the expanded desks bar",
+		kb.AccelAction("Tab"),
+		kb.AccelAction("Enter"),
+		ac.WaitUntilExists(desk2DeskNameView.Focused()),
+		kb.TypeAction(newDeskName),
+		kb.AccelAction("Enter"),
+	)(ctx); err != nil {
+		s.Fatal("Failed to create a new desk through expanded new desk button: ", err)
+	}
+	// Verifies that there 2 desks.
+	deskMiniViewsInfo, err := ash.FindDeskMiniViews(ctx, ac, 2)
+	if err != nil {
+		s.Fatal("Failed to find desks: ", err)
+	}
+
+	firstDeskMiniViewLoc, secondDeskMiniViewLoc := deskMiniViewsInfo[0].Location, deskMiniViewsInfo[1].Location
+	// Verifies that the new desk button inside the expanded desks bar has the same size as the desk preview.
+	newDeskButtonInExpandedDesksBarLoc, err := ac.Location(ctx, nodewith.ClassName("ExpandedDesksBarButton"))
+	if err != nil {
+		s.Fatal("Failed to get the location of the new desk button inside expanded desks bar: ", err)
+	}
+	if (*newDeskButtonInExpandedDesksBarLoc).Width != secondDeskMiniViewLoc.Width {
+		s.Fatalf("Expected width of the expanded new desk button is %, got %d instead", secondDeskMiniViewLoc.Width, (*newDeskButtonInExpandedDesksBarLoc).Width)
+	}
+	if (*newDeskButtonInExpandedDesksBarLoc).Height != secondDeskMiniViewLoc.Height {
+		s.Fatalf("Expected height of the expanded new desk button is %, got %d instead", secondDeskMiniViewLoc.Height, (*newDeskButtonInExpandedDesksBarLoc).Height)
+	}
+
+	// Drag "new desk" to be the first desk and delete it.
+	if err := pc.Drag(
+		secondDeskMiniViewLoc.CenterPoint(),
+		pc.DragTo(firstDeskMiniViewLoc.CenterPoint(), time.Second))(ctx); err != nil {
+		s.Fatal("Failed to drag and drop desks: ", err)
+	}
+	closeDeskButton := nodewith.ClassName("CloseDeskButton")
+	if err := ac.LeftClick(closeDeskButton)(ctx); err != nil {
+		s.Fatal("Failed to delete new desk: ", err)
+	}
+
+	// It should switch back to zero state desks bar when there is only 1 desk.
+	// Click the new desk button inside the zero state desks bar should create
+	// a new desk and expand the desks bar.
+	addDeskButton := nodewith.ClassName("ZeroStateIconButton")
+	newDeskNameView := nodewith.ClassName("DeskNameView").Name("Desk 2")
+	if err := uiauto.Combine(
+		"create a new desk from zero state desks bar",
+		ac.LeftClick(addDeskButton),
+		// The focus on the new desk should be on the desk name field.
+		ac.WaitUntilExists(newDeskNameView.Focused()),
+		kb.TypeAction(newDeskName),
+		kb.AccelAction("Enter"),
+	)(ctx); err != nil {
+		s.Fatal("Failed to create a new desk: ", err)
+	}
+	if _, err := ash.FindDeskMiniViews(ctx, ac, 2); err != nil {
+		s.Fatal("Failed to find desks: ", err)
+	}
+}
--- src/chromiumos/tast/local/chrome/ash/desks.go
+++ src/chromiumos/tast/local/chrome/ash/desks.go
@@ -9,6 +9,8 @@ import (
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
 )
 
 // CreateNewDesk requests Ash to create a new Virtual Desk which would fail if
@@ -66,4 +68,18 @@ func ActivateAdjacentDesksToTargetIndex(ctx context.Context, tconn *chrome.TestC
 		return errors.Errorf("failed to activate desk at index %v", index)
 	}
 	return nil
+}
+
+// FindDeskMiniViews returns a list of DeskMiniView nodes and verifies the number of nodes.
+// TODO(crbug/1251558): use autotest api to get the number of desks instead.
+func FindDeskMiniViews(ctx context.Context, ac *uiauto.Context, count int) ([]uiauto.NodeInfo, error) {
+	deskMiniViews := nodewith.ClassName("DeskMiniView")
+	deskMiniViewsInfo, err := ac.NodesInfo(ctx, deskMiniViews)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find all desk mini views")
+	}
+	if len(deskMiniViewsInfo) != count {
+		return nil, errors.Errorf("expected %v desks, but got %v instead", count, len(deskMiniViewsInfo))
+	}
+	return deskMiniViewsInfo, nil
 }
