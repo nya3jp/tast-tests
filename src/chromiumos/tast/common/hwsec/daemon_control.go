4241063d49b80b37c2084eedda0dbf8f3048ae1f
Ching-Kang Yen <chingkang@google.com>
2021-11-03 09:37:34 +0800

    tast-tests: add tpm2-simulator to daemon_control
    BUG=b:195775720
    TEST=tast run <ip> hwsec.DaemonsRestartStress.vm
    
    Change-Id: If9ebdea3e01759f19440475ebb393f87b6f17cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3258464
    Reviewed-by: Leo Lai <cylai@google.com>
    Reviewed-by: Yi Chou <yich@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Ching-Kang Yen <chingkang@chromium.org>
    Commit-Queue: Ching-Kang Yen <chingkang@chromium.org>

--- src/chromiumos/tast/common/hwsec/daemon_control.go
+++ src/chromiumos/tast/common/hwsec/daemon_control.go
@@ -189,6 +189,13 @@ var UIDaemon = &DaemonInfo{
 	HasDBus:    false,
 }
 
+// TPM2SimulatorDaemon represents the DaemonsInfo for tpm2 simulator.
+var TPM2SimulatorDaemon = &DaemonInfo{
+	Name:       "tpm2-simulator",
+	DaemonName: "tpm2-simulator",
+	HasDBus:    false,
+}
+
 // LowLevelTPMDaemons represents the low level TPM daemons.
 var LowLevelTPMDaemons = []*DaemonInfo{
 	TcsdDaemon,
