f6a0a05b05993785a25903e5c928f33efddd4b52
Andres Calderon Jaramillo <andrescj@google.com>
2021-11-03 00:07:46 +0000

    Revert "policy: promote DisableScreenshotsHotkey"
    This reverts commit 87e86a5c7097093753b831a707ceb192a1b3e930.
    
    Reason for revert: Fails in the CQ (b/204928006)
    
    Original change's description:
    > policy: promote DisableScreenshotsHotkey
    >
    > policy.DisableScreenshotsHotkey is stable enough to be promoted to the
    > CQ:
    > https://cros-health-prod.googleplex.com/test/policy.DisableScreenshotsHotkey
    >
    > BUG=none
    > TEST=none
    >
    > Change-Id: I6544513b513d4c284fd7bb3b32eb725ec88cfd7d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3245500
    > Tested-by: Oleh Lamzin <lamzin@google.com>
    > Auto-Submit: Oleh Lamzin <lamzin@google.com>
    > Reviewed-by: Alexander Hartl <alexanderhartl@google.com>
    > Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    > Commit-Queue: Oleh Lamzin <lamzin@google.com>
    
    Bug: none
    Change-Id: I25cf9c555a090aa1eb4dc43a0bc721076854e1bf

--- src/chromiumos/tast/local/bundles/cros/policy/disable_screenshots_hotkey.go
+++ src/chromiumos/tast/local/bundles/cros/policy/disable_screenshots_hotkey.go
@@ -30,7 +30,7 @@ func init() {
 			"chromeos-commercial-remote-management@google.com",
 		},
 		SoftwareDeps: []string{"chrome"},
-		Attr:         []string{"group:mainline"},
+		Attr:         []string{"group:mainline", "informational"},
 		Fixture:      fixture.ChromePolicyLoggedIn,
 	})
 }
