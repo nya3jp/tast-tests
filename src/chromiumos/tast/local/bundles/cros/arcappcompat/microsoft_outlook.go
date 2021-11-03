35ad3bbf6ea826c5ddf6319d2dbcba36b38ba3c4
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-11-03 12:10:26 -0700

    arcappcompat: Add TestAccountPool for arcappcompat tests
    It is observed that arcappcompat tests are failing in install
     with Authentication error due to Playstore rate limit issue.
    Currently arcappcompat tests are using a single test account
    for most of the apps. Adding TestAccountPool for arcappcompat
    tests will fix the play store rate limit issue as the test
    could use any test account from the testaccount pool.
    
    BUG=b:197558039
    TEST=tast run <eve> arcappcompat.ABCKids.clamshell_mode
    tast run <eve> arcappcompat.ABCKids.tablet_mode
    tast run <helios> arcappcompat.ABCKids.vm_clamshell_mode
    tast run <helios> arcappcompat.ABCKids.vm_tablet_mode
    
    Cq-Depend: chrome-internal:4234621
    Cq-Depend: chromium:3300401
    Change-Id: Ia16ba25b65c60f4e207116980117ba277dfe6c43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3260117
    Commit-Queue: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Tested-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Reviewed-by: David Welling <davidwelling@google.com>
    Reviewed-by: Renuga Nanjappan <rnanjappan@chromium.org>
    (cherry picked from commit 2e9db24596f08ad90538a2f3b5aaea7adf46b196)

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/microsoft_outlook.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/microsoft_outlook.go
@@ -50,7 +50,7 @@ func init() {
 			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
 			// Skip on tablet only models.
 			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
-			Pre:               pre.AppCompatBooted,
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
 		}, {
 			Name: "tablet_mode",
 			Val: testutil.TestParams{
@@ -61,7 +61,7 @@ func init() {
 			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
 			// Skip on clamshell only models.
 			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
-			Pre:               pre.AppCompatBootedInTabletMode,
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
 		}, {
 			Name: "vm_clamshell_mode",
 			Val: testutil.TestParams{
@@ -72,7 +72,7 @@ func init() {
 			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
 			// Skip on tablet only models.
 			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
-			Pre:               pre.AppCompatBooted,
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
 		}, {
 			Name: "vm_tablet_mode",
 			Val: testutil.TestParams{
@@ -83,11 +83,11 @@ func init() {
 			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
 			// Skip on clamshell only models.
 			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
-			Pre:               pre.AppCompatBootedInTabletMode,
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
 		}},
 		Timeout: 10 * time.Minute,
-		VarDeps: []string{"arcappcompat.username", "arcappcompat.password",
-			"arcappcompat.MicrosoftOutlook.emailid", "arcappcompat.MicrosoftOutlook.password"},
+		Vars:    []string{"arcappcompat.gaiaPoolDefault"},
+		VarDeps: []string{"arcappcompat.MicrosoftOutlook.emailid", "arcappcompat.MicrosoftOutlook.password"},
 	})
 }
 
