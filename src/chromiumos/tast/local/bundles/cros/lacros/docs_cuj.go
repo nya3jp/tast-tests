5ef786fe060aa58af67322c74bab6dd2228edeea
Tiago Vignatti <tvignatti@igalia.com>
2021-11-03 15:47:08 -0300

    lacros: Add Google Docs CUJ
    This adds Google Docs CUJ, specifically a large document load page
    benchmark to Lacros.
    
    This is preparation of matching the thread priorities settings in lacros
    the same way as being used for ash-chrome.
    
    BUG=chromium:1262133
    TEST=tast run $device lacros.DocsCUJ
    
    Change-Id: Iafea2a1b009de2a0f1109cbbd8d4879cb22a0017

---
+++ src/chromiumos/tast/local/bundles/cros/lacros/docs_cuj.go
@@ -0,0 +1,139 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package lacros
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/lacros"
+	"chromiumos/tast/local/chrome/lacros/launcher"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DocsCUJ,
+		Desc:         "Runs Google Docs CUJ against both ash-chrome and lacros-chrome",
+		Contacts:     []string{"hidehiko@chromium.org", "tvignatti@igalia.com", "lacros-team@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", "lacros"},
+		Fixture:      "lacros",
+		Timeout:      6 * time.Minute,
+	})
+}
+
+const (
+	// Google Docs with 20+ pages of random text with 50 comments. The URL points to a comment and will skip
+	// down to the comment once the page is fully loaded.
+	docsURLToComment = "https://docs.google.com/document/d/1MW7lAk9RZ-6zxpObNwF0r80nu-N1sXo5f7ORG4usrJQ/edit?disco=AAAAP6EbSF8"
+)
+
+func DocsCUJ(ctx context.Context, s *testing.State) {
+	f := s.FixtValue().(launcher.FixtValue)
+
+	cleanup, err := lacros.SetupPerfTest(ctx, f.TestAPIConn(), "lacros.DocsCUJ")
+	if err != nil {
+		s.Fatal("Failed to set up lacros perf test: ", err)
+	}
+	defer cleanup(ctx)
+
+	pv := perf.NewValues()
+
+	// Run against ash-chrome.
+	if loadTime, visibleLoadTime, err := runDocsPageLoad(ctx, docsURLToComment, func(ctx context.Context, url string) (*chrome.Conn, lacros.CleanupCallback, error) {
+		return lacros.SetupCrosTestWithPage(ctx, f, url)
+	}); err != nil {
+		s.Error("Failed to run ash-chrome benchmark: ", err)
+	} else {
+		pv.Set(perf.Metric{
+			Name:      "docs.load.ash",
+			Unit:      "seconds",
+			Direction: perf.SmallerIsBetter,
+		}, loadTime.Seconds())
+
+		pv.Set(perf.Metric{
+			Name:      "docs.load_and_visible.ash",
+			Unit:      "seconds",
+			Direction: perf.SmallerIsBetter,
+		}, visibleLoadTime.Seconds())
+	}
+
+	// Run against lacros-chrome.
+	if loadTime, visibleLoadTime, err := runDocsPageLoad(ctx, docsURLToComment, func(ctx context.Context, url string) (*chrome.Conn, lacros.CleanupCallback, error) {
+		conn, _, _, cleanup, err := lacros.SetupLacrosTestWithPage(ctx, f, url)
+		return conn, cleanup, err
+	}); err != nil {
+		s.Error("Failed to run lacros-chrome benchmark: ", err)
+	} else {
+		pv.Set(perf.Metric{
+			Name:      "docs.load.lacros",
+			Unit:      "seconds",
+			Direction: perf.SmallerIsBetter,
+		}, loadTime.Seconds())
+
+		pv.Set(perf.Metric{
+			Name:      "docs.load_and_visible.lacros",
+			Unit:      "seconds",
+			Direction: perf.SmallerIsBetter,
+		}, visibleLoadTime.Seconds())
+	}
+
+	if err := pv.Save(s.OutDir()); err != nil {
+		s.Error("Cannot save perf data: ", err)
+	}
+}
+
+// runDocsPageLoad navigates to the Google Docs URL page and benchmark the time to load it.
+// It returns the page loading time (loadTime) and the user-visible milestone of loading the page
+// (visibleLoadTime), given the latter really captures the real user experience speacially when
+// loading large pages.
+func runDocsPageLoad(
+	ctx context.Context,
+	url string,
+	setup func(ctx context.Context, url string) (*chrome.Conn, lacros.CleanupCallback, error)) (time.Duration, time.Duration, error) {
+	conn, cleanup, err := setup(ctx, chrome.BlankURL)
+	if err != nil {
+		return 0.0, 0.0, errors.Wrap(err, "failed to open a new tab")
+	}
+	defer cleanup(ctx)
+
+	// TODO(https://crbug.com/1263337): atm ash-chrome is opening maximized but lacros
+	// isn't. It's rather important to get this bug fixed to get consistent tests.
+	// w, err := lacros.FindFirstNonBlankWindow(ctx, f.TestAPIConn())
+	// if err != nil {
+	// 	return 0.0, 0.0, err
+	// }
+	//
+	// if err := ash.SetWindowStateAndWait(ctx, f.TestAPIConn(), w.ID, ash.WindowStateMaximized); err != nil {
+	// 	return 0.0, errors.Wrap(err, "Failed to maximize window")
+	// }
+
+	start := time.Now()
+
+	// Navigate the blankpage to the document file to be loaded.
+	// This blocks until the loading is completed and is a important metric already.
+	if err := conn.Navigate(ctx, url); err != nil {
+		return 0.0, 0.0, errors.Wrap(err, "failed to navigate a blankpage to the URL")
+	}
+
+	// TODO(tvignatti): Save load time perf data as well.
+	loadTime := time.Since(start)
+
+	// Check whether comment link is loaded and visible.
+	// WaitForExpr has to be used since the comment link is not updated immediately.
+	const expr = `document.querySelector("#docos-stream-view > div.docos-docoview-tesla-conflict.docos-docoview-resolve-button-visible.docos-anchoreddocoview.docos-docoview-active.docos-docoview-active-experiment")
+	.innerText`
+	if err := conn.WaitForExpr(ctx, expr); err != nil {
+		return 0.0, 0.0, errors.Wrap(err, "failed to wait the comment link to be loaded and visible")
+	}
+
+	visibleLoadTime := time.Since(start)
+
+	return time.Duration(loadTime), time.Duration(visibleLoadTime), nil
+}
