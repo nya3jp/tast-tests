a91974486baa9e4ecf068fbef7c407c2e063c362
Angus L. M. McLean IV <angusmclean@chromium.org>
2021-11-03 09:47:33 -0700

    Add holding space test for drag and drop to pin
    Adds a test for pinning to holding space using drag and drop from
    the files app.
    
    BUG=chromium:1187731
    TEST=tast run <DUT> holdingspace.DragAndDropPin
    
    Change-Id: I3b1ddc3874251beb14eecf01005be860631aa1d4

--- src/chromiumos/tast/local/chrome/uiauto/holdingspace/holdingspace.go
+++ src/chromiumos/tast/local/chrome/uiauto/holdingspace/holdingspace.go
@@ -5,6 +5,10 @@
 package holdingspace
 
 import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 )
 
@@ -52,4 +56,20 @@ func FindScreenCaptureView() *nodewith.Finder {
 // FindTray returns a finder which locates the holding space tray node.
 func FindTray() *nodewith.Finder {
 	return nodewith.ClassName(holdingSpaceTrayClassName)
+}
+
+// ResetHoldingSpaceOptions is defined in autotest_private.idl.
+type ResetHoldingSpaceOptions struct {
+	MarkTimeOfFirstAdd bool `json:"markTimeOfFirstAdd"`
+}
+
+// ResetHoldingSpace calls autotestPrivate to remove all items in the holding space model
+// and resets all holding space prefs.
+func ResetHoldingSpace(ctx context.Context, tconn *chrome.TestConn,
+	options ResetHoldingSpaceOptions) error {
+	if err := tconn.Call(ctx, nil,
+		"tast.promisify(chrome.autotestPrivate.resetHoldingSpace)", options); err != nil {
+		return errors.Wrap(err, "failed to reset holding space")
+	}
+	return nil
 }
