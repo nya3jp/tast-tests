e71d82314c33e8749f38daf56705f3d0f1818957
Alvin Jia <alvinjia@google.com>
2021-11-05 10:03:49 +1100

    tast-tests: selectors for uidetection prototype
    Add the data structure storing the detection info: request and result.
    Add three constructors for word, textblock and custom icon.
    
    BUG=b:203358189
    TEST=None
    
    Change-Id: Icd33ad9baf6102d469c743a7674fb32357abcfab

---
+++ src/chromiumos/tast/local/uidetection/textblock_finder.go
@@ -0,0 +1,27 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package uidetection
+
+import (
+	"strings"
+
+	pb "chromiumos/tast/local/uidetection/api"
+)
+
+// TextBlock returns a finder for a given textblock.
+// Use TextBlock if you want to find a group of nearby text,
+// generally representing an entire text element.
+// E.g. use uidetection.TextBlock([]string{"Save", "As"})
+// to find the "Save As" menu item.
+func TextBlock(words []string) *Finder {
+	detectionRequest := &pb.DetectionRequest{
+		DetectionRequestType: &pb.DetectionRequest_TextBlockDetectionRequest{
+			TextBlockDetectionRequest: &pb.TextBlockDetectionRequest{
+				Words: words,
+			},
+		},
+	}
+	return newFromRequest(detectionRequest, strings.Join(words, ","))
+}
