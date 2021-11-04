a500f018263192c9a1f2cb5c3c2871c9263a14ca
Stephen Boyd <swboyd@chromium.org>
2021-11-03 23:05:02 -0700

    kernel: Make pstore console ramoops test less flaky
    Sometimes the message that this test writes to the kernel log isn't
    present in the console-ramoops file because the ramoops file is full of
    other kernel messages or errors/warnings that happened between when
    /dev/kmsg was written and reboot actually rebooted. Let's just test to
    confirm that the file exists and has data. The file is only populated by
    the kernel if the ramoops is valid from the previous kernel, so we don't
    really need to confirm that the file contents have anything we wrote
    from the previous kernel.
    
    In fact, we can skip the reboot path entirely if the file is already
    present as it means console ramoops worked for _this_ kernel boot,
    making this test much faster to run in the general case. If the file
    isn't there for some reason, for example if userspace deleted the
    ramoops file from pstore, then we'll run reboot and check for the file.
    This should reduce the flakyness of this test and we should be able to
    promote it to critical.
    
    BUG=b:189058499
    TEST=tast run $DUT kernel.PstoreConsoleRamoops
    
    Change-Id: I7a3128019ab74f5f97d2c06abab86059fac85864

--- src/chromiumos/tast/remote/bundles/cros/kernel/pstore_console_ramoops.go
+++ src/chromiumos/tast/remote/bundles/cros/kernel/pstore_console_ramoops.go
@@ -8,7 +8,6 @@ import (
 	"context"
 	"io/ioutil"
 	"path/filepath"
-	"regexp"
 	"strings"
 
 	"chromiumos/tast/ssh/linuxssh"
@@ -28,50 +27,46 @@ func init() {
 	})
 }
 
-// PstoreConsoleRamoops Writes a blurb to kernel logs, reboots, and checks to
-// make sure console-ramoops has that message. This confirms that pstore is
-// properly saving the previous kernel log.
+// PstoreConsoleRamoops confirms that pstore is properly saving the previous kernel log.
 func PstoreConsoleRamoops(ctx context.Context, s *testing.State) {
-	const testKey = "tast is rebooting for PstoreConsoleRamoops"
-
 	d := s.DUT()
 
-	if err := linuxssh.WriteFile(ctx, d.Conn(), "/dev/kmsg", []byte(testKey), 0644); err != nil {
-		s.Fatal("Failed to write message to /dev/kmsg on the DUT: ", err)
+	checkForPstoreConsoleRamoops := func(ctx context.Context, destName string) bool {
+		ramoopsDir := filepath.Join(s.OutDir(), destName)
+		if err := linuxssh.GetFile(ctx, d.Conn(), "/sys/fs/pstore/", ramoopsDir, linuxssh.PreserveSymlinks); err != nil {
+			s.Fatal("Failed to copy ramoops dir after reboot on the DUT: ", err)
+		}
+
+		files, err := ioutil.ReadDir(ramoopsDir)
+		if err != nil {
+			s.Fatal("Failed to list ramoops directory: ", err)
+		}
+
+		for _, file := range files {
+			if strings.HasPrefix(file.Name(), "console-ramoops") && file.Size() > 0 {
+				return true
+			}
+		}
+
+		return false
 	}
 
+	if checkForPstoreConsoleRamoops(ctx, "before-reboot") {
+		// The ramoops file already exists so this kernel under test
+		// has a working ramoops implementation that has kept the
+		// kernel log from the previous boot. No need to reboot.
+		return
+	}
+
+	// Userspace must have deleted the file or this kernel is from cold boot.
 	s.Log("Rebooting DUT")
 	if err := d.Reboot(ctx); err != nil {
 		s.Fatal("Failed to reboot the DUT: ", err)
 	}
 
-	ramoopsDir := filepath.Join(s.OutDir(), "console-ramoops")
-	if err := linuxssh.GetFile(ctx, d.Conn(), "/sys/fs/pstore/", ramoopsDir, linuxssh.PreserveSymlinks); err != nil {
-		s.Fatal("Failed to copy ramoops dir after reboot on the DUT: ", err)
-	}
-
-	files, err := ioutil.ReadDir(ramoopsDir)
-	if err != nil {
-		s.Fatal("Failed to list ramoops directory: ", err)
-	}
-
-	for _, file := range files {
-		if !strings.HasPrefix(file.Name(), "console-ramoops") {
-			continue
-		}
-
-		f, err := ioutil.ReadFile(filepath.Join(ramoopsDir, file.Name()))
-		if err != nil {
-			s.Fatal("Failed to read ramoops file: ", err)
-		}
-
-		goodSigRegexp := regexp.MustCompile(testKey)
-		if !goodSigRegexp.Match(f) {
-			s.Error("Couldn't find reboot signature in ramoops file ", file.Name())
-		}
-
+	if checkForPstoreConsoleRamoops(ctx, "after-reboot") {
 		return
 	}
 
-	s.Error("Couldn't find reboot signature in any console-ramoops file")
+	s.Error("Couldn't find any console-ramoops file")
 }
