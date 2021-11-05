a8db82429ed61fc29c86045dd26d1f918f4b7685
Victor-Gabriel Savu <vsavu@google.com>
2021-11-05 14:59:58 +0100

    omaha: Use a fixture to load the test configuration
    Use a fixture to load device parameters once for all tests.
    Move the parameters to a JSON file to allow for dynamic loading of
    data in the future.
    
    BUG=b:205275302
    TEST=tast run <ip> '("group:omaha")'
    
    Cq-Depend: chromium:3263986
    Change-Id: I8ecf0cd4cbdf13b508a07f3ab2c8be365815e5f4

---
+++ src/chromiumos/tast/common/fixture/omaha.go
@@ -0,0 +1,11 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fixture
+
+// Fixture defined in chromiumos/tast/remote/bundles/cros/omaha/params/fixture.go.
+const (
+	// Omaha is a fixture name.
+	Omaha = "omaha"
+)
