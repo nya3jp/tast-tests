bb673802f5535966418176fe0b34839f94c02b12
Jeff Chen <jeffcchen@google.com>
2021-11-05 17:00:49 +0000

    Updating tast tests to run with linear outputs as well.
    BUG=b:203240043
    TEST=VDVDA tast tests (arc.VideoDecodeAccelVDVDA*)
    
    Cq-Depend: chromium:3256071
    Change-Id: I1408f1dda95fbf5205002c11999895b6b1b10bf7

--- src/chromiumos/tast/local/bundles/cros/arc/video_decode_accel_vd_vda.go
+++ src/chromiumos/tast/local/bundles/cros/arc/video_decode_accel_vd_vda.go
@@ -13,6 +13,11 @@ import (
 	"chromiumos/tast/testing"
 )
 
+type videoDecodeAccelVDVDATestParam struct {
+	dataPath        string
+	useLinearOutput bool
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         VideoDecodeAccelVDVDA,
@@ -23,43 +28,43 @@ func init() {
 		Timeout:      4 * time.Minute,
 		Params: []testing.Param{{
 			Name:              "h264",
-			Val:               "test-25fps.h264",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "test-25fps.h264"},
 			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
 			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
 		}, {
 			Name:              "vp8",
-			Val:               "test-25fps.vp8",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "test-25fps.vp8"},
 			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
 		}, {
 			Name:              "vp9",
-			Val:               "test-25fps.vp9",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "test-25fps.vp9"},
 			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
 		}, {
 			Name: "vp9_2",
-			Val:  "test-25fps.vp9_2",
+			Val:  videoDecodeAccelVDVDATestParam{dataPath: "test-25fps.vp9_2"},
 			// TODO(crbug.com/911754): reenable this test once HDR VP9.2 is implemented.
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_2},
 			ExtraData:         []string{"test-25fps.vp9_2", "test-25fps.vp9_2.json"},
 		}, {
 			Name:              "h264_resolution_switch",
-			Val:               "switch_1080p_720p_240frames.h264",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "switch_1080p_720p_240frames.h264"},
 			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
 			ExtraData:         []string{"switch_1080p_720p_240frames.h264", "switch_1080p_720p_240frames.h264.json"},
 		}, {
 			Name:              "vp8_resolution_switch",
-			Val:               "resolution_change_500frames.vp8.ivf",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "resolution_change_500frames.vp8.ivf"},
 			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			ExtraData:         []string{"resolution_change_500frames.vp8.ivf", "resolution_change_500frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp9_resolution_switch",
-			Val:               "resolution_change_500frames.vp9.ivf",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "resolution_change_500frames.vp9.ivf"},
 			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"resolution_change_500frames.vp9.ivf", "resolution_change_500frames.vp9.ivf.json"},
@@ -67,14 +72,14 @@ func init() {
 			// This test uses a video that makes use of the VP9 show-existing-frame feature and is used in Android CTS:
 			// https://android.googlesource.com/platform/cts/+/HEAD/tests/tests/media/res/raw/vp90_2_17_show_existing_frame.vp9
 			Name:              "vp9_show_existing_frame",
-			Val:               "vda_smoke-vp90_2_17_show_existing_frame.vp9",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "vda_smoke-vp90_2_17_show_existing_frame.vp9"},
 			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"vda_smoke-vp90_2_17_show_existing_frame.vp9", "vda_smoke-vp90_2_17_show_existing_frame.vp9.json"},
 		}, {
 			// H264 stream in which a profile changes from Baseline to Main.
 			Name:              "h264_profile_change",
-			Val:               "test-25fps_basemain.h264",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "test-25fps_basemain.h264"},
 			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
 			ExtraData:         []string{"test-25fps_basemain.h264", "test-25fps_basemain.h264.json"},
@@ -83,7 +88,67 @@ func init() {
 			// Decode VP9 spatial-SVC stream. Precisely the structure in the stream is called k-SVC, where spatial-layers are at key-frame only.
 			// The structure is used in Hangouts Meet. go/vp9-svc-hangouts for detail.
 			Name:              "vp9_keyframe_spatial_layers",
-			Val:               "keyframe_spatial_layers_180p_360p.vp9.ivf",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "keyframe_spatial_layers_180p_360p.vp9.ivf"},
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "vaapi"},
+			ExtraData:         []string{"keyframe_spatial_layers_180p_360p.vp9.ivf", "keyframe_spatial_layers_180p_360p.vp9.ivf.json"},
+		}, {
+			Name:              "h264_linear_output",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "test-25fps.h264", useLinearOutput: true},
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
+		}, {
+			Name:              "vp8_linear_output",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "test-25fps.vp8", useLinearOutput: true},
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
+		}, {
+			Name:              "vp9_linear_output",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "test-25fps.vp9", useLinearOutput: true},
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
+		}, {
+			Name:              "h264_resolution_switch_linear_output",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "switch_1080p_720p_240frames.h264", useLinearOutput: true},
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+			ExtraData:         []string{"switch_1080p_720p_240frames.h264", "switch_1080p_720p_240frames.h264.json"},
+		}, {
+			Name:              "vp8_resolution_switch_linear_output",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "resolution_change_500frames.vp8.ivf", useLinearOutput: true},
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+			ExtraData:         []string{"resolution_change_500frames.vp8.ivf", "resolution_change_500frames.vp8.ivf.json"},
+		}, {
+			Name:              "vp9_resolution_switch_linear_output",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "resolution_change_500frames.vp9.ivf", useLinearOutput: true},
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         []string{"resolution_change_500frames.vp9.ivf", "resolution_change_500frames.vp9.ivf.json"},
+		}, {
+			// This test uses a video that makes use of the VP9 show-existing-frame feature and is used in Android CTS:
+			// https://android.googlesource.com/platform/cts/+/HEAD/tests/tests/media/res/raw/vp90_2_17_show_existing_frame.vp9
+			Name:              "vp9_show_existing_frame_linear_output",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "vda_smoke-vp90_2_17_show_existing_frame.vp9", useLinearOutput: true},
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         []string{"vda_smoke-vp90_2_17_show_existing_frame.vp9", "vda_smoke-vp90_2_17_show_existing_frame.vp9.json"},
+		}, {
+			// H264 stream in which a profile changes from Baseline to Main.
+			Name:              "h264_profile_change_linear_output",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "test-25fps_basemain.h264", useLinearOutput: true},
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+			ExtraData:         []string{"test-25fps_basemain.h264", "test-25fps_basemain.h264.json"},
+		}, {
+			// Run with HW decoder using VA-API only because only the HW decoder can decode SVC stream correctly today.
+			// Decode VP9 spatial-SVC stream. Precisely the structure in the stream is called k-SVC, where spatial-layers are at key-frame only.
+			// The structure is used in Hangouts Meet. go/vp9-svc-hangouts for detail.
+			Name:              "vp9_keyframe_spatial_layers_linear_output",
+			Val:               videoDecodeAccelVDVDATestParam{dataPath: "keyframe_spatial_layers_180p_360p.vp9.ivf", useLinearOutput: true},
 			ExtraAttr:         []string{"group:mainline", "informational"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "vaapi"},
 			ExtraData:         []string{"keyframe_spatial_layers_180p_360p.vp9.ivf", "keyframe_spatial_layers_180p_360p.vp9.ivf.json"},
@@ -92,7 +157,8 @@ func init() {
 }
 
 func VideoDecodeAccelVDVDA(ctx context.Context, s *testing.State) {
-	if err := decoding.RunAccelVideoTest(ctx, s.OutDir(), s.DataPath(s.Param().(string)), decoding.VDVDA); err != nil {
+	param := s.Param().(videoDecodeAccelVDVDATestParam)
+	if err := decoding.RunAccelVideoTest(ctx, s.OutDir(), s.DataPath(param.dataPath), decoding.TestParams{DecoderType: decoding.VDVDA, LinearOutput: param.useLinearOutput}); err != nil {
 		s.Fatal("test failed: ", err)
 	}
 }
--- src/chromiumos/tast/local/bundles/cros/arc/video_decode_accel_vd_vda_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/video_decode_accel_vd_vda_perf.go
@@ -13,10 +13,14 @@ import (
 	"chromiumos/tast/testing"
 )
 
+type videoDecodeAccelVDVDAPerfTestParam struct {
+	dataPath        string
+	useLinearOutput bool
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         VideoDecodeAccelVDVDAPerf,
-		LacrosStatus: testing.LacrosVariantUnknown,
 		Desc:         "Measures performance of hardware decode acceleration performance using media::VideoDecoder through the VDA interface, by running the video_decode_accelerator_perf_tests binary (see go/vd-migration)",
 		Contacts:     []string{"dstaessens@chromium.org", "chromeos-video-eng@google.com"},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
@@ -24,70 +28,88 @@ func init() {
 		Timeout:      4 * time.Minute,
 		Params: []testing.Param{{
 			Name:              "h264_1080p_30fps",
-			Val:               "1080p_30fps_300frames.h264",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.h264"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
 			ExtraData:         []string{"1080p_30fps_300frames.h264", "1080p_30fps_300frames.h264.json"},
 		}, {
 			Name:              "h264_1080p_60fps",
-			Val:               "1080p_60fps_600frames.h264",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_60fps_600frames.h264"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264_60},
 			ExtraData:         []string{"1080p_60fps_600frames.h264", "1080p_60fps_600frames.h264.json"},
 		}, {
 			Name:              "h264_2160p_30fps",
-			Val:               "2160p_30fps_300frames.h264",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_30fps_300frames.h264"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K},
 			ExtraData:         []string{"2160p_30fps_300frames.h264", "2160p_30fps_300frames.h264.json"},
 		}, {
 			Name:              "h264_2160p_60fps",
-			Val:               "2160p_60fps_600frames.h264",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_60fps_600frames.h264"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K60},
 			ExtraData:         []string{"2160p_60fps_600frames.h264", "2160p_60fps_600frames.h264.json"},
 		}, {
 			Name:              "vp8_1080p_30fps",
-			Val:               "1080p_30fps_300frames.vp8.ivf",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.vp8.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			ExtraData:         []string{"1080p_30fps_300frames.vp8.ivf", "1080p_30fps_300frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp8_1080p_60fps",
-			Val:               "1080p_60fps_600frames.vp8.ivf",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_60fps_600frames.vp8.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_60},
 			ExtraData:         []string{"1080p_60fps_600frames.vp8.ivf", "1080p_60fps_600frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp8_2160p_30fps",
-			Val:               "2160p_30fps_300frames.vp8.ivf",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_30fps_300frames.vp8.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_4K},
 			ExtraData:         []string{"2160p_30fps_300frames.vp8.ivf", "2160p_30fps_300frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp8_2160p_60fps",
-			Val:               "2160p_60fps_600frames.vp8.ivf",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_60fps_600frames.vp8.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_4K60},
 			ExtraData:         []string{"2160p_60fps_600frames.vp8.ivf", "2160p_60fps_600frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp9_1080p_30fps",
-			Val:               "1080p_30fps_300frames.vp9.ivf",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.vp9.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"1080p_30fps_300frames.vp9.ivf", "1080p_30fps_300frames.vp9.ivf.json"},
 		}, {
 			Name:              "vp9_1080p_60fps",
-			Val:               "1080p_60fps_600frames.vp9.ivf",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_60fps_600frames.vp9.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_60},
 			ExtraData:         []string{"1080p_60fps_600frames.vp9.ivf", "1080p_60fps_600frames.vp9.ivf.json"},
 		}, {
 			Name:              "vp9_2160p_30fps",
-			Val:               "2160p_30fps_300frames.vp9.ivf",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_30fps_300frames.vp9.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_4K},
 			ExtraData:         []string{"2160p_30fps_300frames.vp9.ivf", "2160p_30fps_300frames.vp9.ivf.json"},
 		}, {
 			Name:              "vp9_2160p_60fps",
-			Val:               "2160p_60fps_600frames.vp9.ivf",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_60fps_600frames.vp9.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_4K60},
 			ExtraData:         []string{"2160p_60fps_600frames.vp9.ivf", "2160p_60fps_600frames.vp9.ivf.json"},
-		}},
+		},
+			{
+				Name:              "h264_linear_output_1080p_30fps",
+				Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.h264", useLinearOutput: true},
+				ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+				ExtraData:         []string{"1080p_30fps_300frames.h264", "1080p_30fps_300frames.h264.json"},
+			},
+			{
+				Name:              "vp8_linear_output_1080p_30fps",
+				Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.vp8.ivf", useLinearOutput: true},
+				ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+				ExtraData:         []string{"1080p_30fps_300frames.vp8.ivf", "1080p_30fps_300frames.vp8.ivf.json"},
+			}, {
+				Name:              "vp9_linear_output_1080p_30fps",
+				Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.vp9.ivf", useLinearOutput: true},
+				ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+				ExtraData:         []string{"1080p_30fps_300frames.vp9.ivf", "1080p_30fps_300frames.vp9.ivf.json"},
+			}},
 	})
 }
 
 func VideoDecodeAccelVDVDAPerf(ctx context.Context, s *testing.State) {
-	if err := decoding.RunAccelVideoPerfTest(ctx, s.OutDir(), s.DataPath(s.Param().(string)), decoding.VDVDA); err != nil {
+	param := s.Param().(videoDecodeAccelVDVDAPerfTestParam)
+	if err := decoding.RunAccelVideoPerfTest(ctx, s.OutDir(), s.DataPath(param.dataPath), decoding.TestParams{DecoderType: decoding.VDVDA, LinearOutput: param.useLinearOutput}); err != nil {
 		s.Fatal("test failed: ", err)
 	}
 }
