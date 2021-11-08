15944e2314261e119aca2f3a262c6fc5bafc016e
minch <minch@chromium.org>
2021-11-08 10:04:09 -0800

    tast-tests: Fix failures caused by class removal
    CloseDeskButton has been removed and the usage has been migrated to
    CloseButton
    
    BUG=chromium:1264920
    TEST=tast -verbose run * wmp.VirtualDesksBasic
    
    Change-Id: I1740a60bd54a73ed9bacfa188645f97838c179a4

--- src/chromiumos/tast/local/bundles/cros/wmp/virtual_desks_basic.go
+++ src/chromiumos/tast/local/bundles/cros/wmp/virtual_desks_basic.go
@@ -144,7 +144,7 @@ func VirtualDesksBasic(ctx context.Context, s *testing.State) {
 	}
 
 	// Delete the new desk.
-	closeDeskButton := nodewith.ClassName("CloseDeskButton")
+	closeDeskButton := nodewith.ClassName("CloseButton")
 	if err := ac.LeftClick(closeDeskButton)(ctx); err != nil {
 		s.Fatal("Failed to delete the new desk: ", err)
 	}
