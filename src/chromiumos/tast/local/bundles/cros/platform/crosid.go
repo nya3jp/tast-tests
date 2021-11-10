bce9adcdffef1774fbd90eeffd613b8d45be1a6c
Jack Rosenthal <jrosenth@chromium.org>
2021-11-09 18:45:43 -0700

    platform.CrosID: Add new tast test
    Add a new tast test which does just a basic run that crosid runs and
    passes on all unibuild systems.
    
    BUG=b:202766035
    TEST=pending
    
    Change-Id: I752a4334a173c864eee35d91f1a2c2fc9b905b10

---
+++ src/chromiumos/tast/local/bundles/cros/platform/crosid.go
@@ -0,0 +1,32 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosID,
+		Desc: "Tests that unibuild devices are able to probe identity via crosid",
+		Contacts: []string{
+			"jrosenth@chromium.org", // Test author
+		},
+		SoftwareDeps: []string{"cros_config"},
+		Attr:         []string{"group:mainline", "informational"},
+	})
+}
+
+func CrosID(ctx context.Context, s *testing.State) {
+	out, err := testexec.CommandContext(ctx, "crosid", "-v").CombinedOutput()
+	status, ok := testexec.ExitCode(err)
+	if !ok {
+		s.Fatalf("crosid exited with status %s: %s", status, string(out))
+	}
+}
