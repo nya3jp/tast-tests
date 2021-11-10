58574cd4015aa67a0850e6ccc92ed7636a798938
Hyung Tae Kim <hyungtaekim@chromium.org>
2021-11-10 15:43:12 -0800

    lacros-au: Save screen record for troubleshooting flake
    This CL saves faillogs and a screen record log from DUT to
    use for troubleshooting flakes that cause Lacros update
    test to fail in opening a Lacros browser.
    
    BUG=chromium:1258664
    TEST=# Start TLW + TLS locally
    TEST=tast run \
    TEST=  -var=provision.TLSAddr=${ADDR} ${DUT} \
    TEST=  lacros.Update*
    
    Change-Id: Iade15197522b81ca62d8c8eae11a77037afe01cb

--- src/chromiumos/tast/remote/bundles/cros/lacros/update_stateful_to_rootfs.go
+++ src/chromiumos/tast/remote/bundles/cros/lacros/update_stateful_to_rootfs.go
@@ -38,7 +38,7 @@ func init() {
 
 func UpdateStatefulToRootfs(ctx context.Context, s *testing.State) {
 	// Create a UpdateTestService client.
-	conn, err := rpc.Dial(ctx, s.DUT(), s.RPCHint(), "cros")
+	conn, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
 	if err != nil {
 		s.Fatal("Failed to connect to DUT: ", err)
 	}
