1fa312c17c26c95d0816e8aec3b6f59471fa12e6
Sangchun Han <sangchun@google.com>
2021-11-09 19:57:23 -0700

    Add network.ShillNoErrorLog Tast test.
    BUG=b:204365880
    TEST="$ tast run localhost:2222 network.ShillNoErrorLog" after SSH remote port forwarding
    
    Change-Id: I29ea7c6e4c7d0b3994b6a83b49077f650ec73898

---
+++ src/chromiumos/tast/common/shillconst/netlog_known_errors.go
@@ -0,0 +1,36 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package shillconst
+
+// AllowedEntry defines the error log entry that is allowed/expected.
+type AllowedEntry struct {
+	Program      string
+	FileName     string
+	MessageRegex string
+	Counter      int
+}
+
+// InitializeAllowedEntries returns the allowed log entries with Counter = 0.
+func InitializeAllowedEntries() []AllowedEntry {
+	return []AllowedEntry{
+		{"patchpaneld", "ethod_invoker.h", ".*CallMethodAndBlockWithTimeout.*", 0},
+		{"patchpaneld", "manager.cc", ".*Invalid namespace name.*", 0},
+		{"patchpaneld", "object_proxy.cc", ".*Failed to call method.*", 0},
+		{"patchpaneld", "scoped_ns.cc", ".*Could not open namespace.*", 0},
+		{"patchpaneld", "shill_client.cc", ".*Unable to get manager properties.*", 0},
+		{"shill", "cellular_capability_3gpp.cc", ".*No slot found for.*", 0},
+		{"shill", "cellular.cc", ".*StartModem failed.*", 0},
+		{"shill", "device_info.cc", ".*Add Link message for.*does not have .*", 0},
+		{"shill", "dns_client.cc", ".*No valid DNS server addresses.*", 0},
+		{"shill", "ethod_invoker.h", ".*CallMethodAndBlockWithTimeout.*", 0},
+		{"shill", "http_request.cc", ".*Failed to start DNS client.*", 0},
+		{"shill", "object_proxy.cc", ".*Failed to call method.*", 0},
+		{"shill", "portal_detector.cc", ".*HTTP probe failed to start.*", 0},
+		{"shill", "unknown", ".*", 0},
+		{"shill", "utils.cc", ".*AddDBusError.*", 0},
+		{"shill", "wifi.cc", ".*does not support MAC address randomization.*", 0},
+		{"wpa_supplicant", "", ".*Permission denied.*", 0},
+	}
+}
