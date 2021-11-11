14a9a545e2006f8ea99dbfb2cbbe3c1cb20547d9
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-11-11 14:05:17 -0800

    arcappcompat: Fail the test if the device display power is off
    BUG=b:183135675
    
    It is observed that one of the reasons for the “Failed to start
    the activity of an app” issue is due to “Failed to ensure the
    display is on”. Updated this issue to fatal.
    
    TEST=tast run <helios> arcappcompat.ABCKids.vm_clamshell_mode
             tast run <helios> arcappcompat.ABCKids.vm_tablet_mode
             tast run <kevin> arcappcompat.ABCKids.clamshell_mode
             tast run <kevin> arcappcompat.ABCKids.tablet_mode
    
    Change-Id: I8b3b1a955cb021c3a5116daa138a1d3ca1737c98

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/testutil/testutil.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/testutil/testutil.go
@@ -114,7 +114,7 @@ func RunTestCases(ctx context.Context, s *testing.State, appPkgName, appActivity
 	defer act.Close()
 	// TODO(b/166637700): Remove this if a proper solution is found that doesn't require the display to be on.
 	if err := power.TurnOnDisplay(ctx); err != nil {
-		s.Log("Failed to ensure the display is on: ", err)
+		s.Fatal("Failed to ensure the display is on: ", err)
 	}
 	if err := act.Start(ctx, tconn); err != nil {
 		s.Fatal("Failed to start app before test cases: ", err)
@@ -157,7 +157,7 @@ func RunTestCases(ctx context.Context, s *testing.State, appPkgName, appActivity
 			defer cancel()
 			// TODO(b/166637700): Remove this if a proper solution is found that doesn't require the display to be on.
 			if err := power.TurnOnDisplay(ctx); err != nil {
-				s.Log("Failed to ensure the display is on: ", err)
+				s.Fatal("Failed to ensure the display is on: ", err)
 			}
 			// Launch the app.
 			if err := act.Start(ctx, tconn); err != nil {
@@ -279,7 +279,7 @@ func setUpDevice(ctx context.Context, s *testing.State, appPkgName, appActivity 
 	s.Log("Installing app")
 	// TODO(b/166637700): Remove this if a proper solution is found that doesn't require the display to be on.
 	if err := power.TurnOnDisplay(ctx); err != nil {
-		s.Log("Failed to ensure the display is on: ", err)
+		s.Fatal("Failed to ensure the display is on: ", err)
 	}
 	if err := apps.Launch(ctx, tconn, apps.PlayStore.ID); err != nil {
 		s.Fatal("Failed to launch Play Store: ", err)
@@ -410,7 +410,7 @@ func MinimizeRestoreApp(ctx context.Context, s *testing.State, tconn *chrome.Tes
 	s.Log("Minimizing the window")
 	defaultState, err := ash.GetARCAppWindowState(ctx, tconn, appPkgName)
 	if err != nil {
-		s.Error("Failed to get the default window state: ", err)
+		s.Fatal("Failed to get the default window state: ", err)
 	}
 	if _, err := ash.SetARCAppWindowState(ctx, tconn, appPkgName, ash.WMEventMinimize); err != nil {
 		s.Error("Failed to minimize the window: ", err)
@@ -443,7 +443,7 @@ func MinimizeRestoreApp(ctx context.Context, s *testing.State, tconn *chrome.Tes
 func ClamshellResizeWindow(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
 	info, err := ash.GetARCAppWindowInfo(ctx, tconn, appPkgName)
 	if err != nil {
-		s.Error("Failed to get window info: ", err)
+		s.Fatal("Failed to get window info: ", err)
 	}
 	goalState := ash.WindowStateMaximized
 	if info.State == ash.WindowStateFullscreen {
@@ -465,7 +465,7 @@ func ClamshellResizeWindow(ctx context.Context, s *testing.State, tconn *chrome.
 	if t == arc.Container {
 		info, err := ash.GetARCAppWindowInfo(ctx, tconn, appPkgName)
 		if err != nil {
-			s.Error("Failed to get window info: ", err)
+			s.Fatal("Failed to get window info: ", err)
 		}
 		s.Logf("App Resize info, info.CanResize %+v", info.CanResize)
 		if !info.CanResize {
@@ -637,7 +637,7 @@ func ResizeLock(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a
 func checkCompatModeButton(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) error {
 	info, err := ash.GetARCAppWindowInfo(ctx, tconn, appPkgName)
 	if err != nil {
-		s.Error("Failed to get window info: ", err)
+		s.Fatal("Failed to get window info: ", err)
 	}
 	// Check if app is launched in maximized or in fullscreen state.
 	if info.State == ash.WindowStateMaximized || info.State == ash.WindowStateFullscreen {
@@ -895,7 +895,7 @@ func TouchviewRotate(ctx context.Context, s *testing.State, tconn *chrome.TestCo
 func MouseScrollAction(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
 	checkForScrollLayout := d.Object(ui.PackageName(appPkgName), ui.Scrollable(true), ui.Focusable(true), ui.Enabled(true))
 	if err := checkForScrollLayout.WaitForExists(ctx, DefaultUITimeout); err != nil {
-		s.Log("ScrollLayout doesn't exist. Page is not scrollable and skipping the test: ", err)
+		s.Log("ScrollLayout doesn't exist. Page is not scrollable. Skipping the test: ", err)
 		return
 	}
 	// To perform mouse scroll actions.
@@ -913,7 +913,7 @@ func MouseScrollAction(ctx context.Context, s *testing.State, tconn *chrome.Test
 func TouchScreenScroll(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
 	checkForScrollLayout := d.Object(ui.PackageName(appPkgName), ui.Scrollable(true), ui.Focusable(true), ui.Enabled(true))
 	if err := checkForScrollLayout.WaitForExists(ctx, DefaultUITimeout); err != nil {
-		s.Log("ScrollLayout doesn't exist. Page is not scrollable and skipping the test: ", err)
+		s.Log("ScrollLayout doesn't exist. Page is not scrollable. Skipping the test: ", err)
 		return
 	}
 
@@ -985,6 +985,7 @@ func OrientationSize(ctx context.Context, s *testing.State, tconn *chrome.TestCo
 		tabletModeEnabled, err := ash.TabletModeEnabled(ctx, tconn)
 		if err != nil {
 			s.Fatal("Failed to get tablet mode: ", err)
+			return
 		}
 		if tabletModeEnabled {
 			s.Log("Device is in tablet mode. Skipping test")
@@ -1055,7 +1056,6 @@ func Largescreenlayout(ctx context.Context, s *testing.State, tconn *chrome.Test
 	}
 	deviceDisplayWidth := info.DisplayWidth
 	deviceDisplayHeight := info.DisplayHeight
-
 	if appWidth >= deviceDisplayWidth {
 		testing.ContextLogf(ctx, "App utilizes the device display screen and its appWidth %+v appHeight %+v  deviceDisplayWidth %+v deviceDisplayHeight %+v", appWidth, appHeight, deviceDisplayWidth, deviceDisplayHeight)
 	} else {
@@ -1070,7 +1070,7 @@ func ReOpenWindow(ctx context.Context, s *testing.State, tconn *chrome.TestConn,
 	// Create an activity handle.
 	act, err := arc.NewActivity(a, appPkgName, appActivity)
 	if err != nil {
-		s.Fatal("Failed to create new app activity: ", err)
+		s.Fatal("Failed to create new app activity. Skipping the test: ", err)
 	}
 	defer act.Close()
 
@@ -1121,7 +1121,7 @@ func SplitScreen(ctx context.Context, s *testing.State, tconn *chrome.TestConn, 
 
 	windowInfo, err := ash.GetARCAppWindowInfo(ctx, tconn, appPkgName)
 	if err != nil {
-		s.Error("Failed to get window info: ", err)
+		s.Fatal("Failed to get window info: ", err)
 	}
 	s.Logf("App Resize info, info.CanResize %+v", windowInfo.CanResize)
 	if !windowInfo.CanResize {
