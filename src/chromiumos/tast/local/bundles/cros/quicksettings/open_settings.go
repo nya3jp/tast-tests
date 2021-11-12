2c1403bd22c53b2997061eb5fea98197965f32bc
minch <minch@chromium.org>
2021-11-12 12:49:11 -0800

    tast-tests: Fix failures caused by class removal
    TopShortcutButton has been removed and the usage has been migrated to
    IconButton. See crrev.com/c/3267360.
    
    BUG=chromium:1268959
    TEST=tast -verbose run localhost:2222 quicksettings.OpenSettings
    
    Change-Id: I2852be3acdb279b79c3bc1edd2b26e9482efd804

--- src/chromiumos/tast/local/bundles/cros/quicksettings/open_settings.go
+++ src/chromiumos/tast/local/bundles/cros/quicksettings/open_settings.go
@@ -25,7 +25,7 @@ func init() {
 			"chromeos-sw-engprod@google.com",
 			"amehfooz@chromium.org",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      "chromeLoggedIn",
 	})
