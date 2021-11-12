2102f4d0b7d03e817967ab17e472f70d18a3f1b4
alexanderhartl <alexanderhartl@google.com>
2021-11-12 16:51:32 +0000

    tast: Add a package for tape
    This adds the tape package for communication with the TAPE google
    cloud project to tast. TAPE is used to retrieve an owned test
    account for a test and configure policices for that account.
    
    BUG=b:187709578
    TEST=tested manually against a local running version of TAPE
    
    Change-Id: I90f4f77f9753100cf703a745931c45cf3b7b5886

---
+++ src/chromiumos/tast/common/tape/tape.go
@@ -0,0 +1,247 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package tape enables access to the TAPE service which offers access to owned test accounts and
+// configuration of policies on DPanel for those accounts.
+// The TAPE project is documented here: TODO(alexanderhartl): add link once its finished
+package tape
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+
+	"chromiumos/tast/errors"
+)
+
+// TokenDir is the location where the access token for tape is stored.
+const TokenDir = "/tmp/tokensource"
+
+// TokenFile is the file where the access token for tape is stored.
+const TokenFile = "tokensource.json"
+
+const tapeURL = "https://tape-307412.ey.r.appspot.com/"
+const tapeAudience = "770216225211-ihjn20dlehf94m9l4l5h0b0iilvd1vhc.apps.googleusercontent.com"
+
+// Account is a struct representing an owned test account with its credentials.
+type Account struct {
+	Email    string `json:"email"`
+	GaiaID   int64  `json:"gaiaid"`
+	Orgunit  string `json:"orgunit"`
+	Password string `json:"password"`
+	Timeout  int64  `json:"timeout"`
+}
+
+// TokenString creates a json string from an oauth.Token generated for a serviceAccount
+// which can be used for http connections to the TAPE GCP.
+func TokenString(ctx context.Context, serviceAccountFile string) (string, error) {
+	data, err := ioutil.ReadFile(serviceAccountFile)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to read credential json")
+	}
+
+	config, err := google.JWTConfigFromJSON(data)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to generate JWT config")
+	}
+	customClaims := make(map[string]interface{})
+	customClaims["target_audience"] = tapeAudience
+
+	config.PrivateClaims = customClaims
+	config.UseIDToken = true
+
+	ts := config.TokenSource(ctx)
+	token, err := ts.Token()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get Token")
+	}
+	tsJSON, err := json.Marshal(token)
+	if err != nil {
+		return "", errors.Wrap(err, "failed create JSON from token")
+	}
+
+	return string(tsJSON), nil
+}
+
+// tapeTokensource is a Tokensource which returns an access token to authenticate against the TAPE GCP.
+type tapeTokensource struct{ AccessToken *oauth2.Token }
+
+func (ts tapeTokensource) Token() (*oauth2.Token, error) {
+	if ts.AccessToken == nil || ts.AccessToken.Expiry.Before(time.Now()) {
+		tokenJSON, err := ioutil.ReadFile(filepath.Join(TokenDir, TokenFile))
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to read token file")
+		}
+		err = json.Unmarshal(tokenJSON, &ts.AccessToken)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to unmarshal the token")
+		}
+
+		if ts.AccessToken.Expiry.Before(time.Now()) {
+			return nil, errors.Wrap(err, "no valid token found")
+		}
+	}
+	return ts.AccessToken, nil
+}
+
+// NewTapeClient creates a http client which provides the necessary token to connect to the TAPE
+// GCP from a token string. All functions in the tape package should be passed this http client.
+func NewTapeClient(ctx context.Context) (*http.Client, error) {
+	ts := tapeTokensource{AccessToken: nil}
+	token, err := ts.Token()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get access token")
+	}
+	return oauth2.NewClient(ctx, oauth2.ReuseTokenSource(token, ts)), nil
+}
+
+// sendRequestWithTimeout makes a call to the specified REST endpoint of TAPE with the given http method and payload.
+func sendRequestWithTimeout(ctx context.Context, method, endpoint string, timeout time.Duration, payload *bytes.Reader, client *http.Client) (*http.Response, error) {
+	// Shorten the context as the call should not take longer than timeout.
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	// Create a request.
+	req, err := http.NewRequestWithContext(ctx, method, tapeURL+endpoint, payload)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create request")
+	}
+	req.Header.Set("Content-Type", "application/json")
+	// Send the request.
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get a response from TAPE")
+	}
+	// Check if the call was successful.
+	if response.StatusCode != 200 {
+		return nil, errors.Errorf("%s at %s returned %d", method, endpoint, response.StatusCode)
+	}
+	return response, nil
+}
+
+// RequestAccount calls TAPE to obtain credentials for an available owned test account and returns it.
+// The returned Account can not be obtained by other calls to RequestAccount until it is released or it times out
+// after the given timeout is reached. A timeout of 0 will use the DEFAULT_ACCOUNT_TIMEOUT of the TAPE server
+// which is 2 hours, timeouts larger than 2 days = 172800 seconds are not allowed.
+// The returned password is temporary and will be valid for roughly one day.
+func RequestAccount(ctx context.Context, timeout time.Duration, client *http.Client) (*Account, error) {
+	const maxTimeout = 172800 * time.Second
+	if timeout > maxTimeout {
+		return nil, errors.Errorf("timeout may not be larger than %v seconds", maxTimeout.Seconds())
+	}
+	payloadBytes, err := json.Marshal(timeout.Seconds())
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal data")
+	}
+	payload := bytes.NewReader(payloadBytes)
+	response, err := sendRequestWithTimeout(ctx, "POST", "requestAccount", 30*time.Second, payload, client)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to make REST call")
+	}
+	defer response.Body.Close()
+	// Read the response.
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read response from TAPE")
+	}
+	var acc Account
+	err = json.Unmarshal([]byte(respBody), &acc)
+	if err != nil {
+		return nil, err
+	}
+	return &acc, nil
+}
+
+// RegeneratePassword calls TAPE to obtain a new temporary Password for the given Account and returns the Account.
+// A temporary password will be valid for roughly a day. Tests are not expected to use this as RequestAccount will
+// already provide a temporary password.
+func (acc *Account) RegeneratePassword(ctx context.Context, client *http.Client) error {
+	payloadBytes, err := json.Marshal(acc)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal data")
+	}
+	payload := bytes.NewReader(payloadBytes)
+	response, err := sendRequestWithTimeout(ctx, "POST", "regeneratePassword", 30*time.Second, payload, client)
+	if err != nil {
+		return errors.Wrap(err, "failed to make REST call")
+	}
+	defer response.Body.Close()
+	// Read the response.
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return errors.Wrap(err, "failed to read response from TAPE")
+	}
+	var password string
+	err = json.Unmarshal([]byte(respBody), &password)
+	if err != nil {
+		return err
+	}
+	acc.Password = password
+	return nil
+}
+
+// CleanUp calls TAPE to clean up the Account. All policies will be set to their default values and all other
+// state changes will also be reverted. However the account will not be released with this call.
+// Accounts will always be cleaned up when they are requested with RequestAccount, tests are not expected to call
+// this function to clean up when a test is finished.
+func (acc *Account) CleanUp(ctx context.Context, client *http.Client) error {
+	payloadBytes, err := json.Marshal(acc)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert Account to json")
+	}
+	payload := bytes.NewReader(payloadBytes)
+	response, err := sendRequestWithTimeout(ctx, "POST", "cleanUp", 60*time.Second, payload, client)
+	if err != nil {
+		return errors.Wrap(err, "failed to make REST call")
+	}
+	defer response.Body.Close()
+	// Read the response.
+	_, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		return errors.Wrap(err, "failed to read response from TAPE")
+	}
+	return nil
+}
+
+// ReleaseAccount calls TAPE to release the Account so it becomes available again.
+func (acc *Account) ReleaseAccount(ctx context.Context, client *http.Client) error {
+	payloadBytes, err := json.Marshal(acc)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert Account to json")
+	}
+	payload := bytes.NewReader(payloadBytes)
+	response, err := sendRequestWithTimeout(ctx, "POST", "releaseAccount", 30*time.Second, payload, client)
+	if err != nil {
+		return errors.Wrap(err, "failed to make REST call")
+	}
+	defer response.Body.Close()
+	// Read the response.
+	_, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		return errors.Wrap(err, "failed to read response from TAPE")
+	}
+	return nil
+}
+
+// SetPolicy calls TAPE to set policySchema in DPanel.
+func (acc *Account) SetPolicy(ctx context.Context, policySchema PolicySchema, client *http.Client) error {
+	payloadBytes, err := policySchema.Schema2JSON(acc.Orgunit)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal data")
+	}
+	payload := bytes.NewReader(payloadBytes)
+	response, err := sendRequestWithTimeout(ctx, "POST", "setPolicy", 30*time.Second, payload, client)
+	if err != nil {
+		return errors.Wrap(err, "failed to make REST call")
+	}
+	defer response.Body.Close()
+	return nil
+}
