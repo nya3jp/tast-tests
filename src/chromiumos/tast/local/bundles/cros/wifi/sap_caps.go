0e4f600225d8fd0b5f97c185081c20d42eaa4e52
Jintao Lin <jintaolin@chromium.org>
2021-11-12 18:20:31 +0800

    wifi: Add wifi softAP capability test
    Adding `iw list` check for SoftAP capabilities. Checking
    if DUT supports softAP, max number of associated STAs supported.
    
    This change also separates the HE capabilities for STA and AP in
    accordance with iw and nl80211. So when DUT supports HE in STA mode,
    the test checks if DUT also supports HE in SoftAP mode.
    
    BUG=b:205914971
    
    TEST:tast run <dut-ip> wifi.SAPCaps (trogdor)
    TEST:tast run <dut-ip> wifi.Caps wifi.Caps80211ac wifi.HeCaps (trogdor, hatch)
    
    Cq-Depend: chromium:3282756
    Change-Id: I341b0bc9c493f2cbc171abe234489385b558ede5

---
+++ src/chromiumos/tast/local/bundles/cros/wifi/sap_caps.go
@@ -0,0 +1,64 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+
+	"chromiumos/tast/local/network/iw"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SAPCaps,
+		Desc: "Verifies DUT supports SoftAP and a minimum set of required protocols",
+		Contacts: []string{
+			"jintaolin@chromium.org",          // Test author
+			"chromeos-wifi-champs@google.com", // WiFi oncall rotation; or http://b/new?component=893827
+		},
+		Attr:         []string{"group:wificell", "wificell_func", "wificell_unstable"},
+		SoftwareDeps: []string{"wifi"},
+		HardwareDeps: hwdep.D(hwdep.WifiSAP()),
+	})
+}
+
+func SAPCaps(ctx context.Context, s *testing.State) {
+	iwr := iw.NewLocalRunner()
+	res, err := iwr.ListPhys(ctx)
+	if err != nil {
+		s.Fatal("ListPhys failed: ", err)
+	}
+	if len(res) == 0 {
+		s.Fatal("Expect at least one wireless phy; found nothing")
+	}
+
+	apSupported := false
+	for _, mode := range res[0].Modes {
+		if mode == "AP" {
+			apSupported = true
+		}
+	}
+	if !apSupported {
+		s.Error("AP mode not supported")
+	}
+
+	if res[0].MaxSTAs < 16 {
+		s.Error("Less than 16 associated STAs are supported in AP mode: ", res[0].MaxSTAs)
+	}
+
+	if res[0].SupportHESTA {
+		if !res[0].SupportHEAP {
+			s.Error("Device doesn't support HE in AP mode")
+		}
+	}
+
+	if res[0].SupportHE40HE80STA {
+		if !res[0].SupportHE40HE80AP {
+			s.Error("Device doesn't support 5ghz HE in AP mode")
+		}
+	}
+}
