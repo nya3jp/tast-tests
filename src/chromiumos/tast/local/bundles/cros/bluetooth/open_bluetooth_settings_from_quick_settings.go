cb1f88f99942dec4851edb9bc6f0b21217fe945e
Chad Duffin <chadduffin@chromium.org>
2021-11-13 02:02:19 +0000

    [TAST] Create tests for the Bluetooth surfaces.
    BUG=chromium:1010321
    TEST=tast run <DUT> bluetooth.*Bluetooth*
    
    Change-Id: Ic34516fb539ef9a79d8420bb4cdc69a980f83be5

---
+++ src/chromiumos/tast/local/bundles/cros/bluetooth/open_bluetooth_settings_from_quick_settings.go
@@ -0,0 +1,59 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bluetooth
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/quicksettings"
+	"chromiumos/tast/testing"
+)
+
+// bluetoothSubPageURL is the URL of the Bluetooth sub-page within the OS Settings.
+const bluetoothSubPageURL = "chrome://os-settings/bluetoothDevices"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: OpenBluetoothSettingsFromQuickSettings,
+		Desc: "Checks that clicking the Settings button on the detailed Bluetooth page within the Quick Settings navigates to the Bluetooth Settings",
+		Contacts: []string{
+			"chadduffin@chromium.org",
+			"cros-connectivity@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedInWithBluetoothEnabled",
+	})
+}
+
+// OpenBluetoothSettingsFromQuickSettings tests that a user can successfully
+// navigate through to the Bluetooth sub-page within the OS Settings from the
+// Settings button in the Bluetooth detailed view within the Quick Settings.
+func OpenBluetoothSettingsFromQuickSettings(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	if err := quicksettings.NavigateToBluetoothDetailedView(ctx, tconn); err != nil {
+		s.Fatal("Failed to navigate to the detailed Bluetooth view: ", err)
+	}
+
+	if err := uiauto.New(tconn).LeftClick(quicksettings.BluetoothDetailedViewSettingsButton)(ctx); err != nil {
+		s.Fatal("Failed to click the Bluetooth Settings button: ", err)
+	}
+
+	// Check if the Bluetooth sub-page within the OS Settings was opened.
+	matcher := chrome.MatchTargetURL(bluetoothSubPageURL)
+	conn, err := cr.NewConnForTarget(ctx, matcher)
+	if err != nil {
+		s.Fatal("Failed to open the Bluetooth settings: ", err)
+	}
+	defer conn.Close()
+}
---
+++ src/chromiumos/tast/local/bundles/cros/bluetooth/pair_new_device_from_bluetooth_quick_settings.go
@@ -0,0 +1,64 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bluetooth
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bluetooth"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/quicksettings"
+	"chromiumos/tast/testing"
+)
+
+// bluetoothPairingDialogURL is the URL of the Bluetooth pairing dialog.
+const bluetoothPairingDialogURL = "chrome://bluetooth-pairing/"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PairNewDeviceFromBluetoothQuickSettings,
+		Desc: "Checks that the pairing dialog can be opened from within the Bluetooth Quick Settings",
+		Contacts: []string{
+			"chadduffin@chromium.org",
+			"cros-connectivity@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedInWithBluetoothEnabled",
+	})
+}
+
+// PairNewDeviceFromBluetoothQuickSettings tests that a user can successfully
+// open the pairing dialog from the "Pair new device" button in the detailed
+// Bluetooth view within the Quick Settings.
+func PairNewDeviceFromBluetoothQuickSettings(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	if err := quicksettings.NavigateToBluetoothDetailedView(ctx, tconn); err != nil {
+		s.Fatal("Failed to navigate to the detailed Bluetooth view: ", err)
+	}
+
+	if err := bluetooth.PollForBTEnabled(ctx); err != nil {
+		s.Fatal("Expected Bluetooth to be enabled: ", err)
+	}
+
+	if err := uiauto.New(tconn).LeftClick(quicksettings.BluetoothDetailedViewPairNewDeviceButton)(ctx); err != nil {
+		s.Fatal("Failed to click the \"Pair new device\" button: ", err)
+	}
+
+	// Check if the Bluetooth pairing dialog was opened.
+	matcher := chrome.MatchTargetURL(bluetoothPairingDialogURL)
+	conn, err := cr.NewConnForTarget(ctx, matcher)
+	if err != nil {
+		s.Fatal("Failed to open the Bluetooth pairing dialog: ", err)
+	}
+	defer conn.Close()
+}
