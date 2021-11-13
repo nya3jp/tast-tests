137cffee44df6c72f1fea35c4b973b15ef14b2ab
Chad Duffin <chadduffin@chromium.org>
2021-11-13 02:02:19 +0000

    [TAST] Create tests for the Bluetooth surfaces.
    BUG=chromium:1010321
    TEST=tast run <DUT> bluetooth.*Bluetooth*
    
    Change-Id: Ic34516fb539ef9a79d8420bb4cdc69a980f83be5

--- src/chromiumos/tast/local/bundles/cros/bluetooth/bluetoothutil/doc.go
+++ src/chromiumos/tast/local/bundles/cros/bluetooth/bluetoothutil/doc.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package bluetooth contains local Tast tests that exercise the CrOS Bluetooth
-// surfaces.
-package bluetooth
+// Package bluetoothutil contains constants and helper functions for Bluetooth
+// tests.
+package bluetoothutil
