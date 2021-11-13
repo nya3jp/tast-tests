909affb6dc4be468dd3e7c40b9d9caf0859018b4
Chad Duffin <chadduffin@chromium.org>
2021-11-13 02:02:19 +0000

    [TAST] Create tests for the Bluetooth surfaces.
    BUG=chromium:1010321
    TEST=tast run <DUT> bluetooth.*Bluetooth*
    
    Change-Id: Ic34516fb539ef9a79d8420bb4cdc69a980f83be5

--- src/chromiumos/tast/local/bundles/cros/bluetooth/doc.go
+++ src/chromiumos/tast/local/bundles/cros/bluetooth/doc.go
@@ -2,5 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package bluetooth contains local Tast tests that exercise the Chrome OS Bluetooth stack.
+// Package bluetooth contains local Tast tests that exercise the CrOS Bluetooth
+// surfaces.
 package bluetooth
---
+++ src/chromiumos/tast/local/bundles/cros/bluetooth/fixture.go
@@ -0,0 +1,36 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bluetooth
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name: "chromeLoggedInWithBluetoothRevamp",
+		Desc: "Logs into a user session with the BluetoothRevamp feature flag enabled",
+		Contacts: []string{
+			"chadduffin@chromium.org",
+			"cros-connectivity@google.com",
+		},
+		Impl:            ChromeLoggedInWithBluetoothRevampEnabled(),
+		SetUpTimeout:    chrome.LoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+	})
+}
+
+// ChromeLoggedInWithBluetoothRevampEnabled returns a fixture implementation
+// that builds on the existing chromeLoggedIn fixture to also enable the
+// BluetoothRevamp feature flag.
+func ChromeLoggedInWithBluetoothRevampEnabled() testing.FixtureImpl {
+	return chrome.NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+		return []chrome.Option{chrome.EnableFeatures("BluetoothRevamp")}, nil
+	})
+}
