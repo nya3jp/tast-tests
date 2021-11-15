018d1b7c02e00b4bc5e83d5d0323e639b697775e
David Welling <davidwelling@google.com>
2021-11-15 17:55:29 +0000

    arcgameperf: add roblox launch test
    Adds a test to measure launch metrics for Roblox. Defaults to a limited
    number of boards to start in order to allow testing to be done.
    
    BUG=b:206442649
    TEST=tast run <HOST> arcappgameperf.*
    
    Change-Id: I4ced6d30aa3efbbf05faf95356a6f973c7d58d8d

---
+++ src/chromiumos/tast/local/bundles/cros/arcappgameperf/roblox_launch.go
@@ -0,0 +1,56 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arcappgameperf
+
+import (
+	"context"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappgameperf/pre"
+	"chromiumos/tast/local/bundles/cros/arcappgameperf/testutil"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RobloxLaunch,
+		Desc:         "Captures launch metrics for Roblox",
+		Contacts:     []string{"davidwelling@google.com", "arc-engprod@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome"},
+		HardwareDeps: hwdep.D(hwdep.Model("eve", "hatch", "fizz", "zork")),
+		Params: []testing.Param{
+			{
+				ExtraSoftwareDeps: []string{"android_p"},
+				Pre:               pre.ArcAppGamePerfBooted,
+			}, {
+				Name:              "vm",
+				ExtraSoftwareDeps: []string{"android_vm"},
+				Pre:               pre.ArcAppGamePerfBooted,
+			}},
+		Timeout: 10 * time.Minute,
+		VarDeps: []string{"arcappgameperf.username", "arcappgameperf.password"},
+	})
+}
+
+func RobloxLaunch(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.roblox.client"
+		appActivity = ".startup.ActivitySplash"
+	)
+
+	testutil.PerformLaunchTest(ctx, s, appPkgName, appActivity, func(params testutil.TestParams) (isLaunched bool, err error) {
+		// onAppReady: Landing will appear in logcat after the game is fully loaded.
+		if err := params.Arc.WaitForLogcat(ctx, arc.RegexpPred(regexp.MustCompile(`onAppReady:\sLanding`))); err != nil {
+			return false, errors.Wrap(err, "onAppReady was not found in LogCat")
+		}
+
+		return true, nil
+	})
+}
