66ddeba8e5c34aa5d6363781a269059133e59062
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2021-11-15 05:25:06 +0000

    DO NOT MERGE: Example usage of the tree tests.
    This is an example test that violates the label rule.
    
    BUG=b:194852911
    TEST=../platform/tast/fast_build.sh -t chromiumos/tast/restrictions/cros
    and see expected error happens.
    
    Change-Id: I9e3cc7e8317f29317d293389a90a76e4e4f3deb3

---
+++ src/chromiumos/tast/local/bundles/cros/example/fragile_matcher.go
@@ -0,0 +1,44 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/testing"
+	"chromiumos/tast/restrictions"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name:            "exampleFixture",
+		Desc:            "An example",
+		Contacts:        []string{},
+		SetUpTimeout:    30 * time.Second,
+		ResetTimeout:    5 * time.Second,
+		TearDownTimeout: 10 * time.Second,
+		Labels:          []string{restrictions.FragileUIMatcherLabel},
+		// Implementation ommitted, because this is just example of test metadata.
+	})
+	testing.AddTest(&testing.Test{
+		Func:     FragileMatcher,
+		Desc:     "Fixture uses fragile UI matcher",
+		Contacts: []string{"nya@chromium.org", "tast-owners@google.com"},
+		Attr:     []string{"group:mainline"},
+		Fixture:  "exampleFixture",
+		Params: []testing.Param{{
+			// Unit test fails, because the depended fixture uses fragile UI matcher
+		}, {
+			// Unit test fails, because the test is mainline
+			Name: "allow",
+			ExtraLabels:[]string{restrictions.FragileUIMatcherLabel},
+		}},
+	})
+}
+
+func FragileMatcher(ctx context.Context, s *testing.State) {
+	// No errors means the test passed.
+}
