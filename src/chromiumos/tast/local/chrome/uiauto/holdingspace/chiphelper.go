0e082d42d28e8ee4102a93002a0c7ca814f0045f
Lance Wang <lance.wang@cienet.com>
2021-11-15 11:15:24 +0800

    mta: Improve holdingspace.Download test coverage
    Improve test's coverage by adding pin, unpin, select and launch
    single/multi-files operations.
    
    [MTA f339b] Verifies pin, unpin, select and launch single/multi-files
    operations can be done on Holding Space.
    
    BUG=b:200987282
    TEST=tast run <DUT> holdingspace.Download.*
    
    Change-Id: I3694278e0c32c64169a44cc43ee9eb2e702a0185

---
+++ src/chromiumos/tast/local/chrome/uiauto/holdingspace/chiphelper.go
@@ -0,0 +1,103 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package holdingspace
+
+import (
+	"fmt"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+)
+
+// ChipType indicates the type of a chip.
+type ChipType int
+
+const (
+	// Done indicates the chip is done.
+	Done ChipType = iota
+	// Downloading indicates the chip is downloading.
+	Downloading
+	// Paused indicates the chip is paused.
+	Paused
+)
+
+// ChipHelper is a helper that can easier interact with chips in the HoldingSpace.
+// Chips in the HoldingSpace could have various names available on the a11y tree,
+// depending on its type (such as download/pinned) or status, this helper wraps those
+// details and provide a method: (*ChipHelper).Finder(fileName string), which allows caller to
+// find the chip with only the file-name(s) specified.
+//
+// Here are some typical usages to retrieve the finder:
+//
+//	holdingspace.DownloadChipHelper(holdingspace.Downloading).Finder("fileName")
+//	holdingspace.PinnedChipHelper().Finder("fileName")
+//
+// And here are some typical usages to interact multiple chips:
+//
+//	holdingspace.DownloadChipHelper(holdingspace.Downloading).WaitUntilAllRemoved("fileName1")
+//	holdingspace.DownloadChipHelper(holdingspace.Paused).WaitUntilAllExist("fileName1", "fileName2")
+//	holdingspace.PinnedChipHelper().WaitUntilAllRemoved("fileName1", "fileName2", "fileName3")
+type ChipHelper struct {
+	chipFinder *nodewith.Finder
+	chipType   ChipType
+}
+
+// DownloadChipHelper returns the helper to interact with the chips under download section in holdingspace.
+func DownloadChipHelper(chipType ChipType) *ChipHelper {
+	return &ChipHelper{
+		chipFinder: FindDownloadChip(),
+		chipType:   chipType,
+	}
+}
+
+// PinnedChipHelper returns the helper to interact with the chips under pinned file section in holdingspace.
+func PinnedChipHelper() *ChipHelper {
+	return &ChipHelper{
+		chipFinder: FindPinnedFileChip(),
+		chipType:   Done,
+	}
+}
+
+// Finder returns the finder of the chip.
+func (chip *ChipHelper) Finder(fileName string) *nodewith.Finder {
+	return chip.chipFinder.Name(chip.name(fileName))
+}
+
+// name returns the name of the chip.
+func (chip *ChipHelper) name(fileName string) string {
+	switch chip.chipType {
+	case Downloading:
+		return fmt.Sprintf("Downloading %s", fileName)
+	case Paused:
+		return fmt.Sprintf("Download paused %s", fileName)
+	default:
+		return fileName
+	}
+}
+
+// WaitUntilAllRemoved waits until all chips with specified file names are removed.
+func (chip *ChipHelper) WaitUntilAllRemoved(tconn *chrome.TestConn, files []string) uiauto.Action {
+	ui := uiauto.New(tconn)
+	actions := make([]uiauto.Action, 0, len(files))
+	for _, file := range files {
+		actions = append(actions,
+			ui.WaitUntilGone(chip.Finder(file)),
+			ui.EnsureGoneFor(chip.Finder(file), 5*time.Second),
+		)
+	}
+	return uiauto.Combine("wait for all chips are removed", actions...)
+}
+
+// WaitUntilAllExist waits until all chips with specified file names are exist.
+func (chip *ChipHelper) WaitUntilAllExist(tconn *chrome.TestConn, files []string) uiauto.Action {
+	ui := uiauto.New(tconn)
+	actions := make([]uiauto.Action, 0, len(files))
+	for _, file := range files {
+		actions = append(actions, ui.WaitUntilExists(chip.Finder(file)))
+	}
+	return uiauto.Combine("wait for all chips exist", actions...)
+}
