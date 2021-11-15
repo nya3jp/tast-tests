f11a996dc4fb2799f3c9871b419e31eaef172022
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2021-11-15 05:25:06 +0000

    Add fixture tree label verifier test.
    BUG=none
    TEST=fast_build.sh -t chromiumos/tast/test
    
    Change-Id: I9904882d56cfe3b1b4505a90de2f72a4d05ca85f

---
+++ src/chromiumos/tast/test/fixtree_test.go
@@ -0,0 +1,42 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package main implements a unit test for cros test bundles.
+//
+// The test checks the restrictions enforced for the test metadata.
+package main
+
+import (
+	gotesting "testing"
+
+	_ "chromiumos/tast/local/bundles/cros/allcategories"
+	_ "chromiumos/tast/remote/bundles/cros/allcategories"
+	"chromiumos/tast/testing/testcheck"
+)
+
+func TestFixtTest(t *gotesting.T) {
+	nodes := testcheck.EntityDependencies()
+	fragileLabel := "use_fragile_matcher"
+	for _, v := range nodes {
+		if v.HasLabel(fragileLabel) {
+			if v.HasAttr("group:mainline") {
+				t.Errorf("test %s, has \"%s\" label, but is a mainline test",
+					v.Name, fragileLabel)
+			}
+			continue
+		}
+		if v.Parent == "" {
+			continue
+		}
+		p, ok := nodes[v.Parent]
+		if !ok {
+			t.Errorf("fixture %s, referenced by %s %s, not found", v.Parent, v.Type, v.Name)
+			continue
+		}
+		if p.HasLabel(fragileLabel) {
+			t.Errorf("%s %s doesn't have \"%s\" label, but the parent %s %s does.",
+				v.Type, v.Name, fragileLabel, p.Type, p.Name)
+		}
+	}
+}
