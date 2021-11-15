db3426b88c82c88510df4580fffd2bd4f4dbb530
Harvey Yang <chenghaoyang@chromium.org>
2021-11-15 17:20:08 +0800

    hardware: Add tracing in SensorIioservice
    BUG=b:194266474
    TEST=run on coachz
    
    Change-Id: Idbb07a89a23148744ed669c17bd108ef6361723e

--- src/chromiumos/tast/local/bundles/cros/hardware/sensor_iioservice.go
+++ src/chromiumos/tast/local/bundles/cros/hardware/sensor_iioservice.go
@@ -9,14 +9,20 @@ import (
 	"fmt"
 	"strings"
 
+	"android.googlesource.com/platform/external/perfetto/protos/perfetto/metrics/github.com/google/perfetto/perfetto_proto"
+
 	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/local/bundles/cros/hardware/iio"
+	"chromiumos/tast/local/tracing"
 	"chromiumos/tast/testing"
 )
 
 const onErrorOccurred = "OnErrorOccurred:"
 const latencyExceedsTolerance = "Max latency exceeds latency tolerance."
 const succeedReadingSamples = "Number of success reads"
+
+const traceMetricCPU = "android_cpu"
+const targetProcessName = "/usr/sbin/iioservice"
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -27,9 +33,34 @@ func init() {
 			"chenghaoyang@chromium.org", // Test author
 			"chromeos-sensors@google.com",
 		},
+		Data:         []string{tracing.TBMTracedProbesConfigFile, tracing.TraceProcessor()},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"iioservice"},
 	})
+}
+
+// processCPUMetric extracts information of the target process in the
+// cpu metric.
+func processCPUMetric(cpuMetric *perfetto_proto.AndroidCpuMetric, s *testing.State) {
+	foundTarget := false
+	for _, processInfo := range cpuMetric.GetProcessInfo() {
+		if processInfo.GetName() == targetProcessName {
+			foundTarget = true
+
+			metric := processInfo.GetMetrics()
+			s.Log("megacycles: ", metric.GetMcycles())
+			s.Log("runtime in nanosecond: ", metric.GetRuntimeNs())
+			s.Log("min_freq in kHz: ", metric.GetMinFreqKhz())
+			s.Log("max_freq in kHz: ", metric.GetMaxFreqKhz())
+			s.Log("avg_freq in kHz: ", metric.GetAvgFreqKhz())
+
+			break
+		}
+	}
+
+	if foundTarget == false {
+		s.Error("Failed to find the target process: ", targetProcessName)
+	}
 }
 
 // SensorIioservice reads all devices' samples from daemon iioservice.
@@ -44,6 +75,12 @@ func SensorIioservice(ctx context.Context, s *testing.State) {
 	}
 
 	for _, sn := range sensors {
+		// Start a trace session using the perfetto command line tool.
+		traceConfigPath := s.DataPath(tracing.TBMTracedProbesConfigFile)
+		sess, err := tracing.StartSession(ctx, traceConfigPath)
+		// The temporary file of trace data is no longer needed when returned.
+		defer sess.RemoveTraceResultFile()
+
 		maxFreq = sn.MaxFrequency
 
 		if sn.Name == iio.Ring {
@@ -74,5 +111,16 @@ func SensorIioservice(ctx context.Context, s *testing.State) {
 		} else {
 			s.Logf("Test passed on device name: %v, id: %v", sn.Name, sn.IioID)
 		}
+
+		if err := sess.Stop(); err != nil {
+			s.Fatal("Failed to stop the tracing session: ", err)
+		}
+
+		metrics, err := sess.RunMetrics(ctx, s.DataPath(tracing.TraceProcessor()), []string{traceMetricCPU})
+		if err != nil {
+			s.Fatal("Failed to RunMetrics: ", err)
+		}
+
+		processCPUMetric(metrics.GetAndroidCpu(), s)
 	}
 }
