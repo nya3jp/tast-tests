253c66a0fbaa629a4f86437064e2fe63b07192b8
Anastasiia N <anastasiian@chromium.org>
2021-11-17 21:47:43 +0100

    tast-tests: AddAccountFromOgb test for CrOS Account Manager
    - Go to google.com, open One Google Bar
    - Add account in the system dialog
    - Check that account is present in OS Settings, OGB and ARC
    
    BUG=chromium:1268351
    TEST=tast run <ip> accountmanager.AddAccountFromOgb.vm
    
    Change-Id: I7033b5049cb09282f1d3ff794cc8fa2eec26fdd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3288700
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Tested-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/accountmanager/add_account_from_ogb.go
@@ -0,0 +1,134 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package accountmanager
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/accountmanager"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AddAccountFromOGB,
+		Desc:         "Verify that a secondary account can be added from One Google Bar",
+		Contacts:     []string{"anastasiian@chromium.org", "team-dent@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+			Fixture:           "loggedInToChromeAndArc",
+			Val:               browser.TypeAsh,
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Fixture:           "loggedInToChromeAndArc",
+			Val:               browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"android_p", "lacros"},
+			Fixture:           "loggedInToChromeAndArcWithLacros",
+			Val:               browser.TypeLacros,
+		}, {
+			Name:              "vm_lacros",
+			ExtraSoftwareDeps: []string{"android_vm", "lacros"},
+			Fixture:           "loggedInToChromeAndArcWithLacros",
+			Val:               browser.TypeLacros,
+		}},
+		VarDeps: []string{"accountmanager.username1", "accountmanager.password1"},
+		Timeout: 6 * time.Minute,
+	})
+}
+
+func AddAccountFromOGB(ctx context.Context, s *testing.State) {
+	username := s.RequiredVar("accountmanager.username1")
+	password := s.RequiredVar("accountmanager.password1")
+
+	// Reserve one minute for various cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, time.Minute)
+	defer cancel()
+
+	cr := s.FixtValue().(accountmanager.FixtureData).Chrome()
+
+	// Connect to Test API to use it with the UI library.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// Setup the browser.
+	br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(browser.Type))
+	if err != nil {
+		s.Fatal("Failed to setup chrome: ", err)
+	}
+	defer closeBrowser(cleanupCtx)
+
+	ui := uiauto.New(tconn).WithTimeout(time.Minute)
+	a := s.FixtValue().(accountmanager.FixtureData).ARC
+
+	if err := accountmanager.OpenOneGoogleBar(ctx, tconn, br); err != nil {
+		s.Fatal("Failed to open OGB: ", err)
+	}
+
+	if err := clickAddAccount(ctx, ui); err != nil {
+		s.Fatal("Failed to find add account link: ", err)
+	}
+
+	s.Log("Adding a secondary Account")
+	if err := accountmanager.AddAccount(ctx, tconn, username, password); err != nil {
+		s.Fatal("Failed to add a secondary Account: ", err)
+	}
+
+	s.Log("Verifying that account is present in OS Settings")
+	// Open Account Manager page in OS Settings and find Add Google Account button.
+	if _, err := ossettings.LaunchAtPageURL(ctx, tconn, cr, "accountManager", ui.Exists(nodewith.Name("Add Google Account").Role(role.Button))); err != nil {
+		s.Fatal("Failed to launch Account Manager page: ", err)
+	}
+	// Find "More actions, <email>" button to make sure that account was added.
+	moreActionsButton := nodewith.Name("More actions, " + username).Role(role.Button)
+	if err := ui.WaitUntilExists(moreActionsButton)(ctx); err != nil {
+		s.Fatal("Failed to find More actions button: ", err)
+	}
+
+	// Check that account is present in OGB.
+	s.Log("Verifying that account is present in OGB")
+	secondaryAccountListItem := nodewith.NameContaining(username).Role(role.Link)
+	if err := accountmanager.CheckOneGoogleBar(ctx, tconn, br, ui.WaitUntilExists(secondaryAccountListItem)); err != nil {
+		s.Fatal("Failed to check that account is present in OGB: ", err)
+	}
+
+	// Check that account is present in ARC.
+	s.Log("Verifying that account is present in ARC")
+	if present, err := accountmanager.IsAccountPresentInArc(ctx, tconn, a, username); err != nil {
+		s.Fatal("Failed to check that account is present in ARC err: ", err)
+	} else if !present {
+		s.Fatal("Account is not present in ARC")
+	}
+}
+
+// clickAddAccount clicks on 'add another account' button in OGB.
+func clickAddAccount(ctx context.Context, ui *uiauto.Context) error {
+	addAccount := nodewith.Name("Add another account").Role(role.Link)
+	if err := uiauto.Combine("Click add account",
+		ui.WaitUntilExists(addAccount),
+		ui.LeftClick(addAccount),
+	)(ctx); err != nil {
+		return errors.Wrap(err, "failed to find and click add account link")
+	}
+	return nil
+}
