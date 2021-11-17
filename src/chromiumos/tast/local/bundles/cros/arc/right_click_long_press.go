e96cdeed32de8166e44b6502f125387c061faa2e
Tetsui Ohkubo <tetsui@chromium.org>
2021-11-17 19:57:09 +0900

    tast-tests: Add arc.RightClickLongPress
    This CL adds automated integration test for Right Click Long Press ARC
    app compat feature. When ResizeLock is enabled for the app, right clicks
    are converted to long presses.
    
    BUG=b:202679170
    TEST=tast run rammus-arc-r-DUT arc.RightClickLongPress
    
    Change-Id: I06fdc66ec028d592ca0b31944c7a6ca736c44704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3288859
    Tested-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arc/right_click_long_press.go
@@ -0,0 +1,87 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/android/adb"
+	"chromiumos/tast/common/android/ui"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/wm"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto/mouse"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RightClickLongPress,
+		Desc:         "Checks right click is properly converted to long press in compat mode",
+		Contacts:     []string{"yhanada@chromium.org", "arc-framework+tast@google.com"},
+		SoftwareDeps: []string{"chrome", "android_vm"},
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      "arcBootedWithTouchModeMouse",
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func RightClickLongPress(ctx context.Context, s *testing.State) {
+	a := s.FixtValue().(*arc.PreData).ARC
+	cr := s.FixtValue().(*arc.PreData).Chrome
+	d := s.FixtValue().(*arc.PreData).UIDevice
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	const (
+		apk          = "ArcLongPressTest.apk"
+		pkg          = "org.chromium.arc.testapp.longpress"
+		activityName = ".MainActivity"
+	)
+
+	if err := a.Install(ctx, arc.APKPath(apk), adb.InstallOptionFromPlayStore); err != nil {
+		s.Fatal("Failed to install the app: ", err)
+	}
+
+	act, err := arc.NewActivity(a, pkg, activityName)
+	if err != nil {
+		s.Fatalf("Failed to create a new activity %q: %v", activityName, err)
+	}
+	defer act.Close()
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+	if err := act.Start(ctx, tconn); err != nil {
+		s.Fatalf("Failed to start the activity %q", activityName)
+	}
+	defer act.Stop(cleanupCtx, tconn)
+
+	if err := wm.CheckVisibility(ctx, tconn, wm.BubbleDialogClassName, true); err != nil {
+		s.Fatal("Failed to wait for splash: ", err)
+	}
+
+	if err := wm.CloseSplash(ctx, tconn, wm.InputMethodClick, nil); err != nil {
+		s.Fatal("Failed to close splash: ", err)
+	}
+
+	window, err := ash.GetARCAppWindowInfo(ctx, tconn, pkg)
+	if err != nil {
+		s.Fatal("Failed to get ARC app window info: ", err)
+	}
+
+	if err := mouse.Click(tconn, window.TargetBounds.CenterPoint(), mouse.RightButton)(ctx); err != nil {
+		s.Fatal("Failed to right click the activity: ", err)
+	}
+
+	const fieldID = pkg + ":id/long_press_count"
+
+	if err := d.Object(ui.ID(fieldID)).WaitForText(ctx, "1", 30*time.Second); err != nil {
+		s.Fatal("Failed to wait for long press: ", err)
+	}
+}
