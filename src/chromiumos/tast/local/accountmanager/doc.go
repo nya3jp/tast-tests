17a848a73e61b0b2bdd1bdc2d8bb4a80ca77cdd9
Anastasiia N <anastasiian@chromium.org>
2021-11-17 21:44:14 +0100

    tast-tests: Add a fixture for AccountManager tests
    BUG=chromium:1268351
    TEST=None
    
    Change-Id: If4e9731d855b453f8662ff268e5abe2380228f79

---
+++ src/chromiumos/tast/local/accountmanager/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package accountmanager provides common functionality for managing accounts on
+// Chrome OS in-session e.g. adding an account in the system account addition
+// dialog.
+package accountmanager
