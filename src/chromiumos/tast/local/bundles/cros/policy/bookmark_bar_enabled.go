f277f22a36317941254a63e76373c197c40ae4a8
Georg Neis <neis@chromium.org>
2021-11-18 15:14:38 +0100

    policy: Refactor BookmarkBarEnabled tast for lacros
    Add a lacros variation and refactor the test code such that no
    branching on the chrome type is needed at the top-level.
    
    Also simplify the test by installing the bookmark via the
    ManagedBookmarks policy.
    
    BUG=chromium:1259192
    TEST=tast run policy.BookmarkBarEnabled*
    
    Change-Id: I9a7553a86827c04bf846ab548b2d74ff0a5b19a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3463676
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Tested-by: Georg Neis <neis@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/policy/bookmark_bar_enabled.go
+++ src/chromiumos/tast/local/bundles/cros/policy/bookmark_bar_enabled.go
@@ -7,16 +7,20 @@ package policy
 import (
 	"context"
 	"strings"
+	"time"
 
 	"chromiumos/tast/common/fixture"
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/lacros/lacrosfixt"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
-	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/testing"
 )
@@ -24,7 +28,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         BookmarkBarEnabled,
-		LacrosStatus: testing.LacrosVariantNeeded,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Test the behavior of BookmarkBarEnabled policy: check if bookmark bar is shown based on the value of the policy",
 		Contacts: []string{
 			"snijhara@google.com", // Test author
@@ -32,7 +36,17 @@ func init() {
 		},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline"},
-		Fixture:      fixture.ChromePolicyLoggedIn,
+		Vars:         []string{lacrosfixt.LacrosDeployedBinary},
+		Params: []testing.Param{{
+			Fixture: fixture.ChromePolicyLoggedIn,
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			ExtraAttr:         []string{"informational"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val:               browser.TypeLacros,
+		}},
 	})
 }
 
@@ -40,118 +54,89 @@ func init() {
 // states the policy introduces. When enabled the bookmark bar
 // appears with list of bookmarks otherwise it should not appear.
 func BookmarkBarEnabled(ctx context.Context, s *testing.State) {
-	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
 
+	// Reserve ten seconds for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
 	// Connect to Test API to use it with the UI library.
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to create Test API connection: ", err)
 	}
-
 	ui := uiauto.New(tconn)
 
-	const bookmarkName = "Policies"
-	// Bookmark the URL: chrome://policy.
-	func() {
-		conn, err := cr.NewConn(ctx, "chrome://policy")
-		if err != nil {
-			s.Fatal("Failed to connect to chrome: ", err)
-		}
-		defer conn.Close()
-
-		// Set up keyboard.
-		kb, err := input.Keyboard(ctx)
-		if err != nil {
-			s.Fatal("Failed to get keyboard: ", err)
-		}
-		defer kb.Close()
-
-		// Bookmark the current URL using keyboard shortcut.
-		if err := kb.Accel(ctx, "Ctrl+d"); err != nil {
-			s.Fatal("Failed to open bookmark added popup: ", err)
-		}
-
-		// Adding bookmark dialog should be shown.
-		// Find and click the name field in the dialog to specify the bookmark name.
-		bookmarkNameField := nodewith.Name("Bookmark name").Role(role.TextField)
-		if err := uiauto.Combine("find and click the bookmark name text field",
-			ui.WaitUntilExists(bookmarkNameField),
-			ui.LeftClick(bookmarkNameField),
-		)(ctx); err != nil {
-			s.Fatal("Failed to find and click the bookmark name text field: ", err)
-		}
-
-		// Select the existing text in the bookmark name field so that it can be deleted by pressing
-		// backspace in the next step.
-		if err := kb.Accel(ctx, "Ctrl+a"); err != nil {
-			s.Fatal("Failed to select bookmark name: ", err)
-		}
-
-		// Delete the existing text and type a name for the current bookmark.
-		if err := kb.Type(ctx, "\b"+bookmarkName); err != nil {
-			s.Fatal("Failed to type bookmark name: ", err)
-		}
-
-		// Click the "Done" button on the dialog.
-		if err := ui.LeftClick(nodewith.Name("Done").Role(role.Button).First())(ctx); err != nil {
-			s.Fatal("Failed to click the add bookmark done button: ", err)
-		}
-	}()
+	const folderName = "MyFolder12345"
+	var managedBookmarks = &policy.ManagedBookmarks{Val: []*policy.RefBookmarkType{
+		{
+			ToplevelName: folderName,
+			Name:         "Google",
+			Url:          "https://google.com/",
+		},
+	}}
 
 	for _, param := range []struct {
-		name           string                     // name is the subtest name.
-		wantBookmarbar bool                       // wantBookmarbar is the expected existence of the bookmark bar.
-		policy         *policy.BookmarkBarEnabled // policy is the policy we test.
+		name            string                     // name is the subtest name.
+		wantBookmarkBar bool                       // wantBookmarkBar is the expected existence of the bookmark bar.
+		policy          *policy.BookmarkBarEnabled // policy is the policy we test.
 	}{
 		{
-			name:           "unset",
-			wantBookmarbar: false,
-			policy:         &policy.BookmarkBarEnabled{Stat: policy.StatusUnset},
+			name:            "unset",
+			wantBookmarkBar: false,
+			policy:          &policy.BookmarkBarEnabled{Stat: policy.StatusUnset},
 		},
 		{
-			name:           "disabled",
-			wantBookmarbar: false,
-			policy:         &policy.BookmarkBarEnabled{Val: false},
+			name:            "disabled",
+			wantBookmarkBar: false,
+			policy:          &policy.BookmarkBarEnabled{Val: false},
 		},
 		{
-			name:           "enabled",
-			wantBookmarbar: true,
-			policy:         &policy.BookmarkBarEnabled{Val: true},
+			name:            "enabled",
+			wantBookmarkBar: true,
+			policy:          &policy.BookmarkBarEnabled{Val: true},
 		},
 	} {
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
-			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
-
 			// Perform cleanup.
 			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
 				s.Fatal("Failed to clean up: ", err)
 			}
 
 			// Update policies.
-			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.policy}); err != nil {
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{managedBookmarks, param.policy}); err != nil {
 				s.Fatal("Failed to update policies: ", err)
 			}
 
+			br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(browser.Type))
+			if err != nil {
+				s.Fatal("Failed to set up browser: ", err)
+			}
+			defer closeBrowser(cleanupCtx)
+
+			defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
+
 			// Open a new URL to check whether bookmark bar is shown.
-			vconn, err := cr.NewConn(ctx, "chrome://version")
+			vconn, err := br.NewConn(ctx, "chrome://version")
 			if err != nil {
 				s.Fatal("Failed to connect to chrome: ", err)
 			}
 			defer vconn.Close()
 
 			// Confirm whether bookmark bar is shown with the bookmarked URL.
-			// // TODO(crbug.com/1236546): Replace this with a helper function to check the existence of a UI node.
-			bookmarkedButton := nodewith.Name(bookmarkName).Role(role.Button).First()
-			if err = ui.WaitUntilExists(bookmarkedButton)(ctx); err != nil {
+			// TODO(crbug.com/1236546): Replace this with a helper function to check the existence of a UI node.
+			folderButton := nodewith.Name(folderName).Role(role.PopUpButton).First()
+			if err = ui.WaitUntilExists(folderButton)(ctx); err != nil {
 				if !strings.Contains(err.Error(), nodewith.ErrNotFound) {
 					s.Fatal("Failed to wait for the bookmark bar: ", err)
 				}
-				if param.wantBookmarbar {
-					s.Error("Bookmark bar with bookmarked URL not found: ", err)
+				if param.wantBookmarkBar {
+					s.Error("Bookmark bar with folder not found: ", err)
 				}
-			} else if !param.wantBookmarbar {
-				s.Error("Unexpected bookmark bar with bookmarked URL found: ", err)
+			} else if !param.wantBookmarkBar {
+				s.Error("Unexpected button in bookmark bar found: ", err)
 			}
 		})
 	}
