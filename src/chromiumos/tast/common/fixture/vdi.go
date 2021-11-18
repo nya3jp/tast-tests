1a4a74fb1c6449463c6270eb19e9886f1f25c99a
Kamil Szarek <kamilszarek@google.com>
2021-11-18 10:18:18 +0000

    vdi: Add fixtures for user session running Citrix and VMware
    Add fixture for starting Chrome using accounts configured for installing
    VMware Horizon and Citrix Workspace apps. Each fixture connects to the
    VDI server and logs in.
    
    BUG=b:204534030
    TEST=./fast_build.sh -T
    
    Cq-Depend: chrome-internal:4268778
    Change-Id: Ia00399305fcf72c712abbf3dc5d0581c61b8a5ac

---
+++ src/chromiumos/tast/common/fixture/vdi.go
@@ -0,0 +1,29 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fixture
+
+// Fixtures defined in chromiumos/tast/local/vdi/fixtures/vdi.go.
+const (
+	// CitrixLaunched is a fixture name.
+	CitrixLaunched = "citrixLaunched"
+	// VmwareLaunched is a fixture name.
+	VmwareLaunched = "vmwareLaunched"
+)
+
+// Fixtures defined in chromiumos/tast/local/vdi/fixtures/vdi_msg.go.
+const (
+	// MgsCitrixLaunched is a fixture name.
+	MgsCitrixLaunched = "mgsCitrixLaunched"
+	// MgsVmwareLaunched is a fixture name.
+	MgsVmwareLaunched = "mgsVmwareLaunched"
+)
+
+// Fixtures defined in chromiumos/tast/local/vdi/fixtures/vdi_kiosk.go.
+const (
+	// KioskCitrixLaunched is a fixture name.
+	KioskCitrixLaunched = "kioskCitrixLaunched"
+	// KioskVmwareLaunched is a fixture name.
+	KioskVmwareLaunched = "kioskVmwareLaunched"
+)
