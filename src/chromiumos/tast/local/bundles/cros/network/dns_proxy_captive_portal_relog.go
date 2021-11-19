04831975bee2436c2751a49a304aa88be6b3c6a7
Jason Jeremy Iman <jasongustaman@google.com>
2021-11-19 13:16:23 +0900

    network: DNSProxy: Add captive portal re-login test
    This patch checks that system and ARC are in the correct state
    when a re-login happened in a captive portal state.
    
    BUG=b:206027483
    TEST=tast run <DUT> network.DNSProxyCaptivePortal;
    TEST=tast run <DUT> network.DNSProxyCaptivePortalRelog;
    
    Change-Id: I5d7762dce218065faefccf8c38b4106b6b981c66

--- src/chromiumos/tast/local/bundles/cros/network/dns_proxy_captive_portal_relog.go
+++ src/chromiumos/tast/local/bundles/cros/network/dns_proxy_captive_portal_relog.go
@@ -6,10 +6,13 @@ package network
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/bundles/cros/network/dns"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/network"
@@ -19,26 +22,35 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         DNSProxyCaptivePortal,
-		Desc:         "Verify dns-proxy behaves correctly when shill detects a captive portal",
+		Func:         DNSProxyCaptivePortalRelog,
+		Desc:         "Verify dns-proxy behaves correctly when shill detects a captive portal and a re-login is done",
 		Contacts:     []string{"jasongustaman@google.com", "garrick@google.com", "cros-networking@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"chrome"},
+		SoftwareDeps: []string{"chrome", "arc"},
 		Timeout:      5 * time.Minute,
 	})
 }
 
-func DNSProxyCaptivePortal(ctx context.Context, s *testing.State) {
+const arcDNSProxyChain = "redirect_arc_dns"
+
+func DNSProxyCaptivePortalRelog(ctx context.Context, s *testing.State) {
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 30*time.Second)
 	defer cancel()
 
 	// Start Chrome with the dns-proxy feature flags enabled.
-	cr, err := chrome.New(ctx, chrome.EnableFeatures("EnableDnsProxy", "DnsProxyEnableDOH"))
+	cr, err := chrome.New(ctx, chrome.ARCEnabled(), chrome.EnableFeatures("EnableDnsProxy", "DnsProxyEnableDOH"))
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
 	defer cr.Close(cleanupCtx)
+
+	// Start ARC.
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close(cleanupCtx)
 
 	// Ensure connectivity is available.
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
@@ -52,6 +64,14 @@ func DNSProxyCaptivePortal(ctx context.Context, s *testing.State) {
 		return digMatch(ctx, dns.DigProxyIPRE, true)
 	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
 		s.Fatal("Failed to verify system proxy: ", err)
+	}
+
+	// Currently, there is no simple way to do plain-text DNS from ARC.
+	// Check ARC state by checking its DNS redirection rules instead.
+	if exist, err := iptablesRulesExist(ctx, arcDNSProxyChain); err != nil {
+		s.Error("Failed to check if iptables rules exist: ", err)
+	} else if !exist {
+		s.Error("ARC DNS proxy chain does not exist")
 	}
 
 	// Shill's captive portal detector works by probing various endpoints over HTTP and HTTPS.
@@ -83,6 +103,46 @@ func DNSProxyCaptivePortal(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to verify switch over to Google DNS: ", err)
 	}
 
+	// Verify that when the state is in captive portal, ARC redirection rules do not exist.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if exist, err := iptablesRulesExist(ctx, arcDNSProxyChain); err != nil {
+			return err
+		} else if exist {
+			return errors.New("ARC DNS proxy chain exist in captive portal state")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+		s.Error("Failed to get DNS proxy in the correct state: ", err)
+	}
+
+	// Re-start Chrome, emulate a logout and login.
+	cr, err = chrome.New(ctx, chrome.ARCEnabled(), chrome.EnableFeatures("EnableDnsProxy", "DnsProxyEnableDOH"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	// Start ARC.
+	a, err = arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close(cleanupCtx)
+
+	// Verify the system proxy is still not the current nameserver and name resolution works.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		return dns.DigMatch(ctx, dns.DigProxyIPRE, false)
+	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+		s.Fatal("Failed to verify switch over to Google DNS: ", err)
+	}
+
+	// Verify that ARC redirection rules still do not exist.
+	if exist, err := iptablesRulesExist(ctx, arcDNSProxyChain); err != nil {
+		s.Error("Failed to check if iptables rules exist: ", err)
+	} else if exist {
+		s.Error("ARC DNS proxy chain exist in captive portal state")
+	}
+
 	// Unblock the HTTP/S ports which should cause shill's portal detector to succeed, which will
 	// move the connection state back to 'online' and subsequently reconnect dns-proxy.
 	unblock = false
@@ -97,4 +157,25 @@ func DNSProxyCaptivePortal(ctx context.Context, s *testing.State) {
 	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
 		s.Fatal("Failed to verify switchover to system proxy: ", err)
 	}
+
+	// When the state is back to online, verify that ARC redirection rules are re-added.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if exist, err := iptablesRulesExist(ctx, arcDNSProxyChain); err != nil {
+			return err
+		} else if !exist {
+			return errors.New("ARC DNS proxy chain does not exist")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+		s.Error("Failed to get DNS proxy in the correct state: ", err)
+	}
+}
+
+// iptablesRulesExist check if there is at least one rule in the chain.
+func iptablesRulesExist(ctx context.Context, chain string) (bool, error) {
+	out, err := testexec.CommandContext(ctx, "iptables", "-t", "nat", "-S", chain).Output()
+	if err != nil {
+		return false, err
+	}
+	return strings.Count(string(out), "\n") > 1, nil
 }
