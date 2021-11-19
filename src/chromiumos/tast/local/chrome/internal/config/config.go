744185a9885d9b8c3d71a44523a01d5f7dc50644
Ricky Liang <jcliang@chromium.org>
2021-11-19 12:55:57 +0800

    TEST-ONLY: Set keepstate in Chrome.Config
    BUG=None
    TEST=None
    
    Change-Id: I3444ac725c3d9e161caae2e1335bb4772ded2a80

--- src/chromiumos/tast/local/chrome/internal/config/config.go
+++ src/chromiumos/tast/local/chrome/internal/config/config.go
@@ -344,7 +344,7 @@ func NewConfig(opts []Option) (*Config, error) {
 	cfg := &Config{
 		m: MutableConfig{
 			Creds:                           defaultCreds,
-			KeepState:                       false,
+			KeepState:                       true,
 			KeepOwnership:                   false,
 			LoginMode:                       FakeLogin,
 			VKEnabled:                       false,
