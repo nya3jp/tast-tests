6ce9fe5654a662db856edf0f9c92d69c6bd88c89
Edward Hill <ecgh@chromium.org>
2021-11-18 20:57:16 -0700

    ti50: Add Ti50Rescue
    This test uses UART rescue via servo to flash an image.
    
    BUG=none
    TEST=tast run -var=servo=localhost:9999 \
    -var=image=/local_mounts/ti50/common/build/ti50/dauntless/dauntless/full_image.A.signed \
    $fake_dut firmware.Ti50Rescue
    
    Change-Id: Id923e6a5861a1118579c64a89d895216784df794

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/ti50_rescue.go
@@ -0,0 +1,82 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/remote/firmware"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Ti50Rescue,
+		Desc:         "Use UART rescue to flash Ti50 image",
+		Contacts:     []string{"ecgh@chromium.org", "ti50-core@google.com"},
+		Attr:         []string{"group:firmware"},
+		Vars:         []string{"servo", "image"},
+		HardwareDeps: hwdep.D(hwdep.ChromeEC()),
+		Data:         []string{firmware.ConfigFile},
+	})
+}
+
+func Ti50Rescue(ctx context.Context, s *testing.State) {
+	servoSpec := s.RequiredVar("servo")
+	h := firmware.NewHelper(s.DUT(), s.RPCHint(), s.DataPath(firmware.ConfigFile), servoSpec, "", "", "", "")
+	defer h.Close(ctx)
+
+	if err := h.RequireServo(ctx); err != nil {
+		s.Fatal("Failed to connect to servo: ", err)
+	}
+
+	image := s.RequiredVar("image")
+
+	cmd := testexec.CommandContext(ctx, "ls", image)
+	if err := cmd.Run(); err != nil {
+		s.Fatal("Image file not found: ", err)
+	}
+
+	uartdev, err := h.Servo.GetString(ctx, "raw_cr50_uart_pty")
+	if err != nil {
+		s.Fatal("Servo error: ", err)
+	}
+
+	err = h.Servo.SetString(ctx, "cr50_ec3po_interp_connect", "off")
+	if err != nil {
+		s.Fatal("Servo error: ", err)
+	}
+
+	cmd = testexec.CommandContext(ctx, "cr50-rescue", "--dauntless", "-v", "-i", image, "-d", uartdev)
+
+	err = cmd.Start()
+	if err != nil {
+		s.Fatal("Failed to start rescue: ", err)
+	}
+	testing.Sleep(ctx, 2*time.Second)
+
+	err = h.Servo.SetString(ctx, "pch_disable", "on")
+	if err != nil {
+		s.Fatal("Servo error: ", err)
+	}
+	err = h.Servo.SetString(ctx, "pch_disable", "off")
+	if err != nil {
+		s.Fatal("Servo error: ", err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		s.Fatal("Failed to complete rescue: ", err)
+	}
+	cmd.DumpLog(ctx)
+
+	err = h.Servo.SetString(ctx, "cr50_ec3po_interp_connect", "on")
+	if err != nil {
+		s.Fatal("Servo error: ", err)
+	}
+}
