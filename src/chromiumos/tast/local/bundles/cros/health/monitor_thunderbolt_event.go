bbf7eeae3f72bfcb04302b08e9fda36aea214dcb
Pathan Jilani <pathan.jilani@intel.com>
2021-11-19 06:50:49 +0530

    health: add ProbeWifiInfo, MonitorThunderboltEvent, ProbeBusInfo tests
    In this CL we have added variants that support vPro features.
    And we are disabling data access protection for Thunderbolt devices.
    
    Sample command to execute:
    tast run <DUT IP> health.ProbeWifiInfo health.MonitorThunderboltEvent health.ProbeBusInfo.thunderbolt
    
    BUG=b:198745197
    TEST=Ran on DUT locally
    
    Change-Id: I226e69ec70de0a1e0b7e9e45d99dd4d642e6a166

--- src/chromiumos/tast/local/bundles/cros/health/monitor_thunderbolt_event.go
+++ src/chromiumos/tast/local/bundles/cros/health/monitor_thunderbolt_event.go
@@ -16,6 +16,8 @@ import (
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/typec/typecutils"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/croshealthd"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
@@ -29,22 +31,32 @@ func init() {
 			"intel-chrome-system-automation-team@intel.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
-		HardwareDeps: hwdep.D(hwdep.Model("brya")),
-		Fixture:      "crosHealthdRunning",
+		// TODO(b/207569436): Define hardware dependency and get rid of hard-coding the models.
+		HardwareDeps: hwdep.D(hwdep.Model("brya", "redrix", "kano", "anahera", "primus", "crota")),
+		Fixture:      "chromeLoggedIn",
 	})
 }
 
 func MonitorThunderboltEvent(ctx context.Context, s *testing.State) {
 	var (
-		deviceRemoved          = regexp.MustCompile(`Device removed`)
-		deviceAdded            = regexp.MustCompile(`Device added`)
-		outFile                = "/tmp/tbt_logs.txt"
-		thundeBoltMonitorEvent = "sudo nohup cros-health-tool event --category=thunderbolt --length_seconds=600 > " + outFile + " 2>&1 &"
-		pidCmd                 = "ps -aux | grep -i nohup | awk -F' ' '{print $2}' | head -1"
-		killTbtEvent           = "kill -9 $(" + pidCmd + ")"
-		timeOut                = 30 * time.Second
+		deviceRemoved = regexp.MustCompile(`Device removed`)
+		deviceAdded   = regexp.MustCompile(`Device added`)
+		timeOut       = 30 * time.Second
 	)
 
+	out, err := ioutil.TempFile("/tmp/", "tbt_logs.*.txt")
+	if err != nil {
+		s.Fatal("Failed to create temp file: ", err)
+	}
+	outFile := out.Name()
+
+	// For Thunderbolt TR devices ,first we have to disable the data procetion then plug the device.
+	// This Code will work for all TBT(AR,TR) device.
+	cr := s.FixtValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
 	port, err := typecutils.CheckPortsForTBTPartner(ctx)
 	if err != nil {
 		s.Fatal("Failed to determine Thunderbolt device from PD identity: ", err)
@@ -57,10 +69,20 @@ func MonitorThunderboltEvent(ctx context.Context, s *testing.State) {
 	ctxForCleanUp := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
 	defer cancel()
+
 	// Run Cmd in background.
-	if err := testexec.CommandContext(ctx, "sh", "-c", thundeBoltMonitorEvent).Run(); err != nil {
-		s.Fatal("Failed to run monitor event: ", err)
+	cmd := testexec.CommandContext(ctx, "sudo", "nohup", "cros-health-tool", "event", "--category=thunderbolt", "--length_seconds=600", "&")
+	cmd.Stdout = out
+	if err := cmd.Start(); err != nil {
+		s.Fatal("Failed to start command: ", err)
 	}
+
+	go func() {
+		defer cmd.Kill()
+		defer cmd.Wait()
+		<-ctx.Done()
+	}()
+
 	getThunderBoltEventOutput := func() string {
 		output, err := ioutil.ReadFile(outFile)
 		if err != nil {
@@ -75,26 +97,15 @@ func MonitorThunderboltEvent(ctx context.Context, s *testing.State) {
 	defer func() {
 		// Delete tmp file.
 		os.RemoveAll(outFile)
-		if err := testexec.CommandContext(ctxForCleanUp, "sh", "-c", killTbtEvent).Run(); err != nil {
-			s.Log("Failed to kill command thundeBoltMonitorEvent execution: ", err)
-		}
 		if err := testexec.CommandContext(ctxForCleanUp, "ectool", "pdcontrol", "resume", portStr).Run(); err != nil {
 			s.Log("Failed to perform replug: ", err)
 		}
 	}()
 
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		if !deviceRemoved.MatchString(getThunderBoltEventOutput()) {
-			return errors.New("failed to detect deviceRemoved TBT Event")
-		}
-		return nil
+	if err := croshealthd.DisableDataAccessProtection(ctx, tconn); err != nil {
+		s.Fatal("Failed to disable data access protection: ", err)
+	}
 
-	}, &testing.PollOptions{Timeout: timeOut}); err != nil {
-		s.Fatal("Failed to verify no Thunderbolt devices connected after unplug: ", err)
-	}
-	if err := os.Truncate(outFile, 0); err != nil {
-		s.Fatal("Failed to truncate: ", err)
-	}
 	if err := testexec.CommandContext(ctx, "ectool", "pdcontrol", "resume", portStr).Run(); err != nil {
 		s.Fatal("Failed to simulate replug: ", err)
 	}
@@ -105,6 +116,25 @@ func MonitorThunderboltEvent(ctx context.Context, s *testing.State) {
 		return nil
 	}, &testing.PollOptions{Timeout: timeOut}); err != nil {
 		s.Fatal("Failed to verify Thunderbolt devices connected after plug: ", err)
+	}
+
+	// After plug the Thunderbolt device we will validate the output file if thunderbolt device is detected or not,
+	// then we flush the output file so that it should not contain any old data for next iteration.
+	if err := os.Truncate(outFile, 0); err != nil {
+		s.Fatal("Failed to truncate: ", err)
+	}
+
+	if err := testexec.CommandContext(ctx, "ectool", "pdcontrol", "suspend", portStr).Run(); err != nil {
+		s.Fatal("Failed to simulate unplug: ", err)
+	}
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if !deviceRemoved.MatchString(getThunderBoltEventOutput()) {
+			return errors.New("failed to detect deviceRemoved TBT Event")
+		}
+		return nil
+
+	}, &testing.PollOptions{Timeout: timeOut}); err != nil {
+		s.Fatal("Failed to verify no Thunderbolt devices connected after unplug: ", err)
 	}
 
 }
