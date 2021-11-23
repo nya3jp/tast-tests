91ead1d0cb88811b000347c4aee50dfcd714b4cd
Judy Hsiao <judyhsiao@chromium.org>
2021-11-23 15:40:54 +0800

    tast-tests: audio.SoundCardInit: promote test to critical
    https://tastboard.corp.google.com/test/audio.SoundCardInit.recent_reboot
    https://tastboard.corp.google.com/test/audio.SoundCardInit.boot_time_calibration
    https://tastboard.corp.google.com/test/audio.SoundCardInit.exit_success
    
    BUG=b:198550559
    TEST=`tast run ${DUT} audio.SoundCardInit.*`
    
    Change-Id: I5b1ebfbd0133667b1567b1e9df9c8514324d9b45

--- src/chromiumos/tast/local/bundles/cros/audio/sound_card_init.go
+++ src/chromiumos/tast/local/bundles/cros/audio/sound_card_init.go
@@ -25,7 +25,7 @@ func init() {
 		Desc:         "Verfies sound_card_init boot time calibration logic",
 		HardwareDeps: hwdep.D(hwdep.SmartAmp(), hwdep.SkipOnModel("atlas", "nocturne", "volteer2")),
 		Contacts:     []string{"judyhsiao@chromium.org", "cychiang@chromium.org"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		Timeout:      1 * time.Minute,
 		Params: []testing.Param{
 			{
