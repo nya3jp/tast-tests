de7623285cb4edbdb213a7afb1dcf1836f076509
Yuke Liao <liaoyuke@chromium.org>
2021-11-23 04:58:24 +0000

    Revert "Use lacros faillog in ShelfLaunch"
    This reverts commit 9e8d335f6875c86f80091aa65fbca74872f9c160.
    
    Reason for revert:
    
    This CL has a bug, and that lacros.log is not copied over correctly.
    
    Lacros faillog util has the following code:
    // Copy lacros log at the point of failure.
    if err := fsutil.CopyFile(LogFile(ctx), filepath.Join(dir, "lacros.log")); err != nil {
    	testing.ContextLog(ctx, "Failed to save lacros logs: ", err)
    }
    
    func LogFile(ctx context.Context) string {
    	out, ok := testing.ContextOutDir(ctx)
    	if !ok {
    		testing.ContextLog(ctx, "OutDir not found")
    		return ""
    	}
    	return filepath.Join(out, "lacros.log")
    }
    
    which is assuming that lacros.log is in the output dir, this is true for commandline based launches because we intentionally directed stdout and stderr to that file, however, for UI based launches, this is wrong as the lacros.log is written to /home/chronos/user/lacros/lacros.log
    
    Original change's description:
    > Use lacros faillog in ShelfLaunch
    >
    > This will collect some more information, like screenshots. This is
    > important as some recent evidence on the linked bug indicates that the
    > bug may only occur on UI launches.
    >
    > BUG=chromium:1263337
    > TEST=fast_build.sh
    >
    > Change-Id: Ia90295c970e562671b8572df883ba6c64dc1689f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3290779
    > Tested-by: Eliot Courtney <edcourtney@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    
    Bug: chromium:1263337
    Change-Id: I090efb290587334b68daab1099aab830192dab66

--- src/chromiumos/tast/local/bundles/cros/lacros/shelf_launch.go
+++ src/chromiumos/tast/local/bundles/cros/lacros/shelf_launch.go
@@ -7,11 +7,12 @@ package lacros
 import (
 	"context"
 	"os"
+	"path/filepath"
 	"time"
 
+	"chromiumos/tast/fsutil"
 	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/chrome/ash"
-	"chromiumos/tast/local/chrome/lacros/faillog"
 	"chromiumos/tast/local/chrome/lacros/launcher"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
@@ -124,7 +125,10 @@ func ShelfLaunch(ctx context.Context, s *testing.State) {
 	s.Log("Checking that Lacros window is visible")
 	if err := launcher.WaitForLacrosWindow(ctx, tconn, "New Tab"); err != nil {
 		// Grab Lacros logs to assist debugging before exiting.
-		faillog.Save(ctx, s.FixtValue().(launcher.FixtValue).LacrosPath())
+		if errCopy := fsutil.CopyFile(filepath.Join(launcher.LacrosUserDataDir, "lacros.log"), filepath.Join(s.OutDir(), "lacros.log")); errCopy != nil {
+			s.Log("Failed to copy /home/chronos/user/lacros/lacros.log to the OutDir ", errCopy)
+		}
+
 		s.Fatal("Failed waiting for Lacros window to be visible: ", err)
 	}
 
