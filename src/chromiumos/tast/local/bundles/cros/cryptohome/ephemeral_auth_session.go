429af1e9a0d7cbbd589a3872a47fac31e3ea83dc
Daniil Lunev <dlunev@chromium.org>
2021-11-24 10:05:33 +1100

    cryptohome: add tests for new mount interface
    No test for migration yet for the StartMigration call is not plumbed
    through CLI.
    
    BUG=None
    TEST=tast run 192.168.1.7 cryptohome.AuthSessionMount.*
    TEST=tast run 192.168.1.7 cryptohome.GuestAuthSession
    TEST=tast run 192.168.1.7 cryptohome.EphemeralAuthSession
    TEST=tast run 192.168.1.7 cryptohome.PersistentAuthSession
    
    Cq-Depend: chromium:3297256
    Change-Id: Id3281417c6dd4ea56bc2dcb3fa03e081da35da97

---
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/ephemeral_auth_session.go
@@ -0,0 +1,114 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/local/cryptohome"
+	hwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: EphemeralAuthSession,
+		Desc: "Test guest sessions with auth session API",
+		Contacts: []string{
+			"dlunev@chromium.org",
+			"hardikgoyal@chromium.org",
+			"cryptohome-core@chromium.org",
+		},
+		Attr: []string{"group:mainline", "informational"},
+		Data: []string{"testcert.p12"},
+	})
+}
+
+func EphemeralAuthSession(ctx context.Context, s *testing.State) {
+	const (
+		ownerUser       = "owner@owner.owner"
+		userName        = "foo@bar.baz"
+		userPassword    = "secret"
+		testFile        = "file"
+		testFileContent = "content"
+	)
+
+	cmdRunner := hwseclocal.NewCmdRunner()
+	helper, err := hwseclocal.NewHelper(cmdRunner)
+	if err != nil {
+		s.Fatal("Failed to create hwsec local helper: ", err)
+	}
+	daemonController := helper.DaemonController()
+
+	// Ensure cryptohomed is started and wait for it to be available.
+	if err := daemonController.Ensure(ctx, hwsec.CryptohomeDaemon); err != nil {
+		s.Fatal("Failed to ensure cryptohomed: ", err)
+	}
+
+	if err := cryptohome.UnmountAll(ctx); err != nil {
+		s.Fatal("Failed to unmount vaults for preparation: ", err)
+	}
+
+	// Set up the first user as the owner. It is required to mount ephemeral.
+	if err := hwseclocal.SetUpVaultAndUserAsOwner(ctx, s.DataPath("testcert.p12"), ownerUser, "whatever", "whatever", helper.CryptohomeClient()); err != nil {
+		cryptohome.UnmountAll(ctx)
+		cryptohome.RemoveVault(ctx, ownerUser)
+		s.Fatal("Failed to setup vault and user as owner: ", err)
+	}
+	if err := cryptohome.UnmountAll(ctx); err != nil {
+		s.Fatal("Failed to unmount vaults for preparation: ", err)
+	}
+	defer cryptohome.RemoveVault(ctx, ownerUser)
+
+	// Setup an ephemeral session.
+	authSessionID, err := cryptohome.AuthenticateWithAuthSession(ctx, userName, userPassword, true, false)
+	if err != nil {
+		s.Fatal("Failed to authenticate ephemeral user: ", err)
+	}
+	defer cryptohome.InvalidateAuthSession(ctx, authSessionID)
+
+	if err := cryptohome.PrepareEphemeralVault(ctx, authSessionID); err != nil {
+		s.Fatal("Failed to prepare ephemeral vault: ", err)
+	}
+	defer cryptohome.UnmountAll(ctx)
+
+	if err := cryptohome.PrepareEphemeralVault(ctx, authSessionID); err == nil {
+		s.Fatal("Secondary prepare attempt for the same user should fail, but succeeded")
+	}
+
+	// Write a test file to verify non-persistence.
+	userPath, err := cryptohome.UserPath(ctx, userName)
+	if err != nil {
+		s.Fatal("Failed to get user vault path: ", err)
+	}
+
+	filePath := filepath.Join(userPath, testFile)
+	if err := ioutil.WriteFile(filePath, []byte(testFileContent), 0644); err != nil {
+		s.Fatal("Failed to write a file to the vault: ", err)
+	}
+
+	// Unmount and mount again.
+	if err := cryptohome.UnmountAll(ctx); err != nil {
+		s.Fatal("Failed to unmount vaults for re-mounting: ", err)
+	}
+
+	authSessionID, err = cryptohome.AuthenticateWithAuthSession(ctx, userName, userPassword, true, false)
+	if err != nil {
+		s.Fatal("Failed to authenticate ephemeral user: ", err)
+	}
+	defer cryptohome.InvalidateAuthSession(ctx, authSessionID)
+
+	if err := cryptohome.PrepareEphemeralVault(ctx, authSessionID); err != nil {
+		s.Fatal("Failed to prepare ephemeral vault: ", err)
+	}
+
+	// Verify non-persistentce.
+	if _, err := ioutil.ReadFile(filePath); err == nil {
+		s.Fatal("File is persisted across ephemeral session boundary")
+	}
+}
---
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/persistent_auth_session.go
@@ -0,0 +1,113 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"path/filepath"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/local/cryptohome"
+	hwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PersistentAuthSession,
+		Desc: "Test guest sessions with auth session API",
+		Contacts: []string{
+			"dlunev@chromium.org",
+			"hardikgoyal@chromium.org",
+			"cryptohome-core@chromium.org",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func PersistentAuthSession(ctx context.Context, s *testing.State) {
+	const (
+		userName        = "foo@bar.baz"
+		userPassword    = "secret"
+		testFile        = "file"
+		testFileContent = "content"
+	)
+
+	cmdRunner := hwseclocal.NewCmdRunner()
+	helper, err := hwseclocal.NewHelper(cmdRunner)
+	if err != nil {
+		s.Fatal("Failed to create hwsec local helper: ", err)
+	}
+	daemonController := helper.DaemonController()
+
+	// Ensure cryptohomed is started and wait for it to be available.
+	if err := daemonController.Ensure(ctx, hwsec.CryptohomeDaemon); err != nil {
+		s.Fatal("Failed to ensure cryptohomed: ", err)
+	}
+
+	if err := cryptohome.UnmountAll(ctx); err != nil {
+		s.Fatal("Failed to unmount vaults for preparation: ", err)
+	}
+
+	if err := cryptohome.RemoveVault(ctx, userName); err != nil {
+		s.Fatal("Failed to remove old vault for preparation: ", err)
+	}
+
+	if err := cryptohome.CreateUserWithAuthSession(ctx, userName, userPassword, false); err != nil {
+		s.Fatal("Failed to create the user: ", err)
+	}
+	defer cryptohome.RemoveVault(ctx, userName)
+
+	// Mount the vault for the first time.
+	authSessionID, err := cryptohome.AuthenticateWithAuthSession(ctx, userName, userPassword, false, false)
+	if err != nil {
+		s.Fatal("Failed to authenticate persistent user: ", err)
+	}
+	defer cryptohome.InvalidateAuthSession(ctx, authSessionID)
+
+	if err := cryptohome.PreparePersistentVault(ctx, authSessionID, false); err != nil {
+		s.Fatal("Failed to prepare persistent vault: ", err)
+	}
+	defer cryptohome.UnmountAll(ctx)
+
+	if err := cryptohome.PreparePersistentVault(ctx, authSessionID, false); err == nil {
+		s.Fatal("Secondary prepare attempt for the same user should fail, but succeeded")
+	}
+
+	// Write a test file to verify persistence.
+	userPath, err := cryptohome.UserPath(ctx, userName)
+	if err != nil {
+		s.Fatal("Failed to get user vault path: ", err)
+	}
+
+	filePath := filepath.Join(userPath, testFile)
+	if err := ioutil.WriteFile(filePath, []byte(testFileContent), 0644); err != nil {
+		s.Fatal("Failed to write a file to the vault: ", err)
+	}
+
+	// Unmount and mount again.
+	if err := cryptohome.UnmountAll(ctx); err != nil {
+		s.Fatal("Failed to unmount vaults for re-mounting: ", err)
+	}
+
+	authSessionID, err = cryptohome.AuthenticateWithAuthSession(ctx, userName, userPassword, false, false)
+	if err != nil {
+		s.Fatal("Failed to authenticate persistent user: ", err)
+	}
+	defer cryptohome.InvalidateAuthSession(ctx, authSessionID)
+
+	if err := cryptohome.PreparePersistentVault(ctx, authSessionID, false); err != nil {
+		s.Fatal("Failed to prepare persistent vault: ", err)
+	}
+
+	// Verify that file is still there.
+	if content, err := ioutil.ReadFile(filePath); err != nil {
+		s.Fatal("Failed to read back test file: ", err)
+	} else if bytes.Compare(content, []byte(testFileContent)) != 0 {
+		s.Fatalf("Incorrect tests file content. got: %q, want: %q", content, testFileContent)
+	}
+}
