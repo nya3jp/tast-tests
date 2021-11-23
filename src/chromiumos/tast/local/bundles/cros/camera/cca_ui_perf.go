d10748605bb148c19cc80c3cf4769ad237528028
Kuo Jen Wei <inker@google.com>
2021-11-23 08:15:04 +0800

    camera: Add dynamic tiering variant for perf test
    BUG=b:201335131
    TEST=tast run <DUT> camera.CCAUIPerf*
    
    Change-Id: I340ef2975c0b76c4e3c7d4ee090a211363eab854

--- src/chromiumos/tast/local/bundles/cros/camera/cca_ui_perf.go
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_perf.go
@@ -26,7 +26,13 @@ func init() {
 		SoftwareDeps: []string{"camera_app", "chrome", caps.BuiltinOrVividCamera},
 		Data:         []string{"cca_ui.js"},
 		Timeout:      8 * time.Minute,
-		Fixture:      "ccaTestBridgeReady",
+		Params: []testing.Param{{
+			Fixture: "ccaTestBridgeReady",
+		}, {
+			// TODO(b:201335131): Remove dynamic tiering test after the feature fully landed.
+			Name:    "wasm_dynamic_tier",
+			Fixture: "ccaTestBridgeReadyWithWasmDynamicTiering",
+		}},
 	})
 }
 
@@ -34,12 +40,12 @@ func init() {
 // performance through some UI operations.
 func CCAUIPerf(ctx context.Context, s *testing.State) {
 	perfData := cca.NewPerfData()
+	resetChrome := s.FixtValue().(cca.FixtureData).ResetChrome
 
 	// App launch tests.
-	resetChrome := s.FixtValue().(cca.FixtureData).ResetChrome
+	const appLaunchTestTimeout = 60 * time.Second
 	startApp := s.FixtValue().(cca.FixtureData).StartApp
 	stopApp := s.FixtValue().(cca.FixtureData).StopApp
-	const appLaunchTestTimeout = 60 * time.Second
 	appLaunchTestCtx, cancel := context.WithTimeout(ctx, appLaunchTestTimeout)
 	s.Run(appLaunchTestCtx, "testAppLaunch", func(ctx context.Context, s *testing.State) {
 		if err := testAppLaunch(ctx, resetChrome, startApp, stopApp, perfData); err != nil {
