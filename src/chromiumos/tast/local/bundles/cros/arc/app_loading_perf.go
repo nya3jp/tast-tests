c6aa67333f600e84d580240658ad6c2e820f53a7
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2021-11-24 11:14:11 -0800

    arcappcompat: Create GaiaLoginPoolVars for handling testaccountpool
    It is observed that arcappcompat tests are failing in install
     with Authentication error due to Playstore rate limit issue.
    Currently arcappcompat tests are using a single test account
    for most of the apps. Adding TestAccountPool for arcappcompat
    tests will fix the play store rate limit issue as the test
    could use any test account from the testaccount pool.Also
    in "pre" file created a struct GaiaLoginPoolVars for handling
    Testaccountpool so that any test can use their own pool credentials.
    
    BUG=b:197558039
    TEST=tast run <kevin> arcappcompat.ABCKids.clamshell_mode
    tast run <helios> arcappcompat.ABCKids.vm_clamshell_mode
    tast run <eve> arcappgameperf.RobloxLaunch.vm
    tast run <eve> arc.AppLoadingPerf.vm
    
    Cq-Depend: chrome-internal:4234621
    Change-Id: I87a8b905c077a6554168cecece41fa016b2109ae

--- src/chromiumos/tast/local/bundles/cros/arc/app_loading_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/app_loading_perf.go
@@ -34,20 +34,20 @@ var (
 
 	// arcAppLoadingBooted is a precondition similar to arc.Booted(). The only difference from arc.Booted() is
 	// that it disables some heavy post-provisioned Android activities that use system resources.
-	arcAppLoadingBooted = arc.NewPrecondition("arcapploading_booted", arcAppLoadingGaia, false /* O_DIRECT */, append(arc.DisableSyncFlags())...)
+	arcAppLoadingBooted = arc.NewPrecondition("arcapploading_booted", arcAppLoadingGaia, nil /* GAIALOGINPOOLVARS */, false /* O_DIRECT */, append(arc.DisableSyncFlags())...)
 
 	// arcAppLoadingVMBooted is a precondition similar to arc.VMBooted(). The only difference from arc.VMBooted() is
 	// that it disables some heavy post-provisioned Android activities that use system resources.
-	arcAppLoadingVMBooted = arc.NewPrecondition("arcapploading_vmbooted", arcAppLoadingGaia, false /* O_DIRECT */, append(arc.DisableSyncFlags())...)
+	arcAppLoadingVMBooted = arc.NewPrecondition("arcapploading_vmbooted", arcAppLoadingGaia, nil /* GAIALOGINPOOLVARS */, false /* O_DIRECT */, append(arc.DisableSyncFlags())...)
 
 	// arcAppLoadingRtVcpuVMBooted adds feature to boot ARC with realtime vcpu is enabled.
-	arcAppLoadingRtVcpuVMBooted = arc.NewPrecondition("arcapploading_rt_vcpu_vmbooted", arcAppLoadingGaia, false /* O_DIRECT */, append(arc.DisableSyncFlags(), "--enable-features=ArcRtVcpuDualCore,ArcRtVcpuQuadCore")...)
+	arcAppLoadingRtVcpuVMBooted = arc.NewPrecondition("arcapploading_rt_vcpu_vmbooted", arcAppLoadingGaia, nil /* GAIALOGINPOOLVARS */, false /* O_DIRECT */, append(arc.DisableSyncFlags(), "--enable-features=ArcRtVcpuDualCore,ArcRtVcpuQuadCore")...)
 
 	// arcAppLoadingODirectVMBooted enables O_DIRECT for crosvm.
-	arcAppLoadingODirectVMBooted = arc.NewPrecondition("arcapploading_odirect_vmbooted", arcAppLoadingGaia, true /* O_DIRECT */, append(arc.DisableSyncFlags())...)
+	arcAppLoadingODirectVMBooted = arc.NewPrecondition("arcapploading_odirect_vmbooted", arcAppLoadingGaia, nil /* GAIALOGINPOOLVARS */, true /* O_DIRECT */, append(arc.DisableSyncFlags())...)
 
 	// arcAppLoadingDalvikMemoryProfileVMBooted enables ArcUseDalvikMemoryProfile chrome feature.
-	arcAppLoadingDalvikMemoryProfileVMBooted = arc.NewPrecondition("arcapploading_dalvik_memory_profile_vmbooted", arcAppLoadingGaia, false /* O_DIRECT */, append(arc.DisableSyncFlags(), "--enable-features=ArcUseDalvikMemoryProfile")...)
+	arcAppLoadingDalvikMemoryProfileVMBooted = arc.NewPrecondition("arcapploading_dalvik_memory_profile_vmbooted", arcAppLoadingGaia, nil /* GAIALOGINPOOLVARS */, false /* O_DIRECT */, append(arc.DisableSyncFlags(), "--enable-features=ArcUseDalvikMemoryProfile")...)
 )
 
 func init() {
