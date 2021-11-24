cdc8cad73bf4b39b3c8e7419cd62fa830b9e7c7b
Yi Chou <yich@google.com>
2021-11-24 21:08:17 +0800

    hwsec: Add ChapsRemount test
    This test could help us verifieing chaps works correctly after remount.
    
    BUG=b:207604665
    TEST=tast run $DUT hwsec.ChapsRemount #success after crrev.com/c/3298645
    
    Change-Id: I1cde3a26166a9afe5416d656b77640a171f18b69

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/chaps_remount.go
@@ -0,0 +1,104 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/common/pkcs11"
+	"chromiumos/tast/common/pkcs11/pkcs11test"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/hwsec/util"
+	libhwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ChapsRemount,
+		Desc: "Verifies chaps works correctly after remount",
+		Attr: []string{"group:mainline", "informational"},
+		Contacts: []string{
+			"yich@google.com",
+			"cros-hwsec@chromium.org",
+		},
+		Timeout: 4 * time.Minute,
+	})
+}
+
+func ChapsRemount(ctx context.Context, s *testing.State) {
+	r := libhwseclocal.NewCmdRunner()
+
+	helper, err := libhwseclocal.NewHelper(r)
+	if err != nil {
+		s.Fatal("Failed to create hwsec helper: ", err)
+	}
+	cryptohome := helper.CryptohomeClient()
+
+	pkcs11Util, err := pkcs11.NewChaps(ctx, r, cryptohome)
+	if err != nil {
+		s.Fatal("Failed to create PKCS#11 Utility: ", err)
+	}
+
+	const scratchpadPath = "/tmp/ChapsRemountTest"
+
+	// Remove all keys/certs before the test as well.
+	if err := pkcs11test.CleanupScratchpad(ctx, r, scratchpadPath); err != nil {
+		s.Fatal("Failed to clean scratchpad before the start of test: ", err)
+	}
+	util.CleanupKeysBeforeTest(ctx, pkcs11Util, cryptohome)
+
+	// Prepare the scratchpad.
+	f1, f2, err := pkcs11test.PrepareScratchpadAndTestFiles(ctx, r, scratchpadPath)
+	if err != nil {
+		s.Fatal("Failed to initialize the scratchpad space: ", err)
+	}
+	// Remove all keys/certs, if any at the end. i.e. Cleanup after ourselves.
+	defer pkcs11test.CleanupScratchpad(ctx, r, scratchpadPath)
+
+	// Create the various keys.
+	keys, err := util.CreateKeysForTesting(ctx, r, pkcs11Util, cryptohome, scratchpadPath, util.RSAKey)
+	if err != nil {
+		s.Fatal("Failed to create keys for testing: ", err)
+	}
+	defer func() {
+		if err := util.CleanupTestingKeys(ctx, keys, pkcs11Util, cryptohome); err != nil {
+			s.Error("Failed to cleanup testing keys: ", err)
+		}
+	}()
+	// Give the cleanup 10 seconds to finish.
+	shortenedCtx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	// Test the various keys.
+	for _, k := range keys {
+		// Test the various mechanisms.
+		for _, m := range []pkcs11.MechanismInfo{pkcs11.SHA1RSAPKCS, pkcs11.SHA256RSAPKCS} {
+			if err := pkcs11test.SignAndVerify(shortenedCtx, pkcs11Util, k, f1, f2, &m); err != nil {
+				s.Error("SignAndVerify failed: ", err)
+			}
+		}
+	}
+
+	// Remount the cryptohome.
+	if _, err := cryptohome.Unmount(ctx, util.FirstUsername); err != nil {
+		s.Fatal("Failed to unmount the first user: ", err)
+	}
+	if err := cryptohome.MountVault(ctx, util.PasswordLabel, hwsec.NewPassAuthConfig(util.FirstUsername, util.FirstPassword), true, hwsec.NewVaultConfig()); err != nil {
+		s.Fatal("Failed to re-mount the first user: ", err)
+	}
+
+	// Test the various keys again.
+	for _, k := range keys {
+		// Test the various mechanisms.
+		for _, m := range []pkcs11.MechanismInfo{pkcs11.SHA1RSAPKCS, pkcs11.SHA256RSAPKCS} {
+			if err := pkcs11test.SignAndVerify(shortenedCtx, pkcs11Util, k, f1, f2, &m); err != nil {
+				s.Error("SignAndVerify after re-mount failed: ", err)
+			}
+		}
+	}
+}
