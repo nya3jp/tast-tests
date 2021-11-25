f7f4d92c73885d42c2bf5ad466cb15d2d087da04
Victor-Gabriel Savu <vsavu@google.com>
2021-11-25 13:21:30 +0100

    policy: Add test for most update_engine policies
    Most update_engine policies just set an xml attribute, change
    the DeviceTargetVersionSelector test to allow testing of multiple
    policies.
    
    BUG=None
    TEST=tast run <ip> policy.UpdateEnginePolicies
    
    Change-Id: I4a76725f7587ab82d179b3eea97bb6910d7f74c0

---
+++ src/chromiumos/tast/local/bundles/cros/policy/update_engine_policies.go
@@ -0,0 +1,211 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+type updateEngineTestParam struct {
+	// policyValues are the policies that need to be set.
+	policyValues []policy.Policy
+	// policyParam os the xml attribute that needs to be set by update_engine.
+	policyParam string
+	// testValue is the value for the policyParam attribute
+	testValue string
+
+	// Some values are too generic or are always set, allow skipping the check when the policies are unset.
+	// checkParam indicates whether to check for the xml attribute.
+	checkParam bool
+	// checkVal indicates whether to check for the value.
+	checkVal bool
+}
+
+const (
+	deviceTargetVersionSelectorVal = "0,1626155736-"
+	deviceTargetVersionPrefixVal   = "1000."
+	deviceReleaseLtsTagVal         = "lts"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: UpdateEnginePolicies,
+		Desc: "Check of policies are properly propagating to update_engine by checking the logs",
+		Contacts: []string{
+			"vsavu@google.com", // Test author
+			"chromeos-commercial-remote-management@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      fixture.ChromeEnrolledLoggedIn,
+		Timeout:      1 * time.Minute,
+		Params: []testing.Param{{
+			Name: "device_target_version_selector",
+			Val: &updateEngineTestParam{
+				policyValues: []policy.Policy{&policy.DeviceTargetVersionSelector{Val: deviceTargetVersionSelectorVal}},
+				testValue:    deviceTargetVersionSelectorVal,
+				policyParam:  "targetversionselector",
+				checkParam:   true,
+				checkVal:     true,
+			},
+		}, {
+			Name: "device_target_version_prefix",
+			Val: &updateEngineTestParam{
+				policyValues: []policy.Policy{&policy.DeviceTargetVersionPrefix{Val: deviceTargetVersionPrefixVal}},
+				testValue:    deviceTargetVersionPrefixVal,
+				policyParam:  "targetversionprefix",
+				checkParam:   true,
+				checkVal:     true,
+			},
+		}, {
+			Name: "device_release_lts_tag",
+			Val: &updateEngineTestParam{
+				policyValues: []policy.Policy{&policy.DeviceReleaseLtsTag{Val: deviceReleaseLtsTagVal}},
+				testValue:    deviceReleaseLtsTagVal,
+				policyParam:  "ltstag",
+				checkParam:   true,
+			},
+		}, {
+			Name: "device_rollback_to_target_version",
+			Val: &updateEngineTestParam{
+				policyValues: []policy.Policy{
+					&policy.DeviceTargetVersionPrefix{Val: deviceTargetVersionPrefixVal},
+					&policy.DeviceRollbackToTargetVersion{Val: 2},
+				},
+				testValue:   "true",
+				policyParam: "rollback_allowed",
+			},
+		}},
+	})
+}
+
+func UpdateEnginePolicies(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	param := s.Param().(*updateEngineTestParam)
+
+	const waitTime = 10 * time.Second
+
+	const updateEngineLog = "/var/log/update_engine.log"
+
+	// Restart update-engine after clearing policies.
+	defer upstart.RestartJob(ctx, "update-engine")
+	defer policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{})
+
+	// Restart update engine, clear the logs and request an update.
+	clearAndUpdate := func(ctx context.Context) {
+		ctx, cancel := context.WithTimeout(ctx, 20*time.Second)
+		defer cancel()
+
+		if err := upstart.StopJob(ctx, "update-engine"); err != nil {
+			s.Fatal("Failed to stop update_engine: ", err)
+		}
+
+		realLog, err := os.Readlink(updateEngineLog)
+		if err != nil {
+			s.Fatal("Failed to find the real update_engine log: ", err)
+		}
+
+		if err := os.Remove(realLog); err != nil {
+			s.Fatal("Failed to clear update_engine logs: ", err)
+		}
+
+		if err := os.Remove(updateEngineLog); err != nil {
+			s.Fatal("Failed to clear update_engine logs: ", err)
+		}
+
+		if err := upstart.StartJob(ctx, "update-engine"); err != nil {
+			s.Fatal("Failed to start update_engine: ", err)
+		}
+
+		// update_engine is not ready right after start.
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			// Make sure update_engine_client does not hang.
+			ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+			defer cancel()
+
+			if err := testexec.CommandContext(ctx, "update_engine_client", "--check_for_update").Run(testexec.DumpLogOnError); err != nil {
+				return err
+			}
+
+			return nil
+		}, nil); err != nil {
+			s.Fatal("Failed to trigger update check: ", err)
+		}
+	}
+
+	// Set the policy and check that the attribute is set.
+	if err := policyutil.ServeAndVerify(ctx, fdms, cr, param.policyValues); err != nil {
+		s.Fatal("Failed to update policies: ", err)
+	}
+	clearAndUpdate(ctx)
+
+	attributeEntry := param.policyParam + "=\"" + param.testValue + "\""
+	s.Log("Waiting for the log entry to show up")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		dat, err := ioutil.ReadFile("/var/log/update_engine.log")
+		if err != nil {
+			s.Fatal("Failed to read update_engine logs: ", err)
+		}
+
+		if err := ioutil.WriteFile(filepath.Join(s.OutDir(), "set_log.txt"), dat, 0644); err != nil {
+			s.Error("Failed to dump update_engine logs: ", err)
+		}
+
+		if !strings.Contains(string(dat), attributeEntry) {
+			return errors.Errorf("%q not in the update_engine logs", attributeEntry)
+		}
+
+		return nil
+	}, &testing.PollOptions{
+		Timeout: waitTime,
+	}); err != nil {
+		s.Error("Could not find expected values: ", err)
+	}
+
+	// Clear policies to make sure attribute is not always sent.
+	if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{}); err != nil {
+		s.Fatal("Failed to clear policies: ", err)
+	}
+	clearAndUpdate(ctx)
+
+	s.Log("Waiting for update_engine to have a chance to log")
+	if err := testing.Sleep(ctx, waitTime); err != nil {
+		s.Fatal("Failed to wait for messages: ", err)
+	}
+
+	dat, err := ioutil.ReadFile("/var/log/update_engine.log")
+	if err != nil {
+		s.Fatal("Failed to read update_engine logs: ", err)
+	}
+
+	if err := ioutil.WriteFile(filepath.Join(s.OutDir(), "unset_log.txt"), dat, 0644); err != nil {
+		s.Error("Failed to dump update_engine logs: ", err)
+	}
+
+	if param.checkParam && strings.Contains(string(dat), param.policyParam) {
+		s.Errorf("Unexpectedly found %q in the update_engine logs", param.policyParam)
+	}
+
+	if param.checkVal && strings.Contains(string(dat), param.testValue) {
+		s.Errorf("Unexpectedly found test value %q in the update_engine logs", param.testValue)
+	}
+}
