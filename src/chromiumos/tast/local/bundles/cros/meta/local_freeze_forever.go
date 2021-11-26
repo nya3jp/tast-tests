a3a8b0f30ea6c21e4cc3a94dd99bbd0b3fc52e3f
Keigo Oka <oka@chromium.org>
2021-11-26 10:07:00 +0000

    meta: Test stale bundles are killed
    Add a new test which asserts stale bundles are killed when
    new tests are run.
    
    BUG=b:187957164
    TEST=fast_build.sh -T
    
    Change-Id: I0359f9cf47868795f520041256546fd1e12e1e6c

---
+++ src/chromiumos/tast/local/bundles/cros/meta/local_freeze_forever.go
@@ -0,0 +1,28 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package meta
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     LocalFreezeForever,
+		Desc:     "Always freezes forever",
+		Contacts: []string{"tast-owners@google.com"},
+		Timeout:  100 * time.Hour,
+	})
+}
+
+func LocalFreezeForever(ctx context.Context, s *testing.State) {
+	// Log for meta test to detect start of the test.
+	s.Log("LocalFreezeForever started")
+	var ch chan struct{}
+	<-ch
+}
