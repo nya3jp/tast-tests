72c5d1ad96279da9d99a556c2d898dbc9401431f
Keigo Oka <oka@chromium.org>
2021-11-26 10:07:00 +0000

    meta: Test stale bundles are killed
    Add a new test which asserts stale bundles are killed when
    new tests are run.
    
    BUG=b:187957164
    TEST=fast_build.sh -T
    
    Change-Id: I0359f9cf47868795f520041256546fd1e12e1e6c

---
+++ src/chromiumos/tast/remote/bundles/cros/meta/run_tests_kill_stale_bundles.go
@@ -0,0 +1,78 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package meta
+
+import (
+	"bufio"
+	"context"
+	"io"
+	"strings"
+	"time"
+
+	"chromiumos/tast/remote/bundles/cros/meta/tastrun"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     RunTestsKillStaleBundles,
+		Desc:     "Verifies that Tast run kills already running local bundles",
+		Attr:     []string{"group:mainline", "informational"},
+		Contacts: []string{"oka@chromium.org", "tast-core@google.com"},
+	})
+}
+
+func RunTestsKillStaleBundles(ctx context.Context, s *testing.State) {
+	cmd, err := tastrun.NewCommand(ctx, s, "run", nil, []string{"meta.LocalFreezeForever"})
+	if err != nil {
+		s.Fatal("Creating first command to run tast: ", err)
+	}
+	r, w := io.Pipe()
+	cmd.Stdout = w
+
+	s.Log("Starting ", strings.Join(cmd.Args, " "))
+	if err := cmd.Start(); err != nil {
+		s.Fatal("Starting tast: ", err)
+	}
+	defer func() { // avoid leaving a zombie process
+		cmd.Process.Kill()
+		cmd.Wait()
+	}()
+	sc := bufio.NewScanner(r)
+
+	started := make(chan struct{})
+	go func() {
+		for sc.Scan() {
+			line := sc.Text()
+			s.Log(line)
+			if strings.Contains(line, "LocalFreezeForever started") {
+				close(started)
+			}
+		}
+	}()
+
+	<-started
+	_, _, err = tastrun.Exec(ctx, s, "run", nil, []string{"meta.LocalPass"})
+	if err != nil {
+		s.Fatal("Second tast run failed: ", err)
+	}
+
+	shortCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	done := make(chan struct{})
+	go func() {
+		// Already running bundle should be killed when a new bundle is run, and
+		// tast should terminate.
+		cmd.Wait()
+		close(done)
+	}()
+	select {
+	case <-done:
+		// OK
+	case <-shortCtx.Done():
+		s.Fatal("The first command did not terminate")
+	}
+}
