c49a717acb618a64eea495f7fb8fd86c266f5ca9
Kalvin Lee <kdlee@chromium.org>
2021-11-29 14:06:53 -0800

    usbprinter: Refactor usbprinter.Start()
    This CL
    
    *   introduces the new Printer struct that holds the running
        virtual-usb-printer process and
    *   refactors usbprinter.Start() to use the functional options
        pattern, returning a Printer instead of a raw command.
    
    BUG=b:206732481
    TEST=tast run <dut> '("dep:virtual_usb_printer")'
    
    Change-Id: Iab2c8527983ca3dc2074420b9cd997dadd769e54

--- src/chromiumos/tast/local/bundles/cros/printer/add_usb_printer.go
+++ src/chromiumos/tast/local/bundles/cros/printer/add_usb_printer.go
@@ -28,24 +28,18 @@ func init() {
 }
 
 func AddUSBPrinter(ctx context.Context, s *testing.State) {
-	// Path to JSON descriptors file
-	const descriptors = "/usr/local/etc/virtual-usb-printer/usb_printer.json"
-
 	if err := printer.ResetCups(ctx); err != nil {
 		s.Fatal("Failed to reset cupsd: ", err)
 	}
 
-	devInfo, err := usbprinter.LoadPrinterIDs(descriptors)
-	if err != nil {
-		s.Fatalf("Failed to load printer IDs from %v: %v", descriptors, err)
-	}
-
-	printer, err := usbprinter.Start(ctx, devInfo, descriptors, "", "")
+	pr, err := usbprinter.Start(ctx,
+		usbprinter.WithDescriptors("usb_printer.json"))
+	defer func() {
+		if err := pr.Stop(ctx); err != nil {
+			s.Error("Failed to stop virtual printer: ", err)
+		}
+	}()
 	if err != nil {
 		s.Fatal("Failed to attach virtual printer: ", err)
 	}
-
-	// Test cleanup.
-	printer.Kill()
-	printer.Wait()
 }
