e0c91e686959424e3fe4311a422f97372ff8221d
Shuhei Takahashi <nya@chromium.org>
2021-11-29 17:23:42 +0900

    DO NOT SUBMIT: Unit tests for combined cros bundle
    Change-Id: I93da98b849d2754e7c5882d4b28babe5566fce1e

--- src/chromiumos/tast/remote/bundles/cros/main.go
+++ src/chromiumos/tast/remote/bundles/cros/main.go
@@ -9,40 +9,6 @@ package main
 
 import (
 	"chromiumos/tast/remote/bundlemain"
-
-	// These packages register their tests via init functions.
-	_ "chromiumos/tast/remote/bundles/cros/arc"
-	_ "chromiumos/tast/remote/bundles/cros/autoupdate"
-	_ "chromiumos/tast/remote/bundles/cros/camera"
-	_ "chromiumos/tast/remote/bundles/cros/crash"
-	_ "chromiumos/tast/remote/bundles/cros/enterprise"
-	_ "chromiumos/tast/remote/bundles/cros/example"
-	_ "chromiumos/tast/remote/bundles/cros/factory"
-	_ "chromiumos/tast/remote/bundles/cros/filemanager"
-	_ "chromiumos/tast/remote/bundles/cros/firmware"
-	_ "chromiumos/tast/remote/bundles/cros/hardware"
-	_ "chromiumos/tast/remote/bundles/cros/hwsec"
-	_ "chromiumos/tast/remote/bundles/cros/kernel"
-	_ "chromiumos/tast/remote/bundles/cros/lacros"
-	_ "chromiumos/tast/remote/bundles/cros/meta"
-	_ "chromiumos/tast/remote/bundles/cros/nearbyshare"
-	_ "chromiumos/tast/remote/bundles/cros/network"
-	_ "chromiumos/tast/remote/bundles/cros/network/allowlist"
-	_ "chromiumos/tast/remote/bundles/cros/omaha"
-	_ "chromiumos/tast/remote/bundles/cros/platform"
-	_ "chromiumos/tast/remote/bundles/cros/policy"
-	_ "chromiumos/tast/remote/bundles/cros/power"
-	_ "chromiumos/tast/remote/bundles/cros/security"
-	_ "chromiumos/tast/remote/bundles/cros/syzcorpus"
-	_ "chromiumos/tast/remote/bundles/cros/syzkaller"
-	_ "chromiumos/tast/remote/bundles/cros/typec"
-	_ "chromiumos/tast/remote/bundles/cros/ui"
-	_ "chromiumos/tast/remote/bundles/cros/usbc"
-	_ "chromiumos/tast/remote/bundles/cros/wifi"
-	_ "chromiumos/tast/remote/bundles/cros/wilco"
-
-	_ "chromiumos/tast/remote/bundles/cros/factory/fixture"
-	_ "chromiumos/tast/remote/meta" // import fixture for meta tests
 )
 
 func main() {
---
+++ src/chromiumos/tast/restrictions/cros/imports_remote.go
@@ -0,0 +1,1 @@
+../../remote/bundles/cros/imports.go
