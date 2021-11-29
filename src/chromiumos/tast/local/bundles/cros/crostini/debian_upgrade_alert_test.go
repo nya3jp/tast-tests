7a5eccac63766c3a1e5546ed0bf0ac39cd15c92f
Zubin Pratap <zubinpratap@google.com>
2021-11-29 03:55:49 +0000

    crostini: Debian OS Upgrade UI alert.
    Automated test to check that the UI alerts the user that the
    crostini linux container is no longer supported and should be
    upgraded to the latest version. This is achieved by creating a
    mock /etc/os-release file that refers to stretch, then
    restarting the crostini container to trigger the ui alert.
    
    Test params are generated using the minimal set, which for now
    is Buster only. When Debian OS next releases a new stable, this
    test will need to be updated.
    
    On cleanup the original os-release file that is symlinked
    to /usr/lib/os-release is restored.
    
    BUG=b:205193011
    TEST=Ran on hatch DUT and ony hatch lab DUTs.
    
    Change-Id: I12a51f41ec3d81d1e6224c16876f5b28f06ed7b9

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/debian_upgrade_alert_test.go
@@ -0,0 +1,27 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+// To update test parameters after modifying this file, run:
+// TAST_GENERATE_UPDATE=1 ~/trunk/src/platform/tast/tools/go.sh test -count=1 chromiumos/tast/local/bundles/cros/crostini/
+
+// See src/chromiumos/tast/local/crostini/params.go for more documentation
+
+import (
+	"testing"
+	"time"
+
+	"chromiumos/tast/common/genparams"
+	"chromiumos/tast/local/crostini"
+)
+
+func TestDebianUpgradeAlertParams(t *testing.T) {
+	params := crostini.MakeTestParamsFromList(t, []crostini.Param{{
+		Timeout:            14 * time.Minute,
+		MinimalSet:         true,
+		SelfManagedInstall: false,
+	}})
+	genparams.Ensure(t, "debian_upgrade_alert.go", params)
+}
