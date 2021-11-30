f83b98628f18f8faf19fb04fd3ec1a70bf2c6419
Kerker Yang <kerker@google.com>
2021-11-30 10:39:14 +0800

    health: add MonitorUsbEvent test
    Monitors the USB event detected properly or not by using `udevadm` to
    trigger the event.
    
    BUG=b:203753639
    TEST=tast run $IP health.MonitorUsbEvent
    
    Cq-Depend: chromium:3085700
    Change-Id: Ia2906da33446da7bea0f1f45c01c5c83d34c33a3

---
+++ src/chromiumos/tast/local/bundles/cros/health/monitor_usb_event.go
@@ -0,0 +1,58 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package health
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"regexp"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MonitorUsbEvent,
+		Desc:         "Monitors the USB event detected properly or not",
+		Contacts:     []string{"cros-tdm-tpe-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "crosHealthdRunning",
+	})
+}
+
+func MonitorUsbEvent(ctx context.Context, s *testing.State) {
+	outFile, err := ioutil.TempFile("", "usb_logs")
+	if err != nil {
+		s.Fatal("Failed to create temp file: ", err)
+	}
+	defer os.Remove(outFile.Name())
+
+	// Run monitor command in background.
+	monitorCmd := testexec.CommandContext(ctx, "cros-health-tool", "event", "--category=usb", "--length_seconds=10")
+	monitorCmd.Stdout = outFile
+	monitorCmd.Stderr = outFile
+	if err := monitorCmd.Start(); err != nil {
+		outFile.Close()
+		s.Fatal("Failed to run healthd monitor command: ", err)
+	}
+
+	// Trigger USB event.
+	if err := testexec.CommandContext(ctx, "udevadm", "trigger", "-s", "usb", "-c", "add").Run(); err != nil {
+		s.Fatal("Failed to trigger usb add event: ", err)
+	}
+
+	monitorCmd.Wait()
+	outFile.Close()
+
+	deviceAddedPattern := regexp.MustCompile(`"event": "Add"`)
+	if output, err := ioutil.ReadFile(outFile.Name()); err != nil {
+		s.Fatal("Failed to read output data: ", err)
+	} else if !deviceAddedPattern.MatchString(string(output)) {
+		s.Fatal("Failed to detect USB Event, event output: ", string(output))
+	}
+}
