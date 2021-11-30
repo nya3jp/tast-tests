e80aaae1144527923d59dbcbf2c82787bf0d0932
Ching-Kang Yen <chingkang@google.com>
2021-11-30 15:44:41 +0800

    tast-tests: introduce cross_version_login_data.go
    Introduce several functions for cross-version login testing in
    `cross_Version_login_data.go`, that will be used to save and load the
    data.
    
    BUG=b:195775720
    TEST=None
    
    Change-Id: Idf08874c85865d579b34a2eec0e4270e118b53fe

---
+++ src/chromiumos/tast/local/hwsec/cross_version_login_data.go
@@ -0,0 +1,109 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"crypto/rsa"
+	"os"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// CrossVersionLoginConfig contains the information for cross-version login
+type CrossVersionLoginConfig struct {
+	AuthConfig hwsec.AuthConfig
+	RsaKey     *rsa.PrivateKey
+	KeyLabel   string
+}
+
+// NewPassAuthCrossVersionLoginConfig creates cross version-login config from password auth config
+func NewPassAuthCrossVersionLoginConfig(authConfig *hwsec.AuthConfig, keyLabel string) *CrossVersionLoginConfig {
+	config := &CrossVersionLoginConfig{}
+	config.AuthConfig = *authConfig
+	config.KeyLabel = keyLabel
+	return config
+}
+
+// NewChallengeAuthCrossVersionLoginConfig creates cross-version login config from challenge auth config and rsa key
+func NewChallengeAuthCrossVersionLoginConfig(authConfig *hwsec.AuthConfig, keyLabel string, rsaKey *rsa.PrivateKey) *CrossVersionLoginConfig {
+	config := &CrossVersionLoginConfig{}
+	config.AuthConfig = *authConfig
+	config.KeyLabel = keyLabel
+	config.RsaKey = rsaKey
+	return config
+}
+
+// CreateCrossVersionLoginData creates the compressed file of data that is used in cross-version login test.
+func CreateCrossVersionLoginData(ctx context.Context, daemonController *hwsec.DaemonController, filePath string) error {
+	if err := stopHwsecDaemons(ctx, daemonController); err != nil {
+		return err
+	}
+	defer ensureHwsecDaemons(ctx, daemonController)
+
+	args := []string{
+		"--exclude=/home/.shadow/*/mount",
+		"-Jcvf",
+		filePath,
+		"/mnt/stateful_partition/unencrypted/tpm2-simulator/NVChip",
+		"/home/.shadow/",
+	}
+	if err := testexec.CommandContext(ctx, "tar", args...).Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "failed to compress the cryptohome data")
+	}
+	return nil
+}
+
+// LoadCrossVersionLoginData loads the data that is used in cross-version login test.
+func LoadCrossVersionLoginData(ctx context.Context, daemonController *hwsec.DaemonController, filePath string) error {
+	if err := stopHwsecDaemons(ctx, daemonController); err != nil {
+		return err
+	}
+	defer ensureHwsecDaemons(ctx, daemonController)
+
+	// Remove the `/home/.shadow` first to prevent any unexpected file remaining.
+	if err := os.RemoveAll("/home/.shadow"); err != nil {
+		return errors.Wrap(err, "failed to remove old data")
+	}
+
+	if err := testexec.CommandContext(ctx, "tar", "Jxvf", filePath, "-C", "/").Run(); err != nil {
+		return errors.Wrap(err, "failed to decompress the cryptohome data")
+	}
+	return nil
+}
+
+func stopHwsecDaemons(ctx context.Context, daemonController *hwsec.DaemonController) error {
+	if err := daemonController.TryStop(ctx, hwsec.UIDaemon); err != nil {
+		return errors.Wrap(err, "failed to try to stop UI")
+	}
+	if err := daemonController.TryStopDaemons(ctx, hwsec.HighLevelTPMDaemons); err != nil {
+		return errors.Wrap(err, "failed to try to stop high-level TPM daemons")
+	}
+	if err := daemonController.TryStopDaemons(ctx, hwsec.LowLevelTPMDaemons); err != nil {
+		return errors.Wrap(err, "failed to try to stop low-level TPM daemons")
+	}
+	if err := daemonController.TryStop(ctx, hwsec.TPM2SimulatorDaemon); err != nil {
+		return errors.Wrap(err, "failed to try to stop tpm2-simulator")
+	}
+	return nil
+}
+
+func ensureHwsecDaemons(ctx context.Context, daemonController *hwsec.DaemonController) {
+	if err := daemonController.Ensure(ctx, hwsec.TPM2SimulatorDaemon); err != nil {
+		testing.ContextLog(ctx, "Failed to ensure tpm2-simulator: ", err)
+	}
+	if err := daemonController.EnsureDaemons(ctx, hwsec.LowLevelTPMDaemons); err != nil {
+		testing.ContextLog(ctx, "Failed to ensure low-level TPM daemons: ", err)
+	}
+	if err := daemonController.EnsureDaemons(ctx, hwsec.HighLevelTPMDaemons); err != nil {
+		testing.ContextLog(ctx, "Failed to ensure high-level TPM daemons: ", err)
+	}
+	if err := daemonController.Ensure(ctx, hwsec.UIDaemon); err != nil {
+		testing.ContextLog(ctx, "Failed to ensure UI: ", err)
+	}
+}
