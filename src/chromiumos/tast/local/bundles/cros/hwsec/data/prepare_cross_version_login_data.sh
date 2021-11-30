4360011299c9dc7a8e78f65d82500f02986a2659
Ching-Kang Yen <chingkang@google.com>
2021-11-30 15:43:22 +0800

    tast-tests: add cross-version login data preparation
    Add tast-tests and scripts to generate the data used in cross-version
    login data.
    
    BUG=b:195775720
    TEST=bash prepare_cross_version_login_data.sh <testing_rsa> betty \
         R91-13904.0.0
    
    Change-Id: Ifcce87f2f116c4a03cc5018dae3bffff0a915675

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/data/prepare_cross_version_login_data.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+error() {
+  echo "[Error] $(basename "$0"): $*" >&2
+}
+
+die() {
+  error "$*"
+  exit 1
+}
+
+KEY_FILE="$1"
+BOARD="$2"
+VERSION="$3"
+
+HOST="127.0.0.1"
+PORT="9222"
+
+DATE="$(date +"%Y%m%d")"
+BASE_DIR="$(dirname "${BASH_SOURCE[0]}")"
+DATA_DIR="${BASE_DIR}/cross_version_login"
+TMP_DIR="/tmp/cross_version_login"
+IMAGE="${TMP_DIR}/chromiumos_test_image.bin"
+
+test -f "${KEY_FILE}" || die "keyfile '${KEY_FILE}' does not exist"
+test -n "${BOARD}" || die "missing board"
+test -n "${VERSION}" || die "missing version"
+
+# Generates the content of the external data file for tast-tests.
+generate_external_file() {
+  local gs_url="$1"
+  local filepath="$2"
+  cat <<EOF
+{
+  "url": "${gs_url}",
+  "size": $(stat -c %s "${filepath}"),
+  "sha256sum": "$(sha256sum "${filepath}" | cut -d ' ' -f 1)"
+}
+EOF
+}
+
+# Gets the image of target version from Google Cloud Storage.
+fetch_image() {
+  local compressed_image="${TMP_DIR}/image.tar.xz"
+  local gs_url="gs://chromeos-image-archive/${BOARD}-release/${VERSION}/chromiumos_test_image.tar.xz"
+  if ! gsutil cp "${gs_url}" "${compressed_image}"; then
+    error "gsutil failed to cp '${gs_url}' '${compressed_image}'"
+    return 1
+  fi
+  if ! tar Jxvf "${compressed_image}" -C "${TMP_DIR}" ; then
+    error "failed to decompress the '${compressed_image}'"
+    return 1
+  fi
+  return 0
+}
+
+# Uploads the data to the Google Cloud Storage and generate the external data
+# file for tast-tests.
+upload_data() {
+  local prefix="${VERSION}_${BOARD}_${DATE}"
+
+  local data_name="${prefix}_data.tar.xz"
+  local data_path="${TMP_DIR}/${data_name}"
+  local remote_data_path="${TMP_DIR}/data.tar.xz"
+
+  local config_name="${prefix}_config.json"
+  local config_path="${DATA_DIR}/${config_name}"
+  local remote_config_path="${TMP_DIR}/config.json"
+
+  local external_file="${DATA_DIR}/${data_name}.external"
+  local gs_url="gs://chromiumos-test-assets-public/tast/cros/hwsec/cross_version_login/${data_name}"
+
+  if ! scp -o StrictHostKeyChecking=no -i "${KEY_FILE}" -P "${PORT}" \
+      "root@${HOST}:${remote_data_path}" "${data_path}"
+  then
+    error "failed to scp the file '${data_path}'"
+    return 1
+  fi
+
+  if ! scp -o StrictHostKeyChecking=no -i "${KEY_FILE}" -P "${PORT}" \
+      "root@${HOST}:${remote_config_path}" "${config_path}"
+  then
+    error "failed to scp the file '${config_path}'"
+    return 1
+  fi
+
+  if ! generate_external_file "${gs_url}" "${data_path}" > "${external_file}"
+  then
+    error "failed to write the file '${external_file}'"
+    return 1
+  fi
+  if ! gsutil cp "${data_path}" "${gs_url}" ; then
+    error "gsutil failed to cp '${data_path}' '${gs_url}'"
+    return 1
+  fi
+  return 0
+}
+
+# Creates and upload the data file for cross-version login testing.
+prepare_data() (
+  local test_name="hwsec.PrepareCrossVersionLoginData"
+  if ! tast run -failfortests "${HOST}:${PORT}" "${test_name}" ; then
+    error "tast failed to run ${test_name}"
+    return 1
+  fi
+  upload_data
+)
+
+mkdir -p "${TMP_DIR}" || die "failed to mkdir ${TMP_DIR}"
+if fetch_image ; then
+  if cros_vm --log-level=warning --start --image-path="${IMAGE}" \
+               --board="${BOARD}" ;
+  then
+    prepare_data
+    cros_vm --log-level=warning --stop
+  else
+    error "cros_vm failed to start"
+  fi
+fi
+rm -rf "${TMP_DIR}"
---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/prepare_cross_version_login_data.go
@@ -0,0 +1,155 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/json"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
+
+	cpb "chromiumos/system_api/cryptohome_proto"
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/dbusutil"
+	hwseclocal "chromiumos/tast/local/hwsec"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PrepareCrossVersionLoginData,
+		Desc: "Create data for cross-version login test",
+		Contacts: []string{
+			"chingkang@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func createChallengeResponseData(ctx context.Context, lf hwseclocal.LogFunc, cryptohome *hwsec.CryptohomeClient) (*hwseclocal.CrossVersionLoginConfig, error) {
+	const (
+		dbusName    = "org.chromium.TestingCryptohomeKeyDelegate"
+		testUser    = "testuser"
+		keyLabel    = hwseclocal.CrossVersionLoginKeyLabel
+		keySizeBits = 2048
+		keyAlg      = cpb.ChallengeSignatureAlgorithm_CHALLENGE_RSASSA_PKCS1_V1_5_SHA1
+	)
+	randReader := rand.New(rand.NewSource(0 /* seed */))
+	rsaKey, err := rsa.GenerateKey(randReader, keySizeBits)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to generate RSA key")
+	}
+	pubKeySPKIDER, err := x509.MarshalPKIXPublicKey(&rsaKey.PublicKey)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to generate SubjectPublicKeyInfo")
+	}
+
+	dbusConn, err := dbusutil.SystemBus()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect to system D-Bus bus")
+	}
+	if _, err := dbusConn.RequestName(dbusName, 0 /* flags */); err != nil {
+		return nil, errors.Wrap(err, "failed to request the well-known D-Bus name")
+	}
+	defer dbusConn.ReleaseName(dbusName)
+
+	keyDelegate, err := hwseclocal.NewCryptohomeKeyDelegate(
+		lf, dbusConn, testUser, keyAlg, rsaKey, pubKeySPKIDER)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to export D-Bus key delegate")
+	}
+	defer keyDelegate.Close()
+
+	authConfig := hwsec.NewChallengeAuthConfig(testUser, dbusName, keyDelegate.DbusPath, pubKeySPKIDER, keyAlg)
+	// Create the challenge-response protected cryptohome.
+	if err := cryptohome.MountVault(ctx, keyLabel, authConfig, true, hwsec.NewVaultConfig()); err != nil {
+		return nil, errors.Wrap(err, "failed to create cryptohome")
+	}
+	if keyDelegate.ChallengeCallCnt == 0 {
+		return nil, errors.New("no key challenges made during mount")
+	}
+
+	// Authenticate while the cryptohome is still mounted (modeling the case of
+	// the user unlocking the device from the Lock Screen).
+	if _, err := cryptohome.CheckVault(ctx, keyLabel, authConfig); err != nil {
+		return nil, errors.Wrap(err, "failed to check the key for the mounted cryptohome")
+	}
+	return hwseclocal.NewChallengeAuthCrossVersionLoginConfig(authConfig, rsaKey), nil
+}
+
+func createPasswordData(ctx context.Context) (*hwseclocal.CrossVersionLoginConfig, error) {
+	// Add the new password login data
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to log in by Chrome")
+	}
+	cr.Close(ctx)
+	authConfig := hwsec.NewPassAuthConfig(cr.Creds().User, cr.Creds().Pass)
+	return hwseclocal.NewPassAuthCrossVersionLoginConfig(authConfig), nil
+}
+
+func PrepareCrossVersionLoginData(ctx context.Context, s *testing.State) {
+	cmdRunner := hwseclocal.NewLoglessCmdRunner()
+	helper, err := hwseclocal.NewHelper(cmdRunner)
+	if err != nil {
+		s.Fatal("Failed to create hwsec local helper: ", err)
+	}
+	daemonController := helper.DaemonController()
+	cryptohome := hwsec.NewCryptohomeClient(cmdRunner)
+
+	s.Log("Restarting ui job")
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Fatal("Failed to restart ui job: ", err)
+	}
+
+	var configList []hwseclocal.CrossVersionLoginConfig
+
+	if config, err := createPasswordData(ctx); err != nil {
+		s.Fatal("Failed to create password data: ", err)
+	} else {
+		configList = append(configList, *config)
+	}
+
+	s.Log("Restarting ui job")
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Fatal("Failed to restart ui job: ", err)
+	}
+
+	if config, err := createChallengeResponseData(ctx, s.Logf, cryptohome); err != nil {
+		// We could not use latest tast to create challenge-response data before R96, so here we only log the error.
+		s.Log("Failed to create challenge-response data: ", err)
+	} else {
+		configList = append(configList, *config)
+	}
+
+	// Create compressed data
+	tmpDir := "/tmp/cross_version_login"
+	dataPath := filepath.Join(tmpDir, "data.tar.xz")
+	if err := os.MkdirAll(tmpDir, 0700); err != nil {
+		s.Fatalf("Failed to create directory %q: %v", tmpDir, err)
+	}
+
+	if err := hwseclocal.CreateCrossVersionLoginData(ctx, daemonController, dataPath); err != nil {
+		s.Fatal("Failed to create cross-version-login data: ", err)
+	}
+
+	// Create cross-version-login config
+	configPath := filepath.Join(tmpDir, "config.json")
+	configJSON, err := json.MarshalIndent(configList, "", "  ")
+	s.Log(string(configJSON))
+	if err != nil {
+		s.Fatal("Failed to encode the cross-version-login config to json")
+	}
+	if err := ioutil.WriteFile(configPath, configJSON, 0644); err != nil {
+		s.Fatalf("Failed to write file to %q: %v", configPath, err)
+	}
+}
