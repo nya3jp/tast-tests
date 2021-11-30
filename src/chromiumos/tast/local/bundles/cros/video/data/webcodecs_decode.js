8c42fd9f33f52003f7e4761fa1f1440ffbbef22e
Hirokazu Honda <hiroh@chromium.org>
2021-12-01 01:31:46 +0900

    video: Verify decoded frames by md5 checksums in video.WebCodecsDecode
    This CL add frame content validation by comparing md5 checksums in
    video.WebCodecsDecode. However, if the format of decoded frames are
    RGB format, they are likely converted to the RGB format using GPU
    after decoding. So their contents are dependent on GPU. We skip the
    md5sum validation in the case.
    
    BUG=b:173164490
    TEST=video.WebCodecsDecode.*
    
    Change-Id: I693de5fb1758c5101f7b0a7e53c116844dfa8e9c

--- src/chromiumos/tast/local/bundles/cros/video/data/webcodecs_decode.js
+++ src/chromiumos/tast/local/bundles/cros/video/data/webcodecs_decode.js
@@ -4,10 +4,43 @@
 
 'use strict';
 
+class Frames {
+  constructor(frames) {
+    this.frames = frames;
+  }
+
+  length() {
+    return this.frames.length;
+  }
+
+  async getFrameInfo(index) {
+    if (index >= this.frames.length) {
+      return undefined;
+    }
+
+    const frame = this.frames[index];
+    let buffer = new Uint8Array(frame.allocationSize());
+    let layouts = await frame.copyTo(buffer);
+
+    // Avoid String.fromCharCode() maximum call stack size problem by calling
+    // it one by one.
+    let base64Buffer = '';
+    for (const c of buffer) {
+      base64Buffer += String.fromCharCode(c);
+    }
+
+    return {
+      frameBuffer: btoa(base64Buffer),
+      format: frame.format,
+      layouts: layouts
+    };
+  }
+}
+
 async function DecodeFrames(videoURL, width, height, numFrames,
                             hardwareAcceleration) {
   let decodedFrames = await decodeVideoInURL(videoURL, numFrames,
-                                             hardwareAcceleration);
+                                         hardwareAcceleration);
   TEST.expect(decodedFrames.length == numFrames,
               'Number of decoded frames mismatch: ' + decodedFrames.length);
   for (const frame of decodedFrames) {
@@ -18,5 +51,7 @@ async function DecodeFrames(videoURL, width, height, numFrames,
       frame.visibleRect.width == width && frame.visibleRect.height == height,
       'Unexpected width and height of visible rect: ' +
                 JSON.stringify(frame.visibleRect));
+
+    return new Frames(decodedFrames);
   }
 }
