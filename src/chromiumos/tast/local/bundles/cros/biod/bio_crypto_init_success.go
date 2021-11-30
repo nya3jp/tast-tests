d1fdaa535bb709ed59b8ccb36d4034dc358108b1
Tom Hughes <tomhughes@chromium.org>
2021-11-30 12:57:27 -0800

    firmware: Promote BioCryptoInitSuccess to CQ
    BUG=b:150468438
    TEST=none
    
    Change-Id: I11ba8ba645efc50042aee304b196ca07cddb60be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3308544
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Renuga Nanjappan <rnanjappan@chromium.org>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/biod/bio_crypto_init_success.go
+++ src/chromiumos/tast/local/bundles/cros/biod/bio_crypto_init_success.go
@@ -24,7 +24,7 @@ func init() {
 			"hesling@chromium.org",
 			"chromeos-fingerprint@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"biometrics_daemon"},
 		HardwareDeps: hwdep.D(hwdep.Fingerprint()),
 	})
