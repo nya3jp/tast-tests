a6ecbe76dbfb7f6d8cf4e48fc8d424f33e66b084
Ching-Kang Yen <chingkang@google.com>
2021-11-30 14:54:32 +0800

    tast-tests: introduce challenge_response.go
    In order to test the functionality of challenge response auth in the
    cross-version login tast-test. This CL move several related functions from
    `local/bundles/cros/hwsec/challenge_response_mount.go` to
    `local/hwsec/challange_response.go`.
    
    BUG=b:195775720
    TEST=tast run <ip> hwsec.ChallengeResponseMount
    
    Change-Id: Ia7df80da1759ae5036921c42e682233ed9e4dbbb

--- src/chromiumos/tast/local/bundles/cros/hwsec/challenge_response_mount.go
+++ src/chromiumos/tast/local/bundles/cros/hwsec/challenge_response_mount.go
@@ -6,15 +6,9 @@ package hwsec
 
 import (
 	"context"
-	"crypto"
 	"crypto/rsa"
-	"crypto/sha1"
 	"crypto/x509"
 	"math/rand"
-	"reflect"
-
-	"github.com/godbus/dbus"
-	"github.com/golang/protobuf/proto"
 
 	cpb "chromiumos/system_api/cryptohome_proto"
 	"chromiumos/tast/common/hwsec"
@@ -36,121 +30,6 @@ func init() {
 		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"tpm"},
 	})
-}
-
-type logFunc func(string, ...interface{})
-
-// cryptohomeKeyDelegate is a testing implementation of the
-// CryptohomeKeyDelegate D-Bus object defined here:
-// src/platform2/cryptohome/dbus_bindings/org.chromium.CryptohomeKeyDelegateInterface.xml .
-// This D-Bus service is getting called by cryptohomed during the test.
-type cryptohomeKeyDelegate struct {
-	lf               logFunc
-	dbusConn         *dbus.Conn
-	dbusPath         string
-	dbusIface        string
-	user             string
-	keyAlg           cpb.ChallengeSignatureAlgorithm
-	rsaKey           *rsa.PrivateKey
-	pubKeySPKIDER    []byte
-	challengeCallCnt int
-}
-
-// ChallengeKey handles the incoming ChallengeKey D-Bus call. It returns the
-// KeyChallengeResponse proto with the challenge's signature calculated using
-// the testing private key.
-func (d *cryptohomeKeyDelegate) ChallengeKey(
-	marshAccountID, marshChallReq []byte) (
-	marshChallResp []byte, error *dbus.Error) {
-	d.challengeCallCnt++
-	localMarshChallResp, err := handleChallengeKey(
-		d.user, d.keyAlg, d.rsaKey, d.pubKeySPKIDER, marshAccountID, marshChallReq)
-	if err != nil {
-		d.lf("ChallengeKey handler failed: %s", err)
-		return nil, dbus.MakeFailedError(err)
-	}
-	return localMarshChallResp, nil
-}
-
-// newCryptohomeKeyDelegate creates cryptohomeKeyDelegate and exports this as a
-// D-Bus service running on the given bus.
-func newCryptohomeKeyDelegate(
-	lf logFunc, dbusConn *dbus.Conn, testUser string,
-	keyAlg cpb.ChallengeSignatureAlgorithm, rsaKey *rsa.PrivateKey,
-	pubKeySPKIDER []byte) (*cryptohomeKeyDelegate, error) {
-	const (
-		dbusPath  = "/org/chromium/CryptohomeKeyDelegate"
-		dbusIface = "org.chromium.CryptohomeKeyDelegateInterface"
-	)
-	keyDelegate := cryptohomeKeyDelegate{
-		lf, dbusConn, dbusPath, dbusIface, testUser, keyAlg, rsaKey, pubKeySPKIDER,
-		0 /* challengeCallCnt */}
-	if err := dbusConn.Export(&keyDelegate, dbusPath, dbusIface); err != nil {
-		return nil, err
-	}
-	return &keyDelegate, nil
-}
-
-// close unexports the cryptohomeKeyDelegate instance as a D-Bus object.
-func (d *cryptohomeKeyDelegate) close() {
-	d.dbusConn.Export(nil, dbus.ObjectPath(d.dbusPath), d.dbusIface)
-}
-
-// handleChallengeKey is the actual implementation of the ChallengeKey D-Bus.
-func handleChallengeKey(
-	testUser string, keyAlg cpb.ChallengeSignatureAlgorithm,
-	rsaKey *rsa.PrivateKey, pubKeySPKIDER, marshAccountID, marshChallReq []byte) (
-	marshChallResp []byte, err error) {
-	var accountID cpb.AccountIdentifier
-	if err := proto.Unmarshal(marshAccountID, &accountID); err != nil {
-		return nil, errors.Wrap(err, "failed unmarshaling AccountIdentifier")
-	}
-	var challReq cpb.KeyChallengeRequest
-	if err := proto.Unmarshal(marshChallReq, &challReq); err != nil {
-		return nil, errors.Wrap(err, "failed unmarshaling KeyChallengeRequest")
-	}
-	if accountID.AccountId == nil {
-		return nil, errors.New("missing account_id")
-	}
-	if *accountID.AccountId != testUser {
-		return nil, errors.Errorf("wrong account_id: expected %q, got %q", testUser, *accountID.AccountId)
-	}
-	if challReq.ChallengeType == nil ||
-		*challReq.ChallengeType != cpb.KeyChallengeRequest_CHALLENGE_TYPE_SIGNATURE {
-		return nil, errors.Errorf("wrong challenge_type: %s", challReq.ChallengeType)
-	}
-	sigReqData := challReq.SignatureRequestData
-	if sigReqData == nil {
-		return nil, errors.New("missing signature_request_data")
-	}
-	if sigReqData.DataToSign == nil {
-		return nil, errors.New("missing data_to_sign")
-	}
-	if sigReqData.PublicKeySpkiDer == nil ||
-		!reflect.DeepEqual(sigReqData.PublicKeySpkiDer, pubKeySPKIDER) {
-		return nil, errors.Errorf("bad public_key_spki_der: expected %s, got %s", pubKeySPKIDER,
-			sigReqData.PublicKeySpkiDer)
-	}
-	if sigReqData.SignatureAlgorithm == nil ||
-		*sigReqData.SignatureAlgorithm != keyAlg {
-		return nil, errors.Errorf("wrong signature_algorithm: expected %s, got %s", keyAlg,
-			sigReqData.SignatureAlgorithm)
-	}
-	dataToSignHash := sha1.Sum(sigReqData.DataToSign)
-	sig, err := rsa.SignPKCS1v15(nil, rsaKey, crypto.SHA1, dataToSignHash[:])
-	if err != nil {
-		return nil, errors.Wrap(err, "failed generating signature")
-	}
-	localMarshChallResp, err := proto.Marshal(
-		&cpb.KeyChallengeResponse{
-			SignatureResponseData: &cpb.SignatureKeyChallengeResponseData{
-				Signature: sig,
-			},
-		})
-	if err != nil {
-		return nil, errors.Wrap(err, "failed marshaling KeyChallengeResponse")
-	}
-	return localMarshChallResp, nil
 }
 
 func clearDevicePolicy(ctx context.Context) error {
@@ -211,24 +90,24 @@ func ChallengeResponseMount(ctx context.Context, s *testing.State) {
 	}
 	defer dbusConn.ReleaseName(dbusName)
 
-	keyDelegate, err := newCryptohomeKeyDelegate(
+	keyDelegate, err := hwseclocal.NewCryptohomeKeyDelegate(
 		s.Logf, dbusConn, testUser, keyAlg, rsaKey, pubKeySPKIDER)
 	if err != nil {
 		s.Fatal("Failed to export D-Bus key delegate: ", err)
 	}
-	defer keyDelegate.close()
+	defer keyDelegate.Close()
 
 	// Create the challenge-response protected cryptohome.
-	if err := utility.MountVault(ctx, keyLabel, hwsec.NewChallengeAuthConfig(testUser, dbusName, keyDelegate.dbusPath, pubKeySPKIDER, keyAlg), true, hwsec.NewVaultConfig()); err != nil {
+	if err := utility.MountVault(ctx, keyLabel, hwsec.NewChallengeAuthConfig(testUser, dbusName, keyDelegate.DbusPath, pubKeySPKIDER, keyAlg), true, hwsec.NewVaultConfig()); err != nil {
 		s.Fatal("Failed to create cryptohome: ", err)
 	}
-	if keyDelegate.challengeCallCnt == 0 {
+	if keyDelegate.ChallengeCallCnt == 0 {
 		s.Fatal("No key challenges made during mount")
 	}
 
 	// Authenticate while the cryptohome is still mounted (modeling the case of
 	// the user unlocking the device from the Lock Screen).
-	if _, err := utility.CheckVault(ctx, keyLabel, hwsec.NewChallengeAuthConfig(testUser, dbusName, keyDelegate.dbusPath, pubKeySPKIDER, keyAlg)); err != nil {
+	if _, err := utility.CheckVault(ctx, keyLabel, hwsec.NewChallengeAuthConfig(testUser, dbusName, keyDelegate.DbusPath, pubKeySPKIDER, keyAlg)); err != nil {
 		s.Fatal("Failed to check the key for the mounted cryptohome: ", err)
 	}
 
@@ -237,11 +116,11 @@ func ChallengeResponseMount(ctx context.Context, s *testing.State) {
 	}
 
 	// Mount the existing challenge-response protected cryptohome.
-	keyDelegate.challengeCallCnt = 0
-	if err := utility.MountVault(ctx, keyLabel, hwsec.NewChallengeAuthConfig(testUser, dbusName, keyDelegate.dbusPath, pubKeySPKIDER, keyAlg), false, hwsec.NewVaultConfig()); err != nil {
+	keyDelegate.ChallengeCallCnt = 0
+	if err := utility.MountVault(ctx, keyLabel, hwsec.NewChallengeAuthConfig(testUser, dbusName, keyDelegate.DbusPath, pubKeySPKIDER, keyAlg), false, hwsec.NewVaultConfig()); err != nil {
 		s.Fatal("Failed to mount existing cryptohome: ", err)
 	}
-	if keyDelegate.challengeCallCnt == 0 {
+	if keyDelegate.ChallengeCallCnt == 0 {
 		s.Fatal("No key challenges made during mount")
 	}
 }
