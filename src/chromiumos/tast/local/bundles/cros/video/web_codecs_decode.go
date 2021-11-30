10870f79c30745f706dbaaf73907c719ae34050e
Hirokazu Honda <hiroh@chromium.org>
2021-12-01 01:31:46 +0900

    video: Verify decoded frames by md5 checksums in video.WebCodecsDecode
    This CL add frame content validation by comparing md5 checksums in
    video.WebCodecsDecode. However, if the format of decoded frames are
    RGB format, they are likely converted to the RGB format using GPU
    after decoding. So their contents are dependent on GPU. We skip the
    md5sum validation in the case.
    
    BUG=b:173164490
    TEST=video.WebCodecsDecode.*
    
    Change-Id: I693de5fb1758c5101f7b0a7e53c116844dfa8e9c

--- src/chromiumos/tast/local/bundles/cros/video/web_codecs_decode.go
+++ src/chromiumos/tast/local/bundles/cros/video/web_codecs_decode.go
@@ -29,40 +29,40 @@ func init() {
 		Params: []testing.Param{{
 			Name:      "av1_sw",
 			Val:       webcodecs.TestDecodeArgs{VideoFile: "bear-320x240.av1.mp4", Acceleration: webcodecs.PreferSoftware},
-			ExtraData: []string{"bear-320x240.av1.mp4"},
+			ExtraData: []string{"bear-320x240.av1.mp4", "bear-320x240.av1.mp4.json"},
 		}, {
 			Name:              "av1_hw",
 			Val:               webcodecs.TestDecodeArgs{VideoFile: "bear-320x240.av1.mp4", Acceleration: webcodecs.PreferHardware},
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1},
-			ExtraData:         []string{"bear-320x240.av1.mp4"},
+			ExtraData:         []string{"bear-320x240.av1.mp4", "bear-320x240.av1.mp4.json"},
 		}, {
 			Name:              "h264_sw",
 			Val:               webcodecs.TestDecodeArgs{VideoFile: "bear-320x240.h264.mp4", Acceleration: webcodecs.PreferSoftware},
 			ExtraSoftwareDeps: []string{"proprietary_codecs"},
-			ExtraData:         []string{"bear-320x240.h264.mp4"},
+			ExtraData:         []string{"bear-320x240.h264.mp4", "bear-320x240.h264.mp4.json"},
 		}, {
 			Name:              "h264_hw",
 			Val:               webcodecs.TestDecodeArgs{VideoFile: "bear-320x240.h264.mp4", Acceleration: webcodecs.PreferHardware},
 			ExtraSoftwareDeps: []string{"proprietary_codecs", caps.HWDecodeH264},
-			ExtraData:         []string{"bear-320x240.h264.mp4"},
+			ExtraData:         []string{"bear-320x240.h264.mp4", "bear-320x240.h264.mp4.json"},
 		}, {
 			Name:      "vp8_sw",
 			Val:       webcodecs.TestDecodeArgs{VideoFile: "bear-320x240.vp8.mp4", Acceleration: webcodecs.PreferSoftware},
-			ExtraData: []string{"bear-320x240.vp8.mp4"},
+			ExtraData: []string{"bear-320x240.vp8.mp4", "bear-320x240.vp8.mp4.json"},
 		}, {
 			Name:              "vp8_hw",
 			Val:               webcodecs.TestDecodeArgs{VideoFile: "bear-320x240.vp8.mp4", Acceleration: webcodecs.PreferHardware},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
-			ExtraData:         []string{"bear-320x240.vp8.mp4"},
+			ExtraData:         []string{"bear-320x240.vp8.mp4", "bear-320x240.vp8.mp4.json"},
 		}, {
 			Name:      "vp9_sw",
 			Val:       webcodecs.TestDecodeArgs{VideoFile: "bear-320x240.vp9.mp4", Acceleration: webcodecs.PreferSoftware},
-			ExtraData: []string{"bear-320x240.vp9.mp4"},
+			ExtraData: []string{"bear-320x240.vp9.mp4", "bear-320x240.vp9.mp4.json"},
 		}, {
 			Name:              "vp9_hw",
 			Val:               webcodecs.TestDecodeArgs{VideoFile: "bear-320x240.vp9.mp4", Acceleration: webcodecs.PreferHardware},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
-			ExtraData:         []string{"bear-320x240.vp9.mp4"},
+			ExtraData:         []string{"bear-320x240.vp9.mp4", "bear-320x240.vp9.mp4.json"},
 		}},
 	})
 }
@@ -70,7 +70,7 @@ func init() {
 func WebCodecsDecode(ctx context.Context, s *testing.State) {
 	args := s.Param().(webcodecs.TestDecodeArgs)
 	if err := webcodecs.RunDecodeTest(ctx, s.FixtValue().(*chrome.Chrome),
-		s.DataFileSystem(), args); err != nil {
+		s.DataFileSystem(), args, s.DataPath(args.VideoFile+".json"), s.OutDir()); err != nil {
 		s.Error("Test failed: ", err)
 	}
 }
