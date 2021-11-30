adb0761db8980cc161288ad9d94f67ae6a789915
Roman Sorokin <rsorokin@chromium.org>
2021-11-30 14:50:13 +0100

    lockscreen.Signout: Add waiting logic for the login screen
    Also isolated testing steps a bit
    
    BUG=b:190654593
    TEST=run locally
    
    Change-Id: I90443f70c423a0fdaf66ef72fad5b1ff84837296

--- src/chromiumos/tast/local/bundles/cros/lockscreen/signout.go
+++ src/chromiumos/tast/local/bundles/cros/lockscreen/signout.go
@@ -53,130 +53,128 @@ func Signout(ctx context.Context, s *testing.State) {
 	signoutWithKeyboardShortcut := s.Param().(testParam).withShortcut
 	checkCrashes := s.Param().(testParam).checkCrashes
 
-	cr, err := chrome.New(ctx, chrome.ExtraArgs("--force-tablet-mode=clamshell", "--disable-virtual-keyboard"))
-	if err != nil {
-		s.Fatal("Chrome login failed: ", err)
-	}
-	defer cr.Close(ctx)
-
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Getting test API connection failed: ", err)
-	}
-
-	_, err = cr.NewConn(ctx, "chrome://settings")
-	if err != nil {
-		s.Fatal("Failed to open a tab: ", err)
-	}
-	_, err = cr.NewConn(ctx, "chrome://os-credits")
-	if err != nil {
-		s.Fatal("Failed to open a tab: ", err)
-	}
-
-	if err := lockscreen.Lock(ctx, tconn); err != nil {
-		s.Fatal("Failed to lock the screen: ", err)
-	}
-
-	var oldCrashes []string
-	if checkCrashes {
-		oldCrashes, err = crash.GetCrashes(crash.DefaultDirs()...)
+	func() {
+		cr, err := chrome.New(ctx, chrome.ExtraArgs("--force-tablet-mode=clamshell", "--disable-virtual-keyboard"))
 		if err != nil {
-			s.Fatal("GetCrashes failed: ", err)
+			s.Fatal("Chrome login failed: ", err)
 		}
-	}
+		defer cr.Close(ctx)
 
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
-	oldProc, err := ashproc.Root()
-	if err != nil {
-		s.Fatal("Failed to get Chrome root PID: ", err)
-	}
-
-	// We wait here to give Chrome more chances to shutdown in 3 seconds and
-	// won't get killed by the session manager.
-	if err := cpu.WaitUntilIdle(ctx); err != nil {
-		s.Fatal("Failed to wait for CPU to become idle: ", err)
-	}
-
-	if signoutWithKeyboardShortcut {
-		kb, err := input.Keyboard(ctx)
+		tconn, err := cr.TestAPIConn(ctx)
 		if err != nil {
-			s.Fatal("Failed to get keyboard: ", err)
-		}
-		if err := kb.Accel(ctx, "Ctrl+Shift+Q"); err != nil {
-			s.Fatal("Failed emulate shortcut 1st press: ", err)
-		}
-		if err := kb.Accel(ctx, "Ctrl+Shift+Q"); err != nil {
-			s.Fatal("Failed emulate shortcut 2nd press: ", err)
-		}
-	} else {
-		// Sign out using button.
-		ui := uiauto.New(tconn)
-		signOutButton := nodewith.Name("Sign out").Role(role.Button)
-		buttonFound, err := ui.IsNodeFound(ctx, signOutButton)
-		if !buttonFound {
-			s.Fatal("Signout button was not found: ", err)
+			s.Fatal("Getting test API connection failed: ", err)
 		}
 
-		// We click multiple times because device might be in the suspended
-		// state after cpu.WaitUntilIdle call. And some clicks might be ignored
-		// by the button.
-		// We ignore errors here because when we click on "Sign out" button
-		// Chrome shuts down and the connection is closed. So we always get an
-		// error.
-		ui.LeftClickUntil(signOutButton, ui.Gone(signOutButton))(ctx)
-	}
-
-	// Wait for Chrome restart
-	if err := procutil.WaitForTerminated(ctx, oldProc, 30*time.Second); err != nil {
-		s.Fatal("Timeout waiting for Chrome to shutdown: ", err)
-	}
-	if _, err := ashproc.WaitForRoot(ctx, 30*time.Second); err != nil {
-		s.Fatal("Timeout waiting for Chrome to start: ", err)
-	}
-
-	if checkCrashes {
-		newCrashes, err := crash.GetCrashes(crash.DefaultDirs()...)
+		_, err = cr.NewConn(ctx, "chrome://settings")
 		if err != nil {
-			s.Fatal("GetCrashes failed: ", err)
+			s.Fatal("Failed to open a tab: ", err)
 		}
-		if len(oldCrashes) != len(newCrashes) {
-			oldCrashesMap := make(map[string]bool)
-			for _, oldCrash := range oldCrashes {
-				oldCrashesMap[oldCrash] = true
+		_, err = cr.NewConn(ctx, "chrome://os-credits")
+		if err != nil {
+			s.Fatal("Failed to open a tab: ", err)
+		}
+
+		if err := lockscreen.Lock(ctx, tconn); err != nil {
+			s.Fatal("Failed to lock the screen: ", err)
+		}
+
+		var oldCrashes []string
+		if checkCrashes {
+			oldCrashes, err = crash.GetCrashes(crash.DefaultDirs()...)
+			if err != nil {
+				s.Fatal("GetCrashes failed: ", err)
+			}
+		}
+
+		defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+		oldProc, err := ashproc.Root()
+		if err != nil {
+			s.Fatal("Failed to get Chrome root PID: ", err)
+		}
+
+		// We wait here to give Chrome more chances to shutdown in 3 seconds and
+		// won't get killed by the session manager.
+		if err := cpu.WaitUntilIdle(ctx); err != nil {
+			s.Fatal("Failed to wait for CPU to become idle: ", err)
+		}
+
+		if signoutWithKeyboardShortcut {
+			kb, err := input.Keyboard(ctx)
+			if err != nil {
+				s.Fatal("Failed to get keyboard: ", err)
+			}
+			if err := kb.Accel(ctx, "Ctrl+Shift+Q"); err != nil {
+				s.Fatal("Failed emulate shortcut 1st press: ", err)
+			}
+			if err := kb.Accel(ctx, "Ctrl+Shift+Q"); err != nil {
+				s.Fatal("Failed emulate shortcut 2nd press: ", err)
+			}
+		} else {
+			// Sign out using button.
+			ui := uiauto.New(tconn)
+			signOutButton := nodewith.Name("Sign out").Role(role.Button)
+			buttonFound, err := ui.IsNodeFound(ctx, signOutButton)
+			if !buttonFound {
+				s.Fatal("Signout button was not found: ", err)
 			}
 
-			for _, crash := range newCrashes {
-				// Save only new crashes
-				if oldCrashesMap[crash] {
-					continue
-				}
-				out := filepath.Join(s.OutDir(), filepath.Base(crash))
-				if err := fsutil.CopyFile(crash, out); err != nil {
-					s.Logf("Failed to save %v: %v", crash, err)
-				}
-			}
-			s.Fatal("Something crashed during the test")
+			// We click multiple times because device might be in the suspended
+			// state after cpu.WaitUntilIdle call. And some clicks might be ignored
+			// by the button.
+			// We ignore errors here because when we click on "Sign out" button
+			// Chrome shuts down and the connection is closed. So we always get an
+			// error.
+			ui.LeftClickUntil(signOutButton, ui.Gone(signOutButton))(ctx)
 		}
-	}
+
+		// Wait for Chrome restart
+		if err := procutil.WaitForTerminated(ctx, oldProc, 30*time.Second); err != nil {
+			s.Fatal("Timeout waiting for Chrome to shutdown: ", err)
+		}
+		if _, err := ashproc.WaitForRoot(ctx, 30*time.Second); err != nil {
+			s.Fatal("Timeout waiting for Chrome to start: ", err)
+		}
+
+		if checkCrashes {
+			newCrashes, err := crash.GetCrashes(crash.DefaultDirs()...)
+			if err != nil {
+				s.Fatal("GetCrashes failed: ", err)
+			}
+			if len(oldCrashes) != len(newCrashes) {
+				oldCrashesMap := make(map[string]bool)
+				for _, oldCrash := range oldCrashes {
+					oldCrashesMap[oldCrash] = true
+				}
+
+				for _, crash := range newCrashes {
+					// Save only new crashes
+					if oldCrashesMap[crash] {
+						continue
+					}
+					out := filepath.Join(s.OutDir(), filepath.Base(crash))
+					if err := fsutil.CopyFile(crash, out); err != nil {
+						s.Logf("Failed to save %v: %v", crash, err)
+					}
+				}
+				s.Fatal("Something crashed during the test")
+			}
+		}
+	}()
 
 	// Restart chrome for testing
-	if cr, err = chrome.New(ctx, chrome.NoLogin(), chrome.KeepState(), chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey"))); err != nil {
+	cr, err := chrome.New(ctx, chrome.NoLogin(), chrome.KeepState(), chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey")))
+	if err != nil {
 		s.Fatal("Chrome restart for testing failed: ", err)
 	}
-	tconn, err = cr.SigninProfileTestAPIConn(ctx)
+	tconn, err := cr.SigninProfileTestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Getting signing test API connection failed: ", err)
 	}
 
 	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
 
-	st, err := lockscreen.GetState(ctx, tconn)
-	if err != nil {
-		s.Fatal("Failed to get login state: ", err)
-	}
-
-	if !st.ReadyForPassword {
-		s.Fatal("Chrome is not on the login screen: ", err)
+	if _, err := lockscreen.WaitState(ctx, tconn, func(st lockscreen.State) bool { return st.ReadyForPassword }, 10*time.Second); err != nil {
+		s.Fatal("Failed to wait for login screen: ", err)
 	}
 }
