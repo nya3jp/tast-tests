af3119df4a324b97dc496bed4734ab850eadb83f
Chad Duffin <chadduffin@chromium.org>
2021-12-01 19:50:58 +0000

    [TAST] Add tests for Bluetooth surfaces.
    This change adds tests that cover:
    
     - Opening the pairing dialog from the Bluetooth Settings.
     - Opening the pairing dialog from the OS Settings.
     - Toggling Bluetooth from the Bluetooth Settings.
     - Toggling Bluetooth from the OS Settings.
    
    BUG=chromium:1010321
    TEST=tast run <DUT> bluetooth.*Bluetooth*
    
    Change-Id: I57dbf1ffdf26fd1838e035a234a137050e0ffc53

---
+++ src/chromiumos/tast/local/bundles/cros/bluetooth/toggle_bluetooth_from_bluetooth_settings.go
@@ -0,0 +1,71 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bluetooth
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bluetooth"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+var bluetoothSettingsBluetoothToggleButton = nodewith.NameContaining("Bluetooth").Role(role.ToggleButton)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ToggleBluetoothFromBluetoothSettings,
+		Desc: "Checks that Bluetooth can be enabled and disabled from the Bluetooth Settings sub-page",
+		Contacts: []string{
+			"chadduffin@chromium.org",
+			"cros-connectivity@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedInWithBluetoothEnabled",
+	})
+}
+
+// ToggleBluetoothFromBluetoothSettings tests that a user can successfully
+// toggle the Bluetooth state using the Bluetooth toggle within the
+// Bluetooth Settings sub-page.
+func ToggleBluetoothFromBluetoothSettings(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	app, err := ossettings.NavigateToBluetoothSettingsPage(ctx, s, tconn)
+	defer app.Close(ctx)
+
+	if err != nil {
+		s.Fatal("Failed to show the Bluetooth Settings sub-page: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	if err := ui.WaitUntilExists(bluetoothSettingsBluetoothToggleButton)(ctx); err != nil {
+		s.Fatal("Failed to find the Bluetooth toggle: ", err)
+	}
+
+	state := false
+	const iterations = 20
+	for i := 0; i < iterations; i++ {
+		s.Logf("Toggling Bluetooth (iteration %d of %d)", i+1, iterations)
+
+		if err := ui.LeftClick(bluetoothSettingsBluetoothToggleButton)(ctx); err != nil {
+			s.Fatal("Failed to click the Bluetooth toggle: ", err)
+		}
+		if err := bluetooth.PollForAdapterState(ctx, state); err != nil {
+			s.Fatal("Failed to toggle Bluetooth state: ", err)
+		}
+		state = !state
+	}
+}
