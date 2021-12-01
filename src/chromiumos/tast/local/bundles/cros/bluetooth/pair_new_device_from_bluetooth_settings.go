2192f06cb5a06151173d97a9b7ae11398a1fff17
Chad Duffin <chadduffin@chromium.org>
2021-12-01 21:04:30 +0000

    [TAST] Add tests for Bluetooth surface coverage.
    This change adds tests that cover:
    
     - Opening the pairing dialog from Bluetooth Settings.
     - Opening the pairing dialog from OS Settings.
     - Toggling Bluetooth from the OS Settings.
    
    BUG=chromium:1010321
    TEST=tast run <DUT> bluetooth.*
    
    Change-Id: If0bf316940c2630c40b5775fef650a461f49f2aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3311823
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Tested-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/bluetooth/pair_new_device_from_bluetooth_settings.go
@@ -0,0 +1,56 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bluetooth
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PairNewDeviceFromBluetoothSettings,
+		Desc: "Checks that the pairing dialog can be opened from the Bluetooth Settings sub-page",
+		Contacts: []string{
+			"chadduffin@chromium.org",
+			"cros-connectivity@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedInWithBluetoothEnabled",
+	})
+}
+
+// PairNewDeviceFromBluetoothSettings tests that a user can successfully open
+// the pairing dialog from the "Pair new device" button on the Bluetooth
+// Settings sub-page.
+func PairNewDeviceFromBluetoothSettings(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	app, err := ossettings.NavigateToBluetoothSettingsPage(ctx, tconn)
+	defer app.Close(ctx)
+
+	if err != nil {
+		s.Fatal("Failed to show the Bluetooth Settings sub-page: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	if err := uiauto.Combine("Open the \"Pair new device\" dialog",
+		ui.LeftClick(ossettings.BluetoothPairNewDeviceButton),
+		ui.WaitUntilExists(ossettings.BluetoothPairNewDeviceModal),
+	)(ctx); err != nil {
+		s.Fatal("Failed to open the \"Pair new device\" dialog: ", err)
+	}
+}
