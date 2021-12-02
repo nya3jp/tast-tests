31303d1130b79f3f43728d086dc80b26eb5cb260
Jeremy Bettis <jbettis@google.com>
2021-12-02 09:49:06 -0700

    faft: Convert firmware_SoftwareSync to tast
    Add new function to BiosService to corrupt an EC section.  It adds one
    to every byte of the section.
    
    Add new test firmware.SoftwareSync. It has mostly the same functionality
    as the autotest, but doesn't handle the EFS2 cases yet, and some steps
    were omitted until they can be checked to make sure they are really
    needed.
    
    This test also clears DISABLE_EC_SOFTWARE_SYNC instead of failing like
    the old test did. Since it restores the firmware backup, if a custom EC
    was being used with DISABLE_EC_SOFTWARE_SYNC, it should back in that
    state after the test.
    
    BUG=b:194910957
    TEST=Ran on lenoa (no efs2)
    
    Change-Id: I029597e5ae4a9a432486ec37a282cabb6f5d53d0

--- src/chromiumos/tast/local/bundles/cros/firmware/bios_service.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/bios_service.go
@@ -82,3 +82,19 @@ func (bs *BiosService) EnableAPSoftwareWriteProtect(ctx context.Context, req *em
 	}
 	return &empty.Empty{}, nil
 }
+
+// CorruptECSection writes garbage over part of the specified firmware section.
+func (bs *BiosService) CorruptECSection(ctx context.Context, req *pb.CorruptSection) (*empty.Empty, error) {
+	img, err := bios.NewImage(ctx, bios.ImageSection(req.Section), bios.ECProgrammer)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not read firmware")
+	}
+	for i, v := range img.Data {
+		img.Data[i] = (v + 1) & 0xff
+	}
+	err = img.WriteFlashrom(ctx, bios.ImageSection(req.Section), bios.ECProgrammer)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not write firmware")
+	}
+	return &empty.Empty{}, nil
+}
---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/software_sync.go
@@ -0,0 +1,163 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"bytes"
+	"context"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	common "chromiumos/tast/common/firmware"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/remote/firmware"
+	"chromiumos/tast/remote/firmware/fixture"
+	pb "chromiumos/tast/services/cros/firmware"
+	"chromiumos/tast/ssh"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SoftwareSync,
+		Desc:         "Servo based EC software sync test",
+		Contacts:     []string{"jbettis@google.com", "chromeos-firmware@google.com"},
+		Attr:         []string{"group:firmware", "firmware_unstable"},
+		ServiceDeps:  []string{"tast.cros.firmware.BiosService"},
+		HardwareDeps: hwdep.D(hwdep.ChromeEC()),
+		Params: []testing.Param{
+			{Name: "normal",
+				Fixture: fixture.NormalMode,
+			},
+			{Name: "dev",
+				Fixture: fixture.DevModeGBB,
+			},
+		},
+	})
+}
+
+const hashCommand = "ectool echash | grep hash: | sed \"s/hash:\\s\\+//\""
+
+func SoftwareSync(ctx context.Context, s *testing.State) {
+	h := s.FixtValue().(*fixture.Value).Helper
+
+	if err := h.RequireServo(ctx); err != nil {
+		s.Fatal("Failed to init servo: ", err)
+	}
+
+	ms, err := firmware.NewModeSwitcher(ctx, h)
+	if err != nil {
+		s.Fatal("Creating mode switcher: ", err)
+	}
+
+	// TODO(b/194910957): old test disables EC WP here
+
+	if err := h.RequireBiosServiceClient(ctx); err != nil {
+		s.Fatal("Requiring BiosServiceClient: ", err)
+	}
+	bs := h.BiosServiceClient
+
+	old, err := bs.GetGBBFlags(ctx, &empty.Empty{})
+	if err != nil {
+		s.Fatal("initial GetGBBFlags failed: ", err)
+	}
+
+	if common.GBBFlagsContains(old, pb.GBBFlag_DISABLE_EC_SOFTWARE_SYNC) {
+		s.Log("Clearing GBB flag DISABLE_EC_SOFTWARE_SYNC")
+		req := pb.GBBFlagsState{Clear: []pb.GBBFlag{pb.GBBFlag_DISABLE_EC_SOFTWARE_SYNC}}
+
+		if _, err := bs.ClearAndSetGBBFlags(ctx, &req); err != nil {
+			s.Fatal("Failed to clear gbb flag: ", err)
+		}
+
+		// TODO(b/194910957): old test does warm reset, seems unneeded
+	}
+
+	backup, err := bs.BackupECRW(ctx, &empty.Empty{})
+	if err != nil {
+		s.Fatal("Could not backup EC firmware: ", err)
+	}
+	cleanupContext := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 30*time.Second)
+	defer cancel()
+	defer func(ctx context.Context) {
+		s.Log("Deleting temp file")
+		if err := h.DUT.Conn().CommandContext(ctx, "rm", "-f", backup.Path).Run(ssh.DumpLogOnError); err != nil {
+			s.Fatal("Failed to delete firmware backup: ", err)
+		}
+	}(cleanupContext)
+
+	// TODO(b/194910957): Old test checks that fw-a section does not have preamble flag PREAMBLE_USE_RO_NORMAL. this really needed?
+	// TODO(b/194910957): Old test unlocks CCD, is this needed?
+
+	// Check CR50 boot mode is NORMAL
+	output, err := h.Servo.RunCR50CommandGetOutput(ctx, "ec_comm", []string{`boot_mode\s*:\s*(\S+)\b`})
+	if err != nil {
+		s.Fatal("CR50 cmd failed: ", err)
+	}
+	if output[0][1] != "NORMAL" {
+		s.Fatalf("CR50 boot mode incorrect, got %q want %q", output[0][1], "NORMAL")
+	}
+
+	s.Log("Corrupt EC firmware RW body")
+	activeCopy, err := h.Servo.GetString(ctx, "ec_active_copy")
+	if err != nil {
+		s.Fatal("EC active copy failed: ", err)
+	}
+	if !strings.HasPrefix(activeCopy, "RW") {
+		s.Fatalf("EC active copy incorrect, got %q want RW", activeCopy)
+	}
+	ecHashBefore, err := h.DUT.Conn().CommandContext(ctx, "sh", "-c", hashCommand).
+		Output()
+	if err != nil {
+		s.Fatal("Failed to get ec hash: ", err)
+	}
+	ecSection := "EC_RW"
+	if activeCopy == "RW_B" {
+		ecSection = "EC_RW_B"
+	}
+	s.Log("Corrupt the EC section: ", ecSection)
+	defer func(ctx context.Context) {
+		s.Log("Restoring EC firmware backup")
+		if _, err := bs.RestoreECRW(ctx, backup); err != nil {
+			s.Fatal("Failed to restore EC firmware: ", err)
+		}
+	}(cleanupContext)
+	if _, err = bs.CorruptECSection(ctx, &pb.CorruptSection{Section: ecSection}); err != nil {
+		s.Fatal("Failed to corrupt EC: ", err)
+	}
+
+	s.Log("Reboot AP, check EC hash, and software sync it")
+	if err := ms.ModeAwareReboot(ctx, firmware.WarmReset); err != nil {
+		s.Fatal("Failed to reboot: ", err)
+	}
+	h.CloseRPCConnection(ctx)
+	if err := h.RequireBiosServiceClient(ctx); err != nil {
+		s.Fatal("Requiring BiosServiceClient: ", err)
+	}
+	bs = h.BiosServiceClient
+
+	s.Log("Expect EC in RW and RW is restored")
+	ecHashAfter, err := h.DUT.Conn().CommandContext(ctx, "sh", "-c", hashCommand).
+		Output()
+	if err != nil {
+		s.Fatal("Failed to get ec hash: ", err)
+	}
+	if !bytes.Equal(ecHashAfter, ecHashBefore) {
+		s.Fatalf("EC hash wrong, got %s want %s", ecHashAfter, ecHashBefore)
+	}
+	activeCopy, err = h.Servo.GetString(ctx, "ec_active_copy")
+	if err != nil {
+		s.Fatal("EC active copy failed: ", err)
+	}
+	if !strings.HasPrefix(activeCopy, "RW") {
+		s.Fatalf("EC active copy incorrect, got %q want RW", activeCopy)
+	}
+
+	// TODO(b/194910957): run_test_corrupt_hash_in_cr50 if EC_FEATURE_EFS2
+}
