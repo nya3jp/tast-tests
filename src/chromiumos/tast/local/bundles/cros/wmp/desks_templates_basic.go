0e60be4ce0d43c424ec47cc5e36b21dc70a2c873
Yanzhu Du <yzd@google.com>
2021-12-02 17:30:35 -0800

    tast-tests: Add wmp.DesksTemplatesBasic Tast Test
    This test enters overview mode and save the current desk as a desk
    template.
    
    BUG=chromium:1276205
    TEST=tast -verbose run <IP> wmp.DesksTemplatesBasic
    
    Change-Id: I9cab05fc2a4554c19324583c5c04b73fe3711e82

---
+++ src/chromiumos/tast/local/bundles/cros/wmp/desks_templates_basic.go
@@ -0,0 +1,147 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wmp
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/optin"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DesksTemplatesBasic,
+		Desc: "Checks desks can be saved as a desk template",
+		Contacts: []string{
+			"yzd@chromium.org",
+			"chromeos-wmp@google.com",
+			"cros-commercial-productivity-eng@google.com",
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "android_vm"},
+		Timeout:      chrome.GAIALoginTimeout + arc.BootTimeout + 120*time.Second,
+		VarDeps:      []string{"ui.gaiaPoolDefault"},
+	})
+}
+
+func DesksTemplatesBasic(ctx context.Context, s *testing.State) {
+	// Reserve five seconds for various cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	cr, err := chrome.New(ctx,
+		chrome.GAIALoginPool(s.RequiredVar("ui.gaiaPoolDefault")),
+		chrome.EnableFeatures("DesksTemplates"),
+		chrome.ARCSupported(),
+		chrome.ExtraArgs(arc.DisableSyncFlags()...))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
+	if err != nil {
+		s.Fatal("Failed to ensure clamshell mode: ", err)
+	}
+	defer cleanup(cleanupCtx)
+
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	ac := uiauto.New(tconn)
+
+	// Setup for launching ARC apps.
+	if err := optin.PerformAndClose(ctx, cr, tconn); err != nil {
+		s.Fatal("Failed to optin to Play Store and Close: ", err)
+	}
+
+	// Setup ARC.
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close(cleanupCtx)
+
+	if err := a.WaitIntentHelper(ctx); err != nil {
+		s.Fatal("Failed to wait for ARC Intent Helper: ", err)
+	}
+
+	// Opens PlayStore, Chrome and Files.
+	for _, app := range []apps.App{apps.PlayStore, apps.Chrome, apps.Files} {
+		if err := apps.Launch(ctx, tconn, app.ID); err != nil {
+			s.Fatalf("Failed to open %s: %v", app.Name, err)
+		}
+		if err := ash.WaitForApp(ctx, tconn, app.ID, time.Minute); err != nil {
+			s.Fatalf("%s did not appear in shelf after launch: %s", app.Name, err)
+		}
+	}
+
+	if err := ac.WaitForLocation(nodewith.Root())(ctx); err != nil {
+		s.Fatal("Failed to wait for app launch events to be completed: ", err)
+	}
+
+	// Enters overview mode.
+	if err := ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+		s.Fatal("Failed to set overview mode: ", err)
+	}
+
+	// Find the "save desk as a template" button.
+	saveDeskButton := nodewith.ClassName("PillButton")
+	desksTemplatesGridView := nodewith.ClassName("DesksTemplatesGridView")
+
+	if err := uiauto.Combine(
+		"save a desk template",
+		ac.LeftClick(saveDeskButton),
+		// Wait for the desk templates grid shows up.
+		ac.WaitUntilExists(desksTemplatesGridView),
+	)(ctx); err != nil {
+		s.Fatal("Failed to save a desk template: ", err)
+	}
+
+	// Exits overview mode.
+	if err = ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
+		s.Fatal("Failed to exit overview mode: ", err)
+	}
+
+	// Re-enters overview mode, so we can save another desk template.
+	if err := ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+		s.Fatal("Failed to set overview mode: ", err)
+	}
+
+	// Save another desk template.
+	if err := uiauto.Combine(
+		"save another desk template",
+		ac.LeftClick(saveDeskButton),
+		// Wait for the desk templates grid shows up.
+		ac.WaitUntilExists(desksTemplatesGridView),
+	)(ctx); err != nil {
+		s.Fatal("Failed to save a desk template: ", err)
+	}
+
+	// Verifies that there are two saved desk template.
+	deskTemplatesInfo, err := ash.FindDeskTemplates(ctx, ac)
+	if err != nil {
+		s.Fatal("Failed to find desk templates: ", err)
+	}
+	if len(deskTemplatesInfo) != 2 {
+		s.Fatalf("Got %v desk template(s), want two desk templates", len(deskTemplatesInfo))
+	}
+}
