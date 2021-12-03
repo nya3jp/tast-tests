62680fad77482250b528172b8ca38557e6b91431
Chinglin Yu <chinglinyu@chromium.org>
2021-12-03 20:50:18 +0800

    platform: add test CheckTracefsInstances
    Test platform.CheckTracefsInstances asserts that we don't have more
    tracefs instances (the default plus the ones under instances/) than the
    number of CPU cores/threads. This test is kept non-critical as a warning
    that further investigation of boot performance is needed for the failing
    devices.
    
    BUG=b:206335074
    TEST=tast run $IP_ADDR platform.CheckTracefsInstances
    
    Change-Id: Ieba2b05f8582b540102e79dea0914552daa8340e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3314053
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Chinglin Yu <chinglinyu@chromium.org>
    Tested-by: Chinglin Yu <chinglinyu@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/platform/check_tracefs_instances.go
@@ -0,0 +1,52 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"io/ioutil"
+
+	"github.com/shirou/gopsutil/cpu"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     CheckTracefsInstances,
+		Desc:     "Checks that the number of tracefs instances doesn't exceed the number of CPU cores/threads",
+		Contacts: []string{"chinglinyu@chromium.org"},
+		Attr:     []string{"group:mainline", "informational"},
+	})
+}
+
+// CheckTracefsInstances checks that we don't have more tracefs instances than the number of processors as a warning of potential boot performance issues with SELinux labeling.
+func CheckTracefsInstances(ctx context.Context, s *testing.State) {
+	const tracefsInstancesPath = "/sys/kernel/debug/tracing/instances"
+
+	cpuInfo, err := cpu.InfoWithContext(ctx)
+	if err != nil {
+		s.Fatal("Failed to get CPU info: ", err)
+	}
+	np := len(cpuInfo) // np: number of processors.
+
+	instances, err := ioutil.ReadDir(tracefsInstancesPath)
+	if err != nil {
+		s.Fatal("Failed to read the instances directory: ", err)
+	}
+
+	ni := 1 // ni: number of instances, starting from 1 (the default instance).
+	for _, instance := range instances {
+		if instance.IsDir() {
+			ni++ // Found additional instances.
+		}
+	}
+
+	s.Logf("Number of processors: %d, number of tracefs instances: %d", np, ni)
+	if ni > 0 && ni > np {
+		// This is potentially harmful to boot performance. Fail the test as a warning.
+		s.Fatalf("More tracefs instances than the number of processors (got %d, want <= %d): tracefs labeling is potentially slow", ni, np)
+	}
+}
