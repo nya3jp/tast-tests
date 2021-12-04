b217ba9fa972384ed8d99f5fd002e6b4549d2df3
Anastasiia N <anastasiian@chromium.org>
2021-12-04 21:00:56 +0100

    tast-tests: Move browser setup in accountmanager tests
    AddAccountFromOGB test, which opens the One Google Bar in the browser
    immediately after the browserfixt.SetUp call, fails to load OGB more
    often than AddAccountOSSettings test.
    
    In this situation `browserfixt.SetUp(...)` succeeds,
    `br.NewConn(ctx, "https://www.google.com")` succeeds but the browser
    window doesn't open.
    
    Move the browser setup call before the test cleanup to allow more time
    for the setup.
    
    BUG=chromium:1268351
    TEST=tast run <ip> accountmanager.*
    
    Change-Id: Id844be68e3b8fd70849a2df53f808813f61f39b4

--- src/chromiumos/tast/local/bundles/cros/accountmanager/add_account_from_ogb.go
+++ src/chromiumos/tast/local/bundles/cros/accountmanager/add_account_from_ogb.go
@@ -71,17 +71,17 @@ func AddAccountFromOGB(ctx context.Context, s *testing.State) {
 	}
 	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
 
-	s.Log("Runing test cleanup")
-	if err := accountmanager.TestCleanup(ctx, tconn, cr, s.Param().(browser.Type)); err != nil {
-		s.Fatal("Failed to do cleanup: ", err)
-	}
-
 	// Setup the browser.
 	br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(browser.Type))
 	if err != nil {
 		s.Fatal("Failed to setup chrome: ", err)
 	}
 	defer closeBrowser(cleanupCtx)
+
+	s.Log("Runing test cleanup")
+	if err := accountmanager.TestCleanup(ctx, tconn, cr, s.Param().(browser.Type)); err != nil {
+		s.Fatal("Failed to do cleanup: ", err)
+	}
 
 	ui := uiauto.New(tconn).WithTimeout(time.Minute)
 	a := s.FixtValue().(accountmanager.FixtureData).ARC
