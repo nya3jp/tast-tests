c8a8daf79031dcf9d634ac540b3bc93fb23e05e6
Andres Calderon Jaramillo <andrescj@chromium.org>
2021-12-06 04:02:11 +0000

    arc/video: Add variants for out-of-process video decoding.
    This CL creates variants for the ARC video decoding tests to exercise
    the disabled-by-default out-of-process video decoding feature in Chrome.
    
    BUG=b:195769334
    TEST=arc.VideoDecodeAccel.*_oopvd* on volteer
    TEST=arc.VideoDecodeAccelPerf.*_oopvd* on volteer
    
    Change-Id: Ia84ad0246d2f4399cf3b76e038a6fa347c48c55d

--- src/chromiumos/tast/local/bundles/cros/arc/video_decode_accel.go
+++ src/chromiumos/tast/local/bundles/cros/arc/video_decode_accel.go
@@ -16,41 +16,86 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         VideoDecodeAccel,
-		Desc:         "Verifies ARC++ hardware decode acceleration by running the c2_e2e_test APK",
-		Contacts:     []string{"akahuang@chromium.org", "chromeos-video-eng@google.com"},
+		Func: VideoDecodeAccel,
+		Desc: "Verifies ARC++ hardware decode acceleration by running the c2_e2e_test APK",
+		Contacts: []string{
+			"akahuang@chromium.org",
+			"andrescj@chromium.org", // For the 'oopvd' variants.
+			"chromeos-video-eng@google.com",
+		},
 		Data:         []string{c2e2etest.X86ApkName, c2e2etest.ArmApkName},
 		SoftwareDeps: []string{"chrome"},
-		Fixture:      "arcBootedWithVideoLogging",
 		Timeout:      4 * time.Minute,
 		Params: []testing.Param{{
 			Name:              "h264",
 			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.h264"},
+			Fixture:           "arcBootedWithVideoLogging",
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "android_p"},
+			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
+		}, {
+			Name:              "h264_oopvd",
+			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.h264"},
+			Fixture:           "arcBootedWithVideoLoggingAndOutOfProcessVideoDecoding",
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "android_p"},
 			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
 		}, {
 			Name:              "h264_vm",
 			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.h264"},
+			Fixture:           "arcBootedWithVideoLogging",
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "android_vm"},
+			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
+		}, {
+			Name:              "h264_oopvd_vm",
+			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.h264"},
+			Fixture:           "arcBootedWithVideoLoggingAndOutOfProcessVideoDecoding",
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "android_vm"},
 			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
 		}, {
 			Name:              "vp8",
 			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp8"},
+			Fixture:           "arcBootedWithVideoLogging",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8, "android_p"},
+			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
+		}, {
+			Name:              "vp8_oopvd",
+			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp8"},
+			Fixture:           "arcBootedWithVideoLoggingAndOutOfProcessVideoDecoding",
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8, "android_p"},
 			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
 		}, {
 			Name:              "vp8_vm",
 			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp8"},
+			Fixture:           "arcBootedWithVideoLogging",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8, "android_vm"},
+			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
+		}, {
+			Name:              "vp8_oopvd_vm",
+			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp8"},
+			Fixture:           "arcBootedWithVideoLoggingAndOutOfProcessVideoDecoding",
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8, "android_vm"},
 			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
 		}, {
 			Name:              "vp9",
 			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp9"},
+			Fixture:           "arcBootedWithVideoLogging",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "android_p"},
+			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
+		}, {
+			Name:              "vp9_oopvd",
+			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp9"},
+			Fixture:           "arcBootedWithVideoLoggingAndOutOfProcessVideoDecoding",
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "android_p"},
 			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
 		}, {
 			Name:              "vp9_vm",
 			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp9"},
+			Fixture:           "arcBootedWithVideoLogging",
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "android_vm"},
+			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
+		}, {
+			Name:              "vp9_oopvd_vm",
+			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp9"},
+			Fixture:           "arcBootedWithVideoLoggingAndOutOfProcessVideoDecoding",
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "android_vm"},
 			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
 		}},
