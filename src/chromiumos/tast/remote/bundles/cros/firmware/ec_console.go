ccb6ef3a882ed3aec74961a88a9c74689b4be8fc
Jeremy Bettis <jbettis@google.com>
2021-12-06 13:33:49 -0700

    faft: New bringup test for ec console
    From Guybrush: Functional Test Tracker
    
    Connect to EC console via C2D2 UART (or legacy servo)
    
    BUG=b:208690862
    TEST=tast run --var servo=${SERVO?}:nossh \
      --var noSSH=true --var board=grunt --var model=treeya \
      ${SPARE_CHROMEBOOK?} firmware.ECConsole
    
    Change-Id: I937bb030f1f1805e26e28eb5621a91173fcfdd0b

---
+++ src/chromiumos/tast/remote/bundles/cros/firmware/ec_console.go
@@ -0,0 +1,39 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+
+	"chromiumos/tast/remote/firmware"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ECConsole,
+		Desc:     "Verifies that EC console is working",
+		Contacts: []string{"jbettis@chromium.org", "cros-fw-engprod@google.com"},
+		Attr:     []string{"group:firmware", "firmware_unstable", "firmware_bringup"},
+		Vars:     []string{"servo"},
+	})
+}
+
+// ECConsole opens the EC console and runs the version command.
+func ECConsole(ctx context.Context, s *testing.State) {
+	servoSpec, _ := s.Var("servo")
+	h := firmware.NewHelper(nil, nil, "", servoSpec, "", "", "", "")
+	defer h.Close(ctx)
+
+	if err := h.RequireServo(ctx); err != nil {
+		s.Fatal("Failed to require servo: ", err)
+	}
+
+	strings, err := h.Servo.RunECCommandGetOutput(ctx, "version", []string{`Chip:\s*([^\n]*)\n`})
+	if err != nil {
+		s.Fatal("ec console version command: ", err)
+	}
+	s.Logf("EC Chip: %s", strings[0][1])
+}
