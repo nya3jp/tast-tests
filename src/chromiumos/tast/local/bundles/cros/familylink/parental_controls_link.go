cb1c3971eab0bf222ecb729862e8f14d68e48f4c
Victor Chen <victor.chen@cienet.com>
2021-12-07 17:22:43 +0800

    mta: Add test familylink.ParentalControlsLink
    [MTA 1f9d0] Verify 'Parental controls' opens
     https://families.google.com/families
    
    BUG=b:200987147
    TEST=tast run -var=geller.parentUser='<parentUser>' -var=geller.parentPassword='<parentPassword>' -var=geller.childUser='<childUser>' -var=geller.childPassword='<childPassword>' <DUT> familylink.ParentalControlsLink
    
    Change-Id: I5a19e3b94a47b540b24d3610ab57f4f74fee5da5

---
+++ src/chromiumos/tast/local/bundles/cros/familylink/parental_controls_link.go
@@ -0,0 +1,89 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package familylink
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/familylink"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ParentalControlsLink,
+		// TODO(b/250500759): Support lacros for this test once this issue is fixed.
+		LacrosStatus: testing.LacrosVariantNeeded,
+		Desc:         "Verify 'Parental controls' setting opens https://families.google.com/families when Play Store is disabled",
+		Contacts: []string{
+			"victor.chen@cienet.com",
+			"cienet-development@googlegroups.com",
+			"chromeos-sw-engprod@google.com",
+			"cros-families-eng+test@google.com ",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      time.Minute,
+		Fixture:      "familyLinkGellerLogin", // Expecting the ARC to be disabled in this test.
+	})
+}
+
+const familiesURL = "https://families.google.com/families"
+
+// ParentalControlsLink verifies 'Parental controls' opens https://families.google.com/families.
+func ParentalControlsLink(ctx context.Context, s *testing.State) {
+	var (
+		cr    = s.FixtValue().(*familylink.FixtData).Chrome
+		tconn = s.FixtValue().(*familylink.FixtData).TestConn
+		ui    = uiauto.New(tconn)
+	)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+	defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_dump")
+
+	settings, err := ossettings.LaunchAtPage(ctx, tconn, nodewith.Name("Accounts").Role(role.Link))
+	if err != nil {
+		s.Fatal("Failed to open Accounts page: ", err)
+	}
+	defer func(ctx context.Context) {
+		faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_dump_after_open_settings")
+
+		if err := browser.CloseAllTabs(ctx, tconn); err != nil {
+			s.Log("Failed to close tabs: ", err)
+		}
+
+		settings.Close(ctx)
+	}(cleanupCtx)
+
+	if err := uiauto.Combine("open parental controls link",
+		ui.LeftClick(nodewith.NameContaining("Parental controls Open").FinalAncestor(ossettings.WindowFinder)),
+		ui.WaitUntilExists(nodewith.NameContaining("Families").HasClass("BrowserFrame")),
+	)(ctx); err != nil {
+		s.Fatal("Failed to click parental controls link and wait families page exists: ", err)
+	}
+
+	tabs, err := browser.CurrentTabs(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to find current tabs: ", err)
+	}
+
+	if len(tabs) != 1 {
+		s.Fatalf("Failed to verify the expected page opened: unexpected tab number: want 1, got %d", len(tabs))
+	}
+
+	if tabs[0].URL != familiesURL {
+		s.Fatalf("Failed to verify the expected page opened: want %q, got %q", familiesURL, tabs[0].URL)
+	}
+}
