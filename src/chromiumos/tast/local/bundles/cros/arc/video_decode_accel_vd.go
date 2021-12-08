85efadcb56e327c8fdd468a4e30873db4d39336f
David Staessens <dstaessens@google.com>
2021-12-08 16:40:48 +0900

    Tast: Add new VideoDecodeAccelVD tests for the ARCVM VD video stack.
    This CL introduces tests for the new ARCVM VD video stack. The VD stack
    talks directly to the Chrome media::VideoDecoders through the GpuArcVD
    service and the mojo::VideoDecoder interface introduced in
    crrev.com/c/3026312.
    
    BUG=b:189278506
    TEST=tast run DUT arc.VideoDecodeAccelVD.*
    
    Change-Id: Iddbc48c26ba872a3d2ace58a0081dcaf09beab81

---
+++ src/chromiumos/tast/local/bundles/cros/arc/video_decode_accel_vd.go
@@ -0,0 +1,48 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/local/bundles/cros/arc/c2e2etest"
+	"chromiumos/tast/local/bundles/cros/arc/video"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VideoDecodeAccelVD,
+		Desc:         "Verifies ARCVM hardware decode acceleration using a media::VideoDecoder by running the c2_e2e_test APK (see go/arcvm-vd)",
+		Contacts:     []string{"chromeos-video-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		Data:         []string{c2e2etest.X86ApkName, c2e2etest.ArmApkName},
+		SoftwareDeps: []string{"chrome", "android_vm"},
+		Fixture:      "arcBootedWithVideoLoggingVD",
+		Timeout:      4 * time.Minute,
+		Params: []testing.Param{{
+			Name:              "h264_vm",
+			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.h264"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+			ExtraData:         []string{"test-25fps.h264", "test-25fps.h264.json"},
+		}, {
+			Name:              "vp8_vm",
+			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp8"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+			ExtraData:         []string{"test-25fps.vp8", "test-25fps.vp8.json"},
+		}, {
+			Name:              "vp9_vm",
+			Val:               video.DecodeTestOptions{TestVideo: "test-25fps.vp9"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         []string{"test-25fps.vp9", "test-25fps.vp9.json"},
+		}},
+	})
+}
+
+func VideoDecodeAccelVD(ctx context.Context, s *testing.State) {
+	video.RunAllARCVideoTests(ctx, s, s.Param().(video.DecodeTestOptions))
+}
