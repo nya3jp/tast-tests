c11a2f7590812c50c616ab67bb622d8409d6858b
Moja Hsu <mojahsu@google.com>
2021-12-09 10:33:42 +0800

    camera: Add face auto exposure running in camerabox
    Add face auto exposure test. We use the face image provided by IQ team
    to check the luma value of face with and without face auto exposure. Due
    to the brightness may not be same in different tablets, the test does
    brightness calibration first.
    
    BUG=b:203608939
    TEST=tast run $DUT camera.FaceAutoExposure.front
    
    Change-Id: I1d037372253c2d62ab5ae0884a6dec894ac5807c

--- src/chromiumos/tast/remote/bundles/cros/camera/face_detection.go
+++ src/chromiumos/tast/remote/bundles/cros/camera/face_detection.go
@@ -6,11 +6,11 @@ package camera
 
 import (
 	"context"
-	"regexp"
 
 	"chromiumos/tast/common/camera/chart"
 	"chromiumos/tast/common/media/caps"
 	"chromiumos/tast/remote/bundles/cros/camera/camerabox"
+	"chromiumos/tast/remote/bundles/cros/camera/face"
 	"chromiumos/tast/rpc"
 	pb "chromiumos/tast/services/cros/camerabox"
 	"chromiumos/tast/testing"
@@ -46,33 +46,12 @@ func FaceDetection(ctx context.Context, s *testing.State) {
 	d := s.DUT()
 	facing := s.Param().(pb.Facing)
 
-	// Check if this DUT has a fd enabled camera with the tested facing.
-	out, err := d.Conn().CommandContext(ctx, "media_v4l2_test", "--list_usbcam").Output()
+	roiSupport, err := face.CheckRoiSupport(ctx, d, facing)
 	if err != nil {
-		s.Fatal(err, " failed to list usb cameras")
-	}
-	checkRoiSupport := false
-	usbCameraRegexp := regexp.MustCompile(`/dev/video\d+`)
-	for _, m := range usbCameraRegexp.FindAllStringSubmatch(string(out), -1) {
-		device := m[0]
-		out, err := d.Conn().CommandContext(ctx, "media_v4l2_test", "--gtest_filter=*GetRoiSupport*", "--device_path="+device).Output()
-		if err != nil {
-			s.Fatal(err, " failed to get roi support info")
-		}
-		r := regexp.MustCompile("Facing:(front|back):(1|0)")
-		m2 := r.FindAllStringSubmatch(string(out), -1)
-		s.Logf("Find device:%s facing:%s, roi support:%s", device, m2[0][1], m2[0][2])
-		if facing == pb.Facing_FACING_BACK && m2[0][1] == "back" && m2[0][2] == "1" {
-			checkRoiSupport = true
-			break
-		}
-		if facing == pb.Facing_FACING_FRONT && m2[0][1] == "front" && m2[0][2] == "1" {
-			checkRoiSupport = true
-			break
-		}
+		s.Fatal("Failed to check ROI support: ", err)
 	}
 
-	if !checkRoiSupport {
+	if !roiSupport {
 		s.Log("Skip this DUT, because it doesn't support roi")
 		return
 	}
