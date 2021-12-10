4cb18e08f811c86a9510c54c1f7b761880ca1235
Alan Ding <alanding@google.com>
2021-12-10 01:48:40 -0800

    tast-test: Support guest ureadahead pack validation for ARCVM
    For ARCVM, we also need to validate the ureadahead pack in the guest OS.
    Refactored ureadahead dump log file validation code into separate
    function so it can be reused by host and guest pack checks.
    
    BUG=b:209733624
    TEST=tast run $DUT arc.UreadaheadValidation.vm
    
    Change-Id: Ibecde3c99a6debb563c9b95eadeedc220e34e0a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3328071
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Tested-by: Alan Ding <alanding@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Alan Ding <alanding@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/ureadahead_validation.go
+++ src/chromiumos/tast/local/bundles/cros/arc/ureadahead_validation.go
@@ -25,10 +25,12 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         UreadaheadValidation,
 		LacrosStatus: testing.LacrosVariantUnknown,
-		Desc:         "Validates that ARC ureadahead pack exists and looks valid",
-		Contacts: []string{"khmel@google.com",
+		Desc:         "Validates that ARC ureadahead packs in the host/guest OS exist and are valid",
+		Contacts: []string{
+			"khmel@google.com",
 			"alanding@google.com",
-			"arc-performance@google.com"},
+			"arc-performance@google.com",
+		},
 		// NOTE: This test should never be promoted to critical. It has build dependency and it will
 		//       always fail in PFQ since we don't have ureadahead caches generated at PFQ time.
 		Attr: []string{"group:mainline", "informational", "group:arc-functional"},
@@ -41,7 +43,7 @@ func init() {
 			ExtraSoftwareDeps: []string{"android_vm"},
 		}},
 		// Minimum acceptable.
-		Timeout: 4 * time.Minute,
+		Timeout: 5 * time.Minute,
 	})
 }
 
@@ -65,11 +67,9 @@ func ureadaheadPackRequired(ctx context.Context, vmEnabled bool) (bool, error) {
 
 func UreadaheadValidation(ctx context.Context, s *testing.State) {
 	const (
-		// normally generated ureadahead pack covers >350MB of data.
-		minAcceptableUreadaheadPackSizeKB = 300 * 1024
-
-		// name of ureadahead log.
-		ureadaheadLogName = "ureadahead.log"
+		// Names of ureadahead dump logs.
+		ureadaheadLogName   = "ureadahead.log"
+		ureadaheadVMLogName = "vm_ureadahead.log"
 	)
 
 	vmEnabled, err := arc.VMEnabled()
@@ -79,7 +79,6 @@ func UreadaheadValidation(ctx context.Context, s *testing.State) {
 
 	packPath := ""
 	if vmEnabled {
-		// TODO(khmel): Add guest side validation.
 		packPath = "/opt/google/vms/android/ureadahead.pack"
 	} else {
 		packPath = "/opt/google/containers/android/ureadahead.pack"
@@ -108,7 +107,7 @@ func UreadaheadValidation(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to create log file: ", err)
 	}
 	cmd.Stdout = logFile
-	// don't set cmd.Stderr, so it goes to default log buffer
+	// Don't set cmd.Stderr, so it goes to default log buffer
 	// and DumpLogOnError can dump it.
 	err = cmd.Run(testexec.DumpLogOnError)
 	logFile.Close()
@@ -117,14 +116,78 @@ func UreadaheadValidation(ctx context.Context, s *testing.State) {
 		s.Fatalf("Failed to get the ureadahead stats %s: %v", packPath, err)
 	}
 
-	re := regexp.MustCompile(`^(\d+) inode groups, (\d+) files, (\d+) blocks \((\d+) kB\)$`)
-
+	// Verify the host pack file dump.
 	logFile, err = os.Open(logPath)
 	if err != nil {
 		s.Fatal("Failed to open log file: ", err)
 	}
 	defer logFile.Close()
+	if err = checkPackFileDump(ctx, logPath); err != nil {
+		s.Fatalf("Failed to verify ureadahead pack file dump, please check %q: %v", ureadaheadLogName, err)
+	}
 
+	// If VM, also verify guest OS ureadahead dump.
+	if vmEnabled {
+		vmLogPath := filepath.Join(s.OutDir(), ureadaheadVMLogName)
+		if err = dumpGuestPack(ctx, vmLogPath); err != nil {
+			s.Fatal("Failed to dump ureadahead pack in the guest OS: ", err)
+		}
+
+		// Verify the guest pack file dump.
+		if err = checkPackFileDump(ctx, vmLogPath); err != nil {
+			s.Fatalf("Failed to verify ureadahead pack file dump, please check %q: %v", ureadaheadVMLogName, err)
+		}
+	}
+}
+
+// dumpGuestPack dumps the ureadahead pack in the guest OS.
+func dumpGuestPack(ctx context.Context, logPath string) error {
+	// File path for ureadahead pack in the quest OS.
+	const ureadaheadDataDir = "/var/lib/ureadahead"
+
+	outdir, ok := testing.ContextOutDir(ctx)
+	if !ok {
+		return errors.New("failed to get name of the output directory")
+	}
+
+	// Connect to ARCVM instance.
+	a, err := arc.New(ctx, outdir)
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to ARCVM")
+	}
+	defer a.Close(ctx)
+
+	// Check for existence of newly generated pack file on guest side.
+	srcPath := filepath.Join(ureadaheadDataDir, "pack")
+	if _, err := a.FileSize(ctx, srcPath); err != nil {
+		return errors.Wrap(err, "failed to ensure pack file exists")
+	}
+
+	logFile, err := os.Create(logPath)
+	if err != nil {
+		return errors.Wrap(err, "failed to create log file")
+	}
+	defer logFile.Close()
+
+	// Capture stdout into log file.
+	cmd := a.Command(ctx, "/system/bin/ureadahead", "--dump")
+	cmd.Stdout = logFile
+	return cmd.Run(testexec.DumpLogOnError)
+}
+
+// checkPackFileDump verifies the validity of the generated pack file using
+// ureadahead's own pack file dump functionality.
+func checkPackFileDump(ctx context.Context, logPath string) error {
+	// normally generated ureadahead pack covers >300MB of data.
+	const minAcceptableUreadaheadPackSizeKB = 300 * 1024
+
+	logFile, err := os.Open(logPath)
+	if err != nil {
+		return errors.Wrap(err, "failed to open log file")
+	}
+	defer logFile.Close()
+
+	re := regexp.MustCompile(`^(\d+) inode groups, (\d+) files, (\d+) blocks \((\d+) kB\)$`)
 	scanner := bufio.NewScanner(logFile)
 
 	matchFound := false
@@ -136,26 +199,28 @@ func UreadaheadValidation(ctx context.Context, s *testing.State) {
 			continue
 		}
 		if matchFound {
-			s.Fatalf("More than 1 match found. Please check %q. Last match: %q", ureadaheadLogName, str)
+			return errors.Wrapf(err, "failed with more than 1 match found. Last match: %q", str)
 		}
 
 		// Parsing (\d+) group that represents number of Kb handled by this ureadahead pack
 		sizeKB, err = strconv.Atoi(result[4])
 		if err != nil {
-			s.Fatalf("Failed to parse group %q from %q: %v", result[4], str, err)
+			return errors.Wrapf(err, "failed to parse group %q from %q", result[4], str)
 		}
 		matchFound = true
 	}
 
 	if err := scanner.Err(); err != nil {
-		s.Fatalf("Failed to read log file %q: %v", ureadaheadLogName, err)
+		return errors.Wrap(err, "failed to read log file")
 	}
 
 	if !matchFound {
-		s.Fatalf("Failed to parse ureadahead pack dump. Please check %q", ureadaheadLogName)
+		return errors.Wrap(err, "failed to parse ureadahead pack dump")
 	}
 
 	if sizeKB < minAcceptableUreadaheadPackSizeKB {
-		s.Errorf("Pack size %d kB is too small. It is expected to be min %d kb", sizeKB, minAcceptableUreadaheadPackSizeKB)
+		return errors.Errorf("failed due to pack size %d kB too small. It is expected to be min %d kb", sizeKB, minAcceptableUreadaheadPackSizeKB)
 	}
+
+	return nil
 }
