3e986168ae1ba3bfaea925f93f9ac4bc68a4d8bc
David Welling <davidwelling@google.com>
2021-12-12 00:01:41 +0000

    arcappcompat: Make copy paste tablet tests critical
    Set the non-vm tests to be critical since there are only known, flakey
    failures which are caused by the infrastructure and not the tests
    themselves.
    
    BUG=b:194900373
    TEST=push apk && tast run ${HOST} arc.StandardizedKeyboardCopy*
    
    Change-Id: I86f5f4a089fbb849d74342a06285eca5fe3831d1

--- src/chromiumos/tast/local/bundles/cros/arc/standardized_keyboard_copy_paste.go
+++ src/chromiumos/tast/local/bundles/cros/arc/standardized_keyboard_copy_paste.go
@@ -22,7 +22,7 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnknown,
 		Desc:         "Functional test that installs an app and tests standard keyboard copy/paste functionality. Test are performed in clamshell and touchview mode. This does not test the virtual, on-screen keyboard",
 		Contacts:     []string{"davidwelling@google.com", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Timeout:      10 * time.Minute,
 		Fixture:      "arcBooted",
@@ -37,11 +37,13 @@ func init() {
 			ExtraHardwareDeps: hwdep.D(standardizedtestutil.TabletHardwareDep),
 		}, {
 			Name:              "vm",
+			ExtraAttr:         []string{"informational"},
 			Val:               standardizedtestutil.GetClamshellTest(runStandardizedKeyboardCopyPasteTest),
 			ExtraSoftwareDeps: []string{"android_vm"},
 			ExtraHardwareDeps: hwdep.D(standardizedtestutil.ClamshellHardwareDep),
 		}, {
 			Name:              "vm_tablet_mode",
+			ExtraAttr:         []string{"informational"},
 			Val:               standardizedtestutil.GetTabletTest(runStandardizedKeyboardCopyPasteTest),
 			ExtraSoftwareDeps: []string{"android_vm"},
 			ExtraHardwareDeps: hwdep.D(standardizedtestutil.TabletHardwareDep),
