a4a0128b9c3537d020a0aa5ef784dbf9654c8882
Jun Sheng <shengjun@google.com>
2021-12-13 16:55:47 +1100

    tast-tests: refactor VK typing test on omnibox
    1. Refactor the test to use UserAction.
    2. Extend test coverage to multiple input methods, login mode and VK type.
    3. Demote the test to informational due to major change.
    
    BUG=b:210370410
    TEST=tast run <DUT> inputs.VirtualKeyboardTypingOmnibox*
    
    Change-Id: I52499f60f606d61fade0133cd4b00c57199d99bb

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_omnibox.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_omnibox.go
@@ -6,18 +6,22 @@ package inputs
 
 import (
 	"context"
-	"strings"
+	"path/filepath"
 	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/bundles/cros/inputs/data"
 	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/util"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/ime"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/chrome/uiauto/vkb"
+	"chromiumos/tast/local/chrome/useractions"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
@@ -28,52 +32,104 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnknown,
 		Desc:         "Checks that the virtual keyboard works in Chrome browser omnibox",
 		Contacts:     []string{"essential-inputs-team@google.com"},
-		Attr:         []string{"group:mainline", "group:input-tools"},
+		Attr:         []string{"group:input-tools"},
 		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
 		Timeout:      5 * time.Minute,
 		Params: []testing.Param{{
-			Pre:               pre.VKEnabledTablet,
+			Pre:               pre.VKEnabledTabletReset,
+			Val:               []ime.InputMethod{ime.EnglishUS, ime.Japanese, ime.ChinesePinyin},
 			ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
-			ExtraAttr:         []string{"group:input-tools-upstream"},
+			ExtraAttr:         []string{"group:mainline", "informational", "group:input-tools-upstream"},
+		}, {
+			Name:              "guest",
+			Pre:               pre.VKEnabledTabletInGuest,
+			Val:               []ime.InputMethod{ime.EnglishUSWithInternationalKeyboard, ime.JapaneseWithUSKeyboard, ime.Korean},
+			ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
+			ExtraAttr:         []string{"group:mainline", "informational", "group:input-tools-upstream"},
+		}, {
+			Name:      "a11y",
+			Pre:       pre.VKEnabledClamshellReset,
+			Val:       []ime.InputMethod{ime.EnglishUK, ime.AlphanumericWithJapaneseKeyboard, ime.Arabic},
+			ExtraAttr: []string{"group:mainline", "informational", "group:input-tools-upstream"},
 		}, {
 			Name:              "informational",
-			Pre:               pre.VKEnabledTablet,
+			Pre:               pre.VKEnabledTabletReset,
+			Val:               []ime.InputMethod{ime.EnglishUS, ime.Japanese, ime.Arabic},
 			ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
-			ExtraAttr:         []string{"informational"},
+			ExtraAttr:         []string{"group:mainline", "informational"},
 		}}})
 }
 
 func VirtualKeyboardTypingOmnibox(ctx context.Context, s *testing.State) {
-	// typingKeys indicates a key series that tapped on virtual keyboard.
-	const typingKeys = "go"
 	cr := s.PreValue().(pre.PreData).Chrome
 	tconn := s.PreValue().(pre.PreData).TestAPIConn
-
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
-	// Warning: Please do not launch Browser via cr.NewConn(ctx, "")
-	// to test omnibox typing. It might be indeterminate whether default url string
-	// "about:blank" is highlighted or not.
-	// In that case, typing test can either replace existing url or insert into it.
-	// A better way to do it is launching Browser from launcher, url is empty by default.
-	if err := apps.Launch(ctx, tconn, apps.Chrome.ID); err != nil {
-		s.Fatalf("Failed to launch %s: %s", apps.Chrome.Name, err)
-	}
-	if err := ash.WaitForApp(ctx, tconn, apps.Chrome.ID, time.Minute); err != nil {
-		s.Fatalf("%s did not appear in shelf after launch: %s", apps.Chrome.Name, err)
-	}
-
-	omniboxFinder := nodewith.Role(role.TextField).Attribute("inputType", "url")
+	uc := s.PreValue().(pre.PreData).UserContext
 	vkbCtx := vkb.NewContext(cr, tconn)
 
-	if err := uiauto.Combine("verify virtual keyboard input on omnibox",
-		vkbCtx.ClickUntilVKShown(omniboxFinder),
-		vkbCtx.TapKeysIgnoringCase(strings.Split(typingKeys, "")),
-		// Hide virtual keyboard to submit candidate.
-		vkbCtx.HideVirtualKeyboard(),
-		// Validate text.
-		util.WaitForFieldTextToBeIgnoringCase(tconn, omniboxFinder, typingKeys),
-	)(ctx); err != nil {
-		s.Fatal("Failed to verify virtual keyboard input on omnibox: ", err)
+	testIMEs := s.Param().([]ime.InputMethod)
+
+	subtest := func(testName string, inputData data.InputData) func(ctx context.Context, s *testing.State) {
+		return func(ctx context.Context, s *testing.State) {
+			cleanupCtx := ctx
+			// Use a shortened context for test operations to reserve time for cleanup.
+			ctx, shortCancel := ctxutil.Shorten(ctx, 10*time.Second)
+			defer shortCancel()
+
+			defer func(ctx context.Context) {
+				outDir := filepath.Join(s.OutDir(), testName)
+				faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, s.HasError, cr, "ui_tree_"+testName)
+
+				if err := vkbCtx.HideVirtualKeyboard()(ctx); err != nil {
+					s.Log("Failed to hide virtual keyboard: ", err)
+				}
+			}(cleanupCtx)
+
+			// Warning: Please do not launch Browser via cr.NewConn(ctx, "")
+			// to test omnibox typing. It might be indeterminate whether default url string
+			// "about:blank" is highlighted or not.
+			// In that case, typing test can either replace existing url or insert into it.
+			// A better way to do it is launching Browser from launcher, url is empty by default.
+			if err := apps.Launch(ctx, tconn, apps.Chrome.ID); err != nil {
+				s.Fatalf("Failed to launch %s: %s", apps.Chrome.Name, err)
+			}
+			defer func(ctx context.Context) {
+				if err := apps.Close(ctx, tconn, apps.Chrome.ID); err != nil {
+					testing.ContextLog(ctx, "Failed to close Chrome browser: ", err)
+				}
+			}(cleanupCtx)
+
+			if err := ash.WaitForApp(ctx, tconn, apps.Chrome.ID, time.Minute); err != nil {
+				s.Fatalf("%s did not appear in shelf after launch: %s", apps.Chrome.Name, err)
+			}
+
+			omniboxFinder := nodewith.Role(role.TextField).Attribute("inputType", "url")
+
+			validateAction := uiauto.Combine("verify virtual keyboard input on omnibox",
+				vkbCtx.ClickUntilVKShown(omniboxFinder),
+				vkbCtx.TapKeysIgnoringCase(inputData.CharacterKeySeq),
+				func(ctx context.Context) error {
+					if inputData.SubmitFromSuggestion {
+						return vkbCtx.SelectFromSuggestion(inputData.ExpectedText)(ctx)
+					}
+					return nil
+				},
+				// Validate text.
+				util.WaitForFieldTextToBeIgnoringCase(tconn, omniboxFinder, inputData.ExpectedText),
+			)
+
+			if err := useractions.NewUserAction("VK typing",
+				validateAction,
+				uc,
+				&useractions.UserActionCfg{
+					Tags: []useractions.ActionTag{useractions.ActionTagVKTyping},
+					Attributes: map[string]string{
+						useractions.AttributeInputField: "Omnibox",
+					},
+				},
+			).Run(ctx); err != nil {
+				s.Fatal("Failed to verify virtual keyboard input on omnibox: ", err)
+			}
+		}
 	}
+	util.RunSubtestsPerInputMethodAndMessage(ctx, uc, s, testIMEs, []data.Message{data.TypingMessageHello}, subtest)
 }
