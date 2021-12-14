ba5910e0e5de14048763f1239dd4db525483e931
Paul Moy <pmoy@chromium.org>
2021-12-14 13:10:43 -0700

    scanapp: add hardware.go
    This file contains a new tast test, scanapp.Hardware, which is intended
    to be run on real scanning hardware.
    
    BUG=b:195064784
    TEST=tast run --build $DUT_IP scanapp.Hardware
    
    Cq-Depend: chrome-internal:4349937
    Change-Id: Ib67b1ea5b8d272fa778cba306f7ede644eebe880

---
+++ src/chromiumos/tast/local/bundles/cros/scanapp/hardware.go
@@ -0,0 +1,66 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package scanapp
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/scanapp/scanning"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/scanapp"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Hardware,
+		Desc: "Tests that the Scan app can be used on real hardware",
+		Contacts: []string{
+			"cros-peripherals@google.com",
+			"project-bolton@google.com",
+		},
+		Attr: []string{
+			"group:paper-io",
+			"paper-io_scanning",
+		},
+		SoftwareDeps: []string{"chrome"}, // TODO(pmoy): Add "printscan" once this test runs on the lab scanners.
+		VarDeps:      []string{"scanapp.Hardware.Scanner0"},
+		Timeout:      30 * time.Minute,
+		Fixture:      "chromeLoggedIn",
+	})
+}
+
+func Hardware(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	var scanners = []scanning.ScannerDescriptor{
+		{
+			ScannerName: s.RequiredVar("scanapp.Hardware.Scanner0"),
+			SupportedSources: []scanning.SupportedSource{
+				{
+					SourceType:           scanapp.SourceFlatbed,
+					SupportedColorModes:  []scanapp.ColorMode{scanapp.ColorModeColor, scanapp.ColorModeGrayscale},
+					SupportedPageSizes:   []scanapp.PageSize{scanapp.PageSizeA4, scanapp.PageSizeLegal, scanapp.PageSizeLetter, scanapp.PageSizeFitToScanArea},
+					SupportedResolutions: []scanapp.Resolution{scanapp.Resolution300DPI},
+				},
+				{
+					SourceType:           scanapp.SourceADFOneSided,
+					SupportedColorModes:  []scanapp.ColorMode{scanapp.ColorModeColor, scanapp.ColorModeGrayscale},
+					SupportedPageSizes:   []scanapp.PageSize{scanapp.PageSizeA4, scanapp.PageSizeLegal, scanapp.PageSizeLetter, scanapp.PageSizeFitToScanArea},
+					SupportedResolutions: []scanapp.Resolution{scanapp.Resolution300DPI},
+				},
+				{
+					SourceType:           scanapp.SourceADFTwoSided,
+					SupportedColorModes:  []scanapp.ColorMode{scanapp.ColorModeColor, scanapp.ColorModeGrayscale},
+					SupportedPageSizes:   []scanapp.PageSize{scanapp.PageSizeA4, scanapp.PageSizeLegal, scanapp.PageSizeLetter, scanapp.PageSizeFitToScanArea},
+					SupportedResolutions: []scanapp.Resolution{scanapp.Resolution300DPI},
+				},
+			},
+		},
+	}
+
+	scanning.RunHardwareTests(ctx, s, cr, scanners)
+}
