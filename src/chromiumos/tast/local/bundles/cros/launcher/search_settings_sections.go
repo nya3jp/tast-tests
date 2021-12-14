773821825dbba626c2fd3b8caae64d70f9c97dbc
Anastasiia N <anastasiian@google.com>
2021-12-14 11:10:19 +0100

    tast-tests: Update SearchSettingsSections tests
    Provide the Finder in "wantValue" instead of just the expected text and
    make sure that the node is actually inside the Settings window.
    
    BUG=chromium:1267815
    TEST=tast run <ip> launcher.SearchSettingsSections
    
    Change-Id: I4e03d86eac5df0157b2a069377179db56aa56b43

--- src/chromiumos/tast/local/bundles/cros/launcher/search_settings_sections.go
+++ src/chromiumos/tast/local/bundles/cros/launcher/search_settings_sections.go
@@ -14,16 +14,17 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/launcher"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
 
 type searchSettingsTestCase struct {
-	searchTerm        string // searchTerm is the text that is entered in the Launcher.
-	searchResult      string // searchResult is the text that should be selected in the Launcher.
-	wantValue         string // wantValue is the expected string that should be present on the opened OS Settings section (e.g. a page title).
-	passwordProtected bool   // whether the settings page is password protected.
+	searchTerm        string           // searchTerm is the text that is entered in the Launcher.
+	searchResult      string           // searchResult is the text that should be selected in the Launcher.
+	wantValue         *nodewith.Finder // wantValue is the expected node that should be present on the opened OS Settings section (e.g. a page title).
+	passwordProtected bool             // whether the settings page is password protected.
 }
 
 const deviceUserPassword = "testpass"
@@ -46,43 +47,43 @@ func init() {
 				{
 					searchTerm:        "Guest browsing",
 					searchResult:      "Guest browsing, Manage other people",
-					wantValue:         "Manage other people",
+					wantValue:         nodewith.Name("Manage other people").Role(role.Heading),
 					passwordProtected: false,
 				},
 				{
 					searchTerm:        "Show usernames and photos on the sign-in screen",
 					searchResult:      "Show usernames and photos on the sign-in screen, Manage other people",
-					wantValue:         "Manage other people",
+					wantValue:         nodewith.Name("Manage other people").Role(role.Heading),
 					passwordProtected: false,
 				},
 				{
 					searchTerm:        "Restrict sign-in",
 					searchResult:      "Restrict sign-in, Manage other people",
-					wantValue:         "Manage other people",
+					wantValue:         nodewith.Name("Manage other people").Role(role.Heading),
 					passwordProtected: false,
 				},
 				{
 					searchTerm:        "Add restricted user",
 					searchResult:      "Add restricted user, Manage other people",
-					wantValue:         "Manage other people",
+					wantValue:         nodewith.Name("Manage other people").Role(role.Heading),
 					passwordProtected: false,
 				},
 				{
 					searchTerm:        "Screen lock PIN",
 					searchResult:      "Screen lock PIN, Lock screen and sign-in",
-					wantValue:         "Lock screen and sign-in",
+					wantValue:         nodewith.NameStartingWith("Lock screen").Role(role.Heading),
 					passwordProtected: true,
 				},
 				{
 					searchTerm:        "Lock screen",
 					searchResult:      "Lock screen and sign-in, Security and Privacy",
-					wantValue:         "Lock screen and sign-in",
+					wantValue:         nodewith.NameStartingWith("Lock screen").Role(role.Heading),
 					passwordProtected: true,
 				},
 				{
 					searchTerm:        "Add Google account",
 					searchResult:      "Add Google Account, My accounts",
-					wantValue:         "Add Google Account",
+					wantValue:         nodewith.Name("Add Google Account").Role(role.Button),
 					passwordProtected: false,
 				},
 			},
@@ -93,13 +94,13 @@ func init() {
 				{
 					searchTerm:        "Fingerprint settings",
 					searchResult:      "Fingerprint settings, Lock screen and sign-in",
-					wantValue:         "Fingerprint",
+					wantValue:         nodewith.Name("Fingerprint").Role(role.Heading),
 					passwordProtected: true,
 				},
 				{
 					searchTerm:        "Add fingerprint",
 					searchResult:      "Add fingerprint, Fingerprint",
-					wantValue:         "Fingerprint",
+					wantValue:         nodewith.Name("Fingerprint").Role(role.Heading),
 					passwordProtected: true,
 				},
 			},
@@ -162,7 +163,8 @@ func SearchSettingsSections(ctx context.Context, s *testing.State) {
 				}
 			}
 
-			expectedNode := nodewith.Name(tc.wantValue).First()
+			settings := nodewith.NameStartingWith("Settings").Role(role.Window).First()
+			expectedNode := tc.wantValue.Ancestor(settings).First()
 			if err := ui.WaitUntilExists(expectedNode)(ctx); err != nil {
 				s.Fatalf("Failed to find the node with name %q: %v", tc.wantValue, err)
 			}
