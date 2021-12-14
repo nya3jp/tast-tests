38e69dc8febcaa602b2973a04330f9aca1d72638
Paul Moy <pmoy@chromium.org>
2021-12-14 13:04:18 -0700

    scanning: add RunHardwareTests()
    This function takes a list of scanners and loops through the supported
    options for each scanner to make sure the scan app can select the
    option. It will be called by a new tast test.
    
    BUG=b:195064784
    TEST=tast run --build $DUT_IP scanapp.Scan
    
    Change-Id: Ib6aed601f1536160690574a9010a181c92c283d6

--- src/chromiumos/tast/local/bundles/cros/scanapp/scanning/scanning.go
+++ src/chromiumos/tast/local/bundles/cros/scanapp/scanning/scanning.go
@@ -93,6 +93,21 @@ type ScannerStruct struct {
 	EsclCaps    string
 }
 
+// SupportedSource describes the options supported by a particular scan source.
+type SupportedSource struct {
+	SourceType           scanapp.Source       `json:"SourceType"`
+	SupportedColorModes  []scanapp.ColorMode  `json:"ColorModes"`
+	SupportedPageSizes   []scanapp.PageSize   `json:"PageSizes"`
+	SupportedResolutions []scanapp.Resolution `json:"Resolutions"`
+}
+
+// ScannerDescriptor contains the parameters used to test the scan app on real
+// hardware.
+type ScannerDescriptor struct {
+	ScannerName      string            `json:"Name"`
+	SupportedSources []SupportedSource `json:"Sources"`
+}
+
 // RunAppSettingsTests takes in the Chrome instance and the specific testing parameters
 // and performs the test, including attaching the virtual USB printer, launching
 // the scanapp, clicking through the settings, and verifying proper image output.
@@ -185,4 +200,61 @@ func RunAppSettingsTests(ctx context.Context, s *testing.State, cr *chrome.Chrom
 	// to finish using the printer (e.g. CUPS background probing).
 	usbprinter.StopPrinter(cleanupCtx, printer, devInfo)
 	printer = nil
+}
+
+// RunHardwareTests tests that the scan app can select each of the options
+// provided by `scanner`. This function is intended to be run on real hardware,
+// not the virtual USB printer.
+func RunHardwareTests(ctx context.Context, s *testing.State, cr *chrome.Chrome, scanner ScannerDescriptor) {
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	app, err := scanapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch app: ", err)
+	}
+
+	if err := app.ClickMoreSettings()(ctx); err != nil {
+		s.Fatal("Failed to expand More settings: ", err)
+	}
+
+	// Loop through all of the supported options. Skip file type for now, since
+	// that is not a property of the scanners themselves and we're not
+	// performing any real scans.
+	if err := app.SelectScanner(scanner.ScannerName)(ctx); err != nil {
+		s.Fatalf("Failed to select scanner: %s: %v", scanner.ScannerName, err)
+	}
+	// Sleep to allow the supported sources to load and stabilize.
+	if err := testing.Sleep(ctx, 2*time.Second); err != nil {
+		s.Fatal("Failed to sleep after selecting scanner: ", err)
+	}
+	for _, source := range scanner.SupportedSources {
+		if err := app.SelectSource(source.SourceType)(ctx); err != nil {
+			s.Fatalf("Failed to select source: %s: %v", source.SourceType, err)
+		}
+		// Sleep to allow the source-specific options to load and stabilize.
+		if err := testing.Sleep(ctx, 2*time.Second); err != nil {
+			s.Fatal("Failed to sleep after selecting source: ", err)
+		}
+		for _, colorMode := range source.SupportedColorModes {
+			if err := app.SelectColorMode(colorMode)(ctx); err != nil {
+				s.Fatalf("Failed to select color mode: %s: %v", colorMode, err)
+			}
+			for _, pageSize := range source.SupportedPageSizes {
+				if err := app.SelectPageSize(pageSize)(ctx); err != nil {
+					s.Fatalf("Failed to select page size: %s: %v", pageSize, err)
+				}
+				for _, resolution := range source.SupportedResolutions {
+					if err := app.SelectResolution(resolution)(ctx); err != nil {
+						s.Fatalf("Failed to select resolution: %s, %v", resolution, err)
+					}
+				}
+			}
+		}
+	}
 }
