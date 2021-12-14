a563a522bf9214e0afa2148b197c86350f4d4b86
Pathan Jilani <pathan.jilani@intel.com>
2021-12-14 09:24:52 +0530

    typec: Add USBStorageFunctionality
    Verifies USB pendrive functionality on TBT Dock station and USB4 Gatkex card
    
    BUG=b:195902740
    TEST=tast run --var=typec.dutTbtPort=<dut_TBT_port> --var=typec.cSwitchPort=<cswitch_port_ID> --var=typec.domainIP=localhost:9000 --var=ui.signinProfileTestExtensionManifestKey=<ui_mainfest_key> <DUTIP> typec.USBStorageFunctionality.usb3_pendrive_tbt_dock
    
    Change-Id: I38aad17ed53fc886136102e839e2eab757aa1682

---
+++ src/chromiumos/tast/local/bundles/cros/typec/setup/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package setup implements utils for typec testing setup.
+package setup
