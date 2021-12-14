91ef12cc421432d3fa179627ae79ec897b89be8f
Roman Sorokin <rsorokin@chromium.org>
2021-12-14 11:09:21 +0100

    [tast] oobe.Smoke: Migrate to the automation tools
    BUG=chromium:1286336
    TEST=run locally
    
    Change-Id: I9f9d96cb5ea3779f186c37a2ac7fcffad8e5684a

--- src/chromiumos/tast/local/bundles/cros/oobe/smoke.go
+++ src/chromiumos/tast/local/bundles/cros/oobe/smoke.go
@@ -6,8 +6,14 @@ package oobe
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/chrome/uiauto/state"
 	"chromiumos/tast/testing"
 )
 
@@ -22,11 +28,14 @@ func init() {
 		},
 		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
+		VarDeps:      []string{"ui.signinProfileTestExtensionManifestKey"},
 	})
 }
 
 func Smoke(ctx context.Context, s *testing.State) {
-	cr, err := chrome.New(ctx, chrome.NoLogin())
+	cr, err := chrome.New(ctx,
+		chrome.NoLogin(),
+		chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey")))
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
@@ -38,11 +47,24 @@ func Smoke(ctx context.Context, s *testing.State) {
 	}
 	defer oobeConn.Close()
 
+	tconn, err := cr.SigninProfileTestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create the signin profile test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	ui := uiauto.New(tconn).WithTimeout(10 * time.Second)
+
 	if err := oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.WelcomeScreen.isVisible()"); err != nil {
 		s.Fatal("Failed to wait for the welcome screen to be visible: ", err)
 	}
-	if err := oobeConn.Eval(ctx, "OobeAPI.screens.WelcomeScreen.clickNext()", nil); err != nil {
-		s.Fatal("Failed to click welcome page next button: ", err)
+
+	focusedButton := nodewith.State(state.Focused, true).Role(role.Button)
+	if err := uiauto.Combine("Click next on the welcome screen",
+		ui.WaitUntilExists(focusedButton),
+		ui.LeftClick(focusedButton),
+	)(ctx); err != nil {
+		s.Fatal("Failed to click welcome screen next button: ", err)
 	}
 
 	shouldSkipNetworkScreen := false
@@ -54,10 +76,7 @@ func Smoke(ctx context.Context, s *testing.State) {
 		if err := oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.NetworkScreen.isVisible()"); err != nil {
 			s.Fatal("Failed to wait for the network screen to be visible: ", err)
 		}
-		if err := oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.NetworkScreen.nextButton.isEnabled()"); err != nil {
-			s.Fatal("Failed to wait for the network screen next button to be enabled: ", err)
-		}
-		if err := oobeConn.Eval(ctx, "OobeAPI.screens.NetworkScreen.clickNext()", nil); err != nil {
+		if err := ui.LeftClickUntil(focusedButton, ui.Gone(focusedButton))(ctx); err != nil {
 			s.Fatal("Failed to click network page next button: ", err)
 		}
 	}
@@ -71,10 +90,20 @@ func Smoke(ctx context.Context, s *testing.State) {
 		if err := oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.EulaScreen.isVisible()"); err != nil {
 			s.Fatal("Failed to wait for the eula screen to be visible: ", err)
 		}
-		if err := oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.EulaScreen.nextButton.isEnabled()"); err != nil {
-			s.Fatal("Failed to wait for the accept eula button to be enabled: ", err)
+		eulaWebview := nodewith.State(state.Focused, true).Role(role.Iframe)
+		if err := ui.WaitUntilExists(eulaWebview)(ctx); err != nil {
+			s.Fatal("Failed to wait for the eula screen to be visible: ", err)
 		}
-		if err := oobeConn.Eval(ctx, "OobeAPI.screens.EulaScreen.clickNext()", nil); err != nil {
+		var eulaNextButton string
+		if err := oobeConn.Eval(ctx, "OobeAPI.screens.EulaScreen.getNextButtonName()", &eulaNextButton); err != nil {
+			s.Fatal("Failed to get eula next button name: ", err)
+		}
+		eulaScreenNextButton := nodewith.Role(role.Button).Name(eulaNextButton)
+		if err := uiauto.Combine("Click next on the EULA screen",
+			// Button is not focused on the screen. We focus the webview with EULA by default.
+			ui.WaitUntilExists(eulaScreenNextButton.State(state.Focused, false)),
+			ui.LeftClickUntil(eulaScreenNextButton, ui.Gone(eulaScreenNextButton)),
+		)(ctx); err != nil {
 			s.Fatal("Failed to click accept eula button: ", err)
 		}
 	}
@@ -82,11 +111,13 @@ func Smoke(ctx context.Context, s *testing.State) {
 	if err := oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.UserCreationScreen.isVisible()"); err != nil {
 		s.Fatal("Failed to wait for the user creation screen to be visible: ", err)
 	}
-	if err := oobeConn.Eval(ctx, "OobeAPI.screens.UserCreationScreen.clickNext()", nil); err != nil {
+
+	if err := ui.LeftClickUntil(focusedButton, ui.Gone(focusedButton))(ctx); err != nil {
 		s.Fatal("Failed to click user creation screen next button: ", err)
 	}
 
-	if err := oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.GaiaScreen.isVisible()"); err != nil {
+	gaiaInput := nodewith.State(state.Focused, true).Role(role.TextField).Ancestor(nodewith.Role(role.Iframe))
+	if err := ui.WaitUntilExists(gaiaInput)(ctx); err != nil {
 		s.Fatal("Failed to wait for the login screen to be visible: ", err)
 	}
 }
