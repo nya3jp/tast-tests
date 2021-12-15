4e92f063cbc6bcee3da65680e7d1dd63f089b97a
kevin wu <kevin.wu@cienet.com>
2021-12-15 16:56:54 +0800

    mta: add test case of taskmanager.BrowserEndProcessDisabled
    [MTA 9ccb9] Verify that "Browser" cannot be killed from Task Manager.
    
    BUG=b:190654712
    TEST=tast run <DUT> taskmanager.BrowserEndProcessDisabled
    
    Change-Id: I81746a43d57d9810547d52221f1276da9a0422e9

---
+++ src/chromiumos/tast/local/bundles/cros/taskmanager/browser_end_process_disabled.go
@@ -0,0 +1,66 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package taskmanager
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/restriction"
+	"chromiumos/tast/local/chrome/uiauto/taskmanager"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     BrowserEndProcessDisabled,
+		Desc:     "Verify that 'Browser' cannot be killed from Task Manager",
+		Contacts: []string{"kevin.wu@cienet.com", "cash.hsu@cienet.com", "cienet-development@googlegroups.com", "chromeos-sw-engprod@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+		Fixture:  "chromeLoggedIn",
+	})
+}
+
+// BrowserEndProcessDisabled verifies that "Browser" cannot be killed from Task Manager.
+func BrowserEndProcessDisabled(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to take keyboard: ", err)
+	}
+	defer kb.Close()
+
+	s.Log("Open Task Manager")
+	tm := taskmanager.New(tconn, kb)
+	if err := tm.Open(ctx); err != nil {
+		s.Fatal("Failed to open Task Manager: ", err)
+	}
+	defer func(ctx context.Context) {
+		faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_dump")
+		tm.Close(ctx, tconn)
+	}(cleanupCtx)
+
+	if err := uiauto.Combine("check end process state",
+		tm.SelectProcess(taskmanager.FindProcess().Name("Browser")),
+		uiauto.New(tconn).CheckRestriction(taskmanager.EndProcessFinder, restriction.Disabled),
+	)(ctx); err != nil {
+		s.Fatal("Failed to disable end process for Browser: ", err)
+	}
+}
