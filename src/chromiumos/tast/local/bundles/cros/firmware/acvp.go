56806f916508bd02698f000d3ee09fb06ad22b4f
Mary Ruthven <mruthven@chromium.org>
2021-12-16 12:25:03 -0800

    acvp: add support for firmware hash vectors
    Firmware hash tests need to send CMD_START with an empty message and
    then CMD_FINISH with the request text. Modify getHashCommands to support
    the firmware hash commands
    
    Add a sha2_256_short_fw test that will use the firmware path.
    
    BUG=b:210879337
    TEST=tast run ${IP} firmware.ACVP.sha2_256_short
    tast run ${IP} firmware.ACVP.sha2_256_short_fw
    
    Change-Id: I7c231a20ad4359a93cfd2b1f525d78bf35337ca2

--- src/chromiumos/tast/local/bundles/cros/firmware/acvp.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/acvp.go
@@ -112,6 +112,17 @@ func init() {
 				"sha2-256-short-expected.json",
 			},
 		}, {
+			Name: "sha2_256_short_fw",
+			Val: data{
+				inputFile:    "sha2-256-short.json",
+				expectedFile: "sha2-256-short-expected.json",
+				cmdType:      "firmware",
+			},
+			ExtraData: []string{
+				"sha2-256-short.json",
+				"sha2-256-short-expected.json",
+			},
+		}, {
 			Name: "sha2_384_full",
 			Val: data{
 				inputFile:    "sha2-384-full.json",
@@ -242,6 +253,8 @@ const (
 	// 0 - start, 1 - cont., 2 - finish, 3 - single
 	// 4 - SW HMAC single shot (TPM code)
 	// 5 - HW HMAC SHA256 single shot (dcrypto code)
+	tpmHashStart     = "00"
+	tpmHashFinish    = "02"
 	tpmHashCmdMode   = "03"
 	tpmHMACCmdModeFW = "04"
 	tpmHMACCmdModeHW = "05"
@@ -681,7 +694,7 @@ func getTrunksCmds(b []byte) ([]string, uint8, error) {
 		if err != nil {
 			return nil, 0, err
 		}
-		return []string{getHashCommand(hp)}, 1, nil
+		return getHashCommands(hp), 1, nil
 	case "hmacDRBG":
 		if argLen != 7 {
 			return nil, 0, errors.Errorf("incorrect number of args for DRBG: got %d, want 7", argLen)
@@ -794,8 +807,14 @@ func getHashCommand(hp *hashPrimitive) string {
 	cmdBody.WriteString(hp.cmd)
 	cmdBody.WriteString(hp.alg)
 	cmdBody.WriteString("00")
-	cmdBody.WriteString(fmt.Sprintf("%04x", len(hp.msg)/2))
-	cmdBody.WriteString(hp.msg)
+
+	// Don't send a message with hash start.
+	if hp.cmd == tpmHashStart {
+		cmdBody.WriteString("0000")
+	} else {
+		cmdBody.WriteString(fmt.Sprintf("%04x", len(hp.msg)/2))
+		cmdBody.WriteString(hp.msg)
+	}
 	if hp.cmd == tpmHMACCmdModeHW || hp.cmd == tpmHMACCmdModeFW {
 		cmdBody.WriteString(fmt.Sprintf("%04x", len(hp.key)/2))
 		cmdBody.WriteString(hp.key)
@@ -803,6 +822,21 @@ func getHashCommand(hp *hashPrimitive) string {
 	cmdHeader.WriteString(fmt.Sprintf("%08x", cmdBody.Len()/2+cr50HeaderSize))
 	cmdHeader.WriteString("200000000001")
 	return cmdHeader.String() + cmdBody.String()
+}
+
+// getHashCommands constructs the list of hash commands for the hash type.
+func getHashCommands(hp *hashPrimitive) []string {
+	// HMAC and Hardware Hash commands only need one command.
+	if hp.cmd != tpmHashCmdMode || !acvpIsFirmwareTest {
+		return []string{getHashCommand(hp)}
+	}
+	// The firmware hash sequence is tpmHashStart then tpmHashFinish.
+	result := make([]string, 2)
+	hp.cmd = tpmHashStart
+	result[0] = getHashCommand(hp)
+	hp.cmd = tpmHashFinish
+	result[1] = getHashCommand(hp)
+	return result
 }
 
 // getAESCommand constructs a trunks AES command
