56ebf40c8fe50f09fab3bc19c74b6b41fe039a53
Anastasiia N <anastasiian@google.com>
2021-12-20 14:44:57 +0100

    tast-tests: Stabilize system account addition
    The iframe that shows the Gaia sign-in screen is hidden by default and
    is being shown after the webpage inside it is loaded. It looks like
    sometimes the ui tree is not updated at the right moment and it thinks
    that the iframe node is empty. Fix this by going to the previous screen
    and navigating back to the iframe node. This will hide and show the node
    and will make the ui tree update.
    
    Add accountmanager.SystemDialog test to track the stability of the
    account addition dialog separately from other tests.
    
    BUG=chromium:1268351
    TEST=tast run <ip> accountmanager.*
    
    Change-Id: I826b53ebf932d6d68727b9b59ba7f2a6566780a5

---
+++ src/chromiumos/tast/local/bundles/cros/accountmanager/system_dialog.go
@@ -0,0 +1,78 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package accountmanager provides functions to manage accounts in-session.
+package accountmanager
+
+import (
+	"context"
+
+	"chromiumos/tast/local/accountmanager"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SystemDialog,
+		Desc:         "In-session account addition using system 'Add account' dialog",
+		Contacts:     []string{"anastasiian@chromium.org", "team-dent@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Pre:          chrome.LoggedIn(),
+		VarDeps:      []string{"accountmanager.username1", "accountmanager.password1"},
+	})
+}
+
+func SystemDialog(ctx context.Context, s *testing.State) {
+	username := s.RequiredVar("accountmanager.username1")
+	password := s.RequiredVar("accountmanager.password1")
+
+	cr := s.PreValue().(*chrome.Chrome)
+	// Connect to Test API to use it with the UI library.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	s.Log("Runing test cleanup")
+	if err := accountmanager.TestCleanup(ctx, tconn, cr, browser.TypeAsh); err != nil {
+		s.Fatal("Failed to do cleanup: ", err)
+	}
+
+	ui := uiauto.New(tconn).WithTimeout(accountmanager.DefaultUITimeout)
+
+	// Open Account Manager page in OS Settings and find Add Google Account button.
+	addAccountButton := nodewith.Name("Add Google Account").Role(role.Button)
+	if _, err := ossettings.LaunchAtPageURL(ctx, tconn, cr, "accountManager", ui.Exists(addAccountButton)); err != nil {
+		s.Fatal("Failed to launch Account Manager page: ", err)
+	}
+
+	// Click the button to open account addition dialog.
+	if err := ui.LeftClick(addAccountButton)(ctx); err != nil {
+		s.Fatal("Failed to click Add Google Account button: ", err)
+	}
+
+	s.Log("Adding a secondary Account")
+	if err := accountmanager.AddAccount(ctx, tconn, username, password); err != nil {
+		s.Fatal("Failed to add a secondary Account: ", err)
+	}
+
+	// Make sure that the settings page is focused again.
+	if err := ui.WaitUntilExists(addAccountButton)(ctx); err != nil {
+		s.Fatal("Failed to find Add Google Account button: ", err)
+	}
+	// Find "More actions, <email>" button to make sure that account was added.
+	moreActionsButton := nodewith.Name("More actions, " + username).Role(role.Button)
+	if err := ui.WaitUntilExists(moreActionsButton)(ctx); err != nil {
+		s.Fatal("Failed to find More actions button: ", err)
+	}
+}
