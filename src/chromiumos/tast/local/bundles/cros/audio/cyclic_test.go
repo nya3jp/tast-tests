701051197653f0455ced51e93b777af088183c94
Eddy Hsu <eddyhsu@google.com>
2021-12-20 09:34:24 +0800

    [WIP] tast-tests: Add CyclicTest tests.
    Add CyclicTest tests which will benchmark and ensure the os providing
    required scheduling lantency.
    
    BUG=b:211523913
    TEST=tast run ${DUT_IP} audio.CyclicTest.*
    
    Change-Id: I39f15bcfd6f529dd16b6ac707a71ae9ee561629a

---
+++ src/chromiumos/tast/local/bundles/cros/audio/cyclic_test.go
@@ -0,0 +1,192 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package audio
+
+import (
+	"context"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+// cyclicTestParameters contains all the data needed to run a single test iteration.
+type cyclicTestParameters struct {
+	Priority       int // Priority of the process
+	Threads        int // Number of threads
+	IntervalUs     int // Interval time
+	Loops          int // Number of times
+	P99ThresholdUs int // P99 latency threshold
+}
+
+const CrasPriority = 12
+const CrasClientPriority = 10
+const DefaultIntervalUs = 1000
+const DefaultLoops = 1000
+const DefaultP99ThresholdUs = 40
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     CyclicTest,
+		Desc:     "benchmarks of cyclic_test",
+		Contacts: []string{"eddyhsu@chromium.org"},
+		Attr:     []string{"group:crosbolt", "crosbolt_perbuild"},
+		Timeout:  2 * time.Minute,
+		Params: []testing.Param{
+			{
+				Name: "cras_pr",
+				Val: cyclicTestParameters{
+					Priority:       CrasPriority,
+					Threads:        1,
+					IntervalUs:     DefaultIntervalUs,
+					Loops:          DefaultLoops,
+					P99ThresholdUs: DefaultP99ThresholdUs,
+				},
+			},
+			{
+				Name: "cras_client_pr",
+				Val: cyclicTestParameters{
+					Priority:       CrasClientPriority,
+					Threads:        1,
+					IntervalUs:     DefaultIntervalUs,
+					Loops:          DefaultLoops,
+					P99ThresholdUs: DefaultP99ThresholdUs,
+				},
+			},
+			{
+				Name: "cras_pr_multi_thread",
+				Val: cyclicTestParameters{
+					Priority:       CrasPriority,
+					Threads:        4,
+					IntervalUs:     DefaultIntervalUs,
+					Loops:          DefaultLoops,
+					P99ThresholdUs: DefaultP99ThresholdUs,
+				},
+			},
+			{
+				Name: "cras_client_pr_multi_thread",
+				Val: cyclicTestParameters{
+					Priority:       CrasClientPriority,
+					Threads:        4,
+					IntervalUs:     DefaultIntervalUs,
+					Loops:          DefaultLoops,
+					P99ThresholdUs: DefaultP99ThresholdUs,
+				},
+			},
+		},
+	})
+}
+
+// cyclicTestStat contains the statistics of a thread.
+type cyclicTestStat struct {
+	median int
+	p99    int
+}
+
+// ParseLatency parses `log`, collects the latencies of each thread
+// and returns the results.
+// The order of latencies for each thread is the same as the log.
+func ParseLatency(log string, threads int, s *testing.State) [][]int {
+	latencies := make([][]int, threads)
+	dataRe := regexp.MustCompile(`^[ \t]+\d+:[ \t]+\d+:[ \t]+\d+$`)
+	integerRe := regexp.MustCompile(`\d+`)
+	for _, line := range strings.Split(log, "\n") {
+		if !dataRe.MatchString(line) {
+			continue
+		}
+		ints := integerRe.FindAllString(line, 3)
+
+		tid, err := strconv.Atoi(ints[0])
+		if err != nil {
+			s.Fatal("Failed to parse data: ", err)
+		}
+		latency, err := strconv.Atoi(ints[2])
+		if err != nil {
+			s.Fatal("Failed to parse data: ", err)
+		}
+
+		latencies[tid] = append(latencies[tid], latency)
+	}
+	return latencies
+}
+
+// CalculateStats calcultes the statistics results of `latencies` and
+// returns as `cyclicTestStat`.
+func CalculateStats(latencies [][]int) []cyclicTestStat {
+	stats := make([]cyclicTestStat, len(latencies))
+	for idx := range stats {
+		sort.Ints(latencies[idx])
+		count := len(latencies[idx])
+		if count == 0 {
+			continue
+		}
+		stats[idx].median = latencies[idx][count/2]
+		stats[idx].p99 = latencies[idx][count*99/100]
+	}
+	return stats
+}
+
+func CyclicTest(ctx context.Context, s *testing.State) {
+	param := s.Param().(cyclicTestParameters)
+	out, err := testexec.CommandContext(ctx, "cyclictest",
+		"--priority="+strconv.Itoa(param.Priority),
+		"--interval="+strconv.Itoa(param.IntervalUs),
+		"--threads="+strconv.Itoa(param.Threads),
+		"--loops="+strconv.Itoa(param.Loops),
+		"--distance=0",
+		"--verbose").Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to execute cyclictest: ", err)
+	}
+
+	// The log will look like(task_number:count:latency_us):
+	// Max CPUs = 8
+	// Online CPUs = 8
+	// # /dev/cpu_dma_latency set to 0us
+	// Thread 0 Interval: 1000
+	//        0:       0:       9
+	//        0:       1:      18
+	//        0:       2:      15
+	//        0:       3:      14
+	//        0:       4:      14
+	//        0:       5:      14
+	//        0:       6:      24
+	//        0:       7:      16
+	//        0:       8:      15
+	//        0:       9:      14
+	// ...
+	latencies := ParseLatency(string(out), param.Threads, s)
+	stats := CalculateStats(latencies)
+
+	p := perf.NewValues()
+	for index, stat := range stats {
+		name := "Thread_" + strconv.Itoa(index)
+		p99Latency := perf.Metric{
+			Name:      name,
+			Variant:   "p99_latency",
+			Unit:      "us",
+			Direction: perf.SmallerIsBetter}
+		p.Set(p99Latency, float64(stat.p99))
+		medianLatency := perf.Metric{
+			Name:      name,
+			Variant:   "p50_latency",
+			Unit:      "us",
+			Direction: perf.SmallerIsBetter}
+		p.Set(medianLatency, float64(stat.median))
+
+		if stat.p99 > param.P99ThresholdUs {
+			s.Error("p99 latency exceeds threshold: ", stat.p99,
+				" > ", param.P99ThresholdUs)
+		}
+	}
+	if err := p.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
