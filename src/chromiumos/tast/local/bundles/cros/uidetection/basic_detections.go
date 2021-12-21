00df3de1f5431264f5bfc37c57cdcd6874f09343
Alvin Jia <alvinjia@google.com>
2021-12-21 14:54:10 +1100

    tast-tests: add a test for uidetection lib
    Add a Tast test to verify UI detection library is working,
    which helps to debug uidetection API related test failures.
    
    BUG=b:211514474
    TEST=tast run <DUT> uidetection.BasicDetections
    
    Change-Id: I922c2494b5ab5f019684a10905b0bf40711772b2

---
+++ src/chromiumos/tast/local/bundles/cros/uidetection/basic_detections.go
@@ -0,0 +1,70 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package uidetection
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/uidetection"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         BasicDetections,
+		Desc:         "Confirm that the image-based uidetetion library works as intended",
+		Contacts:     []string{"alvinjia@google.com", "chromeos-engprod-sydney@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedIn",
+		Timeout:      3 * time.Minute,
+		Data:         []string{"logo_chrome.png"},
+	})
+}
+
+// expectError returns an error if the error returned by an action doesn't match the expectation.
+func expectError(action uiauto.Action, expectation string) func(ctx context.Context) error {
+	return func(ctx context.Context) error {
+		if err := action(ctx); err == nil {
+			return errors.Errorf("didn't get the expected error: %s", expectation)
+		} else if !strings.Contains(err.Error(), expectation) {
+			return errors.Errorf("expected error: %s, actual error: %s", expectation, err)
+		} else {
+			return nil
+		}
+	}
+}
+
+func BasicDetections(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "basic_detections")
+
+	ud := uidetection.NewDefault(tconn)
+	// Perform UI interaction to click Chrome logo to open Chrome,
+	// click "Add shortcut", and click "cancel".
+	// This covers all three UI detections: custom icon detection,
+	// textblock detection and word detection.
+	if err := uiauto.Combine("verify detections",
+		ud.LeftClick(uidetection.CustomIcon(s.DataPath("logo_chrome.png"))),
+		ud.LeftClick(uidetection.TextBlock([]string{"Add", "shortcut"})),
+		ud.LeftClick(uidetection.Word("Cancel")),
+		ud.WaitUntilGone(uidetection.Word("Cancel")),
+		expectError(ud.Exists(uidetection.Word("Google")), uidetection.ErrMultipleMatch),
+		expectError(ud.Exists(uidetection.Word("Google").Nth(4)), uidetection.ErrNthNotFound),
+		expectError(ud.Exists(uidetection.TextBlock([]string{"Add", "shortcut"}).Nth(2)), uidetection.ErrMultipleMatch),
+	)(ctx); err != nil {
+		s.Fatal("Failed to perform image-based UI interactions: ", err)
+	}
+}
---
+++ src/chromiumos/tast/local/bundles/cros/uidetection/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package uidetection contains code to test the uidetection library.
+package uidetection
