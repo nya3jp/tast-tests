ae24252c0d4634a5bff89a81e2f4a49a5826fc39
Alvin Jia <alvinjia@google.com>
2021-12-21 14:54:10 +1100

    tast-tests: add a test for uidetection lib
    Add a Tast test to verify UI detection library is working,
    which helps to debug uidetection API related test failures.
    
    BUG=b:211514474
    TEST=tast run <DUT> uidetection.BasicDetections
    
    Change-Id: I922c2494b5ab5f019684a10905b0bf40711772b2

--- src/chromiumos/tast/local/bundles/cros/imports.go
+++ src/chromiumos/tast/local/bundles/cros/imports.go
@@ -93,6 +93,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/typec"
 	_ "chromiumos/tast/local/bundles/cros/u2fd"
 	_ "chromiumos/tast/local/bundles/cros/ui"
+	_ "chromiumos/tast/local/bundles/cros/uidetection"
 	_ "chromiumos/tast/local/bundles/cros/video"
 	_ "chromiumos/tast/local/bundles/cros/vm"
 	_ "chromiumos/tast/local/bundles/cros/wallpaper"
