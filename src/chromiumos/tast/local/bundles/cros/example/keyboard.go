86bd8fc476b8074eb0751dbf4386ee838c75827d
Eric Lok <lokeric@google.com>
2021-12-22 07:50:58 +0000

    tast: example.Keyboard is again not capturing correct input text
    Problem is occuring because after <input> is in focus, it
    does not immediately allow for keyboard text input (there's a
    slight time delay). More details in attached bug
    
    BUG=b:211720347
    TEST=~/trunk/src/platform/tast/fast_build.sh -T
    tast run <DUT> example.Keyboard
    
    Change-Id: I3dcb150bb836461818a78342fc60839a97dbb23e

--- src/chromiumos/tast/local/bundles/cros/example/keyboard.go
+++ src/chromiumos/tast/local/bundles/cros/example/keyboard.go
@@ -6,14 +6,13 @@ package example
 
 import (
 	"context"
-	"io"
-	"net/http"
-	"net/http/httptest"
 	"time"
 
-	"chromiumos/tast/errors"
-	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/a11y"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/policyutil/fixtures"
 	"chromiumos/tast/testing"
 )
 
@@ -25,48 +24,40 @@ func init() {
 		Contacts:     []string{"ricardoq@chromium.org", "tast-owners@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
-		Pre:          chrome.LoggedIn(),
+		Fixture:      "chromePolicyLoggedIn",
 	})
 }
 
 func Keyboard(ctx context.Context, s *testing.State) {
+	// Test Values
 	const (
-		html        = "<!DOCTYPE html><input id='text' type='text' autofocus>"
-		elementExpr = "document.getElementById('text')"
-		valueExpr   = elementExpr + ".value"
+		html      = "<input id='text' type='text' autofocus>"
+		inputText = "Hello, world!"
 	)
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/html")
-		io.WriteString(w, html)
-	}))
-	defer server.Close()
 
-	s.Log("Loading input page")
-	cr := s.PreValue().(*chrome.Chrome)
-	conn, err := cr.NewConn(ctx, server.URL)
+	// 1. Boilerplate setup + create tab with input form
+	cr := s.FixtValue().(*fixtures.FixtData).Chrome()
+
+	c, err := a11y.NewTabWithHTML(ctx, cr, html)
 	if err != nil {
-		s.Fatal("Creating renderer failed: ", err)
+		s.Fatal("Failed to open a new tab with HTML: ", err)
 	}
-	defer conn.Close()
+	defer c.Close()
 
-	// waitForStringExpr waits for expr to evaluate to expected with short timeout.
-	waitForStringExpr := func(expr, expected string) error {
-		s.Log("Waiting for text from ", expr)
-		return testing.Poll(ctx, func(ctx context.Context) error {
-			var s string
-			if err := conn.Eval(ctx, expr, &s); err != nil {
-				return err
-			}
-			if s != expected {
-				return errors.Errorf("%s = %q; want %q", expr, s, expected)
-			}
-			return nil
-		}, &testing.PollOptions{Timeout: 10 * time.Second})
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
 	}
+	ui := uiauto.New(tconn).WithTimeout(10 * time.Second)
 
+	// 2. Wait for focux on text box, then enter input text value
 	s.Log("Waiting for focus")
-	if err := conn.WaitForExpr(ctx, elementExpr+" === document.activeElement"); err != nil {
-		s.Fatal("Failed waiting for focus: ", err)
+	textbox := nodewith.First().Onscreen()
+	if err := uiauto.Combine("Focus text box",
+		ui.WaitUntilExists(textbox),
+		ui.FocusAndWait(textbox),
+	)(ctx); err != nil {
+		s.Fatal("Failed to focus the text box: ", err)
 	}
 
 	s.Log("Finding and opening keyboard device")
@@ -76,15 +67,15 @@ func Keyboard(ctx context.Context, s *testing.State) {
 	}
 	defer ew.Close()
 
-	const inputText = "Hello, world!"
 	s.Logf("Injecting keyboard events for %q", inputText)
 	if err = ew.Type(ctx, inputText); err != nil {
 		s.Fatal("Failed to write events: ", err)
 	}
-	if err := waitForStringExpr(valueExpr, inputText); err != nil {
-		// Stop test here for getting screenshot of this wrong state.
-		// TODO(yamaguchi): Change back to Error() once the flakiness is fixed.
-		s.Fatal("Failed to get input text (this can be flaky): ", err)
+
+	// 3. Assert inputted text matches expected value
+	textboxWithContent := nodewith.Attribute("value", inputText).Onscreen()
+	if err := ui.WaitUntilExists(textboxWithContent)(ctx); err != nil {
+		s.Fatal("Failed to verify text input: ", err)
 	}
 
 	const (
@@ -98,9 +89,6 @@ func Keyboard(ctx context.Context, s *testing.State) {
 	}
 	if err := ew.Type(ctx, dataURL+"\n"); err != nil {
 		s.Fatal("Failed to write events: ", err)
-	}
-	if err := waitForStringExpr(bodyExpr, pageText); err != nil {
-		s.Error("Failed to get page text: ", err)
 	}
 
 	// Not all Chromebooks have the same layout for the function keys.
