69c2dad808ee26c5a4224db41ca83132992f8032
Ashley Prasad <ashleydp@google.com>
2021-12-28 19:57:22 +0000

    firmwareupdateapp: create app renders test
    - Configures firmware update app tast "category" for testing.
    - Adds basic test verifying app opens successfully when enabled.
    
    BUG=chromium:1252981
    TEST=tast run ${DUT} firmwareupdate.RenderApp
    
    Change-Id: I512f5d41ce279d713b157e687db5ebc117b5bbf9

---
+++ src/chromiumos/tast/local/chrome/uiauto/firmwareupdateapp/firmwareupdateapp.go
@@ -0,0 +1,52 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package firmwareupdateapp contains drivers for controlling the ui of
+// firmware update SWA.
+package firmwareupdateapp
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+)
+
+var appRootNodeParams = nodewith.Name(apps.FirmwareUpdate.Name).Role(role.Window)
+
+// FirmwareUpdateRootNode returns the root ui node of Firmware Update
+// application.
+// An error is returned if root node cannot be found in UI.
+func FirmwareUpdateRootNode(ctx context.Context, tconn *chrome.TestConn) (*nodewith.Finder, error) {
+	ui := uiauto.New(tconn)
+	err := ui.WithTimeout(20 * time.Second).WaitUntilExists(appRootNodeParams)(ctx)
+	return appRootNodeParams, err
+}
+
+// Launch opens firmware update then returns results of attempting to access
+// the application root node.
+// An error is returned if the application fails to launch.
+func Launch(ctx context.Context, tconn *chrome.TestConn) (*nodewith.Finder, error) {
+	err := apps.Launch(ctx, tconn, apps.FirmwareUpdate.ID)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to launch firmware update app")
+	}
+
+	err = ash.WaitForApp(ctx, tconn, apps.FirmwareUpdate.ID, time.Minute)
+	if err != nil {
+		return nil, errors.Wrap(err, "firmware update  app did not appear in shelf after launch")
+	}
+
+	dxRootnode, err := FirmwareUpdateRootNode(ctx, tconn)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find firmware update  app")
+	}
+	return dxRootnode, nil
+}
