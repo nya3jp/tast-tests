33378308cb05699db1a16a60e7beae79818c9891
Jonathan Fan <jonfan@google.com>
2021-12-30 17:14:04 -0800

    Add external facing grpc svc KeyboardService
    It is part of the Exposing Tast Services through gRPC initiative.
    The service is intended for use outside of Tast (e.g. Mobly).
    For more information, see http://go/tast-tcp-grpc-svc
    
    This CL includes:
    1) Expose keyboard functionalities Type, Accel, AccelPress, AccelRelease
    2) Remote test for KeyboardService
    
    BUG=b:208267010
    TEST=tast run -build=true <DUT> ui.KeyboardServiceGRPC
    
    Cq-Depend: chromium:3362668
    Change-Id: Idd136396a202f21aa0d63d0017bc632e34c109a2

---
+++ src/chromiumos/tast/remote/bundles/cros/inputs/keyboard_service_grpc.go
@@ -0,0 +1,181 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package inputs
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/crosserverutil"
+	chromepb "chromiumos/tast/services/cros/browser"
+	inputspb "chromiumos/tast/services/cros/inputs"
+	uipb "chromiumos/tast/services/cros/ui"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         KeyboardServiceGRPC,
+		Desc:         "Check basic functionality of KeyboardService",
+		Contacts:     []string{"jonfan@google.com", "chromeos-sw-engprod@google.com"},
+		SoftwareDeps: []string{"chrome"},
+		Vars:         []string{"grpcServerPort"},
+		HardwareDeps: hwdep.D(hwdep.FormFactor(hwdep.Clamshell)),
+	})
+}
+
+// KeyboardServiceGRPC check KeyboardService functionalities like Type, Accel, AccelPress and AccelRelease.
+func KeyboardServiceGRPC(ctx context.Context, s *testing.State) {
+	//Connect to gRPC Server on DUT.
+	hostname, port := getGRPCHostnamePort(ctx, s)
+	cl, err := crosserverutil.Dial(ctx, s.DUT(), hostname, port)
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	// Start Chrome on the DUT.
+	cs := chromepb.NewChromeServiceClient(cl.Conn)
+	loginReq := &chromepb.NewRequest{}
+	if _, err := cs.New(ctx, loginReq, grpc.WaitForReady(true)); err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cs.Close(ctx, &empty.Empty{})
+
+	uiautoSvc := uipb.NewAutomationServiceClient(cl.Conn)
+	keyboardSvc := inputspb.NewKeyboardServiceClient(cl.Conn)
+
+	// Open launcher and start apps with keyboard shortcuts.
+	launcherButton := &uipb.Finder{
+		NodeWiths: []*uipb.NodeWith{
+			{Value: &uipb.NodeWith_HasClass{HasClass: "ash/HomeButton"}},
+			{Value: &uipb.NodeWith_Name{Name: "Launcher"}},
+		},
+	}
+	if _, err := uiautoSvc.WaitUntilExists(ctx, &uipb.WaitUntilExistsRequest{Finder: launcherButton}); err != nil {
+		s.Fatal("Failed to find launcher button: ", err)
+	}
+
+	for _, appName := range []string{"Chrome", "Files"} {
+		if err := startAppFromLauncher(ctx, uiautoSvc, keyboardSvc, appName); err != nil {
+			s.Fatal(fmt.Sprintf("failed to start %v app with keyboard: ", appName), err)
+		}
+	}
+
+	// Use keyboard Alt-Tab to change the focus to from Files to Chrome App.
+	if err := altTab(ctx, keyboardSvc); err != nil {
+		s.Fatal("Failed to Alt-Tab: ", err)
+	}
+
+	// Close apps with shortcuts.
+	browserWindow := &uipb.Finder{
+		NodeWiths: []*uipb.NodeWith{
+			{Value: &uipb.NodeWith_HasClass{HasClass: "BrowserFrame"}},
+			{Value: &uipb.NodeWith_Name{Name: "Chrome - New Tab"}},
+		},
+	}
+	filesAppWindow := &uipb.Finder{
+		NodeWiths: []*uipb.NodeWith{
+			{Value: &uipb.NodeWith_HasClass{HasClass: "Widget"}},
+			{Value: &uipb.NodeWith_Name{Name: "Files - My files"}},
+		},
+	}
+
+	for _, appFinder := range []*uipb.Finder{browserWindow, filesAppWindow} {
+		if err := verifyCloseAppWithShortcuts(ctx, uiautoSvc, keyboardSvc, appFinder); err != nil {
+			s.Fatal("Failed to verify closing app with shortcuts: ", err)
+		}
+	}
+}
+
+func verifyCloseAppWithShortcuts(ctx context.Context, uiautoSvc uipb.AutomationServiceClient,
+	keyboardSvc inputspb.KeyboardServiceClient, appFinder *uipb.Finder) error {
+	//Verify that app is present to begin with.
+	if res, _ := uiautoSvc.IsNodeFound(ctx, &uipb.IsNodeFoundRequest{Finder: appFinder}); !res.Found {
+		return errors.New("failed to find app")
+	}
+
+	//Use keyboard shortcut to close app.
+	if _, err := keyboardSvc.Accel(ctx, &inputspb.AccelRequest{Key: "Ctrl+W"}); err != nil {
+		return errors.Wrap(err, "failed to type Ctrl+W")
+	}
+
+	testing.Sleep(ctx, 200*time.Millisecond)
+
+	if res, _ := uiautoSvc.IsNodeFound(ctx, &uipb.IsNodeFoundRequest{Finder: appFinder}); res.Found {
+		return errors.New("failed to close app")
+	}
+
+	return nil
+}
+
+func altTab(ctx context.Context, keyboardSvc inputspb.KeyboardServiceClient) error {
+
+	if _, err := keyboardSvc.AccelPress(ctx, &inputspb.AccelPressRequest{Key: "Alt"}); err != nil {
+		return errors.Wrap(err, "failed to press alt")
+	}
+	defer keyboardSvc.AccelRelease(ctx, &inputspb.AccelReleaseRequest{Key: "Alt"})
+	if err := testing.Sleep(ctx, 500*time.Millisecond); err != nil {
+		return errors.Wrap(err, "failed to wait")
+	}
+	if _, err := keyboardSvc.Accel(ctx, &inputspb.AccelRequest{Key: "Tab"}); err != nil {
+		return errors.Wrap(err, "failed to type tab")
+	}
+	if err := testing.Sleep(ctx, time.Second); err != nil {
+		return errors.Wrap(err, "failed to wait")
+	}
+
+	return nil
+}
+
+func startAppFromLauncher(ctx context.Context, uiautoSvc uipb.AutomationServiceClient, keyboardSvc inputspb.KeyboardServiceClient, appName string) error {
+	// Press the search key to bring the launcher into focus.
+	if _, err := keyboardSvc.Accel(ctx, &inputspb.AccelRequest{Key: "Search"}); err != nil {
+		return errors.Wrap(err, "failed to press Search")
+	}
+
+	testing.Sleep(ctx, 100*time.Millisecond)
+
+	//Search for app.
+	if _, err := keyboardSvc.Type(ctx, &inputspb.TypeRequest{Key: appName}); err != nil {
+		return errors.Wrapf(err, "failed to type: %v", appName)
+	}
+
+	testing.Sleep(ctx, 1000*time.Millisecond)
+
+	// Click on the first item in the search tile list.
+	installedAppButton := &uipb.Finder{
+		NodeWiths: []*uipb.NodeWith{
+			{Value: &uipb.NodeWith_HasClass{HasClass: "SearchResultTileItemView"}},
+			{Value: &uipb.NodeWith_First{}},
+		},
+	}
+
+	if _, err := uiautoSvc.LeftClick(ctx, &uipb.LeftClickRequest{Finder: installedAppButton}); err != nil {
+		return errors.Wrapf(err, "failed to open App: %v", appName)
+	}
+
+	return nil
+}
+
+func getGRPCHostnamePort(ctx context.Context, s *testing.State) (string, int) {
+	grpcServerPort := 4444
+	if portStr, ok := s.Var("grpcServerPort"); ok {
+		if portInt, err := strconv.Atoi(portStr); err == nil {
+			grpcServerPort = portInt
+		}
+	}
+	uri := s.DUT().HostName()
+	hostname := strings.Split(uri, ":")[0]
+	return hostname, grpcServerPort
+}
