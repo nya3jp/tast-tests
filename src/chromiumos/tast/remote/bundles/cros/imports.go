19a14c34ab824997091bc6ba7ab8bd5a30343fd8
Jonathan Fan <jonfan@google.com>
2021-12-30 17:14:04 -0800

    Add external facing grpc svc KeyboardService
    It is part of the Exposing Tast Services through gRPC initiative.
    The service is intended for use outside of Tast (e.g. Mobly).
    For more information, see http://go/tast-tcp-grpc-svc
    
    This CL includes:
    1) Expose keyboard functionalities Type, Accel, AccelPress, AccelRelease
    2) Remote test for KeyboardService
    
    BUG=b:208267010
    TEST=tast run -build=true <DUT> ui.KeyboardServiceGRPC
    
    Cq-Depend: chromium:3362668
    Change-Id: Idd136396a202f21aa0d63d0017bc632e34c109a2

--- src/chromiumos/tast/remote/bundles/cros/imports.go
+++ src/chromiumos/tast/remote/bundles/cros/imports.go
@@ -18,6 +18,7 @@ import (
 	_ "chromiumos/tast/remote/bundles/cros/firmware"
 	_ "chromiumos/tast/remote/bundles/cros/hardware"
 	_ "chromiumos/tast/remote/bundles/cros/hwsec"
+	_ "chromiumos/tast/remote/bundles/cros/inputs"
 	_ "chromiumos/tast/remote/bundles/cros/kernel"
 	_ "chromiumos/tast/remote/bundles/cros/lacros"
 	_ "chromiumos/tast/remote/bundles/cros/meta"
