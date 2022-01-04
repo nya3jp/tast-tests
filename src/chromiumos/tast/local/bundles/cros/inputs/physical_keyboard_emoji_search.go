ce763792a0aa6c1a732cb7efc3ffe8e3e71a1300
Jun Sheng <shengjun@google.com>
2022-01-04 11:54:25 +1100

    tast-tests: promote PhysicalKeyboardEmojiSearch to CQ
    The test has been stable for 20+ builds.
    https://tastboard.corp.google.com/test/inputs.PhysicalKeyboardEmojiSearch
    
    BUG=None
    TEST=None
    
    Change-Id: Id63e7f5a8ea98aca148e6e6af148439ae1e3fdf4

--- src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_emoji_search.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_emoji_search.go
@@ -6,10 +6,11 @@ package inputs
 
 import (
 	"context"
+	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/testserver"
-	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
@@ -23,7 +24,7 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnknown,
 		Desc:         "Check that emoji search works well",
 		Contacts:     []string{"jopalmer@chromium.org", "essential-inputs-team@google.com"},
-		Attr:         []string{"group:input-tools", "group:input-tools-upstream", "group:mainline", "informational"},
+		Attr:         []string{"group:input-tools", "group:input-tools-upstream", "group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Pre:          pre.NonVKClamshell,
 		HardwareDeps: hwdep.D(hwdep.Model(pre.StableModels...), hwdep.SkipOnModel("kodama", "kefka")),
@@ -32,17 +33,14 @@ func init() {
 
 func PhysicalKeyboardEmojiSearch(ctx context.Context, s *testing.State) {
 	cleanupCtx := ctx
-	ctx, cancel := uiauto.ReserveForVNCRecordingCleanup(ctx)
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
 	defer cancel()
-
-	stopRecording := uiauto.RecordVNCVideo(cleanupCtx, s)
-	defer stopRecording()
 
 	cr := s.PreValue().(pre.PreData).Chrome
 	tconn := s.PreValue().(pre.PreData).TestAPIConn
 	uc := s.PreValue().(pre.PreData).UserContext
 
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree")
 
 	keyboard, err := input.Keyboard(ctx)
 	if err != nil {
