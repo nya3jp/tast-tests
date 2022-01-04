2218b992ce1909ab8cc3b7ed4cc7c868a564ab19
Jun Sheng <shengjun@google.com>
2022-01-04 16:36:04 +1100

    tast-tests: fix inputs.PhysicalKeyboardGrammarCheck
    The test is flaky due to grammar check trigger delay.
    
    Refactor the test with below changes:
    1. Refactor the test to use UserAction.
    2. Using mouse click & retry to trigger grammar check.
    3. Enable the test to run on betty board.
    
    BUG=b:207749524
    TEST=tast run <DUT> inputs.PhysicalKeyboardGrammarCheck
    
    Change-Id: Iff89be5d5f33e0035ce0ac1f5d784e1eda538f8a

--- src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_grammar_check.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_grammar_check.go
@@ -6,7 +6,10 @@ package inputs
 
 import (
 	"context"
+	"math/rand"
+	"time"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/testserver"
 	"chromiumos/tast/local/bundles/cros/inputs/util"
@@ -14,6 +17,8 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/chrome/useractions"
+	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
@@ -25,7 +30,7 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnknown,
 		Desc:         "Checks on device grammar check with physical keyboard typing",
 		Contacts:     []string{"jiwan@chromium.org", "essential-inputs-team@google.com"},
-		Attr:         []string{"group:mainline", "group:input-tools", "informational"},
+		Attr:         []string{"group:mainline", "group:input-tools", "group:input-tools-upstream", "informational"},
 		HardwareDeps: hwdep.D(hwdep.Model(pre.GrammarEnabledModels...)),
 		SoftwareDeps: []string{"chrome"},
 		Pre:          pre.NonVKClamshellWithGrammarCheck,
@@ -40,6 +45,7 @@ func PhysicalKeyboardGrammarCheck(ctx context.Context, s *testing.State) {
 
 	cr := s.PreValue().(pre.PreData).Chrome
 	tconn := s.PreValue().(pre.PreData).TestAPIConn
+	uc := s.PreValue().(pre.PreData).UserContext
 
 	defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree")
 
@@ -57,19 +63,48 @@ func PhysicalKeyboardGrammarCheck(ctx context.Context, s *testing.State) {
 
 	inputField := testserver.TextAreaInputField
 	ui := uiauto.New(tconn)
+	sentenseTextFinder := nodewith.Name(inputText).Role(role.StaticText)
 	grammarWindowFinder := nodewith.ClassName("GrammarSuggestionWindow").Role(role.Window)
 	grammarSuggestionButtonFinder := nodewith.ClassName("SuggestionView").Role(role.Button).Ancestor(grammarWindowFinder)
 
-	if err := uiauto.Combine("accept grammar check suggestion",
+	validateAction := uiauto.Combine("accept grammar check suggestion",
 		its.Clear(inputField),
 		its.ClickFieldAndWaitForActive(inputField),
 		keyboard.TypeAction(inputText),
 		util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), inputText),
-		keyboard.AccelAction("Left"),
-		keyboard.AccelAction("Left"),
+		// The grammar check can be delayed a few seconds.
+		// Retry clicking the sentense area to wait for trigger.
+		ui.Retry(5, uiauto.Combine("click sentense to trigger grammar suggestion",
+			func(ctx context.Context) error {
+				sentenseTextLoc, err := ui.Location(ctx, sentenseTextFinder)
+				if err != nil {
+					return errors.Wrap(err, "failed to get sentense location")
+				}
+				// If the cursor is already in the middle of wrong sentense,
+				// clicking the same location will not trigger grammar window.
+				// Using random location to click around.
+				sentenseCenterPoint := sentenseTextLoc.CenterPoint()
+				rand.Seed(time.Now().UnixNano())
+				clickLoc := coords.Point{X: sentenseCenterPoint.X - 20 + rand.Intn(40), Y: sentenseCenterPoint.Y}
+				return ui.MouseClickAtLocation(0, clickLoc)(ctx)
+			},
+			ui.WithTimeout(3*time.Second).WaitUntilExists(grammarWindowFinder),
+		)),
 		ui.LeftClick(grammarSuggestionButtonFinder),
 		util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), expectedText),
-	)(ctx); err != nil {
+	)
+
+	if err := useractions.NewUserAction(
+		"Accept grammar check suggestion",
+		validateAction,
+		uc,
+		&useractions.UserActionCfg{
+			Attributes: map[string]string{
+				useractions.AttributeInputField: string(inputField),
+			},
+			Tags: []useractions.ActionTag{useractions.ActionTagGrammarCheck},
+		},
+	).Run(ctx); err != nil {
 		s.Fatal("Fail to accept grammar check suggestion: ", err)
 	}
 }
