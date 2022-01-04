97ee680978f240b186274ae5785df4c41b8fb9b2
Eddy Hsu <eddyhsu@chromium.org>
2022-01-04 16:07:19 +0800

    tast-tests: Add cyclic test with normal priority.
    Add cyclictest with scheduling policy normal0 w, w/o stress.
    
    BUG=b:211523913
    TEST=tast run ${DUT_IP} audio.CyclicBench.normal*
    
    Change-Id: I8eaed644e3a9b28d683e2d23e1148f650c918515

--- src/chromiumos/tast/local/bundles/cros/audio/cyclic_bench.go
+++ src/chromiumos/tast/local/bundles/cros/audio/cyclic_bench.go
@@ -127,7 +127,7 @@ func init() {
 				},
 			},
 			{
-				Name: "rr12_1thread_10ms_stress_normal_2workers_per_cpu",
+				Name: "rr12_1thread_10ms_stress_nice0_2workers_per_cpu",
 				Val: cyclicTestParameters{
 					SchedPolicy:    rrSched,
 					Priority:       crasPriority,
@@ -135,6 +135,55 @@ func init() {
 					IntervalUs:     defaultIntervalUs,
 					Loops:          defaultLoops,
 					P99ThresholdUs: defaultP99ThresholdUs,
+					StressSched:    otherSched,
+				},
+			},
+			{
+				Name: "nice0_1thread_10ms",
+				Val: cyclicTestParameters{
+					SchedPolicy:    otherSched,
+					Priority:       0,
+					Threads:        1,
+					IntervalUs:     defaultIntervalUs,
+					Loops:          defaultLoops,
+					P99ThresholdUs: defaultP99ThresholdUs,
+					StressSched:    none,
+				},
+			},
+			{
+				// nice-20 will cause invalid tast test name.
+				Name: "nice_20_1thread_10ms",
+				Val: cyclicTestParameters{
+					SchedPolicy:    otherSched,
+					Priority:       -20,
+					Threads:        1,
+					IntervalUs:     defaultIntervalUs,
+					Loops:          defaultLoops,
+					P99ThresholdUs: defaultP99ThresholdUs,
+					StressSched:    none,
+				},
+			},
+			{
+				Name: "nice19_1thread_10ms",
+				Val: cyclicTestParameters{
+					SchedPolicy:    otherSched,
+					Priority:       19,
+					Threads:        1,
+					IntervalUs:     defaultIntervalUs,
+					Loops:          defaultLoops,
+					P99ThresholdUs: 5000,
+					StressSched:    none,
+				},
+			},
+			{
+				Name: "nice0_1thread_10ms_stress_nice0_2workers_per_cpu",
+				Val: cyclicTestParameters{
+					SchedPolicy:    otherSched,
+					Priority:       0,
+					Threads:        1,
+					IntervalUs:     defaultIntervalUs,
+					Loops:          defaultLoops,
+					P99ThresholdUs: 30000,
 					StressSched:    otherSched,
 				},
 			},
@@ -220,6 +269,39 @@ func getNumberOfCPU(ctx context.Context) (int, error) {
 	return -1, errors.New("can't find CPU(s) info in lscpu")
 }
 
+func getCommandContext(ctx context.Context, param cyclicTestParameters) (*testexec.Cmd, error) {
+	switch param.SchedPolicy {
+	case rrSched:
+		return testexec.CommandContext(ctx, "cyclictest",
+			"--policy="+param.SchedPolicy.String(),
+			"--priority="+strconv.Itoa(param.Priority),
+			"--interval="+strconv.Itoa(param.IntervalUs),
+			"--threads="+strconv.Itoa(param.Threads),
+			"--loops="+strconv.Itoa(param.Loops),
+			// When there are multi-threads, the interval of the i-th
+			// thread will be (`interval` + i * `distance`).
+			// Set distance to 0 to make all the intervals equal.
+			"--distance=0",
+			"--verbose"), nil
+	case otherSched:
+		return testexec.CommandContext(ctx, "nice",
+			"-n", strconv.Itoa(param.Priority),
+			"cyclictest",
+			"--policy=other",
+			"--interval="+strconv.Itoa(param.IntervalUs),
+			"--threads="+strconv.Itoa(param.Threads),
+			"--loops="+strconv.Itoa(param.Loops),
+			// When there are multi-threads, the interval of the i-th
+			// thread will be (`interval` + i * `distance`).
+			// Set distance to 0 to make all the intervals equal.
+			"--distance=0",
+			"--verbose"), nil
+	case none:
+		return nil, errors.New("unsupported scheduling policy")
+	}
+	return nil, errors.New("unsupported scheduling policy")
+}
+
 func CyclicBench(ctx context.Context, s *testing.State) {
 	param := s.Param().(cyclicTestParameters)
 
@@ -248,18 +330,11 @@ func CyclicBench(ctx context.Context, s *testing.State) {
 		}
 	}
 
-	out, err := testexec.CommandContext(ctx, "cyclictest",
-		// TODO(eddyhsu): supports other types of policy.
-		"--policy="+param.SchedPolicy.String(),
-		"--priority="+strconv.Itoa(param.Priority),
-		"--interval="+strconv.Itoa(param.IntervalUs),
-		"--threads="+strconv.Itoa(param.Threads),
-		"--loops="+strconv.Itoa(param.Loops),
-		// When there are multi-threads, the interval of the i-th
-		// thread will be (`interval` + i * `distance`).
-		// Set distance to 0 to make all the intervals equal.
-		"--distance=0",
-		"--verbose").Output(testexec.DumpLogOnError)
+	cmd, err := getCommandContext(ctx, param)
+	if err != nil {
+		s.Fatal("Failed to get command context of cyclictest: ", err)
+	}
+	out, err := cmd.Output(testexec.DumpLogOnError)
 	if err != nil {
 		s.Fatal("Failed to execute cyclictest: ", err)
 	}
