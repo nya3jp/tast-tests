be654d0a236ebef7b731a0158dfb4489dd9bde57
Yue Li <updowndota@google.com>
2022-01-04 03:30:56 -0800

    Tast: Add quickanswers.Translation Tast tests
    BUG=b:212209813
    TEST=Test on DUT with command: tast -verbose run DUT quickanswers.Translation
    
    Change-Id: Ibafbbe8a519ddcdc156fe6482058dfed4b2c05e5

---
+++ src/chromiumos/tast/local/bundles/cros/quickanswers/translation.go
@@ -0,0 +1,88 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package quickanswers
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/event"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Translation,
+		Desc: "Test Quick Answers unit conversion feature",
+		Contacts: []string{
+			"updowndota@google.com",
+			"croissant-eng@google.com",
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		VarDeps:      []string{"quickanswers.username", "quickanswers.password"},
+	})
+}
+
+// Translation tests Quick Answers translation fearture.
+func Translation(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx,
+		chrome.GAIALogin(chrome.Creds{
+			User: s.RequiredVar("quickanswers.username"),
+			Pass: s.RequiredVar("quickanswers.password"),
+		}),
+		chrome.EnableFeatures("QuickAnswersV2"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	if err := tconn.Call(ctx, nil, `tast.promisify(chrome.autotestPrivate.setWhitelistedPref)`, "settings.quick_answers.enabled", true); err != nil {
+		s.Fatal("Failed to enable Quick Answers: ", err)
+	}
+
+	// Open page with query word on it.
+	const queryWord = "翻译"
+	conn, err := cr.NewConn(ctx, "https://google.com/search?q="+queryWord)
+	if err != nil {
+		s.Fatal("Failed to create new Chrome connection: ", err)
+	}
+	defer conn.Close()
+	defer conn.CloseTarget(ctx)
+
+	// Wait for the query word to appear.
+	query := nodewith.Name(queryWord).Role(role.StaticText).First()
+	if err := ui.WaitUntilExists(query)(ctx); err != nil {
+		s.Fatal("Failed to wait for query to load: ", err)
+	}
+
+	// Select the word and setup watcher to wait for text selection event.
+	if err := ui.WaitForEvent(nodewith.Root(),
+		event.TextSelectionChanged,
+		ui.Select(query, 0, query, 2))(ctx); err != nil {
+		s.Fatal("Failed to select query: ", err)
+	}
+
+	// Right click the selected query word and ensure the Quick Answers UI shows up with the translation result.
+	quickAnswers := nodewith.ClassName("QuickAnswersView")
+	translationResult := nodewith.NameContaining("translate").ClassName("Label")
+	if err := uiauto.Combine("Show context menu",
+		ui.RightClick(query),
+		ui.WaitUntilExists(quickAnswers),
+		ui.WaitUntilExists(translationResult))(ctx); err != nil {
+		s.Fatal("Quick Answers result not showing up: ", err)
+	}
+}
