be9a33be6b1bc5927f83501da6a8ed8df14edc03
Billy Zhao <billyzhao@chromium.org>
2022-01-04 07:14:18 +0000

    tast-tests: Support WiFi6e and add WiFi6e tests
    Add support to the GT-AXE11000 router. This is a WiFi6e router. Tests
    are added to test WPA3 on the 6ghz band. OWE (6ghz open networks) are currently not supported
    on ChromeOS devices so those tests are not included but the security
    file has been included. We also test hidden networks but this is
    currently non-functional.
    
    BUG=b:204498033
    TEST=tast run wifi.AxSimpleConnect.*
    
    Change-Id: I9364af77ccb0293ff14f96867d24afc3f7fd9d11

---
+++ src/chromiumos/tast/remote/wificell/router/axrouter/owe_config_param.go
@@ -0,0 +1,21 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package axrouter
+
+// secOwe provides Gen method to build a new Config.
+type secOwe struct {
+}
+
+// Gen builds a ConfigParam list to allow the router to support an open network flow.
+func (f *secOwe) Gen(axtype AxType, band BandEnum) ([]ConfigParam, error) {
+	radio := BandToRadio(axtype, band)
+	routerConfigParams := []ConfigParam{{radio, KeyAKM, "owe"}, {radio, KeyAuthMode, "owe"}}
+	return routerConfigParams, nil
+}
+
+// NewSecOweConfigParamFac builds a SecConfigParamFac for the OWE (WiFi Enhanced Open) network for the 802.11ax AP.
+func NewSecOweConfigParamFac() SecConfigParamFac {
+	return &secOwe{}
+}
