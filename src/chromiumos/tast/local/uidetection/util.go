2f80c8812e8c3fb40afc24d3945b4eee11e49270
Alvin Jia <alvinjia@google.com>
2022-01-04 15:13:00 +1100

    tast-tests: fix taking stable screenshot issue
    Currently, uidetection compares two images between two
    pollings in the bytes format, which contains metadata that
    should not be considered.
    
    This could result in errors "screen has not stopped changing
    after XXX second" https://stainless.corp.google.com/search?view=matrix&row=test&col=build&first_date=2021-12-29&last_date=2022-01-04&test=uidetection&reason=stop&exclude_cts=false&exclude_not_run=false&exclude_non_release=true&exclude_au=true&exclude_acts=true&exclude_retried=false&exclude_non_production=false&exclude_non_critical=false
    
    Revised to compare only the raw image pixels.
    
    BUG=b:213093997
    TEST=tast run <DUT> uidetection.BasicDetections
    
    Change-Id: Ie43dbfa32620df398610fbd29910d0dd6adb69cc

--- src/chromiumos/tast/local/uidetection/util.go
+++ src/chromiumos/tast/local/uidetection/util.go
@@ -57,16 +57,16 @@ func TakeScreenshot(ctx context.Context, tconn *chrome.TestConn) ([]byte, error)
 
 // TakeStableScreenshot takes a stable screenshot that doesn't changed between two pollings.
 func TakeStableScreenshot(ctx context.Context, tconn *chrome.TestConn, pollOpts testing.PollOptions) ([]byte, error) {
-	var lastScreen []byte
-	var currentScreen []byte
+	var lastScreen image.Image
+	var currentScreen image.Image
 	start := time.Now()
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
 		var err error
-		currentScreen, err = TakeScreenshot(ctx, tconn)
+		currentScreen, err = screenshot.CaptureChromeImageWithTestAPI(ctx, tconn)
 		if err != nil {
 			return errors.Wrap(err, "failed to take immediate screenshot")
 		}
-		if bytes.Compare(currentScreen, lastScreen) != 0 {
+		if !equal(currentScreen, lastScreen) {
 			lastScreen = currentScreen
 			elapsed := time.Since(start)
 			return errors.Errorf("screen has not stopped changing after %s, perhaps increase timeout or use TakeScreenshot", elapsed)
@@ -75,5 +75,30 @@ func TakeStableScreenshot(ctx context.Context, tconn *chrome.TestConn, pollOpts 
 	}, &pollOpts); err != nil {
 		return nil, errors.Wrap(err, "failed to take stable screenshot")
 	}
-	return currentScreen, nil
+
+	// Convert image.Image to []byte.
+	imgBuf := new(bytes.Buffer)
+	if err := png.Encode(imgBuf, currentScreen); err != nil {
+		return nil, errors.Wrap(err, "failed to write the PNG image into byte buffer")
+	}
+	return imgBuf.Bytes(), nil
+}
+
+func equal(imgA, imgB image.Image) bool {
+	if imgA == nil && imgB == nil {
+		return true
+	} else if imgA == nil || imgB == nil {
+		return false
+	}
+	if imgA.Bounds() != imgB.Bounds() {
+		return false
+	}
+	for y := imgA.Bounds().Min.Y; y < imgA.Bounds().Max.Y; y++ {
+		for x := imgA.Bounds().Min.X; x < imgA.Bounds().Max.X; x++ {
+			if imgA.At(x, y) != imgB.At(x, y) {
+				return false
+			}
+		}
+	}
+	return true
 }
