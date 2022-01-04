712e0b26a4989c73e6fce9f5f6db8dd353cd0d4a
Billy Zhao <billyzhao@chromium.org>
2022-01-04 07:14:18 +0000

    tast-tests: Support WiFi6e and add WiFi6e tests
    Add support to the GT-AXE11000 router. This is a WiFi6e router. Tests
    are added to test WPA3 on the 6ghz band. OWE (6ghz open networks) are currently not supported
    on ChromeOS devices so those tests are not included but the security
    file has been included. We also test hidden networks but this is
    currently non-functional.
    
    BUG=b:204498033
    TEST=tast run wifi.AxSimpleConnect.*
    
    Change-Id: I9364af77ccb0293ff14f96867d24afc3f7fd9d11

---
+++ src/chromiumos/tast/remote/wificell/router/axrouter/sae_config_param.go
@@ -0,0 +1,30 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package axrouter
+
+import "chromiumos/tast/errors"
+
+// secSAE provides Gen method to build a new Config.
+type secSAE struct {
+	psk    string
+	cipher CipherEnum
+}
+
+// Gen builds a ConfigParam list to allow the router to support a WPA network flow.
+func (f *secSAE) Gen(axtype AxType, band BandEnum) ([]ConfigParam, error) {
+	radio := BandToRadio(axtype, band)
+	routerConfigParams := []ConfigParam{{radio, KeyAKM, "sae"}, {radio, KeyAuthMode, "sae"}, {radio, KeyWPAPSK, f.psk}}
+	if f.cipher == AES {
+		routerConfigParams = append(routerConfigParams, ConfigParam{radio, KeyCrypto, "aes"})
+	} else {
+		return nil, errors.New("crypto schema not supported")
+	}
+	return routerConfigParams, nil
+}
+
+// NewSecSAEConfigParamFac builds a SecConfigParamFac for WPA3-SAE network for the 802.11ay AP.
+func NewSecSAEConfigParamFac(psk string, cipher CipherEnum) SecConfigParamFac {
+	return &secSAE{psk, cipher}
+}
