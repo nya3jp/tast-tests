2b790d35db6dfea447a03ade9eac1f94edf7d798
Judy Hsiao <judyhsiao@chromium.org>
2022-01-07 15:05:17 +0800

    tast: audio: add sound_card_init remote test
    Add remote test to verify that sound_card_init runs successfully at
    every boot. This test enhances the test coverage because we test
    sound_card_init execution by rebooting the DUT instead of
    starting sound_card_init when its dependencies are all ready after boot.
    
    BUG=b:208548076
    TEST=`tast run ${DUT_IP} audio.SoundCardInit`
    
    Change-Id: Idc45837adff6f9b9841be24b9d619ed6d083e637

---
+++ src/chromiumos/tast/remote/bundles/cros/audio/sound_card_init.go
@@ -0,0 +1,123 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package audio
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/dutfs"
+	"chromiumos/tast/remote/firmware/fingerprint/rpcdut"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: SoundCardInit,
+		Desc: "Verifies sound_card_init finishes successfully at boot time",
+		// Skips atlas, nocturne as they don't use sound_card_init to initialized their smart amps.
+		// Skip volteer2 as it's a reference design device not an official launched device.
+		HardwareDeps: hwdep.D(hwdep.SmartAmp(), hwdep.SkipOnModel("atlas", "nocturne", "volteer2")),
+		Contacts:     []string{"judyhsiao@chromium.org", "yuhsuan@chromium.org"},
+		Attr:         []string{"group:mainline", "informational"},
+		Timeout:      5 * time.Minute,
+	})
+}
+
+const soundCardInitTimeout = 10 * time.Second
+
+// soundCardInitRunTimeFile is the file stores previous sound_card_init run time.
+const soundCardInitRunTimeFile = "/var/lib/sound_card_init/%s/run"
+
+// crasStopTimeFile is the file stores previous CRAS stop time.
+const crasStopTimeFile = "/var/lib/cras/stop"
+
+func parseSoundCardID(dump string) (string, error) {
+	re := regexp.MustCompile(`card 0: ([a-z0-9]+) `)
+	m := re.FindStringSubmatch(dump)
+
+	if len(m) != 2 {
+		return "", errors.New("no sound card")
+	}
+	return m[1], nil
+}
+
+func removeSoundCardInitFiles(ctx context.Context, d *rpcdut.RPCDUT, soundCardID string) error {
+	// Clear all sound_card_init files.
+	fs := dutfs.NewClient(d.RPC().Conn)
+	if err := fs.Remove(ctx, crasStopTimeFile); err != nil && !os.IsNotExist(err) {
+		return errors.Wrapf(err, "failed to rm file: %s: %v", crasStopTimeFile, err)
+	}
+	file := fmt.Sprintf(soundCardInitRunTimeFile, soundCardID)
+	if err := fs.Remove(ctx, file); err != nil && !os.IsNotExist(err) {
+		return errors.Wrapf(err, "failed to rm file: %s: %v", file, err)
+	}
+	return nil
+}
+
+func verifySoundCardInitFinished(ctx context.Context, d *rpcdut.RPCDUT, soundCardID string) error {
+	// Poll for sound_card_init run time file being updated, which means sound_card_init completes running.
+	fs := dutfs.NewClient(d.RPC().Conn)
+	err := testing.Poll(ctx, func(ctx context.Context) error {
+		file := fmt.Sprintf(soundCardInitRunTimeFile, soundCardID)
+		exists, err := fs.Exists(ctx, file)
+		if err != nil {
+			return errors.Wrapf(err, "failed to stat %s", file)
+		}
+		if exists {
+			return nil
+		}
+		return errors.New(file + " does not exist")
+	}, &testing.PollOptions{Timeout: soundCardInitTimeout})
+	return err
+}
+
+// SoundCardInit verifies sound_card_init finishes successfully at boot time.
+func SoundCardInit(ctx context.Context, s *testing.State) {
+	// Shorten deadline to leave time for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	d, err := rpcdut.NewRPCDUT(ctx, s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect RPCDUT: ", err)
+	}
+	// Ensure the rpc connection is closed at the end of this test.
+	defer func(ctx context.Context) {
+		if err := d.Close(ctx); err != nil {
+			s.Fatal("Failed to close RPCDUT: ", err)
+		}
+	}(cleanupCtx)
+
+	dump, err := d.Conn().CommandContext(ctx, "aplay", "-l").Output()
+	if err != nil {
+		s.Fatal("Failed to aplay -l: ", err)
+	}
+	soundCardID, err := parseSoundCardID(string(dump))
+	if err != nil {
+		s.Fatal("Failed to parse sound card name: ", err)
+	}
+
+	if err := removeSoundCardInitFiles(ctx, d, soundCardID); err != nil {
+		s.Fatal("Failed to remove previous files: ", err)
+	}
+
+	// Reboot
+	if err := d.Reboot(ctx); err != nil {
+		s.Fatal("Failed to reboot the DUT: ", err)
+	}
+
+	// Poll for sound_card_init run time file being updated, which means sound_card_init completes running.
+	if err := verifySoundCardInitFinished(ctx, d, soundCardID); err != nil {
+		s.Fatal("Failed to wait for sound_card_init completion: ", err)
+	}
+}
