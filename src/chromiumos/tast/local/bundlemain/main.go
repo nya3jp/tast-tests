ab570f35123572ac56d40c4683138d2a245561d2
Derek Beckett <dbeckett@chromium.org>
2022-01-10 14:26:08 -0800

    tast - remove hook to check for /usr/local/telemetry
    BUG=b:203609358
    TEST=cq
    
    Change-Id: Ife26200c708380a69c508bb0b119ee07090e6b20

--- src/chromiumos/tast/local/bundlemain/main.go
+++ src/chromiumos/tast/local/bundlemain/main.go
@@ -124,24 +124,11 @@ func hwsecCheckTPMState(ctx context.Context, origStatus *hwsec.NonsensitiveStatu
 	return nil
 }
 
-func logTelemetryState(ctx context.Context, s *testing.TestHookState, verb string) {
-	// Check Telemetry folder status. See b/203609358
-	telemetryDir := "/usr/local/telemetry"
-	_, err := os.Stat(telemetryDir)
-	if err != nil {
-		s.Logf("Failed to find %s %s: %v", telemetryDir, verb, err)
-	} else {
-		s.Logf("Found %s %s", telemetryDir, verb)
-	}
-}
-
 func testHookLocal(ctx context.Context, s *testing.TestHookState) func(ctx context.Context, s *testing.TestHookState) {
 	endLogFn, err := syslog.CollectSyslog()
 	if err != nil {
 		s.Log("Saving log position: ", err)
 	}
-
-	logTelemetryState(ctx, s, "pre-test")
 
 	// Ensure disk space and record the current free space.
 	checkFreeSpace := false
@@ -180,9 +167,6 @@ func testHookLocal(ctx context.Context, s *testing.TestHookState) func(ctx conte
 	}
 
 	return func(ctx context.Context, s *testing.TestHookState) {
-
-		logTelemetryState(ctx, s, "post-test")
-
 		// Ensure the TPM is in the expect state after tast finish.
 		if err := hwsecCheckTPMState(ctx, hwsecTpmStatus, hwsecDACounter); err != nil {
 			s.Error("Failed to check TPM state: ", err)
