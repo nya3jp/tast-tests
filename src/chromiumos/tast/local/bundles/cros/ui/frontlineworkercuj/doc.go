73cda87ed3495c4db7808afa2e5b683519196c17
Alston Huang <alston.huang@cienet.com>
2022-01-10 15:01:01 +0800

    cuj: Declare functions for structures used in Frontline Worker CUJ
    BUG=b:213542905
    TEST=fast_build.sh -T
    
    Change-Id: I4ff94f3bf2a6fe1699cde09860b2e442352d3834

---
+++ src/chromiumos/tast/local/bundles/cros/ui/frontlineworkercuj/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package frontlineworkercuj contains frontlineworker CUJ test cases library.
+package frontlineworkercuj
