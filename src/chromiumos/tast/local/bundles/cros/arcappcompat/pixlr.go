9b6db08be8775b48dbbc874a62e59ba424e84a75
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2022-01-10 16:45:45 -0800

    arcappcompat: Update Tast Test for Pixlr
    BUG=b:213939480
    TEST=tast run <helios> arcappcompat.Pixlr.vm_clamshell_mode
    
    Disabled the test since Pixlr isn’t compatible with chromebook.
    
    Change-Id: Ie32b0c0230c4e668b2db4d90edd66a3b75e24300

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/pixlr.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/pixlr.go
@@ -34,7 +34,8 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnknown,
 		Desc:         "Functional test for Pixlr that install, launch the app and check that the main page is open, also checks Pixlr correctly changes the window state in both clamshell and touchview mode",
 		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:appcompat"},
+		// Disabled the test since Pixlr is not compatible with Chromebook.
+		// Attr:         []string{"group:appcompat"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Name: "clamshell_mode",
