cd767b56d950c8e2786bfbf0c8023fad1af246f7
Eddy Hsu <eddyhsu@chromium.org>
2022-01-11 13:51:44 +0800

    tast-tests: Add vm.AudioCyclicBench.*
    to test missing scheduling which is critical to CRAS.
    
    This runs the cyclictest within the vm.
    
    BUG=b:211523913
    TEST=tast run ${DUT_IP} vm.AudioCyclicBench.*
    
    Cq-Depend: chromium:3380169
    Change-Id: I9aa5c3cde95d3bd30bb5acb7b4eaca69d00a079d

---
+++ src/chromiumos/tast/local/bundles/cros/vm/data/run-cyclic-test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This script is meant to be run as PID 1 inside a VM.
+set -ex
+
+die() {
+  echo "$1"
+  exit 1
+}
+
+usage() {
+  die "Usage: $(basename "$0")"
+}
+
+main() {
+  [[ "$$" == "1" ]] || die "Not runnnig as PID 1"
+
+  export PYTHONHOME=/usr/local
+  export PATH=${PATH}:/usr/local/bin
+  cyclic_bench.py "$@"
+}
+
+main "$@"
