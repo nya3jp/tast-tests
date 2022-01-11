8776b8b00a48e3e4d1a5aeb2b1789e45f0a02ed2
Matt Stark <msta@google.com>
2022-01-11 12:02:46 +1100

    Reduce kmsvnc log in tast during screen recording.
    During screen recording, it adds a lot of clutter to the logs which
    provides no use to the user.
    
    BUG=b:211505471
    TEST=Ran the test and checked that the logs were being redirected to kmsvnc.log
    
    Change-Id: Iaf8ace3fe3545cb26f4dc0773a2724c45460be00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3379567
    Reviewed-by: Jun Sheng <shengjun@chromium.org>
    Tested-by: Matt Stark <msta@google.com>
    Commit-Queue: Matt Stark <msta@google.com>

--- src/chromiumos/tast/local/bundles/cros/graphics/kmsvnc_connect.go
+++ src/chromiumos/tast/local/bundles/cros/graphics/kmsvnc_connect.go
@@ -34,7 +34,7 @@ func init() {
 func KmsvncConnect(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(*chrome.Chrome)
 
-	k, err := kmsvnc.NewKmsvnc(ctx)
+	k, err := kmsvnc.NewKmsvnc(ctx, true)
 	if err != nil {
 		s.Fatal("Failed to start kmsvnc: ", err)
 	}
