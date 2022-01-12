4dc1290697d3dae27980342669549873e6e4f6a7
Alvin Jia <alvinjia@google.com>
2022-01-12 19:07:36 +1100

    uidetection: refactor takeStableScreenshot method
    Refactor takeStableScreenshot() to save the current and the
    previous screenshots if there is error.
    
    Refactor Location() to save the screenshots when it failes to
    find an element on it.
    
    Revise the BasicDetections test to use ImmediateScreenshot
    in scenarios where the text cursor is blinking,
    
    BUG=b:213093997
    TEST=tast run <DUT> uidetection.BasicDetections
    
    Change-Id: Ib604d98a1f85aa81fd310e40fc18a56bff6ceae9

--- src/chromiumos/tast/local/bundles/cros/uidetection/basic_detections.go
+++ src/chromiumos/tast/local/bundles/cros/uidetection/basic_detections.go
@@ -57,8 +57,8 @@ func BasicDetections(ctx context.Context, s *testing.State) {
 	// textblock detection and word detection.
 	if err := uiauto.Combine("verify detections",
 		ud.LeftClick(uidetection.CustomIcon(s.DataPath("logo_chrome.png"))),
-		ud.LeftClick(uidetection.TextBlock([]string{"Add", "shortcut"})),
-		ud.LeftClick(uidetection.Word("Cancel")),
+		ud.WithScreenshotStrategy(uidetection.ImmediateScreenshot).LeftClick(uidetection.TextBlock([]string{"Add", "shortcut"})),
+		ud.WithScreenshotStrategy(uidetection.ImmediateScreenshot).LeftClick(uidetection.Word("Cancel")),
 		ud.WaitUntilGone(uidetection.Word("Cancel")),
 		// Check the negative cases.
 		expectError(ud.Exists(uidetection.Word("Google")), uidetection.ErrMultipleMatch),
