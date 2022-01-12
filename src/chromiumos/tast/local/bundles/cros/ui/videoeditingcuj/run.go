8e442910325e93b9ccef6ccde7dd8cf2c23a78a7
Jane Yang <jane.yang@cienet.com>
2022-01-12 10:10:17 +0800

    EDU video editing cuj: Video editing on the web WeVideo main case
    1. Open the video editor: WeVideo
    2. Open stock video available in WeVideo
    3. Add text to small segment of video
    4. Concurrently open GDocs in separate browser window. Enter notes at 70wpm
    5. Cross fade from first video to a second stock video
    6. Play the video
    
    BUG=b:233289255
    TEST=tast run <vars> <dut> ui.EDUVideoEditingCUJ.premium_wevideo
    
    Change-Id: Ie784accd1492229bac629767c2e79a70e2380c86

---
+++ src/chromiumos/tast/local/bundles/cros/ui/videoeditingcuj/run.go
@@ -0,0 +1,215 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package videoeditingcuj contains the test code for VideoEditingOnTheWeb CUJ.
+package videoeditingcuj
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/action"
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/ui/cuj"
+	"chromiumos/tast/local/bundles/cros/ui/googleapps"
+	"chromiumos/tast/local/bundles/cros/ui/videoeditingcuj/wevideo"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/ui/cujrecorder"
+	"chromiumos/tast/testing"
+)
+
+const (
+	weVideoTitle    = "WeVideo"
+	googleDocsTitle = "Google Docs"
+	clip1           = "Springtime Birds Migration Northern Parula Warbler 4K"
+	clip2           = "Hammock and Beach Chairs"
+	clipTime1       = "00:23:00"
+	clipTime2       = "00:04:00"
+	videoTrack      = "Video 1"
+	textTrack       = "Text 1"
+	demoText        = "Springtime Birds"
+	docParagraph    = "The Little Prince's story follows a young prince who visits various planets in space, including Earth, and addresses themes of loneliness, friendship, love, and loss."
+)
+
+// Run runs the EDUVideoEditingCUJ test.
+func Run(ctx context.Context, outDir string, cr *chrome.Chrome, tabletMode bool) error {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 15*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create test API connection")
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to open the keyboard")
+	}
+	defer kb.Close()
+
+	// Give 10 seconds to set initial settings. It is critical to ensure
+	// cleanupSetting can be executed with a valid context so it has its
+	// own cleanup context from other cleanup functions. This is to avoid
+	// other cleanup functions executed earlier to use up the context time.
+	cleanupSettingsCtx := ctx
+	ctx, cancel = ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	cleanupSetting, err := cuj.InitializeSetting(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to set initial settings")
+	}
+	defer cleanupSetting(cleanupSettingsCtx)
+
+	uiHdl, err := uiHandler(ctx, tconn, tabletMode)
+	if err != nil {
+		return err
+	}
+
+	testing.ContextLog(ctx, "Start to get browser start time")
+	_, browserStartTime, err := cuj.GetBrowserStartTime(ctx, tconn, true, tabletMode, browser.TypeAsh)
+	if err != nil {
+		return errors.Wrap(err, "failed to get browser start time")
+	}
+
+	options := cujrecorder.NewPerformanceCUJOptions()
+	recorder, err := cujrecorder.NewRecorder(ctx, cr, nil, options)
+	if err != nil {
+		return errors.Wrap(err, "failed to create a recorder")
+	}
+	defer recorder.Close(cleanupCtx)
+
+	if err := recorder.Run(ctx, func(ctx context.Context) error {
+		return videoEditingScenario(ctx, tconn, cr, kb, uiHdl, tabletMode, outDir)
+	}); err != nil {
+		return errors.Wrap(err, "failed to run the video editing on the WeVideo web")
+	}
+
+	pv := perf.NewValues()
+	pv.Set(perf.Metric{
+		Name:      "Browser.StartTime",
+		Unit:      "ms",
+		Direction: perf.SmallerIsBetter,
+	}, float64(browserStartTime.Milliseconds()))
+
+	// Use a short timeout value so it can return fast in case of failure.
+	recordCtx, cancel := context.WithTimeout(ctx, time.Minute)
+	defer cancel()
+	if err := recorder.Record(recordCtx, pv); err != nil {
+		return errors.Wrap(err, "failed to record")
+	}
+	if err = pv.Save(outDir); err != nil {
+		return errors.Wrap(err, "failed to store values")
+	}
+	if err := recorder.SaveHistograms(outDir); err != nil {
+		return errors.Wrap(err, "failed to save histogram raw data")
+	}
+	return nil
+}
+
+func videoEditingScenario(ctx context.Context, tconn *chrome.TestConn, cr *chrome.Chrome, kb *input.KeyboardEventWriter,
+	uiHdl cuj.UIActionHandler, tabletMode bool, outDir string) (retErr error) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 15*time.Second)
+	defer cancel()
+	account := cr.Creds().User
+	w := wevideo.NewWeVideo(tconn, kb, uiHdl, tabletMode)
+	if err := w.Open(cr)(ctx); err != nil {
+		return errors.Wrap(err, "failed to open the WeVideo page")
+	}
+	defer func(ctx context.Context) {
+		cleanup(ctx, tconn, cr, w, outDir, retErr)
+	}(cleanupCtx)
+
+	if err := uiauto.Combine("run the video editing scenario",
+		maximizeBrowserWindow(ctx, tconn, tabletMode),
+		w.Login(account),
+		w.Create(),
+		w.AddStockVideo(clip1, clipTime1, videoTrack),
+		w.AddStockVideo(clip2, clipTime2, videoTrack),
+		w.AddText(clip1, textTrack, demoText),
+	)(ctx); err != nil {
+		return err
+	}
+
+	if err := googleapps.NewGoogleDocs(cr, tconn, true)(ctx); err != nil {
+		return err
+	}
+	defer func(ctx context.Context) {
+		docCleanup(ctx, tconn, cr, uiHdl, outDir, retErr)
+	}(cleanupCtx)
+
+	return uiauto.Combine("run the video editing scenario",
+		googleapps.EditDoc(tconn, kb, docParagraph),
+		uiHdl.SwitchToAppWindowByName("Chrome", weVideoTitle),
+		w.AddTransition(clip2),
+		w.PlayVideo(clip1),
+	)(ctx)
+}
+
+// maximizeBrowserWindow returns an action that maximize the WeVideo window to show all the browser UI elements for precise clicking.
+func maximizeBrowserWindow(ctx context.Context, tconn *chrome.TestConn, tabletMode bool) action.Action {
+	return func(ctx context.Context) error {
+		if tabletMode {
+			return nil
+		}
+		// Find the WeVideo browser window.
+		window, err := ash.FindWindow(ctx, tconn, func(w *ash.Window) bool {
+			return (w.WindowType == ash.WindowTypeBrowser || w.WindowType == ash.WindowTypeLacros) && strings.Contains(w.Title, weVideoTitle)
+		})
+		if err != nil {
+			return errors.Wrap(err, "failed to find the WeVideo window")
+		}
+		if err := ash.SetWindowStateAndWait(ctx, tconn, window.ID, ash.WindowStateMaximized); err != nil {
+			// Just log the error and try to continue.
+			testing.ContextLog(ctx, "Try to continue the test even though maximizing the WeVideo window failed: ", err)
+		}
+		return nil
+	}
+}
+
+func uiHandler(ctx context.Context, tconn *chrome.TestConn, tabletMode bool) (cuj.UIActionHandler, error) {
+	var uiHdl cuj.UIActionHandler
+	var err error
+	if tabletMode {
+		if uiHdl, err = cuj.NewTabletActionHandler(ctx, tconn); err != nil {
+			return uiHdl, errors.Wrap(err, "failed to create tablet action handler")
+		}
+	} else {
+		if uiHdl, err = cuj.NewClamshellActionHandler(ctx, tconn); err != nil {
+			return uiHdl, errors.Wrap(err, "failed to create clamshell action handler")
+		}
+	}
+	return uiHdl, nil
+}
+
+func cleanup(ctx context.Context, tconn *chrome.TestConn, cr *chrome.Chrome, w *wevideo.WeVideo, outDir string, retErr error) {
+	// The screenshot and ui tree dump must been taken before the connection is closed.
+	faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, func() bool { return retErr != nil }, cr, "ui_dump")
+	w.Close(ctx)
+	cuj.CloseAllWindows(ctx, tconn)
+}
+
+func docCleanup(ctx context.Context, tconn *chrome.TestConn, cr *chrome.Chrome, uiHdl cuj.UIActionHandler, outDir string, retErr error) {
+	// docCleanup switches to the document page and deletes it.
+	docCleanup := uiauto.Combine("switch to the document page and delete it",
+		uiHdl.SwitchToAppWindowByName("Chrome", googleDocsTitle),
+		googleapps.DeleteDoc(tconn),
+	)
+	// If case fails, dump the last screen before deleting the document.
+	faillog.DumpUITreeWithScreenshotOnError(ctx, outDir, func() bool { return retErr != nil }, cr, "ui_dump_last")
+	if err := docCleanup(ctx); err != nil {
+		// Only log the error.
+		testing.ContextLog(ctx, "Failed to clean up the document: ", err)
+	}
+}
