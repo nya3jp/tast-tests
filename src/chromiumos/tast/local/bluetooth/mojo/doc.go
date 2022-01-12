3ae97dc31bea317953e67bf1b11d613b45e79690
Shijin Abraham <shijinabraham@google.com>
2022-01-12 01:10:39 -0800

    bluetooth: Add Bluetooth test that using Mojo API and fixture
    Add example Mojo api calls for Bluetooth
    Add fixture for tests using Bluetooth mojo API
    Add example test
    
    BUG=None
    TEST=bluetooth.ToggleBluetoothUsingMojo
    
    Change-Id: I7b37bfdd312cbf4ae500c24e25b01afce99127d0

---
+++ src/chromiumos/tast/local/bluetooth/mojo/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package mojo provides functions to access the bluetooth mojo interface
+// defined in cros_bluetooth_config.mojom
+package mojo
