b26a7151343d64dd34f930e6e501a18cf3fdd633
Shijin Abraham <shijinabraham@google.com>
2022-01-12 01:10:39 -0800

    bluetooth: Add Bluetooth test that using Mojo API and fixture
    Add example Mojo api calls for Bluetooth
    Add fixture for tests using Bluetooth mojo API
    Add example test
    
    BUG=None
    TEST=bluetooth.ToggleBluetoothUsingMojo
    
    Change-Id: I7b37bfdd312cbf4ae500c24e25b01afce99127d0

---
+++ src/chromiumos/tast/local/bluetooth/mojo/fixture.go
@@ -0,0 +1,82 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package mojo
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name: "bluetoothMojoJSObject",
+		Desc: "Get JS object for Bluetooth mojo interface via OS Settings App, with Bluetooth Revamp flag enabled",
+		Contacts: []string{
+			"shijinabraham@google.com",
+			"cros-conn-test-team@google.com",
+		},
+		Impl:            &JSObject{},
+		Parent:          "chromeLoggedInWithBluetoothEnabled",
+		SetUpTimeout:    chrome.LoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+	})
+}
+
+// JSObject hold the chrome connection and the Mojo JS object
+type JSObject struct {
+	Crconn *chrome.Conn
+	Js     chrome.JSObject
+}
+
+func (m *JSObject) Reset(ctx context.Context) error {
+
+	if err := m.Crconn.Call(ctx, &(m.Js), BTConfigJS); err != nil {
+		return errors.Wrap(err, "failed to create Bluetooth mojo JS")
+	}
+
+	if err := m.Js.Call(ctx, nil, `function init(){ this.initSysPropObs()}`); err != nil {
+		return errors.Wrap(err, "failed to initailize the observer")
+	}
+
+	return nil
+}
+
+func (*JSObject) PreTest(ctx context.Context, s *testing.FixtTestState) {
+}
+
+func (*JSObject) PostTest(ctx context.Context, s *testing.FixtTestState) {
+}
+
+func (*JSObject) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+
+	cr := s.ParentValue().(*chrome.Chrome)
+
+	//Open OS settings App Bluetooth Subpage
+	url := "chrome://os-settings/bluetooth"
+	crconn, err := apps.LaunchOSSettings(ctx, cr, url)
+	if err != nil {
+		s.Fatal("Failed to open settings app: ", err)
+	}
+
+	var js chrome.JSObject
+
+	if err := crconn.Call(ctx, &js, BTConfigJS); err != nil {
+		s.Fatal(errors.Wrap(err, "failed to create Bluetooth mojo JS"))
+	}
+
+	if err := js.Call(ctx, nil, `function init(){ this.initSysPropObs()}`); err != nil {
+		s.Fatal(errors.Wrap(err, "failed to initailize the observer"))
+	}
+
+	return &JSObject{crconn, js}
+}
+
+func (*JSObject) TearDown(ctx context.Context, s *testing.FixtState) {
+}
