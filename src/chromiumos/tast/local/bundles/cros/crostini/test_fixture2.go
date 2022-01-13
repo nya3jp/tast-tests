a4ac058b25b745df9fcc4706a62712ae126adc32
Judy Wu <jinrongwu@google.com>
2022-01-13 11:09:18 +1100

    Add Crostini Fixture
    It is one of the cls to migrate Crostini from Precondition to Fixture.
    
    BUG=None
    TEST=Ran on hatch
    
    Change-Id: I00f70b111d2db2bc51feb59224a3ccdde900f0ad

---
+++ src/chromiumos/tast/local/bundles/cros/crostini/test_fixture2.go
@@ -0,0 +1,63 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/crostini"
+	"chromiumos/tast/local/crostini/ui/terminalapp"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TestFixture2,
+		Desc:         "Test fixture, this should be run together with TestFixture to test PreTest and PostTest",
+		Contacts:     []string{"jinrongwu@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "vm_host"},
+		Params: []testing.Param{
+			{
+				Name:              "buster_stable",
+				ExtraSoftwareDeps: []string{"dlc"},
+				ExtraHardwareDeps: crostini.CrostiniStable,
+				Fixture:           "crostiniBuster",
+				Timeout:           2 * time.Minute,
+			},
+		},
+	})
+}
+
+// TestFixture2 is a test to test the fixture, not a real test.
+// TODO (jinrongwu): to remove it once all crostini test cases have been migrated to fixture successfully.
+func TestFixture2(ctx context.Context, s *testing.State) {
+	tconn := s.FixtValue().(crostini.FixtureData).Tconn
+
+	// Open Files app.
+	filesApp, err := filesapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to open Files app: ", err)
+	}
+
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	if err := filesApp.OpenLinuxFiles()(ctx); err != nil {
+		s.Fatal("Failed to open Linux files: ", err)
+	}
+
+	// Open Terminal app.
+	_, err = terminalapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to open Terminal app: ", err)
+	}
+
+	s.Log("=================================================")
+	s.Log("SUCCEEDED")
+	s.Log("=================================================")
+}
