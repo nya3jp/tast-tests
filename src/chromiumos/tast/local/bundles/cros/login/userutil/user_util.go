86930df8e8217637ba03f97727397e723d4da4dd
Jakub Flis <jaflis@google.com>
2022-01-18 09:14:57 +0000

    login.RemoveUsersExceptOwner: Verify that user is device owner
    Fix flakiness by waiting until the first user becomes device owner
    before creating other users. Currently users are created without
    waiting for establishing the ownership, which leads to race conditions.
    
    BUG=b:200987502
    TEST=tested on kindred and karma
    
    Change-Id: Ib661bfcf1d177dcef28574b5b1ce70c078d77544

--- src/chromiumos/tast/local/bundles/cros/login/userutil/user_util.go
+++ src/chromiumos/tast/local/bundles/cros/login/userutil/user_util.go
@@ -10,7 +10,9 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
+	"time"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
 )
@@ -19,6 +21,14 @@ import (
 // closes the session, so it should be used only for creating new users, not logging in
 func CreateUser(ctx, cleanUpCtxs context.Context, s *testing.State, username, password string, extraOpts ...chrome.Option) {
 	cr := login(ctx, s, username, password, extraOpts...)
+	cr.Close(cleanUpCtxs)
+}
+
+// CreateDeviceOwner creates a user like the CreateUser function, but before closing the session
+// it waits until the user becomes device owner
+func CreateDeviceOwner(ctx, cleanUpCtxs context.Context, s *testing.State, username, password string, extraOpts ...chrome.Option) {
+	cr := login(ctx, s, username, password, extraOpts...)
+	WaitForOwnership(ctx, s, cr)
 	cr.Close(cleanUpCtxs)
 }
 
@@ -67,4 +77,40 @@ func GetKnownEmailsFromLocalState(s *testing.State) map[string]bool {
 	}
 
 	return knownEmails
+}
+
+// WaitForOwnership waits for up to 20 seconds for for the current user to become device owner.
+// Normally it should take less then a few seconds
+func WaitForOwnership(ctx context.Context, s *testing.State, cr *chrome.Chrome) {
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating login test API connection failed: ", err)
+	}
+
+	// wait until user becomes the device owner
+	testing.ContextLog(ctx, "Waiting for the user to become device owner")
+
+	var pollOpts = &testing.PollOptions{Interval: 1 * time.Second, Timeout: 20 * time.Second}
+	var status struct {
+		IsOwner bool
+	}
+
+	err = testing.Poll(ctx, func(ctx context.Context) error {
+		if err := tconn.Eval(ctx, `tast.promisify(chrome.autotestPrivate.loginStatus)()`, &status); err != nil {
+			// this is caused by failure to run javascript to get login status
+			// quit polling
+			return testing.PollBreak(err)
+		}
+		testing.ContextLogf(ctx, "is owner: %b", status.IsOwner)
+
+		if !status.IsOwner {
+			return errors.New("User did not become device owner yet")
+		}
+
+		return nil
+	}, pollOpts)
+
+	if err != nil {
+		s.Fatal("User failed to become device owner: ", err)
+	}
 }
