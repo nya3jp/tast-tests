d1354e8af206a990b1dee07a47a1a03a9ddf1ec9
Yue Li <updowndota@google.com>
2022-01-20 10:43:29 -0800

    Tast: Add quickanswers.ConsentAllowed Tast tests
    BUG=b:212209813
    TEST=Test on DUT with command: tast -verbose run DUT quickanswers.ConsentAllowed
    
    Change-Id: Ie895ff11768f5810e7b7f089bec7127869e594db

---
+++ src/chromiumos/tast/local/bundles/cros/quickanswers/consent_allowed.go
@@ -0,0 +1,87 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package quickanswers
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/event"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ConsentAllowed,
+		Desc: "Test Quick Answers consent flow",
+		Contacts: []string{
+			"updowndota@google.com",
+			"croissant-eng@google.com",
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// ConsentAllowed tests Quick Answers consent flow.
+func ConsentAllowed(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("QuickAnswersV2"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	// Open page with the query word on it.
+	var queryWord = "icosahedron"
+	conn, err := cr.NewConn(ctx, "https://google.com/search?q="+queryWord)
+	if err != nil {
+		s.Fatal("Failed to create new Chrome connection: ", err)
+	}
+	defer conn.Close()
+	defer conn.CloseTarget(ctx)
+
+	// Wait for the query word to appear.
+	query := nodewith.Name(queryWord).Role(role.StaticText).First()
+	if err := ui.WaitUntilExists(query)(ctx); err != nil {
+		s.Fatal("Failed to wait for query to load: ", err)
+	}
+
+	// Select the word and setup watcher to wait for text selection event.
+	if err := ui.WaitForEvent(nodewith.Root(),
+		event.TextSelectionChanged,
+		ui.Select(query, 0, query, 2))(ctx); err != nil {
+		s.Fatal("Failed to select query: ", err)
+	}
+
+	// Right click the selected word and ensure the consent UI shows up.
+	userConsent := nodewith.ClassName("UserConsentView")
+	allowButton := nodewith.Name("Allow").ClassName("CustomizedLabelButton")
+	if err := uiauto.Combine("Show consent UI",
+		ui.RightClick(query),
+		ui.WaitUntilExists(userConsent))(ctx); err != nil {
+		s.Fatal("Quick Answers consent UI not showing up: ", err)
+	}
+
+	// Left click the allow button and ensure the Quick Answers UI shows up with the query result.
+	quickAnswers := nodewith.ClassName("QuickAnswersView")
+	definitionResult := nodewith.NameContaining("twenty plane faces").ClassName("Label")
+	if err := uiauto.Combine("Show Quick Answers query result",
+		ui.LeftClick(allowButton),
+		ui.WaitUntilExists(quickAnswers),
+		ui.WaitUntilExists(definitionResult))(ctx); err != nil {
+		s.Fatal("Quick Answers query result not showing up: ", err)
+	}
+}
