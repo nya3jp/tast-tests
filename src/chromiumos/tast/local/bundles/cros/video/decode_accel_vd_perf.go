7a587636a85d8b08b08cf825caeb4b49c0368f90
Jeff Chen <jeffcchen@google.com>
2022-01-20 18:50:56 +0000

    [VAAPI Lock] Tast test variants with the VAAPI Lock disabled.
    Tast test variants for having the VAAPI lock disabled for [en|de]coding.
    
    BUG=b:182008564
    TEST=Ran tests on Volteer
    
    Cq-Depend: chromium:3398818, chromium:3404573
    Change-Id: Ib9c23933bb59bd2354306c00e3760a57a86a7786

--- src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_perf.go
+++ src/chromiumos/tast/local/bundles/cros/video/decode_accel_vd_perf.go
@@ -12,6 +12,11 @@ import (
 	"chromiumos/tast/testing"
 )
 
+type videoDecodeAccelVdPerfTestParam struct {
+	dataPath         string
+	disableVaapiLock bool
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         DecodeAccelVDPerf,
@@ -22,110 +27,138 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Name:              "av1_1080p_30fps",
-			Val:               "1080p_30fps_300frames.av1.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_30fps_300frames.av1.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1},
 			ExtraData:         []string{"1080p_30fps_300frames.av1.ivf", "1080p_30fps_300frames.av1.ivf.json"},
 		}, {
 			Name:              "av1_1080p_60fps",
-			Val:               "1080p_60fps_600frames.av1.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.av1.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1_60},
 			ExtraData:         []string{"1080p_60fps_600frames.av1.ivf", "1080p_60fps_600frames.av1.ivf.json"},
 		}, {
 			Name:              "av1_2160p_30fps",
-			Val:               "2160p_30fps_300frames.av1.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_30fps_300frames.av1.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1_4K},
 			ExtraData:         []string{"2160p_30fps_300frames.av1.ivf", "2160p_30fps_300frames.av1.ivf.json"},
 		}, {
 			Name:              "av1_2160p_60fps",
-			Val:               "2160p_60fps_600frames.av1.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_60fps_600frames.av1.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeAV1_4K60},
 			ExtraData:         []string{"2160p_60fps_600frames.av1.ivf", "2160p_60fps_600frames.av1.ivf.json"},
 		}, {
 			Name:              "h264_1080p_30fps",
-			Val:               "1080p_30fps_300frames.h264",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_30fps_300frames.h264"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			ExtraData:         []string{"1080p_30fps_300frames.h264", "1080p_30fps_300frames.h264.json"},
 		}, {
 			Name:              "h264_1080p_60fps",
-			Val:               "1080p_60fps_600frames.h264",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.h264"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264_60, "proprietary_codecs"},
 			ExtraData:         []string{"1080p_60fps_600frames.h264", "1080p_60fps_600frames.h264.json"},
 		}, {
 			Name:              "h264_2160p_30fps",
-			Val:               "2160p_30fps_300frames.h264",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_30fps_300frames.h264"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K, "proprietary_codecs"},
 			ExtraData:         []string{"2160p_30fps_300frames.h264", "2160p_30fps_300frames.h264.json"},
 		}, {
 			Name:              "h264_2160p_60fps",
-			Val:               "2160p_60fps_600frames.h264",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_60fps_600frames.h264"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K60, "proprietary_codecs"},
 			ExtraData:         []string{"2160p_60fps_600frames.h264", "2160p_60fps_600frames.h264.json"},
 		}, {
 			Name:              "hevc_1080p_30fps",
-			Val:               "1080p_30fps_300frames.hevc",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_30fps_300frames.hevc"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC, "proprietary_codecs", "protected_content"},
 			ExtraData:         []string{"1080p_30fps_300frames.hevc", "1080p_30fps_300frames.hevc.json"},
 		}, {
 			Name:              "hevc_1080p_60fps",
-			Val:               "1080p_60fps_600frames.hevc",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.hevc"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC60, "proprietary_codecs", "protected_content"},
 			ExtraData:         []string{"1080p_60fps_600frames.hevc", "1080p_60fps_600frames.hevc.json"},
 		}, {
 			Name:              "hevc_2160p_30fps",
-			Val:               "2160p_30fps_300frames.hevc",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_30fps_300frames.hevc"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC4K, "proprietary_codecs", "protected_content"},
 			ExtraData:         []string{"2160p_30fps_300frames.hevc", "2160p_30fps_300frames.hevc.json"},
 		}, {
 			Name:              "hevc_2160p_60fps",
-			Val:               "2160p_60fps_600frames.hevc",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_60fps_600frames.hevc"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC4K60, "proprietary_codecs", "protected_content"},
 			ExtraData:         []string{"2160p_60fps_600frames.hevc", "2160p_60fps_600frames.hevc.json"},
 		}, {
 			Name:              "vp8_1080p_30fps",
-			Val:               "1080p_30fps_300frames.vp8.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_30fps_300frames.vp8.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			ExtraData:         []string{"1080p_30fps_300frames.vp8.ivf", "1080p_30fps_300frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp8_1080p_60fps",
-			Val:               "1080p_60fps_600frames.vp8.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.vp8.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_60},
 			ExtraData:         []string{"1080p_60fps_600frames.vp8.ivf", "1080p_60fps_600frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp8_2160p_30fps",
-			Val:               "2160p_30fps_300frames.vp8.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_30fps_300frames.vp8.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_4K},
 			ExtraData:         []string{"2160p_30fps_300frames.vp8.ivf", "2160p_30fps_300frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp8_2160p_60fps",
-			Val:               "2160p_60fps_600frames.vp8.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_60fps_600frames.vp8.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_4K60},
 			ExtraData:         []string{"2160p_60fps_600frames.vp8.ivf", "2160p_60fps_600frames.vp8.ivf.json"},
 		}, {
 			Name:              "vp9_1080p_30fps",
-			Val:               "1080p_30fps_300frames.vp9.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_30fps_300frames.vp9.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraData:         []string{"1080p_30fps_300frames.vp9.ivf", "1080p_30fps_300frames.vp9.ivf.json"},
 		}, {
 			Name:              "vp9_1080p_60fps",
-			Val:               "1080p_60fps_600frames.vp9.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.vp9.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_60},
 			ExtraData:         []string{"1080p_60fps_600frames.vp9.ivf", "1080p_60fps_600frames.vp9.ivf.json"},
 		}, {
 			Name:              "vp9_2160p_30fps",
-			Val:               "2160p_30fps_300frames.vp9.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_30fps_300frames.vp9.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_4K},
 			ExtraData:         []string{"2160p_30fps_300frames.vp9.ivf", "2160p_30fps_300frames.vp9.ivf.json"},
 		}, {
 			Name:              "vp9_2160p_60fps",
-			Val:               "2160p_60fps_600frames.vp9.ivf",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "2160p_60fps_600frames.vp9.ivf"},
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_4K60},
 			ExtraData:         []string{"2160p_60fps_600frames.vp9.ivf", "2160p_60fps_600frames.vp9.ivf.json"},
-		}},
+		}, {
+			Name:              "av1_1080p_60fps_vaapi_lock_disabled",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.av1.ivf", disableVaapiLock: true},
+			ExtraSoftwareDeps: []string{caps.HWDecodeAV1_60, "thread_safe_libva_backend"},
+			ExtraData:         []string{"1080p_60fps_600frames.av1.ivf", "1080p_60fps_600frames.av1.ivf.json"},
+		}, {
+			Name:              "h264_1080p_60fps_vaapi_lock_disabled",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.h264", disableVaapiLock: true},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_60, "proprietary_codecs", "thread_safe_libva_backend"},
+			ExtraData:         []string{"1080p_60fps_600frames.h264", "1080p_60fps_600frames.h264.json"},
+		}, {
+			Name:              "hevc_1080p_60fps_vaapi_lock_disabled",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.hevc", disableVaapiLock: true},
+			ExtraSoftwareDeps: []string{caps.HWDecodeHEVC60, "proprietary_codecs", "protected_content", "thread_safe_libva_backend"},
+			ExtraData:         []string{"1080p_60fps_600frames.hevc", "1080p_60fps_600frames.hevc.json"},
+		}, {
+			Name:              "vp8_1080p_60fps_vaapi_lock_disabled",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.vp8.ivf", disableVaapiLock: true},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_60, "thread_safe_libva_backend"},
+			ExtraData:         []string{"1080p_60fps_600frames.vp8.ivf", "1080p_60fps_600frames.vp8.ivf.json"},
+		}, {
+			Name:              "vp9_1080p_60fps_vaapi_lock_disabled",
+			Val:               videoDecodeAccelVdPerfTestParam{dataPath: "1080p_60fps_600frames.vp9.ivf", disableVaapiLock: true},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_60, "thread_safe_libva_backend"},
+			ExtraData:         []string{"1080p_60fps_600frames.vp9.ivf", "1080p_60fps_600frames.vp9.ivf.json"},
+		},
+		},
 	})
 }
 
 func DecodeAccelVDPerf(ctx context.Context, s *testing.State) {
-	if err := decoding.RunAccelVideoPerfTest(ctx, s.OutDir(), s.DataPath(s.Param().(string)), decoding.TestParams{DecoderType: decoding.VD}); err != nil {
+	param := s.Param().(videoDecodeAccelVdPerfTestParam)
+
+	if err := decoding.RunAccelVideoPerfTest(ctx, s.OutDir(), s.DataPath(param.dataPath), decoding.TestParams{DecoderType: decoding.VD, DisableVaapiLock: param.disableVaapiLock}); err != nil {
 		s.Fatal("test failed: ", err)
 	}
 }
