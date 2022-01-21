574a1f1e210f7081bf74acd1d0ef06318b86b9e6
Toni Barzic <tbarzic@google.com>
2022-01-21 12:44:11 -0800

    Parameterize launcher app drag test for productivity launcher
    Updates launcher.AppDragAndDrop test to test productivity launcher.
    For bubble launcher, which does not support pagination, implement test
    that the view can be scrolled during drag (by hovering pointer over
    top/bottom app list view edge), which gets run instead of dragging the
    app to the next page.
    
    BUG=chromium:1288455
    TEST=ran tast launcher.AppDragAndDrop.* on nautilus
    
    Change-Id: If95fccfbdffc188bc9904ddc156669da629acfb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3414693
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/launcher/app_drag_and_drop.go
+++ src/chromiumos/tast/local/bundles/cros/launcher/app_drag_and_drop.go
@@ -30,8 +30,16 @@ func init() {
 			"cienet-development@googlegroups.com",
 			"chromeos-sw-engprod@google.com",
 		},
-		Attr:    []string{"group:mainline", "informational"},
-		Fixture: "chromeLoggedInWith100FakeApps",
+		Attr: []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Name:    "productivity_launcher",
+			Val:     true,
+			Fixture: "chromeLoggedInWith100FakeAppsProductivityLauncher",
+		}, {
+			Name:    "",
+			Val:     false,
+			Fixture: "chromeLoggedInWith100FakeApps",
+		}},
 	})
 }
 
@@ -54,8 +62,8 @@ func AppDragAndDrop(ctx context.Context, s *testing.State) {
 		modeName string
 		isTablet bool
 	}{
-		{"drag and drop in clamshell mode", false},
-		{"drag and drop in tablet mode", true},
+		{"drag and drop app in clamshell mode", false},
+		{"drag and drop app in tablet mode", true},
 	} {
 		cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, subtest.isTablet)
 		if err != nil {
@@ -63,6 +71,7 @@ func AppDragAndDrop(ctx context.Context, s *testing.State) {
 		}
 		defer cleanup(cleanupCtx)
 
+		productivityLauncher := s.Param().(bool)
 		f := func(ctx context.Context, s *testing.State) {
 			defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, subtest.modeName+"_ui_dump")
 
@@ -72,8 +81,16 @@ func AppDragAndDrop(ctx context.Context, s *testing.State) {
 				}
 			}
 
-			if err := launcher.Open(tconn)(ctx); err != nil {
-				s.Fatal("Failed to open the launcher: ", err)
+			usingBubbleLauncher := productivityLauncher && !subtest.isTablet
+			// Open the Launcher and go to Apps list page.
+			if usingBubbleLauncher {
+				if err := launcher.OpenBubbleLauncher(tconn)(ctx); err != nil {
+					s.Fatal("Failed to open bubble launcher: ", err)
+				}
+			} else {
+				if err := launcher.Open(tconn)(ctx); err != nil {
+					s.Fatal("Failed to open the launcher: ", err)
+				}
 			}
 
 			if err := launcher.WaitForStableNumberOfApps(ctx, tconn); err != nil {
@@ -83,21 +100,41 @@ func AppDragAndDrop(ctx context.Context, s *testing.State) {
 			// Each subtest requires at least 3 items on the current page - the first page may have a (default) page break after several
 			// default apps, and depending on the device may not have enough apps to satisfy this requirement.
 			// To work around this, start the test on the second launcher page.
-			if err := newLauncherPageSwitcher(ui).switchToPage(1)(ctx); err != nil {
-				s.Fatal("Failed to switch to second page for test: ", err)
+			if !usingBubbleLauncher {
+				if err := newLauncherPageSwitcher(ui).switchToPage(1)(ctx); err != nil {
+					s.Fatal("Failed to switch to second page for test: ", err)
+				}
 			}
 
-			firstItem, err := getFirstItemOnCurrentPage(ctx, tconn)
+			firstItem, err := launcher.FirstNonRecentAppItem(ctx, tconn)
 			if err != nil {
-				s.Fatal("Failed to get the first item on the current page: ", err)
+				s.Fatal("Failed to count recent apps items: ", err)
 			}
 
-			if err := dragIconToIcon(ctx, tconn, ui, firstItem); err != nil {
+			if !usingBubbleLauncher {
+				firstItem, err = getFirstItemOnCurrentPage(ctx, tconn, firstItem)
+				if err != nil {
+					s.Fatal("Failed to get the first item on the current page: ", err)
+				}
+			}
+
+			if err := dragIconToIcon(ctx, tconn, ui, firstItem, productivityLauncher); err != nil {
 				s.Fatal("Failed to drag the first icon to the second icon: ", err)
 			}
 
-			if err := dragIconToNextPage(ctx, tconn, ui, firstItem); err != nil {
-				s.Fatal("Failed to drag the first icon to next page: ", err)
+			if !usingBubbleLauncher {
+				if err := dragIconToNextPage(ctx, tconn, ui, firstItem); err != nil {
+					s.Fatal("Failed to drag the first icon to next page: ", err)
+				}
+			} else {
+				dropIndex, err := dragFirstIconToScrollableContainerBottom(ctx, tconn, ui)
+				if err != nil {
+					s.Fatal("Failed to drag the first icon to bottom of scrollable container: ", err)
+				}
+
+				if err := dragIconToScrollableContainerTop(ctx, tconn, ui, dropIndex); err != nil {
+					s.Fatal("Failed to drag the last item to top of scrollable container: ", err)
+				}
 			}
 		}
 
@@ -107,7 +144,10 @@ func AppDragAndDrop(ctx context.Context, s *testing.State) {
 	}
 }
 
-func dragIconToIcon(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.Context, firstItem int) error {
+// dragIconToIcon drags an app list item at firstItem index in the current app list page to an item at index firstItem + 2, creating a folder.
+// It then removes all items from the folder, and verifies the original item gets dropped into a different location.
+// productivityLauncher indicates whether the test is run for productivityLauncher, which subtly changes folder interfactions.
+func dragIconToIcon(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.Context, firstItem int, productivityLauncher bool) error {
 	srcInfo, err := ui.Info(ctx, nodewith.HasClass(launcher.ExpandedItemsClass).Nth(firstItem))
 	if err != nil {
 		return errors.Wrap(err, "failed to get information of first icon")
@@ -126,8 +166,22 @@ func dragIconToIcon(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.Cont
 		return errors.Wrap(err, "failed to drag the first icon to the third icon")
 	}
 
+	// For productivity launcher, folders get automatically opened after getting created by dragging - make sure the created folder gets closed.
+	if productivityLauncher {
+		if err := launcher.CloseFolderView(ctx, tconn); err != nil {
+			return errors.Wrap(err, "failed to close the folder")
+		}
+	}
+
 	if err := launcher.RemoveIconFromFolder(tconn)(ctx); err != nil {
 		return errors.Wrap(err, "failed to drag out the icon from folder")
+	}
+
+	// Productivity launcher supports single-item folders, so the folder should still exist after removing second to last item.
+	if productivityLauncher {
+		if err := launcher.RemoveIconFromFolder(tconn)(ctx); err != nil {
+			return errors.Wrap(err, "failed to drag out the icon from single-item folder")
+		}
 	}
 
 	locAfter, err := ui.Location(ctx, src)
@@ -143,7 +197,7 @@ func dragIconToIcon(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.Cont
 	return nil
 }
 
-// dragIconToNextPage drags an icon to from the current to the next page.
+// dragIconToNextPage drags an icon at index itemIndex in the app list UI from the current to the next page.
 func dragIconToNextPage(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.Context, itemIndex int) error {
 	srcInfo, err := ui.Info(ctx, nodewith.HasClass(launcher.ExpandedItemsClass).Nth(itemIndex))
 	if err != nil {
@@ -177,9 +231,72 @@ func dragIconToNextPage(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.
 	return nil
 }
 
+// dragFirstIconToScrollableContainerBottom drags the first item in the scrollable apps grid view to the last available slot in the view.
+// Returns the index of the drag item view in the scrollable app grid after successful drag operation.
+func dragFirstIconToScrollableContainerBottom(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.Context) (int, error) {
+	scrollableGridItems := nodewith.ClassName(launcher.ExpandedItemsClass).Ancestor(nodewith.ClassName("ScrollableAppsGridView"))
+
+	dragItem := scrollableGridItems.Nth(0)
+	dragItemInfo, err := ui.Info(ctx, dragItem)
+	if err != nil {
+		return -1, errors.Wrap(err, "failed to get drag item info")
+	}
+	dragItemName := dragItemInfo.Name
+
+	allItemsInfo, err := ui.NodesInfo(ctx, scrollableGridItems)
+	if err != nil {
+		return -1, errors.Wrap(err, "failed to get list of grid items")
+	}
+	itemCount := len(allItemsInfo)
+	targetItem := scrollableGridItems.Nth(itemCount - 1)
+
+	if err := launcher.DragItemInBubbleLauncherWithScrolling(ctx, tconn, ui, dragItem, targetItem /*up=*/, false); err != nil {
+		return -1, errors.Wrap(err, "bubble launcher scroll failed")
+	}
+
+	// Drag item should have been moved right of the first item in the scrollable grid.
+	lastItemInfo, err := ui.Info(ctx, scrollableGridItems.Nth(itemCount-1))
+	if err != nil {
+		return -1, errors.Wrap(err, "failed to get second app item info")
+	}
+
+	if dragItemName != lastItemInfo.Name {
+		return -1, errors.Wrapf(err, "Last item %s is not the drag item %s", lastItemInfo.Name, dragItemInfo.Name)
+	}
+	return itemCount - 1, nil
+}
+
+// dragIconToScrollableContainerTop drags an app list item at itemIndex in the scrollable apps grid view to the slot after the first item.
+func dragIconToScrollableContainerTop(ctx context.Context, tconn *chrome.TestConn, ui *uiauto.Context, itemIndex int) error {
+	scrollableGridItems := nodewith.ClassName(launcher.ExpandedItemsClass).Ancestor(nodewith.ClassName("ScrollableAppsGridView"))
+	dragItem := scrollableGridItems.Nth(itemIndex)
+	dragItemInfo, err := ui.Info(ctx, dragItem)
+	if err != nil {
+		return errors.Wrap(err, "failed to get drag item info")
+	}
+	dragItemName := dragItemInfo.Name
+
+	targetItem := scrollableGridItems.Nth(0)
+
+	if err := launcher.DragItemInBubbleLauncherWithScrolling(ctx, tconn, ui, dragItem, targetItem /*up=*/, true); err != nil {
+		return errors.Wrap(err, "bubble launcher scroll failed")
+	}
+
+	// Drag item should have been moved right of the first item in the scrollable grid.
+	secondItemInfo, err := ui.Info(ctx, scrollableGridItems.Nth(1))
+	if err != nil {
+		return errors.Wrap(err, "failed to get second app item info")
+	}
+
+	if dragItemName != secondItemInfo.Name {
+		return errors.Wrapf(err, "Last item %s is not the drag item %s", secondItemInfo.Name, dragItemName)
+	}
+	return nil
+}
+
 // getFirstItemOnCurrentPage returns the index on the first app list item that's on the current page
-func getFirstItemOnCurrentPage(ctx context.Context, tconn *chrome.TestConn) (int, error) {
-	for currentIndex := 0; ; currentIndex++ {
+func getFirstItemOnCurrentPage(ctx context.Context, tconn *chrome.TestConn, minIndex int) (int, error) {
+	for currentIndex := minIndex; ; currentIndex++ {
 		itemOnCurrentPage, err := launcher.IsItemOnCurrentPage(ctx, tconn, nodewith.ClassName(launcher.ExpandedItemsClass).Nth(currentIndex))
 		if err != nil {
 			return -1, errors.Wrap(err, "checking whether item is on page failed")
