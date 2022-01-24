1286acd62772def2aa8f311baaab74f03700d836
Taoyu Li <taoyl@google.com>
2022-01-24 23:00:27 +0900

    network: add VPNMojoConf test
    This patch adds a test to configure VPN through Chrome mojo API
    chromeos.networkConfig.mojom.CrosNetworkConfig.configureNetwork()
    and verify that shill service is correctly created.
    
    BUG=b:200229838
    TEST=tast run $DUT network.VPNMojoConf
    
    Change-Id: I66ba53b0b36579d5a6085f5cab5976d65b018e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3412611
    Reviewed-by: Jie Jiang <jiejiang@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Taoyu Li <taoyl@chromium.org>
    Commit-Queue: Taoyu Li <taoyl@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/network/vpn_policy.go
+++ src/chromiumos/tast/local/bundles/cros/network/vpn_policy.go
@@ -6,12 +6,9 @@ package network
 
 import (
 	"context"
-	"time"
 
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
-	"chromiumos/tast/common/shillconst"
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/network/vpn"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/policyutil"
@@ -116,49 +113,9 @@ func VPNPolicy(ctx context.Context, s *testing.State) {
 				s.Fatalf("Failed to update %s policy: %v", tc.subtest, err)
 			}
 
-			if err := verifyVPNProfile(ctx, m, tc.guid); err != nil {
+			if err := vpn.VerifyVPNProfile(ctx, m, tc.guid, true); err != nil {
 				s.Errorf("Verifying %s profile defined VPN failed: %v", tc.subtest, err)
 			}
 		})
 	}
-}
-
-func verifyVPNProfile(ctx context.Context, m *shill.Manager, serviceGUID string) error {
-	testing.ContextLog(ctx, "Trying to find service with guid ", serviceGUID)
-
-	findServiceProps := make(map[string]interface{})
-	findServiceProps["GUID"] = serviceGUID
-	findServiceProps["Type"] = "vpn"
-	service, err := m.WaitForServiceProperties(ctx, findServiceProps, 5*time.Second)
-	if err != nil {
-		testing.ContextLog(ctx, "Cannot find service matching guid ", serviceGUID)
-		return err
-	}
-	testing.ContextLogf(ctx, "Found service %v matching guid %s, connecting", service, serviceGUID)
-
-	pw, err := service.CreateWatcher(ctx)
-	if err != nil {
-		return errors.Wrap(err, "failed to create watcher")
-	}
-	defer pw.Close(ctx)
-	if err = service.Connect(ctx); err != nil {
-		return errors.Wrapf(err, "failed to connect the service %v", service)
-	}
-	defer func() {
-		if err = service.Disconnect(ctx); err != nil {
-			testing.ContextLog(ctx, "Failed to disconnect service ", service)
-		}
-	}()
-
-	timeoutCtx, cancel := context.WithTimeout(ctx, 35*time.Second)
-	defer cancel()
-	state, err := pw.ExpectIn(timeoutCtx, shillconst.ServicePropertyState, append(shillconst.ServiceConnectedStates, shillconst.ServiceStateFailure))
-	if err != nil {
-		return err
-	}
-
-	if state == shillconst.ServiceStateFailure {
-		return errors.Errorf("service %v became failure state", service)
-	}
-	return nil
 }
