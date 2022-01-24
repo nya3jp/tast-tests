1068ad14e08adcb7404cfcaf5dc5751147f28705
Marcello Salomao <msalomao@google.com>
2022-01-25 02:31:15 +1100

    security: Isolate archive test setup and tear-down into new fixtures
    Leverage fixtures to effectively test archive mounting
    under guest mode and with regular users.
    
    BUG=chromium:1157348
    TEST=tast run $DUT "*ZipMount*"
    
    Cq-Depend: chromium:3411092
    Change-Id: I641c0c3005cc0c937b091f6317742e7876368cd0

--- src/chromiumos/tast/local/bundles/cros/filemanager/pre/pre.go
+++ src/chromiumos/tast/local/bundles/cros/filemanager/pre/pre.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO: migrate filemanager preconditions to fixtures
+
 // Package pre contains the preconditions used by the filemanager tests.
 package pre
 
