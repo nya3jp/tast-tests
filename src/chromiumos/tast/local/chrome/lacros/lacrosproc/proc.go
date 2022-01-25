6dbad61379d73be94a418be52868034420295e72
Florian Zubeil <zubeil@google.com>
2022-01-25 12:57:59 +0000

    kiosk: Test lacros restarts properly in web kiosk mode
    Testing the behaviour of lacros-chrome crashing in web kiosk mode.
    Lacros is currently not recovering correctly
    (https://crbug.com/1290810), so the test will currently fail.
    
    BUG=b:215521643
    TEST=tast run <IP> kiosk.LacrosRestartOnCrash
    
    Change-Id: I3e5ee66c9a447396fd80e7f457dbb6ad2e0a97ab

---
+++ src/chromiumos/tast/local/chrome/lacros/lacrosproc/proc.go
@@ -0,0 +1,56 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package lacrosproc provides utilities to find lacros Chrome processes.
+package lacrosproc
+
+import (
+	"path/filepath"
+
+	"github.com/shirou/gopsutil/process"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome/internal/chromeproc"
+)
+
+// LacrosLocation specifies how lacros has been deployed,
+// since this effects the location of the binary.
+type LacrosLocation int
+
+const (
+	// Rootfs lacros location.
+	Rootfs LacrosLocation = iota
+	// Stateful lacros location.
+	Stateful
+	// Deployed lacros location (e.g. via deploy_chrome.py).
+	Deployed
+)
+
+const (
+	rootfsLacrosExecPath   = "/run/lacros/chrome"
+	statefulLacrosExecPath = "/run/imageloader/lacros-*/*/chrome"
+	deployedLacrosExecPath = "/usr/local/lacros-chrome"
+)
+
+// Root returns the Process instance of the root lacros-chrome process.
+func Root(t LacrosLocation) (*process.Process, error) {
+	switch t {
+	case Rootfs:
+		return chromeproc.Root(rootfsLacrosExecPath)
+	case Stateful:
+		matches, err := filepath.Glob(statefulLacrosExecPath)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(matches) != 1 {
+			return nil, errors.Errorf("found %d lacros executables, expected 1. Pattern: %s", len(matches), statefulLacrosExecPath)
+		}
+
+		return chromeproc.Root(matches[0])
+	case Deployed:
+		return chromeproc.Root(deployedLacrosExecPath)
+	}
+	return nil, errors.Errorf("unknown lacros type %d", t)
+}
