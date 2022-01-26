13e1a85617e2bd818156c002c2fd74c32c8d2c0b
Jonathan Fan <jonfan@google.com>
2022-01-25 22:56:48 -0800

    Enable TCP based cros server logging to stdout
    Enable stdout logging option for TCP based grpc server.
    Process the stdout and stderr logs from the SSH Client
    to Tast client logs.
    
    See go/tcp-tast-server-grpc-stdout-logging
    
    BUG=b:208267010
    TEST=tast run -build=true <DUT> browser.ChromeServiceGRPC
    
    Cq-Depend: chromium:3469828
    Change-Id: I660737e77497cb757d986b79427af9fecece364d

--- src/chromiumos/tast/remote/crosserverutil/cros_server_util.go
+++ src/chromiumos/tast/remote/crosserverutil/cros_server_util.go
@@ -6,6 +6,7 @@
 package crosserverutil
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"strconv"
@@ -108,7 +109,7 @@ func Dial(ctx context.Context, d *dut.DUT, hostname string, port int, useForward
 
 // StartCrosServer initiates the cros server process and grpc server on DUT through SSH
 func StartCrosServer(ctx context.Context, sshConn *ssh.Conn, port int) error {
-	args := []string{"-rpctcp", "-port", strconv.Itoa(port)}
+	args := []string{"-rpctcp", "-port", strconv.Itoa(port), "-stdoutlogging"}
 	testing.ContextLog(ctx, "Start CrOS server with parameters: ", args)
 
 	// Try to kill any process using the desired port
@@ -123,13 +124,29 @@ func StartCrosServer(ctx context.Context, sshConn *ssh.Conn, port int) error {
 	}
 
 	// Start CrOS server as a separate process
-	// TODO(jonfan): Pipe the output from ssh command to testing.contextlog with a marker prefix
-	// For cros server log to be effective, changes in cros server has to be made such that
-	// log from individual grpc services can be aggregated to the cros server log instead of
-	// being exposed through the grp  directional log streaming service
 	// TODO(jonfan): To keep the path of cros private and encapsulated from the users, create a
 	// shell script or symlink, e.g. /usr/bin/cros, that resolves the path for cros
 	cmd := sshConn.CommandContext(ctx, "/usr/local/libexec/tast/bundles/local_pushed/cros", args...)
+
+	// combine stdout and stderr to a single reader by assigning a single pipe to cmd.Stdout
+	// and cmd.Stderr
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to setup StdOut pipe")
+	}
+	cmd.Stderr = cmd.Stdout
+	stdoutScanner := bufio.NewScanner(cmdReader)
+
+	// Pipe the output from ssh command to testing.contextlog
+	go func() {
+		// The command session will close the stderr and stdout upon termination
+		// causing the scanner to exit the loop.
+		for stdoutScanner.Scan() {
+			line := stdoutScanner.Text()
+			testing.ContextLog(ctx, "cros: ", line)
+		}
+	}()
+
 	if err := cmd.Start(); err != nil {
 		return errors.Wrapf(err, "failed to start CrOS server with parameter: %v", args)
 	}
@@ -156,7 +173,10 @@ func StopCrosServer(ctx context.Context, sshConn *ssh.Conn, port int) error {
 		return err
 	}
 	testing.ContextLogf(ctx, "Kill CrOS server process pid: %d port: %d", pid, port)
-	if out, err := sshConn.CommandContext(ctx, "kill", "-9", strconv.Itoa(pid)).CombinedOutput(); err != nil {
+	// Cros server process intercepts SIGINT and SIGTERM to gracefully stop gRPC server
+	// and the cros process. Killing with SIGTERM  provides the client side an opportunity
+	// to receive logs during the server shutdown routine.
+	if out, err := sshConn.CommandContext(ctx, "kill", "-15", strconv.Itoa(pid)).CombinedOutput(); err != nil {
 		return errors.Wrapf(err, "failed to kill CrOS server process pid: %d port: %d StdOut: %v", pid, port, out)
 	}
 
