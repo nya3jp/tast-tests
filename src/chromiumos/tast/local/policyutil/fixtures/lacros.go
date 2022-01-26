b783ec8f9e63a570b223c6813ce2e0a10380ad7a
Daniel d'Andrada <dandrader@google.com>
2022-01-26 12:02:37 +0100

    WindowCaptureAllowedByOrigins policy test
    BUG=chromium:1288387
    TEST=tast run $DUT policy.WindowCaptureAllowedByOrigins[.lacros]
    
    Change-Id: I8c78f6cb4a19497b14f02e5622453e14cb1864f2

--- src/chromiumos/tast/local/policyutil/fixtures/lacros.go
+++ src/chromiumos/tast/local/policyutil/fixtures/lacros.go
@@ -45,4 +45,38 @@ func init() {
 		Parent:          fixture.PersistentLacros,
 		Vars:            []string{lacrosfixt.LacrosDeployedBinary},
 	})
+
+	// TODO(crbug/1291205): Make it possible to add extra chrome args to fixture.LacrosPolicyLoggedIn without all that code duplication.
+	testing.AddFixture(&testing.Fixture{
+		Name:     fixture.LacrosPolicyLoggedInWindowCapture,
+		Desc:     "LacrosPolicyLoggedIn that skips the capture source dialog",
+		Contacts: []string{"dandrader@google.com", "chromeos-commercial-remote-management@google.com"},
+		Impl: lacrosfixt.NewComposedFixture(lacrosfixt.Rootfs, func(v lacrosfixt.FixtValue, pv interface{}) interface{} {
+			return &struct {
+				fakedms.HasFakeDMS
+				lacrosfixt.FixtValue
+			}{
+				pv.(fakedms.HasFakeDMS),
+				v,
+			}
+		}, func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			fdms, ok := s.ParentValue().(*fakedms.FakeDMS)
+			if !ok {
+				return nil, errors.New("parent is not a FakeDMS fixture")
+			}
+			opts := []chrome.Option{
+				chrome.DMSPolicy(fdms.URL),
+				chrome.FakeLogin(chrome.Creds{User: "tast-user@managedchrome.com", Pass: "test0000"}),
+				chrome.ExtraArgs("--disable-lacros-keep-alive"),
+				// The only line that this Fixture adds in comparison to fixture.LacrosPolicyLoggedIn
+				chrome.ExtraArgs("--auto-select-desktop-capture-source=Chrome"),
+			}
+			return opts, nil
+		}),
+		SetUpTimeout:    chrome.LoginTimeout + 7*time.Minute,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		Parent:          fixture.PersistentLacros,
+		Vars:            []string{lacrosfixt.LacrosDeployedBinary},
+	})
 }
