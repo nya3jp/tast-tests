cad29bb9346544fa265abd37649c6bd4183864b9
Jan Kopański <jkopanski@google.com>
2022-01-26 16:44:32 +0100

    dlp: Add Lacros variant for DataLeakPreventionRulesListPrivacyScreen
    BUG=chromium:1291191
    TEST=tast run <ip> dlp.DataLeakPreventionRulesListPrivacyScreen\*
    
    Change-Id: Ie84d2dd899f33a534c284f33882f62a076559001

--- src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_privacy_screen.go
+++ src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_privacy_screen.go
@@ -9,11 +9,15 @@ import (
 	"strings"
 	"time"
 
+	"chromiumos/tast/common/fixture"
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
 	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
@@ -34,13 +38,26 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		HardwareDeps: hwdep.D(hwdep.PrivacyScreen()),
 		Attr:         []string{"group:mainline"},
-		Fixture:      "chromePolicyLoggedIn",
+		Params: []testing.Param{{
+			Fixture: fixture.ChromePolicyLoggedIn,
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraAttr:         []string{"informational"},
+			ExtraSoftwareDeps: []string{"lacros"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val:               browser.TypeLacros,
+		}},
 	})
 }
 
 func DataLeakPreventionRulesListPrivacyScreen(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	fakeDMS := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
 
 	// DLP policy with privacy screen blocked restriction.
 	policyDLP := []policy.Policy{&policy.DataLeakPreventionRulesList{
@@ -98,9 +115,15 @@ func DataLeakPreventionRulesListPrivacyScreen(ctx context.Context, s *testing.St
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
 			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
 
+			br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(browser.Type))
+			if err != nil {
+				s.Fatal("Failed to open the browser: ", err)
+			}
+			defer closeBrowser(cleanupCtx)
+
 			ui := uiauto.New(tconn)
 
-			conn, err := cr.NewConn(ctx, "https://"+param.url)
+			conn, err := br.NewConn(ctx, "https://"+param.url)
 			if err != nil {
 				s.Fatal("Failed to open page: ", err)
 			}
@@ -123,7 +146,7 @@ func DataLeakPreventionRulesListPrivacyScreen(ctx context.Context, s *testing.St
 				s.Errorf("Privacy screen prop value: got %v; want false", value)
 			}
 
-			if _, err := cr.NewConn(ctx, "https://www.google.com"); err != nil {
+			if _, err := br.NewConn(ctx, "https://www.google.com"); err != nil {
 				s.Error("Failed to open page: ", err)
 			}
 
