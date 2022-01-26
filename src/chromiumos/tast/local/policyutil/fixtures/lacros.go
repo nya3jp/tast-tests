a48c9dd06f4e2a320c49b812afb1e8cf927f8770
Anastasiia N <anastasiian@google.com>
2022-01-26 13:22:23 +0100

    tast-tests: Add a tast test for Lacros main profile behavior
    This test is for managed user.
    
    BUG=b:215674255
    TEST=tast run <ip> policy.LacrosMainProfileLogin
    
    Change-Id: Id7ba931e048276be382a0d3c684d3d6f1ec6d5df

--- src/chromiumos/tast/local/policyutil/fixtures/lacros.go
+++ src/chromiumos/tast/local/policyutil/fixtures/lacros.go
@@ -45,4 +45,33 @@ func init() {
 		Parent:          fixture.PersistentLacros,
 		Vars:            []string{lacrosfixt.LacrosDeployedBinary},
 	})
+
+	testing.AddFixture(&testing.Fixture{
+		Name:     fixture.LacrosPolicyLoggedInRealUser,
+		Desc:     "Fixture for a running FakeDMS with lacros with a real managed user logged on",
+		Contacts: []string{"anastasiian@chromium.org", "chromeos-commercial-remote-management@google.com"},
+		Impl: lacrosfixt.NewComposedFixture(lacrosfixt.Rootfs, func(v lacrosfixt.FixtValue, pv interface{}) interface{} {
+			return &struct {
+				fakedms.HasFakeDMS
+				lacrosfixt.FixtValue
+			}{
+				pv.(fakedms.HasFakeDMS),
+				v,
+			}
+		}, func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			fdms, ok := s.ParentValue().(*fakedms.FakeDMS)
+			if !ok {
+				return nil, errors.New("parent is not a FakeDMS fixture")
+			}
+			opts := []chrome.Option{chrome.DMSPolicy(fdms.URL),
+				chrome.GAIALogin(chrome.Creds{User: s.RequiredVar("policy.ManagedUser.user_name"), Pass: s.RequiredVar("policy.ManagedUser.password")}),
+				chrome.ExtraArgs("--disable-lacros-keep-alive")}
+			return opts, nil
+		}),
+		SetUpTimeout:    chrome.LoginTimeout + 7*time.Minute,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		Parent:          fixture.PersistentLacrosRealUser,
+		Vars:            []string{"policy.ManagedUser.user_name", "policy.ManagedUser.password", lacrosfixt.LacrosDeployedBinary},
+	})
 }
