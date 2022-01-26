64720314c9489a398c3e269076683a3708c4a84c
Irem Uguz <iremuguz@google.com>
2022-01-26 10:14:35 +0000

    tast: Add multiple captures case to debugd.PacketCapture
    Add multiple packet captures running at the same time test case to
    debugd.PacketCapture tast test. Change the part in the test where we
    stop the packet capture explicitly for all the test cases to only stop
    it when the capture is not expected to terminate itself (due to error or
    file size limit).
    
    BUG=b:207359039
    TEST=Run the modified tast test by running "tast -verbose run <DUT IP>
    debugd.PacketCapture" command.
    
    Change-Id: I159d4effcbec89e830d61043d8441cb84703cdae

--- src/chromiumos/tast/local/bundles/cros/debugd/packet_capture.go
+++ src/chromiumos/tast/local/bundles/cros/debugd/packet_capture.go
@@ -7,6 +7,7 @@ package debugd
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
@@ -66,50 +67,66 @@ func PacketCapture(ctx context.Context, s *testing.State) {
 		// Output file size limit. Zero if there's no size limit.
 		// Use MiBs to be compatible with packet capture DBus inputs and make tests more readable.
 		maxFileSizeMiBs int64
+		// The number of packet capture operations that will be initiated by the test.
+		numberOfCaptures int
+		// shouldTerminate will be true if the test needs to stop the packet capture operations explicitly.
+		// It'll be set to false if the packet capture operation will terminate itself due to an error or
+		// reaching maximum file size limit.
+		shouldTerminate bool
 	}{
 		{
 			name:      "unallowed_by_policy",
 			policyVal: &policy.DeviceDebugPacketCaptureAllowed{Val: false},
 			options: map[string]dbus.Variant{
 				"device": (dbus.MakeVariant("lo"))},
-			expectSuccess:   false,
-			captureSizeMiBs: 5,
-			maxFileSizeMiBs: 0,
+			expectSuccess:    false,
+			captureSizeMiBs:  5,
+			maxFileSizeMiBs:  0,
+			numberOfCaptures: 1,
+			shouldTerminate:  false,
 		},
 		{
 			name:      "successful_device_based_capture",
 			policyVal: &policy.DeviceDebugPacketCaptureAllowed{Val: true},
 			options: map[string]dbus.Variant{
 				"device": (dbus.MakeVariant("lo"))},
-			expectSuccess:   true,
-			captureSizeMiBs: 5,
-			maxFileSizeMiBs: 0,
+			expectSuccess:    true,
+			captureSizeMiBs:  5,
+			maxFileSizeMiBs:  0,
+			numberOfCaptures: 1,
+			shouldTerminate:  true,
 		},
 		{
-			name:            "empty_arguments",
-			policyVal:       &policy.DeviceDebugPacketCaptureAllowed{Val: true},
-			options:         map[string]dbus.Variant{},
-			expectSuccess:   false,
-			captureSizeMiBs: 5,
-			maxFileSizeMiBs: 0,
+			name:             "empty_arguments",
+			policyVal:        &policy.DeviceDebugPacketCaptureAllowed{Val: true},
+			options:          map[string]dbus.Variant{},
+			expectSuccess:    false,
+			captureSizeMiBs:  5,
+			maxFileSizeMiBs:  0,
+			numberOfCaptures: 1,
+			shouldTerminate:  false,
 		},
 		{
 			name:      "wrong_device",
 			policyVal: &policy.DeviceDebugPacketCaptureAllowed{Val: true},
 			options: map[string]dbus.Variant{
 				"device": (dbus.MakeVariant("fake_device"))},
-			expectSuccess:   false,
-			captureSizeMiBs: 5,
-			maxFileSizeMiBs: 0,
+			expectSuccess:    false,
+			captureSizeMiBs:  5,
+			maxFileSizeMiBs:  0,
+			numberOfCaptures: 1,
+			shouldTerminate:  false,
 		},
 		{
 			name:      "high_volume_capture",
 			policyVal: &policy.DeviceDebugPacketCaptureAllowed{Val: true},
 			options: map[string]dbus.Variant{
 				"device": (dbus.MakeVariant("lo"))},
-			expectSuccess:   true,
-			captureSizeMiBs: 500,
-			maxFileSizeMiBs: 0,
+			expectSuccess:    true,
+			captureSizeMiBs:  500,
+			maxFileSizeMiBs:  0,
+			numberOfCaptures: 1,
+			shouldTerminate:  true,
 		},
 		{
 			name:      "max_size_option",
@@ -117,9 +134,22 @@ func PacketCapture(ctx context.Context, s *testing.State) {
 			options: map[string]dbus.Variant{
 				"device":   (dbus.MakeVariant("lo")),
 				"max_size": (dbus.MakeVariant(5))},
-			expectSuccess:   true,
-			captureSizeMiBs: 15,
-			maxFileSizeMiBs: 5,
+			expectSuccess:    true,
+			captureSizeMiBs:  15,
+			maxFileSizeMiBs:  5,
+			numberOfCaptures: 1,
+			shouldTerminate:  false,
+		},
+		{
+			name:      "multiple_captures",
+			policyVal: &policy.DeviceDebugPacketCaptureAllowed{Val: true},
+			options: map[string]dbus.Variant{
+				"device": (dbus.MakeVariant("lo"))},
+			expectSuccess:    true,
+			captureSizeMiBs:  5,
+			maxFileSizeMiBs:  0,
+			numberOfCaptures: 5,
+			shouldTerminate:  true,
 		},
 	} {
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
@@ -133,75 +163,95 @@ func PacketCapture(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to update policies: ", err)
 			}
 
-			// Create output file for packet capture operation.
-			of, err := ioutil.TempFile("", "test.pcap")
-			if err != nil {
-				s.Fatal("Failed to create output file: ", err)
-			}
-			if err = os.Chmod(of.Name(), 0666); err != nil {
-				s.Fatal("Failed to make output file writable for packet capture: ", err)
+			type packetCaptureDetails struct {
+				// Process handle of packet capture process.
+				handle string
+				// The name of the file that packet capture output will be written into.
+				outputFile string
+				// Indicates the packet capture process is running and not stopped yet.
+				isCapturing bool
 			}
 
-			// Remove the created output file aftert the test ends.
+			testCaptures := make([]packetCaptureDetails, param.numberOfCaptures)
+
+			// Stop the packet capture operations if they haven't yet when the test ends.
 			defer func() {
-				if err := os.Remove(of.Name()); err != nil {
-					s.Error("Failed to remove the output file : ", err)
+				for i := 0; i < param.numberOfCaptures; i++ {
+					if !testCaptures[i].isCapturing {
+						continue
+					}
+
+					// Stop packet capture.
+					if err := dbg.PacketCaptureStop(ctx, testCaptures[i].handle); err != nil {
+						s.Error("PacketCaptureStop DBus call failed: ", err)
+					}
 				}
 			}()
 
-			readPipe, writePipe, err := os.Pipe()
-			if err != nil {
-				s.Fatal("Failed to create status pipe: ", err)
-			}
-			defer func() {
-				writePipe.Close()
-				readPipe.Close()
-			}()
+			// Start packet captures for testing.
+			for i := 0; i < param.numberOfCaptures; i++ {
+				fileName := fmt.Sprintf("test%d.pcap", i)
+				// Create output file for packet capture operation.
+				of, err := ioutil.TempFile("", fileName)
+				if err != nil {
+					s.Fatal("Failed to create output file: ", err)
+				}
+				if err = os.Chmod(of.Name(), 0666); err != nil {
+					s.Fatal("Failed to make output file writable for packet capture: ", err)
+				}
 
-			isCapturing := true
-			testing.ContextLog(ctx, "Making packet capture D-Bus call")
-			handle, err := dbg.PacketCaptureStart(ctx, of, writePipe, param.options)
-			defer func() {
-				if !isCapturing {
+				// Remove the created output file after the test ends.
+				defer func() {
+					if err := os.Remove(of.Name()); err != nil {
+						s.Error("Failed to remove the output file : ", err)
+					}
+				}()
+
+				readPipe, writePipe, err := os.Pipe()
+				if err != nil {
+					s.Fatal("Failed to create status pipe: ", err)
+				}
+				defer func() {
+					writePipe.Close()
+					readPipe.Close()
+				}()
+
+				testing.ContextLog(ctx, "Making packet capture D-Bus call")
+				handle, err := dbg.PacketCaptureStart(ctx, of, writePipe, param.options)
+
+				if err == nil && !param.expectSuccess {
+					// If there is no error returned when failure is expected, check the
+					// status output of the call. dbugd's packet capture tool runs an
+					// executable for packet capture operation and if there's an error
+					// during execution, it doesn't return error but it writes on status
+					// pipe. Status output will be non-empty in case of a failure.
+					writePipe.Close()
+					readPipe.SetDeadline(time.Now().Add(15 * time.Second))
+
+					testing.ContextLog(ctx, "Reading error from packet capture")
+					var buf bytes.Buffer
+					if _, err := io.Copy(&buf, readPipe); err != nil {
+						s.Fatal("Failed to read error: ", err)
+					}
+					if buf.String() == "" {
+						s.Error("PacketCaptureStart succeeded when it's expected to fail")
+					}
 					return
+				} else if err != nil && !param.expectSuccess {
+					// An error is expected so test result is successful.
+					return
+				} else if err != nil && param.expectSuccess {
+					s.Fatal("PacketCaptureStart DBus call failed to start packet capture process: ", err)
 				}
 
-				// Stop packet capture.
-				if err := dbg.PacketCaptureStop(ctx, handle); err != nil {
-					s.Error("PacketCaptureStop DBus call failed: ", err)
-				}
-			}()
-
-			if err == nil && !param.expectSuccess {
-				// If there is no error returned when failure is expected, check the
-				// status output of the call. dbugd's packet capture tool runs an
-				// executable for packet capture operation and if there's an error
-				// during execution, it doesn't return error but it writes on status
-				// pipe. Status output will be non-empty in case of a failure.
-				writePipe.Close()
-				readPipe.SetDeadline(time.Now().Add(15 * time.Second))
-
-				testing.ContextLog(ctx, "Reading error from packet capture")
-				var buf bytes.Buffer
-				if _, err := io.Copy(&buf, readPipe); err != nil {
-					s.Fatal("Failed to read error: ", err)
-				}
-				if buf.String() == "" {
-					s.Error("PacketCaptureStart succeeded when it's expected to fail")
-				}
-				return
-			} else if err != nil && !param.expectSuccess {
-				// An error is expected so test result is successful.
-				return
-			} else if err != nil && param.expectSuccess {
-				s.Fatal("PacketCaptureStart DBus call failed to start packet capture process: ", err)
+				testCaptures[i] = packetCaptureDetails{handle: handle, outputFile: of.Name(), isCapturing: true}
 			}
 
 			// Notification ID of the packet capture notification. It is hard-coded in Chrome as
 			// DebugdNotificationHandler::kPacketCaptureNotificationId.
 			notificationID := "debugd-packetcapture"
 
-			// A notification must be shown after packet capture starts successfully.
+			// A notification must be shown after packet captures starts successfully.
 			s.Log("Checking if packet capture notification is visible")
 			if _, err = ash.WaitForNotification(ctx, tconn, 5*time.Second, ash.WaitIDContains(notificationID)); err != nil {
 				s.Error("Packet capture notification is not visible")
@@ -212,32 +262,42 @@ func PacketCapture(ctx context.Context, s *testing.State) {
 			sendNetworkDataToLocalhost(param.captureSizeMiBs, s)
 
 			// Stop packet capture.
-			if err = dbg.PacketCaptureStop(ctx, handle); err != nil {
-				s.Error("PacketCaptureStop DBus call failed: ", err)
-			} else {
-				isCapturing = false
+			if param.shouldTerminate {
+				for i := 0; i < param.numberOfCaptures; i++ {
+					// The notification should stay visible even after stopping some of the packet captures.
+					if _, err = ash.WaitForNotification(ctx, tconn, 5*time.Second, ash.WaitIDContains(notificationID)); err != nil {
+						s.Error("Packet capture notification is not visible")
+					}
+					if err = dbg.PacketCaptureStop(ctx, testCaptures[i].handle); err != nil {
+						s.Error("PacketCaptureStop DBus call failed: ", err)
+					} else {
+						testCaptures[i].isCapturing = false
+					}
+				}
 			}
 
-			// Notification must be gone after the packet capture is stopped.
+			// Notification must be gone after all packet captures are stopped.
 			s.Log("Checking if packet capture notification is gone")
 			if ash.WaitUntilNotificationGone(ctx, tconn, 5*time.Second, ash.WaitIDContains(notificationID)) != nil {
 				s.Error("Notification isn't gone after stopping packet capture")
 			}
 
-			// Check the output file size.
-			fi, err := os.Stat(of.Name())
-			if err != nil {
-				s.Fatal("Can't get output file status information: ", err)
-			}
-			fs := fi.Size()
-			if fs == 0 {
-				s.Error("Output file is empty. Couldn't capture any packets")
-				return
-			}
-			// Convert the file size into MiBs.
-			fs = fs / (1024 * 1024)
-			if param.maxFileSizeMiBs != 0 && fs > param.maxFileSizeMiBs {
-				s.Error("Output file exceeded size limit")
+			// Check output file sizes.
+			for i := 0; i < param.numberOfCaptures; i++ {
+				fi, err := os.Stat(testCaptures[i].outputFile)
+				if err != nil {
+					s.Fatal("Can't get output file status information: ", err)
+				}
+				fs := fi.Size()
+				if fs == 0 {
+					s.Error("Output file is empty. Couldn't capture any packets")
+					return
+				}
+				// Convert the file size into MiBs.
+				fs = fs / (1024 * 1024)
+				if param.maxFileSizeMiBs != 0 && fs > param.maxFileSizeMiBs {
+					s.Error("Output file exceeded size limit")
+				}
 			}
 		})
 	}
