cee46bdb47287a5e83337dc038751d7a5a3de976
Steffen Seckler <sseckler@google.com>
2022-01-27 15:35:56 +0000

    Tast test for OnFileDownloadedEnterpriseConnector
    Adds tast test for OnFileDownloadedEnterpriseConnector.
    Checks download of:
    - encrypted files
    - malicious files
    - (too large files) -- currently commented out, as download is not so
      fast.
    
    Checks for:
    - blocked/allowed
    
    TODO:
    - Include test for Lacros
    - Check dialogues
    
    BUG=chromium:1291538
    TEST=Ran new test using DUT.
    
    Change-Id: Ifad64b9e90f831bd60392d0af3db2ecd1b60f154

---
+++ src/chromiumos/tast/local/bundles/cros/enterpriseconnectors/test_download.go
@@ -0,0 +1,234 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package enterpriseconnectors
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/testing"
+)
+
+type userParams struct {
+	username                string // username for gaia login.
+	password                string // password for gaia login.
+	allowsImmediateDelivery bool   // specifies whether immediate delivery of files is allowed
+	allowsUnscannableFiles  bool   // specifies whether unscannable files (large or encrypted) are allowed
+	scansEnabled            bool   // specifies whether malware and dlp scans are enabled
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TestDownload,
+		LacrosStatus: testing.LacrosVariantUnknown,
+		Desc:         "Enterprise connector test",
+		Timeout:      10 * time.Minute,
+		Contacts: []string{
+			"sseckler@google.com",
+		},
+		SoftwareDeps: []string{
+			"chrome",
+			"chrome_internal",
+		},
+		Attr: []string{
+			"group:mainline",
+			"informational",
+		},
+		VarDeps: []string{
+			"enterpriseconnectors.username1",
+			"enterpriseconnectors.password1",
+			"enterpriseconnectors.username2",
+			"enterpriseconnectors.password2",
+			"enterpriseconnectors.username3",
+			"enterpriseconnectors.password3",
+			"enterpriseconnectors.username4",
+			"enterpriseconnectors.password4",
+		},
+		Params: []testing.Param{
+			{
+				Name: "scan_enabled_allows_immediate_and_unscannable",
+				Val: userParams{
+					username:                "enterpriseconnectors.username1",
+					password:                "enterpriseconnectors.password1",
+					allowsImmediateDelivery: true,
+					allowsUnscannableFiles:  true,
+					scansEnabled:            true,
+				},
+			},
+			{
+				Name: "scan_enabled_blocks_immediate_and_unscannable",
+				Val: userParams{
+					username:                "enterpriseconnectors.username2",
+					password:                "enterpriseconnectors.password2",
+					allowsImmediateDelivery: false,
+					allowsUnscannableFiles:  false,
+					scansEnabled:            true,
+				},
+			},
+			{
+				Name: "scan_disabled_allows_immediate_and_unscannable",
+				Val: userParams{
+					username:                "enterpriseconnectors.username3",
+					password:                "enterpriseconnectors.password3",
+					allowsImmediateDelivery: true,
+					allowsUnscannableFiles:  true,
+					scansEnabled:            false,
+				},
+			},
+			{
+				Name: "scan_disabled_blocks_immediate_and_unscannable",
+				Val: userParams{
+					username:                "enterpriseconnectors.username4",
+					password:                "enterpriseconnectors.password4",
+					allowsImmediateDelivery: false,
+					allowsUnscannableFiles:  false,
+					scansEnabled:            false,
+				},
+			},
+		},
+	})
+}
+
+func TestDownload(ctx context.Context, s *testing.State) {
+
+	userParams := s.Param().(userParams)
+	username := s.RequiredVar(userParams.username)
+	password := s.RequiredVar(userParams.password)
+
+	URL := "https://bce-testingsite.appspot.com/"
+
+	// Start up Chrome.
+	cr, err := chrome.New(
+		ctx,
+		chrome.GAIALogin(chrome.Creds{User: username, Pass: password}),
+		chrome.ProdPolicy(),
+	)
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	// Clear Downloads directory.
+	files, err := ioutil.ReadDir(filesapp.DownloadPath)
+	if err != nil {
+		s.Fatal("Failed to get files from Downloads directory")
+	}
+	for _, file := range files {
+		if err = os.RemoveAll(filepath.Join(filesapp.DownloadPath, file.Name())); err != nil {
+			s.Fatal("Failed to remove file: ", file.Name())
+		}
+	}
+
+	// Create Browser
+	br := cr.Browser()
+
+	dconn, err := br.NewConn(ctx, "")
+	if err != nil {
+		s.Fatal("Failed to connect to chrome: ", err)
+	}
+	defer dconn.Close()
+
+	// Needed to allow loading of enterprise connectors and reduce flakiness of tests.
+	testing.Sleep(ctx, 15*time.Second)
+
+	for _, param := range []struct {
+		testName         string
+		dlFileName       string
+		dlLinkIdentifier string
+		dlIsBad          bool
+		dlIsUnscannable  bool
+	}{
+		{
+			testName:         "Encrypted malware",
+			dlFileName:       "unknown_malware_encrypted.zip",
+			dlLinkIdentifier: "Encrypted file",
+			dlIsBad:          true,
+			dlIsUnscannable:  true,
+		},
+		/*{
+			test_name:          "Large file",
+			dl_file_name:       "100MB.bin",
+			dl_link_identifier: "Large file",
+			dl_is_bad:          false,
+			dl_is_unscannable:  true,
+		},*/
+		{
+			testName:         "Unknown malware",
+			dlFileName:       "unknown_malware.zip",
+			dlLinkIdentifier: "Unknown malware",
+			dlIsBad:          true,
+			dlIsUnscannable:  false,
+		},
+		{
+			testName:         "Known malware",
+			dlFileName:       "content.exe",
+			dlLinkIdentifier: "Known malware",
+			dlIsBad:          true,
+			dlIsUnscannable:  false,
+		},
+	} {
+		s.Run(ctx, param.testName, func(ctx context.Context, s *testing.State) {
+			dlFileName := param.dlFileName
+
+			shouldBlockDownload := false
+			if userParams.scansEnabled {
+				if param.dlIsUnscannable {
+					shouldBlockDownload = !userParams.allowsUnscannableFiles
+				} else {
+					shouldBlockDownload = param.dlIsBad
+				}
+			}
+
+			dconn, err := br.NewConn(ctx, URL)
+			if err != nil {
+				s.Fatal("Failed to connect to chrome: ", err)
+			}
+			defer dconn.Close()
+
+			//err = dconn.Eval(ctx, `document.getElementById(`+param.dl_link_id+`).click()`, nil)
+			err = dconn.Eval(ctx, `Array.from(document.getElementsByTagName("a")).filter(elem => elem.innerHTML.includes("`+param.dlLinkIdentifier+`"))[0].click()`, nil)
+			if err != nil {
+				s.Fatal("Failed to execute JS expression: ", err)
+			}
+
+			tconn, err := cr.TestAPIConn(ctx)
+			if err != nil {
+				s.Fatal("Failed to create Test API connection: ", err)
+			}
+
+			files, err := filesapp.Launch(ctx, tconn)
+			if err != nil {
+				s.Fatal("Launching the Files App failed: ", err)
+			}
+			defer files.Close(ctx)
+
+			if err := files.OpenDownloads()(ctx); err != nil {
+				s.Fatal("Opening Downloads folder failed: ", err)
+			}
+			if err := files.WithTimeout(10 * time.Second).WaitForFile(dlFileName)(ctx); err != nil {
+				if !shouldBlockDownload {
+					if errors.Is(err, context.DeadlineExceeded) {
+						s.Error("Download was blocked: ", err)
+					} else {
+						s.Fatal("Failed to wait for ", dlFileName, ": ", err)
+					}
+				}
+			} else {
+				if shouldBlockDownload {
+					s.Error("Download was not blocked")
+				}
+				if err := os.Remove(filesapp.DownloadPath + dlFileName); err != nil {
+					s.Error("Failed to remove ", dlFileName, ": ", err)
+				}
+			}
+		})
+	}
+}
