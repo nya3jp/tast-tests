95dbce7f59861ca9d9ab5ef13fa1ae04d31b7a8f
Sonja Laurila <laurila@google.com>
2022-01-27 15:09:58 +0100

    policy: Add tast test for AutofillCreditCardEnabled
    BUG=chromium:1288277
    TEST=tast run <ip> policy.AutofillCreditCardEnabled
    
    Change-Id: I77ac7ad1fb90cf773c02debdad757184cc9d0d9d

---
+++ src/chromiumos/tast/local/bundles/cros/policy/autofill_credit_card_enabled.go
@@ -0,0 +1,120 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/uiauto/checked"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/restriction"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AutofillCreditCardEnabled,
+		LacrosStatus: testing.LacrosVariantUnknown,
+		Desc:         "Behavior of AutofillCreditCardEnabled policy, checking the correspoding toggle button states (restriction and checked) after setting the policy",
+		Contacts: []string{
+			"laurila@google.com", // Test author
+			"chromeos-commercial-remote-management@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Fixture: fixture.ChromePolicyLoggedIn,
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val:               browser.TypeLacros,
+		}},
+	})
+}
+
+func AutofillCreditCardEnabled(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	// Reserve 10 seconds for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	for _, param := range []struct {
+		name            string
+		wantRestriction restriction.Restriction           // wantRestriction is the wanted restriction state of the toggle button for the "Save and fill payment methods" option.
+		wantChecked     checked.Checked                   // wantChecked is the wanted checked state of the toggle button for the "Save and fill payment methods" option.
+		policy          *policy.AutofillCreditCardEnabled // policy is the value of the policy.
+	}{
+		{
+			name:            "unset",
+			wantRestriction: restriction.None,
+			wantChecked:     checked.True,
+			policy:          &policy.AutofillCreditCardEnabled{Stat: policy.StatusUnset},
+		},
+		{
+			name:            "allow",
+			wantRestriction: restriction.None,
+			wantChecked:     checked.True,
+			policy:          &policy.AutofillCreditCardEnabled{Val: true},
+		},
+		{
+			name:            "deny",
+			wantRestriction: restriction.Disabled,
+			wantChecked:     checked.False,
+			policy:          &policy.AutofillCreditCardEnabled{Val: false},
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name+".txt")
+
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.policy}); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
+			// Setup browser based on the chrome type.
+			br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(browser.Type))
+			if err != nil {
+				s.Fatal("Failed to open the browser: ", err)
+			}
+			defer closeBrowser(cleanupCtx)
+
+			if err := policyutil.SettingsPage(ctx, cr, br, "payments").
+				SelectNode(ctx, nodewith.
+					NameStartingWith("Save and fill payment").
+					Role(role.ToggleButton)).
+				Restriction(param.wantRestriction).
+				Checked(param.wantChecked).
+				Verify(); err != nil {
+				s.Error("Unexpected settings state: ", err)
+			}
+		})
+	}
+}
