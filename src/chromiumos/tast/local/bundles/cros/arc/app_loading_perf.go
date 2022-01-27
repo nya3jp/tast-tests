a6ef8d5e15756b70d2ece18a1fb2dbf74f0006d2
Alan Ding <alanding@google.com>
2022-01-26 23:46:54 -0800

    tast-tests: Fix timeout exceeded for arc.AppLoadingPerf.vm on ARM
    ARM ARCVM boards are taking longer on the network performance subflow
    and failing early due to context deadline exceeded. This will prevent
    data collection on ARM boards..
    
    Updated per subflow timeout and verified on kukui-arc-r.
    
    BUG=b:216585645
    TEST=tast run $DUT arc.AppLoadingPerf.vm
    
    Change-Id: Ica53df346da141213af6cfd7ac5f51e981a059ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3418844
    Reviewed-by: Alvin Jia <alvinjia@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Tested-by: Alan Ding <alanding@chromium.org>
    Commit-Queue: Alan Ding <alanding@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/app_loading_perf.go
+++ src/chromiumos/tast/local/bundles/cros/arc/app_loading_perf.go
@@ -332,10 +332,10 @@ func calcGeometricMean(scores []float64) (float64, error) {
 
 // runAppLoadingTest will test each app loading subflow with timeout.
 func runAppLoadingTest(ctx context.Context, config apploading.TestConfig, a *arc.ARC, cr *chrome.Chrome) (float64, error) {
-	shorterCtx, cancel := context.WithTimeout(ctx, 390*time.Second)
+	shorterCtx, cancel := context.WithTimeout(ctx, 510*time.Second)
 	defer cancel()
 
-	// Each subflow should take no longer than 6.5 minutes based on stainless
+	// Each subflow should take no longer than 8.5 minutes based on stainless
 	// data. If it takes longer, very likely the app is stuck (e.g. b/169367367).
 	return apploading.RunTest(shorterCtx, config, a, cr)
 }
