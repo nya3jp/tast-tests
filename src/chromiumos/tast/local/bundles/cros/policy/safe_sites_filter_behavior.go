6faa64e2b3b876972098305fa645c7e27651285b
Jeroen Dhollander <jeroendh@google.com>
2022-01-28 14:09:46 +0100

    policy: Add SafeSitesFilterBehavior test
    BUG=chromium:1288353
    TEST=tast run <ip> policy.SafeSitesFilterBehavior
    Change-Id: I89179b020262223d4c57803629dc45c89690819d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3423958
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Tested-by: Jeroen Dhollander <jeroendh@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/policy/safe_sites_filter_behavior.go
@@ -0,0 +1,116 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+const unsafeSite = "https://porn.com"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SafeSitesFilterBehavior,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Check that the SafeSitesFilterBehavior policy can block/allow unsafe sites",
+		Contacts: []string{
+			"jeroendh@google.com", // Test author
+			"chromeos-commercial-remote-management@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Fixture: fixture.ChromePolicyLoggedIn,
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			ExtraAttr:         []string{"informational"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val:               browser.TypeLacros,
+		}},
+	})
+}
+
+func SafeSitesFilterBehavior(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	// Reserve ten seconds for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	for _, tc := range []struct {
+		name            string
+		value           *policy.SafeSitesFilterBehavior
+		shouldBeBlocked bool
+	}{
+		{
+			name:            "enabled",
+			value:           &policy.SafeSitesFilterBehavior{Val: 1},
+			shouldBeBlocked: true,
+		},
+		{
+			name:            "disabled",
+			value:           &policy.SafeSitesFilterBehavior{Val: 0},
+			shouldBeBlocked: false,
+		},
+		{
+			name:            "unset",
+			value:           &policy.SafeSitesFilterBehavior{Stat: policy.StatusUnset},
+			shouldBeBlocked: false,
+		},
+	} {
+		s.Run(ctx, tc.name, func(ctx context.Context, s *testing.State) {
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to reset Chrome: ", err)
+			}
+
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{tc.value}); err != nil {
+				s.Fatal("Failed to serve and verify policies: ", err)
+			}
+
+			br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(browser.Type))
+			if err != nil {
+				s.Fatal("Failed to setup chrome: ", err)
+			}
+			defer closeBrowser(cleanupCtx)
+
+			conn, err := br.NewConn(ctx, unsafeSite)
+			if err != nil {
+				s.Fatal("Failed to connect to Chrome: ", err)
+			}
+			defer conn.Close()
+
+			content, err := conn.PageContent(ctx)
+			if err != nil {
+				s.Fatal("Failed to access the page content: ", err)
+			}
+
+			isBlocked := strings.Contains(content, "ERR_BLOCKED_BY_ADMINISTRATOR")
+
+			if isBlocked != tc.shouldBeBlocked {
+				if tc.shouldBeBlocked {
+					s.Error("Unsafe content should be blocked, but it is not blocked")
+				} else {
+					s.Error("Unsafe content should not be blocked, but it is blocked")
+				}
+			}
+		})
+	}
+}
