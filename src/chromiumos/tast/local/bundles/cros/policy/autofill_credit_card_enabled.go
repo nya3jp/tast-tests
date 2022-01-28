b5b32766574c85415cf86f2b8d14dd51c2d41c21
Sonja Laurila <laurila@google.com>
2022-01-28 17:19:08 +0100

    DO NOT LAND YET! policy: Complement the tast test for AutofillCreditCardEnabled
    BUG=chromium:1288277
    TEST=tast run <ip> policy.AutofillCreditCardEnabled
    
    Change-Id: I6fec88f7388c71a7636c3fe9e3c3d5c1e68da034

--- src/chromiumos/tast/local/bundles/cros/policy/autofill_credit_card_enabled.go
+++ src/chromiumos/tast/local/bundles/cros/policy/autofill_credit_card_enabled.go
@@ -14,14 +14,17 @@ import (
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/browser"
 	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/checked"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/restriction"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/testing"
 )
@@ -46,6 +49,7 @@ func init() {
 			Fixture:           fixture.LacrosPolicyLoggedIn,
 			Val:               browser.TypeLacros,
 		}},
+		Data: []string{"autofill_credit_card_enabled.html"},
 	})
 }
 
@@ -58,14 +62,19 @@ func AutofillCreditCardEnabled(ctx context.Context, s *testing.State) {
 	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
 	defer cancel()
 
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
 	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
 	defer server.Close()
 
 	for _, param := range []struct {
 		name            string
-		wantRestriction restriction.Restriction           // wantRestriction is the wanted restriction state of the toggle button for the "Save and fill payment methods" option.
-		wantChecked     checked.Checked                   // wantChecked is the wanted checked state of the toggle button for the "Save and fill payment methods" option.
-		policy          *policy.AutofillCreditCardEnabled // policy is the value of the policy.
+		wantRestriction restriction.Restriction
+		wantChecked     checked.Checked
+		policy          *policy.AutofillCreditCardEnabled
 	}{
 		{
 			name:            "unset",
@@ -114,6 +123,67 @@ func AutofillCreditCardEnabled(ctx context.Context, s *testing.State) {
 				Checked(param.wantChecked).
 				Verify(); err != nil {
 				s.Error("Unexpected settings state: ", err)
+			}
+
+			if param.wantChecked == checked.True {
+				ui := uiauto.New(tconn)
+
+				// Click the button to add a new credit card.
+				if err := ui.LeftClick(nodewith.Name("Add").Role(role.Button))(ctx); err != nil {
+					s.Fatal("Failed to click the Add button: ", err)
+				}
+
+				// Find the Save button node, meaning the form is open.
+				if err := ui.WaitUntilExists(nodewith.Name("Save").Role(role.Button))(ctx); err != nil {
+					s.Fatal("Failed to find the Save button: ", err)
+				}
+
+				kb, err := input.Keyboard(ctx)
+				if err != nil {
+					s.Fatal(errors.Wrap(err, "failed to get the keyboard"))
+				}
+				defer kb.Close()
+
+				// Fill in the credit card details and save.
+				if err := ui.LeftClick(nodewith.Role(role.TextField).Name("Card number"))(ctx); err != nil {
+					s.Fatal("Failed to click the Card number text field: ", err)
+				}
+				if err := kb.Type(ctx, "1234123412341234"); err != nil {
+					s.Fatal("Failed to type Card number text field: ", err)
+				}
+				if err := ui.LeftClick(nodewith.Role(role.TextField).Name("Name on card"))(ctx); err != nil {
+					s.Fatal("Failed to click the Name on card text field: ", err)
+				}
+				if err := kb.Type(ctx, "Test"); err != nil {
+					s.Fatal("Failed to type to the Name on card text field: ", err)
+				}
+				if err := ui.LeftClick(nodewith.Role(role.TextField).Name("Card nickname"))(ctx); err != nil {
+					s.Fatal("Failed to click the Card nickname text field: ", err)
+				}
+				if err := kb.Type(ctx, "Test"); err != nil {
+					s.Fatal("Failed to type Card nickname text field: ", err)
+				}
+				if err := ui.LeftClick(nodewith.Role(role.Button).Name("Save"))(ctx); err != nil {
+					s.Fatal("Failed to click the Save button: ", err)
+				}
+
+				// Open the website with the credit card form.
+				conn, err := br.NewConn(ctx, server.URL+"/"+"autofill_credit_card_enabled.html")
+				if err != nil {
+					s.Fatal("Failed to open website: ", err)
+				}
+				defer conn.Close()
+
+				if err := ui.WaitUntilExists(nodewith.Name("OK").Role(role.Button))(ctx); err != nil {
+					s.Fatal("Failed to find the ok button: ", err)
+				}
+
+				if err := ui.LeftClick(nodewith.Role(role.InlineTextBox).Name("Name on card"))(ctx); err != nil {
+					s.Fatal("Failed to click the Name on card text field: ", err)
+				}
+
+				// TODO(laurila): Blocked due to the insecure form warning until crbug.com/1292020 has landed.
+				// &policy.InsecureFormsWarningsEnabled{Val: false} also did not work to by-pass this.
 			}
 		})
 	}
