5715416e014e890d9a74285ee93cae04ffcae2c8
Sonja Laurila <laurila@google.com>
2022-01-28 17:19:08 +0100

    NOT READY! policy: Extend the tast test for AutofillCreditCardEnabled
    BUG=chromium:1288277
    TEST=tast run <ip> policy.AutofillCreditCardEnabled
    
    Change-Id: I6fec88f7388c71a7636c3fe9e3c3d5c1e68da034

--- src/chromiumos/tast/local/bundles/cros/policy/autofill_credit_card_enabled.go
+++ src/chromiumos/tast/local/bundles/cros/policy/autofill_credit_card_enabled.go
@@ -14,14 +14,17 @@ import (
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/browser"
 	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/checked"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/restriction"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/testing"
 )
@@ -46,6 +49,7 @@ func init() {
 			Fixture:           fixture.LacrosPolicyLoggedIn,
 			Val:               browser.TypeLacros,
 		}},
+		Data: []string{"autofill_credit_card_enabled.html"},
 	})
 }
 
@@ -58,14 +62,39 @@ func AutofillCreditCardEnabled(ctx context.Context, s *testing.State) {
 	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
 	defer cancel()
 
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
 	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
 	defer server.Close()
 
+	creditCardFields := []struct {
+		// The field's name on the Chrome's settings page.
+		fieldName string
+		// The value which is set on the settings page and which should have been filled into the html input field after the autofill has been triggered.
+		fieldValue string
+		// The field's corresponding id on autofill_credit_card_enabled.html
+		htmlFieldID string
+	}{
+		{
+			fieldName:   "Card number",
+			fieldValue:  "1234123412341234",
+			htmlFieldID: "cc-number",
+		},
+		{
+			fieldName:   "Name on card",
+			fieldValue:  "Tester",
+			htmlFieldID: "cc-name",
+		},
+	}
+
 	for _, param := range []struct {
 		name            string
-		wantRestriction restriction.Restriction           // wantRestriction is the wanted restriction state of the toggle button for the "Save and fill payment methods" option.
-		wantChecked     checked.Checked                   // wantChecked is the wanted checked state of the toggle button for the "Save and fill payment methods" option.
-		policy          *policy.AutofillCreditCardEnabled // policy is the value of the policy.
+		wantRestriction restriction.Restriction
+		wantChecked     checked.Checked
+		policy          *policy.AutofillCreditCardEnabled
 	}{
 		{
 			name:            "unset",
@@ -114,6 +143,80 @@ func AutofillCreditCardEnabled(ctx context.Context, s *testing.State) {
 				Checked(param.wantChecked).
 				Verify(); err != nil {
 				s.Error("Unexpected settings state: ", err)
+			}
+
+			if param.wantChecked == checked.True {
+				ui := uiauto.New(tconn)
+
+				// Click the button to add a new credit card.
+				if err := ui.LeftClick(nodewith.Name("Add").Role(role.Button))(ctx); err != nil {
+					s.Fatal("Failed to click the Add button: ", err)
+				}
+
+				// Find the Save button node, meaning the form is open.
+				if err := ui.WaitUntilExists(nodewith.Name("Save").Role(role.Button))(ctx); err != nil {
+					s.Fatal("Failed to find the Save button: ", err)
+				}
+
+				kb, err := input.Keyboard(ctx)
+				if err != nil {
+					s.Fatal(errors.Wrap(err, "failed to get the keyboard"))
+				}
+				defer kb.Close()
+
+				// Fill in the credit card details and click on the save button.
+				for _, creditCardField := range creditCardFields {
+					textField := nodewith.Role(role.TextField).Name(creditCardField.fieldName)
+					if err := ui.MakeVisible(textField)(ctx); err != nil {
+						s.Fatal("Failed to make the text field visible: ", err)
+					}
+					if err := ui.LeftClick(textField)(ctx); err != nil {
+						s.Fatal("Failed to click the text field: ", err)
+					}
+					if err := kb.Type(ctx, creditCardField.fieldValue); err != nil {
+						s.Fatal("Failed to type to the text field: ", err)
+					}
+				}
+
+				if err := ui.LeftClick(nodewith.Role(role.Button).Name("Save"))(ctx); err != nil {
+					s.Fatal("Failed to click the Save button: ", err)
+				}
+
+				// Open the website with the credit card form.
+				conn, err := br.NewConn(ctx, server.URL+"/"+"autofill_credit_card_enabled.html")
+				if err != nil {
+					s.Fatal("Failed to open website: ", err)
+				}
+				defer conn.Close()
+
+				if err := ui.WaitUntilExists(nodewith.Name("OK").Role(role.Button))(ctx); err != nil {
+					s.Fatal("Failed to find the ok button: ", err)
+				}
+
+				// Trigger the autofill by clicking the Name on card field and choosing the suggested credit card (this could be any of the credit card fields).
+				if err := ui.LeftClick(nodewith.Role(role.InlineTextBox).Name("Name on card"))(ctx); err != nil {
+					s.Fatal("Failed to click the Name on card text field: ", err)
+				}
+
+				// TODO(laurila): Blocked due to the insecure form warning until crbug.com/1292020 has landed.
+				// &policy.InsecureFormsWarningsEnabled{Val: false} also did not work to by-pass this.
+
+				// The rows below this lines do not pass the tests yet (before HTTPS connection). Uncomment when ready.
+
+				// if err := ui.LeftClick(nodewith.Role(role.ListBoxOption).ClassName("AutofillPopupSuggestionView"))(ctx); err != nil {
+				// 	s.Fatal("Failed to click the AutofillPopupSuggestionView listBoxOption: ", err)
+				// }
+
+				// // Run JavaScript checks to confirm that all the fields are set correctly.
+				// for _, creditCardField := range creditCardFields {
+				// 	var valueFromHTML string
+				// 	if err := conn.Eval(ctx, "document.getElementById('"+creditCardField.htmlFieldID+"').value", &valueFromHTML); err != nil {
+				// 		s.Fatal("Failed to complete the JS test for htmlFieldID="+creditCardField.htmlFieldID, err)
+				// 	}
+				// 	if valueFromHTML != creditCardField.fieldValue {
+				// 		s.Fatal("Credit card field was not set properly. Actual value " + valueFromHTML + "+ does not match with expected " + creditCardField.fieldValue)
+				// 	}
+				// }
 			}
 		})
 	}
