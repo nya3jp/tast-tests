0882f5af77387a6c12e6b98200ccda87f41313cb
Simon Hangl <simonha@google.com>
2022-01-30 23:55:58 +0100

    Add CA certificate configuration helper.
    This CL adds a helper to add self-signed certificates
    to trusted authorities in chrome. This is required to
    access https sites hosted on localhost without chrome
    complaining about security. Test keys and certificates
    (CA, server) are added in the data folder.
    
    BUG=chromium:1292384
    TEST=fast_build.sh -T
    
    Change-Id: I9a937a5e48cf4e67ca9ce5d4b79b1630e4a54079

--- src/chromiumos/tast/local/bundles/cros/apps/isolatedapp/https.go
+++ src/chromiumos/tast/local/bundles/cros/apps/isolatedapp/https.go
@@ -5,8 +5,17 @@
 package isolatedapp
 
 import (
+	"context"
 	"io/ioutil"
 	"net/http"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/policyutil"
 )
 
 /*
@@ -17,9 +26,16 @@ type HTTPSServer struct {
 	ServerKeyPath         string
 	ServerCertificatePath string
 	HostedFilesBasePath   string
+	CaCertificatePath     string
 }
 
 var serverConfiguration HTTPSServer
+
+func copyFile(source, destination string) error {
+	bytesRead, err := ioutil.ReadFile(source)
+	ioutil.WriteFile(destination, bytesRead, 0644)
+	return err
+}
 
 /*
 StartServer starts up an https server without blocking.
@@ -38,5 +54,47 @@ func StartServer(configuration HTTPSServer) {
 	})
 
 	go http.ListenAndServeTLS(":443", serverConfiguration.ServerCertificatePath, serverConfiguration.ServerKeyPath, nil)
+
+}
+
+/*
+ConfigureChromeToAcceptCertificate adds the specified CA certificate to the authorities configuration of chrome.
+The server certificate will then be accepted by chrome without complaining about security.
+*/
+func ConfigureChromeToAcceptCertificate(ctx context.Context, configuration HTTPSServer, chrome *chrome.Chrome, targetBrowser *browser.Browser, testConnection *chrome.TestConn) error {
+
+	certificateDestination := filesapp.MyFilesPath + "/" + filesapp.Downloads + "/ca-cert.pem"
+	copyFile(configuration.CaCertificatePath, certificateDestination)
+	policyutil.SettingsPage(ctx, chrome, targetBrowser, "certificates")
+	ui := uiauto.New(testConnection)
+	authorities := nodewith.NameContaining("Authorities").Role(role.Tab)
+	importButton := nodewith.NameContaining("Import").Role(role.Button)
+	certFileItem := nodewith.NameContaining("ca-cert.pem").First()
+	openButton := nodewith.NameContaining("Open").Role(role.Button)
+	trust1Checkbox := nodewith.NameContaining("Trust this certificate for identifying websites").Role(role.CheckBox)
+	trust2Checkbox := nodewith.NameContaining("Trust this certificate for identifying email users").Role(role.CheckBox)
+	trust3Checkbox := nodewith.NameContaining("Trust this certificate for identifying software makers").Role(role.CheckBox)
+	okButton := nodewith.NameContaining("OK").Role(role.Button)
+	if err := uiauto.Combine("set_cerficiate",
+		ui.WaitUntilExists(authorities),
+		ui.LeftClick(authorities),
+		ui.WaitUntilExists(importButton),
+		ui.LeftClick(importButton),
+		ui.WaitUntilExists(certFileItem),
+		ui.LeftClick(certFileItem),
+		ui.WaitUntilExists(openButton),
+		ui.LeftClick(openButton),
+		ui.WaitUntilExists(trust1Checkbox),
+		ui.WaitUntilExists(trust2Checkbox),
+		ui.WaitUntilExists(trust3Checkbox),
+		ui.WaitUntilExists(okButton),
+		ui.LeftClick(trust1Checkbox),
+		ui.LeftClick(trust2Checkbox),
+		ui.LeftClick(trust3Checkbox),
+		ui.LeftClick(okButton),
+	)(ctx); err != nil {
+		return err
+	}
+	return nil
 
 }
