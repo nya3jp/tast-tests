8349e81626ffb3d071fa24e79a2bc844e853b5c1
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-01-30 13:16:17 -0800

    tast-tests: browserutil package
    Generalize the util functions below to Browser, move to
    browserutil package for user by others:
    - CloseAboutBlank
    
    Add example.Browser to test it out.
    
    More functions could be generalized and moved in a follow-up CL
    - lacros.FindFirstBlankWindow
    - lacros.FindFirstNonBlankWindow
    - lacros.WaitForLacrosWindow
    
    See go/lacros-tast-api-comparison for the discussion.
    See go/lacros-tast-porting for the porting guidelines.
    
    BUG=chromium:1254152
    TEST=tast run example.Browser.lacros
    TEST=tast run example.Browser  # for ash-chrome
    
    Change-Id: I19dc10056b624d8b3c0896230b90055c1ae4339b

---
+++ src/chromiumos/tast/local/bundles/cros/example/browser.go
@@ -0,0 +1,84 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/browser/browserutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Browser,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Tests the Browser Tast library. See http://go/lacros-tast-porting for the guidelines on how to use",
+		Contacts:     []string{"lacros-tast@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Fixture: "chromeLoggedIn",
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			Fixture:           "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Val:               browser.TypeLacros,
+		}},
+	})
+}
+
+func Browser(ctx context.Context, s *testing.State) {
+	// Set a browser instance from the existing chrome session.
+	// It will open a new window for lacros-chrome, but not for ash-chrome.
+	bt := s.Param().(browser.Type)
+	br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), bt)
+	if err != nil {
+		s.Fatalf("Failed to set up a browser: %v, err: %v", bt, err)
+	}
+	defer closeBrowser(ctx)
+
+	// Open a few more blank tabs and windows.
+	const numNewWindows = 3
+	for i := 0; i < numNewWindows; i++ {
+		var opts []browser.CreateTargetOption
+		if i%2 == 0 {
+			opts = append(opts, browser.WithNewWindow())
+		}
+		if _, err := br.NewConn(ctx, chrome.BlankURL, opts...); err != nil {
+			s.Fatalf("Failed to open a window, browser: %v, err: %v", bt, err)
+		}
+		// TODO: Replace Sleep with a new util of waiting the browser window to be visible in a follow up.
+		testing.Sleep(ctx, time.Second)
+	}
+
+	// Close all the tabs and windows opened above.
+	for i := 0; i < numNewWindows; i++ {
+		err := browserutil.CloseAboutBlank(ctx, br, s.Param().(browser.Type))
+		if err != nil {
+			s.Fatalf("Failed to close about:blank, browser: %v, err: %v", bt, err)
+		}
+	}
+
+	// Ensure there is no window left open for ash-chrome, and still one open for lacros-chrome.
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to the test API connection: ", err)
+	}
+	windows, err := ash.GetAllWindows(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get all windows: ", err)
+	}
+	if (bt == browser.TypeLacros && len(windows) > 1) || (bt == browser.TypeAsh && len(windows) > 0) {
+		s.Fatalf("Failed to close all open windows, browser: %v, err: %v", bt, err)
+	}
+}
