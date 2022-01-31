c5307a7c99261a840127d23a7ab2bffe26430bbf
Muhamed Parić <muhamedp@google.com>
2022-01-31 18:41:20 +0100

    dlp: Add warning subtests to DLP screenshot test
    Adds two more subtests for warn level screenshot restrictions, for both
    accepting and canceling the dialog.
    
    BUG=chromium:1292639
    TEST=tast run -build=true localhost:9222
    dlp.DataLeakPreventionRulesListScreenshot.*
    
    Change-Id: I5c00fd5d3518cef3c1eddf626168ac36394027a3

--- src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_screenshot.go
+++ src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_screenshot.go
@@ -12,6 +12,8 @@ import (
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/dlp/restrictionlevel"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/browser"
@@ -23,11 +25,74 @@ import (
 	"chromiumos/tast/testing"
 )
 
+// Notification strings.
+const (
+	captureNotAllowedTitle = "Can't capture confidential content"
+	captureNotAllowedID    = "screen_capture_dlp_blocked"
+	captureTakenTitle      = "Screenshot taken"
+	captureTakenID         = "capture_mode_notification"
+)
+
+// A struct containing parameters for different screenshot tests.
+type screenshotTestParams struct {
+	name                  string
+	url                   string
+	restriction           restrictionlevel.RestrictionLevel
+	wantNotificationTitle string
+	wantNotificationID    string
+	policyDLP             []policy.Policy
+	browserType           browser.Type
+}
+
+// DLP policy that blocks taking a screenshot.
+var screenshotBlockPolicy = []policy.Policy{&policy.DataLeakPreventionRulesList{
+	Val: []*policy.DataLeakPreventionRulesListValue{
+		{
+			Name:        "Disable taking screenshots of confidential content",
+			Description: "User should not be able to take screenshots of confidential content",
+			Sources: &policy.DataLeakPreventionRulesListValueSources{
+				Urls: []string{
+					"example.com",
+				},
+			},
+			Restrictions: []*policy.DataLeakPreventionRulesListValueRestrictions{
+				{
+					Class: "SCREENSHOT",
+					Level: "BLOCK",
+				},
+			},
+		},
+	},
+},
+}
+
+// DLP policy that warns when taking a screenshot.
+var screenshotWarnPolicy = []policy.Policy{&policy.DataLeakPreventionRulesList{
+	Val: []*policy.DataLeakPreventionRulesListValue{
+		{
+			Name:        "Warn before taking a screenshot confidential content",
+			Description: "User should be warned before taking a screenshot of confidential content",
+			Sources: &policy.DataLeakPreventionRulesListValueSources{
+				Urls: []string{
+					"example.com",
+				},
+			},
+			Restrictions: []*policy.DataLeakPreventionRulesListValueRestrictions{
+				{
+					Class: "SCREENSHOT",
+					Level: "WARN",
+				},
+			},
+		},
+	},
+},
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         DataLeakPreventionRulesListScreenshot,
 		LacrosStatus: testing.LacrosVariantExists,
-		Desc:         "Test behavior of DataLeakPreventionRulesList policy with screenshot blocked restriction",
+		Desc:         "Test behavior of DataLeakPreventionRulesList policy with screenshot restrictions",
 		Contacts: []string{
 			"vishal38785@gmail.com", // Test author
 			"chromeos-dlp@google.com",
@@ -35,14 +100,107 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline", "informational"},
 		Params: []testing.Param{{
+			Name:    "ash_blocked",
 			Fixture: fixture.ChromePolicyLoggedIn,
-			Val:     browser.TypeAsh,
+			Val: screenshotTestParams{
+				name:                  "blocked",
+				url:                   "https://www.example.com/",
+				restriction:           restrictionlevel.Blocked,
+				wantNotificationTitle: captureNotAllowedTitle,
+				wantNotificationID:    captureNotAllowedID,
+				policyDLP:             screenshotBlockPolicy,
+				browserType:           browser.TypeAsh,
+			},
 		}, {
-			Name:              "lacros",
+			Name:    "ash_allowed",
+			Fixture: fixture.ChromePolicyLoggedIn,
+			Val: screenshotTestParams{
+				name:                  "allowed",
+				url:                   "https://www.chromium.com/",
+				restriction:           restrictionlevel.Allowed,
+				wantNotificationTitle: captureTakenTitle,
+				wantNotificationID:    captureTakenID,
+				policyDLP:             screenshotBlockPolicy,
+				browserType:           browser.TypeAsh,
+			},
+		}, {
+			Name:      "ash_warn_proceeded",
+			ExtraAttr: []string{"informational"},
+			Fixture:   fixture.ChromePolicyLoggedIn,
+			Val: screenshotTestParams{
+				name:                  "warn_proceded",
+				url:                   "https://www.example.com/",
+				restriction:           restrictionlevel.WarnProceeded,
+				wantNotificationTitle: captureTakenTitle,
+				wantNotificationID:    captureTakenID,
+				policyDLP:             screenshotWarnPolicy,
+				browserType:           browser.TypeAsh,
+			},
+		}, {
+			Name:      "ash_warn_cancelled",
+			ExtraAttr: []string{"informational"},
+			Fixture:   fixture.ChromePolicyLoggedIn,
+			Val: screenshotTestParams{
+				name:        "warn_cancelled",
+				url:         "https://www.example.com/",
+				restriction: restrictionlevel.WarnCancelled,
+				policyDLP:   screenshotWarnPolicy,
+				browserType: browser.TypeAsh,
+			},
+		}, {
+			Name:              "lacros_blocked",
 			ExtraAttr:         []string{"informational"},
 			ExtraSoftwareDeps: []string{"lacros"},
 			Fixture:           fixture.LacrosPolicyLoggedIn,
-			Val:               browser.TypeLacros,
+			Val: screenshotTestParams{
+				name:                  "blocked",
+				url:                   "https://www.example.com/",
+				restriction:           restrictionlevel.Blocked,
+				wantNotificationTitle: captureNotAllowedTitle,
+				wantNotificationID:    captureNotAllowedID,
+				policyDLP:             screenshotBlockPolicy,
+				browserType:           browser.TypeLacros,
+			},
+		}, {
+			Name:              "lacros_allowed",
+			ExtraAttr:         []string{"informational"},
+			ExtraSoftwareDeps: []string{"lacros"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val: screenshotTestParams{
+				name:                  "allowed",
+				url:                   "https://www.chromium.com/",
+				restriction:           restrictionlevel.Allowed,
+				wantNotificationTitle: captureTakenTitle,
+				wantNotificationID:    captureTakenID,
+				policyDLP:             screenshotBlockPolicy,
+				browserType:           browser.TypeLacros,
+			},
+		}, {
+			Name:              "lacros_warn_proceeded",
+			ExtraAttr:         []string{"informational"},
+			ExtraSoftwareDeps: []string{"lacros"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val: screenshotTestParams{
+				name:                  "warn_proceeded",
+				url:                   "https://www.example.com/",
+				restriction:           restrictionlevel.WarnProceeded,
+				wantNotificationTitle: captureTakenTitle,
+				wantNotificationID:    captureTakenID,
+				policyDLP:             screenshotWarnPolicy,
+				browserType:           browser.TypeLacros,
+			},
+		}, {
+			Name:              "lacros_warn_cancelled",
+			ExtraAttr:         []string{"informational"},
+			ExtraSoftwareDeps: []string{"lacros"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val: screenshotTestParams{
+				name:        "warn_cancelled",
+				url:         "https://www.example.com/",
+				restriction: restrictionlevel.WarnCancelled,
+				policyDLP:   screenshotWarnPolicy,
+				browserType: browser.TypeLacros,
+			},
 		}},
 	})
 }
@@ -54,37 +212,6 @@ func DataLeakPreventionRulesListScreenshot(ctx context.Context, s *testing.State
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
 	defer cancel()
-
-	// DLP policy with screenshots blocked restriction.
-	policyDLP := []policy.Policy{&policy.DataLeakPreventionRulesList{
-		Val: []*policy.DataLeakPreventionRulesListValue{
-			{
-				Name:        "Disable Screenshot in confidential content",
-				Description: "User should not be able to take screen of confidential content",
-				Sources: &policy.DataLeakPreventionRulesListValueSources{
-					Urls: []string{
-						"example.com",
-					},
-				},
-				Restrictions: []*policy.DataLeakPreventionRulesListValueRestrictions{
-					{
-						Class: "SCREENSHOT",
-						Level: "BLOCK",
-					},
-				},
-			},
-		},
-	},
-	}
-
-	// Update the policy blob.
-	pb := fakedms.NewPolicyBlob()
-	pb.AddPolicies(policyDLP)
-
-	// Update policy.
-	if err := policyutil.ServeBlobAndRefresh(ctx, fakeDMS, cr, pb); err != nil {
-		s.Fatal("Failed to serve and refresh: ", err)
-	}
 
 	// Connect to Test API.
 	tconn, err := cr.TestAPIConn(ctx)
@@ -98,72 +225,86 @@ func DataLeakPreventionRulesListScreenshot(ctx context.Context, s *testing.State
 	}
 	defer keyboard.Close()
 
-	// Notification strings.
-	const captureNotAllowed = "Can't capture confidential content"
-	const captureAllowed = "Screenshot taken"
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree_"+s.Param().(screenshotTestParams).name)
 
-	for _, param := range []struct {
-		name                  string
-		wantNotificationTitle string
-		wantNotificationID    string
-		wantAllowed           bool
-		url                   string
-	}{
-		{
-			name:                  "example",
-			wantAllowed:           false,
-			wantNotificationTitle: captureNotAllowed,
-			wantNotificationID:    "screen_capture_dlp_blocked",
-			url:                   "https://www.example.com/",
-		},
-		{
-			name:                  "chromium",
-			wantAllowed:           true,
-			wantNotificationTitle: captureAllowed,
-			wantNotificationID:    "capture_mode_notification",
-			url:                   "https://www.chromium.org/",
-		},
-	} {
-		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
-			defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
+	// Update the policy blob.
+	pb := fakedms.NewPolicyBlob()
+	pb.AddPolicies(s.Param().(screenshotTestParams).policyDLP)
 
-			if err := ash.CloseNotifications(ctx, tconn); err != nil {
-				s.Fatal("Failed to close notifications: ", err)
-			}
-
-			// Clean up previous screenshots.
-			if err := screenshot.RemoveScreenshots(); err != nil {
-				s.Fatal("Failed to remove screenshots: ", err)
-			}
-
-			br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(browser.Type))
-			if err != nil {
-				s.Fatal("Failed to open the browser: ", err)
-			}
-			defer closeBrowser(cleanupCtx)
-
-			conn, err := br.NewConn(ctx, param.url)
-			if err != nil {
-				s.Error("Failed to open page: ", err)
-			}
-			defer conn.Close()
-
-			if err := keyboard.Accel(ctx, "Ctrl+F5"); err != nil {
-				s.Fatal("Failed to press Ctrl+F5 to take screenshot: ", err)
-			}
-
-			if _, err := ash.WaitForNotification(ctx, tconn, 15*time.Second, ash.WaitIDContains(param.wantNotificationID), ash.WaitTitle(param.wantNotificationTitle)); err != nil {
-				s.Fatalf("Failed to wait for notification with title %q: %v", param.wantNotificationTitle, err)
-			}
-
-			// Check if the screenshot is taken.
-			has, err := screenshot.HasScreenshots()
-			if err != nil {
-				s.Fatal("Failed to check whether screenshot is present: ", err)
-			}
-			if has != param.wantAllowed {
-				s.Errorf("Unexpected screenshot allowed: get %v; want %v", has, param.wantAllowed)
-			}
-		})
+	// Update policy.
+	if err := policyutil.ServeBlobAndRefresh(ctx, fakeDMS, cr, pb); err != nil {
+		s.Fatal("Failed to serve and refresh: ", err)
 	}
+
+	// Close previous notifications.
+	if err := ash.CloseNotifications(ctx, tconn); err != nil {
+		s.Fatal("Failed to close notifications: ", err)
+	}
+
+	// Clean up previous screenshots.
+	if err := screenshot.RemoveScreenshots(); err != nil {
+		s.Fatal("Failed to remove screenshots: ", err)
+	}
+
+	br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(screenshotTestParams).browserType)
+	if err != nil {
+		s.Fatal("Failed to open the browser: ", err)
+	}
+	defer closeBrowser(cleanupCtx)
+
+	conn, err := br.NewConn(ctx, s.Param().(screenshotTestParams).url)
+	if err != nil {
+		s.Fatal("Failed to open page: ", err)
+	}
+	defer conn.Close()
+
+	if err := testScreenshot(ctx, tconn, keyboard, s.Param().(screenshotTestParams)); err != nil {
+		s.Fatal("Failed to run test body: ", err)
+	}
+}
+
+// testScreenshot attempts to take a screenshot, and returns an error if the behavior is different than expected.
+func testScreenshot(ctx context.Context, tconn *chrome.TestConn, keyboard *input.KeyboardEventWriter, params screenshotTestParams) error {
+	// Press Ctrl+F5 to take the screenshot.
+	if err := keyboard.Accel(ctx, "Ctrl+F5"); err != nil {
+		return errors.Wrap(err, "failed to type screenshot hotkey")
+	}
+
+	if params.restriction == restrictionlevel.WarnProceeded {
+		// Hit Enter, which is equivalent to clicking on the "Capture anyway" button.
+		if err := keyboard.Accel(ctx, "Enter"); err != nil {
+			return errors.Wrap(err, "failed to hit Enter")
+		}
+	}
+
+	if params.restriction == restrictionlevel.WarnCancelled {
+		// Hit Esc, which is equivalent to clicking on the "Cancel" button.
+		if err := keyboard.Accel(ctx, "Esc"); err != nil {
+			return errors.Wrap(err, "failed to hit Esc")
+		}
+
+		// Ensure that a screenshot was not taken.
+		if _, err := ash.WaitForNotification(ctx, tconn, 15*time.Second, ash.WaitIDContains(captureTakenID), ash.WaitTitle(captureTakenTitle)); err == nil {
+			return errors.Wrap(err, "screenshot taken notification appeared after user cancelled action")
+		}
+
+	} else {
+		if _, err := ash.WaitForNotification(ctx, tconn, 15*time.Second, ash.WaitIDContains(params.wantNotificationID), ash.WaitTitle(params.wantNotificationTitle)); err != nil {
+			return errors.Wrapf(err, "failed to wait for notification with title %q: %v", params.wantNotificationTitle, err)
+		}
+
+		// Check if a screenshot was taken.
+		takenScreenshot, err := screenshot.HasScreenshots()
+		if err != nil {
+			return errors.Wrap(err, "failed to check if a screenshot was taken")
+		}
+
+		wantScreenshotTaken := (params.restriction == restrictionlevel.Allowed) || (params.restriction == restrictionlevel.WarnProceeded)
+
+		if takenScreenshot != wantScreenshotTaken {
+			return errors.Wrapf(err, "unexpected screenshot allowed: got %v; want %v", takenScreenshot, wantScreenshotTaken)
+		}
+	}
+
+	return nil
 }
