d36c076d74854bab4c2a4ec7406268a9e1ed657d
Chad Duffin <chadduffin@chromium.org>
2022-01-31 19:01:40 +0000

    [TAST] Fix Quick Settings, Bluetooth tests.
    Update Bluetooth tests to explicitly disable the Bluetooth revamp
    feature flag to avoid issues with the flag becoming enabled by default.
    
    BUG=b:216199857,b:216303490,b:218882177
    TEST=Verified on-device.
    
    Change-Id: If715887f9bd262277ee4b153bb8fdbfb30594896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3444760
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Tested-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/bluetooth/ui_toggle_from_bt_quicksettings.go
+++ src/chromiumos/tast/local/bundles/cros/bluetooth/ui_toggle_from_bt_quicksettings.go
@@ -31,7 +31,7 @@ func init() {
 		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
-		Fixture:      "chromeLoggedIn",
+		Fixture:      "chromeLoggedInWithBluetoothRevampDisabled",
 	})
 }
 
@@ -50,9 +50,9 @@ func UIToggleFromBTQuicksettings(ctx context.Context, s *testing.State) {
 
 	// TODO(b/188767517): Add unique identifiers to UI elements used in these tests
 	// Bluetooth button in the quick setting menu, when Bluetooth is on.
-	bluetoothTurnOffButton := nodewith.NameContaining("Toggle Bluetooth. Bluetooth is on").ClassName("FeaturePodIconButton").Role(role.ToggleButton)
+	bluetoothTurnOffButton := nodewith.NameContaining("Toggle Bluetooth. Bluetooth is on").ClassName("IconButton").Role(role.ToggleButton)
 	// Bluetooth button in the quick setting menu, when Bluetooth is off.
-	bluetoothTurnOnButton := nodewith.NameContaining("Toggle Bluetooth. Bluetooth is off").ClassName("FeaturePodIconButton").Role(role.ToggleButton)
+	bluetoothTurnOnButton := nodewith.NameContaining("Toggle Bluetooth. Bluetooth is off").ClassName("IconButton").Role(role.ToggleButton)
 	// Bluetooth Quick Settings UI.
 	bluetoothSettings := nodewith.ClassName("BluetoothDetailedViewLegacy")
 	// Bluetooth button in the bluetooth quick setting menu, when Bluetooth is off.
