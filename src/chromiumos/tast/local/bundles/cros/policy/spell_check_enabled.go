65c65e7f37725eb0cd37624f0fd0a739713fefc4
Jit Yao Yap <jityao@google.com>
2022-02-01 10:19:44 +0100

    policy: Fix SpellCheckEnabled Lacros policy test for tablet devices
    BUG=chromium:1288370
    TEST=tast run <device> policy.SpellCheckEnabled
    
    Change-Id: I1342d094f222f277da8e4985972bf2862e6d699a

--- src/chromiumos/tast/local/bundles/cros/policy/spell_check_enabled.go
+++ src/chromiumos/tast/local/bundles/cros/policy/spell_check_enabled.go
@@ -6,7 +6,6 @@ package policy
 
 import (
 	"context"
-	"encoding/json"
 	"time"
 
 	"chromiumos/tast/common/fixture"
@@ -59,28 +58,32 @@ func SpellCheckEnabled(ctx context.Context, s *testing.State) {
 	}
 
 	for _, param := range []struct {
-		name            string
-		value           *policy.SpellcheckEnabled
-		wantRestriction bool
-		wantChecked     bool
+		name                  string
+		value                 *policy.SpellcheckEnabled
+		wantRestriction       restriction.Restriction
+		wantChecked           checked.Checked
+		wantSpellCheckEnabled bool
 	}{
 		{
-			name:            "enabled",
-			value:           &policy.SpellcheckEnabled{Val: true},
-			wantRestriction: true,
-			wantChecked:     true,
+			name:                  "enabled",
+			value:                 &policy.SpellcheckEnabled{Val: true},
+			wantRestriction:       restriction.Disabled,
+			wantChecked:           checked.True,
+			wantSpellCheckEnabled: true,
 		},
 		{
-			name:            "disabled",
-			value:           &policy.SpellcheckEnabled{Val: false},
-			wantRestriction: true,
-			wantChecked:     false,
+			name:                  "disabled",
+			value:                 &policy.SpellcheckEnabled{Val: false},
+			wantRestriction:       restriction.Disabled,
+			wantChecked:           checked.False,
+			wantSpellCheckEnabled: false,
 		},
 		{
-			name:            "unset",
-			value:           &policy.SpellcheckEnabled{Stat: policy.StatusUnset},
-			wantRestriction: false,
-			wantChecked:     true,
+			name:                  "unset",
+			value:                 &policy.SpellcheckEnabled{Stat: policy.StatusUnset},
+			wantRestriction:       restriction.None,
+			wantChecked:           checked.True,
+			wantSpellCheckEnabled: true,
 		},
 	} {
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
@@ -98,68 +101,46 @@ func SpellCheckEnabled(ctx context.Context, s *testing.State) {
 
 			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
 
-			conn, err := br.NewConn(ctx, "")
+			if err := policyutil.SettingsPage(ctx, cr, br, "languages").
+				SelectNode(ctx, nodewith.
+					Name("Spell check").
+					Role(role.ToggleButton)).
+				Restriction(param.wantRestriction).
+				Checked(param.wantChecked).
+				Verify(); err != nil {
+				s.Fatal("Unexpected toggle button state: ", err)
+			}
+
+			hasSpellCheck, err := spellCheckEnabled(ctx, param.name, tconn, br)
 			if err != nil {
-				s.Fatal("Failed to connect to chrome: ", err)
+				s.Fatal("Failed to check spell check status: ", err)
 			}
 
-			// Open Language settings page.
-			if err := conn.Navigate(ctx, "chrome://settings/languages"); err != nil {
-				s.Fatal("Failed to open Language settings page: ", err)
-			}
-			defer conn.Close()
-
-			// Create a uiauto.Context with default timeout.
-			ui := uiauto.New(tconn)
-
-			spellCheckFinder := nodewith.Name("Spell check").Role(role.ToggleButton)
-			if err := ui.WithTimeout(5 * time.Second).WaitUntilExists(spellCheckFinder)(ctx); err != nil {
-				s.Fatal("Failed to find Spell check toggle: ", err)
-			}
-
-			var info *uiauto.NodeInfo
-			if info, err = ui.WithTimeout(5*time.Second).Info(ctx, spellCheckFinder); err != nil {
-				s.Fatal("Failed to get node info for Spell check toggle: ", err)
-			}
-
-			isChecked := info.Checked == checked.True
-			if param.wantChecked != isChecked {
-				b, _ := json.Marshal(info)
-				s.Fatalf("Unexpected toggle button checked state, got %s", b)
-			}
-
-			isRestricted := info.Restriction == restriction.Disabled
-			if param.wantRestriction != isRestricted {
-				b, _ := json.Marshal(info)
-				s.Fatalf("Unexpected toggle button restricted state, got %s", b)
-			}
-
-			// Spell check is enabled if policy is enabled or unset.
-			hasSpellCheck := spellCheckEnabled(ctx, param.name, s, tconn, conn, ui)
-
-			if param.wantChecked != hasSpellCheck {
+			if param.wantSpellCheckEnabled != hasSpellCheck {
 				s.Fatalf("Unexpected spellcheck enabled status, expected %v, got %v", param.wantChecked, hasSpellCheck)
 			}
 		})
 	}
 }
 
-func spellCheckEnabled(parent context.Context, name string, s *testing.State, tconn *browser.TestConn, conn *browser.Conn, ui *uiauto.Context) bool {
-	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+func spellCheckEnabled(ctx context.Context, name string, tconn *browser.TestConn, br *browser.Browser) (bool, error) {
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
 	defer cancel()
 
 	// Open a data URI of a page containing a textarea.
-	if err := conn.Navigate(ctx, "data:text/html, <html><body><textarea aria-label='textarea'/>"); err != nil {
-		s.Fatal("Failed to open page with textarea: ", err)
+	conn, err := br.NewConn(ctx, "data:text/html, <html><body><textarea aria-label='textarea'/>")
+	if err != nil {
+		return false, errors.Wrap(err, "failed to open page with textarea")
 	}
 	defer conn.Close()
 
+	ui := uiauto.New(tconn)
 	textArea := nodewith.Name("textarea")
 	if err := uiauto.Combine("Focus textarea",
 		ui.WaitUntilExists(textArea),
 		ui.LeftClick(textArea),
 	)(ctx); err != nil {
-		s.Fatal("Failed to focus text area: ", err)
+		return false, errors.Wrap(err, "failed to focus text area")
 	}
 
 	// Check if Spell Check is enabled by checking that the spelling marker is added after text is typed.
@@ -179,18 +160,18 @@ func spellCheckEnabled(parent context.Context, name string, s *testing.State, tc
 			chrome.automation.removeTreeChangeObserver(observer);
 		});
 	}`, name); err != nil {
-		s.Fatal("Could not observe spelling marker changes: ", err)
+		return false, errors.Wrap(err, "could not observe spelling marker changes")
 	}
 
 	kb, err := input.Keyboard(ctx)
 	if err != nil {
-		s.Fatal("Could not create input keyboard: ", err)
+		return false, errors.Wrap(err, "could not create input keyboard")
 	}
 	defer kb.Close()
 
 	// Type misspelled word into keyboard to generate a spelling marker.
 	if err := kb.Type(ctx, "aaaaa "); err != nil {
-		s.Fatal("Could not type on keyboard: ", err)
+		return false, errors.Wrap(err, "could not type on keyboard")
 	}
 
 	enabled := false
@@ -205,10 +186,10 @@ func spellCheckEnabled(parent context.Context, name string, s *testing.State, tc
 		return nil
 	}, &testing.PollOptions{Interval: 10 * time.Millisecond, Timeout: 1 * time.Second}); err != nil {
 		if errors.Is(err, falseErr) {
-			return false
+			return false, nil
 		}
-		s.Fatal("Could not evaluate spellCheckEnabled: ", err)
+		return false, errors.Wrap(err, "could not evaluate spellCheckEnabled")
 	}
 
-	return enabled
+	return enabled, nil
 }
