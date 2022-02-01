fe7b9ff18ee680e46d1a60ab69edf4a40c0ba02b
Simon Hangl <simonha@google.com>
2022-02-01 10:17:09 +0100

    tast-test for isolated app cross origin behavior.
    Add tast-test to make sure isolated apps cannot
    leave their origin within the original window.
    
    BUG=chromium:1292899
    TEST=tast run apps.Crossoriginforbidden
    
    Change-Id: I8f04f0d95f42e337e455cda1c083989c0f32cd73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3429239
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Tested-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Simon Hangl <simonha@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/apps/cross_origin_forbidden.go
@@ -0,0 +1,158 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package apps
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/apps/isolatedapp"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+const (
+	mainAppFile              = "cross_origin_forbidden_index.html"
+	serverKeyFile            = "cross_origin_forbidden_key.pem"
+	serverCertificateFile    = "cross_origin_forbidden_certificate.pem"
+	certificateAuthorityFile = "cross_origin_forbidden_ca_cert.pem"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CrossOriginForbidden,
+		LacrosStatus: testing.LacrosVariantUnknown,
+		Desc:         "Checks whether chrome refuses to follow cross origin links in isolated web apps",
+		Contacts: []string{
+			"simonha@google.com", // Test author
+			"chromeos-commercial-remote-management@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      chrome.GAIALoginTimeout + time.Minute,
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      fixture.ChromePolicyLoggedInIsolatedApp,
+		Data: []string{
+			mainAppFile,
+			serverKeyFile,
+			serverCertificateFile,
+			certificateAuthorityFile,
+			"cross_origin_forbidden.webmanifest",
+			"cross_origin_forbidden_icon-192x192.png",
+			"cross_origin_forbidden_icon-512x512.png",
+		},
+	})
+}
+
+func CrossOriginForbidden(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	// Open a keyboard device.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to open keyboard device: ", err)
+	}
+	defer kb.Close()
+
+	baseDirectory, fileName := filepath.Split(s.DataPath(mainAppFile))
+	httpsServerConfiguration := isolatedapp.HTTPSServerConfiguration{
+		Headers: map[string]string{
+			"Cross-Origin-Embedder-Policy": "require-corp",
+			"Cross-Origin-Opener-Policy":   "same-origin",
+		},
+		ServerKeyPath:         s.DataPath(serverKeyFile),
+		ServerCertificatePath: s.DataPath(serverCertificateFile),
+		HostedFilesBasePath:   baseDirectory,
+		CaCertificatePath:     s.DataPath(certificateAuthorityFile),
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), browser.TypeAsh)
+	if err != nil {
+		s.Fatal("Failed to open the browser: ", err)
+	}
+	defer closeBrowser(ctx)
+
+	isolatedapp.ConfigureChromeToAcceptCertificate(ctx, httpsServerConfiguration, cr, br, tconn)
+	server := isolatedapp.StartServer(httpsServerConfiguration)
+	if server.Error != nil {
+		s.Fatal("Could not start https server: ", err)
+	}
+	defer server.Close()
+
+	policies := []policy.Policy{
+		&policy.WebAppInstallForceList{
+			Val: []*policy.WebAppInstallForceListValue{
+				{
+					Url:                    server.Address + "/" + fileName,
+					DefaultLaunchContainer: "window",
+					CreateDesktopShortcut:  false,
+					CustomName:             "",
+					FallbackAppName:        "",
+					CustomIcon: &policy.WebAppInstallForceListValueCustomIcon{
+						Hash: "",
+						Url:  "",
+					},
+				},
+			},
+		},
+	}
+	if err := policyutil.ServeAndVerify(ctx, fdms, cr, policies); err != nil {
+		s.Fatal("Failed to update policies: ", err)
+	}
+
+	// Wait until the PWA is installed.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		const name = "CrossOriginForbidden"
+		if err := launcher.SearchAndLaunch(tconn, kb, name)(ctx); err != nil {
+			return errors.Wrapf(err, "failed to launch %s", name)
+		}
+
+		windows, err := ash.GetAllWindows(ctx, tconn)
+		if err != nil {
+			return testing.PollBreak(errors.Wrap(err, "failed to get windows"))
+		}
+
+		for _, window := range windows {
+			if window.Title == name {
+				return nil
+			}
+		}
+		return errors.New("failed to find a window with the PWA")
+	}, nil); err != nil {
+		s.Error("PWA wasn't installed: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+	navigateButton := nodewith.NameContaining("Navigate").Role(role.Link)
+	cannotOpenExpectedWindow := nodewith.NameContaining("Google Chrome OS can't open this page.").First()
+	if err := uiauto.Combine("cross_origin_link",
+		ui.WaitUntilExists(navigateButton),
+		ui.LeftClick(navigateButton),
+		// This is the expectation we are waiting for
+		ui.WaitUntilExists(cannotOpenExpectedWindow),
+	)(ctx); err != nil {
+		s.Fatal(errors.Wrap(err, "Cross origin link check failed"))
+	}
+
+}
