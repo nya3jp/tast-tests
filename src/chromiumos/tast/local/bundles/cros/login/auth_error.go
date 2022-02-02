165ba6cea4b4bef746fefef3dd2f310e7738e54b
Bohdan Tyshchenko <bohdanty@google.com>
2022-02-02 08:56:01 +0000

    oobe: Add tast test for learn more after wrong password check
    Automate workflow which checks existence of the help dialog after wrong
    password entered twice and that "Learn More" button inside it open a
    help window.
    
    BUG=b:200987434
    TEST=tast run ${DUT_IP} login.LearnMoreWrongPassword
    
    Change-Id: Ibd2ae9b51490962fa5229f2d5d7e01f27b04e7db

--- src/chromiumos/tast/local/bundles/cros/login/auth_error.go
+++ src/chromiumos/tast/local/bundles/cros/login/auth_error.go
@@ -13,6 +13,7 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/lockscreen"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
@@ -22,23 +23,30 @@ func init() {
 		Func:         AuthError,
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Tests that auth error disappears when user perform some action",
-		Contacts:     []string{"rsorokin@google.com", "chromeos-sw-engprod@google.com", "cros-oac@google.com"},
+		Contacts: []string{"rsorokin@google.com", "bohdanty@google.com",
+			"chromeos-sw-engprod@google.com", "cros-oac@google.com"},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline"},
 		VarDeps:      []string{"ui.signinProfileTestExtensionManifestKey"},
 		Timeout:      chrome.LoginTimeout + 30*time.Second,
+		Params: []testing.Param{{
+			Val:       false,
+			ExtraAttr: []string{"group:mainline"},
+		}, {
+			Name:      "learn_more_help_dialog",
+			Val:       true,
+			ExtraAttr: []string{"group:mainline", "informational"},
+		}},
 	})
 }
 
 func AuthError(ctx context.Context, s *testing.State) {
-	var creds chrome.Creds
-
 	// Create user on the device.
 	cr, err := chrome.New(ctx)
 	if err != nil {
 		s.Fatal("Chrome login failed: ", err)
 	}
-	creds = cr.Creds()
+	creds := cr.Creds()
 	cr.Close(ctx)
 
 	cr, err = chrome.New(ctx,
@@ -73,8 +81,8 @@ func AuthError(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to enter password: ", err)
 	}
 
-	ui := uiauto.New(tconn)
-	if err := ui.WithTimeout(10 * time.Second).WaitUntilExists(lockscreen.AuthErrorFinder)(ctx); err != nil {
+	ui := uiauto.New(tconn).WithTimeout(10 * time.Second)
+	if err := ui.WaitUntilExists(lockscreen.AuthErrorFinder)(ctx); err != nil {
 		s.Fatal("Failed to wait for auth error: ", err)
 	}
 
@@ -88,16 +96,36 @@ func AuthError(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to enter password: ", err)
 	}
 
-	if err := ui.WithTimeout(10 * time.Second).WaitUntilExists(lockscreen.ConsecutiveAuthErrorFinder)(ctx); err != nil {
+	if err := ui.WaitUntilExists(lockscreen.ConsecutiveAuthErrorFinder)(ctx); err != nil {
 		s.Fatal("Failed to wait for consecutive auth error: ", err)
 	}
 
-	// Typing password should make the auth error gone
-	if err := lockscreen.TypePassword(ctx, tconn, creds.User, creds.Pass, keyboard); err != nil {
-		s.Fatal("Failed to type password: ", err)
-	}
+	checkLearnMoreDialog := s.Param().(bool)
 
-	if err := ui.WithTimeout(10 * time.Second).WaitUntilGone(lockscreen.ConsecutiveAuthErrorFinder)(ctx); err != nil {
-		s.Fatal("Failed to wait for auth error gone: ", err)
+	if !checkLearnMoreDialog {
+		// Typing password should make the auth error gone
+		if err := lockscreen.TypePassword(ctx, tconn, creds.User, creds.Pass, keyboard); err != nil {
+			s.Fatal("Failed to type password: ", err)
+		}
+
+		if err := ui.WaitUntilGone(lockscreen.ConsecutiveAuthErrorFinder)(ctx); err != nil {
+			s.Fatal("Failed to wait for auth error gone: ", err)
+		}
+	} else {
+		if err := ui.LeftClick(nodewith.Role(role.Button).NameStartingWith("Learn more"))(ctx); err != nil {
+			s.Fatal("Failed to click Learn more button")
+		}
+
+		if err := ui.WaitUntilExists(nodewith.Role(role.Window).NameStartingWith("Help"))(ctx); err != nil {
+			s.Fatal("Failed to wait for help diaglog")
+		}
+
+		if err := ui.LeftClick(nodewith.Role(role.Button).NameStartingWith("Close"))(ctx); err != nil {
+			s.Fatal("Failed to close help dialog")
+		}
+
+		if err := ui.WaitUntilGone(nodewith.Role(role.Window).NameStartingWith("Help"))(ctx); err != nil {
+			s.Fatal("Failed to wait for help dialog to close")
+		}
 	}
 }
