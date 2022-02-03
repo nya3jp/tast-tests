584c750dd3d4f585e988ceda2ce89f19c45bb75e
Anastasiia N <anastasiian@google.com>
2022-02-03 09:13:42 +0100

    tast-tests: Add a test for cryptohome recovery
    Add a test that uses cryptohome-test-tool command line tool to execute
    the crytohome recovery flow with fake/local mediation.
    
    BUG=b:213304135
    TEST=tast run <ip> cryptohome.Recovery
    
    Change-Id: I5eaf41a78873f743c154eaacb8008131005dab69

---
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/recovery_crypto.go
@@ -0,0 +1,56 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cryptohome
+
+import (
+	"context"
+
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RecoveryCrypto,
+		Desc:         "Checks that cryptohome recovery process succeeds with fake/local mediation",
+		Contacts:     []string{"anastasiian@chromium.org", "cros-lurs@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"tpm"},
+	})
+}
+
+func RecoveryCrypto(ctx context.Context, s *testing.State) {
+	testTool, newErr := cryptohome.NewRecoveryTestTool()
+	if newErr != nil {
+		s.Fatal("Failed to initialize RecoveryTestTool", newErr)
+	}
+	defer removeDir(s, testTool)
+
+	if err := testTool.CreateHsmPayload(ctx); err != nil {
+		s.Fatal("Failed to execute CreateHsmPayload: ", err)
+	}
+
+	if err := testTool.CreateRecoveryRequest(ctx); err != nil {
+		s.Fatal("Failed to execute CreateRecoveryRequest: ", err)
+	}
+
+	if err := testTool.FakeMediate(ctx); err != nil {
+		s.Fatal("Failed to execute FakeMediate: ", err)
+	}
+
+	if err := testTool.Decrypt(ctx); err != nil {
+		s.Fatal("Failed to execute Decrypt: ", err)
+	}
+
+	if err := testTool.Validate(ctx); err != nil {
+		s.Fatal("Failed to validate: ", err)
+	}
+}
+
+func removeDir(s *testing.State, testTool *cryptohome.RecoveryTestTool) {
+	if err := testTool.RemoveDir(); err != nil {
+		s.Fatal("Failed to remove dir: ", err)
+	}
+}
