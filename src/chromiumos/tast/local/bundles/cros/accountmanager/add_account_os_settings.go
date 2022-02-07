f78c3d332a4c4865a6630fc25bb803d525ec5ee2
Anastasiia N <anastasiian@google.com>
2022-02-07 12:59:07 +0100

    tast-tests: Pass androidui.Device to IsAccountPresentInArc
    Pass androidui.Device so that we don't need to initialize the UI
    Automator on every call.
    
    BUG=chromium:1268351
    TEST=tast run <ip> accountmanager.*
    
    Change-Id: I16a9d34ce9402a8cd5c5321fa1458d5675f4ccdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3426182
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Tested-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/accountmanager/add_account_os_settings.go
+++ src/chromiumos/tast/local/bundles/cros/accountmanager/add_account_os_settings.go
@@ -89,6 +89,13 @@ func AddAccountOSSettings(ctx context.Context, s *testing.State) {
 
 	ui := uiauto.New(tconn).WithTimeout(time.Minute)
 	a := s.FixtValue().(accountmanager.FixtureData).ARC
+	defer a.DumpUIHierarchyOnError(ctx, s.OutDir(), s.HasError)
+
+	arcDevice, err := a.NewUIDevice(ctx)
+	if err != nil {
+		s.Fatal("Failed to initialize UI Automator: ", err)
+	}
+	defer arcDevice.Close(ctx)
 
 	// Open Account Manager page in OS Settings and find Add Google Account button.
 	addAccountButton := nodewith.Name("Add Google Account").Role(role.Button)
@@ -130,7 +137,7 @@ func AddAccountOSSettings(ctx context.Context, s *testing.State) {
 
 	// Check that account is present in ARC.
 	s.Log("Verifying that account is present in ARC")
-	if present, err := accountmanager.IsAccountPresentInArc(ctx, tconn, a, username); err != nil {
+	if present, err := accountmanager.IsAccountPresentInArc(ctx, tconn, arcDevice, username); err != nil {
 		s.Fatalf("Failed to check that account is present in ARC err=%v", err)
 	} else if !present {
 		s.Fatal("Account is not present in ARC")
@@ -164,7 +171,7 @@ func AddAccountOSSettings(ctx context.Context, s *testing.State) {
 
 	// Check that account is not present in ARC.
 	s.Log("Verifying that account is not present in ARC")
-	if present, err := accountmanager.IsAccountPresentInArc(ctx, tconn, a, username); err != nil {
+	if present, err := accountmanager.IsAccountPresentInArc(ctx, tconn, arcDevice, username); err != nil {
 		s.Fatalf("Failed to check that account is NOT present in ARC, err=%v", err)
 	} else if present {
 		s.Fatal("Account is still present in ARC")
