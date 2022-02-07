0b3b9030b26308fd53959055c5017cdc6eb7c43f
Oleh Lamzin <lamzin@google.com>
2022-02-07 15:25:18 +0100

    Introduce telemetryExtension fixture
    New telemetryExtension fixture responsible for launching Chrome with
    pre-installed Telemety Extension, opening PWA UI and companion Chrome
    extension.
    
    BUG=b:218301604
    TEST=tast run <IP> telemetryextension.API*
    
    Change-Id: Ica69a55bc19d9d5f7dd22f01f77cd82edbf9af80

---
+++ src/chromiumos/tast/local/bundles/cros/telemetryextension/fixture/fixture.go
@@ -0,0 +1,165 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package fixture contains Telemetry Extension fixture.
+package fixture
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/fsutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/sysutil"
+	"chromiumos/tast/testing"
+)
+
+const (
+	manifestJSON = "manifest.json"
+	swJS         = "sw.js"
+
+	cleanupTimeout = chrome.ResetTimeout + 20*time.Second
+)
+
+var dataFiles = []string{manifestJSON, swJS}
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name: "telemetryExtension",
+		Desc: "Telemetry Extension fixture with running PWA and companion Telemetry Extension",
+		Contacts: []string{
+			"lamzin@google.com", // Fixture and Telemetry Extension author
+			"mgawad@google.com", // Telemetry Extension author
+			"cros-oem-services-team@google.com",
+		},
+		Impl:            newTelemetryExtensionFixture(),
+		SetUpTimeout:    chrome.LoginTimeout + 30*time.Second + cleanupTimeout,
+		TearDownTimeout: cleanupTimeout,
+		Data:            dataFiles,
+	})
+}
+
+func newTelemetryExtensionFixture() *telemetryExtensionFixture {
+	return &telemetryExtensionFixture{}
+}
+
+// telemetryExtensionFixture implements testing.FixtureImpl.
+type telemetryExtensionFixture struct {
+	dir string
+	cr  *chrome.Chrome
+
+	v Value
+}
+
+// Value is a value exposed by fixture to tests.
+type Value struct {
+	PwaConn *chrome.Conn
+	ExtConn *chrome.Conn
+}
+
+func (f *telemetryExtensionFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	cleanupCtx, cancel := ctxutil.Shorten(ctx, cleanupTimeout)
+	defer cancel()
+
+	defer func(ctx context.Context) {
+		if s.HasError() {
+			f.cleanUp(ctx, s)
+		}
+	}(cleanupCtx)
+
+	dir, err := ioutil.TempDir("", "telemetry_extension")
+	if err != nil {
+		s.Fatal("Failed to create temporary directory for TelemetryExtension: ", err)
+	}
+	f.dir = dir
+
+	if err := os.Chown(dir, int(sysutil.ChronosUID), int(sysutil.ChronosGID)); err != nil {
+		s.Fatal("Failed to chown TelemetryExtension dir: ", err)
+	}
+
+	for _, file := range dataFiles {
+		if err := fsutil.CopyFile(s.DataPath(file), filepath.Join(dir, file)); err != nil {
+			s.Fatalf("Failed to copy %q file to %q: %v", file, dir, err)
+		}
+
+		if err := os.Chown(filepath.Join(dir, file), int(sysutil.ChronosUID), int(sysutil.ChronosGID)); err != nil {
+			s.Fatalf("Failed to chown %q: %v", file, err)
+		}
+	}
+
+	cr, err := chrome.New(ctx, chrome.UnpackedExtension(dir))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	f.cr = cr
+
+	pwaConn, err := cr.NewConn(ctx, "https://www.google.com")
+	if err != nil {
+		s.Fatal("Failed to create connection to google.com: ", err)
+	}
+	f.v.PwaConn = pwaConn
+
+	if err := chrome.AddTastLibrary(ctx, pwaConn); err != nil {
+		s.Fatal("Failed to add Tast library to google.com: ", err)
+	}
+
+	extConn, err := cr.NewConn(ctx, "chrome-extension://gogonhoemckpdpadfnjnpgbjpbjnodgc/sw.js")
+	if err != nil {
+		s.Fatal("Failed to create connection to Telemetry Extension: ", err)
+	}
+	f.v.ExtConn = extConn
+
+	if err := chrome.AddTastLibrary(ctx, extConn); err != nil {
+		s.Fatal("Failed to add Tast library to Telemetry Extension: ", err)
+	}
+
+	return &f.v
+}
+
+func (f *telemetryExtensionFixture) TearDown(ctx context.Context, s *testing.FixtState) {
+	f.cleanUp(ctx, s)
+}
+
+func (f *telemetryExtensionFixture) PreTest(ctx context.Context, s *testing.FixtTestState) {}
+
+func (f *telemetryExtensionFixture) PostTest(ctx context.Context, s *testing.FixtTestState) {}
+
+func (f *telemetryExtensionFixture) Reset(ctx context.Context) error {
+	return nil
+}
+
+// cleanUp releases all associated resources with fixture.
+func (f *telemetryExtensionFixture) cleanUp(ctx context.Context, s *testing.FixtState) {
+	if f.v.ExtConn != nil {
+		if err := f.v.ExtConn.Close(); err != nil {
+			s.Error("Failed to close connection to Telemetry Extension: ", err)
+		}
+		f.v.ExtConn = nil
+	}
+
+	if f.v.PwaConn != nil {
+		if err := f.v.PwaConn.Close(); err != nil {
+			s.Error("Failed to close connection to google.com: ", err)
+		}
+		f.v.PwaConn = nil
+	}
+
+	if f.cr != nil {
+		if err := f.cr.Close(ctx); err != nil {
+			s.Error("Failed to close Chrome: ", err)
+		}
+		f.cr = nil
+	}
+
+	if f.dir != "" {
+		if err := os.RemoveAll(f.dir); err != nil {
+			s.Error("Failed to remove directory with Telemetry Extension: ", err)
+		}
+		f.dir = ""
+	}
+}
