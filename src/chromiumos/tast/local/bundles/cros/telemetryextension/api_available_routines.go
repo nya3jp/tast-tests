703a582968c8409ffa3e9c94b269ff558eb95f25
Oleh Lamzin <lamzin@google.com>
2022-02-07 15:25:18 +0100

    Introduce telemetryExtension fixture
    New telemetryExtension fixture responsible for launching Chrome with
    pre-installed Telemety Extension, opening PWA UI and companion Chrome
    extension.
    
    BUG=b:218301604
    TEST=tast run <IP> telemetryextension.API*
    
    Change-Id: Ica69a55bc19d9d5f7dd22f01f77cd82edbf9af80

---
+++ src/chromiumos/tast/local/bundles/cros/telemetryextension/api_available_routines.go
@@ -0,0 +1,80 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package telemetryextension
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/telemetryextension/dep"
+	"chromiumos/tast/local/bundles/cros/telemetryextension/fixture"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         APIAvailableRoutines,
+		LacrosStatus: testing.LacrosVariantNeeded,
+		Fixture:      "telemetryExtension",
+		Desc:         "Tests chrome.os.diagnostics.getAvailableRoutines Chrome Extension API function exposed to Telemetry Extension",
+		Contacts: []string{
+			"lamzin@google.com", // Test and Telemetry Extension author
+			"mgawad@google.com", // Telemetry Extension author
+			"cros-oem-services-team@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{
+			{
+				Name:              "target_models",
+				ExtraHardwareDeps: dep.TargetModels(),
+			},
+			{
+				Name:              "low_priority_target_models",
+				ExtraHardwareDeps: dep.LowPriorityTargetModels(),
+			},
+		},
+	})
+}
+
+// APIAvailableRoutines tests chrome.os.diagnostics.getAvailableRoutines Chrome Extension API functionality.
+func APIAvailableRoutines(ctx context.Context, s *testing.State) {
+	v := s.FixtValue().(*fixture.Value)
+
+	type response struct {
+		Routines []string `json:"routines"`
+	}
+
+	var resp response
+	if err := v.ExtConn.Call(ctx, &resp,
+		"tast.promisify(chrome.os.diagnostics.getAvailableRoutines)",
+	); err != nil {
+		s.Fatal("Failed to get response from Telemetry extenion service worker: ", err)
+	}
+
+	wantRoutines := []string{
+		"battery_capacity",
+		"battery_health",
+		"cpu_cache",
+		"cpu_stress",
+		"cpu_floating_point_accuracy",
+		"cpu_prime_search",
+		"battery_discharge",
+		"battery_charge",
+		"memory",
+	}
+
+	for _, want := range wantRoutines {
+		exist := false
+		for _, got := range resp.Routines {
+			if want == got {
+				exist = true
+				break
+			}
+		}
+		if !exist {
+			s.Errorf("Wanted %q routine is missing in available routines %v", want, resp.Routines)
+		}
+	}
+}
