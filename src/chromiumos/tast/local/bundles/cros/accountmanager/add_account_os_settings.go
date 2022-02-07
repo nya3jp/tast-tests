e7361f246b5ca1e9ddead038477c7a4e93a0b835
Anastasiia N <anastasiian@google.com>
2022-02-07 17:13:01 +0100

    tast-tests: Refactor accountmanager* tests
    - Add OpenAccountManagerSettingsAction and
      CheckIsAccountPresentInArcAction to allow usage with uiauto.Combine.
    - Refactor CheckIsAccountPresentInArcAction to use longer timeout when
      checking if account exists. "Account is not present in ARC" is one of
      the most common failures right now, see
      https://tastboard.corp.google.com/test/accountmanager.ChangeARCAvailability.
    
    BUG=chromium:1268351
    TEST=tast run <ip> accountmanager.*
    
    Change-Id: I57b81755dd8c17d3b277ceeb3ddc4970c4a65564

--- src/chromiumos/tast/local/bundles/cros/accountmanager/add_account_os_settings.go
+++ src/chromiumos/tast/local/bundles/cros/accountmanager/add_account_os_settings.go
@@ -16,7 +16,6 @@ import (
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
-	"chromiumos/tast/local/chrome/uiauto/ossettings"
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/testing"
 )
@@ -97,14 +96,12 @@ func AddAccountOSSettings(ctx context.Context, s *testing.State) {
 	}
 	defer arcDevice.Close(ctx)
 
-	// Open Account Manager page in OS Settings and find Add Google Account button.
+	// Open Account Manager page in OS Settings and click Add Google Account button.
 	addAccountButton := nodewith.Name("Add Google Account").Role(role.Button)
-	if _, err := ossettings.LaunchAtPageURL(ctx, tconn, cr, "accountManager", ui.Exists(addAccountButton)); err != nil {
-		s.Fatal("Failed to launch Account Manager page: ", err)
-	}
-
-	// Click the button to open account addition dialog.
-	if err := ui.LeftClick(addAccountButton)(ctx); err != nil {
+	if err := uiauto.Combine("Click Add Google Account button",
+		accountmanager.OpenAccountManagerSettingsAction(tconn, cr),
+		ui.LeftClickUntil(addAccountButton, ui.Exists(accountmanager.GetAddAccountDialog())),
+	)(ctx); err != nil {
 		s.Fatal("Failed to click Add Google Account button: ", err)
 	}
 
@@ -137,25 +134,11 @@ func AddAccountOSSettings(ctx context.Context, s *testing.State) {
 
 	// Check that account is present in ARC.
 	s.Log("Verifying that account is present in ARC")
-	if present, err := accountmanager.IsAccountPresentInArc(ctx, tconn, arcDevice, username); err != nil {
-		s.Fatalf("Failed to check that account is present in ARC err=%v", err)
-	} else if !present {
-		s.Fatal("Account is not present in ARC")
+	if err := accountmanager.CheckIsAccountPresentInArcAction(tconn, arcDevice, username, true)(ctx); err != nil {
+		s.Fatal("Failed to check that account is present in ARC: ", err)
 	}
 
-	// Open OS Settings again.
-	if _, err := ossettings.LaunchAtPageURL(ctx, tconn, cr, "accountManager", ui.Exists(addAccountButton)); err != nil {
-		s.Fatal("Failed to launch Account Manager page: ", err)
-	}
-	// Find and click "More actions, <email>" button.
-	if err := uiauto.Combine("Click More actions",
-		ui.WaitUntilExists(moreActionsButton),
-		ui.LeftClick(moreActionsButton),
-	)(ctx); err != nil {
-		s.Fatal("Failed to click More actions button: ", err)
-	}
-
-	if err := accountmanager.RemoveAccountFromOSSettings(ctx, tconn, s.Param().(browser.Type)); err != nil {
+	if err := accountmanager.RemoveAccountFromOSSettings(ctx, tconn, cr, s.Param().(browser.Type), username); err != nil {
 		s.Fatal("Failed to remove account from OS Settings: ", err)
 	}
 
@@ -171,9 +154,7 @@ func AddAccountOSSettings(ctx context.Context, s *testing.State) {
 
 	// Check that account is not present in ARC.
 	s.Log("Verifying that account is not present in ARC")
-	if present, err := accountmanager.IsAccountPresentInArc(ctx, tconn, arcDevice, username); err != nil {
-		s.Fatalf("Failed to check that account is NOT present in ARC, err=%v", err)
-	} else if present {
-		s.Fatal("Account is still present in ARC")
+	if err := accountmanager.CheckIsAccountPresentInArcAction(tconn, arcDevice, username, false)(ctx); err != nil {
+		s.Fatal("Failed to check that account is NOT present in ARC: ", err)
 	}
 }
