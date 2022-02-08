1a6e880cfe7587acb05d2ee4e5d54d736464f8cb
Won Chung <wonchung@google.com>
2022-02-08 02:22:54 +0000

    typec: Add a test to check typec connector's physical_location
    All devices now have physical_location in their sysfs if ACPI table
    exposes _PLD output. This test checks that physical_location directory
    is present for typec connectors.
    
    This proves 3 things:
    1. _PLD is correctly added to USB port by coreboot devicetree
    2. coreboot correctly copies USB port's _PLD to corresponding typec
       connector's _PLD
    3. kernel properly creates sysfs directory (physical_location) and files
       to describe typec connector's _PLD
    
    Currently, only brya devices have custom _PLD for USB port, so
    board:brya is added as a dependency. As more future boards support, they
    shall also be added.
    
    BUG=b:218507705
    TEST=tast run <DUT> typec.PhysicalLocation
    TEST=tast unit test passed
    
    Cq-Depend: chromium:3657640
    Change-Id: I25e3261c28ee27c5d2518d06a6247fa3d058a93c

---
+++ src/chromiumos/tast/local/bundles/cros/typec/physical_location.go
@@ -0,0 +1,81 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package typec
+
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PhysicalLocation,
+		Desc:         "Checks if physical location information is present for Type C connectors",
+		Contacts:     []string{"wonchung@google.com", "chromeos-usb@google.com"},
+		Attr:         []string{"group:mainline", "group:typec", "informational"},
+		SoftwareDeps: []string{"typec_physical_location"},
+	})
+}
+
+const typecPath = "/sys/class/typec/"
+
+// PhysicalLocation checks if valid physical location is present for typec connector devices.
+func PhysicalLocation(ctx context.Context, s *testing.State) {
+	ports, err := ioutil.ReadDir(typecPath)
+	if err != nil {
+		s.Fatal("Could not read typec directory")
+	}
+
+	for _, port := range ports {
+		matched, err := regexp.MatchString(`^port\d+$`, port.Name())
+		if err != nil {
+			s.Errorf("Could not match regex with %s: %v", port.Name(), err)
+		} else if !matched {
+			s.Logf("Skipping %s since it is not a port", port.Name())
+		} else if err := checkForPhysicalLocationDir(filepath.Join(typecPath, port.Name())); err != nil {
+			s.Errorf("Failed to verify proper physical location for %s: %v",
+				port.Name(), err)
+		}
+	}
+}
+
+// checkForPhysicalLocationDir checks if given typec port has a physical location directory with valid fields.
+func checkForPhysicalLocationDir(portPath string) error {
+	physicalLocationPath := filepath.Join(portPath, "physical_location")
+	files, err := ioutil.ReadDir(physicalLocationPath)
+	if err != nil {
+		return errors.Wrapf(err, "could not read directory %s", physicalLocationPath)
+	}
+
+	for _, file := range files {
+		filePath := filepath.Join(physicalLocationPath, file.Name())
+		data, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			return errors.Wrapf(err, "could not read file %s", filePath)
+		}
+
+		d := strings.Trim(string(data), "\n")
+		if matched, err := regexp.MatchString(`^\w+$`, d); err != nil {
+			return errors.Wrapf(err, "could not match regex with %s in file %s", d, filePath)
+		} else if !matched {
+			return errors.Errorf("value %s in file %s is not valid", d, filePath)
+		}
+
+		if d == "unknown" {
+			return errors.Errorf("unknown physical location value in file %s", filePath)
+		}
+		if file.Name() == "lid" && d == "yes" {
+			return errors.New("not located on the lid, but lid set as yes")
+		}
+	}
+
+	return nil
+}
