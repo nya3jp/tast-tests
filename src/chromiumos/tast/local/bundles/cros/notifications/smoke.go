c6bf67c3a96f4771c184954c7ae018d2a7ef9756
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-02-08 15:36:26 -0800

    tast-tests: Add lacros variant to notifications.Smoke
    The following tests need Lacros enabled, open tabs and test web content in the browser.
    - notifications.Smoke
    
    See go/lacros-tast-porting for the porting guidelines.
    
    BUG=b:218564056
    TEST=tast run notifications.Smoke.lacros
    TEST=tast run '("group:mainline" && !"dep:lacros" && "name:notifications.*")'  # ash-chrome tests passed
    
    Change-Id: I3bd2439783cabd285813e345dc5d6e6e890f90ac

--- src/chromiumos/tast/local/bundles/cros/notifications/smoke.go
+++ src/chromiumos/tast/local/bundles/cros/notifications/smoke.go
@@ -12,6 +12,7 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/launcher"
@@ -27,7 +28,7 @@ const uiTimeout = 30 * time.Second
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         Smoke,
-		LacrosStatus: testing.LacrosVariantNeeded,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Checks that notifications appear in notification centre and can be interacted with",
 		Contacts: []string{
 			"chromeos-sw-engprod@google.com",
@@ -36,18 +37,38 @@ func init() {
 		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
-		Fixture:      "chromeLoggedIn",
+		Params: []testing.Param{{
+			Fixture: "chromeLoggedIn",
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			Fixture:           "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Val:               browser.TypeLacros,
+		}},
 	})
 }
 
 // Smoke tests that notifications appear in notification centre.
 func Smoke(ctx context.Context, s *testing.State) {
-	cr := s.FixtValue().(*chrome.Chrome)
-
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to create Test API connection: ", err)
 	}
+
+	// Setup a browser.
+	bt := s.Param().(browser.Type)
+	br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), bt)
+	if err != nil {
+		s.Fatal("Failed to open the browser: ", err)
+	}
+	defer closeBrowser(ctx)
+	bTconn, err := br.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatalf("Failed to create Test API connection for %v browser: %v", bt, err)
+	}
+
 	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
 
 	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
