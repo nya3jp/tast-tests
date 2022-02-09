68f0439fb2eb652dbccdc77b626f87d133c337b8
Ben Reich <benreich@chromium.org>
2022-02-09 21:50:47 +1100

    samba: Add filemanager.SMBFileOperations copy test
    This adds the necessary scaffolding for testing all the file operations
    from and to a Samba share. This initially implements the copy operation
    but leaves it generic enough to implement the remaining operations.
    
    In addition to the above it updates the fixture to ensure the smb.conf
    file forces itself as chronos and subsequently chown's the guestshare to
    ensure the user can write to try.
    
    BUG=chromium:1295640
    TEST=tast run DUT filemanager.SMBFileOperations*
    
    Change-Id: I8e437de3171b17438df8227de2b95de49469ba99

---
+++ src/chromiumos/tast/local/bundles/cros/filemanager/smb_file_operations.go
@@ -0,0 +1,114 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package filemanager
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/smb"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SMBFileOperations,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verify Files app can mount an SMB share and verify the contents",
+		Contacts: []string{
+			"benreich@chromium.org",
+			"chromeos-files-syd@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "smbStarted",
+		Params: []testing.Param{{
+			Name: "copy",
+			Val:  smbFileOperationTest{Fn: testCopyOperation},
+		}},
+	})
+}
+
+type smbFileOperationTestFunc func(ctx context.Context, ew *input.KeyboardEventWriter, s *testing.State, files *filesapp.FilesApp)
+
+type smbFileOperationTest struct {
+	Fn smbFileOperationTestFunc
+}
+
+func SMBFileOperations(ctx context.Context, s *testing.State) {
+
+	fixt := s.FixtValue().(smb.FixtureData)
+	testFunc := s.Param().(smbFileOperationTest).Fn
+
+	// Open the test API.
+	tconn, err := fixt.Chrome.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+	// Launch the files application.
+	files, err := filesapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Launching the Files App failed: ", err)
+	}
+
+	// Open the Add SMB share dialog and focus the first text field.
+	ui := uiauto.New(tconn)
+	fileShareURLTextBox := nodewith.Name("File share URL").Role(role.TextField)
+	if err := uiauto.Combine("Click add SMB file share",
+		files.ClickMoreMenuItem("Services", "SMB file share"),
+		ui.WaitForLocation(fileShareURLTextBox),
+		ui.LeftClick(fileShareURLTextBox))(ctx); err != nil {
+		s.Fatal("Failed to click add SMB share: ", err)
+	}
+
+	// Get a handle to the input keyboard.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard handle: ", err)
+	}
+	defer kb.Close()
+
+	if err := kb.Type(ctx, `\\localhost\guestshare`); err != nil {
+		s.Fatal("Failed entering the new SMB file share path: ", err)
+	}
+
+	if err := kb.Accel(ctx, "Enter"); err != nil {
+		s.Fatal("Failed pressing enter: ", err)
+	}
+
+	testFunc(ctx, kb, s, files)
+}
+
+func testCopyOperation(ctx context.Context, kb *input.KeyboardEventWriter, s *testing.State, files *filesapp.FilesApp) {
+	const textFile = "test.txt"
+	testFileLocation := filepath.Join(filesapp.DownloadPath, textFile)
+	if err := ioutil.WriteFile(testFileLocation, []byte("blahblah"), 0644); err != nil {
+		s.Fatalf("Failed to create file %q: %s", testFileLocation, err)
+	}
+	defer os.Remove(testFileLocation)
+
+	pasteFile := func(ctx context.Context) error {
+		return kb.Accel(ctx, "Ctrl+V")
+	}
+
+	if err := uiauto.Combine("Wait for SMB to mount",
+		files.OpenDownloads(),
+		files.ClickContextMenuItem(textFile, filesapp.Copy),
+		files.OpenPath("Files - guestshare", "guestshare"),
+		pasteFile,
+		files.WaitForFile(textFile))(ctx); err != nil {
+		s.Fatal("Failed to wait for SMB to mount: ", err)
+	}
+}
