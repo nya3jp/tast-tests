ef04ec42af7c259d71edbbdee308e4baddcf1b97
Martin Rodriguez <rodmartin@google.com>
2022-02-11 00:17:14 +0100

    Enable feature Journeys for HistoryClustersVisible
    Enable feature flag for HistoryClustersVisible policy tast test.
    
    BUG=chromium:1288325
    TEST=tast run <device> policy.HistoryClustersVisible
    
    Change-Id: Ic3e27f02355c1d5dbc2e67a7d663346a4332b81a

--- src/chromiumos/tast/local/policyutil/fixtures/lacros.go
+++ src/chromiumos/tast/local/policyutil/fixtures/lacros.go
@@ -46,6 +46,37 @@ func init() {
 		Vars:            []string{lacrosfixt.LacrosDeployedBinary},
 	})
 
+	// TODO(b/218907052): Remove fixture after Journeys flag  is enabled by default.
+	testing.AddFixture(&testing.Fixture{
+		Name:     fixture.LacrosPolicyLoggedInFeatureJourneys,
+		Desc:     "Fixture for a running FakeDMS with lacros and enabling the feature flag Journeys",
+		Contacts: []string{"rodmartin@google.com", "chromeos-commercial-remote-management@google.com"},
+		Impl: lacrosfixt.NewComposedFixture(lacrosfixt.Rootfs, func(v lacrosfixt.FixtValue, pv interface{}) interface{} {
+			return &struct {
+				fakedms.HasFakeDMS
+				lacrosfixt.FixtValue
+			}{
+				pv.(fakedms.HasFakeDMS),
+				v,
+			}
+		}, func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			fdms, ok := s.ParentValue().(*fakedms.FakeDMS)
+			if !ok {
+				return nil, errors.New("parent is not a FakeDMS fixture")
+			}
+			opts := []chrome.Option{chrome.DMSPolicy(fdms.URL),
+				chrome.FakeLogin(chrome.Creds{User: "tast-user@managedchrome.com", Pass: "test0000"}),
+				chrome.ExtraArgs("--disable-lacros-keep-alive"),
+				chrome.LacrosExtraArgs("--enable-features=Journeys")}
+			return opts, nil
+		}),
+		SetUpTimeout:    chrome.LoginTimeout + 7*time.Minute,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		Parent:          fixture.PersistentLacros,
+		Vars:            []string{lacrosfixt.LacrosDeployedBinary},
+	})
+
 	testing.AddFixture(&testing.Fixture{
 		Name:     fixture.LacrosPolicyLoggedInRealUser,
 		Desc:     "Fixture for a running FakeDMS with lacros with a real managed user logged on",
