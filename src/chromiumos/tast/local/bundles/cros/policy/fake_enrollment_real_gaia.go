634beed9aa415ba9e66625ad1ba4b5f5f4180a85
Oleh Lamzin <lamzin@google.com>
2022-02-10 13:06:55 +0100

    Add FakeEnrollmentRealGAIA test
    Change-Id: I054515359b7e1cde3fd36a627d5af0c9e44ca415

---
+++ src/chromiumos/tast/local/bundles/cros/policy/fake_enrollment_real_gaia.go
@@ -0,0 +1,77 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         FakeEnrollmentRealGAIA,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Tests that real GAIA account can be used along with fake enrollment",
+		Contacts: []string{
+			"lamzin@google.com", // Test author
+			"chromeos-commercial-remote-management@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      fixture.FakeDMSEnrolled,
+		Vars: []string{
+			"policy.FakeEnrollmentRealGAIA.user_name",
+			"policy.FakeEnrollmentRealGAIA.password",
+		},
+	})
+}
+
+// FakeEnrollmentRealGAIA tests that real GAIA accouont can be used along with fake enrollment.
+func FakeEnrollmentRealGAIA(ctx context.Context, s *testing.State) {
+	fdms, ok := s.FixtValue().(*fakedms.FakeDMS)
+	if !ok {
+		s.Fatal("Parent is not a FakeDMS fixture")
+	}
+
+	username := s.RequiredVar("policy.FakeEnrollmentRealGAIA.user_name")
+	password := s.RequiredVar("policy.FakeEnrollmentRealGAIA.password")
+
+	pb := fakedms.NewPolicyBlob()
+	pb.DeviceAffiliationIds = []string{"default_affiliation_id"}
+	pb.UserAffiliationIds = []string{"default_affiliation_id"}
+	pb.PolicyUser = username
+
+	if err := fdms.WritePolicyBlob(pb); err != nil {
+		s.Fatal("Failed to write policy blob: ", err)
+	}
+
+	cr, err := chrome.New(ctx,
+		chrome.KeepEnrollment(),
+		chrome.GAIALogin(chrome.Creds{User: username, Pass: password}),
+		chrome.DMSPolicy(fdms.URL),
+		chrome.CustomLoginTimeout(chrome.ManagedUserLoginTimeout))
+	if err != nil {
+		s.Fatal("Chrome startup failed: ", err)
+	}
+
+    // Telemetry Extension work only for affiliated users.
+	pb.AddPolicy(&policy.ExtensionInstallForcelist{Val: []string{
+		"gogonhoemckpdpadfnjnpgbjpbjnodgc;https://clients2.google.com/service/update2/crx",
+	}})
+
+	if err := policyutil.ServeBlobAndRefresh(ctx, fdms, cr, pb); err != nil {
+		s.Fatal("Failed to serve and refresh: ", err)
+	}
+
+	s.Log("Sleeping")
+	testing.Sleep(ctx, time.Minute)
+}
