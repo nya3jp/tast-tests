1ec9dc45519357c4fb92df76eed0c9b513b1deb3
Arthur Chuang <arthur.chuang@cienet.com>
2022-02-11 22:27:43 +0800

    faft:Fix bug in ECWakeOnCharge()
    Add delay in waiting for the hibernation command
    to fully propagate, and delay to ensure lid state
    properly set. Avoid setting servo control when DUT
    is still in the process of waking up from hibernation.
    
    BUG=b:192394835
    TEST=tast run <DUT> firmware.ECWakeOnCharge
    
    Change-Id: I62a1505237697d0c71a73a94b4b8db67181a32a6

--- src/chromiumos/tast/common/servo/ec.go
+++ src/chromiumos/tast/common/servo/ec.go
@@ -211,10 +211,6 @@ func (s *Servo) ECHibernate(ctx context.Context, option HibernationOpt) error {
 					return nil
 				}(targetKeyRelease, targetKeyName)
 			}
-			// Sleep briefly before releasing all the keys to ensure EC put in hibernation mode.
-			if err := testing.Sleep(ctx, 1*time.Second); err != nil {
-				return errors.Wrap(err, "failed to sleep")
-			}
 			return nil
 		}(ctx); err != nil {
 			return err
@@ -223,6 +219,12 @@ func (s *Servo) ECHibernate(ctx context.Context, option HibernationOpt) error {
 		if err := s.RunECCommand(ctx, "hibernate"); err != nil {
 			return errors.Wrap(err, "failed to run EC command: hibernate")
 		}
+	}
+
+	// Delay for a few seconds to allow proper propagation of the
+	// hibernation command, prior to checking EC unresponsive.
+	if err := testing.Sleep(ctx, 5*time.Second); err != nil {
+		return errors.Wrap(err, "failed to sleep")
 	}
 	if err := s.CheckUnresponsiveEC(ctx); err != nil {
 		return errors.Wrap(err, "while verifying whether EC is unresponsive after hibernating DUT")
