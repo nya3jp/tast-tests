c25d90faa27505c88d368b2192290a1162c58e8c
Tuo Shan <shantuo@google.com>
2022-02-11 23:13:31 +0000

    debugd: Add perf tool tests
    These tests test the debugd's GetPerfOutputV2 function. The generic
    test cases are copies from the same test suite from the autotest
    used to test the present GetPerfOutputFd function. The added
    ETM specific tests test the ETM recording flow for supported boards.
    
    BUG=b:201726173
    TEST=tast run $DUT debugd.PerfTool.*
    
    Cq-Depend: chromium:3262985
    Change-Id: I7d2ff36c65d646ea9fa9b287650e18af5ee2cf87

---
+++ src/chromiumos/tast/local/bundles/cros/debugd/perf_tool.go
@@ -0,0 +1,279 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package debugd
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/debugd"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+type testCase struct {
+	quipperArgs    []string // quipper arguments without the duration
+	disableCPUIdle bool
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: PerfTool,
+		Desc: "Tests D-Bus methods related to PerfTool",
+		Contacts: []string{
+			"shantuo@google.com",
+			"cwp-team@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Name: "cycles",
+			Val: testCase{
+				quipperArgs: []string{"--", "record", "-a", "-e", "cycles", "-c", "1000003"},
+			},
+		}, {
+			Name: "etm",
+			Val: testCase{
+				quipperArgs: []string{"--run_inject", "--inject_args", "inject;--itrace=i512il;--strip",
+					"--", "record", "-e", "cs_etm/autofdo/", "-a", "-N"},
+				disableCPUIdle: true,
+			},
+			ExtraSoftwareDeps: []string{"arm"},
+			ExtraHardwareDeps: hwdep.D(hwdep.Platform("trogdor", "herobrine")),
+		}},
+	})
+}
+
+const defaultDuration = 4
+
+// PerfTool tests D-bus methods related to debugd's PerfTool.
+func PerfTool(ctx context.Context, s *testing.State) {
+	dbgd, err := debugd.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to debugd D-Bus service: ", err)
+	}
+
+	testSingleCall(ctx, s, dbgd)
+	testConsecutiveCalls(ctx, s, dbgd)
+	testConcurrentCalls(ctx, s, dbgd)
+	testStopEarly(ctx, s, dbgd)
+}
+
+func getPerfOutput(ctx context.Context, s *testing.State, d *debugd.Debugd,
+	tc testCase, durationSec int) (*os.File, uint64, error) {
+	qprArgs := append([]string{"--duration", strconv.Itoa(durationSec)}, tc.quipperArgs...)
+
+	rPipe, wPipe, err := os.Pipe()
+	if err != nil {
+		s.Fatal("Failed to create status pipe: ", err)
+	}
+	defer wPipe.Close()
+
+	sessionID, err := d.GetPerfOutputV2(ctx, qprArgs, tc.disableCPUIdle, wPipe)
+	if err != nil {
+		rPipe.Close()
+		return nil, 0, err
+	}
+	if sessionID == 0 {
+		s.Fatal("Invalid session ID from GetPerfOutputFd")
+	}
+	return rPipe, sessionID, nil
+}
+
+func checkPerfData(s *testing.State, result []byte) {
+	const minResultLength = 20
+	s.Logf("GetPerfOutputV2() returned %d bytes of perf data", len(result))
+	if len(result) < minResultLength {
+		s.Fatal("Perf output is too small")
+	}
+	if bytes.HasPrefix(result, []byte("<process exited with status: ")) {
+		s.Fatalf("Quipper failed: %s", string(result))
+	}
+}
+
+func testSingleCall(ctx context.Context, s *testing.State, d *debugd.Debugd) {
+	s.Run(ctx, "testSingleCall", func(ctx context.Context, s *testing.State) {
+		tc := s.Param().(testCase)
+		if tc.disableCPUIdle {
+			time.AfterFunc(time.Second, func() {
+				if err := checkCPUIdleDisabled(true); err != nil {
+					s.Error("CPU Idle state not disabled during ETM collection: ", err)
+				}
+			})
+		}
+
+		output, sessionID, err := getPerfOutput(ctx, s, d, tc, defaultDuration)
+		if err != nil {
+			s.Fatal("Failed to call GetPerfOutputV2: ", err)
+		}
+		defer output.Close()
+
+		s.Log("Session ID: ", sessionID)
+		var buf bytes.Buffer
+		if _, err := io.Copy(&buf, output); err != nil {
+			s.Fatal("Failed to read perf output: ", err)
+		}
+		// Wait 1 second to avoid race.
+		testing.Sleep(ctx, time.Second)
+		if tc.disableCPUIdle {
+			if err := checkCPUIdleDisabled(false); err != nil {
+				s.Error("CPU Idle state not restored after perf collection: ", err)
+			}
+		}
+		checkPerfData(s, buf.Bytes())
+	})
+}
+
+func testConsecutiveCalls(ctx context.Context, s *testing.State, d *debugd.Debugd) {
+	s.Run(ctx, "testConsecutiveCalls", func(ctx context.Context, s *testing.State) {
+		tc := s.Param().(testCase)
+		durationSec := 1
+		for i := 0; i < 3; i++ {
+			output, sessionID, err := getPerfOutput(ctx, s, d, tc, durationSec)
+			if err != nil {
+				s.Fatal("Failed to call GetPerfOutputV2: ", err)
+			}
+			defer output.Close()
+
+			s.Log("Session ID: ", sessionID)
+			var buf bytes.Buffer
+			if _, err := io.Copy(&buf, output); err != nil {
+				s.Fatal("Failed to read perf output: ", err)
+			}
+			checkPerfData(s, buf.Bytes())
+		}
+	})
+}
+
+func testConcurrentCalls(ctx context.Context, s *testing.State, d *debugd.Debugd) {
+	s.Run(ctx, "testConcurrentCalls", func(ctx context.Context, s *testing.State) {
+		tc := s.Param().(testCase)
+		repetition := 3
+		errc := make(chan error, repetition)
+		var wg sync.WaitGroup
+		for i := 0; i < repetition; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				output, sessionID, err := getPerfOutput(ctx, s, d, tc, defaultDuration)
+				if err != nil {
+					errc <- err
+					return
+				}
+				defer output.Close()
+				s.Log("Session ID: ", sessionID)
+				var buf bytes.Buffer
+				if _, err := io.Copy(&buf, output); err != nil {
+					s.Fatal("Failed to read perf output: ", err)
+				}
+				checkPerfData(s, buf.Bytes())
+			}()
+		}
+		wg.Wait()
+		close(errc)
+
+		ec := 0
+		for err := range errc {
+			s.Log("\"Existing perf tool running\" error expected, got: ", err)
+			ec++
+		}
+		if ec != repetition-1 {
+			s.Errorf("Calling GetPerfOutputV2 %d times concurrently, got %d errors, want %d",
+				repetition, ec, repetition-1)
+		}
+	})
+}
+
+func testStopEarly(ctx context.Context, s *testing.State, d *debugd.Debugd) {
+	s.Run(ctx, "testStopEarly", func(ctx context.Context, s *testing.State) {
+		tc := s.Param().(testCase)
+		durationSec := 15
+		stop := 4
+		start := time.Now()
+
+		if tc.disableCPUIdle {
+			time.AfterFunc(time.Second, func() {
+				if err := checkCPUIdleDisabled(true); err != nil {
+					s.Error("CPU Idle state not disabled during ETM collection: ", err)
+				}
+			})
+		}
+
+		output, sessionID, err := getPerfOutput(ctx, s, d, tc, durationSec)
+		if err != nil {
+			s.Fatal("Failed to call GetPerfOutputV2: ", err)
+		}
+		defer output.Close()
+
+		time.AfterFunc(time.Duration(stop)*time.Second, func() {
+			if err := d.StopPerf(ctx, sessionID); err != nil {
+				s.Fatal("Failed to call StopPerf: ", err)
+			}
+		})
+
+		s.Log("Session ID: ", sessionID)
+		var buf bytes.Buffer
+		if _, err := io.Copy(&buf, output); err != nil {
+			s.Fatal("Failed to read perf output: ", err)
+		}
+
+		rt := time.Now().Sub(start)
+		if rt >= time.Duration(durationSec)*time.Second {
+			s.Errorf("Failed to stop perf after %d seconds", stop)
+		}
+		s.Log("Real perf elapsed time: ", rt)
+		// Wait 1 second to avoid race.
+		testing.Sleep(ctx, time.Second)
+		if tc.disableCPUIdle {
+			if err := checkCPUIdleDisabled(false); err != nil {
+				s.Error("CPU Idle state not restored after perf collection: ", err)
+			}
+		}
+		checkPerfData(s, buf.Bytes())
+	})
+}
+
+// checkCPUIdleDisabled verifies whether all CPU's idle states match the given
+// disabled status.
+func checkCPUIdleDisabled(disabled bool) error {
+	const cpuTopologyLocation = "/sys/devices/system/cpu/present"
+	const cpuidlePathPat = "/sys/devices/system/cpu/cpu%d/cpuidle/state%d/disable"
+	b, err := ioutil.ReadFile(cpuTopologyLocation)
+	if err != nil {
+		return err
+	}
+	var min, max int
+	if _, err := fmt.Sscanf(string(b), "%d-%d", &min, &max); err != nil {
+		return err
+	}
+	for cpu := min; cpu <= max; cpu++ {
+		for state := 0; ; state++ {
+			cpuidlePath := fmt.Sprintf(cpuidlePathPat, cpu, state)
+			f, err := os.Open(cpuidlePath)
+			if err != nil {
+				if os.IsNotExist(err) {
+					break
+				}
+				return err
+			}
+			disable := make([]byte, 1)
+			if n, err := f.Read(disable); err != nil || n != 1 {
+				return err
+			}
+			if (disable[0] == '1') != disabled {
+				return errors.Errorf("file %s shows %s, which does not match the expected state %v",
+					cpuidlePath, string(disable), disabled)
+			}
+		}
+	}
+	return nil
+}
