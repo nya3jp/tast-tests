2852b12f2d741b754611e116ba48236eaebc5f49
Jared Bennett <jaredbennett@google.com>
2022-02-14 14:54:52 -0800

    tast-tests: router-specific file copy util for eap cred install
    The linuxssh.PutFiles function relies on tar and uses tar options that
    are not supported by OpenWrt routers. This adds a more manual
    router.PutFiles utility that does not depend on tar but instead copies
    each file manually. The eap.InstallRouterCredentials function has been
    updated to use the new router.PutFiles instead of linuxssh.PutFiles.
    
    BUG=b:211517577
    TEST=tast run -var=router=$LEGACY_ROUTER -var=pcap=$PCAP $DUT \
     wifi.SimpleConnect.8021xwep
    
    Change-Id: I3c3536055c54dd977170cdd64ca6e31c4ff9f853

---
+++ src/chromiumos/tast/common/wificell/router/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package router provides common local and remote utilities for accessing or
+// controlling different routers.
+package router
