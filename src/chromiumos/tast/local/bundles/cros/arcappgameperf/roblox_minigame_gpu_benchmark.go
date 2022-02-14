8cb95c0aa2840a348e0b1d2787a9278ea642c2e1
David Welling <davidwelling@google.com>
2022-02-14 23:34:23 +0000

    arcgameperf: Adjust Roblox Test Description
    Adjust the description of the Roblox mini-game test to better represent
    what it is doing.
    
    BUG=b:214293520
    TEST=tast run $DUT arcappgameperf.RobloxMini*
    
    Change-Id: I9484a9dcfac3ffc14c5709c43bde7c5e159c3a97

--- src/chromiumos/tast/local/bundles/cros/arcappgameperf/roblox_minigame_gpu_benchmark.go
+++ src/chromiumos/tast/local/bundles/cros/arcappgameperf/roblox_minigame_gpu_benchmark.go
@@ -27,7 +27,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         RobloxMinigameGpuBenchmark,
 		LacrosStatus: testing.LacrosVariantUnknown,
-		Desc:         "Captures launch metrics for Roblox",
+		Desc:         "Logs in to Roblox, loads a mini-game, and records performance metrics",
 		Contacts:     []string{"davidwelling@google.com", "arc-engprod@google.com"},
 		Attr:         []string{"group:crosbolt", "crosbolt_nightly"},
 		SoftwareDeps: []string{"chrome"},
