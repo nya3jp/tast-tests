cdfc769937e690969b8c5b970980a2444e21a6b9
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-02-15 12:15:45 -0800

    tast-tests: Add lacros variant to ui.NotificationPopupPerf
    The following tests need Lacros enabled, and test notifications from browser.
    - ui.NotificationPopupPerf
    
    See go/lacros-tast-porting for the porting guidelines.
    
    BUG=b:216242056
    TEST=tast run ui.NotificationPopupPerf.lacros
    TEST=tast run '("group:mainline" && !"dep:lacros" && "name:ui.*")'  # ash-chrome tests
    
    Change-Id: I3a177b1316b70319f7d0070f7ada5b5cce463f4a

--- src/chromiumos/tast/local/bundles/cros/ui/notification_popup_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/notification_popup_perf.go
@@ -14,6 +14,7 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
 	"chromiumos/tast/local/power"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
@@ -22,14 +23,22 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         NotificationPopupPerf,
-		LacrosStatus: testing.LacrosVariantUnknown,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Measures animation smoothness of notification popup animations",
 		Contacts:     []string{"leandre@chromium.org", "amehfooz@chromium.org", "chromeos-wmp@google.com", "chromeos-sw-engprod@google.com"},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome"},
 		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
-		Fixture:      "chromeLoggedIn",
-		Timeout:      3 * time.Minute,
+		Params: []testing.Param{{
+			Fixture: "chromeLoggedIn",
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			Fixture:           "lacrosPrimary",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Val:               browser.TypeLacros,
+		}},
+		Timeout: 3 * time.Minute,
 	})
 }
 
@@ -39,29 +48,41 @@ func NotificationPopupPerf(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to turn on display: ", err)
 	}
 
-	cr := s.FixtValue().(*chrome.Chrome)
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to connect to test API: ", err)
 	}
 
+	// Setup a browser.
+	bt := s.Param().(browser.Type)
+	br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), bt)
+	if err != nil {
+		s.Fatal("Failed to open the browser: ", err)
+	}
+	defer closeBrowser(ctx)
+	bTconn, err := br.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatalf("Failed to create Test API connection for %v browser: %v", bt, err)
+	}
+
 	// Pre-add some notifications to show remove animation on the first run.
-	ids, err := addNotifications(ctx, tconn)
+	ids, err := addNotifications(ctx, tconn, bTconn)
 	if err != nil {
 		s.Fatal("Failed to add notifications: ", err)
 	}
 
 	// This includes adding notifications to show popup fade in and move up animation,
 	// then remove notification in reverse order (newer then older) to show fade out and move down animation.
-	pv := perfutil.RunMultiple(ctx, s, cr.Browser(), perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
+	pv := perfutil.RunMultiple(ctx, s, br, perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
 		for _, id := range ids {
-			if err := browser.ClearNotification(ctx, tconn, id); err != nil {
+			if err := browser.ClearNotification(ctx, bTconn, id); err != nil {
 				return errors.Wrapf(err, "failed to clear notification (id: %s): ", id)
 			}
 		}
 		ids = nil
 
-		ids, err = addNotifications(ctx, tconn)
+		ids, err = addNotifications(ctx, tconn, bTconn)
 		if err != nil {
 			return errors.Wrap(err, "failed to add notifications")
 		}
@@ -78,7 +99,8 @@ func NotificationPopupPerf(ctx context.Context, s *testing.State) {
 
 // addNotifications create some test notifications and return the ids of those notifications
 // in reverse order (newer then older).
-func addNotifications(ctx context.Context, tconn *chrome.TestConn) ([]string, error) {
+// tconn is used for ash to wait notifications on the receiver side, while bTconn is for either ash-chrome or lacros-chrome browser to send notifications on the sender side.
+func addNotifications(ctx context.Context, tconn *chrome.TestConn, bTconn *browser.TestConn) ([]string, error) {
 	var ids []string
 	const uiTimeout = 30 * time.Second
 	ts := []browser.NotificationType{
@@ -88,7 +110,7 @@ func addNotifications(ctx context.Context, tconn *chrome.TestConn) ([]string, er
 		browser.NotificationTypeList,
 	}
 	for _, t := range ts {
-		id, err := browser.CreateTestNotification(ctx, tconn, t, fmt.Sprintf("Test%sNotification", t), "test message")
+		id, err := browser.CreateTestNotification(ctx, bTconn, t, fmt.Sprintf("Test%sNotification", t), "test message")
 		if err != nil {
 			return nil, errors.Wrapf(err, "failed to create %s notification: ", t)
 		}
