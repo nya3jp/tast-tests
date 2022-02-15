0debeb235cafd9794143f456073f62fae62b004c
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-02-15 15:46:01 -0800

    tast-tests: Add lacros variant to shelf.OpenCloseSwitchApps
    The following tests need Lacros enabled and interact with the browser
    app from Shelf.
    - shelf.OpenCloseSwitchApps
    
    This also adds a PrimaryBrowser util to the apps package that can
    provide the browser app info based on the primary browser type.
    
    See go/lacros-tast-porting for the porting guidelines.
    
    BUG=b:219815913
    TEST=tast run shelf.OpenCloseSwitchApps.lacros
    TEST=tast run '("group:mainline" && !"dep:lacros" && "name:shelf.*")'  # ash-chrome tests
    
    Change-Id: Ib24f502084af4bf3aab0eb31f9d04b03e6591e08

--- src/chromiumos/tast/local/apps/apps.go
+++ src/chromiumos/tast/local/apps/apps.go
@@ -14,6 +14,7 @@ import (
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
@@ -410,6 +411,26 @@ func ChromeOrChromium(ctx context.Context, tconn *chrome.TestConn) (App, error) 
 		}
 	}
 	return App{}, errors.New("Neither Chrome or Chromium were found in available apps")
+}
+
+// PrimaryBrowser returns the primary browser for the current build.
+// 'Chromium' on non branded ash-chrome builds (e.g amd64-generic)
+// 'Chrome' on branded ash-chrome builds
+// 'Lacros' on branded lacros-chrome builds (eg, lacros64)
+func PrimaryBrowser(ctx context.Context, tconn *chrome.TestConn, bt browser.Type) (App, error) {
+	switch bt {
+	case browser.TypeAsh:
+		browserApp, err := ChromeOrChromium(ctx, tconn)
+		if err != nil {
+			return App{}, errors.Wrap(err, "failed to find the browser app for ash-chrome")
+		}
+		return browserApp, nil
+	case browser.TypeLacros:
+		return Lacros, nil
+	default:
+		return App{}, errors.Errorf("Unrecognized browser type: %v", bt)
+	}
+	return App{}, errors.New("no primary browser was found in available apps")
 }
 
 // InstallPWAForURL navigates to a PWA, attempts to install and returns the installed app ID.
