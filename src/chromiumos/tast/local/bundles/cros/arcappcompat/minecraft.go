501a18329038d0b7279321e411ede6e277ddf842
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2022-02-14 16:04:13 -0800

    arcappcompat: Automate Minecraft education in tast
    BUG=b:219522191
    TEST=tast run <eve> arcappcompat.Minecraft.tablet_mode
    tast run <eve> arcappcompat.Minecraft.vm_clamshell_mode
    test_that --board=eve chromeos6-row3-rack11-host17.cros tast.appcompat_release
    
    Change-Id: I7378335559fa57cd5f9cd43d99a2c1e1c5e36db0

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/minecraft.go
@@ -0,0 +1,110 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+// clamshellLaunchForMinecraft launches Minecraft in clamshell mode.
+var clamshellLaunchForMinecraft = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForMinecraft},
+}
+
+// touchviewLaunchForMinecraft launches Minecraft in tablet mode.
+var touchviewLaunchForMinecraft = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForMinecraft},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Minecraft,
+		LacrosStatus: testing.LacrosVariantUnknown,
+		Desc:         "Functional test for Minecraft that installs and checks Minecraft correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat", "appcompat_release"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name: "clamshell_mode",
+			Val: testutil.TestParams{
+				LaunchTests: clamshellLaunchForMinecraft,
+				CommonTests: testutil.ClamshellCommonTests,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				LaunchTests: touchviewLaunchForMinecraft,
+				CommonTests: testutil.TouchviewSmokeTests,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "vm_clamshell_mode",
+			Val: testutil.TestParams{
+				LaunchTests: clamshellLaunchForMinecraft,
+				CommonTests: testutil.ClamshellSmokeTests,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				LaunchTests: touchviewLaunchForMinecraft,
+				CommonTests: testutil.TouchviewSmokeTests,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}},
+		Timeout: 10 * time.Minute,
+		Vars:    []string{"arcappcompat.gaiaPoolDefault"},
+	})
+}
+
+// Minecraft test uses library for opting into the playstore and installing app.
+// Checks Minecraft correctly changes the window states in both clamshell and touchview mode.
+func Minecraft(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.mojang.minecraftedu"
+		appActivity = "com.mojang.minecraftpe.MainActivity"
+	)
+	testSet := s.Param().(testutil.TestParams)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testSet)
+}
+
+// launchAppForMinecraft verify app and
+// verify app reached main activity page of the app.
+func launchAppForMinecraft(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+
+	// Check for launch verifier.
+	launchVerifier := d.Object(ui.PackageName(appPkgName))
+	if err := launchVerifier.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		testutil.DetectAndHandleCloseCrashOrAppNotResponding(ctx, s, d)
+		s.Fatal("launchVerifier doesn't exists: ", err)
+	}
+}
