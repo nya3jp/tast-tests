7bc9c1d4595604687b1bf0420da03d6508f2104a
Georg Neis <neis@chromium.org>
2022-02-16 15:03:55 +0000

    policy: Refactor FullscreenAllowed tast for lacros
    Add a lacros variation and refactor the test code such that
    no branching on the chrome type is needed at the top-level.
    
    BUG=chromium:1259192
    TEST=tast run policy.FullscreenAllowed*
    
    Change-Id: I865536da655708e68ed4c685078c0162ecc06dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3469094
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Tested-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/policy/fullscreen_allowed.go
+++ src/chromiumos/tast/local/bundles/cros/policy/fullscreen_allowed.go
@@ -11,8 +11,11 @@ import (
 	"chromiumos/tast/common/fixture"
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/policyutil"
@@ -22,7 +25,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         FullscreenAllowed,
-		LacrosStatus: testing.LacrosVariantNeeded,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Behavior of FullscreenAllowed policy: checking if fullscreen is allowed or not",
 		Contacts: []string{
 			"swapnilgupta@google.com", // Test author
@@ -30,7 +33,15 @@ func init() {
 		},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline", "informational"},
-		Fixture:      fixture.ChromePolicyLoggedIn,
+		Params: []testing.Param{{
+			Fixture: fixture.ChromePolicyLoggedIn,
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val:               browser.TypeLacros,
+		}},
 	})
 }
 
@@ -38,6 +49,11 @@ func init() {
 func FullscreenAllowed(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	// Reserve ten seconds for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
 
 	for _, param := range []struct {
 		name                  string
@@ -61,8 +77,6 @@ func FullscreenAllowed(ctx context.Context, s *testing.State) {
 		},
 	} {
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
-			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
-
 			// Perform cleanup.
 			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
 				s.Fatal("Failed to clean up: ", err)
@@ -73,19 +87,20 @@ func FullscreenAllowed(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to update policies: ", err)
 			}
 
-			// Open browser window.
-			conn, err := cr.NewConn(ctx, "")
+			conn, _, closeBrowser, err := browserfixt.SetUpWithURL(ctx, s.FixtValue(), s.Param().(browser.Type), "about:blank")
 			if err != nil {
-				s.Fatal("Failed to open browser window: ", err)
+				s.Fatal("Failed to setup chrome: ", err)
 			}
+			defer closeBrowser(cleanupCtx)
 			defer conn.Close()
+			defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
 
 			var isFullScreen bool
 			if err := conn.Eval(ctx, `window.innerHeight == screen.availHeight`, &isFullScreen); err != nil {
 				s.Fatal("Failed to execute JS expression: ", err)
 			}
 			// Check that the screen is not in full screen mode currently.
-			if isFullScreen == true {
+			if isFullScreen {
 				s.Error("Browser should be not be in full screen mode initially")
 			}
 
