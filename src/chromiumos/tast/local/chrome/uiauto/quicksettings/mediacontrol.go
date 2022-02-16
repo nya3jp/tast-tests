f3ccc80d040b8ec8f6c56becfbb58c7ce4915e0c
Ting Chen <ting.chen@cienet.com>
2022-02-16 11:44:57 +0800

    mta: add test case arc.PinUnpinMediaPod
    [MTA f646c] Check the pin/unpin/re-pin for the media control pod.
    
    BUG=b:200987404
    TEST=tast run -var=ui.gaiaPoolDefault='<user>:<pwd>' <DUT> arc.PinUnpinMediaPod
    
    Change-Id: I26b6676130ceeccd03c67f8f0ea65fb50b49f01c

--- src/chromiumos/tast/local/chrome/uiauto/quicksettings/mediacontrol.go
+++ src/chromiumos/tast/local/chrome/uiauto/quicksettings/mediacontrol.go
@@ -5,6 +5,8 @@
 package quicksettings
 
 import (
+	"context"
+
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
@@ -14,11 +16,23 @@ import (
 // MediaControlsPod is the 'Media controls' pod in the Quick Settings.
 var MediaControlsPod = nodewith.NameStartingWith("Media controls").HasClass("Button").Ancestor(RootFinder)
 
+// MediaControlsDetailView is the detailed Media controls view within the Quick Settings.
+var MediaControlsDetailView = nodewith.HasClass("TrayDetailedView")
+
 // PinnedMediaControls is the pinned 'Media controls' widget in shelf.
 var PinnedMediaControls = nodewith.Role(role.Button).Name("Control your music, videos, and more").Ancestor(StatusAreaWidget)
 
 // MediaControlsDialog is the opened 'Media controls' panel in shelf.
 var MediaControlsDialog = nodewith.Role(role.Dialog).Name("Media controls").HasClass("RootView")
+
+// PinMediaControlsPod pins the Media controls pod from the detail page.
+func PinMediaControlsPod(tconn *chrome.TestConn) uiauto.Action {
+	ui := uiauto.New(tconn)
+	return uiauto.Combine("click pin button and check the widget",
+		ui.LeftClick(nodewith.Name("Pin to shelf").HasClass("IconButton").Ancestor(MediaControlsDetailView)),
+		ui.WaitUntilExists(PinnedMediaControls),
+	)
+}
 
 // UnpinMediaControlsPod unpins the media controls widget from shelf.
 func UnpinMediaControlsPod(tconn *chrome.TestConn) uiauto.Action {
@@ -28,4 +42,21 @@ func UnpinMediaControlsPod(tconn *chrome.TestConn) uiauto.Action {
 		ui.LeftClick(nodewith.Role(role.Button).Name("Unpin").Ancestor(MediaControlsDialog)),
 		ui.WaitUntilGone(PinnedMediaControls),
 	)
+}
+
+// NavigateToMediaControlsSubpage navigates to the detailed Media controls view
+// within the Quick Settings. This is safe to call even when the Quick Settings
+// are already open.
+func NavigateToMediaControlsSubpage(tconn *chrome.TestConn, title string) uiauto.Action {
+	return func(ctx context.Context) error {
+		if err := Expand(ctx, tconn); err != nil {
+			return err
+		}
+
+		ui := uiauto.New(tconn)
+		return uiauto.Combine("click the Media controls title",
+			ui.LeftClick(nodewith.Name(title).HasClass("Label").Ancestor(MediaControlsPod)),
+			ui.WaitUntilExists(MediaControlsDetailView),
+		)(ctx)
+	}
 }
