78b7d56a48d789fbc22af9013a718c0cd0618774
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-02-17 15:31:57 -0800

    DO NOT SUBMIT (in progress) tast-tests: Add lacros variant to ui.TabletOperations
    The following test needs Lacros enabled, and test Javascript exception
    in browser.
    - ui.TabletOperations
    
    See go/lacros-tast-porting for the porting guidelines.
    
    BUG=b:216242056
    TEST=tast run ui.TabletOperations.lacrors*
    TEST=tast run '("group:mainline" && !"dep:lacros" && "name:ui.*")'  # ash-chrome tests
    
    Change-Id: I71c2c644229ea1d6a035ed9049d9ed49dbb2367d

--- src/chromiumos/tast/local/bundles/cros/ui/tablet_operations.go
+++ src/chromiumos/tast/local/bundles/cros/ui/tablet_operations.go
@@ -12,9 +12,12 @@ import (
 
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/bundles/cros/ui/perfutil"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
 	"chromiumos/tast/local/chrome/display"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
@@ -30,12 +33,13 @@ type testParam struct {
 	// the major/minor values, only used if touchRadiusOverride is set to true.
 	touchMajorValue int32
 	touchMinorValue int32
+	browserType     browser.Type
 }
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         TabletOperations,
-		LacrosStatus: testing.LacrosVariantNeeded,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Check if the performance around user operations for tablet mode is good enough; see also go/cros-ui-perftests-cq#heading=h.fwfk0yg3teo1",
 		Contacts: []string{
 			"xdai@chromium.org",
@@ -44,7 +48,6 @@ func init() {
 			"mukai@chromium.org", // Tast author
 		},
 		Attr:         []string{"group:mainline", "informational"},
-		Fixture:      "chromeLoggedIn",
 		SoftwareDeps: []string{"chrome", "no_chrome_dcheck"},
 		HardwareDeps: hwdep.D(
 			hwdep.InternalDisplay(),
@@ -54,30 +57,42 @@ func init() {
 		),
 		Params: []testing.Param{
 			{
+				Fixture: "chromeLoggedIn",
 				ExtraHardwareDeps: hwdep.D(
 					hwdep.SkipOnModel(perfutil.UnstableModels...),
 					// Exclude kohaku because "EnableNeuralPalmDetectionFilter" is enabled
 					// there and rejects touches for swiping right. See b/196859354.
 					hwdep.SkipOnModel("kohaku")),
-				Val: testParam{},
+				Val: testParam{browserType: browser.TypeAsh},
 			},
 			// TODO(crbug.com/1168774): remove "unstable" once we see stability on all platforms.
 			{
 				Name:              "unstable",
+				Fixture:           "chromeLoggedIn",
 				ExtraAttr:         []string{"informational"},
 				ExtraHardwareDeps: hwdep.D(hwdep.Model(perfutil.UnstableModels...)),
-				Val:               testParam{},
+				Val:               testParam{browserType: browser.TypeAsh},
 			},
 			// Run kohaku with specific size touch. See b/196859354.
 			{
 				Name:              "kohaku",
+				Fixture:           "chromeLoggedIn",
 				ExtraHardwareDeps: hwdep.D(hwdep.Model("kohaku")),
 				ExtraAttr:         []string{"informational"},
 				Val: testParam{
 					touchRadiusOverride: true,
 					touchMajorValue:     25,
 					touchMinorValue:     24,
+					browserType:         browser.TypeAsh,
 				},
+			},
+			{
+				Name:    "lacros",
+				Fixture: "lacrosPrimary",
+				ExtraHardwareDeps: hwdep.D(
+					hwdep.SkipOnModel(perfutil.UnstableModels...),
+					hwdep.SkipOnModel("kohaku")),
+				Val: testParam{browserType: browser.TypeLacros},
 			},
 		},
 	})
@@ -100,7 +115,7 @@ func TabletOperations(ctx context.Context, s *testing.State) {
 	)
 	// When custom expectation value needs to be set, modify expects here.
 
-	cr := s.FixtValue().(*chrome.Chrome)
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to get the connection to the test API: ", err)
@@ -109,11 +124,18 @@ func TabletOperations(ctx context.Context, s *testing.State) {
 	ctx, cancel := ctxutil.Shorten(ctx, 2*time.Second)
 	defer cancel()
 
-	if err := ash.CreateWindows(ctx, tconn, cr, "", 2); err != nil {
+	paramVal := s.Param().(testParam)
+	// Set up a browser.
+	bt := paramVal.browserType
+	br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), bt)
+	defer closeBrowser(ctx)
+
+	// TODO: Check if an extra blank window popped up via SetUp needs to be closed for lacros-chrome or not.
+	if err := ash.CreateWindows(ctx, tconn, br, "", 2); err != nil {
 		s.Fatal("Failed to create new windows: ", err)
 	}
 
-	r := perfutil.NewRunner(cr.Browser())
+	r := perfutil.NewRunner(br)
 	r.Runs = 5
 	r.RunTracing = false
 
@@ -224,10 +246,14 @@ func TabletOperations(ctx context.Context, s *testing.State) {
 			return errors.Wrap(err, "hotseat is in an unexpected state")
 		}
 		ui := uiauto.New(tconn)
-		// Tap the chrome icon in the app-list to re-activate the browser window.
-		button := nodewith.ClassName("AppListItemView").NameRegex(regexp.MustCompile("^(Chrome|Chromium)$"))
+		// Tap the browser icon (either Chromium, Chrome or Lacros) in the app-list to re-activate the browser window.
+		browserApp, err := apps.PrimaryBrowser(ctx, tconn, bt)
+		if err != nil {
+			s.Fatalf("Could not find the %v browser app: %v", bt, err)
+		}
+		button := nodewith.ClassName("AppListItemView").NameRegex(regexp.MustCompile("^" + browserApp.Name + "$"))
 		if err := ui.WaitUntilExists(button)(ctx); err != nil {
-			return errors.Wrap(err, "failed to find the Chrome icon")
+			return errors.Wrapf(err, "failed to find the browser icon: %v", browserApp.Name)
 		}
 		loc, err := ui.Location(ctx, button)
 		if err != nil {
@@ -241,7 +267,9 @@ func TabletOperations(ctx context.Context, s *testing.State) {
 		}
 		var wid int
 		// At least one of the windows should resume, i.e. not in minimized status.
+		// TODO: no window is in normal state: context deadline exceeded; last error follows: no window satisfies the condition
 		if err := ash.WaitForCondition(ctx, tconn, func(w *ash.Window) bool {
+			// s.Logf("hyungtaekim: w.State: %v, w: %#v", w.State, w)
 			if w.State != ash.WindowStateMinimized {
 				wid = w.ID
 				return true
@@ -334,7 +362,6 @@ func TabletOperations(ctx context.Context, s *testing.State) {
 		}
 
 		// Swipe on the splitview divider to exit splitview.
-		paramVal := s.Param().(testParam)
 		if paramVal.touchRadiusOverride {
 			if err := stw.SetSize(ctx, paramVal.touchMajorValue, paramVal.touchMinorValue); err != nil {
 				return errors.Wrap(err, "failed to set size for device")
