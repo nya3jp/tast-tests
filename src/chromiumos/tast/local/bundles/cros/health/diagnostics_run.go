4188e31aa6b74f945ca3dd08ba251b8c0240cc59
Denny Huang <dennyh@google.com>
2022-02-18 10:06:32 +0000

    health.DiagnosticsRun: enable nvme-self-test routine on morphius
    BUG=b:175305207
    TEST=tast run $morphius-dut health.DiagnosticsRun
    
    Change-Id: Icf0992ce5f6171993bb47076dfcfa36791ab992a

--- src/chromiumos/tast/local/bundles/cros/health/diagnostics_run.go
+++ src/chromiumos/tast/local/bundles/cros/health/diagnostics_run.go
@@ -65,8 +65,6 @@ func init() {
 			Timeout:           3 * time.Minute,
 			ExtraSoftwareDeps: []string{"nvme"},
 			ExtraHardwareDeps: hwdep.D(hwdep.Nvme(), hwdep.NvmeSelfTest(),
-				// TODO(http://b/175305207): some zork nvme controllers lock up
-				hwdep.SkipOnModel("morphius"),
 				// eve uses Samsung nvme drives which do not support self-test
 				hwdep.SkipOnModel("eve")),
 		}, {
