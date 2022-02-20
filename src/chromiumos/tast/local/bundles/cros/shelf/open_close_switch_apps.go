48bb478ca99b5775002396a7331f346b436e9db8
Alexander Alekseev <alemate@chromium.org>
2022-02-19 21:30:05 -0800

    tast-tests: Make OpenCloseSwitchApps informational.
    Test failed for the two consecutive Chrome uprev failures: 101.0.4897.0
    and  101.0.4898.0.
    
    BUG=chromium:1299244
    TEST=fast_build.sh -T
    
    Change-Id: I02594cff03d7d43011707e4f6fef7eded9aec775

--- src/chromiumos/tast/local/bundles/cros/shelf/open_close_switch_apps.go
+++ src/chromiumos/tast/local/bundles/cros/shelf/open_close_switch_apps.go
@@ -31,7 +31,7 @@ func init() {
 			"tbarzic@chromium.org",
 			"cros-system-ui-eng@google.com",
 		},
-		Attr:         []string{"group:mainline"},
+		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Pre:          chrome.LoggedIn(),
 	})
