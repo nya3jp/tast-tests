6b23b098d71aa741fcaf4b7a00fd7a8f6b2faf8f
Nikolai Artemiev <nartemiev@google.com>
2022-02-22 20:19:52 +1100

    tast-tests: Add flashrom writeprotect support test
    Add a test that checks writeprotect operations are supported for the
    DUT's flash. Just runs --wp-status for now since WP support is
    all-or-nothing.
    
    BUG=b:215611404
    TEST=tast run on kasumi dut with W25Q128.W flash
    
    Change-Id: I223ee6aded9b2ccee3e35bbebf9c9cfda4fb10f1

---
+++ src/chromiumos/tast/local/bundles/cros/firmware/flashrom_write_protect.go
@@ -0,0 +1,37 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package firmware
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/shutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         FlashromWriteProtect,
+		Desc:         "Checks that flashrom supports writeprotect commands on the device's flash IC",
+		Contacts:     []string{"nartemiev@google.com", "chromeos-firmware@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"flashrom"},
+		Timeout:      3 * time.Minute,
+	})
+}
+
+func FlashromWriteProtect(ctx context.Context, s *testing.State) {
+	// WP support is all-or-nothing, just run a simple status command
+	// to check the flash IC is supported
+	var cmd = testexec.CommandContext(ctx, "flashrom", "--wp-status")
+
+	var output, err = cmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatalf("%q failed: %s", shutil.EscapeSlice(cmd.Args), err)
+	}
+	s.Logf("%q output: %s", shutil.EscapeSlice(cmd.Args), output)
+}
