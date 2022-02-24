7a314491e2c74ec45a8dfe82f1f423692db51688
Jun Sheng <shengjun@chromium.org>
2022-02-24 03:08:27 +0000

    Revert "inputs: Add TouchpadFunctionality"
    This reverts commit 7ed2110f901c3ef63b169c352fc973df537dc3a2.
    
    Reason for revert: Revert this cl for a further improvement. Comments added in the original cl.
    
    Original change's description:
    > inputs: Add TouchpadFunctionality
    >
    > Verify Touchpad primary button clicks
    >
    > BUG=b:195902740
    >
    > TEST: tast run --build=true <DUTIP> inputs.TouchpadFunctionality.clamshell_mode
    >
    > Change-Id: Ie945342714d17932c71cd8d61ab97cac32d6a924
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3303233
    > Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    > Tested-by: Pathan Jilani <pathan.jilani@intel.com>
    > Reviewed-by: Ambalavanan M M <ambalavanan.m.m@intel.com>
    > Commit-Queue: Pathan Jilani <pathan.jilani@intel.com>
    
    Bug: b:195902740
    Change-Id: I5c696f0c4eeeabd60900d9dfd04f72ae241d3df3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3486817
    Auto-Submit: Jun Sheng <shengjun@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jun Sheng <shengjun@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/inputs/tablet/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2021 The Chromium OS Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Package tablet provides the control of getting and setting tablet_mode_angle.
-package tablet
