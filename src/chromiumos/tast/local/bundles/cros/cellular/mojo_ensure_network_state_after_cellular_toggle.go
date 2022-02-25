4d70b924e71a817a68a1c0f39a190881b8377a8b
Shijin Abraham <shijinabraham@google.com>
2022-02-25 15:49:21 -0800

    tast: Add test that disables Cellular and check WiFi/Ethernet is unchanged
    This is port of enableDisableCellular test network_ChromeCellularEndToEnd tauto test
    
    BUG=b:220027306
    TEST=tast run cellular.MojoEnsureNetworkStateAfterCellularToggle on Limozeen with Ethernet connected
    
    Change-Id: I75216d27da4508ef1101a461f3fa0589f41e9046

---
+++ src/chromiumos/tast/local/bundles/cros/cellular/mojo_ensure_network_state_after_cellular_toggle.go
@@ -0,0 +1,119 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/cellular"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/network/netconfig"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MojoEnsureNetworkStateAfterCellularToggle,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Enable/disable Cellular service using Mojo and check WiFi/Ethernet are not affected",
+		Contacts:     []string{"shijinabraham@google.com", "cros-network-health@google.com", "chromeos-cellular-team@google.com"},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:cellular", "cellular_unstable"},
+		Timeout:      10 * time.Minute,
+		Fixture:      "cellular",
+	})
+}
+
+// compareDeviceStatelist compare the network state before starting the test with the state during the test.
+func compareDeviceStatelist(orgDeviceState, curDeviceState []netconfig.DeviceStateProperties, enabled bool) error {
+	for _, cfg := range curDeviceState {
+		deviceType := cfg.Type
+		deviceState := cfg.DeviceState
+		// check cellular device against the known status
+		if deviceType == netconfig.Cellular {
+			if enabled {
+				if deviceState != netconfig.EnabledDST {
+					return errors.Errorf("unexpected Cellular state expected: enabled got: %s", deviceState)
+				}
+			} else {
+				if deviceState != netconfig.DisabledDST {
+					return errors.Errorf("unexpected Cellular state expected: disabled got:", deviceState)
+				}
+			}
+		}
+
+		// Checking only Ethernet and WiFi
+		if deviceType != netconfig.Ethernet && deviceType != netconfig.WiFi {
+			continue
+		}
+
+		// Ethernet and WiFi should maintain the original state
+		for _, cfg := range orgDeviceState {
+			if cfg.Type == deviceType && cfg.DeviceState != deviceState {
+				return errors.Errorf("unexpected state for device: %d Expected: %d got: %d", cfg.Type, cfg.DeviceState, deviceState)
+			}
+		}
+	}
+	return nil
+}
+
+// MojoEnsureNetworkStateAfterCellularToggle enables/distable cellular device using Mojo and confirms using mojo that WiFi/Ethernet are not affected.
+func MojoEnsureNetworkStateAfterCellularToggle(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	helper, err := cellular.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create cellular.Helper: ", err)
+	}
+
+	// Enable Wifi
+	wifiManager, err := shill.NewWifiManager(ctx, nil)
+	if err != nil {
+		s.Fatal("Failed to create shill Wi-Fi manager: ", err)
+	}
+	if err := wifiManager.Enable(ctx, true); err != nil {
+		s.Fatal("Failed to enable Wi-Fi: ", err)
+	}
+
+	netConn, err := netconfig.NewCrosNetworkConfig(ctx, cr)
+	if err != nil {
+		s.Fatal("Failed to get network Mojo Object: ", err)
+	}
+	defer netConn.Close(ctx)
+
+	orgDeviceState, err := netConn.GetDeviceStateList(ctx)
+	if err != nil {
+		s.Fatal("Failed to get DeviceStateList: ", err)
+	}
+
+	const iterations = 5
+	for i := 0; i < iterations; i++ {
+		enabled := i%2 != 0
+		s.Logf("Toggling Cellular state to %t (iteration %d of %d)", enabled, i+1, iterations)
+
+		if err = netConn.SetNetworkTypeEnabledState(ctx, netconfig.Cellular, enabled); err != nil {
+			s.Fatal("Failed to set cellular state: ", err)
+		}
+
+		if err = helper.WaitForEnabledState(ctx, enabled); err != nil {
+			s.Fatal("cellular state is not as expected: ", err)
+		}
+
+		curDeviceState, err := netConn.GetDeviceStateList(ctx)
+		if err != nil {
+			s.Fatal("Failed to get DeviceStateList: ", err)
+		}
+		if err := compareDeviceStatelist(orgDeviceState, curDeviceState, enabled); err != nil {
+			s.Fatal("Device state comparsion failed: ", err)
+		}
+	}
+}
