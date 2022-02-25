b106537ab4698da43a9a9bf051ee73edcbd8d631
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-02-24 16:20:51 -0800

    dev.RemoteDesktop
    BUG=None
    TEST=tast run -checktestdeps=false ${DUT} dev.RemoteDesktop.lacros_test
    TEST=tast run -checktestdeps=false ${DUT} dev.RemoteDesktop.test
    TEST=tast run -checktestdeps=false -var=user=<user> -var=pass=<pass> ${DUT} dev.RemoteDesktop
    
    Change-Id: I6110ac93d79de9255f8ee94938ec24ea6da476bf

--- src/chromiumos/tast/local/bundles/cros/dev/remote_desktop.go
+++ src/chromiumos/tast/local/bundles/cros/dev/remote_desktop.go
@@ -7,24 +7,38 @@ package dev
 import (
 	"context"
 	"strconv"
-	"strings"
+	"time"
 
-	"chromiumos/tast/errors"
-	"chromiumos/tast/local/arc"
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
 	"chromiumos/tast/local/chrome/uiauto/crd"
+	"chromiumos/tast/local/dev"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
 
-// rdpVars represents the configurable parameters for the remote desktop session.
-type rdpVars struct {
-	user      string
-	pass      string
-	contact   string
-	wait      bool
-	reset     bool
-	extraArgs []string
+type testParam struct {
+	browserType browser.Type
+	manual      bool
+}
+
+func shouldWait(s *testing.State, manual bool) bool {
+	waitStr, ok := s.Var("wait")
+	if !ok {
+		// Only wait for remote connection when running manually.
+		if manual {
+			waitStr = "true"
+		} else {
+			waitStr = "false"
+		}
+	}
+	wait, err := strconv.ParseBool(waitStr)
+	if err != nil {
+		s.Fatal("Failed to parse the variable `wait`: ", err)
+	}
+	return wait
 }
 
 func init() {
@@ -39,136 +53,79 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		Vars: []string{
 			// For running manually.
-			"user", "pass", "contact", "wait", "extra_args", "reset",
-		},
-		VarDeps: []string{
-			// For automated testing.
-			"dev.username", "dev.password",
+			"wait",
 		},
 		Params: []testing.Param{{
 			// For running manually.
-			Name: "",
+			Name:    "",
+			Fixture: dev.ChromeLoggedIn,
+			Val:     testParam{browserType: browser.TypeAsh, manual: true},
 		}, {
 			// For automated testing.
 			Name:      "test",
+			Fixture:   dev.ChromeLoggedIn,
 			ExtraAttr: []string{"group:mainline", "informational"},
 			// TODO(b/151111783): This is a speculative fix to limit the number of sessions. It
 			// seems that the test account is throttled by the CRD backend, so the test is failing
 			// with a periodic pattern. The model list is handcrafted to cover various platforms.
 			ExtraHardwareDeps: hwdep.D(hwdep.Model("atlas", "careena", "dru", "eve", "kohaku",
 				"krane", "nocturne")),
+			Val: testParam{browserType: browser.TypeAsh, manual: false},
+		}, {
+			// For automated testing with lacros.
+			Name:              "lacros_test",
+			Fixture:           dev.LacrosLoggedIn,
+			ExtraAttr:         []string{"group:mainline", "informational"},
+			ExtraSoftwareDeps: []string{"lacros"},
+			// TODO(b/151111783): This is a speculative fix to limit the number of sessions. It
+			// seems that the test account is throttled by the CRD backend, so the test is failing
+			// with a periodic pattern. The model list is handcrafted to cover various platforms.
+			ExtraHardwareDeps: hwdep.D(hwdep.Model("atlas", "careena", "dru", "eve", "kohaku",
+				"krane", "nocturne")),
+			Val: testParam{browserType: browser.TypeLacros, manual: false},
 		}},
 	})
-}
-
-// getVars extracts the testing parameters from testing.State. The user
-// provided credentials would override the credentials from config file.
-func getVars(s *testing.State) rdpVars {
-	user, hasUser := s.Var("user")
-	if !hasUser {
-		user = s.RequiredVar("dev.username")
-	}
-
-	pass, hasPass := s.Var("pass")
-	if !hasPass {
-		pass = s.RequiredVar("dev.password")
-	}
-
-	contact, hasContact := s.Var("contact")
-
-	// Manual test requires username + password/contact.
-	manual := false
-	if hasUser && (hasPass || hasContact) {
-		manual = true
-	}
-
-	resetStr, ok := s.Var("reset")
-	if !ok {
-		resetStr = "false"
-	}
-	reset, err := strconv.ParseBool(resetStr)
-	if err != nil {
-		s.Fatal("Failed to parse the variable `reset`: ", err)
-	}
-
-	waitStr, ok := s.Var("wait")
-	if !ok {
-		// Only wait for remote connection when running manually.
-		if manual {
-			waitStr = "true"
-		} else {
-			waitStr = "false"
-		}
-	}
-	wait, err := strconv.ParseBool(waitStr)
-	if err != nil {
-		s.Fatal("Failed to parse the variable `wait`: ", err)
-	}
-
-	extraArgsStr, ok := s.Var("extra_args")
-	if !ok {
-		extraArgsStr = ""
-	}
-	extraArgs := strings.Fields(extraArgsStr)
-
-	return rdpVars{
-		user:      user,
-		pass:      pass,
-		contact:   contact,
-		wait:      wait,
-		reset:     reset,
-		extraArgs: extraArgs,
-	}
 }
 
 func RemoteDesktop(ctx context.Context, s *testing.State) {
 	// TODO(shik): The button names only work in English locale, and adding
 	// "lang=en-US" for Chrome does not work.
 
-	vars := getVars(s)
-	var opts []chrome.Option
+	// Save a few seconds for cleanup
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
 
-	chromeARCOpt := chrome.ARCDisabled()
-	if arc.Supported() {
-		chromeARCOpt = chrome.ARCSupported()
-	}
-	opts = append(opts, chromeARCOpt)
-	opts = append(opts, chrome.GAIALogin(chrome.Creds{
-		User:    vars.user,
-		Pass:    vars.pass,
-		Contact: vars.contact,
-	}))
-
-	if !vars.reset {
-		opts = append(opts, chrome.KeepState())
-	}
-
-	if len(vars.extraArgs) > 0 {
-		opts = append(opts, chrome.ExtraArgs(vars.extraArgs...))
-	}
-
-	cr, err := chrome.New(ctx, opts...)
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	bt := s.Param().(testParam).browserType
+	br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), bt)
 	if err != nil {
-		// In case of authentication error, provide a more informative message to the user.
-		if strings.Contains(err.Error(), "chrome.Auth") {
-			err = errors.Wrap(err, "please supply a password with -var=pass=<password>")
-		} else if strings.Contains(err.Error(), "chrome.Contact") {
-			err = errors.Wrap(err, "please supply a contact email with -var=contact=<contact>")
-		}
-		s.Fatal("Failed to start Chrome: ", err)
+		s.Fatalf("Failed to open the %v browser: %v", bt, err)
 	}
-	defer cr.Close(ctx)
+	defer closeBrowser(cleanupCtx)
+
+	// TODO:
+	// if err != nil {
+	// 	// In case of authentication error, provide a more informative message to the user.
+	// 	if strings.Contains(err.Error(), "chrome.Auth") {
+	// 		err = errors.Wrap(err, "please supply a password with -var=pass=<password>")
+	// 	} else if strings.Contains(err.Error(), "chrome.Contact") {
+	// 		err = errors.Wrap(err, "please supply a contact email with -var=contact=<contact>")
+	// 	}
+	// 	s.Fatal("Failed to start Chrome: ", err)
+	// }
+	// defer cr.Close(ctx)
 
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to connect Test API: ", err)
 	}
 
-	if err := crd.Launch(ctx, cr, tconn); err != nil {
+	if err := crd.Launch(ctx, br, tconn); err != nil {
 		s.Fatal("Failed to Launch: ", err)
 	}
 
-	if vars.wait {
+	if shouldWait(s, s.Param().(testParam).manual) {
 		s.Log("Waiting connection")
 		if err := crd.WaitConnection(ctx, tconn); err != nil {
 			s.Fatal("No client connected: ", err)
