83e0de6975944a1de809edd3957a60920b9ff513
Jun Sheng <shengjun@google.com>
2022-02-25 17:31:57 +1100

    tast-tests: add test on IME tray input function
    A few input modalities can be triggered from IME tray. This test validates Voice & Handwriting & Emoji function test by triggering from IME tray.
    
    BUG=b:221364640
    TEST=tast run <DUT> inputs.InputMethodShelfInputs*
    
    Change-Id: I2970fd3946cc0d77a290c3d9557726c301cea295

---
+++ src/chromiumos/tast/local/bundles/cros/inputs/input_method_shelf_inputs.go
@@ -0,0 +1,224 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package inputs
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/inputs/data"
+	"chromiumos/tast/local/bundles/cros/inputs/pre"
+	"chromiumos/tast/local/bundles/cros/inputs/testserver"
+	"chromiumos/tast/local/bundles/cros/inputs/util"
+	"chromiumos/tast/local/chrome/ime"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/imesettings"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/chrome/uiauto/vkb"
+	"chromiumos/tast/local/chrome/useractions"
+	"chromiumos/tast/local/input/voice"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+var testMessages = []data.Message{
+	data.VoiceMessageHello,
+	data.HandwritingMessageHello,
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         InputMethodShelfInputs,
+		LacrosStatus: testing.LacrosVariantNeeded,
+		Desc:         "Test voice input from Optin menu",
+		Contacts:     []string{"shengjun@chromium.org", "essential-inputs-team@google.com"},
+		SoftwareDeps: []string{"chrome", "google_virtual_keyboard"},
+		Attr:         []string{"group:mainline", "group:input-tools", "informational"},
+		Data:         data.ExtractExternalFiles(testMessages, []ime.InputMethod{ime.DefaultInputMethod}),
+		Timeout:      5 * time.Minute,
+		Params: []testing.Param{{
+			Pre:               pre.NonVKClamshellReset,
+			ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
+		}, {
+			Name:              "informational",
+			Pre:               pre.NonVKClamshellReset,
+			ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
+		}},
+	})
+}
+
+func InputMethodShelfInputs(ctx context.Context, s *testing.State) {
+	cr := s.PreValue().(pre.PreData).Chrome
+	tconn := s.PreValue().(pre.PreData).TestAPIConn
+	uc := s.PreValue().(pre.PreData).UserContext
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree")
+
+	// Setup CRAS Aloop for audio test.
+	cleanup, err := voice.EnableAloop(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to load Aloop: ", err)
+	}
+	defer cleanup(ctx)
+
+	if err := imesettings.EnableInputOptionsInShelf(uc, true).Run(ctx); err != nil {
+		s.Fatal("Failed to show input options in shelf: ", err)
+	}
+
+	its, err := testserver.Launch(ctx, cr, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch inputs test server: ", err)
+	}
+	defer its.Close()
+
+	inputField := testserver.TextAreaInputField
+	testIME := ime.DefaultInputMethod
+
+	testName := "voice_input"
+	testDescription := "verify voice input triggered from opt-in menu"
+	voiceInputData, ok := data.VoiceMessageHello.GetInputData(testIME)
+	if !ok {
+		s.Fatal("Failed to get voice test data of input method: ", testIME)
+	}
+
+	ui := uiauto.New(tconn)
+	imeMenuTrayButtonFinder := nodewith.Name("IME menu button").Role(role.Button)
+	voiceInputItem := nodewith.Name("Voice").HasClass("SystemMenuButton")
+	voicePrivacyConfirmButton := nodewith.Name("Got it").HasClass("voice-got-it")
+	handwritingInputItem := nodewith.Name("Handwriting").HasClass("SystemMenuButton")
+	emojiInputMenuItem := nodewith.Name("Emojis").HasClass("SystemMenuButton")
+
+	verifyAudioInputAction := uiauto.Combine(testDescription,
+		its.Clear(inputField),
+		uiauto.New(tconn).Sleep(time.Second),
+		its.ClickFieldAndWaitForActive(inputField),
+		ui.LeftClick(imeMenuTrayButtonFinder),
+		ui.LeftClick(voiceInputItem),
+		ui.LeftClick(voicePrivacyConfirmButton),
+		ui.Sleep(1*time.Second),
+		func(ctx context.Context) error {
+			return voice.AudioFromFile(ctx, s.DataPath(voiceInputData.VoiceFile))
+		},
+		util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), voiceInputData.ExpectedText),
+	)
+
+	s.Run(ctx, testName, func(ctx context.Context, s *testing.State) {
+		defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+testName)
+
+		if err := useractions.NewUserAction("Voice input",
+			verifyAudioInputAction,
+			uc,
+			&useractions.UserActionCfg{
+				Attributes: map[string]string{
+					useractions.AttributeInputField:   string(inputField),
+					useractions.AttributeTestScenario: "Validate voice input triggered from opt-in menu",
+				},
+				Tags: []useractions.ActionTag{
+					useractions.ActionTagVKVoiceInput,
+					useractions.ActionTagIMEShelf,
+				},
+				Callback: func(ctx context.Context, actionErr error) error {
+					vkbCtx := vkb.NewContext(cr, tconn)
+					return vkbCtx.HideVirtualKeyboard()(ctx)
+				},
+			},
+		).Run(ctx); err != nil {
+			s.Fatal("Failed to validate voice input: ", err)
+		}
+	})
+
+	testName = "handwriting"
+	testDescription = "verify voice input triggered from opt-in menu"
+	hwInputData, ok := data.HandwritingMessageHello.GetInputData(testIME)
+	if !ok {
+		s.Fatal("Failed to get handwriting test data of input method: ", testIME)
+	}
+
+	hwFilePath := s.DataPath(hwInputData.HandwritingFile)
+	verifyHandWritingInputAction := uiauto.Combine(testDescription,
+		its.Clear(inputField),
+		uiauto.New(tconn).Sleep(time.Second),
+		its.ClickFieldAndWaitForActive(inputField),
+		ui.LeftClick(imeMenuTrayButtonFinder),
+		ui.LeftClick(handwritingInputItem),
+		func(ctx context.Context) error {
+			hwCtx, err := vkb.NewContext(cr, tconn).NewHandwritingContext(ctx)
+			if err != nil {
+				return errors.Wrap(err, "failed to initiate handwriting context")
+			}
+			return uiauto.Combine("handwriting input",
+				its.WaitForHandwritingEngineReadyOnField(hwCtx, inputField, hwFilePath),
+				hwCtx.DrawStrokesFromFile(hwFilePath),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), hwInputData.ExpectedText),
+			)(ctx)
+		},
+	)
+
+	s.Run(ctx, testName, func(ctx context.Context, s *testing.State) {
+		defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+testName)
+		if err := useractions.NewUserAction("VK handwriting",
+			verifyHandWritingInputAction,
+			uc,
+			&useractions.UserActionCfg{
+				Attributes: map[string]string{
+					useractions.AttributeInputField:   string(inputField),
+					useractions.AttributeTestScenario: "Validate handwriting input triggered from opt-in menu",
+				},
+				Tags: []useractions.ActionTag{
+					useractions.ActionTagVKHandWriting,
+					useractions.ActionTagIMEShelf,
+				},
+				Callback: func(ctx context.Context, actionErr error) error {
+					vkbCtx := vkb.NewContext(cr, tconn)
+					return vkbCtx.HideVirtualKeyboard()(ctx)
+				},
+			},
+		).Run(ctx); err != nil {
+			s.Fatal("Failed to validate handwriting input: ", err)
+		}
+	})
+
+	testName = "emoji"
+	testDescription = "verify emoji input triggered from opt-in menu"
+	inputEmoji := "😄"
+	emojiPickerFinder := nodewith.Name("Emoji Picker").Role(role.RootWebArea)
+	emojiItem := nodewith.Name(inputEmoji).Ancestor(emojiPickerFinder).First()
+
+	verifyEmojiInputAction := uiauto.Combine(testDescription,
+		its.Clear(inputField),
+		uiauto.New(tconn).Sleep(time.Second),
+		its.ClickFieldAndWaitForActive(inputField),
+		ui.LeftClick(imeMenuTrayButtonFinder),
+		ui.LeftClick(emojiInputMenuItem),
+		ui.LeftClick(emojiItem),
+		util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), inputEmoji),
+	)
+
+	s.Run(ctx, testName, func(ctx context.Context, s *testing.State) {
+		defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+testName)
+		if err := useractions.NewUserAction(
+			"Input Emoji with Emoji Picker",
+			verifyEmojiInputAction,
+			uc,
+			&useractions.UserActionCfg{
+				Attributes: map[string]string{useractions.AttributeInputField: string(inputField), useractions.AttributeTestScenario: "Input emoji by emoji picker via IME tray"},
+				Tags: []useractions.ActionTag{
+					useractions.ActionTagEmoji,
+					useractions.ActionTagEmojiPicker,
+					useractions.ActionTagIMEShelf,
+				},
+			}).Run(ctx); err != nil {
+			s.Fatal("Failed to validate emoji input: ", err)
+		}
+	})
+}
--- src/chromiumos/tast/local/chrome/uiauto/imesettings/user_actions.go
+++ src/chromiumos/tast/local/chrome/uiauto/imesettings/user_actions.go
@@ -12,6 +12,8 @@ import (
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome/ime"
 	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/chrome/useractions"
 	"chromiumos/tast/local/input"
 )
@@ -241,6 +243,48 @@ func SetVKAutoCapitalization(uc *useractions.UserContext, im ime.InputMethod, is
 				useractions.ActionTagAutoCapitalization,
 			},
 		})
+}
+
+// EnableInputOptionsInShelf returns a user action to change IME setting
+// to show / hide IME options in shelf.
+func EnableInputOptionsInShelf(uc *useractions.UserContext, shown bool) *useractions.UserAction {
+	ui := uiauto.New(uc.TestAPIConn())
+	imeMenuTrayButtonFinder := nodewith.Name("IME menu button").Role(role.Button)
+
+	action := func(ctx context.Context) error {
+		setting, err := LaunchAtInputsSettingsPage(ctx, uc.TestAPIConn(), uc.Chrome())
+		if err != nil {
+			return errors.Wrap(err, "failed to launch input settings")
+		}
+
+		return uiauto.Combine("test input method settings change",
+			setting.ToggleShowInputOptionsInShelf(uc.Chrome(), shown),
+			func(ctx context.Context) error {
+				if shown {
+					return ui.WaitUntilExists(imeMenuTrayButtonFinder)(ctx)
+				}
+				return ui.WaitUntilGone(imeMenuTrayButtonFinder)(ctx)
+			},
+			setting.Close,
+		)(ctx)
+	}
+
+	userActionName := "Show input options in the shelf"
+	if !shown {
+		userActionName = "Hide input options in the shelf"
+	}
+	return useractions.NewUserAction(
+		userActionName,
+		action,
+		uc,
+		&useractions.UserActionCfg{
+			Tags: []useractions.ActionTag{
+				useractions.ActionTagEssentialInputs,
+				useractions.ActionTagOSSettings,
+				useractions.ActionTagIMESettings,
+			},
+		},
+	)
 }
 
 // SetKoreanKeyboardLayout returns a user action to change 'Korean keyboard layout' setting.
