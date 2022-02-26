5bc0404f2fbf8097b30a9670b88a95bf9118d73b
pjlee <pjlee@google.com>
2022-02-26 01:20:22 +0000

    arcgameperf: ACUITI Roblox login detection
    Implement an alternate Roblox login test using ACUITI.
    
    BUG=b:221252582
    TEST=TEST=tast run $DUT arcappgameperf.RobloxACUITI*
    
    Change-Id: I2d4873d8b1ec70ad8c6f11cc499615b9d6038dca

---
+++ src/chromiumos/tast/local/bundles/cros/arcappgameperf/roblox_acuiti_login.go
@@ -0,0 +1,121 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arcappgameperf
+
+import (
+	"context"
+	"regexp"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/action"
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappgameperf/pre"
+	"chromiumos/tast/local/bundles/cros/arcappgameperf/testutil"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/uidetection"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RobloxACUITILogin,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Captures login metrics for Roblox",
+		Contacts:     []string{"davidwelling@google.com", "pjlee@google.com", "arc-engprod@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_nightly"},
+		SoftwareDeps: []string{"chrome"},
+		// TODO(b/206442649): Remove after initial testing is complete.
+		HardwareDeps: hwdep.D(hwdep.Model(testutil.ModelsToTest()...)),
+		Params: []testing.Param{
+			{
+				ExtraSoftwareDeps: []string{"android_p"},
+				Pre:               pre.ArcAppGamePerfBooted,
+			}, {
+				Name:              "vm",
+				ExtraSoftwareDeps: []string{"android_vm"},
+				Pre:               pre.ArcAppGamePerfBooted,
+			}},
+		Timeout: 10 * time.Minute,
+		VarDeps: []string{"arcappgameperf.username", "arcappgameperf.password", "arcappgameperf.roblox_username", "arcappgameperf.roblox_password"},
+	})
+}
+
+func RobloxACUITILogin(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.roblox.client"
+		appActivity = ".startup.ActivitySplash"
+		// The inputs rendered by Roblox are not immediately active after being clicked
+		// so wait a moment for the engine to make the input active before interacting with it.
+		waitForActiveInputTime = time.Second * 5
+		// Stores how long the game should be benchmarked.
+		gameBenchmarkTime = time.Minute * 1
+	)
+
+	testutil.PerformTest(ctx, s, appPkgName, appActivity, func(params testutil.TestParams) error {
+		// Get Username and Password for Roblox.
+		username := s.RequiredVar("arcappgameperf.roblox_username")
+		password := s.RequiredVar("arcappgameperf.roblox_password")
+
+		// Start up keyboard and ACUITI.
+		kb, err := input.VirtualKeyboard(ctx)
+		if err != nil {
+			return errors.Wrap(err, "failed to open virtual keyboard")
+		}
+		defer kb.Close()
+
+		uda := uidetection.NewDefault(params.TestConn).WithOptions(uidetection.Retries(3)).WithTimeout(time.Minute)
+
+		// Make sure Roblox is launched.
+		if err := uiauto.Combine("Confirm launch",
+			uda.WithTimeout(time.Minute*3).WaitUntilExists(uidetection.Word("Privacy")),
+		)(ctx); err != nil {
+			return errors.Wrap(err, "failed to confirm launch")
+		}
+
+		if err := uiauto.Combine("Enter login information",
+			// Click login button.
+			uda.Tap(uidetection.TextBlock([]string{"Log", "In"})),
+
+			// Click username field and enter username.
+			uda.Tap(uidetection.Word("Username/Email/Phone")),
+			action.Sleep(waitForActiveInputTime),
+			kb.TypeAction(username),
+
+			// Click password field and enter password.
+			uda.Tap(uidetection.Word("Password").First()),
+			action.Sleep(waitForActiveInputTime),
+			kb.TypeAction(password),
+
+			// Click the log in button.
+			uda.Tap(uidetection.TextBlock(strings.Split("Log In", " ")).First()),
+		)(ctx); err != nil {
+			return errors.Wrap(err, "failed to finish test")
+		}
+
+		// Start timer for metrics.
+		startTime := time.Now()
+
+		// onAppReady: AvatarExperienceLandingPage will appear in logcat after the game is fully logged in.
+		if err := params.Arc.WaitForLogcat(ctx, arc.RegexpPred(regexp.MustCompile(`onAppReady:\sAvatarExperienceLandingPage`))); err != nil {
+			return errors.Wrap(err, "\"onAppReady: AvatarExperienceLandingPage\" was not found in LogCat")
+		}
+
+		// Save the metric in crosbolt.
+		loginTime := time.Now().Sub(startTime)
+		perfValues := perf.NewValues()
+		perfValues.Set(testutil.LoginTimePerfMetric(), loginTime.Seconds())
+
+		if err := perfValues.Save(s.OutDir()); err != nil {
+			return errors.Wrap(err, "failed to save performance values")
+		}
+
+		return nil
+	})
+}
