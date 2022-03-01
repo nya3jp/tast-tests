9fb0e3b1b9271e9cc1af5fd57be9fb32f8260882
Emmanuel Arias Soto <eariassoto@google.com>
2022-03-01 09:49:16 +0000

    tast-tests: Add test for DefaultJavascriptJitSetting
    BUG=chromium:1288296
    TEST=tast run <DUT ip> policy.DefaultJavascriptJitSetting
    
    Change-Id: I8bf6e707db7f746a454841350495b8bb73c8f32e

---
+++ src/chromiumos/tast/local/bundles/cros/policy/default_javascript_jit_setting.go
@@ -0,0 +1,126 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DefaultJavascriptJitSetting,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Check that the DefaultJavaScriptJitSetting policy blocks or allows the JIT compiler",
+		Contacts: []string{
+			"eariassoto@google.com", // Test author
+			"chromeos-commercial-remote-management@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Params: []testing.Param{{
+			Fixture: fixture.ChromePolicyLoggedIn,
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Fixture:           fixture.LacrosPolicyLoggedIn,
+			Val:               browser.TypeLacros,
+		}},
+		Data: []string{"jit_test.html", "is_jit_enabled.wasm"},
+	})
+}
+
+func DefaultJavascriptJitSetting(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	// Reserve ten seconds for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer server.Close()
+
+	for _, tc := range []struct {
+		name  string
+		value *policy.DefaultJavaScriptJitSetting
+		allow bool
+	}{
+		{
+			name:  "allow",
+			value: &policy.DefaultJavaScriptJitSetting{Val: 1},
+			allow: true,
+		},
+		{
+			name:  "block",
+			value: &policy.DefaultJavaScriptJitSetting{Val: 2},
+			allow: false,
+		},
+		{
+			name:  "unset",
+			value: &policy.DefaultJavaScriptJitSetting{Stat: policy.StatusUnset},
+			allow: true,
+		},
+	} {
+		s.Run(ctx, tc.name, func(ctx context.Context, s *testing.State) {
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Error("Failed to reset Chrome: ", err)
+			}
+
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{tc.value}); err != nil {
+				s.Error("Failed to serve and verify policies: ", err)
+			}
+
+			// TODO(crbug.com/1259615): This should be part of the fixture.
+			br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(browser.Type))
+			if err != nil {
+				s.Error("Failed to setup chrome: ", err)
+			}
+			defer closeBrowser(cleanupCtx)
+
+			conn, err := br.NewConn(ctx, server.URL+"/jit_test.html")
+			if err != nil {
+				s.Error("Failed to connect to Chrome: ", err)
+			}
+			defer conn.Close()
+
+			jitAllowed := false
+			// We need this error to let Poll execute until the condition is true or ultimately fail after the polling timeout.
+			jitEvalFalseErr := errors.New("isJitEnabled() evaluated to false")
+			if err := testing.Poll(ctx, func(ctx context.Context) error {
+				if err := conn.Eval(ctx, "isJitEnabled()", &jitAllowed); err != nil {
+					return testing.PollBreak(err)
+				}
+				if !jitAllowed {
+					return jitEvalFalseErr
+				}
+				return nil
+			}, &testing.PollOptions{Interval: 10 * time.Millisecond, Timeout: 1 * time.Second}); err != nil {
+				if !errors.Is(err, jitEvalFalseErr) {
+					s.Error("Could not evaluate function isJitEnabled: ", err)
+				}
+			}
+
+			if jitAllowed != tc.allow {
+				s.Errorf("Unexpected JIT compiler status: got %v, want %v", jitAllowed, tc.allow)
+			}
+		})
+	}
+}
