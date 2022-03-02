cb98bbbabbe3c8a8f16c38ae5e1c10b0da40cae7
Pathan Jilani <pathan.jilani@intel.com>
2022-03-02 14:56:26 +0530

    touchpad: Add FunctionalityCheck
    Verify Touchpad primary button clicks.
    
    BUG=b:195902740
    
    TEST: tast run --build=true <DUTIP> touchpad.FunctionalityCheck.clamshell_mode
    
    Change-Id: Ia8318b7bee079c3d956a1e2d3f0f040f540c0fc0

--- src/chromiumos/tast/local/bundles/cros/imports.go
+++ src/chromiumos/tast/local/bundles/cros/imports.go
@@ -94,6 +94,7 @@ import (
 	_ "chromiumos/tast/local/bundles/cros/storage"
 	_ "chromiumos/tast/local/bundles/cros/system"
 	_ "chromiumos/tast/local/bundles/cros/telemetryextension"
+	_ "chromiumos/tast/local/bundles/cros/touchpad"
 	_ "chromiumos/tast/local/bundles/cros/typec"
 	_ "chromiumos/tast/local/bundles/cros/u2fd"
 	_ "chromiumos/tast/local/bundles/cros/ui"
