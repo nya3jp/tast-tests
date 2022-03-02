8c36ea8c06fb89481d94f1afd37ef7a45c9efcd9
Pathan Jilani <pathan.jilani@intel.com>
2022-03-02 14:56:26 +0530

    touchpad: Add FunctionalityCheck
    Verify Touchpad primary button clicks.
    
    BUG=b:195902740
    
    TEST: tast run --build=true <DUTIP> touchpad.FunctionalityCheck.clamshell_mode
    
    Change-Id: Ia8318b7bee079c3d956a1e2d3f0f040f540c0fc0

--- src/chromiumos/tast/local/chrome/ash/tablet.go
+++ src/chromiumos/tast/local/chrome/ash/tablet.go
@@ -6,7 +6,11 @@ package ash
 
 import (
 	"context"
+	"regexp"
+	"strconv"
 
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 )
 
@@ -50,5 +54,58 @@ func EnsureTabletModeEnabled(ctx context.Context, tconn *chrome.TestConn, enable
 	// state even when the state changes in another part of the test script.
 	return func(ctx context.Context) error {
 		return SetTabletModeEnabled(ctx, tconn, originallyEnabled)
+	}, nil
+}
+
+// SetTabletModeUsingEC sets tabletModeAngle for provided lidAngle, hysAngle.
+func SetTabletModeUsingEC(ctx context.Context, lidAngle, hysAngle int) error {
+	tabletLidAngle := strconv.Itoa(lidAngle)
+	tabletHysAngle := strconv.Itoa(hysAngle)
+	if err := testexec.CommandContext(ctx, "ectool", "motionsense", "tablet_mode_angle", tabletLidAngle, tabletHysAngle).Run(); err != nil {
+		return errors.Wrap(err, "failed to execute tablet_mode_angle command")
+	}
+	return nil
+}
+
+// ModeValues returns tabletModeAngle values.
+func ModeValues(ctx context.Context) (int, int, error) {
+	re := regexp.MustCompile(`tablet_mode_angle=(\d+) hys=(\d+)`)
+	out, err := testexec.CommandContext(ctx, "ectool", "motionsense", "tablet_mode_angle").Output()
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "failed to retrieve tablet_mode_angle settings")
+	}
+	m := re.FindSubmatch(out)
+	if len(m) != 3 {
+		return 0, 0, errors.Wrapf(err, "failed to get initial tablet_mode_angle settings: got submatches %+v", m)
+	}
+	initLidAngle, err := strconv.Atoi(string(m[1]))
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "failed to convert initLidAngle to integer")
+	}
+	initHys, err := strconv.Atoi(string(m[2]))
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "failed to convert initHys to integer")
+	}
+	return initLidAngle, initHys, nil
+}
+
+// EnsureTabletModeEnabledWithKeyboardDisabled makes sure that the tablet mode state is enabled using EC tool,
+// which takes care of disabling the Keyboard and Trackpad.
+// It returns a function which reverts back to the original state.
+func EnsureTabletModeEnabledWithKeyboardDisabled(ctx context.Context, lidAngle, hysAngle int) (func(ctx context.Context) error, error) {
+	// Get the initial tablet_mode_angle settings to restore at the end of test.
+	initLidAngle, initHys, err := ModeValues(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get initial tablet_mode_angle values")
+	}
+	if initLidAngle != lidAngle || initHys != hysAngle {
+		if err = SetTabletModeUsingEC(ctx, lidAngle, hysAngle); err != nil {
+			return nil, errors.Wrap(err, "failed to set DUT to tablet mode")
+		}
+	}
+	// Always revert to the original state; so it can always be back to the original
+	// state even when the state changes in another part of the test script.
+	return func(ctx context.Context) error {
+		return SetTabletModeUsingEC(ctx, initLidAngle, initHys)
 	}, nil
 }
