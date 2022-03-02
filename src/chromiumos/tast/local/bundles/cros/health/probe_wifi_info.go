c00d472bd30de78440880e498df394abcdebed85
Pathan Jilani <pathan.jilani@intel.com>
2022-03-02 12:17:49 +0530

    health: Verify wirelessLinkInfo
    If Wifi router is connected verify both host and router data.
    
    Sample command to execute:
    tast run <DUT IP> health.ProbeWifiInfo
    
    BUG=b:222016691
    TEST=Ran on DUT locally
    
    Change-Id: I6149d0aa3c4c34ed402686b6c98037b24bb05ad6

--- src/chromiumos/tast/local/bundles/cros/health/probe_wifi_info.go
+++ src/chromiumos/tast/local/bundles/cros/health/probe_wifi_info.go
@@ -14,9 +14,20 @@ import (
 	"chromiumos/tast/testing"
 )
 
+type wirelessLinkInfo struct {
+	AccessPointAddress string `json:"access_point_address_str"`
+	EncyptionOn        bool   `json:"encyption_on"`
+	LinkQuality        string `json:"link_quality"`
+	RxBitRateMbps      string `json:"rx_bit_rate_mbps"`
+	SignalLevelDBm     int    `json:"signal_level_dBm"`
+	TxBitRateMbps      string `json:"tx_bit_rate_mbps"`
+	TxPowerDBm         int    `json:"tx_power_dBm"`
+}
+
 type wirelessInteface struct {
-	InterfaceName     string `json:"interface_name"`
-	PowerManagementOn bool   `json:"power_management_on"`
+	InterfaceName     string            `json:"interface_name"`
+	LinkInfo          *wirelessLinkInfo `json:"link_info"`
+	PowerManagementOn bool              `json:"power_management_on"`
 }
 
 type networkInterface struct {
@@ -39,8 +50,7 @@ func init() {
 		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "diagnostics"},
-
-		Fixture: "crosHealthdRunning",
+		Fixture:      "crosHealthdRunning",
 	})
 }
 
@@ -69,6 +79,42 @@ func ProbeWifiInfo(ctx context.Context, s *testing.State) {
 			} else {
 				if ifc.WirelessInterfaces.PowerManagementOn {
 					s.Fatal("Failed to get PowerManagementOn value as false when power_scheme not present")
+				}
+			}
+
+			// Check whether router connected or not.
+			iwconfigOut, err := testexec.CommandContext(ctx, "iwconfig").Output()
+			if err != nil {
+				s.Fatal("Failed to execute iwconfig command: ", err)
+			}
+			// Check whether 'Access Point: Not-Associated' is presented or not.
+			want = "Access Point: Not-Associated"
+			isRouterconnected := !strings.Contains(string(iwconfigOut), want)
+			if isRouterconnected {
+				if ifc.WirelessInterfaces.LinkInfo.AccessPointAddress == "" {
+					s.Fatal("Failed to get AccessPointAddress")
+				}
+				if !ifc.WirelessInterfaces.LinkInfo.EncyptionOn {
+					s.Fatal("Failed to get EncyptionOn")
+				}
+				if ifc.WirelessInterfaces.LinkInfo.LinkQuality == "" {
+					s.Fatal("Failed to get LinkQuality")
+				}
+				if ifc.WirelessInterfaces.LinkInfo.RxBitRateMbps == "" {
+					s.Fatal("Failed to get RxBitRateMbps")
+				}
+				if ifc.WirelessInterfaces.LinkInfo.SignalLevelDBm > 0 {
+					s.Fatal("Failed to get SignalLevelDBm")
+				}
+				if ifc.WirelessInterfaces.LinkInfo.TxBitRateMbps == "" {
+					s.Fatal("Failed to get TxBitRateMbps")
+				}
+				if ifc.WirelessInterfaces.LinkInfo.TxPowerDBm < 0 {
+					s.Fatal("Failed to get TxPowerDBm")
+				}
+			} else {
+				if ifc.WirelessInterfaces.LinkInfo != nil {
+					s.Fatal("Failed to validate empty LinkInfo data")
 				}
 			}
 		}
