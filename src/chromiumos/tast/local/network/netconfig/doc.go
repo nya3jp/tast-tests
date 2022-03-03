fccbafb9ddb55b336e74deb8b61ac76d3e9c4b2d
Shijin Abraham <shijinabraham@google.com>
2022-03-03 09:39:06 -0800

    tast: Test to ensure ethernet is preferred over cellular
    This test make sure ethernet is preferred over cellular when both are connected
    and WiFi is disabled. This is a port of  ethernetPreferred subtest of
    cellular_ChromeEndToEnd.py tauto test.
    
    Fix some lint warning due to lack of comments
    
    BUG=b:220027306
    TEST=Ran cellular.MojoEthernetPreferredOverCellular on Limozeen with Ethernet and cellular connection
    
    Change-Id: Ibd7b4b616b40d2305e3563809bf7381204a09350

---
+++ src/chromiumos/tast/local/network/netconfig/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package netconfig contains functions to establish mojo connection to cros_network_config
+// along with some helper functions.
+package netconfig
