a6b24b76fb9c185a5063dc87eb50af78281be1fd
Newman Liu <chriswu@allion.corp-partner.google.com>
2022-03-03 16:19:35 +0800

    docking: add scripts in WWCB and utilities, modify code style
    these scripts are testing Chromebook being connected with docking station
    and docking station is connected with fixtures,
    include monitor, power, USB, network, ..etc
    
    utilities are base functions, stored in utils folder, divided into two parts
    one is for control Chromebook, filename start with Chromebook ,
    and the other is for control fixture, filename start with fixture. e.g. fixture_api.go
    
    modify code style by following repohooks recommendation
    1. Import should be grouped into standard packages,
    third-party packages and chromiumos packages in this order separated by empty lines.
    2. Use errors.Wrap() instead of errors.Wrapf()
    3. Messages of the error type should not be capitalized
    4. s.Logf() string arg should not contain trailing punctuation
    5. When two or more consecutive named function parameters share a type,
    you can omit the type from all but the last
    6. Use errors.New() instead of errors.Errorf()
    7. Tast requires exactly one symbol (test function or service type) to be exported in an entry file;
    unexport func Dock13ChangeResolution if it is not one
    8. comment on exported function Dock13ChangeResolution_Step2 should be of the form "Dock13ChangeResolution_Step2 ..."
    9. don't use underscores in Go names; func Dock13ChangeResolution_Step2 should be Dock13ChangeResolutionStep2
    10. exported type FixtureType should have comment or be unexported
    11. time.Sleep ignores context deadline; use testing.Poll or testing.Sleep instead
    12. if block ends with a return statement, so drop this else and outdent its block
    13. The number of verbs in format literal mismatches with the number of arguments
    14. Tast forbids inter-file references in entry files;
    move var defaultOSSettingsPollOptions in dock12_light_mode.go to a shared package
    15. import of chromiumos/tast/local/bundles/cros/arc/motioninput is only allowed from
    chromiumos/tast/local/bundles/*/arc or its descendant
    16. .*Copyright(?: \(c\))? (20[0-9]{2})(?:-20[0-9]{2})? The Chromium OS Authors\.All rights reserved\.\n.
    *Use of this source code is governed by a BSD-style license that can
    be\n.*found in the LICENSE file\.\n
    
    BUG=b:222188262
    FIXED=b:222188262
    TEST=tast run -var=FixtureWebUrl=192.168.1.201:8585 192.168.1.200
    crostini.Dock1PersistentSetting
    
    Change-Id: I11740005cc38b75709fbe2057c39a31e9e5f8ed1

---
+++ src/chromiumos/tast/local/bundles/cros/wwcb/utils/chromebook_const.go
@@ -0,0 +1,23 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package utils
+
+// youtube
+const (
+	VideoTitle = "SMPTE Television"
+	YouTubeURL = "https://www.youtube.com/watch?v=l4bDVq-nP-0&t=65s"
+)
+
+// testing app
+const (
+	// first app
+	SettingsPkg = "com.android.settings"
+	SettingsAct = ".Settings"
+
+	// second app
+	TestappPkg = "org.chromium.arc.testapp.windowmanager24"
+	TestappAct = "org.chromium.arc.testapp.windowmanager.ResizeableLandscapeActivity"
+	TestappApk = "ArcWMTestApp_24.apk"
+)
