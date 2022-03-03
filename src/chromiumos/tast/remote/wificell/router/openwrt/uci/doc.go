16fc3face684ae469a808ca4e89f8c8ba85820e3
Jared Bennett <jaredbennett@google.com>
2022-03-03 14:59:22 -0800

    tast-tests: openwrt uci cli support
    BUG=b:211517577
    TEST=None directly, though will be used in upcoming OpenWrt tests.
    
    Change-Id: I5e52d9bb306d08e5d7d7be2662c2370801fb580c

---
+++ src/chromiumos/tast/remote/wificell/router/openwrt/uci/doc.go
@@ -0,0 +1,13 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package uci contains utility functions to wrap around the uci program.
+//
+// The UCI (United Configuration Interface) system is used to centralize the
+// configuration of OpenWrt services.
+//
+// See https://openwrt.org/docs/guide-user/base-system/uci for documentation on
+// the UCI system and https://openwrt.org/docs/guide-user/base-system/uci#command-line_utility
+// for documentation on the uci CLI which this package uses.
+package uci
