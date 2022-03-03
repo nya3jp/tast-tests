86e654c00d08b08e7f2389224c390ab834a1b57d
Jared Bennett <jaredbennett@google.com>
2022-03-03 13:36:14 -0800

    tast-tests: added routertype and pcaptype vars to wificell fixtures
    BUG=b:211517577
    TEST=tast run \
     -var=router=$LEGACY_ROUTER \
     -var=pcap=$LEGACY_PCAP \
     $DUT \
     wifi.SimpleConnect.80211a && \
    tast run \
     -var=router=$LEGACY_ROUTER \
     -var=routertype=legacy \
     -var=pcap=$LEGACY_PCAP \
     -var=pcaptype=legacy \
     $DUT \
     wifi.SimpleConnect.80211a
    
    Change-Id: I673271a0eee2b2e53ee4e4ff6a5e0c5eb2d12fc3

--- src/chromiumos/tast/remote/wificell/router/common/support/support.go
+++ src/chromiumos/tast/remote/wificell/router/common/support/support.go
@@ -7,8 +7,10 @@ package support
 import (
 	"context"
 	"net"
+	"strings"
 
 	"chromiumos/tast/common/network/iw"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/remote/wificell/dhcp"
 	"chromiumos/tast/remote/wificell/framesender"
 	"chromiumos/tast/remote/wificell/hostapd"
@@ -24,6 +26,20 @@ const (
 	// AxT is the ax router type.
 	AxT
 )
+
+// ParseRouterType parses a RouterType from a string.
+func ParseRouterType(rTypeStr string) (RouterType, error) {
+	var rType RouterType
+	switch strings.ToLower(rTypeStr) {
+	case "legacy":
+		rType = LegacyT
+	case "ax":
+		rType = AxT
+	default:
+		return -1, errors.Errorf("unknown RouterType %q", rTypeStr)
+	}
+	return rType, nil
+}
 
 // Router contains the basic methods that must be implemented across all routers.
 type Router interface {
