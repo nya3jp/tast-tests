8a798d481811516e316b36f187397a5e4d8239f3
alanlxl <alanlxl@chromium.org>
2022-03-04 23:02:03 +1100

    Reland "power: add coverage for smart dim downloadable model"
    This is a reland of commit 29a297d3fa02cf27e9a2b195448102c8c878ae62
    Fix the flakiness by:
    1. Adds fixtures that setup chrome/lacros with essential arguments.
    2. Make sure builtin model is in use by passing in invalid version
       string, so that WorkerType verification can pass.
    
    Original change's description:
    > power: add coverage for smart dim downloadable model
    >
    > This CL adds a new variant "flatbuffer" to power.SmartDim.
    > It triggers an on-demand smart dim component to enable
    > downloadable model (flatbuffer), and checks the WorkerType
    > histogram to make sure downloadable model is indeed invoked.
    >
    > BUG=b:218932913
    > TEST=tast -verbose run ${DUT} power.SmartDim
    > TEST=tast -verbose run ${DUT} power.SmartDim.flatbuffer
    > TEST=tast -verbose run ${DUT} power.SmartDim.lacros
    >
    > Change-Id: I097fcc24252d1aea203bd9e818e146704fe39c94
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3498155
    > Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    > Tested-by: Xinglong Luan <alanlxl@chromium.org>
    > Reviewed-by: Judy Wu <jinrongwu@google.com>
    > Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    
    BUG=b:218932913
    TEST=tast -verbose run ${DUT} power.SmartDim
    TEST=tast -verbose run ${DUT} power.SmartDim.flatbuffer
    TEST=tast -verbose run ${DUT} power.SmartDim.lacros
    
    Change-Id: Ieaa77adf1180a92af81366b6ddc4f6d30b043d17

---
+++ src/chromiumos/tast/local/power/fixture.go
@@ -0,0 +1,67 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/lacros/lacrosfixt"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	chromeQuickMetricsCollectionArg := "--external-metrics-collection-interval=1"
+
+	// Sets version to "unknown" to ensure smart dim uses builtin model.
+	chromeSmartDimBuiltinModelArg := "--enable-features=SmartDimExperimentalComponent:smart_dim_experimental_version/unknown"
+
+	testing.AddFixture(&testing.Fixture{
+		Name:     "chromeFastHistograms",
+		Desc:     "Logged into a user session and enabled quick metrics collection for fast histogram validation",
+		Contacts: []string{"alanlxl@google.com"},
+		Impl: chrome.NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return []chrome.Option{
+				chrome.ExtraArgs(chromeQuickMetricsCollectionArg),
+			}, nil
+		}),
+		SetUpTimeout:    chrome.LoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+	})
+
+	testing.AddFixture(&testing.Fixture{
+		Name:     "chromeFastHistogramsAndBuiltinSmartDimModel",
+		Desc:     "Similar to chromeFastHistograms, plus force chrome to use builtin smart dim models",
+		Contacts: []string{"alanlxl@google.com"},
+		Impl: chrome.NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return []chrome.Option{
+				chrome.ExtraArgs(chromeQuickMetricsCollectionArg),
+				chrome.ExtraArgs(chromeSmartDimBuiltinModelArg),
+			}, nil
+		}),
+		SetUpTimeout:    chrome.LoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+	})
+
+	testing.AddFixture(&testing.Fixture{
+		Name:     "lacrosFastHistogramsAndBuiltinSmartDimModel",
+		Desc:     "Similar to chromeFastHistogramsAndBuiltinSmartDimModel but on lacros",
+		Contacts: []string{"alanlxl@google.com"},
+		Impl: lacrosfixt.NewFixture(lacrosfixt.Rootfs, func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return []chrome.Option{
+				chrome.ExtraArgs(chromeQuickMetricsCollectionArg),
+				chrome.ExtraArgs(chromeSmartDimBuiltinModelArg),
+			}, nil
+		}),
+		SetUpTimeout:    chrome.LoginTimeout + 7*time.Minute,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		Vars:            []string{lacrosfixt.LacrosDeployedBinary},
+	})
+
+}
