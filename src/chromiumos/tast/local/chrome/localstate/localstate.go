239c1129bc57d0f788c1c923c4e125f8d45f23a5
Jieting Yang <yjt@google.com>
2022-03-09 21:21:41 -0800

    [Lacros] Add the variation smoke test for Lacros
    Add Lacros variation smoke test. It will download seeds, inject seeds,
    verify page rendered correctly in Lacros
    
    BUG=chromium:1305074
    TEST=tast run -var=lacrosDeployedBinary=/usr/local/lacros-chrome <DUT>
     lacros.VariationSmoke
    
    Change-Id: I2b8deabdacada9b13cf29cfa33d8324954f0f189

--- src/chromiumos/tast/local/chrome/localstate/localstate.go
+++ src/chromiumos/tast/local/chrome/localstate/localstate.go
@@ -63,3 +63,50 @@ func UnmarshalPref(bt browser.Type, pref string) (interface{}, error) {
 	}
 	return localState, nil
 }
+
+// Marshal will update Local State with localState
+// localState includes pref names and pref values that will be written to local state
+// pref name with dot format "foo.bar.baz" is not allowed in localState interface,
+// localState could include nested map type {"foo":{"bar":{"baz": 1234}}}
+func Marshal(bt browser.Type, localState interface{}) error {
+	path := localStatePathAsh
+	if bt == browser.TypeLacros {
+		path = localStatePathLacros
+	}
+	s, err := json.Marshal(localState)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal Local State")
+	}
+	if err := ioutil.WriteFile(path, s, 0644); err != nil {
+		return errors.Wrap(err, "failed to write Local State")
+	}
+	return nil
+}
+
+// MarshalPref will update the pref with val in local state
+// if pref has format "foo.bar.baz", it will be written as {"foo":{"bar":{"baz": 1234}}}
+func MarshalPref(bt browser.Type, pref string, val interface{}) error {
+	var localState interface{}
+	if err := Unmarshal(bt, &localState); err != nil {
+		return errors.Wrap(err, "failed to retrieve Local State contents")
+	}
+	localStateMap, ok := localState.(map[string]interface{})
+	if !ok {
+		return errors.Wrap(nil, "localState has an unexpected value type")
+	}
+	path := strings.Split(pref, ".")
+	var newPref string
+	var newValue interface{} = val
+	for i := len(path) - 1; i > 0; i-- {
+		newPref = path[i]
+		dict := make(map[string]interface{})
+		dict[newPref] = newValue
+		newValue = dict
+	}
+	newPref = path[0]
+	localStateMap[newPref] = newValue
+	if err := Marshal(bt, localStateMap); err != nil {
+		return errors.Wrap(err, "failed to wirte Local State contents")
+	}
+	return nil
+}
