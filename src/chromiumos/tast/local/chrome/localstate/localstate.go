2048643bc3beef0aa4a83f747d5dc2e1293c9988
Jieting Yang <yjt@google.com>
2022-03-09 21:21:41 -0800

    [Lacros] Add the variation smoke test for Lacros
    Add Lacros variation smoke test. It will download seeds, inject seeds,
    verify page rendered correctly in Lacros
    
    chump because of unrelated CQ failure
    
    BUG=chromium:1305074
    TEST=tast run -var=lacrosDeployedBinary=/usr/local/lacros-chrome <DUT>
     lacros.VariationSmoke
    
    Change-Id: I2b8deabdacada9b13cf29cfa33d8324954f0f189

--- src/chromiumos/tast/local/chrome/localstate/localstate.go
+++ src/chromiumos/tast/local/chrome/localstate/localstate.go
@@ -9,6 +9,7 @@ package localstate
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
 	"strings"
 
 	"chromiumos/tast/errors"
@@ -62,4 +63,69 @@ func UnmarshalPref(bt browser.Type, pref string) (interface{}, error) {
 		localState = value
 	}
 	return localState, nil
+}
+
+// Marshal will update Local State with localState.
+// localState includes pref names and pref values that will be written to local state.
+// pref name with dot format "foo.bar.baz" is not allowed in localState interface.
+// localState could include nested map type {"foo":{"bar":{"baz": 1234}}}.
+func Marshal(bt browser.Type, localState interface{}) error {
+	path := localStatePathAsh
+	if bt == browser.TypeLacros {
+		path = localStatePathLacros
+	}
+	s, err := json.Marshal(localState)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal Local State")
+	}
+	if err := ioutil.WriteFile(path, s, 0644); err != nil {
+		return errors.Wrap(err, "failed to write Local State")
+	}
+	return nil
+}
+
+// MarshalPref will update the pref with val in local state.
+// pref name could have format "foo.bar.baz". Each component separated by '.' will
+// be used as key of the dict. pref must not be an empty string.
+func MarshalPref(bt browser.Type, pref string, val interface{}) error {
+	if pref == "" {
+		return errors.New("perf name cannot be empty, use Marshal if you're overwriting the entire file content")
+	}
+	var localStatePath = localStatePathAsh
+	if bt == browser.TypeLacros {
+		localStatePath = localStatePathLacros
+	}
+	var localState interface{}
+	if _, err := os.Stat(localStatePath); err == nil {
+		if err := Unmarshal(bt, &localState); err != nil {
+			return errors.Wrap(err, "failed to retrieve Local State contents")
+		}
+	} else if errors.Is(err, os.ErrNotExist) {
+		// If local state does not exist, create it. Initialize localState by an empty
+		// map
+		localState = make(map[string]interface{})
+	} else {
+		return errors.New("failed to retrieve Local State contents")
+	}
+	dict, ok := localState.(map[string]interface{})
+	if !ok {
+		return errors.New("localState has an unexpected value type")
+	}
+	keys := strings.Split(pref, ".")
+	for _, key := range keys[:len(keys)-1] {
+		e, ok := dict[key]
+		if !ok {
+			e = make(map[string]interface{})
+			dict[key] = e
+		}
+		dict, ok = e.(map[string]interface{})
+		if !ok {
+			return errors.New("failed to read Local State content")
+		}
+	}
+	dict[keys[len(keys)-1]] = val
+	if err := Marshal(bt, localState); err != nil {
+		return errors.Wrap(err, "failed to wirte Local State contents")
+	}
+	return nil
 }
