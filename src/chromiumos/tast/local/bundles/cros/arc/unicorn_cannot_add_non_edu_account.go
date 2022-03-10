08a7510e7c86cf87d73c306c24c1662d4987a617
Anastasiia N <anastasiian@google.com>
2022-03-10 12:30:10 +0100

    tast-tests: Refactor account addition from ARC
    Add a common ClickAddAccountInSettings method which also clicks on
    "Google" button if present.
    
    BUG=b:222773147
    TEST=tast run <ip> accountmanager.ARCAccountPicker*
    TEST=tast run <ip> arc.MultipleArcProfile*
    TEST=tast run <ip> arc.UnicornCannotAddNonEduAccount*
    
    Change-Id: I0c96fb9d957c11a84c0a69180c2f5270badb7102

--- src/chromiumos/tast/local/bundles/cros/arc/unicorn_cannot_add_non_edu_account.go
+++ src/chromiumos/tast/local/bundles/cros/arc/unicorn_cannot_add_non_edu_account.go
@@ -117,39 +117,10 @@ func openAndroidSettings(ctx context.Context, cr *chrome.Chrome, tconn *chrome.T
 
 // addAndroidAccount adds a second ARC account from ARC Settings->Accounts Screen.
 func addAndroidAccount(ctx context.Context, arcDevice *androidui.Device, cr *chrome.Chrome, tconn *chrome.TestConn, parentPassword, gellerParentUser, gellerParentPass string) error {
-	const (
-		scrollClassName   = "android.widget.ScrollView"
-		textViewClassName = "android.widget.TextView"
-	)
-
 	ui := uiauto.New(tconn)
-	// Scroll until Accounts is visible.
-	scrollLayout := arcDevice.Object(androidui.ClassName(scrollClassName),
-		androidui.Scrollable(true))
-	accounts := arcDevice.Object(androidui.ClassName("android.widget.TextView"),
-		androidui.TextMatches("(?i)Accounts"), androidui.Enabled(true))
-	if err := scrollLayout.WaitForExists(ctx, 10*time.Second); err == nil {
-		scrollLayout.ScrollTo(ctx, accounts)
-	}
-	if err := accounts.Click(ctx); err != nil {
-		return errors.Wrap(err, "failed to click on Accounts")
-	}
 
-	addAccount := arcDevice.Object(androidui.ClassName("android.widget.TextView"),
-		androidui.TextMatches("(?i)Add account"), androidui.Enabled(true))
-	if err := addAccount.WaitForExists(ctx, 10*time.Second); err != nil {
-		return errors.Wrap(err, "failed finding Add account")
-	}
-	if err := addAccount.Click(ctx); err != nil {
-		return errors.Wrap(err, "failed to click Add account")
-	}
-
-	// Click on Google button which appears only on tablet flow.
-	gaiaButton := arcDevice.Object(androidui.ClassName("android.widget.TextView"), androidui.TextMatches("(?i)Google"))
-	if err := gaiaButton.WaitForExists(ctx, 10*time.Second); err != nil {
-		testing.ContextLog(ctx, "Google button doesn't exist: ", err)
-	} else if err := gaiaButton.Click(ctx); err != nil {
-		return errors.Wrap(err, "failed to click Google")
+	if err := arc.ClickAddAccountInSettings(ctx, arcDevice, tconn); err != nil {
+		return errors.Wrap(err, "failed to open Add account dialog from ARC")
 	}
 
 	if err := familylink.NavigateEduCoexistenceFlow(ctx, cr, tconn, parentPassword, gellerParentUser, gellerParentPass); err != nil {
