006a49c9613496eafbd62b3b5ee62aac19ccaf5c
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-03-11 10:54:18 -0800

    tast-tests: Add browserfixt.CreateWindows for migration
    CreateWindows is a util that makes the transition from
    ash.CreateWindows easy for both ash-chrome and lacros-chrome.
    It is necessary to address the difference of application life cycle
    between them.
    Unlike ash-chrome which instance is always available even with
    no window open, lacros-chrome needs to opens at least one extra blank
    window before instantiating.
    
    Also add example.BrowserFixt to test it out.
    
    See go/lacros-tast-porting for the porting guidelines.
    
    BUG=None (TBD)
    TEST=tast run example.BrowserFixt*
    
    Change-Id: I8d3d64312fb8ac0123c90f4b461ab85f74fe2ddd

---
+++ src/chromiumos/tast/local/bundles/cros/example/browser_fixt.go
@@ -0,0 +1,89 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         BrowserFixt,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Tests the browserfixt package. See http://go/lacros-tast-porting for the guidelines on how to use",
+		Contacts:     []string{"lacros-tast@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Fixture: "chromeLoggedIn",
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			Fixture:           "lacrosPrimary",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Val:               browser.TypeLacros,
+		}},
+	})
+}
+
+func BrowserFixt(ctx context.Context, s *testing.State) {
+	// Reserve some time for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to the test API connection: ", err)
+	}
+
+	bt := s.Param().(browser.Type)
+	for _, url := range []string{
+		"about:blank",
+		"",
+		"chrome://newtab",
+		"chrome://version",
+		"http://www.google.com",
+	} {
+		s.Run(ctx, fmt.Sprintf("browserfixt.CreateWindows browser: %v, url: %v", bt, url), func(ctx context.Context, s *testing.State) {
+			// Make sure no window is open.
+			if err := ash.CloseAllWindows(ctx, tconn); err != nil {
+				s.Fatal("Failed to ensure no window is open: ", err)
+			}
+
+			// Open the exact number of browser windows.
+			const numNewWindows = 2
+			_ /* br */, closeBrowser, err := browserfixt.CreateWindows(ctx, s.FixtValue(), bt, tconn, url, numNewWindows)
+			if err != nil {
+				s.Fatalf("Failed to set up a browser: %v, err: %v", bt, err)
+			}
+			defer closeBrowser(cleanupCtx)
+
+			if err := testing.Poll(ctx, func(ctx context.Context) error {
+				ws, err := ash.GetAllWindows(ctx, tconn)
+				if err != nil {
+					return errors.Wrap(err, "failed to get all windows")
+				}
+				if len(ws) != numNewWindows {
+					return errors.Wrapf(err, "failed to find open windows expected: %v, got: %v", numNewWindows, len(ws))
+				}
+				return nil
+			}, &testing.PollOptions{Timeout: 10 * time.Second, Interval: time.Second}); err != nil {
+				s.Fatalf("Failed to open the desired number of %v browser windows: %v", bt, err)
+			}
+		})
+	}
+}
--- src/chromiumos/tast/local/chrome/browser/browserfixt/browserfixt.go
+++ src/chromiumos/tast/local/chrome/browser/browserfixt/browserfixt.go
@@ -11,6 +11,7 @@ import (
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/browser"
 	"chromiumos/tast/local/chrome/lacros"
 	"chromiumos/tast/local/chrome/lacros/lacrosfixt"
@@ -78,5 +79,75 @@ func SetUpWithURL(ctx context.Context, f interface{}, bt browser.Type, url strin
 
 	default:
 		return nil, nil, nil, errors.Errorf("unrecognized browser type %s", string(bt))
+	}
+}
+
+// CreateWindows is a util that makes the transition from ash.CreateWindows easy for both ash-chrome and lacros-chrome.
+// It is necessary to address the difference of application life cycle between them.
+// Unlike ash-chrome which instance is always available even with no window open, lacros-chrome needs to opens at least one extra blank window before instantiating.
+// CreateWindows create n browser windows with specified URL and wait for them to become visible.
+// It will fail and return an error if at least one request fails to fulfill. Note that this will
+// parallelize the requests to create windows, which may be bad if the caller
+// wants to measure the performance of Chrome. This should be used for a
+// preparation, before the measurement happens.
+// tconn is from *ash-chrome*.
+func CreateWindows(ctx context.Context, f interface{}, bt browser.Type, tconn *chrome.TestConn, url string, n int) (*browser.Browser, func(ctx context.Context), error) {
+	// TODO(crbug.com/1290318): Find a way to open a lacros-chrome with any chrome:// URLs.
+	// Due to crbug.com/1290318 lacros-chrome can't open any chrome:// URLs other than blank page upon initializing.
+	// For blank URL, use SetUpWithURL for the first window and ash.CreateWindows for the rest.
+	// For other URLs to work around, let lacros-chrome start with a blank page, open the given number of windows with the given URL, then close the first blank page.
+	switch url {
+	case "", chrome.BlankURL:
+		// Open the first window with a given URL on startup.
+		_, br, closeBrowser, err := SetUpWithURL(ctx, f, bt, url)
+		if err != nil {
+			return nil, nil, err
+		}
+		// Open one less windows.
+		if n > 1 {
+			if err := ash.CreateWindows(ctx, tconn, br, url, n-1); err != nil {
+				return nil, nil, err
+			}
+		}
+		return br, closeBrowser, nil
+
+	default:
+		// Open a blank window for lacros-chrome, but no window for ash-chrome.
+		// XXX: This looks ugly to copy the entire SetUp func, modify it to return the lacros instance and eventually call CloseAboutBlank.
+		// This could be simplied with "br, closeBrowser, err := SetUp(ctx, f, bt)"
+		// and https://crrev.com/c/3426334 (util to close any browser window with the URL)
+		br, closeBrowser, l, err := func(ctx context.Context, f interface{}, bt browser.Type) (*browser.Browser, func(ctx context.Context), *lacros.Lacros, error) {
+			switch bt {
+			case browser.TypeAsh:
+				cr := f.(chrome.HasChrome).Chrome()
+				return cr.Browser(), func(context.Context) {}, nil, nil
+			case browser.TypeLacros:
+				f := f.(lacrosfixt.FixtValue)
+				l, err := lacros.Launch(ctx, f)
+				if err != nil {
+					return nil, nil, nil, errors.Wrap(err, "failed to launch lacros-chrome")
+				}
+				closeLacros := func(ctx context.Context) {
+					l.Close(ctx) // Ignore error.
+				}
+				return l.Browser(), closeLacros, l, nil
+			default:
+				return nil, nil, nil, errors.Errorf("unrecognized browser type %s", string(bt))
+			}
+		}(ctx, f, bt)
+		if err != nil {
+			return nil, nil, err
+		}
+		// Open the exact number of windows.
+		if err := ash.CreateWindows(ctx, tconn, br, url, n); err != nil {
+			return nil, nil, err
+		}
+		// Close the extra blank window if lacros-chrome.
+		if bt == browser.TypeLacros {
+			if err := l.CloseAboutBlank(ctx, tconn, 1); err != nil {
+				return nil, nil, errors.Wrap(err, "failed to close about:blank for lacros")
+			}
+		}
+		return br, closeBrowser, nil
 	}
 }
