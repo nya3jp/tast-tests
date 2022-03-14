4a596f6273bad67caf9771a70bdbb41692091c91
Curtis McMullan <curtismcmullan@chromium.org>
2022-03-14 15:03:41 +1100

    tast: expand multiword tast coverage
    This CL expands the testcases covered by tast for the multiword feature.
    
    BUG=b:217561305
    TEST=tast -verbose run -build=true 127.0.0.1:9222
    inputs.PhysicalKeyboardMultiwordSuggestion
    
    Change-Id: I8448e06ab8bccd3243464ceb41b3f520d1106315

--- src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_multiword_suggestion.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_multiword_suggestion.go
@@ -37,11 +37,6 @@ func init() {
 }
 
 func PhysicalKeyboardMultiwordSuggestion(ctx context.Context, s *testing.State) {
-	const (
-		inputText    = "ho"
-		expectedText = "how are you"
-	)
-
 	cr := s.PreValue().(pre.PreData).Chrome
 	tconn := s.PreValue().(pre.PreData).TestAPIConn
 	uc := s.PreValue().(pre.PreData).UserContext
@@ -74,31 +69,126 @@ func PhysicalKeyboardMultiwordSuggestion(ctx context.Context, s *testing.State) 
 	suggestionWindowFinder := nodewith.HasClass("SuggestionWindowView").Role(role.Window)
 	ui := uiauto.New(tconn)
 
-	// TODO(b/224628222): Expecting an ML candidate to remain the same in tests can be somewhat flakey
-	//   in future runs of the test. Update this test to capture the candidate shown on screen and
-	//   validate against that candidate.
-	validateAction := uiauto.Combine("accept multiword suggestion",
-		its.Clear(inputField),
-		its.ClickFieldAndWaitForActive(inputField),
-		keyboard.TypeAction(inputText),
-		util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), inputText),
-		ui.WaitUntilExists(suggestionWindowFinder),
-		keyboard.AccelAction("Tab"),
-		util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), expectedText),
-	)
-
-	if err := uiauto.UserAction(
-		"Accept multiword suggestion",
-		validateAction,
-		uc,
-		&useractions.UserActionCfg{
-			Attributes: map[string]string{
-				useractions.AttributeTestScenario: "Accept first candidate using Tab key",
-				useractions.AttributeInputField:   string(inputField),
-				useractions.AttributeFeature:      useractions.FeatureMultiwordSuggestion,
-			},
+	// TODO(b/224628222): Expecting an ML candidate to remain the same in
+	//   tests can be somewhat flakey in future runs of the test. Update
+	//   these tests to capture the candidate shown on screen and validate
+	//   against that candidate.
+	subtests := []struct {
+		name     string
+		scenario string
+		errStr   string
+		action   uiauto.Action
+	}{
+		{
+			// Trigger suggestion "how are you" and insert into
+			// textfield with tab.
+			name:     "AcceptSuggestionWithTab",
+			scenario: "verify suggestion appears and accepted with tab key",
+			errStr:   "Failed to accept suggestion: %v",
+			action: uiauto.Combine("accept multiword suggestion with tab",
+				keyboard.TypeAction("ho"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "ho"),
+				ui.WaitUntilExists(suggestionWindowFinder),
+				keyboard.AccelAction("Tab"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "how are you"),
+			),
 		},
-	)(ctx); err != nil {
-		s.Fatal("Fail to accept multiword suggestion: ", err)
+		{
+			// Trigger suggestion "how are you" and insert into
+			// textfield with down + enter key.
+			name:     "AcceptSuggestionWithDownAndEnter",
+			scenario: "verify suggestion appears and accepted with down and enter key",
+			errStr:   "Failed to accept suggestion: %v",
+			action: uiauto.Combine("accept multiword suggestion with down and enter",
+				keyboard.TypeAction("ho"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "ho"),
+				ui.WaitUntilExists(suggestionWindowFinder),
+				keyboard.AccelAction("Down"),
+				keyboard.AccelAction("Enter"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "how are you"),
+			),
+		},
+		{
+			// Trigger suggestion "my name is" and dismiss with
+			// multiple white space at the end of the text.
+			name:     "SuggestionShouldAppearOnlyAtEndOfText",
+			scenario: "verify suggestion dismissed with multiple whitespace",
+			errStr:   "Failed to dismiss suggestion with whitespace: %v",
+			action: uiauto.Combine("dismiss multiword suggestion with multiple whitespace",
+				keyboard.TypeAction("my name"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "my name"),
+				ui.WaitUntilExists(suggestionWindowFinder),
+				keyboard.TypeAction("  "),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "my name  "),
+				ui.WaitUntilGone(suggestionWindowFinder),
+			),
+		},
+		{
+			// Trigger suggestion "how are you", partially type
+			// suggestion, and dismiss suggestion by deleting text
+			// beyond suggestion trigger point.
+			name:     "SuggestionTrackedAndDismissedWithTextUpdates",
+			scenario: "track typing in suggestion and dismiss when deleting past trigger point",
+			errStr:   "Failed to dismiss suggestion: %v",
+			action: uiauto.Combine("dismiss multiword suggestion by deleting past trigger point",
+				keyboard.TypeAction("hi there h"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "hi there h"),
+				ui.WaitUntilExists(suggestionWindowFinder),
+				keyboard.TypeAction("ow a"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "hi there how a"),
+				ui.WaitUntilExists(suggestionWindowFinder),
+				keyboard.AccelAction("Backspace"), // "hi there how "
+				keyboard.AccelAction("Backspace"), // "hi there how"
+				keyboard.AccelAction("Backspace"), // "hi there ho"
+				keyboard.AccelAction("Backspace"), // "hi there h"
+				ui.WaitUntilExists(suggestionWindowFinder),
+				keyboard.AccelAction("Backspace"), // "hi there "
+				ui.WaitUntilGone(suggestionWindowFinder),
+			),
+		},
+		{
+			// Suggestions should handle newlines gracefully.
+			name:     "SuggestionHandlesNewline",
+			scenario: "suggestions handles newline gracefully",
+			errStr:   "Failed to accept suggestion: %v",
+			action: uiauto.Combine("suggestion handles newline gracefully",
+				keyboard.TypeAction("goo"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "goo"),
+				ui.WaitUntilExists(suggestionWindowFinder),
+				keyboard.AccelAction("Enter"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "goo\n"),
+				ui.WaitUntilGone(suggestionWindowFinder),
+				keyboard.AccelAction("Backspace"),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "goo"),
+				ui.WaitUntilExists(suggestionWindowFinder),
+				keyboard.AccelAction("Tab"),
+				ui.WaitUntilGone(suggestionWindowFinder),
+				util.WaitForFieldTextToBeIgnoringCase(tconn, inputField.Finder(), "good morning"),
+			),
+		},
+	}
+
+	for _, subtest := range subtests {
+		s.Run(ctx, subtest.name, func(ctx context.Context, s *testing.State) {
+			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+string(subtest.name))
+
+			if err := uiauto.UserAction(
+				"Multiword suggestion",
+				uiauto.Combine("...",
+					its.Clear(inputField),
+					its.ClickFieldAndWaitForActive(inputField),
+					subtest.action,
+				),
+				uc, &useractions.UserActionCfg{
+					Attributes: map[string]string{
+						useractions.AttributeTestScenario: subtest.scenario,
+						useractions.AttributeInputField:   string(inputField),
+						useractions.AttributeFeature:      useractions.FeatureMultiwordSuggestion,
+					},
+				},
+			)(ctx); err != nil {
+				s.Fatalf(subtest.errStr, err)
+			}
+		})
 	}
 }
