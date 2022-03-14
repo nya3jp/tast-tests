969bf1fa9caf759fd2e9a4a98b14bcefb2e930c4
Victor-Gabriel Savu <vsavu@google.com>
2022-03-14 16:55:32 +0000

    policy: Clear enrollment without reboot
    TPM 2.0 can be reset without a reboot, use this to clear enrollment
    in the Enrolled fixture to skip reboots.
    
    BUG=b:223565358
    TEST=tast run <ip> policy.SystemTimezoneAutomaticDetection
    TEST=Ran on both TPM2.0 and TPM1.2 devices (sarien+asuka)
    
    Change-Id: If1e8901bb662b7c18f9e5ece86b02ca5ea917eff

---
+++ src/chromiumos/tast/local/bundles/cros/hwsec/ownership_service.go
@@ -0,0 +1,63 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hwsec
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/errors"
+	hwseclocal "chromiumos/tast/local/hwsec"
+	hwsecpb "chromiumos/tast/services/cros/hwsec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddService(&testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			hwsecpb.RegisterOwnershipServiceServer(srv, &OwnershipService{s})
+		},
+	})
+}
+
+type OwnershipService struct {
+	s *testing.ServiceState
+}
+
+// EnsureTPMIsReset calls the local EnsureTPMIsReset hwsec helpers.
+func (*OwnershipService) EnsureTPMIsReset(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	testing.ContextLog(ctx, "Requesting a local TPM reset")
+
+	cmdRunner := hwseclocal.NewCmdRunner()
+	helper, err := hwseclocal.NewHelper(cmdRunner)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create local helper")
+	}
+
+	if err := helper.EnsureTPMIsReset(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to reset TPMd")
+	}
+
+	return &empty.Empty{}, nil
+}
+
+// EnsureTPMAndSystemStateAreReset calls the local EnsureTPMAndSystemStateAreReset hwsec helpers.
+func (*OwnershipService) EnsureTPMAndSystemStateAreReset(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	testing.ContextLog(ctx, "Requesting a local TPM and state reset")
+
+	cmdRunner := hwseclocal.NewCmdRunner()
+	helper, err := hwseclocal.NewHelper(cmdRunner)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create local helper")
+	}
+
+	if err := helper.EnsureTPMAndSystemStateAreReset(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to reset TPMd")
+	}
+
+	return &empty.Empty{}, nil
+}
