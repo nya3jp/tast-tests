195d1e74be22b71e0784ce34d98d1c3a1262ab04
Eliot Courtney <edcourtney@google.com>
2022-03-15 12:16:39 +0900

    Remove external lacros binary based Tast testing.
    This CL removes the ability to use an external tarball for the lacros
    binary in Tast tests. Note that a custom lacros can still be used with
    LacrosDeployedVar. The external lacros binary path is not being used
    anywhere currently.
    
    BUG=chromium:1297542
    TEST=fast_build.sh -T
    
    Change-Id: Ia57c76543d062e06cc5e96e3eac764ac80fdcb2b

--- src/chromiumos/tast/local/chrome/lacros/lacrosfixt/util.go
+++ src/chromiumos/tast/local/chrome/lacros/lacrosfixt/util.go
@@ -4,48 +4,6 @@
 
 package lacrosfixt
 
-import (
-	"context"
-	"os"
-
-	"chromiumos/tast/common/testexec"
-	"chromiumos/tast/errors"
-	"chromiumos/tast/local/sysutil"
-	"chromiumos/tast/testing"
-)
-
-// prepareLacrosBinary ensures that lacros-chrome binary is available on
-// disk and ready to launch. Does not launch the binary.
-// This will extract lacros-chrome to where the lacrosRootPath constant points to.
-func prepareLacrosBinary(ctx context.Context, s *testing.FixtState) error {
-	testing.ContextLog(ctx, "Preparing the environment to run Lacros")
-	if err := os.RemoveAll(lacrosTestPath); err != nil && !os.IsNotExist(err) {
-		return errors.Wrap(err, "failed to remove old test artifacts directory")
-	}
-
-	if err := os.MkdirAll(lacrosTestPath, os.ModePerm); err != nil {
-		return errors.Wrap(err, "failed to make new test artifacts directory")
-	}
-
-	if err := os.Chown(lacrosTestPath, int(sysutil.ChronosUID), int(sysutil.ChronosGID)); err != nil {
-		return errors.Wrap(err, "failed to chown test artifacts directory")
-	}
-
-	testing.ContextLog(ctx, "Extracting lacros binary")
-	tarCmd := testexec.CommandContext(ctx, "sudo", "-E", "-u", "chronos",
-		"tar", "-xvf", s.DataPath(dataArtifact), "-C", lacrosTestPath)
-
-	if err := tarCmd.Run(testexec.DumpLogOnError); err != nil {
-		return errors.Wrap(err, "failed to untar test artifacts")
-	}
-
-	if err := os.Chmod(lacrosRootPath, 0777); err != nil {
-		return errors.Wrap(err, "failed to change permissions of the binary root dir path")
-	}
-
-	return nil
-}
-
 // ExtensionArgs returns a list of args needed to pass to a lacros instance to enable the test extension.
 func ExtensionArgs(extID, extList string) []string {
 	return []string{
