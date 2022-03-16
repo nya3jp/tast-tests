2ea5f9e7aa08985e5e92cac5043d3a92de0a7282
Daniel Campello <campello@chromium.org>
2022-03-16 09:46:57 -0600

    fwupd: make sure of charging state before each test
    fwupd tests are sensitive to the battery charging state. This change
    makes sure that the appropriate state is achieved before the test
    starts.
    
    BUG=b:210378099,b:198158869
    TEST=tast run dut firmware.Fwupd*
    
    Change-Id: I69b8be880fdf192d16982b8ea1cda63eab6ff651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3530406
    Tested-by: Daniel Campello <campello@chromium.org>
    Auto-Submit: Daniel Campello <campello@chromium.org>
    Reviewed-by: Benson Leung <bleung@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Daniel Campello <campello@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/firmware/fwupd/util.go
+++ src/chromiumos/tast/local/bundles/cros/firmware/fwupd/util.go
@@ -4,5 +4,66 @@
 
 package fwupd
 
+import (
+	"context"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/power/setup"
+	"chromiumos/tast/testing"
+)
+
 // ReleaseURI contains the release URI of the test webcam device in the system.
 const ReleaseURI = "https://storage.googleapis.com/chromeos-localmirror/lvfs/test/3fab34cfa1ef97238fb24c5e40a979bc544bb2b0967b863e43e7d58e0d9a923f-fakedevice124.cab"
+
+const (
+	// This is a string that appears when the computer is discharging.
+	dischargeString = `uint32 [0-9]\s+uint32 2`
+)
+
+func setBatteryNormal(ctx context.Context) error {
+	if err := testexec.CommandContext(ctx, "ectool", "chargecontrol", "normal").Run(testexec.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "ectool chargecontrol failed")
+	}
+	return nil
+}
+
+// SetFwupdChargingState sets the battery charging state and polls for
+// the appropriate change to be registered by powerd via its dbus
+// method.
+func SetFwupdChargingState(ctx context.Context, charge bool) error {
+	if charge {
+		if err := setBatteryNormal(ctx); err != nil {
+			return err
+		}
+	} else {
+		if _, err := setup.SetBatteryDischarge(ctx, 20.0); err != nil {
+			return err
+		}
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		cmd := testexec.CommandContext(ctx, "dbus-send", "--print-reply", "--system", "--type=method_call",
+			"--dest=org.chromium.PowerManager", "/org/chromium/PowerManager",
+			"org.chromium.PowerManager.GetBatteryState")
+		output, err := cmd.Output(testexec.DumpLogOnError)
+		if err != nil {
+			return err
+		}
+		matched, err := regexp.Match(dischargeString, output)
+		if charge == matched || err != nil {
+			return errors.New("powerd has not registered a battery state change")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 10 * time.Second}); err != nil {
+		return errors.Wrap(err, "battery polling was unsuccessful")
+	}
+
+	if !charge {
+		defer setBatteryNormal(ctx)
+	}
+
+	return nil
+}
