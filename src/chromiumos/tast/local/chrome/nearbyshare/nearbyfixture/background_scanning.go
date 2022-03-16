0dddf5e15e898a6cd68acd485b93115b845ded28
Kyle Shimabukuro <kyleshima@chromium.org>
2022-03-16 16:24:29 -0700

    Clean up nearby fixtures
    The main fixture.go file was getting pretty unmanageable with all the
    new fixtures we've added recently. I've reorganized the fixtures to make
    it easier to add new fixtures and find existing ones.
    
    BUG=b:207026818
    TEST=tast run <dut> '("group:nearby-share")'
    
    Change-Id: Ibde6a2a1201775aee68c126cc5959c907b956f4a

---
+++ src/chromiumos/tast/local/chrome/nearbyshare/nearbyfixture/background_scanning.go
@@ -0,0 +1,37 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package nearbyfixture
+
+import (
+	"time"
+
+	nearbycommon "chromiumos/tast/common/cros/nearbyshare"
+	"chromiumos/tast/local/chrome/nearbyshare/nearbysnippet"
+	"chromiumos/tast/testing"
+)
+
+// addBackgroundScanningFixtures registers fixtures for tests with background scanning enabled.
+func addBackgroundScanningFixtures() {
+	testing.AddFixture(&testing.Fixture{
+		Name: "nearbyShareDataUsageOfflineNoOneBackgroundScanningEnabled",
+		Desc: "Nearby Share enabled on CrOS and Android configured with 'Data Usage' set to 'Offline' and 'Visibility' set to 'No One'",
+		Impl: NewNearbyShareFixture(fixtureOptions{
+			crosDataUsage:              nearbycommon.DataUsageOffline,
+			crosVisibility:             nearbycommon.VisibilityNoOne,
+			androidDataUsage:           nearbysnippet.DataUsageOffline,
+			androidVisibility:          nearbysnippet.VisibilityNoOne,
+			crosSelectAndroidAsContact: false,
+		}),
+		Contacts: []string{
+			"chromeos-sw-engprod@google.com",
+		},
+		Parent:          "nearbyShareGAIALoginBackgroundScanningEnabled",
+		SetUpTimeout:    2 * time.Minute,
+		ResetTimeout:    resetTimeout,
+		TearDownTimeout: resetTimeout,
+		PreTestTimeout:  resetTimeout,
+		PostTestTimeout: resetTimeout,
+	})
+}
