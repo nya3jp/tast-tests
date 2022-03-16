02e35dd5f4232316f4c26e75f1cd92dfbdbb0051
Jared Bennett <jaredbennett@google.com>
2022-03-15 17:04:40 -0700

    tast-tests: routertype and pcaptype resolved from host if not specified
    BUG=b:211517577
    TEST=tast run -var=router=$LEGACY_ROUTER -var=pcap=$LEGACY_ROUTER $DUT \
    wifi.SimpleConnect.80211a && \
    tast run -var=router=$OPENWRT_ROUTER -var=pcap=$LEGACY_PCAP $DUT \
    wifi.SimpleConnect.80211b && \
    tast run -var=router=$LEGACY_ROUTER -var=pcap=$OPENWRT_PCAP $DUT \
    wifi.SimpleConnect.80211b
    
    Change-Id: Ibfd2350489de3c5556e420292800df3bd243948f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3527635
    Tested-by: Jared Bennett <jaredbennett@google.com>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Reviewed-by: Harpreet Grewal <harpreet@chromium.org>
    Commit-Queue: Jared Bennett <jaredbennett@google.com>

--- src/chromiumos/tast/remote/wificell/router/common/common.go
+++ src/chromiumos/tast/remote/wificell/router/common/common.go
@@ -5,7 +5,13 @@
 package common
 
 import (
+	"context"
+	"regexp"
 	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/ssh"
+	"chromiumos/tast/ssh/linuxssh"
 )
 
 const (
@@ -29,5 +35,49 @@ const (
 	BridgePrefix = "tastbr"
 )
 
-// RouterCloseContextDuration is a shorter context.Context duration is used for running things before Router.Close to reserve time for it to run.
+// RouterCloseContextDuration is a shorter context.Context duration is used for
+// running things before Router.Close to reserve time for it to run.
 const RouterCloseContextDuration = 5 * time.Second
+
+// HostFileContentsMatch checks if the file at remoteFilePath on the remote
+// host exists and that its contents match using the regex string matchRegex.
+//
+// Returns true if the file exists and its contents matches. Returns false
+// with a nil error if the file does not exist.
+func HostFileContentsMatch(ctx context.Context, host *ssh.Conn, remoteFilePath, matchRegex string) (bool, error) {
+	// Verify that the file exists.
+	fileExists, err := HostFileExists(ctx, host, remoteFilePath)
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to check for the existence of file %q", remoteFilePath)
+	}
+	if !fileExists {
+		return false, nil
+	}
+
+	// Verify that the file contents match.
+	matcher, err := regexp.Compile(matchRegex)
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to compile regex string %q", matchRegex)
+	}
+	fileContents, err := linuxssh.ReadFile(ctx, host, remoteFilePath)
+	return matcher.Match(fileContents), nil
+}
+
+// HostFileExists checks the host for the file at remoteFilePath and returns
+// true if remoteFilePath exists and is a regular file.
+func HostFileExists(ctx context.Context, host *ssh.Conn, remoteFilePath string) (bool, error) {
+	return HostTestPath(ctx, host, "-f", remoteFilePath)
+}
+
+// HostTestPath runs "test <testFlag> <remoteFilePath>" on the host and
+// returns true if the test passes and false if the test fails.
+func HostTestPath(ctx context.Context, host *ssh.Conn, testFlag, remotePath string) (bool, error) {
+	if err := host.CommandContext(ctx, "test", testFlag, remotePath).Run(); err != nil {
+		if err.Error() == "Process exited with status 1" {
+			// Test was successfully evaluated and returned false.
+			return false, nil
+		}
+		return false, errors.Wrapf(err, "failed to run 'test %q %q' on remote host", testFlag, remotePath)
+	}
+	return true, nil
+}
