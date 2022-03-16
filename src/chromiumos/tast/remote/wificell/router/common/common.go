52d6ec75d12bedd54c387b4ac741b1fa2095885b
Jared Bennett <jaredbennett@google.com>
2022-03-15 17:04:40 -0700

    tast-tests: routertype and pcaptype resolved from host if not specified
    BUG=b:211517577
    TEST=tast run -var=router=$LEGACY_ROUTER -var=pcap=$LEGACY_ROUTER $DUT \
    wifi.SimpleConnect.80211a && \
    tast run -var=router=$OPENWRT_ROUTER -var=pcap=$LEGACY_PCAP $DUT \
    wifi.SimpleConnect.80211b && \
    tast run -var=router=$LEGACY_ROUTER -var=pcap=$OPENWRT_PCAP $DUT \
    wifi.SimpleConnect.80211b
    
    Change-Id: Ibfd2350489de3c5556e420292800df3bd243948f

--- src/chromiumos/tast/remote/wificell/router/common/common.go
+++ src/chromiumos/tast/remote/wificell/router/common/common.go
@@ -5,7 +5,11 @@
 package common
 
 import (
+	"context"
 	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/ssh"
 )
 
 const (
@@ -29,5 +33,34 @@ const (
 	BridgePrefix = "tastbr"
 )
 
-// RouterCloseContextDuration is a shorter context.Context duration is used for running things before Router.Close to reserve time for it to run.
+// RouterCloseContextDuration is a shorter context.Context duration is used for
+// running things before Router.Close to reserve time for it to run.
 const RouterCloseContextDuration = 5 * time.Second
+
+// HostFileContentsMatch checks if the file at remoteFilePath on the remote
+// host that matches the grepMatchStr, as determined by the host's grep command.
+//
+// Returns true if the file exists and grep finds matches. Returns false
+// with a nil error if the file does not exist. Returns false with a non-nil
+// error if there was an issue checking for file existence or running grep with
+// the file.
+func HostFileContentsMatch(ctx context.Context, host *ssh.Conn, remoteFilePath, grepMatchStr string) (bool, error) {
+	// Verify that the file exists
+	if err := host.CommandContext(ctx, "test", "-f", remoteFilePath).Run(); err != nil {
+		if err.Error() == "Process exited with status 1" {
+			// File does not exist
+			return false, nil
+		}
+		return false, errors.Wrapf(err, "failed to check for the existence of file %q", remoteFilePath)
+	}
+
+	// Verify that grep can find results with the provided match string
+	if err := host.CommandContext(ctx, "grep", "-q", grepMatchStr, remoteFilePath).Run(); err != nil {
+		if err.Error() == "Process exited with status 1" {
+			// Grep match failed
+			return false, nil
+		}
+		return false, errors.Wrapf(err, "failed to run 'grep -q %q %q'", grepMatchStr, remoteFilePath)
+	}
+	return true, nil
+}
