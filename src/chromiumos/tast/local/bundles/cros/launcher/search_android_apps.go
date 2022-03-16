fd223680eacc2ab783e6a231aad944173e3c1e5e
Toni Barzic <tbarzic@google.com>
2022-03-15 20:47:41 -0700

    Run launcher app search tests for productivity launcher
    Parameterizes launcher.SearchAndroidApps and launcher.SearchBuiltInApps
    for productivity launcher and tablet mode.
    
    BUG=chromium:1288455
    TEST=tast run <dut> launcher.Search*  (dut in kukui, nautilus,
    kukui-arc-r)
    
    Change-Id: I30126c070f62de807e37ebd4642da4915803b7e6

--- src/chromiumos/tast/local/bundles/cros/launcher/search_android_apps.go
+++ src/chromiumos/tast/local/bundles/cros/launcher/search_android_apps.go
@@ -8,14 +8,17 @@ import (
 	"context"
 	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/arc/optin"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/launcher"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -32,10 +35,41 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		Timeout:      5 * time.Minute,
 		Params: []testing.Param{{
+			Name:              "productivity_launcher_clamshell_mode",
 			ExtraSoftwareDeps: []string{"android_p"},
+			Val:               launcher.TestCase{ProductivityLauncher: true, TabletMode: false},
 		}, {
-			Name:              "vm",
+			Name:              "clamshell_mode",
+			ExtraSoftwareDeps: []string{"android_p"},
+			Val:               launcher.TestCase{ProductivityLauncher: false, TabletMode: false},
+		}, {
+			Name:              "productivity_launcher_tablet_mode",
+			Val:               launcher.TestCase{ProductivityLauncher: true, TabletMode: true},
+			ExtraSoftwareDeps: []string{"android_p"},
+			ExtraHardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+		}, {
+			Name:              "tablet_mode",
+			Val:               launcher.TestCase{ProductivityLauncher: false, TabletMode: true},
+			ExtraSoftwareDeps: []string{"android_p"},
+			ExtraHardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+		}, {
+			Name:              "productivity_launcher_clamshell_mode_vm",
 			ExtraSoftwareDeps: []string{"android_vm"},
+			Val:               launcher.TestCase{ProductivityLauncher: true, TabletMode: false},
+		}, {
+			Name:              "clamshell_mode_vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Val:               launcher.TestCase{ProductivityLauncher: false, TabletMode: false},
+		}, {
+			Name:              "productivity_launcher_tablet_mode_vm",
+			Val:               launcher.TestCase{ProductivityLauncher: true, TabletMode: true},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			ExtraHardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+		}, {
+			Name:              "tablet_mode_vm",
+			Val:               launcher.TestCase{ProductivityLauncher: false, TabletMode: true},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			ExtraHardwareDeps: hwdep.D(hwdep.InternalDisplay()),
 		}},
 		VarDeps: []string{"ui.gaiaPoolDefault"},
 	})
@@ -43,20 +77,35 @@ func init() {
 
 // SearchAndroidApps tests launching an Android app from the Launcher.
 func SearchAndroidApps(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	testCase := s.Param().(launcher.TestCase)
+	tabletMode := testCase.TabletMode
+
+	productivityLauncher := testCase.ProductivityLauncher
+	var launcherFeatureOpt chrome.Option
+	if productivityLauncher {
+		launcherFeatureOpt = chrome.EnableFeatures("ProductivityLauncher")
+	} else {
+		launcherFeatureOpt = chrome.DisableFeatures("ProductivityLauncher")
+	}
 
 	cr, err := chrome.New(ctx,
 		chrome.GAIALoginPool(s.RequiredVar("ui.gaiaPoolDefault")),
 		chrome.ARCSupported(),
-		chrome.ExtraArgs(arc.DisableSyncFlags()...))
+		chrome.ExtraArgs(arc.DisableSyncFlags()...),
+		launcherFeatureOpt)
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
-	defer cr.Close(ctx)
+	defer cr.Close(cleanupCtx)
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to create test API connection: ", err)
 	}
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
 
 	if err := optin.PerformAndClose(ctx, cr, tconn); err != nil {
 		s.Fatal("Failed to optin to Play Store and Close: ", err)
@@ -67,7 +116,7 @@ func SearchAndroidApps(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to start ARC: ", err)
 	}
-	defer a.Close(ctx)
+	defer a.Close(cleanupCtx)
 	if err := a.WaitIntentHelper(ctx); err != nil {
 		s.Fatal("Failed to wait for ARC Intent Helper: ", err)
 	}
@@ -77,6 +126,20 @@ func SearchAndroidApps(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to find keyboard: ", err)
 	}
 	defer kb.Close()
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, tabletMode)
+	if err != nil {
+		s.Fatal("Failed to ensure clamshell/tablet mode: ", err)
+	}
+	defer cleanup(cleanupCtx)
+
+	// When a DUT switches from tablet mode to clamshell mode, sometimes it takes a while to settle down.
+	if !tabletMode {
+		if err := ash.WaitForLauncherState(ctx, tconn, ash.Closed); err != nil {
+			s.Fatal("Launcher not closed after transition to clamshell mode: ", err)
+		}
+	}
+
 	if err := launcher.SearchAndWaitForAppOpen(tconn, kb, apps.PlayStore)(ctx); err != nil {
 		s.Fatal("Failed to launch Play Store: ", err)
 	}
--- src/chromiumos/tast/local/bundles/cros/launcher/search_built_in_apps.go
+++ src/chromiumos/tast/local/bundles/cros/launcher/search_built_in_apps.go
@@ -6,13 +6,17 @@ package launcher
 
 import (
 	"context"
+	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/launcher"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 func init() {
@@ -27,29 +31,65 @@ func init() {
 		},
 		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
-		Params: []testing.Param{
-			{
-				Val:     apps.Settings,
-				Fixture: "chromeLoggedIn",
-			}},
+		Params: []testing.Param{{
+			Name:    "productivity_launcher_clamshell_mode",
+			Fixture: "chromeLoggedInWith100FakeAppsProductivityLauncher",
+			Val:     launcher.TestCase{ProductivityLauncher: true, TabletMode: false},
+		}, {
+			Name:    "clamshell_mode",
+			Fixture: "chromeLoggedInWith100FakeAppsLegacyLauncher",
+			Val:     launcher.TestCase{ProductivityLauncher: false, TabletMode: false},
+		}, {
+			Name:              "productivity_launcher_tablet_mode",
+			Fixture:           "chromeLoggedInWith100FakeAppsProductivityLauncher",
+			Val:               launcher.TestCase{ProductivityLauncher: true, TabletMode: true},
+			ExtraHardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+		}, {
+			Name:              "tablet_mode",
+			Fixture:           "chromeLoggedInWith100FakeAppsLegacyLauncher",
+			Val:               launcher.TestCase{ProductivityLauncher: false, TabletMode: true},
+			ExtraHardwareDeps: hwdep.D(hwdep.InternalDisplay()),
+		}},
 	})
 }
 
 // SearchBuiltInApps searches for the Settings app in the Launcher.
 func SearchBuiltInApps(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(*chrome.Chrome)
-	app := s.Param().(apps.App)
+	app := apps.Settings
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to connect Test API: ", err)
 	}
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
 
 	kb, err := input.Keyboard(ctx)
 	if err != nil {
 		s.Fatal("Failed to find keyboard: ", err)
 	}
 	defer kb.Close()
+
+	testCase := s.Param().(launcher.TestCase)
+	tabletMode := testCase.TabletMode
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, tabletMode)
+	if err != nil {
+		s.Fatal("Failed to ensure clamshell/tablet mode: ", err)
+	}
+	defer cleanup(cleanupCtx)
+
+	// When a DUT switches from tablet mode to clamshell mode, sometimes it takes a while to settle down.
+	if !tabletMode {
+		if err := ash.WaitForLauncherState(ctx, tconn, ash.Closed); err != nil {
+			s.Fatal("Launcher not closed after transition to clamshell mode: ", err)
+		}
+	}
+
 	if err := launcher.SearchAndWaitForAppOpen(tconn, kb, app)(ctx); err != nil {
 		s.Fatal("Failed to launch app: ", err)
 	}
