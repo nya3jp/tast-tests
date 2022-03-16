12bfeb0b84eaed19081c85611c4702cc11dab0aa
lschyi <lschyi@google.com>
2022-03-16 16:58:03 +0800

    factory: Add toolkit service for installation/uninstallation
    The commands for installing/uninstalling factory toolkit in remote and
    local tests are the same, but cannot share between them as the API for
    sending shell command to the DUT is different in remote and local tests.
    
    Create a dedicated service for handling requests from remote tests to
    install/uninstall factory toolkit so that we only need to maintain one
    implementation for instructing DUT to install/uninstall the factory
    toolkit in local test packages.
    
    BUG=b:225083013
    TEST=None
    
    Change-Id: I2e2fa0b085b8231e61e6f1dae5d3cb397ab77e3e

---
+++ src/chromiumos/tast/local/bundles/cros/factory/toolkit/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package toolkit contains local Tast tests that exercise factory toolkit
+// installation and uninstallation.
+package toolkit
