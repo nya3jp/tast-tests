9c7866dee715d3310b58b9ff1d976f2f03c9f698
Eliot Courtney <edcourtney@google.com>
2022-03-17 12:58:05 +0900

    Remove killLacros.
    This is to reduced the number of dependencies on lacrosPath, as part of
    getting rid of command line launch of lacros in Tast tests. Since lacros
    will be controlled by ash, we shouldn't kill it manually.
    
    BUG=chromium:1300410
    TEST=tast run lacros.Basic
    
    Change-Id: I3e0506b7943ee67f97b86e55d04cd902aaaa400a

--- src/chromiumos/tast/local/chrome/lacros/util.go
+++ src/chromiumos/tast/local/chrome/lacros/util.go
@@ -7,11 +7,7 @@ package lacros
 import (
 	"context"
 	"strings"
-	"syscall"
 	"time"
-
-	"github.com/shirou/gopsutil/process"
-	"golang.org/x/sys/unix"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
@@ -66,49 +62,4 @@ func CloseLacros(ctx context.Context, l *Lacros) {
 	if l != nil {
 		l.Close(ctx) // Ignore error.
 	}
-}
-
-// killLacros kills all binaries whose executable contains the base path
-// to lacros-chrome.
-func killLacros(ctx context.Context, lacrosPath string) error {
-	if lacrosPath == "" {
-		return errors.New("Path to lacros-chrome cannot be empty")
-	}
-
-	// Kills all instances of lacros-chrome and other related executables.
-	pids, err := PidsFromPath(ctx, lacrosPath)
-	if err != nil {
-		return errors.Wrap(err, "error finding pids for lacros-chrome")
-	}
-	for _, pid := range pids {
-		// We ignore errors, since it's possible the process has
-		// already been killed.
-		unix.Kill(pid, syscall.SIGKILL)
-	}
-	return nil
-}
-
-// PidsFromPath returns the pids of all processes with a given path in their
-// command line. This is typically used to find all chrome-related binaries,
-// e.g. chrome, nacl_helper, etc. They typically share a path, even though their
-// binary names differ.
-// There may be a race condition between calling this method and using the pids
-// later. It's possible that one of the processes is killed, and possibly even
-// replaced with a process with the same pid.
-func PidsFromPath(ctx context.Context, path string) ([]int, error) {
-	all, err := process.Pids()
-	if err != nil {
-		return nil, err
-	}
-
-	pids := make([]int, 0)
-	for _, pid := range all {
-		if proc, err := process.NewProcess(pid); err != nil {
-			// Assume that the process exited.
-			continue
-		} else if exe, err := proc.Exe(); err == nil && strings.Contains(exe, path) {
-			pids = append(pids, int(pid))
-		}
-	}
-	return pids, nil
 }
