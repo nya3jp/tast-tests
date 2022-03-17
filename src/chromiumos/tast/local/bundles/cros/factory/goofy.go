59888c9cd04a2449bed2e37b7b92da5fdd9588c9
lschyi <lschyi@google.com>
2022-03-17 10:33:40 +0800

    factory: Refactor remote and local tests for installing factory toolkit
    Based on the dedicated toolkit service to install/uninstall the factory
    toolkit is built, refactor the code in local test and remote fixture to
    use it.
    
    BUG=b:225083013
    TEST=tast run $DUT factory.Goofy factory.Kiosk factory.Finalize
    factory.Toolkit
    
    Change-Id: If090c01318cbe487b83b95ec9ffec4d57a047ed2

--- src/chromiumos/tast/local/bundles/cros/factory/goofy.go
+++ src/chromiumos/tast/local/bundles/cros/factory/goofy.go
@@ -6,11 +6,8 @@ package factory
 
 import (
 	"context"
-	"encoding/json"
-	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"chromiumos/tast/common/testexec"
@@ -46,11 +43,6 @@ func Goofy(fullCtx context.Context, s *testing.State) {
 	setupFactory(ctx, s)
 	defer cleanup(fullCtx, s)
 
-	if err := setTestList(ctx, testListName); err != nil {
-		s.Fatal("Failed to set TestList: ", err)
-	}
-	s.Logf("TestList is set to %s", testListName)
-
 	// Make sure the temp file is not there.
 	os.Remove(finishFlagFilePath)
 	if err := testexec.CommandContext(ctx, "start", "factory").Run(testexec.DumpLogOnError); err != nil {
@@ -71,7 +63,12 @@ func Goofy(fullCtx context.Context, s *testing.State) {
 }
 
 func setupFactory(ctx context.Context, s *testing.State) {
-	ver, err := installFactoryToolKit(ctx, s)
+	installer := toolkit.Installer{
+		InstallerPath: toolkit.ToolkitInstallerPath,
+		NoEnable:      true,
+		TestList:      toolkit.TastTestListName,
+	}
+	ver, err := installer.InstallFactoryToolKitFromToolkitInstaller(ctx)
 	if err != nil {
 		s.Fatal("Install fail: ", err)
 	}
@@ -103,84 +100,8 @@ func cleanup(ctx context.Context, s *testing.State) {
 	s.Log("Uninstalled factory toolkit")
 }
 
-func installFactoryToolKit(ctx context.Context, s *testing.State) (version string, err error) {
-	// the path should be synced with factory-mini.ebuild
-	const toolkitInstallerPath = "/usr/local/factory-toolkit/install_factory_toolkit.run"
-
-	if _, err := os.Stat(toolkitInstallerPath); err != nil {
-		return "", errors.Wrapf(err, "failed to find factory toolkit installer: %s", toolkitInstallerPath)
-	}
-
-	// TODO(b/150189948): Support installing toolkit from artifacts
-	// factory_image.zip
-	return installFactoryToolKitFromToolkitInstaller(ctx, toolkitInstallerPath)
-}
-
-func installFactoryToolKitFromFactoryImage(ctx context.Context, imagePath string) (version string, err error) {
-	// Create an temp directory.
-	tempDir, err := ioutil.TempDir("", "")
-	if err != nil {
-		return "", errors.Wrap(err, "failed to create temp dir")
-	}
-	defer os.RemoveAll(tempDir)
-
-	// Unzip toolkit installer from image.
-	if err := testexec.CommandContext(ctx, "unzip", imagePath, "toolkit/install_factory_toolkit.run", "-d", tempDir).Run(testexec.DumpLogOnError); err != nil {
-		return "", errors.Wrap(err, "failed to unzip factory toolkit from factory_image.zip")
-	}
-
-	installerPath := filepath.Join(tempDir, "toolkit/install_factory_toolkit.run")
-	return installFactoryToolKitFromToolkitInstaller(ctx, installerPath)
-}
-
-func installFactoryToolKitFromToolkitInstaller(ctx context.Context, installerPath string) (version string, err error) {
-	if err := testexec.CommandContext(ctx, installerPath, "--", "--yes", "--no-enable").Run(testexec.DumpLogOnError); err != nil {
-		return "", errors.Wrap(err, "failed to install factory toolkit")
-	}
-
-	// Get the factory toolkit version.
-	b, err := ioutil.ReadFile("/usr/local/factory/TOOLKIT_VERSION")
-	if err != nil {
-		return "", errors.Wrap(err, "failed to read version file")
-	}
-	version = strings.TrimSpace(string(b))
-	return version, nil
-}
-
-func uninstallFactoryToolKit(ctx context.Context) error {
-	return testexec.CommandContext(ctx, "factory_uninstall", "--yes").Run(testexec.DumpLogOnError)
-}
-
 // stopGoofy and cleanup all factory configuration.
 func stopGoofy(ctx context.Context) error {
 	// Cleanup the state files except logs.
 	return testexec.CommandContext(ctx, "factory_restart", "-S", "-s", "-t", "-r").Run(testexec.DumpLogOnError)
-}
-
-// setTestList set the default TestList to `testListID` via factory test-list
-// command.
-func setTestList(ctx context.Context, testListID string) error {
-	type DataFile struct {
-		ID string `json:"id"`
-	}
-	const testListConfigPath = "/usr/local/factory/py/test/test_lists/active_test_list.json"
-	var data DataFile
-
-	if err := testexec.CommandContext(ctx, "factory", "test-list", testListID).Run(testexec.DumpLogOnError); err != nil {
-		return errors.Wrap(err, "failed to execute `factory test-list` command")
-	}
-
-	// Verify the config content is correct.
-	b, err := ioutil.ReadFile(testListConfigPath)
-	if err != nil {
-		return errors.Wrap(err, "failed to read TestList config file")
-	}
-	if err := json.Unmarshal(b, &data); err != nil {
-		return errors.Wrap(err, "failed to decode JSON")
-	}
-	if data.ID != testListID {
-		return errors.Wrap(err, "testList config file doesn't contain the correct test id")
-	}
-
-	return nil
 }
