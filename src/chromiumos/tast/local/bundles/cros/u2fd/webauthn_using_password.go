d2c420f669ce6de3909ae7d8982e99d0f22701c6
Howard Yang <hcyang@google.com>
2022-03-17 14:14:25 +0800

    u2fd: Promote WebauthnUsingPassword to CQ
    Remove the informational attribute from the u2fd.WebauthnUsingPassword
    test so it runs on CQ. This test passed 17 consecutive builds after
    landed until it meets an issue that is fixed by now, and it continues to
    pass. There's no observed flakiness in this test, making it suitable for
    running in CQ. In addition, we currently have no tests in CQ about u2fd
    such that CQ will pass even if u2fd fails to start at all. We want a
    stable test about u2f/WebAuthn running in CQ to monitor if any CL breaks
    its behavior.
    
    BUG=None
    TEST=tastboard consistently passing
    
    Change-Id: I06fb983614c5bd074536f733647a1c10221867b7

--- src/chromiumos/tast/local/bundles/cros/u2fd/webauthn_using_password.go
+++ src/chromiumos/tast/local/bundles/cros/u2fd/webauthn_using_password.go
@@ -29,7 +29,7 @@ func init() {
 			"hcyang@google.com",
 			"cros-hwsec@chromium.org",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Name:              "tpm1",
