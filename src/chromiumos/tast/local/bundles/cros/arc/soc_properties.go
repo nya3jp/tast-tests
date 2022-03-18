3cf8e1000615d448bc4a7d1e2cc258d1c6967648
Matthew DeVore <matvore@google.com>
2022-03-17 17:52:25 -0700

    soc_properties: informational test to validate ro.soc.*
    Verifies ro.soc.manufacturer and ro.soc.model conform to the expected
    format. This is informational since the properties do not work for all
    SOC models of possible devices. It will be switched to critical when all
    models can be detected.
    
    The ro.soc.model regexp is based on that found in
    go/cdd-proposal-signoff, but made a little more restrictive.
    
    BUG=b:175610620,b:225373614
    TEST=Verified a more restrictive regexp for either prop causes test failure
    TEST=Verified a missing property causes test failure
    
    Change-Id: Id7a6a3217c46f07754d6aca5113051934e3ea2d2

---
+++ src/chromiumos/tast/local/bundles/cros/arc/soc_properties.go
@@ -0,0 +1,88 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"chromiumos/tast/fsutil"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SocProperties,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks SOC model-related properties (ro.soc.*)",
+		Contacts:     []string{"matvore@google.com", "niwa@chromium.org", "arcvm-eng@google.com"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "arcBooted",
+		Timeout:      4 * time.Minute,
+
+		// TODO(b/225373614): Merge with BuildProperties once all SOCs
+		// can be detected, which will make this testcase
+		// non-informational and CQ-blocking.
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func SocProperties(ctx context.Context, s *testing.State) {
+	const (
+		propertyManufacturer = "ro.soc.manufacturer"
+		propertyModel        = "ro.soc.model"
+	)
+
+	a := s.FixtValue().(*arc.PreData).ARC
+
+	// TODO(b/225373614): De-duplicate with the identical anonymous function
+	// in BuildProperties, once these two tests are merged. It is tricky to
+	// do now because it captures three local vars and args.
+	getProperty := func(propertyName string) string {
+		var value string
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			out, err := a.Command(ctx, "getprop", propertyName).Output()
+			if err != nil {
+				return err
+			}
+			value = strings.TrimSpace(string(out))
+
+			return nil
+		}, &testing.PollOptions{Timeout: 10 * time.Second}); err != nil {
+			s.Fatalf("Failed to get %q: %v", propertyName, err)
+		}
+
+		return value
+	}
+
+	// getprop will not return error if the property is not found, just an
+	// empty line (\x0a). So we save extra debugging info whenever the
+	// regexp does not match.
+	saveCpuinfo := false
+
+	manufacturer := getProperty(propertyManufacturer)
+	s.Logf("manufacturer: %q", manufacturer)
+	if re := regexp.MustCompile(`^(?:Intel|AMD|Mediatek|Rockchip|Qualcomm)$`); !re.MatchString(manufacturer) {
+		s.Errorf("%s property is missing or ill-formed: %q", propertyManufacturer, manufacturer)
+		saveCpuinfo = true
+	}
+
+	model := getProperty(propertyModel)
+	s.Logf("model: %q", model)
+	if re := regexp.MustCompile(`^[0-9A-Za-z /-]+$`); !re.MatchString(model) {
+		s.Errorf("%s property is missing or ill-formed: %q", propertyModel, model)
+		saveCpuinfo = true
+	}
+
+	if saveCpuinfo {
+		dest := filepath.Join(s.OutDir(), "cpuinfo")
+		err := fsutil.CopyFile("/proc/cpuinfo", dest)
+		s.Logf("saved <test_output_dir>/cpuinfo (err=%v)", err)
+	}
+}
