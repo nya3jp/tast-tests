d76bd5ffcc27a035ffce443fb1a4d3dbde30d797
Andrew Lassalle <andrewlassalle@chromium.org>
2022-03-23 16:13:12 -0700

    cellular: Verify the contents of the modem DLCs
    If a board uses modem DLCs, verify that all files are present in the
    DLCs, as well as in the rootfs.
    
    BUG=b:210722875
    TEST=tast run tast_nipperkinoffice cellular.ModemFWManifestVerification
    TEST=tast run tast_storo cellular.ModemFWManifestVerification
    
    Change-Id: I8dd75729984f5eb7f89bc24fd173d24a56d2e469

--- src/chromiumos/tast/local/bundles/cros/cellular/modem_fw_manifest_verification.go
+++ src/chromiumos/tast/local/bundles/cros/cellular/modem_fw_manifest_verification.go
@@ -11,6 +11,8 @@ import (
 
 	"chromiumos/tast/local/cellular"
 	"chromiumos/tast/local/dlc"
+	"chromiumos/tast/local/modemfwd"
+	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
 
@@ -31,11 +33,20 @@ func ModemFWManifestVerification(ctx context.Context, s *testing.State) {
 		return !os.IsNotExist(err)
 	}
 
+	// Stop modemfwd so that the DLCs don't get uninstalled in the middle of the test.
+	if err := upstart.StopJob(ctx, modemfwd.JobName); err != nil {
+		s.Fatalf("Failed to stop %q: %s", modemfwd.JobName, err)
+	}
+
 	if !fileExists(cellular.GetModemFirmwareManifestPath()) {
 		s.Fatal("Cannot find ", cellular.GetModemFirmwareManifestPath())
 	}
 
-	modemFirmwarePath := cellular.GetModemFirmwarePath()
+	dutVariant, err := cellular.GetDeviceVariant(ctx)
+	if err != nil {
+		s.Fatalf("Failed to get device variant: %s", err)
+	}
+
 	manifest, err := cellular.ParseModemFirmwareManifest(ctx)
 	if err != nil {
 		s.Fatal("Failed to parse the firmware manifest: ", err)
@@ -43,53 +54,78 @@ func ModemFWManifestVerification(ctx context.Context, s *testing.State) {
 
 	missingFiles := make(map[string]bool)
 	var mainFirmwares map[string]bool
+	dlcCounter := 0
 	for _, device := range manifest.Device {
-		// Verify that we don't have repeated main FWs.
-		mainFirmwares = make(map[string]bool)
-		for _, mainFirmware := range device.MainFirmware {
-			if mainFirmwares[mainFirmware.Version] {
-				s.Fatalf("Repeated value for main firmware: %q in variant: %q", mainFirmware.Version, device.Variant)
-			}
-			mainFirmwares[mainFirmware.Version] = true
-		}
-		// Verify that all files exist.
-		for _, mainFW := range device.MainFirmware {
-			if !fileExists(filepath.Join(modemFirmwarePath, mainFW.Filename)) {
-				missingFiles[mainFW.Filename] = true
-			}
-		}
-		for _, oemFW := range device.OemFirmware {
-			if !fileExists(filepath.Join(modemFirmwarePath, oemFW.Filename)) {
-				missingFiles[oemFW.Filename] = true
-			}
-			// Verify if main FWs used by OEM FW exist.
-			for _, oemVersion := range oemFW.MainFirmwareVersion {
-				if !mainFirmwares[oemVersion] {
-					s.Fatalf("Main firmware %q referenced by OEM FW %q does not exist", oemVersion, oemFW.Version)
-				}
-			}
-		}
-		for _, carrierFW := range device.CarrierFirmware {
-			if !fileExists(filepath.Join(modemFirmwarePath, carrierFW.Filename)) {
-				missingFiles[carrierFW.Filename] = true
-			}
-			// Verify if main FWs used by carrier FW exist.
-			if carrierFW.MainFirmwareVersion != "" && !mainFirmwares[carrierFW.MainFirmwareVersion] {
-				s.Fatalf("Main firmware %q referenced by carrier FW %q does not exist", carrierFW.MainFirmwareVersion, carrierFW.Version)
-			}
-			if len(carrierFW.CarrierId) == 0 {
-				s.Fatalf("There is no carrier id defined for carrier FW %q", carrierFW.Version)
-			}
-		}
-	}
+		// rootfs path
+		modemFirmwarePaths := []string{cellular.GetModemFirmwarePath()}
 
-	// Verify that the DLC exists in the dlcservice manifest
-	for _, device := range manifest.Device {
 		if device.GetDlcId() != "" {
-			_, err := dlc.GetDlcState(ctx, device.DlcId)
-
+			dlcCounter++
+			// Only the variant that matches the device's variant will contain a DLC that is
+			// already installed by modemfwd.
+			// Manually install the DLCs for other variants. This won't work on non test
+			// images since the DLCs are purged.  It works on test images because the modem
+			// DLCs have the DLC_PRELOAD flag in their ebuilds.
+			if dutVariant != device.Variant {
+				dlc.Install(ctx, device.DlcId, "")
+			}
+			state, err := dlc.GetDlcState(ctx, device.DlcId)
+			// Verify that the DLC exists in the dlcservice manifest
 			if err != nil {
 				s.Fatalf("Failed to get state info for DLC %q: %q", device.DlcId, err)
+			}
+			if state.RootPath == "" {
+				s.Fatalf("Failed to get mount path for DLC %q", device.DlcId)
+			}
+			// Append the DLC mount path for verification.
+			// Until DLC is not fully proven to work 100% for modemfwd, we keep a copy of all
+			// modem FWs in the rootfs, so we need to verify that the files are in both locations.
+			// When it's time to remove those images from the roofs, only one path will be needed,
+			// and the DLC path will override the rootfs path.
+			modemFirmwarePaths = append(modemFirmwarePaths, state.RootPath)
+		}
+
+		for _, modemFirmwarePath := range modemFirmwarePaths {
+			s.Logf("Firmware path location for variant %q: %q", device.Variant, modemFirmwarePath)
+			// Verify that we don't have repeated main FWs.
+			mainFirmwares = make(map[string]bool)
+			for _, mainFirmware := range device.MainFirmware {
+				if mainFirmwares[mainFirmware.Version] {
+					s.Fatalf("Repeated value for main firmware: %q in variant: %q", mainFirmware.Version, device.Variant)
+				}
+				mainFirmwares[mainFirmware.Version] = true
+			}
+			// Verify that all files exist.
+			for _, mainFW := range device.MainFirmware {
+				fullPath := filepath.Join(modemFirmwarePath, mainFW.Filename)
+				if !fileExists(fullPath) {
+					missingFiles[fullPath] = true
+				}
+			}
+			for _, oemFW := range device.OemFirmware {
+				fullPath := filepath.Join(modemFirmwarePath, oemFW.Filename)
+				if !fileExists(fullPath) {
+					missingFiles[fullPath] = true
+				}
+				// Verify if main FWs used by OEM FW exist.
+				for _, oemVersion := range oemFW.MainFirmwareVersion {
+					if !mainFirmwares[oemVersion] {
+						s.Fatalf("Main firmware %q referenced by OEM FW %q does not exist", oemVersion, oemFW.Version)
+					}
+				}
+			}
+			for _, carrierFW := range device.CarrierFirmware {
+				fullPath := filepath.Join(modemFirmwarePath, carrierFW.Filename)
+				if !fileExists(fullPath) {
+					missingFiles[fullPath] = true
+				}
+				// Verify if main FWs used by carrier FW exist.
+				if carrierFW.MainFirmwareVersion != "" && !mainFirmwares[carrierFW.MainFirmwareVersion] {
+					s.Fatalf("Main firmware %q referenced by carrier FW %q does not exist", carrierFW.MainFirmwareVersion, carrierFW.Version)
+				}
+				if len(carrierFW.CarrierId) == 0 {
+					s.Fatalf("There is no carrier id defined for carrier FW %q", carrierFW.Version)
+				}
 			}
 		}
 	}
@@ -100,5 +136,9 @@ func ModemFWManifestVerification(ctx context.Context, s *testing.State) {
 			keys = append(keys, k)
 		}
 		s.Fatalf("The following firmware files are specified in the manifest, but don't exist: %q", keys)
+	}
+
+	if dlcCounter > 0 && dlcCounter != len(manifest.Device) {
+		s.Fatal("There is an unequal number of variants and DLCs")
 	}
 }
