a3cdbec5e5cdd991329fac097529a65db379623a
lschyi <lschyi@google.com>
2022-03-24 10:59:19 +0800

    factory: Add HWID database test
    The HWID database generation is composed of the probing and then
    building a database. The probing stage could be affected by the command
    , tools it calls, or different hardware types in the real world. The
    probing output is directly passed to building stage, thus any
    unexpected output or failure breaks building the database. Add a test to
    ensure the flow from probing to building database is valid, and catch
    any possible regression that breaks this flow in lab.
    
    BUG=b:223075358
    TEST=tast run $DUT factory.HWIDDatabase
    
    Change-Id: I748711017f6d8c9918d03439cdf1aadd2801dcc2

---
+++ src/chromiumos/tast/remote/bundles/cros/factory/hwiddatabase.go
@@ -0,0 +1,46 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package factory
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/remote/bundles/cros/factory/fixture"
+	"chromiumos/tast/ssh"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HWIDDatabase,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Test the flow from collecting materials, generating HWID database, to verifing the database is valid",
+		Contacts:     []string{"lschyi@google.com", "chromeos-factory-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		Timeout:      2 * time.Minute,
+		Fixture:      fixture.EnsureToolkit,
+		// Skip "nyan_kitty" due to slow reboot speed.
+		HardwareDeps: hwdep.D(hwdep.SkipOnModel("kitty")),
+		SoftwareDeps: append([]string{"factory_flow"}, fixture.EnsureToolkitSoftwareDeps...),
+	})
+}
+
+func HWIDDatabase(ctx context.Context, s *testing.State) {
+	conn := s.DUT().Conn()
+	buildDatabaseCmd := conn.CommandContext(ctx, "hwid", "build-database")
+	if err := buildDatabaseCmd.Run(ssh.DumpLogOnError); err != nil {
+		s.Fatal("Failed to build the HWID database: ", err)
+	}
+
+	// Only verifies the format, probed components. As the database is built
+	// locally and checksum is not the testing target, skip the check of
+	// checksum here.
+	verifyDatabaseCmd := conn.CommandContext(ctx, "hwid", "verify-database", "--no-verify-checksum")
+	if err := verifyDatabaseCmd.Run(ssh.DumpLogOnError); err != nil {
+		s.Fatal("Verify built HWID database failed: ", err)
+	}
+}
