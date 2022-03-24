ba5c2ef933ad4e3297edcab43b4e5d1ab3c5eeba
David Stevens <stevensd@chromium.org>
2022-03-24 19:33:09 +0900

    memory: add ManaTEE memory metrics
    Add ManaTEE stats to memory metrics. The primary metric is the amount
    of memory used by the hypervisor outside of directly backing guest
    memory. This also adds metrics for total memory visible in the
    hypervisor, which tracks memory allocated very early in the hypervisor
    kernel's boot, and unevictable memory, which tracks initramfs size.
    
    BUG=b:224657537
    TEST=multivm.Login.novm
    
    Change-Id: I08e4da093d88c9e8d1bf1a900f86655050afa207

---
+++ src/chromiumos/tast/local/memory/manatee.go
@@ -0,0 +1,125 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package memory
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+)
+
+var pgrepRE = regexp.MustCompile(`(?m)^(\d+)$`)
+var meminfoRE = regexp.MustCompile(`(?m)^(\S+):\s+(\d+) kB$`)
+
+// ManaTEEMetrics generates metrics for ManaTEE hypervisor memory usage.
+func ManaTEEMetrics(ctx context.Context, p *perf.Values, outdir, suffix string) error {
+	manateePath, err := exec.LookPath("manatee")
+	if err != nil {
+		// If the manatee binary doesn't exist, jkkk
+		return nil
+	}
+
+	pgrepCmd := testexec.CommandContext(ctx, manateePath, "-a", "shell-notty", "--", "-c", "pgrep crosvm")
+	pgrepOut, err := pgrepCmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		return errors.Wrap(err, "failed to execute pgrep")
+	}
+	var pids []int
+	matches := pgrepRE.FindAllSubmatch(pgrepOut, -1)
+	for _, match := range matches {
+		pid, err := strconv.Atoi(string(match[1]))
+		if err != nil {
+			return errors.Wrapf(err, "failed to parse crosvm pid %q", match[1])
+		}
+		pids = append(pids, pid)
+	}
+
+	var crosGuestRssKiB uint64
+	for _, pid := range pids {
+		manateeCmd := fmt.Sprintf("cat /proc/%d/smaps", pid)
+
+		smapsCmd := testexec.CommandContext(ctx, manateePath, "-a", "shell-notty", "--", "-c", manateeCmd)
+		smapsOut, err := smapsCmd.Output(testexec.DumpLogOnError)
+		if err != nil {
+			return errors.Wrap(err, "failed to cat smaps")
+		}
+		smaps, err := ParseSmapsData(smapsOut)
+		if err != nil {
+			return errors.Wrap(err, "failed to parse smaps")
+		}
+		for _, smap := range smaps {
+			if strings.Contains(smap.name, "crosvm_guest") {
+				crosGuestRssKiB += smap.rss
+			}
+		}
+	}
+
+	if crosGuestRssKiB == 0 {
+		return errors.Wrap(err, "failed to find crosvm guest memory")
+	}
+
+	meminfoCmd := testexec.CommandContext(ctx, manateePath, "-a", "shell-notty", "--", "-c", "cat /proc/meminfo")
+	meminfoOut, err := meminfoCmd.Output(testexec.DumpLogOnError)
+	if err != nil {
+		return errors.Wrap(err, "failed to cat meminfo")
+	}
+
+	var memTotatKiB, memFreeKiB, unevictableKiB uint64 = 0, 0, 0
+	matches = meminfoRE.FindAllSubmatch(meminfoOut, -1)
+	for _, match := range matches {
+		val, err := strconv.ParseUint(string(match[2]), 10, 64)
+		if err != nil {
+			return errors.Wrapf(err, "failed to parse value %q", match[2])
+		}
+		if string(match[1]) == "MemTotal" {
+			memTotatKiB = val
+		} else if string(match[1]) == "MemFree" {
+			memFreeKiB = val
+		} else if string(match[1]) == "Unevictable" {
+			unevictableKiB = val
+		}
+	}
+	if memTotatKiB == 0 || memFreeKiB == 0 {
+		return errors.Errorf("failed to read host memory %d %d", memTotatKiB, memFreeKiB)
+	}
+
+	hypervisorMemKiB := memTotatKiB - memFreeKiB - crosGuestRssKiB
+
+	p.Set(
+		perf.Metric{
+			Name:      fmt.Sprintf("manatee_total_mem%s", suffix),
+			Unit:      "MiB",
+			Direction: perf.SmallerIsBetter,
+		},
+		float64(memTotatKiB)/KiBInMiB,
+	)
+
+	p.Set(
+		perf.Metric{
+			Name:      fmt.Sprintf("manatee_hypervisor_mem%s", suffix),
+			Unit:      "MiB",
+			Direction: perf.SmallerIsBetter,
+		},
+		float64(hypervisorMemKiB)/KiBInMiB,
+	)
+
+	p.Set(
+		perf.Metric{
+			Name:      fmt.Sprintf("manatee_hypervisor_unevictable%s", suffix),
+			Unit:      "MiB",
+			Direction: perf.SmallerIsBetter,
+		},
+		float64(unevictableKiB)/KiBInMiB,
+	)
+
+	return nil
+}
