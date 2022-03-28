495e02d4936ec36b5220ff589d6ca9e0a268743b
Pavan Holla <pholla@chromium.org>
2022-03-28 06:11:34 +0000

    hermes: Migrate autotest suite to tast
    Since tast provides a stork library, and is used by the UI team as well,
    we are deprecating the autotest hermes suite.
    
    BUG=b:219591580
    TEST=tast run $ip cellular.HermesTestCI.hermes_only
    
    Change-Id: I96783d17bc3033bdd00f57109ef32a9c8efb5e36

--- src/chromiumos/tast/local/hermes/hermes.go
+++ src/chromiumos/tast/local/hermes/hermes.go
@@ -6,6 +6,14 @@
 // https://chromium.googlesource.com/chromiumos/platform2/+/HEAD/hermes/README.md
 package hermes
 
+import (
+	"context"
+	"reflect"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+)
+
 // Hermes D-Bus constants.
 const (
 	DBusHermesManagerPath      = "/org/chromium/Hermes/Manager"
@@ -14,3 +22,25 @@ const (
 	DBusHermesEuiccInterface   = "org.chromium.Hermes.Euicc"
 	DBusHermesProfileInterface = "org.chromium.Hermes.Profile"
 )
+
+// Choose test type
+const (
+	HermesOnly  = "hermes_only"
+	HermesAndMM = "hermes_and_mm"
+)
+
+// CheckProperty reads a DBus property on a DBusObject. Returns an error if the value does not match the expected value
+func CheckProperty(ctx context.Context, o *dbusutil.DBusObject, prop string, expected interface{}) error {
+	var actual interface{}
+	if err := o.Get(ctx, prop, &actual); err != nil {
+		return errors.Wrap(err, "failed to check property")
+	}
+	if reflect.TypeOf(actual) != reflect.TypeOf(expected) {
+		return errors.Errorf("unexpected %s, got: %T, want: %T", prop, actual, expected)
+	}
+	if actual != expected {
+		return errors.Errorf("unexpected %s, got: %v, want: %v", prop, actual, expected)
+	}
+
+	return nil
+}
