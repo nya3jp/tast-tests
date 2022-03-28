3b20e1d2eddb361d4d62793987d174c391a73304
Ambalavanan MM <ambalavanan.m.m@intel.com>
2022-03-28 09:59:06 +0530

    camera: Add CCAUIMultiCameraSuspendResume test
    This patch takes picture or records video in camera, performs suspend
    resume and checks if camera instance exists after resume.
    
    BUG=b:225081951
    TEST=tast run -var="servo=localhost:9999" <DUT IP>
    camera.CCAUIMultiCameraSuspendResume.user_facing_photo
    
    Change-Id: I320c5f308e2287d0c757b95f0690ecc0343fbb8b

---
+++ src/chromiumos/tast/services/cros/camera/gen.go
@@ -0,0 +1,12 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. cca_service.proto
+
+// Package camera provides all camera related types compiled from protobuf.
+package camera
+
+// Run the following command in CrOS chroot to regenerate protocol buffer bindings:
+//
+// ~/trunk/src/platform/tast/tools/go.sh generate chromiumos/tast/services/cros/camera
