3b6fe128efc569a0b59bdc80456c9521e2509a7d
Cristina Guerrero <crisguerrero@chromium.org>
2022-03-29 14:30:52 +0000

    tast-tests: Add PEAP wifi to rollback test
    Extend rollback_service to support the verification of multiple
    networks and make these transparent to the tests that use the
    service. Add verification of preservation of a PEAP wifi.
    
    Rollback integration tests only check a PSK network is preserved,
    but more type of networks are supported by rollback. Extend the
    service so it is possible to add more type of networks to the tests.
    The configuration of the supported networks is transparent to the
    tests that make use of it.
    
    Create supported_networks to contain the list of configurations
    and methods to verify the networks are preserved. The service does
    not have detailed information about the supported networks and make
    use of this file to being able to set and test them.
    
    BUG=chromium:1264389, b/217546714, b/227562233
    TEST=tast -verbose run DUT policy.EnterpriseRollbackInPlace
    tast run -var=updateutil.tlwAddress="localhost:7151" localhost:2200
    autoupdate.EnterpriseRollbackPreviousVersion.* all
    All the tests pass when PEAP configuration is not included (current
    state of the tests with just PSK still works with the modification).
    Tests pass when also adding the new PEAP wifi configuration.
    
    Change-Id: I33f5601165d546caf9ed9265365f7f9abd0db3ac

--- src/chromiumos/tast/local/network/netconfig/cros_network_config_types.go
+++ src/chromiumos/tast/local/network/netconfig/cros_network_config_types.go
@@ -55,11 +55,51 @@ type ManagedString struct {
 	ActiveValue string `json:"activeValue"`
 }
 
+// ManagedStringList contains active value, if required one may add policy value
+// and source.
+type ManagedStringList struct {
+	ActiveValue []string `json:"activeValue"`
+}
+
+// ManagedBoolean contains active value, if required one may add policy value
+// and source.
+type ManagedBoolean struct {
+	ActiveValue bool `json:"activeValue"`
+}
+
+// ManagedSubjectAltNameMatchList contains active value, if required one may add
+// policy value and source.
+type ManagedSubjectAltNameMatchList struct {
+	ActiveValue []SubjectAltName `json:"activeValue"`
+}
+
+// ManagedEAPProperties contains properties for EAP networks.
+// Currently only PEAP networks without CA certificates are supported.
+// We include the same fields as EAPConfigProperties.
+type ManagedEAPProperties struct {
+	AnonymousIdentity   ManagedString                  `json:"anonymousIdentity,omitempty"`
+	Identity            ManagedString                  `json:"identity,omitempty"`
+	Inner               ManagedString                  `json:"inner,omitempty"`
+	Outer               ManagedString                  `json:"outer,omitempty"`
+	Password            ManagedString                  `json:"password,omitempty"`
+	SaveCredentials     ManagedBoolean                 `json:"saveCredentials,omitempty"`
+	ClientCertType      ManagedString                  `json:"clientCertType,omitempty"`
+	DomainSuffixMatch   ManagedStringList              `json:"domainSuffixMatch,omitempty"`
+	SubjectAltNameMatch ManagedSubjectAltNameMatchList `json:"subjectAltNameMatch,omitempty"`
+	UseSystemCAs        ManagedBoolean                 `json:"useSystemCas,omitempty"`
+}
+
 // ManagedWiFiProperties contain managed properties of a wifi connection.
 type ManagedWiFiProperties struct {
-	Passphrase ManagedString `json:"passphrase,omitempty"`
-	Ssid       ManagedString `json:"ssid"`
-	Security   SecurityType  `json:"security"`
+	// Passphrase is only used for PSK networks and Eap is only used for EAP.
+	// These fields should not be included in the json object at all otherwise
+	// (not even as an empty object). Setting the optional field as a pointer
+	// allows it to be nullable and to not appear in the json object if not
+	// provided.
+	Eap        *ManagedEAPProperties `json:"eap,omitempty"`
+	Passphrase *ManagedString        `json:"passphrase,omitempty"`
+	Ssid       ManagedString         `json:"ssid"`
+	Security   SecurityType          `json:"security"`
 }
 
 // NetworkTypeManagedProperties contains managed properties for one of the
@@ -75,13 +115,51 @@ type ManagedProperties struct {
 	TypeProperties NetworkTypeManagedProperties `json:"typeProperties"`
 }
 
+// SubjectAltNameType is the type for SubjectAltName.
+type SubjectAltNameType int
+
+// Allowed types for the alternative subject name.
+const (
+	Email SubjectAltNameType = iota
+	DNS
+	URI
+)
+
+// SubjectAltName contains the information of an alternative subject name.
+type SubjectAltName struct {
+	Type  SubjectAltNameType `json:"type"`
+	Value string             `json:"value"`
+}
+
+// EAPConfigProperties contains properties for EAP networks.
+// Currently only PEAP networks without CA certificates are supported, so the
+// fields related to certificates are not included: ServerCAPEMs, ServerCARefs,
+// ServerCARef (deprecated), SubjectMatch, TLSVersionMax, UseProactiveKeyCaching.
+type EAPConfigProperties struct {
+	AnonymousIdentity   string           `json:"anonymousIdentity,omitempty"`
+	Identity            string           `json:"identity,omitempty"`
+	Inner               string           `json:"inner,omitempty"` // "Automatic"
+	Outer               string           `json:"outer"`           // "PEAP"
+	Password            string           `json:"password,omitempty"`
+	SaveCredentials     bool             `json:"saveCredentials,omitempty"` // true.
+	ClientCertType      string           `json:"clientCertType,omitempty"`  // "None".
+	DomainSuffixMatch   []string         `json:"domainSuffixMatch"`         // Empty in manual example but not optional in mojo.
+	SubjectAltNameMatch []SubjectAltName `json:"subjectAltNameMatch"`       // Empty in manual example but not optional in mojo.
+	UseSystemCAs        bool             `json:"useSystemCAs,omitempty"`    // false. Defaults to true.
+}
+
 // WiFiConfigProperties is used to create new configurations or augment
 // existing ones.
 type WiFiConfigProperties struct {
-	Passphrase string         `json:"passphrase,omitempty"`
-	Ssid       string         `json:"ssid,omitempty"`
-	Security   SecurityType   `json:"security"`
-	HiddenSsid HiddenSsidMode `json:"hiddenSsid"`
+	// Eap configuration is only used if the wifi security is WpaEap and it should
+	// not be included in the json object at all otherwise (not even as an empty
+	// object). Setting the field Eap as a pointer allows it to be nullable and
+	// to not appear in the json object if not provided.
+	Eap        *EAPConfigProperties `json:"eap,omitempty"`
+	Passphrase string               `json:"passphrase,omitempty"`
+	Ssid       string               `json:"ssid,omitempty"`
+	Security   SecurityType         `json:"security"`
+	HiddenSsid HiddenSsidMode       `json:"hiddenSsid"`
 }
 
 // NetworkTypeConfigProperties contains properties for one type of network.
