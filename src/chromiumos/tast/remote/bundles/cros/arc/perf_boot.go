120bc28eac50d68c1e3f1a6c4501ea991ad91244
Hung Nguyen <hungmn@google.com>
2022-03-30 00:22:24 +0000

    Update default iterations for arc.PerfBoot to 10.
    Also updated the timeout of the test to 20 minutes since it runs 10
    times by default. The metrics for each iteration will be saved in separate
    directories while the aggregated metrics will contain averages for all
    iterations.
    
    BUG=b:185450485
    TEST=tast run $DUT arc.PerfBoot.vm and tast run $DUT arc.PerfBoot
    
    Change-Id: I711ee207344f9a38bf03bdff3a0c6a5a6a45b48b

--- src/chromiumos/tast/remote/bundles/cros/arc/perf_boot.go
+++ src/chromiumos/tast/remote/bundles/cros/arc/perf_boot.go
@@ -6,14 +6,23 @@ package arc
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 
 	"chromiumos/tast/common/perf"
+	"chromiumos/tast/common/perf/perfpb"
 	"chromiumos/tast/rpc"
 	"chromiumos/tast/services/cros/arc"
 	"chromiumos/tast/testing"
+)
+
+var (
+	defaultIterations = 10 // The number of boot iterations. Can be overridden by var "arc.PerfBoot.iterations".
 )
 
 func init() {
@@ -29,7 +38,8 @@ func init() {
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome"},
 		ServiceDeps:  []string{"tast.cros.arc.PerfBootService"},
-		Timeout:      5 * time.Minute,
+		Vars:         []string{"arc.PerfBoot.iterations"},
+		Timeout:      20 * time.Minute,
 		Params: []testing.Param{{
 			ExtraSoftwareDeps: []string{"android_p"},
 		}, {
@@ -39,7 +49,7 @@ func init() {
 	})
 }
 
-func PerfBoot(ctx context.Context, s *testing.State) {
+func perfBootOnce(ctx context.Context, s *testing.State, i, iterations int) *perfpb.Values {
 	d := s.DUT()
 
 	// Connect to the gRPC server on the DUT.
@@ -73,16 +83,73 @@ func PerfBoot(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("PerfBootService.GetPerfValues returned an error: ", err)
 	}
-	for _, m := range res.Values {
-		if m.Multiple {
-			s.Logf("Logcat event entry: tag=%s unit=%s values=%v", m.Name, m.Unit, m.Value)
-		} else {
-			s.Logf("Logcat event entry: tag=%s unit=%s value=%f", m.Name, m.Unit, m.Value[0])
-		}
+
+	// Save raw data for this iteration.
+	savedRaw := filepath.Join(s.OutDir(), fmt.Sprintf("raw.%03d", i+1))
+	if err = os.Mkdir(savedRaw, 0755); err != nil {
+		s.Fatalf("Failed to create path %s", savedRaw)
 	}
 
 	p := perf.NewValuesFromProto(res)
-	if err = p.Save(s.OutDir()); err != nil {
-		s.Fatal("Failed to save perf data: ", err)
+	if err = p.Save(savedRaw); err != nil {
+		s.Fatal("Failed to save perf raw data: ", err)
+	}
+
+	return res
+}
+
+func PerfBoot(ctx context.Context, s *testing.State) {
+	iterations := defaultIterations
+	if iter, ok := s.Var("arc.PerfBoot.iterations"); ok {
+		if i, err := strconv.Atoi(iter); err == nil {
+			iterations = i
+		} else {
+			// User might want to override the default value of iterations but passed a malformed value. Fail the test to inform the user.
+			s.Fatal("Invalid arc.PerfBoot.iterations value: ", iter)
+		}
+	}
+
+	pv := perf.NewValues()
+	singleMetrics := make(map[perf.Metric][]float64)
+	for i := 0; i < iterations; i++ {
+		// Run the boot test once.
+		res := perfBootOnce(ctx, s, i, iterations)
+		for _, m := range res.Values {
+			if m.Multiple {
+				pv.Append(perf.Metric{
+					Name:      m.Name,
+					Unit:      m.Unit,
+					Direction: perf.Direction(m.Direction),
+					Multiple:  m.Multiple,
+				}, m.Value...)
+				s.Logf("Logcat event entry: tag=%s unit=%s values=%v", m.Name, m.Unit, m.Value)
+			} else {
+				metric := perf.Metric{
+					Name:      m.Name,
+					Unit:      m.Unit,
+					Direction: perf.Direction(m.Direction),
+					Multiple:  m.Multiple,
+				}
+				_, ok := singleMetrics[metric]
+				if ok {
+					singleMetrics[metric] = append(singleMetrics[metric], m.Value...)
+				} else {
+					singleMetrics[metric] = m.Value
+				}
+				s.Logf("Logcat event entry: tag=%s unit=%s value=%f", m.Name, m.Unit, m.Value[0])
+			}
+		}
+	}
+
+	for k, values := range singleMetrics {
+		sum := 0.0
+		for _, v := range values {
+			sum += v
+		}
+		pv.Set(k, sum/float64(len(values)))
+	}
+
+	if err := pv.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
 	}
 }
