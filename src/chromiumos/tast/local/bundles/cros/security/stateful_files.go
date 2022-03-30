7ba6bfbf89b865bd1e0876acaba63b3ed621d073
Cheng-Han Yang <chenghan@google.com>
2022-03-30 16:19:55 +0800

    security.StatefulFiles: Add check for rmad files
    unencrypted/rmad-data is used for storing rmad state file
    (go/shimless-state-preservation). /var/lib/rmad is used for other files
    generated by the daemon during runtime.
    
    BUG=b:227166371
    TEST=tast run $DUT security.StatefulFiles
    
    Change-Id: I387376082b935a2066ce149bb70bc7501edad423

--- src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
+++ src/chromiumos/tast/local/bundles/cros/security/stateful_files.go
@@ -238,6 +238,11 @@ func StatefulFiles(ctx context.Context, s *testing.State) {
 		prependPatterns(chk.NewPattern(chk.Tree("encrypted/var/lib/rmtfs"), chk.Users("rmtfs"), chk.NotMode(022)))
 	}
 
+	if _, err := user.Lookup("rmad"); err == nil {
+		prependPatterns(chk.NewPattern(chk.Tree("encrypted/var/lib/rmad"), chk.Users("rmad"), chk.Groups("rmad"), chk.NotMode(022)))
+		prependPatterns(chk.NewPattern(chk.Tree("unencrypted/rma-data"), chk.Users("rmad"), chk.Groups("rmad"), chk.NotMode(022)))
+	}
+
 	if moblab.IsMoblab() {
 		// On moblab devices, there are additional user dirs and tons of stuff (MySQL, etc.) in /var.
 		prependPatterns(
