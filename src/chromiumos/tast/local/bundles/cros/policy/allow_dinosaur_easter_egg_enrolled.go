d9bff90998fe4d13a4e574b36481e15f60ac6f03
Victor-Gabriel Savu <vsavu@google.com>
2022-03-30 11:40:17 +0000

    policy: Promote AllowDinosaurEasterEggEnrolled
    This also promotes the ChromeEnrolledLoggedIn fixture to the CQ.
    
    BUG=None
    TEST=None
    
    Change-Id: Ida5741bb1aff812e51359e6993343448048c1fff

--- src/chromiumos/tast/local/bundles/cros/policy/allow_dinosaur_easter_egg_enrolled.go
+++ src/chromiumos/tast/local/bundles/cros/policy/allow_dinosaur_easter_egg_enrolled.go
@@ -25,7 +25,7 @@ func init() {
 			"vsavu@google.com", // Test author
 			"chromeos-commercial-remote-management@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      fixture.ChromeEnrolledLoggedIn,
 	})
