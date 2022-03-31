726e0896ac7acb43da92d348ab28796fb4c92508
Yichen Zhou <yichenz@chromium.org>
2022-03-31 10:30:29 +0000

    tast-tests: ensure enough bots in the call in MeetCUJ
    BUG=b:225210951
    TEST=tast run <DUT> UI.MeetCUJ*
    
    Change-Id: I4a30de2f7d53f5c3ac24dcd6ca4594a4dfb821af

--- src/chromiumos/tast/local/bundles/cros/ui/meet_cuj.go
+++ src/chromiumos/tast/local/bundles/cros/ui/meet_cuj.go
@@ -320,8 +320,20 @@ func MeetCUJ(ctx context.Context, s *testing.State) {
 	// Add 15 minutes to the bot duration, to ensure that the bots stay long enough
 	// for the test to detect the video codecs used for encoding and decoding.
 	if !codeOk {
-		if _, err := bc.AddBots(sctx, meetingCode, meet.num, meetTimeout+15*time.Minute, meet.botsOptions...); err != nil {
-			s.Fatal("Failed to create bots: ", err)
+		addBotsCount := meet.num
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			botList, err := bc.AddBots(sctx, meetingCode, addBotsCount, meetTimeout+15*time.Minute, meet.botsOptions...)
+			if err != nil {
+				return errors.Errorf("failed to create %d bots", addBotsCount)
+			}
+			s.Logf("%d bots started", len(botList))
+			addBotsCount -= len(botList)
+			if addBotsCount > 0 {
+				return errors.Errorf("still need to create %d bots", addBotsCount)
+			}
+			return nil
+		}, &testing.PollOptions{Timeout: time.Minute}); err != nil {
+			s.Fatal("Failed to ensure enough bots in the meet call: ", err)
 		}
 	}
 
