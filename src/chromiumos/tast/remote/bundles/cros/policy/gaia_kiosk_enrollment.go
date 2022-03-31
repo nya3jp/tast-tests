67f050fd3055e4a7636eaa890a98a86fc42dd044
Ruben Zakarian <rzakarian@google.com>
2022-03-31 12:41:36 -0700

    ChromeOS Enterprise: Add Integration Test
    Adding a test that kiosk enrolls using a real DMServer.
    
    BUG=b:196112165
    TEST=ran test on a physical device.
    https://paste.googleplex.com/6441187633266688
    
    Change-Id: I2477f6cda5bf5f9f069b34df019d6d445bae1090

---
+++ src/chromiumos/tast/remote/bundles/cros/policy/gaia_kiosk_enrollment.go
@@ -0,0 +1,95 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/remote/policyutil"
+	"chromiumos/tast/rpc"
+	ks "chromiumos/tast/services/cros/kiosk"
+	ps "chromiumos/tast/services/cros/policy"
+	"chromiumos/tast/testing"
+)
+
+type testCreds struct {
+	username string // username for Chrome login
+	password string // password to login
+	dmserver string // device management server url
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GAIAKioskEnrollment,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "GAIA Enroll a device without checking policies",
+		Contacts: []string{
+			"rzakarian@google.com", // Test author
+			"chromeos-commercial-remote-management@google.com",
+		},
+		Attr:         []string{"group:dpanel-end2end"},
+		SoftwareDeps: []string{"reboot", "chrome"},
+		ServiceDeps:  []string{"tast.cros.policy.PolicyService", "tast.cros.kiosk.KioskService"},
+		Timeout:      7 * time.Minute,
+		Params: []testing.Param{
+			{
+				Name: "autopush",
+				Val: testCreds{
+					username: "policy.GAIAKioskEnrollment.user_name",
+					password: "policy.GAIAKioskEnrollment.password",
+					dmserver: "https://crosman-alpha.sandbox.google.com/devicemanagement/data/api",
+				},
+			},
+		},
+		Vars: []string{
+			"policy.GAIAKioskEnrollment.user_name",
+			"policy.GAIAKioskEnrollment.password",
+		},
+	})
+}
+
+func GAIAKioskEnrollment(ctx context.Context, s *testing.State) {
+	param := s.Param().(testCreds)
+	username := s.RequiredVar(param.username)
+	password := s.RequiredVar(param.password)
+	dmServerURL := param.dmserver
+
+	defer func(ctx context.Context) {
+		if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
+			s.Error("Failed to reset TPM after test: ", err)
+		}
+	}(ctx)
+
+	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Minute)
+	defer cancel()
+
+	if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT()); err != nil {
+		s.Fatal("Failed to reset TPM: ", err)
+	}
+
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	pc := ps.NewPolicyServiceClient(cl.Conn)
+	kc := ks.NewKioskServiceClient(cl.Conn)
+
+	if _, err := pc.GAIAEnrollUsingChrome(ctx, &ps.GAIAEnrollUsingChromeRequest{
+		Username:    username,
+		Password:    password,
+		DmserverURL: dmServerURL,
+	}); err != nil {
+		s.Fatal("Failed to enroll using chrome: ", err)
+	}
+
+	if _, err := kc.ConfirmKioskStarted(ctx, &ks.ConfirmKioskStartedRequest{}); err != nil {
+		s.Fatal("Failed to start kiosk mode: ", err)
+	}
+
+}
