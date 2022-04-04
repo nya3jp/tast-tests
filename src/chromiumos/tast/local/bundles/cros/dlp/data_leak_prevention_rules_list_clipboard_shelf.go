5e4af9e8c1a069d0eace83bd2b8ed7517c7d1374
Aya ElAttar <ayaelattar@chromium.org>
2022-04-04 21:31:16 +0200

    Fix & Promote clipboard shelf test
    BUG=chromium:1304823
    TEST=tast run <ip> dlp.DataLeakPreventionRulesListClipboardShelf
    
    Change-Id: Icafe25c3e0107b7647cce592a3d26cd7e0071df1

--- src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_clipboard_shelf.go
+++ src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_clipboard_shelf.go
@@ -33,7 +33,7 @@ func init() {
 			"chromeos-dlp@google.com",
 		},
 		SoftwareDeps: []string{"chrome"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		Fixture:      "chromePolicyLoggedIn",
 	})
 }
@@ -108,6 +108,10 @@ func DataLeakPreventionRulesListClipboardShelf(ctx context.Context, s *testing.S
 				s.Fatal("Failed to open the launcher: ", err)
 			}
 
+			if err := launcher.WaitForStableNumberOfApps(ctx, tconn); err != nil {
+				s.Fatal("Failed to wait for item count in app list to stabilize: ", err)
+			}
+
 			s.Log("Right clicking shelf box")
 			if err := rightClickShelfbox(ctx, tconn, param.url, param.wantAllowed); err != nil {
 				s.Error("Failed to right click shelf box: ", err)
@@ -153,6 +157,7 @@ func pasteShelfbox(ctx context.Context, tconn *chrome.TestConn, keyboard *input.
 
 	searchNode := nodewith.ClassName("SearchBoxView").First()
 	if err := uiauto.Combine("Paste content in shelf box",
+		ui.WaitUWaitUntilExists(searchNode),
 		ui.LeftClick(searchNode),
 		keyboard.AccelAction("Ctrl+V"))(ctx); err != nil {
 		return errors.Wrap(err, "failed to paste content in shelf box")
