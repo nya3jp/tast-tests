6217d43aa18a4c9ccfb2def422a2044877d0a97b
Eunice Sun <eunicesun@google.com>
2022-04-04 02:31:09 +0000

    hps: remove attribute so that the test would only be run manually
    BUG=b:215649920
    TEST=tast run -buildbundle=cros -var=tablet=<tablet IP> <DUT IP> hps.CameraboxPresence*
    
    Change-Id: I4eb9a8d26b821789dd30ce055ba51662a8c4097b

--- src/chromiumos/tast/remote/bundles/cros/hps/camerabox_presence.go
+++ src/chromiumos/tast/remote/bundles/cros/hps/camerabox_presence.go
@@ -6,18 +6,14 @@ package hps
 
 import (
 	"context"
-	"path/filepath"
 	"strconv"
-	"strings"
 	"time"
 
 	"chromiumos/tast/common/camera/chart"
 	"chromiumos/tast/common/hps/hpsutil"
 	"chromiumos/tast/common/media/caps"
-	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/rpc"
-	"chromiumos/tast/ssh/linuxssh"
 	"chromiumos/tast/testing"
 )
 
@@ -33,7 +29,7 @@ func init() {
 			"mblsha@google.com",
 			"chromeos-hps-swe@google.com",
 		},
-		Attr:         []string{"group:camerabox", "group:hps", "hps_perbuild"},
+		Attr:         []string{"group:camerabox", "group:hps"},
 		Timeout:      20 * time.Minute,
 		SoftwareDeps: []string{"hps", "chrome", caps.BuiltinCamera},
 		Vars:         []string{"tablet"},
@@ -53,51 +49,24 @@ func CameraboxPresence(ctx context.Context, s *testing.State) {
 	// the HPS would already be able to correctly adjust the exposure right from the start.
 
 	// Sending powercycle python file to DUT with HPS
-	powercycleTmpDir, err := d.Conn().CommandContext(ctx, "mktemp", "-d", "/tmp/powercycle_XXXXX").Output()
-	if err != nil {
-		s.Fatal("Failed to create test directory under /tmp for putting powercycle file: ", err)
-	}
-	powercycleDirPath := strings.TrimSpace(string(powercycleTmpDir))
-	powercycleFilePath := filepath.Join(powercycleDirPath, hpsutil.P2PowerCycleFilename)
-	defer d.Conn().CommandContext(ctx, "rm", "-r", powercycleDirPath).Output()
-	if _, err := linuxssh.PutFiles(
-		ctx, d.Conn(), map[string]string{
-			s.DataPath(hpsutil.P2PowerCycleFilename): powercycleFilePath,
-		},
-		linuxssh.DereferenceSymlinks); err != nil {
-		s.Fatalf("Failed to send data to remote data path %v: %v", powercycleFilePath, err)
-	}
-	testing.ContextLog(ctx, "Sending file to dut, path being: ", powercycleFilePath)
 
-	// Extract files from tar
 	archive := s.DataPath(hpsutil.PersonPresentPageArchiveFilename)
-	dirPath := filepath.Dir(archive)
-	testing.ContextLog(ctx, "dirpath: ", dirPath)
-
-	tarOut, err := testexec.CommandContext(ctx, "tar", "--strip-components=1", "-xvf", archive, "-C", dirPath).Output()
-	testing.ContextLog(ctx, "Extracting following files: ", string(tarOut))
+	powercycleDirPath, powercycleFilePath, err := CreateTmpDir(ctx, d.Conn())
 	if err != nil {
-		s.Fatal("Failed to untar test artifacts: ", err)
+		s.Fatal("Tmp dir creation failed on DUT")
 	}
+	filePaths, err := SendImageTar(ctx, d.Conn(), archive, powercycleDirPath, powercycleFilePath)
 
 	// Creating hps context
 	hctx, err := hpsutil.NewHpsContext(ctx, powercycleFilePath, hpsutil.DeviceTypeBuiltin, s.OutDir(), d.Conn())
 	if err != nil {
 		s.Fatal("Error creating HpsContext: ", err)
 	}
-
 	// Connecting to the other tablet that will render the picture
 	var chartAddr string
 	if altAddr, ok := s.Var("tablet"); ok {
 		chartAddr = altAddr
 	}
-
-	picture := filepath.Join(dirPath, "IMG_7451.jpg")
-	chartPaths := []string{
-		filepath.Join(dirPath, "no-person-present.html"),
-		filepath.Join(dirPath, "person-present.html"),
-		filepath.Join(dirPath, "two-people-present.html")}
-	filePaths := append(chartPaths, picture)
 
 	c, hostPaths, err := chart.New(ctx, d, chartAddr, s.OutDir(), filePaths)
 	if err != nil {
