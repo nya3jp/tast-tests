3dac3d750dbc830382d921902954ace04b056e4f
Alexander Alekseev <alemate@chromium.org>
2022-04-06 01:05:16 -0700

    tast-tests: Create cujrecorder.Recorder.
    This CL creates shared cujrecorder.Recorder by renaming
    ui/cuj.Recorder and moving it to shared components.
    
    This should allow any tests to report data to the TPS dashboard.
    
    BUG=b:224597726
    BUG=chromium:1297938
    
    TEST=tast run <DUT> ui.QuickCheckCUJ
    
    Change-Id: I7a937f13cbc6699081ae63a3e1aa2c4ca4b6da6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3574165
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/productivitycuj/run.go
+++ src/chromiumos/tast/local/bundles/cros/ui/productivitycuj/run.go
@@ -18,6 +18,7 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/graphics"
 	"chromiumos/tast/local/input/voice"
+	"chromiumos/tast/local/ui/cujrecorder"
 	"chromiumos/tast/testing"
 )
 
@@ -54,8 +55,8 @@ func Run(ctx context.Context, cr *chrome.Chrome, app ProductivityApp, tier cuj.T
 	defer cancel()
 
 	testing.ContextLog(ctx, "Start recording actions")
-	options := cuj.NewPerformanceCUJOptions()
-	recorder, err := cuj.NewRecorder(ctx, cr, nil, options, cuj.MetricConfigs([]*chrome.TestConn{tconn})...)
+	options := cujrecorder.NewPerformanceCUJOptions()
+	recorder, err := cujrecorder.NewRecorder(ctx, cr, nil, options, cuj.MetricConfigs([]*chrome.TestConn{tconn})...)
 	if err != nil {
 		return errors.Wrap(err, "failed to create the recorder")
 	}
