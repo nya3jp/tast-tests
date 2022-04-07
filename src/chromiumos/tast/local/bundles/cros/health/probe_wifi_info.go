de28bab9dfdc6ed4b1b31ec7fb0abaf95fb310fd
Kalin Stoyanov <kalin@chromium.org>
2022-04-07 10:41:47 -0700

    Change Lacros status metadata for health package
    BUG=b:228328153
    TEST=fast_build.sh -T
    
    Change-Id: Ia6e03537f9261eafab8002129068b92e65338f5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3577213
    Tested-by: Kalin Stoyanov <kalin@chromium.org>
    Reviewed-by: Hyung Tae Kim <hyungtaekim@chromium.org>
    Reviewed-by: Chung-Sheng Wu <chungsheng@google.com>
    Auto-Submit: Kalin Stoyanov <kalin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Kalin Stoyanov <kalin@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/health/probe_wifi_info.go
+++ src/chromiumos/tast/local/bundles/cros/health/probe_wifi_info.go
@@ -41,7 +41,7 @@ type wifiInfo struct {
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         ProbeWifiInfo,
-		LacrosStatus: testing.LacrosVariantUnknown,
+		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Check that we can probe cros_healthd for WIFI info",
 		Contacts: []string{
 			"cros-tdm-tpe-eng@google.com",
