22ec3daf47c08b006c6ca579343dea4e09341ebf
Avery Musbach <amusbach@chromium.org>
2022-04-07 16:04:59 -0700

    tast-tests: Use minimum device scale factor in ui.MeetCUJ.*
    This is to ensure the meeting participants' video can all fit together.
    
    BUG=b:227665352
    TEST=tast run <DUT> ui.MeetCUJ.49p
    
    Change-Id: If2be4f9deebece02360c67df34af100fec535265

--- src/chromiumos/tast/local/bundles/cros/ui/meet_cuj.go
+++ src/chromiumos/tast/local/bundles/cros/ui/meet_cuj.go
@@ -315,7 +315,7 @@ func MeetCUJ(ctx context.Context, s *testing.State) {
 		s.Log("Created a room with the code ", meetingCode)
 	}
 
-	sctx, cancel := context.WithTimeout(ctx, 90 * time.Second)
+	sctx, cancel := context.WithTimeout(ctx, 90*time.Second)
 	defer cancel()
 	// Add 15 minutes to the bot duration, to ensure that the bots stay long enough
 	// for the test to detect the video codecs used for encoding and decoding.
@@ -324,7 +324,7 @@ func MeetCUJ(ctx context.Context, s *testing.State) {
 		if err := testing.Poll(ctx, func(ctx context.Context) error {
 			botList, err := bc.AddBots(sctx, meetingCode, addBotsCount, meetTimeout+15*time.Minute, meet.botsOptions...)
 			if err != nil {
-				return testing.PollBreak(errors.Errorf("failed to create %d bots: %v", addBotsCount, err))
+				return testing.PollBreak(errors.Wrapf(err, "failed to create %d bots", addBotsCount))
 			}
 			s.Logf("%d bots started", len(botList))
 			// If there are fewer bots created than requested, send another request for more bots.
@@ -454,6 +454,19 @@ func MeetCUJ(ctx context.Context, s *testing.State) {
 			s.Fatal("Failed to close New Tab window: ", err)
 		}
 	}
+
+	// Sets the display zoom factor to minimum, to ensure that all
+	// meeting participants' video can be shown simultaneously.
+	info, err := display.GetPrimaryInfo(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get the primary display info: ", err)
+	}
+	zoomInitial := info.DisplayZoomFactor
+	zoomMin := info.AvailableDisplayZoomFactors[0]
+	if err := display.SetDisplayProperties(ctx, tconn, info.ID, display.DisplayProperties{DisplayZoomFactor: &zoomMin}); err != nil {
+		s.Fatalf("Failed to set display zoom factor to minimum %f: %v", zoomMin, err)
+	}
+	defer display.SetDisplayProperties(closeCtx, tconn, info.ID, display.DisplayProperties{DisplayZoomFactor: &zoomInitial})
 
 	inTabletMode, err := ash.TabletModeEnabled(ctx, tconn)
 	s.Logf("Is in tablet-mode: %t", inTabletMode)
@@ -655,7 +668,7 @@ func MeetCUJ(ctx context.Context, s *testing.State) {
 		if err := meetConn.Eval(ctx, "hrTelemetryApi.getParticipantCount()", &participantCount); err != nil {
 			return errors.Wrap(err, "failed to get participant count")
 		}
-		if expectedParticipantCount := meet.num+1; participantCount != expectedParticipantCount {
+		if expectedParticipantCount := meet.num + 1; participantCount != expectedParticipantCount {
 			return errors.Errorf("got %d participants, expected %d", participantCount, expectedParticipantCount)
 		}
 
