151fa12bc43882c01afd37be79627e7418cd66f7
Damien Dejean <damiendejean@google.com>
2022-04-08 12:55:28 +0000

    wifi: Passpoint: add set of selection tests.
    Passpoint placeholder test is replaced by a set of selection tests that
    verify how Shill and wpa_supplicant are matching with Passpoint
    networks. The device is (virtually) exposed to multiple access points and
    the test ensures the device connects to the expected one.
    
    BUG=b:162258594
    TEST=tast run ${DUT} wifi.PasspointSelection.*
    
    Change-Id: Idc6e88b437ff1e2bda31c9b13f0a1ee86544fdb6

---
+++ src/chromiumos/tast/local/bundles/cros/wifi/passpoint/const.go
@@ -0,0 +1,21 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package passpoint
+
+const (
+	// ProfileUser is the name of the fake test user profile
+	ProfileUser = "test"
+)
+
+// Domains and Organisation Identifier (OI) used to test Passpoint
+// network selection.
+const (
+	BlueDomain         = "sp-blue.com"
+	GreenDomain        = "sp-green.com"
+	RedDomain          = "sp-red.com"
+	HomeOI      uint64 = 0x871d2e
+	RoamingOI1  uint64 = 0x1bc50050
+	RoamingOI2  uint64 = 0x1bc500b5
+)
