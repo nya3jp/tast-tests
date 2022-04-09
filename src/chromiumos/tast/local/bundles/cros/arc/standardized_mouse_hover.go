f4d6b9cc922e1bf3f81d90a2803f2810cfd5e501
David Welling <davidwelling@google.com>
2022-04-09 16:40:03 -0700

    arcappcompat: Make additional standardized tests critical.
    Make StandardizedKeyboardKeys.tablet_mode, StandardizedMouseHover.vm,
    StandardizedMouseLeftClick.vm, and StandardizedMouseScroll.vm critical.
    
    BUG=b:194900373
    TEST=tast unit tests passed.
    
    Change-Id: If134c4f7fab0ca444747523ce5d7492709cc709e

--- src/chromiumos/tast/local/bundles/cros/arc/standardized_mouse_hover.go
+++ src/chromiumos/tast/local/bundles/cros/arc/standardized_mouse_hover.go
@@ -22,13 +22,14 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Functional test that installs an app and tests standard mouse hover functionality. Tests are only performed in clamshell mode as tablets don't allow mice",
 		Contacts:     []string{"davidwelling@google.com", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome", "no_chrome_dcheck"},
 		Timeout:      10 * time.Minute,
 		Fixture:      "arcBooted",
 		Params: []testing.Param{
 			{
 				Val:               standardizedtestutil.GetClamshellTest(runStandardizedMouseHoverTest),
+				ExtraAttr:         []string{"informational"},
 				ExtraSoftwareDeps: []string{"android_p"},
 				ExtraHardwareDeps: hwdep.D(standardizedtestutil.ClamshellHardwareDep),
 			}, {
