b4567fa148bc173ab3c74cab2bd318b72245412f
Li-Yu Yu <aaronyu@google.com>
2022-04-12 02:42:52 +0800

    audio: Add UCMSequences test
    This is a basic test that exercises all enable/disable sequences,
    implemented as the first step to avoid HiFi.conf changes breaking audio.
    
    BUG=b:229558276
    TEST=tast run $DUT audio.UCMSequences
    
    Change-Id: I1f99c148760eccb0632d1424749e5a22768e741c

---
+++ src/chromiumos/tast/local/bundles/cros/audio/ucm_sequences.go
@@ -0,0 +1,207 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package audio
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/audio"
+	"chromiumos/tast/local/crosconfig"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     UCMSequences,
+		Desc:     "Exercise UCM config enable/disable sequences",
+		Contacts: []string{"aaronyu@google.com", "chromeos-audio-bugs@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+		Timeout:  1 * time.Minute,
+		Params: []testing.Param{
+			{
+				Name: "section_verb",
+				Val: ucmSequencesParam{
+					alsaucmCommander: staticCommander{
+						{
+							name:      "SectionVerb",
+							extraArgs: nil,
+						},
+					},
+				},
+			},
+			{
+				Name:              "speaker",
+				ExtraHardwareDeps: hwdep.D(hwdep.Speaker()),
+				Val: ucmSequencesParam{
+					alsaucmCommander: staticCommander{
+						{
+							name:      "EnableSequence",
+							extraArgs: []string{"set", "_enadev", "Speaker"},
+						},
+						{
+							name:      "DisableSequence",
+							extraArgs: []string{"set", "_disdev", "Speaker"},
+						},
+					},
+				},
+			},
+			{
+				Name:              "internal_mic",
+				ExtraHardwareDeps: hwdep.D(hwdep.Microphone()),
+				Val: ucmSequencesParam{
+					alsaucmCommander: staticCommander{
+						{
+							name:      "EnableSequence",
+							extraArgs: []string{"set", "_enadev", "Internal Mic"},
+						},
+						{
+							name:      "DisableSequence",
+							extraArgs: []string{"set", "_disdev", "Internal Mic"},
+						},
+					},
+				},
+			},
+			{
+				Name: "section_modifier",
+				Val: ucmSequencesParam{
+					alsaucmCommander: modifierCommander{},
+				},
+			},
+		},
+	})
+}
+
+type ucmSequencesParam struct {
+	alsaucmCommander
+}
+
+type alsaucmCommander interface {
+	// commands returns a list of alsaucm commands to run
+	commands(ctx context.Context, ucmName string) ([]alsaucmCommand, error)
+}
+
+type alsaucmCommand struct {
+	name      string   // human readable name describing the command
+	extraArgs []string // extra args after `alsaucm -c$cardname set _verb HiFi`
+}
+
+// staticCommander is a ucmSequencesParam that returns a predetermined list of ucmCommands
+type staticCommander []alsaucmCommand
+
+var _ alsaucmCommander = staticCommander{}
+
+func (p staticCommander) commands(ctx context.Context, ucmName string) ([]alsaucmCommand, error) {
+	return p, nil
+}
+
+// staticCommands is a ucmSequencesParam that returns a list of ucmCommands
+// based on available "SectionModifier"s in HiFi.conf
+type modifierCommander struct{}
+
+var _ alsaucmCommander = modifierCommander{}
+
+func (p modifierCommander) commands(ctx context.Context, ucmName string) ([]alsaucmCommand, error) {
+	cmd := testexec.CommandContext(ctx,
+		"alsaucm",
+		"-c"+ucmName,
+		"list",
+		"_modifiers/HiFi",
+	)
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to list modifiers")
+	}
+
+	// Find the names listed after the ": "
+	// example:
+	// 0: Hotword Model ar_sa
+	// 1: Hotword Model cmn
+	var commands []alsaucmCommand
+	for _, line := range strings.Split(string(out), "\n") {
+		parts := strings.SplitN(line, ": ", 2)
+		if len(parts) == 2 {
+			modifier := parts[1]
+			commands = append(commands, alsaucmCommand{modifier, []string{"set", "_enamod", modifier}})
+		}
+	}
+	return commands, nil
+}
+
+func UCMSequences(ctx context.Context, s *testing.State) {
+	// since we are messing with mixer controls, restart CRAS after running the test
+	defer audio.RestartCras(ctx)
+
+	param := s.Param().(ucmSequencesParam)
+
+	cards, err := audio.GetSoundCards()
+	if err != nil {
+		s.Fatal("Failed to get sound cards: ", err)
+	}
+
+	ucmSuffix, err := crosconfig.Get(ctx, "/audio/main", "ucm-suffix")
+	if err != nil && !crosconfig.IsNotFound(err) {
+		s.Fatal("Cannot get ucm suffix: ", err)
+	}
+
+	for _, card := range cards {
+		ucmName := card.ShortName
+		if ucmSuffix != "" {
+			ucmName += "." + ucmSuffix
+		}
+		ucmSequencesTestCard(ctx, s, param.alsaucmCommander, ucmName)
+	}
+}
+
+func ucmSequencesTestCard(ctx context.Context, s *testing.State, c alsaucmCommander, ucmName string) {
+	const ucmBasePath = "/usr/share/alsa/ucm"
+
+	s.Logf("Testing UCM: %s", ucmName)
+
+	// Check we have a complete HiFi.conf
+	// If not, skip the test.
+	hifiConf := filepath.Join(ucmBasePath, ucmName, "HiFi.conf")
+	b, err := os.ReadFile(hifiConf)
+	if err != nil && os.IsNotExist(err) {
+		s.Log("Skipping due to missing HiFi.conf")
+		return
+	}
+	if err != nil {
+		s.Error("Failed to read HiFi.conf: ", err)
+		return
+	}
+	if !bytes.Contains(b, []byte("SectionVerb")) {
+		s.Log("Skipping due to missing SectionVerb in HiFi.conf")
+		return
+	}
+
+	ucmCmds, err := c.commands(ctx, ucmName)
+	if err != nil {
+		s.Error("Cannot get UCM commands: ", err)
+		return
+	}
+	for _, ucmCmd := range ucmCmds {
+		cmd := testexec.CommandContext(ctx,
+			"alsaucm",
+			"-c"+ucmName,
+			"set",
+			"_verb",
+			"HiFi",
+		)
+		cmd.Args = append(cmd.Args, ucmCmd.extraArgs...)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			s.Errorf("%s %s failed:%s%s",
+				cmd.Path, strings.Join(cmd.Args, " "), "\n", string(out))
+		}
+	}
+}
