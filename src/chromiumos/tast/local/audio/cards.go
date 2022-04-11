de5e04617ab26be61e9ecbb522dfc2191065db54
Li-Yu Yu <aaronyu@google.com>
2022-04-12 02:41:40 +0800

    audio: Add function to get sound cards
    BUG=b:229558276
    TEST=~/chromiumos/src/platform/tast/fast_build.sh -T
    
    Change-Id: Ibbe0781286c8f057adfab5d8714d5caa90edf40c

---
+++ src/chromiumos/tast/local/audio/cards.go
@@ -0,0 +1,78 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package audio
+
+import (
+	"os"
+	"regexp"
+	"strings"
+
+	"chromiumos/tast/errors"
+)
+
+// Card is a card listed in /proc/asound/cards as:
+// ## [ID             ]: Driver - ShortName
+//                       LongName
+//
+// snd_iprintf(buffer, "%2i [%-15s]: %s - %s\n",
+// 	idx,
+// 	card->id,
+// 	card->driver,
+// 	card->shortname);
+// snd_iprintf(buffer, "                      %s\n",
+// 	card->longname);
+type Card struct {
+	ID        string
+	Driver    string
+	ShortName string
+	LongName  string
+}
+
+// cardRegexp parses the string from printf "%2i [%-15s]: %s - %s\n"
+// See doc for Card.
+var cardRegexp = regexp.MustCompile(`[ \d]{2} \[(.{15})\]: (\S+) - (.+)
+\s+(.+)
+`)
+
+// parseSoundCards parses Cards from s in /proc/asound/cards's format
+func parseSoundCards(s string) ([]Card, error) {
+	if s == "--- no soundcards ---\n" {
+		return nil, nil
+	}
+
+	lines := strings.Count(s, "\n")
+	if lines%2 != 0 {
+		return nil, errors.Errorf("expected even number of lines, got %d", lines)
+	}
+
+	var cards []Card
+
+	matches := cardRegexp.FindAllStringSubmatch(s, -1)
+	for _, m := range matches {
+		cards = append(cards, Card{
+			ID:        strings.TrimSpace(m[1]),
+			Driver:    m[2],
+			ShortName: m[3],
+			LongName:  m[4],
+		})
+	}
+
+	if lines != len(cards)*2 {
+		return nil, errors.Errorf(
+			"expected to find %d cards from %d lines, but found %d cards",
+			lines/2, lines, len(cards))
+	}
+
+	return cards, nil
+}
+
+// GetSoundCards returns Cards from /proc/asound/cards
+func GetSoundCards() ([]Card, error) {
+	b, err := os.ReadFile("/proc/asound/cards")
+	if err != nil {
+		return nil, err
+	}
+	return parseSoundCards(string(b))
+}
