87d96bcd39a118f996c745f5b4d13befc0ace7b0
Wei Lee <wtlee@google.com>
2022-04-11 15:48:30 +0800

    camera: Make test compatible after having new Settings UI
    In the new settings UI design, we:
    1. Remove the square mode and put the square option in the aspect
       ratio settings.
    2. Change the structure of resolution settings
    3. Add aspect ratio settings for Photo mode
    4. Moves FPS toggle into video resolution settings
    
    BUG=b:215484798
    TEST=tast run [DUT] camera.CCAUI*
    
    Change-Id: Ie3a0dab176a03125173b12a115cc3cbe2b7a1552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3585266
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Tested-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/camera/cca_ui_resolutions.go
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_resolutions.go
@@ -80,7 +80,7 @@ func getOrientedResolution(ctx context.Context, app *cca.App, r cca.Resolution) 
 	return r, nil
 }
 
-func imageResolution(path string) (*cca.Resolution, error) {
+func imageResolution(path string, handleOrientation bool) (*cca.Resolution, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to open captured file for decoding jpeg")
@@ -91,31 +91,43 @@ func imageResolution(path string) (*cca.Resolution, error) {
 		return nil, errors.Wrap(err, "failed to decode captured file")
 	}
 
-	// Read display rotation number from exif.
-	f2, err := os.Open(path)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to open captured file for reading exif")
-	}
-	defer f2.Close()
-	x, err := exif.Decode(f2)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to decode exif")
-	}
-	tag, err := x.Get(exif.Orientation)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get orientation from exif")
-	}
-	o, err := tag.Int(0)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get integer value of orientation tag")
-	}
-	if 5 <= o && o <= 8 {
-		return &cca.Resolution{Width: c.Height, Height: c.Width}, nil
+	if handleOrientation {
+		// Read display rotation number from exif.
+		f2, err := os.Open(path)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to open captured file for reading exif")
+		}
+		defer f2.Close()
+		x, err := exif.Decode(f2)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to decode exif")
+		}
+		tag, err := x.Get(exif.Orientation)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to get orientation from exif")
+		}
+		o, err := tag.Int(0)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to get integer value of orientation tag")
+		}
+		if 5 <= o && o <= 8 {
+			return &cca.Resolution{Width: c.Height, Height: c.Width}, nil
+		}
 	}
 	return &cca.Resolution{Width: c.Width, Height: c.Height}, nil
 }
 
 func testPhotoResolution(ctx context.Context, app *cca.App) error {
+	// TODO(b/215484798): Remove the logic for old UI once the new UI applied.
+	useNewUI, err := app.Exist(ctx, cca.PhotoResolutionSettingButton)
+	if err != nil {
+		return errors.Wrap(err, "failed to check existence of the photo resolution settings button")
+	}
+	if useNewUI {
+		return testPhotoResolutionAndAspectRatio(ctx, app)
+	}
+
+	// The test logic for legacy photo resolutions UI.
 	return app.RunThroughCameras(ctx, func(facing cca.Facing) error {
 		if err := app.SwitchMode(ctx, cca.Photo); err != nil {
 			return errors.Wrap(err, "failed to switch to photo mode")
@@ -140,17 +152,9 @@ func testPhotoResolution(ctx context.Context, app *cca.App) error {
 				return errors.Wrapf(err, "inconsistent preview aspect ratio get %d:%d; want %d:%d", pr.Width, pr.Height, or.Width, or.Height)
 			}
 
-			info, err := app.TakeSinglePhoto(ctx, cca.TimerOff)
+			ir, err := takePhotoAndGetResolution(ctx, app, true)
 			if err != nil {
-				return errors.Wrap(err, "failed to take photo")
-			}
-			path, err := app.FilePathInSavedDir(ctx, info[0].Name())
-			if err != nil {
-				return errors.Wrap(err, "failed to get file path")
-			}
-			ir, err := imageResolution(path)
-			if err != nil {
-				return errors.Wrap(err, "failed to get image resolution")
+				return err
 			}
 			if ir.Width != or.Width || ir.Height != or.Height {
 				return errors.Wrapf(err, "incorrect captured resolution get %dx%d; want %dx%d", ir.Width, ir.Height, or.Width, or.Height)
@@ -158,6 +162,40 @@ func testPhotoResolution(ctx context.Context, app *cca.App) error {
 			return nil
 		})
 	})
+}
+
+// takePhotoAndGetResolution takes a photo and extract the resolution of the taken photo
+func takePhotoAndGetResolution(ctx context.Context, app *cca.App, handleOrientation bool) (*cca.Resolution, error) {
+	info, err := app.TakeSinglePhoto(ctx, cca.TimerOff)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to take photo")
+	}
+	path, err := app.FilePathInSavedDir(ctx, info[0].Name())
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get file path")
+	}
+	ir, err := imageResolution(path, handleOrientation)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get image resolution")
+	}
+	return ir, nil
+}
+
+// recordVideoAndGetResolution records a video and extract the resolution of the recoreded video
+func recordVideoAndGetResolution(ctx context.Context, app *cca.App) (*cca.Resolution, error) {
+	info, err := app.RecordVideo(ctx, cca.TimerOff, time.Second)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to record video")
+	}
+	path, err := app.FilePathInSavedDir(ctx, info.Name())
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get file path")
+	}
+	vr, err := videoTrackResolution(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to extract resolution from the video")
+	}
+	return vr, nil
 }
 
 // videoTrackResolution returns the resolution from video file under specified path.
@@ -202,6 +240,16 @@ func videoTrackResolution(path string) (*cca.Resolution, error) {
 }
 
 func testVideoResolution(ctx context.Context, app *cca.App) error {
+	// TODO(b/215484798): Remove the logic for old UI once the new UI applied.
+	useNewUI, err := app.Exist(ctx, cca.VideoResolutionSettingButton)
+	if err != nil {
+		return errors.Wrap(err, "failed to check existence of the video resolution settings button")
+	}
+	if useNewUI {
+		return testVideoResolutionAndFPS(ctx, app)
+	}
+
+	// The test logic for legacy video resolutions UI.
 	return app.RunThroughCameras(ctx, func(facing cca.Facing) error {
 		if err := app.SwitchMode(ctx, cca.Video); err != nil {
 			return errors.Wrap(err, "failed to switch to video mode")
@@ -220,15 +268,7 @@ func testVideoResolution(ctx context.Context, app *cca.App) error {
 				return errors.Wrapf(err, "inconsistent preview aspect ratio get %d:%d; want %d:%d", pr.Width, pr.Height, or.Width, or.Height)
 			}
 
-			info, err := app.RecordVideo(ctx, cca.TimerOff, time.Second)
-			if err != nil {
-				return errors.Wrap(err, "failed to record video")
-			}
-			path, err := app.FilePathInSavedDir(ctx, info.Name())
-			if err != nil {
-				return errors.Wrap(err, "failed to get file path")
-			}
-			vr, err := videoTrackResolution(path)
+			vr, err := recordVideoAndGetResolution(ctx, app)
 			if err != nil {
 				return err
 			}
@@ -286,49 +326,22 @@ func iterateResolutions(ctx context.Context, app *cca.App, rt cca.ResolutionType
 	toggleOption := func(index int) (cca.Resolution, error) {
 		var r cca.Resolution
 		err := withInnerResolutionSetting(ctx, app, rt, facing, func() error {
-			value, err := app.AttributeWithIndex(ctx, optionUI, index, "data-width")
+			width, err := attributeValueOfOption(ctx, app, optionUI, index, "data-width")
 			if err != nil {
 				return err
 			}
-			width, err := strconv.Atoi(value)
-			if err != nil {
-				return errors.Wrapf(err, "failed to convert width value %v to int", width)
-			}
-			value, err = app.AttributeWithIndex(ctx, optionUI, index, "data-height")
+			height, err := attributeValueOfOption(ctx, app, optionUI, index, "data-height")
 			if err != nil {
 				return err
 			}
-			height, err := strconv.Atoi(value)
-			if err != nil {
-				return errors.Wrapf(err, "failed to convert height value %v to int", height)
+			if err := clickOptionAndWaitConfiguration(ctx, app, optionUI, index); err != nil {
+				return errors.Wrap(err, "failed to click option and wait configration done")
 			}
-
-			testing.ContextLogf(ctx, "Switch to %v facing %v resolution %dx%d", facing, rt, width, height)
-			checked, err := app.IsCheckedWithIndex(ctx, optionUI, index)
-			if err != nil {
-				return err
-			}
-			if checked {
-				testing.ContextLogf(ctx, "%d th resolution option is already checked", index)
-			} else {
-				if err := app.TriggerConfiguration(ctx, func() error {
-					testing.ContextLogf(ctx, "Checking with %d th resolution option", index)
-					if err := app.ClickWithIndex(ctx, optionUI, index); err != nil {
-						return errors.Wrap(err, "failed to click on resolution item")
-					}
-					return nil
-				}); err != nil {
-					return errors.Wrap(err, "camera configuration failed after switching resolution")
-				}
-			}
-
 			r.Width = width
 			r.Height = height
 			return nil
-
 		})
 		return r, err
-
 	}
 
 	for index := 0; index < numOptions; index++ {
@@ -342,4 +355,258 @@ func iterateResolutions(ctx context.Context, app *cca.App, rt cca.ResolutionType
 	}
 
 	return nil
+}
+
+func clickOptionAndWaitConfiguration(ctx context.Context, app *cca.App, optionUI cca.UIComponent, index int) error {
+	testing.ContextLogf(ctx, "Switch to #%v of %v", index, optionUI.Name)
+	checked, err := app.IsCheckedWithIndex(ctx, optionUI, index)
+	if err != nil {
+		return err
+	}
+	if checked {
+		testing.ContextLogf(ctx, "#%d resolution option is already checked", index)
+	} else {
+		if err := app.TriggerConfiguration(ctx, func() error {
+			testing.ContextLogf(ctx, "Checking with #%d resolution option", index)
+			if err := app.ClickWithIndex(ctx, optionUI, index); err != nil {
+				return errors.Wrap(err, "failed to click on resolution item")
+			}
+			return nil
+		}); err != nil {
+			return errors.Wrap(err, "camera configuration failed after switching resolution")
+		}
+	}
+
+	return nil
+}
+
+// attributeValueOfOption returns the attribute value of |index| th of
+// |optionsUI| given by the name of the attribute. The value will be in integer.
+func attributeValueOfOption(ctx context.Context, app *cca.App, optionsUI cca.UIComponent, index int, attribute string) (int, error) {
+	stringValue, err := app.AttributeWithIndex(ctx, optionsUI, index, attribute)
+	if err != nil {
+		return -1, err
+	}
+	intValue, err := strconv.Atoi(stringValue)
+	if err != nil {
+		return -1, errors.Wrapf(err, "failed to convert the value (%v) of the attribute (%v) to int", stringValue, attribute)
+	}
+	return intValue, nil
+}
+
+// getOrientedAspectRatio gets aspect ratio with respect to screen orientation.
+func getOrientedAspectRatio(ctx context.Context, app *cca.App, aspectRatio float64) (float64, error) {
+	orientation, err := app.GetScreenOrientation(ctx)
+	if err != nil {
+		return 0, err
+	}
+	isLandscape := (orientation == cca.LandscapePrimary || orientation == cca.LandscapeSecondary)
+	if (isLandscape && aspectRatio < 1) || (!isLandscape && aspectRatio > 1) {
+		aspectRatio = 1 / aspectRatio
+	}
+	return aspectRatio, nil
+}
+
+// testPhotoResolutionAndAspectRatio tests the mutual behavior of photo resolutions and aspect ratio.
+func testPhotoResolutionAndAspectRatio(ctx context.Context, app *cca.App) error {
+	return app.RunThroughCameras(ctx, func(facing cca.Facing) error {
+		if facing == cca.FacingExternal {
+			return nil
+		}
+
+		if err := app.SwitchMode(ctx, cca.Photo); err != nil {
+			return errors.Wrap(err, "failed to switch to photo mode")
+		}
+
+		if err := cca.MainMenu.Open(ctx, app); err != nil {
+			return err
+		}
+		defer cca.MainMenu.Close(ctx, app)
+
+		if err := cca.PhotoAspectRatioMenu.Open(ctx, app); err != nil {
+			return err
+		}
+		defer cca.PhotoAspectRatioMenu.Close(ctx, app)
+
+		aspectRatioOptions := cca.FrontAspectRatioOptions
+		if facing == cca.FacingBack {
+			aspectRatioOptions = cca.BackAspectRatioOptions
+		}
+		numOptions, err := app.CountUI(ctx, aspectRatioOptions)
+		if err != nil {
+			return errors.Wrap(err, "failed to count the aspect ratio options")
+		} else if numOptions < 2 {
+			// Ensure that at least two options are provided since "square" will
+			// always be an option.
+			return errors.Wrapf(err, "unexpected amount of options: %v", numOptions)
+		}
+
+		// Checks that when changing the aspect ratio preference of current
+		// running camera, the camera stream will be reconfigured.
+		for index := 0; index < numOptions; index++ {
+			if err := clickOptionAndWaitConfiguration(ctx, app, aspectRatioOptions, index); err != nil {
+				return errors.Wrap(err, "failed to click the aspect ratio option and wait for the configration done")
+			}
+
+			value, err := app.AttributeWithIndex(ctx, aspectRatioOptions, index, "data-aspect-ratio")
+			if err != nil {
+				return err
+			}
+			aspectRatio, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				return errors.Wrapf(err, "failed to convert aspect ratio value %v to float", aspectRatio)
+			}
+			orientedAspectRatio, err := getOrientedAspectRatio(ctx, app, aspectRatio)
+			if err != nil {
+				return errors.Wrap(err, "failed to get oriented aspect ratio value")
+			}
+
+			// For each aspect ratio, tests through all possible photo resolution options in the photos resolution settings.
+			if err := clickThroughAllPhotoResolutionOptions(ctx, app, facing, orientedAspectRatio); err != nil {
+				return errors.Wrap(err, "failed to check photo resolution options")
+			}
+		}
+		return nil
+	})
+}
+
+// checkResolutionAspectRatio checks the aspect ratio of |resolution| is close to the given |aspectRatio|.
+func checkResolutionAspectRatio(ctx context.Context, resolution *cca.Resolution, aspectRatio float64) error {
+	// aspectRatioTolerance is the small aspect ratio comparison tolerance for
+	// judging the treeya's special resolution 848:480(1.766...) which should be
+	// counted as 16:9(1.77...).
+	const aspectRatioTolerance = 0.02
+	if math.Abs(resolution.AspectRatio()-aspectRatio) > aspectRatioTolerance {
+		return errors.Errorf("inconsistent aspect ratio. Resolution: %d:%d; Want aspect ratio: %v", resolution.Width, resolution.Height, aspectRatio)
+	}
+	return nil
+}
+
+// clickThroughAllPhotoResolutionOptions tries out all the photo resolution options under current aspect ratio and ensures the configuration works successfully.
+func clickThroughAllPhotoResolutionOptions(ctx context.Context, app *cca.App, facing cca.Facing, aspectRatio float64) error {
+	if err := cca.PhotoAspectRatioMenu.Close(ctx, app); err != nil {
+		return errors.Wrap(err, "failed to close the aspect ratio settings page")
+	}
+	defer cca.PhotoAspectRatioMenu.Open(ctx, app)
+
+	if err := cca.PhotoResolutionMenu.Open(ctx, app); err != nil {
+		return err
+	}
+	defer cca.PhotoResolutionMenu.Close(ctx, app)
+
+	photoResolotionOptions := cca.FrontPhotoResolutionOptions
+	if facing == cca.FacingBack {
+		photoResolotionOptions = cca.BackPhotoResolutionOptions
+	}
+
+	numOptions, err := app.CountUI(ctx, photoResolotionOptions)
+	if err != nil {
+		return errors.Wrap(err, "failed to count the aspect ratio options")
+	}
+
+	for index := 0; index < numOptions; index++ {
+		if err := clickOptionAndWaitConfiguration(ctx, app, photoResolotionOptions, index); err != nil {
+			return errors.Wrap(err, "failed to click the aspect ratio option and wait for the configration done")
+		}
+
+		// Ensure preview viewport has correct aspect ratio.
+		pr, err := app.GetPreviewViewportSize(ctx)
+		if err != nil {
+			return err
+		}
+		if err := checkResolutionAspectRatio(ctx, &pr, aspectRatio); err != nil {
+			return err
+		}
+
+		// Ensure captured photo has correct aspect ratio.
+		ir, err := takePhotoAndGetResolution(ctx, app, aspectRatio != float64(1))
+		if err != nil {
+			return err
+		}
+		if err := checkResolutionAspectRatio(ctx, ir, aspectRatio); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// testVideoResolutionAndFPS tests the behavior of selecting video resolution and the FPS buttons.
+func testVideoResolutionAndFPS(ctx context.Context, app *cca.App) error {
+	// The FPS buttons are currently available on external cameras only. Add
+	// corresponding tests once they are supported on built-in cameras.
+	return app.RunThroughCameras(ctx, func(facing cca.Facing) error {
+		if facing == cca.FacingExternal {
+			return nil
+		}
+
+		if err := app.SwitchMode(ctx, cca.Video); err != nil {
+			return errors.Wrap(err, "failed to switch to video mode")
+		}
+
+		if err := cca.MainMenu.Open(ctx, app); err != nil {
+			return err
+		}
+		defer cca.MainMenu.Close(ctx, app)
+
+		if err := cca.VideoResolutionMenu.Open(ctx, app); err != nil {
+			return err
+		}
+		defer cca.VideoResolutionMenu.Close(ctx, app)
+
+		videoResolutionOptions := cca.FrontVideoResolutionOptions
+		if facing == cca.FacingBack {
+			videoResolutionOptions = cca.BackVideoResolutionOptions
+		}
+		numOptions, err := app.CountUI(ctx, videoResolutionOptions)
+		if err != nil {
+			return errors.Wrap(err, "failed to count the video resolution options")
+		}
+
+		for index := 0; index < numOptions; index++ {
+			if err := clickOptionAndWaitConfiguration(ctx, app, videoResolutionOptions, index); err != nil {
+				return errors.Wrap(err, "failed to click the aspect ratio option and wait for the configration done")
+			}
+
+			width, err := attributeValueOfOption(ctx, app, videoResolutionOptions, index, "data-width")
+			if err != nil {
+				return err
+			}
+			height, err := attributeValueOfOption(ctx, app, videoResolutionOptions, index, "data-height")
+			if err != nil {
+				return err
+			}
+
+			shouldCheckResolution := (width == 3840 && height == 2160) ||
+				(width == 2560 && height == 1440) ||
+				(width == 1920 && height == 1080) ||
+				(width == 1280 && height == 720)
+			if !shouldCheckResolution {
+				continue
+			}
+
+			r := cca.Resolution{Width: width, Height: height}
+			or, err := getOrientedResolution(ctx, app, r)
+			if err != nil {
+				return errors.Wrap(err, "failed to get oriented resolution")
+			}
+			// Ensure preview viewport has correct aspect ratio as the target video resolution.
+			pr, err := app.GetPreviewViewportSize(ctx)
+			if err != nil {
+				return err
+			}
+			if err := checkResolutionAspectRatio(ctx, &pr, or.AspectRatio()); err != nil {
+				return err
+			}
+
+			// Ensure captured video has correct resolution.
+			vr, err := recordVideoAndGetResolution(ctx, app)
+			if err != nil {
+				return err
+			}
+			if vr.Width != or.Width || vr.Height != or.Height {
+				return errors.Wrapf(err, "incorrect captured resolution get %dx%d; want %dx%d", vr.Width, vr.Height, or.Width, or.Height)
+			}
+		}
+		return nil
+	})
 }
