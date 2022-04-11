42b728c0ca8a73c6ff20aa7dbe70586b7e22c892
Xibin Liu <xliu@cienet.com>
2022-04-11 11:03:42 -0700

    cuj: modify MetricConfigs() to support lacros
    Pass in the test API conns to the function so test can specify metrics
    collection either lacros-Chrome, ash-Chrome, or both.
    
    BUG=b:227525974
    TEST=fast_build.sh -T
    
    Change-Id: Ia62eed18f635522bc1efd44dd38ac3d88a53f776

--- src/chromiumos/tast/local/bundles/cros/ui/videocuj/video_cuj.go
+++ src/chromiumos/tast/local/bundles/cros/ui/videocuj/video_cuj.go
@@ -135,7 +135,7 @@ func Run(ctx context.Context, resources TestResources, param TestParams) error {
 	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
 	defer cancel()
 
-	recorder, err := cuj.NewRecorder(ctx, cr, a, cuj.MetricConfigs()...)
+	recorder, err := cuj.NewRecorder(ctx, cr, a, cuj.MetricConfigs([]*chrome.TestConn{tconn})...)
 	if err != nil {
 		return errors.Wrap(err, "failed to create a recorder")
 	}
