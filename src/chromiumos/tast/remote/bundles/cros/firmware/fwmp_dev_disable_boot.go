89c0927376dce549099d04f6b3a4e7ea84670900
Yi Chou <yich@google.com>
2022-04-11 16:51:17 +0800

    faft: Use ownership ID to check the TPM status
    These TPM related commands in cryptohome had been deprecated, and
    the The TPM ownership may be automatically taken in the future, checking
    the TPM owned status would not useful in that case. We should check the
    ownership ID to make sure the TPM had been cleared.
    
    BUG=b:228800370, b:227682591
    TEST=tast run $DUT firmware.FwmpDevDisableBoot
    
    Change-Id: I733050bfaca12baf9da6398c11ef9a7212e095e9

--- src/chromiumos/tast/remote/bundles/cros/firmware/fwmp_dev_disable_boot.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/fwmp_dev_disable_boot.go
@@ -6,9 +6,7 @@ package firmware
 
 import (
 	"context"
-	"regexp"
 	"strings"
-	"time"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/remote/firmware"
@@ -16,14 +14,6 @@ import (
 	"chromiumos/tast/ssh"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
-)
-
-const (
-	// The duration for takeOwnershipDelay was determined experimentally. The following warning would occur without
-	// this delay: "WARNING cryptohome: [bus.cc(638)] Bus::SendWithReplyAndBlock took 11465ms to process message:
-	// type=method_call, path=/org/chromium/UserDataAuth, interface=org.chromium.InstallAttributesInterface,
-	// member=SetFirmwareManagementParameters".
-	takeOwnershipDelay = 20 * time.Second
 )
 
 func init() {
@@ -45,47 +35,16 @@ func FwmpDevDisableBoot(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to init servo: ", err)
 	}
 
-	// checkTPMNotOwned returns error if TPM is owned.
-	checkTPMNotOwned := func(ctx context.Context) error {
-		out, err := s.DUT().Conn().CommandContext(ctx, "cryptohome", "--action=status").Output(ssh.DumpLogOnError)
-		if err != nil {
-			return errors.Wrap(err, "unable to read TPM status")
-		}
-		regex := `\"owned\":(\s+\w+\s?)`
-		expMatch := regexp.MustCompile(regex)
-		matches := expMatch.FindStringSubmatch(string(out))
-		if len(matches) < 2 {
-			return errors.Errorf("failed to match regex %q in %q", expMatch, string(out))
-		}
-		tpmStatus := strings.TrimSpace(matches[1])
-		if tpmStatus == "true" {
-			return errors.New("got TPM Owned: true, expected it to be false")
-		}
-		return nil
-	}
-
 	setFWMP := func(ctx context.Context, flags string) error {
 		s.Log("Taking ownership")
-		if err := s.DUT().Conn().CommandContext(ctx, "cryptohome", "--action=tpm_take_ownership").Run(ssh.DumpLogOnError); err != nil {
+		if err := s.DUT().Conn().CommandContext(ctx, "tpm_manager_client", "take_ownership").Run(ssh.DumpLogOnError); err != nil {
 			return errors.Wrap(err, "failed to take ownership")
-		}
-		s.Logf("Sleeping for %s before taking ownership", takeOwnershipDelay)
-		if err := testing.Sleep(ctx, takeOwnershipDelay); err != nil {
-			return errors.Wrap(err, "failed to sleep")
-		}
-		if err := s.DUT().Conn().CommandContext(ctx, "cryptohome", "--action=tpm_wait_ownership").Run(ssh.DumpLogOnError); err != nil {
-			return errors.Wrap(err, "failed to wait for ownership")
 		}
 		s.Log("Setting firmware management parameters")
 		if err := s.DUT().Conn().CommandContext(ctx, "cryptohome", "--action=set_firmware_management_parameters", "--flags=0x"+flags).Run(ssh.DumpLogOnError); err != nil {
 			return errors.Wrapf(err, "failed to set firmware management parameters with flags 0x%s", flags)
 		}
 		return nil
-	}
-
-	s.Log("Checking that TPM is not owned")
-	if err := checkTPMNotOwned(ctx); err != nil {
-		s.Fatal("While checking TPM status: ", err)
 	}
 
 	// Set DUT in "dev mode disable" state by setting TPM flags to "0x1".
@@ -104,6 +63,12 @@ func FwmpDevDisableBoot(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to create mode switcher: ", err)
 	}
 
+	ownershipData, err := s.DUT().Conn().CommandContext(ctx, "hwsec-ownership-id", "id").Output(ssh.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to get ownership ID: ", err)
+	}
+	ownershipID := strings.TrimSpace(string(ownershipData))
+
 	// When dev mode is disabled by FWMP, DUT is expected to boot into normal mode.
 	var opts []firmware.ModeSwitchOption
 	opts = append(opts, firmware.SkipModeCheckAfterReboot, firmware.PressEnterAtToNorm)
@@ -111,9 +76,9 @@ func FwmpDevDisableBoot(ctx context.Context, s *testing.State) {
 		s.Fatal("Unexpected error occurred while attempting to boot DUT: ", err)
 	}
 
-	// Confirm TPM is not owned.
-	s.Log("Checking that TPM is not owned at the end of the test")
-	if err := checkTPMNotOwned(ctx); err != nil {
-		s.Fatal("While checking TPM status: ", err)
+	// Confirm TPM ownership changed.
+	s.Log("Checking that TPM ownership changed at the end of the test")
+	if err = s.DUT().Conn().CommandContext(ctx, "hwsec-ownership-id", "diff", "--id="+ownershipID).Run(ssh.DumpLogOnError); err != nil {
+		s.Fatal("While checking TPM ownership: ", err)
 	}
 }
