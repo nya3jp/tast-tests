0921e0c03715e08ff85eabcfb44e1522689c7ee7
Wei Lee <wtlee@google.com>
2022-04-11 11:29:34 +0800

    camera: Make tests compatible after moving grid/timer out of settings UI
    According to the new Settings UI design, the grid/timer option will be
    moved out of settings UI and be placed at options group. Ideally, the
    tests should be moved to somewhere like CCAUIPreviewOptions.
    
    This CL also tweaks the test logic for mirror button since according to
    the new UI spec, it also has a option panel to be consistent with
    grid/timer options.
    
    BUG=b:215484798
    TEST=tast run [DUT] camera.CCAUISettings camera.CCAUIPreviewOptions
    
    Change-Id: Ie33b7543b55916b7d23e8f2e87c774b0d48781de

--- src/chromiumos/tast/local/camera/cca/cca.go
+++ src/chromiumos/tast/local/camera/cca/cca.go
@@ -1132,20 +1132,30 @@ func (a *App) DisableQRCodeDetection(ctx context.Context) error {
 
 // SetTimerOption sets the timer option to on/off.
 func (a *App) SetTimerOption(ctx context.Context, state TimerState) error {
+	// TODO(b/215484798): Removed the logic for old UI once the new UI applied.
+	useOldUI, err := a.ExistOption(ctx, TimerOption)
+	if err != nil {
+		return errors.Wrap(err, "failed to check the existence of the timer toggle")
+	}
+
 	active := state == TimerOn
 	if cur, err := a.State(ctx, "timer"); err != nil {
 		return err
 	} else if cur != active {
-		if _, err := a.ToggleOption(ctx, TimerOption); err != nil {
-			return err
-		}
-	}
-	// Fix timer to 3 seconds for saving test time.
-	if active {
-		if delay3, err := a.State(ctx, "timer-3s"); err != nil {
-			return err
-		} else if !delay3 {
-			return errors.New("default timer is not set to 3 seconds")
+		if useOldUI {
+			if _, err := a.ToggleOption(ctx, TimerOption); err != nil {
+				return err
+			}
+		} else {
+			if err := a.Click(ctx, OpenTimerPanelButton); err != nil {
+				return errors.Wrap(err, "failed to open timer option panel")
+			}
+			if err := a.ClickChildIfContain(ctx, OptionsContainer, "3 seconds"); err != nil {
+				return errors.Wrap(err, "failed to click the 3s timer button")
+			}
+			if err := a.WaitForState(ctx, "timer-3s", true); err != nil {
+				return errors.Wrap(err, "failed to wait for 3s-timer being active")
+			}
 		}
 	}
 	return nil
