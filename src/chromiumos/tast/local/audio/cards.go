5dbef6f54587ca1eb9ec22fb5fb10244c2385b24
Li-Yu Yu <aaronyu@google.com>
2022-04-12 02:41:40 +0800

    audio: Add function to get sound cards
    BUG=None
    TEST=~/chromiumos/src/platform/tast/fast_build.sh -T
    
    Change-Id: Ibbe0781286c8f057adfab5d8714d5caa90edf40c

---
+++ src/chromiumos/tast/local/audio/cards.go
@@ -0,0 +1,66 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package audio
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+// A card listed in /proc/asound/cards as:
+// ## [ID             ]: Driver - ShortName
+//                       LongName
+//
+// snd_iprintf(buffer, "%2i [%-15s]: %s - %s\n",
+// 	idx,
+// 	card->id,
+// 	card->driver,
+// 	card->shortname);
+// snd_iprintf(buffer, "                      %s\n",
+// 	card->longname);
+type Card struct {
+	ID        string
+	Driver    string
+	ShortName string
+	LongName  string
+}
+
+var cardRegexp = regexp.MustCompile(`[ \d]{2} \[(.{15})\]: (\S+) - (.+)
+\s+(.+)
+`)
+
+func parseSoundCards(s string) (cards []Card, err error) {
+	if s == "--- no soundcards ---\n" {
+		return
+	}
+
+	matches := cardRegexp.FindAllStringSubmatch(s, -1)
+	if cards, lines := len(matches), strings.Count(s, "\n"); lines != cards*2 {
+		return nil, fmt.Errorf(
+			"found %d cards from %d lines, should find %g cards instead",
+			cards, lines, float64(lines)/2)
+	}
+
+	for _, m := range matches {
+		cards = append(cards, Card{
+			strings.TrimSpace(m[1]),
+			m[2],
+			m[3],
+			m[4],
+		})
+	}
+	return
+}
+
+// Get sound cards from /proc/asound/cards
+func GetSoundCards() (cards []Card, err error) {
+	b, err := os.ReadFile("/proc/asound/cards")
+	if err != nil {
+		return
+	}
+	return parseSoundCards(string(b))
+}
