0de39a249ef7c94e9bff3059516bb8e8ef1ef1f8
Newman Liu <chriswu@allion.corp-partner.google.com>
2022-04-12 14:53:59 +0800

    docking: Audio Test over USB via a Dock Station
    Add wwcb/dock4 to test headphone, microphone,
    Chrome OS speaker via a Dock.
    
    Upload data/test_video.mp4 as testing requirement.
    
    Add screenshot function if the test failed.
    Update comments in wwcb/utils.
    
    Pre-condition:
    1. External display (HDMI/DP/VGA/DVI/USB-C).
    2. Docking station.
    3. External audio device (Headphone/Microphone/Speaker).
    
    Procedure:
    1. Boot up the Chromebook.
    2. Connect external display to docking station.
    3. Connect docking station to Chromebook.
    4. Connect exteral audio device onto Dock station.
    5. Play YouTube video.
    6. Record audio and verify playback.
    
    Verification:
    1. By default, "Output/Input - USB Audio" should be
    checked associate with docking station.
    2. Make sure audio playback without any issue.
    
    BUG=b:229924405
    FIXED=b:229924405
    TEST=tast run -var=utils.WWCBIP=192.168.1.198 -var=DockingID=1912901 -var=1stExtDispID=2007901 192.168.1.223 wwcb.Dock4UsbAudio
    TEST=tast unit test passed.
    
    Change-Id: Ic32521f2cf85fea8505545381923f05108156a04

--- src/chromiumos/tast/local/bundles/cros/wwcb/utils/wwcb_fixture.go
+++ src/chromiumos/tast/local/bundles/cros/wwcb/utils/wwcb_fixture.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Copyright 2022 The ChromiumOS Authors.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,12 +14,10 @@ import (
 // InitFixtures resets all fixtures at the beginning of testing.
 // To avoid fixtures status are not unified then cause fail.
 func InitFixtures(ctx context.Context) error {
-	testing.ContextLog(ctx, "Initialize fixtures")
 	// Disconnect all fixtures.
 	if err := CloseAll(ctx); err != nil {
 		return err
 	}
-	// Docking station uses port 1.
 	// Turn off & on docking station power.
 	if err := ControlAviosys(ctx, "0", "1"); err != nil {
 		return errors.Wrap(err, "failed to turn off docking station power")
@@ -28,4 +26,19 @@ func InitFixtures(ctx context.Context) error {
 		return errors.Wrap(err, "failed to turn on docking station power")
 	}
 	return nil
+}
+
+// DumpScreenshotOnError checks the given hasError function and dumps each display screenshot in the WWCB server.
+func DumpScreenshotOnError(ctx context.Context, hasError func() bool, fixtureIDs []string) {
+	if !hasError() {
+		return
+	}
+
+	testing.ContextLog(ctx, "Start dumping screenshot")
+	fixtureIDs = append(fixtureIDs, "chromebook")
+	for _, id := range fixtureIDs {
+		if _, err := ScreenCapture(ctx, id); err != nil {
+			testing.ContextLog(ctx, "Failed to capture screenshot: ", err)
+		}
+	}
 }
