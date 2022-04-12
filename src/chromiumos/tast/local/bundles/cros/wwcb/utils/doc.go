2c26323ca471cf027664dc9877ec8b04f8549382
Newman Liu <chriswu@allion.corp-partner.google.com>
2022-04-12 14:44:05 +0800

    docking: add dock1_persistent_settings.go script in WWCB
    Test persistent settings of window bound placement across displays in one use session via a Dock
    Command Line explaination: Tast run -var=utils.WWCBIP=[The IP of WWCB service] -var=Docking=[The fixture
    ID that control docking station] -var=1stExtDisp=[The fixture ID that
    control 1st external display] [The IP of Chromebook]
    wwcb.Dock1PersistentSettings
    
    BUG=b:222188262
    FIXED=b:222188262
    TEST=tast run -var=utils.WWCBIP=192.168.1.198 -var=Docking=1912901 -var=1stExtDisp=2007901 192.168.1.200 wwcb.Dock1PersistentSettings
    TEST=tast unit test passed.
    
    Change-Id: I9771acbcbbe99164e19f3bc54155011aee86f737

---
+++ src/chromiumos/tast/local/bundles/cros/wwcb/utils/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package utils contains utility functions for wwcb tests.
+package utils
