f00a35f055f2ec997ae6a485010edc8c3dc2a4d5
Jason Rhee <jasonrhee@google.com>
2022-04-12 13:45:17 -0700

    [SmartLock] Use DoDefault() instead of LeftClick() for smartlock tests
    There is currently a bug where ImmediateLocation() doesn't report correct
    UI element coordinates. This solution does a left click on the arrow button
    without requiring coordinates. Bug filed for ImmediateLocation() coords.
    
    BUG=b/226204290
    TESTED=Ran multiple smartlock.Unlock test runs on chromeos15-row3-metro1-unit1.
    
    Change-Id: Iec8f8efa9cd7bf5b0cb6cfeebac1943ad29e546a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3582827
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Tested-by: Jason Rhee <jasonrhee@google.com>
    Commit-Queue: Jason Rhee <jasonrhee@google.com>
    (cherry picked from commit a7d6eb3f374dd37114591cf0b5033129c42afc5c)

--- src/chromiumos/tast/local/chrome/uiauto/lockscreen/lockscreen.go
+++ src/chromiumos/tast/local/chrome/uiauto/lockscreen/lockscreen.go
@@ -178,7 +178,7 @@ func SubmitPIN(ctx context.Context, tconn *chrome.TestConn) error {
 // ClickSmartLockArrowButton clicks the arrow button to login with Smart Lock.
 func ClickSmartLockArrowButton(ctx context.Context, tconn *chrome.TestConn) error {
 	ui := uiauto.New(tconn)
-	return ui.WithTimeout(uiTimeout).WithInterval(time.Second).LeftClickUntil(SmartLockArrowButtonFinder, ui.Gone(SmartLockArrowButtonFinder))(ctx)
+	return ui.WithTimeout(uiTimeout).DoDefault(SmartLockArrowButtonFinder)(ctx)
 }
 
 // WaitForSmartLockReady waits for UI signal that the chromebook is ready to be unlocked by Smart Lock.
