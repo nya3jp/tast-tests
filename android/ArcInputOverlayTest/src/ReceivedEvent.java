6354895058f8e883adc33473c00931b7a86265a2
Cici Ruan <cuicuiruan@google.com>
2022-04-13 16:20:37 -0700

    arc/gio: Add a test apk for input overlay test
    This test apk is used for testing input overlay feature.
    1. It basically prints the received event on the UI so Tast-test can
       read what is received.
    2. The text field is added for testing if the touch injector still
       applies if the text field is focused.
    
    BUG=b:229156145
    TEST=emerge-${BOARD} tast-local-apks-cros is successful and app is
    running after installment.
    
    Change-Id: I69e43188e893a0ea12578ce3951182ffbb04db5e

---
+++ android/ArcInputOverlayTest/src/ReceivedEvent.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.inputoverlay;
+
+import android.view.InputDevice;
+import android.view.InputEvent;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+
+// ReceivedEvent includes the readable input events received by the app.
+class ReceivedEvent {
+  public Long eventTime;
+  public Long receiveTimeNs;
+  public String source;
+  public String code;
+  public String action;
+  public String actionButton;
+  public ReceivedEvent(InputEvent event, Long receiveTimeNs) {
+    this.eventTime = event.getEventTime();
+    this.receiveTimeNs = receiveTimeNs;
+    source = sourceToString(event);
+    code = codeToString(event);
+    action = actionToString(event);
+    actionButton = actionButtonToString(event);
+  }
+
+   private String sourceToString(InputEvent event) {
+    final int source = event.getSource();
+    switch (source) {
+      case InputDevice.SOURCE_KEYBOARD:
+        return "keyboard";
+      case InputDevice.SOURCE_JOYSTICK:
+        return "joystick";
+      case InputDevice.SOURCE_GAMEPAD:
+        return "gamepad";
+      case InputDevice.SOURCE_TOUCHSCREEN:
+        return "touchscreen";
+      case InputDevice.SOURCE_TOUCHPAD:
+        return "touchpad";
+      case InputDevice.SOURCE_MOUSE:
+        return "mouse";
+      default:
+        return "Unsupported source";
+    }
+  }
+
+  // KeyEvent code to string.
+  private static String codeToString(InputEvent event) {
+    if (event instanceof KeyEvent) {
+      final int code = ((KeyEvent) event).getKeyCode();
+      return KeyEvent.keyCodeToString(code);
+    }
+    // For event is not KeyEvent.
+    return "N/A keycode";
+  }
+
+  // Event action to string.
+  private static String actionToString(InputEvent event) {
+    if (event instanceof MotionEvent) {
+      final int action = ((MotionEvent) event).getActionMasked();
+      return MotionEvent.actionToString(action);
+    } else if (event instanceof KeyEvent) {
+      final int action = ((KeyEvent) event).getAction();
+      switch (action) {
+        case KeyEvent.ACTION_DOWN:
+          return "ACTION_DOWN";
+        case KeyEvent.ACTION_UP:
+          return "ACTION_UP";
+        default:
+          return "N/A action";
+      }
+    } else {
+      return "N/A action";
+    }
+  }
+
+  // MotionEvent action button to string. Only consider the mouse primary button and secondary
+  // button for now.
+  private static String actionButtonToString(InputEvent event) {
+    if (event instanceof MotionEvent) {
+      final int actionBtn = ((MotionEvent) event).getActionButton();
+      if ((actionBtn & MotionEvent.BUTTON_PRIMARY) != 0) {
+        return "BUTTON_PRIMARY";
+      }
+      if ((actionBtn & MotionEvent.BUTTON_SECONDARY) != 0 ) {
+        return "BUTTON_SECONDARY";
+      }
+    }
+    return "N/A action_button";
+  }
+
+}
