bf6c9afe2d2bc9a355c4bfdc3f8ecb5faee30f65
Cici Ruan <cuicuiruan@google.com>
2022-04-13 16:20:37 -0700

    arc/gio: Add a test apk for input overlay test
    This test apk is used for testing input overlay feature.
    
    BUG=b:229156145
    TEST=emerge-${BOARD} tast-local-apks-cros is successful and app is
    running after installment.
    
    Change-Id: I69e43188e893a0ea12578ce3951182ffbb04db5e

---
+++ android/ArcInputOverlayTest/src/MainActivity.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package org.chromium.arc.testapp.inputoverlay;
+
+import android.app.Activity;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.InputDevice;
+import android.view.InputEvent;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends Activity {
+  final static String TAG = "InputOverlayTest";
+
+  private Button mButton;
+  private EditText mEdit;
+  private ListView mList;
+  private EventListAdapter mAdapter;
+  private ArrayList<ReceivedEvent> mEvents;
+  private ExecutorService mExecutor;
+  private View mView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mEdit = findViewById(R.id.m_edit);
+    mButton = findViewById(R.id.m_button);
+    mList = findViewById(R.id.m_list);
+    mEvents = new ArrayList<>();
+    mAdapter = new EventListAdapter(getApplicationContext(), mEvents);
+    mList.setAdapter(mAdapter);
+    mExecutor = Executors.newSingleThreadExecutor();
+    mView = findViewById(R.id.main_view);
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+  }
+
+  @Override
+  protected void onStop() {
+    try {
+      mExecutor.awaitTermination(5, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    super.onStop();
+  }
+
+  @Override
+  public boolean dispatchTouchEvent(MotionEvent ev) {
+    printEvent(ev);
+    // Stop dispatching gamepad event.
+    if (isGameEvent(ev)) {
+      return true;
+    }
+    return super.dispatchTouchEvent(ev);
+  }
+
+  @Override
+  public boolean dispatchKeyEvent(KeyEvent ev) {
+    if (ev.getKeyCode() == KeyEvent.KEYCODE_TAB && ev.getAction() == KeyEvent.ACTION_DOWN) {
+      mEdit.clearFocus();
+      mEdit.setCursorVisible(false);
+      return false;
+    } else if (ev.getKeyCode() == KeyEvent.KEYCODE_TAB && ev.getAction() == KeyEvent.ACTION_UP) {
+      return false;
+    }
+    printEvent(ev);
+    // Stop dispatching gamepad event.
+    if (isGameEvent(ev)) {
+      return true;
+    }
+    return super.dispatchKeyEvent(ev);
+  }
+
+  @Override
+  public boolean dispatchGenericMotionEvent(MotionEvent ev) {
+    printEvent(ev);
+    // Stop dispatching gamepad event.
+    if (isGameEvent(ev)) {
+      return true;
+    }
+    return super.dispatchGenericMotionEvent(ev);
+  }
+
+  @Override
+  public boolean dispatchTrackballEvent(MotionEvent ev) {
+    printEvent(ev);
+    return super.dispatchTrackballEvent(ev);
+  }
+
+  private void printEvent(InputEvent event) {
+    Log.v(TAG, event.toString());
+    ReceivedEvent ev = new ReceivedEvent(event, SystemClock.elapsedRealtimeNanos());
+    mEvents.add(ev);
+    mAdapter.notifyDataSetChanged();
+  }
+
+  private boolean isGameEvent(InputEvent event) {
+    final int source = event.getSource();
+    if ((source & InputDevice.SOURCE_GAMEPAD) != 0
+        || (source & InputDevice.SOURCE_JOYSTICK) != 0) {
+      return true;
+    }
+    return false;
+  }
+}
