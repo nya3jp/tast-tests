188aaecc6a5532d0d140b2ea231fdf0262a31b07
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-04-17 13:44:16 -0700

    tast-tests: Add lacros variant to firmware.ECVerifyVK
    The test needs Lacros enabled to verify that the virtual keyboard
    is present in the tablet mode.
    
    This also includes testability improvement to share the chrome instance
    between two services
    and a fix to update the query of finding the visible VK.
    
    See go/lacros-tast-porting for the porting guidelines.
    
    BUG=b:228326787
    TEST=Start manually servod from the labstation when needed
    TEST=  $ ssh $SERVO_HOSTNAME
    TEST=  localhost ~ # start servod PORT=9999
    TEST=tast run -var=servo=localhost:9999:ssh:<LOCAL_SERVO_SSH_PORT> ${DUT} \
    TEST=  firmware.ECVerifyVK.lacros \
    TEST=  firmware.ECVerifyVK*
    
    Change-Id: Id2d14d0bb02443e5adee699e897f04f3945c3fff

--- src/chromiumos/tast/local/bundles/cros/ui/check_virtual_keyboard_service.go
+++ src/chromiumos/tast/local/bundles/cros/ui/check_virtual_keyboard_service.go
@@ -6,6 +6,7 @@ package ui
 
 import (
 	"context"
+	"path/filepath"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
@@ -13,10 +14,14 @@ import (
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
 	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/chrome/uiauto/touch"
+	"chromiumos/tast/local/common"
 	pb "chromiumos/tast/services/cros/ui"
 	"chromiumos/tast/testing"
 )
@@ -24,41 +29,63 @@ import (
 func init() {
 	testing.AddService(&testing.Service{
 		Register: func(srv *grpc.Server, s *testing.ServiceState) {
-			pb.RegisterCheckVirtualKeyboardServiceServer(srv, &CheckVirtualKeyboardService{})
+			pb.RegisterCheckVirtualKeyboardServiceServer(srv, &CheckVirtualKeyboardService{
+				sharedObject: common.SharedObjectsForServiceSingleton,
+			})
 		},
 	})
 }
 
 // CheckVirtualKeyboardService implements the methods defined in CheckVirtualKeyboardServiceServer.
 type CheckVirtualKeyboardService struct {
-	cr    *chrome.Chrome
-	tconn *chrome.TestConn
+	cr           *chrome.Chrome
+	br           *browser.Browser
+	closeBrowser func(ctx context.Context)
+	tconn        *chrome.TestConn // from ash-chrome
+	sharedObject *common.SharedObjectsForService
 }
 
 // NewChromeLoggedIn Logs into a user session.
-func (cvk *CheckVirtualKeyboardService) NewChromeLoggedIn(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+func (cvk *CheckVirtualKeyboardService) NewChromeLoggedIn(ctx context.Context, req *pb.NewBrowserRequest) (*empty.Empty, error) {
+	cvk.sharedObject.ChromeMutex.Lock()
+	defer cvk.sharedObject.ChromeMutex.Unlock()
 
 	if cvk.cr != nil {
 		return nil, errors.New("Chrome already available")
 	}
 
-	cr, err := chrome.New(ctx)
+	bt := browser.TypeLacros
+	cfg := browserfixt.DefaultLacrosConfig
+	if req.BrowserType == pb.NewBrowserRequest_ASH {
+		bt = browser.TypeAsh
+		cfg = nil
+	}
+	cr, br, closeBrowser, err := browserfixt.SetUpWithNewChrome(ctx, bt, cfg)
 	if err != nil {
 		return nil, err
 	}
 
 	cvk.cr = cr
+	cvk.br = br
+	cvk.closeBrowser = closeBrowser
+	tconn, err := cvk.cr.TestAPIConn(ctx)
+	if err != nil {
+		return nil, err
+	}
+	cvk.tconn = tconn
+	// Store the newly created chrome in the shared object so UtilsService or other services can use it.
+	cvk.sharedObject.Chrome = cr
+
 	return &empty.Empty{}, nil
 }
 
 // OpenChromePage opens a chrome page.
 func (cvk *CheckVirtualKeyboardService) OpenChromePage(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
-
 	if cvk.cr == nil {
 		return nil, errors.New("Chrome not available")
 	}
 	// Open an empty page.
-	conn, err := cvk.cr.NewConn(ctx, "chrome://newtab/")
+	conn, err := cvk.br.NewConn(ctx, "chrome://newtab/")
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to open empty Chrome page")
 	}
@@ -69,13 +96,6 @@ func (cvk *CheckVirtualKeyboardService) OpenChromePage(ctx context.Context, req 
 
 // TouchChromeAddressBar uses touch screen to send a tap on the address bar.
 func (cvk *CheckVirtualKeyboardService) TouchChromeAddressBar(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
-
-	tconn, err := cvk.cr.TestAPIConn(ctx)
-	if err != nil {
-		return nil, err
-	}
-	cvk.tconn = tconn
-
 	addressBarNode := nodewith.Role(role.TextField).Name("Address and search bar")
 	tc, err := touch.New(ctx, cvk.tconn)
 	if err != nil {
@@ -90,13 +110,7 @@ func (cvk *CheckVirtualKeyboardService) TouchChromeAddressBar(ctx context.Contex
 
 // ClickChromeAddressBar sends a left click on the address bar.
 func (cvk *CheckVirtualKeyboardService) ClickChromeAddressBar(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
-	tconn, err := cvk.cr.TestAPIConn(ctx)
-	if err != nil {
-		return nil, err
-	}
-	cvk.tconn = tconn
-
-	uiauto := uiauto.New(tconn)
+	uiauto := uiauto.New(cvk.tconn)
 	addressBarNode := nodewith.Role(role.TextField).Name("Address and search bar")
 	if err := uiauto.LeftClickUntil(
 		addressBarNode,
@@ -109,7 +123,6 @@ func (cvk *CheckVirtualKeyboardService) ClickChromeAddressBar(ctx context.Contex
 
 // CheckVirtualKeyboardIsPresent checks whether the virtual keyboard is present.
 func (cvk *CheckVirtualKeyboardService) CheckVirtualKeyboardIsPresent(ctx context.Context, req *pb.CheckVirtualKeyboardRequest) (*pb.CheckVirtualKeyboardResponse, error) {
-
 	if cvk.cr == nil {
 		return nil, errors.New("Chrome not available")
 	}
@@ -117,13 +130,14 @@ func (cvk *CheckVirtualKeyboardService) CheckVirtualKeyboardIsPresent(ctx contex
 	var exists bool
 	uiauto := uiauto.New(cvk.tconn)
 
-	vkNode := nodewith.Name("Chrome OS Virtual Keyboard").Role(role.Keyboard).Onscreen()
+	vkNode := nodewith.Name("Chrome OS Virtual Keyboard").Role(role.Keyboard).Visible()
 	if err := uiauto.WithTimeout(3 * time.Second).WaitUntilExists(vkNode)(ctx); err != nil {
 		if !req.IsDutTabletMode {
 			return &pb.CheckVirtualKeyboardResponse{
 				IsVirtualKeyboardPresent: exists,
 			}, nil
 		}
+		saveLogs(ctx, cvk, func() bool { return true }, "check")
 		return nil, errors.Wrap(err, "unable to detect Chrome OS virtual keyboard")
 	}
 
@@ -133,10 +147,26 @@ func (cvk *CheckVirtualKeyboardService) CheckVirtualKeyboardIsPresent(ctx contex
 
 // CloseChrome closes a Chrome session and cleans up the resources obtained by NewChrome.
 func (cvk *CheckVirtualKeyboardService) CloseChrome(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	if cvk.closeBrowser != nil {
+		cvk.closeBrowser(ctx)
+		cvk.closeBrowser = nil
+	}
 	if cvk.cr == nil {
 		return nil, errors.New("Chrome not available")
 	}
 	err := cvk.cr.Close(ctx)
 	cvk.cr = nil
+	// Clear the chrome in the shared object so UtilsService or other services can no longer refer to it.
+	cvk.sharedObject.Chrome = nil
 	return &empty.Empty{}, err
+}
+
+func saveLogs(ctx context.Context, cvk *CheckVirtualKeyboardService, hasError func() bool, msg string) error {
+	outDir, ok := testing.ContextOutDir(ctx)
+	if !ok {
+		return errors.New("no output directory")
+	}
+	faillog.DumpUITreeOnError(ctx, filepath.Join(outDir, "CheckVirtualKeyboardService-"+msg), hasError, cvk.tconn)
+	faillog.SaveScreenshotOnError(ctx, cvk.cr, filepath.Join(outDir, "CheckVirtualKeyboardService-"+msg), hasError)
+	return nil
 }
