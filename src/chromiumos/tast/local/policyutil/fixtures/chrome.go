8cbaa243f21a3736659921551bf48b957cf14755
Jerry Liu <pzliu@google.com>
2022-04-18 15:02:45 -0700

    tast-tests: update wallpaper tast tests for personalization hub
    Existing tast tests are running from the wallpaper app instead of the
    personalization hub. This is because the `PersonalizationHub` flag is
    turned off. This commit turn on the flag by default and update the
    wallpaper tast tests to open wallpaper subpage from personalization hub.
    
    BUG=b:225986339
    TEST=tast unit test passed
    
    Cq-Depend: chromium:3595055
    Change-Id: Ied88fa80501e7acb3eeb38fa2e75ccd11e6c94fd

--- src/chromiumos/tast/local/policyutil/fixtures/chrome.go
+++ src/chromiumos/tast/local/policyutil/fixtures/chrome.go
@@ -65,6 +65,21 @@ func init() {
 		Parent:          fixture.FakeDMS,
 	})
 
+	// TODO(b/230901276): Remove fixture after updating policy.UserAvatarImage.
+	testing.AddFixture(&testing.Fixture{
+		Name:     fixture.ChromePolicyLoggedInWithoutPersonalizationHub,
+		Desc:     "Logged into a user session without personalization hub",
+		Contacts: []string{"pzliu@google.com", "assistive-eng@google.com", "chromeos-commercial-remote-management@google.com"},
+		Impl: &policyChromeFixture{
+			extraOpts: []chrome.Option{chrome.EnablePersonalizationHub(false)},
+		},
+		SetUpTimeout:    chrome.ManagedUserLoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		PostTestTimeout: 15 * time.Second,
+		Parent:          fixture.FakeDMS,
+	})
+
 	testing.AddFixture(&testing.Fixture{
 		Name:     fixture.ChromeEnrolledLoggedIn,
 		Desc:     "Logged into a user session with enrollment",
