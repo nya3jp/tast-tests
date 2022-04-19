652261b7f9a6dada1386a9403754ee38ea5276c3
Jeremy Bettis <jbettis@google.com>
2022-04-19 09:57:15 -0600

    tast: Add case to BootupTImes for booting from s5
    To catch performance problems when the power button is pressed in those
    10s before the device goes from S5->G3, add a new test variant that
    watches the EC log until it sees that it has entered the S5 state, and
    then presses the power button as soon as possible.
    
    BUG=b:229643099
    TEST=Ran on eldrid
    
    Change-Id: I358161f83940ae62e4248aa152dc033879feb883

--- src/chromiumos/tast/remote/bundles/cros/platform/bootup_times.go
+++ src/chromiumos/tast/remote/bundles/cros/platform/bootup_times.go
@@ -20,6 +20,7 @@ import (
 	"chromiumos/tast/services/cros/platform"
 	"chromiumos/tast/services/cros/security"
 	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
 )
 
 type bootupTimes struct {
@@ -30,11 +31,13 @@ const (
 	reboot       string = "reboot"
 	lidCloseOpen string = "lidCloseOpen"
 	powerButton  string = "powerButton"
+	bootFromS5   string = "bootFromS5"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         BootupTimes,
+		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Boot performance test after reboot, powerbutton and lid close open",
 		Contacts:     []string{"pathan.jilani@intel.com", "intel-chrome-system-automation-team@intel.com"},
 		Attr:         []string{"group:mainline", "informational"},
@@ -57,6 +60,11 @@ func init() {
 			Name:    "power_button",
 			Val:     bootupTimes{bootType: powerButton},
 			Timeout: 5 * time.Minute,
+		}, {
+			Name:              "from_s5",
+			Val:               bootupTimes{bootType: bootFromS5},
+			Timeout:           5 * time.Minute,
+			ExtraHardwareDeps: hwdep.D(hwdep.ChromeEC()),
 		}},
 	})
 }
@@ -215,7 +223,7 @@ func BootupTimes(ctx context.Context, s *testing.State) {
 				return errors.Wrap(err, "failed to get power state S5 error")
 			}
 			if pwrState != "S5" {
-				return errors.New("System is not in S5")
+				return errors.Errorf("System is not in S5, got: %s", pwrState)
 			}
 			return nil
 		}, &testing.PollOptions{Timeout: 20 * time.Second}); err != nil {
@@ -239,6 +247,84 @@ func BootupTimes(ctx context.Context, s *testing.State) {
 			s.Fatal("Failed to shutdown: ", err)
 		}
 		pwrNormalPress()
+	} else if btType.bootType == bootFromS5 {
+		s.Log("Capturing EC log")
+		if err := pxy.Servo().SetOnOff(ctx, servo.ECUARTCapture, servo.On); err != nil {
+			s.Fatal("Failed to capture EC UART: ", err)
+		}
+		defer func() {
+			if err := pxy.Servo().SetOnOff(ctx, servo.ECUARTCapture, servo.Off); err != nil {
+				s.Fatal("Failed to disable capture EC UART: ", err)
+			}
+		}()
+		var leftoverLines string
+		readyForPowerOn := regexp.MustCompile(`power state 1 = S5`)
+		tooLateToPowerOn := regexp.MustCompile(`power state 0 = G3`)
+		powerOnFinished := regexp.MustCompile(`power state 3 = S0`)
+		powerButtonPressFinished := regexp.MustCompile(`PB task 0 = idle`)
+		didPowerOn := false
+		hitS5 := false
+		donePowerOff := false
+
+		if err := dut.Conn().CommandContext(ctx, "sh", "-c", "rm -rf /var/log/metrics/*").Run(); err != nil {
+			s.Fatal("Failed to remove /var/log/metrics/* files: ", err)
+		}
+		// Use the ec command here instead of power_key, because servo sleeps before the command returns
+		if err := pxy.Servo().RunECCommand(ctx, "powerbtn 8500"); err != nil {
+			s.Fatal("Unable to power off: ", err)
+		}
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			if lines, err := pxy.Servo().GetQuotedString(ctx, servo.ECUARTStream); err != nil {
+				s.Fatal("Failed to read UART: ", err)
+			} else if lines != "" {
+				// It is possible to read partial lines, so save the part after newline for later
+				lines = leftoverLines + lines
+				if crlfIdx := strings.LastIndex(lines, "\r\n"); crlfIdx < 0 {
+					leftoverLines = lines
+					lines = ""
+				} else {
+					leftoverLines = lines[crlfIdx+2:]
+					lines = lines[:crlfIdx+2]
+				}
+
+				for _, l := range strings.Split(lines, "\r\n") {
+					s.Logf("%q", l)
+					if match := readyForPowerOn.FindString(l); match != "" && !didPowerOn {
+						s.Logf("Found S5: %q", l)
+						hitS5 = true
+					}
+					if match := powerButtonPressFinished.FindString(l); match != "" && !didPowerOn {
+						s.Logf("Found power button release: %q", l)
+						donePowerOff = true
+					}
+					// If the long press above is done, and we've seen S5, then do a short press to power on.
+					if hitS5 && donePowerOff && !didPowerOn {
+						s.Log("Pressing power button")
+						if err := pxy.Servo().SetString(ctx, servo.ECUARTCmd, "powerbtn 200"); err != nil {
+							return testing.PollBreak(err)
+						}
+						didPowerOn = true
+					}
+
+					if match := tooLateToPowerOn.FindString(l); match != "" && !didPowerOn {
+						s.Logf("Found G3: %q", l)
+						s.Error("Power state reached G3, power button pressed too late")
+					}
+					if match := powerOnFinished.FindString(l); didPowerOn && match != "" {
+						s.Logf("Found S0: %q", l)
+						return nil
+					}
+				}
+			}
+			return errors.New("Not in S0 yet")
+		}, &testing.PollOptions{Interval: time.Millisecond * 200, Timeout: 60 * time.Second}); err != nil {
+			s.Error("EC output parsing failed: ", err)
+		}
+		waitCtx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+		defer cancel()
+		if err := dut.WaitConnect(waitCtx); err != nil {
+			s.Fatal("Failed to wait connect DUT: ", err)
+		}
 	}
 	// Validating prev sleep state for power modes.
 	if btType.bootType == "reboot" {
