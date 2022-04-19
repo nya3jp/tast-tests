06ae50e466e1e17c3e15cb449539bab7463a2954
Yuki Awano <yawano@google.com>
2022-04-19 15:30:20 -0700

    Use a fake Android app to stabilize test
    - Installing an Android app from Play Store can introduce flakiness.
      Install a fake app to stabilize.
    - The fake app pretends it as a YouTube app as Assistant can open it.
    
    BUG=b:228643716
    TEST=tast run DUT assistant.AndroidAndWeb
    TEST=tast/fast_build.sh -T passes
    
    Change-Id: I9f0e4f3e4a1f1ec549f27326e8db0895e876ac7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3595042
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Tested-by: Yuki Awano <yawano@google.com>

---
+++ android/AssistantAndroidAppTest/src/com/google/android/youtube/MainActivity.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package com.google.android.youtube;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {}
