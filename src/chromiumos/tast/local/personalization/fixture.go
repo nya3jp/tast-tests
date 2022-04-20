68de84b179256b560799cb3e8295612b84502c4a
Thuong Phan <thuongphan@google.com>
2022-04-20 20:36:05 +0000

    tast-tests: set screensaver in personalization hub
    Add tast tests to set Google Photos and Art Gallery screensavers in
    personalization hub. Also remove the old LockScreenIdle test using
    Settings > Personalization to access Screensaver.
    
    BUG=b:225985885
    TEST=tast run <IP> personalization.SetScreensaver.google_photos
         tast run <IP> personalization.SetScreensaver.art_gallery
    
    Change-Id: Idfcbee5f321a020f47f3fdb3ca5c402f0ef3d6da

---
+++ src/chromiumos/tast/local/personalization/fixture.go
@@ -0,0 +1,70 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package personalization
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name: "personalizationDefault",
+		Desc: "Login with Personalization Hub enabled",
+		Contacts: []string{
+			"thuongphan@google.com",
+			"chromeos-sw-engprod@google.com",
+			"assistive-eng@google.com",
+		},
+		Impl: chrome.NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return []chrome.Option{chrome.EnableFeatures("PersonalizationHub")}, nil
+		}),
+		SetUpTimeout:    chrome.LoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+	})
+	testing.AddFixture(&testing.Fixture{
+		Name: "personalizationWithDarkLightMode",
+		Desc: "Login with Personalization Hub and Dark Light mode enabled",
+		Contacts: []string{
+			"thuongphan@google.com",
+			"chromeos-sw-engprod@google.com",
+			"assistive-eng@google.com",
+		},
+		Impl: chrome.NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return []chrome.Option{chrome.EnableFeatures("PersonalizationHub", "DarkLightMode")}, nil
+		}),
+		SetUpTimeout:    chrome.LoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+	})
+	testing.AddFixture(&testing.Fixture{
+		Name: "personalizationWithGaiaLogin",
+		Desc: "Login using Gaia account with Personalization Hub enabled",
+		Contacts: []string{
+			"thuongphan@google.com",
+			"chromeos-sw-engprod@google.com",
+			"assistive-eng@google.com",
+		},
+		Impl: chrome.NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return []chrome.Option{
+				chrome.GAIALogin(chrome.Creds{
+					User: s.RequiredVar("ambient.username"),
+					Pass: s.RequiredVar("ambient.password"),
+				}),
+				chrome.EnableFeatures("PersonalizationHub"),
+			}, nil
+		}),
+		SetUpTimeout:    chrome.GAIALoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		Vars: []string{
+			"ambient.username",
+			"ambient.password",
+		},
+	})
+}
