ba4e7834dd303ae64100900842e8bb3aa2a0b9e2
Navin Ramsaroop <ramsaroop@google.com>
2022-04-20 20:06:44 +0000

    Record runtime and total cycles for a specific test
    Report the duration of a CUJ test in order to identify shorter
    tests and more accurately compare two test results together. Record
    the total cycles, to see how often we are repeating certain tests
    within a single run to reach a minimum target test time length.
    
    BUG=b:229648489
    TEST=Run different CUJ tests locally (i.e. tast run <DUT> ui.MeetCUJ.*) and then check the local results-chart.json in the output to see if the total duration and cycles were both recorded correctly.
    
    Change-Id: If54d2d104ec2092b3d2319c8d843a2fe94ab5759

--- src/chromiumos/tast/local/bundles/cros/ui/cuj/recorder.go
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/recorder.go
@@ -173,6 +173,9 @@ type Recorder struct {
 	// duration is the total running time of the recorder.
 	duration time.Duration
 
+	// Total number of times that the test has been successfully run.
+	testCyclesCount int64
+
 	// Time when recording was started.
 	// Defined only for the running recorder.
 	startedAtTm time.Time
@@ -576,6 +579,8 @@ func (r *Recorder) Run(ctx context.Context, f func(ctx context.Context) error) (
 			testing.ContextLogf(ctx, "Failed to stop recording: %s", err)
 		}
 	}(ctx, runCtx)
+
+	r.testCyclesCount++
 	if err := f(runCtx); err != nil {
 		return err
 	}
@@ -586,7 +591,11 @@ func (r *Recorder) Run(ctx context.Context, f func(ctx context.Context) error) (
 // duration. It may exceed that duration to complete the last call to f.
 func (r *Recorder) RunFor(ctx context.Context, f func(ctx context.Context) error, minimumDuration time.Duration) error {
 	return r.Run(ctx, func(ctx context.Context) error {
+		// Decrement test cycles to prevent double counting, since
+		// Run() increments cycles count independently
+		r.testCyclesCount--
 		for end := time.Now().Add(minimumDuration); time.Now().Before(end); {
+			r.testCyclesCount++
 			if err := f(ctx); err != nil {
 				return err
 			}
@@ -708,6 +717,19 @@ func (r *Recorder) Record(ctx context.Context, pv *perf.Values) error {
 		Unit:      "unitless",
 		Direction: perf.BiggerIsBetter,
 	}, batteryDischargeReport)
+
+	pv.Set(perf.Metric{
+		Name:      "TestMetrics.TestCyclesCount",
+		Unit:      "count",
+		Direction: perf.SmallerIsBetter,
+	}, float64(r.testCyclesCount))
+
+	pv.Set(perf.Metric{
+		Name: "TestMetrics.TotalTestRunTime",
+		Unit: "s",
+		// Longer runtime correlates to better performance data, so bigger is better
+		Direction: perf.BiggerIsBetter,
+	}, r.duration.Seconds())
 
 	displayInfo.Record(pv)
 	r.frameDataTracker.Record(pv)
