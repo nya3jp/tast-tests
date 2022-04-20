d9d6b25cd62194900c28a2ac3da492f454203d06
Yulun Wu <yulunwu@chromium.org>
2022-04-19 18:25:49 -0700

    [TAST] Add tests for productivity launcher answer cards.
    BUG=chromium:1317741
    
    TEST=manually run on nocturne
    
    Change-Id: Ib61cbb5d1936f63748a2b22632b2c80c73c41c8d

--- src/chromiumos/tast/local/chrome/fixture.go
+++ src/chromiumos/tast/local/chrome/fixture.go
@@ -189,6 +189,18 @@ func init() {
 	})
 
 	testing.AddFixture(&testing.Fixture{
+		Name:     "chromeLoggedInWithProductivityLauncher",
+		Desc:     "Logged into a user session with productivity launcher enabled",
+		Contacts: []string{"yulunwu@chromium.org"},
+		Impl: NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]Option, error) {
+			return []Option{EnableFeatures("ProductivityLauncher")}, nil
+		}),
+		SetUpTimeout:    LoginTimeout,
+		ResetTimeout:    ResetTimeout,
+		TearDownTimeout: ResetTimeout,
+	})
+
+	testing.AddFixture(&testing.Fixture{
 		Name:     "chromeLoggedWithGaia",
 		Desc:     "Logged into a session with Gaia user",
 		Contacts: []string{"jinrongwu@google.com"},
