44ca7b0b35d89d241e5b7f5cadd4e54e9d13aa10
Victor Chen <victor.chen@cienet.com>
2022-04-20 15:27:57 +0800

    mta: add test case launcher.UninstallApp
    [MTA fd3b9] Verify app can uninstall from the list of app in launcher.
    
    BUG=b:200987347
    TEST=tast run -var=ui.gaiaPoolDefault='<user>:<pwd>' <DUT> launcher.UninstallApp.*
    
    Change-Id: I53e5daf71dbb5490fd4eb9d68e4c15d48f756fd7

---
+++ src/chromiumos/tast/local/bundles/cros/launcher/uninstall_app.go
@@ -0,0 +1,234 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package launcher
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/playstore"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/cws"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+type appSource int
+
+const (
+	fromCWS appSource = iota
+	fromPlayStore
+)
+
+type appInfo struct {
+	name         string
+	id           string
+	source       appSource
+	isTabletMode bool
+
+	cwsApp cws.App
+
+	arcAppPkgName string
+}
+
+const installationTimeout = cws.InstallationTimeout
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         UninstallApp,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verifies app can be deleted from the list of app in launcher",
+		Contacts: []string{
+			"cienet-development@googlegroups.com",
+			"chromeos-sw-engprod@google.com",
+			"victor.chen@cienet.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		Timeout:      3*time.Minute + installationTimeout,
+		Params: []testing.Param{
+			{
+				Name: "cws",
+				Val: &appInfo{
+					name:         "Wicked Good Unarchiver",
+					id:           "mljpablpddhocfbnokacjggdbmafjnon",
+					source:       fromCWS,
+					isTabletMode: false,
+					cwsApp: cws.App{
+						Name: "Wicked Good Unarchiver",
+						URL:  "https://chrome.google.com/webstore/detail/wicked-good-unarchiver/mljpablpddhocfbnokacjggdbmafjnon",
+					},
+				},
+				Fixture: "chromeLoggedInWithGaiaProductivityLauncher",
+			},
+			{
+				Name: "cws_tablet",
+				Val: &appInfo{
+					name:         "Wicked Good Unarchiver",
+					id:           "mljpablpddhocfbnokacjggdbmafjnon",
+					source:       fromCWS,
+					isTabletMode: true,
+					cwsApp: cws.App{
+						Name: "Wicked Good Unarchiver",
+						URL:  "https://chrome.google.com/webstore/detail/wicked-good-unarchiver/mljpablpddhocfbnokacjggdbmafjnon",
+					},
+				},
+				Fixture: "chromeLoggedInWithGaiaProductivityLauncher",
+			},
+			{
+				Name: "arc",
+				Val: &appInfo{
+					name:          "Calendar",
+					id:            "decaoeahkmjpajbmlbpogjjkjbjokeed",
+					source:        fromPlayStore,
+					isTabletMode:  false,
+					arcAppPkgName: "com.google.android.calendar",
+				},
+				Fixture:           "arcBootedWithPlayStoreProductivityLauncher",
+				ExtraSoftwareDeps: []string{"arc"},
+			},
+			{
+				Name: "arc_tablet",
+				Val: &appInfo{
+					name:          "Calendar",
+					id:            "decaoeahkmjpajbmlbpogjjkjbjokeed",
+					source:        fromPlayStore,
+					isTabletMode:  true,
+					arcAppPkgName: "com.google.android.calendar",
+				},
+				Fixture:           "arcBootedWithPlayStoreProductivityLauncher",
+				ExtraSoftwareDeps: []string{"arc"},
+			},
+		},
+	})
+}
+
+// UninstallApp verifies app can be deleted from the list of app in launcher.
+func UninstallApp(ctx context.Context, s *testing.State) {
+	app := s.Param().(*appInfo)
+
+	var cr *chrome.Chrome
+	var a *arc.ARC
+
+	switch app.source {
+	case fromCWS:
+		cr = s.FixtValue().(chrome.HasChrome).Chrome()
+	case fromPlayStore:
+		cr = s.FixtValue().(*arc.PreData).Chrome
+		a = s.FixtValue().(*arc.PreData).ARC
+	default:
+		s.Fatal("Unexpected app source: ", app.source)
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	if err := installApp(ctx, cr, tconn, a, app); err != nil {
+		s.Fatalf("Failed to install app %s: %v", app.name, err)
+	}
+	// Ensure cleanup the app if test case fail before uninstalling it.
+	defer uninstall(cleanupCtx, cr, tconn, app)
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "uninstall_app_from_launcher")
+
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, app.isTabletMode)
+	if err != nil {
+		s.Fatalf("Failed to ensure tablet mode enabled(%v): %v", app.isTabletMode, err)
+	}
+	defer cleanup(cleanupCtx)
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "uninstall_app_from_launcher_after_tablet_mode_enabled")
+
+	// Wait for the launcher to change to closed state after exiting tablet mode,
+	// to make sure launcher is not still animating to hidden state from tablet mode change to clamshell mode.
+	if !app.isTabletMode {
+		if err := ash.WaitForLauncherState(ctx, tconn, ash.Closed); err != nil {
+			s.Fatal("Failed to wait launcher closed: ", err)
+		}
+	}
+
+	if err := launcher.Open(tconn)(ctx); err != nil {
+		s.Fatal("Failed to open launcher: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	allAppsRoot := nodewith.Ancestor(nodewith.Role(role.Group).NameContaining("All Apps"))
+	appItem := allAppsRoot.Name(app.name).HasClass(launcher.ExpandedItemsClass)
+
+	if err := launcher.UninstallsAppUsingContextMenu(ctx, tconn, appItem); err != nil {
+		s.Fatal("Failed to delete app in launcher: ", err)
+	}
+
+	if err := ui.EnsureGoneFor(nodewith.Name(app.name).HasClass(launcher.ExpandedItemsClass), 10*time.Second)(ctx); err != nil {
+		s.Fatalf("The app %q still remains in launcher: %v", app.name, err)
+	}
+}
+
+// installApp installs the app and returns an error indicating if the app has been successfully installed.
+func installApp(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn, arcSession *arc.ARC, app *appInfo) error {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	testing.ContextLog(ctx, "Installing app")
+
+	switch app.source {
+	case fromCWS:
+		// The cws has already sets the installation timeout, use the ctx directly.
+		if err := cws.InstallApp(ctx, cr, tconn, app.cwsApp); err != nil {
+			return errors.Wrapf(err, "failed to install %s", app.name)
+		}
+	case fromPlayStore:
+		if arcSession == nil {
+			return errors.New("arc session not provided")
+		}
+		device, err := arcSession.NewUIDevice(ctx)
+		if err != nil {
+			return errors.Wrap(err, "failed to create new ARC UI device")
+		}
+		defer device.Close(cleanupCtx)
+
+		if err := playstore.InstallOrUpdateAppAndClose(ctx, tconn, arcSession, device, app.arcAppPkgName, &playstore.Options{TryLimit: -1, InstallationTimeout: installationTimeout}); err != nil {
+			return errors.Wrapf(err, "failed to install %s", app.name)
+		}
+	default:
+		return errors.Errorf("unexpected app source %v", app.source)
+	}
+
+	return nil
+}
+
+// uninstall uninstalls the app via ossettings.
+func uninstall(ctx context.Context, cr *chrome.Chrome, tconn *chrome.TestConn, app *appInfo) error {
+	isInstalled, err := ash.ChromeAppInstalled(ctx, tconn, app.id)
+	if err != nil {
+		return errors.Wrap(err, "failed to check app's existance")
+	}
+
+	if !isInstalled {
+		return nil
+	}
+
+	defer func(ctx context.Context) {
+		settings := ossettings.New(tconn)
+		settings.Close(ctx)
+	}(ctx)
+	testing.ContextLogf(ctx, "Uninstall app: %q", app.name)
+	return ossettings.UninstallApp(ctx, tconn, cr, app.name, app.id)
+}
