7ca60f9d7e09e45dcb439234593efcfac9ee52ee
HenryYang <yanghenry@google.com>
2022-04-21 15:17:22 +1000

    Add shimless rma sample test which requires battery disconnection
    BUG=b:224884338
    TEST=tast run -var=servo=localhost:9999:ssh:8023 localhost:8024
    shimlessrma.DisableHWWPWithBatteryDisconnection (test in local env)
    
    Change-Id: I49ff499624bd02a252ff2d9bdfc384e94ca6ea36

--- src/chromiumos/tast/remote/bundles/cros/shimlessrma/disable_hwwp_with_battery_disconnection.go
+++ src/chromiumos/tast/remote/bundles/cros/shimlessrma/disable_hwwp_with_battery_disconnection.go
@@ -7,18 +7,25 @@ package shimlessrma
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 
+	"chromiumos/tast/common/servo"
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/dut"
+	"chromiumos/tast/remote/bundles/cros/shimlessrma/web"
+	"chromiumos/tast/remote/firmware"
 	"chromiumos/tast/remote/firmware/fixture"
 	"chromiumos/tast/rpc"
 	pb "chromiumos/tast/services/cros/shimlessrma"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
+
+const timeInSecondToWaitForReboot = 10
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -38,10 +45,10 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		HardwareDeps: hwdep.D(hwdep.ChromeEC()),
 		ServiceDeps:  []string{"tast.cros.browser.ChromeService", "tast.cros.shimlessrma.AppService"},
-		Fixture:      fixture.DevMode,
+		Fixture:      fixture.DevModeGBB,
 		// TODO: Figure out how to reboot to Normal Mode
 		// Fixture: fixture.NormalMode,
-		Timeout: 5 * time.Minute,
+		Timeout: 10 * time.Minute,
 	})
 }
 
@@ -57,6 +64,70 @@ func DisableHWWPWithBatteryDisconnection(ctx context.Context, s *testing.State) 
 
 	d := h.DUT
 
+	cl, client := createShimlessClient(ctx, s, d)
+	defer cl.Close(cleanupCtx)
+	defer client.CloseShimlessRMA(cleanupCtx, &empty.Empty{})
+
+	// Set WP as enabled as starting point.
+	changeWriteProtectStatus(ctx, s, h, servo.FWWPStateOn)
+
+	web.WelcomePageOperation(ctx, s, client)
+	web.ComponentsPageOperation(ctx, s, client)
+	web.OwnerPageOperation(ctx, s, client)
+	web.WipeDevicePageOperation(ctx, s, client)
+	web.WriteProtectPageOperation(ctx, s, client)
+
+	// Disables WP by CCD.
+	changeWriteProtectStatus(ctx, s, h, servo.FWWPStateOff)
+
+	// Wait for reboot completed.
+	testing.Sleep(ctx, timeInSecondToWaitForReboot*time.Second)
+
+	cl, client = createShimlessClient(ctx, s, d)
+	defer cl.Close(cleanupCtx)
+	defer client.CloseShimlessRMA(cleanupCtx, &empty.Empty{})
+
+	web.WriteProtectDisabledPageOperation(ctx, s, client)
+
+	// Bypass firmware installation for now.
+	bypassFirmwareInstallation(ctx, s, d)
+
+	// Wait for reboot completed.
+	testing.Sleep(ctx, timeInSecondToWaitForReboot*time.Second)
+
+	cl, client = createShimlessClient(ctx, s, d)
+	defer cl.Close(cleanupCtx)
+	defer client.CloseShimlessRMA(cleanupCtx, &empty.Empty{})
+
+	web.FirmwareInstallationPageOperation(ctx, s, client)
+	web.DeviceInformationPageOperation(ctx, s, client)
+	web.DeviceProvisionPageOperation(ctx, s, client)
+
+	// Another reboot after provisioning
+	testing.Sleep(ctx, timeInSecondToWaitForReboot*time.Second)
+
+	cl, client = createShimlessClient(ctx, s, d)
+	defer cl.Close(cleanupCtx)
+	defer client.CloseShimlessRMA(cleanupCtx, &empty.Empty{})
+
+	web.CalibratePageOperation(ctx, s, client)
+
+	// Enables WP by CCD.
+	changeWriteProtectStatus(ctx, s, h, servo.FWWPStateOn)
+	web.WriteProtectEnabledPageOperation(ctx, s, client)
+
+	// Wait for reboot completed.
+	testing.Sleep(ctx, timeInSecondToWaitForReboot*time.Second)
+
+	cl, client = createShimlessClient(ctx, s, d)
+	defer cl.Close(cleanupCtx)
+	defer client.CloseShimlessRMA(cleanupCtx, &empty.Empty{})
+
+	web.FinalizingRepairPageOperation(ctx, s, client)
+	web.RepairCompeletedPageOperation(ctx, s, client)
+}
+
+func createShimlessClient(ctx context.Context, s *testing.State, d *dut.DUT) (*rpc.Client, pb.AppServiceClient) {
 	if err := d.WaitConnect(ctx); err != nil {
 		s.Fatal("Failed connect to DUT: ", err)
 	}
@@ -66,17 +137,38 @@ func DisableHWWPWithBatteryDisconnection(ctx context.Context, s *testing.State) 
 	if err != nil {
 		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
 	}
-	defer cl.Close(cleanupCtx)
 	s.Log("Setup rpc successfully")
 
 	request := &pb.NewShimlessRMARequest{
 		ManifestKey: s.RequiredVar("ui.signinProfileTestExtensionManifestKey"),
+		Reconnect:   false,
 	}
 	client := pb.NewAppServiceClient(cl.Conn)
 	if _, err := client.NewShimlessRMA(ctx, request, grpc.WaitForReady(true)); err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
-	defer client.CloseShimlessRMA(cleanupCtx, &empty.Empty{})
 
-	client.TestWelcomeAndCancel(ctx, &empty.Empty{})
+	return cl, client
+}
+
+func changeWriteProtectStatus(ctx context.Context, s *testing.State, h *firmware.Helper, status servo.FWWPStateValue) {
+	if err := h.Servo.SetFWWPState(ctx, status); err != nil {
+		s.Fatal("Failed to disable write protect: ", err)
+	}
+}
+
+func bypassFirmwareInstallation(ctx context.Context, s *testing.State, d *dut.DUT) {
+	// Add "firmware_updated":true to state file.
+	_, err := d.Conn().CommandContext(ctx, "sed", "-i", fmt.Sprintf("s/%s/%s/g", "}$", ",\"firmware_updated\":true}"), "/mnt/stateful_partition/unencrypted/rma-data/state").Output()
+	if err != nil {
+		s.Fatal("Failed to update state file: ", err)
+	}
+	// Wait for state file updated.
+	s.Log("Sleep for 3 seconds after bypass firmware installation")
+	testing.Sleep(ctx, timeInSecondToWaitForReboot*time.Second)
+
+	s.Log("Restart dut after firmware installed")
+	if err := d.Reboot(ctx); err != nil {
+		s.Fatal("Failed to reboot DUT: ", err)
+	}
 }
