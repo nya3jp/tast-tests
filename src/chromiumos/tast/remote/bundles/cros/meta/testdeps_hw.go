56441ccafe393617d75d9f7f604f62f5fdaa6e66
Terry Yan <yichiyan@google.com>
2022-04-20 23:49:59 -0700

    Create new Tests to test new features
    Create new Tests for Tast Hardware/Software Dependencies Support For Companion DUTs
    testdeps_sw.go
    primary: octopus_sparky360, 	companion:hana-hana, 		result: pass
    primary: non-octopus board, 	companion:hana-hana, 		result: skip
    primary: octopus_sparky360, 	companion:non-hana board, 	result: skip
    primary: non-octopus board, 	companion:non-hana board, 	result: skip
    
    testdeps_hw.go
    primary: octopus_sparky360, 	companion:hana-hana, 		result: pass
    primary: non-sparky360 model, 	companion:hana-hana, 		result: skip
    primary: octopus_sparky360, 	companion:non-hana model, 	result: skip
    primary: non-sparky360 model, 	companion:non-hana model, 	result: skip
    
    testparam_dep.go
    name: swdep
    primary: octopus_sparky360, 	companion:hana-hana, 		result: pass
    primary: non-octopus board, 	companion:hana-hana, 		result: skip
    primary: octopus_sparky360, 	companion:non-hana board, 	result: skip
    primary: non-octopus board, 	companion:non-hana board, 	result: skip
    
    testparam_dep.go
    name: hwdep
    primary: octopus_sparky360, 	companion:hana-hana, 		result: pass
    primary: non-sparky360 model, 	companion:hana-hana, 		result: skip
    primary: octopus_sparky360, 	companion:non-hana model, 	result: skip
    primary: non-sparky360 model, 	companion:non-hana model, 	result: skip
    
    BUG=b:207672316
    TEST=tast unit test
    TEST=manually tested by changed DUT model and board
    
    Cq-Depend: chromium:3564270
    Change-Id: I2e635f3e08237bc3ab7b098f12a53b37f199c9ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3598185
    Commit-Queue: Terry Yan <yichiyan@google.com>
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Tested-by: Terry Yan <yichiyan@google.com>
    Reviewed-by: Terry Yan <yichiyan@google.com>

---
+++ src/chromiumos/tast/remote/bundles/cros/meta/testdeps_hw.go
@@ -0,0 +1,43 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package meta
+
+import (
+	"context"
+
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: TestdepsHw,
+		HardwareDepsForAll: map[string]hwdep.Deps{
+			"":    hwdep.D(hwdep.Model("sparky360")),
+			"cd1": hwdep.D(hwdep.Model("hana")),
+		},
+		Desc:     "Ensure DUTs hardware dependencies with Primary:octopus-sparky360 companion:hana-hana will pass, either or both changed will skip",
+		Contacts: []string{"seewaifu@chromium.org", "yichiyan@google.com", "tast-owners@google.com"},
+	})
+}
+
+func TestdepsHw(ctx context.Context, s *testing.State) {
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	companionDUT := s.CompanionDUT("cd1")
+	if companionDUT == nil {
+		s.Fatal("Failed to get companion DUT cd1")
+	}
+	companionCl, err := rpc.Dial(ctx, companionDUT, s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the companion DUT: ", err)
+	}
+	defer companionCl.Close(ctx)
+}
