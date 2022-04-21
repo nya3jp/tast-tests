ac0c5b39a3d81d04631175bdd10ce224008e22b0
HenryYang <yanghenry@google.com>
2022-04-21 14:52:40 +1000

    Implement new shimless rma gRPC interface
    BUG=b:224884338
    TEST=NONE(The test will be added in subsenquent CL.)
    Cq-Depend: chromium:3596350, chrome-internal:3596349
    
    Change-Id: Icd3faed4b34ab9c0640a2614d11dd5e48118f38a

--- src/chromiumos/tast/local/bundles/cros/shimlessrma/app_service.go
+++ src/chromiumos/tast/local/bundles/cros/shimlessrma/app_service.go
@@ -6,6 +6,7 @@ package shimlessrma
 
 import (
 	"context"
+	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
@@ -34,15 +35,20 @@ type AppService struct {
 }
 
 // NewShimlessRMA creates ShimlessRMA.
-func (shimlessRMA *AppService) NewShimlessRMA(ctx context.Context, req *pb.NewShimlessRMARequest) (*empty.Empty, error) {
-	// Make sure rmad is not currently running.
-	if err := testexec.CommandContext(ctx, "stop", "rmad").Run(); err != nil {
-		return nil, errors.Wrap(err, "failed to stop rmad")
-	}
+func (shimlessRMA *AppService) NewShimlessRMA(ctx context.Context,
+	req *pb.NewShimlessRMARequest) (*empty.Empty, error) {
 
-	// Create a valid empty rmad state file.
-	if err := shimlessrmaapp.CreateEmptyStateFile(); err != nil {
-		return nil, errors.Wrap(err, "failed to create rmad state file")
+	// If Reconnect is true, it means UI restarting during Shimless RMA testing.
+	// Then, we don't need to stop rmad or create empty state file.
+	if !req.Reconnect {
+		// Make sure rmad is not currently running.
+		// Ignore the error since ramd may not run at all.
+		testexec.CommandContext(ctx, "stop", "rmad").Run()
+
+		// Create a valid empty rmad state file.
+		if err := shimlessrmaapp.CreateEmptyStateFile(); err != nil {
+			return nil, errors.Wrap(err, "failed to create rmad state file")
+		}
 	}
 
 	cr, err := chrome.New(ctx, chrome.EnableFeatures("ShimlessRMAFlow"),
@@ -70,20 +76,24 @@ func (shimlessRMA *AppService) NewShimlessRMA(ctx context.Context, req *pb.NewSh
 }
 
 // CloseShimlessRMA closes and releases the resources obtained by New.
-func (shimlessRMA *AppService) CloseShimlessRMA(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
-	if err := shimlessrmaapp.RemoveStateFile(); err != nil {
-		return nil, errors.Wrap(err, "failed to remote state file")
-	}
+func (shimlessRMA *AppService) CloseShimlessRMA(ctx context.Context,
+	req *empty.Empty) (*empty.Empty, error) {
 
-	if err := shimlessRMA.cr.Close(ctx); err != nil {
-		return nil, errors.Wrap(err, "failed to close Shimless RMA app")
-	}
+	// Ignore failure handle in this method,
+	// since we want to execute all of these anyway.
+	shimlessRMA.app.WaitForStateFileDeleted()(ctx)
+
+	testexec.CommandContext(ctx, "stop", "rmad").Run()
+
+	shimlessrmaapp.RemoveStateFile()
+
+	shimlessRMA.cr.Close(ctx)
 
 	return &empty.Empty{}, nil
 }
 
 // TestWelcomeAndCancel tests welcome page is loaded and then cancel it.
-func (shimlessRMA *AppService) TestWelcomeAndCancel(ctx context.Context, rreq *empty.Empty) (*empty.Empty, error) {
+func (shimlessRMA *AppService) TestWelcomeAndCancel(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
 	// TODO: refactor testing steps to separate method later.
 	if err := shimlessRMA.app.WaitForWelcomePageToLoad()(ctx); err != nil {
 		return nil, errors.Wrap(err, "failed to load welcome page")
@@ -93,12 +103,46 @@ func (shimlessRMA *AppService) TestWelcomeAndCancel(ctx context.Context, rreq *e
 		return nil, errors.Wrap(err, "failed to click cancel button")
 	}
 
-	if err := shimlessRMA.app.WaitForStateFileDeleted()(ctx); err != nil {
-		return nil, errors.Wrap(err, "failed to cancel RMA, state file not deleted")
+	return &empty.Empty{}, nil
+}
+
+// WaitForPageToLoad waits the page with title to be loaded.
+func (shimlessRMA *AppService) WaitForPageToLoad(ctx context.Context,
+	req *pb.WaitForPageToLoadRequest) (*empty.Empty, error) {
+	waitTimeout := time.Duration(req.DurationInSecond) * time.Second
+	if err := shimlessRMA.app.WaitForPageToLoad(req.Title, waitTimeout)(ctx); err != nil {
+		return nil, errors.Wrapf(err, "failed to load page: %s", req.Title)
 	}
 
-	if err := testexec.CommandContext(ctx, "stop", "rmad").Run(); err != nil {
-		return nil, errors.Wrap(err, "failed to stop RMA")
+	return &empty.Empty{}, nil
+}
+
+// LeftClickButton left clicks the button with label.
+func (shimlessRMA *AppService) LeftClickButton(ctx context.Context,
+	req *pb.LeftClickButtonRequest) (*empty.Empty, error) {
+	if err := shimlessRMA.app.LeftClickButton(req.Label)(ctx); err != nil {
+		return nil, errors.Wrapf(err, "failed to left click button: %s", req.Label)
+	}
+
+	return &empty.Empty{}, nil
+}
+
+// WaitUntilButtonEnabled waits for button with label eanbled.
+func (shimlessRMA *AppService) WaitUntilButtonEnabled(ctx context.Context,
+	req *pb.WaitUntilButtonEnabledRequest) (*empty.Empty, error) {
+	waitTimeout := time.Duration(req.DurationInSecond) * time.Second
+	if err := shimlessRMA.app.WaitUntilButtonEnabled(req.Label, waitTimeout)(ctx); err != nil {
+		return nil, errors.Wrapf(err, "failed to left click button: %s", req.Label)
+	}
+
+	return &empty.Empty{}, nil
+}
+
+// LeftClickRadioButton clicks radio button.
+func (shimlessRMA *AppService) LeftClickRadioButton(ctx context.Context,
+	req *pb.LeftClickRadioButtonRequest) (*empty.Empty, error) {
+	if err := shimlessRMA.app.LeftClickRadioButton(req.Label)(ctx); err != nil {
+		return nil, errors.Wrapf(err, "failed to left click radio button: %s", req.Label)
 	}
 
 	return &empty.Empty{}, nil
