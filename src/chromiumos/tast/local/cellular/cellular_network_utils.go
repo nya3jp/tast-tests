b43a58ef5cf2c2cbd3ff9610618302861e87638f
Madhav <madhavadas@google.com>
2022-04-22 08:06:36 +0000

    tast-tests: Enable ARC/Crostini connectivity tests for cellular
    Verify internet access from ARC and crostini via cellular interface.
    
    BUG=b:229913594
    TEST=tast --verbose run dut cellular.CrostiniCellularNetworkConnectivit
         tast --verbose run dut cellular.ArcCellularNetworkConnectivit
    
    Change-Id: Ia8a9e866ade2c2e285f2b91c901f4a84b0883f8b

---
+++ src/chromiumos/tast/local/cellular/cellular_network_utils.go
@@ -0,0 +1,74 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+const (
+	pingTimeout      = 10 * time.Second
+	googleDotComIPv6 = "ipv6.google.com"
+	googleDotComIPv4 = "ipv4.google.com"
+)
+
+// VerifyCrostiniIPConnectivity verifies the ip connectivity from crostini via cellular interface.
+func VerifyCrostiniIPConnectivity(ctx context.Context, cmd func(context.Context, ...string) *testexec.Cmd, ipType string) error {
+	if ipType == "ipv4" || ipType == "ipv4v6" {
+		testing.ContextLog(ctx, "Verify IPv4 connectivity")
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			if err := cmd(ctx, "/bin/ping", "-c1", "-w1", googleDotComIPv4).Run(); err != nil {
+				return errors.Wrap(err, "failed ipv6 ping test in Crostini")
+			}
+			return nil
+		}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+			return errors.Wrap(err, "failed ipv6 ping test in Crostini")
+		}
+	}
+	if ipType == "ipv6" || ipType == "ipv4v6" {
+		testing.ContextLog(ctx, "Verify IPv6 connectivity")
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			if err := cmd(ctx, "/bin/ping6", "-c1", "-w1", googleDotComIPv6).Run(); err != nil {
+				return errors.Wrap(err, "failed ipv6 ping test in Crostini")
+			}
+			return nil
+		}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+			return errors.Wrap(err, "failed ipv6 ping test in Crostini")
+		}
+	}
+	return nil
+}
+
+// VerifyArcIPConnectivity verifies the ip connectivity from ARC via cellular interface.
+func VerifyArcIPConnectivity(ctx context.Context, cmd func(context.Context, string, ...string) *testexec.Cmd, ipType string) error {
+	if ipType == "ipv4" || ipType == "ipv4v6" {
+		testing.ContextLog(ctx, "Verify IPv4 connectivity")
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			if err := cmd(ctx, "/system/bin/ping", "-c1", "-w1", googleDotComIPv4).Run(); err != nil {
+				return errors.Wrap(err, "failed ipv6 ping test in Crostini")
+			}
+			return nil
+		}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+			return errors.Wrap(err, "failed ipv6 ping test in Crostini")
+		}
+	}
+	if ipType == "ipv6" || ipType == "ipv4v6" {
+		testing.ContextLog(ctx, "Verify IPv6 connectivity")
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			if err := cmd(ctx, "/system/bin/ping6", "-c1", "-w1", googleDotComIPv6).Run(); err != nil {
+				return errors.Wrap(err, "failed ipv6 ping test in Crostini")
+			}
+			return nil
+		}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+			return errors.Wrap(err, "failed ipv6 ping test in Crostini")
+		}
+	}
+	return nil
+}
