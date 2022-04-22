7ffd93f6137d7804bbc3d6ae3d54e065cfec9022
Toby Huang <tobyhuang@chromium.org>
2022-04-22 07:03:39 +0000

    Projector: Expand SharedScreencast Tast test
    This CL clicks the share button, copies the
    shareable link to clipboard, and verifies the clipboard data.
    
    It also tests translating to French, searching the transcript,
    navigating the transcript, skipping backwards and forwards, and
    playing/pausing.
    
    BUG=b:229633372,b:229633530,b:229633434,b:229633442,b:229633874,b:229633652,b:229633880,b:229632587
    TEST=./repeat-test.sh -r 20 -- -buildbundle=cros localhost:2222 projector.*
    
    Change-Id: Ib0b954653a88a285b4acbdfdee0059e70c000bf4

---
+++ src/chromiumos/tast/local/chrome/projector/projector.go
@@ -0,0 +1,33 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package projector is used for writing Projector tests.
+package projector
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+)
+
+// RefreshApp returns an action that refreshes the Screencast app by right-clicking.
+func RefreshApp(ctx context.Context, tconn *chrome.TestConn) uiauto.Action {
+	ui := uiauto.New(tconn)
+	appWindow := nodewith.Name("Screencast").Role(role.Application)
+	reload := nodewith.Name("Reload Ctrl+R").Role(role.MenuItem)
+
+	return func(ctx context.Context) error {
+		if err := uiauto.Combine("refresh app throught right-click context menu",
+			ui.RightClickUntil(appWindow, ui.Exists(reload)),
+			ui.LeftClick(reload),
+		)(ctx); err != nil {
+			return errors.Wrap(err, "failed to refresh app")
+		}
+		return nil
+	}
+}
