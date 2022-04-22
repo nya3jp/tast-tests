e56837eadcb6c7c85895258ad45f11c6b121a5ad
Madhav <madhavadas@google.com>
2022-04-22 08:06:36 +0000

    tast-tests: Enable Host/ARC/Crostini connectivity tests for cellular
    Verify internet access from Host, ARC and crostini via cellular interface.
    
    BUG=b:229913594
    TEST=tast --verbose run dut cellular.CrostiniCellularNetworkConnectivity
         tast --verbose run dut cellular.ArcCellularNetworkConnectivity
          tast --verbose run dut cellular.HostCellularNetworkConnectivit
    
    Change-Id: Ia8a9e866ade2c2e285f2b91c901f4a84b0883f8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3602040
    Tested-by: Madhav <madhavadas@google.com>
    Commit-Queue: Madhav <madhavadas@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/cellular/arc_cellular_network_connectivity.go
@@ -0,0 +1,76 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/cellular"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/modemmanager"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ArcCellularNetworkConnectivity,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verifies that Arc has network connectivity via cellular interface",
+		Contacts:     []string{"madhavadas@google.com", "chromeos-cellular-team@google.com"},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_active"},
+		SoftwareDeps: []string{"chrome", "vm_host"},
+		Fixture:      "cellular",
+		Timeout:      4 * time.Minute,
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+	})
+}
+
+func ArcCellularNetworkConnectivity(ctx context.Context, s *testing.State) {
+	if _, err := modemmanager.NewModemWithSim(ctx); err != nil {
+		s.Fatal("Could not find MM dbus object with a valid sim: ", err)
+	}
+
+	helper, err := cellular.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create cellular.Helper: ", err)
+	}
+
+	// StartARC
+	func() {
+		cr, err := chrome.New(ctx, chrome.ARCEnabled())
+		if err != nil {
+			s.Fatal("Failed to connect to Chrome: ", err)
+		}
+		defer cr.Close(ctx)
+		a, err := arc.New(ctx, s.OutDir())
+		if err != nil {
+			s.Fatal("Failed to start ARC: ", err)
+		}
+		defer a.Close(ctx)
+	}()
+
+	ipType, err := helper.GetCurrentIPType(ctx)
+	if err != nil {
+		s.Fatal("Failed to read APN info: ", err)
+	}
+
+	verifyIPConnectivity := func(ctx context.Context) error {
+		if err := cellular.VerifyIPConnectivity(ctx, arc.BootstrapCommand, ipType, "/system/bin"); err != nil {
+			return errors.Wrap(err, "failed connectivity test")
+		}
+		return nil
+	}
+	if err := helper.RunTestOnCellularInterface(ctx, verifyIPConnectivity); err != nil {
+		s.Fatal("Failed to run test on cellular interface: ", err)
+	}
+}
---
+++ src/chromiumos/tast/local/bundles/cros/cellular/host_cellular_network_connectivity.go
@@ -0,0 +1,55 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/cellular"
+	"chromiumos/tast/local/modemmanager"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HostCellularNetworkConnectivity,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verifies that host has network connectivity via cellular interface",
+		Contacts:     []string{"madhavadas@google.com", "chromeos-cellular-team@google.com"},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_active"},
+		Timeout:      1 * time.Minute,
+	})
+}
+
+func HostCellularNetworkConnectivity(ctx context.Context, s *testing.State) {
+	if _, err := modemmanager.NewModemWithSim(ctx); err != nil {
+		s.Fatal("Could not find MM dbus object with a valid sim: ", err)
+	}
+
+	helper, err := cellular.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create cellular.Helper: ", err)
+	}
+
+	ipType, err := helper.GetCurrentIPType(ctx)
+	if err != nil {
+		s.Fatal("Failed to read APN info: ", err)
+	}
+
+	s.Log("ip-type:", ipType)
+	verifyHostIPConnectivity := func(ctx context.Context) error {
+		if err := cellular.VerifyIPConnectivity(ctx, testexec.CommandContext, ipType, "/bin"); err != nil {
+			return errors.Wrap(err, "failed connectivity test")
+		}
+		return nil
+	}
+
+	if err := helper.RunTestOnCellularInterface(ctx, verifyHostIPConnectivity); err != nil {
+		s.Fatal("Failed to run test on cellular interface: ", err)
+	}
+}
