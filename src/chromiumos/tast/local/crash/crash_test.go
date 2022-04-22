aa9c0289e40a2c3f4098e13a570622cba34e5ace
Jun Yu <junyuu@chromium.org>
2022-04-22 18:40:25 +0000

    tast-tests: Fix wifi.CheckIntelFWDump failure caused by crash_reporter
    The cleanup code as well as unit test are added to tast/local/crash.
    Clear the filter by writing "0" to /sys/class/devcoredump/devcd%d/data
    The existence of these files indicates that devcoredump was invoked
    shortly before. devcoredump is invoked by crash_reporter during crash
    collection and testing, by which a file filter is created to ignore
    Intel WiFi FW dumps.
    
    The code with same cleanup logic is added to crash.KernelIwlwifiError
    and wifi.CheckIntelFWDump to ensure: 1. the former test should do its
    own cleanup completely. 2. In case of footprints impacting next test
    still exists, the wifi.CheckIntelFWDump will do the cleanup by itself.
    The running time will increase 30 seconds for crash.KernelIwlwifiError
    The extra waiting time is observed for crash_reporter to safely exit.
    
    BUG=b:228462848, b:226981380
    TEST=../tast/fast_build.sh -T
    TEST=Ran wifi.CheckIntelFWDump right after crash.KernelIwlwifiError \
         for 10 times. The results are consistent wifi.CheckIntelFWDump \
         run individually.
    TEST=Ran crash.KernelIwlwifiError individually and passed
    TEST=wifi_matfunc passes on volteer
    
    Change-Id: I555bd6ebb7c01fd5abdbe87644fb03be1ab18206

--- src/chromiumos/tast/local/crash/crash_test.go
+++ src/chromiumos/tast/local/crash/crash_test.go
@@ -5,6 +5,7 @@
 package crash
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io/ioutil"
@@ -555,5 +556,40 @@ func TestDeleteCoreDumps(t *gotesting.T) {
 	want := []string{"a.core", "a.txt", "b.dmp", "b.jpg", "c.core", "c.dmp"}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Error("Files mismatch after successful deleteCoreDumps (-got +want):\n", diff)
+	}
+}
+
+func TestCleanupDevcoredump(t *gotesting.T) {
+	td := testutil.TempDir(t)
+	defer os.RemoveAll(td)
+
+	dirCD := filepath.Join(td, KernelDevCDDir)
+	dirCD1 := filepath.Join(dirCD, "devcd1")
+	if err := os.MkdirAll(dirCD1, 0777); err != nil {
+		t.Fatal("Failed to create dir: ", err)
+	}
+
+	if err := os.WriteFile(filepath.Join(dirCD1, "data"), nil, 0666); err != nil {
+		t.Fatal("Failed to touch file: ", err)
+	}
+
+	if _, err := os.Stat(filepath.Join(dirCD1, "data")); os.IsNotExist(err) {
+		t.Fatal("Can not find data")
+	}
+
+	// cleanDevcoredump doesn't delete the devcoredump instance, instead writes '0' it.
+	// the deletion is handled by a concurrent kernel thread
+	if err := cleanupDevcoredump(context.Background(), 100*time.Millisecond, dirCD, kernelDevCDName); err != nil {
+		t.Fatalf("Couldn't cleanup devcoredump data file %s", dirCD1)
+	}
+
+	// check if a '0' was written to the devcoredump instance
+	data, err := os.ReadFile(filepath.Join(dirCD1, "data"))
+	if err != nil {
+		t.Fatalf("Devcoredump data file doesn't exist in %s/", dirCD1)
+	}
+	// nothing but a single '0' is expected
+	if bytes.Compare(data, []byte("0")) != 0 {
+		t.Fatalf("cleanupDevcoredump failed to write '0' to %s/data", dirCD1)
 	}
 }
