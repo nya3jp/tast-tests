ee53f31aa28b04a2f20997e178146df161807458
Toby Huang <tobyhuang@chromium.org>
2022-04-22 07:03:39 +0000

    Projector: Add Tast test for opening app and launching creation flow
    This CL adds a Tast test that opens the Projector app and goes through
    the creation flow. It clicks the new screencast button, opens the
    annotator, changes the marker color to blue, draws a dot, clears the
    canvas, and stops the recording. It also dismisses the tutorial videos
    and deletes the screencast item to clean up for the next test.
    
    This CL also adds a fixture so future Projector Tast tests can share
    common setup code.
    
    BUG=b:229631476,b:229632058,b:229631504,b:229634051,b:229634049,b:229633784,b:229634235,b:229631680,b:229634287,b:229632276,b:229634227,b:229632608
    TEST=./repeat-test.sh -r 20 -- -buildbundle=cros localhost:2222 projector.CreationFlow
    
    Change-Id: I8cb71f8ca7ce8a2beb6f8f02fdd2d55c65b3d97f

---
+++ src/chromiumos/tast/local/bundles/cros/projector/creation_flow.go
@@ -0,0 +1,119 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package projector is used for writing Projector tests.
+package projector
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome/projector"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CreationFlow,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Launches the Projector app and goes through the new screencast creation flow",
+		Contacts:     []string{"tobyhuang@chromium.org", "cros-projector@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome", "ondevice_speech"},
+		Timeout:      10 * time.Minute,
+		Fixture:      "projectorLogin",
+	})
+}
+
+func CreationFlow(ctx context.Context, s *testing.State) {
+	ctxForCleanUp := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	tconn := s.FixtValue().(*projector.FixtData).TestConn
+
+	defer faillog.DumpUITreeOnError(ctxForCleanUp, s.OutDir(), s.HasError, tconn)
+
+	ui := uiauto.New(tconn).WithTimeout(2 * time.Minute)
+
+	appWindow := nodewith.Name("Screencast").Role(role.Application)
+	reload := nodewith.Name("Reload Ctrl+R").Role(role.MenuItem)
+	maximizeButton := nodewith.Name("Maximize").Role(role.Button)
+	newScreencastButton := nodewith.Name("New screencast").Role(role.Button)
+	clickAnywhereToRecord := nodewith.Name("Click anywhere to record full screen").Role(role.StaticText)
+	annotatorTrayButton := nodewith.NameStartingWith("Toggle marker.").Role(role.Button)
+	inkCanvas := nodewith.ClassName("ink-engine").Role(role.Canvas)
+	blueMarkerButton := nodewith.Name("Blue").Role(role.Button)
+	stopRecordingButton := nodewith.Name("Stop screen recording").Role(role.Button)
+	tutorialsText := nodewith.Name("Getting started").Role(role.StaticText)
+	closeTutorialsButton := nodewith.Name("Close tutorials").Role(role.Button)
+	screencastItemMoreOptionsButton := nodewith.Name("More options").Role(role.PopUpButton).Nth(1)
+	deleteMenuItem := nodewith.Name("Delete").Role(role.MenuItem)
+	deleteButton := nodewith.Name("Delete").Role(role.Button)
+
+	if err := launcher.LaunchAndWaitForAppOpen(tconn, apps.Projector)(ctx); err != nil {
+		s.Fatal("Failed to open Projector app: ", err)
+	}
+
+	// UI action for refreshing the app until the element we're looking for exists.
+	refreshApp := func(ctx context.Context) error {
+		if err := uiauto.Combine("refresh app",
+			ui.RightClickUntil(appWindow, ui.Exists(reload)),
+			ui.LeftClick(reload),
+		)(ctx); err != nil {
+			return errors.Wrap(err, "failed to refresh app")
+		}
+		return nil
+	}
+
+	s.Log("Launching the new screencast creation flow")
+	if err := uiauto.Combine("launch the new screencast creation flow",
+		// Maximize the app to mitigate screen size
+		// differences on different devices.
+		ui.LeftClick(maximizeButton),
+		ui.WithInterval(5*time.Second).RetryUntil(refreshApp, ui.Exists(newScreencastButton)),
+		ui.LeftClickUntil(newScreencastButton, ui.Gone(newScreencastButton)),
+		ui.WaitUntilExists(clickAnywhereToRecord),
+		ui.LeftClickUntil(clickAnywhereToRecord, ui.Gone(clickAnywhereToRecord)),
+		ui.WaitUntilExists(annotatorTrayButton),
+		// Enable the annotator.
+		ui.WithInterval(time.Second).LeftClickUntil(annotatorTrayButton, ui.Exists(inkCanvas)),
+		// Open the color picker.
+		ui.RightClickUntil(annotatorTrayButton, ui.Exists(blueMarkerButton)),
+		// Change marker color to blue.
+		ui.LeftClickUntil(blueMarkerButton, ui.Gone(blueMarkerButton)),
+		// Draw a blue dot.
+		// TODO(b/229634049): Verify the drawing rendered.
+		ui.LeftClick(inkCanvas),
+		// Clear the canvas.
+		// TODO(b/229634049): Verify the canvas cleared.
+		ui.RightClick(inkCanvas),
+		// Disable the annotator.
+		ui.WithInterval(time.Second).LeftClickUntil(annotatorTrayButton, ui.Gone(inkCanvas)),
+		// This test saves the screencast to the DriveFS
+		// directory.
+		ui.WaitUntilExists(stopRecordingButton),
+		ui.LeftClickUntil(stopRecordingButton, ui.Gone(stopRecordingButton)),
+		// Dismiss the tutorial videos in case they hide the screencast item on small screens.
+		ui.WithInterval(5*time.Second).RetryUntil(refreshApp, ui.Exists(closeTutorialsButton)),
+		ui.LeftClickUntil(closeTutorialsButton, ui.Gone(tutorialsText)),
+		// We need to clean up this screencast to prevent
+		// taking up Drive quota over time.
+		ui.WaitUntilExists(screencastItemMoreOptionsButton),
+		ui.MakeVisible(screencastItemMoreOptionsButton),
+		ui.LeftClickUntil(screencastItemMoreOptionsButton, ui.Exists(deleteMenuItem)),
+		ui.LeftClickUntil(deleteMenuItem, ui.Exists(deleteButton)),
+		ui.LeftClickUntil(deleteButton, ui.Gone(deleteButton)),
+	)(ctx); err != nil {
+		s.Fatal("Failed to go through the new screencast creation flow: ", err)
+	}
+}
