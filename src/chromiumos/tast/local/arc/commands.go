ec752688c1b2e8af3ea7e01c419fcc3703164c8d
pjlee <pjlee@google.com>
2022-04-22 00:25:40 +0000

    arcappgameperf: Add SurfaceFlinger FPS and latency calculations
    Using SurfaceFlinger, obtain frame data to calculate FPS and average latency from the Android perspective.
    
    BUG=b:228454928
    TEST=child CL
    
    Change-Id: I6abc4049364827b27bcaf78145c087251f8ce4d1

--- src/chromiumos/tast/local/arc/commands.go
+++ src/chromiumos/tast/local/arc/commands.go
@@ -17,6 +17,12 @@ func (a *ARC) Command(ctx context.Context, name string, args ...string) *testexe
 	return a.device.ShellCommand(ctx, name, args...)
 }
 
+// SurfaceFlingerLatencyCommand returns a command specifically for calling
+// the SurfaceFlinger command to obtain latencies, via adb.
+func (a *ARC) SurfaceFlingerLatencyCommand(ctx context.Context, surfaceViewName string) *testexec.Cmd {
+	return a.device.SurfaceFlingerLatencyCommand(ctx, surfaceViewName)
+}
+
 // BootstrapCommand runs a command with android-sh.
 //
 // It is very rare you want to call this function from your test; call Command
