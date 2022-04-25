93ec62fb925fbae52880b22913556ac4135b2b9e
Jae Hoon Kim <kimjae@chromium.org>
2022-04-25 20:12:37 +0000

    tast: update_engine: Consumer Auto Update Test
    Test to validate consumer auto update feature being toggled and making
    sure update checks work as intended.
    
    BUG=chromium:1278079
    TEST=tast run $IP platform.UpdateEngine*
    
    Change-Id: Ic5a7b90a3194fd9072cdf9e16bf9e55b05795dc4

---
+++ src/chromiumos/tast/local/bundles/cros/platform/update_engine_consumer_auto_update_feature.go
@@ -0,0 +1,115 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/updateengine"
+	"chromiumos/tast/testing"
+)
+
+type featuresTestParam struct {
+	enabled bool
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: UpdateEngineConsumerAutoUpdateFeature,
+		Desc: "Verifies that update engine features work as intended",
+		Contacts: []string{
+			"kimjae@chromium.org",
+			"chromeos-core-services@google.com",
+		},
+		Fixture: "updateEngineReady",
+		Attr:    []string{"group:mainline"},
+		Timeout: 3 * time.Minute,
+		Params: []testing.Param{
+			{
+				Name: "enabled",
+				Val: featuresTestParam{
+					enabled: true,
+				},
+				ExtraAttr: []string{"informational"},
+			},
+			{
+				Name: "disabled",
+				Val: featuresTestParam{
+					enabled: false,
+				},
+				ExtraAttr: []string{"informational"},
+			},
+		},
+	})
+}
+
+func UpdateEngineConsumerAutoUpdateFeature(ctx context.Context, s *testing.State) {
+	enabled := s.Param().(featuresTestParam).enabled
+	if err := setupConsumerAutoUpdate(ctx, updateengine.ConsumerAutoUpdate, enabled); err != nil {
+		s.Fatal("Failed to setup test: ", err)
+	}
+	if enabled {
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			if status, err := updateengine.Status(ctx); err != nil {
+				return testing.PollBreak(err)
+			} else if status.LastCheckedTime == 0 {
+				return errors.New("Update check was not performed when consumer auto update was turned on")
+			}
+			return nil
+		}, &testing.PollOptions{Timeout: time.Second * 10}); err != nil {
+			s.Fatal("Failed enabled test: ", err)
+		}
+	} else {
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			if status, err := updateengine.Status(ctx); err != nil {
+				return testing.PollBreak(err)
+			} else if status.LastCheckedTime != 0 {
+				return testing.PollBreak(errors.New("Update check performed when consumer auto update was turned off"))
+			}
+			return nil
+		}, &testing.PollOptions{Timeout: time.Second * 10}); err != nil {
+			s.Fatal("Failed disabled test: ", err)
+		}
+	}
+}
+
+func setupConsumerAutoUpdate(ctx context.Context, feature updateengine.Feature, enabled bool) error {
+	// Mark being OOBE complete, so update-engine won't block update checks.
+	if err := updateengine.MarkOobeCompletion(ctx); err != nil {
+		return err
+	}
+
+	// Toggle consumer auto updates.
+	if err := updateengine.ToggleFeature(ctx, feature, enabled); err != nil {
+		return err
+	}
+
+	// Verify that consumer auto update.
+	if v, err := updateengine.IsFeatureEnabled(ctx, feature); err != nil {
+		return err
+	} else if v != enabled {
+		return errors.New("failed to toggle consumer auto update feature")
+	}
+
+	// Temporarily stop update-engine to override background update check interval.
+	if err := updateengine.StopDaemon(ctx); err != nil {
+		return err
+	}
+
+	// No delay in performing background update check.
+	if err := updateengine.SetPref(ctx, updateengine.TestUpdateCheckIntervalTimeout, "0"); err != nil {
+		return err
+	}
+	if err := updateengine.StartDaemon(ctx); err != nil {
+		return err
+	}
+	if err := updateengine.WaitForService(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ src/chromiumos/tast/local/updateengine/prefs.go
@@ -0,0 +1,81 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package updateengine provides ways to interact with update_engine daemon and utilities.
+package updateengine
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+const (
+	prefsDir  = "/var/lib/update_engine/prefs"
+	prefsPerm = 0600
+)
+
+// Pref is the name prefs update_engine supports.
+type Pref string
+
+// List of prefs that update_engine utilizes.
+const (
+	// Pref which allows update_engine to perform background checks (non-interactive) in seconds.
+	TestUpdateCheckIntervalTimeout Pref = "test-update-check-interval-timeout"
+)
+
+// SetPref will set the update_engine pref.
+// Multi-level prefs will create prefPath's missing directories.
+func SetPref(ctx context.Context, prefPath Pref, prefVal string) error {
+	path := filepath.Join(prefsDir, string(prefPath))
+	if err := os.MkdirAll(filepath.Dir(path), os.ModeDir); err != nil {
+		return err
+	}
+	return os.WriteFile(path, []byte(prefVal), prefsPerm)
+}
+
+// ForceClearPrefs will force clearing the pref without caring about update_engine being shutdown.
+func ForceClearPrefs(ctx context.Context) error {
+	testing.ContextLog(ctx, "Force clearing all ", JobName, " prefs")
+	if err := os.RemoveAll(prefsDir); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ClearPrefs will clear the update_engine prefs + have update_engine refresh
+// the cleared prefs.
+func ClearPrefs(ctx context.Context) error {
+	testing.ContextLog(ctx, "Clearing all ", JobName, " prefs")
+	if err := StopDaemon(ctx); err != nil {
+		return err
+	}
+
+	var err error
+	// Always try to respawn update_engine incase of pref clearing failure.
+	// Users of this package might not spawn update_engine back up or use
+	// the correct fixtures/etc.
+	defer func() {
+		wrapErr := func(newErr error) {
+			if err != nil {
+				err = errors.Wrapf(err, "%s", newErr)
+			} else {
+				err = newErr
+			}
+		}
+		if tmperr := StartDaemon(ctx); tmperr != nil {
+			wrapErr(tmperr)
+			return
+		}
+		if tmperr := WaitForService(ctx); tmperr != nil {
+			wrapErr(tmperr)
+		}
+	}()
+
+	err = ForceClearPrefs(ctx)
+	return err
+}
