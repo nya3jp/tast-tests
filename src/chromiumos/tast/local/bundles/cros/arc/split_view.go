39645f7121b038a35ce4b8f405ae2ef5cac938fe
Toshiki Kikuchi <toshikikikuchi@google.com>
2022-04-25 15:27:53 +0900

    arc.SplitView: Extract method into helper
    This CL extracts out dragToSnapFirstOverviewWindow() method into the helper
    package.
    This allows other tests to reuse this method.
    
    BUG=b:217655499
    TEST=tast run DUT arc.SplitView.*
    
    Change-Id: I86daa00cfaec3f244f1f2573e8de9ac03956bb5c

--- src/chromiumos/tast/local/bundles/cros/arc/split_view.go
+++ src/chromiumos/tast/local/bundles/cros/arc/split_view.go
@@ -12,6 +12,7 @@ import (
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/wm"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/display"
@@ -72,48 +73,6 @@ func init() {
 			},
 		},
 	})
-}
-
-// dragToSnapFirstOverviewWindow finds the first window in overview, and drags
-// to snap it to the primary side (the secondary side if primary is false) by
-// using the given pointer.Context.
-// This function assumes that overview is already active.
-func dragToSnapFirstOverviewWindow(ctx context.Context, tconn *chrome.TestConn, pc pointer.Context, primary bool) error {
-	info, err := display.GetPrimaryInfo(ctx, tconn)
-	if err != nil {
-		return errors.Wrap(err, "failed to get the primary display info")
-	}
-
-	w, err := ash.FindFirstWindowInOverview(ctx, tconn)
-	if err != nil {
-		// If you see this error on the second window snap (to the right), check if
-		// b/143499564 has been reintroduced.
-		return errors.Wrap(err, "failed to find window in overview grid")
-	}
-
-	center := w.OverviewInfo.Bounds.CenterPoint()
-	target := info.Bounds.RightCenter()
-	if primary {
-		target = info.Bounds.LeftCenter()
-	}
-
-	waitForDragged := func(ctx context.Context) error {
-		if err := testing.Poll(ctx, func(ctx context.Context) error {
-			if _, err := ash.DraggedWindowInOverview(ctx, tconn); err != nil {
-				return errors.Wrap(err, "failed to get dragged overview item")
-			}
-			return nil
-		}, &testing.PollOptions{Interval: 500 * time.Millisecond, Timeout: 5 * time.Second}); err != nil {
-			return errors.Wrap(err, "failed to wait for overview item to be dragged")
-		}
-		return nil
-	}
-
-	if err := pc.Drag(center, waitForDragged, pc.DragTo(target, time.Second))(ctx); err != nil {
-		return errors.Wrap(err, "failed to drag to snap from overview")
-	}
-
-	return nil
 }
 
 type windowStateExpectations []struct {
@@ -276,7 +235,7 @@ func SplitView(ctx context.Context, s *testing.State) {
 	defer ash.SetOverviewModeAndWait(cleanupCtx, tconn, false)
 
 	// Snap activity to left.
-	if err := dragToSnapFirstOverviewWindow(ctx, tconn, pc, true /* primary */); err != nil {
+	if err := wm.DragToSnapFirstOverviewWindow(ctx, tconn, pc, true /* primary */); err != nil {
 		s.Fatal("Failed to drag window from overview and snap left: ", err)
 	}
 	if err := waitForWindowStates(ctx, tconn, windowStateExpectations{{leftAct, ash.WindowStateLeftSnapped, arc.WindowStatePrimarySnapped}}); err != nil {
@@ -287,7 +246,7 @@ func SplitView(ctx context.Context, s *testing.State) {
 	}
 
 	// Snap activity to right.
-	if err := dragToSnapFirstOverviewWindow(ctx, tconn, pc, false /* primary */); err != nil {
+	if err := wm.DragToSnapFirstOverviewWindow(ctx, tconn, pc, false /* primary */); err != nil {
 		s.Fatal("Failed to drag window from overview and snap right: ", err)
 	}
 	if err := waitForWindowStates(ctx, tconn, windowStateExpectations{{rightAct, ash.WindowStateRightSnapped, arc.WindowStateSecondarySnapped}}); err != nil {
