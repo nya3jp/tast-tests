f7eeff3793e75826d2fcba3c3f11411f06fd3479
Paul Moy <pmoy@chromium.org>
2022-04-26 14:54:38 -0600

    printer: add RejectDocumentFormat test
    This test checks that lpadmin correctly handles the case where printers
    reject Get-Printer-Attributes requests that contain the document-format
    attribute. Older printers sometimes demonstrate this behavior.
    
    This first version of the test exercises the code in lpadmin that
    retries the failed Get-Printer-Attributes request without the
    document-format attribute, but it does not finish configuring the
    printer.
    
    BUG=b:175631323
    TEST=tast run --build $DUT_IP printer.RejectDocumentFormat
    
    Cq-Depend: chromium:3607067
    Change-Id: Iefa28f9d7816aa1d6a14e4423b909d9a9136233f

---
+++ src/chromiumos/tast/local/bundles/cros/printer/reject_document_format.go
@@ -0,0 +1,81 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package printer
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/printing/usbprinter"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RejectDocumentFormat,
+		LacrosStatus: testing.LacrosVariantNeeded,
+		Desc:         "Tests that lpadmin handles a printer rejecting get-printer-attributes requests containing the document-format attribute",
+		Contacts:     []string{"pmoy@chromium.org", "project-bolton@google.com"},
+		Attr: []string{
+			"group:mainline",
+			"informational",
+			"group:paper-io",
+			"paper-io_printing",
+		},
+		Timeout:      2 * time.Minute,
+		SoftwareDeps: []string{"chrome", "cros_internal", "cups", "virtual_usb_printer"},
+		Data:         []string{"reject_document_format_script.textproto"},
+		Fixture:      "virtualUsbPrinterModulesLoadedWithChromeLoggedIn",
+	})
+}
+
+func RejectDocumentFormat(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	printer, err := usbprinter.Start(ctx,
+		usbprinter.WithIPPUSBDescriptors(),
+		usbprinter.WithGenericIPPAttributes(),
+		usbprinter.WithMockPrinterScriptPath(s.DataPath("reject_document_format_script.textproto")))
+	if err != nil {
+		s.Fatal("Failed to start IPP-over-USB printer: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := printer.Stop(ctx); err != nil {
+			s.Error("Failed to stop printer: ", err)
+		}
+	}(cleanupCtx)
+
+	uri := fmt.Sprintf("ippusb://%s_%s/ipp/print", printer.DevInfo.VID, printer.DevInfo.PID)
+
+	cmd := testexec.CommandContext(ctx, "lpadmin", "-v", uri, "-m", "everywhere")
+	_, stderr, err := cmd.SeparatedOutput()
+	// We don't expect lpadmin to succeed, since the mock printer is only
+	// scripted to respond enough to test the desired behavior of this test.
+	// Instead of success, we're looking for a specific exit code and a
+	// corresponding error message that ensure lpadmin took the correct code
+	// path.
+	if code, ok := testexec.ExitCode(err); !ok || code != 9 {
+		s.Fatal("Expected exit status 9 from `lpadmin`: ", err)
+	}
+	if !strings.Contains(string(stderr), "Failed to execute Get-Printer-Attributes request - retrying without document-format attribute") {
+		s.Fatal("Error message did not contain `retrying without document-format`: ", string(stderr))
+	}
+}
