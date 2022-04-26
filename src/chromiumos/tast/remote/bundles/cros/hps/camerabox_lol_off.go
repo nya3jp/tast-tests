c9c1d8696458d424099c9451e43aad3d46df8194
Eunice Sun <eunicesun@google.com>
2022-04-26 05:25:17 +0000

    hps: create grpc services and simple test
    BUG=b:229925364
    TEST=tast run -buildbundle=cros -var=tablet=<tablet IP> <DUT IP>  hps.CameraboxLoLOff
    
    Change-Id: I4d9787e994b652902b11b65504b6207566c37bc7

---
+++ src/chromiumos/tast/remote/bundles/cros/hps/camerabox_lol_off.go
@@ -0,0 +1,109 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package hps
+
+import (
+	"context"
+	"time"
+
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/common/camera/chart"
+	"chromiumos/tast/common/hps/hpsutil"
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/remote/bundles/cros/hps/utils"
+	"chromiumos/tast/rpc"
+	pb "chromiumos/tast/services/cros/hps"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CameraboxLoLOff,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verify that the HPS can dim the screen within the right time range when LoL is off",
+		Data:         []string{hpsutil.PersonPresentPageArchiveFilename},
+		Contacts: []string{
+			"eunicesun@google.com",
+			"mblsha@google.com",
+			"chromeos-hps-swe@google.com",
+		},
+		Attr:         []string{"group:camerabox", "group:hps", "hps_perbuild"},
+		Timeout:      6 * time.Minute,
+		SoftwareDeps: []string{"hps", "chrome", caps.BuiltinCamera},
+		ServiceDeps:  []string{"tast.cros.browser.ChromeService", "tast.cros.hps.HpsService"},
+		Vars:         []string{"tablet", "grpcServerPort"},
+	})
+}
+
+func CameraboxLoLOff(ctx context.Context, s *testing.State) {
+
+	d := s.DUT()
+
+	archive := s.DataPath(hpsutil.PersonPresentPageArchiveFilename)
+	filePaths, err := utils.UntarImages(ctx, archive)
+	if err != nil {
+		s.Fatal("Tmp dir creation failed on DUT")
+	}
+	// Creating hps context.
+	hctx, err := hpsutil.NewHpsContext(ctx, "", hpsutil.DeviceTypeBuiltin, s.OutDir(), d.Conn())
+	if err != nil {
+		s.Fatal("Error creating HpsContext: ", err)
+	}
+
+	// Connecting to the other tablet that will render the picture.
+	var chartAddr string
+	if altAddr, ok := s.Var("tablet"); ok {
+		chartAddr = altAddr
+	}
+	c, hostPaths, err := chart.New(ctx, d, chartAddr, s.OutDir(), filePaths)
+	if err != nil {
+		s.Fatal("Failed to send the files: ", err)
+	}
+	c.Display(ctx, hostPaths[0])
+
+	// Connecting to Taeko.
+	cl, err := rpc.Dial(ctx, d, s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	req := &pb.StartUIWithCustomScreenPrivacySettingRequest{
+		Setting: "Lock on Leave",
+		Enable:  false,
+	}
+	client := pb.NewHpsServiceClient(cl.Conn)
+	if _, err := client.StartUIWithCustomScreenPrivacySetting(hctx.Ctx, req, grpc.WaitForReady(true)); err != nil {
+		s.Fatal("Failed to change setting: ", err)
+	}
+
+	brightness, err := utils.GetBrightness(hctx.Ctx, d.Conn())
+	if err != nil {
+		s.Fatal("Error failed to get brightness: ", err)
+	}
+	testing.ContextLog(ctx, "Brightness: ", brightness)
+
+	// It should not dim after 6s.
+	testing.Sleep(ctx, utils.QuickDimTime)
+	newBrightness, err := utils.GetBrightness(hctx.Ctx, d.Conn())
+	if err != nil {
+		s.Fatal("Error when getting brightness: ", err)
+	}
+	if newBrightness != brightness {
+		s.Fatal("Unexpected brightness change")
+	}
+
+	// It should not lock after 2mins.
+	testing.Sleep(ctx, utils.QuickLockTime)
+	newBrightness, err = utils.GetBrightness(hctx.Ctx, d.Conn())
+	if err != nil {
+		s.Fatal("Error when getting brightness: ", err)
+	}
+	if newBrightness != brightness {
+		s.Fatal("Unexpected brightness change")
+	}
+
+}
