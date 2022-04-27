839ea5579fbe716c5fd76e8863d5f9b9e0d4bad5
Howard Yang <hcyang@google.com>
2022-04-27 16:58:20 +0800

    autoupdate: Add cross version chrome login test
    We already have a vault compat cross version test, but we want another
    test that is more "end-to-end". That is, a test that logs in Chrome for
    the first time at milestone N, updates to milestone M (>N), then logs in
    again. This also serves as the base test for future cross version tests
    we want to add that build upon Chrome (like WebAuthn cross version
    test). When those tests failed, we can identify better if it's the
    target behavior failing across version, or is it login itself that is
    failing.
    
    Refactored the autoupdatelib interface to follow tast writing
    guidelines.
    
    BUG=b:230070328
    TEST=run autoupdate.* with the method written in README.md.
    
    Change-Id: I0750da54ad1d15c2cf47b44816815331a52f895f

---
+++ src/chromiumos/tast/remote/bundles/cros/autoupdate/n_to_m_chrome_login.go
@@ -0,0 +1,111 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package autoupdate
+
+import (
+	"bytes"
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/common/hwsec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/bundles/cros/autoupdate/util"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/ui"
+	"chromiumos/tast/testing"
+)
+
+const (
+	chromeDefaultUsername = "testuser@gmail.com"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NToMChromeLogin,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verify cross version vault's compatibility",
+		Contacts: []string{
+			"hcyang@google.com", // Test author
+			"cros-hwsec@google.com",
+		},
+		Attr:         []string{"group:autoupdate"},
+		SoftwareDeps: []string{"tpm", "reboot", "chrome"},
+		ServiceDeps: []string{
+			"tast.cros.browser.ChromeService",
+			"tast.cros.autoupdate.NebraskaService",
+			"tast.cros.autoupdate.UpdateService",
+		},
+		Timeout: util.TotalTestTime,
+	})
+}
+
+func NToMChromeLogin(ctx context.Context, s *testing.State) {
+	env, err := util.NewHwsecEnv(s.DUT())
+	if err != nil {
+		s.Fatal("Failed to create hwsec env: ", err)
+	}
+
+	ops := &util.Operations{
+		PreUpdate: func(ctx context.Context) error {
+			return util.ClearTpm(ctx, env)
+		},
+		PostUpdate: func(ctx context.Context) error {
+			cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
+			if err != nil {
+				s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+			}
+			defer cl.Close(ctx)
+			return createUserAndCreateTestFile(ctx, env, cl.Conn)
+		},
+		PostRollback: func(ctx context.Context) error {
+			cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
+			if err != nil {
+				s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+			}
+			defer cl.Close(ctx)
+			return loginUserAndReadTestFile(ctx, env, cl.Conn)
+		},
+	}
+
+	if err := util.NToMTest(ctx, s.DUT(), s.OutDir(), s.RPCHint(), ops, 3 /*deltaM*/); err != nil {
+		s.Fatal("Failed to run cross version test: ", err)
+	}
+}
+
+func createUserAndCreateTestFile(ctx context.Context, env *util.HwsecEnv, conn *grpc.ClientConn) error {
+	cr := ui.NewChromeServiceClient(conn)
+
+	// Login Chrome for first time, and create a test file.
+	if _, err := cr.New(ctx, &ui.NewRequest{}); err != nil {
+		return errors.Wrap(err, "failed to start Chrome")
+	}
+	defer cr.Close(ctx, &empty.Empty{})
+
+	if err := hwsec.WriteUserTestContent(ctx, env.Utility, env.CmdRunner, chromeDefaultUsername, util.TestFile, util.TestFileContent); err != nil {
+		return errors.Wrap(err, "failed to write test content")
+	}
+
+	return nil
+}
+
+func loginUserAndReadTestFile(ctx context.Context, env *util.HwsecEnv, conn *grpc.ClientConn) error {
+	cr := ui.NewChromeServiceClient(conn)
+
+	// Login Chrome to see if the test file still exists
+	if _, err := cr.New(ctx, &ui.NewRequest{KeepState: true}); err != nil {
+		return errors.Wrap(err, "failed to start Chrome")
+	}
+	defer cr.Close(ctx, &empty.Empty{})
+
+	if content, err := hwsec.ReadUserTestContent(ctx, env.Utility, env.CmdRunner, chromeDefaultUsername, util.TestFile); err != nil {
+		return errors.Wrap(err, "failed to read test content")
+	} else if !bytes.Equal(content, []byte(util.TestFileContent)) {
+		return errors.Errorf("unexpected test file content: got %q, want %q", string(content), util.TestFileContent)
+	}
+
+	return nil
+}
---
+++ src/chromiumos/tast/remote/bundles/cros/autoupdate/util/constants.go
@@ -0,0 +1,12 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package util
+
+// This file contains some shared constants for autoupdate tests.
+const (
+	TestFile        = "compat_testing_file"
+	EncstatefulFile = "/mnt/stateful_partition/encrypted/file"
+	TestFileContent = "content"
+)
