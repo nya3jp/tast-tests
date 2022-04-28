f7610402b25adcb3eac962de0ad05b624b142f7f
Georg Neis <neis@chromium.org>
2022-04-28 01:39:02 +0000

    lacros: Allow extensions besides test API
    There is no need to disallow any extensions when starting Lacros.
    
    BUG=None
    TEST=fast-build.sh -T
    TEST=cros tryjob -g 3610467 --hwtest rammus-full-tryjob
    TEST=cros tryjob -g 3610467 --hwtest puff-release-tryjob
    
    Change-Id: Iafcb5ced08b31612a69b5e8224c07e7d0c2796f2

--- src/chromiumos/tast/local/chrome/lacros/lacrosfixt/config.go
+++ src/chromiumos/tast/local/chrome/lacros/lacrosfixt/config.go
@@ -92,11 +92,10 @@ func NewConfig(ops ...Option) *Config {
 // ExtensionArgs returns a list of args needed to pass to a lacros instance to enable the test extension.
 func ExtensionArgs(extID, extList string) []string {
 	return []string{
-		"--remote-debugging-port=0",              // Let Chrome choose its own debugging port.
-		"--enable-experimental-extension-apis",   // Allow Chrome to use the Chrome Automation API.
-		"--allowlisted-extension-id=" + extID,    // Whitelists the test extension to access all Chrome APIs.
-		"--load-extension=" + extList,            // Load extensions.
-		"--disable-extensions-except=" + extList, // Disable extensions other than the Tast test extension.
+		"--remote-debugging-port=0",            // Let Chrome choose its own debugging port.
+		"--enable-experimental-extension-apis", // Allow Chrome to use the Chrome Automation API.
+		"--allowlisted-extension-id=" + extID,  // Whitelists the test extension to access all Chrome APIs.
+		"--load-extension=" + extList,          // Load extensions.
 	}
 }
 
