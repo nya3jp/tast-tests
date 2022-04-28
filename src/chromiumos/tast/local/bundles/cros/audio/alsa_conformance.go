b67d2c03584980e2b12b2b4a913bf5808e38f734
Judy Hsiao <judyhsiao@chromium.org>
2022-04-28 06:38:21 +0000

    tast-tests: Mark audio.ALSAConformance as critical
    The test is stable: https://tastboard.corp.google.com/test/audio.ALSAConformance
    
    BUG=b:230069491
    TEST=None
    
    Change-Id: I14082b66c6f4de0ea4d3ecb9ae7cb94cb154cc9a

--- src/chromiumos/tast/local/bundles/cros/audio/alsa_conformance.go
+++ src/chromiumos/tast/local/bundles/cros/audio/alsa_conformance.go
@@ -27,7 +27,7 @@ func init() {
 		Func:         ALSAConformance,
 		Desc:         "Runs alsa_conformance_test to test basic functions of ALSA",
 		Contacts:     []string{"yuhsuan@chromium.org", "cychiang@chromium.org"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		HardwareDeps: hwdep.D(hwdep.Speaker(), hwdep.Microphone()),
 		Timeout:      10 * time.Minute,
 	})
