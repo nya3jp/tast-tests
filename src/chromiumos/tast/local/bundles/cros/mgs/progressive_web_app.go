0f3552ad4470a1df67219e9aac89725ba90cd870
Miriam Polzer <mpolzer@google.com>
2022-04-29 08:52:37 +0000

    mgs: Add PWA test
    Add a test that installs and opens a test PWA in MGS.
    
    BUG=b:200987809
    TEST=tast run localhost:2222 mgs.PlatformWebApp
         ~/trunk/src/platform/tast/fast_build.sh -T
    
    Change-Id: Ib17157025507ddd256b4e81b59ca1d0a8777ada8

---
+++ src/chromiumos/tast/local/bundles/cros/mgs/progressive_web_app.go
@@ -0,0 +1,83 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package mgs
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/mgs"
+	"chromiumos/tast/testing"
+)
+
+const port = 8080
+const url = "http://localhost:%v/pwa_index.html"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ProgressiveWebApp,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verify that Progressive Web Apps (PWA) are working in a managed guest session by trying to install and start a test PWA",
+		Contacts: []string{
+			"mpolzer@google.com", // Test author
+			"chromeos-kiosk-eng+TAST@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:mainline", "informational"},
+		Data:         []string{"pwa_manifest.json", "pwa_service.js", "pwa_index.html", "pwa_icon.png"},
+		Fixture:      fixture.FakeDMSEnrolled,
+	})
+}
+
+func ProgressiveWebApp(ctx context.Context, s *testing.State) {
+	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	mgs, cr, err := mgs.New(
+		ctx,
+		fdms,
+		mgs.DefaultAccount(),
+		mgs.AutoLaunch(mgs.MgsAccountID),
+	)
+	if err != nil {
+		s.Fatal("Failed to start MGS: ", err)
+	}
+	defer mgs.Close(ctx)
+
+	mux := http.NewServeMux()
+	fs := http.FileServer(s.DataFileSystem())
+	mux.Handle("/", fs)
+
+	server := &http.Server{Addr: fmt.Sprintf(":%v", port), Handler: mux}
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			s.Fatal("Failed to create local server: ", err)
+		}
+	}()
+	defer server.Shutdown(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	id, err := apps.InstallPWAForURL(ctx, cr, fmt.Sprintf(url, port), 15*time.Second)
+	if err != nil {
+		s.Fatal("Failed to install PWA for URL: ", err)
+	}
+
+	if err := ash.WaitForChromeAppInstalled(ctx, tconn, id, 15*time.Second); err != nil {
+		s.Fatal("Failed to wait for PWA to be installed: ", err)
+	}
+
+	if err := ash.WaitForApp(ctx, tconn, id, 15*time.Second); err != nil {
+		s.Fatal("Failed to wait for PWA to open: ", err)
+	}
+}
