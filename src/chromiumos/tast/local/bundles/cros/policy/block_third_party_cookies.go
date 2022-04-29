cda266b232bde3c5a555309ac92bbcee71b01bf1
phweiss <phweiss@google.com>
2022-04-29 15:58:58 +0200

    tast: Blocking third-party cookies via policy
    The existing test only checked that the policy changes the settings
    regarding third-party cookies, but it didn't actually set a cookie and
    observer that it gets blocked.
    
    This CL sets up two servers, the "main" server under 127.0.0.1 and a
    third-party server serving from localhost. (This distinction is
    fortunately enough to make it count as third-party.)
    
    Third-party cookies can only be set via a secure HTTPS connection.
    Therefore, this CL extends the https tast-module to include support
    for multiple servers at once and for custom HTTP handler functions
    that can be set per-file.
    
    Also, a certificate is added for the 127.0.0.1 IP address, the
    existing certificate only works localhost. The same CA-cert is used
    to sign both of these certificates, so only on Certificate Authority
    needs to be configured in the test.
    
    BUG=b:200988635
    BUG=chromium:1186217
    TEST=tast run dut policy.BlockThirdPartyCookies
    TEST=tast run dut policy.BlockThirdPartyCookies.lacros
    
    Change-Id: Ic9ef74e9b965e66f842f69b1042964f1665269cf

--- src/chromiumos/tast/local/bundles/cros/policy/block_third_party_cookies.go
+++ src/chromiumos/tast/local/bundles/cros/policy/block_third_party_cookies.go
@@ -6,6 +6,11 @@ package policy
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"chromiumos/tast/common/fixture"
@@ -15,11 +20,13 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/browser"
 	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/checked"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/restriction"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/https"
 	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/testing"
 )
@@ -31,6 +38,7 @@ func init() {
 		Desc:         "Test the behavior of BlockThirdPartyCookies policy: check if third party cookies are allowed based on policy value",
 		Contacts: []string{
 			"snijhara@google.com", // Test author
+			"phweiss@google.com",  // Test author
 			"chromeos-commercial-remote-management@google.com",
 		},
 		SoftwareDeps: []string{"chrome"},
@@ -45,6 +53,15 @@ func init() {
 			Fixture:           fixture.LacrosPolicyLoggedIn,
 			Val:               browser.TypeLacros,
 		}},
+		Data: []string{
+			"third_party_cookies.html",
+			"third_party_cookies.js",
+			"certificate.pem",
+			"key.pem",
+			"cert_for_127.0.0.1.pem",
+			"key_for_127.0.0.1.pem",
+			"ca-cert.pem",
+		},
 	})
 }
 
@@ -65,24 +82,109 @@ func BlockThirdPartyCookies(ctx context.Context, s *testing.State) {
 		"Block third-party cookies",
 	}
 
+	// TODO(crbug.com/1298550): Don't rely on all files being in same directory.
+	baseDirectory, _ := filepath.Split(s.DataPath("certificate.pem"))
+	localhostConfiguration := https.ServerConfiguration{
+		ServerKeyPath:         s.DataPath("key.pem"),
+		ServerCertificatePath: s.DataPath("certificate.pem"),
+		CaCertificatePath:     s.DataPath("ca-cert.pem"),
+		HostedFilesBasePath:   baseDirectory,
+	}
+
+	// This certificate is signed with the same CA key, but specifies 127.0.0.1 instead of
+	// localhost as Subject Alternative Name.
+	IPConfiguration := https.ServerConfiguration{
+		ServerKeyPath:         s.DataPath("key_for_127.0.0.1.pem"),
+		ServerCertificatePath: s.DataPath("cert_for_127.0.0.1.pem"),
+		CaCertificatePath:     s.DataPath("ca-cert.pem"),
+		HostedFilesBasePath:   baseDirectory,
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get TestConn: ", err)
+	}
+
+	// For third_party_cookies.html, we need to insert the right port number to the other server into the HTML.
+	path := s.DataPath("third_party_cookies.html")
+	htmlBuffer, err := ioutil.ReadFile(path)
+	if err != nil {
+		s.Fatal("Couldn't read .html template file")
+	}
+	htmlContent := string(htmlBuffer)
+
+	// Read third_party_cookies.js, because we need to write it in our custom handler that also sets the cookies.
+	jsPath := s.DataPath("third_party_cookies.js")
+	jsBuffer, err := ioutil.ReadFile(jsPath)
+	if err != nil {
+		s.Fatal("Couldn't read .js file")
+	}
+	jsContent := string(jsBuffer)
+
+	// Serve the modified HTML content instead of the original file.
+	IPConfiguration.HandleFunc("/third_party_cookies.html", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(200)
+		fmt.Fprintf(w, htmlContent)
+	})
+
+	// Serve the unmodified js content and set the third-party cookie.
+	localhostConfiguration.HandleFunc("/third_party_cookies.js", func(w http.ResponseWriter, req *http.Request) {
+		// SameSite and Secure are mandatory for third-party cookies, that's why we are using https.
+		cookie := &http.Cookie{
+			Name:     "token",
+			Value:    "some_token",
+			MaxAge:   3000,
+			Domain:   "localhost",
+			SameSite: http.SameSiteNoneMode,
+			Secure:   true,
+		}
+		http.SetCookie(w, cookie)
+		w.WriteHeader(200)
+		fmt.Fprintf(w, jsContent)
+	})
+
+	// Start both servers.
+	// First party.
+	ipServer := https.StartServer(IPConfiguration)
+	if ipServer.Error != nil {
+		s.Fatal("Could not start https server: ", err)
+	}
+	defer ipServer.Close()
+
+	// Third party.
+	localhostServer := https.StartServer(localhostConfiguration)
+	if localhostServer.Error != nil {
+		s.Fatal("Could not start https server: ", err)
+	}
+	defer localhostServer.Close()
+
+	ipPort := ipServer.Address[strings.LastIndex(ipServer.Address, ":")+1:]
+	localhostPort := localhostServer.Address[strings.LastIndex(localhostServer.Address, ":")+1:]
+
+	// Provide the third-party server's port in the html file.
+	htmlContent = strings.Replace(htmlContent, "LOCALHOST_PORT", localhostPort, 1)
+
 	for _, param := range []struct {
 		name            string                    // name is the name of the test case.
 		wantRestriction []restriction.Restriction // The expected restriction states of the radio buttons in
 		// radioButtonNames.
 		wantChecked []checked.Checked // The expected checked states of the radio buttons in
 		// radioButtonNames.
-		policy *policy.BlockThirdPartyCookies // policy is the policy we test.
+		wantCookie bool                           // Whether the third-party cookie should be successfully set.
+		policy     *policy.BlockThirdPartyCookies // policy is the policy we test.
 	}{
 		{
 			name:            "unset",
 			wantRestriction: []restriction.Restriction{restriction.None, restriction.None},
 			wantChecked:     []checked.Checked{checked.False, checked.False},
+			wantCookie:      true,
 			policy:          &policy.BlockThirdPartyCookies{Stat: policy.StatusUnset},
 		},
 		{
 			name:            "allow",
 			wantRestriction: []restriction.Restriction{restriction.None, restriction.Disabled},
 			wantChecked:     []checked.Checked{checked.True, checked.False},
+			wantCookie:      true,
 			policy:          &policy.BlockThirdPartyCookies{Val: false},
 		},
 		{
@@ -92,6 +194,7 @@ func BlockThirdPartyCookies(ctx context.Context, s *testing.State) {
 			// the cookies settings page.
 			wantRestriction: []restriction.Restriction{restriction.Disabled, restriction.None},
 			wantChecked:     []checked.Checked{checked.False, checked.True},
+			wantCookie:      false,
 			policy:          &policy.BlockThirdPartyCookies{Val: true},
 		},
 	} {
@@ -114,6 +217,7 @@ func BlockThirdPartyCookies(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to setup chrome: ", err)
 			}
 			defer closeBrowser(cleanupCtx)
+			https.ConfigureChromeToAcceptCertificate(ctx, localhostConfiguration, cr, br, tconn)
 
 			// Open cookies settings page.
 			conn, err := br.NewConn(ctx, "chrome://settings/cookies")
@@ -134,7 +238,53 @@ func BlockThirdPartyCookies(ctx context.Context, s *testing.State) {
 					s.Errorf("Unexpected settings state for the %q button: %v", radioButtonName, err)
 				}
 			}
-			// TODO(crbug.com/1186217): Verify that third party cookies are actually blocked.
+
+			// Load page that sets one 127.0.0.1 first-party cookie and one localhost third-party cookie.
+			conn2, err := br.NewConn(ctx, "https://127.0.0.1:"+ipPort+"/third_party_cookies.html")
+			if err != nil {
+				s.Fatal("Failed to connect to the browser: ", err)
+			}
+			defer conn2.Close()
+
+			// Check which cookies got created.
+			conn3, err := br.NewConn(ctx, "chrome://settings/siteData")
+			if err != nil {
+				s.Fatal("Failed to connect to the browser: ", err)
+			}
+			defer conn3.Close()
+
+			ui := uiauto.New(tconn)
+			localhostText := nodewith.NameStartingWith("localhost").Role(role.StaticText)
+			ipButton := nodewith.NameStartingWith("127.0.0.1").Role(role.Button)
+			expandButton := nodewith.NameStartingWith("127.0.0.1").Role(role.Button).ClassName("icon-expand-more")
+			removeAllButton := nodewith.Name("Remove All").Role(role.Button)
+			confirmRemoveAllButton := nodewith.Name("Clear all").Role(role.Button)
+
+			// The removeAllButton is labeled "Clear all data" for non-Google Chromium
+			// builds. Use this to tell the difference between the two cases.
+			if err := ui.WaitUntilExists(removeAllButton)(ctx); err != nil {
+				removeAllButton = nodewith.Name("Clear all data").Role(role.Button)
+				confirmRemoveAllButton = nodewith.Name("Clear").Role(role.Button)
+				// The expand button only exists in non-Google branded builds, and only
+				// if first and third party cookies exist. Click it to reveal third
+				// party cookies, but ignore errors.
+				ui.LeftClick(expandButton)(ctx)
+			}
+
+			checkCookieExistence := ui.WaitUntilExists(localhostText)
+			if !param.wantCookie {
+				checkCookieExistence = ui.Gone(localhostText)
+			}
+
+			if err := uiauto.Combine("check_and_clear_cookies",
+				ui.WaitUntilExists(ipButton),
+				checkCookieExistence,
+				ui.LeftClick(removeAllButton),
+				ui.WaitUntilExists(confirmRemoveAllButton),
+				ui.LeftClick(confirmRemoveAllButton),
+			)(ctx); err != nil {
+				s.Error("Failed to check and clean up cookies: ", err)
+			}
 		})
 	}
 }
