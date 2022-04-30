ac09c12b1fa07e9531ba6e9aaf818c66b3c5f05a
pjlee <pjlee@google.com>
2022-04-30 00:35:40 +0000

    arc/gio: Add GIO touch injector tast test
    Add a tast test to test GIO touch injector correctness.
    
    BUG=b:229156145
    TEST=tast run $DUT arc.InputOverlayTouchInjector*
    
    Change-Id: I15ec31fe01740d2f886639ef72a861616f54c45a

--- src/chromiumos/tast/local/bundles/cros/arc/testutil/testutil.go
+++ src/chromiumos/tast/local/bundles/cros/arc/testutil/testutil.go
@@ -8,13 +8,20 @@ package testutil
 import (
 	"context"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 
+	"chromiumos/tast/common/action"
 	"chromiumos/tast/common/android/ui"
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/cpu"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/screenshot"
 	"chromiumos/tast/testing"
 )
@@ -26,7 +33,33 @@ const (
 
 	// cleanupOnErrorTime reserves time for cleanup in case of an error.
 	cleanupOnErrorTime = time.Second * 30
+	// errorMargin denotes the allowable +/- difference from the calculated x and
+	// y coordinate.
+	errorMargin = 3
+	// WaitForActiveInputTime reserves time between and for hold-release controls
+	// to ensure stability.
+	WaitForActiveInputTime = time.Second
+	// tapMode is the number of expected logcat lines from a tap event.
+	tapMode mode = 2
+	// moveMode is the number of expected logcat lines from a press-release event.
+	moveMode mode = 3
 )
+
+var (
+	// TopTap denotes the heuristics for the top tap input overlay mapping.
+	TopTap = ButtonHeuristics{0.5, 0.5}
+	// BotTap denotes the heuristic for the bottom tap input overlay mapping.
+	BotTap = ButtonHeuristics{0.9, 0.9}
+	// emptyHeuristic is used to pass in an empty ButtonHeuristics.
+	emptyHeuristic = ButtonHeuristics{}
+)
+
+// ButtonHeuristics contains heuristics regarding the percentages on the ARC
+// phone window where input mappings are located.
+type ButtonHeuristics struct {
+	xHeuristic float64
+	yHeuristic float64
+}
 
 // TestParams stores data common to the tests run in this package.
 type TestParams struct {
@@ -35,7 +68,12 @@ type TestParams struct {
 	Device            *ui.Device
 	Activity          *arc.Activity
 	ActivityStartTime time.Time
+	lastTimestamp     string
+	windowContentSize coords.Point
 }
+
+// mode specifies the type of tap event via the number of expected logcat lines.
+type mode int
 
 // coolDownConfig returns the config to wait for the machine to cooldown for game performance tests.
 // This overrides the default config timeout (5 minutes) and temperature threshold (46 C)
@@ -113,9 +151,199 @@ func SetupTestApp(ctx context.Context, s *testing.State, testFunc PerformTestFun
 		Device:            d,
 		Activity:          act,
 		ActivityStartTime: startTime,
+		lastTimestamp:     "00:00:00.000",
 	}); err != nil {
 		s.Fatal("Failed to perform test: ", err)
 	}
+}
+
+// MoveOverlayButton returns a function that takes in the given character corresponding
+// to a move keystroke and returns an error if tapping the keystroke did not result in
+// the correct feedback.
+func MoveOverlayButton(kb *input.KeyboardEventWriter, key string, params *TestParams) action.Action {
+	return func(ctx context.Context) error {
+		// Hold and release given key, which is associated to an overlay button.
+		if err := uiauto.Combine("Tap overlay keys and ensure proper behavior",
+			kb.AccelPressAction(key),
+			action.Sleep(WaitForActiveInputTime),
+			kb.AccelReleaseAction(key),
+		)(ctx); err != nil {
+			return errors.Wrapf(err, "hold and release key %s failed", key)
+		}
+		// Poll for move action pressed; return error if feedback not received correctly;
+		// here, we do not check for correct tap location.
+		if err := pollTouchedCorrectly(ctx, params, moveMode, emptyHeuristic); err != nil {
+			return errors.Wrapf(err, "failed to check key %s", key)
+		}
+		return nil
+	}
+}
+
+// TapOverlayButton returns a function that takes in the given character corresponding
+// to a tap keystroke and returns an error if tapping the keystroke did not result in
+// the correct feedback.
+func TapOverlayButton(kb *input.KeyboardEventWriter, key string, params *TestParams, heuristic ButtonHeuristics) action.Action {
+	return func(ctx context.Context) error {
+		// Tap given key, which is associated to an overlay button.
+		if err := kb.Type(ctx, key); err != nil {
+			return errors.Wrap(err, "failed to type key")
+		}
+		// Poll for tap action pressed; return error if feedback not received correctly.
+		if err := pollTouchedCorrectly(ctx, params, tapMode, heuristic); err != nil {
+			return errors.Wrapf(err, "failed to check key %s", key)
+		}
+		return nil
+	}
+}
+
+// pollTouchedCorrectly makes sure the feedback for a tap touch injection is correct.
+// The mode parameter specifies whether three lines (move action) or two lines (tap action)
+// should be checked for in logcat, while the x and y heuristic are percentages that
+// denote the approximate location of an input overlay button on the screen, in
+// the context of the Android phone window.
+func pollTouchedCorrectly(ctx context.Context, params *TestParams, m mode, heuristic ButtonHeuristics) error {
+	out, err := params.Arc.OutputLogcatGrep(ctx, "InputOverlayTest")
+	if err != nil {
+		return errors.Wrap(err, "failed to execute logcat command")
+	}
+
+	lines := strings.Split(strings.Replace(string(out), ",", "", -1), "\n")
+	// Last line can be empty.
+	if len(lines) > 0 && lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1]
+	}
+	// Make sure that the length of the array is at least as long as expected.
+	if len(lines) < int(m) {
+		return errors.Errorf("only %v lines returned by logcat: %s", len(lines), lines[0])
+	}
+	lines = lines[len(lines)-int(m):]
+	firstLine := strings.Split(lines[0], " ")
+
+	/*
+	  An example line is shown below:
+
+	  "05-03 09:03:45.233  2634  2634 V InputOverlayTest: MotionEvent { action=ACTION_UP,
+	    actionButton=0, id[0]=0, x[0]=362.0, y[0]=642.0, toolType[0]=TOOL_TYPE_FINGER,
+	    buttonState=0, classification=NONE, metaState=META_NUM_LOCK_ON, flags=0x1,
+	    edgeFlags=0x0, pointerCount=1, historySize=0, eventTime=39410, downTime=39359,
+	    deviceId=1, source=0x1002, displayId=0 }"
+
+	  For this test, first of all, we care about the freshness of the log, which we
+	  can extract from the second item (e.g. "09:03:45.233"). We also want to make note
+	  of the action to verify that the correct sequence of actions took place. Finally,
+	  for the first line, we care about the coordinates of the tap location, which are
+	  given by the "x[0]=" and "y[0]=" elements. We parse this line to look for or
+	  obtain all the above information.
+	*/
+
+	// Check for fresh timestamp.
+	if firstLine[1] < params.lastTimestamp {
+		return errors.New("action timestamp not fresh")
+	}
+	params.lastTimestamp = firstLine[1]
+
+	// Check that the first line has "ACTION_UP" and the last line has "ACTION_DOWN".
+	if !strings.Contains(lines[0], "ACTION_DOWN") {
+		return errors.Errorf("ACTION_DOWN not found: %s", lines[0])
+	}
+	// Press-release buttons will also have an "ACTION_MOVE" motion event.
+	if m == moveMode {
+		if !strings.Contains(lines[1], "ACTION_MOVE") {
+			return errors.Errorf("ACTION_MOVE not found: %s", lines[1])
+		}
+	}
+	if !strings.Contains(lines[int(m)-1], "ACTION_UP") {
+		return errors.Errorf("ACTION_UP not found: %s", lines[int(m)-1])
+	}
+
+	// No need to check positioning for joystick controls.
+	if m == moveMode {
+		return nil
+	}
+
+	// Get coordinate of tap reported in logcat for relative positioning.
+	newPoint, err := parsePoint(firstLine)
+	if err != nil {
+		return errors.Wrapf(err, "failed to parse for a coordinate: %s", lines[0])
+	}
+
+	// Get window bounds, if necessary
+	if err := calculateWindowContentSize(ctx, params); err != nil {
+		return errors.Wrap(err, "failed to get application window bounds")
+	}
+
+	// Check that the tapped location is close enough.
+	if err := confirmApproximateLocation(newPoint, params, heuristic); err != nil {
+		return errors.Wrap(err, "failed to confirm approximate location")
+	}
+
+	return nil
+}
+
+// parsePoint returns the x and y coordinate contained within a logcat output line.
+func parsePoint(line []string) (coords.Point, error) {
+	empty := coords.Point{}
+	xIdx := -1
+	yIdx := -1
+	for i, str := range line {
+		if strings.Contains(str, "x[0]=") {
+			xIdx = i
+		}
+		if strings.Contains(str, "y[0]=") {
+			yIdx = i
+			break
+		}
+	}
+	if xIdx < 0 {
+		return empty, errors.New("x coordinate for tap not found")
+	}
+	if yIdx < 0 {
+		return empty, errors.New("y coordinate for tap not found")
+	}
+	// Both strings "x[0]=" and "y[0]=" are 5 characters long, and we exclude them
+	// to extract the coordinate.
+	x, err := strconv.ParseFloat(line[xIdx][5:], 32)
+	if err != nil {
+		return empty, errors.Wrap(err, "failed to parse x coordinate")
+	}
+	y, err := strconv.ParseFloat(line[yIdx][5:], 32)
+	if err != nil {
+		return empty, errors.Wrap(err, "failed to parse x coordinate")
+	}
+	return coords.NewPoint(int(x), int(y)), nil
+}
+
+// calculateWindowContentSize saves the window bounds of the GIO test application in the provided
+// TestParams pointer.
+func calculateWindowContentSize(ctx context.Context, params *TestParams) error {
+	if params.windowContentSize != (coords.Point{}) {
+		return nil
+	}
+
+	loc, err := params.Activity.SurfaceBounds(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to obtain activity window bounds")
+	}
+
+	appWidth := loc.BottomRight().X - loc.TopLeft().X
+	appHeight := loc.BottomRight().Y - loc.TopLeft().Y
+	params.windowContentSize = coords.NewPoint(appWidth, appHeight)
+
+	return nil
+}
+
+// confirmApproximateLocation returns an error if the given point does not fall within the
+// approximate location in the activity given by the heuristic parameters.
+func confirmApproximateLocation(point coords.Point, params *TestParams, heuristic ButtonHeuristics) error {
+	x := int(float64(params.windowContentSize.X) * heuristic.xHeuristic)
+	y := int(float64(params.windowContentSize.Y) * heuristic.yHeuristic)
+	if point.X < (x-errorMargin) || point.X > (x+errorMargin) {
+		return errors.Errorf("x coordinate of tap (%d) not close enough to UI element on screen (%d)", point.X, x)
+	}
+	if point.Y < (y-errorMargin) || point.Y > (y+errorMargin) {
+		return errors.Errorf("y coordinate of tap (%d) not close enough to UI element on screen (%d)", point.Y, y)
+	}
+	return nil
 }
 
 // captureScreenshot takes a screenshot and saves it with the provided filename.
