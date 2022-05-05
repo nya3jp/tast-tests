3d69d87ba62ad3c2c944c3ed1a9d41f5f3b9e16b
Anastasiia N <anastasiian@google.com>
2022-05-05 19:09:52 +0000

    tast-tests: Fix LacrosMainProfileLogin test
    - Enable Lacros FRE to show the dialog with sync consent
    - Accept sync
    - Continue the test as before
    
    BUG=chromium:1319776
    TEST=tast run <ip> policy.LacrosMainProfileLogin
    
    Change-Id: I03bd101f3cef3f36622de25b55ee369d028bd31e

--- src/chromiumos/tast/local/policyutil/fixtures/lacros.go
+++ src/chromiumos/tast/local/policyutil/fixtures/lacros.go
@@ -84,7 +84,9 @@ func init() {
 				}
 				// The policyChromeFixture specifies FakeLogin, but the GAIALogin we specify
 				// here will overwrite it, since these options are applied after policyChromeFixture's options.
-				return lacrosfixt.NewConfig(lacrosfixt.ChromeOptions(chrome.GAIALogin(gaiaCreds))).Opts()
+				return lacrosfixt.NewConfig(
+					lacrosfixt.ChromeOptions(chrome.GAIALogin(gaiaCreds)),
+					lacrosfixt.EnableChromeFRE()).Opts()
 			},
 		},
 		SetUpTimeout:    chrome.LoginTimeout + 7*time.Minute,
