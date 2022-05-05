df9d4ec9fc52f98b37e57efa07347a09d6dadd18
Alvin Lee <alvinlee@google.com>
2022-05-05 15:47:37 +0000

    dlp: Remove 3P website dependency for ClipboardHistory
    Previously dlp.DataLeakPreventionRulesListClipboardHistory used
    external websites (google.com, example.com and chromium.org) as
    source and destinations to test clipboard restrictions. This CL
    modifies the clipboard test to use locally hosted websites.
    
    BUG=chromium:1293022, chromium:1315556, chromium:1322925
    TEST=tast run <ip> dlp.DataLeakPreventionRulesListClipboardHistory.*
    
    Change-Id: I55339dcac72c211d8c67d4e24f88ad5059f337ca

--- src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_clipboard_history.go
+++ src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_clipboard_history.go
@@ -6,6 +6,9 @@ package dlp
 
 import (
 	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"time"
 
 	"chromiumos/tast/common/fixture"
@@ -38,6 +41,7 @@ func init() {
 		},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline", "informational"},
+		Data:         []string{"text_1.html", "text_2.html", "editable_text_box.html"},
 		Params: []testing.Param{{
 			Name:    "ash",
 			Fixture: fixture.ChromePolicyLoggedIn,
@@ -55,12 +59,16 @@ func DataLeakPreventionRulesListClipboardHistory(ctx context.Context, s *testing
 	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	fakeDMS := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
 
-	// Reserve ten seconds for cleanup.
-	cleanupCtx := ctx
-	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
-	defer cancel()
+	allowedServer := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer allowedServer.Close()
 
-	if err := policyutil.ServeAndVerify(ctx, fakeDMS, cr, policy.StandardDLPPolicyForClipboard()); err != nil {
+	blockedServer := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer blockedServer.Close()
+
+	destServer := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer destServer.Close()
+
+	if err := policyutil.ServeAndVerify(ctx, fakeDMS, cr, policy.PopulateDLPPolicyForClipboard(blockedServer.URL, destServer.URL)); err != nil {
 		s.Fatal("Failed to serve and verify the DLP policy: ", err)
 	}
 
@@ -84,20 +92,27 @@ func DataLeakPreventionRulesListClipboardHistory(ctx context.Context, s *testing
 	for _, param := range []struct {
 		name        string
 		copyAllowed bool
-		url         string
+		sourceURL   string
 	}{
 		{
 			name:        "copyBlocked",
 			copyAllowed: false,
-			url:         "www.example.com",
+			sourceURL:   blockedServer.URL + "/text_1.html",
 		},
 		{
 			name:        "copyAllowed",
 			copyAllowed: true,
-			url:         "www.chromium.org",
+			sourceURL:   allowedServer.URL + "/text_2.html",
 		},
 	} {
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			// Reserve ten seconds for cleanup.
+			cleanupCtx := ctx
+			ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+			defer cancel()
+
+			defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
+
 			// Setup browser based on the chrome type.
 			br, closeBrowser, err := browserfixt.SetUp(ctx, s.FixtValue(), s.Param().(browser.Type))
 			if err != nil {
@@ -105,14 +120,14 @@ func DataLeakPreventionRulesListClipboardHistory(ctx context.Context, s *testing
 			}
 			defer closeBrowser(cleanupCtx)
 
-			sourceCon, err := br.NewConn(ctx, "https://"+param.url)
+			sourceConn, err := br.NewConn(ctx, param.sourceURL)
 			if err != nil {
-				s.Fatal("Failed to open page: ", err)
+				s.Fatalf("Failed to open page %q: %v", param.sourceURL, err)
 			}
-			defer sourceCon.Close()
+			defer sourceConn.Close()
 
-			if err := webutil.WaitForQuiescence(ctx, sourceCon, 10*time.Second); err != nil {
-				s.Fatalf("Failed to wait for %q to achieve quiescence: %v", param.url, err)
+			if err := webutil.WaitForQuiescence(ctx, sourceConn, 10*time.Second); err != nil {
+				s.Fatalf("Failed to wait for %q to achieve quiescence: %v", param.sourceURL, err)
 			}
 
 			if err := uiauto.Combine("copy all text from source website",
@@ -126,47 +141,27 @@ func DataLeakPreventionRulesListClipboardHistory(ctx context.Context, s *testing
 				s.Fatal("Failed to get clipboard content: ", err)
 			}
 
-			googleConn, err := br.NewConn(ctx, "https://www.google.com/?hl=en")
+			destURL := destServer.URL + "/editable_text_box.html"
+			destConn, err := br.NewConn(ctx, destURL)
 			if err != nil {
-				s.Fatal("Failed to open page: ", err)
+				s.Fatalf("Failed to open page %q: %v", destURL, err)
 			}
-			defer googleConn.Close()
+			defer destConn.Close()
 
-			if err := webutil.WaitForQuiescence(ctx, googleConn, 10*time.Second); err != nil {
-				s.Fatal("Failed to wait for google.com to achieve quiescence: ", err)
+			if err := webutil.WaitForQuiescence(ctx, destConn, 10*time.Second); err != nil {
+				s.Fatalf("Failed to wait for %q to achieve quiescence: %v", destURL, err)
 			}
-
-			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+param.name)
 
 			ui := uiauto.New(tconn)
 
-			searchNode := nodewith.Name("Search").Role(role.TextFieldWithComboBox).State(state.Editable, true).First()
-
-			if err := ui.WaitUntilExists(searchNode)(ctx); err != nil {
-				s.Fatal("Failed to find search bar: ", err)
-			}
-
-			searchNodeInfo, err := ui.Info(ctx, searchNode)
-			if err != nil {
-				s.Fatal("Error retrieving info for search node: ", err)
-			}
-
-			// If the search bar is invisible, it is probably overlaid by the Google consent banner.
-			// It does not provide usable ids, so we try to quit it with Shift+Tab, then Enter.
-			if searchNodeInfo.State[state.Invisible] {
-				s.Log("Search bar is invisible, closing consent banner")
-				if err := keyboard.Accel(ctx, "Shift+Tab+Enter"); err != nil {
-					s.Fatal("Failed to press Shift+Tab+Enter to close consent banner: ", err)
-				}
-			}
-
-			// Clicks onto the search bar, opens the clipboard history menu (Search+V) and
+			// Clicks onto the text box, opens the clipboard history menu (Search+V) and
 			// pastes the first item (Enter).
+			textBoxNode := nodewith.Name("textarea").Role(role.TextField).State(state.Editable, true).First()
 			clipboardHistoryNode := nodewith.NameStartingWith("Clipboard history").Role(role.MenuBar)
 			if err := uiauto.Combine("Pasting into search bar",
-				ui.WaitUntilExists(searchNode.Visible()),
-				ui.LeftClick(searchNode),
-				ui.WaitUntilExists(searchNode.Focused()),
+				ui.WaitUntilExists(textBoxNode.Visible()),
+				ui.LeftClick(textBoxNode),
+				ui.WaitUntilExists(textBoxNode.Focused()),
 				keyboard.AccelAction("Search+V"),
 				ui.WaitUntilExists(clipboardHistoryNode),
 				keyboard.AccelAction("Enter"),
@@ -175,7 +170,8 @@ func DataLeakPreventionRulesListClipboardHistory(ctx context.Context, s *testing
 			}
 
 			// Verify notification bubble.
-			notifError := clipboard.CheckClipboardBubble(ctx, ui, param.url)
+			parsedSourceURL, _ := url.Parse(blockedServer.URL)
+			notifError := clipboard.CheckClipboardBubble(ctx, ui, parsedSourceURL.Hostname())
 
 			if !param.copyAllowed && notifError != nil {
 				s.Error("Expected notification but found an error: ", notifError)
