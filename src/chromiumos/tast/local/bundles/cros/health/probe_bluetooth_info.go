510ded60d593e1473f1447ebcd429e533d5388ef
Byron Lee <byronlee@google.com>
2022-05-05 09:19:29 +0000

    health: Extend ProbeBluetoothInfo testing of adapter info
    BUG=b:222598143
    TEST=tast run $DUT_IP health.ProbeBluetoothInfo
    
    Change-Id: I5770472f40edf26dc12377ed358ecbd3fa44eb71

--- src/chromiumos/tast/local/bundles/cros/health/probe_bluetooth_info.go
+++ src/chromiumos/tast/local/bundles/cros/health/probe_bluetooth_info.go
@@ -53,7 +53,26 @@ func init() {
 	})
 }
 
-func validateBluetoothAdapterData(ctx context.Context, info *bluetoothInfo) error {
+func compareSlice(cands, targets []string) bool {
+	if len(cands) != len(targets) {
+		return false
+	}
+	for _, target := range targets {
+		found := false
+		for _, cand := range cands {
+			if cand == target {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func validateBluetoothAdapterData(ctx context.Context, s *testing.State, info *bluetoothInfo) error {
 	// Get Bluetooth adapter values to compare to the output of cros_healthd.
 	adapters, err := bluetooth.Adapters(ctx)
 	if err != nil {
@@ -108,27 +127,59 @@ func validateBluetoothAdapterData(ctx context.Context, info *bluetoothInfo) erro
 
 	if uuids, err := adapters[0].UUIDs(ctx); err != nil {
 		return err
-	} else if len(info.Adapters[0].UUIDs) != len(uuids) {
+	} else if !compareSlice(info.Adapters[0].UUIDs, uuids) {
 		return errors.Errorf("invalid uuids value: got %v; want %v", info.Adapters[0].UUIDs, uuids)
-	} else {
-		for _, uuid := range uuids {
-			found := false
-			for _, cand := range info.Adapters[0].UUIDs {
-				if uuid == cand {
-					found = true
-					break
-				}
-			}
-			if !found {
-				return errors.Errorf("invalid uuids value: got %v; want %v", info.Adapters[0].UUIDs, uuids)
-			}
-		}
 	}
 
 	if modalias, err := adapters[0].Modalias(ctx); err != nil {
 		return err
 	} else if info.Adapters[0].Modalias != modalias {
 		return errors.Errorf("invalid modalias value: got %v; want %v", info.Adapters[0].Modalias, modalias)
+	}
+
+	// Get AdminPolicyStatus1 interface.
+	adminPolicies, err := bluetooth.AdminPolicies(ctx)
+	if err != nil {
+		return err
+	}
+	var adminPolicy *bluetooth.AdminPolicy
+	for _, cand := range adminPolicies {
+		if cand.Path() == adapters[0].Path() {
+			adminPolicy = cand
+		}
+	}
+	if adminPolicy == nil {
+		return errors.Errorf("could not find object path %v in AdminPolicyStatus1 interface", adapters[0].Path())
+	}
+
+	if serviceAllowList, err := adminPolicy.ServiceAllowList(ctx); err != nil {
+		return err
+	} else if !compareSlice(info.Adapters[0].ServiceAllowList, serviceAllowList) {
+		return errors.Errorf("invalid serviceAllowList value: got %v; want %v", info.Adapters[0].ServiceAllowList, serviceAllowList)
+	}
+
+	// Get LEAdvertisingManager1 interface.
+	advertisings, err := bluetooth.Advertisings(ctx)
+	if err != nil {
+		return err
+	}
+	var advertising *bluetooth.Advertising
+	for _, cand := range advertisings {
+		if cand.Path() == adapters[0].Path() {
+			advertising = cand
+		}
+	}
+	if advertising == nil {
+		return errors.Errorf("could not find object path %v in LEAdvertisingManager1 interface", adapters[0].Path())
+	}
+
+	if supportedCapabilities, err := advertising.SupportedCapabilities(ctx); err != nil {
+		return err
+	} else if info.Adapters[0].SupportedCapabilities.MaxAdvLen != supportedCapabilities.MaxAdvLen ||
+		info.Adapters[0].SupportedCapabilities.MaxScnRspLen != supportedCapabilities.MaxScnRspLen ||
+		info.Adapters[0].SupportedCapabilities.MaxTxPower != supportedCapabilities.MaxTxPower ||
+		info.Adapters[0].SupportedCapabilities.MinTxPower != supportedCapabilities.MinTxPower {
+		return errors.Errorf("invalid supportedCapabilities value: got %v; want %v", info.Adapters[0].SupportedCapabilities, supportedCapabilities)
 	}
 
 	return nil
@@ -141,7 +192,7 @@ func ProbeBluetoothInfo(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to get Bluetooth telemetry info: ", err)
 	}
 
-	if err := validateBluetoothAdapterData(ctx, &info); err != nil {
+	if err := validateBluetoothAdapterData(ctx, s, &info); err != nil {
 		s.Fatalf("Failed to validate bluetooth adapter data, err [%v]", err)
 	}
 }
