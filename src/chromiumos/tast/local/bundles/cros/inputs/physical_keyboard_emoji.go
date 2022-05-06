a3b03c96983a04da7a251a037dba1669b14352f0
Jun Sheng <shengjun@google.com>
2022-05-06 18:15:48 +1000

    tast-tests: merge PK emoji tests
    Happen to see b/221789208. PhysicalKeyboardEmojiInformational has been stable for a while.
    
    https://tastboard.corp.google.com/test/inputs.PhysicalKeyboardEmojiInformational
    
    BUG=b:221789208
    TEST=None
    
    Change-Id: I42ce32b6cb5696cdb05711502dc0fc832a874606

--- src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_emoji.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_emoji.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,10 +9,16 @@ import (
 	"time"
 
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/inputs/emojipicker"
 	"chromiumos/tast/local/bundles/cros/inputs/fixture"
 	"chromiumos/tast/local/bundles/cros/inputs/pre"
 	"chromiumos/tast/local/bundles/cros/inputs/testserver"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/useractions"
+	"chromiumos/tast/local/coords"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
 )
@@ -22,23 +28,19 @@ func init() {
 		Func:         PhysicalKeyboardEmoji,
 		LacrosStatus: testing.LacrosVariantNeeded,
 		Desc:         "Checks that right click input field and select emoji with physical keyboard",
-		Contacts:     []string{"jopalmer@chromium.org", "essential-inputs-team@google.com"},
+		Contacts:     []string{"shengjun@chromium.org", "jopalmer@chromium.org", "essential-inputs-team@google.com"},
 		Attr:         []string{"group:mainline", "group:input-tools"},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      fixture.ClamshellNonVK,
-		Params: []testing.Param{
-			{
-				ExtraAttr:         []string{"group:input-tools-upstream"},
-				ExtraHardwareDeps: hwdep.D(hwdep.Model(pre.StableModels...), hwdep.SkipOnModel("kodama", "kefka")),
-			},
-			{
-				Name:      "informational",
-				ExtraAttr: []string{"informational"},
-				// Skip on grunt & zork boards due to b/213400835.
-				ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels, hwdep.SkipOnPlatform("grunt", "zork")),
-			},
-		},
-	})
+		Params: []testing.Param{{
+			ExtraAttr:         []string{"group:input-tools-upstream"},
+			ExtraHardwareDeps: hwdep.D(hwdep.Model(pre.StableModels...), hwdep.SkipOnModel("kodama", "kefka")),
+		}, {
+			Name:      "informational",
+			ExtraAttr: []string{"informational"},
+			// Skip on grunt & zork boards due to b/213400835.
+			ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels, hwdep.SkipOnPlatform("grunt", "zork")),
+		}}})
 }
 
 func PhysicalKeyboardEmoji(ctx context.Context, s *testing.State) {
@@ -53,13 +55,102 @@ func PhysicalKeyboardEmoji(ctx context.Context, s *testing.State) {
 
 	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree")
 
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer kb.Close()
+
 	its, err := testserver.Launch(ctx, cr, tconn)
 	if err != nil {
 		s.Fatal("Failed to launch inputs test server: ", err)
 	}
 	defer its.Close()
 
-	if err := its.InputEmojiWithEmojiPicker(uc, testserver.TextAreaInputField, "😂")(ctx); err != nil {
-		s.Fatal("Failed to verify emoji picker: ", err)
-	}
+	inputField := testserver.TextAreaInputField
+	inputEmoji := "😂"
+	ui := emojipicker.NewUICtx(tconn)
+
+	s.Run(ctx, "emoji_input", func(ctx context.Context, s *testing.State) {
+		defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_emoji_input")
+		if err := its.InputEmojiWithEmojiPicker(uc, inputField, inputEmoji)(ctx); err != nil {
+			s.Fatal("Failed to verify emoji picker: ", err)
+		}
+	})
+
+	// Tap ESC key to dismiss emoji picker.
+	// This test is also covered in browser test https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/views/bubble/bubble_contents_wrapper_unittest.cc;drc=7059ce9510b276afe73ce0bc389a72b58f482420;l=154.
+	// Keep this test since it is still required to complete the entire E2E test journey.
+	dismissByKeyboardAction := uiauto.UserAction(
+		"Dismiss emoji picker",
+		uiauto.Combine("dismiss emoji picker by tapping ESC key",
+			kb.AccelAction("ESC"),
+			emojipicker.WaitUntilGone(tconn),
+		),
+		uc,
+		&useractions.UserActionCfg{
+			Attributes: map[string]string{
+				useractions.AttributeTestScenario: "Dismiss emoji picker by tapping ESC key",
+				useractions.AttributeFeature:      useractions.FeatureEmojiPicker,
+			},
+		},
+	)
+
+	// Mouse click to dismiss emoji picker.
+	dismissByMouseAction := uiauto.UserAction(
+		"Dismiss emoji picker",
+		uiauto.Combine("dismiss emoji picker by mouse click",
+			func(ctx context.Context) error {
+				emojiPickerLoc, err := ui.Location(ctx, emojipicker.RootFinder)
+				if err != nil {
+					return errors.Wrap(err, "failed to get emoji picker location")
+				}
+				// Click anywhere outside emoji picker will dismiss it.
+				// Using TopRight + 50 is safe in this case.
+				clickPoint := coords.Point{
+					X: emojiPickerLoc.TopRight().X + 50,
+					Y: emojiPickerLoc.TopRight().Y,
+				}
+				return ui.MouseClickAtLocation(0, clickPoint)(ctx)
+			},
+			emojipicker.WaitUntilGone(tconn),
+		),
+		uc,
+		&useractions.UserActionCfg{
+			Attributes: map[string]string{
+				useractions.AttributeTestScenario: "Dismiss emoji picker by mouse click",
+				useractions.AttributeFeature:      useractions.FeatureEmojiPicker,
+			},
+		},
+	)
+
+	s.Run(ctx, "recently_used", func(ctx context.Context, s *testing.State) {
+		defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_recently")
+
+		if err := uiauto.Combine("validate recently used emojis",
+			its.TriggerEmojiPickerFromContextMenu(inputField),
+			// Clear recent used emojis.
+			uiauto.UserAction(
+				"Clear recently used emoji",
+				uiauto.Combine("clear recently used emoji",
+					ui.LeftClick(emojipicker.RecentUsedMenu),
+					ui.LeftClick(emojipicker.ClearRecentlyUsedButtonFinder),
+					ui.WaitUntilGone(emojipicker.RecentUsedMenu),
+					dismissByKeyboardAction,
+					// Launch emoji picker again to confirm it is not only removed from UI.
+					its.TriggerEmojiPickerFromContextMenu(inputField),
+					ui.WaitUntilGone(emojipicker.RecentUsedMenu),
+					dismissByMouseAction,
+				),
+				uc,
+				&useractions.UserActionCfg{
+					Attributes: map[string]string{
+						useractions.AttributeFeature: useractions.FeatureEmojiPicker,
+					},
+				},
+			),
+		)(ctx); err != nil {
+			s.Fatal("Failed to clear recently used emoji: ", err)
+		}
+	})
 }
