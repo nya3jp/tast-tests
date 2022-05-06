8440b0601e2fb4c6b37475da32fcd7b3adf89b97
Ahmed Fakhry <afakhry@google.com>
2022-05-05 19:00:18 -0700

    capture_selfie_cam: Tast test for selecting cameras
    BUG=chromium:1316053
    TEST=tast -verbose run <IP> wmp.CaptureSelfieCamSelection
    
    Change-Id: I5dedd1256a09e02b71890811db6028c8db8569bc

---
+++ src/chromiumos/tast/local/bundles/cros/wmp/capture_selfie_cam_selection.go
@@ -0,0 +1,52 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wmp
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         CaptureSelfieCamSelection,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks that we can select cameras from the capture settings menu",
+		Contacts: []string{
+			"afakhry@chromium.org",
+			"chromeos-wmp@google.com",
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func CaptureSelfieCamSelection(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 15*time.Second)
+	defer cancel()
+
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("CaptureModeSelfieCamera"), chrome.ExtraArgs("--use-fake-device-for-media-stream=device-count=2"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	ac := uiauto.New(tconn)
+}
