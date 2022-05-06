471cb624b8d32cdbe34cb7e2eb8058e8a3d08846
Jie Jiang <jiejiang@chromium.org>
2022-05-06 19:28:43 +0900

    network: VPNConnect uses its own shillReset fixture
    This patch adds a vpnShillReset fixture that can be used by VPNConnect.*
    tests and other VPN-related tests. Different with the original
    shillReset fixture, vpnShillReset only resets shill when the test
    failed, since VPN tests 1) do not really need a fresh shill state; 2)
    will cleanup the service when test passes. This change has two benefits:
    - Reduce required time to run the tests slightly (7m30s -> 7m for
      VPNConnect.* on octopus-kernelnext).
    - Reduce potential risk to do a shill restart which could cause
      flakiness (just a guess at the moment, see bug page for details)
    
    BUG=b:228272750
    TEST=tast run $DUT network.VPNConnect.*
    
    Change-Id: I0a20fdc059fe54615469d9d5ad967a1f4cb144f9

---
+++ src/chromiumos/tast/local/bundles/cros/network/vpn/fixture.go
@@ -0,0 +1,91 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package vpn
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/network/shill"
+	"chromiumos/tast/local/network"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name: "vpnShillReset",
+		Desc: "A fixture that ensures shill is in a default state when the test starts and resets shill after the test if it failed",
+		Contacts: []string{
+			"jiejiang@google.com",        // fixture maintainer
+			"cros-networking@google.com", // platform networking team
+		},
+		SetUpTimeout:    shill.ResetShillTimeout + 5*time.Second,
+		PostTestTimeout: shill.ResetShillTimeout + 5*time.Second,
+		TearDownTimeout: shill.ResetShillTimeout + 5*time.Second,
+		Impl:            &vpnFixture{},
+	})
+}
+
+func resetShillWithLockingHook(ctx context.Context) error {
+	// We lose connectivity along the way here, and if that races with the
+	// recover_duts network-recovery hooks, it may interrupt us. Lock the hook
+	// before shill restarted.
+	unlock, err := network.LockCheckNetworkHook(ctx)
+	if err != nil {
+		return err
+	}
+	defer unlock()
+
+	if errs := shill.ResetShill(ctx); len(errs) != 0 {
+		for _, err := range errs {
+			testing.ContextLog(ctx, "ResetShill error: ", err)
+		}
+		return errs[0]
+	}
+
+	return nil
+}
+
+type vpnFixture struct{}
+
+func (f *vpnFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	if err := resetShillWithLockingHook(ctx); err != nil {
+		s.Fatal("Failed to reset shill: ", err)
+	}
+
+	// Provides pass-through for the value yielded by the parent fixture.
+	return s.ParentValue()
+}
+
+func (f *vpnFixture) Reset(ctx context.Context) error {
+	return nil
+}
+
+func (f *vpnFixture) PreTest(ctx context.Context, s *testing.FixtTestState) {}
+
+func (f *vpnFixture) PostTest(ctx context.Context, s *testing.FixtTestState) {
+	if !s.HasError() {
+		return
+	}
+
+	testing.ContextLog(ctx, "Test failed, reseting shill")
+	if err := resetShillWithLockingHook(ctx); err != nil {
+		s.Error("Failed to reset shill in PostTest: ", err)
+	}
+}
+
+func (f *vpnFixture) TearDown(ctx context.Context, s *testing.FixtState) {
+	// Restart ui so that cryptohome unmounts all user mounts before shill is
+	// restarted so that shill does not keep the mounts open perpetually.
+	// TODO(b/205726835): Remove once the mount propagation for shill is fixed.
+	if err := upstart.RestartJob(ctx, "ui"); err != nil {
+		s.Error("Failed to restart ui: ", err)
+	}
+
+	if err := resetShillWithLockingHook(ctx); err != nil {
+		s.Error("Failed to reset shill in TearDown: ", err)
+	}
+}
