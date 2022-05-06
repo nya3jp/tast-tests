cada4d35c5831094b95a6e39ed7c0380abe0871c
Jason Stanko <jstanko@google.com>
2022-05-06 16:16:10 +0000

    cellular: Add framework for eSIM Mojo API
    This change adds library functions for the esim_manager Mojo API. It
    also adds a simple eSIM profile nickname change test.
    
    BUG=b:187885859
    TEST=cellular.MojoChangeESimNickname
    
    Change-Id: I1c15e920f3c5e7b5041bce7bedbc6b8f3b45847c

---
+++ src/chromiumos/tast/local/bundles/cros/cellular/mojo_change_esim_nickname.go
@@ -0,0 +1,73 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/cellular/esim/mojo"
+	"chromiumos/tast/local/stork"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         MojoChangeESimNickname,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Installs a new eSIM profile on the device and then changes its nickname",
+		Contacts: []string{
+			"jstanko@google.com",
+			"cros-connectivity@google.com",
+		},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_test_esim"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedInWithMojoTestEuicc",
+		Timeout:      5 * time.Minute,
+	})
+}
+
+const (
+	testName = "Test Nickname"
+)
+
+func MojoChangeESimNickname(ctx context.Context, s *testing.State) {
+	eSimMojo := s.FixtValue().(*mojo.FixtData)
+
+	activationCode, cleanupFunc, err := stork.FetchStorkProfile(ctx)
+	if err != nil {
+		s.Fatal("Failed to fetch Stork profile: ", err)
+	}
+	defer cleanupFunc(ctx)
+
+	var euicc = eSimMojo.Euicc
+
+	s.Log("Installing Stork profile with activation code: ", activationCode)
+	installResult, profile, err := euicc.InstallProfileFromActivationCode(ctx, string(activationCode), "" /*confirmationCode*/)
+	if err != nil {
+		s.Fatal("Failed to install eSIM profile via Mojo: ", err)
+	}
+	if installResult != mojo.ProfileInstallSuccess {
+		s.Fatal("eSIM install failed with error code: ", installResult)
+	}
+
+	s.Log("Installed Stork profile with ICCID: ", profile.Iccid)
+	defer profile.UninstallProfile(ctx)
+
+	s.Log("Changing profile nickname")
+	_, err = profile.SetProfileNickname(ctx, mojo.NewString16(testName))
+	if err != nil {
+		s.Fatal("Failed to set eSIM profile nickname via Mojo: ", err)
+	}
+
+	profileProperties, err := profile.GetProperties(ctx)
+	if err != nil {
+		s.Fatal("Failed to get eSIM profile properties via Mojo: ", err)
+	}
+
+	if profileProperties.Nickname.String() != testName {
+		s.Fatal("Failed to set eSIM profile nickname via Mojo")
+	}
+}
