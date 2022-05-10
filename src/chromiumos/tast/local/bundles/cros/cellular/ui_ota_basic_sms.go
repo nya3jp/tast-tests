007907e6774e39187ec2cfe569cd27fe88ac17e7
Srikanth Oruganti <srikanthkumar@google.com>
2022-05-10 13:29:59 -0700

    [tast] Add OTA SMS tests in tast
    Adds cellular ui helper and SMS tast tests
    
    BUG=b:215571987
    TEST= tast --verbose run localhost:2228 cellular.UIOtaBasicSms
    
    Change-Id: Ibb49231107112c57bb4b1252ab2528eb30ad3b5c

---
+++ src/chromiumos/tast/local/bundles/cros/cellular/ui_ota_basic_sms.go
@@ -0,0 +1,146 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/mmconst"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/cellular"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/modemmanager"
+	"chromiumos/tast/testing"
+)
+
+// This test is only run on the cellular_single_active group. All boards in that group
+// provide the Modem.SimSlots property and have at least one provisioned SIM slot.
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         UIOtaBasicSms,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verifies that MT SMS is received appears as notificatoin on UI",
+		Contacts:     []string{"srikanthkumar@google.com", "chromeos-cellular-team@google.com"},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_active"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      10 * time.Minute,
+		Vars: []string{
+			"ui.oac_username",
+			"ui.oac_password",
+		},
+	})
+}
+
+// UIOtaBasicSms validates MT SMS, uses google voice to send SMS.
+func UIOtaBasicSms(ctx context.Context, s *testing.State) {
+
+	/* a) Check cellular connection and get mobile number on dut
+	   b) Create and send SMS on google voice ui from chrome web interface
+	   c) Check UI notifications for SMS, verify SMS content
+	   d) Clear received SMS notification
+	*/
+
+	messageToSend := "Hello " + time.Now().Format("01-02-2022 16:10:10")
+
+	helper, err := cellular.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create cellular.Helper: ", err)
+	}
+
+	// Ensure that a Cellular Service was created.
+	if _, err := helper.FindService(ctx); err != nil {
+		s.Fatal("Unable to find Cellular Service: ", err)
+	}
+
+	// Read modem property OwnNumber.
+	modem, err := modemmanager.NewModem(ctx)
+	if err != nil {
+		s.Fatal("Failed to create modem: ", err)
+	}
+
+	props, err := modem.GetProperties(ctx)
+	if err != nil {
+		s.Fatal("Failed to call GetProperties on modem: ", err)
+	}
+
+	value, err := props.Get(mmconst.ModemPropertyOwnNumbers)
+	var phoneNumbers [2]string
+	phoneNumbers[0] = "918317507407"
+	if value != nil {
+		phoneNumber, ok := value.([]string)
+		if !ok {
+			s.Fatal("Empty ownnumbers property: ", err)
+		}
+		phoneNumbers[0] = phoneNumber[0]
+	}
+
+	s.Logf("Phone number: %s to send message: %s", phoneNumbers[0], messageToSend)
+
+	// Create cleanup context to ensure UI tree dumps correctly.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	uiHelper, err := cellular.NewUIHelper(ctx, s.RequiredVar("ui.oac_username"), s.RequiredVar("ui.oac_password"))
+
+	if err != nil {
+		faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, uiHelper.Tconn)
+		s.Fatal("Failed to create cellular.NewUiHelper: ", err)
+	}
+
+	s.Log("open google voice web page")
+
+	// Keyboard to input key inputs.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer kb.Close()
+
+	// Create gv MO sms using google voice configured with owned test accounts.
+	gConn, err := uiHelper.GoogleVoiceLogin(ctx)
+	if err != nil {
+		s.Fatal("Failed to open Google voice website: ", err)
+	}
+	defer gConn.Close()
+	defer gConn.CloseTarget(cleanupCtx)
+
+	if gConn == nil {
+		s.Fatal("Could not create new chrome tab")
+	}
+
+	s.Log("After finding messages tab")
+	err = uiHelper.SendMessage(ctx, phoneNumbers[0], messageToSend)
+	if err != nil {
+		faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, uiHelper.Tconn)
+		s.Fatal("Failed to send message: ", err)
+	}
+
+	s.Log("Check for SMS message")
+	err = uiHelper.ValidateMessage(ctx, messageToSend, phoneNumbers[0])
+	if err != nil {
+		faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, uiHelper.Tconn)
+		s.Fatal("Failed validation of message: ", err)
+	}
+
+	alertDialog := nodewith.Role(role.AlertDialog).ClassName("MessagePopupView").Onscreen()
+	// Click on alert dialog to close.
+	if err := uiauto.Combine("Click on alert dialog",
+		uiHelper.UI.WaitUntilExists(alertDialog),
+		uiHelper.UIHandler.Click(alertDialog),
+		kb.AccelAction("Ctrl+X"),
+		uiHelper.UI.WaitUntilGone(alertDialog),
+	)(ctx); err != nil {
+		faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, uiHelper.Tconn)
+		s.Fatal("Failed to click on notification  dialog: ", err)
+	}
+
+}
