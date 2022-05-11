84ef4ee9a716da9a9600686709724cf962b39f21
Momoko Hattori <momohatt@google.com>
2022-05-11 18:14:48 +0900

    arc: Add arc.ExternalStorageDisabled
    This CL adds a tast test arc.ExternalStorageDisabled to ensure that
    the enterprise policy ExternalStorageDisabled is correctly applied for
    ARC. In particular, it checks the following two points when the policy
    is set to true:
    1. MyFiles is mounted on ARC.
    2. removable media is not mounted on ARC.
    The bug associated to the first part (b/217531658) was fixed in
    crrev.com/c/3650484. Also, crrev.com/c/3655844 added an additional
    policy check in ArcVolumeMounterBridge in preparation for the second
    part.
    
    BUG=b:217531658, b:233308595
    TEST=tast run <DUT> arc.EnableExternalStorage*
    TEST=tast run <DUT> arc.ExternalStorageDisabled*
    TEST=tast run <DUT> arc.OptinManaged*
    TEST=tast run <DUT> arc.RemovableMedia*
    TEST=Check that `tast run <DUT> arc.ExternalStorageDisabled.vm` randomly
         fails with crrev.com/c/3650484 reverted
    
    Change-Id: I89fa2811ffcf5882aae66f2af2dd10dd2b5ea5cc

---
+++ src/chromiumos/tast/local/bundles/cros/arc/external_storage_disabled.go
@@ -0,0 +1,105 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/removablemedia"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ExternalStorageDisabled,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks that ExternalStorageDisabled policy is correctly applied to ARC",
+		Contacts: []string{
+			"arc-storage@google.com",
+			"momohatt@google.com",
+		},
+		Attr:         []string{"group:mainline", "group:arc-functional"},
+		VarDeps:      []string{"arc.managedAccountPool"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			ExtraAttr:         []string{"informational"},
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraAttr:         []string{"informational"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+		Timeout: 6 * time.Minute,
+	})
+}
+
+func ExternalStorageDisabled(ctx context.Context, s *testing.State) {
+	// Actual username and password are read from vars/arc.yaml.
+	creds, err := chrome.PickRandomCreds(s.RequiredVar("arc.managedAccountPool"))
+	if err != nil {
+		s.Fatal("Failed to get login creds: ", err)
+	}
+
+	policies := []policy.Policy{
+		&policy.ArcEnabled{Val: true, Stat: policy.StatusSet},
+		&policy.ExternalStorageDisabled{Val: true, Stat: policy.StatusSet},
+	}
+
+	fdms, err := policyutil.SetUpFakePolicyServer(ctx, s.OutDir(), creds.User, policies)
+	if err != nil {
+		s.Fatal("Failed to set up fake policy server: ", err)
+	}
+	defer fdms.Stop(ctx)
+
+	// If fdms forces ARC opt-in, then ARC opt-in will start in background, right after chrome is created.
+	cr, err := chrome.New(ctx,
+		chrome.GAIALogin(creds),
+		chrome.DMSPolicy(fdms.URL),
+		chrome.ARCSupported(),
+		chrome.ExtraArgs(arc.DisableSyncFlags()...))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	a, err := arc.New(ctx, s.OutDir())
+	if err != nil {
+		s.Fatal("Failed to start ARC: ", err)
+	}
+	defer a.Close(ctx)
+
+	s.Log("Performing MyFiles sharing check")
+
+	if err := arc.WaitForARCMyFilesVolumeMount(ctx, a); err != nil {
+		s.Fatal("Failed to wait for MyFiles to be mounted in ARC: ", err)
+	}
+
+	const (
+		imageSize = 64 * 1024 * 1024
+		diskName  = "MyDisk"
+	)
+
+	// Create a filesystem image and mount it on the host side. This should work
+	// even when the ExternalStorageDisabled policy is set to true since we
+	// directly ask CrosDisks to mount it without having it check the policy
+	// with Chrome here.
+	_, cleanupFunc, err := removablemedia.CreateAndMountImage(ctx, imageSize, diskName)
+	if err != nil {
+		s.Fatal("Failed to set up image: ", err)
+	}
+	defer cleanupFunc(ctx)
+
+	s.Log("Performing removable media sharing check")
+
+	// Check that the image is not mounted on ARC.
+	if err := arc.WaitForARCRemovableMediaVolumeMount(ctx, a); err == nil {
+		s.Fatal("The volume is unexpectedly mounted on ARC")
+	}
+}
