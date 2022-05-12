dce452af9d05f2bb84a5d61aa629a1f5be0acdd4
Jerry Liu <pzliu@google.com>
2022-05-11 17:28:07 -0700

    tast-tests: Add test for opening personalization hub from launcher
    Add a tast test to search for personalization hub and wallpaper app from
    launcher.
    
    BUG=b:231343549
    TEST=tast run <DUT> personalization.OpenPersonalizationHubFromLauncher
    
    Change-Id: Id590bf0d11a3418a1e23de64deead3bc2195b64b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3642955
    Reviewed-by: David Stevens <stevensd@chromium.org>
    Tested-by: Jerry Liu <pzliu@google.com>
    Reviewed-by: Thuong Phan <thuongphan@chromium.org>
    Commit-Queue: Jerry Liu <pzliu@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/personalization/open_personalization_hub_from_launcher.go
@@ -0,0 +1,74 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package personalization
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/personalization"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         OpenPersonalizationHubFromLauncher,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Test opening personalization hub app by searching in launcher",
+		Contacts: []string{
+			"pzliu@google.com",
+			"chromeos-sw-engprod@google.com",
+			"assistive-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		VarDeps:      []string{"ambient.username", "ambient.password"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "personalizationDefault",
+	})
+}
+
+func OpenPersonalizationHubFromLauncher(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	// The test has a dependency of network speed, so we give uiauto.Context ample
+	// time to wait for nodes to load.
+	ui := uiauto.New(tconn).WithTimeout(30 * time.Second)
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer kb.Close()
+
+	if err := uiauto.Combine("open wallpaper app by searching in launcher",
+		personalization.SearchForAppInLauncher("change wallpaper", "Change wallpaper", kb, ui),
+		ui.WaitUntilExists(nodewith.Role(role.Button).NameContaining("Wallpaper").HasClass("breadcrumb")),
+	)(ctx); err != nil {
+		s.Fatal("Failed to open wallpaper app from launcher: ", err)
+	}
+
+	if err := uiauto.Combine("open personalization hub by searching in launcher",
+		personalization.SearchForAppInLauncher("personalization hub", "Personalization", kb, ui),
+		ui.Exists(nodewith.Role(role.Window).NameContaining("Personalization").First()),
+	)(ctx); err != nil {
+		s.Fatal("Failed to open personalization hub from launcher: ", err)
+	}
+}
