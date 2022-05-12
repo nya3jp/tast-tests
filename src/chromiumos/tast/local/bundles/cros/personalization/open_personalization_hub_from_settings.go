f4bd3167679a84b5e05ec959818f2acb1d06ae3d
Jerry Liu <pzliu@google.com>
2022-05-12 16:14:59 -0700

    tast-tests: Add test for opening personalization hub from settings
    Add a tast test to open personalization hub from settings in two ways:
    1. Click "Personalization" - "Personalize your device".
    2. Search for personalization hub in settings app search bar.
    
    BUG=b:232143706
    TEST=tast run <DUT> personalization.OpenPersonalizationHubFromSettings
    
    Change-Id: I3f685819dbf910082253618cf91da2bc346a5ba6

---
+++ src/chromiumos/tast/local/bundles/cros/personalization/open_personalization_hub_from_settings.go
@@ -0,0 +1,78 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package personalization
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/personalization"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         OpenPersonalizationHubFromSettings,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Test opening personalization hub app from Settings app",
+		Contacts: []string{
+			"pzliu@google.com",
+			"chromeos-sw-engprod@google.com",
+			"assistive-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "personalizationDefault",
+	})
+}
+
+func OpenPersonalizationHubFromSettings(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	// The test has a dependency of network speed, so we give uiauto.Context ample
+	// time to wait for nodes to load.
+	ui := uiauto.New(tconn).WithTimeout(30 * time.Second)
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer kb.Close()
+
+	if err := uiauto.Combine("open personalization hub from settings",
+		personalization.SearchForAppInLauncher("settings", "Settings, Installed App", kb, ui),
+		ui.LeftClick(nodewith.Role(role.Link).NameContaining("Personalization").HasClass("item")),
+		ui.LeftClick(nodewith.Role(role.Link).NameContaining("Personalize your device").First()),
+		ui.WaitUntilExists(nodewith.Role(role.Window).NameContaining("Personalization").First()),
+	)(ctx); err != nil {
+		s.Fatal("Failed to open personalization hub from settings: ", err)
+	}
+
+	if err := uiauto.Combine("open personalization hub by searching in settings",
+		personalization.SearchForAppInLauncher("settings", "Settings, Installed App", kb, ui),
+		ui.WaitUntilExists(nodewith.Role(role.TextField).HasClass("Textfield")),
+		kb.TypeAction("personalization"),
+		kb.AccelAction("Enter"),
+		ui.WaitUntilExists(nodewith.Role(role.Window).NameContaining("Personalization").First()),
+	)(ctx); err != nil {
+		s.Fatal("Failed to open personalization hub by searching in settings: ", err)
+	}
+}
