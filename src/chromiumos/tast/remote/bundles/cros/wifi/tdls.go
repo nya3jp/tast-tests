85d25599b1025704fefaa50669c1d0c2b0c126a9
Jacek Siuda <jck@semihalf.com>
2022-05-13 18:29:52 +0000

    wifi: TDLS Setup/Teardown test
    Add TDLS Setup/Teardown test to the TDLS test cases.
    First, a successful TDLS Setup is verified.
    Then, ICMP ping is sent to check connection and Transmitter/Receiver
    addresses are verified to make sure they match Source/Destination.
    Finally, TDLS Teardown is tested.
    
    BUG=b:225176836
    TEST=tast run -companiondut=cd1:$PEER_HOST $DUT wifi.TDLS
    
    Cq-Depend: chromium:3655483, chromium:3655484
    Change-Id: I52eddf21d104691766b7c12e12e22035e8bf54f7

--- src/chromiumos/tast/remote/bundles/cros/wifi/tdls.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/tdls.go
@@ -23,6 +23,7 @@ import (
 )
 
 type tdlsPeer struct {
+	dutIdx int
 	conn   *ssh.Conn
 	ifName string
 	mac    net.HardwareAddr
@@ -60,6 +61,13 @@ func init() {
 					name:     "Discover: passive",
 					testFunc: tdlsDiscover,
 					reverse:  true,
+				}, {
+					name:     "Setup/Teardown: active",
+					testFunc: tdlsSetupTeardown,
+				}, {
+					name:     "Setup/Teardown: passive",
+					testFunc: tdlsSetupTeardown,
+					reverse:  true,
 				}},
 			},
 		}})
@@ -75,6 +83,80 @@ func tdlsDiscover(ctx context.Context, tc *tdlsTestcase, peer1, peer2 tdlsPeer) 
 	err := r.TDLSCmd(ctx, "tdls_discover", peer2.mac.String())
 	if err != nil {
 		return errors.Wrap(err, "failed TDLS Discover")
+	}
+	testing.ContextLog(ctx, "Success")
+	return nil
+}
+
+// tdlsSetupTeardown tests support for TDLS Setup and Teardown messages.
+func tdlsSetupTeardown(ctx context.Context, tc *tdlsTestcase, peer1, peer2 tdlsPeer) error {
+	// (Sub)Test steps:
+	// 1. Run control Ping to make sure the traffic passes.
+	// 2. Start packet capture.
+	// 3. Run TDLS Setup (via wpa_cli), make sure the response is `OK`.
+	//    (wpa_cli responds OK only after successful negotiation).
+	// 4. Run Ping.
+	// 5. Run TDLS Teardown (via wpa_cli), make sure the response is `OK`.
+	// 6. Make sure captured ICMP packets went through TDLS tunnel (TA/RA==SA/DA).
+	testing.ContextLogf(ctx, "Peer 1 MAC: %s", peer1.mac.String())
+	testing.ContextLogf(ctx, "Peer 2 MAC: %s", peer2.mac.String())
+	testing.ContextLogf(ctx, "Peer 2 IP: %s", peer2.ip.String())
+	freqOpts, err := tc.ap.Config().PcapFreqOptions()
+	if err != nil {
+		return errors.Wrap(err, "failed to get Freq Opts")
+	}
+
+	result, err := tc.tf.PingFromSpecificDUT(ctx, peer1.dutIdx, peer2.ip.String())
+	if err != nil {
+		return err
+	}
+	if result.Received == 0 {
+		return errors.New("no traffic passed through in initial test")
+	}
+	if result.Sent != result.Received {
+		testing.ContextLogf(ctx, "WARNING: packets lost during transmission: %v<%v", result.Received, result.Sent)
+	}
+
+	// Run ping and check Receiver address matches Destination (packets in tunnel).
+	pcapPath, err := wifiutil.CollectPcapForAction(ctx, tc.pcap, wifiutil.UniqueAPName(), tc.ap.Config().Channel, freqOpts,
+		func(ctx context.Context) error {
+			// Setup TDLS tunnel
+			r := wpacli.NewRunner(&cmd.RemoteCmdRunner{Host: peer1.conn})
+			err := r.TDLSCmd(ctx, "tdls_setup", peer2.mac.String())
+			if err != nil {
+				return errors.Wrap(err, "failed TDLS Setup")
+			}
+			testing.ContextLog(ctx, "Setup success")
+
+			result, err := tc.tf.PingFromSpecificDUT(ctx, peer1.dutIdx, peer2.ip.String())
+			if err != nil {
+				return err
+			}
+			if result.Received == 0 {
+				return errors.New("no traffic passed through")
+			}
+			if result.Sent != result.Received {
+				testing.ContextLogf(ctx, "WARNING: packets lost during transmission: %v<%v", result.Received, result.Sent)
+			}
+			// Teardown the tunnel.
+			err = r.TDLSCmd(ctx, "tdls_teardown", peer2.mac.String())
+			if err != nil {
+				return errors.Wrap(err, "failed TDLS Teardown")
+			}
+			testing.ContextLog(ctx, "Teardown success")
+			return nil
+		})
+	if err != nil {
+		return errors.Wrap(err, "failed to collect pcap or perform action")
+	}
+
+	pkts, err := wifiutil.FindNonTDLSPackets(pcapPath, []net.HardwareAddr{peer1.mac, peer2.mac})
+	if err != nil {
+		return errors.Wrap(err, "failed to fiter packets")
+	} else if len(pkts) > 0 {
+		// Better to log offending packets once than to return them with error which gets printed multiple times.
+		testing.ContextLogf(ctx, "Found invalid packets:%s", wifiutil.DumpPkts(pkts))
+		return errors.New("invalid packets spotted")
 	}
 	testing.ContextLog(ctx, "Success")
 	return nil
@@ -125,6 +207,7 @@ func TDLS(ctx context.Context, s *testing.State) {
 	// Connect DUTs.
 	for i := 0; i < 2; i++ {
 		dutIdx := i // Work around the closure limitations.
+		tdlsPeers[i].dutIdx = i
 
 		// Check TDLS support. If one of the devices does not support it,
 		// there's no point in continuing connection.
