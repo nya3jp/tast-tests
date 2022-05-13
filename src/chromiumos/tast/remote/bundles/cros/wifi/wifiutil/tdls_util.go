b73bb22cf581732a885347e35f755f0b99b6b63e
Jacek Siuda <jck@semihalf.com>
2022-05-13 18:29:52 +0000

    wifi: Add TDLS Setup/Teardown test
    Add TDLS Setup/Teardown test to the TDLS test cases.
    First, a successful TDLS Setup is verified.
    Then, ICMP ping is sent to check connection and Transmitter/Receiver
    addresses are verified to make sure they match Source/Destination.
    Finally, TDLS Teardown is tested.
    
    BUG=b:225176836
    TEST=tast run -companiondut=cd1:$PEER_HOST $DUT wifi.TDLS
    TEST=tast unit test passed
    
    Cq-Depend: chromium:3655483, chromium:3655484
    Change-Id: I52eddf21d104691766b7c12e12e22035e8bf54f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3648187
    Reviewed-by: Arowa Suliman <arowa@chromium.org>
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Tested-by: Jacek Siuda <jsiuda@google.com>
    Commit-Queue: Jacek Siuda <jsiuda@google.com>

--- src/chromiumos/tast/remote/bundles/cros/wifi/wifiutil/tdls_util.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/wifiutil/tdls_util.go
@@ -5,11 +5,18 @@
 package wifiutil
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 
 	"chromiumos/tast/common/network/iw"
 	"chromiumos/tast/errors"
 	remoteiw "chromiumos/tast/remote/network/iw"
+	"chromiumos/tast/remote/wificell/pcap"
 	"chromiumos/tast/ssh"
 )
 
@@ -40,4 +47,74 @@ func CheckTDLSSupport(ctx context.Context, conn *ssh.Conn) error {
 func Scan(ctx context.Context, conn *ssh.Conn, ifName, ssid string) error {
 	_, err := remoteiw.NewRemoteRunner(conn).TimedScan(ctx, ifName, nil, []string{ssid})
 	return err
+}
+
+func macEqual(a1, a2 net.HardwareAddr) bool {
+	return bytes.Compare(a1, a2) == 0
+}
+
+type flagPair struct{ f1, f2 bool }
+
+func dstAddress(dot11 *layers.Dot11) net.HardwareAddr {
+	// Destination address is contextual, depending on the contents of FromDS/ToDS flags.
+	addressMatrix := map[flagPair]net.HardwareAddr{
+		{false, false}: dot11.Address1, {false, true}: dot11.Address3,
+		{true, false}: dot11.Address1, {true, true}: dot11.Address3}
+	return addressMatrix[flagPair{dot11.Flags.FromDS(), dot11.Flags.ToDS()}]
+}
+
+func srcAddress(dot11 *layers.Dot11) net.HardwareAddr {
+	// Source address is contextual, depending on the contents of FromDS/ToDS flags.
+	addressMatrix := map[flagPair]net.HardwareAddr{
+		{false, false}: dot11.Address2, {false, true}: dot11.Address2,
+		{true, false}: dot11.Address3, {true, true}: dot11.Address4}
+	return addressMatrix[flagPair{dot11.Flags.FromDS(), dot11.Flags.ToDS()}]
+}
+
+// filterTrnsRecv filters packets that have given src/dst addresses, but different Transmitter/Receiver addresses.
+func filterTrnsRecv(dot11 *layers.Dot11, src, dst net.HardwareAddr) bool {
+	if !macEqual(dstAddress(dot11), dst) || !macEqual(srcAddress(dot11), src) {
+		// These packets are not interesting.
+		return false
+	}
+	if !macEqual(dot11.Address1, dst) || !macEqual(dot11.Address2, src) {
+		// If Transmitter/Receiver is different than src/dst, we want to see this packet.
+		return true
+	}
+	// Correct packet, filter out.
+	return false
+}
+
+// FindNonTDLSPackets finds ICMP packets that were not sent through TDLS.
+func FindNonTDLSPackets(pcapPath string, addrs []net.HardwareAddr) ([]gopacket.Packet, error) {
+	if len(addrs) != 2 {
+		return nil, errors.New("Needs exactly two addresses")
+	}
+	filters := []pcap.Filter{
+		pcap.RejectLowSignal(),
+		pcap.Dot11FCSValid(),
+		pcap.TypeFilter(layers.LayerTypeICMPv4,
+			func(layer gopacket.Layer) bool { return true }),
+		pcap.TypeFilter(
+			layers.LayerTypeDot11,
+			func(layer gopacket.Layer) bool {
+				dot11 := layer.(*layers.Dot11)
+				if dot11.Type.MainType() != layers.Dot11TypeData {
+					return false
+				}
+				return filterTrnsRecv(dot11, addrs[0], addrs[1]) ||
+					filterTrnsRecv(dot11, addrs[1], addrs[0])
+			},
+		),
+	}
+	return pcap.ReadPackets(pcapPath, filters...)
+}
+
+// DumpPkts dissects slice of packets into a string.
+func DumpPkts(pkts []gopacket.Packet) string {
+	var ret string
+	for i, pkt := range pkts {
+		ret = fmt.Sprintf("%s%d: %s\n", ret, i, pkt.Dump())
+	}
+	return ret
 }
