ef3b7b27d492b9dcd51e0db62265acd36014fc8a
pjlee <pjlee@google.com>
2022-05-13 16:37:26 +0000

    arc/gio: Refactor GIO launch test
    With the addition of the educational dialog, there needs to be an additional click at the beginning of the action sequence to click through the input overlay educational dialog pop-up. In addition, due to b/231097154, the educational dialog screen causes uiauto to fail, so the entire launch detection test must be refactored to use ACUITI instead.
    
    BUG=b:229156145
    TEST=tast run $DUT arc.InputOverlayLaunch*
    TEST=tast unit test passed.
    
    Change-Id: I5cc22a91dcb19c530a9c4db0bd9bcdb208f335f4

--- src/chromiumos/tast/local/bundles/cros/arc/input_overlay_launch.go
+++ src/chromiumos/tast/local/bundles/cros/arc/input_overlay_launch.go
@@ -12,6 +12,7 @@ import (
 	"chromiumos/tast/local/bundles/cros/arc/testutil"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/uidetection"
 	"chromiumos/tast/testing"
 )
 
@@ -23,7 +24,11 @@ func init() {
 		Contacts:     []string{"pjlee@google.com", "cuicuiruan@google.com", "arc-app-dev@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
-		Fixture:      "arcBootedWithInputOverlay",
+		Data: []string{"input-overlay-top-button.png",
+			"input-overlay-bottom-button.png",
+			"input-overlay-menu.png",
+			"input-overlay-joystick.png"},
+		Fixture: "arcBootedWithInputOverlay",
 		Params: []testing.Param{
 			{
 				ExtraSoftwareDeps: []string{"android_p"},
@@ -39,18 +44,19 @@ func InputOverlayLaunch(ctx context.Context, s *testing.State) {
 	testutil.SetupTestApp(ctx, s, func(params testutil.TestParams) error {
 		// Start up UIAutomator.
 		ui := uiauto.New(params.TestConn).WithTimeout(time.Minute)
+		// Start up ACUITI.
+		uda := uidetection.NewDefault(params.TestConn).WithOptions(uidetection.Retries(3)).WithTimeout(time.Minute)
 
 		if err := uiauto.Combine("Find gaming input overlay UI elements",
+			// Tap educational dialog.
+			ui.LeftClick(nodewith.Name("Got it").HasClass("LabelButtonLabel")),
 			// Find input overlay game control.
-			ui.WaitUntilExists(nodewith.ClassName("ImageButton")),
+			uda.WaitUntilExists(uidetection.CustomIcon(s.DataPath("input-overlay-menu.png"))),
 			// Find input overlay tap buttons.
-			ui.WaitUntilExists(nodewith.Name("m")),
-			ui.WaitUntilExists(nodewith.Name("n")),
+			uda.WaitUntilExists(uidetection.CustomIcon(s.DataPath("input-overlay-top-button.png"))),
+			uda.WaitUntilExists(uidetection.CustomIcon(s.DataPath("input-overlay-bottom-button.png"))),
 			// Find input overlay joystick buttons.
-			ui.WaitUntilExists(nodewith.Name("w")),
-			ui.WaitUntilExists(nodewith.Name("d")),
-			ui.WaitUntilExists(nodewith.Name("s")),
-			ui.WaitUntilExists(nodewith.Name("a")),
+			uda.WaitUntilExists(uidetection.CustomIcon(s.DataPath("input-overlay-joystick.png"))),
 		)(ctx); err != nil {
 			return errors.Wrap(err, "one or more items not loaded")
 		}
