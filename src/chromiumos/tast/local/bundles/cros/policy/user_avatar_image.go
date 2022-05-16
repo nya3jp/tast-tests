2447ff4c0cc005859a9fb269ac1104b072a036cd
Jerry Liu <pzliu@google.com>
2022-05-16 15:23:31 -0700

    tast-tests: Turn PersonalizationHub flag on by default
    Personalization Hub will be launched in M104. We need to make sure all
    the existing tast tests are compatible with this change.
    
    BUG=b:232144869
    TEST=CQ all tast tests should pass
    
    Change-Id: Ie3d5d64baf1582487ae0a7a4a156296641708feb

--- src/chromiumos/tast/local/bundles/cros/policy/user_avatar_image.go
+++ src/chromiumos/tast/local/bundles/cros/policy/user_avatar_image.go
@@ -45,7 +45,7 @@ func init() {
 		},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline"},
-		Fixture:      fixture.ChromePolicyLoggedIn,
+		Fixture:      fixture.ChromePolicyLoggedInWithoutPersonalizationHub,
 		Data:         []string{"user_avatar_image.jpeg"},
 	})
 }
