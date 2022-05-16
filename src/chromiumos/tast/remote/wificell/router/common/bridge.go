7b89e4afd1b8c05f98649eac63a05265b3beca76
Jared Bennett <jaredbennett@google.com>
2022-05-16 14:39:02 -0700

    tast-tests: legacy bridge and veth support refactored to common
    This change refactors the logic around managing bridges and
    veths out of the legacy router implementation into the common
    package so that it may be reused in other router
    implementations, most notably OpenWrt.
    
    BUG=b:211517577
    TEST=Successfully ran tast test wifi.RoamFT.psk, which uses the
    bridge and veth support, on a wificell with a legacy router.
    
    Change-Id: Ieafa5492ef5b848d3281fe178192bceb0c29c128

---
+++ src/chromiumos/tast/remote/wificell/router/common/bridge.go
@@ -0,0 +1,41 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package common
+
+import (
+	"context"
+	"fmt"
+
+	"chromiumos/tast/common/network/ip"
+	"chromiumos/tast/remote/wificell/wifiutil"
+	"chromiumos/tast/testing"
+)
+
+// BridgePrefix is the prefix for the bridge interface.
+const BridgePrefix = "tastbr"
+
+// NewBridge returns a bridge name for tests to use. Note that the caller is responsible to call ReleaseBridge.
+func NewBridge(ctx context.Context, ipr *ip.Runner, bridgeID int) (string, error) {
+	br := fmt.Sprintf("%s%d", BridgePrefix, bridgeID)
+	if err := ipr.AddLink(ctx, br, "bridge"); err != nil {
+		return "", err
+	}
+	if err := ipr.SetLinkUp(ctx, br); err != nil {
+		if err := ipr.DeleteLink(ctx, br); err != nil {
+			testing.ContextLog(ctx, "Failed to delete bridge while NewBridge has failed: ", err)
+		}
+		return "", err
+	}
+	return br, nil
+}
+
+// ReleaseBridge releases the bridge.
+func ReleaseBridge(ctx context.Context, ipr *ip.Runner, br string) error {
+	var firstErr error
+	wifiutil.CollectFirstErr(ctx, &firstErr, ipr.FlushIP(ctx, br))
+	wifiutil.CollectFirstErr(ctx, &firstErr, ipr.SetLinkDown(ctx, br))
+	wifiutil.CollectFirstErr(ctx, &firstErr, ipr.DeleteLink(ctx, br))
+	return firstErr
+}
