b41741e6ce76f33d817af9e55e0d48aefbfe8960
Terry Cheong <htcheong@google.com>
2022-05-16 04:52:33 +0000

    quicksettings: remove keyboard after MicEnabled is finished
    The new keyboard in MicEnabled has some conflict with
    FocusMicGainSlider, defer closing the keyboard in MicEnabled should fix the issue.
    
    BUG=b:228933838
    TEST=tast run <DUT> audio.UIInput.gain
    
    Change-Id: Ia9d660546bf45249a65c93851539f7fd6773fb1e

--- src/chromiumos/tast/local/chrome/uiauto/quicksettings/quicksettings.go
+++ src/chromiumos/tast/local/chrome/uiauto/quicksettings/quicksettings.go
@@ -597,6 +597,7 @@ func MicEnabled(ctx context.Context, tconn *chrome.TestConn) (bool, error) {
 	ui := uiauto.New(tconn)
 	// Scroll the mic toggle into view.
 	kb, err := input.Keyboard(ctx)
+	defer kb.Close()
 	if err != nil {
 		return false, errors.Wrap(err, "failed to setup keyboard")
 	}
