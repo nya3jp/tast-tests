ecce7d1f4c3bddb5a768c5a3a9358567f7002a18
Paul Moy <pmoy@chromium.org>
2022-05-16 16:55:47 -0600

    scanapp: Add HardwareAllCombinations
    Add a new test which tests each combination of a scanner's supported
    color modes, page sizes and resolutions.
    
    Modularize the underlying logic of the existing randomized combination
    test so that it can be shared between the existing test, new test and
    forthcoming critical combinations test.
    
    BUG=b:195064784
    TEST=tast run --build $DUT_IP scanapp.Hardware*
    
    Change-Id: I7373a34845c0132280d666dcca658c2656cfeafd

---
+++ src/chromiumos/tast/local/bundles/cros/scanapp/hardware_all_combinations.go
@@ -0,0 +1,77 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package scanapp
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/scanapp/scanning"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HardwareAllCombinations,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Tests that the Scan app can be used on real hardware with all settings combinations",
+		Contacts: []string{
+			"cros-peripherals@google.com",
+			"project-bolton@google.com",
+		},
+		Attr: []string{
+			"group:paper-io",
+			"paper-io_scanning",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      10 * time.Minute,
+		Fixture:      "chromeLoggedIn",
+		Params: []testing.Param{
+			// Format for test cases is as follows:
+			// Name: manufacturer_model
+			// Val: scanner descriptor file
+			// ExtraData: scanner descriptor file
+			{
+				//MFP in lab
+				Name:      "brother_dcp_l2550dw",
+				Val:       "brother_dcp_l2550dw_descriptor.json",
+				ExtraData: []string{"brother_dcp_l2550dw_descriptor.json"},
+				ExtraAttr: []string{"paper-io_mfp_printscan"},
+			}, {
+				//MFP in lab
+				Name:      "hp_laserjet_pro_m478",
+				Val:       "hp_laserjet_pro_m478_descriptor.json",
+				ExtraData: []string{"hp_laserjet_pro_m478_descriptor.json"},
+				ExtraAttr: []string{"paper-io_mfp_printscan"},
+			}, {
+				//MFP in lab
+				Name:      "sharp_mx_b467f",
+				Val:       "sharp_mx_b467f_descriptor.json",
+				ExtraData: []string{"sharp_mx_b467f_descriptor.json"},
+				ExtraAttr: []string{"paper-io_mfp_printscan"},
+			},
+		},
+	})
+}
+
+func HardwareAllCombinations(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	fileContents, err := ioutil.ReadFile(s.DataPath(s.Param().(string)))
+	if err != nil {
+		s.Fatal("Unable to read scanner descriptor file: ", err)
+	}
+
+	var scanner scanning.ScannerDescriptor
+	err = json.Unmarshal(fileContents, &scanner)
+	if err != nil {
+		s.Fatal("Unable to unmarshal scanner descriptor file: ", err)
+	}
+
+	scanning.RunHardwareTests(ctx, s, cr, scanner, scanning.HardwareTestRunAllCombinations)
+}
