72a7d16439f0d36a10fd2a85b679de9a95fb3a27
Anastasiia N <anastasiian@google.com>
2022-05-17 11:35:12 +0000

    tast-tests: Extend LacrosMainProfileLogin test
    Add a test case when the sync is off. The Chrome is restarted for every
    subtest to clear the local state.
    
    Create policyRealUserFixture:
    FakeDMS::SetPersistentPolicyUser can be called only from fixtures.
    
    BUG=chromium:1319776
    TEST=tast run <ip> policy.LacrosMainProfileLogin
    
    Change-Id: Ia68b59618fca138b7fd2309efe5613710db60db1

--- src/chromiumos/tast/local/bundles/cros/policy/lacros_main_profile_login.go
+++ src/chromiumos/tast/local/bundles/cros/policy/lacros_main_profile_login.go
@@ -10,12 +10,13 @@ import (
 
 	"chromiumos/tast/common/fixture"
 	"chromiumos/tast/ctxutil"
-	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/lacros"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/policyutil/fixtures"
 	"chromiumos/tast/testing"
 )
 
@@ -31,56 +32,84 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      fixture.LacrosPolicyLoggedInRealUser,
+		Timeout:      2*chrome.LoginTimeout + time.Minute,
 	})
 }
 
 func LacrosMainProfileLogin(ctx context.Context, s *testing.State) {
-	// Reserve 10 seconds for various cleanup.
-	cleanupCtx := ctx
-	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
-	defer cancel()
-
-	// Set up the browser.
-	cr, l, _, err := lacros.Setup(ctx, s.FixtValue(), browser.TypeLacros)
-	if err != nil {
-		s.Fatal("Failed to initialize test: ", err)
-	}
-	defer lacros.CloseLacros(cleanupCtx, l)
-
-	// Connect to Test API to use it with the UI library.
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to connect Test API: ", err)
-	}
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
-	ui := uiauto.New(tconn)
-
-	welcomeButton := nodewith.Name("Let's go").Role(role.Button)
-	acceptSyncButton := nodewith.Name("Yes, I'm in").Role(role.Button)
-	if err := uiauto.Combine("accept sync",
-		ui.WaitUntilExists(welcomeButton),
-		ui.LeftClick(welcomeButton),
-		ui.WaitUntilExists(acceptSyncButton),
-		ui.LeftClick(acceptSyncButton),
-	)(ctx); err != nil {
-		s.Fatal("Failed to accept sync: ", err)
-	}
-
-	profileToolbarButton := nodewith.ClassName("AvatarToolbarButton").Role(role.Button).Focusable()
 	profileMenu := nodewith.NameStartingWith("Accounts and sync").Role(role.Menu)
-	loggedInUserEmail := nodewith.Name(cr.User()).Role(role.StaticText).Ancestor(profileMenu)
-	syncIsOnMessage := nodewith.Name("Sync is on").Role(role.StaticText).Ancestor(profileMenu)
-	if err := uiauto.Combine("open the toolbar and check that the sync is on",
-		ui.WaitUntilExists(profileToolbarButton),
-		// Sync message may show an error in the beginning, but should change to 'sync is on'.
-		ui.WithTimeout(time.Minute).LeftClickUntil(profileToolbarButton,
-			uiauto.Combine("check that the user is logged in",
-				ui.Exists(loggedInUserEmail),
-				ui.Exists(syncIsOnMessage),
-			),
-		),
-	)(ctx); err != nil {
-		s.Fatal("Failed to check that the sync is on: ", err)
+
+	for _, param := range []struct {
+		// name is the subtest name.
+		name        string
+		syncButton  *nodewith.Finder
+		syncMessage *nodewith.Finder
+	}{
+		{
+			name:        "no_policy_sync_on",
+			syncButton:  nodewith.Name("Yes, I'm in").Role(role.Button),
+			syncMessage: nodewith.Name("Sync is on").Role(role.StaticText).Ancestor(profileMenu),
+		},
+		{
+			name:        "no_policy_sync_off",
+			syncButton:  nodewith.Name("No thanks").Role(role.Button),
+			syncMessage: nodewith.Name("Sync is off").Role(role.StaticText).Ancestor(profileMenu),
+		},
+	} {
+		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
+			// Reserve 30 seconds for various cleanup.
+			cleanupCtx := ctx
+			ctx, cancel := ctxutil.Shorten(ctx, 30*time.Second)
+			defer cancel()
+
+			// Start chrome.
+			cr, err := chrome.New(ctx, s.FixtValue().(*fixtures.PolicyRealUserFixtData).Opts()...)
+			if err != nil {
+				s.Fatal("Failed to start Chrome: ", err)
+			}
+			defer cr.Close(cleanupCtx)
+
+			// Connect to Test API to use it with the UI library.
+			tconn, err := cr.TestAPIConn(ctx)
+			if err != nil {
+				s.Fatal("Failed to connect Test API: ", err)
+			}
+			defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
+			// Launch Lacros.
+			lacros, err := lacros.Launch(ctx, tconn)
+			if err != nil {
+				s.Fatal("Failed to launch lacros-chrome: ", err)
+			}
+			defer lacros.Close(cleanupCtx)
+
+			ui := uiauto.New(tconn)
+
+			welcomeButton := nodewith.Name("Let's go").Role(role.Button)
+			if err := uiauto.Combine("accept or decline sync",
+				ui.WaitUntilExists(welcomeButton),
+				ui.LeftClick(welcomeButton),
+				ui.WaitUntilExists(param.syncButton),
+				ui.LeftClick(param.syncButton),
+			)(ctx); err != nil {
+				s.Fatal("Failed to accept or decline sync: ", err)
+			}
+
+			profileToolbarButton := nodewith.ClassName("AvatarToolbarButton").Role(role.Button).Focusable()
+			loggedInUserEmail := nodewith.Name(cr.User()).Role(role.StaticText).Ancestor(profileMenu)
+
+			if err := uiauto.Combine("open the toolbar and check that the sync is on",
+				ui.WaitUntilExists(profileToolbarButton),
+				// Sync message may show an error in the beginning, but should change to 'sync is on/off'.
+				ui.WithTimeout(time.Minute).LeftClickUntil(profileToolbarButton,
+					uiauto.Combine("check that the user is logged in",
+						ui.Exists(loggedInUserEmail),
+						ui.Exists(param.syncMessage),
+					),
+				),
+			)(ctx); err != nil {
+				s.Fatal("Failed to check that the sync is on: ", err)
+			}
+		})
 	}
 }
