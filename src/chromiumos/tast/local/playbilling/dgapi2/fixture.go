a8d8091b2d69d130f95c2fc2fa68c99a3b57726b
Alexey Shpakov <ashpakov@google.com>
2022-05-17 07:54:26 +0000

    tast-tests: Add logs for Dgapi2 tests
    Currently when Dgapi2 tests fail, it is hard to analyse them,
    because we are missing the test app logs.
    
    At the end of the test expose app logs in a separate file.
    
    BUG=b:231645492
    TEST=tast run dut appsplatform.Dgapi2GetDetails.vm
    
    Change-Id: Id061a800ed3e94fd96844b45a316226a08b1578f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3651850
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Alvin Jia <alvinjia@google.com>
    Tested-by: Alexey Shpakov <ashpakov@google.com>
    Commit-Queue: Alexey Shpakov <ashpakov@google.com>

--- src/chromiumos/tast/local/playbilling/dgapi2/fixture.go
+++ src/chromiumos/tast/local/playbilling/dgapi2/fixture.go
@@ -6,6 +6,9 @@ package dgapi2
 
 import (
 	"context"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"chromiumos/tast/local/arc"
@@ -84,12 +87,34 @@ func (f *playBillingDgapi2Fixture) PreTest(ctx context.Context, s *testing.FixtT
 }
 
 func (f *playBillingDgapi2Fixture) PostTest(ctx context.Context, s *testing.FixtTestState) {
+	trySaveLogs(ctx, f.TestApp)
+
 	if err := f.TestApp.SignOut(ctx); err != nil {
 		s.Fatal("Failed to sign out of test app: ", err)
 	}
 
 	if err := f.TestApp.Close(ctx); err != nil {
 		s.Fatal("Failed to close Dgapi2 test app: ", err)
+	}
+}
+
+func trySaveLogs(ctx context.Context, testApp *TestAppDgapi2) {
+	dir, ok := testing.ContextOutDir(ctx)
+	if !ok || dir == "" {
+		testing.ContextLog(ctx, "Failed to get name of an out directory")
+		return
+	}
+
+	logs, err := testApp.Dgapi2Logs(ctx)
+	if err != nil {
+		testing.ContextLog(ctx, "Failed to get logs: ", err)
+		return
+	}
+
+	path := filepath.Join(dir, "testapp_logs.txt")
+	if err := ioutil.WriteFile(path, []byte(strings.Join(logs, "\n")), 0644); err != nil {
+		testing.ContextLog(ctx, "Error writing logs to the file: ", err)
+		return
 	}
 }
 
