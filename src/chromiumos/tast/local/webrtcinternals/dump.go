1153a9bf05dd4f73af7fef52677f6c97df34d885
Avery Musbach <amusbach@chromium.org>
2022-05-17 13:25:52 -0700

    Add types for marshaling/unmarshaling WebRTC internals dumps
    This CL adds package webrtcinternals with types for marshaling/
    unmarshaling JSON data downloaded from chrome://webrtc-internals. It
    will be used like this:
    
    var webRTC webrtcinternals.Dump
    if err := json.Unmarshal(webRTCJSON, &webRTC); err != nil {
    	s.Fatal("Failed to unmarshal WebRTC Internals dump: ", err)
    }
    
    BUG=b:231751159
    TEST=../tast/fast_build.sh -T
    
    Change-Id: Ibec88172e46d487599b39d18d616f347f74e7ebc

---
+++ src/chromiumos/tast/local/webrtcinternals/dump.go
@@ -0,0 +1,52 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package webrtcinternals provides types for marshaling/unmarshaling
+// webrtc_internals_dump.txt downloaded from chrome://webrtc-internals.
+package webrtcinternals
+
+// Dump represents the entire contents of webrtc_internals_dump.txt.
+type Dump struct {
+	GetUserMedia    []GetUserMediaEntry       `json:"getUserMedia,omitempty"`
+	PeerConnections map[string]PeerConnection `json:"PeerConnections,omitempty"`
+	UserAgent       string                    `json:"UserAgent,omitempty"`
+}
+
+// GetUserMediaEntry represents an entry in the GetUserMedia field of Dump.
+type GetUserMediaEntry struct {
+	Rid            int     `json:"rid"`
+	Pid            int     `json:"pid"`
+	RequestID      int     `json:"request_id"`
+	Origin         string  `json:"origin"`
+	Timestamp      float64 `json:"timestamp"`
+	StreamID       string  `json:"stream_id,omitempty"`
+	Audio          string  `json:"audio,omitempty"`
+	Video          string  `json:"video,omitempty"`
+	AudioTrackInfo string  `json:"audio_track_info,omitempty"`
+	VideoTrackInfo string  `json:"video_track_info,omitempty"`
+}
+
+// PeerConnection represents an entry in the PeerConnections field of Dump.
+type PeerConnection struct {
+	Pid              int                  `json:"pid"`
+	RTCConfiguration string               `json:"rtcConfiguration"`
+	Constraints      string               `json:"constraints"`
+	URL              string               `json:"url"`
+	Stats            map[string]Statistic `json:"stats"`
+	UpdateLog        []Update             `json:"updateLog"`
+}
+
+// Statistic represents an entry in the Stats field of PeerConnection.
+type Statistic struct {
+	StartTime string `json:"startTime"`
+	EndTime   string `json:"endTime"`
+	Values    string `json:"values"`
+}
+
+// Update represents an entry in the UpdateLog field of PeerConnection.
+type Update struct {
+	Time  string `json:"time"`
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
