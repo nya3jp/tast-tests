a1e2a5ad5b5734aad51a523392ced1ca33f8efa1
Galen Corey <galenemco@chromium.org>
2022-05-19 17:27:11 +0000

    familylink: Add Lacros variant for Unicorn login Tast test
    Add a Lacros variant for the Unicorn login Tast test. This change also
    allows easily creating Lacros variants for any familylink fixture. It
    also changes how the signed-in user is detected, because the “account”
    row on the family link user internals page is dependent on sync being
    enabled, and it is not trivial to enable sync in Lacros for a Tast test.
    This implementation of VerifyUserSignedIntoBrowserAsChild works in both
    ash and Lacros.
    
    BUG=chromium:1313067
    TEST=both variants of unicorn login pass
    
    Change-Id: I3e61196e4eff27cb141b84e774f9d497fabe184a

--- src/chromiumos/tast/local/bundles/cros/familylink/unicorn_login.go
+++ src/chromiumos/tast/local/bundles/cros/familylink/unicorn_login.go
@@ -17,7 +17,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         UnicornLogin,
-		LacrosStatus: testing.LacrosVariantNeeded,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Checks if Unicorn login is working",
 		Contacts:     []string{"chromeos-sw-engprod@google.com", "cros-oac@google.com", "tobyhuang@chromium.org", "cros-families-eng+test@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
@@ -29,7 +29,14 @@ func init() {
 			"unicorn.childUser",
 			"unicorn.childPassword",
 		},
-		Fixture: "familyLinkUnicornLogin",
+		Params: []testing.Param{{
+			Val:     browser.TypeAsh,
+			Fixture: "familyLinkUnicornLogin",
+		}, {
+			Name:    "lacros",
+			Val:     browser.TypeLacros,
+			Fixture: "familyLinkUnicornLoginWithLacros",
+		}},
 	})
 }
 
@@ -43,8 +50,7 @@ func UnicornLogin(ctx context.Context, s *testing.State) {
 	if tconn == nil {
 		s.Fatal("Failed to create test API connection")
 	}
-	// TODO(https://crbug.com/1313067) set browser type to be Ash or LaCrOS based on param.
-	if err := familylink.VerifyUserSignedIntoBrowserAsChild(ctx, cr, tconn, browser.TypeAsh, s.RequiredVar("unicorn.childUser")); err != nil {
+	if err := familylink.VerifyUserSignedIntoBrowserAsChild(ctx, cr, tconn, s.Param().(browser.Type), s.RequiredVar("unicorn.childUser")); err != nil {
 		s.Fatal("Failed to verify user signed into browser: ", err)
 	}
 }
