da39c76284860e403794d9c58a4624cc2bc1e85d
Xibin Liu <xliu@cienet.com>
2022-05-19 10:24:31 -0700

    cuj: Seperate ash/lacros chrome metrics
    Seperate the common metrics as ash-Chrome metrics, lacros-Chrome
    metrics, and common metrics. Also provide option for test case to
    choose to collect metrics seperately or from all browsers.
    
    BUG=b:233016697
    TEST=fast_build.sh -T
    
    Change-Id: I5cca0c6e847b1c01c370e86bc9f9b8d3bef29c65

--- src/chromiumos/tast/local/ui/cujrecorder/common_metrics.go
+++ src/chromiumos/tast/local/ui/cujrecorder/common_metrics.go
@@ -8,18 +8,59 @@ import (
 	"chromiumos/tast/common/perf"
 )
 
-// MetricConfigs returns metrics which are required to be collected by CUJ tests.
-func MetricConfigs() []MetricConfig {
-	return []MetricConfig{
+var (
+	// BrowsingFocusedMetrics contains the metrics that are most useful for measuring the web browsing performance.
+	// These metrics can be collected from Lacros-Chrome browser only if Lacros is enabled.
+	BrowsingFocusedMetrics = []MetricConfig{
 		// Responsiveness.
 		NewCustomMetricConfig("Browser.Tabs.TotalSwitchDuration.NoSavedFrames_NotLoaded", "ms", perf.SmallerIsBetter, []int64{100, 1000}),
 		NewCustomMetricConfig("Browser.Tabs.TotalSwitchDuration.NoSavedFrames_Loaded", "ms", perf.SmallerIsBetter, []int64{100, 1000}),
 		NewCustomMetricConfig("Browser.Tabs.TotalSwitchDuration.WithSavedFrames", "ms", perf.SmallerIsBetter, []int64{100, 1000}),
 		NewCustomMetricConfig("Event.Latency.ScrollUpdate.Touch.TimeToScrollUpdateSwapBegin4", "microseconds", perf.SmallerIsBetter, []int64{25000, 150000}),
 		NewCustomMetricConfig("PageLoad.InteractiveTiming.InputDelay3", "ms", perf.SmallerIsBetter, []int64{25, 300}),
-		NewCustomMetricConfig("Event.Latency.EndToEnd.KeyPress", "microseconds", perf.SmallerIsBetter, []int64{25000, 300000}),
-		NewCustomMetricConfig("Event.Latency.EndToEnd.Mouse", "microseconds", perf.SmallerIsBetter, []int64{25000, 300000}),
-		NewCustomMetricConfig("Event.Latency.EndToEnd.TouchpadPinch2", "microseconds", perf.SmallerIsBetter, []int64{25000, 300000}),
+		// Smoothness.
+		NewSmoothnessMetricConfig("Chrome.Tabs.AnimationSmoothness.TabLoading"),
+		NewSmoothnessMetricConfig("Chrome.Tabs.AnimationSmoothness.HoverCard.FadeOut"),
+		NewSmoothnessMetricConfig("Chrome.Tabs.AnimationSmoothness.HoverCard.FadeIn"),
+		// Browser Render Latency.
+		NewCustomMetricConfig("PageLoad.PaintTiming.NavigationToLargestContentfulPaint2", "ms", perf.SmallerIsBetter, []int64{300, 2000}),
+		NewCustomMetricConfig("PageLoad.PaintTiming.NavigationToFirstContentfulPaint", "ms", perf.SmallerIsBetter, []int64{300, 2000}),
+		NewCustomMetricConfig("Browser.Responsiveness.JankyIntervalsPerThirtySeconds", "janks", perf.SmallerIsBetter, []int64{0, 3}),
+		NewCustomMetricConfig("Browser.Responsiveness.JankyIntervalsPerThirtySeconds3", "janks", perf.SmallerIsBetter, []int64{0, 3}),
+		// Startup Latency.
+		NewCustomMetricConfig("Startup.FirstWebContents.NonEmptyPaint3", "ms", perf.SmallerIsBetter, []int64{300, 2000}),
+		// Others to monitor.
+		NewCustomMetricConfig("MPArch.RWH_TabSwitchPaintDuration", "ms", perf.SmallerIsBetter, []int64{800, 1600}),
+		NewCustomMetricConfig("PageLoad.InteractiveTiming.FirstInputDelay4", "ms", perf.SmallerIsBetter, []int64{800, 1600}),
+		NewCustomMetricConfig("SessionRestore.ForegroundTabFirstPaint4", "ms", perf.SmallerIsBetter, []int64{800, 1600}),
+		NewCustomMetricConfig("WebRTC.Video.DroppedFrames.Capturer", "percent", perf.SmallerIsBetter, []int64{50, 80}),
+		NewCustomMetricConfig("WebRTC.Video.DroppedFrames.Encoder", "percent", perf.SmallerIsBetter, []int64{50, 80}),
+		NewCustomMetricConfig("WebRTC.Video.DroppedFrames.EncoderQueue", "percent", perf.SmallerIsBetter, []int64{50, 80}),
+		NewCustomMetricConfig("WebRTC.Video.DroppedFrames.RateLimiter", "percent", perf.SmallerIsBetter, []int64{50, 80}),
+	}
+
+	// SystemOnlyMetrics contains metrics that are only used to measure the CrOS system performance.
+	// These metrics should be collected from Ash-Chrome only if Lacros is enabled.
+	SystemOnlyMetrics = []MetricConfig{
+		// Media Quality.
+		NewCustomMetricConfig("Cras.FetchDelayMilliSeconds", "ms", perf.SmallerIsBetter, []int64{0, 20}),
+		NewCustomMetricConfig("Cras.UnderrunsPerDevice", "count", perf.SmallerIsBetter, []int64{0, 10}),
+		// ARC App Kill Metrics
+		NewCustomMetricConfig("Arc.App.LowMemoryKills.LMKD.CachedCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
+		NewCustomMetricConfig("Arc.App.LowMemoryKills.LMKD.ForegroundCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
+		NewCustomMetricConfig("Arc.App.LowMemoryKills.LMKD.PerceptibleCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
+		NewCustomMetricConfig("Arc.App.LowMemoryKills.Pressure.CachedCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
+		NewCustomMetricConfig("Arc.App.LowMemoryKills.Pressure.ForegroundCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
+		NewCustomMetricConfig("Arc.App.LowMemoryKills.Pressure.PerceptibleCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
+		NewCustomMetricConfig("Arc.App.LowMemoryKills.LinuxOOMCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
+		// Others to monitor.
+		NewCustomMetricConfig("Power.BatteryDischargeRate", "mW", perf.SmallerIsBetter, []int64{50, 100}),
+	}
+
+	// SystemFocusedMetrics contains the metrics that are most useful to measure the CrOS non web browsing related operations.
+	// These metrics can be collected from Ash-Chrome only if Lacros is enabled.
+	SystemFocusedMetrics = []MetricConfig{
+		// Responsiveness.
 		NewCustomMetricConfig("Ash.SplitViewResize.PresentationTime.ClamshellMode.SingleWindow", "ms", perf.SmallerIsBetter, []int64{25, 300}),
 		NewCustomMetricConfig("Ash.TabDrag.PresentationTime.ClamshellMode", "ms", perf.SmallerIsBetter, []int64{25, 300}),
 		NewCustomMetricConfig("Ash.TabDrag.PresentationTime.MaxLatency.ClamshellMode", "ms", perf.SmallerIsBetter, []int64{25, 300}),
@@ -30,11 +71,8 @@ func MetricConfigs() []MetricConfig {
 		NewCustomMetricConfig("Apps.PaginationTransition.DragScroll.PresentationTime.TabletMode", "ms", perf.SmallerIsBetter, []int64{25, 300}),
 		NewCustomMetricConfig("Apps.StateTransition.Drag.PresentationTime.MaxLatency.TabletMode", "ms", perf.SmallerIsBetter, []int64{25, 300}),
 		NewCustomMetricConfig("Apps.StateTransition.Drag.PresentationTime.TabletMode", "ms", perf.SmallerIsBetter, []int64{25, 300}),
-
 		// Smoothness.
 		NewCustomMetricConfig("Ash.Smoothness.PercentDroppedFrames_1sWindow", "percent", perf.SmallerIsBetter, []int64{50, 80}),
-		NewCustomMetricConfig("Graphics.Smoothness.PercentDroppedFrames.CompositorThread.Video", "percent", perf.SmallerIsBetter, []int64{20, 50}),
-		NewCustomMetricConfig("Graphics.Smoothness.PercentDroppedFrames.AllInteractions", "percent", perf.SmallerIsBetter, []int64{20, 50}),
 		NewSmoothnessMetricConfig("Apps.PaginationTransition.AnimationSmoothness.ClamshellMode"),
 		NewSmoothnessMetricConfig("Apps.StateTransition.AnimationSmoothness"),
 		NewSmoothnessMetricConfig("Apps.StateTransition.AnimationSmoothness.Close.ClamshellMode"),
@@ -56,47 +94,10 @@ func MetricConfigs() []MetricConfig {
 		NewSmoothnessMetricConfig("Ash.Overview.AnimationSmoothness.Enter.MinimizedTabletMode"),
 		NewSmoothnessMetricConfig("Ash.Overview.AnimationSmoothness.Exit.TabletMode"),
 		NewSmoothnessMetricConfig("Ash.Overview.AnimationSmoothness.Close.TabletMode"),
-		NewSmoothnessMetricConfig("Ash.Rotation.AnimationSmoothness"),
 		NewSmoothnessMetricConfig("Ash.WindowCycleView.AnimationSmoothness.Container"),
 		NewSmoothnessMetricConfig("Ash.WindowCycleView.AnimationSmoothness.Show"),
-		NewSmoothnessMetricConfig("Ash.Window.AnimationSmoothness.CrossFade"),
-		NewSmoothnessMetricConfig("Ash.Window.AnimationSmoothness.Hide"),
 		NewSmoothnessMetricConfig("Ash.Window.AnimationSmoothness.Snap"),
-		NewSmoothnessMetricConfig("Chrome.Tabs.AnimationSmoothness.TabLoading"),
-		NewSmoothnessMetricConfig("Chrome.Tabs.AnimationSmoothness.HoverCard.FadeOut"),
-		NewSmoothnessMetricConfig("Chrome.Tabs.AnimationSmoothness.HoverCard.FadeIn"),
-
-		// Browser Render Latency.
-		NewCustomMetricConfig("PageLoad.PaintTiming.NavigationToLargestContentfulPaint2", "ms", perf.SmallerIsBetter, []int64{300, 2000}),
-		NewCustomMetricConfig("PageLoad.PaintTiming.NavigationToFirstContentfulPaint", "ms", perf.SmallerIsBetter, []int64{300, 2000}),
-		NewCustomMetricConfig("Browser.Responsiveness.JankyIntervalsPerThirtySeconds", "janks", perf.SmallerIsBetter, []int64{0, 3}),
-		NewCustomMetricConfig("Browser.Responsiveness.JankyIntervalsPerThirtySeconds3", "janks", perf.SmallerIsBetter, []int64{0, 3}),
-
-		// Startup Latency.
-		NewCustomMetricConfig("Startup.FirstWebContents.NonEmptyPaint3", "ms", perf.SmallerIsBetter, []int64{300, 2000}),
-
-		// Media Quality.
-		NewCustomMetricConfig("Cras.FetchDelayMilliSeconds", "ms", perf.SmallerIsBetter, []int64{0, 20}),
-		NewCustomMetricConfig("Cras.UnderrunsPerDevice", "count", perf.SmallerIsBetter, []int64{0, 10}),
-
-		// ARC App Kill Metrics
-		NewCustomMetricConfig("Arc.App.LowMemoryKills.LMKD.CachedCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
-		NewCustomMetricConfig("Arc.App.LowMemoryKills.LMKD.ForegroundCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
-		NewCustomMetricConfig("Arc.App.LowMemoryKills.LMKD.PerceptibleCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
-		NewCustomMetricConfig("Arc.App.LowMemoryKills.Pressure.CachedCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
-		NewCustomMetricConfig("Arc.App.LowMemoryKills.Pressure.ForegroundCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
-		NewCustomMetricConfig("Arc.App.LowMemoryKills.Pressure.PerceptibleCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
-		NewCustomMetricConfig("Arc.App.LowMemoryKills.LinuxOOMCount10Minutes", "apps", perf.SmallerIsBetter, []int64{0, 5}),
-
 		// Others to monitor.
-		NewCustomMetricConfig("MPArch.RWH_TabSwitchPaintDuration", "ms", perf.SmallerIsBetter, []int64{800, 1600}),
-		NewCustomMetricConfig("PageLoad.InteractiveTiming.FirstInputDelay4", "ms", perf.SmallerIsBetter, []int64{800, 1600}),
-		NewCustomMetricConfig("SessionRestore.ForegroundTabFirstPaint4", "ms", perf.SmallerIsBetter, []int64{800, 1600}),
-		NewCustomMetricConfig("Power.BatteryDischargeRate", "mW", perf.SmallerIsBetter, []int64{50, 100}),
-		NewCustomMetricConfig("EventLatency.TotalLatency", "ms", perf.SmallerIsBetter, []int64{800, 1600}),
-		NewCustomMetricConfig("Graphics.Smoothness.PercentDroppedFrames.AllSequences", "percent", perf.SmallerIsBetter, []int64{20, 50}),
-		NewCustomMetricConfig("Media.Video.Roughness.60fps", "ms", perf.SmallerIsBetter, []int64{20, 50}),
-		NewCustomMetricConfig("Media.DroppedFrameCount", "count", perf.SmallerIsBetter, []int64{10, 40}),
 		NewLatencyMetricConfig("Ash.DragWindowFromShelf.PresentationTime"),
 		NewLatencyMetricConfig("Ash.HotseatTransition.Drag.PresentationTime"),
 		NewSmoothnessMetricConfig("Ash.Homescreen.AnimationSmoothness"),
@@ -112,9 +113,67 @@ func MetricConfigs() []MetricConfig {
 		NewSmoothnessMetricConfig("Ash.HotseatTransition.AnimationSmoothness.TransitionToHiddenHotseat"),
 		NewSmoothnessMetricConfig("Apps.StateTransition.AnimationSmoothness.FadeInOverview"),
 		NewSmoothnessMetricConfig("Apps.StateTransition.AnimationSmoothness.FadeOutOverview"),
-		NewCustomMetricConfig("WebRTC.Video.DroppedFrames.Capturer", "percent", perf.SmallerIsBetter, []int64{50, 80}),
-		NewCustomMetricConfig("WebRTC.Video.DroppedFrames.Encoder", "percent", perf.SmallerIsBetter, []int64{50, 80}),
-		NewCustomMetricConfig("WebRTC.Video.DroppedFrames.EncoderQueue", "percent", perf.SmallerIsBetter, []int64{50, 80}),
-		NewCustomMetricConfig("WebRTC.Video.DroppedFrames.RateLimiter", "percent", perf.SmallerIsBetter, []int64{50, 80}),
 	}
+	// BrowsingAndSystemMetrics contains the metrics that are both important for the user browsing experience and other CrOS operations.
+	// These metrics can be collect from both Ash-Chrome and Lacros-Chrome when Lacros is enabled.
+	BrowsingAndSystemMetrics = []MetricConfig{
+		// Responsiveness.
+		NewCustomMetricConfig("Event.Latency.EndToEnd.KeyPress", "microseconds", perf.SmallerIsBetter, []int64{25000, 300000}),
+		NewCustomMetricConfig("Event.Latency.EndToEnd.Mouse", "microseconds", perf.SmallerIsBetter, []int64{25000, 300000}),
+		NewCustomMetricConfig("Event.Latency.EndToEnd.TouchpadPinch2", "microseconds", perf.SmallerIsBetter, []int64{25000, 300000}),
+		// Smoothness.
+		NewCustomMetricConfig("Graphics.Smoothness.PercentDroppedFrames.CompositorThread.Video", "percent", perf.SmallerIsBetter, []int64{20, 50}),
+		NewCustomMetricConfig("Graphics.Smoothness.PercentDroppedFrames.AllInteractions", "percent", perf.SmallerIsBetter, []int64{20, 50}),
+		NewSmoothnessMetricConfig("Ash.Window.AnimationSmoothness.CrossFade"),
+		NewSmoothnessMetricConfig("Ash.Window.AnimationSmoothness.Hide"),
+		NewSmoothnessMetricConfig("Ash.Rotation.AnimationSmoothness"),
+		// Others to monitor.
+		NewCustomMetricConfig("Graphics.Smoothness.PercentDroppedFrames.AllSequences", "percent", perf.SmallerIsBetter, []int64{20, 50}),
+		NewCustomMetricConfig("EventLatency.TotalLatency", "ms", perf.SmallerIsBetter, []int64{800, 1600}),
+		NewCustomMetricConfig("Media.Video.Roughness.60fps", "ms", perf.SmallerIsBetter, []int64{20, 50}),
+		NewCustomMetricConfig("Media.DroppedFrameCount", "count", perf.SmallerIsBetter, []int64{10, 40}),
+	}
+)
+
+// MetricConfigs returns metrics which are required to be collected by CUJ tests.
+func MetricConfigs() []MetricConfig {
+	var configs []MetricConfig
+	configs = append(configs, BrowsingFocusedMetrics...)
+	configs = append(configs, SystemOnlyMetrics...)
+	configs = append(configs, SystemFocusedMetrics...)
+	configs = append(configs, BrowsingAndSystemMetrics...)
+	return configs
+}
+
+// MetricCollectOption indicates how does the test want to collect metrics from
+// the ash-Chrome and lacros-Chrome.
+type MetricCollectOption int
+
+const (
+	// MetricsCollectFullSystem indicates the test wants to collect as many metrics for the full system as possible.
+	// All metrics except the Ash only ones will be collected from both ash-Chrome and lacros-Chrome.
+	MetricsCollectFullSystem MetricCollectOption = iota
+	// MetricsCollectBrowsingSeperated indicates the test wants to collect web browsing releated
+	// metrics seperately from lacros-Browser only, and collect CrOS system related metrics
+	// from ash-Browser only. There are still some commom metrics which could be collected
+	// from both.
+	MetricsCollectBrowsingSeperated
+)
+
+// BrowserBasedMetricConfigs returns the metric configs that are seperated as
+// ash-Chrome only configs, lacros-Chrome only configs, and common configs for both Ash and Lacros browsers.
+func BrowserBasedMetricConfigs(option MetricCollectOption) (ashConfigs, lacrosConfigs, commonConfigs []MetricConfig) {
+	if option == MetricsCollectFullSystem {
+		ashConfigs = append(ashConfigs, SystemOnlyMetrics...)
+		// Put all other metrics into common configs so they can be collected from all browsers.
+		commonConfigs = append(commonConfigs, BrowsingFocusedMetrics...)
+		commonConfigs = append(commonConfigs, SystemFocusedMetrics...)
+		commonConfigs = append(commonConfigs, BrowsingAndSystemMetrics...)
+	} else if option == MetricsCollectBrowsingSeperated {
+		ashConfigs = append(ashConfigs, SystemOnlyMetrics...)
+		ashConfigs = append(ashConfigs, SystemFocusedMetrics...)
+		lacrosConfigs = append(lacrosConfigs, BrowsingFocusedMetrics...)
+		commonConfigs = append(commonConfigs, BrowsingAndSystemMetrics...)
+	}
+	return ashConfigs, lacrosConfigs, commonConfigs
 }
