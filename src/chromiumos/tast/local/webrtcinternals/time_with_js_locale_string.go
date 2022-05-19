790636ddd059ce9b47b3bd0f21481087e3b86c97
Avery Musbach <amusbach@chromium.org>
2022-05-19 14:51:14 -0700

    webrtcinternals: Marshal/unmarshal update log entry times
    BUG=b:231751159
    TEST=../tast/fast_build.sh -T
    
    Change-Id: Ia8d5e7771951ea714b0331b4e356ccc2c966b890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3656135
    Reviewed-by: Yichen Zhou <yichenz@chromium.org>
    Tested-by: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>

---
+++ src/chromiumos/tast/local/webrtcinternals/time_with_js_locale_string.go
@@ -0,0 +1,38 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package webrtcinternals
+
+import "time"
+
+// TimeWithJSLocaleString holds time.Time and implements interfaces
+// encoding.TextMarshaler and encoding.TextUnmarshaler based on the
+// behavior of JavaScript toLocaleString().
+// Note: The JavaScript function toLocaleString() uses local time by
+// default, and so TimeWithJSLocaleString unmarshals with the local time
+// zone time.Local. TimeWithJSLocaleString is useful for parsing only in
+// the same time zone where JavaScript toLocaleString() was called.
+type TimeWithJSLocaleString time.Time
+
+// jsLocaleStringLayout is a date/time layout for use with time.Format,
+// time.Parse, and time.ParseInLocation, based on the behavior of
+// JavaScript toLocaleString().
+const jsLocaleStringLayout = "1/2/2006, 3:04:05 PM"
+
+// MarshalText encodes TimeWithJSLocaleString with jsLocaleStringLayout.
+func (t TimeWithJSLocaleString) MarshalText() (text []byte, err error) {
+	return []byte(time.Time(t).Format(jsLocaleStringLayout)), nil
+}
+
+// UnmarshalText decodes TimeWithJSLocaleString with jsLocaleStringLayout
+// and time.Local.
+func (t *TimeWithJSLocaleString) UnmarshalText(text []byte) error {
+	parsed, err := time.ParseInLocation(jsLocaleStringLayout, string(text), time.Local)
+	if err != nil {
+		return err
+	}
+
+	*t = TimeWithJSLocaleString(parsed)
+	return nil
+}
