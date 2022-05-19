4586c6ce488b93d07988bfb4d067ad7ded4ae2a3
Avery Musbach <amusbach@chromium.org>
2022-05-19 16:44:59 -0700

    webrtcinternals: Marshal/unmarshal start/end times of stats
    BUG=b:231751159
    TEST=../tast/fast_build.sh -T
    
    Change-Id: Ia91bee8873738698a8900f7e5ed980adcbc77518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3657522
    Tested-by: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Alexander Hartl <alexanderhartl@google.com>
    Reviewed-by: Yichen Zhou <yichenz@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>

---
+++ src/chromiumos/tast/local/webrtcinternals/time_with_nanoseconds.go
@@ -0,0 +1,30 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package webrtcinternals
+
+import "time"
+
+// TimeWithNanoseconds holds time.Time and implements interfaces
+// encoding.TextMarshaler and encoding.TextUnmarshaler with
+// time.RFC3339Nano. Compared to time.Time which implements those
+// interfaces with time.RFC3339, the difference is that
+// TimeWithNanoseconds includes nanoseconds in its text representation.
+type TimeWithNanoseconds time.Time
+
+// MarshalText encodes TimeWithNanoseconds with time.RFC3339Nano.
+func (t TimeWithNanoseconds) MarshalText() (text []byte, err error) {
+	return []byte(time.Time(t).Format(time.RFC3339Nano)), nil
+}
+
+// UnmarshalText decodes TimeWithNanoseconds with time.RFC3339Nano.
+func (t *TimeWithNanoseconds) UnmarshalText(text []byte) error {
+	parsed, err := time.Parse(time.RFC3339Nano, string(text))
+	if err != nil {
+		return err
+	}
+
+	*t = TimeWithNanoseconds(parsed)
+	return nil
+}
