edddab10b4b4e8ddd1193857de24b64d5024624a
Theodore Tefera <ttefera@google.com>
2022-05-18 17:24:51 -0700

    arc: Add tast test for dynamic color.
    Add tast test to make sure that the ArcSystemUIBridge sends over the
    theme_color and correctly changes the android secure settings.
    
    BUG=b:219591999
    TEST=tast run $DUT arc.DynamicColor.vm
    
    Change-Id: I033a3cd05541fe7d78f0f47e26679bc9d05ac8ec

---
+++ src/chromiumos/tast/local/bundles/cros/arc/dynamic_color.go
@@ -0,0 +1,61 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"encoding/json"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DynamicColor,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks that ArcSystemUIService changes Settings.Secure",
+		Contacts:     []string{"arc-app-dev@google.com, ttefera@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "arcBootedWithoutUIAutomator",
+		ServiceDeps:  []string{"tast.cros.nearbyservice.NearbyShareService"}, // Need to find out the arcsystemuiservice equivalent
+		Params: []testing.Param{
+			{
+				Name:              "vm",
+				ExtraSoftwareDeps: []string{"android_vm"},
+			},
+		},
+	})
+}
+
+// DynamicColor calls function in ArcSystemUIService and checks to see if Android Settings.Secure changed.
+func DynamicColor(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*arc.PreData).Chrome
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+	var ret bool
+	if err := tconn.Call(ctx, &ret, "tast.promisify(chrome.autotestPrivate.sendOverlayColor)", 50); err != nil {
+		s.Fatal(err, " --> Failed to send overlay color.")
+	}
+	s.Log(ret, " ~~~Geez this took so long to get")
+
+	a := s.FixtValue().(*arc.PreData).ARC
+
+	cmd := a.Command(ctx, "settings", "get", "secure", "theme_customization_overlay_packages")
+	output, err := cmd.Output()
+	if err != nil {
+		s.Fatal(err, " --> Failed to get secure settings.")
+	}
+	var result map[string]interface{}
+	json.Unmarshal([]byte(string(output)), &result)
+	themeOverlay := result
+	s.Log(themeOverlay["android.theme.customization.system_palette"].(float64), " ~~~ the palette!")
+	s.Log(themeOverlay["android.theme.customization.theme_style"].(string), " ~~~ the style!")
+	s.Log(string(output), " ~~~Oh my goodness we did it")
+}
