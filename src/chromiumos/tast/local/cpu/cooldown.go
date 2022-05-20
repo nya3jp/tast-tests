4a485e670c5ef36853e2649baaf391d0d45fb144
Eliot Courtney <edcourtney@google.com>
2022-05-20 16:32:41 +0900

    Add per-model thresholds for temperature cool-down.
    See go/tast-perf-test-temperature for more information.
    
    This CL introduces per model temperature values for some models that we
    have observed run particularly hot.
    
    BUG=b:213665549, b:211742992, b:211956697, b:223987885, b:213857860,
    b:212953692, b:212234979, b:211956695, b:213139595, b:213139556,
    b:212953751, b:213664822, b:212953749, b:213436479, b:213858218,
    b:213858157, b:213139487, b:211850892, b:212235625, b:213034583
    TEST=tast run <dut> lacros.Octane
    TEST=tast unit test passed
    
    Change-Id: Iaf786e93e907d6f7aa8f23279a144dde67763c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3659117
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Charles William Dick <cwd@google.com>

--- src/chromiumos/tast/local/cpu/cooldown.go
+++ src/chromiumos/tast/local/cpu/cooldown.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/crosconfig"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
@@ -28,10 +29,27 @@ const (
 	CoolDownStopUI
 )
 
+// TemperatureThresholdMode defines various modes of how to decide the temperature threshold
+// to cool down to.
+type TemperatureThresholdMode int
+
+const (
+	// TemperatureThresholdFixed defines a fixed temperature threshold to wait
+	// for.
+	TemperatureThresholdFixed = iota
+	// TemperatureThresholdPerModel means to check for model-specific temperature
+	// thresholds, and use those. If there is no model-specific temperature
+	// threshold, we use the given fixed threshold.
+	TemperatureThresholdPerModel
+)
+
 // CoolDownConfig contains the config to wait for the machine to cooldown.
 type CoolDownConfig struct {
 	PollTimeout  time.Duration
 	PollInterval time.Duration
+	// TemperatureThresholdMode denotes how the temperature threshold should
+	// be chosen.
+	TemperatureThresholdMode TemperatureThresholdMode
 	// TemperatureThreshold is the threshold for CPU temperature.
 	TemperatureThreshold int
 	CoolDownMode         CoolDownMode
@@ -40,10 +58,11 @@ type CoolDownConfig struct {
 // DefaultCoolDownConfig returns the default config to wait for the machine to cooldown.
 func DefaultCoolDownConfig(mode CoolDownMode) CoolDownConfig {
 	return CoolDownConfig{
-		PollTimeout:          300 * time.Second,
-		PollInterval:         2 * time.Second,
-		TemperatureThreshold: 46000,
-		CoolDownMode:         mode,
+		PollTimeout:              300 * time.Second,
+		PollInterval:             2 * time.Second,
+		TemperatureThresholdMode: TemperatureThresholdPerModel,
+		TemperatureThreshold:     46000,
+		CoolDownMode:             mode,
 	}
 }
 
@@ -111,6 +130,11 @@ func Temperature(ctx context.Context) (int, string, error) {
 func WaitUntilCoolDown(ctx context.Context, config CoolDownConfig) (time.Duration, error) {
 	timeBefore := time.Now()
 
+	threshold, err := temperatureThreshold(ctx, config)
+	if err != nil {
+		return 0, err
+	}
+
 	switch config.CoolDownMode {
 	case CoolDownPreserveUI:
 	case CoolDownStopUI:
@@ -131,9 +155,9 @@ func WaitUntilCoolDown(ctx context.Context, config CoolDownConfig) (time.Duratio
 			return testing.PollBreak(errors.Wrap(err, "failed to get CPU temperature"))
 		}
 
-		if t > config.TemperatureThreshold {
-			testing.ContextLogf(ctx, "Waiting until %s temperature (%d) falls below %d", z, t, config.TemperatureThreshold)
-			return errors.Errorf("timed out while waiting until %s temperature (%d) falls below %d", z, t, config.TemperatureThreshold)
+		if t > threshold {
+			testing.ContextLogf(ctx, "Waiting until %s temperature (%d) falls below %d", z, t, threshold)
+			return errors.Errorf("timed out while waiting until %s temperature (%d) falls below %d", z, t, threshold)
 		}
 		return nil
 	}, &testing.PollOptions{Timeout: config.PollTimeout, Interval: config.PollInterval}); err != nil {
@@ -144,4 +168,84 @@ func WaitUntilCoolDown(ctx context.Context, config CoolDownConfig) (time.Duratio
 	duration := timeAfter.Sub(timeBefore)
 	testing.ContextLogf(ctx, "CPU is cooled down (took %f seconds)", duration.Seconds())
 	return duration, nil
+}
+
+// temperatureThreshold gets the temperatuer threshold given the CoolDownConfig.
+func temperatureThreshold(ctx context.Context, config CoolDownConfig) (int, error) {
+	switch config.TemperatureThresholdMode {
+	case TemperatureThresholdFixed:
+		return config.TemperatureThreshold, nil
+	case TemperatureThresholdPerModel:
+		name, err := crosconfig.Get(ctx, "/", "name")
+		if err != nil {
+			return 0, errors.Wrap(err, "could not find model name")
+		}
+		if temperature, ok := modelTemperatureThresholds[name]; ok {
+			testing.ContextLogf(ctx, "Using specific temperature threshold %d for model %s", temperature, name)
+			return temperature, nil
+		}
+		testing.ContextLogf(ctx, "No per model temperature threshold, using default of %d on model %s",
+			config.TemperatureThreshold, name)
+		return config.TemperatureThreshold, nil
+	}
+	return config.TemperatureThreshold, nil
+}
+
+// modelTemperatureThresholds contains thresholds for models which we have
+// determined have run particularly hot. In particular, these are set to the
+// 95th percentile idle temperature for each model, as determined by the output
+// of the power.IdleTemperature test. If the 95th percentile temperature is less
+// than the default threshold of 46 degrees, it is not listed in here.
+var modelTemperatureThresholds = map[string]int{
+	"galith":     54000,
+	"jelboz":     53800,
+	"jelboz360":  53800,
+	"berknip":    53800,
+	"magneto":    53000,
+	"galtic360":  53000,
+	"chronicler": 53000,
+	"hana":       52609,
+	"metaknight": 52000,
+	"pirette":    52000,
+	"drawman":    52000,
+	"lantis":     51000,
+	"anahera":    51000,
+	"pasara":     51000,
+	"pirika":     51000,
+	"galith360":  51000,
+	"blipper":    51000,
+	"ezkinil":    50800,
+	"elm":        50673,
+	"magma":      50000,
+	"magister":   50000,
+	"drawcia":    50000,
+	"beetley":    50000,
+	"kracko360":  50000,
+	"maglith":    50000,
+	"magpie":     50000,
+	"drawlat":    50000,
+	"dirinboz":   49800,
+	"gallop":     49000,
+	"lalala":     49000,
+	"maglia":     49000,
+	"cret":       48000,
+	"magolor":    48000,
+	"duffy":      48000,
+	"cret360":    48000,
+	"maglet":     48000,
+	"eldrid":     48000,
+	"gooey":      48000,
+	"kracko":     48000,
+	"boten":      48000,
+	"sion":       48000,
+	"vilboz":     47800,
+	"ampton":     47000,
+	"sasukette":  47000,
+	"galtic":     47000,
+	"bugzzy":     47000,
+	"bobba360":   47000,
+	"foob360":    47000,
+	"galnat":     47000,
+	"vilboz360":  46800,
+	"vilboz14":   46800,
 }
