477500fd764fd74b05d022f1fddcbfc610110899
HenryYang <yanghenry@google.com>
2022-05-23 11:20:26 +1000

    Extend Battery Test Case to Support RSU Test Case as well
    BUG=b:224884338
    TEST=tast run -var=servo=localhost:9999:ssh:8023 localhost:8025 shimlessrma.DisableHWWP.*
    
    Cq-Depend: chromium:3656932
    Change-Id: Ifa3c8da33dc7c9e9d139719c48343c6dbe7e7e93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3656934
    Reviewed-by: Alvin Jia <alvinjia@google.com>
    Reviewed-by: Michael Pishchagin <mblsha@google.com>
    Tested-by: Henry Yang <yanghenry@google.com>
    Commit-Queue: Henry Yang <yanghenry@google.com>

---
+++ src/chromiumos/tast/remote/bundles/cros/shimlessrma/disable_hwwp.go
@@ -0,0 +1,180 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package shimlessrma contains integration tests for Shimless RMA SWA.
+package shimlessrma
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/action"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/remote/bundles/cros/shimlessrma/rmaweb"
+	"chromiumos/tast/remote/firmware/fixture"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+type writeProtectDisableOption string
+
+const (
+	manual writeProtectDisableOption = "MANUAL"
+	rsu    writeProtectDisableOption = "RSU"
+)
+
+type param struct {
+	wp writeProtectDisableOption
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DisableHWWP,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Can complete Shimless RMA successfully. Disable HWWP with Battery Disconnection",
+		Contacts: []string{
+			"yanghenry@google.com",
+			"chromeos-engprod-syd@google.com",
+		},
+		Attr: []string{"group:shimless_rma", "shimless_rma_experimental"},
+		VarDeps: []string{
+			"ui.signinProfileTestExtensionManifestKey",
+		},
+		SoftwareDeps: []string{"chrome"},
+		HardwareDeps: hwdep.D(hwdep.ChromeEC()),
+		ServiceDeps:  []string{"tast.cros.browser.ChromeService", "tast.cros.shimlessrma.AppService"},
+		Fixture:      fixture.NormalMode,
+		Timeout:      10 * time.Minute,
+		Params: []testing.Param{{
+			Name: "battery_disconnection",
+			Val: param{
+				wp: manual,
+			},
+		}, {
+			Name: "rsu",
+			Val: param{
+				wp: rsu,
+			},
+		}},
+	})
+}
+
+func DisableHWWP(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	firmwareHelper := s.FixtValue().(*fixture.Value).Helper
+	dut := firmwareHelper.DUT
+	key := s.RequiredVar("ui.signinProfileTestExtensionManifestKey")
+	p := s.Param().(param)
+	wpOption := p.wp
+
+	if err := firmwareHelper.RequireServo(ctx); err != nil {
+		s.Fatal("Fail to init servo: ", err)
+	}
+
+	uiHelper, err := rmaweb.NewUIHelper(ctx, dut, firmwareHelper, s.RPCHint(), key, false)
+	if err != nil {
+		s.Fatal("Fail to initialize RMA Helper: ", err)
+	}
+	defer uiHelper.DisposeResource(cleanupCtx)
+
+	if err := uiHelper.SetupInitStatus(ctx); err != nil {
+		s.Fatal("Fail to setup init status: ", err)
+	}
+
+	if err := genreateActionCombinedToDisableWP(wpOption, uiHelper)(ctx); err != nil {
+		s.Fatal("Fail to navigate to Disable Write Protect page and turn off write protect: ", err)
+	}
+
+	// Wait for reboot start.
+	testing.Sleep(ctx, rmaweb.WaitForRebootStart)
+
+	uiHelper, err = rmaweb.NewUIHelper(ctx, dut, firmwareHelper, s.RPCHint(), key, true)
+	if err != nil {
+		s.Fatal("Fail to initialize RMA Helper: ", err)
+	}
+	defer uiHelper.DisposeResource(cleanupCtx)
+
+	if err := action.Combine("Navigate to firmware installation page and install firmware",
+		uiHelper.WriteProtectDisabledPageOperation,
+		uiHelper.BypassFirmwareInstallation,
+	)(ctx); err != nil {
+		s.Fatal("Fail to navigate to firmware installation page and install firmware: ", err)
+	}
+
+	// Wait for reboot start.
+	testing.Sleep(ctx, rmaweb.WaitForRebootStart)
+
+	uiHelper, err = rmaweb.NewUIHelper(ctx, dut, firmwareHelper, s.RPCHint(), key, true)
+	if err != nil {
+		s.Fatal("Fail to initialize RMA Helper: ", err)
+	}
+	defer uiHelper.DisposeResource(cleanupCtx)
+
+	if err := action.Combine("Navigate to Device Provision page",
+		uiHelper.FirmwareInstallationPageOperation,
+		uiHelper.DeviceInformationPageOperation,
+		uiHelper.DeviceProvisionPageOperation,
+	)(ctx); err != nil {
+		s.Fatal("Fail to navigate to Device Provision page: ", err)
+	}
+
+	// Another reboot after provisioning
+	testing.Sleep(ctx, rmaweb.WaitForRebootStart)
+
+	uiHelper, err = rmaweb.NewUIHelper(ctx, dut, firmwareHelper, s.RPCHint(), key, true)
+	if err != nil {
+		s.Fatal("Fail to initialize RMA Helper: ", err)
+	}
+	defer uiHelper.DisposeResource(cleanupCtx)
+
+	// TODO: I comment out the following code due to a bug in Shimless RMA.
+	// I will add it back after Shimless RMA fix it.
+	// Bug link: b:231906070
+	/**
+	web.WriteProtectEnabledPageOperation(ctx, s, client)
+	s.Log("WriteProtectEnabledPageOperation")
+
+	// Wait for reboot start.
+	testing.Sleep(ctx, rmaweb.WaitForRebootStart)
+
+	cl, client, error = rmaweb.CreateShimlessClient(ctx, s.RPCHint(), dut, firmwareHelper, s.RequiredVar("ui.signinProfileTestExtensionManifestKey"), true)
+	defer cl.Close(cleanupCtx)
+	defer client.CloseShimlessRMA(cleanupCtx, &empty.Empty{})
+	if err != nil {
+		s.Fatal("Fail to create Shimless RMA Client: ", err)
+	}
+	s.Log("Init Shimless RMA successfully after enable CCD")
+	*/
+
+	if err := action.Combine("Navigate to Repair Complete page",
+		uiHelper.FinalizingRepairPageOperation,
+		uiHelper.RepairCompeletedPageOperation,
+	)(ctx); err != nil {
+		s.Fatal("Fail to navigate to Repair Complete page: ", err)
+	}
+}
+
+func genreateActionCombinedToDisableWP(option writeProtectDisableOption, uiHelper *rmaweb.UIHelper) action.Action {
+	if option == manual {
+		return action.Combine("Navigate to Manual Disable Write Protect page and turn off write protect",
+			uiHelper.WelcomePageOperation,
+			uiHelper.ComponentsPageOperation,
+			uiHelper.OwnerPageOperation,
+			uiHelper.WipeDevicePageOperation,
+			uiHelper.WriteProtectPageChooseManual,
+		)
+	}
+
+	return action.Combine("Navigate to RSU page and turn off write protect",
+		uiHelper.WelcomePageOperation,
+		uiHelper.ComponentsPageOperation,
+		uiHelper.OwnerPageOperation,
+		uiHelper.WipeDevicePageOperation,
+		uiHelper.WriteProtectPageChooseRSU,
+		uiHelper.RSUPageOperation,
+	)
+}
