695599c104de9d92ea42499d6b3b2f1e2f21ce5a
Yulun Wu <yulunwu@chromium.org>
2022-05-23 17:54:25 +0000

    Promote OpenCloseSwitchApps as it is no longer flaky.
    BUG=chromium:1299244
    
    TEST=manually run on nocturne
    
    Change-Id: I91d1d187e05edb147ea8349fc7763a3602b6c382
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3661173
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Tested-by: Yulun Wu <yulunwu@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/shelf/open_close_switch_apps.go
+++ src/chromiumos/tast/local/bundles/cros/shelf/open_close_switch_apps.go
@@ -32,7 +32,7 @@ func init() {
 			"tbarzic@chromium.org",
 			"cros-system-ui-eng@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Fixture: "chromeLoggedIn",
