8c0496086e59e9ac2e45f1c10d3adc9eba2492c3
HenryYang <yanghenry@google.com>
2022-05-23 11:00:25 +1000

    Fix Stale UI Label and Add Support for RSU Case
    The main change includes:
    1) Fix stable label name, e.g. Change "Next >" to "Next"
    2) Add Support for RSU, e.g. RSUPageOperation method
    3) Other fundamental change: e.g. Add DisposeResource to tear down
       resources.
    
    BUG=b:224884338
    TEST=NONE(The test will be added in subsenquent CL.)
    
    Cq-Depend: chromium:3656934
    Change-Id: I8340823562c1f35132e89a7ecff66c84b414e560

--- src/chromiumos/tast/remote/bundles/cros/shimlessrma/rmaweb/ui.go
+++ src/chromiumos/tast/remote/bundles/cros/shimlessrma/rmaweb/ui.go
@@ -7,24 +7,52 @@ package rmaweb
 
 import (
 	"context"
+	"fmt"
+	"regexp"
+	"time"
 
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/common/servo"
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/firmware"
+	"chromiumos/tast/rpc"
 	pb "chromiumos/tast/services/cros/shimlessrma"
+	"chromiumos/tast/testing"
 )
 
 const timeInSecondToLoadPage = 30
 const timeinSecondToEnableButton = 5
 const longTimeInSecondToEnableButton = 60
 
+// WaitForRebootStart indicates the time to wait before reboot starting.
+const WaitForRebootStart = 10 * time.Second
+
 // UIHelper holds the resources required to communicate with Shimless RMA App.
 type UIHelper struct {
 	// Client contains Shimless RMA App client.
-	Client pb.AppServiceClient
+	Client         pb.AppServiceClient
+	Dut            *dut.DUT
+	FirmwareHelper *firmware.Helper
+	RPCClient      *rpc.Client
 }
 
 // NewUIHelper creates UIHelper.
-func NewUIHelper(client pb.AppServiceClient) *UIHelper {
-	uiHelper := &UIHelper{client}
-	return uiHelper
+func NewUIHelper(ctx context.Context, dut *dut.DUT, firmwareHelper *firmware.Helper, rpcHint *testing.RPCHint, key string, reconnect bool) (*UIHelper, error) {
+	cl, client, err := createShimlessClient(ctx, dut, firmwareHelper, rpcHint, key, reconnect)
+	if err != nil {
+		return nil, err
+	}
+	uiHelper := &UIHelper{client, dut, firmwareHelper, cl}
+	return uiHelper, nil
+}
+
+// DisposeResource will close the resources which are required in UIHelper.
+func (uiHelper *UIHelper) DisposeResource(cleanupCtx context.Context) {
+	uiHelper.RPCClient.Close(cleanupCtx)
+	uiHelper.Client.CloseShimlessRMA(cleanupCtx, &empty.Empty{})
 }
 
 // WelcomePageOperation handles all operations on Welcome Page.
@@ -32,7 +60,7 @@ func (uiHelper *UIHelper) WelcomePageOperation(ctx context.Context) error {
 	if err := uiHelper.waitForPageToLoad(ctx, "Chromebook repair", timeInSecondToLoadPage); err != nil {
 		return err
 	}
-	if err := uiHelper.waitAndClickButton(ctx, "Get started >", longTimeInSecondToEnableButton); err != nil {
+	if err := uiHelper.waitAndClickButton(ctx, "Get started", longTimeInSecondToEnableButton); err != nil {
 		return err
 	}
 
@@ -47,7 +75,7 @@ func (uiHelper *UIHelper) ComponentsPageOperation(ctx context.Context) error {
 	if err := uiHelper.clickButton(ctx, "Base Accelerometer"); err != nil {
 		return err
 	}
-	if err := uiHelper.clickButton(ctx, "Next >"); err != nil {
+	if err := uiHelper.clickButton(ctx, "Next"); err != nil {
 		return err
 	}
 
@@ -59,40 +87,42 @@ func (uiHelper *UIHelper) OwnerPageOperation(ctx context.Context) error {
 	if err := uiHelper.waitForPageToLoad(ctx, "After repair, who will be using the device?", timeInSecondToLoadPage); err != nil {
 		return err
 	}
-	if err := uiHelper.clickRadioButton(ctx, "Device will go to the same owner"); err != nil {
+	if err := uiHelper.clickRadioButton(ctx, "Device will go to the same user"); err != nil {
 		return err
 	}
-	if err := uiHelper.waitAndClickButton(ctx, "Next >", timeinSecondToEnableButton); err != nil {
+	if err := uiHelper.waitAndClickButton(ctx, "Next", timeinSecondToEnableButton); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// WriteProtectPageOperation handles all operations on WP Page.
-func (uiHelper *UIHelper) WriteProtectPageOperation(ctx context.Context) error {
-	if err := uiHelper.waitForPageToLoad(ctx, "Select how you would like to disable write-protect", timeInSecondToLoadPage); err != nil {
-		return err
-	}
-	if err := uiHelper.clickRadioButton(ctx, "Manually disable write-protect"); err != nil {
-		return err
-	}
-	if err := uiHelper.waitAndClickButton(ctx, "Next >", timeinSecondToEnableButton); err != nil {
-		return err
-	}
+// WriteProtectPageChooseRSU handles all operations on WP Page and select RSU.
+func (uiHelper *UIHelper) WriteProtectPageChooseRSU(ctx context.Context) error {
+	return uiHelper.writeProtectPageOperation(ctx, "Perform RMA Server Unlock (RSU)")
+}
 
-	return nil
+// WriteProtectPageChooseManual handles all operations on WP Page and select manual option.
+func (uiHelper *UIHelper) WriteProtectPageChooseManual(ctx context.Context) error {
+	if err := uiHelper.writeProtectPageOperation(ctx, "Manually turn off"); err != nil {
+		return err
+	}
+	if err := uiHelper.disconnectBatteryByCr50(ctx); err != nil {
+		return err
+	}
+	return uiHelper.changeWriteProtectStatus(ctx, servo.FWWPStateOff)
+
 }
 
 // WipeDevicePageOperation handles all operations on wipe device Page.
 func (uiHelper *UIHelper) WipeDevicePageOperation(ctx context.Context) error {
-	if err := uiHelper.waitForPageToLoad(ctx, "Preserve Device Data", timeInSecondToLoadPage); err != nil {
+	if err := uiHelper.waitForPageToLoad(ctx, "Device is going to the same user. Erase user data?", timeInSecondToLoadPage); err != nil {
 		return err
 	}
-	if err := uiHelper.clickRadioButton(ctx, "Wipe device"); err != nil {
+	if err := uiHelper.clickRadioButton(ctx, "Erase all data"); err != nil {
 		return err
 	}
-	if err := uiHelper.waitAndClickButton(ctx, "Next >", timeinSecondToEnableButton); err != nil {
+	if err := uiHelper.waitAndClickButton(ctx, "Next", timeinSecondToEnableButton); err != nil {
 		return err
 	}
 
@@ -104,7 +134,7 @@ func (uiHelper *UIHelper) WriteProtectDisabledPageOperation(ctx context.Context)
 	if err := uiHelper.waitForPageToLoad(ctx, "Write Protect is turned off", timeInSecondToLoadPage); err != nil {
 		return err
 	}
-	if err := uiHelper.clickButton(ctx, "Next >"); err != nil {
+	if err := uiHelper.clickButton(ctx, "Next"); err != nil {
 		return err
 	}
 
@@ -116,7 +146,7 @@ func (uiHelper *UIHelper) WriteProtectEnabledPageOperation(ctx context.Context) 
 	if err := uiHelper.waitForPageToLoad(ctx, "Manually enable write-protect", timeInSecondToLoadPage); err != nil {
 		return err
 	}
-	if err := uiHelper.clickButton(ctx, "Next >"); err != nil {
+	if err := uiHelper.clickButton(ctx, "Next"); err != nil {
 		return err
 	}
 
@@ -128,7 +158,7 @@ func (uiHelper *UIHelper) FirmwareInstallationPageOperation(ctx context.Context)
 	if err := uiHelper.waitForPageToLoad(ctx, "Install firmware image", timeInSecondToLoadPage); err != nil {
 		return err
 	}
-	if err := uiHelper.waitAndClickButton(ctx, "Next >", longTimeInSecondToEnableButton); err != nil {
+	if err := uiHelper.waitAndClickButton(ctx, "Next", longTimeInSecondToEnableButton); err != nil {
 		return err
 	}
 
@@ -140,7 +170,7 @@ func (uiHelper *UIHelper) DeviceInformationPageOperation(ctx context.Context) er
 	if err := uiHelper.waitForPageToLoad(ctx, "Please confirm device information", timeInSecondToLoadPage); err != nil {
 		return err
 	}
-	if err := uiHelper.clickButton(ctx, "Next >"); err != nil {
+	if err := uiHelper.clickButton(ctx, "Next"); err != nil {
 		return err
 	}
 
@@ -161,7 +191,7 @@ func (uiHelper *UIHelper) CalibratePageOperation(ctx context.Context) error {
 	if err := uiHelper.waitForPageToLoad(ctx, "Prepare to calibrate device components", timeInSecondToLoadPage); err != nil {
 		return err
 	}
-	if err := uiHelper.waitAndClickButton(ctx, "Next >", longTimeInSecondToEnableButton); err != nil {
+	if err := uiHelper.waitAndClickButton(ctx, "Next", longTimeInSecondToEnableButton); err != nil {
 		return err
 	}
 
@@ -170,10 +200,15 @@ func (uiHelper *UIHelper) CalibratePageOperation(ctx context.Context) error {
 
 // FinalizingRepairPageOperation handles all operations on finalizing repair Page.
 func (uiHelper *UIHelper) FinalizingRepairPageOperation(ctx context.Context) error {
-	if err := uiHelper.waitForPageToLoad(ctx, "Finalizing repair", timeInSecondToLoadPage); err != nil {
+	// Firstly, reset battery signal & turn on WP.
+	if err := uiHelper.connectBatteryByCr50(ctx); err != nil {
 		return err
 	}
-	if err := uiHelper.waitAndClickButton(ctx, "Next >", longTimeInSecondToEnableButton); err != nil {
+	if err := uiHelper.changeWriteProtectStatus(ctx, servo.FWWPStateOn); err != nil {
+		return err
+	}
+
+	if err := uiHelper.waitForPageToLoad(ctx, "Finalizing repair", timeInSecondToLoadPage); err != nil {
 		return err
 	}
 
@@ -182,10 +217,151 @@ func (uiHelper *UIHelper) FinalizingRepairPageOperation(ctx context.Context) err
 
 // RepairCompeletedPageOperation handles all operations on repair completed Page.
 func (uiHelper *UIHelper) RepairCompeletedPageOperation(ctx context.Context) error {
-	if err := uiHelper.waitForPageToLoad(ctx, "Repair completed", longTimeInSecondToEnableButton); err != nil {
+	if err := uiHelper.waitForPageToLoad(ctx, "Repair is complete", longTimeInSecondToEnableButton); err != nil {
 		return err
 	}
 	if err := uiHelper.clickButton(ctx, "Reboot"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RSUPageOperation handles all operations on RSU Page.
+func (uiHelper *UIHelper) RSUPageOperation(ctx context.Context) error {
+	// Change battery status and WP status
+	if err := uiHelper.disconnectBatteryByCr50(ctx); err != nil {
+		return err
+	}
+	if err := uiHelper.changeWriteProtectStatus(ctx, servo.FWWPStateOff); err != nil {
+		return err
+	}
+
+	if err := uiHelper.waitForPageToLoad(ctx, "Perform RMA Server Unlock", timeInSecondToLoadPage); err != nil {
+		return err
+	}
+	if err := uiHelper.clickLink(ctx, "this URL"); err != nil {
+		return err
+	}
+	url, err := uiHelper.retrieveTextByPrefix(ctx, "https")
+	if err != nil {
+		return err
+	}
+	// Extract the parameter from url.
+	challengeCode, err := uiHelper.parseChallengeCode(url)
+	if err != nil {
+		return err
+	}
+
+	output, err := uiHelper.Dut.Conn().CommandContext(ctx, "/usr/local/bin/rma_reset", "-c", challengeCode).Output()
+	if err != nil {
+		return err
+	}
+	authCode, err := uiHelper.parseAuthCode(string(output))
+	if err != nil {
+		return err
+	}
+
+	if err := uiHelper.clickButton(ctx, "Done"); err != nil {
+		return err
+	}
+	if err := uiHelper.enterIntoTextInput(ctx, authCode, "Enter the 8-character unlock code"); err != nil {
+		return err
+	}
+	if err := uiHelper.clickButton(ctx, "Next"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// BypassFirmwareInstallation will skip firmware installation.
+func (uiHelper *UIHelper) BypassFirmwareInstallation(ctx context.Context) error {
+	// This sleep is important since we need to wait for RMAD to update state file completed.
+	testing.Sleep(ctx, 3*time.Second)
+	// Add "firmware_updated":true to state file.
+	if _, err := uiHelper.Dut.Conn().CommandContext(ctx, "sed", "-i", fmt.Sprintf("s/%s/%s/g", ".$", ",\"firmware_updated\":true}"), "/mnt/stateful_partition/unencrypted/rma-data/state").Output(); err != nil {
+		return err
+	}
+
+	if err := uiHelper.Dut.Reboot(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// OpenCCDIfNotOpen opens CCD if CCD is not open yet.
+func (uiHelper *UIHelper) OpenCCDIfNotOpen(ctx context.Context) error {
+	if val, err := uiHelper.FirmwareHelper.Servo.GetString(ctx, servo.GSCCCDLevel); err != nil {
+		return err
+	} else if val != servo.Open {
+		if err := uiHelper.FirmwareHelper.Servo.SetString(ctx, servo.CR50Testlab, servo.Open); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// SetupInitStatus setup initial status for shimless testing.
+func (uiHelper *UIHelper) SetupInitStatus(ctx context.Context) error {
+	// If error is raised, then Factory is already disabled.
+	// Therefore, ignore any error.
+	uiHelper.changeFactoryMode(ctx, "disable")
+	// Open CCD needs to be executed after disable Factory mode.
+	// It is because disable Factory mode will also lock CCD.
+	if err := uiHelper.OpenCCDIfNotOpen(ctx); err != nil {
+		return err
+	}
+	if err := uiHelper.changeWriteProtectStatus(ctx, servo.FWWPStateOn); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createShimlessClient(ctx context.Context, dut *dut.DUT, firmwareHelper *firmware.Helper, rpcHint *testing.RPCHint, key string, reconnect bool) (*rpc.Client, pb.AppServiceClient, error) {
+	if err := firmwareHelper.WaitConnect(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	// Setup rpc.
+	cl, err := rpc.Dial(ctx, dut, rpcHint)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	request := &pb.NewShimlessRMARequest{
+		ManifestKey: key,
+		Reconnect:   reconnect,
+	}
+	client := pb.NewAppServiceClient(cl.Conn)
+	if _, err := client.NewShimlessRMA(ctx, request, grpc.WaitForReady(true)); err != nil {
+		return nil, nil, err
+	}
+
+	return cl, client, nil
+}
+
+func (uiHelper *UIHelper) changeWriteProtectStatus(ctx context.Context, status servo.FWWPStateValue) error {
+	err := uiHelper.FirmwareHelper.Servo.SetFWWPState(ctx, status)
+	return err
+}
+
+func (uiHelper *UIHelper) changeFactoryMode(ctx context.Context, status string) error {
+	_, err := uiHelper.Dut.Conn().CommandContext(ctx, "gsctool", "-aF", status).Output()
+	return err
+}
+
+func (uiHelper *UIHelper) writeProtectPageOperation(ctx context.Context, radioButtonLabel string) error {
+	if err := uiHelper.waitForPageToLoad(ctx, "Select how you would like to turn off Write Protect", timeInSecondToLoadPage); err != nil {
+		return err
+	}
+	if err := uiHelper.clickRadioButton(ctx, radioButtonLabel); err != nil {
+		return err
+	}
+	if err := uiHelper.waitAndClickButton(ctx, "Next", timeinSecondToEnableButton); err != nil {
 		return err
 	}
 
@@ -224,4 +400,64 @@ func (uiHelper *UIHelper) clickRadioButton(ctx context.Context, label string) er
 	})
 
 	return err
+}
+
+func (uiHelper *UIHelper) clickLink(ctx context.Context, label string) error {
+	if _, err := uiHelper.Client.LeftClickLink(ctx, &pb.LeftClickLinkRequest{
+		Label: label,
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (uiHelper *UIHelper) retrieveTextByPrefix(ctx context.Context, prefix string) (string, error) {
+	res, err := uiHelper.Client.RetrieveTextByPrefix(ctx, &pb.RetrieveTextByPrefixRequest{
+		Prefix: prefix,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return res.Value, nil
+}
+
+func (uiHelper *UIHelper) parseChallengeCode(url string) (string, error) {
+	re := regexp.MustCompile("challenge=([^&]*)")
+	match := re.FindStringSubmatch(url)
+	if match == nil {
+		return "", errors.New("fail to get Challenge Code")
+	}
+	return match[1], nil
+}
+
+func (uiHelper *UIHelper) parseAuthCode(raw string) (string, error) {
+	re := regexp.MustCompile(`Authcode:\s+([a-zA-Z0-9]*)`)
+	match := re.FindStringSubmatch(raw)
+	if match == nil {
+		return "", errors.New("fail to get Auth Code")
+	}
+
+	return match[1], nil
+}
+
+func (uiHelper *UIHelper) enterIntoTextInput(ctx context.Context, content, textInputName string) error {
+	_, err := uiHelper.Client.EnterIntoTextInput(ctx, &pb.EnterIntoTextInputRequest{
+		TextInputName: textInputName,
+		Content:       content,
+	})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (uiHelper *UIHelper) disconnectBatteryByCr50(ctx context.Context) error {
+	return uiHelper.FirmwareHelper.Servo.RunCR50Command(ctx, "bpforce disconnect atboot")
+}
+
+func (uiHelper *UIHelper) connectBatteryByCr50(ctx context.Context) error {
+	return uiHelper.FirmwareHelper.Servo.RunCR50Command(ctx, "bpforce follow_batt_pres atboot")
 }
