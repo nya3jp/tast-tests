376cc44324d5dfd9b0b9ee8578da3636b7e092c7
Ambalavanan MM <ambalavanan.m.m@intel.com>
2022-05-24 09:00:20 +0530

    power: Add UsbEthernetBehaviourColdreboot test
    This CL performs cold reboot and checks the USB ethernet enumeration.
    
    BUG=b:225081951
    TEST=tast run -var="servo=localhost:9999" <DUT IP>
    power.UsbEthernetBehaviourColdreboot.typea
    
    Change-Id: Iefe7dfcde8134f3164bd86166bd0410d33d8c62d

---
+++ src/chromiumos/tast/remote/bundles/cros/power/usb_ethernet_behaviour_coldreboot.go
@@ -0,0 +1,137 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/common/servo"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/firmware"
+	"chromiumos/tast/remote/firmware/fixture"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/power"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         UsbEthernetBehaviourColdreboot,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "USB type-C/type-A to Ethernet adapter behaves properly when device cold reboots",
+		Contacts:     []string{"ambalavanan.m.m@intel.com", "intel-chrome-system-automation-team@intel.com"},
+		SoftwareDeps: []string{"chrome"},
+		ServiceDeps:  []string{"tast.cros.power.EthernetService"},
+		Fixture:      fixture.NormalMode,
+		Vars:         []string{"power.iteration"},
+		HardwareDeps: hwdep.D(hwdep.ChromeEC()),
+		Timeout:      15 * time.Minute,
+		Params: []testing.Param{{
+			Name: "typea",
+			Val:  regexp.MustCompile(`If 0.*Class=.*480M`),
+		}, {
+			Name: "typec",
+			Val:  regexp.MustCompile(`If 0.*Class=.*5000M`),
+		}},
+	})
+}
+
+func UsbEthernetBehaviourColdreboot(ctx context.Context, s *testing.State) {
+	h := s.FixtValue().(*fixture.Value).Helper
+	wantType := s.Param().(*regexp.Regexp)
+
+	if err := checkUSBType(ctx, wantType); err != nil {
+		s.Fatal("Failed to verify USB type: ", err)
+	}
+
+	if err := h.RequireServo(ctx); err != nil {
+		s.Fatal("Failed to connect to servo: ", err)
+	}
+
+	iteration := 1
+	if val, ok := s.Var("power.iteration"); ok {
+		i, err := strconv.Atoi(val)
+		if err != nil {
+			s.Fatal("Failed to convert var to integer: ", err)
+		}
+		iteration = i
+	}
+
+	browseOverEthernet := func() {
+		cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
+		if err != nil {
+			s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+		}
+		defer cl.Close(ctx)
+
+		client := power.NewEthernetServiceClient(cl.Conn)
+
+		s.Log("Sleeping for a few seconds before starting a new Chrome")
+		if err := testing.Sleep(ctx, 5*time.Second); err != nil {
+			s.Fatal("Failed to sleep for a few seconds: ", err)
+		}
+
+		if _, err := client.New(ctx, &empty.Empty{}); err != nil {
+			s.Fatal("Failed to start Chrome: ", err)
+		}
+
+		request := power.WifiRequest{Enabled: false}
+		if _, err := client.SetWifi(ctx, &request); err != nil {
+			s.Fatal("Failed to disable Wi-Fi: ", err)
+		}
+
+		urlrequest := power.BrowseRequest{Url: "https://www.google.com"}
+		if _, err := client.Browse(ctx, &urlrequest); err != nil {
+			s.Fatal("Failed to browse: ", err)
+		}
+	}
+
+	for i := 1; i <= iteration; i++ {
+		s.Logf("Iteration: %d/%d", i, iteration)
+
+		browseOverEthernet()
+
+		cmd := h.DUT.Conn().CommandContext(ctx, "/sbin/shutdown", "-h", "now")
+		if err := cmd.Start(); err != nil {
+			s.Fatal("Failed to shut down DUT: ", err)
+		}
+
+		if err := h.WaitForPowerStates(ctx, firmware.PowerStateInterval, firmware.PowerStateTimeout, "G3"); err != nil {
+			s.Fatal("Failed to get G3 powerstate: ", err)
+		}
+
+		s.Log("Power DUT back on with short press of the power button")
+		if err := h.Servo.KeypressWithDuration(ctx, servo.PowerKey, servo.DurTab); err != nil {
+			s.Fatal("Failed to power on DUT with short press of the power button: ", err)
+		}
+
+		if err := h.WaitConnect(ctx); err != nil {
+			s.Fatal("Failed to reconnect to DUT after restarting: ", err)
+		}
+
+		browseOverEthernet()
+	}
+}
+
+// checkUSBType checks whether usb is 3.0 or 2.0.
+func checkUSBType(ctx context.Context, usbDetectionRe *regexp.Regexp) error {
+	lsusbCMD := "lsusb -t"
+	out, err := testexec.CommandContext(ctx, "sh", "-c", lsusbCMD).Output(testexec.DumpLogOnError)
+	if err != nil {
+		return errors.Wrap(err, "failed to execute lsusb command")
+	}
+
+	if !usbDetectionRe.MatchString(string(out)) {
+		return errors.New("failed to verify usb version")
+	}
+	return nil
+}
