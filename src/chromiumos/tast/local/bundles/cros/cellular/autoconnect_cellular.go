1bda16cf0adacf6794cbc139f471877d6707421b
Pavan Holla <pholla@chromium.org>
2022-05-24 15:22:37 +0000

    cellular: Add test for autoconnect
    A simple test that sets autoconnect and expects cellular to be connected
    after login.
    
    BUG=b:233604061
    TEST=tast run $cr cellular.AutoconnectCellular
    
    Change-Id: I26639e20fe535d76cd3b687261e2170b0977b4de

---
+++ src/chromiumos/tast/local/bundles/cros/cellular/autoconnect_cellular.go
@@ -0,0 +1,77 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/shillconst"
+	"chromiumos/tast/local/cellular"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/modemmanager"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AutoconnectCellular,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verifies that host has network connectivity via cellular interface",
+		Contacts:     []string{"pholla@google.com", "chromeos-cellular-team@google.com"},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_active"},
+		Timeout:      3 * time.Minute,
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func AutoconnectCellular(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	if _, err := modemmanager.NewModemWithSim(ctx); err != nil {
+		s.Fatal("Could not find MM dbus object with a valid sim: ", err)
+	}
+
+	helper, err := cellular.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create cellular.Helper: ", err)
+	}
+
+	// Set up the device to autoconnect and disable any roaming restrictions.
+	cleanup, err := helper.InitServiceProperty(ctx, shillconst.ServicePropertyAutoConnect, true)
+	if err != nil {
+		s.Fatal("Could not initialize autoconnect to true: ", err)
+	}
+	defer cleanup(cleanupCtx)
+	cleanup, err = helper.InitDeviceProperty(ctx, shillconst.DevicePropertyCellularPolicyAllowRoaming, true)
+	if err != nil {
+		s.Fatal("Could not set PolicyAllowRoaming to true: ", err)
+	}
+	defer cleanup(cleanupCtx)
+	cleanup, err = helper.InitServiceProperty(ctx, shillconst.ServicePropertyCellularAllowRoaming, true)
+	if err != nil {
+		s.Fatal("Could not set AllowRoaming property to true: ", err)
+	}
+	defer cleanup(cleanupCtx)
+
+	// The connection will not occur from the login screen, so we log in.
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+	// chrome.Chrome.Close() will not log the user out.
+	defer upstart.RestartJob(ctx, "ui")
+
+	service, err := helper.FindServiceForDevice(ctx)
+	if err != nil {
+		s.Fatal("Unable to find Cellular Service for Device: ", err)
+	}
+
+	// Ensure service's state matches expectations.
+	if err := service.WaitForProperty(ctx, shillconst.ServicePropertyState, shillconst.ServiceStateOnline, 150*time.Second); err != nil {
+		s.Fatal("Failed to get service state: ", err)
+	}
+}
