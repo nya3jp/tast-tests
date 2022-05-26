eecb948e1af2b03b36d54b54444888e54700173b
Jie Jiang <jiejiang@chromium.org>
2022-05-26 15:19:42 +0900

    network: Add a virtualnet package for routing tests
    This package provides the utils to create a veth on DUT with
    configurable IPv4/IPv6 connectivity and thus can be used for multiple
    networking tests.
    
    Files introduced in this patch and their functionalites:
    - routing.go: just to show how this package can be used in the test,
      this file and this line will be removed before the tast review;
    - pool.go: utils to allocate subnets in a test;
    - env/: the basic building block which contains a pair of veths, a netns
      and a chroot;
    - dnsmasq/ and radvd/: v4 and v6 functionalities for Env;
    - virtualnet.go: provides functions which are supposed to be used in
      most of the tests.
    
    BUG=b:192436642
    TEST=Execute CreateRouterEnv(), checked that:
      1) veth interface got the v4 and v6 addresses as expected;
      2) interface status in shill went to Connected as expected;
      3) netns and veths were removed and logs were collected properly after
         cleanup.
    
    Change-Id: I137fd0936393638658ba4155cc87e93456cb4c92

---
+++ src/chromiumos/tast/local/bundles/cros/network/virtualnet/radvd/radvd.go
@@ -0,0 +1,119 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package radvd provides the utils to run the radvd server inside a
+// virtualnet.Env. This simulate an IPv6 router advertising the IPv6
+// configuration of the network with RA packets.
+package radvd
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"net"
+	"os"
+	"strings"
+	"text/template"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/network/virtualnet/env"
+)
+
+const confTemplate = `
+interface {{.ifname}} {
+	MinRtrAdvInterval 3;
+	MaxRtrAdvInterval 4;
+	AdvSendAdvert on;
+	AdvManagedFlag on;
+	prefix {{.prefix}} {};
+	{{if .dns}}
+	RDNSS {{.dns}} {};
+	{{end}}
+};`
+
+// Paths in chroot.
+const (
+	radvdPath = "/usr/local/sbin/radvd"
+	confPath  = "/tmp/radvd.conf"
+	pidPath   = "/tmp/radvd.pid"
+	logPath   = "/tmp/radvd.log"
+)
+
+type radvd struct {
+	env    *env.Env
+	prefix *net.IPNet
+	dns    []string
+	cmd    *testexec.Cmd
+}
+
+// New creates a new radvd object. The returned object can be passed to
+// Env.StartServer(), its lifetime will be managed by the Env object.
+func New(prefix *net.IPNet, dns []string) *radvd {
+	return &radvd{prefix: prefix, dns: dns}
+}
+
+// Start starts the radvd process.
+func (r *radvd) Start(ctx context.Context, env *env.Env) error {
+	r.env = env
+
+	// Prepare config file.
+	confVals := map[string]string{
+		"ifname": r.env.VethInName,
+		"prefix": r.prefix.String(),
+	}
+	if len(r.dns) > 0 {
+		confVals["dns"] = strings.Join(r.dns, ",")
+	}
+	b := &bytes.Buffer{}
+	template.Must(template.New("").Parse(confTemplate)).Execute(b, confVals)
+	if err := ioutil.WriteFile(r.env.ChrootPath(confPath), []byte(b.String()), 0644); err != nil {
+		return errors.Wrap(err, "failed to write config file")
+	}
+
+	// Configure IPv6 environment in netns.
+	if err := r.env.RunWithoutChroot(ctx, "sysctl", "-w", "net.ipv6.conf."+r.env.VethInName+".forwarding=1"); err != nil {
+		return errors.Wrapf(err, "failed to enable ipv6 forwarding in %s", r.env.NetNSName)
+	}
+	if err := r.env.RunWithoutChroot(ctx, "sysctl", "-w", "net.ipv6.conf.all.forwarding=1"); err != nil {
+		return errors.Wrapf(err, "failed to enable ipv6 forwarding in %s", r.env.NetNSName)
+	}
+	if err := r.env.RunWithoutChroot(ctx, "sysctl", "-w", "net.ipv6.conf."+r.env.VethInName+".accept_ra=2"); err != nil {
+		return errors.Wrapf(err, "failed to configure accept_ra for %s", r.env.VethInName)
+	}
+
+	// Start the command.
+	cmd := []string{
+		radvdPath,
+		"--nodaemon",
+		"-d", "4",
+		"-C", confPath,
+		"-p", pidPath,
+		"-m", "logfile", "-l", logPath,
+	}
+	r.cmd = r.env.CreateCommand(ctx, cmd...)
+	if err := r.cmd.Start(); err != nil {
+		return errors.Wrap(err, "failed to start radvd daemon")
+	}
+
+	return nil
+}
+
+// Stop stops the radvd process.
+func (r *radvd) Stop(ctx context.Context) error {
+	if r.cmd == nil || r.cmd.Process == nil {
+		return nil
+	}
+	if err := r.cmd.Kill(); err != nil {
+		return errors.Wrap(err, "failed to kill radvd processs")
+	}
+	r.cmd.Wait()
+	r.cmd = nil
+	return nil
+}
+
+// WriteLogs writes logs into |f|.
+func (r *radvd) WriteLogs(ctx context.Context, f *os.File) error {
+	return r.env.ReadAndWriteLogIfExists(r.env.ChrootPath(logPath), f)
+}
