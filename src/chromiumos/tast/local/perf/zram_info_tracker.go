743a5522fea23c88c547a5c1a345024144e1e5fb
Navin Ramsaroop <ramsaroop@google.com>
2022-05-26 20:50:13 +0000

    Report additional zram metrics
    BUG=b:227151126
    TEST=Run ui.GoogleSheetsCUJ and check results-chart.json for the
    new zram metrics.
    
    Change-Id: I149dc7df2e59ae0658e8694e6eb681553bc9210f

--- src/chromiumos/tast/local/perf/zram_info_tracker.go
+++ src/chromiumos/tast/local/perf/zram_info_tracker.go
@@ -22,14 +22,41 @@ const (
 	zramDevPath             = "/dev/zram0"
 	zramMmStatPath          = "/sys/block/zram0/mm_stat"
 	zramResetMemUsedMaxPath = "/sys/block/zram0/mem_used_max"
+	zramMmStatMaxIndex      = 4
 )
+
+// The stat file represents device's mm statistics. It consists of a single
+// line of text and contains the stats separated by whitespace. The order of
+// the fields is as follows:
+// 1. orig_data_size
+// 2. compr_data_size
+// 3. mem_used_total
+// 4. mem_limit
+// 5. mem_used_max
+// 6. same_pages
+// 7. pages_compacted
+// 8. huge_pages (optional)
+// 9. huge_pages_since (optional)
+//
+// See https://www.kernel.org/doc/html/latest/admin-guide/blockdev/zram.html
+var zramFieldNames = []string{
+	"OrigDataSize",
+	"ComprDataSize",
+	"MemUsedTotal",
+	"MemLimit",
+	"Max",
+	"SamePages",
+	"PagesCompacted",
+	"HugePages",
+	"HugePagesSince",
+}
 
 // ZramInfoTracker is a helper to collect zram info.
 type ZramInfoTracker struct {
 	hasZram         bool
 	prefix          string
+	stats           []float64
 	memUsedMaxStart float64
-	memUsedMaxEnd   float64
 }
 
 // NewZramInfoTracker creates a new instance of ZramInfoTracker. If zram is not
@@ -48,29 +75,28 @@ func NewZramInfoTracker(metricPrefix string) (*ZramInfoTracker, error) {
 	}, nil
 }
 
-// getMmUsedMax gets the "mem_used_max" in zram's mm_stat.
-func getMmUsedMax(ctx context.Context) (float64, error) {
+// getMmStat returns a list of all of the values in zram's mm_stat. The length of
+// this list is dependent on how many of the optional fields are reported by the
+// kernel.
+//
+// mm_stat file is found here: /sys/block/zram<id>/mm_stat
+func getMmStat(ctx context.Context) ([]float64, error) {
 	out, err := testexec.CommandContext(ctx,
 		"cat", zramMmStatPath).Output(testexec.DumpLogOnError)
 	if err != nil {
-		return 0, errors.Wrap(err, "failed to dump zram mm_stat")
+		return nil, errors.Wrap(err, "failed to dump zram mm_stat")
 	}
 
-	// File /sys/block/zram<id>/mm_stat
-	//
-	// The stat file represents device's mm statistics. It consists of a single
-	// line of text and contains the stats separated by whitespace. "mm_used_max"
-	// is the 5th field.
-	//
-	// mem_used_max     the maximum amount of memory zram have consumed to
-	//                  store the data
-	//
-	// See https://www.kernel.org/doc/html/latest/admin-guide/blockdev/zram.html
-	memUsedMax, err := strconv.ParseFloat(strings.Fields(string(out))[4], 64)
-	if err != nil {
-		return 0, errors.Wrap(err, "failed to parse signal strength field")
+	statsRaw := strings.Fields(string(out))
+	numFields := len(statsRaw)
+	stats := make([]float64, numFields)
+	for i := 0; i < numFields; i++ {
+		stats[i], err = strconv.ParseFloat(statsRaw[i], 64)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to parse %s", zramFieldNames[i])
+		}
 	}
-	return memUsedMax, nil
+	return stats, nil
 }
 
 // Start indicates that the zram tracking should start. It resets the mem_used_max
@@ -86,11 +112,11 @@ func (t *ZramInfoTracker) Start(ctx context.Context) error {
 		return errors.Wrap(err, "failed to reset mem_used_max counter")
 	}
 
-	memUsedMax, err := getMmUsedMax(ctx)
+	stats, err := getMmStat(ctx)
 	if err != nil {
-		return errors.Wrap(err, "failed to read mem_used_max counter")
+		return errors.Wrap(err, "failed to read mm_stat")
 	}
-	t.memUsedMaxStart = memUsedMax
+	t.memUsedMaxStart = stats[zramMmStatMaxIndex]
 
 	return nil
 }
@@ -102,13 +128,17 @@ func (t *ZramInfoTracker) Stop(ctx context.Context) error {
 		return nil
 	}
 
-	memUsedMax, err := getMmUsedMax(ctx)
+	stats, err := getMmStat(ctx)
 	if err != nil {
-		return errors.Wrap(err, "failed to read mem_used_max counter")
+		return errors.Wrap(err, "failed to read mm_stat")
 	}
-	t.memUsedMaxEnd = math.Max(t.memUsedMaxEnd, memUsedMax)
 
-	if t.memUsedMaxEnd == t.memUsedMaxStart {
+	// Update the current mem_used_max if the previous max was higher.
+	stats[zramMmStatMaxIndex] = math.Max(stats[zramMmStatMaxIndex], t.memUsedMaxStart)
+
+	t.stats = stats
+
+	if t.stats[zramMmStatMaxIndex] == t.memUsedMaxStart {
 		testing.ContextLog(ctx, "Zram mem_used_max is not changed")
 	}
 
@@ -121,9 +151,11 @@ func (t *ZramInfoTracker) Record(pv *perf.Values) {
 		return
 	}
 
-	pv.Set(perf.Metric{
-		Name:      t.prefix + "RAM.Zram.Max",
-		Unit:      "bytes",
-		Direction: perf.SmallerIsBetter,
-	}, float64(t.memUsedMaxEnd))
+	for i, stat := range t.stats {
+		pv.Set(perf.Metric{
+			Name:      t.prefix + "RAM.Zram." + zramFieldNames[i],
+			Unit:      "bytes",
+			Direction: perf.SmallerIsBetter,
+		}, stat)
+	}
 }
