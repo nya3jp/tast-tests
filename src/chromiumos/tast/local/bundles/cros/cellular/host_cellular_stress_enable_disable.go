a5ae63924f92cc646350d5c7a744c5c3a81bf276
Madhav <madhavadas@google.com>
2022-05-26 05:24:28 +0000

    tast-tests: Stress test enabling and disabling cellular interface
    Stress test enabling and disabling cellular interface and
    validate network connectivity after each disable/enable.
    
    BUG=b:233972604
    TEST=tast -verbose run dut cellular.HostCellularStressEnableDisable
    
    Change-Id: I75f7a9d438d08653fafc1a6597fe59d47531bf10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3670180
    Tested-by: Madhav <madhavadas@google.com>
    Commit-Queue: Madhav <madhavadas@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/cellular/host_cellular_stress_enable_disable.go
@@ -0,0 +1,73 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/cellular"
+	"chromiumos/tast/local/modemmanager"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HostCellularStressEnableDisable,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verifies that host has network connectivity via cellular interface",
+		Contacts:     []string{"madhavadas@google.com", "chromeos-cellular-team@google.com"},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_active"},
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func HostCellularStressEnableDisable(ctx context.Context, s *testing.State) {
+	if _, err := modemmanager.NewModemWithSim(ctx); err != nil {
+		s.Fatal("Could not find MM dbus object with a valid sim: ", err)
+	}
+
+	helper, err := cellular.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create cellular.Helper: ", err)
+	}
+
+	stressTestHostIPConnectivity := func(ctx context.Context) error {
+		for i := 1; i < 31; i++ {
+			s.Logf("Test loop: %d", i)
+			s.Log("Disable")
+			if _, err := helper.Disable(ctx); err != nil {
+				return errors.Wrap(err, "failed to disable modem")
+			}
+			s.Log("Enable")
+			// Enable and get service to set autoconnect based on test parameters.
+			if _, err := helper.Enable(ctx); err != nil {
+				return errors.Wrap(err, "failed to enable modem")
+			}
+			s.Log("Enable autoconnect")
+			if _, err := helper.SetServiceAutoConnect(ctx, true); err != nil {
+				return errors.Wrap(err, "failed to enable AutoConnect")
+			}
+			s.Log("Wait for service to connect")
+			if err := helper.IsConnected(ctx); err != nil {
+				return errors.Wrap(err, "failed to connect to Service")
+			}
+			ipType, err := helper.GetCurrentIPType(ctx)
+			if err != nil {
+				return errors.Wrap(err, "failed to read APN info")
+			}
+			if err := cellular.VerifyIPConnectivity(ctx, testexec.CommandContext, ipType, "/bin"); err != nil {
+				return errors.Wrap(err, "failed connectivity test")
+			}
+		}
+		return nil
+	}
+
+	if err := helper.RunTestOnCellularInterface(ctx, stressTestHostIPConnectivity); err != nil {
+		s.Fatal("Failed to run test on cellular interface: ", err)
+	}
+}
