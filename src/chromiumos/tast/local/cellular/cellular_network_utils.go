005d56086c27e8760383085d618e975a76f0411c
Madhav <madhavadas@google.com>
2022-05-26 05:24:28 +0000

    tast-tests: Stress test enabling and disabling cellular interface
    Stress test enabling and disabling cellular interface and
    validate network connectivity after each disable/enable.
    
    BUG=b:233972604
    TEST=tast -verbose run dut cellular.HostCellularStressEnableDisable
    
    Change-Id: I75f7a9d438d08653fafc1a6597fe59d47531bf10

--- src/chromiumos/tast/local/cellular/cellular_network_utils.go
+++ src/chromiumos/tast/local/cellular/cellular_network_utils.go
@@ -20,55 +20,99 @@ const (
 	googleDotComIPv4 = "ipv4.google.com"
 )
 
-// VerifyCrostiniIPConnectivity verifies the ip connectivity from crostini via cellular interface.
-func VerifyCrostiniIPConnectivity(ctx context.Context, cmd func(context.Context, ...string) *testexec.Cmd, ipType string) error {
-	if ipType == "ipv4" || ipType == "ipv4v6" {
-		testing.ContextLog(ctx, "Verify IPv4 connectivity")
-		if err := testing.Poll(ctx, func(ctx context.Context) error {
-			if err := cmd(ctx, "/bin/ping", "-c1", "-w1", googleDotComIPv4).Run(); err != nil {
-				return errors.Wrap(err, "failed ipv4 ping test in Crostini")
-			}
-			return nil
-		}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+func verifyCrostiniIPv4Ping(ctx context.Context, cmd func(context.Context, ...string) *testexec.Cmd) error {
+	testing.ContextLog(ctx, "Verify IPv4 connectivity")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := cmd(ctx, "/bin/ping", "-c1", "-w1", googleDotComIPv4).Run(); err != nil {
 			return errors.Wrap(err, "failed ipv4 ping test in Crostini")
 		}
+		return nil
+	}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+		return errors.Wrap(err, "failed ipv4 ping test in Crostini")
 	}
-	if ipType == "ipv6" || ipType == "ipv4v6" {
-		testing.ContextLog(ctx, "Verify IPv6 connectivity")
-		if err := testing.Poll(ctx, func(ctx context.Context) error {
-			if err := cmd(ctx, "/bin/ping6", "-c1", "-w1", googleDotComIPv6).Run(); err != nil {
-				return errors.Wrap(err, "failed ipv6 ping test in Crostini")
-			}
-			return nil
-		}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+	return nil
+}
+
+func verifyCrostiniIPv6Ping(ctx context.Context, cmd func(context.Context, ...string) *testexec.Cmd) error {
+	testing.ContextLog(ctx, "Verify IPv6 connectivity")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := cmd(ctx, "/bin/ping6", "-c1", "-w1", googleDotComIPv6).Run(); err != nil {
 			return errors.Wrap(err, "failed ipv6 ping test in Crostini")
 		}
+		return nil
+	}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+		return errors.Wrap(err, "failed ipv6 ping test in Crostini")
+	}
+	return nil
+}
+
+// VerifyCrostiniIPConnectivity verifies the ip connectivity from crostini via cellular interface.
+func VerifyCrostiniIPConnectivity(ctx context.Context, cmd func(context.Context, ...string) *testexec.Cmd, ipType string) error {
+	if ipType == "ipv4" {
+		if err := verifyCrostiniIPv4Ping(ctx, cmd); err != nil {
+			return err
+		}
+	}
+	if ipType == "ipv4v6" {
+		if err := verifyCrostiniIPv4Ping(ctx, cmd); err != nil {
+			testing.ContextLog(ctx, "Failed ipv4 ping test : ", err)
+		}
+		if err := verifyCrostiniIPv6Ping(ctx, cmd); err != nil {
+			return err
+		}
+	}
+	if ipType == "ipv6" {
+		if err := verifyCrostiniIPv6Ping(ctx, cmd); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func verifyIPv4Ping(ctx context.Context, cmd func(context.Context, string, ...string) *testexec.Cmd, bindir string) error {
+	testing.ContextLog(ctx, "Verify IPv4 connectivity")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := cmd(ctx, filepath.Join(bindir, "ping"), "-c1", "-w1", googleDotComIPv4).Run(); err != nil {
+			return errors.Wrap(err, "failed ipv4 ping test")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+		return errors.Wrap(err, "failed ipv4 ping test")
+	}
+	return nil
+}
+
+func verifyIPv6Ping(ctx context.Context, cmd func(context.Context, string, ...string) *testexec.Cmd, bindir string) error {
+	testing.ContextLog(ctx, "Verify IPv6 connectivity")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := cmd(ctx, filepath.Join(bindir, "ping6"), "-c1", "-w1", googleDotComIPv6).Run(); err != nil {
+			return errors.Wrap(err, "failed ipv6 ping test")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
+		return errors.Wrap(err, "failed ipv6 ping test")
 	}
 	return nil
 }
 
 // VerifyIPConnectivity verifies the ip connectivity from Host/ARC via cellular interface.
 func VerifyIPConnectivity(ctx context.Context, cmd func(context.Context, string, ...string) *testexec.Cmd, ipType, bindir string) error {
-	if ipType == "ipv4" || ipType == "ipv4v6" {
-		testing.ContextLog(ctx, "Verify IPv4 connectivity")
-		if err := testing.Poll(ctx, func(ctx context.Context) error {
-			if err := cmd(ctx, filepath.Join(bindir, "ping"), "-c1", "-w1", googleDotComIPv4).Run(); err != nil {
-				return errors.Wrap(err, "failed ipv4 ping test")
-			}
-			return nil
-		}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
-			return errors.Wrap(err, "failed ipv4 ping test")
+	if ipType == "ipv4" {
+		if err := verifyIPv4Ping(ctx, cmd, bindir); err != nil {
+			return err
 		}
 	}
-	if ipType == "ipv6" || ipType == "ipv4v6" {
-		testing.ContextLog(ctx, "Verify IPv6 connectivity")
-		if err := testing.Poll(ctx, func(ctx context.Context) error {
-			if err := cmd(ctx, filepath.Join(bindir, "ping6"), "-c1", "-w1", googleDotComIPv6).Run(); err != nil {
-				return errors.Wrap(err, "failed ipv6 ping test")
-			}
-			return nil
-		}, &testing.PollOptions{Timeout: pingTimeout}); err != nil {
-			return errors.Wrap(err, "failed ipv6 ping test")
+	if ipType == "ipv4v6" {
+		if err := verifyIPv4Ping(ctx, cmd, bindir); err != nil {
+			testing.ContextLog(ctx, "Failed ipv4 ping test : ", err)
+		}
+		if err := verifyIPv6Ping(ctx, cmd, bindir); err != nil {
+			return err
+		}
+	}
+	if ipType == "ipv6" {
+		if err := verifyIPv6Ping(ctx, cmd, bindir); err != nil {
+			return err
 		}
 	}
 	return nil
