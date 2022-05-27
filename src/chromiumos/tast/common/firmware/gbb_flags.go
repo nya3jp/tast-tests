e2a872c7e540c47863ed14a5adad99149da78d4d
Jeremy Bettis <jbettis@google.com>
2022-05-27 13:07:36 -0600

    faft: Stop using GBB flag ENTER_TRIGGERS_TONORM
    If you don't use ENTER_TRIGGERS_TONORM, then it is not possible to get
    from dev mode to normal mode while pressing Ctrl-D & ENTER randomly.
    
    Therefore, stop using ENTER_TRIGGERS_TONORM.
    
    BUG=b:229637316
    TEST=tast run firmware.Fixture* firmware.BootMode* on grunt & brya
    
    Change-Id: I021535299e255c780491e2394482535fcd101c8b

--- src/chromiumos/tast/common/firmware/gbb_flags.go
+++ src/chromiumos/tast/common/firmware/gbb_flags.go
@@ -34,9 +34,9 @@ func AllGBBFlags() []pb.GBBFlag {
 	return allGBBFlags
 }
 
-// FAFTGBBFlags returns the flags that faft sets in firmware_test.py before starting a test.
+// FAFTGBBFlags returns the flags that faft sets before starting a test.
 func FAFTGBBFlags() []pb.GBBFlag {
-	return []pb.GBBFlag{pb.GBBFlag_RUNNING_FAFT, pb.GBBFlag_ENTER_TRIGGERS_TONORM}
+	return []pb.GBBFlag{pb.GBBFlag_RUNNING_FAFT}
 }
 
 // RebootRequiredGBBFlags returns flags that require a DUT reboot after they are changed.
