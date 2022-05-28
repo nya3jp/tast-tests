117fd9cd96c6bccd897c9532a9b9283165824c74
Hung Nguyen <hungmn@google.com>
2022-05-28 05:51:30 +0000

    Add cache validation for TTS.
    When the TTS pregenerated cache exists:
    - The pregenerated cache should be the same as the genuine cache generated with cache setup disabled.
    - TTS should be initialized from cache when the cache setup is enabled.
    
    BUG=b:229014327
    TEST=tast DUT arc.CacheValidation.r with other validations disabled as
    they failed without any changes.
    TEST=tast run hatch arc.DataCollector.vm_local
    
    Change-Id: I1d84937a0bb14775fbe392e964e06a3608580823

--- src/chromiumos/tast/local/bundles/cros/arc/tts_cache_service.go
+++ src/chromiumos/tast/local/bundles/cros/arc/tts_cache_service.go
@@ -8,11 +8,14 @@ import (
 	"context"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
-	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/a11y"
 	"chromiumos/tast/local/bundles/cros/arc/cache"
 	arcpb "chromiumos/tast/services/cros/arc"
 	"chromiumos/tast/testing"
@@ -32,7 +35,7 @@ type TTSCacheService struct {
 }
 
 // Generate generates TTS cache.
-func (c *TTSCacheService) Generate(ctx context.Context, request *empty.Empty) (res *arcpb.TTSCacheResponse, retErr error) {
+func (c *TTSCacheService) Generate(ctx context.Context, request *arcpb.TTSCacheRequest) (res *arcpb.TTSCacheResponse, retErr error) {
 	targetDir, err := ioutil.TempDir("", "tts_cache")
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to created target dir for TTS cache")
@@ -43,10 +46,15 @@ func (c *TTSCacheService) Generate(ctx context.Context, request *empty.Empty) (r
 		}
 	}()
 
-	// Boot ARC without existing TTS caches enabled to let TTS generate cache.
-	testing.ContextLog(ctx, "Starting ARC, with ArcEnableTTSCaching feature turned on")
+	args := []string{"--enable-features=ArcEnableTTSCaching"}
+	if !request.TtsCacheSetupEnabled {
+		args = append(args, "--arc-disable-tts-cache")
+	}
 
-	cr, a, err := cache.OpenSession(ctx, []string{"--enable-features=ArcEnableTTSCaching"}, targetDir)
+	// Boot ARC without existing TTS caches enabled to let TTS generate cache.
+	testing.ContextLog(ctx, "Starting ARC with the following arguments: ", args)
+
+	cr, a, err := cache.OpenSession(ctx, args, targetDir)
 	if err != nil {
 		os.RemoveAll(targetDir)
 		return nil, errors.Wrap(err, "failed to generage TTS cache")
@@ -60,8 +68,50 @@ func (c *TTSCacheService) Generate(ctx context.Context, request *empty.Empty) (r
 		return nil, errors.Wrap(err, "failed to generage TTS cache")
 	}
 
+	pregenCacheFileName := cache.PregeneratedTTSStateCache
+	pregenTTSCacheFileSrc := filepath.Join("/system/etc", cache.TTSStateCache)
+	pregenTTSCacheFileDst := filepath.Join(targetDir, pregenCacheFileName)
+	if err := a.PullFile(ctx, pregenTTSCacheFileSrc, pregenTTSCacheFileDst); err != nil {
+		testing.ContextLog(ctx, "Could not pull pregenerated TTS cache from Android, this may mean that the cache was not installed when building the image")
+		pregenCacheFileName = ""
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed creating test API connection")
+	}
+
+	testing.ContextLog(ctx, "Wating for TTS engine to be loaded")
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		voices, err := a11y.Voices(ctx, tconn)
+		if err != nil {
+			return errors.Wrap(err, "failed to get voices")
+		}
+
+		for _, voice := range voices {
+			if strings.HasPrefix(voice.Name, "Android") {
+				return nil
+			}
+		}
+
+		return errors.New("TTS engine is not loaded")
+	}, &testing.PollOptions{
+		Timeout: 15 * time.Second,
+	}); err != nil {
+		return nil, errors.Wrap(err, "failed waiting for TTS engine to load")
+	}
+
+	const ttsCacheReadProp = "ro.arc.tts.initialized_from_cache"
+	propVal, getPropErr := a.GetProp(ctx, ttsCacheReadProp)
+	if getPropErr != nil {
+		return nil, errors.Wrapf(getPropErr, "failed to get prop %q", ttsCacheReadProp)
+	}
+	initializedFromCache := propVal == "1"
+
 	return &arcpb.TTSCacheResponse{
-		TargetDir:         targetDir,
-		TtsStateCacheName: cache.TTSStateCache,
+		TargetDir:                     targetDir,
+		TtsStateCacheName:             cache.TTSStateCache,
+		PregeneratedTtsStateCacheName: pregenCacheFileName,
+		EngineInitializedFromCache:    initializedFromCache,
 	}, nil
 }
