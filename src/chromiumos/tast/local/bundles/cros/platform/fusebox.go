3322e59232e0fbd7f442109ebf6d4cf18e120295
Noel Gordon <noel@chromium.org>
2022-05-30 12:05:48 +1000

    tast-tests: add fusebox daemon mount and request test
    Use cros-disks to mount fusebox daemon with a fake file system then
    verify the daemon successfully mounts and responds to requests from
    Kernel FUSE (stat(2) a file system fake file entry), and also D-Bus
    method requests (call fusebox daemon TestIsAlive D-Bus method).
    
    BUG=chromium:1329684
    TEST=tast run root@DUT platform.Fusebox
    
    Change-Id: I35a67fc048fabeb4a8c167241df845f31288cd0a

---
+++ src/chromiumos/tast/local/bundles/cros/platform/fusebox.go
@@ -0,0 +1,93 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	"chromiumos/tast/local/crosdisks"
+	"chromiumos/tast/local/dbusutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Fusebox,
+		Desc: "Mount fusebox deamon and verify it can respond to requests",
+		Contacts: []string{
+			"noel@chromium.org",
+			"benreich@chromium.org",
+			"chromeos-files-app@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func Fusebox(ctx context.Context, s *testing.State) {
+	const (
+		dbusName      = "org.chromium.FuseBoxReverseService"
+		dbusPath      = "/org/chromium/FuseBoxReverseService"
+		dbusInterface = "org.chromium.FuseBoxReverseService"
+	)
+
+	s.Log("Connecting to CrosDisks D-Bus service")
+	cd, err := crosdisks.New(ctx)
+	if err != nil {
+		s.Fatal("Failed connecting to CrosDisks D-Bus service: ", err)
+	}
+	defer cd.Close()
+
+	w, err := cd.WatchMountCompleted(ctx)
+	if err != nil {
+		s.Fatal("Failed to get MountCompleted event watcher: ", err)
+	}
+	defer w.Close(ctx)
+
+	const source = "fusebox://fusebox-tast-test-requests"
+	options := []string{"--fake", "--debug", "--v=2"}
+
+	s.Logf("Mounting fusebox source: %q", source)
+	if err := cd.Mount(ctx, source, "fusebox", options); err != nil {
+		s.Fatal("Failed during CrosDisks Mount call: ", err)
+	}
+	defer cd.Unmount(ctx, source, nil /* options */)
+
+	s.Log("Awaiting MountCompleted event")
+	m, err := w.Wait(ctx)
+	if err != nil {
+		s.Fatal("Failed awaiting MountCompleted event: ", err)
+	}
+
+	s.Logf("CrosDisks mounted %q at %v", m.SourcePath, m.MountPath)
+	if m.SourcePath != source {
+		s.Fatal("Failed: invalid source: ", m.SourcePath)
+	} else if m.MountPath != "/media/fuse/fusebox-tast-test-requests" {
+		s.Fatal("Failed: invalid mount-point: ", m.MountPath)
+	}
+
+	// Test FUSE request: stat(2) fake file system file entry "hello".
+	hello := filepath.Join(m.MountPath, "hello")
+	s.Log("Calling stat(2) on fusebox fake file entry ", hello)
+	if _, err := os.Stat(hello); err != nil {
+		s.Fatal("Failed stat(2): ", err)
+	}
+
+	// Test D-BUS request: call fusebox daemon D-BUS interface method.
+	s.Logf("Connecting to D-Bus interface %s", dbusName)
+	_, dbusObj, err := dbusutil.Connect(ctx, dbusName, dbusPath)
+	if err != nil {
+		s.Fatalf("Failed to connect to D-Bus interface %s: %v", dbusName, err)
+	}
+
+	var result bool
+	s.Log("Calling fusebox D-Bus interface TestIsAlive method")
+	if err := dbusObj.CallWithContext(ctx, dbusInterface+".TestIsAlive", 0).Store(&result); err != nil {
+		s.Fatal("Failed to call D-Bus TestIsAlive method: ", err)
+	} else if !result {
+		s.Fatal("Failed: D-Bus TestIsAlive method returned false")
+	}
+}
