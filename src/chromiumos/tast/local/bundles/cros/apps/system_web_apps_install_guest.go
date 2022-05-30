7c4338ee7d122ac6db863d5d1330ea8dc9334989
Jiewei Qian <qjw@chromium.org>
2022-05-30 07:35:26 +0000

    tast-tests: improve and re-enable SystemWebAppsInstallGuest
    This CL updates SystemWebAppInstallGuest to use Guest login fixture,
    and simplifies test assertions to be more reliable.
    
    This CL re-enable the test and run it as informational.
    
    This CL adds a new FindSystemWebAppByOrigin method to query installed
    SWAs. This method will replace ListSystemWebApps* after their usage
    has been removed in subsequent CLs.
    
    BUG=chromium:1330190
    TEST=run tast on hatch DUT
    
    Change-Id: I8e6739a94580b0f9a97416fd69264b2d6726b4a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3676158
    Tested-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Christopher Lam <calamity@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/apps/system_web_apps_install_guest.go
+++ src/chromiumos/tast/local/bundles/cros/apps/system_web_apps_install_guest.go
@@ -6,7 +6,6 @@ package apps
 
 import (
 	"context"
-	"strings"
 	"time"
 
 	"chromiumos/tast/local/apps"
@@ -20,68 +19,46 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Checks that system web apps are installed in guest mode",
 		Contacts: []string{
+			"qjw@chromium.org",
 			"chrome-apps-platform-rationalization@google.com",
 		},
 		Timeout:      3 * time.Minute,
 		SoftwareDeps: []string{"chrome"},
-		// TODO(crbug/1190865): Disabled as the guest session is causing subsequent tests issues.
-		// Attr:         []string{"group:mainline"},
-		Params: []testing.Param{{
-			Name: "all_apps",
-			Val:  []chrome.Option{chrome.EnableFeatures("EnableAllSystemWebApps")},
-		}, {
-			Name: "default_enabled_apps",
-			Val:  []chrome.Option{},
-		}},
+		Fixture:      "chromeLoggedInGuest",
+		Attr:         []string{"group:mainline", "informational"},
 	})
 }
 
 // SystemWebAppsInstallGuest tests that system web apps are installed on a guest profile.
 func SystemWebAppsInstallGuest(ctx context.Context, s *testing.State) {
-	chromeOpts := s.Param().([]chrome.Option)
-	chromeOpts = append(chromeOpts, chrome.GuestLogin())
-
-	cr, err := chrome.New(ctx, chromeOpts...)
-	if err != nil {
-		s.Fatal("Failed to start Chrome: ", err)
-	}
-	defer cr.Close(ctx)
+	cr := s.FixtValue().(*chrome.Chrome)
 
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
 		s.Fatal("Failed to connect Test API: ", err)
 	}
 
-	installedSystemWebApps, err := apps.ListSystemWebApps(ctx, tconn)
-	if err != nil {
-		s.Fatal("Failed to get installed system apps: ", err)
-	}
-
-	registeredInternalNames, err := apps.ListSystemWebAppsInternalNames(ctx, tconn)
+	registeredSystemWebApps, err := apps.ListRegisteredSystemWebApps(ctx, tconn)
 	if err != nil {
 		s.Fatal("Failed to get registered system apps: ", err)
 	}
 
-	for i, internalName := range registeredInternalNames {
-		// Terminal SWA is always installed but it is disabled (controlled by Crostini App Service publisher) in guest mode.
-		// Exclude it from the list as it does not show in installedSystemWebApps.
-		// https://source.chromium.org/chromium/chromium/src/+/HEAD:chrome/browser/apps/app_service/crostini_apps.cc;l=240
-		if internalName == "Terminal" {
-			registeredInternalNames = append(registeredInternalNames[:i], registeredInternalNames[i+1:]...)
-			break
-		}
+	// Verify a set of system web apps that triggers different install code paths are installed.
+	testAppInternalNames := map[string]bool{
+		"OSSettings": true,
+		"Media":      true,
+		"Help":       true,
 	}
 
-	// Only the list length is compares and not the names because the registeredInternalNames are different to
-	// installedSystemWebApps, e.g. OSSettings vs. Settings.
-	if len(registeredInternalNames) != len(installedSystemWebApps) {
-		var installedNames []string
-		for _, app := range installedSystemWebApps {
-			installedNames = append(installedNames, app.Name)
+	for _, swa := range registeredSystemWebApps {
+		if testAppInternalNames[swa.InternalName] {
+			app, err := apps.FindSystemWebAppByOrigin(ctx, tconn, swa.StartURL)
+			if err != nil {
+				s.Fatalf("Failed to match system web app by origin, app: %s, origin: %s, errror: %v", swa.InternalName, swa.StartURL, err)
+			}
+			if app == nil {
+				s.Fatal("Failed to find system web app that should have been installed: ", swa.InternalName)
+			}
 		}
-
-		s.Logf("Registered apps: %s", strings.Join(registeredInternalNames, ", "))
-		s.Logf("Installed apps: %s", strings.Join(installedNames, ", "))
-		s.Fatalf("Unexpected number of installed apps: want %d; got %d", len(registeredInternalNames), len(installedNames))
 	}
 }
