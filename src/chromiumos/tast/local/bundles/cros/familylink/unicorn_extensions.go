0b5e2853ad4506f93eaa7ee4dff78e349f536e78
Galen Corey <galenemco@chromium.org>
2022-05-31 15:51:57 +0000

    familylink: Lacros variant to extensions Tast test
    Add a Lacros variant for unicorn extensions Tast
    test.
    
    BUG=b:190654893
    TEST=both variants pass
    
    Change-Id: I96e427a56591edaef72ad104206b57ebbdb3b651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3680478
    Commit-Queue: Galen Corey <galenemco@chromium.org>
    Reviewed-by: Matt Stark <msta@google.com>
    Tested-by: Galen Corey <galenemco@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/familylink/unicorn_extensions.go
+++ src/chromiumos/tast/local/bundles/cros/familylink/unicorn_extensions.go
@@ -17,7 +17,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         UnicornExtensions,
-		LacrosStatus: testing.LacrosVariantNeeded,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Checks if Unicorn user can add extension with parent permission",
 		Contacts:     []string{"chromeos-sw-engprod@google.com", "cros-oac@google.com", "tobyhuang@chromium.org", "cros-families-eng+test@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
@@ -27,7 +27,14 @@ func init() {
 			"family.parentEmail",
 			"family.parentPassword",
 		},
-		Fixture: "familyLinkUnicornLogin",
+		Params: []testing.Param{{
+			Val:     browser.TypeAsh,
+			Fixture: "familyLinkUnicornLogin",
+		}, {
+			Name:    "lacros",
+			Val:     browser.TypeLacros,
+			Fixture: "familyLinkUnicornLoginWithLacros",
+		}},
 	})
 }
 
@@ -42,8 +49,7 @@ func UnicornExtensions(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to create test API connection")
 	}
 
-	// TODO(https://crbug.com/1313067) set browser type to be Ash or LaCrOS based on param.
-	if err := familylink.NavigateExtensionApprovalFlow(ctx, cr, tconn, browser.TypeAsh, s.RequiredVar("family.parentEmail"), s.RequiredVar("family.parentPassword")); err != nil {
+	if err := familylink.NavigateExtensionApprovalFlow(ctx, cr, tconn, s.Param().(browser.Type), s.RequiredVar("family.parentEmail"), s.RequiredVar("family.parentPassword")); err != nil {
 		s.Fatal("Failed to add extension: ", err)
 	}
 }
