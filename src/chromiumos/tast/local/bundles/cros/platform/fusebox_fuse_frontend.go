4b03aaded1f83d2c5bb6dc0443e1d91f302deaf3
Noel Gordon <noel@chromium.org>
2022-06-01 02:11:40 +1000

    tast-tests: add fusebox daemon FUSE frontend file system test
    Mount fusebox daemon with a fake file system and call POSIX API using
    commands to test the FUSE frontend and file system interfaces.
    
    Note: FUSE op Rename is not implemented by the fake file system: just
    report the error output and return success.
    
    Reference: go/fusebox-tast-tests
    
    BUG=chromium:1329395,chromium:1329684
    TEST=tast run root@DUT platform.FuseboxFuseFrontend
    
    Change-Id: Ibaeeccfc5cfbf18df93cf7469e75dfe278ed1f16

---
+++ src/chromiumos/tast/local/bundles/cros/platform/fusebox_fuse_frontend.go
@@ -0,0 +1,221 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/crosdisks"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FuseboxFuseFrontend,
+		Desc: "Mount fusebox daemon and test FUSE frontend POSIX operations",
+		Contacts: []string{
+			"noel@chromium.org",
+			"benreich@chromium.org",
+			"chromeos-files-app@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+func create(ctx context.Context, s *testing.State, path, file string) string {
+	file = filepath.Join(path, file)
+
+	if f, err := os.Create(file); err != nil {
+		s.Fatalf("create %s failed: %v", file, err)
+	} else {
+		f.Close()
+	}
+
+	return "create " + file
+}
+
+func write(ctx context.Context, s *testing.State, path, file string) string {
+	file = filepath.Join(path, file)
+
+	if err := os.WriteFile(file, []byte("bon jour\n"), 0644); err != nil {
+		s.Fatalf("write %s failed: %v", file, err)
+	}
+
+	return "write " + file
+}
+
+func read(ctx context.Context, s *testing.State, path, file string) string {
+	file = filepath.Join(path, file)
+
+	data, err := os.ReadFile(file)
+	if err != nil {
+		s.Fatalf("read %s failed: %v", file, err)
+	}
+
+	return "read [" + strings.TrimSpace(string(data)) + "]"
+}
+
+func seek(ctx context.Context, s *testing.State, path, file string) string {
+	file = filepath.Join(path, file)
+
+	f, err := os.Open(file)
+	if err != nil {
+		s.Fatalf("open %s failed: %v", file, err)
+	}
+
+	if _, err := f.Seek(6, 0); err != nil {
+		s.Fatalf("seek %s at 6 bytes failed: %v", file, err)
+	}
+
+	data := make([]byte, 5)
+	if _, err = f.Read(data); err != nil {
+		s.Fatalf("read %s failed: %v", file, err)
+	} else {
+		f.Close()
+	}
+
+	return "seek [" + strings.TrimSpace(string(data)) + "]"
+}
+
+func cp(ctx context.Context, s *testing.State, path, src, des string) string {
+	src = filepath.Join(path, src)
+	des = filepath.Join(path, des)
+
+	const cp = "/bin/cp"
+	if _, err := testexec.CommandContext(ctx, cp, src, des).Output(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("cp %s to %s failed: %v", src, des, err)
+	}
+
+	return "cp " + src
+}
+
+func mkdir(ctx context.Context, s *testing.State, path, dir string) string {
+	dir = filepath.Join(path, dir)
+
+	const mkdir = "/bin/mkdir"
+	if _, err := testexec.CommandContext(ctx, mkdir, dir).Output(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("mkdir %s failed: %v", dir, err)
+	}
+
+	return "mkdir " + dir
+}
+
+func rmdir(ctx context.Context, s *testing.State, path, dir string) string {
+	dir = filepath.Join(path, dir)
+
+	const rm = "/bin/rm" // using the force -rf
+	if _, err := testexec.CommandContext(ctx, rm, "-rf", dir).Output(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("rmdir %s failed: %v", dir, err)
+	}
+
+	return "rmdir " + dir
+}
+
+func rm(ctx context.Context, s *testing.State, path, file string) string {
+	file = filepath.Join(path, file)
+
+	const rm = "/bin/rm"
+	if _, err := testexec.CommandContext(ctx, rm, file).Output(testexec.DumpLogOnError); err != nil {
+		s.Fatalf("rm %s failed: %v", file, err)
+	}
+
+	return "rm " + file
+}
+
+func rename(ctx context.Context, s *testing.State, path, src, des string) string {
+	src = filepath.Join(path, src)
+	des = filepath.Join(path, des)
+
+	const mv = "/bin/mv"
+	if output, err := testexec.CommandContext(ctx, mv, src, des).CombinedOutput(); err != nil {
+		return fmt.Sprintf("rename ENOTSUP %s", output)
+	}
+
+	return "rename " + src
+}
+
+func ls(ctx context.Context, s *testing.State, path string) string {
+	const ls = "/bin/ls"
+
+	output, err := testexec.CommandContext(ctx, ls, "-ls", "-R", path).Output(testexec.DumpLogOnError)
+	if err != nil {
+		s.Fatalf("List %s failed: %v", path, err)
+	}
+
+	return strings.TrimSpace("ls " + string(output[:]))
+}
+
+func FuseboxFuseFrontend(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	cd, err := crosdisks.New(ctx)
+	if err != nil {
+		s.Fatal("Failed connecting to CrosDisks D-Bus service: ", err)
+	}
+	defer cd.Close()
+
+	w, err := cd.WatchMountCompleted(ctx)
+	if err != nil {
+		s.Fatal("Failed to get MountCompleted event watcher: ", err)
+	}
+	defer w.Close(cleanupCtx)
+
+	const source = "fusebox://fusebox-fuse-frontend-test"
+	options := []string{"--fake"}
+	if err := cd.Mount(ctx, source, "fusebox", options); err != nil {
+		s.Fatal("CrosDisks Mount call failed: ", err)
+	}
+	defer cd.Unmount(cleanupCtx, source, nil /* options */)
+
+	m, err := w.Wait(ctx)
+	if err != nil {
+		s.Fatal("Failed awaiting MountCompleted event: ", err)
+	} else if m.SourcePath != source {
+		s.Fatal("Failed invalid mount source: ", m.SourcePath)
+	} else if m.MountPath != "/media/fuse/fusebox-fuse-frontend-test" {
+		s.Fatal("Failed invalid mount point: ", m.MountPath)
+	} else {
+		s.Log("CrosDisks mounted ", m.MountPath)
+	}
+
+	// Send POSIX API test commands to the fusebox FUSE frontend.
+	output := []string{
+		ls(ctx, s, m.MountPath),
+		create(ctx, s, m.MountPath, "file"),
+		cp(ctx, s, m.MountPath, "hello", "copy"),
+		ls(ctx, s, m.MountPath),
+		read(ctx, s, m.MountPath, "copy"),
+		write(ctx, s, m.MountPath, "copy"),
+		read(ctx, s, m.MountPath, "copy"),
+		ls(ctx, s, m.MountPath),
+		seek(ctx, s, m.MountPath, "hello"),
+		mkdir(ctx, s, m.MountPath, "dir"),
+		create(ctx, s, m.MountPath, "dir/file"),
+		mkdir(ctx, s, m.MountPath, "dir/dir"),
+		ls(ctx, s, m.MountPath),
+		rename(ctx, s, m.MountPath, "file", "fail"),
+		ls(ctx, s, m.MountPath),
+		rm(ctx, s, m.MountPath, "file"),
+		rmdir(ctx, s, m.MountPath, "dir"),
+		ls(ctx, s, m.MountPath),
+		rm(ctx, s, m.MountPath, "copy"),
+		ls(ctx, s, m.MountPath),
+	}
+
+	if false { // Note: set true to see the POSIX command output.
+		for _, line := range output {
+			s.Logf("%s", line)
+		}
+	}
+}
