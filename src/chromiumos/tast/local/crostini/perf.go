6e46532bff6f160d8979b0fa764edb31b7213257
Nicholas Verne <nverne@chromium.org>
2022-05-31 11:29:48 +1000

    crostini: Capture startup perf data in fixtures
    In Chrome we have UMA metrics for the CrostiniRestarter.
    This is used by the installer and any subsequent crostini
    launches, so we sample the before and after values of
    the Crostini.RestarterTimeInState2 histograms before and
    after installing Crostini in the fixture.
    
    BUG=b:231511895
    TEST=tast run <DUT> crostini.xxx, check output directory for
    results.json file.
    
    Change-Id: I32c100d6fd8b8c5c2bae406cde05475d42636014

---
+++ src/chromiumos/tast/local/crostini/perf.go
@@ -0,0 +1,100 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package crostini
+
+import (
+	"context"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/metrics"
+)
+
+// RestartStages maps Chrome UMA metrics to corresponding crosbolt metrics.
+var RestartStages = map[string]string{
+	"Crostini.RestarterTimeInState2.Start":              "Start",
+	"Crostini.RestarterTimeInState2.InstallImageLoader": "InstallImageLoader",
+	"Crostini.RestarterTimeInState2.CreateDiskImage":    "CreateDiskImage",
+	"Crostini.RestarterTimeInState2.StartTerminaVm":     "StartTerminaVm",
+	"Crostini.RestarterTimeInState2.StartLxd":           "StartLxd",
+	"Crostini.RestarterTimeInState2.CreateContainer":    "CreateContainer",
+	"Crostini.RestarterTimeInState2.SetupContainer":     "SetupContainer",
+	"Crostini.RestarterTimeInState2.StartContainer":     "StartContainer",
+	"Crostini.RestarterTimeInState2.ConfigureContainer": "ConfigureContainer",
+}
+
+// Recording holds data for sampling Chrome metrics and emitting corresponding
+// perf.Values to a file which can be consumed by crosbolt.
+type Recording struct {
+	metricBaseName string
+	variants       map[string]string
+	recorder       *metrics.Recorder
+	ctx            context.Context
+	tconn          *chrome.TestConn
+}
+
+// NewRecording creates a Recording object from a map of Chrome metric to
+// perf.Metric variant names.
+func NewRecording(metricBaseName string, variants map[string]string) *Recording {
+	return &Recording{metricBaseName: metricBaseName, variants: variants}
+}
+
+// Start samples the initial chrome metric values from which to record changes
+// using UpdatePerf (below).
+func (r *Recording) Start(ctx context.Context, tconn *chrome.TestConn) error {
+	names := make([]string, len(r.variants))
+	for n := range r.variants {
+		names = append(names, n)
+	}
+	recorder, err := metrics.StartRecorder(ctx, tconn, names...)
+	if err != nil {
+		return err
+	}
+	r.recorder = recorder
+	r.ctx = ctx
+	r.tconn = tconn
+	return nil
+}
+
+// UpdatePerf records changes in the Recording's Chrome metrics, appending the
+// bucket mid-points to |values|, then saves them in outDir when outDir is not
+// an empty string.
+func (r *Recording) UpdatePerf(values *perf.Values, outDir string) error {
+	if r.recorder == nil {
+		return errors.New("There is no recorder for perf metrics")
+	}
+	diffs, err := r.recorder.Histogram(r.ctx, r.tconn)
+	if err != nil {
+		return errors.Wrap(err, "couldn't create metrics.Recorder")
+	}
+	for _, hist := range diffs {
+		if len(hist.Buckets) > 0 {
+			variant, ok := r.variants[hist.Name]
+			if !ok {
+				continue
+			}
+			metric := perf.Metric{
+				Name:      r.metricBaseName,
+				Variant:   variant,
+				Unit:      "ms",
+				Direction: perf.SmallerIsBetter,
+				Multiple:  true,
+			}
+			for _, bucket := range hist.Buckets {
+				mid := float64(bucket.Max+bucket.Min) * 0.5
+				for i := 0; i < int(bucket.Count); i++ {
+					values.Append(metric, mid)
+				}
+			}
+		}
+	}
+	if len(outDir) > 0 {
+		if err = values.Save(outDir); err != nil {
+			return errors.Wrapf(err, "couldn't save metrics to %q", outDir)
+		}
+	}
+	return nil
+}
