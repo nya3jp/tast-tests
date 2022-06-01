3b85546bc2731487c936bff1ce94a5f7023f9449
Jie Jiang <jiejiang@chromium.org>
2022-06-01 17:46:42 +0900

    network: virtualnet: Add CreateRouterServerEnv()
    The new added function sets up an environment with two Env objects: one
    for router, which will be managed by shill; and one for server, which is
    connected the router env. This setup is useful to verify the default
    routes by checking the connectivity to services on the server env.
    
    BUG=b:192436642
    TEST=checked static IP addresses in the server Env are pingable on DUT
    
    Change-Id: I5ee3dc88856daaebbb723f1033d73d7f47145595

--- src/chromiumos/tast/local/bundles/cros/network/virtualnet/dnsmasq/dnsmasq.go
+++ src/chromiumos/tast/local/bundles/cros/network/virtualnet/dnsmasq/dnsmasq.go
@@ -87,11 +87,9 @@ func (d *dnsmasq) Start(ctx context.Context, env *env.Env) error {
 	}
 
 	// Install gateway address and routes.
-	if err := d.env.RunWithoutChroot(ctx, "ip", "addr", "add", gateway.String(), "dev", d.env.VethInName); err != nil {
-		return errors.Wrap(err, "failed to install gateway address")
-	}
-	if err := d.env.RunWithoutChroot(ctx, "ip", "route", "add", d.subnet.String(), "dev", d.env.VethInName); err != nil {
-		return errors.Wrap(err, "failed to install route")
+	d.env.VethInStaticIPv4Addr = gateway
+	if err := d.env.InstallIPAndRoute(ctx, d.env.VethInName, gateway, d.subnet); err != nil {
+		return errors.Wrap(err, "failed to configure IPv4 in netns")
 	}
 
 	// Start the command.
