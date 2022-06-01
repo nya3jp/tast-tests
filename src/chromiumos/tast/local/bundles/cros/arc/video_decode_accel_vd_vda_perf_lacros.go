78fb8d37b5b4be8391991a0d2780187d86b4053b
Bartłomiej Grzesik <bag@semihalf.com>
2022-06-01 10:24:26 +0000

    Add lacros variants to video tasts for testing
    This CL is intended to help compare test results. It is not supposed to
    land.
    
    Change-Id: If17cbf16afd264a6e860f9825071bbd6026fe0f7

---
+++ src/chromiumos/tast/local/bundles/cros/arc/video_decode_accel_vd_vda_perf_lacros.go
@@ -0,0 +1,116 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/local/media/decoding"
+	"chromiumos/tast/testing"
+)
+
+type videoDecodeAccelVDVDAPerfTestParam struct {
+	dataPath        string
+	useLinearOutput bool
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VideoDecodeAccelVDVDAPerfLacros,
+		Desc:         "Measures performance of hardware decode acceleration performance using media::VideoDecoder through the VDA interface, by running the video_decode_accelerator_perf_tests binary (see go/vd-migration)",
+		Contacts:     []string{},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"arc", "chrome", "video_decoder_direct", "lacros"},
+        Fixture:      "lacrosWithArcBooted",
+		Timeout:      4 * time.Minute,
+		Params: []testing.Param{{
+			Name:              "h264_1080p_30fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.h264"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+			ExtraData:         []string{"1080p_30fps_300frames.h264", "1080p_30fps_300frames.h264.json"},
+		}, {
+			Name:              "h264_1080p_60fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_60fps_600frames.h264"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_60},
+			ExtraData:         []string{"1080p_60fps_600frames.h264", "1080p_60fps_600frames.h264.json"},
+		}, {
+			Name:              "h264_2160p_30fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_30fps_300frames.h264"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K},
+			ExtraData:         []string{"2160p_30fps_300frames.h264", "2160p_30fps_300frames.h264.json"},
+		}, {
+			Name:              "h264_2160p_60fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_60fps_600frames.h264"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeH264_4K60},
+			ExtraData:         []string{"2160p_60fps_600frames.h264", "2160p_60fps_600frames.h264.json"},
+		}, {
+			Name:              "vp8_1080p_30fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.vp8.ivf"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+			ExtraData:         []string{"1080p_30fps_300frames.vp8.ivf", "1080p_30fps_300frames.vp8.ivf.json"},
+		}, {
+			Name:              "vp8_1080p_60fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_60fps_600frames.vp8.ivf"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_60},
+			ExtraData:         []string{"1080p_60fps_600frames.vp8.ivf", "1080p_60fps_600frames.vp8.ivf.json"},
+		}, {
+			Name:              "vp8_2160p_30fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_30fps_300frames.vp8.ivf"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_4K},
+			ExtraData:         []string{"2160p_30fps_300frames.vp8.ivf", "2160p_30fps_300frames.vp8.ivf.json"},
+		}, {
+			Name:              "vp8_2160p_60fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_60fps_600frames.vp8.ivf"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP8_4K60},
+			ExtraData:         []string{"2160p_60fps_600frames.vp8.ivf", "2160p_60fps_600frames.vp8.ivf.json"},
+		}, {
+			Name:              "vp9_1080p_30fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.vp9.ivf"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+			ExtraData:         []string{"1080p_30fps_300frames.vp9.ivf", "1080p_30fps_300frames.vp9.ivf.json"},
+		}, {
+			Name:              "vp9_1080p_60fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_60fps_600frames.vp9.ivf"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_60},
+			ExtraData:         []string{"1080p_60fps_600frames.vp9.ivf", "1080p_60fps_600frames.vp9.ivf.json"},
+		}, {
+			Name:              "vp9_2160p_30fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_30fps_300frames.vp9.ivf"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_4K},
+			ExtraData:         []string{"2160p_30fps_300frames.vp9.ivf", "2160p_30fps_300frames.vp9.ivf.json"},
+		}, {
+			Name:              "vp9_2160p_60fps",
+			Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "2160p_60fps_600frames.vp9.ivf"},
+			ExtraSoftwareDeps: []string{caps.HWDecodeVP9_4K60},
+			ExtraData:         []string{"2160p_60fps_600frames.vp9.ivf", "2160p_60fps_600frames.vp9.ivf.json"},
+		},
+			{
+				Name:              "h264_linear_output_1080p_30fps",
+				Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.h264", useLinearOutput: true},
+				ExtraSoftwareDeps: []string{caps.HWDecodeH264},
+				ExtraData:         []string{"1080p_30fps_300frames.h264", "1080p_30fps_300frames.h264.json"},
+			},
+			{
+				Name:              "vp8_linear_output_1080p_30fps",
+				Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.vp8.ivf", useLinearOutput: true},
+				ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
+				ExtraData:         []string{"1080p_30fps_300frames.vp8.ivf", "1080p_30fps_300frames.vp8.ivf.json"},
+			}, {
+				Name:              "vp9_linear_output_1080p_30fps",
+				Val:               videoDecodeAccelVDVDAPerfTestParam{dataPath: "1080p_30fps_300frames.vp9.ivf", useLinearOutput: true},
+				ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
+				ExtraData:         []string{"1080p_30fps_300frames.vp9.ivf", "1080p_30fps_300frames.vp9.ivf.json"},
+			}},
+	})
+}
+
+func VideoDecodeAccelVDVDAPerfLacros(ctx context.Context, s *testing.State) {
+	param := s.Param().(videoDecodeAccelVDVDAPerfTestParam)
+	if err := decoding.RunAccelVideoPerfTest(ctx, s.OutDir(), s.DataPath(param.dataPath), decoding.TestParams{DecoderType: decoding.VDVDA, LinearOutput: param.useLinearOutput, TestCases: decoding.CappedFlag | decoding.UncappedFlag}); err != nil {
+		s.Fatal("test failed: ", err)
+	}
+}
---
+++ src/chromiumos/tast/local/bundles/cros/arc/video_encode_accel_lacros.go
@@ -0,0 +1,205 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arc/c2e2etest"
+	"chromiumos/tast/local/bundles/cros/arc/video"
+	"chromiumos/tast/local/media/videotype"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+const (
+	// Enable to cache the extracted raw video to speed up the test.
+	veaCacheExtractedVideo = false
+	// Enable to download the video file encoded by the test.
+	veaPullEncodedVideo = false
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VideoEncodeAccelLacros,
+		LacrosStatus: testing.LacrosVariantUnknown,
+		Desc:         "Verifies ARC++ and ARCVM hardware encode acceleration by running the arcvideoencoder_test binary",
+		Contacts:     []string{},
+		Attr:         []string{"group:mainline", "informational"},
+		Data:         []string{c2e2etest.X86ApkName, c2e2etest.ArmApkName},
+		SoftwareDeps: []string{"chrome", "lacros"},
+		Fixture:      "lacrosWithArcBootedWithVideoLogging",
+		Timeout:      12 * time.Minute,
+		Params: []testing.Param{{
+			Name: "h264_192p_i420",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Bear192P,
+				PixelFormat: videotype.I420,
+			},
+			ExtraData:         []string{video.Bear192P.Name},
+			ExtraSoftwareDeps: []string{"android_p", caps.HWEncodeH264},
+		}, {
+			Name: "h264_192p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Bear192P,
+				PixelFormat: videotype.I420,
+			},
+			ExtraData:         []string{video.Bear192P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeH264},
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform(video.EncoderBlocklistVM...)),
+		}, {
+			Name: "h264_360p_i420",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Tulip360P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Tulip360P.Name},
+			ExtraSoftwareDeps: []string{"android_p", caps.HWEncodeH264},
+		}, {
+			Name: "h264_360p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Tulip360P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Tulip360P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeH264},
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform(video.EncoderBlocklistVM...)),
+		}, {
+			Name: "h264_720p_i420",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Tulip720P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Tulip720P.Name},
+			ExtraSoftwareDeps: []string{"android_p", caps.HWEncodeH264},
+		}, {
+			Name: "h264_720p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Tulip720P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Tulip720P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeH264},
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform(video.EncoderBlocklistVM...)),
+		}, {
+			Name: "h264_1080p_i420",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Crowd1080P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Crowd1080P.Name},
+			ExtraSoftwareDeps: []string{"android_p", caps.HWEncodeH264},
+		}, {
+			Name: "h264_1080p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.H264MainProf,
+				Params:      video.Crowd1080P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Crowd1080P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeH264},
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform(video.EncoderBlocklistVM...)),
+		}, {
+			Name: "vp8_192p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP8Prof,
+				Params:      video.Bear192P,
+				PixelFormat: videotype.I420,
+			},
+			ExtraData:         []string{video.Bear192P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP8},
+			ExtraHardwareDeps: hwdep.D(
+				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
+				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
+		}, {
+			Name: "vp8_360p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP8Prof,
+				Params:      video.Tulip360P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Tulip360P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP8},
+			ExtraHardwareDeps: hwdep.D(
+				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
+				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
+		}, {
+			Name: "vp8_720p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP8Prof,
+				Params:      video.Tulip720P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Tulip720P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP8},
+			ExtraHardwareDeps: hwdep.D(
+				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
+				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
+		}, {
+			Name: "vp8_1080p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP8Prof,
+				Params:      video.Crowd1080P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Crowd1080P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP8},
+			ExtraHardwareDeps: hwdep.D(
+				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
+				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
+		}, {
+			Name: "vp9_192p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP9Prof,
+				Params:      video.Bear192P,
+				PixelFormat: videotype.I420,
+			},
+			ExtraData:         []string{video.Bear192P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP9},
+			ExtraHardwareDeps: hwdep.D(
+				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
+				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
+		}, {
+			Name: "vp9_360p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP9Prof,
+				Params:      video.Tulip360P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Tulip360P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP9},
+			ExtraHardwareDeps: hwdep.D(
+				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
+				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
+		}, {
+			Name: "vp9_720p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP9Prof,
+				Params:      video.Tulip720P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Tulip720P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP9},
+			ExtraHardwareDeps: hwdep.D(
+				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
+				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
+		}, {
+			Name: "vp9_1080p_i420_vm",
+			Val: video.EncodeTestOptions{
+				Profile:     videotype.VP9Prof,
+				Params:      video.Crowd1080P,
+				PixelFormat: videotype.I420},
+			ExtraData:         []string{video.Crowd1080P.Name},
+			ExtraSoftwareDeps: []string{"android_vm", caps.HWEncodeVP9},
+			ExtraHardwareDeps: hwdep.D(
+				hwdep.SkipOnPlatform(video.EncoderBlocklistVM...),
+				hwdep.Platform(video.EncoderAllowlistVPxVM...)),
+		}},
+	})
+}
+
+func VideoEncodeAccelLacros(ctx context.Context, s *testing.State) {
+	video.RunARCVideoTest(ctx, s, s.FixtValue().(*arc.PreData).ARC,
+		s.Param().(video.EncodeTestOptions), veaPullEncodedVideo, veaCacheExtractedVideo)
+}
