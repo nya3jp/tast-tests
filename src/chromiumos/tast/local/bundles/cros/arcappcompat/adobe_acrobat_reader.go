639bd5a6c9cf44ec072bba4d7956e71d5e4f8efd
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2022-05-31 17:29:16 -0700

    arcappcompat: add appcompat top apps suite to top arcappcompat tests
    BUG=b:227769823
    TEST=test_that --board=eve <ip-address> tast.appcompat_top_apps-shard-0
    test_that --board=eve <ip-address> tast.appcompat_top_apps-shard-1
    test_that --board=eve <ip-address> tast.appcompat_top_apps-shard-2
    test_that --board=eve <ip-address> tast.appcompat_top_apps-shard-3
    test_that --board=eve <ip-address> tast.appcompat_top_apps-shard-4
    
    Cq-Depend: chromium:3681821
    Change-Id: I47d6801606916ea7bf06b3f76239dba10e061fa5

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/adobe_acrobat_reader.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/adobe_acrobat_reader.go
@@ -34,7 +34,7 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Functional test for AdobeAcrobatReader that installs the app also verifies it is logged in and that the main page is open, checks AdobeAcrobatReader correctly changes the window state in both clamshell and touchview mode",
 		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:appcompat", "appcompat_smoke"},
+		Attr:         []string{"group:appcompat", "appcompat_smoke", "appcompat_top_apps"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Name: "clamshell_mode",
