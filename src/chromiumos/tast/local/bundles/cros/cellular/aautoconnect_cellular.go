2b1be66ba35e4b968a6b62d4f2c2562167ddace4
Pavan Holla <pholla@chromium.org>
2022-06-02 14:51:07 +0000

    cellular: Promote autoconnect test to stable suite.
    BUG=b:233604061
    TEST=tast run '("group:cellular" && "cellular_sim_active" && !"cellular_unstable")'
    
    Change-Id: I5147ce783071127847b3b2f353051a28bcb6f3b4

--- src/chromiumos/tast/local/bundles/cros/cellular/aautoconnect_cellular.go
+++ src/chromiumos/tast/local/bundles/cros/cellular/aautoconnect_cellular.go
@@ -22,7 +22,7 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Verifies that host has network connectivity via cellular interface",
 		Contacts:     []string{"pholla@google.com", "chromeos-cellular-team@google.com"},
-		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_active"},
+		Attr:         []string{"group:cellular", "cellular_sim_active"},
 		Timeout:      3 * time.Minute,
 		SoftwareDeps: []string{"chrome"},
 	})
