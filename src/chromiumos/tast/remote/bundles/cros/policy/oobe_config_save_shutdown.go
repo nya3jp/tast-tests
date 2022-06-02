04c910fbe04f66cd5aad08a52860470cdce17f44
Miriam Polzer <mpolzer@google.com>
2022-06-02 12:34:28 +0000

    policy: Logging and reordering oobe_config_save
    In the test, oobe_config_save was running too early because it runs once
    Chrome is restarted. Reorder the commands.
    
    Also add logging for now. I noticed that oobe_config_save ran while the
    previous test was unfinished. This may indicate race conditions in tast
    itself. The logs may help to clarify.
    
    BUG=b:234698759
    TEST=tast run <dut> policy.OobeConfigSaveShutdown
    
    Change-Id: I05931d279adffc8174aef60b5d69c37a365e752a

--- src/chromiumos/tast/remote/bundles/cros/policy/oobe_config_save_shutdown.go
+++ src/chromiumos/tast/remote/bundles/cros/policy/oobe_config_save_shutdown.go
@@ -34,6 +34,20 @@ func init() {
 }
 
 func OobeConfigSaveShutdown(ctx context.Context, s *testing.State) {
+	// Setting up Chrome will restart ui job which would trigger a reboot once update is pending.
+	// Thus, already set it up here.
+	s.Log("Connecting to client")
+	client, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	s.Log("Logging into Chrome testing")
+	service := autoupdate.NewUpdateUIServiceClient(client.Conn)
+	if _, err = service.New(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Failed log into Chrome: ", err)
+	}
+
+	s.Log("Adding data save flag")
 	if err := s.DUT().Conn().CommandContext(ctx, "touch", "/mnt/stateful_partition/.save_rollback_data").Run(); err != nil {
 		s.Fatal("Failed to initiate rollback data save: ", err)
 	}
@@ -42,17 +56,7 @@ func OobeConfigSaveShutdown(ctx context.Context, s *testing.State) {
 		s.DUT().Conn().CommandContext(ctx, "rm", "/mnt/stateful_partition/.save_rollback_data").Run()
 	}(ctx)
 
-	// Setting up Chrome will restart ui job which would trigger a reboot once update is pending.
-	// Thus, already set it up here.
-	client, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
-	if err != nil {
-		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
-	}
-	service := autoupdate.NewUpdateUIServiceClient(client.Conn)
-	if _, err = service.New(ctx, &empty.Empty{}); err != nil {
-		s.Fatal("Failed log into Chrome: ", err)
-	}
-
+	s.Log("Faking pending update")
 	if err := s.DUT().Conn().CommandContext(ctx, "update_engine_client", "--set_status=6").Run(); err != nil {
 		s.Fatal("Failed to fake a pending update: ", err)
 	}
@@ -61,6 +65,7 @@ func OobeConfigSaveShutdown(ctx context.Context, s *testing.State) {
 		s.DUT().Conn().CommandContext(ctx, "restart", "update-engine").Run()
 	}(ctx)
 
+	s.Log("Clicking relaunch button")
 	if _, err = service.RelaunchAfterUpdate(ctx, &empty.Empty{}); err != nil {
 		s.Fatal("Failed to click relaunch on the test device: ", err)
 	}
@@ -73,12 +78,13 @@ func OobeConfigSaveShutdown(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to reconnect to DUT after relaunch: ", err)
 	}
 
+	s.Log("Waiting for log files and data save flag")
 	// Need to poll here because it may take a bit longer for content in /var to be readable after relaunch.
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
 		// Note that this check relies on log format in /var/log/messages.
 		logs, _ := s.DUT().Conn().CommandContext(ctx, "sh", "-c", `grep "INFO oobe_config_save" /var/log/messages`).Output()
 		if len(string(logs)) == 0 {
-			return errors.New("found no indication of oobe_config_save running in the /var/log/messages")
+			return errors.New("found no indication of oobe_config_save running in /var/log/messages")
 		}
 
 		logs, _ = s.DUT().Conn().CommandContext(ctx, "sh", "-c", `grep "ERR oobe_config_save" /var/log/messages`).Output()
