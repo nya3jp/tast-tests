1f6e0e4a9a14480c477b667c10daac28c10297d7
Michael Rygiel <michaelrygiel@google.com>
2022-06-03 19:47:13 +0000

    network: Add http server to virtualnet package
    This builds upon the virtualnet package and adds an http server. The
    captive portal environment creates a separate network namespace
    connected to the global namespace through a virtual ethernet pair. An
    http server and dns server are created in the new network namespace.
    When a portal recheck is called in shill, the dns will reroute the http
    call to the http server, which will cause the service state to be
    changed to redirect-found.
    
    BUG=b:234866776
    TEST=tast run ${DUT} network.ShillCaptivePortalHTTP, checked that
    rechecking portal on veth will change the service state to online
    
    Cq-Depend: chromium:3670620
    Change-Id: I763bf185e3a471edf870972105a38f39897575fc

---
+++ src/chromiumos/tast/local/bundles/cros/network/shill_captive_portal_http.go
@@ -0,0 +1,79 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/shillconst"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/network/virtualnet"
+	"chromiumos/tast/local/bundles/cros/network/virtualnet/subnet"
+	"chromiumos/tast/local/shill"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     ShillCaptivePortalHTTP,
+		Desc:     "Ensures that setting up a virtual ethernet pair with a DNS server that points portal detection queries to an http server that responds with '302 Redirect' with a Redirect URL. This results in a service state of 'redirect-found' for the virtural ethernet service",
+		Contacts: []string{"michaelrygiel@google.com", "cros-networking@google.com"},
+		Attr:     []string{"group:mainline", "informational"},
+		Fixture:  "shillReset",
+	})
+}
+
+func ShillCaptivePortalHTTP(ctx context.Context, s *testing.State) {
+	m, err := shill.NewManager(ctx)
+	if err != nil {
+		s.Fatal("Failed to create manager proxy: ", err)
+	}
+
+	testing.ContextLog(ctx, "Enabling portal detection on ethernet")
+	// Relying on shillReset test fixture to undo the enabling of portal detection.
+	if err := m.EnablePortalDetection(ctx); err != nil {
+		s.Fatal("Enable Portal Detection failed: ", err)
+	}
+
+	opts := virtualnet.EnvOptions{
+		Priority:              5,
+		NameSuffix:            "",
+		EnableDHCP:            true,
+		RAServer:              false,
+		HTTPServer:            true,
+		AddressToForceGateway: "www.gstatic.com",
+	}
+	pool := subnet.NewPool()
+	service, portalEnv, err := virtualnet.CreateRouterEnv(ctx, m, pool, opts)
+	if err != nil {
+		s.Fatal("Failed to create a portal env: ", err)
+	}
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+	defer portalEnv.Cleanup(cleanupCtx)
+
+	pw, err := service.CreateWatcher(ctx)
+	if err != nil {
+		s.Fatal("Failed to create watcher: ", err)
+	}
+	defer pw.Close(cleanupCtx)
+	s.Log("Make service restart portal detector")
+	if err := m.RecheckPortal(ctx); err != nil {
+		s.Fatal("Failed to invoke RecheckPortal on shill: ", err)
+	}
+
+	timeoutCtx, cancel := context.WithTimeout(ctx, 35*time.Second)
+	defer cancel()
+	s.Log("Check if service state is 'redirect-found'")
+	var ServiceRedirectState = []interface{}{
+		shillconst.ServiceStateRedirectFound,
+	}
+	_, err = pw.ExpectIn(timeoutCtx, shillconst.ServicePropertyState, ServiceRedirectState)
+	if err != nil {
+		s.Fatal("Service state is unexpected: ", err)
+	}
+}
