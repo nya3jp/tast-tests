2b93fe8bb6c6cd6d7b76af9e60cc834af6124797
Maksim Ivanov <emaxx@chromium.org>
2022-06-03 02:36:42 +0200

    tast-tests: Test kiosk file persistency
    Extend cryptohome.KioskSession to also check the file system
    preservation: a file written into the cryptohome is still available
    after remounting it.
    
    BUG=b:234775539
    TEST=tast run cryptohome.KioskSession
    
    Change-Id: I938f2296e80a003e2e200e80752551b9a73733fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3687853
    Reviewed-by: Po-Hsien Wang <pwang@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Tested-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Joseph Pang <pjlee@google.com>
    Reviewed-by: Hardik Goyal <hardikgoyal@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/cryptohome/kiosk_session.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/kiosk_session.go
@@ -5,8 +5,13 @@
 package cryptohome
 
 import (
+	"bytes"
 	"context"
+	"io/ioutil"
+	"path/filepath"
+	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/cryptohome"
 	"chromiumos/tast/testing"
 )
@@ -24,19 +29,57 @@ func init() {
 }
 
 func KioskSession(ctx context.Context, s *testing.State) {
+	const (
+		testFile        = "file"
+		testFileContent = "content"
+		cleanupTime     = 20 * time.Second
+	)
+
+	ctxForCleanUp := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, cleanupTime)
+	defer cancel()
+
 	// Unmount all user vaults before we start.
 	if err := cryptohome.UnmountAll(ctx); err != nil {
 		s.Log("Failed to unmount all before test starts: ", err)
 	}
 
+	// Create and mount the kiosk for the first time.
 	if err := cryptohome.MountKiosk(ctx); err != nil {
 		s.Fatal("Failed to mount kiosk: ", err)
 	}
-	// Unmount Vault.
+	defer cryptohome.UnmountAll(ctxForCleanUp)
+
+	// Write a test file to verify persistence.
+	userPath, err := cryptohome.UserPath(ctx, cryptohome.KioskUser)
+	if err != nil {
+		s.Fatal("Failed to get kiosk user vault path: ", err)
+	}
+	filePath := filepath.Join(userPath, testFile)
+	if err := ioutil.WriteFile(filePath, []byte(testFileContent), 0644); err != nil {
+		s.Fatal("Failed to write a file to the vault: ", err)
+	}
+
+	// Unmount and mount again.
+	cryptohome.UnmountVault(ctx, cryptohome.KioskUser)
+	if _, err := ioutil.ReadFile(filePath); err == nil {
+		s.Fatal("File is readable after unmount")
+	}
+	if err := cryptohome.MountKiosk(ctx); err != nil {
+		s.Fatal("Failed to mount kiosk: ", err)
+	}
+
+	// Verify that file is still there.
+	if content, err := ioutil.ReadFile(filePath); err != nil {
+		s.Fatal("Failed to read back test file: ", err)
+	} else if bytes.Compare(content, []byte(testFileContent)) != 0 {
+		s.Fatalf("Incorrect tests file content. got: %q, want: %q", content, testFileContent)
+	}
+
 	cryptohome.UnmountVault(ctx, cryptohome.KioskUser)
 
 	// Test if existing kiosk vault can be signed in with AuthSession.
 	if err := cryptohome.AuthSessionMountFlow(ctx, true /*Kiosk User*/, cryptohome.KioskUser, "" /* Empty passkey*/, false /*Create User*/); err != nil {
-		s.Fatal("Failed to Mount with AuthSession -: ", err)
+		s.Fatal("Failed to Mount with AuthSession: ", err)
 	}
 }
