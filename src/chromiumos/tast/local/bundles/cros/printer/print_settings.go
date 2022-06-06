3544e210f34294c4792111f56334493f20ddf5d0
Bryan Cain <bryancain@chromium.org>
2022-06-06 11:15:46 -0600

    tast-tests: add test for adjusting settings in print dialog
    This test uses the functions added in the previous few commits.
    
    BUG=b:195063930
    TEST=tast run printer.PrintSettings
    
    Change-Id: Ic73dfcca38145b75e3b876e50c1ce0f1ba6046af

---
+++ src/chromiumos/tast/local/bundles/cros/printer/print_settings.go
@@ -0,0 +1,202 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package printer
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/printmanagementapp"
+	"chromiumos/tast/local/chrome/uiauto/printpreview"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/printing/printer"
+	"chromiumos/tast/local/printing/usbprinter"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PrintSettings,
+		LacrosStatus: testing.LacrosVariantNeeded,
+		Desc:         "Tests that settings can be set in the print dialog",
+		Contacts:     []string{"cros-peripherals@google.com"},
+		Attr: []string{
+			"group:mainline",
+			"informational",
+			"group:paper-io",
+			"paper-io_printing",
+		},
+		Timeout:      2 * time.Minute,
+		SoftwareDeps: []string{"chrome", "cros_internal", "cups", "virtual_usb_printer"},
+		Fixture:      "virtualUsbPrinterModulesLoaded",
+	})
+}
+
+func PrintSettings(ctx context.Context, s *testing.State) {
+	const (
+		settingsLabel = "Printers"
+		settingsPage  = "osPrinting"
+	)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Chrome instance: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	s.Log("Installing printer")
+	if err := printer.ResetCups(ctx); err != nil {
+		s.Fatal("Failed to reset cupsd: ", err)
+	}
+
+	printer, err := usbprinter.Start(ctx,
+		usbprinter.WithIPPUSBDescriptors(),
+		usbprinter.WithGenericIPPAttributes(),
+		usbprinter.WaitUntilConfigured())
+	if err != nil {
+		s.Fatal("Failed to start IPP-over-USB printer: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := printer.Stop(ctx); err != nil {
+			s.Error("Failed to stop printer: ", err)
+		}
+	}(ctx)
+
+	// Open OS Settings and navigate to the Printing page.
+	ui := uiauto.New(tconn)
+	entryFinder := nodewith.Name(settingsLabel).Role(role.Link).Ancestor(ossettings.WindowFinder)
+	if _, err := ossettings.LaunchAtPageURL(ctx, tconn, cr, settingsPage, ui.Exists(entryFinder)); err != nil {
+		s.Fatal("Failed to launch Settings page: ", err)
+	}
+
+	const printerName = "DavieV Virtual USB Printer (USB)"
+	const printerLabel = "DavieV Virtual USB Printer (USB) DavieV Virtual USB Printer (USB)"
+	savePrinterButton := nodewith.ClassName("save-printer-button").NameContaining(printerName).Ancestor(ossettings.WindowFinder)
+	editPrinterButton := nodewith.ClassName("icon-more-vert").Ancestor(ossettings.WindowFinder)
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get the keyboard: ", err)
+	}
+	defer kb.Close()
+
+	if err := uiauto.Combine("click Settings Printer entry, save printer, and open Print Preview with shortcut Ctrl+P",
+		ui.LeftClick(entryFinder),
+		ui.LeftClick(savePrinterButton),
+		ui.WithTimeout(time.Minute).WaitUntilExists(editPrinterButton),
+		kb.AccelAction("Ctrl+P"),
+		printpreview.WaitForPrintPreview(tconn),
+	)(ctx); err != nil {
+		s.Fatal("Failed to save virtual USB printer and open Print Preview: ", err)
+	}
+
+	// Select printer and click Print button.
+	s.Log("Selecting printer")
+	if err := printpreview.SelectPrinter(ctx, tconn, printerLabel); err != nil {
+		s.Fatal("Failed to select printer: ", err)
+	}
+
+	if err := printpreview.ExpandMoreSettings(ctx, tconn); err != nil {
+		s.Fatal("Failed to expand 'more settings': ", err)
+	}
+
+	if err := printpreview.SetDropdown(ctx, tconn, "Layout", "Landscape"); err != nil {
+		s.Fatal("Failed to set layout dropdown: ", err)
+	}
+
+	if err := printpreview.SetPaperSize(ctx, tconn, "A4"); err != nil {
+		s.Fatal("Failed to set paper size: ", err)
+	}
+
+	if err := printpreview.SetMargins(ctx, tconn, "Minimum"); err != nil {
+		s.Fatal("Failed to set margins: ", err)
+	}
+
+	if err := printpreview.SetPagesPerSheet(ctx, tconn, "2"); err != nil {
+		s.Fatal("Failed to set pages per sheet: ", err)
+	}
+
+	if err := printpreview.SetScaleDefault(ctx, tconn); err != nil {
+		s.Fatal("Failed to set scale to default: ", err)
+	}
+
+	if err := printpreview.SetCheckboxState(ctx, tconn, "Headers and footers", false); err != nil {
+		s.Fatal("Failed to set checkbox state: ", err)
+	}
+
+	if err := printpreview.SetScaleCustom(ctx, tconn, 71); err != nil {
+		s.Fatal("Failed to set scale to custom: ", err)
+	}
+
+	if err := printpreview.OpenAdvancedSettings(ctx, tconn); err != nil {
+		s.Fatal("Failed to open advanced settings dialog: ", err)
+	}
+
+	if err := printpreview.SetAdvancedSetting(ctx, tconn, "Job sheets", "Standard"); err != nil {
+		s.Fatal("Failed to set advanced setting: ", err)
+	}
+
+	if err := printpreview.SetAdvancedSetting(ctx, tconn, "Page delivery", "same-order"); err != nil {
+		s.Fatal("Failed to set advanced setting: ", err)
+	}
+
+	/*if err := printpreview.SetAdvancedSetting(ctx, tconn, "", "this should fail"); err != nil {
+		s.Fatal("Failed to set advanced setting: ", err)
+	}*/
+
+	if err := printpreview.CloseAdvancedSettings(ctx, tconn); err != nil {
+		s.Fatal("Failed to close advanced settings dialog: ", err)
+	}
+
+	if err = printpreview.Print(ctx, tconn); err != nil {
+		s.Fatal("Failed to print: ", err)
+	}
+
+	s.Log("Waiting for print job to complete")
+	if err = testing.Poll(ctx, func(ctx context.Context) error {
+		out, err := testexec.CommandContext(ctx, "lpstat", "-W", "completed", "-o").Output(testexec.DumpLogOnError)
+		if err != nil {
+			return err
+		}
+		if len(out) == 0 {
+			return errors.New("Print job has not completed yet")
+		}
+		testing.ContextLog(ctx, "Print job has completed")
+		return nil
+	}, nil); err != nil {
+		s.Fatal("Print job failed to complete: ", err)
+	}
+
+	// Launch Print Management app.
+	printManagementApp, err := printmanagementapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch Print Management app: ", err)
+	}
+
+	if err := uiauto.Combine("Verify print job",
+		printManagementApp.VerifyHistoryLabel(),
+		printManagementApp.VerifyPrintJob(),
+	)(ctx); err != nil {
+		s.Fatal("Failed to verify print job: ", err)
+	}
+}
