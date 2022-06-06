89863b7b96dfb6ef89c6f89a5d9d1f8b8e0f7bb9
Jie Jiang <jiejiang@chromium.org>
2022-06-06 20:50:58 +0900

    network: virtualnet: Read ip addrs on ifaces
    This patch adds the code to read the ip addrs on the interfaces from the
    kernel. It is required because some addresses are unknown to the test
    codes (e.g., IPv6 addresses acquired by SLAAC).
    
    Also adds a util function which can be used to enter the netns
    associated with the env.
    
    BUG=b:192436642
    TEST=../platform/tast/fast_build.sh -T
    
    Change-Id: I76b16a5852a6584ff4c4c3e7b2cedd8330bea1bc

--- src/chromiumos/tast/local/bundles/cros/network/virtualnet/env/env.go
+++ src/chromiumos/tast/local/bundles/cros/network/virtualnet/env/env.go
@@ -7,10 +7,15 @@ package env
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"syscall"
+
+	"golang.org/x/sys/unix"
 
 	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
@@ -166,6 +171,153 @@ func (e *Env) StartServer(ctx context.Context, name string, server server) error
 		return errors.Wrapf(err, "failed to start server %s", name)
 	}
 	return nil
+}
+
+// IfaceAddrs represents the IP addresses configured on an interface.
+type IfaceAddrs struct {
+	// IPv4Addr is the IPv4 address on the interface. There is only one IPv4
+	// address on an interface.
+	IPv4Addr net.IP
+	// IPv6Addrs is the list of IPv6 addresses (excluding link-local address) on
+	// the interface.
+	IPv6Addrs []net.IP
+}
+
+// All returns all addresses (excluding the IPv6 link-local address) on this
+// interface.
+func (addrs *IfaceAddrs) All() []net.IP {
+	var ret []net.IP
+	if addrs.IPv4Addr != nil {
+		ret = append(ret, addrs.IPv4Addr)
+	}
+	return append(ret, addrs.IPv6Addrs...)
+}
+
+// GetVethInAddrs returns the current IP addresses configured on the veth
+// interface inside this Env. Note that this function reads the addresses from
+// the kernel directly, which also includes syscalls to change the netns, and
+// thus it's better that the caller caches the results if possible. Note that if
+// an address is configured dynamically (e.g., the IPv6 SLAAC address), it may
+// not be ready immediately after the Env is ready (or the corresponding server
+// starts).
+func (e *Env) GetVethInAddrs(ctx context.Context) (retAddrs *IfaceAddrs, retErr error) {
+	cleanup, err := e.EnterNetNS(ctx)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to enter the associated netns %s", e.NetNSName)
+	}
+	defer func() {
+		if tempErr := cleanup(); tempErr != nil {
+			testing.ContextLogf(ctx, "Failed to go back to the original netns from netns %s: %v", e.NetNSName, tempErr)
+			if retErr == nil {
+				retErr = tempErr
+			}
+		}
+	}()
+	iface, err := net.InterfaceByName(e.VethInName)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get interface object for the in interface")
+	}
+
+	addrs, err := iface.Addrs()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to list addrs on the in interface")
+	}
+
+	// Each object in |addrs| implements the net.Addr interface, which is not
+	// very easy to use. The following code convert it to a CIDR string and then a
+	// net.IP object.
+	var ret IfaceAddrs
+	for _, addr := range addrs {
+		ip, _, err := net.ParseCIDR(addr.String())
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to parse CIDR string %s", addr.String())
+		}
+		if ipv4Addr := ip.To4(); ipv4Addr != nil {
+			ret.IPv4Addr = ipv4Addr
+			continue
+		}
+		if ipv6Addr := ip.To16(); ipv6Addr != nil {
+			if !ipv6Addr.IsLinkLocalUnicast() {
+				ret.IPv6Addrs = append(ret.IPv6Addrs, ipv6Addr)
+			}
+			continue
+		}
+		return nil, errors.Wrapf(err, "%s is neither a v4 addr nor a v6 addr", ip.String())
+	}
+	return &ret, nil
+}
+
+// EnterNetNS makes the following execution of the current OS thread in the
+// netns associated with this Env, and returns the cleanup function which make
+// the thread going back to the root netns. Notes that this function calls
+// runtime.LockOSThread() to bind the calling goroutine to the current thread,
+// since the netns only takes effect on a thread. The cleanup function MUST be
+// called on the same goroutine with this function.
+func (e *Env) EnterNetNS(ctx context.Context) (func() error, error) {
+	// A helper function wraps the code to open the netns file with proper error
+	// and log messages.
+	openNSByPath := func(path string) (*os.File, func(), error) {
+		f, err := os.Open(path)
+		if err != nil {
+			return nil, nil, errors.Wrapf(err, "failed to open ns with path %s", path)
+		}
+		closeAndLogOnFailure := func() {
+			if err := f.Close(); err != nil {
+				testing.ContextLogf(ctx, "Failed to close file for ns %s: %v", path, err)
+			}
+		}
+		return f, closeAndLogOnFailure, err
+	}
+
+	success := false
+
+	runtime.LockOSThread()
+	defer func() {
+		if !success {
+			runtime.UnlockOSThread()
+		}
+	}()
+
+	// Open the current ns which will be used later in the cleanup closure.
+	pid := syscall.Getpid()
+	tid := syscall.Gettid()
+	currentNSFile, currentNSClose, err := openNSByPath(fmt.Sprintf("/proc/%d/task/%d/ns/net", pid, tid))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if !success {
+			currentNSClose()
+		}
+	}()
+
+	// Open and enter the target ns.
+	targetNSFile, targertNSClose, err := openNSByPath("/run/netns/" + e.NetNSName)
+	if err != nil {
+		return nil, err
+	}
+	defer targertNSClose()
+	if err := unix.Setns(int(targetNSFile.Fd()), unix.CLONE_NEWNET); err != nil {
+		return nil, errors.Wrapf(err, "failed to enter netns %s", e.NetNSName)
+	}
+
+	success = true
+	return func() error {
+		// File should always be closed.
+		defer currentNSClose()
+
+		tidNow := syscall.Gettid()
+		if tid != tidNow {
+			return errors.Errorf("cleanup func does not run on the same thread with the one enters the netns %s", e.NetNSName)
+		}
+		// Thread should be unlocked as long as we are on the same thread.
+		defer runtime.UnlockOSThread()
+
+		if err := unix.Setns(int(currentNSFile.Fd()), unix.CLONE_NEWNET); err != nil {
+			return errors.Wrap(err, "failed to go back to the original netns")
+		}
+		return nil
+	}, nil
 }
 
 func (e *Env) createLogFile(ctx context.Context) (*os.File, error) {
