388f461190cc6078c2c6c434f741629fe69decf4
Kshitij Shah <tij@google.com>
2022-06-07 09:57:48 +0000

    tast: firmware_ConsecutiveBoot to tast
    This test shuts down and turns on device over multiple iterations
    and verifies boot is preserved. Merge it with existing tast test
    firmware.ConsecutiveBootPowerButton.
    
    BUG=b:194907897,b:194907748
    TEST=firmware.ConsecutiveBoot.*
    
    Change-Id: Ibf6fa5f92ab2650406f19068c582c5718c39b045

--- src/chromiumos/tast/remote/bundles/cros/firmware/consecutive_boot.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/consecutive_boot.go
@@ -16,60 +16,112 @@ import (
 	"chromiumos/tast/testing/hwdep"
 )
 
+type consecutiveBootMethod int
+
+const (
+	consecutiveBootWithPowerBtn consecutiveBootMethod = iota
+	consecutiveBootWithShutdownCmd
+)
+
 type argsForConsecutiveBoot struct {
-	bootMode string
-	numIters int
+	bootMethod consecutiveBootMethod
+	bootMode   string
+	numIters   int
 }
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         ConsecutiveBootPowerButton,
-		Desc:         "Test DUT shuts down and boots to ChromeOS with the power key over many iterations",
+		Func:         ConsecutiveBoot,
+		Desc:         "Test DUT shuts down and boots to ChromeOS over many iterations",
 		Contacts:     []string{"tij@google.com", "cros-fw-engprod@google.com"},
 		Attr:         []string{"group:firmware", "firmware_unstable"},
 		HardwareDeps: hwdep.D(hwdep.ChromeEC()),
 		Params: []testing.Param{
 			{
-				Name:    "normal_mode_10",
+				Name:    "power_button_normal_mode_10",
 				Fixture: fixture.NormalMode,
 				Timeout: 20 * time.Minute,
 				Val: argsForConsecutiveBoot{
-					bootMode: "normal",
-					numIters: 3,
+					bootMethod: consecutiveBootWithPowerBtn,
+					bootMode:   "normal",
+					numIters:   10,
 				},
 			},
 			{
-				Name:    "dev_mode_10",
+				Name:    "power_button_dev_mode_10",
 				Fixture: fixture.DevModeGBB,
 				Timeout: 20 * time.Minute,
 				Val: argsForConsecutiveBoot{
-					bootMode: "developer",
-					numIters: 10,
+					bootMethod: consecutiveBootWithPowerBtn,
+					bootMode:   "developer",
+					numIters:   10,
 				},
 			},
 			{
-				Name:    "normal_mode_100",
+				Name:    "power_button_normal_mode_100",
 				Fixture: fixture.NormalMode,
-				Timeout: 250 * time.Minute,
+				Timeout: 300 * time.Minute,
 				Val: argsForConsecutiveBoot{
-					bootMode: "normal",
-					numIters: 100,
+					bootMethod: consecutiveBootWithPowerBtn,
+					bootMode:   "normal",
+					numIters:   100,
 				},
 			},
 			{
-				Name:    "dev_mode_100",
+				Name:    "power_button_dev_mode_100",
 				Fixture: fixture.DevModeGBB,
-				Timeout: 250 * time.Minute,
+				Timeout: 300 * time.Minute,
 				Val: argsForConsecutiveBoot{
-					bootMode: "developer",
-					numIters: 100,
+					bootMethod: consecutiveBootWithPowerBtn,
+					bootMode:   "developer",
+					numIters:   100,
+				},
+			},
+			{
+				Name:    "shutdown_cmd_normal_mode_10",
+				Fixture: fixture.NormalMode,
+				Timeout: 20 * time.Minute,
+				Val: argsForConsecutiveBoot{
+					bootMethod: consecutiveBootWithShutdownCmd,
+					bootMode:   "normal",
+					numIters:   10,
+				},
+			},
+			{
+				Name:    "shutdown_cmd_dev_mode_10",
+				Fixture: fixture.DevModeGBB,
+				Timeout: 20 * time.Minute,
+				Val: argsForConsecutiveBoot{
+					bootMethod: consecutiveBootWithShutdownCmd,
+					bootMode:   "developer",
+					numIters:   10,
+				},
+			},
+			{
+				Name:    "shutdown_cmd_normal_mode_100",
+				Fixture: fixture.NormalMode,
+				Timeout: 300 * time.Minute,
+				Val: argsForConsecutiveBoot{
+					bootMethod: consecutiveBootWithShutdownCmd,
+					bootMode:   "normal",
+					numIters:   100,
+				},
+			},
+			{
+				Name:    "shutdown_cmd_dev_mode_100",
+				Fixture: fixture.DevModeGBB,
+				Timeout: 300 * time.Minute,
+				Val: argsForConsecutiveBoot{
+					bootMethod: consecutiveBootWithShutdownCmd,
+					bootMode:   "developer",
+					numIters:   100,
 				},
 			},
 		},
 	})
 }
 
-func ConsecutiveBootPowerButton(ctx context.Context, s *testing.State) {
+func ConsecutiveBoot(ctx context.Context, s *testing.State) {
 	h := s.FixtValue().(*fixture.Value).Helper
 	testArgs := s.Param().(argsForConsecutiveBoot)
 	if err := h.RequireServo(ctx); err != nil {
@@ -88,6 +140,27 @@ func ConsecutiveBootPowerButton(ctx context.Context, s *testing.State) {
 		return nil
 	}
 
+	shutdownWithPowerButton := func() {
+		s.Log("Pressing power key until device shuts down")
+		if err := h.Servo.KeypressWithDuration(ctx, servo.PowerKey, servo.Dur(h.Config.HoldPwrButtonPowerOff)); err != nil {
+			s.Fatal("Failed to press power key: ", err)
+		}
+	}
+
+	shutdownWithShutdownCmd := func() {
+		s.Log("Sending `/sbin/shutdown -P now` to shutdown dut")
+		if err := h.DUT.Conn().CommandContext(ctx, "/sbin/shutdown", "-P", "now").Start(); err != nil {
+			s.Fatal("Failed to run `/sbin/shutdown -P now` cmd: ", err)
+		}
+	}
+
+	var shutdownFunc func()
+	if testArgs.bootMethod == consecutiveBootWithPowerBtn {
+		shutdownFunc = shutdownWithPowerButton
+	} else if testArgs.bootMethod == consecutiveBootWithShutdownCmd {
+		shutdownFunc = shutdownWithShutdownCmd
+	}
+
 	for i := 0; i < testArgs.numIters; i++ {
 		s.Logf("Running iteration %d out of %d ", i+1, testArgs.numIters)
 		s.Log("Verifying boot mode is ", testArgs.bootMode)
@@ -100,10 +173,7 @@ func ConsecutiveBootPowerButton(ctx context.Context, s *testing.State) {
 			s.Fatal("Failed to get boot id: ", err)
 		}
 
-		s.Log("Pressing power key until device shuts down")
-		if err := h.Servo.KeypressWithDuration(ctx, servo.PowerKey, servo.Dur(h.Config.HoldPwrButtonPowerOff)); err != nil {
-			s.Fatal("Failed to press power key: ", err)
-		}
+		shutdownFunc()
 
 		s.Logf("Sleep for %s so shutdown completes", h.Config.Shutdown)
 		if err := testing.Sleep(ctx, h.Config.Shutdown); err != nil {
@@ -125,7 +195,7 @@ func ConsecutiveBootPowerButton(ctx context.Context, s *testing.State) {
 			s.Fatal("Failed to get S0 powerstate: ", err)
 		}
 
-		s.Log("Check for S0 powerstate")
+		s.Log("Wait for DUT to connect")
 		if err := h.WaitConnect(ctx); err != nil {
 			s.Fatal("Failed to wait for device to connect: ", err)
 		}
