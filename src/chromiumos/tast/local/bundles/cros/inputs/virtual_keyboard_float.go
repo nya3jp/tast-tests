40f8c31a1fa9ef7ab7d97efedca07af6f6f91637
Alvin Jia <alvinjia@google.com>
2022-06-07 16:43:59 +1000

    tast-tests: fix VK float and glideTyping
    Reset the VK back to docked mode.
    
    BUG=None
    TEST=tast run <DUT> inputs.VirtualKeyboardGlideTyping.* inputs.VirtualKeyboardFloat.*
    
    Change-Id: Ia4dce0cd3d31339ae5c584b42d902743d82af5d9

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_float.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_float.go
@@ -67,6 +67,15 @@ func VirtualKeyboardFloat(ctx context.Context, s *testing.State) {
 	if err := vkbCtx.SetFloatingMode(uc, true)(ctx); err != nil {
 		s.Fatal("Failed to set VK to floating mode: ", err)
 	}
+	defer func(ctx context.Context) {
+		if err := uiauto.Combine("reset VK to docked mode",
+			vkbCtx.ShowVirtualKeyboard(),
+			vkbCtx.SetFloatingMode(uc, false),
+			vkbCtx.HideVirtualKeyboard(),
+		)(ctx); err != nil {
+			s.Log("Failed to reset VK to docked mode: ", err)
+		}
+	}(cleanupCtx)
 
 	validateDragVKAction := func(ctx context.Context) error {
 		// Get current center point of drag button.
