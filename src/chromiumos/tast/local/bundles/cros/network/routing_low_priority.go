edf42756c9fb589fc751b8760d24bb3b7ed63164
Jie Jiang <jiejiang@chromium.org>
2022-06-07 14:42:38 +0900

    network: Add routing_low_priority test
    This patch adds the first routing test which verifies the routing setup
    in the case that a network with lower priority shows up.
    
    BUG=b:192436642
    TEST=tast run $DUT network.Routing*
    
    Change-Id: I7f1b9910a11f16ec02f1b6ec0ed017d0daae3667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3692563
    Commit-Queue: Jie Jiang <jiejiang@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Taoyu Li <taoyl@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Tested-by: Jie Jiang <jiejiang@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/network/routing_low_priority.go
@@ -0,0 +1,74 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/bundles/cros/network/routing"
+	"chromiumos/tast/local/bundles/cros/network/virtualnet"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RoutingLowPriority,
+		Desc:         "Verify the routing semantics in the case that there is a dual-stack network and then another network with lower priority shows up",
+		Contacts:     []string{"jiejiang@google.com", "cros-networking@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		LacrosStatus: testing.LacrosVariantUnneeded,
+	})
+}
+
+// RoutingLowPriority verifies that, when a new network with lower priority
+// becomes Online, the default network should not change, and thus this new
+// network becomes the secondary network.
+func RoutingLowPriority(ctx context.Context, s *testing.State) {
+	testEnv := routing.NewTestEnv()
+	if err := testEnv.SetUp(ctx); err != nil {
+		s.Fatal("Failed to set up routing test env: ", err)
+	}
+	defer func() {
+		if err := testEnv.TearDown(ctx); err != nil {
+			s.Error("Failed to tear down routing test env: ", err)
+		}
+	}()
+
+	testNetworkOpts := virtualnet.EnvOptions{
+		Priority:   routing.LowPriority,
+		NameSuffix: routing.TestSuffix,
+		EnableDHCP: true,
+		RAServer:   true,
+	}
+	if err := testEnv.CreateNetworkEnvForTest(ctx, testNetworkOpts); err != nil {
+		s.Fatal("Failed to create network for test: ", err)
+	}
+	if err := testEnv.WaitForServiceOnline(ctx, testEnv.TestService); err != nil {
+		s.Fatal("Failed to wait for service in test online: ", err)
+	}
+
+	if errs := testEnv.VerifyTestNetwork(ctx, routing.VerifyOptions{
+		IPv4:      true,
+		IPv6:      true,
+		IsPrimary: false,
+		Timeout:   30 * time.Second,
+	}); len(errs) != 0 {
+		for _, err := range errs {
+			s.Error("Failed to verify test network after creating test network: ", err)
+		}
+	}
+
+	if errs := testEnv.VerifyBaseNetwork(ctx, routing.VerifyOptions{
+		IPv4:      true,
+		IPv6:      true,
+		IsPrimary: true,
+		Timeout:   0,
+	}); len(errs) != 0 {
+		for _, err := range errs {
+			s.Error("Failed to verify base network after creating test network: ", err)
+		}
+	}
+}
