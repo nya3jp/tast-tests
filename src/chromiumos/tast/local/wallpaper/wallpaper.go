fa6831f901fa10175c94d107ce5fe068f9b2eb00
ChromeOS Developer <dmblack@google.com>
2022-06-08 10:15:35 -0700

    Fix wallpaper tests.
    The role for collection grid items recently changed:
    https://chromium-review.googlesource.com/c/chromium/src/+/3689049
    
    BUG=b:230139831
    TEST=tast run $DUT wallpaper.*
    
    Change-Id: I801c62f06dd1dcaae142c16a884df858f497d19d

--- src/chromiumos/tast/local/wallpaper/wallpaper.go
+++ src/chromiumos/tast/local/wallpaper/wallpaper.go
@@ -41,7 +41,7 @@ func OpenWallpaperPicker(ui *uiauto.Context) uiauto.Action {
 // SelectCollection returns an action to select the collection with the given collection name.
 func SelectCollection(ui *uiauto.Context, collection string) uiauto.Action {
 	// Collections that are fully loaded will have a name like "Name 10 Images".
-	loadedCollections := nodewith.Role(role.Button).HasClass("photo-inner-container").NameRegex(regexp.MustCompile(`.*\d+\s[iI]mages`))
+	loadedCollections := nodewith.Role(role.ListBoxOption).HasClass("photo-inner-container").NameRegex(regexp.MustCompile(`.*\d+\s[iI]mages`))
 	desiredCollection := loadedCollections.NameStartingWith(collection)
 	return uiauto.Combine(fmt.Sprintf("select collection %q", collection),
 		// We should at least wait for a few collections to be loaded.
