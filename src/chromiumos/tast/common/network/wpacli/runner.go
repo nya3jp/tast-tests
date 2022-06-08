b16d91681432c05b1876e2769cfbe12c9d788c5c
Arowa Suliman <arowa@google.com>
2022-06-08 15:39:38 -0700

    wpacli_runner: Add scanning and managing func
    Adding functions for scanning, managing and checking the status of
    networks. All these command will be used for testing the WiFi Direct
    functionality.
    
    BUG=b:204796480
    TEST=ran the tests:
    tast run -companiondut=cd1:localhost:2220 -var=router=localhost:2210 -var=pcap=localhost:2212 localhost:2200  wifi.P2PSimpleConnect
    test_that $DUT suite:wifi_matfunc
    
    Change-Id: I18017b6fef4faaca8645e1beff2733fafeba327e

--- src/chromiumos/tast/common/network/wpacli/runner.go
+++ src/chromiumos/tast/common/network/wpacli/runner.go
@@ -7,10 +7,14 @@ package wpacli
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"chromiumos/tast/common/network/cmd"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/ssh"
 )
 
 // Runner contains methods involving wpa_cli command.
@@ -51,7 +55,7 @@ func (r *Runner) ClearBSSIDIgnore(ctx context.Context) error {
 		return errors.Wrap(err, "failed running wpa_cli bssid_ignore clear")
 	}
 	if !strings.Contains(string(cmdOut), "OK") {
-		return errors.Errorf("failed to expect 'OK' in wpa_cli bssid_ignore clear output: %s", string(cmdOut))
+		return errors.Errorf("failed to detect 'OK' in the output of 'wpa_cli bssid_ignore clear', output: %s", string(cmdOut))
 	}
 	return nil
 }
@@ -88,7 +92,7 @@ func (r *Runner) Set(ctx context.Context, prop Property, val string) error {
 		return errors.Wrapf(err, "failed running wpa_cli set %s %s", string(prop), val)
 	}
 	if !strings.Contains(string(cmdOut), "OK") {
-		return errors.Errorf("failed to get 'OK' in wpa_cli set output: %s", string(cmdOut))
+		return errors.Errorf("failed to detect 'OK' in the output of 'wpa_cli set %s %s', output: %s", string(prop), val, string(cmdOut))
 	}
 	return nil
 }
@@ -101,6 +105,18 @@ func (r *Runner) run(ctx context.Context, expected string, opts ...string) error
 	}
 	if !strings.Contains(string(cmdOut), expected) {
 		return errors.Errorf("failed to get %q in wpa_cli %s output: %s", expected, strings.Join(opts, " "), string(cmdOut))
+	}
+	return nil
+}
+
+// RemoveAllNetworks removes all saved network profiles.
+func (r *Runner) RemoveAllNetworks(ctx context.Context) error {
+	cmdOut, err := r.cmd.Output(ctx, "sudo", sudoWPACLI("remove_network", "all")...)
+	if err != nil {
+		return errors.Wrap(err, "failed running wpa_cli remove_network all")
+	}
+	if !strings.Contains(string(cmdOut), "OK") {
+		return errors.Errorf("failed to detect 'OK' in the output of 'wpa_cli remove_network all', output: %s", string(cmdOut))
 	}
 	return nil
 }
@@ -123,4 +139,210 @@ func (r *Runner) TDLSTeardown(ctx context.Context, mac string) error {
 // TDLSLinkStatus runs tdls_link_status command.
 func (r *Runner) TDLSLinkStatus(ctx context.Context, mac string) error {
 	return r.run(ctx, "TDLS link status: connected", "tdls_link_status", mac)
+}
+
+// addNetwork adds a wpa_supplicant network and returns the network ID.
+func (r *Runner) addNetwork(ctx context.Context) (int, error) {
+	cmdOut, err := r.cmd.Output(ctx, "sudo", sudoWPACLI("add_network")...)
+	if err != nil {
+		return -1, errors.Wrap(err, "failed running wpa_cli add_network")
+	}
+	lines := strings.Split(string(cmdOut), "\n")
+	if len(lines) < 2 {
+		return -1, errors.Wrap(err, "invalid output of 'wpa_cli add_network' commmand")
+	}
+	netID, err := strconv.Atoi(lines[1])
+	if err != nil {
+		return -1, err
+	}
+	return netID, nil
+}
+
+// setNetwork sets a wpa_supplicant network variable.
+func (r *Runner) setNetwork(ctx context.Context, networkID int, variable, value string) error {
+	cmdOut, err := r.cmd.Output(ctx, "sudo", sudoWPACLI("set_network", strconv.Itoa(networkID), variable, value)...)
+	if err != nil {
+		return errors.Wrapf(err, "failed running wpa_cli set_network %d %s %s", networkID, variable, value)
+	}
+	if !strings.Contains(string(cmdOut), "OK") {
+		return errors.Errorf("failed to detect 'OK' in the output of 'wpa_cli set_network %d %s %s', output: %s", networkID, variable, value, string(cmdOut))
+	}
+	return nil
+}
+
+// NetworkSSID returns the ssid of the network with ID network_id.
+func (r *Runner) NetworkSSID(ctx context.Context, networkID int, iface string) (string, error) {
+	cmdOut, err := r.cmd.Output(ctx, "sudo", sudoWPACLI("-i", iface, "get_network", strconv.Itoa(networkID), "ssid")...)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed running wpa_cli get_network %d ssid", networkID)
+	}
+
+	return strings.Trim(string(cmdOut), "\""), nil
+}
+
+// statusMap returns a generated status key/value map from the output of the WiFi interface.
+func (r *Runner) statusMap(ctx context.Context, iface string) (map[string]string, error) {
+	cmdOut, err := r.cmd.Output(ctx, "sudo", sudoWPACLI("-i", iface, "status")...)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed running wpa_cli -i %s status", iface)
+	}
+	statusMap := make(map[string]string)
+	for _, line := range strings.Split(string(cmdOut), "\n") {
+		if strings.Contains(line, "=") {
+			statusMap[strings.Split(line, "=")[0]] = strings.Split(line, "=")[1]
+		}
+	}
+
+	return statusMap, nil
+}
+
+// scan requests new BSS scan.
+func (r *Runner) scan(ctx context.Context) error {
+	cmdOut, err := r.cmd.Output(ctx, "sudo", sudoWPACLI("scan")...)
+	if err != nil {
+		return errors.Wrap(err, "failed running wpa_cli scan")
+	}
+	if !strings.Contains(string(cmdOut), "OK") {
+		return errors.Errorf("failed to detect 'OK' in the output of 'wpa_cli scan', output: %s", string(cmdOut))
+	}
+	return nil
+}
+
+// scanResults returns latest scan results.
+func (r *Runner) scanResults(ctx context.Context) ([]map[string]string, error) {
+	var networks []map[string]string
+	cmdOut, err := r.cmd.Output(ctx, "sudo", sudoWPACLI("scan_results")...)
+	if err != nil {
+		return networks, errors.Wrap(err, "failed running wpa_cli scan_results")
+	}
+	scanResultsPattern := strings.Join([]string{
+		"(?P<BSSID>[0-9a-f:]{17})", // BSSID
+		"(?P<Frequency>[0-9]+)",    // Frequency
+		"(?P<SignalLevel>-[0-9]+)", // Signal level
+		"(?P<EncryptionTypes>.*)",  // Encryption types
+		"(?P<SSID>.*)"},            // SSID
+		"\t")
+	compRegEx := regexp.MustCompile(scanResultsPattern)
+	for _, line := range strings.Split(string(cmdOut), "\n") {
+		if match := compRegEx.FindStringSubmatch(line); match != nil {
+			paramsMap := make(map[string]string)
+			for i, name := range compRegEx.SubexpNames() {
+				paramsMap[name] = match[i]
+			}
+			networks = append(networks, paramsMap)
+		}
+	}
+	return networks, nil
+}
+
+// Interfaces returns a list of the available interfaces:.
+func (r *Runner) Interfaces(ctx context.Context) ([]string, error) {
+	cmdOut, err := r.cmd.Output(ctx, "sudo", sudoWPACLI("interface")...)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed running wpa_cli interface")
+	}
+	var interfaces []string
+	startParsing := false
+	for _, line := range strings.Split(string(cmdOut), "\n") {
+		// Start parsing after the line "Available interfaces:".
+		if strings.Contains(line, "Available interfaces:") {
+			startParsing = true
+			continue
+		}
+		if startParsing {
+			interfaces = append(interfaces, line)
+		}
+	}
+
+	return interfaces, nil
+}
+
+// IsConnected checks that the network is connected.
+func (r *Runner) IsConnected(ctx context.Context, iface string) (bool, error) {
+	statusMap, err := r.statusMap(ctx, iface)
+	if err != nil {
+		return false, err
+	}
+	statusWPAState, ok := statusMap["wpa_state"]
+	if !ok {
+		return false, errors.Errorf("the wpa_state key was not found in the status map %v", statusMap)
+	}
+	if statusWPAState == "CONNECTED" {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// checkScanResults checks if the latest scan results has a network with ssid that match the passed ssid.
+func (r *Runner) checkScanResults(ctx context.Context, ssid string) error {
+	results, err := r.scanResults(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get the scan results")
+	}
+	for _, network := range results {
+		if network["SSID"] == ssid {
+			return nil
+		}
+	}
+
+	return errors.Errorf("failed to find SSID=%s in the scan results", ssid)
+}
+
+// ScanNetwork scans for specific network with SSID and returns nil if the network id found
+// in the scan results.
+func (r *Runner) ScanNetwork(ctx context.Context, dutConn *ssh.Conn, ssid string) error {
+	timeoutCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	const wpaMonitorStopTimeout = 2 * time.Second
+	wpaMonitor := new(WPAMonitor)
+	stop, ctx, err := wpaMonitor.StartWPAMonitor(timeoutCtx, dutConn, wpaMonitorStopTimeout)
+	if err != nil {
+		return errors.Wrap(err, "failed to start wpa monitor")
+	}
+	defer stop()
+
+	if err := r.checkScanResults(ctx, ssid); err == nil {
+		return nil
+	}
+
+	scanOnce := func(ctx context.Context, ssid string) error {
+		if err := r.scan(ctx); err != nil {
+			// Don't fail if the scan command failed due to the radio being busy.
+			if !strings.Contains(fmt.Sprint(err), "FAIL-BUSY") {
+				return errors.Wrap(err, "failed to trigger scan")
+			}
+		}
+
+		for {
+			event, err := wpaMonitor.WaitForEvent(ctx)
+			if err != nil {
+				return errors.Wrap(err, "failed to wait for ScanResultsEvent")
+			}
+			if event == nil { // timeout
+				return errors.Wrap(err, "timeout waiting for the ScanResultsEvent")
+
+			}
+			if _, scanSuccess := event.(*ScanResultsEvent); scanSuccess {
+				break
+			}
+		}
+
+		if err := r.checkScanResults(ctx, ssid); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	var scanErr error
+	for scan := 0; scan < 2; scan++ {
+		err := scanOnce(ctx, ssid)
+		if err == nil {
+			return nil
+		}
+		scanErr = err
+	}
+
+	return scanErr
 }
