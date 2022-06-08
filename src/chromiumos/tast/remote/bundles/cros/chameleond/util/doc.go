c1ef4b4573783a8360abf222f0f2ada3853673e2
Jared Bennett <jaredbennett@google.com>
2022-06-07 17:08:20 -0700

    cft-chameleon: Add Chameleond Manager Service fixture and initial test suite
    BUG=b:233808909
    TEST=All chameleond.ChameleondService tests passing except get_connector_type, but that appears to be a Chameleond bug as the service preforms as expected -- the results are just unexpected.
    
    Change-Id: I9ed9429effc7627d9d8ef7e4471120fc9acfd724

---
+++ src/chromiumos/tast/remote/bundles/cros/chameleond/util/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package util contains utilities for developing remote tests for testing the
+// Chameleond Manager Service.
+package util
