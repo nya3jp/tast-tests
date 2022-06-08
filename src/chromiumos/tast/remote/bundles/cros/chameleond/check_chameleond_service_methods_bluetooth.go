b0ba876d60b2aac4a0b129dcdc3ceb2370834e3c
Jared Bennett <jaredbennett@google.com>
2022-06-08 16:57:46 -0700

    cft-chameleon: Add ChameleondService bluetooth tests
    BUG=b:233808909
    TEST=The new tests do not all pass with my test chameleond device,
    but do correctly call the service.
    
    Change-Id: I45c5b497ffd290b7b788442208230ea3453f6899

--- src/chromiumos/tast/remote/bundles/cros/chameleond/check_chameleond_service_methods_bluetooth.go
+++ src/chromiumos/tast/remote/bundles/cros/chameleond/check_chameleond_service_methods_bluetooth.go
@@ -8,6 +8,8 @@ import (
 	"context"
 	"time"
 
+	pbchameleond "go.chromium.org/chromiumos/config/go/platform/chameleon/chameleond/rpc"
+
 	"chromiumos/tast/remote/bundles/cros/chameleond/util"
 	"chromiumos/tast/testing"
 )
@@ -24,13 +26,61 @@ func init() {
 		SoftwareDeps: []string{},
 		Fixture:      "simpleChameleond",
 		Timeout:      1 * time.Minute,
-		Params:       []testing.Param{
-			// TODO rpc AudioBoardResetBluetooth(AudioBoardResetBluetoothRequest) returns (AudioBoardResetBluetoothResponse);
-			// TODO rpc AudioBoardDisableBluetooth(AudioBoardDisableBluetoothRequest) returns (AudioBoardDisableBluetoothResponse);
-			// TODO rpc AudioBoardIsBluetoothEnabled(AudioBoardIsBluetoothEnabledRequest) returns (AudioBoardIsBluetoothEnabledResponse);
-			// TODO rpc ResetBluetoothRef(ResetBluetoothRefRequest) returns (ResetBluetoothRefResponse);
-			// TODO rpc DisableBluetoothRef(DisableBluetoothRefRequest) returns (DisableBluetoothRefResponse);
-			// TODO rpc IsBluetoothRefDisabled(IsBluetoothRefDisabledRequest) returns (IsBluetoothRefDisabledResponse);
+		Params: []testing.Param{
+			{
+				Name: "audio_board_reset_bluetooth",
+				Val: &util.CheckChameleondServiceMethodsTestCase{
+					CallMethod: func(ctx context.Context, client pbchameleond.ChameleondServiceClient) (interface{}, error) {
+						return client.AudioBoardResetBluetooth(ctx, &pbchameleond.AudioBoardResetBluetoothRequest{})
+					},
+					ExpectedResponse: &pbchameleond.AudioBoardResetBluetoothResponse{},
+				},
+			},
+			{
+				Name: "audio_board_disable_bluetooth",
+				Val: &util.CheckChameleondServiceMethodsTestCase{
+					CallMethod: func(ctx context.Context, client pbchameleond.ChameleondServiceClient) (interface{}, error) {
+						return client.AudioBoardDisableBluetooth(ctx, &pbchameleond.AudioBoardDisableBluetoothRequest{})
+					},
+					ExpectedResponse: &pbchameleond.AudioBoardDisableBluetoothResponse{},
+				},
+			},
+			{
+				Name: "audio_board_is_bluetooth_enabled",
+				Val: &util.CheckChameleondServiceMethodsTestCase{
+					CallMethod: func(ctx context.Context, client pbchameleond.ChameleondServiceClient) (interface{}, error) {
+						return client.AudioBoardIsBluetoothEnabled(ctx, &pbchameleond.AudioBoardIsBluetoothEnabledRequest{})
+					},
+					ExpectedResponse: &pbchameleond.AudioBoardIsBluetoothEnabledResponse{},
+				},
+			},
+			{
+				Name: "reset_bluetooth_ref",
+				Val: &util.CheckChameleondServiceMethodsTestCase{
+					CallMethod: func(ctx context.Context, client pbchameleond.ChameleondServiceClient) (interface{}, error) {
+						return client.ResetBluetoothRef(ctx, &pbchameleond.ResetBluetoothRefRequest{})
+					},
+					ExpectedResponse: &pbchameleond.ResetBluetoothRefResponse{},
+				},
+			},
+			{
+				Name: "disable_bluetooth_ref",
+				Val: &util.CheckChameleondServiceMethodsTestCase{
+					CallMethod: func(ctx context.Context, client pbchameleond.ChameleondServiceClient) (interface{}, error) {
+						return client.DisableBluetoothRef(ctx, &pbchameleond.DisableBluetoothRefRequest{})
+					},
+					ExpectedResponse: &pbchameleond.DisableBluetoothRefResponse{},
+				},
+			},
+			{
+				Name: "is_bluetooth_ref_disabled",
+				Val: &util.CheckChameleondServiceMethodsTestCase{
+					CallMethod: func(ctx context.Context, client pbchameleond.ChameleondServiceClient) (interface{}, error) {
+						return client.IsBluetoothRefDisabled(ctx, &pbchameleond.IsBluetoothRefDisabledRequest{})
+					},
+					ExpectedResponse: &pbchameleond.IsBluetoothRefDisabledResponse{},
+				},
+			},
 		},
 	})
 }
