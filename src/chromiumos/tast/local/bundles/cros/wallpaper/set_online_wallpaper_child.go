f6b2447af339c357a17ced5a5fdca60db3b7b0d1
Thuong Phan <thuongphan@google.com>
2022-06-08 05:33:10 +0000

    tast-tests: Clean up personalization and wallpaper tast tests
    - Use fixtures for all tests
    - Consolidate string constants and node finders into constants.go.
    - Fix broken tests due to UI changes and fixture changes.
    
    BUG=b:233808954
    TEST=tast run <DUT> personalization.*
         tast run <DUT> wallpaper.*
    
    Change-Id: I7cd6ad145d97fbfe3ff093389da55314248f1ef5

--- src/chromiumos/tast/local/bundles/cros/wallpaper/set_online_wallpaper_child.go
+++ src/chromiumos/tast/local/bundles/cros/wallpaper/set_online_wallpaper_child.go
@@ -13,11 +13,11 @@ import (
 	"chromiumos/tast/local/chrome/familylink"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
-	"chromiumos/tast/local/chrome/uiauto/nodewith"
-	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/wallpaper"
 	"chromiumos/tast/testing"
 )
+
+const notificationID = "time-limit-screen-time"
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -25,12 +25,16 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Test setting online wallpapers in the new wallpaper app for a child user",
 		Contacts: []string{
-			"tobyhuang@chromium.org", "cros-families-eng+test@google.com", "chromeos-sw-engprod@google.com",
+			"tobyhuang@chromium.org",
+			"thuongphan@google.com",
+			"cros-families-eng+test@google.com",
+			"chromeos-sw-engprod@google.com",
+			"assistive-eng@google.com",
 		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		VarDeps:      []string{"unicorn.wallpaperCategory", "unicorn.wallpaperName"},
-		Fixture:      "familyLinkUnicornLoginWithPersonalizationHub",
+		Fixture:      "familyLinkUnicornLogin",
 		Timeout:      5 * time.Minute,
 	})
 }
@@ -41,8 +45,6 @@ func SetOnlineWallpaperChild(ctx context.Context, s *testing.State) {
 
 	tconn := s.FixtValue().(*familylink.FixtData).TestConn
 
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
 	// Force Chrome to be in clamshell mode to make sure wallpaper preview is not
 	// enabled.
 	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
@@ -51,15 +53,32 @@ func SetOnlineWallpaperChild(ctx context.Context, s *testing.State) {
 	}
 	defer cleanup(ctx)
 
+	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
+
 	// The test has a dependency of network speed, so we give uiauto.Context ample
 	// time to wait for nodes to load.
 	ui := uiauto.New(tconn).WithTimeout(30 * time.Second)
+
+	// Family link notification will appear after logging in, it will interfere with the test if we start at the same time.
+	// Close the notification after it appears, then check if the notification is already gone before starting the test.
+	if _, err := ash.WaitForNotification(ctx, tconn, 20*time.Second, ash.WaitIDContains(notificationID)); err != nil {
+		s.Fatal("Failed to wait for family link notification: ", err)
+	}
+
+	if err := ash.CloseNotifications(ctx, tconn); err != nil {
+		s.Fatal("Failed to close all notifications: ", err)
+	}
+
+	if err := ash.WaitUntilNotificationGone(ctx, tconn, 10*time.Second, ash.WaitIDContains(notificationID)); err != nil {
+		s.Fatal("Failed to wait for family link notification to disappear: ", err)
+	}
 
 	if err := uiauto.Combine(fmt.Sprintf("Change the wallpaper to %s %s", collection, image),
 		wallpaper.OpenWallpaperPicker(ui),
 		wallpaper.SelectCollection(ui, collection),
 		wallpaper.SelectImage(ui, image),
-		ui.WaitUntilExists(nodewith.Name(fmt.Sprintf("Currently set %v", image)).Role(role.Heading)))(ctx); err != nil {
+		ui.WaitUntilExists(wallpaper.CurrentWallpaperWithSpecificNameFinder(image)),
+	)(ctx); err != nil {
 		s.Fatalf("Failed to validate selected wallpaper %s %s: %v", collection, image, err)
 	}
 }
