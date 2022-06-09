1e5a9d54a1cb8dde6440b14718215094f9ccff04
Yongshun Liu <yongshun@chromium.org>
2022-06-09 12:10:36 -0700

    tast-tests: Add SaveAndRecall tests support
    This adds support to test SaveAndRecall, and also as part of
    refactoring, adds test case to cover DeskTemplates delete function.
    wmp.DeskTemplatesDelete is written also to clean up synced desks for
    test accounts.
    
    BUG=chromium:1334030
    TEST=tast -verbose run <IP> wmp.DeskTemplates*, ui.DeskTemplatesCUJ,
    
    Change-Id: I0d7481ba1cf3bc3e8b1dc9721d8d4f7c8c723f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3723225
    Tested-by: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Reviewed-by: Matthew Zhu <zhumatthew@google.com>

--- src/chromiumos/tast/local/bundles/cros/wmp/wmputils/wm.go
+++ src/chromiumos/tast/local/bundles/cros/wmp/wmputils/wm.go
@@ -7,12 +7,17 @@ package wmputils
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/event"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/testing"
 )
 
@@ -51,4 +56,66 @@ func EnsureOnlyBrowserWindowOpen(ctx context.Context, tconn *chrome.TestConn, bt
 func IsBrowserWindow(w *ash.Window, bt browser.Type) bool {
 	return (bt == browser.TypeAsh && w.WindowType == ash.WindowTypeBrowser) ||
 		(bt == browser.TypeLacros && w.WindowType == ash.WindowTypeLacros)
+}
+
+// VerifyWindowCount verifies that there are `windowCount` app windows.
+func VerifyWindowCount(ctx context.Context, tconn *chrome.TestConn, windowCount int) error {
+	ws, err := ash.GetAllWindows(ctx, tconn)
+	if err != nil {
+		return errors.Wrap(err, "failed to get all open windows")
+	}
+	if len(ws) != windowCount {
+		return errors.Wrapf(err, "found inconsistent number of window(s): got %v, want %v", len(ws), windowCount)
+	}
+
+	return nil
+}
+
+// WaitforAppsToLaunch waits for the given apps to launch.
+func WaitforAppsToLaunch(ctx context.Context, tconn *chrome.TestConn, ac *uiauto.Context, appsList []apps.App) error {
+	for _, app := range appsList {
+		if err := ash.WaitForApp(ctx, tconn, app.ID, time.Minute); err != nil {
+			return errors.Wrapf(err, "%s did not appear in shelf after launch", app.Name)
+		}
+
+		// Some apps may take a long time to load such as Play Store. Wait for launch event to be completed.
+		if err := ac.WithInterval(2*time.Second).WaitUntilNoEvent(nodewith.Root(), event.LocationChanged)(ctx); err != nil {
+			return errors.Wrap(err, "failed to wait for the app launch event to be completed")
+		}
+	}
+
+	if err := VerifyWindowCount(ctx, tconn, len(appsList)); err != nil {
+		return errors.Wrap(err, "failed to verify window count")
+	}
+
+	return nil
+}
+
+// WaitforAppsToBeVisible waits for the windows of the given apps to be visible.
+func WaitforAppsToBeVisible(ctx context.Context, tconn *chrome.TestConn, ac *uiauto.Context, appsList []apps.App) error {
+	for _, app := range appsList {
+		// Wait for the launched app window to become visible.
+		if err := ash.WaitForCondition(ctx, tconn, func(w *ash.Window) bool {
+			return w.IsVisible && strings.Contains(w.Title, app.Name)
+		}, &testing.PollOptions{Timeout: 30 * time.Second}); err != nil {
+			return errors.Wrapf(err, "%s app window not visible after launching", app.Name)
+		}
+	}
+
+	return nil
+}
+
+// OpenApps opens the given apps, waits for them to launch and their windows to appear..
+func OpenApps(ctx context.Context, tconn *chrome.TestConn, ac *uiauto.Context, appsList []apps.App) error {
+	for _, app := range appsList {
+		if err := apps.Launch(ctx, tconn, app.ID); err != nil {
+			return errors.Wrapf(err, "failed to open %s", app.Name)
+		}
+	}
+
+	if err := WaitforAppsToLaunch(ctx, tconn, ac, appsList); err != nil {
+		return errors.Wrap(err, "failed to wait for app launch")
+	}
+
+	return nil
 }
