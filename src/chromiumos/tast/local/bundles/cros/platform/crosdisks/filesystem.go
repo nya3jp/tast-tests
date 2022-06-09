25d11592ae200692d0d51b9576c6feb7c8bde624
François Degros <fdegros@chromium.org>
2022-06-09 15:27:17 +1000

    [CrosDisksArchiveBig] Add test for big files
    Add tests platform.CrosDisksArchiveBig dealing with "big" archives.
    These tests are informational only for the time being.
    
    Add tests mounting and decompressing a big file of 6GB. This big file
    has a very repetitive contents that can be compressed very well, which
    means that the archive file itself is much smaller.
    
    Add tests for archives containing many small files
    
    BUG=chromium:1326797, chromium:1335301
    TEST=tast run dut platform.CrosDisksArchive*
    
    Change-Id: I252229fb3d6d892bcec3013d1acf6611381711ab

--- src/chromiumos/tast/local/bundles/cros/platform/crosdisks/filesystem.go
+++ src/chromiumos/tast/local/bundles/cros/platform/crosdisks/filesystem.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package crosdisks provides a series of tests to verify CrosDisks'
-// D-Bus API behavior.
+// Package crosdisks provides a series of tests to verify CrosDisks' D-Bus API
+// behavior.
 package crosdisks
 
 import (
@@ -34,9 +34,10 @@ func formatDevice(ctx context.Context, formatCmd, device string) error {
 	return nil
 }
 
-// withLoopbackDeviceDo initializes a loopback device (optionally formatting it) and calls
-// the provided function enclosed within the scope of validity of the loopback device.
-func withLoopbackDeviceDo(ctx context.Context, cd *crosdisks.CrosDisks, sizeBytes int64, formatCmd string, f func(ctx context.Context, ld *crosdisks.LoopbackDevice) error) (err error) {
+// WithLoopbackDeviceDo initializes a loopback device (optionally formatting it)
+// and calls the provided function enclosed within the scope of validity of the
+// loopback device.
+func WithLoopbackDeviceDo(ctx context.Context, cd *crosdisks.CrosDisks, sizeBytes int64, formatCmd string, f func(ctx context.Context, ld *crosdisks.LoopbackDevice) error) (err error) {
 	ctxForCleanUp := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, time.Second*10)
 	defer cancel()
@@ -62,17 +63,18 @@ func withLoopbackDeviceDo(ctx context.Context, cd *crosdisks.CrosDisks, sizeByte
 	if formatCmd != "" {
 		testing.ContextLogf(ctx, "Formatting %q with %q", ld.DevicePath(), formatCmd)
 		if err := formatDevice(ctx, formatCmd, ld.DevicePath()); err != nil {
-			return errors.Wrapf(err, "failed to format the loopback device %q with %q: %v", ld.DevicePath(), formatCmd, err)
+			return errors.Wrapf(err, "failed to format the loopback device %q with %q", ld.DevicePath(), formatCmd)
 		}
 	}
 
 	return f(ctx, ld)
 }
 
-// testMountFilesystem mounts the loopback device, attempts a write and returns an error if unsuccessful.
+// testMountFilesystem mounts the loopback device, attempts a write and returns
+// an error if unsuccessful.
 func testMountFilesystem(ctx context.Context, cd *crosdisks.CrosDisks, ld *crosdisks.LoopbackDevice, label string) error {
 	expectedMountPath := filepath.Join("/media/removable", label)
-	return withMountDo(ctx, cd, ld.DevicePath(), "", []string{"rw"}, func(ctx context.Context, mountPath string) error {
+	return WithMountDo(ctx, cd, ld.DevicePath(), "", []string{"rw"}, func(ctx context.Context, mountPath string) error {
 		if expectedMountPath != mountPath {
 			return errors.Errorf("unexpected mount_path: got %q; want %q", mountPath, expectedMountPath)
 		}
@@ -100,7 +102,8 @@ func testMountFilesystem(ctx context.Context, cd *crosdisks.CrosDisks, ld *crosd
 
 const loopbackSizeBytes = 16 * 1024 * 1024
 
-// RunFilesystemTests executes a set of tests which mount different filesystems using CrosDisks.
+// RunFilesystemTests executes a set of tests which mount different filesystems
+// using CrosDisks.
 func RunFilesystemTests(ctx context.Context, s *testing.State) {
 	cd, err := crosdisks.New(ctx)
 	if err != nil {
@@ -108,9 +111,10 @@ func RunFilesystemTests(ctx context.Context, s *testing.State) {
 	}
 	defer cd.Close()
 
-	err = withLoopbackDeviceDo(ctx, cd, loopbackSizeBytes, "", func(ctx context.Context, ld *crosdisks.LoopbackDevice) error {
-		// Ideally we should run also some failure tests, e.g. unknown/no filesystem, etc, but cros-disks
-		// is too fragile and remains in a half-broken state after that, so we only check known good scenarios.
+	err = WithLoopbackDeviceDo(ctx, cd, loopbackSizeBytes, "", func(ctx context.Context, ld *crosdisks.LoopbackDevice) error {
+		// Ideally we should run also some failure tests, e.g. unknown/no
+		// filesystem, etc, but cros-disks is too fragile and remains in a
+		// half-broken state after that, so we only check known good scenarios.
 		s.Run(ctx, "vfat", func(ctx context.Context, state *testing.State) {
 			if err := formatDevice(ctx, "mkfs.vfat -n EMPTY1", ld.DevicePath()); err != nil {
 				state.Fatal("Could not format device: ", err)
