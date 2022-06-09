0028b7e6c93aaf99b89070bad037c10c36af7766
Mike Taylor <miketaylr@chromium.org>
2022-06-09 22:01:14 +0000

    Mark ScreenBrightnessPercent as informational due to being flaky
    This started failing on 06/09.
    
    See https://ci.chromium.org/p/chromium/builders/try/chromeos-kevin-rel
    
    Change-Id: Iece810aac9daca5f038c6403e24ee6b371bdc1f2

--- src/chromiumos/tast/local/bundles/cros/policy/screen_brightness_percent.go
+++ src/chromiumos/tast/local/bundles/cros/policy/screen_brightness_percent.go
@@ -31,7 +31,7 @@ func init() {
 		},
 		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
 		SoftwareDeps: []string{"chrome"},
-		Attr:         []string{"group:mainline"},
+		Attr:         []string{"group:mainline", "informational"},
 		Fixture:      fixture.ChromePolicyLoggedIn,
 	})
 }
