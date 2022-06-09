b9979bbf4f20257a373de200c29318fdf2f649c9
Tim Van Patten <timvp@google.com>
2022-06-09 16:27:13 -0600

    power: Add test ShutdownWithCommandBatteryCutoff
    Add the test ShutdownWithCommandBatteryCutoff:
    1. Issue `ectool batterycutoff`.
    2. Issue `shutdown -h now`.
    3. Power on DUT with power button keypress.
    4. Verify DUT boots successful and can log into ChromeOS.
    
    BUG=b:217911928
    TEST=tast run <IP> power.ShutdownWithCommandBatteryCutoff
    
    Change-Id: Iad429c7c194a70414ed2895e4c0c1134b1ff8069

---
+++ src/chromiumos/tast/remote/bundles/cros/power/shutdown_with_command_battery_cutoff.go
@@ -0,0 +1,109 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package power
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/common/servo"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/powercontrol"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/security"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: ShutdownWithCommandBatteryCutoff, LacrosStatus: testing.LacrosVariantUnneeded, Desc: "Verifies that system comes back after executing shutdown command with battery cutoff",
+		Contacts:     []string{"timvp@google.com", "cros-fw-engprod@google.com"},
+		ServiceDeps:  []string{"tast.cros.security.BootLockboxService"},
+		SoftwareDeps: []string{"chrome", "reboot"},
+		Attr:         []string{"group:mainline", "informational"},
+		Vars:         []string{"servo"},
+		HardwareDeps: hwdep.D(hwdep.ChromeEC()),
+		Timeout:      5 * time.Minute,
+	})
+}
+
+func ShutdownWithCommandBatteryCutoff(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 2*time.Minute)
+	defer cancel()
+
+	dut := s.DUT()
+	servoSpec, _ := s.Var("servo")
+	pxy, err := servo.NewProxy(ctx, servoSpec, dut.KeyFile(), dut.KeyDir())
+	if err != nil {
+		s.Fatal("Failed to connect to servo: ", err)
+	}
+	defer pxy.Close(cleanupCtx)
+
+	defer func(ctx context.Context) {
+		testing.ContextLog(ctx, "Performing cleanup")
+		testing.ContextLog(ctx, "Rebooting EC to restore battery connection")
+		if err := pxy.Servo().RunECCommand(ctx, "reboot"); err != nil {
+			s.Fatal("Failed to reboot EC: ", err)
+		}
+		// Wait a little at the end of the test to make sure the EC finishes booting before the next test runs.
+		defer func() {
+			s.Log("Waiting for boot to finish")
+			if err := testing.Sleep(ctx, 20*time.Second); err != nil {
+				s.Fatal("Failed to sleep: ", err)
+			}
+		}()
+		if !dut.Connected(ctx) {
+			if err := powercontrol.PowerOntoDUT(ctx, pxy, dut); err != nil {
+				s.Fatal("Failed to wake up DUT at cleanup: ", err)
+			}
+		}
+	}(cleanupCtx)
+
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(cleanupCtx)
+
+	client := security.NewBootLockboxServiceClient(cl.Conn)
+	if _, err := client.NewChromeLogin(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+
+	// Cut off the battery to simulate booting without a battery.
+	testing.ContextLog(ctx, "Cut off the battery")
+	if err := dut.Conn().CommandContext(ctx, "ectool", "batterycutoff").Run(); err != nil {
+		s.Fatal("Failed to issue `ectool batterycutoff`: ", err)
+	}
+
+	powerOffCtx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	if err := dut.Conn().CommandContext(powerOffCtx, "shutdown", "-h", "now").Run(); err != nil && !errors.Is(err, context.DeadlineExceeded) {
+		s.Fatal("Failed to execute shutdown command: ", err)
+	}
+
+	sdCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	if err := dut.WaitUnreachable(sdCtx); err != nil {
+		s.Fatal("Failed to shutdown DUT: ", err)
+	}
+
+	if err := powercontrol.ValidateG3PowerState(ctx, pxy); err != nil {
+		s.Fatal("Failed to enter G3 after shutdown: ", err)
+	}
+
+	if err := powercontrol.PowerOntoDUT(ctx, pxy, dut); err != nil {
+		s.Fatal("Failed to wake up DUT: ", err)
+	}
+
+	if err := powercontrol.ChromeOSLogin(ctx, dut, s.RPCHint()); err != nil {
+		s.Fatal("Failed to login to chrome: ", err)
+	}
+}
