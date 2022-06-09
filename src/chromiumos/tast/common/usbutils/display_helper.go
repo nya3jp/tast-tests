9800dd53eb1d716224ec53cf55d317f57e00b818
Ambalavanan MM <ambalavanan.m.m@intel.com>
2022-06-09 18:08:38 +0530

    power: Add SystemPeripheralsFunctionalityCheck test
    BUG=b:225081951
    TEST=tast run -var=servo=localhost:9999 <DUT_IP> power.SystemPeripheralsFunctionalityCheck.suspend_gold
    
    Change-Id: I5dd8a90be063c65397f6fbc479650d3514fbcf4b

---
+++ src/chromiumos/tast/common/usbutils/display_helper.go
@@ -0,0 +1,61 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package usbutils
+
+import (
+	"context"
+	"io/ioutil"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/dut"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/ssh/linuxssh"
+	"chromiumos/tast/testing"
+)
+
+// externalDisplayDetection verifies connected extended display is detected or not.
+func externalDisplayDetection(ctx context.Context, dut *dut.DUT, numberOfDisplays int, regexpPatterns []*regexp.Regexp, remoteTest bool) error {
+	displayInfoFile := "/sys/kernel/debug/dri/0/i915_display_info"
+	displayInfo := regexp.MustCompile(`.*pipe\s+[BCD]\]:\n.*active=yes, mode=.[0-9]+x[0-9]+.: [0-9]+.*\s+[hw: active=yes]+`)
+	var out []byte
+	var err error
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if remoteTest {
+			out, err = linuxssh.ReadFile(ctx, dut.Conn(), displayInfoFile)
+		} else {
+			out, err = ioutil.ReadFile(displayInfoFile)
+		}
+		if err != nil {
+			return errors.Wrap(err, "failed to run display info command")
+		}
+
+		matchedString := displayInfo.FindAllString(string(out), -1)
+		if len(matchedString) != numberOfDisplays {
+			return errors.New("connected external display info not found")
+		}
+
+		for _, pattern := range regexpPatterns {
+			if !(pattern).MatchString(string(out)) {
+				return errors.Errorf("failed %q error message", pattern)
+			}
+		}
+
+		return nil
+	}, &testing.PollOptions{Timeout: 15 * time.Second}); err != nil {
+		return errors.Wrap(err, "please connect external display as required")
+	}
+	return nil
+}
+
+// ExternalDisplayDetectionForLocal verifies connected extended display is detected or not for local tests.
+func ExternalDisplayDetectionForLocal(ctx context.Context, numberOfDisplays int, regexpPatterns []*regexp.Regexp) error {
+	return externalDisplayDetection(ctx, nil, numberOfDisplays, regexpPatterns, false)
+}
+
+// ExternalDisplayDetectionForRemote verifies connected extended display is detected or not for remote tests.
+func ExternalDisplayDetectionForRemote(ctx context.Context, dut *dut.DUT, numberOfDisplays int, regexpPatterns []*regexp.Regexp) error {
+	return externalDisplayDetection(ctx, dut, numberOfDisplays, regexpPatterns, true)
+}
