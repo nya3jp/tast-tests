c92a435886204fde43bc96b155c4c47045951111
slutskii <slutskii@google.com>
2022-06-10 16:41:54 +0000

    kerberos: Add kerberos config in kerberos.Automatic* tests
    Added kerberos config to the ticket in the kerberos.Automatic* tests
    (it is already done in kerberos.Manual* tests) so it can help resolve
    DNS issues on some devices.
    
    BUG=chromium:1249427
    TEST=tast -verbose run localhost:2200 kerberos.Automatic*
    
    Change-Id: If6c1bfbbd5154b52c100039ca7140cb337bf89fa

--- src/chromiumos/tast/local/bundles/cros/kerberos/automatic_ticket_access_website.go
+++ src/chromiumos/tast/local/bundles/cros/kerberos/automatic_ticket_access_website.go
@@ -78,9 +78,10 @@ func AutomaticTicketAccessWebsite(ctx context.Context, s *testing.State) {
 
 	defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_automatic_ticket")
 
-	kerberosAcc := policy.KerberosAccountsValueOmitKrb5conf{
+	kerberosAcc := policy.KerberosAccountsValue{
 		Principal: config.KerberosAccount,
 		Password:  password,
+		Krb5conf:  []string{config.RealmsConfig},
 	}
 
 	// Update policies.
