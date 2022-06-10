5088dec3c7db030a0c84fb318d81ba45950cd3e0
Arowa Suliman <arowa@google.com>
2022-06-10 10:41:59 -0700

    wifi: add RTL8852CE
    BUG=b:216428884
    TEST=ran the test in Taeko device with 8852CE chip:
    tast run $DUT wifi.Driver [[ PASS ]]
    
    Cq-Depend: chromium:3699900
    Change-Id: I40aedc9f483b9e491a4effcdfd4877e5199730e6

--- src/chromiumos/tast/local/bundles/cros/wifi/driver.go
+++ src/chromiumos/tast/local/bundles/cros/wifi/driver.go
@@ -138,6 +138,9 @@ var expectedWLANDriver = map[wlan.DeviceID]map[string]string{
 	wlan.Realtek8852APCIE: {
 		"5.10": "wireless/realtek/rtw89/rtw89_pci.ko",
 	},
+	wlan.Realtek8852CPCIE: {
+		"5.10": "wireless/realtek/rtw89/rtw89_8852ce.ko",
+	},
 	wlan.MediaTekMT7921PCIE: {
 		"5.4":  "wireless/mediatek/mt76/mt7921/mt7921e.ko",
 		"5.10": "wireless/mediatek/mt76/mt7921/mt7921e.ko",
