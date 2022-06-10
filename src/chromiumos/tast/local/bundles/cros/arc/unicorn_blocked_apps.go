abf2e3e1e542307fc31f446b74d663342340ae43
Renuga Nanjappan <rnanjappan@google.com>
2022-06-09 20:10:08 -0700

    tast tests: Fix the blocked apps test
    The test has been checking the screen for
    install button not to be present. Instead
    this should check if install (greyed out)
    is present so that play store loading delay
    isn't counted as Pass where install button
    is not present.
    
    BUG=b:237023275
    TEST=tast run <DUT> arc.UnicornBlockedApps.vm (Verify that test fails
    when playstore fails to load).
    
    Change-Id: Iad4968d2208b7ab25e188ecac351cf2b8834e004

--- src/chromiumos/tast/local/bundles/cros/arc/unicorn_blocked_apps.go
+++ src/chromiumos/tast/local/bundles/cros/arc/unicorn_blocked_apps.go
@@ -131,9 +131,9 @@ func UnicornBlockedApps(ctx context.Context, s *testing.State) {
 	}
 
 	// Verify that install button is disabled for the blocked app.
-	installButton := d.Object(ui.ClassName("android.widget.Button"), ui.TextMatches("(?i)"+installButtonText), ui.Enabled(true))
-	if err := installButton.Exists(ctx); err == nil {
-		s.Fatal("Install Button Exisits for Blocked App: ", err)
+	installButton := d.Object(ui.ClassName("android.widget.Button"), ui.TextMatches("(?i)"+installButtonText), ui.Enabled(false))
+	if err := installButton.WaitForExists(ctx, DefaultUITimeout); err != nil {
+		s.Fatal("Failed to find the disabled install button for blocked app: ", err)
 	}
 
 }
