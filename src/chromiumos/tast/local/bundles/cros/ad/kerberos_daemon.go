88c4476dd706bce038ace905012b793a8fa88bac
slutskii <slutskii@google.com>
2022-06-10 12:43:58 +0000

    kerberos: Promote ad.KerberosDaemon to the Commit Queue
    Promoted stable test to CQ.
    
    BUG=chromium:1268384
    TEST=tast -verbose run localhost:2200 ad.KerberosDaemon
    
    Change-Id: Ideb703f6716046f530a25ee7884c71dc9c14c7ce

--- src/chromiumos/tast/local/bundles/cros/ad/kerberos_daemon.go
+++ src/chromiumos/tast/local/bundles/cros/ad/kerberos_daemon.go
@@ -26,7 +26,7 @@ func init() {
 			"fsandrade@chromium.org",
 			"chromeos-commercial-identity@google.com",
 		},
-		Attr: []string{"group:mainline", "informational"},
+		Attr: []string{"group:mainline"},
 	})
 }
 
