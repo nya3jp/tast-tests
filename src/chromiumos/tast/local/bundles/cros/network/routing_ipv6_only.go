e5bc56083f0b4ab03e4bfe8f4a8eb84a6f71397e
Jie Jiang <jiejiang@chromium.org>
2022-06-13 12:46:19 +0900

    network: Add routing_ipv6_only test
    This test verifies the IPv6-only environment, and the DHCP timeout event
    has no effect.
    
    BUG=b:192436642
    TEST=tast run $DUT network.Routing*
    
    Change-Id: Ic5d7eba9a487352e2b91217d700a884f2a9560fc

---
+++ src/chromiumos/tast/local/bundles/cros/network/routing_ipv6_only.go
@@ -0,0 +1,83 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/network/routing"
+	"chromiumos/tast/local/bundles/cros/network/virtualnet"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RoutingIPv6Only,
+		Desc:         "Verify the shill behavior and routing semantics in an IPv6-only environment",
+		Contacts:     []string{"jiejiang@google.com", "cros-networking@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		LacrosStatus: testing.LacrosVariantUnneeded,
+	})
+}
+
+func RoutingIPv6Only(ctx context.Context, s *testing.State) {
+	// Use a shortened context for test operations to reserve time for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	testEnv := routing.NewTestEnv()
+	if err := testEnv.SetUp(ctx); err != nil {
+		s.Fatal("Failed to set up routing test env: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := testEnv.TearDown(ctx); err != nil {
+			s.Error("Failed to tear down routing test env: ", err)
+		}
+	}(cleanupCtx)
+
+	// Setup the IPv6-only test network, and verify its connectivity.
+	testNetworkOpts := virtualnet.EnvOptions{
+		Priority:   routing.HighPriority,
+		NameSuffix: routing.TestSuffix,
+		EnableDHCP: false,
+		RAServer:   true,
+	}
+	if err := testEnv.CreateNetworkEnvForTest(ctx, testNetworkOpts); err != nil {
+		s.Fatal("Failed to create network for test: ", err)
+	}
+	if err := testEnv.WaitForServiceOnline(ctx, testEnv.TestService); err != nil {
+		s.Fatal("Failed to wait for the test service online: ", err)
+	}
+	if errs := testEnv.VerifyTestNetwork(ctx, routing.VerifyOptions{
+		IPv4:      false,
+		IPv6:      true,
+		IsPrimary: true,
+		Timeout:   5 * time.Second,
+	}); len(errs) != 0 {
+		for _, err := range errs {
+			s.Fatal("Failed to verify connectivity to the test network before DHCP timeout: ", err)
+		}
+		return
+	}
+
+	// Trigger the DHCP timeout event, and verify that the connectivty is not affected.
+	testing.ContextLog(ctx, "Waiting for DHCP timeout event for ", routing.DHCPExtraTimeout)
+	testing.Sleep(ctx, routing.DHCPExtraTimeout)
+	testing.ContextLog(ctx, "DHCP timeout was triggered")
+	if errs := testEnv.VerifyTestNetwork(ctx, routing.VerifyOptions{
+		IPv4:      false,
+		IPv6:      true,
+		IsPrimary: true,
+		Timeout:   0,
+	}); len(errs) != 0 {
+		for _, err := range errs {
+			s.Fatal("Failed to verify connectivity to the test network after DHCP timeout: ", err)
+		}
+		return
+	}
+}
