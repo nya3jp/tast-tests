50542de57d7583319a079c7dfade1c37ccf2e2ad
pjlee <pjlee@google.com>
2022-06-13 22:27:45 +0000

    arc/gio: Stabilize GIO Display Test
    Change toggle icon image and add specificity to node queries for the
    InputOverlayDisplay test, as well as convert some UIAuto calls to
    ACUITI.
    
    BUG=b:234678883
    TEST=./fast_build.sh -T && tast run $DUT arc.InputOverlayDisplay*
    
    Change-Id: I982f3630a890ead49c47a97969e7f13d0744eaeb

--- src/chromiumos/tast/local/bundles/cros/arc/input_overlay_display.go
+++ src/chromiumos/tast/local/bundles/cros/arc/input_overlay_display.go
@@ -12,6 +12,7 @@ import (
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/arc/gio"
 	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/uidetection"
@@ -51,6 +52,7 @@ func InputOverlayDisplay(ctx context.Context, s *testing.State) {
 		ui := uiauto.New(params.TestConn).WithTimeout(time.Minute)
 		// Start up ACUITI.
 		uda := uidetection.NewDefault(params.TestConn).WithOptions(uidetection.Retries(3)).WithScreenshotStrategy(uidetection.ImmediateScreenshot).WithTimeout(time.Minute)
+		defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, params.TestConn)
 
 		// CUJ: Hide game overlay.
 		s.Log("Display CUJ #1: hide game overlay")
@@ -64,20 +66,21 @@ func InputOverlayDisplay(ctx context.Context, s *testing.State) {
 			// Exit out of menu.
 			uda.Tap(uidetection.CustomIcon(s.DataPath("input-overlay-menu-close.png")).Below(uidetection.Word("BUTTON"))),
 			// Poll UI elements no longer exist, but overlay is still responsive.
-			ui.Gone(nodewith.Name("m")),
+			ui.Gone(nodewith.Name("m").HasClass("LabelButtonLabel")),
 			gio.TapOverlayButton(kb, "m", &params, gio.TopTap),
-			ui.Gone(nodewith.Name("w")),
+			ui.Gone(nodewith.Name("w").HasClass("LabelButtonLabel")),
 			gio.MoveOverlayButton(kb, "w", &params),
 			// Poll edits can still be done.
-			ui.LeftClick(nodewith.Name("Game Control").HasClass("ImageButton")),
+			uda.Tap(uidetection.CustomIcon(s.DataPath("input-overlay-menu.png"))),
 			uda.Tap(uidetection.Word("Customize")),
-			ui.WaitUntilExists(nodewith.Name("m")),
-			ui.WaitUntilExists(nodewith.Name("w")),
+			ui.WaitUntilExists(nodewith.Name("m").HasClass("LabelButtonLabel")),
+			ui.WaitUntilExists(nodewith.Name("w").HasClass("LabelButtonLabel")),
 			// Exit out.
 			uda.Tap(uidetection.Word("Cancel")),
 		)(ctx); err != nil {
 			s.Error("Failed to verify game overlay hidden: ", err)
 			// Reset activity.
+			s.Fatal("I want a tree")
 			if err := gio.CloseAndRelaunchActivity(ctx, &params); err != nil {
 				s.Fatal("Failed to reset application after failed CUJ: ", err)
 			}
@@ -93,16 +96,17 @@ func InputOverlayDisplay(ctx context.Context, s *testing.State) {
 			// Exit out of menu.
 			uda.Tap(uidetection.CustomIcon(s.DataPath("input-overlay-menu-close.png")).Below(uidetection.Word("BUTTON"))),
 			// Poll UI elements no longer exist, and overlay is unresponsive.
-			ui.Gone(nodewith.Name("m")),
+			ui.Gone(nodewith.Name("m").HasClass("LabelButtonLabel")),
 			not(gio.TapOverlayButton(kb, "m", &params, gio.TopTap)),
-			ui.Gone(nodewith.Name("w")),
+			ui.Gone(nodewith.Name("w").HasClass("LabelButtonLabel")),
 			not(gio.MoveOverlayButton(kb, "w", &params)),
 			// Check "Customize" button disabled.
-			ui.LeftClick(nodewith.Name("Game Control").HasClass("ImageButton")),
+			uda.Tap(uidetection.CustomIcon(s.DataPath("input-overlay-menu.png"))),
 			uda.Tap(uidetection.Word("Customize")),
 			not(uda.Gone(uidetection.Word("Customize"))),
 		)(ctx); err != nil {
 			s.Error("Failed to verify game overlay disabled: ", err)
+			s.Fatal("I want a tree")
 		}
 
 		return nil
