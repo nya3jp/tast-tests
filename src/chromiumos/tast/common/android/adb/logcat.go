8e712bdf2743ea7372bc844c9fca5fe0fb419b25
pjlee <pjlee@google.com>
2022-06-13 23:16:50 +0000

    arcappgameperf: Return the line in WaitForLogcat
    Currently, WaitForLogcat simply throws away the candidate line after it
    is found; with this change, we capture and return it instead.
    
    BUG=b:230500493
    TEST=./fast_build.sh -T
    
    Change-Id: If2276cbd2a7e46c43ae071fa35f05627000cb31a

--- src/chromiumos/tast/common/android/adb/logcat.go
+++ src/chromiumos/tast/common/android/adb/logcat.go
@@ -24,23 +24,24 @@ func RegexpPred(exp *regexp.Regexp) func(string) bool {
 	}
 }
 
-// WaitForLogcat keeps scanning logcat. The function pred is called on the logcat contents line by line. This function returns successfully if pred returns true. If pred never returns true, this function returns an error as soon as the context is done.
+// WaitForLogcat keeps scanning logcat. The function pred is called on the logcat contents line by line. This function returns the candidate line successfully if pred returns true. If pred never returns true, this function returns an error as soon as the context is done.
 // An optional quitFunc will be polled at regular interval, which can be used to break early if for example the activity which is supposed to print the exp has crashed
-func (d *Device) WaitForLogcat(ctx context.Context, pred func(string) bool, quitFunc ...func() bool) error {
+func (d *Device) WaitForLogcat(ctx context.Context, pred func(string) bool, quitFunc ...func() bool) (string, error) {
+	ret := ""
 	if len(quitFunc) > 1 {
-		return errors.New("only 1 quitFunc is supported")
+		return ret, errors.New("only 1 quitFunc is supported")
 	}
 
 	cmd := d.ShellCommand(ctx, "logcat")
 
 	pipe, err := cmd.StdoutPipe()
 	if err != nil {
-		return errors.Wrap(err, "failed to open StdoutPipe")
+		return ret, errors.Wrap(err, "failed to open StdoutPipe")
 	}
 	defer pipe.Close()
 
 	if err := cmd.Start(); err != nil {
-		return errors.Wrapf(err, "failed to start %s", shutil.EscapeSlice(cmd.Args))
+		return ret, errors.Wrapf(err, "failed to start %s", shutil.EscapeSlice(cmd.Args))
 	}
 	defer func() {
 		cmd.Kill()
@@ -64,6 +65,7 @@ func (d *Device) WaitForLogcat(ctx context.Context, pred func(string) bool, quit
 			default:
 				if pred(scanner.Text()) {
 					status <- nil
+					ret = scanner.Text()
 					return
 				}
 			}
@@ -93,13 +95,13 @@ func (d *Device) WaitForLogcat(ctx context.Context, pred func(string) bool, quit
 	case err := <-status:
 		close(breakChan)
 		if err != nil {
-			return errors.Wrap(err, "error while scanning logcat")
+			return ret, errors.Wrap(err, "error while scanning logcat")
 		}
-		return nil
+		return ret, nil
 	case <-ctx.Done():
 		// This is usually a timeout.
 		close(breakChan)
-		return errors.Wrap(ctx.Err(), "context was done while waiting match of pred in logcat")
+		return ret, errors.Wrap(ctx.Err(), "context was done while waiting match of pred in logcat")
 	}
 }
 
--- src/chromiumos/tast/local/arc/logcat.go
+++ src/chromiumos/tast/local/arc/logcat.go
@@ -19,6 +19,13 @@ func RegexpPred(exp *regexp.Regexp) func(string) bool {
 // WaitForLogcat keeps scanning logcat. The function pred is called on the logcat contents line by line. This function returns successfully if pred returns true. If pred never returns true, this function returns an error as soon as the context is done.
 // An optional quitFunc will be polled at regular interval, which can be used to break early if for example the activity which is supposed to print the exp has crashed
 func (a *ARC) WaitForLogcat(ctx context.Context, pred func(string) bool, quitFunc ...func() bool) error {
+	_, err := a.device.WaitForLogcat(ctx, pred, quitFunc...)
+	return err
+}
+
+// WaitForLogcatString keeps scanning logcat. The function pred is called on the logcat contents line by line. This function returns the candidate line successfully if pred returns true. If pred never returns true, this function returns an error as soon as the context is done.
+// An optional quitFunc will be polled at regular interval, which can be used to break early if for example the activity which is supposed to print the exp has crashed
+func (a *ARC) WaitForLogcatString(ctx context.Context, pred func(string) bool, quitFunc ...func() bool) (string, error) {
 	return a.device.WaitForLogcat(ctx, pred, quitFunc...)
 }
 
