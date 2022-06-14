106dfc6d1b648e1912977071282c028a108e2720
Ambalavanan MM <ambalavanan.m.m@intel.com>
2022-06-14 17:45:53 +0530

    typec: Add HDMIPlugUnplugAudioVideoPlayback test
    BUG=b:225081951
    TEST=tast run -var=typec.cSwitchPort=<cswitch_port_ID> -var=typec.domainIP=localhost:9000 <DUT_IP> typec.HDMIPlugUnplugAudioVideoPlayback
    
    Change-Id: I28cc664518d1816ec5dc92bf01f555ac64b64d7d

---
+++ src/chromiumos/tast/local/bundles/cros/typec/hdmi_plug_unplug_audio_video_playback.go
@@ -0,0 +1,225 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package typec
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"time"
+
+	"chromiumos/tast/common/action"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/audio"
+	"chromiumos/tast/local/audio/crastestclient"
+	typecutilshelper "chromiumos/tast/local/bundles/cros/typec/typecutils"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/cswitch"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HDMIPlugUnplugAudioVideoPlayback,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verifies HDMI plug/unplug using USB type-C adapter during audio/video playback",
+		Contacts:     []string{"ambalavanan.m.m@intel.com", "intel-chrome-system-automation-team@intel.com"},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:typec"},
+		Data:         []string{"bear-320x240.h264.mp4", "video.html", "playback.js"},
+		Vars:         []string{"typec.cSwitchPort", "typec.domainIP"},
+		Fixture:      "chromeLoggedIn",
+		Timeout:      7 * time.Minute,
+	})
+}
+
+func HDMIPlugUnplugAudioVideoPlayback(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 2*time.Minute)
+	defer cancel()
+
+	// Config file which contains expected values of USB4 parameters.
+	const jsonTestConfig = "test_config.json"
+
+	// cswitch port ID.
+	cSwitchON := s.RequiredVar("typec.cSwitchPort")
+	// IP address of Tqc server hosting device.
+	domainIP := s.RequiredVar("typec.domainIP")
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to initialize keyboard input: ", err)
+	}
+
+	// Create C-Switch session that performs hot plug-unplug on TBT device.
+	sessionID, err := cswitch.CreateSession(ctx, domainIP)
+	if err != nil {
+		s.Fatal("Failed to create session: ", err)
+	}
+
+	cSwitchOFF := "0"
+	defer func(ctx context.Context) {
+		if err := cswitch.ToggleCSwitchPort(ctx, sessionID, cSwitchOFF, domainIP); err != nil {
+			s.Fatal("Failed to disable c-switch port: ", err)
+		}
+		if err := cswitch.CloseSession(ctx, sessionID, domainIP); err != nil {
+			s.Log("Failed to close session: ", err)
+		}
+	}(cleanupCtx)
+
+	if err := cswitch.ToggleCSwitchPort(ctx, sessionID, cSwitchON, domainIP); err != nil {
+		s.Fatal("Failed to enable c-switch port: ", err)
+	}
+
+	if err := typecutilshelper.FindConnectedDisplay(ctx, "HDMI|DP", 1); err != nil {
+		s.Fatal("Failed to find connected display: ", err)
+	}
+
+	if err := typecutilshelper.CheckDisplayInfo(ctx, true); err != nil {
+		s.Fatal("Failed to check display info : ", err)
+	}
+
+	srv := httptest.NewServer(http.FileServer(s.DataFileSystem()))
+	defer srv.Close()
+	url := srv.URL + "/video.html"
+	conn, err := cr.NewConn(ctx, url)
+	if err != nil {
+		s.Fatal("Failed to load video_with_rounded_corners.html: ", err)
+	}
+
+	display := switchWindowToDisplay(ctx, tconn, kb, true)
+	if err := display(ctx); err != nil {
+		s.Fatal("Failed to switch windows to display: ", err)
+	}
+
+	videoFile := "bear-320x240.h264.mp4"
+	if err := conn.Call(ctx, nil, "playUntilEnd", videoFile, true); err != nil {
+		s.Fatal("Failed to play video: ", err)
+	}
+
+	expectedAudioOuputNode := "HDMI"
+	cras, err := audio.NewCras(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Cras object: ", err)
+	}
+
+	// Get current audio output device info.
+	deviceName, deviceType, err := cras.SelectedOutputDevice(ctx)
+	if err != nil {
+		s.Fatal("Failed to get the selected audio device: ", err)
+	}
+
+	if deviceType != expectedAudioOuputNode {
+		if err := cras.SetActiveNodeByType(ctx, expectedAudioOuputNode); err != nil {
+			s.Fatalf("Failed to select active device %s: %v", expectedAudioOuputNode, err)
+		}
+		deviceName, deviceType, err = cras.SelectedOutputDevice(ctx)
+		if err != nil {
+			s.Fatal("Failed to get the selected audio device: ", err)
+		}
+		if deviceType != expectedAudioOuputNode {
+			s.Fatalf("Failed to set the audio node type: got %q; want %q", deviceType, expectedAudioOuputNode)
+		}
+	}
+	out, err := testexec.CommandContext(ctx, "cras_test_client").Output()
+	if err != nil {
+		s.Fatal("Failed to exceute cras_test_client command: ", err)
+	}
+	re := regexp.MustCompile(`yes.*HDMI.*2\*`)
+	if !re.MatchString(string(out)) {
+		s.Fatal("Failed to select HDMI as output audio node")
+	}
+
+	if err := verifyAudioRoute(ctx, deviceName); err != nil {
+		s.Fatalf("Failed to verify audio routing through %q: %v", expectedAudioOuputNode, err)
+	}
+}
+
+// extendedDisplayWindowClassName obtains the class name of the root window on the extended display.
+func extendedDisplayWindowClassName(ctx context.Context, tconn *chrome.TestConn) (string, error) {
+	ui := uiauto.New(tconn)
+	// Root window on extended display has the class name in RootWindow-<id> format.
+	// We found extended display window could be RootWindow-1, or RootWindow-2.
+	// Here we try 1 to 10.
+	for i := 1; i <= 10; i++ {
+		className := fmt.Sprintf("RootWindow-%d", i)
+		win := nodewith.ClassName(className).Role(role.Window)
+		if err := ui.Exists(win)(ctx); err == nil {
+			return className, nil
+		}
+	}
+	return "", errors.New("failed to find any window with class name RootWindow-1 to RootWindow-10")
+}
+
+// switchWindowToDisplay switches current window to expected display.
+func switchWindowToDisplay(ctx context.Context, tconn *chrome.TestConn, kb *input.KeyboardEventWriter, externalDisplay bool) action.Action {
+	return func(ctx context.Context) error {
+		var expectedRootWindow *nodewith.Finder
+		var display string
+		ui := uiauto.New(tconn)
+		w, err := ash.FindWindow(ctx, tconn, func(w *ash.Window) bool {
+			return w.IsActive && w.IsFrameVisible
+		})
+		if err != nil {
+			return errors.Wrap(err, "failed to get current active window")
+		}
+		if externalDisplay {
+			display = "external display"
+			extendedWinClassName, err := extendedDisplayWindowClassName(ctx, tconn)
+			if err != nil {
+				return errors.Wrap(err, "failed to find root window on external display")
+			}
+			expectedRootWindow = nodewith.ClassName(extendedWinClassName).Role(role.Window)
+		} else {
+			display = "internal display"
+			// Root window on built-in display.
+			expectedRootWindow = nodewith.ClassName("RootWindow-0").Role(role.Window)
+		}
+		currentWindow := nodewith.Name(w.Title).Role(role.Window)
+		expectedWindow := currentWindow.Ancestor(expectedRootWindow).First()
+		if err := ui.Exists(expectedWindow)(ctx); err != nil {
+			testing.ContextLog(ctx, "Expected window not found: ", err)
+			testing.ContextLogf(ctx, "Switch window %q to %s", w.Title, display)
+			return uiauto.Combine("switch window to "+display,
+				kb.AccelAction("Search+Alt+M"),
+				ui.WithTimeout(3*time.Second).WaitUntilExists(expectedWindow),
+			)(ctx)
+		}
+		return nil
+	}
+}
+
+// verifyAudioRoute checks whether audio is routing via deviceName or not.
+func verifyAudioRoute(ctx context.Context, deviceName string) error {
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		devName, err := crastestclient.FirstRunningDevice(ctx, audio.OutputStream)
+		testing.ContextLog(ctx, string(devName))
+		if err != nil {
+			return errors.Wrap(err, "failed to detect running output device")
+		}
+		if deviceName != devName {
+			return errors.Wrapf(err, "failed to route the audio through expected audio node: got %q; want %q", devName, deviceName)
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: 10 * time.Second, Interval: 250 * time.Millisecond}); err != nil {
+		return errors.Wrapf(err, "timeout waiting for %q", deviceName)
+	}
+	return nil
+}
