1cc98d3f8d49b380eaf27984cda0a3428ebe6e73
Patryk Duda <pdk@semihalf.com>
2022-06-15 16:10:24 +0200

    fingerprint: Implement encryption status parsing
    Until now, check if FPMCU was initialized with TPM seed was based on
    matching 'ectool' command output with regex. Actually, it could be done
    in a better way: we can parse the output and extract flags. This
    approach is used to obtain flash protect flags. New implementation also
    has unit tests.
    
    This patch introduces no functional changes.
    
    BUG=b:233276848
    TEST=Run 'tast run <IP> firmware.FpTpmSeed' make sure that test passes.
    TEST=Reboot FPMCU with 'ectool --name=cros_fp reboot_ec'.
         Run 'tast run <IP> firmware.FpTpmSeed' make sure that test fails
         with 'TPM seed is not set' error.
    TEST=fast_build.sh -v -t chromiumos/tast/remote/firmware/fingerprint
    
    Change-Id: I3a53c71c78c7d1736b541b4cdf7f857e794d374f

---
+++ src/chromiumos/tast/remote/firmware/fingerprint/fingerprint_encryption_test.go
@@ -0,0 +1,62 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fingerprint
+
+import "testing"
+
+func TestEncryptionStatusEctoolUnmarshalerTPMSeed(t *testing.T) {
+	// It should not matter that there are tabs to the left of the example output.
+	var out = `
+	FPMCU encryption status: 0x00000001 FPTPM_seed_set
+	Valid flags:             0x00000001 FPTPM_seed_set
+	`
+	var expect = EncryptionStatus{
+		Current: EncryptionStatusTPMSeedSet,
+		Valid:   EncryptionStatusTPMSeedSet,
+	}
+
+	actual, err := unmarshalEctoolEncryptionStatus(out)
+	if err != nil {
+		t.Fatal("Failed to unmarshal encryption status: ", err)
+	}
+
+	if actual != expect {
+		t.Fatalf("Unmarshaled encryption status block %+v doesn't match expected block %+v.", actual, expect)
+	}
+}
+
+func TestEncryptionStatusEctoolUnmarshalerNoTPMSeed(t *testing.T) {
+	// It should not matter that there are tabs to the left of the example output.
+	var out = `
+	FPMCU encryption status: 0x00000000
+	Valid flags:             0x00000001 FPTPM_seed_set
+	`
+	var expect = EncryptionStatus{
+		Current: 0,
+		Valid:   EncryptionStatusTPMSeedSet,
+	}
+
+	actual, err := unmarshalEctoolEncryptionStatus(out)
+	if err != nil {
+		t.Fatal("Failed to unmarshal encryption status: ", err)
+	}
+
+	if actual != expect {
+		t.Fatalf("Unmarshaled encryption status block %+v doesn't match expected block %+v.", actual, expect)
+	}
+}
+
+func TestEncryptionStatusFlagsIsSet(t *testing.T) {
+	var flags EncryptionStatusFlags
+
+	if flags.IsSet(EncryptionStatusTPMSeedSet) {
+		t.Fatal("Flag EncryptionStatusTPMSeedSet was reported as set.")
+	}
+
+	flags = EncryptionStatusTPMSeedSet
+	if !flags.IsSet(EncryptionStatusTPMSeedSet) {
+		t.Fatal("Flag EncryptionStatusTPMSeedSet was reported as not set.")
+	}
+}
