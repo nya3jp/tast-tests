f76c15ca593b59dc133315b3636ba4eb966604ac
Shijin Abraham <shijinabraham@google.com>
2022-06-15 17:17:00 -0700

    bluetooth: Access bluetooth mojo object using an import
    BUG=b:231076808
    TEST=Ran bluetooth.ToggleBluetoothUsingMojo
    
    Change-Id: Ibd456718ff3bda24e5e4b1a81135ce7bd8659ee2

--- src/chromiumos/tast/local/bluetooth/mojo/js_wrapper.go
+++ src/chromiumos/tast/local/bluetooth/mojo/js_wrapper.go
@@ -5,10 +5,11 @@
 package mojo
 
 // BTConfigJS is javascript code that initializes the config object and defines
-// some additional functions
-const BTConfigJS = `function() {
+// some additional functions.
+const BTConfigJS = `async function () {
+    let btmojo = await import('chrome://resources/mojo/chromeos/services/bluetooth_config/public/mojom/cros_bluetooth_config.mojom-webui.js');
     return {
-        bluetoothConfig: chromeos.bluetoothConfig.mojom.CrosBluetoothConfig.getRemote(),
+        bluetoothConfig : btmojo.CrosBluetoothConfig.getRemote(),
 
         // for interface SystemPropertiesObserver
         // OnPropertiesUpdated(BluetoothSystemProperties properties);
@@ -18,10 +19,9 @@ const BTConfigJS = `function() {
             this.systemProperties_.modificationState = properties.modificationState;
         },
 
-
         // Initialization
         initSysPropObs: function() {
-            this.SysPropObsReceiver = new chromeos.bluetoothConfig.mojom.SystemPropertiesObserverReceiver(this);
+            this.SysPropObsReceiver = new btmojo.SystemPropertiesObserverReceiver(this);
             this.bluetoothConfig.observeSystemProperties(this.SysPropObsReceiver.$.bindNewPipeAndPassRemote());
         },
 
