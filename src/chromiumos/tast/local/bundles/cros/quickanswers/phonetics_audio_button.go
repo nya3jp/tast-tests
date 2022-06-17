012d8e6867bf6bf832ffa28b393b92c546158745
Yue Li <updowndota@google.com>
2022-06-17 00:18:08 -0700

    Tast: Add quickanswers.PhoneticsAudioButton Tast tests
    Also added lacros variant.
    
    BUG=b:232146685
    TEST=Test on DUT with command: tast -verbose run DUT quickanswers.PhoneticsAudioButton
    
    Change-Id: Ia661691d9af9c5e67e1d6c59740b2a67f6b33c8b

---
+++ src/chromiumos/tast/local/bundles/cros/quickanswers/phonetics_audio_button.go
@@ -0,0 +1,132 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package quickanswers
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/audio"
+	"chromiumos/tast/local/audio/crastestclient"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/event"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PhoneticsAudioButton,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Test Quick Answers phonetics audio button",
+		Contacts: []string{
+			"updowndota@google.com",
+			"croissant-eng@google.com",
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Fixture: "chromeLoggedIn",
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			Fixture:           "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Val:               browser.TypeLacros,
+		}},
+	})
+}
+
+// PhoneticsAudioButton tests Quick Answers phonetics audio button.
+func PhoneticsAudioButton(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	if err := tconn.Call(ctx, nil, `tast.promisify(chrome.autotestPrivate.setWhitelistedPref)`, "settings.quick_answers.enabled", true); err != nil {
+		s.Fatal("Failed to enable Quick Answers: ", err)
+	}
+
+	// Setup a browser.
+	bt := s.Param().(browser.Type)
+	br, closeBrowser, err := browserfixt.SetUp(ctx, cr, bt)
+	if err != nil {
+		s.Fatal("Failed to open the browser: ", err)
+	}
+	defer closeBrowser(cleanupCtx)
+
+	// Open page with the query word on it.
+	const queryWord = "icosahedron"
+	conn, err := br.NewConn(ctx, "https://google.com/search?q="+queryWord)
+	if err != nil {
+		s.Fatal("Failed to create new Chrome connection: ", err)
+	}
+	defer conn.Close()
+	defer conn.CloseTarget(ctx)
+
+	ui := uiauto.New(tconn)
+	// Wait for the query word to appear.
+	query := nodewith.Name(queryWord).Role(role.StaticText).First()
+	if err := ui.WaitUntilExists(query)(ctx); err != nil {
+		s.Fatal("Failed to wait for query to load: ", err)
+	}
+
+	// Select the word and setup watcher to wait for text selection event.
+	if err := ui.WaitForEvent(nodewith.Root(),
+		event.TextSelectionChanged,
+		ui.Select(query, 0 /*startOffset*/, query, 2 /*endOffset*/))(ctx); err != nil {
+		s.Fatal("Failed to select query: ", err)
+	}
+
+	// Right click the selected word and ensure the Quick Answers UI shows up with the phonetics audio button.
+	// Left click the phonetics audio button to play the audio.
+	quickAnswers := nodewith.ClassName("QuickAnswersView")
+	audioButton := nodewith.ClassName("ImageButton").Name("Listen")
+	if err := uiauto.Combine("Show context menu",
+		ui.RightClick(query),
+		ui.WaitUntilExists(quickAnswers),
+		ui.WaitUntilExists(audioButton),
+		ui.LeftClick(audioButton))(ctx); err != nil {
+		s.Fatal("Quick Answers result not showing up: ", err)
+	}
+
+	// Get Current active node.
+	cras, err := audio.NewCras(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Cras object")
+	}
+	audioDeviceName, _, err := cras.SelectedOutputDevice(ctx)
+	if err != nil {
+		s.Fatal("Failed to get the selected audio device: ", err)
+	}
+
+	// Ensure the phonetics audio is played on the active audio device.
+	devName, err := crastestclient.FirstRunningDevice(ctx, audio.OutputStream)
+	if err != nil {
+		s.Fatal("Failed to detect running output device: ", err)
+	}
+	if audioDeviceName != devName {
+		s.Fatalf("Failed to route the audio through expected audio node: got %q; want %q", devName, audioDeviceName)
+	}
+
+	// Dismiss the context menu and ensure the Quick Answers UI also dismiss.
+	if err := uiauto.Combine("Dismiss context menu",
+		ui.LeftClick(query),
+		ui.WaitUntilGone(quickAnswers))(ctx); err != nil {
+		s.Fatal("Quick Answers result not dismissed: ", err)
+	}
+}
