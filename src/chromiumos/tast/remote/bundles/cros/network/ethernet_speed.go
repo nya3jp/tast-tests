c5d837a93c94473bbe8a9230ef05d476313b4d77
Ambalavanan MM <ambalavanan.m.m@intel.com>
2022-06-17 11:11:50 +0530

    network: Add EthernetSpeed test
    This CL generates a temp. file and transfers it from Ubuntu host
    to the DUT and calculates the time taken to transfer via Ethernet.
    
    BUG=b:225081951
    TEST=tast run -var="servo=localhost:9999" <DUT IP>
    network.EthernetSpeed
    
    Change-Id: Ib425c6fc70f0a4f7b2230fa417e28ef9a1488016

---
+++ src/chromiumos/tast/remote/bundles/cros/network/ethernet_speed.go
@@ -0,0 +1,115 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/remote/firmware/fixture"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/services/cros/network"
+	"chromiumos/tast/services/cros/wifi"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         EthernetSpeed,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Ethernet LAN Speed Test",
+		SoftwareDeps: []string{"chrome"},
+		Data:         []string{"testing_rsa"},
+		ServiceDeps:  []string{"tast.cros.network.EthernetService", "tast.cros.wifi.ShillService"},
+		Contacts:     []string{"ambalavanan.m.m@intel.com", "intel-chrome-system-automation-team@intel.com"},
+		Fixture:      fixture.NormalMode,
+	})
+}
+
+func EthernetSpeed(ctx context.Context, s *testing.State) {
+	h := s.FixtValue().(*fixture.Value).Helper
+
+	testfileName := "test.img"
+	tmpDir, err := ioutil.TempDir("", "tast-tmp")
+	if err != nil {
+		s.Fatal("Failed to create temp dir: ", err)
+	}
+	testfilePath := filepath.Join(tmpDir, testfileName)
+
+	cl, err := rpc.Dial(ctx, h.DUT, s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	// Disable WiFi.
+	wifiClient := wifi.NewShillServiceClient(cl.Conn)
+	if _, err := wifiClient.SetWifiEnabled(ctx, &wifi.SetWifiEnabledRequest{Enabled: false}); err != nil {
+		s.Fatal("Could not disable Wifi: ", err)
+	}
+
+	// Assert WiFi is down.
+	if response, err := wifiClient.GetWifiEnabled(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Failed to WiFi status: ", err)
+	} else if response.Enabled {
+		s.Fatal("Failed: Wifi is on, expected to be off ")
+	}
+
+	client := network.NewEthernetServiceClient(cl.Conn)
+
+	if _, err := client.New(ctx, &empty.Empty{}); err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+
+	path, err := client.DownloadPath(ctx, &empty.Empty{})
+	if err != nil {
+		s.Fatal("Failed to get Download path: ", err)
+	}
+
+	// Chmod the keyfile so that ssh connections do not fail due to
+	// open permissions.
+	cmd := testexec.CommandContext(ctx, "cp", s.DataPath("testing_rsa"), tmpDir)
+	if err := cmd.Run(); err != nil {
+		s.Fatal("Failed to copy testing_rsa to tast temp dir: ", err)
+	}
+	sshKey := filepath.Join(tmpDir, "testing_rsa")
+	if err := os.Chmod(sshKey, 0600); err != nil {
+		s.Fatal("Unable to chmod sshkey to 0600: ", err)
+	}
+
+	// Generate a 1GB file for scp.
+	if err := testexec.CommandContext(
+		ctx, "dd", "if=/dev/zero", "of="+testfilePath, "bs=1", "count=0", "seek=1G").Run(testexec.DumpLogOnError); err != nil {
+		s.Fatal("Failed to generate test file: ", err)
+	}
+
+	hostName := s.DUT().HostName()
+	dutIP := strings.Split(hostName, ":")[0]
+
+	args := []string{"-i", sshKey, "-o", "StrictHostKeyChecking=no", "-o",
+		"UserKnownHostsFile=/dev/null", testfilePath,
+		"root@" + dutIP + ":" + path.DownloadPath}
+
+	start := time.Now()
+	if err = testexec.CommandContext(ctx, "scp", args...).Run(); err != nil {
+		s.Fatal("Failed to do scp: ", err)
+	}
+	elapsedTime := time.Since(start)
+
+	defer h.DUT.Conn().CommandContext(ctx, "rm", testfilePath).Run()
+
+	expectedTime := 10 * time.Second
+	if elapsedTime > expectedTime {
+		s.Errorf("Failed to transfer file within %v Seconds. Elapsed time: %v Seconds", expectedTime, elapsedTime)
+	}
+
+}
