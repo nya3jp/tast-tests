066563e3391c782beca0408520950f76ab35bb0a
Alvin Jia <alvinjia@google.com>
2022-06-20 04:52:37 +0000

    tast-tests: promote stable crostini tests
    These selected tests are stable with 20+ passes:
    https://stainless.corp.google.com/search?view=matrix&row=test&col=build&first_date=2022-06-07&last_date=2022-06-20&test=crostini%5C..*_stable&build=%28R104%7CR105%29-%5Cd*&exclude_board=volteer&exclude_model=lars%7Ccave%7Cchell%7Ckano%7Cvolmar%7Cprimus%7Ccaroline%7Ccerise%7Ctaniks%7Cgimble%7Credrix%7Cvolteer2%7Celdrid%7Cezkinil%7Cgumboz%7Cvilboz14&status=GOOD&status=FAIL&status=ERROR&exclude_reason=RMA+target+not+found%3A+context+deadline+exceeded%7Cfailed+to+establish+connection+to+Chrome+Debugging+Protocol+with+debugging+port%7CChromebook+is+offline%7CFailed+to+start+Chrome%3A+login+failed&exclude_cts=false&exclude_not_run=false&exclude_non_release=true&exclude_au=true&exclude_acts=true&exclude_retried=false&exclude_non_production=false&exclude_non_critical=false
    
    The tests in https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3709457
    are not included in this CL.
    
    BUG=b:236556423
    TEST=tast run <DUT> crostini.*
    
    Change-Id: I5ab628c258da03f40f4ee43a0bec604a02d43f45

--- src/chromiumos/tast/local/bundles/cros/crostini/copy_paste.go
+++ src/chromiumos/tast/local/bundles/cros/crostini/copy_paste.go
@@ -79,8 +79,8 @@ func init() {
 		Func:         CopyPaste,
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Test copy paste functionality",
-		Contacts:     []string{"sidereal@google.com", "cros-containers-dev@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Contacts:     []string{"clumptini+oncall@google.com"},
+		Attr:         []string{"group:mainline"},
 		Data:         []string{copyApplet, pasteApplet},
 		SoftwareDeps: []string{"chrome", "vm_host"},
 		Params: []testing.Param{
