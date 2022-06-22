9f9c2e6f96d22867db2010e6293ef78a80444d08
Navin Ramsaroop <ramsaroop@google.com>
2022-06-22 19:12:03 +0000

    Add keyboard input to VideoCUJ
    Augment VideoCUJ to press and hold the up and down arrow with the video
    playing, thus changing the volume, to capture input latency with regards
    to holding down a specific key. Additionally, pause and play the video
    with the spacebar to capture general key-press input latency.
    
    BUG=b:236030517
    TEST=Run ui.VideoCUJ.clamshell and ui.VideoCUJ.lacros and verify
    that each results-chart.json has values for
    Event.Latency.EndToEnd.KeyPress.
    
    Change-Id: I915338b38b2c4c576c9c64a287c6c6f67a2338b4

--- src/chromiumos/tast/local/bundles/cros/ui/cuj/inputsimulations/keyboard.go
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/inputsimulations/keyboard.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"chromiumos/tast/common/action"
+	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/input"
 )
 
@@ -18,6 +19,31 @@ func RepeatKeyPressFor(ctx context.Context, kw *input.KeyboardEventWriter, key s
 		"press ["+key+"] and sleep",
 		kw.AccelAction(key),
 		action.Sleep(delay)))
+}
+
+// RepeatKeyPress presses the specified key |n| times, with a |delay| between key presses.
+func RepeatKeyPress(ctx context.Context, kw *input.KeyboardEventWriter, key string, delay time.Duration, n int) error {
+	return uiauto.Repeat(
+		n,
+		action.Combine("press "+key+" and sleep",
+			kw.AccelAction(key),
+			action.Sleep(delay),
+		),
+	)(ctx)
+}
+
+// RepeatKeyHold holds a key down for |keyHoldDuration| |n| times,
+// with a |delay| between each key release and the next key press.
+func RepeatKeyHold(ctx context.Context, kw *input.KeyboardEventWriter, key string, keyHoldDuration, delay time.Duration, n int) error {
+	return uiauto.Repeat(
+		n,
+		action.Combine("press and hold "+key+" and sleep",
+			kw.AccelPressAction(key),
+			action.Sleep(keyHoldDuration),
+			kw.AccelReleaseAction(key),
+			action.Sleep(delay),
+		),
+	)(ctx)
 }
 
 func runActionFor(ctx context.Context, minDuration time.Duration, a action.Action) error {
