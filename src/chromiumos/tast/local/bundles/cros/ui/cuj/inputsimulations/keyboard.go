51b45db400058f49c20941eb9857bb549d958d30
Navin Ramsaroop <ramsaroop@google.com>
2022-06-22 19:12:03 +0000

    Add keyboard input to VideoCUJ
    Augment VideoCUJ to press and hold the up and down arrow with the video
    playing, thus changing the volume, to capture input latency with regards
    to holding down a specific key. Additionally, pause and play the video
    with the spacebar to capture general key-press input latency.
    
    BUG=b:236030517
    TEST=Run ui.VideoCUJ.clamshell and ui.VideoCUJ.lacros and verify
    that each results-chart.json has values for
    Event.Latency.EndToEnd.KeyPress.
    
    Change-Id: I915338b38b2c4c576c9c64a287c6c6f67a2338b4

--- src/chromiumos/tast/local/bundles/cros/ui/cuj/inputsimulations/keyboard.go
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/inputsimulations/keyboard.go
@@ -14,10 +14,34 @@ import (
 	"chromiumos/tast/local/input"
 )
 
-// RepeatKeyPressFor presses the specified key repeatedly, with a delay between key presses.
+// RepeatKeyPressFor presses the specified key repeatedly, with a |delay| between key presses.
 func RepeatKeyPressFor(ctx context.Context, kw *input.KeyboardEventWriter, key string, delay, duration time.Duration) error {
 	return uiauto.New(nil).WithInterval(delay).WithTimeout(duration+time.Minute).RetryUntil(
 		kw.AccelAction(key),
+		targetTime(time.Now().Add(duration)),
+	)(ctx)
+}
+
+// RepeatKeyPress presses the specified key |n| times, with a |delay| between key presses.
+func RepeatKeyPress(ctx context.Context, kw *input.KeyboardEventWriter, key string, delay time.Duration, n int) error {
+	return uiauto.Repeat(
+		n,
+		action.Combine("press "+key+" and sleep",
+			kw.AccelAction(key),
+			action.Sleep(delay),
+		),
+	)(ctx)
+}
+
+// RepeatKeyHoldFor holds a key down for |keyHoldDuration|,
+// with a |delay| between each key release and the next key press.
+func RepeatKeyHoldFor(ctx context.Context, kw *input.KeyboardEventWriter, key string, keyHoldDuration, delay, duration time.Duration) error {
+	return uiauto.New(nil).WithInterval(delay).WithTimeout(duration+time.Minute).RetryUntil(
+		action.Combine("press "+key+" and hold",
+			kw.AccelPressAction(key),
+			action.Sleep(keyHoldDuration),
+			kw.AccelReleaseAction(key),
+		),
 		targetTime(time.Now().Add(duration)),
 	)(ctx)
 }
