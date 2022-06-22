32539d453cb770fe7c3b0b9eaa806e178ea91eaf
Navin Ramsaroop <ramsaroop@google.com>
2022-06-22 19:12:03 +0000

    Add keyboard input latency to VideoCUJ
    Augment VideoCUJ to press and hold the up and down arrow with the video
    playing, to capture input latency with regards to holding down a
    specific key. Additionally, pause and play the video with the spacebar
    to capture general key-press input latency.
    
    BUG=b:236030517
    TEST=tast run <DUT> ui.VideoCUJ.clamshell
    
    Change-Id: I915338b38b2c4c576c9c64a287c6c6f67a2338b4

--- src/chromiumos/tast/local/bundles/cros/ui/cuj/inputsimulations/keyboard.go
+++ src/chromiumos/tast/local/bundles/cros/ui/cuj/inputsimulations/keyboard.go
@@ -14,12 +14,25 @@ import (
 	"chromiumos/tast/local/input"
 )
 
-// RepeatKeyPressFor presses the specified key repeatedly, with a delay between key presses.
+// RepeatKeyPressFor presses the specified key repeatedly, with a |delay| between key presses.
 func RepeatKeyPressFor(ctx context.Context, kw *input.KeyboardEventWriter, key string, delay, duration time.Duration) error {
 	return uiauto.New(nil).WithInterval(delay).WithTimeout(duration+time.Minute).RetryUntil(
 		kw.AccelAction(key),
 		targetTime(time.Now().Add(duration)),
 	)(ctx)
+}
+
+// RepeatKeyHoldFor repeatedly holds a key down for |keyHoldDuration|,
+// with a |delay| between key release the next key press.
+func RepeatKeyHoldFor(ctx context.Context, kw *input.KeyboardEventWriter, key string, keyHoldDuration, delay, duration time.Duration) error {
+	return uiauto.New(nil).WithInterval(delay).WithTimeout(duration+time.Minute).RetryUntil(
+		uiauto.Combine(
+			"press "+key+" and hold",
+			kw.AccelPressAction(key),
+			action.Sleep(keyHoldDuration),
+			kw.AccelReleaseAction(key),
+		),
+		targetTime(time.Now().Add(duration)))(ctx)
 }
 
 func targetTime(t time.Time) action.Action {
