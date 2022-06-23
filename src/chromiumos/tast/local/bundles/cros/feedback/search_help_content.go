3b261b7836b0fcc87a6d63bfb01c454c8aa46cc2
wenyu zhang <zhangwenyu@google.com>
2022-06-23 19:11:05 +0000

    Feedback: tast-test suggested help contents are updated
    Verify the suggested help contents are updated as user type
    issue descriptions.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.SearchHelpContent
    
    Change-Id: I0f0fde14df1116e770e06f24eaa31ab24748e144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3722616
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: David Welling <davidwelling@google.com>
    Tested-by: Wenyu Zhang <zhangwenyu@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/search_help_content.go
@@ -0,0 +1,138 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SearchHelpContent,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Suggested help content is updated as user enters issue description",
+		Contacts: []string{
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// SearchHelpContent verifies the suggested help content will be updated as user
+// enters issue description.
+func SearchHelpContent(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	s.Log("Set up chrome")
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("OsFeedback"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	s.Log("Set up test API")
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	ui := uiauto.New(tconn)
+
+	s.Log("Set up keyboard")
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	s.Log("Launch feedback app")
+	feedbackRootNode, err := feedbackapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app: ", err)
+	}
+
+	s.Log("Verify help content title")
+	title := nodewith.Name("Top help content").Role(role.StaticText).Ancestor(feedbackRootNode)
+	if err := ui.WaitUntilExists(title)(ctx); err != nil {
+		s.Fatal("Failed to find the help content title: ", err)
+	}
+
+	s.Log("Verify there are five default help links")
+	_, err = verifyLinks(ctx, tconn, 5)
+	if err != nil {
+		s.Fatal("Failed to find five help links: ", err)
+	}
+
+	s.Log("Find the issue description text input")
+	issueDescriptionInput := nodewith.Role(role.TextField).Ancestor(feedbackRootNode)
+	if err := uiauto.Combine("Focus text field",
+		ui.WaitUntilExists(issueDescriptionInput),
+		ui.EnsureFocused(issueDescriptionInput),
+	)(ctx); err != nil {
+		s.Fatal("Failed to find the issue description text input: ", err)
+	}
+
+	s.Log("Type issue description")
+	if err := kb.Type(ctx, "I am not able to connect to Bluetooth"); err != nil {
+		s.Fatal("Failed to type issue description: ", err)
+	}
+
+	s.Log("Verify help content title has changed")
+	updatedTitle := nodewith.Name("Suggested help content").Role(role.StaticText).Ancestor(
+		feedbackRootNode)
+	if err := ui.WaitUntilExists(updatedTitle)(ctx); err != nil {
+		s.Fatal("Failed to find the updated help content title: ", err)
+	}
+
+	s.Log("Verify there are five help content link")
+	updatedHelpLink, err := verifyLinks(ctx, tconn, 5)
+	if err != nil {
+		s.Fatal("Failed to find five help links: ", err)
+	}
+
+	s.Log("Verify the link can be opened")
+	if err := ui.LeftClick(updatedHelpLink.First())(ctx); err != nil {
+		s.Fatal("Failed to open help link: ", err)
+	}
+
+	if err = ash.WaitForApp(ctx, tconn, apps.Chrome.ID, time.Minute); err != nil {
+		s.Fatal("Could not find app in shelf after launch: ", err)
+	}
+}
+
+// verifyLinks function verifies there are n links existed.
+func verifyLinks(ctx context.Context, tconn *chrome.TestConn, n int) (
+	*nodewith.Finder, error) {
+	ui := uiauto.New(tconn)
+
+	link := nodewith.Role(role.Link).Ancestor(nodewith.Role(role.Iframe))
+	for i := 0; i < n; i++ {
+		item := link.Nth(i)
+		if err := ui.WithTimeout(20 * time.Second).WaitUntilExists(item)(ctx); err != nil {
+			return nil, errors.Wrap(err, "failed to find help links")
+		}
+	}
+	return link, nil
+}
---
+++ src/chromiumos/tast/local/chrome/uiauto/feedbackapp/feedbackapp.go
@@ -0,0 +1,39 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package feedbackapp contains drivers for controlling the ui of feedback SWA.
+package feedbackapp
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+)
+
+// Launch starts the Feedback app via the default method.
+func Launch(ctx context.Context, tconn *chrome.TestConn) (*nodewith.Finder, error) {
+	if err := apps.Launch(ctx, tconn, apps.Feedback.ID); err != nil {
+		return nil, errors.Wrap(err, "failed to launch feedback app")
+	}
+
+	if err := ash.WaitForApp(ctx, tconn, apps.Feedback.ID, time.Minute); err != nil {
+		return nil, errors.Wrap(err, "feedback app did not appear in shelf after launch")
+	}
+
+	ui := uiauto.New(tconn)
+
+	feedbackRootNode := nodewith.Name(apps.Feedback.Name).Role(role.Window)
+	if err := ui.WithTimeout(20 * time.Second).WaitUntilExists(feedbackRootNode)(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to find feedback app")
+	}
+
+	return feedbackRootNode, nil
+}
