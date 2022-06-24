231bfbbc82ffcd6c7e84cc7808672e6b394bb40f
Jacek Siuda <jck@semihalf.com>
2022-06-24 16:35:41 +0000

    wifi: Improve TDLS Discovery test
    Improve TDLS Discovery test by checking if packets follow expected route
    and are of correct format.
    
    During brief test we found one device that did not receive TDLS Teardown
    from the previous test case (bad PS) and was sending its own
    TDLSDiscovery via tunnel.
    This improvement should catch such cases automatically.
    
    BUG=b:225176836
    TEST=tast run -companiondut=cd1:$PEER_HOST $DUT wifi.TDLS
    TEST=tast unit test passed
    
    Change-Id: I40dbf4b71b9e9e56e47ad756d45593c83800d39e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3721052
    Reviewed-by: Billy Zhao <billyzhao@chromium.org>
    Tested-by: Jacek Siuda <jsiuda@google.com>
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Commit-Queue: Jacek Siuda <jsiuda@google.com>

--- src/chromiumos/tast/remote/bundles/cros/wifi/wifiutil/tdls_util.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/wifiutil/tdls_util.go
@@ -85,6 +85,14 @@ func filterTrnsRecv(dot11 *layers.Dot11, src, dst net.HardwareAddr) bool {
 	return false
 }
 
+// filterTDLSTunnelAddr passes packets that are P2P and Receiver address equals src and Transmitter equals dst.
+func filterTDLSTunnelAddr(dot11 *layers.Dot11, src, dst net.HardwareAddr) bool {
+	if dot11.Flags.FromDS() || dot11.Flags.ToDS() {
+		return false
+	}
+	return macEqual(dot11.Address1, dst) && macEqual(dot11.Address2, src)
+}
+
 // FindNonTDLSPackets finds ICMP packets that were not sent through TDLS.
 func FindNonTDLSPackets(pcapPath string, addrs []net.HardwareAddr) ([]gopacket.Packet, error) {
 	if len(addrs) != 2 {
@@ -108,6 +116,90 @@ func FindNonTDLSPackets(pcapPath string, addrs []net.HardwareAddr) ([]gopacket.P
 		),
 	}
 	return pcap.ReadPackets(pcapPath, filters...)
+}
+
+// FindAnomalousTDLSDiscoveryPackets finds TDLS Discovery packets that deviate from standard.
+func FindAnomalousTDLSDiscoveryPackets(pcapPath string, addrs []net.HardwareAddr) ([]gopacket.Packet, error) {
+	if len(addrs) != 2 {
+		return nil, errors.New("Needs exactly two addresses")
+	}
+	// Discovery request.
+	filters := []pcap.Filter{
+		pcap.RejectLowSignal(),
+		pcap.Dot11FCSValid(),
+		pcap.TypeFilter(
+			layers.LayerTypeSNAP,
+			func(layer gopacket.Layer) bool {
+				snap := layer.(*layers.SNAP)
+				if snap.Type != 0x890D {
+					return false
+				}
+				if len(snap.Payload) < 3 {
+					return true
+				}
+				PayloadType := snap.Payload[0]
+				CategoryCode := snap.Payload[1]
+				ActionCode := snap.Payload[2]
+				const (
+					PayloadTypeTDLS                = 2
+					CategoryCodeTDLS               = 12
+					ActionCodeTDLSDiscoveryRequest = 10
+				)
+				if PayloadType != PayloadTypeTDLS || CategoryCode != CategoryCodeTDLS || ActionCode != ActionCodeTDLSDiscoveryRequest {
+					return false
+				}
+
+				return true
+			},
+		),
+		pcap.TypeFilter(
+			layers.LayerTypeDot11,
+			func(layer gopacket.Layer) bool {
+				dot11 := layer.(*layers.Dot11)
+				// We want to see TDLS Discovery Request sent in tunnel
+				return filterTDLSTunnelAddr(dot11, addrs[0], addrs[1])
+			},
+		),
+	}
+	reqPackets, err := pcap.ReadPackets(pcapPath, filters...)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read packets")
+	}
+
+	// Discovery response. It is based on an entirely different layer,
+	// thus we need to re-read the entire file with a different filter set.
+	filters = []pcap.Filter{
+		pcap.RejectLowSignal(),
+		pcap.Dot11FCSValid(),
+		pcap.TypeFilter(
+			layers.LayerTypeDot11,
+			func(layer gopacket.Layer) bool {
+				dot11 := layer.(*layers.Dot11)
+
+				if dot11.Type != layers.Dot11TypeMgmtAction {
+					return false
+				}
+				CategoryCode := dot11.Payload[0]
+				ActionCode := dot11.Payload[1]
+				const (
+					CategoryCodePublicAction        = 4
+					ActionCodeTDLSDiscoveryResponse = 14
+				)
+				if CategoryCode != CategoryCodePublicAction || ActionCode != ActionCodeTDLSDiscoveryResponse {
+					return false
+				}
+
+				// We want to see TDLS Discovery Response _not_ sent directly.
+				return !filterTDLSTunnelAddr(dot11, addrs[1], addrs[0])
+			},
+		),
+	}
+	resPackets, err := pcap.ReadPackets(pcapPath, filters...)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read packets")
+	}
+
+	return append(reqPackets, resPackets...), err
 }
 
 // DumpPkts dissects slice of packets into a string.
