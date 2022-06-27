476c57fdf539d3a3482bc78bf0d7d4d614128e28
tinghaolin <tinghaolin@google.com>
2022-06-27 03:00:12 +0000

    network: Add the tcpdump utility to the local test environment
    This modification adds the tcpdump utility in the local test
    environment. It extracts the logic related to the tcpdump
    command from the cpature.go file so that both local and remote
    tests can use the tcpdump utility.
    
    BUG=b:237283936
    TEST=Run wifi_matfunc to check that all tests pass and
    tast run ${DUT} network.ShillCaptivePortalDNS to check
    tcpdump utility can be used in local test environment.
    
    Change-Id: I21a50338b9a59bbf0f8611da12000cbab4d8ca68

--- src/chromiumos/tast/remote/network/cmd/runner.go
+++ src/chromiumos/tast/remote/network/cmd/runner.go
@@ -7,8 +7,11 @@ package cmd
 
 import (
 	"context"
+	"io"
+	"os"
 
 	"chromiumos/tast/common/network/cmd"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/ssh"
 )
 
@@ -18,6 +21,7 @@ const logName = "cmdOutput.txt"
 type RemoteCmdRunner struct {
 	Host         *ssh.Conn
 	NoLogOnError bool // Default false: dump log on error.
+	command      *ssh.Cmd
 }
 
 var _ cmd.Runner = (*RemoteCmdRunner)(nil)
@@ -35,4 +39,53 @@ func (r *RemoteCmdRunner) Output(ctx context.Context, cmd string, args ...string
 		return cc.Output()
 	}
 	return cc.Output(ssh.DumpLogOnError)
+}
+
+// CreateCmd creates a command.
+func (r *RemoteCmdRunner) CreateCmd(ctx context.Context, cmd string, args ...string) {
+	r.command = r.Host.CommandContext(ctx, cmd, args...)
+}
+
+// SetStdOut sets the standard output of existed command.
+func (r *RemoteCmdRunner) SetStdOut(stdoutFile *os.File) {
+	r.command.Stdout = stdoutFile
+}
+
+// StderrPipe sets standard error pipe of existed command.
+func (r *RemoteCmdRunner) StderrPipe() (io.ReadCloser, error) {
+	return r.command.StderrPipe()
+}
+
+// StartCmd starts a command that is created by r.Create().
+func (r *RemoteCmdRunner) StartCmd() error {
+	if !r.CmdExists() {
+		return errors.New("there is no command object to start")
+	}
+	return r.command.Start()
+}
+
+// WaitCmd waits a command that is created by r.Create().
+func (r *RemoteCmdRunner) WaitCmd() error {
+	if !r.CmdExists() {
+		return errors.New("there is no command object to wait")
+	}
+	r.command.Wait()
+	return nil
+}
+
+// CmdExists check if the command is created by r.Create().
+func (r *RemoteCmdRunner) CmdExists() bool {
+	return r.command != nil
+}
+
+// ReleaseProcess detaches the command process from the parent process and keeps it alive
+// after the parent process dies.
+// The function only works for local test environment.
+func (r *RemoteCmdRunner) ReleaseProcess() error {
+	return nil
+}
+
+// ResetCmd will set the command to null.
+func (r *RemoteCmdRunner) ResetCmd() {
+	r.command = nil
 }
