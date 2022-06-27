30795fe9a2cd49609e7793ca500061d3e484d2cb
Robert Zieba <robertzieba@google.com>
2022-06-27 13:25:39 -0600

    tast/remote/firmware: Add common suspend functionality
    TAST currently doesn't have common code to suspend/wake a DUT. This
    commit adds an initial version of common suspend code.
    
    BUG=b:221240975
    TEST=Verified that tast builds
    
    Change-Id: Ie05cec8e98cba2805428246c991c2d7f4a2714f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3733248
    Tested-by: Robert Zieba <robertzieba@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Robert Zieba <robertzieba@google.com>
    Commit-Queue: Robert Zieba <robertzieba@google.com>

---
+++ src/chromiumos/tast/remote/firmware/suspend/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package suspend implements remote libraries for suspend testing.
+package suspend
