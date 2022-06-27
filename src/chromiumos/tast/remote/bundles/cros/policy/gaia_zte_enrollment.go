adc2207984361ceea5044a69a276d2028aaa9ab9
Ruben Zakarian <rzakarian@google.com>
2022-06-27 12:33:05 -0700

    Policy: Add integration test.
    Add integration test for ZTE enrollment.
    
    BUG=b:230622059
    TEST=ran test on local and lab machines
    
    Change-Id: I4cd439a0f7fc9dfbcb48430abacbc778efd4f0a7

---
+++ src/chromiumos/tast/remote/bundles/cros/policy/gaia_zte_enrollment.go
@@ -0,0 +1,88 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/remote/policyutil"
+	"chromiumos/tast/rpc"
+	ps "chromiumos/tast/services/cros/policy"
+	"chromiumos/tast/testing"
+)
+
+type testInformation struct {
+	username string // username for Chrome login
+	password string // password to login
+	dmserver string // device management server url
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GAIAZTEEnrollment,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "GAIA Enroll a device without checking policies",
+		Contacts: []string{
+			"rzakarian@google.com", // Test author
+			"chromeos-commercial-remote-management@google.com",
+		},
+		Attr:         []string{"group:dpanel-end2end"},
+		SoftwareDeps: []string{"reboot", "chrome"},
+		ServiceDeps:  []string{"tast.cros.policy.PolicyService"},
+		Timeout:      7 * time.Minute,
+		Params: []testing.Param{
+			{
+				Name: "autopush",
+				Val: testInformation{
+					username: "policy.GAIAZTEEnrollment.user_name",
+					password: "policy.GAIAZTEEnrollment.password",
+					dmserver: "https://crosman-alpha.sandbox.google.com/devicemanagement/data/api",
+				},
+			},
+		},
+		Vars: []string{
+			"policy.GAIAZTEEnrollment.user_name",
+			"policy.GAIAZTEEnrollment.password",
+		},
+	})
+}
+
+func GAIAZTEEnrollment(ctx context.Context, s *testing.State) {
+	param := s.Param().(testInformation)
+	username := s.RequiredVar(param.username)
+	password := s.RequiredVar(param.password)
+	dmServerURL := param.dmserver
+
+	defer func(ctx context.Context) {
+		if err := policyutil.EnsureTPMAndSystemStateAreResetRemote(ctx, s.DUT()); err != nil {
+			s.Error("Failed to reset TPM after test: ", err)
+		}
+	}(ctx)
+
+	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Minute)
+	defer cancel()
+
+	if err := policyutil.EnsureTPMAndSystemStateAreResetRemote(ctx, s.DUT()); err != nil {
+		s.Fatal("Failed to reset TPM: ", err)
+	}
+
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	pc := ps.NewPolicyServiceClient(cl.Conn)
+
+	if _, err := pc.GAIAEnrollUsingChrome(ctx, &ps.GAIAEnrollUsingChromeRequest{
+		Username:    username,
+		Password:    password,
+		DmserverURL: dmServerURL,
+	}); err != nil {
+		s.Fatal("Failed to enroll using chrome: ", err)
+	}
+}
