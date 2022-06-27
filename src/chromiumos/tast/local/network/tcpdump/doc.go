51f9d6d426cc3d02df1f5866f0758bbf81fb20f3
tinghaolin <tinghaolin@google.com>
2022-06-27 03:00:12 +0000

    network: Add the tcpdump utility to the local test environment
    This modification adds the tcpdump utility in the local test
    environment. It extracts the logic related to the tcpdump
    command from the cpature.go file so that both local and remote
    tests can use the tcpdump utility.
    
    BUG=b:237283936
    TEST=Run wifi_matfunc to check that all tests pass and
    tast run ${DUT} network.ShillCaptivePortalDNS to check
    tcpdump utility can be used in local test environment and
    run fast_build.sh to rebuild the tast executable.
    
    Change-Id: I21a50338b9a59bbf0f8611da12000cbab4d8ca68

---
+++ src/chromiumos/tast/local/network/tcpdump/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package tcpdump provides utilities for starting and closing the tcpdump command
+// to capture and analyze network traffic through the system.
+package tcpdump
