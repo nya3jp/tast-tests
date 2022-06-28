63cad8ec6458c0b90ecd2eb58aa81ccaed192796
Jan Lanik <janlanik@google.com>
2022-06-28 08:12:15 +0000

    [Privacy Hub] Tast for privacy hub existence
    Tests the privacy hub settings subpage via the Tast framework.
    Verifies the existence of the Privacy Hub subpage.
    Verifies that the Camera Button is present in the settings page.
    
    BUG=b:234608109
    TEST=Manually verified the test's function together with the chrome
    patch that introduces the Privacy Hub section into the settings menu (
    https://chromium-review.googlesource.com/c/chromium/src/+/3669894/6 )
    
    Change-Id: I263b4b2579807fd7595cf8f55419a71ee449267c

---
+++ src/chromiumos/tast/local/bundles/cros/privacyhub/settings_page.go
@@ -0,0 +1,115 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package privacyhub contains tests for privacy hub.
+package privacyhub
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SettingsPage,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks that PrivacyHub settings page exists and that it contains the expected elements",
+		Contacts:     []string{"janlanik@google.com", "privacy-hub@google.com"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      5 * time.Minute,
+		Attr:         []string{"group:mainline", "informational"},
+		Params: []testing.Param{
+			{
+				Name: "feature_on",
+				Val:  true,
+			},
+			{
+				Name: "feature_off",
+				Val:  false,
+			},
+		},
+	})
+}
+
+func checkAllToggles(cleanupCtx, ctx *context.Context, tconn *chrome.TestConn, cr *chrome.Chrome, timeout time.Duration) error {
+	checkToggle := func(name string) error {
+		cameraLabel := nodewith.Name(name).Role(role.ToggleButton)
+		settings, err := ossettings.LaunchAtPageURL(*ctx, tconn, cr, "osPrivacy/privacyHub", uiauto.New(tconn).WithTimeout(timeout).WaitUntilExists(cameraLabel))
+		if err != nil {
+			return errors.Wrapf(err, "failed to find %s in Privacy Hub page", name)
+		}
+		defer settings.Close(*cleanupCtx)
+		return nil
+	}
+
+	if err := checkToggle("Camera"); nil != err {
+		return err
+	}
+	if err := checkToggle("Microphone"); nil != err {
+		return err
+	}
+
+	return nil
+}
+
+func SettingsPage(ctx context.Context, s *testing.State) {
+	// Shorten deadline to leave time for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	featureOn := s.Param().(bool)
+
+	var cr *chrome.Chrome
+	var err error
+	if featureOn {
+		cr, err = chrome.New(ctx, chrome.EnableFeatures("CrosPrivacyHub"))
+	} else {
+		cr, err = chrome.New(ctx)
+	}
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create test API connection: ", err)
+	}
+
+	settings, err := ossettings.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch OS settings: ", err)
+	}
+	defer settings.Close(cleanupCtx)
+
+	privacyMenu := nodewith.Name("Privacy Hub")
+	timeout := time.Second * 5
+	if featureOn {
+		// Check that the Privacy Hub section exists.
+		if err := uiauto.New(tconn).WithTimeout(timeout).WaitUntilExists(privacyMenu)(ctx); err != nil {
+			s.Fatal("Failed to find Privacy Hub in OS setting page: ", err)
+		}
+		// Check that the Privacy Hub section contains a button for camera.
+		if err = checkAllToggles(&cleanupCtx, &ctx, tconn, cr, timeout); err != nil {
+			s.Fatal("Failed to verify all PrivacyHub menu toggles: ", err)
+		}
+	} else {
+		// Check that the Privacy Hub section does not exist if feature flag is not explicitly set.
+		// This will be removed when PrivacyHub is in production.
+		if err := uiauto.New(tconn).WithTimeout(timeout).WaitUntilExists(privacyMenu)(ctx); err == nil {
+			s.Fatal("Found Privacy Hub in OS setting page even though the flag is not set: ", err)
+		}
+
+	}
+}
