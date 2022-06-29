df6b06bbd2ecff49127a8685ac2824b65f065801
Ben Reich <benreich@chromium.org>
2022-06-29 18:32:51 +1000

    tast-tests: Migrate tests to use cryptohome.{Downloads,MyFiles}Path
    The usage of the /home/chronos/user/Downloads bind mount is being
    deprecated. This moves a chunk of tests away from using the
    filesapp.DownloadPath and filesapp.MyFilesPath and to directly
    retrieving the mount via cryptohome. The tests affected in this patch
    are:
    
      - ui.DMICRecord*
      - network.CertSettingsPage*
      - scanapp.OpenScanInFilesApp*
      - scanapp.MultiPageScan*
      - typec.HotplugUSBHeadsetAudioPlayback*
    
    This also updates the scanapp methods that run app settings tests to
    use cryptohome method MyFilesPath as well.
    
    BUG=b:233132576
    TEST=tast run all tests listed above
    TEST=./fast_build.sh -T
    
    Change-Id: Iec8c56fe4997a6ddc216cf3d79fbd1d8499f5e21

--- src/chromiumos/tast/local/bundles/cros/typec/hotplug_usb_headset_audio_playback.go
+++ src/chromiumos/tast/local/bundles/cros/typec/hotplug_usb_headset_audio_playback.go
@@ -26,6 +26,7 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/filesapp"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/cryptohome"
 	"chromiumos/tast/local/cswitch"
 	"chromiumos/tast/testing"
 )
@@ -116,7 +117,11 @@ func HotplugUSBHeadsetAudioPlayback(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to detect USB headset after plug: ", err)
 	}
 
-	if err := audioPlay(ctx, tconn); err != nil {
+	downloadsPath, err := cryptohome.DownloadsPath(ctx, cr.NormalizedUser())
+	if err != nil {
+		s.Fatal("Failed to get user's Download path: ", err)
+	}
+	if err := audioPlay(ctx, tconn, downloadsPath); err != nil {
 		s.Fatal("Failed to play audio file: ", err)
 	}
 
@@ -169,7 +174,7 @@ func HotplugUSBHeadsetAudioPlayback(ctx context.Context, s *testing.State) {
 
 // audioPlay generates wav audio file, play using default player
 // and verify audio routing through BT device.
-func audioPlay(ctx context.Context, tconn *chrome.TestConn) error {
+func audioPlay(ctx context.Context, tconn *chrome.TestConn, downloadsPath string) error {
 	cras, err := audio.NewCras(ctx)
 	if err != nil {
 		return errors.Wrap(err, "failed to create Cras object")
@@ -198,7 +203,7 @@ func audioPlay(ctx context.Context, tconn *chrome.TestConn) error {
 
 	// Generate sine raw input file that lasts 60 seconds.
 	rawFileName := "AudioFile.raw"
-	rawFilePath := filepath.Join(filesapp.DownloadPath, rawFileName)
+	rawFilePath := filepath.Join(downloadsPath, rawFileName)
 	rawFile := audio.TestRawData{
 		Path:          rawFilePath,
 		BitsPerSample: 16,
@@ -214,7 +219,7 @@ func audioPlay(ctx context.Context, tconn *chrome.TestConn) error {
 	defer os.Remove(rawFile.Path)
 
 	wavFileName := "AudioFile.wav"
-	wavFile := filepath.Join(filesapp.DownloadPath, wavFileName)
+	wavFile := filepath.Join(downloadsPath, wavFileName)
 	if err := audio.ConvertRawToWav(ctx, rawFilePath, wavFile, 48000, 2); err != nil {
 		return errors.Wrap(err, "failed to convert raw to wav")
 	}
