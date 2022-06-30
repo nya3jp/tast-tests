4c03e53c9ca754715722fab3c5da86b69ce2dcb2
Angel <angelsan@google.com>
2022-06-30 22:57:04 +0000

    tast-test: Check if saving a session log file from Diagnostics app appears in holding space.
    BUG=b:200987266
    TEST=tast run <test-device-ip> holdingspace.DiagnosticsAppSaveSessionLog
    
    Change-Id: I169266594cb4e0787bf855714021a7b424f3333d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3739600
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Commit-Queue: Angel Sanchez <angelsan@google.com>
    Auto-Submit: Angel Sanchez <angelsan@google.com>
    Tested-by: Angel Sanchez <angelsan@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/holdingspace/diagnostics_app_save_session_log.go
@@ -0,0 +1,88 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package holdingspace
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/diagnosticsapp"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/holdingspace"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DiagnosticsAppSaveSessionLog,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks that session logs saved from Diagnostics App appear in Holding Space",
+		Contacts: []string{
+			"angelsan@chromium.org",
+			"dmblack@chromium.org",
+			"chromeos-sw-engprod@google.com",
+			"cros-system-ui-eng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "chromeLoggedIn",
+	})
+}
+
+// DiagnosticsAppSaveSessionLog tests the functionality of files existing in Holding Space by
+// saving a session log from the Diagnostics app.
+func DiagnosticsAppSaveSessionLog(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_dump")
+
+	// Reset the holding space.
+	if err := holdingspace.ResetHoldingSpace(ctx, tconn,
+		holdingspace.ResetHoldingSpaceOptions{}); err != nil {
+		s.Fatal("Failed to reset holding space: ", err)
+	}
+
+	// Ensure session log does not exist.
+	downloadsPath, err := cryptohome.DownloadsPath(ctx, cr.NormalizedUser())
+	if err != nil {
+		s.Fatal("Failed to get users Download path: ", err)
+	}
+	const filename = "session_log.txt"
+	filePath := filepath.Join(downloadsPath, filename)
+	if _, err := os.Stat(filePath); err == nil {
+		os.Remove(filePath)
+	}
+	defer os.Remove(filePath)
+
+	dxRootnode, err := diagnosticsapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Could not open diagnostics app: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+	uiauto.Combine("Save session log and verify file appears in holding space",
+		ui.LeftClick(diagnosticsapp.DxLogButton.Ancestor(dxRootnode)),
+		ui.LeftClick(nodewith.Name("Save").Role(role.Button)),
+		ui.LeftClick(holdingspace.FindTray()),
+		ui.WaitUntilExists(holdingspace.FindDownloadChip().Name(filename)),
+	)(ctx)
+}
