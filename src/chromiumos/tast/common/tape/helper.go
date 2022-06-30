480301f75231ef81fa7f803f4758d2c7f7d05aad
alexanderhartl <alexanderhartl@google.com>
2022-06-30 16:12:51 +0000

    tape: refactor tape package
    Adding helper functions and pool ids to tape.
    
    BUG=None
    TEST=ran arc.DataMigration, arc.EnterpriseLogin, enterprise.ArcSnapshot,
    enterprise.ArcInstallLogging and arcprefgame.Roblox* tests
    
    Cq-Depend: chromium:3738119, chromium:3738123, chrome-internal:4846842
    Change-Id: I28303c8a0a3323aa405bc560f94726e78686c1ae

---
+++ src/chromiumos/tast/common/tape/helper.go
@@ -0,0 +1,177 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package tape
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+)
+
+// ServiceAccountVar holds the name of the variable which stores the service account credentials for TAPE.
+const ServiceAccountVar = "tape.service_account_key"
+
+type clientOption struct {
+	client      *client
+	credsJSON   []byte
+	requestOpts []RequestAccountOption
+}
+
+// ClientOption provides options for getting a client for an account manager.
+type ClientOption func(*clientOption)
+
+// WithCredsJSON provides the option to get a client from service account credentials.
+func WithCredsJSON(jsonData []byte) ClientOption {
+	return func(opt *clientOption) {
+		opt.credsJSON = jsonData
+	}
+}
+
+// WithClient provides the option to use an existing client.
+func WithClient(client *client) ClientOption {
+	return func(opt *clientOption) {
+		opt.client = client
+	}
+}
+
+func getClient(ctx context.Context, opts ...ClientOption) (*client, error) {
+	// Copy over all options.
+	options := clientOption{}
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	var client *client
+	var err error
+	if options.client != nil {
+		client = options.client
+	} else if len(options.credsJSON) > 0 {
+		client, err = NewClient(ctx, options.credsJSON)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to create tape client")
+		}
+	} else {
+		return nil, errors.New("One of tape.client or credsJSON must be set")
+	}
+	return client, nil
+}
+
+type genericAccountManager struct {
+	client   *client
+	Accounts []*GenericAccount
+}
+
+// NewGenericAccountManager leases a generic account, stores it in a genericAccountManager struct and returns both. It requires
+// a credsJSON byte array with the credentials of a service account to create a tape client for the genericAccountManager.
+func NewGenericAccountManager(ctx context.Context, credsJSON []byte, opts ...RequestAccountOption) (*genericAccountManager, *GenericAccount, error) {
+	client, err := NewClient(ctx, credsJSON)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to create tape client")
+	}
+
+	return NewGenericAccountManagerFromClient(ctx, client, opts...)
+}
+
+// NewGenericAccountManagerFromClient leases a generic account, stores it in a genericAccountManager struct and returns both. It requires
+// a tape client to assign it to the genericAccountManager.
+func NewGenericAccountManagerFromClient(ctx context.Context, client *client, opts ...RequestAccountOption) (*genericAccountManager, *GenericAccount, error) {
+	manager := &genericAccountManager{
+		client: client,
+	}
+
+	account, err := manager.RequestAccount(ctx, opts...)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to request account")
+	}
+	return manager, account, nil
+}
+
+// RequestAccount leases a generic account, stores it in the genericAccountManager and returns it.
+func (ah *genericAccountManager) RequestAccount(ctx context.Context, opts ...RequestAccountOption) (*GenericAccount, error) {
+	account, err := ah.client.RequestGenericAccount(ctx, opts...)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to request owned test account")
+	}
+
+	ah.Accounts = append(ah.Accounts, account)
+
+	return account, nil
+}
+
+// CleanUp releases all generic accounts that are stored in the genericAccountManager.
+func (ah *genericAccountManager) CleanUp(ctx context.Context) error {
+	var combinedErrors error
+	for _, account := range ah.Accounts {
+		err := ah.client.ReleaseGenericAccount(ctx, account)
+		if err != nil {
+			combinedErrors = errors.Wrap(combinedErrors, err.Error())
+		}
+	}
+
+	if combinedErrors != nil {
+		return errors.Wrap(combinedErrors, "failed to release some accounts")
+	}
+
+	return nil
+}
+
+type ownedTestAccountManager struct {
+	client   *client
+	Accounts []*OwnedTestAccount
+}
+
+// NewOwnedTestAccountManager leases an owned test account, stores it in an ownedTestAccountManager struct and returns both. It requires
+// a credsJSON byte array with the credentials of a service account to create a tape client for the ownedTestAccountManager.
+func NewOwnedTestAccountManager(ctx context.Context, credsJSON []byte, lock bool, opts ...RequestAccountOption) (*ownedTestAccountManager, *OwnedTestAccount, error) {
+	client, err := NewClient(ctx, credsJSON)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to create tape client")
+	}
+
+	return NewOwnedTestAccountManagerFromClient(ctx, client, lock, opts...)
+}
+
+// NewOwnedTestAccountManagerFromClient leases an owned test account, stores it in an ownedTestAccountManager struct and returns both.
+// It requires a tape client to assign it to the ownedTestAccountManager.
+func NewOwnedTestAccountManagerFromClient(ctx context.Context, client *client, lock bool, opts ...RequestAccountOption) (*ownedTestAccountManager, *OwnedTestAccount, error) {
+	manager := &ownedTestAccountManager{
+		client: client,
+	}
+
+	account, err := manager.RequestAccount(ctx, lock, opts...)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to request account")
+	}
+	return manager, account, nil
+}
+
+// RequestAccount leases an owned test account, stores it in the ownedTestAccountManager and returns it.
+func (ah *ownedTestAccountManager) RequestAccount(ctx context.Context, lock bool, opts ...RequestAccountOption) (*OwnedTestAccount, error) {
+	account, err := ah.client.RequestOwnedTestAccount(ctx, lock, opts...)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to request owned test account")
+	}
+
+	ah.Accounts = append(ah.Accounts, account)
+
+	return account, nil
+}
+
+// CleanUp releases all owned test accounts that are stored in the ownedTestAccountManager.
+func (ah *ownedTestAccountManager) CleanUp(ctx context.Context) error {
+	var combinedErrors error
+	for _, account := range ah.Accounts {
+		err := ah.client.ReleaseOwnedTestAccount(ctx, account)
+		if err != nil {
+			combinedErrors = errors.Wrap(combinedErrors, err.Error())
+		}
+	}
+
+	if combinedErrors != nil {
+		return errors.Wrap(combinedErrors, "failed to release some accounts")
+	}
+
+	return nil
+}
