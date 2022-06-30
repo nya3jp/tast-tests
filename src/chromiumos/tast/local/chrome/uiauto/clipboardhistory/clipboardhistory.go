095b0c209bb7107eb732c364a81d8f87544a8f36
Colin Kincaid <ckincaid@chromium.org>
2022-06-30 22:29:51 +0000

    multipaste: Add test for pasting items from clipboard history
    This test exercises basic clipboard history functionality, pasting text
    using the three straightforward paste actions. Follow-up CLs will add
    testing for other content types and the virtual keyboard paste action.
    
    BUG=b:200987157
    TEST=tast -verbose run <DUT> clipboardhistory.Paste.*
    
    Change-Id: I799039213c42dae995e5c46bcb2139086bd09aab

---
+++ src/chromiumos/tast/local/chrome/uiauto/clipboardhistory/clipboardhistory.go
@@ -0,0 +1,17 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package clipboardhistory
+
+import (
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+)
+
+const clipboardHistoryTextItemViewClassName = "ClipboardHistoryTextItemView"
+
+// FindFirstTextItem returns a finder which locates the first text item in the
+// clipboard history menu.
+func FindFirstTextItem() *nodewith.Finder {
+	return nodewith.ClassName(clipboardHistoryTextItemViewClassName).First()
+}
