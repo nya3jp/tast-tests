5a8ec8a4947f0492bac89221009df1bb6005f447
Paramjit Oberoi <psoberoi@google.com>
2022-06-30 21:05:24 -0700

    Test that kernel crash reports contain hypervisor log on ManaTEE
    BUG=b:224887095
    TEST=ran tast test
    
    Cq-Depend: chromium:3739610
    Change-Id: I6ccb545d5231598375531369bc789fd1c06e3ebb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3739844
    Tested-by: Paramjit Oberoi <psoberoi@google.com>
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Commit-Queue: Paramjit Oberoi <psoberoi@google.com>

---
+++ src/chromiumos/tast/remote/hypervisor/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package hypervisor contains support code for writing hypervisor tests.
+package hypervisor
