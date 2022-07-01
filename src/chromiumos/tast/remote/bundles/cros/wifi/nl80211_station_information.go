3cb1c9476e0baac9253ae1bc64e3736a27148499
Kaidong Wang <kaidong@google.com>
2022-07-01 01:31:33 +0000

    tast-tests: WiFi: Verify the support for nl80211 station information
    Most modern WiFi chips support nl80211 stack, but they may not support
    all the attributes defined in nl80211_sta_info. For example, MVL8897
    doesn’t support attributes such as NL80211_STA_INFO_TX_RETRIES,
    NL80211_STA_INFO_TX_FAILED, NL80211_STA_INFO_RX_DROP_MISC.
    
    This test checks the support for nl80211 station information which is
    required by WiFi link statistics feature, and if these attributes have
    reasonable values. The design doc is available at
    go/shill-wifi-nl80211-test
    
    BUG=b:235532246
    TEST=tast run -var=router=localhost:2210 -var=pcap=localhost:2220
    localhost:2200  wifi.Nl80211StationInformation
    
    Change-Id: I1a92523fb34406fe84715da1bfea1bff14964cd5

---
+++ src/chromiumos/tast/remote/bundles/cros/wifi/nl80211_station_information.go
@@ -0,0 +1,191 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifi
+
+import (
+	"context"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/network/iw"
+	"chromiumos/tast/remote/wificell"
+	"chromiumos/tast/remote/wificell/hostapd"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+type nl80211Attribute struct {
+	name string
+	// lowerBound and upperBound are inclusive
+	lowerBound int64
+	upperBound int64
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: Nl80211StationInformation,
+		Desc: "Verify the support for nl80211 station information",
+		Contacts: []string{
+			"chromeos-wifi-champs@google.com", // WiFi oncall rotation; or http://b/new?component=893827
+			"kaidong@google.com",
+		},
+		Attr:        []string{"group:wificell", "wificell_func", "wificell_unstable"},
+		ServiceDeps: []string{wificell.TFServiceName},
+		// Marvell chips don't support all the attributes
+		HardwareDeps: hwdep.D(hwdep.WifiNotMarvell()),
+		Fixture:      "wificellFixt",
+		Timeout:      2 * time.Minute,
+	})
+}
+
+/*
+nl80211 is the 802.11 netlink interface public header. It provides station
+information of an interface, such as "rx bytes", "tx bytes", "rx packets", "tx
+packets".
+
+This test uses the iw tool to get the nl80211 station information of the WiFi
+interface, and checks if the DUT supports all nl80211 attributes required by the
+WiFi link statistics feature.
+
+Attribute in nl80211_sta_info           Label in the iw output      Data type
+NL80211_STA_INFO_RX_BYTES               rx bytes                    u32
+NL80211_STA_INFO_RX_PACKETS             rx packets                  u32
+NL80211_STA_INFO_TX_BYTES               tx bytes                    u32
+NL80211_STA_INFO_TX_PACKETS             tx packets                  u32
+NL80211_STA_INFO_TX_RETRIES             tx retries                  u32
+NL80211_STA_INFO_TX_FAILED              tx failed                   u32
+NL80211_STA_INFO_RX_DROP_MISC           rx drop misc                u64
+NL80211_STA_INFO_BEACON_SIGNAL_AVG      beacon signal avg           u8
+NL80211_STA_INFO_SIGNAL_AVG             signal avg                  u8
+NL80211_STA_INFO_SIGNAL                 signal                      u8
+*/
+
+const (
+	nl80211RxBytes         = "rx bytes"
+	nl80211TxBytes         = "tx bytes"
+	nl80211RxPackets       = "rx packets"
+	nl80211TxPackets       = "tx packets"
+	nl80211RxDrop          = "rx drop misc"
+	nl80211TxRetries       = "tx retries"
+	nl80211TxFailed        = "tx failed"
+	nl80211BeaconSignalAvg = "beacon signal avg"
+	nl80211SignalAvg       = "signal avg"
+	nl80211Signal          = "signal"
+	// The difference between NL80211_STA_INFO_SIGNAL_AVG,
+	// NL80211_STA_INFO_SIGNAL and NL80211_STA_INFO_BEACON_SIGNAL_AVG. This
+	// value is subject to changes based on test results.
+	nl80211SignalError      = 10
+	nl80211SignalLowerBound = -100
+	nl80211SignalUpperBound = -25
+)
+
+func Nl80211StationInformation(ctx context.Context, s *testing.State) {
+
+	// nl80211 station information attributes required by the WiFi link
+	// statistics feature.
+	// Some devices support NL80211_STA_INFO_RX_BYTES64 and
+	// NL80211_STA_INFO_TX_BYTES64, then "rx bytes" and "tx bytes" are
+	// represented by u64.
+	nl80211Attributes := []nl80211Attribute{
+		{nl80211RxBytes, 1, math.MaxInt64},
+		{nl80211TxBytes, 1, math.MaxInt64},
+		{nl80211RxPackets, 1, math.MaxUint32},
+		{nl80211TxPackets, 1, math.MaxUint32},
+		{nl80211RxDrop, 0, math.MaxInt64},
+		{nl80211TxRetries, 0, math.MaxUint32},
+		{nl80211TxFailed, 0, math.MaxUint32},
+		{nl80211BeaconSignalAvg, nl80211SignalLowerBound, nl80211SignalUpperBound},
+		{nl80211SignalAvg, nl80211SignalLowerBound, nl80211SignalUpperBound},
+		{nl80211Signal, nl80211SignalLowerBound, nl80211SignalUpperBound}}
+
+	tf := s.FixtValue().(*wificell.TestFixture)
+
+	apOpts := []hostapd.Option{hostapd.Mode(hostapd.Mode80211nPure), hostapd.Channel(6), hostapd.HTCaps(hostapd.HTCapHT40)}
+	apIface, err := tf.ConfigureAP(ctx, apOpts, nil)
+	if err != nil {
+		s.Fatal("Failed to configure ap, err: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := tf.DeconfigAP(ctx, apIface); err != nil {
+			s.Error("Failed to deconfig ap, err: ", err)
+		}
+	}(ctx)
+	ctx, cancel := tf.ReserveForDeconfigAP(ctx, apIface)
+	defer cancel()
+	s.Log("AP setup done")
+
+	_, err = tf.ConnectWifiAP(ctx, apIface)
+	if err != nil {
+		s.Fatal("Failed to connect to WiFi, err: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := tf.CleanDisconnectWifi(ctx); err != nil {
+			s.Error("Failed to disconnect WiFi, err: ", err)
+		}
+	}(ctx)
+	ctx, cancel = tf.ReserveForDisconnect(ctx)
+	defer cancel()
+	s.Log("Connected")
+
+	// Get "iw dev interface station dump" output
+	iwr := iw.NewRemoteRunner(s.DUT().Conn())
+	clientIface, err := tf.ClientInterface(ctx)
+	if err != nil {
+		s.Fatal("Failed to get the client interface, err: ", err)
+	}
+	stationInformation, err := iwr.AllStationInformation(ctx, clientIface)
+	if err != nil {
+		s.Fatal("Failed to get the nl80211 station information, err: ", err)
+	}
+
+	attributeValues := make(map[string]int64)
+	// Check if all required attributes exist in the output
+	s.Log("The nl80211 station information is")
+	for _, attribute := range nl80211Attributes {
+		output, ok := stationInformation[attribute.name]
+		if !ok {
+			s.Errorf("nl80211_sta_info attribute %q is missing", attribute.name)
+			continue
+		}
+		value, err := parseValue(attribute.name, output)
+		// Check if the values of attributes fall in the range
+		if err != nil || value < attribute.lowerBound || value > attribute.upperBound {
+			s.Errorf("nl80211_sta_info attribute %q has invalid value: %s", attribute.name, output)
+			continue
+		}
+		attributeValues[attribute.name] = value
+		s.Logf("%s: %s", attribute.name, output)
+	}
+
+	// Check if the difference between signal, signal avg and beacon signal avg
+	// is too large
+	for _, attribute := range []string{nl80211Signal, nl80211SignalAvg} {
+		signalValue, ok1 := attributeValues[attribute]
+		beaconSignalAvgValue, ok2 := attributeValues[nl80211BeaconSignalAvg]
+		if ok1 && ok2 && math.Abs(float64(signalValue-beaconSignalAvgValue)) > nl80211SignalError {
+			s.Errorf("The difference between %q %d and %q %d is larger than the threshold %d", attribute, attributeValues[attribute], nl80211BeaconSignalAvg, attributeValues[nl80211BeaconSignalAvg], nl80211SignalError)
+		}
+	}
+}
+
+func parseValue(name, text string) (value int64, err error) {
+	number := text
+	// An example of the signal output, the values in the brackets are signal
+	// strength per chain
+	// signal:              -56 [-56, -59] dBm
+	// signal avg:          -60 dBm
+	// beacon signal avg:   -58 dBm
+	if name == nl80211Signal || name == nl80211SignalAvg || name == nl80211BeaconSignalAvg {
+		numbers := strings.Split(text, " ")
+		if len(numbers) == 0 {
+			return value, errors.New("failed to get the number")
+		}
+		number = numbers[0]
+	}
+	return strconv.ParseInt(number, 10, 64)
+}
