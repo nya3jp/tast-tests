ba5cf81ccfe66bd6fb0f6a0867401d610ebdebd9
Michael Rygiel <michaelrygiel@google.com>
2022-07-01 20:10:49 +0000

    network: Parameterize shill_captive_portal_http.go tast test
    This builds upon the shill_captive_portal_http.go tast test and
    paramaterizes it to also include a test for the 'portal-suspected' state
    when the httpserver redirect has no location in the header. The
    HTTPResponseHandler function can now be passed from the test to
    customize the handler.
    
    BUG=b:237811539
    TEST=tast run ${DUT} network.ShillCaptivePortalHTTP*
    
    Change-Id: I3eda623e76af816cb12b2e29a647a126828b370f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3739230
    Tested-by: Michael Rygiel <michaelrygiel@google.com>
    Commit-Queue: Michael Rygiel <michaelrygiel@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>

--- src/chromiumos/tast/local/bundles/cros/network/virtualnet/httpserver/httpserver.go
+++ src/chromiumos/tast/local/bundles/cros/network/virtualnet/httpserver/httpserver.go
@@ -19,38 +19,41 @@ import (
 
 // Paths in chroot.
 const (
-	logPath     = "/tmp/httpServer.log"
-	redirectURL = "http://www.foo.com"
+	logPath = "/tmp/httpServer.log"
 )
 
 type httpserver struct {
 	// port is the port that the HTTP server will listen and serve on.
 	port   string
+	handle func(rw http.ResponseWriter, req *http.Request)
 	server *http.Server
 	env    *env.Env
 }
 
 // Handler creates the object to handle the response for the HTTP server.
-type Handler struct{}
+type Handler struct {
+	handle func(rw http.ResponseWriter, req *http.Request)
+}
 
 // ServeHTTP will have the HTTP server respond with 302 redirects and a redirect URL.
 func (h *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	http.Redirect(rw, req, redirectURL, http.StatusFound)
+	h.handle(rw, req)
 }
 
 // New creates a new httpserver object. The returned object can be passed to
 // Env.StartServer(), its lifetime will be managed by the Env object. The
-// httpserver will only respond with 302 redirects. Port will be the port the
+// httpserver will only respond with |handle|. Port will be the port the
 // HTTP server listens and serves on.
-func New(port string) *httpserver {
-	return &httpserver{port: port}
+func New(port string, handle func(rw http.ResponseWriter, req *http.Request)) *httpserver {
+	return &httpserver{port: port, handle: handle}
 }
 
 // Start starts the HTTP server in a separate process. The HTTP server listens on
 // any IPv4 and IPv6 address within the namespace.
 func (h *httpserver) Start(ctx context.Context, env *env.Env) (retErr error) {
 	h.env = env
-	h.server = &http.Server{Addr: fmt.Sprintf(":%v", h.port), Handler: &Handler{}}
+	handler := &Handler{handle: h.handle}
+	h.server = &http.Server{Addr: fmt.Sprintf(":%v", h.port), Handler: handler}
 
 	errChannel := make(chan error)
 	go func() {
