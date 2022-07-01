bd50e8790bcc333eccf8ba60b14df7daf0726a37
Michael Rygiel <michaelrygiel@google.com>
2022-07-01 20:10:49 +0000

    network: Parameterize shill_captive_portal_http.go tast test
    This builds upon the shill_captive_portal_http.go tast test and
    paramaterizes it to also include a test for the 'portal-suspected' state
    when the httpserver redirect has no location in the header. The
    HTTPResponseHandler function can now be passed from the test to
    customize the handler.
    
    BUG=b:237811539
    TEST=tast run ${DUT} network.ShillCaptivePortalHTTP*
    
    Change-Id: I3eda623e76af816cb12b2e29a647a126828b370f

--- src/chromiumos/tast/local/bundles/cros/network/virtualnet/virtualnet.go
+++ src/chromiumos/tast/local/bundles/cros/network/virtualnet/virtualnet.go
@@ -6,6 +6,7 @@ package virtualnet
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"chromiumos/tast/common/shillconst"
@@ -34,8 +35,10 @@ type EnvOptions struct {
 	// RAServer enables the RA server in the Env. IPv6 addresses can be obtained
 	// on the interface by SLAAC.
 	RAServer bool
-	// HTTPServer enables the HTTP server in the Env.
-	HTTPServer bool
+	// HTTPServerResponseHandler is the handler function for the HTTPServer to
+	// customize how the HTTPServer should respond to requests. If the handler is
+	// defined, then this enables the HTTP server in the Env.
+	HTTPServerResponseHandler func(rw http.ResponseWriter, req *http.Request)
 	// AddressToForceGateway is the address to force an IPv4 or IPv6 address to
 	// the gateway. When paired with a dnsmasq server, the DNS is queried for this
 	// address, and dnsmasq will respond with the address to the gateway. For the
@@ -93,8 +96,8 @@ func CreateRouterEnv(ctx context.Context, m *shill.Manager, pool *subnet.Pool, o
 		}
 	}
 
-	if opts.HTTPServer {
-		httpserver := httpserver.New("80")
+	if opts.HTTPServerResponseHandler != nil {
+		httpserver := httpserver.New("80", opts.HTTPServerResponseHandler)
 		if err := router.StartServer(ctx, "httpserver", httpserver); err != nil {
 			return nil, nil, errors.Wrap(err, "failed to start http server")
 		}
