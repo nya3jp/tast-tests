dc97870812bd29283196e0c5245a04bee9ca683c
Seewai Fu <seewaifu@google.com>
2022-07-01 17:45:24 -0700

    tast: Update example tests to use an artifact existed for all builders
     Update example tests to use an artifact existed for all builders.
    
    BUG=b:237837177
    TEST=../tast/fast_build.sh -T
    TEST=tast run -build=true -buildartifactsurl=gs://chromeos-image-archive/brya-manatee-release/R105-14947.0.0/ 127.0.0.1:2222 meta.ArtifactFiles meta.RemoteArtifactFile
    TEST=tast run -build=true -buildartifactsurl=gs://chromeos-image-archive/brya-manatee-release/R105-14948.0.0/ 127.0.0.1:2222 meta.ArtifactFiles meta.RemoteArtifactFiles
    
    Change-Id: I41390df75752ad3d2c802642d2ae25f32cc9484c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3739234
    Reviewed-by: Kshitij Pancholi <panchok@google.com>
    Commit-Queue: Seewai Fu <seewaifu@google.com>
    Tested-by: Seewai Fu <seewaifu@google.com>

--- src/chromiumos/tast/local/bundles/cros/meta/artifact_files.go
+++ src/chromiumos/tast/local/bundles/cros/meta/artifact_files.go
@@ -17,17 +17,19 @@ func init() {
 		Desc:     "Demonstrates how to use artifact data files",
 		Contacts: []string{"nya@chromium.org", "tast-owners@chromium.org"},
 		Attr:     []string{"group:mainline", "informational"},
-		Data:     []string{"artifact_files_UPLOADED"},
+		Data:     []string{"artifact_files_metadata_json"},
 	})
 }
 
 // ArtifactFiles tests if local test can download artifact files.
+// Do not promote this test because there is no guarantee the file will exist in future.
+// This test is only for demonstration purpose.
 func ArtifactFiles(ctx context.Context, s *testing.State) {
 	// Build artifacts can be used as an external data file and read with
 	// s.DataPath just similarly as internal data files or static external data files.
 	// However, this works for ChromeOS images built on official builders only;
 	// on developer builds an error is raised.
-	if b, err := ioutil.ReadFile(s.DataPath("artifact_files_UPLOADED")); err != nil {
+	if b, err := ioutil.ReadFile(s.DataPath("artifact_files_metadata_json")); err != nil {
 		s.Error("Failed reading artifact external data file: ", err)
 	} else {
 		s.Logf("Read artifact external data file (%d bytes)", len(b))
