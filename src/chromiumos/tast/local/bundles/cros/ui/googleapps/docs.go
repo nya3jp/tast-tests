b34796ecff59a67e5ea30f8895da6ace28bedf75
Jane Yang <jane.yang@cienet.com>
2022-07-05 10:54:04 +0800

    cuj: Add api for googleapps and refine the code
    1. Add ChangeDocTextColor, ChangeDocFontSize, UndoDoc, RedoDoc to googleapps
    2. Refine google docs and slides.
    3. Change the timeout for saving document to 5 seconds.
    4. Extending the timeout to click leave button.
    
    BUG=b:239108235
    TEST=fast_build.sh -T
    
    Change-Id: I6885104eeb8dd330bb3f972124d96fe6f47d78b2

--- src/chromiumos/tast/local/bundles/cros/ui/googleapps/docs.go
+++ src/chromiumos/tast/local/bundles/cros/ui/googleapps/docs.go
@@ -24,6 +24,8 @@ import (
 // docsName represents the name of the Google Docs web area.
 const docsName = "Google Docs"
 
+var docWebArea = nodewith.NameContaining(docsName).Role(role.RootWebArea)
+
 // NewGoogleDocs returns an action to create a new google document.
 func NewGoogleDocs(ctx context.Context, tconn *chrome.TestConn, br *browser.Browser, uiHandler cuj.UIActionHandler, newWindow bool) error {
 	var opts []browser.CreateTargetOption
@@ -36,13 +38,12 @@ func NewGoogleDocs(ctx context.Context, tconn *chrome.TestConn, br *browser.Brow
 		return errors.Wrap(err, "failed to open the google document")
 	}
 	defer conn.Close()
-	return webutil.WaitForQuiescence(ctx, conn, longerUIWaitTime)
+	return webutil.WaitForQuiescence(ctx, conn, longUITimeout)
 }
 
 // RenameDoc returns an action to rename the document.
 func RenameDoc(tconn *chrome.TestConn, kb *input.KeyboardEventWriter, title string) action.Action {
 	ui := uiauto.New(tconn)
-	docWebArea := nodewith.NameContaining(docsName).Role(role.RootWebArea)
 	renameTextbox := nodewith.Name("Rename").ClassName("docs-title-input").Ancestor(docWebArea).Editable().Focusable()
 	return ui.Retry(5, uiauto.NamedCombine("rename document",
 		ui.WaitUntilExists(docWebArea),
@@ -58,11 +59,60 @@ func RenameDoc(tconn *chrome.TestConn, kb *input.KeyboardEventWriter, title stri
 // EditDoc returns an action to edit the document.
 func EditDoc(tconn *chrome.TestConn, kb *input.KeyboardEventWriter, paragraph string) action.Action {
 	ui := uiauto.New(tconn)
-	docWebArea := nodewith.NameContaining(docsName).Role(role.RootWebArea)
 	content := nodewith.Name("Document content").Role(role.TextField).Ancestor(docWebArea).Editable().First()
+	canvas := nodewith.Role(role.Canvas).Ancestor(docWebArea).First()
 	return uiauto.NamedCombine("edit document",
 		ui.WaitUntilExists(content),
+		ui.LeftClick(canvas),
 		kb.TypeAction(paragraph),
+		waitForDocsSaved(tconn),
+	)
+}
+
+// ChangeDocTextColor returns an action to change text color to specific color.
+func ChangeDocTextColor(tconn *chrome.TestConn, color string) action.Action {
+	ui := uiauto.New(tconn)
+	moreButton := nodewith.Name("More").Role(role.ToggleButton).Ancestor(docWebArea)
+	textColorButton := nodewith.Name("Text color").Role(role.PopUpButton).Ancestor(docWebArea)
+	colorButton := nodewith.Name(color).Role(role.Cell).Ancestor(docWebArea)
+	return uiauto.Retry(retryTimes, uiauto.NamedCombine("change document text color to "+color,
+		uiauto.IfSuccessThen(ui.Gone(textColorButton), ui.LeftClickUntil(moreButton, ui.WithTimeout(shortUITimeout).WaitUntilExists(textColorButton))),
+		ui.LeftClickUntil(textColorButton, ui.WithTimeout(shortUITimeout).WaitUntilExists(colorButton)),
+		ui.LeftClick(colorButton),
+		waitForDocsSaved(tconn),
+	))
+}
+
+// ChangeDocFontSize returns an action to change font size to specific font size.
+func ChangeDocFontSize(tconn *chrome.TestConn, size string) action.Action {
+	ui := uiauto.New(tconn)
+	moreButton := nodewith.Name("More").Role(role.ToggleButton).Ancestor(docWebArea)
+	fontSizeTextField := nodewith.Name("Font size").Role(role.TextField).Ancestor(docWebArea)
+	fontSizeOption18 := nodewith.Name(size).Role(role.ListBoxOption).Ancestor(docWebArea)
+	return uiauto.Retry(retryTimes, uiauto.NamedCombine("change document font size to "+size,
+		uiauto.IfSuccessThen(ui.Gone(fontSizeTextField), ui.LeftClickUntil(moreButton, ui.WithTimeout(shortUITimeout).WaitUntilExists(fontSizeTextField))),
+		ui.LeftClickUntil(fontSizeTextField, ui.WithTimeout(shortUITimeout).WaitUntilExists(fontSizeOption18)),
+		ui.LeftClick(fontSizeOption18),
+		waitForDocsSaved(tconn),
+	))
+}
+
+// UndoDoc returns an action to undo document.
+func UndoDoc(tconn *chrome.TestConn) action.Action {
+	ui := uiauto.New(tconn)
+	undoButton := nodewith.NameContaining("Undo").Role(role.Button).Ancestor(docWebArea)
+	return uiauto.NamedCombine("undo document",
+		ui.LeftClick(undoButton),
+		waitForDocsSaved(tconn),
+	)
+}
+
+// RedoDoc returns an action to redo document.
+func RedoDoc(tconn *chrome.TestConn) action.Action {
+	ui := uiauto.New(tconn)
+	redoButton := nodewith.NameContaining("Redo").Role(role.Button).Ancestor(docWebArea)
+	return uiauto.NamedCombine("redo document",
+		ui.LeftClick(redoButton),
 		waitForDocsSaved(tconn),
 	)
 }
@@ -70,7 +120,6 @@ func EditDoc(tconn *chrome.TestConn, kb *input.KeyboardEventWriter, paragraph st
 // DeleteDoc returns an action to delete the document.
 func DeleteDoc(tconn *chrome.TestConn) action.Action {
 	ui := uiauto.New(tconn)
-	docWebArea := nodewith.NameContaining(docsName).Role(role.RootWebArea)
 	docHomeWebArea := nodewith.Name(docsName).Role(role.RootWebArea)
 	application := nodewith.Role(role.Application).Ancestor(docWebArea) // Google Docs appliction node.
 	fileButton := nodewith.Name("File").Role(role.MenuItem).Ancestor(application)
@@ -84,8 +133,9 @@ func DeleteDoc(tconn *chrome.TestConn) action.Action {
 		ui.DoDefault(goToDocsHome),
 		// When leaving the edit document, sometimes the "Leave Site?" dialog box will pop up.
 		// If it appears, click the leave button.
-		uiauto.IfSuccessThen(ui.WithTimeout(5*time.Second).WaitUntilExists(leaveButton), ui.DoDefault(leaveButton)),
-		ui.WithTimeout(longerUIWaitTime).WaitUntilExists(docHomeWebArea),
+		uiauto.IfSuccessThen(ui.WithTimeout(10*time.Second).WaitUntilExists(leaveButton),
+			ui.DoDefaultUntil(leaveButton, ui.WithTimeout(shortUITimeout).WaitUntilGone(leaveButton))),
+		ui.WithTimeout(longUITimeout).WaitUntilExists(docHomeWebArea),
 	)
 }
 
