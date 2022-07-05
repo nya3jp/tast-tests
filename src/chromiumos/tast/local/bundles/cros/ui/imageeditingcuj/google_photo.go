347e28360a859c5ba75b5ecef7360bbc2419601e
Alston Huang <alston.huang@cienet.com>
2022-07-05 20:33:51 +0800

    cuj: Supports lacros for EDU image editing cuj
    1. Use DoDefault, DoDefaultUntil instead of Click to prevent wrong click in lacros.
    2. Maximize the window so that mouse drags can be performed in the correct location.
    
    BUG=b:238268374
    TEST=tast run <var> <dut> ui.EDUImageEditingCUJ.basic_lacros_google_photos
    
    Change-Id: Ie2237fa497da7ba49b3e953e47e95b826e5a42c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3752491
    Tested-by: Alston Huang <alston.huang@cienet.com>
    Commit-Queue: Alston Huang <alston.huang@cienet.com>
    Reviewed-by: Jun Sheng <shengjun@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/imageeditingcuj/google_photo.go
+++ src/chromiumos/tast/local/bundles/cros/ui/imageeditingcuj/google_photo.go
@@ -12,6 +12,8 @@ import (
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/ui/cuj"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/mouse"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
@@ -39,7 +41,7 @@ var (
 
 // GooglePhotos holds the information used to do Google Photos testing.
 type GooglePhotos struct {
-	cr       *chrome.Chrome
+	br       *browser.Browser
 	tconn    *chrome.TestConn
 	ui       *uiauto.Context
 	uiHdl    cuj.UIActionHandler
@@ -49,9 +51,8 @@ type GooglePhotos struct {
 }
 
 // NewGooglePhotos returns the the manager of Google Photos, caller will able to control Google Photos through this object.
-func NewGooglePhotos(cr *chrome.Chrome, tconn *chrome.TestConn, uiHdl cuj.UIActionHandler, kb *input.KeyboardEventWriter, password string) *GooglePhotos {
+func NewGooglePhotos(tconn *chrome.TestConn, uiHdl cuj.UIActionHandler, kb *input.KeyboardEventWriter, password string) *GooglePhotos {
 	return &GooglePhotos{
-		cr:       cr,
 		tconn:    tconn,
 		ui:       uiauto.New(tconn),
 		uiHdl:    uiHdl,
@@ -60,10 +61,15 @@ func NewGooglePhotos(cr *chrome.Chrome, tconn *chrome.TestConn, uiHdl cuj.UIActi
 	}
 }
 
+// SetBrowser sets browser to chrome or lacros.
+func (g *GooglePhotos) SetBrowser(br *browser.Browser) {
+	g.br = br
+}
+
 // Open opens the Google Photos.
 func (g *GooglePhotos) Open() uiauto.Action {
 	return func(ctx context.Context) (err error) {
-		g.conn, err = g.cr.NewConn(ctx, cuj.GooglePhotosURL)
+		g.conn, err = g.br.NewConn(ctx, cuj.GooglePhotosURL)
 		if err != nil {
 			return errors.Wrapf(err, "failed to open URL: %s", cuj.GooglePhotosURL)
 		}
@@ -79,12 +85,17 @@ func (g *GooglePhotos) Open() uiauto.Action {
 
 		link := photosWebAreaFinder.Name("Go to Google Photos").Role(role.Link).First()
 		continueButton := photosWebAreaFinder.Name("Continue").Role(role.Button)
-		return uiauto.NamedCombine("enter Google Photos",
+		if err := uiauto.NamedCombine("enter Google Photos",
 			// If not logged in, the URL will navigate to the Google Photos homepage.
 			uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(link), g.uiHdl.Click(link)),
 			g.login(),
-			uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(continueButton), g.uiHdl.Click(continueButton)),
-		)(ctx)
+			uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(continueButton), g.ui.DoDefault(continueButton)),
+		)(ctx); err != nil {
+			return err
+		}
+		return ash.ForEachWindow(ctx, g.tconn, func(w *ash.Window) error {
+			return ash.SetWindowStateAndWait(ctx, g.tconn, w.ID, ash.WindowStateMaximized)
+		})
 	}
 }
 
@@ -111,18 +122,24 @@ func (g *GooglePhotos) login() uiauto.Action {
 		}
 	}
 
+	continueButton := nodewith.Name("Continue").Role(role.Button).Ancestor(photosWebArea)
 	nextButton := nodewith.Name("Next").Role(role.Button).Ancestor(googlePhotosWebArea)
-	continueButton := nodewith.Name("Continue").Role(role.Button).Ancestor(googlePhotosWebArea)
+	continueButton = nodewith.Name("Continue").Role(role.Button).Ancestor(googlePhotosWebArea)
 	showPasswordCheckBox := nodewith.Name("Show password").Role(role.CheckBox).Focusable()
 	passwordField := nodewith.Name("Enter your password").Role(role.TextField).Editable()
-	return uiauto.NamedCombine("login google account",
-		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(nextButton), g.uiHdl.Click(nextButton)),
-		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(continueButton), g.uiHdl.Click(continueButton)),
-		g.ui.WaitUntilExists(passwordField),
-		g.ui.LeftClick(showPasswordCheckBox),
-		g.ui.LeftClick(passwordField),
-		confirmInput(passwordField, g.password),
-		g.kb.AccelAction("Enter"),
+	return uiauto.NamedAction("login google account",
+		uiauto.IfFailThen(
+			g.ui.WithTimeout(shortUITimeout).WaitUntilExists(continueButton),
+			uiauto.Combine("login google account",
+				uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(nextButton), g.uiHdl.Click(nextButton)),
+				uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(continueButton), g.uiHdl.Click(continueButton)),
+				g.ui.WaitUntilExists(passwordField),
+				g.ui.LeftClick(showPasswordCheckBox),
+				g.ui.LeftClick(passwordField),
+				confirmInput(passwordField, g.password),
+				g.kb.AccelAction("Enter"),
+			),
+		),
 	)
 }
 
@@ -138,14 +155,14 @@ func (g *GooglePhotos) Upload(fileName string) uiauto.Action {
 	closeDialogButton := nodewith.Name("Close dialog").Role(role.Button).Focusable()
 	noThanksButton := nodewith.Name("No thanks").Role(role.Button).Focusable()
 	return uiauto.NamedCombine("upload the photo",
-		g.uiHdl.Click(uploadButton),
-		g.uiHdl.Click(computerItem),
-		g.uiHdl.Click(downloadsItem),
+		g.ui.DoDefault(uploadButton),
+		g.ui.DoDefaultUntil(computerItem, g.ui.WithTimeout(shortUITimeout).WaitUntilExists(downloadsItem)),
+		g.ui.DoDefault(downloadsItem),
 		g.uiHdl.Click(testImageOption),
 		g.uiHdl.Click(openButton),
-		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(continueButton), g.uiHdl.Click(continueButton)),
-		g.uiHdl.Click(closeDialogButton),
-		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(noThanksButton), g.uiHdl.Click(noThanksButton)),
+		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(continueButton), g.ui.DoDefault(continueButton)),
+		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(closeDialogButton), g.ui.DoDefault(closeDialogButton)),
+		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(noThanksButton), g.ui.DoDefault(noThanksButton)),
 	)
 }
 
@@ -158,14 +175,12 @@ func (g *GooglePhotos) AddFilters() uiauto.Action {
 	blushButton := nodewith.Name("Blush").Role(role.RadioButton).Ancestor(filtersGroup)
 	blushButtonFocused := blushButton.Focused()
 	return uiauto.NamedCombine("add filters to photo",
-		g.uiHdl.Click(photoLink),
-		g.uiHdl.Click(editButton),
-		g.uiHdl.Click(autoButton),
-		g.ui.WaitUntilExists(autoButtonFocused),
-		g.uiHdl.Click(blushButton),
-		g.ui.WaitUntilExists(blushButtonFocused),
+		g.ui.DoDefaultUntil(photoLink, g.ui.WithTimeout(shortUITimeout).WaitUntilExists(editButton)),
+		g.ui.DoDefault(editButton),
+		g.ui.DoDefaultUntil(autoButton, g.ui.WithTimeout(shortUITimeout).WaitUntilExists(autoButtonFocused)),
+		g.ui.DoDefaultUntil(blushButton, g.ui.WithTimeout(shortUITimeout).WaitUntilExists(blushButtonFocused)),
 		uiauto.IfSuccessThen(g.ui.Gone(doneButton), g.ui.MouseMoveTo(canvas, dragTime)),
-		g.uiHdl.Click(doneButton),
+		g.ui.DoDefault(doneButton),
 	)
 }
 
@@ -182,25 +197,31 @@ func (g *GooglePhotos) Edit() uiauto.Action {
 		endPoint := coords.NewPoint(int(x), int(y))
 		return mouse.Drag(g.tconn, startPoint, endPoint, dragTime)(ctx)
 	}
+	okButton := nodewith.Name("OK").Role(role.Button).Focusable().Ancestor(editPhotoWebArea)
+	closeDialogButton := nodewith.Name("Close dialog").Role(role.Button).Focusable().Ancestor(editPhotoWebArea)
 	adjustmentsTab := nodewith.Name("Basic adjustments").Role(role.Tab)
 	return uiauto.NamedCombine("edit the photo",
-		g.uiHdl.Click(editButton),
-		g.uiHdl.Click(adjustmentsTab),
+		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(okButton), g.ui.DoDefault(okButton)),
+		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(closeDialogButton), g.ui.DoDefault(closeDialogButton)),
+		g.ui.DoDefaultUntil(editButton, g.ui.WithTimeout(shortUITimeout).WaitUntilExists(adjustmentsTab)),
+		g.ui.DoDefault(adjustmentsTab),
 		dragLightSlider,
-		g.uiHdl.Click(doneButton),
+		g.ui.DoDefaultUntil(doneButton, g.ui.WithTimeout(shortUITimeout).WaitUntilGone(doneButton)),
 	)
 }
 
 // Rotate rotates the photo.
 func (g *GooglePhotos) Rotate() uiauto.Action {
+	closeDialogButton := nodewith.Name("Close dialog").Role(role.Button).Focusable().Ancestor(editPhotoWebArea)
 	rotateButton := nodewith.Name("Crop & rotate").Role(role.Button)
 	rotateLeftButton := nodewith.Name("Rotate left").Role(role.Button)
 	confirmButton := nodewith.Name("Confirm crop & rotate").Role(role.Button)
 	return uiauto.NamedCombine("rotate the photo",
-		g.uiHdl.Click(editButton),
-		g.uiHdl.Click(rotateButton),
-		g.uiHdl.Click(rotateLeftButton),
-		g.uiHdl.Click(confirmButton),
+		uiauto.IfSuccessThen(g.ui.WithTimeout(shortUITimeout).WaitUntilExists(closeDialogButton), g.ui.DoDefault(closeDialogButton)),
+		g.ui.DoDefaultUntil(editButton, g.ui.WithTimeout(shortUITimeout).WaitUntilExists(rotateButton)),
+		g.ui.DoDefaultUntil(rotateButton, g.ui.WithTimeout(shortUITimeout).WaitUntilExists(rotateLeftButton)),
+		g.ui.DoDefault(rotateLeftButton),
+		g.ui.DoDefaultUntil(confirmButton, g.ui.WithTimeout(shortUITimeout).WaitUntilGone(confirmButton)),
 	)
 }
 
@@ -208,7 +229,7 @@ func (g *GooglePhotos) Rotate() uiauto.Action {
 func (g *GooglePhotos) ReduceColor() uiauto.Action {
 	return func(ctx context.Context) error {
 		adjustmentsTab := nodewith.Name("Basic adjustments").Role(role.Tab)
-		if err := g.uiHdl.Click(adjustmentsTab)(ctx); err != nil {
+		if err := g.ui.DoDefault(adjustmentsTab)(ctx); err != nil {
 			return err
 		}
 		location, err := g.getSliderLocation(ctx, "Color")
@@ -223,7 +244,7 @@ func (g *GooglePhotos) ReduceColor() uiauto.Action {
 			uiauto.Sleep(2*time.Second), // Given the time to make change.
 			mouse.Drag(g.tconn, blackLocationPoint, whiteLocationPoint, dragTime*2),
 			uiauto.Sleep(2*time.Second), // Given the time to make change.
-			g.uiHdl.Click(doneButton),
+			g.ui.DoDefaultUntil(doneButton, g.ui.WithTimeout(shortUITimeout).WaitUntilGone(doneButton)),
 		)(ctx)
 	}
 }
@@ -242,8 +263,8 @@ func (g *GooglePhotos) Crop() uiauto.Action {
 	cropButton := nodewith.Name("Crop & rotate").Role(role.Button)
 	confirmButton := nodewith.Name("Confirm crop & rotate").Role(role.Button)
 	return uiauto.NamedCombine("crop the photo",
-		g.uiHdl.Click(editButton),
-		g.uiHdl.Click(cropButton),
+		g.ui.DoDefault(editButton),
+		g.ui.DoDefault(cropButton),
 		dragCropFrame,
 		g.uiHdl.Click(confirmButton),
 	)
@@ -254,8 +275,8 @@ func (g *GooglePhotos) UndoEdit() uiauto.Action {
 	undo := nodewith.Name("Undo edits").Role(role.Button)
 	return uiauto.NamedCombine("undo all the edits",
 		g.ui.MouseMoveTo(canvas, dragTime),
-		g.uiHdl.Click(undo),
-		g.uiHdl.Click(doneButton),
+		g.ui.DoDefault(undo),
+		g.ui.DoDefault(doneButton),
 	)
 }
 
@@ -269,7 +290,7 @@ func (g *GooglePhotos) CleanUp() uiauto.Action {
 		if err := g.ui.WithTimeout(shortUITimeout).WaitUntilExists(editPhotoWebArea.Focused())(ctx); err == nil {
 			return uiauto.NamedCombine("leave the edit mode",
 				g.ui.MouseMoveTo(canvas, dragTime),
-				g.uiHdl.Click(doneButton),
+				g.ui.DoDefault(doneButton),
 			)(ctx)
 		}
 		photoLink := nodewith.NameStartingWith("Photo - Square").Role(role.Link).Ancestor(photosWebArea)
