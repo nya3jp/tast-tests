24934fdc6b411c81f67ce3d9524db80f2a39a27a
Anastasiia N <anastasiian@google.com>
2022-07-06 08:44:48 +0000

    tast-tests: Test policies in LacrosMainProfileLogin
    Test the behavior with SyncDisabled and EnableSyncConsent policies.
    
    BUG=chromium:1319776
    TEST=tast run <ip> policy.LacrosMainProfileLogin
    
    Change-Id: I8f09510cf4829e8977e548c48588276a29442546

--- src/chromiumos/tast/local/bundles/cros/policy/lacros_main_profile_login.go
+++ src/chromiumos/tast/local/bundles/cros/policy/lacros_main_profile_login.go
@@ -9,6 +9,8 @@ import (
 	"time"
 
 	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/lacros"
@@ -16,6 +18,7 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/local/policyutil/fixtures"
 	"chromiumos/tast/testing"
 )
@@ -32,30 +35,92 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      fixture.LacrosPolicyLoggedInRealUser,
-		Timeout:      2*chrome.LoginTimeout + time.Minute,
+		Timeout:      9*chrome.LoginTimeout + 2*time.Minute,
 	})
 }
 
 func LacrosMainProfileLogin(ctx context.Context, s *testing.State) {
-	profileMenu := nodewith.NameStartingWith("Accounts and sync").Role(role.Menu)
+	profileMenu := nodewith.Name("Settings - Sync and Google services").Role(role.RootWebArea)
+	// When sync is on - "Turn off" button is shown.
+	syncOnState := nodewith.Name("Turn off").Role(role.Button).Ancestor(profileMenu)
+	// When sync is off - "Turn on sync" button is shown.
+	syncOffState := nodewith.NameStartingWith("Turn on sync").Role(role.Button).Ancestor(profileMenu)
+	syncDisabledState := nodewith.Name("Sync disabled").Role(role.StaticText).Ancestor(profileMenu)
+	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
 
 	for _, param := range []struct {
 		// name is the subtest name.
 		name string
 		// the button that should be pressed to accept of decline sync.
+		// if not present - FRE is expected to be skipped.
 		syncButton *nodewith.Finder
 		// the sync message that is expected to be shown at the end of the test.
-		syncMessage *nodewith.Finder
+		syncMessage             *nodewith.Finder
+		enableSyncConsentPolicy *policy.EnableSyncConsent
+		syncDisabledPolicy      *policy.SyncDisabled
 	}{
+		// cases with no policy.
 		{
-			name:        "no_policy_sync_on",
-			syncButton:  nodewith.Name("Yes, I'm in").Role(role.Button),
-			syncMessage: nodewith.Name("Sync is on").Role(role.StaticText).Ancestor(profileMenu),
+			name:                    "no_policy_sync_on",
+			syncButton:              nodewith.Name("Yes, I'm in").Role(role.Button),
+			syncMessage:             syncOnState,
+			enableSyncConsentPolicy: &policy.EnableSyncConsent{Stat: policy.StatusUnset},
+			syncDisabledPolicy:      &policy.SyncDisabled{Stat: policy.StatusUnset},
 		},
 		{
-			name:        "no_policy_sync_off",
-			syncButton:  nodewith.Name("No thanks").Role(role.Button),
-			syncMessage: nodewith.Name("Sync is off").Role(role.StaticText).Ancestor(profileMenu),
+			name:                    "no_policy_sync_off",
+			syncButton:              nodewith.Name("No thanks").Role(role.Button),
+			syncMessage:             syncOffState,
+			enableSyncConsentPolicy: &policy.EnableSyncConsent{Stat: policy.StatusUnset},
+			syncDisabledPolicy:      &policy.SyncDisabled{Stat: policy.StatusUnset},
+		},
+		// EnableSyncConsent = true is the same as above.
+		{
+			name:                    "sync_consent_enabled_sync_on",
+			syncButton:              nodewith.Name("Yes, I'm in").Role(role.Button),
+			syncMessage:             syncOnState,
+			enableSyncConsentPolicy: &policy.EnableSyncConsent{Val: true},
+			syncDisabledPolicy:      &policy.SyncDisabled{Stat: policy.StatusUnset},
+		},
+		// SyncDisabled = false is the same as above.
+		{
+			name:                    "sync_disabled_false_sync_on",
+			syncButton:              nodewith.Name("Yes, I'm in").Role(role.Button),
+			syncMessage:             syncOnState,
+			enableSyncConsentPolicy: &policy.EnableSyncConsent{Stat: policy.StatusUnset},
+			syncDisabledPolicy:      &policy.SyncDisabled{Val: false},
+		},
+		// SyncDisabled = true -> FRE is skipped, sync is disabled.
+		{
+			name:                    "sync_disabled",
+			syncMessage:             syncDisabledState,
+			enableSyncConsentPolicy: &policy.EnableSyncConsent{Stat: policy.StatusUnset},
+			syncDisabledPolicy:      &policy.SyncDisabled{Val: true},
+		},
+		{
+			name:                    "sync_disabled_with_sync_consent_disabled",
+			syncMessage:             syncDisabledState,
+			enableSyncConsentPolicy: &policy.EnableSyncConsent{Val: false},
+			syncDisabledPolicy:      &policy.SyncDisabled{Val: true},
+		},
+		{
+			name:                    "sync_disabled_with_sync_consent_enabled",
+			syncMessage:             syncDisabledState,
+			enableSyncConsentPolicy: &policy.EnableSyncConsent{Val: true},
+			syncDisabledPolicy:      &policy.SyncDisabled{Val: true},
+		},
+		// EnableSyncConsent = false -> FRE is skipped, sync is enabled.
+		{
+			name:                    "sync_consent_disabled",
+			syncMessage:             syncOnState,
+			enableSyncConsentPolicy: &policy.EnableSyncConsent{Val: false},
+			syncDisabledPolicy:      &policy.SyncDisabled{Stat: policy.StatusUnset},
+		},
+		{
+			name:                    "sync_consent_disabled_with_sync_disabled_false",
+			syncMessage:             syncOnState,
+			enableSyncConsentPolicy: &policy.EnableSyncConsent{Val: false},
+			syncDisabledPolicy:      &policy.SyncDisabled{Val: false},
 		},
 	} {
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
@@ -71,12 +136,22 @@ func LacrosMainProfileLogin(ctx context.Context, s *testing.State) {
 			}
 			defer cr.Close(cleanupCtx)
 
+			// Perform cleanup.
+			if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+				s.Fatal("Failed to clean up: ", err)
+			}
+
+			// Update policies.
+			policies := []policy.Policy{param.enableSyncConsentPolicy, param.syncDisabledPolicy}
+			if err := policyutil.ServeAndRefresh(ctx, fdms, cr, policies); err != nil {
+				s.Fatal("Failed to update policies: ", err)
+			}
+
 			// Connect to Test API to use it with the UI library.
 			tconn, err := cr.TestAPIConn(ctx)
 			if err != nil {
 				s.Fatal("Failed to connect Test API: ", err)
 			}
-			defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
 
 			// Launch Lacros.
 			lacros, err := lacros.Launch(ctx, tconn)
@@ -84,32 +159,29 @@ func LacrosMainProfileLogin(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to launch lacros-chrome: ", err)
 			}
 			defer lacros.Close(cleanupCtx)
+			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, param.name)
 
 			ui := uiauto.New(tconn)
 
-			welcomeButton := nodewith.Name("Let's go").Role(role.Button)
-			if err := uiauto.Combine("accept or decline sync",
-				ui.WaitUntilExists(welcomeButton),
-				ui.DoDefaultUntil(welcomeButton, ui.Exists(param.syncButton)),
-				ui.DoDefault(param.syncButton),
-			)(ctx); err != nil {
-				s.Fatal("Failed to accept or decline sync: ", err)
+			if param.syncButton != nil {
+				welcomeButton := nodewith.Name("Let's go").Role(role.Button)
+				if err := uiauto.Combine("accept or decline sync",
+					ui.WaitUntilExists(welcomeButton),
+					ui.DoDefaultUntil(welcomeButton, ui.Exists(param.syncButton)),
+					ui.DoDefault(param.syncButton),
+				)(ctx); err != nil {
+					s.Fatal("Failed to accept or decline sync: ", err)
+				}
 			}
 
-			profileToolbarButton := nodewith.ClassName("AvatarToolbarButton").Role(role.Button).Focusable()
-			loggedInUserEmail := nodewith.Name(cr.User()).Role(role.StaticText).Ancestor(profileMenu)
+			conn, err := lacros.NewConn(ctx, "chrome://settings/syncSetup")
+			if err != nil {
+				s.Fatal("Failed to open a new tab in Lacros browser: ", err)
+			}
+			defer conn.Close()
 
-			if err := uiauto.Combine("open the toolbar and check that the sync is on",
-				ui.WaitUntilExists(profileToolbarButton),
-				// Sync message may show an error in the beginning, but should change to 'sync is on/off'.
-				ui.WithTimeout(time.Minute).DoDefaultUntil(profileToolbarButton,
-					uiauto.Combine("check that the user is logged in",
-						ui.Exists(loggedInUserEmail),
-						ui.Exists(param.syncMessage),
-					),
-				),
-			)(ctx); err != nil {
-				s.Fatal("Failed to check that the sync is on: ", err)
+			if err := ui.WaitUntilExists(param.syncMessage)(ctx); err != nil {
+				s.Fatal("Failed to find the expected sync message: ", err)
 			}
 		})
 	}
