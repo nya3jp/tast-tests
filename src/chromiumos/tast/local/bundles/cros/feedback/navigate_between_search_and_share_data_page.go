26e4c269546d0b6f71a823c37b8ad5dad9ed2447
wenyu zhang <zhangwenyu@google.com>
2022-07-06 21:41:39 +0000

    feedback: Test navigating between search and share data page
    Verify user can navigate from search page to share data page. And
    essential elements exist in share data page. Verify user can go
    back from share data page to search page. The previously entered
    issue description still exists.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.NavigateBetweenSearchAndShareDataPage
    
    Change-Id: I6f9c40f36783dee78d8533a0cf82bbf6514c255a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3749667
    Tested-by: Wenyu Zhang <zhangwenyu@google.com>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Jun Sheng <shengjun@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/navigate_between_search_and_share_data_page.go
@@ -0,0 +1,94 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NavigateBetweenSearchAndShareDataPage,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "User is able to navigate between the search and share data page",
+		Contacts: []string{
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// NavigateBetweenSearchAndShareDataPage verifies the user can navigate
+// between the search and share data page.
+func NavigateBetweenSearchAndShareDataPage(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	s.Log("Setting up chrome")
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("OsFeedback"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
+
+	// Launching feedback app and navigating to share data page.
+	feedbackRootNode, err := feedbackapp.LaunchAndGoToShareDataPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and go to share data page: ", err)
+	}
+
+	// Verifying essential elements exist in the share data page.
+	sendButton := nodewith.Name("Send").Role(role.Button).Ancestor(feedbackRootNode)
+	attachfilesTitle := nodewith.Name("Attach files").Role(role.StaticText).Ancestor(
+		feedbackRootNode)
+	emailTitle := nodewith.Name("Email").Role(role.StaticText).Ancestor(feedbackRootNode)
+	shareDiagnosticDataTitle := nodewith.Name("Share diagnostic data").Role(
+		role.StaticText).Ancestor(feedbackRootNode)
+
+	if err := uiauto.Combine("Verify essential elements exist",
+		ui.WaitUntilExists(sendButton),
+		ui.WaitUntilExists(attachfilesTitle),
+		ui.WaitUntilExists(emailTitle),
+		ui.WaitUntilExists(shareDiagnosticDataTitle),
+	)(ctx); err != nil {
+		s.Fatal("Failed to find element: ", err)
+	}
+
+	// Looking for back button and click.
+	backButton := nodewith.Name("Back").Role(role.Button).Ancestor(feedbackRootNode)
+	if err := ui.DoDefault(backButton)(ctx); err != nil {
+		s.Fatal("Failed to click back button: ", err)
+	}
+
+	// Verifying the issue description input stores the text user entered previously.
+	issueDescription := nodewith.Name(feedbackapp.IssueText).Role(role.StaticText).Ancestor(
+		feedbackRootNode)
+	if err := ui.WaitUntilExists(issueDescription)(ctx); err != nil {
+		s.Fatal("Failed to find issue description user entered previously: ", err)
+	}
+}
