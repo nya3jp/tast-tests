6036a8d5651f7d8a2ea3619b133229c9e7f9aea0
wenyu zhang <zhangwenyu@google.com>
2022-07-06 22:52:19 +0000

    feedback: Test submitting feedback and closing the app
    Verify user can submit a feedback successfully and then close
    the app.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.SubmitFeedbackAndCloseApp
    
    Change-Id: I64cd18cc296fe962006775cc486f1de1b1e0c53c

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/submit_feedback_and_close_app.go
@@ -0,0 +1,109 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SubmitFeedbackAndCloseApp,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "User is able to submit a report and then close feedback app",
+		Contacts: []string{
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// SubmitFeedbackAndCloseApp verifies the user can submit a report and
+// close Feedback app.
+func SubmitFeedbackAndCloseApp(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	s.Log("Setting up chrome")
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("OsFeedback"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn)
+
+	// Launching feedback app and go to share data page.
+	feedbackRootNode, err := feedbackapp.LaunchAndGoToShareDataPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and navigating to share data page: ", err)
+	}
+
+	// Looking for send button and then submit the feedback.
+	sendButton := nodewith.Name("Send").Role(role.Button).Ancestor(feedbackRootNode)
+	if err := ui.DoDefault(sendButton)(ctx); err != nil {
+		s.Fatal("Failed to submit feedback: ", err)
+	}
+
+	// Verifying essential elements exist in the share data page.
+	title := nodewith.Name("Thanks for your feedback").Role(role.StaticText).Ancestor(
+		feedbackRootNode)
+	newReportButton := nodewith.Name("Send new report").Role(role.Button).Ancestor(
+		feedbackRootNode)
+	exploreAppLink := nodewith.NameContaining("Explore app").Role(role.Link).Ancestor(
+		feedbackRootNode)
+	diagnosticsAppLink := nodewith.NameContaining("Diagnostics app").Role(role.Link).Ancestor(
+		feedbackRootNode)
+
+	if err := uiauto.Combine("Verify essential elements exist",
+		ui.WaitUntilExists(title),
+		ui.WaitUntilExists(newReportButton),
+		ui.WaitUntilExists(exploreAppLink),
+		ui.WaitUntilExists(diagnosticsAppLink),
+	)(ctx); err != nil {
+		s.Fatal("Failed to find element: ", err)
+	}
+
+	// Looking for done button and click.
+	doneButton := nodewith.Name("Done").Role(role.Button).Ancestor(feedbackRootNode)
+	if err := ui.DoDefault(doneButton)(ctx); err != nil {
+		s.Fatal("Failed to find done button: ", err)
+	}
+
+	// Verifying feedback window is closed.
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if err := ash.WaitForApp(ctx, tconn, apps.Feedback.ID, 10*time.Second); err == nil {
+			return errors.Wrap(err, "feedback app is not closed")
+		}
+
+		return nil
+	}, &testing.PollOptions{Timeout: time.Minute}); err != nil {
+		s.Fatal("Failed to verify feedback window is closed: ", err)
+	}
+}
