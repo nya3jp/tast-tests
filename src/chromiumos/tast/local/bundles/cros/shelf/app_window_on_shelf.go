2645989f82298d8fcf288194115cc92e6f4f0068
Wen-Chien Wang <wcwang@chromium.org>
2022-07-07 23:21:44 +0000

    [tast-tests] Verify app window on shelf
    This CL adds a Tast test to verify that opening a chrome app as a
    window/tab will match the number of windows and instances on
    shelf.
    
    BUG=b:200987156
    TEST=launcher.AppWindowOnShelf.* on a volteer device
    
    Change-Id: I6c2720d3689d4783a5c167fc878f667f39cdcd5d

--- src/chromiumos/tast/local/bundles/cros/shelf/app_window_on_shelf.go
+++ src/chromiumos/tast/local/bundles/cros/shelf/app_window_on_shelf.go
@@ -6,11 +6,13 @@ package shelf
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"time"
 
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto"
@@ -34,7 +36,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         AppWindowOnShelf,
 		LacrosStatus: testing.LacrosVariantNeeded,
-		Desc:         "Checks the window instances when opening an chrome app in a new window/tab",
+		Desc:         "Checks the window instances when opening a chrome app in a new window/tab",
 		Contacts: []string{
 			"wcwang@chromium.org",
 			"tbarzic@chromium.org",
@@ -44,20 +46,12 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
 			Name:    "productivity_launcher_clamshell_mode",
-			Val:     launcher.TestCase{ProductivityLauncher: true, TabletMode: false},
+			Val:     launcher.TestCase{TabletMode: false},
 			Fixture: "chromeLoggedInWith100FakeAppsProductivityLauncher",
-		}, {
-			Name:    "clamshell_mode",
-			Val:     launcher.TestCase{ProductivityLauncher: false, TabletMode: false},
-			Fixture: "chromeLoggedInWith100FakeAppsLegacyLauncher",
 		}, {
 			Name:    "productivity_launcher_tablet_mode",
-			Val:     launcher.TestCase{ProductivityLauncher: true, TabletMode: true},
+			Val:     launcher.TestCase{TabletMode: true},
 			Fixture: "chromeLoggedInWith100FakeAppsProductivityLauncher",
-		}, {
-			Name:    "tablet_mode",
-			Val:     launcher.TestCase{ProductivityLauncher: false, TabletMode: true},
-			Fixture: "chromeLoggedInWith100FakeAppsLegacyLauncher",
 		}},
 	})
 }
@@ -75,12 +69,7 @@ func AppWindowOnShelf(ctx context.Context, s *testing.State) {
 	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
 	defer cancel()
 
-	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
-	defer faillog.SaveScreenshotOnError(cleanupCtx, cr, s.OutDir(), s.HasError)
-
-	testCase := s.Param().(launcher.TestCase)
-	tabletMode := testCase.TabletMode
-	productivityLauncher := testCase.ProductivityLauncher
+	tabletMode := s.Param().(launcher.TestCase).TabletMode
 	originallyEnabled, err := ash.TabletModeEnabled(ctx, tconn)
 	if err != nil {
 		s.Fatal("Failed to check if DUT is in tablet mode: ", err)
@@ -91,6 +80,9 @@ func AppWindowOnShelf(ctx context.Context, s *testing.State) {
 	}
 	defer cleanup(ctx)
 
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+	defer faillog.SaveScreenshotOnError(cleanupCtx, cr, s.OutDir(), s.HasError)
+
 	// Ensure that the tablet launcher is closed before opening a launcher instance for test in clamshell.
 	if originallyEnabled && !tabletMode {
 		if err := ash.WaitForLauncherState(ctx, tconn, ash.Closed); err != nil {
@@ -98,18 +90,12 @@ func AppWindowOnShelf(ctx context.Context, s *testing.State) {
 		}
 	}
 
-	usingBubbleLauncher := productivityLauncher && !tabletMode
-	var appsGridView *nodewith.Finder
-	// Open the Launcher and go to Apps list page.
-	if usingBubbleLauncher {
-		if err := launcher.OpenBubbleLauncher(tconn)(ctx); err != nil {
-			s.Fatal("Failed to open bubble launcher: ", err)
-		}
-		appsGridView = nodewith.ClassName("ScrollableAppsGridView")
-	} else {
-		if err := launcher.OpenExpandedView(tconn)(ctx); err != nil {
-			s.Fatal("Failed to open Expanded Application list view: ", err)
-		}
+	if err := openLauncher(ctx, tconn, tabletMode); err != nil {
+		s.Fatal("Failed to open the launcher")
+	}
+
+	appsGridView := nodewith.ClassName("ScrollableAppsGridView")
+	if tabletMode {
 		appsGridView = nodewith.ClassName("AppsGridView")
 	}
 
@@ -128,81 +114,62 @@ func AppWindowOnShelf(ctx context.Context, s *testing.State) {
 	}
 
 	// Check if a chrome instance is created for the tab.
-	w, err := ash.GetActiveWindow(ctx, tconn)
+	activeWindow, err := ash.GetActiveWindow(ctx, tconn)
 	if err != nil {
 		s.Fatal("Error getting the active window")
 	}
-	if !strings.Contains(w.Title, "Chrome") {
+
+	chromeBrowser, err := apps.ChromeOrChromium(ctx, tconn)
+	if err != nil {
+		s.Fatal("Could not determine the correct Chrome app to use: ", err)
+	}
+	if !strings.Contains(activeWindow.Title, chromeBrowser.Name) {
 		s.Fatal("The chrome app is not opened in the chrome browser")
 	}
 
-	// Open the Launcher and go to Apps list page.
-	if usingBubbleLauncher {
-		if err := launcher.OpenBubbleLauncher(tconn)(ctx); err != nil {
-			s.Fatal("Failed to open bubble launcher: ", err)
-		}
-	} else {
-		if err := launcher.OpenExpandedView(tconn)(ctx); err != nil {
-			s.Fatal("Failed to open Expanded Application list view: ", err)
-		}
+	if err := openLauncher(ctx, tconn, tabletMode); err != nil {
+		s.Fatal("Failed to open the launcher")
 	}
-
 	// Open the `app` as a window
 	if err := openAppAs(ctx, tconn, app, window); err != nil {
 		s.Fatal("Failed to open app in a new window for the first time: ", err)
 	}
 
-	fakeAppOnShelf := nodewith.NameContaining("fake app").ClassName("ash/ShelfAppButton")
-	if err := ui.WaitUntilExists(fakeAppOnShelf)(ctx); err != nil {
+	appItemOnShelf := nodewith.NameContaining("fake app").ClassName("ash/ShelfAppButton")
+	if err := ui.WaitUntilExists(appItemOnShelf)(ctx); err != nil {
 		s.Fatal("The chrome app is not opened in a new window")
 	}
 
-	w, err = ash.GetActiveWindow(ctx, tconn)
+	activeWindow, err = ash.GetActiveWindow(ctx, tconn)
 	if err != nil {
 		s.Fatal("Error getting the active window")
 	}
-	if !strings.Contains(w.Title, "fake app ") {
+	if !strings.Contains(activeWindow.Title, "fake app ") {
 		s.Fatal("The chrome app window is not activated")
 	}
 
-	// Open the Launcher and go to Apps list page.
-	if usingBubbleLauncher {
-		if err := launcher.OpenBubbleLauncher(tconn)(ctx); err != nil {
-			s.Fatal("Failed to open bubble launcher: ", err)
-		}
-	} else {
-		if err := launcher.OpenExpandedView(tconn)(ctx); err != nil {
-			s.Fatal("Failed to open Expanded Application list view: ", err)
-		}
+	if err := openLauncher(ctx, tconn, tabletMode); err != nil {
+		s.Fatal("Failed to open the launcher")
 	}
-
 	// Open the `app` as a window
 	if err := openAppAs(ctx, tconn, app, window); err != nil {
 		s.Fatal("Failed to open app in a new window for the second time: ", err)
 	}
 
-	// Open the Launcher and go to Apps list page.
-	if usingBubbleLauncher {
-		if err := launcher.OpenBubbleLauncher(tconn)(ctx); err != nil {
-			s.Fatal("Failed to open bubble launcher: ", err)
-		}
-	} else {
-		if err := launcher.OpenExpandedView(tconn)(ctx); err != nil {
-			s.Fatal("Failed to open Expanded Application list view: ", err)
-		}
+	if err := openLauncher(ctx, tconn, tabletMode); err != nil {
+		s.Fatal("Failed to open the launcher")
 	}
-
 	// Open the `app` as a tab
 	if err := openAppAs(ctx, tconn, app, tab); err != nil {
 		s.Fatal("Failed to open app in a new tab for the second time: ", err)
 	}
 
 	// Check if the chrome browser is open now.
-	w, err = ash.GetActiveWindow(ctx, tconn)
+	activeWindow, err = ash.GetActiveWindow(ctx, tconn)
 	if err != nil {
 		s.Fatal("Error getting the active window")
 	}
-	if !strings.Contains(w.Title, "Chrome") {
+	if !strings.Contains(activeWindow.Title, chromeBrowser.Name) {
 		s.Fatal("The chrome app is not opened in the chrome browser")
 	}
 
@@ -229,7 +196,7 @@ func AppWindowOnShelf(ctx context.Context, s *testing.State) {
 			s.Fatal("Failed to swipe up the hotseat to show extended shelf: ", err)
 		}
 	}
-	if err := ui.LeftClick(fakeAppOnShelf)(ctx); err != nil {
+	if err := ui.LeftClick(appItemOnShelf)(ctx); err != nil {
 		s.Fatal("Failed to click on the fake app on shelf")
 	}
 
@@ -245,10 +212,9 @@ func AppWindowOnShelf(ctx context.Context, s *testing.State) {
 	}
 }
 
+// openAppAs opens a chrome extension app as window/tab according to `instanceType`. The series of actions are done in launcher.
 func openAppAs(ctx context.Context, tconn *chrome.TestConn, app *nodewith.Finder, instanceType newInstanceType) error {
 	newInstanceMenuItem := nodewith.NameContaining("New ").ClassName("MenuItemView")
-	newTabOption := nodewith.Name("New tab").ClassName("MenuItemView")
-	newWindowOption := nodewith.Name("New window").ClassName("MenuItemView")
 
 	ui := uiauto.New(tconn)
 
@@ -259,49 +225,33 @@ func openAppAs(ctx context.Context, tconn *chrome.TestConn, app *nodewith.Finder
 		return errors.Wrapf(err, "failed to open the context menu on %+q", app.Pretty())
 	}
 
-	// Check if the context menu is showing "New window" or "New tab"
-	isNewTabShown, tabErr := ui.IsNodeFound(ctx, newTabOption)
-	isNewWindowShown, windowErr := ui.IsNodeFound(ctx, newWindowOption)
-
-	if tabErr != nil || windowErr != nil || (!isNewTabShown && !isNewWindowShown) {
-		return errors.New("error checking the New window/tab menu item")
+	if instanceType == tab {
+		newInstanceMenuItem = nodewith.Name("New tab").ClassName("MenuItemView")
+	} else {
+		newInstanceMenuItem = nodewith.Name("New window").ClassName("MenuItemView")
 	}
 
-	if instanceType == tab {
-		if !isNewTabShown {
-			if err := setLaunchAppAs(ctx, tconn, app, instanceType); err != nil {
-				return err
-			}
-		} else {
-			// If the default is to open a new tab, directly click on the menu item to open.
-			if err := uiauto.Combine("Open the app in a new tab",
-				ui.LeftClick(newTabOption),
-				ui.WaitUntilGone(newInstanceMenuItem),
-			)(ctx); err != nil {
-				return err
-			}
-			return nil
+	isMenuItemFound, err := ui.IsNodeFound(ctx, newInstanceMenuItem)
+	if err != nil {
+		return errors.Wrapf(err, "error finding the New %s menu item", instanceType)
+	}
+	if !isMenuItemFound {
+		if err := setLaunchAppAs(ctx, tconn, app, instanceType); err != nil {
+			return err
 		}
 	} else {
-		// instanceType == Window
-		if !isNewWindowShown {
-			if err := setLaunchAppAs(ctx, tconn, app, instanceType); err != nil {
-				return err
-			}
-		} else {
-			// If the default is to open a new window, directly click on the menu item to open.
-			if err := uiauto.Combine("Open the app in a new window",
-				ui.LeftClick(newWindowOption),
-				ui.WaitUntilGone(newInstanceMenuItem),
-			)(ctx); err != nil {
-				return err
-			}
-			return nil
+		// If the default instance to open matches the `instanceType`, directly click on the menu item to open.
+		if err := uiauto.Combine(fmt.Sprintf("Open the app in a new %s", instanceType),
+			ui.LeftClick(newInstanceMenuItem),
+			ui.WaitUntilGone(newInstanceMenuItem),
+		)(ctx); err != nil {
+			return err
 		}
+		return nil
 	}
 
 	// Right click on the context menu again to open a new window/tab on the app
-	if err := uiauto.Combine("Open a new window/tab on the app",
+	if err := uiauto.Combine(fmt.Sprintf("Open a new %s on the app", instanceType),
 		ui.RightClick(app),
 		ui.WaitUntilExists(newInstanceMenuItem),
 		ui.LeftClick(newInstanceMenuItem),
@@ -312,6 +262,8 @@ func openAppAs(ctx context.Context, tconn *chrome.TestConn, app *nodewith.Finder
 	return nil
 }
 
+// setLaunchAppAs set the default new chrome app instance to be opened as window/tab, according to `instanceType`.
+// Note that this function doesn't actually open a new instance.
 func setLaunchAppAs(ctx context.Context, tconn *chrome.TestConn, appFinder *nodewith.Finder, instanceType newInstanceType) error {
 	newInstanceMenuItem := nodewith.NameContaining("New ").ClassName("MenuItemView")
 	newTabOption := nodewith.Name("New tab").ClassName("MenuItemView")
@@ -336,7 +288,7 @@ func setLaunchAppAs(ctx context.Context, tconn *chrome.TestConn, appFinder *node
 		targetButton = newWindowOption
 	}
 
-	if err := uiauto.Combine("Set to open the app as window/tab in default",
+	if err := uiauto.Combine(fmt.Sprintf("Set to open the app as %s in default", instanceType),
 		mouse.Move(tconn, submenuArrow, 0),
 		ui.WaitUntilExists(targetButton),
 		ui.LeftClick(targetButton),
@@ -345,5 +297,19 @@ func setLaunchAppAs(ctx context.Context, tconn *chrome.TestConn, appFinder *node
 		return errors.Wrapf(err, "failed to set the default new instance to %s", instanceType)
 	}
 
+	return nil
+}
+
+func openLauncher(ctx context.Context, tconn *chrome.TestConn, tabletMode bool) error {
+	// Open the Launcher and go to Apps list page.
+	if tabletMode {
+		if err := launcher.OpenExpandedView(tconn)(ctx); err != nil {
+			return errors.Wrap(err, "failed to open Expanded Application list view")
+		}
+	} else {
+		if err := launcher.OpenBubbleLauncher(tconn)(ctx); err != nil {
+			return errors.Wrap(err, "failed to open bubble launcher")
+		}
+	}
 	return nil
 }
