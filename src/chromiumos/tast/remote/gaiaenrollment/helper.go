ae9c3167eca9d2f6eb67425d9e1a3e6a0ecbb877
Ruben Zakarian <rzakarian@google.com>
2022-07-08 11:11:19 -0700

    Policy: Modify Integration test.
    Kiosk enrollment test - added steps to deprovision device at the end of
    the run. Also added the suite so the test will kick off nightly now.
    
    BUG=b:196112165
    TEST=ran test on local and lab machines
    
    Change-Id: Ieb5ba2f90256c19e6f926d325d342a3b97b75c4c

---
+++ src/chromiumos/tast/remote/gaiaenrollment/helper.go
@@ -0,0 +1,11 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package gaiaenrollment
+
+// TestParams is a helper struct to keep parameters for gaia enrollment tests.
+type TestParams struct {
+	DMServer string // device management server url
+	PoolID   string // poolID for the used test account
+}
