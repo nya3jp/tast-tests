73cd719ed13919505dec60bcf7030a41195acbde
Ren-Pei Zeng <kamesan@chromium.org>
2022-07-11 16:35:24 +0800

    camera: Add AutoFraming test
    Add camera.AutoFraming Tast test that exercises the auto-framing core
    pipeline.
    
    BUG=b:210929757
    TEST=tast run $DUT camera.AutoFraming
    
    Cq-Depend: chromium:3804407, chromium:3804408, chromium:3807219
    Cq-Depend: chromium:3807200
    Change-Id: I9bc6b7a8cb0834a5bee949f0c2037fa3caf03477

---
+++ src/chromiumos/tast/local/bundles/cros/camera/auto_framing.go
@@ -0,0 +1,39 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/local/gtest"
+	"chromiumos/tast/testing"
+)
+
+const testImageFile = "person_4032x3024.nv12"
+const gtestExecutable = "auto_framing_test"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AutoFraming,
+		Desc:         "Auto-framing core pipeline unit test",
+		Contacts:     []string{"kamesan@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"camera_feature_auto_framing"},
+		Data:         []string{testImageFile, testImageFile + ".json"},
+		Timeout:      4 * time.Minute,
+	})
+}
+
+func AutoFraming(ctx context.Context, s *testing.State) {
+	if _, err := gtest.New(
+		gtestExecutable,
+		gtest.Logfile(filepath.Join(s.OutDir(), gtestExecutable+".log")),
+		gtest.ExtraArgs("--test_image_path="+s.DataPath(testImageFile)),
+	).Run(ctx); err != nil {
+		s.Errorf("Failed to run %v: %v", gtestExecutable, err)
+	}
+}
