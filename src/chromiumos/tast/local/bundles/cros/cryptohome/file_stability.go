fdcf4e2fbd70d5181270a7cc2961de55b880e433
Ian Barkley-Yeung <iby@chromium.org>
2022-07-11 11:31:39 -0700

    Promote cryptohome.FileStability
    There are several errors on tastboard, but they all appear to be tast
    framework issues, not issues with the test.
    
    BUG=None
    TEST=CQ
    
    Change-Id: I7139d90315cb1cc1e4ee44f440d111448a5570a0

--- src/chromiumos/tast/local/bundles/cros/cryptohome/file_stability.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/file_stability.go
@@ -30,7 +30,7 @@ func init() {
 		Desc:         "Checks that the cryptohome is stable after login",
 		Contacts:     []string{"iby@chromium.org", "chromeos-security@google.com"},
 		SoftwareDeps: []string{"chrome"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		Pre:          chrome.LoggedIn(),
 		Params: []testing.Param{{
 			Name: "normal",
