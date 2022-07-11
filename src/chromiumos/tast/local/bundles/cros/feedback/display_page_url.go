bfa091b2c11c56f09ad4ea03bc499588f897918f
wenyu zhang <zhangwenyu@google.com>
2022-07-11 19:34:50 +0000

    feedback: Test displaying page url
    Verifying the url matches the current website from where
    user opens the Feedback app.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.DisplayPageURL
    
    Change-Id: I6d24aeaebfe0217288aa3cd06d9e6be3c31f3837

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/display_page_url.go
@@ -0,0 +1,100 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DisplayPageURL,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verify url matches the current website where Feedback app is opened",
+		Contacts: []string{
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// DisplayPageURL verifies the url matches the current website from where
+// user opens the Feedback app.
+func DisplayPageURL(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	s.Log("Setting up chrome")
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("OsFeedback"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
+
+	// Setting up keyboard.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	// Opening chrome browser.
+	if err := apps.Launch(ctx, tconn, apps.Chrome.ID); err != nil {
+		s.Fatal("Failed to launch chrome app: ", err)
+	}
+
+	if err := ash.WaitForApp(ctx, tconn, apps.Chrome.ID, time.Minute); err != nil {
+		s.Fatal("Chrome app did not appear in shelf after launch: ", err)
+	}
+
+	url := "www.google.com"
+
+	// Enter url.
+	if err := kb.Type(ctx, url); err != nil {
+		s.Fatal("Failed to enter url: ", err)
+	}
+
+	if err := kb.Accel(ctx, "Enter"); err != nil {
+		s.Fatal("Failed to enter: ", err)
+	}
+
+	// Launching feedback app and go to share data page.
+	feedbackRootNode, err := feedbackapp.LaunchAndGoToShareDataPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and go to share data page: ", err)
+	}
+
+	// Verifying url text exist.
+	urlText := nodewith.NameContaining(url).Role(role.StaticText).Ancestor(feedbackRootNode)
+	if err := ui.WaitUntilExists(urlText)(ctx); err != nil {
+		s.Fatal("Failed to find element: ", err)
+	}
+}
