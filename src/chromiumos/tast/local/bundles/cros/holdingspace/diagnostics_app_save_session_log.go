ba9b4172e1e787e614954e596c07a43153ea16a2
Angel <angelsan@google.com>
2022-07-11 23:07:57 +0000

    tast-test: Check if saving a session log file from Diagnostics app appears in holding space.
    BUG=b:200987266
    TEST=tast run <test-device-ip> holdingspace.DiagnosticsAppSaveSessionLog
    
    Change-Id: I13936572329337178a9e8b853a9bb80213e91886

--- src/chromiumos/tast/local/bundles/cros/holdingspace/diagnostics_app_save_session_log.go
+++ src/chromiumos/tast/local/bundles/cros/holdingspace/diagnostics_app_save_session_log.go
@@ -26,7 +26,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         DiagnosticsAppSaveSessionLog,
 		LacrosStatus: testing.LacrosVariantUnneeded,
-		Desc:         "Checks that saving session log from Diagnostics App appears in Holding Space",
+		Desc:         "Checks that session logs saved from Diagnostics App appears in Holding Space",
 		Contacts: []string{
 			"angelsan@chromium.org",
 			"dmblack@chromium.org",
@@ -40,7 +40,7 @@ func init() {
 }
 
 // DiagnosticsAppSaveSessionLog tests the functionality of files existing in Holding Space by
-// saving a session log file from the Diagnostics app.
+// saving a session log from the Diagnostics app.
 func DiagnosticsAppSaveSessionLog(ctx context.Context, s *testing.State) {
 	cr := s.PreValue().(*chrome.Chrome)
 
@@ -53,7 +53,7 @@ func DiagnosticsAppSaveSessionLog(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to create Test API connection: ", err)
 	}
 
-	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree_diagnostics_app_save_session_log")
 
 	// Reset the holding space.
 	if err := holdingspace.ResetHoldingSpace(ctx, tconn,
@@ -61,18 +61,16 @@ func DiagnosticsAppSaveSessionLog(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to reset holding space: ", err)
 	}
 
-	// Check if file already exist.
-	const filename = "session_log.txt"
+	// Ensure session log does not exist.
 	downloadsPath, err := cryptohome.DownloadsPath(ctx, cr.NormalizedUser())
 	if err != nil {
 		s.Fatal("Failed to get users Download path: ", err)
 	}
+	const filename = "session_log.txt"
 	filePath := filepath.Join(downloadsPath, filename)
 	if _, err := os.Stat(filePath); err == nil {
-		// File already exists. Remove it.
 		os.Remove(filePath)
 	}
-	// Remove session_log.txt file that will later be created.
 	defer os.Remove(filePath)
 
 	dxRootnode, err := diagnosticsapp.Launch(ctx, tconn)
@@ -80,20 +78,12 @@ func DiagnosticsAppSaveSessionLog(ctx context.Context, s *testing.State) {
 		s.Fatal("Could not open diagnostics app: ", err)
 	}
 
-	// Find Save test details button.
-	// Click Save test details button.
-	// Find Save button and click Save button.
-	// Click holding space tray.
-	// Verify file appears in holding space.
 	ui := uiauto.New(tconn)
-	saveTestDetailsButton := diagnosticsapp.DxLogButton.Ancestor(dxRootnode)
-	if err := uiauto.Combine("Save session_log.txt file and verify file appears in holding space",
-		ui.LeftClick(saveTestDetailsButton),
+	uiauto.Combine("Save session log and verify file appears in holding space",
+		ui.LeftClick(diagnosticsapp.DxLogButton.Ancestor(dxRootnode)),
 		ui.LeftClick(nodewith.Name("Save").Role(role.Button)),
 		ui.LeftClick(holdingspace.FindTray()),
 		ui.WaitUntilExists(holdingspace.FindDownloadChip().Name(filename)),
-	)(ctx); err != nil {
-		s.Fatal("Could not verify session_log.txt in holding space: ", err)
-	}
+	)(ctx)
 
 }
