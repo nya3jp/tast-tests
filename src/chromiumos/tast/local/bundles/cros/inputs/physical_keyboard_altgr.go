912d0172bd928c13eacac28f57690c1e9a5d93ab
jhtin <jhtin@chromium.org>
2022-07-11 10:31:55 +1000

    Added a new tast test for altgr input on browsers.
    BUG=b:237498932
    TEST=CQ
    
    Change-Id: Ie351ddaf9580ff989dcbc06ccd2f2dd61ecf55c6

---
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_altgr.go
@@ -0,0 +1,149 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package inputs
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/inputs/fixture"
+	"chromiumos/tast/local/bundles/cros/inputs/pre"
+	"chromiumos/tast/local/bundles/cros/inputs/testserver"
+	"chromiumos/tast/local/bundles/cros/inputs/util"
+	"chromiumos/tast/local/chrome/ime"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/useractions"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PhysicalKeyboardAltgr,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Checks that user can lock altgr modifier key on physical keyboard",
+		Contacts:     []string{"shengjun@chromium.org", "essential-inputs-team@google.com"},
+		Attr:         []string{"group:mainline", "group:input-tools"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		SearchFlags:  util.IMESearchFlags([]ime.InputMethod{ime.DefaultInputMethod}),
+		Timeout:      2 * time.Minute,
+		Params: []testing.Param{
+			{
+				Fixture:           fixture.ClamshellNonVK,
+				ExtraAttr:         []string{"informational", "group:input-tools-upstream"},
+				ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
+			},
+			{
+				Name:              "informational",
+				Fixture:           fixture.ClamshellNonVK,
+				ExtraAttr:         []string{"informational"},
+				ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
+			},
+			{
+				Name:              "lacros",
+				Fixture:           fixture.LacrosClamshellNonVK,
+				ExtraSoftwareDeps: []string{"lacros"},
+				ExtraAttr:         []string{"informational"},
+				ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
+			},
+		},
+	})
+}
+
+func PhysicalKeyboardAltgr(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(fixture.FixtData).Chrome
+	tconn := s.FixtValue().(fixture.FixtData).TestAPIConn
+	uc := s.FixtValue().(fixture.FixtData).UserContext
+
+	testCases := []struct {
+		inputMethod         ime.InputMethod
+		typeAction          string
+		expectedText        string
+		expectedShiftedText string
+	}{
+		// TODO(b/237498932): Add other keyboards listed in bug report.
+		{
+			inputMethod:         ime.EnglishUSWithInternationalKeyboard,
+			typeAction:          "abcdefghijklmnopqrstuvwxyz0123456789",
+			expectedText:        "áb©ðéfghíjœøµñóöä®ßþúvåxüæ’¡²³¤€¼½¾‘",
+			expectedShiftedText: "ÁB¢ÐÉFGHÍJŒØµÑÓÖÄ®§ÞÚVÅXÜÆ£",
+		},
+		{
+			inputMethod:         ime.Swedish,
+			typeAction:          "abcdefghijklmnopqrstuvwxyz0123456789",
+			expectedText:        "ª”©ð€đŋħ→łµnœþ@®ßþ↓“ł»←«}¡@£$€¥{[]",
+			expectedShiftedText: "º’©Ð¢ªŊĦıŁºNŒÞΩ®§Þ↑‘Ł>¥<°¹²³¼¢⅝÷«»",
+		},
+	}
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree")
+
+	keyboard, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer keyboard.Close()
+
+	its, err := testserver.LaunchBrowser(ctx, s.FixtValue().(fixture.FixtData).BrowserType, cr, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch inputs test server: ", err)
+	}
+	inputField := testserver.TextAreaInputField
+
+	defer its.CloseAll(cleanupCtx)
+
+	for _, testcase := range testCases {
+		name := "PKAltgrModifierWorksFor" + testcase.inputMethod.ShortLabel
+		scenario := "PK Altgr Modifier Works For " + testcase.inputMethod.Name
+
+		s.Run(ctx, name, func(ctx context.Context, s *testing.State) {
+			// Reset Altgr, in case Altgr is in a held-down state (if release action did not get run due to failures)
+			defer keyboard.AccelAction("Altgr")(cleanupCtx)
+			defer keyboard.AccelAction("Shift")(cleanupCtx)
+
+			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+string(name))
+			im := testcase.inputMethod
+
+			s.Log("Set current input method to: ", im)
+			if err := im.InstallAndActivateUserAction(uc)(ctx); err != nil {
+				s.Fatalf("Failed to set input method to %v: %v: ", im, err)
+			}
+
+			if err := uiauto.UserAction(scenario,
+				uiauto.Combine(scenario,
+					its.Clear(inputField),
+					its.ClickFieldAndWaitForActive(inputField),
+					keyboard.AccelPressAction("Altgr"),
+					keyboard.TypeAction(testcase.typeAction),
+					util.WaitForFieldTextToBe(tconn, inputField.Finder(), testcase.expectedText),
+					its.Clear(inputField),
+					its.ClickFieldAndWaitForActive(inputField),
+					keyboard.AccelPressAction("Shift"),
+					keyboard.TypeAction(testcase.typeAction),
+					util.WaitForFieldTextToBe(tconn, inputField.Finder(), testcase.expectedShiftedText),
+					keyboard.AccelReleaseAction("Altgr"),
+					keyboard.AccelReleaseAction("Shift"),
+				),
+				uc,
+				&useractions.UserActionCfg{
+					Attributes: map[string]string{
+						useractions.AttributeTestScenario: scenario,
+						useractions.AttributeFeature:      useractions.FeaturePKTyping,
+						useractions.AttributeInputMethod:  im.Name,
+					},
+				},
+			)(ctx); err != nil {
+				s.Fatal("Failed to validate altgr: ", err)
+			}
+		})
+	}
+}
