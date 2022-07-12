15b6dda7d3abb6e59d1f924cafe0a1092c2fc802
jhtin <jhtin@chromium.org>
2022-07-12 15:03:29 +1000

    Added altgr test cases for Norwegian/Polish/UK/Dutch
    BUG=b:237498932
    TEST=CQ
    
    Change-Id: I62860f3951089b04570d161766391912ec128b64

--- src/chromiumos/tast/local/chrome/ime/input_method.go
+++ src/chromiumos/tast/local/chrome/ime/input_method.go
@@ -299,6 +299,22 @@ var NepaliTransliteration = InputMethod{
 	VoiceLanguage:       LanguageNe,
 }
 
+// DutchNetherlands represents the input method of Dutch (Netherlands).
+var DutchNetherlands = InputMethod{
+	Name:                "Dutch (Netherlands)",
+	ID:                  "xkb:us:intl:nld",
+	HandwritingLanguage: LanguageNl,
+	VoiceLanguage:       LanguageNl,
+}
+
+// Norwegian represents the input method of Norwegian.
+var Norwegian = InputMethod{
+	Name:                "Norwegian",
+	ID:                  "xkb:no::nob",
+	HandwritingLanguage: LanguageNo,
+	VoiceLanguage:       LanguageNo,
+}
+
 // Odia represents the input method of Odia.
 var Odia = InputMethod{
 	Name:                "Odia",
@@ -313,6 +329,14 @@ var PersianTransliteration = InputMethod{
 	ID:                  "fa-t-i0-und",
 	HandwritingLanguage: LanguageFa,
 	VoiceLanguage:       LanguageFa,
+}
+
+// Polish represents the input method of Polish.
+var Polish = InputMethod{
+	Name:                "Polish",
+	ID:                  "xkb:pl::pol",
+	HandwritingLanguage: LanguagePl,
+	VoiceLanguage:       LanguagePl,
 }
 
 // Punjabi represents the input method of Punjabi.
@@ -364,7 +388,7 @@ var Urdu = InputMethod{
 }
 
 // inputMethods represents in-use (available) IMEs in ChromeOS.
-// Only listed input methods are promised to be available.
+// Any IMEs displayed in OS settings can be added to this list.
 var inputMethods = []InputMethod{
 	EnglishUS,
 	EnglishUSWithInternationalKeyboard,
@@ -386,6 +410,7 @@ var inputMethods = []InputMethod{
 	ChineseTraditionalPinyin,
 	ChineseWubi,
 	ChineseZhuyin,
+	DutchNetherlands,
 	Korean,
 	Arabic,
 	GreekTransliteration,
@@ -395,8 +420,10 @@ var inputMethods = []InputMethod{
 	Malayalam,
 	Marathi,
 	NepaliTransliteration,
+	Norwegian,
 	Odia,
 	PersianTransliteration,
+	Polish,
 	Punjabi,
 	Russian,
 	Sanskrit,
