52702b26332bb74cb57f814eae8b15b701acb66a
wenyu zhang <zhangwenyu@google.com>
2022-07-12 18:22:33 +0000

    feedback: Refactor using WaitUntilGone
    Refactor tast test using WaitUntilGone instead of Poll.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.SubmitFeedbackAndCloseApp
    
    Change-Id: Iac2dfb0ea37ffa98ebe49ca0e74432308d310b93

--- src/chromiumos/tast/local/bundles/cros/feedback/submit_feedback_and_close_app.go
+++ src/chromiumos/tast/local/bundles/cros/feedback/submit_feedback_and_close_app.go
@@ -9,10 +9,7 @@ import (
 	"time"
 
 	"chromiumos/tast/ctxutil"
-	"chromiumos/tast/errors"
-	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
@@ -57,21 +54,21 @@ func SubmitFeedbackAndCloseApp(ctx context.Context, s *testing.State) {
 	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
 		"ui_dump")
 
-	ui := uiauto.New(tconn)
+	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
 
-	// Launching feedback app and go to share data page.
+	// Launch feedback app and go to share data page.
 	feedbackRootNode, err := feedbackapp.LaunchAndGoToShareDataPage(ctx, tconn)
 	if err != nil {
 		s.Fatal("Failed to launch feedback app and navigating to share data page: ", err)
 	}
 
-	// Looking for send button and then submit the feedback.
+	// Find send button and then submit the feedback.
 	sendButton := nodewith.Name("Send").Role(role.Button).Ancestor(feedbackRootNode)
 	if err := ui.DoDefault(sendButton)(ctx); err != nil {
 		s.Fatal("Failed to submit feedback: ", err)
 	}
 
-	// Verifying essential elements exist in the share data page.
+	// Verify essential elements exist in the share data page.
 	title := nodewith.Name("Thanks for your feedback").Role(role.StaticText).Ancestor(
 		feedbackRootNode)
 	newReportButton := nodewith.Name("Send new report").Role(role.Button).Ancestor(
@@ -90,20 +87,13 @@ func SubmitFeedbackAndCloseApp(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to find element: ", err)
 	}
 
-	// Looking for done button and click.
+	// Click done button and verify feedback window is closed.
 	doneButton := nodewith.Name("Done").Role(role.Button).Ancestor(feedbackRootNode)
-	if err := ui.DoDefault(doneButton)(ctx); err != nil {
-		s.Fatal("Failed to find done button: ", err)
-	}
 
-	// Verifying feedback window is closed.
-	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		if err := ash.WaitForApp(ctx, tconn, apps.Feedback.ID, 10*time.Second); err == nil {
-			return errors.Wrap(err, "feedback app is not closed")
-		}
-
-		return nil
-	}, &testing.PollOptions{Timeout: time.Minute}); err != nil {
+	if err := uiauto.Combine("Verify feedback window is closed",
+		ui.DoDefault(doneButton),
+		ui.WaitUntilGone(feedbackRootNode),
+	)(ctx); err != nil {
 		s.Fatal("Failed to verify feedback window is closed: ", err)
 	}
 }
