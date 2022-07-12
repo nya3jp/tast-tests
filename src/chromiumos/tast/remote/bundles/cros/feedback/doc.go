fae179e8ea0474acd9144a6603cfda17f0bf8157
wenyu zhang <zhangwenyu@google.com>
2022-07-12 19:26:42 +0000

    feedback: Test launching feedback from power button
    Verify user can press power button, click feedback button
    and launch the feedback app. Verify essential elements
    exist in feedback app.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.LaunchFeedbackFromPowerButton
    
    Change-Id: Ib993e0be71dea9b9c5bec8b78db0a6921e6319c3

---
+++ src/chromiumos/tast/remote/bundles/cros/feedback/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package feedback contains tests that verify user interaction with
+// feedback app.
+package feedback
