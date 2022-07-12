495555b2b8aa761505d6b8ddfe6a66977927dc2c
Patryk Duda <pdk@semihalf.com>
2022-07-12 18:08:53 +0200

    firmware_test_handler: Sync filesystem on enabling/disabling FP updater
    Under some circumstances Zork board doesn't preserve changes that the
    test are making to disable FP updater. When the test reboots DUT for the
    first time after disabling rootfs verification the changes could be
    lost.
    
    The patch adds running 'sync' after disabling/enabling FP updater which
    solves the problem.
    
    BUG=b:233277071
    TEST=Install ChromeOS zork-arc-r canary-channel image on Morphius.
         Run 'tast run <IP> firmware.FpAddEntropy'. Make sure test passes.
    TEST=TAST unit test passed.
    
    Change-Id: Ia97bbc04cb640606ba91674245eb8313debc6a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3758128
    Tested-by: Patryk Duda <patrykd@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Patryk Duda <patrykd@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- src/chromiumos/tast/remote/firmware/fingerprint/firmware_test_handler.go
+++ src/chromiumos/tast/remote/firmware/fingerprint/firmware_test_handler.go
@@ -17,6 +17,7 @@ import (
 	"chromiumos/tast/remote/sysutil"
 	"chromiumos/tast/rpc"
 	"chromiumos/tast/services/cros/platform"
+	"chromiumos/tast/ssh"
 	"chromiumos/tast/testing"
 )
 
@@ -369,12 +370,28 @@ func isFPUpdaterEnabled(ctx context.Context, d *rpcdut.RPCDUT) (bool, error) {
 func enableFPUpdater(ctx context.Context, d *rpcdut.RPCDUT) error {
 	fs := dutfs.NewClient(d.RPC().Conn)
 	testing.ContextLog(ctx, "Enabling the fingerprint updater")
-	return fs.Remove(ctx, filepath.Join(fingerprintFirmwarePathBase, disableFpUpdaterFile))
+	disableFpUpdaterPath := filepath.Join(fingerprintFirmwarePathBase, disableFpUpdaterFile)
+	if err := fs.Remove(ctx, disableFpUpdaterPath); err != nil {
+		return errors.Wrapf(err, "failed to remove %q", disableFpUpdaterPath)
+	}
+	// Sync filesystem to make sure that FP updater is enabled correctly.
+	if err := d.Conn().CommandContext(ctx, "sync").Run(ssh.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "failed to sync DUT")
+	}
+	return nil
 }
 
 // disableFPUpdater disables the fingerprint updater if it is enabled.
 func disableFPUpdater(ctx context.Context, d *rpcdut.RPCDUT) error {
 	fs := dutfs.NewClient(d.RPC().Conn)
 	testing.ContextLog(ctx, "Disabling the fingerprint updater")
-	return fs.WriteFile(ctx, filepath.Join(fingerprintFirmwarePathBase, disableFpUpdaterFile), nil, 0)
+	disableFpUpdaterPath := filepath.Join(fingerprintFirmwarePathBase, disableFpUpdaterFile)
+	if err := fs.WriteFile(ctx, disableFpUpdaterPath, nil, 0); err != nil {
+		return errors.Wrapf(err, "failed to create %q", disableFpUpdaterPath)
+	}
+	// Sync filesystem to make sure that FP updater is disabled correctly.
+	if err := d.Conn().CommandContext(ctx, "sync").Run(ssh.DumpLogOnError); err != nil {
+		return errors.Wrap(err, "failed to sync DUT")
+	}
+	return nil
 }
