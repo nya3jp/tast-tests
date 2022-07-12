c92a8941e8b6b4dac395713a6a0cd6466fb5e6d6
Chung-Sheng Wu <chungsheng@google.com>
2022-07-12 17:53:54 +0800

    mojo: Add tast to test service manager policy
    Add new tast test to verify the policy files of mojo service manager.
    
    BUG=b:236798109
    TEST=tast run $DUT mojo.ServiceManagerPolicy
    
    Change-Id: Ifeefa287e12c06c0279745f63e268580e37e87c5

---
+++ src/chromiumos/tast/local/bundles/cros/mojo/service_manager_policy.go
@@ -0,0 +1,37 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package mojo tests the system daemon mojo_service_manager to verify the mojo
+// functionality.
+package mojo
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ServiceManagerPolicy,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Check the policy files of mojo service manager",
+		Contacts: []string{
+			"chromeos-mojo-service-manager@google.com",
+			"chungsheng@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"mojo_service_manager"},
+		Timeout:      1 * time.Minute,
+	})
+}
+
+func ServiceManagerPolicy(ctx context.Context, s *testing.State) {
+	cmd := testexec.CommandContext(ctx, "mojo_service_manager", "--log_level=-1", "--check_policy")
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		s.Error("Policy file validation failed: ", err)
+	}
+}
