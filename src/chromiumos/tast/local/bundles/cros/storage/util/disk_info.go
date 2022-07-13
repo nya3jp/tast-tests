7204dc0b7d5952d0cdd4a06f98a192916f6f8537
Daniil Lunev <dlunev@chromium.org>
2022-07-13 20:39:52 +1000

    storage: Expect zeroes after trim on UFS in StorageQual
    BUG=b:228796927
    TEST=$ tast -verbose run -var=tast_disk_size_gb=128 192.168.1.15 storage.FullQualificationStress.functional
    
    Change-Id: I55962585f9c838c537083afeceeeb0d604c4ff4a

--- src/chromiumos/tast/local/bundles/cros/storage/util/disk_info.go
+++ src/chromiumos/tast/local/bundles/cros/storage/util/disk_info.go
@@ -120,6 +120,23 @@ func IsNVME(testPath string) bool {
 	return strings.Contains(testPath, "nvme")
 }
 
+// IsUFS returns whether the device is a UFS device.
+func IsUFS(testPath string) bool {
+	if !strings.Contains(testPath, "/dev/sd") {
+		return false
+	}
+
+	// Extract "sdX" part out of the testPath.
+	dev := strings.Split(testPath, "/")[2][0:3]
+
+	// UFS device should have a unit descriptor directory in sysfs.
+	target := "/sys/block/" + dev + "/device/unit_descriptor"
+	if _, err := os.Stat(target); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
 // GetNVMEIdNSFeature returns the feature value for the NVMe disk using
 // nvme id-ns.
 func GetNVMEIdNSFeature(ctx context.Context, diskPath, feature string) (string, error) {
