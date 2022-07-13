7bfa573ac224e472c74ae4e10212330d061f7bdb
Xiyuan Xia <xiyuan@google.com>
2022-07-13 10:51:01 -0700

    tast-tests: Add 3 and 4 windows cases to OverviewPerf
    Add 3 and 4 windows cases to ui.OverviewPerf to cover 90% and 95% users.
    
    BUG=b:236862486
    TEST=tast run $DUT ui.OverviewPerf*
    
    Change-Id: Id4866fb9017503e9a0b803f322a8a07f968ae2cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3760782
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Avery Musbach <amusbach@chromium.org>
    Tested-by: Xiyuan Xia <xiyuan@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/ui/overview_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/overview_perf.go
@@ -6,6 +6,7 @@ package ui
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"time"
@@ -35,18 +36,18 @@ func init() {
 		Params: []testing.Param{{
 			Val:     browser.TypeAsh,
 			Fixture: "chromeLoggedIn",
-			Timeout: 7 * time.Minute,
+			Timeout: 14 * time.Minute,
 		}, {
 			Name:    "skia_renderer",
 			Val:     browser.TypeAsh,
 			Fixture: "chromeLoggedInWith100FakeAppsSkiaRenderer",
-			Timeout: 7 * time.Minute,
+			Timeout: 14 * time.Minute,
 		}, {
 			Name:              "lacros",
 			Val:               browser.TypeLacros,
 			Fixture:           "lacros",
 			ExtraSoftwareDeps: []string{"lacros"},
-			Timeout:           10 * time.Minute,
+			Timeout:           20 * time.Minute,
 		}},
 		Data: []string{"animation.html", "animation.js"},
 	})
@@ -57,11 +58,6 @@ func OverviewPerf(ctx context.Context, s *testing.State) {
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
 	defer cancel()
-
-	// Ensure display on to record ui performance correctly.
-	if err := power.TurnOnDisplay(ctx); err != nil {
-		s.Fatal("Failed to turn on display: ", err)
-	}
 
 	cr, l, cs, err := lacros.Setup(ctx, s.FixtValue(), s.Param().(browser.Type))
 	if err != nil {
@@ -89,125 +85,17 @@ func OverviewPerf(ctx context.Context, s *testing.State) {
 	runner := perfutil.NewRunner(cr.Browser())
 	currentWindows := 0
 	// Run the overview mode enter/exit flow for various situations.
-	// - change the number of browser windows, 2 or 8
+	// - change the number of browser windows,
 	// - the window system status; clamshell mode with maximized windows,
 	//   clamshell mode with normal windows, tablet mode with maximized
 	//   windows, tablet mode with minimized windows (the home screen),
 	//   tablet split view with maximized overview windows, or tablet
 	//   split view with minimized overview windows.
-	for i, test := range []struct {
-		fullDescription     string
-		windowsDescription  string
-		windows             int
-		tablet              bool
-		overviewWindowState ash.WindowStateType
-		splitView           bool
-		histograms          []string
-	}{{
-		fullDescription:     "SingleClamshellMode-2windows",
-		windowsDescription:  "2windows",
-		windows:             2,
-		tablet:              false,
-		overviewWindowState: ash.WindowStateMaximized,
-		splitView:           false,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.SingleClamshellMode",
-			"Ash.Overview.AnimationSmoothness.Exit.SingleClamshellMode"},
-	}, {
-		fullDescription:     "ClamshellMode-2windows",
-		windowsDescription:  "2windows",
-		windows:             2,
-		tablet:              false,
-		overviewWindowState: ash.WindowStateNormal,
-		splitView:           false,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.ClamshellMode",
-			"Ash.Overview.AnimationSmoothness.Exit.ClamshellMode"},
-	}, {
-		fullDescription:     "TabletMode-2windows",
-		windowsDescription:  "2windows",
-		windows:             2,
-		tablet:              true,
-		overviewWindowState: ash.WindowStateMaximized,
-		splitView:           false,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.TabletMode",
-			"Ash.Overview.AnimationSmoothness.Exit.TabletMode"},
-	}, {
-		fullDescription:     "MinimizedTabletMode-2windows",
-		windowsDescription:  "2windows",
-		windows:             2,
-		tablet:              true,
-		overviewWindowState: ash.WindowStateMinimized,
-		splitView:           false,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.MinimizedTabletMode",
-			"Ash.Overview.AnimationSmoothness.Exit.MinimizedTabletMode"},
-	}, {
-		fullDescription:     "SplitView-2windows",
-		windowsDescription:  "2windows",
-		windows:             2,
-		tablet:              true,
-		overviewWindowState: ash.WindowStateMaximized,
-		splitView:           true,
-		// The overview exit animation does not include the window being activated.
-		// Thus, the SplitView-2windows case has no overview exit animation at all.
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.SplitView"},
-	}, {
-		fullDescription:     "SingleClamshellMode-8windows",
-		windowsDescription:  "8windows",
-		windows:             8,
-		tablet:              false,
-		overviewWindowState: ash.WindowStateMaximized,
-		splitView:           false,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.SingleClamshellMode",
-			"Ash.Overview.AnimationSmoothness.Exit.SingleClamshellMode"},
-	}, {
-		fullDescription:     "ClamshellMode-8windows",
-		windowsDescription:  "8windows",
-		windows:             8,
-		tablet:              false,
-		overviewWindowState: ash.WindowStateNormal,
-		splitView:           false,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.ClamshellMode",
-			"Ash.Overview.AnimationSmoothness.Exit.ClamshellMode"},
-	}, {
-		fullDescription:     "TabletMode-8windows",
-		windowsDescription:  "8windows",
-		windows:             8,
-		tablet:              true,
-		overviewWindowState: ash.WindowStateMaximized,
-		splitView:           false,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.TabletMode",
-			"Ash.Overview.AnimationSmoothness.Exit.TabletMode"},
-	}, {
-		fullDescription:     "MinimizedTabletMode-8windows",
-		windowsDescription:  "8windows",
-		windows:             8,
-		tablet:              true,
-		overviewWindowState: ash.WindowStateMinimized,
-		splitView:           false,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.MinimizedTabletMode",
-			"Ash.Overview.AnimationSmoothness.Exit.MinimizedTabletMode"},
-	}, {
-		fullDescription:     "SplitView-8windowsincludingmaximizedoverviewwindows",
-		windowsDescription:  "8windowsincludingmaximizedoverviewwindows",
-		windows:             8,
-		tablet:              true,
-		overviewWindowState: ash.WindowStateMaximized,
-		splitView:           true,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.SplitView",
-			"Ash.Overview.AnimationSmoothness.Exit.SplitView"},
-	}, {
-		fullDescription:     "SplitView-8windowsincludingminimizedoverviewwindows",
-		windowsDescription:  "8windowsincludingminimizedoverviewwindows",
-		windows:             8,
-		tablet:              true,
-		overviewWindowState: ash.WindowStateMinimized,
-		splitView:           true,
-		histograms: []string{"Ash.Overview.AnimationSmoothness.Enter.SplitView",
-			"Ash.Overview.AnimationSmoothness.Exit.SplitView"},
-	}} {
+	for i, windows := range []int{2, 3, 4, 8} {
 		// This assumes that the test scenarios are sorted by
 		// number of windows. If not, then this will generate
 		// Panic: runtime error: makeslice: cap out of range
-		if err := ash.CreateWindows(ctx, tconn, cs, url, test.windows-currentWindows); err != nil {
+		if err := ash.CreateWindows(ctx, tconn, cs, url, windows-currentWindows); err != nil {
 			s.Fatal("Failed to create browser windows: ", err)
 		}
 
@@ -218,71 +106,145 @@ func OverviewPerf(ctx context.Context, s *testing.State) {
 			}
 		}
 
-		currentWindows = test.windows
+		currentWindows = windows
 
-		// Here we try to set tablet mode enabled/disabled,
-		// but keep in mind that the tests run on devices
-		// that do not support tablet mode. Thus, in case
-		// of an error, we skip to the next scenario.
-		if err := ash.SetTabletModeEnabled(ctx, tconn, test.tablet); err != nil {
-			s.Logf("Skipping the case of %s as it failed to set tablet mode %v: %v", test.fullDescription, test.tablet, err)
+		windowsDescription := fmt.Sprintf("%dwindows", windows)
+		for _, test := range []struct {
+			fullDescriptionFmt  string
+			tablet              bool
+			overviewWindowState ash.WindowStateType
+			histogramSuffix     string
+		}{
+			{"SingleClamshellMode-%dwindows", false, ash.WindowStateMaximized, "SingleClamshellMode"},
+			{"ClamshellMode-%dwindows", false, ash.WindowStateNormal, "ClamshellMode"},
+			{"TabletMode-%dwindows", true, ash.WindowStateMaximized, "TabletMode"},
+			{"MinimizedTabletMode-%dwindows", true, ash.WindowStateMinimized, "MinimizedTabletMode"},
+		} {
+			fullDescription := fmt.Sprintf(test.fullDescriptionFmt, windows)
+			if err := doTestCase(
+				ctx, s, tconn, runner, fullDescription, windowsDescription, test.tablet, test.overviewWindowState,
+				false /*splitview*/, "Ash.Overview.AnimationSmoothness.Enter."+test.histogramSuffix,
+				"Ash.Overview.AnimationSmoothness.Exit."+test.histogramSuffix,
+			); err != nil {
+				s.Fatalf("Test case %q failed: %s", fullDescription, err)
+			}
+		}
+
+		if windows == 2 {
+			// The overview exit animation does not include the window being activated.
+			// Thus, the SplitView-2windows case has no overview exit animation at all.
+			if err := doTestCase(
+				ctx, s, tconn, runner, "SplitView-2windows", "2windows", true /*tablet*/, ash.WindowStateMaximized,
+				true /*splitview*/, "Ash.Overview.AnimationSmoothness.Enter.SplitView",
+			); err != nil {
+				s.Fatal("Test case \"SplitView-2windows\" failed: ", err)
+			}
 			continue
 		}
 
-		// Set all windows to the state that
-		// we want for the overview windows.
-		if err := ash.ForEachWindow(ctx, tconn, func(w *ash.Window) error {
-			return ash.SetWindowStateAndWait(ctx, tconn, w.ID, test.overviewWindowState)
-		}); err != nil {
-			s.Fatalf("Failed to set all windows to state %v: %v", test.overviewWindowState, err)
-		}
-
-		// For tablet split view scenarios, snap
-		// a window and then exit overview.
-		if test.splitView {
-			ws, err := ash.GetAllWindows(ctx, tconn)
-			if err != nil {
-				s.Fatal("Failed to get windows: ", err)
-			}
-
-			if len(ws) == 0 {
-				s.Fatal("Found no windows")
-			}
-
-			if err := ash.SetWindowStateAndWait(ctx, tconn, ws[0].ID, ash.WindowStateLeftSnapped); err != nil {
-				s.Fatal("Failed to snap window: ", err)
-			}
-
-			if err := ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
-				s.Fatal("Failed to exit overview: ", err)
+		for _, test := range []struct {
+			fullDescriptionFmt    string
+			windowsDescriptionFmt string
+			overviewWindowState   ash.WindowStateType
+		}{
+			{"SplitView-%dwindowsincludingmaximizedoverviewwindows", "%dwindowsincludingmaximizedoverviewwindows", ash.WindowStateMaximized},
+			{"SplitView-%dwindowsincludingminimizedoverviewwindows", "%dwindowsincludingminimizedoverviewwindows", ash.WindowStateMinimized},
+		} {
+			fullDescription := fmt.Sprintf(test.fullDescriptionFmt, windows)
+			windowsDescription := fmt.Sprintf(test.windowsDescriptionFmt, windows)
+			if err := doTestCase(
+				ctx, s, tconn, runner, fullDescription, windowsDescription, true /*tablet*/, test.overviewWindowState,
+				true /*splitview*/, "Ash.Overview.AnimationSmoothness.Enter.SplitView",
+				"Ash.Overview.AnimationSmoothness.Exit.SplitView",
+			); err != nil {
+				s.Fatalf("Test case %q failed: %s", fullDescription, err)
 			}
 		}
-
-		// Wait for 3 seconds to stabilize the result. Note that this doesn't
-		// have to be cpu.WaitUntilIdle(). It may wait too much.
-		// TODO(mukai): find the way to wait more properly on the idleness of Ash.
-		// https://crbug.com/1001314.
-		if err := testing.Sleep(ctx, 3*time.Second); err != nil {
-			s.Fatal("Failed to wait: ", err)
-		}
-
-		runner.RunMultiple(ctx, s, test.fullDescription,
-			perfutil.RunAndWaitAll(tconn,
-				func(ctx context.Context) error {
-					if err := ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
-						return errors.Wrap(err, "failed to enter into the overview mode")
-					}
-					if err := ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
-						return errors.Wrap(err, "failed to exit from the overview mode")
-					}
-					return nil
-				},
-				test.histograms...,
-			),
-			perfutil.StoreAll(perf.BiggerIsBetter, "percent", test.windowsDescription))
 	}
 
 	if err := runner.Values().Save(ctx, s.OutDir()); err != nil {
 		s.Error("Failed saving perf data: ", err)
 	}
+}
+
+// doTestCase runs the overview entering/exiting test using the given arguments.
+func doTestCase(
+	ctx context.Context,
+	s *testing.State,
+	tconn *chrome.TestConn,
+	runner *perfutil.Runner,
+	fullDescription,
+	windowsDescription string,
+	tablet bool,
+	overviewWindowState ash.WindowStateType,
+	splitView bool,
+	histograms ...string,
+) error {
+	// Ensure display on to record ui performance correctly.
+	if err := power.TurnOnDisplay(ctx); err != nil {
+		return errors.Wrap(err, "failed to turn on display")
+	}
+
+	// Here we try to set tablet mode enabled/disabled,
+	// but keep in mind that the tests run on devices
+	// that do not support tablet mode. Thus, in case
+	// of an error, we skip to the next scenario.
+	if err := ash.SetTabletModeEnabled(ctx, tconn, tablet); err != nil {
+		testing.ContextLogf(ctx, "Skipping the case of %s as it failed to set tablet mode %v: %v", fullDescription, tablet, err)
+		return nil
+	}
+
+	// Set all windows to the state that
+	// we want for the overview windows.
+	if err := ash.ForEachWindow(ctx, tconn, func(w *ash.Window) error {
+		return ash.SetWindowStateAndWait(ctx, tconn, w.ID, overviewWindowState)
+	}); err != nil {
+		return errors.Wrapf(err, "failed to set all windows to state %v", overviewWindowState)
+	}
+
+	// For tablet split view scenarios, snap
+	// a window and then exit overview.
+	if splitView {
+		ws, err := ash.GetAllWindows(ctx, tconn)
+		if err != nil {
+			return errors.Wrap(err, "failed to get windows")
+		}
+
+		if len(ws) == 0 {
+			return errors.Wrap(err, "found no windows")
+		}
+
+		if err := ash.SetWindowStateAndWait(ctx, tconn, ws[0].ID, ash.WindowStateLeftSnapped); err != nil {
+			return errors.Wrap(err, "failed to snap window")
+		}
+
+		if err := ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
+			return errors.Wrap(err, "failed to exit overview")
+		}
+	}
+
+	// Wait for 3 seconds to stabilize the result. Note that this doesn't
+	// have to be cpu.WaitUntilIdle(). It may wait too much.
+	// TODO(mukai): find the way to wait more properly on the idleness of Ash.
+	// https://crbug.com/1001314.
+	if err := testing.Sleep(ctx, 3*time.Second); err != nil {
+		return errors.Wrap(err, "failed to wait")
+	}
+
+	runner.RunMultiple(ctx, s, fullDescription,
+		perfutil.RunAndWaitAll(tconn,
+			func(ctx context.Context) error {
+				if err := ash.SetOverviewModeAndWait(ctx, tconn, true); err != nil {
+					return errors.Wrap(err, "failed to enter into the overview mode")
+				}
+				if err := ash.SetOverviewModeAndWait(ctx, tconn, false); err != nil {
+					return errors.Wrap(err, "failed to exit from the overview mode")
+				}
+				return nil
+			},
+			histograms...,
+		),
+		perfutil.StoreAll(perf.BiggerIsBetter, "percent", windowsDescription))
+
+	return nil
 }
