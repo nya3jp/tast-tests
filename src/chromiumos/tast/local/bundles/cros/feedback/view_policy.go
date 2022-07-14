9520262d7e010f46984e6579250e795c15ef3f31
wenyu zhang <zhangwenyu@google.com>
2022-07-14 00:57:03 +0000

    feedback: Test user can view policy
    Verify user can view legal help page, privacy policy and
    terms of service in the share data page.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.ViewPolicy.*
    
    Change-Id: Idb5d2d22bc725b11e5f9d48f4ab5edfdfbb1b4f5

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/view_policy.go
@@ -0,0 +1,116 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+// testParam contains all the data needed to run a single test iteration.
+type testParam struct {
+	linkName    string
+	linkAddress string
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ViewPolicy,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "User is able to view policy, legal help and terms of service",
+		Contacts: []string{
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name: "legal_help_page",
+			Val: testParam{
+				linkName:    "Legal Help page",
+				linkAddress: "support.google.com/legal/answer/3110420",
+			},
+		}, {
+			Name: "privacy_policy",
+			Val: testParam{
+				linkName:    "Privacy Policy",
+				linkAddress: "policies.google.com/privacy",
+			},
+		}, {
+			Name: "terms_of_service",
+			Val: testParam{
+				linkName:    "Terms of Service",
+				linkAddress: "policies.google.com/terms",
+			},
+		}},
+	})
+}
+
+// ViewPolicy verifies the user is able to view policy, legal help and terms of service.
+func ViewPolicy(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	s.Log("Set up chrome")
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("OsFeedback"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn)
+
+	// Set up keyboard.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	// Launch feedback app and navigate to share data page.
+	feedbackRootNode, err := feedbackapp.LaunchAndGoToShareDataPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and go to share data page: ", err)
+	}
+
+	// Find link and click.
+	link := nodewith.NameContaining(s.Param().(testParam).linkName).Role(
+		role.Link).Ancestor(feedbackRootNode)
+	if err := ui.DoDefault(link)(ctx); err != nil {
+		s.Fatal("Failed to find link: ", err)
+	}
+
+	// Verify browser is opened.
+	if err = ash.WaitForApp(ctx, tconn, apps.Chrome.ID, time.Minute); err != nil {
+		s.Fatal("Could not find browser in shelf after launch: ", err)
+	}
+
+	// Verify browser contains correct address.
+	if err := nodewith.NameContaining(s.Param().(testParam).linkAddress).Role(
+		role.TextField); err == nil {
+		s.Fatal("Failed to find link address: ", err)
+	}
+}
