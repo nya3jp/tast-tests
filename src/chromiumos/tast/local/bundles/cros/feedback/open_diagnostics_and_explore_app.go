982eda58878b8e126e8cdfa76ed33d9f4aac24d0
wenyu zhang <zhangwenyu@google.com>
2022-07-14 23:08:15 +0000

    feedback: Test opening diagnostics and explore app
    Verify user can open diagnostics and explore app from
    feedback app.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.OpenDiagnosticsAndExploreApp.*
    
    Change-Id: Ida10b9b125737cba85439919e7a71845651e7b7a

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/open_diagnostics_and_explore_app.go
@@ -0,0 +1,96 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+// testParam contains all the data needed to run a single test iteration.
+type testParam struct {
+	appLinkName string
+	app         apps.App
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         OpenDiagnosticsAndExploreApp,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "User is able to open diagnostics and explore app",
+		Contacts: []string{
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name: "open_explore_app",
+			Val: testParam{
+				appLinkName: "Explore app",
+				app:         apps.Help,
+			},
+		}, {
+			Name: "open_diagnostics_app",
+			Val: testParam{
+				appLinkName: "Diagnostics app",
+				app:         apps.Diagnostics,
+			},
+		}},
+	})
+}
+
+// OpenDiagnosticsAndExploreApp verifies the user is able to open diagnostics and explore app.
+func OpenDiagnosticsAndExploreApp(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	s.Log("Setting up chrome")
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("OsFeedback"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn)
+
+	// Launch feedback app and navigate to confirmation page.
+	feedbackRootNode, err := feedbackapp.LaunchAndGoToConfirmationPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and go to confirmation page: ", err)
+	}
+
+	// Find app link and click.
+	appLink := nodewith.NameContaining(s.Param().(testParam).appLinkName).Role(
+		role.Link).Ancestor(feedbackRootNode).First()
+	if err := ui.DoDefault(appLink)(ctx); err != nil {
+		s.Fatal("Failed to find app link: ", err)
+	}
+
+	// Verify app is opened.
+	if err = ash.WaitForApp(ctx, tconn, s.Param().(testParam).app.ID, time.Minute); err != nil {
+		s.Fatal("Could not find app in shelf after launch: ", err)
+	}
+}
