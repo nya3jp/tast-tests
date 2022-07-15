a39ec41df49bdb578361caedec7af066488762b9
Yuki Awano <yawano@google.com>
2022-07-15 16:37:17 -0700

    tast-tests: Fix assistant.Hotkey.*
    - Due to a bug in assistant.Hotkey.*, it was doing nothing previously.
    
    BUG=b:239242002
    TEST=tast run DUT assistant.Hotkey.*
    
    Change-Id: Iaa7eafcaecc53a71e1bce96a921bd57f72fac976

--- src/chromiumos/tast/local/bundles/cros/assistant/hotkey.go
+++ src/chromiumos/tast/local/bundles/cros/assistant/hotkey.go
@@ -6,7 +6,6 @@ package assistant
 
 import (
 	"context"
-	"time"
 
 	"chromiumos/tast/common/action"
 	"chromiumos/tast/local/assistant"
@@ -25,7 +24,6 @@ func init() {
 		Contacts:     []string{"yawano@google.com", "assistive-eng@google.com"},
 		SoftwareDeps: []string{"chrome", "chrome_internal"},
 		Fixture:      "assistant",
-		Timeout:      3 * time.Minute,
 		Params: []testing.Param{
 			{
 				Name:              "assistant_key",
@@ -52,12 +50,14 @@ func Hotkey(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to create test API connection: ", err)
 	}
 
-	assistantUI := nodewith.HasClass("AssistantPageView")
-	action.Combine("Press hotkey and confirm that it toggles Assistant UI visibility",
+	assistantUI := nodewith.HasClass("AssistantDialogPlate")
+	if err := action.Combine("Press hotkey and confirm that it toggles Assistant UI visibility",
 		uiauto.New(tconn).WaitUntilGone(assistantUI),
 		func(ctx context.Context) error { return assistant.ToggleUIWithHotkey(ctx, tconn, accel) },
 		uiauto.New(tconn).WaitUntilExists(assistantUI),
 		func(ctx context.Context) error { return assistant.ToggleUIWithHotkey(ctx, tconn, accel) },
 		uiauto.New(tconn).WaitUntilGone(assistantUI),
-	)
+	)(ctx); err != nil {
+		s.Fatal("Failed to toggle Assistant UI with a hotkey: ", err)
+	}
 }
