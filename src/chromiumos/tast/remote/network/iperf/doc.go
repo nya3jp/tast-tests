d98a0942a05c5cba32a72b3ade78ea5a8506f256
Jason Stanko <jstanko@google.com>
2022-07-15 22:20:01 +0000

    Iperf : initial implementation of Iperf
    Add libraries to wrap Iperf cli for cellular performance tests. Commands executed remotely via ssh.
    
    BUG=b:238343555
    TEST=cellular.Iperf(WiP)
    
    Change-Id: I9aaaf2e290740ce510b215e571a82ea875deb751

---
+++ src/chromiumos/tast/remote/network/iperf/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package iperf is used for running performance tests using linux Iperf suite.
+package iperf
