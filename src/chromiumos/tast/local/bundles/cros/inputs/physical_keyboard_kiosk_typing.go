c3bda8fc388c6211bd0864577ab38ac0959202a7
jhtin <jhtin@chromium.org>
2022-07-15 19:47:02 +1000

    inputs: Adding Physical Keyboard lacros + ash tast test in kiosk mode.
    Steps:
    1) Runs local e14s-test page server
    2) Loads the e14s-test page via a kiosk app that points to it.
     - lacros or ash depending on test name
    3) Types using PK.
    
    BUG=b:237498286
    TEST=tast run [dut ip] inputs.PhysicalKeyboardKioskTyping
    TEST=tast run [dut ip] inputs.PhysicalKeyboardKioskTyping.lacros
    
    Change-Id: I4c62853584592a7ca8da32726aede56c4f000f68

---
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_kiosk_typing.go
@@ -0,0 +1,137 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package inputs
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/inputs/inputactions"
+	"chromiumos/tast/local/bundles/cros/inputs/testserver"
+	"chromiumos/tast/local/bundles/cros/inputs/util"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ime"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/useractions"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/kioskmode"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PhysicalKeyboardKioskTyping,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Checks that user can type on physical keyboard in kiosk mode",
+		Contacts:     []string{"jhtin@chromium.org", "essential-inputs-team@google.com"},
+		Attr:         []string{"group:mainline", "group:input-tools"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		SearchFlags:  util.IMESearchFlags([]ime.InputMethod{ime.DefaultInputMethod}),
+		Timeout:      2 * time.Minute,
+		Params: []testing.Param{
+			{
+				Fixture:   fixture.KioskAutoLaunchCleanup,
+				ExtraAttr: []string{"informational", "group:input-tools-upstream"},
+				Val:       chrome.ExtraArgs(""),
+			},
+			{
+				Name:      "lacros",
+				Fixture:   fixture.KioskAutoLaunchCleanup,
+				ExtraAttr: []string{"informational", "group:input-tools-upstream"},
+				Val:       chrome.ExtraArgs("--enable-features=LacrosSupport,WebKioskEnableLacros", "--lacros-availability-ignore"),
+			},
+		},
+	})
+}
+
+func PhysicalKeyboardKioskTyping(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 15*time.Second)
+	defer cancel()
+
+	// Launches the server for e14s-test page.
+	server := testserver.LaunchServer(ctx)
+	defer server.Close()
+
+	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	// Creating a kiosk mode configuration that will launch an app that points to the e14s-test page.
+	webKioskAccountID := "arbitrary_id_web_kiosk_1@managedchrome.com"
+	webKioskAccountType := policy.AccountTypeKioskWebApp
+	webKioskTitle := "TastKioskModeSetByPolicyE14sPage"
+	webKioskURL := server.URL + "/e14s-test"
+	webKioskPolicy := policy.DeviceLocalAccountInfo{
+		AccountID:   &webKioskAccountID,
+		AccountType: &webKioskAccountType,
+		WebKioskAppInfo: &policy.WebKioskAppInfo{
+			Url:     &webKioskURL,
+			Title:   &webKioskTitle,
+			IconUrl: &webKioskURL,
+		}}
+
+	localAccountsConfiguration := &policy.DeviceLocalAccounts{
+		Val: []policy.DeviceLocalAccountInfo{
+			webKioskPolicy,
+		},
+	}
+
+	chromeOptions := s.Param().(chrome.Option)
+	kiosk, cr, err := kioskmode.New(
+		ctx,
+		fdms,
+		kioskmode.AutoLaunch(webKioskAccountID),
+		kioskmode.CustomLocalAccounts(localAccountsConfiguration),
+		kioskmode.ExtraChromeOptions(chromeOptions),
+	)
+	if err != nil {
+		s.Fatal("Failed to start Chrome in Kiosk mode: ", err)
+	}
+	defer kiosk.Close(cleanupCtx)
+
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree")
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to get Test API connection: ", err)
+	}
+
+	userContext, err := inputactions.NewInputsUserContextWithoutState(ctx, "", s.OutDir(), cr, tconn, nil)
+	if err != nil {
+		s.Fatal("Failed to create inputs user context: ", err)
+	}
+
+	keyboard, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer keyboard.Close()
+
+	ui := uiauto.New(tconn)
+	inputField := testserver.TextAreaInputField
+
+	actionName := "PK find inputfield and type"
+	if err := uiauto.UserAction(actionName,
+		uiauto.Combine(actionName,
+			ui.WaitUntilExists(inputField.Finder()),
+			ui.MakeVisible(inputField.Finder()),
+			ui.LeftClick(inputField.Finder()),
+			keyboard.TypeAction("abcdefghijklmnopqrstuvwxyz01234! ABCDEFGHIJKLMNOPQRSTUVWXYZ01234!"),
+			util.WaitForFieldTextToBe(tconn, inputField.Finder(), "abcdefghijklmnopqrstuvwxyz01234! ABCDEFGHIJKLMNOPQRSTUVWXYZ01234!"),
+		),
+		userContext,
+		&useractions.UserActionCfg{
+			Attributes: map[string]string{
+				useractions.AttributeFeature: useractions.FeaturePKTyping,
+			},
+		},
+	)(ctx); err != nil {
+		s.Fatal("Failed to validate typing output: ", err)
+	}
+}
