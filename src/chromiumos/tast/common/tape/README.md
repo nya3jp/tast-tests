87cd37dbbd90b950ec7f3bf790c927ea9bd25501
David Welling <davidwelling@google.com>
2022-07-15 13:15:27 -0700

    arcappgameperf: Support local logins for TAPE
    Add support for using a locally generated OAUTH token for authenticating
    with TAPE, and support copying service accounts from remote bots to the
    DUT in order to use service account credentials when running in the lab.
    
    BUG=chromium:1339418
    TEST=./fast_build.sh -T
    
    Cq-Depend: chrome-internal:4880344
    Change-Id: Id00bb86f7cc64097cbea319f36fa94eee7f22968

---
+++ src/chromiumos/tast/common/tape/README.md
@@ -0,0 +1,29 @@
+# TAPE Security
+Note that TAPE requires local OAUTH for running locally, and uses known service
+accounts when running in the lab. Context for this can be found here:
+https://bugs.chromium.org/p/chromium/issues/detail?id=1339418. This is all
+handled by the TAPE logic but requires a few manual steps when running tests
+that use the TAPE service.
+
+## Generating a local refresh token
+All of this must be done outside of chroot. After the refresh token is generated,
+re-authentication should not be required. Note that parameters (denoted as
+`{{PARAM_NAME}}`) should be retrieved from https://source.corp.google.com/chromeos_internal/src/platform/tast-tests-private/vars/tape.yaml.
+
+1. In a browser, navigate to:
+   1. https://accounts.google.com/o/oauth2/auth?client_id={{CLIENT_ID}}&response_type=code&scope=openid%20email&access_type=offline&redirect_uri=urn:ietf:wg:oauth:2.0:oob
+2. Authenticate and copy the retrieved code. This will be referred to as {{AUTH_CODE}}
+3. Run:
+```shell
+curl --verbose \
+	      --data client_id={{CLIENT_ID}} \
+	      --data client_secret={{CLIENT_SECRET}} \
+	      --data code={{AUTH_CODE}} \
+	      --data redirect_uri=urn:ietf:wg:oauth:2.0:oob \
+	      --data grant_type=authorization_code \
+	      https://oauth2.googleapis.com/token
+```
+4. Save the value of `refresh_token`. This will be used when running tests.
+
+## Testing locally
+1. Add `--var tape.local_refresh_token=<VALUE>` when running a test that uses TAPE.
--- src/chromiumos/tast/common/tape/helpers.go
+++ src/chromiumos/tast/common/tape/helpers.go
@@ -6,13 +6,44 @@ package tape
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/ssh/linuxssh"
+	"chromiumos/tast/testing"
 )
 
 // ServiceAccountVar holds the name of the variable which stores the service account credentials for TAPE.
 const ServiceAccountVar = "tape.service_account_key"
+
+// AuthenticationConfigJSONVar holds the name of the variable which stores
+// the authentication config variables for TAPE.
+const AuthenticationConfigJSONVar = "tape.authentication_config_json"
+
+// LocalRefreshTokenVar holds the name of the variable which stores the
+// local refresh token for TAPE authentication.
+const LocalRefreshTokenVar = "tape.local_refresh_token"
+
+// dutServiceAccountDir holds the path that service accounts from the host
+// will be stored on the DUT.
+const dutServiceAccountDir = "/tmp/tape/service_account"
+
+// AuthenticationConfig represents the data that's stored in the AuthenticationConfigJSONVar.
+type AuthenticationConfig struct {
+	ClientID                     string   `json:"client_id"`
+	ClientSecret                 string   `json:"client_secret"`
+	DroneServiceAccountLocations []string `json:"drone_service_account_locations"`
+}
+
+// serviceAccountPathOnDUT returns the path that a service account should be saved
+// to on the DUT given the name from the remote host.
+func serviceAccountPathOnDUT(pathOnHost string) string {
+	return fmt.Sprintf("%s/%s", dutServiceAccountDir, filepath.Base(pathOnHost))
+}
 
 // LeaseAccount leases an account from the given poolID for the provided amount of time and optionally locks it.
 func LeaseAccount(ctx context.Context, poolID string, leaseLength time.Duration, lockAccount bool, credsJSON []byte) (account *Account, cleanup func(ctx context.Context) error, err error) {
@@ -34,3 +65,88 @@ func LeaseAccount(ctx context.Context, poolID string, leaseLength time.Duration,
 		return nil
 	}, nil
 }
+
+// LeaseGenericAccount leases a generic account for the given pool.
+func LeaseGenericAccount(ctx context.Context, poolID string, leaseLength time.Duration, localRefreshToken string, authenticationConfigJSON []byte) (account *GenericAccount, cleanup func(ctx context.Context) error, err error) {
+	client, err := NewTapeClient(ctx, WithLocalAndRemoteAuthentication(localRefreshToken, authenticationConfigJSON))
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to setup TAPE client")
+	}
+
+	params := RequestGenericAccountParams{
+		TimeoutInSeconds: int32(leaseLength.Seconds()),
+		PoolID:           &poolID,
+	}
+
+	gar, err := RequestGenericAccount(ctx, params, client)
+	if err != nil {
+		return gar, nil, errors.Wrap(err, "failed to request generic account")
+	}
+
+	return gar, func(ctx context.Context) error {
+		if err := ReleaseGenericAccount(ctx, gar, client); err != nil {
+			return errors.Wrap(err, "failed to release generic account")
+		}
+
+		return nil
+	}, nil
+}
+
+// NewBaseTapeFixture creates a base fixture that loads service accounts needed
+// by tape from the remote host to the DUT. This fixture must be instantiated
+// and extended by any test that uses TAPE.
+func NewBaseTapeFixture() *tapeBaseFixt {
+	return &tapeBaseFixt{}
+}
+
+type tapeBaseFixt struct{}
+
+func (*tapeBaseFixt) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	// TODO(b/204845193): workaround as fixture do not recover connection
+	if err := s.DUT().Connect(ctx); err != nil {
+		s.Fatal("Failed to reconnect to DUT: ", err)
+	}
+
+	// Parse out the shared variables.
+	sharedVars, ok := s.Var(AuthenticationConfigJSONVar)
+	if !ok {
+		s.Fatal("Authentication config variable not set")
+	}
+
+	var parsedSharedVars AuthenticationConfig
+	if err := json.Unmarshal([]byte(sharedVars), &parsedSharedVars); err != nil {
+		s.Fatal("Failed to unmarshal data: ", err)
+	}
+
+	// Clear out the path to where the files will live on the DUT.
+	if _, err := s.DUT().Conn().CommandContext(ctx, "rm", "-rf", dutServiceAccountDir).Output(); err != nil {
+		s.Fatal("Failed to remove credentials directory: ", err)
+	}
+
+	// Write each of the files from the server down to the host.
+	var credentialPaths = make(map[string]string)
+	for _, path := range parsedSharedVars.DroneServiceAccountLocations {
+		// Make sure the path exists.
+		if _, err := os.Stat(path); err != nil {
+			s.Logf("service account does not exist on host at: %s", path)
+			continue
+		}
+
+		credentialPaths[path] = serviceAccountPathOnDUT(path)
+	}
+
+	if _, err := linuxssh.PutFiles(ctx, s.DUT().Conn(), credentialPaths, linuxssh.PreserveSymlinks); err != nil {
+		s.Fatal("Failed to write credentials to DUT: ", err)
+	}
+
+	return nil
+}
+func (*tapeBaseFixt) TearDown(ctx context.Context, s *testing.FixtState) {
+	// Remove the credentials directory.
+	if _, err := s.DUT().Conn().CommandContext(ctx, "rm", "-rf", dutServiceAccountDir).Output(); err != nil {
+		s.Fatal("Failed to remove credentials directory: ", err)
+	}
+}
+func (*tapeBaseFixt) Reset(ctx context.Context) error                        { return nil }
+func (*tapeBaseFixt) PreTest(ctx context.Context, s *testing.FixtTestState)  {}
+func (*tapeBaseFixt) PostTest(ctx context.Context, s *testing.FixtTestState) {}
