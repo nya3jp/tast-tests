5243331b56c2fb4b9a776a5527de508a29b9e117
David Welling <davidwelling@google.com>
2022-07-15 13:15:27 -0700

    arcappgameperf: Support local logins for TAPE
    Add support for using a locally generated OAUTH token for authenticating
    with TAPE, and support copying service accounts from remote bots to the
    DUT in order to use service account credentials when running in the lab.
    
    BUG=chromium:1339418
    TEST=./fast_build.sh -T
    
    Cq-Depend: chrome-internal:4880344
    Change-Id: Id00bb86f7cc64097cbea319f36fa94eee7f22968

---
+++ src/chromiumos/tast/remote/tape/fixture.go
@@ -0,0 +1,28 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package tape holds remote features required to use TAPE.
+package tape
+
+import (
+	"time"
+
+	"chromiumos/tast/common/tape"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name:            "tapeRemoteBase",
+		Desc:            "Writes a TAPE access token to the DUT in a shared location for accessing the TAPE API from the DUT",
+		Contacts:        []string{"davidwelling@google.com", "arc-engprod@google.com"},
+		Impl:            tape.NewBaseTapeFixture(),
+		Vars:            []string{tape.AuthenticationConfigJSONVar, tape.LocalRefreshTokenVar},
+		SetUpTimeout:    10 * time.Second,
+		TearDownTimeout: 10 * time.Second,
+		PreTestTimeout:  1 * time.Minute,
+		PostTestTimeout: 1 * time.Minute,
+	})
+
+}
