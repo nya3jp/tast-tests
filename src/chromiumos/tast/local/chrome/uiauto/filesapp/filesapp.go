ce0999f6300f3a8670fa3a35a3068fa26c243fb1
Ben Reich <benreich@chromium.org>
2022-07-20 12:54:13 +1000

    tast-tests: Remove filesapp.{DownloadsPath,MyFilesPath}
    The last remaining usages of these deprecated variables exist in the
    pita.* tests which are being removed at chrome-internal:4888397.
    
    BUG=b:233132576
    TEST=./fast_build.sh -T
    
    Change-Id: I2f415c962e53dbe84fd2b9959d9afab2870b17af

--- src/chromiumos/tast/local/chrome/uiauto/filesapp/filesapp.go
+++ src/chromiumos/tast/local/chrome/uiauto/filesapp/filesapp.go
@@ -27,28 +27,6 @@ import (
 	"chromiumos/tast/testing"
 )
 
-// DEPRECATED: The following path is deprecated so please do not use it anymore.
-// All usages are being migrated away, to get the users Downloads or MyFiles
-// path please use the cryptohome library, e.g.:
-//
-// 	downloadsPath, err := cryptohome.DownloadsPath(ctx, cr.NormalizedUser())
-// 	if err != nil {
-// 		s.Fatal("Failed to retrieve users Downloads path: ", err)
-// 	}
-//
-// Or to retrieve the MyFiles path location:
-//
-// 	myFilesPath, err := cryptohome.MyFilesPath(ctx, cr.NormalizedUser())
-// 		if err != nil {
-// 			s.Fatal("Failed to retrieve users MyFiles path: ", err)
-// 		}
-
-// DownloadPath is the location of Downloads for the user.
-const DownloadPath = "/home/chronos/user/Downloads/"
-
-// MyFilesPath is the location of My files for the user.
-const MyFilesPath = "/home/chronos/user/MyFiles"
-
 // FilesTitlePrefix is the prefix of the Ash window title.
 const FilesTitlePrefix = "Files - "
 
