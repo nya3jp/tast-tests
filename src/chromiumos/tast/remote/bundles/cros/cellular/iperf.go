d0c5f01cb9889177408a82fac8ab400a54ac6957
Jason Stanko <jstanko@google.com>
2022-07-21 16:21:47 +0000

    Iperf: Add Iperf callbox test
    Adds Iperf CallboxManager APIs for enabling cellular performance tests. Also adds a simple Iperf test that checks actual throughput against maximum achievable throughput.
    
    Note that the UDP results returned by the callbox are inaccurate for Rx/download tests so an additional CL will be needed to get server side results.
    
    BUG=b:238343555
    TEST=tast run -var=callbox=<CB> -var=callboxManager=<CBM> <DUT> cellular.Iperf.*
    
    Related-To: Ic4f03e9609e8573079f026005e5b00a29ad33af2
    Change-Id: I4866154e5d854c1def5247c6ebda4b1dc2eb099e

---
+++ src/chromiumos/tast/remote/bundles/cros/cellular/iperf.go
@@ -0,0 +1,125 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	cbiperf "chromiumos/tast/remote/cellular/callbox/iperf"
+	"chromiumos/tast/remote/cellular/callbox/manager"
+	"chromiumos/tast/remote/network/iperf"
+	"chromiumos/tast/testing"
+)
+
+type iperfTestCaseConfiguration struct {
+	testType          cbiperf.TestType
+	additionalOptions []iperf.ConfigOption
+}
+
+type iperfTestCase struct {
+	callboxOpts         *manager.ConfigureCallboxRequestBody
+	iperfConfigurations []iperfTestCaseConfiguration
+}
+
+// cellularInterface is the name of the cellular interface to use.
+// TODO(b/241964523): Dynamically get cellular interface from shill at runtime.
+const cellularInterface = "rmnet_data0"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Iperf,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Conducts cellular performance tests between DUT and callbox using Iperf to compare actual throughput results with expected for a given network configuration",
+		Contacts: []string{
+			"jstanko@google.com",
+			"cros-connectivity@google.com",
+		},
+		Attr:         []string{"group:cellular", "cellular_callbox"},
+		ServiceDeps:  []string{"tast.cros.example.ChromeService"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "callboxManagedFixture",
+		Timeout:      15 * time.Minute,
+		Params: []testing.Param{
+			{
+				// Establishes a mimo2x2 connection with the callbox and runs multiple iperf sessions with various configurations
+				Name: "mimo2x2",
+				Val: iperfTestCase{
+					callboxOpts: &manager.ConfigureCallboxRequestBody{
+						Hardware:     "CMW",
+						CellularType: "LTE",
+						ParameterList: []string{
+							"band", "2",
+							"bw", "20",
+							"mimo", "2x2",
+							"tm", "3",
+							"pul", "0",
+							"pdl", "high",
+						},
+					},
+					iperfConfigurations: []iperfTestCaseConfiguration{
+						{
+							testType: cbiperf.TestTypeUDPTx,
+							// use lower bitrate for udp upload otherwise results can be very inconsistent
+							additionalOptions: []iperf.ConfigOption{iperf.MaxBandwidthOption(100 * iperf.Mbps)},
+						},
+						{
+							testType: cbiperf.TestTypeTCPTx,
+						},
+						{
+							testType: cbiperf.TestTypeUDPRx,
+						},
+						{
+							testType: cbiperf.TestTypeTCPRx,
+						},
+					},
+				},
+			},
+		},
+	})
+}
+
+func Iperf(ctx context.Context, s *testing.State) {
+	tc := s.Param().(iperfTestCase)
+	tf := s.FixtValue().(*manager.TestFixture)
+	dutConn := s.DUT().Conn()
+
+	tf.ConnectToCallbox(ctx, s, dutConn, tc.callboxOpts, cellularInterface)
+
+	testManager := cbiperf.NewTestManager(tf.Vars.Callbox, dutConn, tf.CallboxManagerClient)
+
+	for _, config := range tc.iperfConfigurations {
+		subTest := func(ctx context.Context, s *testing.State) {
+			history, err := testManager.RunOnce(ctx, config.testType, cellularInterface, config.additionalOptions)
+			if err != nil {
+				s.Fatal("Failed to run iperf session: ", err)
+			}
+
+			result, err := iperf.NewResultFromHistory(*history)
+			if err != nil {
+				s.Fatal("Failed to calculate iperf result statistics: ", err)
+			}
+
+			min, target, err := testManager.CalculateExpectedThroughput(ctx, config.testType)
+			if err != nil {
+				s.Fatal("Failed to calculate expected throughput: ", err)
+			}
+
+			if result.Throughput < min {
+				s.Fatalf("Throughput below required, got: %v want: %v Mbit/s", result.Throughput/iperf.Mbps, min/iperf.Mbps)
+			}
+
+			if result.Throughput < target {
+				s.Logf("WARNING: Throughput below target, got: %v want: %v Mbit/s", result.Throughput/iperf.Mbps, target/iperf.Mbps)
+			}
+
+			s.Logf("Finished Iperf test %v, minimum: %v target: %v actual: %v +/- %v Mbit/s",
+				config.testType, min/iperf.Mbps, target/iperf.Mbps, result.Throughput/iperf.Mbps, result.StdDeviation/iperf.Mbps)
+		}
+
+		s.Run(ctx, fmt.Sprintf("Testcase %s", config.testType), subTest)
+	}
+}
