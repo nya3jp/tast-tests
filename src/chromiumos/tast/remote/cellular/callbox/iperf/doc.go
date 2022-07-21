6914370eb73729e2550ecf11c99f3e92686707cc
Jason Stanko <jstanko@google.com>
2022-07-21 16:21:47 +0000

    Iperf: Add Iperf callbox test
    Adds Iperf CallboxManager APIs for enabling cellular performance tests. Also adds a simple Iperf test that checks actual throughput against maximum achievable throughput.
    
    Note that the UDP results returned by the callbox are inaccurate for Rx/download tests so an additional CL will be needed to get server side results.
    
    BUG=b:238343555
    TEST=tast run -var=callbox=<CB> -var=callboxManager=<CBM> <DUT> cellular.Iperf.*
    
    Related-To: Ic4f03e9609e8573079f026005e5b00a29ad33af2
    Change-Id: I4866154e5d854c1def5247c6ebda4b1dc2eb099e

---
+++ src/chromiumos/tast/remote/cellular/callbox/iperf/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package iperf implements the client and server interfaces required to use Iperf with callboxes
+package iperf
