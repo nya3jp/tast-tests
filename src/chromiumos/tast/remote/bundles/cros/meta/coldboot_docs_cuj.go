7f2de3f1f4b8e3217ab28845411d706612d843bc
Tiago Vignatti <tvignatti@igalia.com>
2022-07-21 14:40:04 -0300

    meta: Add test to capture Google Docs CUJ cold boot metrics
    From a practical perspective, Lacros architects are likely interested in
    the time of the first Google Docs opening, not multiple opening times in
    a row. The latter is what lacros.DocsCUJ measures.
    
    Therefore, this newly added test is helpful to understand the effects of
    the first Google Docs opening, by rebooting the DUT every time a single
    iteration of DocsCUJ runs.
    
    BUG=chromium:1328923
    TEST=tast run device meta.ColdbootDocsCUJ*
    
    Change-Id: I934cda4ce51f5c62913c129455ee761adf603834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3781302
    Auto-Submit: Tiago Vignatti <tvignatti@igalia.com>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Tested-by: Tiago Vignatti <tvignatti@igalia.com>
    Commit-Queue: Tiago Vignatti <tvignatti@igalia.com>

---
+++ src/chromiumos/tast/remote/bundles/cros/meta/coldboot_docs_cuj.go
@@ -0,0 +1,96 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package meta
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"chromiumos/tast/remote/bundles/cros/meta/tastrun"
+	"chromiumos/tast/rpc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ColdbootDocsCUJ,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Capture Google Docs CUJ metrics after cold booting the system",
+		Contacts:     []string{"tvignatti@igalia.com", "lacros-team@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", "lacros"},
+		VarDeps:      []string{"ui.gaiaPoolDefault"},
+		Vars:         []string{"meta.ColdbootDocsCUJ.iterations"},
+		Timeout:      15 * time.Minute,
+		Params: []testing.Param{{
+			Val: false,
+		}, {
+			Name: "reverse",
+			Val:  true,
+		}},
+	})
+}
+
+func ColdbootDocsCUJ(ctx context.Context, s *testing.State) {
+	const (
+		// The number of iterations. In order to collect meaningful average and data variability,
+		// the default value is defined large enough as "10". Can be overridden by var
+		// "meta.ColdbootDocsCUJ.iterations".
+		defaultIterations = 10
+	)
+
+	iterations := defaultIterations
+	if iter, ok := s.Var("meta.ColdbootDocsCUJ.iterations"); ok {
+		i, err := strconv.Atoi(iter)
+		if err != nil {
+			s.Fatal("Invalid meta.ColdbootDocsCUJ.iterations value: ", iter)
+		}
+		iterations = i
+	}
+
+	for i := 0; i < iterations; i++ {
+		// Run the Docs CUJ local test to get the metrics.
+		coldbootDocsCUJOnce(ctx, s, i, iterations)
+	}
+}
+
+func coldbootDocsCUJOnce(ctx context.Context, s *testing.State, i, iterations int) {
+	isReverse := s.Param().(bool)
+
+	s.Logf("Running iteration %d/%d", i+1, iterations)
+	d := s.DUT()
+
+	// TODO(https://crbug.com/1346752): Find a way to get rid of CTRL+D trick "OS verification is
+	// OFF" every time it boots.
+	s.Log("Rebooting DUT")
+	if err := d.Reboot(ctx); err != nil {
+		s.Fatal("Failed to reboot DUT: ", err)
+	}
+
+	// Need to reconnect to the gRPC server after rebooting DUT.
+	cl, err := rpc.Dial(ctx, d, s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+
+	resultsDir := filepath.Join(s.OutDir(), fmt.Sprintf("subtest_results_%d", i))
+
+	flags := []string{
+		"-resultsdir=" + resultsDir,
+		"-var=lacros.DocsCUJ.iterations=1",
+	}
+
+	var variantName string
+	if isReverse {
+		variantName = ".reverse"
+	}
+	if _, _, err := tastrun.Exec(ctx, s, "run", flags, []string{"lacros.DocsCUJ" + variantName}); err != nil {
+		s.Fatal("Failed to run tast: ", err)
+	}
+}
