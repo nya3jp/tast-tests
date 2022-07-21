37fe9297d5f12d5f5c7421c43b6ee83ee8822040
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2022-07-20 17:25:26 -0700

    arcappcompat: Run UIAutomator Test Using Tast For Google Keep app
    BUG=b:230759792
    TEST=tast run <eve> arcappcompat.GoogleKeep.vm_clamshell_mode
    tast run <eve> arcappcompat.GoogleKeep.vm_tablet_mode
    tast run <kevin> arcappcompat.GoogleKeep.tablet_mode
    tast run <kevin> arcappcompat.GoogleKeep.clamshell_mode
    
    Tast Test for Google keep app verifies launch app
    from the playstore, verify if the app is logged in
    and check if the app reached the main activity page.
    
    Change-Id: I9aabe8ad5cba9165d9c1ef92bc270a20ed10110e

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/google_keep.go
@@ -0,0 +1,131 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+// clamshellLaunchForGoogleKeep launches GoogleKeep in clamshell mode.
+var clamshellLaunchForGoogleKeep = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForGoogleKeep, Timeout: testutil.LaunchTestCaseTimeout},
+}
+
+// touchviewLaunchForGoogleKeep launches GoogleKeep in tablet mode.
+var touchviewLaunchForGoogleKeep = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForGoogleKeep, Timeout: testutil.LaunchTestCaseTimeout},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GoogleKeep,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Functional test for GoogleKeep that installs the app also verifies it is logged in and that the main page is open, checks GoogleKeep correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat", "appcompat_top_apps"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name: "clamshell_mode",
+			Val: testutil.TestParams{
+				LaunchTests: clamshellLaunchForGoogleKeep,
+				CommonTests: testutil.ClamshellSmokeTests,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "tablet_mode",
+			Val: testutil.TestParams{
+				LaunchTests: touchviewLaunchForGoogleKeep,
+				CommonTests: testutil.TouchviewSmokeTests,
+			},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "vm_clamshell_mode",
+			Val: testutil.TestParams{
+				LaunchTests: clamshellLaunchForGoogleKeep,
+				CommonTests: testutil.ClamshellSmokeTests,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "vm_tablet_mode",
+			Val: testutil.TestParams{
+				LaunchTests: touchviewLaunchForGoogleKeep,
+				CommonTests: testutil.TouchviewSmokeTests,
+			},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}},
+		Timeout: 20 * time.Minute,
+		Vars:    []string{"arcappcompat.gaiaPoolDefault"},
+	})
+}
+
+// GoogleKeep test uses library for opting into the playstore and installing app.
+// Checks GoogleKeep correctly changes the window states in both clamshell and touchview mode.
+func GoogleKeep(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.google.android.keep"
+		appActivity = ".activities.BrowseActivity"
+	)
+
+	testSet := s.Param().(testutil.TestParams)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testSet)
+}
+
+// launchAppForGoogleKeep verifies GoogleKeep is logged in and
+// verify GoogleKeep reached main activity page of the app.
+func launchAppForGoogleKeep(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+
+	const (
+		getStartedButtonText = "Get started"
+		accountIconID        = "com.google.android.keep:id/account_particle"
+	)
+
+	// Click on getStarted button.
+	getStartedButton := d.Object(ui.ClassName(testutil.AndroidButtonClassName), ui.TextMatches("(?i)"+getStartedButtonText))
+	if err := getStartedButton.WaitForExists(ctx, testutil.DefaultUITimeout); err != nil {
+		s.Log("getStartedButton doesn't exist: ", err)
+	} else if err := getStartedButton.Click(ctx); err != nil {
+		s.Fatal("Failed to click on getStartedButton: ", err)
+	}
+
+	// Check if app is logged in with account.
+	accountIcon := d.Object(ui.ID(accountIconID))
+	if err := accountIcon.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		s.Fatal(" accountIcon does not exist: ", err)
+	}
+
+	testutil.HandleDialogBoxes(ctx, s, d, appPkgName)
+	// Check for launch verifier.
+	launchVerifier := d.Object(ui.PackageName(appPkgName))
+	if err := launchVerifier.WaitForExists(ctx, testutil.LongUITimeout); err != nil {
+		testutil.DetectAndHandleCloseCrashOrAppNotResponding(ctx, s, d)
+		s.Fatal("launchVerifier doesn't exists: ", err)
+	}
+}
