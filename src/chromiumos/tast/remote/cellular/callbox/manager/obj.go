4f328838a5e67fd039442230b02bf22bd8a9ec7f
Jason Stanko <jstanko@google.com>
2022-07-21 16:21:47 +0000

    Iperf: Add Iperf callbox test
    Adds Iperf CallboxManager APIs for enabling cellular performance tests. Also adds a simple Iperf test that checks actual throughput against maximum achievable throughput.
    
    Note that the UDP results returned by the callbox are inaccurate for Rx/download tests so an additional CL will be needed to get server side results.
    
    BUG=b:238343555
    TEST=tast run -var=callbox=<CB> -var=callboxManager=<CBM> <DUT> cellular.Iperf.*
    
    Related-To: Ic4f03e9609e8573079f026005e5b00a29ad33af2
    Change-Id: I4866154e5d854c1def5247c6ebda4b1dc2eb099e

--- src/chromiumos/tast/remote/cellular/callbox/manager/obj.go
+++ src/chromiumos/tast/remote/cellular/callbox/manager/obj.go
@@ -4,54 +4,108 @@
 
 package manager
 
-import "encoding/json"
-
-// RequestBody is a request object that can be marshalled into a byte array.
-type RequestBody interface {
-	// Marshall marshals this object into a byte array.
-	Marshall() ([]byte, error)
-}
-
 // ConfigureCallboxRequestBody is the request body for ConfigureCallbox requests.
 type ConfigureCallboxRequestBody struct {
-	Callbox       string
-	Hardware      string
-	CellularType  string
-	ParameterList []string
-}
-
-// Marshall marshals this object into a byte array.
-func (r *ConfigureCallboxRequestBody) Marshall() ([]byte, error) {
-	return json.Marshal(map[string]interface{}{
-		"callbox":        r.Callbox,
-		"hardware":       r.Hardware,
-		"cellular_type":  r.CellularType,
-		"parameter_list": r.ParameterList,
-	})
+	Callbox       string   `json:"callbox,omitempty"`
+	Hardware      string   `json:"hardware,omitempty"`
+	CellularType  string   `json:"cellular_type,omitempty"`
+	ParameterList []string `json:"parameter_list,omitempty"`
 }
 
 // BeginSimulationRequestBody is the request body for BeginSimulation requests.
 type BeginSimulationRequestBody struct {
-	Callbox string
-}
-
-// Marshall marshals this object into a byte array.
-func (r *BeginSimulationRequestBody) Marshall() ([]byte, error) {
-	return json.Marshal(map[string]interface{}{
-		"callbox": r.Callbox,
-	})
+	Callbox string `json:"callbox,omitempty"`
 }
 
 // SendSmsRequestBody is the request body for SendSms requests.
 type SendSmsRequestBody struct {
-	Callbox string
-	Message string
+	Callbox string `json:"callbox,omitempty"`
+	Message string `json:"sms,omitempty"`
 }
 
-// Marshall marshals this object into a byte array.
-func (r *SendSmsRequestBody) Marshall() ([]byte, error) {
-	return json.Marshal(map[string]interface{}{
-		"callbox": r.Callbox,
-		"sms":     r.Message,
-	})
+// ConfigureIperfRequestBody is the request body for iperf configuration requests.
+type ConfigureIperfRequestBody struct {
+	Callbox    string              `json:"callbox,omitempty"`
+	Time       int                 `json:"time,omitempty"`
+	PacketSize int                 `json:"psize,omitempty"`
+	Clients    []IperfClientConfig `json:"clients,omitempty"`
+	Servers    []IperfServerConfig `json:"servers,omitempty"`
+}
+
+// IperfClientConfig is a configuration to use with a callbox Iperf client instance.
+type IperfClientConfig struct {
+	IP                  string  `json:"ip,omitempty"`
+	Port                int     `json:"port,omitempty"`
+	Protocol            string  `json:"proto,omitempty"`
+	WindowSize          int64   `json:"wsize,omitempty"`
+	ParallelConnections int     `json:"pconnections,omitempty"`
+	MaxBitRate          float64 `json:"mbitrate,omitempty"`
+}
+
+// IperfServerConfig is a configuration to use with a callbox Iperf server instance.
+type IperfServerConfig struct {
+	IP         string `json:"ip,omitempty"`
+	Port       int    `json:"port,omitempty"`
+	Protocol   string `json:"proto,omitempty"`
+	WindowSize int64  `json:"wsize,omitempty"`
+}
+
+// StartIperfRequestBody is the request body for iperf start requests.
+type StartIperfRequestBody struct {
+	Callbox string `json:"callbox,omitempty"`
+}
+
+// StopIperfRequestBody is the request body for Iperf stop requests.
+type StopIperfRequestBody struct {
+	Callbox string `json:"callbox,omitempty"`
+}
+
+// CloseIperfRequestBody is the request body for Iperf close requests.
+type CloseIperfRequestBody struct {
+	Callbox string `json:"callbox,omitempty"`
+}
+
+// FetchIperfResultRequestBody is the request body for Iperf results query requests.
+type FetchIperfResultRequestBody struct {
+	Callbox string `json:"callbox,omitempty"`
+}
+
+// FetchIperfResultResponseBody is the response body for an Iperf result query requests.
+type FetchIperfResultResponseBody struct {
+	Clients []*IperfClientResult `json:"clients"`
+	Servers []*IperfServerResult `json:"servers"`
+}
+
+// IperfServerResult is the current Iperf measurement for an Iperf server instance.
+type IperfServerResult struct {
+	ID          int     `json:"counter"`
+	Throughput  float64 `json:"throughput"`
+	PercentLoss float64 `json:"loss"`
+}
+
+// IperfClientResult is the current Iperf measurement for an Iperf client instance.
+type IperfClientResult struct {
+	ID         int     `json:"counter"`
+	Throughput float64 `json:"throughput"`
+}
+
+// FetchIperfIPRequestBody is the request body for an Iperf IP query request.
+type FetchIperfIPRequestBody struct {
+	Callbox string `json:"callbox,omitempty"`
+}
+
+// FetchIperfIPResponseBody is the response body for an Iperf IP query request.
+type FetchIperfIPResponseBody struct {
+	IP string `json:"ip"`
+}
+
+// FetchMaxThroughputRequestBody is the request body for a maximum throughput request.
+type FetchMaxThroughputRequestBody struct {
+	Callbox string `json:"callbox,omitempty"`
+}
+
+// FetchMaxThroughputResponseBody is the request body for a maximum throughput request.
+type FetchMaxThroughputResponseBody struct {
+	Uplink   float64 `json:"uplink"`
+	Downlink float64 `json:"downlink"`
 }
