ae88b64252f0e8dfb06ee36876623b1df3910256
Brian Ho <hob@chromium.org>
2022-07-22 17:11:36 -0400

    tast-tests: Add passthrough cmd decoder variants for a few tests
    The passthrough command decoder is a Chrome GPU feature currently
    being rolled out via Finch. Since this feature changes a lot, we'd
    like to run a few simple end-to-end Tast tests as a sanity check
    that changes to ANGLE don't cause crashes on CrOS.
    
    Note that this is a temporary addition. Once we launch the feature
    by default, we can remove these variants.
    
    Bug: b:233238923
    Change-Id: I23da16a06d45a9a606f9f50b2b3d0ef1ae05d303

--- src/chromiumos/tast/local/chrome/fixture.go
+++ src/chromiumos/tast/local/chrome/fixture.go
@@ -87,6 +87,20 @@ func init() {
 		TearDownTimeout: ResetTimeout,
 	})
 
+	// TOOD(b/233238923): Remove when passthrough is enabled by default.
+	testing.AddFixture(&testing.Fixture{
+		Name:     "chromeLoggedInWith100FakeAppsPassthroughCmdDecoder",
+		Desc:     "Logged into a user session with 100 fake apps and the passthrough command decoder enabled",
+		Contacts: []string{"hob@chromium.org"},
+		Impl: NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]Option, error) {
+			return []Option{EnableFeatures("DefaultPassthroughCommandDecoder")}, nil
+		}),
+		Parent:          "install100Apps",
+		SetUpTimeout:    LoginTimeout,
+		ResetTimeout:    ResetTimeout,
+		TearDownTimeout: ResetTimeout,
+	})
+
 	testing.AddFixture(&testing.Fixture{
 		Name:     "chromeLoggedInWith100FakeAppsDoubleBufferingAndLegacyLauncher",
 		Desc:     "Logged into a user session with 100 fake apps with productivity launcher disabled and double buffer compositing",
