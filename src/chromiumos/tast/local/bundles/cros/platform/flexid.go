7e07d39c8e5d58c0882b713f4121c9847e596baf
Joseph Sussman <josephsussman@google.com>
2022-07-26 20:43:19 +0000

    Add platform.FlexID
    Add a test for the http://go/chromeos-flex-id. Only runs on ChromeOS
    Flex devices for now.
    
    BUG=b:239435414
    TEST=tast run <DUT> platform.FlexID
    
    Change-Id: I8e6d38a819a4ba776031a9240eff41cd3026e9c4

---
+++ src/chromiumos/tast/local/bundles/cros/platform/flexid.go
@@ -0,0 +1,50 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package platform
+
+import (
+	"context"
+	"unicode"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FlexID,
+		Desc: "Tests that the go/chromeos-flex-id is properly-formed",
+		Contacts: []string{
+			"josephsussman@google.com", // Test author
+			"chromeos-flex-eng@google.com",
+		},
+		SoftwareDeps: []string{"flex_id"},
+		Attr:         []string{"group:mainline", "informational"},
+	})
+}
+
+// isAllASCII returns true if output only contains ASCII characters.
+func isAllASCII(output string) bool {
+	for i := 0; i < len(output); i++ {
+		if output[i] > unicode.MaxASCII {
+			return false
+		}
+	}
+	return true
+}
+
+func FlexID(ctx context.Context, s *testing.State) {
+	out, err := testexec.CommandContext(ctx, "flex_id_tool").CombinedOutput()
+	status, ok := testexec.ExitCode(err)
+
+	outString := string(out)
+	if !ok {
+		s.Fatalf("flex_id_tool exited with status %d: %s", status, outString)
+	}
+
+	if !IsAllASCII(outString) {
+		s.Errorf("%s contains non-ASCII characters", outString)
+	}
+}
