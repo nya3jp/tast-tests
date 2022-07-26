e5b0a9d94e27b0e646f132ce018d546b4e453b10
David Welling <davidwelling@google.com>
2022-07-26 14:16:11 -0700

    tape: Add a sample test to confirm TAPE functionality
    Add a test to confirm TAPE is able to lease generic accounts when run
    locally, and in the lab. This only needs to run on a single board since
    it serves as a simple check to confirm DUTs can reach the TAPE service.
    
    BUG=chromium:1339418
    TEST=tast run --var tape.local_refresh_token=<VALUE>
    tape.GenericAccount*
    
    Change-Id: I5f36b454406f31562ea0bda0b24ffbb9a1dbe541

---
+++ src/chromiumos/tast/local/bundles/cros/tape/generic_accounts.go
@@ -0,0 +1,54 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package tape
+
+import (
+	"context"
+	"time"
+
+	tape2 "chromiumos/tast/common/tape"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         GenericAccounts,
+		Desc:         "Confirm that the generic account leasing for TAPE works as intended",
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Contacts:     []string{"davidwelling@google.com", "alexanderhartl@google.com", "arc-engprod@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		Timeout:      5 * time.Minute,
+		VarDeps:      []string{tape2.LocalRefreshTokenVar, tape2.AuthenticationConfigJSONVar},
+		// TODO(b/240322950): Remove the remote fixture requirement at some point.
+		Fixture: "tapeRemoteBase",
+		// Limit to eve. This test does not need to run on more than one device.
+		HardwareDeps: hwdep.D(hwdep.Model("eve")),
+	})
+}
+
+func GenericAccounts(ctx context.Context, s *testing.State) {
+	// Ensure empty creds are set to ensure that service account in the lab is
+	// used when running remotely, and that local credentials are passed in when running locally.
+	// See the TAPE Readme for more information.
+	var emptyCreds []byte
+	client, err := tape2.NewClient(ctx, emptyCreds)
+	if err != nil {
+		s.Fatal("Failed to setup the TAPE client: ", err)
+	}
+
+	// Lease the account.
+	account, err := client.RequestGenericAccount(ctx, tape2.WithPoolID("tape.generic_accounts_test"), tape2.WithTimeout(5*60))
+	if err != nil {
+		s.Fatal("Failed to request a generic account: ", err)
+	}
+
+	s.Logf("Received account with username: %s", account.Username)
+
+	// Release the account.
+	if err := client.ReleaseGenericAccount(ctx, account); err != nil {
+		s.Fatal("Failed to release the generic account: ", err)
+	}
+}
