eae1b8d4672848a3c35217332e6ab0c17ee85594
Nergi Rahardi <nergi@google.com>
2022-07-27 03:25:23 +0900

    Promote arc.Gamepad tast test to critical
    arc.Gamepad: https://tastboard.corp.google.com/test/arc.Gamepad
    arc.Gamepad.vm: https://tastboard.corp.google.com/test/arc.Gamepad.vm
    
    BUG=b:238973189
    TEST=tast run arc.Gamepad.*
    
    Change-Id: I38db5c69bc7008047b6bb9e1758795a71ea96d34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3787997
    Auto-Submit: Nergi Rahardi <nergi@google.com>
    Reviewed-by: Alvin Jia <alvinjia@google.com>
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Tested-by: Nergi Rahardi <nergi@google.com>
    Commit-Queue: Nergi Rahardi <nergi@google.com>

--- src/chromiumos/tast/local/bundles/cros/arc/gamepad.go
+++ src/chromiumos/tast/local/bundles/cros/arc/gamepad.go
@@ -28,7 +28,7 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Checks gamepad support works on Android",
 		Contacts:     []string{"yhanada@chromium.org", "arc-framework+tast@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      "arcBooted",
 		Params: []testing.Param{{
