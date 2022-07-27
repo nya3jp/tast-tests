24f9c280a02289a859a322a0d31930f11ae3f7fc
Benjamin Gordon <bmgordon@chromium.org>
2022-07-27 15:37:07 -0600

    health: Move usb utils to shared package
    We want to add a new non-healthd test that needs to do the same USB
    device parsing as health.usb.ExpectedDevices.  Move this package to be
    shared so that the new test can access it.  The shared package is called
    usbutil instead of usb to match the naming of similar existing packages.
    
    BUG=b:238353195
    TEST=tast run health.*
    
    Change-Id: Id24207de0241a8fd071d094f649fa083aa17d90c

--- src/chromiumos/tast/local/usbutil/usb.go
+++ src/chromiumos/tast/local/usbutil/usb.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package usb provides USB util functions for health tast.
-package usb
+// Package usbutil provides USB util functions for checking device information.
+package usbutil
 
 import (
 	"bufio"
