1a556ece9cdaea1112d1823c551d643748c99e94
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-07-26 17:25:01 -0700

    tast-tests: refactor for lacros extensions
    Currently ash and lacros-chrome are loading custom extensions in Tast
    in a different way that might be confusing to test writers.
    
    This change provides a more consistent way of loading lacros extensions
    similarly to ash extensions.
    
    The steps to enable lacros extensions are as follows:
    1. Create temp ext dirs with test content in tests or fixtures
    2. Get a chrome option from chrome.LacrosUnpackedExtension(extDir)
    3. Pass it to chrome.New or browserfixt.SetUpWithNewChrome
    4. Then let chrome take care of the rest of the work
    
    Also it adds new lacros fixtures that load fake apps from lacros
    extensions. The existing fixtures that use the old extension fixtures
    will be updated in a follow up CL.
    
    See go/lacros-tast-extensions for details.
    
    BUG=chromium:1343713, chromium:1309565, b:225229931
    TEST=tast run example.BrowserWithNewChrome
    TEST=tast run lacros.MigrateExtensionState*
    TEST=tast run shelf.AppRearrangement.rearrange_chrome_apps  // tests using ash extensions
    TEST=tast run '("group:mainline" && "dep:lacros" && !informational)'  // all critical
    
    Change-Id: I1416cf074051f3fa2b737318e4e9cffdc98863fe

--- src/chromiumos/tast/local/chrome/lacros/lacrosfixt/config.go
+++ src/chromiumos/tast/local/chrome/lacros/lacrosfixt/config.go
@@ -7,13 +7,10 @@ package lacrosfixt
 import (
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/internal/config"
-	"chromiumos/tast/local/chrome/internal/extension"
 	"chromiumos/tast/local/chrome/lacros"
 )
 
@@ -66,13 +63,6 @@ func EnableChromeFRE() Option {
 	}
 }
 
-// UnpackedExtension returns an Option that adds dir of unpacked extension to be loaded to Lacros.
-func UnpackedExtension(dir string) Option {
-	return func(c *Config) {
-		c.extraExtDirs = append(c.extraExtDirs, dir)
-	}
-}
-
 // Config holds runtime vars or other variables needed to set up Lacros.
 type Config struct {
 	selection     lacros.Selection
@@ -83,7 +73,6 @@ type Config struct {
 	deployed      bool
 	deployedPath  string // dirpath to lacros executable file
 	enableFRE     bool
-	extraExtDirs  []string
 }
 
 // NewConfig creates a new LacrosConfig instance.
@@ -113,19 +102,18 @@ func NewConfig(ops ...Option) *Config {
 	return cfg
 }
 
-// ExtensionArgs returns a list of args needed to pass to a lacros instance to enable the test extension.
-func ExtensionArgs(extID, extList string) []string {
-	return []string{
-		"--remote-debugging-port=0",            // Let Chrome choose its own debugging port.
-		"--enable-experimental-extension-apis", // Allow Chrome to use the Chrome Automation API.
-		"--allowlisted-extension-id=" + extID,  // Whitelists the test extension to access all Chrome APIs.
-		"--load-extension=" + extList,          // Load extensions.
-	}
-}
-
 // Opts returns common chrome options for Lacros for the Config.
 func (cfg *Config) Opts() ([]chrome.Option, error) {
 	var opts []chrome.Option
+
+	// Default args that are copied from ash-chrome to lacros-chrome. See chrome.RestartChromeForTesting for the args set to ash-chrome.
+	args := []string{
+		"--remote-debugging-port=0",            // Let Chrome choose its own debugging port.
+		"--enable-experimental-extension-apis", // Allow Chrome to use the Chrome Automation API.
+		"--force-color-profile=srgb",           // Force chrome to treat the display as sRGB. See b/221643955 for details.
+		"--force-raster-color-profile=srgb",    // Force rendering to run in the sRGB color space. See b/221643955 for details.
+	}
+	opts = append(opts, chrome.LacrosExtraArgs(args...))
 
 	// Disable launching lacros on login.
 	opts = append(opts, chrome.ExtraArgs("--disable-login-lacros-opening"))
@@ -148,38 +136,8 @@ func (cfg *Config) Opts() ([]chrome.Option, error) {
 		opts = append(opts, chrome.LacrosExtraArgs("--no-first-run"))
 	}
 
-	// Force color profile to sRGB regardless of device. See b/221643955 for details.
-	opts = append(opts, chrome.LacrosExtraArgs("--force-color-profile=srgb"))
-	opts = append(opts, chrome.LacrosExtraArgs("--force-raster-color-profile=srgb"))
-
 	// Enable hangout services extension to allow for Meet screen sharing.
 	opts = append(opts, chrome.LacrosExtraArgs("--enable-hangout-services-extension-for-testing"))
-
-	// TODO(crbug.com/1343713): Remove operations with side effects from `Opts()`.
-	dir, err := ioutil.TempDir("", "lacros_extensions.")
-	if err != nil {
-		return nil, err
-	}
-	if err := os.Chmod(dir, 0755); err != nil {
-		return nil, err
-	}
-
-	var extOpts []chrome.Option
-	for _, extDir := range cfg.extraExtDirs {
-		extOpts = append(extOpts, chrome.UnpackedExtension(extDir))
-	}
-	extCfg, err := config.NewConfig(extOpts)
-	if err != nil {
-		return nil, err
-	}
-	// TODO(crbug.com/1343713): Remove operations with side effects from `Opts()`.
-	// We reuse the custom extension from the chrome package for exposing private interfaces.
-	exts, err := extension.PrepareExtensions(filepath.Join(dir, "extensions"), extCfg, extension.GuestModeDisabled)
-	if err != nil {
-		return nil, err
-	}
-	extList := strings.Join(exts.DeprecatedDirs(), ",")
-	opts = append(opts, chrome.LacrosExtraArgs(ExtensionArgs(chrome.TestExtensionID, extList)...))
 
 	// Enable Lacros.
 	// Note that specifying the feature LacrosSupport has side-effects, so
