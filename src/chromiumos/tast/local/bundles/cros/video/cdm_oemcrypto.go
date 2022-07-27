e29141c103f85fad9934c06662e3f61dd4f7181c
Jeffrey Kardatzke <jkardatzke@google.com>
2022-07-27 15:02:52 -0700

    tast-tests: Update for handling WV14 test variants
    This adds support for running with either WV14 or WV16 compatible
    firmware on Intel devices. WV16 is configured as the default, and if we
    detect a version incompatibility then we switch to the WV14 variant.
    This will be removed once all volteer/brya devices have WV16 FW pinned.
    
    BUG=b:240453750
    TEST=tast run DUT video.CDMOEMCrypto.* w/ WV14 and WV16 FW
    
    Cq-Depend: chrome-internal:4900345
    Change-Id: I408828516f5426b7273d39070489863b8235e63a

--- src/chromiumos/tast/local/bundles/cros/video/cdm_oemcrypto.go
+++ src/chromiumos/tast/local/bundles/cros/video/cdm_oemcrypto.go
@@ -6,6 +6,7 @@ package video
 
 import (
 	"context"
+	"os"
 	"path/filepath"
 	"time"
 
@@ -35,6 +36,12 @@ func init() {
 }
 
 func CDMOEMCrypto(ctx context.Context, s *testing.State) {
+	// This is a marker file to indicate the FW version is incompatible with the
+	// binary. Clear this if present before we start to avoid falsely detecting
+	// that.
+	const fwInvalidMarkerFile = "/var/lib/oemcrypto/wv16_fw_version_invalid"
+	os.Remove(fwInvalidMarkerFile)
+
 	testExec := s.Param().(string)
 	logdir := filepath.Join(s.OutDir(), "gtest")
 	s.Log("Running ", testExec)
@@ -42,6 +49,20 @@ func CDMOEMCrypto(ctx context.Context, s *testing.State) {
 		gtest.Logfile(filepath.Join(logdir, testExec+".log")),
 		gtest.Filter("-*Huge*"),
 	).Run(ctx); err != nil {
-		s.Errorf("%s failed: %v", testExec, err)
+		// Check if the marker file is there to indicate FW version incompatibility
+		// on Intel and then invoke the WV14 variant instead.
+		if _, staterr := os.Stat(fwInvalidMarkerFile); staterr == nil {
+			s.Log("Found indicator for FW mismatch, running WV14 variant")
+			os.Remove(fwInvalidMarkerFile)
+			testExec += "-wv14"
+			if _, err = gtest.New(testExec,
+				gtest.Logfile(filepath.Join(logdir, testExec+".log")),
+				gtest.Filter("-*Huge*"),
+			).Run(ctx); err != nil {
+				s.Errorf("%s failed: %v", testExec, err)
+			}
+		} else {
+			s.Errorf("%s failed: %v", testExec, err)
+		}
 	}
 }
