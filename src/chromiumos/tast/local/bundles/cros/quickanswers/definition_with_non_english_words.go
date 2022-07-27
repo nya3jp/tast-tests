6b491b618b612408ce928032e55a5334daf87364
Yue Li <updowndota@google.com>
2022-07-27 02:09:48 -0700

    Tast: Add quickanswers.DefinitionWithNonEnglishWords Tast tests
    BUG=b:239777614
    TEST=Test on DUT with command: tast -verbose run DUT quickanswers.DefinitionWithNonEnglishWords
    
    Change-Id: I6beeb88d87252531b09da7f4b00e58692391b664

---
+++ src/chromiumos/tast/local/bundles/cros/quickanswers/definition_with_non_english_words.go
@@ -0,0 +1,138 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package quickanswers
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/lacros/lacrosfixt"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+// queryInfo contains information related to the test Quick answers query.
+type queryInfo struct {
+	queryWord       string
+	languageName    string
+	languageCode    string
+	responseKeyword string
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DefinitionWithNonEnglishWords,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Test Quick Answers definition feature on non-English words",
+		Contacts: []string{
+			"updowndota@google.com",
+			"croissant-eng@google.com",
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Val: browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Val:               browser.TypeLacros,
+		}},
+	})
+}
+
+// DefinitionWithNonEnglishWords tests Quick Answers definition feature on non-English words.
+func DefinitionWithNonEnglishWords(ctx context.Context, s *testing.State) {
+	// Reserve five seconds for various cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	// Setup chrome session with the Quick Answers for more locales feature flag enabled for any browser.
+	bt := s.Param().(browser.Type)
+	opt := chrome.EnableFeatures("QuickAnswersForMoreLocales")
+	if bt == browser.TypeLacros {
+		opt = chrome.LacrosEnableFeatures("QuickAnswersForMoreLocales")
+	}
+	cr, br, closeBrowser, err := browserfixt.SetUpWithNewChrome(ctx, bt, lacrosfixt.NewConfig(), opt)
+	if err != nil {
+		s.Fatal("Failed to connect to Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+	defer closeBrowser(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	if err := tconn.Call(ctx, nil, `tast.promisify(chrome.autotestPrivate.setWhitelistedPref)`, "settings.quick_answers.enabled", true); err != nil {
+		s.Fatal("Failed to enable Quick Answers: ", err)
+	}
+
+	var queries = [...]queryInfo{
+		{"pentágono", "Spanish", "es", "cinco lados"},
+		{"settimana", "Italian", "it", "sette giorni"},
+		{"semaine", "French", "fr", "sept jours"},
+		{"futebol", "Portuguese", "pt", "11 jogadores"},
+		{"verdreifachen", "German", "de", "dreimal"},
+	}
+
+	const languagesList = "en,es,it,fr,pt,de"
+
+	if err := tconn.Call(ctx, nil, `tast.promisify(chrome.settingsPrivate.setPref)`, "settings.language.preferred_languages", languagesList); err != nil {
+		s.Fatal("Failed to set preferred languages: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer kb.Close()
+
+	for _, query := range queries {
+		s.Log("Testing definition query with " + query.languageName + " word: " + query.queryWord)
+
+		// Open page with the query word on it.
+		conn, err := br.NewConn(ctx, "https://google.com/search?q="+query.queryWord)
+		if err != nil {
+			s.Fatal("Failed to create new Chrome connection: ", err)
+		}
+		defer conn.Close()
+		defer conn.CloseTarget(ctx)
+
+		// Wait for the query word to appear.
+		queryText := nodewith.Name(query.queryWord).Role(role.StaticText).First()
+		if err := ui.WaitUntilExists(queryText)(ctx); err != nil {
+			s.Fatal("Failed to wait for query to load: ", err)
+		}
+
+		// Right click the selected word and ensure the Quick Answers UI shows up with the definition result.
+		quickAnswers := nodewith.ClassName("QuickAnswersView")
+		definitionResult := nodewith.NameContaining(query.responseKeyword).ClassName("QuickAnswersTextLabel")
+		if err := uiauto.Combine("Show context menu",
+			ui.RightClick(queryText),
+			ui.WaitUntilExists(quickAnswers),
+			ui.WaitUntilExists(definitionResult))(ctx); err != nil {
+			s.Fatal("Quick Answers result not showing up: ", err)
+		}
+
+		// Dismiss the context menu and ensure the Quick Answers UI also dismiss.
+		if err := uiauto.Combine("Dismiss context menu",
+			kb.AccelAction("Esc"),
+			ui.WaitUntilGone(quickAnswers))(ctx); err != nil {
+			s.Fatal("Quick Answers result not dismissed: ", err)
+		}
+	}
+}
