85bc2563756f8cc7f08dca8f933f65bc9a43382a
Michael Checo <michaelcheco@google.com>
2022-07-28 23:06:06 +0000

    rgbkbd: Update caps lock test and add utils
    - Call SetTestingMode before starting Chrome to ensure that
      the correct value is returned for the GetRgbKeyboardCapabilities
      call made at startup.
    - Add helper function to reset rgbkbd to it's initial state due to
      SetCapsLockState/SetStaticBackgroundColor calls made at startup.
    - Add utilities to simply interactions with the rgbkbd DBus service
      as well as utilities that make it easier to verify logs, both of
      which will be needed by all rgb tast tests.
    
    BUG=b:214077425
    TEST=tast run <DUT> rgbkbd.CapslockRgbStateUpdates
    
    Change-Id: I0d619b2e31078199c9190bed1ced0130d6d3b717

---
+++ src/chromiumos/tast/local/rgbkbd/rgbkbd.go
@@ -0,0 +1,83 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package rgbkbd contains utilities for communicating with rgbkbd.
+package rgbkbd
+
+import (
+	"context"
+
+	"github.com/godbus/dbus/v5"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+)
+
+const (
+	dbusName      = "org.chromium.Rgbkbd"
+	dbusPath      = "/org/chromium/Rgbkbd"
+	dbusInterface = "org.chromium.Rgbkbd"
+)
+
+// Rgbkbd is used to interact with the rgbkbd process over D-Bus.
+type Rgbkbd struct {
+	conn *dbus.Conn
+	obj  dbus.BusObject
+}
+
+// NewRgbkbd connects to rgbkbd via D-Bus and returns a Rgbkbd object.
+func NewRgbkbd(ctx context.Context) (*Rgbkbd, error) {
+	conn, obj, err := dbusutil.Connect(ctx, dbusName, dbusPath)
+	if err != nil {
+		return nil, err
+	}
+	return &Rgbkbd{conn, obj}, nil
+}
+
+// call is a thin wrapper over CallWithContext.
+func (c *Rgbkbd) call(ctx context.Context, method string, args ...interface{}) *dbus.Call {
+	return c.obj.CallWithContext(ctx, dbusInterface+"."+method, 0, args...)
+}
+
+// SetTestingMode allows the Rgbkbd daemon to write calls to a log file.
+func (c *Rgbkbd) SetTestingMode(ctx context.Context, capability uint32) error {
+	if err := c.call(ctx, "SetTestingMode", true, capability).Err; err != nil {
+		return errors.Wrap(err, "failed to call method SetTestingMode")
+	}
+	return nil
+}
+
+// SetStaticBackgroundColor changes the keyboard backlight color.
+func (c *Rgbkbd) SetStaticBackgroundColor(ctx context.Context, r, g, b uint8) error {
+	if err := c.call(ctx, "SetStaticBackgroundColor", r, g, b).Err; err != nil {
+		return errors.Wrap(err, "failed to call method SetStaticBackgroundColor")
+	}
+	return nil
+}
+
+// SetCapsLockState changes the caps lock state.
+func (c *Rgbkbd) SetCapsLockState(ctx context.Context, enabled bool) error {
+	if err := c.call(ctx, "SetCapsLockState", enabled).Err; err != nil {
+		return errors.Wrap(err, "failed to call method SetCapsLockState")
+	}
+	return nil
+}
+
+// ResetRgbkbdState restores rgbkbd's initial state by resetting the keyboard
+// to the default background color and disabling caps lock.
+func ResetRgbkbdState(ctx context.Context, rgbkbd *Rgbkbd) error {
+	// Reset caps lock state.
+	err := rgbkbd.SetCapsLockState(ctx, false)
+	if err != nil {
+		return errors.Wrap(err, "Call to ResetRgbkbdState failed")
+	}
+
+	// Reset to default background color. Overrides wallpaper extracted
+	// color which varies depending on the device used.
+	err = rgbkbd.SetStaticBackgroundColor(ctx, 255, 255, 210)
+	if err != nil {
+		return errors.Wrap(err, "Call to ResetRgbkbdState failed")
+	}
+	return nil
+}
