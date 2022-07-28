532ded6f8bdd3cc2ed9da7bcfee6ef3290f4caeb
Michael Checo <michaelcheco@google.com>
2022-07-28 23:06:06 +0000

    rgbkbd: Update caps lock test and add dbus util
    BUG=b:214077425
    TEST=tast run <DUT> rgbkbd.CapslockRgbStateUpdates
    
    Change-Id: I0d619b2e31078199c9190bed1ced0130d6d3b717

---
+++ src/chromiumos/tast/local/rgbkbd/util.go
@@ -0,0 +1,46 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package rgbkbd contains utilities for rgbkbd tests.
+package rgbkbd
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+func printLogContents(expected, actual []string) string {
+	content := fmt.Sprintf("Expected: %v | Actual: %v", expected, actual)
+	return content
+}
+
+// AreLastLogLinesEqual extracts the last N lines of a log where N is the
+// length of the |expected| log and validates that the rgbkbd log matches
+// our expected log.
+func AreLastLogLinesEqual(expected []string, actual string) (bool, string) {
+	linesToCheck := getLastNLines(actual, len(expected))
+	matches := reflect.DeepEqual(linesToCheck, expected)
+	msg := ""
+	if !matches {
+		msg = printLogContents(expected, linesToCheck)
+	}
+	return matches, msg
+}
+
+// splitLog returns the log string as an array of individual entries.
+// We use '\n' as the separator because a newline is added between each log
+// entry.
+func splitLog(log string) []string {
+	lines := strings.Split(log, "\n")
+	// Remove last entry since '\n' adds an empty element to the end of the arr
+	return lines[:len(lines)-1]
+}
+
+// getLastNLines returns any logs written starting at |num|.
+func getLastNLines(log string, num int) (remainingLog []string) {
+	split := splitLog(log)
+	remainingLog = split[len(split)-num:]
+	return remainingLog
+}
