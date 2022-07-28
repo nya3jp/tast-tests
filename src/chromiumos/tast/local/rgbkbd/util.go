20301ae17f6cf48059b1e531a38c1d2e9da6a1d5
Michael Checo <michaelcheco@google.com>
2022-07-28 23:06:06 +0000

    rgbkbd: Update caps lock test and add utils
    - Call SetTestingMode before starting Chrome to ensure that
      the correct value is returned for the GetRgbKeyboardCapabilities
      call made at startup.
    - Add helper function to reset rgbkbd to it's initial state due to
      SetCapsLockState/SetStaticBackgroundColor calls made at startup.
    - Add utilities to simply interactions with the rgbkbd DBus service
      as well as utilities that make it easier to verify logs, both of
      which will be needed by all rgb tast tests.
    
    BUG=b:214077425
    TEST=tast run <DUT> rgbkbd.CapslockRgbStateUpdates
    
    Change-Id: I0d619b2e31078199c9190bed1ced0130d6d3b717

---
+++ src/chromiumos/tast/local/rgbkbd/util.go
@@ -0,0 +1,68 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package rgbkbd contains utilities for rgbkbd tests.
+package rgbkbd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"reflect"
+	"strings"
+
+	"chromiumos/tast/errors"
+)
+
+func writeErrOutputToLog(outDir, log string) error {
+	if err := ioutil.WriteFile(filepath.Join(outDir, "capslock_rgb_state_updates.txt"),
+		[]byte(log), 0644); err != nil {
+		return err
+	}
+	return nil
+}
+
+func logContents(expected, actual []string) string {
+	content := fmt.Sprintf("Expected: %v | Actual: %v", expected, actual)
+	return content
+}
+
+// LastLogLinesMatch extracts the last N lines of a log where N is the
+// length of the |expected| log and validates that the rgbkbd log matches
+// our expected log.
+func LastLogLinesMatch(expected []string, actual, outDir string) (bool, error) {
+	linesToCheck, err := lastNLines(actual, len(expected))
+	if err != nil {
+		return false, err
+	}
+
+	matches := reflect.DeepEqual(linesToCheck, expected)
+	if !matches {
+		err := writeErrOutputToLog(outDir, logContents(expected, linesToCheck))
+		if err != nil {
+			return false, errors.New("unable to write to log file: ", err)
+		}
+		return matches, errors.New("unexpected logs, find more details in capslock_rgb_state_updates.txt")
+	}
+	return matches, nil
+}
+
+// splitLog returns the log string as an array of individual entries.
+// We use '\n' as the separator because a newline is added between each log
+// entry.
+func splitLog(log string) []string {
+	lines := strings.Split(log, "\n")
+	// Remove last entry since '\n' adds an empty element to the end of the arr
+	return lines[:len(lines)-1]
+}
+
+// lastNLines returns any logs written starting at |num|.
+func lastNLines(log string, num int) (remainingLog []string, err error) {
+	split := splitLog(log)
+	if len(split) < num {
+		return nil, errors.Errorf("Length of split log array (%d) is < num: %d", len(split), num)
+	}
+	remainingLog = split[len(split)-num:]
+	return remainingLog, nil
+}
