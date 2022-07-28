aafbeff37453ba1d8fb2a8b6e701f26c8f4b5ca9
Michael Checo <michaelcheco@google.com>
2022-07-28 23:06:06 +0000

    rgbkbd: Update caps lock test and add dbus util
    BUG=b:214077425
    TEST=tast run <DUT> rgbkbd.CapslockRgbStateUpdates
    
    Change-Id: I0d619b2e31078199c9190bed1ced0130d6d3b717

---
+++ src/chromiumos/tast/local/rgbkbd/rgbkbd.go
@@ -0,0 +1,49 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package rgbkbd contains utilities for communicating with rgbkbd.
+package rgbkbd
+
+import (
+	"context"
+
+	"github.com/godbus/dbus/v5"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
+)
+
+const (
+	dbusName      = "org.chromium.Rgbkbd"
+	dbusPath      = "/org/chromium/Rgbkbd"
+	dbusInterface = "org.chromium.Rgbkbd"
+)
+
+// Rgbkbd is used to interact with the rgbkbd process over D-Bus.
+type Rgbkbd struct {
+	conn *dbus.Conn
+	obj  dbus.BusObject
+}
+
+// NewRgbkbd connects to rgbkbd via D-Bus and returns a Rgbkbd object.
+func NewRgbkbd(ctx context.Context) (*Rgbkbd, error) {
+	conn, obj, err := dbusutil.Connect(ctx, dbusName, dbusPath)
+	if err != nil {
+		return nil, err
+	}
+	return &Rgbkbd{conn, obj}, nil
+}
+
+// call is a thin wrapper over CallWithContext.
+func (c *Rgbkbd) call(ctx context.Context, method string, args ...interface{}) *dbus.Call {
+	return c.obj.CallWithContext(ctx, dbusInterface+"."+method, 0, args...)
+}
+
+// SetTestingMode allows the Rgbkbd daemon to write calls to a log file.
+func (c *Rgbkbd) SetTestingMode(ctx context.Context, capability uint32) error {
+	if err := c.call(ctx, "SetTestingMode", true, capability).Err; err != nil {
+		return errors.Wrap(err, "failed to call method SetTestingMode")
+	}
+	return nil
+}
