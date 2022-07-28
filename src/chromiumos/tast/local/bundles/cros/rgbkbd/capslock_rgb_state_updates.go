b1a5311337003877b592bb1f8ef14b725bb3c4a7
Michael Checo <michaelcheco@google.com>
2022-07-28 23:06:06 +0000

    rgbkbd: Update caps lock test and add dbus util
    BUG=b:214077425
    TEST=tast run <DUT> rgbkbd.CapslockRgbStateUpdates
    
    Change-Id: I0d619b2e31078199c9190bed1ced0130d6d3b717

--- src/chromiumos/tast/local/bundles/cros/rgbkbd/capslock_rgb_state_updates.go
+++ src/chromiumos/tast/local/bundles/cros/rgbkbd/capslock_rgb_state_updates.go
@@ -10,13 +10,11 @@ import (
 	"log"
 	"time"
 
-	"github.com/godbus/dbus/v5"
-
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
-	"chromiumos/tast/local/dbusutil"
 	"chromiumos/tast/local/input"
+	"chromiumos/tast/local/rgbkbd"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
 )
@@ -42,15 +40,28 @@ func CapslockRgbStateUpdates(ctx context.Context, s *testing.State) {
 		dbusName             = "org.chromium.Rgbkbd"
 		dbusPath             = "/org/chromium/Rgbkbd"
 		dbusInterface        = "org.chromium.Rgbkbd"
-		dbusMethod           = "SetTestingMode"
 		individualKey uint32 = 1
-
-		job = "rgbkbd"
+		job                  = "rgbkbd"
 	)
 
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
 	defer cancel()
+
+	s.Logf("Restarting %s job and waiting for %s service", job, dbusName)
+	if err := upstart.RestartJob(ctx, job); err != nil {
+		s.Fatalf("Failed to start %s: %v", job, err)
+	}
+
+	rgbkbd, err := rgbkbd.NewRgbkbd(ctx)
+	if err != nil {
+		s.Fatalf("Failed to connect to %s: %v", dbusName, err)
+	}
+
+	err = rgbkbd.SetTestingMode(ctx, individualKey)
+	if err != nil {
+		s.Error("Failed to set testing mode: ", err)
+	}
 
 	cr, err := chrome.New(ctx, chrome.EnableFeatures("RgbKeyboard"))
 	if err != nil {
@@ -63,21 +74,6 @@ func CapslockRgbStateUpdates(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to connect Test API: ", err)
 	}
 	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
-
-	s.Logf("Restarting %s job and waiting for %s service", job, dbusName)
-	if err := upstart.RestartJob(ctx, job); err != nil {
-		s.Fatalf("Failed to start %s: %v", job, err)
-	}
-
-	_, obj, err := dbusutil.Connect(ctx, dbusName, dbus.ObjectPath(dbusPath))
-	if err != nil {
-		s.Fatalf("Failed to connect to %s: %v", dbusName, err)
-	}
-
-	if err := obj.CallWithContext(ctx, dbusInterface+"."+
-		dbusMethod, 0, true, individualKey).Err; err != nil {
-		s.Error("Failed to set testing mode: ", err)
-	}
 
 	kb, err := input.Keyboard(ctx)
 	if err != nil {
@@ -95,10 +91,15 @@ func CapslockRgbStateUpdates(ctx context.Context, s *testing.State) {
 	}
 
 	var actualContent = string(content)
-	const expectedLog = "RGB::SetKeyColor - 44,25,55,210\n" +
-		"RGB::SetKeyColor - 57,25,55,210\n" +
+	const expectedLog = "RGB::SetKeyColor - 44,255,255,210\n" +
+		"RGB::SetKeyColor - 57,255,255,210\n" +
+		"RGB::SetAllKeyColors - 255,255,210\n" +
+		"RGB::SetAllKeyColors - 0,81,231\n" +
 		"RGB::SetKeyColor - 44,255,255,210\n" +
-		"RGB::SetKeyColor - 57,255,255,210\n"
+		"RGB::SetKeyColor - 57,255,255,210\n" +
+		"RGB::SetKeyColor - 44,0,81,231\n" +
+		"RGB::SetKeyColor - 57,0,81,231\n"
+
 	if actualContent != expectedLog {
 		s.Fatalf("Logs do not match: expected %s, actual %s",
 			expectedLog, actualContent)
