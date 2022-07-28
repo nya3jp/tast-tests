daec32fc55108a33712a7a7fec69917ff14fa061
Michael Checo <michaelcheco@google.com>
2022-07-28 23:06:06 +0000

    rgbkbd: Update caps lock test and add dbus util
    BUG=b:214077425
    TEST=tast run <DUT> rgbkbd.CapslockRgbStateUpdates
    
    Change-Id: I0d619b2e31078199c9190bed1ced0130d6d3b717

---
+++ src/chromiumos/tast/local/rgbkbd/util.go
@@ -0,0 +1,50 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package rgbkbd contains utilities for rgbkbd tests.
+package rgbkbd
+
+import (
+	"fmt"
+	"strings"
+)
+
+func printLogContents(expected, actual []string) string {
+	content := fmt.Sprintf("Expected: %v | Actual: %v", expected, actual)
+	return content
+}
+
+// AreLogsEqual checks if two logs contain the same entries in the same order.
+func AreLogsEqual(expected, actual []string) (bool, string) {
+	if len(expected) != len(actual) {
+		return false, fmt.Sprintf("Logs are not the same length: (%d, %d)", len(expected), len(actual))
+	}
+
+	for i, v := range actual {
+		if v != expected[i] {
+			return false, printLogContents(expected, actual)
+		}
+	}
+	return true, ""
+}
+
+// splitLog returns the log string as an array of individual entries.
+// We use '\n' as the separator because a newline is added between each log
+// entry.
+func splitLog(log string) []string {
+	lines := strings.Split(log, "\n")
+	// Remove last entry since '\n' adds an empty element to the end of the arr
+	return lines[:len(lines)-1]
+}
+
+// TODO(michaelcheco): Add tast test that verifies calls made at startup
+// (initial caps lock state, default keyboard backlight color) when RGB keyboard
+// is supported.
+
+// GetRemainingLog returns any logs written after the initial calls made at startup.
+func GetRemainingLog(log string) (remainingLog []string) {
+	split := splitLog(log)
+	remainingLog = split[4:]
+	return remainingLog
+}
