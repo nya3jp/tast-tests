e78961096d4854b109234505aeda17513eb6acb2
Cristina Guerrero <crisguerrero@chromium.org>
2022-07-29 15:48:18 +0000

    tast tests: Create common helper for rollback tests
    EnterpriseRollbackInPlace and EnterpriseRollbackPreviousVersion
    follow some steps that are similar so there is repeated code.
    
    Move common steps to a helper so they can be used in all
    rollback tests to clean up current tests and facilitate the
    creation of new ones.
    
    BUG=b:240247079
    TEST=tast -verbose run DUT policy.EnterpriseRollbackInPlace
    tast run -var=updateutil.tlwAddress="localhost:7151" DUT
    autoupdate.EnterpriseRollbackPreviousVersion.rollback_1_version
    
    Change-Id: I7e5eed17f3ae6d5159544efadc16b9fc8376ad38

--- src/chromiumos/tast/remote/bundles/cros/policy/enterprise_rollback_in_place.go
+++ src/chromiumos/tast/remote/bundles/cros/policy/enterprise_rollback_in_place.go
@@ -10,11 +10,8 @@ import (
 
 	"chromiumos/tast/common/hwsec"
 	"chromiumos/tast/ctxutil"
-	"chromiumos/tast/dut"
 	"chromiumos/tast/errors"
-	"chromiumos/tast/remote/policyutil"
-	"chromiumos/tast/rpc"
-	aupb "chromiumos/tast/services/cros/autoupdate"
+	"chromiumos/tast/remote/rollback"
 	"chromiumos/tast/testing"
 )
 
@@ -38,8 +35,6 @@ func init() {
 	})
 }
 
-var logsAndCrashes = []string{"/var/log", "/var/spool/crash", "/home/chronos/crash", "/mnt/stateful_partition/unencrypted/preserve/crash", "/run/crash_reporter/crash"}
-
 // EnterpriseRollbackInPlace does not expect to use enrollment so any
 // functionality that depend on the enrollment of the device should be not be
 // added to this test.
@@ -48,137 +43,32 @@ func EnterpriseRollbackInPlace(ctx context.Context, s *testing.State) {
 	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Minute)
 	defer cancel()
 	defer func(ctx context.Context) {
-		s.DUT().Conn().CommandContext(ctx, "stop", "oobe_config_save").Run()
-
-		if err := s.DUT().Conn().CommandContext(ctx, "rm", "-f", "/mnt/stateful_partition/.save_rollback_data").Run(); err != nil {
-			s.Error("Failed to remove data save flag: ", err)
-		}
-
-		if err := s.DUT().Conn().CommandContext(ctx, "rm", "-f", "/mnt/stateful_partition/rollback_data").Run(); err != nil {
-			s.Error("Failed to remove rollback data: ", err)
-		}
-
-		if err := simulatePowerwash(ctx, s.DUT(), s.RPCHint()); err != nil {
-			s.Error("Failed to simulate powerwash after test: ", err)
+		if err := rollback.ClearRollbackAndSystemData(ctx, s.DUT(), s.RPCHint()); err != nil {
+			s.Error("Failed to clean rollback data after test: ", err)
 		}
 	}(cleanupCtx)
 
-	if err := simulatePowerwash(ctx, s.DUT(), s.RPCHint()); err != nil {
+	if err := rollback.SimulatePowerwash(ctx, s.DUT(), s.RPCHint()); err != nil {
 		s.Fatal("Failed to simulate powerwash before test: ", err)
 	}
-	networksInfo, err := configureNetworks(ctx, s.DUT(), s.RPCHint())
+
+	networksInfo, err := rollback.ConfigureNetworks(ctx, s.DUT(), s.RPCHint())
 	if err != nil {
 		s.Fatal("Failed to configure networks: ", err)
 	}
-	if err := saveRollbackData(ctx, s.DUT()); err != nil {
-		s.Fatal("Failed to save rollback data: ", err)
-	}
 
-	sensitive, err := sensitiveDataForPstore(ctx, s.DUT())
+	sensitive, err := rollback.SaveRollbackData(ctx, s.DUT())
 	if err != nil {
-		s.Fatal("Failed to read sensitive data for pstore: ", err)
+		s.Fatal("Failed to save rollback data: ", err)
 	}
 
 	// Ineffective reset is ok here as the device steps through oobe automatically
 	s.Log("Simulating powerwash and rebooting the DUT to fake rollback")
-	if err := simulatePowerwashAndReboot(ctx, s.DUT()); err != nil && !errors.Is(err, hwsec.ErrIneffectiveReset) {
+	if err := rollback.SimulatePowerwashAndReboot(ctx, s.DUT()); err != nil && !errors.Is(err, hwsec.ErrIneffectiveReset) {
 		s.Fatal("Failed to simulate powerwash and reboot to fake an enterprise rollback: ", err)
 	}
 
-	if err := ensureSensitiveDataIsNotLogged(ctx, s.DUT(), sensitive); err != nil {
-		s.Fatal("Failed while checking that sensitive data is not logged: ", err)
-	}
-
-	if err := verifyRollback(ctx, networksInfo, s.DUT(), s.RPCHint()); err != nil {
+	if err := rollback.VerifyRollbackData(ctx, s.DUT(), s.RPCHint(), networksInfo, sensitive); err != nil {
 		s.Fatal("Failed to verify rollback: ", err)
 	}
-}
-
-// TODO(b/240247079): Move functions to a helper common for rollback.
-
-func simulatePowerwash(ctx context.Context, dut *dut.DUT, rpcHint *testing.RPCHint) error {
-	return policyutil.EnsureTPMAndSystemStateAreReset(ctx, dut, rpcHint)
-}
-
-// simulatePowerwashAndReboot relies on the helper method to always reboot apart
-// from doing the powerwash. If the internal functionality of
-// EnsureTPMAndSystemStateAreResetRemote changes in the future and does not
-// reboot the device, we will need to add reboot logic here for rollback to
-// happen. See b/240541326.
-func simulatePowerwashAndReboot(ctx context.Context, dut *dut.DUT) error {
-	return policyutil.EnsureTPMAndSystemStateAreResetRemote(ctx, dut)
-}
-
-func configureNetworks(ctx context.Context, dut *dut.DUT, rpcHint *testing.RPCHint) ([]*aupb.NetworkInformation, error) {
-	client, err := rpc.Dial(ctx, dut, rpcHint)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to connect to the RPC service on the DUT")
-	}
-	defer client.Close(ctx)
-
-	rollbackService := aupb.NewRollbackServiceClient(client.Conn)
-	response, err := rollbackService.SetUpNetworks(ctx, &aupb.SetUpNetworksRequest{})
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to configure networks on client")
-	}
-	return response.Networks, nil
-}
-
-func saveRollbackData(ctx context.Context, dut *dut.DUT) error {
-	if err := dut.Conn().CommandContext(ctx, "touch", "/mnt/stateful_partition/.save_rollback_data").Run(); err != nil {
-		return errors.Wrap(err, "failed to write rollback data save file")
-	}
-
-	if err := dut.Conn().CommandContext(ctx, "start", "oobe_config_save").Run(); err != nil {
-		return errors.Wrap(err, "failed to run oobe_config_save")
-	}
-
-	// The following two commands would be done by clobber_state during powerwash
-	// but the test does not powerwash.
-	if err := dut.Conn().CommandContext(ctx, "sh", "-c", `cat /var/lib/oobe_config_save/data_for_pstore > /dev/pmsg0`).Run(); err != nil {
-		return errors.Wrap(err, "failed to read rollback key")
-	}
-	// Adds a newline to pstore.
-	if err := dut.Conn().CommandContext(ctx, "sh", "-c", `echo "" >> /dev/pmsg0`).Run(); err != nil {
-		return errors.Wrap(err, "failed to add newline after rollback key")
-	}
-
-	return nil
-}
-
-func sensitiveDataForPstore(ctx context.Context, dut *dut.DUT) (string, error) {
-	sensitive, err := dut.Conn().CommandContext(ctx, "cat", "/var/lib/oobe_config_save/data_for_pstore").Output()
-	if err != nil {
-		return "", errors.Wrap(err, "failed to read data_for_pstore")
-	}
-	return string(sensitive), nil
-}
-
-func ensureSensitiveDataIsNotLogged(ctx context.Context, dut *dut.DUT, sensitive string) error {
-	for _, folder := range logsAndCrashes {
-		err := dut.Conn().CommandContext(ctx, "grep", "-rq", sensitive, folder).Run()
-		if err == nil {
-			return errors.Errorf("sensitive data found by grep in folder %q", folder)
-		}
-	}
-	return nil
-}
-
-func verifyRollback(ctx context.Context, networks []*aupb.NetworkInformation, dut *dut.DUT, rpcHint *testing.RPCHint) error {
-	client, err := rpc.Dial(ctx, dut, rpcHint)
-	if err != nil {
-		return errors.Wrap(err, "failed to connect to the RPC service on the DUT")
-	}
-	defer client.Close(ctx)
-
-	rollbackService := aupb.NewRollbackServiceClient(client.Conn)
-	response, err := rollbackService.VerifyRollback(ctx, &aupb.VerifyRollbackRequest{Networks: networks})
-	if err != nil {
-		return errors.Wrap(err, "failed to verify rollback on client")
-	}
-	if !response.Successful {
-		return errors.Errorf("rollback was not successful: %s", response.VerificationDetails)
-	}
-
-	return nil
 }
