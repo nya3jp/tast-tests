d15a3fb4aefdf381b44f3111278e7b9669f4a419
Danny Wang <wangdanny@google.com>
2022-08-02 23:24:09 +0000

    Feedback: Test to show missing description error message
    Verify clicking continue button with no issue description will
    show an error message. Now enter text, the error message will disappear.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.ShowMissingDescriptionErrorMessage
    
    Change-Id: Iab92e6f554728bbafb421a418902997cdee4f63f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3806335
    Reviewed-by: Wenyu Zhang <zhangwenyu@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Reviewed-by: Ricardo Quesada <ricardoq@chromium.org>
    Tested-by: Danny Wang <wangdanny@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/show_missing_description_error_message.go
@@ -0,0 +1,102 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ShowMissingDescriptionErrorMessage,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Show missing description error when continue without description",
+		Contacts: []string{
+			"wangdanny@google.com",
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// ShowMissingDescriptionErrorMessage verifies the user click continue button
+// with no issue description will show error message. Now enter text, error message will disappear.
+func ShowMissingDescriptionErrorMessage(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	s.Log("Setting up chrome")
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("OsFeedback"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
+
+	// Set up keyboard.
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to find keyboard: ", err)
+	}
+	defer kb.Close()
+
+	// Launch feedback app.
+	feedbackRootNode, err := feedbackapp.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app: ", err)
+	}
+
+	// Find continue button and click it.
+	continueButton := nodewith.Name("Continue").Role(role.Button)
+	if err := ui.DoDefault(continueButton)(ctx); err != nil {
+		s.Fatal("Failed to click continue button: ", err)
+	}
+
+	// Verify missing description error message exists.
+	missingDescriptionErrorMessage := nodewith.Name("Description is required").Role(
+		role.StaticText).Ancestor(feedbackRootNode)
+	if err := ui.WaitUntilExists(missingDescriptionErrorMessage)(ctx); err != nil {
+		s.Fatal("Failed to find missing description error message: ", err)
+	}
+
+	// Find the issue description text input.
+	issueDescriptionInput := nodewith.Role(role.TextField).Ancestor(feedbackRootNode)
+	if err := ui.EnsureFocused(issueDescriptionInput)(ctx); err != nil {
+		s.Fatal("Failed to find the issue description text input: ", err)
+	}
+
+	// Type issue description.
+	if err := kb.Type(ctx, "Make the Description is required disappear"); err != nil {
+		s.Fatal("Failed to type issue description: ", err)
+	}
+
+	// Verify missing description error message is disappeared.
+	if err := ui.WaitUntilGone(missingDescriptionErrorMessage)(ctx); err != nil {
+		s.Fatal("Failed to make missing description error message disappeared: ", err)
+	}
+}
