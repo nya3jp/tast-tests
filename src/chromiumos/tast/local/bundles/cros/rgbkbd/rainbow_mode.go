85da9d1dd4357510a5115b5992eef780f71e2574
Michael Checo <michaelcheco@google.com>
2022-08-03 17:13:10 +0000

    rgbkbd: rainbow mode tast test
    BUG=b:214077425
    TEST=tast run <DUT> rgbkbd.RainbowMode
    
    Change-Id: I1e3df72e44b8e7bdc7c9e68d6a3e43559a68c8c8

---
+++ src/chromiumos/tast/local/bundles/cros/rgbkbd/rainbow_mode.go
@@ -0,0 +1,120 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package rgbkbd
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/rgbkbd"
+	"chromiumos/tast/local/upstart"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         RainbowMode,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks that rainbow mode updates the correct number of keys for each device",
+		Contacts: []string{
+			"michaelcheco@google.com",
+			"cros-peripherals@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// RainbowMode checks that rainbow mode updates the correct number of keys for each device.
+func RainbowMode(ctx context.Context, s *testing.State) {
+	const (
+		dbusName             = "org.chromium.Rgbkbd"
+		dbusPath             = "/org/chromium/Rgbkbd"
+		dbusInterface        = "org.chromium.Rgbkbd"
+		dbusMethod           = "SetTestingMode"
+		individualKey uint32 = 1
+
+		job = "rgbkbd"
+	)
+	for _, tc := range []struct {
+		name           string
+		capability     uint32
+		expectedLogLen int
+	}{
+		{
+			name:           "kIndividualKey",
+			capability:     1,
+			expectedLogLen: 77,
+		}, {
+			name:           "kFourZoneFortyLed",
+			capability:     2,
+			expectedLogLen: 40,
+		},
+		{
+			name:           "kFourZoneTwelveLed",
+			capability:     3,
+			expectedLogLen: 12,
+		},
+		{
+			name:           "kFourZoneFourLed",
+			capability:     4,
+			expectedLogLen: 4,
+		},
+	} {
+		s.Run(ctx, tc.name, func(ctx context.Context, s *testing.State) {
+			cleanupCtx := ctx
+			ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+			defer cancel()
+
+			s.Logf("Restarting %s job and waiting for %s service", job, dbusName)
+			if err := upstart.RestartJob(ctx, job); err != nil {
+				s.Fatalf("Failed to start %s: %v", job, err)
+			}
+
+			rgbkbdService, err := rgbkbd.NewRgbkbd(ctx)
+			if err != nil {
+				s.Fatalf("Failed to connect to %s: %v", dbusName, err)
+			}
+
+			err = rgbkbdService.SetTestingMode(ctx, tc.capability)
+			if err != nil {
+				s.Fatal("Failed to set testing mode: ", err)
+			}
+
+			cr, err := chrome.New(ctx, chrome.EnableFeatures("RgbKeyboard"))
+			if err != nil {
+				s.Fatal("Failed to start Chrome: ", err)
+			}
+			defer cr.Close(cleanupCtx)
+
+			tconn, err := cr.TestAPIConn(ctx)
+			if err != nil {
+				s.Fatal("Failed to connect Test API: ", err)
+			}
+			defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+			err = rgbkbdService.SetRainbowMode(ctx)
+			if err != nil {
+				s.Fatal("Failed to call set rainbow mode: ", err)
+			}
+			content, err := ioutil.ReadFile("/run/rgbkbd/log")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			count, err := rgbkbd.RainbowModeCount(string(content))
+			if err != nil {
+				s.Fatal("Failed to get rainbow mode count: ", err)
+			}
+			if count != tc.expectedLogLen {
+				s.Fatalf("Unexpected # of calls to SetKeyColor, Expected (%d) | Actual (%d)", tc.expectedLogLen, count)
+			}
+		})
+	}
+}
