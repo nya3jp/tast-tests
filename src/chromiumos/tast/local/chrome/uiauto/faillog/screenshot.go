64ac042d858bc1f41de2778d0feb48e7846f784c
Jane Yang <jane.yang@cienet.com>
2022-08-03 17:47:36 +0800

    cuj: Take screenshots from both internal and external display on failure
    BUG=b:241730700
    TEST=fast_build.sh -T
    
    Change-Id: Ie1d873f2f511b1b83af93faf4bb50b575d3f4247

--- src/chromiumos/tast/local/chrome/uiauto/faillog/screenshot.go
+++ src/chromiumos/tast/local/chrome/uiauto/faillog/screenshot.go
@@ -7,10 +7,12 @@ package faillog
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/display"
 	"chromiumos/tast/local/screenshot"
 	"chromiumos/tast/testing"
 )
@@ -41,5 +43,25 @@ func SaveScreenshotToFileOnError(ctx context.Context, cr *chrome.Chrome, outDir 
 	testing.ContextLog(ctx, "Test failed. Saving screenshot to ", screenshotFile)
 	if err := screenshot.CaptureChrome(ctx, cr, screenshotFile); err != nil {
 		testing.ContextLog(ctx, "Failed to take screenshot: ", err)
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		testing.ContextLog(ctx, "Failed to obtain test API conn to dump UI tree: ", err)
+		return
+	}
+	displayInfos, err := display.GetInfo(ctx, tconn)
+	if err != nil {
+		testing.ContextLog(ctx, "Failed to get display info: ", err)
+		return
+	}
+	for idx, info := range displayInfos {
+		if idx > 0 {
+			screenshotFile = fmt.Sprintf("%s/%s-display-%d-%s.png", dir, fileName, idx, info.ID)
+		}
+		testing.ContextLogf(ctx, "Saving extended display %d screenshot to %s", idx, screenshotFile)
+		if err := screenshot.CaptureChromeForDisplay(ctx, cr, info.ID, screenshotFile); err != nil {
+			testing.ContextLogf(ctx, "Failed to capture screenshot for display ID %q: %v", info.ID, err)
+		}
 	}
 }
