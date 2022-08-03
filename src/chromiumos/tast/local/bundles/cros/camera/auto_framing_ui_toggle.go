652960b7e8d005c8679ec9fbe880fbdaebb1ccac
Ren-Pei Zeng <kamesan@chromium.org>
2022-08-03 19:23:39 +0800

    camera: Add AutoFramingUIToggle test
    Add camera.AutoFramingUIToggle Tast test that exercises camera
    auto-framing feature with UI interaction.
    
    BUG=b:210929757, b:221980963, b:247590171
    TEST=tast run $DUT camera.AutoFramingUIToggle
    
    Cq-Depend: chromium:3915647
    Change-Id: I18d5dcea7820d920111feafc2e5d2fa0c675ece9

---
+++ src/chromiumos/tast/local/bundles/cros/camera/auto_framing_ui_toggle.go
@@ -0,0 +1,209 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package camera
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/media/caps"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/camera/cca"
+	"chromiumos/tast/local/camera/histogramutil"
+	"chromiumos/tast/local/chrome/uiauto/quicksettings"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AutoFramingUIToggle,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks toggling Auto-framing from UI works",
+		Contacts:     []string{"kamesan@chromium.org", "chromeos-camera-eng@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"camera_feature_auto_framing", "chrome", caps.BuiltinCamera},
+		Fixture:      "ccaTestBridgeReady",
+	})
+}
+
+func AutoFramingUIToggle(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(cca.FixtureData).Chrome
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to establish connection to the test API extension")
+	}
+
+	type Action struct {
+		name string
+		run  func(ctx context.Context, app *cca.App) error
+	}
+	sleep := func(d time.Duration) Action {
+		return Action{
+			name: "sleep",
+			run: func(ctx context.Context, app *cca.App) error {
+				return testing.Sleep(ctx, d)
+			},
+		}
+	}
+	toggleFraming := func(enable bool) Action {
+		return Action{
+			name: "toggle \"Camera framing\" in Quick Settings",
+			run: func(ctx context.Context, app *cca.App) error {
+				return quicksettings.ToggleSetting(ctx, tconn, quicksettings.SettingPodCameraFraming, enable)
+			},
+		}
+	}
+	ccaTakePhotoNTimes := func(n int) Action {
+		return Action{
+			name: "take photo in CCA",
+			run: func(ctx context.Context, app *cca.App) error {
+				for i := 0; i < n; i++ {
+					if _, err := app.TakeSinglePhoto(ctx, cca.TimerOff); err != nil {
+						return errors.Wrapf(err, "failed to take %v-th photo", i)
+					}
+				}
+				return nil
+			},
+		}
+	}
+	var ccaRecordingStartTime time.Time
+	ccaStartRecording := func() Action {
+		return Action{
+			name: "start recording video in CCA",
+			run: func(ctx context.Context, app *cca.App) error {
+				ccaRecordingStartTime, err = app.StartRecording(ctx, cca.TimerOff)
+				return err
+			},
+		}
+	}
+	ccaStopRecording := func() Action {
+		return Action{
+			name: "stop recording video in CCA",
+			run: func(ctx context.Context, app *cca.App) error {
+				_, _, err := app.StopRecording(ctx, cca.TimerOff, ccaRecordingStartTime)
+				return err
+			},
+		}
+	}
+
+	const (
+		autoFramingNoError  = 0.0
+		minDetectionLatency = 1e3
+		enabledTimeMargin   = 10.0
+	)
+	histogramTests := func(enabledCount int, enabledTime float64) histogramutil.HistogramTests {
+		return histogramutil.HistogramTests{
+			"ChromeOS.Camera.AutoFraming.AverageDetectionLatency": histogramutil.AssertHistogramMeanGt(minDetectionLatency),
+			"ChromeOS.Camera.AutoFraming.DetectionHitRate":        histogramutil.AssertHistogramInRange(0.0, 100.0),
+			"ChromeOS.Camera.AutoFraming.EnabledCount":            histogramutil.AssertHistogramEq(float64(enabledCount)),
+			"ChromeOS.Camera.AutoFraming.EnabledTime":             histogramutil.AssertHistogramInRange(enabledTime-enabledTimeMargin, enabledTime+enabledTimeMargin),
+			"ChromeOS.Camera.AutoFraming.Error":                   histogramutil.AssertHistogramEq(autoFramingNoError),
+		}
+	}
+
+	runTestWithApp := s.FixtValue().(cca.FixtureData).RunTestWithApp
+	subTestTimeout := 60 * time.Second
+	actionInterval := 3 * time.Second
+	for _, tc := range []struct {
+		name           string
+		mode           cca.Mode
+		actions        []Action
+		histogramTests histogramutil.HistogramTests
+	}{
+		{
+			name: "testTogglingFraming",
+			mode: cca.Photo,
+			actions: []Action{
+				toggleFraming(true),
+				sleep(actionInterval),
+				toggleFraming(false),
+				sleep(actionInterval),
+				toggleFraming(true),
+				toggleFraming(false),
+				sleep(actionInterval),
+				toggleFraming(true),
+				toggleFraming(false),
+				toggleFraming(true),
+				sleep(actionInterval),
+				toggleFraming(false),
+				toggleFraming(true),
+				toggleFraming(false),
+				toggleFraming(true),
+				sleep(actionInterval),
+			},
+			// Quick clicks are debounced to at most one ON <-> OFF toggling.
+			histogramTests: histogramTests(3, 60.0),
+		},
+		{
+			name: "testPhotoTaking",
+			mode: cca.Photo,
+			actions: []Action{
+				toggleFraming(true),
+				sleep(actionInterval),
+				ccaTakePhotoNTimes(1),
+				toggleFraming(false),
+				sleep(actionInterval),
+				ccaTakePhotoNTimes(2),
+				toggleFraming(true),
+				sleep(actionInterval),
+				ccaTakePhotoNTimes(3),
+				toggleFraming(false),
+				sleep(actionInterval),
+				ccaTakePhotoNTimes(2),
+				toggleFraming(true),
+				sleep(actionInterval),
+				ccaTakePhotoNTimes(1),
+			},
+			histogramTests: histogramTests(3, 60.0),
+		},
+		{
+			name: "testVideoRecording",
+			mode: cca.Video,
+			actions: []Action{
+				toggleFraming(true),
+				ccaStartRecording(),
+				sleep(actionInterval),
+				toggleFraming(false),
+				sleep(actionInterval),
+				ccaStopRecording(),
+				sleep(actionInterval),
+				ccaStartRecording(),
+				sleep(actionInterval),
+				toggleFraming(true),
+				sleep(actionInterval),
+				ccaStopRecording(),
+			},
+			histogramTests: histogramTests(2, 40.0),
+		},
+	} {
+		subTestCtx, cancel := context.WithTimeout(ctx, subTestTimeout)
+		s.Run(subTestCtx, tc.name, func(ctx context.Context, s *testing.State) {
+			if err := runTestWithApp(ctx, func(ctx context.Context, app *cca.App) error {
+				if err := app.SwitchMode(ctx, tc.mode); err != nil {
+					return errors.Wrap(err, "failed to switch mode in CCA")
+				}
+				recorder, err := tc.histogramTests.Record(ctx, tconn)
+				if err != nil {
+					return errors.Wrap(err, "failed to start histogram recorder")
+				}
+				if err := quicksettings.Expand(ctx, tconn); err != nil {
+					return errors.Wrap(err, "failed to expand Quick Settings")
+				}
+				for _, a := range tc.actions {
+					if err := a.run(ctx, app); err != nil {
+						return errors.Wrapf(err, "failed to %v", a.name)
+					}
+				}
+				if err = app.Close(ctx); err != nil {
+					return errors.Wrap(err, "failed to close Chrome Camera App")
+				}
+				return tc.histogramTests.Verify(ctx, tconn, recorder)
+			}, cca.TestWithAppParams{StopAppOnlyIfExist: true}); err != nil {
+				s.Errorf("Failed to pass %v subtest: %v", tc.name, err)
+			}
+		})
+		cancel()
+	}
+}
