29d32d130a2ad4f72af903f6ac65edc4fa48a834
Jintao Lin <jintaolin@chromium.org>
2022-08-04 23:38:34 +0000

    wifi: Update Lacros metadata
    Update the metadata for wifi tests.
    1. wifi.PolicyBasic does not use browser, only use ash-chrome for
       enterprise policy provisioning
    2. wifi.UiToggleFromWifiSettings does not use browser, only use
       ash-chrome for network setting page operation
    3. wifi.ApBrowseInternet uses browser to check connection. But this case
       is not run in any test suites. No need to add a lacros variant until
       the purpose is clear (b:223502256)
    
    BUG=b:228326041
    TEST=./fast_build.sh -T
    
    Change-Id: I2035ac2d30ba603de2c55e9e671f6c5e1327401c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3811485
    Reviewed-by: Jie Jiang <jiejiang@chromium.org>
    Commit-Queue: Jintao Lin <jintaolin@chromium.org>
    Reviewed-by: Jun Sheng <shengjun@chromium.org>
    Tested-by: Jintao Lin <jintaolin@chromium.org>
    Reviewed-by: Billy Zhao <billyzhao@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/wifi/ui_toggle_from_wifi_settings.go
+++ src/chromiumos/tast/local/bundles/cros/wifi/ui_toggle_from_wifi_settings.go
@@ -22,7 +22,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         UIToggleFromWIFISettings,
-		LacrosStatus: testing.LacrosVariantUnknown,
+		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Enable and disable WiFi from ChromeOS Settings UI",
 		Contacts:     []string{"pathan.jilani@intel.com", "cros-network-health@google.com", "intel-chrome-system-automation-team@intel.com"},
 		Attr:         []string{"group:mainline", "informational"},
