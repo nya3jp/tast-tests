5755adb7d67be0f71022d270a16c116e192a8c5c
Jason Stanko <jstanko@google.com>
2022-08-04 00:19:30 +0000

    callbox: Fixed missing port in forwarding address
    Fixed issue preventing callbox fixture from establishing forwarded connection due to a misisng port in the address string.
    
    BUG=b:241401333
    TEST=tast run <DUT> cellular.AssertData
    
    Change-Id: I97886d5ed922bbb60de13c58d848220a2a7e2cba

--- src/chromiumos/tast/remote/cellular/callbox/manager/forwarded.go
+++ src/chromiumos/tast/remote/cellular/callbox/manager/forwarded.go
@@ -41,7 +41,7 @@ func newForwardedCallboxManager(ctx context.Context, config *forwardedCallboxMan
 		KeyDir:         config.sshKeyDir,
 		ConnectTimeout: 10 * time.Second,
 		User:           config.sshUser,
-		Hostname:       fmt.Sprintf("%s:%d", config.sshHostname, 22),
+		Hostname:       fmt.Sprintf("%s:%d", config.sshHostname, 2300),
 	}
 	conn, err := ssh.New(ctx, sshOptions)
 	if err != nil {
@@ -53,7 +53,7 @@ func newForwardedCallboxManager(ctx context.Context, config *forwardedCallboxMan
 	onFwdError := func(err error) {
 		testing.ContextLog(ctx, "Ssh forwarding error: ", err)
 	}
-	fcm.localPortForwarder, err = conn.ForwardLocalToRemote("tcp", "localhost", config.remoteCallboxManagerAddress, onFwdError)
+	fcm.localPortForwarder, err = conn.ForwardLocalToRemote("tcp", "localhost:5000", config.remoteCallboxManagerAddress, onFwdError)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to forward local port to remote callbox manager")
 	}
@@ -80,7 +80,7 @@ func (fcm *forwardedCallboxManager) LocalAddress() string {
 // Close closes the local port forward and the connection to the host.
 func (fcm *forwardedCallboxManager) Close(ctx context.Context) error {
 	var firstError error
-	if err := fcm.Close(ctx); err != nil {
+	if err := fcm.localPortForwarder.Close(); err != nil {
 		firstError = err
 	}
 	if err := fcm.host.Close(ctx); err != nil && firstError != nil {
