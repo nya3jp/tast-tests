1b854b48907d74a5350754f0351a1297b4765ad4
Ray Xu <rayxu@google.com>
2022-08-05 21:36:13 +0000

    printer: promote printer.ProxyAdd.* tast to CQ
    These tast tests are reliable passing as informational tests, promote
    them to CQ by updating attributes in add_test.go for these parameterize
    tast
    
    BUG=b:238639187
    TEST=tast run dut printer.ProxyAdd.media_source_by_pass_tray
    printer.ProxyAdd.media_source_bottom printer.ProxyAdd.oki_tray4
    printer.ProxyAdd.hp_ljcolor printer.ProxyAdd.nec printer.ProxyAdd.star_m
    
    Change-Id: Ib2bc93e2046187fac64e55005a34e71fb6ebc0b5

--- src/chromiumos/tast/local/bundles/cros/printer/add_test.go
+++ src/chromiumos/tast/local/bundles/cros/printer/add_test.go
@@ -111,6 +111,72 @@ func TestAddParams(t *testing.T) {
 		test("unsupported_no_pin", "printer_unsupported_GenericPostScript.ppd.gz", "printer_pin_print_unsupported_golden.ps"),
 		test("unsupported_pin", "printer_unsupported_GenericPostScript.ppd.gz", "printer_pin_print_unsupported_golden.ps", ippprint.WithJobPassword("1234")),
 	})
+	// TODO: promote printer.add* tests by merging codeWithProxy with code after b/240307209 is fixed
+	codeWithProxy := genparams.Template(t, `{{ range . }} {
+        Name: {{ .Name | fmt }},
+        Val: &ippprint.Params{
+                PPDFile:      {{ .PPDFile | fmt }},
+                PrintFile:    {{ .PrintFile | fmt }},
+                ExpectedFile: {{ .ExpectedFile | fmt }},
+                {{ if .Options }}
+                Options:      {{ .Options | fmt }},
+                {{ end }}
+        },
+        ExtraData: []string{ {{ .PrintFile | fmt }}, {{ .PPDFile | fmt }}, {{ .ExpectedFile | fmt }} },
+        {{ if .ExtraAttr }}
+        ExtraAttr: {{ .ExtraAttr | fmt }},
+        {{ end }}
+}, {{ end }}`, []base{
+		// Collate
+		test2("epson_software_collate", "printer_EpsonWF3620.ppd", "printer_collate_epson_software_collate_golden.bin", ippprint.WithCopies(2), ippprint.Collate()),
+		test2("epson_software_uncollated", "printer_EpsonWF3620.ppd", "printer_collate_epson_software_uncollated_golden.bin", ippprint.WithCopies(2)),
+		test2("epson_hardware_collate", "printer_EpsonWFC20590.ppd", "printer_collate_epson_hardware_collate_golden.ps", ippprint.WithCopies(2), ippprint.Collate()),
+		test2("epson_hardware_uncollated", "printer_EpsonWFC20590.ppd", "printer_collate_epson_hardware_uncollated_golden.ps", ippprint.WithCopies(2)),
+
+		// Resolution
+		test("lexmark_600dpi", "printer_Lexmark.ppd", "printer_resolution_lexmark_600dpi_golden.ps", ippprint.WithResolution("600dpi")),
+		test("lexmark_1200dpi", "printer_Lexmark.ppd", "printer_resolution_lexmark_1200dpi_golden.ps", ippprint.WithResolution("1200dpi")),
+		test("lexmark_2400x600dpi", "printer_Lexmark.ppd", "printer_resolution_lexmark_2400x600dpi_golden.ps", ippprint.WithResolution("2400x600dpi")),
+
+		// Verify common media-source values by-pass-tray (Multipurpose) and bottom (Lower).
+		test("media_source_by_pass_tray", "printer_add_generic_printer_GenericPostScript.ppd.gz", "printer_add_media_source_bypass_golden.ps", "media-source=by-pass-tray"),
+		test("media_source_bottom", "printer_add_generic_printer_GenericPostScript.ppd.gz", "printer_add_media_source_bottom_golden.ps", "media-source=bottom"),
+
+		// Verify that the OKI PPD with Tray4 is handled correctly.
+		test("oki_tray4", "printer_add_oki.ppd.gz", "printer_add_oki_tray4_golden.ps", "media-source=tray-4"),
+
+		// Add
+		test("dymo_lw", "printer_add_dymo_printer_lw450.ppd", "printer_add_dymo_lw_printer_golden.bin"),
+		test("dymo_lm", "printer_add_dymo_printer_lm450.ppd", "printer_add_dymo_lm_printer_golden.bin"),
+		test("epson", "printer_EpsonWF3620.ppd", "printer_add_epson_printer_golden.ps"),
+		test("epson_color", "printer_EpsonGenericColorModel.ppd", "printer_add_epson_printer_color_golden.bin", "print-color-mode=color"),
+		test("epson_monochrome", "printer_EpsonGenericColorModel.ppd", "printer_add_epson_printer_monochrome_golden.bin", "print-color-mode=monochrome"),
+		test("generic", "printer_add_generic_printer_GenericPostScript.ppd.gz", "printer_add_generic_printer_golden.ps"),
+		test("hp_pclm", "printer_add_hp_printer_pclm.ppd.gz", "printer_add_hp_printer_pclm_out.pclm"),
+		test("hp_ljcolor", "printer_add_hp_ljcolor.ppd.gz", "printer_add_hp_printer_ljcolor_out.pcl"),
+		test("hp_pwg_raster_color", "hp_ipp_everywhere.ppd", "printer_add_hp_ipp_everywhere_golden.pwg"),
+		test("hp_pwg_raster_monochrome", "hp_ipp_everywhere.ppd", "printer_add_hp_pwg_raster_monochrome_golden.pwg", "print-color-mode=monochrome"),
+		test("nec", "printer_nec_npdl.ppd", "printer_add_nec_golden.bin"),
+		test("star", "printer_add_star_printer_rastertostar.ppd.gz", "printer_add_star_printer_rastertostar.bin"),
+		test("star_m", "printer_add_star_printer_rastertostarm.ppd.gz", "printer_add_star_printer_rastertostarm.bin"),
+		test("star_lm", "printer_add_star_printer_rastertostarlm.ppd.gz", "printer_add_star_printer_rastertostarlm.bin"),
+
+		// Pin print
+		test("hp_no_pin", "printer_HP.ppd", "printer_pin_print_hp_no_pin_golden.ps"),
+		test("hp_pin", "printer_HP.ppd", "printer_pin_print_hp_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("lexmark_no_pin", "printer_Lexmark.ppd", "printer_pin_print_lexmark_no_pin_golden.ps"),
+		test("lexmark_pin", "printer_Lexmark.ppd", "printer_pin_print_lexmark_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("ricoh_jobpassword_no_pin", "printer_Ricoh_JobPassword.ppd", "printer_pin_print_ricoh_JobPassword_no_pin_golden.ps"),
+		test("ricoh_jobpassword_pin", "printer_Ricoh_JobPassword.ppd", "printer_pin_print_ricoh_JobPassword_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("ricoh_lockedprintpassword_nopin", "printer_Ricoh_LockedPrintPassword.ppd", "printer_pin_print_ricoh_LockedPrintPassword_no_pin_golden.ps"),
+		test("ricoh_lockedprintpassword_pin", "printer_Ricoh_LockedPrintPassword.ppd", "printer_pin_print_ricoh_LockedPrintPassword_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("ricoh_password_no_pin", "printer_Ricoh_password.ppd", "printer_pin_print_ricoh_password_no_pin_golden.ps"),
+		test("ricoh_password_pin", "printer_Ricoh_password.ppd", "printer_pin_print_ricoh_password_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("sharp_no_pin", "printer_Sharp.ppd", "printer_pin_print_sharp_no_pin_golden.ps"),
+		test("sharp_pin", "printer_Sharp.ppd", "printer_pin_print_sharp_pin_golden.ps", ippprint.WithJobPassword("1234")),
+		test("unsupported_no_pin", "printer_unsupported_GenericPostScript.ppd.gz", "printer_pin_print_unsupported_golden.ps"),
+		test("unsupported_pin", "printer_unsupported_GenericPostScript.ppd.gz", "printer_pin_print_unsupported_golden.ps", ippprint.WithJobPassword("1234")),
+	})
 	genparams.Ensure(t, "add.go", code)
-	genparams.Ensure(t, "proxy_add.go", code)
+	genparams.Ensure(t, "proxy_add.go", codeWithProxy)
 }
