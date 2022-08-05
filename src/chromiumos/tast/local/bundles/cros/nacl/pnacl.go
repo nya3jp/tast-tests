1363bc768cfe335e7c29652f11fe0da473615aaa
Oleksandr Kulkov <melfice@google.com>
2022-08-05 12:24:17 +0000

    Add Lacros variant of nacl.Pnacl
    BUG=chromium:1344120
    TEST=tast -verbose run ${DEVICE_IP} nacl.Pnacl.lacros
    
    Change-Id: I5e971d220988314dd981b7211c8416da8653be10

--- src/chromiumos/tast/local/bundles/cros/nacl/pnacl.go
+++ src/chromiumos/tast/local/bundles/cros/nacl/pnacl.go
@@ -12,6 +12,9 @@ import (
 
 	"chromiumos/tast/fsutil"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/lacros"
+	"chromiumos/tast/local/chrome/lacros/lacrosfixt"
 	"chromiumos/tast/testing"
 )
 
@@ -25,12 +28,20 @@ var extensionFiles = []string{
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         Pnacl,
-		LacrosStatus: testing.LacrosVariantNeeded,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Tests running a PNaCl module",
 		Contacts:     []string{"emaxx@chromium.org", "nacl-eng@google.com"},
 		Data:         extensionFiles,
 		SoftwareDeps: []string{"chrome", "nacl"},
 		Attr:         []string{"group:mainline"},
+		Params: []testing.Param{{
+			Val: browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Val:               browser.TypeLacros,
+		},
+		},
 	})
 }
 
@@ -53,15 +64,52 @@ func Pnacl(ctx context.Context, s *testing.State) {
 		s.Fatalf("Failed to compute extension ID for %v: %v", extDir, err)
 	}
 
-	cr, err := chrome.New(ctx, chrome.UnpackedExtension(extDir))
+	var opts []chrome.Option
+	bt := s.Param().(browser.Type)
+	switch bt {
+	case browser.TypeLacros:
+		cfg := lacrosfixt.NewConfig(
+			lacrosfixt.Mode(lacros.LacrosOnly),
+			lacrosfixt.UnpackedExtension(extDir),
+			lacrosfixt.ChromeOptions())
+
+		opts, err = cfg.Opts()
+		if err != nil {
+			s.Fatal("Failed to preparing lacros launching: ", err)
+		}
+	case browser.TypeAsh:
+		opts = []chrome.Option{
+			chrome.UnpackedExtension(extDir),
+		}
+	}
+
+	cr, err := chrome.New(ctx, opts...)
 	if err != nil {
 		s.Fatal("Chrome login failed: ", err)
 	}
 	defer cr.Close(ctx)
 
-	s.Log("Connecting to background page")
 	bgURL := chrome.ExtensionBackgroundPageURL(extID)
-	conn, err := cr.NewConnForTarget(ctx, chrome.MatchTargetURL(bgURL))
+	var conn *chrome.Conn
+	switch bt {
+	case browser.TypeLacros:
+		tconn, err := cr.TestAPIConn(ctx)
+		if err != nil {
+			s.Fatal("Failed to get ash tconn: ", err)
+		}
+
+		l, err := lacros.LaunchWithURL(ctx, tconn, "about:blank")
+		if err != nil {
+			s.Fatal("Failed to launch lacros: ", err)
+		}
+		defer l.Close(ctx)
+
+		s.Log("Connecting to background page")
+		conn, err = l.NewConnForTarget(ctx, chrome.MatchTargetURL(bgURL))
+	case browser.TypeAsh:
+		s.Log("Connecting to background page")
+		conn, err = cr.NewConnForTarget(ctx, chrome.MatchTargetURL(bgURL))
+	}
 	if err != nil {
 		s.Fatalf("Failed to connect to background page at %v: %v", bgURL, err)
 	}
