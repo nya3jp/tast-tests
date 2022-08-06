90303f904dbf2878c102264d98c0d2ea24a0de7b
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2022-08-06 00:24:24 +0000

    [CrOS Network] Add tast test for WiFi network toggle
    This change adds a new tast test, that checks to make sure toggling wifi
    in network quick settings detailed view updates the state of wifi in
    shill.
    
    BUG=b:240582778
    TEST=Ran newly added tast test on device
    
    Change-Id: I0d818d913ee343407fba36c021322358fbb7e79a

--- src/chromiumos/tast/local/chrome/uiauto/quicksettings/network.go
+++ src/chromiumos/tast/local/chrome/uiauto/quicksettings/network.go
@@ -16,22 +16,38 @@ var (
 	// NetworkDetailedView is the detailed Network view within the Quick Settings.
 	NetworkDetailedView = nodewith.HasClass("NetworkListView").Ancestor(RootFinder)
 
-	// NetworkFeaturePodLabelButton is the label child of the Network feature pod button.
+	// NetworkDetailedViewRevamp is the detailed Network view within Quick
+	// Settings with QuickSettingsNetworkRevamp enabled.
+	NetworkDetailedViewRevamp = nodewith.HasClass("NetworkDetailedNetworkViewImpl").Ancestor(RootFinder)
+
+	// NetworkFeaturePodLabelButton is the label child of the Network feature pod
+	// button.
 	NetworkFeaturePodLabelButton = nodewith.HasClass("FeaturePodLabelButton").NameContaining("network").Ancestor(RootFinder)
 
 	// networkSettingsButton is the button shown on the Network detailed view.
 	networkSettingsButton = nodewith.HasClass("IconButton").Name("Network settings").Ancestor(RootFinder)
+
+	// NetworkDetailedViewWifiToggleButtonRevamp is the WiFi toggle within the Network
+	// detailed view with QuickSettingsNetworkRevamp flag enabled.
+	NetworkDetailedViewWifiToggleButtonRevamp = nodewith.HasClass("TrayToggleButton").NameContaining("Wi-Fi").Ancestor(NetworkDetailedViewRevamp)
 )
 
 // NavigateToNetworkDetailedView will navigate to the detailed Network view
 // within the Quick Settings. This is safe to call even when the Quick Settings
 // are already open.
-func NavigateToNetworkDetailedView(ctx context.Context, tconn *chrome.TestConn) error {
+func NavigateToNetworkDetailedView(ctx context.Context, tconn *chrome.TestConn, revampEnabled bool) error {
 	if err := Expand(ctx, tconn); err != nil {
 		return err
 	}
 
 	ui := uiauto.New(tconn)
+
+	if revampEnabled {
+		return uiauto.Combine("click the Network feature pod label",
+			ui.LeftClick(NetworkFeaturePodLabelButton),
+			ui.WaitUntilExists(NetworkDetailedViewRevamp),
+		)(ctx)
+	}
 
 	return uiauto.Combine("click the Network feature pod label",
 		ui.LeftClick(NetworkFeaturePodLabelButton),
@@ -41,8 +57,16 @@ func NavigateToNetworkDetailedView(ctx context.Context, tconn *chrome.TestConn) 
 
 // OpenNetworkSettings will open the Network settings within the Quick Settings.
 // NavigateToNetworkDetailedView() must be called in advance.
-func OpenNetworkSettings(ctx context.Context, tconn *chrome.TestConn) error {
+func OpenNetworkSettings(ctx context.Context, tconn *chrome.TestConn, revampEnabled bool) error {
 	ui := uiauto.New(tconn)
+
+	if revampEnabled {
+		return uiauto.Combine("click the Network settings",
+			ui.LeftClick(networkSettingsButton),
+			ui.WaitUntilGone(NetworkDetailedViewRevamp),
+		)(ctx)
+	}
+
 	return uiauto.Combine("click the Network settings",
 		ui.LeftClick(networkSettingsButton),
 		ui.WaitUntilGone(NetworkDetailedView),
