f6363b811471f53f38f5d970061c96780bd0ff5d
Jason Stanko <jstanko@google.com>
2022-08-06 00:46:12 +0000

    iperf: Add ability to get server side logs from Iperf
    When running Iperf UDP test, client side results are inaccurate as they only include the data that was sent and not what was actually received. Though it is possible to get the server side results from the client when using the iperf CLI, it is not possible via the cmw500 API. This CL adds the ability to fetch the most recent results from the Iperf server.
    
    BUG=b:238343555
    TEST=tast run -var=callbox=<CB> -var=callboxManager=<CBM> <DUT> cellular.Iperf*
    
    Cq-Depend: chromium:3769602, chromium:3795621
    Change-Id: If4613cebe56a6ef7f186a4f7ec76844195f98730

--- src/chromiumos/tast/remote/cellular/callbox/iperf/callbox_server.go
+++ src/chromiumos/tast/remote/cellular/callbox/iperf/callbox_server.go
@@ -18,6 +18,7 @@ import (
 type CallboxServer struct {
 	client  *manager.CallboxManagerClient
 	callbox string
+	poller  *serverResultPoller
 }
 
 // NewCallboxServer creates a CallboxServer for the given callbox.
@@ -25,6 +26,7 @@ func NewCallboxServer(callbox string, client *manager.CallboxManagerClient) (*Ca
 	return &CallboxServer{
 		client:  client,
 		callbox: callbox,
+		poller:  newServerResultPoller(callbox, client),
 	}, nil
 }
 
@@ -50,29 +52,60 @@ func (c *CallboxServer) Start(ctx context.Context, cfg *iperf.Config) error {
 		return errors.Wrap(err, "failed to start Iperf server on the callbox")
 	}
 
+	if cfg.FetchServerResults {
+		if err := c.poller.start(ctx, cfg); err != nil {
+			return errors.Wrap(err, "failed to start Iperf server results poller")
+		}
+	}
+
 	return nil
 }
 
 // Close releases any additional resources held open by the server.
 func (c *CallboxServer) Close(ctx context.Context) {
-	ctx, cancel := context.WithTimeout(ctx, commandTimeoutMargin)
+	stopCtx, cancel := context.WithTimeout(ctx, commandTimeoutMargin)
 	defer cancel()
 
-	if err := c.client.CloseIperf(ctx, &manager.CloseIperfRequestBody{Callbox: c.callbox}); err != nil {
+	if err := c.client.CloseIperf(stopCtx, &manager.CloseIperfRequestBody{Callbox: c.callbox}); err != nil {
 		testing.ContextLog(ctx, "Failed to close Iperf, err: ", err)
+	}
+
+	stopCtx, cancel = context.WithTimeout(ctx, commandTimeoutMargin)
+	defer cancel()
+
+	if err := c.poller.stop(stopCtx); err != nil {
+		testing.ContextLog(ctx, "Failed to stop iperf server results poller, err: ", err)
 	}
 }
 
 // Stop terminates any Iperf servers running on the remote machine.
 func (c *CallboxServer) Stop(ctx context.Context) error {
-	ctx, cancel := context.WithTimeout(ctx, commandTimeoutMargin)
+	stopCtx, cancel := context.WithTimeout(ctx, commandTimeoutMargin)
 	defer cancel()
 
-	if err := c.client.StopIperf(ctx, &manager.StopIperfRequestBody{Callbox: c.callbox}); err != nil {
-		return errors.Wrap(err, "failed to stop Iperf on the callbox")
+	var allErrors error
+	if err := c.client.StopIperf(stopCtx, &manager.StopIperfRequestBody{Callbox: c.callbox}); err != nil {
+		allErrors = errors.Wrapf(allErrors, "failed to stop iperf on the callbox: %v", err) // NOLINT
 	}
 
-	return nil
+	stopCtx, cancel = context.WithTimeout(ctx, commandTimeoutMargin)
+	defer cancel()
+
+	if err := c.poller.stop(stopCtx); err != nil {
+		allErrors = errors.Wrapf(allErrors, "failed to stop iperf server results poller: %v", err) // NOLINT
+	}
+
+	return allErrors
+}
+
+// FetchResult fetches the most recently available result from the callbox server.
+func (c *CallboxServer) FetchResult(ctx context.Context, config *iperf.Config) (*iperf.Result, error) {
+	results, err := c.poller.fetchResult(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to fetch results from poller ")
+	}
+
+	return results, nil
 }
 
 func newServerRequest(c *CallboxServer, config *iperf.Config) *manager.ConfigureIperfRequestBody {
