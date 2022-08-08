bea867d6b634b7138eec69ce29ef2fe14c98e287
Ashley Prasad <ashleydp@google.com>
2022-08-08 18:26:33 +0000

    diagnosticsapp: remove information from test
    BUG=b:241802489
    TEST=Compiles
    
    Change-Id: Iba1d2603695eb4d0321f1f472529700d9fc9d0de

--- src/chromiumos/tast/local/bundles/cros/diagnostics/connectivity.go
+++ src/chromiumos/tast/local/bundles/cros/diagnostics/connectivity.go
@@ -26,7 +26,7 @@ func init() {
 			"menghuan@google.com",
 			"cros-peripherals@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"chrome"},
 	})
 }
