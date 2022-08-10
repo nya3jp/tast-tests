ab40ddec2cb4b1856975c697b0c9e760273e8495
wenyu zhang <zhangwenyu@google.com>
2022-08-10 05:47:25 +0000

    diagnosticsapp: Apply fixture to Connectivity tast test
    Apply diagnosticsPrep fixture to diagnostics.Connectivity
    tast test, to make sure cros_healthd service is running
    and SystemDataProvider mojom is connected.
    
    BUG=b:229165358
    TEST=tools/repeat-test.sh -s -r 20 --DUT diagnostics.Connectivity
    TEST=./fast_build.sh -T
    
    Change-Id: Id946b9a2c7deb069ddc0b21d873f8dfa8329e606

--- src/chromiumos/tast/local/bundles/cros/diagnostics/connectivity.go
+++ src/chromiumos/tast/local/bundles/cros/diagnostics/connectivity.go
@@ -11,7 +11,6 @@ import (
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/diagnosticsapp"
-	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/testing"
 )
 
@@ -28,27 +27,14 @@ func init() {
 		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
+		Fixture:      "diagnosticsPrep",
 	})
 }
 
 // Connectivity verifies that the Connectivity page can be navigated to.
 func Connectivity(ctx context.Context, s *testing.State) {
-	cr, err := chrome.New(ctx, chrome.EnableFeatures("DiagnosticsAppNavigation", "EnableNetworkingInDiagnosticsApp"))
-	if err != nil {
-		s.Fatal("Failed to start Chrome: ", err)
-	}
-	defer cr.Close(ctx) // Close our own chrome instance
-
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to connect Test API: ", err)
-	}
-	defer faillog.DumpUITreeOnError(ctx, s.OutDir(), s.HasError, tconn)
-
-	dxRootnode, err := diagnosticsapp.Launch(ctx, tconn)
-	if err != nil {
-		s.Fatal("Failed to launch diagnostics app: ", err)
-	}
+	tconn := s.FixtValue().(*chrome.TestConn)
+	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
 
 	// Open navigation if device is narrow view.
 	if err := diagnosticsapp.ClickNavigationMenuButton(ctx, tconn); err != nil {
@@ -56,9 +42,8 @@ func Connectivity(ctx context.Context, s *testing.State) {
 	}
 
 	// Find the Connectivity navigation item.
-	ui := uiauto.New(tconn)
-	connectivityTab := diagnosticsapp.DxConnectivity.Ancestor(dxRootnode)
-	if err := ui.WithTimeout(20 * time.Second).WaitUntilExists(connectivityTab)(ctx); err != nil {
+	connectivityTab := diagnosticsapp.DxConnectivity.Ancestor(diagnosticsapp.DxRootNode)
+	if err := ui.WaitUntilExists(connectivityTab)(ctx); err != nil {
 		s.Fatal("Failed to find the Connectivity navigation item: ", err)
 	}
 	pollOpts := testing.PollOptions{Interval: time.Second, Timeout: 20 * time.Second}
@@ -66,9 +51,9 @@ func Connectivity(ctx context.Context, s *testing.State) {
 		s.Fatal("Could not click the Connectivity tab: ", err)
 	}
 
-	// Find the first routine action button
-	networkListContainer := diagnosticsapp.DxNetworkList.Ancestor(dxRootnode)
-	if err := ui.WithTimeout(20 * time.Second).WaitUntilExists(networkListContainer)(ctx); err != nil {
+	// Find the first routine action button.
+	networkListContainer := diagnosticsapp.DxNetworkList.Ancestor(diagnosticsapp.DxRootNode)
+	if err := ui.WaitUntilExists(networkListContainer)(ctx); err != nil {
 		s.Fatal("Failed to find the network list: ", err)
 	}
 }
