b31123f78269ff06fb5dd4bbb11f56d35d633969
Chase Culpepper <cculpepper@google.com>
2022-08-10 16:43:44 +0000

    Adding timeout to error msg for testing.Poll
    Adding the timeout duration to the error message when the testing.Poll
    functions throws an error
    
    BUG=b:187788934
    TEST=Implemented meta test to demonstrate error from testing.Poll
    TEST=Ran unit tests
    
    Change-Id: I778c69a5c7657ad5d4c522402958430c84888493

---
+++ src/chromiumos/tast/local/bundles/cros/meta/local_poll.go
@@ -0,0 +1,40 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package meta
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:     LocalPoll,
+		Desc:     "Intentionally fails due to timeout",
+		Contacts: []string{"tast-owners@google.com"},
+	})
+}
+
+func LocalPoll(ctx context.Context, s *testing.State) {
+	// CAUTION: when using the Poll utility, errors should be handled correctly.
+	// Here, the test is expecting a timeour error because the passed in function
+	// always returns an error.
+	timeout := time.Second + 2*time.Millisecond + 3*time.Nanosecond
+	opts := &testing.PollOptions{
+		Timeout:  timeout,
+		Interval: time.Millisecond,
+	}
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		return errors.New("error to force to timeout")
+	}, opts); err != nil {
+		s.Logf("Get expected error from poll: %s", err)
+	} else {
+		s.Error("Error with poll: ", err)
+	}
+}
