10f0216308d2f93e9f6668b89c5395376f69e1c4
Jane Yang <jane.yang@cienet.com>
2022-08-10 17:01:57 +0800

    cuj: Measure the performance of simulated clinician workstation on Citrix Workspace app
    Test Scenario:
    1. Open Citrix Workspace client app and Maximize the app. All remaining actions are in Citrix.
    2. Open 3 browser windows x 2 tabs inside Citrix. Substitute Google, Google Photos, Wikipedia, WebMD for health provider website.
    3. Do a search in wikipedia and another in google (substitue for health query).
    4. Switch between browser windows/tabs.
    5. Concurrently Open google keep and type notes at 70 wpm.
    6. Upload photo to google photos.
    7. Logout and login again.
    
    BUG=b:244251009
    TEST=tast run <vars> <dut> spera.ClinicianWorkstationCUJ.basic
    
    Change-Id: I6eafaa7a1ad05ecd456a168e7f61e1942d7138dc

---
+++ src/chromiumos/tast/local/bundles/cros/spera/clinician_workstation_cuj.go
@@ -0,0 +1,108 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package spera
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/spera/enterprisecuj"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/cuj"
+	"chromiumos/tast/local/chrome/display"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ClinicianWorkstationCUJ,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Measure the performance of simulated clinician workstation operations on the Citrix Workspace client app",
+		Contacts:     []string{"xliu@cienet.com", "jane.yang@cienet.com"},
+		Vars: []string{
+			// Optional. Expecting "tablet" or "clamshell". Other values will be be taken as "clamshell".
+			"spera.cuj_mode",
+			// Required. Credentials used to login Citrix.
+			"spera.citrix_url",
+			"spera.citrix_username",
+			"spera.citrix_password",
+			"spera.citrix_desktopname",
+			// Required. Used for UI detection API.
+			"uidetection.key_type",
+			"uidetection.key",
+			"uidetection.server",
+		},
+		Params: []testing.Param{
+			{
+				Name:    "basic",
+				Fixture: "enrolledLoggedInToCUJUser",
+				Timeout: 10 * time.Minute,
+			},
+		},
+		Data: enterprisecuj.ClinicianWorkstationData,
+	})
+}
+
+func ClinicianWorkstationCUJ(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(chrome.HasChrome).Chrome()
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to test API: ", err)
+	}
+
+	var tabletMode bool
+	if mode, ok := s.Var("spera.cuj_mode"); ok {
+		tabletMode = mode == "tablet"
+		cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, tabletMode)
+		if err != nil {
+			s.Fatalf("Failed to enable tablet mode to %v: %v", tabletMode, err)
+		}
+		defer cleanup(cleanupCtx)
+	} else {
+		// Use default screen mode of the DUT.
+		tabletMode, err = ash.TabletModeEnabled(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to get DUT default screen mode: ", err)
+		}
+	}
+	s.Log("Running test with tablet mode: ", tabletMode)
+	var uiHandler cuj.UIActionHandler
+	if tabletMode {
+		cleanup, err := display.RotateToLandscape(ctx, tconn)
+		if err != nil {
+			s.Fatal("Failed to rotate display to landscape: ", err)
+		}
+		defer cleanup(cleanupCtx)
+		if uiHandler, err = cuj.NewTabletActionHandler(ctx, tconn); err != nil {
+			s.Fatal("Failed to create tablet action handler: ", err)
+		}
+	} else {
+		if uiHandler, err = cuj.NewClamshellActionHandler(ctx, tconn); err != nil {
+			s.Fatal("Failed to create clamshell action handler: ", err)
+		}
+	}
+
+	params := &enterprisecuj.TestParams{
+		OutDir:          s.OutDir(),
+		CitrixServerURL: s.RequiredVar("spera.citrix_url"),
+		CitrixUserName:  s.RequiredVar("spera.citrix_username"),
+		CitrixPassword:  s.RequiredVar("spera.citrix_password"),
+		DesktopName:     s.RequiredVar("spera.citrix_desktopname"),
+		TabletMode:      tabletMode,
+		DataPath:        s.DataPath,
+		UIHandler:       uiHandler,
+	}
+	scenario := enterprisecuj.NewClinicianWorkstationScenario()
+	if err := enterprisecuj.Run(ctx, cr, scenario, params); err != nil {
+		s.Fatal("Failed to run the clinician workstation cuj: ", err)
+	}
+}
