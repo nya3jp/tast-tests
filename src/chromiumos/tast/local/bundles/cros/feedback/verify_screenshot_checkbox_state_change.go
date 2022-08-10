fc543d8f022b23a2f5061a95ec38d510d8e85f49
Danny Wang <wangdanny@google.com>
2022-08-10 18:41:23 +0000

    Feedback: Verify the screenshot checkbox state change when click it
    Verify the the default state for screenshot checkbox is unchecked
    and user can check it to change the state.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.VerifyScreenshotCheckboxStateChange
    
    Change-Id: I26c6994c52b01fb96b7e1ba8286c5e51f996c729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3824424
    Tested-by: Danny Wang <wangdanny@google.com>
    Reviewed-by: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Mohamed Omar <mohamedaomar@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/verify_screenshot_checkbox_state_change.go
@@ -0,0 +1,77 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VerifyScreenshotCheckboxStateChange,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verify the screenshot checkbox state change when click it",
+		Contacts: []string{
+			"wangdanny@google.com",
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// VerifyScreenshotCheckboxStateChange verifies the default state for screenshot checkbox
+// is unchecked and user can check it to change the state.
+func VerifyScreenshotCheckboxStateChange(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	s.Log("Setting up chrome")
+	cr, err := chrome.New(ctx, chrome.EnableFeatures("OsFeedback"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
+
+	// Launch feedback app and go to share data page.
+	feedbackRootNode, err := feedbackapp.LaunchAndGoToShareDataPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and go to share data page: ", err)
+	}
+
+	// Verify the checkbox in share data page is unchecked and then click it.
+	checkboxAncestor := nodewith.Name("Attach screenshot").Role(
+		role.GenericContainer).Ancestor(feedbackRootNode)
+	checkbox := nodewith.Role(role.CheckBox).Ancestor(checkboxAncestor)
+	if err := uiauto.Combine("Verify checkbox is unchecked and click it",
+		ui.WaitUntilExists(checkbox.Attribute("checked", "false")),
+		ui.DoDefault(checkbox),
+		ui.WaitUntilExists(checkbox.Attribute("checked", "true")),
+	)(ctx); err != nil {
+		s.Fatal("Failed to change the checkbox state: ", err)
+	}
+}
