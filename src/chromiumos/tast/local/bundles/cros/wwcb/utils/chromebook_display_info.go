2ec10283e7049553fd3f7886e6c7c69add64e147
Judy Hsiao <judyhsiao@google.com>
2022-08-11 03:35:43 +0000

    Revert "docking: Fix comments in wwcb/utils and wwcb/dock1"
    This reverts commit 1ee275539cbc8dcf3ade2aee7976341325dc9e39.
    
    Reason for revert: b:242127622
    
    Original change's description:
    > docking: Fix comments in wwcb/utils and wwcb/dock1
    >
    > Fix comments in wwcb/utils and wwcb/dock1_persistent_settings.go to align with Go-style.
    > Also add couple files (chromebook_system.go, wwcb_constants.go & wwcb_usb.go).
    >
    > BUG=b:222188262
    > FIXED=b:222188262
    > TEST=tast run -var=utils.WWCBIP=192.168.1.198 -var=DockingID=1912901 -var=1stExtDispID=2007901 192.168.1.200 wwcb.Dock1PersistentSettings
    >
    > Change-Id: Ic4f8b5547eac00bc556c386c9be7f36a1b5737f1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3780745
    > Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    > Reviewed-by: Frank Lin <flin@google.com>
    > Tested-by: Frank Lin <flin@google.com>
    
    Bug=b:222188262, b:242127622
    
    Change-Id: I184c155577c65fd326f74235d7f09c77da184ce3

--- src/chromiumos/tast/local/bundles/cros/wwcb/utils/chromebook_display_info.go
+++ src/chromiumos/tast/local/bundles/cros/wwcb/utils/chromebook_display_info.go
@@ -19,6 +19,13 @@ import (
 	"chromiumos/tast/testing"
 )
 
+// ExternalDisplay returns a display which is not internal
+func ExternalDisplay(ctx context.Context, tconn *chrome.TestConn) (*display.Info, error) {
+	return display.FindInfo(ctx, tconn, func(info *display.Info) bool {
+		return !info.IsInternal
+	})
+}
+
 // GetInternalAndExternalDisplays returns internal and external display info.
 func GetInternalAndExternalDisplays(ctx context.Context, tconn *chrome.TestConn) (result DisplayLayout, err error) {
 	infos, err := display.GetInfo(ctx, tconn)
@@ -46,28 +53,29 @@ func GetInternalAndExternalDisplays(ctx context.Context, tconn *chrome.TestConn)
 	return result, err
 }
 
-// EnsureDisplayPrimary checks whether the given display is in requested display property. If not, make sure to set display property to the requested display property.
-func EnsureDisplayPrimary(ctx context.Context, tconn *chrome.TestConn, disp *display.Info) error {
+// EnsureDisplayIsPrimary if the display is not primary, then set properties & check
+func EnsureDisplayIsPrimary(ctx context.Context, tconn *chrome.TestConn, disp *display.Info) error {
+	// check prop at first
 	if disp.IsPrimary {
 		return nil
 	}
 
 	testing.ContextLogf(ctx, "Setting display [%s,%s] to be primary", disp.ID, disp.Name)
 
-	// Set the display to primary.
+	// set the display to primary
 	isPrimary := true
 	if err := display.SetDisplayProperties(ctx, tconn, disp.ID, display.DisplayProperties{IsPrimary: &isPrimary}); err != nil {
-		return errors.Wrap(err, "failed to set display properties")
+		return errors.Wrap(err, "failed to make internal display become primary")
 	}
 
-	// Expect the display is primary. Return err after poll timeout.
+	// check prop in the end
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
 		primaryInfo, err := display.GetPrimaryInfo(ctx, tconn)
 		if err != nil {
-			return errors.Wrap(err, "failed to get primary display info")
+			return errors.Wrap(err, "failed to get primary display info ")
 		}
 		if primaryInfo.ID != disp.ID {
-			return errors.New("unable to set display as primary")
+			return errors.New("failed to set want display to be primary: ")
 		}
 		return nil
 	}, &testing.PollOptions{Timeout: 5 * time.Second}); err != nil {
