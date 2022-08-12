244833426db1eae1818eda5cc13c552945fcdf1c
Chad Duffin <chadduffin@chromium.org>
2022-08-12 16:16:10 +0000

    [TAST] Prepare Bluetooth UI tests for Floss.
    This change makes all of the necessary preparation to be able to run the
    existing Bluetooth UI tests with Floss while still supporting BlueZ.
    
    BUG=b:242071154
    TEST=Ran affected tests on DUT.
    
    Change-Id: I7db277fd6ab1df6ec3f6646db04604e9e96fb982

---
+++ src/chromiumos/tast/local/bluetooth/bluez/bluez.go
@@ -0,0 +1,31 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package bluez contains helpers to interact with the system's bluetooth bluez
+// adapters.
+package bluez
+
+import (
+	"context"
+)
+
+// BlueZ provides an implementation of the Bluetooth interface used by
+// Bluetooth tests so that we can ensure coverage using BlueZ.
+type BlueZ struct {
+}
+
+// Enable powers on the adapter.
+func (b *BlueZ) Enable(ctx context.Context) error {
+	return Enable(ctx)
+}
+
+// PollForAdapterState polls the bluetooth adapter state until expected state is received or a timeout occurs.
+func (b *BlueZ) PollForAdapterState(ctx context.Context, exp bool) error {
+	return PollForAdapterState(ctx, exp)
+}
+
+// PollForEnabled polls the bluetooth adapter state until the adapter is powered on.
+func (b *BlueZ) PollForEnabled(ctx context.Context) error {
+	return PollForBTEnabled(ctx)
+}
---
+++ src/chromiumos/tast/local/bluetooth/floss/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package floss provides a Floss implementation of the Bluetooth interface.
+package floss
