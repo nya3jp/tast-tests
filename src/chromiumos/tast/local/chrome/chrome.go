e10707b9a29df515906dd62fe1436f46d17cd44c
Jake Barnes <jakebarnes@chromium.org>
2022-08-12 18:25:10 +1000

    tast-tests: Allow locking chrome from crosint camera fixture
    BUG=b:243858143
    TEST=running Cq-Depend tast tests
    
    Cq-Depend: chrome-internal:4930001, chromium:3881595, chromium:3881749
    Change-Id: I8fc772f555ddfeeda795f1b01d8b565a1e72ed58

--- src/chromiumos/tast/local/chrome/chrome.go
+++ src/chromiumos/tast/local/chrome/chrome.go
@@ -109,6 +109,7 @@ var prePackages = []string{
 	"chromiumos/tast/local/bundles/cros/inputs/fixture",
 	"chromiumos/tast/local/bundles/cros/inputs/pre",
 	"chromiumos/tast/local/bundles/crosint/arc",
+	"chromiumos/tast/local/bundles/crosint/camera/fixtures",
 	"chromiumos/tast/local/bundles/crosint/pita/pre",
 	"chromiumos/tast/local/bundles/pita/pita/pre",
 	"chromiumos/tast/local/chrome",
