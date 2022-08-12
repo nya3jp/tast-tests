bc6e68b6a40857ae52dbb6800a0c7cc87abd6c2b
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2022-08-11 19:21:18 -0700

    arcappcompat: Add 5 more apps to appcompat_release suite
    BUG=b:241147836
    TEST=test_that --board=eve <ip-address> tast.appcompat_release-shard-0
    test_that --board=eve <ip-address> tast.appcompat_release-shard-1
    test_that --board=eve <ip-address> tast.appcompat_release-shard-2
    test_that --board=eve <ip-address> tast.appcompat_release-shard-3
    TEST=tast unit test passed
    
    Cq-Depend: chromium:3811679
    Change-Id: Ic59548c6e4b16d9c7ea32932c723ed36d25ccf08

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/concepts.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/concepts.go
@@ -38,6 +38,54 @@ func init() {
 		SoftwareDeps: []string{"chrome"},
 		HardwareDeps: hwdep.D(hwdep.SkipOnModel("elm")),
 		Params: []testing.Param{{
+			Name: "clamshell_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  clamshellLaunchForConcepts,
+				ReleaseTests: testutil.ClamshellReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "tablet_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  touchviewLaunchForConcepts,
+				ReleaseTests: testutil.TouchviewReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "vm_clamshell_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  clamshellLaunchForConcepts,
+				ReleaseTests: testutil.ClamshellReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "vm_tablet_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  touchviewLaunchForConcepts,
+				ReleaseTests: testutil.TouchviewReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
 			Name: "clamshell_mode",
 			Val: testutil.TestParams{
 				LaunchTests: clamshellLaunchForConcepts,
--- src/chromiumos/tast/local/bundles/cros/arcappcompat/gmail.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/gmail.go
@@ -37,6 +37,54 @@ func init() {
 		Attr:         []string{"group:appcompat", "appcompat_smoke", "appcompat_top_apps"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
+			Name: "clamshell_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  clamshellLaunchForGmail,
+				ReleaseTests: testutil.ClamshellReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "tablet_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  touchviewLaunchForGmail,
+				ReleaseTests: testutil.TouchviewReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "vm_clamshell_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  clamshellLaunchForGmail,
+				ReleaseTests: testutil.ClamshellReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "vm_tablet_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  touchviewLaunchForGmail,
+				ReleaseTests: testutil.TouchviewReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
 			Name: "clamshell_mode",
 			Val: testutil.TestParams{
 				LaunchTests: clamshellLaunchForGmail,
