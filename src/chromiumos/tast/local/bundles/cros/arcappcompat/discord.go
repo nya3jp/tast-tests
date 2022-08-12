41bae1719c7cd6d1832b6c0b00189427578c0210
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2022-08-11 19:03:11 -0700

    arcappcompat: add “appcompat_release” as an extra attribute for release testing apps.
    BUG=b:241015051
    TEST=test_that --board=eve <ip-address> tast.appcompat_release-shard-0
    test_that --board=eve <ip-address> tast.appcompat_release-shard-1
    test_that --board=eve <ip-address> tast.appcompat_release-shard-2
    TEST=tast unit test passed
    
    This CL helps to run specific set of testcases for “appcompat_release” suite
    
    Change-Id: Ic9846adfd76f4234d16407fe196e0e47325eec1f

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/discord.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/discord.go
@@ -36,47 +36,51 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Functional test for Discord that installs the app also verifies it is logged in and that the main page is open, checks Discord correctly changes the window state in both clamshell and touchview mode",
 		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:appcompat", "appcompat_release"},
+		Attr:         []string{"group:appcompat"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
-			Name: "clamshell_mode",
+			Name: "clamshell_mode_release",
 			Val: testutil.TestParams{
-				LaunchTests: clamshellLaunchForDiscord,
-				CommonTests: testutil.ClamshellSmokeTests,
+				LaunchTests:  clamshellLaunchForDiscord,
+				ReleaseTests: testutil.ClamshellReleaseTests,
 			},
+			ExtraAttr:         []string{"appcompat_release"},
 			ExtraSoftwareDeps: []string{"android_p"},
 			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
 			// Skip on tablet only models.
 			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
 			Pre:               pre.AppCompatBootedUsingTestAccountPool,
 		}, {
-			Name: "tablet_mode",
+			Name: "tablet_mode_release",
 			Val: testutil.TestParams{
-				LaunchTests: touchviewLaunchForDiscord,
-				CommonTests: testutil.TouchviewSmokeTests,
+				LaunchTests:  touchviewLaunchForDiscord,
+				ReleaseTests: testutil.TouchviewReleaseTests,
 			},
+			ExtraAttr:         []string{"appcompat_release"},
 			ExtraSoftwareDeps: []string{"android_p"},
 			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
 			// Skip on clamshell only models.
 			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
 			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
 		}, {
-			Name: "vm_clamshell_mode",
+			Name: "vm_clamshell_mode_release",
 			Val: testutil.TestParams{
-				LaunchTests: clamshellLaunchForDiscord,
-				CommonTests: testutil.ClamshellSmokeTests,
+				LaunchTests:  clamshellLaunchForDiscord,
+				ReleaseTests: testutil.ClamshellReleaseTests,
 			},
+			ExtraAttr:         []string{"appcompat_release"},
 			ExtraSoftwareDeps: []string{"android_vm"},
 			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
 			// Skip on tablet only models.
 			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
 			Pre:               pre.AppCompatBootedUsingTestAccountPool,
 		}, {
-			Name: "vm_tablet_mode",
+			Name: "vm_tablet_mode_release",
 			Val: testutil.TestParams{
-				LaunchTests: touchviewLaunchForDiscord,
-				CommonTests: testutil.TouchviewSmokeTests,
+				LaunchTests:  touchviewLaunchForDiscord,
+				ReleaseTests: testutil.TouchviewReleaseTests,
 			},
+			ExtraAttr:         []string{"appcompat_release"},
 			ExtraSoftwareDeps: []string{"android_vm"},
 			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
 			// Skip on clamshell only models.
--- src/chromiumos/tast/local/bundles/cros/arcappcompat/minecraft.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/minecraft.go
@@ -36,9 +36,57 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Functional test for Minecraft that installs and checks Minecraft correctly changes the window state in both clamshell and touchview mode",
 		Contacts:     []string{"mthiyagarajan@chromium.org", "cros-appcompat-test-team@google.com"},
-		Attr:         []string{"group:appcompat", "appcompat_release", "appcompat_top_apps"},
+		Attr:         []string{"group:appcompat", "appcompat_top_apps"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
+			Name: "clamshell_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  clamshellLaunchForMinecraft,
+				ReleaseTests: testutil.ClamshellReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "tablet_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  touchviewLaunchForMinecraft,
+				ReleaseTests: testutil.TouchviewReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "vm_clamshell_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  clamshellLaunchForMinecraft,
+				ReleaseTests: testutil.ClamshellReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "vm_tablet_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  touchviewLaunchForMinecraft,
+				ReleaseTests: testutil.TouchviewReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
 			Name: "clamshell_mode",
 			Val: testutil.TestParams{
 				LaunchTests: clamshellLaunchForMinecraft,
