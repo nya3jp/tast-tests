55cfdda4396653138e13417ebb4de83d9e5be0e2
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-08-15 00:20:59 -0700

    tast-tests: Add WaitForAppVisible
    WaitForAppVisible finds app window by checking window title and waits
    until it becomes visible in ash.
    This can be used to make sure the app appears in the a11y tree
    after apps.Launch is called.
    
    Minor change: WaitForAppsCondition that doesn't need to be exported is
    removed.
    
    BUG=b:225229931
    TEST=tast_run wmp.VirtualDesksShortcuts*
    
    Change-Id: I8cb0c7a3acf180ad1912cd109d429bdda600e33a

--- src/chromiumos/tast/local/chrome/ash/shelf.go
+++ src/chromiumos/tast/local/chrome/ash/shelf.go
@@ -610,29 +610,43 @@ func AppShown(ctx context.Context, tconn *chrome.TestConn, appID string) (bool, 
 
 // WaitForApp waits for the app specified by appID to appear in the shelf.
 func WaitForApp(ctx context.Context, tconn *chrome.TestConn, appID string, timeout time.Duration) error {
-	return WaitForAppCondition(ctx, tconn, appID, timeout, uiPollingInterval, func() (bool, error) {
-		return AppShown(ctx, tconn, appID)
-	}, "app is not shown yet")
+	return testing.Poll(ctx, func(ctx context.Context) error {
+		if visible, err := AppShown(ctx, tconn, appID); err != nil {
+			return testing.PollBreak(err)
+		} else if !visible {
+			return errors.New("app is not shown yet")
+		}
+		return nil
+	}, &testing.PollOptions{Timeout: timeout})
 }
 
 // WaitForAppClosed waits for the app specified by appID to be closed.
 func WaitForAppClosed(ctx context.Context, tconn *chrome.TestConn, appID string) error {
-	return WaitForAppCondition(ctx, tconn, appID, time.Minute, uiPollingInterval, func() (bool, error) {
-		shown, err := AppShown(ctx, tconn, appID)
-		return !shown, err
-	}, "app is not closed yet")
-}
-
-// WaitForAppCondition waits for the app specified by appID to meet the given condition within the given timeout.
-func WaitForAppCondition(ctx context.Context, tconn *chrome.TestConn, appID string, timeout, interval time.Duration, cond func() (bool, error), msg string) error {
 	return testing.Poll(ctx, func(ctx context.Context) error {
-		if ok, err := cond(); err != nil {
+		if visible, err := AppShown(ctx, tconn, appID); err != nil {
 			return testing.PollBreak(err)
-		} else if !ok {
-			return errors.New(msg)
+		} else if visible {
+			return errors.New("app is not closed yet")
 		}
 		return nil
-	}, &testing.PollOptions{Timeout: timeout, Interval: interval})
+	}, &testing.PollOptions{Timeout: time.Minute})
+}
+
+// WaitForAppVisible waits for the app specified to be visible.
+func WaitForAppVisible(ctx context.Context, tconn *chrome.TestConn, appName string) error {
+	if err := WaitForCondition(ctx, tconn, func(w *Window) bool {
+		if !w.IsVisible {
+			return false
+		}
+		// Check app window by title. Note that Lacros window is prefixed with "Chrome", not "Lacros".
+		if w.WindowType == WindowTypeLacros {
+			return strings.Contains(w.Title, "Chrome")
+		}
+		return strings.Contains(w.Title, appName)
+	}, defaultPollOptions); err != nil {
+		return errors.Wrapf(err, "failed to wait for app to be visible for %v", appName)
+	}
+	return nil
 }
 
 // AutoHide sets shelf auto hide behavior from the wallpaper context menu.
