70884965e374e9f2f764af3f7a7b540e048f639a
Derek Beckett <dbeckett@chromium.org>
2022-08-15 12:47:18 -0700

    disable failing parameterized inputs.* tests
    Test is below minimum pass criteria.
    
    BUG=b:241943011
    TEST=cq
    
    Change-Id: I06db5649292a4b996552491817a6af23bffc336d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3832621
    Reviewed-by: Jun Sheng <shengjun@chromium.org>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Jun Sheng <shengjun@chromium.org>
    Auto-Submit: Derek Beckett <dbeckett@chromium.org>
    Tested-by: Derek Beckett <dbeckett@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_input_fields.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_typing_input_fields.go
@@ -64,13 +64,15 @@ func init() {
 				ExtraAttr:         []string{"informational"},
 				ExtraHardwareDeps: hwdep.D(pre.InputsUnstableModels),
 			},
+			/* Disabled due to <1% pass rate over 30 days. See b/241943011
 			{
 				Name:              "lacros",
 				ExtraHardwareDeps: hwdep.D(pre.InputsStableModels),
 				Fixture:           fixture.LacrosTabletVK,
 				ExtraAttr:         []string{"informational"},
 				ExtraSoftwareDeps: []string{"lacros"},
-			},
+			}
+			*/
 		},
 	})
 }
