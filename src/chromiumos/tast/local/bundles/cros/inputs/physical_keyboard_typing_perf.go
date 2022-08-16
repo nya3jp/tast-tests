8614a8b843a241b27828b6712321db71b3417386
Darren Shen <shend@chromium.org>
2022-08-16 15:50:35 +1000

    tast: Add physical keyboard typing latency tests.
    Add a performance test for key event latency on physical keyboard.
    Use the UMA histogram InputMethod.KeyEventLatency which measures
    the time spent in the input method (excludes time spent in the app).
    
    To start, we'll measure the latency of US English and Chinese Pinyin.
    The input data needs to be improved as it is not representative of
    the real world.
    
    Measure the mean, % of key events that are < 5ms (called "fast"),
    % of key events that are < 20ms (called "acceptable"). This allows
    us to capture the overall shape with the mean, as well as capture
    any outlier values.
    
    We will later define objectives for each of these metrics.
    
    BUG=b:241994527
    TEST=tast run <dut> inputs.PhysicalKeyboardPerformance*
    
    Change-Id: I12cd3ff211cac31ac56b24bc127cfa65e5eaaa86

---
+++ src/chromiumos/tast/local/bundles/cros/inputs/physical_keyboard_typing_perf.go
@@ -0,0 +1,182 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package inputs
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/perf"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/bundles/cros/inputs/fixture"
+	"chromiumos/tast/local/bundles/cros/inputs/testserver"
+	"chromiumos/tast/local/bundles/cros/inputs/util"
+	"chromiumos/tast/local/chrome/ime"
+	"chromiumos/tast/local/chrome/metrics"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/useractions"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+type typingPerfTestParam struct {
+	inputMethod ime.InputMethod
+	keys        string
+}
+
+// TODO(b/241994527): Use better input data that better reflect real world usage.
+
+// From 'Alice in Wonderland'
+var enUSTestData = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversations?'"
+
+// From go/3828217 with random concatenation.
+var pinyinTestData = "ke yi zuo ziji xiang zuodeshi chuan lai de ziji de shengyin henyou yisi de zhe yang xian ba zai zhe bian hai yu dao le zhe ming de zhu chiren li chen zhe ci lai shi weile xin chang pian de shi buguo da jia buyong dan xinwo hui jin kuaihao qi laide dan mei ci doumei you jihui quyou lan yi xiazai yan chu qian wojie shou le zhuan fang"
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         PhysicalKeyboardTypingPerf,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Checks the physical keyboard typing performance",
+		Contacts:     []string{"shend@chromium.org", "essential-inputs-team@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome", "chrome_internal"},
+		SearchFlags:  util.IMESearchFlags([]ime.InputMethod{ime.EnglishUS, ime.ChinesePinyin}),
+		Timeout:      5 * time.Minute,
+		Params: []testing.Param{
+			{
+				Name:    "en_us",
+				Fixture: fixture.ClamshellNonVK,
+				Val: typingPerfTestParam{
+					inputMethod: ime.EnglishUS,
+					keys:        enUSTestData,
+				},
+				ExtraAttr: []string{"group:input-tools-upstream"},
+			},
+			{
+				Name:    "en_us_lacros",
+				Fixture: fixture.LacrosClamshellNonVK,
+				Val: typingPerfTestParam{
+					inputMethod: ime.EnglishUS,
+					keys:        enUSTestData,
+				},
+				ExtraSoftwareDeps: []string{"lacros"},
+			},
+			{
+				Name:    "pinyin",
+				Fixture: fixture.ClamshellNonVK,
+				Val: typingPerfTestParam{
+					inputMethod: ime.ChinesePinyin,
+					keys:        pinyinTestData,
+				},
+				ExtraAttr: []string{"group:input-tools-upstream"},
+			},
+			{
+				Name:    "pinyin_lacros",
+				Fixture: fixture.LacrosClamshellNonVK,
+				Val: typingPerfTestParam{
+					inputMethod: ime.ChinesePinyin,
+					keys:        pinyinTestData,
+				},
+				ExtraSoftwareDeps: []string{"lacros"},
+			},
+		},
+	})
+}
+
+func PhysicalKeyboardTypingPerf(ctx context.Context, s *testing.State) {
+	inputMethod := s.Param().(typingPerfTestParam).inputMethod
+	testKeys := s.Param().(typingPerfTestParam).keys
+	cr := s.FixtValue().(fixture.FixtData).Chrome
+	tconn := s.FixtValue().(fixture.FixtData).TestAPIConn
+	uc := s.FixtValue().(fixture.FixtData).UserContext
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
+
+	// Add IME for testing.
+	if err := inputMethod.InstallAndActivateUserAction(uc)(ctx); err != nil {
+		s.Fatal("Failed to switch to Korean IME")
+	}
+	uc.SetAttribute(useractions.AttributeInputMethod, inputMethod.Name)
+
+	keyboard, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to get keyboard: ", err)
+	}
+	defer keyboard.Close()
+
+	its, err := testserver.LaunchBrowser(ctx, s.FixtValue().(fixture.FixtData).BrowserType, cr, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch inputs test server: ", err)
+	}
+	defer its.CloseAll(cleanupCtx)
+
+	var inputField = testserver.TextAreaInputField
+
+	if err := uiauto.UserAction(
+		"PK input",
+		uiauto.Combine("clear input field and type",
+			its.ClearThenClickFieldAndWaitForActive(inputField),
+			keyboard.TypeAction(testKeys),
+		),
+		uc, &useractions.UserActionCfg{
+			Attributes: map[string]string{
+				useractions.AttributeTestScenario: "type long text",
+				useractions.AttributeFeature:      useractions.FeaturePKTyping,
+				useractions.AttributeInputField:   string(inputField),
+			},
+		},
+	)(ctx); err != nil {
+		s.Fatalf("Failed to type in %s: %v", inputField, err)
+	}
+
+	latencyHistogram, err := metrics.GetHistogram(ctx, tconn, "InputMethod.KeyEventLatency")
+	if err != nil {
+		s.Fatal("Failed to get histograms: ", err)
+	}
+
+	pv := perf.NewValues()
+
+	// Record the mean.
+	mean, err := latencyHistogram.Mean()
+	if err != nil {
+		s.Fatal("Failed to get mean: ", err)
+	}
+	pv.Set(perf.Metric{
+		Name:      "key_latency_mean",
+		Unit:      "ms",
+		Direction: perf.SmallerIsBetter,
+	}, mean)
+
+	// Record percentage of 'fast' key events. We define 'fast' as < 5ms.
+	percentFast, err := util.PercentSamplesBelow(latencyHistogram, 5)
+	if err != nil {
+		s.Fatal("Failed to get percentage of fast key events: ", err)
+	}
+	pv.Set(perf.Metric{
+		Name:      "key_latency_fast",
+		Unit:      "percent",
+		Direction: perf.BiggerIsBetter,
+	}, percentFast)
+
+	// Record percentage of 'acceptable' key events. We define 'acceptable' as < 15ms.
+	percentAcceptable, err := util.PercentSamplesBelow(latencyHistogram, 15)
+	if err != nil {
+		s.Fatal("Failed to get percentage of acceptable key events: ", err)
+	}
+	pv.Set(perf.Metric{
+		Name:      "key_latency_acceptable",
+		Unit:      "percent",
+		Direction: perf.BiggerIsBetter,
+	}, percentAcceptable)
+
+	if err := pv.Save(s.OutDir()); err != nil {
+		s.Error("Failed saving perf data: ", err)
+	}
+}
