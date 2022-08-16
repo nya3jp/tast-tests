dadad63d7eeff5f79f34689b8ce12245743c7f90
Ruben Zakarian <rzakarian@google.com>
2022-08-16 11:08:08 -0700

    policy: Modify integration test.
    Creating a helper file for annotations.
    
    BUG=b:241820021
    TEST=ran test on local and lab machines
    
    Change-Id: Iee53dc45210d81e8592bc075e5e9c848aaabd235

---
+++ src/chromiumos/tast/local/bundles/cros/policy/annotations/annotations.go
@@ -0,0 +1,94 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package annotations
+
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/cryptohome"
+)
+
+func startLogging(ctx context.Context, cr *chrome.Chrome, br *browser.Browser) error {
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		errors.Wrap(err, "failed to create Test API connection")
+	}
+
+	netConn, err := br.NewConn(ctx, "chrome://net-export")
+	if err != nil {
+		errors.Wrap(err, "failed to load chrome://net-export")
+	}
+
+	// Click Start Log button.
+	startLoggingBtn := `document.getElementById("start-logging")`
+	if err := netConn.WaitForExpr(ctx, startLoggingBtn); err != nil {
+		errors.Wrap(err, "failed to wait for the Start Logging button to load")
+
+	}
+	if err := netConn.Eval(ctx, startLoggingBtn+`.click()`, nil); err != nil {
+		errors.Wrap(err, "failed to click the Start Logging button")
+	}
+
+	// Click Save button to choose the filename for log file.
+	ui := uiauto.New(tconn)
+	saveButton := nodewith.Name("Save").Role(role.Button)
+	if err := uiauto.Combine("Click 'Save' button",
+		ui.WaitUntilExists(saveButton),
+		ui.WaitUntilEnabled(saveButton),
+		ui.DoDefault(saveButton),
+		ui.WaitUntilGone(saveButton),
+	)(ctx); err != nil {
+		errors.Wrap(err, "failed to click")
+	}
+	return nil
+}
+
+func stopLoggingCheckLogs(ctx context.Context, cr *chrome.Chrome, br *browser.Browser) error {
+	// Open the net-export page.
+	netConn, err := br.NewConn(ctx, "chrome://net-export")
+	if err != nil {
+		errors.Wrap(err, "failed to load chrome://net-export")
+	}
+
+	// Click Stop Logging button.
+	stopLoggingBtn := `document.getElementById("stop-logging")`
+	if err := netConn.WaitForExpr(ctx, stopLoggingBtn); err != nil {
+		errors.Wrap(err, "failed to wait for the Stop Logging button to load")
+	}
+	if err := netConn.Eval(ctx, stopLoggingBtn+`.click()`, nil); err != nil {
+		errors.Wrap(err, "failed to click the Stop Logging button to load")
+	}
+
+	// Get the net export log file.
+	downloadsPath, err := cryptohome.DownloadsPath(ctx, cr.NormalizedUser())
+	if err != nil {
+		errors.Wrap(err, "failed to get user's Download path")
+	}
+	downloadName := "chrome-net-export-log.json"
+	downloadLocation := filepath.Join(downloadsPath, downloadName)
+
+	// Read the net export log file.
+	logFile, err := ioutil.ReadFile(downloadLocation)
+	if err != nil {
+		errors.Wrap(err, "failed to open logfile")
+	}
+	// Check if the traffic annotation exists in the log file.
+	// Specifically checking for autofill_query:88863520.
+	isExist, _ := regexp.Match("\"traffic_annotation\":88863520", logFile)
+	if !isExist {
+		errors.Wrap(err, "failed to locate traffic annotation in logfile")
+	}
+
+	return nil
+}
---
+++ src/chromiumos/tast/local/bundles/cros/policy/annotations/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package annotations provides the implementation and helper functions for traffic annotations related policy tests.
+package annotations
