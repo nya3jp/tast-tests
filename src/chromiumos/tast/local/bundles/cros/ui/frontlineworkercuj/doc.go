22fa7a5771abb3d54d0fe54abdc9b17c19de8c68
Alston Huang <alston.huang@cienet.com>
2022-08-16 10:22:14 +0800

    cuj: Move FrontlineWorkerCUJ to SPERA directory - phase 2
    Remove files from ui directory.
    
    BUG=b:242590511
    TEST=tast run <vars> <dut> spera.FrontlineWorkerCUJ.*
    
    Change-Id: Ic581d04a2c7fa76365c9c1144d64c36c247b1c86

--- src/chromiumos/tast/local/bundles/cros/ui/frontlineworkercuj/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 The ChromiumOS Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Package frontlineworkercuj contains frontlineworker CUJ test cases library.
-package frontlineworkercuj
