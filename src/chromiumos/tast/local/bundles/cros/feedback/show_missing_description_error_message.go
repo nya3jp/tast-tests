979ea305096de9440451d92cbb22c3456ef57bb3
Danny Wang <wangdanny@google.com>
2022-08-16 20:39:30 +0000

    Feedback: Add chromeLoggedInWithOsFeedback fixture
    BUG=b:185624798
    TEST=tast run <DUT> feedback.<test_name>
    
    Change-Id: I9104c76f6e74cc528fe4c47a1b0c69220beb67fc

--- src/chromiumos/tast/local/bundles/cros/feedback/show_missing_description_error_message.go
+++ src/chromiumos/tast/local/bundles/cros/feedback/show_missing_description_error_message.go
@@ -30,6 +30,7 @@ func init() {
 			"xiangdongkong@google.com",
 			"cros-feedback-app@google.com",
 		},
+		Fixture:      "chromeLoggedInWithOsFeedback",
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 	})
@@ -38,16 +39,11 @@ func init() {
 // ShowMissingDescriptionErrorMessage verifies the user click continue button
 // with no issue description will show error message. Now enter text, error message will disappear.
 func ShowMissingDescriptionErrorMessage(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
 	defer cancel()
-
-	s.Log("Setting up chrome")
-	cr, err := chrome.New(ctx, chrome.EnableFeatures("OsFeedback"))
-	if err != nil {
-		s.Fatal("Failed to start Chrome: ", err)
-	}
-	defer cr.Close(cleanupCtx)
 
 	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
