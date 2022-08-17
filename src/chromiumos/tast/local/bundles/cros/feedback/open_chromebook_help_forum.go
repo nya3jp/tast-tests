878c05a484aa5084f76ea7bbed0e2f61f9eaa022
Danny Wang <wangdanny@google.com>
2022-08-17 22:10:50 +0000

    Feedback: Verify user can open chromebook help forum
    BUG=b:185624798
    TEST=tast run <DUT> feedback.OpenChromebookHelpForum.*
    
    Change-Id: I5d4084a60a7afa820f07f6d535154783d961e99f

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/open_chromebook_help_forum.go
@@ -0,0 +1,79 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         OpenChromebookHelpForum,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "User is able to open chromebook help forum",
+		Contacts: []string{
+			"wangdanny@google.com",
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Fixture:      "chromeLoggedInWithOsFeedback",
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+const chromebookHelpForumAddress = "support.google.com/chromebook/?hl=en#topic=3399709"
+
+// OpenChromebookHelpForum verifies the user is able to open chromebook help forum.
+func OpenChromebookHelpForum(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn)
+
+	// Launch feedback app and navigate to confirmation page.
+	feedbackRootNode, err := feedbackapp.LaunchAndGoToConfirmationPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and go to confirmation page: ", err)
+	}
+
+	// Find link and click.
+	link := nodewith.NameContaining("Chromebook community").Ancestor(feedbackRootNode)
+	if err := ui.DoDefault(link)(ctx); err != nil {
+		s.Fatal("Failed to find link: ", err)
+	}
+
+	// Verify browser is opened.
+	if err = ash.WaitForApp(ctx, tconn, apps.Chrome.ID, time.Minute); err != nil {
+		s.Fatal("Could not find browser in shelf after launch: ", err)
+	}
+
+	// Verify browser contains correct address.
+	if err := nodewith.Name(chromebookHelpForumAddress); err == nil {
+		s.Fatal("Failed to find link address: ", err)
+	}
+}
