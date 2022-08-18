57280b0cf5d55f06befd0546cb59abf0fe7b562b
Jacek Siuda <jck@semihalf.com>
2022-08-18 15:51:31 +0000

    wifi: Improve SoftAP Capability test
    Log device name.
    Refactor the test code slightly: move checks to subfunctions.
    Fail test only if 2.4G band is missing (SoftAP feature MVP).
    Log number of Clients only if information is present.
    
    BUG=b:243012527
    TEST=tast run $DUT wifi.SAPCaps
    TEST=fast_build.sh -T
    
    Change-Id: I8ee68a23eaefd41856b319f0a7ffe69a8c9f31f4

--- src/chromiumos/tast/local/bundles/cros/wifi/sap_caps.go
+++ src/chromiumos/tast/local/bundles/cros/wifi/sap_caps.go
@@ -8,10 +8,13 @@ import (
 	"context"
 	"io/ioutil"
 	"path/filepath"
+	"reflect"
 
-	"chromiumos/tast/local/network/iw"
+	"chromiumos/tast/common/network/iw"
+	localiw "chromiumos/tast/local/network/iw"
 	"chromiumos/tast/testing"
 	"chromiumos/tast/testing/hwdep"
+	"chromiumos/tast/testing/wlan"
 )
 
 func init() {
@@ -27,8 +30,23 @@ func init() {
 	})
 }
 
+// SAPCaps verifies level of support for Soft AP according to the driver.
 func SAPCaps(ctx context.Context, s *testing.State) {
-	iwr := iw.NewLocalRunner()
+	// This test checks that DUT has necessary capabilities to run Soft AP (MVP):
+	// * Supports AP mode,
+	// * 2.4GHz band,
+	// * HE in AP mode if HE is present in STA mode.
+	// Also, it detects opportunities for more functionalities:
+	// * Max number of Clients (if information is present).
+	// Concurrency is already checked by ConcurrencyCaps test, we won't do it here.
+	dev, err := wlan.DeviceInfo()
+	if err != nil {
+		s.Fatal("Failed reading the WLAN device information: ", err)
+	}
+	// Log the actual device name.
+	s.Log("Device: ", dev.Name)
+
+	iwr := localiw.NewLocalRunner()
 	res, out, err := iwr.ListPhys(ctx)
 	if err != nil {
 		s.Fatal("ListPhys failed: ", err)
@@ -40,14 +58,58 @@ func SAPCaps(ctx context.Context, s *testing.State) {
 	// Save `iw list` text to log file.
 	ioutil.WriteFile(filepath.Join(s.OutDir(), "iw_list"), out, 0644)
 
-	apSupported := false
-	for _, mode := range res[0].Modes {
-		if mode == "AP" {
-			apSupported = true
+	checkMode := func(phy *iw.Phy, mode iw.IfType) bool {
+		for _, m := range phy.Modes {
+			if m == string(mode) {
+				testing.ContextLogf(ctx, "Device supports %s mode", mode)
+				return true
+			}
 		}
+		return false
 	}
-	if !apSupported {
-		s.Error("AP mode not supported")
+
+	checkBands := func(phy *iw.Phy) bool {
+		var has24GHz bool
+		for _, b := range phy.Bands {
+			if len(b.FrequencyFlags) == 0 {
+				// Just-in-case.
+				continue
+			}
+			// Frequency is coded in map keys.
+			keys := reflect.ValueOf(b.FrequencyFlags).MapKeys()
+			f := keys[0].Interface().(int)
+			if f >= 2412 && f <= 2484 {
+				has24GHz = true
+				// We're not looking for any more bands.
+				break
+			} else if f >= 5160 && f < 5925 {
+				// 5GHz
+			} else if f >= 5925 && f <= 7115 {
+				// 6GHz
+			} else {
+				testing.ContextLogf(ctx, "Unknown frequency %d", f)
+			}
+		}
+		// Minimum requirement for MVP
+		return has24GHz
+	}
+
+	// We're using phys[0] basing on two assumptions:
+	// 1. All phys of the same device will have the same capabilities.
+	// 2. We support only one WiFi device per DUT.
+	if !checkMode(res[0], iw.IfTypeAP) {
+		s.Error("Device does not declare AP-mode support")
+	}
+
+	if !checkBands(res[0]) {
+		s.Error("Device does not declare all expected bands (2.4G)")
+	}
+
+	if res[0].MaxSTAs > 0 {
+		s.Log("Max concurrent clients: ", res[0].MaxSTAs)
+		if res[0].MaxSTAs < 16 {
+			s.Log("Less than 16 associated STAs are supported in AP mode: ", res[0].MaxSTAs)
+		}
 	}
 
 	if res[0].SupportHESTA {
@@ -62,7 +124,4 @@ func SAPCaps(ctx context.Context, s *testing.State) {
 		}
 	}
 
-	if res[0].MaxSTAs < 16 {
-		s.Log("Less than 16 associated STAs are supported in AP mode: ", res[0].MaxSTAs)
-	}
 }
