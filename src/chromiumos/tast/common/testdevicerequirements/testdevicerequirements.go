a63cd07eed2d82022d20bbaf02f07116da9f890d
Jesse McGuire <jessemcguire@google.com>
2022-08-19 09:46:32 -0700

    tast-tests: Add requirements mapping to boot_perf
    Add requirements mapping to boot_perf in support of traceability POC in
    PVS.
    
    BUG=b:241470929
    TEST=compiled bundle, ran test, CQ
    
    Change-Id: Ib5147239442d228f529a0f034a8a5979c6cdbac1

---
+++ src/chromiumos/tast/common/testdevicerequirements/testdevicerequirements.go
@@ -0,0 +1,33 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package testdevicerequirements provides a way to define ChromeOS
+// software and device requirements used for mapping test cases to
+// requirements
+//
+// # Usage
+//
+// import tdreq "chromiumos/tast/common/testdevicerequirement"
+//
+// testing.AddTest(&testing.Test{
+//
+//	Func:         ExampleTest,
+//	LacrosStatus: testing.LacrosVariantNeeded,
+//	Desc:         "Fake example test",
+//	...
+//	Requirements: []string{tdreq.BootPerfKernel, tdreq.BootPerfLogin},
+//	...
+//
+// )
+//
+// Contact cros-test-xfn-requirements@google.com with any questions.
+package testdevicerequirements
+
+const (
+	// BootPerfKernel is a ChromeOS Platform requirement. Please contact cros-test-xfn-requirements@google.com with any questions.
+	BootPerfKernel = "boot-perf-0001-v01"
+
+	// BootPerfLogin is a ChromeOS Platform requirement. Please contact cros-test-xfn-requirements@google.com with any questions.
+	BootPerfLogin = "boot-perf-0002-v01"
+)
