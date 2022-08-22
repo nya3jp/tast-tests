1ebee9c892f866d48bb970171f7be451236701b0
Stefan Buciu <stefanbuciu@google.com>
2022-08-22 09:44:06 +0000

    policy: Add PCI tags to policy tests
    Move PCI utils to the common directory and rename constants. Add Search
    Flags to a couple of policy tests.
    
    BUG=b:243362191
    TEST=run a modified test on a DUT
    
    Change-Id: I700a7903e55dab804f7bc606b7fbbf48f8ba7c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3845006
    Tested-by: Ștefan Buciu <stefanbuciu@google.com>
    Commit-Queue: Ștefan Buciu <stefanbuciu@google.com>
    Reviewed-by: Kamil Szarek <kamilszarek@google.com>
    Reviewed-by: Alexander Hartl <alexanderhartl@google.com>

--- src/chromiumos/tast/local/bundles/cros/policy/util/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2022 The ChromiumOS Authors.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Package util contains some common utilities or constants that are used by
-// multiple policy tests.
-package util
