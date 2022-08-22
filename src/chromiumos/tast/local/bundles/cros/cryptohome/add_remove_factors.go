024ef0b691b52efc1c1ccfdb46eb58eb13999461
John Admanski <jadmanski@google.com>
2022-08-22 15:33:39 -0700

    tast-tests: Add supported factors coverage to the AddRemoveFactors test
    This doesn't modify the overall test flow, it just adds more to the
    expectations. In addition to checking the configured factors, the test
    now also checks the supported factors.
    
    The expectations aren't quite as simple because we need to look at some
    of the test and hardware parameters to decided exactly what the expected
    factors are, but they're still pretty basic to compute.
    
    BUG=b:227729894
    TEST=tast test <dut> cryptohome.AddRemoveFactors.*
    TEST=tast unit tests passed
    
    Cq-Depend: chromium:3833556
    Change-Id: I673dfc8c1e5bbc513bd37504304ac4875b22f92a

--- src/chromiumos/tast/local/bundles/cros/cryptohome/add_remove_factors.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/add_remove_factors.go
@@ -8,6 +8,9 @@ import (
 	"context"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
 	uda "chromiumos/system_api/user_data_auth_proto"
 	"chromiumos/tast/common/hwsec"
 	"chromiumos/tast/ctxutil"
@@ -46,19 +49,24 @@ func init() {
 }
 
 // Structure for specifying the expected fields in an AuthFactor.
-type expectedFactor struct {
+type expectedConfiguredFactor struct {
 	factorType uda.AuthFactorType
 	label      string
 }
 
 // compareReplyToExpectations will compare the reply from ListAuthFactors to a set of expected factors.
-// The `when` parameter should be a string that can be included in the error messages describing when this
-// expectation was checked. It should generally look something like "before xyz" or "after abc".
-func compareReplyToExpectations(when string, reply *uda.ListAuthFactorsReply, expectations []expectedFactor, s *testing.State) {
-	if len(reply.GetConfiguredAuthFactors()) != len(expectations) {
-		s.Fatalf("ListAuthFactors reported the wrong number of factors (got %d, want %d) %s", len(reply.GetConfiguredAuthFactors()), len(expectations), when)
+// The comparison will check both configured factors (specified by type and label only) as well as the
+// supported factors.
+//
+// The `when` parameter should be a string that can be included in the error messages describing when
+// this expectation was checked. It should generally look something like "before xyz" or "after abc".
+func compareReplyToExpectations(when string, reply *uda.ListAuthFactorsReply, expectedConfigured []expectedConfiguredFactor, expectedSupported []uda.AuthFactorType, s *testing.State) {
+	// Compare the configured and expected configured factors. Currently order matters, but it might make
+	// sense to change that in the future as we configure more factors in the test.
+	if len(reply.GetConfiguredAuthFactors()) != len(expectedConfigured) {
+		s.Fatalf("ListAuthFactors reported the wrong number of factors (got %d, want %d) %s", len(reply.GetConfiguredAuthFactors()), len(expectedConfigured), when)
 	}
-	for i, expected := range expectations {
+	for i, expected := range expectedConfigured {
 		factor := reply.ConfiguredAuthFactors[i]
 		if factor.Type != expected.factorType {
 			s.Fatalf("Auth factor %d does not have the correct type (got %d, want %d) %s", i, factor.Type, expected.factorType, when)
@@ -66,6 +74,11 @@ func compareReplyToExpectations(when string, reply *uda.ListAuthFactorsReply, ex
 		if factor.Label != expected.label {
 			s.Fatalf("Auth factor %d does not have the correct label (got %q, want %q) %s", i, factor.Label, expected.label, when)
 		}
+	}
+	// Compare the supported and expected supports factors. Order does not matter.
+	typeLess := func(a, b uda.AuthFactorType) bool { return a < b }
+	if diff := cmp.Diff(reply.SupportedAuthFactors, expectedSupported, cmpopts.SortSlices(typeLess)); diff != "" {
+		s.Errorf("Mismatch in supported auth factors %s (-got +want) %s", when, diff)
 	}
 }
 
@@ -102,6 +115,12 @@ func AddRemoveFactors(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to remove old vault for preparation: ", err)
 	}
 
+	// Determine if the client supports using PIN auth.
+	supportsPin, err := client.SupportsLECredentials(ctx)
+	if err != nil {
+		s.Fatal("Unable to determine if PINs are supported: ", err)
+	}
+
 	// Enable the UserSecretStash experiment if USS is specified.
 	if userParam.useUserSecretStash {
 		cleanupUSSExperiment, err := helper.EnableUserSecretStash(ctx)
@@ -125,18 +144,36 @@ func AddRemoveFactors(ctx context.Context, s *testing.State) {
 	}
 	defer client.UnmountAll(ctxForCleanUp)
 
-	// Expected auth factors at different points in the test.
-	var expectedNoFactors = []expectedFactor{}
-	var expectedOnlyPassword = []expectedFactor{
+	// Expected configured auth factors at different points in the test.
+	var expectedNoFactors = []expectedConfiguredFactor{}
+	var expectedOnlyPassword = []expectedConfiguredFactor{
 		{uda.AuthFactorType_AUTH_FACTOR_TYPE_PASSWORD, passwordLabel},
 	}
 
-	// List the auth factors for the user. There should be none.
+	// Expected supported auth factors at different points in the test.
+	var expectedAllSupported = []uda.AuthFactorType{
+		uda.AuthFactorType_AUTH_FACTOR_TYPE_PASSWORD,
+		uda.AuthFactorType_AUTH_FACTOR_TYPE_KIOSK,
+	}
+	var expectedNoKioskSupported = []uda.AuthFactorType{
+		uda.AuthFactorType_AUTH_FACTOR_TYPE_PASSWORD,
+	}
+	// The supported lists may also need PIN or RECOVERY, depending on the DUT and the test params.
+	if supportsPin {
+		expectedAllSupported = append(expectedAllSupported, uda.AuthFactorType_AUTH_FACTOR_TYPE_PIN)
+		expectedNoKioskSupported = append(expectedNoKioskSupported, uda.AuthFactorType_AUTH_FACTOR_TYPE_PIN)
+	}
+	if userParam.useUserSecretStash {
+		expectedAllSupported = append(expectedAllSupported, uda.AuthFactorType_AUTH_FACTOR_TYPE_CRYPTOHOME_RECOVERY)
+		expectedNoKioskSupported = append(expectedNoKioskSupported, uda.AuthFactorType_AUTH_FACTOR_TYPE_CRYPTOHOME_RECOVERY)
+	}
+
+	// List the auth factors for the user. There should be no factors, configured, an maximum factors supported.
 	listFactorsAtStartReply, err := client.ListAuthFactors(ctx, userName)
 	if err != nil {
 		s.Fatal("Failed to list auth factors before adding any factors: ", err)
 	}
-	compareReplyToExpectations("before adding factors", listFactorsAtStartReply, expectedNoFactors, s)
+	compareReplyToExpectations("before adding factors", listFactorsAtStartReply, expectedNoFactors, expectedAllSupported, s)
 
 	// Add a password auth factor to the user.
 	if err := client.AddAuthFactor(ctx, authSessionID, passwordLabel, userPassword); err != nil {
@@ -148,7 +185,7 @@ func AddRemoveFactors(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to list auth factors after adding password: ", err)
 	}
-	compareReplyToExpectations("after adding password", listFactorsAfterAddPasswordReply, expectedOnlyPassword, s)
+	compareReplyToExpectations("after adding password", listFactorsAfterAddPasswordReply, expectedOnlyPassword, expectedNoKioskSupported, s)
 
 	// Unmount the user.
 	if err := client.UnmountAll(ctx); err != nil {
@@ -160,5 +197,5 @@ func AddRemoveFactors(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to list auth factors after unmount: ", err)
 	}
-	compareReplyToExpectations("after unmount", listFactorsAfterUnmount, expectedOnlyPassword, s)
+	compareReplyToExpectations("after unmount", listFactorsAfterUnmount, expectedOnlyPassword, expectedNoKioskSupported, s)
 }
