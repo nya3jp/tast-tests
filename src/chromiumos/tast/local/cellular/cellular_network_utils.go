060cbc275ea16ee38f252d6ccea6b290fdd730f4
Madhav <madhavadas@google.com>
2022-08-24 00:33:43 +0000

    tast-tests: Add speed test to cellular interface
    Added support for running speedtest.py on cellular
    interface
    
    BUG=b:244581860
    TEST=tast -verbose run dut cellular.HostCellularSpeedtest
    
    Cq-Depend: chromium:3900425, chromium:3888779
    Change-Id: I025c49299262374a1056b755df179c7ec066257b

--- src/chromiumos/tast/local/cellular/cellular_network_utils.go
+++ src/chromiumos/tast/local/cellular/cellular_network_utils.go
@@ -6,6 +6,7 @@ package cellular
 
 import (
 	"context"
+	"encoding/json"
 	"path/filepath"
 	"time"
 
@@ -16,6 +17,7 @@ import (
 
 const (
 	pingTimeout      = 10 * time.Second
+	speedtestTimeout = 1 * time.Minute
 	googleDotComIPv6 = "ipv6.google.com"
 	googleDotComIPv4 = "ipv4.google.com"
 )
@@ -103,4 +105,28 @@ func VerifyIPConnectivity(ctx context.Context, cmd func(context.Context, string,
 		return errors.New("no ip network found")
 	}
 	return nil
+}
+
+// RunHostIPSpeedTest runs speedtest on cellular interface and returns the download and upload speeds in bps
+func RunHostIPSpeedTest(ctx context.Context, cmd func(context.Context, string, ...string) *testexec.Cmd, bindir string) (upload, download float64, err error) {
+	testing.ContextLog(ctx, "Run IP Connectivity Speed Test")
+	var uploadSpeed, downloadSpeed float64 = 0.0, 0.0
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		out, err := cmd(ctx, filepath.Join(bindir, "speedtest-cli"), "--json").Output()
+		if err != nil {
+			return errors.Wrap(err, "failed speed test")
+		}
+		var data map[string]interface{}
+		if err := json.Unmarshal(out, &data); err != nil {
+			return errors.Wrap(err, "failed to unmarshal output")
+		}
+		downloadSpeed = data["download"].(float64)
+		uploadSpeed = data["upload"].(float64)
+		testing.ContextLogf(ctx, " Download %.2f bps", downloadSpeed)
+		testing.ContextLogf(ctx, " Upload   %.2f bps", uploadSpeed)
+		return nil
+	}, &testing.PollOptions{Timeout: speedtestTimeout}); err != nil {
+		return 0, 0, errors.Wrap(err, "failed speed test")
+	}
+	return uploadSpeed, downloadSpeed, nil
 }
