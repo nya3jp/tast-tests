274b714849797bf9a073c003718d19375f8694be
Matt Stark <msta@google.com>
2022-08-24 12:33:27 +1000

    Add a tconn service that allows direct interaction with chrome APIs via tconn.
    BUG=b:242936676
    TEST=tast test ui.TconnServiceGRPC passes
    
    Change-Id: Ie9519b3f274410b81b140a94ce53110d8b3b0270

--- src/chromiumos/tast/services/cros/ui/gen.go
+++ src/chromiumos/tast/services/cros/ui/gen.go
@@ -2,14 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. conference_service.proto
-//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. screenlock_service.proto
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. audio_service.proto
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. automation_service.proto
 //go:generate protoc -I . --go_out=plugins=grpc:../../../../.. check_power_menu_service.proto
 //go:generate protoc -I . --go_out=plugins=grpc:../../../../.. check_virtual_keyboard_service.proto
 //go:generate protoc -I . --go_out=plugins=grpc:../../../../.. chrome_service.proto
-//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. automation_service.proto
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. conference_service.proto
 //go:generate protoc -I . --go_out=plugins=grpc:../../../../.. screen_recorder_service.proto
-//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. audio_service.proto
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. screenlock_service.proto
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. tconn_service.proto
 
 // Package ui provides all ui related types compiled from protobuf.
 package ui
