ca896b6233fa2082ba2e21a2e5bccec3339618f1
Jason Stanko <jstanko@google.com>
2022-08-24 02:43:56 +0000

    [WiP]tast-tests: Refactor CellularPolicyConnection
    Refactors network.CellularPolicyConnection to use NetworkProvider
    rather than hardcoded network names when selecting Managed and
    unmanaged networks.
    
    DO NOT MERGE: added temporary test cellular.NetworkProviderChangeNames
    to verify NetworkProvider utility functionality and will be removed once
    b:239460113 is verfied.
    
    BUG=b:242318523
    TEST=tast run <DUT> network.CelularPolicyConnection
    
    Change-Id: Ia803f77244a4f5e5af6bbb30f648fcec226f949b

---
+++ src/chromiumos/tast/local/bundles/cros/cellular/network_provider_change_names.go
@@ -0,0 +1,80 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/network"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         NetworkProviderChangeNames,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Tests name changing of NetworkProvider utility, example test for development and example purposes, not to be Merged",
+		Contacts: []string{
+			"jstanko@google.com",
+			"cros-connectivity@google.com@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_prod_esim"},
+		Fixture:      "cellular",
+		Timeout:      9 * time.Minute,
+	})
+}
+
+func NetworkProviderChangeNames(ctx context.Context, s *testing.State) {
+	networkProvider, err := network.NewCellularNetworkProvider(ctx, false)
+	if err != nil {
+		s.Fatal("Failed to create cellular network provider: ", err)
+	}
+
+	networkNames, err := networkProvider.NetworkNames(ctx)
+	if err != nil {
+		s.Fatal("Failed ot get network names: ", err)
+	}
+	for _, name := range networkNames {
+		testing.ContextLogf(ctx, "Found cellular network: %s", name)
+	}
+
+	if err := rename(ctx, networkProvider, "CellularNetwork"); err != nil {
+		s.Fatal("Failed to rename eSIM networks: ", err)
+	}
+
+	if err := rename(ctx, networkProvider, "MobileNetwork"); err != nil {
+		s.Fatal("Failed to rename eSIM networks: ", err)
+	}
+}
+
+func rename(ctx context.Context, networkProvider network.CellularNetworkProvider, namePrefix string) error {
+	testing.ContextLogf(ctx, "renaming eSIM profiles %s", namePrefix)
+	if err := networkProvider.RenameESimProfiles(ctx, namePrefix); err != nil {
+		return errors.Wrap(err, "failed to rename eSIM profiles")
+	}
+
+	networkNames, err := networkProvider.ESimNetworkNames(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get eSIM network names")
+	}
+
+	seen := make(map[string]bool)
+	for _, name := range networkNames {
+		testing.ContextLogf(ctx, "Found cellular network: %s", name)
+		if seen[name] {
+			return errors.Wrapf(err, "failed to check renamed eSIM, duplicate networks %s found", name)
+		}
+		if !strings.HasPrefix(name, namePrefix) {
+			return errors.Wrapf(err, "renamed profile does not have prefix: %s", namePrefix)
+		}
+		seen[name] = true
+	}
+
+	return nil
+}
