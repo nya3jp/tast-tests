5f8759b0e686225cc32d1358121a15ce984e4745
Jared Bennett <jaredbennett@google.com>
2022-08-25 13:00:13 -0700

    tast-tests: Add btpeers device type support and discovery test
    This change adds a new EmulatedBTPeerDevice struct that tests
    can use to easily configure a btpeer to act as any supported
    bluetooth device and cache related key data about it. It also
    adds a new test, DiscoverEmulatedBTPeerDevices, which
    demonstrates this new support and also validates that a DUT
    can discover the emulated device as expected.
    
    Testing of EmulatedBTPeerDevice found a few gaps in both the
    Chameleond XMLRPC interface and in the XMLRPC client library,
    so both have been updated accordingly.
    
    BUG=b:217633791
    TEST=tast unit tests passed; Ran new test DiscoverEmulatedBTPeerDevices
    successfully with two btpeers
    
    Change-Id: If0174f09d4e278f14efe34d2d1c086391a9d682d

---
+++ src/chromiumos/tast/remote/bundles/cros/bluetooth/discover_emulated_btpeer_devices.go
@@ -0,0 +1,74 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bluetooth
+
+import (
+	"context"
+	"time"
+
+	cbt "chromiumos/tast/common/chameleon/devices/common/bluetooth"
+	"chromiumos/tast/remote/bluetooth"
+	bts "chromiumos/tast/services/cros/bluetooth"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DiscoverEmulatedBTPeerDevices,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Tests that btpeers can be set to emulate a type device and that the DUT can discover them as those devices",
+		Contacts: []string{
+			"jaredbennett@google.com",
+			"cros-connectivity@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		ServiceDeps:  []string{"tast.cros.bluetooth.BTTestService"},
+		Fixture:      "chromeLoggedInWith2BTPeers",
+		Timeout:      time.Second * 30,
+	})
+}
+
+// DiscoverEmulatedBTPeerDevices tests that btpeers can be set to emulate a
+// type device and that the DUT can discover them as those devices.
+func DiscoverEmulatedBTPeerDevices(ctx context.Context, s *testing.State) {
+	fv := s.FixtValue().(*bluetooth.FixtValue)
+
+	// Discover btpeer1 as a keyboard.
+	keyboardDevice, err := bluetooth.NewEmulatedBTPeerDevice(ctx, fv.BTPeers[0].BluetoothKeyboardDevice())
+	if err != nil {
+		s.Fatal("Failed to configure btpeer1 as a keyboard device: ", err)
+	}
+	if keyboardDevice.DeviceType() != cbt.DeviceTypeKeyboard {
+		s.Fatalf("Attempted to emulate btpeer device as a %s, but the actual device type is %s", cbt.DeviceTypeKeyboard, keyboardDevice.DeviceType())
+	}
+	if _, err := fv.BTS.DiscoverDevice(ctx, &bts.DiscoverDeviceRequest{
+		Device: keyboardDevice.BTSDevice(),
+	}); err != nil {
+		s.Fatalf("DUT failed to discover btpeer1 as %s: %v", keyboardDevice.String(), err)
+	}
+
+	// Discover btpeer2 as a mouse.
+	mouseDevice, err := bluetooth.NewEmulatedBTPeerDevice(ctx, fv.BTPeers[1].BluetoothMouseDevice())
+	if err != nil {
+		s.Fatal("Failed to configure btpeer2 as a mouse device: ", err)
+	}
+	if mouseDevice.DeviceType() != cbt.DeviceTypeMouse {
+		s.Fatalf("Attempted to emulate btpeer device as a %s, but the actual device type is %s", cbt.DeviceTypeMouse, mouseDevice.DeviceType())
+	}
+	if _, err := fv.BTS.DiscoverDevice(ctx, &bts.DiscoverDeviceRequest{
+		Device: mouseDevice.BTSDevice(),
+	}); err != nil {
+		s.Fatalf("DUT failed to discover btpeer1 as %s: %v", mouseDevice.String(), err)
+	}
+
+	// Confirm that btpeer1 is also still discoverable as a keyboard, since both
+	// peers should be usable at the same time.
+	if _, err := fv.BTS.DiscoverDevice(ctx, &bts.DiscoverDeviceRequest{
+		Device: keyboardDevice.BTSDevice(),
+	}); err != nil {
+		s.Fatalf("DUT failed to still discover btpeer1 as %s: %v", keyboardDevice.String(), err)
+	}
+}
