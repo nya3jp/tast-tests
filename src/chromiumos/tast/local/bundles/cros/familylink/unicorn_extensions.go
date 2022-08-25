56da61266195e95646dfab1c80b279366f6ad1e0
Zork <zork@google.com>
2022-08-25 12:31:13 -0700

    tast: Wait for preference to sync in familylink.UnicornExtensions*
    This removes a race condition where we would get the wrong dialog if the
    policy hadn't finished syncing yet.
    
    BUG=b:241943380
    TEST=tast run localhost:9222 familylink.UnicornExtensions.lacros
    
    Change-Id: Id1788c076d332cfbe45f80e83f3f8effacbe102e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3857485
    Reviewed-by: Toby Huang <tobyhuang@chromium.org>
    Commit-Queue: Zork <zork@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Zork <zork@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/familylink/unicorn_extensions.go
+++ src/chromiumos/tast/local/bundles/cros/familylink/unicorn_extensions.go
@@ -9,8 +9,11 @@ import (
 	"context"
 	"time"
 
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/browser"
 	"chromiumos/tast/local/chrome/familylink"
+	"chromiumos/tast/local/chrome/lacros"
 	"chromiumos/tast/testing"
 )
 
@@ -19,10 +22,12 @@ func init() {
 		Func:         UnicornExtensions,
 		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Checks if Unicorn user can add extension with parent permission",
-		Contacts:     []string{"chromeos-sw-engprod@google.com", "cros-oac@google.com", "tobyhuang@chromium.org", "cros-families-eng+test@google.com"},
+		Contacts:     []string{"chromeos-sw-engprod@google.com", "cros-oac@google.com", "galenemco@chromium.org", "cros-families-eng+test@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
-		Timeout:      time.Minute,
+		// This test has a long timeout because syncing settings can occasionally
+		// take a long time.
+		Timeout: 5 * time.Minute,
 		VarDeps: []string{
 			"family.parentEmail",
 			"family.parentPassword",
@@ -30,16 +35,59 @@ func init() {
 		Params: []testing.Param{{
 			Val:     browser.TypeAsh,
 			Fixture: "familyLinkUnicornLogin",
-		},
-		/* Disabled due to <1% pass rate over 30 days. See b/241943380
-		{
+		}, {
 			Name:    "lacros",
 			Val:     browser.TypeLacros,
 			Fixture: "familyLinkUnicornLoginWithLacros",
-		}
-		*/
-		},
+		}},
 	})
+}
+
+func boolPref(ctx context.Context, tconn *chrome.TestConn, prefName string) (bool, error) {
+	var value struct {
+		Value bool `json:"value"`
+	}
+	if err := tconn.Call(ctx, &value, "tast.promisify(chrome.settingsPrivate.getPref)", prefName); err != nil {
+		return false, err
+	}
+	return value.Value, nil
+}
+
+func waitForBoolPrefValue(ctx context.Context, tconn *chrome.TestConn, prefName string, expectedValue bool, timeout time.Duration) error {
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		value, err := boolPref(ctx, tconn, prefName)
+		if err != nil {
+			return err
+		}
+		if value != expectedValue {
+			return errors.Errorf("%q is not the right value", prefName)
+		}
+		return nil
+	}, &testing.PollOptions{Interval: 10 * time.Millisecond, Timeout: timeout}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func waitForBoolPrefValueFromAshOrLacros(ctx context.Context, tconn *chrome.TestConn, bt browser.Type, prefName string, expectedValue bool, timeout time.Duration) error {
+	// TODO(b/244515056): Move this function to a shared location.
+	if bt == browser.TypeAsh {
+		return waitForBoolPrefValue(ctx, tconn, prefName, expectedValue, timeout)
+	}
+
+	// Launch Lacros so that we can sync the preference and poll its status.
+	l, err := lacros.Launch(ctx, tconn)
+	if err != nil {
+		return err
+	}
+	// Ensure we close Lacros before we return.
+	defer l.Close(ctx)
+
+	ltconn, err := l.TestAPIConn(ctx)
+	if err != nil {
+		return err
+	}
+	return waitForBoolPrefValue(ctx, ltconn, prefName, expectedValue, timeout)
 }
 
 func UnicornExtensions(ctx context.Context, s *testing.State) {
@@ -51,6 +99,10 @@ func UnicornExtensions(ctx context.Context, s *testing.State) {
 	}
 	if tconn == nil {
 		s.Fatal("Failed to create test API connection")
+	}
+
+	if err := waitForBoolPrefValueFromAshOrLacros(ctx, tconn, s.Param().(browser.Type), "profile.managed.extensions_may_request_permissions", true, 4*time.Minute); err != nil {
+		s.Fatal("Failed to wait for pref: ", err)
 	}
 
 	if err := familylink.NavigateExtensionApprovalFlow(ctx, cr, tconn, s.Param().(browser.Type), s.RequiredVar("family.parentEmail"), s.RequiredVar("family.parentPassword")); err != nil {
