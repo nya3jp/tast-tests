1fd9e046495c82f07f67a8b3a4714b4bcbc07156
Carlos <carlos.sagastume@cienet.com>
2022-08-25 19:13:59 +0800

    faft: Fix bug FwmpDevDisableBoot()
    * Add verification to check for TPM owner password present.
    * Add poll to retry setting firmware management parameters flags.
    * Extend the overall timeout for this test to 10 minutes.
    
    BUG=b:200305269
    TEST=manual on dirinboz, delbin, ampton, sparky360, dood.
    
    Change-Id: I6e826a005f2f5df3892da7bb3a7db341eba245bb

--- src/chromiumos/tast/remote/bundles/cros/firmware/fwmp_dev_disable_boot.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/fwmp_dev_disable_boot.go
@@ -6,8 +6,11 @@ package firmware
 
 import (
 	"context"
+	"regexp"
 	"strings"
+	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/remote/firmware"
 	"chromiumos/tast/remote/firmware/fixture"
@@ -23,6 +26,7 @@ func init() {
 		Contacts: []string{"cienet-firmware@cienet.corp-partner.google.com", "chromeos-firmware@google.com"},
 		// TODO(b/235742217): This test might be leaving broken DUTS that can't be auto-repaired. Add attr firmware_unstable when fixed.
 		Attr:         []string{"group:firmware"},
+		Timeout:      10 * time.Minute,
 		Fixture:      fixture.DevMode,
 		HardwareDeps: hwdep.D(hwdep.ChromeEC(), hwdep.Vboot2()),
 	})
@@ -41,23 +45,78 @@ func FwmpDevDisableBoot(ctx context.Context, s *testing.State) {
 		if err := s.DUT().Conn().CommandContext(ctx, "tpm_manager_client", "take_ownership").Run(ssh.DumpLogOnError); err != nil {
 			return errors.Wrap(err, "failed to take ownership")
 		}
-		s.Log("Setting firmware management parameters")
-		if err := s.DUT().Conn().CommandContext(ctx, "cryptohome", "--action=set_firmware_management_parameters", "--flags=0x"+flags).Run(ssh.DumpLogOnError); err != nil {
-			return errors.Wrapf(err, "failed to set firmware management parameters with flags 0x%s", flags)
+
+		/*
+			Allow some delay to ensure that this command has fully propagated.
+			Without this delay, the following error would likely appear when
+			setting firmware management parameters:
+			'Failed to call SetFirmwareManagementParameters: (dbus, org.freedesktop.DBus.Error.ServiceUnknown,
+			Error calling D-Bus method: org.chromium.InstallAttributesInterface.SetFirmwareManagementParameters:
+			The name org.chromium.UserDataAuth was not provided by any .service files)'
+		*/
+		s.Logf("Sleeping for %s", 5*time.Second)
+		if err := testing.Sleep(ctx, 5*time.Second); err != nil {
+			return errors.Wrap(err, "failed to sleep")
 		}
+
+		// Set FWMP flags in a poll to increase the chances of success.
+		s.Log("Setting firmware management parameters")
+		reOwnerPassword := regexp.MustCompile(`flags:\s*(0|1)`)
+		var currentFlagVal [][]byte
+		if err := testing.Poll(ctx, func(ctx context.Context) error {
+			if err := s.DUT().Conn().CommandContext(ctx, "cryptohome", "--action=set_firmware_management_parameters", "--flags=0x"+flags).Run(ssh.DumpLogOnError); err != nil {
+				return errors.Wrapf(err, "failed to set firmware management parameters with flags 0x%s", flags)
+			}
+
+			// Verify the flags have been set as expected.
+			out, err := s.DUT().Conn().CommandContext(ctx, "cryptohome", "--action=get_firmware_management_parameters").Output(ssh.DumpLogOnError)
+			if err != nil {
+				return errors.Wrap(err, "failed to get firmware management parameter flags")
+			}
+
+			currentFlagVal = reOwnerPassword.FindSubmatch(out)
+			if string(currentFlagVal[1]) != flags {
+				return errors.Wrapf(err, "flags haven't been set correctly: expected flags to be %q but got the following output: %s", flags, string(out))
+			}
+
+			return nil
+		}, &testing.PollOptions{Timeout: 25 * time.Second, Interval: 3 * time.Second}); err != nil {
+			if string(currentFlagVal[1]) == "1" {
+				return errors.Wrap(err, "dev mode is disabled by FWMP flags=0x1, please run 'cryptohome --action=set_firmware_management_parameters --flags=0x0' to recover DUTs")
+			}
+			return err
+		}
+
 		return nil
 	}
+
+	// Verify that TPM owner password is present.
+	out, err := s.DUT().Conn().CommandContext(ctx, "tpm_manager_client", "status", "--nonsensitive").Output(ssh.DumpLogOnError)
+	if err != nil {
+		s.Fatal("Failed to get TMP status: ", err)
+	}
+	reOwnerPassword := regexp.MustCompile(`is_owner_password_present: (\S*)`)
+	ownerPasswordState := reOwnerPassword.FindSubmatch(out)
+	if string(ownerPasswordState[1]) != "true" {
+		s.Fatal("TPM owner password is not present, and received output: ", string(out))
+	}
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Minute)
+	defer cancel()
+
+	// Set DUT in "dev mode enable" state by setting TPM flags to "0x0" at the end of the test.
+	defer func(cleanupCtx context.Context) {
+		s.Log("Reverting the 'dev mode disable' state on DUT at the end of test")
+		if err := setFWMP(cleanupCtx, "0"); err != nil {
+			s.Fatal("Failed while taking ownership and setting flags at the end of test: ", err)
+		}
+	}(cleanupCtx)
 
 	// Set DUT in "dev mode disable" state by setting TPM flags to "0x1".
 	if err := setFWMP(ctx, "1"); err != nil {
 		s.Fatal("Failed while taking ownership and setting flags: ", err)
 	}
-	defer func() {
-		s.Log("Reverting the 'dev mode disable' state on DUT at the end of test")
-		if err := setFWMP(ctx, "0"); err != nil {
-			s.Fatal("Failed while taking ownership and setting flags at the end of test: ")
-		}
-	}()
 
 	ms, err := firmware.NewModeSwitcher(ctx, h)
 	if err != nil {
