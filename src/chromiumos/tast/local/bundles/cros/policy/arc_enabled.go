22ab2a566366baf5a11ab62279cf58d1c14c92d9
Muhammad Hasan Khan <mhasank@chromium.org>
2022-08-26 17:51:02 +0000

    tast-tests: Promote policy.ArcEnabled to critical
    The test is stable.
    
    TEST=https://tastboard.corp.google.com/test/policy.ArcEnabled
    BUG=None
    
    Change-Id: I0b8211c1d3b376c16f4d83456d718fe27adb691b

--- src/chromiumos/tast/local/bundles/cros/policy/arc_enabled.go
+++ src/chromiumos/tast/local/bundles/cros/policy/arc_enabled.go
@@ -30,10 +30,11 @@ func init() {
 		Desc:         "Behavior of ArcEnabled policy, checking whether ARC is enabled after setting the policy",
 		Contacts: []string{
 			"gabormagda@google.com", // Test author
+			"arc-commercial@google.com",
 			"chromeos-commercial-remote-management@google.com",
 		},
 		SoftwareDeps: []string{"chrome"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		Timeout:      2 * time.Minute,
 		Params: []testing.Param{{
 			ExtraSoftwareDeps: []string{"android_p"},
