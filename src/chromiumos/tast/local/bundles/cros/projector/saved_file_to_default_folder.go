93255dee4e58dcd0ba2c73a748d3d7cb045edfd5
Xiqi Ruan <xiqiruan@chromium.org>
2022-08-26 12:49:26 -0700

    tast-tests: Verify files saved to right place in DriveFS
    Verify files saved to the default folder in DriveFS.
    
    BUG=b:229631590
    TEST=tast run $dut projector.CreationFlow
    
    Change-Id: I2118cf33ff79a0ea743060842adba91f245f192a

---
+++ src/chromiumos/tast/local/bundles/cros/projector/saved_file_to_default_folder.go
@@ -0,0 +1,83 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package projector is used for writing Projector tests.
+package projector
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/apps"
+	"chromiumos/tast/local/chrome/projector"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/drivefs"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         SavedFileToDefaultFolder,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Launches the Projector app and goes through the new screencast creation flow with annotator",
+		Contacts:     []string{"xiqiruan@chromium.org", "cros-projector+tast@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Timeout:      10 * time.Minute,
+		Fixture:      "projectorLogin",
+	})
+}
+
+func SavedFileToDefaultFolder(ctx context.Context, s *testing.State) {
+	ctxForCleanUp := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 4*time.Minute)
+	defer cancel()
+
+	tconn := s.FixtValue().(*projector.FixtData).TestConn
+	cr := s.FixtValue().(*projector.FixtData).Chrome
+	driveFsClient, err := drivefs.NewDriveFs(ctx, cr.NormalizedUser())
+	if err != nil {
+		s.Fatal("Failed waiting for DriveFS to start: ", err)
+	}
+	s.Log("Drivefs fully started")
+
+	defer faillog.DumpUITreeOnError(ctxForCleanUp, s.OutDir(), s.HasError, tconn)
+
+	if err := launcher.LaunchAndWaitForAppOpen(tconn, apps.Projector)(ctx); err != nil {
+		s.Fatal("Failed to open Projector app: ", err)
+	}
+
+	if err := projector.DismissOnboardingDialog(ctx, tconn); err != nil {
+		s.Fatal("Failed to close the onboarding dialog: ", err)
+	}
+
+	// We need to clean up any screencasts after the test to
+	// prevent taking up Drive quota over time.
+	defer projector.DeleteScreencastItems(ctxForCleanUp, tconn)
+	if err := projector.LaunchCreationFlow(ctx, tconn, false /*launchAnnotator*/); err != nil {
+		s.Fatal("Failed to go through the new screencast creation flow: ", err)
+	}
+
+	// Verifies Screencast saved to right location.
+	ui := uiauto.New(tconn).WithTimeout(2 * time.Minute)
+	screencastItem := nodewith.ClassName("screencast-media").Role(role.GenericContainer).First()
+	screencastTitle := nodewith.Role(role.StaticText).Ancestor(nodewith.ClassName("screencast-title").Ancestor(screencastItem))
+	info, err := ui.Info(ctx, screencastTitle)
+	if err != nil {
+		s.Fatal("Fail to get screencast tiltle info: ", err)
+	}
+	// The container folder path should be like
+	// "{Drivefs mounted path}/root/Screencast recordings/{screencastTitle text}"
+	testFilePath := driveFsClient.MyDrivePath(filepath.Join("Screencast recordings", info.Name))
+	if _, err := os.Stat(testFilePath); err != nil {
+		s.Fatal("Failed to locate screencast in default folder: ", err)
+	}
+}
