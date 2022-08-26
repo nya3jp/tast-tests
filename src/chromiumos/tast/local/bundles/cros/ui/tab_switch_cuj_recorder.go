42af0b297eaa3c6d2c93804bad802a20cca9c77a
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-08-25 23:20:59 -0700

    tast-tests: Simplify ui.TabSwitchCUJ for consistency
    Some part of the code could be simplified using browser abstraction.
    This change should not affect the existing testing path.
    
    Set LacrosVariantUnneeded to ui.TabSwitchCUJRecorder that is not used.
    
    BUG=b:243042610, b:216242056
    TEST=tast run ui.TabSwitchCUJ*
    
    Change-Id: I852913086d7fd93075b0e40e93d8e203ff295016

--- src/chromiumos/tast/local/bundles/cros/ui/tab_switch_cuj_recorder.go
+++ src/chromiumos/tast/local/bundles/cros/ui/tab_switch_cuj_recorder.go
@@ -17,7 +17,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         TabSwitchCUJRecorder,
-		LacrosStatus: testing.LacrosVariantUnknown,
+		LacrosStatus: testing.LacrosVariantUnneeded, // used to record all web traffic via wpr so that later TabSwitchCUJ could run without really talking to real sites
 		Desc:         "Run tab-switching CUJ test in chromewpr recording mode",
 		Contacts:     []string{"mukai@chromium.org", "tclaiborne@chromium.org", "chromeos-wmp@google.com"},
 		SoftwareDeps: []string{"chrome"},
