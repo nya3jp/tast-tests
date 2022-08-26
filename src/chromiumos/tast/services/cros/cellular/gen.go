966145e440254945593ce7473786427f5191b6cb
Jason Stanko <jstanko@google.com>
2022-08-26 18:35:48 +0000

    cellular: Fetch cellular interface name at runtime
    This removes the hardcoded cellular interface name used in callbox tests
    and instead fetches it at runtime. It does this by adding a Tast gRPC
    service to call local Tast shill libraries. This service will later be
    extended to include more utilities required by remote cellular tests
    (b:243937649).
    
    BUG=b:241964523, b:243937649
    TEST=tast run -var=callboxManager=<CBM> <DUT>  '(cellular_callbox)'
    
    Cq-Depend: chromium:3865599
    Change-Id: Ic8d403816d1122f0ed23b65c3d8725428fcdde7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3863032
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Tested-by: Jason Stanko <jstanko@google.com>
    Commit-Queue: Jason Stanko <jstanko@google.com>

---
+++ src/chromiumos/tast/services/cros/cellular/gen.go
@@ -0,0 +1,11 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//go:generate protoc -I . --go_out=plugins=grpc:../../../../.. remote_cellular_service.proto
+
+package cellular
+
+// Run the following command in CrOS chroot to regenerate protocol buffer bindings:
+//
+// ~/trunk/src/platform/tast/tools/go.sh generate chromiumos/tast/services/cros/cellular
