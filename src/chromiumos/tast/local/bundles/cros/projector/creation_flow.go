e61ef05d6b09239588487864a93a61d873d3e67d
Toby Huang <tobyhuang@chromium.org>
2022-08-28 18:49:55 +0000

    Projector: Refactor creation flow into helper function
    Now that multiple Tast tests require the creation flow to create a new
    screencast, this CL refactors the code into a helper function. We can
    split the creation flow into multiple tests instead of appending to
    one monolithic test. Only the original creation flow Tast test will be
    responsible for testing the annotator.
    
    BUG=b:243589606
    TEST=./repeat-test.sh -r 10 -- -buildbundle=cros localhost:2222 projector.*
    
    Change-Id: I7ae98db974eba7d863e766cd3e1271de59a8a002
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3861378
    Reviewed-by: Xiqi Ruan <xiqiruan@chromium.org>
    Auto-Submit: Toby Huang <tobyhuang@chromium.org>
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: David Stevens <stevensd@chromium.org>
    Tested-by: Toby Huang <tobyhuang@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/projector/creation_flow.go
+++ src/chromiumos/tast/local/bundles/cros/projector/creation_flow.go
@@ -7,13 +7,10 @@ package projector
 
 import (
 	"context"
-	"regexp"
 	"time"
 
 	"chromiumos/tast/ctxutil"
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/apps"
-	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/projector"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
@@ -28,7 +25,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         CreationFlow,
 		LacrosStatus: testing.LacrosVariantUnneeded,
-		Desc:         "Launches the Projector app and goes through the new screencast creation flow",
+		Desc:         "Launches the Projector app and goes through the new screencast creation flow with annotator",
 		Contacts:     []string{"tobyhuang@chromium.org", "cros-projector+tast@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "ondevice_speech"},
@@ -40,7 +37,7 @@ func init() {
 
 func CreationFlow(ctx context.Context, s *testing.State) {
 	ctxForCleanUp := ctx
-	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	ctx, cancel := ctxutil.Shorten(ctx, 4*time.Minute)
 	defer cancel()
 
 	tconn := s.FixtValue().(*projector.FixtData).TestConn
@@ -49,14 +46,7 @@ func CreationFlow(ctx context.Context, s *testing.State) {
 
 	ui := uiauto.New(tconn).WithTimeout(2 * time.Minute)
 
-	newScreencastButton := nodewith.Name("New screencast").Role(role.Button).Focusable()
 	screencastItem := nodewith.ClassName("screencast-media").Role(role.GenericContainer).First()
-	clickOrTapRegex := regexp.MustCompile("(Click|Tap) anywhere to record full screen")
-	clickOrTapAnywhereToRecord := nodewith.NameRegex(clickOrTapRegex).Role(role.StaticText)
-	annotatorTrayButton := nodewith.NameStartingWith("Toggle marker.").Role(role.Button)
-	inkCanvas := nodewith.ClassName("ink-engine").Role(role.Canvas)
-	blueMarkerButton := nodewith.Name("Blue").Role(role.Button)
-	stopRecordingButton := nodewith.Name("Stop screen recording").Role(role.Button)
 	tutorialsText := nodewith.Name("Getting started").Role(role.StaticText)
 	closeTutorialsButton := nodewith.Name("Close tutorials").Role(role.Button)
 
@@ -68,82 +58,23 @@ func CreationFlow(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to close the onboarding dialog: ", err)
 	}
 
-	// UI action for refreshing the app until the element we're
-	// looking for exists.
-	refreshApp := projector.RefreshApp(ctx, tconn)
-
 	// We need to clean up any screencasts after the test to
 	// prevent taking up Drive quota over time.
-	defer deleteScreencastItems(ctxForCleanUp, tconn)
+	defer projector.DeleteScreencastItems(ctxForCleanUp, tconn)
 
-	s.Log("Launching the new screencast creation flow")
-	if err := uiauto.Combine("launch the new screencast creation flow",
-		ui.WithInterval(5*time.Second).RetryUntil(refreshApp, ui.Exists(newScreencastButton)),
+	s.Log("Setting up the new screencast creation flow")
+	if err := uiauto.Combine("Setting up the new screencast creation flow",
 		// Make sure there are no existing screencasts before
 		// starting the test.
 		ui.Gone(screencastItem),
-		// Expect the Screencast app to minimize once the
-		// recording session starts, so the button should
-		// disappear.
-		ui.LeftClickUntil(newScreencastButton, ui.Gone(newScreencastButton)),
-		ui.WaitUntilExists(clickOrTapAnywhereToRecord),
-		ui.LeftClickUntil(clickOrTapAnywhereToRecord, ui.Gone(clickOrTapAnywhereToRecord)),
-		ui.WaitUntilExists(annotatorTrayButton),
-		// Enable the annotator.
-		ui.WithInterval(time.Second).LeftClickUntil(annotatorTrayButton, ui.Exists(inkCanvas)),
-		// Open the color picker.
-		ui.RightClickUntil(annotatorTrayButton, ui.Exists(blueMarkerButton)),
-		// Change marker color to blue.
-		ui.LeftClickUntil(blueMarkerButton, ui.Gone(blueMarkerButton)),
-		// Draw a blue dot.
-		// TODO(b/229634049): Verify the drawing rendered.
-		ui.LeftClick(inkCanvas),
-		// Clear the canvas.
-		// TODO(b/229634049): Verify the canvas cleared.
-		ui.RightClick(inkCanvas),
-		// Disable the annotator.
-		ui.WithInterval(time.Second).LeftClickUntil(annotatorTrayButton, ui.Gone(inkCanvas)),
-		// This test saves the screencast to the DriveFS
-		// directory.
-		ui.WaitUntilExists(stopRecordingButton),
-		// Expect the Screencast app to maximize after
-		// recording stops.
-		ui.LeftClickUntil(stopRecordingButton, ui.Gone(stopRecordingButton)),
 		// Dismiss the tutorial videos in case they hide the screencast item on small screens.
-		ui.WithInterval(5*time.Second).RetryUntil(refreshApp, ui.Exists(closeTutorialsButton)),
+		ui.WaitUntilExists(closeTutorialsButton),
 		ui.LeftClickUntil(closeTutorialsButton, ui.Gone(tutorialsText)),
 	)(ctx); err != nil {
+		s.Fatal("Failed to set up the new screencast creation flow: ", err)
+	}
+
+	if err := projector.LaunchCreationFlow(ctx, tconn, true /*launchAnnotator*/); err != nil {
 		s.Fatal("Failed to go through the new screencast creation flow: ", err)
 	}
-}
-
-// deleteScreencastItems is a helper function to delete all screencast items in the gallery view.
-func deleteScreencastItems(ctx context.Context, tconn *chrome.TestConn) error {
-	testing.ContextLog(ctx, "Deleting screencasts")
-
-	ui := uiauto.New(tconn).WithTimeout(2 * time.Minute)
-
-	screencastItem := nodewith.ClassName("screencast-media").Role(role.GenericContainer).First()
-	screencastItemMoreOptionsButton := nodewith.Name("More options").Role(role.PopUpButton).Ancestor(screencastItem)
-	deleteMenuItem := nodewith.Name("Delete").Role(role.MenuItem)
-	deleteButton := nodewith.Name("Delete").Role(role.Button)
-
-	deleteScreencastItem := func(ctx context.Context) error {
-		if err := uiauto.Combine("delete first screencast item",
-			ui.WaitUntilExists(screencastItemMoreOptionsButton),
-			ui.MakeVisible(screencastItemMoreOptionsButton),
-			ui.LeftClickUntil(screencastItemMoreOptionsButton, ui.Exists(deleteMenuItem)),
-			ui.LeftClickUntil(deleteMenuItem, ui.Exists(deleteButton)),
-			ui.LeftClickUntil(deleteButton, ui.Gone(deleteButton)),
-		)(ctx); err != nil {
-			return errors.Wrap(err, "failed to delete screencast item")
-		}
-		return nil
-	}
-
-	if err := ui.WithInterval(5*time.Second).RetryUntil(deleteScreencastItem, ui.Gone(screencastItem))(ctx); err != nil {
-		return errors.Wrap(err, "failed to delete all leftover screencast items")
-	}
-
-	return nil
 }
