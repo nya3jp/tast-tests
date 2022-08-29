9c709ed42da41aee3a794394c1b79c943ed4615d
Patryk Duda <pdk@semihalf.com>
2022-08-29 17:49:28 +0200

    fingerprint: Introduce firmware.FpRebootPowerCycle test
    BUG=b:240626388
    TEST=Run 'tast run <IP> firmware.FpRebootPowerCycle' on hatch, nocturne,
         and zork. Make sure test pass.
    TEST=Run 'tast run <IP> firmware.FpRebootPowerCycle' on brya. Make sure
         test fails.
    TEST=TAST unit tests passed.
    
    Change-Id: I00965a6ecccd23ba3fda0524e2013537934621c1

---
+++ src/chromiumos/tast/remote/firmware/fingerprint/fingerprint_reset_flags_test.go
@@ -0,0 +1,34 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fingerprint
+
+import "testing"
+
+func TestResetFlagsEctoolUnmarshaler(t *testing.T) {
+	var out = `0x00000c02`
+	var expect = ResetFlags(ResetFlagsResetPin | ResetFlagsSysjump | ResetFlagsHard)
+		
+	actual, err := unmarshalEctoolFlags(out)
+	if err != nil {
+		t.Fatal("Failed to unmarshal reset flags: ", err)
+	}
+
+	if actual != expect {
+		t.Fatalf("Unmarshaled reset flags  %+v doesn't match expected flags %+v.", actual, expect)
+	}
+}
+
+func TestResetFlagsIsSet(t *testing.T) {
+	var flags ResetFlags
+
+	if flags.IsSet(ResetFlagsPowerOn) {
+		t.Fatal("Flag ResetFlagsPowerOn was reported as set.")
+	}
+
+	flags = ResetFlagsPowerOn
+	if !flags.IsSet(ResetFlagsPowerOn) {
+		t.Fatal("Flag ResetFlagsPowerOn was reported as not set.")
+	}
+}
