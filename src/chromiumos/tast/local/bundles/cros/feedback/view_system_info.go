7669f5902841aa5ce2135e741f884070efa76e88
Danny Wang <wangdanny@google.com>
2022-08-29 19:18:36 +0000

    Feedback: Verify user can click and view sysetm info
    BUG=b:185624798
    TEST=tast run <DUT> feedback.ViewSystemInfo
    
    Change-Id: I9c728d91f1d78bb5f5727db1006ca4e3cc6e5358

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/view_system_info.go
@@ -0,0 +1,73 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ViewSystemInfo,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verify user can click and view sysetm info",
+		Contacts: []string{
+			"wangdanny@google.com",
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Fixture:      "chromeLoggedInWithOsFeedback",
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// ViewSystemInfo verifies user can click and view sysetm info.
+func ViewSystemInfo(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
+
+	// Launch feedback app and go to share data page.
+	feedbackRootNode, err := feedbackapp.LaunchAndGoToShareDataPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and go to share data page: ", err)
+	}
+
+	// Click system and app info link.
+	systemAndAppInfo := nodewith.Name("system and app info").Role(
+		role.Link).Ancestor(feedbackRootNode)
+	if err := ui.DoDefault(systemAndAppInfo)(ctx); err != nil {
+		s.Fatal("Failed to find and click system and app info link: ", err)
+	}
+
+	// Verify user can view system and app info.
+	systemInfoDetails := nodewith.Name("System Information Preview").First()
+	if err := ui.WaitUntilExists(systemInfoDetails)(ctx); err != nil {
+		s.Error("Failed to view system and app info: ", err)
+	}
+}
