57b2ac4bc1c16477620076bd0564cfb94db77340
Jason Hsiao <jason.hsiao@cienet.com>
2022-08-29 12:07:43 +0800

    EDU 3D modeling cuj: Clean up existing copies before making new ones
    1. Refine func Delete.
    2. Delete existing copies before making new ones.
    
    BUG=b:245997618
    TEST=tast run <vars> <dut> ui.EDU3DModelingCUJ.*
    TEST=tast run <vars> <dut> spera.EDU3DModelingCUJ.*
    
    Change-Id: Iff62ebdd30058fab88212ebb75b1f9c08cd46d34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3890383
    Tested-by: Alston Huang <alston.huang@cienet.com>
    Reviewed-by: Jun Sheng <shengjun@chromium.org>
    Commit-Queue: Alston Huang <alston.huang@cienet.com>

--- src/chromiumos/tast/local/bundles/cros/ui/edu3dmodelingcuj/run.go
+++ src/chromiumos/tast/local/bundles/cros/ui/edu3dmodelingcuj/run.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The ChromiumOS Authors.
+// Copyright 2022 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -26,7 +26,10 @@ import (
 	"chromiumos/tast/testing"
 )
 
-const tinkerCadTerm = "Tinkercad"
+const (
+	tinkerCadTerm  = "Tinkercad"
+	fileNamePrefix = "Copy of"
+)
 
 // Run runs the EDU3DModelingCUJ test.
 func Run(ctx context.Context, cr *chrome.Chrome, isTablet bool, bt browser.Type, outDir, sampleDesignURL, rotateIconPath string) (retErr error) {
@@ -156,6 +159,11 @@ func Run(ctx context.Context, cr *chrome.Chrome, isTablet bool, bt browser.Type,
 			if err := uiHandler.SwitchToChromeTabByIndex(0)(ctx); err != nil {
 				return errors.Wrap(err, "failed to switch tab")
 			}
+		}
+
+		// Delete existing copies if previous tests failed to delete them.
+		if err := tinkerCad.Delete(ctx, fileNamePrefix); err != nil {
+			testing.ContextLog(ctx, "Failed to delete existing copies: ", err)
 		}
 
 		// Copy the design from URL than delete it at the final step.
