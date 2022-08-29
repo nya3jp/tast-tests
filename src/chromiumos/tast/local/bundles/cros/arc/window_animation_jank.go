a537317a20b3a89495cb502990003c5a560d90fc
Yuka Shu <yukashu@google.com>
2022-08-29 08:07:56 +0000

    tast-tests: a test for ArcWindowAnimationJankTest
    BUG=b:245042021
    TEST=tast run DUT arc.WindowAnimationJank.vm
    
    Change-Id: I6d38d905fa1786733abe835bf637e96536f691ee

---
+++ src/chromiumos/tast/local/bundles/cros/arc/window_animation_jank.go
@@ -0,0 +1,67 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package arc
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         WindowAnimationJank,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Sample test to run ArcWindowAnimationJankTest.apk",
+		Contacts:     []string{"yukashu@chromium.org", "sstan@chromium.org", "brpol@google.com"},
+		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		SoftwareDeps: []string{"chrome"},
+		Fixture:      "arcBooted",
+		Data:         []string{"ArcWindowAnimationJankTest.apk", "config.pbtxt"},
+		Timeout:      30 * time.Minute,
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+		}},
+	})
+}
+
+func WindowAnimationJank(ctx context.Context, s *testing.State) {
+
+	const (
+		apkName      = "ArcWindowAnimationJankTest.apk"
+		pkgName      = "org.chromium.arc.testapp.windowanimationjank"
+		activityName = "ElementLayoutActivity"
+	)
+	a := s.FixtValue().(*arc.PreData).ARC
+	cr := s.FixtValue().(*arc.PreData).Chrome
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+	// defer conn.Close()
+	s.Log("Installing: ", apkName)
+	if err := a.Install(ctx, s.DataPath(apkName)); err != nil {
+		s.Fatal("Failed to install app: ", err)
+	}
+
+	s.Log("Running test")
+
+	s.Log("Starting app")
+
+	act, err := arc.NewActivity(a, pkgName, "."+activityName)
+	if err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	// defer act.Close()
+	if err := act.StartWithDefaultOptions(ctx, tconn); err != nil {
+		s.Fatal("Failed to start the ElementLayoutActivity: ", err)
+	}
+
+}
