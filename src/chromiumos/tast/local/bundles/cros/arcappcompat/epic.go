d116826e563e04a8a4dac03a100e2f7705868d68
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2022-08-30 14:09:49 -0700

    arcappcompat: Run UIAutomator Test Using Tast For Epic app
    Tast Test for Epic app verifies launch app from the playstore
    and checks if the app correctly changes the window state in
    both clamshell and touchview mode.
    
    BUG=b:230760261
    TEST=tast run <eve> arcappcompat.Epic.vm_clamshell_mode_top_apps
    tast run <eve> arcappcompat.Epic.vm_tablet_mode_top_apps
    tast run <kevin> arcappcompat.Epic.tablet_mode_top_apps
    tast run <kevin> arcappcompat.Epic.clamshell_mode_top_apps
    
    Change-Id: Ic1d6b1dcf447dd7cff0f0b2837e993680ee7abde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3863039
    Commit-Queue: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Tested-by: Meenakshi Thiyagarajan <mthiyagarajan@google.com>
    Reviewed-by: Renuga Nanjappan <rnanjappan@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/epic.go
@@ -0,0 +1,162 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arcappcompat will have tast tests for android apps on Chromebooks.
+package arcappcompat
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/android/ui"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/pre"
+	"chromiumos/tast/local/bundles/cros/arcappcompat/testutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+// clamshellLaunchForEpic launches Epic in clamshell mode.
+var clamshellLaunchForEpic = []testutil.TestCase{
+	{Name: "Launch app in Clamshell", Fn: launchAppForEpic, Timeout: testutil.LaunchTestCaseTimeout},
+}
+
+// touchviewLaunchForEpic launches Epic in tablet mode.
+var touchviewLaunchForEpic = []testutil.TestCase{
+	{Name: "Launch app in Touchview", Fn: launchAppForEpic, Timeout: testutil.LaunchTestCaseTimeout},
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Epic,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Functional test for Epic that installs the app also verifies that the main page is open, checks Epic correctly changes the window state in both clamshell and touchview mode",
+		Contacts:     []string{"mthiyagarajan@google.com", "cros-appcompat-test-team@google.com"},
+		Attr:         []string{"group:appcompat"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name: "clamshell_mode_default",
+			Val: testutil.TestParams{
+				LaunchTests: clamshellLaunchForEpic,
+				CommonTests: testutil.ClamshellCommonTests,
+			},
+			ExtraAttr:         []string{"appcompat_default"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "tablet_mode_default",
+			Val: testutil.TestParams{
+				LaunchTests: touchviewLaunchForEpic,
+				CommonTests: testutil.TouchviewCommonTests,
+			},
+			ExtraAttr:         []string{"appcompat_default"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "vm_clamshell_mode_default",
+			Val: testutil.TestParams{
+				LaunchTests: clamshellLaunchForEpic,
+				CommonTests: testutil.ClamshellCommonTests,
+			},
+			ExtraAttr:         []string{"appcompat_default"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "vm_tablet_mode_default",
+			Val: testutil.TestParams{
+				LaunchTests: touchviewLaunchForEpic,
+				CommonTests: testutil.TouchviewCommonTests,
+			},
+			ExtraAttr:         []string{"appcompat_default"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "clamshell_mode_top_apps",
+			Val: testutil.TestParams{
+				LaunchTests: clamshellLaunchForEpic,
+				TopAppTests: testutil.ClamshellTopAppTests,
+			},
+			ExtraAttr:         []string{"appcompat_top_apps"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "tablet_mode_top_apps",
+			Val: testutil.TestParams{
+				LaunchTests: touchviewLaunchForEpic,
+				TopAppTests: testutil.TouchviewTopAppTests,
+			},
+			ExtraAttr:         []string{"appcompat_top_apps"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "vm_clamshell_mode_top_apps",
+			Val: testutil.TestParams{
+				LaunchTests: clamshellLaunchForEpic,
+				TopAppTests: testutil.ClamshellTopAppTests,
+			},
+			ExtraAttr:         []string{"appcompat_top_apps"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "vm_tablet_mode_top_apps",
+			Val: testutil.TestParams{
+				LaunchTests: touchviewLaunchForEpic,
+				TopAppTests: testutil.TouchviewTopAppTests,
+			},
+			ExtraAttr:         []string{"appcompat_top_apps"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}},
+		Timeout: 20 * time.Minute,
+		Vars:    []string{"arcappcompat.gaiaPoolDefault"},
+	})
+}
+
+// Epic test uses library for opting into the playstore and installing app.
+// Checks Epic correctly changes the window states in both clamshell and touchview mode.
+func Epic(ctx context.Context, s *testing.State) {
+	const (
+		appPkgName  = "com.getepic.Epic"
+		appActivity = ".activities.MainActivity"
+	)
+
+	testSet := s.Param().(testutil.TestParams)
+	testutil.RunTestCases(ctx, s, appPkgName, appActivity, testSet)
+}
+
+// launchAppForEpic verifies Epic reached main activity page of the app.
+func launchAppForEpic(ctx context.Context, s *testing.State, tconn *chrome.TestConn, a *arc.ARC, d *ui.Device, appPkgName, appActivity string) {
+	testutil.HandleDialogBoxes(ctx, s, d, appPkgName)
+	// Check for launch verifier.
+	launchVerifier := d.Object(ui.PackageName(appPkgName))
+	if err := launchVerifier.WaitForExists(ctx, testutil.ShortUITimeout); err != nil {
+		testutil.DetectAndHandleCloseCrashOrAppNotResponding(ctx, s, d)
+		s.Fatal("launchVerifier doesn't exists: ", err)
+	}
+}
