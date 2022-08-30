ea46c2daaf20071eef9f82dcc1b88842a50e2c60
Muhammad Hasan Khan <mhasank@chromium.org>
2022-08-29 17:16:10 -0700

    tast-tests: Simplify arc.UnicornBlockedApps
    We don't need to serve a policy with blocked apps in it since Play Store
    does not rely on ARC policy to know which apps are blocked.
    
    Added a check to wait for provisioning before launching Play Store.
    
    Added the DisableSyncFlags flag in fixture to avoid flakiness due to
    Play Store updates triggering during the test.
    
    BUG=b:237023275
    TEST=tast run DUT arc.UnicornBlockedApps
    
    Change-Id: I547e172804c9a81381ee016a5cd0e30881446b43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3862947
    Tested-by: Muhammad Hasan Khan <mhasank@chromium.org>
    Commit-Queue: Muhammad Hasan Khan <mhasank@chromium.org>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Yao Li <yaohuali@google.com>
    Auto-Submit: Muhammad Hasan Khan <mhasank@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/unicorn_blocked_apps.go
+++ src/chromiumos/tast/local/bundles/cros/arc/unicorn_blocked_apps.go
@@ -6,18 +6,12 @@ package arc
 
 import (
 	"context"
-	"sort"
 	"time"
 
 	"chromiumos/tast/common/android/ui"
 	"chromiumos/tast/common/policy"
-	"chromiumos/tast/errors"
-	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/arc"
-	"chromiumos/tast/local/arc/optin"
-	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/familylink"
-	"chromiumos/tast/local/chrome/uiauto/launcher"
 	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/testing"
 )
@@ -29,9 +23,13 @@ func init() {
 		Desc:         "Checks if blocked apps cannot be installed from Child Account",
 		Contacts:     []string{"rnanjappan@chromium.org", "cros-arc-te@google.com"},
 		Attr:         []string{"group:mainline", "informational", "group:arc-functional"},
-		SoftwareDeps: []string{"chrome"},
-		Timeout:      4 * time.Minute,
-		VarDeps:      []string{"arc.parentUser", "arc.parentPassword", "arc.childUser", "arc.childPassword"},
+		SoftwareDeps: []string{
+			"chrome",
+			"chrome_internal",
+			"play_store",
+		},
+		Timeout: 6 * time.Minute,
+		VarDeps: []string{"arc.parentUser", "arc.parentPassword", "arc.childUser", "arc.childPassword"},
 		Params: []testing.Param{{
 			ExtraSoftwareDeps: []string{"android_p"},
 		}, {
@@ -44,29 +42,19 @@ func init() {
 
 func UnicornBlockedApps(ctx context.Context, s *testing.State) {
 	const (
-		DefaultUITimeout  = 60 * time.Second
-		installButtonText = "install"
+		DefaultUITimeout     = 1 * time.Minute
+		installButtonText    = "install"
+		provisioningTimeout  = 3 * time.Minute
+		maxAttempts          = 2
+		playStorePackage     = "com.android.vending"
+		assetBrowserActivity = "com.android.vending.AssetBrowserActivity"
 	)
-	pkgName := []string{"com.google.android.apps.youtube.creator"}
-
 	fdms := s.FixtValue().(*familylink.FixtData).FakeDMS
 	cr := s.FixtValue().(*familylink.FixtData).Chrome
 	tconn := s.FixtValue().(*familylink.FixtData).TestConn
-
-	// Update the policy with blocked apps list.
 	arcEnabledPolicy := &policy.ArcEnabled{Val: true}
-	blockedApps := []policy.Application{
-		{
-			PackageName: pkgName[0],
-			InstallType: "BLOCKED",
-		},
-	}
-	blockedAppsPolicy := &policy.ArcPolicy{
-		Val: &policy.ArcPolicyValue{
-			Applications: blockedApps,
-		},
-	}
-	policies := []policy.Policy{blockedAppsPolicy, arcEnabledPolicy}
+
+	policies := []policy.Policy{arcEnabledPolicy}
 	pb := policy.NewBlob()
 	pb.PolicyUser = s.FixtValue().(*familylink.FixtData).PolicyUser
 	pb.AddPolicies(policies)
@@ -75,20 +63,6 @@ func UnicornBlockedApps(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to serve policies: ", err)
 	}
 
-	// Ensure chrome://policy shows correct ArcEnabled and ArcPolicy values.
-	if err := policyutil.Verify(ctx, tconn, []policy.Policy{&policy.ArcEnabled{Val: true}}); err != nil {
-		s.Fatal("Failed to verify ArcEnabled: ", err)
-	}
-
-	if err := verifyArcPolicyBlockedApps(ctx, tconn, pkgName); err != nil {
-		s.Fatal("Failed to verify blocked apps in ArcPolicy: ", err)
-	}
-
-	if err := launcher.LaunchApp(tconn, apps.PlayStore.Name)(ctx); err != nil {
-		s.Fatal("Failed to launch Play Store")
-	}
-	defer apps.Close(ctx, tconn, apps.PlayStore.ID)
-
 	// Setup ARC.
 	a, err := arc.New(ctx, s.OutDir())
 	if err != nil {
@@ -96,26 +70,26 @@ func UnicornBlockedApps(ctx context.Context, s *testing.State) {
 	}
 	defer a.Close(ctx)
 
+	if err := a.WaitForProvisioning(ctx, provisioningTimeout); err != nil {
+		s.Fatal("Failed to wait for provisioning: ", err)
+	}
+
+	s.Log("Starting Play Store")
+	act, err := arc.NewActivity(a, playStorePackage, assetBrowserActivity)
+	if err != nil {
+		s.Fatal("Failed to create new activity: ", err)
+	}
+	defer act.Close()
+
+	if err := act.Start(ctx, tconn); err != nil {
+		s.Fatal("Failed starting Play Store or Play Store is empty: ", err)
+	}
+
 	d, err := a.NewUIDevice(ctx)
 	if err != nil {
 		s.Fatal("Failed initializing UI Automator: ", err)
 	}
 	defer d.Close(ctx)
-
-	// Verify PlayStore is Open.
-	if err := optin.WaitForPlayStoreShown(ctx, tconn, time.Minute); err != nil {
-		// Ensure Play Store is not empty.
-		s.Log("Starting Play Store")
-		act, err := arc.NewActivity(a, "com.android.vending", "com.android.vending.AssetBrowserActivity")
-		if err != nil {
-			s.Fatal("Failed to create new activity: ", err)
-		}
-		defer act.Close()
-
-		if err := act.StartWithDefaultOptions(ctx, tconn); err != nil {
-			s.Fatal("Failed starting Play Store or Play Store is empty: ", err)
-		}
-	}
 
 	searchText := d.Object(ui.ClassName("android.widget.TextView"), ui.Text("Search for apps & games"))
 	if err := searchText.WaitForExists(ctx, DefaultUITimeout); err != nil {
@@ -142,57 +116,4 @@ func UnicornBlockedApps(ctx context.Context, s *testing.State) {
 	} else if enabled {
 		s.Fatal("Install button is enabled")
 	}
-}
-
-// verifyArcPolicyBlockedApps matches ArcPolicy BLOCKD apps list with expected packages.
-func verifyArcPolicyBlockedApps(ctx context.Context, tconn *chrome.TestConn, appslist []string) error {
-	dps, err := policyutil.PoliciesFromDUT(ctx, tconn)
-	if err != nil {
-		return err
-	}
-
-	expected := &policy.ArcPolicy{}
-	actual, ok := dps.Chrome[expected.Name()]
-	if !ok {
-		return errors.New("no such a policy ArcPolicy")
-	}
-	value, err := expected.UnmarshalAs(actual.ValueJSON)
-	if err != nil {
-		return err
-	}
-	arcPolicyValue, ok := value.(policy.ArcPolicyValue)
-	if !ok {
-		return errors.Errorf("cannot extract ArcPolicyValue %s", value)
-	}
-
-	apps := arcPolicyValue.Applications
-	blocked := make(map[string]bool)
-	for _, application := range apps {
-		packageName := application.PackageName
-		installType := application.InstallType
-		if installType == "BLOCKED" {
-			blocked[packageName] = true
-		}
-	}
-	for _, app := range appslist {
-		if blocked[app] {
-			delete(blocked, app)
-		} else {
-			return errors.Errorf("the package not BLOCKED by policy: %s", app)
-		}
-	}
-	if len(blocked) != 0 {
-		return errors.Errorf("Extra BLOCKED packages in ArcPolicy: %s", makeList(blocked))
-	}
-	return nil
-}
-
-// makeList returns a list of keys from map.
-func makeList(packages map[string]bool) []string {
-	var packagesList []string
-	for pkg := range packages {
-		packagesList = append(packagesList, pkg)
-	}
-	sort.Strings(packagesList)
-	return packagesList
 }
