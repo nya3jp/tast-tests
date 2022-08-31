2f8cced3187727944a12ebe8c08e2ea50c37ec86
Jun Sheng <shengjun@google.com>
2022-08-31 18:18:14 +1000

    tast-tests: enable Lacros to Cursive smoke test
    1. Add Lacros flavour to Cursive smoke test.
    2. Refactor CloseBrowser function to return error.
    3. Validate session before close browser.
    In some cases, browser is already closed before cleanup. Calling closeBrowser() throws out nil pointer error.
    4. Add chrome.CloseTargets to allow closing multiple targets.
    
    Launching Lacros browser functions mostly return a cleanup method to close browser. This cleanup method omits potential errors which is not ideal and also causes inconvenience to callers.
    
    Using func(ctx context.Context) error lets callers decide omit the error or not, and makes it compatible with uiauto.Action.
    
    BUG=b:219814505
    TEST=tast run <DUT> apps.CursiveSmoke*
    
    Change-Id: If7c5ccb97192a6029d78e966247fad901453c33a

--- src/chromiumos/tast/local/bundles/cros/apps/cursive_smoke.go
+++ src/chromiumos/tast/local/bundles/cros/apps/cursive_smoke.go
@@ -14,7 +14,10 @@ import (
 	"chromiumos/tast/local/apps"
 	"chromiumos/tast/local/bundles/cros/apps/cursive"
 	"chromiumos/tast/local/bundles/cros/apps/fixture"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
@@ -42,24 +45,31 @@ var cursiveEnabledModels = []string{
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         CursiveSmoke,
-		LacrosStatus: testing.LacrosVariantUnknown,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Cursive smoke test app launching and basic function",
 		Contacts: []string{
 			"shengjun@chromium.org",
 			"gabpalado@google.com",
 		},
 		Attr:         []string{"group:mainline", "informational"},
-		Fixture:      fixture.LoggedIn,
 		Timeout:      5 * time.Minute,
 		SoftwareDeps: []string{"chrome", "chrome_internal"},
 		HardwareDeps: hwdep.D(hwdep.Model(cursiveEnabledModels...)),
 		Vars:         []string{"cursiveServerURL"},
+		Params: []testing.Param{{
+			Fixture: fixture.LoggedIn,
+		}, {
+			Name:              "lacros",
+			Fixture:           fixture.LacrosLoggedIn,
+			ExtraSoftwareDeps: []string{"lacros"},
+		}},
 	})
 }
 
 func CursiveSmoke(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(fixture.FixtData).Chrome
 	tconn := s.FixtValue().(fixture.FixtData).TestAPIConn
+	browserType := s.FixtValue().(fixture.FixtData).BrowserType
 
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
@@ -76,24 +86,29 @@ func CursiveSmoke(ctx context.Context, s *testing.State) {
 		appURL = serverURL
 	}
 
-	conn, err := cr.NewConn(ctx, appURL)
+	conn, br, closeBrowser, err := browserfixt.SetUpWithURL(ctx, cr, browserType, appURL)
 	if err != nil {
-		s.Fatalf("Failed to open URL %q: %v", appURL, err)
+		s.Fatal("Failed to set up browser: ", err)
 	}
-	defer conn.Close()
-	defer conn.CloseTarget(cleanupCtx)
+
+	shouldClose := true
+	defer func(ctx context.Context) {
+		if shouldClose {
+			closeBrowser(cleanupCtx)
+			conn.Close()
+		}
+	}(cleanupCtx)
 
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
 		// Wait for longer time after second launch, since it can be delayed on low-end devices.
 		if err := ui.WithTimeout(30 * time.Second).WaitUntilExists(installIcon)(ctx); err != nil {
 			testing.ContextLog(ctx, "Install button is not shown initially. See b/230413572")
 			testing.ContextLog(ctx, "Refresh page to enable installation")
-			if reloadErr := conn.Eval(ctx, `location.reload()`, nil); reloadErr != nil {
+			if reloadErr := br.ReloadActiveTab(ctx); reloadErr != nil {
 				return testing.PollBreak(errors.Wrap(reloadErr, "failed to reload page"))
 			}
 			return err
 		}
-
 		return nil
 	}, &testing.PollOptions{Timeout: 3 * time.Minute}); err != nil {
 		s.Fatal("Failed to wait for Cursive to be installable: ", err)
@@ -110,10 +125,30 @@ func CursiveSmoke(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to wait for installed app: ", err)
 	}
 
-	// Cursive is automatically launched after installation.
-	// Reset Chrome state will close all opened targets.
-	if err := cr.ResetState(ctx); err != nil {
-		s.Fatal("Failed to reset Chrome state: ", err)
+	// The web page is the PWA to be installed. Keeping the page opened
+	// will make it in A11y which can make the ui finder a bit harder.
+	// Besides, have the web page & PWA app launched at the same time,
+	// can potentially cause conflicts? (I guess it is possible in theory),
+	// and nice to test the PWA launch in a clean environment align with real user experience.
+	if browserType == browser.TypeLacros {
+		if err := closeBrowser(ctx); err != nil {
+			s.Fatal("Failed to close Lacros: ", err)
+		}
+		// b/244390727 Lacros needs to be opened to make PWA launchable.
+		_, closeBrowser, err := browserfixt.SetUp(ctx, cr, browser.TypeLacros)
+		if err != nil {
+			s.Fatal("Failed to set up Lacros: ", err)
+		}
+		defer closeBrowser(cleanupCtx)
+	}
+	shouldClose = false
+
+	// Close all ash apps and pages to make a clean desk.
+	targetsToClose := func(t *chrome.Target) bool {
+		return t.Type == "page" || t.Type == "app"
+	}
+	if err := cr.CloseTargets(ctx, targetsToClose); err != nil {
+		s.Fatal("Failed to close targets: ", err)
 	}
 
 	// Validate Cursive can be launched from shelf.
