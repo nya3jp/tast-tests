a0aa70380edc53e46817e36c33b64468736e3dba
Taoyu Li <taoyl@google.com>
2022-08-31 17:39:09 +0900

    arc: IPv6Connectivity test adapts to ARCVM
    BUG=b:148306892
    TEST=tast run $DUT arc.IPv6Connectivity on guybrush (ARCVM)
    TEST=tast run $DUT arc.IPv6Connectivity on atlas (container)
    
    Change-Id: I31824b68cf6053a5baffc2216b1046fb03cabcb7

--- src/chromiumos/tast/local/bundles/cros/arc/ipv6_connectivity.go
+++ src/chromiumos/tast/local/bundles/cros/arc/ipv6_connectivity.go
@@ -9,10 +9,12 @@ import (
 	"net"
 	"time"
 
+	pp "chromiumos/system_api/patchpanel_proto"
 	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
+	patchpanel "chromiumos/tast/local/network/patchpanel_client"
 	"chromiumos/tast/local/network/routing"
 	"chromiumos/tast/testing"
 )
@@ -24,7 +26,7 @@ func init() {
 		Desc:         "Checks IPv6 connectivity inside ARC",
 		Contacts:     []string{"taoyl@google.com", "cros-networking@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
-		SoftwareDeps: []string{"android_p", "chrome"},
+		SoftwareDeps: []string{"arc", "chrome"},
 		Timeout:      4 * time.Minute,
 		Fixture:      "arcBooted",
 	})
@@ -49,11 +51,15 @@ func IPv6Connectivity(ctx context.Context, s *testing.State) {
 	}(cleanupCtx)
 
 	vethName := testEnv.BaseRouter.VethOutName
+	arcIfname, err := getARCInterfaceName(ctx, vethName)
+	if err != nil {
+		s.Fatalf("Failed to get ARC interface name corresponding to %s: %v", vethName, err)
+	}
 
 	// Check if testEnv prefix propagated into ARC, and log it for debugging.
 	const addressPollTimeout = 5 * time.Second
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		out, err := a.Command(ctx, "/system/bin/ip", "-6", "addr", "show", "scope", "global", "dev", vethName).Output(testexec.DumpLogOnError)
+		out, err := a.Command(ctx, "/system/bin/ip", "-6", "addr", "show", "scope", "global", "dev", arcIfname).Output(testexec.DumpLogOnError)
 		if err != nil {
 			return err
 		}
@@ -63,7 +69,7 @@ func IPv6Connectivity(ctx context.Context, s *testing.State) {
 		testing.ContextLog(ctx, "ARC address information: ", string(out))
 		return nil
 	}, &testing.PollOptions{Timeout: addressPollTimeout}); err != nil {
-		s.Fatalf("Failed to get global IPv6 address on %s in ARC: %v", vethName, err)
+		s.Fatalf("Failed to get global IPv6 address on %s in ARC: %v", arcIfname, err)
 	}
 
 	// ping virtual router address and virtual server address from ARC.
@@ -80,12 +86,29 @@ func IPv6Connectivity(ctx context.Context, s *testing.State) {
 	pingAddrs = append(pingAddrs, routerAddrs.IPv6Addrs...)
 	pingAddrs = append(pingAddrs, serverAddrs.IPv6Addrs...)
 	for _, ip := range pingAddrs {
-		if err := arc.ExpectPingSuccess(ctx, a, vethName, ip.String()); err != nil {
-			s.Fatalf("Failed to ping %s from ARC over %q: %v", ip.String(), vethName, err)
+		if err := arc.ExpectPingSuccess(ctx, a, arcIfname, ip.String()); err != nil {
+			s.Fatalf("Failed to ping %s from ARC over %q: %v", ip.String(), arcIfname, err)
 		}
 		if err := arc.ExpectPingSuccess(ctx, a, "", ip.String()); err != nil {
 			s.Fatalf("Failed to ping %s from ARC over default network: %v", ip.String(), err)
 		}
 	}
 
+}
+
+func getARCInterfaceName(ctx context.Context, hostIfname string) (string, error) {
+	pc, err := patchpanel.New(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create patchpanel client")
+	}
+	response, err := pc.GetDevices(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get patchpanel devices")
+	}
+	for _, device := range response.Devices {
+		if (device.GuestType == pp.NetworkDevice_ARCVM || device.GuestType == pp.NetworkDevice_ARC) && device.PhysIfname == hostIfname {
+			return device.GuestIfname, nil
+		}
+	}
+	return "", errors.Errorf("no ARC device matching %s is found", hostIfname)
 }
