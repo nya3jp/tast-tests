85cb9565894805362606e2075653bb7ebe663805
Nikhil Nayunigari <nikhilcn@google.com>
2022-09-01 18:10:17 +0000

    [CrOS Cellular] Adding a tast test to apply managed networks policy
    Test that applies managed cellular networks policy and verifies that
    the user cannot add new network from OS settings and quick settings.
    
    BUG=b:242351599
    TEST=tast run localhost:2233 cellular.ESimAllowCellularPolicyNetworksOn
    
    Change-Id: I9a91a53c9fe4c614c387ad38fb6d1a721077f269

---
+++ src/chromiumos/tast/local/bundles/cros/cellular/allow_cellular_policy_networks_on.go
@@ -0,0 +1,113 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cellular
+
+import (
+	"context"
+
+	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/local/cellular"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/ossettings"
+	"chromiumos/tast/local/chrome/uiauto/quicksettings"
+	"chromiumos/tast/local/chrome/uiauto/restriction"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         AllowCellularPolicyNetworksOn,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Tests the policy that only allows managed cellular networks",
+		Contacts: []string{
+			"nikhilcn@google.com",
+			"cros-connectivity@google.com",
+		},
+		SoftwareDeps: []string{"chrome"},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_active"},
+		Fixture:      fixture.FakeDMSEnrolled,
+		Vars:         []string{"autotest_host_info_labels"},
+	})
+}
+
+func AllowCellularPolicyNetworksOn(ctx context.Context, s *testing.State) {
+	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
+
+	// Start a Chrome instance that will fetch policies from the FakeDMS.
+	cr, err := chrome.New(ctx,
+		chrome.FakeLogin(chrome.Creds{User: fixtures.Username, Pass: fixtures.Password}),
+		chrome.DMSPolicy(fdms.URL),
+		chrome.KeepEnrollment())
+	if err != nil {
+		s.Fatal("Chrome login failed: ", err)
+	}
+	defer cr.Close(ctx)
+
+	// Resets chrome and cleans up any pre-existing policies.
+	if err := policyutil.ResetChrome(ctx, fdms, cr); err != nil {
+		s.Fatal("Failed to reset chrome: ", err)
+	}
+
+	tconn, err := cr.TestAPIConn(ctx)
+
+	globalConfig := &policy.ONCGlobalNetworkConfiguration{
+		AllowOnlyPolicyCellularNetworks: true,
+	}
+
+	deviceNetworkPolicy := &policy.DeviceOpenNetworkConfiguration{
+		Val: &policy.ONC{
+			GlobalNetworkConfiguration: globalConfig,
+			NetworkConfigurations:      []*policy.ONCNetworkConfiguration{},
+		},
+	}
+
+	// Apply Global Network Configuration.
+	if err := policyutil.ServeAndRefresh(ctx, fdms, cr, []policy.Policy{deviceNetworkPolicy}); err != nil {
+		s.Fatal("Failed to ServeAndRefresh ONC policy: ", err)
+	}
+
+	app, err := ossettings.Launch(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch OS Settings: ", err)
+	}
+
+	defer app.Close(ctx)
+
+	if err := app.SetToggleOption(cr, "Mobile data enable", true)(ctx); err != nil {
+		s.Fatal("Failed to enable mobile data in UI: ", err)
+	}
+
+	helper, err := cellular.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create cellular.Helper: ", err)
+	}
+	if err := helper.WaitForEnabledState(ctx, true); err != nil {
+		s.Fatal("Failed to enable Cellular state: ", err)
+	}
+
+	app, err = ossettings.OpenMobileDataSubpage(ctx, tconn, cr)
+	if err != nil {
+		s.Fatal("Failed to open mobile data settings: ", err)
+	}
+
+	ui := uiauto.New(tconn)
+
+	if err := ui.CheckRestriction(ossettings.AddCellularButton, restriction.Disabled)(ctx); err != nil {
+		s.Fatal("Add cellular button is not disabled: ", err)
+	}
+
+	if err := quicksettings.NavigateToNetworkDetailedView(ctx, tconn, false); err != nil {
+		s.Fatal("Failed to navigate to the detailed Network view: ", err)
+	}
+
+	if err := ui.Gone(quicksettings.AddCellularButton)(ctx); err != nil {
+		s.Fatal("Add cellular button is still present: ", err)
+	}
+}
