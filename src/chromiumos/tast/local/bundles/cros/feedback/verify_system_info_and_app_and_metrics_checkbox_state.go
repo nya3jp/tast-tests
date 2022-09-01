7a1309ed2e88945dca1b4498c818bf74c8d38454
Danny Wang <wangdanny@google.com>
2022-09-01 18:31:00 +0000

    Feedback: Verfiy system and app info and metrics checkbox is checked by default
    BUG=b:185624798
    TEST=tast run <DUT> feedback.VerifySystemInfoAndAppAndMetricsCheckboxState
    
    Change-Id: Icdd4037163c7da2b3fb875984889f6391dd91dbb

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/verify_system_info_and_app_and_metrics_checkbox_state.go
@@ -0,0 +1,69 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package feedback
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         VerifySystemInfoAndAppAndMetricsCheckboxState,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verify the system info and app and metrics checkbox is checked",
+		Contacts: []string{
+			"wangdanny@google.com",
+			"zhangwenyu@google.com",
+			"xiangdongkong@google.com",
+			"cros-feedback-app@google.com",
+		},
+		Fixture:      "chromeLoggedInWithOsFeedback",
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// VerifySystemInfoAndAppAndMetricsCheckboxState verifies system info and app and
+// metrics checkbox is checked by default.
+func VerifySystemInfoAndAppAndMetricsCheckboxState(ctx context.Context, s *testing.State) {
+	cr := s.FixtValue().(*chrome.Chrome)
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect to Test API: ", err)
+	}
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
+		"ui_dump")
+
+	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
+
+	// Launch feedback app and go to share data page.
+	feedbackRootNode, err := feedbackapp.LaunchAndGoToShareDataPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and go to share data page: ", err)
+	}
+
+	// Verify the system info and app and metrics checkbox is checked by default.
+	checkboxAncestor := nodewith.Name("Send system and app info and metrics").Role(
+		role.GenericContainer).Ancestor(feedbackRootNode)
+	checkbox := nodewith.Role(role.CheckBox).Ancestor(checkboxAncestor)
+	if err := ui.WaitUntilExists(checkbox.Attribute("checked", "true"))(ctx); err != nil {
+		s.Fatal("Failed to verify the checkbox is checked by default: ", err)
+	}
+}
