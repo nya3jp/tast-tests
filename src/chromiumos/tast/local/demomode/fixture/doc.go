295a0db1a436a5f40a45f97df2e07ce80f72b084
Yao Li <yaohuali@google.com>
2022-09-01 18:15:17 -0700

    Tast: Clear TPM before/after running demo mode test
    Many existing Tast tests use accounts on managedchrome.com. Running any
    of these tests would lock the DUT onto that domain, and prevent the DUT
    from entering demo mode (cros-demo-mode.com).
    
    Similarly, running demo mode test may lock the DUT in its domain.
    
    The solution is to reset TPM before/after running demo mode test. This
    CL creates a fixture to do TPM reset.
    
    BUG=b:235602920
    TEST=tast run dut demomode.SetUp
    
    Change-Id: Ifac18eb150b214383989d4fa4f8a14be2061b7cc

---
+++ src/chromiumos/tast/local/demomode/fixture/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package fixture contains Tast Fixture to reset TPM during SetUp and TearDown.
+package fixture
