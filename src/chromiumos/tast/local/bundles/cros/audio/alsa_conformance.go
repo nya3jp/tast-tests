d302a2b263c5426fb5d2a285ccfb70bfb58983e4
Judy Hsiao <judyhsiao@chromium.org>
2022-09-05 08:46:00 +0000

    ALSAConformance: skip taniks, dumo, dru and nasher
    Skip "taniks" before b/245056845 is fixed.
    Skip "dumo" and	"dru" before b/245061122 is fixed.
    Skip "nasher" before b/245063090 is fixed.
    Skip "bob" before b/245058202 is fixed.
    
    BUG=b:245056845, b:245061122, b:245063090, b:245058202
    TEST=None
    
    Change-Id: I2ff262d711359176836a18c4dc907c2e1356f8ff

--- src/chromiumos/tast/local/bundles/cros/audio/alsa_conformance.go
+++ src/chromiumos/tast/local/bundles/cros/audio/alsa_conformance.go
@@ -40,7 +40,11 @@ func init() {
 		// TODO(b/243344261) : remove "babymega" when b/243344261 is fixed.
 		// TODO(b/243344614) : remove "babytiger" when b/243344614 is fixed.
 		// TODO(b/243345196) : remove "blacktiplte" when b/243345196 is fixed.
-		HardwareDeps: hwdep.D(hwdep.Speaker(), hwdep.Microphone(), hwdep.SkipOnModel("chronicler", "nautilus", "nautiluslte", "soraka", "karma", "beetley", "redrix", "gimble", "primus", "anahera", "babymega", "babytiger", "blacktiplte")),
+		// TODO(b/245058202) : remove "bob" when b/245058202 is fixed.
+		// TODO(b/245056845) : remove "taniks" when b/245056845 is fixed.
+		// TODO(b/245061122) : remove "dumo" and "dru" when b/245061122 is fixed.
+		// TODO(b/245063090) : remove "nasher" when b/245063090 is fixed.
+		HardwareDeps: hwdep.D(hwdep.Speaker(), hwdep.Microphone(), hwdep.SkipOnModel("chronicler", "nautilus", "nautiluslte", "soraka", "karma", "beetley", "redrix", "gimble", "primus", "anahera", "babymega", "babytiger", "blacktiplte", "taniks", "bob", "dumo", "dru", "nasher")),
 		Timeout:      10 * time.Minute,
 	})
 }
