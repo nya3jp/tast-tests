f9d10f67bb8a2cfdba703214b87f046db4e735f0
Marcello Salomao <msalomao@google.com>
2022-09-06 17:59:36 +1000

    Harden pooled storage tast test
    This tast test currently relies on hardcoded i18n
    strings. Change it to rely on special classNames
    instead.
    
    Related CL: http://go/cl-c/3875623
    
    BUG=chromium:1355708
    TEST=tast run dut *.DrivefsPooledStorage.*
    
    Change-Id: I202741788f62054fafbdfcae3989286fdfa6428f

--- src/chromiumos/tast/local/bundles/cros/filemanager/drivefs_pooled_storage.go
+++ src/chromiumos/tast/local/bundles/cros/filemanager/drivefs_pooled_storage.go
@@ -7,7 +7,6 @@ package filemanager
 import (
 	"context"
 	"path/filepath"
-	"regexp"
 	"time"
 
 	"chromiumos/tast/ctxutil"
@@ -22,11 +21,10 @@ import (
 )
 
 type testCase struct {
-	user                     string
-	pass                     string
-	expectedBannerText       string
-	expectedVisualSignalText string
-	expectedStorageMeterText string
+	user                      string
+	pass                      string
+	expectedBannerClass       string
+	expectedStorageMeterClass string
 }
 
 func init() {
@@ -62,34 +60,28 @@ func init() {
 		Params: []testing.Param{{
 			Name: "low",
 			Val: testCase{
-				user:                     "filemanager.DrivefsPooledStorage.WarnUsername",
-				pass:                     "filemanager.DrivefsPooledStorage.WarnPassword",
-				expectedBannerText:       "Storage low 15% left of your 30 GB individual storage.",
-				expectedVisualSignalText: "",
-				expectedStorageMeterText: "",
+				user:                      "filemanager.DrivefsPooledStorage.WarnUsername",
+				pass:                      "filemanager.DrivefsPooledStorage.WarnPassword",
+				expectedBannerClass:       "tast-drive-low-individual-space",
+				expectedStorageMeterClass: "",
 			},
 		}, {
 			Name: "full_individual",
 			Val: testCase{
-				user:                     "filemanager.DrivefsPooledStorage.FullUsername",
-				pass:                     "filemanager.DrivefsPooledStorage.FullPassword",
-				expectedBannerText:       "Warning: You’ve used all your individual Google Workspace storage.",
-				expectedVisualSignalText: "There is not enough free space in your Google Drive to complete the upload.",
-				expectedStorageMeterText: "0 bytes available",
+				user:                      "filemanager.DrivefsPooledStorage.FullUsername",
+				pass:                      "filemanager.DrivefsPooledStorage.FullPassword",
+				expectedBannerClass:       "tast-drive-out-of-individual-space",
+				expectedStorageMeterClass: "tast-storage-meter-empty",
 			},
 		}, {
 			Name: "full_organization",
 			Val: testCase{
-				user:                     "filemanager.DrivefsPooledStorage.OrgFullUsername",
-				pass:                     "filemanager.DrivefsPooledStorage.OrgFullPassword",
-				expectedBannerText:       "Warning: Test Org has used all of its Google Workspace storage.",
-				expectedVisualSignalText: "Your organization requires more storage to complete the upload.",
-				expectedStorageMeterText: "",
+				user:                      "filemanager.DrivefsPooledStorage.OrgFullUsername",
+				pass:                      "filemanager.DrivefsPooledStorage.OrgFullPassword",
+				expectedBannerClass:       "tast-drive-out-of-organization-space",
+				expectedStorageMeterClass: "",
 			},
 		}},
-		Data: []string{
-			"test_1KB.txt",
-		},
 	})
 }
 
@@ -144,26 +136,24 @@ func DrivefsPooledStorage(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to navigate to My drive and open the 'More menu': ", err)
 	}
 
-	if tc.expectedStorageMeterText != "" {
+	if tc.expectedStorageMeterClass != "" {
 		// Read storage meter.
-		storageMeter, err := files.Info(
+		_, err := files.Info(
 			ctx,
-			nodewith.Ancestor(nodewith.HasClass("chrome-menu files-menu")).NameRegex(regexp.MustCompile(" ((used)|(available))$")).First())
+			nodewith.HasClass(tc.expectedStorageMeterClass).First())
 		if err != nil {
-			s.Fatal("Error retrieving storage meter: ", err)
-		} else if storageMeter.Name != tc.expectedStorageMeterText {
-			s.Fatalf("Unexpected storage meter contents. Expected %q, found: %q", tc.expectedStorageMeterText, storageMeter.Name)
+			s.Fatalf("Error retrieving storage meter with expected class %q. %v", tc.expectedStorageMeterClass, err)
 		}
 	}
 
-	if tc.expectedBannerText != "" {
+	if tc.expectedBannerClass != "" {
 		// Read active banner.
 		_, err := files.Info(
 			ctx,
-			nodewith.Ancestor(nodewith.HasClass("warning-message")).Name(tc.expectedBannerText).First())
+			nodewith.HasClass(tc.expectedBannerClass).Role("banner").First())
 
 		if err != nil {
-			s.Fatalf("Error retrieving banner with expected contents %q. %v", tc.expectedBannerText, err)
+			s.Fatalf("Error retrieving banner with expected class %q. %v", tc.expectedBannerClass, err)
 		}
 	}
 
