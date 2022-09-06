2555dc52541041c18c1676c6e3b58d5c19dc0fae
Yi Xie <yixie@chromium.org>
2022-09-06 19:01:44 +0900

    kiosk: Block FakeDMS access in AppsCachedOffline tast test
    Chrome still thinks it's online even if all outgoing traffic is blocked
    by iptables. By also blocking access to FakeDMS it will correctly
    recognize the offline state simulated by iptables.
    
    BUG=b:243451552
    TEST=tast run DUT kiosk.AppsCachedOffline
    
    Change-Id: Ic84ec0418ebabdf763b3400d4ba0f43d6e5ce517

--- src/chromiumos/tast/local/bundles/cros/kiosk/apps_cached_offline.go
+++ src/chromiumos/tast/local/bundles/cros/kiosk/apps_cached_offline.go
@@ -6,21 +6,28 @@ package kiosk
 
 import (
 	"context"
+	"net/url"
+	"strconv"
 	"time"
 
 	"chromiumos/tast/common/fixture"
+	"chromiumos/tast/common/network/firewall"
 	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/kioskmode"
 	"chromiumos/tast/local/network"
+	local_firewall "chromiumos/tast/local/network/firewall"
 	"chromiumos/tast/local/syslog"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func: AppsCachedOffline,
-		Desc: "Checks if Kiosk apps can be cached and launched offline",
+		Func:         AppsCachedOffline,
+		LacrosStatus: testing.LacrosVariantUnknown,
+		Desc:         "Checks if Kiosk apps can be cached and launched offline",
 		Contacts: []string{
 			"yixie@google.com", // Test author
 			"chromeos-kiosk-eng+TAST@google.com",
@@ -54,9 +61,45 @@ func AppsCachedOffline(ctx context.Context, s *testing.State) {
 	restartAndLaunchKiosk := func(ctx context.Context) error {
 		reader, err := syslog.NewReader(ctx, syslog.Program("chrome"))
 		if err != nil {
-			s.Fatal("Failed to start log reader: ", err)
+			return errors.Wrap(err, "failed to start log reader")
 		}
 		defer reader.Close()
+
+		// Additionally block access to FakeDMS to make Chrome think it's offline.
+		firewallRunner := local_firewall.NewLocalRunner()
+		fdmsURL, err := url.Parse(fdms.URL)
+		if err != nil {
+			return errors.Wrap(err, "failed to parse FakeDMS URL")
+		}
+		fdmsPort, err := strconv.Atoi(fdmsURL.Port())
+		if err != nil {
+			return errors.Wrap(err, "failed to parse port from FakeDMS URL")
+		}
+
+		commonRuleArgs := []firewall.RuleOption{
+			firewall.OptionProto(firewall.L4ProtoTCP),
+			firewall.OptionUIDOwner("chronos"),
+			firewall.OptionDPort(fdmsPort),
+			firewall.OptionJumpTarget(firewall.TargetDrop),
+		}
+		ruleArgs := []firewall.RuleOption{firewall.OptionAppendRule(firewall.OutputChain)}
+		ruleArgs = append(ruleArgs, commonRuleArgs...)
+		if err := firewallRunner.ExecuteCommand(ctx, ruleArgs...); err != nil {
+			return errors.Wrap(err, "failed to block access to FakeDMS")
+		}
+
+		// Reserve 3 seconds to resume firewall settings.
+		cleanupCtx := ctx
+		ctx, cancel := ctxutil.Shorten(ctx, 3*time.Second)
+		defer cancel()
+
+		defer func(cleanupCtx context.Context) {
+			ruleArgs := []firewall.RuleOption{firewall.OptionDeleteRule(firewall.OutputChain)}
+			ruleArgs = append(ruleArgs, commonRuleArgs...)
+			if err := firewallRunner.ExecuteCommand(ctx, ruleArgs...); err != nil {
+				s.Fatal("Failed to restore access to FakeDMS: ", err)
+			}
+		}(cleanupCtx)
 
 		_, err = kiosk.RestartChromeWithOptions(
 			ctx,
@@ -65,11 +108,11 @@ func AppsCachedOffline(ctx context.Context, s *testing.State) {
 			chrome.KeepState(),
 		)
 		if err != nil {
-			s.Fatal("Failed to restart Chrome: ", err)
+			return errors.Wrap(err, "failed to restart Chrome")
 		}
 
 		if err := kioskmode.ConfirmKioskStarted(ctx, reader); err != nil {
-			s.Fatal("Kiosk is not started after restarting Chrome: ", err)
+			return errors.Wrap(err, "kiosk is not started after restarting Chrome")
 		}
 
 		return nil
