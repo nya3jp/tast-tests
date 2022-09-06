3da3cd09e15247ab775b8b940e11ac16cc94cffd
John Admanski <jadmanski@google.com>
2022-09-06 14:41:11 -0700

    tast-tests: Add an AddRemoveFactorsEphemeral test.
    This is basically a stripped down AddRemoveFactors test that covers
    ephemeral users. Originally I tried just parameterizing the existing
    test but it grew so many if conditions that I felt the flow was actually
    harder to understand than just making a new, similar test.
    
    BUG=b:208357938
    TEST=fastbuild.sh -T
    TEST=tast test <dut> cryptohome.AddRemoveFactors.*
    
    Change-Id: I8353877b92cf39e3483b4518f8087fea764ba431

--- src/chromiumos/tast/common/cryptohome/helpers.go
+++ src/chromiumos/tast/common/cryptohome/helpers.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The ChromiumOS Authors.
+// Copyright 2022 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,6 +21,17 @@ This file implements miscellaneous and unsorted helpers.
 func ExpectAuthIntents(intents, expectedIntents []uda.AuthIntent) error {
 	less := func(a, b uda.AuthIntent) bool { return a < b }
 	diff := cmp.Diff(intents, expectedIntents, cmpopts.SortSlices(less))
+	if diff == "" {
+		return nil
+	}
+	return errors.New(diff)
+}
+
+// ExpectAuthFactorTypes checks whether two given sets of types are equal, and
+// in case they're not returns an error containing the formatted difference.
+func ExpectAuthFactorTypes(types, expectedTypes []uda.AuthFactorType) error {
+	less := func(a, b uda.AuthFactorType) bool { return a < b }
+	diff := cmp.Diff(types, expectedTypes, cmpopts.SortSlices(less))
 	if diff == "" {
 		return nil
 	}
