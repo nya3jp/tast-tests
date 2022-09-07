9e965a0e4e4838de9578a394f41ef4250fad31f8
Prashant Malani <pmalani@chromium.org>
2022-09-07 23:37:41 +0000

    typec: Add typecd test
    Add a test to check that typecd is running on a system.
    
    BUG=b:245609746
    TEST=Verify test is passing on guybrush and brya.
    
    Change-Id: Ide0611bde86687f0133a0979c3e469acecd9e842
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3884836
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Benson Leung <bleung@google.com>
    Reviewed-by: Jie Jiang <jiejiang@chromium.org>
    Tested-by: Prashant Malani <pmalani@chromium.org>
    Reviewed-by: Sergey Frolov <sfrolov@google.com>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/typec/typecd.go
@@ -0,0 +1,32 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package typec
+
+import (
+	"context"
+
+	"chromiumos/tast/local/typecutils"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Typecd,
+		LacrosStatus: testing.LacrosVariantUnknown,
+		Desc:         "Checks that typecd is running on a system",
+		Contacts:     []string{"pmalani@chromium.org", "chromeos-usb@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		HardwareDeps: hwdep.D(hwdep.ECFeatureTypecCmd(), hwdep.ChromeEC()),
+	})
+}
+
+// Typecd checks that typecd is running on the system.
+func Typecd(ctx context.Context, s *testing.State) {
+	_, err := typecutils.TypecdPID(ctx)
+	if err != nil {
+		s.Fatal("Failed to get typecd PID: ", err)
+	}
+}
