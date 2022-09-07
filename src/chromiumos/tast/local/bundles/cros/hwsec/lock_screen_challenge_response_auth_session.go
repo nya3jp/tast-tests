917f83b58e5866a07a49dd14583fd12347e098e0
Thomas Cedeno <thomascedeno@google.com>
2022-09-07 21:14:02 +0000

    cryptohome: Add Smart Card support to AddRemoveFactors
    With ListAuthFactors showing smart cards as supported,
    modify tast test cryptohome.AddRemoveFactors to handle
    smart cards.
    
    BUG=b:241016536
    TEST=tast run <DUT> cryptohome.AddRemoveFactors.*
    
    Change-Id: I512e5c26ded5d4fde3c2be5856f1eada2065e8a5

--- src/chromiumos/tast/local/bundles/cros/hwsec/lock_screen_challenge_response_auth_session.go
+++ src/chromiumos/tast/local/bundles/cros/hwsec/lock_screen_challenge_response_auth_session.go
@@ -12,7 +12,6 @@ import (
 
 	cpb "chromiumos/system_api/cryptohome_proto"
 	"chromiumos/tast/common/hwsec"
-	"chromiumos/tast/local/bundles/cros/hwsec/util"
 	"chromiumos/tast/local/cryptohome"
 	"chromiumos/tast/local/dbusutil"
 	hwseclocal "chromiumos/tast/local/hwsec"
@@ -108,7 +107,7 @@ func lockScreenLogin(ctx context.Context, s *testing.State, isEphemeral bool) {
 	}
 	defer dbusConn.ReleaseName(dbusName)
 
-	keyDelegate, err := util.NewCryptohomeKeyDelegate(
+	keyDelegate, err := hwsec.NewCryptohomeKeyDelegate(
 		s.Logf, dbusConn, testUser, keyAlgs, rsaKey, pubKeySPKIDER)
 	if err != nil {
 		s.Fatal("Failed to export D-Bus key delegate: ", err)
