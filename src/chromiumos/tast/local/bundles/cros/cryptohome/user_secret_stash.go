14e1dbdb22e5ff83c02d52c666ebded2fa82b59f
Zi Lin <lziest@google.com>
2022-09-08 21:37:45 +0000

    cryptohome: add a fixture for USS tests.
    The fixuture will set the USS flag prior to each test. It will ensure
    for once the cryptohomed is running, thus saving the duplicate code in
    each individual test.
    
    Also added a test for remove vk-based auth factor.
    
    BUG=b:245606607
    TEST=tast run DUT:port cryptohome.*
    
    Change-Id: Ibb797180679c3c847014ba35bb816a43bc325d83

--- src/chromiumos/tast/local/bundles/cros/cryptohome/user_secret_stash.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/user_secret_stash.go
@@ -24,7 +24,8 @@ func init() {
 			"emaxx@chromium.org",
 			"cryptohome-core@google.com",
 		},
-		Attr: []string{"group:mainline"},
+		Attr:    []string{"group:mainline"},
+		Fixture: "ussAuthSessionFixture",
 	})
 }
 
@@ -42,32 +43,6 @@ func UserSecretStash(ctx context.Context, s *testing.State) {
 
 	cmdRunner := hwseclocal.NewCmdRunner()
 	client := hwsec.NewCryptohomeClient(cmdRunner)
-	helper, err := hwseclocal.NewHelper(cmdRunner)
-	if err != nil {
-		s.Fatal("Failed to create hwsec local helper: ", err)
-	}
-	daemonController := helper.DaemonController()
-
-	// Wait for cryptohomed becomes available if needed.
-	if err := daemonController.Ensure(ctx, hwsec.CryptohomeDaemon); err != nil {
-		s.Fatal("Failed to ensure cryptohomed: ", err)
-	}
-
-	// Clean up obsolete state, in case there's any.
-	if err := client.UnmountAll(ctx); err != nil {
-		s.Fatal("Failed to unmount vaults for preparation: ", err)
-	}
-	if err := cryptohome.RemoveVault(ctx, userName); err != nil {
-		s.Fatal("Failed to remove old vault for preparation: ", err)
-	}
-
-	// Enable the UserSecretStash experiment for the duration of the test by
-	// creating a flag file that's checked by cryptohomed.
-	cleanupUSSExperiment, err := helper.EnableUserSecretStash(ctx)
-	if err != nil {
-		s.Fatal("Failed to enable the UserSecretStash experiment: ", err)
-	}
-	defer cleanupUSSExperiment(ctx)
 
 	// Create and mount the persistent user.
 	authSessionID, err := client.StartAuthSession(ctx, userName /*ephemeral=*/, false)
