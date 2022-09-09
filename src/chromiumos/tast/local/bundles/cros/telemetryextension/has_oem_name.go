9c538ae3eecd159ae3fa0aaebf62d478f86b3fe7
Oleh Lamzin <lamzin@google.com>
2022-09-09 13:41:04 +0000

    telemetryextension: add HadOEMName test
    This CL is part of new Telemetry Extension testing strategy:
    go/telemetry-extension-testing-strategy
    
    BUG=b:245724073
    TEST=tast run <IP> telemetryextension.HasOEMName.*
    
    Change-Id: Iae434535fb87a7060389482b762abf41a3a7cb99

---
+++ src/chromiumos/tast/local/bundles/cros/telemetryextension/has_oem_name.go
@@ -0,0 +1,54 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package telemetryextension
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/telemetryextension/dep"
+	"chromiumos/tast/local/bundles/cros/telemetryextension/vendorutils"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: HasOEMName,
+		Desc: "Verifies that DUT has correct OEM name",
+		Contacts: []string{
+			"lamzin@google.com", // Test and Telemetry Extension author
+			"cros-oem-services-team@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+		Params: []testing.Param{
+			{
+				Name:              "asus",
+				Val:               "ASUS",
+				ExtraHardwareDeps: dep.AsusModels(),
+			},
+			{
+				Name:              "hp",
+				Val:               "HP",
+				ExtraHardwareDeps: dep.HPModels(),
+			},
+		},
+	})
+}
+
+// HasOEMName tests that DUT has correct OEM name which comes from
+//   - /sys/devices/virtual/dmi/id/sys_vendor (old approach) or
+//   - /sys/firmware/vpd/ro/oem_name (new approach for unreleased models) or
+//   - CrOSConfig (new approach).
+func HasOEMName(ctx context.Context, s *testing.State) {
+	oemName, ok := s.Param().(string)
+	if !ok {
+		s.Fatal("Failed to convert params value into string: ", s.Param())
+	}
+
+	if vendor, err := vendorutils.FetchVendor(ctx); err != nil {
+		s.Error("Failed to read vendor name: ", err)
+	} else if got, want := vendor, oemName; got != want {
+		s.Errorf("Unexpected vendor name = got %q, want %q", got, want)
+	}
+}
