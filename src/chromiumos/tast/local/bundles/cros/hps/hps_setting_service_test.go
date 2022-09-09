1a82ae4c34e47eb4dcd29c8975999260288f7762
Michael Pishchagin <mblsha@google.com>
2022-09-09 11:44:01 +1000

    hps: Wait for powerd to write new settings after changing settings.
    If we don't do this we might read the outdated settings from
    powerlog.LATEST.
    
    BUG=b:237862387
    TEST=./fast_build.sh -T
    
    Change-Id: I9b6845e88bb8898a764e5a41d836ad2c21dd6391

--- src/chromiumos/tast/local/bundles/cros/hps/hps_setting_service_test.go
+++ src/chromiumos/tast/local/bundles/cros/hps/hps_setting_service_test.go
@@ -16,21 +16,24 @@ import (
 
 const (
 	valid1 = `
-	2022-05-04T04:10:50.189648Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
-	2022-05-04T04:12:04.597625Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
-	2022-05-04T04:12:04.608987Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=no-op use_audio=1 use_video=1 wake_locks=\n
-	2022-05-04T04:12:05.542450Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=shutdown use_audio=1 use_video=1 wake_locks=\n
-	2022-05-04T04:12:11.852847Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
-	2022-05-04T04:12:13.780787Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=6s screen_off=7m30s lock=0s quick_lock=2m6s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
-	2022-05-04T04:12:15.608926Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
+	2022-05-04T04:10:50.189648Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=1m quick_dim=1m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
+	2022-05-04T04:12:04.597625Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=2m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
+	2022-05-04T04:12:04.608987Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=3m quick_dim=3m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=no-op use_audio=1 use_video=1 wake_locks=\n
+	2022-05-04T04:12:05.542450Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=4m quick_dim=4m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=shutdown use_audio=1 use_video=1 wake_locks=\n
+	2022-05-04T04:12:11.852847Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=5m quick_dim=5m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
+	2022-05-04T04:12:13.780787Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=6m quick_dim=6m screen_off=7m30s lock=0s quick_lock=2m6s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
+	2022-05-04T04:12:15.608926Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=8m quick_dim=7m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
 	2022-05-04T04:12:46.090911Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=10s screen_off=7m30s lock=0s quick_lock=2m10s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=
+	2022-05-10T04:12:46.090911Z INFO powerd: [state_controller.cc(1166)] Updated settings
 	`
-	valid2 = `2022-05-04T04:12:46.835388Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks= \n
-	2022-05-06T00:30:20.734450Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
-	2022-05-06T00:30:20.374935Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
-	2022-05-06T00:30:20.734450Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
-	2022-05-06T00:30:45.736758Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=10s screen_off=7m30s lock=0s quick_lock=2m10s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
+	valid2 = `
+	2022-05-04T04:12:46.835388Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=1m quick_dim=1m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks= \n
+	2022-05-06T00:30:20.734450Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=2m quick_dim=2m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
+	2022-05-06T00:30:20.374935Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=3m quick_dim=3m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
+	2022-05-06T00:30:20.734450Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=4m quick_dim=4m screen_off=7m30s lock=0s quick_lock=8m30s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
+	2022-05-06T00:30:45.736758Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=6m quick_dim=5m screen_off=7m30s lock=0s quick_lock=2m10s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
 	2022-05-09T01:48:07.360070Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=5m quick_dim=6s screen_off=5m30s lock=0s quick_lock=2m6s idle_warn=0s idle=6m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=
+	2022-05-10T04:12:46.090911Z INFO powerd: [state_controller.cc(1166)] Updated settings
 	`
 	invalid1 = ""
 	invalid2 = `2022-05-04T04:12:13.780787Z INFO powerd: [state_controller.cc(1166)] Updated settings: dim=7m quick_dim=6s screen_off=7m30s lock=0s quick_lock=2m6s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=\n
@@ -47,12 +50,7 @@ func formatString(res *pb.RetrieveDimMetricsResponse) string {
 }
 
 func compareResult(result, expected *pb.RetrieveDimMetricsResponse) bool {
-	if result.DimDelay != expected.DimDelay ||
-		result.ScreenOffDelay != expected.ScreenOffDelay ||
-		result.LockDelay != expected.LockDelay {
-		return false
-	}
-	return true
+	return formatString(result) == formatString(expected)
 }
 
 type inputReq struct {
@@ -60,9 +58,31 @@ type inputReq struct {
 	isQuickDim bool
 }
 
-func TestRetrieveDimMetricsValid(t *testing.T) {
+func TestPowerdLastUpdatedSettings(t *testing.T) {
+	for i, tc := range []struct {
+		input  []byte
+		output string
+	}{
+		{
+			input:  []byte(valid1),
+			output: "Updated settings: dim=7m quick_dim=10s screen_off=7m30s lock=0s quick_lock=2m10s idle_warn=0s idle=8m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=",
+		},
+		{
+			input:  []byte(valid2),
+			output: "Updated settings: dim=5m quick_dim=6s screen_off=5m30s lock=0s quick_lock=2m6s idle_warn=0s idle=6m30s (no-op) lid_closed=suspend use_audio=1 use_video=1 wake_locks=",
+		},
+	} {
+		out := powerdLastUpdatedSettings(tc.input)
+		if out != tc.output {
+			t.Errorf(`Incorrect Output: index: %v
+Expected %q
+Got      %q`, i, tc.output, out)
+		}
+	}
+}
 
-	for _, tc := range []struct {
+func TestRetrieveDimMetricsValid(t *testing.T) {
+	for i, tc := range []struct {
 		input  *inputReq
 		output *pb.RetrieveDimMetricsResponse
 	}{
@@ -111,19 +131,19 @@ func TestRetrieveDimMetricsValid(t *testing.T) {
 			},
 		},
 	} {
-
 		result, err := processContent(tc.input.content, tc.input.isQuickDim)
 		if err != nil {
 			t.Errorf("error retrieving metrics: %q", err)
 		}
-		if compareResult(result, tc.output) {
-			t.Errorf("Incorrect Output: expected %q, get %q", formatString(tc.output), formatString(result))
+		if !compareResult(result, tc.output) {
+			t.Errorf(`Incorrect Output: index: %v, isQuickDim: %t
+Expected %q
+Got      %q`, i, tc.input.isQuickDim, formatString(tc.output), formatString(result))
 		}
 	}
 }
 
 func TestRetrieveDimMetricsInvalid(t *testing.T) {
-
 	for _, tc := range []struct {
 		input  *inputReq
 		output *pb.RetrieveDimMetricsResponse
@@ -145,6 +165,13 @@ func TestRetrieveDimMetricsInvalid(t *testing.T) {
 		{
 			input: &inputReq{
 				content:    []byte(invalid2),
+				isQuickDim: false,
+			},
+			output: nil,
+		},
+		{
+			input: &inputReq{
+				content:    []byte(invalid2),
 				isQuickDim: true,
 			},
 			output: nil,
@@ -153,6 +180,13 @@ func TestRetrieveDimMetricsInvalid(t *testing.T) {
 			input: &inputReq{
 				content:    []byte(invalid3),
 				isQuickDim: false,
+			},
+			output: nil,
+		},
+		{
+			input: &inputReq{
+				content:    []byte(invalid3),
+				isQuickDim: true,
 			},
 			output: nil,
 		},
