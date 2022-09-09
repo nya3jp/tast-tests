aae8f2b81b9d9254111ad819b5335baf29a49d7c
Oleh Lamzin <lamzin@google.com>
2022-09-09 14:27:12 +0000

    telemetryextension: add Feature* prefix
    Add Feature* prefix to some tests.
    
    This CL is part of new Telemetry Extension testing strategy:
    go/telemetry-extension-testing-strategy
    
    BUG=b:245724111
    TEST=tast run <IP> telemetryextension.*
    
    Change-Id: I5b6d93031fa5ae611c9fbb9b01075ebf019db6c5

--- src/chromiumos/tast/local/bundles/cros/telemetryextension/feature_api_battery_capacity_routine.go
+++ src/chromiumos/tast/local/bundles/cros/telemetryextension/feature_api_battery_capacity_routine.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Copyright 2022 The ChromiumOS Authors.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,8 +7,6 @@ package telemetryextension
 import (
 	"context"
 
-	"chromiumos/tast/common/testexec"
-	"chromiumos/tast/errors"
 	"chromiumos/tast/local/bundles/cros/telemetryextension/dep"
 	"chromiumos/tast/local/bundles/cros/telemetryextension/fixture"
 	"chromiumos/tast/testing"
@@ -16,12 +14,12 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         APIOEMData,
+		Func:         FeatureAPIBatteryCapacityRoutine,
 		LacrosStatus: testing.LacrosVariantExists,
-		Desc:         "Tests chrome.os.telemetry.getOemData Chrome Extension API function exposed to Telemetry Extension",
+		Desc:         "Tests chrome.os.diagnostics.runBatteryCapacityRoutine Chrome Extension API function exposed to Telemetry Extension",
 		Contacts: []string{
-			"lamzin@google.com", // Test and Telemetry Extension author
-			"mgawad@google.com", // Telemetry Extension author
+			"lamzin@google.com",    // Test and Telemetry Extension author
+			"bkersting@google.com", // Test author
 			"cros-oem-services-team@google.com",
 		},
 		Attr:         []string{"group:mainline", "informational"},
@@ -71,39 +69,25 @@ func init() {
 	})
 }
 
-// APIOEMData tests chrome.os.telemetry.getOemData Chrome Extension API functionality.
-func APIOEMData(ctx context.Context, s *testing.State) {
+func FeatureAPIBatteryCapacityRoutine(ctx context.Context, s *testing.State) {
 	v := s.FixtValue().(*fixture.Value)
 
-	want, err := oemData(ctx)
-	if err != nil {
-		s.Fatal("Failed to get OEM data: ", err)
-	}
-
 	type response struct {
-		OemData string `json:"oemData"`
+		Status string `json:"status"`
 	}
 
+	// get result from the diagnostics API.
 	var resp response
 	if err := v.ExtConn.Call(ctx, &resp,
-		"tast.promisify(chrome.os.telemetry.getOemData)",
-	); err != nil {
+		"tast.promisify(chrome.os.diagnostics.runBatteryCapacityRoutine)"); err != nil {
 		s.Fatal("Failed to get response from Telemetry extenion service worker: ", err)
 	}
 
-	if got := resp.OemData; got != want {
-		s.Errorf("Unexpected OEM data: got %q; want %q", got, want)
+	// Assert the status is "passed" or "failed". We allow a failing or unsupported routine
+	//  (note: no "error", "cancelled", etc.) because we can't make assumptions about the
+	// DUT's battery state. "passed", "unsupported" and "failed" signal that the request
+	// successfully reached cros_healthd.
+	if resp.Status != "passed" && resp.Status != "failed" && resp.Status != "unsupported" {
+		s.Errorf(`Unexpected routine status: got %q; want "passed" or "failed" or "unsupported"`, resp.Status)
 	}
-}
-
-func oemData(ctx context.Context) (string, error) {
-	b, err := testexec.CommandContext(ctx, "/usr/share/cros/oemdata.sh").Output(testexec.DumpLogOnError)
-	if err != nil {
-		testing.ContextLog(ctx, "oemdata.sh output: ", string(b))
-		return "", errors.Wrap(err, "failed to get OEM data")
-	}
-	if len(b) == 0 {
-		return "", errors.New("OEM data is empty")
-	}
-	return string(b), nil
 }
