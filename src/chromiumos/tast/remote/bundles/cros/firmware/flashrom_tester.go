7164e97aad1b9895f8394ee8b7424dd718d833c3
Evan Benn <evanbenn@chromium.org>
2022-09-12 10:00:36 +1000

    flashrom_tester: Disable test
    Test probably bricked some DUTS in the lab. Disable the test.
    
    BUG=b:245996788
    TEST=fast_build.sh -T
    
    Change-Id: I46a01b42841478c1b1d705fc788e5da18b3c2bb3

--- src/chromiumos/tast/remote/bundles/cros/firmware/flashrom_tester.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/flashrom_tester.go
@@ -33,7 +33,7 @@ func init() {
 		Func:         FlashromTester,
 		Desc:         "Tast wrapper that runs flashrom_tester",
 		Contacts:     []string{"nartemiev@google.com", "cros-flashrom-team@google.com"},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{}, // TODO(b/239126062): Move to custom suite/schedule.
 		SoftwareDeps: []string{"crossystem", "flashrom"},
 		HardwareDeps: hwdep.D(hwdep.ChromeEC()),
 		Timeout:      30 * time.Minute,
