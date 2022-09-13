b036ddf72da9098ba9e64f01c340b59fb020fb7f
Luciano Pacheco <lucmult@chromium.org>
2022-09-13 15:02:04 +1000

    WIP: Perf test
    Change-Id: Id4d71c11bdfe3bc90134cd7a9c1f6d6ac3eaa7e4

--- src/chromiumos/tast/local/bundles/cros/filemanager/ui_perf.go
+++ src/chromiumos/tast/local/bundles/cros/filemanager/ui_perf.go
@@ -22,7 +22,6 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/filesapp"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
-	"chromiumos/tast/local/cpu"
 	"chromiumos/tast/local/cryptohome"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
@@ -83,16 +82,46 @@ func UIPerf(ctx context.Context, s *testing.State) {
 
 	pv := perf.NewValues()
 
-	// Measure how long 100 and 1,000 files take to render in Files app listbox.
-	for _, totalFiles := range []int{100, 1000} {
-		testCase := fmt.Sprintf("%d_files", totalFiles)
-		uma := fmt.Sprintf("FileBrowser.DirectoryListLoad.my_files.%d", totalFiles)
+	// // Measure how long 100 and 1,000 files take to render in Files app listbox.
+	// for _, totalFiles := range []int{100, 1000} {
+	// 	testCase := fmt.Sprintf("%d_files", totalFiles)
+	// 	uma := fmt.Sprintf("FileBrowser.DirectoryListLoad.my_files.%d", totalFiles)
+	// 	s.Run(ctx, testCase, func(ctx context.Context, s *testing.State) {
+	// 		defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, testCase)
+
+	// 		// Run the directory listing test and observe the histogram for output.
+	// 		histograms, err := metrics.Run(ctx, tconn, func(ctx context.Context) error {
+	// 			return testDirectoryListing(ctx, s, files, ew, testCase, downloadsPath, totalFiles)
+	// 		}, uma)
+	// 		if err != nil {
+	// 			s.Fatal("Failed to retrieve histogram data: ", err)
+	// 		}
+
+	// 		if len(histograms) != 1 {
+	// 			s.Fatalf("Failed to record histogram, got %d want 1", len(histograms))
+	// 		}
+	// 		if histograms[0].Sum == 0 {
+	// 			s.Fatal("Failed to record a histogram value for: ", uma)
+	// 		}
+
+	// 		pv.Set(perf.Metric{
+	// 			Name:      testCase,
+	// 			Unit:      "ms",
+	// 			Direction: perf.SmallerIsBetter,
+	// 		}, float64(histograms[0].Sum))
+	// 	})
+	// }
+
+	// Measure how long to display 10 and 100 available apps for a selected file.
+	for _, totalApps := range []int{10, 100} {
+		testCase := fmt.Sprintf("%d_apps", totalApps)
+		uma := fmt.Sprintf("FileBrowser.UpdateAvailableApps.%d", totalApps)
 		s.Run(ctx, testCase, func(ctx context.Context, s *testing.State) {
 			defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, testCase)
 
 			// Run the directory listing test and observe the histogram for output.
 			histograms, err := metrics.Run(ctx, tconn, func(ctx context.Context) error {
-				return testDirectoryListing(ctx, s, files, ew, testCase, downloadsPath, totalFiles)
+				return testAvailableAppsListing(ctx, s, files, ew, testCase, downloadsPath, totalApps)
 			}, uma)
 			if err != nil {
 				s.Fatal("Failed to retrieve histogram data: ", err)
@@ -104,13 +133,8 @@ func UIPerf(ctx context.Context, s *testing.State) {
 			if histograms[0].Sum == 0 {
 				s.Fatal("Failed to record a histogram value for: ", uma)
 			}
-
-			pv.Set(perf.Metric{
-				Name:      testCase,
-				Unit:      "ms",
-				Direction: perf.SmallerIsBetter,
-			}, float64(histograms[0].Sum))
 		})
+
 	}
 
 	if err := pv.Save(s.OutDir()); err != nil {
@@ -140,10 +164,11 @@ func testDirectoryListing(ctx context.Context, s *testing.State, files *filesapp
 		return errors.Wrapf(err, "failed to create folder with name %q", folderPath)
 	}
 
+	// TODO: Reenable it.
 	// Wait until cpu idle before starting performance measures.
-	if err := cpu.WaitUntilIdle(ctx); err != nil {
-		return errors.Wrap(err, "failed to wait for CPU to be idle")
-	}
+	//if err := cpu.WaitUntilIdle(ctx); err != nil {
+	//	return errors.Wrap(err, "failed to wait for CPU to be idle")
+	//}
 
 	filesAllLoaded := func(ctx context.Context) error {
 		return testing.Poll(ctx, func(ctx context.Context) error {
@@ -160,5 +185,58 @@ func testDirectoryListing(ctx context.Context, s *testing.State, files *filesapp
 	return uiauto.Combine("Navigate to folder and keep selecting all until total count matches expected",
 		files.OpenPath(filesapp.FilesTitlePrefix+filesapp.Downloads, filesapp.Downloads, testCase),
 		filesAllLoaded,
+	)(ctx)
+}
+
+func createPWAs(ctx context.Context, folderPath string, totalApps int) error {
+	// for i := 0; i < totalApps; i++ {
+	// 	filePath := filepath.Join(folderPath, fmt.Sprintf("File-%d.file", i))
+	// 	if err := ioutil.WriteFile(filePath, []byte("blah"), 0644); err != nil {
+	// 		return errors.Wrapf(err, "failed to create file with path %q", filePath)
+	// 	}
+	// }
+	return nil
+}
+
+func testAvailableAppsListing(ctx context.Context, s *testing.State, files *filesapp.FilesApp, ew *input.KeyboardEventWriter, testCase, downloadsPath string, totalApps int) error {
+	folderPath := filepath.Join(downloadsPath, testCase)
+	if err := os.MkdirAll(folderPath, 0644); err != nil {
+		return errors.Wrapf(err, "failed to create directory %q", folderPath)
+	}
+	// TODO: Reenable it.
+	// defer os.RemoveAll(folderPath)
+
+	// Setup one text file to be selected.
+	fileName := "file.txt"
+	filePath := filepath.Join(folderPath, fileName)
+	if err := ioutil.WriteFile(filePath, []byte("blah"), 0644); err != nil {
+		return errors.Wrapf(err, "failed to create file with path %q", filePath)
+	}
+
+	// Setup the apps.
+	if err := createPWAs(ctx, folderPath, totalApps); err != nil {
+		return errors.Wrapf(err, "failed to create apps with name %q", folderPath)
+	}
+
+	// TODO: Reenable it.
+	// Wait until cpu idle before starting performance measures.
+	//if err := cpu.WaitUntilIdle(ctx); err != nil {
+	//	return errors.Wrap(err, "failed to wait for CPU to be idle")
+	//}
+
+	selectFile := func(ctx context.Context) error {
+		return testing.Poll(ctx, func(ctx context.Context) error {
+			return files.SelectFile(fileName)(ctx)
+			//.Exists(nodewith.Name(fmt.Sprintf("%d files selected", )).Role(role.StaticText))(ctx)
+			// The Interval is 2s as pressing Ctrl+A has a UI performance hit, it will
+			// slow down the listing whilst the selection is tallied, want to avoid
+			// calling this too often to ensure the data is less tainted.
+		}, &testing.PollOptions{Timeout: time.Minute, Interval: 2 * time.Second})
+	}
+
+
+	return uiauto.Combine("Navigate to folder, select the file and wait until the total apps count matches expected",
+		files.OpenPath(filesapp.FilesTitlePrefix+filesapp.Downloads, filesapp.Downloads, testCase),
+		selectFile,
 	)(ctx)
 }
