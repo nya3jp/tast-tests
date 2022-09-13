aacbc02a3421de177b54207a09658197dec0f23b
Jason Jeremy Iman <jasongustaman@google.com>
2022-09-13 13:22:32 +0900

    network: DNS proxy over VPN tests use virtualnet
    The test is updated such that the primary network is using virtualnet.
    Alongside that, the DNS and DoH server is updated to be a local server
    also created through virtualnet.
    
    The environment is set up as follows:
    -------------    ----------    --------------
    | interface | -> | router | -> | DNS server |
    -------------    ----------    --------------
                         |         --------------------
    	             --------> | VPN + DNS server |
                                   --------------------
    
    Alongside that, the block rules for VPN is also updated from FORWARD
    to INPUT. This is done because the DNS server now lives inside the
    VPN server. Previously the DNS server is outside the VPN server and
    as such the DNS queries are forwarded by the VPN.
    
    BUG=b:243620248
    TEST=tast run <DUT> network.DNSProxyOverVPN.*
    
    Change-Id: I885e249f81aac47dbf7a863853baba49531b613d

--- src/chromiumos/tast/local/bundles/cros/network/dns_proxy_over_vpn.go
+++ src/chromiumos/tast/local/bundles/cros/network/dns_proxy_over_vpn.go
@@ -7,7 +7,6 @@ package network
 import (
 	"bytes"
 	"context"
-	"strings"
 	"time"
 
 	"chromiumos/tast/common/testexec"
@@ -17,6 +16,9 @@ import (
 	"chromiumos/tast/local/bundles/cros/network/vpn"
 	"chromiumos/tast/local/crostini"
 	"chromiumos/tast/local/multivm"
+	"chromiumos/tast/local/network/virtualnet/certs"
+	"chromiumos/tast/local/network/virtualnet/env"
+	"chromiumos/tast/local/network/virtualnet/subnet"
 	"chromiumos/tast/testing"
 )
 
@@ -30,9 +32,7 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Ensure that DNS proxies are working correctly over VPN",
 		Contacts:     []string{"jasongustaman@google.com", "garrick@google.com", "cros-networking@google.com"},
-		// TODO(b/243620248): Disable the tests temporarily before we set up DNS and
-		// DoH servers properly in virtualnet.Env.
-		Attr:         []string{},
+		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "vm_host", "arc", "dlc", "no_kernel_upstream"},
 		Data:         []string{crostini.GetContainerMetadataArtifact("buster", false), crostini.GetContainerRootfsArtifact("buster", false)},
 		Pre:          multivm.ArcCrostiniStartedWithDNSProxy(),
@@ -56,9 +56,6 @@ func init() {
 		}},
 	})
 }
-
-// VPN interface name prefix for the test.
-const vpnIfnamePrefix = "ppp"
 
 // DNSProxyOverVPN tests DNS functionality with DNS proxy active.
 // There are 3 parts to this test:
@@ -85,12 +82,6 @@ func DNSProxyOverVPN(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to ping 8.8.8.8: ", err)
 	}
 
-	// Toggle plain-text DNS or secureDNS depending on test parameter.
-	params := s.Param().(dnsProxyOverVPNTestParams)
-	if err := dns.SetDoHMode(ctx, cr, tconn, params.mode, dns.GoogleDoHProvider); err != nil {
-		s.Fatal("Failed to set DNS-over-HTTPS mode: ", err)
-	}
-
 	// Ensure connectivity is available inside Crostini's container.
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
 		return cont.Command(ctx, "ping", "-c1", "-w1", "8.8.8.8").Run()
@@ -103,29 +94,22 @@ func DNSProxyOverVPN(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to install dig in container: ", err)
 	}
 
-	// Connect to VPN.
-	config := vpn.Config{
-		Type:     vpn.TypeL2TPIPsec,
-		AuthType: vpn.AuthTypePSK,
-	}
-	conn, err := vpn.NewConnection(ctx, config)
+	// Set up virtualnet environment.
+	pool := subnet.NewPool()
+	env, err := dns.SetupDNSEnv(ctx, pool)
 	if err != nil {
-		s.Fatal("Failed to create connection object: ", err)
+		s.Fatal("Failed to setup DNS env: ", err)
 	}
-	defer func() {
-		if err := conn.Cleanup(cleanupCtx); err != nil {
-			s.Error("Failed to clean up connection: ", err)
-		}
-	}()
-	if err := conn.SetUp(ctx); err != nil {
-		s.Fatal("Failed to setup VPN server: ", err)
+	defer env.Cleanup(cleanupCtx)
+	defer env.Router.Cleanup(cleanupCtx)
+	defer env.Server.Cleanup(cleanupCtx)
+
+	// Create and connect to a VPN server.
+	vpnServer, err := connectToVPN(ctx, pool, env.Router, env.Certs)
+	if err != nil {
+		s.Fatal("Failed to create VPN server: ", err)
 	}
-	if _, err := conn.Connect(ctx); err != nil {
-		s.Fatal("Failed to connect to VPN server: ", err)
-	}
-	if err := conn.Server.SetupInternetAccess(ctx); err != nil {
-		s.Fatal("Failed to setup internet connectivity for VPN: ", err)
-	}
+	defer vpnServer.Cleanup(cleanupCtx)
 
 	// Wait for the updated network configuration (VPN) to be propagated to the proxy.
 	if err := waitUntilNATIptablesConfigured(ctx); err != nil {
@@ -145,9 +129,10 @@ func DNSProxyOverVPN(ctx context.Context, s *testing.State) {
 		}
 	}
 
-	ns, err := vpnNamespace(ctx)
-	if err != nil {
-		s.Fatal("Failed to get VPN's network namespaces")
+	// Toggle plain-text DNS or secureDNS depending on test parameter.
+	params := s.Param().(dnsProxyOverVPNTestParams)
+	if err := dns.SetDoHMode(ctx, cr, tconn, params.mode, dns.ExampleDoHProvider); err != nil {
+		s.Fatal("Failed to set DNS-over-HTTPS mode: ", err)
 	}
 
 	// DNS queries that should be routed through VPN should fail if DNS queries on the VPN server are blocked.
@@ -160,7 +145,7 @@ func DNSProxyOverVPN(ctx context.Context, s *testing.State) {
 		{Client: dns.Crostini, ExpectErr: true},
 		{Client: dns.ARC}}
 	// Block DNS queries over VPN through iptables.
-	if errs := dns.NewVPNBlock(ns).Run(ctx, func(ctx context.Context) {
+	if errs := dns.NewVPNBlock(vpnServer.NetNSName).Run(ctx, func(ctx context.Context) {
 		if errs := dns.TestQueryDNSProxy(ctx, vpnBlockedTC, a, cont, dns.NewQueryOptions()); len(errs) != 0 {
 			s.Error("Failed DNS query check: ", errs)
 		}
@@ -180,36 +165,13 @@ func DNSProxyOverVPN(ctx context.Context, s *testing.State) {
 		{Client: dns.ARC}}
 	// Block DoH queries over VPN to verify that when a VPN is on, DoH is disabled and DNS will work.
 	// When a VPN is active, the default proxy and ARC proxy will disable secure DNS in order to have consistent behavior on different VPN types.
-	if errs := dns.NewDoHVPNBlock(ns).Run(ctx, func(ctx context.Context) {
+	if errs := dns.NewDoHVPNBlock(vpnServer.NetNSName).Run(ctx, func(ctx context.Context) {
 		if errs := dns.TestQueryDNSProxy(ctx, secureDNSBlockedTC, a, cont, dns.NewQueryOptions()); len(errs) != 0 {
 			s.Error("Failed DNS query check: ", errs)
 		}
 	}); len(errs) > 0 {
 		s.Fatal("Failed to block secure DNS over VPN: ", errs)
 	}
-}
-
-// vpnNamespace iterates through available network namespaces and return the namespace with the VPN server.
-// VPN namespace is identified by checking if the namespace contains a VPN interface.
-func vpnNamespace(ctx context.Context) (string, error) {
-	out, err := testexec.CommandContext(ctx, "ip", "netns", "list").Output()
-	if err != nil {
-		return "", err
-	}
-
-	for _, o := range strings.Split(strings.TrimSpace(string(out)), "\n") {
-		ns := strings.Fields(o)[0]
-		ifnames, err := testexec.CommandContext(ctx, "ip", "netns", "exec", ns, "ls", "/sys/class/net").Output()
-		if err != nil {
-			return "", nil
-		}
-		for _, ifname := range strings.Fields(string(ifnames)) {
-			if strings.HasPrefix(ifname, vpnIfnamePrefix) {
-				return ns, nil
-			}
-		}
-	}
-	return "", nil
 }
 
 // waitUntilNATIptablesConfigured waits until the NAT rule output of iptables is fully configured.
@@ -233,4 +195,58 @@ func waitUntilNATIptablesConfigured(ctx context.Context) error {
 		}
 		return nil
 	}, &testing.PollOptions{Interval: 2 * time.Second, Timeout: 15 * time.Second})
+}
+
+// connectToVPN creates a VPN server and connects to it.
+func connectToVPN(ctx context.Context, pool *subnet.Pool, router *env.Env, httpsCerts *certs.Certs) (serverEnv *env.Env, err error) {
+	serverIPv4Subnet, err := pool.AllocNextIPv4Subnet()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to allocate v4 subnet")
+	}
+	serverIPv6Subnet, err := pool.AllocNextIPv6Subnet()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to allocate v6 subnet")
+	}
+	server, err := dns.CreateDNSServerEnv(ctx, "vpnserver", serverIPv4Subnet, serverIPv6Subnet, router, httpsCerts)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to set up server env")
+	}
+
+	success := false
+	defer func() {
+		if success {
+			return
+		}
+		if err := server.Cleanup(ctx); err != nil {
+			testing.ContextLog(ctx, "Failed to cleanup server env: ", err)
+		}
+	}()
+
+	// Connect to VPN.
+	config := vpn.Config{
+		Type:     vpn.TypeL2TPIPsec,
+		AuthType: vpn.AuthTypePSK,
+	}
+	conn, err := vpn.NewConnectionWithEnvs(ctx, config, server, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create connection object")
+	}
+	defer func() {
+		if success {
+			return
+		}
+		if err := conn.Cleanup(ctx); err != nil {
+			testing.ContextLog(ctx, "Failed to clean up connection: ", err)
+		}
+	}()
+
+	if err := conn.SetUp(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to setup VPN server")
+	}
+	if _, err := conn.Connect(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to connect to VPN server")
+	}
+
+	success = true
+	return server, nil
 }
