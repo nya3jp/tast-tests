33770ce357c9e9c279707d75e4e261c03dca9d9f
Jason Jeremy Iman <jasongustaman@google.com>
2022-09-13 12:46:48 +0900

    network: virtualnet dnsmasq as DNS server
    The current dnsmasq always runs as a DHCP server (and optionally
    as a DNS server). This patch updates the behavior such that it is
    possible for dnsmasq to only be a DNS server.
    
    This patch also allows a virtualnet router to set a custom DNS
    server addresses.
    
    BUG=b:243620248
    TEST=With the follow-up patches;
         tast run <DUT> network.DNSProxy.*;
         tast run <DUT> network.DNSProxyOverVPN.*;
    
    Change-Id: I81e3bd5ffb7ffb517f7ea65d39f912383bbfe080

--- src/chromiumos/tast/local/network/virtualnet/dnsmasq/dnsmasq.go
+++ src/chromiumos/tast/local/network/virtualnet/dnsmasq/dnsmasq.go
@@ -24,9 +24,11 @@ import (
 const confTemplate = `
 port={{.port}}
 interface={{.ifname}}
+{{if .subnet}}
 dhcp-range={{.pool_start}},{{.pool_end}},{{.netmask}},12h
 dhcp-option=option:netmask,{{.netmask}}
 dhcp-option=option:router,{{.gateway}}
+{{end}}
 {{if .address}}
 address={{.address}}
 {{end}}
@@ -51,9 +53,10 @@ type dnsmasq struct {
 	resolvedHost    string
 	resolveHostToIP net.IP
 	// dns defines the addresses for external DNS servers.
-	dns       []string
-	cmd       *testexec.Cmd
-	enableDNS bool
+	dns        []string
+	cmd        *testexec.Cmd
+	enableDNS  bool
+	enableDHCP bool
 }
 
 // New creates a new dnsmasq object. enableDNS enables the DNS functionality. It supports
@@ -61,34 +64,48 @@ type dnsmasq struct {
 // in dnsmasq configuration. If resolveHostToIP is not set, resolvedHost is resolved to the
 // gateway. dns contains the DNS server list which will be broadcasted by DHCP. The returned
 // object can be passed to Env.StartServer(), its lifetime will be managed by the Env object.
-func New(subnet *net.IPNet, enableDNS bool, dns []string, resolvedHost string, resolveHostToIP net.IP) *dnsmasq {
-	return &dnsmasq{subnet: subnet, enableDNS: enableDNS, dns: dns, resolvedHost: resolvedHost, resolveHostToIP: resolveHostToIP}
+func New(enableDHCP, enableDNS bool, subnet *net.IPNet, dns []string, resolvedHost string, resolveHostToIP net.IP) *dnsmasq {
+	return &dnsmasq{enableDHCP: enableDHCP, enableDNS: enableDNS, subnet: subnet, dns: dns, resolvedHost: resolvedHost, resolveHostToIP: resolveHostToIP}
 }
 
 // Start starts the dnsmasq process.
 func (d *dnsmasq) Start(ctx context.Context, env *env.Env) error {
 	d.env = env
 
-	ip := d.subnet.IP.To4()
-	if ip == nil {
-		return errors.Errorf("given subnet %s is not invalid", d.subnet.String())
-	}
-
-	gateway := net.IPv4(ip[0], ip[1], ip[2], 1)
-	poolStart := net.IPv4(ip[0], ip[1], ip[2], 50)
-	poolEnd := net.IPv4(ip[0], ip[1], ip[2], 150)
-
-	// d.subnet.Mask is of type Mask and thus cannot be stringified as an IP.
-	mask := net.IPv4(255, 255, 255, 255).Mask(d.subnet.Mask)
-
 	// Prepare config file.
 	confVals := map[string]string{
-		"ifname":     d.env.VethInName,
-		"pool_start": poolStart.String(),
-		"pool_end":   poolEnd.String(),
-		"netmask":    mask.String(),
-		"gateway":    gateway.String(),
-		"port":       "0", // disable DNS
+		"ifname": d.env.VethInName,
+		"port":   "0", // disable DNS
+	}
+
+	var gateway net.IP
+	if d.enableDHCP {
+		if d.subnet == nil {
+			return errors.New("failed to start dnsmasq with DHCP: empty subnet")
+		}
+
+		ip := d.subnet.IP.To4()
+		if ip == nil {
+			return errors.Errorf("given subnet %s is not invalid", d.subnet.String())
+		}
+
+		gateway = net.IPv4(ip[0], ip[1], ip[2], 1)
+		poolStart := net.IPv4(ip[0], ip[1], ip[2], 50)
+		poolEnd := net.IPv4(ip[0], ip[1], ip[2], 150)
+
+		// d.subnet.Mask is of type Mask and thus cannot be stringified as an IP.
+		mask := net.IPv4(255, 255, 255, 255).Mask(d.subnet.Mask)
+
+		confVals["subnet"] = d.subnet.String()
+		confVals["pool_start"] = poolStart.String()
+		confVals["pool_end"] = poolEnd.String()
+		confVals["netmask"] = mask.String()
+		confVals["gateway"] = gateway.String()
+
+		// Install gateway address and routes.
+		if err := d.env.ConfigureInterface(ctx, d.env.VethInName, gateway, d.subnet); err != nil {
+			return errors.Wrap(err, "failed to configure IPv4 in netns")
+		}
 	}
 
 	if len(d.dns) > 0 {
@@ -97,10 +114,13 @@ func (d *dnsmasq) Start(ctx context.Context, env *env.Env) error {
 
 	var resolvedIP, resolvedHost string
 
-	if d.resolveHostToIP == nil {
+	if d.resolveHostToIP != nil {
+		resolvedIP = d.resolveHostToIP.String()
+	} else if gateway != nil {
 		resolvedIP = gateway.String()
 	} else {
-		resolvedIP = d.resolveHostToIP.String()
+		// Defaults to localhost address.
+		resolvedIP = "127.0.0.1"
 	}
 
 	if d.resolvedHost == "" {
@@ -117,11 +137,6 @@ func (d *dnsmasq) Start(ctx context.Context, env *env.Env) error {
 	template.Must(template.New("").Parse(confTemplate)).Execute(b, confVals)
 	if err := os.WriteFile(d.env.ChrootPath(confPath), []byte(b.String()), 0644); err != nil {
 		return errors.Wrap(err, "failed to write config file")
-	}
-
-	// Install gateway address and routes.
-	if err := d.env.ConfigureInterface(ctx, d.env.VethInName, gateway, d.subnet); err != nil {
-		return errors.Wrap(err, "failed to configure IPv4 in netns")
 	}
 
 	// Start the command.
