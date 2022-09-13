5ff9839108ba19f973b5831f7757f80019f3bb12
Luciano Pacheco <lucmult@chromium.org>
2022-09-13 15:02:04 +1000

    Tast: Files app perf test displaying 10 apps for selection
    BUG=b:228114084
    TEST=tast filemanager.UIPerf.*
    
    Change-Id: I690db1b107fd7b2b80423406534f9d1ce2a3d63c

--- src/chromiumos/tast/local/bundles/cros/filemanager/ui_perf.go
+++ src/chromiumos/tast/local/bundles/cros/filemanager/ui_perf.go
@@ -15,7 +15,6 @@ import (
 	"chromiumos/tast/common/perf"
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
-	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/metrics"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
@@ -24,14 +23,28 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/cpu"
 	"chromiumos/tast/local/cryptohome"
+	"chromiumos/tast/local/filemanager"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
 
+type perfTestFunc = func(ctx context.Context, s *testing.State, files *filesapp.FilesApp, ew *input.KeyboardEventWriter, testCase, downloadsPath string, testSize int) error
+
+type perfTest struct {
+	// Each test can run with multiple sizes, use when the initial setup can be shared for all sizes.
+	sizes []int
+	// The function that simulate the user operation.
+	testFunc perfTestFunc
+	// The UMA name that should be collected after running `testFunc`.
+	umaName string
+	// Whether the test requires to install PWAs.
+	installPwas bool
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         UIPerf,
-		LacrosStatus: testing.LacrosVariantUnneeded,
+		LacrosStatus: testing.LacrosVariantNeeded,
 		Desc:         "UI performance metrics for Files app",
 		Contacts: []string{
 			"benreich@chromium.org",
@@ -41,21 +54,40 @@ func init() {
 		Timeout:      4 * time.Minute,
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
+		Params: []testing.Param{
+			{
+				Name: "directory_list",
+				Val: perfTest{
+					sizes:       []int{100, 1000},
+					testFunc:    testDirectoryListing,
+					umaName:     "FileBrowser.DirectoryListLoad.my_files.%d",
+					installPwas: false,
+				},
+				Fixture: "openFilesApp",
+			},
+			{
+				Name: "list_apps",
+				Val: perfTest{
+					sizes:       []int{10}, // The metric is 10 apps.
+					testFunc:    testAvailableAppsListing,
+					umaName:     "FileBrowser.UpdateAvailableApps.%d",
+					installPwas: true,
+				},
+				Fixture: "install10Pwas",
+			},
+		},
 	})
 }
 
 func UIPerf(ctx context.Context, s *testing.State) {
+	fixt := s.FixtValue().(filemanager.FixtureData)
+	params := s.Param().(perfTest)
 
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
 	defer cancel()
 
-	// Start Chrome with SWA flag.
-	cr, err := chrome.New(ctx)
-	if err != nil {
-		s.Fatal("Cannot start Chrome: ", err)
-	}
-	defer cr.Close(cleanupCtx)
+	cr := fixt.Chrome
 
 	downloadsPath, err := cryptohome.DownloadsPath(ctx, cr.NormalizedUser())
 	if err != nil {
@@ -68,11 +100,7 @@ func UIPerf(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to connect to test API: ", err)
 	}
 
-	// Launch the Files app.
-	files, err := filesapp.Launch(ctx, tconn)
-	if err != nil {
-		s.Fatal("Launching the Files App failed: ", err)
-	}
+	files := fixt.FilesWindow
 
 	// Get a keyboard handle to use accelerators such as Ctrl+A.
 	ew, err := input.Keyboard(ctx)
@@ -83,16 +111,17 @@ func UIPerf(ctx context.Context, s *testing.State) {
 
 	pv := perf.NewValues()
 
-	// Measure how long 100 and 1,000 files take to render in Files app listbox.
-	for _, totalFiles := range []int{100, 1000} {
-		testCase := fmt.Sprintf("%d_files", totalFiles)
-		uma := fmt.Sprintf("FileBrowser.DirectoryListLoad.my_files.%d", totalFiles)
+	// Measure each "size" iteration of the test.
+	for _, size := range params.sizes {
+		testCase := fmt.Sprintf("%d_size", size)
+		s.Logf("Running test: %s", testCase)
+		uma := fmt.Sprintf(params.umaName, size)
 		s.Run(ctx, testCase, func(ctx context.Context, s *testing.State) {
 			defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, testCase)
 
 			// Run the directory listing test and observe the histogram for output.
 			histograms, err := metrics.Run(ctx, tconn, func(ctx context.Context) error {
-				return testDirectoryListing(ctx, s, files, ew, testCase, downloadsPath, totalFiles)
+				return params.testFunc(ctx, s, files, ew, testCase, downloadsPath, size)
 			}, uma)
 			if err != nil {
 				s.Fatal("Failed to retrieve histogram data: ", err)
@@ -122,7 +151,7 @@ func UIPerf(ctx context.Context, s *testing.State) {
 
 func createDirectoryListing(ctx context.Context, folderPath string, files int) error {
 	for i := 0; i < files; i++ {
-		filePath := filepath.Join(folderPath, fmt.Sprintf("File-%d.file", i))
+		filePath := filepath.Join(folderPath, fmt.Sprintf("File-%d.txt", i))
 		if err := ioutil.WriteFile(filePath, []byte("blah"), 0644); err != nil {
 			return errors.Wrapf(err, "failed to create file with path %q", filePath)
 		}
@@ -160,5 +189,59 @@ func testDirectoryListing(ctx context.Context, s *testing.State, files *filesapp
 	return uiauto.Combine("Navigate to folder and keep selecting all until total count matches expected",
 		files.OpenPath(filesapp.FilesTitlePrefix+filesapp.Downloads, filesapp.Downloads, testCase),
 		filesAllLoaded,
+	)(ctx)
+}
+
+func testAvailableAppsListing(ctx context.Context, s *testing.State, files *filesapp.FilesApp, ew *input.KeyboardEventWriter, testCase, downloadsPath string, totalApps int) error {
+	const totalFiles = 100
+	folderPath := filepath.Join(downloadsPath, testCase)
+	if err := os.MkdirAll(folderPath, 0644); err != nil {
+		return errors.Wrapf(err, "failed to create directory %q", folderPath)
+	}
+	defer os.RemoveAll(folderPath)
+
+	// Setup 100 text files to be selected.
+	if err := createDirectoryListing(ctx, folderPath, totalFiles); err != nil {
+		return errors.Wrapf(err, "failed to create folder with name %q", folderPath)
+	}
+
+	// Wait until cpu idle before starting performance measures.
+	if err := cpu.WaitUntilIdle(ctx); err != nil {
+		return errors.Wrap(err, "failed to wait for CPU to be idle")
+	}
+
+	selectFile := func(ctx context.Context) error {
+		return testing.Poll(ctx, func(ctx context.Context) error {
+			if err := ew.Accel(ctx, "ctrl+A"); err != nil {
+				return testing.PollBreak(errors.Wrap(err, "failed to select all files using Ctrl+A"))
+			}
+			return files.Exists(nodewith.Name(fmt.Sprintf("%d files selected", totalFiles)).Role(role.StaticText))(ctx)
+			// The Interval is 2s as pressing Ctrl+A has a UI performance hit, it will
+			// slow down the listing whilst the selection is tallied, want to avoid
+			// calling this too often to ensure the data is less tainted.
+		}, &testing.PollOptions{Timeout: time.Minute, Interval: 2 * time.Second})
+	}
+
+	checkNumApps := func(ctx context.Context) error {
+		return testing.Poll(ctx, func(ctx context.Context) error {
+			nodes, err := files.NodesInfo(ctx, nodewith.Role(role.MenuItem).Ancestor(nodewith.Role(role.Menu).Name(filesapp.OpenWith)))
+			if err != nil {
+				return testing.PollBreak(errors.Wrap(err, "failed to check the menuitems for Open with menu"))
+			}
+			// The menu might show more apps, like built-in apps in addition to the PWAs installed in the test.
+			if len(nodes) >= totalApps {
+				return nil
+			}
+			return errors.Errorf("waiting for at least %d apps, got %d ", totalApps, len(nodes))
+		}, &testing.PollOptions{Timeout: time.Minute, Interval: 2 * time.Second})
+	}
+
+	return uiauto.Combine("Navigate to folder, select all files and wait until the total apps count matches expected",
+		files.OpenPath(filesapp.FilesTitlePrefix+filesapp.Downloads, filesapp.Downloads, testCase),
+		selectFile,
+		files.ExpandOpenDropdown(),
+		checkNumApps,
+		// Use OpenPath() to dismiss the dropdown menu, otherwise it interferes with next test.
+		files.OpenPath(filesapp.FilesTitlePrefix+filesapp.Downloads, filesapp.Downloads, testCase),
 	)(ctx)
 }
