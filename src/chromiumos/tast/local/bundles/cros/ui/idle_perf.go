1a78c6e8115948718e515c3ebf783f5659d13dfa
Navin Ramsaroop <ramsaroop@google.com>
2022-09-13 21:50:32 +0000

    Increase IdlePerf duration to 10 minutes
    The current duration of 30 seconds is not enough to record meaningful
    power data, so increasing it to 10 minutes should make Ash/Lacros
    comparisons more accurate.
    
    BUG=b:245627671
    TEST=tast run <DUT> ui.IdlePerf.ash
    
    Change-Id: I771d1ff789318b3beb706197f03aca1774efa84b

--- src/chromiumos/tast/local/bundles/cros/ui/idle_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/idle_perf.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Copyright 2020 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -30,7 +30,7 @@ const (
 )
 
 const (
-	idleDuration   = 30 * time.Second
+	idleDuration   = 10 * time.Minute
 	emptyWindowURL = "about:blank"
 )
 
