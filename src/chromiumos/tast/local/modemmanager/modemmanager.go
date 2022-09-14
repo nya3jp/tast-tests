7852a6f6ecb010ac84060e4e03b1c4a9a53f4568
Jason Stanko <jstanko@google.com>
2022-09-14 19:08:13 +0000

    cellular: Migrate cellular_Callbox_AssertSMS
    This migrates the cellular_Callbox_AssertSMS test to Tast. It verifies
    that an SMS message sent by the callbox is received on the DUT.
    
    BUG=b:245546425
    TEST=TEST=tast run -var=callboxManager=<CBM> <DUT> cellular.AssertSMS
    
    Change-Id: I89a9a13219e55ff7af8fec62de25177a072ec64c

--- src/chromiumos/tast/local/modemmanager/modemmanager.go
+++ src/chromiumos/tast/local/modemmanager/modemmanager.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Copyright 2021 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,8 +12,10 @@ const (
 	DBusModemmanagerInterface            = "org.freedesktop.ModemManager1"
 	DBusModemmanagerBearerInterface      = "org.freedesktop.ModemManager1.Bearer"
 	DBusModemmanagerModemInterface       = "org.freedesktop.ModemManager1.Modem"
+	DBusModemmanagerMessageInterface     = "org.freedesktop.ModemManager1.Modem.Messaging"
 	DBusModemmanager3gppModemInterface   = "org.freedesktop.ModemManager1.Modem.Modem3gpp"
 	DBusModemmanagerSimpleModemInterface = "org.freedesktop.ModemManager1.Modem.Simple"
 	DBusModemmanagerSARInterface         = "org.freedesktop.ModemManager1.Modem.Sar"
 	DBusModemmanagerSimInterface         = "org.freedesktop.ModemManager1.Sim"
+	DBusModemmanagerSmsInterface         = "org.freedesktop.ModemManager1.Sms"
 )
