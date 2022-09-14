3fd06d85a70781ab9380d4103ea56ee18d4fc2b3
Tim Van Patten <timvp@google.com>
2022-09-14 12:53:16 -0600

    power/shutdown_with_command_battery_cutoff: Restrict to hwdep.NoBatteryBootSupported()
    The hwdep NoBatteryBootSupported() is added with the following CLs:
    
    1. https://crrev.com/c/3938863
    2. https://crrev.com/c/3935661
    
    This CL uses the new hwdep to retrict the test
    ShutdownWithCommandBatteryCutoff to devices that support booting with no
    battery. Currently, no devices have this hwdep enabled, but it will be
    enabled for:
    
    1. skyrim15w[360]
    2. skyrim6w[360]
    
    BUG=b:217911928
    BUG=b:251478678
    TEST=tast run <IP> power.ShutdownWithCommandBatteryCutoff
    
    Cq-Depend: chromium:3938863, chromium:3935661
    Change-Id: I7396d3e4be01db3cbfb6d5d68d1ce004e07c1df9

--- src/chromiumos/tast/remote/bundles/cros/power/shutdown_with_command_battery_cutoff.go
+++ src/chromiumos/tast/remote/bundles/cros/power/shutdown_with_command_battery_cutoff.go
@@ -30,8 +30,8 @@ func init() {
 		Attr:         []string{"group:mainline", "informational", "group:firmware", "firmware_unstable"},
 		Fixture:      fixture.NormalMode,
 		Vars:         []string{"servo"},
-		HardwareDeps: hwdep.D(hwdep.ChromeEC(), hwdep.Battery()),
 		Timeout:      5 * time.Minute,
+		HardwareDeps: hwdep.D(hwdep.ChromeEC(), hwdep.NoBatteryBootSupported()),
 	})
 }
 
