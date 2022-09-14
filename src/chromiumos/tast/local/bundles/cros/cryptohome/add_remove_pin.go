6defed97a105a57b9082566deaa9b01b3e0a1691
Anastasiia N <anastasiian@chromium.org>
2022-09-14 20:43:11 +0000

    tast-tests: Promote cryptohome.AddRemovePin
    The test passed in last 20+ builds.
    https://tastboard.corp.google.com/test/cryptohome.AddRemovePin
    
    BUG=None
    TEST=CQ
    
    Change-Id: I6cee045a800073f936374dd5d562c651942721a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3892063
    Reviewed-by: Alvin Jia <alvinjia@google.com>
    Tested-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/cryptohome/add_remove_pin.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/add_remove_pin.go
@@ -24,7 +24,7 @@ func init() {
 			"anastasiian@chromium.org",
 			"cryptohome-core@google.com",
 		},
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"pinweaver", "reboot"},
 		Fixture:      "ussAuthSessionFixture",
 	})
