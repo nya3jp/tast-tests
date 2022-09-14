68e65d9e91c85d593debb6fe525a88d64390f1ed
Bastian Kersting <bkersting@google.com>
2022-09-14 10:02:32 +0000

    telemetryextension: Add VPD feature test
    This CL introduces a new feature test that checks whether certain VPD
    fields are set correctly.
    
    This CL is part of new Telemetry Extension testing strategy:
    go/telemetry-extension-testing-strategy
    
    BUG=b:245724111
    TEST=tast run <IP> telemetryextension.FeatureVPDFields
    
    Change-Id: Icde44c6220a9bffa9943a665e02be9ee30b54a2d

---
+++ src/chromiumos/tast/local/bundles/cros/telemetryextension/feature_vpd_fields.go
@@ -0,0 +1,59 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package telemetryextension
+
+import (
+	"context"
+	"io/ioutil"
+
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         FeatureVPDFields,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Tests that VPD fields are properly set on the DUT",
+		Contacts: []string{
+			"lamzin@google.com",    // Telemetry Extension author
+			"bkersting@google.com", // Test and Telemetry Extension author
+			"cros-oem-services-team@google.com",
+		},
+		Attr: []string{"group:telemetry_extension_hw"},
+		Params: []testing.Param{
+			{
+				Name: "activate_date",
+				Val:  "/sys/firmware/vpd/rw/ActivateDate",
+			},
+			{
+				Name: "model_name",
+				Val:  "/sys/firmware/vpd/ro/model_name",
+			},
+			{
+				Name: "serial_number",
+				Val:  "/sys/firmware/vpd/ro/serial_number",
+			},
+			{
+				Name: "sku_number",
+				Val:  "/sys/firmware/vpd/ro/serial_number",
+			},
+		},
+	})
+}
+
+func FeatureVPDFields(ctx context.Context, s *testing.State) {
+	vpdPath, ok := s.Param().(string)
+	if !ok {
+		s.Fatal("Failed to convert params value into string: ", s.Param())
+	}
+
+	bytes, err := ioutil.ReadFile(vpdPath)
+	if err != nil {
+		s.Errorf("Unexpected err reading the VPD field at %q: %s", vpdPath, err)
+	}
+	if len(bytes) == 0 {
+		s.Errorf("Unexpected empty VPD field")
+	}
+}
