43a82c681c04d0223b65b568b919fb77f78e29dd
Yu-Hsuan Hsu <yuhsuan@chromium.org>
2022-09-14 16:39:00 +0800

    tast: Skip boards correctly
    Replace model with platform to skip boards correctly. In addition,
    create .unstable tests to run on those skipped boards.
    
    Also fix the the license to pass the cros_license_check.
    
    BUG=b:239434993
    TEST=Run CrasPlay and CrasRecord on octopus
    
    Change-Id: I281011858a61ff5866eecf29018f8578d76af44f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3895459
    Tested-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Reviewed-by: Avery Musbach <amusbach@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/audio/cras_play.go
+++ src/chromiumos/tast/local/bundles/cros/audio/cras_play.go
@@ -19,13 +19,20 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:     CrasPlay,
-		Desc:     "Verifies CRAS playback function works correctly",
-		Contacts: []string{"yuhsuan@chromium.org", "cychiang@chromium.org"},
-		// TODO(b/240269271): remove "octopus" and "hatch" when b/240269271 is fixed.
-		// TODO(b/240271671): remove "nocturne" when b/240271671 is fixed.
-		HardwareDeps: hwdep.D(hwdep.Speaker(), hwdep.SkipOnModel("octopus", "hatch", "nocturne")),
+		Func:         CrasPlay,
+		Desc:         "Verifies CRAS playback function works correctly",
+		Contacts:     []string{"yuhsuan@chromium.org", "cychiang@chromium.org"},
+		HardwareDeps: hwdep.D(hwdep.Speaker()),
 		Attr:         []string{"group:mainline"},
+		Params: []testing.Param{{
+			// TODO(b/240269271): remove "octopus" and "hatch" when b/240269271 is fixed.
+			// TODO(b/240271671): remove "nocturne" when b/240271671 is fixed.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform("octopus", "hatch", "nocturne")),
+		}, {
+			Name:              "unstable",
+			ExtraHardwareDeps: hwdep.D(hwdep.Platform("octopus", "hatch", "nocturne")),
+			ExtraAttr:         []string{"informational"},
+		}},
 	})
 }
 
--- src/chromiumos/tast/local/bundles/cros/audio/cras_record.go
+++ src/chromiumos/tast/local/bundles/cros/audio/cras_record.go
@@ -19,13 +19,20 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:     CrasRecord,
-		Desc:     "Verifies CRAS record function works correctly",
-		Contacts: []string{"yuhsuan@chromium.org", "cychiang@chromium.org"},
-		// TODO(b/240269271): remove "octopus" and "hatch" when b/240269271 is fixed.
-		// TODO(b/240271671): remove "nocturne" when b/240271671 is fixed.
-		HardwareDeps: hwdep.D(hwdep.Microphone(), hwdep.SkipOnModel("octopus", "hatch", "nocturne")),
+		Func:         CrasRecord,
+		Desc:         "Verifies CRAS record function works correctly",
+		Contacts:     []string{"yuhsuan@chromium.org", "cychiang@chromium.org"},
+		HardwareDeps: hwdep.D(hwdep.Microphone()),
 		Attr:         []string{"group:mainline"},
+		Params: []testing.Param{{
+			// TODO(b/240269271): remove "octopus" and "hatch" when b/240269271 is fixed.
+			// TODO(b/240271671): remove "nocturne" when b/240271671 is fixed.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnPlatform("octopus", "hatch", "nocturne")),
+		}, {
+			Name:              "unstable",
+			ExtraHardwareDeps: hwdep.D(hwdep.Platform("octopus", "hatch", "nocturne")),
+			ExtraAttr:         []string{"informational"},
+		}},
 	})
 }
 
