846cd7badeff86c536e8280471357cac8b9c03d7
Navin Ramsaroop <ramsaroop@google.com>
2022-09-15 02:31:50 +0000

    Add screen record functionality to cujrecorder.Recorder
    Add the ScreenRecorder to the recorder so that developers can more
    easily share videos of what tests actually do. Now, you can run each
    CUJ test with the flag -var=record=true, and the test will record
    everything within cujrecorder.Recorder.Run. One exclusion is
    ui.QuickCheckCUJ, where in cujrecorder.Recorder.Run, we go to the lock
    screen. Entering the lock screen ends any screen recording in progress,
    so if we wanted to record that test, we would have to do additional
    changes not included in this CL.
    
    One issue that arose is that the current code for starting the screen
    recording expects a popup to show up, but frequently the popup doesn't
    show up, especially during secondary tests within a fixture. Therefore,
    I logged the error if the popup doesn't show up instead of failing the
    test.
    
    BUG=b:244628474
    TEST=tast run -var=record=true <DUT> ui.ArcYoutubeCUJ.vm
    ui.DesksCUJ ui.GoogleSheetsCUJ ui.GoogleSlidesCUJ ui.MeetCUJ.4p
    ui.MeetMultiTaskingCUJ ui.TabSwitchCUJ ui.TaskSwitchCUJ.vm
    ui.VideoCUJ.clamshell ui.WindowArrangementCUJ.clamshell_mode_vm
    ui.WindowStateTransitionsCUJ, and uploaded the videos to go/cuj-videos
    
    Change-Id: I86ed4a88306e126883a973e520ff789f4d21dc21

--- src/chromiumos/tast/local/bundles/cros/ui/taskswitchcuj/task_switch_cuj.go
+++ src/chromiumos/tast/local/bundles/cros/ui/taskswitchcuj/task_switch_cuj.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The ChromiumOS Authors.
+// Copyright 2022 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,6 +7,8 @@ package taskswitchcuj
 
 import (
 	"context"
+	"fmt"
+	"path/filepath"
 	"time"
 
 	"chromiumos/tast/common/action"
@@ -106,6 +108,12 @@ func Run(ctx context.Context, s *testing.State) {
 	}
 	recorder.EnableTracing(s.OutDir(), s.DataPath(cujrecorder.SystemTraceConfigFile))
 	defer recorder.Close(closeCtx)
+
+	if _, ok := s.Var("record"); ok {
+		if err := recorder.AddScreenRecorder(ctx, tconn, filepath.Join(s.OutDir(), fmt.Sprintf("%s-record.webm", s.TestName()))); err != nil {
+			s.Fatal("Failed to add screen recorder: ", err)
+		}
+	}
 
 	topRow, err := input.KeyboardTopRowLayout(ctx, kw)
 	if err != nil {
