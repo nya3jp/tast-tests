25922ab372d9bb0b52942d5ee75645c454ab09b1
Gabor Zsolt Magda <gabormagda@google.com>
2022-09-15 17:46:13 +0200

    policy: Fix lock screen PIN related tests
    These tests were broken due to a UI change.
    
    BUG=b:240635043
    TEST=tast run <DUT> policy.PinUnlock* \
         policy.QuickUnlockModeAllowlist policy.WebauthnFactors
    
    Change-Id: I3f2ed2e16f37d25d81b1d4afda8d1c8462e5b284

--- src/chromiumos/tast/local/bundles/cros/policy/pin_unlock_maximum_length.go
+++ src/chromiumos/tast/local/bundles/cros/policy/pin_unlock_maximum_length.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Copyright 2021 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -124,9 +124,9 @@ func PinUnlockMaximumLength(ctx context.Context, s *testing.State) {
 				s.Fatal("Failed to type password: ", err)
 			}
 
-			if err := uiauto.Combine("switch to PIN or password and wait for PIN dialog",
-				// Find and click on radio button "PIN or password".
-				ui.LeftClick(nodewith.Name("PIN or password").Role(role.RadioButton)),
+			if err := uiauto.Combine("switch to PIN and wait for PIN dialog",
+				// Find and click on radio button "PIN".
+				ui.LeftClick(nodewith.Name("PIN").Role(role.RadioButton)),
 				// Find and click on "Set up PIN" button.
 				ui.LeftClick(nodewith.Name("Set up PIN").Role(role.Button)),
 				// Wait for the PIN pop up window to appear.
