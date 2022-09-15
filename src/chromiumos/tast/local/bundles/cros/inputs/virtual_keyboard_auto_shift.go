ace1e228acf64ea3ed45d2ecf900374d460ebe86
John Palmer <jopalmer@chromium.org>
2022-09-15 12:04:00 +1000

    Unblock CQ by restoring state in auto shift state.
    Auto shift test can leave VK in shift locked state which breaks
    subsequent tests and is blocking uprev. Fix by unshifting manually at
    the end of the test.
    
    BUG=None
    TEST=CQ
    
    Change-Id: Ib7a109b79f2cbb823bbd88f7d76330da4340eb8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3898152
    Commit-Queue: Alvin Jia <alvinjia@google.com>
    Reviewed-by: Alvin Jia <alvinjia@google.com>
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Tested-by: John Palmer <jopalmer@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_auto_shift.go
+++ src/chromiumos/tast/local/bundles/cros/inputs/virtual_keyboard_auto_shift.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Copyright 2021 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -370,6 +370,8 @@ func VirtualKeyboardAutoShift(ctx context.Context, s *testing.State) {
 				vkbCtx.WaitUntilShiftStatus(vkb.ShiftStateLocked),
 				vkbCtx.TapKeys(strings.Split("ELLO", "")),
 				util.WaitForFieldTextToBe(tconn, inputField.Finder(), "hELLO"),
+				manualShift,
+				vkbCtx.WaitUntilShiftStatus(vkb.ShiftStateNone),
 			),
 			uc,
 			&useractions.UserActionCfg{
