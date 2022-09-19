9110ce23d331e5b20cb5de855521a1cf0f2ea8a6
pjlee <pjlee@google.com>
2022-09-19 20:46:00 +0000

    arc/gio: Add input overlay performance tast test
    Add a tast test to measure input latency for tap actions in gaming input
    overlay.
    
    BUG=b:247606326
    TEST=./fast_build.sh -T && tast run $DUT arc.InputOverlayPerf*
    
    Change-Id: Ice21b288286ae80b49129e2d353429715540d97c

--- android/ArcInputOverlayTest/src/ReceivedEvent.java
+++ android/ArcInputOverlayTest/src/ReceivedEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Copyright 2022 The ChromiumOS Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -26,6 +26,10 @@ class ReceivedEvent {
     code = codeToString(event);
     action = actionToString(event);
     actionButton = actionButtonToString(event);
+  }
+
+  public String toString() {
+    return action + " " + String.valueOf(receiveTimeNs);
   }
 
    private String sourceToString(InputEvent event) {
