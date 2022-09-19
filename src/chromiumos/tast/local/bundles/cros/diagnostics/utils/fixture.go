be79b29af2151cad289f008563978834ff7e5e37
David Padlipsky <dpad@google.com>
2022-09-19 23:17:08 +0000

    Diagnostics: Update input diagnostics tast tests to use fixture
    - Adds test fixture for input diagnostics
    - fixture.go moved to utils package to allow use of types declared
      within fixture.go in the tests
    
    BUG=b:207089601
    TEST=tast run $DUT diagnostics.Input*
    
    Change-Id: I74d2794344edadcf5c5a96cf9fd92b01f7b564cf

--- src/chromiumos/tast/local/bundles/cros/diagnostics/utils/fixture.go
+++ src/chromiumos/tast/local/bundles/cros/diagnostics/utils/fixture.go
@@ -2,14 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package diagnostics
+package utils
 
 import (
 	"context"
 	"time"
 
 	"chromiumos/tast/errors"
-	"chromiumos/tast/local/bundles/cros/diagnostics/utils"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto/diagnosticsapp"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
@@ -25,7 +24,28 @@ func init() {
 			"ashleydp@google.com",         // Fixture maintainer
 			"cros-peripherals@google.com", // team mailing list
 		},
-		Impl:            newDiagnosticsPrepFixture(),
+		Impl: newDiagnosticsPrepFixture(func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return nil, nil
+		}),
+		SetUpTimeout:    chrome.LoginTimeout + 15*time.Second,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+		PreTestTimeout:  15 * time.Second,
+		PostTestTimeout: 5 * time.Second,
+	})
+
+	testing.AddFixture(&testing.Fixture{
+		Name: "diagnosticsPrepForInputDiagnostics",
+		Desc: "Ensure relevant service is running before diagnostics ui test",
+		Contacts: []string{
+			"zhangwenyu@google.com",       // Fixture maintainer
+			"ashleydp@google.com",         // Fixture maintainer
+			"dpad@google.com",             // Fixture maintainer
+			"cros-peripherals@google.com", // team mailing list
+		},
+		Impl: newDiagnosticsPrepFixture(func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return []chrome.Option{chrome.EnableFeatures("EnableInputInDiagnosticsApp")}, nil
+		}),
 		SetUpTimeout:    chrome.LoginTimeout + 15*time.Second,
 		ResetTimeout:    chrome.ResetTimeout,
 		TearDownTimeout: chrome.ResetTimeout,
@@ -36,22 +56,34 @@ func init() {
 
 const appURL = "chrome://diagnostics/"
 
+// FixtureData contains the data available for use in diagnostics tests
+type FixtureData struct {
+	Cr    *chrome.Chrome
+	Tconn *chrome.TestConn
+}
+
 // diagnosticsPrepFixture is a fixture to ensure relevant service is running
 // before diagnostics ui test.
 type diagnosticsPrepFixture struct {
 	cr    *chrome.Chrome
-	api   *utils.MojoAPI
+	api   *MojoAPI
 	tconn *chrome.TestConn
+	fopt  chrome.OptionsCallback
 }
 
-func newDiagnosticsPrepFixture() testing.FixtureImpl {
-	return &diagnosticsPrepFixture{}
+func newDiagnosticsPrepFixture(fopt chrome.OptionsCallback) testing.FixtureImpl {
+	return &diagnosticsPrepFixture{fopt: fopt}
 }
 
 func (f *diagnosticsPrepFixture) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
 	success := false
 
-	cr, err := chrome.New(ctx)
+	opts, err := f.fopt(ctx, s)
+	if err != nil {
+		s.Fatal("Failed to obtain Chrome options: ", err)
+	}
+
+	cr, err := chrome.New(ctx, opts...)
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
@@ -61,7 +93,7 @@ func (f *diagnosticsPrepFixture) SetUp(ctx context.Context, s *testing.FixtState
 		}
 	}()
 
-	if err := utils.EnsureCrosHealthdRunning(ctx); err != nil {
+	if err := EnsureCrosHealthdRunning(ctx); err != nil {
 		s.Fatal("Failed to ensure cros healthd running: ", err)
 	}
 
@@ -74,7 +106,7 @@ func (f *diagnosticsPrepFixture) SetUp(ctx context.Context, s *testing.FixtState
 	f.cr = cr
 	f.tconn = tconn
 
-	return tconn
+	return &FixtureData{Cr: cr, Tconn: tconn}
 }
 
 func (f *diagnosticsPrepFixture) TearDown(ctx context.Context, s *testing.FixtState) {
@@ -103,9 +135,9 @@ func (f *diagnosticsPrepFixture) PreTest(ctx context.Context, s *testing.FixtTes
 	}
 
 	// Make sure mojo API is connected.
-	var api *utils.MojoAPI
+	var api *MojoAPI
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		if api, err = utils.SystemDataProviderMojoAPI(ctx, conn); err != nil {
+		if api, err = SystemDataProviderMojoAPI(ctx, conn); err != nil {
 			return errors.Wrap(err, "unable to get systemDataProvider mojo API")
 		}
 
