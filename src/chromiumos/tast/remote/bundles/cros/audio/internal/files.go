b8257d50879713a9feb8b0bab87775aa960abf05
Li-Yu Yu <aaronyu@google.com>
2022-09-19 19:22:41 +0800

    audio.FileMigration: Add test to check migration of /var/lib/cras/stop
    Pre M96, /var/lib/cras/stop was created with root:root.
    Now it should be in cras:cras.
    Add test to check that the ownership is migrated.
    
    Also, sound_card_init.go is formatted to make preupload happy.
    
    BUG=b:246970233
    TEST=tast run $dut audio.FileOwnershipMigration
    
    Cq-Depend: chromium:3901955
    Change-Id: Ibab03896be3b19ea21b1c7b00ea7de6f820b5805

---
+++ src/chromiumos/tast/remote/bundles/cros/audio/internal/files.go
@@ -0,0 +1,10 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package internal
+
+const (
+	// CrasStopTimeFile is the file stores previous CRAS stop time.
+	CrasStopTimeFile = "/var/lib/cras/stop"
+)
