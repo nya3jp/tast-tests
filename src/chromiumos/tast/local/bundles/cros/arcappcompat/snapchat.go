1943d4db6033deeb2abb8f34f879444b814ea41e
Meenakshi Thiyagarajan <mthiyagarajan@google.com>
2022-09-19 10:52:20 -0700

    arcappcompat: add 4 more apps to “appcompat_release” suite
    Added Facebook, Instagram, Snapchat, YouTube Kids to “appcompat_release” suite
    
    BUG=b:246976583
    TEST=test_that --board=eve <ip-address> tast.appcompat_release-shard-0
    test_that --board=eve <ip-address> tast.appcompat_release-shard-1
    test_that --board=eve <ip-address> tast.appcompat_release-shard-2
    test_that --board=eve <ip-address> tast.appcompat_release-shard-3
    Cq-Depend: chromium:3905350
    
    Change-Id: Idc225e46e18c598858e46b5b18638fb48c205f70

--- src/chromiumos/tast/local/bundles/cros/arcappcompat/snapchat.go
+++ src/chromiumos/tast/local/bundles/cros/arcappcompat/snapchat.go
@@ -88,6 +88,54 @@ func init() {
 			// Skip on clamshell only models.
 			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
 			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "clamshell_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  clamshellLaunchForSnapchat,
+				ReleaseTests: testutil.ClamshellReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "tablet_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  touchviewLaunchForSnapchat,
+				ReleaseTests: testutil.TouchviewReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_p"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
+		}, {
+			Name: "vm_clamshell_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  clamshellLaunchForSnapchat,
+				ReleaseTests: testutil.ClamshellReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on tablet only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(testutil.TabletOnlyModels...)),
+			Pre:               pre.AppCompatBootedUsingTestAccountPool,
+		}, {
+			Name: "vm_tablet_mode_release",
+			Val: testutil.TestParams{
+				LaunchTests:  touchviewLaunchForSnapchat,
+				ReleaseTests: testutil.TouchviewReleaseTests,
+			},
+			ExtraAttr:         []string{"appcompat_release"},
+			ExtraSoftwareDeps: []string{"android_vm"},
+			// TODO(b/189704585): Remove hwdep.SkipOnModel once the solution is found.
+			// Skip on clamshell only models.
+			ExtraHardwareDeps: hwdep.D(hwdep.TouchScreen(), hwdep.SkipOnModel(testutil.ClamshellOnlyModels...)),
+			Pre:               pre.AppCompatBootedInTabletModeUsingTestAccountPool,
 		}},
 		Timeout: 10 * time.Minute,
 		Vars:    []string{"arcappcompat.gaiaPoolDefault"},
