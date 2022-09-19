273922d10a436d64adf085e5931ebd126478c148
Anastasiia N <anastasiian@chromium.org>
2022-09-19 18:17:19 +0000

    accountmanager: Improve test cleanup
    Wait for account list to appear for 5 seconds before returning. This
    will make sure that we actually delete the accounts in cleanup.
    
    BUG=b:243452146
    TEST=tast run <ip> accountmanager*
    
    Change-Id: Ie80f8dc209bac652db5db8c870a7ae223011ae2e

--- src/chromiumos/tast/local/accountmanager/account_manager.go
+++ src/chromiumos/tast/local/accountmanager/account_manager.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Copyright 2021 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,6 +7,7 @@ package accountmanager
 import (
 	"context"
 	"regexp"
+	"strings"
 	"time"
 
 	"chromiumos/tast/common/action"
@@ -426,13 +427,14 @@ func TestCleanup(ctx context.Context, tconn *chrome.TestConn, cr *chrome.Chrome)
 			return errors.Wrap(err, "failed to launch Account Manager page")
 		}
 
-		moreActionsFound, err := ui.IsNodeFound(ctx, moreActionsButton)
-		if err != nil {
-			return errors.Wrap(err, "failed to search for More actions button")
-		}
-		if !moreActionsFound {
-			// There are no "More actions, *" buttons left. It means all secondary accounts are removed.
-			break
+		// Wait for 5 seconds for the account list to appear.
+		if err := ui.WithTimeout(5 * time.Second).WaitUntilExists(moreActionsButton)(ctx); err != nil {
+			if strings.Contains(err.Error(), nodewith.ErrNotFound) && strings.Contains(err.Error(), context.DeadlineExceeded.Error()) {
+				// There are no "More actions, *" buttons left. It means all secondary accounts are removed.
+				break
+			}
+
+			return errors.Wrap(err, "failed to wait for More actions buttons")
 		}
 
 		// Select specific "More actions, <email>" button.
