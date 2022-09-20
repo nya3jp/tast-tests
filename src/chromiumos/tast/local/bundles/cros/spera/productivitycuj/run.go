affd639e4d9b640e451d3df0600346e44525d519
Alston Huang <alston.huang@cienet.com>
2022-09-20 11:41:50 +0800

    productivity cuj: Fix lacros metrics not being collected correctly
    BUG=b:247654467
    TEST=tast run <vars> <dut> ui.MicrosoftOfficeWebCUJ.*
    TEST=tast run <vars> <dut> ui.GoogleDocsWebCUJ.*
    TEST=tast run <vars> <dut> spera.MicrosoftOfficeWebCUJ.*
    TEST=tast run <vars> <dut> spera.GoogleDocsWebCUJ.*
    
    Change-Id: I16507ea467c498f2a831246e7c028043663be10d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3904835
    Reviewed-by: Jun Sheng <shengjun@chromium.org>
    Commit-Queue: Alston Huang <alston.huang@cienet.com>
    Tested-by: Alston Huang <alston.huang@cienet.com>

--- src/chromiumos/tast/local/bundles/cros/spera/productivitycuj/run.go
+++ src/chromiumos/tast/local/bundles/cros/spera/productivitycuj/run.go
@@ -36,15 +36,16 @@ func Run(ctx context.Context, cr *chrome.Chrome, app ProductivityApp, tier cuj.T
 		return errors.Wrap(err, "failed to get browser start time")
 	}
 	br := cr.Browser()
+	var bTconn *chrome.TestConn
 	if l != nil {
 		defer l.Close(ctx)
+		bTconn, err = l.TestAPIConn(ctx)
+		if err != nil {
+			return errors.Wrap(err, "failed to get lacros test API conn")
+		}
 		br = l.Browser()
 	}
 	app.SetBrowser(br)
-	bTconn, err := l.TestAPIConn(ctx)
-	if err != nil {
-		return errors.Wrap(err, "failed to get browser TestAPIConn")
-	}
 
 	// Give 10 seconds to set initial settings. It is critical to ensure
 	// cleanupSetting can be executed with a valid context so it has its
@@ -72,7 +73,7 @@ func Run(ctx context.Context, cr *chrome.Chrome, app ProductivityApp, tier cuj.T
 		return errors.Wrap(err, "failed to create the recorder")
 	}
 	defer recorder.Close(cleanUpRecorderCtx)
-	if err := cuj.AddPerformanceCUJMetrics(tconn, nil, recorder); err != nil {
+	if err := cuj.AddPerformanceCUJMetrics(tconn, bTconn, recorder); err != nil {
 		return errors.Wrap(err, "failed to add metrics to recorder")
 	}
 
