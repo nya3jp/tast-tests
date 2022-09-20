62e0529862a81f7567319cbb2b2bfeaa8e85de25
Shengsong Tan <sstan@google.com>
2022-09-20 11:32:46 +0900

    arc: Add perfetto trace-based metrics
    This CL add wrap API for run perfetto internal metrics by
    trace_processor.
    
    The metrics defined in
    Android AOSP: external/perfetto/protos/perfetto/metrics/
    
    BUG=b:245042021
    TEST=manually test by DEMO cl https://crrev.com/c/3881652/
    
    Change-Id: I2b4d2abc18081a31def4c76ea94df1fc79d7c02f

--- src/chromiumos/tast/local/arc/perfetto/perfetto.go
+++ src/chromiumos/tast/local/arc/perfetto/perfetto.go
@@ -9,10 +9,66 @@ import (
 	"bytes"
 	"context"
 	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/arc"
+)
+
+const traceProcessorURL = "https://get.perfetto.dev/trace_processor"
+
+var (
+	tmpDir             = filepath.Join(os.TempDir(), "arc_perfetto")
+	traceProcessorPath = filepath.Join(tmpDir, "trace_processor")
+)
+
+// Metrics defined in external/perfetto/protos/perfetto/metrics/metrics.proto
+// of Android repo.
+const (
+	AndroidBatteryMetric            = "android_batt"
+	AndroidCPUMetric                = "android_cpu"
+	AndroidMemoryMetric             = "android_mem"
+	AndroidMemoryUnaggregatedMetric = "android_mem_unagg"
+	AndroidPackageList              = "android_package_list"
+	AndroidIonMetric                = "android_ion"
+	AndroidFastrpcMetric            = "android_fastrpc"
+	AndroidLmkMetric                = "android_lmk"
+	AndroidPowerRails               = "android_powrails"
+	AndroidStartupMetric            = "android_startup"
+	TraceMetadata                   = "trace_metadata"
+	TraceAnalysisStats              = "trace_stats"
+	UnsymbolizedFrames              = "unsymbolized_frames"
+	JavaHeapStats                   = "java_heap_stats"
+	JavaHeapHistogram               = "java_heap_histogram"
+	AndroidLmkReasonMetric          = "android_lmk_reason"
+	AndroidHwuiMetric               = "android_hwui_metric"
+	AndroidDisplayMetrics           = "display_metrics"
+	AndroidTaskNames                = "android_task_names"
+	AndroidSurfaceflingerMetric     = "android_surfaceflinger"
+	AndroidGpuMetric                = "android_gpu"
+	AndroidSysUICujMetrics          = "android_sysui_cuj"
+	AndroidJankCujMetric            = "android_jank_cuj"
+	AndroidHwcomposerMetrics        = "android_hwcomposer"
+	G2dMetrics                      = "g2d"
+	AndroidDmaHeapMetric            = "android_dma_heap"
+	AndroidTraceQualityMetric       = "android_trace_quality"
+	ProfilerSmaps                   = "profiler_smaps"
+	AndroidMultiuserMetric          = "android_multiuser"
+	AndroidSimpleperfMetric         = "android_simpleperf"
+	AndroidCameraMetric             = "android_camera"
+	// AndroidDvfsMetric is the metrics for dynamic voltage and frequency scaling.
+	AndroidDvfsMetric               = "android_dvfs"
+	AndroidNetworkMetric            = "android_netperf"
+	AndroidCameraUnaggregatedMetric = "android_camera_unagg"
+	AndroidRtRuntimeMetric          = "android_rt_runtime"
+	AndroidIrqRuntimeMetric         = "android_irq_runtime"
+	AndroidTrustyWorkqueues         = "android_trusty_workqueues"
+	AndroidOtherTracesMetric        = "android_other_traces"
+	AndroidBinderMetric             = "android_binder"
+	AndroidFrameTimelineMetric      = "android_frame_timeline_metric"
 )
 
 // ForceEnableTrace will overwrite tracing_on flag in kernel tracefs. In some cases this flag
@@ -75,5 +131,45 @@ func Trace(ctx context.Context, a *arc.ARC, traceConfigPath, traceResultPath str
 		return errors.Wrap(ferr, "finish trace but errors happen on test func")
 	}
 
+	return nil
+}
+
+// Metrics use the higher-level query interface that run pre-baked queries called metrics.
+// They defined in Android-AOSP path: external/perfetto/protos/perfetto/metrics/android/
+// It also support customized metrics, see https://perfetto.dev/docs/analysis/metrics
+func Metrics(ctx context.Context, traceResultPath string, metrics ...string) ([]byte, error) {
+	if err := ensureTraceProcessorExistence(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to initialize trace processor")
+	}
+
+	// Explicit use python since the tmp filesystem may be mounted with "noexec" option.
+	return testexec.CommandContext(ctx, "python", traceProcessorPath, "--run-metrics", strings.Join(metrics, ","), traceResultPath).Output(testexec.DumpLogOnError)
+}
+
+func ensureTempDir() error {
+	if err := os.MkdirAll(tmpDir, 0700); err != nil {
+		return errors.Wrap(err, "failed to create temp dir for perfetto")
+	}
+	return nil
+}
+
+func ensureTraceProcessorExistence(ctx context.Context) error {
+	if err := ensureTempDir(); err != nil {
+		// Returns err without wrap since it just try to create dir.
+		return err
+	}
+	if _, err := os.Stat(traceProcessorPath); err == nil {
+		// If trace processor already exist on DUT, just return.
+		return nil
+	} else if os.IsNotExist(err) {
+		if err := testexec.CommandContext(ctx, "curl", "-o", traceProcessorPath, "-L", traceProcessorURL).Run(testexec.DumpLogOnError); err != nil {
+			return errors.Wrap(err, "failed to download trace processor")
+		}
+		if err := testexec.CommandContext(ctx, "chmod", "+x", traceProcessorPath).Run(testexec.DumpLogOnError); err != nil {
+			return errors.Wrap(err, "failed to chmod for trace processor")
+		}
+	} else {
+		return errors.Wrap(err, "failed to ensure trace processer existence")
+	}
 	return nil
 }
