bea8f0581c1e908537a1c53fa4ca463660a93771
Avery Musbach <amusbach@chromium.org>
2022-09-20 15:34:13 -0700

    ui: Use minimum zoom in OverviewDragWindowPerf
    The tests require that browser windows can be snapped in split view.
    This requires that the work area length is at least twice the minimum
    length of a browser window. To ensure this, the tests will set the
    display zoom factor to minimum.
    
    BUG=chromium:1364955
    TEST=tast run <DUT> ui.OverviewDragWindowPerf.*
    
    Change-Id: I01c05d7f1ed96a9ce8209152d5dd20675ee05dc0

--- src/chromiumos/tast/local/bundles/cros/ui/overview_drag_window_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/overview_drag_window_perf.go
@@ -7,6 +7,7 @@ package ui
 import (
 	"context"
 	"fmt"
+	"sort"
 	"time"
 
 	"chromiumos/tast/common/perf"
@@ -342,6 +343,26 @@ func OverviewDragWindowPerf(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to ensure in tablet mode: ", err)
 	}
 	defer cleanup(cleanupCtx)
+
+	// Ensures that the display zoom factor is 90% (allowing for rounding error) or
+	// less, to ensure that the work area length is at least twice the minimum length
+	// of a browser window, so that browser windows can be snapped in split view.
+	const zoomMaximum = 0.90000005
+	info, err := display.GetPrimaryInfo(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to get the primary display info: ", err)
+	}
+	if zoomInitial := info.DisplayZoomFactor; zoomInitial > zoomMaximum {
+		i := sort.Search(len(info.AvailableDisplayZoomFactors), func(i int) bool { return info.AvailableDisplayZoomFactors[i] > zoomMaximum })
+		if i == 0 {
+			s.Fatalf("Lowest available display zoom factor is %f; want 90%% (allowing for rounding error) or less", info.AvailableDisplayZoomFactors[0])
+		}
+		zoomForTest := info.AvailableDisplayZoomFactors[i-1]
+		if err := display.SetDisplayProperties(ctx, tconn, info.ID, display.DisplayProperties{DisplayZoomFactor: &zoomForTest}); err != nil {
+			s.Fatalf("Failed to set display zoom factor to %f: %v", zoomForTest, err)
+		}
+		defer display.SetDisplayProperties(cleanupCtx, tconn, info.ID, display.DisplayProperties{DisplayZoomFactor: &zoomInitial})
+	}
 
 	tsw, err := input.Touchscreen(ctx)
 	if err != nil {
