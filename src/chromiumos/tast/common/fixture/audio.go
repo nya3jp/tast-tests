fcb7922f26735b5b67aaf3ac8cc62699636dbef5
Li-Yu Yu <aaronyu@google.com>
2022-09-20 13:58:11 +0800

    Move crasStopped fixture name to fixture package
    BUG=None
    TEST=~/chromiumos/src/platform/tast/fast_build.sh -T
    
    Change-Id: If9f154dea82bdf58ffe65bdf2eed3ff2e0bd7cf1

---
+++ src/chromiumos/tast/common/fixture/audio.go
@@ -0,0 +1,10 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fixture
+
+// Fixtures defined in chromiumos/tast/local/audio/fixture.go
+const (
+	CrasStopped = "crasStopped"
+)
