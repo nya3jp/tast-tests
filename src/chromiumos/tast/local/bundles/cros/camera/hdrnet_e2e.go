c2dc7f307fdd1de6ee94283dc6a64d7b6071dc4f
Ren-Pei Zeng <kamesan@chromium.org>
2022-09-23 05:04:50 +0800

    camera: Move histogram-related utilities to common package
    BUG=None
    TEST=tast run $DUT camera.HDRnetE2E
    
    Change-Id: I4154d8c4eb15f1972f703db7b94c1f351075c64f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3914428
    Tested-by: Ren-Pei Zeng <kamesan@chromium.org>
    Reviewed-by: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Joseph Pang <pjlee@google.com>
    Commit-Queue: Ren-Pei Zeng <kamesan@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/camera/hdrnet_e2e.go
+++ src/chromiumos/tast/local/bundles/cros/camera/hdrnet_e2e.go
@@ -12,8 +12,8 @@ import (
 	"chromiumos/tast/common/media/caps"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/local/camera/cca"
+	"chromiumos/tast/local/camera/histogramutil"
 	"chromiumos/tast/local/chrome"
-	"chromiumos/tast/local/chrome/metrics"
 	"chromiumos/tast/testing"
 )
 
@@ -78,112 +78,6 @@ func HDRnetE2E(ctx context.Context, s *testing.State) {
 	}
 }
 
-type histogramVerifier func(m *metrics.Histogram) error
-type histogramTests map[string]histogramVerifier
-
-// names returns the names of the histograms tracked by |ht| as string slice.
-func (ht histogramTests) names() []string {
-	names := make([]string, len(ht))
-	i := 0
-	for v := range ht {
-		names[i] = v
-		i++
-	}
-	return names
-}
-
-// record starts recording changes of the histograms tracked by |ht|.
-func (ht histogramTests) record(ctx context.Context, tconn *chrome.TestConn) (*metrics.Recorder, error) {
-	metrics.ClearHistogramTransferFile()
-	return metrics.StartRecorder(ctx, tconn, ht.names()...)
-}
-
-// wait waits until the histograms tracked by |ht| have new values.
-func (ht histogramTests) wait(ctx context.Context, tconn *chrome.TestConn, recorder *metrics.Recorder) ([]*metrics.Histogram, error) {
-	// It takes time for Chrome to refresh the histograms. Leave 3 seconds
-	// for the clean-up tasks in case of error.
-	ctxDeadline, _ := ctx.Deadline()
-	histTimeout := ctxDeadline.Sub(time.Now()) - 3*time.Second
-	return recorder.WaitAll(ctx, tconn, histTimeout)
-}
-
-// verify waits until the histograms tracked by |ht| are ready and verifies the
-// histogram values recorded using the HistogramVerifier associated with each
-// histogram.
-func (ht histogramTests) verify(ctx context.Context, tconn *chrome.TestConn, recorder *metrics.Recorder) error {
-	histograms, err := ht.wait(ctx, tconn, recorder)
-	if err != nil {
-		return errors.Wrap(err, "failed to get updated histograms")
-	}
-
-	for _, h := range histograms {
-		if err := ht[h.Name](h); err != nil {
-			// Dump the whole histogram diff so that we get a better
-			// idea of what else also failed.
-			testing.ContextLog(ctx, "Histogram diff: ", histograms)
-			return err
-		}
-	}
-
-	return nil
-}
-
-// assertHistogramEq returns a HistogramVerifier that can be used to check if a
-// histogram's value equals to |value|.
-func assertHistogramEq(value float64) histogramVerifier {
-	return func(m *metrics.Histogram) error {
-		// We assume that there's only one sample hence we can check
-		// against the histogram mean.
-		if len(m.Buckets) != 1 {
-			return errors.Errorf("invalid %s: %v", m.Name, m.Buckets)
-		}
-		if mean, err := m.Mean(); err != nil {
-			return errors.Wrap(err, "failed to get histogram mean")
-		} else if mean != value {
-			return errors.Errorf("unexpected value of %s: %v does not equal to %v", m.Name, mean, value)
-		}
-		return nil
-	}
-}
-
-// assertHistogramIn returns a HistogramVerifier that can be used to check if a
-// histogram's value is in |values|.
-func assertHistogramIn(values ...float64) histogramVerifier {
-	return func(m *metrics.Histogram) error {
-		// We assume that there's only one sample hence we can check
-		// against the histogram mean.
-		if len(m.Buckets) != 1 {
-			return errors.Errorf("invalid %s: %v", m.Name, m.Buckets)
-		}
-		mean, err := m.Mean()
-		if err != nil {
-			return errors.Wrap(err, "failed to get histogram mean")
-		}
-		for _, v := range values {
-			if v == mean {
-				return nil
-			}
-		}
-		return errors.Errorf("unexpected value of %s: %v is not one of %v", m.Name, mean, values)
-	}
-}
-
-// assertHistogramMeanGt returns a HistogramVerifier that can be used to check
-// if a histogram's mean value is greater than |value|.
-func assertHistogramMeanGt(value float64) histogramVerifier {
-	return func(m *metrics.Histogram) error {
-		if len(m.Buckets) == 0 {
-			return errors.Errorf("invalid %s: %v", m.Name, m.Buckets)
-		}
-		if mean, err := m.Mean(); err != nil {
-			return errors.Wrap(err, "failed to get histogram mean")
-		} else if mean <= value {
-			return errors.Errorf("unexpected mean of %s: %v is not greater than %v", m.Name, mean, value)
-		}
-		return nil
-	}
-}
-
 // testPhotoTaking tests if the HDRnet pipeline works as expected by taking a
 // photo in Chrome Camera App and check the histogram values.
 func testPhotoTaking(ctx context.Context, app *cca.App, tconn *chrome.TestConn) error {
@@ -199,18 +93,18 @@ func testPhotoTaking(ctx context.Context, app *cca.App, tconn *chrome.TestConn) 
 	const multipleYUVWithBLOB = 3.0
 	const multipleYUVOfDifferentAspectRatioWithBLOB = 5.0
 
-	histogramTests := histogramTests{
-		"ChromeOS.Camera.HDRnet.AverageLatency.Preprocessing":  assertHistogramMeanGt(minProcessingLatency),
-		"ChromeOS.Camera.HDRnet.AverageLatency.RgbPipeline":    assertHistogramMeanGt(minProcessingLatency),
-		"ChromeOS.Camera.HDRnet.AverageLatency.Postprocessing": assertHistogramMeanGt(minProcessingLatency),
-		"ChromeOS.Camera.HDRnet.Error":                         assertHistogramEq(hdrnetNoError),
-		"ChromeOS.Camera.HDRnet.NumStillShotsTaken":            assertHistogramEq(expectedStillShotsTaken),
-		"ChromeOS.Camera.HDRnet.StreamConfiguration":           assertHistogramIn(singleYUVWithBLOB, multipleYUVWithBLOB, multipleYUVOfDifferentAspectRatioWithBLOB),
+	histogramTests := histogramutil.HistogramTests{
+		"ChromeOS.Camera.HDRnet.AverageLatency.Preprocessing":  histogramutil.AssertHistogramMeanGt(minProcessingLatency),
+		"ChromeOS.Camera.HDRnet.AverageLatency.RgbPipeline":    histogramutil.AssertHistogramMeanGt(minProcessingLatency),
+		"ChromeOS.Camera.HDRnet.AverageLatency.Postprocessing": histogramutil.AssertHistogramMeanGt(minProcessingLatency),
+		"ChromeOS.Camera.HDRnet.Error":                         histogramutil.AssertHistogramEq(hdrnetNoError),
+		"ChromeOS.Camera.HDRnet.NumStillShotsTaken":            histogramutil.AssertHistogramEq(expectedStillShotsTaken),
+		"ChromeOS.Camera.HDRnet.StreamConfiguration":           histogramutil.AssertHistogramIn(singleYUVWithBLOB, multipleYUVWithBLOB, multipleYUVOfDifferentAspectRatioWithBLOB),
 	}
 
 	// Open CCA, take a picture and then close CCA. The histograms are
 	// uploaded only when the camera device is closed.
-	recorder, err := histogramTests.record(ctx, tconn)
+	recorder, err := histogramTests.Record(ctx, tconn)
 	if err != nil {
 		return errors.Wrap(err, "failed to start histogram recorder")
 	}
@@ -223,7 +117,7 @@ func testPhotoTaking(ctx context.Context, app *cca.App, tconn *chrome.TestConn) 
 	if err = app.Close(ctx); err != nil {
 		return errors.Wrap(err, "failed to close Chrome Camera App")
 	}
-	return histogramTests.verify(ctx, tconn, recorder)
+	return histogramTests.Verify(ctx, tconn, recorder)
 }
 
 // testVideoRecording tests if the HDRnet pipeline works as expected by
@@ -241,13 +135,13 @@ func testVideoRecording(ctx context.Context, app *cca.App, tconn *chrome.TestCon
 	const multipleYUVWithBLOB = 3.0
 	const multipleYUVOfDifferentAspectRatioWithBLOB = 5.0
 
-	histogramTests := histogramTests{
-		"ChromeOS.Camera.HDRnet.AverageLatency.Preprocessing":  assertHistogramMeanGt(minProcessingLatency),
-		"ChromeOS.Camera.HDRnet.AverageLatency.RgbPipeline":    assertHistogramMeanGt(minProcessingLatency),
-		"ChromeOS.Camera.HDRnet.AverageLatency.Postprocessing": assertHistogramMeanGt(minProcessingLatency),
-		"ChromeOS.Camera.HDRnet.Error":                         assertHistogramEq(hdrnetNoError),
-		"ChromeOS.Camera.HDRnet.NumStillShotsTaken":            assertHistogramEq(expectedStillShotsTaken),
-		"ChromeOS.Camera.HDRnet.StreamConfiguration":           assertHistogramIn(singleYUVWithBLOB, multipleYUVWithBLOB, multipleYUVOfDifferentAspectRatioWithBLOB),
+	histogramTests := histogramutil.HistogramTests{
+		"ChromeOS.Camera.HDRnet.AverageLatency.Preprocessing":  histogramutil.AssertHistogramMeanGt(minProcessingLatency),
+		"ChromeOS.Camera.HDRnet.AverageLatency.RgbPipeline":    histogramutil.AssertHistogramMeanGt(minProcessingLatency),
+		"ChromeOS.Camera.HDRnet.AverageLatency.Postprocessing": histogramutil.AssertHistogramMeanGt(minProcessingLatency),
+		"ChromeOS.Camera.HDRnet.Error":                         histogramutil.AssertHistogramEq(hdrnetNoError),
+		"ChromeOS.Camera.HDRnet.NumStillShotsTaken":            histogramutil.AssertHistogramEq(expectedStillShotsTaken),
+		"ChromeOS.Camera.HDRnet.StreamConfiguration":           histogramutil.AssertHistogramIn(singleYUVWithBLOB, multipleYUVWithBLOB, multipleYUVOfDifferentAspectRatioWithBLOB),
 	}
 
 	// Open CCA, record a 5-second video and then close CCA. The histograms
@@ -257,7 +151,7 @@ func testVideoRecording(ctx context.Context, app *cca.App, tconn *chrome.TestCon
 	}
 	// Starts histogram recording again after switch to Video mode ignore
 	// the histograms generated in previous CCA modes.
-	recorder, err := histogramTests.record(ctx, tconn)
+	recorder, err := histogramTests.Record(ctx, tconn)
 	if err != nil {
 		return errors.Wrap(err, "failed to start histogram recorder")
 	}
@@ -267,5 +161,5 @@ func testVideoRecording(ctx context.Context, app *cca.App, tconn *chrome.TestCon
 	if err = app.Close(ctx); err != nil {
 		return errors.Wrap(err, "failed to close Chrome Camera App")
 	}
-	return histogramTests.verify(ctx, tconn, recorder)
+	return histogramTests.Verify(ctx, tconn, recorder)
 }
