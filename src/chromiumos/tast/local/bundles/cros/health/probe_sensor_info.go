313778794911ce3c88cdbfb6ec490add55634aa4
Byron Lee <byronlee@google.com>
2022-09-22 05:27:33 +0000

    health: Extend ProbeSensorInfo to validate sensor attributes
    BUG=b:243095984
    
    TEST=tast run $DUT_IP health.ProbeSensorInfo
    
    Change-Id: If412303737c3d3d25ce89d677cc734371c5dd81e

--- src/chromiumos/tast/local/bundles/cros/health/probe_sensor_info.go
+++ src/chromiumos/tast/local/bundles/cros/health/probe_sensor_info.go
@@ -7,8 +7,11 @@ package health
 import (
 	"context"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/google/go-cmp/cmp"
 
 	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
@@ -93,6 +96,120 @@ func validateLidAngle(ctx context.Context, info *sensorInfo) error {
 	return nil
 }
 
+// sensorType covert raw value to value of sensor type enum in Healthd.
+// Unsupported sensor type is acceptable now and return "". If we want to cover
+// all sensor types in the future, we should raise error here.
+func sensorType(rawType string) string {
+	if rawType == "ACCEL" {
+		return "Accel"
+	} else if rawType == "LIGHT" {
+		return "Light"
+	} else if rawType == "ANGLVEL" {
+		return "Gyro"
+	} else if rawType == "ANGL" {
+		return "Angle"
+	} else if rawType == "GRAVITY" {
+		return "Gravity"
+	} else {
+		return ""
+	}
+}
+
+// sensorLocation covert raw value to value of sensor location enum in Healthd.
+func sensorLocation(rawLoaction string) string {
+	if rawLoaction == "base" {
+		return "Base"
+	} else if rawLoaction == "lid" {
+		return "Lid"
+	} else if rawLoaction == "camera" {
+		return "Camera"
+	} else {
+		return "Unknown"
+	}
+}
+
+// parseQueryLogs parses the logs of iioservice_query for each single sensor.
+// Unsupported sensor types will be skipped and return nil.
+func parseQueryLogs(logs []string) (*sensorAttributes, error) {
+	ret := sensorAttributes{}
+	for _, line := range logs {
+		// Format: "... INFO iioservice_query: ... GetAttributesCallback(): ${ATTRIBUTE_NAME}: ${ATTRIBUTE_VALUE}\n"
+		tks := strings.Split(line, "GetAttributesCallback():")
+		if len(tks) != 2 {
+			return nil, errors.New("failed to parse sensor attributes")
+		}
+		tks = strings.Split(strings.TrimSpace(tks[1]), ":")
+		if len(tks) != 2 {
+			return nil, errors.New("failed to parse sensor attributes")
+		}
+
+		if tks[0] == "Device id" {
+			deviceID, err := strconv.ParseInt(strings.TrimSpace(tks[1]), 10, 32)
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to parse sensor device ID")
+			}
+			ret.DeviceID = int32(deviceID)
+		} else if tks[0] == "Type" {
+			sensorType := sensorType(strings.TrimSpace(tks[1]))
+			// Unsupported sensor type will be skipped.
+			if sensorType == "" {
+				return nil, nil
+			}
+			ret.Type = sensorType
+		} else if tks[0] == "name" {
+			name := strings.TrimSpace(tks[1])
+			if name != "" {
+				ret.Name = &name
+			}
+		} else if tks[0] == "location" {
+			ret.Location = sensorLocation(strings.TrimSpace(tks[1]))
+		}
+	}
+	return &ret, nil
+}
+
+// expectedSensorAttributes get expected sensorAttributes via iioservice_query.
+func expectedSensorAttributes(ctx context.Context) (*[]sensorAttributes, error) {
+	_, bStderr, err := testexec.CommandContext(ctx, "iioservice_query", "--device_type=0",
+		"--attributes=name location").SeparatedOutput(testexec.DumpLogOnError)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to run iioservice_query command")
+	}
+
+	// Each line is log of iioservice_query. For each sensor, there are four logs,
+	// ordered by device ID, type, name, and location.
+	const numAttribues = 4
+	lines := strings.Split(strings.TrimSpace(string(bStderr)), "\n")
+	if len(lines)%numAttribues != 0 {
+		return nil, errors.Wrap(err, "failed to parse iioservice_query output")
+	}
+
+	var ret []sensorAttributes
+	for i := 0; i < len(lines); i += numAttribues {
+		attr, err := parseQueryLogs(lines[i : i+numAttribues])
+		if err != nil {
+			return nil, err
+		}
+		if attr != nil {
+			ret = append(ret, *attr)
+		}
+	}
+	return &ret, nil
+}
+
+func validateSensorAttributes(ctx context.Context, g *[]sensorAttributes) error {
+	e, err := expectedSensorAttributes(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get expected sensor attributes")
+	}
+	sort.Slice(*e, func(i, j int) bool { return (*e)[i].DeviceID < (*e)[j].DeviceID })
+	sort.Slice(*g, func(i, j int) bool { return (*g)[i].DeviceID < (*g)[j].DeviceID })
+	if d := cmp.Diff(e, g); d != "" {
+		return errors.Wrapf(err, "failed to validate sensor attributes (-expected + got): %s", d)
+	}
+	return nil
+}
+
 func ProbeSensorInfo(ctx context.Context, s *testing.State) {
 	params := croshealthd.TelemParams{Category: croshealthd.TelemCategorySensor}
 	var info sensorInfo
@@ -100,7 +217,11 @@ func ProbeSensorInfo(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to get sensor telemetry info: ", err)
 	}
 
+	if err := validateSensorAttributes(ctx, &info.Sensors); err != nil {
+		s.Fatalf("Failed to validate sensor attributes, err [%v]", err)
+	}
+
 	if err := validateLidAngle(ctx, &info); err != nil {
-		s.Fatalf("Failed to validate sensor data, err [%v]", err)
+		s.Fatalf("Failed to validate lid angle, err [%v]", err)
 	}
 }
