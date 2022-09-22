623ad94f2e6135d269739eaa28a53d8865bdd17f
Yulun Wu <yulunwu@chromium.org>
2022-09-22 10:28:19 -0700

    [TAST] Deflake answer card tests.
    This CL aims to deflake answer card tests by breaking up each test case
    to be its own independent test.
    
    BUG=chromium:1364742
    
    TEST=tast run $DUT_IP launcher.AnswerCards*
    
    Change-Id: I0347958e33612aa2957b0832f89c58680eb5c4d4

--- src/chromiumos/tast/local/bundles/cros/launcher/answer_cards.go
+++ src/chromiumos/tast/local/bundles/cros/launcher/answer_cards.go
@@ -14,9 +14,20 @@ import (
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/launcher"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
+
+// searchTestCase describes modes in which the launcher UI can be shown, and by which launcher test should generally be parameterized.
+// It additionally provides a search query and the expected result.
+// Use a struct because it makes the individual test cases more readable.
+type searchTestCase struct {
+	TabletMode     bool
+	SearchKeyword  string
+	ExpectedResult *nodewith.Finder
+	Retries        int
+}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -28,17 +39,49 @@ func init() {
 			"yulunwu@chromium.org",
 			"cros-system-ui-eng@google.com",
 		},
+		Fixture:      "chromeLoggedIn",
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
-			Name:    "clamshell_mode",
-			Fixture: "chromeLoggedIn",
-			Val:     launcher.TestCase{TabletMode: false},
+			Name: "definition_card_clamshell",
+			Val: searchTestCase{TabletMode: false,
+				SearchKeyword:  "definition of flaky",
+				ExpectedResult: launcher.SearchResultListItemFinder.NameRegex(regexp.MustCompile("/.*/")),
+			},
+		}, {
+			Name: "translation_card_clamshell",
+			Val: searchTestCase{TabletMode: false,
+				SearchKeyword:  "translate hello into spanish",
+				ExpectedResult: launcher.SearchResultListItemFinder.NameContaining("Hola"),
+			},
+		}, {
+			Name: "addition_card_clamshell",
+			Val: searchTestCase{TabletMode: false,
+				SearchKeyword:  "1+1",
+				ExpectedResult: launcher.SearchResultListItemFinder.NameContaining("1+1, 2"),
+			},
+		}, {
+			Name: "unit_conversion_card_clamshell",
+			Val: searchTestCase{TabletMode: false,
+				SearchKeyword:  "455 lb in kg",
+				ExpectedResult: launcher.SearchResultListItemFinder.NameRegex(regexp.MustCompile("455 lb in kg, 206.*")),
+			},
+		}, {
+			Name: "stock_card_clamshell",
+			Val: searchTestCase{TabletMode: false,
+				SearchKeyword:  "goog stock",
+				ExpectedResult: launcher.SearchResultListItemFinder.NameRegex(regexp.MustCompile("NASDAQ")),
+			},
+		}, {
+			Name: "weather_card_clamshell",
+			Val: searchTestCase{TabletMode: false,
+				SearchKeyword:  "weather",
+				ExpectedResult: launcher.SearchResultListItemFinder.NameRegex(regexp.MustCompile("-?[1-9][0-9]*")),
+			},
 		},
 		/* Disabled due to <1% pass rate over 30 days. See b/241943050
 		{
 			Name:              "tablet_mode",
-			Fixture:           "chromeLoggedIn",
 			Val:               launcher.TestCase{TabletMode: true},
 			ExtraHardwareDeps: hwdep.D(hwdep.InternalDisplay()),
 		}
@@ -47,9 +90,16 @@ func init() {
 	})
 }
 
-type answerCardTestCase struct {
-	searchKeyword  string
-	validateAction uiauto.Action
+func runAswerCardTestCase(ctx context.Context, tconn *chrome.TestConn,
+	kb *input.KeyboardEventWriter,
+	searchKeyword string, expectedResult *nodewith.Finder, ui *uiauto.Context) bool {
+	if err := uiauto.Combine("search launcher",
+		launcher.Search(tconn, kb, searchKeyword),
+		ui.WaitUntilExists(expectedResult),
+	)(ctx); err != nil {
+		return false
+	}
+	return true
 }
 
 // AnswerCards checks inline answers for special queries.
@@ -71,7 +121,7 @@ func AnswerCards(ctx context.Context, s *testing.State) {
 	}
 	defer kb.Close()
 
-	testCase := s.Param().(launcher.TestCase)
+	testCase := s.Param().(searchTestCase)
 	tabletMode := testCase.TabletMode
 
 	// SetUpLauncherTest opens the launcher.
@@ -83,49 +133,22 @@ func AnswerCards(ctx context.Context, s *testing.State) {
 
 	ui := uiauto.New(tconn)
 
-	subtests := []answerCardTestCase{
-		{
-			searchKeyword:  "definition of flaky",
-			validateAction: ui.WaitUntilExists(launcher.SearchResultListItemFinder.NameRegex(regexp.MustCompile("/.*/"))),
-		},
-		{
-			searchKeyword:  "translate hello into spanish",
-			validateAction: ui.WaitUntilExists(launcher.SearchResultListItemFinder.NameContaining("Hola")),
-		},
-		{
-			searchKeyword:  "1+1",
-			validateAction: ui.WaitUntilExists(launcher.SearchResultListItemFinder.NameContaining("1+1, 2")),
-		},
-		{
-			searchKeyword:  "is europe a continent",
-			validateAction: ui.WaitUntilExists(launcher.SearchResultListItemFinder.NameContaining("is europe a continent, yes")),
-		},
-		{
-			searchKeyword:  "455 lb in kg",
-			validateAction: ui.WaitUntilExists(launcher.SearchResultListItemFinder.NameRegex(regexp.MustCompile("455 lb in kg, 206.*"))),
-		},
-		{
-			searchKeyword:  "goog stock",
-			validateAction: ui.WaitUntilExists(launcher.SearchResultListItemFinder.NameRegex(regexp.MustCompile("NASDAQ"))),
-		},
-		{
-			searchKeyword:  "weather",
-			validateAction: ui.WaitUntilExists(launcher.SearchResultListItemFinder.NameRegex(regexp.MustCompile("-?[1-9][0-9]*"))),
-		},
-	}
+	defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+string(testCase.SearchKeyword))
 
-	for _, subtest := range subtests {
-		s.Run(ctx, subtest.searchKeyword, func(ctx context.Context, s *testing.State) {
-			defer faillog.DumpUITreeWithScreenshotOnError(ctx, s.OutDir(), s.HasError, cr, "ui_tree_"+string(subtest.searchKeyword))
+	retries := 3
+	for i := 0; i < retries; i++ {
+		if runAswerCardTestCase(ctx, tconn, kb, testCase.SearchKeyword, testCase.ExpectedResult, ui) {
+			// Test succeeded - no need for retries.
+			return
+		}
 
-			if err := uiauto.Combine("search launcher",
-				launcher.Search(tconn, kb, subtest.searchKeyword),
-				subtest.validateAction,
-			)(ctx); err != nil {
-				s.Fatal("Failed to search: ", err)
-			}
-		})
-		// Clear the input, even if the subtest failed.
+		s.Logf("Failed to search: %s attempt %d out of %d", testCase.SearchKeyword, i, retries)
+
+		if i == retries-1 {
+			s.Fatal("Unable to show answer card for: ", testCase.SearchKeyword)
+		}
+
+		// Clear the input and try again.
 		kb.TypeKey(ctx, input.KEY_ESC)
 	}
 }
