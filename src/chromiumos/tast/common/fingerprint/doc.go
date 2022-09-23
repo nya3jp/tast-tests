23abea71958eba3e1936229b01470280b56600b2
Craig Hesling <hesling@chromium.org>
2022-09-22 17:36:05 -0700

    fingerprint: Move FPBoardName to common/fingerprint
    We need access to these fingerprint constants in local
    and remote tests.
    
    BUG=b:248325196
    TEST=fast_build.sh -T
    
    Change-Id: I2ced87fc6e87ae75341082a008c95371c9c7e341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3915614
    Tested-by: Craig Hesling <hesling@chromium.org>
    Commit-Queue: Craig Hesling <hesling@chromium.org>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Jun Sheng <shengjun@chromium.org>

---
+++ src/chromiumos/tast/common/fingerprint/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package fingerprint defines constants and logic that is common to remote
+// and local fingerprint tests.
+package fingerprint
