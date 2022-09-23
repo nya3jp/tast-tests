d80fb11af9d837fa5bd3ba0b55be0d82e4b92527
Craig Hesling <hesling@chromium.org>
2022-09-23 00:34:42 -0700

    biod: Add test for fingerprint firmware file in rootfs
    BUG=b:248610343
    TEST=# Test on nocturne, hatch/dragonair
         tast -verbose run <DUT> biod.FirmwareFile
    
    Change-Id: Ia92300542bbd9dd2ce02a930dedcfd544d33c6ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3914149
    Commit-Queue: Craig Hesling <hesling@chromium.org>
    Reviewed-by: Gabor Zsolt Magda <gabormagda@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Craig Hesling <hesling@chromium.org>

---
+++ src/chromiumos/tast/local/bundles/cros/biod/firmware_file.go
@@ -0,0 +1,49 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package biod
+
+import (
+	"context"
+	"path/filepath"
+
+	fp "chromiumos/tast/common/fingerprint"
+	"chromiumos/tast/local/crosconfig"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: FirmwareFile,
+		Desc: "Checks that the specific fingerprint firmware file is in rootfs",
+		Contacts: []string{
+			"hesling@chromium.org",
+			"chromeos-fingerprint@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"biometrics_daemon"},
+		HardwareDeps: hwdep.D(hwdep.Fingerprint()),
+	})
+}
+
+// FirmwareFile checks that the specific fingerprint firmware file is in rootfs.
+func FirmwareFile(ctx context.Context, s *testing.State) {
+	board, err := crosconfig.Get(ctx, "/fingerprint", "board")
+	if err != nil {
+		s.Fatal("Failed to fetch board from cros-config: ", err)
+	}
+
+	files, err := filepath.Glob(fp.FirmwareFilePattern(fp.BoardName(board)))
+	if err != nil {
+		s.Fatal("Failed to glob for firmware files: ", err)
+	}
+	if len(files) == 0 {
+		s.Fatalf("Couldn't find the fingerprint firmware file for board %q", board)
+	}
+	if len(files) > 1 {
+		s.Fatal("Too many fingerprint firmware files were found: ", files)
+	}
+	s.Logf("Found firmware file %q", files[0])
+}
