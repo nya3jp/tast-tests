07c8beb6977dd2b8267318b4ac971a7850b70966
Craig Hesling <hesling@chromium.org>
2022-09-23 16:31:54 -0700

    biod: Add test for cros-config fp support matches real device
    This test checks that the cros-config fingerprint description
    matches the physical fingerprint hardware. This will mostly
    be focusing on issues with fp configuration in cros-config, but
    may also be able to detect cros-ec driver binding issues.
    
    We separate the main test directions:
    
    1) cros-config then check we have /dev/cros_fp
    2) have /dev/cros_fp check cros-config
    
    Doing this makes it easier to understand the code and the errors
    being emitted.
    
    BUG=b:248610343
    TEST=fast_build.sh -T
    TEST=# Test on FP devices: hatch
         # Test on non-FP devices: eve
         tast -verbose run <DUT> biod.CrosConfigMatchesDevice
    TEST=# Test on FP devices: hatch
         ssh localhost:2222 -- flash_fp_mcu # Ctrl-C right after /dev/cros_fp unbind step
         tast -verbose run localhost:2222 biod.CrosConfigMatchesDevice
         # Ensure that we see test Fail because
         # ".../dev/cros_fp is not preset".
         cat /tmp/tast/results/latest/tests/biod.CrosConfigMatchesDevice/dmesg.txt
         # Ensure that dmesg was captured
    TEST=Other manual tests were done by placing s.Logs and
         corrupting property names.
    
    Change-Id: I5c839a9fe94e33de5b9bbb83debfab408daee1d5

---
+++ src/chromiumos/tast/local/bundles/cros/biod/cros_config_matches_device.go
@@ -0,0 +1,100 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package biod
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+
+	fp "chromiumos/tast/common/fingerprint"
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/crosconfig"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosConfigMatchesDevice,
+		Desc: "Checks that fingerprint support in cros-config agrees with the device/driver",
+		Contacts: []string{
+			"hesling@chromium.org",
+			"chromeos-fingerprint@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+		// Note that hwdep for fingerprint relies on cros-config.
+		// We omit any dependencies on hwdep fingerprint so that we can detect
+		// issues where cros-config fails to mention fingerprint support.
+	})
+}
+
+// captureDmesg saves the current dmesg to dmesg.txt so that we could debug
+// possible cros-ec binding issues.
+//
+// If running the test manually, check the following file path:
+// /tmp/tast/results/latest/tests/biod.CrosConfigMatchesDevice/dmesg.txt
+func captureDmesg(ctx context.Context, dir string) error {
+	testing.ContextLog(ctx, "Please check the captured dmesg.txt for cros-ec binding issues")
+
+	dmesg, err := testexec.CommandContext(ctx, "dmesg").CombinedOutput()
+	if err != nil {
+		return errors.Wrap(err, "failed to invoke dmesg to get possible bind error")
+	}
+	err = os.WriteFile(filepath.Join(dir, "dmesg.txt"), dmesg, 0644)
+	if err != nil {
+		return errors.Wrap(err, "failed to write dmesg.txt")
+	}
+	return nil
+}
+
+// CrosConfigMatchesDevice checks that cros-config fingerprint support
+// matches the fingerprint device seen.
+//
+// 1. If cros-config supports fingerprint --> Expect /dev/cros_fp to exist
+// 2. If /dev/cros_fp exists --> Expect cros-config to support fingerprint
+// 3. Ensure that cros-config board matches the actual FPMCU version/board.
+func CrosConfigMatchesDevice(ctx context.Context, s *testing.State) {
+	sensorLocation, err := crosconfig.Get(ctx, "/fingerprint", "sensor-location")
+	if err != nil && !crosconfig.IsNotFound(err) {
+		s.Fatal("Failed to invoke cros_config for sensor-location: ", err)
+	}
+	cfgFpSupported := fp.SensorLoc(sensorLocation).IsSupported()
+
+	// Reading from /dev/cros_fp tells us if the file exists and provides the
+	// FPMCU version.
+	version, err := os.ReadFile("/dev/cros_fp")
+	if err != nil && !os.IsNotExist(err) {
+		s.Fatal("Failed to read /dev/cros_fp for reasons other than does-not-exist: ", err)
+	}
+	fpDevNotExists := os.IsNotExist(err)
+	fpDevExists := err == nil
+
+	if cfgFpSupported && fpDevNotExists {
+		s.Error("Cros-config supports fingerprint, but /dev/cros_fp is not preset")
+		// We capture dmesg after to ensure that the above error is shown first.
+		if err := captureDmesg(ctx, s.OutDir()); err != nil {
+			s.Error("Failed to capture dmesg: ", err)
+		}
+		return
+	}
+	if fpDevExists && !cfgFpSupported {
+		s.Fatal("The /dev/cros_fp device exists, but cros-config doesn't support fingerprint")
+	}
+
+	if !cfgFpSupported {
+		s.Log("Fingerprint is properly unsupported on device")
+		return
+	}
+
+	board, err := crosconfig.Get(ctx, "/fingerprint", "board")
+	if err != nil {
+		s.Fatal("Failed to get board from cros_config: ", err)
+	}
+	if ver := string(version); !strings.Contains(ver, board) {
+		s.Fatalf("The cros-config board %q doesn't match FPMCU version %q", board, ver)
+	}
+}
