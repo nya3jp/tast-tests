8de19eacb57f24fea5edbdb6a6a8adfead3bb78d
Craig Hesling <hesling@chromium.org>
2022-09-22 21:33:30 -0700

    biod: Add test to check fingerprint cros-configs
    This is based on:
    https://source.chromium.org/chromium/chromiumos/platform2/+/main:chromeos-config/cros_config_host/cros_config_schema.yaml;l=626;drc=06f98e0887bae6532ed765e2fb2c8b7bcaf3a718
    
    BUG=b:248325196
    TEST=fast_build.sh -T
    TEST=# Test on FP devices: nocturne, hatch,
         # volteer/eldrid, zork, coachz
         # Test on non-FP devices: eve
         tast -verbose run <DUT> biod.CrosConfig
    
    Change-Id: I3fb53554cd9a8b86fa5722d73f7239472ca8b917

--- src/chromiumos/tast/common/fingerprint/config.go
+++ src/chromiumos/tast/common/fingerprint/config.go
@@ -18,3 +18,76 @@ const (
 	BoardNameNocturne     BoardName = "nocturne_fp"
 	BoardNameNami         BoardName = "nami_fp"
 )
+
+// IsValid checks if the BoardName is a valid fingerprint board name.
+func (b BoardName) IsValid() bool {
+	switch b {
+	case BoardNameBloonchipper,
+		BoardNameDartmonkey,
+		BoardNameNocturne,
+		BoardNameNami:
+		return true
+	default:
+		return false
+	}
+}
+
+// SensorLoc represents the cros-config fingerprint sensor-location.
+type SensorLoc string
+
+// Possible values for cros-config fingerprint sensor-location.
+const (
+	SensorLocNone                      SensorLoc = "none"
+	SensorLocPowerButtonTopLeft        SensorLoc = "power-button-top-left"
+	SensorLocKeyboardBottomLeft        SensorLoc = "keyboard-bottom-left"
+	SensorLocKeyboardBottomRight       SensorLoc = "keyboard-bottom-right"
+	SensorLocKeyboardTopRight          SensorLoc = "keyboard-top-right"
+	SensorLocRightSide                 SensorLoc = "right-side"
+	SensorLocLightSide                 SensorLoc = "left-side"
+	SensorLocLeftOfPowerButtonTopRight SensorLoc = "left-of-power-button-top-right"
+)
+
+// IsValid checks if SensorLoc is a valid fingerprint sensor-location.
+func (l SensorLoc) IsValid() bool {
+	switch l {
+	case SensorLocNone,
+		SensorLocPowerButtonTopLeft,
+		SensorLocKeyboardBottomLeft,
+		SensorLocKeyboardBottomRight,
+		SensorLocKeyboardTopRight,
+		SensorLocRightSide,
+		SensorLocLightSide,
+		SensorLocLeftOfPowerButtonTopRight:
+		return true
+	default:
+		return false
+	}
+}
+
+// IsSupported determine if fingerprint is supported on the device based on
+// the SensorLoc. It allows parsing the "" shortcut for not-defined.
+func (l SensorLoc) IsSupported() bool {
+	if l != SensorLoc("") && l != SensorLocNone {
+		return true
+	}
+	return false
+}
+
+// SensorType represents the cros-config fingerprint fingerprint-sensor-type.
+type SensorType string
+
+// Possible values for cros-config fingerprint fingerprint-sensor-type.
+const (
+	SensorTypeStandAlone    SensorType = "stand-alone"
+	SensorTypeOnPowerButton SensorType = "on-power-button"
+)
+
+// IsValid checks if SensorType is a valid fingerprint sensor-type.
+func (t SensorType) IsValid() bool {
+	switch t {
+	case SensorTypeStandAlone, SensorTypeOnPowerButton:
+		return true
+	default:
+		return false
+	}
+}
---
+++ src/chromiumos/tast/local/bundles/cros/biod/cros_config.go
@@ -0,0 +1,86 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package biod
+
+import (
+	"context"
+
+	fp "chromiumos/tast/common/fingerprint"
+	"chromiumos/tast/local/crosconfig"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CrosConfig,
+		Desc: "Checks that the fingerprint cros-config is reasonable",
+		Contacts: []string{
+			"hesling@chromium.org",
+			"chromeos-fingerprint@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+		// We omit any dependencies on fingerprint so that we can ensure that
+		// the the fingerprint cros-config section is always reasonable.
+	})
+}
+
+// CrosConfig checks that the /fingerprint section of cros-config is reasonable.
+func CrosConfig(ctx context.Context, s *testing.State) {
+
+	// sensor-location
+
+	sensorLocation, err := crosconfig.Get(ctx, "/fingerprint", "sensor-location")
+	if err != nil && !crosconfig.IsNotFound(err) {
+		s.Fatal("Failed to invoke cros_config for sensor-location: ", err)
+	}
+	// When FP is not supported, sensor-location is not-defined or "none".
+	// We check for "" in other parts of the code, but that is just a shortcut
+	// for not-defined. Let's be strict with this rule here.
+	if crosconfig.IsNotFound(err) ||
+		fp.SensorLoc(sensorLocation) == fp.SensorLocNone {
+		s.Log("Fingerprint is properly unsupported on device")
+		return
+	}
+	if !fp.SensorLoc(sensorLocation).IsValid() {
+		s.Fatal("Failed to invoke cros_config for board: ", err)
+	}
+
+	// board
+
+	board, err := crosconfig.Get(ctx, "/fingerprint", "board")
+	if err != nil && !crosconfig.IsNotFound(err) {
+		s.Fatal("Failed to invoke cros_config for board: ", err)
+	}
+	if crosconfig.IsNotFound(err) {
+		s.Fatal("Config /fingerprint board is missing")
+	}
+	if !fp.BoardName(board).IsValid() {
+		s.Fatalf("Config /fingerprint board %q is invalid", board)
+	}
+
+	// fingerprint-sensor-type
+
+	sensorType, err := crosconfig.Get(ctx, "/fingerprint", "fingerprint-sensor-type")
+	if err != nil && !crosconfig.IsNotFound(err) {
+		s.Fatal("Failed to invoke cros_config for sensor_type: ", err)
+	}
+	if crosconfig.IsNotFound(err) {
+		s.Log("Not using ", fp.SensorTypeStandAlone, " for fingerprint-sensor-type")
+		return
+	}
+	if !fp.SensorType(sensorType).IsValid() {
+		s.Fatalf("Config /fingerprint fingerprint-sensor-type %q is invalid",
+			sensorType)
+	}
+
+	// Additional Logic
+
+	if fp.SensorLoc(sensorLocation) == fp.SensorLocPowerButtonTopLeft {
+		if fp.SensorType(sensorType) != fp.SensorTypeOnPowerButton {
+			s.Fatalf("Sensor location is on power button, but type %q doesn't match", sensorType)
+		}
+	}
+
+}
