b01fa8ca7b16b8de83d6a3244f47ba2217416fc0
Li-Yu Yu <aaronyu@google.com>
2022-09-23 19:28:43 +0800

    Add platform.ServicesOnBoot test to catch upstart races
    b/245592231 and b/245517774 describes incorrect upstart dependencies
    causing a P0 bug. platform.ServicesOnBoot checks that services are
    started correctly without any retry to catch such issues.
    
    BUG=b:246971348
    TEST=~/trunk/src/platform/tast/tools/go.sh generate chromiumos/tast/services/cros/platform
    TEST=~/trunk/src/platform/tast/fast_build.sh -T
    TEST=tast run dut platform.ServicesOnBoot.\*
    
    Change-Id: Ica62ae7a7f331a54ad9fa187f169da17aff59459

---
+++ src/chromiumos/tast/remote/bundles/cros/platform/fixtures/services_on_boot_fixt.go
@@ -0,0 +1,144 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fixtures
+
+import (
+	"context"
+	"regexp"
+	"strconv"
+	"time"
+
+	"google.golang.org/protobuf/types/known/durationpb"
+
+	"chromiumos/tast/common/upstart"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/firmware/fingerprint/rpcdut"
+	"chromiumos/tast/services/cros/platform"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddFixture(&testing.Fixture{
+		Name:            ServicesOnBootFixt,
+		Desc:            "Reboot and collect upstart service failures",
+		Contacts:        []string{"aaronyu@google.com", "chromeos-audio-sw@google.com"},
+		Impl:            &servicesOnBootFixt{},
+		SetUpTimeout:    5 * time.Minute,
+		TearDownTimeout: 1 * time.Minute,
+		ServiceDeps:     []string{"tast.cros.platform.UpstartService"},
+	})
+}
+
+// servicesOnBootFixt is a fixture for the platform.ServiceOnBoot test.
+type servicesOnBootFixt struct {
+	ServicesOnBootFixtVal
+	rpcdut *rpcdut.RPCDUT
+}
+
+// ServicesOnBootFixtVal contains public fields for the platform.ServiceOnBoot test.
+type ServicesOnBootFixtVal struct {
+	Failures []ServiceFailure
+	Upstart  platform.UpstartServiceClient
+}
+
+var _ testing.FixtureImpl = &servicesOnBootFixt{}
+
+// ServiceFailure is a log entry from upstart logs.
+type ServiceFailure struct {
+	Message        string
+	JobName        string
+	ProcessName    string
+	ExitStatus     int // The exit status of the job. -1 means abnormal exit.
+	KilledBySignal string
+}
+
+func (fixt *servicesOnBootFixt) SetUp(ctx context.Context, s *testing.FixtState) interface{} {
+	s.Log("Rebooting DUT")
+	if err := s.DUT().Reboot(ctx); err != nil {
+		s.Fatal("Failed to reboot DUT: ", err)
+	}
+
+	var err error
+	fixt.rpcdut, err = rpcdut.NewRPCDUT(s.FixtContext(), s.DUT(), s.RPCHint(), "cros")
+	if err != nil {
+		s.Fatal("Failed to connect RPCDUT: ", err)
+	}
+
+	fixt.Upstart = platform.NewUpstartServiceClient(fixt.rpcdut.RPC().Conn)
+	s.Log("Waiting for system-services")
+	if _, err := fixt.Upstart.WaitForJobStatus(ctx, &platform.WaitForJobStatusRequest{
+		JobName: "system-services",
+		Goal:    string(upstart.StartGoal),
+		State:   string(upstart.RunningState),
+		Timeout: durationpb.New(3 * time.Minute),
+	}); err != nil {
+		s.Fatal("Failed to wait for system-services: ", err)
+	}
+
+	const sleepDuration = 30 * time.Second
+	s.Logf("Sleeping for %s to collect service activity", sleepDuration)
+	testing.Sleep(ctx, sleepDuration)
+
+	// Parse dmesg to get service failures
+	// Not using /var/log/upstart.log as we're not interested in service
+	// failures from previous boots.
+	dmesg, err := s.DUT().Conn().CommandContext(ctx, "dmesg").Output()
+	if err != nil {
+		s.Fatal("Failed to run dmesg: ", err)
+	}
+	fixt.Failures, err = parseServiceFailures(string(dmesg))
+	if err != nil {
+		s.Fatal("Cannot parse service failures from demsg: ", err)
+	}
+
+	return &fixt.ServicesOnBootFixtVal
+}
+
+func parseServiceFailures(dmesg string) ([]ServiceFailure, error) {
+	// Regular expression for upstart logs:
+	//	[    1.374461] init: early-failure main process (315) terminated with status 124
+	//	[    9.131824] init: failsafe-delay main process (944) killed by TERM signal
+	initRE := regexp.MustCompile(`\[[0-9\. ]+\] init: (\S+) (\S+) process \(\d+\) (?:terminated with status (\d+)|killed by (\S+) signal)`)
+
+	messages := initRE.FindAllStringSubmatch(string(dmesg), -1)
+	result := make([]ServiceFailure, 0, len(messages))
+	for _, message := range messages {
+		failure := ServiceFailure{
+			Message:     message[0],
+			JobName:     message[1],
+			ProcessName: message[2],
+		}
+		if message[3] != "" {
+			var err error
+			failure.ExitStatus, err = strconv.Atoi(message[3])
+			if err != nil {
+				return nil, errors.Errorf("cannot parse exit status of %q", message[0])
+			}
+		} else {
+			failure.ExitStatus = -1
+			failure.KilledBySignal = message[4]
+		}
+		result = append(result, failure)
+	}
+	return result, nil
+}
+
+func (*servicesOnBootFixt) Reset(ctx context.Context) error {
+	return nil
+}
+
+func (*servicesOnBootFixt) PreTest(ctx context.Context, s *testing.FixtTestState) {}
+
+func (*servicesOnBootFixt) PostTest(ctx context.Context, s *testing.FixtTestState) {}
+
+func (fixt *servicesOnBootFixt) TearDown(ctx context.Context, s *testing.FixtState) {
+	if fixt.rpcdut != nil {
+		if err := fixt.rpcdut.Close(ctx); err != nil {
+			s.Fatal("Failed to close RPCDUT: ", err)
+		}
+	}
+
+	fixt = &servicesOnBootFixt{}
+}
