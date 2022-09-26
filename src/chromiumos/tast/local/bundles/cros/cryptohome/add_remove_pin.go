12be30fafe20c42a4a6ea9a7a8eac4149ad2a807
Hardik Goyal <hardikgoyal@google.com>
2022-09-26 16:18:04 -0700

    tast-tests: Parameterize test to use VK
    This CL also changes the test slightly. Instead of logging in with
    pin, it now logs in with password because for VK there is a test that
    you cannot delete the credential you logged in with. This will be gone
    once we have USS only backing store.
    
    BUG=b:240594146
    TEST=CQ Dry Passes
    
    Change-Id: I4cfa7493bf252c93550197e9da4e88ced7531aa0

--- src/chromiumos/tast/local/bundles/cros/cryptohome/add_remove_pin.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/add_remove_pin.go
@@ -19,23 +19,30 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func: AddRemovePin,
-		Desc: "Adds, removes and re-adds pin in user secret stash",
+		Func: AddRemovePIN,
+		Desc: "Adds, removes and re-adds PIN with specified backing store",
 		Contacts: []string{
 			"anastasiian@chromium.org",
 			"cryptohome-core@google.com",
 		},
 		Attr:         []string{"group:mainline"},
 		SoftwareDeps: []string{"pinweaver", "reboot"},
-		Fixture:      "ussAuthSessionFixture",
+		Params: []testing.Param{{
+			Name:    "with_uss",
+			Fixture: "ussAuthSessionFixture",
+		}, {
+			Name:      "with_vk",
+			ExtraAttr: []string{"informational"},
+		},
+		},
 	})
 }
 
-func AddRemovePin(ctx context.Context, s *testing.State) {
+func AddRemovePIN(ctx context.Context, s *testing.State) {
 	const (
 		userName                   = "foo@bar.baz"
 		userPassword               = "secret"
-		userPin                    = "123456"
+		userPIN                    = "123456"
 		passwordLabel              = "online-password"
 		pinLabel                   = "test-pin"
 		cryptohomeErrorKeyNotFound = 15
@@ -67,9 +74,9 @@ func AddRemovePin(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to write a file to the vault: ", err)
 	}
 
-	addAndAuthenticatePinAuthFactor := func() error {
-		// Add a pin auth factor to the user.
-		if err := client.AddPinAuthFactor(ctx, authSessionID, pinLabel, userPin); err != nil {
+	addAndAuthenticatePINAuthFactor := func() error {
+		// Add a PIN auth factor to the user.
+		if err := client.AddPinAuthFactor(ctx, authSessionID, pinLabel, userPIN); err != nil {
 			return errors.Wrap(err, "failed to create persistent user")
 		}
 
@@ -78,12 +85,15 @@ func AddRemovePin(ctx context.Context, s *testing.State) {
 			return errors.Wrap(err, "failed to unmount vaults for re-mounting")
 		}
 
-		// Authenticate a new auth session via the new added pin auth factor and mount the user.
+		if err := client.InvalidateAuthSession(ctx, authSessionID); err != nil {
+			return errors.Wrap(err, "failed to invalidate AuthSession")
+		}
+		// Authenticate a new auth session via the new added PIN auth factor and mount the user.
 		authSessionID, err = client.StartAuthSession(ctx, userName, false /*ephemeral*/, uda.AuthIntent_AUTH_INTENT_DECRYPT)
 		if err != nil {
 			return errors.Wrap(err, "failed to start auth session for re-mounting")
 		}
-		if err := client.AuthenticatePinAuthFactor(ctx, authSessionID, pinLabel, userPin); err != nil {
+		if err := client.AuthenticatePinAuthFactor(ctx, authSessionID, pinLabel, userPIN); err != nil {
 			return errors.Wrap(err, "failed to authenticate with auth session")
 		}
 		if err := client.PreparePersistentVault(ctx, authSessionID, false /*ecryptfs*/); err != nil {
@@ -92,7 +102,10 @@ func AddRemovePin(ctx context.Context, s *testing.State) {
 
 		// Verify that file is still there.
 		if err := cryptohome.VerifyFileForPersistence(ctx, userName); err != nil {
-			s.Fatal("Failed to verify test file: ", err)
+			return errors.Wrap(err, "failed to verify test file persistence")
+		}
+		if err := client.InvalidateAuthSession(ctx, authSessionID); err != nil {
+			return errors.Wrap(err, "failed to invalidate AuthSession")
 		}
 		return nil
 	}
@@ -102,17 +115,24 @@ func AddRemovePin(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to add a password authfactor: ", err)
 	}
 
-	// Can add and successfully authenticate via pin.
-	if err := addAndAuthenticatePinAuthFactor(); err != nil {
-		s.Fatal("Failed to add and authenticate with pin authfactor: ", err)
+	// Can add and successfully authenticate via PIN.
+	if err := addAndAuthenticatePINAuthFactor(); err != nil {
+		s.Fatal("Failed to add and authenticate with PIN authfactor: ", err)
 	}
 
-	// Remove the pin auth factor.
+	authSessionID, err = client.StartAuthSession(ctx, userName, false /*ephemeral*/, uda.AuthIntent_AUTH_INTENT_DECRYPT)
+	if err != nil {
+		s.Fatal("Failed to start auth session: ", err)
+	}
+	if _, err := client.AuthenticateAuthFactor(ctx, authSessionID, passwordLabel, userPassword); err != nil {
+		s.Fatal("Failed to authenticate with auth session with user password: ", err)
+	}
 	if err := client.RemoveAuthFactor(ctx, authSessionID, pinLabel); err != nil {
-		s.Fatal("Failed to create persistent user: ", err)
+		s.Fatal("Failed to remove PIN AuthFactor: ", err)
 	}
 
-	err = client.AuthenticatePinAuthFactor(ctx, authSessionID, pinLabel, userPin)
+	// Authentication in the same session should fail as well.
+	err = client.AuthenticatePinAuthFactor(ctx, authSessionID, pinLabel, userPIN)
 	var exitErr *hwsec.CmdExitError
 	if !errors.As(err, &exitErr) {
 		s.Fatalf("Unexpected error: got %q; want *hwsec.CmdExitError", err)
@@ -121,8 +141,8 @@ func AddRemovePin(ctx context.Context, s *testing.State) {
 		s.Fatalf("Unexpected exit code: got %d; want %d", exitErr.ExitCode, cryptohomeErrorKeyNotFound)
 	}
 
-	// Can add and successfully authenticate via pin.
-	if err := addAndAuthenticatePinAuthFactor(); err != nil {
-		s.Fatal("Failed to add and authenticate with pin authfactor: ", err)
+	// Can add and successfully authenticate via PIN.
+	if err := addAndAuthenticatePINAuthFactor(); err != nil {
+		s.Fatal("Failed to add and authenticate with PIN authfactor: ", err)
 	}
 }
