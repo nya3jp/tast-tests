5baa5b68f12d6a13d2e4581f048cf4387f35c57c
Craig Hesling <hesling@chromium.org>
2022-09-26 17:00:03 -0700

    biod: Generalize missing fingerprint-sensor-type
    Generalize missing fingerprint-sensor-type as error that currently
    doesn't fail the test.
    
    BUG=b:248325196,b:248610343
    TEST=fast_build.sh -T
    
    Change-Id: If0f8cdb7a7d5fe89da3b2a58b7aa2303bd8cf8cc

--- src/chromiumos/tast/local/bundles/cros/biod/cros_config.go
+++ src/chromiumos/tast/local/bundles/cros/biod/cros_config.go
@@ -61,7 +61,7 @@ func CrosConfig(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to invoke cros_config for sensor_type: ", err)
 	}
 	if crosconfig.IsNotFound(err) {
-		s.Logf("Not using %q for fingerprint-sensor-type", fp.SensorTypeStandAlone)
+		s.Log("Not using fingerprint-sensor-type")
 		return
 	}
 	if !fp.SensorType(sensorType).IsValid() {
