4b2108927d2f85592bbe7840cce1e73ccda5d277
Miriam Polzer <mpolzer@google.com>
2022-09-27 08:53:27 +0000

    rollback: Do not call prod DMServer
    After rollback, an automatic re-enrollment request is sent to prod
    DMServer. Try to prevent that during tests by setting the URL to
    nonsense when restarting Chrome.
    
    This doesn't fully prevent sending requests to prod DMServer, as Chrome
    first automatically restarts and we can't reliably modify
    /etc/chrome-dev.conf.
    
    BUG=b:248367972
    TEST=tast run <dut> policy.EnterpriseRollbackInPlace
    
    Change-Id: I21aca50fa866ed693172d445b04a08a6f1fc7981

--- src/chromiumos/tast/local/bundles/cros/autoupdate/rollback_service.go
+++ src/chromiumos/tast/local/bundles/cros/autoupdate/rollback_service.go
@@ -136,7 +136,10 @@ func (r *RollbackService) VerifyRollback(ctx context.Context, request *aupb.Veri
 		return nil, errors.Wrapf(err, "failed to convert milestone %s to integer", lsbContent[lsbrelease.Milestone])
 	}
 
-	cr, err := chrome.New(ctx, chrome.DeferLogin())
+	// Chrome would send an auto re-enrollment request to the real DMServer
+	// which will fail because the device wasn't enrolled at all.
+	// Try to prevent that by setting DMServer URL to nonsense.
+	cr, err := chrome.New(ctx, chrome.DMSPolicy("do-not-call-any-dmserver"), chrome.DeferLogin())
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to restart Chrome for testing after rollback")
 	}
