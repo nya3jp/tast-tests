632be53ef6ee18c01f555ef26441dab00e2a1a83
Terry Yan <yichiyan@google.com>
2022-09-29 03:49:45 -0700

    tast-test update: using golang.org/x/sys/unix instead of syscall in disk
    BUG=b:187787902
    TEST=fast_build.sh -T
    
    Change-Id: Ic4b017b5f28971e5eb88d19619f480c205a2aa8d

--- src/chromiumos/tast/local/disk/space.go
+++ src/chromiumos/tast/local/disk/space.go
@@ -5,15 +5,15 @@
 package disk
 
 import (
-	"syscall"
+	"golang.org/x/sys/unix"
 
 	"chromiumos/tast/errors"
 )
 
 // FreeSpace returns the number of free bytes available at a specific path
 func FreeSpace(path string) (uint64, error) {
-	var stat syscall.Statfs_t
-	if err := syscall.Statfs(path, &stat); err != nil {
+	var stat unix.Statfs_t
+	if err := unix.Statfs(path, &stat); err != nil {
 		return 0, errors.Wrapf(err, "failed to get disk stats for %s", path)
 	}
 
