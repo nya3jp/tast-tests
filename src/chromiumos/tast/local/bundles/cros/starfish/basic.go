3522336ed62eb31fd40cb7784d46d4eed5190c32
Nagi Marupaka <nmarupaka@google.com>
2022-09-29 17:17:32 +0000

    [starfish] Add starfish module in tast-tests
    Starfish is a multi-SIM board that enables enhanced testing capabilities
    in cellular domain. This CL adds the control and interface code for the
    board.
    
    BUG=b:237894606
    TEST=./fast_build.sh -T
    TEST=tast run <dut> starfish.basic
    
    Cq-Depend: chromium:3928327
    Change-Id: Idfae74aeaa98aa36379b4400291ff952c18d2bf6

---
+++ src/chromiumos/tast/local/bundles/cros/starfish/basic.go
@@ -0,0 +1,33 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package starfish
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/local/starfish"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Basic,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Verifies that host has network connectivity via cellular interface",
+		Contacts:     []string{"nmarupaka@google.com", "chromeos-cellular-team@google.com"},
+		Attr:         []string{"group:cellular", "cellular_unstable", "cellular_sim_active"},
+		Timeout:      3 * time.Minute,
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+// Basic tests if the helper is being created successfully.
+func Basic(ctx context.Context, s *testing.State) {
+	_, err := starfish.NewHelper(ctx)
+	if err != nil {
+		s.Fatal("Failed to create starfish.Helper: ", err)
+	}
+}
