f3266cf1e8e65821df884361368b73bc1770e020
Yury Khmel <khmel@chromium.org>
2022-09-29 11:21:29 -0700

    This adds configuration for idle waiting.
    Needed to be flexible with some perf tests especially for low-end
    devices.
    
    BUG=b:243194102
    TEST=tast run -buildbundle crosint ekko arc.RuntimePerf.sniper3d_vm
         FPS is stable 55+. Also tested krane vm, ekko container.
    
    Change-Id: Ic383906a1908143146bbd6361a342aa12d7a9b38

--- src/chromiumos/tast/local/cpu/idle.go
+++ src/chromiumos/tast/local/cpu/idle.go
@@ -12,37 +12,58 @@ import (
 	"chromiumos/tast/testing"
 )
 
+// IdleConfig contains the config to wait for the CPU is idle.
+type IdleConfig struct {
+	// time to wait for CPU to become idle.
+	Timeout time.Duration
+	// percentage below which CPU is ideally considered idle, gradually
+	// increased up to CPUUsagePercentMax.
+	CPUUsagePercentBase float64
+	// maximum percentage below which CPU is considered idle.
+	CPUUsagePercentMax float64
+	// number of steps to take while relaxing target CPU idle percentage
+	// from CPUUsagePercentBase to CPUUsagePercentMax. Each step has a
+	// duration of Timeout/Steps. Steps must be greater than 1.
+	Steps int
+}
+
+// DefaultIdleConfig returns the default config to wait for the CPU is idle.
+func DefaultIdleConfig() IdleConfig {
+	return IdleConfig{
+		Timeout:             2 * time.Minute,
+		CPUUsagePercentBase: 5.0,
+		CPUUsagePercentMax:  20.0,
+		Steps:               5,
+	}
+}
+
 // WaitUntilIdle waits until the CPU is idle, for a maximum of 120s. The CPU is
 // considered idle if the average usage over all CPU cores is less than 5%.
 // This percentage will be gradually increased to 20%, as older boards might
 // have a hard time getting below 5%.
 func WaitUntilIdle(ctx context.Context) error {
-	const (
-		// time to wait for CPU to become idle.
-		waitIdleCPUTimeout = 120 * time.Second
-		// percentage below which CPU is ideally considered idle, gradually
-		// increased up to idleCPUUsagePercentMax.
-		idleCPUUsagePercentBase = 5.0
-		// maximum percentage below which CPU is considered idle.
-		idleCPUUsagePercentMax = 20.0
-		// times we wait for CPU to become idle, idle percentage is increased each time.
-		idleCPUSteps = 5
-	)
+	return WaitUntilIdleWithConfig(ctx, DefaultIdleConfig())
+}
 
+// WaitUntilIdleWithConfig waits until the CPU is idle, based on provided configuration.
+func WaitUntilIdleWithConfig(ctx context.Context, config IdleConfig) error {
 	// Wait for the CPU to become idle. It's e.g. possible the board just booted
 	// and is running various startup programs. Some slower platforms have a
 	// hard time getting below 10% CPU usage, so we'll gradually increase the
 	// CPU idle threshold.
+	if config.Steps < 2 {
+		return errors.Errorf("invalid Steps in config: got %d; want >= 2", config.Steps)
+	}
 	var err error
 	startTime := time.Now()
-	idleIncrease := (idleCPUUsagePercentMax - idleCPUUsagePercentBase) / (idleCPUSteps - 1)
+	idleIncrease := (config.CPUUsagePercentMax - config.CPUUsagePercentBase) / (float64(config.Steps) - 1)
 	testing.ContextLogf(ctx, "Waiting for idle CPU at most %v, threshold will be gradually relaxed (from %.1f%% to %.1f%%)",
-		waitIdleCPUTimeout, idleCPUUsagePercentBase, idleCPUUsagePercentMax)
-	for i := 0; i < idleCPUSteps; i++ {
-		idlePercent := idleCPUUsagePercentBase + (idleIncrease * float64(i))
-		timeout := waitIdleCPUTimeout / idleCPUSteps
+		config.Timeout, config.CPUUsagePercentBase, config.CPUUsagePercentMax)
+	for i := 0; i < config.Steps; i++ {
+		idlePercent := config.CPUUsagePercentBase + (idleIncrease * float64(i))
+		timeout := time.Duration(config.Timeout.Seconds()/float64(config.Steps)) * time.Second
 		testing.ContextLogf(ctx, "Waiting up to %v for CPU usage to drop below %.1f%% (%d/%d)",
-			timeout.Round(time.Second), idlePercent, i+1, idleCPUSteps)
+			timeout.Round(time.Second), idlePercent, i+1, config.Steps)
 		var usage float64
 		if usage, err = waitUntilIdleStep(ctx, timeout, idlePercent); err == nil {
 			testing.ContextLogf(ctx, "Waiting for idle CPU took %v (usage: %.1f%%, threshold: %.1f%%)",
