1b1782e079fb1e973ced2d411dfe3d3bb5ccc088
Miguel Casas <mcasas@chromium.org>
2022-09-28 18:36:57 -0700

    video: enable PlatformDecoding.ffmpeg_vaapi_hevc*
    This CL extends PlatformDecoding.ffmpeg_vaapi* to hevc. The test
    vectors are the same as for the vaapi implementation. The actual
    changes are just to platform_decoding_test.go, since the other
    file is autogenerated.
    
    BUG=b:241731425, b:241733687
    TEST=on Brya, all pass except for ffmpeg_vaapi_hevc_main_bug_241731425 and
    ffmpeg_vaapi_hevc_main_bug_241733687
    
    Change-Id: Ic8877d270cec957de37da6da75000bb8ef8fe6e2

--- src/chromiumos/tast/local/bundles/cros/video/platform_decoding_test.go
+++ src/chromiumos/tast/local/bundles/cros/video/platform_decoding_test.go
@@ -1348,6 +1348,47 @@ func TestPlatformDecodingParams(t *testing.T) {
 		params = append(params, param)
 	}
 
+	// Generate ffmpeg HEVC tests.
+	for _, group := range []string{"main"} {
+		files := hevcFiles[group]
+
+		param := paramData{
+			Name:         fmt.Sprintf("ffmpeg_vaapi_hevc_%s", group),
+			Decoder:      ffmpegMD5Path,
+			CmdBuilder:   "ffmpegMD5VAAPIargs",
+			Files:        files,
+			Timeout:      defaultTimeout,
+			SoftwareDeps: []string{"vaapi", caps.HWDecodeHEVC},
+			Metadata:     genExtraData(files),
+			Attr:         []string{"graphics_video_hevc"},
+		}
+		params = append(params, param)
+	}
+
+	// Generate ffmpeg HEVC tests from bugs.
+	for _, testGroup := range []string{"main"} {
+		bugIDs := make([]string, 0, len(hevcFilesFromBugs[testGroup]))
+		// Sort the keys so the order of output of the tests is deterministic.
+		for k := range hevcFilesFromBugs[testGroup] {
+			bugIDs = append(bugIDs, k)
+		}
+		sort.Strings(bugIDs)
+		for _, bugID := range bugIDs {
+			files := hevcFilesFromBugs[testGroup][bugID]
+
+			params = append(params, paramData{
+				Name:         fmt.Sprintf("ffmpeg_vaapi_hevc_%s_bug_%s", testGroup, bugID),
+				Decoder:      ffmpegMD5Path,
+				CmdBuilder:   "ffmpegMD5VAAPIargs",
+				Files:        files,
+				Timeout:      time.Minute,
+				SoftwareDeps: []string{"vaapi", caps.HWDecodeHEVC},
+				Metadata:     genExtraData(files),
+				Attr:         []string{"graphics_video_hevc"},
+			})
+		}
+	}
+
 	code := genparams.Template(t, `{{ range . }}{
 		Name: {{ .Name | fmt }},
 		Val:  platformDecodingParams{
