69e02412d46c16369e1a32c82edb5e01d7b9cd1f
Yichen Zhou <yichenz@chromium.org>
2022-09-29 12:39:17 +0000

    lacros:Storing lacros log to the outdir of the test
    Store all lacros.log* to the outdir of the test when the lacros
    instance closes.
    
    BUG=b:249538698
    TEST=tast run <IP> ui.VideoCUJ.lacros and verify there
         is the lacros/lacros.log* file in tests directory.
    
    Change-Id: Iba8e136d43008528d47728e7104cd899e376f679

--- src/chromiumos/tast/local/chrome/lacros/lacros.go
+++ src/chromiumos/tast/local/chrome/lacros/lacros.go
@@ -6,16 +6,21 @@ package lacros
 
 import (
 	"context"
+	"os"
+	"path/filepath"
+	"regexp"
 	"time"
 
 	"android.googlesource.com/platform/external/perfetto/protos/perfetto/trace/github.com/google/perfetto/perfetto_proto"
 	"github.com/mafredri/cdp/protocol/target"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/fsutil"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/browser"
 	"chromiumos/tast/local/chrome/internal/cdputil"
 	"chromiumos/tast/local/chrome/internal/driver"
+	"chromiumos/tast/local/chrome/internal/lacros"
 	"chromiumos/tast/local/chrome/jslog"
 	"chromiumos/tast/local/chrome/lacros/lacrosinfo"
 	"chromiumos/tast/testing"
@@ -69,6 +74,31 @@ func (l *Lacros) CloseResources(ctx context.Context) {
 
 // Close closes all lacros chrome targets and the dev session.
 func (l *Lacros) Close(ctx context.Context) error {
+	// Save lacros.log* to outDir.
+	if outDir, ok := testing.ContextOutDir(ctx); ok {
+		dstDir := filepath.Join(outDir, "lacros_logs")
+		if err := os.MkdirAll(dstDir, 0755); err != nil {
+			testing.ContextLog(ctx, "Error creating lacros log directory: ", err)
+		}
+
+		re := regexp.MustCompile("lacros.log(.*)")
+		filepath.Walk(lacros.UserDataDir, func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() && info.Name() != "lacros" {
+				return filepath.SkipDir
+			}
+			if re.MatchString(info.Name()) {
+				if err := fsutil.CopyFile(path, filepath.Join(dstDir, info.Name())); err != nil {
+					testing.ContextLogf(ctx, "Failed to save %s to %s: %v", info.Name(), dstDir, err)
+				} else {
+					testing.ContextLogf(ctx, "%s saved to %s", info.Name(), dstDir)
+				}
+			}
+			return nil
+		})
+	} else {
+		testing.ContextLog(ctx, "No output directory exists, not saving lacros log file")
+	}
+
 	// Get all pages. Note that we can't get all targets, because one of them
 	// will be the test extension or devtools and we don't want to kill that.
 	// Further note that this will mean pages are not restored, compared to killing
