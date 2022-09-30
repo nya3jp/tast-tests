493ecf327b88fe39a81aa9b4660f2f6336235183
Nigel Tao <nigeltao@chromium.org>
2022-09-30 16:29:47 +1000

    fusebox: add tmpfs-backed tast test
    Fusebox talks to the Linux kernel (via FUSE) and Chrome (via D-Bus).
    Prior to this commit, the "built_in/fuse_status" test only exercised the
    FUSE side. After this commit, both sides are exercised.
    
    Exercising the D-Bus side requires Chrome (the D-Bus server for
    org.chromium.FuseBoxService) to be running, so the tast test now
    requires Chrome in its SoftwareDeps (and calling "chrome.New(ctx)" will
    log in) and no longer has to explicitly manage cros-disks (since Chrome
    does that).
    
    (For software archaeologists, an earlier, now-deleted tast test also
    tickled the org.chromium.FuseBoxReverseService interface, and note the
    "Reverse", but the o.c.FBRS service was hosted by the Fusebox daemon,
    not Chrome, so it was still only exercising the Fusebox-Linux half of
    the two connections.)
    
    This is one half of a multi-repo change. The other half is at
    https://crrev.com/c/3925348
    
    BUG=chromium:1249754
    TEST=tast run root@DUT platform.Fusebox
    
    Change-Id: Ib94e34daf6f0af7796ed446b0b7592cf87091f28

---
+++ src/chromiumos/tast/local/bundles/cros/filemanager/fusebox.go
@@ -0,0 +1,93 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package filemanager
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Fusebox,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Mount fusebox daemon and verify it responds to requests",
+		Contacts: []string{
+			"benreich@chromium.org",
+			"nigeltao@chromium.org",
+			"noel@chromium.org",
+			"chromeos-files-syd@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+	})
+}
+
+func Fusebox(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 30*time.Second)
+	defer cancel()
+
+	// Logging into Chrome should launch Fusebox (via cros-disks).
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Cannot start Chrome: ", err)
+	}
+
+	// Poll until the "fuse_status" file shows up. The "fuse_status" and "ok\n"
+	// magic strings are defined in "platform2/fusebox/built_in.cc".
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		const fuseStatusFilename = "/media/fuse/fusebox/built_in/fuse_status"
+		if got, err := os.ReadFile(fuseStatusFilename); err != nil {
+			return err
+		} else if want := "ok\n"; string(got) != want {
+			return testing.PollBreak(errors.Errorf("got %q, want %q", got, want))
+		}
+		return nil
+	}, nil); err != nil {
+		s.Fatal("ReadFile(fuse_status) failed: ", err)
+	}
+
+	// Make a temporary directory.
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Creating test API connection failed: ", err)
+	}
+	tdd := tempDirData{}
+	if err := tconn.Call(ctx, &tdd, `tast.promisify(chrome.autotestPrivate.makeFuseboxTempDir)`); err != nil {
+		s.Fatal("makeFuseboxTempDir failed: ", err)
+	} else if tdd.FuseboxFilePath == "" {
+		s.Fatal("FuseboxFilePath is empty")
+	}
+	defer tconn.Call(cleanupCtx, nil, `chrome.autotestPrivate.removeFuseboxTempDir`, tdd.FuseboxFilePath)
+
+	// That temporary directory has two names: a fusebox one at
+	// "/media/fuse/fusebox/tmp.foo" and an underlying one at "/tmp/.foo".
+	// Creating "hello.txt" in the second should be visible in the first.
+	// TODO(crbug.com/1360740): and vice versa.
+	const helloTxt = "hello.txt"
+	const world = "world\n"
+	helloFuseboxFilename := filepath.Join(tdd.FuseboxFilePath, helloTxt)
+	helloUnderlyingFilename := filepath.Join(tdd.UnderlyingFilePath, helloTxt)
+	if err := os.WriteFile(helloUnderlyingFilename, []byte(world), 0777); err != nil {
+		s.Fatal("WriteFile(hello.txt) failed: ", err)
+	} else if got, err := os.ReadFile(helloFuseboxFilename); err != nil {
+		s.Fatal("ReadFile(hello.txt) failed: ", err)
+	} else if string(got) != world {
+		s.Fatalf("ReadFile(hello.txt): got %q, want %q", got, world)
+	}
+}
+
+type tempDirData struct {
+	FuseboxFilePath    string `json:"fuseboxFilePath"`
+	UnderlyingFilePath string `json:"underlyingFilePath"`
+}
