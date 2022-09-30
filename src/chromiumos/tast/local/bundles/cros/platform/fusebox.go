00aa4047b5a45de4f3564a2098c533a21ba0add5
Nigel Tao <nigeltao@chromium.org>
2022-09-30 16:29:47 +1000

    fusebox: add tmpfs-backed tast test
    Fusebox talks to the Linux kernel (via FUSE) and Chrome (via D-Bus). Prior to
    this commit, the "built_in/fuse_status" test only exercised the FUSE side.
    After this commit, both sides are exercised.
    
    Exercising the D-Bus side requires Chrome (the D-Bus server for
    org.chromium.FuseBoxService) to be running, so the tast test now requires
    Chrome in its SoftwareDeps (and calling "chrome.New(ctx)" will log in) and no
    longer has to explicitly manage cros-disks (since Chrome does that).
    
    (For software archaeologists, an earlier, now-deleted tast test also tickled
    the org.chromium.FuseBoxReverseService interface, and note the "Reverse", but
    the o.c.FBRS service was hosted by the Fusebox daemon, not Chrome, so it was
    still only exercising the Fusebox-Linux half of the two connections.)
    
    This is one half of a multi-repo change. The other half is at
    https://crrev.com/c/3925348
    
    BUG=chromium:1249754
    TEST=tast run root@DUT platform.Fusebox
    
    Change-Id: Ib94e34daf6f0af7796ed446b0b7592cf87091f28

--- src/chromiumos/tast/local/bundles/cros/platform/fusebox.go
+++ src/chromiumos/tast/local/bundles/cros/platform/fusebox.go
@@ -8,74 +8,84 @@ import (
 	"context"
 	"os"
 	"path/filepath"
-	"syscall"
 	"time"
 
 	"chromiumos/tast/ctxutil"
-	"chromiumos/tast/local/crosdisks"
-	"chromiumos/tast/local/procutil"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func: Fusebox,
-		Desc: "Mount fusebox daemon and verify it responds to requests",
+		Func:         Fusebox,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Mount fusebox daemon and verify it responds to requests",
 		Contacts: []string{
 			"noel@chromium.org",
 			"benreich@chromium.org",
 			"nigeltao@chromium.org",
 			"chromeos-files-syd@google.com",
 		},
-		Attr: []string{"group:mainline"},
+		Attr:         []string{"group:mainline"},
+		SoftwareDeps: []string{"chrome"},
 	})
 }
 
 func Fusebox(ctx context.Context, s *testing.State) {
-	cleanupCtx := ctx
-	ctx, cancel := ctxutil.Shorten(ctx, 5*time.Second)
+	ctx, cancel := ctxutil.Shorten(ctx, 30*time.Second)
 	defer cancel()
 
-	terminateDaemonIfNeeded := func(daemon string) {
-		if proc, err := procutil.FindUnique(procutil.ByExe(daemon)); err == nil {
-			testing.ContextLog(ctx, "Terminating existing daemon")
-			if err = proc.SendSignal(syscall.SIGTERM); err != nil {
-				testing.ContextLog(ctx, "Failed to terminate daemon: ", err)
-			}
+	// Logging into Chrome should launch Fusebox (via cros-disks).
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Cannot start Chrome: ", err)
+	}
+
+	// Poll until the "fuse_status" file shows up. The "fuse_status" and "ok\n"
+	// magic strings are defined in "platform2/fusebox/built_in.cc".
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		const fuseStatusFilename = "/media/fuse/fusebox/built_in/fuse_status"
+		if got, err := os.ReadFile(fuseStatusFilename); err != nil {
+			return err
+		} else if want := "ok\n"; string(got) != want {
+			return errors.Errorf("got %q, want %q", got, want)
 		}
+		return nil
+	}, nil); err != nil {
+		s.Fatal("ReadFile(fuse_status) failed: ", err)
 	}
 
-	terminateDaemonIfNeeded("/usr/bin/fusebox")
-
-	cd, err := crosdisks.New(ctx)
+	// Make a temporary directory.
+	tconn, err := cr.TestAPIConn(ctx)
 	if err != nil {
-		s.Fatal("Failed to connect to CrosDisks service: ", err)
+		s.Fatal("Creating test API connection failed: ", err)
 	}
-	defer cd.Close()
+	tdd := tempDirData{}
+	if err := tconn.Call(ctx, &tdd, `tast.promisify(chrome.autotestPrivate.makeFuseboxTempDir)`); err != nil {
+		s.Fatal("makeFuseboxTempDir failed: ", err)
+	} else if tdd.FuseboxFilePath == "" {
+		s.Fatal("FuseboxFilePath is empty")
+	}
+	defer tconn.Call(ctx, nil, `chrome.autotestPrivate.removeFuseboxTempDir`, tdd.FuseboxFilePath)
 
-	w, err := cd.WatchMountCompleted(ctx)
-	if err != nil {
-		s.Fatal("Failed to create MountCompleted event watcher: ", err)
+	// That temporary directory has two names: a fusebox one at
+	// "/media/fuse/fusebox/tmp.foo" and an underlying one at "/tmp/.foo".
+	// Creating "hello.txt" in the second should be visible in the first.
+	// TODO(crbug.com/1360740): and vice versa.
+	const world = "world\n"
+	helloFuseboxFilename := filepath.Join(tdd.FuseboxFilePath, "hello.txt")
+	helloUnderlyingFilename := filepath.Join(tdd.UnderlyingFilePath, "hello.txt")
+	if err := os.WriteFile(helloUnderlyingFilename, []byte(world), 0777); err != nil {
+		s.Fatal("WriteFile(hello.txt) failed: ", err)
+	} else if got, err := os.ReadFile(helloFuseboxFilename); err != nil {
+		s.Fatal("ReadFile(hello.txt) failed: ", err)
+	} else if string(got) != world {
+		s.Fatalf("ReadFile(hello.txt): got %q, want %q", got, world)
 	}
-	defer w.Close(cleanupCtx)
+}
 
-	const source = "fusebox://fusebox-basic-test"
-	if err := cd.Mount(ctx, source, "fusebox", nil); err != nil {
-		s.Fatal("CrosDisks Mount call failed: ", err)
-	}
-	defer cd.Unmount(cleanupCtx, source, nil)
-
-	m, err := w.Wait(ctx)
-	if err != nil {
-		s.Fatal("CrosDisks MountCompleted event failed: ", err)
-	}
-
-	// The "fuse_status" and "ok\n" magic strings are defined in
-	// "platform2/fusebox/built_in.cc".
-	fuseStatusFilename := filepath.Join(m.MountPath, "built_in/fuse_status")
-	if got, err := os.ReadFile(fuseStatusFilename); err != nil {
-		s.Fatal("ReadFile failed: ", err)
-	} else if want := "ok\n"; string(got) != want {
-		s.Fatalf("ReadFile: got %q, want %q", got, want)
-	}
+type tempDirData struct {
+	FuseboxFilePath    string `json:"fuseboxFilePath"`
+	UnderlyingFilePath string `json:"underlyingFilePath"`
 }
