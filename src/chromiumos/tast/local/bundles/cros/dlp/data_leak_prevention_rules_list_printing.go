e59d8fdf21677e484ffe5bc9a446250b5f405a30
Marc Grimme <marcgrimme@google.com>
2022-09-30 09:04:04 +0200

    Remove unnecessary external dep in tast test
    Remove dependency to chromium.com and replace with localserver to deflake test.
    Also add WaitForQuiescence to stabilize on slow devices.
    
    BUG=b:249747208
    TEST=tast -verbose run -build=true dut_vm dlp.DataLeakPreventionRulesListPrinting.*
    
    Change-Id: I9c7abacf6fcbd3c023647f4f606a685ad1f455fc

--- src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_printing.go
+++ src/chromiumos/tast/local/bundles/cros/dlp/data_leak_prevention_rules_list_printing.go
@@ -6,6 +6,11 @@ package dlp
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"path"
 	"time"
 
 	"chromiumos/tast/common/fixture"
@@ -22,6 +27,7 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/chrome/webutil"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/testing"
@@ -31,55 +37,66 @@ import (
 var printDialog = nodewith.Name("Print").HasClass("RootView").Role(role.Window)
 
 type printingTestParams struct {
-	name        string
-	url         string
-	restriction restrictionlevel.RestrictionLevel
-	policyDLP   []policy.Policy
-	browserType browser.Type
+	name                    string
+	path                    string
+	restriction             restrictionlevel.RestrictionLevel
+	browserType             browser.Type
+	waitTimeSecNotification time.Duration
+	waitTimeSecWarning      time.Duration
 }
 
-// DLP policy with printing blocked restriction.
-var blockPolicy = []policy.Policy{&policy.DataLeakPreventionRulesList{
-	Val: []*policy.DataLeakPreventionRulesListValue{
-		{
-			Name:        "Disable printing of confidential content",
-			Description: "User should not be able to print confidential content",
-			Sources: &policy.DataLeakPreventionRulesListValueSources{
-				Urls: []string{
-					"example.com",
+const (
+	dlpPrintingBlockedPath = "/blocked"
+	dlpPrintingAllowedPath = "/allowed"
+	dlpPrintingWarnPath    = "/warn"
+)
+
+// printingBlockPolicy returns policy that blocks printing based on given URL.
+func printingBlockPolicy(url string) []policy.Policy {
+	return []policy.Policy{&policy.DataLeakPreventionRulesList{
+		Val: []*policy.DataLeakPreventionRulesListValue{
+			{
+				Name:        "Disable printing of confidential content",
+				Description: "User should not be able to print confidential content",
+				Sources: &policy.DataLeakPreventionRulesListValueSources{
+					Urls: []string{
+						url,
+					},
 				},
-			},
-			Restrictions: []*policy.DataLeakPreventionRulesListValueRestrictions{
-				{
-					Class: "PRINTING",
-					Level: "BLOCK",
+				Restrictions: []*policy.DataLeakPreventionRulesListValueRestrictions{
+					{
+						Class: "PRINTING",
+						Level: "BLOCK",
+					},
 				},
 			},
 		},
 	},
-},
+	}
 }
 
-// DLP policy with printing warn restriction.
-var warnPolicy = []policy.Policy{&policy.DataLeakPreventionRulesList{
-	Val: []*policy.DataLeakPreventionRulesListValue{
-		{
-			Name:        "Warn before printing confidential content",
-			Description: "User should be warned before printing confidential content",
-			Sources: &policy.DataLeakPreventionRulesListValueSources{
-				Urls: []string{
-					"example.com",
+// printingWarnPolicy returns policy that warns in case of printing based on given URL.
+func printingWarnPolicy(url string) []policy.Policy {
+	return []policy.Policy{&policy.DataLeakPreventionRulesList{
+		Val: []*policy.DataLeakPreventionRulesListValue{
+			{
+				Name:        "Warn before printing confidential content",
+				Description: "User should be warned before printing confidential content",
+				Sources: &policy.DataLeakPreventionRulesListValueSources{
+					Urls: []string{
+						url,
+					},
 				},
-			},
-			Restrictions: []*policy.DataLeakPreventionRulesListValueRestrictions{
-				{
-					Class: "PRINTING",
-					Level: "WARN",
+				Restrictions: []*policy.DataLeakPreventionRulesListValueRestrictions{
+					{
+						Class: "PRINTING",
+						Level: "WARN",
+					},
 				},
 			},
 		},
 	},
-},
+	}
 }
 
 func init() {
@@ -97,44 +114,48 @@ func init() {
 			Name:    "ash_blocked",
 			Fixture: fixture.ChromePolicyLoggedIn,
 			Val: printingTestParams{
-				name:        "blocked",
-				url:         "https://www.example.com/",
-				restriction: restrictionlevel.Blocked,
-				policyDLP:   blockPolicy,
-				browserType: browser.TypeAsh,
+				name:                    "blocked",
+				path:                    dlpPrintingBlockedPath,
+				restriction:             restrictionlevel.Blocked,
+				browserType:             browser.TypeAsh,
+				waitTimeSecNotification: 10,
+				waitTimeSecWarning:      2,
 			},
 		}, {
 			Name:      "ash_allowed",
 			ExtraAttr: []string{"informational"},
 			Fixture:   fixture.ChromePolicyLoggedIn,
 			Val: printingTestParams{
-				name:        "allowed",
-				url:         "https://www.chromium.com/",
-				restriction: restrictionlevel.Allowed,
-				policyDLP:   blockPolicy,
-				browserType: browser.TypeAsh,
+				name:                    "allowed",
+				path:                    dlpPrintingAllowedPath,
+				restriction:             restrictionlevel.Allowed,
+				browserType:             browser.TypeAsh,
+				waitTimeSecNotification: 10,
+				waitTimeSecWarning:      2,
 			},
 		}, {
 			Name:      "ash_warn_proceeded",
 			ExtraAttr: []string{"informational"},
 			Fixture:   fixture.ChromePolicyLoggedIn,
 			Val: printingTestParams{
-				name:        "warn_proceded",
-				url:         "https://www.example.com/",
-				restriction: restrictionlevel.WarnProceeded,
-				policyDLP:   warnPolicy,
-				browserType: browser.TypeAsh,
+				name:                    "warn_proceded",
+				path:                    dlpPrintingWarnPath,
+				restriction:             restrictionlevel.WarnProceeded,
+				browserType:             browser.TypeAsh,
+				waitTimeSecNotification: 10,
+				waitTimeSecWarning:      2,
 			},
 		}, {
 			Name:      "ash_warn_cancelled",
 			ExtraAttr: []string{"informational"},
 			Fixture:   fixture.ChromePolicyLoggedIn,
 			Val: printingTestParams{
-				name:        "warn_cancelled",
-				url:         "https://www.example.com/",
-				restriction: restrictionlevel.WarnCancelled,
-				policyDLP:   warnPolicy,
-				browserType: browser.TypeAsh,
+				name:                    "warn_cancelled",
+				path:                    dlpPrintingWarnPath,
+				restriction:             restrictionlevel.WarnCancelled,
+				browserType:             browser.TypeAsh,
+				waitTimeSecNotification: 10,
+				waitTimeSecWarning:      2,
 			},
 		}, {
 			Name:              "lacros_blocked",
@@ -142,11 +163,12 @@ func init() {
 			ExtraSoftwareDeps: []string{"lacros"},
 			Fixture:           fixture.LacrosPolicyLoggedIn,
 			Val: printingTestParams{
-				name:        "blocked",
-				url:         "https://www.example.com/",
-				restriction: restrictionlevel.Blocked,
-				policyDLP:   blockPolicy,
-				browserType: browser.TypeLacros,
+				name:                    "blocked",
+				path:                    dlpPrintingBlockedPath,
+				restriction:             restrictionlevel.Blocked,
+				browserType:             browser.TypeLacros,
+				waitTimeSecNotification: 10,
+				waitTimeSecWarning:      5,
 			},
 		}, {
 			Name:              "lacros_allowed",
@@ -154,11 +176,12 @@ func init() {
 			ExtraSoftwareDeps: []string{"lacros"},
 			Fixture:           fixture.LacrosPolicyLoggedIn,
 			Val: printingTestParams{
-				name:        "allowed",
-				url:         "https://www.chromium.com/",
-				restriction: restrictionlevel.Allowed,
-				policyDLP:   blockPolicy,
-				browserType: browser.TypeLacros,
+				name:                    "allowed",
+				path:                    dlpPrintingAllowedPath,
+				restriction:             restrictionlevel.Allowed,
+				browserType:             browser.TypeLacros,
+				waitTimeSecNotification: 10,
+				waitTimeSecWarning:      5,
 			},
 		}, {
 			Name:              "lacros_warn_proceeded",
@@ -166,11 +189,12 @@ func init() {
 			ExtraSoftwareDeps: []string{"lacros"},
 			Fixture:           fixture.LacrosPolicyLoggedIn,
 			Val: printingTestParams{
-				name:        "warn_proceeded",
-				url:         "https://www.example.com/",
-				restriction: restrictionlevel.WarnProceeded,
-				policyDLP:   warnPolicy,
-				browserType: browser.TypeLacros,
+				name:                    "warn_proceeded",
+				path:                    dlpPrintingWarnPath,
+				restriction:             restrictionlevel.WarnProceeded,
+				browserType:             browser.TypeLacros,
+				waitTimeSecNotification: 10,
+				waitTimeSecWarning:      5,
 			},
 		}, {
 			Name:              "lacros_warn_cancelled",
@@ -178,11 +202,12 @@ func init() {
 			ExtraSoftwareDeps: []string{"lacros"},
 			Fixture:           fixture.LacrosPolicyLoggedIn,
 			Val: printingTestParams{
-				name:        "warn_cancelled",
-				url:         "https://www.example.com/",
-				restriction: restrictionlevel.WarnCancelled,
-				policyDLP:   warnPolicy,
-				browserType: browser.TypeLacros,
+				name:                    "warn_cancelled",
+				path:                    dlpPrintingWarnPath,
+				restriction:             restrictionlevel.WarnCancelled,
+				browserType:             browser.TypeLacros,
+				waitTimeSecNotification: 10,
+				waitTimeSecWarning:      5,
 			},
 		}},
 	})
@@ -202,15 +227,32 @@ func DataLeakPreventionRulesListPrinting(ctx context.Context, s *testing.State) 
 		s.Fatal("Failed to connect to test API: ", err)
 	}
 
+	// Setup test HTTP server.
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello DLP client you navigated to ", r.URL.Path)
+	}))
+	defer server.Close()
+
 	keyboard, err := input.VirtualKeyboard(ctx)
 	if err != nil {
 		s.Fatal("Failed to get keyboard: ", err)
 	}
 	defer keyboard.Close()
 
+	// Construct URL to pass to test and policy.
+	testURL, err := url.Parse(server.URL)
+	if err != nil {
+		s.Fatal("Failed to parse url: ", err)
+	}
+	testURL.Path = path.Join(testURL.Path, s.Param().(printingTestParams).path)
+
 	// Update the policy blob.
 	pb := policy.NewBlob()
-	pb.AddPolicies(s.Param().(printingTestParams).policyDLP)
+	if s.Param().(printingTestParams).restriction == restrictionlevel.Blocked {
+		pb.AddPolicies(printingBlockPolicy(testURL.String()))
+	} else if s.Param().(printingTestParams).restriction == restrictionlevel.WarnCancelled || s.Param().(printingTestParams).restriction == restrictionlevel.WarnProceeded {
+		pb.AddPolicies(printingWarnPolicy(testURL.String()))
+	}
 
 	// Update policy.
 	if err := policyutil.ServeBlobAndRefresh(ctx, fakeDMS, cr, pb); err != nil {
@@ -223,7 +265,7 @@ func DataLeakPreventionRulesListPrinting(ctx context.Context, s *testing.State) 
 	}
 	defer closeBrowser(cleanupCtx)
 
-	conn, err := br.NewConn(ctx, s.Param().(printingTestParams).url)
+	conn, err := br.NewConn(ctx, testURL.String())
 	if err != nil {
 		s.Fatal("Failed to open page: ", err)
 	}
@@ -231,26 +273,30 @@ func DataLeakPreventionRulesListPrinting(ctx context.Context, s *testing.State) 
 
 	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree_"+s.Param().(printingTestParams).name)
 
+	// Type the shortcut.
+	if err := keyboard.Accel(ctx, "Ctrl+P"); err != nil {
+		s.Fatal("Failed to type printing hotkey: ", err)
+	}
+	// Waiting for UI to be stable so that we can be sure we find UI components if necessary.
+	if err := webutil.WaitForQuiescence(ctx, conn, s.Param().(printingTestParams).waitTimeSecWarning*time.Second); err != nil {
+		s.Fatalf("Failed to wait for %q to achieve quiescence: %v", testURL, err)
+	}
+
 	// Make a call to print page.
-	if err := testPrinting(ctx, tconn, keyboard, s.Param().(printingTestParams).restriction); err != nil {
+	if err := testPrinting(ctx, tconn, keyboard, s.Param().(printingTestParams).restriction, s.Param().(printingTestParams).waitTimeSecWarning*time.Second); err != nil {
 		s.Fatal("Failed to run test body: ", err)
 	}
 
 	// Confirm that the notification only appeared if expected.
 	if s.Param().(printingTestParams).restriction == restrictionlevel.Blocked {
-		if _, err := ash.WaitForNotification(ctx, tconn, 15*time.Second, ash.WaitIDContains("print_dlp_blocked"), ash.WaitTitle("Printing is blocked")); err != nil {
+		if _, err := ash.WaitForNotification(ctx, tconn, s.Param().(printingTestParams).waitTimeSecNotification*time.Second, ash.WaitIDContains("print_dlp_blocked"), ash.WaitTitle("Printing is blocked")); err != nil {
 			s.Error("Failed to wait for notification with title 'Printing is blocked': ", err)
 		}
 	}
 }
 
 // testPrinting tests whether printing is possible via hotkey (Ctrl + P).
-func testPrinting(ctx context.Context, tconn *chrome.TestConn, keyboard *input.KeyboardEventWriter, restriction restrictionlevel.RestrictionLevel) error {
-	// Type the shortcut.
-	if err := keyboard.Accel(ctx, "Ctrl+P"); err != nil {
-		return errors.Wrap(err, "failed to type printing hotkey")
-	}
-
+func testPrinting(ctx context.Context, tconn *chrome.TestConn, keyboard *input.KeyboardEventWriter, restriction restrictionlevel.RestrictionLevel, waitTime time.Duration) error {
 	if restriction == restrictionlevel.WarnProceeded {
 		// Hit Enter, which is equivalent to clicking on the "Print anyway" button.
 		if err := keyboard.Accel(ctx, "Enter"); err != nil {
@@ -269,11 +315,11 @@ func testPrinting(ctx context.Context, tconn *chrome.TestConn, keyboard *input.K
 	ui := uiauto.New(tconn)
 
 	if restriction == restrictionlevel.Allowed || restriction == restrictionlevel.WarnProceeded {
-		if err := ui.WithTimeout(5 * time.Second).WaitUntilExists(printDialog)(ctx); err != nil {
+		if err := ui.WithTimeout(waitTime).WaitUntilExists(printDialog)(ctx); err != nil {
 			return errors.Wrap(err, "failed to find the printing window")
 		}
 	} else {
-		if err := ui.EnsureGoneFor(printDialog, 5*time.Second)(ctx); err != nil {
+		if err := ui.EnsureGoneFor(printDialog, waitTime)(ctx); err != nil {
 			return errors.Wrap(err, "should not show the printing window")
 		}
 	}
