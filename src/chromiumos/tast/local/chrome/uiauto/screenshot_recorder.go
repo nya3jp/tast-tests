59821e4116bb086c105c6551b87429f9f025adb2
Navin Ramsaroop <ramsaroop@google.com>
2022-09-30 20:07:59 +0000

    tast-tests: Create utility that takes screenshots at fixed intervals
    Create a ScreenshotRecorder that takes screenshots of the of the device
    throughout the execution of cujrecorder.Recorder.Run. The utility also
    takes a screenshot at the time Stop is called, to ensure we see the
    state of the device at the end of the test. Each screenshot is stored
    as a jpg file and zipped to lessen their file sizes.
    
    This utility is currently only active on ui.TabSwitchCUJ*, to ensure
    that it works properly before expanding it to other tests.
    
    BUG=b:240575502
    TEST=tast run <DUT> ui.MeetCUJ*, and ensure that the only new file in
    the out directory is test-screenshots.zip.
    
    Change-Id: I29f9350d14f5fd6cc328ffd44c9bf4dedbbcbb51

---
+++ src/chromiumos/tast/local/chrome/uiauto/screenshot_recorder.go
@@ -0,0 +1,220 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package uiauto
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+// zipName is the name of the zipfile that is created in by |compress|.
+const zipName = "test-screenshots"
+
+// ScreenshotRecorder is a utility used to take screenshots at
+// specified intervals during a test.
+type ScreenshotRecorder struct {
+	// recording is whether or not the recorder has started recording.
+	recording bool
+
+	// interval is the delay between each screenshot.
+	interval time.Duration
+
+	// lastErr is the most recent error that occurred in the recorder.
+	lastErr error
+
+	// outDir is the overall testing out directory. The eventual zip
+	// file will be stored here.
+	outDir string
+
+	// startTime is the time the |Start| function is called.
+	startTime time.Time
+
+	// numImages is the number of images that have already been taken.
+	numImages int
+
+	// maxImages is the maximum number of images this recorder is
+	// allowed to take, not including the end image.  Setting this
+	// value is important to ensure tests that are hanging for much
+	// longer than expected don't take unexpected number of screenshots.
+	maxImages int
+
+	// List of the file paths for the as-of-yet uncompressed images.
+	// Once |compress| is called, this list is cleared.
+	uncompressed []string
+
+	stopc    chan struct{}
+	stopackc chan struct{}
+}
+
+// NewScreenshotRecorder creates a ScreenshotRecorder that can take
+// screenshots of the display at the specified |interval|. After
+// creating the recorder, you could start recording by calling
+// |Start|, and stop recording by calling |Stop|.
+func NewScreenshotRecorder(ctx context.Context, interval time.Duration, maxImages int) (*ScreenshotRecorder, error) {
+	if maxImages <= 0 {
+		return nil, errors.Errorf("cannot create screenshot recorder with %d max images", maxImages)
+	}
+
+	dir, ok := testing.ContextOutDir(ctx)
+	if !ok || dir == "" {
+		return nil, errors.New("failed to get the out directory")
+	}
+
+	return &ScreenshotRecorder{
+		interval:  interval,
+		outDir:    dir,
+		maxImages: maxImages,
+		stopc:     make(chan struct{}),
+		stopackc:  make(chan struct{}),
+	}, nil
+}
+
+// Start starts the screenshot recorder. The first screenshot is taken
+// after |interval| has passed. This function cannot be called on an
+// already started recorder without |Stop| being called first.
+func (r *ScreenshotRecorder) Start(ctx context.Context) error {
+	if r.recording {
+		return errors.New("screenshot recording has already started")
+	}
+	r.recording = true
+
+	r.startTime = time.Now()
+
+	testing.ContextLog(ctx, "screenshot_recorder: Taking a screenshot every ", r.interval)
+	go func() {
+		done := false
+		for !done {
+			select {
+			case <-time.After(r.interval):
+				r.lastErr = r.captureDisplay(ctx, "")
+				if r.lastErr != nil {
+					testing.ContextLog(ctx, "screenshot_recorder: Failed to take screenshot: ", r.lastErr)
+					done = true
+					break
+				}
+
+				if r.numImages < r.maxImages {
+					break
+				}
+
+				testing.ContextLogf(ctx, "screenshot_recorder: The max number of screenshots have been taken (%d); will not take any more", r.maxImages)
+				done = true
+
+				// Since the max images have been taken, compress now.
+				if err := r.compress(ctx); err != nil {
+					testing.ContextLog(ctx, "screenshot_recorder: Failed to compress the screenshots: ", err)
+				}
+			case <-r.stopc:
+				testing.ContextLog(ctx, "screenshot_recorder: Background signaled to stop")
+				done = true
+			case <-ctx.Done():
+				done = true
+			}
+		}
+
+		// Let the foreground task know we are done.
+		close(r.stopackc)
+	}()
+
+	return nil
+}
+
+// Stop stops the screenshot recorder. It takes one last picture that
+// does not count towards the |r.maxImages| count. It also compresses
+// any uncompressed screenshots into a zip file. This function can only
+// be called after |Start| has been called.
+func (r *ScreenshotRecorder) Stop(ctx context.Context) error {
+	if !r.recording {
+		return errors.New("start recording wasn't called before stop recording")
+	}
+
+	// Send stop message to screenshot routine.
+	close(r.stopc)
+
+	// Wait for confirmation that the screenshot recorder has stopped.
+	var ctxIsFinished bool
+	select {
+	case <-time.After(30 * time.Second):
+		return errors.New("timed out waiting for screenshot recorder to stop")
+	case <-r.stopackc:
+		testing.ContextLog(ctx, "screenshot_recorder: Screenshot recorder stopped successfully")
+		break
+	case <-ctx.Done():
+		ctxIsFinished = true
+		break
+	}
+
+	if r.lastErr != nil {
+		return errors.Wrap(r.lastErr, "screenshot recording failed")
+	}
+
+	if !ctxIsFinished {
+		// Take a final screenshot. Append file name with "-end" to
+		// explain why this screenshot was taken at a different
+		// interval than the other screenshots.
+		if err := r.captureDisplay(ctx, "-end"); err != nil {
+			return errors.Wrap(err, "failed to take one last screenshot")
+		}
+	}
+
+	if err := r.compress(ctx); err != nil {
+		return errors.Wrap(err, "failed to compress screenshots")
+	}
+
+	return nil
+}
+
+// compress compresses all currently uncompressed screenshots into a
+// zip file |zipName|. Any successive calls to this function will
+// update the original zip file to include any new screenshots taken
+// since the last compression.
+func (r *ScreenshotRecorder) compress(ctx context.Context) error {
+	// Create the command to zip the files. Command follows this format:
+	// zip -9 -m -j /path/to/test-screenshots.zip <path-to-screenshot-1> <path-to-screenshot-2>
+	//
+	// -9 sets the highest compression rate to lessen the zip size.
+	// -m deletes the original uncompressed files.
+	// -j keeps only the screenshot names (and not the whole path) in
+	// the zip file.
+	cmd := testexec.CommandContext(ctx, "zip", append(
+		[]string{"-9", "-m", "-j", filepath.Join(r.outDir, zipName)},
+		r.uncompressed...,
+	)...)
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		return errors.Errorf("failed running %q", strings.Join(cmd.Args, " "))
+	}
+
+	// Clear the list of uncompressed screenshots.
+	r.uncompressed = []string{}
+
+	return nil
+}
+
+// captureDisplay takes a screenshot of the active display and saves it
+// to a file with the following format:
+// <iteration>-<number of seconds since start>-<optional suffix>.jpg
+// i.e. 1-10.jpg, 1-20-end.jpg
+func (r *ScreenshotRecorder) captureDisplay(ctx context.Context, fileSuffix string) error {
+	testing.ContextLogf(ctx, "screenshot_recorder: Will take screenshot #%d", r.numImages+1)
+
+	secondsSinceStart := int(time.Now().Sub(r.startTime).Seconds())
+	path := filepath.Join(r.outDir, fmt.Sprintf("%d-%d%s.jpg", r.numImages+1, secondsSinceStart, fileSuffix))
+
+	r.uncompressed = append(r.uncompressed, path)
+
+	cmd := testexec.CommandContext(ctx, "screenshot", path)
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		return errors.Errorf("failed running %q", strings.Join(cmd.Args, " "))
+	}
+	r.numImages++
+	return nil
+}
