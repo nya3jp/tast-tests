dcc5d4595f3e48241b53cef1ede8a1b8802ee427
Craig Hesling <hesling@chromium.org>
2022-09-30 21:00:16 -0400

    biod: Add test that checks minimal /dev/cros_fp permissions
    Note that we did not hard code the correct file group.
    We only check that the file group is non-root.
    
    BUG=b:241864458, b:194926925, b:248610343
    TEST=tast run localhost:2222 biod.DevicePermissions
    
    Change-Id: Ib69922bf0e24bbd19ad3f4d705142c579e945f31

---
+++ src/chromiumos/tast/local/bundles/cros/biod/device_permissions.go
@@ -0,0 +1,61 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package biod
+
+import (
+	"context"
+
+	"golang.org/x/sys/unix"
+
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: DevicePermissions,
+		Desc: "Checks /dev/cros_fp's permissions and owner/group",
+		Contacts: []string{
+			"hesling@chromium.org",
+			"chromeos-fingerprint@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"biometrics_daemon"},
+		HardwareDeps: hwdep.D(hwdep.Fingerprint()),
+	})
+}
+
+// DevicePermissions checks that /dev/cros_fp has the correct file permission
+// and a reasonable owner/group.
+//
+// We don't check what exact group owns the device, but we do ensure that it
+// isn't root.
+func DevicePermissions(ctx context.Context, s *testing.State) {
+	var info unix.Stat_t
+	err := unix.Stat("/dev/cros_fp", &info)
+	if err != nil {
+		s.Fatal("Failed to stat /dev/cros_fp: ", err)
+	}
+
+	// Remove type and extra bits.
+	permTest := info.Mode & 0o777
+	// Expect "u=rw,g=rw,o=" permission.
+	permExpected := uint32(0o660)
+
+	if permTest != permExpected {
+		s.Errorf("The /dev/cros_fp file has permission %o, but we expected %o",
+			permTest, permExpected)
+	}
+	if id := info.Uid; id != 0 {
+		s.Errorf("The /dev/cros_fp file owner is uid %d, but we expected root (0)", id)
+	}
+	if id := info.Gid; id == 0 {
+		s.Error("The /dev/cros_fp file group is root (0), but we expected not root (0)")
+	}
+}
