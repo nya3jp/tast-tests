74457710a9bfa421dec26f490b559a3e7fd565e0
Craig Hesling <hesling@chromium.org>
2022-09-30 21:06:32 -0400

    biod: Add CommandContextUser and example/test
    This function allows for creating a testexec.CommandContext
    that runs as a particular user. This is useful for checking
    indirect access rights of files and actions.
    
    BUG=b:241864458, b:194926925, b:248610343
    TEST=tast run localhost:2222 biod.ExampleCommandContextUser
    
    Change-Id: I422dacc3ce98f86174fe14310b03e57e32e12b5c

---
+++ src/chromiumos/tast/common/testexec/command_context_user.go
@@ -0,0 +1,59 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testexec
+
+import (
+	"context"
+	"os/user"
+	"strconv"
+	"syscall"
+
+	"chromiumos/tast/errors"
+)
+
+// CommandContextUser creates a CommandContext that will run as the
+// given username.
+//
+// The returned error is a result of the user/group lookup steps.
+func CommandContextUser(ctx context.Context, username, name string, arg ...string) (*Cmd, error) {
+	usr, err := user.Lookup(username)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to lookup username")
+	}
+	uid, err := strconv.Atoi(usr.Uid)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse uid")
+	}
+	gid, err := strconv.Atoi(usr.Gid)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse gid")
+	}
+	grps, err := usr.GroupIds()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to retrieve groups")
+	}
+	gids := make([]uint32, len(grps))
+	for i, g := range grps {
+		id, err := strconv.Atoi(g)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse groups id")
+		}
+		gids[i] = uint32(id)
+	}
+
+	cmd := CommandContext(ctx, name, arg...)
+	// We do this conditional initialization, in case CommandContext changes
+	// its use of SysProcAttr.
+	if cmd.Cmd.SysProcAttr == nil {
+		cmd.Cmd.SysProcAttr = new(syscall.SysProcAttr)
+	}
+	// If we only provide the minimal Uid, the process won't have
+	cmd.Cmd.SysProcAttr.Credential = &syscall.Credential{
+		Uid:    uint32(uid),
+		Gid:    uint32(gid),
+		Groups: gids,
+	}
+	return cmd, nil
+}
---
+++ src/chromiumos/tast/local/bundles/cros/example/command_context_user.go
@@ -0,0 +1,83 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package example
+
+import (
+	"context"
+	"sort"
+	"strings"
+
+	"chromiumos/tast/common/testexec"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: CommandContextUser,
+		Desc: "Example and test of the CommandContextUser function",
+		Contacts: []string{
+			"hesling@chromium.org",
+			"chromeos-fingerprint@google.com",
+		},
+		Attr: []string{"group:mainline", "informational"},
+	})
+}
+
+// CommandContextUser exercises CommandContextUser function.
+//
+// We ensure that the uid, gid, and group ids are the same when running
+// "id" as the user as it is running "id" with the user specified.
+// We can't just run "id" and "id chronos", since it include a security
+// context that might differ.
+func CommandContextUser(ctx context.Context, s *testing.State) {
+	cmd := testexec.CommandContext(ctx,
+		"bash", "-c",
+		"id --user chronos && id --group chronos && id --groups chronos",
+	)
+	expectedOut, err := cmd.Output()
+	if err != nil {
+		s.Fatal("Failed to run id: ", err)
+	}
+
+	cmd, err = testexec.CommandContextUser(ctx, "chronos",
+		"bash", "-c",
+		"id --user && id --group && id --groups",
+	)
+	if err != nil {
+		s.Fatal("Failed to setup CommandContextUser: ", err)
+	}
+	testOut, err := cmd.Output()
+	if err != nil {
+		s.Fatal("Failed to run id as chronos: ", err)
+	}
+
+	expectedOutLines := strings.Split(string(expectedOut), "\n")
+	testOutLines := strings.Split(string(testOut), "\n")
+
+	// We only have 3 lines with 3 \n's, but the final \n creates a 4th
+	// empty split item.
+	if count := len(expectedOutLines); count != 4 {
+		s.Fatalf("The expected output has %d newlines, but we expected 4", count)
+	}
+	if count := len(testOutLines); count != 4 {
+		s.Fatalf("The test output has %d newlines, but we expected 4", count)
+	}
+	// Check that the list of IDs in each of the 3 lines match, when sorted.
+	// Only the third, last, line should actually contain a list of IDs.
+	for line := 0; line < 3; line++ {
+		expectedIds := strings.Split(expectedOutLines[line], " ")
+		sort.Strings(expectedIds)
+		expectedStr := strings.Join(expectedIds, " ")
+
+		testIds := strings.Split(testOutLines[line], " ")
+		sort.Strings(testIds)
+		testStr := strings.Join(testIds, " ")
+
+		if strings.Compare(expectedStr, testStr) != 0 {
+			s.Fatalf("output line %d expected IDs don't match test IDs: %q != %q",
+				line, expectedStr, testStr)
+		}
+	}
+}
