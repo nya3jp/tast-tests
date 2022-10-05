8a2c06a3a9f43a821211c76f5d81b2dd4e10641c
Chad Duffin <chadduffin@chromium.org>
2022-10-05 22:44:40 +0000

    [TAST] Add DumpUITree to ChromeUIService.
    This change adds an API to the ChromeUIService that dumps the UI tree to
    the output directory for the provided testing context.
    
    BUG=None
    TEST=Manually tested with a temporary test.
    
    Change-Id: I751955ec0fa5bd532596f50b064f661f3ffd15cf

--- src/chromiumos/tast/local/bundles/cros/ui/chrome_ui_service.go
+++ src/chromiumos/tast/local/bundles/cros/ui/chrome_ui_service.go
@@ -11,6 +11,7 @@ import (
 	"google.golang.org/grpc"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/common"
 	"chromiumos/tast/local/upstart"
 	pb "chromiumos/tast/services/cros/ui"
 	"chromiumos/tast/testing"
@@ -19,13 +20,17 @@ import (
 func init() {
 	testing.AddService(&testing.Service{
 		Register: func(srv *grpc.Server, s *testing.ServiceState) {
-			pb.RegisterChromeUIServiceServer(srv, &ChromeUIService{})
+			pb.RegisterChromeUIServiceServer(srv, &ChromeUIService{
+				sharedObject: common.SharedObjectsForServiceSingleton,
+			})
 		},
 	})
 }
 
 // ChromeUIService implements the methods defined in ChromeUIServiceServer.
-type ChromeUIService struct{}
+type ChromeUIService struct {
+	sharedObject *common.SharedObjectsForService
+}
 
 // EnsureLoginScreen emulates log out, and ensures login screen.
 func (c *ChromeUIService) EnsureLoginScreen(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
@@ -33,4 +38,21 @@ func (c *ChromeUIService) EnsureLoginScreen(ctx context.Context, req *empty.Empt
 		return &empty.Empty{}, errors.Wrap(err, "failed to restart ui job")
 	}
 	return &empty.Empty{}, nil
+}
+
+// DumpUITree dumps the UI tree to the context output directory of the test.
+func (c *ChromeUIService) DumpUITree(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	cr := c.sharedObject.Chrome
+	if cr == nil {
+		return &empty.Empty{}, errors.New("Chrome has not been started")
+	}
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to create test API connection")
+	}
+	contextOutDir, err := testing.ContextOutDir(ctx)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to get the context output directory")
+	}
+	return &empty.Empty{}, faillog.DumpUITree(ctx, contextOutDir, tconn)
 }
