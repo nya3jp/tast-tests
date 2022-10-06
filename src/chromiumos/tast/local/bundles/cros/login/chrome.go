edfcbfab59e8ba61b9b781b39662da92e61706e2
Maksim Ivanov <emaxx@chromium.org>
2022-10-06 14:34:16 +0000

    tast-tests: Run login.Chrome w/wo AuthFactor
    Parameterize the login.Chrome end-to-end login test to have new
    sub-test-cases: one for --enable-features=UseAuthFactors, and one for
    --disable-features=UseAuthFactors.
    
    This allows to verify both codepaths in the tests regardless of the
    default value of the feature.
    
    BUG=b:251433302
    TEST=tast run $DUT login.Chrome.*
    
    Change-Id: I3564e396fddd8d95ac3baaf1b38a3a84beea1251

--- src/chromiumos/tast/local/bundles/cros/login/chrome.go
+++ src/chromiumos/tast/local/bundles/cros/login/chrome.go
@@ -17,6 +17,11 @@ import (
 	"chromiumos/tast/testing"
 )
 
+type chromeTestParams struct {
+	numTrial int
+	opts     []chrome.Option
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         Chrome,
@@ -30,17 +35,27 @@ func init() {
 		},
 		SoftwareDeps: []string{"chrome"},
 		Params: []testing.Param{{
-			Val:       1,
+			Val:       chromeTestParams{numTrial: 1},
 			ExtraAttr: []string{"group:mainline"},
 			Timeout:   chrome.LoginTimeout + 45*time.Second,
 		}, {
+			Name:      "auth_factor_experiment_on",
+			Val:       chromeTestParams{numTrial: 1, opts: []chrome.Option{chrome.EnableFeatures("UseAuthFactors")}},
+			ExtraAttr: []string{"group:mainline", "informational"},
+			Timeout:   chrome.LoginTimeout + 45*time.Second,
+		}, {
+			Name:      "auth_factor_experiment_off",
+			Val:       chromeTestParams{numTrial: 1, opts: []chrome.Option{chrome.DisableFeatures("UseAuthFactors")}},
+			ExtraAttr: []string{"group:mainline", "informational"},
+			Timeout:   chrome.LoginTimeout + 45*time.Second,
+		}, {
 			Name:      "stress",
-			Val:       50,
+			Val:       chromeTestParams{numTrial: 50},
 			ExtraAttr: []string{"group:stress"},
 			Timeout:   50*chrome.LoginTimeout + 45*time.Second,
 		}, {
 			Name:    "forever",
-			Val:     1000000,
+			Val:     chromeTestParams{numTrial: 1000000},
 			Timeout: 365 * 24 * time.Hour,
 		}},
 	})
@@ -71,18 +86,18 @@ func Chrome(ctx context.Context, s *testing.State) {
 	}))
 	defer server.Close()
 
-	numTrial := s.Param().(int)
-	for i := 0; i < numTrial; i++ {
-		if numTrial > 1 {
-			s.Logf("Trial %d/%d", i+1, numTrial)
+	params := s.Param().(chromeTestParams)
+	for i := 0; i < params.numTrial; i++ {
+		if params.numTrial > 1 {
+			s.Logf("Trial %d/%d", i+1, params.numTrial)
 		}
 
-		testChromeLoginInSession(ctx, s, sm, server.URL, content)
+		testChromeLoginInSession(ctx, s, sm, &params, server.URL, content)
 	}
 }
 
-func testChromeLoginInSession(ctx context.Context, s *testing.State, sm *session.SessionManager, url, expected string) {
-	testChromeLogin(ctx, s, sm, url, expected)
+func testChromeLoginInSession(ctx context.Context, s *testing.State, sm *session.SessionManager, params *chromeTestParams, url, expected string) {
+	testChromeLogin(ctx, s, sm, params, url, expected)
 
 	sw, err := sm.WatchSessionStateChanged(ctx, "stopped")
 	if err != nil {
@@ -104,7 +119,7 @@ func testChromeLoginInSession(ctx context.Context, s *testing.State, sm *session
 	}
 }
 
-func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionManager, url, expected string) {
+func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionManager, params *chromeTestParams, url, expected string) {
 	// Start listening for a "started" SessionStateChanged D-Bus signal from session_manager.
 	sw, err := sm.WatchSessionStateChanged(ctx, "started")
 	if err != nil {
@@ -112,7 +127,8 @@ func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionM
 	}
 	defer sw.Close(ctx)
 
-	cr, err := chrome.New(ctx, chrome.ExtraArgs("--vmodule=login_display_host*=4,oobe_ui=4"))
+	opts := append([]chrome.Option{chrome.ExtraArgs("--vmodule=login_display_host*=4,oobe_ui=4")}, params.opts...)
+	cr, err := chrome.New(ctx, opts...)
 	if err != nil {
 		s.Fatal("Chrome login failed: ", err)
 	}
