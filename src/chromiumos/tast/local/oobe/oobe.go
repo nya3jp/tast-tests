312a4f17916f9de54ebc7bc556d5d463f902cc80
Andrew Dear <andrewdear@google.com>
2022-10-07 21:56:42 +0000

    [CrOS Bluetooth] Add non applicable device tast test.
    Add a tast test that checks if the OOBE HID Detection page is
    shown on non applicable devices.
    
    BUG=b:246649651
    TEST=tast -verbose run -build=true $DUT bluetooth.OobeHidNonApplicableDevice
    
    Change-Id: Ie01d552a0919c90aa919247ca84dc7426149104f

---
+++ src/chromiumos/tast/local/oobe/oobe.go
@@ -0,0 +1,20 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package oobe contains helpers for shared logic in OOBE related tests.
+package oobe
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+)
+
+// IsWelcomeScreenVisible checks the current page in OOBE to see if it's
+// currently on the welcome page.
+func IsWelcomeScreenVisible(ctx context.Context, oobeConn *chrome.Conn) error {
+	// Check that the Welcome screen is visible.
+	err := oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.WelcomeScreen.isVisible()")
+	return err
+}
