a97184a9775acc3a2bd0ef35ce59daae1c74a0c1
Andrew Dear <andrewdear@google.com>
2022-10-07 21:56:42 +0000

    [CrOS Bluetooth] Add non applicable device tast test.
    Add a tast test that checks if the OOBE HID Detection page is
    shown on non applicable devices.
    
    BUG=b:246649651
    TEST=tast -verbose run -build=true $DUT bluetooth.OobeHidNonApplicableDevice
    
    Change-Id: Ie01d552a0919c90aa919247ca84dc7426149104f

---
+++ src/chromiumos/tast/local/oobe/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package oobe contains helpers for shared logic in OOBE related tests.
+package oobe
