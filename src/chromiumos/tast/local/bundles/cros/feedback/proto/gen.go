16dfbd217ea8fd24e24c53a765f16b1261e5b0c1
Danny Wang <wangdanny@google.com>
2022-10-07 20:38:36 +0000

    feedback: Verify feedbackUserCtlConsent value in the report
    BUG=b:185624798
    TEST=tast run <DUT> feedback.VerifyFeedbackUserCtlConsentValue.*
    
    Change-Id: I605a4699bb2d31c4202c5bedcea431fee498d9e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3940029
    Reviewed-by: David Welling <davidwelling@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Reviewed-by: Wenyu Zhang <zhangwenyu@google.com>
    Tested-by: Danny Wang <wangdanny@google.com>

---
+++ src/chromiumos/tast/local/bundles/cros/feedback/proto/gen.go
@@ -0,0 +1,11 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// go:generate protoc -I . --go_out=../../../../../platform/tast-tests/src ./*.proto
+
+package proto
+
+// protos are in the ~/chromiumos/src/platform2/feedback/components/feedback/proto
+// go to this directory and run the above commend line
+// to genearte the pb.go files.
