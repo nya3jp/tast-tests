98f66e1bd664d38c53a4c42e953cc515be70e8e9
Rajat Khandelwal <rajat.khandelwal@intel.com>
2022-10-08 15:37:05 +0530

    tast-tests: typec: Add login script for typec functionality
    Add a test which explicitly does login, desired for the
    functionality of type-c devices. Automation scripts have a
    python login script but it doesn't enable peripheral data access.
    Thus creating a script just to aid in login.
    
    It has two modes: normal and complete. Normal will only help logging in
    and will change the mode if required. Complete will enable
    data peripheral access in addition, enabling PCIe tunneling.
    
    This will help us in automation since login continues to pose us as a problem.
    
    BUG= b/244693753
    TEST= Ran both modes on skolas setup with USB4 connected. Script runs as expected.
    typec.TypecLogin.normal
    typec.TypecLogin.complete
    
    Signed-off-by: Rajat Khandelwal <rajat.khandelwal@intel.com>
    Change-Id: I562adce070666c2b4c9f66775573615162a2cd5a

---
+++ src/chromiumos/tast/local/bundles/cros/typec/typec_login.go
@@ -0,0 +1,63 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package typec
+
+import (
+	"context"
+
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/typecutils"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         TypecLogin,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Processes login and enables data peripheral access setting (when mode set to complete) to enable TBT/USB4",
+		Contacts:     []string{"rajat.khandelwal@intel.com"},
+		Attr:         []string{"group:typec"},
+		SoftwareDeps: []string{"chrome"},
+		VarDeps:      []string{"ui.signinProfileTestExtensionManifestKey"},
+		Data:         []string{"testcert.p12"},
+		Params: []testing.Param{
+			// To login and change mode without enabling data peripheral access
+			{
+				Name:      "normal",
+				Val:       false,
+			},
+			// To login and enable data peripheral access
+			{
+				Name:      "complete",
+				Val:       true,
+			}},
+	})
+}
+
+// TypecLogin does the following:
+// If normal, login happens with only changing mode, if required.
+// If complete, data peripheral access is also enabled.
+//
+func TypecLogin(ctx context.Context, s *testing.State) {
+	// Get to the Chrome login screen.
+	cr, err := chrome.New(ctx,
+		chrome.DeferLogin(),
+		chrome.LoadSigninProfileExtension(s.RequiredVar("ui.signinProfileTestExtensionManifestKey")))
+	if err != nil {
+		s.Fatal("Failed to start Chrome at login screen: ", err)
+	}
+	defer cr.Close(ctx)
+
+	peripheralDataEnableReq := s.Param().(bool)
+	if peripheralDataEnableReq == true {
+		if err = typecutils.EnablePeripheralDataAccess(ctx, s.DataPath("testcert.p12")); err != nil {
+			s.Fatal("Failed to enable peripheral data access setting: ", err)
+		}
+	}
+
+	if err := cr.ContinueLogin(ctx); err != nil {
+		s.Fatal("Failed to login: ", err)
+	}
+}
