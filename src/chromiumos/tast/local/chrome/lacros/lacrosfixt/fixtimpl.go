8b52bfea0dd4501f6c104d3b3c46631458a27457
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-10-10 16:49:35 -0700

    tast-tests: Lacros variants to shelf.AppWindowOnShelf
    DO NOT SUBMIT: confirm the expected behavior of the extension apps
    on the Shelf when opening from Launcher.
    
    This also adds a fixture that loads Lacros with 100 fake apps.
    
    BUG=b:219815913
    TEST=shelf.AppWindowOnShelf.clamshell_mode*
    
    Change-Id: Iac3df744d01108744369365b5867431d8e10c41c

--- src/chromiumos/tast/local/chrome/lacros/lacrosfixt/fixtimpl.go
+++ src/chromiumos/tast/local/chrome/lacros/lacrosfixt/fixtimpl.go
@@ -229,4 +229,20 @@ func init() {
 		ResetTimeout:    chrome.ResetTimeout,
 		TearDownTimeout: chrome.ResetTimeout,
 	})
+
+	// lacrosWith100FakeAppsNoAppSort is similar to "chromeLoggedInWith100FakeAppsNoAppSort" fixture but with lacros enabled.
+	testing.AddFixture(&testing.Fixture{
+		Name:     "lacrosWith100FakeAppsNoAppSort",
+		Desc:     "Lacros Chrome loading 100 fake apps with app sorting disabled but keepalive enabled",
+		Contacts: []string{"jamescook@chromium.org", "hyungtaekim@chromium.org"},
+		Impl: chrome.NewLoggedInFixture(func(ctx context.Context, s *testing.FixtState) ([]chrome.Option, error) {
+			return NewConfig(
+				KeepAlive(true), // Opening fake apps from the Launcher depend on appservice that needs KeepAlive on.
+				ChromeOptions(chrome.DisableFeatures("LauncherAppSort"))).Opts()
+		}),
+		Parent:          "install100LacrosApps",
+		SetUpTimeout:    chrome.LoginTimeout,
+		ResetTimeout:    chrome.ResetTimeout,
+		TearDownTimeout: chrome.ResetTimeout,
+	})
 }
