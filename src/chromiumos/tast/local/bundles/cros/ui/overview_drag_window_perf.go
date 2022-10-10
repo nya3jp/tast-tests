56fb333863b9216f6e1c889f0b0f20a9d3ab866f
Avery Musbach <amusbach@chromium.org>
2022-10-10 15:23:27 -0700

    ui: Use 80% zoom or less, in OverviewDragWindowPerf
    Following up after CL:3908277, use 80% zoom instead of 90% zoom. For the
    test to pass on all devices, the windows need to be snappable in split
    view even in portrait orientation.
    
    BUG=b:251158778
    TEST=tast run <DUT> ui.OverviewDragWindowPerf.*
    
    Change-Id: Ief1f87b69082adb408680d9ef96d15a3e3532985

--- src/chromiumos/tast/local/bundles/cros/ui/overview_drag_window_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/overview_drag_window_perf.go
@@ -345,10 +345,10 @@ func OverviewDragWindowPerf(ctx context.Context, s *testing.State) {
 	}
 	defer cleanup(cleanupCtx)
 
-	// Ensures that the display zoom factor is 90% (allowing for rounding error) or
+	// Ensures that the display zoom factor is 80% (allowing for rounding error) or
 	// less, to ensure that the work area length is at least twice the minimum length
 	// of a browser window, so that browser windows can be snapped in split view.
-	const zoomMaximum = 0.90000005
+	const zoomMaximum = 0.80000005
 	info, err := display.GetPrimaryInfo(ctx, tconn)
 	if err != nil {
 		s.Fatal("Failed to get the primary display info: ", err)
@@ -356,7 +356,7 @@ func OverviewDragWindowPerf(ctx context.Context, s *testing.State) {
 	if zoomInitial := info.DisplayZoomFactor; zoomInitial > zoomMaximum {
 		i := sort.Search(len(info.AvailableDisplayZoomFactors), func(i int) bool { return info.AvailableDisplayZoomFactors[i] > zoomMaximum })
 		if i == 0 {
-			s.Fatalf("Lowest available display zoom factor is %f; want 90%% (allowing for rounding error) or less", info.AvailableDisplayZoomFactors[0])
+			s.Fatalf("Lowest available display zoom factor is %f; want 80%% (allowing for rounding error) or less", info.AvailableDisplayZoomFactors[0])
 		}
 		zoomForTest := info.AvailableDisplayZoomFactors[i-1]
 		if err := display.SetDisplayProperties(ctx, tconn, info.ID, display.DisplayProperties{DisplayZoomFactor: &zoomForTest}); err != nil {
