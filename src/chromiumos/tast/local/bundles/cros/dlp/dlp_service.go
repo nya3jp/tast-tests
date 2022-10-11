953ebdec7396a8e1e097cc4e4492d03f0dbd45f0
Luca Accorsi <accorsi@google.com>
2022-10-11 12:55:53 +0000

    dlp: Add reporting tast test for clipboard copy and paste
    BUG=chromium:1373868
    TEST=tast -verbose run DUT policy.DlpReporting*
    
    Change-Id: I327470e627b754fe31c9899b3a82740b91a5b193

---
+++ src/chromiumos/tast/local/bundles/cros/dlp/dlp_service.go
@@ -0,0 +1,197 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package dlp
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/bundles/cros/dlp/clipboard"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/lacros"
+	"chromiumos/tast/local/chrome/lacros/lacrosfixt"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/chrome/uiauto/state"
+	"chromiumos/tast/local/chrome/webutil"
+	"chromiumos/tast/local/input"
+	pb "chromiumos/tast/services/cros/dlp"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddService(&testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			pb.RegisterDataLeakPreventionServiceServer(srv, &DataLeakPreventionService{s: s})
+		},
+	})
+}
+
+type DataLeakPreventionService struct {
+	s      *testing.ServiceState
+	chrome *chrome.Chrome
+}
+
+// EnrollAndLogin enrolls the device and logs in with the provided account credentials.
+func (service *DataLeakPreventionService) EnrollAndLogin(ctx context.Context, req *pb.EnrollAndLoginRequest) (_ *empty.Empty, retErr error) {
+
+	opts := []chrome.Option{
+		chrome.GAIAEnterpriseEnroll(chrome.Creds{User: req.Username, Pass: req.Password}),
+		chrome.GAIALogin(chrome.Creds{User: req.Username, Pass: req.Password}),
+		chrome.EnableFeatures(req.EnabledFeatures),
+		chrome.DMSPolicy(req.DmserverUrl),
+		chrome.EncryptedReportingAddr(fmt.Sprintf("%v/record", req.ReportingServerUrl)),
+		chrome.CustomLoginTimeout(chrome.EnrollmentAndLoginTimeout),
+	}
+
+	bt := browser.TypeAsh
+	var lcfg *lacrosfixt.Config
+	if req.EnableLacros {
+		bt = browser.TypeLacros
+		lcfg = lacrosfixt.NewConfig(lacrosfixt.Mode(lacros.NotSpecified), lacrosfixt.Selection(lacros.NotSelected))
+	}
+
+	cr, err := browserfixt.NewChrome(ctx, bt, lcfg, opts...)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start chrome")
+	}
+
+	service.chrome = cr
+	return &empty.Empty{}, nil
+}
+
+// StopChrome disconnects from Chrome.
+func (service *DataLeakPreventionService) StopChrome(ctx context.Context, req *empty.Empty) (*empty.Empty, error) {
+	var lastErr error
+
+	if service.chrome == nil {
+		return nil, errors.New("no active Chrome instance")
+	}
+
+	if err := service.chrome.Close(ctx); err != nil {
+		testing.ContextLog(ctx, "Failed to close Chrome: ", err)
+		lastErr = errors.Wrap(err, "failed to close Chrome")
+	}
+	service.chrome = nil
+
+	return &empty.Empty{}, lastErr
+}
+
+// ClipboardCopyPaste performs a copy and paste action.
+func (service *DataLeakPreventionService) ClipboardCopyPaste(ctx context.Context, req *pb.ActionRequest) (_ *empty.Empty, retErr error) {
+
+	baseDir := "/tmp"
+
+	// Create an HTML page with some text.
+	textFilename := "text.html"
+	textContent := []byte("<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><title>Random Text 1</title></head><body>Sample text about random things.</body></html>")
+	if err := os.WriteFile(filepath.Join(baseDir, textFilename), textContent, 0644); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed write a file")
+	}
+
+	// Create an HTML page with an input box.
+	inputFilename := "input.html"
+	inputContent := []byte("<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><title>Editable Text Box</title></head><body><textarea aria-label='textarea' rows='1' cols='100'></textarea></body></html>")
+	if err := os.WriteFile(filepath.Join(baseDir, inputFilename), inputContent, 0644); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed write a file")
+	}
+
+	sourceServer := httptest.NewServer(http.FileServer(http.Dir(baseDir)))
+	defer sourceServer.Close()
+
+	destServer := httptest.NewServer(http.FileServer(http.Dir(baseDir)))
+	defer destServer.Close()
+
+	browserType := browser.TypeAsh
+	if req.BrowserType == pb.BrowserType_LACROS {
+		browserType = browser.TypeLacros
+	}
+
+	br, closeBrowser, err := browserfixt.SetUp(ctx, service.chrome, browserType)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to open the browser")
+	}
+	defer closeBrowser(ctx)
+
+	sourceURL := sourceServer.URL + "/" + textFilename
+	sourceConn, err := br.NewConn(ctx, sourceURL)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to open the source page")
+	}
+	defer sourceConn.Close()
+
+	if err := webutil.WaitForQuiescence(ctx, sourceConn, 10*time.Second); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to wait to achieve quiescence")
+	}
+
+	// Connect to Test API.
+	tconn, err := service.chrome.TestAPIConn(ctx)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to connect to test API")
+	}
+
+	keyboard, err := input.VirtualKeyboard(ctx)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to get keyboard")
+	}
+	defer keyboard.Close()
+
+	if err := tconn.WaitForExpr(ctx, "chrome.clipboard"); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to wait for chrome.clipboard API to become available")
+	}
+
+	if err := uiauto.Combine("copy all text from source website",
+		keyboard.AccelAction("Ctrl+A"),
+		keyboard.AccelAction("Ctrl+C"))(ctx); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to copy text from source browser")
+	}
+
+	copiedString, err := clipboard.GetClipboardContent(ctx, tconn)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to get clipboard content")
+	}
+
+	destURL := destServer.URL + "/" + inputFilename
+	destConn, err := br.NewConn(ctx, destURL)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to open the destination page")
+	}
+	defer destConn.Close()
+
+	if err := webutil.WaitForQuiescence(ctx, destConn, 10*time.Second); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to wait to achieve quiescence")
+	}
+
+	ui := uiauto.New(tconn)
+
+	textBoxNode := nodewith.Name("textarea").Role(role.TextField).State(state.Editable, true).First()
+	if err := uiauto.Combine("Pasting into text box",
+		ui.WaitUntilExists(textBoxNode.Visible()),
+		ui.LeftClick(textBoxNode),
+		ui.WaitUntilExists(textBoxNode.Focused()),
+		keyboard.AccelAction("Ctrl+V"),
+	)(ctx); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to paste into text box")
+	}
+
+	// We are only testing report mode, since we expect the text to be copied.
+	if err := clipboard.CheckPastedContent(ctx, ui, copiedString); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to verify pasted content")
+	}
+
+	return &empty.Empty{}, nil
+}
---
+++ src/chromiumos/tast/remote/dlputil/dlp_util.go
@@ -0,0 +1,102 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package dlputil
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/reportingutil"
+)
+
+// Action represents the supported DLP actions.
+type Action int
+
+const (
+	// ClipboardCopyPaste identifies a clipboard copy and paste action.
+	ClipboardCopyPaste Action = iota
+)
+
+// String returns a string representation of `Action`.
+func (action Action) String() string {
+	switch action {
+	case ClipboardCopyPaste:
+		return "CLIPBOARD"
+	default:
+		return fmt.Sprintf("String() not defined for Action %d", int(action))
+	}
+}
+
+// EventsBundle contains an events vector per restriction level and it is populated by `retrieveEvents`.
+type EventsBundle struct {
+	block       []reportingutil.InputEvent
+	report      []reportingutil.InputEvent
+	warn        []reportingutil.InputEvent
+	warnProceed []reportingutil.InputEvent
+}
+
+// RetrieveEvents returns events for every restriction level having a timestamp greater than `testStartTime` with the given `clientID`.
+func RetrieveEvents(ctx context.Context, customerID, APIKey, clientID string, testStartTime time.Time) (*EventsBundle, error) {
+
+	// Retrieve all DLP events stored in the server side.
+	dlpEvents, err := reportingutil.LookupEvents(ctx, reportingutil.ReportingServerURL, customerID, APIKey, "DLP_EVENTS")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to look up events")
+	}
+
+	// Reduce events to those associate to `clientID` and starting after `testStartTime`.
+	prunedEvents, err := reportingutil.PruneEvents(ctx, dlpEvents, clientID, testStartTime, func(e reportingutil.InputEvent) bool {
+		return true
+	})
+	if err != nil {
+		return nil, errors.New("failed to prune events")
+	}
+
+	var events EventsBundle
+
+	// Organize events according to their mode.
+	for _, event := range prunedEvents {
+		mode := event.WrappedEncryptedData.DlpPolicyEvent.Mode
+		switch {
+		case mode == "BLOCK":
+			events.block = append(events.block, event)
+		case mode == "REPORT":
+			events.report = append(events.report, event)
+		case mode == "WARN":
+			events.warn = append(events.warn, event)
+		case mode == "WARN_PROCEED":
+			events.warnProceed = append(events.warnProceed, event)
+		default:
+			return nil, errors.New("unsupported event mode")
+		}
+	}
+
+	return &events, nil
+
+}
+
+// ValidateReportEvents checks whether events array contains the correct events.
+func ValidateReportEvents(action Action, events *EventsBundle) error {
+
+	// We are only testing report mode
+	expectedBlockEvents := 0
+	expectedReportEvents := 1
+	expectedWarnEvents := 0
+	expectedWarnProceedEvents := 0
+
+	if len(events.block) != expectedBlockEvents || len(events.report) != expectedReportEvents || len(events.warn) != expectedWarnEvents || len(events.warnProceed) != expectedWarnProceedEvents {
+		return errors.Errorf("unexpected events = got %d BLOCK, %d REPORT, %d WARN, and %d WARN_PROCEED events. Want %d BLOCK, %d REPORT, %d WARN, and %d WARN_PROCEED events",
+			len(events.block), len(events.report), len(events.warn), len(events.warnProceed),
+			expectedBlockEvents, expectedReportEvents, expectedWarnEvents, expectedWarnProceedEvents)
+	}
+
+	if len(events.report) > 0 && events.report[0].WrappedEncryptedData.DlpPolicyEvent.Restriction != Action.String(action) {
+		return errors.Errorf("unexpected restriction = got %v, want %v", events.report[0].WrappedEncryptedData.DlpPolicyEvent.Restriction, Action.String(action))
+	}
+
+	return nil
+}
