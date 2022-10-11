9ec7af2f1d4a6f6858ce26e5307ab208f9081c22
Luca Accorsi <accorsi@google.com>
2022-10-11 12:55:53 +0000

    dlp: Add reporting tast test for clipboard copy and paste
    BUG=chromium:1373868
    TEST=tast -verbose run DUT policy.DlpReporting*
    
    Change-Id: I327470e627b754fe31c9899b3a82740b91a5b193

---
+++ src/chromiumos/tast/remote/bundles/cros/policy/dlp_reporting.go
@@ -0,0 +1,228 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"chromiumos/tast/common/tape"
+	"chromiumos/tast/remote/dlputil"
+	"chromiumos/tast/remote/policyutil"
+	"chromiumos/tast/remote/reportingutil"
+	"chromiumos/tast/rpc"
+	dlp "chromiumos/tast/services/cros/dlp"
+	ps "chromiumos/tast/services/cros/policy"
+	"chromiumos/tast/testing"
+)
+
+const (
+	// restrictionBlockReportingEnabledUsername is the path to the secret username having block restriction level for all components and reporting enabled.
+	restrictionBlockReportingEnabledUsername = "dlp.restriction_level_block_reporting_enabled_username"
+
+	// restrictionBlockReportingEnabledPassword is the path to the secret password having block restriction level for all components and reporting enabled.
+	restrictionBlockReportingEnabledPassword = "dlp.restriction_level_block_reporting_enabled_password"
+
+	// restrictionBlockReportingDisabledUsername is the path to the secret username having block restriction level for all components and reporting disabled.
+	restrictionBlockReportingDisabledUsername = "dlp.restriction_level_block_reporting_disabled_username"
+
+	// restrictionBlockReportingDisabledPassword is the path to the secret password having block restriction level for all components and reporting disabled.
+	restrictionBlockReportingDisabledPassword = "dlp.restriction_level_block_reporting_disabled_password"
+
+	// restrictionReportReportingEnabledUsername is the path to the secret username having report restriction level for all components and reporting enabled.
+	restrictionReportReportingEnabledUsername = "dlp.restriction_level_report_reporting_enabled_username"
+
+	// restrictionReportReportingEnabledPassword is the path to the secret password having report restriction level for all components and reporting enabled.
+	restrictionReportReportingEnabledPassword = "dlp.restriction_level_report_reporting_enabled_password"
+
+	// restrictionReportReportingDisabledUsername is the path to the secret username having report restriction level for all components and reporting disabled.
+	restrictionReportReportingDisabledUsername = "dlp.restriction_level_report_reporting_disabled_username"
+
+	// restrictionReportReportingDisabledPassword is the path to the secret password having report restriction level for all components and reporting disabled.
+	restrictionReportReportingDisabledPassword = "dlp.restriction_level_report_reporting_disabled_password"
+
+	// restrictionWarnReportingEnabledUsername is the path to the secret username having warn restriction level for all components and reporting enabled.
+	restrictionWarnReportingEnabledUsername = "dlp.restriction_level_warn_reporting_enabled_username"
+
+	// restrictionWarnReportingEnabledPassword is the path to the secret password having warn restriction level for all components and reporting enabled.
+	restrictionWarnReportingEnabledPassword = "dlp.restriction_level_warn_reporting_enabled_password"
+
+	// restrictionWarnReportingDisabledUsername is the path to the secret username having warn restriction level for all components and reporting disabled.
+	restrictionWarnReportingDisabledUsername = "dlp.restriction_level_warn_reporting_disabled_username"
+
+	// restrictionWarnReportingDisabledPassword is the path to the secret password having warn restriction level for all components and reporting disabled.
+	restrictionWarnReportingDisabledPassword = "dlp.restriction_level_warn_reporting_disabled_password"
+
+	// restrictionAllowReportingEnabledUsername is the path to the secret username having allow restriction level for all components and reporting enabled.
+	restrictionAllowReportingEnabledUsername = "dlp.restriction_level_allow_reporting_enabled_username"
+
+	// restrictionAllowReportingEnabledPassword is the path to the secret password having allow restriction level for all components and reporting enabled.
+	restrictionAllowReportingEnabledPassword = "dlp.restriction_level_allow_reporting_enabled_password"
+
+	// restrictionAllowReportingDisabledUsername is the path to the secret username having allow restriction level for all components and reporting disabled.
+	restrictionAllowReportingDisabledUsername = "dlp.restriction_level_allow_reporting_disabled_username"
+
+	// restrictionAllowReportingDisabledPassword is the path to the secret password having allow restriction level for all components and reporting disabled.
+	restrictionAllowReportingDisabledPassword = "dlp.restriction_level_allow_reporting_disabled_password"
+)
+
+// testParams contains parameters for testing different DLP configurations.
+type testParams struct {
+	Username         string          // username for Chrome enrollment
+	Password         string          // password for Chrome enrollment
+	Mode             dlp.Mode        // mode of the applied restriction
+	BrowserType      dlp.BrowserType // which browser the test should use
+	ReportingEnabled bool            // test should expect reporting to be enabled
+	Action           dlputil.Action  // which action the test should use
+}
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DlpReporting,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Tests DLP actions and check whether the correct events are generated, sent, and received from the server side",
+		Contacts: []string{
+			"chromeos-dlp@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational", "group:dpanel-end2end"},
+		SoftwareDeps: []string{"reboot", "chrome"},
+		ServiceDeps: []string{
+			"tast.cros.hwsec.OwnershipService",
+			"tast.cros.dlp.DataLeakPreventionService",
+			"tast.cros.browser.ChromeService",
+			"tast.cros.policy.PolicyService",
+			"tast.cros.tape.Service",
+		},
+		Timeout: 7 * time.Minute,
+		VarDeps: []string{
+			restrictionBlockReportingEnabledUsername,
+			restrictionBlockReportingEnabledPassword,
+			restrictionBlockReportingDisabledUsername,
+			restrictionBlockReportingDisabledPassword,
+			restrictionWarnReportingEnabledUsername,
+			restrictionWarnReportingEnabledPassword,
+			restrictionWarnReportingDisabledUsername,
+			restrictionWarnReportingDisabledPassword,
+			restrictionReportReportingEnabledUsername,
+			restrictionReportReportingEnabledPassword,
+			restrictionReportReportingDisabledUsername,
+			restrictionReportReportingDisabledPassword,
+			restrictionAllowReportingEnabledUsername,
+			restrictionAllowReportingEnabledPassword,
+			restrictionAllowReportingDisabledUsername,
+			restrictionAllowReportingDisabledPassword,
+			reportingutil.ManagedChromeCustomerIDPath,
+			reportingutil.EventsAPIKeyPath,
+			tape.ServiceAccountVar,
+		},
+		Params: []testing.Param{
+			{
+				Name: "ash_report_reporting_enabled",
+				Val: testParams{
+					Username:         restrictionReportReportingEnabledUsername,
+					Password:         restrictionReportReportingEnabledPassword,
+					Mode:             dlp.Mode_REPORT,
+					BrowserType:      dlp.BrowserType_ASH,
+					ReportingEnabled: true,
+					Action:           dlputil.ClipboardCopyPaste,
+				},
+			},
+			{
+				Name: "lacros_report_reporting_enabled",
+				Val: testParams{
+					Username:         restrictionReportReportingEnabledUsername,
+					Password:         restrictionReportReportingEnabledPassword,
+					Mode:             dlp.Mode_REPORT,
+					BrowserType:      dlp.BrowserType_LACROS,
+					ReportingEnabled: true,
+					Action:           dlputil.ClipboardCopyPaste,
+				},
+				ExtraSoftwareDeps: []string{"lacros"},
+			},
+		},
+	})
+}
+
+func DlpReporting(ctx context.Context, s *testing.State) {
+
+	params := s.Param().(testParams)
+
+	username := s.RequiredVar(params.Username)
+	password := s.RequiredVar(params.Password)
+	customerID := s.RequiredVar(reportingutil.ManagedChromeCustomerIDPath)
+	APIKey := s.RequiredVar(reportingutil.EventsAPIKeyPath)
+	sa := []byte(s.RequiredVar(tape.ServiceAccountVar))
+
+	// Reset the DUT state.
+	defer func(ctx context.Context) {
+		if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT(), s.RPCHint()); err != nil {
+			s.Error("Failed to reset TPM after test: ", err)
+		}
+	}(ctx)
+	if err := policyutil.EnsureTPMAndSystemStateAreReset(ctx, s.DUT(), s.RPCHint()); err != nil {
+		s.Fatal("Failed to reset TPM: ", err)
+	}
+
+	// Establish RPC connection to the DUT.
+	cl, err := rpc.Dial(ctx, s.DUT(), s.RPCHint())
+	if err != nil {
+		s.Fatal("Failed to connect to the RPC service on the DUT: ", err)
+	}
+	defer cl.Close(ctx)
+	defer reportingutil.Deprovision(ctx, cl.Conn, sa, customerID)
+
+	// Create client instance of the DataLeakPrevention service.
+	service := dlp.NewDataLeakPreventionServiceClient(cl.Conn)
+
+	// Use the service to enroll the DUT and login.
+	if _, err := service.EnrollAndLogin(ctx, &dlp.EnrollAndLoginRequest{
+		Username:           username,
+		Password:           password,
+		DmserverUrl:        reportingutil.DmServerURL,
+		ReportingServerUrl: reportingutil.ReportingServerURL,
+		EnableLacros:       params.BrowserType == dlp.BrowserType_LACROS,
+		EnabledFeatures:    "EncryptedReportingPipeline",
+	}); err != nil {
+		s.Fatal("Remote call EnrollAndLogin() failed: ", err)
+	}
+	defer service.StopChrome(ctx, &empty.Empty{})
+
+	// Create client instance of the Policy service just to retrieve the clientID.
+	pc := ps.NewPolicyServiceClient(cl.Conn)
+
+	// TODO(crbug.com/1376853): consider whether porting this method to the DataLeakPrevention service.
+	c, err := pc.ClientID(ctx, &empty.Empty{})
+	if err != nil {
+		s.Fatal("Failed to grab client ID from device: ", err)
+	}
+
+	// We are going to filter the events also based on the test time.
+	testStartTime := time.Now()
+
+	switch params.Action {
+	case dlputil.ClipboardCopyPaste:
+		service.ClipboardCopyPaste(ctx, &dlp.ActionRequest{
+			BrowserType: params.BrowserType,
+			Mode:        params.Mode,
+		})
+	}
+
+	s.Log("Waiting 60 seconds to make sure events reach the server and are processed")
+	if err := testing.Sleep(ctx, 60*time.Second); err != nil {
+		s.Fatal("Failed to sleep: ", err)
+	}
+
+	events, err := dlputil.RetrieveEvents(ctx, customerID, APIKey, c.ClientId, testStartTime)
+	if err != nil {
+		s.Fatal("Failed to retrieve events: ", err)
+	}
+
+	if err := dlputil.ValidateEvents(params.ReportingEnabled, params.Mode, params.Action, events); err != nil {
+		s.Fatal("Failed to validate events: ", err)
+	}
+
+}
---
+++ src/chromiumos/tast/remote/dlputil/dlp_util.go
@@ -0,0 +1,127 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package dlputil
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/remote/reportingutil"
+	dlp "chromiumos/tast/services/cros/dlp"
+)
+
+// Action represents the supported DLP actions.
+type Action int
+
+const (
+	// ClipboardCopyPaste identifies a clipboard copy and paste action.
+	ClipboardCopyPaste Action = iota
+)
+
+// String returns a string representation of `Action`.
+func (action Action) String() string {
+	switch action {
+	case ClipboardCopyPaste:
+		return "CLIPBOARD"
+	default:
+		return fmt.Sprintf("String() not defined for Action %d", int(action))
+	}
+}
+
+// EventsBundle contains an events vector per restriction level and it is populated by `retrieveEvents`.
+type EventsBundle struct {
+	block       []reportingutil.InputEvent
+	report      []reportingutil.InputEvent
+	warn        []reportingutil.InputEvent
+	warnProceed []reportingutil.InputEvent
+}
+
+// RetrieveEvents returns events for every restriction level having a timestamp greater than `testStartTime` with the given `clientID`.
+func RetrieveEvents(ctx context.Context, customerID, APIKey, clientID string, testStartTime time.Time) (*EventsBundle, error) {
+
+	// Retrieve all DLP events stored in the server side.
+	dlpEvents, err := reportingutil.LookupEvents(ctx, reportingutil.ReportingServerURL, customerID, APIKey, "DLP_EVENTS")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to look up events")
+	}
+
+	// Reduce events to those associate to `clientID` and starting after `testStartTime`.
+	prunedEvents, err := reportingutil.PruneEvents(ctx, dlpEvents, clientID, testStartTime, func(e reportingutil.InputEvent) bool {
+		return true
+	})
+	if err != nil {
+		return nil, errors.New("failed to prune events")
+	}
+
+	var events EventsBundle
+
+	// Organize events according to their mode.
+	for _, event := range prunedEvents {
+		mode := event.WrappedEncryptedData.DlpPolicyEvent.Mode
+		switch {
+		case mode == "BLOCK":
+			events.block = append(events.block, event)
+		case mode == "REPORT":
+			events.report = append(events.report, event)
+		case mode == "WARN":
+			events.warn = append(events.warn, event)
+		case mode == "WARN_PROCEED":
+			events.warnProceed = append(events.warnProceed, event)
+		default:
+			return nil, errors.New("unsupported event mode")
+		}
+	}
+
+	return &events, nil
+
+}
+
+// ValidateEvents checks whether events array contains the correct events according to `reportingEnabled` and `mode`.
+func ValidateEvents(reportingEnabled bool, mode dlp.Mode, action Action, events *EventsBundle) error {
+
+	expectedBlockEvents := 0
+	expectedReportEvents := 0
+	expectedWarnEvents := 0
+	expectedWarnProceedEvents := 0
+
+	var expectedEvents []reportingutil.InputEvent
+
+	if reportingEnabled {
+		switch mode {
+		case dlp.Mode_BLOCK:
+			expectedEvents = events.block
+			expectedBlockEvents = 1
+		case dlp.Mode_REPORT:
+			expectedEvents = events.report
+			expectedReportEvents = 1
+		case dlp.Mode_WARN_CANCEL:
+			expectedEvents = events.warn
+			expectedWarnEvents = 1
+		case dlp.Mode_WARN_PROCEED:
+			expectedEvents = events.warn
+			expectedWarnEvents = 1
+			expectedWarnProceedEvents = 1
+		}
+	}
+
+	if len(events.block) != expectedBlockEvents || len(events.report) != expectedReportEvents || len(events.warn) != expectedWarnEvents || len(events.warnProceed) != expectedWarnProceedEvents {
+		return errors.Errorf("unexpected events = got %d BLOCK, %d REPORT, %d WARN, and %d WARN_PROCEED events. Want %d BLOCK, %d REPORT, %d WARN, and %d WARN_PROCEED events",
+			len(events.block), len(events.report), len(events.report), len(events.warnProceed), expectedBlockEvents, expectedReportEvents, expectedWarnEvents, expectedWarnProceedEvents)
+	}
+
+	if len(expectedEvents) > 0 && expectedEvents[0].WrappedEncryptedData.DlpPolicyEvent.Restriction != Action.String(action) {
+		return errors.Errorf("unexpected restriction = got %v, want %v", expectedEvents[0].WrappedEncryptedData.DlpPolicyEvent.Restriction, Action.String(action))
+	}
+
+	if mode == dlp.Mode_WARN_PROCEED {
+		if len(events.warnProceed) > 0 && events.warnProceed[0].WrappedEncryptedData.DlpPolicyEvent.Restriction != Action.String(action) {
+			return errors.Errorf("unexpected restriction = got %v, want %v", events.warnProceed[0].WrappedEncryptedData.DlpPolicyEvent.Restriction, Action.String(action))
+		}
+	}
+
+	return nil
+}
