745ae185edadf33517dedcf74e1bbcd9d8b1023d
Arisa Tsuji <arisai@google.com>
2022-10-12 17:39:53 +0900

    network: check if WireGuard can configure IPv6 address
    This patch updated the tast test to check if an IPv6 address can be
    configured for wireguard. In crrev.com/c/3907310, shill receives an IPv4
    address and IPv6 addresses in a string list and creates IPAddress
    objects for overlay IPs and allowed IPs. This patch checks if wireguard
    can configure the IPv6 addresses with IPv4 addresses correctly (with
    peers) by pinging.
    
    BUG=b:251686186
    TEST=tast run $DUT network.VPNConnect.*
    TEST=../platform/tast/fast_build.sh -T
    
    Change-Id: I1756a82019aed57d3a7f0d75511e412519676182

--- src/chromiumos/tast/local/bundles/cros/network/arc_vpn_disabled.go
+++ src/chromiumos/tast/local/bundles/cros/network/arc_vpn_disabled.go
@@ -49,15 +49,15 @@ func ARCVPNDisabled(ctx context.Context, s *testing.State) {
 	if _, err := conn.Connect(ctx); err != nil {
 		s.Fatal("Failed to connect to VPN server: ", err)
 	}
-	if err := routing.ExpectPingSuccessWithTimeout(ctx, conn.Server.OverlayIP, "chronos", 10*time.Second); err != nil {
-		s.Fatalf("Failed to ping from host %s: %v", conn.Server.OverlayIP, err)
+	if err := routing.ExpectPingSuccessWithTimeout(ctx, conn.Server.OverlayIPv4, "chronos", 10*time.Second); err != nil {
+		s.Fatalf("Failed to ping from host %s: %v", conn.Server.OverlayIPv4, err)
 	}
 	// Currently, ARC VPN is disabled by default.
 	// TODO(b/147256449): Explicitly disable ARC VPN once the feature becomes enabled-by-defalt
 	if err := arcvpn.WaitForARCServiceState(ctx, a, arcvpn.Pkg, arcvpn.Svc, false); err != nil {
 		s.Fatalf("Failed to stop %s: %v", arcvpn.Svc, err)
 	}
-	if err := arc.ExpectPingSuccess(ctx, a, "vpn", conn.Server.OverlayIP); err == nil {
-		s.Fatalf("Failed to verify %s was unreachable from ARC over 'vpn'", conn.Server.OverlayIP)
+	if err := arc.ExpectPingSuccess(ctx, a, "vpn", conn.Server.OverlayIPv4); err == nil {
+		s.Fatalf("Expected unable to ping %s from ARC over 'vpn', but was reachable", conn.Server.OverlayIPv4)
 	}
 }
