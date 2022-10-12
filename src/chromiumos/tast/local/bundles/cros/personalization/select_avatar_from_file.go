f32886b37cc7d5b6fc798def06d5807c03547418
Jerry Liu <pzliu@google.com>
2022-10-12 18:10:58 +0000

    tast-tests: Deflake personalization.SelectAvatarFromFile
    We added a waiting time between clicking the default avatar and
    verifying the avatar has changed. Also forced this test to run only in
    clamshell mode.
    
    BUG=b:252945595
    TEST=tast run $DUT personalization.SelectAvatarFromFile
    
    Change-Id: I5d030cd91428b8c1e30db2e53dd043d70f71cae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3949733
    Auto-Submit: Jerry Liu <pzliu@google.com>
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Reviewed-by: Matthew Warton <mwarton@google.com>
    Commit-Queue: Jerry Liu <pzliu@google.com>
    Tested-by: Jerry Liu <pzliu@google.com>

--- src/chromiumos/tast/local/bundles/cros/personalization/select_avatar_from_file.go
+++ src/chromiumos/tast/local/bundles/cros/personalization/select_avatar_from_file.go
@@ -10,6 +10,7 @@ import (
 
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
@@ -70,9 +71,8 @@ func selectProfileImage(ctx context.Context, ui *uiauto.Context) error {
 	selectedAvatarOption := nodewith.Role(role.ListBoxOption).NameContaining(googleProfilePhoto).HasClass("tast-selected-profile-image")
 
 	return uiauto.Combine("change avatar back to Google profile photo",
-		ui.WaitUntilExists(googleProfilePhotoContainer),
-		ui.DoDefault(googleProfilePhotoContainer),
-		ui.WaitUntilExists(selectedAvatarOption),
+		ui.WithTimeout(3*time.Second).DoDefault(googleProfilePhotoContainer),
+		ui.WithTimeout(3*time.Second).WaitUntilExists(selectedAvatarOption),
 	)(ctx)
 }
 
@@ -87,6 +87,15 @@ func SelectAvatarFromFile(ctx context.Context, s *testing.State) {
 	if err != nil {
 		s.Fatal("Failed to create Test API connection: ", err)
 	}
+
+	// Force Chrome to be in clamshell mode to make sure it's possible to close
+	// the personalization hub.
+	cleanup, err := ash.EnsureTabletModeEnabled(ctx, tconn, false)
+	if err != nil {
+		s.Fatal("Failed to ensure DUT is not in tablet mode: ", err)
+	}
+	defer cleanup(cleanupCtx)
+
 	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
 
 	ui := uiauto.New(tconn).WithTimeout(30 * time.Second)
