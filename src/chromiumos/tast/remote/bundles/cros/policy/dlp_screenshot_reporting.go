5ce3d3874b6e8f2c56df9f385e01fc573147ff2b
Luca Accorsi <accorsi@google.com>
2022-10-13 14:58:36 +0000

    dlp: Add reporting tast test for screenshot
    BUG=chromium:1374300
    TEST=tast -verbose run DUT policy.DlpScreenshotReporting*
    
    Change-Id: I64a902f9aba5a1ba24e056422775cabea35c921b

---
+++ src/chromiumos/tast/remote/bundles/cros/policy/dlp_screenshot_reporting.go
@@ -0,0 +1,63 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/tape"
+	"chromiumos/tast/remote/dlputil"
+	"chromiumos/tast/remote/reportingutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DlpScreenshotReporting,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Test whether screenshot events are correctly reported for every restriction level",
+		Contacts: []string{
+			"accorsi@google.com", // Test author
+			"chromeos-dlp@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational", "group:dpanel-end2end"},
+		SoftwareDeps: []string{"reboot", "chrome"},
+		ServiceDeps: []string{
+			"tast.cros.hwsec.OwnershipService",
+			"tast.cros.dlp.DataLeakPreventionService",
+			"tast.cros.browser.ChromeService",
+			"tast.cros.policy.PolicyService",
+			"tast.cros.tape.Service",
+		},
+		Timeout: 7 * time.Minute,
+		VarDeps: []string{
+			dlputil.RestrictionBlockReportingEnabledUsername,
+			dlputil.RestrictionBlockReportingEnabledPassword,
+			dlputil.RestrictionBlockReportingDisabledUsername,
+			dlputil.RestrictionBlockReportingDisabledPassword,
+			dlputil.RestrictionWarnReportingEnabledUsername,
+			dlputil.RestrictionWarnReportingEnabledPassword,
+			dlputil.RestrictionWarnReportingDisabledUsername,
+			dlputil.RestrictionWarnReportingDisabledPassword,
+			dlputil.RestrictionReportReportingEnabledUsername,
+			dlputil.RestrictionReportReportingEnabledPassword,
+			dlputil.RestrictionReportReportingDisabledUsername,
+			dlputil.RestrictionReportReportingDisabledPassword,
+			dlputil.RestrictionAllowReportingEnabledUsername,
+			dlputil.RestrictionAllowReportingEnabledPassword,
+			dlputil.RestrictionAllowReportingDisabledUsername,
+			dlputil.RestrictionAllowReportingDisabledPassword,
+			reportingutil.ManagedChromeCustomerIDPath,
+			reportingutil.EventsAPIKeyPath,
+			tape.ServiceAccountVar,
+		},
+		Params: dlputil.TestParameters,
+	})
+}
+
+func DlpScreenshotReporting(ctx context.Context, s *testing.State) {
+	dlputil.DlpActionReporting(ctx, s, dlputil.Screenshot)
+}
