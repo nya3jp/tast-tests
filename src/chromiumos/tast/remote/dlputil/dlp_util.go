f8e7ab7c01d7c354f2aafdd118184bdd8f13b976
Luca Accorsi <accorsi@google.com>
2022-10-13 14:58:36 +0000

    dlp: Add reporting tast test for screenshot
    BUG=chromium:1374300
    TEST=tast -verbose run DUT policy.DlpScreenshotReporting*
    
    Change-Id: I64a902f9aba5a1ba24e056422775cabea35c921b

--- src/chromiumos/tast/remote/dlputil/dlp_util.go
+++ src/chromiumos/tast/remote/dlputil/dlp_util.go
@@ -300,6 +300,8 @@ const (
 	ClipboardCopyPaste Action = iota
 	// Printing identifies a printing action.
 	Printing
+	// Screenshot identifies a screenshot action.
+	Screenshot
 )
 
 func (action Action) String() string {
@@ -308,6 +310,8 @@ func (action Action) String() string {
 		return "CLIPBOARD"
 	case Printing:
 		return "PRINTING"
+	case Screenshot:
+		return "SCREENSHOT"
 	default:
 		return fmt.Sprintf("String() not defined for Action %d", int(action))
 	}
@@ -470,6 +474,11 @@ func DlpActionReporting(ctx context.Context, s *testing.State, action Action) er
 		})
 	case Printing:
 		service.Print(ctx, &dlp.ActionRequest{
+			BrowserType: params.BrowserType,
+			Mode:        params.Mode,
+		})
+	case Screenshot:
+		service.Screenshot(ctx, &dlp.ActionRequest{
 			BrowserType: params.BrowserType,
 			Mode:        params.Mode,
 		})
