231239fc2122fbcb23bdd3ec7f8835b3b2a0e965
Po-Hsien Wang <pwang@chromium.org>
2022-10-13 14:14:00 -0700

    graphics.HardwareProbe: Add a test to verify hardware_probe
    Add a test to verify hardware_probe can successfully identify the
    information.
    
    BUG=b:256217403
    BUG=b:253113846
    TEST=CQ
    
    Change-Id: Ie4d035dafcc6feec1b7621be3141e6d4db5fe75b

---
+++ src/chromiumos/tast/local/bundles/cros/graphics/hardware_probe.go
@@ -0,0 +1,41 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package graphics
+
+import (
+	"context"
+	"path/filepath"
+
+	"chromiumos/tast/local/graphics"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func: HardwareProbe,
+		Desc: "Verify hardware_probe binary can detect various information",
+		Contacts: []string{
+			"pwang@chromium.org",
+			"chromeos-gfx@google.com",
+		},
+		Attr:    []string{"group:graphics", "graphics_drm", "group:mainline", "informational"},
+		Fixture: "gpuWatchDog",
+	})
+}
+
+// HardwareProbe verifies we can successfully retrieve various device information via hardware_probe.
+func HardwareProbe(ctx context.Context, s *testing.State) {
+	file := filepath.Join(s.OutDir(), "hardware_probe.json")
+	result, err := graphics.GetHardwareProbeResult(ctx, file)
+	if err != nil {
+		s.Fatal("Failed to run hardware_probe: ", err)
+	}
+
+	s.Log("Successfully get the information: ", result)
+	for _, info := range result.GPUInfo {
+		s.Log("GPU_Family: ", info.Family)
+	}
+	s.Log("CPU_Family: ", result.CPUFamily)
+}
--- src/chromiumos/tast/local/graphics/hardware_probe.go
+++ src/chromiumos/tast/local/graphics/hardware_probe.go
@@ -7,6 +7,8 @@ package graphics
 
 import (
 	"context"
+	"encoding/json"
+	"os"
 	"regexp"
 
 	"chromiumos/tast/common/testexec"
@@ -16,6 +18,40 @@ import (
 const (
 	hardwareProbeBinary = "/usr/local/graphics/hardware_probe"
 )
+
+type pciDevice struct {
+	BDF     string `json:'BDF'`
+	Name    string `json:'Name'`
+	BootVGA bool   `json:"BootVGA"`
+}
+
+type gpuInfo struct {
+	Family string `json:'Family'`
+	Vendor string `json:'GPUVendor'`
+}
+
+type hardwareProbeResult struct {
+	VGADevice []pciDevice `json:"VGA_Devices"`
+	GPUInfo   []gpuInfo   `json:"GPU_Family"`
+	CPUFamily string      `json:"CPU_SOC_Family"`
+}
+
+// GetHardwareProbeResult saves the informatio to path and returns detailed information gathered by hardware_probe binaries in the DUT.
+func GetHardwareProbeResult(ctx context.Context, path string) (hardwareProbeResult, error) {
+	if out, err := testexec.CommandContext(ctx, hardwareProbeBinary, "-output="+path).Output(testexec.DumpLogOnError); err != nil {
+		return hardwareProbeResult{}, errors.Wrapf(err, "failed to run %v, output: %v", hardwareProbeBinary, string(out))
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return hardwareProbeResult{}, errors.Wrapf(err, "failed to read %v", path)
+	}
+	var result hardwareProbeResult
+	if err := json.Unmarshal(data, &result); err != nil {
+		return hardwareProbeResult{}, errors.Wrapf(err, "failed to unmarshal data: %v", string(data))
+	}
+	return result, nil
+}
 
 func runAndGrepRegex(ctx context.Context, args []string, regexStr string) ([]string, error) {
 	re := regexp.MustCompile(regexStr)
