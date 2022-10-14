50e74ee4d51cc25ce9c0d2cfb1e52ca545202a31
Syed Faaiz Hussain <syedfaaiz@google.com>
2022-10-13 18:56:14 -0700

    kernel: move useful functions to seperate directory
    Moving some functions to a shared a package since they are useful for other
    tests as well.
    
    BUG=b:249372493
    TEST=tast run homedut kernel.ConfigVerify
    
    Change-Id: I6621bbefd186bc53ec374abd9cbf4b4fd0e81fef

--- src/chromiumos/tast/local/bundles/cros/kernel/config_verify.go
+++ src/chromiumos/tast/local/bundles/cros/kernel/config_verify.go
@@ -5,16 +5,10 @@
 package kernel
 
 import (
-	"compress/gzip"
 	"context"
-	"io"
-	"io/ioutil"
-	"os"
 	"regexp"
-	"strings"
 
-	"chromiumos/tast/common/testexec"
-	"chromiumos/tast/errors"
+	"chromiumos/tast/local/kernel"
 	"chromiumos/tast/local/sysutil"
 	"chromiumos/tast/testing"
 )
@@ -30,12 +24,12 @@ func init() {
 		},
 		Attr: []string{"group:mainline"},
 		Params: []testing.Param{{
-			Name:		"chromeos",
-			Val:		addExtraCheckForChromeOS,
+			Name: "chromeos",
+			Val:  addExtraCheckForChromeOS,
 		}, {
 			// For https://chromeos.kernelci.org/.
-			Name:		"chromeos_kernelci",
-			Val:		addExtraCheckForChromeOSKernelCI,
+			Name:              "chromeos_kernelci",
+			Val:               addExtraCheckForChromeOSKernelCI,
 			ExtraSoftwareDeps: []string{"chromeos_kernelci"},
 		}},
 	})
@@ -49,7 +43,7 @@ func ConfigVerify(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to get kernel version and arch: ", err)
 	}
 
-	conf, err := readKernelConfig(ctx)
+	conf, err := kernel.ReadKernelConfig(ctx)
 	if err != nil {
 		s.Fatal("Failed to read kernel config: ", err)
 	}
@@ -60,66 +54,6 @@ func ConfigVerify(ctx context.Context, s *testing.State) {
 	kcc = addExtraFunc(kcc, ver, arch)
 
 	kcc.test(conf, s)
-}
-
-// readKernelConfig reads the kernel config key value pairs trimming CONFIG_ prefix from the keys.
-func readKernelConfig(ctx context.Context) (map[string]string, error) {
-	configs, err := readKernelConfigBytes(ctx)
-	if err != nil {
-		return nil, err
-	}
-	res := make(map[string]string)
-
-	for _, line := range strings.Split(string(configs), "\n") {
-		line := strings.TrimSpace(line)
-		if line == "" || strings.HasPrefix(line, "#") {
-			continue
-		}
-		kv := strings.SplitN(line, "=", 2)
-		if len(kv) < 2 || kv[1] == "" {
-			return nil, errors.Errorf("unexpected config line %q", line)
-		}
-		const configPrefix = "CONFIG_"
-		if !strings.HasPrefix(kv[0], configPrefix) {
-			return nil, errors.Errorf("config %q doesn't start with %s unexpectedly", kv[0], configPrefix)
-		}
-		res[strings.TrimPrefix(kv[0], configPrefix)] = kv[1]
-	}
-	return res, nil
-}
-
-func readKernelConfigBytes(ctx context.Context) ([]byte, error) {
-	const filename = "/proc/config.gz"
-	// Load configs module to generate /proc/config.gz.
-	if err := testexec.CommandContext(ctx, "modprobe", "configs").Run(); err != nil {
-		return nil, errors.Wrap(err, "failed to generate kernel config file")
-	}
-	var r io.ReadCloser
-	f, err := os.Open(filename)
-	if err != nil {
-		testing.ContextLogf(ctx, "Falling back: failed to open %s: %v", filename, err)
-		u, err := sysutil.Uname()
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to get uname")
-		}
-		fallbackFile := "/boot/config-" + u.Release
-		r, err = os.Open(fallbackFile)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to open %s", fallbackFile)
-		}
-	} else { // Normal path.
-		defer f.Close()
-		r, err = gzip.NewReader(f)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to create gzip reader for %s", filename)
-		}
-	}
-	defer r.Close()
-	configs, err := ioutil.ReadAll(r)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to read config")
-	}
-	return configs, nil
 }
 
 // kernelConfigCheck contains configs to check.
