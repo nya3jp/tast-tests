51ad1628a24b63de0e771f8aa382cae1972e9590
Chad Duffin <chadduffin@chromium.org>
2022-10-14 21:52:21 +0000

    [TAST] Refactor BTTestService to support Floss.
    This change refactors the existing BTTestService to provide support for
    Floss. The major changes required were:
    
    - Move required BlueZ logic out of the service.
    - Remove no longer required logic.
    - Expand the generic Bluetooth interface.
    
    This is possible due to the fact that the planned Bluetooth UI tests do
    not use any lower level Bluetooth functionality, and instead will opt to
    rely solely on information from the UI and from interacting with the UI.
    This change does not restrict us from adding more in the future.
    
    A convenience method that returns the names and addresses of Bluetooth
    devices found is provided.
    
    BUG=b:242057403,b:242071154
    TEST=Manually on a local device and on lab devices.
    
    Change-Id: I63c2cfb73d1d5c4d8e51dd357c5c00d8ee7c9338

--- src/chromiumos/tast/local/bluetooth/bluetooth.go
+++ src/chromiumos/tast/local/bluetooth/bluetooth.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The ChromiumOS Authors.
+// Copyright 2022 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,7 +11,35 @@ import (
 // Bluetooth defines the core interface used by Bluetooth tests to interact
 // with the Bluetooth implementation in an implementation agnostic manner.
 type Bluetooth interface {
+	// Enable powers on the adapter.
 	Enable(ctx context.Context) error
+
+	// PollForAdapterState polls the bluetooth adapter state until expected
+	// state is received or a timeout occurs.
 	PollForAdapterState(ctx context.Context, exp bool) error
+
+	// PollForEnabled polls the bluetooth adapter state until the adapter is
+	// powered on.
 	PollForEnabled(ctx context.Context) error
+
+	// Devices returns information on the devices known to the Bluetooth
+	// adapter.
+	Devices(ctx context.Context) ([]*DeviceInfo, error)
+
+	// StartDiscovery starts discovery.
+	StartDiscovery(ctx context.Context) error
+
+	// StopDiscovery stops discovery.
+	StopDiscovery(ctx context.Context) error
+
+	// Reset removes all connected and paired devices and ensures the adapter
+	// is powered.
+	Reset(ctx context.Context) error
+}
+
+// DeviceInfo defines a structure used by Bluetooth tests to retrieve
+// information on devices that are known to the Bluetooth adapter.
+type DeviceInfo struct {
+	Address string
+	Name    string
 }
