0c50a0a313ef3e4843b0479ada4a6d7f6456074f
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-10-14 23:04:03 -0700

    tast-tests: Lacros variants to ui.TabLoadingAnimationPerf
    DO NOT SUBMIT needs more work to collect the metrics.
    
    BUG=b:216242056
    TEST=tast run $DUT ui.TabLoadingAnimationPerf*
    
    Change-Id: Ie8cec886032fad6f55ceb639d4c497261b9d34b6

--- src/chromiumos/tast/local/bundles/cros/ui/tab_loading_animation_perf.go
+++ src/chromiumos/tast/local/bundles/cros/ui/tab_loading_animation_perf.go
@@ -13,6 +13,8 @@ import (
 	uiperf "chromiumos/tast/local/bundles/cros/ui/perf"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
 	"chromiumos/tast/local/perfutil"
 	"chromiumos/tast/local/power"
 	"chromiumos/tast/testing"
@@ -22,14 +24,22 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         TabLoadingAnimationPerf,
-		LacrosStatus: testing.LacrosVariantUnknown,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Measures the animation smoothness of tab loading animation",
 		Contacts:     []string{"yichenz@chromium.org", "chromeos-wmp@google.com"},
 		Attr:         []string{"group:crosbolt", "crosbolt_perbuild"},
 		SoftwareDeps: []string{"chrome"},
 		HardwareDeps: hwdep.D(hwdep.InternalDisplay()),
-		Fixture:      "chromeLoggedIn",
-		Timeout:      4 * time.Minute,
+		Params: []testing.Param{{
+			Fixture: "chromeLoggedIn",
+			Val:     browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			Fixture:           "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Val:               browser.TypeLacros,
+		}},
+		Timeout: 4 * time.Minute,
 		Data: []string{
 			"tab_loading_test.html",
 		},
@@ -57,8 +67,16 @@ func TabLoadingAnimationPerf(ctx context.Context, s *testing.State) {
 	server := httptest.NewServer(http.FileServer(s.DataFileSystem()))
 	defer server.Close()
 
+	// Note that for Lacros there is always one extra new tab page opened not to terminate the browser process.
+	bt := s.Param().(browser.Type)
+	br, closeBrowser, err := browserfixt.SetUp(ctx, cr, bt)
+	if err != nil {
+		s.Fatal("Failed to open the browser: ", err)
+	}
+	defer closeBrowser(ctx)
+	// Note that `cr` and `tconn` from ash-chrome are passed to perfutil since the metrics are recorded in ash-chrome.
 	pv := perfutil.RunMultiple(ctx, cr.Browser(), uiperf.Run(s, perfutil.RunAndWaitAll(tconn, func(ctx context.Context) error {
-		conn, err := cr.NewConn(ctx, server.URL+"/tab_loading_test.html")
+		conn, err := br.NewConn(ctx, server.URL+"/tab_loading_test.html")
 		if err != nil {
 			s.Fatal("Failed to open a testing page: ", err)
 		}
