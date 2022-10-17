79a030eab4d34d1b247486d0079ae4e400eb4842
Luca Accorsi <accorsi@google.com>
2022-10-17 11:27:40 +0000

    dlp: Add reporting tast test for printing
    BUG=chromium:1374198
    TEST=tast -verbose run DUT policy.DlpClipboardPrinting*
    
    Change-Id: Ia7cb888a93a7e65decbddf92367de00666092500

--- src/chromiumos/tast/local/bundles/cros/dlp/dlp_service.go
+++ src/chromiumos/tast/local/bundles/cros/dlp/dlp_service.go
@@ -218,3 +218,63 @@ func (service *DataLeakPreventionService) ClipboardCopyPaste(ctx context.Context
 	return &empty.Empty{}, nil
 
 }
+
+// Print performs a print action.
+func (service *DataLeakPreventionService) Print(ctx context.Context, req *pb.ActionRequest) (_ *empty.Empty, retErr error) {
+
+	keyboard, err := input.VirtualKeyboard(ctx)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to get keyboard")
+	}
+	defer keyboard.Close()
+
+	browserType := browser.TypeAsh
+	if req.BrowserType == pb.BrowserType_LACROS {
+		browserType = browser.TypeLacros
+	}
+
+	br, closeBrowser, err := browserfixt.SetUp(ctx, service.chrome, browserType)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to open the browser")
+	}
+	defer closeBrowser(ctx)
+
+	// Create an html page with some text.
+	baseDir := "/tmp"
+	textFilename := "text.html"
+	textContent := []byte("<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><title>Random Text 1</title></head><body>Sample text about random things.</body></html>")
+	if err := os.WriteFile(baseDir+"/"+textFilename, textContent, 0644); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed write a file")
+	}
+
+	server := httptest.NewServer(http.FileServer(http.Dir(baseDir)))
+	defer server.Close()
+
+	conn, err := br.NewConn(ctx, server.URL+"/"+textFilename)
+	if err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to open page")
+	}
+	defer conn.Close()
+
+	if err := webutil.WaitForQuiescence(ctx, conn, 10*time.Second); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to wait to achieve quiescence")
+	}
+
+	// Test printing using hotkey (Ctrl + P).
+	if err := keyboard.Accel(ctx, "Ctrl+P"); err != nil {
+		return &empty.Empty{}, errors.Wrap(err, "failed to type printing hotkey")
+	}
+
+	if req.Mode == pb.Mode_WARN_PROCEED {
+		if err := keyboard.Accel(ctx, "Enter"); err != nil {
+			return &empty.Empty{}, errors.Wrap(err, "failed to hit Enter")
+		}
+	} else if req.Mode == pb.Mode_WARN_CANCEL {
+		if err := keyboard.Accel(ctx, "Esc"); err != nil {
+			return &empty.Empty{}, errors.Wrap(err, "failed to hit Esc")
+		}
+	}
+
+	return &empty.Empty{}, nil
+
+}
