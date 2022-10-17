ff727096ba80e34af705e94c31913290d5537ffd
Luca Accorsi <accorsi@google.com>
2022-10-17 11:27:40 +0000

    dlp: Add reporting tast test for printing
    BUG=chromium:1374198
    TEST=tast -verbose run DUT policy.DlpClipboardPrinting*
    
    Change-Id: Ia7cb888a93a7e65decbddf92367de00666092500

---
+++ src/chromiumos/tast/remote/bundles/cros/policy/dlp_printing_reporting.go
@@ -0,0 +1,64 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package policy
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/common/tape"
+	"chromiumos/tast/remote/dlputil"
+	"chromiumos/tast/remote/reportingutil"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         DlpPrintingReporting,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Test whether printing events are correctly reported for every restriction level",
+		Contacts: []string{
+			"chromeos-dlp@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational", "group:dpanel-end2end"},
+		SoftwareDeps: []string{"reboot", "chrome"},
+		ServiceDeps: []string{
+			"tast.cros.hwsec.OwnershipService",
+			"tast.cros.dlp.DataLeakPreventionService",
+			"tast.cros.browser.ChromeService",
+			"tast.cros.policy.PolicyService",
+			"tast.cros.tape.Service",
+		},
+		Timeout: 7 * time.Minute,
+		VarDeps: []string{
+			dlputil.RestrictionBlockReportingEnabledUsername,
+			dlputil.RestrictionBlockReportingEnabledPassword,
+			dlputil.RestrictionBlockReportingDisabledUsername,
+			dlputil.RestrictionBlockReportingDisabledPassword,
+			dlputil.RestrictionWarnReportingEnabledUsername,
+			dlputil.RestrictionWarnReportingEnabledPassword,
+			dlputil.RestrictionWarnReportingDisabledUsername,
+			dlputil.RestrictionWarnReportingDisabledPassword,
+			dlputil.RestrictionReportReportingEnabledUsername,
+			dlputil.RestrictionReportReportingEnabledPassword,
+			dlputil.RestrictionReportReportingDisabledUsername,
+			dlputil.RestrictionReportReportingDisabledPassword,
+			dlputil.RestrictionAllowReportingEnabledUsername,
+			dlputil.RestrictionAllowReportingEnabledPassword,
+			dlputil.RestrictionAllowReportingDisabledUsername,
+			dlputil.RestrictionAllowReportingDisabledPassword,
+			reportingutil.ManagedChromeCustomerIDPath,
+			reportingutil.EventsAPIKeyPath,
+			tape.ServiceAccountVar,
+		},
+		Params: dlputil.TestParameters,
+	})
+}
+
+func DlpPrintingReporting(ctx context.Context, s *testing.State) {
+
+	dlputil.ValidateActionReporting(ctx, s, dlputil.Printing)
+
+}
