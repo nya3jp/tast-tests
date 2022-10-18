2163fc199b8610d4151f267c7d4ce689dc769978
Elijah Hewer <hewer@chromium.org>
2022-10-18 18:51:39 +0000

    wmp: Add tast test to float window
    Add a tast test to check if a window can be properly floated using the
    keyboard shortcut and WMEvent. Test includes a Lacros variant.
    
    BUG=b:253493157
    TEST=tast run $DUT wmp.FloatWindow.*
    
    Change-Id: I84e40afc99d9df74aa8e61d9b4477f01ccf16472

---
+++ src/chromiumos/tast/local/bundles/cros/wmp/float_window.go
@@ -0,0 +1,131 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wmp
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/ash"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/lacros/lacrosfixt"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/filesapp"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         FloatWindow,
+		LacrosStatus: testing.LacrosVariantExists,
+		Desc:         "Test that the float shortcut works on a floatable window",
+		Contacts: []string{
+			"hewer@chromium.org",
+			"chromeos-wmp@google.com",
+			"chromeos-sw-engprod@google.com",
+		},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Params: []testing.Param{{
+			Name: "ash",
+			Val:  browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			Val:               browser.TypeLacros,
+			ExtraSoftwareDeps: []string{"lacros"},
+		}},
+	})
+}
+
+// FloatWindow floats an open app window using the keyboard shortcut.
+func FloatWindow(ctx context.Context, s *testing.State) {
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+	defer cancel()
+
+	bt := s.Param().(browser.Type)
+	cr, _, closeBrowser, err := browserfixt.SetUpWithNewChrome(ctx, bt, lacrosfixt.NewConfig(),
+		chrome.EnableFeatures("CrOSLabsFloatWindow"))
+	if err != nil {
+		s.Fatal("Failed to start Chrome: ", err)
+	}
+	defer cr.Close(cleanupCtx)
+	defer closeBrowser(cleanupCtx)
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to connect Test API: ", err)
+	}
+
+	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_tree")
+
+	ui := uiauto.New(tconn)
+
+	// If we are in lacros-chrome, there is already a Chrome window open.
+	if bt == browser.TypeLacros {
+		if err := ui.LeftClick(nodewith.Name("Close").First())(ctx); err != nil {
+			s.Fatal("Failed to close Lacros browser: ", err)
+		}
+	}
+
+	if _, err := filesapp.Launch(ctx, tconn); err != nil {
+		s.Fatal("Failed to launch the files app: ", err)
+	}
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed to initialize keyboard: ", err)
+	}
+
+	if err := kb.Accel(ctx, "Search+Alt+F"); err != nil {
+		s.Fatal("Failed to input float accelerator: ", err)
+	}
+	if err := testing.Sleep(ctx, time.Second); err != nil {
+		s.Fatal("Failed to sleep: ", err)
+	}
+
+	// Get files app window (should be the only window open).
+	windows, err := ash.GetAllWindows(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to fetch app windows: ", err)
+	}
+	if len(windows) != 1 {
+		s.Fatal("Wrong number of windows open: ", len(windows))
+	}
+
+	if windows[0].State != ash.WindowStateFloated {
+		s.Fatal("Window is not in the floated state: ", err)
+	}
+
+	if err := ui.LeftClick(nodewith.Name("Restore"))(ctx); err != nil {
+		s.Fatal("Failed to unfloat the window: ", err)
+	}
+	if err := testing.Sleep(ctx, time.Second); err != nil {
+		s.Fatal("Failed to sleep: ", err)
+	}
+
+	windows, err = ash.GetAllWindows(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to fetch app windows: ", err)
+	}
+	if len(windows) != 1 {
+		s.Fatal("Wrong number of windows open: ", len(windows))
+	}
+
+	if windows[0].State != ash.WindowStateNormal {
+		s.Fatal("Failed to return window to the normal state: ", err)
+	}
+
+	err = ash.SetWindowStateAndWait(ctx, tconn, windows[0].ID, ash.WindowStateFloated)
+	if err != nil {
+		s.Fatal("Failed to float window using WMEventType: ", err)
+	}
+}
