6e756ed96b2410fa4774693ac77898bcac9535b3
Navin Ramsaroop <ramsaroop@google.com>
2022-10-18 13:19:33 -0700

    ui: Fix URL in GoogleSlidesCUJ
    BUG=b:253518019
    TEST=tast run <DUT> ui.GoogleSlidesCUJ
    
    Change-Id: I035f5c990323d975215751291a9b9ae8cd86af45

--- src/chromiumos/tast/local/bundles/cros/ui/google_slides_cuj.go
+++ src/chromiumos/tast/local/bundles/cros/ui/google_slides_cuj.go
@@ -56,7 +56,7 @@ func init() {
 func GoogleSlidesCUJ(ctx context.Context, s *testing.State) {
 	const slidesScrollTimeout = 10 * time.Minute
 
-	slidesURL, err := cuj.GetTestSheetsURL(ctx)
+	slidesURL, err := cuj.GetTestSlidesURL(ctx)
 	if err != nil {
 		s.Fatal("Failed to get Google Slides URL: ", err)
 	}
