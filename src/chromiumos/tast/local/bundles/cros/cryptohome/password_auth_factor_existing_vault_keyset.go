fc23c35db88fa6d4d82751a6fd58e833a63a5344
Betul Soysal <betuls@google.com>
2022-10-20 17:32:18 +0000

    tast-tests: Update PasswordAuthFactorExistingVaultKeyset
    This test was created after AuthenticateAuthFactor and AddAuthFactor
    APIs implemented for password AuthFactor type and not updated later.
    Update the test to cover all APIs and for multiple AuthFactor cases
    like password and PIN types.
    
    BUG=b:254738156
    TEST=tast run DUT cryptohome.PasswordAuthFactorExistingVaultKeyset
    
    Change-Id: If425b01c10ceb7c9718281333418c1b837e12064

--- src/chromiumos/tast/local/bundles/cros/cryptohome/password_auth_factor_existing_vault_keyset.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/password_auth_factor_existing_vault_keyset.go
@@ -6,16 +6,14 @@ package cryptohome
 
 import (
 	"context"
-	"io/ioutil"
 	"os"
-	"path"
 	"path/filepath"
 	"time"
 
 	uda "chromiumos/system_api/user_data_auth_proto"
-	cryptohomecommon "chromiumos/tast/common/cryptohome"
 	"chromiumos/tast/common/hwsec"
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/cryptohome"
 	hwseclocal "chromiumos/tast/local/hwsec"
 	"chromiumos/tast/testing"
@@ -33,30 +31,41 @@ func init() {
 	})
 }
 
-// PasswordAuthFactorExistingVaultKeyset tests that AuthenticateAuthFactor uses
-// existing VaultKeyset to authenticate an existing user even if USS experiment
-// is enabled.
+const (
+	userName  = "foo@bar.baz"
+	shadowDir = "/home/.shadow"
+)
+
+func checkKeyFileExists(ctx context.Context, keysetFile string) error {
+	hash, err := cryptohome.UserHash(ctx, userName)
+	if err != nil {
+		return errors.Wrap(err, "failed to get user hash")
+	}
+	if _, err = os.Stat(filepath.Join(shadowDir, hash, keysetFile)); err != nil {
+		return errors.Wrap(err, "failed to stat keyset file")
+	}
+	return nil
+}
+
+// PasswordAuthFactorExistingVaultKeyset tests the AuthFactor API flows when
+// UserSecretStash is enabled for an existing user with legacy VaultKesyets such that:
+// - AuthenticateAuthFactor uses VaultKeyset to authenticate an existing
+// - AddAuthFactor adds the new factor as a VaultKeyset
+// - UpdateAuthFactor successfully updates an existing factor stored via VaultKeyset
+// - RemoveAuthFActor removes an existing factor stored via VaultKeyset
 func PasswordAuthFactorExistingVaultKeyset(ctx context.Context, s *testing.State) {
 	const (
-		userName      = "foo@bar.baz"
-		userPassword  = "secret"
-		passwordLabel = "gaia"
-		ussFlagFile   = "/var/lib/cryptohome/uss_enabled"
-		shadow        = "/home/.shadow"
-		keysetFile    = "master.0" // nocheck
-		ussFile       = "user_secret_stash/uss"
+		userPassword     = "secret"
+		userPin          = "1234"
+		userPinNew       = "4321"
+		passwordLabel    = "gaia"
+		pinLabel         = "pin"
+		firstKeysetFile  = "master.0" // nocheck
+		secondKeysetFile = "master.1" // nocheck
+		ussFile          = "user_secret_stash/uss"
 	)
 
-	// Enable the UserSecretStash experiment for the duration of the test by
-	// creating a flag file that's checked by cryptohomed.
-	if err := os.MkdirAll(path.Dir(ussFlagFile), 0755); err != nil {
-		s.Fatal("Failed to create the UserSecretStash flag file directory: ", err)
-	}
-	if err := ioutil.WriteFile(ussFlagFile, []byte{}, 0644); err != nil {
-		s.Fatal("Failed to write the UserSecretStash flag file: ", err)
-	}
-	defer os.Remove(ussFlagFile)
-
+	ctxForCleanup := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
 	defer cancel()
 
@@ -74,62 +83,150 @@ func PasswordAuthFactorExistingVaultKeyset(ctx context.Context, s *testing.State
 	}
 
 	// Clean up obsolete state, in case there's any.
-	if err := cryptohome.UnmountAll(ctx); err != nil {
-		s.Fatal("Failed to unmount vaults for preparation: ", err)
+	if err := client.UnmountAll(ctx); err != nil {
+		s.Error("Failed to unmount vaults for preparation: ", err)
 	}
-	if err := cryptohome.RemoveVault(ctx, userName); err != nil {
+	if _, err := client.RemoveVault(ctx, userName); err != nil {
 		s.Fatal("Failed to remove old vault for preparation: ", err)
 	}
 
-	// Mount user cryptohome for test user with the legacy interface.
-	// This represents the existing user with existing VaultKeysets
-	if err := cryptohome.CreateVault(ctx, userName, userPassword); err != nil {
-		s.Fatal("Failed to mount user vault: ", err)
-	}
-	defer cryptohome.RemoveVault(ctx, userName)
-
-	// Unmount user vault directory and daemon-store directories.
-	if err := cryptohome.UnmountVault(ctx, userName); err != nil {
-		s.Error("Failed to unmount user vault: ", err)
-	}
-
-	// Authenticate a new auth session via the auth factor and mount the user.
-	authSessionID, err := client.StartAuthSession(ctx, userName /*ephemeral=*/, false, uda.AuthIntent_AUTH_INTENT_DECRYPT)
+	// 1. Create a new user when UserSecretStash is not enabled and add a password factor.
+	//
+	// Start an Auth session and get an authSessionID.
+	authSessionID, err := client.StartAuthSession(ctx, userName, false /*ephemeral*/, uda.AuthIntent_AUTH_INTENT_DECRYPT)
 	if err != nil {
-		s.Fatal("Failed to start auth session for re-mounting: ", err)
+		s.Fatal("Failed to start Auth session: ", err)
+	}
+	defer client.InvalidateAuthSession(ctxForCleanup, authSessionID)
+	// Create user vault.
+	if err := client.CreatePersistentUser(ctx, authSessionID); err != nil {
+		s.Fatal("Failed to create user: ", err)
+	}
+	defer client.RemoveVault(ctxForCleanup, userName)
+	// Mount user home directories and daemon-store directories.
+	if err := client.PreparePersistentVault(ctx, authSessionID, false /*ecryptfs*/); err != nil {
+		s.Fatal("Failed to mount user profile after creation: ", err)
+	}
+	defer client.Unmount(ctxForCleanup, userName)
+	// Add password AuthFactor.
+	if err := client.AddAuthFactor(ctx, authSessionID, passwordLabel, userPassword); err != nil {
+		s.Fatal("Failed to add password AuthFactor: ", err)
+	}
+	// Check that the password VaultKeyset file is created.
+	if err := checkKeyFileExists(ctx, firstKeysetFile); err != nil {
+		s.Fatal("Failed to check password VaultKeyset file: ", err)
+	}
+	// Invalidate AuthSession and unmount user directories.
+	if err := client.CleanupSession(ctx, authSessionID); err != nil {
+		s.Fatal("Failed to cleanup user AuthSession: ", err)
 	}
 
-	// Make sure vault keyset exists before authenticating.
-	hash, err := cryptohome.UserHash(ctx, userName)
+	// 2. Enable the UserSecretStash experiment for the duration of the test by
+	// creating a flag file that's checked by cryptohomed.
+	cleanupUSSExperiment, err := helper.EnableUserSecretStash(ctx)
 	if err != nil {
-		s.Fatal("Failed to get user hash: ", err)
+		s.Fatal("Failed to enable the UserSecretStash experiment: ", err)
 	}
-	_, err = os.Stat(filepath.Join(shadow, hash, keysetFile))
+	defer cleanupUSSExperiment(ctxForCleanup)
+
+	// 3. Add a PIN credential to the existing user after UserSecretStash is enabled.
+	// PIN VaultKeyset should be added and UserSecretStash shouldn't be created.
+	//
+	// Start an Auth session and get an authSessionID.
+	authSessionID, err = client.StartAuthSession(ctx, userName, false /*ephemeral*/, uda.AuthIntent_AUTH_INTENT_DECRYPT)
 	if err != nil {
-		s.Fatal("Keyset file not found: ", err)
+		s.Fatal("Failed to start Auth session: ", err)
+	}
+	defer client.InvalidateAuthSession(ctxForCleanup, authSessionID)
+	// Authenticate with password AuthFactor.
+	if _, err := client.AuthenticateAuthFactor(ctx, authSessionID, passwordLabel, userPassword); err != nil {
+		s.Fatal("Failed to authenticate with password AuthFactor: ", err)
+	}
+	// Mount user home directories and daemon-store directories.
+	if err := client.PreparePersistentVault(ctx, authSessionID, false /*ecryptfs*/); err != nil {
+		s.Fatal("Failed to mount user profile: ", err)
+	}
+	// Add PIN AuthFactor and authenticate with it.
+	if err := client.AddPinAuthFactor(ctx, authSessionID, pinLabel, userPin); err != nil {
+		s.Fatal("Failed to add PIN AuthFactor: ", err)
+	}
+	if _, err := client.AuthenticatePinAuthFactor(ctx, authSessionID, pinLabel, userPin); err != nil {
+		s.Fatal("Failed to authenticate with pin AuthFactor: ", err)
+	}
+	// Check that the PIN VaultKeyset file is created.
+	if err := checkKeyFileExists(ctx, secondKeysetFile); err != nil {
+		s.Fatal("Failed to check PIN VaultKeyset file: ", err)
+	}
+	// Check that UserSecretStash file is not created.
+	if err := checkKeyFileExists(ctx, ussFile); err == nil {
+		s.Fatal("UserSecretStash file shouldn't have been created but found in disk: ", err)
+	}
+	// Invalidate AuthSession and unmount user directories.
+	if err := client.CleanupSession(ctx, authSessionID); err != nil {
+		s.Fatal("Failed to cleanup user AuthSession: ", err)
 	}
 
-	authReply, err := client.AuthenticateAuthFactor(ctx, authSessionID, passwordLabel, userPassword)
+	// 4. Update and then remove VaultKeyset backed PIN credential when UserSecretStash is enabled.
+	// Both update and remove operations should operate with the PIN VaultKeyset, and UserSecretStash shouldn't be created.
+	//
+	// Start an Auth session and get an authSessionID.
+	authSessionID, err = client.StartAuthSession(ctx, userName, false /*ephemeral*/, uda.AuthIntent_AUTH_INTENT_DECRYPT)
 	if err != nil {
-		s.Fatal("Failed to authenticate with auth session: ", err)
+		s.Fatal("Failed to start Auth session: ", err)
 	}
-	if !authReply.Authenticated {
-		s.Fatal("AuthSession not authenticated despite successful reply")
+	defer client.InvalidateAuthSession(ctxForCleanup, authSessionID)
+	// Authenticate with password AuthFactor.
+	if _, err := client.AuthenticateAuthFactor(ctx, authSessionID, passwordLabel, userPassword); err != nil {
+		s.Fatal("Failed to authenticate with password AuthFactor: ", err)
 	}
-	if err := cryptohomecommon.ExpectAuthIntents(authReply.AuthorizedFor, []uda.AuthIntent{
-		uda.AuthIntent_AUTH_INTENT_DECRYPT,
-		uda.AuthIntent_AUTH_INTENT_VERIFY_ONLY,
-	}); err != nil {
-		s.Fatal("Unexpected AuthSession authorized intents: ", err)
+	// Mount user home directories and daemon-store directories.
+	if err := client.PreparePersistentVault(ctx, authSessionID, false /*ecryptfs*/); err != nil {
+		s.Fatal("Failed to mount user profile after authenticating with password: ", err)
 	}
-	if err := client.PreparePersistentVault(ctx, authSessionID /*ecryptfs=*/, false); err != nil {
-		s.Fatal("Failed to prepare persistent vault: ", err)
+	// Add PIN AuthFactor.
+	if err := client.UpdatePinAuthFactor(ctx, authSessionID, pinLabel, userPinNew); err != nil {
+		s.Fatal("Failed to add password AuthFactor: ", err)
 	}
-	defer cryptohome.UnmountVault(ctx, userName)
-
-	// Check that USS file is not created
-	_, err = os.Stat(filepath.Join(shadow, hash, ussFile))
-	if err == nil {
-		s.Fatal("USS file created when there is vault keyset: ", err)
+	// Check that the PIN VaultKeyset file still exists.
+	if err := checkKeyFileExists(ctx, secondKeysetFile); err != nil {
+		s.Fatal("Failed to check PIN VaultKeyset file: ", err)
+	}
+	// Check that UserSecretStash file is not created.
+	if err := checkKeyFileExists(ctx, ussFile); err == nil {
+		s.Fatal("UserSecretStash file shouldn't have been created but found in disk: ", err)
+	}
+	// Invalidate AuthSession to re-authenticate with the updated PIN
+	if err := client.InvalidateAuthSession(ctx, authSessionID); err != nil {
+		s.Fatal("Failed to invaldiate AuthSession: ", err)
+	}
+	// Start a new session and test authenticate with the updated PIN & not authenticate with the old PIN.
+	authSessionID, err = client.StartAuthSession(ctx, userName, false /*ephemeral*/, uda.AuthIntent_AUTH_INTENT_DECRYPT)
+	if err != nil {
+		s.Fatal("Failed to start Auth session: ", err)
+	}
+	defer client.InvalidateAuthSession(ctxForCleanup, authSessionID)
+	// Authenticate with old PIN should fail.
+	if _, err := client.AuthenticatePinAuthFactor(ctx, authSessionID, pinLabel, userPin); err == nil {
+		s.Fatal("Authenticating with the old PIN suceeded but should have failed: ", err)
+	}
+	// Authenticate with updated PIN.
+	if _, err := client.AuthenticatePinAuthFactor(ctx, authSessionID, pinLabel, userPinNew); err != nil {
+		s.Fatal("Authenticating with the updated PIN failed: ", err)
+	}
+	// Authenticate with password AuthFactor to remove the PIN factor.
+	if _, err := client.AuthenticateAuthFactor(ctx, authSessionID, passwordLabel, userPassword); err != nil {
+		s.Fatal("Failed to authenticate with password AuthFactor: ", err)
+	}
+	// Remove PIN factor.
+	if err := client.RemoveAuthFactor(ctx, authSessionID, pinLabel); err != nil {
+		s.Fatal("Authenticating with the updated PIN failed: ", err)
+	}
+	// Check that the PIN VaultKeyset file is deleted.
+	if err := checkKeyFileExists(ctx, secondKeysetFile); err == nil {
+		s.Fatal("PIN VaultKeyset file exists, should have been deleted: ", err)
+	}
+	// Authenticate with PIN should now failed.
+	if _, err := client.AuthenticatePinAuthFactor(ctx, authSessionID, pinLabel, userPinNew); err == nil {
+		s.Fatal("Authenticating with PIN should have failed after removal, but suceeded: ", err)
 	}
 }
