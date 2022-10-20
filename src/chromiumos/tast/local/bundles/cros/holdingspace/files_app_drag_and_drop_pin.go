ad19ac92b4a464fb81d3022abac8b070b672a433
angel <angelsan@google.com>
2022-10-20 02:09:23 +0000

    Drag and drop out of holdingspace into files app and chrome.
    BUG=b:200987409
    TEST=tast run <DUT> holdingspace.FilesAppDragAndDropPin
    
    Change-Id: I55dcdf128c33377878c2fbff46edfc2441fe2b99

--- src/chromiumos/tast/local/bundles/cros/holdingspace/files_app_drag_and_drop_pin.go
+++ src/chromiumos/tast/local/bundles/cros/holdingspace/files_app_drag_and_drop_pin.go
@@ -6,17 +6,23 @@ package holdingspace
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
 	"chromiumos/tast/local/chrome/uiauto/filesapp"
 	"chromiumos/tast/local/chrome/uiauto/holdingspace"
+	"chromiumos/tast/local/chrome/uiauto/mouse"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/cryptohome"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
@@ -36,12 +42,20 @@ func init() {
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Pre:          chrome.LoggedIn(),
+		Params: []testing.Param{{
+			Name: "single_file",
+			Val:/*count=*/ 1,
+		}, {
+			Name: "multiple_files",
+			Val:/*count=*/ 2,
+		}},
 	})
 }
 
 // FilesAppDragAndDropPin tests the functionality of pinning files to Holding Space by
-// dragging and dropping from the Files app.
+// dragging and dropping single/multiple files to/from the Files app.
 func FilesAppDragAndDropPin(ctx context.Context, s *testing.State) {
+	param := s.Param().(int)
 	cr := s.PreValue().(*chrome.Chrome)
 
 	cleanupCtx := ctx
@@ -55,10 +69,16 @@ func FilesAppDragAndDropPin(ctx context.Context, s *testing.State) {
 
 	defer faillog.DumpUITreeOnError(cleanupCtx, s.OutDir(), s.HasError, tconn)
 
+	myFilesPath, err := cryptohome.MyFilesPath(ctx, cr.NormalizedUser())
+	if err != nil {
+		s.Fatal("Failed to get user's MyFiles path: ", err)
+	}
+
 	fsapp, err := filesapp.Launch(ctx, tconn)
 	if err != nil {
 		s.Fatal("Could not open filesapp: ", err)
 	}
+	defer fsapp.Close(ctx)
 
 	// Reset the holding space and `MarkTimeOfFirstAdd` to make the `HoldingSpaceTrayIcon`
 	// show.
@@ -67,27 +87,7 @@ func FilesAppDragAndDropPin(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to reset holding space: ", err)
 	}
 
-	// Create our file, with appropriate permissions so we can delete later.
-	const testFile = "test.txt"
-	myFilesPath, err := cryptohome.MyFilesPath(ctx, cr.NormalizedUser())
-	if err != nil {
-		s.Fatal("Failed to get user's MyFiles path: ", err)
-	}
-	testFilePath := filepath.Join(myFilesPath, testFile)
-	if err := ioutil.WriteFile(testFilePath, []byte("Per aspera, ad astra"),
-		0644); err != nil {
-		s.Fatalf("Creating file %q failed: %s", testFilePath, err)
-	}
-	defer os.Remove(testFilePath)
-
 	uia := uiauto.New(tconn)
-
-	// Get the location of the tray for dragging.
-	tray := holdingspace.FindTray()
-	trayLocation, err := uia.Location(ctx, tray)
-	if err != nil {
-		s.Fatal("Failed to find holding space tray location: ", err)
-	}
 
 	kb, err := input.Keyboard(ctx)
 	if err != nil {
@@ -95,12 +95,83 @@ func FilesAppDragAndDropPin(ctx context.Context, s *testing.State) {
 	}
 	defer kb.Close()
 
-	if err := uiauto.Combine("Drag and drop file on holding space",
-		fsapp.DragAndDropFile(testFile, trayLocation.CenterPoint(), kb),
-		uia.LeftClick(holdingspace.FindTray()),
-		uia.WaitUntilExists(holdingspace.FindPinnedFileChip().Name(testFile)),
-	)(ctx); err != nil {
-		s.Fatalf("Failed to pin item %q by dragging: %s", testFile, err)
+	// Perform additional parameterized testing.
+	if err := testDragAndDrop(ctx, tconn, uia, kb, fsapp, myFilesPath, param); err != nil {
+		s.Fatal("Fail to perform parameterized testing: ", err)
 	}
 
+}
+
+func testDragAndDrop(ctx context.Context, tconn *chrome.TestConn, uia *uiauto.Context, kb *input.KeyboardEventWriter, fsapp *filesapp.FilesApp, myFilesPath string, param int) error {
+	// Create number of files needed.
+	var listFiles []string
+	for i := 0; i < param; i++ {
+		testFile := fmt.Sprintf("test%d.txt", i)
+		testFilePath := filepath.Join(myFilesPath, testFile)
+		if err := ioutil.WriteFile(testFilePath, []byte("Per aspera, ad astra"), 0644); err != nil {
+			errors.Errorf("creating file %q failed: %s", testFilePath, err)
+		}
+		defer os.Remove(testFilePath)
+		listFiles = append(listFiles, testFile)
+	}
+
+	tray := holdingspace.FindTray()
+	trayLocation, err := uia.Location(ctx, tray)
+	if err != nil {
+		errors.Wrap(err, "failed to find holding space tray location")
+	}
+
+	fsappLocation, err := uia.Location(ctx, nodewith.Role(role.ListBox))
+	if err != nil {
+		errors.Wrap(err, "failed to get Files App location")
+	}
+
+	if err := uiauto.Combine(fmt.Sprintf("Pin %d file(s) from Files App to Holding Space by drag and drop", param),
+		fsapp.DragAndDropFile(strings.Join(listFiles, " "), trayLocation.CenterPoint(), kb),
+		uia.LeftClick(tray),
+		func(ctx context.Context) error {
+			for _, fileName := range listFiles {
+				if err := uia.WaitUntilExists(holdingspace.FindPinnedFileChip().Name(fileName))(ctx); err != nil {
+					errors.Errorf("failed to verify that %s exists in holding space: %s", fileName, err)
+				}
+			}
+			return nil
+		},
+	)(ctx); err != nil {
+		errors.Errorf("failed to pin %d items %v from Files App to Holding Space by drag and drop: %s", param, listFiles, err)
+	}
+
+	if err := uiauto.Combine(fmt.Sprintf("Copy %d file(s) from Holding Space to Files App by drag and drop", param),
+		func(ctx context.Context) error {
+			if err := kb.AccelPress(ctx, "Ctrl"); err != nil {
+				errors.Wrap(err, "failed to press Ctrl")
+			}
+			for _, fileName := range listFiles {
+				if err := uia.LeftClick(holdingspace.FindPinnedFileChip().Name(fileName))(ctx); err != nil {
+					errors.Errorf("failed to select %s : %s", fileName, err)
+				}
+			}
+			lastFileLocation, err := uia.Location(ctx, holdingspace.FindPinnedFileChip().Name(listFiles[len(listFiles)-1]))
+			if err != nil {
+				errors.Wrap(err, "failed to get holding space test file location")
+			}
+			if err := kb.AccelRelease(ctx, "Ctrl"); err != nil {
+				errors.Wrap(err, "failed to release Ctrl")
+			}
+			if err = mouse.Drag(tconn, lastFileLocation.CenterPoint(), fsappLocation.CenterPoint(), time.Second)(ctx); err != nil {
+				errors.Errorf("failed to drag and drop %d pinned files(s) %v from Holding Space to Files App: %s", param, listFiles, err)
+			}
+			return err
+		},
+		uia.Gone(holdingspace.FindChip()),
+	)(ctx); err != nil {
+		errors.Errorf("failed to copy %d file(s) from Holding Space to Files App by drag and drop", param)
+	}
+
+	// Remove the copied file(s).
+	for _, fileName := range listFiles {
+		os.Remove(filepath.Join(myFilesPath, fmt.Sprintf("%s (1).txt", fileName[:len(fileName)-len(filepath.Ext(fileName))])))
+	}
+
+	return nil
 }
