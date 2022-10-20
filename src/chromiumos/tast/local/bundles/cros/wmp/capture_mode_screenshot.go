c94e5b3e71039262f6c034263ebc4d05ce60d955
Edward Hill <ecgh@chromium.org>
2022-10-20 19:41:34 +0000

    capture-mode-polish: Tast test to verify capturing screenshot
    Add parameterized test to verify the screenshot capture behaviors
    in fullscreen, partial screen and window mode.
    
    BUG=b:253102741
    TEST=tast run <IP> wmp.CaptureModeScreenshot.*
    
    Change-Id: I4d75ff4fee15f407006be05f738c299f3d5a6f3a

--- src/chromiumos/tast/local/bundles/cros/wmp/capture_mode_screenshot.go
+++ src/chromiumos/tast/local/bundles/cros/wmp/capture_mode_screenshot.go
@@ -19,22 +19,35 @@ import (
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         ScreenshotWhileRecording,
+		Func:         CaptureModeScreenshot,
 		LacrosStatus: testing.LacrosVariantUnneeded,
-		Desc:         "Verify that the screenshot can be performed while doing screen recording",
+		Desc:         "Take a fullscreen, partial or window screen shot by pressing the 'Screen capture' button in the quick settings and verify the existence of the screenshot",
 		Contacts: []string{
-			"xdai@google.com",
+			"michelefan@chromium.com",
 			"chromeos-wmp@google.com",
 			"chromeos-sw-engprod@google.com",
 		},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      "chromeLoggedIn",
+		Params: []testing.Param{
+			{
+				Name: "fullscreen",
+				Val:  wmp.FullScreen,
+			},
+			{
+				Name: "partialscreen",
+				Val:  wmp.PartialScreen,
+			},
+			{
+				Name: "window",
+				Val:  wmp.Window,
+			},
+		},
 	})
 }
 
-// ScreenshotWhileRecording verifies that taking screenshot while doing screen recording works.
-func ScreenshotWhileRecording(ctx context.Context, s *testing.State) {
+func CaptureModeScreenshot(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 
 	tconn, err := cr.TestAPIConn(ctx)
@@ -42,15 +55,15 @@ func ScreenshotWhileRecording(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to connect Test API: ", err)
 	}
 
-	// For verifying the screenshot and screen recording later, delete all screen capture files first.
 	downloadsPath, err := cryptohome.DownloadsPath(ctx, cr.NormalizedUser())
 	if err != nil {
 		s.Fatal("Failed to get user's Download path: ", err)
 	}
 	if err := wmp.DeleteAllScreenCaptureFiles(downloadsPath, true /*deleteScreenRecording=*/, true /*deleteScreenshots=*/); err != nil {
-		s.Fatal("Failed to delete all screenshots and screen recordings: ", err)
+		s.Fatal("Failed to delete all screenshots: ", err)
 	}
 
+	// Reserve 15 seconds for the cleanup.
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 15*time.Second)
 	defer cancel()
@@ -61,38 +74,20 @@ func ScreenshotWhileRecording(ctx context.Context, s *testing.State) {
 	defer wmputils.EnsureCaptureModeActivated(tconn, false)(cleanupCtx)
 	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr, "ui_dump")
 
-	s.Log("Launch 'Screen capture' and start recording")
-	if err := wmp.StartFullScreenRecording(tconn)(ctx); err != nil {
-		s.Fatal("Failed to start screen recording: ", err)
-	}
-	s.Log("Start screenshot while recording is in progress")
-	if err := wmp.LaunchScreenCapture(ctx, tconn); err != nil {
-		s.Fatal("Failed to launch 'Screen capture again': ", err)
-	}
-	if err := wmp.CaptureScreenshot(ctx, tconn, wmp.FullScreen)(ctx); err != nil {
-		s.Fatal("Failed to capture screenshot while recording is in progress: ", err)
-	}
-	s.Log("End screen recording now")
-	if err := wmp.EndScreenRecording(tconn)(ctx); err != nil {
-		s.Fatal("Failed to end screen recording: ", err)
-	}
+	s.Log(ctx, "Launch 'Screen capture' and capture screenshot")
+	source := s.Param().(wmp.CaptureModeSource)
 
+	if err := wmp.CaptureScreenshot(ctx, tconn, source)(ctx); err != nil {
+		s.Fatalf("Failed to capture %v screenshot: %v", source, err)
+	}
 	defer func() {
 		if err := wmp.DeleteAllScreenCaptureFiles(downloadsPath, true /*deleteScreenRecording=*/, true /*deleteScreenshots=*/); err != nil {
-			s.Fatal("Failed to delete all screenshots and screen recordings: ", err)
+			s.Log(ctx, "Failed to delete the screenshot")
 		}
 	}()
 
-	s.Log("Check the existence and the size of the screenshot and recording")
-	if err := wmp.CheckScreenshot(ctx, tconn, downloadsPath, wmp.FullScreen); err != nil {
+	s.Log(ctx, "Check the existence and the size of the screenshot")
+	if err := wmp.CheckScreenshot(ctx, tconn, downloadsPath, source); err != nil {
 		s.Fatal("Failed to verify the screenshot: ", err)
-	}
-	s.Log("Check the existence and the size of the screen recording")
-	has, err := wmputils.HasScreenRecord(ctx, downloadsPath)
-	if err != nil {
-		s.Fatal("Failed to check whether screen record is present: ", err)
-	}
-	if !has {
-		s.Fatal("No screen record is stored in Downloads folder")
 	}
 }
