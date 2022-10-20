fca137d7405fd141257d99844a16cc2ce8f05482
Jason Hsiao <jason.hsiao@cienet.com>
2022-10-20 13:43:38 +0800

    cuj: Modify the LacrosStatus
    Modify the LacrosStatus of the tab switch cuj and extended display cuj.
    
    BUG=b:254760331
    TEST=tast run <vars> <dut> spera.ExtendedDisplayCUJ.*
    TEST=tast run <vars> <dut> spera.TabSwitchCUJRecorder2.*
    
    Change-Id: I2da0a4b8ab74ed46da410c74131f3abb4c2705a8

--- src/chromiumos/tast/local/bundles/cros/spera/tab_switch_cuj_recorder2.go
+++ src/chromiumos/tast/local/bundles/cros/spera/tab_switch_cuj_recorder2.go
@@ -18,7 +18,7 @@ import (
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         TabSwitchCUJRecorder2,
-		LacrosStatus: testing.LacrosVariantNeeded,
+		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Run tab-switching CUJ test in chromewpr recording mode",
 		Contacts:     []string{"abergman@google.com", "tclaiborne@chromium.org", "xliu@cienet.com", "alfredyu@cienet.com"},
 		SoftwareDeps: []string{"chrome"},
