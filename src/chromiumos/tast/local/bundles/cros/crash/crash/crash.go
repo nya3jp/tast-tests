646e943ddb4aaa8562446ead1f08668e0864f678
Miriam Zimmerman <mutexlox@google.com>
2022-10-20 11:02:18 -0700

    crash: Verify .meta file references in crash.User.
    Verify that generated .meta file references all refer to valid
    file paths in crash.User* tests.
    
    Also fix a few lint errors.
    
    BUG=b:186651742
    TEST=tast run forward_crbook crash.User*
    
    Change-Id: Ibb7d488909300e67ed841a3658d390ab8cbc9224

--- src/chromiumos/tast/local/bundles/cros/crash/crash/crash.go
+++ src/chromiumos/tast/local/bundles/cros/crash/crash/crash.go
@@ -16,8 +16,9 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
+
+	"golang.org/x/sys/unix"
 
 	"chromiumos/tast/common/testexec"
 	"chromiumos/tast/errors"
@@ -92,7 +93,7 @@ func checkCrashDirectoryPermissions(path string) error {
 	if err != nil {
 		return err
 	}
-	s, ok := fileInfo.Sys().(*syscall.Stat_t)
+	s, ok := fileInfo.Sys().(*unix.Stat_t)
 	if !ok {
 		return errors.New("failed to cast to Stat_t")
 	}
@@ -212,7 +213,7 @@ func RunCrasherProcess(ctx context.Context, cr *chrome.Chrome, opts CrasherOptio
 		filterBasename = filterBasename[:15]
 	}
 	if err := crash.EnableCrashFiltering(ctx, filterBasename); err != nil {
-		return nil, errors.Wrapf(err, "failed to replace crash filter: %v", err)
+		return nil, errors.Wrap(err, "failed to replace crash filter")
 	}
 	command = append(command, opts.CrasherPath)
 	if !opts.CauseCrash {
@@ -284,7 +285,7 @@ func RunCrasherProcess(ctx context.Context, cr *chrome.Chrome, opts CrasherOptio
 	}
 
 	result := CrasherResult{
-		Crashed:             (crasherExitCode == 128+int(syscall.SIGSEGV)),
+		Crashed:             (crasherExitCode == 128+int(unix.SIGSEGV)),
 		CrashReporterCaught: crashReporterCaught,
 		ReturnCode:          crasherExitCode,
 	}
@@ -296,6 +297,33 @@ func crashFilePrefix(crasherPath string) string {
 	// The prefix of report file names. Basename of the executable, but non-alphanumerics replaced by underscores.
 	// See CrashCollector::Sanitize in src/platform2/crash-repoter/crash_collector.cc.
 	return nonAlphaNumericRegex.ReplaceAllLiteralString(filepath.Base(crasherPath), "_")
+}
+
+// verifyMetaFileRefs verifies that all "upload_file_" refs in the meta file, as well as the "payload",
+// specify valid file paths.
+func verifyMetaFileRefs(metaPath string) error {
+	contents, err := os.ReadFile(metaPath)
+	if err != nil {
+		return errors.Wrap(err, "failed to read meta file")
+	}
+	dir := filepath.Dir(metaPath)
+	lines := strings.Split(string(contents), "\n")
+	for _, l := range lines {
+		if strings.HasPrefix(l, "payload=") || strings.HasPrefix(l, "upload_file_") {
+			parts := strings.Split(l, "=")
+			if len(parts) != 2 {
+				return errors.Errorf("invalid format for .meta file: %s", l)
+			}
+			f := parts[1]
+			if strings.HasPrefix(l, "/") {
+				return errors.Errorf("unexpected absolute path in meta file: %s", f)
+			}
+			if _, err := os.Stat(filepath.Join(dir, f)); err != nil {
+				return errors.Wrapf(err, "could not stat %s", f)
+			}
+		}
+	}
+	return nil
 }
 
 // RunCrasherProcessAndAnalyze executes a crasher process and extracts result data from dumps and logs.
@@ -453,6 +481,11 @@ func RunCrasherProcessAndAnalyze(ctx context.Context, cr *chrome.Chrome, opts Cr
 	result.Meta = crashReportFiles[".meta"]
 	result.Log = crashReportFiles[".log"]
 	result.Pslog = crashReportFiles[".pslog"]
+
+	if err := verifyMetaFileRefs(result.Meta); err != nil {
+		return nil, errors.Wrap(err, ".meta file has invalid file reference")
+	}
+
 	return result, nil
 }
 
@@ -598,9 +631,9 @@ func RunCrashTest(ctx context.Context, cr *chrome.Chrome, s *testing.State, test
 		if !ok {
 			return errors.Wrap(err, "failed to get exit status from crash_sender: failed to cast to exec.ExitError")
 		}
-		s, ok := e.Sys().(syscall.WaitStatus)
+		s, ok := e.Sys().(unix.WaitStatus)
 		if !ok {
-			return errors.Wrap(err, "failed to get exit status from crash_sender: failed to cast to syscall.WaitStatus")
+			return errors.Wrap(err, "failed to get exit status from crash_sender: failed to cast to unix.WaitStatus")
 		}
 		if s.ExitStatus() != 1 {
 			return errors.Wrap(err, "failed to kill crash_sender")
