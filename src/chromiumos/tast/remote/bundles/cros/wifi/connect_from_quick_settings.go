18aca6fc9c00b2f86baa762f8d56a705c6b0228a
Chad Duffin <chadduffin@chromium.org>
2022-10-21 18:46:32 +0000

    [TAST] Connect to both open and secured WiFi.
    This change iterates on the "connect to WiFi from Quick Settings" test
    to cover both open and secured networks.
    
    https://drive.google.com/file/d/19hTltiPz0NhUIdRQfcF2hk0itN8wHkco/view?usp=sharing
    
    BUG=b:244776362
    TEST=Ran on lab devices.
    
    Change-Id: I0a59522b273ec378814a57cdd7416f3a9bd71cdc

--- src/chromiumos/tast/remote/bundles/cros/wifi/connect_from_quick_settings.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/connect_from_quick_settings.go
@@ -9,17 +9,27 @@ import (
 
 	"google.golang.org/protobuf/types/known/emptypb"
 
+	"chromiumos/tast/common/wifi/security"
 	"chromiumos/tast/common/wifi/security/base"
+	"chromiumos/tast/common/wifi/security/wpa"
+	"chromiumos/tast/remote/bundles/cros/wifi/wifiutil"
 	"chromiumos/tast/remote/wificell"
 	ap "chromiumos/tast/remote/wificell/hostapd"
 	"chromiumos/tast/rpc"
 	"chromiumos/tast/services/cros/chrome/uiauto/ossettings"
 	"chromiumos/tast/services/cros/chrome/uiauto/quicksettings"
+	"chromiumos/tast/services/cros/inputs"
 	"chromiumos/tast/services/cros/ui"
 	"chromiumos/tast/testing"
 )
 
-const networkSSID = "NetworkSSID"
+const networkPassphrase = "fourwordsalluppercase"
+
+type networkSecurity struct {
+	ssid    string
+	secured bool
+	factory security.ConfigFactory
+}
 
 func init() {
 	testing.AddTest(&testing.Test{
@@ -35,23 +45,44 @@ func init() {
 			"tast.cros.browser.ChromeService",
 			"tast.cros.chrome.uiauto.ossettings.OsSettingsService",
 			"tast.cros.chrome.uiauto.quicksettings.QuickSettingsService",
+			"tast.cros.inputs.KeyboardService",
 			"tast.cros.ui.AutomationService",
 			wificell.TFServiceName,
 		},
 		SoftwareDeps: []string{"chrome"},
 		Fixture:      "wificellFixtWithCapture",
+		Params: []testing.Param{{
+			Name: "open",
+			Val: networkSecurity{
+				ssid:    "OpenWiFi",
+				secured: false,
+				factory: base.NewConfigFactory(),
+			},
+		}, {
+			Name: "secured",
+			Val: networkSecurity{
+				ssid:    "SecureWiFi",
+				secured: true,
+				factory: wpa.NewConfigFactory(
+					networkPassphrase,
+					wpa.Mode(wpa.ModePureWPA),
+					wpa.Ciphers(wpa.CipherTKIP),
+				),
+			},
+		}},
 	})
 }
 
 func ConnectFromQuickSettings(ctx context.Context, s *testing.State) {
 	tf := s.FixtValue().(*wificell.TestFixture)
+	p := s.Param().(networkSecurity)
 
 	apInterface, err := tf.ConfigureAP(ctx, []ap.Option{
-		ap.SSID(networkSSID),
-		ap.Mode(ap.Mode80211a),
-		ap.Channel(64),
+		ap.SSID(p.ssid),
+		ap.Mode(ap.Mode80211g),
+		ap.Channel(1),
 		ap.SpectrumManagement(),
-	}, base.NewConfigFactory())
+	}, p.factory)
 	if err != nil {
 		s.Fatal("Failed to configure AP: ", err)
 	}
@@ -80,39 +111,50 @@ func ConnectFromQuickSettings(ctx context.Context, s *testing.State) {
 	}); err != nil {
 		s.Fatal("Failed to open Chrome on the DUT: ", err)
 	}
-
 	if _, err = qs.NavigateToNetworkDetailedView(ctx, &emptypb.Empty{}); err != nil {
-		s.Error("Failed to navigate to the detailed Network within Quick Settings: ", err)
+		s.Fatal("Failed to navigate to the detailed Network within Quick Settings: ", err)
 	}
 
 	networkFinder := &ui.Finder{
 		NodeWiths: []*ui.NodeWith{
-			{Value: &ui.NodeWith_NameContaining{NameContaining: networkSSID}},
+			{Value: &ui.NodeWith_NameContaining{NameContaining: p.ssid}},
 			{Value: &ui.NodeWith_First{First: true}},
 		},
-	}
-	if _, err := uiautomation.WaitUntilExists(
-		ctx, &ui.WaitUntilExistsRequest{Finder: networkFinder}); err != nil {
-		s.Fatal("Failed to find the network button: ", err)
 	}
 	if _, err := uiautomation.LeftClick(
 		ctx, &ui.LeftClickRequest{Finder: networkFinder}); err != nil {
 		s.Fatal("Failed to click the network button: ", err)
 	}
 
-	// TODO(b/244492317): Replace the function calls below once we have a single method
-	// that can confirm that a given network is connected.
-	if _, err := os.OpenNetworkDetailPage(ctx, &ossettings.OpenNetworkDetailPageRequest{
-		NetworkName: networkSSID,
-		NetworkType: ossettings.OpenNetworkDetailPageRequest_WIFI,
-	}); err != nil {
-		s.Fatal("Failed to navigate to the network detail page: ", err)
+	if s.Param().(networkSecurity).secured {
+		if _, err := uiautomation.LeftClick(
+			ctx, &ui.LeftClickRequest{Finder: wifiutil.PasswordFieldFinder}); err != nil {
+			s.Fatal("Failed to click the network button: ", err)
+		}
+
+		kb := inputs.NewKeyboardServiceClient(rpcClient.Conn)
+
+		if _, err := kb.Type(ctx, &inputs.TypeRequest{
+			Key: networkPassphrase,
+		}); err != nil {
+			s.Fatal("Failed to enter the password for the network: ", err)
+		}
+
+		if _, err := uiautomation.LeftClick(
+			ctx, &ui.LeftClickRequest{Finder: wifiutil.ConnectButtonFinder}); err != nil {
+			s.Fatal("Failed to click the connect button: ", err)
+		}
+		if _, err = qs.NavigateToNetworkDetailedView(ctx, &emptypb.Empty{}); err != nil {
+			s.Fatal("Failed to navigate to the detailed Network within Quick Settings: ", err)
+		}
 	}
 
 	networkConnectedStateFinder := &ui.Finder{
 		NodeWiths: []*ui.NodeWith{
 			{Value: &ui.NodeWith_NameContaining{NameContaining: "Connected"}},
+			{Value: &ui.NodeWith_HasClass{HasClass: "UnfocusableLabel"}},
 			{Value: &ui.NodeWith_Role{Role: ui.Role_ROLE_STATIC_TEXT}},
+			{Value: &ui.NodeWith_Ancestor{Ancestor: networkFinder}},
 		},
 	}
 	if _, err := uiautomation.WaitUntilExists(
@@ -121,12 +163,12 @@ func ConnectFromQuickSettings(ctx context.Context, s *testing.State) {
 	}
 
 	if _, err = os.Close(ctx, &emptypb.Empty{}); err != nil {
-		s.Error("Failed to close OS Settings: ", err)
+		s.Fatal("Failed to close OS Settings: ", err)
 	}
 	if _, err = chrome.Close(ctx, &emptypb.Empty{}); err != nil {
-		s.Error("Failed to close Chrome on the DUT: ", err)
+		s.Fatal("Failed to close Chrome on the DUT: ", err)
 	}
 	if err = rpcClient.Close(ctx); err != nil {
-		s.Error("Failed to close RPC client: ", err)
+		s.Fatal("Failed to close RPC client: ", err)
 	}
 }
