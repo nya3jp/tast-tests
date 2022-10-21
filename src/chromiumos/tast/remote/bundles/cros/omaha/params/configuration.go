66c9daa3c584402d8d8641d605e5e658d7c13131
Victor-Gabriel Savu <vsavu@google.com>
2022-10-21 12:45:32 +0000

    omaha: Simplify the omaha config
    Prepare for the CoLVIS integration by simplifying configuration.json.
    
    BUG=b:228927935
    TEST=tast run <ip> omaha.*
    
    Change-Id: I30ae484b2ef2e1dacb6ddd6aea96b4c4809b35a7

--- src/chromiumos/tast/remote/bundles/cros/omaha/params/configuration.go
+++ src/chromiumos/tast/remote/bundles/cros/omaha/params/configuration.go
@@ -7,26 +7,21 @@ package params
 import (
 	"encoding/json"
 	"io/ioutil"
+	"strconv"
 
 	"chromiumos/tast/errors"
 )
 
 // Configuration contains the expected state of Omaha.
 type Configuration struct {
-	// OldVersion is a version older that then any available stable and should
-	// update to the latest stable without any stepping stone.
-	OldVersion string
-
 	// ChromeOSVersionFromMilestone maps Chrome milestones to ChromeOS version prefixes.
 	ChromeOSVersionFromMilestone map[int]int
 
 	// ChromeOSLTRMilestoneWithMinimumMinor maps LTS milestones to LTR-only minor versions.
 	ChromeOSLTRMilestoneWithMinimumMinor map[int]int
 
-	// CurrentStableChrome is the current stable milestone of ChromeOS.
-	CurrentStableChrome int
-	// CurrentStableChrome is the next stable milestone of ChromeOS.
-	NextStableChrome int
+	// StableChrome is the current stable milestone of ChromeOS.
+	StableChrome int
 }
 
 // DumpToFile writes the device parameters to a file.
@@ -37,4 +32,50 @@ func (d *Configuration) DumpToFile(path string) error {
 	}
 
 	return ioutil.WriteFile(path, file, 0644)
+}
+
+// PreviousMilestone returns the previous milestone if the config contains it.
+func (d *Configuration) PreviousMilestone(milestone int) (int, error) {
+	// M95 does not exist, we still need to work.
+	if milestone == 96 {
+		if _, ok := d.ChromeOSVersionFromMilestone[94]; ok {
+			return 94, nil
+		}
+	}
+
+	if _, ok := d.ChromeOSVersionFromMilestone[milestone-1]; ok {
+		return milestone - 1, nil
+	}
+
+	return 0, errors.Errorf("no previous milestones of %d found", milestone)
+}
+
+// NextMilestone returns the next milestone if the config contains it.
+func (d *Configuration) NextMilestone(milestone int) (int, error) {
+	if _, ok := d.ChromeOSVersionFromMilestone[milestone-1]; ok {
+		return milestone - 1, nil
+	}
+	return 0, errors.Errorf("no next milestones of %d found", milestone)
+}
+
+// PreviousMilestoneOSVersion generates a ChromeOS version that matches the
+// previous milestone prefix.
+func (d *Configuration) PreviousMilestoneOSVersion(milestone int) (string, error) {
+	m, err := d.PreviousMilestone(milestone)
+	if err != nil {
+		return "", err
+	}
+
+	return strconv.FormatInt(int64(d.ChromeOSVersionFromMilestone[m]), 10) + ".0.0", nil
+}
+
+// NextMilestoneOSVersion generates a ChromeOS version that matches the
+// next milestone prefix.
+func (d *Configuration) NextMilestoneOSVersion(milestone int) (string, error) {
+	m, err := d.NextMilestone(milestone)
+	if err != nil {
+		return "", err
+	}
+
+	return strconv.FormatInt(int64(d.ChromeOSVersionFromMilestone[m]), 10) + ".0.0", nil
 }
