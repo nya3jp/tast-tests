913214d5d9fc40fe555a7ad7df8effcfc5316990
Danny Wang <wangdanny@google.com>
2022-10-26 00:09:08 +0000

    feedback: Combine tast test cases
    This cl combines previous tast test cases into one test. Description
    should always be included in the report. Screenshot should be included
    in the report when the screenshot checkbox is checked. Diagnostic data
    should be included in the report when the Diagnostic data checkbox is
    checked. Email should be included in the report by default. Attachment
    should be included in the report when the attach file checkbox is
    checked.
    
    BUG=b:185624798
    TEST=tast run <DUT> feedback.VerifyEssentialData.*
    
    Change-Id: Ice363e867236bae335a2afbdbef010e7455cfcd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3979161
    Reviewed-by: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Tested-by: Danny Wang <wangdanny@google.com>

--- src/chromiumos/tast/local/bundles/cros/feedback/verify_essential_data.go
+++ src/chromiumos/tast/local/bundles/cros/feedback/verify_essential_data.go
@@ -8,27 +8,35 @@ import (
 	"context"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/golang/protobuf/proto"
 
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
+	"chromiumos/tast/fsutil"
 	fpb "chromiumos/tast/local/bundles/cros/feedback/proto"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/localstate"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
-	"chromiumos/tast/local/chrome/uiauto/feedbackapp"
+	fa "chromiumos/tast/local/chrome/uiauto/feedbackapp"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/cryptohome"
 	"chromiumos/tast/testing"
 )
+
+const histogramsZip = "histograms.zip"
+const systemLogsZip = "system_logs.zip"
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         VerifyEssentialData,
 		LacrosStatus: testing.LacrosVariantUnneeded,
-		Desc:         "Verify description and screenshot values in the report",
+		Desc:         "Verify if essential data is in the report in different conditions",
 		Contacts: []string{
 			"wangdanny@google.com",
 			"zhangwenyu@google.com",
@@ -37,22 +45,26 @@ func init() {
 		},
 		Fixture:      "chromeLoggedInWithOsFeedbackSaveReportToLocalForE2ETesting",
 		Attr:         []string{"group:mainline", "informational"},
+		Data:         []string{fa.PngFile},
 		SoftwareDeps: []string{"chrome"},
 		Timeout:      5 * time.Minute,
 		Params: []testing.Param{{
-			Name: "include_screenshot",
+			Name: "share_data",
 			Val:  true,
 		}, {
-			Name: "not_include_screenshot",
+			Name: "not_share_data",
 			Val:  false,
 		}},
 	})
 }
 
-// VerifyEssentialData verifies if description and screenshot are
+// VerifyEssentialData verifies if essential data is
 // in the report in different conditions. Description should always be
-// included in the report and screenshot should be included in the report
-// when the screenshot checkbox is checked.
+// included in the report. Screenshot should be included in the report
+// when the screenshot checkbox is checked. Diagnostic data should be included
+// in the report when the Diagnostic data checkbox is checked. Email should be
+// included in the report by default. Attachment should be included in the report
+// when the attach file checkbox is checked.
 func VerifyEssentialData(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(*chrome.Chrome)
 
@@ -67,26 +79,95 @@ func VerifyEssentialData(ctx context.Context, s *testing.State) {
 	defer faillog.DumpUITreeWithScreenshotOnError(cleanupCtx, s.OutDir(), s.HasError, cr,
 		"ui_dump")
 
-	// Clean up in both beginning and the end.
+	downloadsPath, err := cryptohome.DownloadsPath(ctx, cr.NormalizedUser())
+	if err != nil {
+		s.Fatal("Failed to get user's Download path: ", err)
+	}
+
 	cleanUp := func() {
-		if err := os.RemoveAll(feedbackapp.ReportPath); err != nil {
+		if err := os.RemoveAll(fa.ReportPath); err != nil {
 			s.Error("Failed to remove feedback report: ", err)
 		}
+
+		files, err := ioutil.ReadDir(downloadsPath)
+		if err != nil {
+			s.Error("Failed to read files in Downloads: ", err)
+		} else {
+			for _, f := range files {
+				path := filepath.Join(downloadsPath, f.Name())
+				if err := os.RemoveAll(path); err != nil {
+					s.Errorf("Failed to RemoveAll(%v): %v", path, err)
+				}
+			}
+		}
 	}
+
+	// Clean up at both the beginning and the end to make sure
+	// the path is clean.
 	cleanUp()
 	defer cleanUp()
 
 	ui := uiauto.New(tconn).WithTimeout(20 * time.Second)
-	includeScreenshot := s.Param().(bool)
-
-	// Launch feedback app and go to share data page.
-	feedbackRootNode, err := feedbackapp.LaunchAndGoToShareDataPage(ctx, tconn)
-	if err != nil {
-		s.Fatal("Failed to launch feedback app and navigate to share data page: ", err)
+	shareData, castResult := s.Param().(bool)
+	if !castResult {
+		s.Fatal("Failed to cast param val to bool")
 	}
 
-	// Check the screenshot checkbox if needed.
-	if includeScreenshot {
+	// Copy the file to Downloads for uploading purpose.
+	fileName := fa.PngFile
+	if err := fsutil.CopyFile(
+		s.DataPath(fileName), filepath.Join(downloadsPath, fileName)); err != nil {
+		s.Fatal("Failed to copy file to Downloads: ", err)
+	}
+
+	// Launch feedback app and go to share data page.
+	feedbackRootNode, err := fa.LaunchAndGoToShareDataPage(ctx, tconn)
+	if err != nil {
+		s.Fatal("Failed to launch feedback app and navigating to share data page: ", err)
+	}
+
+	// Find add file button and click.
+	addFileButton := nodewith.NameContaining("Add file").Role(
+		role.Button).Ancestor(feedbackRootNode)
+	if err := ui.DoDefault(addFileButton)(ctx); err != nil {
+		s.Fatal("Failed to click add file button: ", err)
+	}
+
+	// Open Downloads dir and select the png file to upload.
+	if err := uiauto.Combine("Open Downloads dir and select PNG file",
+		ui.LeftClick(nodewith.Name("Downloads").Role(role.TreeItem)),
+		ui.LeftClick(nodewith.NameContaining(fileName).Role(role.StaticText).First()),
+		ui.LeftClick(nodewith.Name("Open").Role(role.Button)),
+	)(ctx); err != nil {
+		s.Fatal("Failed to open Downloads dir and select PNG file: ", err)
+	}
+
+	// Verify the uploaded png file exists.
+	fileFinder := nodewith.NameContaining(fileName).Role(
+		role.StaticText).Ancestor(feedbackRootNode)
+	if err := ui.WaitUntilExists(fileFinder)(ctx); err != nil {
+		s.Fatal("Failed to find png file: ", err)
+	}
+
+	// Find default selected email address.
+	var localState struct {
+		Emails []string `json:"LoggedInUsers"`
+	}
+
+	if err := localstate.Unmarshal(browser.TypeAsh, &localState); err != nil {
+		s.Fatal("Failed to extract Local State: ", err)
+	}
+
+	var selectedEmail string
+	for _, email := range localState.Emails {
+		if err := ui.WaitUntilExists(nodewith.NameContaining(email).First())(
+			ctx); err == nil {
+			selectedEmail = email
+		}
+	}
+
+	if shareData {
+		// Check the screenshot checkbox.
 		checkbox := nodewith.Role(role.CheckBox).Ancestor(feedbackRootNode).First()
 		if err := uiauto.Combine("Verify checkbox is unchecked and click it",
 			ui.WaitUntilExists(checkbox.Attribute("checked", "false")),
@@ -94,6 +175,38 @@ func VerifyEssentialData(ctx context.Context, s *testing.State) {
 			ui.WaitUntilExists(checkbox.Attribute("checked", "true")),
 		)(ctx); err != nil {
 			s.Fatal("Failed to change the checkbox state: ", err)
+		}
+	} else {
+		// Uncheck the share diagnostic data checkbox.
+		diagnosticDataCheckboxContainer := nodewith.Name("Send system and app info and metrics").Role(
+			role.GenericContainer).Ancestor(feedbackRootNode)
+		diagnosticDataCheckbox := nodewith.Role(role.CheckBox).Ancestor(diagnosticDataCheckboxContainer)
+		if err := uiauto.Combine("Uncheck the share diagnostic data checkbox",
+			ui.DoDefault(diagnosticDataCheckbox),
+			ui.WaitUntilExists(diagnosticDataCheckbox.Attribute("checked", "false")),
+		)(ctx); err != nil {
+			s.Fatal("Failed to uncheck the share diagnostic data checkbox: ", err)
+		}
+
+		// Choose not to include email.
+		emailDropdown := nodewith.Name("Select email").Role(role.ListBox)
+		dontIncludeEmailOption := nodewith.Name("anonymous user").Role(role.ListBoxOption)
+		if err := uiauto.Combine("choose not to include Email",
+			ui.LeftClickUntil(emailDropdown, ui.WithTimeout(
+				2*time.Second).WaitUntilExists(dontIncludeEmailOption)),
+			ui.LeftClick(dontIncludeEmailOption),
+		)(ctx); err != nil {
+			s.Fatal("Failed to choose not include Email: ", err)
+		}
+
+		// Uncheck the attach file checkbox.
+		attachFileCheckboxContainer := nodewith.Name("Attach file").Role(role.GenericContainer)
+		attachFileCheckbox := nodewith.Role(role.CheckBox).Ancestor(attachFileCheckboxContainer)
+		if err := uiauto.Combine("Uncheck the attach file checkbox",
+			ui.DoDefault(attachFileCheckbox),
+			ui.WaitUntilExists(attachFileCheckbox.Attribute("checked", "false")),
+		)(ctx); err != nil {
+			s.Fatal("Failed to uncheck the attach file checkbox: ", err)
 		}
 	}
 
@@ -113,7 +226,7 @@ func VerifyEssentialData(ctx context.Context, s *testing.State) {
 	var content []byte
 
 	if err := testing.Poll(ctx, func(ctx context.Context) error {
-		content, err = ioutil.ReadFile(feedbackapp.ReportPath)
+		content, err = ioutil.ReadFile(fa.ReportPath)
 		if err != nil {
 			return errors.Wrap(err, "failed to read report content")
 		}
@@ -128,20 +241,76 @@ func VerifyEssentialData(ctx context.Context, s *testing.State) {
 		s.Fatal("Failed to parse report: ", err)
 	}
 
-	// Get description and screenshot to verify their values.
+	// Verify if description is in the report.
 	reportDescription := report.GetCommonData().GetDescription()
-	reportScreenshot := report.GetScreenshot()
-
-	if reportDescription != feedbackapp.IssueText {
+	if reportDescription != fa.IssueText {
 		s.Fatal("Failed to get correct report description")
 	}
-	if includeScreenshot {
+
+	// Verify feedback report contains screenshot based on user selection.
+	reportScreenshot := report.GetScreenshot()
+	if shareData {
 		if reportScreenshot == nil {
 			s.Fatal("Failed to include the screenshot in the report")
 		}
 	} else {
 		if reportScreenshot != nil {
 			s.Fatal("Failed to not include the screenshot in the report")
+		}
+	}
+
+	// Verify feedback report contains system_log.zip and histograms.zip based on user selection.
+	histogramsExists := false
+	systemLogsExists := false
+	productSpecificBinaryData := report.GetProductSpecificBinaryData()
+	for _, element := range productSpecificBinaryData {
+		if element.GetName() == histogramsZip {
+			histogramsExists = true
+		}
+		if element.GetName() == systemLogsZip {
+			systemLogsExists = true
+		}
+	}
+	if shareData {
+		if !histogramsExists || !systemLogsExists {
+			s.Fatal("Failed to share system and app info and metrics")
+		}
+	} else {
+		if histogramsExists || systemLogsExists {
+			s.Fatal("Failed to not share system and app info and metrics")
+		}
+	}
+
+	// Verify feedback report contains email based on user selection.
+	userEmail := report.GetCommonData().GetUserEmail()
+	expectedEmail := selectedEmail
+	if shareData {
+		if userEmail != selectedEmail {
+			s.Fatalf("Expected email %s does not exist", expectedEmail)
+		}
+	} else {
+		if userEmail != "" {
+			s.Fatalf("Unexpected email %s does exist", expectedEmail)
+		}
+	}
+
+	// Verify feedback report contains selected file based on user selection.
+	// File will be compressed to a zip file.
+	zipFile := fileName + ".zip"
+	fileExist := false
+	for _, element := range productSpecificBinaryData {
+		if element.GetName() == zipFile {
+			fileExist = true
+			break
+		}
+	}
+	if shareData {
+		if !fileExist {
+			s.Fatal("Failed to verify selected file is in the report")
+		}
+	} else {
+		if fileExist {
+			s.Fatal("Failed to verify selected file is not in the report")
 		}
 	}
 }
