f6d6aab1e8bfa1e5386c900525495f07220ee159
Cristina Guerrero <crisguerrero@chromium.org>
2022-10-26 13:45:39 +0000

    tast-tests: Update NetwokConfigAPIBaseline
    OobeNetworkConfigBaseline checks the netconfig API works during OOBE.
    
    Rename and extend test to also check the API works after login. There
    are complex tests (like rollback) that rely on this API to work. The
    idea is adding this tests to the CQ to be able to detect issues in the
    network API timely.
    
    BUG=b:250876596
    TEST=tast run DUT network.ConfigBaseline.oobe
    tast run DUT network.ConfigBaseline.login
    
    Change-Id: I035129ef17882a4cbacf55e20c953e32d686aa63

--- src/chromiumos/tast/local/bundles/cros/network/config_baseline.go
+++ src/chromiumos/tast/local/bundles/cros/network/config_baseline.go
@@ -15,36 +15,71 @@ import (
 	"chromiumos/tast/testing"
 )
 
+type testParam struct {
+	useLogin bool
+}
+
 func init() {
 	testing.AddTest(&testing.Test{
-		Func:         OobeNetworkConfigBaseline,
+		Func:         ConfigBaseline,
 		LacrosStatus: testing.LacrosVariantUnneeded,
-		Desc:         "Use the CrosNetworkConfig API during OOBE to configure a basic network configuration and check it is setup as expected",
+		Desc:         "Use the CrosNetworkConfig API during OOBE and after login to configure a basic network configuration and check it is setup as expected",
 		Contacts: []string{
 			"crisguerrero@chromium.com", // Test author
 		},
 		SoftwareDeps: []string{"chrome"},
 		Attr:         []string{"group:mainline", "informational"},
 		Timeout:      1 * time.Minute,
+		Params: []testing.Param{{
+			Name: "oobe",
+			Val: testParam{
+				useLogin: false,
+			},
+		}, {
+			Name: "login",
+			Val: testParam{
+				useLogin: true,
+			},
+		},
+		},
 	})
 }
 
-// OobeNetworkConfigBaseline tests a basic network configuration during OOBE
+// ConfigBaseline tests a basic network configuration during OOBE or after login
 // using the CrosNetworkConfig API. This test is intended as a baseline example
-// of how to use such API during OOBE.
-func OobeNetworkConfigBaseline(ctx context.Context, s *testing.State) {
+// of how to use such API and make sure it works.
+// Various more complex tests rely on the network API to work, so it is
+// important it runs in the CQ to detect possible issues with it on time.
+func ConfigBaseline(ctx context.Context, s *testing.State) {
 	// Reserve ten seconds for cleanup.
 	cleanupCtx := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
 	defer cancel()
 
-	cr, err := chrome.New(ctx, chrome.DeferLogin())
+	param := s.Param().(testParam)
+	var opts []chrome.Option
+	if !param.useLogin {
+		// Defer login to use the API during OOBE.
+		opts = []chrome.Option{
+			chrome.DeferLogin(),
+		}
+	}
+	cr, err := chrome.New(ctx, opts...)
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
 	defer cr.Close(cleanupCtx)
 
-	api, err := netconfig.CreateOobeCrosNetworkConfig(ctx, cr)
+	// How we connect to the API depends on if we are in the OOBE or have access
+	// to a chrome instance after login.
+	var api *nc.CrosNetworkConfig
+	if param.useLogin {
+		// Connect using chrome.
+		api, err = netconfig.CreateLoggedInCrosNetworkConfig(ctx, cr)
+	} else {
+		// Connect using OOBE.
+		api, err = netconfig.CreateOobeCrosNetworkConfig(ctx, cr)
+	}
 	if err != nil {
 		s.Fatal("Failed to get network Mojo Object: ", err)
 	}
