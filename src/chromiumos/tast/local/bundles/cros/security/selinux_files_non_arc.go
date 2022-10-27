047d5d36de0daa5f98513af19873391596c82bc1
Hyung Tae Kim <hyungtaekim@chromium.org>
2022-10-26 17:34:37 -0700

    security: Lacros variants to SELinuxFilesARC and NonARC
    DO NOT SUBMIT it needs to confirm what files to add and how.
    
    We need to cover the rootfs chrome binary for the lacros variant test.
    
    BUG=b:253808823, b:254328916
    TEST=tast run $DUT security.SELinuxFilesARC*
    TEST=tast run $DUT security.SELinuxFilesNonARC*
    
    Change-Id: I0f6e36fc0b9aa1f0d4e528e23a8e3557748647ff

--- src/chromiumos/tast/local/bundles/cros/security/selinux_files_non_arc.go
+++ src/chromiumos/tast/local/bundles/cros/security/selinux_files_non_arc.go
@@ -8,39 +8,65 @@ import (
 	"context"
 
 	"chromiumos/tast/local/bundles/cros/security/selinux"
-	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/lacros"
+	"chromiumos/tast/local/chrome/lacros/lacrosfixt"
 	"chromiumos/tast/testing"
 )
 
 func init() {
 	testing.AddTest(&testing.Test{
-		Func: SELinuxFilesNonARC,
-		// TODO(b/254328916): Make sure we also cover the rootfs chrome binary in lacros variant tests.
-		LacrosStatus: testing.LacrosVariantNeeded,
+		Func:         SELinuxFilesNonARC,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Checks SELinux labels on Chrome-specific files on devices that don't support ARC",
 		Contacts:     []string{"fqj@chromium.org", "jorgelo@chromium.org", "chromeos-security@google.com"},
 		Attr:         []string{"group:mainline", "informational"},
 		SoftwareDeps: []string{"chrome", "selinux", "no_android"},
+		Params: []testing.Param{{
+			Val: browser.TypeAsh,
+		}, {
+			Name:              "lacros",
+			ExtraSoftwareDeps: []string{"lacros"},
+			Val:               browser.TypeLacros,
+		}},
 	})
 }
 
+type nonArcFileTestCase struct {
+	path      string // absolute file path
+	context   string // expected SELinux file context
+	recursive bool
+	filter    selinux.FileLabelCheckFilter
+}
+
 func SELinuxFilesNonARC(ctx context.Context, s *testing.State) {
-	cr, err := chrome.New(ctx)
-	defer cr.Close(ctx)
+	cr, err := browserfixt.NewChrome(ctx, browser.TypeLacros,
+		lacrosfixt.NewConfig(lacrosfixt.Selection(lacros.Rootfs)), // Check only RootFS Lacros
+	)
 	if err != nil {
 		s.Fatal("Failed to start Chrome: ", err)
 	}
-	for _, testArg := range []struct {
-		path      string // absolute file path
-		context   string // expected SELinux file context
-		recursive bool
-		filter    selinux.FileLabelCheckFilter
-	}{
+	defer cr.Close(ctx)
+
+	var testArgs []nonArcFileTestCase
+
+	testArgs = append(testArgs, []nonArcFileTestCase{
 		{path: "/opt/google/chrome/chrome", context: "chrome_browser_exec"},
 		{path: "/run/chrome/wayland-0", context: "wayland_socket"},
 		{path: "/run/session_manager", context: "cros_run_session_manager", recursive: true},
 		{path: "/var/log/chrome", context: "cros_var_log_chrome", recursive: true},
-	} {
+	}...)
+
+	// Append lacros test cases.
+	// Update the list below when new lacros specific files are added or removed.
+	if s.Param().(browser.Type) == browser.TypeLacros {
+		testArgs = append(testArgs, []nonArcFileTestCase{
+			{path: "/run/lacros/chrome", context: "chrome_browser_exec"},
+		}...)
+	}
+
+	for _, testArg := range testArgs {
 		filter := testArg.filter
 		if filter == nil {
 			filter = selinux.CheckAll
