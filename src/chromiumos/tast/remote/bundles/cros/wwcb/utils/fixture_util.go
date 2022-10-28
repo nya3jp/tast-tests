ed119e11fe0868aac330020b0a9114df139b5dba
Chris Wu <chriswu@allion.corp-partner.google.com>
2022-10-28 15:18:51 +0800

    Add utilities for remote test
    Change-Id: I0820c216ccb77aa3d699709ce6a873acf1d56615

---
+++ src/chromiumos/tast/remote/bundles/cros/wwcb/utils/fixture_util.go
@@ -0,0 +1,202 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package utils
+
+import (
+	"chromiumos/tast/testing"
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"go.bug.st/serial"
+	// "chromiumos/tast/remote/bundles/cros/wwcb/lib/go.bug.st/serial"
+)
+
+var (
+	fixtureUid = map[string]string{
+		"AUS19129_C01_01": "1912901",
+		"AUS19129_C01_02": "1912902",
+
+		"AHS20079_A00_01": "2007901",
+		"AHS20079_A00_02": "2007902",
+
+		"AUS20019_D00_01": "2001901",
+		"AUS20019_D00_02": "2001902",
+		"AUS20019_D00_03": "2001903",
+		"AUS20019_D00_04": "2001904",
+		"AUS20019_D00_05": "2001905",
+
+		"ADT21090_B00_01": "2109001",
+		"ADT21090_B00_02": "2109002",
+
+		"XXRJ45SW_X00_01": "j45sw01",
+	}
+
+	fixtureIsDisplay = map[string]bool{
+		"1912901": true,
+		"1912902": true,
+		"2007901": true,
+		"2007902": true,
+		"2001901": false,
+		"2001902": false,
+		"2001903": false,
+		"2001904": false,
+		"2001905": false,
+		"2109001": true,
+		"2109002": true,
+		"j45sw01": false,
+	}
+
+	fixtureCmd = map[string]map[string]string{
+		"1912901": {"off": "0", "on": "1", "flip": "2"},
+		"1912902": {"off": "0", "on": "1", "flip": "2"},
+		"2007901": {"off": "2", "on": "1"},
+		"2007902": {"off": "2", "on": "1"},
+		"2001901": {"off": "2", "on": "1"},
+		"2001902": {"off": "2", "on": "1"},
+		"2001903": {"off": "2", "on": "1"},
+		"2001904": {"off": "2", "on": "1"},
+		"2001905": {"off": "2", "on": "1"},
+		"2109001": {"off": "2", "on": "1"},
+		"2109002": {"off": "2", "on": "1"},
+		"j45sw01": {"off": "2", "on": "1"},
+	}
+
+	// key:text fixture uid , value:usb port id
+	fixtureOnline = make(map[string]string)
+)
+
+// InitFixture initializes fixtures.
+func InitFixture(ctx context.Context) error {
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		return error.wrap(err, "failed to get port list")
+	}
+
+	if len(ports) == 0 {
+		return error.wrap("No serial ports found!")
+	}
+
+	//
+	testing.Sleep(ctx, 3000)
+
+	// Print the list of detected ports
+	for _, port := range ports {
+		// Open the first serial port detected at 9600bps N81
+		mode := &serial.Mode{
+			BaudRate: 9600,
+		}
+
+		usb_port, err := serial.Open(port, mode)
+		if err != nil {
+			return error.wrap(err)
+		}
+
+		var t time.Duration = 3 * time.Second
+		usb_port.SetReadTimeout(t)
+
+		_, err = usb_port.Write([]byte("i"))
+		if err != nil {
+			return error.wrap(err)
+		}
+
+		// Read and print the response
+		buff := make([]byte, 1000)
+		for {
+			// Reads up to 100 bytes
+			n, err := usb_port.Read(buff)
+
+			if err != nil {
+				return error.wrap(err)
+				break
+			}
+
+			if n == 0 {
+				fmt.Println("\nEOF")
+				break
+			}
+
+			mString := string(buff[:n])
+			mString = strings.Replace(mString, "\r", "", -1)
+			res := strings.Split(mString, "\n")
+
+			for _, s := range res {
+				if len(s) > 22 && strings.Count(s[0:15], "_") == 2 {
+					serial := s[0:15]
+					fixture_id := fmt.Sprintf("test fixture id: %s => port: %s \n", serial, port)
+					testing.ContextLog(ctx, fixture_id)
+
+					uid, found := fixtureUid[serial]
+
+					if found {
+						fixtureOnline[uid] = port
+					} else {
+						s := fmt.Sprintf("serial %s is not in fixture list.", serial)
+						return error.wrap(err, s)
+					}
+				}
+			}
+		}
+		usb_port.Close()
+	}
+}
+
+func ControlFixture(ctx context.Context, uid string, cmd string) error {
+	s := fmt.Sprintf("Fixture '%s' set '%s' ", uid, cmd)
+	testing.ContextLog(ctx, s)
+
+	port, found := fixtureOnline[uid]
+
+	if !found {
+		s := fmt.Sprintf("uid %s is not in online fixture list.", uid)
+		return error.wrap(s)
+	}
+
+	mode := &serial.Mode{
+		BaudRate: 9600,
+	}
+
+	usb_port, err := serial.Open(port, mode)
+	if err != nil {
+		return error.wrap(err)
+	}
+
+	var t time.Duration = 3 * time.Second
+	usb_port.SetReadTimeout(t)
+
+	cmdNum := fixtureCmd[uid][cmd]
+
+	_, err = usb_port.Write([]byte(cmdNum))
+	if err != nil {
+		return error.wrap(err)
+	}
+
+	usb_port.Close()
+
+	// sleep for test fixture wake up
+	t = 7 * time.Second
+	time.Sleep(t)
+}
+
+func OpenAllFixture(ctx context.Context) {
+	for uid := range fixtureOnline {
+		ControlFixture(ctx, uid, "on")
+	}
+}
+
+func CloseAllFixture(ctx context.Context) {
+	for uid := range fixtureOnline {
+		ControlFixture(ctx, uid, "off")
+	}
+}
+
+func PrintAllFixture(ctx context.Context) {
+	for uid, port := range fixtureOnline {
+		s := fmt.Sprintf("print all fixture id: %s => port: %s \n", uid, port)
+		testing.ContextLog(ctx, s)
+	}
+}
