54ad85be601ea860119a1947cb209ca2a3168333
Artyom Chen <artyomchen@google.com>
2022-10-28 10:26:59 +0000

    policy: Check the lid close for LockOnIdleSuspend
    At present ChromeOsLockOnIdleSuspend only checks the button states
    on the settings page. Add the extra check for the case
    when the lid is closed on the device.
    
    BUG=chromium:1264764
    TEST=tast run <dut> policy.ChromeOsLockOnIdleSuspend
    TEST=fast_build.sh -T in cros chroot
    
    Change-Id: I2a752bc3da5494c48fe77a48640e4b3a9d74efb7

--- src/chromiumos/tast/local/bundles/cros/policy/chrome_os_lock_on_idle_suspend.go
+++ src/chromiumos/tast/local/bundles/cros/policy/chrome_os_lock_on_idle_suspend.go
@@ -6,19 +6,25 @@ package policy
 
 import (
 	"context"
+	"time"
 
+	pmpb "chromiumos/system_api/power_manager_proto"
 	"chromiumos/tast/common/fixture"
 	"chromiumos/tast/common/pci"
 	"chromiumos/tast/common/policy"
 	"chromiumos/tast/common/policy/fakedms"
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto/checked"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
+	"chromiumos/tast/local/chrome/uiauto/lockscreen"
 	"chromiumos/tast/local/chrome/uiauto/nodewith"
 	"chromiumos/tast/local/chrome/uiauto/restriction"
 	"chromiumos/tast/local/chrome/uiauto/role"
+	"chromiumos/tast/local/input"
 	"chromiumos/tast/local/policyutil"
 	"chromiumos/tast/local/policyutil/fixtures"
+	"chromiumos/tast/local/power"
 	"chromiumos/tast/testing"
 )
 
@@ -26,7 +32,7 @@ func init() {
 	testing.AddTest(&testing.Test{
 		Func:         ChromeOsLockOnIdleSuspend,
 		LacrosStatus: testing.LacrosVariantUnneeded,
-		Desc:         "Behavior of ChromeOsLockOnIdleSuspend policy, checking the correspoding toggle button states (restriction and checked) after setting the policy",
+		Desc:         "Behavior of ChromeOsLockOnIdleSuspend policy",
 		Contacts: []string{
 			"gabormagda@google.com", // Test author
 		},
@@ -44,29 +50,60 @@ func ChromeOsLockOnIdleSuspend(ctx context.Context, s *testing.State) {
 	cr := s.FixtValue().(chrome.HasChrome).Chrome()
 	fdms := s.FixtValue().(fakedms.HasFakeDMS).FakeDMS()
 
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, 20*time.Second)
+	defer cancel()
+
+	tconn, err := cr.TestAPIConn(ctx)
+	if err != nil {
+		s.Fatal("Failed to create Test API connection: ", err)
+	}
+
+	emitter, err := power.NewPowerManagerEmitter(ctx)
+	if err != nil {
+		s.Fatal("Unable to create power manager emitter: ", err)
+	}
+	defer func(ctx context.Context) {
+		if err := emitter.Stop(ctx); err != nil {
+			s.Log("Unable to stop emitter: ", err)
+		}
+	}(cleanupCtx)
+
+	kb, err := input.Keyboard(ctx)
+	if err != nil {
+		s.Fatal("Failed creating keyboard: ", err)
+	}
+	defer kb.Close()
+
+	const lockTimeout = 5 * time.Second
+
 	for _, param := range []struct {
 		name            string
+		wantLockDevice  bool
 		wantRestriction restriction.Restriction           // wantRestriction is the wanted restriction state of the checkboxes in Browsing history.
 		wantChecked     checked.Checked                   // wantChecked is the wanted checked state of the checkboxes in Browsing history.
-		value           *policy.ChromeOsLockOnIdleSuspend // value is the value of the policy.
+		policyVal       *policy.ChromeOsLockOnIdleSuspend // crosLockOnIdleSuspendPolicy is the value of the ChromeOsLockOnIdleSuspend policy.
 	}{
 		{
 			name:            "forced",
+			wantLockDevice:  true,
 			wantRestriction: restriction.Disabled,
 			wantChecked:     checked.True,
-			value:           &policy.ChromeOsLockOnIdleSuspend{Val: true},
+			policyVal:       &policy.ChromeOsLockOnIdleSuspend{Val: true},
 		},
 		{
 			name:            "disabled",
+			wantLockDevice:  false,
 			wantRestriction: restriction.Disabled,
 			wantChecked:     checked.False,
-			value:           &policy.ChromeOsLockOnIdleSuspend{Val: false},
+			policyVal:       &policy.ChromeOsLockOnIdleSuspend{Val: false},
 		},
 		{
 			name:            "unset",
+			wantLockDevice:  false,
 			wantRestriction: restriction.None,
 			wantChecked:     checked.False,
-			value:           &policy.ChromeOsLockOnIdleSuspend{Stat: policy.StatusUnset},
+			policyVal:       &policy.ChromeOsLockOnIdleSuspend{Stat: policy.StatusUnset},
 		},
 	} {
 		s.Run(ctx, param.name, func(ctx context.Context, s *testing.State) {
@@ -78,7 +115,7 @@ func ChromeOsLockOnIdleSuspend(ctx context.Context, s *testing.State) {
 			}
 
 			// Update policies.
-			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.value}); err != nil {
+			if err := policyutil.ServeAndVerify(ctx, fdms, cr, []policy.Policy{param.policyVal}); err != nil {
 				s.Fatal("Failed to update policies: ", err)
 			}
 
@@ -91,6 +128,42 @@ func ChromeOsLockOnIdleSuspend(ctx context.Context, s *testing.State) {
 				Checked(param.wantChecked).
 				Verify(); err != nil {
 				s.Error("Unexpected OS settings state: ", err)
+			}
+
+			// Close the lid and check if the policy works correctly.
+			// TODO(crbug.com/1264764): check if the policy works correctly when the user is idle and the device suspends.
+			eventType := pmpb.InputEvent_LID_CLOSED
+			if err := emitter.EmitInputEvent(ctx, &pmpb.InputEvent{Type: &eventType}); err != nil {
+				s.Fatal("Send LID_CLOSED failed: ", err)
+			}
+			// Defer the screen unlock to ensure subsequent tests aren't affected by the screen remaining locked.
+			defer func() {
+				eventType := pmpb.InputEvent_LID_OPEN
+				if err := emitter.EmitInputEvent(cleanupCtx, &pmpb.InputEvent{Type: &eventType}); err != nil {
+					s.Fatal("Send LID_OPEN failed: ", err)
+				}
+
+				const authTimeout = 5 * time.Second
+
+				s.Log("Unlocking screen by typing the given password")
+
+				if err := kb.Type(cleanupCtx, fixtures.Password+"\n"); err != nil {
+					s.Fatal("Typing the password failed: ", err)
+				}
+				if st, err := lockscreen.WaitState(cleanupCtx, tconn, func(st lockscreen.State) bool { return !st.Locked }, authTimeout); err != nil {
+					s.Fatalf("Waiting for screen to be unlocked failed: %v (last status %+v)", err, st)
+				}
+			}()
+
+			st, err := lockscreen.WaitState(ctx, tconn, func(st lockscreen.State) bool { return st.Locked && st.ReadyForPassword }, lockTimeout)
+			screenLocked := st.Locked && st.ReadyForPassword
+
+			if screenLocked && !param.wantLockDevice {
+				s.Fatal("Screen should not be locked: ", err)
+			}
+
+			if !screenLocked && param.wantLockDevice {
+				s.Fatal("Screen should be locked: ", err)
 			}
 		})
 	}
