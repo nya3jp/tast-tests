6feccb8baf9e9dc31dc97d040d70079863fc195c
Judy Hsiao <judyhsiao@chromium.org>
2022-10-31 07:24:50 +0000

    AlsaConformnace: skip audio_unstable and test code cleanup
    1. Skip audio_unstable platforms for audio tests.
    2. Rename unstableModels to alsaConformanceUnstableModels as the varibale
       is shared among the audio package. Rename it to avoid naming conflict.
    3. Rename AlsaConformance.stable to AlsaConformance by removing the "Name"
       attribute in the testing.Param.
    
    BUG=b:234534871
    TEST=tast run $DUT audio.ALSAConformance
    
    Change-Id: Id581fff389e8dded4f829cdc85e1692fffca762d

--- src/chromiumos/tast/local/bundles/cros/audio/alsa_conformance.go
+++ src/chromiumos/tast/local/bundles/cros/audio/alsa_conformance.go
@@ -41,7 +41,7 @@ import (
 // TODO(b/249023249) : remove "vell" when b/249023249 is fixed.
 // TODO(b/249207920) : remove "astronaut", "blacktip", "blacktip360", "epaulette", "lava", "nasher360", "rabbid", "robo", "robo360", "santa", "whitetip" when b/249207920 is fixed.
 // TODO(b/250468510) : remove "hana" when b/250468510 is fixed.
-var unstableModels = []string{"chronicler", "nautilus", "nautiluslte", "soraka", "karma", "beetley", "redrix", "gimble", "primus", "anahera", "babymega", "babytiger", "blacktiplte", "taniks", "bob", "nasher", "sasukette", "vell", "astronaut", "blacktip", "blacktip360", "epaulette", "lava", "nasher360", "rabbid", "robo", "robo360", "santa", "whitetip", "hana"}
+var alsaConformanceUnstableModels = []string{"chronicler", "nautilus", "nautiluslte", "soraka", "karma", "beetley", "redrix", "gimble", "primus", "anahera", "babymega", "babytiger", "blacktiplte", "taniks", "bob", "nasher", "sasukette", "vell", "astronaut", "blacktip", "blacktip360", "epaulette", "lava", "nasher360", "rabbid", "robo", "robo360", "santa", "whitetip", "hana"}
 
 // TODO(b/136614687): Relex the criteria for grunt devices, the audio still sounds fine as CRAS can compensate the rate, if the rate error is not huge.
 var relexedCriteriaModels = []string{"aleena", "barla", "careena", "kasumi", "kasumi360", "liara", "treeya360", "treeya"}
@@ -56,12 +56,16 @@ func init() {
 		Timeout:      10 * time.Minute,
 		Params: []testing.Param{
 			{
-				Name:              "stable",
-				ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(unstableModels...)),
+				ExtraSoftwareDeps: []string{"audio_stable"},
+				ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(alsaConformanceUnstableModels...)),
+			}, {
+				Name:              "unstable_platform",
+				ExtraSoftwareDeps: []string{"audio_unstable"},
+				ExtraAttr:         []string{"informational"},
 			},
 			{
 				Name:              "unstable",
-				ExtraHardwareDeps: hwdep.D(hwdep.Model(unstableModels...)),
+				ExtraHardwareDeps: hwdep.D(hwdep.Model(alsaConformanceUnstableModels...)),
 				ExtraAttr:         []string{"informational"},
 			},
 		},
