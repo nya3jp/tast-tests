3babc8036c0a79134e577e923a20cb2fd3552bda
pjlee <pjlee@google.com>
2022-10-31 20:32:01 +0000

    arc/gio: Add move actions to GIO perf test
    Add GIO move action statistics to be captured in the GIO perf test, in
    addition to the existing GIO tap action statistics.
    
    BUG=b:256672718
    TEST=./fast_build.sh -T && tast run $DUT arc.InputOverlayPerf*
    
    Change-Id: I5338c7e4875ee4d640490a8f8e1ce3ac045c9ef4

--- src/chromiumos/tast/local/bundles/cros/arc/gio/testutil.go
+++ src/chromiumos/tast/local/bundles/cros/arc/gio/testutil.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -258,7 +259,7 @@ func TapOverlayButton(kb *input.KeyboardEventWriter, key string, params *TestPar
 
 // PopulateReceivedTimes populates the given array of events with event timestamps,
 // as presented in logcat.
-func PopulateReceivedTimes(ctx context.Context, params TestParams, numLines int) ([]inputlatency.InputEvent, error) {
+func PopulateReceivedTimes(ctx context.Context, params TestParams, numLines int, regex *regexp.Regexp) ([]inputlatency.InputEvent, error) {
 	out, err := params.Arc.OutputLogcatGrep(ctx, "InputOverlayPerf")
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to execute logcat command")
@@ -270,7 +271,7 @@ func PopulateReceivedTimes(ctx context.Context, params TestParams, numLines int)
 	}
 	// Make sure that the length of the array is at least as long as expected.
 	if len(lines) < numLines {
-		return nil, errors.Errorf("only %v lines returned by logcat: %s", len(lines), lines[0])
+		return nil, errors.Errorf("only %d lines returned by logcat; wanted %d lines", len(lines), numLines)
 	}
 	lines = lines[len(lines)-numLines:]
 
@@ -284,6 +285,11 @@ func PopulateReceivedTimes(ctx context.Context, params TestParams, numLines int)
 
 	events := make([]inputlatency.InputEvent, 0, numLines)
 	for _, line := range lines {
+		// Throw away the line if it does not match the given regex.
+		if len(regex.FindAllString(line, -1)) != 1 {
+			continue
+		}
+
 		lineSplit := strings.Split(line, " ")
 		timestamp, err := strconv.ParseInt(lineSplit[len(lineSplit)-1], 10, 64)
 		if err != nil {
