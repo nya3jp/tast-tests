026e72245a916e18d4a5dde46c2ca7d1b1e4865c
Jane Yang <jane.yang@cienet.com>
2022-10-31 11:35:51 +0800

    cuj: Add Essential and Advanced tiers to cuj tier
    BUG=b:256950007
    TEST=fast_build.sh -T
    
    Change-Id: Id46697a1d21749fc7c9560bdf40daa0fa706ba84

--- src/chromiumos/tast/local/chrome/cuj/tier.go
+++ src/chromiumos/tast/local/chrome/cuj/tier.go
@@ -8,6 +8,8 @@ package cuj
 type Tier int
 
 const (
+	// TODO: When basic, plus, premium are no longer used, remove them.
+
 	// Basic is the test tier covering some simple CUJ test scenarios. Basic tier CUJ tests are supposed
 	// to run on most of the DUT models (both high-end and low-end), if not all.
 	Basic Tier = iota
@@ -16,4 +18,13 @@ const (
 	// Premium is the test tier covering the most complex test scenarios. CUJ tests in premium tier will
 	// drive the DUT to use more of its system resources.
 	Premium
+
+	// Essential and Advanced are the tiers used for Spera V2.
+
+	// Essential is the test tier covering some simple CUJ test scenarios. Essential tier CUJ tests are supposed
+	// to run on most of the DUT models (both high-end and low-end), if not all.
+	Essential
+	// Advanced is the test tier covering the most complex test scenarios. CUJ tests in advanced tier will
+	// drive the DUT to use more of its system resources.
+	Advanced
 )
