09eac804813b65bee1d2f8686201311fa6ebbf02
Judy Hsiao <judyhsiao@chromium.org>
2022-10-31 06:26:39 +0000

    CrasRecordQuality: promote test to critical
    BUG=b:230695223
    TEST=tast run <dut> audio.CrasRecordQuality
    
    Change-Id: I4f20f83557c5ee620cfdbef99b578a383144bb6e

--- src/chromiumos/tast/local/bundles/cros/audio/cras_record_quality.go
+++ src/chromiumos/tast/local/bundles/cros/audio/cras_record_quality.go
@@ -29,11 +29,10 @@ func init() {
 		Desc:         "Verifies recorded samples from CRAS are correct",
 		Contacts:     []string{"yuhsuan@chromium.org", "cychiang@chromium.org"},
 		HardwareDeps: hwdep.D(hwdep.Microphone()),
-		Attr:         []string{"group:mainline", "informational"},
+		Attr:         []string{"group:mainline"},
 		Timeout:      3 * time.Minute,
 		Params: []testing.Param{{
 			ExtraSoftwareDeps: []string{"audio_stable"},
-			Attr:              []string{"group:mainline"},
 			ExtraHardwareDeps: hwdep.D(hwdep.SkipOnModel(crasRecordQualityUnstableModels...)),
 		}, {
 			Name:              "unstable_platform",
