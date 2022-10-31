0ecbc686041c86a3711575b20ce790ca48e612e4
Chris Wu <chriswu@allion.corp-partner.google.com>
2022-10-31 10:22:45 +0800

    utilities:WWCB remote test utitity function
    The utilities is for specific component
    
    BUG=b:256087525
    TEST=tast run -var=DockingID=1912901 -var=ExtDispID=2109001 192.168.1.164 wwcb.Dock1Persistent
    
    Change-Id: Id84a3e7d8f9c9aabacb4b683c59c28b0250f4d3f

---
+++ src/chromiumos/tast/remote/bundles/cros/wwcb/utils/ippower_util.go
@@ -0,0 +1,63 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package utils used to do some component excution function.
+package utils
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+var (
+	wwcbIP   = "192.168.1.168"
+	user     = "admin"
+	password = "12345678"
+)
+
+// OpenIppower is for open ip power.
+func OpenIppower(ctx context.Context, ports []int) error {
+	myport := ""
+	for _, port := range ports {
+		if port < 1 || port > 4 {
+			log.Fatal("the port format error.")
+		} else {
+			s := fmt.Sprintf("+p6%v=1", port)
+			myport += s
+		}
+	}
+
+	url := fmt.Sprintf("http://%s/set.cmd?user=%s+pass=%s+cmd=setpower%s", wwcbIP, user, password, myport)
+	testing.ContextLogf(ctx, "request: %s", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		return errors.Wrap(err, "failed to send request")
+	}
+	defer resp.Body.Close()
+}
+
+// CloseIppower is for close ip power.
+func CloseIppower(ctx context.Context, ports []int) error {
+	myport := ""
+	for _, port := range ports {
+		if port < 1 || port > 4 {
+			return errors.New("the port format error")
+		}
+		s := fmt.Sprintf("+p6%v=0", port)
+		myport += s
+	}
+
+	url := fmt.Sprintf("http://%s/set.cmd?user=%s+pass=%s+cmd=setpower%s", wwcbIP, user, password, myport)
+	testing.ContextLogf(ctx, "request: %s", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		return errors.Wrap(err, "failed to send request")
+	}
+	defer resp.Body.Close()
+}
