e6da273c67090b07cb10884806f9f636cb1a68a8
Kartik Hegde <khegde@google.com>
2022-11-01 02:10:24 +0000

    tast: Add baseline network health API test
    BUG=b:256722369
    TEST=tast run localhost:2200 network.NetworkHealthGetActiveNetworks
    
    Change-Id: I3fe7f434c64c99e4c3f7db2621571ce0a7af266c

---
+++ src/chromiumos/tast/local/bundles/cros/network/health_get_network_list.go
@@ -0,0 +1,52 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package network
+
+import (
+	"context"
+
+	"chromiumos/tast/local/bundles/cros/network/health"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HealthGetNetworkList,
+		LacrosStatus: testing.LacrosVariantNeeded,
+		Desc:         "Validates that NetworkHealth API accurately gets networks",
+		Contacts: []string{
+			"khegde@chromium.org",            // test maintainer
+			"stevenjb@chromium.org",          // network-health tech lead
+			"cros-network-health@google.com", // network-health team
+		},
+		SoftwareDeps: []string{"chrome", "no_qemu"},
+		Attr:         []string{"group:mainline", "informational"},
+		Fixture:      "shillReset",
+	})
+}
+
+// HealthGetNetworkList validates that the NetworkHealth API correctly retrieves
+// networks.
+func HealthGetNetworkList(ctx context.Context, s *testing.State) {
+	cr, err := chrome.New(ctx)
+	if err != nil {
+		s.Fatal("Failed start Chrome: ", err)
+	}
+	defer cr.Close(ctx)
+
+	netConn, err := health.CreateLoggedInNetworkHealth(ctx, cr)
+	if err != nil {
+		s.Fatal("Failed to get network Mojo Object: ", err)
+	}
+	defer netConn.Close(ctx)
+
+	networks, err := netConn.GetNetworkList(ctx)
+	if err != nil {
+		s.Fatal("Failed to run GetNetworkList: ", err)
+	}
+
+	s.Log("Length of networks is ", len(networks))
+}
