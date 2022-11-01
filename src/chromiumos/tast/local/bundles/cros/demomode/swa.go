26f904b82c22af286a0e61c374aa75db62d3ceba
Jackson Tadie <jacksontadie@google.com>
2022-11-01 00:34:33 +0000

    Modify demomode.SWA test to also verify offline functionality
    BUG=b:256043585
    TEST=tast run <device-ip> demomode.SWA.{offline,online}
    
    Change-Id: I921c56f9b94deed38556111bae2611b90fb7b929

--- src/chromiumos/tast/local/bundles/cros/demomode/swa.go
+++ src/chromiumos/tast/local/bundles/cros/demomode/swa.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
 	"chromiumos/tast/local/chrome/uiauto"
 	"chromiumos/tast/local/chrome/uiauto/faillog"
@@ -17,6 +18,7 @@ import (
 	"chromiumos/tast/local/chrome/uiauto/role"
 	"chromiumos/tast/local/coords"
 	"chromiumos/tast/local/demomode/fixture"
+	"chromiumos/tast/local/network"
 	"chromiumos/tast/testing"
 )
 
@@ -33,74 +35,96 @@ func init() {
 		// We require "arc" and "chrome_internal" because the ARC TOS screen
 		// is only shown for chrome-branded builds when the device is ARC-capable.
 		SoftwareDeps: []string{"chrome", "chrome_internal", "arc", "tpm"},
+		Params: []testing.Param{{
+			Name: "online",
+			Val:  true, // shouldRunOnline
+		}, {
+			Name: "offline",
+			Val:  false, // shouldRunOnline
+		}},
 	})
 }
 
 func SWA(ctx context.Context, s *testing.State) {
-	cr, err := chrome.New(ctx,
-		chrome.NoLogin(),
-		chrome.ARCSupported(),
-		chrome.KeepEnrollment(),
-		chrome.EnableFeatures("DemoModeSWA"),
-		// --force-devtools-available forces devtools on regardless of policy (devtools is
-		// disabled in Demo Mode policy) to support connecting to the test API extension.
-		//
-		// --component-updater=test-request adds a "test-request" parameter to Omaha
-		// update requests, causing the fetched Demo Mode App component to come from a
-		// test cohort.
-		chrome.ExtraArgs("--force-devtools-available", "--component-updater=test-request"))
-	if err != nil {
-		s.Fatal("Failed to restart Chrome: ", err)
+	// Behavior that will be tested both online and offline.
+	restartChromeAndVerifySWA := func(ctx context.Context) error {
+		cr, err := chrome.New(ctx,
+			chrome.NoLogin(),
+			chrome.ARCSupported(),
+			chrome.KeepEnrollment(),
+			chrome.EnableFeatures("DemoModeSWA"),
+			// --force-devtools-available forces devtools on regardless of policy (devtools is
+			// disabled in Demo Mode policy) to support connecting to the test API extension.
+			//
+			// --component-updater=test-request adds a "test-request" parameter to Omaha
+			// update requests, causing the fetched Demo Mode App component to come from a
+			// test cohort.
+			chrome.ExtraArgs("--force-devtools-available", "--component-updater=test-request"))
+		if err != nil {
+			return errors.Wrap(err, "failed to restart Chrome")
+		}
+
+		clearUpCtx := ctx
+		ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
+		defer cancel()
+		defer cr.Close(clearUpCtx)
+
+		tconn, err := cr.TestAPIConn(ctx)
+		if err != nil {
+			return errors.Wrap(err, "failed to create the test API connection")
+		}
+		defer faillog.DumpUITreeOnError(clearUpCtx, s.OutDir(), s.HasError, tconn)
+		ui := uiauto.New(tconn).WithTimeout(100 * time.Second)
+
+		// Verify that splash screen has disappeared before moving mouse.
+		splashScreen := nodewith.ClassName("WallpaperView").Ancestor(nodewith.ClassName("AlwaysOnTopWallpaperContainer"))
+		if err := ui.WaitUntilGone(splashScreen)(ctx); err != nil {
+			return errors.Wrap(err, "failed to wait until splash screen is gone")
+		}
+
+		s.Log("Waiting for Demo Mode App to launch")
+		demoApp := nodewith.Name("Demo Mode App").First()
+		if err := ui.WaitUntilExists(demoApp)(ctx); err != nil {
+			return errors.Wrap(err, "failed to wait until Demo App exists")
+		}
+
+		// Verify app is fullscreen (Attract Loop) by checking that the toolbar
+		// is not present
+		s.Log("Confirming that app is in fullscreen mode")
+		toolbar := nodewith.Role(role.Toolbar).Ancestor(demoApp)
+		if err := ui.WaitUntilGone(toolbar)(ctx); err != nil {
+			return errors.Wrap(err, "failed to confirm that the toolbar is not present")
+		}
+
+		pc := pointer.NewMouse(tconn)
+		defer pc.Close()
+
+		demoAppLocation, _ := ui.Location(ctx, demoApp)
+
+		// Move mouse (arbitrarily) from center of demo app to screen corner to
+		// trigger interaction, breaking fullscreen Attract Loop.
+		if err := pc.Drag(
+			demoAppLocation.CenterPoint(),
+			pc.DragTo(coords.NewPoint(0, 0), 1*time.Second))(ctx); err != nil {
+			return errors.Wrap(err, "failed to drag mouse across screen")
+		}
+
+		// Assert that app is now windowed by presence of toolbar.
+		s.Log("Waiting for windowed Highlights mode")
+		if err := ui.WaitUntilExists(toolbar)(ctx); err != nil {
+			return errors.Wrap(err, "failed to wait for the toolbar to be present")
+		}
+		return nil
 	}
 
-	clearUpCtx := ctx
-	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
-	defer cancel()
-	defer cr.Close(clearUpCtx)
-
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to create the test API connection: ", err)
-	}
-	defer faillog.DumpUITreeOnError(clearUpCtx, s.OutDir(), s.HasError, tconn)
-	ui := uiauto.New(tconn).WithTimeout(100 * time.Second)
-
-	// Verify that splash screen has disappeared before moving mouse.
-	splashScreen := nodewith.ClassName("WallpaperView").Ancestor(nodewith.ClassName("AlwaysOnTopWallpaperContainer"))
-	if err := ui.WaitUntilGone(splashScreen)(ctx); err != nil {
-		s.Fatal("Failed to wait until splash screen is gone: ", err)
-	}
-
-	s.Log("Waiting for Demo Mode App to launch")
-	demoApp := nodewith.Name("Demo Mode App").First()
-	if err := ui.WaitUntilExists(demoApp)(ctx); err != nil {
-		s.Fatal("Failed to wait until Demo App exists: ", err)
-	}
-
-	// Verify app is fullscreen (Attract Loop) by checking that the toolbar
-	// is not present
-	s.Log("Confirming that app is in fullscreen mode")
-	toolbar := nodewith.Role(role.Toolbar).Ancestor(demoApp)
-	if err := ui.WaitUntilGone(toolbar)(ctx); err != nil {
-		s.Fatal("Failed to confirm that the toolbar is not present: ", err)
-	}
-
-	pc := pointer.NewMouse(tconn)
-	defer pc.Close()
-
-	demoAppLocation, _ := ui.Location(ctx, demoApp)
-
-	// Move mouse (arbitrarily) from center of demo app to screen corner to
-	// trigger interaction, breaking fullscreen Attract Loop.
-	if err := pc.Drag(
-		demoAppLocation.CenterPoint(),
-		pc.DragTo(coords.NewPoint(0, 0), 1*time.Second))(ctx); err != nil {
-		s.Fatal("Failed to drag mouse across screen: ", err)
-	}
-
-	// Assert that app is now windowed by presence of toolbar.
-	s.Log("Waiting for windowed Highlights mode")
-	if err := ui.WaitUntilExists(toolbar)(ctx); err != nil {
-		s.Fatal("Failed to wait for the toolbar to be present: ", err)
+	shouldRunOnline := s.Param().(bool)
+	if shouldRunOnline {
+		if err := restartChromeAndVerifySWA(ctx); err != nil {
+			s.Fatal("Failed to verify SWA functionality online: ", err)
+		}
+	} else {
+		if err := network.ExecFuncOnChromeOffline(ctx, restartChromeAndVerifySWA); err != nil {
+			s.Fatal("Failed to verify SWA functionality offline: ", err)
+		}
 	}
 }
