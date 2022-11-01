06d864ec749b58e33b5ec1ef701793773a419546
Kaidong Wang <kaidong@google.com>
2022-11-01 23:34:42 +0000

    tast-tests: Add WPA vs 802.1x to wifi.MultiAuth
    wifi.MultiAuth tests the ability to select network correctly among APs
    with similar network configurations, by configuring two APs with the
    same SSID/channel/mode but different security config and connecting to
    each in turn. It only tests the open vs WPA test case.
    
    Add WPA vs 802.1x test case to wifi.MultiAuth. In WPA-PSK, all stations
    share the same passphrase, its leakage may result in attack. WPA-EAP
    reduces the risk by adopting a RADIUS server to authenticate the
    clients. The stabilization task is b/254338156.
    
    BUG=b:253660970
    TEST=tast run -var=router=localhost:2210 -var=pcap=localhost:2220
    localhost:2200  wifi.MultiAuth
    
    Change-Id: I89559ac4d7934b1ab48ee067177d181fb6407332

--- src/chromiumos/tast/remote/bundles/cros/wifi/multi_auth.go
+++ src/chromiumos/tast/remote/bundles/cros/wifi/multi_auth.go
@@ -7,10 +7,23 @@ package wifi
 import (
 	"context"
 
+	"chromiumos/tast/common/crypto/certificate"
+	"chromiumos/tast/common/wifi/security"
 	"chromiumos/tast/common/wifi/security/wpa"
+	"chromiumos/tast/common/wifi/security/wpaeap"
 	"chromiumos/tast/remote/wificell"
 	"chromiumos/tast/remote/wificell/hostapd"
 	"chromiumos/tast/testing"
+)
+
+type multiAuthParam struct {
+	name []string
+	cfg  []security.ConfigFactory
+}
+
+// EAP certs/keys for EAP tests.
+var (
+	multiAuthCert = certificate.TestCert1()
 )
 
 func init() {
@@ -23,17 +36,42 @@ func init() {
 		Attr:        []string{"group:wificell", "wificell_func"},
 		ServiceDeps: []string{wificell.TFServiceName},
 		Fixture:     "wificellFixtWithCapture",
+		Params: []testing.Param{
+			{
+				Name: "open_wpa",
+				Val: multiAuthParam{
+					name: []string{"Open", "WPA"},
+					cfg:  []security.ConfigFactory{nil, wpa.NewConfigFactory("chromeos", wpa.Mode(wpa.ModePureWPA), wpa.Ciphers(wpa.CipherCCMP))},
+				}},
+			{
+				Name: "psk_8021x",
+				Val: multiAuthParam{
+					// In WPA-PSK, all stations share the same passphrase, and its
+					// leakage may result in attack. WPA-EAP reduces the risk by
+					// adopting a RADIUS server to authenticate the clients.
+					name: []string{"WPA", "WPA-EAP"},
+					cfg: []security.ConfigFactory{wpa.NewConfigFactory("chromeos", wpa.Mode(wpa.ModePureWPA), wpa.Ciphers(wpa.CipherCCMP)),
+						wpaeap.NewConfigFactory(multiAuthCert.CACred.Cert, multiAuthCert.ServerCred, wpaeap.ClientCACert(multiAuthCert.CACred.Cert), wpaeap.ClientCred(multiAuthCert.ClientCred))},
+				},
+				ExtraAttr: []string{"wificell_unstable"}},
+		},
 	})
 }
+
+/*
+This test is to check the ability to identify the target network from APs with
+similar network configurations and connect to the target network correctly, even
+if it may be less secure.
+*/
 
 func MultiAuth(ctx context.Context, s *testing.State) {
 	tf := s.FixtValue().(*wificell.TestFixture)
 
+	param := s.Param().(multiAuthParam)
 	apOpts := []hostapd.Option{hostapd.SSID(hostapd.RandomSSID("TAST_TEST_MultiAuth")), hostapd.Mode(hostapd.Mode80211g), hostapd.Channel(1)}
-	wpaCfg := wpa.NewConfigFactory("chromeos", wpa.Mode(wpa.ModePureWPA), wpa.Ciphers(wpa.CipherCCMP))
 
-	s.Log("Configuring AP 0 (Open)")
-	ap0, err := tf.ConfigureAP(ctx, apOpts, nil)
+	s.Logf("Configuring AP 0 (%s)", param.name[0])
+	ap0, err := tf.ConfigureAP(ctx, apOpts, param.cfg[0])
 	if err != nil {
 		s.Fatal("Failed to configure AP 0: ", err)
 	}
@@ -45,8 +83,8 @@ func MultiAuth(ctx context.Context, s *testing.State) {
 	ctx, cancel := tf.ReserveForDeconfigAP(ctx, ap0)
 	defer cancel()
 
-	s.Log("Configuring AP 1 (WPA)")
-	ap1, err := tf.ConfigureAP(ctx, apOpts, wpaCfg)
+	s.Logf("Configuring AP 1 (%s)", param.name[1])
+	ap1, err := tf.ConfigureAP(ctx, apOpts, param.cfg[1])
 	if err != nil {
 		s.Fatal("Failed to configure AP 1: ", err)
 	}
@@ -82,4 +120,5 @@ func MultiAuth(ctx context.Context, s *testing.State) {
 	if err := tf.VerifyConnection(ctx, ap1); err != nil {
 		s.Fatal("Failed to verify connection: ", err)
 	}
+
 }
