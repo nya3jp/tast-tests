aca9062df99b147cf60b74729e855c5f5fdebe65
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2022-11-01 20:01:24 +0000

    [OOBE TAST] Add OOBE HID screen service
    This CL adds a new OOBE HID screen service, that simulates
    connecting and disconnecting a USB Keyboard and mouse device.
    It also checks to make sure HID screen is in the correct UI
    states.
    
    BUG=b:246648906, b:246649150
    TEST=tast unit test passed
    
    Change-Id: Ifdafbca95491243bc93606c5206d211fdda699d8

--- src/chromiumos/tast/local/oobe/oobe.go
+++ src/chromiumos/tast/local/oobe/oobe.go
@@ -50,3 +50,15 @@ func IsHidDetectionKeyboardNotDetected(ctx context.Context, oobeConn *chrome.Con
 	ui := uiauto.New(tconn).WithTimeout(10 * time.Second)
 	return ui.WaitUntilExists(keyboardNotDetectedTextNode)(ctx)
 }
+
+// IsHidMouseDetected checks if the current page in OOBE to see if it's
+// currently on the HID Detection page.
+func IsHidMouseDetected(ctx context.Context, oobeConn *chrome.Conn) error {
+	return oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.HIDDetectionScreen.mouseDetected()")
+}
+
+// IsHidKeyboardDetected checks if the current page in OOBE to see if it's
+// currently on the HID Detection page.
+func IsHidKeyboardDetected(ctx context.Context, oobeConn *chrome.Conn) error {
+	return oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.HIDDetectionScreen.keyboardDetected()")
+}
