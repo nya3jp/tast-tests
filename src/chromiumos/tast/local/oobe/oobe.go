d48750d0d7540fbab3d8000c4abee565bc2fbf87
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2022-11-02 00:19:37 +0000

    [OOBE TAST] Add hid screen mouse only tast test
    This CL adds a new OOBE tast test. The test checks if a single mouse
    devices can be connected and found in OOBE HID detection screen.
    
    BUG=b:246649150
    TEST=tast unit test passed
    
    Change-Id: I40627dac644a083bc437918a3d4db01c0605e967

--- src/chromiumos/tast/local/oobe/oobe.go
+++ src/chromiumos/tast/local/oobe/oobe.go
@@ -8,11 +8,33 @@ package oobe
 import (
 	"context"
 
+	"chromiumos/tast/errors"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 )
 
 // IsWelcomeScreenVisible checks the current page in OOBE to see if it's
 // currently on the welcome page.
 func IsWelcomeScreenVisible(ctx context.Context, oobeConn *chrome.Conn) error {
 	return oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.WelcomeScreen.isVisible()")
+}
+
+// ClickHidScreenNextButton clicks on the next button in OOBE HID detection screen.
+func ClickHidScreenNextButton(ctx context.Context, ui *uiauto.Context, oobeConn *chrome.Conn) error {
+
+	var nextbuttonName string
+	if err := oobeConn.Eval(ctx, "OobeAPI.screens.HIDDetectionScreen.getNextButtonName()", &nextbuttonName); err != nil {
+		return errors.Wrap(err, "failed to retrieve the next button")
+	}
+
+	if err := uiauto.Combine("Click on next button",
+		ui.WaitUntilExists(nodewith.Name(nextbuttonName).Role(role.Button)),
+		ui.LeftClick(nodewith.Name(nextbuttonName).Role(role.Button)),
+	)(ctx); err != nil {
+		return errors.Wrap(err, "failed to click on next button")
+	}
+
+	return nil
 }
