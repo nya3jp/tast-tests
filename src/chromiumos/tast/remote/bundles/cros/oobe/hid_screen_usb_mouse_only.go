4e8663670cc73e17850ef7da0f9f0fd450ef2146
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2022-11-02 00:19:37 +0000

    [OOBE TAST] Add hid screen mouse only tast test
    This CL adds a new OOBE tast test. The test checks if a single mouse
    devices can be connected and found in OOBE HID detection screen.
    
    BUG=b:246649150
    TEST=tast unit test passed
    
    Change-Id: I40627dac644a083bc437918a3d4db01c0605e967

---
+++ src/chromiumos/tast/remote/bundles/cros/oobe/hid_screen_usb_mouse_only.go
@@ -0,0 +1,128 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package oobe
+
+import (
+	"context"
+	"time"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/remote/bluetooth"
+	util "chromiumos/tast/remote/bundles/cros/oobe/servoutil"
+	crui "chromiumos/tast/remote/cros/ui"
+	oobeui "chromiumos/tast/remote/cros/ui/oobeui"
+	inputspb "chromiumos/tast/services/cros/inputs"
+	"chromiumos/tast/services/cros/ui"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/testing/hwdep"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         HidScreenUsbMouseOnly,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks that a USB mouse is connected to in OOBE",
+		Contacts: []string{
+			"tjohnsonkanu@google.com",
+			"cros-connectivity@google.com",
+		},
+		VarDeps:      []string{"servo"},
+		Attr:         []string{"group:mainline", "informational", "group:bluetooth", "bluetooth_btpeers_1"},
+		SoftwareDeps: []string{"chrome"},
+		ServiceDeps: []string{
+			"tast.cros.ui.AutomationService",
+			"tast.cros.ui.ChromeUIService",
+			"tast.cros.bluetooth.BTTestService",
+			"tast.cros.inputs.MouseService",
+		},
+		Fixture:      "chromeOobeWith1BTPeer",
+		HardwareDeps: hwdep.D(hwdep.FormFactor(hwdep.Chromebase, hwdep.Chromebox, hwdep.Chromebit)),
+	})
+}
+
+// HidScreenUsbMouseOnly tests that a single USB mouse is connected to during OOBE.
+func HidScreenUsbMouseOnly(ctx context.Context, s *testing.State) {
+	fv := s.FixtValue().(*bluetooth.FixtValue)
+
+	// Shorten deadline to leave time for cleanup
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(cleanupCtx, 5*time.Second)
+	defer cancel()
+
+	uiautoSvc := ui.NewAutomationServiceClient(fv.DUTRPCClient.Conn)
+	crUISvc := ui.NewChromeUIServiceClient(fv.DUTRPCClient.Conn)
+	mouseSvc := inputspb.NewMouseServiceClient(fv.DUTRPCClient.Conn)
+
+	defer func() {
+		if !s.HasError() {
+			return
+		}
+		if _, err := crUISvc.DumpUITree(cleanupCtx, &emptypb.Empty{}); err != nil {
+			testing.ContextLog(cleanupCtx, "Failed to dump UI tree: ", err)
+		}
+	}()
+
+	util.TurnOfServoKeyboardIfOn(ctx, s)
+
+	if err := crui.CheckNodeWithNameExists(ctx, uiautoSvc, oobeui.ContinueButtonEnabledNodeName); err == nil {
+		s.Fatal("Continue button should be disabled: ", err)
+	}
+
+	// Connect USB mouse and make sure continue button is enabled.
+	if _, err := mouseSvc.NewMouse(ctx, &emptypb.Empty{}); err != nil {
+		s.Fatal("Failed to emulate mouse: ", err)
+	}
+
+	if err := crui.CheckNodeWithNameExists(ctx, uiautoSvc, oobeui.FoundUSBMouseNodeName); err != nil {
+		s.Fatal("Failed to find node: ", err)
+	}
+
+	if err := crui.CheckNodeWithNameExists(ctx, uiautoSvc, oobeui.ContinueButtonEnabledNodeName); err != nil {
+		s.Fatal("Failed to find node: ", err)
+	}
+
+	// Disconnect mouse and make sure continue button is disabled.
+	if _, err := mouseSvc.CloseMouse(ctx, &emptypb.Empty{}); err != nil {
+		s.Fatal("Failed to disconnect mouse: ", err)
+	}
+
+	if err := crui.CheckNodeWithNameExists(ctx, uiautoSvc, oobeui.SearchingForMouseNodeName); err != nil {
+		s.Fatal("Failed to find node: ", err)
+	}
+
+	if err := crui.CheckNodeWithNameExists(ctx, uiautoSvc, oobeui.ContinueButtonEnabledNodeName); err == nil {
+		s.Fatal("Continue button should be disabled: ", err)
+	}
+
+	// Connect mouse device and make sure continue button is enabled.
+	if _, err := mouseSvc.NewMouse(ctx, &emptypb.Empty{}); err != nil {
+		s.Fatal("Failed to emulate mouse: ", err)
+	}
+
+	if err := crui.CheckNodeWithNameExists(ctx, uiautoSvc, oobeui.FoundUSBMouseNodeName); err != nil {
+		s.Fatal("Failed to find node: ", err)
+	}
+
+	if err := crui.CheckNodeWithNameExists(ctx, uiautoSvc, oobeui.ContinueButtonEnabledNodeName); err != nil {
+		s.Fatal("Failed to find node: ", err)
+	}
+
+	// Navigate to welcome screen.
+	if _, err := uiautoSvc.LeftClick(
+		ctx, &ui.LeftClickRequest{Finder: oobeui.ContinueButtonFinder}); err != nil {
+		s.Fatal("Failed to click continue button: ", err)
+	}
+
+	if _, err := crUISvc.WaitForWelcomeScreen(ctx, &emptypb.Empty{}); err != nil {
+		s.Fatal("Failed to enter welcome page")
+	}
+
+	// Cleanup mouse resources.
+	if _, err := mouseSvc.CloseMouse(ctx, &emptypb.Empty{}); err != nil {
+		s.Fatal("Failed to disconnect mouse: ", err)
+	}
+}
