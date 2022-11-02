4493b60a9f64d8a6ec7690fb9dc4882889d628e2
John Palmer <jopalmer@chromium.org>
2022-11-02 22:10:31 +0000

    Revert "Add extra sleep to ensure that click is correct."
    This reverts commit d1563a724c7088a8259edb192988707671190aeb.
    
    Reason for revert: Doesn't seem to have helped fix things. revert to rm the sleep call to speed things up
    
    Original change's description:
    > Add extra sleep to ensure that click is correct.
    >
    > Without the extra sleep, the dropdown may not be fully rendered leading
    > to flakiness.
    >
    > BUG=b:256017476
    > TEST=CQ
    >
    > Change-Id: Ic1c5a12e8239ff153a26376d16fe4d34156938be
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3990413
    > Reviewed-by: Alvin Jia <alvinjia@google.com>
    > Commit-Queue: John Palmer <jopalmer@chromium.org>
    > Tested-by: John Palmer <jopalmer@chromium.org>
    
    Bug: b:256017476
    Change-Id: Ia7fe5c2e643e4424c73d9d456360dd8d1d62ea5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3999522
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Alvin Jia <alvinjia@google.com>
    Tested-by: John Palmer <jopalmer@chromium.org>
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Alvin Jia <alvinjia@google.com>

--- src/chromiumos/tast/local/chrome/uiauto/ossettings/ossettings.go
+++ src/chromiumos/tast/local/chrome/uiauto/ossettings/ossettings.go
@@ -283,7 +283,6 @@ func (s *OSSettings) SetDropDownOption(cr *chrome.Chrome, optionName, expected s
 			s.LeftClick(oldOptionFinder),
 			s.LeftClick(optionFinder),
 		),
-		uiauto.Sleep(time.Second),
 		s.LeftClick(settingFinder),
 		uiauto.Sleep(time.Second),
 	)
