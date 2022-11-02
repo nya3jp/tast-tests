d39e26664df3d82f3b08bf4d6c40f5574d67772f
Wendy Aleman <awendy@google.com>
2022-11-02 18:18:18 +0000

    Adding Lacros Variants to Login Tests
    Adding variants for login.Chrome
    
    BUG=b:20798939
    TEST=tast run login.Chrome*
    
    Change-Id: I960dc3e0c30af7fd8cba4d9b20f2b447745d0acf

--- src/chromiumos/tast/local/bundles/cros/login/chrome.go
+++ src/chromiumos/tast/local/bundles/cros/login/chrome.go
@@ -11,7 +11,11 @@ import (
 	"net/http/httptest"
 	"time"
 
+	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/browser"
+	"chromiumos/tast/local/chrome/browser/browserfixt"
+	"chromiumos/tast/local/chrome/lacros/lacrosfixt"
 	"chromiumos/tast/local/session"
 	"chromiumos/tast/local/upstart"
 	"chromiumos/tast/testing"
@@ -20,12 +24,13 @@ import (
 type chromeTestParams struct {
 	numTrial int
 	opts     []chrome.Option
+	bt       browser.Type
 }
 
 func init() {
 	testing.AddTest(&testing.Test{
 		Func:         Chrome,
-		LacrosStatus: testing.LacrosVariantNeeded,
+		LacrosStatus: testing.LacrosVariantExists,
 		Desc:         "Checks that Chrome supports login",
 		Contacts: []string{
 			"bohdanty@google.com",
@@ -34,30 +39,90 @@ func init() {
 			"chromeos-sw-engprod@google.com",
 		},
 		SoftwareDeps: []string{"chrome"},
-		Params: []testing.Param{{
-			Val:       chromeTestParams{numTrial: 1},
-			ExtraAttr: []string{"group:mainline"},
-			Timeout:   chrome.LoginTimeout + 45*time.Second,
-		}, {
-			Name:      "auth_factor_experiment_on",
-			Val:       chromeTestParams{numTrial: 1, opts: []chrome.Option{chrome.EnableFeatures("UseAuthFactors")}},
-			ExtraAttr: []string{"group:mainline", "informational"},
-			Timeout:   chrome.LoginTimeout + 45*time.Second,
-		}, {
-			Name:      "auth_factor_experiment_off",
-			Val:       chromeTestParams{numTrial: 1, opts: []chrome.Option{chrome.DisableFeatures("UseAuthFactors")}},
-			ExtraAttr: []string{"group:mainline", "informational"},
-			Timeout:   chrome.LoginTimeout + 45*time.Second,
-		}, {
-			Name:      "stress",
-			Val:       chromeTestParams{numTrial: 50},
-			ExtraAttr: []string{"group:stress"},
-			Timeout:   50*chrome.LoginTimeout + 45*time.Second,
-		}, {
-			Name:    "forever",
-			Val:     chromeTestParams{numTrial: 1000000},
-			Timeout: 365 * 24 * time.Hour,
-		}},
+		Params: []testing.Param{
+			{
+				Val: chromeTestParams{
+					numTrial: 1,
+					bt:       browser.TypeAsh},
+				ExtraAttr: []string{"group:mainline"},
+				Timeout:   chrome.LoginTimeout + 45*time.Second,
+			}, {
+				Name:              "lacros",
+				ExtraSoftwareDeps: []string{"lacros"},
+				Fixture:           "lacros",
+				Val: chromeTestParams{
+					numTrial: 1,
+					bt:       browser.TypeLacros},
+				ExtraAttr: []string{"group:mainline"},
+				Timeout:   chrome.LoginTimeout + 45*time.Second,
+			},
+			{
+				Name: "auth_factor_experiment_on",
+				Val: chromeTestParams{
+					numTrial: 1,
+					opts:     []chrome.Option{chrome.EnableFeatures("UseAuthFactors")},
+					bt:       browser.TypeAsh},
+				ExtraAttr: []string{"group:mainline", "informational"},
+				Timeout:   chrome.LoginTimeout + 45*time.Second,
+			}, {
+				Name:              "auth_factor_experiment_on_lacros",
+				ExtraSoftwareDeps: []string{"lacros"},
+				Fixture:           "lacros",
+				Val: chromeTestParams{
+					numTrial: 1,
+					opts:     []chrome.Option{chrome.EnableFeatures("UseAuthFactors")},
+					bt:       browser.TypeLacros},
+				ExtraAttr: []string{"group:mainline", "informational"},
+				Timeout:   chrome.LoginTimeout + 45*time.Second,
+			}, {
+				Name: "auth_factor_experiment_off",
+				Val: chromeTestParams{
+					numTrial: 1,
+					opts:     []chrome.Option{chrome.DisableFeatures("UseAuthFactors")},
+					bt:       browser.TypeAsh},
+				ExtraAttr: []string{"group:mainline", "informational"},
+				Timeout:   chrome.LoginTimeout + 45*time.Second,
+			}, {
+				Name:              "auth_factor_experiment_off_lacros",
+				ExtraSoftwareDeps: []string{"lacros"},
+				Fixture:           "lacros",
+				Val: chromeTestParams{
+					numTrial: 1,
+					opts:     []chrome.Option{chrome.DisableFeatures("UseAuthFactors")},
+					bt:       browser.TypeLacros},
+				ExtraAttr: []string{"group:mainline", "informational"},
+				Timeout:   chrome.LoginTimeout + 45*time.Second,
+			}, {
+				Name: "stress",
+				Val: chromeTestParams{
+					numTrial: 50,
+					bt:       browser.TypeAsh},
+				ExtraAttr: []string{"group:stress"},
+				Timeout:   50*chrome.LoginTimeout + 45*time.Second,
+			}, {
+				Name:              "stress_lacros",
+				ExtraSoftwareDeps: []string{"lacros"},
+				Fixture:           "lacros",
+				Val: chromeTestParams{
+					numTrial: 50,
+					bt:       browser.TypeLacros},
+				ExtraAttr: []string{"group:stress"},
+				Timeout:   50*chrome.LoginTimeout + 45*time.Second,
+			}, {
+				Name: "forever",
+				Val: chromeTestParams{
+					numTrial: 1000000,
+					bt:       browser.TypeAsh},
+				Timeout: 365 * 24 * time.Hour,
+			}, {
+				Name:              "forever_lacros",
+				ExtraSoftwareDeps: []string{"lacros"},
+				Fixture:           "lacros",
+				Val: chromeTestParams{
+					numTrial: 1000000,
+					bt:       browser.TypeLacros},
+				Timeout: 365 * 24 * time.Hour,
+			}},
 	})
 }
 
@@ -120,6 +185,11 @@ func testChromeLoginInSession(ctx context.Context, s *testing.State, sm *session
 }
 
 func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionManager, params *chromeTestParams, url, expected string) {
+	// Reserve a few seconds for cleanup.
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(cleanupCtx, 10*time.Second)
+	defer cancel()
+
 	// Start listening for a "started" SessionStateChanged D-Bus signal from session_manager.
 	sw, err := sm.WatchSessionStateChanged(ctx, "started")
 	if err != nil {
@@ -127,12 +197,14 @@ func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionM
 	}
 	defer sw.Close(ctx)
 
+	// Connect to a fresh ash-chrome instance (cr) and get a browser instance (br) for browser functionality.
 	opts := append([]chrome.Option{chrome.ExtraArgs("--vmodule=login_display_host*=4,oobe_ui=4")}, params.opts...)
-	cr, err := chrome.New(ctx, opts...)
+	cr, _, closeBrowser, err := browserfixt.SetUpWithNewChrome(ctx, params.bt, lacrosfixt.NewConfig(), opts...)
 	if err != nil {
-		s.Fatal("Chrome login failed: ", err)
+		s.Fatalf("Chrome login failed with %v browser: %v", params.bt, err)
 	}
 	defer cr.Close(ctx)
+	defer closeBrowser(cleanupCtx)
 
 	s.Log("Waiting for SessionStateChanged \"started\" D-Bus signal from session_manager")
 	select {
@@ -142,7 +214,7 @@ func testChromeLogin(ctx context.Context, s *testing.State, sm *session.SessionM
 		s.Fatal("Didn't get SessionStateChanged signal: ", ctx.Err())
 	}
 
-	conn, err := cr.NewConn(ctx, url)
+	conn, _, _, err := browserfixt.SetUpWithURL(ctx, cr, params.bt, url)
 	if err != nil {
 		s.Fatalf("Failed to connect to %v: %v", url, err)
 	}
