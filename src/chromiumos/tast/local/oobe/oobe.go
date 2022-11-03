997273fd4056976bfc47e6a6f33f5a1d87db4fef
Andrew Dear <andrewdear@google.com>
2022-11-03 04:45:00 +0000

    [Tast OOBE] HID Preconnected Touchscreen Only
    Work in progress.
    
    BUG=b:246648423
    TEST=not complete
    
    Change-Id: I5d4e0dd4c02060f18c8a4e8614d4de195f8f0877

--- src/chromiumos/tast/local/oobe/oobe.go
+++ src/chromiumos/tast/local/oobe/oobe.go
@@ -7,12 +7,46 @@ package oobe
 
 import (
 	"context"
+	"time"
 
 	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/chrome/uiauto"
+	"chromiumos/tast/local/chrome/uiauto/nodewith"
+	"chromiumos/tast/local/chrome/uiauto/role"
 )
 
 // IsWelcomeScreenVisible checks the current page in OOBE to see if it's
 // currently on the welcome page.
 func IsWelcomeScreenVisible(ctx context.Context, oobeConn *chrome.Conn) error {
 	return oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.WelcomeScreen.isVisible()")
+}
+
+// IsHidDetectionScreenVisible checks if the current page in OOBE to see if it's
+// currently on the HID Detection page.
+func IsHidDetectionScreenVisible(ctx context.Context, oobeConn *chrome.Conn) error {
+	return oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.HIDDetectionScreen.isVisible()")
+}
+
+// IsHidDetectionTouchscreenDetected checks if a touchscreen is detected in the
+// OOBE HID Detection page.
+func IsHidDetectionTouchscreenDetected(ctx context.Context, oobeConn *chrome.Conn) error {
+	return oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.HIDDetectionScreen.touchscreenDetected()")
+}
+
+// IsHidDetectionContinueButtonEnabled checks if the continue button is enabled
+// in the OOBE HID Detection page.
+func IsHidDetectionContinueButtonEnabled(ctx context.Context, oobeConn *chrome.Conn) error {
+	return oobeConn.WaitForExprFailOnErr(ctx, "OobeAPI.screens.HIDDetectionScreen.canClickNext()")
+}
+
+// IsHidDetectionKeyboardNotDetected checks if there is no keyboard detected in
+// the OOBE HID Detection page.
+func IsHidDetectionKeyboardNotDetected(ctx context.Context, oobeConn *chrome.Conn, tconn *chrome.TestConn) error {
+	var keyboardNotDetectedText string
+	if err := oobeConn.Eval(ctx, "OobeAPI.screens.HIDDetectionScreen.getKeyboardNotDetectedText()", &keyboardNotDetectedText); err != nil {
+		return err
+	}
+	keyboardNotDetectedTextNode := nodewith.Role(role.StaticText).Name(keyboardNotDetectedText)
+	ui := uiauto.New(tconn).WithTimeout(10 * time.Second)
+	return ui.WaitUntilExists(keyboardNotDetectedTextNode)(ctx)
 }
