f397ca351d9aacd8778e4a5b3b8243d4a63ac932
Zi Lin <lziest@google.com>
2022-11-03 21:52:40 +0000

    cryptohome: refactor one test to use the uss fixture.
    BUG=b:240594146
    TEST=tast run DUT:PORT cryptohome.*
    
    Change-Id: If5950246f178d289ea6dbdec5a360f1d7e03c484

--- src/chromiumos/tast/local/bundles/cros/cryptohome/password_auth_factor_existing_vault_keyset.go
+++ src/chromiumos/tast/local/bundles/cros/cryptohome/password_auth_factor_existing_vault_keyset.go
@@ -6,9 +6,7 @@ package cryptohome
 
 import (
 	"context"
-	"io/ioutil"
 	"os"
-	"path"
 	"path/filepath"
 	"time"
 
@@ -29,7 +27,8 @@ func init() {
 			"betuls@google.com",
 			"cryptohome-core@google.com",
 		},
-		Attr: []string{"group:mainline", "informational"},
+		Attr:    []string{"group:mainline", "informational"},
+		Fixture: "ussAuthSessionFixture",
 	})
 }
 
@@ -47,36 +46,11 @@ func PasswordAuthFactorExistingVaultKeyset(ctx context.Context, s *testing.State
 		ussFile       = "user_secret_stash/uss"
 	)
 
-	// Enable the UserSecretStash experiment for the duration of the test by
-	// creating a flag file that's checked by cryptohomed.
-	if err := os.MkdirAll(path.Dir(ussFlagFile), 0755); err != nil {
-		s.Fatal("Failed to create the UserSecretStash flag file directory: ", err)
-	}
-	if err := ioutil.WriteFile(ussFlagFile, []byte{}, 0644); err != nil {
-		s.Fatal("Failed to write the UserSecretStash flag file: ", err)
-	}
-	defer os.Remove(ussFlagFile)
-
 	ctx, cancel := ctxutil.Shorten(ctx, 10*time.Second)
 	defer cancel()
 
 	cmdRunner := hwseclocal.NewCmdRunner()
 	client := hwsec.NewCryptohomeClient(cmdRunner)
-	helper, err := hwseclocal.NewHelper(cmdRunner)
-	if err != nil {
-		s.Fatal("Failed to create hwsec local helper: ", err)
-	}
-	daemonController := helper.DaemonController()
-
-	// Wait for cryptohomed becomes available if needed.
-	if err := daemonController.Ensure(ctx, hwsec.CryptohomeDaemon); err != nil {
-		s.Fatal("Failed to ensure cryptohomed: ", err)
-	}
-
-	// Clean up obsolete state, in case there's any.
-	if err := cryptohome.UnmountAll(ctx); err != nil {
-		s.Fatal("Failed to unmount vaults for preparation: ", err)
-	}
 	if err := cryptohome.RemoveVault(ctx, userName); err != nil {
 		s.Fatal("Failed to remove old vault for preparation: ", err)
 	}
@@ -99,13 +73,12 @@ func PasswordAuthFactorExistingVaultKeyset(ctx context.Context, s *testing.State
 		s.Fatal("Failed to start auth session for re-mounting: ", err)
 	}
 
-	// Make sure vault keyset exists before authenticating.
-	hash, err := cryptohome.UserHash(ctx, userName)
+	// Make sure one vault keyset exists before authenticating.
+	sr, err := client.GetUserShadowRoot(ctx, userName)
 	if err != nil {
-		s.Fatal("Failed to get user hash: ", err)
+		s.Fatal("Failed to get user shadow root: ", err)
 	}
-	_, err = os.Stat(filepath.Join(shadow, hash, keysetFile))
-	if err != nil {
+	if _, err := os.Stat(filepath.Join(sr, keysetFile)); err != nil {
 		s.Fatal("Keyset file not found: ", err)
 	}
 
@@ -128,8 +101,7 @@ func PasswordAuthFactorExistingVaultKeyset(ctx context.Context, s *testing.State
 	defer cryptohome.UnmountVault(ctx, userName)
 
 	// Check that USS file is not created
-	_, err = os.Stat(filepath.Join(shadow, hash, ussFile))
-	if err == nil {
+	if _, err = os.Stat(filepath.Join(sr, ussFile)); err == nil {
 		s.Fatal("USS file created when there is vault keyset: ", err)
 	}
 }
