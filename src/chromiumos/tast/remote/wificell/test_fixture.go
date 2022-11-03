ff7100d6a60f43edcc91cbb574522a8d8d7bf175
Billy Zhao <billyzhao@chromium.org>
2022-11-03 22:58:07 +0000

    tast-tests: Update newRouter to always infer router
    In some tests, we invoke NewTestFixture which currently defaults to a
    legacy router type for the router and pcap. This means if a new router
    is generated, the router resolver in newRouter() won't trigger since the
    default router type isn't support.UnknownT. We force the router to
    always resolve the router type. We throw an error if we cannot resolve
    the router type. We also change the default types
    to UnknownT instead of defaulting to LegacyT. For future router
    adapters, we will have to update the router resolving mechanism
    
    BUG=b:257334215,b:250673442
    TEST=tast run wifi_matfunc with this patch on brya against openwrt
    router/pcap and gale router/pcap
    
    Change-Id: Iecdaaa2803671a48aab58faced7e3ac2a03c8578

--- src/chromiumos/tast/remote/wificell/test_fixture.go
+++ src/chromiumos/tast/remote/wificell/test_fixture.go
@@ -342,9 +342,9 @@ func NewTestFixture(fullCtx, daemonCtx context.Context, d *dut.DUT, rpcHint *tes
 		capturers: make(map[*APIface]*pcap.Capturer),
 		aps:       make(map[*APIface]struct{}),
 		// Set the router's default router type.
-		routerType: support.LegacyT,
+		routerType: support.UnknownT,
 		// Set the pcap capture device's default router type.
-		pcapType: support.LegacyT,
+		pcapType: support.UnknownT,
 		// Set the debug values on the DUT by default.
 		setLogging: true,
 		// Default log level used in WiFi tests.
@@ -1559,16 +1559,13 @@ func newRouter(ctx, daemonCtx context.Context, host *ssh.Conn, name string, rtyp
 	ctx, st := timing.Start(ctx, "NewRouter")
 	defer st.End()
 
-	if rtype == support.UnknownT {
-		if resolvedType, err := resolveRouterTypeFromHost(ctx, host); err != nil {
-			return nil, errors.Wrap(err, "failed to resolve router type from host")
-		} else if resolvedType == support.UnknownT {
-			rtype = support.LegacyT
-			testing.ContextLogf(ctx, "Unable to resolve specific router type from host, defaulting to %q", rtype.String())
-		} else {
-			rtype = resolvedType
-			testing.ContextLogf(ctx, "Resolved host router type to be %q", rtype.String())
-		}
+	if resolvedType, err := resolveRouterTypeFromHost(ctx, host); err != nil {
+		return nil, errors.Wrap(err, "failed to resolve router type from host")
+	} else if resolvedType == support.UnknownT {
+		return nil, errors.Wrapf(err, "Unable to resolve specific router type from host, defaulting to %q", rtype.String())
+	} else {
+		rtype = resolvedType
+		testing.ContextLogf(ctx, "Resolved host router type to be %q", rtype.String())
 	}
 
 	switch rtype {
