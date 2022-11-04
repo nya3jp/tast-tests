a6907b89cf92632c8b35261533ebf3b4a670a1e5
Navin Ramsaroop <ramsaroop@google.com>
2022-11-04 15:31:49 -0700

    tast-tests: Improve local development for tast-tests
    This change adds two runtime arguments for CUJ tast tests: cuj.isLocal
    and cujrecorder.keepWifi.
    
    cuj.isLocal quickens the runtime of tests by skipping CPU idle and
    cooldown time. This flag is useful for quickly checking on changes of a
    tast test, when performance data is not important.
    
    cujrecorder.keepWifi forces the cujrecorder.Recorder to keep the Wifi in
    its initial state. Devices that require Wifi for an active SSH
    connection will fail CUJ tests, because we turn the Wifi off by default.
    
    BUG=b:256715411
    TEST=tast run -var=cuj.isLocal=y -var=cujrecorder.keepWifi=y
    <DUT> ui.QuickCheckCUJ
    
    Change-Id: I1e2684282e2d7dc51859ef316d5313f79daa065e

--- src/chromiumos/tast/local/ui/cujrecorder/recorder.go
+++ src/chromiumos/tast/local/ui/cujrecorder/recorder.go
@@ -49,6 +49,14 @@ const checkInterval = 5 * time.Second
 // SystemTraceConfigFile is a perfetto tracing config.
 const SystemTraceConfigFile = "perfetto/system_trace_config.pbtxt"
 
+// keepWifi forces the Wifi to remain in its initial state,
+// regardless of the options passed to the Recorder.
+var keepWifi = testing.RegisterVarString(
+	"cujrecorder.keepWifi",
+	"",
+	"A variable signifying whether to force skipping disabling wifi for the Recorder",
+)
+
 // MetricConfig is the configuration for the recorder.
 type MetricConfig struct {
 	// The name of the histogram to be recorded.
@@ -340,7 +348,6 @@ func NewRecorderWithTestConn(ctx context.Context, tconn *chrome.TestConn, cr *ch
 
 	powerTestOptions := setup.PowerTestOptions{
 		// The default for the following options is to disable these setting.
-		Wifi:       setup.DisableWifiInterfaces,
 		NightLight: setup.DisableNightLight,
 		Powerd:     setup.DisablePowerd,
 		DPTF:       setup.DisableDPTF,
@@ -348,9 +355,6 @@ func NewRecorderWithTestConn(ctx context.Context, tconn *chrome.TestConn, cr *ch
 		Bluetooth:  setup.DisableBluetoothInterfaces,
 	}
 	// Check recorder options and don't change them when required.
-	if options.DoNotChangeWifi {
-		powerTestOptions.Wifi = setup.DoNotChangeWifiInterfaces
-	}
 	if options.DoNotChangePowerd {
 		powerTestOptions.Powerd = setup.DoNotChangePowerd
 	}
@@ -367,6 +371,20 @@ func NewRecorderWithTestConn(ctx context.Context, tconn *chrome.TestConn, cr *ch
 	if options.DischargeThreshold != nil {
 		dischargeThreshold = *options.DischargeThreshold
 	}
+
+	// By default, the recorder will disable Wifi interfaces. Disabling
+	// Wifi can be problematic in local testing when an SSH connection
+	// is required to connect to a DUT. Use the runtime variable
+	// cujrecorder.keepWifi to allow developers to skip toggling
+	// Wifi in the test, ignoring the Wifi option passed to the recorder.
+	if keepWifi.Value() != "" {
+		testing.ContextLog(ctx, "Skipping disabling wifi because cujrecorder.keepWifi is set")
+	} else if options.DoNotChangeWifi {
+		powerTestOptions.Wifi = setup.DoNotChangeWifiInterfaces
+	} else {
+		powerTestOptions.Wifi = setup.DisableWifiInterfaces
+	}
+
 	// Create batteryDischarge with both discharge and ignoreErr set to true.
 	batteryDischarge := setup.NewBatteryDischarge(true, true, dischargeThreshold)
 
