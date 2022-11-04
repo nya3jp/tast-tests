601d766ef4256f41cc247bfcaaf3aa93f8aff82d
Hirokazu Honda <hiroh@chromium.org>
2022-11-04 16:12:19 +0900

    webrtc.RTCPeerConnection: Use RTCTestParams for test parameters
    webrtc.RTCPeerConnection uses its own rtcTest structure for test
    parameters and pass the values to RunRTCPeerConnection(). This CL
    uses peerconnection.RTCTestParams as well as RTCPeerConnectionPerf
    does.
    
    BUG=None
    TEST=webrtc.RTCPeerConnection*
    
    Change-Id: I8cb382e12377bd07a765e0c63524ddeb61d98436

--- src/chromiumos/tast/local/bundles/cros/webrtc/rtc_peer_connection.go
+++ src/chromiumos/tast/local/bundles/cros/webrtc/rtc_peer_connection.go
@@ -14,40 +14,29 @@ import (
 	"chromiumos/tast/testing/hwdep"
 )
 
-// verifyHWAcceleratorMode is the type of codec to verify hardware acceleration for.
-type verifyHWAcceleratorMode int
-
-const (
-	// VerifyHWEncoderUsed refers to WebRTC hardware accelerated video encoding.
-	verifyHWEncoderUsed verifyHWAcceleratorMode = iota
-	// VerifyHWDecoderUsed refers to WebRTC hardware accelerated video decoding.
-	verifyHWDecoderUsed
-	// VerifySWEncoderUsed refers to WebRTC software video encoding.
-	verifySWEncoderUsed
-	// VerifySWDecoderUsed refers to WebRTC software video decoding.
-	verifySWDecoderUsed
-	// NoVerifyHWAcceleratorUsed means it doesn't matter if WebRTC uses any accelerated video.
-	noVerifyHWAcceleratorUsed
-)
-
-// rtcTest is used to describe the config used to run each test case.
-type rtcTest struct {
-	// Whether to verify if a hardware accelerator was used, and which one, e.g.
-	// decoder, encoder, if so.
-	verifyMode verifyHWAcceleratorMode
-	profile    string // Codec to try, e.g. VP8, VP9.
-	// Simulcast is a technique to send multiple differently encoded versions
-	// of the same media source in different RTP streams; this is used for
-	// example for video conference services.
-	// See https://www.w3.org/TR/webrtc/#simulcast-functionality
-	simulcast bool
-	// ScalableVideoCodec "scalabilityMode" identifier.
-	// https://www.w3.org/TR/webrtc-svc/#scalabilitymodes
-	svc string
-	// If non-empty, the media to send through the RTC connection will be obtained
-	// using getDisplayMedia() and the value corresponds to the surface type. If
-	// empty, the media to send will be obtained using getUserMedia().
+type testOptions struct {
+	simulcasts       int
+	svc              string
 	displayMediaType peerconnection.DisplayMediaType
+}
+
+func noVerifyCodecTest(profile string, opts testOptions) peerconnection.RTCTestParams {
+	return peerconnection.MakeRTCTestParams(
+		peerconnection.NoVerifyDecoderMode, peerconnection.NoVerifyEncoderMode,
+		profile, opts.simulcasts, opts.svc, opts.displayMediaType)
+
+}
+
+func hwDecoderTest(profile string, opts testOptions) peerconnection.RTCTestParams {
+	return peerconnection.MakeRTCTestParams(
+		peerconnection.VerifyHWDecoderUsed, peerconnection.NoVerifyEncoderMode,
+		profile, opts.simulcasts, opts.svc, opts.displayMediaType)
+}
+
+func hwEncoderTest(profile string, opts testOptions) peerconnection.RTCTestParams {
+	return peerconnection.MakeRTCTestParams(
+		peerconnection.NoVerifyDecoderMode, peerconnection.VerifyHWEncoderUsed,
+		profile, opts.simulcasts, opts.svc, opts.displayMediaType)
 }
 
 func init() {
@@ -65,157 +54,156 @@ func init() {
 		Attr:         []string{"group:graphics", "graphics_video", "graphics_perbuild"},
 		Params: []testing.Param{{
 			Name:              "h264",
-			Val:               rtcTest{verifyMode: noVerifyHWAcceleratorUsed, profile: "H264"},
+			Val:               noVerifyCodecTest("H264", testOptions{}),
 			ExtraSoftwareDeps: []string{"proprietary_codecs"},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:    "vp8",
-			Val:     rtcTest{verifyMode: noVerifyHWAcceleratorUsed, profile: "VP8"},
+			Val:     noVerifyCodecTest("VP8", testOptions{}),
 			Fixture: "chromeVideoWithFakeWebcam",
 		}, {
 			Name:    "vp8_simulcast",
-			Val:     rtcTest{verifyMode: noVerifyHWAcceleratorUsed, profile: "VP8", simulcast: true},
+			Val:     noVerifyCodecTest("VP8", testOptions{simulcasts: 3}),
 			Fixture: "chromeVideoWithFakeWebcam",
 		}, {
 			Name:    "vp9",
-			Val:     rtcTest{verifyMode: noVerifyHWAcceleratorUsed, profile: "VP9"},
+			Val:     noVerifyCodecTest("VP9", testOptions{}),
 			Fixture: "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "h264_dec",
-			Val:               rtcTest{verifyMode: verifyHWDecoderUsed, profile: "H264"},
+			Val:               hwDecoderTest("H264", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "proprietary_codecs"},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "h264_dec_alt",
-			Val:               rtcTest{verifyMode: verifyHWDecoderUsed, profile: "H264"},
+			Val:               hwDecoderTest("H264", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.HWDecodeH264, "video_decoder_legacy_supported", "proprietary_codecs"},
 			Fixture:           "chromeVideoWithFakeWebcamAndAlternateVideoDecoder",
 		}, {
 			Name:              "vp8_dec",
-			Val:               rtcTest{verifyMode: verifyHWDecoderUsed, profile: "VP8"},
+			Val:               hwDecoderTest("VP8", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "vp8_dec_alt",
-			Val:               rtcTest{verifyMode: verifyHWDecoderUsed, profile: "VP8"},
+			Val:               hwDecoderTest("VP8", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP8, "video_decoder_legacy_supported"},
 			Fixture:           "chromeVideoWithFakeWebcamAndAlternateVideoDecoder",
 		}, {
 			Name:              "vp9_dec",
-			Val:               rtcTest{verifyMode: verifyHWDecoderUsed, profile: "VP9"},
+			Val:               hwDecoderTest("VP9", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "vp9_dec_alt",
-			Val:               rtcTest{verifyMode: verifyHWDecoderUsed, profile: "VP9"},
+			Val:               hwDecoderTest("VP9", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9, "video_decoder_legacy_supported"},
 			Fixture:           "chromeVideoWithFakeWebcamAndAlternateVideoDecoder",
 		}, {
 			// This is a decoding test of 2 temporal layers test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_dec_svc_l1t2",
-			Val:               rtcTest{verifyMode: verifyHWDecoderUsed, profile: "VP9", svc: "L1T2"},
+			Val:               hwDecoderTest("VP9", testOptions{svc: "L1T2"}),
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabled",
 		}, {
 			// This is a decoding test of 3 temporal layers test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_dec_svc_l1t3",
-			Val:               rtcTest{verifyMode: verifyHWDecoderUsed, profile: "VP9", svc: "L1T3"},
+			Val:               hwDecoderTest("VP9", testOptions{svc: "L1T3"}),
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabled",
 		}, {
 			// This is a decoding test of 3 spatial layers, 3 temporal layers (each) k-SVC test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_dec_svc_l3t3_key",
-			Val:               rtcTest{verifyMode: verifyHWDecoderUsed, profile: "VP9", svc: "L3T3_KEY"},
+			Val:               hwDecoderTest("VP9", testOptions{svc: "L3T3_KEY"}),
 			ExtraSoftwareDeps: []string{caps.HWDecodeVP9},
 			ExtraHardwareDeps: hwdep.D(hwdep.SupportsVP9KSVCHWDecoding()),
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabled",
 		}, {
 			Name:              "h264_enc",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "H264"},
+			Val:               hwEncoderTest("H264", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeH264, "proprietary_codecs"},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "h264_enc_cam",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "H264"},
+			Val:               hwEncoderTest("H264", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.BuiltinCamera, caps.HWEncodeH264, "proprietary_codecs"},
 			Fixture:           "chromeCameraPerf",
 		}, {
 			Name:              "vp8_enc",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP8"},
+			Val:               hwEncoderTest("VP8", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "vp8_enc_cam",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP8"},
+			Val:               hwEncoderTest("VP8", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.BuiltinCamera, caps.HWEncodeVP8},
 			Fixture:           "chromeCameraPerf",
 		}, {
 			Name:              "vp8_enc_simulcast",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP8", simulcast: true},
+			Val:               hwEncoderTest("VP8", testOptions{simulcasts: 3}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
 			Fixture:           "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "vp8_capture_monitor",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP8", displayMediaType: peerconnection.CaptureMonitor},
+			Val:               hwEncoderTest("VP8", testOptions{displayMediaType: peerconnection.CaptureMonitor}),
 			ExtraHardwareDeps: hwdep.D(hwdep.InternalDisplay()),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
 			Fixture:           "chromeScreenCapture",
 		}, {
 			Name:              "vp8_capture_window",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP8", displayMediaType: peerconnection.CaptureWindow},
+			Val:               hwEncoderTest("VP8", testOptions{displayMediaType: peerconnection.CaptureWindow}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
 			Fixture:           "chromeWindowCapture",
 		}, {
 			Name:              "vp8_capture_tab",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP8", displayMediaType: peerconnection.CaptureTab},
+			Val:               hwEncoderTest("VP8", testOptions{displayMediaType: peerconnection.CaptureTab}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
 			Fixture:           "chromeTabCapture",
 		}, {
 			// This is a 2 temporal layers test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp8_enc_svc_l1t2",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP8", svc: "L1T2"},
+			Val:               hwEncoderTest("VP8", testOptions{svc: "L1T2"}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabledWithHWVp8TemporalLayerEncoding",
 		}, {
 			// This is an encoding test of 3 temporal layers test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp8_enc_svc_l1t3",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP8", svc: "L1T3"},
+			Val:               hwEncoderTest("VP8", testOptions{svc: "L1T3"}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP8},
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabledWithHWVp8TemporalLayerEncoding",
 		}, {
-			Name:              "vp9_enc",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP9"},
-			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
-			Fixture:           "chromeVideoWithFakeWebcam",
+			Name:    "vp9_enc",
+			Val:     hwEncoderTest("VP9", testOptions{}),
+			Fixture: "chromeVideoWithFakeWebcam",
 		}, {
 			Name:              "vp9_enc_cam",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP9"},
+			Val:               hwEncoderTest("VP9", testOptions{}),
 			ExtraSoftwareDeps: []string{caps.BuiltinCamera, caps.HWEncodeVP9},
 			Fixture:           "chromeCameraPerf",
 		}, {
 			// This is a 2 temporal layers test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_enc_svc_l1t2",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP9", svc: "L1T2"},
+			Val:               hwEncoderTest("VP9", testOptions{svc: "L1T2"}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabled",
 		}, {
 			// This is an encoding test of 3 temporal layers test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_enc_svc_l1t3",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP9", svc: "L1T3"},
+			Val:               hwEncoderTest("VP9", testOptions{svc: "L1T3"}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabled",
 		}, {
 			// This is an encoding test of 3 spatial layers, 3 temporal layers (each) k-SVC test, via the (experimental) API.
 			// See https://www.w3.org/TR/webrtc-svc/#scalabilitymodes for SVC identifiers.
 			Name:              "vp9_enc_svc_l3t3_key",
-			Val:               rtcTest{verifyMode: verifyHWEncoderUsed, profile: "VP9", svc: "L3T3_KEY"},
+			Val:               hwEncoderTest("VP9", testOptions{svc: "L3T3_KEY"}),
 			ExtraSoftwareDeps: []string{caps.HWEncodeVP9},
 			Fixture:           "chromeVideoWithFakeWebcamAndSVCEnabled",
 		}},
@@ -225,22 +213,9 @@ func init() {
 // RTCPeerConnection verifies that a PeerConnection works correcttly and, if
 // specified, verifies it uses accelerated encoding / decoding.
 func RTCPeerConnection(ctx context.Context, s *testing.State) {
-	verifyDecoderMode := peerconnection.NoVerifyDecoderMode
-	verifyEncoderMode := peerconnection.NoVerifyEncoderMode
-	testOpt := s.Param().(rtcTest)
-
-	switch testOpt.verifyMode {
-	case verifyHWEncoderUsed:
-		verifyEncoderMode = peerconnection.VerifyHWEncoderUsed
-	case verifyHWDecoderUsed:
-		verifyDecoderMode = peerconnection.VerifyHWDecoderUsed
-	case verifySWEncoderUsed:
-		verifyEncoderMode = peerconnection.VerifySWEncoderUsed
-	case verifySWDecoderUsed:
-		verifyDecoderMode = peerconnection.VerifySWDecoderUsed
-	}
-
-	if err := peerconnection.RunRTCPeerConnection(ctx, s.FixtValue().(*chrome.Chrome), s.DataFileSystem(), verifyDecoderMode, verifyEncoderMode, testOpt.profile, testOpt.simulcast, testOpt.svc, testOpt.displayMediaType); err != nil {
+	params := s.Param().(peerconnection.RTCTestParams)
+	if err := peerconnection.RunRTCPeerConnection(
+		ctx, s.FixtValue().(*chrome.Chrome), s.DataFileSystem(), params); err != nil {
 		s.Error("Failed to run RunRTCPeerConnection: ", err)
 	}
 }
