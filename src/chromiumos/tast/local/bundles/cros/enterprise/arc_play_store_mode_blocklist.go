03504ea4759c886a5ea4a8324500ccf3d77f163b
Muhammad Hasan Khan <mhasank@chromium.org>
2022-11-04 16:49:29 -0700

    tast-tests: Add a test for blocklist Play Store mode
    This test ensures that Play Store does not show empty when the Play
    Store mode in the policy is set to blocklist and there are no apps in
    the policy.
    
    BUG=b:235602184
    TEST=tast run DUT enterprise.ARCPlayStoreModeBlocklist.vm
    
    Change-Id: Iefea6f32933ed1536d039951a771d5b5a832d40c

---
+++ src/chromiumos/tast/local/bundles/cros/enterprise/arc_play_store_mode_blocklist.go
@@ -0,0 +1,130 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package enterprise
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"time"
+
+	"chromiumos/tast/common/policy"
+	"chromiumos/tast/ctxutil"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/bundles/cros/enterprise/arcent"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/policyutil"
+	"chromiumos/tast/local/retry"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         ARCPlayStoreModeBlocklist,
+		LacrosStatus: testing.LacrosVariantUnneeded,
+		Desc:         "Checks that apps are shown when Play Store mode is blocklist",
+		Contacts:     []string{"mhasank@chromium.org", "arc-commercial@google.com"},
+		Attr:         []string{"group:mainline"},
+		SoftwareDeps: []string{"chrome", "play_store"},
+		Timeout:      15 * time.Minute,
+		VarDeps: []string{
+			arcent.LoginPoolVar,
+		},
+		Params: []testing.Param{
+			{
+				ExtraSoftwareDeps: []string{"android_p"},
+				ExtraAttr:         []string{"informational"},
+			},
+			{
+				Name:              "vm",
+				ExtraSoftwareDeps: []string{"android_vm"},
+				ExtraAttr:         []string{"informational"},
+			}},
+	})
+}
+
+// ARCPlayStoreModeBlocklist Verifies that apps are shown when Play Store mode is blocklist.
+func ARCPlayStoreModeBlocklist(ctx context.Context, s *testing.State) {
+	const (
+		bootTimeout      = 4 * time.Minute
+		defaultUITimeout = 1 * time.Minute
+	)
+
+	rl := &retry.Loop{Attempts: 1,
+		MaxAttempts: 2,
+		DoRetries:   true,
+		Fatalf:      s.Fatalf,
+		Logf:        s.Logf}
+
+	creds, err := chrome.PickRandomCreds(s.RequiredVar(arcent.LoginPoolVar))
+	if err != nil {
+		rl.Exit("get login creds", err)
+	}
+	login := chrome.GAIALogin(creds)
+
+	arcPolicy := arcent.CreateArcPolicyWithApps([]string{}, arcent.InstallTypeAvailable)
+	arcPolicy.Val.PlayStoreMode = arcent.PlayStoreModeBlockList
+	arcEnabledPolicy := &policy.ArcEnabled{Val: true}
+	policies := []policy.Policy{arcEnabledPolicy, arcPolicy}
+
+	cleanupCtx := ctx
+	ctx, cancel := ctxutil.Shorten(ctx, time.Minute)
+	defer cancel()
+
+	fdms, err := policyutil.SetUpFakePolicyServer(ctx, s.OutDir(), creds.User, policies)
+	if err != nil {
+		rl.Exit("setup fake policy server", err)
+	}
+	defer fdms.Stop(cleanupCtx)
+
+	if err := testing.Poll(ctx, func(ctx context.Context) (retErr error) {
+		cr, err := chrome.New(
+			ctx,
+			login,
+			chrome.ARCSupported(),
+			chrome.UnRestrictARCCPU(),
+			chrome.DMSPolicy(fdms.URL),
+			chrome.ExtraArgs(arc.DisableSyncFlags()...))
+		if err != nil {
+			return rl.Retry("connect to Chrome", err)
+		}
+		defer cr.Close(cleanupCtx)
+
+		tconn, err := cr.TestAPIConn(ctx)
+		if err != nil {
+			return rl.Retry("create test API connection", err)
+		}
+
+		a, err := arc.NewWithTimeout(ctx, s.OutDir(), bootTimeout)
+		if err != nil {
+			return rl.Exit("start ARC by policy", err)
+		}
+		defer a.Close(cleanupCtx)
+
+		if err := arcent.ConfigureProvisioningLogs(ctx, a); err != nil {
+			return rl.Exit("configure provisioning logs", err)
+		}
+
+		defer arcent.DumpBugReportOnError(cleanupCtx, func() bool {
+			return s.HasError() || retErr != nil
+		}, a, filepath.Join(s.OutDir(), fmt.Sprintf("bugreport_%d.zip", rl.Attempts)))
+
+		if err := arcent.WaitForProvisioning(ctx, a, rl.Attempts); err != nil {
+			return rl.Exit("wait for provisioning", err)
+		}
+
+		defer a.DumpUIHierarchyOnError(cleanupCtx, s.OutDir(), func() bool {
+			return s.HasError() || retErr != nil
+		})
+
+		if err := arcent.EnsurePlayStoreNotEmpty(ctx, tconn, cr, a, s.OutDir(), rl.Attempts); err != nil {
+			return rl.Exit("verify Play Store is not empty", err)
+		}
+
+		return nil
+	}, nil); err != nil {
+		rl.Exit("verify blocked app cannot be installed: ", err)
+	}
+}
