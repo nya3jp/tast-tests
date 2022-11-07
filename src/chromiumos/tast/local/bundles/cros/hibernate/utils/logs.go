ec884251593df5f8829dd421b626fd4b1dfef1a0
Jakub Mlynarczyk <jakubm@google.com>
2022-11-07 19:46:25 +1100

    hiberman: Move the log parsing into utils package
    To keep things a bit more tidy (similar to other tast tests, ex.
    health), I've decided to put log parsing and streaming logic into a
    separate utils/log file.
    With more tests to come, this logic will be requried across multiple
    tests.
    
    BUG=b:237084693
    TEST=tast run brya-hibernate-dut hibernate.HibermanDryRun
    
    Change-Id: I301f9c05d73acec36aeebf70432cf2f0df0ebc7b

---
+++ src/chromiumos/tast/local/bundles/cros/hibernate/utils/logs.go
@@ -0,0 +1,78 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package utils provides util functions for hibernate tast.
+package utils
+
+import (
+	"bufio"
+	"context"
+	"strings"
+	"time"
+)
+
+// DetectLog reads the string channel until the expected string matches or context expires.
+func DetectLog(ctx context.Context, s *testing.State, ch <-chan string, expMsg string) bool {
+	for {
+		select {
+		case msg := <-ch:
+			if strings.Contains(msg, expMsg) {
+				return true
+			}
+		case <-ctx.Done():
+			s.Fatalf("Didn't see %q in channel: %v", expMsg, ctx.Err())
+		}
+	}
+	return false
+}
+
+// StreamLogs tails the /var/log/messages and wrtires them to the channel.
+func StreamLogs(ctx context.Context) (*testexec.Cmd, <-chan string, error) {
+	// Start a process that writes messages to stdout as they're logged.
+	cmd := testexec.CommandContext(ctx, "tail", "-f", "/var/log/messages")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, nil, errors.Wrap(err, "failed to fetch /var/log/messages")
+	}
+
+	// Start a goroutine that just passes lines from dmesg to a channel.
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+
+		// Writes msg to ch and returns true if more messages should be written.
+		writeMsg := func(msg string) bool {
+			// To avoid blocking forever on a write to ch if nobody's reading from
+			// it, we use a non-blocking write. If the channel isn't writable, sleep
+			// briefly and then check if the context's deadline has been reached.
+			for {
+				if ctx.Err() != nil {
+					return false
+				}
+
+				select {
+				case ch <- msg:
+					return true
+				default:
+					testing.Sleep(ctx, 10*time.Millisecond)
+				}
+			}
+		}
+
+		// The Scan method will return false once the dmesg process is killed.
+		sc := bufio.NewScanner(stdout)
+		for sc.Scan() {
+			if !writeMsg(sc.Text()) {
+				break
+			}
+		}
+		// Don't bother checking sc.Err(). The test will already fail if the expected
+		// message isn't seen.
+	}()
+
+	return cmd, ch, nil
+}
