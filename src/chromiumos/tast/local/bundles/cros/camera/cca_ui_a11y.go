3deef51758221c5387e1a4cf61d0e3383a3d295d
Dorah <dorahkim@google.com>
2022-11-08 06:10:24 +0000

    tast-tests: Removing envoking ChromeVox from the tast
    This test checked if ChromeVox always reads CCA(Chrome Camera App)'s elements.
    However as CCA and ChromeVox use CPU resources a lot, the tast became so
    much flaky. Instead of matching with ChromeVox statements, the test checks the
    focused element's aria label is not null.
    
    BUG=b:238982700
    TEST=tast run $DUT camera.CCAUIA11y
    
    Change-Id: Id9aa5ac798257ead5ed8475514c3e58cdece4371

--- src/chromiumos/tast/local/bundles/cros/camera/cca_ui_a11y.go
+++ src/chromiumos/tast/local/bundles/cros/camera/cca_ui_a11y.go
@@ -12,11 +12,7 @@ import (
 	"chromiumos/tast/common/media/caps"
 	"chromiumos/tast/ctxutil"
 	"chromiumos/tast/errors"
-	"chromiumos/tast/local/a11y"
-	"chromiumos/tast/local/audio/crastestclient"
 	"chromiumos/tast/local/camera/cca"
-	"chromiumos/tast/local/chrome/uiauto/nodewith"
-	"chromiumos/tast/local/cpu"
 	"chromiumos/tast/local/input"
 	"chromiumos/tast/testing"
 )
@@ -35,34 +31,10 @@ func init() {
 
 func CCAUIA11y(ctx context.Context, s *testing.State) {
 	app := s.FixtValue().(cca.FixtureData).App()
-	cr := s.FixtValue().(cca.FixtureData).Chrome
 
 	// Shorten deadline to leave time for cleanup.
-	ctxCleanup := ctx
 	ctx, cancel := ctxutil.Shorten(ctx, 3*time.Second)
 	defer cancel()
-
-	// Mute the device to avoid noisiness.
-	if err := crastestclient.Mute(ctx); err != nil {
-		s.Fatal("Failed to mute: ", err)
-	}
-	defer crastestclient.Unmute(ctxCleanup)
-
-	tconn, err := cr.TestAPIConn(ctx)
-	if err != nil {
-		s.Fatal("Failed to create Test API connection: ", err)
-	}
-
-	// Get a speech monitor for the Google TTS engine.
-	ed := a11y.TTSEngineData{
-		ExtID:                     a11y.GoogleTTSExtensionID,
-		UseOnSpeakWithAudioStream: false,
-	}
-	sm, err := a11y.RelevantSpeechMonitor(ctx, cr, tconn, ed)
-	if err != nil {
-		s.Fatal("Failed to connect to the TTS background page: ", err)
-	}
-	defer sm.Close()
 
 	ew, err := input.Keyboard(ctx)
 	if err != nil {
@@ -73,36 +45,10 @@ func CCAUIA11y(ctx context.Context, s *testing.State) {
 	visited := make(map[string]bool)
 	tab := "Tab"
 
-	if err := cpu.WaitUntilIdle(ctx); err != nil {
-		s.Error("Failed to wait CPU untile idle: ", err)
-	}
-
-	// Turning on ChromeVox by keyboard is in a11y/chromevox_toggle_on_shortcut.go
-	// Connect to ChromeVox using NewChromeVoxConn.
-	if err := a11y.SetFeatureEnabled(ctx, tconn, a11y.SpokenFeedback, true); err != nil {
-		s.Fatal("Failed to enable spoken feedback: ", err)
-	}
-	defer func() {
-		if err := a11y.SetFeatureEnabled(ctxCleanup, tconn, a11y.SpokenFeedback, false); err != nil {
-			s.Error("Failed to disable spoken feedback: ", err)
-		}
-	}()
-
-	cvconn, err := a11y.NewChromeVoxConn(ctx, cr)
-	if err != nil {
-		s.Fatal("Failed to connect to the ChromeVox background page: ", err)
-	}
-	defer cvconn.Close()
-
-	finder := nodewith.HasClass("shutter")
-	if err = cvconn.WaitForFocusedNode(ctx, tconn, finder); err != nil {
-		s.Error("Failed to wait for initial ChromeVox focus: ", err)
-	}
-
 	for true {
 		arialabel, err := app.ReturnFocusedElementAriaLabel(ctx)
 		if err != nil {
-			s.Fatal("Failed to get a focused node: ", err)
+			s.Error("Failed to get a focused node: ", err)
 		}
 
 		if visited[arialabel] {
@@ -114,14 +60,10 @@ func CCAUIA11y(ctx context.Context, s *testing.State) {
 		// spoken: Document scanning now available. Search plus Left arrow to access.
 		arialabel = strings.Replace(arialabel, "+", "plus", -1)
 
-		if err = sm.Consume(ctx, []a11y.SpeechExpectation{a11y.NewRegexExpectation(arialabel)}); err != nil {
-			s.Fatal("Failed to match speeches: ", err)
-		}
-
 		visited[arialabel] = true
 
 		if arialabel == "Take photo" {
-			if err := takePictureByKeyboard(ctx, sm, app); err != nil {
+			if err := takePictureByKeyboard(ctx, ew, app); err != nil {
 				s.Fatal("Failed to take a picture: ", err)
 			}
 		}
@@ -132,7 +74,7 @@ func CCAUIA11y(ctx context.Context, s *testing.State) {
 	}
 }
 
-func takePictureByKeyboard(ctx context.Context, sm *a11y.SpeechMonitor, app *cca.App) error {
+func takePictureByKeyboard(ctx context.Context, ew *input.KeyboardEventWriter, app *cca.App) error {
 	dir, err := app.SavedDir(ctx)
 	if err != nil {
 		return errors.Wrap(err, "failed to get result saved directory")
@@ -140,9 +82,9 @@ func takePictureByKeyboard(ctx context.Context, sm *a11y.SpeechMonitor, app *cca
 
 	start := time.Now()
 
-	space := []string{"Space"}
-	if err := a11y.PressKeysAndConsumeExpectations(ctx, sm, space, []a11y.SpeechExpectation{}); err != nil {
-		return errors.Wrap(err, "failed to press the shutter button")
+	space := "Space"
+	if err = ew.Accel(ctx, space); err != nil {
+		return errors.Wrap(err, "failed to press space key")
 	}
 
 	if err := app.WaitForState(ctx, "taking", false); err != nil {
