53b61d0fe4d39e14c6585016d352bb85e619c6d8
Arowa Suliman <arowa@google.com>
2022-11-09 14:58:52 -0800

    wifi: promote CheckIntelFWDump.
    The script intel_fw_extractor.go is updated in the previous CL (chromium:4015692).
    Move CheckIntelFWDump to run as part of the wifi_matfunc suite.
    
    BUG=b:242055720
    TEST=Ran the test in two DUTs:
    With YOYO MAGIC (Koaku, brya/gimble):
         tast run $DUT crash.KernelIwlwifiError [[ PASS ]]
         tast run $DUT wifi.CheckIntelFWDump [[ PASS ]]
    With non Memory API MAGIC (Eve-StP2):
         tast run $DUT crash.KernelIwlwifiError [[ PASS ]]
         tast run $DUT wifi.CheckIntelFWDump [[ PASS ]]
    
    Change-Id: I885ecf411b6502606230cbf8f5836877293b8858

--- src/chromiumos/tast/local/bundles/cros/wifi/check_intel_fw_dump.go
+++ src/chromiumos/tast/local/bundles/cros/wifi/check_intel_fw_dump.go
@@ -29,9 +29,7 @@ func init() {
 			"chromeos-wifi-champs@google.com", // WiFi oncall rotation; or http://b/new?component=893827
 			"cros-telemetry@google.com",
 		},
-		// TODO(b:242055720), Change the test to stable after the intel_fw_extractor.go is updated
-		// with the new changes made in fw-dump-extractor.pl.
-		Attr: []string{"group:mainline", "group:wificell", "wificell_unstable"},
+		Attr: []string{"group:mainline", "group:wificell", "wificell_func"},
 		// TODO(b:169152720), Remove "no_kernel_upstream" to enable the test to run on
 		// boards with upstream kernel when upstream iwlwifi is able to produce valid
 		// fw dumps.
