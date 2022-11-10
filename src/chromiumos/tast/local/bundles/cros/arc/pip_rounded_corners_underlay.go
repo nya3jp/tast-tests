de77b0d51416c78b46f9b4a099109e48c506edb1
Jack Rosenthal <jrosenth@chromium.org>
2022-11-10 13:18:44 -0700

    Fix group:informational labeled tests
    These should be "informational", not "group:informational".
    
    BUG=b:255335280
    TEST=fast_build.sh -T
    
    Change-Id: I4b35b74b5eb4dec9055225855c835f9405daee92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/4022164
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Auto-Submit: Jack Rosenthal <jrosenth@chromium.org>
    Owners-Override: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>

--- src/chromiumos/tast/local/bundles/cros/arc/pip_rounded_corners_underlay.go
+++ src/chromiumos/tast/local/bundles/cros/arc/pip_rounded_corners_underlay.go
@@ -24,7 +24,7 @@ func init() {
 		LacrosStatus: testing.LacrosVariantUnneeded,
 		Desc:         "Verifies that ARC++ PIP rounded corners are implemented with a hardware underlay",
 		Contacts:     []string{"amusbach@chromium.org", "oshima@chromium.org", "chromeos-perf@google.com"},
-		Attr:         []string{"group:mainline", "group:informational"},
+		Attr:         []string{"group:mainline", "informational"},
 		// Video playback doesn't work well on VM boards.
 		SoftwareDeps: []string{"chrome", "no_qemu", "proprietary_codecs"},
 		HardwareDeps: hwdep.D(hwdep.SupportsNV12Overlays()),
