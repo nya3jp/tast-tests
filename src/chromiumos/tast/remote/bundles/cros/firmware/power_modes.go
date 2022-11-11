e50c4cc6ea5cb5e0c15a9f72f03d7885f99b4608
Derek Basehore <dbasehore@google.com>
2022-11-10 17:09:52 -0800

    firmware: add Chrome EC and tablet mode angle hwdeps
    firmware.PowerModes requires both the Chrome EC and tablet mode angle
    hardware functionality, so this adds the hwdeps to the test.
    
    BUG=b:252842510
    TEST=firmware.PowerModes.warmreset
    
    Change-Id: I8dda2b8e2aecae32ea1d0e276c6690913d39529a

--- src/chromiumos/tast/remote/bundles/cros/firmware/power_modes.go
+++ src/chromiumos/tast/remote/bundles/cros/firmware/power_modes.go
@@ -39,6 +39,7 @@ func init() {
 		Contacts:     []string{"pathan.jilani@intel.com", "intel-chrome-system-automation-team@intel.com", "cros-fw-engprod@google.com"},
 		ServiceDeps:  []string{"tast.cros.ui.ScreenLockService"},
 		SoftwareDeps: []string{"chrome", "reboot"},
+		HardwareDeps: hwdep.D(hwdep.ChromeEC(), hwdep.ECFeatureTabletModeAngle()),
 		Vars: []string{"servo",
 			"firmware.mode", // Optional. Expecting "tablet". By default firmware.mode will be "clamshell".
 		},
